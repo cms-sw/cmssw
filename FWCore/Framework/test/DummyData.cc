f519048c5ddfde88174e31d09ea6875041c76ea1
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-05-29 23:41:30 +0000

    initial population

---
+++ FWCore/Framework/test/DummyData.cc
@@ -0,0 +1,13 @@
+/*
+ *  DummyData.cc
+ *
+ *  Created by Chris Jones on 4/4/05.
+ *
+ */
+
+#include "FWCore/CoreFramework/test/DummyData.h"
+using edm::eventsetup::test::DummyData;
+
+#include "FWCore/CoreFramework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(DummyData);
