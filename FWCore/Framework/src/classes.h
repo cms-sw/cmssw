3d2fda4b01bd462654227393d475f724209b11e9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-06-04 23:48:34 +0000

    Put data dictionaries in the same package as the corresponding objects

--- FWCore/Framework/src/classes.h
+++ FWCore/Framework/src/classes.h
@@ -1,1 +1,5 @@
+#include <vector>
+#include "FWCore/CoreFramework/interface/EventAux.h"
+#include "FWCore/CoreFramework/interface/Provenance.h"
+#include "FWCore/CoreFramework/interface/EventProvenance.h"
 #include "FWCore/CoreFramework/src/ToyProducts.h"
