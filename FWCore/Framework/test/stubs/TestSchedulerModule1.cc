5ac4d7ee60b761fa12426c00dcf8958186688cfd
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-06-09 15:58:10 +0000

    modified to use a different EDProduct

--- FWCore/Framework/test/stubs/TestSchedulerModule1.cc
+++ FWCore/Framework/test/stubs/TestSchedulerModule1.cc
@@ -3,7 +3,7 @@
    Test Modules for ScheduleBuilder
 
    \author Stefano ARGIRO
-   \version $Id: TestSchedulerModule1.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $
+   \version $Id: TestSchedulerModule1.cc,v 1.2 2005/06/09 08:29:12 argiro Exp $
    \date 19 May 2005
 */
 
@@ -15,13 +15,13 @@
 
 #include "FWCore/CoreFramework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/CoreFramework/test/stubs/DummyProduct.h"
+#include "FWCore/CoreFramework/src/ToyProducts.h"
 #include <memory>
 #include <string>
 
 using namespace edm;
 
-static const char CVSId[] = "$Id: TestSchedulerModule1.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $";
+static const char CVSId[] = "$Id: TestSchedulerModule1.cc,v 1.2 2005/06/09 08:29:12 argiro Exp $";
 
 class TestSchedulerModule1 : public EDProducer
 {
@@ -39,8 +39,7 @@ void TestSchedulerModule1::produce(Event& e, EventSetup const&)
 {
 
   std::string myname = pset_.getString("module_name");
-  std::auto_ptr<edmtest::DummyProduct> product(new edmtest::DummyProduct);
-  product->setName(myname);
+  std::auto_ptr<edmtest::StringProduct> product(new edmtest::StringProduct(myname)); 
   e.put(product);
 }
 
--- FWCore/Framework/test/stubs/TestSchedulerModule2.cc
+++ FWCore/Framework/test/stubs/TestSchedulerModule2.cc
@@ -3,11 +3,11 @@
    Test Module for testScheduler
 
    \author Stefano ARGIRO
-   \version $Id: TestSchedulerModule2.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $
+   \version $Id: TestSchedulerModule2.cc,v 1.2 2005/06/09 08:29:12 argiro Exp $
    \date 19 May 2005
 */
 
-static const char CVSId[] = "$Id: TestSchedulerModule2.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $";
+static const char CVSId[] = "$Id: TestSchedulerModule2.cc,v 1.2 2005/06/09 08:29:12 argiro Exp $";
 
 
 #include "FWCore/CoreFramework/interface/EDProducer.h"
@@ -17,36 +17,35 @@ static const char CVSId[] = "$Id: TestSchedulerModule2.cc,v 1.1 2005/05/29 02:29
 
 #include "FWCore/CoreFramework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/CoreFramework/test/stubs/DummyProduct.h"
+#include "FWCore/CoreFramework/src/ToyProducts.h"
 #include <memory>
 #include <string>
 
-using namespace edm;
+namespace edm{
 
-class TestSchedulerModule2 : public EDProducer
-{
- public:
-  explicit TestSchedulerModule2(ParameterSet const& p):pset_(p){}
+  class TestSchedulerModule2 : public EDProducer
+  {
+  public:
+    explicit TestSchedulerModule2(ParameterSet const& p):pset_(p){}
 
-  void produce(Event& e, EventSetup const&);
+    void produce(Event& e, EventSetup const&);
 
- private:
-  ParameterSet pset_;
-};
+  private:
+    ParameterSet pset_;
+  };
 
 
-void TestSchedulerModule2::produce(Event& e, EventSetup const&)
-{
+  void TestSchedulerModule2::produce(Event& e, EventSetup const&)
+  {
 
-  std::string myname = pset_.getString("module_name");
-  std::auto_ptr<edmtest::DummyProduct> product(new edmtest::DummyProduct);
-  product->setName(myname);
-  e.put(product);
+    std::string myname = pset_.getString("module_name");
+    std::auto_ptr<edmtest::StringProduct> product(new edmtest::StringProduct(myname));
+    e.put(product);
+    
+  }
 
-}
-
-DEFINE_FWK_MODULE(TestSchedulerModule2)
-
+  DEFINE_FWK_MODULE(TestSchedulerModule2)
+}//namespace  
 
 // Configure (x)emacs for this file ...
 // Local Variables:
