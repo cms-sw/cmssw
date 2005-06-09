f4e0d4ba3433e044d1876d9d1da1aa8562e3cfbe
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-06-09 02:43:53 +0000

    initial population

---
+++ FWCore/Skeletons/perl/Record/record.h
@@ -0,0 +1,27 @@
+nocap#ifndef SKELPACKAGE_SKELNAME_H
+nocap#define SKELPACKAGE_SKELNAME_H
+nocap// -*- C++ -*-
+nocap//
+nocap// Package:     skelpackage
+nocap// Class  :     skelname
+nocap// 
+nocap/**\class skelname skelname.h skelsubsys/skelpackage/interface/skelname.h
+nocap
+nocap Description: <one line class summary>
+nocap
+nocap Usage:
+nocap    <usage>
+nocap
+nocap*/
+nocap//
+nocap// Author:      John Doe
+nocap// Created:     day-mon-xx
+nocap// RCS(Id)
+nocap//
+nocap
+nocap#include "FWCore/CoreFramework/interface/EventSetupRecordImplementation.h"
+nocap
+nocapclass skelname : public edm::eventsetup::EventSetupRecordImplementation<skelname> {};
+nocap
+nocap#endif /* SKELPACKAGE_SKELNAME_H */
+
