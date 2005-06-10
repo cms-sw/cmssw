dc03f57048bca34a2711f033d5ac62f1ac755ccc
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-06-10 03:57:15 +0000

    add support for untracked parameters

--- FWCore/ParameterSet/interface/ParameterSet.h
+++ FWCore/ParameterSet/interface/ParameterSet.h
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------
-// $Id: ParameterSet.h,v 1.3 2005/05/19 02:36:11 chrjones Exp $
+// $Id: ParameterSet.h,v 1.1 2005/05/29 02:29:54 wmtan Exp $
 //
 // Declaration for ParameterSet(parameter set) and related types
 // ----------------------------------------------------------------------
@@ -25,113 +25,93 @@
 // ----------------------------------------------------------------------
 // contents
 
-namespace edm
-{
-  // exception
-  class ParameterSetError;
-
-  class ParameterSet;
-  bool  operator==( ParameterSet const&, ParameterSet const& );
-  bool  operator!=( ParameterSet const&, ParameterSet const& );
-
-  // forward declarations
-  class Entry;
-  class Path;
-
-}  // namespace edm
-
+namespace edm {
 
 // ----------------------------------------------------------------------
 // edm::ParameterSetError
 
-class edm::ParameterSetError
-  : public std::runtime_error
-{
-public:
-  explicit  ParameterSetError( std::string const& mesg )
-    : std::runtime_error( mesg )
-  { }
+  class ParameterSetError : public std::runtime_error {
+  public:
+    explicit ParameterSetError( std::string const& mesg ) : std::runtime_error( mesg ) {}
 
-  virtual ~ParameterSetError() throw()
-  { }
+    virtual ~ParameterSetError() throw() {}
 
-};  // ParameterSetError
+  };  // ParameterSetError
 
 
 // ----------------------------------------------------------------------
 // edm::ParameterSet
 
-class edm::ParameterSet
-{
-public:
-  // default-construct
-  ParameterSet()
-    : tbl()
-  { }
+  class ParameterSet {
+  public:
+    // default-construct
+    ParameterSet() : tbl() {}
 
-  // construct from coded string
-  explicit ParameterSet( std::string const& );
+    // construct from coded string
+    explicit ParameterSet( std::string const& );
 
-  // Entry-handling
-  Entry const&  retrieve( std::string const& ) const;
-  void           insert( bool ok_to_replace
-                         , std::string const&
-                         , Entry const&
-                         );
-  void           augment( ParameterSet const& from );
+    // Entry-handling
+    Entry const& retrieve( std::string const& ) const;
+    Entry const* const retrieveUntracked( std::string const& ) const;
+    void insert( bool ok_to_replace, std::string const& , Entry const& );
+    void augment( ParameterSet const& from );
 
-  // encode
-  std::string  toString() const;
-  std::string  toStringOfTracked() const;
+    // encode
+    std::string toString() const;
+    std::string toStringOfTracked() const;
 
-  // value accessors
+    // value accessors
 
-  // Bool, vBool
-  bool               getBool( std::string const& ) const;
+    // All untracked parameters with default values
+    template< class T >
+    T
+    getUntracked( std::string const&, T const& defaultValue ) const;
 
-  // Int32, vInt32
-  int               getInt32( std::string const& ) const;
-  std::vector<int>  getVInt32( std::string const& ) const;
+    // Bool, vBool
+    bool getBool( std::string const& ) const;
 
-  // Uint32, vUint32
-  unsigned               getUInt32( std::string const& ) const;
-  std::vector<unsigned>  getVUInt32( std::string const& ) const;
+    // Int32, vInt32
+    int              getInt32( std::string const& ) const;
+    std::vector<int> getVInt32( std::string const& ) const;
 
-  // Double, vDouble
-  double               getDouble( std::string const& ) const;
-  std::vector<double>  getVDouble( std::string const& ) const;
+    // Uint32, vUint32
+    unsigned int              getUInt32( std::string const& ) const;
+    std::vector<unsigned int> getVUInt32( std::string const& ) const;
 
-  // String, vString
-  std::string               getString( std::string const& ) const;
-  std::vector<std::string>  getVString( std::string const& ) const;
+    // Double, vDouble
+    double              getDouble( std::string const& ) const;
+    std::vector<double> getVDouble( std::string const& ) const;
 
-  // ParameterSet, vPSet
-  ParameterSet              getPSet( std::string const& ) const;
-  std::vector<ParameterSet>  getVPSet( std::string const& ) const;
+    // String, vString
+    std::string              getString( std::string const& ) const;
+    std::vector<std::string> getVString( std::string const& ) const;
 
-private:
-  typedef  std::map<std::string, Entry>  table;
-  table  tbl;
+    // ParameterSet, vPSet
+    ParameterSet              getPSet( std::string const& ) const;
+    std::vector<ParameterSet> getVPSet( std::string const& ) const;
 
-  // verify class invariant
-  void  validate() const;
+  private:
+    typedef std::map<std::string, Entry> table;
+    table tbl;
 
-  // decode
-  bool  fromString( std::string const& );
+    // verify class invariant
+    void validate() const;
 
-};  // ParameterSet
+    // decode
+    bool fromString( std::string const& );
 
+  };  // ParameterSet
 
-inline bool
-  edm::operator==( ParameterSet const& a, ParameterSet const& b )
-{ return a.toStringOfTracked() == b.toStringOfTracked(); }
+  inline bool
+  operator==( ParameterSet const& a, ParameterSet const& b ) {
+    return a.toStringOfTracked() == b.toStringOfTracked();
+  }
 
-
-inline bool
-  edm::operator!=( ParameterSet const& a, ParameterSet const& b )
-{ return !(a == b); }
-
-
+  inline bool
+  operator!=( ParameterSet const& a, ParameterSet const& b ) {
+    return !(a == b);
+  }
+} // namespace edm
 // ----------------------------------------------------------------------
 // epilog
 
