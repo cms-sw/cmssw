b4d492e5819b05af6ce4daedd06138c6ddc54069
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-06-20 15:22:03 +0000

    Added this class to encapsulate process building and validation

---
+++ FWCore/ParameterSet/interface/ProcessPSetBuilder.h
@@ -0,0 +1,89 @@
+
+/**
+   \file
+   Declaration of class ProcessPSetBuilder
+
+   \author Stefano ARGIRO
+   \version $Id$
+   \date 16 Jun 2005
+*/
+
+#ifndef _edm_ProcessPSetBuilder_h_
+#define _edm_ProcessPSetBuilder_h_
+
+static const char CVSId_edm_ProcessPSetBuilder[] = 
+"$Id$";
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace edm {
+
+  namespace pset{class Node;class WrapperNode;}
+  class ScheduleValidator;
+  class ParameterSet;
+  class ProcessDesc;
+    /**
+     \class ProcessPSetBuilder ProcessPSetBuilder.h "edm/ProcessPSetBuilder.h"
+
+     \brief Encapsulates the functionality to build a Process ParameterSet
+            from the configuration language
+
+     \author Stefano ARGIRO
+     \date 16 Jun 2005
+  */
+  class ProcessPSetBuilder {
+
+  public:
+    /// construct from the configuration language string
+    ProcessPSetBuilder( const std::string& config);
+
+    ~ProcessPSetBuilder();
+ 
+    /// get the ParameterSet that describes the process
+    boost::shared_ptr<ParameterSet> getProcessPSet() const;
+    
+    /// get the dependencies for this module
+    /** the return string is a list of comma-separated 
+      * names of the modules on which modulename depends*/
+    std::string  getDependencies(const std::string& modulename);
+
+  private:
+    
+    typedef boost::shared_ptr<pset::WrapperNode> WrapperNodePtr;
+    typedef std::vector<std::string> Strs;
+    typedef std::map<std::string, WrapperNodePtr > SeqMap;
+    typedef boost::shared_ptr<pset::Node> NodePtr;
+
+    /// recursively extract names of modules and store them in Strs;
+    void getNames(const pset::Node* n, Strs& out);
+
+    /// perform sequence substitution for this node
+    void sequenceSubstitution(NodePtr& node, SeqMap&  sequences);
+
+    /// Take a path Wrapper node and extract names
+    /** put the name of this path in @param paths
+     *  and put the names of the modules for this path in @param out */
+    void fillPath(WrapperNodePtr n, Strs& paths, ParameterSet* out);
+
+    /// diagnostic function
+    void dumpTree(NodePtr& node);
+
+    /// the validation object
+    ScheduleValidator*  validator_;
+    
+    boost::shared_ptr<ProcessDesc>   processDesc_;
+
+    boost::shared_ptr<ParameterSet> processPSet_;
+  };
+} // edm
+
+
+#endif // _edm_ProcessPSetBuilder_h_
+
+
+
+
+
---
+++ FWCore/ParameterSet/src/ProcessPSetBuilder.cc
@@ -0,0 +1,159 @@
+/**
+   \file
+   Implementation of calss ProcessPSetBuilder
+
+   \author Stefano ARGIRO
+   \version $Id$
+   \date 17 Jun 2005
+*/
+
+static const char CVSId[] = "$Id$";
+
+
+#include <FWCore/ParameterSet/interface/ProcessPSetBuilder.h>
+#include "FWCore/ParameterSet/interface/Makers.h"
+#include "FWCore/ParameterSet/interface/parse.h"
+#include "FWCore/ParameterSet/interface/Nodes.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ProcessDesc.h"
+#include "FWCore/ParameterSet/interface/Entry.h"
+
+#include "FWCore/ParameterSet/src/ScheduleValidator.h"
+
+#include "boost/shared_ptr.hpp"
+#include <vector>
+#include <map>
+#include <stdexcept>
+#include <iostream>
+
+using namespace boost;
+using namespace edm;
+using namespace edm::pset;
+using namespace std;
+
+ProcessPSetBuilder::~ProcessPSetBuilder(){delete validator_;}
+
+ProcessPSetBuilder::ProcessPSetBuilder( const std::string& config){
+  
+  boost::shared_ptr<edm::pset::NodePtrList> nodelist = 
+    edm::pset::parse(config.c_str());
+  if( 0 == nodelist.get() ) {
+    throw runtime_error("Unable to parse configuration file."
+			"  Please check the error message reported earlier.");
+  }
+  
+  processDesc_= edm::pset::makeProcess(nodelist);
+
+   SeqMap sequences;
+
+   // loop on path fragments
+   ProcessDesc::PathContainer::iterator pathIt; 
+   Strs pathnames;
+   
+   for(pathIt= processDesc_->pathFragments_.begin();
+       pathIt!=processDesc_->pathFragments_.end();
+       ++pathIt){
+     
+     if ((*pathIt)->type()=="sequence") {
+       sequences[(*pathIt)->name()]= (*pathIt);
+     }
+     
+     if ((*pathIt)->type()=="path") {
+       sequenceSubstitution((*pathIt)->wrapped_, sequences);
+       fillPath((*pathIt),pathnames,&processDesc_->pset_);
+     }
+     
+   } // loop on path fragments
+   
+   processDesc_->pset_.insert(true,"paths",Entry(pathnames,true));
+   
+   validator_= 
+     new ScheduleValidator(processDesc_->pathFragments_,processDesc_->pset_); 
+   
+   validator_->validate();
+   
+   processPSet_= 
+     shared_ptr<edm::ParameterSet>(new ParameterSet(processDesc_->pset_));
+}
+
+
+void ProcessPSetBuilder::getNames(const Node* n, Strs& out){
+  if(n->type()=="operand"){ 
+    out.push_back(n->name());
+  } else {	
+    const OperatorNode& op = dynamic_cast<const OperatorNode&>(*n);
+    getNames(op.left_.get(),out);
+    getNames(op.right_.get(),out);
+  }
+} // getNames
+
+
+void ProcessPSetBuilder::fillPath(WrapperNodePtr n, Strs&   paths,  
+			      ParameterSet* out){
+  
+  Strs names;
+  getNames(n->wrapped_.get(),names);    
+  out->insert(true,n->name(),Entry(names,true));
+  paths.push_back(n->name()); // add to the list of paths
+  
+} // fillPath(..) 
+
+
+
+void ProcessPSetBuilder::sequenceSubstitution(NodePtr& node, 
+			    SeqMap&  sequences){
+  
+  if (node->type()=="operand"){
+    SeqMap::iterator seqIt = sequences.find(node->name()); 
+    if (seqIt!= sequences.end()){
+      node = seqIt->second->wrapped_;
+    }
+  } // if operator
+  else {
+    OperatorNode* onode = dynamic_cast<OperatorNode*>(node.get());
+    
+    
+    SeqMap::iterator seqIt = sequences.find(onode->left_->name()); 
+    if (seqIt!= sequences.end()) {
+      onode->left_= seqIt->second->wrapped_;
+      onode->left_->setParent(onode);
+    }
+    seqIt = sequences.find(onode->right_->name()); 
+    if (seqIt!= sequences.end()){
+      onode->right_= seqIt->second->wrapped_; 
+      onode->right_->setParent(onode);
+    }
+    sequenceSubstitution( onode->left_, sequences);
+    sequenceSubstitution( onode->right_,sequences);
+    
+  }// else (operand)
+  
+} // sequenceSubstitution
+
+
+void ProcessPSetBuilder::dumpTree(NodePtr& node){
+  if(node->type()=="operand"){ 
+    cout << " Operand " << node->name()<< " p:";
+    if (node->getParent()) cout <<  node->getParent()->name();cout<< endl;
+  } else{	
+    OperatorNode* op = dynamic_cast<OperatorNode*>(node.get());
+    cout << " Operator: " << op->name()<<"["<<op->type()<<"]" 
+	 << " l:" << op->left_ << " r:"<<op->right_<< " p:";
+    if (op->parent_)cout<<  op->parent_->name();cout<< endl;
+    dumpTree(op->left_);
+    dumpTree(op->right_);
+  }
+} // dumpTree
+
+
+std::string  ProcessPSetBuilder::getDependencies(const std::string& modulename){
+  return validator_->dependencies(modulename);
+}
+
+
+boost::shared_ptr<edm::ParameterSet>  
+ProcessPSetBuilder::getProcessPSet() const{
+
+  return processPSet_;
+
+}
