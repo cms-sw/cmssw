c9440a34bf6dc012dc30225f9a1dc04bb6c9187a
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-06-23 19:55:34 +0000

    first submission

---
+++ FWCore/Framework/interface/ESPreFunctorDecorator.h
@@ -0,0 +1,66 @@
+#ifndef COREFRAMEWORK_ESPREFUNCTORDECORATOR_H
+#define COREFRAMEWORK_ESPREFUNCTORDECORATOR_H
+// -*- C++ -*-
+//
+// Package:     CoreFramework
+// Class  :     ESPreFunctorDecorator
+// 
+/**\class ESPreFunctorDecorator ESPreFunctorDecorator.h FWCore/CoreFramework/interface/ESPreFunctorDecorator.h
+
+ Description: A Decorator that works as a adapter to call a Functor before each call to the decorated method
+
+ Usage:
+    This Decorator can be used to create a decorator used in the ESProducer::setWhatProduced method.  This Decorator
+will adapt a Functor (a class that implements 'operator()') st that it is called before every call made to
+the ESProducer's 'produce' method.
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Wed Jun 22 11:40:41 EDT 2005
+// $Id$
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+
+namespace edm {
+   namespace eventsetup {
+
+template<class TRecord, class TFunctor >
+class ESPreFunctorDecorator
+{
+
+   public:
+      ESPreFunctorDecorator( const TFunctor& iCaller ) :
+         caller_(iCaller ) {}
+      //virtual ~ESPreFunctorDecorator();
+
+      // ---------- const member functions ---------------------
+
+      // ---------- static member functions --------------------
+
+      // ---------- member functions ---------------------------
+      void pre( const TRecord& iRecord ) {
+         caller_( iRecord );
+      }
+   
+      void post( const TRecord& iRecord ) {
+      }
+   
+   private:
+      //ESPreFunctorDecorator( const ESPreFunctorDecorator& ); // stop default
+
+      const ESPreFunctorDecorator& operator=( const ESPreFunctorDecorator& ); // stop default
+
+      // ---------- member data --------------------------------
+      TFunctor caller_;
+};
+
+   }
+}
+
+#endif /* COREFRAMEWORK_ESPREFUNCTORDECORATOR_H */
