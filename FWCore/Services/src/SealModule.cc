644218658d7c81d690d18ef9d1b099cd548c0a26
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-06-25 22:31:02 +0000

    added registration of EmptyESSource

--- FWCore/Services/src/SealModule.cc
+++ FWCore/Services/src/SealModule.cc
@@ -3,9 +3,12 @@
 #include "FWCore/CoreFramework/interface/MakerMacros.h"
 #include "FWCore/FWCoreServices/src/EmptyInputService.h"
 #include "FWCore/FWCoreServices/src/AsciiOutputModule.h"
+#include "FWCore/FWCoreServices/src/EmptyESSource.h"
+#include "FWCore/CoreFramework/interface/SourceFactory.h"
 
 namespace edm {
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_INPUT_SERVICE(EmptyInputService);
   DEFINE_ANOTHER_FWK_MODULE(AsciiOutputModule);
+  DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(EmptyESSource);
 }
