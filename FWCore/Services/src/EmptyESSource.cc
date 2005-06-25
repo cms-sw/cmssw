8a3aca061104d19085e0dfc57e3673e911073bd2
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-06-25 22:30:35 +0000

    first submission

---
+++ FWCore/Services/src/EmptyESSource.cc
@@ -0,0 +1,53 @@
+#include <set>
+#include <algorithm>
+#include <sstream>
+
+#include "FWCore/CoreFramework/interface/EventSetupRecordIntervalFinder.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/FWCoreServices/src/EmptyESSource.h"
+
+namespace edm {
+
+EmptyESSource::EmptyESSource(const edm::ParameterSet & pset) :
+   recordName_(pset.getParameter<std::string>("recordname"))
+{
+   std::vector<unsigned int> temp( pset.getParameter< std::vector<unsigned int> >("firstvalid") );
+   std::copy( temp.begin(), temp.end(), inserter(setOfIOV_ , setOfIOV_.end()));
+   
+   
+   eventsetup::EventSetupRecordKey recordKey = eventsetup::EventSetupRecordKey::TypeTag::findType( recordName_ );
+   if ( recordKey == edm::eventsetup::EventSetupRecordKey() ) {
+      std::ostringstream errorMessage;
+      errorMessage<<" The Record type named \""<<recordName_<<"\" could not be found. Please check the spelling. \n"
+         <<"If the spelling is fine, try to move the declaration of the TestingIntevalFinder to the end of the configuration file.";
+      throw std::runtime_error( errorMessage.str().c_str() );
+   }
+   findingRecordWithKey( recordKey );
+}
+  
+void 
+EmptyESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey&,
+                               const edm::Timestamp& iTime, 
+                               edm::ValidityInterval& oInterval ) {
+   oInterval = edm::ValidityInterval::invalidInterval();
+   //if no intervals given, fail immediately
+   if ( setOfIOV_.size() == 0 ) {
+      return;
+   }
+   
+   std::pair< std::set<edm::Timestamp>::iterator, 
+      std::set<edm::Timestamp>::iterator > itFound = setOfIOV_.equal_range( iTime );
+   
+   if ( itFound.first == setOfIOV_.end() ) {
+      return;
+   }
+   
+   edm::Timestamp endOfInterval = edm::Timestamp::endOfTime();
+   
+   if( itFound.second != setOfIOV_.end() ) { 
+      endOfInterval = (itFound.second->value()) -1;
+   }
+   oInterval = edm::ValidityInterval( *(itFound.first), endOfInterval);
+}
+
+}
---
+++ FWCore/Services/src/EmptyESSource.h
@@ -0,0 +1,60 @@
+#ifndef FWCORESERVICES_EMPTYESSOURCE_H
+#define FWCORESERVICES_EMPTYESSOURCE_H
+// -*- C++ -*-
+//
+// Package:     FWCoreServices
+// Class  :     EmptyESSource
+// 
+/**\class EmptyESSource EmptyESSource.h FWCore/FWCoreServices/src/EmptyESSource.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Sat Jun 25 17:23:12 EDT 2005
+// $Id$
+//
+
+// system include files
+#include <set>
+
+// user include files
+#include "FWCore/CoreFramework/interface/EventSetupRecordIntervalFinder.h"
+
+// forward declarations
+namespace edm {
+class ParameterSet;
+   
+class EmptyESSource : public  eventsetup::EventSetupRecordIntervalFinder
+{
+
+   public:
+      EmptyESSource(const ParameterSet& );
+      //virtual ~EmptyESSource();
+
+      // ---------- const member functions ---------------------
+
+      // ---------- static member functions --------------------
+
+      // ---------- member functions ---------------------------
+   void setIntervalFor( const edm::eventsetup::EventSetupRecordKey&,
+                        const edm::Timestamp& iTime, 
+                        edm::ValidityInterval& oInterval );
+      
+   private:
+      EmptyESSource( const EmptyESSource& ); // stop default
+
+      const EmptyESSource& operator=( const EmptyESSource& ); // stop default
+      
+      // ---------- member data --------------------------------
+      std::string recordName_;
+      std::set <edm::Timestamp> setOfIOV_;
+      
+};
+}
+
+#endif /* FWCORESERVICES_EMPTYESSOURCE_H */
