e91ac86649df4a6ef9b80e821add50553b0fd6fb
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-01 00:07:27 +0000

    add all the event setup tests to script

--- FWCore/Framework/test/run_all.sh
+++ FWCore/Framework/test/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #----------------------------------------------------------------------
-# $Id: run_all.sh,v 1.1 2005/05/29 02:29:54 wmtan Exp $
+# $Id: run_all.sh,v 1.2 2005/06/05 02:07:45 wmtan Exp $
 #----------------------------------------------------------------------
 
 # Pass in name and status
@@ -17,5 +17,16 @@ do_or_die maker2_t
 do_or_die EventProcessor_t
 do_or_die EventProcessor2_t
 do_or_die ScheduleExecutorFromPSet_t
-
-
+do_or_die core_eventsetup_producer_t.exe
+do_or_die core_eventsetup_proxyfactoryproducer_t.exe
+do_or_die core_eventsetup_callback_t.exe
+do_or_die core_eventsetup_proxyfactoryproducer_t.exe
+do_or_die core_eventsetup_callback_t.exe
+do_or_die core_eventsetup_products_t.exe
+do_or_die eventsetup_t
+do_or_die eventsetuprecord_t
+do_or_die dependentrecord_t
+do_or_die interval_t
+do_or_die datakey_t
+do_or_die full_chain_t
+do_or_die eventsetup_plugin_t
