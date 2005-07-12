441e111a7eaf555492b045eb62742bf90f51eb23
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-12 23:24:35 +0000

    Move from CoreFramework to Framework

---
+++ FWCore/Framework/interface/Frameworkfwd.h
@@ -0,0 +1,57 @@
+#ifndef EDM_FWD_H
+#define EDM_FWD_H
+
+/*----------------------------------------------------------------------
+  
+Forward declarations of types in the EDM.
+
+$Id: Frameworkfwd.h,v 1.6 2005/07/08 19:09:02 wmtan Exp $
+
+----------------------------------------------------------------------*/
+
+namespace edm
+{
+  class BasicHandle;
+  class BranchKey;
+  class EDAnalyzer;
+  class EDFilter;
+  class EDProducer;
+  class EDProduct;
+  class Event;
+  class EventAux;
+  class EventPrincipal;
+  class EventProvenance;
+  class EventRegistry;
+  class EventSetup;
+  class InputService;
+  class InputServiceDescription;
+  class LuminositySection;
+  class ModuleDescription;
+  class ModuleDescriptionSelector;
+  class OutputModule;
+  class ParameterSet;
+  class ProcessNameSelector;
+  class Provenance;
+  class PS_ID;
+  class RefBase;
+  class RefVectorBase;
+  class Retriever;
+  class Run;
+  class RunHandler;
+  class Selector;
+
+  template <class T> class Wrapper;
+  template <class T> class Handle;
+  template <class T> class Ref;
+  template <class T> class RefVector;
+  template <class T> class RefVectorIterator;
+}
+
+// The following are trivial enough so that the real headers can be included.
+#include "FWCore/EDProduct/interface/CollisionID.h"
+#include "FWCore/Framework/interface/ConditionsID.h"
+#include "FWCore/EDProduct/interface/EDP_ID.h"
+#include "FWCore/Framework/interface/PassID.h"
+#include "FWCore/Framework/interface/VersionNumber.h"
+
+#endif
