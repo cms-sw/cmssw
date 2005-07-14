2c13a0d9ff87ac9564e4cb131df8309972c87412
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2005-07-14 16:05:23 +0000

    commented out parts of PhysicsManager

--- SimG4Core/Physics/src/PhysicsManager.cc
+++ SimG4Core/Physics/src/PhysicsManager.cc
@@ -7,12 +7,12 @@
 PhysicsManager::PhysicsManager(seal::Context * context,edm::ParameterSet & pSet)
     : Component (context,"SimG4Core/PhysicsManager")
 {
-    edm::ParameterSet physicsSet
-          = pSet.getParameter<edm::ParameterSet>("Physics");
-    seal::Handle<PhysicsList> physics = PhysicsListFactory::get()->create
-        (physicsSet.getParameter<std::string> ("type"), context);
-    ASSERT (physics); 
-    G4RunManagerKernel * kernel = G4RunManagerKernel::GetRunManagerKernel();
-    kernel->SetPhysics(physics.get());
-    kernel->InitializePhysics();
+//     edm::ParameterSet physicsSet
+//           = pSet.getParameter<edm::ParameterSet>("Physics");
+//     seal::Handle<PhysicsList> physics = PhysicsListFactory::get()->create
+//         (physicsSet.getParameter<std::string> ("type"), context);
+//     ASSERT (physics); 
+//     G4RunManagerKernel * kernel = G4RunManagerKernel::GetRunManagerKernel();
+//     kernel->SetPhysics(physics.get());
+//     kernel->InitializePhysics();
 }
