a7a084bdea4b91b5433968066162b011aa0db5d1
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-07-19 18:21:57 +0000

    Initial version of DetId class

---
+++ DataFormats/DetId/interface/DetId.h
@@ -0,0 +1,48 @@
+#ifndef DATAFORMATS_DETID_H
+#define DATAFORMATS_DETID_H 1
+
+#include <boost/cstdint.hpp>
+#include <ostream>
+
+/** \class DetId
+    
+   $Date: $
+   $Revision: $
+*/
+class DetId {
+public:
+  enum Detector { Tracker=1,Muon=2,Ecal=3,Hcal=4 };
+  /// Create an empty or null id (also for persitence)
+  DetId();
+  /// Create an id from a raw number
+  DetId(uint32_t id);
+  /// Create an id, filling the detector and subdetector fields as specified
+  DetId(Detector det, int subdet);
+
+  /// get the detector field from this detid
+  Detector det() const { return Detector((id_>>28)&0xF); }
+  /// get the contents of the subdetector field (should be protected?)
+  int subdetId() const { return ((id_>>25)&0x7); }
+
+  uint32_t operator()() { return id_; }
+
+  /// get the raw id 
+  uint32_t rawId() const { return id_; }
+  /// is this a null id ?
+  bool null() const { return id_==0; }
+  
+  /// equality
+  int operator==(const DetId& id) const { return id_==id.id_; }
+  /// inequality
+  int operator!=(const DetId& id) const { return id_!=id.id_; }
+  /// comparison
+  int operator<(const DetId& id) const { return id_<id.id_; }
+
+protected:
+  uint32_t id_;
+};
+
+
+//std::ostream& operator<<(std::ostream& s, const DetId& id);
+
+#endif
---
+++ DataFormats/DetId/src/DetId.cc
@@ -0,0 +1,7 @@
+#include "DataFormats/DetId/interface/DetId.h"
+
+DetId::DetId() : id_(0) { }
+DetId::DetId(uint32_t id) : id_(id) { }
+DetId::DetId(Detector det, int subdet) {
+  id_=((det&0xF)<<28)|((subdet&0x7)<<25);
+}
