7b075c0da99d454bbfa9b0b7af016eaf7c64e2cf
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-07-21 20:42:52 +0000

    Added product registration. Functions produces<Product>("label") registers to an internal list, that then is read by the Worker that wraps the EDProducter

--- FWCore/Framework/interface/EDProducer.h
+++ FWCore/Framework/interface/EDProducer.h
@@ -6,12 +6,15 @@
 EDProducer: The base class of all "modules" that will insert new
 EDProducts into an Event.
 
-$Id: EDProducer.h,v 1.3 2005/07/08 00:09:38 chrjones Exp $
+$Id: EDProducer.h,v 1.4 2005/07/14 22:50:52 wmtan Exp $
+
 
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-
+#include "FWCore/Framework/src/TypeID.h"
+#include <string>
+#include <utility>
 namespace edm
 {
   class EDProducer
@@ -20,11 +23,34 @@ namespace edm
     typedef EDProducer ModuleType;
 
     virtual ~EDProducer();
-    virtual void produce(Event& e, EventSetup const& c) = 0;
+    virtual void produce(Event& e, EventSetup const& c)=0;
     virtual void beginJob( EventSetup const& ) ;
     virtual void endJob() ;
-    
+ 
+    /// declare what type of product will make and with which optional label 
+    /** the statement
+        \code
+           produces<ProductType>("optlabel");
+        \endcode
+        should be added to the producer ctor for every product */
+
+    template <class ProductType> 
+    void  produces( std::string label="" ){
+
+      ProductType aproduct;
+      productList_.push_back(std::make_pair(TypeID(aproduct), label));
+      
+    }
+
+    typedef std::list<std::pair<TypeID,std::string> > TypeLabelList;
+    /// used by the fwk to register the list of products of this module 
+    const TypeLabelList& getTypeLabelList() const;
+
+  private:
+    TypeLabelList productList_;
   };
+
+
 }
 
 #endif // EDM_EDPRODUCER_INCLUDED
