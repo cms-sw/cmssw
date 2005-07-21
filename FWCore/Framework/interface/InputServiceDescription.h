01a0e6d477207eb6cb1dcf54115065543de52040
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-07-21 20:45:47 +0000

    Modified ctor to take as argument a ptr to the ProductRegistry, to be accessed by the InputService

--- FWCore/Framework/interface/InputServiceDescription.h
+++ FWCore/Framework/interface/InputServiceDescription.h
@@ -6,23 +6,29 @@
 InputServiceDescription : the stuff that is needed to configure an
 input service that does not come in through the ParameterSet  
 
-$Id: InputServiceDescription.h,v 1.1 2005/05/29 02:29:53 wmtan Exp $
+$Id: InputServiceDescription.h,v 1.2 2005/07/14 22:50:52 wmtan Exp $
 ----------------------------------------------------------------------*/
 #include <string>
 #include "FWCore/Framework/interface/PassID.h"
 
 namespace edm
 {
+  class ProductRegistry;
+
   struct InputServiceDescription
   {
-    InputServiceDescription() : process_name(), pass() { }
-    InputServiceDescription(const std::string& name, PassID pid) :
+    InputServiceDescription() : process_name(), pass(), preg_(0) { }
+    InputServiceDescription(const std::string& name, PassID pid, 
+			    ProductRegistry* preg) :
       process_name(name),
-      pass(pid)
+      pass(pid),
+      preg_(preg)
+	 
     {}
 
     std::string process_name;
     PassID      pass;
+    ProductRegistry * preg_;
   };
 }
 
