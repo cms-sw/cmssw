1fe5cfa771e31ca5e2ba2b131b2da31c5778827d
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-22 23:53:18 +0000

    correct rule violations

--- FWCore/Framework/interface/EDProducer.h
+++ FWCore/Framework/interface/EDProducer.h
@@ -6,7 +6,7 @@
 EDProducer: The base class of all "modules" that will insert new
 EDProducts into an Event.
 
-$Id: EDProducer.h,v 1.4 2005/07/14 22:50:52 wmtan Exp $
+$Id: EDProducer.h,v 1.5 2005/07/21 20:42:52 argiro Exp $
 
 
 ----------------------------------------------------------------------*/
@@ -15,17 +15,15 @@ $Id: EDProducer.h,v 1.4 2005/07/14 22:50:52 wmtan Exp $
 #include "FWCore/Framework/src/TypeID.h"
 #include <string>
 #include <utility>
-namespace edm
-{
-  class EDProducer
-  {
+namespace edm {
+  class EDProducer {
   public:
     typedef EDProducer ModuleType;
 
     virtual ~EDProducer();
-    virtual void produce(Event& e, EventSetup const& c)=0;
-    virtual void beginJob( EventSetup const& ) ;
-    virtual void endJob() ;
+    virtual void produce(Event& e, EventSetup const& c) = 0;
+    virtual void beginJob(EventSetup const&);
+    virtual void endJob();
  
     /// declare what type of product will make and with which optional label 
     /** the statement
@@ -35,16 +33,19 @@ namespace edm
         should be added to the producer ctor for every product */
 
     template <class ProductType> 
-    void  produces( std::string label="" ){
-
+    void produces(std::string const& instanceName) {
       ProductType aproduct;
-      productList_.push_back(std::make_pair(TypeID(aproduct), label));
-      
+      productList_.push_back(std::make_pair(TypeID(aproduct), instanceName));
+    }
+
+    template <class ProductType> 
+    void produces(){
+      produces<ProductType>(std::string());
     }
 
     typedef std::list<std::pair<TypeID,std::string> > TypeLabelList;
     /// used by the fwk to register the list of products of this module 
-    const TypeLabelList& getTypeLabelList() const;
+    TypeLabelList const& getTypeLabelList() const;
 
   private:
     TypeLabelList productList_;
