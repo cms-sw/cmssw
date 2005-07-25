ba9970fe1c7515140a0ec75e5206b8a59239b9f0
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-07-25 15:11:22 +0000

    added IdealGeometryRecord

---
+++ Geometry/Records/interface/IdealGeometryRecord.h
@@ -0,0 +1,27 @@
+#ifndef RECORDS_IDEALGEOMETRYRECORD_H
+#define RECORDS_IDEALGEOMETRYRECORD_H
+// -*- C++ -*-
+//
+// Package:     Records
+// Class  :     IdealGeometryRecord
+// 
+/**\class IdealGeometryRecord IdealGeometryRecord.h Geometry/Records/interface/IdealGeometryRecord.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Mon Jul 25 11:05:09 EDT 2005
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class IdealGeometryRecord : public edm::eventsetup::EventSetupRecordImplementation<IdealGeometryRecord> {};
+
+#endif /* RECORDS_IDEALGEOMETRYRECORD_H */
+
