697344d940e7ad503372b0d3686fd0d5cc69441b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-26 23:08:32 +0000

    It's <algorithm>, not <algorithms>scramv1 b -v

--- FWCore/Framework/src/ProductRegistry.cc
+++ FWCore/Framework/src/ProductRegistry.cc
@@ -4,15 +4,15 @@
 
    \author Stefano ARGIRO
    \co-author William Tanenbaum
-   \version $Id: ProductRegistry.cc,v 1.2 2005/07/23 05:10:32 wmtan Exp $
+   \version $Id: ProductRegistry.cc,v 1.3 2005/07/26 23:03:52 wmtan Exp $
    \date 19 Jul 2005
 */
 
-static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.2 2005/07/23 05:10:32 wmtan Exp $";
+static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.3 2005/07/26 23:03:52 wmtan Exp $";
 
 
 #include <FWCore/Framework/interface/ProductRegistry.h>
-#include <algorithms>
+#include <algorithm>
 
 using namespace edm;
 
