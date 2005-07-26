5e61e6988f2e57e106f240fafbe5225fd904d8bd
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2005-07-26 10:13:49 +0000

    added templates

---
+++ Documentation/CodingRules/Template.cc
@@ -0,0 +1,17 @@
+#include "Utilities/Configuration/interface/Architecture.h"
+
+/** \file Template.cc
+ *
+ *  $Date: $
+ *  $Revision: $
+ *  \author Unknown
+ */
+
+#include "Subsystem/Package/interface/Template.h"
+
+Template::Template(){}
+
+Template::~Template(){}
+
+
+
---
+++ Documentation/CodingRules/Template.h
@@ -0,0 +1,25 @@
+#ifndef Template_H
+#define Template_H
+
+/** \class Template
+ *  To be documented here.
+ *
+ *  $Date: $
+ *  $Revision: $
+ *  \author Unknown
+ */
+
+class Template {
+ public:
+  /// Constructor
+  Template();
+
+  /// Destructor
+  virtual ~Template();
+
+ protected:
+
+ private:
+
+};
+#endif // Template_H
