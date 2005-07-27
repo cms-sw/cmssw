71d40a57c370b3f278d08ce91d100db97de32735
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-27 04:39:58 +0000

    add vector of ProductDescription pointers

--- FWCore/Framework/interface/OutputModule.h
+++ FWCore/Framework/interface/OutputModule.h
@@ -6,29 +6,33 @@
 OutputModule: The base class of all "modules" that write Events to an
 output stream.
 
-$Id: OutputModule.h,v 1.7 2005/07/23 07:32:57 wmtan Exp $
+$Id: OutputModule.h,v 1.8 2005/07/26 23:11:42 wmtan Exp $
 
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/GroupSelector.h"
+#include "FWCore/Framework/interface/ProductDescription.h"
+#include <vector>
 
 namespace edm {
   class ParameterSet;
   class EventPrincipal;
+  class ProductDescription;
   class ProductRegistry;
   class OutputModule {
   public:
     typedef OutputModule ModuleType;
 
-    explicit OutputModule(ParameterSet const& pset) : groupSelector_(pset) {}
+    explicit OutputModule(ParameterSet const& pset) : preg_(), descVec_(), groupSelector_(pset) {}
     virtual ~OutputModule();
     virtual void write(EventPrincipal const& e) = 0;
     bool selected(ProductDescription const& desc) const {return groupSelector_.selected(desc);}
-    ProductRegistry const& productRegistry() const {return *preg_;}
-    void setProductRegistry(ProductRegistry const* reg_) {preg_ = reg_;}
+    void setProductRegistry(ProductRegistry & reg_);
+  protected:
+    ProductRegistry *preg_;
+    std::vector<ProductDescription const *> descVec_;
   private:
     GroupSelector groupSelector_;
-    ProductRegistry const* preg_;
   };
 }
 
--- FWCore/Framework/src/OutputModule.cc
+++ FWCore/Framework/src/OutputModule.cc
@@ -1,12 +1,23 @@
 /*----------------------------------------------------------------------
-$Id: OutputModule.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $
+$Id: OutputModule.cc,v 1.2 2005/07/14 22:50:53 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/OutputModule.h"
+#include "FWCore/Framework/interface/ProductRegistry.h"
 
-namespace edm
-{
-  OutputModule::~OutputModule() 
-  { }
+namespace edm {
+  OutputModule::~OutputModule() {
+  }
 
+  void
+  OutputModule::setProductRegistry(ProductRegistry & reg_) {
+    reg_.sort();
+    preg_ = &reg_;  
+    for (ProductRegistry::ProductList::const_iterator it = reg_.productList().begin();
+          it != reg_.productList().end(); ++it) {
+      if (selected(*it)) {
+        descVec_.push_back(&*it);
+      }
+    }
+  }
 }
