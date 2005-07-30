f1cc7df098dd38faa0e3e5a49ba93d1878d92f4e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-30 23:47:00 +0000

    first product ID is 1, not 0

--- FWCore/Framework/src/ProductRegistry.cc
+++ FWCore/Framework/src/ProductRegistry.cc
@@ -4,11 +4,11 @@
 
    \author Stefano ARGIRO
    \co-author Bill Tanenbaum
-   \version $Id: ProductRegistry.cc,v 1.6 2005/07/27 23:39:11 wmtan Exp $
+   \version $Id: ProductRegistry.cc,v 1.7 2005/07/30 04:41:52 wmtan Exp $
    \date 19 Jul 2005
 */
 
-static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.6 2005/07/27 23:39:11 wmtan Exp $";
+static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.7 2005/07/30 04:41:52 wmtan Exp $";
 
 
 #include <FWCore/Framework/interface/ProductRegistry.h>
@@ -18,15 +18,15 @@ using namespace edm;
 
 void
 ProductRegistry::addProduct(ProductDescription& productDesc) {
-  productDesc.product_id.id_ = nextID_++;
+  productDesc.productID_.id_ = ++nextID_;
   productList_.insert(std::make_pair(BranchKey(productDesc), productDesc));
 }
 
 void
 ProductRegistry::copyProduct(ProductDescription const& productDesc) {
   productList_.insert(std::make_pair(BranchKey(productDesc), productDesc));
-  if (productDesc.product_id.id_ >= nextID_) {
-    nextID_ = productDesc.product_id.id_ + 1;
+  if (productDesc.productID_.id_ >= nextID_) {
+    nextID_ = productDesc.productID_.id_ + 1;
   }
 }
 
