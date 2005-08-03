2dcc8d9c19cbbcfd5de7cb8d87078653773aeea9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-08-03 20:51:11 +0000

    set the branchName_ on registration

--- FWCore/Framework/src/ProductRegistry.cc
+++ FWCore/Framework/src/ProductRegistry.cc
@@ -4,11 +4,11 @@
 
    \author Stefano ARGIRO
    \co-author Bill Tanenbaum
-   \version $Id: ProductRegistry.cc,v 1.7 2005/07/30 04:41:52 wmtan Exp $
+   \version $Id: ProductRegistry.cc,v 1.8 2005/07/30 23:47:00 wmtan Exp $
    \date 19 Jul 2005
 */
 
-static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.7 2005/07/30 04:41:52 wmtan Exp $";
+static const char CVSId[] = "$Id: ProductRegistry.cc,v 1.8 2005/07/30 23:47:00 wmtan Exp $";
 
 
 #include <FWCore/Framework/interface/ProductRegistry.h>
@@ -18,12 +18,14 @@ using namespace edm;
 
 void
 ProductRegistry::addProduct(ProductDescription& productDesc) {
+  productDesc.init();
   productDesc.productID_.id_ = ++nextID_;
   productList_.insert(std::make_pair(BranchKey(productDesc), productDesc));
 }
 
 void
 ProductRegistry::copyProduct(ProductDescription const& productDesc) {
+  productDesc.init();
   productList_.insert(std::make_pair(BranchKey(productDesc), productDesc));
   if (productDesc.productID_.id_ >= nextID_) {
     nextID_ = productDesc.productID_.id_ + 1;
