830e00c697aa25345f63186ac8bf7860555e4af5
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-08-04 18:16:18 +0000

    Namespace bug fixed

--- CalibFormats/HcalObjects/interface/HcalCoder.h
+++ CalibFormats/HcalObjects/interface/HcalCoder.h
@@ -11,17 +11,17 @@
     Abstract interface of a coder/decoder which converts ADC values to
     and from femtocolumbs of collected charge.
 
-   $Date: $
-   $Revision: $
+   $Date: 2005/07/27 19:44:31 $
+   $Revision: 1.1 $
 */
 class HcalCoder {
 public:
-  virtual void adc2fC(const HBHEDataFrame& df, CaloSamples& lf) const = 0;
-  virtual void adc2fC(const HODataFrame& df, CaloSamples& lf) const = 0;
-  virtual void adc2fC(const HFDataFrame& df, CaloSamples& lf) const = 0;
-  virtual void fC2adc(const CaloSamples& clf, HBHEDataFrame& df) const = 0;
-  virtual void fC2adc(const CaloSamples& clf, HFDataFrame& df) const = 0;
-  virtual void fC2adc(const CaloSamples& clf, HODataFrame& df) const = 0;
+  virtual void adc2fC(const cms::HBHEDataFrame& df, CaloSamples& lf) const = 0;
+  virtual void adc2fC(const cms::HODataFrame& df, CaloSamples& lf) const = 0;
+  virtual void adc2fC(const cms::HFDataFrame& df, CaloSamples& lf) const = 0;
+  virtual void fC2adc(const CaloSamples& clf, cms::HBHEDataFrame& df) const = 0;
+  virtual void fC2adc(const CaloSamples& clf, cms::HFDataFrame& df) const = 0;
+  virtual void fC2adc(const CaloSamples& clf, cms::HODataFrame& df) const = 0;
 };
 
 #endif
