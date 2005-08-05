32022bfb12d9570a7131a23b2e5969e80e1804a6
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2005-08-05 14:34:03 +0000

    It was a producer which was producing nothing. It is now an analyzer

--- EventFilter/EcalTBRawToDigi/test/stubs/EcalDigiDumperModule.cc
+++ EventFilter/EcalTBRawToDigi/test/stubs/EcalDigiDumperModule.cc
@@ -3,13 +3,13 @@
  * dummy module  for the test of  DaqFileInputService
  *   
  * 
- * $Date: 2005/07/13 12:56:02 $
- * $Revision: 1.2 $
+ * $Date: 2005/08/03 18:06:48 $
+ * $Revision: 1.1 $
  * \author N. Amapane - S. Argiro'
  *
 */
 
-#include <FWCore/Framework/interface/EDProducer.h>
+#include <FWCore/Framework/interface/EDAnalyzer.h>
 #include <FWCore/Framework/interface/Event.h>
 #include <FWCore/Framework/interface/MakerMacros.h>
 #include <DataFormats/EcalDigi/interface/EcalDigiCollections.h>
@@ -21,44 +21,41 @@ using namespace cms;
 using namespace std;
 
 
-  class EcalDigiDumperModule: public edm::EDProducer{
-
-  public:
-    EcalDigiDumperModule(const edm::ParameterSet& ps){}
+class EcalDigiDumperModule: public edm::EDAnalyzer{
+  
+public:
+  EcalDigiDumperModule(const edm::ParameterSet& ps){   }
+  
+protected:
+  
+  void analyze( const edm::Event & e, const  edm::EventSetup& c){
     
-  protected:
-
-    void produce( edm::Event & e, const  edm::EventSetup& c){
-      
-    // ...Reconstruction, first step are the unpacking modules to
-    // build digis...
-      
-      
-      edm::Handle<EBDigiCollection>  digis;
-      e.getByLabel("ecalEBunpacker", digis);
-
-      cout << " ^^^^^^^^^^^^^^^^^^ EcalDigiDumperModule  collection size " << digis->size() << endl;
-      
-      for ( EBDigiCollection::const_iterator digiItr = digis->begin(); digiItr != digis->end(); ++digiItr ) {
+    
+    edm::Handle<EBDigiCollection>  digis;
+    e.getByLabel("ecalEBunpacker", digis);
+    
+    cout << " ^^^^^^^^^^^^^^^^^^ EcalDigiDumperModule  collection size " << digis->size() << endl;
+    
+    for ( EBDigiCollection::const_iterator digiItr = digis->begin(); digiItr != digis->end(); ++digiItr ) {
 	
-        cout << " Dump the ADC counts for this event " << endl;
-	for ( int i=0; i< (*digiItr).size() ; ++i ) {
-	  cout <<  (*digiItr).sample(i) << " ";
-	}       
-	cout << " " << endl;
-
-
-      }	
-
-	
-     
+      cout << " Dump the ADC counts for this event " << endl;
+      for ( int i=0; i< (*digiItr).size() ; ++i ) {
+	cout <<  (*digiItr).sample(i) << " ";
+      }       
+      cout << " " << endl;
       
+      
+    }	
+    
+    
+    
+    
+    
+    
+  } // produce
+};// class EcalDigiDumperModule
 
-
-    } // produce
-  };// class EcalDigiDumperModule
- 
 DEFINE_FWK_MODULE(EcalDigiDumperModule)
-
+  
 
 
