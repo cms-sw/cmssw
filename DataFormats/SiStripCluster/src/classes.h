33f26969825b74e1bea22b968b5a3270b2cc411c
Oliver Gutsche <sha1-2307d037a116d9bb0c52272fc9aa8ec9cc58a51e@cern.ch>
2005-08-18 16:43:04 +0000

    enable SEAL dictionary generation

---
+++ DataFormats/SiStripCluster/src/classes.h
@@ -0,0 +1,14 @@
+#ifndef SISTRIPCLUSTER_CLASSES_H
+#define SISTRIPCLUSTER_CLASSES_H
+
+#include "DataFormats/SiStripCluster/interface/SiStripClusterCollection.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    edm::Wrapper<SiStripClusterCollection> siStripClusterCollectionWrapper;
+  }
+}
+
+#endif // SISTRIPCLUSTER_CLASSES_H
