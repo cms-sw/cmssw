21d8103dda10074ba55af5d051b9c7466781794c
Rick Cavanaugh <sha1-821d96d34f96c9a702c199fc2c305113785482d3@cern.ch>
2005-08-18 14:02:51 +0000

    some minor mods related to the use of std::vector

--- DataFormats/METObjects/src/classes.h
+++ DataFormats/METObjects/src/classes.h
@@ -10,6 +10,8 @@ namespace {
   edm::Wrapper<BaseMET> dummy1;
   edm::Wrapper<TowerMET> dummy2;
   edm::Wrapper<TowerMETCollection> dummy3;
+  edm::Wrapper< std::vector<TowerMET> > dummy4;
+  std::vector<TowerMET> dummy5;
 }
 }
 #endif // METOBJECTS_CLASSES_H
