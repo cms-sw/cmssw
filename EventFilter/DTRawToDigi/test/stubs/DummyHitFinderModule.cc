8236745bcd387239f148aea6ba6133dfe1a5deb1
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-08-23 09:29:47 +0000

    turned into a EDAnalyzer

--- EventFilter/DTRawToDigi/test/stubs/DummyHitFinderModule.cc
+++ EventFilter/DTRawToDigi/test/stubs/DummyHitFinderModule.cc
@@ -3,13 +3,13 @@
  * dummy module  for the test of  DaqFileInputService
  *   
  * 
- * $Date: 2005/07/13 09:06:50 $
- * $Revision: 1.1 $
+ * $Date: 2005/07/13 12:56:02 $
+ * $Revision: 1.2 $
  * \author N. Amapane - S. Argiro'
  *
 */
 
-#include <FWCore/Framework/interface/EDProducer.h>
+#include <FWCore/Framework/interface/EDAnalyzer.h>
 #include <FWCore/Framework/interface/Event.h>
 #include <FWCore/Framework/interface/MakerMacros.h>
 #include <DataFormats/DTDigis/interface/DTDigiCollection.h>
@@ -19,14 +19,14 @@
 
 
 
-  class DummyHitFinderModule: public edm::EDProducer{
+  class DummyHitFinderModule: public edm::EDAnalyzer{
 
   public:
     DummyHitFinderModule(const edm::ParameterSet& ps){}
     
   protected:
 
-    void produce( edm::Event & e, const  edm::EventSetup& c){
+    void analyze( edm::Event const & e, const  edm::EventSetup& c){
       
     // ...Reconstruction, first step are the unpacking modules to
     // build digis...
@@ -35,24 +35,25 @@
       edm::Handle<DTDigiCollection> dtdigis;
       
       e.getByLabel("dtunpacker", dtdigis);
-      // the vector of available layers
-      std::vector<DTLayerId> layerids = dtdigis->layers();
+  
 
-      for (std::vector<DTLayerId>::const_iterator layer = layerids.begin();
-	   layer!= layerids.end(); ++ layer){
-
-	for (DTDigiCollection::const_iterator digi = 
-	       dtdigis->layerBegin(*layer);
-	     digi !=dtdigis->layerEnd(*layer); ++digi ){
-	  
-	  std::cout << "Layer: "  <<digi->layer() 
-		    << " counts: "<<digi->countsTDC() << std::endl;
+      DTDigiCollection::DigiRangeIterator detUnitIt;
+      for (detUnitIt=dtdigis->begin();
+	   detUnitIt!=dtdigis->end();
+	   ++detUnitIt){
+	
+	for (DTDigiCollection::const_iterator digiIt = 
+	       detUnitIt->second.first;
+	     digiIt!=detUnitIt->second.second;
+	     ++digiIt){
+	  std::cout << "Layer: "  <<digiIt->layer() 
+		    << " counts: "<<digiIt->countsTDC() << std::endl;
 
 	}// for cells
       }// for layers
 
 
-    } // produce
+    } // analyze
   };// class DummyHitFinderModule
  
 DEFINE_FWK_MODULE(DummyHitFinderModule)
