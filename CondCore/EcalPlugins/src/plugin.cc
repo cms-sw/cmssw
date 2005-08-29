f741bc898ff20461faa162f6400e61403b7c19f8
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-29 17:52:50 +0000

    added

---
+++ CondCore/EcalPlugins/src/plugin.cc
@@ -0,0 +1,15 @@
+/*
+ *  plugin.cc
+ *  CMSSW
+ *
+ *  Created by Chris Jones on 7/24/05.
+ *
+ */
+
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondFormats/EcalObjects/interface/EcalPedestals.h"
+#include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
+
+
+DEFINE_SEAL_MODULE();
+REGISTER_PLUGIN(EcalPedestalsRcd,EcalPedestals);
