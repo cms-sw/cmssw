fd17540e86c1866e7868663bb6c28bf5506d0396
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-29 09:01:56 +0000

    removed

--- CondFormats/DataRecord/interface/ECalPedestalsRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef ECALPEDESTALSRCD_H
-#define ECALPEDESTALSRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class ECalPedestalsRcd : public edm::eventsetup::EventSetupRecordImplementation<ECalPedestalsRcd> {};
-#endif
--- CondFormats/DataRecord/interface/HCalPedestalsRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef HCALPEDESTALSRCD_H
-#define HCALPEDESTALSRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class HCalPedestalsRcd : public edm::eventsetup::EventSetupRecordImplementation<HCalPedestalsRcd> {};
-#endif
