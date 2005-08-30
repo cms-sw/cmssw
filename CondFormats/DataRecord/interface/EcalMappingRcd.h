551c437b3c4bb40d73f11aca60784a5e46b9f6eb
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-30 19:34:45 +0000

    added

---
+++ CondFormats/DataRecord/interface/EcalMappingRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALMAPPINGRCD_H
+#define ECALMAPPINGRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalMappingRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalMappingRcd> {};
+#endif
