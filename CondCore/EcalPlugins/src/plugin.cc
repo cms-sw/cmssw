a6b2a286074cb97bd5320b878abab95df0092677
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-30 19:32:27 +0000

    add EcalMapping registration

--- CondCore/EcalPlugins/src/plugin.cc
+++ CondCore/EcalPlugins/src/plugin.cc
@@ -9,7 +9,9 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
-
+#include "CondFormats/EcalObjects/interface/EcalMapping.h"
+#include "CondFormats/DataRecord/interface/EcalMappingRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(EcalPedestalsRcd,EcalPedestals);
+REGISTER_PLUGIN(EcalMappingRcd,EcalMapping);
