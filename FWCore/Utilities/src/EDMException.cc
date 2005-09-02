4821d31e281864dfa90494aacd3ab69de700bd19
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-09-02 21:14:24 +0000

    misspelled file name.  Should be EDMException.cc.

---
+++ FWCore/Utilities/src/EDMException.cc
@@ -0,0 +1,29 @@
+
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace edm {
+  namespace errors {
+    struct FilledMap {
+      FilledMap();
+      edm::Exception::CodeMap trans_;
+    };
+    FilledMap::FilledMap() {
+      EDM_MAP_ENTRY_NONS(trans_, Unknown);
+      EDM_MAP_ENTRY_NONS(trans_, ProductNotFound);
+      EDM_MAP_ENTRY_NONS(trans_, NoProductSpecified);
+      EDM_MAP_ENTRY_NONS(trans_, InsertFailure);
+      EDM_MAP_ENTRY_NONS(trans_, Configuration);
+      EDM_MAP_ENTRY_NONS(trans_, LogicError);
+      EDM_MAP_ENTRY_NONS(trans_, InvalidReference);
+      EDM_MAP_ENTRY_NONS(trans_, ModuleFailure);
+      EDM_MAP_ENTRY_NONS(trans_, ScheduleExecutionFailure);
+      EDM_MAP_ENTRY_NONS(trans_, EventProcessorFailure);
+      EDM_MAP_ENTRY_NONS(trans_, NotFound);
+    }
+  }
+
+  void getCodeTable(edm::Exception::CodeMap*& setme) {
+    static errors::FilledMap fm;
+    setme = &fm.trans_;
+  }
+}
