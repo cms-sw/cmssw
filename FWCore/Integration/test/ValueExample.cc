0ac3fa1cc2f1235b24e9180fe9a0f232784df9f3
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-09-08 10:05:38 +0000

    added Service example

---
+++ FWCore/Integration/test/ValueExample.cc
@@ -0,0 +1,70 @@
+// -*- C++ -*-
+//
+// Package:     test
+// Class  :     ValueExample
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Mon Sep  5 19:52:01 EDT 2005
+// $Id$
+//
+
+// system include files
+
+// user include files
+#include "FWCore/Integration/test/ValueExample.h"
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+ValueExample::ValueExample(const edm::ParameterSet& iPSet):
+value_( iPSet.getParameter<int>("value") )
+{
+}
+
+// ValueExample::ValueExample(const ValueExample& rhs)
+// {
+//    // do actual copying here;
+// }
+
+ValueExample::~ValueExample()
+{
+}
+
+//
+// assignment operators
+//
+// const ValueExample& ValueExample::operator=(const ValueExample& rhs)
+// {
+//   //An exception safe implementation is
+//   ValueExample temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+
+//
+// const member functions
+//
+
+//
+// static member functions
+//
+using namespace edm::serviceregistry;
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_SERVICE_MAKER(ValueExample,ParameterSetMaker<ValueExample>)
