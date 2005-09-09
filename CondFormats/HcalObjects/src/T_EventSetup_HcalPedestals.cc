a43f35761d4633ef9ab6f25cff07de0262f3f9d6
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-09-09 12:34:35 +0000

    correct

--- CondFormats/HcalObjects/src/T_EventSetup_HcalPedestals.cc
+++ CondFormats/HcalObjects/src/T_EventSetup_HcalPedestals.cc
@@ -8,13 +8,13 @@
 //
 // Author:      Chris Jones
 // Created:     Mon Apr 18 16:42:52 EDT 2005
-// $Id: T_EventSetup_EcalPedestals.cc,v 1.1 2005/08/29 17:59:30 xiezhen Exp $
+// $Id: T_EventSetup_HcalPedestals.cc,v 1.1 2005/09/09 12:11:33 xiezhen Exp $
 //
 
 // system include files
 
 // user include files
-#include "CondFormats/EcalObjects/interface/EcalPedestals.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
-EVENTSETUP_DATA_REG(EcalPedestals);
+EVENTSETUP_DATA_REG(HcalPedestals);
