c61dd961a998e8d2aab05bfa349685e4857bffd7
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-09-15 15:23:20 +0000

    added LoadAllDictionaries service

--- FWCore/Services/src/SealModule.cc
+++ FWCore/Services/src/SealModule.cc
@@ -5,14 +5,17 @@
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "FWCore/Services/src/EventSetupRecordDataGetter.h"
 #include "FWCore/Services/src/Tracer.h"
+#include "FWCore/Services/src/LoadAllDictionaries.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 using edm::AsciiOutputModule;
 using edm::EventSetupRecordDataGetter;
 using edm::EmptyESSource;
 using edm::service::Tracer;
+using edm::service::LoadAllDictionaries;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(AsciiOutputModule)
 DEFINE_ANOTHER_FWK_MODULE(EventSetupRecordDataGetter)
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(EmptyESSource)
 DEFINE_ANOTHER_FWK_SERVICE(Tracer)
+DEFINE_ANOTHER_FWK_SERVICE_MAKER(LoadAllDictionaries,edm::serviceregistry::NoArgsMaker<LoadAllDictionaries>)
