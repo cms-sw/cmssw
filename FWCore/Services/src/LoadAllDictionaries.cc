72e38120aa018668bcfb531c1b562f8577488355
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-09-28 04:20:58 +0000

    only load dictionaries of EDProducts

--- FWCore/Services/src/LoadAllDictionaries.cc
+++ FWCore/Services/src/LoadAllDictionaries.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Thu Sep 15 09:47:48 EDT 2005
-// $Id: LoadAllDictionaries.cc,v 1.1 2005/09/15 15:22:59 chrjones Exp $
+// $Id: LoadAllDictionaries.cc,v 1.2 2005/09/21 13:43:09 chrjones Exp $
 //
 
 // system include files
@@ -37,7 +37,7 @@ edm::service::LoadAllDictionaries::LoadAllDictionaries(const edm::ParameterSet& 
 {
    bool doLoad(iConfig.getUntrackedParameter("doLoad",true));
    if(doLoad) {
-      seal::PluginManager                       *db =  seal::PluginManager::get ();
+      seal::PluginManager                       *db =  seal::PluginManager::get();
       seal::PluginManager::DirectoryIterator    dir;
       seal::ModuleCache::Iterator               plugin;
       seal::ModuleDescriptor                    *cache;
@@ -47,16 +47,19 @@ edm::service::LoadAllDictionaries::LoadAllDictionaries(const edm::ParameterSet& 
       //std::cout <<"LoadAllDictionaries"<<std::endl;
       
       const std::string mycat("Capability");
+      const std::string mystring("edm::Wrapper");
       
-      for (dir = db->beginDirectories (); dir != db->endDirectories (); ++dir) {
-         for (plugin = (*dir)->begin (); plugin != (*dir)->end (); ++plugin) {
+      for (dir = db->beginDirectories(); dir != db->endDirectories(); ++dir) {
+         for (plugin = (*dir)->begin(); plugin != (*dir)->end(); ++plugin) {
             for (cache=(*plugin)->cacheRoot(), i=0; i < cache->children(); ++i) {
                //std::cout <<" "<<cache->child(i)->token(0)<<std::endl;
-               if (cache->child (i)->token (0)==mycat) {
-                  const std::string cap = cache->child (i)->token (1);
+               if (cache->child(i)->token(0) == mycat) {
+                  const std::string cap = cache->child(i)->token(1);
                   //std::cout <<"  "<<cap<<std::endl;
                   // check that cap starts with either LCGDict or LCGReflex (not really required)
-                  seal::PluginCapabilities::get()->load(cap);
+		  if (cap.find(mystring) != std::string::npos) { 
+                    seal::PluginCapabilities::get()->load(cap);
+                  }
                }
             }
          }
