9f18108f909837663db4d534e6187588fb04328f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-09-28 05:24:23 +0000

    Rename InputService to InputSource

---
+++ FWCore/Framework/interface/InputSource.h
@@ -0,0 +1,78 @@
+#ifndef Framework_InputSource_h
+#define Framework_InputSource_h
+
+
+/*----------------------------------------------------------------------
+  
+InputSource: Abstract interface for all primary input sources. Input
+sources are responsible for creating an EventPrincipal, using data
+from some source controlled by the service, and external to the
+EventPrincipal itself.
+
+The InputSource is also responsible for dealing with the "process
+name list" contained within the EventPrincipal. Each InputSource has
+to know what "process" (HLT, PROD, USER, USER1, etc.) the program is
+part of. The InputSource is repsonsible for pushing this process name
+onto the end of the process name list.
+
+For now, we specify this process name to the constructor of the
+InputSource. This should be improved.
+
+ Some questions about this remain:
+
+   1. What should happen if we "rerun" a process? i.e., if "USER1" is
+   already last in our input file, and we run again a job which claims
+   to be "USER1", what should happen? For now, we just quietly add
+   this to the history.
+
+   2. Do we need to detect a problem with a history like:
+         HLT PROD USER1 PROD
+   or is it up to the user not to do something silly? Right now, there
+   is no protection against such sillyness.
+
+Some examples of InputSource subclasses may be:
+
+ 1) EmptySource: creates EventPrincipals which contain no EDProducts.
+ 2) PoolSource: creates EventPrincipals which "contain" the data
+    read from a POOL file. This service should provide for delayed loading
+    of data, thus the quotation marks around contain.
+ 3) DAQInputSource: creats EventPrincipals which contain raw data, as
+    delivered by the L1 trigger and event builder. 
+
+$Id: InputSource.h,v 1.6 2005/09/01 23:30:49 wmtan Exp $
+
+----------------------------------------------------------------------*/
+
+#include <memory>
+#include <string>
+
+namespace edm {
+  class EventPrincipal;
+  class ProductRegistry;
+  class InputSourceDescription;
+  class InputSource {
+  public:
+    explicit InputSource(InputSourceDescription const&);
+    virtual ~InputSource();
+
+    // Indicate inability to get a new event by returning a null
+    // auto_ptr.
+    std::auto_ptr<EventPrincipal> readEvent();
+
+    ProductRegistry & productRegistry() const {return *preg_;}
+    
+  protected:
+    // A pointer to the ProductRegistry;
+    ProductRegistry * preg_;
+
+    // The process name we add to each EventPrincipal.
+    std::string const process_;
+
+  private:
+    // Indicate inability to get a new event by returning a null
+    // auto_ptr.
+    virtual std::auto_ptr<EventPrincipal> read() = 0;
+  };
+}
+
+#endif
--- FWCore/Framework/src/InputSource.cc
+++ FWCore/Framework/src/InputSource.cc
@@ -1,25 +1,25 @@
 /*----------------------------------------------------------------------
-$Id: InputService.cc,v 1.6 2005/07/27 23:38:22 wmtan Exp $
+$Id: InputSource.cc,v 1.7 2005/07/30 23:47:52 wmtan Exp $
 ----------------------------------------------------------------------*/
 #include <cassert>
 
-#include "FWCore/Framework/interface/InputService.h"
-#include "FWCore/Framework/interface/InputServiceDescription.h"
+#include "FWCore/Framework/interface/InputSource.h"
+#include "FWCore/Framework/interface/InputSourceDescription.h"
 #include "FWCore/Framework/interface/EventPrincipal.h"
 
 namespace edm {
 
-  InputService::InputService(InputServiceDescription const& desc) :
+  InputSource::InputSource(InputSourceDescription const& desc) :
       preg_(desc.preg_),
       process_(desc.processName_) {
     assert(preg_ != 0);
     assert(!process_.empty());
   }
 
-  InputService::~InputService() {}
+  InputSource::~InputSource() {}
 
   std::auto_ptr<EventPrincipal>
-  InputService::readEvent() {
+  InputSource::readEvent() {
     // Do we need any error handling (e.g. exception translation)
     // here?
     std::auto_ptr<EventPrincipal> ep(this->read());
