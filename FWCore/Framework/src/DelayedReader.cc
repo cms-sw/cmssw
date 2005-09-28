8bafae241748bca14c07f23d45a4d9d516063b87
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-09-28 04:51:23 +0000

    Rename Retriever to DelayedReader

---
+++ FWCore/Framework/src/DelayedReader.cc
@@ -0,0 +1,14 @@
+#include "FWCore/Framework/interface/DelayedReader.h"
+
+/*----------------------------------------------------------------------
+  
+$Id: DelayedReader.cc,v 1.2 2005/07/14 22:50:53 wmtan Exp $
+
+----------------------------------------------------------------------*/
+
+
+namespace edm
+{
+  DelayedReader::~DelayedReader() 
+  { };
+}
