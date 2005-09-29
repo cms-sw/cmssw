b46fdc34077542f790989a659cf29a6624d14d1f
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-09-29 08:55:18 +0000

    added

---
+++ CondFormats/DataRecord/interface/CSCXtalkRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCXTALKRCD_H
+#define CSCXTALKRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCXtalkRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCXtalkRcd> {};
+#endif
---
+++ CondFormats/DataRecord/src/CSCPedestalsRcd.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/DataRecord/interface/CSCPedestalsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(CSCPedestalsRcd);
