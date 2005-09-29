c2a429cf50a2cbe3fe6f26bbd533f4ffe38d57e9
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-09-29 23:19:43 +0000

    Put plugin for Hcal calibrations Move Hcal calibration machinery test to "test"

--- CondCore/HcalPlugins/src/plugin.cc
+++ CondCore/HcalPlugins/src/plugin.cc
@@ -8,8 +8,17 @@
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/HcalObjects/interface/HcalPedestals.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestalWidths.h"
+#include "CondFormats/HcalObjects/interface/HcalGains.h"
+#include "CondFormats/HcalObjects/interface/HcalGainWidths.h"
 #include "CondFormats/DataRecord/interface/HcalPedestalsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalPedestalWidthsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalGainsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalGainWidthsRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(HcalPedestalsRcd,HcalPedestals);
+REGISTER_PLUGIN(HcalPedestalWidthsRcd,HcalPedestalWidths);
+REGISTER_PLUGIN(HcalGainsRcd,HcalGains);
+REGISTER_PLUGIN(HcalGainWidthsRcd,HcalGainWidths);
 
