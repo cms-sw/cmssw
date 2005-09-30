ad3853f8f6495ba3743b421089d84568b8b1aae9
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-09-30 15:17:26 +0000

    Hcal calibrations data records

---
+++ CondFormats/DataRecord/interface/HcalGainWidthsRcd.h
@@ -0,0 +1,7 @@
+// this file is generated automatically by /afs/fnal.gov/files/home/room1/ratnikov/bin/makeNewClass.sh
+// name: ratnikov, date: Mon Sep 26 17:02:44 CDT 2005
+#ifndef HcalGainWidthsRcd_H
+#define HcalGainWidthsRcd_H
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class HcalGainWidthsRcd : public edm::eventsetup::EventSetupRecordImplementation<HcalGainWidthsRcd> {};
+#endif
