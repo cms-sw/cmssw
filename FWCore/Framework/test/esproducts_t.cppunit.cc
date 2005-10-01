4cd314a4b8a79648bb759fc08cc7136ca324d6d8
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-10-01 18:08:01 +0000

    modified to match new namespace use added test for direct use of 'produced' object

--- FWCore/Framework/test/esproducts_t.cppunit.cc
+++ FWCore/Framework/test/esproducts_t.cppunit.cc
@@ -10,17 +10,20 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "FWCore/Framework/interface/ESProducts.h"
-using namespace edm::eventsetup::produce;
+using edm::ESProducts;
+using edm::es::products;
 
 class testEsproducts: public CppUnit::TestFixture
 {
 CPPUNIT_TEST_SUITE(testEsproducts);
 CPPUNIT_TEST(constPtrTest);
+CPPUNIT_TEST(manyTest);
 CPPUNIT_TEST_SUITE_END();
 public:
   void setUp(){}
   void tearDown(){}
   void constPtrTest();
+  void manyTest();
 };
 
 ///registration of the test so that the runner can find it
@@ -49,6 +52,34 @@ void testEsproducts::constPtrTest()
    
    CPPUNIT_ASSERT(readInt == &int_);
    CPPUNIT_ASSERT(readFloat == &float_);
+}
+
+ESProducts<const int*, const float*, const double*>
+returnManyPointers(const int* iInt, const float* iFloat, const double* iDouble)
+{
+   return edm::es::produced << iInt << iFloat << iDouble;
+}
+
+void testEsproducts::manyTest()
+{
+   int int_ = 0;
+   float float_ = 0;
+   double double_ = 0;
+   
+   ESProducts<const int*, const float*, const double*> product = 
+      returnManyPointers(&int_, &float_,&double_);
+   
+   const int* readInt = 0;
+   const float* readFloat = 0;
+   const double* readDouble = 0;
+   
+   product.assignTo(readInt);
+   product.assignTo(readFloat);
+   product.assignTo(readDouble);
+   
+   CPPUNIT_ASSERT(readInt == &int_);
+   CPPUNIT_ASSERT(readFloat == &float_);
+   CPPUNIT_ASSERT(readDouble == &double_);
 }
 
    
