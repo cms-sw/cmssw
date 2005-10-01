a04c9741b1f8b00492db7daa583e1a4a751e8166
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-10-01 19:30:39 +0000

    removed modules now in DSTAlgos

--- PhysicsTools/CandAlgos/src/SealModule.cc
+++ PhysicsTools/CandAlgos/src/SealModule.cc
@@ -1,7 +1,5 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "PhysicsTools/CandAlgos/src/DSTPreOutput.h"
-#include "PhysicsTools/CandAlgos/src/TrackCandidateProducer.h"
 #include "PhysicsTools/CandAlgos/src/TwoSameBodyCombiner.h"
 #include "PhysicsTools/CandAlgos/src/TwoDifferentBodyCombiner.h"
 #include "PhysicsTools/CandAlgos/interface/SelectorProducer.h"
@@ -14,8 +12,6 @@ namespace phystools {
   typedef SelectorProducer<PtMinSelector> PtMinSelectorProducer;
   typedef SelectorProducer<MassWindowSelector> MassWindowSelectorProducer;
   
-  DEFINE_ANOTHER_FWK_MODULE( DSTPreOutput );
-  DEFINE_ANOTHER_FWK_MODULE( TrackCandidateProducer );
   DEFINE_ANOTHER_FWK_MODULE( TwoSameBodyCombiner );
   DEFINE_ANOTHER_FWK_MODULE( TwoDifferentBodyCombiner );
   DEFINE_ANOTHER_FWK_MODULE( PtMinSelectorProducer );
