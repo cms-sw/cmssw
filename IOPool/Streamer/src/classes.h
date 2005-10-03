96727987407d563ea05d820e26d980533f1c80d0
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-10-03 18:49:42 +0000

    remove unnecessary definitions

--- IOPool/Streamer/src/classes.h
+++ IOPool/Streamer/src/classes.h
@@ -6,14 +6,6 @@
 #include "FWCore/EDProduct/interface/Wrapper.h"
 
 namespace { namespace {
-  edm::ProdPair dummy1;
-  edm::SendProds dummy2;
-  edm::SendEvent dummy3;
-  edm::SendDescs dummy4;
-  edm::ProductDescription dummy5;
-  edm::BranchEntryDescription dummy6;
-  edmtestprod::StreamTestThing dummy7;
   edm::Wrapper<edmtestprod::StreamTestThing> dummy8;
-  edm::SendJobHeader dummy9;
 }}
 
