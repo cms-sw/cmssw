33ea78638ceadbcf726d38b96050a471fcba9ed6
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-04 14:09:13 +0000

    Removed cms namespace

--- DataFormats/CaloRecHit/interface/CaloRecHit.h
+++ DataFormats/CaloRecHit/interface/CaloRecHit.h
@@ -1,32 +1,30 @@
-#ifndef CALORECHIT_H
-#define CALORECHIT_H 1
+#ifndef DATAFORMATS_CALORECHIT_CALORECHIT_H
+#define DATAFORMATS_CALORECHIT_CALORECHIT_H 1
 
 #include "DataFormats/DetId/interface/DetId.h"
 #include <ostream>
 
-namespace cms {
 
-  /** \class CaloRecHit
-    
-  $Date: 2005/09/15 14:44:25 $
-  $Revision: 1.2 $
-  \author J. Mans - Minnesota
-  */
-  class CaloRecHit {
-  public:
-    CaloRecHit(); // for persistence
-    explicit CaloRecHit(const DetId& id, float energy, float time);
-    virtual ~CaloRecHit();
-    float energy() const { return energy_; }
-    float time() const { return time_; }
-    const DetId& detid() const { return id_; }
-  private:
-    DetId id_;
-    float energy_;
-    float time_;
-  };
+/** \class CaloRecHit
+ * 
+ * $Date: 2005/09/26 14:10:28 $
+ * $Revision: 1.3 $
+ *\author J. Mans - Minnesota
+ */
+class CaloRecHit {
+public:
+  CaloRecHit(); // for persistence
+  explicit CaloRecHit(const DetId& id, float energy, float time);
+  virtual ~CaloRecHit();
+  float energy() const { return energy_; }
+  float time() const { return time_; }
+  const DetId& detid() const { return id_; }
+private:
+  DetId id_;
+  float energy_;
+  float time_;
+};
 
-  std::ostream& operator<<(std::ostream& s, const CaloRecHit& hit);
-}
+std::ostream& operator<<(std::ostream& s, const CaloRecHit& hit);
   
 #endif
--- DataFormats/CaloRecHit/src/CaloRecHit.cc
+++ DataFormats/CaloRecHit/src/CaloRecHit.cc
@@ -1,18 +1,16 @@
 #include "DataFormats/CaloRecHit/interface/CaloRecHit.h"
 
-namespace cms {
 
-  CaloRecHit::CaloRecHit() : energy_(0), time_(0) {
-  }
-  
-  CaloRecHit::CaloRecHit(const DetId& id, float energy, float time) : id_(id),energy_(energy), time_(time) {
-  }
-  
-  CaloRecHit::~CaloRecHit() {
-  }
-
-  std::ostream& operator<<(std::ostream& s, const CaloRecHit& hit) {
-    return s << hit.detid().rawId() << ", " << hit.energy() << " GeV, " << hit.time() << " ns";
-  }
-
+CaloRecHit::CaloRecHit() : energy_(0), time_(0) {
 }
+
+CaloRecHit::CaloRecHit(const DetId& id, float energy, float time) : id_(id),energy_(energy), time_(time) {
+}
+
+CaloRecHit::~CaloRecHit() {
+}
+
+std::ostream& operator<<(std::ostream& s, const CaloRecHit& hit) {
+  return s << hit.detid().rawId() << ", " << hit.energy() << " GeV, " << hit.time() << " ns";
+}
+
