707aba270963beb08462d0a91bc87933e6bdd0ae
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-04 20:33:14 +0000

    Switch to iterators, drop HappySelector

--- DataFormats/HcalRecHit/test/HcalRecHitDump.cc
+++ DataFormats/HcalRecHit/test/HcalRecHitDump.cc
@@ -13,8 +13,8 @@ namespace cms {
 
   /** \class HcalRecHitDump
       
-  $Date: $
-  $Revision: $
+  $Date: 2005/08/04 18:14:50 $
+  $Revision: 1.1 $
   \author J. Mans - Minnesota
   */
   class HcalRecHitDump : public edm::EDAnalyzer {
@@ -22,19 +22,6 @@ namespace cms {
     explicit HcalRecHitDump(edm::ParameterSet const& conf);
     virtual void analyze(edm::Event const& e, edm::EventSetup const& c);
   };
-
-
-
- class HRHHappySelector : public edm::Selector {
-  public:
-    HRHHappySelector() { }
-  private:
-    virtual bool doMatch(const edm::Provenance& p) const {
-      //      cout << p << endl;
-      return true;
-    }
- };
-
 
   HcalRecHitDump::HcalRecHitDump(edm::ParameterSet const& conf) {
   }
@@ -44,45 +31,40 @@ namespace cms {
     std::vector<edm::Handle<HORecHitCollection> > ho;
     std::vector<edm::Handle<HFRecHitCollection> > hf;
     
-    HRHHappySelector s;
-    
     try {
-      e.getMany(s,hbhe);
-      //      cout << "Selected " << hbhe.size() << endl;
+      e.getManyByType(hbhe);
       std::vector<edm::Handle<HBHERecHitCollection> >::iterator i;
       for (i=hbhe.begin(); i!=hbhe.end(); i++) {
 	const HBHERecHitCollection& c=*(*i);
 	
-	for (unsigned int j=0; j<c.size(); j++)
-	  cout << c[j] << std::endl;
+	for (HBHERecHitCollection::const_iterator j=c.begin(); j!=c.end(); j++) 
+	  cout << *j << std::endl;
       }
     } catch (...) {
       cout << "No HB/HE RecHits." << endl;
     }
     
     try {
-      e.getMany(s,hf);
-      //      cout << "Selected " << hf.size() << endl;
+      e.getManyByType(hf);
       std::vector<edm::Handle<HFRecHitCollection> >::iterator i;
       for (i=hf.begin(); i!=hf.end(); i++) {
 	const HFRecHitCollection& c=*(*i);
 	
-	for (unsigned int j=0; j<c.size(); j++)
-	  cout << c[j] << std::endl;
+	for (HFRecHitCollection::const_iterator j=c.begin(); j!=c.end(); j++) 
+	  cout << *j << std::endl;
       }
     } catch (...) {
       cout << "No HF RecHits." << endl;
     }
     
     try {
-      e.getMany(s,ho);
-      //      cout << "Selected " << ho.size() << endl;
+      e.getManyByType(ho);
       std::vector<edm::Handle<HORecHitCollection> >::iterator i;
       for (i=ho.begin(); i!=ho.end(); i++) {
 	const HORecHitCollection& c=*(*i);
 	
-	for (unsigned int j=0; j<c.size(); j++)
-	  cout << c[j] << std::endl;
+	for (HORecHitCollection::const_iterator j=c.begin(); j!=c.end(); j++) 
+	  cout << *j << std::endl;
       }
     } catch (...) {
       cout << "No HO RecHits." << endl;
