d4974bf779d2a15d9982a2fa715a69d7d4d8790e
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-06 16:02:37 +0000

    Added code to create ROOT scripts drawing geometries

--- Geometry/CaloEventSetup/test/CaloGeometryAnalyzer.cc
+++ Geometry/CaloEventSetup/test/CaloGeometryAnalyzer.cc
@@ -29,7 +29,10 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include <fstream>
 
 //
 // class decleration
@@ -44,6 +47,8 @@ class CaloGeometryAnalyzer : public edm::EDAnalyzer {
       virtual void analyze( const edm::Event&, const edm::EventSetup& );
    private:
       // ----------member data ---------------------------
+  void build(const CaloGeometry& cg, DetId::Detector det, HcalSubdetector subdetn, const char* name);
+  int pass_;
 };
 
 //
@@ -60,7 +65,7 @@ class CaloGeometryAnalyzer : public edm::EDAnalyzer {
 CaloGeometryAnalyzer::CaloGeometryAnalyzer( const edm::ParameterSet& iConfig )
 {
    //now do what ever initialization is needed
-
+  pass_=0;
 }
 
 
@@ -73,6 +78,37 @@ CaloGeometryAnalyzer::~CaloGeometryAnalyzer()
 }
 
 
+void CaloGeometryAnalyzer::build(const CaloGeometry& cg, DetId::Detector det, HcalSubdetector subdetn, const char* name) {
+  std::fstream f(name,std::ios_base::out);
+  const CaloSubdetectorGeometry* geom=cg.getSubdetectorGeometry(det,subdetn);
+
+  f << "{" << std::endl;
+  f << "  TGeoManager* geoManager = new TGeoManager(\"ROOT\", \"" << name << "\");" << std::endl;
+  f << "  TGeoMaterial* dummyMaterial = new TGeoMaterial(\"Vacuum\", 0,0,0); " << std::endl;
+  f << "  TGeoMedium* dummyMedium =  new TGeoMedium(\"Vacuum\",1,dummyMaterial);" << std::endl;
+  f << "  TGeoVolume* world=geoManager->MakeBox(\"world\",dummyMedium, 8000.0, 8000.0, 14000.0); " << std::endl;
+  f << "  geoManager->SetTopVolume(world); " << std::endl;
+  f << "  TGeoVolume* box; " << std::endl;
+  int n=0;
+  std::vector<DetId> ids=geom->getValidDetIds(det,subdetn);
+  for (std::vector<DetId>::iterator i=ids.begin(); i!=ids.end(); i++) {
+    n++;
+    const CaloCellGeometry* cell=geom->getGeometry(*i);
+    f << "  // " << HcalDetId(*i) << std::endl;
+    if (subdetn==HcalForward) 
+      f << "  box=geoManager->MakeBox(\"point\",dummyMedium,1.0,1.0,1.0);" << std::endl;
+    else
+      f << "  box=geoManager->MakeBox(\"point\",dummyMedium,3.0,3.0,3.0);" << std::endl;
+    f << "  world->AddNode(box,"<< n << ",new TGeoHMatrix(TGeoTranslation(" << 
+      cell->getPosition().x() << "," << cell->getPosition().y() << "," << cell->getPosition().z() << ")));" << std::endl;
+    //   f << (HcalDetId)(*i) << " " << cell->getPosition() << std::endl;
+  }
+  f << "  geoManager->CloseGeometry();" << std::endl;
+  f << "world->Voxelize(\"\"); // now the new geometry is valid for tracking, so you can do \n // even raytracing \n //  if (!canvas) { \n    TCanvas* canvas=new TCanvas(\"EvtDisp\",\"EvtDisp\",500,500); \n //  } \n  canvas->Modified(); \n  canvas->Update();      \n  world->Draw(); \n";
+  f << "}" << std::endl;
+  f.close();
+}
+
 //
 // member functions
 //
@@ -82,13 +118,22 @@ void
 CaloGeometryAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup )
 {
    using namespace edm;
-
+   
    std::cout << "Here I am " << std::endl;
+
+   edm::ESHandle<CaloGeometry> pG;
+   iSetup.get<IdealGeometryRecord>().get(pG);     
    //
    // get the hcal geometry
    //
-   edm::ESHandle<CaloGeometry> pG;
-   iSetup.get<IdealGeometryRecord>().get(pG);     
+   if (pass_==0) {
+     build(*pG,DetId::Hcal,HcalBarrel,"hb.C");
+     build(*pG,DetId::Hcal,HcalEndcap,"he.C");
+     build(*pG,DetId::Hcal,HcalOuter,"ho.C");
+     build(*pG,DetId::Hcal,HcalForward,"hf.C");
+   }
+
+   pass_++;
       
 }
 
