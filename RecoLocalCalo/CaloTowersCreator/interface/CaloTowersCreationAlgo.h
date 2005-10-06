3fd46a7193f3b5c849fb4cfa186e676fdbc926d0
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2005-10-06 01:11:40 +0000

    not-compiling preliminary version

--- RecoLocalCalo/CaloTowersCreator/interface/CaloTowersCreationAlgo.h
+++ RecoLocalCalo/CaloTowersCreator/interface/CaloTowersCreationAlgo.h
@@ -2,33 +2,63 @@
 #define RECOLOCALCALO_CALOTOWERSCREATOR_CALOTOWERSCREATIONALGO_H 1
 
 #include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
-#include "Geometry/CaloTopology/interface/HcalTopology.h"
-#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include <map>
+class HcalTopology;
+class CaloGeometry;
+class CaloRecHit;
+class DetId;
 
 /** \class CaloTowersCreationAlgo
   *  
-  * $Date: $
-  * $Revision: $
+  * $Date: 2005/10/05 17:02:05 $
+  * $Revision: 1.1 $
   * \author J. Mans - Minnesota
   */
 class CaloTowersCreationAlgo {
 public:
   CaloTowersCreationAlgo(const HcalTopology* topo, const CaloGeometry* geo);
 
-  bool create(const CaloTowerDetId& id, CaloTowerCollection& destCollection,
+  CaloTowersCreationAlgo(double EBthreshold, double EEthreshold, double HcalThreshold,
+    double HBthreshold, double HESthreshold, double HEDthreshold,
+    double HOthreshold, double HF1threshold, double HF2threshold,
+    double EBweight, double EEweight, 
+    double HBweight, double HESweight, double HEDweight, 
+    double HOweight, double HF1weight, double HF2weight,
+    double EcutTower, double EBSumThreshold, double EESumThreshold,
+    const HcalTopology* topo, const CaloGeometry* geo, bool useHODefault);
+
+  bool create(CaloTowerCollection& destCollection,
 	      const HBHERecHitCollection& hbhe, 
 	      const HORecHitCollection& ho, 
-	      const HFRecHitCollection& hf) const; // eventually will need ECAL also.
+	      const HFRecHitCollection& hf); // eventually will need ECAL also.
 
 private:
-  void hadForTower(const CaloTowerDetId& id, const HBHERecHitCollection& hbhe, std::vector<const HBHERecHit*>& hits) const;
-  const HORecHit* outerForTower(const CaloTowerDetId& id, const HORecHitCollection& ho) const;
-  void forwardForTower(const CaloTowerDetId& id, const HFRecHitCollection& hf, std::vector<const HFRecHit*>& hits) const;
-  
-  void reconstructTower(CaloTower& tower,const std::vector<const HBHERecHit*>& hbhe, const HORecHit* ho, const std::vector<const HFRecHit*>& hf) const;
-  const HcalTopology* theTopology;
+  /// adds a single hit to the tower
+  void assignHit(const CaloRecHit * recHit);
+
+  /// looks for a given tower in the internal cache.  If it can't find it, it makes it.
+  CaloTower & find(CaloTowerDetId & id);
+
+  /// helper method to look up the appropriate threshold & weight
+  void getThresholdAndWeight(const DetId & detId, double & threshold, double & weight) const;
+
+  double theEBthreshold, theEEthreshold, theHcalThreshold;
+  double theHBthreshold, theHESthreshold,  theHEDthreshold; 
+  double theHOthreshold, theHF1threshold, theHF2threshold;
+  double theEBweight, theEEweight; 
+  double theHBweight, theHESweight, theHEDweight, theHOweight, theHF1weight, theHF2weight;
+  double theEcutTower, theEBSumThreshold, theEESumThreshold;
+
+  const HcalTopology* theHcalTopology;
   const CaloGeometry* theGeometry;
+
+  bool theHOIsUsedByDefault;
+
+  // internal map
+  typedef std::map<CaloTowerDetId, CaloTower *> CaloTowerMap;
+  CaloTowerMap theCaloTowerMap;
 };
 
 #endif
--- RecoLocalCalo/CaloTowersCreator/src/CaloTowersCreationAlgo.cc
+++ RecoLocalCalo/CaloTowersCreator/src/CaloTowersCreationAlgo.cc
@@ -1,77 +1,206 @@
 #include "RecoLocalCalo/CaloTowersCreator/interface/CaloTowersCreationAlgo.h"
+#include "Geometry/CaloTopology/interface/HcalTopology.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "Geometry/CaloTopology/interface/CaloTowerTopology.h"
+#include<iostream>
 
-/* 
-   Algorithm is to loop over all possible towers and see if any of the tower constituents are present.
-*/
-void CaloTowersCreationAlgo::hadForTower(const CaloTowerDetId& id, const HBHERecHitCollection& hbhe, std::vector<const HBHERecHit*>& hits) const {
-  // create all valid HcalBarrel and HcalEndcap detids at this position
-  HBHERecHitCollection::const_iterator i;
-
-  if (id.ietaAbs()<=theTopology->lastHBRing()) {
-    i=hbhe.find(HcalDetId(HcalBarrel,id.ieta(),id.iphi(),1));
-    if (i!=hbhe.end()) hits.push_back(&(*i));
-    i=hbhe.find(HcalDetId(HcalBarrel,id.ieta(),id.iphi(),2));
-    if (i!=hbhe.end()) hits.push_back(&(*i));
-  }
-  if (id.ietaAbs()>=theTopology->firstHERing() && id.ietaAbs()<=theTopology->lastHERing()) {
-    i=hbhe.find(HcalDetId(HcalEndcap,id.ieta(),id.iphi(),1));
-    if (i!=hbhe.end()) hits.push_back(&(*i));
-    i=hbhe.find(HcalDetId(HcalEndcap,id.ieta(),id.iphi(),2));
-    if (i!=hbhe.end()) hits.push_back(&(*i));
-    i=hbhe.find(HcalDetId(HcalEndcap,id.ieta(),id.iphi(),3));
-    if (i!=hbhe.end()) hits.push_back(&(*i));
-  }
+CaloTowersCreationAlgo::CaloTowersCreationAlgo(const HcalTopology* topo, const CaloGeometry* geo)
+ : theEBthreshold(-1000.),
+   theEEthreshold(-1000.),
+   theHcalThreshold(-1000.),
+   theHBthreshold(-1000.),
+   theHESthreshold(-1000.),
+   theHEDthreshold(-1000.),
+   theHOthreshold(-1000.),
+   theHF1threshold(-1000.),
+   theHF2threshold(-1000.),
+   theEBweight(1.),
+   theEEweight(1.),
+   theHBweight(1.),
+   theHESweight(1.),
+   theHEDweight(1.),
+   theHOweight(1.),
+   theHF1weight(1.),
+   theHF2weight(1.),
+   theEcutTower(-1000.),
+   theEBSumThreshold(-1000.),
+   theEESumThreshold(-1000.),
+   theHcalTopology(topo),
+   theGeometry(geo),
+   theHOIsUsedByDefault(true)
+{
 }
 
-const HORecHit* CaloTowersCreationAlgo::outerForTower(const CaloTowerDetId& id, const HORecHitCollection& ho) const {
-  // create any HcalOuter detid at this position
-  HORecHitCollection::const_iterator i;
-  const HORecHit* retval=0;
 
-  if (id.ietaAbs()<=theTopology->lastHORing()) {
-    i=ho.find(HcalDetId(HcalOuter,id.ieta(),id.iphi(),4)); // TODO: replace magic number!
-    if (i!=ho.end()) retval=&(*i);
-  }
-  return retval;
+CaloTowersCreationAlgo::CaloTowersCreationAlgo(double EBthreshold, double EEthreshold, double HcalThreshold,
+    double HBthreshold, double HESthreshold, double  HEDthreshold,
+    double HOthreshold, double HF1threshold, double HF2threshold,
+    double EBweight, double EEweight,
+    double HBweight, double HESweight, double HEDweight,
+    double HOweight, double HF1weight, double HF2weight,
+    double EcutTower, double EBSumThreshold, double EESumThreshold,
+    const HcalTopology* topo, const CaloGeometry* geo, bool useHODefault)
+
+ : theEBthreshold(EBthreshold),
+   theEEthreshold(EEthreshold),
+   theHcalThreshold(HcalThreshold),
+   theHBthreshold(HBthreshold),
+   theHESthreshold(HESthreshold),
+   theHEDthreshold(HEDthreshold),
+   theHOthreshold(HOthreshold),
+   theHF1threshold(HF1threshold),
+   theHF2threshold(HF2threshold),
+   theEBweight(EBweight),
+   theEEweight(EEweight),
+   theHBweight(HBweight),
+   theHESweight(HESweight),
+   theHEDweight(HEDweight),
+   theHOweight(HOweight),
+   theHF1weight(HF1weight),
+   theHF2weight(HF2weight),
+   theEcutTower(EcutTower),
+   theEBSumThreshold(EBSumThreshold),
+   theEESumThreshold(EESumThreshold),
+   theHcalTopology(topo),
+   theGeometry(geo),
+   theHOIsUsedByDefault(useHODefault)
+{
 }
 
-void CaloTowersCreationAlgo::forwardForTower(const CaloTowerDetId& id, const HFRecHitCollection& hf, std::vector<const HFRecHit*>& hits) const {
-  // create all valid HcalForward detids at this position
-  HFRecHitCollection::const_iterator i;
+void CaloTowersCreationAlgo::create(CaloTowerCollection & result, const HBHERecHitCollection& hbhe, 
+                            const HORecHitCollection & ho, const HFRecHitCollection& hf)
+{
 
-  if (id.ietaAbs()>=theTopology->firstHFRing()) {
-    i=hf.find(HcalDetId(HcalForward,id.ieta(),id.iphi(),1)); 
-    if (i!=hf.end()) hits.push_back(&(*i));
-    i=hf.find(HcalDetId(HcalForward,id.ieta(),id.iphi(),2)); 
-    if (i!=hf.end()) hits.push_back(&(*i));
+  theTowerMap.clear();
+
+  for(HBHERecHitCollection::const_iterator hbheItr = hbhe.begin();
+      hbheItr != hbhe.end(); ++hbheItr)
+  {
+    assignHit(*hbheItr);
+  }   
+
+  for(HORecHitCollection::const_iterator hoItr = ho.begin();
+      hoItr != ho.end(); ++hoItr)
+    assignHit(*hoItr);
   }
+
+  for(HFRecHitCollection::const_iterator hfItr = hf.begin();
+      hfItr != hf.end(); ++hfItr)
+  {
+    assignHit(*hfItr);
+  }
+
+
+  // now copy this map into the final collection
+  for(CaloTowerMap::const_iterator mapItr = theTowerMap.begin();
+      mapItr != theTowerMap.end(); ++ mapItr)
+  {
+    result.push_back(mapItr->second);
+  }
+
 }
 
-void CaloTowersCreationAlgo::reconstructTower(CaloTower& tower,const std::vector<const HBHERecHit*>& hbhe, const HORecHit* ho, const std::vector<const HFRecHit*>& hf) const {
+
+void CaloTowersCreationAlgo::assignHit(const CaloRecHit * recHit) {
+  DetId detId = recHit->detid();
+  CaloTowerDetId towerDetId = theTowerTopology->towerOf(detId);
+  CaloTower & tower = find(towerDetId);
+
+  double threshold, weight;
+  getThresholdAndWeight(detId, threshold, weight);
+  double energy = recHit->energy();
+  if(energy >= threshold) {
+    // TODO calculate crystal by crystal
+    double sintheta = 1. / cosh(tower.eta);
+    double eT = energy * sintheta * weight;
+
+    DetId::Detector det = detId.det();
+    if(det == DetId::Ecal) {
+      tower.eT_em += eT;
+    }
+    // HCAL
+    else {
+      HcalDetId hcalDetId(detId);
+      if(hcalDetId.subdet() == HcalOuter) {
+        tower.eT_outer += eT;
+      } 
+      else {
+        tower.eT_had += eT;
+      }
+    }
+    tower.constituents.push_back(detId);
+  } 
+}
+
+
+CaloTower & CaloTowersCreationAlgo::find(const CaloTowerDetId & detId) {
+  CaloTower * result;
+  CaloTowerMap::iterator itr = theCaloTowerMap.find(detId);
+  if(itr != theCaloTowerMap.end()) {
+    result = *itr;
+  }
+  else {
+    // need to build a new tower
+    result = new CaloTower(detId);
+    result->eta = theTowerGeometry->   ->eta();
+    result->phi = theTowerGeometry->   ->phi();
+
+    // store it in the map
+    theCaloTowerMap.insert(pair<CaloTowerDetId, CaloTower *>(detId, result));
+  }
+  return result;
+}
+ 
   
-  
-
-}
-
-bool CaloTowersCreationAlgo::create(const CaloTowerDetId& id, CaloTowerCollection& destCollection,
-				    const HBHERecHitCollection& hbhe, 
-				    const HORecHitCollection& ho, 
-				    const HFRecHitCollection& hf) const {
-  std::vector<const HBHERecHit*> hbhe_hits;
-  hadForTower(id,hbhe,hbhe_hits);
-  const HORecHit*  ho_hit = outerForTower(id,ho);
-  std::vector<const HFRecHit*> hf_hits;
-  forwardForTower(id,hf,hf_hits);
-  
-  bool anyhits=(ho_hit!=0 || !hbhe_hits.empty() || !hf_hits.empty());
-  if (anyhits) {
-    CaloTower tower(id);
-    // get the tower's location from the geometry
-
-    // set the tower's values 
-    reconstructTower(tower,hbhe_hits,ho_hit, hf_hits);
-    destCollection.push_back(tower);
+void CaloTowersCreationAlgo::getThresholdAndWeight(const DetId & detId, double & threshold, double & weight) const {
+  DetId::Detector det = detId.det();
+  if(det == DetId::Ecal) {
+    // may or may not be EB.  We'll find out.
+    cms::EBDetId ebDetId(detId);
+    cms::EcalSubdetector subdet = ebDetId.subdet();
+    if(subdet == cms::EcalBarrel) {
+      threshold = theEBthreshold;
+      weight = theEBweight;
+    }
+    else if(subdet == cms::EcalEndcap) {
+      threshold = theEEthreshold;
+      weight = theEEweight;
+    }
   }
+  else if(det == DetId::Hcal) {
+    HcalDetId hcalDetId(detId);
+    HcalSubdetector subdet = hcalDetId.subdet();
 
-  return anyhits;
+    if(subdet == HcalBarrel) {
+      threshold = theHBthreshold;
+      weight = theHBweight;
+    }
+
+    else if(subdet == HcalEndcap) {
+      // check if it's single or double tower
+      if(hcalDetId.ieta() < theHcalTopology->firstHEDoublePhiRing()) {
+        threshold = theHESthreshold;
+        weight = theHESweight;
+      }
+      else {
+        threshold = theHEDthreshold;
+        weight = theHEDweight;
+      }
+    }
+
+    else if(subdet == HcalForward) {
+      if(hcalDetId.depth() == 1) {
+        threshold = theHF1threshold;
+        weight = theHF1weight;
+      } else {
+        threshold = theHF2threshold;
+        weight = theHF2weight;
+      }
+    }
+  }
+  else {
+    std::cout << "BAD CELL det " << det << std::endl;
+  }
 }
+
