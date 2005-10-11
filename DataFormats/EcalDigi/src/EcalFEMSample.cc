04d75e162a4d611dc52753d37fd939bf6404681b
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2005-10-11 07:47:31 +0000

    Added FEM sample

---
+++ DataFormats/EcalDigi/src/EcalFEMSample.cc
@@ -0,0 +1,12 @@
+#include "DataFormats/EcalDigi/interface/EcalFEMSample.h"
+
+
+
+EcalFEMSample::EcalFEMSample(int adc, int gainId) {
+  theSample=(adc&0xFF) | ((gainId&0x3)<<12);
+}
+
+std::ostream& operator<<(std::ostream& s, const EcalFEMSample& samp) {
+  s << "ADC=" << samp.adc() << ", gainId=" << samp.gainId();
+  return s;
+}
