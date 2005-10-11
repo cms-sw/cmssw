b4f2e07066f38f5f605a8dc4487411944139d11e
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-11 17:12:52 +0000

    Fixed pedestal subtraction order (after gain)

--- RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
@@ -12,7 +12,7 @@ namespace HcalSimpleRecAlgoImpl {
     double ampl=0;
     for (int i=ifirst; i<tool.size() && i<n+ifirst; i++) {
       int capid=digi[i].capid();
-      ampl+=(tool[i]-calibs.pedestal(capid))*calibs.gain(capid);
+      ampl+=tool[i]*calibs.gain(capid)-calibs.pedestal(capid);
     }
 
     return RecHit(digi.id(),ampl,0);    
