ea8d79039a037f57f150f06ae0ff9dcc125e92ed
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-10-11 17:10:36 +0000

    Move -non-services to FWCore/Modules

--- FWCore/Services/src/SealModule.cc
+++ FWCore/Services/src/SealModule.cc
@@ -1,23 +1,11 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Services/src/AsciiOutputModule.h"
-#include "FWCore/Services/src/EmptyESSource.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-#include "FWCore/Services/src/EventSetupRecordDataGetter.h"
 #include "FWCore/Services/src/Tracer.h"
 #include "FWCore/Services/src/LoadAllDictionaries.h"
-#include "FWCore/Services/src/EventContentAnalyzer.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
-using edm::AsciiOutputModule;
-using edm::EventSetupRecordDataGetter;
-using edm::EmptyESSource;
 using edm::service::Tracer;
 using edm::service::LoadAllDictionaries;
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(AsciiOutputModule)
-DEFINE_ANOTHER_FWK_MODULE(EventSetupRecordDataGetter)
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(EmptyESSource)
 DEFINE_ANOTHER_FWK_SERVICE(Tracer)
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(LoadAllDictionaries,edm::serviceregistry::ParameterSetMaker<LoadAllDictionaries>)
-DEFINE_ANOTHER_FWK_MODULE(EventContentAnalyzer)
