a2ea908e5bff1d174b130caf2f6d5b63b9af7da2
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-10-11 19:33:05 +0000

    added support for EDProducer's 'callWhenNewProductsRegistered' method to which allows EDProducers to respond to products being added by Sources or other EDProducers

--- FWCore/Framework/src/ProducerWorker.cc
+++ FWCore/Framework/src/ProducerWorker.cc
@@ -1,6 +1,6 @@
 
 /*----------------------------------------------------------------------
-$Id: ProducerWorker.cc,v 1.13 2005/09/08 10:57:35 chrjones Exp $
+$Id: ProducerWorker.cc,v 1.14 2005/10/03 19:05:24 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/src/ProducerWorker.h"
@@ -14,14 +14,70 @@ $Id: ProducerWorker.cc,v 1.13 2005/09/08 10:57:35 chrjones Exp $
 #include "FWCore/Framework/interface/ProductRegistry.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/ConstProductRegistry.h"
+
+
 #include <string>
 #include <sstream>
+#include "boost/function.hpp"
 
 using namespace std;
 
 namespace edm
 {
+   static void addToRegistry(const EDProducer::TypeLabelList::const_iterator& iBegin,
+                             const EDProducer::TypeLabelList::const_iterator& iEnd,
+                             const ModuleDescription& iDesc,
+                             ProductRegistry& iReg,
+                             bool iIsListener=false){
+      for(EDProducer::TypeLabelList::const_iterator p=iBegin; p!=iEnd; ++p) {
+         
+         
+         BranchDescription pdesc(iDesc,
+                                 p->typeID_.userClassName(),
+                                 p->typeID_.friendlyClassName(), 
+                                 p->productInstanceName_,
+                                 p->productPtr_);
+         iReg.addProduct(pdesc,iIsListener);
+      }//for
+   }
+   namespace {
+     class CallbackWrapper {
+       public:  
+        CallbackWrapper(boost::shared_ptr<EDProducer> iProd,
+                        boost::function<void(const BranchDescription&)> iCallback,
+                        ProductRegistry* iReg,
+                        const ModuleDescription& iDesc):
+        prod_(&(*iProd)), callback_(iCallback), reg_(iReg), mdesc_(iDesc),
+        lastSize_(iProd->typeLabelList().size()) {}
+        
+        void operator()(const BranchDescription& iDesc){
+           callback_(iDesc);
+           addToRegistry();
+        }
+        
+        void addToRegistry() {
+           EDProducer::TypeLabelList const& plist = prod_->typeLabelList();
+           
+           if(lastSize_!=plist.size()){
+              EDProducer::TypeLabelList::const_iterator pStart = plist.begin();
+              advance(pStart,lastSize_);
+              edm::addToRegistry(pStart,plist.end(),mdesc_,*reg_);
+              lastSize_ = plist.size();
+           }
+        }
 
+      private:
+        EDProducer* prod_;
+        boost::function<void(const BranchDescription&)> callback_;
+        ProductRegistry* reg_;
+        ModuleDescription mdesc_;
+        unsigned int lastSize_;
+         
+     };
+  }
+   
   ProducerWorker::ProducerWorker(std::auto_ptr<EDProducer> ed,
 				 const ModuleDescription& md,
 				 const WorkerParams& wp):
@@ -29,8 +85,7 @@ namespace edm
     producer_(ed),
     actions_(wp.actions_) {
     
-    EDProducer::TypeLabelList const& plist = producer_->typeLabelList();
-    if (plist.empty()) {
+    if (producer_->typeLabelList().empty() && producer_->registrationCallback().empty()) {
       throw edm::Exception(errors::NoProductSpecified,"Producer")
         << "Module " << md.moduleName_
         << " did not specify that it produces a product.\n"
@@ -40,20 +95,28 @@ namespace edm
         << " multiple products of the same type.";
     }
 
-    EDProducer::TypeLabelList::const_iterator p;
-    // loop on products declared by the User Module
-    // and register them with ProductRegistry
-    for(p=plist.begin(); p!=plist.end(); ++p) {
-           
- 
-     BranchDescription pdesc(md,
-       p->typeID_.userClassName(),
-       p->typeID_.friendlyClassName(), 
-       p->productInstanceName_,
-       p->productPtr_);
-       wp.reg_->addProduct(pdesc);
-     }//for
+    //If we have a callback, first tell the callback about all the entries already in the
+    // product registry, then add any items this producer wants to add to the registry 
+    // and only after that do we register the callback. This is done so the callback does not
+    // get called for items registered by this producer (avoids circular reference problems)
+    bool isListener = false;
+    if(!(producer_->registrationCallback().empty())) {
+       isListener=true;
+       //NOTE: If implementation changes from a map, need to check that iterators are still valid
+       // after an insert with the new container, else need to copy the container and iterate over the copy
+       for(ProductRegistry::ProductList::const_iterator itEntry=wp.reg_->productList().begin();
+           itEntry!=wp.reg_->productList().end(); ++itEntry){
+          producer_->registrationCallback()(itEntry->second);
+       }
+    }
+    EDProducer::TypeLabelList const& plist = producer_->typeLabelList();
 
+    addToRegistry(plist.begin(),plist.end(),md,*(wp.reg_),isListener);
+    if(!(producer_->registrationCallback().empty())) {
+       Service<ConstProductRegistry> regService;
+       regService->watchProductAdditions(CallbackWrapper(producer_,producer_->registrationCallback(),
+                                                         wp.reg_,md));
+    }
   }
 
   ProducerWorker::~ProducerWorker() {
