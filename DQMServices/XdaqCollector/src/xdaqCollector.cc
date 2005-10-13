e1db8c03bde5841668c8c3a650a51fdf34aef50e
Dimitrios Tsirigkas <sha1-7a25099d73dbb3e60e5b1352f9ab2dd1067a4a69@cern.ch>
2005-10-13 16:44:17 +0000

    zdoing

--- DQMServices/XdaqCollector/src/xdaqCollector.cc
+++
@@ -1,9 +0,0 @@
-#include "DQMServices/xdaqCollector/interface/xdaqCollector.h"
-
-//
-// provides factory method for instantion of HellWorld application
-//
-CollectorRoot *xdaqCollector::DummyConsumerServer::instance_=0;
-
-XDAQ_INSTANTIATE(xdaqCollector)
-
