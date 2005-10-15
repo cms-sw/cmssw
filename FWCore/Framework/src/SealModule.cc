a16ddabfe87de218b58b1eacffc36c561d28c0f6
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-10-15 02:03:59 +0000

    Move EmptySource to test

--- FWCore/Framework/src/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/src/EmptySource.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-
-using edm::EmptySource;
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_INPUT_SOURCE(EmptySource)
