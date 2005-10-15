70fde08729bfc9a3279bc16e61a8c1c304c3796f
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2005-10-15 10:17:24 +0000

    small fix

--- SimG4Core/Notification/interface/Observer.h
+++ SimG4Core/Notification/interface/Observer.h
@@ -11,12 +11,10 @@ class Observer : public boost::signals::trackable
 {
 public:
     typedef Observer<Event> self;
-    Observer() { boost::bind(&self::callBack,this,_1); init(); }
-    virtual ~Observer() { end(); }
+    Observer() { boost::bind(&self::callBack,this,_1); }
+    virtual ~Observer() {}
     void callBack(Event const * ev) const 
     { const_cast<self*>(this)->update(ev); }
-    virtual void init() = 0;
-    virtual void end() = 0;
     virtual void update(Event const * ev) const = 0;
 };
 
