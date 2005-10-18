41b8eb2325b32e475ea413d0fcf4b8bb125d096c
James Kowalkowski <sha1-19a7633f7f2c73cd2080529650f067bdb0f96f44@cern.ch>
2005-10-18 15:08:20 +0000

    new simple example

---
+++ IOPool/Streamer/test/ThreadTest_t.cpp
@@ -0,0 +1,135 @@
+
+#include "IOPool/Streamer/interface/EventBuffer.h"
+#include "boost/thread/thread.hpp"
+#include "boost/bind.hpp"
+#include "boost/signal.hpp"
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace edm;
+using namespace std;
+
+// ---------------------------------------
+
+typedef boost::signal<void (int)> DataSignal;
+typedef boost::signals::connection Connection;
+
+struct Consumer
+{
+  Consumer(EventBuffer& b, DataSignal& c);
+  ~Consumer() { c_.disconnect(); }
+
+  void operator()();
+
+  void callme(int value);
+
+  EventBuffer* b_;
+  Connection c_;
+};
+
+Consumer::Consumer(EventBuffer& b, DataSignal& s):
+  b_(&b),
+  c_(s.connect(boost::bind(&Consumer::callme,this,_1)))
+{
+
+}
+
+void Consumer::operator()()
+{
+  while(1)
+    {
+      EventBuffer::ConsumerBuffer ob(*b_);
+      if(ob.size()==0) break;
+      int* i = (int*)ob.buffer();
+      cout << "C" << *i << endl;
+    }
+}
+
+void Consumer::callme(int value)
+{
+  EventBuffer::ProducerBuffer ib(*b_);
+  int* v = (int*)ib.buffer();
+  *v = value;
+  cout << "P" << value << endl;
+
+  if(value==-1)
+    ib.commit(0);
+  else
+    ib.commit(sizeof(int));  
+}
+
+// -----------------------
+
+struct Producer
+{
+  Producer(EventBuffer& b, int total):b_(&b),total_(total) { }
+
+  void operator()();
+
+  EventBuffer* b_;
+  int total_;
+
+  DataSignal sig_;
+};
+
+void Producer::operator()()
+{
+  for(int i=0;i<total_;++i)
+    {
+      //boost::thread::yield();
+      for(int j=0;j<(1<<17);++j);
+      /*
+      EventBuffer::ProducerBuffer ib(*b_);
+      int* v = (int*)ib.buffer();
+      *v = i;
+      cout << "P" << i << endl;
+      ib.commit(sizeof(int));
+      */
+      sig_(i);
+    }
+
+  sig_(-1);
+  //EventBuffer::ProducerBuffer ib(*b_);
+  //ib.commit(0);
+
+}
+
+void prunner(Producer* p)
+{
+  (*p)();
+  cout << "PD" << endl;
+}
+
+void crunner(Consumer* c)
+{
+  (*c)();
+  cout << "CD" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc<3)
+    {
+      cerr << "usage: " << argv[0] << " event_size queue_depth number_to_gen"
+	   << endl;
+      return -1;
+    }
+
+  int event_sz = atoi(argv[1]);
+  int queue_dep = atoi(argv[2]);
+  int total = atoi(argv[3]);
+
+  cout << "(1)" << endl;
+  EventBuffer buf(event_sz,queue_dep);
+  Producer p(buf,total);
+  Consumer c(buf,p.sig_);
+  cout << "(2)" << endl;
+  boost::thread con(boost::bind(crunner,&c));
+  boost::thread pro(boost::bind(prunner,&p));
+  cout << "(3)" << endl;
+
+  con.join();
+  pro.join();
+  return 0;
+}
