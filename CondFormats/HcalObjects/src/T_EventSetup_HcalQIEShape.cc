f152c8c38614eb7ae50a57ade58608e55d74fc62
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-10-20 05:47:37 +0000

    lost setup definitions

--- CondFormats/HcalObjects/src/T_EventSetup_HcalQIEShape.cc
+++ CondFormats/HcalObjects/src/T_EventSetup_HcalQIEShape.cc
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 // $Id: T_EventSetup_HcalQIEData.cc,v 1.1 2005/10/18 23:34:56 fedor Exp $
 
-#include "CondFormats/HcalObjects/interface/HcalQIEData.h"
+#include "CondFormats/HcalObjects/interface/HcalQIEShape.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
-EVENTSETUP_DATA_REG(HcalQIEData);
+EVENTSETUP_DATA_REG(HcalQIEShape);
