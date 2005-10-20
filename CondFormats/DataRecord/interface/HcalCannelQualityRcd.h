62956f9342b9a1d1ab24b16231eaf8f2ae275f39
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-10-20 06:18:34 +0000

    Correct typo in file name

--- CondFormats/DataRecord/interface/HcalCannelQualityRcd.h
+++
@@ -1,7 +0,0 @@
-// this file is generated automatically by /afs/fnal.gov/files/home/room1/ratnikov/bin/makeNewClass.sh
-// name: ratnikov, date: Tue Oct 18 19:00:27 CDT 2005
-#ifndef HcalCannelQualityRcd_H
-#define HcalCannelQualityRcd_H
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class HcalCannelQualityRcd : public edm::eventsetup::EventSetupRecordImplementation<HcalCannelQualityRcd> {};
-#endif
