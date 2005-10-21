5c06cffa120109073913ada749e8d9d1e7955e1a
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-10-21 09:08:05 +0000

    added particle vertex

--- PhysicsTools/CandUtils/src/helicityAngle.cc
+++ PhysicsTools/CandUtils/src/helicityAngle.cc
@@ -1,4 +1,4 @@
-// $Id: helicityAngle.cc,v 1.1 2005/07/29 07:05:57 llista Exp $
+// $Id: helicityAngle.cc,v 1.2 2005/10/03 10:12:11 llista Exp $
 #include "PhysicsTools/CandUtils/interface/helicityAngle.h"
 #include "PhysicsTools/Candidate/interface/Candidate.h"
 #include "PhysicsTools/CandUtils/interface/Booster.h"
@@ -7,7 +7,7 @@ using namespace std;
 
 double helicityAngle( const Candidate & cand ) {
   assert( cand.numberOfDaughters() == 2 );
-  Particle::Vector boost = - cand.p4().boostVector(); 
+  Particle::Momentum boost = - cand.p4().boostVector(); 
   Particle::LorentzVector dp = cand[ 0 ]->p4();
   dp.boost( boost );
   double h = dp.angle( boost );
