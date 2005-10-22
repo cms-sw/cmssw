b8ca646c6a8be6e3af0b040082c9623217811211
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2005-10-22 02:14:47 +0000

    got conversion factors backwards

--- SimCalorimetry/CaloSimAlgos/src/CaloSimParameters.cc
+++ SimCalorimetry/CaloSimAlgos/src/CaloSimParameters.cc
@@ -4,7 +4,7 @@
 namespace cms {
   std::ostream & operator<<(std::ostream & os, const CaloSimParameters & p) {
     os << "CALO SIM PARAMETERS" << std::endl;
-    os << p.simHitToPhotoelectrons() << " SimHit energy per pe" << std::endl;
+    os << p.simHitToPhotoelectrons() << " pe per SimHit energy " << std::endl;
     os << p.photoelectronsToAnalog() << " Analog signal to be digitized per pe" << std::endl;
     os << "Sampling Factor:      " << p.samplingFactor() << " Incident energy / SimHit Energy " << std::endl;
     return os;
