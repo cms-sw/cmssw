52fd425c86a67cb6ec1f853820fa56cac7c1d914
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2005-10-24 17:10:06 +0000

    first version fo this new data record for ECAL weights

---
+++ CondFormats/DataRecord/interface/EcalWeightRecAlgoWeightsRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALWEIGHTRECALGOWEIGHTS_H
+#define ECALWEIGHTRECALGOWEIGHTS_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalWeightRecAlgoWeightsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalWeightRecAlgoWeightsRcd> {};
+#endif
---
+++ CondFormats/DataRecord/src/EcalWeightRecAlgoWeightsRcd.cc
@@ -0,0 +1,5 @@
+#include "CondFormats/DataRecord/interface/EcalWeightRecAlgoWeightsRcd.h"
+
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(EcalWeightRecAlgoWeightsRcd);
