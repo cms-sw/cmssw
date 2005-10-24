dfdd2be384bea443e29598dc874cefffd3b728a4
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-10-24 12:59:52 +0000

    moved from CandUtils

---
+++ PhysicsTools/CandAlgos/src/cutParser.h
@@ -0,0 +1,49 @@
+#if !defined(PACKAGE_CUTPARSER_H)
+#define PACKAGE_CUTPARSER_H
+// -*- C++ -*-
+//
+// Package:     <package>
+// Module:      cutParser
+// 
+/**\class cutParser cutParser.h package/cutParser.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Chris D Jones
+// Created:     Sun Aug  7 20:45:51 EDT 2005
+// $Id: cutParser.h,v 1.1 2005/10/21 13:56:43 llista Exp $
+//
+// Revision history
+//
+// $Log: cutParser.h,v $
+// Revision 1.1  2005/10/21 13:56:43  llista
+// added Chris Jones cut parser
+//
+
+// system include files
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <map>
+
+// user include files
+
+// forward declarations
+namespace aod {
+  class Candidate;
+  class Selector;
+}
+namespace aod {
+  typedef double ( aod::Candidate::* PCandMethod )() const;
+  typedef std::map<std::string, PCandMethod> CandidateMethods;
+  
+  bool cutParser( const std::string & iString,
+		  const CandidateMethods & iMethods,
+		  boost::shared_ptr<aod::Selector> & oCut );
+}
+
+#endif /* PACKAGE_CUTPARSER_H */
