f67456a0ad4c031b5768746cdf8d014b3d02f034
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2005-10-24 20:33:57 +0000

    auto_ptrs are flaky.  Make copies of the calibations & widths

--- SimCalorimetry/HcalSimAlgos/src/HcalNoisifier.cc
+++ SimCalorimetry/HcalSimAlgos/src/HcalNoisifier.cc
@@ -16,19 +16,17 @@ namespace cms {
 
   void HcalNoisifier::noisify(CaloSamples & frame) const {
     HcalDetId hcalDetId(frame.id());
-    const HcalCalibrations * calibrations = theCalibrationHandle->getHcalCalibrations(hcalDetId).get();
-    const HcalCalibrationWidths * widths  = theCalibrationHandle->getHcalCalibrationWidths(hcalDetId).get();
+    HcalCalibrations calibrations = *(theCalibrationHandle->getHcalCalibrations(hcalDetId));
+    HcalCalibrationWidths widths = *(theCalibrationHandle->getHcalCalibrationWidths(hcalDetId));
     for(int tbin = 0; tbin < frame.size(); ++tbin) {
-       //@@ assumes the capIDs start at zero
-       int capId = (theStartingCapId + tbin)%4;
-      //@@ replace by a real random number generator
-      double pedestalJitter = theRandGaussian->shoot(0., widths->pedestal(capId));
-      double gainJitter = theRandGaussian->shoot(1., widths->gain(capId));
-      frame[tbin] *= gainJitter;
+
+      int capId = (theStartingCapId + tbin)%4;
+      double pedestal = theRandGaussian->shoot(calibrations.pedestal(capId), widths.pedestal(capId));
+      double gain = theRandGaussian->shoot(calibrations.gain(capId), widths.gain(capId));
       // pedestals come in units of GeV.  Use gain to convert
-      frame[tbin] += (calibrations->pedestal(capId) + pedestalJitter) / calibrations->gain(capId);
+      frame[tbin] = (frame[tbin]+pedestal) / gain;
     }
-std::cout << "AFTER HCAL NOISE NOISE " << std::endl;
+std::cout << "AFTER HCAL NOISE  " << std::endl;
     std::cout << frame << std::endl;
   }
 }
