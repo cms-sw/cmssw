804133082618fa2471c7e8dccbb7ee6295105e1d
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-10-24 15:33:46 +0000

    use boost's program_options package to handle parsing of command line arguments

--- FWCore/Framework/bin/cmsRun.cpp
+++ FWCore/Framework/bin/cmsRun.cpp
@@ -4,34 +4,84 @@ This is a generic main that can be used with any plugin and a
 PSet script.   See notes in EventProcessor.cpp for details about
 it.
 
-$Id: cmsRun.cpp,v 1.2 2005/09/02 19:30:46 chrjones Exp $
+$Id: cmsRun.cpp,v 1.3 2005/09/09 16:43:50 wmtan Exp $
 
 ----------------------------------------------------------------------*/  
 
 #include <exception>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <boost/program_options.hpp>
 
 #include "FWCore/Framework/interface/EventProcessor.h"
 #include "FWCore/Utilities/interface/ProblemTracker.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 using namespace std;
+static const char* const kParameterSetOpt = "parameter-set";
+static const char* const kParameterSetCommandOpt = "parameter-set,p";
+static const char* const kHelpOpt = "help";
+static const char* const kHelpCommandOpt = "help,h";
 
 // -----------------------------------------------
 
 int main(int argc, char* argv[])
 {
-  // Right now option processing is not really present.
-  // The EventProcessor should handle this in the future.
+  using namespace boost::program_options;
+  std::string descString(argv[0]);
+  descString += " [options] [--";
+  descString += kParameterSetOpt;
+  descString += "] config_file \nAllowed options";
+  options_description desc(descString);
+  
+  desc.add_options()
+    (kHelpCommandOpt, "produce help message")
+    (kParameterSetCommandOpt,value<std::string>(), "configuration file")
+    ;
 
+  positional_options_description p;
+  p.add(kParameterSetOpt, -1);
+  
+  variables_map vm;
+  try {
+    store(command_line_parser(argc,argv).options(desc).positional(p).run(),vm);
+    notify(vm);
+  } catch(const error& iException) {
+    std::cout << iException.what()<<std::endl;
+    return 1;
+  }
+    
+  if(vm.count(kHelpOpt)){
+    std::cout << desc <<std::endl;
+    return 1;
+  }
+  
+  if(!vm.count(kParameterSetOpt)){
+    std::cout <<"no configuration file given \n"
+    <<" please do '"<<argv[0]<<" --"<<kHelpOpt<<"'."<<std::endl;
+    return 1;
+  }
+
+  ifstream configFile(vm[kParameterSetOpt].as<std::string>().c_str());
+  if(!configFile) {
+    std::cout <<"unable to open configuration file "
+    <<vm[kParameterSetOpt].as<std::string>()<<std::endl;
+    return 1;
+  }
+  
+  string configstring;
+  string line;
+  
+  while(std::getline(configFile,line)) { configstring+=line; configstring+="\n"; }
+  
   edm::AssertHandler ah;
 
   int rc = -1; // we should never return this value!
   try
     {
-      edm::EventProcessor proc(argc,argv);
+      edm::EventProcessor proc(configstring);
       proc.beginJob();
       proc.run();
       if(proc.endJob()) {
