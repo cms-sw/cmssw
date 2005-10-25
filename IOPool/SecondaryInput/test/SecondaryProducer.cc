8831eaf5a37c73c5cb5372e5758472ae38e7e40c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-10-25 21:11:08 +0000

    Add test of Secondary Input Source

---
+++ IOPool/SecondaryInput/test/SecondaryProducer.cc
@@ -0,0 +1,74 @@
+// File: SecondaryProducer.cc
+// Description:  see SecondaryProducer.h
+// Author:  Bill Tanenbaum
+//
+//--------------------------------------------
+
+#include "IOPool/SecondaryInput/test/SecondaryProducer.h"
+#include "FWCore/Framework/src/TypeID.h"
+#include "FWCore/Framework/interface/BasicHandle.h"
+#include "FWCore/Framework/interface/EventPrincipal.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/src/SecondaryInputSourceFactory.h"
+#include "FWCore/Integration/test/OtherThingCollection.h"
+#include "FWCore/Integration/test/ThingCollection.h"
+
+using namespace std;
+
+namespace edm {
+
+  // Constructor 
+  SecondaryProducer::SecondaryProducer(const edm::ParameterSet& ps) {
+    // make secondary input source
+    secInput_ = makeSecInput(ps);
+
+    produces<edmreftest::ThingCollection>();
+    produces<edmreftest::OtherThingCollection>("testUserTag");
+  }
+
+  // Virtual destructor needed.
+  SecondaryProducer::~SecondaryProducer() { }  
+
+  
+  // Functions that get called by framework every event
+  void SecondaryProducer::produce(edm::Event& e, const edm::EventSetup&) { 
+
+    typedef edmreftest::ThingCollection TC;
+    typedef edmreftest::OtherThingCollection OTC;
+    typedef edm::Wrapper<TC> WTC;
+    typedef edm::Wrapper<OTC> WOTC;
+
+    std::vector<EventPrincipal*> result;
+    secInput_->readMany(e.id().event()-1, 1, result);
+
+    EventPrincipal &p = *result[0];
+    EDProduct const* ep = p.getByType(TypeID(typeid(TC))).wrapper();
+    assert(ep);
+    WTC const* wtp = dynamic_cast<WTC const*>(ep);
+    assert(wtp);
+    EDProduct const* epo = p.getByType(TypeID(typeid(OTC))).wrapper();
+    assert(epo);
+    WOTC const* wop = dynamic_cast<WOTC const*>(epo);
+    assert(wop);
+    TC const* tp = wtp->product();
+    OTC const* op = wop->product();
+    auto_ptr<TC> thing(new TC(*tp));
+    auto_ptr<OTC> otherThing(new OTC(*op));
+
+    // Put output into event
+    e.put(thing);
+    e.put(otherThing, "testUserTag");
+  }
+
+
+  boost::shared_ptr<SecondaryInputSource> SecondaryProducer::makeSecInput(ParameterSet const& ps) {
+    ParameterSet sec_input = ps.getParameter<ParameterSet>("input");
+
+    boost::shared_ptr<SecondaryInputSource> input_(static_cast<SecondaryInputSource *>
+      (SecondaryInputSourceFactory::get()->makeSecondaryInputSource(sec_input).release()));
+    return input_;
+  }
+
+} //edm
+using edm::SecondaryProducer;
+DEFINE_FWK_MODULE(SecondaryProducer)
---
+++ IOPool/SecondaryInput/test/SecondaryProducer.h
@@ -0,0 +1,42 @@
+#ifndef SecondaryInput_SecondaryProducer_h
+#define SecondaryInput_SecondaryProducer_h
+
+/** \class SecondaryProducer
+ *
+ * \author Bill Tanenbaum
+ *
+ *
+ ************************************************************/
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/SecondaryInputSource.h"
+
+namespace edm {
+  class SecondaryProducer: public edm::EDProducer {
+  public:
+
+    /** standard constructor*/
+    explicit SecondaryProducer(const edm::ParameterSet& ps);
+
+    /**Default destructor*/
+    virtual ~SecondaryProducer();
+
+    /**Cumulates the pileup events onto this event*/
+    virtual void produce(edm::Event& e1, const edm::EventSetup& c);
+
+
+  private:
+
+    virtual void put(edm::Event &e) {}
+
+    boost::shared_ptr<SecondaryInputSource> makeSecInput(ParameterSet const& ps);
+
+    boost::shared_ptr<SecondaryInputSource> secInput_;
+  };
+}//edm
+
+#endif
