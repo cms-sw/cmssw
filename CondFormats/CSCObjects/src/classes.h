d4dbfa5b5f2fb8d921a5b5e65613e51de279ff0f
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2005-10-25 16:37:41 +0000

    Changed X to cross

--- CondFormats/CSCObjects/src/classes.h
+++ CondFormats/CSCObjects/src/classes.h
@@ -6,9 +6,9 @@ template std::map< int, CSCPedestals::Item >::const_iterator;
 template std::map< int, CSCGains::Item >::iterator;
 template std::map< int, CSCGains::Item >::const_iterator;
 
-#include "CondFormats/CSCObjects/interface/CSCXtalk.h"
-template std::map< int, CSCXtalk::Item >::iterator;
-template std::map< int, CSCXtalk::Item >::const_iterator;
+#include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
+template std::map< int, CSCcrosstalk::Item >::iterator;
+template std::map< int, CSCcrosstalk::Item >::const_iterator;
 
 //#include "CondFormats/CSCObjects/interface/CSCMapping.h"
 //template std::map< int, cms::EBDetId >::iterator;
