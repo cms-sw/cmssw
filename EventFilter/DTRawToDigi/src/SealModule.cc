5471b115313b2fc77fe02e91c4ce5fb0d84b3008
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2005-10-26 11:35:56 +0000

    cleanup

--- EventFilter/DTRawToDigi/src/SealModule.cc
+++ EventFilter/DTRawToDigi/src/SealModule.cc
@@ -1,4 +1,8 @@
 #include <FWCore/Framework/interface/MakerMacros.h>
 
 #include <EventFilter/DTRawToDigi/src/DTUnpackingModule.h>
+#include <EventFilter/DTRawToDigi/src/DTDigiToRaw.h>
+
+
 DEFINE_FWK_MODULE(DTUnpackingModule)
+//DEFINE_ANOTHER_FWK_MODULE(DTDigiToRawModule)
