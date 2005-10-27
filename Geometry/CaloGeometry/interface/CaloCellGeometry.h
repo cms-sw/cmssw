2dfb6f4484aed05e99983d72cc48617a01a15038
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2005-10-27 17:46:06 +0000

    Added default constructor

--- Geometry/CaloGeometry/interface/CaloCellGeometry.h
+++ Geometry/CaloGeometry/interface/CaloCellGeometry.h
@@ -7,18 +7,21 @@
 
 /** \class CaloCellGeometry
     
-$Date: 2005/09/30 13:42:44 $
-$Revision: 1.1 $
+$Date: 2005/10/03 22:33:49 $
+$Revision: 1.2 $
 \author J. Mans, P. Meridiani
 */
 class CaloCellGeometry {
 public:
+  CaloCellGeometry() {;} ;
   virtual ~CaloCellGeometry() {;};
   virtual bool inside(const GlobalPoint & point) const =0 ;  
   virtual const std::vector<GlobalPoint> & getCorners() const =0 ;
   
   /** return the position of reference for this cell */
   const GlobalPoint& getPosition() const { return refPoint_; }
+  void setPosition(const GlobalPoint& refPoint) { refPoint_=refPoint; }
+
 protected:
   CaloCellGeometry(const GlobalPoint& positionOfReference);
 private:
