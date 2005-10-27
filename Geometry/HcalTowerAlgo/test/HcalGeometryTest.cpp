63cc64d00b0fe059a541335a0521805464be1d77
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-27 16:26:36 +0000

    Added dumping code

--- Geometry/HcalTowerAlgo/test/HcalGeometryTest.cpp
+++ Geometry/HcalTowerAlgo/test/HcalGeometryTest.cpp
@@ -1,15 +1,30 @@
 #include "Geometry/HcalTowerAlgo/interface/HcalGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
 #include "Geometry/HcalTowerAlgo/interface/HcalHardcodeGeometryLoader.h"
 #include <iostream>
+
+
 
 
 int main() {
 
   HcalHardcodeGeometryLoader l;
-  l.load(DetId::Hcal,HcalBarrel);
-  l.load(DetId::Hcal,HcalEndcap);
-  l.load(DetId::Hcal,HcalOuter);
-  l.load(DetId::Hcal,HcalForward);
+  std::auto_ptr<CaloSubdetectorGeometry> b=l.load(DetId::Hcal,HcalBarrel);
+  std::auto_ptr<CaloSubdetectorGeometry> e=l.load(DetId::Hcal,HcalEndcap);
+  std::auto_ptr<CaloSubdetectorGeometry> o=l.load(DetId::Hcal,HcalOuter);
+  std::auto_ptr<CaloSubdetectorGeometry> f=l.load(DetId::Hcal,HcalForward);
+
+  std::vector<DetId> ids=b->getValidDetIds(DetId::Hcal,HcalBarrel);
+  for (std::vector<DetId>::iterator i=ids.begin(); i!=ids.end(); i++) {
+    HcalDetId hid=(*i);
+    if (hid.iphi()!=1) continue;
+    const CaloCellGeometry* geom=b->getGeometry(hid);
+    std::vector<GlobalPoint> corners=geom->getCorners();
+    std::cout << hid << std::endl;
+    for (std::vector<GlobalPoint>::iterator j=corners.begin(); j!=corners.end(); j++) {
+      std::cout << "  " << *j << std::endl;
+    }
+  }
 
   return 0;
 }
