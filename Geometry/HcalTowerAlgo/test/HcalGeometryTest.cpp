6322d759be4ab391fda6a66481b33b205b00beca
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-28 18:11:50 +0000

    Fixed various bugs in the corners definition pointed out by Ianna

--- Geometry/HcalTowerAlgo/test/HcalGeometryTest.cpp
+++ Geometry/HcalTowerAlgo/test/HcalGeometryTest.cpp
@@ -26,5 +26,34 @@ int main() {
     }
   }
 
+  std::cout << std::endl << " FORWARD : " << std::endl;
+  ids=f->getValidDetIds(DetId::Hcal,HcalForward);
+  for (std::vector<DetId>::iterator i=ids.begin(); i!=ids.end(); i++) {
+    HcalDetId hid=(*i);
+    if (hid.iphi()!=1 && hid.iphi()!=2 && hid.iphi()!=3) continue;
+    std::cout << hid << std::endl;
+    
+    const CaloCellGeometry* geom=f->getGeometry(hid);
+    std::vector<GlobalPoint> corners=geom->getCorners();
+    for (std::vector<GlobalPoint>::iterator j=corners.begin(); j!=corners.end(); j++) {
+      std::cout << "  " << *j << std::endl;
+    }
+  }
+
+
+  std::cout << std::endl << " ENDCAP : " << std::endl;
+  ids=e->getValidDetIds(DetId::Hcal,HcalEndcap);
+  for (std::vector<DetId>::iterator i=ids.begin(); i!=ids.end(); i++) {
+    HcalDetId hid=(*i);
+    if (hid.iphi()!=1 && hid.iphi()!=2 && hid.iphi()!=3) continue;
+    std::cout << hid << std::endl;
+    
+    const CaloCellGeometry* geom=e->getGeometry(hid);
+    std::vector<GlobalPoint> corners=geom->getCorners();
+    for (std::vector<GlobalPoint>::iterator j=corners.begin(); j!=corners.end(); j++) {
+      std::cout << "  " << *j << std::endl;
+    }
+  }
+
   return 0;
 }
