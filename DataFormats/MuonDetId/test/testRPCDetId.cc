85e61ca163f6f91b3001f3456d848e697b5bc064
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2005-10-28 08:10:37 +0000

    bug fixes

--- DataFormats/MuonDetId/test/testRPCDetId.cc
+++ DataFormats/MuonDetId/test/testRPCDetId.cc
@@ -3,7 +3,7 @@
    test file for RPCDetId
 
    \author Stefano ARGIRO
-   \version $Id: testRPCDetId.cc,v 1.4 2005/10/27 10:00:00 segoni Exp $
+   \version $Id: testRPCDetId.cc,v 1.1 2005/10/28 08:00:19 segoni Exp $
    \date 27 Jul 2005
 */
 
@@ -38,38 +38,39 @@ void testRPCDetId::testOne(){
 
 
   for (int region=RPCDetId::minRegionId; region<=RPCDetId::maxRegionId; ++region)
-    {
+  {
       int minRing=RPCDetId::minRingForwardId;
       int maxRing=RPCDetId::maxRingForwardId;
       if (!region) 
-         {
-	   minRing=RPCDetId::minRingBarrelId;
-	   maxRing=RPCDetId::maxRingBarrelId;
-         } 
+      {
+	minRing=RPCDetId::minRingBarrelId;
+	maxRing=RPCDetId::maxRingBarrelId;
+      } 
     
       for (int ring=minRing; ring<=maxRing; ++ring) 	    
         for (int station=RPCDetId::minStationId; ring<=RPCDetId::maxStationId; ++station)
           for (int sector=RPCDetId::minSectorId; sector<=RPCDetId::maxSectorId; ++sector)
             for (int layer=RPCDetId::minLayerId; layer<=RPCDetId::maxLayerId; ++layer)
               for (int subSector=RPCDetId::minSubSectorId; subSector<=RPCDetId::maxSubSectorId; ++subSector)
-                for (int roll=RPCDetId::minRollId; roll<=RPCDetId::maxRollId; ++roll)
+                for (int roll=RPCDetId::minRollId; roll<=RPCDetId::maxRollId; ++roll){
 
-	          RPCDetId detid(region, ring, station, sector, layer, subsector, roll);
+	          RPCDetId detid(region, ring, station, sector, layer, subSector, roll);
 
-	         CPPUNIT_ASSERT(detid.region() == region);
-                 CPPUNIT_ASSERT(detid.ring() == ring);
-                 CPPUNIT_ASSERT(detid.station() == station);
-	         CPPUNIT_ASSERT(detid.sector() == sector);
-                 CPPUNIT_ASSERT(detid.layer() == layer);
-	         CPPUNIT_ASSERT(detid.subsector() == subsector);
-                 CPPUNIT_ASSERT(detid.roll() == roll);
+	          CPPUNIT_ASSERT(detid.region() == region);
+                  CPPUNIT_ASSERT(detid.ring() == ring);
+                  CPPUNIT_ASSERT(detid.station() == station);
+	          CPPUNIT_ASSERT(detid.sector() == sector);
+                  CPPUNIT_ASSERT(detid.layer() == layer);
+	          CPPUNIT_ASSERT(detid.subsector() == subSector);
+                  CPPUNIT_ASSERT(detid.roll() == roll);
 
-	    // test constructor from id
-	    int myId = detid.rawId();
-	    RPCDetId anotherId(myId);
-	    CPPUNIT_ASSERT(detid==anotherId);
-
-   }
+	          // test constructor from id
+	          int myId = detid.rawId();
+	          RPCDetId anotherId(myId);
+	          CPPUNIT_ASSERT(detid==anotherId);
+  
+               }
+  }
 
 
 }
@@ -106,6 +107,6 @@ void testRPCDetId::testMemberOperators(){
   RPCDetId unit1(0,-2,1,2,2,1,1);
   RPCDetId unit2=unit1;
   
-  CPPUNIT_ASSERT(layer2==layer1);
+  CPPUNIT_ASSERT(unit2==unit1);
 
 }
