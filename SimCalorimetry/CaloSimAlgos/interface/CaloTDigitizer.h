1baf544b964a67c8df5cb4122859b0a5a6c4b4c3
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2005-10-29 01:07:27 +0000

    Three template arguments was getting ridiculous.  Switched over to a Traits class

--- SimCalorimetry/CaloSimAlgos/interface/CaloTDigitizer.h
+++ SimCalorimetry/CaloSimAlgos/interface/CaloTDigitizer.h
@@ -10,33 +10,44 @@
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloHitResponse.h"
 
 namespace cms {
-  template<class Digi, class ElectronicsSim>
+  template<class Traits>
   class CaloTDigitizer
   {
   public:
+    /// these are the types that need to be defined in the Traits
+    /// class.  The ElectronicsSim needs to have an interface
+    /// that you'll see in the run() method
+    typedef typename Traits::ElectronicsSim ElectronicsSim;
+    typedef typename Traits::Digi Digi;
+    typedef typename Traits::DigiCollection DigiCollection;
 
-    CaloTDigitizer(CaloHitResponse * hitResponse, ElectronicsSim * electronicsSim,
-                   const std::vector<DetId> & cellIds)
+    CaloTDigitizer(CaloHitResponse * hitResponse, ElectronicsSim * electronicsSim)
     :  theHitResponse(hitResponse),
        theElectronicsSim(electronicsSim),
-       theCellIds(cellIds) {}
+       theDetIds(0) {}
 
     /// doesn't delete the pointers passed in
     ~CaloTDigitizer() {}
 
+    /// will get done every event by the producers
+    void setDetIds(const std::vector<DetId> & detIds) {theDetIds = detIds;}
+
     /// turns hits into digis
     /// user must delete the pointer returned
-    void run(const std::vector<CaloHit> & input, std::auto_ptr<std::vector<Digi> > & output) {
+    void run(const std::vector<CaloHit> & input, DigiCollection & output) {
+      assert(theDetIds.size() != 0);
+
       theHitResponse->run(input);
       // make a raw digi for evey cell
-      for(std::vector<DetId>::const_iterator idItr = theCellIds.begin();
-          idItr != theCellIds.end(); ++idItr)
+      for(std::vector<DetId>::const_iterator idItr = theDetIds.begin();
+          idItr != theDetIds.end(); ++idItr)
       {
-         Digi digi;
+         Digi digi(*idItr);
          CaloSamples analogSignal(theHitResponse->findSignal(*idItr));
          bool addNoise = true;
          theElectronicsSim->analogToDigital(analogSignal , digi, addNoise);
-         output->push_back(digi);
+         output.push_back(digi);
+std::cout << digi << std::endl;
       }
 
       // free up some memory
@@ -47,7 +58,7 @@ namespace cms {
   private:
     CaloHitResponse * theHitResponse;
     ElectronicsSim * theElectronicsSim;
-    std::vector<DetId> theCellIds;
+    std::vector<DetId> theDetIds;
   };
 }
 #endif
