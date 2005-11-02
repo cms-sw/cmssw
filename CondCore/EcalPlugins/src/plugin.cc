8ac2542f60d88e8cd10613b0fd082f34d58cf5a0
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-11-02 15:21:18 +0000

    remove EcalMapping

--- CondCore/EcalPlugins/src/plugin.cc
+++ CondCore/EcalPlugins/src/plugin.cc
@@ -9,13 +9,10 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
-#include "CondFormats/EcalObjects/interface/EcalMapping.h"
-#include "CondFormats/DataRecord/interface/EcalMappingRcd.h"
 #include "CondFormats/EcalObjects/interface/EcalWeightRecAlgoWeights.h"
 #include "CondFormats/DataRecord/interface/EcalWeightRecAlgoWeightsRcd.h"
 
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(EcalPedestalsRcd,EcalPedestals);
-REGISTER_PLUGIN(EcalMappingRcd,EcalMapping);
 REGISTER_PLUGIN(EcalWeightRecAlgoWeightsRcd,EcalWeightRecAlgoWeights);
