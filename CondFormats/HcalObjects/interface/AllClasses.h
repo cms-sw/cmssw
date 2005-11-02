cae0993fbc2740a62e2af4cb252c9b8de6277de8
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-11-02 21:52:12 +0000

    organize headers

---
+++ CondFormats/HcalObjects/interface/AllClasses.h
@@ -0,0 +1,13 @@
+#ifndef CondFormats_HcalObjects_AllClasses_h
+#define CondFormats_HcalObjects_AllClasses_h
+
+class HcalPedestals;
+class HcalPedestalWidths;
+class HcalGains;
+class HcalGainWidths;
+class HcalQIEShape;
+class HcalQIEData;
+class HcalChannelQuality;
+class HcalElectronicsMap;
+
+#endif
---
+++ CondFormats/HcalObjects/interface/AllObjects.h
@@ -0,0 +1,13 @@
+#ifndef CondFormats_HcalObjects_AllObjects_h
+#define CondFormats_HcalObjects_AllObjects_h
+
+#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestalWidths.h"
+#include "CondFormats/HcalObjects/interface/HcalGains.h"
+#include "CondFormats/HcalObjects/interface/HcalGainWidths.h"
+#include "CondFormats/HcalObjects/interface/HcalQIEShape.h"
+#include "CondFormats/HcalObjects/interface/HcalQIEData.h"
+#include "CondFormats/HcalObjects/interface/HcalChannelQuality.h"
+#include "CondFormats/HcalObjects/interface/HcalElectronicsMap.h"
+
+#endif
