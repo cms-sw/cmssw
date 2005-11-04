110d1bf74605be84bd824feec5d5541208637cd5
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2005-11-04 16:18:44 +0000

    first actual implementation

--- DataFormats/RPCDigi/src/RPCDigi.cc
+++ DataFormats/RPCDigi/src/RPCDigi.cc
@@ -1,7 +1,7 @@
 /** \file
  * 
- *  $Date: 2005/11/03 13:48:54 $
- *  $Revision: 1.2 $
+ *  $Date: 2005/11/03 15:25:32 $
+ *  $Revision: 1.1 $
  *
  * \author Ilaria Segoni
  */
@@ -15,9 +15,70 @@
 using namespace std;
 
 
-RPCDigi::RPCDigi (int channelID, int bxID){
-  chID_=channelID;
-  bxID_=bxID;
+RPCDigi::RPCDigi(int strip, int bx){
+    setStripBx( strip , bx );
 }
 
 
+/// Copy constructor
+//RPCDigi::RPCDigi(const RPCDigi& digi) {
+//  persistentData = digi.persistentData;
+//}
+
+
+
+/// Assignment
+//RPCDigi& 
+//RPCDigi::operator=(const RPCDigi& digi){
+ // persistentData = digi.persistentData;
+ // return *this;
+//}
+
+/// Comparison
+bool RPCDigi::operator == (const RPCDigi& digi) const {
+  if ( !(strip() == digi.strip())     ||
+       !(bx()== digi.bx()) ) return false;
+  return true;
+}
+
+///Print Digi Content
+void RPCDigi::print() const {
+  cout << "strip " << strip() 
+       << " bx   " << bx() << endl;
+}
+
+/// Getter methods:
+int RPCDigi::strip() const { return data()->strip; }
+
+int RPCDigi::bx() const { return data()->bx; }
+
+
+/// Setter methods:
+
+void RPCDigi::setStripBx(int strip, int bx) {
+  data()->strip=strip;
+  data()->bx=bx;
+}
+
+void RPCDigi::setStrip(int strip) {
+  data()->strip=strip;
+}
+
+void RPCDigi::setBx(int bx) {
+  data()->bx=bx;
+}
+
+
+
+
+///  Private members
+
+RPCDigi::PackedDigiType* 
+RPCDigi::data() {
+  return reinterpret_cast<PackedDigiType*>(&persistentData);
+}
+
+const RPCDigi::PackedDigiType* 
+RPCDigi::data() const {
+  return reinterpret_cast<const PackedDigiType*>(&persistentData);
+}
