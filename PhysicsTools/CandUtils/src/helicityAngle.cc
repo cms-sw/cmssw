0b3bedcfd37a510d8a89eb588e69ab7e0ed7691c
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-11-04 09:40:00 +0000

    operator[] changed into daughter(i) for Candidates

--- PhysicsTools/CandUtils/src/helicityAngle.cc
+++ PhysicsTools/CandUtils/src/helicityAngle.cc
@@ -1,4 +1,4 @@
-// $Id: helicityAngle.cc,v 1.2 2005/10/03 10:12:11 llista Exp $
+// $Id: helicityAngle.cc,v 1.3 2005/10/21 09:08:05 llista Exp $
 #include "PhysicsTools/CandUtils/interface/helicityAngle.h"
 #include "PhysicsTools/Candidate/interface/Candidate.h"
 #include "PhysicsTools/CandUtils/interface/Booster.h"
@@ -8,7 +8,7 @@ using namespace std;
 double helicityAngle( const Candidate & cand ) {
   assert( cand.numberOfDaughters() == 2 );
   Particle::Momentum boost = - cand.p4().boostVector(); 
-  Particle::LorentzVector dp = cand[ 0 ]->p4();
+  Particle::LorentzVector dp = cand.daughter( 0 )->p4();
   dp.boost( boost );
   double h = dp.angle( boost );
   if ( h > M_PI / 2 ) h = M_PI - h;
