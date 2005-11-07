4fbfaa315ba6bedf1ced241847bd3357b509585a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-11-07 02:56:33 +0000

    PCaloHitContainer class replaced by typedef

--- SimDataFormats/CaloHit/interface/PCaloHitContainer.h
+++ SimDataFormats/CaloHit/interface/PCaloHitContainer.h
@@ -3,31 +3,10 @@
 
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include <vector>
-#include <string>
 
 namespace edm {
-  class PCaloHitContainer {
-  public:
-    typedef std::vector<PCaloHit> PCaloHitSingleContainer ;
-
-    /// insert a digi for a given layer
-    void insertHits (PCaloHitSingleContainer &) ;
-    void insertHit (const PCaloHit &) ;
-    // changed by PG
-    void clear () ;
-
-    unsigned int size () const ;
-    PCaloHit operator[] (int i) const ; 
- 
-    PCaloHitSingleContainer::const_iterator begin () const ;
-    PCaloHitSingleContainer::const_iterator end () const ;
- 
-  private:
-    PCaloHitSingleContainer m_data ;
-
-  };
+    typedef std::vector<PCaloHit> PCaloHitContainer;
 } // edm
-
 
 #endif
 
--- SimDataFormats/CaloHit/src/classes.h
+++ SimDataFormats/CaloHit/src/classes.h
@@ -1,14 +1,10 @@
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
-#include <vector>
-#include <map>
-#include <string>
 #include "FWCore/EDProduct/interface/Wrapper.h"
 
 namespace {
 namespace {
   PCaloHit dummy44;
-  std::vector<PCaloHit> dummy7;
   edm::PCaloHitContainer dummy777;
   edm::Wrapper<edm::PCaloHitContainer> dummy7777;
 
