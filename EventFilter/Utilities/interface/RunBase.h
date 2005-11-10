b3e1f6dd7de95a9fe703a1e387af26712c450423
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2005-11-10 14:45:48 +0000

    added Run Base

---
+++ EventFilter/Utilities/interface/RunBase.h
@@ -0,0 +1,13 @@
+#include "xdata/include/xdata/UnsignedLong.h"
+#include <string>
+
+namespace evf
+{
+  class RunBase
+    {
+    public:
+      static xdata::UnsignedLong runNumber_;
+      static std::string sourceId_;
+    };
+
+}
---
+++ EventFilter/Utilities/src/RunBase.cc
@@ -0,0 +1,4 @@
+#include "EventFilter/Utilities/interface/RunBase.h"
+
+xdata::UnsignedLong evf::RunBase::runNumber_ = 0;
+std::string evf::RunBase::sourceId_ = "Offline";
