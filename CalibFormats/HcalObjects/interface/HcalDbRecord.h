dec49fc776dbbac9c50f08274d9f22bb9bb03342
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-11-15 18:48:53 +0000

    Change declarations of parent records to definitions

--- CalibFormats/HcalObjects/interface/HcalDbRecord.h
+++ CalibFormats/HcalObjects/interface/HcalDbRecord.h
@@ -16,21 +16,20 @@
 //
 // Author:      
 // Created:     Tue Aug  9 19:10:36 CDT 2005
-// $Id: HcalDbRecord.h,v 1.2 2005/10/04 18:05:54 fedor Exp $
+// $Id: HcalDbRecord.h,v 1.3 2005/10/28 01:33:28 fedor Exp $
 //
 #include "boost/mpl/vector.hpp"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 // #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 
-class HcalPedestalsRcd;
-class HcalPedestalWidthsRcd;
-class HcalGainsRcd;
-class HcalGainWidthsRcd;
-class HcalQIEShapeRcd;
-class HcalQIEDataRcd;
-class HcalChannelQualityRcd;
-class HcalElectronicsMapRcd;
-
+#include "CondFormats/DataRecord/interface/HcalChannelQualityRcd.h"
+#include "CondFormats/DataRecord/interface/HcalElectronicsMapRcd.h"      
+#include "CondFormats/DataRecord/interface/HcalGainWidthsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalGainsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalPedestalWidthsRcd.h"
+#include "CondFormats/DataRecord/interface/HcalPedestalsRcd.h" 
+#include "CondFormats/DataRecord/interface/HcalQIEDataRcd.h"
+#include "CondFormats/DataRecord/interface/HcalQIEShapeRcd.h"
 
 // class HcalDbRecord : public edm::eventsetup::EventSetupRecordImplementation<HcalDbRecord> {};
 
