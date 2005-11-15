49be2a5aa3f0769d62997436b441c4b3d2113534
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-11-15 03:41:04 +0000

    removed use of boost bind

--- SimG4Core/Notification/interface/Observer.h
+++ SimG4Core/Notification/interface/Observer.h
@@ -1,21 +1,15 @@
 #ifndef SimG4Core_Notification_Observer_H
 #define SimG4Core_Notification_Observer_H
 
-#include <iostream>
-#include <string>
 #include <boost/signal.hpp>
-#include <boost/bind.hpp>
 
-template<typename Event>
+template<typename T>
 class Observer : public boost::signals::trackable 
 {
 public:
-    typedef Observer<Event> self;
-    Observer() { boost::bind(&self::callBack,this,_1); }
+    Observer() {}
     virtual ~Observer() {}
-    void callBack(Event const * ev) const 
-    { const_cast<self*>(this)->update(ev); }
-    virtual void update(Event const * ev) const = 0;
+    virtual void update(T const *) const = 0;
 };
 
 #endif
