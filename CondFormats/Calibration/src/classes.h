c2d8345d9d6deca841784ec3e3f6780925db174a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-11-17 17:02:07 +0000

    remove DTT0

--- CondFormats/Calibration/src/classes.h
+++ CondFormats/Calibration/src/classes.h
@@ -1,5 +1,4 @@
 #include "CondFormats/Calibration/interface/Pedestals.h"
-#include "CondFormats/Calibration/interface/DTT0.h"
 // Declaration of the iterator (necessary for the generation of the dictionary)
 template std::vector< Pedestals::Item >::iterator;
 template std::vector< Pedestals::Item >::const_iterator;
