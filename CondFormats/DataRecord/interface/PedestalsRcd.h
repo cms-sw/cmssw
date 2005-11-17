af9a9a81316a3feda8d42c1a23811ff6e8003c0a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-11-17 17:41:59 +0000

    added for testing

---
+++ CondFormats/DataRecord/interface/PedestalsRcd.h
@@ -0,0 +1,6 @@
+#ifndef PEDESTALSRCD_H
+#define PEDESTALSRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class PedestalsRcd : public edm::eventsetup::EventSetupRecordImplementation<PedestalsRcd> {};
+#endif
