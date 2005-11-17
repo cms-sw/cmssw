2a00e7a6747b8d2ebcf8ad056cb71ebe863861ce
Michael Henry Schmitt <sha1-3781f210b057607fc6ee487d8b6ee9ae3e98f9eb@cern.ch>
2005-11-17 13:04:59 +0000

    zeroth version

---
+++ DataFormats/CSCDigi/interface/CSCStripDigi.h
@@ -0,0 +1,83 @@
+#ifndef CSCStripDigi_CSCStripDigi_h
+#define CSCStripDigi_CSCStripDigi_h
+
+/** \class CSCStripDigi
+ *
+ * Digi for CSC Cathode Strips.
+ *  
+ *  $Date: 2005/10/07 17:40:53 $
+ *  $Revision: 1.1 $
+ *
+ * \author M. Schmitt, Northwestern
+ *
+ */
+
+#include <vector>
+
+class CSCStripDigi{
+
+public:
+
+  //
+  // The definition of the CSC strip Digi.
+  // This should never be used directly, only by calling data().
+  // Made public to be able to generate lcgdict, SA, 27/4/05
+  //
+  struct theStripDigi {
+    unsigned int strip;
+    std::vector<int> ADCCounts;
+  };
+
+  // Construct from the strip number and the ADC readings.
+  explicit CSCStripDigi (int strip, std::vector<int> ADCCounts);
+
+  // Copy constructor
+  CSCStripDigi (const CSCStripDigi& digi);
+
+  // Default construction.
+  CSCStripDigi ();
+
+  // Assignment operator
+  CSCStripDigi& operator=(const CSCStripDigi& digi);
+
+  // Digis are equal if they are on the same strip and have same ADC readings
+  bool operator==(const CSCStripDigi& digi) const;
+
+  // Get the strip number
+  int getStrip() const;
+
+  // Get ADC readings
+  std::vector<int> getADCCounts() const;
+
+  // Set the strip number
+  void setStrip(int strip);
+
+  // Set with a vector of ADC readings
+  void setADCCounts (std::vector<int> ADCCounts);
+
+  // Print content of digi
+  void print() const;
+
+  // Print the binary representation of the digi
+  void dump() const;
+
+private:
+  friend class testCSCStripDigis;
+
+  // Set data words
+  void set(int strip, std::vector<int> ADCCounts);
+
+  // access
+  theStripDigi* data(); 
+  const theStripDigi* data() const;
+  theStripDigi aStripDigi;
+};
+
+#include<iostream>
+// needed by COBRA
+//inline std::ostream & operator<<(std::ostream & o, const CSCStripDigi& digi) {
+//  return o << " " << digi.strip()
+//	   << " " << digi.ADCCounts();
+//}
+#endif
+
---
+++ DataFormats/CSCDigi/src/CSCStripDigi.cc
@@ -0,0 +1,86 @@
+/** \file
+ * 
+ *  $Date: 2005/10/07 17:40:53 $
+ *  $Revision: 1.1 $
+ *
+ * \author M.Schmitt, Northwestern
+ */
+#include <DataFormats/CSCDigi/interface/CSCStripDigi.h>
+#include <iostream>
+#include <bitset>
+#include <vector>
+
+using namespace std;
+
+// Constructors
+CSCStripDigi::CSCStripDigi (int strip, vector<int> ADCCounts){
+  set(strip, ADCCounts);
+}
+
+CSCStripDigi::CSCStripDigi (){
+  vector<int> ZeroCounts(8,0);
+  set(0,ZeroCounts);
+}
+
+// Copy constructor
+CSCStripDigi::CSCStripDigi(const CSCStripDigi& digi) {
+  aStripDigi = digi.aStripDigi;
+}
+
+// Assignment
+CSCStripDigi& 
+CSCStripDigi::operator=(const CSCStripDigi& digi){
+  aStripDigi = digi.aStripDigi;
+  return *this;
+}
+
+// Comparison
+bool
+CSCStripDigi::operator == (const CSCStripDigi& digi) const {
+  if ( getStrip() != digi.getStrip() ) return false;
+  if ( getADCCounts().size() != digi.getADCCounts().size() ) return false;
+  if ( getADCCounts() != digi.getADCCounts() ) return false;
+  return true;
+}
+
+// Getters
+int CSCStripDigi::getStrip() const { return data()->strip; }
+std::vector<int> CSCStripDigi::getADCCounts() const { return data()->ADCCounts; }
+
+// Setters
+void CSCStripDigi::setStrip(int strip) {
+  data()->strip = strip;
+}
+void CSCStripDigi::setADCCounts(vector<int>ADCCounts) {
+  bool badVal = false;
+  for (int i=0; i<(int)ADCCounts.size(); i++) {
+    if (ADCCounts[i] < 1) badVal = true;
+  }
+  if ( !badVal ) {
+    data()->ADCCounts = ADCCounts;
+  } else {
+    vector<int> ZeroCounts(8,0);
+    data()->ADCCounts = ZeroCounts;
+  }
+}
+
+// Debug
+void
+CSCStripDigi::print() const {
+  //  cout << "CSC Strip: " << strip() << " ADC Counts: ";
+  //  for (int i=0; i<ADCCounts().size(); i++) {cout << ADCCounts[i] << " ";}
+  cout << "\n";
+}
+
+void
+CSCStripDigi::dump() const {
+  // Do we need this?
+}
+
+// ----- Private members
+void
+CSCStripDigi::set(int strip, vector<int> ADCCounts) {
+  data()->strip = strip;
+  data()->ADCCounts = ADCCounts;
+}
+
