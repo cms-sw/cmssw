d0fcf18a69302166551422938e5c9fc675ed293c
Michael Henry Schmitt <sha1-3781f210b057607fc6ee487d8b6ee9ae3e98f9eb@cern.ch>
2005-11-18 10:59:51 +0000

    include CSCComparatorDigi

--- DataFormats/CSCDigi/src/classes.h
+++ DataFormats/CSCDigi/src/classes.h
@@ -4,6 +4,8 @@
 #include <DataFormats/CSCDigi/interface/CSCRPCDigiCollection.h>
 #include <DataFormats/CSCDigi/interface/CSCStripDigi.h>
 #include <DataFormats/CSCDigi/interface/CSCStripDigiCollection.h>
+#include <DataFormats/CSCDigi/interface/CSCComparatorDigi.h>
+#include <DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h>
 #include <FWCore/EDProduct/interface/Wrapper.h>
 #include <vector>
 #include <map>
@@ -14,6 +16,7 @@ namespace{
   CSCWireDigi cWD_;
   CSCRPCDigi  cRD_;
   CSCStripDigi cSD_;
+  CSCComparatorDigi cCD_;
 
   CSCWireDigi::PersistentPacking ppWD_;
   CSCRPCDigi::PersistentPacking ppRD_;
@@ -21,18 +24,22 @@ namespace{
   std::vector<CSCWireDigi>  vWD_;
   std::vector<CSCRPCDigi>   vRD_;
   std::vector<CSCStripDigi>  vSD_;
+  std::vector<CSCComparatorDigi>  vCD_;
 
   std::vector<std::vector<CSCWireDigi> >  vvWD_; 
   std::vector<std::vector<CSCRPCDigi>  >  vvRD_;
   std::vector<std::vector<CSCStripDigi> >  vvSD_; 
+  std::vector<std::vector<CSCComparatorDigi> >  vvCD_; 
 
   CSCWireDigiCollection clWD_;
   CSCRPCDigiCollection  clRD_;
   CSCStripDigiCollection clSD_;
+  CSCComparatorDigiCollection clCD_;
     
   edm::Wrapper<CSCWireDigiCollection> wWD_;
   edm::Wrapper<CSCRPCDigiCollection> wRD_;
   edm::Wrapper<CSCStripDigiCollection> wSD_;
+  edm::Wrapper<CSCComparatorDigiCollection> wCD_;
 
   }
 }
