539bb88bb27a11b7a90f5134d6ec3402bf5ffef4
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2005-11-20 11:43:01 +0000

    Drift Tube topology, first commit

---
+++ Geometry/CommonTopologies/interface/DTTopology.h
@@ -0,0 +1,66 @@
+#ifndef Geometry_CommonTopologies_DTTopology_H
+#define Geometry_CommonTopologies_DTTopology_H
+
+/** \class DTTopology
+ *
+ * interface for the DriftTube detector. 
+ * Extends the Topology interface with methods relevant for
+ * the DT detectors.
+ *  
+ *  $Date: 2005/11/10 13:21:02 $
+ *  $Revision: 1.4 $
+ *
+ * \author R. Bellan - INFN Torino
+ *
+ */
+
+/*
+#include "Geometry/CommonDetAlgo/interface/LocalError.h"
+#include "Geometry/Vector/interface/LocalPoint.h"
+#include "Geometry/CommonDetAlgo/interface/MeasurementPoint.h"
+#include "Geometry/CommonDetAlgo/interface/MeasurementError.h"
+*/
+
+#include "Geometry/CommonTopologies/interface/Topology.h"
+#include "Geometry/Vector/interface/LocalPoint.h"
+
+class DTTopology: public Topology {
+ public:
+  
+  //Constructor: number of wire in the layer and its lenght
+  DTTopology(int nChannels,float lenght);
+
+  virtual ~DTTopology() {}
+  
+  // Conversion between measurement coordinates
+  // and local cartesian coordinates
+
+  LocalPoint localPosition( const MeasurementPoint& ) const;
+
+  LocalError localError( const MeasurementPoint&, const MeasurementError& ) const;
+
+  MeasurementPoint measurementPosition( const LocalPoint&) const;
+
+  MeasurementError measurementError( const LocalPoint&, const LocalError& ) const;
+
+  // return the wire number, starting from a LocalPoint
+  int channel( const LocalPoint& p) const;
+
+  // return the x-wire position in the layer, starting from its wire number.
+  float wirePosition(int wireNumber);
+  
+  // They return the cell dimensions:
+  float cellWidth(){return theWidth;}
+  float cellHeight(){return theHeight;}
+  float cellLenght(){return theLenght;}
+
+private:
+  float theNChannels;
+  float theWidth;
+  float theHeight;
+  float theLenght;
+
+  Local2DPoint theOffSet;
+};
+
+#endif
---
+++ Geometry/CommonTopologies/src/DTTopology.cc
@@ -0,0 +1,51 @@
+#include "Geometry/CommonTopologies/interface/DTTopology.h"
+#include "Geometry/Vector/interface/LocalPoint.h"
+
+#include <iostream>
+
+DTTopology::DTTopology(int nChannels,float lenght):theNChannels(nChannels),theLenght(lenght){
+  theOffset = LocalPoint(-theNChannels/2. * theWidth, -theLenght/2.);
+  theWidth=4.2;  // cm FIXME now put by hand, check the number!
+  theHeight=1.3; // cm ...
+  
+#ifdef VERBOSE
+  cout <<"Constructing DTTopology with:"<<endl
+       <<"number of wires = "<<theNChannels
+       <<", width = "<<theWidth
+       <<", height = "<<theHeight
+       <<", length = "<<theLenght
+       <<endl;
+#endif
+}
+
+
+//FIXME the x from tdrift is missing!!Now only wire position in the layer.
+LocalPoint DTTopology::localPosition( const MeasurementPoint& mp) const{
+    return LocalPoint( (mp.x() - 0.5)*theWidth + theOffset.x() , 
+		       (1-mp.y())*theLength + theOffset.y());
+}
+
+LocalError DTTopology::localError( const MeasurementPoint& mp, const MeasurementError& me) const{
+  return LocalError(me.uu()*(theWidth*theWidth), 0,
+		    me.vv()*(theLength*theLength));
+}
+
+MeasurementPoint DTTopology::measurementPosition( const LocalPoint& lp) const{
+  return MeasurementPoint( static_cast<int>( (lp.x()-theOffset.x())/theWidth + 0.5),
+			   1 - (lp.y()-theOffset.y())/theLength);
+}
+
+MeasurementError DTTopology::measurementError( const LocalPoint& lp, const LocalError& le) const{
+  return MeasurementError(le.xx()/(theWidth*theWidth),0,
+			  le.yy()/(theLength*theLength));
+}
+
+int DTTopology::channel( const LocalPoint& lp) const{
+  float static_cast<int>( (lp.x()-theOffset.x())/theWidth + 0.5);
+}
+
+// return the x wire position in the layer, starting from its wire number.
+float DTTopology::wirePosition(int wireNumber){
+  return  (wireNumber - 0.5)*theWidth + theOffset.x();
+}
+  
