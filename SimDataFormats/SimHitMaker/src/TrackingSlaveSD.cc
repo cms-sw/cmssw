da3aa7ad0f8335b8f839850877e94a7cb37bd69b
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2005-11-21 08:46:02 +0000

    add some debug cout

--- SimDataFormats/SimHitMaker/src/TrackingSlaveSD.cc
+++ SimDataFormats/SimHitMaker/src/TrackingSlaveSD.cc
@@ -1,30 +1,43 @@
 #include "SimDataFormats/SimHitMaker/interface/TrackingSlaveSD.h"
 
 #include <iostream>
+#define debug
 
 using std::cout;
 using std::endl;
 
-TrackingSlaveSD::TrackingSlaveSD(std::string myName) : name_(myName)
-{ cout << " TrackingSlaveSD " << name_ << endl; }
+TrackingSlaveSD::TrackingSlaveSD(std::string myName) : name_(myName){
+#ifdef debug
+ cout << " TrackingSlaveSD " << name_ << endl; 
+#endif
+}
 
-TrackingSlaveSD::~TrackingSlaveSD() 
+TrackingSlaveSD::~TrackingSlaveSD()
 {}
 
-void TrackingSlaveSD::Initialize()
-{ hits_.clear(); }
+void TrackingSlaveSD::Initialize(){
+
+#ifdef debug
+  std::cout << " initialize TrackingSlaveSD "<< name_ << std::endl;
+#endif  
+  hits_.clear(); 
+}
 
 bool TrackingSlaveSD::format()
 {
-    cout << " TrackingSlaveSD " << name_ 
-	 << " formatting " << hits_.size() << " hits." << endl;
-    return true;
+#ifdef debug 
+  cout << " TrackingSlaveSD " << name_<< " formatting " << hits_.size() << " hits." << endl;
+#endif  
+  return true;
 }
 
 bool TrackingSlaveSD::processHits(const PSimHit & ps)
 {
-    hits_.push_back(ps);
-    return true;
+#ifdef debug
+  std::cout <<" Sent Hit " << ps << " to ROU " << name_ << std::endl;
+#endif
+  hits_.push_back(ps);
+  return true;
 } 
 
 void TrackingSlaveSD::setTrackId(PSimHit & hit, unsigned int k)
