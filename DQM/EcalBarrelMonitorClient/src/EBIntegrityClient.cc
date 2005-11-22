a1de0c8edaacd9e9505636e4e991f2dc801a32c6
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2005-11-22 18:15:42 +0000

    Quality criteria introduced

--- DQM/EcalBarrelMonitorClient/src/EBIntegrityClient.cc
+++ DQM/EcalBarrelMonitorClient/src/EBIntegrityClient.cc
@@ -1,8 +1,8 @@
 /*
  * \file EBIntegrityClient.cc
  * 
- * $Date: 2005/11/20 16:43:39 $
- * $Revision: 1.26 $
+ * $Date: 2005/11/20 16:58:38 $
+ * $Revision: 1.27 $
  * \author G. Della Ricca
  *
 */
@@ -13,10 +13,13 @@ EBIntegrityClient::EBIntegrityClient(const edm::ParameterSet& ps, MonitorUserInt
 
   mui_ = mui;
 
+
   Char_t histo[50];
 
   h00_ = 0;
   for ( int i = 0; i < 36; i++ ) {
+
+    h_[i] = 0;
 
     h01_[i] = 0;
     h02_[i] = 0;
@@ -28,7 +31,7 @@ EBIntegrityClient::EBIntegrityClient(const edm::ParameterSet& ps, MonitorUserInt
 
   }
 
-  threshCry_ = 1.;
+  threshCry_ = 0.;
 
 }
 
@@ -36,8 +39,11 @@ EBIntegrityClient::~EBIntegrityClient(){
 
   this->unsubscribe();
 
+
   if ( h00_ ) delete h00_;
   for ( int i = 0; i < 36; i++ ) {
+    
+    if ( h_[i] ) delete h_[i];
   
     if ( h01_[i] ) delete h01_[i];
     if ( h02_[i] ) delete h02_[i];
@@ -70,10 +76,13 @@ void EBIntegrityClient::beginRun(const edm::EventSetup& c){
   h00_ = 0;
   for ( int i = 0; i < 36; i++ ) {
 
+    if ( h_[i] ) delete h_[i];
+
     if ( h01_[i] ) delete h01_[i];
     if ( h02_[i] ) delete h02_[i];
     if ( h03_[i] ) delete h03_[i];
     if ( h04_[i] ) delete h04_[i];
+    h_[i] = 0;
     h01_[i] = 0;
     h02_[i] = 0;
     h03_[i] = 0;
@@ -111,6 +120,8 @@ void EBIntegrityClient::endRun(EcalCondDBInterface* econn, RunIOV* runiov, RunTa
 
   cout << "Writing RunConsistencyDatObjects to database ..." << endl;
 
+  float n_min_bin = 50.;
+
   float num00;
 
   for ( int ism = 1; ism <= 36; ism++ ) {
@@ -120,75 +131,85 @@ void EBIntegrityClient::endRun(EcalCondDBInterface* econn, RunIOV* runiov, RunTa
     for ( int ie = 1; ie <= 85; ie++ ) {
       for ( int ip = 1; ip <= 20; ip++ ) {
 
-        num00 = -1.;
+        double numEventsinCry = 0.;
+        numEventsinCry = h_[ism-1]->GetBinEntries(h_[ism-1]->GetBin(ie, ip)) / 3.;
+        
+        // cout << "Number of events per crystal (" << ie << "," << ip << ") SM " << ism << " " << numEventsinCry << endl;
 
-        if ( h00_ ) {
-          num00  = h00_->GetBinContent(h00_->GetBin(ie, ip));
-        }
-
-        num01 = num02 = num03 = num04 = -1.;
-
-        bool update_channel = false;
-
-        if ( h01_[ism-1] ) {
-          num01  = h01_[ism-1]->GetBinContent(h01_[ism-1]->GetBin(ie, ip));
-          update_channel = true;
-        }
-
-        if ( h02_[ism-1] ) {
-          num02  = h02_[ism-1]->GetBinContent(h02_[ism-1]->GetBin(ie, ip));
-          update_channel = true;
-        }
-
-        if ( h03_[ism-1] ) {
-          num03  = h03_[ism-1]->GetBinContent(h03_[ism-1]->GetBin(ie, ip));
-          update_channel = true;
-        }
-
-        if ( h04_[ism-1] ) {
-          num04  = h04_[ism-1]->GetBinContent(h04_[ism-1]->GetBin(ie, ip));
-          update_channel = true;
-        }
-
-        if ( update_channel ) {
-
-          if ( ie == 1 && ip == 1 ) {
-
-            cout << "Inserting dataset for SM=" << ism << endl;
-
-            cout << "(" << ie << "," << ip << ") " << num00 << " " << num01 << " " << num02 << " " << num03 << " " << num04 << endl;
-
+        if ( numEventsinCry > (double)n_min_bin ) {
+          
+          num00 = -1.;
+          
+          if ( h00_ ) {
+            num00  = h00_->GetBinContent(h00_->GetBin(ie, ip));
+          }
+          
+          num01 = num02 = num03 = num04 = -1.;
+          
+          bool update_channel = false;
+          
+          if ( h01_[ism-1] ) {
+            num01  = h01_[ism-1]->GetBinContent(h01_[ism-1]->GetBin(ie, ip));
+            update_channel = true;
           }
 
-          c.setExpectedEvents(0);
-          c.setProblemsInGain(int(num01));
-          c.setProblemsInId(int(num02));
-          c.setProblemsInSample(int(-999));
-          c.setProblemsInADC(int(-999));
-
-          float val;
-
-          if ( g01_[ism-1] ) {
-            val = 1.;
-            g01_[ism-1]->SetBinContent(g01_[ism-1]->GetBin(ie, ip), val);
+          if ( h02_[ism-1] ) {
+            num02  = h02_[ism-1]->GetBinContent(h02_[ism-1]->GetBin(ie, ip));
+            update_channel = true;
           }
 
-          if ( econn ) {
-            try {
-              ecid = econn->getEcalLogicID("EB_crystal_index", ism, ie-1, ip-1);
-              dataset[ecid] = c;
-            } catch (runtime_error &e) {
-              cerr << e.what() << endl;
+          if ( h03_[ism-1] ) {
+            num03  = h03_[ism-1]->GetBinContent(h03_[ism-1]->GetBin(ie, ip));
+            update_channel = true;
+          }
+
+          if ( h04_[ism-1] ) {
+            num04  = h04_[ism-1]->GetBinContent(h04_[ism-1]->GetBin(ie, ip));
+            update_channel = true;
+          }
+
+          if ( update_channel ) {
+
+            if ( ie == 1 && ip == 1 ) {
+
+              cout << "Inserting dataset for SM=" << ism << endl;
+
+              cout << "(" << ie << "," << ip << ") " << num00 << " " << num01 << " " << num02 << " " << num03 << " " << num04 << endl;
+
             }
+
+            c.setExpectedEvents(0);
+            c.setProblemsInGain(int(num01));
+            c.setProblemsInId(int(num02));
+            c.setProblemsInSample(int(-999));
+            c.setProblemsInADC(int(-999));
+
+            float val;
+
+            if ( g01_[ism-1] ) {
+              val = 1.;
+              if ( (float) (( num01 + num02 ) / numEventsinCry / 2. ) > threshCry_ ) 
+                val = 0.;
+              g01_[ism-1]->SetBinContent(g01_[ism-1]->GetBin(ie, ip), val);
+            }
+
+            if ( econn ) {
+              try {
+                ecid = econn->getEcalLogicID("EB_crystal_index", ism, ie-1, ip-1);
+                dataset[ecid] = c;
+              } catch (runtime_error &e) {
+                cerr << e.what() << endl;
+              }
+            }
+
           }
-
+        
         }
-
       }
     }
-  
+    
   }
-
+  
   if ( econn ) {
     try {
       cout << "Inserting dataset ... " << flush;
@@ -204,6 +225,7 @@ void EBIntegrityClient::endRun(EcalCondDBInterface* econn, RunIOV* runiov, RunTa
 void EBIntegrityClient::subscribe(void){
 
   // subscribe to all monitorable matching pattern
+  mui_->subscribe("*/EcalBarrel/EBPedPreSampleTask/Gain12/EBPT pedestal PreSample SM*");
   mui_->subscribe("*/EcalBarrel/EcalIntegrity/DCC size error");
   mui_->subscribe("*/EcalBarrel/EcalIntegrity/Gain/EI gain SM*");
   mui_->subscribe("*/EcalBarrel/EcalIntegrity/ChId/EI ChId SM*");
@@ -215,6 +237,7 @@ void EBIntegrityClient::subscribe(void){
 void EBIntegrityClient::subscribeNew(void){
 
   // subscribe to new monitorable matching pattern
+  mui_->subscribeNew("*/EcalBarrel/EBPedPreSampleTask/Gain12/EBPT pedestal PreSample SM*");
   mui_->subscribeNew("*/EcalBarrel/EcalIntegrity/DCC size error");
   mui_->subscribeNew("*/EcalBarrel/EcalIntegrity/Gain/EI gain SM*");
   mui_->subscribeNew("*/EcalBarrel/EcalIntegrity/ChId/EI ChId SM*");
@@ -226,6 +249,7 @@ void EBIntegrityClient::subscribeNew(void){
 void EBIntegrityClient::unsubscribe(void){
   
   // unsubscribe to all monitorable matching pattern
+  mui_->unsubscribe("*/EcalBarrel/EBPedPreSampleTask/Gain12/EBPT pedestal PreSample SM*");
   mui_->unsubscribe("*/EcalBarrel/EcalIntegrity/DCC size error");
   mui_->unsubscribe("*/EcalBarrel/EcalIntegrity/Gain/EI gain SM*");
   mui_->unsubscribe("*/EcalBarrel/EcalIntegrity/ChId/EI ChId SM*");
@@ -260,6 +284,17 @@ void EBIntegrityClient::analyze(const edm::Event& e, const edm::EventSetup& c){
   }
 
   for ( int ism = 1; ism <= 36; ism++ ) {
+
+    sprintf(histo, "Collector/FU0/EcalBarrel/EBPedPreSampleTask/Gain12/EBPT pedestal PreSample SM%02d G12", ism);
+    me = mui_->get(histo);
+    if ( me ) {
+      cout << "Found '" << histo << "'" << endl;
+      ob = dynamic_cast<MonitorElementT<TNamed>*> (me);
+      if ( ob ) {
+        if ( h_[ism-1] ) delete h_[ism-1];
+        h_[ism-1] = dynamic_cast<TProfile2D*> ((ob->operator->())->Clone());
+      }
+    }
 
     sprintf(histo, "Collector/FU0/EcalBarrel/EcalIntegrity/Gain/EI gain SM%02d", ism);
     me = mui_->get(histo);
@@ -342,8 +377,6 @@ void EBIntegrityClient::htmlOutput(int run, string htmlDir, string htmlName){
   // Produce the plots to be shown as .jpg files from existing histograms
 
   int csize = 250;
-
-  double histMax = 1.e15;
 
   int pCol3[3] = { 2, 3, 10 };
 
@@ -467,10 +500,13 @@ void EBIntegrityClient::htmlOutput(int run, string htmlDir, string htmlName){
         imgNameME[iCanvas-1] = meName + ".jpg";
         imgName = htmlDir + imgNameME[iCanvas-1];
         gStyle->SetOptStat(" ");
+        gStyle->SetPalette(1, 0);
         obj2f->GetXaxis()->SetNdivisions(17);
         obj2f->GetYaxis()->SetNdivisions(4);
         cMe->SetGridx();
         cMe->SetGridy();
+        obj2f->SetMinimum(-0.00000001);
+        obj2f->SetMaximum();
         obj2f->Draw("colz");
         cMe->Update();
         cMe->SaveAs(imgName.c_str());
