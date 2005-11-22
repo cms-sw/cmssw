a390717a3dc2539cdf6c3584d0d04b6a927b43fb
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2005-11-22 09:47:10 +0000

    added

---
+++ EventFilter/CSCRawToDigi/src/SealModule.cc
@@ -0,0 +1,5 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include <EventFilter/CSCRawToDigi/interface/CSCDCCUnpacker.h>
+
+DEFINE_FWK_MODULE(CSCDCCUnpacker)
