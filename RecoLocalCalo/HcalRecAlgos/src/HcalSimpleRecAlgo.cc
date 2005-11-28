2b5f0b3060d2fc7258c7d0909e3e814819169a71
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2005-11-28 19:03:57 +0000

    New info for rec hit timing.  Depends on (currently blank) timing correction functions.

--- RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
@@ -3,19 +3,37 @@
 HcalSimpleRecAlgo::HcalSimpleRecAlgo(int firstSample, int samplesToAdd) : firstSample_(firstSample), samplesToAdd_(samplesToAdd) {
 }
 
+float timeshift_hbhe(float wpksamp) { return 0; }
+float timeshift_hf(float wpksamp) { return 0; }
+float timeshift_ho(float wpksamp) { return 0; }
+
 namespace HcalSimpleRecAlgoImpl {
   template<class Digi, class RecHit>
   inline RecHit reco(const Digi& digi, const HcalCoder& coder, const HcalCalibrations& calibs, int ifirst, int n) {
     CaloSamples tool;
     coder.adc2fC(digi,tool);
 
-    double ampl=0;
+    double ampl=0; int maxI = -1; double maxA = -1e10; float ta=0;
     for (int i=ifirst; i<tool.size() && i<n+ifirst; i++) {
       int capid=digi[i].capid();
-      ampl+=(tool[i]-calibs.pedestal(capid))*calibs.gain(capid);
+      ta = (tool[i]-calibs.pedestal(capid))*calibs.gain(capid);
+      ampl+=ta;
+      if(ta>maxA){
+	maxA=ta;
+	maxI=i;
+      }
     }
+    
+    if(maxI==ifirst || maxI==(n+ifirst)) printf("HcalSimpleRecAlgoImpl:  This is bad!!!\n");
 
-    return RecHit(digi.id(),ampl,0);    
+    int capid=digi[maxI-1].capid();
+    float t0 = (tool[maxI-1]-calibs.pedestal(capid))*calibs.gain(capid);
+    capid=digi[maxI+1].capid();
+    float t2 = (tool[maxI+1]-calibs.pedestal(capid))*calibs.gain(capid);    
+    float wpksamp = (maxA + 2.0*t2) / (t0 + maxA + t2);
+    float time = (maxI - digi.presamples())*25.0 + timeshift_hbhe(wpksamp);
+    
+    return RecHit(digi.id(),ampl,time);    
   }
 }
 
