f81bbdd7f5536b2f844239ec37dc9073a01c64d9
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2005-11-28 16:41:36 +0000

    added the new producer using the anal fit to the seal dictioanry

--- RecoLocalCalo/EcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/EcalRecProducers/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalWeightUncalibRecHitProducer.h"
+#include "RecoLocalCalo/EcalRecProducers/interface/EcalAnalFitUncalibRecHitProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalWeightUncalibRecHitProducer );
+DEFINE_ANOTHER_FWK_MODULE( EcalAnalFitUncalibRecHitProducer );
