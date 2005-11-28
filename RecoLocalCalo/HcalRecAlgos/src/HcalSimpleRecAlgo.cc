31e100dd39e2a773046b8ab40a06aae473aebb64
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2005-11-28 19:36:11 +0000

    Modified for separate HF timing structure...

--- RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalSimpleRecAlgo.cc
@@ -3,9 +3,9 @@
 HcalSimpleRecAlgo::HcalSimpleRecAlgo(int firstSample, int samplesToAdd) : firstSample_(firstSample), samplesToAdd_(samplesToAdd) {
 }
 
-float timeshift_hbhe(float wpksamp) { return 0; }
+float timeshift_hbheho(float wpksamp) { return 0; }
 float timeshift_hf(float wpksamp) { return 0; }
-float timeshift_ho(float wpksamp) { return 0; }
+
 
 namespace HcalSimpleRecAlgoImpl {
   template<class Digi, class RecHit>
@@ -31,7 +31,7 @@ namespace HcalSimpleRecAlgoImpl {
     capid=digi[maxI+1].capid();
     float t2 = (tool[maxI+1]-calibs.pedestal(capid))*calibs.gain(capid);    
     float wpksamp = (maxA + 2.0*t2) / (t0 + maxA + t2);
-    float time = (maxI - digi.presamples())*25.0 + timeshift_hbhe(wpksamp);
+    float time = (maxI - digi.presamples())*25.0 + timeshift_hbheho(wpksamp);
     
     return RecHit(digi.id(),ampl,time);    
   }
@@ -40,9 +40,33 @@ namespace HcalSimpleRecAlgoImpl {
 HBHERecHit HcalSimpleRecAlgo::reconstruct(const HBHEDataFrame& digi, const HcalCoder& coder, const HcalCalibrations& calibs) const {
   return HcalSimpleRecAlgoImpl::reco<HBHEDataFrame,HBHERecHit>(digi,coder,calibs,firstSample_,samplesToAdd_);
 }
-HFRecHit HcalSimpleRecAlgo::reconstruct(const HFDataFrame& digi, const HcalCoder& coder, const HcalCalibrations& calibs) const {
-  return HcalSimpleRecAlgoImpl::reco<HFDataFrame,HFRecHit>(digi,coder,calibs,firstSample_,samplesToAdd_);
-}
 HORecHit HcalSimpleRecAlgo::reconstruct(const HODataFrame& digi, const HcalCoder& coder, const HcalCalibrations& calibs) const {
   return HcalSimpleRecAlgoImpl::reco<HODataFrame,HORecHit>(digi,coder,calibs,firstSample_,samplesToAdd_);
+}
+
+HFRecHit HcalSimpleRecAlgo::reconstruct(const HFDataFrame& digi, const HcalCoder& coder, const HcalCalibrations& calibs) const {
+  CaloSamples tool;
+  coder.adc2fC(digi,tool);
+  
+  double ampl=0; int maxI = -1; double maxA = -1e10; float ta=0;
+  for (int i=firstSample_; i<tool.size() && i<samplesToAdd_+firstSample_; i++) {
+    int capid=digi[i].capid();
+    ta = (tool[i]-calibs.pedestal(capid))*calibs.gain(capid);
+    ampl+=ta;
+    if(ta>maxA){
+      maxA=ta;
+      maxI=i;
+    }
+  }
+  
+  if(maxI==firstSample_ || maxI==(samplesToAdd_+firstSample_)) printf("HcalSimpleRecAlgoImpl:  This is bad!!!\n");
+  
+  int capid=digi[maxI-1].capid();
+  float t0 = (tool[maxI-1]-calibs.pedestal(capid))*calibs.gain(capid);
+  capid=digi[maxI+1].capid();
+  float t2 = (tool[maxI+1]-calibs.pedestal(capid))*calibs.gain(capid);    
+  float wpksamp = (maxA + 2.0*t2) / (t0 + maxA + t2);
+  float time = (maxI - digi.presamples())*25.0 + timeshift_hf(wpksamp);
+  
+  return HFRecHit(digi.id(),ampl,time); 
 }
