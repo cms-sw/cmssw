75972f7754b68649526f6a6fd0115ab387cae95b
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2005-11-29 19:40:26 +0000

    *** empty log message ***

--- CondFormats/DataRecord/src/SiStripReadoutCablingRcd.cc
+++ CondFormats/DataRecord/src/SiStripReadoutCablingRcd.cc
@@ -1,4 +1,4 @@
-#include "CondFormats/DataRecord/interface/SiStripReadOutCablingRcd.h"
+#include "CondFormats/DataRecord/interface/SiStripReadoutCablingRcd.h"
 #include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
 
 EVENTSETUP_RECORD_REG(SiStripReadOutCablingRcd);
