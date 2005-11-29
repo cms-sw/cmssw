9425fafac72857703c132c264bc56ef9a9134421
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-11-29 12:09:59 +0000

    added first implementation

---
+++ DataFormats/HLTReco/src/classes.h
@@ -0,0 +1,14 @@
+#include "DataFormats/HLTReco/interface/HLTResult.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+
+namespace {
+  namespace {
+    reco::HLTResult< 8> h1;
+    reco::HLTResult<16> h2;
+    reco::HLTResult<24> h3;
+
+    edm::Wrapper<reco::HLTResult< 8> > w1;
+    edm::Wrapper<reco::HLTResult<16> > w2;
+    edm::Wrapper<reco::HLTResult<24> > w3;
+  }
+}
