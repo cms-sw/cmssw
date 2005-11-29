7b453f08c8b63e5dfb12e98ffebf4afdbf8af488
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2005-11-29 11:03:25 +0000

    added SiStrip object records

---
+++ CondFormats/DataRecord/interface/SiStripControlCablingRcd.h
@@ -0,0 +1,6 @@
+#ifndef SISTRIPCONTROLCABLINGRCD_H
+#define SISTRIPCONTROLCABLINGRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class SiStripControlCablingRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripControlCablingRcd> {};
+#endif
