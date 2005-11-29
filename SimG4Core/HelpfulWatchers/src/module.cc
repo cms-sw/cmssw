80f0063a8aeaa46d018a2049f06c02857c37bedb
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-11-29 18:42:56 +0000

    added BeginOfTrackCounter

--- SimG4Core/HelpfulWatchers/src/module.cc
+++ SimG4Core/HelpfulWatchers/src/module.cc
@@ -1,8 +1,11 @@
 #include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
 #include "SimG4Core/HelpfulWatchers/src/SimTracer.h"
+#include "SimG4Core/HelpfulWatchers/src/BeginOfTrackCounter.h"
 
 #include "PluginManager/ModuleDef.h"
 
+using namespace simwatcher;
 DEFINE_SEAL_MODULE ();
 DEFINE_SIMWATCHER (SimTracer);
+DEFINE_SIMWATCHER (BeginOfTrackCounter);
 
