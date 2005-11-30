0efd345e29bdab18de92366c8ad5a18991e0a8d3
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2005-11-30 17:13:41 +0000

    *** empty log message ***

---
+++ DataFormats/DTDigi/test/dumpDTDigi.cc
@@ -0,0 +1,53 @@
+#include <FWCore/Framework/interface/MakerMacros.h>
+#include <FWCore/Framework/interface/EDAnalyzer.h>
+#include <FWCore/Framework/interface/Event.h>
+#include <DataFormats/DTDigi/interface/DTDigiCollection.h>
+
+
+#include <iostream>
+
+using namespace edm;
+using namespace std;
+
+namespace test{
+  class DumpFEDRawDataProduct: public EDAnalyzer{
+  
+  public:
+    DumpFEDRawDataProduct(const ParameterSet& pset){}
+ 
+    void analyze(const Event & e, const EventSetup& c){
+      cout << "--- Run: " << e.id().run()
+	   << " Event: " << e.id().event() << endl;
+      Handle<DTDigiCollection> dtDigis;
+      e.getByLabel("dtunpacker", dtDigis);
+
+      DTDigiCollection::DigiRangeIterator detUnitIt;
+      for (detUnitIt=digiCollection.begin();
+	   detUnitIt!=digiCollection.end();
+	   ++detUnitIt){
+
+	const DTDetId& id = (*detUnitIt).first;
+	const DTDigiCollection::Range& range = (*detUnitIt).second;
+
+	// We have inserted digis for only one DetUnit...
+	CPPUNIT_ASSERT(id==layer);
+
+	// Loop over the digis of this DetUnit
+	for (DTDigiCollection::const_iterator digiIt = range.first;
+	     digiIt!=range.second;
+	     ++digiIt){
+
+
+	  CPPUNIT_ASSERT((*digiIt).wire()==1);
+	  CPPUNIT_ASSERT((*digiIt).number()==4);
+	  CPPUNIT_ASSERT((*digiIt).countsTDC()==5);
+
+
+	}// for digis in layer
+      }// for layers
+    }
+    
+  };
+DEFINE_FWK_MODULE(DumpFEDRawDataProduct)      
+}
+    
