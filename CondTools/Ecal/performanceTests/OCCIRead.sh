8510286d3df9f850eb97d8fec03da50a5b629263
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2005-11-30 16:49:04 +0000

    Fixed missing server env variables

--- CondTools/Ecal/performanceTests/OCCIRead.sh
+++ CondTools/Ecal/performanceTests/OCCIRead.sh
@@ -3,12 +3,15 @@
 source setup.sh
 source stats.sh
 
+THISDIR=`pwd`
 SCRAM_ARCH=slc3_ia32_gcc323
 CMSSWVERSION=CMSSW_0_2_0_pre7
-THISDIR=`pwd`
-CMSSWVERSION=CMSSW_0_2_0_pre7 #change to the CMSSW version for testing
 export CVSROOT=:kserver:cmscvs.cern.ch:/cvs_server/repositories/CMSSW
 export SCRAM_ARCH=slc3_ia32_gcc323
+SERVICENAME=cms_val_lb
+SERVERNAME=${SERVICENAME}.cern.ch
+SERVERHOSTNAME=int2r1-v.cern.ch
+
 READER=${CMSSWVERSION}/test/${SCRAM_ARCH}/OCCIPedReader
 LIMIT=0 # Limit to N object, 0 for no limit
 NUMTESTS=5
