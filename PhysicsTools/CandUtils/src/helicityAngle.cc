6e7b4f3df6f32503b4f1a96ddbb53342168db85f
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2005-12-01 11:59:50 +0000

    renamed Vertex type to Point

--- PhysicsTools/CandUtils/src/helicityAngle.cc
+++ PhysicsTools/CandUtils/src/helicityAngle.cc
@@ -1,4 +1,4 @@
-// $Id: helicityAngle.cc,v 1.3 2005/10/21 09:08:05 llista Exp $
+// $Id: helicityAngle.cc,v 1.4 2005/11/04 09:40:00 llista Exp $
 #include "PhysicsTools/CandUtils/interface/helicityAngle.h"
 #include "PhysicsTools/Candidate/interface/Candidate.h"
 #include "PhysicsTools/CandUtils/interface/Booster.h"
@@ -7,7 +7,7 @@ using namespace std;
 
 double helicityAngle( const Candidate & cand ) {
   assert( cand.numberOfDaughters() == 2 );
-  Particle::Momentum boost = - cand.p4().boostVector(); 
+  Particle::Vector boost = - cand.p4().boostVector(); 
   Particle::LorentzVector dp = cand.daughter( 0 )->p4();
   dp.boost( boost );
   double h = dp.angle( boost );
