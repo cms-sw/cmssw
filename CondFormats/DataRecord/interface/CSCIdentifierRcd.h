df48fa2405efa0bb57a9f04a0b6fbf56df436715
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2005-12-02 10:15:45 +0000

    Added Identifier

---
+++ CondFormats/DataRecord/interface/CSCIdentifierRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCINDENTIFIERSRCD_H
+#define CSCIDENTIFIERRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCIdentifierRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCIdentifierRcd> {};
+#endif
