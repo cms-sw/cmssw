0c337fb8dd848f90eab7406ebb83a978368b3596
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2005-12-02 09:59:47 +0000

    Uncommented CSCIdentifier

--- CondCore/CSCPlugins/src/plugin.cc
+++ CondCore/CSCPlugins/src/plugin.cc
@@ -13,13 +13,13 @@
 #include "CondFormats/DataRecord/interface/CSCGainsRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
 #include "CondFormats/DataRecord/interface/CSCcrosstalkRcd.h"
-//#include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
-//#include "CondFormats/DataRecord/interface/CSCIdentifierRcd.h"
+#include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
+#include "CondFormats/DataRecord/interface/CSCIdentifierRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCReadoutMapping.h"
 #include "CondFormats/DataRecord/interface/CSCReadoutMappinRcd.h"
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(CSCPedestalsRcd,CSCPedestals);
 REGISTER_PLUGIN(CSCGainsRcd,CSCGains);
 REGISTER_PLUGIN(CSCcrosstalkRcd,CSCcrosstalk);
-//REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
+REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
 REGISTER_PLUGIN(CSCReadoutMappingRcd,CSCReadoutMapping);
