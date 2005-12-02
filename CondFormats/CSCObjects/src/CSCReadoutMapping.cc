28de8ab62a7e5ef1f213aada4710c66e1fa35840
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2005-12-02 14:15:11 +0000

    improved

--- CondFormats/CSCObjects/src/CSCReadoutMapping.cc
+++ CondFormats/CSCObjects/src/CSCReadoutMapping.cc
@@ -6,12 +6,12 @@ CSCReadoutMapping::CSCReadoutMapping() : myName_("CSCReadoutMapping"), debugV_( 
 
 CSCReadoutMapping::~CSCReadoutMapping(){}
 
-int CSCReadoutMapping::chamber( int endcap, int station, int vme, int dmb, int tmb ){
+int CSCReadoutMapping::chamber( int endcap, int station, int vme, int dmb, int tmb ) const {
   // Build hw id from input, find sw id to match
   int cid = 0;
   int hid = hwId( endcap, station, vme, dmb, tmb );
   // Search for that hw id in mapping
-  std::map<int,int>::iterator it = hw2sw_.find( hid );
+  std::map<int,int>::const_iterator it = hw2sw_.find( hid );
   if ( it != hw2sw_.end() ) {
     cid = it->second;
     if ( debugV() ) std::cout << myName_ << ": for requested hw id = " << hid <<
@@ -24,7 +24,7 @@ int CSCReadoutMapping::chamber( int endcap, int station, int vme, int dmb, int t
   return cid;
 }
 
-CSCDetId CSCReadoutMapping::detId( int endcap, int station, int vme, int dmb, int tmb, int layer ){
+CSCDetId CSCReadoutMapping::detId( int endcap, int station, int vme, int dmb, int tmb, int layer ) const {
   int cid = chamber( endcap, station, vme, dmb, tmb );
   int lid = cid + layer;
   return CSCDetId( lid );
@@ -46,17 +46,7 @@ void CSCReadoutMapping::addRecord( int endcap, int station, int ring, int chambe
   }
 } 
 
-int CSCReadoutMapping::hwId( int endcap, int station, int vmecrate, int dmb, int tmb ) {
-  int id = 0;
-  //@@ FIXME This is ONLY for Slice Test Nov-2005
-  id = vmecrate * 16 + dmb;
-  if ( debugV() ) std::cout << myName_ <<": hardware id for endcap " << endcap <<
-    " station " << station << " vmecrate " << vmecrate << " dmb slot " << dmb <<
-    " tmb slot " << tmb << " = " << id << std::endl;
-  return id;
-}
-
-int CSCReadoutMapping::swId( int endcap, int station, int ring, int chamber ) {
+int CSCReadoutMapping::swId( int endcap, int station, int ring, int chamber ) const {
   // Software id is just CSCDetId for the chamber
   return CSCDetId::rawIdMaker( endcap, station, ring, chamber, 0 ); // usual detid for chamber, i.e. layer=0
 }
