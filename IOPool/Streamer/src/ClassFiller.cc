f320791c8ff8bd7e156dfb15f6a3aeb25e8edcac
James Kowalkowski <sha1-19a7633f7f2c73cd2080529650f067bdb0f96f44@cern.ch>
2005-12-05 03:58:22 +0000

    unused variable

--- IOPool/Streamer/src/ClassFiller.cc
+++ IOPool/Streamer/src/ClassFiller.cc
@@ -159,7 +159,7 @@ namespace edm {
       seal::reflex::Type cc = seal::reflex::Type::byName(name);
 
       // next two lines are for explicitly causing every object to get defined
-      pool::IClassLoader* cl = getClassLoader();
+      //pool::IClassLoader* cl = getClassLoader();
 
 	  // jbk - I'm leaving this out unless we really need it -
 	  // its job is to declare each of the types to ROOT
