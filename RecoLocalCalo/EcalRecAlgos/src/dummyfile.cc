e83973262c39d177d0825e712aaca3fcc76581b9
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2005-12-05 16:57:00 +0000

    remove unnecessary dummy objects from the dummyfile

--- RecoLocalCalo/EcalRecAlgos/src/dummyfile.cc
+++ RecoLocalCalo/EcalRecAlgos/src/dummyfile.cc
@@ -1,9 +1,4 @@
 // ShR 17 Sept 2005: add this until scram can handle packages with only templated classes
-
-#include "RecoLocalCalo/EcalRecAlgos/interface/EcalUncalibRecHitRecAnalFitAlgo.h"
-#include "RecoLocalCalo/EcalRecAlgos/interface/EcalUncalibRecHitRecWeightsAlgo.h"
-#include "DataFormats/EcalDigi/interface/EBDataFrame.h"
-
 namespace {
   namespace {
     class dummyfile {
@@ -11,7 +6,5 @@ namespace {
       dummyfile() { };
       virtual ~dummyfile() { };
     };
-     EcalUncalibRecHitRecAnalFitAlgo<EBDataFrame> algo1;
-     EcalUncalibRecHitRecWeightsAlgo<EBDataFrame> algo2;
   }
 }
