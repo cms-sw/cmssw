b9cec0d8752e6a12dec93479b1de86d80686ffab
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2005-12-06 11:33:59 +0000

    Add renumbering

--- SimDataFormats/SimHitMaker/interface/TrackingSlaveSD.h
+++ SimDataFormats/SimHitMaker/interface/TrackingSlaveSD.h
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <vector>
+class SimTrackManager;
 
 class TrackingSlaveSD
 {
@@ -15,6 +16,7 @@ public:
     TrackingSlaveSD(std::string);
     virtual ~TrackingSlaveSD();
     virtual void Initialize();
+    virtual void renumbering(const SimTrackManager*); 
     virtual bool processHits(const PSimHit&);
     virtual bool format();
     std::string name() const { return name_; } 
--- SimDataFormats/SimHitMaker/src/TrackingSlaveSD.cc
+++ SimDataFormats/SimHitMaker/src/TrackingSlaveSD.cc
@@ -1,13 +1,14 @@
 #include "SimDataFormats/SimHitMaker/interface/TrackingSlaveSD.h"
+#include "SimG4Core/Application/interface/SimTrackManager.h"
 
 #include <iostream>
-#define debug
+//#define DEBUG
 
 using std::cout;
 using std::endl;
 
 TrackingSlaveSD::TrackingSlaveSD(std::string myName) : name_(myName){
-#ifdef debug
+#ifdef DEBUG
  cout << " TrackingSlaveSD " << name_ << endl; 
 #endif
 }
@@ -17,15 +18,35 @@ TrackingSlaveSD::~TrackingSlaveSD()
 
 void TrackingSlaveSD::Initialize(){
 
-#ifdef debug
+#ifdef DEBUG
   std::cout << " initialize TrackingSlaveSD "<< name_ << std::endl;
 #endif  
   hits_.clear(); 
 }
 
+void TrackingSlaveSD::renumbering(const SimTrackManager* tkManager){
+  //
+  // Now renumber the Hits
+  //
+  std::cout << " TrackingSlaveSD "<<name()<<" renumbering " << hits_.size() <<" hits."<< std::endl;
+  //
+  // now I loop over PSimHits and change the id inside
+  //
+  for(TrackingSlaveSD::Collection::const_iterator it = begin(); it!= end(); it++){
+    PSimHit& temp = const_cast<PSimHit&>(*it);
+    unsigned int nt = tkManager->g4ToSim(temp.trackId());
+#ifdef DEBUG
+    std::cout <<" Studying PSimHit " << temp << std::endl;
+    std::cout <<" Changing TrackID from " << temp.trackId();
+    std::cout <<" with " << nt << std::endl;
+#endif
+    setTrackId( temp, nt);
+  }
+
+}
 bool TrackingSlaveSD::format()
 {
-#ifdef debug 
+#ifdef DEBUG 
   cout << " TrackingSlaveSD " << name_<< " formatting " << hits_.size() << " hits." << endl;
 #endif  
   return true;
@@ -33,7 +54,7 @@ bool TrackingSlaveSD::format()
 
 bool TrackingSlaveSD::processHits(const PSimHit & ps)
 {
-#ifdef debug
+#ifdef DEBUG
   std::cout <<" Sent Hit " << ps << " to ROU " << name_ << std::endl;
 #endif
   hits_.push_back(ps);
