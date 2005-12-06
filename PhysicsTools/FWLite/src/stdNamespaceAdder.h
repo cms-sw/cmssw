14498139c8542de674ca3db7dbe827f0d770ea8b
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-12-06 17:06:23 +0000

    first submission

---
+++ PhysicsTools/FWLite/src/stdNamespaceAdder.h
@@ -0,0 +1,30 @@
+#ifndef FWLite_stdNamespaceAdder_h
+#define FWLite_stdNamespaceAdder_h
+// -*- C++ -*-
+//
+// Package:     FWLite
+// Class  :     stdNamespaceAdder
+// 
+/**\class stdNamespaceAdder stdNamespaceAdder.h PhysicsTools/FWLite/interface/stdNamespaceAdder.h
+
+ Description: Adds back the 'std::' namespace prefix to standard classes
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  
+//         Created:  Tue Dec  6 09:18:09 EST 2005
+// $Id$
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+namespace fwlite {
+   std::string stdNamespaceAdder(const std::string&);
+}
+#endif
