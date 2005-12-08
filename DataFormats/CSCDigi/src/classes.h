a8c3e75152228afc2196e7b36135ea447e226897
Nikolay Terentyev <sha1-7363ba4fe20967489bddf3505d82191852cbf6a9@cern.ch>
2005-12-08 23:38:49 +0000

    CSCALCTDigi added

--- DataFormats/CSCDigi/src/classes.h
+++ DataFormats/CSCDigi/src/classes.h
@@ -6,6 +6,8 @@
 #include <DataFormats/CSCDigi/interface/CSCStripDigiCollection.h>
 #include <DataFormats/CSCDigi/interface/CSCComparatorDigi.h>
 #include <DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h>
+#include <DataFormats/CSCDigi/interface/CSCALCTDigi.h>
+#include <DataFormats/CSCDigi/interface/CSCALCTDigiCollection.h>
 #include <FWCore/EDProduct/interface/Wrapper.h>
 #include <vector>
 #include <map>
@@ -13,33 +15,38 @@
 namespace{ 
   namespace {
 
-  CSCWireDigi cWD_;
-  CSCRPCDigi  cRD_;
-  CSCStripDigi cSD_;
+  CSCWireDigi       cWD_;
+  CSCRPCDigi        cRD_;
+  CSCStripDigi      cSD_;
   CSCComparatorDigi cCD_;
+  CSCALCTDigi       cALCTD_;
 
   CSCWireDigi::PersistentPacking ppWD_;
   CSCRPCDigi::PersistentPacking ppRD_;
+  CSCALCTDigi::PersistentPacking ppALCTD_;
 
-  std::vector<CSCWireDigi>  vWD_;
-  std::vector<CSCRPCDigi>   vRD_;
-  std::vector<CSCStripDigi>  vSD_;
+  std::vector<CSCWireDigi>        vWD_;
+  std::vector<CSCRPCDigi>         vRD_;
+  std::vector<CSCStripDigi>       vSD_;
   std::vector<CSCComparatorDigi>  vCD_;
+  std::vector<CSCALCTDigi>        vALCTD_;
 
-  std::vector<std::vector<CSCWireDigi> >  vvWD_; 
-  std::vector<std::vector<CSCRPCDigi>  >  vvRD_;
-  std::vector<std::vector<CSCStripDigi> >  vvSD_; 
+  std::vector<std::vector<CSCWireDigi> >        vvWD_; 
+  std::vector<std::vector<CSCRPCDigi>  >        vvRD_;
+  std::vector<std::vector<CSCStripDigi> >       vvSD_; 
   std::vector<std::vector<CSCComparatorDigi> >  vvCD_; 
+  std::vector<std::vector<CSCALCTDigi> >        vvALCTD_;
 
-  CSCWireDigiCollection clWD_;
-  CSCRPCDigiCollection  clRD_;
-  CSCStripDigiCollection clSD_;
+  CSCWireDigiCollection       clWD_;
+  CSCRPCDigiCollection        clRD_;
+  CSCStripDigiCollection      clSD_;
   CSCComparatorDigiCollection clCD_;
+  CSCALCTDigiCollection       clALCTD_;
     
-  edm::Wrapper<CSCWireDigiCollection> wWD_;
-  edm::Wrapper<CSCRPCDigiCollection> wRD_;
-  edm::Wrapper<CSCStripDigiCollection> wSD_;
+  edm::Wrapper<CSCWireDigiCollection>      wWD_;
+  edm::Wrapper<CSCRPCDigiCollection>       wRD_;
+  edm::Wrapper<CSCStripDigiCollection>     wSD_;
   edm::Wrapper<CSCComparatorDigiCollection> wCD_;
-
+  edm::Wrapper<CSCALCTDigiCollection>       wALCTD_;
   }
 }
