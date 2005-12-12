d37f791f94cee07f0b237014f9f277aea8ff62cc
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2005-12-12 17:29:50 +0000

    moved some actions to RPCRecord

--- EventFilter/RPCRawToDigi/interface/RPCUnpackingModule.h
+++ EventFilter/RPCRawToDigi/interface/RPCUnpackingModule.h
@@ -2,6 +2,15 @@
 #define EVENTFILTER_RPCUnpackingModule_H
 
 
+/** \class RPCUnpackingModule
+ *  Driver class for unpacking RPC raw data (DCC format)
+ *
+ *  $Date: 2005/10/06 18:25:22 $
+ *  $Revision: 1.1 $
+ *  \author Ilaria Segoni - CERN
+ */
+
+
 #include <FWCore/Framework/interface/MakerMacros.h>
 #include <FWCore/Framework/interface/EDProducer.h>
 #include <EventFilter/RPCRawToDigi/interface/RPCEventData.h>
@@ -12,25 +21,33 @@
 class RPCUnpackingModule: public edm::EDProducer {
   public:
     
+    ///Constructor
     RPCUnpackingModule(const edm::ParameterSet& pset);
+    
+    ///Destructor
     virtual ~RPCUnpackingModule();
-
-  /** Retrieves a RPCDigiCollection from the Event, creates a
+ 
+   /** Retrieves a RPCDigiCollection from the Event, creates a
       FEDRawDataCollection (EDProduct) using the DigiToRaw converter,
       and attaches it to the Event. */
-   void produce(edm::Event & e, const edm::EventSetup& c); 
+    void produce(edm::Event & e, const edm::EventSetup& c); 
+  
+    /// Unpacks FED Header(s), returns number of Headers 
+    int HeaderUnpacker(const unsigned char* headerIndex);
 
-
-   
-   RPCEventData eventData(){return rpcData;}
+    /// Unpacks FED Trailer(s), returns number of Trailers 
+    int TrailerUnpacker(const unsigned char* trailererIndex);
+    
+    ///Fills Container for DQM imformation
+    RPCEventData eventData(){return rpcData;}
 
   private:
   
-  RPCEventData rpcData; 
+    bool printout;  
+    RPCEventData rpcData; 
    
 
   };
 
-DEFINE_FWK_MODULE(RPCUnpackingModule)
 
 #endif
