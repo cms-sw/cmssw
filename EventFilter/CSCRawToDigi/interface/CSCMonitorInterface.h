f1e477bf6b191c5212144ccfb4f80e448984ffd2
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2005-12-12 09:11:35 +0000

    made process pure virtual method

--- EventFilter/CSCRawToDigi/interface/CSCMonitorInterface.h
+++ EventFilter/CSCRawToDigi/interface/CSCMonitorInterface.h
@@ -1,6 +1,18 @@
 #ifndef CSCMonitorInterface_h
 #define CSCMonitorInterface_h
 
+/** \class CSCMonitorInterface
+ *
+ * Interface to the Data Quality Monitoring Module.
+ *  
+ *  $Date: 2005/11/11 10:31:56 $
+ *  $Revision: 1.2 $
+ *
+ * \author Ilaria Segoni (CERN)
+ *
+ */
+
+
 class CSCDCCEventData;
 
 class CSCMonitorInterface{
@@ -9,7 +21,7 @@ public:
 
     CSCMonitorInterface(){}
     virtual ~CSCMonitorInterface(){}
-    virtual void process(CSCDCCEventData & dccData){}
+    virtual void process(CSCDCCEventData & dccData)=0;
 
       
 
