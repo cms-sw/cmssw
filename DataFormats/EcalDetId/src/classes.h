ebe8ea85826bc8beaa9f8aba026e81c81b6cf946
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2005-12-15 16:30:05 +0000

    Added collections

--- DataFormats/EcalDetId/src/classes.h
+++ DataFormats/EcalDetId/src/classes.h
@@ -3,3 +3,24 @@
 #include "DataFormats/EcalDetId/interface/ESDetId.h"
 #include "DataFormats/EcalDetId/interface/EcalTrigTowerDetId.h"
 #include "DataFormats/EcalDetId/interface/EcalPnDiodeDetId.h"
+
+#include "DataFormats/EcalDetId/interface/EcalDetIdCollections.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+
+namespace {
+  namespace {
+    edm::EDCollection<EBDetId> vEBDI_;
+    edm::EDCollection<EcalTrigTowerDetId> vETTDI_;
+
+    EBDetIdCollection theEBDI_;
+    EcalTrigTowerDetIdCollection theETTDI_;
+
+    edm::Wrapper<EBDetIdCollection> anotherEBDIw_;
+    edm::Wrapper<EcalTrigTowerDetIdCollection> anothertheETTDIw_;
+
+
+    edm::Wrapper< edm::EDCollection<EBDetId> > theEBDIw_;
+    edm::Wrapper< edm::EDCollection<EcalTrigTowerDetId> > theETTDIw_;
+ }
+}
+
