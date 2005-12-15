02c6317e58b93949da3df02d802f85a462689aa0
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-12-15 23:15:41 +0000

    Move EDProduct declarations to new header EDProductfwd.h in FWCore/EDProduct

--- FWCore/Framework/interface/Frameworkfwd.h
+++ FWCore/Framework/interface/Frameworkfwd.h
@@ -5,24 +5,23 @@
   
 Forward declarations of types in the EDM.
 
-$Id: Frameworkfwd.h,v 1.10 2005/10/03 19:04:04 wmtan Exp $
+$Id: Frameworkfwd.h,v 1.11 2005/12/12 23:07:48 paterno Exp $
 
 ----------------------------------------------------------------------*/
 
-namespace edm
-{
+#include "FWCore/EDProduct/interface/EDProductfwd.h"
+
+namespace edm {
   class BasicHandle;
   class BranchKey;
   class DelayedReader;
   class EDAnalyzer;
   class EDFilter;
   class EDProducer;
-  class EDProduct;
   class Event;
   class EventAux;
   class EventPrincipal;
   class EventProvenance;
-  class EventRegistry;
   class EventSetup;
   class Group;
   class InputSource;
@@ -34,26 +33,18 @@ namespace edm
   class ParameterSet;
   class ProcessNameSelector;
   class BranchDescription;
-  class ProductID;
   class ProductRegistry;
   class Provenance;
   class RandomAccessInputSource;
-  class RefBase;
-  class RefVectorBase;
   class Run;
   class RunHandler;
   class SecondaryInputSource;
   class Selector;
 
-  template <class T> class Wrapper;
-  template <class T> class Handle;
-  template <class T> class Ref;
-  template <class T> class RefVector;
-  template <class T> class RefVectorIterator;
+  template <typename T> class Handle;
 }
 
 // The following are trivial enough so that the real headers can be included.
-#include "FWCore/EDProduct/interface/EventID.h"
 #include "FWCore/Framework/interface/ConditionsID.h"
 #include "FWCore/Framework/interface/PassID.h"
 #include "FWCore/Framework/interface/VersionNumber.h"
