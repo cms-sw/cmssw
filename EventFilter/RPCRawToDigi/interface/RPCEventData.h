1484e0127e5f02cf4681d5eaa4daed5b46f6baaf
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2005-12-15 17:41:44 +0000

    redesing

--- EventFilter/RPCRawToDigi/interface/RPCEventData.h
+++ EventFilter/RPCRawToDigi/interface/RPCEventData.h
@@ -4,16 +4,17 @@
 /** \class RPCEventData
  * Container for Unpacked RPC Data, used by DQM module
  *
- *  $Date: 2005/11/24 18:06:54 $
- *  $Revision: 1.1 $
+ *  $Date: 2005/12/12 17:29:04 $
+ *  $Revision: 1.2 $
  * \author Ilaria Segoni - CERN
  */
 
 #include <EventFilter/RPCRawToDigi/interface/RPCBXData.h>
 #include <EventFilter/RPCRawToDigi/interface/RMBErrorData.h>
+#include <EventFilter/RPCRawToDigi/interface/RPCChannelData.h>
+#include <EventFilter/RPCRawToDigi/interface/RPCChamberData.h>
 
 #include <vector>
-
 
 using namespace std;
 
@@ -24,19 +25,28 @@ public:
    RPCEventData(){DCCDiscarded=0;}
    ~RPCEventData(){}
 
-   void addBXData(RPCBXData & bxData){data.push_back(bxData);}  
-   void addRMBDiscarded(RMBErrorData & errorData){RMBDiscarded.push_back(errorData);}  
-   void addRMBCorrupted(RMBErrorData & errorData){RMBCorrupted.push_back(errorData);}  
-   void addDCCDiscarded(){DCCDiscarded++;}  
+   /// Insert unpacked information
+   void addBXData(RPCBXData & bx);  
+   void addChnData(RPCChannelData & chn);  
+   void addRPCChamberData(RPCChamberData & chmb);  
+   void addRMBDiscarded(RMBErrorData & errorData);  
+   void addRMBCorrupted(RMBErrorData & errorData);  
+   void addDCCDiscarded();  
    
-   vector<RPCBXData> bxData(){return data;}
-   vector<RMBErrorData> dataRMBDiscarded(){return  RMBDiscarded;}
-   vector<RMBErrorData> dataRMBCorrupted(){return  RMBCorrupted;}
-   int dccDiscarded(){return DCCDiscarded;}
+   /// Access Methods to  unpacked information
+   vector<RPCBXData> bxData();
+   vector<RPCChannelData>  dataChannel();
+   vector<RPCChamberData>  dataChamber();
+   vector<RMBErrorData> dataRMBDiscarded();
+   vector<RMBErrorData> dataRMBCorrupted();
+   int dccDiscarded();
 
 private:
 
-   vector<RPCBXData>  data;
+   vector<RPCBXData>  BXData;
+   vector<RPCChannelData> ChnData;
+   vector<RPCChamberData>  ChmbData;
+    
    vector<RMBErrorData> RMBDiscarded;
    vector<RMBErrorData> RMBCorrupted;
    int DCCDiscarded;
