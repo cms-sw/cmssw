2a6d27f2fc761f34b2cd9f19a8e92a99924bc851
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2005-12-16 20:57:03 +0000

    Acommodate new calibration objects

--- SimCalorimetry/HcalSimAlgos/src/HcalNoisifier.cc
+++ SimCalorimetry/HcalSimAlgos/src/HcalNoisifier.cc
@@ -1,5 +1,7 @@
 #include "SimCalorimetry/HcalSimAlgos/interface/HcalNoisifier.h"
 #include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrationWidths.h"
 #include "CalibFormats/CaloObjects/interface/CaloSamples.h"
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
 #include "CLHEP/Random/RandGaussQ.h"
@@ -17,21 +19,23 @@ namespace cms {
   void HcalNoisifier::noisify(CaloSamples & frame) const {
     assert(theDbService != 0);
     HcalDetId hcalDetId(frame.id());
-    std::auto_ptr<HcalCalibrations> calibrations = theDbService->getHcalCalibrations(hcalDetId);
-    std::auto_ptr<HcalCalibrationWidths> widths = theDbService->getHcalCalibrationWidths(hcalDetId);
+    HcalCalibrations calibrations;
+    HcalCalibrationWidths widths;
+    if (!theDbService->makeHcalCalibration (hcalDetId, &calibrations) ||
+	!theDbService->makeHcalCalibrationWidth (hcalDetId, &widths)) {
+      //TODO handle this gracefully
+      assert (0);
+    }
 
-    //TODO handle this gracefully
-    assert(calibrations.get() != 0);
-    assert(widths.get() != 0);
     for(int tbin = 0; tbin < frame.size(); ++tbin) {
       int capId = (theStartingCapId + tbin)%4;
 //std::cout << "PEDS " << capId << " " << calibrations->pedestal(capId) << " " << widths->pedestal(capId) << " " << calibrations->gain(capId) <<" " << widths->gain(capId) << std::endl;
-      double pedestal = theRandGaussian->shoot(calibrations->pedestal(capId), widths->pedestal(capId));
-      double gain = theRandGaussian->shoot(calibrations->gain(capId), widths->gain(capId));
-      // pedestals come in units of GeV.  Use gain to convert
-      //frame[tbin] = (frame[tbin]+pedestal) / gain;
-      // frame will already be in fC
-      frame[tbin] += pedestal / gain;
+      double pedestal = theRandGaussian->shoot(calibrations.pedestal(capId), widths.pedestal(capId));
+      double gain0 = calibrations.gain(capId);
+      double gain = theRandGaussian->shoot(gain0, widths.gain(capId));
+      // pedestals come in units of fC for now. Could be in ADC's eventually
+      frame[tbin] += pedestal; // noisify pedestal
+      frame[tbin] = frame[tbin] / gain * gain0; // nosify gain
     }
     //std::cout << frame << std::endl;
   }
