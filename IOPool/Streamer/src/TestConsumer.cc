821ce9da3769183a98c5e27149a5badc51323d40
James Kowalkowski <sha1-19a7633f7f2c73cd2080529650f067bdb0f96f44@cern.ch>
2005-12-16 17:35:02 +0000

    allow file event count limits

--- IOPool/Streamer/src/TestConsumer.cc
+++ IOPool/Streamer/src/TestConsumer.cc
@@ -2,39 +2,102 @@
 #include "IOPool/Streamer/src/TestConsumer.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
+#include "boost/shared_ptr.hpp"
+
 #include <string>
+#include <cstring>
 #include <fstream>
+#include <sstream>
 
 using namespace edm;
 using namespace std;
 
 namespace edmtest
 {
+  typedef boost::shared_ptr<ofstream> OutPtr;
+  typedef vector<char> SaveArea;
+
+  string makeFileName(const string& base, int num)
+  {
+    ostringstream ost;
+    ost << base << num << ".dat";
+    return ost.str();
+  }
+
+  OutPtr makeFile(const string name,int num)
+  {
+    OutPtr p(new ofstream(makeFileName(name,num).c_str(),
+			  ios_base::binary | ios_base::out));
+
+    if(!(*p))
+      {
+	throw cms::Exception("Configuration","TestConsumer")
+	  << "cannot open file " << name;
+      }
+
+    return p;
+  }
 
   struct Worker
   {
-    Worker(const string& s);
+    Worker(const string& s, int m);
 
     string filename_;
-    ofstream ost_;
+    int file_num_;
+    int cnt_;
+    int max_;
+    OutPtr ost_; 
+    SaveArea reg_;
+
+    void checkCount();
+    void saveReg(void* buf, int len);
+    void writeReg();
   };
 
-  Worker::Worker(const string& s):
+  Worker::Worker(const string& s,int m):
     filename_(s),
-    ost_(s.c_str(),ios_base::binary | ios_base::out)
+    file_num_(),
+    cnt_(0),
+    max_(m),
+    ost_(makeFile(filename_,file_num_))
   {
-    if(!ost_)
-      {
-	throw cms::Exception("Configuration","TestConsumer")
-	  << "cannot open file " << s;
-      }
   }
   
+  void Worker::checkCount()
+  {
+    if(cnt_!=0 && (cnt_%max_) == 0)
+      {
+	++file_num_;
+	ost_ = makeFile(filename_,file_num_);
+	writeReg();
+      }
+    ++cnt_;
+
+  }
+
+  void Worker::writeReg()
+  {
+    if(!reg_.empty())
+      {
+	int len = reg_.size();
+	ost_->write((const char*)(&len),sizeof(int));
+	ost_->write((const char*)&reg_[0],len);
+      }
+  }
+
+  void Worker::saveReg(void* buf, int len)
+  {
+    reg_.resize(len);
+    memcpy(&reg_[0],buf,len);
+  }
+
+
   // ----------------------------------
 
   TestConsumer::TestConsumer(edm::ParameterSet const& ps, 
 			     edm::EventBuffer* buf):
-    worker_(new Worker(ps.getParameter<string>("fileName"))),
+    worker_(new Worker(ps.getParameter<string>("fileName"),
+		       ps.getUntrackedParameter<int>("numPerFile",1<<31))),
     bufs_(buf)
   {
     // first write out all the product registry data into the front
@@ -48,11 +111,14 @@ namespace edmtest
   
   void TestConsumer::bufferReady()
   {
+    worker_->checkCount();
+
     EventBuffer::ConsumerBuffer cb(*bufs_);
 
     int sz = cb.size();
-    worker_->ost_.write((const char*)(&sz),sizeof(int));
-    worker_->ost_.write((const char*)cb.buffer(),sz);
+    worker_->ost_->write((const char*)(&sz),sizeof(int));
+    worker_->ost_->write((const char*)cb.buffer(),sz);
+
   }
 
   void TestConsumer::stop()
@@ -63,7 +129,7 @@ namespace edmtest
 
   void TestConsumer::sendRegistry(void* buf, int len)
   {
-    worker_->ost_.write((const char*)(&len),sizeof(int));
-    worker_->ost_.write((const char*)buf,len);    
+    worker_->saveReg(buf,len);
+    worker_->writeReg();
   }
 }
