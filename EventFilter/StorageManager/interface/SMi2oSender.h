8de6648b0fb899dc5655be0c9b4aa373e447847a
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2005-12-16 00:44:58 +0000

    Added default web page with statistics.

--- EventFilter/StorageManager/interface/SMi2oSender.h
+++ EventFilter/StorageManager/interface/SMi2oSender.h
@@ -12,6 +12,10 @@
      version 1.1 2005/11/23
        Initial implementation, only creates pool and destination.
        Uses a global pointer. Needs changes for production version.
+     version 1.2 2005/12/15
+       Changed to using a committed heap memory pool allocator and
+       a way to set its size.
+       Added default home page to show statistics.
 
 */
 
@@ -19,6 +23,12 @@
 #include "xdaq/Application.h"
 #include "xdaq/ApplicationContext.h"
 #include "toolbox/mem/MemoryPoolFactory.h"
+#include "xdata/UnsignedLong.h"
+
+#include "xgi/include/xgi/Input.h"
+#include "xgi/include/xgi/Output.h"
+#include "xgi/include/xgi/exception/Exception.h"
+#include "EventFilter/Utilities/interface/Css.h"
 
 class SMi2oSender: public xdaq::Application
 {
@@ -30,11 +40,19 @@ class SMi2oSender: public xdaq::Application
 
   virtual ~SMi2oSender(){}
 
+  void defaultWebPage
+    (xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception);
+
   private:
+
+  void css(xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception)
+    {css_.css(in,out);}
 
   toolbox::mem::Pool          *pool_;
   vector<xdaq::ApplicationDescriptor*> destinations_;
   xdaq::ApplicationDescriptor* firstDestination_;
+  xdata::UnsignedLong          committedpoolsize_;
+  evf::Css css_;
 
 };
 
