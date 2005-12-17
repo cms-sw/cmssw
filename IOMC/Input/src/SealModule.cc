0ede83cab27d460cad228a9d9a90393b822931a8
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2005-12-17 00:25:39 +0000

    PGun(s) moved into a new package IOMC/ParticleGuns

--- IOMC/Input/src/SealModule.cc
+++ IOMC/Input/src/SealModule.cc
@@ -3,18 +3,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOMC/Input/interface/MCFileSource.h"
 
-// Julia Yarba : related to particle gun prototypes
-//
-#include "IOMC/Input/interface/FlatEGunASCIIWriter.h"
-#include "IOMC/Input/interface/FlatRandomEGunSource.h"
 
   using edm::MCFileSource; 
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_INPUT_SOURCE(MCFileSource)
 
-// particle gun prototypes
-//
-using edm::FlatEGunASCIIWriter;
-DEFINE_ANOTHER_FWK_MODULE(FlatEGunASCIIWriter)
-using edm::FlatRandomEGunSource;
-DEFINE_ANOTHER_FWK_INPUT_SOURCE(FlatRandomEGunSource)
