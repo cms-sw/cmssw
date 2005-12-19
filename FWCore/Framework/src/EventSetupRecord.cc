7c631336306aecc80740f5ecdfe056bfce97bff1
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2005-12-19 21:36:44 +0000

    now an error if multiple components can deliver the same data item

--- FWCore/Framework/src/EventSetupRecord.cc
+++ FWCore/Framework/src/EventSetupRecord.cc
@@ -11,9 +11,12 @@
 //
 
 // system include files
+#include <assert.h>
 
 // user include files
 #include "FWCore/Framework/interface/EventSetupRecord.h"
+#include "FWCore/Framework/interface/DataProxy.h"
+#include "FWCore/Framework/interface/ComponentDescription.h"
 
 namespace edm {
    namespace eventsetup {
@@ -68,11 +71,33 @@ EventSetupRecord::add(const DataKey& iKey ,
                     const DataProxy* iProxy)
 {
    //
-   if (0 != find(iKey)) {
+   const DataProxy* proxy = find(iKey);
+   if (0 != proxy) {
       //
       // we already know the field exist, so do not need to check against end()
       //
-      (*proxies_.find(iKey)).second = iProxy ;
+      
+      // POLICY: If a Producer and a Source both claim to deliver the same data, the
+      //  Producer 'trumps' the Source. If two modules of the same type claim to deliver the
+      //  same data, this is an error unless the configuration specifically states which one
+      //  is to be chosen.
+      assert(proxy->providerDescription());
+      assert(iProxy->providerDescription());
+      if(proxy->providerDescription()->isSource_ == iProxy->providerDescription()->isSource_) {
+         //should lookup to see if there is a specified 'chosen' one and only if not, throw the exception
+         throw cms::Exception("EventSetupConflict") <<"two EventSetup "<< 
+         (proxy->providerDescription()->isSource_? "Sources":"Producers")
+         <<" want to deliver type=\""<< iKey.type().name() <<"\" label=\""<<iKey.name().value()<<"\".\n"
+         <<" the two providers are \n"
+         <<"1) type=\""<<proxy->providerDescription()->type_<<"\" label=\""<<proxy->providerDescription()->label_<<"\"\n"
+         <<"2) type=\""<<iProxy->providerDescription()->type_<<"\" label=\""<<iProxy->providerDescription()->label_<<"\"\n"
+         <<"Please either\n   remove one of these "<<(proxy->providerDescription()->isSource_?"Sources":"Producers")
+         <<"\n   or find a way of configuring one of them so it does not deliver this data";
+      } else if(proxy->providerDescription()->isSource_) {
+         (*proxies_.find(iKey)).second = iProxy ;
+      } else {
+         return false;
+      }
    }
    else {
       proxies_.insert(Proxies::value_type(iKey , iProxy)) ;
