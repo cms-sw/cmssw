f920fdea9a3765a8af7404d04aac6278e7a7d246
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2005-12-19 21:29:21 +0000

    DD copy number <->  DetID conversion from Chia

--- SimG4CMS/Calo/interface/ShowerForwardNumberingScheme.h
+++ SimG4CMS/Calo/interface/ShowerForwardNumberingScheme.h
@@ -10,9 +10,18 @@
 class ShowerForwardNumberingScheme : public EcalNumberingScheme {
 
 public:
+
   ShowerForwardNumberingScheme(int);
   ~ShowerForwardNumberingScheme();
   virtual uint32_t getUnitID(const G4Step* aStep) const;
+  void findXY(const int& layer, const int& waf, int& x, int& y) const;
+
+private:
+
+  int iquad_max[40];
+  int iquad_min[40];
+  int Ntot[40];
+  int Ncols[40];
 
 };
 
--- SimG4CMS/Calo/src/ShowerForwardNumberingScheme.cc
+++ SimG4CMS/Calo/src/ShowerForwardNumberingScheme.cc
@@ -9,6 +9,22 @@ using namespace std;
 
 ShowerForwardNumberingScheme::ShowerForwardNumberingScheme(int iv) : 
   EcalNumberingScheme(iv) {
+
+  int iquad_max[40]={5,7,10,11,13,13,14,15,16,17,17,17,18,
+		     19,19,19,19,19,19,19,19,19,19,19,19,19,19,
+		     18,17,17,17,16,15,
+		     14,13,13,11,10,7,5};
+  int iquad_min[40]={1,1,1,1,1,1,1,1,1,1,1,1,1,
+		     4,4,6,6,8,8,8,8,8,8,6,6,4,4,
+		     1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+  Ncols[0] = 2*(iquad_max[0]-iquad_min[0]+1);
+  Ntot[0] = Ncols[0];
+  for (int i=1; i<40; i++) {
+    Ncols[i] = iquad_max[i]-iquad_min[i]+1;
+    Ntot[i] = Ntot[i-1]+2*Ncols[i];
+  }
+
   if (verbosity>0) 
     std::cout << "Creating ShowerForwardNumberingScheme" << std::endl;
 }
@@ -43,10 +59,12 @@ uint32_t ShowerForwardNumberingScheme::getUnitID(const G4Step* aStep) const {
     zside=2*(1-zside)+1;
     // wafer number
     int wafer = copyno[level-4];
+    int x,y;
+    findXY(layer, wafer, x, y);
     // strip number inside wafer
-    int strip = copyno[level-1];
+    int strip = copyno[level-1]-1;
 
-    intIndex =  ESDetId(1,1,1,1,1).rawId(); //Fake for the moment
+    intIndex =  ESDetId(strip, x, y, layer, zside).rawId(); 
 
     if (verbosity>1) {
       std::cout << "ShowerForwardNumberingScheme : zside " 
@@ -63,5 +81,30 @@ uint32_t ShowerForwardNumberingScheme::getUnitID(const G4Step* aStep) const {
     delete[] name;
   }
   return intIndex;
+
+}
+
+void ShowerForwardNumberingScheme::findXY(const int& layer, const int& waf, int& x, int& y)  const {
+
+  int i,ix,iy;
+
+  y = 0;
+  for (i=0; i<40; i++) {
+    if (waf > Ntot[i]) y = i + 1;
+  }
+  x = 19 - iquad_max[y];
+  x = (y==0) ? (x + waf) : (x + waf - Ntot[y-1]);
+
+  int iq = iquad_min[y];
+  if (iq != 1 && x > (Ncols[y]+19-iquad_max[y])) {
+    x = x + (iq-1)*2;
+  }
+
+  if (layer==2) {
+    ix = 39 - x;
+    iy = 39 - y;
+    x = iy;
+    y = ix;
+  }
 
 }
