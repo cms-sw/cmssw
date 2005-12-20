d6692fd1baf28767d124c441638b12950eb6af3d
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2005-12-20 15:43:11 +0000

    RPCDetId

--- DataFormats/MuonDetId/src/classes.h
+++ DataFormats/MuonDetId/src/classes.h
@@ -3,3 +3,4 @@
 #include "DataFormats/MuonDetId/interface/DTLayerId.h"
 #include "DataFormats/MuonDetId/interface/DTWireId.h"
 #include "DataFormats/MuonDetId/interface/CSCDetId.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
