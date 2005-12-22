967ba2b613e148a917bddc8e164f5557cafdb4a2
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-12-22 22:31:59 +0000

    Remove logs

--- RecoTBCalo/HcalTBTools/src/HcalTBTriggerFilter.cc
+++ RecoTBCalo/HcalTBTools/src/HcalTBTriggerFilter.cc
@@ -19,29 +19,29 @@ bool HcalTBTriggerFilter::filter(edm::Event const& e, edm::EventSetup const& c) 
 
   if (h->wasBeamTrigger()) {
     allowed=allowBeam_;
-    if (!allowed) std::cout << "Rejecting beam trigger from " << e.id() << std::endl;
+    //    if (!allowed) std::cout << "Rejecting beam trigger from " << e.id() << std::endl;
   }
 
   if (h->wasLEDTrigger()) {
     allowed=allowLED_;
-    if (!allowed) std::cout << "Rejecting LED trigger from " << e.id() << std::endl;
+    //    if (!allowed) std::cout << "Rejecting LED trigger from " << e.id() << std::endl;
   }
 
   if (h->wasLaserTrigger()) {
     allowed=allowLaser_;
-    if (!allowed) std::cout << "Rejecting laser trigger from " << e.id() << std::endl;
+    //    if (!allowed) std::cout << "Rejecting laser trigger from " << e.id() << std::endl;
   }
 
   if (h->wasInSpillPedestalTrigger() || h->wasOutSpillPedestalTrigger() || h->wasSpillIgnorantPedestalTrigger()) {
     allowed=allowPedestal_;
-    if (!allowed) std::cout << "Rejecting pedestal trigger from " << e.id() << std::endl;
+    //    if (!allowed) std::cout << "Rejecting pedestal trigger from " << e.id() << std::endl;
     if (allowed && h->wasInSpillPedestalTrigger() && !allowPedestalInSpill_) {
       allowed=false;
-      if (!allowed) std::cout << "Rejecting in-spill pedestal trigger from " << e.id() << std::endl;
+      //      if (!allowed) std::cout << "Rejecting in-spill pedestal trigger from " << e.id() << std::endl;
     }
     if (allowed && h->wasOutSpillPedestalTrigger() && !allowPedestalOutSpill_) {
       allowed=false;
-      if (!allowed) std::cout << "Rejecting out-of-spill pedestal trigger from " << e.id() << std::endl;
+      //      if (!allowed) std::cout << "Rejecting out-of-spill pedestal trigger from " << e.id() << std::endl;
     }
   }
 
