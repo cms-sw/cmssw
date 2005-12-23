4f4340cbdaf7ffa18a1dea10d4e1b4ffc5030c69
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-12-23 00:44:27 +0000

    Proper path for running rests under scram

--- FWCore/Framework/test/run_es_refer_tests.sh
+++ FWCore/Framework/test/run_es_refer_tests.sh
@@ -3,9 +3,7 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-(cmsRun test_es_prefer_prods.cfg ) || die 'Failure using test_es_prefer_prods.cfg' $?
-(cmsRun test_es_prefer_sources.cfg ) || die 'Failure using test_es_prefer_sources.cfg' $?
-(cmsRun test_es_prefer_source_beats_prod.cfg ) || die 'Failure using test_es_prefer_source_beats_prod.cfg' $?
-(cmsRun test_prod_trumps_source.cfg ) || die 'Failure using test_prod_trumps_source.cfg' $?
-
-
+(cmsRun ${LOCAL_TEST_DIR}/test_es_prefer_prods.cfg ) || die 'Failure using test_es_prefer_prods.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_es_prefer_sources.cfg ) || die 'Failure using test_es_prefer_sources.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_es_prefer_source_beats_prod.cfg ) || die 'Failure using test_es_prefer_source_beats_prod.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_prod_trumps_source.cfg ) || die 'Failure using test_prod_trumps_source.cfg' $?
