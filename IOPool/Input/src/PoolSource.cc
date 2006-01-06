98552fd65d0cbd4ab9433f0294ea556b11529fb3
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-01-06 02:38:07 +0000

    Move some code to new IOPool/CommonInput package for sharing with other input sources

--- IOPool/Input/src/PoolSource.cc
+++ IOPool/Input/src/PoolSource.cc
@@ -1,30 +1,16 @@
 /*----------------------------------------------------------------------
-$Id: PoolSource.cc,v 1.12 2005/12/01 22:35:23 wmtan Exp $
+$Id: PoolSource.cc,v 1.13 2005/12/02 22:40:21 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include "IOPool/Input/src/PoolSource.h"
-#include "IOPool/Common/interface/PoolNames.h"
+#include "IOPool/CommonInput/interface/RootFile.h"
 #include "IOPool/Common/interface/ClassFiller.h"
-#include "IOPool/Common/interface/RefStreamer.h"
 
-#include "FWCore/EDProduct/interface/EDProduct.h"
-#include "FWCore/Framework/interface/BranchKey.h"
-#include "FWCore/Framework/interface/EventAux.h"
+#include "FWCore/Framework/interface/BranchDescription.h"
 #include "FWCore/Framework/interface/EventPrincipal.h"
-#include "FWCore/Framework/interface/EventProvenance.h"
 #include "FWCore/Framework/interface/ProductRegistry.h"
+#include "FWCore/EDProduct/interface/ProductID.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
-#include "TFile.h"
-#include "TTree.h"
-#include "TBranch.h"
-
-#include <stdexcept>
-
-using std::auto_ptr;
-
-#include <iostream>
 
 namespace edm {
   PoolRASource::PoolRASource(ParameterSet const& pset, InputSourceDescription const& desc) :
@@ -35,9 +21,8 @@ namespace edm {
     file_(pset.getUntrackedParameter("fileName", std::string())),
     files_(pset.getUntrackedParameter("fileNames", std::vector<std::string>())),
     fileIter_(files_.begin()),
-    poolFile_(),
-    remainingEvents_(pset.getUntrackedParameter<int>("maxEvents", -1)),
-    eventID_() {
+    rootFile_(),
+    remainingEvents_(pset.getUntrackedParameter<int>("maxEvents", -1)) {
     ClassFiller();
     if (file_.empty()) {
       if (files_.empty()) { // this will throw;
@@ -56,38 +41,38 @@ namespace edm {
     std::string pfn;
     catalog_.findFile(pfn, file);
 
-    poolFile_ = boost::shared_ptr<PoolFile>(new PoolFile(pfn));
-    if (poolFile_->productRegistry().nextID() > preg_->nextID()) {
-      preg_->setNextID(poolFile_->productRegistry().nextID());
+    rootFile_ = boost::shared_ptr<RootFile>(new RootFile(pfn));
+    if (rootFile_->productRegistry().nextID() > productRegistry().nextID()) {
+      productRegistry().setNextID(rootFile_->productRegistry().nextID());
     }
-    ProductRegistry::ProductList const& prodList = poolFile_->productRegistry().productList();
+    ProductRegistry::ProductList const& prodList = rootFile_->productRegistry().productList();
     for (ProductRegistry::ProductList::const_iterator it = prodList.begin();
         it != prodList.end(); ++it) {
-      preg_->copyProduct(it->second);
+      productRegistry().copyProduct(it->second);
     }
 
-    for (ProductRegistry::ProductList::const_iterator it = preg_->productList().begin();
-         it != preg_->productList().end(); ++it) {
+    for (ProductRegistry::ProductList::const_iterator it = productRegistry().productList().begin();
+         it != productRegistry().productList().end(); ++it) {
       productMap_.insert(std::make_pair(it->second.productID_, it->second));
     }
   }
 
   bool PoolRASource::next() {
-    if(poolFile_->next()) return true;
+    if(rootFile_->next()) return true;
     if(fileIter_ == files_.end()) return false;
 
     // save the product registry from the current file, temporarily
-    boost::shared_ptr<ProductRegistry const> pReg(poolFile_->productRegistrySharedPtr());
+    boost::shared_ptr<ProductRegistry const> pReg(rootFile_->productRegistrySharedPtr());
 
-    // delete the old PoolFile.  The file will be closed.
-    poolFile_.reset();
+    // delete the old RootFile.  The file will be closed.
+    rootFile_.reset();
     
     std::string pfn;
     catalog_.findFile(pfn, *fileIter_);
 
-    poolFile_ = boost::shared_ptr<PoolFile>(new PoolFile(pfn));
+    rootFile_ = boost::shared_ptr<RootFile>(new RootFile(pfn));
     // make sure the new product registry is identical to the old one
-    if (*pReg != poolFile_->productRegistry()) {
+    if (*pReg != rootFile_->productRegistry()) {
       throw cms::Exception("MismatchedInput","PoolSource::next()")
         << "File " << *fileIter_ << "\nhas different product registry than previous files\n";
     }
@@ -111,135 +96,31 @@ namespace edm {
   // the branch containing this EDProduct. That will be done by the Delayed Reader,
   //  when it is asked to do so.
   //
-  auto_ptr<EventPrincipal>
+  std::auto_ptr<EventPrincipal>
   PoolRASource::read() {
     // If we're done, or out of range, return a null auto_ptr
     if (remainingEvents_ == 0) {
-      return auto_ptr<EventPrincipal>(0);
+      return std::auto_ptr<EventPrincipal>(0);
     }
     if (!next()) {
-      return auto_ptr<EventPrincipal>(0);
+      return std::auto_ptr<EventPrincipal>(0);
     }
     --remainingEvents_;
-    EventAux evAux;
-    EventProvenance evProv;
-    EventAux *pEvAux = &evAux;
-    EventProvenance *pEvProv = &evProv;
-    poolFile_->auxBranch()->SetAddress(&pEvAux);
-    poolFile_->provBranch()->SetAddress(&pEvProv);
-    poolFile_->auxBranch()->GetEntry(poolFile_->entryNumber());
-    poolFile_->provBranch()->GetEntry(poolFile_->entryNumber());
-    eventID_ = evAux.id_;
-    // We're not done ... so prepare the EventPrincipal
-    boost::shared_ptr<DelayedReader> store_(new PoolDelayedReader(poolFile_->entryNumber(), *this));
-    auto_ptr<EventPrincipal> thisEvent(new EventPrincipal(evAux.id_, evAux.time_, *preg_, evAux.process_history_, store_));
-    // Loop over provenance
-    std::vector<BranchEntryDescription>::iterator pit = evProv.data_.begin();
-    std::vector<BranchEntryDescription>::iterator pitEnd = evProv.data_.end();
-    for (; pit != pitEnd; ++pit) {
-      if (pit->status != BranchEntryDescription::Success) continue;
-      // BEGIN These lines read all branches
-      // TBranch *br = branches_.find(poolNames::keyName(*pit))->second;
-      // br->SetAddress(p);
-      // br->GetEntry(poolFile_->entryNumber());
-      // auto_ptr<Provenance> prov(new Provenance(*pit));
-      // prov->product = productMap_[prov.event.productID_];
-      // auto_ptr<Group> g(new Group(auto_ptr<EDProduct>(p), prov));
-      // END These lines read all branches
-      // BEGIN These lines defer reading branches
-      auto_ptr<Provenance> prov(new Provenance);
-      prov->event = *pit;
-      prov->product = productMap_[prov->event.productID_];
-      auto_ptr<Group> g(new Group(prov));
-      // END These lines defer reading branches
-      thisEvent->addGroup(g);
-    }
-
-    return thisEvent;
+    return rootFile_->read(productRegistry(), productMap_); 
   }
 
-  auto_ptr<EventPrincipal>
+  std::auto_ptr<EventPrincipal>
   PoolRASource::read(EventID const& id) {
     // For now, don't support multiple runs.
-    assert (id.run() == eventID_.run());
+    assert (id.run() == rootFile_->eventID().run());
     // For now, assume EventID's are all there.
-    EntryNumber offset = static_cast<long>(id.event()) - static_cast<long>(eventID_.event());
-    poolFile_->entryNumber() += offset;
+    RootDelayedReader::EntryNumber offset = static_cast<long>(id.event()) - static_cast<long>(rootFile_->eventID().event());
+    rootFile_->entryNumber() += offset;
     return read();
   }
 
   void
   PoolRASource::skip(int offset) {
-    poolFile_->entryNumber() += offset;
-  }
-
-//---------------------------------------------------------------------
-  PoolRASource::PoolFile::PoolFile(std::string const& fileName) :
-    file_(fileName),
-    entryNumber_(-1),
-    entries_(0),
-    productRegistry_(new ProductRegistry),
-    branches_(),
-    auxBranch_(0),
-    provBranch_(0),
-    filePtr_(0) {
-
-    LogInfo("FwkJob") << "Input file " << file_ << " is about to be opened.";
-    filePtr_ = TFile::Open(file_.c_str());
-    if (filePtr_ == 0) {
-      throw cms::Exception("FileNotFound","PoolRASource::PoolFile::PoolFile()")
-        << "File " << file_ << " was not found.\n";
-    }
-    LogInfo("FwkJob") << "Input file " << file_ << " has been opened successfully.";
-
-    TTree *metaDataTree = dynamic_cast<TTree *>(filePtr_->Get(poolNames::metaDataTreeName().c_str()));
-    assert(metaDataTree != 0);
-
-    // Load streamers for product dictionary and member/base classes.
-    ProductRegistry *ppReg = productRegistry_.get();
-    metaDataTree->SetBranchAddress(poolNames::productDescriptionBranchName().c_str(),(&ppReg));
-    metaDataTree->GetEntry(0);
-
-    TTree *eventTree = dynamic_cast<TTree *>(filePtr_->Get(poolNames::eventTreeName().c_str()));
-    assert(eventTree != 0);
-    entries_ = eventTree->GetEntries();
-
-    auxBranch_ = eventTree->GetBranch(poolNames::auxiliaryBranchName().c_str());
-    provBranch_ = eventTree->GetBranch(poolNames::provenanceBranchName().c_str());
-
-    std::string const wrapperBegin("edm::Wrapper<");
-    std::string const wrapperEnd1(">");
-    std::string const wrapperEnd2(" >");
-
-    ProductRegistry::ProductList const& prodList = productRegistry().productList();
-    for (ProductRegistry::ProductList::const_iterator it = prodList.begin();
-        it != prodList.end(); ++it) {
-      BranchDescription const& prod = it->second;
-      prod.init();
-      TBranch * branch = eventTree->GetBranch(prod.branchName_.c_str());
-      std::string const& name = prod.fullClassName_;
-      std::string const& wrapperEnd = (name[name.size()-1] == '>' ? wrapperEnd2 : wrapperEnd1);
-      std::string const className = wrapperBegin + name + wrapperEnd;
-      branches_.insert(std::make_pair(it->first, std::make_pair(className, branch)));
-    }
-
-  }
-
-  PoolRASource::PoolFile::~PoolFile() {
-    filePtr_->Close();
-  }
-
-  PoolRASource::PoolDelayedReader::~PoolDelayedReader() {}
-
-  auto_ptr<EDProduct>
-  PoolRASource::PoolDelayedReader::get(BranchKey const& k, EventPrincipal const* ep) const {
-    SetRefStreamer(ep);
-    TBranch *br = branches().find(k)->second.second;
-    TClass *cp = gROOT->GetClass(branches().find(k)->second.first.c_str());
-    auto_ptr<EDProduct> p(static_cast<EDProduct *>(cp->New()));
-    EDProduct *pp = p.get();
-    br->SetAddress(&pp);
-    br->GetEntry(entryNumber_);
-    return p;
+    rootFile_->entryNumber() += offset;
   }
 }
--- IOPool/Input/src/PoolSource.h
+++ IOPool/Input/src/PoolSource.h
@@ -5,40 +5,27 @@
 
 PoolSource: This is an InputSource
 
-$Id: PoolSource.h,v 1.8 2005/11/01 23:24:13 wmtan Exp $
+$Id: PoolSource.h,v 1.9 2005/12/01 22:35:23 wmtan Exp $
 
 ----------------------------------------------------------------------*/
 
 #include <memory>
-#include <map>
 #include <vector>
 #include <string>
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/ProductRegistry.h"
-#include "FWCore/Framework/interface/RandomAccessInputSource.h"
-#include "FWCore/Framework/interface/DelayedReader.h"
-#include "FWCore/EDProduct/interface/EDProduct.h"
-#include "FWCore/Framework/interface/BranchKey.h"
-#include "FWCore/Framework/interface/EventProvenance.h"
-#include "FWCore/Framework/interface/EventAux.h"
 #include "IOPool/Common/interface/PoolCatalog.h"
-#include "TBranch.h"
+#include "IOPool/CommonInput/interface/RootDelayedReader.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/RandomAccessInputSource.h"
 
 #include "boost/shared_ptr.hpp"
 
-// forwards
-namespace seal { class Status; }
-
 namespace edm {
 
-  class ParameterSet;
+  class RootFile;
   class PoolRASource : public RandomAccessInputSource {
   public:
-    typedef Long64_t EntryNumber;
-    class PoolFile;
-    class PoolDelayedReader;
-
     explicit PoolRASource(ParameterSet const& pset, InputSourceDescription const& desc);
     virtual ~PoolRASource();
 
@@ -52,65 +39,12 @@ namespace edm {
     bool next();
 
     PoolCatalog catalog_;
-    std::map<ProductID, BranchDescription> productMap_;
+    RootDelayedReader::ProductMap productMap_;
     std::string const file_;
     std::vector<std::string> const files_;
     std::vector<std::string>::const_iterator fileIter_;
-    boost::shared_ptr<PoolFile> poolFile_;
-    EntryNumber remainingEvents_;
-    EventID eventID_;
+    boost::shared_ptr<RootFile> rootFile_;
+    RootDelayedReader::EntryNumber remainingEvents_;
   }; // class PoolRASource
-
-  //------------------------------------------------------------
-  // Nested class PoolFile: supports file reading.
-
-  class PoolRASource::PoolFile {
-  public:
-    typedef std::map<BranchKey, std::pair<std::string, TBranch *> > BranchMap;
-    BranchMap const& branches() const {return branches_;}
-    explicit PoolFile(std::string const& fileName);
-    ~PoolFile();
-    bool next() {return ++entryNumber_ < entries_;} 
-    ProductRegistry const& productRegistry() const {return *productRegistry_;}
-    boost::shared_ptr<ProductRegistry> productRegistrySharedPtr() const {return productRegistry_;}
-    TBranch *auxBranch() {return auxBranch_;}
-    TBranch *provBranch() {return provBranch_;}
-    EntryNumber & entryNumber() {return entryNumber_;}
-
-  private:
-    PoolFile(PoolFile const&); // disable copy construction
-    PoolFile & operator=(PoolFile const&); // disable assignment
-    std::string const file_;
-    EntryNumber entryNumber_;
-    EntryNumber entries_;
-    boost::shared_ptr<ProductRegistry> productRegistry_;
-// We use bare pointers for pointers to ROOT entities.
-// Root owns them and uses bare pointers internally.
-// Therefore,using shared pointers here will do no good.
-    BranchMap branches_;
-    TBranch *auxBranch_;
-    TBranch *provBranch_;
-    TFile *filePtr_;
-  }; // class PoolRASource::PoolFile
-
-
-  //------------------------------------------------------------
-  // Nested class PoolDelayedReader: pretends to support file reading.
-  //
-
-  class PoolRASource::PoolDelayedReader : public DelayedReader {
-  public:
-    PoolDelayedReader(EntryNumber const& entry, PoolRASource const& serv) : entryNumber_(entry), inputSource(serv) {}
-    virtual ~PoolDelayedReader();
-  private:
-    PoolDelayedReader(PoolDelayedReader const&); // disable copy construction
-    PoolDelayedReader & operator=(PoolDelayedReader const&); // disable assignment
-    virtual std::auto_ptr<EDProduct> get(BranchKey const& k, EventPrincipal const* ep) const;
-    PoolFile::BranchMap const& branches() const {return inputSource.poolFile_->branches();}
-    EntryNumber const entryNumber_;
-    PoolRASource const& inputSource;
-  }; // class PoolRASource::PoolDelayedReader
-  //------------------------------------------------------------
-
 }
 #endif
