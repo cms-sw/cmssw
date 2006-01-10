19c7bf8d4662f7b79c1f7eaedcfd33b467d60e3b
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-01-10 17:21:55 +0000

    test GenericHandle class

---
+++ FWCore/Framework/test/generichandle_t.cppunit.cc
@@ -0,0 +1,147 @@
+/*----------------------------------------------------------------------
+
+Test of the EventPrincipal class.
+
+$Id: eventprincipal_t.cppunit.cc,v 1.18 2005/11/02 06:45:55 wmtan Exp $
+
+----------------------------------------------------------------------*/  
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <typeinfo>
+
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "FWCore/EDProduct/interface/ProductID.h"
+#include "FWCore/Framework/interface/BasicHandle.h"
+#include "FWCore/Framework/interface/ProductRegistry.h"
+#include "FWCore/Framework/interface/BranchDescription.h"
+#include "FWCore/EDProduct/interface/Timestamp.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+#include "FWCore/Framework/interface/Selector.h"
+#include "FWCore/Framework/src/TypeID.h"
+#include "FWCore/Framework/src/ToyProducts.h"
+
+#include "FWCore/Framework/interface/EventPrincipal.h"
+
+#include "FWCore/Framework/interface/GenericHandle.h"
+#include <cppunit/extensions/HelperMacros.h>
+
+class testGenericHandle: public CppUnit::TestFixture {
+CPPUNIT_TEST_SUITE(testGenericHandle);
+CPPUNIT_TEST(failgetbyLabelTest);
+CPPUNIT_TEST(getbyLabelTest);
+CPPUNIT_TEST(failWrongType);
+CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp(){}
+  void tearDown(){}
+  void failgetbyLabelTest();
+  void failWrongType();
+  void getbyLabelTest();
+};
+
+///registration of the test so that the runner can find it
+CPPUNIT_TEST_SUITE_REGISTRATION(testGenericHandle);
+
+void testGenericHandle::failWrongType() {
+   try {
+      //intentionally mispelled type
+      edm::GenericHandle h("edmtest::DmmyProduct");
+      CPPUNIT_ASSERT("Failed to thow"==0);
+   }
+   catch (edm::Exception& x) {
+      // nothing to do
+   }
+   catch (...) {
+      CPPUNIT_ASSERT("Threw wrong kind of exception" == 0);
+   }
+}
+void testGenericHandle::failgetbyLabelTest() {
+
+  edm::EventPrincipal ep;
+  ep.addToProcessHistory("PROD");
+  edm::GenericHandle h("edmtest::DummyProduct");
+  try {
+     edm::ModuleDescription modDesc;
+     modDesc.moduleName_="Blah";
+     modDesc.moduleLabel_="blahs"; 
+     edm::Event event(ep, modDesc);
+     
+     std::string label("this does not exist");
+     event.getByLabel(label,h);
+    CPPUNIT_ASSERT("Failed to throw required exception" == 0);      
+  }
+  catch (edm::Exception& x) {
+    // nothing to do
+  }
+  catch (...) {
+    CPPUNIT_ASSERT("Threw wrong kind of exception" == 0);
+  }
+ 
+}
+
+void testGenericHandle::getbyLabelTest() {
+  std::string processName = "PROD";
+
+  typedef edmtest::DummyProduct DP;
+  typedef edm::Wrapper<DP> WDP;
+  std::auto_ptr<DP> pr(new DP);
+  std::auto_ptr<edm::EDProduct> pprod(new WDP(pr));
+  std::auto_ptr<edm::Provenance> pprov(new edm::Provenance);
+  std::string label("fred");
+  std::string productInstanceName("Rick");
+
+  edmtest::DummyProduct dp;
+  edm::TypeID dummytype(dp);
+  std::string className = dummytype.friendlyClassName();
+
+  pprov->product.fullClassName_ = dummytype.userClassName();
+  pprov->product.friendlyClassName_ = className;
+
+
+  pprov->product.module.moduleLabel_ = label;
+  pprov->product.productInstanceName_ = productInstanceName;
+  pprov->product.module.processName_ = processName;
+  pprov->product.init();
+
+  edm::ProductRegistry preg;
+  preg.addProduct(pprov->product);
+  preg.setProductIDs();
+  edm::EventID col(1L);
+  edm::Timestamp fakeTime;
+  edm::EventPrincipal ep(col, fakeTime, preg);
+  ep.addToProcessHistory("PROD");
+
+  ep.put(pprod, pprov);
+  
+  edm::GenericHandle h("edmtest::DummyProduct");
+  try {
+    edm::ModuleDescription modDesc;
+    modDesc.moduleName_="Blah";
+    modDesc.moduleLabel_="blahs"; 
+    edm::Event event(ep, modDesc);
+
+    event.getByLabel(label, productInstanceName,h);
+  }
+  catch (cms::Exception& x) {
+    std::cerr << x.what()<< std::endl;
+    CPPUNIT_ASSERT("Threw exception unexpectedly" == 0);
+  }
+  catch(seal::Error& x){
+     std::cerr <<x.explainSelf()<<std::endl;
+     CPPUNIT_ASSERT("Threw seal Error"==0);
+  }
+  catch(std::exception& x){
+     std::cerr <<x.what()<<std::endl;
+     CPPUNIT_ASSERT("threw std::exception"==0);
+  }
+  catch (...) {
+    std::cerr << "Unknown exception type\n";
+    CPPUNIT_ASSERT("Threw exception unexpectedly" == 0);
+  }
+  CPPUNIT_ASSERT(h.isValid());
+  CPPUNIT_ASSERT(h.provenance()->product.module.moduleLabel_ == label);
+}
+
