d18e664a33179b2cb9da98cef76163b590e56458
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2006-01-10 09:29:06 +0000

    moved hit classes common to tracking detectors to TrackingRecHit

---
+++ DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h
@@ -0,0 +1,45 @@
+#ifndef RecHit2DLocalPos_H
+#define RecHit2DLocalPos_H
+
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
+#include "Geometry/Surface/interface/LocalError.h"
+
+class RecHit2DLocalPos : public TrackingRecHit {
+public:
+
+  typedef Surface::LocalPoint LocalPoint;
+  
+  virtual ~RecHit2DLocalPos() {}
+  
+  virtual RecHit2DLocalPos * clone() const = 0;
+  
+  virtual AlgebraicVector parameters() const;
+
+  virtual AlgebraicSymMatrix parametersError() const;
+
+  virtual AlgebraicMatrix projectionMatrix() const {
+    if ( !isInitialized) initialize();
+    return theProjectionMatrix;
+  }
+
+  virtual int dimension() const { return 2;}
+
+  virtual LocalPoint localPosition() const = 0;
+
+  virtual LocalError localPositionError() const = 0;
+
+  virtual std::vector<const TrackingRecHit*> recHits() const;
+
+  virtual std::vector<TrackingRecHit*> recHits();
+
+private:
+
+  static bool isInitialized;
+
+  static AlgebraicMatrix theProjectionMatrix;
+
+  void initialize() const;
+
+};
+
+#endif
