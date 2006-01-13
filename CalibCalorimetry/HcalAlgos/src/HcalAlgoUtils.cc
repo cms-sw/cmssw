2466a90d450b54e5f0087bce81e6172ab540f794
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2006-01-13 20:10:46 +0000

    moved HcalAlgoUtils to new structure

--- CalibCalorimetry/HcalAlgos/src/HcalAlgoUtils.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalAlgoUtils.cc
@@ -1,8 +1,4 @@
-#ifndef DQM_HCALALGOUTILS_H
-#define DQM_HCALALGOUTILS_H
-
-#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
-#include "CondFormats/HcalObjects/interface/HcalQIECoder.h"
+#include "CalibCalorimetry/HcalAlgos/interface/HcalAlgoUtils.h"
 
 void getLinearizedADC(const HcalQIEShape& shape,
 		      const HcalQIECoder* coder,
@@ -12,7 +8,6 @@ void getLinearizedADC(const HcalQIEShape& shape,
   
   float low = coder->charge(shape,0,capid); 
   float high = coder->charge(shape,bins-1,capid);
-  //  printf("Coder: lo: %f, hi: %f, bins: %d, capid: %d\n",low,high,bins,capid);
   float step = (high-low)/(bins-1);
   low -= step/2.0; high += step/2.0;
   lo = low; hi = high;
@@ -27,5 +22,3 @@ float maxDiff(float one, float two, float three, float four){
   if(four>max) max = four;    if(four<min) min = four;
   return fabs(max-min);
 }
-
-#endif
