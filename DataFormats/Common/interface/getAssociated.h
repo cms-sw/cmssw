f25f87583289eaecdb99eeece96372aec0d870d3
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-01-16 11:32:06 +0000

    moved ref_collection to more general ext_collection

--- DataFormats/Common/interface/getAssociated.h
+++ DataFormats/Common/interface/getAssociated.h
@@ -1,18 +1,16 @@
 #ifndef Common_getAssociated_h
 #define Common_getAssociated_h
-#include "DataFormats/Common/interface/ref_collection.h"
+#include "DataFormats/Common/interface/ext_collection.h"
+#include "FWCore/EDProduct/interface/Ref.h"
+#include "FWCore/EDProduct/interface/RefProd.h"
 
-template<typename C, typename R>
-const typename R::value_type & getAssociated( const  edm::Ref<ref_collection<C, R> > & ref ) {
-  // the following could be a method of edm::Ref
-  const edm::EDProduct * edp = ref.productGetter()->getIt( ref.id() );
-  const edm::Wrapper<ref_collection<C, R> > * w = 
-    dynamic_cast<const edm::Wrapper<ref_collection<C, R> > *>( edp );
-  const ref_collection<C, R> * coll = w->product();
-  // 
-  edm::RefProd<R> assocCollRef = coll->ref();
-  const typename R::value_type & ret = (*assocCollRef)[ ref.index() ];
-  return ret;
-}
+template<typename C, typename T = typename C::value_type>
+struct Associated {
+  template<typename CExt, typename Ext>
+  static const T & get( const edm::Ref<ext_collection<CExt, Ext> > & ref, 
+				 const edm::RefProd<C> & ( Ext:: * getRef )() const ) {
+    return ( * (ref.product()->ext().* getRef)() )[ ref.index() ];
+  }
+};
 
 #endif
