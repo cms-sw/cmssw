455afc30c5e863a5460f882dca358eca0ccf1668
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-01-19 20:32:27 +0000

    added comments to try to make example clearer

--- FWCore/Skeletons/perl/EDProducer/edproducer.cc
+++ FWCore/Skeletons/perl/EDProducer/edproducer.cc
@@ -90,14 +90,18 @@ prodname::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
+   //Read 'ExampleData' from the Event
    Handle<ExampleData> pIn;
    iEvent.getByLabel("example",pIn);
 
-   std::auto_ptr<ExampleData2> pOut(new ExampleData2());
+   //Use the ExampleData to create an ExampleData2 which 
+   // is put into the Event
+   std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
    iEvent.put(pOut);
 #endif
 
 #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   //Read SetupData from the SetupRecord in the EventSetup
    ESHandle<SetupData> pSetup;
    iSetup.get<SetupRecord>().get(pSetup);
 #endif
