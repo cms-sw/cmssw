9fe244d4d8333a15087c576b2e928d353c750b17
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-01-20 21:36:20 +0000

    test to see if same product key is registered multiple times

--- FWCore/Framework/src/ESProxyFactoryProducer.cc
+++ FWCore/Framework/src/ESProxyFactoryProducer.cc
@@ -8,17 +8,18 @@
 //
 // Author:      Chris Jones
 // Created:     Thu Apr  7 21:36:15 CDT 2005
-// $Id: ESProxyFactoryProducer.cc,v 1.6 2005/09/29 21:06:47 chrjones Exp $
+// $Id: ESProxyFactoryProducer.cc,v 1.1 2005/10/03 23:07:34 chrjones Exp $
 //
 
 // system include files
-
+#include "boost/bind.hpp"
 // user include files
 #include "FWCore/Framework/interface/ESProxyFactoryProducer.h"
 #include "FWCore/Framework/interface/ProxyFactoryBase.h"
 
 #include "FWCore/Framework/interface/DataProxy.h"
 
+#include "FWCore/Utilities/interface/Exception.h"
 //
 // constants, enums and typedefs
 //
@@ -94,6 +95,20 @@ ESProxyFactoryProducer::registerFactoryWithKey(const EventSetupRecordKey& iRecor
    FactoryInfo info(temp->makeKey(iLabel),
                     temp);
    
+   //has this already been registered?
+   std::pair<Record2Factories::const_iterator,Record2Factories::const_iterator> range =
+      record2Factories_.equal_range(iRecord);
+   if(range.second != std::find_if(range.first,range.second,
+                                   boost::bind(std::equal_to<DataKey>(),
+                                               boost::bind(&FactoryInfo::key_,
+                                                           boost::bind(&Record2Factories::value_type::second,
+                                                                       _1)),
+                                               info.key_)
+                                   ) ) {
+      throw cms::Exception("IdenticalProducts")<<"Producer has been asked to produce "<<info.key_.type().name()
+      <<" \""<<info.key_.name().value()<<"\" multiple times.\n Please modify the code.";
+   }
+                                               
    record2Factories_.insert(Record2Factories::value_type(iRecord,
                                                          info));
 }
