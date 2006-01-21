288e1fc9a052f616be822f6e9986f0737d7dce3e
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2006-01-21 01:32:19 +0000

    Add XML definition for Electronics Map

--- CalibCalorimetry/HcalAlgos/interface/HcalDbXml.h
+++ CalibCalorimetry/HcalAlgos/interface/HcalDbXml.h
@@ -12,7 +12,7 @@
 /**
    \brief IO for XML instances of Hcal Calibrations
    \author Fedor Ratnikov Oct. 28, 2005
-   $Id: HcalDbXml.h,v 1.1 2005/11/02 21:31:24 fedor Exp $
+   $Id: HcalDbXml.h,v 1.2 2006/01/19 01:37:12 fedor Exp $
    
 */
 namespace HcalDbXml {
@@ -20,5 +20,6 @@ namespace HcalDbXml {
   bool dumpObject (std::ostream& fOutput, unsigned fRun, const std::string& fTag, const HcalPedestals& fObject);
   bool dumpObject (std::ostream& fOutput, unsigned fRun, const std::string& fTag, const HcalGains& fObject, const HcalGainWidths& fError);
   bool dumpObject (std::ostream& fOutput, unsigned fRun, const std::string& fTag, const HcalGains& fObject);
+  bool dumpObject (std::ostream& fOutput, unsigned fRun, const std::string& fTag, const HcalElectronicsMap& fObject) {return false;}
 } 
 #endif
