3df39664f1fba179081f68d6453b0a9c7c5e92d8
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-01-23 18:32:23 +0000

    added the Reader

--- SimMuon/DTDigitizer/src/SealModule.cc
+++ SimMuon/DTDigitizer/src/SealModule.cc
@@ -3,11 +3,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "SimMuon/DTDigitizer/src/DTDigitizer.h"
-
+#include "SimMuon/DTDigitizer/src/DTDigiReader.cc"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTDigitizer)
-
+DEFINE_ANOTHER_FWK_MODULE(DTDigiReader)
 
 #include "SimMuon/DTDigitizer/src/DTDigiSyncFromTable.h"
 #include "SimMuon/DTDigitizer/src/DTDigiSyncTOFCorr.h"
