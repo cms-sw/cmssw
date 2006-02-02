52a4c4d15a78d7f4968e3cb3347132577179287a
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-02-02 15:48:12 +0000

    quality test names to put in the cfg file

---
+++ DQM/RPCMonitorClient/interface/RPCQualityTestTypes.h
@@ -0,0 +1,18 @@
+#ifndef RPCQualityTestTypes_H
+#define RPCQualityTestTypes_H
+
+namespace dqm {
+
+  namespace qTestType{
+
+    static const char XRangeContent[14]="XRangeContent";
+
+ }
+
+}
+
+
+
+
+
+#endif
