9f6931071b5ac93c1232f205e7cec698b1f9c5d0
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-02-02 09:02:54 +0000

    added namespace edm::

--- PhysicsTools/RecoAlgos/src/SealModule.cc
+++ PhysicsTools/RecoAlgos/src/SealModule.cc
@@ -7,10 +7,10 @@
 #include "DataFormats/EGammaReco/interface/Electron.h"
 #include "DataFormats/EGammaReco/interface/Photon.h"
 
-typedef Merger<reco::TrackCollection,    CopyPolicy<reco::Track> >    TrackMerger;
-typedef Merger<reco::MuonCollection,     CopyPolicy<reco::Muon> >     MuonMerger;
-typedef Merger<reco::ElectronCollection, CopyPolicy<reco::Electron> > ElectronMerger;
-typedef Merger<reco::PhotonCollection,   CopyPolicy<reco::Photon> >   PhotonMerger;
+typedef Merger<reco::TrackCollection,    edm::CopyPolicy<reco::Track> >    TrackMerger;
+typedef Merger<reco::MuonCollection,     edm::CopyPolicy<reco::Muon> >     MuonMerger;
+typedef Merger<reco::ElectronCollection, edm::CopyPolicy<reco::Electron> > ElectronMerger;
+typedef Merger<reco::PhotonCollection,   edm::CopyPolicy<reco::Photon> >   PhotonMerger;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( TrackMerger );
