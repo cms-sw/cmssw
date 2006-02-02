a5637b2e7796f5e78b458f194fd0c341505d901f
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-02-02 15:50:32 +0000

    improved file reading

--- DQM/RPCMonitorClient/interface/RPCQualityTester.h
+++ DQM/RPCMonitorClient/interface/RPCQualityTester.h
@@ -5,7 +5,7 @@
  * *
  *  Class that handles the RPC Quality Tests
  * 
- *  $Date: 2006/01/25 16:28:17 $
+ *  $Date: 2006/01/30 17:38:41 $
  *  $Revision: 1.1 $
  *  \author Ilaria Segoni
   */
@@ -14,6 +14,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <map>
 
 class RPCQualityTester
 {
@@ -28,16 +29,18 @@ class RPCQualityTester
   /// Set up Quality Tests
   void SetupTests(MonitorUserInterface * mui) ;
 
-  /// Attaches Quality Tests to ME's
-  void AttachTests(MonitorUserInterface * mui) ;
-  
   /// Set up Quality Tests from Db and Sterts tests configuration
-  void SetupTestsFromDB(MonitorUserInterface * mui) ;
+  void SetupTestsFromTextFile(MonitorUserInterface * mui) ;
  
-  /// Configures Tets
+  /// Fills map<QTestName,MonitorElement>
+  void LinkTeststoME() ;
+ 
+  /// Attaches Quality Tests to ME's
+  void AttachRunTests(MonitorUserInterface * mui) ;
+  
+  /// Configures Test of type ContentsXRangeROOT 
   void SetContentsXRangeROOTTest(MonitorUserInterface * mui,char[20] , float , float[5] ) ;
-  
-  
+
   /// Check Status of Quality Tests
   void CheckTests(MonitorUserInterface * mui);
   
@@ -46,8 +49,8 @@ class RPCQualityTester
   
   
   bool printout;
-  std::string qtest1;
   std::vector<std::string> qTests;
+  std::map< std::string , std::vector<std::string> > qTestToMEMap;
   std::ofstream logFile;
   
 };
--- DQM/RPCMonitorClient/src/RPCQualityTester.cc
+++ DQM/RPCMonitorClient/src/RPCQualityTester.cc
@@ -2,16 +2,17 @@
  *
  *  Implementation of RPCQualityTester
  *
- *  $Date: 2006/01/25 16:28:39 $
+ *  $Date: 2006/01/30 17:39:13 $
  *  $Revision: 1.1 $
  *  \author Ilaria Segoni
  */
 #include "DQM/RPCMonitorClient/interface/RPCQualityTester.h"
+#include "DQM/RPCMonitorClient/interface/RPCQualityTestTypes.h"
 #include "DQMServices/QualityTests/interface/QCriterionRoot.h"
 
 #include<iostream>
 
-
+//
 RPCQualityTester::RPCQualityTester()
 {
   
@@ -22,136 +23,167 @@ RPCQualityTester::RPCQualityTester()
 
 }
 
-
+//
 RPCQualityTester::~RPCQualityTester(){ 
   logFile.close();
 }
 
 
 
-void RPCQualityTester::SetupTestsFromDB(MonitorUserInterface * mui){ 
+//
+void RPCQualityTester::SetupTests(MonitorUserInterface * mui){
+    
+  if(printout) logFile<<"In RPCQualityTester::SetupTests(...)"<<std::endl;  
+  this->SetupTestsFromTextFile(mui);
+  this->LinkTeststoME();
+  this->AttachRunTests(mui);
+
+}
 
 
-  if(printout) logFile<<"in RPCQualityTester::SetupTestsFromDB"<<std::endl;
+
+//
+void RPCQualityTester::SetupTestsFromTextFile(MonitorUserInterface * mui){ 
+
+
+  if(printout) logFile<<"In RPCQualityTester::SetupTestsFromTextFiles(...)"<<std::endl;
   FILE* TestsFile;
   char  fileName[128];
   sprintf(fileName,"QualityTests.db");
+  TestsFile= fopen(fileName,"r");
 
-
-  
- TestsFile= fopen(fileName,"r");
-  
-  
-  int TestType;
-  char TestName[20];
+  char  TestType[20];
+  char  TestName[20];
   float WarningLevel=0;
   float params[5];
   
   for(int ii=0; ii<5; ii++) params[ii]=0.0;
 
-
-  int counter =1;
   int file_status=1;
-  
-  do{
-    
-    file_status=fscanf(TestsFile,"%d%s%f%f%f%f%f%f\n",
+ 
+  do{    
+     file_status=fscanf(TestsFile,"%s%s%f%f%f%f%f%f\n",
                          &TestType,&TestName,&WarningLevel,
 			 &params[0],&params[1],&params[2],&params[3],&params[4]);
 			 
     if(file_status != EOF){			 
-       if(printout)  logFile<<" Reading Quality Tests Configuration: "
-            <<TestType<<" "<<TestName<<" "<<WarningLevel<<" "
-	    <<params[0]<<" "<<params[1]<<" "<<params[2]
-	    <<" "<<params[3]<<" "<<params[4]<< std::endl;
+    
+           if(printout)  logFile<<" Reading Quality Tests Configuration: "
+                          <<TestType<<" "<<TestName <<" "<<WarningLevel<<" "
+	                  <<params[0]<<" "<<params[1]<<" "<<params[2]
+	                  <<" "<<params[3]<<" "<<params[4]<< std::endl;
 	
-     if(TestType==1) this->SetContentsXRangeROOTTest(mui,TestName,WarningLevel,params);    
-       	    
-    }	
+	    
+           if(!std::strcmp(TestType,dqm::qTestType::XRangeContent)) this->SetContentsXRangeROOTTest(mui,TestName,WarningLevel,params);    
+    }
 
-     counter++;
-
-
- }while( file_status!=EOF ); 
+  }while( file_status!=EOF ); 
 
 
 
 }
 
 
+void RPCQualityTester::LinkTeststoME(){ 
+
+
+  if(printout) logFile<<"in RPCQualityTester::LinkTeststoME"<<std::endl;
+  FILE* TestsMEsFile;
+  char  fileName[128];
+  sprintf(fileName,"TestsMEs.db");
+  TestsMEsFile= fopen(fileName,"r");
+  
+  
+  char TestName[20];
+  char MEName[20];
+  
+  int file_status=1;
+  
+  do{    
+      file_status=fscanf(TestsMEsFile,"%s%s\n",&TestName,&MEName);
+			 
+      if(file_status != EOF){			 
+            if(printout) logFile<<"Reading Quality Tests List: " <<TestName <<" "<<MEName << std::endl;
+	    
+	    std::vector<std::string>  MEList;
+	    if( qTestToMEMap.find(TestName) == qTestToMEMap.end()){
+ 	      MEList.push_back(MEName);
+	      qTestToMEMap[TestName]=MEList;
+	    }else{
+	      MEList=qTestToMEMap[TestName];
+	      MEList.push_back(MEName);
+	    }	    
+      
+     }	
+
+  }while( file_status!=EOF ); 
+
+
+
+}
 
 void RPCQualityTester::SetContentsXRangeROOTTest(MonitorUserInterface * mui, char  TestName[20], float  WarningLevel, float  params[5]){
 
-  if(printout) logFile<<"in RPCQualityTester::SetContentsXRangeROOTTest"<<std::endl;
+  if(printout) logFile<<"In RPCQualityTester::SetContentsXRangeROOTTest, configuring "<<TestName<<" test."<<std::endl;
 	
- /// X-axis content withing a given range [Xmin, Xmax]
- qTests.push_back(TestName);
- 
- 
- QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),TestName);
- MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
- me_qc1->setAllowedXRange(params[0],params[1]);
- me_qc1->setWarningProb(WarningLevel);
+  qTests.push_back(TestName);
+  QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),TestName);
+  MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
+  me_qc1->setAllowedXRange(params[0],params[1]);
+  me_qc1->setWarningProb(WarningLevel);
  
 }
 
 
 
-void RPCQualityTester::SetupTests(MonitorUserInterface * mui){    
-  
-  this-> SetupTestsFromDB(mui); 
- 
- /// X-axis content withing a given range [Xmin, Xmax]
- //qtest1 = "xRange";
- //QCriterion * qc1 = mui->createQTest(ContentsXRangeROOT::getAlgoName(),qtest1);
- //MEContentsXRangeROOT * me_qc1 = (MEContentsXRangeROOT *) qc1;
- /// set allowed range in X-axis (default values: histogram's X-range)
- //me_qc1->setAllowedXRange(1.0, 200.0);
- /// set probability limit for test warning (default: 90%)
- //me_qc1->setWarningProb(0.90);
- return;
+void RPCQualityTester::AttachRunTests(MonitorUserInterface * mui){
 
-}
+if(printout) logFile<<"In RPCQualityTester::AttachRunTests(...)"<<std::endl;
 
-void RPCQualityTester::AttachTests(MonitorUserInterface * mui){
-
- /// use test <qtest1> on all MEs matching string ""
- if(printout) logFile<<"NumberOfChannelsWithData ..."<<std::endl;
- mui->useQTest("NumberOfChannelsWithData", qtest1);
- if(printout) logFile<<"...done."<<std::endl;
-
-
+for (std::map<std::string, std::vector<std::string> >::iterator testsMap=qTestToMEMap.begin(); 
+			   testsMap!=qTestToMEMap.end();++testsMap)	   
+    {
+     
+       std::string testName=testsMap->first;
+       std::vector<std::string> MElist=testsMap->second;
+    
+      	     for(std::vector<std::string>::iterator list = MElist.begin(); list != MElist.end(); ++list){
+      	    	  std::string meName = *(list);
+      	    	  if(printout) logFile<<"Attaching Test "<< testName <<" to ME "<<meName<<std::endl;
+      	    	  mui->useQTest(meName, testName);
+      	     }
+    }
 }
 
 
 
 void RPCQualityTester::CheckTests(MonitorUserInterface * mui) 
 {
-  if(printout) logFile << "Checking Status of Quality Tests" << std::endl;
+  if(printout) logFile << "In RPCQualityTester::CheckTests(...)" << std::endl;
 
   
 	int status = 1980;
 	
 	status= mui->getSystemStatus();
-        if(printout) logFile << "The STATUS IS " << status<<std::endl;
-        if(printout) logFile << "Possible states: \nerror:  " 
+        if(printout) logFile << "Possible states: successful "<< dqm::qstatus::STATUS_OK<<", error:  " 
 	<< dqm::qstatus::ERROR<<
-	"\nwarning:  "<< dqm::qstatus::WARNING<<
-	"\n Other: "<< dqm::qstatus::OTHER<<std::endl;
+	", warning:  "<< dqm::qstatus::WARNING<<
+	", Other: "<< dqm::qstatus::OTHER<<std::endl;
+        if(printout) logFile << "The STATUS IS " << status<<std::endl;
 	
 	switch(status)
 	  {
 	  case dqm::qstatus::ERROR:
-	    std::cout << " Error(s)";
+	    logFile << " Error(s)";
 	    break;
 	  case dqm::qstatus::WARNING:
-	    std::cout << " Warning(s)";
+	    logFile << " Warning(s)";
 	    break;
 	  case dqm::qstatus::OTHER:
-	    std::cout << " Some tests did not run;";
+	    logFile << " Some tests did not run;";
 	    break; 
 	  default:
-	    std::cout << " No problems";
+	    logFile << " No problems";
 	  }
 
 	//MonitorElement * me4 = mui->get("Collector/FU0/C1/C2/histo4");
