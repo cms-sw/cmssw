375731d0eb25beeeac526b0ed72125aef7c1c04e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-02-02 20:05:36 +0000

    fixed bug causing all processing after the first event to be skipped

--- FWCore/Framework/src/Worker.cc
+++ FWCore/Framework/src/Worker.cc
@@ -1,6 +1,6 @@
 
 /*----------------------------------------------------------------------
-$Id: Worker.cc,v 1.3 2005/12/14 01:35:44 chrjones Exp $
+$Id: Worker.cc,v 1.4 2006/01/29 23:33:58 jbk Exp $
 ----------------------------------------------------------------------*/
 
 #include <iostream>
@@ -65,7 +65,7 @@ namespace edm
     switch(state_)
       {
       case Ready: break;
-      case Pass: return true;
+      case Pass: break;
       case Fail: return false;
       case Exception:
 	{
