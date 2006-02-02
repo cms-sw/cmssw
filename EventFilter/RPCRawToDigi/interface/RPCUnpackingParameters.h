18ef5d2bdf2e6652a709b6f3b05f7f791c54f1eb
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-02-02 16:24:32 +0000

    hold some unpacking params

---
+++ EventFilter/RPCRawToDigi/interface/RPCUnpackingParameters.h
@@ -0,0 +1,15 @@
+#ifndef RPCUnpackingParameters_h
+#define RPCUnpackingParameters_h
+
+namespace rpc{
+
+	namespace Unpacking{
+	
+	 static const int  SLINK_WORD_SIZE = 8;
+	 static const int  RPC_RECORD_SIZE = 2;
+	
+	}
+
+}
+
+#endif
