9882d7db2b92feb59caaa21c212538e93b0661ea
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-02-03 10:32:25 +0000

    Fixed xerces for 2.7.0

--- DetectorDescription/RegressionTest/src/SaxToDom.h
+++ DetectorDescription/RegressionTest/src/SaxToDom.h
@@ -10,7 +10,7 @@
 #include <string>
 #include <map>
 #include <vector>
-namespace xercesc_2_3 {} using namespace xercesc_2_3;
+namespace xercesc_2_7 {} using namespace xercesc_2_7;
 
 class AttributeList;
 
