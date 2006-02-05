9dfa93a2aa72a2c66a6a338b05022a1c78380222
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-02-05 12:47:02 +0000

    Updates from Chia

--- SimG4CMS/Calo/src/ShowerForwardNumberingScheme.cc
+++ SimG4CMS/Calo/src/ShowerForwardNumberingScheme.cc
@@ -10,21 +10,27 @@ using namespace std;
 ShowerForwardNumberingScheme::ShowerForwardNumberingScheme(int iv) : 
   EcalNumberingScheme(iv) {
 
-  int iquad_max[40]={5,7,10,11,13,13,14,15,16,17,17,17,18,
+  int iquad_M[40] = {5,7,10,11,13,13,14,15,16,17,17,17,18,
 		     19,19,19,19,19,19,19,19,19,19,19,19,19,19,
 		     18,17,17,17,16,15,
 		     14,13,13,11,10,7,5};
-  int iquad_min[40]={1,1,1,1,1,1,1,1,1,1,1,1,1,
+  int iquad_m[40] = {1,1,1,1,1,1,1,1,1,1,1,1,1,
 		     4,4,6,6,8,8,8,8,8,8,6,6,4,4,
 		     1,1,1,1,1,1,1,1,1,1,1,1,1};
 
+  int i;
+  for(i=0; i<40; i++) {
+    iquad_max[i] = iquad_M[i];
+    iquad_min[i] = iquad_m[i];
+  }
+  
   Ncols[0] = 2*(iquad_max[0]-iquad_min[0]+1);
   Ntot[0] = Ncols[0];
-  for (int i=1; i<40; i++) {
+  for (i=1; i<40; i++) {
     Ncols[i] = iquad_max[i]-iquad_min[i]+1;
     Ntot[i] = Ntot[i-1]+2*Ncols[i];
   }
-
+  
   if (verbosity>0) 
     std::cout << "Creating ShowerForwardNumberingScheme" << std::endl;
 }
@@ -36,13 +42,12 @@ ShowerForwardNumberingScheme::~ShowerForwardNumberingScheme() {
 
 uint32_t ShowerForwardNumberingScheme::getUnitID(const G4Step* aStep) const {
 
-  int level = detectorLevel(aStep);
-  uint32_t intIndex = 0;
+  int level = detectorLevel(aStep); 
+  uint32_t intIndex = 0; 
   if (level > 0) {
     int*      copyno = new int[level];
     G4String* name   = new G4String[level];
     detectorLevel(aStep, level, copyno, name);
-
 
     // depth index - silicon layer 1-st or 2-nd
     int layer = 0;
@@ -54,6 +59,7 @@ uint32_t ShowerForwardNumberingScheme::getUnitID(const G4Step* aStep) const {
       std::cout << "ShowerForwardNumberingScheme: Wrong name of Presh. Si."
 		   << " Strip : " << name[level-1] << std::endl;
     }
+
     // Z index +Z = 1 ; -Z = 2
     int zside   = copyno[level-6];
     zside=2*(1-zside)+1;
@@ -68,7 +74,7 @@ uint32_t ShowerForwardNumberingScheme::getUnitID(const G4Step* aStep) const {
 
     if (verbosity>1) {
       std::cout << "ShowerForwardNumberingScheme : zside " 
-		<< zside << " layer " << layer << " wafer " << wafer 
+		<< zside << " layer " << layer << " wafer " << wafer << " X " << x << " Y "<< y
 		<< " strip " << strip << " UnitID 0x" << std::hex << intIndex 
 		<< std::dec << std::endl;
       for (int ich = 0; ich < level; ich++) {
@@ -92,6 +98,7 @@ void ShowerForwardNumberingScheme::findXY(const int& layer, const int& waf, int&
   for (i=0; i<40; i++) {
     if (waf > Ntot[i]) y = i + 1;
   }
+
   x = 19 - iquad_max[y];
   x = (y==0) ? (x + waf) : (x + waf - Ntot[y-1]);
 
