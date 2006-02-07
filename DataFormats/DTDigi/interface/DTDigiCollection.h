8769c70d120b419d9072fc392d3a4e4c3311d5ba
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-02-07 23:27:28 +0000

    doxygen

--- DataFormats/DTDigi/interface/DTDigiCollection.h
+++ DataFormats/DTDigi/interface/DTDigiCollection.h
@@ -2,19 +2,13 @@
 #define DTDigi_DTDigiCollection_h
 
 /** \class DTDigiCollection
- *  ED
+ *  The collection containing DT Digis in the event.
+ *  Digis are grouped by DTLayerId.
  *
- *  $Date: 2005/10/07 17:40:53 $
- *  $Revision: 1.1 $
+ *  $Date: 2006/01/20 14:58:16 $
+ *  $Revision: 1.2 $
  *  \author Stefano ARGIRO
  */
-
-/** \class DTDigiCollection
-   
-   \author Stefano ARGIRO
-   \version $Id: DTDigiCollection.h,v 1.1 2005/10/07 17:40:53 namapane Exp $
-   \date 21 Apr 2005
-*/
 
 #include <DataFormats/MuonDetId/interface/DTLayerId.h>
 #include <DataFormats/DTDigi/interface/DTDigi.h>
