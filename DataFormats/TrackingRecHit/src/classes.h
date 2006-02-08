654999045c20a80e089a651f9a7ca6d1b09208d7
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-02-08 13:22:58 +0000

    added base classes dictionaries

---
+++ DataFormats/TrackingRecHit/src/classes.h
@@ -0,0 +1,8 @@
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
+#include "DataFormats/TrackingRecHit/interface/RecHit1D.h"
+#include "DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h"
+
+namespace {
+  namespace {
+  }
+}
