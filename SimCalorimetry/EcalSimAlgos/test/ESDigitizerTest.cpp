4b6e3db02a80c27617247bd1f2fe4dbb446f90ed
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2006-02-08 21:09:32 +0000

    first commit for ESDigitizerTest

---
+++ SimCalorimetry/EcalSimAlgos/test/ESDigitizerTest.cpp
@@ -0,0 +1,60 @@
+#include "DataFormats/EcalDetId/interface/ESDetId.h"
+#include "DataFormats/EcalDigi/interface/ESDataFrame.h"
+#include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+#include "SimCalorimetry/CaloSimAlgos/interface/CaloHitResponse.h"
+#include "SimCalorimetry/CaloSimAlgos/interface/CaloTDigitizer.h"
+#include "SimCalorimetry/EcalSimAlgos/interface/EcalSimParameterMap.h"
+#include "SimCalorimetry/EcalSimAlgos/interface/ESShape.h"
+#include "SimCalorimetry/EcalSimAlgos/interface/EcalDigitizerTraits.h"
+#include "SimCalorimetry/EcalSimAlgos/interface/ESElectronicsSim.h"
+#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
+#include <vector>
+#include<iostream>
+#include<iterator>
+using namespace std;
+using namespace cms;
+
+int main() {
+  // make a silly little hit in each subdetector, which should
+  // correspond to a 100 GeV particle
+  ESDetId ESDetId(1, 1, 1, 1, 1);
+  PCaloHit ESHit(ESDetId.rawId(), 1., 0.);
+
+  vector<DetId> ESDetIds;
+  ESDetIds.push_back(ESDetId);
+
+  edm::PCaloHitContainer ESHits;
+  ESHits.push_back(ESHit);
+
+  string ESName = "EcalHitsES";
+  vector<string> caloDets, trackingDets;
+  caloDets.push_back(ESName);
+
+  CrossingFrame crossingFrame(-5, 5, 25, trackingDets, caloDets);
+  crossingFrame.addSignalCaloHits(ESName, &ESHits);
+
+  EcalSimParameterMap parameterMap;
+  ESShape shape;
+
+  CaloHitResponse ESResponse(&parameterMap, &shape);
+
+  ESElectronicsSim electronicsSim(15);
+
+  bool addNoise = false;
+  CaloTDigitizer<ESDigitizerTraits> ESDigitizer(&ESResponse, &electronicsSim, addNoise);
+  ESDigitizer.setDetIds(ESDetIds);
+
+  auto_ptr<ESDigiCollection> ESResult(new ESDigiCollection);
+
+  MixCollection<PCaloHit> ESHitCollection(&crossingFrame, ESName);
+
+  ESDigitizer.run(ESHitCollection, *ESResult);
+
+  // print out all the digis
+  cout << "ES Frames" << endl;
+  copy(ESResult->begin(), ESResult->end(), std::ostream_iterator<ESDataFrame>(std::cout, "\n"));
+
+  return 0;
+}
+
+
