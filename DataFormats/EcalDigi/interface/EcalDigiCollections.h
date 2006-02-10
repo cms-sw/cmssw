3c1c818985cf23282058716a0ce4b5a1c3a7188f
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-02-10 13:06:36 +0000

    Added Preshower DIgis

--- DataFormats/EcalDigi/interface/EcalDigiCollections.h
+++ DataFormats/EcalDigi/interface/EcalDigiCollections.h
@@ -3,12 +3,14 @@
 
 #include "DataFormats/EcalDigi/interface/EBDataFrame.h"
 #include "DataFormats/EcalDigi/interface/EEDataFrame.h"
+#include "DataFormats/EcalDigi/interface/ESDataFrame.h"
 #include "DataFormats/EcalDigi/interface/EcalTriggerPrimitiveDigi.h"
 #include "DataFormats/EcalDigi/interface/EcalPnDiodeDigi.h"
 #include "FWCore/EDProduct/interface/SortedCollection.h"
 
 typedef edm::SortedCollection<EBDataFrame> EBDigiCollection;
 typedef edm::SortedCollection<EEDataFrame> EEDigiCollection;
+typedef edm::SortedCollection<ESDataFrame> ESDigiCollection;
 typedef edm::SortedCollection<EcalTriggerPrimitiveDigi> EcalTrigPrimDigiCollection;
 typedef edm::SortedCollection<EcalPnDiodeDigi> EcalPnDiodeDigiCollection;
 
--- DataFormats/EcalDigi/src/classes.h
+++ DataFormats/EcalDigi/src/classes.h
@@ -5,25 +5,30 @@ namespace {
   namespace {
     std::vector<EcalMGPASample> vMGPA_;
     std::vector<EcalFEMSample> vFEM_;
+    std::vector<ESSample> vESSample_;
     std::vector<EcalTriggerPrimitiveSample> vETPS_;
 
     edm::SortedCollection<EBDataFrame> vEB_;
     edm::SortedCollection<EEDataFrame> vEE_;
+    edm::SortedCollection<ESDataFrame> vES_;
     edm::SortedCollection<EcalTriggerPrimitiveDigi> vETP_;
     edm::SortedCollection<EcalPnDiodeDigi> vEPN_;
 
     EBDigiCollection theEB_;
     EEDigiCollection theEE_;
+    ESDigiCollection theES_;
     EcalTrigPrimDigiCollection theETP_;
     EcalPnDiodeDigiCollection theEPN_;
 
     edm::Wrapper<EBDigiCollection> anotherEBw_;
     edm::Wrapper<EEDigiCollection> anotherEEw_;
+    edm::Wrapper<ESDigiCollection> anotherESw_;
     edm::Wrapper<EcalTrigPrimDigiCollection> anotherETPw_;
     edm::Wrapper<EcalPnDiodeDigiCollection> anotherEPNw_;
 
     edm::Wrapper< edm::SortedCollection<EBDataFrame> > theEBw_;
     edm::Wrapper< edm::SortedCollection<EEDataFrame> > theEEw_;
+    edm::Wrapper< edm::SortedCollection<ESDataFrame> > theESw_;
     edm::Wrapper< edm::SortedCollection<EcalTriggerPrimitiveDigi> > theETPw_; 
     edm::Wrapper< edm::SortedCollection<EcalPnDiodeDigi> > theEPNw_; 
  }
