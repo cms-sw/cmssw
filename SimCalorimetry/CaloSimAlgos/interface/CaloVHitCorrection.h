c2fe8f64a28fad2b2fa93020eefc3b5ef0ea3a2c
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-02-11 00:33:16 +0000

    PCaloHit can't be const.  Defeats the whole purpose of a HitCorrection, really.

--- SimCalorimetry/CaloSimAlgos/interface/CaloVHitCorrection.h
+++ SimCalorimetry/CaloSimAlgos/interface/CaloVHitCorrection.h
@@ -1,9 +1,11 @@
 #ifndef CaloSimAlgos_CaloVHitCorrection_h
 #define CaloSimAlgos_CaloVHitCorrection_h
 
+#include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+
 class CaloVHitCorrection {
 public:
-  virtual void correct(const PCaloHit & hit) const = 0;
+  virtual void correct(PCaloHit & hit) const = 0;
 };
 
 #endif
