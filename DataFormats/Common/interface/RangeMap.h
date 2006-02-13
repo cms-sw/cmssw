eafbd155525d6ade291b8937010bb16b3e6253fa
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-02-13 11:54:41 +0000

    added possibility of insert in random order

--- DataFormats/Common/interface/RangeMap.h
+++ DataFormats/Common/interface/RangeMap.h
@@ -2,7 +2,7 @@
 #define Common_RangeMap_h
 #include <map>
 #include "FWCore/Utilities/interface/Exception.h"
-// $Id$
+// $Id: RangeMap.h,v 1.1 2006/02/13 11:42:21 llista Exp $
 namespace edm {
 
   template<typename ID, typename C, typename P>
@@ -10,17 +10,16 @@ namespace edm {
   public:
     typedef typename C::value_type value_type;
     typedef typename C::const_iterator const_iterator;
-    typedef std::map<ID, typename C::size_type> map;
+    typedef std::pair<typename C::size_type, typename C::size_type> pair;
+    typedef std::map<ID, pair> map;
     typedef std::pair<const_iterator, const_iterator> range;
     RangeMap() { }
     range get( ID id ) const {
       const_iterator begin, end;
       typename map::const_iterator i = map_.find( id );
       if ( i != map_.end() ) { 
-	begin = collection_.begin() + i->second;
-	end = ( ++ i != map_.end() ) ? 
-	  collection_.begin() + i->second :
-	  collection_.end();
+	begin = collection_.begin() + i->second.first;
+	end = collection_.begin() + i->second.second;
       } else {
 	begin = end = collection_.end();
       }
@@ -32,9 +31,11 @@ namespace edm {
       if( i != map_.end() ) 
       	throw cms::Exception( "Error" ) << "trying to insert duplicate entry";
       assert( i == map_.end() );
-      map_[ id ] = collection_.size();
+      pair & p = map_[ id ];
+      p.first = collection_.size();
       for( CI i = begin; i != end; ++ i )
 	collection_.push_back( P::clone( * i ) );
+      p.second = collection_.size();
     }
     size_t size() { return collection_.size(); }
     typename C::const_iterator begin() const { return collection_.begin(); }
