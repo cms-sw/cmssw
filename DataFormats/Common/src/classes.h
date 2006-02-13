239c76cc84b239cf8480a2d6b607a7936aa5a6fe
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-02-13 22:25:25 +0000

    ToyProducts.h moved to DataFormats/TestObjects

--- DataFormats/Common/src/classes.h
+++ DataFormats/Common/src/classes.h
@@ -1,25 +1,21 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Common/interface/ProductID.h"
-#include "DataFormats/Common/interface/ParameterSetID.h"
-#include "DataFormats/Common/interface/EDProduct.h"
-#include "DataFormats/Common/interface/EventID.h"
-#include "DataFormats/Common/interface/Timestamp.h"
-#include "DataFormats/Common/interface/RefBase.h"
-#include "DataFormats/Common/interface/RefVectorBase.h"
-#include "DataFormats/Common/interface/RefItem.h"
-#include "DataFormats/Common/interface/TriggerResults.h"
-#include "DataFormats/Common/interface/EventAux.h"
-#include "DataFormats/Common/interface/EventProvenance.h"
 #include "DataFormats/Common/interface/BranchDescription.h"
 #include "DataFormats/Common/interface/BranchEntryDescription.h"
-#include "DataFormats/Common/interface/ProductRegistry.h"
 #include "DataFormats/Common/interface/BranchKey.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "DataFormats/Common/interface/EventAux.h"
+#include "DataFormats/Common/interface/EventID.h"
+#include "DataFormats/Common/interface/EventProvenance.h"
+#include "DataFormats/Common/interface/ParameterSetID.h"
+#include "DataFormats/Common/interface/ProductID.h"
+#include "DataFormats/Common/interface/ProductRegistry.h"
+#include "DataFormats/Common/interface/RefBase.h"
+#include "DataFormats/Common/interface/RefItem.h"
+#include "DataFormats/Common/interface/RefVectorBase.h"
+#include "DataFormats/Common/interface/Timestamp.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Common/interface/ToyProducts.h"
 
 #include <vector>
-#include <string>
-#include <map>
 
 namespace { namespace {
   edm::Wrapper<std::vector<unsigned long> > dummy1;
@@ -45,10 +41,5 @@ namespace { namespace {
   edm::Wrapper<unsigned short> dummyw9;
   edm::Wrapper<double> dummyw10;
   edm::Wrapper<long double> dummyw11;
-  edm::Wrapper<edmtest::DummyProduct> dummyw12;
-  edm::Wrapper<edmtest::IntProduct> dummyw13;
-  edm::Wrapper<edmtest::DoubleProduct> dummyw14;
-  edm::Wrapper<edmtest::StringProduct> dummyw15;
-  edm::Wrapper<edmtest::SCSimpleProduct> dummyw16;
   edm::Wrapper<edm::TriggerResults> dummyw17;
 }}
