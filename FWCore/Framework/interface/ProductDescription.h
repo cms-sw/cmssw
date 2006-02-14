9dc15db6aec1b19017275cf2820c8e846df29570
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-02-14 00:11:27 +0000

    Remove obsolete header

--- FWCore/Framework/interface/ProductDescription.h
+++
@@ -1,9 +0,0 @@
-#ifndef Framework_ProductDescription_h
-#define Framework_ProductDescription_h
-
-#include "DataFormats/Common/interface/BranchDescription.h"
-
-namespace edm {
-  typedef BranchDescription ProductDescription;
-}
-#endif
