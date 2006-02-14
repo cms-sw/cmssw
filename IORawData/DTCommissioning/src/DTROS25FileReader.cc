99a07449b9bec71f15ac22bedc1c70f278e66343
Marco Zanetti <sha1-fba42c5becc5876495df0d6b3436fa25443a9c62@cern.ch>
2006-02-14 16:22:54 +0000

    ROS25 (final DT read out) file reader

---
+++ IORawData/DTCommissioning/src/DTROS25FileReader.cc
@@ -0,0 +1,166 @@
+/** \file
+ *
+ *  $Date: 2005/11/25 18:14:12 $
+ *  $Revision: 1.4 $
+ *  \author M. Zanetti
+ */
+
+#include <IORawData/DTCommissioning/src/DTROS25FileReader.h>
+#include <IORawData/DTCommissioning/src/DTFileReaderHelpers.h>
+
+#include <DataFormats/FEDRawData/interface/FEDHeader.h>
+#include <DataFormats/FEDRawData/interface/FEDTrailer.h>
+#include <DataFormats/FEDRawData/interface/FEDNumbering.h>
+
+#include <FWCore/EDProduct/interface/EventID.h>
+#include <FWCore/EDProduct/interface/Timestamp.h>
+#include <DataFormats/FEDRawData/interface/FEDRawData.h>
+#include <DataFormats/FEDRawData/interface/FEDRawDataCollection.h>
+
+#include <FWCore/ParameterSet/interface/ParameterSet.h>
+
+#include <string>
+#include <iosfwd>
+#include <iostream>
+#include <algorithm>
+   
+using namespace std;
+using namespace edm;
+
+
+DTROS25FileReader::DTROS25FileReader(const edm::ParameterSet& pset) : 
+  runNumber(1), eventNumber(0) {
+      
+  const string & filename = pset.getParameter<string>("fileName");
+
+  inputFile.open(filename.c_str(), ios::in | ios::binary );
+  if( inputFile.fail() ) {
+    throw cms::Exception("InputFileMissing") 
+      << "DTROS25FileReader: the input file: " << filename <<" is not present";
+  }
+}
+
+
+DTROS25FileReader::~DTROS25FileReader(){
+  inputFile.close();
+}
+
+
+bool DTROS25FileReader::fillRawData(EventID& eID,
+				    Timestamp& tstamp, 
+				    FEDRawDataCollection& data){
+
+
+  vector<uint32_t> eventData;
+  size_t estimatedEventDimension = 1024; // dimensione hardcoded
+  eventData.reserve(estimatedEventDimension); 
+  uint32_t word = 0;
+  
+
+  try {   
+
+    bool marked = false;
+
+    // getting the data word by word from the file
+    // do it until you get the ROS25 trailer
+    while ( !isTrailer(word) ) { 
+      
+      // get the first word
+      inputFile.read(dataPointer<uint32_t>( &word ), rosWordLenght);
+      
+      // WARNING!!! ||swapping it|| (Check whether it is necessary) 
+      swap(word);
+
+      if ( !inputFile ) throw 1;
+
+      // get the ROS25 header
+      if (isHeader(word)) marked=true;
+
+      // from now on fill the eventData with the ROS data
+      if (marked) {
+	eventData.push_back(word);
+
+      }
+    } 
+
+    // next event reading will start with meaningless trailer+header from DTLocalDAQ
+    // those will be skipped automatically when seeking for the ROS25 header
+
+    if (eventData.size() > estimatedEventDimension) throw 2;
+    
+    // Setting the Event ID
+    eID = EventID( runNumber, eventNumber);
+
+    // eventDataSize = (Number Of Words)* (Word Size)
+    int eventDataSize = eventData.size()*rosWordLenght;
+
+    // The FED ID is always the first in the DT range
+    FEDRawData& fedRawData = data.FEDData( FEDNumbering::getDTFEDIds().first );
+    fedRawData.resize(eventDataSize);
+    
+    copy(reinterpret_cast<unsigned char*>(&eventData[0]),
+	 reinterpret_cast<unsigned char*>(&eventData[0]) + eventDataSize, fedRawData.data());
+
+    return true;
+  }
+
+  catch( int i ) {
+
+    if ( i == 1 ){
+      cout<<"[DTROS25FileReader]: ERROR! failed to get the trailer"<<endl;
+      return false;
+    }    
+    else {
+      cout<<"[DTROS25FileReader]:"
+	  <<" ERROR! ROS data exceeding estimated event dimension. Event size = "
+	  <<eventData.size()<<endl;
+      return false;
+    }
+    
+  }
+
+}
+
+void DTROS25FileReader::swap(uint32_t & word) {
+  
+  twoNibble* newWorld = reinterpret_cast<twoNibble*>(&word);
+
+  uint16_t msBits_tmp = newWorld->msBits;
+  newWorld->msBits = newWorld->lsBits;
+  newWorld->lsBits = msBits_tmp;
+}
+
+
+bool DTROS25FileReader::isHeader(uint32_t word) {
+
+  bool it_is = false;
+  if ( (word >> 24 ) == 31 ) {
+    it_is = true;
+    eventNumber++;
+  }
+ 
+  return it_is;
+}
+
+
+bool DTROS25FileReader::isTrailer(uint32_t word) {
+ 
+  bool it_is = false;
+  if ( (word >> 24 ) == 63 ) {
+    it_is = true;
+  }
+ 
+  return it_is;
+}
+
+
+bool DTROS25FileReader::checkEndOfFile(){
+
+  bool retval=false;
+  if ( inputFile.eof() ) retval=true;
+  return retval;
+
+}
+
+
+
