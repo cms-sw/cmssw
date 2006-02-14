89c1024becc7aac8db0429bce296c71ac3a0cd99
Marco Zanetti <sha1-fba42c5becc5876495df0d6b3436fa25443a9c62@cern.ch>
2006-02-14 16:24:41 +0000

    adding module definition for ROS25 reader

--- IORawData/DTCommissioning/src/SealModule.cc
+++ IORawData/DTCommissioning/src/SealModule.cc
@@ -1,5 +1,7 @@
 #include "PluginManager/ModuleDef.h"
 #include "DTROS8FileReader.h"
+#include "DTROS25FileReader.h"
 #include <IORawData/DaqSource/interface/DaqReaderPluginFactory.h>
 DEFINE_SEAL_MODULE();
 DEFINE_SEAL_PLUGIN (DaqReaderPluginFactory, DTROS8FileReader, "DTROS8FileReader");
+DEFINE_SEAL_PLUGIN (DaqReaderPluginFactory, DTROS25FileReader, "DTROS25FileReader");
