bb6ccdc0aed14282fe4d8974c978e1d6fb8d7d45
Mark Steven Fischler <sha1-4e5f5390fe08571bcaa72b9888ceb66263060cc9@cern.ch>
2006-02-15 17:13:56 +0000

    Headers to detect improper location of header usage

---
+++ FWCore/MessageLogger/interface/ELset.h
@@ -0,0 +1,44 @@
+#error Wrong ELset.h
+
+#ifndef ELSET_H
+#define ELSET_H
+
+
+// ----------------------------------------------------------------------
+//
+// ELset.h     Provides a set class with the semantics of std::set.
+//              Customizers may substitute for this class to provide either
+//              a set with a different allocator, or whatever else.
+//
+// The following elements of set semantics are relied upon:
+//      insert()
+//	clear()
+//	find() which returns an iterator which may or may not be .end()
+//
+// ----------------------------------------------------------------------
+
+#ifndef ELSTRING_H
+  #include "FWCore/MessageLogger/interface/ELstring.h"
+#endif
+
+#include <set>
+
+
+namespace edm {       
+
+
+struct ELsetS {
+  std::string s;
+  ELsetS (const std::string & ss) : s(ss) {}
+  bool operator< (const ELsetS & t) const { return (s<t.s); }
+};
+
+typedef std::set<ELsetS> ELset_string;
+
+// ----------------------------------------------------------------------
+
+
+}        // end of namespace edm
+
+
+#endif // ELSET_H
