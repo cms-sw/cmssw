7f635577f107fe34d13fff04a4f6c11d59c950ff
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-02-17 09:18:30 +0000

    Defined RPCDigiProducer seal module

---
+++ SimMuon/RPCDigitizer/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "SimMuon/RPCDigitizer/src/RPCDigiProducer.h"
+
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(RPCDigiProducer)
+
