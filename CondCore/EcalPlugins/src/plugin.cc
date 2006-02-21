11da4f1173d7014d251bdef3e7a94aeb3803ae66
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2006-02-21 14:57:06 +0000

    added plugin for new ecal Db objects

--- CondCore/EcalPlugins/src/plugin.cc
+++ CondCore/EcalPlugins/src/plugin.cc
@@ -7,12 +7,33 @@
  */
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
+
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
+
 #include "CondFormats/EcalObjects/interface/EcalWeightRecAlgoWeights.h"
 #include "CondFormats/DataRecord/interface/EcalWeightRecAlgoWeightsRcd.h"
 
+#include "CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h"
+#include "CondFormats/DataRecord/interface/EcalWeightXtalGroupsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalTBWeights.h"
+#include "CondFormats/DataRecord/interface/EcalTBWeightsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalGainRatios.h"
+#include "CondFormats/DataRecord/interface/EcalGainRatiosRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalIntercalibConstants.h"
+#include "CondFormats/DataRecord/interface/EcalIntercalibConstantsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalADCToGeVConstant.h"
+#include "CondFormats/DataRecord/interface/EcalADCToGeVConstantRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(EcalPedestalsRcd,EcalPedestals);
 REGISTER_PLUGIN(EcalWeightRecAlgoWeightsRcd,EcalWeightRecAlgoWeights);
+REGISTER_PLUGIN(EcalWeightXtalGroupsRcd,EcalWeightXtalGroups);
+REGISTER_PLUGIN(EcalTBWeightsRcd,EcalTBWeights);
+REGISTER_PLUGIN(EcalGainRatiosRcd,EcalGainRatios);
+REGISTER_PLUGIN(EcalIntercalibConstantsRcd,EcalIntercalibConstants);
+REGISTER_PLUGIN(EcalADCToGeVConstantRcd,EcalADCToGeVConstant);
