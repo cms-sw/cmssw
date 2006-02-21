20308e3167583aec78abfb3f66d6fe0a3223c28a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-02-21 18:24:25 +0000

    add BlobPedestals

--- CondFormats/Calibration/src/classes.h
+++ CondFormats/Calibration/src/classes.h
@@ -1,4 +1,7 @@
 #include "CondFormats/Calibration/interface/Pedestals.h"
+#include "CondFormats/Calibration/interface/BlobPedestals.h"
 // Declaration of the iterator (necessary for the generation of the dictionary)
 template std::vector< Pedestals::Item >::iterator;
 template std::vector< Pedestals::Item >::const_iterator;
+template std::vector< unsigned int >::iterator;
+template std::vector< unsigned int >::const_iterator;
