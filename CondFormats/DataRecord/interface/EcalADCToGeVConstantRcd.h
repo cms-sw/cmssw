7981eadc7b2268f26eab9a22a94f474d7b8ac288
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2006-02-21 14:55:23 +0000

    added new data records for the newly defined ECAL DB objects

---
+++ CondFormats/DataRecord/interface/EcalADCToGeVConstantRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalADCToGeVConstant_H
+#define CondFormats_DataRecord_EcalADCToGeVConstant_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalADCToGeVConstantRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalADCToGeVConstantRcd> {};
+#endif
---
+++ CondFormats/DataRecord/interface/EcalGainRatiosRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalGainRatiosRcd_H
+#define CondFormats_DataRecord_EcalGainRatiosRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalGainRatiosRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalGainRatiosRcd> {};
+#endif
