36a86a7aa887f63a298744f29a4a6331a555689c
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-02-21 22:56:07 +0000

    Added DigiDump

--- SimMuon/CSCDigitizer/src/SealModule.cc
+++ SimMuon/CSCDigitizer/src/SealModule.cc
@@ -1,8 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimMuon/CSCDigitizer/src/CSCDigiProducer.h"
-
+#include "SimMuon/CSCDigitizer/src/CSCDigiDump.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiProducer)
+DEFINE_ANOTHER_FWK_MODULE(CSCDigiDump)
 
