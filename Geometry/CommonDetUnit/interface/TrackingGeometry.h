12a7037c2f6110e8c69109c968d48a96c55dd63d
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2006-02-22 15:04:39 +0000

    first implementation of GluedDet

--- Geometry/CommonDetUnit/interface/TrackingGeometry.h
+++ Geometry/CommonDetUnit/interface/TrackingGeometry.h
@@ -5,8 +5,8 @@
  *
  *  Base class for the geometry of tracking detectors.
  *
- *  $Date: $
- *  $Revision: $
+ *  $Date: 2005/11/04 18:09:44 $
+ *  $Revision: 1.1 $
  *  \author ...
  */
 
@@ -16,32 +16,44 @@
 
 class GeomDetType;
 class GeomDetUnit;
+class GeomDet;
 
 class TrackingGeometry
 {
 public:
   typedef std::vector<GeomDetType*>          DetTypeContainer;
-  typedef std::vector<GeomDetUnit*>          DetContainer;
+  typedef std::vector<GeomDet*>              DetContainer;
+  typedef std::vector<GeomDetUnit*>          DetUnitContainer;
   typedef std::vector<DetId>                 DetIdContainer;
-  typedef std::map<DetId,GeomDetUnit*>       mapIdToDet;
+  typedef std::map<DetId,GeomDetUnit*>       mapIdToDetUnit;
+  typedef std::map<DetId,GeomDet*>           mapIdToDet;
 
   /// Default constructor
   //  virtual TrackingGeometry() {}
-
+  
   /// Destructor
   virtual ~TrackingGeometry() {}
-
+  
   /// Return a vector of all det types
   virtual const DetTypeContainer&  detTypes()     const = 0;
 
   /// Returm a vector of all GeomDetUnit
+  virtual const DetUnitContainer&      detUnits()         const = 0;
+
+  /// Returm a vector of all GeomDet
   virtual const DetContainer&      dets()         const = 0;
 
-  /// Returm a vector of all DetIds
+  /// Returm a vector of all GeomDetUnit DetIds
+  virtual const DetIdContainer&    detUnitIds()       const = 0;
+
+  /// Returm a vector of all GeomDet DetIds
   virtual const DetIdContainer&    detIds()       const = 0;
 
   /// Return the pointer to the GeomDetUnit corresponding to a given DetId
-  virtual const GeomDetUnit*       idToDet(DetId) const = 0;
+  virtual const GeomDetUnit*       idToDetUnit(DetId) const = 0;
+
+  /// Return the pointer to the GeomDet corresponding to a given DetId
+  virtual const GeomDet*       idToDet(DetId) const = 0; 
 
 };
 
