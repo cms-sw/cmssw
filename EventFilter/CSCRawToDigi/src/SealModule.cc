1ebf228050ea769318f45d9ab4fca37391912de9
tumanov <sha1-47187222be523f6c8f2edbe22a417c456a25dfe9@cern.ch>
2006-02-22 11:33:19 +0000

    *** empty log message ***

--- EventFilter/CSCRawToDigi/src/SealModule.cc
+++ EventFilter/CSCRawToDigi/src/SealModule.cc
@@ -1,5 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include <EventFilter/CSCRawToDigi/interface/CSCDCCUnpacker.h>
+#include <EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h>
+
 
 DEFINE_FWK_MODULE(CSCDCCUnpacker)
+DEFINE_ANOTHER_FWK_MODULE(DigiAnalyzer)
