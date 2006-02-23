d11e120d52e8b6235d8edc4f6742c458aca5c720
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-02-23 18:21:28 +0000

    First commit

---
+++ Geometry/EcalCommonData/src/EcalBaseNumber.cc
@@ -0,0 +1,34 @@
+#include "Geometry/EcalCommonData/interface/EcalBaseNumber.h"
+
+void EcalBaseNumber::addLevel(std::string name,int copyNumber)
+{
+  _sortedBaseNumber.push_back(std::pair<std::string,int>(name,copyNumber));
+}
+
+int EcalBaseNumber::getLevels() const
+{
+  return _sortedBaseNumber.size();
+}
+
+int EcalBaseNumber::getCopyNumber(int level) const
+{
+  return _sortedBaseNumber[level].second;
+}
+
+int EcalBaseNumber::getCopyNumber(std::string levelName) const
+{
+  basenumber_type::const_iterator cur=_sortedBaseNumber.begin();
+  basenumber_type::const_iterator end=_sortedBaseNumber.end();
+  while (cur!=end) {
+    if ((*cur).first==levelName) {
+      return (*cur).second;
+    }
+    cur++;
+  }
+  return 0;
+}
+
+std::string EcalBaseNumber::getLevelName(int level) const
+{
+  return _sortedBaseNumber[level].first;
+}
