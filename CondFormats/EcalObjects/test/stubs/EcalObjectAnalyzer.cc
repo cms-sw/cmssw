46adb19e53e3d898d1fc30047410de9b3c3c1c81
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2006-02-23 16:55:41 +0000

    updated standalone applications to also store a copy of the old weight object along the nely defined object.
    this is needed by me for debugging purposes until I fully understand an odd behaviour
    of EcalTBWeights.
    
    
    data/print_obj.cfg and stubs/EcalObjectAnalyzer.cc are updated accordingly to fetch values for
    all objects currently stored with testEcalObjects.cpp
    
    once all is fine I will remove the other standlone application

--- CondFormats/EcalObjects/test/stubs/EcalObjectAnalyzer.cc
+++ CondFormats/EcalObjects/test/stubs/EcalObjectAnalyzer.cc
@@ -18,11 +18,36 @@ simple analyzer to dump information about ECAL cond objects
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalXtalGroupId.h"
+#include "CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h"
+#include "CondFormats/DataRecord/interface/EcalWeightXtalGroupsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalWeight.h"
+#include "CondFormats/EcalObjects/interface/EcalWeightSet.h"
+#include "CondFormats/EcalObjects/interface/EcalTBWeights.h"
+#include "CondFormats/DataRecord/interface/EcalTBWeightsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalIntercalibConstants.h"
+#include "CondFormats/DataRecord/interface/EcalIntercalibConstantsRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalMGPAGainRatio.h"
+#include "CondFormats/EcalObjects/interface/EcalGainRatios.h"
+#include "CondFormats/DataRecord/interface/EcalGainRatiosRcd.h"
+
+#include "CondFormats/EcalObjects/interface/EcalADCToGeVConstant.h"
+#include "CondFormats/DataRecord/interface/EcalADCToGeVConstantRcd.h"
+
 #include "CondFormats/EcalObjects/interface/EcalWeightRecAlgoWeights.h"
 #include "CondFormats/DataRecord/interface/EcalWeightRecAlgoWeightsRcd.h"
 #include "CLHEP/Matrix/Matrix.h"
+
+
+
 
 using namespace std;
 
@@ -43,38 +68,161 @@ using namespace std;
   {
     using namespace edm::eventsetup;
     // Context is not used.
-    std::cout <<" I AM IN RUN NUMBER "<<e.id().run() <<std::endl;
-    std::cout <<" ---EVENT NUMBER "<<e.id().event() <<std::endl;
+    std::cout <<">>> EcalObjectAnalyzer: processing run "<<e.id().run() << " event: " << e.id().event() << std::endl;
+
     edm::ESHandle<EcalPedestals> pPeds;
     context.get<EcalPedestalsRcd>().get(pPeds);
 
-    //call tracker code
-    //
-    int channelID=1656;
-    //EcalPedestals* myped=const_cast<EcalPedestals*>(pPeds.product());
+    // ADC -> GeV Scale
+    edm::ESHandle<EcalADCToGeVConstant> pAgc;
+    context.get<EcalADCToGeVConstantRcd>().get(pAgc);
+    const EcalADCToGeVConstant* agc = pAgc.product();
+    std::cout << "Global ADC->GeV scale: " << agc->getValue() << " GeV/ADC count" << std::endl;
+
+    // use a channel to fetch values from DB
+    double r1 = (double)std::rand()/( double(RAND_MAX)+double(1) );
+    int ieta =  int( 1 + r1*85 );
+    r1 = (double)std::rand()/( double(RAND_MAX)+double(1) );
+    int iphi =  int( 1 + r1*20 );
+
+    EBDetId ebid(ieta,iphi); //eta,phi
+    std::cout << "EcalObjectAnalyzer: using EBDetId: " << ebid << std::endl;
+
     const EcalPedestals* myped=pPeds.product();
-    std::map<const unsigned int,EcalPedestals::Item>::const_iterator it=myped->m_pedestals.find(channelID);
+    std::map<const unsigned int,EcalPedestals::Item>::const_iterator it=myped->m_pedestals.find(ebid.rawId());
     if( it!=myped->m_pedestals.end() ){
-      std::cout << "Ecal channel: " << channelID
+      std::cout << "EcalPedestal: "
                 << "  mean_x1:  " <<it->second.mean_x1 << " rms_x1: " << it->second.rms_x1
                 << "  mean_x6:  " <<it->second.mean_x6 << " rms_x6: " << it->second.rms_x6
                 << "  mean_x12: " <<it->second.mean_x12 << " rms_x12: " << it->second.rms_x12
                 << std::endl;
+    } else {
+     std::cout << "No pedestal found for this xtal! something wrong with EcalPedestals in your DB? "
+               << std::endl;
     }
 
+    // fetch map of groups of xtals
+    edm::ESHandle<EcalWeightXtalGroups> pGrp;
+    context.get<EcalWeightXtalGroupsRcd>().get(pGrp);
+    const EcalWeightXtalGroups* grp = pGrp.product();
 
-    edm::ESHandle<EcalWeightRecAlgoWeights> pWgts;
-    context.get<EcalWeightRecAlgoWeightsRcd>().get(pWgts);
-    const EcalWeightRecAlgoWeights* wgts = pWgts.product();
+    EcalWeightXtalGroups::EcalXtalGroupsMap::const_iterator git = grp->getMap().find( ebid.rawId() );
+    EcalXtalGroupId gid;
+    if( git != grp->getMap().end() ) {
+      std::cout << "XtalGroupId.id() = " << git->second.id() << std:: endl;
+      gid = git->second;
+    } else {
+      std::cout << "No group id found for this crystal. something wrong with EcalWeightXtalGroups in your DB?"
+                << std::endl;
+   }
 
-    typedef std::vector< std::vector<EcalWeight> > EcalWeightMatrix;
-    EcalWeightMatrix& mat1 = wgts->getWeightsBeforeGainSwitch();
-    EcalWeightMatrix& mat2 = wgts->getWeightsAfterGainSwitch();
+    // Gain Ratios
+    edm::ESHandle<EcalGainRatios> pRatio;
+    context.get<EcalGainRatiosRcd>().get(pRatio);
+    const EcalGainRatios* gr = pRatio.product();
+
+    EcalGainRatios::EcalGainRatioMap::const_iterator grit=gr->getMap().find(ebid.rawId());
+    EcalMGPAGainRatio mgpa;
+    if( grit!=gr->getMap().end() ){
+      mgpa = grit->second;
+
+      std::cout << "EcalMGPAGainRatio: "
+                << "gain 12/6 :  " << mgpa.gain12Over6() << " gain 6/1: " << mgpa.gain6Over1()
+                << std::endl;
+    } else {
+     std::cout << "No MGPA Gain Ratio found for this xtal! something wrong with EcalGainRatios in your DB? "
+               << std::endl;
+    }
+
+    // Intercalib constants
+    edm::ESHandle<EcalIntercalibConstants> pIcal;
+    context.get<EcalIntercalibConstantsRcd>().get(pIcal);
+    const EcalIntercalibConstants* ical = pIcal.product();
+
+    EcalIntercalibConstants::EcalIntercalibConstantMap::const_iterator icalit=ical->getMap().find(ebid.rawId());
+    EcalIntercalibConstants::EcalIntercalibConstant icalconst;
+    if( icalit!=ical->getMap().end() ){
+      icalconst = icalit->second;
+
+      std::cout << "EcalIntercalibConstant: "
+                << icalconst
+                << std::endl;
+    } else {
+     std::cout << "No intercalib const found for this xtal! something wrong with EcalIntercalibConstants in your DB? "
+               << std::endl;
+    }
+
+   // fetch TB weights
+   std::cout <<"Fetching EcalTBWeights from DB " << std::endl;
+   edm::ESHandle<EcalTBWeights> pWgts;
+   context.get<EcalTBWeightsRcd>().get(pWgts);
+   const EcalTBWeights* wgts = pWgts.product();
+   std::cout << "EcalTBWeightMap.size(): " << wgts->getMap().size() << std::endl;
+
+   // look up the correct weights for this  xtal
+   //EcalXtalGroupId gid( git->second );
+   EcalTBWeights::EcalTDCId tdcid(1);
+
+   std::cout << "Lookup EcalWeightSet for groupid: " << gid.id() << " and TDC id " << tdcid << std::endl;
+   EcalTBWeights::EcalTBWeightMap::const_iterator wit = wgts->getMap().find( std::make_pair(gid,tdcid) );
+   EcalWeightSet  wset;
+   if( wit != wgts->getMap().end() ) {
+      wset = wit->second;
+      //std::cout << "weight set it: " << wit << std::endl;
+
+      std::cout << "check size of data members in EcalWeightSet" << std::endl;
+      wit->second.print(std::cout);
+
+
+      //typedef std::vector< std::vector<EcalWeight> > EcalWeightMatrix;
+      EcalWeightSet::EcalWeightMatrix mat1 = wit->second.getWeightsBeforeGainSwitch();
+      EcalWeightSet::EcalWeightMatrix mat2 = wit->second.getWeightsAfterGainSwitch();
+
+      //std::cout << "WeightsBeforeGainSwitch.size: " << mat1.size() << ", WeightsAfterGainSwitch.size: " << mat2.size() << std::endl;
+
+
+/**
+      //EcalWeight awgt = (mat1[0])[0];
+      std::cout << "before switch [0,0]: " << (mat1[0])[0]() << endl;
+      std::cout << "after  switch [0,0]: " << (mat2[0])[0]() << endl;
+
+      HepMatrix clmat1(3,8,0);
+      HepMatrix clmat2(3,8,0);
+      for(int irow=0; irow<3; irow++) {
+       for(int icol=0; icol<8; icol++) {
+         clmat1[irow][icol] = (mat1[irow])[icol]();
+         clmat2[irow][icol] = (mat2[irow])[icol]();
+       }
+     }
+     std::cout << clmat1 << std::endl;
+     std::cout << clmat2 << std::endl;
+**/
+
+   } else {
+     std::cout << "No weights found for EcalGroupId: " << gid.id() << " and  EcalTDCId: " << tdcid << std::endl;
+  }
+
+
+  // old weight object EcalWeightRecAlgoWeights
+    std::cout << "Fetch old object EcalWeightRecAlgoWeights from DB" << std::endl;
+    edm::ESHandle<EcalWeightRecAlgoWeights> poldWgts;
+    context.get<EcalWeightRecAlgoWeightsRcd>().get(poldWgts);
+    const EcalWeightRecAlgoWeights* oldwgts = poldWgts.product();
+
+    //typedef std::vector< std::vector<EcalWeight> > EcalWeightMatrix;
+    EcalWeightMatrix& mat1 = oldwgts->getWeightsBeforeGainSwitch();
+    EcalWeightMatrix& mat2 = oldwgts->getWeightsAfterGainSwitch();
+
+    std::cout << "check the size of its data members: " << endl;
+    std::cout << "WeightsBeforeGainSwitch.size(): " << mat1.size()
+              << ", WeightsAfterGainSwitch.size(): " << mat2.size()
+              << std::endl;
 
     //EcalWeight awgt = (mat1[0])[0];
-    std::cout << "before switch [0,0]: " << (mat1[0])[0]() << endl;
-    std::cout << "after  switch [0,0]: " << (mat2[0])[0]() << endl;
+    //std::cout << "before switch [0,0]: " << (mat1[0])[0]() << endl;
+    //std::cout << "after  switch [0,0]: " << (mat2[0])[0]() << endl;
 
+    std::cout << "make CLHEP matrices from vector<vector<Ecalweight>>" << std::endl;
     HepMatrix clmat1(3,8,0);
     HepMatrix clmat2(3,8,0);
     for(int irow=0; irow<3; irow++) {
@@ -85,9 +233,6 @@ using namespace std;
    }
    std::cout << clmat1 << std::endl;
    std::cout << clmat2 << std::endl;
-
-
-
 
   } //end of ::Analyze()
   DEFINE_FWK_MODULE(EcalObjectAnalyzer)
