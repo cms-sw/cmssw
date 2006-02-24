5ae065e19cbad572ebc2d7b8fbe440c098978e05
Lindsey Gray <sha1-3ec7abf2a9e8e001c55b41ebd09d9de6b752a31e@cern.ch>
2006-02-24 05:35:38 +0000

    Added CLCTDigi and ALCTDigi to dictionary

--- DataFormats/CSCDigi/src/classes.h
+++ DataFormats/CSCDigi/src/classes.h
@@ -6,6 +6,10 @@
 #include <DataFormats/CSCDigi/interface/CSCStripDigiCollection.h>
 #include <DataFormats/CSCDigi/interface/CSCComparatorDigi.h>
 #include <DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h>
+#include <DataFormats/CSCDigi/interface/CSCCLCTDigi.h>
+#include <DataFormats/CSCDigi/interface/CSCCLCTDigiCollection.h>
+#include <DataFormats/CSCDigi/interface/CSCALCTDigi.h>
+#include <DataFormats/CSCDigi/interface/CSCALCTDigiCollection.h>
 #include <DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h>
 #include <DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h>
 #include <FWCore/EDProduct/interface/Wrapper.h>
@@ -19,10 +23,14 @@ namespace{
   CSCRPCDigi  cRD_;
   CSCStripDigi cSD_;
   CSCComparatorDigi cCD_;
+  CSCCLCTDigi cCLCTD_;
+  CSCALCTDigi cALCTD_;
   CSCCorrelatedLCTDigi cCorLCTD_;
 
   CSCWireDigi::PersistentPacking ppWD_;
   CSCRPCDigi::PersistentPacking ppRD_;
+  CSCCLCTDigi::PersistentPacking ppCLCTD_;
+  CSCALCTDigi::PersistentPacking ppALCTD_;
   CSCCorrelatedLCTDigi::PersistentPacking ppCorLCTD_;
   CSCStripDigi::theStripDigi ppSD_;
   CSCComparatorDigi::theComparatorDigi ppCD_; 
@@ -31,24 +39,32 @@ namespace{
   std::vector<CSCRPCDigi>   vRD_;
   std::vector<CSCStripDigi>  vSD_;
   std::vector<CSCComparatorDigi>  vCD_;
+  std::vector<CSCCLCTDigi> vCLCTD_;
+  std::vector<CSCALCTDigi> vALCTD_;
   std::vector<CSCCorrelatedLCTDigi> vCorLCTD_;
 
   std::vector<std::vector<CSCWireDigi> >  vvWD_; 
   std::vector<std::vector<CSCRPCDigi>  >  vvRD_;
   std::vector<std::vector<CSCStripDigi> >  vvSD_; 
-  std::vector<std::vector<CSCComparatorDigi> >  vvCD_; 
+  std::vector<std::vector<CSCComparatorDigi> >  vvCD_;
+  std::vector<std::vector<CSCCLCTDigi> > vvCLCTD_;
+  std::vector<std::vector<CSCALCTDigi> > vvALCTD_;
   std::vector<std::vector<CSCCorrelatedLCTDigi> > vvCorLCTD_;
 
   CSCWireDigiCollection clWD_;
   CSCRPCDigiCollection  clRD_;
   CSCStripDigiCollection clSD_;
   CSCComparatorDigiCollection clCD_;
+  CSCCLCTDigiCollection clCLCTD_;
+  CSCALCTDigiCollection clALCTD_;
   CSCCorrelatedLCTDigiCollection clCorLCTD_;
     
   edm::Wrapper<CSCWireDigiCollection> wWD_;
   edm::Wrapper<CSCRPCDigiCollection> wRD_;
   edm::Wrapper<CSCStripDigiCollection> wSD_;
   edm::Wrapper<CSCComparatorDigiCollection> wCD_;
+  edm::Wrapper<CSCCLCTDigiCollection> wCLCTD_;
+  edm::Wrapper<CSCALCTDigiCollection> wALCTD_;
   edm::Wrapper<CSCCorrelatedLCTDigiCollection> wCorLCTD_;
 
   }
