4a71d503d7adf942a219de75d85ee7c3cf4abf73
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2006-02-24 09:42:33 +0000

    Added needed methods

--- RecoVertex/VertexPrimitives/interface/DummyRecTrack.h
+++ RecoVertex/VertexPrimitives/interface/DummyRecTrack.h
@@ -1,13 +1,27 @@
 #ifndef _DummyRecTrack_H_
 #define _DummyRecTrack_H_
 
+#include "DataFormats/TrajectoryState/interface/TrackCharge.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+
+
 class DummyRecTrack {
 
 public:
 
-  DummyRecTrack() {}
+  DummyRecTrack(const TrackCharge& aCharge, const TrajectoryStateOnSurface aTsos): 
+    theCharge(aCharge), theTsos(aTsos){}
 
   ~DummyRecTrack() {}
+  
+  TrackCharge charge() const {return theCharge;}
+  bool operator== (const DummyRecTrack & a) const {return true;}
+  TrajectoryStateOnSurface impactPointState() const {return theTsos;} 
+
+private:
+
+  TrackCharge theCharge;
+  TrajectoryStateOnSurface theTsos;
 
 };
 
