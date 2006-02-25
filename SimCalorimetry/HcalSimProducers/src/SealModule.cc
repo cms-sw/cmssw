5db61e864817d100f0eb62b7a7838a03fc1dff08
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-02-25 00:30:11 +0000

    publish the hitAnalyzer

--- SimCalorimetry/HcalSimProducers/src/SealModule.cc
+++ SimCalorimetry/HcalSimProducers/src/SealModule.cc
@@ -2,9 +2,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalTrigPrimRecHitProducer.h"
+#include "SimCalorimetry/HcalSimProducers/src/HcalHitAnalyzer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalDigiProducer)
 DEFINE_ANOTHER_FWK_MODULE(HcalTrigPrimRecHitProducer)
+DEFINE_ANOTHER_FWK_MODULE(HcalHitAnalyzer)
 
