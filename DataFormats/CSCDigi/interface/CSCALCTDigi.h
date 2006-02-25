b0be3ec46d069bbf2c6123c7d8d85f0ca735accc
Nikolay Terentyev <sha1-7363ba4fe20967489bddf3505d82191852cbf6a9@cern.ch>
2006-02-25 03:51:24 +0000

    To comply with ALCT format

--- DataFormats/CSCDigi/interface/CSCALCTDigi.h
+++ DataFormats/CSCDigi/interface/CSCALCTDigi.h
@@ -5,8 +5,8 @@
  *
  * Digi for ALCT trigger primitives. 
  *
- * $Date$
- * $Revision$
+ * $Date:$
+ * $Revision:$
  *
  * \author N. Terentiev, CMU
  */
@@ -20,21 +20,29 @@ public:
   /// Enum, structures
 
       /// Length of packed fields
-  enum packing{trknmb_s     = 2, // track number (1,2)
-               keywire_s   = 7, // Key Wire group in layer 3
-               bx_s        = 4, // BX
-               quality_s   = 2, // Quality of pattern
-               pattern_s   = 2, // Pattern number  
-	       valid_s     = 1  // ALCT validity (1- valid ALCT)
+  enum packing{
+                valid_s     = 1, // ALCT validity (1 - valid ALCT and bits 
+                                 // below are meaningful)
+                quality_s   = 2, // Quality of a pattern
+                accel_s     = 1, // 1-Accelerator pattern, 0-CollisionA or
+                                 // Collision B pattern
+                pattern_s  = 1, // 1-CollisionB pattern if accel_s=0,
+                                 // 0-CollisionA pattern if accel_s=0 
+                keywire_s   = 7, // Key Wire group
+                bx_s        = 5, // BX - five low bits of BXN counter 
+                                 // tagged by the ALCT
+                trknmb_s    = 2 // track number (1,2), not a part of ALCT
+                                 // data format
   };
       /// The packed digi content  
   struct PackedDigiType {
-    unsigned int trknmb     : trknmb_s;
+    unsigned int valid     : valid_s;
+    unsigned int quality   : quality_s;
+    unsigned int accel     : accel_s;
+    unsigned int pattern   : pattern_s;
     unsigned int keywire   : keywire_s;
     unsigned int bx        : bx_s;
-    unsigned int quality   : quality_s;
-    unsigned int pattern   : pattern_s;
-    unsigned int valid     : valid_s;
+    unsigned int trknmb    : trknmb_s;
   };
 
       /// The packed data as seen by the persistency - should never be used
@@ -46,7 +54,8 @@ public:
 
   /// Constructors
 
-  explicit CSCALCTDigi (int trknmb, int keywire,int bx, int quality, int pattern, int valid);  
+//  explicit CSCALCTDigi (int trknmb, int keywire,int bx, int quality, int pattern, int valid);  // obsolete
+  explicit CSCALCTDigi (int valid, int quality, int accel, int pattern, int keywire, int bx, int trknmb); 
   explicit CSCALCTDigi (ChannelType channel); /// from channel
   CSCALCTDigi (PackedDigiType packed_value);  /// from a packed value
   CSCALCTDigi (const CSCALCTDigi& digi);      /// copy
@@ -63,19 +72,20 @@ public:
       /// the channel identifier and the digi number packed together
   ChannelType channel() const ;
 
-      /// return track number number
-  int getTrknmb() const;
+      /// return ALCT validity
+  int getValid() const;
+      /// return quality
+  int getQuality() const;
+      /// return accel
+  int getAccel() const;
+      /// return pattern
+  int getPattern() const;
       /// return key wire group
   int getKwire() const;
       /// return BX
   int getBx() const;
-      /// return quality
-  int getQuality() const;
-      /// return pattern
-  int getPattern() const;
-      /// return ALCT validity
-  int getValid() const;
-
+      /// return track number number
+  int getTrknmb() const;
 
   /// Prints
 
@@ -89,17 +99,19 @@ private:
 
   /// Set, access, repack
 
-  void set(int trknmb, int keywire,int bx, int quality, int pattern, int valid);      /// set data words
+//  void set(int trknmb, int keywire,int bx, int quality, int pattern, int valid);      /// set data words
+  void set(int valid, int quality, int accel, int pattern, int keywire, int bx, int trknmb); /// set data words
   void setData(PackedDigiType p);     /// set from a PackedDigiType
   PackedDigiType* data();             /// access to the packed data
   const PackedDigiType* data() const; /// const access to the packed data
   struct ChannelPacking {
-    unsigned int trknmb     : trknmb_s;
+    unsigned int valid     : valid_s;
+    unsigned int quality   : quality_s;
+    unsigned int accel     : accel_s;
+    unsigned int pattern   : pattern_s;
     unsigned int keywire   : keywire_s;
     unsigned int bx        : bx_s;
-    unsigned int quality   : quality_s;
-    unsigned int pattern   : pattern_s;
-    unsigned int valid     : valid_s;
+    unsigned int trknmb    : trknmb_s;
   };                                 /// repack the channel number to an int
 
   PersistentPacking persistentData;
@@ -109,11 +121,12 @@ private:
 #include<iostream>
   /// needed by COBRA
 inline std::ostream & operator<<(std::ostream & o, const CSCALCTDigi& digi) {
-  return o << " " << digi.getTrknmb()
+  return o << " " << digi.getValid()
+           << " " << digi.getQuality()
+           << " " << digi.getAccel()
+           << " " << digi.getPattern()
            << " " << digi.getKwire()
            << " " << digi.getBx()
-           << " " << digi.getQuality()
-           << " " << digi.getPattern()
-	   << " " << digi.getValid();
+           << " " << digi.getTrknmb();
 }
 #endif
