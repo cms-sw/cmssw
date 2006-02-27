fffaa58540e1933a60f07066c71052a1a8b21265
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-02-27 08:17:09 +0000

    SiStripNoises added

--- CondFormats/SiStripObjects/src/classes.h
+++ CondFormats/SiStripObjects/src/classes.h
@@ -1,13 +1,18 @@
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
 #include "CondFormats/SiStripObjects/interface/SiStripReadoutCabling.h"
 #include "CondFormats/SiStripObjects/interface/SiStripControlCabling.h"
 // Declaration of the iterator (necessary for the generation of the dictionary)
 
 template std::vector<SiStripPedestals::SiStripData>::iterator;
 template std::vector<SiStripPedestals::SiStripData>::const_iterator;
-
 template std::map< uint32_t, SiStripPedestalsVector>::iterator;
 template std::map< uint32_t, SiStripPedestalsVector>::const_iterator;
+
+template std::vector<SiStripNoises::SiStripData>::iterator;
+template std::vector<SiStripNoises::SiStripData>::const_iterator;
+template std::map< uint32_t, SiStripNoiseVector>::iterator;
+template std::map< uint32_t, SiStripNoiseVector>::const_iterator;
 
 template std::vector<std::pair<uint32_t, unsigned short> >::iterator;
 template std::vector<std::pair<uint32_t, unsigned short> >::const_iterator;
