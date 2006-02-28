8e14d0bef49cc199961556dafc53407c0baa9988
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-02-28 15:54:01 +0000

    chamged to new name

---
+++ CondFormats/CSCObjects/interface/CSCNoiseMatrix.h
@@ -0,0 +1,20 @@
+#ifndef CSCNoiseMatrix_h
+#define CSCNoiseMatrix_h
+
+#include <vector>
+
+class CSCNoiseMatrix{
+ public:
+  CSCNoiseMatrix();
+  ~CSCNoiseMatrix();
+  
+  struct Item{
+    float tmp0_1,tmp0_2,tmp0_3,tmp0_4,tmp0_5,tmp0_6,tmp0_7,tmp0_8,tmp0_9,tmp0_10,tmp0_11,tmp0_12;
+    float tmp1_1,tmp1_2,tmp1_3,tmp1_4,tmp1_5,tmp1_6,tmp1_7,tmp1_8,tmp1_9,tmp1_10,tmp1_11,tmp1_12;
+  };
+  
+  std::vector<Item> matrix;
+};
+
+#endif
+
