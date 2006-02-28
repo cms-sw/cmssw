4f9bf4e0858649110eca4ab0e54c67995c973c6f
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-02-28 01:05:13 +0000

    define the DigiAnalyzer

--- SimCalorimetry/HcalSimProducers/src/SealModule.cc
+++ SimCalorimetry/HcalSimProducers/src/SealModule.cc
@@ -3,10 +3,12 @@
 #include "SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalTrigPrimRecHitProducer.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalHitAnalyzer.h"
+#include "SimCalorimetry/HcalSimProducers/src/HcalDigiAnalyzer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalDigiProducer)
 DEFINE_ANOTHER_FWK_MODULE(HcalTrigPrimRecHitProducer)
 DEFINE_ANOTHER_FWK_MODULE(HcalHitAnalyzer)
+DEFINE_ANOTHER_FWK_MODULE(HcalDigiAnalyzer)
 
