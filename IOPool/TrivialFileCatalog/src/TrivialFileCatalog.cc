3f5d8efda7b3d257019f9b544d49b159de12a6be
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2006-02-28 13:27:08 +0000

    * BuildFile, TrivialFileCatalog.cc, module.cc, TrivialFileCatalog.h: first import to repository.

---
+++ IOPool/TrivialFileCatalog/src/TrivialFileCatalog.cc
@@ -0,0 +1,313 @@
+/* 
+   Concrete implementation of a FileCatalog.
+   Author: Giulio.Eulisse@cern.ch
+ */
+
+#include <set>
+#include <exception>
+#ifndef POOL_TRIVIALFILECATALOG_H
+#include "IOPool/TrivialFileCatalog/interface/TrivialFileCatalog.h"
+#endif
+#include "POOLCore/PoolMessageStream.h"
+#include "FileCatalog/FCException.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <SealBase/StringList.h>
+#include <SealBase/StringOps.h>
+#include <SealBase/DebugAids.h>
+
+pool::TrivialFileCatalog::TrivialFileCatalog ()
+    : m_connectionStatus (false),
+      m_prefix ("/localscratch/data/"),
+      m_fileType ("ROOT_All"),
+      m_configFilename ("PoolCatalogConfig.txt")
+{
+    if (getenv ("POOL_CATALOG"))
+    {
+	m_configFilename = getenv ("POOL_CATALOG");
+	if (m_configFilename.find ("trivialcatalog_file:") != std::string::npos)
+	{
+	    m_configFilename = m_configFilename.erase (0, 
+						       m_configFilename.find ("/") + 1);	
+	}
+	else
+	{
+	    throw FCTransactionException
+		("TrivialFileCatalog::TrivialFileCatalog",
+		 "Malformed url for file catalog configuration"); 
+	}
+	
+    }
+
+    std::cerr << m_configFilename << std::endl;    
+    
+    std::ifstream configFile;
+    configFile.open (m_configFilename.c_str ());
+
+    
+    PoolMessageStream trivialLog ("TrivialFileCatalog", seal::Msg::Nil );
+    trivialLog << seal::Msg::Info
+	       << "Using catalog configuration " 
+	       << m_configFilename << seal::endmsg;
+
+    char buffer[4096];
+    ASSERT (configFile.good ());
+    ASSERT (configFile.is_open ());
+    
+    while (! configFile.eof ())
+    {
+	configFile.getline (buffer, 4096);
+	std::string line = buffer;
+	
+	if (line != "")
+	{
+	    seal::StringList tokens = seal::StringOps::split (line, "=");
+
+	    if (tokens.size () != 2)
+	    {
+		throw FCTransactionException
+		    ("TrivialFileCatalog::TrivialFileCatalog",
+		     "Wrong configuration file"); 
+	    }	
+	   
+	    std::string parameter = tokens[0];
+	    std::string value = tokens[1];
+	
+	    if (parameter == "prefix")
+	    {
+		m_prefix = value;
+	    }
+	    else if (parameter == "type")
+	    {
+		m_fileType = value;
+	    }
+	}
+    }    
+}
+
+pool::TrivialFileCatalog::~TrivialFileCatalog ()
+{
+}
+
+
+void
+pool::TrivialFileCatalog::connect ()
+{
+    try
+    {
+	PoolMessageStream trivialLog( "TrivialFileCatalog", seal::Msg::Nil );
+	trivialLog << seal::Msg::Info<< "Connecting to the catalog"<<seal::endmsg;
+    }
+    catch (seal::Exception& e)
+    {
+	throw FCconnectionException("TrivialFileCatalog::connect",e.message());
+    }
+    catch( std::exception& er)
+    {
+	throw FCconnectionException("TrivialFileCatalog::connect",er.what());
+    }
+}
+
+void
+pool::TrivialFileCatalog::disconnect () const
+{
+}
+
+void
+pool::TrivialFileCatalog::start () const
+{
+}
+
+void
+pool::TrivialFileCatalog::commit (FileCatalog::CommitMode /*cm=FileCatalog::REFRESH*/) const
+{
+}
+
+void
+pool::TrivialFileCatalog::rollback () const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::rollback",
+	 "Trivial catalogs cannot rollback");
+}
+  
+void
+pool::TrivialFileCatalog::registerPFN (const std::string& /*pfn*/, 
+				       const std::string& /*filetype*/,
+				       FileCatalog::FileID& /*fid*/ ) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::registerPFN",
+	 "It does not make sense to register PFN for TriviaFileCatalogs");    
+}   
+
+void
+pool::TrivialFileCatalog::registerLFN (const std::string& /*pfn*/, 
+				       const std::string& /*lfn*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::registerLFN",
+	 "It does not make sense to register LFN for TriviaFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::addReplicaPFN (const std::string& /*pfn*/, 
+					 const std::string& /*rpf*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::registerLFN",
+	 "It does not make sense to register PFN for TriviaFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::addPFNtoGuid (const FileCatalog::FileID& /*guid*/, 
+					const std::string& /*pf*/, 
+					const std::string& /*filetype*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::addPFNtoGuid",
+	 "It does not make sense to register replicas for TriviaFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::renamePFN (const std::string& /*pfn*/, 
+				     const std::string& /*newpfn*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::renamePFN",
+	 "It does not make sense to rename PFNs for TrivialFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::lookupFileByPFN (const std::string& /*pfn*/, 
+					   FileCatalog::FileID& /*fid*/, 
+					   std::string& /*ftype*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::lookupFileByPFN",
+	 "For the time being this is not implemented. If the mapping between"
+	 "GUID/LFN and PFN is invertible this ca be implemented as well.");    
+}
+
+void
+pool::TrivialFileCatalog::lookupFileByLFN (const std::string& lfn, 
+					   FileCatalog::FileID& fid) const
+{
+    // GUID (FileID) and lfn are the same under TrivialFileCatalog
+    fid = lfn;    
+}
+
+void
+pool::TrivialFileCatalog::lookupBestPFN (const FileCatalog::FileID& fid, 
+					 const FileCatalog::FileOpenMode& /*omode*/,
+					 const FileCatalog::FileAccessPattern& /*amode*/,
+					 std::string& pfn,
+					 std::string& filetype) const
+{
+    filetype = m_fileType;    
+    pfn = m_prefix + fid;    
+} 
+
+void
+pool::TrivialFileCatalog::insertPFN (PFNEntry& /*pentry*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::insertPFN",
+	 "It does not make sense to insert PFNs for TrivialFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::insertLFN (LFNEntry& /*lentry*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::insertLFN",
+	 "It does not make sense to insert LFNs for TrivialFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::deletePFN (const std::string& /*pfn*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::deletePFN",
+	 "It does not make sense to delete PFNs for TrivialFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::deleteLFN (const std::string& /*lfn*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::deleteLFN",
+	 "It does not make sense to delete LFNs for TrivialFileCatalogs");    
+}
+
+void
+pool::TrivialFileCatalog::deleteEntry (const FileCatalog::FileID& /*guid*/) const
+{
+    throw FCTransactionException
+	("TrivialFileCatalog::deleteEntry",
+	 "It does not make sense to delete GUIDs for TrivialFileCatalogs");    
+}
+
+bool
+pool::TrivialFileCatalog::isReadOnly () const
+{
+    return true;    
+}
+ 
+bool
+pool::TrivialFileCatalog::retrievePFN (const std::string& query, 
+				       FCBuf<PFNEntry>& buf, 
+				       const size_t& /*start*/ )
+{
+    // The only query supported is lfn='something' or pfn='something'
+    // No spaces allowed in something.
+    std::cerr << "Query: " << query << std::endl;
+    
+    seal::StringList tokens = seal::StringOps::split (query, "=");
+    
+    if (tokens.size () != 2)
+    {
+	throw FCTransactionException
+	    ("TrivialFileCatalog::retrievePFN",
+	     "malformed query. the only supported one is lfname='something'"
+	     " or guid='something'");
+    }
+    
+    if (tokens[0] != "lfname" 
+	&& tokens[0] != "guid")
+    {
+	throw FCTransactionException
+	    ("TrivialFileCatalog::retrievePFN",
+	     "malformed query. the only supported one is lfname='something'"
+	     " or guid='something'");    
+    }
+    
+    std::string lfn = seal::StringOps::remove (tokens[1], "'");
+    
+    
+    buf.push_back (PFNEntry(m_prefix + lfn, 
+			    lfn, 
+			    m_fileType));    
+    return true;    
+}
+
+bool
+pool::TrivialFileCatalog::retrieveLFN (const std::string& /*query*/, 
+				       FCBuf<LFNEntry>& /*buf*/, 
+				       const size_t& /*start*/)
+{    
+    throw FCTransactionException
+	("TrivialFileCatalog::retrieveGuid",
+	 "Cannot retrieve GUIDs with TrivialCatalogs");
+}
+
+bool
+pool::TrivialFileCatalog::retrieveGuid (const std::string& /*query*/, 
+					FCBuf<FileCatalog::FileID>& /*buf*/, 
+					const size_t& /*start*/)
+{   
+    throw FCTransactionException
+	("TrivialFileCatalog::retrieveGuid",
+	 "Cannot retrieve GUIDs with TrivialCatalogs");
+}
---
+++ IOPool/TrivialFileCatalog/src/module.cc
@@ -0,0 +1,10 @@
+#include "IOPool/TrivialFileCatalog/interface/TrivialFileCatalog.h"
+#include "FileCatalog/FCImplPluginFactory.h"
+#include "FileCatalog/FCMetaImplPluginFactory.h"
+#include "PluginManager/ModuleDef.h"
+
+namespace pool
+{
+DEFINE_SEAL_MODULE();
+DEFINE_SEAL_PLUGIN( FCImplPluginFactory, TrivialFileCatalog, "trivialcatalog");
+} // End of namespace pool
