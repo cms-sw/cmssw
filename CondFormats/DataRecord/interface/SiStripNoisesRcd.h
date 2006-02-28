c504d62d1a009e836ec8ea61a28eab19a1da6f4e
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-02-28 09:52:04 +0000

    Added SiStripNoises object records

---
+++ CondFormats/DataRecord/interface/SiStripNoisesRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormats_SiStripNoisesRcd_h
+#define CondFormats_SiStripNoisesRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripNoisesRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripNoisesRcd> {};
+
+#endif
