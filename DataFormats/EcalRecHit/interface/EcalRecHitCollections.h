0bef09e1600ae8cb77efb36620f438ea021a0236
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-02-28 12:27:09 +0000

    added more edm::Ref types to dictionary

--- DataFormats/EcalRecHit/interface/EcalRecHitCollections.h
+++ DataFormats/EcalRecHit/interface/EcalRecHitCollections.h
@@ -4,10 +4,18 @@
 #include "FWCore/EDProduct/interface/SortedCollection.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
 #include "DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h"
-
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefVector.h"
+#include "DataFormats/Common/interface/RefProd.h"
 
 typedef edm::SortedCollection<EcalRecHit> EcalRecHitCollection;
-typedef edm::SortedCollection<EcalUncalibratedRecHit> EcalUncalibratedRecHitCollection;
+typedef edm::Ref<EcalRecHitCollection> EcalRecHitRef;
+typedef edm::RefVector<EcalRecHitCollection> EcalRecHitRefs;
+typedef edm::RefProd<EcalRecHitCollection> EcalRecHitsRef;
 
+typedef edm::SortedCollection<EcalUncalibratedRecHit> EcalUncalibratedRecHitCollection;
+typedef edm::Ref<EcalUncalibratedRecHitCollection> EcalUncalibratedRecHitRef;
+typedef edm::RefVector<EcalUncalibratedRecHitCollection> EcalUncalibratedRecHitRefs;
+typedef edm::RefProd<EcalUncalibratedRecHitCollection> EcalUncalibratedRecHitsRef;
 
 #endif
--- DataFormats/EcalRecHit/src/classes.h
+++ DataFormats/EcalRecHit/src/classes.h
@@ -10,16 +10,18 @@ namespace {
     std::vector<EcalUncalibratedRecHit> _hitVect;
     edm::SortedCollection<EcalUncalibratedRecHit> _theEURsc;
     EcalUncalibratedRecHitCollection _theEURHitCollection;
+    edm::Wrapper< EcalUncalibratedRecHitCollection > _EURHitProd;
+    EcalUncalibratedRecHitRef _EURHitRef;
+    EcalUncalibratedRecHitRefs _EURHitRefs;
+    EcalUncalibratedRecHitsRef _EURHitsRef;
 
     EcalRecHit _aRecHit;
     std::vector<EcalRecHit> _ERHitVect;
     edm::SortedCollection<EcalRecHit> _theERsc;
     EcalRecHitCollection _theERHitCollection;
-
-
-    edm::Wrapper< EcalUncalibratedRecHitCollection > _EURHitProd;
-    edm::Wrapper< edm::SortedCollection<EcalUncalibratedRecHit> > _theEURHitProd;
     edm::Wrapper< EcalRecHitCollection > _ERHitProd;
-    edm::Wrapper< edm::SortedCollection<EcalRecHit> > _theERHitProd;
+    EcalRecHitRef _ERHitRef;
+    EcalRecHitRefs _ERHitRefs;
+    EcalRecHitsRef _ERHitsRef;
   }
 }
