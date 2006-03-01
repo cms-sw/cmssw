a0ab104b0ed6bfce4fed2d18099ad118ff0fb690
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-03-01 18:16:03 +0000

    Documentation

--- Geometry/CommonDetUnit/interface/GeomDet.h
+++ Geometry/CommonDetUnit/interface/GeomDet.h
@@ -1,22 +1,25 @@
 #ifndef CommonDet_GeomDet_H
 #define CommonDet_GeomDet_H
 
+/** \class GeomDet
+ *  Base class for GeomDetUnit and for composite GeomDet s. 
+ *
+ *  $Date: $
+ *  $Revision: $
+ */
+
+
 #include "Geometry/Surface/interface/BoundPlane.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
 #include <vector>
 
-class Topology;
-class GeomDetType;
 class AlignmentPositionError;
-
-/** Base class for GeomDetUNit and for composite GeomDets, used by RecHit.
- */
 
 class GeomDet {
 public:
 
-  /// empty constructor, requires a call to setSurface from derived class constructor
+  // empty constructor, requires a call to setSurface from derived class constructor
   // GeomDet();
 
   explicit GeomDet( BoundPlane* plane);
@@ -30,8 +33,8 @@ public:
 
   virtual DetId geographicalId() const = 0;
 
-  /** Return pointer to alignment errors (defaults to "null" if not 
-   *  reimplemented in the derived classes) */
+  /// Return pointer to alignment errors. 
+  /// Defaults to "null" if not reimplemented in the derived classes.
   virtual AlignmentPositionError* alignmentPositionError() const { return 0;}
 
   /// Returns direct components, if any
@@ -46,29 +49,29 @@ private:
   ReferenceCountingPointer<BoundPlane>  thePlane;
   AlignmentPositionError*               theAlignmentPositionError;
 
-  // alignment part of interface available only to friend 
+  /// Alignment part of interface, available only to friend 
   friend class AlignableDetUnit;
 
-  /// Relative displacement (with respect to current position)
+  /// Relative displacement (with respect to current position).
   /// Does not move components (if any).
   void move( const GlobalVector& displacement);
 
-  /// Relative rotation (with respect to current orientation)
+  /// Relative rotation (with respect to current orientation).
   /// Does not move components (if any).
   void rotate( const Surface::RotationType& rotation);
 
-  /** Replaces the current position and rotation with new ones; actually replaces the 
-   *  surface with a new surface.
-   *  Does not move components (if any).
-   */
+  /// Replaces the current position and rotation with new ones.
+  /// actually replaces the surface with a new surface.
+  /// Does not move components (if any).
+   
   void setPosition( const Surface::PositionType& position, 
 		    const Surface::RotationType& rotation);
 
-  /** create the AlignmentPositionError for this Det if not existing yet,
-   *  or replace the existing one by the given one. For adding, use the
-   *  +=,-=  methods of the AlignmentPositionError
-   *  Does not affect the AlignmentPositionError of components (if any).
-   */
+  /// create the AlignmentPositionError for this Det if not existing yet,
+  /// or replace the existing one by the given one. For adding, use the
+  /// +=,-=  methods of the AlignmentPositionError
+  /// Does not affect the AlignmentPositionError of components (if any).
+  
   virtual void setAlignmentPositionError (const AlignmentPositionError& ape); 
 
 };
