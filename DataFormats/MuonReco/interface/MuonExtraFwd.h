930c5baf5bb1029771055cf37f44844e4c33945e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-01 14:47:20 +0000

    fixed dictionary and documentation

--- DataFormats/MuonReco/interface/MuonExtraFwd.h
+++ DataFormats/MuonReco/interface/MuonExtraFwd.h
@@ -2,6 +2,7 @@
 #define MuonReco_MuonExtraFwd_h
 #include <vector>
 #include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
 #include "DataFormats/Common/interface/RefVector.h"
 
 namespace reco {
@@ -10,6 +11,8 @@ namespace reco {
   typedef std::vector<MuonExtra> MuonExtraCollection;
   /// presistent reference to a MuonExtra
   typedef edm::Ref<MuonExtraCollection> MuonExtraRef;
+  /// presistent reference to a MuonExtra collection
+  typedef edm::RefProd<MuonExtraCollection> MuonExtrasRef;
   /// vector of references to MuonExtra objects all in the same collection
   typedef edm::RefVector<MuonExtraCollection> MuonExtraRefs;
   /// iterator over a vector of references to MuonExtra objects all in the same collection
--- DataFormats/MuonReco/src/classes.h
+++ DataFormats/MuonReco/src/classes.h
@@ -8,6 +8,7 @@ namespace {
     std::vector<reco::MuonExtra> v2;
     edm::Wrapper<std::vector<reco::MuonExtra> > c2;
     edm::Ref<std::vector<reco::MuonExtra> > r2;
+    edm::RefProd<std::vector<reco::MuonExtra> > rp2;
     edm::RefVector<std::vector<reco::MuonExtra> > rv2;
 
     reco::TrackExtension<reco::MuonExtraRef> te1;
@@ -15,6 +16,7 @@ namespace {
     std::vector<reco::Muon> v1;
     edm::Wrapper<std::vector<reco::Muon> > c1;
     edm::Ref<std::vector<reco::Muon> > r1;
+    edm::RefProd<std::vector<reco::Muon> > rp1;
     edm::RefVector<std::vector<reco::Muon> > rv1;
   }
 }
