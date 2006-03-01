de09b3b5bf6805ed7184fa455923b1fab7476374
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-01 14:47:31 +0000

    added documentation, fixed dictionaries

--- DataFormats/VertexReco/interface/VertexFwd.h
+++ DataFormats/VertexReco/interface/VertexFwd.h
@@ -2,15 +2,18 @@
 #define VertexReco_VertexFwd_h
 #include <vector>
 #include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
 #include "DataFormats/Common/interface/RefVector.h"
 
 namespace reco {
   class Vertex;
-  /// a collection of Vertex objects
+  /// collection of Vertex objects
   typedef std::vector<Vertex> VertexCollection;
-  /// a persistent reference to a Vertex
+  /// persistent reference to a Vertex
   typedef edm::Ref<VertexCollection> VertexRef;
-  /// a vector of references to Vertex objects in the same collection
+  /// persistent reference to a Vertex
+  typedef edm::RefProd<VertexCollection> VertexsRef;
+  /// vector of references to Vertex objects in the same collection
   typedef edm::RefVector<VertexCollection> VertexRefs;
   /// iterator over a vector of references to Vertex objects in the same collection
   typedef VertexRefs::iterator vertex_iterator;
--- DataFormats/VertexReco/src/classes.h
+++ DataFormats/VertexReco/src/classes.h
@@ -8,6 +8,7 @@ namespace {
     std::vector<reco::Vertex> v1;
     edm::Wrapper<std::vector<reco::Vertex> > wc1;
     edm::Ref<std::vector<reco::Vertex> > r1;
+    edm::RefProd<std::vector<reco::Vertex> > rp1;
     edm::RefVector<std::vector<reco::Vertex> > rv1;
   }
 }
