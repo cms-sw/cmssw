2a4896ed78699264155df61a1e3bd70033b317fa
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-02 17:20:26 +0000

    wrong include

--- DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h
+++ DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h
@@ -1,7 +1,7 @@
 #ifndef DATAFORMATS_TRAJECTORYSEED_TRAJECTORYSEEDCOLLECTION_h
 #define DATAFORMATS_TRAJECTORYSEED_TRAJECTORYSEEDCOLLECTION_h
 
-#include "DataFormats/TrajectorySeed/interface/RoadSearchCloud.h"
+#include "DataFormats/Common/interface/OwnVector.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
 
 typedef edm::OwnVector<TrajectorySeed> TrajectorySeedCollection;
