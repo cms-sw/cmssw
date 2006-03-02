80c3cb28dfa82fac96e57eb7675c6e96792c1117
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-03-02 23:42:57 +0000

    Queries to create a view of POOL-ORA EcalPedestals from the (new)online schema

--- OnlineDB/EcalCondDB/sql/cms_payload_o2o.sql
+++ OnlineDB/EcalCondDB/sql/cms_payload_o2o.sql
@@ -1,71 +1,53 @@
+/* query to fill ECALPEDESTALS table */
 
-
-
-CREATE OR REPLACE PROCEDURE payload_o2o AS
-
-obj  VARCHAR2(100);
-id   NUMBER;
-cnt1 NUMBER;
-cnt2 NUMBER;
-
-BEGIN
-  -- EcalPedestals
-  obj  := 'EcalPedestals';
-  SELECT count(*) INTO cnt1 FROM cms_streamuser.ecalpedestals;
-  SELECT max(last_id) INTO id FROM o2o_log WHERE object_name = obj;
-
-  IF id IS NULL THEN
-    -- it is the first transfer, ensure ALL ids are transferred
-    -- use -1 because all iov_id are > 0
-    id := -1;
-  END IF;
-
-  -- execute transfer
-  INSERT INTO cms_streamuser.ecalpedestals
-  (iov_value_id, time)
   SELECT 
-    iov.iov_id, iov.run_num
+    miov.iov_id iov_value_id, 
+    riov.run_num time
   FROM 
-    location_tag ltag, run_tag rtag, run_iov iov,
-    (SELECT DISTINCT iov_id FROM mon_pedestals_dat) dat
-  WHERE ltag.tag_id = rtag.location_id 
-    AND iov.tag_id = rtag.tag_id
-    AND dat.iov_id = iov.iov_id
-    AND ltag.location='H4' 
-    AND rtag.run_type='PEDESTAL'
-    AND iov.iov_id > id
+    location_def ldef, run_type_def rdef, run_tag rtag, run_iov riov,
+    /* inline view selects the mon_run_iov with the greatest subrun_num */
+    (SELECT iov_id, run_iov_id, 
+       MAX(subrun_num) KEEP (DENSE_RANK FIRST ORDER BY subrun_num ASC)
+       FROM mon_run_iov GROUP BY iov_id, run_iov_id) miov
+  WHERE
+      miov.run_iov_id = riov.iov_id
+  AND riov.tag_id = rtag.tag_id
+  AND rdef.def_id = rtag.run_type_id
+  AND ldef.def_id = rtag.location_id
+  AND ldef.location='H4'
+  AND rdef.run_type='PEDESTAL'
+  AND rdef.config_tag='PEDESTAL-STD'
+  AND rdef.config_ver=1
 ;
-    
-  INSERT INTO cms_streamuser.ecalpedestals_value
-  (iov_value_id, pos, det_id, mean_x12, rms_x12, mean_x6, rms_x6, mean_x1, rms_x1)
+
+/* query to fill ECALPEDESTALS_ITEM table */
   SELECT 
-    dat.iov_id,
-    ROW_NUMBER() OVER (PARTITION BY dat.iov_id ORDER BY dat.logic_id ASC),
-    cv.id1,
-    dat.ped_mean_g12,
-    dat.ped_rms_g12,
-    dat.ped_mean_g6,
-    dat.ped_rms_g6,
-    dat.ped_mean_g1,
-    dat.ped_rms_g1
+    dat.iov_id iov_value_id,
+    /* Creates a column of row numbers ordered by logic_id, for std::vec */
+    ROW_NUMBER() OVER (PARTITION BY dat.iov_id ORDER BY dat.logic_id ASC) pos,
+    cv.id1 det_id,
+    dat.ped_mean_g12 mean_x12,
+    dat.ped_rms_g12 rms_x12,
+    dat.ped_mean_g6 mean_x6,
+    dat.ped_rms_g6 rms_x6,
+    dat.ped_mean_g1 mean_x1,
+    dat.ped_rms_g1 rms_x1
   FROM 
-    cms_streamuser.ecalpedestals iov, mon_pedestals_dat dat, channelview cv
-  WHERE dat.iov_id = iov.iov_value_id
+    location_def ldef, run_type_def rdef, run_tag rtag, run_iov riov,
+    /* inline view selects the mon_run_iov with the greatest subrun_num */
+    (SELECT iov_id, run_iov_id, 
+       MAX(subrun_num) KEEP (DENSE_RANK FIRST ORDER BY subrun_num ASC)
+       FROM mon_run_iov GROUP BY iov_id, run_iov_id) miov,
+    mon_pedestals_dat dat, channelview cv
+  WHERE dat.iov_id = miov.iov_id
     AND cv.logic_id = dat.logic_id
     AND cv.name='Offline_det_id'
-    AND iov.iov_value_id > id
+    AND miov.run_iov_id = riov.iov_id
+    AND riov.tag_id = rtag.tag_id
+    AND rdef.def_id = rtag.run_type_id
+    AND ldef.def_id = rtag.location_id
+    AND ldef.location='H4'
+    AND rdef.run_type='PEDESTAL'
+    AND rdef.config_tag='PEDESTAL-STD'
+    AND rdef.config_ver=1
 ;
-
-  -- update log
-  SELECT count(*) INTO cnt2 FROM cms_streamuser.ecalpedestals;
-  IF cnt2 != cnt1 THEN
-    SELECT max(iov_value_id) INTO id FROM cms_streamuser.ecalpedestals;
-    INSERT INTO o2o_log (object_name, last_id, num_transfered, transfer_time)
-      VALUES (obj, id, cnt2 - cnt1, sysdate);
-  END IF;
-
-  COMMIT;
-END;
-/
-
-SHOW ERRORS;
