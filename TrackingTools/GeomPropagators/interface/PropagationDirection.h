72ed9a4fa0f78e0e85de1985b927fdeb7eac867c
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-02 09:03:23 +0000

    forward the include

--- TrackingTools/GeomPropagators/interface/PropagationDirection.h
+++ TrackingTools/GeomPropagators/interface/PropagationDirection.h
@@ -1,7 +1,9 @@
 #ifndef CommonDet_PropagationDirection_H
 #define CommonDet_PropagationDirection_H
 
-enum PropagationDirection {oppositeToMomentum, alongMomentum, anyDirection};
+#include "DataFormats/TrajectorySeed/interface/PropagationDirection.h"
+
+//enum PropagationDirection {oppositeToMomentum, alongMomentum, anyDirection};
   
 
 #endif // CommonDet_PropagationDirection_H
