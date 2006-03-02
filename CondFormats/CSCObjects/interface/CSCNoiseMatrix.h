1e0b220c480804c7d3b1d08a2e61e0089b8df63e
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-03-02 16:08:47 +0000

    matrix has 12 elements only

--- CondFormats/CSCObjects/interface/CSCNoiseMatrix.h
+++ CondFormats/CSCObjects/interface/CSCNoiseMatrix.h
@@ -10,8 +10,7 @@ class CSCNoiseMatrix{
   ~CSCNoiseMatrix();
   
   struct Item{
-    float tmp0_1,tmp0_2,tmp0_3,tmp0_4,tmp0_5,tmp0_6,tmp0_7,tmp0_8,tmp0_9,tmp0_10,tmp0_11,tmp0_12;
-    float tmp1_1,tmp1_2,tmp1_3,tmp1_4,tmp1_5,tmp1_6,tmp1_7,tmp1_8,tmp1_9,tmp1_10,tmp1_11,tmp1_12;
+    float elem1,elem2,elem3,elem4,elem5,elem6,elem7,elem8,elem9,elem10,elem11,elem12;
   };
   
   std::map< int,std::vector<Item> > matrix;
