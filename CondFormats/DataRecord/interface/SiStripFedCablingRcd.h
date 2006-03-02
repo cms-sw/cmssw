227dd6cca6d51d0b6ad46f8d8e701a3359fda209
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-03-02 16:19:38 +0000

    added SiStripFedCablingRcd

---
+++ CondFormats/DataRecord/interface/SiStripFedCablingRcd.h
@@ -0,0 +1,7 @@
+#ifndef CondFormats_DataRecord_SiStripFedCablingRcd_H
+#define CondFormats_DataRecord_SiStripFedCablingRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class SiStripFedCablingRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripFedCablingRcd> {};
+
+#endif // CondFormats_DataRecord_SiStripFedCablingRcd_H
