fac1b98b4e11d75d3e67ea7f71571e38bb18d933
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-03 10:20:44 +0000

    added doxygen documentation

--- PhysicsTools/CandAlgos/src/CandCombiner.h
+++ PhysicsTools/CandAlgos/src/CandCombiner.h
@@ -1,6 +1,17 @@
-#ifndef PHYSICSTOOLS_CANDCOMBINER_H
-#define PHYSICSTOOLS_CANDCOMBINER_H
-// $Id: CandCombiner.h,v 1.3 2005/10/25 08:47:05 llista Exp $
+#ifndef CandAlgos_CandCombiner_h
+#define CandAlgos_CandCombiner_h
+/** \class candmodules::CandCombiner
+ *
+ * performs all possible and selected combinations
+ * of particle pairs using the TwoBoddyCombiner utility
+ *
+ * \author Luca Lista, INFN
+ *
+ * \version $Revision$
+ *
+ * $Id: Track.h,v 1.12 2006/03/01 12:23:40 llista Exp $
+ *
+ */
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "PhysicsTools/CandUtils/interface/TwoBodyCombiner.h"
@@ -15,14 +26,19 @@ namespace candmodules {
 
   class CandCombiner : public edm::EDProducer {
   public:
-    explicit CandCombiner( const edm::ParameterSet & );
+    /// constructor from parameter set
+    explicit CandCombiner( const edm::ParameterSet & );    
+    /// destructor
     ~CandCombiner();
-    
+
   private:
+    /// process an event
     void produce( edm::Event& e, const edm::EventSetup& );
-    
+    /// label vector
     std::vector<candcombiner::ConjInfo> labels_;
+    /// combiner utility
     std::auto_ptr<TwoBodyCombiner> combiner_;
+    /// labels of the two source candidate collections
     std::string source1_, source2_;
   };
 
--- PhysicsTools/CandAlgos/src/CandReducer.h
+++ PhysicsTools/CandAlgos/src/CandReducer.h
@@ -1,17 +1,37 @@
 #ifndef CandAlgos_CandReducer_h
 #define CandAlgos_CandReducer_h
-// Ported from original implementation by Chris Jones
-// $Id: CandReducer.h,v 1.4 2005/10/25 08:47:05 llista Exp $
-//
+/** \class candmodules::CandReducer
+ *
+ * Given a collectin of candidates, produced a
+ * collection of LeafCandidas identical to the
+ * source collection, but removing all daughters
+ * and all components. 
+ *
+ * This is ment to produce a "light" collection
+ * of candiadates just containing kimenatics 
+ * information for very fast analysis purpose
+ *
+ * \author Luca Lista, INFN
+ *
+ * \version $Revision$
+ *
+ * $Id$
+ *
+ */
 #include "FWCore/Framework/interface/EDProducer.h"
 
 namespace candmodules {
 
   class CandReducer : public edm::EDProducer {
   public:
+    /// constructor from parameter set
     explicit CandReducer( const edm::ParameterSet& );
+    /// destructor
     ~CandReducer();
+  private:
+    /// process one evevnt
     void produce( edm::Event& evt, const edm::EventSetup& );
+    /// label of source candidate collection
     std::string src_;
   };
 
