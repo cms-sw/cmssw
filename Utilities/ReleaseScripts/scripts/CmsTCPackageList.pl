e5cc6a093d238be6dfe476ed6ca89dd9f2f792d0
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2006-03-03 17:55:04 +0000

    move location of tag collector

--- Utilities/ReleaseScripts/scripts/CmsTCPackageList.pl
+++ Utilities/ReleaseScripts/scripts/CmsTCPackageList.pl
@@ -4,20 +4,29 @@
 #
 # David Lange, LLNL. October 18, 2005: Port to CMS Tag Collector
 #
+# Script to return a list of tags from CmsTC for a given release
+# Required option --rel
 
 use strict;
 use Getopt::Long;
 Getopt::Long::config('bundling_override');
  
 my %options;
-GetOptions(\%options,'rel=s','pass=s','user=s');
- 
+GetOptions(\%options,'rel=s');
+
+if ( !$options{'rel'} ) {
+    print "CmsTCPackageList.pl usage:\n";
+    print "    CmsTCPackageList.pl --rel <release>\n";
+    print "\n";
+    exit;
+} 
+
 my $rel= $options{'rel'} ? $options{'rel'} : die "Need a release (--rel)";
 
 my $user="cmstcreader";
 my $pass="CmsTC";
 
-open(CMSTCQUERY,"/usr/bin/wget -nv -O- 'http://${user}:${pass}\@pccms211.cern.ch:8080//CmsTagCollector/cgi-bin/CreateTagList?release=${rel}' |");
+open(CMSTCQUERY,"/usr/bin/wget -nv -O- 'http://${user}:${pass}\@cmsdoc.cern.ch/swdev/CmsTC/cgi-bin/CreateTagList?release=${rel}' |");
 
 my %tags;
 while ( <CMSTCQUERY> ) {
