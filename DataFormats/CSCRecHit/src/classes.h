838a4cc18890291b677ee6b5ff5686993aa96625
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2006-03-03 17:23:21 +0000

    first guess

---
+++ DataFormats/CSCRecHit/src/classes.h
@@ -0,0 +1,19 @@
+#include "DataFormats/CSCRec2Hit/interface/CSCRecHit2D.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2DCollection.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2D.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2DCollection.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+#include <vector>
+
+namespace{ 
+  namespace {
+    CSCRecHit2D dh;
+
+    std::vector<CSCRecHit2D>  dv;
+    
+    edm::ClonePolicy<CSCRecHit2D> dcp;
+    edm::OwnVector<CSCRecHit2D,edm::ClonePolicy<CSCRecHit2D> > dov;
+
+    CSCRecHit2DCollection dc;
+    edm::Wrapper<CSCRecHitCollection> dwc;
+  }
---
+++ DataFormats/CSCRecHit/src/classes_xml.h
@@ -0,0 +1,7 @@
+<lcgdict>
+<class name="CSCRecHit2D"/>
+<class name="std::vector<CSCRecHit2D>"/>
+<class name="edm::OwnVector<CSCRecHit2D,edm::ClonePolicy<CSCRecHit2D> >"/>
+<class name="edm::RangeMap <CSCDetId, edm::OwnVector<CSCRecHit2D,edm::ClonePolicy<CSCRecHit2D> >,edm::ClonePolicy<CSCRecHit2D> >"/>
+<class name="edm::Wrapper<edm::RangeMap <CSCDetId, edm::OwnVector<CSCRecHit2D,edm::ClonePolicy<CSCRecHit2D> >,edm::ClonePolicy<CSCRecHit2D> > >"/>
+</lcgdict>
