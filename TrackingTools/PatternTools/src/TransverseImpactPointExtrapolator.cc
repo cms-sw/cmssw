0517685971cf99d2c6042612736c46ca3e56a203
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2006-03-03 17:10:43 +0000

    Needed by TransientTrack

---
+++ TrackingTools/PatternTools/src/TransverseImpactPointExtrapolator.cc
@@ -0,0 +1,129 @@
+#include "TrackingTools/PatternTools/interface/TransverseImpactPointExtrapolator.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
+#include "TrackingTools/GeomPropagators/interface/PropagationDirection.h"
+#include "TrackingTools/GeomPropagators/interface/AnalyticalPropagator.h"
+#include "Geometry/Vector/interface/Point2DBase.h"
+#include "Geometry/Vector/interface/Vector2DBase.h"
+#include "Geometry/Surface/interface/PlaneBuilder.h"
+#include "TrackingTools/TrajectoryState/interface/FakeField.h"
+
+TransverseImpactPointExtrapolator::TransverseImpactPointExtrapolator () :
+  thePropagator(new AnalyticalPropagator(TrackingTools::FakeField::Field::field(), anyDirection)) {}
+
+TransverseImpactPointExtrapolator::TransverseImpactPointExtrapolator (const Propagator& u) :
+  thePropagator(u.clone()) 
+{
+  thePropagator->setPropagationDirection(anyDirection);
+}
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::extrapolate (const FreeTrajectoryState& fts, 
+						const GlobalPoint& vtx) const
+{
+  return doExtrapolation(fts, vtx, *thePropagator);
+}
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::extrapolate (const TrajectoryStateOnSurface tsos, 
+						const GlobalPoint& vtx) const
+{
+  if ( !tsos.isValid() )  return tsos;
+  return doExtrapolation(tsos, vtx, *thePropagator);
+}
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::extrapolate (const FreeTrajectoryState& fts, 
+						const GlobalPoint& vtx, 
+						const Propagator& u) const
+{
+  //
+  // use clone of propagator for bidirectional propagation
+  //
+  DeepCopyPointerByClone<Propagator> p(u.clone());
+  p->setPropagationDirection(anyDirection);
+
+  return doExtrapolation(fts,vtx,*p);
+} 
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::extrapolate (const TrajectoryStateOnSurface tsos, 
+						const GlobalPoint& vtx, 
+						const Propagator& u) const
+{
+  if ( !tsos.isValid() )  return tsos;
+  //
+  // use clone of propagator for bidirectional propagation
+  //
+  DeepCopyPointerByClone<Propagator> p(u.clone());
+  p->setPropagationDirection(anyDirection);
+
+  return doExtrapolation(tsos,vtx,*p);
+} 
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::doExtrapolation (const TrajectoryStateOnSurface tsos, 
+						    const GlobalPoint& vtx, 
+						    const Propagator& p) const
+{
+  //
+  // Compute tip surface
+  //
+  ReferenceCountingPointer<BoundPlane> surface = 
+    tipSurface(tsos.globalPosition(),tsos.globalMomentum(),
+	       1./tsos.transverseCurvature(),vtx);
+  //
+  // propagate
+  //
+  return p.propagate(tsos,*surface);
+}
+
+TrajectoryStateOnSurface 
+TransverseImpactPointExtrapolator::doExtrapolation (const FreeTrajectoryState& fts, 
+						    const GlobalPoint& vtx, 
+						    const Propagator& p) const
+{
+  //
+  // Compute tip surface
+  //
+  ReferenceCountingPointer<BoundPlane> surface = 
+    tipSurface(fts.position(),fts.momentum(),
+	       1./fts.transverseCurvature(),vtx);
+  //
+  // propagate
+  //
+  return p.propagate(fts,*surface);
+}
+
+ReferenceCountingPointer<BoundPlane>
+TransverseImpactPointExtrapolator::tipSurface (const GlobalPoint& position,
+					       const GlobalVector& momentum,
+					       const double& signedTransverseRadius,
+					       const GlobalPoint& vertex) const
+{
+  typedef Point2DBase<double,GlobalTag> PositionType2D;
+  typedef Vector2DBase<double,GlobalTag> DirectionType2D;
+  
+  PositionType2D x0(position.x(),position.y());
+  DirectionType2D t0(-momentum.y(),momentum.x());
+  t0 = t0/t0.mag();
+
+  PositionType2D xc(x0+signedTransverseRadius*t0);
+
+  DirectionType2D vtxDirection(xc.x()-vertex.x(),xc.y()-vertex.y());
+  double vtxDistance = vtxDirection.mag();
+
+  Surface::PositionType origin(vertex);
+  GlobalVector xLocal(vtxDirection.x()/vtxDistance,
+		      vtxDirection.y()/vtxDistance,
+		      0.);
+  if ( vtxDistance<fabs(signedTransverseRadius) )  xLocal = -xLocal;
+  GlobalVector yLocal(0.,0.,1.);
+  GlobalVector zLocal(xLocal.cross(yLocal));
+  if ( zLocal.dot(momentum)<0. ) {
+    yLocal = -yLocal;
+    zLocal = -zLocal;
+  }
+  Surface::RotationType rotation(xLocal,yLocal,zLocal);
+  return PlaneBuilder().plane(origin,rotation);
+}
