cd8b40d6ec78a0acec48e0427ca6714ae32b4f72
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-03-03 15:13:39 +0000

    utility class for constructing strings representing directory paths reflecting control system structure

---
+++ DQM/SiStripCommon/interface/SiStripControlDirPath.h
@@ -0,0 +1,52 @@
+#ifndef DQM_SiStripCommon_SiStripControlDirPath_H
+#define DQM_SiStripCommon_SiStripControlDirPath_H
+
+#include <string>
+
+using namespace std;
+
+class SiStripControlDirPath {
+  
+ public:
+
+  static unsigned short const all_ = 65535;
+
+  SiStripControlDirPath() 
+    : root_("/"),
+    top_("SiStrip"),
+    control_("ControlView"),
+    crate_("FECcrate"),
+    slot_("FECslot"),
+    ring_("FECring"),
+    addr_("CCUaddress"),
+    chan_("CCUchannel"),
+    i2c_("I2Caddress"),
+    sep_("/") {;}
+  ~SiStripControlDirPath() {;}
+  
+  inline string top() { return root_ + top_; }
+  inline string control() {return top_ + sep_ + control_; }
+  string path( // unsigned short crate = all_,
+	      unsigned short slot = all_,
+	      unsigned short ring = all_,
+	      unsigned short addr = all_,
+	      unsigned short chan = all_
+	      // unsigned short i2c = all_ 
+	      );
+  
+ private:
+  
+  string root_;
+  string top_;
+  string control_;
+  string crate_;
+  string slot_;
+  string ring_;
+  string addr_;
+  string chan_;
+  string i2c_;
+  string sep_;
+  
+};
+
+#endif // DQM_SiStripCommon_SiStripControlDirPath_H
