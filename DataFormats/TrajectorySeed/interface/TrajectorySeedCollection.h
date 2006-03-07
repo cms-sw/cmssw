3bca91c15b7c36e3ad0424d4850410e14cdc0c39
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-07 10:05:22 +0000

    tentative change

--- DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h
+++ DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h
@@ -1,10 +1,8 @@
 #ifndef DATAFORMATS_TRAJECTORYSEED_TRAJECTORYSEEDCOLLECTION_h
 #define DATAFORMATS_TRAJECTORYSEED_TRAJECTORYSEEDCOLLECTION_h
 
-#include "DataFormats/Common/interface/OwnVector.h"
-#include "DataFormats/Common/interface/NewPolicy.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
-
-typedef edm::OwnVector<TrajectorySeed, edm::NewPolicy<TrajectorySeed> > TrajectorySeedCollection;
+#include <vector>
+typedef std::vector<TrajectorySeed> TrajectorySeedCollection;
 
 #endif
