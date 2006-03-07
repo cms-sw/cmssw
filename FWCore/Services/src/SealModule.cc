f0bf754f4a10b41204b17f40e3c231d8671c0dce
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-03-07 19:47:19 +0000

    added RandomNumberGeneratorService

--- FWCore/Services/src/SealModule.cc
+++ FWCore/Services/src/SealModule.cc
@@ -4,15 +4,19 @@
 #include "FWCore/Services/src/Timing.h"
 #include "FWCore/Services/src/Memory.h"
 #include "FWCore/Services/src/LoadAllDictionaries.h"
+#include "FWCore/Services/src/RandomNumberGeneratorService.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 using edm::service::Tracer;
 using edm::service::Timing;
 using edm::service::SimpleMemoryCheck;
 using edm::service::LoadAllDictionaries;
+using edm::service::RandomNumberGeneratorService;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_SERVICE(Tracer)
 DEFINE_ANOTHER_FWK_SERVICE(Timing)
 DEFINE_ANOTHER_FWK_SERVICE(SimpleMemoryCheck)
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(LoadAllDictionaries,edm::serviceregistry::ParameterSetMaker<LoadAllDictionaries>)
+typedef edm::serviceregistry::AllArgsMaker<edm::RandomNumberGenerator,RandomNumberGeneratorService> RandomMaker;
+DEFINE_ANOTHER_FWK_SERVICE_MAKER(RandomNumberGeneratorService, RandomMaker)
