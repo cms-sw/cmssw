92a64d2e216cbd4fc4723d1de5702144a45007e9
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-03-08 17:15:51 +0000

    allow CSCZSensors to be read as conditions.

---
+++ CondFormats/DataRecord/interface/CSCZSensorsRcd.h
@@ -0,0 +1,13 @@
+#ifndef CSCZSENSORSRCD_H
+#define CSCZSENSORSRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+/* #include "FWCore/ParameterSet/interface/ParameterSet.h" */
+
+#include <iostream>
+
+class CSCZSensorsRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCZSensorsRcd> {
+    
+};
+
+#endif
