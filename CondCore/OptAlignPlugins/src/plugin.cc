a87a6562b9e1171feb8a0aa611141958399a8ef5
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-03-08 17:16:44 +0000

    "added CSCZSensors for PoolDBESSource addition"

--- CondCore/OptAlignPlugins/src/plugin.cc
+++ CondCore/OptAlignPlugins/src/plugin.cc
@@ -8,6 +8,9 @@
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignments.h"
+#include "CondFormats/OptAlignObjects/interface/CSCZSensors.h"
 #include "CondFormats/DataRecord/interface/OpticalAlignmentsRcd.h"
+#include "CondFormats/DataRecord/interface/CSCZSensorsRcd.h"
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(OpticalAlignmentsRcd,OpticalAlignments);
+REGISTER_PLUGIN(CSCZSensorsRcd,CSCZSensors);
