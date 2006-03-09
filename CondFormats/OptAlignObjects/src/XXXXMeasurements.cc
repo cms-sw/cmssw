f9a7a2e5b96113f11ecf93aee419d8f95d6989d9
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-03-09 10:27:43 +0000

    more fixes :-(

--- CondFormats/OptAlignObjects/src/XXXXMeasurements.cc
+++ CondFormats/OptAlignObjects/src/XXXXMeasurements.cc
@@ -4,11 +4,11 @@ std::ostream & operator<<(std::ostream & os, const XXXXMeasurements & r)
 {
 
   os << " There are " << r.xxxxMeasurements_.size() << " optical alignment objects." << std::endl;
-  size_t max = r.opticalAlignments_.size();
-  size_t oAi = 0;
-  while ( oAi < max ) {
-    os << "\t" << r.opticalAlignments_[oAi];
-    oAi++;
+  size_t max = r.xxxxMeasurements_.size();
+  size_t xi = 0;
+  while ( xi < max ) {
+    os << "\t" << r.xxxMeasurements_[xi];
+    xi++;
   }
   return os;
 }
