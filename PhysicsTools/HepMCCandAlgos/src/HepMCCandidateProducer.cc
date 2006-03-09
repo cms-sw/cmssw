d857ef61b09b1aeaa2c3fda349088cc254704ce2
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-09 10:40:31 +0000

    added exclusion list

--- PhysicsTools/HepMCCandAlgos/src/HepMCCandidateProducer.cc
+++ PhysicsTools/HepMCCandAlgos/src/HepMCCandidateProducer.cc
@@ -1,22 +1,50 @@
-// $Id: TrackCandidateProducer.cc,v 1.7 2006/02/28 11:43:18 llista Exp $
+// $Id: HepMCCandidateProducer.cc,v 1.1 2006/03/08 10:50:07 llista Exp $
 #include "PhysicsTools/HepMCCandAlgos/src/HepMCCandidateProducer.h"
 #include "DataFormats/HepMCCandidate/interface/HepMCCandidate.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "FWCore/Framework/interface/Handle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <CLHEP/HepPDT/DefaultConfig.hh>
+#include <CLHEP/HepPDT/TableBuilder.hh>
+#include <CLHEP/HepPDT/ParticleDataTableT.hh>
 #include <iostream>
+#include <fstream>
 using namespace edm;
 using namespace reco;
 using namespace std;
 
 HepMCCandidateProducer::HepMCCandidateProducer( const ParameterSet & p ) :
   source( p.getParameter<string>( "src" ) ),
-  stableOnly( p.getParameter<bool>( "stableOnly" ) ) {
+  pdtFileName( p.getParameter<string>( "pdtFileName" ) ),
+  stableOnly( p.getParameter<bool>( "stableOnly" ) ),
+  excludeList( p.getParameter<vstring>( "excludeList" ) ),
+  pdt( "PDG table" ) {
   produces<CandidateCollection>();
+  ifstream pdtFile( pdtFileName.c_str() );
+  if( ! pdtFile ) 
+    throw cms::Exception( "FileNotFound", "can't open pdt file" )
+      << "cannot open " << pdtFileName;
+  { // notice: the builder has to be destroyed 
+    // in order to fill the table!
+    HepPDT::TableBuilder builder( pdt );
+    if( ! addPDGParticles( pdtFile, builder ) ) { 
+      throw cms::Exception( "ConfigError", "can't read pdt file" )
+	<< "wrong format of " << pdtFileName;
+    }
+  }
+  for( vstring::const_iterator e = excludeList.begin(); 
+       e != excludeList.end(); ++ e ) {
+    const DefaultConfig::ParticleData * p = pdt.particle( * e );
+    if ( p == 0 ) 
+      throw cms::Exception( "ConfigError", "can't find particle" )
+	<< "can't find particle: " << * e;
+    excludedIds.insert( p->pid() );
+  }
 }
 
-HepMCCandidateProducer::~HepMCCandidateProducer() { }
+HepMCCandidateProducer::~HepMCCandidateProducer() { 
+}
 
 void HepMCCandidateProducer::produce( Event& evt, 
 				      const EventSetup& ) {
@@ -27,8 +55,11 @@ void HepMCCandidateProducer::produce( Event& evt,
   cands->reserve( mc.particles_size() );
   for( HepMC::GenEvent::particle_const_iterator p = mc.particles_begin(); 
        p != mc.particles_end(); ++ p ) {
-    if ( stableOnly && (*p)->status() == 1 )
-      cands->push_back( new HepMCCandidate( * p ) );
+    if ( stableOnly && (*p)->status() == 1 ) {
+      int id = abs( (*p)->pdg_id() );
+      if ( excludedIds.find( id ) != excludedIds.end() )
+	cands->push_back( new HepMCCandidate( * p ) );
+    }
   }
 
   evt.put( cands );
--- PhysicsTools/HepMCCandAlgos/src/HepMCCandidateProducer.h
+++ PhysicsTools/HepMCCandAlgos/src/HepMCCandidateProducer.h
@@ -4,11 +4,14 @@
  *
  * \author Luca Lista, INFN
  *
- * \version $Id: HepMCCandidateProducer.h,v 1.2 2005/12/11 19:02:18 llista Exp $
+ * \version $Id: HepMCCandidateProducer.h,v 1.1 2006/03/08 10:50:07 llista Exp $
  *
  */
 #include "FWCore/Framework/interface/EDProducer.h"
 #include <string>
+#include <vector>
+#include <set>
+#include <CLHEP/HepPDT/DefaultConfig.hh>
 
 namespace edm {
   class ParameterSet;
@@ -22,12 +25,22 @@ class HepMCCandidateProducer : public edm::EDProducer {
   ~HepMCCandidateProducer();
 
  private:
+  /// vector of strings
+  typedef std::vector<std::string> vstring;
   /// process one event
   void produce( edm::Event& e, const edm::EventSetup& );
   /// source collection name  
   std::string source;
+  /// pdt file name
+  std::string pdtFileName;
   // selects only stable particles (HEPEVT status = 1)
   bool stableOnly;
+  /// exclude list
+  vstring excludeList;
+  /// particle data table
+  DefaultConfig::ParticleDataTable pdt;
+  /// set of excluded particle id's
+  std::set<int> excludedIds;
 };
 
 #endif
