07a083e6a475a4bec0fd4e201b0adf0675d11eee
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2006-03-10 08:38:19 +0000

    EcalRecHitAbsAlgo: pure interface class for all algorithms reconstructing rechits
    EcalRecHitSimpleAlgo: one first and simple algo making rechits.
    uses the global ADC->GeV scale and the intercalibration constants

---
+++ RecoLocalCalo/EcalRecAlgos/interface/EcalRecHitSimpleAlgo.h
@@ -0,0 +1,53 @@
+#ifndef RecoLocalCalo_EcalRecAlgos_EcalRecHitSimpleAlgo_HH
+#define RecoLocalCalo_EcalRecAlgos_EcalRecHitSimpleAlgo_HH
+
+/** \class EcalRecHitSimpleAlgo
+  *  Simple algoritm to make rechits from uncalibrated rechits
+  *
+  *  $Id:  $
+  *  $Date:  $
+  *  $Revision:  $
+  *  \author Shahram Rahatlou, University of Rome & INFN, March 2006
+  */
+
+#include "RecoLocalCalo/EcalRecAlgos/interface/EcalRecHitAbsAlgo.h"
+#include <iostream>
+
+class EcalRecHitSimpleAlgo : public EcalRecHitAbsAlgo {
+ public:
+  // default ctor
+  EcalRecHitSimpleAlgo() {
+    adcToGeVConstant_ = -1;
+    adcToGeVConstantIsSet_ = false;
+  }
+
+  virtual void setADCToGeVConstant(const float& value) {
+    adcToGeVConstant_ = value;
+    adcToGeVConstantIsSet_ = true;
+  }
+
+
+  // destructor
+  virtual ~EcalRecHitSimpleAlgo() { };
+
+  /// Compute parameters
+  virtual EcalRecHit makeRecHit(const EcalUncalibratedRecHit& uncalibRH,
+                                const float& intercalibConstant) const {
+
+    if(!adcToGeVConstantIsSet_) {
+      std::cout << "EcalRecHitSimpleAlgo::makeRecHit: adcToGeVConstant_ not set before calling this method!" << 
+                   " will use -1 and produce bogus rechits!" << std::endl;
+    }
+
+    float energy = uncalibRH.amplitude()*adcToGeVConstant_*intercalibConstant;
+    float time   = uncalibRH.jitter();
+
+    return EcalRecHit( uncalibRH.id(), energy, time );
+  }
+
+private:
+  float adcToGeVConstant_;
+  bool  adcToGeVConstantIsSet_;
+
+};
+#endif
