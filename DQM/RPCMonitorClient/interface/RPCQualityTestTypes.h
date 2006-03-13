e334bb05213dbe45f93da0e49e670ff2c2369d03
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-03-13 15:51:52 +0000

    more types

--- DQM/RPCMonitorClient/interface/RPCQualityTestTypes.h
+++ DQM/RPCMonitorClient/interface/RPCQualityTestTypes.h
@@ -1,18 +1,27 @@
 #ifndef RPCQualityTestTypes_H
 #define RPCQualityTestTypes_H
 
-namespace dqm {
+#include <string>
+namespace rpc_dqm {
 
   namespace qTestType{
 
-    static const char XRangeContent[14]="XRangeContent";
+    std::string  XRangeContent="XRangeContent";
+    std::string  YRangeContent="YRangeContent";
+
+    std::string  Comp2RefChi2="Comp2RefChi2";
+    std::string  Comp2RefKolmogorov="Comp2RefKolmogorov";
+
+    std::string  Comp2RefEqualsString="ContentsYRange";
+    std::string  Comp2RefEqualInt="Comp2RefEqualInt";
+    std::string  Comp2RefEqualFloat="Comp2RefEqualFloat";
+    std::string  Comp2RefEqualH1="Comp2RefEqualH1";
+    std::string  Comp2RefEqualH2="Comp2RefEqualH2";
+    std::string  Comp2RefEqualH3="Comp2RefEqualH3";
 
  }
 
 }
-
-
-
 
 
 #endif
