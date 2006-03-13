4b11cd3bd8459aab3c8f76599f25f18fc1d067cb
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2006-03-13 22:31:35 +0000

    move from SimG4Validation/Global

---
+++ Validation/GlobalHits/test/rootlogon.C
@@ -0,0 +1,15 @@
+{
+cout << "Loading FWLite..." << endl;    // load CMSSW libraries
+gSystem->Load("libPhysicsToolsFWLite");
+AutoLibraryLoader::enable();
+
+cout << "Setting Style to Plain..." << endl;
+gROOT->SetStyle("Plain");        // Switches off the ROOT default style
+//gPad->UseCurrentStyle();       // this makes everything black and white,
+                                 // removing the default red border on images
+//gROOT->ForceStyle();           // forces the style chosen above to be used,
+                                 // not the style the rootfile was made with
+
+cout << "Setting Palette to 1..." << endl;
+gStyle->SetPalette(1);         // get better colors than default
+}
