bbe9297f1ca8f2e6bfd39a0f81150bd48204447e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-14 16:34:10 +0000

    added new file

---
+++ SimGeneral/HepPDTRecord/src/ES_ParticleDataTable.cc
@@ -0,0 +1,4 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+
+EVENTSETUP_DATA_REG( DefaultConfig::ParticleDataTable );
