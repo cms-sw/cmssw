2086859e312d336cf36a238f476dd4ff649ef4fe
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-03-15 21:48:30 +0000

    comments respect doxygen style

--- PhysicsTools/FWLite/src/AutoLibraryLoader.h
+++ PhysicsTools/FWLite/src/AutoLibraryLoader.h
@@ -1,50 +1,32 @@
 #ifndef LibraryLoader_AutoLibraryLoader_h
 #define LibraryLoader_AutoLibraryLoader_h
-// -*- C++ -*-
-//
-// Package:     FWLite
-// Class  :     AutoLibraryLoader
-// 
-/**\class AutoLibraryLoader AutoLibraryLoader.h AnalysisTools/FWLite/interface/AutoLibraryLoader.h
-
- Description: ROOT helper class which can automatically load the proper shared library when ROOT needs a new class dictionary
-
- Usage:
-    <usage>
-
-*/
-//
-// Original Author:  
-//         Created:  Wed Nov 30 14:59:33 EST 2005
-// $Id$
-//
-
-// system include files
-
-// user include files
+/**\class AutoLibraryLoader
+ *
+ * ROOT helper class which can automatically load the 
+ * proper shared library when ROOT needs a new class dictionary
+ *
+ * \author Chirs Jones, Cornell
+ *
+ * $Id: AutoLibraryLoader.h,v 1.1 2005/12/01 14:56:55 chrjones Exp $
+ *
+ */
 #include "TClassGenerator.h"
 
-// forward declarations
+class AutoLibraryLoader : public TClassGenerator {
+public:
+  /// return class type
+  virtual TClass *GetClass(const char* classname, Bool_t load);
+  /// return class type
+  virtual TClass *GetClass(const type_info& typeinfo, Bool_t load);
+  /// interface for TClass generators
+  ClassDef(AutoLibraryLoader,1);
+  /// enable automatic library loading  
+  static void enable();
 
-class AutoLibraryLoader : public TClassGenerator
-{
-
-   public:
-
-      virtual TClass *GetClass(const char* classname, Bool_t load);
-      virtual TClass *GetClass(const type_info& typeinfo, Bool_t load);
-      
-      ClassDef(AutoLibraryLoader,1);  // interface for TClass generators
-
-      static void enable();
-   private:
-      AutoLibraryLoader();
-      AutoLibraryLoader(const AutoLibraryLoader&); // stop default
-
-      const AutoLibraryLoader& operator=(const AutoLibraryLoader&); // stop default
-
-      // ---------- member data --------------------------------
-
+private:
+  AutoLibraryLoader();
+  AutoLibraryLoader(const AutoLibraryLoader&); // stop default
+  const AutoLibraryLoader& operator=(const AutoLibraryLoader&); // stop default
 };
 
 
