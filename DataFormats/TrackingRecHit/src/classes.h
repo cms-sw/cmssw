51310e1380f9758410fb59ea99c155f5860389f8
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-15 10:03:10 +0000

    collection

--- DataFormats/TrackingRecHit/src/classes.h
+++ DataFormats/TrackingRecHit/src/classes.h
@@ -2,7 +2,14 @@
 #include "DataFormats/TrackingRecHit/interface/RecHit1D.h"
 #include "DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h"
 
+#include "DataFormats/Common/interface/OwnVector.h"
+#include <vector>
+#include "DataFormats/Common/interface/ClonePolicy.h"
+
 namespace {
   namespace {
+    std::vector<TrackingRecHit*> v1;
+    edm::OwnVector<TrackingRecHit,
+      edm::ClonePolicy<TrackingRecHit> > a6;
   }
 }
