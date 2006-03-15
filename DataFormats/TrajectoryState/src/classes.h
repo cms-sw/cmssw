fae7123203b6f6d542d8bef9f55dd1d2aa397090
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-15 10:03:46 +0000

    first

---
+++ DataFormats/TrajectoryState/src/classes.h
@@ -0,0 +1,9 @@
+#include "DataFormats/TrajectoryState/interface/PTrajectoryStateOnDet.h"
+#include "FWCore/EDProduct/interface/Wrapper.h"
+
+namespace {
+  namespace {
+    PTrajectoryStateOnDet v1;
+    LocalTrajectoryParameters p1;
+  }
+}
