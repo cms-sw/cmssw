697a188c9fb2fbe7c92d7f8452c7a8e7e36c0897
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-03-16 18:07:42 +0000

    first release

---
+++ CondFormats/RPCObjects/src/classes.h
@@ -0,0 +1,5 @@
+#include "CondFormats/RPCObjects/interface/RPCReadOutMapping.h"
+
+// Declaration of the iterator (necessary for the generation of the dictionary)
+template std::vector<RPCReadOutLink>::iterator;
+template std::vector<RPCReadOutLink>::const_iterator;
