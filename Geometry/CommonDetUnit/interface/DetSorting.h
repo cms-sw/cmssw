e64a581d9e2dc90e2dd14da24e98682f09cf3417
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-03-17 15:02:05 +0000

    First implementation of stable sorting

---
+++ Geometry/CommonDetUnit/interface/DetSorting.h
@@ -0,0 +1,65 @@
+#ifndef DetSorting_h
+#define DetSorting_h
+
+#include <Geometry/Surface/interface/GeometricSorting.h>
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+
+
+namespace geomsort{
+
+/** \typedef DetR
+ *
+ *  functor to sort in R using precomputed_value_sort.
+ *  
+ *  Use: 
+ *
+ *  precomputed_value_sort(v.begin(), v.end(), DetR());
+ *
+ *  $Date: 2006/03/16 17:58:56 $
+ *  $Revision: 1.1 $
+ *  \author N. Amapane - CERN
+ */
+
+  typedef ExtractR<GeomDet,float> DetR;
+
+
+/** \typedef DetPhi
+ *
+ *  functor to sort in phi (from -pi to pi) using precomputed_value_sort.
+ *
+ *  Note that sorting in phi is done within the phi range of 
+ *  (-pi, pi]. It may NOT be what you expect if the elements cluster around
+ *  the pi discontinuity.
+ *  
+ *  Use: 
+ *
+ *  precomputed_value_sort(v.begin(), v.end(), DetPhi());
+ *
+ *  $Date: 2006/03/16 17:58:56 $
+ *  $Revision: 1.1 $
+ *  \author N. Amapane - CERN
+ */
+
+  typedef ExtractPhi<GeomDet,float> DetPhi;
+
+
+/** \typedef DetZ
+ *
+ *  functor to sort in Z using precomputed_value_sort.
+ *  
+ *  Use: 
+ *
+ *  precomputed_value_sort(v.begin(), v.end(), DetZ());
+ *
+ *  $Date: 2006/03/16 17:58:56 $
+ *  $Revision: 1.1 $
+ *  \author N. Amapane - CERN
+ */
+
+  typedef ExtractZ<GeomDet,float> DetZ;
+
+
+};
+#endif
+
+
