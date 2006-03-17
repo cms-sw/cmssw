d90a8cbd12d80673d606f5d5ef2dfa2eecc4c7a3
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2006-03-17 21:48:13 +0000

    add gain switch

--- SimCalorimetry/EcalSimAlgos/src/ESShape.cc
+++ SimCalorimetry/EcalSimAlgos/src/ESShape.cc
@@ -1,6 +1,33 @@
 #include "SimCalorimetry/EcalSimAlgos/interface/ESShape.h"
 #include <cmath>
 
+ESShape::ESShape(int Gain):
+  theGain(Gain)
+{
+  setTpeak(20.0);
+
+  if (theGain==0) {
+    A = 6.;
+    Qcf = 4./350.;
+    omegac = 2./25.;
+    norm = 0.11136;
+  }
+  else if (theGain==1) {
+    // preliminary numbers, need to be approved by preshower group
+    A = 5.99994;
+    Qcf = 0.0114319;
+    omegac = 0.0736172;
+    norm = 0.111492;
+  }
+  else if (theGain==2) {
+    // preliminary numbers, need to be approved by preshower group
+    A = 5.99912; 
+    Qcf = 0.0114275;
+    omegac = 0.086403;
+    norm = 0.152841;
+  }
+}
+
 double ESShape::operator () (double time_) const
 {   
   if (time_>0.00001) {
