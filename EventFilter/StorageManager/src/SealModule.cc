05a7ef95ea8d34be25836f1c8ce23a11d7f3b796
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2006-03-17 18:12:19 +0000

    adding in an input source that uses curl to get input from http get

--- EventFilter/StorageManager/src/SealModule.cc
+++ EventFilter/StorageManager/src/SealModule.cc
@@ -3,9 +3,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOPool/Streamer/interface/EventStreamOutput.h"
 #include "EventFilter/StorageManager/src/I2OConsumer.h"
+#include "EventFilter/StorageManager/src/EventStreamHttpReader.h"
 
 typedef edm::EventStreamingModule<edmtest::I2OConsumer> I2OTestConsumer;
+using edmtestp::EventStreamHttpReader;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(I2OTestConsumer)
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(EventStreamHttpReader);
 
