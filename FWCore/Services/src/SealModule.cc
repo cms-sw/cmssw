423dceccc3038d3fcebfa1bb48534dd301bc4185
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-03-21 23:07:37 +0000

    Put RandomNumberGeneratorServicei back.  It had been commented out.

--- FWCore/Services/src/SealModule.cc
+++ FWCore/Services/src/SealModule.cc
@@ -5,7 +5,7 @@
 #include "FWCore/Services/src/Memory.h"
 #include "FWCore/Services/src/Profiling.h"
 #include "FWCore/Services/src/LoadAllDictionaries.h"
-// #include "FWCore/Services/src/RandomNumberGeneratorService.h"
+#include "FWCore/Services/src/RandomNumberGeneratorService.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 using edm::service::Tracer;
@@ -13,7 +13,7 @@ using edm::service::Timing;
 using edm::service::SimpleMemoryCheck;
 using edm::service::SimpleProfiling;
 using edm::service::LoadAllDictionaries;
-// using edm::service::RandomNumberGeneratorService;
+using edm::service::RandomNumberGeneratorService;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_SERVICE(Tracer)
@@ -21,5 +21,5 @@ DEFINE_ANOTHER_FWK_SERVICE(Timing)
 DEFINE_ANOTHER_FWK_SERVICE(SimpleMemoryCheck)
 DEFINE_ANOTHER_FWK_SERVICE(SimpleProfiling)
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(LoadAllDictionaries,edm::serviceregistry::ParameterSetMaker<LoadAllDictionaries>)
-// typedef edm::serviceregistry::AllArgsMaker<edm::RandomNumberGenerator,RandomNumberGeneratorService> RandomMaker;
-// DEFINE_ANOTHER_FWK_SERVICE_MAKER(RandomNumberGeneratorService, RandomMaker)
+typedef edm::serviceregistry::AllArgsMaker<edm::RandomNumberGenerator,RandomNumberGeneratorService> RandomMaker;
+DEFINE_ANOTHER_FWK_SERVICE_MAKER(RandomNumberGeneratorService, RandomMaker)
