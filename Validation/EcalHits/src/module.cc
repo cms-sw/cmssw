e57c269f103d435c264bd556904654cad5099729
xhuang <sha1-4bd1aee960316b9472cdc249dc0e7db0c29cbfd7@cern.ch>
2006-03-21 21:22:42 +0000

    first version

---
+++ Validation/EcalHits/src/module.cc
@@ -0,0 +1,7 @@
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+#include "Validation/EcalHits/interface/EcalSimHitsValidProducer.h"
+#include "PluginManager/ModuleDef.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_SIMWATCHER (EcalSimHitsValidProducer);
+
