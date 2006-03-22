0c7533341c15faac41080fc2ebb224359853f500
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-22 17:07:49 +0000

    first commit

---
+++ RecoLocalTracker/Records/src/TrackerCPERecord.cc
@@ -0,0 +1,4 @@
+#include "RecoLocalTracker/Records/interface/TrackerCPERecord.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(TrackerCPERecord);
