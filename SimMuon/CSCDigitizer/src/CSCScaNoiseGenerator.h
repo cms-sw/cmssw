6ad065eb0ea1390db1dfdbd58ec093f35e526b98
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-03-22 04:10:54 +0000

    Made pure virtual

--- SimMuon/CSCDigitizer/src/CSCScaNoiseGenerator.h
+++ SimMuon/CSCDigitizer/src/CSCScaNoiseGenerator.h
@@ -1,5 +1,5 @@
-#ifndef MU_END_SCA_NOISE_GENERATOR_H
-#define MU_END_SCA_NOISE_GENERATOR_H
+#ifndef CSCDigitizer_CSCScaNoiseGenerator_h
+#define CSCDigitizer_CSCScaNoiseGenerator_h
 
 /** \class CSCScaNoiseGenerator
  * Generate noise for the SCA samples
@@ -8,15 +8,15 @@
  *
  **/
 #include<vector>
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
 
 class CSCScaNoiseGenerator
 {
 public:
-  explicit CSCScaNoiseGenerator(int nScaBins);
-  virtual ~CSCScaNoiseGenerator() {};
+  explicit CSCScaNoiseGenerator(int nScaBins) : nScaBins_(nScaBins) {}
 
   /** returns a list of SCA readings  */
-  virtual std::vector<int> getNoise() const;
+  virtual std::vector<int> getNoise(const CSCDetId & layer, int element) const = 0;
 
 protected:
   int nScaBins_;
