a71912a31739555a0af3a95d26e1a51341f5b748
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2006-03-22 13:40:36 +0000

    New helper classes implemented

---
+++ PhysicsTools/PythonAnalysis/python/cmstools.py
@@ -0,0 +1,42 @@
+"""Python helper tools for CMS FWLite
+
+benedikt.hegner@cern.ch
+
+"""
+
+### define tab completion
+try:
+  import readline, cmscompleter
+  readline.parse_and_bind('tab: complete')
+except:
+  print 'WARNING: Could not load tab completion'
+
+
+### workaround iterator generators for ROOT classes
+def all(container):
+
+  # loop over ROOT::TTree and similar
+  if hasattr(container,'GetEntries'):
+    try:
+      entries = container.GetEntries()
+      for entry in xrange(entries):
+        yield entry
+    except:
+      pass
+
+  # loop over std::vectors and similar
+  elif hasattr(container, 'size'):
+    try:
+      entries = container.size()
+      for entry in xrange(entries):
+        yield container[entry]
+    except:
+      pass
+
+
+### prepare the environment
+### (will be changed for ROOT 5.10)
+print 'Loading FWLite...'
+from ROOT import *
+gSystem.Load('libPhysicsToolsFWLite')
+AutoLibraryLoader.enable()
