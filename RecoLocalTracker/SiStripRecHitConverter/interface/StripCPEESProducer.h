09dbd1eaccc0f565a155f6bfd8fefd9d0f7ef336
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-22 18:18:25 +0000

    not ok yet

---
+++ RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEESProducer.h
@@ -0,0 +1,25 @@
+#ifndef RecoLocaltracker_SiStriprecHitConverter_StripCPEESProducer_h
+#define RecoLocaltracker_SiStriprecHitConverter_StripCPEESProducer_h
+
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "RecoLocalTracker/Records/interface/TrackerCPERecord.h"
+#include "RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h"
+#include <boost/shared_ptr.hpp>
+
+class  StripCPEESProducer: public edm::ESProducer{
+ public:
+  StripCPEESProducer(const edm::ParameterSet & p);
+  virtual ~StripCPEESProducer(); 
+  boost::shared_ptr<StripClusterParameterEstimator> produce(const TrackerCPERecord &);
+ private:
+  boost::shared_ptr<StripClusterParameterEstimator> _cpe;
+  edm::ParameterSet pset_;
+};
+
+
+#endif
+
+
+
+
--- RecoLocalTracker/SiStripRecHitConverter/src/SealModules.cc
+++ RecoLocalTracker/SiStripRecHitConverter/src/SealModules.cc
@@ -1,11 +1,20 @@
+#include "RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h"
+#include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEESProducer.h"
 
 #include "PluginManager/ModuleDef.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/SiStripRecHitConverter.h"
 
-using cms::SiStripRecHitConverter;
+EVENTSETUP_DATA_REG(StripClusterParameterEstimator);
 DEFINE_SEAL_MODULE();
+//DEFINE_ANOTHER_FWK_MODULE(StripCPEESProducer)
+using cms::SiStripRecHitConverter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripRecHitConverter)
 
