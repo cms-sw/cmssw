22530806e7d457fca617a809684c736bdbec0ea7
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-03-23 10:15:56 +0000

    allow better debugging

--- Geometry/TrackerGeometryBuilder/interface/TrackerLayerIdAccessor.h
+++ Geometry/TrackerGeometryBuilder/interface/TrackerLayerIdAccessor.h
@@ -110,33 +110,5 @@ class TrackerLayerIdAccessor {
   
 };
 
-TrackerLayerIdAccessor::TrackerLayerIdAccessor(){}
-
- std::pair<DetId,DetIdPXBSameLayerComparator> TrackerLayerIdAccessor::pixelBarrelLayer(int layer ){
-  PXBDetId id(layer,1,1);
-  return make_pair(id,DetIdPXBSameLayerComparator());
-}
-std::pair<DetId,DetIdPXFSameDiskComparator>  TrackerLayerIdAccessor::pixelForwardDisk(int side, int disk ){
-  PXFDetId id(side,disk,1,1,1);
-  return make_pair(id,DetIdPXFSameDiskComparator());
-}
-std::pair<DetId,DetIdTIBSameLayerComparator> TrackerLayerIdAccessor::stripTIBLayer(int layer ){
-  TIBDetId id(layer,1,1,1,1,1);
-  return make_pair(id,DetIdTIBSameLayerComparator());
-}
-std::pair<DetId,DetIdTOBSameLayerComparator> TrackerLayerIdAccessor::stripTOBLayer(int layer ){
-  TOBDetId id(layer,1,1,1,1);
-  return make_pair(id,DetIdTOBSameLayerComparator());
-}
-std::pair<DetId,DetIdTIDSameDiskComparator> TrackerLayerIdAccessor::stripTIDDisk(int side, int disk ){
-  TIDDetId id(side,disk,1,1,1,1);
-  return make_pair(id,DetIdTIDSameDiskComparator());
-}
-std::pair<DetId,DetIdTECSameDiskComparator> TrackerLayerIdAccessor::stripTECDisk(int side, int disk ){
-  TECDetId id(side,disk,1,1,1,1,1);
-  
-  return make_pair(id,DetIdTECSameDiskComparator());
-}
-
 #endif
 
