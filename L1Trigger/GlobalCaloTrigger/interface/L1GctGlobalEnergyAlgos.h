84cb08bdefeb45501cc55b255f1fd61fdd72504f
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2006-03-23 20:33:00 +0000

    Updating GlobalEnergyAlgos, only Ht implemented so far

--- L1Trigger/GlobalCaloTrigger/interface/L1GctGlobalEnergyAlgos.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctGlobalEnergyAlgos.h
@@ -24,30 +24,74 @@ public:
 	// process the event
 	void process();
 
-	// set input data	
-	void setInputSums(unsigned ex, unsigned ey);
+	// set input data per wheel
+/* 	void setInputWheelEx(unsigned wheel, int energy, bool overflow); */
+/* 	void setInputWheelEy(unsigned wheel, int energy, bool overflow); */
+/* 	void setInputWheelEt(unsigned wheel, unsigned energy, bool overflow); */
+	void setInputWheelHt(unsigned wheel, unsigned energy, bool overflow);
 
-	// return input data
-	inline unsigned long getInputEx() { return inputEx.to_ulong();; }
-	inline unsigned long getInputEy() { return inputEy.to_ulong();; }
+        // An extra contribution to Ht from jets at
+        // the boundary between wheels
+        void setInputBoundaryHt(unsigned energy, bool overflow);
 
-	// return output data	
-	inline unsigned long getEtMiss() { return outputEtMiss.to_ulong(); }
-	inline unsigned long getEtMissPhi()  { return outputEtMissPhi.to_ulong(); }
-	inline unsigned long getEtSum() { return outputEtSum.to_ulong(); }
-	inline unsigned long getEtHad() { return outputEtHad.to_ulong(); }
+/*         // also jet counts */
+/*         void setInputWheelJc(unsigned wheel, unsigned jcnum, unsigned count); */
+/*         void setInputBoundaryJc(unsigned jcnum, unsigned count); */
+
+/* 	// return input data */
+/*         long int getInputExForwardWheel(); */
+/*         long int getInputEyForwardWheel(); */
+/*         long int getInputExBakwardWheel(); */
+/*         long int getInputEyBakwardWheel(); */
+/* 	inline unsigned long getInputEtForwardWheel() { return inputEtForwardWheel.to_ulong();; } */
+	inline unsigned long getInputHtForwardWheel() { return inputHtForwardWheel.to_ulong();; }
+/* 	inline unsigned long getInputEtBakwardWheel() { return inputEtBakwardWheel.to_ulong();; } */
+	inline unsigned long getInputHtBakwardWheel() { return inputHtBakwardWheel.to_ulong();; }
+	inline unsigned long getInputHtBoundaryJets() { return inputHtBoundaryJets.to_ulong();; }
+/*         inline unsigned long getInputJcForwardWheel(unsigned jcnum) {return inputJcForwardWheel[jcnum].to_ulong();; } */
+/*         inline unsigned long getInputJcBakwardWheel(unsigned jcnum) {return inputJcBakwardWheel[jcnum].to_ulong();; } */
+/*         inline unsigned long getInputJcBoundaryJets(unsigned jcnum) {return inputJcBoundaryJets[jcnum].to_ulong();; } */
+
+/* 	// return output data	 */
+	inline unsigned long getEtMiss()    { return outputEtMiss.to_ulong(); }
+	inline unsigned long getEtMissPhi() { return outputEtMissPhi.to_ulong(); }
+	inline unsigned long getEtSum()     { return outputEtSum.to_ulong(); }
+	inline unsigned long getEtHad()     { return outputEtHad.to_ulong(); }
+/* 	inline unsigned long getJetCounts(unsigned jcnum) { return output[jcnum].to_ulong(); } */
 	
 private:
 	
 	// input data - need to confirm number of bits!
-	bitset<12> inputEx;
-	bitset<12> inputEy;
+/* 	bitset<12> inputExForwardWheel; */
+/* 	bitset<12> inputEyForwardWheel; */
+/* 	bitset<12> inputEtForwardWheel; */
+	bitset<12> inputHtForwardWheel;
+/* 	bitset<12> inputExBakwardWheel; */
+/* 	bitset<12> inputEyBakwardWheel; */
+/* 	bitset<12> inputEtBakwardWheel; */
+	bitset<12> inputHtBakwardWheel;
+        bitset<12> inputHtBoundaryJets;
+
+/*         bool ovfloExForwardWheel; */
+/*         bool ovfloEyForwardWheel; */
+/*         bool ovfloEtForwardWheel; */
+        bool ovfloHtForwardWheel;
+/*         bool ovfloExBakwardWheel; */
+/*         bool ovfloEyBakwardWheel; */
+/*         bool ovfloEtBakwardWheel; */
+        bool ovfloHtBakwardWheel;
+        bool ovfloHtBoundaryJets;
+
+/*         vector<bitset<4>> inputJcForwardWheel; */
+/*         vector<bitset<4>> inputJcBakwardWheel; */
+/*         vector<bitset<3>> inputJcBoundaryJets; */
 	
 	// output data
 	bitset<13> outputEtMiss;
 	bitset<6> outputEtMissPhi;
 	bitset<13> outputEtSum;
 	bitset<13> outputEtHad;
+/*         vector<bitset<5>> outputJetCounts; */
 
 };
 
--- L1Trigger/GlobalCaloTrigger/src/L1GctGlobalEnergyAlgos.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctGlobalEnergyAlgos.cc
@@ -1,9 +1,103 @@
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctGlobalEnergyAlgos.h"
 
-L1GctGlobalEnergyAlgos::L1GctGlobalEnergyAlgos()
+L1GctGlobalEnergyAlgos::L1GctGlobalEnergyAlgos() // :
+//   inputJcForwardWheel(12),
+//   inputJcBakwardWheel(12),
+//   inputJcBoundaryJets(12),
+//   outputJetCounts(12)
 {
 }
 
 L1GctGlobalEnergyAlgos::~L1GctGlobalEnergyAlgos()
 {
 }
+
+// clear internal data
+void L1GctGlobalEnergyAlgos::reset()
+{
+  inputHtForwardWheel.reset();
+  inputHtBakwardWheel.reset();
+  inputHtBoundaryJets.reset();
+  //
+  ovfloHtForwardWheel = false;
+  ovfloHtBakwardWheel = false;
+  ovfloHtBoundaryJets = false;
+  //
+  outputEtHad.reset();
+}
+	
+// process the event
+void L1GctGlobalEnergyAlgos::process()
+{
+  unsigned long HtPlus, HtMinus, HtBound, HtSum;
+  bool HtOvflo;
+  bitset<13> HtResult;
+
+  // Form the Ht sum
+  HtPlus  = inputHtForwardWheel.to_ulong();
+  HtMinus = inputHtBakwardWheel.to_ulong();
+  HtBound = inputHtBoundaryJets.to_ulong();
+  //
+  HtSum = HtPlus + HtMinus + HtBound;
+  if (HtSum>0xfff) {
+    HtSum = HtSum % 0x1000;
+    HtOvflo = true;
+  } else {
+    HtOvflo = ovfloHtForwardWheel or ovfloHtBakwardWheel or ovfloHtBoundaryJets;
+  }
+  //
+  HtResult.reset();
+  for (int i=0;i<12;i++) {
+    if (HtSum && (1<<i) != 0) {HtResult.set(i);}
+  }
+  if (HtOvflo) {HtResult.set(12);}
+  //
+  outputEtHad = HtResult;
+}
+
+// set input data per wheel
+// void L1GctGlobalEnergyAlgos::setInputWheelEx(unsigned wheel, int energy, bool overflow)
+// void L1GctGlobalEnergyAlgos::setInputWheelEy(unsigned wheel, int energy, bool overflow)
+// void L1GctGlobalEnergyAlgos::setInputWheelEt(unsigned wheel, unsigned energy, bool overflow)
+void L1GctGlobalEnergyAlgos::setInputWheelHt(unsigned wheel, unsigned energy, bool overflow)
+{
+  unsigned long energyInput;
+  bool          energyOvflo;
+
+  energyInput = energy;
+  if (energyInput>0xfff) {
+    energyInput = energyInput % 0x1000;
+    energyOvflo = true;
+  } else {
+    energyOvflo = overflow;
+  }
+  bitset<12> energyBits(energyInput);
+  if (wheel==0) {
+    inputHtForwardWheel = energyBits;
+    ovfloHtForwardWheel = energyOvflo;
+  } else if (wheel==1) {
+    inputHtBakwardWheel = energyBits;
+    ovfloHtForwardWheel = energyOvflo;
+  }
+}
+
+// An extra contribution to Ht from jets at
+// the boundary between wheels
+void L1GctGlobalEnergyAlgos::setInputBoundaryHt(unsigned energy, bool overflow)
+{
+  unsigned long energyInput;
+  bool          energyOvflo;
+
+  energyInput = energy;
+  if (energyInput>0xfff) {
+    energyInput = energyInput % 0x1000;
+    energyOvflo = true;
+  } else {
+    energyOvflo = overflow;
+  }
+  bitset<12> energyBits(energyInput);
+  inputHtBoundaryJets = energyBits;
+  ovfloHtBoundaryJets = energyOvflo;
+}
+
+
