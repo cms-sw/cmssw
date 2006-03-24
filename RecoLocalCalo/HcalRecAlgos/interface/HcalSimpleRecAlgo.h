85990e9f517047c57ddc8bbf8d2996d5059adabd
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2006-03-24 16:56:32 +0000

    Documentation updates

--- RecoLocalCalo/HcalRecAlgos/interface/HcalSimpleRecAlgo.h
+++ RecoLocalCalo/HcalRecAlgos/interface/HcalSimpleRecAlgo.h
@@ -10,10 +10,17 @@
 #include "CalibFormats/HcalObjects/interface/HcalCoder.h"
 #include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
 
-/** \class HcalSimplerecAlgo
+/** \class HcalSimpleRecAlgo
+
+   This class reconstructs RecHits from Digis for HBHE, HF, and HO by addition
+   of selected time samples, pedestal subtraction, and gain application. The
+   time of the hit is reconstructed using a weighted peak bin calculation
+   supplemented by precise time lookup table. A consumer of this class also
+   has the option of correcting the reconstructed time for energy-dependent
+   time slew associated with the QIE.
     
-   $Date: 2005/11/28 19:56:27 $
-   $Revision: 1.3 $
+   $Date: 2006/02/07 23:29:10 $
+   $Revision: 1.4 $
    \author J. Mans - Minnesota
 */
 class HcalSimpleRecAlgo {
