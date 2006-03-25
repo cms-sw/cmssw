d5a4a61b5cbb2b685b5cf3f757e24204d9e763e7
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-03-25 02:41:39 +0000

    convert to new version of cppunit

--- DataFormats/Common/test/testRunner.cpp
+++ DataFormats/Common/test/testRunner.cpp
@@ -1,10 +1,1 @@
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/ui/text/TestRunner.h>
-using namespace CppUnit;
-
-int main()
-{
-  TextUi::TestRunner runner;
-  runner.addTest( TestFactoryRegistry::getRegistry().makeTest() );
-  return runner.run( "", false ) ? 0 : 1;
-}
+#include <Utilities/Testing/interface/CppUnit_testdriver.icpp>
