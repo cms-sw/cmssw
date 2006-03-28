512f5d0bab2deecc45181c634f9e86a5bfa5733c
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-03-28 00:29:06 +0000

    Compiles and runs. Still need to add file output

--- L1Trigger/GlobalCaloTrigger/test/testJetFinalStage.cpp
+++ L1Trigger/GlobalCaloTrigger/test/testJetFinalStage.cpp
@@ -23,13 +23,16 @@
 #include <stdexcept> //for std::runtime_error()
 using namespace std;
 
+
 //Typedefs for the vector templates used
 typedef vector<L1GctJet> JetsVector;
 typedef unsigned long int ULong;
 
+
 // Name of the files for test input data and results output.
 const string testDataFile = "testJetFinalStageInput.txt";  
 const string resultsFile = "testJetFinalStageOutput.txt";
+
 
 // Constants to tell the program how many jets to read in and out
 // THESE ARE TOTAL GUESSES!
@@ -38,9 +41,10 @@ const int numCentralJets = 4;
 const int numForwardJets = 4;
 const int numTauJets = 4;
 
+
 //  FUNCTION PROTOTYPES
 /// Runs the test, and returns a string with the test result message in.
-string classTest();
+void classTest(L1GctJetFinalStage *myJetFinalStage);
 /// Loads test input and also the known results from a file.
 void loadTestData(JetsVector &inputJets, JetsVector &trueCentralJets,
                   JetsVector &trueForwardJets, JetsVector &trueTauJets);
@@ -50,6 +54,9 @@ void safeOpenFile(ifstream &fin, const string &name);
 void putJetsInVector(ifstream &fin, JetsVector &jets, int numJets);
 /// Gets jet data from the testDataFile (reasonably safely). 
 L1GctJet readSingleJet(ifstream &fin);
+/// Compares JetsVectors, prints a message about the comparison, returns true if identical, else false.
+bool compareJetsVectors(JetsVector &vector1, JetsVector &vector2, const string description);
+
 
 /// Entrypoint of unit test code + error handling
 int main(int argc, char **argv)
@@ -60,7 +67,9 @@ int main(int argc, char **argv)
 
     try
     {
-        cout << "\n" << classTest() << endl;
+        L1GctJetFinalStage * myJetFinalStage = new L1GctJetFinalStage(); //TEST OBJECT on heap;    
+        classTest(myJetFinalStage);
+        delete myJetFinalStage;
     }
     catch(const exception &e)
     {
@@ -70,15 +79,13 @@ int main(int argc, char **argv)
     {
         cerr << "\nError! An unknown exception has occurred!" << endl;
     }
+    
     return 0;   
 }
 
 // Runs the test, and returns a string with the test result message in.
-string classTest()
+void classTest(L1GctJetFinalStage *myJetFinalStage)
 {
-    L1GctJetFinalStage myJetFinalStage; //TEST OBJECT;    
-    bool testPass = true; //Test passing flag.
-    
     // Vectors for reading in test data from the text file.
     JetsVector inputJets;        //Size?
     JetsVector trueCentralJets;  //Size?
@@ -86,7 +93,7 @@ string classTest()
     JetsVector trueTauJets;      //Size?
     
     // Vectors for receiving the output from the object under test.
-    JetsVector outputJets;         //Size
+    JetsVector outputJets;         //Size?
     JetsVector outputCentralJets;  //Size?
     JetsVector outputForwardJets;  //Size?
     JetsVector outputTauJets;      //Size?
@@ -97,57 +104,52 @@ string classTest()
     //Fill the L1GctJetFinalStage with input data.
     for(int i = 0; i < numInputJets; ++i)  //How many? See global constants at top of file
     {
-        myJetFinalStage.setInputJet(i, inputJets[i]);
+        myJetFinalStage->setInputJet(i, inputJets[i]);
     }
 
     // Test the getInputJets() method
-    outputJets = myJetFinalStage.getInputJets();
-    if(outputJets.size() != inputJets.size())  
-    {
-        testPass = false;
+    outputJets = myJetFinalStage->getInputJets();
+    if(!compareJetsVectors(outputJets, inputJets, "initial data input/output")) { return; }
+    
+    myJetFinalStage->process();  //Run algorithm
+    
+    //Get and then test the output jets against known results
+    outputCentralJets = myJetFinalStage->getCentralJets();
+    outputForwardJets = myJetFinalStage->getForwardJets();
+    outputTauJets = myJetFinalStage->getTauJets();
+
+    if(!compareJetsVectors(outputCentralJets, trueCentralJets, "central jets")) { return; }
+    if(!compareJetsVectors(outputForwardJets, trueForwardJets, "forward jets")) { return; }
+    if(!compareJetsVectors(outputTauJets, trueTauJets, "tau jets")) { return; }
+
+    cout << "\nWriting results of processing to file...";
+    /*
+     * Add file output of results
+    */
+    cout << "done!" << endl;
+    
+    //test the reset method.
+    myJetFinalStage->reset();
+    //get all the data again
+    outputJets = myJetFinalStage->getInputJets();
+    outputCentralJets = myJetFinalStage->getCentralJets();
+    outputForwardJets = myJetFinalStage->getForwardJets();
+    outputTauJets = myJetFinalStage->getTauJets();
+    
+    if(outputJets.empty() && outputCentralJets.empty() &&
+       outputForwardJets.empty() && outputTauJets.empty())
+    {   
+        cout << "\nTest class has passed reset method testing." << endl;
     }
     else
     {
-        for(int i = 0; i < numInputJets; ++i)
-        {
-            if(outputJets[i].getRank() != inputJets[i].getRank()) { testPass = false; break; }
-            if(outputJets[i].getEta() != inputJets[i].getEta()) { testPass = false; break; }
-            if(outputJets[i].getPhi() != inputJets[i].getPhi()) {testPass = false; break; }
-        }
+        cout << "\nTest class has FAILED reset method testing!" << endl;
+        return;
     }
-        
-    if(testPass == false)
-    {
-        return "Test class has failed initial data input/output comparison!";
-    }
-    
-    myJetFinder.process();  //Run algorithm
-    
-    //Get and then test the output jets against known results
-    //NEEDS FILLING IN SIMILARLY TO ABOVE
-    outputCentralJets = myJetFinalStage.getCentralJets();
-    outputForwardJets = myJetFinalStage.getForwardJets();
-    outputTauJets = myJetFinalStage.getTauJets();
-    // **Fill in further - needs jet comparison function as done a few lines up ***
-            
-    if(testPass == false)
-    {
-        return "Test class has failed algorithm processing!";
-    }
-    
-    //test the reset method.
-    
-    if(testPass == false
-    {
-        return "Test class has failed reset() method testing!";
-    }
-    
-    /*
-     * Note to self - will have to sort out file output of results
-     * so will have to alter the above slightly
-    */    
 
-    return "Test class has passed!";        
+    cout << "\nTest class has passed all tests." << endl;
+
+    return;        
 }
 
 
@@ -191,19 +193,20 @@ void putJetsInVector(ifstream &fin, JetsVector &jets, int numJets)
 {
     for(int i=0; i < numJets; ++i)
     {
-        jets.push_back(readJetFromFile(fin));
+        jets.push_back(readSingleJet(fin));
     }
 }
 
 //Gets jet data from the testDataFile (reasonably safely). 
 L1GctJet readSingleJet(ifstream &fin)
 {
-    const int numJetComponents = 3;
+    //This reperesents how many numbers there are per line in the input file
+    const int numJetComponents = 3; //3 since we have rank, eta & phi.
     
     ULong jetComponents[numJetComponents];
 
     //read in the data from the file
-    for(int i=0, i < numJetComponents, ++i)
+    for(int i=0; i < numJetComponents; ++i)
     {
         if(fin.eof() || fin.bad())
         {
@@ -211,11 +214,45 @@ L1GctJet readSingleJet(ifstream &fin)
         }
         else
         {
-            fin >> jetComponents[i]
+            fin >> jetComponents[i];
         }
     }
    
     L1GctJet tempJet(jetComponents[0], jetComponents[1], jetComponents[2]);    
 
-    return tempJet
+    return tempJet;
+}
+
+// Compares JetsVectors, prints a message about the comparison, returns true if identical, else false.
+bool compareJetsVectors(JetsVector &vector1, JetsVector &vector2, const string description)
+{
+    bool testPass = true;
+    
+    if(vector1.size() != vector2.size())  
+    {
+        testPass = false;
+    }
+    else
+    {
+        if (!vector1.empty())
+        {
+            for(unsigned int i = 0; i < vector1.size(); ++i)
+            {
+                if(vector1[i].getRank() != vector2[i].getRank()) { testPass = false; break; }
+                if(vector1[i].getEta() != vector2[i].getEta()) { testPass = false; break; }
+                if(vector1[i].getPhi() != vector2[i].getPhi()) { testPass = false; break; }
+            }
+        }
+    }
+    
+    if(testPass == false)
+    {
+        cout << "\nTest class has FAILED " << description << " comparison!" << endl;
+        return false;
+    }
+    else
+    {
+        cout << "\nTest class has passed " << description << " comparison." << endl;
+    }
+    return true;
 }
