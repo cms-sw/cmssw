a187e709322124cc686c68fafb0cafe882027cb9
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-03-29 16:41:54 +0000

    from Noemi...

--- CondCore/OptAlignPlugins/src/plugin.cc
+++ CondCore/OptAlignPlugins/src/plugin.cc
@@ -9,8 +9,17 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignments.h"
 #include "CondFormats/OptAlignObjects/interface/CSCZSensors.h"
+
+#include "CondFormats/OptAlignObjects/interface/MBAChBenchCalPlate.h"
+#include "CondFormats/OptAlignObjects/interface/MBAChBenchSurveyPlate.h"
 #include "CondFormats/DataRecord/interface/OpticalAlignmentsRcd.h"
 #include "CondFormats/DataRecord/interface/CSCZSensorsRcd.h"
+
+#include "CondFormats/DataRecord/interface/MBAChBenchCalPlateRcd.h"
+#include "CondFormats/DataRecord/interface/MBAChBenchSurveyPlateRcd.h"
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(OpticalAlignmentsRcd,OpticalAlignments);
 REGISTER_PLUGIN(CSCZSensorsRcd,CSCZSensors);
+
+REGISTER_PLUGIN(MBAChBenchCalPlateRcd,MBAChBenchCalPlate);
+REGISTER_PLUGIN(MBAChBenchSurveyPlateRcd,MBAChBenchSurveyPlate);
