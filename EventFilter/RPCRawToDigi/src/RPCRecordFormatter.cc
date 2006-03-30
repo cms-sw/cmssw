f28d9e1b806da758ff048c7671c31cba4f23e141
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-03-30 15:19:07 +0000

    does the unpacking

--- EventFilter/RPCRawToDigi/src/RPCRecordFormatter.cc
+++ EventFilter/RPCRawToDigi/src/RPCRecordFormatter.cc
@@ -9,23 +9,23 @@
 
 #include "EventFilter/RPCRawToDigi/interface/RPCRecordFormatter.h"
 #include "EventFilter/RPCRawToDigi/interface/RPCRecord.h"
-#include "EventFilter/RPCRawToDigi/interface/RPCBXData.h"
-#include "EventFilter/RPCRawToDigi/interface/RPCChamberData.h"
-#include "EventFilter/RPCRawToDigi/interface/RPCChannelData.h"
+#include "EventFilter/RPCRawToDigi/interface/RPCLinkBoardData.h"
 #include "EventFilter/RPCRawToDigi/interface/RMBErrorData.h"
+#include "EventFilter/RPCRawToDigi/interface/RPCRawDataPattern.h"
 
 #include "DataFormats/MuonDetId/interface/RPCDetId.h"
 #include "DataFormats/RPCDigi/interface/RPCDigiCollection.h"
 #include "DataFormats/RPCDigi/interface/RPCDigi.h"
+
+#include <DataFormats/FEDRawData/interface/FEDHeader.h>
+#include <DataFormats/FEDRawData/interface/FEDTrailer.h>
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include <vector>
 
-RPCRecordFormatter::RPCRecordFormatter(bool printout){
-	verbosity=printout;
-	currentBx=0;
+RPCRecordFormatter::RPCRecordFormatter():currentRMB(0),currentChannel(0){
 }
 
 RPCRecordFormatter::~RPCRecordFormatter(){
@@ -33,109 +33,109 @@ RPCRecordFormatter::~RPCRecordFormatter(){
 
 /// Note that it takes a reference to std::auto_ptr<RPCDigiCollection> because
 /// I don't want to transfer ownership of RPCDigiCollection (I.S.)
-void RPCRecordFormatter::recordUnpack(RPCRecord::recordTypes typeOfRecord, const unsigned char* recordIndex, 
-std::auto_ptr<RPCDigiCollection> & prod){
+void RPCRecordFormatter::recordUnpack(RPCRecord & theRecord,
+		std::auto_ptr<RPCDigiCollection> & prod, RPCFEDData & rawData){
     
-       
+   int bx=0;
+   ///Temporary Phony RPCDetId
+   int region=0, ring=-1, station=1, sector=1, layer =1, subsector =1, roll=2;
+   RPCDetId DetId(region, ring, station, sector, layer, subsector, roll);
+
+   enum RPCRecord::recordTypes typeOfRecord = theRecord.type();
+   const unsigned int* recordIndexInt= theRecord.buf();
+
    if(typeOfRecord==RPCRecord::StartOfBXData)      {
-	currentBx = this->unpackBXRecord(recordIndex);
+	bx = this->unpackBXRecord(recordIndexInt);
+        rawData.addBXData(bx);
    }	   
     
     if(typeOfRecord==RPCRecord::StartOfChannelData) {
-	currentDetId =this->unpackChannelRecord(recordIndex);
+    	currentRMB=0;
+	currentChannel=0;
+	this->unpackChannelRecord(recordIndexInt);
     }
    
-    /// Unpacking Strips With Hit
-    if(typeOfRecord==RPCRecord::ChamberData)	    {
-	std::vector<int> stripsOn=this->unpackChamberRecord(recordIndex);
-	for(std::vector<int>::iterator pStrip = stripsOn.begin(); pStrip !=
-    		      stripsOn.end(); ++pStrip){
+    /// Unpacking BITS With Hit (uniquely related to strips with hit)
+    if(typeOfRecord==RPCRecord::LinkBoardData)	    {
+	RPCLinkBoardData lbData=this->unpackLBRecord(recordIndexInt);
+    	rawData.addRMBData(currentRMB,currentChannel, lbData);  
 
-		int strip = *(pStrip);
+	std::vector<int> bits=lbData.bitsOn();
+	for(std::vector<int>::iterator pBit = bits.begin(); pBit !=
+    		      bits.end(); ++pBit){
+
+		int bit = *(pBit);
 		/// Creating RPC digi
-		RPCDigi digi(strip, currentBx);
+		/// When channel mapping available calculate strip
+		///and replace bit with strip
+		RPCDigi digi(bit,bx);
 
 		/// Committing digi to the product
-		prod->insertDigi(currentDetId,digi);
+		prod->insertDigi(DetId,digi);
           }
     }
     
-    if(typeOfRecord==RPCRecord::RMBDiscarded) this->unpackRMBCorruptedRecord(recordIndex);
-    if(typeOfRecord==RPCRecord::DCCDiscarded) rpcData.addDCCDiscarded();
-    }
-
-
-
-
-int RPCRecordFormatter::unpackBXRecord(const unsigned char* recordIndex) {
-
-const unsigned int* recordIndexInt=reinterpret_cast<const unsigned int*>(recordIndex);
-
-    RPCBXData bxData(recordIndexInt);
-    edm::LogInfo ("RPCUnpacker")<<"Found BX record, BX= "<<bxData.bx();
-    return bxData.bx();
-    rpcData.addBXData(bxData);
-
-} 
-
-
-RPCDetId RPCRecordFormatter::unpackChannelRecord(const unsigned char* recordIndex) {
-
-const unsigned int* recordIndexInt=reinterpret_cast<const unsigned int*>(recordIndex);
-
-    RPCChannelData chnData(recordIndexInt);
-    edm::LogInfo ("RPCUnpacker")<<"Found start of Channel Data Record, Channel: "<< chnData.channel()<<
- 	 " Readout/Trigger Mother Board: "<<chnData.tbRmb();
-    
-    rpcData.addChnData(chnData);
-
-    ///Temporary Phony RPCDetId
-    int region=0, ring=-1, station=1, sector=1, layer =1, subsector =1, roll=2;
-    RPCDetId detId(region, ring, station, sector, layer, subsector, roll);
-    //RPCDetId detId/*=chnData.detId()*/;
-    return detId;
-
-
-
-} 
-
-std::vector<int> RPCRecordFormatter::unpackChamberRecord(const unsigned char* recordIndex) {
-
-const unsigned int* recordIndexInt=reinterpret_cast<const unsigned int*>(recordIndex);
-
-   RPCChamberData cmbData(recordIndexInt);
-    edm::LogInfo ("RPCUnpacker")<< "Found Chamber Data, Chamber Number: "<<cmbData.chamberNumber()<<
- 	" Partition Data "   <<cmbData.partitionData()<<
- 	" Half Partition "   << cmbData.halfP()<<
- 	" Data Truncated: "  << cmbData.eod()<<
- 	" Partition Number " <<  cmbData.partitionNumber();
-    
-    rpcData.addRPCChamberData(cmbData);
-
-    vector<int> stripID/*=cmbData.getStrips()*/;
-    ///Strip Id
-    int partData= cmbData.partitionData();
-    int partNumb= cmbData.partitionNumber();
-    for (int ii=0; ii<8; ii++) {
-    	if ((partData>>ii) & 0x1) stripID.push_back(partNumb*8 + ii);
-    }
-    
-
-    return stripID;
+    if(typeOfRecord==RPCRecord::RMBDiscarded || typeOfRecord==RPCRecord::RMBCorrupted) this->unpackRMBCorruptedRecord(recordIndexInt,typeOfRecord,rawData);
+    if(typeOfRecord==RPCRecord::DCCDiscarded) rawData.addDCCDiscarded();
 }
 
 
 
-void RPCRecordFormatter::unpackRMBCorruptedRecord(const unsigned char* recordIndex) {
+
+int RPCRecordFormatter::unpackBXRecord(const unsigned int* recordIndexInt) {
+    
+    int bx= ( *recordIndexInt >> rpcraw::bx::BX_SHIFT )& rpcraw::bx::BX_MASK ;
+    edm::LogInfo ("RPCUnpacker")<<"Found BX record, BX= "<<bx;
+    return bx;
+
+} 
+
+
+void RPCRecordFormatter::unpackChannelRecord(const unsigned int* recordIndexInt) {
+
+    currentChannel= (*recordIndexInt>> rpcraw::channel::CHANNEL_SHIFT )& rpcraw::channel::CHANNEL_MASK;
+    currentRMB=(*recordIndexInt>> rpcraw::channel::TB_RMB_SHIFT)  & rpcraw::channel::TB_RMB_MASK;
+
+    edm::LogInfo ("RPCUnpacker")<<"Found start of Channel Data Record, Channel: "<<currentChannel<<
+ 	 " Readout Mother Board: "<<currentRMB;
+} 
+
+RPCLinkBoardData RPCRecordFormatter::unpackLBRecord(const unsigned int* recordIndexInt) {
+    
+    int partitionData= (*recordIndexInt>>rpcraw::lb::PARTITION_DATA_SHIFT)&rpcraw::lb::PARTITION_DATA_MASK;
+    int halfP = (*recordIndexInt >> rpcraw::lb::HALFP_SHIFT ) & rpcraw::lb::HALFP_MASK;
+    int eod = (*recordIndexInt >> rpcraw::lb::EOD_SHIFT ) & rpcraw::lb::EOD_MASK;
+    int partitionNumber = (*recordIndexInt >> rpcraw::lb::PARTITION_NUMBER_SHIFT ) & rpcraw::lb::PARTITION_NUMBER_MASK;
+    int lbNumber = (*recordIndexInt >> rpcraw::lb::LB_SHIFT ) & rpcraw::lb::LB_MASK ;
+
+    edm::LogInfo ("RPCUnpacker")<< "Found LB Data, LB Number: "<< lbNumber<<
+ 	" Partition Data "<< partitionData<<
+ 	" Half Partition " <<  halfP<<
+ 	" Data Truncated: "<< eod<<
+ 	" Partition Number " <<  partitionNumber;
+    std::vector<int> bits;
+    bits.clear();
+    for(int bb=0; bb<8;++bb) {
+	if ((partitionData>>bb)& 0X1) bits.push_back( partitionNumber* rpcraw::bits::BITS_PER_PARTITION + bb); 
+    }
+    
+    RPCLinkBoardData lbData;
+    lbData.setBits(bits);
+    lbData.setHalfP(halfP);
+    lbData.setEod(eod);
+    lbData.setPartitionNumber(partitionNumber);
+    lbData.setLbNumber(lbNumber);
+
+    return lbData ;
+}
 
 
 
-const unsigned int* recordIndexInt=reinterpret_cast<const unsigned int*>(recordIndex);
-
-     RMBErrorData  discarded(recordIndexInt);
-     rpcData.addRMBDiscarded(discarded);
-     rpcData.addRMBCorrupted(discarded);
-
+void RPCRecordFormatter::unpackRMBCorruptedRecord(const unsigned int* recordIndexInt,enum RPCRecord::recordTypes type,RPCFEDData & rawData) {
+    int channel = (* recordIndexInt>> rpcraw::error::CHANNEL_SHIFT )& rpcraw::error::CHANNEL_MASK;
+    int tbRmb   = (* recordIndexInt>> rpcraw::error::TB_RMB_SHIFT)  & rpcraw::error::TB_RMB_MASK;
+    if(type==RPCRecord::RMBDiscarded) rawData.addRMBDiscarded(tbRmb, channel);
+    if(type==RPCRecord::RMBCorrupted) rawData.addRMBCorrupted(tbRmb, channel);  
  }
 
 
