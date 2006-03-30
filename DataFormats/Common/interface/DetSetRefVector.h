a560853980924bcaf359d26bc68ca1082e707eea
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-03-30 20:46:58 +0000

    used for regional reconstruction

---
+++ DataFormats/Common/interface/DetSetRefVector.h
@@ -0,0 +1,280 @@
+#ifndef Common_DetSetRefVector_h
+#define Common_DetSetRefVector_h
+
+/*----------------------------------------------------------------------
+  
+DetSeReftVector: A collection of homogeneous objects that can be used for
+an EDProduct. DetSetVector is *not* designed for use as a base class
+(it has no virtual functions).
+
+DetSetVector<T> contains a vector<DetSet<T> >, sorted on DetId, and
+provides fast (O(log n)) lookups, but only O(n) insertion.
+
+It provides an interface such that EdmRef2 can directly reference, and
+provide access to, individual T objects.
+
+The collection appears to the user as if it were a sequence of
+DetSet<T>; e.g., operator[] returns a DetSet<T>&. However, the
+argument to operator[] specifies the (DetId) identifier of the vector
+to be returned, *not* the ordinal number of the T to be returned.
+
+			  ------------------
+   It is critical that users DO NOT MODIFY the id data member of a
+   DetSet object in a DetSetVector.
+			  ------------------
+
+$Id: DetSetVector.h,v 1.2 2006/03/03 17:10:39 chrjones Exp $
+
+----------------------------------------------------------------------*/
+
+#include <algorithm>
+#include <vector>
+
+#include "boost/concept_check.hpp"
+#include "boost/iterator/indirect_iterator.hpp"
+
+#include "DataFormats/Common/interface/traits.h"
+#include "DataFormats/Common/interface/DetSet.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+
+namespace edm {
+
+  //------------------------------------------------------------
+  // Forward declarations
+  template <class T, class C> class DetSetRefVector;
+
+  //------------------------------------------------------------
+  // Helper function, to regularize throwing of exceptions.
+  //------------------------------------------------------------
+
+  namespace dsrvdetail
+  {
+    // Throw an edm::Exception with an appropriate message
+    inline
+    void _throw_range(det_id_type i)
+    {
+      throw edm::Exception(errors::InvalidReference)
+	<< "DetSetRefVector::operator[] called with index not in collection;\n"
+	<< "index value: " << i;
+    }
+  }
+
+  //------------------------------------------------------------
+  //
+  namespace refhelper {
+    template<typename T, typename C >
+    struct FindDetSetForDetSetVector : public std::binary_function<const C &, edm::det_id_type, const DetSet<T>*> {
+      typedef FindDetSetForDetSetVector<T,C> self;
+      typename self::result_type operator()( typename self::first_argument_type iContainer,  typename self::second_argument_type iIndex) const {
+        return &(*(iContainer.find(iIndex)));
+      }
+    };
+  }
+  
+  //allow comparison of edm::Ref<...> to the det_it_type.  This allows searching without dereferencing the edm::Ref
+  template <class T, class C=DetSetVector<T> >
+    struct CompareRefDetSet {
+    typedef Ref<C, DetSet<T>, refhelper::FindDetSetForDetSetVector<T,C> > ref_type; 
+      bool operator()(const ref_type& iRef, det_id_type iId) {
+        return iRef.index() < iId;
+      }
+      bool operator()(det_id_type iId, const ref_type& iRef ) {
+        return iId < iRef.index();
+      }
+    };
+
+  template <class T, class C=DetSetVector<T> >
+  class DetSetRefVector 
+  {
+    /// DetSetVector requires that T objects can be compared with
+    /// operator<.
+    BOOST_CLASS_REQUIRE(T, boost, LessThanComparableConcept);
+  public:
+
+    typedef DetSet<T>           detset;
+    typedef detset              value_type;
+    typedef Ref<C, DetSet<T>, refhelper::FindDetSetForDetSetVector<T,C> > ref_type;
+    typedef std::vector<ref_type> collection_type;
+
+    typedef detset const&  const_reference;
+
+    //iterator returns a DetSet<T> instead of a Ref<...>
+    typedef boost::indirect_iterator<typename collection_type::const_iterator> const_iterator;
+    typedef typename collection_type::size_type      size_type;
+
+    /// Compiler-generated default c'tor, copy c'tor, d'tor and
+    /// assignment are correct.
+
+    // Add the following only if needed.
+    //template <class InputIterator>
+    //DetSetRefVector(InputIterator b, InputIterator e);
+
+    DetSetRefVector() {}
+    
+    template <typename THandle>
+      DetSetRefVector(const THandle& iHandle, const std::vector<det_id_type>& iDets) {
+        sets_.reserve(iDets.size());
+        det_id_type sanityCheck = 0;
+        for(std::vector<det_id_type>::const_iterator itDetId = iDets.begin();
+            itDetId != iDets.end();
+            ++itDetId) {
+          assert( sanityCheck <= *itDetId && "vector of det_id_type was not ordered");
+          sanityCheck = *itDetId;
+          //the last 'false' says to not get the data right now
+          sets_.push_back( ref_type(iHandle, *itDetId,false) );
+        }
+      }
+
+    void swap(DetSetRefVector& other);
+
+    //    DetSetVector& operator=(DetSetVector const& rhs);
+
+    /// Return true if we contain no DetSets.
+    bool empty() const;
+
+    /// Return the number of contained DetSets
+    size_type size() const;
+
+    // Do we need a short-hand method to return the number of T
+    // instances? If so, do we optimize for size (calculate on the
+    // fly) or speed (keep a current cache)?
+
+    /// Return an iterator to the DetSet with the given id, or end()
+    /// if there is no such DetSet.
+    const_iterator find(det_id_type id) const;
+
+    /// Return a reference to the DetSet with the given detector
+    /// ID. If there is no such DetSet, we throw an edm::Exception.
+    /// **DO NOT MODIFY THE id DATA MEMBER OF THE REFERENCED DetSet!**
+    const_reference operator[](det_id_type i) const;
+
+    /// Return an iterator to the first DetSet.
+    const_iterator begin() const;
+
+    /// Return the off-the-end iterator.
+    const_iterator end() const;
+
+    /// This function will be called by the edm::Event after the
+    /// DetSetVector has been inserted into the Event.
+    //void post_insert();
+
+  private:
+    collection_type   sets_;
+
+  };
+
+  template <class T, class C>
+  inline
+  void
+  DetSetRefVector<T,C>::swap(DetSetRefVector<T,C>& other) 
+  {
+    sets_.swap(other.sets_);
+  }
+
+  template <class T, class C>
+  inline
+  bool
+  DetSetRefVector<T,C>::empty() const 
+  {
+    return sets_.empty();
+  }
+
+  template <class T, class C>
+  inline
+  typename DetSetRefVector<T,C>::size_type
+  DetSetRefVector<T,C>::size() const
+  {
+    return sets_.size();
+  }
+
+  template <class T, class C>
+  inline
+  typename DetSetRefVector<T,C>::const_iterator
+  DetSetRefVector<T,C>::find(det_id_type id) const
+  {
+    if(empty()) {
+      return sets_.end();
+    }
+    std::pair<typename collection_type::const_iterator,typename collection_type::const_iterator> p = 
+    std::equal_range(sets_.begin(), sets_.end(), id, CompareRefDetSet<T,C>() );
+    if ( p.first == p.second ) return sets_.end();
+    // The range indicated by [p.first, p.second) should be exactly of
+    // length 1.
+    assert( std::distance(p.first, p.second) == 1 );
+    return p.first;
+  }
+
+  template <class T, class C>
+  inline
+  typename DetSetRefVector<T,C>::const_reference
+  DetSetRefVector<T,C>::operator[](det_id_type i) const 
+  {
+    // Find the right DetSet, and return a reference to it.  Throw if
+    // there is none.
+    const_iterator it = this->find(i);
+    if ( it == this->end() ) dsrvdetail::_throw_range(i);
+    return *it;
+  }
+
+  template <class T, class C>
+  inline
+  typename DetSetRefVector<T,C>::const_iterator
+  DetSetRefVector<T,C>::begin() const
+  {
+    return sets_.begin();
+  }
+
+  template <class T, class C>
+  inline
+  typename DetSetRefVector<T,C>::const_iterator
+  DetSetRefVector<T,C>::end() const
+  {
+    return sets_.end();
+  }
+
+
+//specialize behavior of edm::Ref to get access to the 'Det'
+
+  namespace refhelper {
+    template<typename T, typename C>
+    struct FindForDetSetRefVector : public std::binary_function<const DetSetRefVector<T,C>&, std::pair<det_id_type, typename DetSet<T>::collection_type::size_type>, const T*> {
+      typedef FindForDetSetRefVector<T,C> self;
+      typename self::result_type operator()( typename self::first_argument_type iContainer,  typename self::second_argument_type iIndex) {
+        return &(*(iContainer.find(iIndex.first)->data.begin()+iIndex.second));
+      }
+    };
+    
+    template<typename T, typename C> 
+      struct FindTrait<DetSetRefVector<T,C>,T> {
+        typedef FindForDetSetRefVector<T,C> value;
+      };
+  }
+  
+   //helper function to make it easier to create a edm::Ref
+   
+   template<class HandleT>
+   Ref< typename HandleT::element_type, typename HandleT::element_type::value_type::value_type> 
+   makeRefToDetSetRefVector(const HandleT& iHandle, 
+             det_id_type iDetID,
+             typename HandleT::element_type::value_type::const_iterator itIter) {
+      typedef typename HandleT::element_type Vec;
+      typename Vec::value_type::collection_type::size_type index=0;
+      typename Vec::const_iterator itFound = iHandle->find(iDetID);
+      index += (itIter- itFound->data.begin());
+      return    Ref< typename HandleT::element_type, typename HandleT::element_type::value_type::value_type> (iHandle,std::make_pair(iDetID,index));
+   }
+   
+   template<class HandleT>
+   Ref< typename HandleT::element_type, typename HandleT::element_type::value_type::value_type> 
+   makeRefToDetSetRefVector(const HandleT& iHandle, 
+             det_id_type iDetID,
+             typename HandleT::element_type::value_type::iterator itIter) {
+      typedef typename HandleT::element_type Vec;
+      typename Vec::detset::const_iterator itIter2 = itIter;
+      return makeRefToDetSetRefVector(iHandle,iDetID,itIter2);
+   }
+}  
+#endif
---
+++ DataFormats/Common/test/DetSetRefVector_t.cppunit.cc
@@ -0,0 +1,157 @@
+// $Id: testDetSetRefVector.cc,v 1.11 2006/02/21 09:59:10 tboccali Exp $
+#include <cppunit/extensions/HelperMacros.h>
+#include "DataFormats/Common/interface/DetSetRefVector.h"
+
+
+class testDetSetRefVector : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(testDetSetRefVector);
+  CPPUNIT_TEST(checkConstruction);
+  CPPUNIT_TEST(checkFind);
+  CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp() {}
+  void tearDown() {}
+  void checkConstruction(); 
+  void checkFind();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(testDetSetRefVector);
+namespace testdetsetrefvector {
+  class Value
+  {
+public:
+    // VALUES must be default constructible
+    Value() : d_(0.0) { }
+    
+    // This constructor is used for testing; it is not required by the
+    // concept VALUE.
+    explicit Value(double d) : d_(d) { }
+    
+    // The compiler-generated copy c'tor seems to do the wrong thing!
+    Value(Value const& other) : d_(other.d_) { }
+    
+    // This access function is used for testing; it is not required by
+    // the concept VALUE.
+    double val() const { return d_; }
+    
+    // VALUES must be destructible 
+    ~Value() {}
+    
+    
+    // VALUES must be LessThanComparable
+    bool operator< (Value const& other) const 
+  { return d_ < other.d_; }  
+    
+    // The private stuff below is all implementation detail, and not
+    // required by the concept VALUE.
+private:
+    double        d_;
+  };  
+  
+  template<class T>
+    struct MyHandle {
+      typedef T element_type;
+      MyHandle(const T* iProd) : prod_(iProd) {}
+      edm::ProductID id() const {return edm::ProductID(1);}
+      const T* product() const {return prod_;}
+      const T* prod_;
+      const T* operator->() const {return prod_;}
+    };
+  
+}
+
+using namespace testdetsetrefvector;
+typedef edm::DetSetVector<Value> dsv_type;
+typedef dsv_type::detset        detset;
+
+void
+testDetSetRefVector::checkConstruction()
+{
+  dsv_type c;
+  detset    d3;
+  Value v1(1.1);
+  Value v2(2.2);
+  d3.id = edm::det_id_type(3);
+  d3.data.push_back(v1);
+  d3.data.push_back(v2);
+  c.insert(d3);
+  detset    d1;
+  Value v1a(4.1);
+  Value v2a(3.2);
+  d1.id = edm::det_id_type(1);
+  d1.data.push_back(v1a);
+  d1.data.push_back(v2a);
+  c.insert(d1);
+  c.post_insert();
+
+  MyHandle<dsv_type> pc2(&c);
+
+  {
+    std::vector<edm::det_id_type> ids;
+    ids.push_back(1);
+    ids.push_back(3);
+    
+    edm::DetSetRefVector<Value> refVector(pc2, ids);
+    CPPUNIT_ASSERT(refVector.size() == ids.size());
+    
+    dsv_type::const_iterator dsvItr = c.begin();
+    for( edm::DetSetRefVector<Value>::const_iterator it = refVector.begin();
+         it != refVector.end();
+         ++it,++dsvItr) {
+      CPPUNIT_ASSERT( it->id == dsvItr->id );
+      CPPUNIT_ASSERT( it->data.size() == dsvItr->data.size());
+    }
+  }
+
+  {
+    std::vector<edm::det_id_type> ids;
+    ids.push_back(3);
+    
+    edm::DetSetRefVector<Value> refVector(pc2, ids);
+    CPPUNIT_ASSERT(refVector.size() == ids.size());
+    
+    edm::DetSetRefVector<Value>::const_iterator itRef = refVector.begin();
+    for( std::vector<edm::det_id_type>::const_iterator itId = ids.begin();
+         itId != ids.end();
+         ++itRef,++itId) {
+      CPPUNIT_ASSERT( itRef->id == *itId);
+      CPPUNIT_ASSERT( itRef->id == c.find(*itId)->id );
+      CPPUNIT_ASSERT( itRef->data.size() == c.find(*itId)->data.size());
+    }
+  }
+}
+
+void
+testDetSetRefVector::checkFind()
+{
+  dsv_type c;
+  detset    d3;
+  Value v1(1.1);
+  Value v2(2.2);
+  d3.id = edm::det_id_type(3);
+  d3.data.push_back(v1);
+  d3.data.push_back(v2);
+  c.insert(d3);
+  detset    d1;
+  Value v1a(4.1);
+  Value v2a(3.2);
+  d1.id = edm::det_id_type(1);
+  d1.data.push_back(v1a);
+  d1.data.push_back(v2a);
+  c.insert(d1);
+  c.post_insert();
+  
+  MyHandle<dsv_type> pc2(&c);
+  
+  {
+    std::vector<edm::det_id_type> ids;
+    ids.push_back(1);
+    ids.push_back(3);
+    
+    edm::DetSetRefVector<Value> refVector(pc2, ids);
+    
+    CPPUNIT_ASSERT( refVector.find(1)->id == c.find(1)->id);
+    CPPUNIT_ASSERT( refVector.find(3)->id == c.find(3)->id);
+    CPPUNIT_ASSERT( refVector.find(4) == refVector.end());
+  }
+}
