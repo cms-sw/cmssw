88ebbbad15019fb6b0415b6f820bc2cbffa1bed5
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-03-30 14:41:07 +0000

    cosmetics

--- EventFilter/RPCRawToDigi/interface/RPCMonitorInterface.h
+++ EventFilter/RPCRawToDigi/interface/RPCMonitorInterface.h
@@ -5,14 +5,14 @@
  *
  * Interface to the Data Quality Monitoring Module.
  *  
- *  $Date: 2005/12/12 09:11:35 $
+ *  $Date: 2005/12/15 17:45:16 $
  *  $Revision: 1.1 $
  *
  * \author Ilaria Segoni (CERN)
  *
  */
 
-class RPCEventData;
+class RPCFEDData;
 
 class RPCMonitorInterface{
 
@@ -20,7 +20,7 @@ public:
 
     RPCMonitorInterface(){}
     virtual ~RPCMonitorInterface(){}
-    virtual void process(RPCEventData & rpcData)=0;
+    virtual void process(RPCFEDData & rpcData)=0;
 
       
 
