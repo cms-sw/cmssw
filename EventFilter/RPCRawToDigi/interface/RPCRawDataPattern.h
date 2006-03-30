1c2fe1e1619d979c9c3cdb55cf5a3b54b73abb34
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-03-30 15:13:06 +0000

    pattern of RPC raw data in namespaces

---
+++ EventFilter/RPCRawToDigi/interface/RPCRawDataPattern.h
@@ -0,0 +1,45 @@
+#ifndef RPCRawDataPattern_h
+#define RPCRawDataPattern_h
+
+namespace rpcraw{
+	namespace bx{
+		static const int BX_MASK  = 0XC;
+		static const int BX_SHIFT = 0;
+	}
+	
+	namespace channel{
+		static const int CHANNEL_MASK  = 0X1F;
+		static const int CHANNEL_SHIFT =0;
+
+		static const int TB_RMB_MASK = 0X3F;
+		static const int TB_RMB_SHIFT =5;
+	}
+	namespace lb{
+		static const int PARTITION_DATA_MASK  = 0XFF;
+		static const int PARTITION_DATA_SHIFT =0;
+
+		static const int HALFP_MASK = 0X1;
+		static const int HALFP_SHIFT =8;
+
+		static const int EOD_MASK = 0X1;
+		static const int EOD_SHIFT =9;
+
+		static const int PARTITION_NUMBER_MASK = 0XF;
+		static const int PARTITION_NUMBER_SHIFT =10;
+
+		static const int LB_MASK = 0X3;
+		static const int LB_SHIFT =14;
+	}
+	namespace bits{
+		static const int BITS_PER_PARTITION=8;
+	}
+	namespace error{
+		static const int CHANNEL_MASK  = 0X1F;
+		static const int CHANNEL_SHIFT =0;
+
+		static const int TB_RMB_MASK = 0X3F;
+		static const int TB_RMB_SHIFT =5;
+	}
+}
+
+#endif
