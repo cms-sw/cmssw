067dd9def70d5033f7ea456cab2de28e2e51ff5d
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-03-30 16:49:18 +0000

    Removed HO ( not handled by this coder )

--- CalibFormats/HcalObjects/interface/HcalTPGCoder.h
+++ CalibFormats/HcalObjects/interface/HcalTPGCoder.h
@@ -4,20 +4,18 @@
 #include "CalibFormats/CaloObjects/interface/IntegerCaloSamples.h"
 #include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HFDataFrame.h"
-#include "DataFormats/HcalDigi/interface/HODataFrame.h"
 
 /** \class HcalTPGCoder
   *  
   * Converts ADC to ET for use in the TPG path
   *
-  * $Date: $
-  * $Revision: $
+  * $Date: 2006/03/27 21:05:12 $
+  * $Revision: 1.1 $
   * \author J. Mans - Minnesota
   */
 class HcalTPGCoder {
 public:
   virtual void adc2ET(const HBHEDataFrame& df, IntegerCaloSamples& ics) const = 0;
-  virtual void adc2ET(const HODataFrame& df, IntegerCaloSamples& ics) const = 0;
   virtual void adc2ET(const HFDataFrame& df, IntegerCaloSamples& ics) const = 0;
 };
 
