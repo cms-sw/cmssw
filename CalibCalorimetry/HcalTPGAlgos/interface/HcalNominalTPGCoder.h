a98584ef732957eceae8b23f45cb3ab91fa12e7f
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-04-03 16:37:39 +0000

    Initial version

---
+++ CalibCalorimetry/HcalTPGAlgos/interface/HcalNominalTPGCoder.h
@@ -0,0 +1,40 @@
+#ifndef CALIBCALORIMETRY_HCALTPGALGOS_HCALNOMINALTPGCODER_H
+#define CALIBCALORIMETRY_HCALTPGALGOS_HCALNOMINALTPGCODER_H 1
+
+#include "CalibFormats/HcalObjects/interface/HcalTPGCoder.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+#include "CalibFormats/HcalObjects/interface/HcalNominalCoder.h"
+
+class HcalDbService;
+class CaloGeometry;
+
+/** \class HcalNominalTPGCoder
+  *  
+  * The nominal coder uses the HcalNominalCoder to perform ADC->fC,
+  * then uses the averaged gain and pedestal values to convert to GeV (Energy)
+  * and the ideal geometry to apply Energy -> ET.
+  *
+  * $Date: $
+  * $Revision: $
+  * \author J. Mans - Minnesota
+  */
+class HcalNominalTPGCoder {
+public:
+  HcalNominalTPGCoder(double LSB_GeV);
+  virtual ~HcalNominalTPGCoder() {}
+  void setupForChannel(const HcalCalibrations& calib);
+  void setupForAuto(HcalDbService* service=0);
+  void setupGeometry(const CaloGeometry& geom);
+  virtual void adc2ET(const HBHEDataFrame& df, IntegerCaloSamples& ics) const;
+  virtual void adc2ET(const HFDataFrame& df, IntegerCaloSamples& ics) const;  
+private:
+  void determineGainPedestal(const HcalCalibrations& calib, double& gain, int& pedestal) const;
+  double lsbGeV_;
+  HcalNominalCoder coder_;
+  double gain_;
+  int pedestal_;
+  std::vector<double> perpIeta_;
+  HcalDbService* service_;
+};
+
+#endif
---
+++ CalibCalorimetry/HcalTPGAlgos/src/HcalNominalTPGCoder.cc
@@ -0,0 +1,92 @@
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibCalorimetry/HcalTPGAlgos/interface/HcalNominalTPGCoder.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+HcalNominalTPGCoder::HcalNominalTPGCoder(double LSB_GeV) {
+  lsbGeV_=LSB_GeV;
+  gain_=-1.0;
+  pedestal_=0;
+  service_=0;
+  perpIeta_.reserve(42);
+  for (int i=0; i<42; i++) perpIeta_.push_back(1.0);
+}
+
+void HcalNominalTPGCoder::setupForChannel(const HcalCalibrations& calib) {
+  determineGainPedestal(calib,gain_,pedestal_);
+}
+
+void HcalNominalTPGCoder::setupForAuto(HcalDbService* service) {
+  service_=service;
+}
+
+void HcalNominalTPGCoder::setupGeometry(const CaloGeometry& geom) {
+  int iphi=1, depth=1;
+  
+  for (int ieta=1; ieta<=41; ieta++) {
+    HcalSubdetector sd=(ieta<17)?(HcalBarrel):((ieta<29)?(HcalEndcap):(HcalForward));
+    HcalDetId id(sd,ieta,iphi,depth);
+    const CaloCellGeometry* cell=geom.getGeometry(id);
+    if (cell==0) {
+      throw cms::Exception("NullPointer") << "No geometry information for " << id;
+    }
+    perpIeta_[ieta]=1.0/cosh(cell->getPosition().eta());
+  }
+}
+
+void HcalNominalTPGCoder::determineGainPedestal(const HcalCalibrations& calib, double& gain, int& pedestal) const {
+  gain=(calib.gain(0)+calib.gain(1)+calib.gain(2)+calib.gain(3))/4;
+  pedestal=int((calib.pedestal(0)+calib.pedestal(1)+calib.pedestal(2)+calib.pedestal(3))/4+0.5);
+}
+
+void HcalNominalTPGCoder::adc2ET(const HBHEDataFrame& df, IntegerCaloSamples& ics) const {
+  double g;
+  int p;
+  if (service_!=0) {
+    HcalCalibrations c;
+    if (service_->makeHcalCalibration(df.id(),&c)) 
+      determineGainPedestal(c,g,p);
+  } else {
+    g=gain_;
+    p=pedestal_;
+  }
+
+  ics=IntegerCaloSamples(df.id(),df.size());
+  CaloSamples cs;
+
+  coder_.adc2fC(df,cs); // convert to fC
+  for (int i=0; i<cs.size(); i++) {
+    double resp=(cs[i]-p)*g;
+    resp*=perpIeta_[df.id().ietaAbs()];
+    resp=resp/lsbGeV_+0.5;
+    if (resp<0) ics[i]=0;
+    else ics[i]=int(resp);
+  }
+  
+}
+
+void HcalNominalTPGCoder::adc2ET(const HFDataFrame& df, IntegerCaloSamples& ics) const {
+  double g;
+  int p;
+  if (service_!=0) {
+    HcalCalibrations c;
+    if (service_->makeHcalCalibration(df.id(),&c)) 
+      determineGainPedestal(c,g,p);
+  } else {
+    g=gain_;
+    p=pedestal_;
+  }
+
+  ics=IntegerCaloSamples(df.id(),df.size());
+  CaloSamples cs;
+
+  coder_.adc2fC(df,cs); // convert to fC
+  for (int i=0; i<cs.size(); i++) {
+    double resp=(cs[i]-p)*g;
+    resp*=perpIeta_[df.id().ietaAbs()];
+    ics[i]=int(resp/lsbGeV_);
+  }
+  
+}
