f31cb9966b4ddba95179d88db31f6ecac68c40d3
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-04-03 20:46:57 +0000

    new trackproducer with messagelogger

--- RecoTracker/TrackProducer/src/TrackProducerAlgorithm.cc
+++ RecoTracker/TrackProducer/src/TrackProducerAlgorithm.cc
@@ -2,11 +2,6 @@
 
 #include "FWCore/Framework/interface/OrphanHandle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "DataFormats/SiStripDetId/interface/TIBDetId.h"
-#include "DataFormats/SiStripDetId/interface/TOBDetId.h"
-#include "DataFormats/SiStripDetId/interface/TIDDetId.h"
-#include "DataFormats/SiStripDetId/interface/TECDetId.h"
-
 
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "Geometry/CommonDetUnit/interface/TrackingGeometry.h"
@@ -21,14 +16,6 @@
 #include "RecoTracker/TransientTrackingRecHit/interface/TkTransientTrackingRecHitBuilder.h"
 #include "TrackingTools/PatternTools/interface/TransverseImpactPointExtrapolator.h"
 
-class rSorter{
-public:
-  bool operator()(const TransientTrackingRecHit& r1, const TransientTrackingRecHit & r2) const {
-    return r1.globalPosition().perp()<r2.globalPosition().perp();
-  }
-};
-
-
 void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
 				 const MagneticField * theMF,
 				 const TrackCandidateCollection& theTCCollection,
@@ -40,37 +27,15 @@ void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
   int cont = 0;
   for (TrackCandidateCollection::const_iterator i=theTCCollection.begin(); i!=theTCCollection.end();i++)
     {
-
-      std::cout <<" IN THE TC LOOP ======= NEW TRACK"<<std::endl;
-
       const TrackCandidate * theTC = &(*i);
       //convert PTrajectoryStateOnDet to TrajectoryStateOnSurface
       TrajectoryStateTransform transformer;
       PTrajectoryStateOnDet state = theTC->trajectoryStateOnDet();
       DetId * detId = new DetId(state.detId());
-
-      std::cout <<" ON DET "<<detId->rawId()<<std::endl;
-      unsigned int pippo = detId->rawId();
-      std::cout <<" TSOS HIT "<<pippo<<std::endl;
-      if (DetId(pippo).subdetId() == StripSubdetector::TIB ) {
-	std::cout <<" I am TIB "<<TIBDetId(pippo).layer()<<std::endl;
-      }else if (DetId(pippo).subdetId() == StripSubdetector::TOB ) { 
-	std::cout <<" I am TOB "<<TOBDetId(pippo).layer()<<std::endl;
-      }else if (DetId(pippo).subdetId() == StripSubdetector::TEC ) { 
-	std::cout <<" I am TEC "<<TECDetId(pippo).wheel()<<std::endl;
-      }else if (DetId(pippo).subdetId() == StripSubdetector::TID ) { 
-	std::cout <<" I am TID "<<TIDDetId(pippo).wheel()<<std::endl;
-      }else{
-	std::cout <<" I am Pixel "<<std::endl;
-      }
-
-
       TrajectoryStateOnSurface theTSOS = transformer.transientState( state,
 								     &(theG->idToDet(*detId)->surface()), 
 								     theMF);
       
-      std::cout <<" FIRST TSOS "<< theTSOS<<std::endl;
-
       //convert the TrackingRecHit vector to a TransientTrackingRecHit vector
       //meanwhile computes the number of degrees of freedom
       edm::OwnVector<TransientTrackingRecHit> hits;
@@ -91,16 +56,8 @@ void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
       
       delete builder;
 
-      //
-      // test implement a sort on the hits here
-      //
-
-      hits.sort(rSorter());
-      std::cout <<" AFTER SORT "<<std::endl;
       ndof = ndof - 5;
       
-      std::cout <<" NDOF"<<ndof <<std::endl;
-
       //variable declarations
       std::vector<Trajectory> trajVec;
       reco::Track * theTrack;
@@ -108,9 +65,6 @@ void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
       
       //perform the fit: the result's size is 1 if it succeded, 0 if fails
       trajVec = theFitter->fit(theTC->seed(), hits, theTSOS);
-
-
-      std::cout <<" I AM HERE!!!!!! "<<trajVec.size() <<std::endl;
       
       TransverseImpactPointExtrapolator * tipe;
       TrajectoryStateOnSurface tsos;
@@ -129,16 +83,11 @@ void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
 	  innertsos = theTraj->lastMeasurement().updatedState();
 	}
 
-
-	std::cout <<" INNERMOST STATE IS "<<innertsos<<std::endl;
 	
 	//extrapolate the innermost state to the point of closest approach to the beamline
 	tsos = tipe->extrapolate(*(innertsos.freeState()), 
 				 GlobalPoint(0,0,0) );//FIXME Correct?
 	
-	std::cout <<" PERIGEE "<<innertsos<<std::endl;
-
-
 	//compute parameters needed to build a Track from a Trajectory    
 	int charge = tsos.charge();
 	const GlobalTrajectoryParameters& gp = tsos.globalParameters();
@@ -152,9 +101,6 @@ void TrackProducerAlgorithm::run(const TrackingGeometry * theG,
 	    cov( i, j ) = m.fast( i + 1 , j + 1 );
 	math::XYZVector mom( p.x(), p.y(), p.z() );
 	math::XYZPoint  vtx( v.x(), v.y(), v.z() );   
-
-	std::cout <<"   P "<< mom.x()<< " "  <<mom.y()<<" " <<mom.z() <<std::endl;
-	std::cout <<" VTX "<< vtx.x()<<" " <<vtx.y()<<" " <<vtx.z() <<std::endl;
 	
 	//build the Track(chiSquared, ndof, found, invalid, lost, q, vertex, momentum, covariance)
 	theTrack = new reco::Track(theTraj->chiSquared(), 
