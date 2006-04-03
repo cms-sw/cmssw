528613f1d392779678817ddedc45724e065d6d27
Matteo Sani <sha1-b1a59c6d8d0f0b19695380739157bd7a38c2f52f@cern.ch>
2006-04-03 14:15:48 +0000

    Added TC algo

--- RecoLocalMuon/CSCSegment/src/SealModule.cc
+++ RecoLocalMuon/CSCSegment/src/SealModule.cc
@@ -3,9 +3,11 @@
 
 #include <RecoLocalMuon/CSCSegment/src/CSCSegmentProducer.h>
 #include <RecoLocalMuon/CSCSegment/src/CSCSegmentBuilderPluginFactory.h>
+
 #include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoSK.h>
+#include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoTC.h>
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCSegmentProducer)
 DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoSK, "CSCSegAlgoSK");
-
+DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoTC, "CSCSegAlgoTC");
