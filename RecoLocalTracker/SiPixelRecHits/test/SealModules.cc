70950a1a24d24f49a2a46b7a414eaf538e5fb4b7
Petar Maksimovic <sha1-f801b06403fb61272bb7dab37fff23c67b271eee@cern.ch>
2006-04-04 19:28:47 +0000

    First pass of PixelNtuplizer

--- RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
+++ RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
@@ -4,8 +4,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalTracker/SiPixelRecHits/test/ReadPixelRecHit.h"
+#include "RecoLocalTracker/SiPixelRecHits/test/PixelNtuplizer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ReadPixelRecHit)
+DEFINE_ANOTHER_FWK_MODULE(PixelNtuplizer)
 
