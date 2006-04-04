3ab00b3143b1c7bc65b01ead11c2fa18ff85aa07
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-04-04 12:55:07 +0000

    some more first check-ins of Noemi's work

---
+++ CondFormats/DataRecord/src/MBAChBenchSurveyPlateRcd.cc
@@ -0,0 +1,5 @@
+#include "CondFormats/DataRecord/interface/MBAChBenchSurveyPlateRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(MBAChBenchSurveyPlateRcd);
+
