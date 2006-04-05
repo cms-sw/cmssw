c6530dc60a78b892ceb66aed8e5067003e1ac39a
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-04-05 15:45:33 +0000

    added start tests

--- DQM/RPCMonitorClient/interface/QTestEnabler.h
+++ DQM/RPCMonitorClient/interface/QTestEnabler.h
@@ -3,10 +3,12 @@
 
 /** \class QTestEnabler
  * *
- *  Class that intiates the DQM Quality Tests
+ *  Class that creates and defined quality tests based on
+ *  the xml configuration file parsed by QTestConfigurationParser.
+ *
  * 
- *  $Date: 2006/03/13 15:50:02 $
- *  $Revision: 1.3 $
+ *  $Date: 2006/04/05 08:04:04 $
+ *  $Revision: 1.1 $
  *  \author Ilaria Segoni
   */
 
@@ -20,15 +22,27 @@ class QTestEnabler{
 
  public:
  
+  ///Constructor
   QTestEnabler(){}
+  ///Destructor
   ~QTestEnabler(){}
-  
-  void enableTests(std::map<std::string, std::map<std::string, std::string> > tests,MonitorUserInterface * mui); 
-  void EnableXRangeTest(std::string testName, std::map<std::string, std::string> params,MonitorUserInterface * mui); 
-  
+  ///Creates and defines quality tests
+  bool enableTests(std::map<std::string, std::map<std::string, std::string> > tests, MonitorUserInterface * mui); 
+  ///Attaches the Tests to the Monitoring element
+  void startTests(std::map<std::string, std::vector<std::string> > mapMeToTests, MonitorUserInterface * mui);
+  ///Returns the vector containing the names of the quality tests that have been created
   std::vector<std::string> testsReady(){return testsEnabled;}
  
  private:
+  ///Creates ContentsXRangeROOT test
+  void EnableXRangeTest(std::string testName, std::map<std::string, std::string> params,MonitorUserInterface * mui); 
+  ///Creates ContentsYRangeROOT test
+  void EnableYRangeTest(std::string testName, std::map<std::string, std::string> params,MonitorUserInterface * mui); 
+   ///Creates DeadChannelROOT test
+  void EnableDeadChannelTest(std::string testName, std::map<std::string, std::string> params,MonitorUserInterface * mui); 
+   ///Creates NoisyChannelROOTtest
+  void EnableNoisyChannelTest(std::string testName, std::map<std::string, std::string> params,MonitorUserInterface * mui);
+ 
   std::vector<std::string> testsEnabled;
  
 
