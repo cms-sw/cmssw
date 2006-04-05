8b77ed0449aabdfe951a6aa7771c2a1fbc4e529a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-04-05 19:24:38 +0000

    add mySiStripNoisesRcd

---
+++ CondFormats/Calibration/src/T_EventSetup_mySiStripNoises.cc
@@ -0,0 +1,6 @@
+// system include files
+// user include files
+#include "CondFormats/Calibration/interface/mySiStripNoises.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(mySiStripNoises);
