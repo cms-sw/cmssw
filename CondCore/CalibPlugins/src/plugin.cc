e0c957ac0ea3205efd8489907b9362a545c901f7
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-04-05 19:19:52 +0000

    add my test SiStripNoises

--- CondCore/CalibPlugins/src/plugin.cc
+++ CondCore/CalibPlugins/src/plugin.cc
@@ -9,7 +9,9 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/Calibration/interface/Pedestals.h"
 #include "CondFormats/DataRecord/interface/PedestalsRcd.h"
-
+#include "CondFormats/Calibration/interface/mySiStripNoises.h"
+#include "CondFormats/DataRecord/interface/mySiStripNoisesRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(PedestalsRcd,Pedestals);
+REGISTER_PLUGIN(mySiStripNoisesRcd,mySiStripNoises);
