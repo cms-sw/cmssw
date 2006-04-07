fa03186cf13391bdfb0328cddaf977cdcb5864c1
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-04-07 05:20:09 +0000

    remove dependencies on RPCDetId

--- CondFormats/RPCObjects/interface/RPCReadOutMapping.h
+++ CondFormats/RPCObjects/interface/RPCReadOutMapping.h
@@ -5,16 +5,18 @@
  *  Description:
  *       Class to map read-out channels to physical RPC strips
  *
- *  $Date: 2006/03/16 17:50 $
+ *  $Date: 2006/03/16 17:09:57 $
  *  $Revision: 1.1 $
  *  \author Marcello Maggi -- INFN Bari
  *
  */
 
-#include "DataFormats/MuonDetId/interface/RPCDetId.h"
 #include <vector>
+#include <map>
 #include <string>
 
+class RPCdeteIndex;
+class RPCelecIndex;
 class RPCReadOutLink {
 
  public:
@@ -22,20 +24,20 @@ class RPCReadOutLink {
   RPCReadOutLink();
   ~RPCReadOutLink();
 
-  int     dccId;
-  int     tbId;
-  int     lboxId;
-  int     mbId;
-  int     lboardId;
-  int channelId;
-  int regionId;
-  int   diskId;
-  int stationId;
-  int  sectorId;
-  int   layerId;
-  int  subsectorId;
-  int    rollId;
-  int stripId;
+  int       dccId;
+  int        tbId;
+  int      lboxId;
+  int        mbId;
+  int    lboardId;
+  int   channelId;
+  int    regionId;
+  int      diskId;
+  int   stationId;
+  int    sectorId;
+  int     layerId;
+  int subsectorId;
+  int      rollId;
+  int     stripId;
 };    
 
 
@@ -48,47 +50,53 @@ class RPCReadOutMapping {
   ~RPCReadOutMapping();
 
   /// read and store full content
-  void initSetup() const;
+  void initSetup();
 
-  /// transform identifiers
-  RPCDetId readOutToGeometry( int      dccId,
-                              int      tbId,
-                              int      lboxId,
-                              int      mbId,
-			       int lboardId,
-                              int  channelId ) const;
+  void readOutToGeometry( int        dccId,
+			  int         tbId,
+			  int       lboxId,
+			  int         mbId,
+			  int     lboardId,
+			  int    channelId,
+			  int&    regionId,
+			  int&      diskId,
+			  int&   stationId,
+			  int&    sectorId,
+			  int&     layerId,
+			  int& subsectorId,
+			  int&      rollId,
+			  int&     stripId);
 
   /// clear map
   void clear();
 
   /// insert connection
-  int insertReadOutGeometryLink( int     dccId,
-                                 int     tbId,
-                                 int     lboxId,
-                                 int     mbId,
-				 int lboard,
-                                 int channelId,
-				 int regionId,
-                                 int   diskId,
-                                 int stationId,
-                                 int  sectorId,
-                                 int   layerId,
-                                 int    subsectorId,
-                                 int    rollId,
-                                 int    stripId );
-
+  void insertReadOutGeometryLink( int       dccId,
+				  int        tbId,
+				  int      lboxId,
+				  int        mbId,
+				  int      lboard,
+				  int   channelId,
+				  int    regionId,
+				  int      diskId,
+				  int   stationId,
+				  int    sectorId,
+				  int     layerId,
+				  int subsectorId,
+				  int      rollId,
+				  int     stripId );
   /// Access methods to the connections
   typedef std::vector<RPCReadOutLink>::const_iterator const_iterator;
   const_iterator begin() const;
   const_iterator end() const;
 
- private:
-
  public:
-  std::string cellMapVersion;
-  std::string  robMapVersion;
-
   std::vector<RPCReadOutLink> readOutRPCMap;
+
+ private:
+  std::map<RPCdeteIndex,RPCelecIndex> dtoe;
+  std::map<RPCelecIndex,RPCdeteIndex> etod;
+
 
 };
 
