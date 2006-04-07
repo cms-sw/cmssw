a1229e9b330b9c3d68d85228dbef4684a386212d
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-04-07 05:32:52 +0000

    add Index map iterators

--- CondFormats/RPCObjects/src/classes.h
+++ CondFormats/RPCObjects/src/classes.h
@@ -5,3 +5,7 @@
 // Declaration of the iterator (necessary for the generation of the dictionary)
 template std::vector<RPCReadOutLink>::iterator;
 template std::vector<RPCReadOutLink>::const_iterator;
+template std::map<RPCdeteIndex, RPCelecIndex>::iterator;
+template std::map<RPCdeteIndex, RPCelecIndex>::const_iterator;
+template std::map<RPCelecIndex, RPCdeteIndex>::iterator;
+template std::map<RPCelecIndex, RPCdeteIndex>::const_iterator;
