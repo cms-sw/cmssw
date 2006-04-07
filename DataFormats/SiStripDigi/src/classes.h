bc2689f575c9fa4da587da19c81cb0586017af03
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-04-07 15:13:12 +0000

    *** empty log message ***

--- DataFormats/SiStripDigi/src/classes.h
+++ DataFormats/SiStripDigi/src/classes.h
@@ -3,19 +3,37 @@
 
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
-#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
-#include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
-#include "DataFormats/SiStripDigi/interface/SiStripEventSummary.h"
+#include <vector>
 
+#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
 namespace {
   namespace {
-    edm::Wrapper<SiStripDigi> zs_digi;
-    edm::Wrapper< edm::DetSetVector<SiStripDigi> > zs_digis;
-    edm::Wrapper<SiStripRawDigi> rawdigi;
-    edm::Wrapper< edm::DetSetVector<SiStripRawDigi> > raw_digis;
+    edm::Wrapper<SiStripDigi> zs0;
+    edm::Wrapper< std::vector<SiStripDigi>  > zs1;
+    edm::Wrapper< edm::DetSet<SiStripDigi> > zs2;
+    edm::Wrapper< std::vector<edm::DetSet<SiStripDigi> > > zs3;
+    edm::Wrapper< edm::DetSetVector<SiStripDigi> > zs4;
+  }
+}
+
+#include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
+namespace {
+  namespace {
+    edm::Wrapper<SiStripRawDigi> raw0;
+    edm::Wrapper< std::vector<SiStripRawDigi>  > raw1;
+    edm::Wrapper< edm::DetSet<SiStripRawDigi> > raw2;
+    edm::Wrapper< std::vector<edm::DetSet<SiStripRawDigi> > > raw3;
+    edm::Wrapper< edm::DetSetVector<SiStripRawDigi> > raw4;
+  }
+}
+    
+#include "DataFormats/SiStripDigi/interface/SiStripEventSummary.h"
+namespace {
+  namespace {
     edm::Wrapper<SiStripEventSummary::Task> task;
     edm::Wrapper<SiStripEventSummary::FedReadoutMode> fed_mode;
     edm::Wrapper<SiStripEventSummary> summary;
+
   }
 }
 
