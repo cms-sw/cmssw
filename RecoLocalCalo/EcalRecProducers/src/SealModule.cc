8981295696feffc281d92ae5e9ffbef682241b17
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-04-07 12:42:29 +0000

    Added ESRecHitProducer

--- RecoLocalCalo/EcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/EcalRecProducers/src/SealModule.cc
@@ -3,8 +3,10 @@
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalWeightUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalAnalFitUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalRecHitProducer.h"
+#include "RecoLocalCalo/EcalRecProducers/interface/ESRecHitProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalWeightUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalAnalFitUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalRecHitProducer );
+DEFINE_ANOTHER_FWK_MODULE( ESRecHitProducer );
