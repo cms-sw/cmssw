d07cd660838db60273ccc8cf70e3a41a1f2f83d9
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2006-04-10 16:19:32 +0000

    adding these files to the new package

---
+++ RecoEcal/EgammaClusterProducers/interface/TestClusterProducer.h
@@ -0,0 +1,42 @@
+#ifndef RecoEcal_EgammaClusterProducers_TestClusterProducer_h_
+#define RecoEcal_EgammaClusterProducers_TestClusterProducer_h_
+
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoEcal/EgammaClusterAlgos/interface/IslandClusterAlgo.h"
+
+
+//
+
+
+class TestClusterProducer : public edm::EDProducer 
+{
+  
+  public:
+
+      TestClusterProducer(const edm::ParameterSet& ps);
+
+      ~TestClusterProducer();
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+
+   private:
+
+      int nMaxPrintout_; // max # of printouts
+      int nEvt_;         // internal counter of events
+ 
+      std::string clusterCollection_;
+      Island * island_p;
+
+};
+
+
+#endif
+
+
