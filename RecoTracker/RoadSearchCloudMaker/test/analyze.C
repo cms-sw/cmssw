726b0ac0809aeaf170227ebec98cc4f8fd5b5626
Stephen Wagner <sha1-007cb192666c25c72e6f07a7e13df677568d86c7@cern.ch>
2006-04-10 16:18:03 +0000

    updated analyze_head.C and analyze.C in test dir

--- RecoTracker/RoadSearchCloudMaker/test/analyze.C
+++ RecoTracker/RoadSearchCloudMaker/test/analyze.C
@@ -1,17 +1,33 @@
 {
+  // book histograms
+  TFile histofile("track_hists.root","RECREATE");
+  TH1F* h_chisqtr = new TH1F("chisqtr","Track chisq",100,0.,40.);
+  TH1F* h_pttr = new TH1F("pttr","Track pT (GeV)",100,8.0,12.0);
 
   TTree *tree = (TTree*)file.Get("Events");
 
   std::vector<reco::Track> trackCollection;
 
   TBranch *branch = tree->GetBranch("recoTracks_RoadSearchDebugTracks__RoadSearch.obj");
+//  TBranch *branch = tree->GetBranch("recoTracks_trackp__RoadSearch.obj");
   branch->SetAddress(&trackCollection);
 
   for ( unsigned int index = 0; index < tree->GetEntries(); ++index ) {
     std::cout << "index: " << index << std::endl;
     branch->GetEntry(index);
     std::cout << "content: " << trackCollection.size() << std::endl;
+    for ( unsigned int bindex = 0; bindex < trackCollection.size(); ++bindex ) {
+      std::cout << "track: " << bindex << std::endl;
+      reco::Track* track = (reco::Track*)trackCollection[bindex];
+      h_chisqtr->Fill(track->chi2());
+      double pT = sqrt(track->px()*track->px()+track->py()*track->py());
+      std::cout << "pT: " << pT << std::endl;
+      h_pttr->Fill(pT);
+    }
   }
   
 
+  // save histograms
+  histofile.Write();
+  histofile.Close();
 }
--- RecoTracker/RoadSearchCloudMaker/test/analyze_head.C
+++ RecoTracker/RoadSearchCloudMaker/test/analyze_head.C
@@ -3,5 +3,6 @@
   gSystem->Load("libPhysicsToolsFWLite.so"); 
   AutoLibraryLoader::enable();
   TFile file("TrackCandidatesToDebugTracks.root");
+//  TFile file("TrackCandidatesToTracks.root");
 
 }
