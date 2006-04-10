1ab549708672ffafe67ab92514b14772efa807ac
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-04-10 14:21:50 +0000

    methods inGevPerCentimeter added

--- TrackingTools/TrajectoryState/interface/FakeField.h
+++ TrackingTools/TrajectoryState/interface/FakeField.h
@@ -13,6 +13,11 @@ namespace  TrackingTools{
       static  GlobalVector inTesla( const math::XYZPoint& pos)
        	 {return GlobalVector(0, 0, 4);}
       static  MagneticField* field() {return theField;}
+      static  GlobalVector inGevPerCentimeter( const GlobalPoint& pos)
+      	 {return inTesla(pos) * 2.99792458e-3;}
+      static  GlobalVector inGevPerCentimeter( const math::XYZPoint& pos)
+       	 {return inTesla(pos) * 2.99792458e-3;}
+      static  MagneticField* field() {return theField;}
      private:
       static MagneticField* theField;
     };
