ce7ebeeaf7cabe37b13c9c1a8451442aa41618bf
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-04-10 21:52:23 +0000

    Moved cms_payload_o2o to cms_payload_mv

--- OnlineDB/EcalCondDB/sql/cms_payload_mv.sql
+++ OnlineDB/EcalCondDB/sql/cms_payload_mv.sql
@@ -1,26 +1,28 @@
+CREATE MATERIALIZED VIEW ECALPEDESTALS 
+AS 
 /* query to fill ECALPEDESTALS table */
-
   SELECT 
     miov.iov_id iov_value_id, 
     riov.run_num time
   FROM 
-    location_def ldef, run_type_def rdef, run_tag rtag, run_iov riov,
     /* inline view selects the mon_run_iov with the greatest subrun_num */
     (SELECT iov_id, run_iov_id, 
        MAX(subrun_num) KEEP (DENSE_RANK FIRST ORDER BY subrun_num ASC)
-       FROM mon_run_iov GROUP BY iov_id, run_iov_id) miov
+       FROM mon_run_iov@cmsomds GROUP BY iov_id, run_iov_id) miov
+    JOIN run_iov@cmsomds riov ON miov.run_iov_id = riov.iov_id
+    JOIN run_tag@cmsomds rtag ON riov.tag_id = rtag.tag_id
+    JOIN run_type_def@cmsomds rdef ON rtag.run_type_id = rdef.def_id
+    JOIN location_def@cmsomds ldef ON rtag.location_id = ldef.def_id
   WHERE
-      miov.run_iov_id = riov.iov_id
-  AND riov.tag_id = rtag.tag_id
-  AND rdef.def_id = rtag.run_type_id
-  AND ldef.def_id = rtag.location_id
-  AND ldef.location='H4'
+      ldef.location='H4'
   AND rdef.run_type='PEDESTAL'
   AND rdef.config_tag='PEDESTAL-STD'
   AND rdef.config_ver=1
 ;
 
 /* query to fill ECALPEDESTALS_ITEM table */
+CREATE MATERIALIZED VIEW ECALPEDESTALS_ITEM
+AS
   SELECT 
     dat.iov_id iov_value_id,
     /* Creates a column of row numbers ordered by logic_id, for std::vec */
@@ -33,19 +35,18 @@
     dat.ped_mean_g1 mean_x1,
     dat.ped_rms_g1 rms_x1
   FROM 
-    location_def ldef, run_type_def rdef, run_tag rtag, run_iov riov,
     /* inline view selects the mon_run_iov with the greatest subrun_num */
     (SELECT iov_id, run_iov_id, 
        MAX(subrun_num) KEEP (DENSE_RANK FIRST ORDER BY subrun_num ASC)
-       FROM mon_run_iov GROUP BY iov_id, run_iov_id) miov,
-    mon_pedestals_dat dat, channelview cv
-  WHERE dat.iov_id = miov.iov_id
-    AND cv.logic_id = dat.logic_id
-    AND cv.name='Offline_det_id'
-    AND miov.run_iov_id = riov.iov_id
-    AND riov.tag_id = rtag.tag_id
-    AND rdef.def_id = rtag.run_type_id
-    AND ldef.def_id = rtag.location_id
+       FROM mon_run_iov@cmsomds GROUP BY iov_id, run_iov_id) miov
+    JOIN run_iov@cmsomds riov ON miov.run_iov_id = riov.iov_id
+    JOIN run_tag@cmsomds rtag ON riov.tag_id = rtag.tag_id
+    JOIN run_type_def@cmsomds rdef ON rtag.run_type_id = rdef.def_id
+    JOIN location_def@cmsomds ldef ON rtag.location_id = ldef.def_id
+    JOIN mon_pedestals_dat@cmsomds dat ON dat.iov_id = miov.iov_id
+    JOIN channelview@cmsomds cv ON cv.logic_id = dat.logic_id
+  WHERE 
+        cv.name='Offline_det_id'
     AND ldef.location='H4'
     AND rdef.run_type='PEDESTAL'
     AND rdef.config_tag='PEDESTAL-STD'
