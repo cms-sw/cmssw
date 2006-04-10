3c27caa3dd72f7e3f960756b28d6c34acdd67071
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-04-10 20:51:35 +0000

    Moved from IOPool/StreamerData

---
+++ DataFormats/Streamer/interface/StreamedProducts.h
@@ -0,0 +1,82 @@
+#ifndef Streamer_StreamedProducts_h
+#define Streamer_StreamedProducts_h
+
+/*
+  Simple packaging of all the event data that is needed to be serialized
+  for transfer.
+
+  The "other stuff in the SendEvent still needs to be
+  populated.
+
+  The product is paired with its provenance, and the entire event
+  is captured in the SendEvent structure.
+ */
+
+#include <vector>
+
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "DataFormats/Common/interface/Provenance.h"
+#include "DataFormats/Common/interface/BranchDescription.h"
+#include "DataFormats/Common/interface/Timestamp.h"
+#include "DataFormats/Common/interface/EventID.h"
+
+namespace edm {
+
+  // ------------------------------------------
+
+  struct ProdPair
+  {
+    ProdPair():
+      prod_(),prov_(),desc_() { }
+    explicit ProdPair(const EDProduct* p):
+      prod_(p),prov_(),desc_() { }
+    ProdPair(const EDProduct* prod,
+	     const Provenance* prov):
+      prod_(prod),
+      prov_(&prov->event),
+      desc_(&prov->product) { }
+
+    const BranchEntryDescription* prov() const { return prov_; }
+    const EDProduct* prod() const { return prod_; }
+    const BranchDescription* desc() const { return desc_; }
+
+    void clear() { prod_=0; prov_=0; desc_=0; }
+
+    const EDProduct* prod_;
+    const BranchEntryDescription* prov_;
+    const BranchDescription* desc_;
+  };
+
+  // ------------------------------------------
+
+  typedef std::vector<ProdPair> SendProds;
+
+  // ------------------------------------------
+
+  struct SendEvent
+  {
+    SendEvent() { }
+    SendEvent(const EventID& id, const Timestamp& t):id_(id),time_(t) { }
+
+    EventID id_;
+    Timestamp time_;
+    SendProds prods_;
+
+    // other tables necessary for provenance lookup
+  };
+
+  typedef std::vector<BranchDescription> SendDescs;
+
+  struct SendJobHeader
+  {
+    SendJobHeader() { }
+
+    SendDescs descs_;
+    // trigger bit descriptions will be added here and permanent
+    //  provenance values
+  };
+
+
+}
+#endif
+
---
+++ DataFormats/Streamer/src/classes.h
@@ -0,0 +1,7 @@
+#include "DataFormats/Streamer/interface/StreamedProducts.h"
+#include "DataFormats/Common/interface/BranchDescription.h"
+
+#include <vector>
+namespace { namespace {
+}}
+
