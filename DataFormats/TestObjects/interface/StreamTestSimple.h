1fb3d4ba26bf50c8b7a70ca595b72b35abbcbf74
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-04-10 20:58:26 +0000

    Objects for Streamer tests moved here from IOPool/StreamerData

---
+++ DataFormats/TestObjects/interface/StreamTestSimple.h
@@ -0,0 +1,30 @@
+#ifndef TestObjects_StreamTestSimple_h
+#define TestObjects_StreamTestSimple_h
+
+#include "DataFormats/Common/interface/SortedCollection.h"
+#include <vector>
+
+namespace edmtestprod
+{
+  struct Simple
+  {
+    typedef int key_type;
+    int key_;
+    double data_;
+
+    key_type  id() const { return key_; }
+    bool operator==(const Simple& s) const { return true; }
+    bool operator<(const Simple& s) const { return true; }
+  };
+
+  typedef edm::SortedCollection<Simple> StreamTestSimple;
+
+  struct X0123456789012345678901234567890123456789012345678901234567890123456789012345678901
+  {
+    int blob_;
+  };
+
+  typedef X0123456789012345678901234567890123456789012345678901234567890123456789012345678901 Pig;
+}
+
+#endif
--- DataFormats/TestObjects/src/classes.h
+++ DataFormats/TestObjects/src/classes.h
@@ -5,6 +5,10 @@
 #include "DataFormats/TestObjects/interface/ThingCollection.h"
 #include "DataFormats/TestObjects/interface/ToyProducts.h"
 
+#include "DataFormats/TestObjects/interface/StreamTestSimple.h"
+#include "DataFormats/TestObjects/interface/StreamTestThing.h"
+#include "DataFormats/TestObjects/interface/StreamTestTmpl.h"
+
 namespace { namespace {
   edm::Wrapper<edmtest::DummyProduct> dummyw12;
   edm::Wrapper<edmtest::IntProduct> dummyw13;
@@ -16,4 +20,13 @@ namespace { namespace {
   edmtest::OtherThingCollection dummy2;
   edm::Wrapper<edmtest::ThingCollection> dummy3;
   edm::Wrapper<edmtest::OtherThingCollection> dummy4;
+
+  edmtestprod::Ord<edmtestprod::Simple> dummy20;
+  edmtestprod::StreamTestTmpl<edmtestprod::Ord<edmtestprod::Simple> > dummy21;
+  edm::Wrapper<edmtestprod::StreamTestTmpl<edmtestprod::Ord<edmtestprod::Simple> > > dummy22;
+  std::vector<edmtestprod::Simple> dummy23;
+  edm::SortedCollection<edmtestprod::Simple,edm::StrictWeakOrdering<edmtestprod::Simple> > dummy24;
+  edm::Wrapper<edm::SortedCollection<edmtestprod::Simple,edm::StrictWeakOrdering<edmtestprod::Simple> > > dummy25;
+  edm::Wrapper<edmtestprod::StreamTestThing> dummy26;
+  edm::Wrapper<edmtestprod::X0123456789012345678901234567890123456789012345678901234567890123456789012345678901> dummy27;
 }}
