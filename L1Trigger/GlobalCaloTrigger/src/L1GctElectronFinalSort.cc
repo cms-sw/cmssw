73772e20a4666d749cbc63efc276b1026f7e722f
mfhansen <sha1-4c6d66dbcfddc4349cf1514bfba4442abf4433f7@cern.ch>
2006-04-11 13:18:00 +0000

    methods filled in

--- L1Trigger/GlobalCaloTrigger/src/L1GctElectronFinalSort.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctElectronFinalSort.cc
@@ -1,9 +1,39 @@
+/*! \file L1GctElectronFinalSort.cc
+ * \Class that does the final sorting of electron candidates
+ *
+ * This class sorts the electron candidates by rank in 
+ * ascending order. Inputs are the 4 highest Et electrons from
+ * the leaf? cards
+ *
+ * \author Maria Hansen
+ * \date April 2006
+ */
+
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctElectronFinalSort.h"
 
-L1GctElectronFinalSort::L1GctElectronFinalSort()
+L1GctElectronFinalSort::L1GctElectronFinalSort():inputCands(0),outputCands(4)
 {
 }
 
 L1GctElectronFinalSort::~L1GctElectronFinalSort()
 {
+}
+
+void L1GctElectronFinalSort::setSortedInput(L1GctEmCand cand)
+{
+  sortedCands.setInputEmCand(cand);
+  inputCands.push_back(cand);
+}
+
+void L1GctElectronFinalSort::process()
+{
+  sortedCands.process();
+  outputCands = sortedCands.getOutput();
+}
+
+void L1GctElectronFinalSort::reset()
+{
+  //sortedCands.
+  inputCands.clear();
+  outputCands.clear();
 }
