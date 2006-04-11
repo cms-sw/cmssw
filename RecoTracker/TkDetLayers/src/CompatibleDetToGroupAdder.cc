3dcbb1888ea3fe381e4a4057e996cf0bd3c4bde1
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2006-04-11 13:51:36 +0000

    implementation of CompatibleDetToGroupAdder ported

--- RecoTracker/TkDetLayers/src/CompatibleDetToGroupAdder.cc
+++ RecoTracker/TkDetLayers/src/CompatibleDetToGroupAdder.cc
@@ -1,5 +1,5 @@
 #include "RecoTracker/TkDetLayers/interface/CompatibleDetToGroupAdder.h"
-//#include "RecoTracker/TkDetLayers/interface/DetGroupMerger.h"
+#include "RecoTracker/TkDetLayers/interface/DetGroupMerger.h"
 
 
 
@@ -10,39 +10,8 @@ bool CompatibleDetToGroupAdder::add( const GeometricSearchDet& det,
 				     const MeasurementEstimator& est,
 				     vector<DetGroup>& result) const
 {
-  cout << "Dummy implementation of CompatibleDetToGroupAdder::add() " << endl; 
-  return false;
-}
-
-bool CompatibleDetToGroupAdder::add( const GeomDet& det,
-				     const TrajectoryStateOnSurface& tsos, 
-				     const Propagator& prop,
-				     const MeasurementEstimator& est,
-				     vector<DetGroup>& result) const
-{
-  cout << "Dummy implementation of CompatibleDetToGroupAdder::add() " << endl; 
-  return false;
-}
-
-
-
-/*
-bool CompatibleDetToGroupAdder::add( const Det& det,
-				     const TrajectoryStateOnSurface& tsos, 
-				     const Propagator& prop,
-				     const MeasurementEstimator& est,
-				     vector<DetGroup>& result) const
-{
-//   cout << "CompatibleDetToGroupAdder called for " 
-//        << typeid(det).name() << " at pos (" 
-//        << det.position().perp() << ", " 
-//        << det.position().phi()  << ", " 
-//        << det.position().z()    << ") " 
-//        << endl;
-
   if (det.hasGroups()) {
-    const CompositeDet& cdet = dynamic_cast< const CompositeDet&>(det);
-    vector<DetGroup> tmp( cdet.groupedCompatibleDets( tsos, prop, est));
+    vector<DetGroup> tmp( det.groupedCompatibleDets( tsos, prop, est));
 
 //     cout << "CompatibleDetToGroupAdder: det hasGroups, returned group vector of size "
 // 	 << tmp.size() << endl; 
@@ -54,10 +23,8 @@ bool CompatibleDetToGroupAdder::add( const Det& det,
     }
   }
   else {
-    const CompositeDet* cdetp = dynamic_cast< const CompositeDet*>(&det);
-    if (cdetp != 0) {
-      vector<CompositeDet::DetWithState> compatDets = cdetp->compatibleDets( tsos, prop, est);
-      if (!compatDets.empty()) {
+    vector<GeometricSearchDet::DetWithState> compatDets = det.compatibleDets( tsos, prop, est);
+    if (!compatDets.empty()) {
 	if (result.empty()) {
 	  result.push_back( DetGroup( 0, 1)); // empty group for insertion
 	}
@@ -65,37 +32,48 @@ bool CompatibleDetToGroupAdder::add( const Det& det,
 	  cout << "CompatibleDetToGroupAdder: det is not grouped but result has more than one group!" 
 	       << endl;
 	}
-	for (vector<CompositeDet::DetWithState>::const_iterator i=compatDets.begin();
+	for (vector<GeometricSearchDet::DetWithState>::const_iterator i=compatDets.begin();
 	     i!=compatDets.end(); i++) {
 	  result.front().push_back( *i);
 	}
 	return true;
-      }
-    }
-    else {
-
-      pair<bool, TrajectoryStateOnSurface> compat = det.compatible( tsos, prop, est);
-
-//       cout << "CompatibleDetToGroupAdder: det has no groups, is compatible? " << compat.first << endl;
-
-      if (compat.first) {
-	DetGroupElement ge( &det, compat.second);
-	if (result.empty()) {
-	  result.push_back( DetGroup( 0, 1)); // empty group for ge insertion
-	}
-	else {
-	  if (result.size() != 1) {
-	    cout << "CompatibleDetToGroupAdder: det is not grouped but result has more than one group!" 
-		 << endl;
-	  }
-	}
-	result.front().push_back(ge); 
-	return true;
-      }
     }
   }
 
-//   cout << "CompatibleDetToGroupAdder: returning false" << endl;
+  //   cout << "CompatibleDetToGroupAdder: returning false" << endl;
   return false;
 }
-*/
+
+#include "TrackingTools/DetLayers/interface/GeomDetCompatibilityChecker.h"
+
+bool CompatibleDetToGroupAdder::add( const GeomDet& det,
+				     const TrajectoryStateOnSurface& tsos, 
+				     const Propagator& prop,
+				     const MeasurementEstimator& est,
+				     vector<DetGroup>& result) const
+{
+  GeomDetCompatibilityChecker theCompatibilityChecker;
+  pair<bool, TrajectoryStateOnSurface> compat = theCompatibilityChecker.isCompatible( &det,tsos, prop, est);
+
+  //       cout << "CompatibleDetToGroupAdder: det has no groups, is compatible? " << compat.first << endl;
+
+  if (compat.first) {
+    DetGroupElement ge( &det, compat.second);
+    if (result.empty()) {
+      result.push_back( DetGroup( 0, 1)); // empty group for ge insertion
+    }
+    else {
+      if (result.size() != 1) {
+	cout << "CompatibleDetToGroupAdder: det is not grouped but result has more than one group!" 
+	     << endl;
+      }
+    }
+    result.front().push_back(ge); 
+    return true;
+  }
+  return false;
+}
+
+
+
+
