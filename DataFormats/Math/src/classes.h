37194528f9f42c321210de34cc0571da601bbf25
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-04-12 13:16:19 +0000

    added missing dictionaries

--- DataFormats/Math/src/classes.h
+++ DataFormats/Math/src/classes.h
@@ -62,5 +62,12 @@ namespace {
     math::ErrorF<4>::type ef4;
     math::ErrorF<5>::type ef5;
     math::ErrorF<6>::type ef6;
+
+    ROOT::Math::RowOffsets<1> ro1;
+    ROOT::Math::RowOffsets<2> ro2;
+    ROOT::Math::RowOffsets<3> ro3;
+    ROOT::Math::RowOffsets<4> ro4;
+    ROOT::Math::RowOffsets<5> ro5;
+    ROOT::Math::RowOffsets<6> ro6;
   }
 }
