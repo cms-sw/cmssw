8fbb3ebf8cadb924c682d06cdb81d642cc938dc0
Teddy Todorov <sha1-f275252eb6186c16cfc98cd1ee74c0717c3df6a4@cern.ch>
2006-04-13 13:47:28 +0000

    add glued dets

--- RecoTracker/MeasurementDet/interface/MeasurementTracker.h
+++ RecoTracker/MeasurementDet/interface/MeasurementTracker.h
@@ -1,10 +1,11 @@
 #ifndef MeasurementTracker_H
 #define MeasurementTracker_H
 
-#include "TrackingTools/MeasurementDet/interface/TrackingSystem.h"
+#include "TrackingTools/MeasurementDet/interface/MeasurementDetSystem.h"
 #include "DataFormats/DetId/interface/DetId.h"
 #include "RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h"
 #include "RecoLocalTracker/ClusterParameterEstimator/interface/PixelClusterParameterEstimator.h"
+#include "Geometry/CommonDetUnit/interface/TrackingGeometry.h"
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -16,23 +17,28 @@
 
 class TkStripMeasurementDet;
 class TkPixelMeasurementDet;
+class TkGluedMeasurementDet;
 class GeometricSearchTracker;
 class TrackingGeometry;
+class SiStripRecHitMatcher;
+class GluedGeomDet;
 
-class MeasurementTracker : public TrackingSystem {
+class MeasurementTracker : public MeasurementDetSystem {
 public:
 
-  MeasurementTracker( const edm::EventSetup&, const edm::Event&);
+  //B.M. MeasurementTracker( const edm::EventSetup&, const edm::Event&);
+  MeasurementTracker( const edm::EventSetup&);
 
   virtual ~MeasurementTracker() {}
-
-  virtual const MeasurementDet* measurementDet(const DetId& id) const;
-  
+ 
   void update( const edm::Event&) const;
 
   const TrackingGeometry* geomTracker() const { return theTrackerGeom;}
 
   const GeometricSearchTracker* geometricSearchTracker() const {return theGeometricSearchTracker;}
+
+  /// MeasurementDetSystem interface
+  virtual const MeasurementDet*       idToDet(const DetId& id) const;
 
 private:
 
@@ -41,19 +47,26 @@ private:
   DetContainer                        theDetMap;
   std::vector<TkStripMeasurementDet*> theStripDets;
   std::vector<TkPixelMeasurementDet*> thePixelDets;
+  std::vector<TkGluedMeasurementDet*> theGluedDets;
   const TrackingGeometry*             theTrackerGeom;
   const GeometricSearchTracker*       theGeometricSearchTracker;
 
   const StripClusterParameterEstimator* stripCPE;
   const PixelClusterParameterEstimator* pixelCPE;
+  SiStripRecHitMatcher*           theHitMatcher;
 
   void initialize(const edm::EventSetup&);
 
-  void addDet( const GeomDet* gd);
   void addStripDet( const GeomDet* gd,
 		    const StripClusterParameterEstimator* cpe);
   void addPixelDet( const GeomDet* gd,
 		    const PixelClusterParameterEstimator* cpe);
+
+  void addGluedDet( const GluedGeomDet* gd, SiStripRecHitMatcher* matcher);
+
+  void addPixelDets( const TrackingGeometry::DetContainer& dets);
+
+  void addStripDets( const TrackingGeometry::DetContainer& dets);
 
 };
 
--- RecoTracker/MeasurementDet/src/MeasurementTracker.cc
+++ RecoTracker/MeasurementDet/src/MeasurementTracker.cc
@@ -1,31 +1,33 @@
 #include "RecoTracker/MeasurementDet/interface/MeasurementTracker.h"
-#include "Geometry/CommonDetUnit/interface/TrackingGeometry.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 #include "Geometry/CommonDetUnit/interface/GeomDet.h"
+#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
 #include "RecoTracker/MeasurementDet/interface/TkStripMeasurementDet.h"
 #include "RecoTracker/MeasurementDet/interface/TkPixelMeasurementDet.h"
+#include "RecoTracker/MeasurementDet/interface/TkGluedMeasurementDet.h"
 #include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
 #include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
 #include "TrackingTools/MeasurementDet/interface/MeasurementDetException.h"
 #include "RecoTracker/TkDetLayers/interface/GeometricSearchTracker.h"
+#include "Geometry/TrackerGeometryBuilder/interface/GluedGeomDet.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "RecoTracker/Record/interface/TrackerRecoGeometryRecord.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "RecoLocalTracker/SiPixelRecHits/interface/CPEFromDetPosition.h"
+#include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
+#include "RecoLocalTracker/SiStripRecHitConverter/interface/SiStripRecHitMatcher.h"
 
 #include <iostream>
 
-MeasurementTracker::MeasurementTracker( const edm::EventSetup& setup, 
-					const edm::Event& event) :
-  theTrackerGeom(0)
+MeasurementTracker::MeasurementTracker( const edm::EventSetup& setup) :
+  theTrackerGeom(0), stripCPE(0)
 {
   using namespace std;
-  initialize( setup);
+  this->initialize( setup);
 
   cout << "MeasurementTracker: initialize OK" << endl;
-
-  update(event);
-
-  cout << "MeasurementTracker: update OK" << endl;
 
 }
 
@@ -33,77 +35,121 @@ void MeasurementTracker::initialize(const edm::EventSetup& setup)
 {
   using namespace std;
     using namespace edm;
-    edm::ESHandle<TrackingGeometry> pDD;
+    edm::ESHandle<TrackerGeometry> pDD;
     setup.get<TrackerDigiGeometryRecord>().get( pDD );
-    const TrackingGeometry& tracker(*pDD);
+    const TrackerGeometry& tracker(*pDD);
     theTrackerGeom = &tracker;
 
-    cout << "MeasurementTracker::initialize: TrackingGeometry accessed" << endl; 
+    cout << "MeasurementTracker::initialize: TrackerGeometry accessed" << endl; 
 
-    const TrackingGeometry::DetContainer& dets = tracker.dets();
+    const TrackerGeometry::DetContainer& dets = tracker.dets();
 
-    std::cout << "got from TrackingGeometry " << dets.size() << std::endl; 
+    std::cout << "got from TrackerGeometry " << dets.size() << std::endl; 
 
+    edm::ParameterSet conf;
+    conf.addParameter("TanLorentzAnglePerTesla",0.106);
+    conf.addUntrackedParameter("VerboseLevel",20);
+    edm::ESHandle<MagneticField> magfield;
+    setup.get<IdealMagneticFieldRecord>().get(magfield);
+    pixelCPE = new CPEFromDetPosition(conf, &(*magfield));
+   
+    cout << "pixelCPE: " << pixelCPE << endl;
+    cout << "typeid(*pixelCPE).name(): " << typeid(*pixelCPE).name() << endl;
 
-    for (TrackingGeometry::DetContainer::const_iterator gd=dets.begin();
-	 gd != dets.end(); gd++) {
+    theHitMatcher = new SiStripRecHitMatcher();
 
-      cout << "GeomDet address " << *gd << endl;
-      cout << "GeomDet Surface address " << &(**gd).surface() << endl;
+    addPixelDets( tracker.detsPXB());
+    addPixelDets( tracker.detsPXF());
+    addStripDets( tracker.detsTIB());
+    addStripDets( tracker.detsTID());
+    addStripDets( tracker.detsTOB());
+    addStripDets( tracker.detsTEC());
 
-      std::cout << (**gd).surface().position() << std::endl; 
-      
-      addDet(*gd);
-    }
-
-   edm::ESHandle<GeometricSearchTracker> gstrackerHandle;
-   setup.get<TrackerRecoGeometryRecord>().get( gstrackerHandle);
-   theGeometricSearchTracker = &(*gstrackerHandle);
+    edm::ESHandle<GeometricSearchTracker> gstrackerHandle;
+    setup.get<TrackerRecoGeometryRecord>().get( gstrackerHandle);
+    theGeometricSearchTracker = &(*gstrackerHandle);
 }
 
-void MeasurementTracker::addDet( const GeomDet* gd)
+
+void MeasurementTracker::addPixelDets( const TrackingGeometry::DetContainer& dets)
 {
-  DetId id(gd->geographicalId());
-  switch(id.subdetId()){
-  case PixelSubdetector::PixelBarrel:
-    addPixelDet(gd, pixelCPE);
-    break;
-  case PixelSubdetector::PixelEndcap:
-    addPixelDet(gd, pixelCPE);
-    break;
-  case StripSubdetector::TIB:
-    addStripDet(gd, stripCPE);
-    break;
-  case StripSubdetector::TID:
-    addStripDet(gd, stripCPE);
-    break;
-  case StripSubdetector::TOB:
-    addStripDet(gd, stripCPE);
-    break;
-  case StripSubdetector::TEC:
-    addStripDet(gd, stripCPE);
-    break;
+  for (TrackerGeometry::DetContainer::const_iterator gd=dets.begin();
+       gd != dets.end(); gd++) {
+      addPixelDet(*gd, pixelCPE);
+  }  
+}
 
-    // glued dets should come here too...
+void MeasurementTracker::addStripDets( const TrackingGeometry::DetContainer& dets)
+{
+  for (TrackerGeometry::DetContainer::const_iterator gd=dets.begin();
+       gd != dets.end(); gd++) {
+    StripSubdetector stripId( (**gd).geographicalId());
 
-  default:
-    throw MeasurementDetException("MeasurementTracker ERROR: not a Tracker Subdetector");
+    const GeomDetUnit* gdu = dynamic_cast<const GeomDetUnit*>(*gd);
+
+//     bool isDetUnit( gdu != 0);
+//     cout << "StripSubdetector glued? " << stripId.glued() 
+// 	 << " is DetUnit? " << isDetUnit << endl;
+
+    if (gdu != 0) {
+      addStripDet(*gd, stripCPE);
+    }
+    else {
+      const GluedGeomDet* gluedDet = dynamic_cast<const GluedGeomDet*>(*gd);
+      if (gluedDet == 0) {
+	throw MeasurementDetException("MeasurementTracker ERROR: GeomDet neither DetUnit nor GluedDet");
+      }
+      addGluedDet(gluedDet, theHitMatcher);
+    }  
   }
 }
 
 void MeasurementTracker::addStripDet( const GeomDet* gd,
 				      const StripClusterParameterEstimator* cpe)
 {
-  TkStripMeasurementDet* det = new TkStripMeasurementDet( gd, cpe);
-  theStripDets.push_back(det);
-  theDetMap[gd->geographicalId()] = det;
+  try {
+    TkStripMeasurementDet* det = new TkStripMeasurementDet( gd, cpe);
+    theStripDets.push_back(det);
+    theDetMap[gd->geographicalId()] = det;
+  }
+  catch(MeasurementDetException& err){
+    cout << "Oops, got a MeasurementDetException: " << err.what() << endl;
+  }
 }
 
 void MeasurementTracker::addPixelDet( const GeomDet* gd,
 				      const PixelClusterParameterEstimator* cpe)
 {
+  cout << "pixelCPE in addPixelDet: " << cpe << endl; 
   TkPixelMeasurementDet* det = new TkPixelMeasurementDet( gd, cpe);
   thePixelDets.push_back(det);
+  theDetMap[gd->geographicalId()] = det;
+}
+
+void MeasurementTracker::addGluedDet( const GluedGeomDet* gd,
+				      SiStripRecHitMatcher* matcher)
+{
+  const MeasurementDet* monoDet = idToDet( gd->monoDet()->geographicalId());
+  if (monoDet == 0) {
+    addStripDet(gd->monoDet(), stripCPE);  // in case glued det comes before components
+    monoDet = idToDet( gd->monoDet()->geographicalId());
+  }
+
+  const MeasurementDet* stereoDet = idToDet( gd->stereoDet()->geographicalId());
+  if (stereoDet == 0) {
+    addStripDet(gd->stereoDet(), stripCPE);  // in case glued det comes before components
+    stereoDet = idToDet( gd->stereoDet()->geographicalId());
+  }
+
+  if (monoDet == 0 || stereoDet == 0) {
+    std::cout << "MeasurementTracker ERROR: GluedDet components not found as MeasurementDets "
+	      << endl;
+    throw MeasurementDetException("MeasurementTracker ERROR: GluedDet components not found as MeasurementDets");
+  }
+
+  TkGluedMeasurementDet* det = new TkGluedMeasurementDet( gd, theHitMatcher,
+							  monoDet, stereoDet);
+  theGluedDets.push_back( det);
   theDetMap[gd->geographicalId()] = det;
 }
 
@@ -115,10 +161,13 @@ void MeasurementTracker::update( const edm::Event& event) const
   typedef SiPixelClusterCollection::Range    PixelClusterRange;
 
   // std::string clusterProducer = conf_.getParameter<std::string>("ClusterProducer");
-  std::string stripClusterProducer ("ClusterProducer"); // FIXME
+  //std::string stripClusterProducer ("ClusterProducer"); // FIXME SiStripClusterizer
+  std::string stripClusterProducer ("ThreeThresholdClusterizer");
   edm::Handle<SiStripClusterCollection> clusterHandle;
   event.getByLabel(stripClusterProducer, clusterHandle);
   const SiStripClusterCollection* clusterCollection = clusterHandle.product();
+
+  cout << "--- siStripClusterColl got " << endl;
 
   // loop over all strip dets
   for (std::vector<TkStripMeasurementDet*>::const_iterator i=theStripDets.begin();
@@ -128,13 +177,16 @@ void MeasurementTracker::update( const edm::Event& event) const
     // push cluster range in det
     (**i).update( range);
   }
-
+  cout << "--- end of loop over dets" << endl;
 
   // Pixel Clusters
-  std::string pixelClusterProducer ("ClusterProducer"); // FIXME
+  std::string pixelClusterProducer ("pixClust"); 
+
   edm::Handle<SiPixelClusterCollection> pixelClusters;
   event.getByLabel(pixelClusterProducer, pixelClusters);
   const SiPixelClusterCollection * pixelCollection = pixelClusters.product();
+  cout << "--- siPixelClusterColl got " << endl;
+
   for (std::vector<TkPixelMeasurementDet*>::const_iterator i=thePixelDets.begin();
        i!=thePixelDets.end(); i++) {
     // foreach det get cluster range
@@ -142,14 +194,24 @@ void MeasurementTracker::update( const edm::Event& event) const
     // push cluster range in det
     (**i).update( range);
   }
-
+  cout << "--- end of loop over dets" << endl;
 
   /// or maybe faster: loop over all strip dets and clear them
   /// loop over dets with clusters and set range
 
 }
 
-const MeasurementDet* MeasurementTracker::measurementDet(const DetId& id) const
+
+
+const MeasurementDet* 
+MeasurementTracker::idToDet(const DetId& id) const
 {
-  return 0;
+  std::map<DetId,MeasurementDet*>::const_iterator it = theDetMap.find(id);
+  if(it !=theDetMap.end()) {
+    return it->second;
+  }else{
+    //throw exception;
+  }
+  
+  return 0; //to avoid compile warning
 }
