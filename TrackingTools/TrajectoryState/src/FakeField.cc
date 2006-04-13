9ac44a87092df59ab5a4189eaa1791d5b01a3dca
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-04-13 14:40:28 +0000

    fixme

--- TrackingTools/TrajectoryState/src/FakeField.cc
+++ TrackingTools/TrajectoryState/src/FakeField.cc
@@ -1,4 +1,4 @@
 #include "TrackingTools/TrajectoryState/interface/FakeField.h"
 
-// FIXME leaks memory... only for testing
+// FIXME VERTEX leaks memory... only for testing
 MagneticField* TrackingTools::FakeField::Field::theField = new ConcreteField;
