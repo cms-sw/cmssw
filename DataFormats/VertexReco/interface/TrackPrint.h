3e5beb3228f93ef405df0b2a9fdba53a7de4e340
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-04-18 16:48:25 +0000

    added ifdef fences

--- DataFormats/VertexReco/interface/TrackPrint.h
+++ DataFormats/VertexReco/interface/TrackPrint.h
@@ -1,5 +1,8 @@
+#ifndef TrackPrint_h
+#define TrackPrint_h
 #include "DataFormats/TrackReco/interface/Track.h"
 #include <iostream>
+#include <sstream>
 
 ostream& operator<<(ostream& os, const reco::Track & tk) {
   
@@ -26,3 +29,31 @@ ostream& operator<<(ostream& os, const reco::Track & tk) {
 
   return os;
 }
+
+ostringstream& operator<<(ostringstream& os, const reco::Track & tk) {
+  
+  os << "track parameters" << endl;
+  {
+    os << "(d_xy, phi_0, q/pT, d_z, p_z/p_T) = ";
+    {
+      for (int i = 0; i < 5; i++) {
+        os.precision(6); os.width(13); os<<tk.parameter(i);
+      }
+    }
+  }
+  os << endl;
+  os << "covariance" << endl;
+  {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+	os.precision(6); os.width(13); os<<tk.covariance(i, j);
+      }
+      os << endl;
+    }
+  }
+  os << endl;
+
+  return os;
+}
+
+#endif
