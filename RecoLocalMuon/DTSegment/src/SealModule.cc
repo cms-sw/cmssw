871c40b6d63ade853d85585c99eae85e6532107d
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-04-19 15:01:17 +0000

    4D module added

--- RecoLocalMuon/DTSegment/src/SealModule.cc
+++ RecoLocalMuon/DTSegment/src/SealModule.cc
@@ -2,11 +2,22 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+DEFINE_SEAL_MODULE();
+
+// ----------- Modules for 2D-segments reco -----------
 #include "RecoLocalMuon/DTSegment/src/DTRecSegment2DProducer.h"
 #include "RecoLocalMuon/DTSegment/src/DTRecSegment2DAlgoFactory.h"
 #include "RecoLocalMuon/DTSegment/src/DTCombinatorialPatternReco.h"
 
-
-DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTRecSegment2DProducer);
 DEFINE_SEAL_PLUGIN (DTRecSegment2DAlgoFactory, DTCombinatorialPatternReco, "DTCombinatorialPatternReco");
+//-------------------------------------------------------------------------------------------------------
+
+// ----------- Modules for 4D-segments reco -----------
+#include "RecoLocalMuon/DTSegment/src/DTRecSegment4DProducer.h"
+#include "RecoLocalMuon/DTSegment/src/DTRecSegment4DAlgoFactory.h"
+#include "RecoLocalMuon/DTSegment/src/DTCombinatorialPatternReco4D.h"
+
+DEFINE_ANOTHER_FWK_MODULE(DTRecSegment4DProducer);
+DEFINE_SEAL_PLUGIN (DTRecSegment4DAlgoFactory, DTCombinatorialPatternReco4D, "DTCombinatorialPatternReco4D");
+//-------------------------------------------------------------------------------------------------------
