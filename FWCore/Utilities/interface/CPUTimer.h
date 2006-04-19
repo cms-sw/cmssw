19988db85cb29f7d8fc33f5c5ca8a0cb8a400888
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-04-19 17:56:07 +0000

    first submission

---
+++ FWCore/Utilities/interface/CPUTimer.h
@@ -0,0 +1,70 @@
+#ifndef Utilities_CPUTimer_h
+#define Utilities_CPUTimer_h
+// -*- C++ -*-
+//
+// Package:     Utilities
+// Class  :     CPUTimer
+// 
+/**\class CPUTimer CPUTimer.h FWCore/Utilities/interface/CPUTimer.h
+
+ Description: Timer which measures the CPU and wallclock time
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Sun Apr 16 20:32:13 EDT 2006
+// $Id$
+//
+
+// system include files
+#include <unistd.h>
+
+// user include files
+
+// forward declarations
+namespace edm {
+class CPUTimer
+{
+
+   public:
+      CPUTimer();
+      virtual ~CPUTimer();
+
+      // ---------- const member functions ---------------------
+      double realTime() const ;
+      double cpuTime() const ;
+      
+      // ---------- static member functions --------------------
+
+      // ---------- member functions ---------------------------
+      void start();
+      void stop();
+      void reset();
+      
+   private:
+      CPUTimer(const CPUTimer&); // stop default
+
+      const CPUTimer& operator=(const CPUTimer&); // stop default
+
+      struct Times {
+        double real_;
+        double cpu_;
+      };
+      
+      Times calculateDeltaTime() const;
+      
+      // ---------- member data --------------------------------
+      enum State {kRunning, kStopped} state_;
+      struct timeval startRealTime_;
+      struct timeval startCPUTime_;
+      
+      double accumulatedRealTime_;
+      double accumulatedCPUTime_;
+      
+};
+}
+
+#endif
---
+++ FWCore/Utilities/src/CPUTimer.cc
@@ -0,0 +1,143 @@
+// -*- C++ -*-
+//
+// Package:     Utilities
+// Class  :     CPUTimer
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Sun Apr 16 20:32:20 EDT 2006
+// $Id$
+//
+
+// system include files
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+// user include files
+#include "FWCore/Utilities/interface/CPUTimer.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+//
+// constants, enums and typedefs
+//
+using namespace edm;
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+CPUTimer::CPUTimer() :
+state_(kStopped),
+startRealTime_(),
+startCPUTime_(),
+accumulatedRealTime_(0),
+accumulatedCPUTime_(0)
+{
+  startRealTime_.tv_sec=0;
+  startRealTime_.tv_usec=0;
+  startCPUTime_.tv_sec=0;
+  startCPUTime_.tv_usec=0;
+}
+
+// CPUTimer::CPUTimer(const CPUTimer& rhs)
+// {
+//    // do actual copying here;
+// }
+
+CPUTimer::~CPUTimer()
+{
+}
+
+//
+// assignment operators
+//
+// const CPUTimer& CPUTimer::operator=(const CPUTimer& rhs)
+// {
+//   //An exception safe implementation is
+//   CPUTimer temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+void 
+CPUTimer::start() {
+  if(kStopped == state_) {
+    rusage theUsage;
+    if( 0 != getrusage(RUSAGE_SELF, &theUsage)) {
+      throw cms::Exception("CPUTimerFailed")<<errno;
+    }
+    startCPUTime_.tv_sec =theUsage.ru_stime.tv_sec+theUsage.ru_utime.tv_sec;
+    startCPUTime_.tv_usec =theUsage.ru_stime.tv_usec+theUsage.ru_utime.tv_usec;
+    
+    gettimeofday(&startRealTime_, 0);
+    state_ = kRunning;
+  }
+}
+
+void 
+CPUTimer::stop() {
+  if(kRunning == state_) {
+    Times t = calculateDeltaTime();
+    accumulatedCPUTime_ += t.cpu_;
+    accumulatedRealTime_ += t.real_;
+    state_=kStopped;
+  }
+}
+
+void 
+CPUTimer::reset(){
+  accumulatedCPUTime_ =0;
+  accumulatedRealTime_=0;
+}
+
+CPUTimer::Times
+CPUTimer::calculateDeltaTime() const
+{
+  rusage theUsage;
+  if( 0 != getrusage(RUSAGE_SELF, &theUsage)) {
+    throw cms::Exception("CPUTimerFailed")<<errno;
+  }
+  const double microsecToSec = 1E-6;
+  
+  struct timeval tp;
+  gettimeofday(&tp, 0);
+  
+  Times returnValue;
+  returnValue.cpu_ = theUsage.ru_stime.tv_sec+theUsage.ru_utime.tv_sec-startCPUTime_.tv_sec+microsecToSec*(theUsage.ru_stime.tv_usec+theUsage.ru_utime.tv_usec-startCPUTime_.tv_usec);
+  returnValue.real_ = tp.tv_sec-startRealTime_.tv_sec+microsecToSec*(tp.tv_usec -startRealTime_.tv_usec);
+  return returnValue;
+}
+//
+// const member functions
+//
+double 
+CPUTimer::realTime() const 
+{ 
+  if(kStopped == state_) {
+    return accumulatedRealTime_;
+  }
+  return accumulatedRealTime_ + calculateDeltaTime().real_; 
+}
+
+double 
+CPUTimer::cpuTime() const 
+{ 
+  if(kStopped== state_) {
+    return accumulatedCPUTime_;
+  }
+  return accumulatedCPUTime_+ calculateDeltaTime().cpu_;
+}
+
+//
+// static member functions
+//
