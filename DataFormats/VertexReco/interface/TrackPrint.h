e49a86582da9e3a5a9c4f2002af954942e3072ec
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-04-19 15:55:59 +0000

    clashing namespace ?

--- DataFormats/VertexReco/interface/TrackPrint.h
+++ DataFormats/VertexReco/interface/TrackPrint.h
@@ -2,35 +2,11 @@
 #define TrackPrint_h
 #include "DataFormats/TrackReco/interface/Track.h"
 #include <iostream>
-#include <sstream>
 
-ostream& operator<<(ostream& os, const reco::Track & tk) {
-  
-  os << "track parameters" << endl;
-  {
-    os << "(d_xy, phi_0, q/pT, d_z, p_z/p_T) = ";
-    {
-      for (int i = 0; i < 5; i++) {
-        os.precision(6); os.width(13); os<<tk.parameter(i);
-      }
-    }
-  }
-  os << endl;
-  os << "covariance" << endl;
-  {
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5; j++) {
-	os.precision(6); os.width(13); os<<tk.covariance(i, j);
-      }
-      os << endl;
-    }
-  }
-  os << endl;
 
-  return os;
-}
+//using namespace std;
 
-ostringstream& operator<<(ostringstream& os, const reco::Track & tk) {
+std::ostream& operator<<(std::ostream& os, const reco::Track & tk) {
   
   os << "track parameters" << endl;
   {
