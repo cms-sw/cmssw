f012e4796e0b50e9e0c935cc9f19cb86c4701054
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2006-04-19 15:55:11 +0000

    added files

---
+++ SimG4Core/KillSecondaries/src/module.cc
@@ -0,0 +1,6 @@
+#include "SimG4Core/KillSecondaries/interface/KillSecondariesRunAction.h"
+#include "SimG4Core/UtilityAction/interface/UtilityActionPluginFactory.h"
+#include "PluginManager/ModuleDef.h"
+    
+DEFINE_SEAL_MODULE ();
+DEFINE_UTILITYACTION(KillSecondariesRunAction);
