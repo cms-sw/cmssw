f220f35aafd6386cb3a5d8911491ca038ae452dc
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2006-04-20 10:20:44 +0000

    added files

---
+++ SimG4Core/CountProcesses/src/module.cc
@@ -0,0 +1,6 @@
+#include "SimG4Core/CountProcesses/interface/CountProcessesAction.h"
+#include "SimG4Core/UtilityAction/interface/UtilityActionPluginFactory.h"
+#include "PluginManager/ModuleDef.h"
+   
+DEFINE_SEAL_MODULE ();
+DEFINE_UTILITYACTION(CountProcessesAction);
