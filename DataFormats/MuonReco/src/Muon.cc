968290a78997c05f9c17a541f4eb4ad4f4e97fe5
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-04-20 14:29:19 +0000

    new Muon structure

--- DataFormats/MuonReco/src/Muon.cc
+++ DataFormats/MuonReco/src/Muon.cc
@@ -1,18 +1,8 @@
 #include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonExtra.h"
-#include <algorithm>
-using namespace std;
 using namespace reco;
 
-Muon::Muon( float chi2, unsigned short ndof, int found, int invalid, int lost,
-	    const Parameters & p, const Covariance & c ) :
-  TrackBase( chi2, ndof, found, invalid, lost, p, c ) {
-}
-
-Muon::Muon( float chi2, unsigned short ndof, int found, int invalid, int lost,
-	    int q, const Point & v, const Vector & p, 
-	    const PosMomError & err ) :
-  TrackBase( chi2, ndof, found, invalid, lost, q, v, p, err ) {
+Muon::Muon( const TrackRef & t, const TrackRef & s, const TrackRef & c, defaultMomentumEstimate d ) :
+  track_( t ), standAlone_( s ), combined_( c ), default_( d ) { 
 }
 
 
--- DataFormats/MuonReco/src/classes.h
+++ DataFormats/MuonReco/src/classes.h
@@ -1,17 +1,9 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonExtra.h"
-#include "DataFormats/MuonReco/interface/CombinedMuon.h"
 #include <vector>
 
 namespace {
   namespace {
-    std::vector<reco::MuonExtra> v2;
-    edm::Wrapper<std::vector<reco::MuonExtra> > c2;
-    edm::Ref<std::vector<reco::MuonExtra> > r2;
-    edm::RefProd<std::vector<reco::MuonExtra> > rp2;
-    edm::RefVector<std::vector<reco::MuonExtra> > rv2;
-
     std::vector<reco::Muon> v1;
     edm::Wrapper<std::vector<reco::Muon> > c1;
     edm::Ref<std::vector<reco::Muon> > r1;
