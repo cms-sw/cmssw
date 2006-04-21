a85a8b8330eb7440129abfbead4b721aa9afab89
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2006-04-21 10:45:42 +0000

    temporaty fix

--- RecoPixelVertexing/PixelTriplets/test/HitTripletProducer.cc
+++ RecoPixelVertexing/PixelTriplets/test/HitTripletProducer.cc
@@ -1,33 +1,19 @@
-#include <iostream>
-#include <memory>
-#include <string>
-
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
-#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
-#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
-#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetType.h"
-
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
-
 #include "RecoPixelVertexing/PixelTriplets/interface/OrderedHitTriplets.h"
 #include "RecoTracker/TkTrackingRegions/interface/GlobalTrackingRegion.h"
 #include "RecoPixelVertexing/PixelTriplets/interface/PixelHitTripletGenerator.h"
 
-
-using namespace std;
 
 class HitTripletProducer : public edm::EDAnalyzer {
 public:
@@ -43,44 +29,29 @@ private:
 HitTripletProducer::HitTripletProducer(const edm::ParameterSet& conf) 
   : generator(0)
 {
-  cout << " here HitTripletProducer CTOR" << endl;
-  edm::LogInfo ("HitTripletProducer")<<"CTOR";
+  edm::LogInfo("HitTripletProducer")<<" CTOR";
 }
 
 HitTripletProducer::~HitTripletProducer() 
 { 
-  cout << " here HitTripletProducer DTOR" << endl;
-  delete generator;
+  edm::LogInfo("HitTripletProducer")<<" DTOR";
+//  delete generator;
 }
 
 void HitTripletProducer::analyze(
     const edm::Event& ev, const edm::EventSetup& es)
 {
-  cout << " HERE begin of produce" << endl;
-
-
-  cout << " ASKING FOR TRACKER!" << endl;
-  edm::ESHandle<TrackerGeometry> tracker;
-  es.get<TrackerDigiGeometryRecord>().get(tracker);
-  cout << " AFTER ASKING FOR TRACKER! "  << endl;
-
   edm::Handle<SiPixelRecHitCollection> pixelHits;
   ev.getByType(pixelHits);
 
-  cout << " before init" << endl;
-  if (!generator) {
-   generator = new PixelHitTripletGenerator;
-  }
+  generator = new PixelHitTripletGenerator;
   generator->init(*pixelHits,es); 
-  cout << " after init" << endl;
 
   GlobalTrackingRegion region;
   OrderedHitTriplets triplets;
-  cout << " here 2, call for triplets" << endl;
   generator->hitTriplets(region,triplets,es);
-  cout << " here 3, after call for triplets" << endl;
-  cout << "size of triplets: " << triplets.size() << endl;
-  cout << " HERE real end" << endl;
+  edm::LogInfo("HitTripletProducer") << "size of triplets: "<<triplets.size();
+  delete generator;
 
 }
 DEFINE_FWK_MODULE(HitTripletProducer)
