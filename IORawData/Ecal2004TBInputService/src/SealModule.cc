92125c3bc2e47964342c4461c6fc4b65407581f6
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-04-21 10:19:15 +0000

    First Release

---
+++ IORawData/Ecal2004TBInputService/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "IORawData/Ecal2004TBInputService/interface/Ecal2004TBSource.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(Ecal2004TBSource)
