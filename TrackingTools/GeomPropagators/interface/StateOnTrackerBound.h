5a7a351adadc23e4285e2979b23b0510d06a516d
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-04-21 14:31:49 +0000

    initial commit

---
+++ TrackingTools/GeomPropagators/interface/StateOnTrackerBound.h
@@ -0,0 +1,44 @@
+#ifndef GeomPropagators_StateOnTrackerBound_H
+#define GeomPropagators_StateOnTrackerBound_H
+
+class FreeTrajectoryState;
+class TrajectoryStateOnSurface;
+class Propagator;
+
+/** Propagates to the Tracker bounds, i.e. either to the
+ *  barrel cylinder or to one of the forward disks that 
+ *  constitute the envelope of the sensitive Tracker volumes.
+ */
+
+
+//Ported from ORCA
+//  $Date: $
+//  $Revision: $
+
+class StateOnTrackerBound {
+public:
+
+  StateOnTrackerBound(Propagator* prop);
+
+  ~StateOnTrackerBound();
+
+  TrajectoryStateOnSurface 
+  operator()( const TrajectoryStateOnSurface& tsos) const;
+
+  TrajectoryStateOnSurface 
+  operator()( const FreeTrajectoryState& fts) const;
+
+private:
+
+  Propagator* thePropagator;
+
+  TrajectoryStateOnSurface
+  checkDisk( const TrajectoryStateOnSurface& firstTry) const;
+
+  TrajectoryStateOnSurface
+  checkCylinder( const TrajectoryStateOnSurface& firstTry) const;
+
+};
+#endif
+
+
