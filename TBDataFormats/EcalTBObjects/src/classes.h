a4a585934cc64164f8869982d9b75996c5751a1c
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-04-21 09:33:23 +0000

    Added Tdc & Hodo Rec dataformats & EventHeader

--- TBDataFormats/EcalTBObjects/src/classes.h
+++ TBDataFormats/EcalTBObjects/src/classes.h
@@ -1,5 +1,8 @@
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBEventHeader.h"
 #include "TBDataFormats/EcalTBObjects/interface/EcalTBTDCRawInfo.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBTDCRecInfo.h"
 #include "TBDataFormats/EcalTBObjects/interface/EcalTBHodoscopeRawInfo.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBHodoscopeRecInfo.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
@@ -7,11 +10,21 @@ namespace {
     std::vector<EcalTBTDCSample> vTDC_;
     std::vector<EcalTBHodoscopePlaneRawHits> vHplaneRawHits_;
     
+    EcalTBEventHeader EHw_;
+
     EcalTBTDCRawInfo TDCw_;
+    EcalTBTDCRecInfo RecTDCw_;
+
     EcalTBHodoscopeRawInfo Hodow_;
+    EcalTBHodoscopeRecInfo RecHodow_;
+
+    edm::Wrapper<EcalTBEventHeader> theEHw_;
 
     edm::Wrapper<EcalTBTDCRawInfo> theTDCw_;
+    edm::Wrapper<EcalTBTDCRecInfo> theRecTDCw_;
+
     edm::Wrapper<EcalTBHodoscopeRawInfo> theHodow_;
+    edm::Wrapper<EcalTBHodoscopeRecInfo> theRecHodow_;
 
  }
 }
