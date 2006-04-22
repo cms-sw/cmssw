e7a6e88ce3ef6941deb11f250b677dfbc753d6bd
Rick Cavanaugh <sha1-821d96d34f96c9a702c199fc2c305113785482d3@cern.ch>
2006-04-22 22:50:04 +0000

    Removed old (non-candidate-based) TowerMET. Modified SealModule (removed TowerMET).

--- RecoMET/METProducers/src/SealModule.cc
+++ RecoMET/METProducers/src/SealModule.cc
@@ -2,13 +2,10 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoMET/METProducers/interface/TowerMETProducer.h"
 #include "RecoMET/METProducers/interface/METProducer.h"
 
-using cms::TowerMETProducer;
 using cms::METProducer;
 
 DEFINE_SEAL_MODULE();
 
-DEFINE_ANOTHER_FWK_MODULE(TowerMETProducer)
 DEFINE_ANOTHER_FWK_MODULE(METProducer)
