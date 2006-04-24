2d51197ff2254313e09ef36611d8ea27cf234c0c
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2006-04-24 00:11:56 +0000

    TrajectoryCleaning is used now

--- RecoTracker/CkfPattern/src/CombinatorialTrackFinder.cc
+++ RecoTracker/CkfPattern/src/CombinatorialTrackFinder.cc
@@ -10,7 +10,12 @@
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
 #include "DataFormats/TrackCandidate/interface/TrackCandidateCollection.h"
 
+#include "TrackingTools/TrajectoryCleaning/interface/TrajectoryCleanerBySharedHits.h"
 
+// --- temporary.for debug
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+// ---
 namespace cms
 {
 
@@ -20,17 +25,19 @@ namespace cms
   {
     isInitialized = 0;
     cout << "--- CombinatorialTrackFinder::constru is called " << endl;
+    theTrajectoryCleaner = new TrajectoryCleanerBySharedHits();
     produces<TrackCandidateCollection>();  //WHAT'S THIS???
   }
 
 
   // Virtual destructor needed.
-  CombinatorialTrackFinder::~CombinatorialTrackFinder() { }  
+  CombinatorialTrackFinder::~CombinatorialTrackFinder() {
+    delete theTrajectoryCleaner;
+  }  
 
   // Functions that gets called by framework every event
   void CombinatorialTrackFinder::produce(edm::Event& e, const edm::EventSetup& es)
-  {
-
+  {    
     // Bad temporary solution!!!
     if(isInitialized == 0){
       combinatorialTrajectoryBuilder_.init(es);
@@ -54,21 +61,53 @@ namespace cms
     // Step C: Invoke the building algorithm
     if ((*collseed).size()>0){
       cout << "---- collseed->size(): " << collseed->size() << " ------" << endl;
-      std::vector<Trajectory> theFinalTrajectories;
+      vector<Trajectory> theFinalTrajectories;
       TrajectorySeedCollection::const_iterator iseed;
-      
+            
+      vector<Trajectory> rawResult;
       for(iseed=theSeedColl.begin();iseed!=theSeedColl.end();iseed++){
-	std::vector<Trajectory> theTmpTrajectories;
-	cout << "--- combtrajBuilder.trajectories(seed,ev) called ----- " << endl;
+	vector<Trajectory> theTmpTrajectories;
 	theTmpTrajectories = combinatorialTrajectoryBuilder_.trajectories(*iseed,e);
-	/*
-	theFinalTrajectories.insert(theFinalTrajectories.end(),
-				    theTmpTrajectories.begin(),
-				    theTmpTrajectories.end()    );
-	*/
+	
+	theTrajectoryCleaner->clean(theTmpTrajectories);
+	
+	for(vector<Trajectory>::const_iterator it=theTmpTrajectories.begin();
+	    it!=theTmpTrajectories.end(); it++){
+	  if( it->isValid() ) rawResult.push_back(*it);
+	}
       }
-   
-     
+      
+      // clean the result
+      vector<Trajectory> unsmoothedResult;
+      theTrajectoryCleaner->clean(rawResult);
+      
+      for (vector<Trajectory>::const_iterator itraw = rawResult.begin();
+	   itraw != rawResult.end(); itraw++) {
+	if((*itraw).isValid()) unsmoothedResult.push_back( *itraw);
+      }
+      
+      cout << " ========== DEBUG TRACKFINDER: start  ========== " << endl;
+      edm::ESHandle<TrackerGeometry> tracker;
+      es.get<TrackerDigiGeometryRecord>().get(tracker);
+      for(iseed=theSeedColl.begin();iseed!=theSeedColl.end();iseed++){
+	DetId tmpId = DetId( iseed->startingState().detId());
+	const GeomDet* tmpDet  = tracker->idToDet( tmpId );
+	GlobalVector gv = tmpDet->surface().toGlobal( iseed->startingState().parameters().momentum() );
+	
+	cout << "seed perp,phi,eta : " 
+	     << gv.perp() << " , " 
+	     << gv.phi() << " , " 
+	     << gv.eta() << endl;
+      }
+      
+      cout << "numb finalTraj: " << unsmoothedResult.size() << endl;
+      for (vector<Trajectory>::const_iterator it = unsmoothedResult.begin();
+	   it != unsmoothedResult.end(); it++) {
+	cout << "n hit valid and invalid: " 
+	     << it->foundHits() << " , " << it->lostHits() << endl;
+      }
+      cout << " ========== DEBUG TRACKFINDER: end ========== " << endl;
+      
       // Step D: write output to file
       ///if ((*output).size()>0) e.put(output);
     }
