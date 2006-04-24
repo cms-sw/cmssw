a65225fd72266b64f693b02e1569e299dec89ebf
mfhansen <sha1-4c6d66dbcfddc4349cf1514bfba4442abf4433f7@cern.ch>
2006-04-24 12:03:10 +0000

    adapted new methods

--- L1Trigger/GlobalCaloTrigger/src/L1GctElectronFinalSort.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctElectronFinalSort.cc
@@ -5,38 +5,64 @@
  * ascending order. Inputs are the 4 highest Et electrons from
  * the leaf? cards
  *
- * \author Maria Hansen
- * \date April 2006
+ * \author  Maria Hansen
+ * \date    21/04/06
+ * \version 1.1
  */
 
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctElectronFinalSort.h"
+
+
+//Overloading the less than operator to use EmCand's
+bool compareInputs(L1GctEmCand a, L1GctEmCand b){
+  return a.getRank() > b.getRank();
+}
+
 
 L1GctElectronFinalSort::L1GctElectronFinalSort():inputCands(0),outputCands(4)
 {
 }
 
-L1GctElectronFinalSort::~L1GctElectronFinalSort()
-{
+L1GctElectronFinalSort::L1GctElectronFinalSort(vector<L1GctElectronSorter*> src){
+  theEmSorters = src;
 }
 
-void L1GctElectronFinalSort::reset()
-{
+L1GctElectronFinalSort::~L1GctElectronFinalSort(){
+}
+
+void L1GctElectronFinalSort::reset(){
   inputCands.clear();
   outputCands.clear();
 }
 
-void L1GctElectronFinalSort::fetchInput() {
-	
+void L1GctElectronFinalSort::fetchInput(){
+  for(vector<L1GctElectronSorter*>::iterator itSorted = theEmSorters.begin();itSorted!=theEmSorters.end();itSorted++){ 
+    inputCands = (*itSorted)->getOutput();
+  }
 }
 
-void L1GctElectronFinalSort::process()
-{
-  //sortedCands.process();
-  //outputCands = sortedCands.getOutput();
+void L1GctElectronFinalSort::process(){
+//Make temporary copy of data
+    vector<L1GctEmCand> data = inputCands;
+    
+//Then sort it
+    sort(data.begin(),data.end(),compareInputs);
+  
+//Copy data to output buffer
+    for(int i = 0; i<4; i++){
+      outputCands[i] = data[i];
+    }
 }
 
-void L1GctElectronFinalSort::setInputEmCand(int i, L1GctEmCand cand)
-{
-  inputCands[i] = cand;
+void L1GctElectronFinalSort::setInputEmCand(int i, L1GctEmCand cand){
+  // inputCands[i] = cand;
+  //using push_back for now until know how many to set the inputCands vector to in constructor
+  inputCands.push_back(cand);
 }
+
+
+
+
+
+
 
