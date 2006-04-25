e1c510923c62f6210f046dc57bad8e891fd15481
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-04-25 00:56:03 +0000

    Added overflow bit functionality + other minor implementation changes

--- L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
@@ -20,7 +20,7 @@ class L1GctRegion
 {
 public:
 
-	L1GctRegion(ULong et=0, bool mip=false, bool quiet=false, bool tauVeto=true);
+	L1GctRegion(ULong et=0, bool mip=false, bool quiet=false, bool tauVeto=true, bool overFlow=false);
 	~L1GctRegion();
 	
     // Getters
@@ -28,21 +28,26 @@ public:
 	bool getMip() const { return myMip; }
 	bool getQuiet() const { return myQuiet; }
     bool getTauVeto() const { return myTauVeto; }
+    bool getOverFlow() const { return myOverFlow; }
     
     // Setters
     void setEt(ULong et) { myEt = et; } 
     void setMip(bool mip) { myMip = mip; }
     void setQuiet(bool quiet) { myQuiet = quiet; }
     void setTauVeto(bool tauVeto) { myTauVeto = tauVeto; }
+    void setOverFlow(bool overFlow) { myOverFlow = overFlow; }
 
     //	ostream& operator << (ostream& os, const L1GctRegion& s);
+    
+    static const int ET_BITWIDTH = 10;
 		
 private:
 
-	bitset<10> myEt;
+	bitset<ET_BITWIDTH> myEt;
 	bool myMip;
 	bool myQuiet;
     bool myTauVeto;
+    bool myOverFlow;
 	
 	
 	
--- L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
@@ -1,11 +1,12 @@
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h"
 
 
-L1GctRegion::L1GctRegion(ULong et, bool mip, bool quiet, bool tauVeto):
+L1GctRegion::L1GctRegion(ULong et, bool mip, bool quiet, bool tauVeto, bool overFlow):
 myEt(et),
 myMip(mip),
 myQuiet(quiet),
-myTauVeto(tauVeto)
+myTauVeto(tauVeto),
+myOverFlow(overFlow)
 {
 }
 
