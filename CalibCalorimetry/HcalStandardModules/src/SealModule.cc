d8533075ada230e8e2d5ad58049820c70280ca96
Michal Szleper <sha1-8dc5ee44d1f0d27d89108086e7cf38e4a70bd448@cern.ch>
2006-04-25 16:32:32 +0000

    Include HcalLedAnalyzer.

--- CalibCalorimetry/HcalStandardModules/src/SealModule.cc
+++ CalibCalorimetry/HcalStandardModules/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include <CalibCalorimetry/HcalStandardModules/interface/HcalPedestalAnalyzer.h>
+#include <CalibCalorimetry/HcalStandardModules/interface/HcalLedAnalyzer.h>
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(HcalPedestalAnalyzer)
+DEFINE_ANOTHER_FWK_MODULE(HcalPedestalAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(HcalLedAnalyzer)
