945068eb4fdf5dab5b3b34d53ae01d4c1ac4a2ac
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2006-04-25 17:58:22 +0000

    label of hit collection configurable

--- RecoTracker/SingleTrackPattern/src/CosmicTrackFinder.cc
+++ RecoTracker/SingleTrackPattern/src/CosmicTrackFinder.cc
@@ -21,6 +21,7 @@ namespace cms
     cosmicTrajectoryBuilder_(conf) ,
     conf_(conf)
   {
+
     //    produces<TrackCandidateCollection>();
     produces<reco::TrackCollection>();
   }
@@ -32,6 +33,9 @@ namespace cms
   // Functions that gets called by framework every event
   void CosmicTrackFinder::produce(edm::Event& e, const edm::EventSetup& es)
   {
+
+    std::string hitProducer = conf_.getParameter<std::string>("HitProducer");
+
     // Step A: Get Inputs 
     
     // retrieve seeds
@@ -42,14 +46,15 @@ namespace cms
     e.getByType(pixelHits);
     //retrieve StripRecHits
     edm::Handle<SiStripRecHit2DMatchedLocalPosCollection> matchedrecHits;
-    e.getByLabel("SiStripRecHits","matchedRecHit" ,matchedrecHits);
-    edm::Handle<SiStripRecHit2DLocalPosCollection> rphirecHits;
-    e.getByLabel("SiStripRecHits","rphiRecHit" ,rphirecHits);
+    //    e.getByLabel("SiStripRecHits","matchedRecHit" ,matchedrecHits);
+    e.getByLabel(hitProducer,"matchedRecHit" ,matchedrecHits);
+   edm::Handle<SiStripRecHit2DLocalPosCollection> rphirecHits;
+    //    e.getByLabel("SiStripRecHits","rphiRecHit" ,rphirecHits);
+   e.getByLabel(hitProducer,"rphiRecHit" ,rphirecHits);
+
     edm::Handle<SiStripRecHit2DLocalPosCollection> stereorecHits;
-    e.getByLabel("SiStripRecHits","stereoRecHit" ,stereorecHits);
-    
-
-
+    //   e.getByLabel("SiStripRecHits","stereoRecHit" ,stereorecHits);
+    e.getByLabel(hitProducer,"stereoRecHit" ,stereorecHits);
 
     // Step B: create empty output collection
     std::auto_ptr<reco::TrackCollection> output(new reco::TrackCollection);
@@ -69,10 +74,7 @@ namespace cms
       
       
       // Step D: write output to file
-      if ((*output).size()>0)	{
-	std::cout<<"cumino "<<(*output).size()<<std::endl;
-	e.put(output);
-      }
+      if ((*output).size()>0)	e.put(output);
     }
   }
   
