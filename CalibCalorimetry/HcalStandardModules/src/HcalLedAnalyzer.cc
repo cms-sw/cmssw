40200f3382029fa9b1587e4de96a011f599918b2
Michal Szleper <sha1-8dc5ee44d1f0d27d89108086e7cf38e4a70bd448@cern.ch>
2006-04-25 16:31:58 +0000

    New package for LED data analysis.

---
+++ CalibCalorimetry/HcalStandardModules/src/HcalLedAnalyzer.cc
@@ -0,0 +1,127 @@
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
+
+#include <CalibCalorimetry/HcalStandardModules/interface/HcalLedAnalyzer.h>
+#include "CalibCalorimetry/HcalAlgos/interface/HcalAlgoUtils.h"
+#include "CalibCalorimetry/HcalAlgos/interface/HcalDbASCIIIO.h"
+#include "CondTools/Hcal/interface/HcalDbPool.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
+
+#include <iostream>
+#include <fstream>
+
+/*
+namespace {
+  bool defaultsFile (const std::string fParam) {
+    return fParam == "defaults";
+  }
+
+  bool asciiFile (const std::string fParam) {
+    return fParam.find (':') == std::string::npos && std::string (fParam, fParam.length () - 4) == ".txt";
+  }
+  
+  bool xmlFile (const std::string fParam) {
+    return fParam.find (':') == std::string::npos && std::string (fParam, fParam.length () - 4) == ".xml";
+  }
+  
+  bool dbFile (const std::string fParam) {
+    return fParam.find (':') != std::string::npos;
+  }
+
+  template <class T> 
+  bool getObject (T* fObject, const std::string& fDb, const std::string& fTag, int fRun) {
+    if (!fObject) return false;
+    if (fDb.empty ()) return false; 
+    if (asciiFile (fDb)) {
+      std::cout << "HcalLedAnalyzer-> USE INPUT: ASCII " << std::endl;
+      std::ifstream stream (fDb.c_str ());
+      HcalDbASCIIIO::getObject (stream, fObject); 
+      return true;
+    }
+    else if (dbFile (fDb)) {
+      std::cout << "HcalLedAnalyzer-> USE INPUT: Pool " << fDb << std::endl;
+      HcalDbPool poolDb (fDb);
+      return poolDb.getObject (fObject, fTag, fRun);
+    }
+    else {
+      std::cerr << "HcalLedAnalyzer-> Unknown input type " << fDb << std::endl;
+      return false;
+    }
+  }
+  
+} */
+
+HcalLedAnalyzer::HcalLedAnalyzer(const edm::ParameterSet& ps){
+
+  m_ledAnal = new HcalLedAnalysis(ps);
+  m_ledAnal->LedSetup(ps.getUntrackedParameter<std::string>("outputFileHist", "HcalLedAnalyzer.root"));
+
+//  m_startSample = ps.getUntrackedParameter<int>("firstSample", 0);
+//  m_endSample = ps.getUntrackedParameter<int>("lastSample", 19);
+  m_inputPedestals_source = ps.getUntrackedParameter<std::string>("inputPedsSource", "");
+  m_inputPedestals_tag = ps.getUntrackedParameter<std::string>("inputPedsTag", "");
+  m_inputPedestals_run = ps.getUntrackedParameter<int>("inputPedsRun", 1);
+
+  // CORAL required variables to be set, even if not needed
+  if (!::getenv("CORAL_AUTH_USER")) ::putenv("CORAL_AUTH_USER=blah");
+  if (!::getenv("CORAL_AUTH_PASSWORD")) ::putenv("CORAL_AUTH_PASSWORD=blah"); 
+}
+
+HcalLedAnalyzer::~HcalLedAnalyzer(){
+  delete m_ledAnal;
+}
+
+void HcalLedAnalyzer::beginJob(const edm::EventSetup& c){
+  m_ievt = 0;
+  led_sample = 1;
+}
+
+void HcalLedAnalyzer::endJob(void) {
+
+  m_ledAnal->LedDone();
+
+//  delete m_inputPeds;
+
+  std::cout<<"Getting out"<<std::endl;
+}
+
+void HcalLedAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& eventSetup){
+
+  m_ievt++;
+
+  ///get digis
+  edm::Handle<HBHEDigiCollection> hbhe; e.getByType(hbhe);
+  edm::Handle<HODigiCollection> ho;     e.getByType(ho);
+  edm::Handle<HFDigiCollection> hf;     e.getByType(hf);
+
+  // get conditions
+  edm::ESHandle<HcalDbService> conditions;
+  eventSetup.get<HcalDbRecord>().get(conditions);
+
+  // get pedestals
+/*  HcalPedestals* inputPeds=0;
+  if(m_ievt==1){
+    if (!m_inputPedestals_source.empty ()) {
+      m_inputPeds = new HcalPedestals ();
+      if (!getObject (m_inputPeds, m_inputPedestals_source, m_inputPedestals_tag, m_inputPedestals_run)) {
+        std::cerr << "HcalLedAnalyzer-> Failed to get pedestal values" << std::endl;
+      }
+    }
+  }
+  inputPeds = new HcalPedestals ();
+  inputPeds = m_inputPeds; */
+
+//  m_ledAnal->processLedEvent(*hbhe, *ho, *hf, *conditions, inputPeds);
+  m_ledAnal->processLedEvent(*hbhe, *ho, *hf, *conditions);
+
+//  delete inputPeds;
+
+  if(m_ievt%1000 == 0)
+    std::cout << "HcalLedAnalyzer: analyzed " << m_ievt << " events" << std::endl;
+
+  return;
+}
+
