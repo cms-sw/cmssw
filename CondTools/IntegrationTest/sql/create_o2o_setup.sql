4d2d9ec13245fedf6c5e734d73fcda3bec207155
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-04-25 20:56:00 +0000

    Changed to work using schema permissions.  Added exception handling.

--- CondTools/IntegrationTest/sql/create_o2o_setup.sql
+++ CondTools/IntegrationTest/sql/create_o2o_setup.sql
@@ -1,10 +1,12 @@
+/* Stores the schema name and top-level-table of an object that undergoes O2O */
+
 CREATE TABLE o2o_setup (
   object_name VARCHAR2(32),
+  schema VARCHAR(32),
   top_level_table VARCHAR2(32),
-  db_link VARCHAR2(32),
   PRIMARY KEY (object_name)
 );
 
-INSERT INTO o2o_setup VALUES ('EcalPedestals', 'ECALPEDESTALS', 'orcon.cern.ch@cms_cond_ecal');
+INSERT INTO o2o_setup VALUES ('EcalPedestals', 'CMS_COND_ECAL', 'ECALPEDESTALS');
 
 COMMIT;
--- CondTools/IntegrationTest/sql/master_payload_o2o.sql
+++ CondTools/IntegrationTest/sql/master_payload_o2o.sql
@@ -7,37 +7,46 @@
  */
 
 CREATE OR REPLACE PROCEDURE master_payload_o2o (
-  object_name IN VARCHAR2
+  i_object_name IN VARCHAR2
 )
 AS
 
-top_level_table VARCHAR2(32);
-db_link VARCHAR2(32);
-table_link VARCHAR2(65);
-start_time TIMESTAMP;
-plsql_block VARCHAR2(256);
-last_id NUMBER(10);
-cnt1 NUMBER(10);
-cnt2 NUMBER(10);
+schema VARCHAR2(32) := NULL;
+top_level_table VARCHAR2(32) := NULL;
+schema_table VARCHAR(65) := NULL;
+start_time TIMESTAMP := NULL;
+plsql_block VARCHAR2(256) := NULL;
+last_id NUMBER(10) := NULL;
+cnt1 NUMBER(10) := 0;
+cnt2 NUMBER(10) := 0;
 
 BEGIN
   -- Check that <object_name>_payload_o2o() exists
 
   -- Get the top level table
-  SELECT top_level_table, db_link INTO top_level_table, db_link 
-    FROM O2O_SETUP WHERE object_name = object_name;
-  
-  -- Check the setup variables
+  BEGIN
+    SELECT schema, top_level_table INTO schema, top_level_table 
+      FROM O2O_SETUP WHERE object_name = i_object_name;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      raise_application_error(-20101, 'Object ' || i_object_name || ' not found in O2O_SETUP table');
+  END;
 
-  table_link := top_level_table || '@' || db_link;
+  schema_table := schema || '.' || top_level_table;
 
   -- Get the count and ID variables
-  EXECUTE IMMEDIATE 'SELECT count(*) FROM ' || table_link INTO cnt1;
-  EXECUTE IMMEDIATE 'SELECT max(IOV_VALUE_ID) FROM ' || table_link INTO last_id;
+  BEGIN
+    EXECUTE IMMEDIATE 'SELECT count(*) FROM ' || schema_table INTO cnt1;
+    EXECUTE IMMEDIATE 'SELECT max(IOV_VALUE_ID) FROM ' || schema_table INTO last_id;
+  EXCEPTION
+    WHEN OTHERS THEN /* table not found */
+      raise_application_error(-20102, 'Cannot read top-level-table ' || schema_table, TRUE);
+  END;
 
+
+  -- It is the first transfer, ensure ALL ids are transferred
+  -- Use -1 because all IOV_VALUE_ID are > 0
   IF last_id IS NULL THEN
-    -- It is the first transfer, ensure ALL ids are transferred
-    -- Use -1 because all IOV_VALUE_ID are > 0
     last_id := -1;
   END IF;
 
@@ -45,18 +54,23 @@ BEGIN
   SELECT systimestamp INTO start_time FROM dual;
 
   -- Execute transfer
-  plsql_block := 'BEGIN ' || object_name || '_payload_o2o@' || db_link ||'(:last_id); END;';
-  EXECUTE IMMEDIATE plsql_block USING last_id;
+  BEGIN
+    plsql_block := 'BEGIN ' || schema || '.' || i_object_name || '_payload_o2o(:last_id); END;';
+    EXECUTE IMMEDIATE plsql_block USING last_id;
+  EXCEPTION
+    WHEN OTHERS THEN /* procedure not found */
+      raise_application_error(-20103, 'Cannot read ' || i_object_name || '_payload_o2o procedure', TRUE);
+  END;
 
   -- Get the count and ID variables again
-  EXECUTE IMMEDIATE 'SELECT count(*) FROM ' || table_link INTO cnt2;
-  EXECUTE IMMEDIATE 'SELECT max(IOV_VALUE_ID) FROM ' || table_link INTO last_id;
+  EXECUTE IMMEDIATE 'SELECT count(*) FROM ' || schema_table INTO cnt2;
+  EXECUTE IMMEDIATE 'SELECT max(IOV_VALUE_ID) FROM ' || schema_table INTO last_id;
 
-
-  -- Write into O2O_LOG
-  INSERT INTO o2o_log (object_name, last_id, num_transfered, transfer_start, transfer_duration)
-       VALUES (object_name, last_id, cnt2 - cnt1, start_time, start_time - systimestamp);
-
+  -- Write into O2O_LOG if there was a change
+  IF cnt2 != cnt1 THEN
+    INSERT INTO o2o_log (object_name, last_id, num_transferred, transfer_start, transfer_duration)
+         VALUES (i_object_name, last_id, cnt2 - cnt1, start_time, start_time - systimestamp);
+  END IF;
   COMMIT;
 
 END;
