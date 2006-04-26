5acce94acfe4eb91b0c919ef4612e54c5f46b83c
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-04-26 22:03:18 +0000

    Turned into procedures

--- CondTools/IntegrationTest/sql/CSCGains_payload_o2o.sql
+++ CondTools/IntegrationTest/sql/CSCGains_payload_o2o.sql
@@ -1,20 +1,34 @@
-REM For CSCGAINS:
+/*
+ *  CSCGains_payload_o2o()
+ *
+ *  CSCGains transform/transfer
+ *  Parameters:  last_id:  The lower bounding IOV_VALUE_ID for objects to transfer
+ */
+
+CREATE OR REPLACE PROCEDURE CSCGains_payload_o2o (
+  last_id IN NUMBER
+)
+AS
+
+BEGIN
+
 insert into "CSCGAINS"
 SELECT
  record_id iov_value_id,
  run_num time
-FROM GAINS;
+FROM GAINS@cmsomds
+WHERE iov_value_id > last_id;
 
-REM For CSCGAINS_MAP:
 insert into "CSCGAINS_MAP"
 SELECT
  map_index map_id,
  record_id iov_value_id,
  layer_id csc_int_id
-FROM GAINS_MAP
- order by iov_value_id,map_id;
+FROM GAINS_MAP@cmsomds
+WHERE iov_value_id > last_id
+order by iov_value_id,map_id;
 
-REM For CSCGAINS_DATA:
+
 insert into "CSCGAINS_DATA"
 SELECT
  GAINS_DATA.vec_index vec_index,
@@ -23,10 +37,15 @@ SELECT
  GAINS_DATA.gain_chi2 gains_chi2, 
  GAINS_DATA.gain_intercept gains_intercept,
  GAINS_DATA.gain_slope gains_slope
-FROM GAINS_DATA,GAINS_MAP
+FROM GAINS_DATA@cmsomds, GAINS_MAP@cmsomds
 WHERE
  GAINS_DATA.map_id=GAINS_MAP.map_id
+AND iov_value_id > last_id
 ORDER BY
  iov_value_id,
  map_id,
  vec_index;
+
+END;
+/
+show errors;
--- CondTools/IntegrationTest/sql/CSCPedestals_payload_o2o.sql
+++ CondTools/IntegrationTest/sql/CSCPedestals_payload_o2o.sql
@@ -1,20 +1,35 @@
-REM For CSCPEDESTALS:
+/*
+ *  CSCPedestals_payload_o2o()
+ *
+ *  CSCPedestals transform/transfer
+ *  Parameters:  last_id:  The lower bounding IOV_VALUE_ID for objects to transfer
+ */
+
+CREATE OR REPLACE PROCEDURE CSCPedestals_payload_o2o (
+  last_id IN NUMBER
+)
+AS
+
+BEGIN
+
 insert into "CSCPEDESTALS"
 SELECT
  record_id iov_value_id,
  run_num time
-FROM PEDESTALS;
+FROM PEDESTALS@cmsomds
+WHERE iov_value_id > last_id;
 
-REM For CSCPEDESTALS_MAP:
+
 insert into "CSCPEDESTALS_MAP"
 SELECT
  map_index map_id,
  record_id iov_value_id,
  layer_id csc_int_id
-FROM PEDESTALS_MAP
+FROM PEDESTALS_MAP@cmsomds
+WHERE iov_value_id > last_id
  order by iov_value_id,map_id;
 
-REM For CSCPEDESTALS_DATA:
+
 insert into "CSCPEDESTALS_DATA"
 SELECT
  PEDESTALS_DATA.vec_index vec_index,
@@ -22,10 +37,15 @@ SELECT
  PEDESTALS_MAP.record_id iov_value_id,
  PEDESTALS_DATA.ped pedestals_ped, 
  PEDESTALS_DATA.rms pedestals_rms
-FROM  PEDESTALS_DATA,PEDESTALS_MAP
+FROM PEDESTALS_DATA@cmsomds, PEDESTALS_MAP@cmsomds
 WHERE
  PEDESTALS_DATA.map_id=PEDESTALS_MAP.map_id
+AND iov_value_id > last_id
 ORDER BY
  iov_value_id,
  map_id,
  vec_index;
+
+END;
+/
+show errors;
