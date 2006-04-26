8b4d725866e121945db1277d00a1284c9398ad71
Andrew Warren Askew <sha1-4f9917bf818f9bc17be25445433598f3a186dfc7@cern.ch>
2006-04-26 19:47:37 +0000

    Added HybridClusterProducer

--- RecoEcal/EgammaClusterProducers/src/SealModule.cc
+++ RecoEcal/EgammaClusterProducers/src/SealModule.cc
@@ -3,10 +3,11 @@
 #include "RecoEcal/EgammaClusterProducers/interface/BumpProducer.h"
 #include "RecoEcal/EgammaClusterProducers/interface/TestClusterProducer.h"
 #include "RecoEcal/EgammaClusterProducers/interface/TestSCProducer.h"
-
+#include "RecoEcal/EgammaClusterProducers/interface/HybridClusterProducer.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(BumpProducer);
 DEFINE_ANOTHER_FWK_MODULE(TestClusterProducer);
 DEFINE_ANOTHER_FWK_MODULE(TestSCProducer);
+DEFINE_ANOTHER_FWK_MODULE(HybridClusterProducer);
