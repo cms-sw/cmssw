a88dbf9a43e556da35aaef7c920f015d216c8ca1
Andrew Warren Askew <sha1-4f9917bf818f9bc17be25445433598f3a186dfc7@cern.ch>
2006-04-26 19:49:35 +0000

    Small updates to includes.

--- RecoEcal/EgammaClusterAlgos/interface/LogPositionCalc.h
+++ RecoEcal/EgammaClusterAlgos/interface/LogPositionCalc.h
@@ -7,14 +7,15 @@
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
-
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 // CMSSW headers from this subsystem
 #include "DataFormats/Math/interface/Point3D.h"
 // C/C++ headers
 #include <string>
 #include <vector>
 typedef math::XYZPoint Point;
-Point getECALposition(std::vector<reco::EcalRecHitData> recHits, const CaloSubdetectorGeometry & geometry);//Position determination
+Point getECALposition(std::vector<reco::EcalRecHitData> recHits, const CaloSubdetectorGeometry);//Position determination
 
 
 
