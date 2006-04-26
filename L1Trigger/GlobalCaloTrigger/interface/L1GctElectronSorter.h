045d1d73ae66b2fe4b8d54fd256073b6e8e62fcf
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2006-04-26 13:02:07 +0000

    updated ElectronSorter methods

--- L1Trigger/GlobalCaloTrigger/interface/L1GctElectronSorter.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctElectronSorter.h
@@ -20,43 +20,48 @@ class L1GctSourceCard;
 class L1GctElectronSorter : public L1GctProcessor
 {
 public:
-        L1GctElectronSorter();
-	L1GctElectronSorter(int inputType);
-	L1GctElectronSorter(vector<L1GctSourceCard*> src);	
-	~L1GctElectronSorter();
-	///
-	/// clear internal buffers
-	virtual void reset();
-	///
-	/// get input data from sources
-	virtual void fetchInput();
-	///
-	/// process the data, fill output buffers
-	virtual void process();
-	///
-	/// set input candidate
-	void setInputEmCand(L1GctEmCand cand);
-	///	
-	/// get input candidates
-	inline vector<L1GctEmCand> getInput() { return inputCands; }
-	///
-	/// get output candidates
-	inline vector<L1GctEmCand> getOutput() { return outputCands; }
+  ///
+  /// constructor; set tyep (isolated or non-isolated)
+  L1GctElectronSorter(bool iso=true);
+  ~L1GctElectronSorter();
+  ///
+  /// clear internal buffers
+  virtual void reset();
+  ///
+  /// get input data from sources
+  virtual void fetchInput();
+  ///
+  /// process the data, fill output buffers
+  virtual void process();
+  ///
+  /// set an input Source Card pointer
+  void setInputSourceCard(int i, L1GctSourceCard* sc);
+  ///
+  /// set input candidate
+  void setInputEmCand(L1GctEmCand cand);
+  ///	
+  /// get input candidates
+  inline vector<L1GctEmCand> getInput() { return inputCands; }
+  ///
+  /// get output candidates
+  inline vector<L1GctEmCand> getOutput() { return outputCands; }
 	
 private:
-	
-	///
-	/// source card input
-	vector<L1GctSourceCard*> theSCs;
-	///
-	/// input data
-	vector<L1GctEmCand> inputCands;
-	///
-	/// output data
-	vector<L1GctEmCand> outputCands;
-	///
-	/// input variable to set iso or non-iso electrons
-	int theInputType;
+  ///
+  /// type of sorter (isolated or non isolated)
+  bool getIsoEmCands;
+  ///
+  /// source card input
+  vector<L1GctSourceCard*> theSCs;
+  ///
+  /// input data
+  vector<L1GctEmCand> inputCands;
+  ///
+  /// output data
+  vector<L1GctEmCand> outputCands;
+  ///
+  /// input variable to set iso or non-iso electrons
+  int theInputType;
 };
 
 #endif /*L1GCTELECTRONSORTER_H_*/
--- L1Trigger/GlobalCaloTrigger/src/L1GctElectronSorter.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctElectronSorter.cc
@@ -7,19 +7,26 @@
  * \author Maria Hansen
  * \date March 2006
  */
-//Below paths must be changed when scramming is possible
+
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctElectronSorter.h"
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctEmCand.h"
-#include<iostream>
+#include "L1Trigger/GlobalCaloTrigger/interface/L1GctSourceCard.h"
+#include <iostream>
+#include <sort>
 
 using std::cout;
+using std::sort;
 
 //Overloading the less than operator to use EmCand's
 bool compare(L1GctEmCand a, L1GctEmCand b){
     return a.getRank() > b.getRank();
 }
 
-L1GctElectronSorter::L1GctElectronSorter():inputCands(0),outputCands(4)
+L1GctElectronSorter::L1GctElectronSorter(bool iso):
+  getIsoEmCands(iso),
+  theSCs(5),
+  inputCands(0),
+  outputCands(4)
 {
 
 }
@@ -29,20 +36,35 @@ L1GctElectronSorter::~L1GctElectronSorter()
 
 }
 
-void L1GctElectronSorter::setInputEmCand(L1GctEmCand cand){
-  inputCands.push_back(cand);
-}
-
+// clear buffers
 void L1GctElectronSorter::reset() {
-	
+  inputCands.clear();
+  outputCands.clear();
 }
 
+// get the input data
 void L1GctElectronSorter::fetchInput() {
-	
+
+  // loop over Source Cards
+  for (unsigned i=0; i<theSCs.size(); i++) {
+    
+    // loop over 4 candidates per Source Card
+    for (unsigned j=0; j<4; j++) {
+
+      // get EM candidates, depending on type
+      if (getIsoEmCands) {
+	setInputEmCand(theSCs[i]->getIsoElectrons()[j]);
+      }
+      else {
+	setInputEmCand(theSCs[i]->getNonIsoElectrons()[j]);
+      }
+    }
+  }
+
 }
 
 //Process sorts the electron candidates after rank and stores the highest four (in the outputCands vector)
-void L1GctElectronSorter::process(){
+void L1GctElectronSorter::process() {
 
 //Make temporary copy of data
     vector<L1GctEmCand> data = inputCands;
@@ -56,5 +78,13 @@ void L1GctElectronSorter::process(){
     }
 }
 
+void L1GctElectronSorter::setInputSourceCard(int i, L1GctSourceCard* sc) {
+  if (i < theSCs.size()) {
+    theSCs[i]=sc;
+  }
+}
 
+void L1GctElectronSorter::setInputEmCand(L1GctEmCand cand){
+  inputCands.push_back(cand);
+}
 
