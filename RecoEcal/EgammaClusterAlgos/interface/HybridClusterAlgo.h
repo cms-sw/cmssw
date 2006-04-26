a53423ecdbeda16685ce5067e933c9f3dc579287
Andrew Warren Askew <sha1-4f9917bf818f9bc17be25445433598f3a186dfc7@cern.ch>
2006-04-26 19:48:17 +0000

    Small updates to go with the new cluster producer.  Now has method to create superclusters.

--- RecoEcal/EgammaClusterAlgos/interface/HybridClusterAlgo.h
+++ RecoEcal/EgammaClusterAlgos/interface/HybridClusterAlgo.h
@@ -2,11 +2,14 @@
 #define RecoEcal_EgammaClusterAlgos_HybridClusterAlgo_h
 
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
 #include "RecoCaloTools/Navigation/interface/EcalBarrelNavigator.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
 #include "RecoEcal/EgammaClusterAlgos/interface/LogPositionCalc.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 #include <vector>
 
 struct less_mag : public std::binary_function<EcalRecHit, EcalRecHit, bool> {
@@ -20,13 +23,14 @@ class HybridClusterAlgo
   double eb_st, ec_st;
   int phi_steps;
   double Ethres, Ewing;
+  double Eseed;
   // Map of DetId, bool is if Det has been 
   // used already.
   std::map<EBDetId, std::pair<EcalRecHit, bool> >  rechits_m;
 
   // The vector of seeds:
   std::vector<EcalRecHit> seeds;
-
+  std::map<int, std::vector<reco::BasicCluster> > _clustered;
  public:
   
   HybridClusterAlgo(){}
@@ -42,16 +46,22 @@ class HybridClusterAlgo
 		    double ec_str, 
 		    int step, 
 		    double ethresh, 
-		    double ewing) : eb_st(eb_str), ec_st(ec_str), 
-    phi_steps(step), Ethres(ethresh), Ewing(ewing)
+		    double ewing,
+		    double eseed) : eb_st(eb_str), ec_st(ec_str), 
+    phi_steps(step), Ethres(ethresh), Ewing(ewing), Eseed(eseed)
   {
     
   }
   
-  std::vector<reco::BasicCluster> makeClusters(EcalRecHitCollection & rechits, const CaloSubdetectorGeometry & geometry);
-  std::vector<reco::BasicCluster> mainSearch(const CaloSubdetectorGeometry & geometry);
+  //  void makeClusters(EcalRecHitCollection & rechits, const CaloSubdetectorGeometry & geometry, reco::BasicClusterCollection &basicClusters);
+  void makeClusters(EcalRecHitCollection & rechits, edm::ESHandle<CaloGeometry> , reco::BasicClusterCollection &basicClusters);
+
+  reco::SuperClusterCollection makeSuperClusters(reco::BasicClusterRefVector);
+
+  void mainSearch( const CaloSubdetectorGeometry geometry);
   double makeDomino(EcalBarrelNavigator &navigator, std::vector <EcalRecHit> &cells);
-  friend Point getECALposition(std::vector<reco::EcalRecHitData> recHits, const CaloSubdetectorGeometry & geometry);//Position determination
+
+  friend Point getECALposition(std::vector<reco::EcalRecHitData> recHits,const CaloSubdetectorGeometry );//Position determination
 
 };
 
