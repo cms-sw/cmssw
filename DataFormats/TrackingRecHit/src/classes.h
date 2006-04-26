6a250a3daf0c3b2724a9a5864b913abe5b9ee2c3
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-04-26 07:40:52 +0000

    added dependences for 2D and 4D segment

--- DataFormats/TrackingRecHit/src/classes.h
+++ DataFormats/TrackingRecHit/src/classes.h
@@ -1,11 +1,15 @@
 #include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
 #include "DataFormats/TrackingRecHit/interface/RecHit1D.h"
 #include "DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h"
+#include "DataFormats/TrackingRecHit/interface/RecSegment2D.h"
+#include "DataFormats/TrackingRecHit/interface/RecSegment4D.h"
 #include "DataFormats/TrackingRecHit/interface/InvalidTrackingRecHit.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   namespace {
+    AlgebraicSymMatrix as;
+
     std::vector<TrackingRecHit*> v1;
     TrackingRecHitCollection c1;
     TrackingRecHitRef r1;
