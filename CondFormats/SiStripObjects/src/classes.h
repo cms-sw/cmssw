7243b9a7d6a6f78a55694fa933584b0977e488fb
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-04-28 09:26:38 +0000

    added fed cabling to classes_def.xml, tidied up classes.h

--- CondFormats/SiStripObjects/src/classes.h
+++ CondFormats/SiStripObjects/src/classes.h
@@ -1,28 +1,27 @@
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
-#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
-#include "CondFormats/SiStripObjects/interface/SiStripReadoutCabling.h"
-#include "CondFormats/SiStripObjects/interface/SiStripControlCabling.h"
-// Declaration of the iterator (necessary for the generation of the dictionary)
-
 template std::vector<SiStripPedestals::SiStripData>::iterator;
 template std::vector<SiStripPedestals::SiStripData>::const_iterator;
 template std::map< uint32_t, SiStripPedestalsVector>::iterator;
 template std::map< uint32_t, SiStripPedestalsVector>::const_iterator;
 
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
 template std::vector<SiStripNoises::SiStripData>::iterator;
 template std::vector<SiStripNoises::SiStripData>::const_iterator;
 template std::map< uint32_t, SiStripNoiseVector>::iterator;
 template std::map< uint32_t, SiStripNoiseVector>::const_iterator;
 
+#include "CondFormats/SiStripObjects/interface/SiStripReadoutCabling.h"
 template std::vector<std::pair<uint32_t, unsigned short> >::iterator;
 template std::vector<std::pair<uint32_t, unsigned short> >::const_iterator;
 
+#include "CondFormats/SiStripObjects/interface/SiStripControlCabling.h"
 template std::vector< std::vector<std::pair<uint32_t, unsigned short> > >::iterator;
 template std::vector< std::vector<std::pair<uint32_t, unsigned short> > >::const_iterator;
 template std::vector<unsigned short>::iterator;
 template std::vector<unsigned short>::const_iterator;
 
 #include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+#include "CondFormats/SiStripObjects/interface/FedChannelConnection.h"
 template std::vector< std::vector<FedChannelConnection> >::iterator;
 template std::vector< std::vector<FedChannelConnection> >::const_iterator;
 template std::vector<FedChannelConnection>::iterator;
