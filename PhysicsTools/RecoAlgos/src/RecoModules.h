d839c8aad54c957942a18b5d71d620b5bcb10cbb
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-04-28 07:11:39 +0000

    added collection recover module for tracks

--- PhysicsTools/RecoAlgos/src/RecoModules.h
+++ PhysicsTools/RecoAlgos/src/RecoModules.h
@@ -1,7 +1,8 @@
 #ifndef RecoAlgos_RecoModules_h
 #define RecoAlgos_RecoModules_h
-// $Id: RecoModules.h,v 1.2 2006/04/10 08:34:51 llista Exp $
+// $Id: RecoModules.h,v 1.3 2006/04/12 10:39:33 llista Exp $
 #include "PhysicsTools/UtilAlgos/interface/Merger.h"
+#include "PhysicsTools/UtilAlgos/interface/CollectionRecoverer.h"
 #include "DataFormats/Common/interface/CopyPolicy.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
@@ -11,13 +12,16 @@
 namespace reco {
   namespace modules {
     /// Framework module to merge an arbitray number of reco::TrackCollection
-    typedef Merger<reco::TrackCollection,    edm::CopyPolicy<reco::Track> >    TrackMerger;
+    typedef Merger<reco::TrackCollection, edm::CopyPolicy<reco::Track> >    TrackMerger;
     /// Framework module to merge an arbitray number of reco::MuonCollection
-    typedef Merger<reco::MuonCollection,     edm::CopyPolicy<reco::Muon> >     MuonMerger;
+    typedef Merger<reco::MuonCollection, edm::CopyPolicy<reco::Muon> >     MuonMerger;
     /// Framework module to merge an arbitray number of reco::ElectronCollection
     typedef Merger<reco::ElectronCollection, edm::CopyPolicy<reco::Electron> > ElectronMerger;
     /// Framework module to merge an arbitray number of reco::PhotonCollection
-    typedef Merger<reco::PhotonCollection,   edm::CopyPolicy<reco::Photon> >   PhotonMerger;
+    typedef Merger<reco::PhotonCollection, edm::CopyPolicy<reco::Photon> >   PhotonMerger;
+
+    /// Recover a collection of reco::Track
+    typedef CollectionRecoverer<reco::TrackCollection, edm::CopyPolicy<reco::Track> > TrackRecoverer; 
   }
 }
 
