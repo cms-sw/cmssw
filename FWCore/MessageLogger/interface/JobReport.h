5511f66090a146b31b887942d8bde0734969e76f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-05-02 02:39:06 +0000

    User interface to JobReport service

---
+++ FWCore/MessageLogger/interface/JobReport.h
@@ -0,0 +1,245 @@
+#ifndef MessageLogger_JobReport_h
+#define MessageLogger_JobReport_h
+// -*- C++ -*-
+//
+// Package:     MessageLogger
+// Class  :     JobReport
+// 
+/**\class JobReport JobReport.h FWCore/MessageLogger/interface/JobReport.h
+
+Description: A service that collections job handling information.
+
+Usage:
+The JobReport service collects 'job handling' information (currently
+file handling) from several sources, collates the information, and
+at appropriate intervales, reports the information to the job report,
+through the MessageLogger.
+
+*/
+
+//
+// Original Author:  Marc Paterno
+// $Id: JobReport.h,v 1.2 2006/04/29 21:26:45 evansde Exp $
+//
+
+#include <cstddef>
+#include <string>
+#include <ostream>
+#include <set>
+#include <vector>
+#include <sstream>
+
+
+#include "boost/scoped_ptr.hpp"
+
+#include "DataFormats/Common/interface/EventID.h"
+
+namespace edm {
+
+    class JobReport {
+    public:
+      typedef std::size_t Token;
+
+      /**\struct InputFile
+  
+      Description: Holds information about an InputFile.
+  
+      Usage: The struct InputFile is a collection of the information that
+      Data Handling wishes to accumulate about the use of a file that has
+      been opened for input.
+      */
+  
+      struct InputFile {
+        typedef std::set<edm::RunNumber_t> RunNumberCollection;
+        typedef std::vector<std::string>   StringVector;
+  
+        std::string     logicalFileName;
+        std::string     physicalFileName;
+        std::string     catalog;
+        std::string     inputSourceClassName; // class which created the file
+        std::string     moduleLabel;   // name of class instance
+        RunNumberCollection runsSeen;
+        size_t          numEventsRead;
+        StringVector    branchNames;
+        bool            fileHasBeenClosed;
+      };
+  
+      /**\struct OutputFile 
+  
+      Description: Holds information about an OutputFile.
+  
+      Usage: The struct OutputFile is a collection of the information that
+      Data Handling wishes to accumulate about the use of a file that has
+      been opened for output.
+      */
+  
+      struct OutputFile {
+        typedef InputFile::RunNumberCollection RunNumberCollection;
+        typedef InputFile::StringVector        StringVector;
+  
+        std::string     logicalFileName;
+        std::string     physicalFileName;
+        std::string     catalog;
+        std::string     outputModuleClassName;
+        std::string     moduleLabel;   // name of class instance
+        RunNumberCollection runsSeen;
+        size_t          numEventsWritten;
+        StringVector    branchNames;
+        std::vector<Token> contributingInputs;
+        bool            fileHasBeenClosed;
+      };
+  
+      struct JobReportImpl {
+        // Helper functions to be called from the xFileClosed functions,
+        // after the appropriate logging is done.
+        /*
+         * Note: We want to keep input files, so we can resolve tokens
+         * when we close output files
+         *
+         * Note2: Erasing the Output files on close invalidates the Tokens
+         * for existing output files since they are based on position in 
+         * the vector, so I have left off implementing these methods for now
+         */
+        void removeInputFileRecord_(Token t); 
+        void removeOutputFileRecord_(Token t);
+  
+        InputFile& getInputFileForToken(Token t);
+        OutputFile& getOutputFileForToken(Token t);
+        /*
+         * Add the input file token provided to every output 
+         * file currently available.
+         * Used whenever a new input file is opened, it's token
+         * is added to all open output files as a contributor
+         */
+        void insertInputForOutputs(Token t);
+  
+        /*
+         * get a vector of Tokens for all currently open
+         * input files. 
+         * Used when a new output file is opened, all currently open
+         * input file tokens are used to initialise its list of contributors
+         */
+        std::vector<Token> openInputFiles(void);
+  
+        /*
+         * Write an InputFile object to the Logger 
+         * Generate XML string for InputFile instance and dispatch to 
+         * job report via MessageLogger
+         */
+        void writeInputFile(InputFile & f);
+        /*
+         * Write an OutputFile object to the Logger 
+         * Generate an XML string for the OutputFile provided and
+         * dispatch it to the logger
+         * Contributing input tokens are resolved to the input LFN and PFN
+         * 
+         * TODO: We have not yet addressed the issue where we cleanup not
+         * contributing input files. 
+         * Also, it is possible to get fake input to output file mappings
+         * if an input file is open already when a new output file is opened
+         * but the input gets closed without contributing events to the
+         * output file due to filtering etc.
+         *
+         */
+        void writeOutputFile(OutputFile & f);
+        
+        /*
+         *  Flush all open files to logger in event of a problem.
+         */
+        void flushFiles(void);
+  
+        std::vector<InputFile> inputFiles_;
+        std::vector<OutputFile> outputFiles_;
+      };
+
+      JobReport();
+
+      ~JobReport();
+         
+      /// Report that an input file has been opened.
+      /// The returned Token should be used for later identification
+      /// of this file.
+      Token inputFileOpened(std::string const& physicalFileName,
+			    std::string const& logicalFileName,
+			    std::string const& catalog,
+			    std::string const& inputSourceClassName,
+			    std::string const& moduleLabel,
+			    std::vector<std::string> const& branchNames);
+
+      /// Report that the event with the given id has been read from
+      /// the file identified by the given Token.
+      void eventReadFromFile(Token fileToken, edm::EventID const& id);
+
+      /// Report that the input file identified by the given Token has
+      /// been closed. An exception will be thrown if the given Token
+      /// was not obtained from inputFileOpened.
+      void inputFileClosed(Token fileToken);
+
+      /// Report that an output file has been opened.
+      /// The returned Token should be used for later identification
+      /// of this file.
+      Token outputFileOpened(std::string const& physicalFileName,
+			     std::string const& logicalFileName,
+			     std::string const& catalog,
+			     std::string const& outputModuleClassName,
+			     std::string const& moduleLabel,
+			     std::vector<std::string> const& branchNames);
+
+      /// Report that the event with the given id has been written to
+      /// the file identified by the given Token.
+      void eventWrittenToFile(Token fileToken, edm::EventID const& id);
+      
+      /// Report that the output file identified by the given Token has
+      /// been closed. An exception will be thrown if the given Token
+      /// was not obtained from outputFileOpened.
+      void outputFileClosed(Token fileToken);
+
+      void reportSkippedEvent(edm::EventID const& id);
+
+
+   protected:
+      boost::scoped_ptr<JobReportImpl>& impl() {return impl_;}
+
+   private:
+      boost::scoped_ptr<JobReportImpl> impl_;
+   };
+
+   std::ostream& operator<< (std::ostream& os, JobReport::InputFile const& f);
+   std::ostream& operator<< (std::ostream& os, JobReport::OutputFile const& f);
+
+    /*
+     * Note that output formatting is spattered across these classes
+     * If something outside these classes requires access to the 
+     * same formatting then we need to refactor it into a common library
+     */
+  template <typename T>
+  std::ostream& formatFile(T f, std::ostream& os) {
+
+    if (f.fileHasBeenClosed) {
+      os << "\n<State  Value=\"closed\"/>";
+    } else {
+      os << "\n<State  Value=\"open\"/>";
+    }
+    os << "\n<LFN>" << f.logicalFileName << "</LFN>";
+    os << "\n<PFN>" << f.physicalFileName << "</PFN>";
+    os << "\n<Catalog>" << f.catalog << "</Catalog>";
+    os << "\n<ModuleLabel>" << f.moduleLabel << "</ModuleLabel>";
+    os << "\n<Runs>";
+    std::set<edm::RunNumber_t>::iterator iRun;
+    for ( iRun = f.runsSeen.begin(); iRun != f.runsSeen.end(); iRun++) {
+      os << "\n  <Run>" << *iRun << "</Run>";
+    }
+    os << "\n</Runs>";
+    os << "\n<Branches>";
+    std::vector<std::string>::iterator iBranch;
+    for (iBranch = f.branchNames.begin(); 
+        iBranch != f.branchNames.end(); 
+        iBranch++) {
+      os << "\n  <Branch>" << *iBranch << "</Branch>";
+    }
+    os << "\n</Branches>";
+    return os;
+  };
+}
+
+#endif
---
+++ FWCore/MessageLogger/src/JobReport.cc
@@ -0,0 +1,306 @@
+
+// -*- C++ -*-
+//
+// Package:     Services
+// Class  :     MessageLogger
+// 
+//
+// Original Author:  Marc Paterno
+// $Id: JobReport.cc,v 1.3 2006/04/29 21:26:45 evansde Exp $
+//
+
+
+#include "FWCore/MessageLogger/interface/JobReport.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <iterator>
+#include <ostream>
+
+using std::string;
+using std::vector;
+using std::ostream;
+      
+namespace edm
+{
+    /*
+     * Note that output formatting is spattered across these classes
+     * If something outside these classes requires access to the 
+     * same formatting then we need to refactor it into a common library
+     */
+    ostream& 
+    operator<< (ostream& os, JobReport::InputFile const& f) {
+      
+      os << "\n<InputFile>";
+      formatFile<JobReport::InputFile>(f, os);
+      os << "\n<InputSourceClass>" << f.inputSourceClassName 
+	 << "</InputSourceClass>";
+      os << "\n<EventsRead>" << f.numEventsRead << "</EventsRead>";
+      os << "\n</InputFile>";
+      return os;
+    }
+
+
+    ostream& 
+    operator<< (ostream& os, JobReport::OutputFile const& f) {
+      formatFile<JobReport::OutputFile>(f, os);           
+      os << "\n<OutputModuleClass>" 
+			<< f.outputModuleClassName 
+			<< "</OutputModuleClass>";
+      os << "\n<TotalEvents>" 
+			<< f.numEventsWritten 
+			<< "</TotalEvents>\n";
+      return os;      
+    }
+    
+    JobReport::InputFile& JobReport::JobReportImpl::getInputFileForToken(JobReport::Token t) {
+	if (t >= inputFiles_.size() ) {
+	    throw edm::Exception(edm::errors::LogicError)
+	      << "Access reported for input file with token "
+	      << t
+	      << " but no matching input file is found\n";
+	}
+
+	if (inputFiles_[t].fileHasBeenClosed) {
+	    throw edm::Exception(edm::errors::LogicError)
+	      << "Access reported for input file with token "
+	      << t
+	      << " after this file has been closed.\n"
+	      << "File record follows:\n"
+	      << inputFiles_[t]
+	      << '\n';
+	}
+	 
+      return inputFiles_[t];
+    }
+
+    JobReport::OutputFile& JobReport::JobReportImpl::getOutputFileForToken(JobReport::Token t) {
+	if (t >= outputFiles_.size() ) {
+	    throw edm::Exception(edm::errors::LogicError)
+	      << "Access reported for output file with token "
+	      << t
+	      << " but no matching output file is found\n";
+	}
+	if (outputFiles_[t].fileHasBeenClosed) {
+	    throw edm::Exception(edm::errors::LogicError)
+	      << "Access reported for output file with token "
+	      << t
+	      << " after this file has been closed.\n"
+	      << "File record follows:\n"
+	      << outputFiles_[t]
+	      << '\n';
+	}
+      return outputFiles_[t];
+    }
+    
+    /*
+     * Add the input file token provided to every output 
+     * file currently available.
+     * Used whenever a new input file is opened, it's token
+     * is added to all open output files as a contributor
+     */
+    void JobReport::JobReportImpl::insertInputForOutputs(JobReport::Token t) {
+	std::vector<JobReport::OutputFile>::iterator outFile;
+	for (outFile = outputFiles_.begin(); 
+	     outFile != outputFiles_.end();
+	     outFile++){
+	  outFile->contributingInputs.push_back(t);
+	}
+    }
+    /*
+     * get a vector of Tokens for all currently open
+     * input files. 
+     * Used when a new output file is opened, all currently open
+     * input file tokens are used to initialise its list of contributors
+     */
+    std::vector<JobReport::Token> JobReport::JobReportImpl::openInputFiles(void) {
+	std::vector<JobReport::Token> result;
+	for (unsigned int i = 0; i < inputFiles_.size(); ++i) {
+	  JobReport::InputFile inFile = inputFiles_[i];
+	  if ( inFile.fileHasBeenClosed == false){
+	    result.push_back(i);
+	  }
+	}
+	return result;
+    }
+
+    /*
+     * Write anJobReport::InputFile object to the Logger 
+     * Generate XML string forJobReport::InputFile instance and dispatch to 
+     * job report via MessageLogger
+     */
+    void JobReport::JobReportImpl::writeInputFile(JobReport::InputFile & f){
+	LogInfo("FwkJob") << f;
+    }
+    
+    /*
+     * Write an OutputFile object to the Logger 
+     * Generate an XML string for the OutputFile provided and
+     * dispatch it to the logger
+     * Contributing input tokens are resolved to the input LFN and PFN
+     * 
+     * TODO: We have not yet addressed the issue where we cleanup not
+     * contributing input files. 
+     * Also, it is possible to get fake input to output file mappings
+     * if an input file is open already when a new output file is opened
+     * but the input gets closed without contributing events to the
+     * output file due to filtering etc.
+     *
+     */
+    void JobReport::JobReportImpl::writeOutputFile(JobReport::OutputFile & f) {
+	LogInfo("FwkJob") << "\n<File>";
+	LogInfo("FwkJob") << f;
+ 	LogInfo("FwkJob") << "\n<Inputs>";
+	
+ 	std::vector<JobReport::Token>::iterator iInput;
+ 	for (iInput = f.contributingInputs.begin(); 
+ 	     iInput != f.contributingInputs.end(); iInput++) {
+ 	    JobReport::InputFile inpFile = inputFiles_[*iInput];
+ 	    LogInfo("FwkJob") <<"\n<Input>";
+ 	    LogInfo("FwkJob") <<"\n  <LFN>" << inpFile.logicalFileName << "</LFN>";
+ 	    LogInfo("FwkJob") <<"\n  <PFN>" << inpFile.physicalFileName << "</PFN>";
+ 	    LogInfo("FwkJob") <<"\n</Input>";
+ 	}
+ 	LogInfo("FwkJob") << "\n</Inputs>";
+ 	LogInfo("FwkJob") << "\n</File>";
+    }
+    
+    /*
+     *  Flush all open files to logger in event of a problem.
+     */
+    void JobReport::JobReportImpl::flushFiles(void) {
+	std::vector<JobReport::InputFile>::iterator ipos;
+	std::vector<JobReport::OutputFile>::iterator opos;
+
+	for (ipos = inputFiles_.begin(); ipos != inputFiles_.end(); ++ipos) {
+	    if (!(ipos->fileHasBeenClosed)) {
+	      writeInputFile(*ipos);
+	    }
+	}
+	for (opos = outputFiles_.begin(); opos != outputFiles_.end(); ++opos) {
+	    if (!(opos->fileHasBeenClosed)) {
+	      writeOutputFile(*opos);
+	    }
+	}
+    };
+
+    JobReport::~JobReport() {}
+
+    JobReport::JobReport() :
+      impl_(new JobReportImpl) {
+    }
+
+    JobReport::Token
+    JobReport::inputFileOpened(string const& physicalFileName,
+			       string const& logicalFileName,
+			       string const& catalog,
+			       string const& inputSourceClassName,
+			       string const& moduleLabel,
+			       vector<string> const& branchNames)
+    {
+      // Do we have to worry about thread safety here? Or is this
+      // service used in a way to make this safe?
+      impl_->inputFiles_.push_back(JobReport::InputFile());
+      JobReport::InputFile& r = impl_->inputFiles_.back();
+
+      r.logicalFileName      = logicalFileName;
+      r.physicalFileName     = physicalFileName;
+      r.catalog              = catalog;
+      r.inputSourceClassName = inputSourceClassName;
+      r.moduleLabel          = moduleLabel;
+      // r.runsSeen is not modified
+      r.numEventsRead        = 0;
+      r.branchNames          = branchNames;
+      r.fileHasBeenClosed    = false;
+    
+      JobReport::Token newToken = impl_->inputFiles_.size()-1;
+        //
+       // Add the new input file token to all output files
+      //  currently open.
+      impl_->insertInputForOutputs(newToken);
+      return newToken;
+    }
+
+    void
+    JobReport::eventReadFromFile(JobReport::Token fileToken, edm::EventID const& id)
+    {
+      JobReport::InputFile& f = impl_->getInputFileForToken(fileToken);
+      f.numEventsRead++;
+      f.runsSeen.insert(id.run());
+    }
+
+    void
+    JobReport::inputFileClosed(JobReport::Token fileToken)
+    {
+      JobReport::InputFile& f = impl_->getInputFileForToken(fileToken);
+      // Dump information to the MessageLogger's JobSummary
+      // about this file.
+      // After setting the file to 'closed', we will no longer be able
+      // to reference it by ID.
+      f.fileHasBeenClosed = true;
+      impl_->writeInputFile(f);
+    }
+
+    JobReport::Token 
+    JobReport::outputFileOpened(string const& physicalFileName,
+				string const& logicalFileName,
+				string const& catalog,
+				string const& outputModuleClassName,
+				string const& moduleLabel,
+				vector<string> const& branchNames)
+    {
+      impl_->outputFiles_.push_back(JobReport::OutputFile());
+      JobReport::OutputFile& r = impl_->outputFiles_.back();
+      
+      r.logicalFileName       = logicalFileName;
+      r.physicalFileName      = physicalFileName;
+      r.catalog               = catalog;
+      r.outputModuleClassName = outputModuleClassName;
+      r.moduleLabel           = moduleLabel;
+      // r.runsSeen is not modified
+      r.numEventsWritten      = 0;
+      r.branchNames           = branchNames;
+      r.fileHasBeenClosed     = false;
+        //
+       // Init list of contributors to list of open input file Tokens
+      //
+      r.contributingInputs = std::vector<JobReport::Token>(impl_->openInputFiles());
+      return impl_->outputFiles_.size()-1;
+    }
+
+
+    void
+    JobReport::eventWrittenToFile(JobReport::Token fileToken, edm::EventID const& id)
+    {
+      JobReport::OutputFile& f = impl_->getOutputFileForToken(fileToken);
+      f.numEventsWritten++;
+      f.runsSeen.insert(id.run());
+    }
+
+
+    void
+    JobReport::outputFileClosed(JobReport::Token fileToken)
+    {
+      JobReport::OutputFile& f = impl_->getOutputFileForToken(fileToken);
+      // Dump information to the MessageLogger's JobSummary
+      // about this file.
+      // After setting the file to 'closed', we will no longer be able
+      // to reference it by ID.
+      f.fileHasBeenClosed = true;
+      impl_->writeOutputFile(f);
+
+    }
+
+    void 
+    JobReport::reportSkippedEvent(edm::EventID const& id)
+    {
+      std::ostringstream msg;
+      msg << "<SkippedEvent Run=\"" << id.run() << "\"";
+      msg << " Event=\"" << id.event() << "\" </SkippedEvent>\n";
+      LogInfo("FwkJob") << msg.str();
+    }
+} //namspace edm
