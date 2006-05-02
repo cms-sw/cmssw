f4a2c3af70defcaf333153a20838acb39d500dc2
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-05-02 09:46:28 +0000

    removed muon candidate

--- DataFormats/RecoCandidate/src/classes.h
+++ DataFormats/RecoCandidate/src/classes.h
@@ -1,6 +1,5 @@
 #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
-#include "DataFormats/RecoCandidate/interface/RecoMuonCandidate.h"
 #include "DataFormats/RecoCandidate/interface/RecoCaloTowerCandidate.h"
 
 namespace {
