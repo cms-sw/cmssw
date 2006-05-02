c2e40da7881f306b2d2f790a62254af6e895ad94
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2006-05-02 12:38:20 +0000

    was TestClusterProducer.h

---
+++ RecoEcal/EgammaClusterProducers/interface/IslandClusterProducer.h
@@ -0,0 +1,42 @@
+#ifndef RecoEcal_EgammaClusterProducers_IslandClusterProducer_h_
+#define RecoEcal_EgammaClusterProducers_IslandClusterProducer_h_
+
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoEcal/EgammaClusterAlgos/interface/IslandClusterAlgo.h"
+
+
+//
+
+
+class IslandClusterProducer : public edm::EDProducer 
+{
+  
+  public:
+
+      IslandClusterProducer(const edm::ParameterSet& ps);
+
+      ~IslandClusterProducer();
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+
+   private:
+
+      int nMaxPrintout_; // max # of printouts
+      int nEvt_;         // internal counter of events
+ 
+      std::string clusterCollection_;
+      IslandClusterAlgo * island_p;
+
+      bool counterExceeded() const { return ((nEvt_ > nMaxPrintout_) || (nMaxPrintout_ < 0));}
+};
+
+
+#endif
