b095616c49e45c0e6702e6064a5b59c3c3207998
Andrew Warren Askew <sha1-4f9917bf818f9bc17be25445433598f3a186dfc7@cern.ch>
2006-05-03 18:15:54 +0000

    Empty ClusterShapeAlgo for development.

---
+++ RecoEcal/EgammaCoreTools/interface/ClusterShapeAlgo.h
@@ -0,0 +1,12 @@
+#ifndef EgammaCoreTools_ClusterShapeAlgo_h
+#define EgammaCoreTools_ClusterShapeAlgo_h
+
+#include "DataFormats/EgammaReco/interface/ClusterShape.h"
+
+class ClusterShapeAlgo
+{
+ public:
+  static reco::ClusterShape Calculate();
+};
+
+#endif
---
+++ RecoEcal/EgammaCoreTools/src/ClusterShapeAlgo.cc
@@ -0,0 +1,6 @@
+#include "RecoEcal/EgammaCoreTools/interface/ClusterShapeAlgo.h"
+#include "DataFormats/EgammaReco/interface/ClusterShape.h"
+
+reco::ClusterShape ClusterShapeAlgo::Calculate()
+{};
+
