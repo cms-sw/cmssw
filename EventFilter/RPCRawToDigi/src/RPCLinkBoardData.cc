fe28e351dda482a73811bd4c4b184cdc431dce5e
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-05-03 16:36:22 +0000

    implemented new constructor

--- EventFilter/RPCRawToDigi/src/RPCLinkBoardData.cc
+++ EventFilter/RPCRawToDigi/src/RPCLinkBoardData.cc
@@ -2,7 +2,7 @@
  *
  *  Implementation of RPCLinkBoardData
  *
- *  $Date: 2005/12/12 17:30:58 $
+ *  $Date: 2006/03/30 15:18:12 $
  *  $Revision: 1.1 $
  *  \author Ilaria Segoni
  */
@@ -15,10 +15,21 @@ halfP_(0),eod_(0),partitionNumber_(0),lbNumber_(0){
  	bitsOn_.clear();
 }
 
+RPCLinkBoardData::RPCLinkBoardData(std::vector<int> bits, int halfP, int eod, int partitionNumber, int lbNumber): 
+halfP_(halfP),eod_(eod),partitionNumber_(partitionNumber),lbNumber_(lbNumber){
+ 	for(std::vector<int>::iterator itr= bits.begin(); itr!= bits.end(); ++itr){
+		bitsOn_.push_back(*itr);
+	}
+}
+
 void  RPCLinkBoardData::setBits(std::vector<int> bits){bitsOn_=bits;}
+
 void  RPCLinkBoardData::setHalfP(int hp){halfP_=hp;}
+
 void  RPCLinkBoardData::setEod(int eod){eod_=eod;}
+
 void  RPCLinkBoardData::setPartitionNumber(int partNumb){partitionNumber_=partNumb;}
+
 void  RPCLinkBoardData::setLbNumber(int lbNumb){lbNumber_=lbNumb;}
 
 
