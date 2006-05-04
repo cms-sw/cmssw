c7bacd39b42e7862f15780e00d3948f2f3f275a9
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-05-04 18:47:47 +0000

    add GenericTransientTrackingRecHitBuilderESProducer

--- TrackingTools/TransientTrackingRecHit/src/module.cc
+++ TrackingTools/TransientTrackingRecHit/src/module.cc
@@ -1,4 +1,5 @@
 #include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
+#include "TrackingTools/TransientTrackingRecHit/interface/GenericTransientTrackingRecHitBuilderESProducer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -7,4 +8,6 @@
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 EVENTSETUP_DATA_REG(TransientTrackingRecHitBuilder);
+DEFINE_FWK_EVENTSETUP_MODULE(GenericTransientTrackingRecHitBuilderESProducer)
+
 
