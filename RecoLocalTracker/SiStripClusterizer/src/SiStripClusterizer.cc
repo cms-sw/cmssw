bd0fa30bf14b86a075155a2e83d3812e5ce8e388
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-05-04 22:30:38 +0000

    inserted VPSet

--- RecoLocalTracker/SiStripClusterizer/src/SiStripClusterizer.cc
+++ RecoLocalTracker/SiStripClusterizer/src/SiStripClusterizer.cc
@@ -39,45 +39,30 @@ namespace cms
   void SiStripClusterizer::produce(edm::Event& e, const edm::EventSetup& es)
   {
     // retrieve producer name of input StripDigiCollection
-    std::string digiProducer = conf_.getParameter<std::string>("DigiProducer");
+    //    std::string digiProducer = conf_.getParameter<std::string>("DigiProducer");
 
     // Step A: create empty output collection
     std::auto_ptr< edm::DetSetVector<SiStripCluster> > output(new edm::DetSetVector<SiStripCluster> );
     std::auto_ptr<SiStripClusterCollection> output_old(new SiStripClusterCollection);//@@ To assure backward compatibility
 
-
-
-    // Step B: Get Inputs 
-    edm::Handle< edm::DetSetVector<SiStripDigi> >    zsDigis;
-    edm::Handle< edm::DetSetVector<SiStripDigi> >    vrDigis;
-    edm::Handle< edm::DetSetVector<SiStripDigi> >    prDigis;
-    edm::Handle< edm::DetSetVector<SiStripDigi> >    smDigis;
-
-    if (digiProducer=="stripdigi"){
-      e.getByLabel(digiProducer,"stripdigi",zsDigis);  //FIXME: fix this label
-    }
-    else{
-      digiProducer="RawToDigi"; //FIXME: fix this label
-      e.getByLabel(digiProducer,"ZeroSuppressed",zsDigis);  //FIXME: fix this label
-      digiProducer="theSiStripZeroSuppression"; //FIXME: fix this label
-      e.getByLabel(digiProducer,"fromVirginRaw"   ,vrDigis);
-      e.getByLabel(digiProducer,"fromProcessedRaw",prDigis);
-      e.getByLabel(digiProducer,"fromScopeMode"   ,smDigis);
-    }
-    
-    // Step C: Get ESObject 
+    // Step B: Get ESObject 
     SiStripNoiseService_.setESObjects(es);
 
-    // Step C: Invoke the strip clusterizer algorithm
-    if (zsDigis->size())
-      SiStripClusterizerAlgorithm_.run(*zsDigis,*output);
-    if (vrDigis->size())
-      SiStripClusterizerAlgorithm_.run(*vrDigis,*output);
-    if (prDigis->size())
-      SiStripClusterizerAlgorithm_.run(*prDigis,*output);
-    if (smDigis->size())
-      SiStripClusterizerAlgorithm_.run(*smDigis,*output);
+    // Step C: Get Inputs 
+    edm::Handle< edm::DetSetVector<SiStripDigi> >  input;
+
+    typedef std::vector<edm::ParameterSet> Parameters;
+    Parameters DigiProducersList = conf_.getParameter<Parameters>("DigiProducersList");
+    Parameters::iterator itDigiProducersList = DigiProducersList.begin();
+    for(; itDigiProducersList != DigiProducersList.end(); ++itDigiProducersList ) {
+      std::string digiProducer = itDigiProducersList->getParameter<std::string>("DigiProducer");
+      std::string digiLabel = itDigiProducersList->getParameter<std::string>("DigiLabel");
+      e.getByLabel(digiProducer,digiLabel,input);  //FIXME: fix this label	
+      if (input->size())
+	SiStripClusterizerAlgorithm_.run(*input,*output);
+    }
     
+
     // Step D: write output to file
     if ( output->size() )
       {
