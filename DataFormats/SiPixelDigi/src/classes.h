80331df044740d5391bb8e1fce57f5ced915fdf9
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2006-05-05 07:39:33 +0000

    Data container moved to DetSetVector

--- DataFormats/SiPixelDigi/src/classes.h
+++ DataFormats/SiPixelDigi/src/classes.h
@@ -1,13 +1,18 @@
 #ifndef SIPIXELDIGI_CLASSES_H
 #define SIPIXELDIGI_CLASSES_H
 
-#include "DataFormats/SiPixelDigi/interface/PixelDigiCollection.h"
+#include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
 #include <vector>
 
 namespace {
   namespace {
-    edm::Wrapper<PixelDigiCollection> digiCollectionWrapper;
+    edm::Wrapper<PixelDigi> zs0;
+    edm::Wrapper< std::vector<PixelDigi>  > zs1;
+    edm::Wrapper< edm::DetSet<PixelDigi> > zs2;
+    edm::Wrapper< std::vector<edm::DetSet<PixelDigi> > > zs3;
+    edm::Wrapper< edm::DetSetVector<PixelDigi> > zs4;
   }
 }
 
