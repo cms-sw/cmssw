e332f288b17172f3f885a01e03265ccdd1969dc5
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-05-07 20:42:33 +0000

    test module TestCluster

---
+++ RecoLocalTracker/SiStripClusterizer/test/SealModules.cc
@@ -0,0 +1,12 @@
+
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoLocalTracker/SiStripClusterizer/test/TestCluster.h"
+
+using cms::TestCluster;
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(TestCluster);
+
