a7f281f5ce795a42dcff69965ee3070d4e5edbd1
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-05-08 07:58:00 +0000

    print nothing if silent

--- DataFormats/TrackReco/src/print.cc
+++ DataFormats/TrackReco/src/print.cc
@@ -7,13 +7,15 @@ using namespace std;
 namespace reco {
   string print( const Track & trk, edm::Verbosity v ) {
     ostringstream out;
-    out << "track parameters" << endl;
-    out << "(Tcurv, theta, phi, d0, d_z, pt) = ";
-    for (int i = 0; i < 5; i++) {
-      out.precision(6); out.width(13); out<<trk.parameter(i);
+    if ( v > edm::Silent ) {
+      out << "track parameters" << endl;
+      out << "(Tcurv, theta, phi, d0, d_z, pt) = ";
+      for (int i = 0; i < 5; i++) {
+	out.precision(6); out.width(13); out<<trk.parameter(i);
+      }
+      out.precision(6); out.width(13); out << trk.pt();
+      out << endl;
     }
-    out.precision(6); out.width(13); out << trk.pt();
-    out << endl;
     if ( v >= edm::Detailed ) {
       out << "covariance" << endl;
       for ( int i = 0; i < 5; i++ ) {
