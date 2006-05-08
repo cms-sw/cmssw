e18948edb0539e279347658462a0e632a17dfd30
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-05-08 14:39:07 +0000

    file location changed

--- TrackingTools/TransientTrackingRecHit/src/module.cc
+++ TrackingTools/TransientTrackingRecHit/src/module.cc
@@ -1,5 +1,5 @@
 #include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
-#include "TrackingTools/TransientTrackingRecHit/interface/GenericTransientTrackingRecHitBuilderESProducer.h"
+#include "TrackingTools/TransientTrackingRecHit/src/GenericTransientTrackingRecHitBuilderESProducer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
