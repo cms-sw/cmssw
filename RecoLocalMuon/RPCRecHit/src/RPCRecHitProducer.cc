8801874238c6c530380ecc4e4e80f4d7ee810843
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-05-08 11:21:22 +0000

    first release

---
+++ RecoLocalMuon/RPCRecHit/src/RPCRecHitProducer.cc
@@ -0,0 +1,92 @@
+/** \file
+ *
+ *  $Date: 2006/05/08 01:00:15 $
+ *  $Revision: 1.1 $
+ *  \author M. Maggi -- INFN Bari
+*/
+
+#include "RPCRecHitProducer.h"
+
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "DataFormats/RPCDigi/interface/RPCDigiCollection.h"
+
+#include "Geometry/RPCGeometry/interface/RPCRoll.h"
+#include "Geometry/RPCGeometry/interface/RPCGeometry.h"
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+#include "DataFormats/RPCRecHit/interface/RPCRecHit.h"
+
+#include "RecoLocalMuon/RPCRecHit/interface/RPCRecHitBaseAlgo.h"
+#include "RecoLocalMuon/RPCRecHit/interface/RPCRecHitAlgoFactory.h"
+#include "DataFormats/RPCRecHit/interface/RPCRecHitCollection.h"
+#include <string>
+
+
+using namespace edm;
+using namespace std;
+
+
+
+
+RPCRecHitProducer::RPCRecHitProducer(const ParameterSet& config){
+  // Set verbose output
+
+  produces<RPCRecHitCollection>("RPCRecHits");
+
+  theRPCDigiLabel = config.getParameter<string>("rpcDigiLabel");
+  
+  // Get the concrete reconstruction algo from the factory
+  string theAlgoName = config.getParameter<string>("recAlgo");
+  theAlgo = RPCRecHitAlgoFactory::get()->create(theAlgoName,
+					       config.getParameter<ParameterSet>("recAlgoConfig"));
+}
+
+RPCRecHitProducer::~RPCRecHitProducer(){
+}
+
+
+
+void RPCRecHitProducer::produce(Event& event, const EventSetup& setup) {
+  // Get the RPC Geometry
+  ESHandle<RPCGeometry> rpcGeom;
+  setup.get<MuonGeometryRecord>().get(rpcGeom);
+
+  // Get the digis from the event
+  Handle<RPCDigiCollection> digis; 
+  event.getByLabel(theRPCDigiLabel, digis);
+
+  // Pass the EventSetup to the algo
+  theAlgo->setES(setup);
+
+  // Create the pointer to the collection which will store the rechits
+  auto_ptr<RPCRecHitCollection> recHitCollection(new RPCRecHitCollection());
+
+
+  // Iterate through all digi collections ordered by LayerId   
+  RPCDigiCollection::DigiRangeIterator rpcdgIt;
+  for (rpcdgIt = digis->begin(); rpcdgIt != digis->end();
+       ++rpcdgIt){
+    // The layerId
+    const RPCDetId& rpcId = (*rpcdgIt).first;
+    // Get the GeomDet from the setup
+    const RPCRoll* roll = rpcGeom->roll(rpcId);
+
+    // Get the iterators over the digis associated with this LayerId
+    const RPCDigiCollection::Range& range = (*rpcdgIt).second;
+    
+    OwnVector<RPCRecHit> recHits =
+      theAlgo->reconstruct(*roll, rpcId, range);
+    
+    if(recHits.size() > 0) //FIXME: is it really needed?
+      recHitCollection->put(rpcId, recHits.begin(), recHits.end());
+  }
+
+  event.put(recHitCollection, "RPCRecHits");
+}
+
+
+
---
+++ RecoLocalMuon/RPCRecHit/src/SealModule.cc
@@ -0,0 +1,12 @@
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoLocalMuon/RPCRecHit/src/RPCRecHitProducer.h"
+
+#include "RecoLocalMuon/RPCRecHit/interface/RPCRecHitAlgoFactory.h"
+#include "RecoLocalMuon/RPCRecHit/src/RPCRecHitStandardAlgo.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(RPCRecHitProducer);
+DEFINE_SEAL_PLUGIN (RPCRecHitAlgoFactory, RPCRecHitStandardAlgo, "RPCRecHitStandardAlgo");
