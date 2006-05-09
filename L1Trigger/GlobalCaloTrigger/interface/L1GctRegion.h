a48ac15ebf44610cc33397755675d8ef107d6682
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-05-09 11:22:43 +0000

    Modified Eta/Phi region numbering functionality

--- L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
@@ -3,55 +3,55 @@
 
 #include <bitset>
 
-using namespace std;
-
 /*
  * A calorimeter trigger region
  * as represented in the GCT
- * author: Jim Brooke
+ * author: Jim Brooke, Robert Frazier
  * date: 20/2/2006
  * 
  */
-
 
 typedef unsigned long int ULong;
 
 class L1GctRegion
 {
 public:
-	L1GctRegion(pair<int, int> loc, ULong et=0, bool mip=false, bool quiet=false, bool tauVeto=true, bool overFlow=false);
+	L1GctRegion(int eta=0, int phi=0, ULong et=0, bool mip=false, bool quiet=false, bool tauVeto=true, bool overFlow=false);
 	~L1GctRegion();
     
     ///External access to the bitwidth of the region Et
     static const int ET_BITWIDTH = 10;
 	
     // Getters
-	int phi() const { return m_loc.first; }
-	int eta() const { return m_loc.second; }
-	ULong getEt() const { return myEt.to_ulong(); }
-	bool getMip() const { return myMip; }
-	bool getQuiet() const { return myQuiet; }
+    int getEta() const { return m_eta; }   ///< Get the eta number (0-21?) of the region
+    int getPhi() const { return m_phi; }   ///< Get the phi number (0-17) of the region
+    ULong getEt() const { return myEt.to_ulong(); }
+    bool getMip() const { return myMip; }
+    bool getQuiet() const { return myQuiet; }
     bool getTauVeto() const { return myTauVeto; }
     bool getOverFlow() const { return myOverFlow; }
     
     // Setters
+    void setEta(int eta) { m_eta = eta; }
+    void setPhi(int phi) { m_phi = phi; }
     void setEt(ULong et) { /*assert(et < (1 << ET_BITWIDTH));*/ myEt = et; } 
     void setMip(bool mip) { myMip = mip; }
     void setQuiet(bool quiet) { myQuiet = quiet; }
     void setTauVeto(bool tauVeto) { myTauVeto = tauVeto; }
     void setOverFlow(bool overFlow) { myOverFlow = overFlow; }
 
-    //	ostream& operator << (ostream& os, const L1GctRegion& s);
+    //  ostream& operator << (ostream& os, const L1GctRegion& s);
     
 
 		
 private:
 
-    ///
-    /// position
-	pair<int,int> m_loc;
+    /// global eta position number of the region (0-21)
+	int m_eta;
+	/// global phi position number of the region (0-17)
+    int m_phi;
 
-	bitset<ET_BITWIDTH> myEt;
+	std::bitset<ET_BITWIDTH> myEt;
 	bool myMip;
 	bool myQuiet;
     bool myTauVeto;
--- L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
@@ -1,12 +1,16 @@
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h"
 
+using namespace std;
 
-L1GctRegion::L1GctRegion(ULong et, bool mip, bool quiet, bool tauVeto, bool overFlow):
-myEt(et),
-myMip(mip),
-myQuiet(quiet),
-myTauVeto(tauVeto),
-myOverFlow(overFlow)
+
+L1GctRegion::L1GctRegion(int eta, int phi, ULong et, bool mip, bool quiet, bool tauVeto, bool overFlow):
+    m_eta(eta),
+    m_phi(phi),
+    myEt(et),
+    myMip(mip),
+    myQuiet(quiet),
+    myTauVeto(tauVeto),
+    myOverFlow(overFlow)
 {
 }
 
