5d0ba15ec350aab6bc6d922408fb6035f5b7480e
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-05-10 16:25:40 +0000

    add a simple test to show the selected CPE

--- RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
+++ RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
@@ -5,9 +5,11 @@
 
 #include "RecoLocalTracker/SiPixelRecHits/test/ReadPixelRecHit.h"
 #include "RecoLocalTracker/SiPixelRecHits/test/PixelNtuplizer.h"
+#include "RecoLocalTracker/SiPixelRecHits/test/CPEAccessTester.cc"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ReadPixelRecHit)
 DEFINE_ANOTHER_FWK_MODULE(PixelNtuplizer)
 
+DEFINE_ANOTHER_FWK_MODULE(CPEAccessTester)
