fc15afdc681628b44a57bb530dd9167e420157b5
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-05-11 21:34:11 +0000

    un-hard-code user

--- CondTools/IntegrationTest/monitoring/poll_db.pl
+++ CondTools/IntegrationTest/monitoring/poll_db.pl
@@ -3,11 +3,13 @@
 use warnings;
 use strict;
 
-my $orcon_pass = "****";
-my $orcoff_pass = "****";
+my $orcon_user = ""
+my $orcon_pass = "__CHANGE_ME__";
+my $orcoff_user = "";
+my $orcoff_pass = "__CHANGE_ME__";
 
-my $conn_orcon = "sqlplus -S cms_cond_ecal/${orcon_pass}\@orcon";
-my $conn_orcoff = "sqlplus -S cms_cond_ecal/${orcoff_pass}\@cms_orcoff";
+my $conn_orcon = "sqlplus -S ${orcon_user}/${orcon_pass}\@orcon";
+my $conn_orcoff = "sqlplus -S ${orcoff_user}/${orcoff_pass}\@cms_orcoff";
 
 my $cmd_orcon = "cat cmds.sql | ${conn_orcon} >> orcon_poll.txt";
 my $cmd_orcoff = "cat cmds.sql | ${conn_orcoff} >> orcoff_poll.txt";
