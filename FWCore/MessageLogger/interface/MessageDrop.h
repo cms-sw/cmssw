7fd745f4dde6e7fd5ea58c3ab0392c30bdaad3d5
Mark Steven Fischler <sha1-4e5f5390fe08571bcaa72b9888ceb66263060cc9@cern.ch>
2006-05-12 20:49:20 +0000

    Repair a deteced unitialized variable (debugEnabled in MessageDrop). Add a space between rnu/event and file/line in debug case.

--- FWCore/MessageLogger/interface/MessageDrop.h
+++ FWCore/MessageLogger/interface/MessageDrop.h
@@ -18,12 +18,18 @@
 //
 // Original Author:  M. Fischler and Jim Kowalkowski
 //         Created:  Tues Feb 14 16:38:19 CST 2006
-// $Id: MessageDrop.h,v 1.2 2006/02/17 22:13:45 fischler Exp $
+// $Id: MessageDrop.h,v 1.3 2006/02/17 22:52:57 wmtan Exp $
 //
 
 // system include files
 
 #include <string>
+
+// Change log
+//
+//  1  mf 5/12/06	initialize debugEnabled to true, to avoid unitialized
+//			data detection in memory checks (and to be safe in
+//			getting enabled output independant of timings) 
 
 
 // user include files
@@ -32,7 +38,11 @@ namespace edm {
 
 struct MessageDrop {
 private:
-  MessageDrop() :  moduleName (""), runEvent("pre-events") { } 
+  MessageDrop() 
+  :  moduleName ("")
+  , runEvent("pre-events")
+  , debugEnabled(true) 					// change log 1
+  {  } 
 public:
   static MessageDrop * instance ();
   std::string moduleName;
