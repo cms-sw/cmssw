708500ab2b30372856edf1628b49aa13e6664a6b
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-05-12 09:59:25 +0000

    fixed overlap method

--- DataFormats/Candidate/src/LeafCandidate.cc
+++ DataFormats/Candidate/src/LeafCandidate.cc
@@ -1,4 +1,4 @@
-// $Id: LeafCandidate.cc,v 1.9 2006/02/21 10:37:32 llista Exp $
+// $Id: LeafCandidate.cc,v 1.1 2006/02/28 10:43:30 llista Exp $
 #include "DataFormats/Candidate/interface/LeafCandidate.h"
 #include "FWCore/Utilities/interface/EDMException.h"
 
@@ -20,7 +20,9 @@ Candidate::iterator LeafCandidate::end() { return iterator( new iterator_leaf );
 
 int LeafCandidate::numberOfDaughters() const { return 0; }
 
-bool LeafCandidate::overlap( const Candidate & ) const { return false; }
+bool LeafCandidate::overlap( const Candidate & o ) const { 
+  return  p4() == o.p4()&&   vertex() == o.vertex() && charge() == o.charge();
+}
 
 const Candidate & LeafCandidate::daughter( size_type ) const {
   throw edm::Exception( edm::errors::LogicError, "Can't access daughters on a leaf Candidate" );
