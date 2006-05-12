1c78915e876d7e1135817f06cbdf4807b684bad5
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-05-12 10:36:30 +0000

    added record for DetCabling

---
+++ CalibTracker/Records/src/SiStripDetCablingRcd.cc
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+//
+// Package:     Records
+// Class  :     SiStripDetCablingRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      dkcira
+
+#include "CalibTracker/Records/interface/SiStripDetCablingRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiStripDetCablingRcd);
