d89d8415d1658243ee60e31d13c1638f2f77029e
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-05-12 10:36:10 +0000

    added record for FecCabling

---
+++ CalibTracker/Records/src/SiStripFecCablingRcd.cc
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+//
+// Package:     Records
+// Class  :     SiStripFecCablingRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      dkcira
+
+#include "CalibTracker/Records/interface/SiStripFecCablingRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiStripFecCablingRcd);
