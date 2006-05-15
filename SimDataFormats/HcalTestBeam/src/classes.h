32b7dbc474c8b6f49655584dfb5984f5d3af0acb
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2006-05-15 10:18:48 +0000

    First version of Hcal Test Beam 2004 Simulation Data Format

---
+++ SimDataFormats/HcalTestBeam/src/classes.h
@@ -0,0 +1,12 @@
+#include "SimDataFormats/HcalTestBeam/interface/PHcalTB04Info.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    std::vector<int>            dumI;
+    std::vector<float>          dumF;
+    PHcalTB04Info               theInfo;
+    edm::Wrapper<PHcalTB04Info> theTB04Info;
+  }
+}
