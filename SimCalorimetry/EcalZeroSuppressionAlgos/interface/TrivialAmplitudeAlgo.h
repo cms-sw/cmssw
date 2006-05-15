0f53612e954ed719fc5ca1ef01608258364e8129
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-05-15 09:11:00 +0000

    First version, zero suppression for a digi and simple energy calculation

---
+++ SimCalorimetry/EcalZeroSuppressionAlgos/interface/TrivialAmplitudeAlgo.h
@@ -0,0 +1,50 @@
+#ifndef EcalZeroSuppressionAlgos_TrivialAmplitudeAlgo_h
+#define EcalZeroSuppressionAlgos_TrivialAmplitudeAlgo_h
+
+/*
+ * \file TrivialAmplitudeAlgo.h
+ *
+ * $Date: 2006/04/10 08:50:50 $
+ * $Revision: 1.1 $
+ * \author F. Cossutti
+ *
+ * simple weights algorithm for energy and pedesta reconstruction
+ *
+*/
+
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
+
+#include <vector>
+
+using namespace std;
+
+template<class C> class TrivialAmplitudeAlgo 
+{
+public:
+
+  explicit TrivialAmplitudeAlgo();
+  virtual ~TrivialAmplitudeAlgo(){};
+
+  double energy(const C& frame);
+
+  double pedestal(const C& frame);
+
+ private:
+
+  std::vector<double> theWeights;
+  std::vector<double> theGainFactors;;
+
+};
+
+#include "TrivialAmplitudeAlgo.icc"
+#endif
