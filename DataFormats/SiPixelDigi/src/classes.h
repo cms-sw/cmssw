44ae95e120294b6e8555a3212c5d63432249b8fd
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-05-15 13:05:06 +0000

    added missing dictionary

--- DataFormats/SiPixelDigi/src/classes.h
+++ DataFormats/SiPixelDigi/src/classes.h
@@ -2,6 +2,7 @@
 #define SIPIXELDIGI_CLASSES_H
 
 #include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
+#include "DataFormats/SiPixelDigi/interface/PixelDigiCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include <vector>
@@ -9,6 +10,7 @@
 namespace {
   namespace {
     edm::Wrapper<PixelDigi> zs0;
+    edm::Wrapper<PixelDigiCollection> zsc0;
     edm::Wrapper< std::vector<PixelDigi>  > zs1;
     edm::Wrapper< edm::DetSet<PixelDigi> > zs2;
     edm::Wrapper< std::vector<edm::DetSet<PixelDigi> > > zs3;
