46f19bd55aca163bb3b3c2adcc6603d4ebf2984c
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-05-15 14:42:41 +0000

    complete chain, with dummy TrackingParticle

--- SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
+++ SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
@@ -1,1 +1,5 @@
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+
+TrackingParticle::TrackingParticle(  const HepMC::GenParticle * p ) :
+  genParticle_( p ) {
+}
---
+++ SimDataFormats/TrackingAnalysis/src/classes.h
@@ -0,0 +1,12 @@
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
+namespace {
+  namespace {
+    TrackingParticleCollection c1;
+    edm::Wrapper<TrackingParticleCollection> w1; 
+    TrackingParticleRef r1;
+    TrackingParticleRefVector rv1;
+    TrackingParticleRefProd rp1;
+  }
+}
