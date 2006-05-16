9f06fd5f344b97f451a878e103926c903bf94867
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2006-05-16 10:33:53 +0000

    resynch to table at http://cmsdoc.cern.ch/cms/TRIDAS/horizontal/RUWG/DAQ_IF_guide/DAQ_IF_guide.html and simplified range checking

--- DataFormats/FEDRawData/interface/FEDNumbering.h
+++ DataFormats/FEDRawData/interface/FEDNumbering.h
@@ -7,13 +7,13 @@
  *  No two feds should have the same id. Each subdetector has a reserved range.
  *  Gaps between ranges give flexibility to the numbering.
  *
- *  $Date: 2005/10/05 16:20:20 $
- *  $Revision: 1.3 $
+ *  $Log
+ *
  *  \author G. Bruno - CERN, EP Division
  */   
 
-
-#include <algorithm>
+#include <vector>
+#include <string>
 
 class FEDNumbering {
 
@@ -25,23 +25,38 @@ class FEDNumbering {
   static std::pair<int,int> getSiPixelFEDIds();
   static std::pair<int,int> getSiStripFEDIds();
 
-  static std::pair<int,int> getDTFEDIds();
-  static std::pair<int,int> getCSCFEDIds();
-  static std::pair<int,int> getRPCFEDIds();
-
   static std::pair<int,int> getPreShowerFEDIds();
-
   static std::pair<int,int> getEcalFEDIds();
-
   static std::pair<int,int> getHcalFEDIds();
 
-  static std::pair<int,int> getTriggerFEDIds();
+  static std::pair<int,int> getCSCFEDIds();
+  static std::pair<int,int> getCSCTFFEDIds();
+
+  static std::pair<int,int> getDTFEDIds();
+  static std::pair<int,int> getDTTFFEDIds();
+
+  static std::pair<int,int> getRPCFEDIds();
+
+  static std::pair<int,int> getTriggerGTPFEDIds();
+  static std::pair<int,int> getTriggerEGTPFEDIds();
+  
+  static std::pair<int,int> getTriggerLTCmtccFEDIds();
+  static std::pair<int,int> getTriggerLTCFEDIds();
 
   static int lastFEDId();
 
+  static void init();
+
+  static bool inRange(int);
+
+  static const std::string &fromDet(int);
+
  private:
+  // MAXFEDID intended to be "last valid" id: [0,MAXFEDID] is the valid range
+  // all other MAX and MIN are ALSO including the boundaries
   static const int MAXFEDID;
 
+  // e.g. [MINSiPixelFEDID, MAXSiPixelFEDID] are all valid indices
   static const int MINSiPixelFEDID;
   static const int MAXSiPixelFEDID;
 
@@ -50,10 +65,14 @@ class FEDNumbering {
 
 
   static const int MINCSCFEDID;
-  static const int MAXCSCFEDID;  
+  static const int MAXCSCFEDID;
+  static const int MINCSCTFFEDID;
+  static const int MAXCSCTFFEDID;
   
   static const int MINDTFEDID;
   static const int MAXDTFEDID;
+  static const int MINDTTFFEDID;
+  static const int MAXDTTFFEDID;
   
   static const int MINRPCFEDID;
   static const int MAXRPCFEDID;
@@ -69,10 +88,18 @@ class FEDNumbering {
   static const int MAXHCALFEDID;
 
   
-  static const int MINTriggerFEDID;
-  static const int MAXTriggerFEDID;
+  static const int MINTriggerGTPFEDID;
+  static const int MAXTriggerGTPFEDID;
+  static const int MINTriggerEGTPFEDID;
+  static const int MAXTriggerEGTPFEDID;
+  static const int MINTriggerLTCFEDID;
+  static const int MAXTriggerLTCFEDID;
+  static const int MINTriggerLTCmtccFEDID;
+  static const int MAXTriggerLTCmtccFEDID;
 
-
+  static std::vector<std::string> from_;
+  static bool *in_;
+  static bool init_;
 };
 
 #endif // FEDNumbering_H
--- DataFormats/FEDRawData/src/FEDNumbering.cc
+++ DataFormats/FEDRawData/src/FEDNumbering.cc
@@ -1,46 +1,61 @@
 /** \file
  *
- *  $Date: 2005/10/06 18:25:22 $
- *  $Revision: 1.4 $
+ *  $Date: 2005/10/18 13:29:04 $
+ *  $Revision: 1.5 $
  *  \author G. Bruno  - CERN, EP Division
  */
 #include "DataFormats/FEDRawData/interface/FEDNumbering.h"
 
+
+const int FEDNumbering::MAXFEDID = 1023; // 10 bits
+
+const int FEDNumbering::MINSiPixelFEDID = 0;
+const int FEDNumbering::MAXSiPixelFEDID = 37;
+
+const int FEDNumbering::MINSiStripFEDID = 50;
+const int FEDNumbering::MAXSiStripFEDID = 489;
+
+
+const int FEDNumbering::MINCSCFEDID = 750;
+const int FEDNumbering::MAXCSCFEDID = 757;  
+const int FEDNumbering::MINCSCTFFEDID = 760;
+const int FEDNumbering::MAXCSCTFFEDID = 760;  
+  
+const int FEDNumbering::MINDTFEDID = 770;
+const int FEDNumbering::MAXDTFEDID = 775;
+const int FEDNumbering::MINDTTFFEDID = 780;
+const int FEDNumbering::MAXDTTFFEDID = 780;
+  
+const int FEDNumbering::MINRPCFEDID = 790;
+const int FEDNumbering::MAXRPCFEDID = 795;
+
+
+const int FEDNumbering::MINPreShowerFEDID = 550;
+const int FEDNumbering::MAXPreShowerFEDID = 596;
+
+const int FEDNumbering::MINECALFEDID = 600;
+const int FEDNumbering::MAXECALFEDID = 670;
+  
+const int FEDNumbering::MINHCALFEDID = 700;
+const int FEDNumbering::MAXHCALFEDID = 731;
+
+  
+const int FEDNumbering::MINTriggerGTPFEDID = 813;
+const int FEDNumbering::MAXTriggerGTPFEDID = 813;
+const int FEDNumbering::MINTriggerEGTPFEDID = 814;
+const int FEDNumbering::MAXTriggerEGTPFEDID = 814;
+const int FEDNumbering::MINTriggerLTCFEDID = 816;
+const int FEDNumbering::MAXTriggerLTCFEDID = 823;
+const int FEDNumbering::MINTriggerLTCmtccFEDID = 815;
+const int FEDNumbering::MAXTriggerLTCmtccFEDID = 815;
+
 using namespace std;
 
-const int FEDNumbering::MAXFEDID=2047;
 
+bool FEDNumbering::init_ = true;
+bool *FEDNumbering::in_ = new bool[1024];
 
-const int FEDNumbering::MINSiPixelFEDID=0;
-const int FEDNumbering::MAXSiPixelFEDID=37;
-
-const int FEDNumbering::MINSiStripFEDID=50;
-const int FEDNumbering::MAXSiStripFEDID=489;
-
-
-const int FEDNumbering::MINCSCFEDID=750;
-const int FEDNumbering::MAXCSCFEDID=757;
-
-const int FEDNumbering::MINDTFEDID=770;
-const int FEDNumbering::MAXDTFEDID=775;
-
-const int FEDNumbering::MINRPCFEDID=790;
-const int FEDNumbering::MAXRPCFEDID=795;
-
-
-const int FEDNumbering::MINPreShowerFEDID=550;
-const int FEDNumbering::MAXPreShowerFEDID=596;
-
-const int FEDNumbering::MINECALFEDID=600;
-const int FEDNumbering::MAXECALFEDID=670;
-
-const int FEDNumbering::MINHCALFEDID=700;
-const int FEDNumbering::MAXHCALFEDID=731;
-
-
-const int FEDNumbering::MINTriggerFEDID=813;
-const int FEDNumbering::MAXTriggerFEDID=815;
-
+vector<string> FEDNumbering::from_(1024,"");
 
 pair<int,int> FEDNumbering::getSiStripFEDIds(){
 
@@ -62,10 +77,22 @@ pair<int,int> FEDNumbering::getDTFEDIds(){
 
 }
 
+pair<int,int> FEDNumbering::getDTTFFEDIds(){
+
+  return pair<int,int> (MINDTTFFEDID, MAXDTTFFEDID);
+
+}
+
+
 
 pair<int,int> FEDNumbering::getCSCFEDIds(){
 
   return pair<int,int> (MINCSCFEDID, MAXCSCFEDID);
+
+}
+pair<int,int> FEDNumbering::getCSCTFFEDIds(){
+
+  return pair<int,int> (MINCSCTFFEDID, MAXCSCTFFEDID);
 
 }
 
@@ -97,9 +124,27 @@ pair<int,int> FEDNumbering::getHcalFEDIds(){
 
 }
 
-pair<int,int> FEDNumbering::getTriggerFEDIds(){
+pair<int,int> FEDNumbering::getTriggerGTPFEDIds(){
 
-  return pair<int,int> (MINTriggerFEDID, MAXTriggerFEDID);
+  return pair<int,int> (MINTriggerGTPFEDID, MAXTriggerGTPFEDID);
+
+}
+
+pair<int,int> FEDNumbering::getTriggerEGTPFEDIds(){
+
+  return pair<int,int> (MINTriggerEGTPFEDID, MAXTriggerEGTPFEDID);
+
+}
+
+pair<int,int> FEDNumbering::getTriggerLTCmtccFEDIds(){
+
+  return pair<int,int> (MINTriggerLTCmtccFEDID, MAXTriggerLTCmtccFEDID);
+
+
+}
+pair<int,int> FEDNumbering::getTriggerLTCFEDIds(){
+
+  return pair<int,int> (MINTriggerLTCFEDID, MAXTriggerLTCFEDID);
 
 }
 
@@ -107,3 +152,92 @@ int FEDNumbering::lastFEDId(){
   return MAXFEDID;
 }
 
+void FEDNumbering::init()
+{
+  int i = 0;
+  for(i=0; i< lastFEDId(); i++)
+    in_[i] = false;
+  for(i=getSiPixelFEDIds().first; i<=getSiPixelFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "SiPixel";
+    }
+  for(i=getSiStripFEDIds().first; i<=getSiStripFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "SiStrip";
+    }
+  for(i=getPreShowerFEDIds().first; i<=getPreShowerFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "PreShower";
+    }
+  for(i=getEcalFEDIds().first; i<=getEcalFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "Ecal";
+    }
+  for(i=getHcalFEDIds().first; i<=getHcalFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "Hcal";
+    }
+  for(i=getCSCFEDIds().first; i<=getCSCFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "CSC";
+    }
+  for(i=getCSCTFFEDIds().first; i<=getCSCTFFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "CSCTF";
+    }
+  for(i=getDTFEDIds().first; i<=getDTFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "DT";
+    }
+  for(i=getDTTFFEDIds().first; i<=getDTTFFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "DTTF";
+    }
+  for(i=getRPCFEDIds().first; i<=getRPCFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "RPC";
+    }
+  for(i=getTriggerGTPFEDIds().first; i<=getTriggerGTPFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "TriggerGTP";
+    }
+  for(i=getTriggerEGTPFEDIds().first; i<=getTriggerEGTPFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "TriggerEGTP";
+    }
+  for(i=getTriggerLTCFEDIds().first; i<=getTriggerLTCFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "TriggerLTC";
+    }
+  for(i=getTriggerLTCmtccFEDIds().first; i<=getTriggerLTCmtccFEDIds().second; i++)
+    {
+      in_[i] = true;
+      from_[i] = "TriggerLTCmtcc";
+    }
+  init_ = false;
+}
+
+bool FEDNumbering::inRange(int i) 
+{
+  if(init_) init();
+  return in_[i];
+}
+
+string const &FEDNumbering::fromDet(int i) 
+{
+  if(init_) init();
+  return from_[i];
+}  
