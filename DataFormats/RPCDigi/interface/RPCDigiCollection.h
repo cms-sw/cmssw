53d82077569291cb037d3f8e31774af04a5adff3
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-05-16 14:36:29 +0000

    Rewritten from scratch

--- DataFormats/RPCDigi/interface/RPCDigiCollection.h
+++ DataFormats/RPCDigi/interface/RPCDigiCollection.h
@@ -1,20 +1,11 @@
 #ifndef RPCDigi_RPCDigiCollection_h
 #define RPCDigi_RPCDigiCollection_h
-
 /** \class RPCDigiCollection
- *  ED
- *
- *  $Date: 2005/10/07 17:40:53 $
- *  $Revision: 1.1 $
- *  \author Stefano ARGIRO
+ *  
+ *  \author Ilaria Segoni 
+ *  \version $Id: RPCDigiCollection.h,v 1.1 2005/11/03 15:26:11 segoni Exp $
+ *  \date 21 Apr 2005
  */
-
-/** \class RPCDigiCollection
-   
-   \author Ilaria Segoni 
-   \version $Id: RPCDigiCollection.h,v 1.1 2005/10/07 17:40:53 segoni Exp $
-   \date 21 Apr 2005
-*/
 
 #include <DataFormats/MuonDetId/interface/RPCDetId.h>
 #include <DataFormats/RPCDigi/interface/RPCDigi.h>
--- DataFormats/RPCDigi/src/RPCDigi.cc
+++ DataFormats/RPCDigi/src/RPCDigi.cc
@@ -1,7 +1,7 @@
 /** \file
  * 
- *  $Date: 2006/04/06 07:54:05 $
- *  $Revision: 1.5 $
+ *  $Date: 2006/05/03 23:51:58 $
+ *  $Revision: 1.6 $
  *
  * \author Ilaria Segoni
  */
@@ -9,76 +9,43 @@
 
 #include <DataFormats/RPCDigi/interface/RPCDigi.h>
 
-#include <iostream>
-#include <bitset>
+RPCDigi::RPCDigi (int strip, int bx) :
+  strip_(strip),
+  bx_(bx)
+{}
 
-using namespace std;
-
-
-RPCDigi::RPCDigi(int strip, int bx){
-	this->setStripBx( strip , bx );
-}
-
-
-/// Copy constructor
-RPCDigi::RPCDigi(const RPCDigi& digi) {
-  this->setStripBx(digi.strip(), digi.bx());
-}
+RPCDigi::RPCDigi ():
+  strip_(0),
+  bx_(0) 
+{}
 
 
-
-/// Assignment
-RPCDigi& 
-RPCDigi::operator=(const RPCDigi& digi){
-	this->setStripBx(digi.strip(), digi.bx());
-	return *this;
-}
-
-///Precedence operator
-bool RPCDigi::operator<(const RPCDigi& digi) const{
-
-if(digi.bx() == this->bx())
- return digi.strip()<this->strip();
-else 
- return digi.bx()<this->bx();
- 
-
-}
-
-/// Comparison
-bool RPCDigi::operator == (const RPCDigi& digi) const {
-  if ( !(this->strip() == digi.strip())     ||
-       !(this->bx()== digi.bx()) ) return false;
+// Comparison
+bool
+RPCDigi::operator == (const RPCDigi& digi) const {
+  if ( strip_ != digi.strip() ||
+       bx_    != digi.bx() ) return false;
   return true;
 }
 
-///Print Digi Content
-void RPCDigi::print() const {
-  cout << "strip " << this->strip() 
-       << " bx   " << this->bx() << endl;
+///Precedence operator
+bool 
+RPCDigi::operator<(const RPCDigi& digi) const{
+
+  if(digi.bx() == this->bx())
+    return digi.strip()<this->strip();
+  else 
+    return digi.bx()<this->bx();
 }
 
-/// Getter methods:
+
 int RPCDigi::strip() const { return strip_; }
 
 int RPCDigi::bx() const { return bx_; }
 
-
-/// Setter methods:
-
-void RPCDigi::setStripBx(int strip, int bx) {
-  strip_=strip;
-  bx_=bx;
+void
+RPCDigi::print() const {
+  std::cout << "Strip " << strip() 
+       << " bx " << bx() <<std::endl;
 }
-
-void RPCDigi::setStrip(int strip) {
-  strip_=strip;
-}
-
-void RPCDigi::setBx(int bx) {
-  bx_=bx;
-}
-
-
-
 
