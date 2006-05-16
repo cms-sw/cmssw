f211bf080cd0d6d3f25d85ce004be033fa68e5cd
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-05-16 09:42:23 +0000

    remove typedef, dox

--- TrackingTools/DetLayers/interface/DetLayer.h
+++ TrackingTools/DetLayers/interface/DetLayer.h
@@ -11,7 +11,8 @@
 
 
 
-/** The DetLayer is the detector abstraction used for track reconstruction.
+/** \class DetLayer
+ *  The DetLayer is the detector abstraction used for track reconstruction.
  *  It inherits from GeometricSearchDet the interface for accessing 
  *  components and compatible components. 
  *  It extends the interface by providing navigation capability 
@@ -25,31 +26,42 @@
 class DetLayer : public GeometricSearchDet {
   
  public:
-  typedef GeometricSearchDet          Det;
 
   DetLayer() : theNavigableLayer(0){};
 
   virtual ~DetLayer();
 
   // Extension of the interface 
+
+  /// The type of detector (pixel, silicon, dt, csc, rpc)
   virtual Module module() const = 0;
+  /// Which part of the detector (barrel, forward)
   virtual Part   part()   const = 0;
 
+  /// Return the NavigableLayer associated with this DetLayer
   NavigableLayer* navigableLayer() const { return theNavigableLayer;}
+
+  /// Set the NavigableLayer associated with this DetLayer
   virtual void setNavigableLayer( NavigableLayer* nlp);
-  
+
+  /// Return the next (closest) layer(s) that can be reached in the specified
+  /// PropagationDirection
+  virtual vector<const DetLayer*> 
+  nextLayers( PropagationDirection timeDirection) const;
+
+  /// Return the next (closest) layer(s) compatible with the specified
+  /// FreeTrajectoryState and PropagationDirection
   virtual vector<const DetLayer*> 
   nextLayers( const FreeTrajectoryState& fts, 
 	      PropagationDirection timeDirection) const;
 
-  virtual vector<const DetLayer*> 
-  nextLayers( PropagationDirection timeDirection) const;
-
-
+  /// Return all layers that can be reached from this one along the
+  /// specified PropagationDirection 
   vector<const DetLayer*> 
   compatibleLayers( PropagationDirection timeDirection ) const;
 
-
+  /// Returns all layers compatible with the specified FreeTrajectoryState
+  /// and PropagationDirection  
   vector<const DetLayer*> 
   compatibleLayers(const FreeTrajectoryState& fts, 
 		   PropagationDirection timeDirection) const;
