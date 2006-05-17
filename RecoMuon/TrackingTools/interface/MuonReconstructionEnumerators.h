1259a6d9bbc4bf894e7f39e98924f5aa4b824e68
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-05-17 18:20:13 +0000

    initial version from ORCA

---
+++ RecoMuon/TrackingTools/interface/MuonReconstructionEnumerators.h
@@ -0,0 +1,9 @@
+#ifndef TrackingTools_MuonReconstructionEnumerators_H
+#define TrackingTools_MuonReconstructionEnumerators_H
+
+enum ReconstructionDirection {insideOut, outsideIn};
+
+enum MuonReconstructionSeedOption {internalSeeds, externalSeeds};
+
+#endif
+
