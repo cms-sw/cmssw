dac27b3bccbb5800c2a67e25053dd6c5cfbb179b
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-05-17 10:01:41 +0000

    update

--- RecoMuon/DetLayers/src/MuonDTDetLayerGeometryBuilder.cc
+++ RecoMuon/DetLayers/src/MuonDTDetLayerGeometryBuilder.cc
@@ -15,39 +15,42 @@ MuonDTDetLayerGeometryBuilder::~MuonDTDetLayerGeometryBuilder() {
 }
 
 vector<DetLayer*> 
-    MuonDTDetLayerGeometryBuilder::buildLayers(const DTGeometry& geo) {
+MuonDTDetLayerGeometryBuilder::buildLayers(const DTGeometry& geo) {
         
-    vector<DetLayer*> detlayers;
-    vector<MuRodBarrelLayer*> result;
-    vector<const DetRod*> muDetRings;
+  vector<DetLayer*> detlayers;
+  vector<MuRodBarrelLayer*> result;
             
-    for(int station = DTChamberId::minStationId; station <= DTChamberId::maxStationId; station++) {
+  for(int station = DTChamberId::minStationId; station <= DTChamberId::maxStationId; station++) {
 
-        for(int wheel = DTChamberId::minWheelId; wheel <= DTChamberId::maxWheelId; wheel++) {
-                
-            vector<const GeomDet*> geomDets;
-            for(int sector = DTChamberId::minSectorId; sector <= DTChamberId::maxSectorId; sector++) {
-		  
-                const GeomDet* geomDet = geo.idToDet(DTChamberId(wheel, station, sector));
-	            if (geomDet) {
-		            geomDets.push_back(geomDet);
-		            LogDebug("RecoMuonDetLayers") << "get DT chamber " <<  DTChamberId(wheel, station, sector) << " " << geomDet;
-		        }
-            }
-                
-		    if (geomDets.size()!=0) {
-                muDetRings.push_back(new MuDetRod(geomDets));
-		        LogDebug("RecoMuonDetLayers") << "New wheel with " << geomDets.size() << " chambers";
-		    }
-        }
-                
-        result.push_back(new MuRodBarrelLayer(muDetRings));  
-	    LogDebug("RecoMuonDetLayers") << "New layer with " << muDetRings.size() << " wheels";
-        muDetRings.clear();  
-    }    
+    vector<const DetRod*> muDetRods;
+    for(int sector = DTChamberId::minSectorId; sector <= DTChamberId::maxSectorId; sector++) {
 
-    for(vector<MuRodBarrelLayer*>::const_iterator it = result.begin(); it != result.end(); it++)
-            detlayers.push_back((DetLayer*)(*it));
+      vector<const GeomDet*> geomDets;
+      for(int wheel = DTChamberId::minWheelId; wheel <= DTChamberId::maxWheelId; wheel++) {		  
+	const GeomDet* geomDet = geo.idToDet(DTChamberId(wheel, station, sector));
+	if (geomDet) {
+	  geomDets.push_back(geomDet);
+	  LogDebug("Muon|DT|RecoMuonDetLayers") << "get DT chamber " <<  DTChamberId(wheel, station, sector)
+					<< " at R=" << geomDet->position().perp()
+					<< ", phi=" << geomDet->position().phi() ;
+	}
+      }
+                
+      if (geomDets.size()!=0) {
+	muDetRods.push_back(new MuDetRod(geomDets));
+	LogDebug("Muon|DT|RecoMuonDetLayers") << "  New MuDetRod with " << geomDets.size()
+				      << " chambers at R=" << muDetRods.back()->position().perp()
+				      << ", phi=" << muDetRods.back()->position().phi();
+      }
+    }
 
-    return detlayers;
+    result.push_back(new MuRodBarrelLayer(muDetRods));  
+    LogDebug("Muon|DT|RecoMuonDetLayers") << "    New MuRodBarrelLayer with " << muDetRods.size()
+				  << " rods, at R " << result.back()->specificSurface().radius();
+  }
+
+  for(vector<MuRodBarrelLayer*>::const_iterator it = result.begin(); it != result.end(); it++)
+    detlayers.push_back((DetLayer*)(*it));
+
+  return detlayers;
 }
