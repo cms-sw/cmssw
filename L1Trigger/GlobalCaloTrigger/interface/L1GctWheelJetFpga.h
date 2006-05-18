933ee2eaba028130a90826fd62247249cedffb7e
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-05-18 13:19:41 +0000

    removed vector<>::push_back() usage

--- L1Trigger/GlobalCaloTrigger/interface/L1GctWheelJetFpga.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctWheelJetFpga.h
@@ -11,91 +11,97 @@
 class L1GctWheelJetFpga : public L1GctProcessor
 {
 public:
-    L1GctWheelJetFpga(int id);
-    ~L1GctWheelJetFpga();
+  L1GctWheelJetFpga(int id);
+  ~L1GctWheelJetFpga();
 
-    typedef std::vector<L1GctJetCand> JetVector;
+  typedef std::vector<L1GctJetCand> JetVector;
 
-    /// clear internal buffers
-    virtual void reset();
+  /// clear internal buffers
+  virtual void reset();
 
-    /// get input data from sources
-    virtual void fetchInput();
+  /// get input data from sources
+  virtual void fetchInput();
 
-    /// process the data, fill output buffers
-    virtual void process();
+  /// process the data, fill output buffers
+  virtual void process();
 
-    /// set input sources
-    void setInputLeafCard(int i, L1GctJetLeafCard* card);
+  /// set input sources
+  void setInputLeafCard(int i, L1GctJetLeafCard* card);
 
-    /// set input data      
-    void setInputJet(int i, L1GctJetCand jet); 
-    void setInputHt (int i, unsigned ht);
+  /// set input data      
+  void setInputJet(int i, L1GctJetCand jet); 
+  void setInputHt (int i, unsigned ht);
     
-    /// get the input jets. Jets 0-5 from leaf card 0, jetfinderA.  Jets 6-11 from leaf card 0, jetfinder B... etc.
-    JetVector getInputJets() const { return m_inputJets; }
+  /// get the input jets. Jets 0-5 from leaf card 0, jetfinderA.  Jets 6-11 from leaf card 0, jetfinder B... etc.
+  JetVector getInputJets() const { return m_inputJets; }
     
-    // get the input Ht
-    unsigned long getInputHt(unsigned leafnum) const { return m_inputHt[leafnum].to_ulong(); }
+  // get the input Ht
+  unsigned long getInputHt(unsigned leafnum) const { return m_inputHt[leafnum].to_ulong(); }
     
-    // get the output jets
-    JetVector getCentralJets() const { return m_centralJets; }
-    JetVector getForwardJets() const { return m_forwardJets; }
-    JetVector getTauJets() const { return m_tauJets; }
+  // get the output jets
+  JetVector getCentralJets() const { return m_centralJets; }
+  JetVector getForwardJets() const { return m_forwardJets; }
+  JetVector getTauJets() const { return m_tauJets; }
     
-    // get the output Ht and jet counts
-    unsigned long getOutputHt() const { return m_outputHt.to_ulong(); }
-    unsigned long getOutputJc(unsigned jcnum) const { return m_outputJc[jcnum].to_ulong(); }
+  // get the output Ht and jet counts
+  unsigned long getOutputHt() const { return m_outputHt.to_ulong(); }
+  unsigned long getOutputJc(unsigned jcnum) const { return m_outputJc[jcnum].to_ulong(); }
 
-    /// Max number of jets of each type (central, foward, tau) we output.
-    static const int MAX_JETS_OUT = 4;
+  /// Max number of jets of each type (central, foward, tau) we output.
+  static const int MAX_JETS_OUT = 4;
    
 private:
-    /// Max number of leaf card pointers
-    static const int MAX_LEAF_CARDS = 3;
-    /// Maximum number of jets we can have as input
-    static const int MAX_JETS_IN = MAX_LEAF_CARDS * L1GctJetLeafCard::MAX_JET_FINDERS * L1GctJetFinder::MAX_JETS_OUT;
+  /// Max number of leaf card pointers
+  static const int MAX_LEAF_CARDS = 3;
+  /// Maximum number of jets we can have as input
+  static const int MAX_JETS_IN = MAX_LEAF_CARDS * L1GctJetLeafCard::MAX_JET_FINDERS * L1GctJetFinder::MAX_JETS_OUT;
     
-    /// algo ID
-    int m_id;
-
-    /// the jet leaf cards
-    std::vector<L1GctJetLeafCard*> m_inputLeafCards;
+  static const int MAX_RAW_CJETS = 36;  ///< Max. possible central jets to sort over
+  static const int MAX_RAW_FJETS = 18;  ///< Max. possible forward jets to sort over
+  static const int MAX_RAW_TJETS = 36;  ///< Max. possible tau jets to sort over
     
-    /// input data. Jets 0-5 from leaf card 0, jetfinderA.  Jets 6-11 from leaf card 0, jetfinder B... etc.
-    JetVector m_inputJets;
+  /// algo ID
+  int m_id;
+
+  /// the jet leaf cards
+  std::vector<L1GctJetLeafCard*> m_inputLeafCards;
     
-    // Holds the all the various inputted jets, re-addressed using proper GCT->GT jet addressing
-    JetVector m_rawCentralJets; 
-    JetVector m_rawForwardJets; 
-    JetVector m_rawTauJets; 
-
-    // input Ht sums from each leaf card
-    static const int NUM_BITS_ENERGY_DATA = 13;
-    static const int OVERFLOW_BIT = NUM_BITS_ENERGY_DATA - 1;
-
-    static const int Emax = (1<<NUM_BITS_ENERGY_DATA);
-    static const int signedEmax = (Emax>>1);
-
-    // input data - need to confirm number of bits!
-    typedef std::bitset<NUM_BITS_ENERGY_DATA> InputEnergyType;
-    std::vector<InputEnergyType> m_inputHt;
-
-    // output data
-    JetVector m_centralJets;
-    JetVector m_forwardJets;
-    JetVector m_tauJets;
+  /// input data. Jets 0-5 from leaf card 0, jetfinderA.  Jets 6-11 from leaf card 0, jetfinder B... etc.
+  JetVector m_inputJets;
     
-    // data sent to GlobalEnergyAlgos
-    typedef std::bitset<3> JcWheelType;
-    std::bitset<NUM_BITS_ENERGY_DATA> m_outputHt;
-    std::vector<JcWheelType> m_outputJc;
+  // Holds the all the various inputted jets, re-addressed using proper GCT->GT jet addressing
+  JetVector m_rawCentralJets; 
+  JetVector m_rawForwardJets; 
+  JetVector m_rawTauJets; 
+
+  // input Ht sums from each leaf card
+  static const int NUM_BITS_ENERGY_DATA = 13;
+  static const int OVERFLOW_BIT = NUM_BITS_ENERGY_DATA - 1;
+
+  static const int Emax = (1<<NUM_BITS_ENERGY_DATA);
+  static const int signedEmax = (Emax>>1);
+
+  // input data - need to confirm number of bits!
+  typedef std::bitset<NUM_BITS_ENERGY_DATA> InputEnergyType;
+  std::vector<InputEnergyType> m_inputHt;
+
+  // output data
+  JetVector m_centralJets;
+  JetVector m_forwardJets;
+  JetVector m_tauJets;
+    
+  // data sent to GlobalEnergyAlgos
+  typedef std::bitset<3> JcWheelType;
+  std::bitset<NUM_BITS_ENERGY_DATA> m_outputHt;
+  std::vector<JcWheelType> m_outputJc;
       
-    //PRIVATE METHODS
-    /// Puts the output from a jetfinder into the correct index range of the m_inputJets array. 
-    void storeJets(JetVector jets, unsigned short iLeaf, unsigned short offset);
-    /// Classifies jets into central, forward or tau, and re-addresses them using global co-ords.
-    void classifyJets();  
+  //PRIVATE METHODS
+  /// Puts the output from a jetfinder into the correct index range of the m_inputJets array. 
+  void storeJets(JetVector jets, unsigned short iLeaf, unsigned short offset);
+  /// Classifies jets into central, forward or tau, and re-addresses them using global co-ords.
+  void classifyJets();
+  /// Sizes the m_rawTauJetsVec, and then sets all tauVeto bits to false.
+  void setupRawTauJetsVec();
 };
 
 #endif /*L1GCTWHEELJETFPGA_H_*/
--- L1Trigger/GlobalCaloTrigger/src/L1GctWheelJetFpga.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctWheelJetFpga.cc
@@ -1,18 +1,23 @@
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctWheelJetFpga.h"
 
+#include "FWCore/Utilities/interface/Exception.h"
+
 using std::vector;
 using std::bitset;
 
 L1GctWheelJetFpga::L1GctWheelJetFpga(int id):
-    m_id(id),
-    m_inputLeafCards(MAX_LEAF_CARDS),
-    m_inputJets(MAX_JETS_IN),
-    m_inputHt(MAX_LEAF_CARDS),
-    m_centralJets(MAX_JETS_OUT),
-    m_forwardJets(MAX_JETS_OUT),
-    m_tauJets(MAX_JETS_OUT),
-    m_outputJc(12)
+  m_id(id),
+  m_inputLeafCards(MAX_LEAF_CARDS),
+  m_inputJets(MAX_JETS_IN),
+  m_rawCentralJets(MAX_RAW_CJETS),
+  m_rawForwardJets(MAX_RAW_FJETS),
+  m_inputHt(MAX_LEAF_CARDS),
+  m_centralJets(MAX_JETS_OUT),
+  m_forwardJets(MAX_JETS_OUT),
+  m_tauJets(MAX_JETS_OUT),
+  m_outputJc(12)
 {
+  setupRawTauJetsVec();  //sets up tau jet vector, but with tau-veto bits set to false.
 }
 
 L1GctWheelJetFpga::~L1GctWheelJetFpga()
@@ -21,180 +26,218 @@ L1GctWheelJetFpga::~L1GctWheelJetFpga()
 
 void L1GctWheelJetFpga::reset()
 {
-    m_inputJets.clear();
-    m_inputJets.resize(MAX_JETS_IN);
-    
-    m_rawCentralJets.clear();
-    m_rawForwardJets.clear();
-    m_rawTauJets.clear();
-    
-    m_centralJets.clear();
-    m_centralJets.resize(MAX_JETS_OUT);
-    m_forwardJets.clear();
-    m_forwardJets.resize(MAX_JETS_OUT);
-    m_tauJets.clear();
-    m_tauJets.resize(MAX_JETS_OUT);
-    
-    for (int i=0; i<MAX_LEAF_CARDS; ++i) 
-    {
-        m_inputHt[i].reset();
-    }
-    m_outputHt.reset();
-    for (int i=0; i<12; ++i) 
-    {
-        m_outputJc[i].reset();
-    }
+  m_inputJets.clear();
+  m_inputJets.resize(MAX_JETS_IN);
+
+  m_rawCentralJets.clear();
+  m_rawCentralJets.resize(MAX_RAW_CJETS);
+  m_rawForwardJets.clear();
+  m_rawForwardJets.resize(MAX_RAW_FJETS);
+  m_rawTauJets.clear();
+  setupRawTauJetsVec();
+
+  m_centralJets.clear();
+  m_centralJets.resize(MAX_JETS_OUT);
+  m_forwardJets.clear();
+  m_forwardJets.resize(MAX_JETS_OUT);
+  m_tauJets.clear();
+  m_tauJets.resize(MAX_JETS_OUT);
+
+  for (int i=0; i<MAX_LEAF_CARDS; ++i)
+  {
+    m_inputHt[i].reset();
+  }
+  m_outputHt.reset();
+  for (int i=0; i<12; ++i)
+  {
+    m_outputJc[i].reset();
+  }
 }
 
 void L1GctWheelJetFpga::fetchInput()
 {
-    //Get Jets
-    for(unsigned short iLeaf = 0; iLeaf < MAX_LEAF_CARDS; ++iLeaf)
-    {
-        assert(m_inputLeafCards[iLeaf] != 0);  //check that the pointers have been set up!
+  //Get Jets
+  for(unsigned short iLeaf = 0; iLeaf < MAX_LEAF_CARDS; ++iLeaf)
+  {
+    assert(m_inputLeafCards[iLeaf] != 0);  //check that the pointers have been set up!
 
-        storeJets(m_inputLeafCards[iLeaf]->getOutputJetsA(), iLeaf, 0);
-        storeJets(m_inputLeafCards[iLeaf]->getOutputJetsB(), iLeaf, MAX_JETS_IN);
-        storeJets(m_inputLeafCards[iLeaf]->getOutputJetsC(), iLeaf, 2*MAX_JETS_IN);
-        
-        // Deal with the Ht inputs
-        // Fetch the output values from each of our input leaf cards.
-        // Use setInputHt() to fill the inputEx[i] variables.
-        setInputHt(iLeaf, m_inputLeafCards[iLeaf]->getOutputHt());        
-    }
+    storeJets(m_inputLeafCards[iLeaf]->getOutputJetsA(), iLeaf, 0);
+    storeJets(m_inputLeafCards[iLeaf]->getOutputJetsB(), iLeaf, MAX_JETS_IN);
+    storeJets(m_inputLeafCards[iLeaf]->getOutputJetsC(), iLeaf, 2*MAX_JETS_IN);
+
+    // Deal with the Ht inputs
+    // Fetch the output values from each of our input leaf cards.
+    // Use setInputHt() to fill the inputEx[i] variables.
+    setInputHt(iLeaf, m_inputLeafCards[iLeaf]->getOutputHt());
+  }
 }
 
 void L1GctWheelJetFpga::process()
 {
-    classifyJets();
+  classifyJets();
 
-    sort(m_rawCentralJets.begin(), m_rawCentralJets.end(), L1GctJetCand::rankGreaterThan());
-    sort(m_rawForwardJets.begin(), m_rawForwardJets.end(), L1GctJetCand::rankGreaterThan());
-    sort(m_rawTauJets.begin(), m_rawTauJets.end(), L1GctJetCand::rankGreaterThan());
-    
-    for(unsigned short iJet = 0; iJet < MAX_JETS_OUT; ++iJet)
-    {
-        m_centralJets[iJet] = m_rawCentralJets[iJet];
-        m_forwardJets[iJet] = m_rawForwardJets[iJet];
-        m_tauJets[iJet] = m_rawTauJets[iJet];
-    }
+  sort(m_rawCentralJets.begin(), m_rawCentralJets.end(), L1GctJetCand::rankGreaterThan());
+  sort(m_rawForwardJets.begin(), m_rawForwardJets.end(), L1GctJetCand::rankGreaterThan());
+  sort(m_rawTauJets.begin(), m_rawTauJets.end(), L1GctJetCand::rankGreaterThan());
 
-    //For use with Ht processing        
-    vector<int> htVal(3);
-    unsigned long htSum;
-    bool htOfl = false;
-    int temp;
+  for(unsigned short iJet = 0; iJet < MAX_JETS_OUT; ++iJet)
+  {
+    m_centralJets[iJet] = m_rawCentralJets[iJet];
+    m_forwardJets[iJet] = m_rawForwardJets[iJet];
+    m_tauJets[iJet] = m_rawTauJets[iJet];
+  }
 
-    // Deal with the Ht summing
-    // Form the Ht sum from the inputs
-    // sent from the Leaf cards
-    for (int i = 0; i < MAX_LEAF_CARDS; ++i) 
-    {
-        // Decode input Ht value with overflow bit
-        temp = (int) m_inputHt[i].to_ulong();
-        if (temp>=Emax) 
-        {
-            htOfl = true;
-            temp = temp % Emax;
-        }
-        htVal[i] = temp;
-    }
+  //For use with Ht processing
+  vector<int> htVal(3);
+  unsigned long htSum;
+  bool htOfl = false;
+  int temp;
 
-    // Form Et sum taking care of overflows
-    temp = htVal[0] + htVal[1] + htVal[2];
-    if(temp>=Emax) 
+  // Deal with the Ht summing
+  // Form the Ht sum from the inputs
+  // sent from the Leaf cards
+  for (int i = 0; i < MAX_LEAF_CARDS; ++i)
+  {
+    // Decode input Ht value with overflow bit
+    temp = (int) m_inputHt[i].to_ulong();
+    if (temp>=Emax)
     {
         htOfl = true;
-        temp -= Emax;
+        temp = temp % Emax;
     }
-    htSum = (unsigned long) temp;
+    htVal[i] = temp;
+  }
 
-    // Convert output back to bitset format 
-    bitset<NUM_BITS_ENERGY_DATA> htBits(htSum);
-    if(htOfl) { htBits.set(OVERFLOW_BIT); }
+  // Form Et sum taking care of overflows
+  temp = htVal[0] + htVal[1] + htVal[2];
+  if(temp>=Emax)
+  {
+    htOfl = true;
+    temp -= Emax;
+  }
+  htSum = (unsigned long) temp;
 
-    m_outputHt = htBits;
-    
+  // Convert output back to bitset format
+  bitset<NUM_BITS_ENERGY_DATA> htBits(htSum);
+  if(htOfl) { htBits.set(OVERFLOW_BIT); }
+
+  m_outputHt = htBits;
+
 }
 
 void L1GctWheelJetFpga::setInputLeafCard(int i, L1GctJetLeafCard* card)
 {
-    assert(i >= 0 && i < MAX_LEAF_CARDS);
+  if(i >= 0 && i < MAX_LEAF_CARDS)
+  {
     m_inputLeafCards[i] = card;
+  }
+  else
+  {
+    throw cms::Exception("RangeError")
+    << "In L1GctWheelJetFpga, Jet Leaf Card " << i << " is outside input range of 0 to "
+    << (MAX_LEAF_CARDS-1) << "\n";
+  }
 }
 
 void L1GctWheelJetFpga::setInputJet(int i, L1GctJetCand jet)
 {
-    assert(i >=0 && i < MAX_JETS_IN);
-    m_inputJets[i] =  jet;    
+  if(i >=0 && i < MAX_JETS_IN)
+  {
+    m_inputJets[i] =  jet;
+  }
+  else
+  {
+    throw cms::Exception("RangeError")
+    << "In L1GctWheelJetFpga, Jet Candidate " << i << " is outside input range of 0 to "
+    << (MAX_LEAF_CARDS-1) << "\n";
+  }
 }
 
 void L1GctWheelJetFpga::setInputHt (int i, unsigned ht)
-{   
-    unsigned long htVal;
-    bool htOfl;
+{
+  unsigned long htVal;
+  bool htOfl;
 
-    int temp;
+  int temp;
 
-    if (i>=0 && i<MAX_LEAF_CARDS) 
-    {
-        // Transform the input variables into the correct range,
-        // and set the overflow bit if they are out of range.
-        // The correct range is between 0 to (Emax-1).
-        // Then copy the inputs into an unsigned long variable.
-        temp = ((int) ht) % Emax;
-        htOfl = (temp != ((int) ht));
-        htVal = (unsigned long) temp;
+  if (i>=0 && i<MAX_LEAF_CARDS)
+  {
+    // Transform the input variables into the correct range,
+    // and set the overflow bit if they are out of range.
+    // The correct range is between 0 to (Emax-1).
+    // Then copy the inputs into an unsigned long variable.
+    temp = ((int) ht) % Emax;
+    htOfl = (temp != ((int) ht));
+    htVal = (unsigned long) temp;
 
-        // Copy the data into the internal bitset format.
-        bitset<NUM_BITS_ENERGY_DATA> htBits(htVal);
-        if (htOfl) {htBits.set(OVERFLOW_BIT);}
+    // Copy the data into the internal bitset format.
+    bitset<NUM_BITS_ENERGY_DATA> htBits(htVal);
+    if (htOfl) {htBits.set(OVERFLOW_BIT);}
 
-        m_inputHt[i] = htBits;
-    }
-} 
+    m_inputHt[i] = htBits;
+  }
+}
 
 void L1GctWheelJetFpga::storeJets(JetVector jets, unsigned short iLeaf, unsigned short offset)
 {
-    for(unsigned short iJet = 0; iJet < L1GctJetFinder::MAX_JETS_OUT; ++iJet)
-    {
-        m_inputJets[iLeaf*MAX_JETS_IN/MAX_LEAF_CARDS + offset + iJet] = jets[iJet];
-    }
+  for(unsigned short iJet = 0; iJet < L1GctJetFinder::MAX_JETS_OUT; ++iJet)
+  {
+    m_inputJets[iLeaf*MAX_JETS_IN/MAX_LEAF_CARDS + offset + iJet] = jets[iJet];
+  }
 }
 
 void L1GctWheelJetFpga::classifyJets()
-{    
-    //Clear the contents of all three of the  raw jet vectors
-    m_rawCentralJets.clear();
-    m_rawForwardJets.clear();
-    m_rawTauJets.clear();
-    
-    //Holds which jet finder we are on, in phi (from 0 to 8).
-    unsigned short int jetFinderIndex = 0;
-    
-    //counter to help in calculation of jetFinderIndex.
-    unsigned short int jetIndex = 0;
-     
-    JetVector::iterator currentJet;
-     
-    for(currentJet = m_inputJets.begin(); currentJet != m_inputJets.end(); ++currentJet)
+{
+  //Clear the contents of all three of the  raw jet vectors
+  m_rawCentralJets.clear();
+  m_rawCentralJets.resize(MAX_RAW_CJETS);
+  m_rawForwardJets.clear();
+  m_rawForwardJets.resize(MAX_RAW_FJETS);
+  m_rawTauJets.clear();
+  setupRawTauJetsVec();
+
+  //Holds which jet finder we are on, in phi (from 0 to 8).
+  unsigned short int jetFinderIndex = 0;
+
+  //counter to help in calculation of jetFinderIndex.
+  unsigned short int inputJetIndex = 0;
+
+  JetVector::iterator currentJet;  
+  
+  //counters for entering the classified jets into the different raw jet vectors
+  unsigned short int numCJets = 0;
+  unsigned short int numFJets = 0;
+  unsigned short int numTJets = 0;
+
+  for(currentJet = m_inputJets.begin(); currentJet != m_inputJets.end(); ++currentJet)
+  {
+    if(currentJet->eta() >= L1GctJetCand::LOCAL_ETA_HF_START)  //forward jet
     {
-        if(currentJet->eta() >= L1GctJetCand::LOCAL_ETA_HF_START)  //forward jet
-        {
-            m_rawForwardJets.push_back(currentJet->convertToGlobalJet(jetFinderIndex, m_id));
-        }
-        else
-        {
-            if(currentJet->tauVeto() == true)  //central non-tau jet.
-            {
-                m_rawCentralJets.push_back(currentJet->convertToGlobalJet(jetFinderIndex, m_id));
-            }
-            else  //must be central tau-jet
-            {
-                m_rawTauJets.push_back(currentJet->convertToGlobalJet(jetFinderIndex, m_id));
-            }
-        }
-        //move onto the next jet finder phi position every 6 jets
-        if(++jetIndex % L1GctJetFinder::MAX_JETS_OUT == 0) { ++jetFinderIndex; }
+        m_rawForwardJets[numFJets++] = currentJet->convertToGlobalJet(jetFinderIndex, m_id);
     }
+    else
+    {
+      if(currentJet->tauVeto() == true)  //central non-tau jet.
+      {
+        m_rawCentralJets[numCJets++] = currentJet->convertToGlobalJet(jetFinderIndex, m_id);
+      }
+      else  //must be central tau-jet
+      {
+        m_rawTauJets[numTJets++] = currentJet->convertToGlobalJet(jetFinderIndex, m_id);
+      }
+    }
+    //move onto the next jet finder phi position every 6 jets
+    if(++inputJetIndex % L1GctJetFinder::MAX_JETS_OUT == 0) { ++jetFinderIndex; }
+  }
+}
+
+void L1GctWheelJetFpga::setupRawTauJetsVec()
+{
+  m_rawTauJets.resize(MAX_RAW_TJETS);
+
+  //now need to set all tau veto bits to false.
+  for(JetVector::iterator currentJet = m_rawTauJets.begin(); currentJet != m_rawTauJets.end(); ++currentJet)
+  {
+    currentJet->setTauVeto(false);
+  }
 }
