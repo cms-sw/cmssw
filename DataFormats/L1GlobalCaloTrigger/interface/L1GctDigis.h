55909ba40e34b7f7b3c325a2a5453a4d1b3f882f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2006-05-18 18:32:42 +0000

    moved L1GctCandidates to L1GctDigis

---
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctDigis.h
@@ -0,0 +1,86 @@
+#ifndef L1GCTDIGI_H
+#define L1GCTDIGI_H
+
+#include <boost/cstdint.hpp>
+#include <ostream>
+
+/*! \file L1GctDigis.h
+ * \Header file for the Gct
+ *  electron and jet candidates
+ * 
+ * \author: Jim Brooke
+ *
+ */
+
+class L1GctCand
+{
+public:
+  L1GctCand();
+  L1GctCand(uint16_t data);
+  L1GctCand(int rank, int phi, int eta);
+  ~L1GctCand();
+
+  ///
+  /// get the raw data
+  uint16_t raw() const { return m_data; }
+  ///
+  /// get rank bits
+  int rank() const { return m_data & 0x3f; }
+  ///
+  /// get phi bits
+  int phi() const { return (m_data>>6) & 0x1f; }
+  ///
+  /// get eta bits
+  int eta() const { return (m_data>>11) & 0xf; }
+
+private:
+
+  uint16_t m_data;
+
+};
+
+std::ostream& operator<<(std::ostream& s, const L1GctCand& cand);
+
+class L1GctIsoEm : public L1GctCand {
+public:
+  L1GctIsoEm();
+  L1GctIsoEm(uint16_t data);
+  L1GctIsoEm(int rank, int phi, int eta);
+  ~L1GctIsoEm();
+ };
+
+class L1GctNonIsoEm : public L1GctCand {
+public:
+  L1GctNonIsoEm();
+  L1GctNonIsoEm(uint16_t data);
+  L1GctNonIsoEm(int rank, int phi, int eta);
+  ~L1GctNonIsoEm();
+ };
+
+class L1GctCenJet : public L1GctCand {
+public:
+  L1GctCenJet();
+  L1GctCenJet(uint16_t data);
+  L1GctCenJet(int rank, int phi, int eta);
+  ~L1GctCenJet();
+ };
+
+class L1GctForJet : public L1GctCand {
+public:
+  L1GctForJet();
+  L1GctForJet(uint16_t data);
+  L1GctForJet(int rank, int phi, int eta);
+  ~L1GctForJet();
+ };
+
+class L1GctTauJet : public L1GctCand {
+public:
+  L1GctTauJet();
+  L1GctTauJet(uint16_t data);
+  L1GctTauJet(int rank, int phi, int eta);
+  ~L1GctTauJet();
+ };
+
+
+
+#endif 
---
+++ DataFormats/L1GlobalCaloTrigger/src/L1GctDigis.cc
@@ -0,0 +1,57 @@
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctDigis.h"
+
+
+L1GctCand::L1GctCand() : m_data(0) { }
+
+L1GctCand::L1GctCand(uint16_t data) : m_data(data) { }
+
+L1GctCand::L1GctCand(int rank, int phi, int eta) {
+  m_data = (rank & 0x3f) + ((phi & 0x1f)<<6) + ((eta & 0xf)<<11);
+}
+
+L1GctCand::~L1GctCand() { } 
+
+
+L1GctIsoEm::L1GctIsoEm() { }
+
+L1GctIsoEm::L1GctIsoEm(uint16_t data) : L1GctCand(data) { }
+
+L1GctIsoEm::L1GctIsoEm(int rank, int phi, int eta) : L1GctCand(rank, phi, eta) { }
+
+L1GctIsoEm::~L1GctIsoEm() { } 
+
+
+L1GctNonIsoEm::L1GctNonIsoEm() { }
+
+L1GctNonIsoEm::L1GctNonIsoEm(uint16_t data) : L1GctCand(data) { }
+
+L1GctNonIsoEm::L1GctNonIsoEm(int rank, int phi, int eta) : L1GctCand(rank, phi, eta) { }
+
+L1GctNonIsoEm::~L1GctNonIsoEm() { } 
+
+
+L1GctCenJet::L1GctCenJet() { }
+
+L1GctCenJet::L1GctCenJet(uint16_t data) : L1GctCand(data) { }
+
+L1GctCenJet::L1GctCenJet(int rank, int phi, int eta) : L1GctCand(rank, phi, eta) { }
+
+L1GctCenJet::~L1GctCenJet() { } 
+
+
+L1GctForJet::L1GctForJet() { }
+
+L1GctForJet::L1GctForJet(uint16_t data) : L1GctCand(data) { }
+
+L1GctForJet::L1GctForJet(int rank, int phi, int eta) : L1GctCand(rank, phi, eta) { }
+
+L1GctForJet::~L1GctForJet() { } 
+
+
+L1GctTauJet::L1GctTauJet() { }
+
+L1GctTauJet::L1GctTauJet(uint16_t data) : L1GctCand(data) { }
+
+L1GctTauJet::L1GctTauJet(int rank, int phi, int eta) : L1GctCand(rank, phi, eta) { }
+
+L1GctTauJet::~L1GctTauJet() { } 
