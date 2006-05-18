88914bbf7e19690369a0fa29ac32edefa0b06565
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2006-05-18 08:24:04 +0000

    Package for Andrea Bocci created

---
+++ RecoBTag/SoftLepton/src/SoftLepton.cc
@@ -0,0 +1,110 @@
+// -*- C++ -*-
+//
+// Package:    SoftLepton
+// Class:      SoftLepton
+// 
+/**\class SoftLepton SoftLepton.cc RecoBTag/SoftLepton/src/SoftLepton.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Andrea Rizzi
+//         Created:  Thu May 18 10:23:15 CEST 2006
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//
+// class decleration
+//
+
+class SoftLepton : public edm::EDProducer {
+   public:
+      explicit SoftLepton(const edm::ParameterSet&);
+      ~SoftLepton();
+
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+SoftLepton::SoftLepton(const edm::ParameterSet& iConfig)
+{
+   //register your products
+/* Examples
+   produces<ExampleData2>();
+
+   //if do put with a label
+   produces<ExampleData2>("label");
+*/
+   //now do what ever other initialization is needed
+
+}
+
+
+SoftLepton::~SoftLepton()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void
+SoftLepton::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+/* This is an event example
+   //Read 'ExampleData' from the Event
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+
+   //Use the ExampleData to create an ExampleData2 which 
+   // is put into the Event
+   std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
+   iEvent.put(pOut);
+*/
+
+/* this is an EventSetup example
+   //Read SetupData from the SetupRecord in the EventSetup
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+*/
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(SoftLepton)
