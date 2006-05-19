7dc87a429b564def5126bdcf8566ee2429898a75
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2006-05-19 14:00:47 +0000

    add another test module

--- SimTracker/TrackerHitAssociation/test/SealModules.cc
+++ SimTracker/TrackerHitAssociation/test/SealModules.cc
@@ -4,9 +4,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "SimTracker/TrackerHitAssociation/test/TestAssociator.h"
+#include "SimTracker/TrackerHitAssociation/test/myTrackAnalyzer.h"
 
 using cms::TestAssociator;
+using cms::myTrackAnalyzer;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TestAssociator)
+DEFINE_ANOTHER_FWK_MODULE(myTrackAnalyzer)
+
 
