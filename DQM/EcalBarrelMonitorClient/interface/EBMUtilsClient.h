d8aead89ed0018b8cfe5d67e97dc402a349700df
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2006-05-21 21:28:03 +0000

    - fix the fix

--- DQM/EcalBarrelMonitorClient/interface/EBMUtilsClient.h
+++ DQM/EcalBarrelMonitorClient/interface/EBMUtilsClient.h
@@ -1,11 +1,11 @@
-// $Id: EBMUtilsClient.h,v 1.3 2006/05/21 19:34:07 dellaric Exp $
+// $Id: EBMUtilsClient.h,v 1.4 2006/05/21 20:42:07 dellaric Exp $
 
 /*!
   \file EBMUtilsClient.h
   \brief Ecal Barrel Monitor Utils for Client
   \author B. Gobbo 
-  \version $Revision: 1.3 $
-  \date $Date: 2006/05/21 19:34:07 $
+  \version $Revision: 1.4 $
+  \date $Date: 2006/05/21 20:42:07 $
 */
 
 #ifndef EBMUtilsClient_H
@@ -28,22 +28,22 @@ class EBMUtilsClient {
     if( me ) {
       MonitorElementT<TNamed>* ob = dynamic_cast<MonitorElementT<TNamed>*>( const_cast<MonitorElement*>(me) );
       if( ob ) { 
-	if( clone ) {
+        if( clone ) {
           if( ret ) delete ret;
-	  std::string s = "ME " + me->getName();
-	  ret = dynamic_cast<T*>((ob->operator->())->Clone(s.c_str())); 
+          ret = 0;
+          std::string s = "ME " + me->getName();
+          ret = dynamic_cast<T*>((ob->operator->())->Clone(s.c_str())); 
         } else {
-	  ret = dynamic_cast<T*>( ob->operator->()); 
-	}
+          ret = dynamic_cast<T*>( ob->operator->()); 
+        }
       }
     }
     return ret;
   }
-
 
  protected:
   EBMUtilsClient() {}
 
 };
 
-#endif // EBMUtilsClient_h
+#endif
