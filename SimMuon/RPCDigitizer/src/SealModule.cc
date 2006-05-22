9e1fd5fbd9afc2936b16b82d334f72e6137c24b7
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2006-05-22 21:17:31 +0000

    add SimParam Digitizer

--- SimMuon/RPCDigitizer/src/SealModule.cc
+++ SimMuon/RPCDigitizer/src/SealModule.cc
@@ -1,8 +1,13 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimMuon/RPCDigitizer/src/RPCDigiProducer.h"
+#include "SimMuon/RPCDigitizer/src/RPCSimFactory.h"
+#include "SimMuon/RPCDigitizer/src/RPCSimSimple.h"
+#include "SimMuon/RPCDigitizer/src/RPCSimParam.h"
 
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(RPCDigiProducer)
+DEFINE_ANOTHER_FWK_MODULE(RPCDigiProducer);
 
+DEFINE_SEAL_PLUGIN(RPCSimFactory,RPCSimSimple,"RPCSimSimple");
+DEFINE_SEAL_PLUGIN(RPCSimFactory,RPCSimParam,"RPCSimParam");
