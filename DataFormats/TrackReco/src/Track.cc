412d801f50df7bb2ca8b187e4873324f5ae010f5
Marcel Vos <sha1-6f5a4aa395afc2bf624238a9e724eaebd2d6d6bc@cern.ch>
2006-05-22 16:52:37 +0000

    changes for hit pattern

--- DataFormats/TrackReco/src/Track.cc
+++ DataFormats/TrackReco/src/Track.cc
@@ -3,13 +3,12 @@
 using namespace reco;
 
 Track::Track( float chi2, unsigned short ndof,  
-	      int found, int lost, int invalid, 
-	      const perigee::Parameters & par, const perigee::Covariance & cov  ) : 
-  TrackBase( chi2, ndof, found, lost, invalid, par, cov ) {
+	      const helix::Parameters & par, const helix::Covariance & cov  ) : 
+  TrackBase( chi2, ndof, par, cov ) {
 }
 
-// Track::Track( float chi2, unsigned short ndof, int found, int invalid, int lost,
-// 	      int q, const Point & v, const Vector & p, 
-// 	      const PosMomError & err ) :
-//   TrackBase( chi2, ndof, found, lost, invalid, q, v, p, err ) {
-// }
+Track::Track( float chi2, unsigned short ndof, 
+	      int q, const Point & v, const Vector & p, 
+	      const PosMomError & err ) :
+  TrackBase( chi2, ndof, q, v, p, err ) {
+}
--- DataFormats/TrackReco/src/TrackBase.cc
+++ DataFormats/TrackReco/src/TrackBase.cc
@@ -2,9 +2,21 @@
 using namespace reco;
 
 TrackBase::TrackBase( double chi2, double ndof,  
-		      int found, int lost, int invalid, 
-		      const perigee::Parameters & par, const perigee::Covariance & cov  ) : 
+		      const helix::Parameters & par, const helix::Covariance & cov  ) : 
   chi2_( chi2 ), ndof_( ndof ), 
-  found_( found ), lost_( lost ), invalid_( invalid ),
   par_( par ), cov_( cov ) {
+
 }
+
+TrackBase::TrackBase( double chi2, double ndof, 
+	      int q, const Point & v, const Vector & p, 
+	      const PosMomError & err ) :
+  chi2_( chi2 ), ndof_( ndof ), 
+  par_( ), cov_( ) {
+  
+  helix::setFromCartesian( q, v, p, err, par_, cov_ );
+
+}
+
+
+
