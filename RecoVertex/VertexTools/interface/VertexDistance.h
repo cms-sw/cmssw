8d2fa1b573fd6fc6f4425d3d9bfa25c752ca24a9
ukerzel <sha1-a31c2afd8bf6095beb77e78276069407986b4658@cern.ch>
2006-05-23 11:50:35 +0000

    add some documentation to VertexDistance.h

--- RecoVertex/VertexTools/interface/VertexDistance.h
+++ RecoVertex/VertexTools/interface/VertexDistance.h
@@ -19,9 +19,21 @@ class VertexDistance {
   virtual Measurement1D distance(const reco::Vertex &, 
 				 const reco::Vertex &) const = 0;
 
+  /**
+   * The signed distance is computed using a vector
+   * from the primary to the secondary vertex and
+   * a given reference vector.
+   * for the 2D case:
+   *   The sign is determined by the scalar product of the x,y component of
+   *   the vector connecting the vertices and the reference vector:
+   *   if the scalar product is greater than zero, the sign is +1, else -1
+   *
+   * for the 3D case:
+   *   Follows same approach, using all three components of the two vectors
+   */
   virtual Measurement1D signedDistance(const reco::Vertex &primVtx , 
-				 const reco::Vertex &secVtx,
-				 const GlobalVector & momentum) const = 0;
+				       const reco::Vertex &secVtx,
+				       const GlobalVector & momentum) const = 0;
 
   virtual float compatibility (const reco::Vertex &, 
 			       const reco::Vertex &) const = 0;
