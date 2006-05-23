4dc0a40722882b009f168594e2e24c8ec1ca39c2
goitom <sha1-b912c4f8b61cf4e8d274924deec638097539d5f4@cern.ch>
2006-05-23 17:48:57 +0000

    first commit

---
+++ DQM/TrackerMonitorTrack/interface/MonitorTrackGlobal.h
@@ -0,0 +1,74 @@
+#ifndef MonitorTrackGlobal_H
+#define MonitorTrackGlobal_H
+// -*- C++ -*-
+//
+// Package:    MonitorTrackGlobal
+// Class:      MonitorTrackGlobal
+// 
+/**\class MonitorTrackGlobal MonitorTrackGlobal.cc DQM/TrackerMonitorTrack/src/MonitorTrackGlobal.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Israel Goitom
+//         Created:  Tue May 23 18:35:30 CEST 2006
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/Daemon/interface/MonitorDaemon.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+//
+// class declaration
+//
+
+class MonitorTrackGlobal : public edm::EDAnalyzer {
+   public:
+      explicit MonitorTrackGlobal(const edm::ParameterSet&);
+      ~MonitorTrackGlobal();
+      virtual void beginJob(edm::EventSetup const& iSetup);
+      virtual void endJob(void);
+
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+  edm::ParameterSet conf_;
+
+  std::map<uint32_t, MonitorElement *> detModules;
+
+//  unsigned int minTracks_;
+
+  DaqMonitorBEInterface * dbe;
+
+  MonitorElement * trackSize;
+
+  MonitorElement * d0VsTheta;
+  MonitorElement * d0VsPhi;
+  MonitorElement * d0VsEta;
+  MonitorElement * z0VsTheta;
+  MonitorElement * z0VsPhi;
+  MonitorElement * z0VsEta;
+
+  MonitorElement * chiSqrdVsTheta;
+  MonitorElement * chiSqrdVsPhi;
+  MonitorElement * chiSqrdVsEta;
+
+};
+#endif
---
+++ DQM/TrackerMonitorTrack/src/MonitorTrackGlobal.cc
@@ -0,0 +1,123 @@
+// -*- C++ -*-
+//
+// Package:    MonitorTrackGlobal
+// Class:      MonitorTrackGlobal
+// 
+/**\class MonitorTrackGlobal MonitorTrackGlobal.cc DQM/TrackerMonitorTrack/src/MonitorTrackGlobal.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Israel Goitom
+//         Created:  Tue May 23 18:35:30 CEST 2006
+// $Id$
+//
+//
+
+#include "DQM/TrackerMonitorTrack/interface/MonitorTrackGlobal.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+MonitorTrackGlobal::MonitorTrackGlobal(const edm::ParameterSet& iConfig)
+{
+  dbe = edm::Service<DaqMonitorBEInterface>().operator->();
+}
+
+
+MonitorTrackGlobal::~MonitorTrackGlobal()
+{
+  delete d0VsTheta;
+  delete d0VsPhi;
+  delete d0VsEta;
+  delete z0VsTheta;
+  delete z0VsPhi;
+  delete z0VsEta;
+  delete chiSqrdVsTheta;
+  delete chiSqrdVsPhi;
+  delete chiSqrdVsEta;
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void
+MonitorTrackGlobal::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+
+   Handle<reco::TrackCollection> trackCollection;
+   iEvent.getByLabel("TrackProducer", trackCollection);
+   trackSize->Fill(trackCollection->size());
+
+//reco::Track singletrack=*(tracks->begin());
+
+ for (reco::TrackCollection::const_iterator track = trackCollection->begin(); track!=trackCollection->end(); ++track)
+   {
+     //LogInfo("Demo")<<"Track "<<(int)(track-trackCollection->begin())<<" Chisq: "<< track->normalizedChi2()<<" "<<track->recHitsSize()<<" hits";
+
+     d0VsTheta->Fill(track->theta(), track->d0());
+     d0VsPhi->Fill(track->phi(), track->d0());
+     d0VsEta->Fill(track->eta(), track->d0());
+
+     z0VsTheta->Fill(track->theta(), track->d0());
+     z0VsPhi->Fill(track->phi(), track->d0());
+     z0VsEta->Fill(track->eta(), track->d0());
+
+     chiSqrdVsTheta->Fill(track->theta(), track->normalizedChi2());
+     chiSqrdVsPhi->Fill(track->phi(), track->normalizedChi2());
+     chiSqrdVsEta->Fill(track->eta(), track->normalizedChi2());
+
+   }
+}
+
+void MonitorTrackGlobal::beginJob(edm::EventSetup const& iSetup)
+{
+  using namespace edm;
+
+  dbe->setCurrentFolder("SiStrip/Track Parameters");
+  trackSize = dbe->book1D("TkSize", "Track size.", 50, 0, 100);
+
+  dbe->setCurrentFolder("SiStrip/Track Parameters/MomentumParameters");
+
+  dbe->setCurrentFolder("SiStrip/Track Parameters/Impact Parameters/d0");
+  d0VsTheta = dbe->book2D("d0 vs. #theta", "Transverse Impact Parameter VS #theta.", 50, 0, 3.2, 50, 0, .2);
+  d0VsPhi = dbe->book2D("d0 vs. #phi", "Transverse Impact Parameter VS #phi.", 50, -4 , 4 , 50, 0, .2);
+  d0VsEta = dbe->book2D("d0 vs. #eta", "Transverse Impact Parameter VS #eta.", 50, -3 , 3 , 50, 0, .2);
+
+  dbe->setCurrentFolder("SiStrip/Track Parameters/Impact Parameters/z0");
+  z0VsTheta = dbe->book2D("z0 vs #theta", "Z Impact Parameter VS #theta.", 50, 0, 3.2, 50, -20, 20);
+  z0VsPhi = dbe->book2D("z0 vs #phi", "Z Impact Parameter VS #phi.", 50, -4, 4, 50, -20, 20);
+  z0VsEta = dbe->book2D("z0 vs #eta", "Z Impact Parameter VS #eta.", 50, -3, 3, 50, -20, 20);
+  
+  dbe->setCurrentFolder("SiStrip/Track Parameters/Trajectory Parameters");
+  chiSqrdVsTheta = dbe->book2D("#chi^{2} vs #theta", "#chi^{2} vs #theta.", 50, 0, 3.2 , 50, 0, 20);
+  chiSqrdVsPhi = dbe->book2D("#chi^{2} vs #phi", "#chi^{2} vs #phi.", 50, -4 , 4 , 50, 0, 20);
+  chiSqrdVsEta = dbe->book2D("#chi^{2} vs #eta", "#chi^{2} vs #eta.", 50, -3 , 3, 50, 0, 20);
+}
+
+void MonitorTrackGlobal::endJob(void)
+{
+  dbe->showDirStructure();
+  dbe->save("test.root");
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MonitorTrackGlobal)
