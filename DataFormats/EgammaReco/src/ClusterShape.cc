7d9077a2675ad608160cf9ee9a27aafb3d1b3275
Andrew Warren Askew <sha1-4f9917bf818f9bc17be25445433598f3a186dfc7@cern.ch>
2006-05-23 16:27:16 +0000

    Updates, methods now close to done.

--- DataFormats/EgammaReco/src/ClusterShape.cc
+++ DataFormats/EgammaReco/src/ClusterShape.cc
@@ -1,8 +1,11 @@
 #include "DataFormats/EgammaReco/interface/ClusterShape.h"
 
 reco::ClusterShape::ClusterShape( double cEE, double cEP, double cPP, 
-                                  double eMax, double e2x2, double e3x3, double e5x5,
-                                  double hadOverEcal ) :
-  covEtaEta_( cEE ), covEtaPhi_( cEP ), covPhiPhi_( cPP ),
-  eMax_(eMax), e2x2_(e2x2), e3x3_(e3x3), e5x5_(e5x5), hadOverEcal_( hadOverEcal ) {
-}
+                  double eMax, DetId eMaxId, double e2nd, DetId e2ndId,
+		  double e2x2, double e3x2, double e3x3, double e5x5,
+        	  double e3x2Ratio, math::XYZPoint location) :
+  covEtaEta_( cEE ), covEtaPhi_( cEP ), covPhiPhi_( cPP ), 
+  eMax_(eMax), e2nd_(e2nd), 
+  e2x2_(e2x2), e3x2_(e3x2), e3x3_(e3x3), e5x5_(e5x5),
+  e3x2Ratio_(e3x2Ratio), location_(location),
+  eMaxId_(eMaxId), e2ndId_(e2ndId){}
