a713850621c1a803d6e65105b6b4ee952a121230
Aaron Dominguez <sha1-5aa85c276f68fbe9c87c461aa7144bbc42f7c82a@cern.ch>
2006-05-23 22:56:49 +0000

    Fixed bug in use of vector of RefTrack's

--- RecoPixelVertexing/PixelVertexFinding/test/PixelVertexTest.cc
+++ RecoPixelVertexing/PixelVertexFinding/test/PixelVertexTest.cc
@@ -15,6 +15,10 @@
 #include "RecoPixelVertexing/PixelVertexFinding/interface/PVPositionBuilder.h"
 
 #include <iostream>
+#include <vector>
+#include "TTree.h"
+#include "TFile.h"
+#include "TDirectory.h"
 
 using namespace std;
 
@@ -22,15 +26,22 @@ class PixelVertexTest : public edm::EDAnalyzer {
 public:
   explicit PixelVertexTest(const edm::ParameterSet& conf);
   ~PixelVertexTest();
-  virtual void beginJob(const edm::EventSetup& es) { }
+  virtual void beginJob(const edm::EventSetup& es);
   virtual void analyze(const edm::Event& ev, const edm::EventSetup& es);
-  virtual void endJob() { }
+  virtual void endJob();
 private:
   edm::ParameterSet conf_; 
+  // Tree of simple vars for testing resolution eff etc
+  TTree *t_;
+  TFile *f_;
+  int ntrk_;
+  int nvtx_;
+  double vz_[10];
+  double vzwt_[10];
 };
 
 PixelVertexTest::PixelVertexTest(const edm::ParameterSet& conf)
-  : conf_(conf)
+  : conf_(conf),t_(0),f_(0)
 {
   edm::LogInfo("PixelVertexTest")<<" CTOR";
 }
@@ -38,6 +49,20 @@ PixelVertexTest::PixelVertexTest(const edm::ParameterSet& conf)
 PixelVertexTest::~PixelVertexTest()
 {
   edm::LogInfo("PixelVertexTest")<<" DTOR";
+  delete f_;
+  //  delete t_;
+}
+
+void PixelVertexTest::beginJob(const edm::EventSetup& es) {
+  // Make my little tree
+  std::string file = conf_.getUntrackedParameter<std::string>("OutputTree","mytree.root");
+  const char* cwd= gDirectory->GetPath();
+  f_ = new TFile(file.c_str(),"RECREATE");
+  t_ = new TTree("t","Pixel Vertex Testing");
+  t_->Branch("vtx",&nvtx_,"nvtx/I");
+  t_->Branch("vz",&vz_,"vz[nvtx]/D");
+  t_->Branch("vzwt",&vzwt_,"vzwt[nvtx]/D");
+  gDirectory->cd(cwd);
 }
 
 void PixelVertexTest::analyze(
@@ -59,13 +84,25 @@ void PixelVertexTest::analyze(
     cout << "\tvertex: " << tracks[i].vertex()
          << "\timpact parameter: " << tracks[i].d0()<< endl;
     cout << "\tcharge: " << tracks[i].charge()<< endl;
-    trks.push_back( tracks, i );
+    trks.push_back( reco::TrackRef(trackCollection, i) );
   cout <<"------------------------------------------------"<<endl;
   }
   PVPositionBuilder pos;  
-  std::cout << "The average z-position of these tracks is " << pos.average(trks).value() << std::endl;
-  std::cout << "The weighted average z-position of these tracks is " << pos.wtAverage(trks).value() << std::endl;
+  nvtx_ = 1;
+  vz_[0] = pos.average(trks).value();
+  vzwt_[0] = pos.wtAverage(trks).value();
+  t_->Fill();
+  std::cout << "The average z-position of these tracks is " << vz_ << std::endl;
+  std::cout << "The weighted average z-position of these tracks is " << vzwt_ << std::endl;
   
 }
- 
+
+void PixelVertexTest::endJob() {
+  if (t_) t_->Print();
+  if (f_) {
+    f_->Print();
+    f_->Write();
+  }
+}
+
 DEFINE_FWK_MODULE(PixelVertexTest)
