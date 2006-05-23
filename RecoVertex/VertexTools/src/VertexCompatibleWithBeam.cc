c76272e471bfa7fce4c67487354a1c4311983be0
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-05-23 09:12:26 +0000

    checkin

---
+++ RecoVertex/VertexTools/src/VertexCompatibleWithBeam.cc
@@ -0,0 +1,55 @@
+#include "RecoVertex/VertexTools/interface/VertexCompatibleWithBeam.h"
+#include "Geometry/Vector/interface/GlobalPoint.h"
+#include "Geometry/CommonDetAlgo/interface/GlobalError.h"
+#include "RecoVertex/VertexTools/interface/VertexDistance.h"
+#include "RecoVertex/VertexPrimitives/interface/ConvertError.h"
+#include "RecoVertex/VertexTools/interface/BeamSpot.h"
+
+
+VertexCompatibleWithBeam::VertexCompatibleWithBeam(const VertexDistance & d, 
+						   float cut) 
+  : theDistance(d.clone()), theCut(cut) 
+{
+  BeamSpot beamSpot;
+  theBeam = VertexState(beamSpot.position(), beamSpot.error());
+}
+
+
+VertexCompatibleWithBeam::VertexCompatibleWithBeam(
+  const VertexCompatibleWithBeam & other) : 
+  theDistance((*other.theDistance).clone()), 
+  theCut(other.theCut), theBeam(other.theBeam) {}
+
+
+VertexCompatibleWithBeam::~VertexCompatibleWithBeam() {
+  delete theDistance;
+}
+
+
+VertexCompatibleWithBeam & 
+VertexCompatibleWithBeam::operator=(const VertexCompatibleWithBeam & other) 
+{
+  if (this == &other) return *this;
+
+  theDistance = (*other.theDistance).clone();
+  theCut = other.theCut;
+  theBeam = other.theBeam;
+
+  return *this;
+}
+
+
+bool VertexCompatibleWithBeam::operator()(const reco::Vertex & v) const 
+{
+  GlobalPoint p(Basic3DVector<float> (v.position()));
+  VertexState vs(p, RecoVertex::convertError(v.error()));
+  return (theDistance->distance(vs, theBeam).value() < theCut);
+}
+
+
+float VertexCompatibleWithBeam::distanceToBeam(const reco::Vertex & v) const
+{
+  GlobalPoint p(Basic3DVector<float> (v.position()));
+  VertexState vs(p, RecoVertex::convertError(v.error()));
+  return theDistance->distance(vs, theBeam).value();
+}
