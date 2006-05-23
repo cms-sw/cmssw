f67685d582c5c06e71a5f8d56e55739c1f1547d4
Isidro Gonzalez Caballero <sha1-4135548fe47aa946fae7634b28336f3000212c43@cern.ch>
2006-05-23 17:52:10 +0000

    Adopting CMS style rules Some more conversions

--- TrackPropagation/Geant4e/interface/ConvertFromToCLHEP.h
+++ TrackPropagation/Geant4e/interface/ConvertFromToCLHEP.h
@@ -1,13 +1,15 @@
-#ifndef ConvertFromToCLHEP_H
-#define ConvertFromToCLHEP_H
+#ifndef TrackPropagation_ConvertFromToCLHEP_h
+#define TrackPropagation_ConvertFromToCLHEP_h
 
 //CLHEP
 #include "CLHEP/Geometry/Point3D.h"
 #include "CLHEP/Vector/ThreeVector.h"
+#include "CLHEP/Vector/Rotation.h"
 
 //CMS
 #include "Geometry/Vector/interface/GlobalPoint.h"
 #include "Geometry/Vector/interface/GlobalVector.h"
+#include "Geometry/Surface/interface/TkRotation.h"
 
 /** Utilities to convert among CLHEP and CMS points and vectors
  */
@@ -15,26 +17,26 @@
 namespace TrackPropagation {
   /** Convert a CMS GlobalPoint to a CLHEP HepPoint3D
    */
-  static HepPoint3D globalPointToPoint3D(const GlobalPoint& p) {
+  static HepPoint3D globalPointToHepPoint3D(const GlobalPoint& p) {
     return HepPoint3D(p.x(), p.y(), p.z());
   }
 
   /** Convert a CLHEP HepPoint3D to a CMS GlobalPoint 
    */
-  static GlobalPoint point3DToGlobalPoint(const HepPoint3D& p) {
+  static GlobalPoint hepPoint3DToGlobalPoint(const HepPoint3D& p) {
     return GlobalPoint(p.x(), p.y(), p.z());
   }
 
 
   /** Convert a CMS GlobalVector to a CLHEP HepNormal3D
    */
-  static HepNormal3D globalVectorToNormal3D(const GlobalVector& p) {
+  static HepNormal3D globalVectorToHepNormal3D(const GlobalVector& p) {
     return HepNormal3D(p.x(), p.y(), p.z());
   }
 
   /** Convert a CLHEP HepNormal3D to a CMS GlobalVector 
    */
-  static GlobalVector normal3DToGlobalVector(const HepNormal3D& p) {
+  static GlobalVector hepNormal3DToGlobalVector(const HepNormal3D& p) {
     return GlobalVector(p.x(), p.y(), p.z());
   }
 
@@ -43,7 +45,7 @@ namespace TrackPropagation {
 
   /** Convert a CMS GlobalVector to a CLHEP Hep3Vector
    */
-  static Hep3Vector globalVectorTo3Vector(const GlobalVector& p) {
+  static Hep3Vector globalVectorToHep3Vector(const GlobalVector& p) {
     return Hep3Vector(p.x(), p.y(), p.z());
   }
 
@@ -58,7 +60,7 @@ namespace TrackPropagation {
 
   /** Convert a CMS GlobalPoint to a CLHEP Hep3Vector
    */
-  static Hep3Vector globalVectorTo3Vector(const GlobalPoint& p) {
+  static Hep3Vector globalPointToHep3Vector(const GlobalPoint& p) {
     return Hep3Vector(p.x(), p.y(), p.z());
   }
 
@@ -66,6 +68,30 @@ namespace TrackPropagation {
    */
   static GlobalPoint hep3VectorToGlobalPoint(const Hep3Vector& p) {
     return GlobalPoint(p.x(), p.y(), p.z());
+  }
+
+
+
+
+
+
+
+
+
+  /** Convert a CMS TkRotation<float> to a CLHEP HepRotation=G4RotationMatrix
+   */
+  static HepRotation tkRotationFToHepRotation(const TkRotation<float>& tkr) {
+    return HepRotation(Hep3Vector(tkr.xx(),tkr.yx(), tkr.zx()),
+		       Hep3Vector(tkr.xy(),tkr.yy(), tkr.zy()),
+		       Hep3Vector(tkr.xz(),tkr.yz(), tkr.zz()));
+  }
+
+  /** Convert a CLHEP Hep3Vector to a CMS GlobalPoint 
+   */
+  static TkRotation<float> hepRotationToTkRotationF(const HepRotation& r) {
+    return TkRotation<float>(r.xx(), r.xy(), r.xz(),
+			     r.yx(), r.yy(), r.yz(),
+			     r.zx(), r.zy(), r.zz());
   }
 }
 
