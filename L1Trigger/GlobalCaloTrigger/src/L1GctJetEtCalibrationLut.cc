2be5889bca2e2a3359a6ae2c413f1438653c893c
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-05-24 13:08:24 +0000

    First pass at jet calibration LUT

--- L1Trigger/GlobalCaloTrigger/src/L1GctJetEtCalibrationLut.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctJetEtCalibrationLut.cc
@@ -8,16 +8,16 @@ L1GctJetEtCalibrationLut::~L1GctJetEtCalibrationLut()
 {
 }
 
-uint16_t L1GctJetEtCalibrationLut::convertToSixBitRank(uint16_t jetEnergy, uint16_t eta)
+uint16_t L1GctJetEtCalibrationLut::convertToSixBitRank(uint16_t jetEnergy, uint16_t eta) const
 {
   if(jetEnergy < (1 << JET_ENERGY_BITWIDTH))
   {
-    return jetEnergy/16
+    return jetEnergy/16;
   }
   return 63;
 }
 
-uint16_t L1GctJetEtCalibrationLut::convertToTenBitRank(uint16_t jetEnergy, uint16_t eta)
+uint16_t L1GctJetEtCalibrationLut::convertToTenBitRank(uint16_t jetEnergy, uint16_t eta) const
 {
   if(jetEnergy < (1 << JET_ENERGY_BITWIDTH))
   {
