81ee2eabe11e455293a557c637a286278e985b17
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2006-05-24 18:51:24 +0000

    Corrected bug in default constructor

--- L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctRegion.h
@@ -11,56 +11,49 @@
  * 
  */
 
-typedef unsigned long int ULong;
-
 class L1GctRegion
 {
 public:
-	L1GctRegion(ULong data=0);
-	L1GctRegion(int eta, int phi, ULong et, bool mip, bool quiet, bool tauVeto, bool overFlow);
-	~L1GctRegion();
+  L1GctRegion(unsigned long data=0);
+  L1GctRegion(int eta, int phi, unsigned long et, bool mip, bool quiet, bool tauVeto, bool overFlow);
+  ~L1GctRegion();
 
-    	friend std::ostream& operator << (std::ostream& os, const L1GctRegion& reg);
+  friend std::ostream& operator << (std::ostream& os, const L1GctRegion& reg);
 
-    // Getters
-    int eta() const { return m_eta; }   ///< Get the eta number (0-21?) of the region
-    int phi() const { return m_phi; }   ///< Get the phi number (0-17) of the region
-    ULong getEt() const { return myEt.to_ulong(); }
-    bool getMip() const { return myMip; }
-    bool getQuiet() const { return myQuiet; }
-    bool getTauVeto() const { return myTauVeto; }
-    bool getOverFlow() const { return myOverFlow; }
-    
-    // Setters
-    void setEta(int eta) { m_eta = eta; }
-    void setPhi(int phi) { m_phi = phi; }
-    void setEt(ULong et) { /*assert(et < (1 << ET_BITWIDTH));*/ myEt = et; } 
-    void setMip(bool mip) { myMip = mip; }
-    void setQuiet(bool quiet) { myQuiet = quiet; }
-    void setTauVeto(bool tauVeto) { myTauVeto = tauVeto; }
-    void setOverFlow(bool overFlow) { myOverFlow = overFlow; }
+  // Getters
+  int eta() const { return m_eta; }   ///< Get the eta number (0-21?) of the region
+  int phi() const { return m_phi; }   ///< Get the phi number (0-17) of the region
+  unsigned long getEt() const { return m_et.to_ulong(); }
+  bool getMip() const { return m_mip; }
+  bool getQuiet() const { return m_quiet; }
+  bool getTauVeto() const { return m_tauVeto; }
+  bool getOverFlow() const { return m_overFlow; }
 
-    //  ostream& operator << (ostream& os, const L1GctRegion& s);
-    
+  // Setters
+  void setEta(int eta) { m_eta = eta; }
+  void setPhi(int phi) { m_phi = phi; }
+  void setEt(unsigned long et) { /*assert(et < (1 << ET_BITWIDTH));*/ m_et = et; } 
+  void setMip(bool mip) { m_mip = mip; }
+  void setQuiet(bool quiet) { m_quiet = quiet; }
+  void setTauVeto(bool tauVeto) { m_tauVeto = tauVeto; }
+  void setOverFlow(bool overFlow) { m_overFlow = overFlow; }
 
-		
+
 private:
 
-    static const int ET_BITWIDTH = 10;
+  static const int ET_BITWIDTH = 10;
 
-    /// global eta position number of the region (0-21)
-	int m_eta;
-	/// global phi position number of the region (0-17)
-    int m_phi;
+  /// global eta position number of the region (0-21)
+  int m_eta;
+  /// global phi position number of the region (0-17)
+  int m_phi;
 
-	std::bitset<ET_BITWIDTH> myEt;
-	bool myMip;
-	bool myQuiet;
-    bool myTauVeto;
-    bool myOverFlow;
-	
-	
-	
+  std::bitset<ET_BITWIDTH> m_et;
+  bool m_mip;
+  bool m_quiet;
+  bool m_tauVeto;
+  bool m_overFlow;
+
 };
 
 std::ostream& operator << (std::ostream& os, const L1GctRegion& reg);
--- L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctRegion.cc
@@ -3,27 +3,29 @@
 using namespace std;
 
 
-L1GctRegion::L1GctRegion(int eta, int phi, ULong et, bool mip, bool quiet, bool tauVeto, bool overFlow):
-    m_eta(eta),
-    m_phi(phi),
-    myEt(et),
-    myMip(mip),
-    myQuiet(quiet),
-    myTauVeto(tauVeto),
-    myOverFlow(overFlow)
+L1GctRegion::L1GctRegion(int eta, int phi, unsigned long et, bool mip, bool quiet, bool tauVeto, bool overFlow):
+  m_eta(eta),
+  m_phi(phi),
+  m_et(et),
+  m_mip(mip),
+  m_quiet(quiet),
+  m_tauVeto(tauVeto),
+  m_overFlow(overFlow)
 {
 }
 
-L1GctRegion::L1GctRegion(ULong rawData)
+L1GctRegion::L1GctRegion(unsigned long rawData):
+  m_eta(0),
+  m_phi(0),
+  m_mip(false),
+  m_quiet(false)
 {
+  m_et = rawData & 0x3ff;  //will put the first 10 bits of rawData into the Et
 
-    myEt = rawData & 0x3ff;  //will put the first 10 bits of rawData into the Et
-    
-    rawData >>= ET_BITWIDTH;  //shift the remaining bits down to remove the 10 bits of Et
-    
-    myOverFlow = rawData & 0x1; //LSB is now overflow bit
-    myTauVeto = (rawData & 0x2) >> 1; //2nd bit is tauveto
-    
+  rawData >>= ET_BITWIDTH;  //shift the remaining bits down to remove the 10 bits of Et
+
+  m_overFlow = rawData & 0x1; //LSB is now overflow bit
+  m_tauVeto = (rawData & 0x2) >> 1; //2nd bit is tauveto
 }
 
 L1GctRegion::~L1GctRegion()
@@ -32,13 +34,13 @@ L1GctRegion::~L1GctRegion()
 
 std::ostream& operator << (std::ostream& os, const L1GctRegion& reg)
 {
-  os << "Et " << reg.myEt;
+  os << "Et " << reg.m_et;
   os << " Eta " << reg.m_eta;
   os << " Phi " << reg.m_phi;
-  os << " MIP " << reg.myMip;
-  os << " QUIET " << reg.myQuiet;
-  os << " TAU " << reg.myTauVeto;
-  os << " OVF " << reg.myOverFlow << std::endl;
+  os << " MIP " << reg.m_mip;
+  os << " QUIET " << reg.m_quiet;
+  os << " TAU " << reg.m_tauVeto;
+  os << " OVF " << reg.m_overFlow << std::endl;
 
   return os;
 }
