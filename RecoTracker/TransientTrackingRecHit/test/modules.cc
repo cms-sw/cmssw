7c697dfb3f1f9e5c806066e98f079c6d5204f604
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-05-25 13:03:35 +0000

    initial test

---
+++ RecoTracker/TransientTrackingRecHit/test/modules.cc
@@ -0,0 +1,10 @@
+
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+
+#include "RecoTracker/TransientTrackingRecHit/test/TTRHBuilderTest.cc"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(TTRHBuilderTest)
