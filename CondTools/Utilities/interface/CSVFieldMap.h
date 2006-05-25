81c58d5e9fa08daaf3f0e2314e425d1b3f0cceaa
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-05-25 17:33:55 +0000

    add CSVFieldMap and test

---
+++ CondTools/Utilities/interface/CSVFieldMap.h
@@ -0,0 +1,18 @@
+#ifndef CSVFieldMap_h
+#define CSVFieldMap_h
+#include <vector>
+#include <map>
+#include <string>
+#include <typeinfo>
+class CSVFieldMap{
+ public:
+  CSVFieldMap(){}
+  ~CSVFieldMap(){}
+  void push_back(const std::string& fieldName, const std::string& fieldType);
+  std::string fieldName( int idx ) const;
+  const std::type_info& fieldType( int idx ) const;
+  int size() const;
+ private:
+  std::vector< std::pair<std::string, std::string > > m_fieldMap;
+};
+#endif
