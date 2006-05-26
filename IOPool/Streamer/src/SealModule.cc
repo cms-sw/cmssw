59df5f3638c0a87724afe88aa142eaaf441f0213
afaq <sha1-85f47aa78e37d28203e5c3f51090413d817de089@cern.ch>
2006-05-26 19:45:42 +0000

    Added New EventStreamFileWriter, while still keeping older similar StreamConsumerTest

--- IOPool/Streamer/src/SealModule.cc
+++ IOPool/Streamer/src/SealModule.cc
@@ -7,8 +7,12 @@
 #include "IOPool/Streamer/src/EventStreamFileReader.h"
 #include "IOPool/Streamer/interface/HLTInfo.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "IOPool/Streamer/src/EventStreamFileWriter.h"
+//typedef edm::EventStreamFileWriter StreamTestConsumer;
 
 typedef edm::EventStreamingModule<edmtest::TestConsumer> StreamTestConsumer;
+
+using edm::EventStreamFileWriter;
 using stor::FragmentInput;
 using edmtestp::EventStreamFileReader;
 
@@ -16,6 +20,7 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(StreamTestConsumer);
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(FragmentInput);
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(EventStreamFileReader);
+DEFINE_ANOTHER_FWK_MODULE(EventStreamFileWriter);
 
 using namespace edm::serviceregistry;
 using stor::HLTInfo;
