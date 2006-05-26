cc31734e164bb47ac07a66642b70b8d25bf274dd
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2006-05-26 15:28:41 +0000

    Add dictionaries for edm::DetSetVector<SiPixelCluster>

--- DataFormats/SiPixelCluster/src/classes.h
+++ DataFormats/SiPixelCluster/src/classes.h
@@ -16,4 +16,16 @@ namespace {
   }
 }
 
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
+namespace {
+  namespace {
+    edm::Wrapper<SiPixelCluster> dummy0;
+    edm::Wrapper< std::vector<SiPixelCluster>  > dummy1;
+    edm::Wrapper< edm::DetSet<SiPixelCluster> > dummy2;
+    edm::Wrapper< std::vector<edm::DetSet<SiPixelCluster> > > dummy3;
+    edm::Wrapper< edm::DetSetVector<SiPixelCluster> > dummy4;
+  }
+}
+
 #endif // SISTRIPCLUSTER_CLASSES_H
