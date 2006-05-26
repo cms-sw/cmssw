305140b4434cdaf26c80182018fe940e48903ee4
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2006-05-26 02:56:47 +0000

    zdc analysis code included

--- SimG4CMS/Forward/src/module.cc
+++ SimG4CMS/Forward/src/module.cc
@@ -1,6 +1,8 @@
 #include "SimG4CMS/Forward/interface/CastorSD.h"
 #include "SimG4CMS/Forward/interface/TotemSD.h"
+#include "SimG4CMS/Forward/interface/ZdcSD.h"
 #include "SimG4CMS/Forward/interface/TotemTestGem.h"
+#include "SimG4CMS/Forward/interface/ZdcTestAnalysis.h"
 #include "SimG4Core/SensitiveDetector/interface/SensitiveDetectorPluginFactory.h"
 #include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
 #include "PluginManager/ModuleDef.h"
@@ -10,5 +12,7 @@ typedef CastorSD CastorSensitiveDetector;
 DEFINE_SENSITIVEDETECTOR(CastorSensitiveDetector);
 typedef TotemSD TotemSensitiveDetector;
 DEFINE_SENSITIVEDETECTOR(TotemSensitiveDetector);
-
+typedef ZdcSD ZdcSensitiveDetector;
+DEFINE_SENSITIVEDETECTOR(ZdcSensitiveDetector);
+DEFINE_SIMWATCHER (ZdcTestAnalysis);
 DEFINE_SIMWATCHER (TotemTestGem);
