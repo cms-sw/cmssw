57acc4fdae52c314fac1c464fd9980e9d1af5be3
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-05-26 17:53:40 +0000

    add R sensor record

---
+++ CondFormats/DataRecord/interface/CSCRSensorsRcd.h
@@ -0,0 +1,11 @@
+#ifndef CSCRSENSORSRCD_H
+#define CSCRSENSORSRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+/* #include "FWCore/ParameterSet/interface/ParameterSet.h" */
+
+class CSCRSensorsRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCRSensorsRcd> {
+    
+};
+
+#endif
--- CondFormats/DataRecord/interface/CSCZSensorsRcd.h
+++ CondFormats/DataRecord/interface/CSCZSensorsRcd.h
@@ -2,9 +2,6 @@
 #define CSCZSENSORSRCD_H
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-/* #include "FWCore/ParameterSet/interface/ParameterSet.h" */
-
-#include <iostream>
 
 class CSCZSensorsRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCZSensorsRcd> {
     
