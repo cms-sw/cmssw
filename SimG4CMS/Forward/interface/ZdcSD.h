5ac51762a49c7a28e10ec25dc72ca963da4ed5ba
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2006-05-26 01:38:29 +0000

    zdc sensitive detector

---
+++ SimG4CMS/Forward/interface/ZdcSD.h
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+// File: ZdcSD.h
+// Date: 02.04
+// Description: Stores hits of Zdc in appropriate  container
+//
+///////////////////////////////////////////////////////////////////////////////
+#ifndef ZdcSD_h
+#define ZdcSD_h
+#include "SimG4CMS/Calo/interface/CaloSD.h"
+#include "SimG4CMS/Forward/interface/ZdcNumberingScheme.h"
+#undef debug
+
+class ZdcSD : public CaloSD {
+
+public:    
+  ZdcSD(G4String, const DDCompactView &, edm::ParameterSet const &,const SimTrackManager*);
+  virtual ~ZdcSD();
+  void setNumberingScheme(ZdcNumberingScheme* scheme);
+  virtual uint32_t setDetUnitId(G4Step* step);
+  virtual double getEnergyDeposit(G4Step*, edm::ParameterSet const &);
+private:    
+
+  int verbosity;
+  double thFibDir;
+  ZdcNumberingScheme * numberingScheme;
+
+
+};
+
+#endif // ZdcSD_h
