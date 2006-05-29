62b23b2a48c9a5b474e5f5d922e019e167ce686d
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-05-29 18:21:00 +0000

    Factored out of Nodes.cc

--- FWCore/ParameterSet/src/PSetNode.cc
+++ FWCore/ParameterSet/src/PSetNode.cc
@@ -1,6 +1,11 @@
 #include "FWCore/ParameterSet/interface/PSetNode.h"
 #include "FWCore/ParameterSet/interface/Visitor.h"
 #include "FWCore/ParameterSet/interface/ReplaceNode.h"
+#include "FWCore/ParameterSet/interface/Entry.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ProcessDesc.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
 
 using namespace std;
 
@@ -41,6 +46,7 @@ namespace edm {
       return modified_ || CompositeNode::isModified();
     }
 
+
     void PSetNode::replaceWith(const ReplaceNode * replaceNode)
     {
       assertNotModified();
@@ -51,6 +57,56 @@ namespace edm {
       nodes_ = replacement->nodes_;
       setModified(true);
     }
+
+
+    edm::Entry PSetNode::makeEntry() const
+    {
+      // verify that this ia not a process related node
+      if(type()=="process")
+        {
+          throw edm::Exception(errors::Configuration,"PSetError")
+            << "ParameterSet: problem with making a parameter set.\n"
+            << "Attempt to convert process input to ParameterSet";
+        }
+
+      boost::shared_ptr<ParameterSet> psetPtr(new ParameterSet);
+      // do the subnodes
+      CompositeNode::insertInto(*psetPtr);
+      return Entry(*psetPtr, !tracked_);
+    }
+
+    void PSetNode::insertInto(edm::ParameterSet & pset) const
+    {
+      pset.insert(false, name, makeEntry());
+    }
+
+
+
+    void PSetNode::insertInto(edm::ProcessDesc & procDesc) const
+    {
+      procDesc.getProcessPSet()->insert(false, name, makeEntry());
+    }
+
+
+    void PSetNode::fillProcess(edm::ProcessDesc & procDesc) const
+    {
+      if(type()!="process")
+      {
+        throw edm::Exception(errors::Configuration,"PSetError")
+          << "ParameterSet: problem with making a parameter set.\n"
+          << "Attempt to make a ProcessDesc with a PSetNode which is not a process";
+      }
+
+      procDesc.getProcessPSet()->insert(true, "@process_name", edm::Entry(name, true));
+      // insert the subnodes as top-level nodes
+      NodePtrList::const_iterator i(nodes()->begin()),e(nodes()->end());
+      for(;i!=e;++i)
+      {
+         (**i).insertInto(procDesc);
+      }
+
+    }
+
 
   }
 }
