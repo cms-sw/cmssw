1a28d7f2d926981c20519ce60f85589a49599865
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2006-05-30 14:57:23 +0000

    Added the EcalFixedAlphaBetaFitUncalibRecHitProducer module

--- RecoLocalCalo/EcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/EcalRecProducers/src/SealModule.cc
@@ -2,11 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalWeightUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalAnalFitUncalibRecHitProducer.h"
+#include "RecoLocalCalo/EcalRecProducers/interface/EcalFixedAlphaBetaFitUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/ESRecHitProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalWeightUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalAnalFitUncalibRecHitProducer );
+DEFINE_ANOTHER_FWK_MODULE( EcalFixedAlphaBetaFitUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( ESRecHitProducer );
