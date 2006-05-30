4d04d4e8efe78ccdc6d154eeccd01236d4fa5956
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-05-30 16:43:43 +0000

    First release for ECAL TB simulation specific information

---
+++ SimDataFormats/EcalTestBeam/src/PEcalTBInfo.cc
@@ -0,0 +1,75 @@
+//
+// $Id:$
+//
+
+// system include files
+
+// user include files
+#include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+PEcalTBInfo::PEcalTBInfo() {
+  clear();
+}
+
+// PEcalTBInfo::PEcalTBInfo(const PEcalTBInfo& rhs) {
+//    // do actual copying here;
+// }
+
+PEcalTBInfo::~PEcalTBInfo() {
+}
+
+//
+// assignment operators
+//
+// const PEcalTBInfo& PEcalTBInfo::operator=(const PEcalTBInfo& rhs) {
+//   //An exception safe implementation is
+//   PEcalTBInfo temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+
+void PEcalTBInfo::clear() {
+  nCrystal_ = 0;
+
+  etaBeam_ = phiBeam_ = 0.;
+  dXbeam_ = dYbeam_ = 0.;
+
+  evXbeam_ = evYbeam_ = 0.;
+}
+  
+void PEcalTBInfo::setCrystal(int nCrystal) {
+  nCrystal_ = nCrystal;
+}
+
+void PEcalTBInfo::setBeamDirection(double etaBeam, double phiBeam) {
+  etaBeam_ = etaBeam;
+  phiBeam_ = phiBeam;
+}
+
+void PEcalTBInfo::setBeamOffset(double dXbeam, double dYbeam) {
+  dXbeam_ = dXbeam;
+  dYbeam_ = dYbeam;
+}
+
+void PEcalTBInfo::setBeamPosition(double evXbeam, double evYbeam) {
+  evXbeam_ = evXbeam;
+  evYbeam_ = evYbeam;
+}
+
+
---
+++ SimDataFormats/EcalTestBeam/src/classes.h
@@ -0,0 +1,10 @@
+#include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace {
+  namespace {
+    PEcalTBInfo                 theInfo;
+    edm::Wrapper<PEcalTBInfo>   theEcalTBInfo;
+  }
+}
