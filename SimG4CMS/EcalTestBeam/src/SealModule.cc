a72f7c6d37fa80c8140cd6033c9c55221d76cead
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-05-31 09:34:57 +0000

    updated versions for Fake TB hodoscope producer

--- SimG4CMS/EcalTestBeam/src/SealModule.cc
+++ SimG4CMS/EcalTestBeam/src/SealModule.cc
@@ -1,7 +1,10 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
- 
-#include "SimG4CMS/EcalTestBeam/interface/EcalTBMCInfoProducer.h"
 
 DEFINE_SEAL_MODULE () ;
+ 
+#include "SimG4CMS/EcalTestBeam/interface/EcalTBMCInfoProducer.h"
 DEFINE_ANOTHER_FWK_MODULE (EcalTBMCInfoProducer) ;
+
+#include "SimG4CMS/EcalTestBeam/interface/FakeTBHodoscopeRawInfoProducer.h"
+DEFINE_ANOTHER_FWK_MODULE (FakeTBHodoscopeRawInfoProducer) ;
