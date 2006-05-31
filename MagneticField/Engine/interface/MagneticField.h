781a411d05a4e2358009f0f394ab90c1280ed07d
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-05-31 13:42:30 +0000

    add also inKGauss

--- MagneticField/Engine/interface/MagneticField.h
+++ MagneticField/Engine/interface/MagneticField.h
@@ -1,6 +1,15 @@
 #ifndef MagneticField_MagneticField_h
 #define MagneticField_MagneticField_h
 
+/** \class MagneticField
+ *
+ *  Base class for the different implementation of magnetic field engines.
+ *
+ *  $Date: $
+ *  $Revision: $
+ *  \author N. Amapane - CERN
+ */
+
 #include "Geometry/Vector/interface/GlobalVector.h"
 #include "Geometry/Vector/interface/GlobalPoint.h"
 
@@ -9,9 +18,19 @@ class MagneticField
  public:
   MagneticField() {;}
   virtual ~MagneticField() {;}
-  virtual GlobalVector inTesla ( const GlobalPoint& ) const = 0;
-  virtual GlobalVector inInverseGeV( const GlobalPoint& glb) const
-    {return inTesla(glb)*2.99792458e-3;}
+
+  /// Field value ad specified global point, in Tesla
+  virtual GlobalVector inTesla (const GlobalPoint& gp) const = 0;
+
+  /// Field value ad specified global point, in KGauss
+  virtual GlobalVector inKGauss(const GlobalPoint& gp) const {
+    return inTesla(gp) * 10.;
+  }
+
+  /// Field value ad specified global point, in 1/Gev
+  virtual GlobalVector inInverseGeV(const GlobalPoint& gp) const {
+    return inTesla(gp) * 2.99792458e-3;
+  }
 };
 
 #endif
