e63f3935863383416f3fe5182326e11de0f2fad3
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-05-31 09:31:58 +0000

    first releas of fake producer for hodoscope info from the generated vertex

---
+++ SimG4CMS/EcalTestBeam/interface/FakeTBHodoscopeRawInfoProducer.h
@@ -0,0 +1,61 @@
+#ifndef FakeTBHodoscopeRawInfoProducer_H
+#define FakeTBHodoscopeRawInfoProducer_H
+/*
+ * \file FakeTBHodoscopeRawInfoProducer.h
+ *
+ * $Id:$
+ *
+ * Mimic the hodoscope raw information using 
+ * the generated vertex of the test beam simulation 
+ *
+ */
+
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBHodoscopePlaneRawHits.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBHodoscopeRawInfo.h"
+#include "Geometry/EcalTestBeam/interface/EcalTBHodoscopeGeometry.h"
+
+using namespace cms;
+using namespace std;
+
+class FakeTBHodoscopeRawInfoProducer: public edm::EDProducer{
+
+  
+ public:
+  
+  /// Constructor
+  FakeTBHodoscopeRawInfoProducer(const edm::ParameterSet& ps);
+  
+  /// Destructor
+  virtual ~FakeTBHodoscopeRawInfoProducer();
+  
+  /// Produce digis out of raw data
+  void produce(edm::Event & event, const edm::EventSetup& eventSetup);
+  
+  // BeginJob
+  //void beginJob(const edm::EventSetup& c);
+  
+  // EndJob
+  //void endJob(void);
+  
+
+private:
+
+  EcalTBHodoscopeGeometry * theTBHodoGeom_;
+
+  string ecalTBInfoLabel_;
+
+};
+
+#endif
