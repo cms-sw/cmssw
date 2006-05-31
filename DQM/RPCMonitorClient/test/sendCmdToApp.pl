931a782894e430c2fc716a24e98dd7608a0514b5
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-05-31 08:19:57 +0000

    cosmetics

--- DQM/RPCMonitorClient/test/sendCmdToApp.pl
+++ DQM/RPCMonitorClient/test/sendCmdToApp.pl
@@ -7,10 +7,9 @@ $port    = $ARGV[1];
 $cmdFile = $ARGV[2];
                                                                                                                              
 die "The file \"$cmdFile\" does not exist\n" unless -e $cmdFile;
-                                                                                                                             
-$curlCmd = "curl --stderr /dev/null -H \"Content-Type: text/xml\" -H \"Content-Description: SOAP Message\" -H \"SOAPAction:
-urn:xdaq-application:lid=0\" http://$host:$port -d \@$cmdFile";
-                                                                                                                             
+
+$curlCmd = "curl --stderr /dev/null -H \"Content-Type: text/xml\" -H \"Content-Description: SOAP Message\" -H \"SOAPAction: urn:xdaq-application:lid=0\" http://$host:$port -d \@$cmdFile";  
+                                                                                                                           
 open CURL, "$curlCmd|";
                                                                                                                              
 print "Sending command to executive $host:$port ";
