314f5d7f778efe438cbfdac14c762363a3502761
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2006-06-01 07:48:09 +0000

    Remove obsolete methods, move getters to .h.

--- DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h
+++ DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h
@@ -1,12 +1,12 @@
-#ifndef CSCCorrelatedLCTDigi_CSCCorrelatedLCTDigi_h
-#define CSCCorrelatedLCTDigi_CSCCorrelatedLCTDigi_h
+#ifndef CSCDigi_CSCCorrelatedLCTDigi_h
+#define CSCDigi_CSCCorrelatedLCTDigi_h
 
 /**\class CSCCorrelatedLCTDigi
  *
  * Digi for Correlated LCT trigger primitives. 
  *
- * $Date: 2006/05/16 15:22:57 $
- * $Revision: 1.5 $
+ * $Date: 2006/05/18 19:59:33 $
+ * $Revision: 1.6 $
  *
  * \author L. Gray, UF
  */
@@ -17,76 +17,81 @@ class CSCCorrelatedLCTDigi
 {
  public:
   
-
   /// Constructors
+  CSCCorrelatedLCTDigi(const int trknmb, const int valid, const int quality, /// from values
+		       const int keywire, const int strip, const int clct_pattern, /// clct pattern is 4 bit pattern! 
+		       const int bend, const int bx);                        /// (pattern) | (strip_type << 3) 
 
-  CSCCorrelatedLCTDigi(int trknmb, int valid, int quality,       /// from values
-		       int keywire, int strip, int clct_pattern, /// clct pattern is 4 bit pattern! 
-		       int bend, int bx);                        /// (pattern) | (strip_type << 3) 
-  CSCCorrelatedLCTDigi         ();                               /// default
+  CSCCorrelatedLCTDigi();                               /// default
 
+  /// clear this LCT
+  void clear();
 
-  /// Gets
+  /// return track number
+  int getTrknmb()  const { return trknmb; }
 
-  /// return track number number
-  int getTrknmb() const;
   /// return valid pattern bit
-  int getValid() const;  // obsolete, use isValid()
-  bool isValid() const;
+  bool isValid()   const { return valid; }
+
   /// return the 4 bit Correlated LCT Quality
-  int getQuality() const;
+  int getQuality() const { return quality; }
+
   /// return the key wire group
-  int getKwire() const;  // obsolete, use getKeyWG()
-  int getKeyWG() const;
+  int getKeyWG()   const { return keywire; }
+
   /// return the strip
-  int getStrip() const;
-  /// return CLCT pattern number
-  int getCLCTPattern() const;
-  /// return pattern 
-  int getPattern() const;
-  /// return strip type
-  int getStriptype() const; // obsolete, use getStripType()
-  int getStripType() const; 
+  int getStrip()   const { return strip; }
+
+  /// return pattern
+  int getPattern() const { return pattern; }
+
   /// return bend
-  int getBend() const;
+  int getBend()   const  { return bend; }
+
   /// return BX
-  int getBx() const;        // obsolete, use getBX()
-  int getBX() const;
-  
-  /// Prints
+  int getBX()     const { return bx; }
+
+  /// return CLCT pattern number
+  int getCLCTPattern() const { return (pattern & 0x7); }
+
+  /// return strip type
+  int getStripType() const   { return ((pattern & 0x8) >> 3); }
+
+  /// Set track number (1,2) after sorting LCTs.
+  void setTrknmb(const uint16_t number) {trknmb = number;}
 
   /// Print content of correlated LCT digi
   void print() const;
 
   ///Comparison
   bool operator == (const CSCCorrelatedLCTDigi &) const;
-  bool operator != (const CSCCorrelatedLCTDigi &rhs) const { return !(this->operator==(rhs)); }
+  bool operator != (const CSCCorrelatedLCTDigi &rhs) const
+    { return !(this->operator==(rhs)); }
 
  private:
 
   friend class testCSCDigis;
-  uint16_t trknmb;
+  uint16_t valid;
   uint16_t quality;
   uint16_t keywire;
   uint16_t strip;
   uint16_t pattern;
   uint16_t bend;
   uint16_t bx;
-  uint16_t valid;
-
+  uint16_t trknmb;
 };
 
 #include<iostream>
-
-inline std::ostream & operator<<(std::ostream & o, const CSCCorrelatedLCTDigi& digi) {
-  return o << " " << digi.getTrknmb()
-	   << " " << digi.isValid()
-	   << " " << digi.getQuality()
-	   << " " << digi.getKeyWG()
-	   << " " << digi.getStrip()
-           << " " << digi.getCLCTPattern()	 
-	   << " " << digi.getStripType()
-	   << " " << digi.getBend()
-	   << " " << digi.getBX();
+inline std::ostream & operator<<(std::ostream & o,
+				 const CSCCorrelatedLCTDigi& digi) {
+  return o << "CSC LCT #"   << digi.getTrknmb()
+	   << ": Valid = "  << digi.isValid()
+	   << " Quality = " << digi.getQuality() << "\n"
+	   <<"  cathode info: Strip = "    << digi.getStrip()
+	   <<" ("           << ((digi.getStripType() == 0) ? 'D' : 'H') << ")"
+	   << " Bend = "    << ((digi.getBend() == 0) ? 'L' : 'R')
+	   << " Pattern = " << digi.getCLCTPattern() << "\n"
+ 	   <<"    anode info: Key wire = " << digi.getKeyWG()
+	   << " BX = "      << digi.getBX() << "\n";
 }
 #endif
