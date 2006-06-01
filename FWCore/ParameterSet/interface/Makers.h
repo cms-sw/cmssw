5eea818bf70c91491f53ba4d3c2c0500c930233c
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-06-01 03:35:08 +0000

    Scale back makePSet, but we still need it for testing

--- FWCore/ParameterSet/interface/Makers.h
+++ FWCore/ParameterSet/interface/Makers.h
@@ -2,26 +2,15 @@
 #define ParameterSet_Makers_h
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/ProcessDesc.h"
-#include "FWCore/ParameterSet/interface/Nodes.h"
-
 #include "boost/shared_ptr.hpp"
 
-#include <memory>
-#include <map>
 #include <string>
 
 namespace edm {
    namespace pset {
-      typedef std::map<std::string, boost::shared_ptr<edm::ParameterSet> > NamedPSets;
-
       boost::shared_ptr<edm::ParameterSet> 
-      makePSet(const NodePtrList& nodes,
-	       const NamedPSets& blocks = NamedPSets(),
-	       const NamedPSets& psets  = NamedPSets());
+      makePSet(const std::string & s);
 
-      boost::shared_ptr<edm::ProcessDesc> 
-      makeProcess(ParseResults const& parsetree);
    }
 }
 #endif
