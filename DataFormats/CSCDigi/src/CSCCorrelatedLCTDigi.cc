16001e74ab8b9530f36c1711a0ce6bb6ac55c29d
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2006-06-01 07:52:28 +0000

    Move getters to .h, add clear().

--- DataFormats/CSCDigi/src/CSCCorrelatedLCTDigi.cc
+++ DataFormats/CSCDigi/src/CSCCorrelatedLCTDigi.cc
@@ -2,62 +2,51 @@
  *
  * Digi for Correlated LCT trigger primitives.
  *
- * $Date: 2006/04/06 11:18:37 $
- * $Revision: 1.6 $
+ * $Date: 2006/05/18 19:59:33 $
+ * $Revision: 1.7 $
  *
  * \author L.Gray, UF
  */
 
 #include <DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h>
 #include <iostream>
-#include <bitset>
 
-CSCCorrelatedLCTDigi::CSCCorrelatedLCTDigi(int itrknmb, int ivalid, int iquality,
-					   int ikeywire, int istrip, int ipattern,
-					   int ibend, int ibx)
-{
-  trknmb = itrknmb;
-  valid  = ivalid;
+/// Constructors
+CSCCorrelatedLCTDigi::CSCCorrelatedLCTDigi(const int itrknmb, const int ivalid,
+					   const int iquality,
+					   const int ikeywire,
+					   const int istrip,
+					   const int ipattern, const int ibend,
+					   const int ibx) {
+  trknmb  = itrknmb;
+  valid   = ivalid;
   quality = iquality;
   keywire = ikeywire;
-  strip = istrip;
+  strip   = istrip;
   pattern = ipattern;
-  bend = ibend;
-  bx = ibx;
+  bend    = ibend;
+  bx      = ibx;
 }
 
 /// Default
-CSCCorrelatedLCTDigi::CSCCorrelatedLCTDigi()
-{
-  trknmb = 0;
-  valid  = 0;
-  quality = 0;
-  keywire = 0;
-  strip = 0;
-  pattern = 0;
-  bend = 0;
-  bx = 0;
+CSCCorrelatedLCTDigi::CSCCorrelatedLCTDigi() {
+  clear(); // set contents to zero
 }
 
-/// Getters
-int CSCCorrelatedLCTDigi::getTrknmb()      const { return trknmb; }
-int CSCCorrelatedLCTDigi::getValid()       const { return valid;  }
-bool CSCCorrelatedLCTDigi::isValid()       const { return valid;  }
-int CSCCorrelatedLCTDigi::getQuality()     const { return quality;}
-int CSCCorrelatedLCTDigi::getKwire()       const { return keywire;}
-int CSCCorrelatedLCTDigi::getKeyWG()       const { return keywire;}
-int CSCCorrelatedLCTDigi::getStrip()       const { return strip;  }
-int CSCCorrelatedLCTDigi::getPattern() const { return pattern; }
-int CSCCorrelatedLCTDigi::getCLCTPattern() const { return (pattern & 0x7); }
-int CSCCorrelatedLCTDigi::getStriptype()   const { return ((pattern & 0x8) >> 3); }
-int CSCCorrelatedLCTDigi::getStripType()   const { return ((pattern & 0x8) >> 3); }
-int CSCCorrelatedLCTDigi::getBend()        const { return bend;   }
-int CSCCorrelatedLCTDigi::getBx()          const { return bx;     }
-int CSCCorrelatedLCTDigi::getBX()          const { return bx;     }
+/// Clears this LCT.
+void CSCCorrelatedLCTDigi::clear() {
+  trknmb  = 0;
+  valid   = 0;
+  quality = 0;
+  keywire = 0;
+  strip   = 0;
+  pattern = 0;
+  bend    = 0;
+  bx      = 0;
+}
 
 /// Comparison
-bool CSCCorrelatedLCTDigi::operator==(const CSCCorrelatedLCTDigi &rhs) const
-{
+bool CSCCorrelatedLCTDigi::operator==(const CSCCorrelatedLCTDigi &rhs) const {
   return ((trknmb == rhs.trknmb)   && (quality == rhs.quality) &&
 	  (keywire == rhs.keywire) && (strip == rhs.strip)     &&
 	  (pattern == rhs.pattern) && (bend == rhs.bend)       &&
@@ -65,17 +54,19 @@ bool CSCCorrelatedLCTDigi::operator==(const CSCCorrelatedLCTDigi &rhs) const
 }
 
 /// Debug
-
-void CSCCorrelatedLCTDigi::print() const
-{
-  std::cout<< " Track number: " << getTrknmb() 
-	   << " Quality: " << getQuality()
-	   << " Key Wire: " << getKeyWG()
-	   << " Strip: " << getStrip()
-	   << " CLCT Pattern: " << getCLCTPattern()
-	   << " Strip Type: " << ( (getStripType() == 0) ? 'D' : 'H' )
-	   << " Bend: " << ( (getBend() == 0) ? 'L' : 'R' )
-	   << " BX: " << getBX()
-	   << " Valid: " << isValid() << std::endl;
+void CSCCorrelatedLCTDigi::print() const {
+  if (isValid()) {
+    std::cout << "CSC LCT #"        << getTrknmb() 
+	      << ": Valid = "       << isValid()
+	      << " Quality = "      << getQuality()
+	      << " Key Wire = "     << getKeyWG()
+	      << " Strip = "        << getStrip()
+	      << " CLCT Pattern = " << getCLCTPattern()
+	      << " Strip Type = "   << ( (getStripType() == 0) ? 'D' : 'H' )
+	      << " Bend = "         << ( (getBend() == 0) ? 'L' : 'R' )
+	      << " BX = "           << getBX() << std::endl;
+  }
+  else {
+    std::cout << "Not a valid correlated LCT." << std::endl;
+  }
 }
-
