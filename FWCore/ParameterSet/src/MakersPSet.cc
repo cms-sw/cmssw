f1f88651c2f1cc94e7414be3893d89a64e6c89f2
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-06-01 03:35:45 +0000

    We still need a scaled-back version of this method, for testing

---
+++ FWCore/ParameterSet/src/MakersPSet.cc
@@ -0,0 +1,22 @@
+#include "FWCore/ParameterSet/interface/Makers.h"
+#include "FWCore/ParameterSet/interface/parse.h"
+
+namespace edm {
+  namespace pset {
+
+    boost::shared_ptr<edm::ParameterSet>
+    makePSet(const std::string & s)
+    {
+      boost::shared_ptr<edm::ParameterSet> result(new ParameterSet);
+
+      NodePtrListPtr nodeList = edm::pset::fullParse(s);
+      for(NodePtrList::const_iterator listItr = nodeList->begin();
+          listItr != nodeList->end(); ++listItr)
+      { 
+        (**listItr).insertInto(*result);
+      }
+
+      return result;
+    }
+  }
+}
