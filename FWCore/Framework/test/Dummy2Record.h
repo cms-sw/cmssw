6f8a6ecfccdd0b1dccf6c8ae43fb14cbfef2120e
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-06-05 15:01:33 +0000

    first submission

---
+++ FWCore/Framework/test/Dummy2Record.h
@@ -0,0 +1,16 @@
+#ifndef Framework_Dummy2Record_h
+#define Framework_Dummy2Record_h
+/*
+ *  Dummy2Record.h
+ *  EDMProto
+ *
+ *  Created by Chris Jones on 4/4/05.
+ *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class Dummy2Record : public edm::eventsetup::EventSetupRecordImplementation<Dummy2Record> {};
+
+#endif
