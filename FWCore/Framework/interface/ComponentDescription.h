e165b8f9153e589e46cb4ab7369e554ebc4872df
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-06-06 20:46:09 +0000

    added additional provenance information

--- FWCore/Framework/interface/ComponentDescription.h
+++ FWCore/Framework/interface/ComponentDescription.h
@@ -16,25 +16,50 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Thu Dec 15 14:07:57 EST 2005
-// $Id$
+// $Id: ComponentDescription.h,v 1.1 2005/12/16 00:26:57 chrjones Exp $
 //
 
 // system include files
 #include <string>
 
 // user include files
+#include "DataFormats/Common/interface/PassID.h"
+#include "DataFormats/Common/interface/ParameterSetID.h"
+#include "DataFormats/Common/interface/VersionNumber.h"
 
 // forward declarations
 namespace edm {
    namespace eventsetup {
       struct ComponentDescription {
-         std::string label_;
-         std::string type_;
+         std::string label_; // A human friendly string that uniquely identifies the label
+         std::string type_;  // A human friendly string that uniquely identifies the name 
          bool isSource_;
+         
+         // The following set of parameters comes from
+         // DataFormats/Common/interface/ModuleDescription.h
+         // to match and have identical provenance information
+
+         // ID of parameter set of the creator
+         ParameterSetID pid_;
+
+         // the release tag of the executable
+         VersionNumber versionNumber_;
+
+         // the physical process that this program was part of (e.g. production)
+         std::string processName_;
+         
+         // what the heck is this? I think its the version of the processName_
+         // e.g. second production pass
+         PassID pass_;
+         /* ----------- end of provenance information ------------- */
+                     
+         
          ComponentDescription() : isSource_(false){}
+         
          ComponentDescription(const std::string& iType,
                               const std::string& iLabel,
                               bool iIsSource) : label_(iLabel),type_(iType),isSource_(iIsSource){}
+                                
          bool operator<( const ComponentDescription& iRHS) const {
             return (type_ == iRHS.type_) ? (label_ < iRHS.label_) : (type_<iRHS.type_);
          }
