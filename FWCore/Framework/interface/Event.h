43662e3fde17519bd6e7c75ff695aa8bb18a6969
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-06-06 22:31:33 +0000

    Support ProductTags

--- FWCore/Framework/interface/Event.h
+++ FWCore/Framework/interface/Event.h
@@ -70,7 +70,7 @@ Getting a reference to an event product before that product is put into the even
 */
 /*----------------------------------------------------------------------
 
-$Id: Event.h,v 1.32 2006/04/28 17:01:17 paterno Exp $
+$Id: Event.h,v 1.33 2006/05/09 07:48:17 chrjones Exp $
 
 ----------------------------------------------------------------------*/
 #include <cassert>
@@ -95,6 +95,7 @@ $Id: Event.h,v 1.32 2006/04/28 17:01:17 paterno Exp $
 #include "FWCore/Framework/interface/TypeID.h"
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/ParameterSet/interface/ProductTag.h"
 
 namespace edm {
 
@@ -141,6 +142,11 @@ namespace edm {
     template <typename PROD>
     void 
     getByLabel(std::string const& label, const std::string& productInstanceName, Handle<PROD>& result) const;
+
+    /// same as above, but using the ProductTag class
+    template <typename PROD>
+    void
+    getByLabel(ProductTag const& tag, Handle<PROD>& result) const;
 
     template <typename PROD>
     void 
@@ -431,6 +437,14 @@ namespace edm {
     gotProductIDs_.push_back(bh.id());
     convert_handle(bh, result);  // throws on conversion error
   }
+
+  template <typename PROD>
+  void
+  Event::getByLabel(ProductTag const& tag, Handle<PROD>& result) const
+  {
+    getByLabel(tag.label(), tag.instance(), result);
+  }
+
 
   template <typename PROD>
   void 
