57a77805fcc8a7de2b68998951993d53a3d211d5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-06-06 22:25:10 +0000

    Enhance test to use catalog for both sources simultaneously

--- IOPool/SecondaryInput/test/TestSecondaryInput.sh
+++ IOPool/SecondaryInput/test/TestSecondaryInput.sh
@@ -2,9 +2,27 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-rm -f ${LOCAL_TMP_DIR}/SecondaryInputTest.root ${LOCAL_TMP_DIR}/SecondaryInputOther.root
+rm -f ${LOCAL_TMP_DIR}/SecondaryInputTest2.root ${LOCAL_TMP_DIR}/SecondaryInputTest.root ${LOCAL_TMP_DIR}/SecondaryInputOther.root
 rm -f ${LOCAL_TMP_DIR}/SecondaryInputTestCatalog.xml ${LOCAL_TMP_DIR}/SecondaryInputTestCatalog.xml.BAK
-rm -f ${LOCAL_TMP_DIR}/PreSecondaryInputTest.cfg ${LOCAL_TMP_DIR}/SecondaryInputTest.cfg
+rm -f ${LOCAL_TMP_DIR}/PreSecondaryInputTest2.cfg ${LOCAL_TMP_DIR}/PreSecondaryInputTest.cfg ${LOCAL_TMP_DIR}/SecondaryInputTest.cfg
+
+cat > ${LOCAL_TMP_DIR}/PreSecondaryInputTest2.cfg << !
+# Configuration file for PreSecondaryInputTest2
+process TEST = {
+	path p = {Thing, OtherThing}
+	module Thing = ThingProducer {untracked int32 debugLevel = 1}
+	module OtherThing = OtherThingProducer {untracked int32 debugLevel = 1}
+	module output = PoolOutputModule {
+		untracked string fileName = '${LOCAL_TMP_DIR}/SecondaryInputTest2.root'
+		untracked string catalog = '${LOCAL_TMP_DIR}/SecondaryInputTestCatalog.xml'
+		untracked string logicalFileName = 'PoolTest2.root'
+		untracked int32 maxSize = 100000
+	}
+	source = EmptySource {untracked int32 maxEvents = 5}
+	endpath ep = {output}
+}
+!
+cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreSecondaryInputTest2.cfg || die 'Failure using PreSecondaryInputTest2.cfg' $?
 
 cat > ${LOCAL_TMP_DIR}/PreSecondaryInputTest.cfg << !
 # Configuration file for PreSecondaryInputTest 
@@ -18,7 +36,7 @@ process TEST = {
 		untracked string logicalFileName = 'PoolTest.root'
 		untracked int32 maxSize = 100000
 	}
-	source = EmptySource {untracked int32 maxEvents = 5}
+	source = EmptySource {untracked int32 maxEvents = 50}
 	endpath ep = {output}
 }
 !
@@ -27,13 +45,15 @@ cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreSecondaryInputTest.cfg || die 'Failur
 cat > ${LOCAL_TMP_DIR}/SecondaryInputTest.cfg << !
 # Configuration file for SecondaryInputTest
 process PROD  = {
-	source = EmptySource { 
+	source = PoolSource { 
 		untracked int32 maxEvents = 42
+		untracked string catalog = '${LOCAL_TMP_DIR}/SecondaryInputTestCatalog.xml'
+		untracked vstring fileNames = {'PoolTest.root'}
 	}
 	module Thing = SecondaryProducer {
-	       secsource input = PoolRASource  {
+	       secsource input = PoolSource  {
 			untracked string catalog = '${LOCAL_TMP_DIR}/SecondaryInputTestCatalog.xml'
-			untracked vstring fileNames = {'PoolTest.root'}
+			untracked vstring fileNames = {'PoolTest2.root'}
 	       }
 	}
 	module Analysis = EventContentAnalyzer {untracked bool verbose = true}
