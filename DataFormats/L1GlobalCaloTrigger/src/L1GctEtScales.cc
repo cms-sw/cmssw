27800ac0cf42db479e9759b4e43e55739d837816
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2006-06-07 14:16:11 +0000

    added singleton class L1GctEtScales as temp fix for converting to GeV

--- DataFormats/L1GlobalCaloTrigger/src/L1GctEtScales.cc
+++ DataFormats/L1GlobalCaloTrigger/src/L1GctEtScales.cc
@@ -16,20 +16,32 @@ L1GctEtScales::~L1GctEtScales() {
 
 double L1GctEtScales::et(L1GctEmCand cand) {
 
+  // this depends on the RCT, right?
+  
+  return 0.;
+
 }
 
 
 double L1GctEtScales::et(L1GctCenJet cand) {
+
+  // this depends on the GCT Jet Et LUT contents
+
+  return 0.;
 
 }
 
 
 double L1GctEtScales::et(L1GctTauJet cand) {
 
+  return 0.;
+
 }
 
 
 double L1GctEtScales::et(L1GctForJet cand) {
+
+  return 0.;
 
 }
 
