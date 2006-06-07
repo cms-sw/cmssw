ecb055fcaad235777f569d354c91a7ac7d2493a9
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2006-06-07 22:29:20 +0000

    Unpacks new TF DAQ data format now

--- EventFilter/CSCTFRawToDigi/src/CSCTFUnpacker.cc
+++ EventFilter/CSCTFRawToDigi/src/CSCTFUnpacker.cc
@@ -6,7 +6,7 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-//FEDRawData 
+//FEDRawData
 #include "DataFormats/FEDRawData/interface/FEDRawData.h"
 #include "DataFormats/FEDRawData/interface/FEDNumbering.h"
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
@@ -30,7 +30,7 @@
 
 //CSC Track Finder Raw Data Formats // TB and DDU
 #include "TBDataFormats/CSCTFTBRawData/interface/CSCTFTBEventData.h"
-//#include "DataFormats/CSCTFRawData/interface/CSCTFEventData.h"
+#include "DataFormats/CSCTFRawData/interface/CSCTFEvent.h"
 // more to come later
 
 #include <iostream>
@@ -38,7 +38,7 @@
 
 CSCTFUnpacker::CSCTFUnpacker(const edm::ParameterSet & pset)
 {
-  LogDebug("CSCTFUnpacker|ctor") << "starting CSCTFConstructor";   
+  LogDebug("CSCTFUnpacker|ctor") << "starting CSCTFConstructor";
 
   instantiateDQM = pset.getUntrackedParameter<bool>("runDQM", false);
   testBeam = pset.getUntrackedParameter<bool>("TestBeamData",false);
@@ -69,108 +69,133 @@ CSCTFUnpacker::CSCTFUnpacker(const edm::ParameterSet & pset)
   produces<CSCCorrelatedLCTDigiCollection>("MuonCSCTFCorrelatedLCTDigi");
   //produces<CSCTFL1TrackCollection>();
 
-  LogDebug("CSCTFUnpacker|ctor") << "... and finished";  
+  LogDebug("CSCTFUnpacker|ctor") << "... and finished";
 }
 
 CSCTFUnpacker::~CSCTFUnpacker(){
-  
+
   //fill destructor here
-  //delete dccData;   
+  //delete dccData;
   delete TFmapping;
 }
 
 
 void CSCTFUnpacker::produce(edm::Event & e, const edm::EventSetup& c)
 {
-  
+
   //create data pointers, to use later
   // CSCTFDDUEventData * ddu = NULL;
   CSCTFTBEventData *tbdata = NULL;
-  
+
   // Get a handle to the FED data collection
   edm::Handle<FEDRawDataCollection> rawdata;
   e.getByLabel("DaqSource", rawdata);
-  
+
   // create the collection of CSC wire and strip Digis
   std::auto_ptr<CSCCorrelatedLCTDigiCollection> LCTProduct(new CSCCorrelatedLCTDigiCollection);
   //std::auto_ptr<CSCTFL1TrackCollection> trackProduct(new CSCRPCDigiCollection);
-  
+
   for(int fedid = FEDNumbering::getCSCFEDIds().first;
       fedid <= ((testBeam) ? (FEDNumbering::getCSCFEDIds().first) : FEDNumbering::getCSCFEDIds().second);
       ++fedid)
     {
-     
+
       const FEDRawData& fedData = rawdata->FEDData(fedid);
       if(fedData.size())
 	{
-	  
-	  if(testBeam) 
-	    tbdata = new CSCTFTBEventData(reinterpret_cast<unsigned short*>(fedData.data()));
-	  else
-	    edm::LogWarning("CSCTFRawToDigi|produce")<< "New data format not implemented yet, waiting on hardware.";
-	  
+
+	  if(testBeam){
+	    tbdata = new CSCTFTBEventData((unsigned short*)fedData.data());
+
 	  LogDebug("CSCTFUnpacker|produce") << (*tbdata);
-	  
+
 	  ++numOfEvents;
-	  
+
 	  if(instantiateDQM)
-	    { 
+	    {
 	      if(tbdata) monitor->process(*tbdata);
 	      // else not implemented yet.
 	    }
-	  
-	  CSCTFTBFrontBlock aFB;
-	  CSCTFTBSPBlock aSPB;	
-	  CSCTFTBSPData aSPD;
-	for(int BX = 1; BX<= tbdata->eventHeader().numBX() ; ++BX)
-	  {
-	    if(testBeam) aFB = tbdata->frontDatum(BX);
-	    for(int FPGA = 1; FPGA <= tbdata->eventHeader().numMPC() ; ++FPGA)
-	      {		
-		for(int MPClink = 1; MPClink <= tbdata->eventHeader().numLinks() ; ++MPClink)
-		  {		      
-		    if(testBeam)
-		      {		    
-			int subsector = 0;
-			int station = 0;
-			
-			if(FPGA == 1) subsector = 1;
-			if(FPGA == 2) subsector = 2;
-			station = (((FPGA - 1) == 0) ? 1 : FPGA-1);
-			
-			int cscid = aFB.frontData(FPGA,MPClink).CSCIDPacked();
-			if(cscid)
-			  {			    
-			    try 
-			      {
-				CSCDetId id = TFmapping->detId(TBendcap,station,TBsector,subsector,cscid,3);
-				// corrlcts reside on the key layer which is layer 3.
-				LCTProduct->insertDigi(id,aFB.frontDigiData(FPGA,MPClink));
-				LogDebug("CSCUnpacker|produce") << "Unpacked digi: "<< aFB.frontDigiData(FPGA,MPClink);
-			      }
-			    catch(cms::Exception &e)
-			      {
-				edm::LogInfo("CSCTFUnpacker|produce") << e.what() << "Not adding digi to collection in event" 
-								      << tbdata->eventHeader().getLvl1num();
-			      }
-			    }
-			
-		      }
-		    else 
+
+		CSCTFTBFrontBlock aFB;
+		CSCTFTBSPBlock aSPB;
+		CSCTFTBSPData aSPD;
+
+		for(int BX = 1; BX<= tbdata->eventHeader().numBX() ; ++BX)
+		  {
+		    aFB = tbdata->frontDatum(BX);
+		    for(int FPGA = 1; FPGA <= tbdata->eventHeader().numMPC() ; ++FPGA)
 		      {
-			//not implemented yet
+			for(int MPClink = 1; MPClink <= tbdata->eventHeader().numLinks() ; ++MPClink)
+			  {
+				int subsector = 0;
+				int station = 0;
+
+				if(FPGA == 1) subsector = 1;
+				if(FPGA == 2) subsector = 2;
+				station = (((FPGA - 1) == 0) ? 1 : FPGA-1);
+
+				int cscid = aFB.frontData(FPGA,MPClink).CSCIDPacked();
+				if(cscid)
+				  {
+				    try
+				      {
+					CSCDetId id = TFmapping->detId(TBendcap,station,TBsector,subsector,cscid,3);
+					// corrlcts reside on the key layer which is layer 3.
+					LCTProduct->insertDigi(id,aFB.frontDigiData(FPGA,MPClink));
+					LogDebug("CSCUnpacker|produce") << "Unpacked digi: "<< aFB.frontDigiData(FPGA,MPClink);
+				      }
+				    catch(cms::Exception &e)
+				      {
+					edm::LogInfo("CSCTFUnpacker|produce") << e.what() << "Not adding digi to collection in event"
+								      << tbdata->eventHeader().getLvl1num();
+				      }
+				    }
+
+			  }
 		      }
 		  }
-	      }
-	  }
+
+	} else {
+		CSCTFEvent tfEvent;
+		if( tfEvent.unpack((unsigned short*)fedData.data(),fedData.size()/2) )
+			edm::LogError("CSCTFUnpacker|produce")<<" problem of unpacking TF event";
+		else {
+			++numOfEvents;
+			// There may be several SPs in event
+			std::vector<CSCSPEvent> SPs = tfEvent.SPs();
+			// Cycle over all of them
+			for(std::vector<CSCSPEvent>::const_iterator sp=SPs.begin(); sp!=SPs.end(); sp++){
+				for(unsigned int tbin=0; tbin<sp->header().nTBINs(); tbin++)
+					for(unsigned int FPGA=0; FPGA<5; FPGA++)
+						for(unsigned int MPClink=0; MPClink<3; ++MPClink){
+							std::vector<CSCSP_MEblock> lct = sp->record(tbin).LCT(FPGA,MPClink);
+							if( lct.size()==0 ) continue;
+							int station = ( FPGA ? FPGA : 1 );
+							int endcap  = sp->header().endcap();
+							int sector  = sp->header().sector();
+							int subsector = ( FPGA>1 ? 0 : FPGA+1 );
+							int cscid   = lct[0].csc() ;
+
+							try{
+								CSCDetId id = TFmapping->detId(endcap,station,sector,subsector,cscid,3);
+								// corrlcts reside on the key layer which is layer 3.
+								LCTProduct->insertDigi(id,CSCCorrelatedLCTDigi(0,lct[0].vp(),lct[0].quality(),lct[0].wireGroup(),lct[0].strip(),lct[0].pattern(),lct[0].l_r(),lct[0].bx0()));
+							} catch(cms::Exception &e) {
+								edm::LogInfo("CSCTFUnpacker|produce") << e.what() << "Not adding digi to collection in event"<<sp->header().L1A();
+							}
+
+						}
+
+				}
+			}
 	}
-    }
-  
+	}
+
+	} //end of if(fedData.size())
+
   e.put(LCTProduct,"MuonCSCTFCorrelatedLCTDigi"); // put processed lcts into the event.
-  
+
   if(tbdata) delete tbdata;
   tbdata = NULL;
 }
-
-
-
