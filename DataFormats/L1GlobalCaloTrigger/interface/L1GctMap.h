b31ef6d0b937d78960551d6e572989d0809429cb
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2006-06-07 13:34:30 +0000

    committed GctMap as temporary fix

---
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctMap.h
@@ -0,0 +1,48 @@
+#ifndef L1GCTMAP_H
+#define L1GCTMAP_H
+
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctRegion.h"
+
+class L1GctMap {
+ public:
+  
+  /// destructor
+  ~L1GctMap();
+
+  /// return the map
+  static L1GctMap* getMap() { 
+    if (m_instance==0) { m_instance = new L1GctMap(); }
+    return m_instance;
+  }
+  
+  /// get the RCT crate number
+  unsigned rctCrate(L1GctRegion r);
+
+  /// get the SC number
+  unsigned sourceCard(L1GctRegion r);
+
+  /// get the eta index within an RCT crate
+  unsigned rctEta(L1GctRegion r);
+
+  /// get the phi index within an RCT crate
+  unsigned rctPhi(L1GctRegion r);
+
+  /// get physical eta 
+  double eta(L1GctRegion r);
+
+  /// get physical eta 
+  double phi(L1GctRegion r);
+
+
+ private:        // methods
+
+  L1GctMap();    // constructor, this is a singleton
+
+ private:        // data
+
+  L1GctMap * m_instance;  // the instance, this is a singleton
+
+
+};
+
+#endif
---
+++ DataFormats/L1GlobalCaloTrigger/src/L1GctMap.cc
@@ -0,0 +1,42 @@
+
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctMap.h"
+
+/// constructor
+L1GctMap() { 
+
+}
+
+/// destructor
+~L1GctMap() {
+
+}
+
+/// get the RCT crate number
+unsigned rctCrate(L1GctRegion r) {
+
+}
+
+/// get the SC number
+unsigned sourceCard(L1GctRegion r) {
+
+}
+
+/// get the eta index within an RCT crate
+unsigned rctEta(L1GctRegion r) {
+
+}
+
+/// get the phi index within an RCT crate
+unsigned rctPhi(L1GctRegion r) {
+
+}
+
+/// get physical eta 
+double eta(L1GctRegion r) {
+
+}
+
+/// get physical eta 
+double phi(L1GctRegion r) {
+
+}
