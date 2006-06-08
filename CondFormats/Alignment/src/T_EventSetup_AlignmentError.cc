5660eabc2a361fe2be15696a3742ddb9af4356a3
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-06-08 12:05:13 +0000

    Register AlignmentErrors in event setup.

---
+++ CondFormats/Alignment/src/T_EventSetup_AlignmentError.cc
@@ -0,0 +1,19 @@
+// -*- C++ -*-
+//
+// Package:     Alignment
+// 
+// Implementation:
+//     create all the 'infrastructure' needed to get into the Context
+//
+// Author:      Frederic Ronga
+// Created:     Wed Jun  7 09:20:28 CEST 2006
+//
+
+// system include files
+
+// user include files
+#include "CondFormats/Alignment/interface/AlignmentErrors.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+//using Alignments;
+EVENTSETUP_DATA_REG(AlignmentErrors);
