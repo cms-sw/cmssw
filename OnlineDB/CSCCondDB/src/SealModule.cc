bfcca7795501d69adbfc04d0b1331abe5e404155
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-06-09 13:20:20 +0000

    added

---
+++ OnlineDB/CSCCondDB/src/SealModule.cc
@@ -0,0 +1,16 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "OnlineDB/CSCCondDB/interface/CSCAFEBAnalyzer.h"
+#include "OnlineDB/CSCCondDB/interface/CSCAFEBdacAnalyzer.h"
+#include "OnlineDB/CSCCondDB/interface/CSCCompThreshAnalyzer.h"
+#include "OnlineDB/CSCCondDB/interface/CSCCrossTalkAnalyzer.h"
+#include "OnlineDB/CSCCondDB/interface/CSCGainAnalyzer.h"
+#include "OnlineDB/CSCCondDB/interface/CSCNoiseMatrixAnalyzer.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(CSCAFEBAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CSCAFEBdacAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CSCCompThreshAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CSCCrossTalkAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CSCGainAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CSCNoiseMatrixAnalyzer);
