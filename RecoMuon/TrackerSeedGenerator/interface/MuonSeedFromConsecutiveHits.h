128c28052ada2bc0aea2fbc32e00a9e0d254c6f4
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-06-09 15:54:58 +0000

    initial commit

---
+++ RecoMuon/TrackerSeedGenerator/interface/MuonSeedFromConsecutiveHits.h
@@ -0,0 +1,80 @@
+#ifndef TrackerSeedGenerator_MuonSeedFromConsecutiveHits_H
+#define TrackerSeedGenerator_MuonSeedFromConsecutiveHits_H
+
+/**  \class MuonSeedFromConsecutiveHits
+ * 
+ *   Create muon seed from hits in two
+ *   consecutive tracker layers
+ * 
+ *
+ *   $Date:  $
+ *   $Revision:  $
+ *
+ *   \author   N. Neumeister            Purdue University
+ *   \author porting C. Liu             Purdue University
+ */
+
+#include "DataFormats/TrajectorySeed/interface/BasicTrajectorySeed.h"
+
+//------------------------------------
+// Collaborating Class Declarations --
+//------------------------------------
+
+#include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHit.h"
+#include "Geometry/CommonDetAlgo/interface/GlobalError.h"
+#include "TrackingTools/PatternTools/interface/TrajectoryMeasurement.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+
+//              ---------------------
+//              -- Class Interface --
+//              ---------------------
+
+class MuonSeedFromConsecutiveHits : public BasicTrajectorySeed {
+
+  public:
+
+    /// constructors
+    MuonSeedFromConsecutiveHits(const TransientTrackingRecHit& outerHit,
+                                const TransientTrackingRecHit& innerHit,
+                                const PropagationDirection direction,
+                                const GlobalPoint& vertexPos,
+                                const GlobalError& vertexErr,
+                                const edm::EventSetup& iSetup);
+                                
+    /// destructor
+    virtual ~MuonSeedFromConsecutiveHits();
+
+    virtual const FreeTrajectoryState& freeTrajectoryState() const;
+    
+    virtual PTrajectoryStateOnDet startingState() const;
+
+    virtual PropagationDirection direction() const;
+
+    virtual range recHits() const;
+
+    virtual std::vector<TrajectoryMeasurement> measurements() const;
+
+    virtual bool share( const BasicTrajectorySeed&) const;
+
+    virtual BasicTrajectorySeed* clone() const;
+
+  private:
+
+    void construct(const TransientTrackingRecHit& outerHit, 
+                   const TransientTrackingRecHit& innerHit,
+                   const GlobalPoint& vertexPos,
+                   const GlobalError& vertexErr,
+                   const edm::EventSetup& iSetup);
+
+ private :
+
+    bool status;
+    PropagationDirection  theDirection;
+    TrajectoryMeasurement theInnerMeas;
+    TrajectoryMeasurement theOuterMeas;
+
+};
+
+#endif 
+
