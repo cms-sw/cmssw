09ccb4a1df28986df7cf4d54aef94651673dd27d
Maya Stavrianakou <sha1-aa1c8106756b8dcf91c301429c8cb72de2cd2c73@cern.ch>
2006-06-09 15:21:04 +0000

    fixed StackingAction

--- SimG4Core/KillSecondaries/src/KillSecondariesStackingAction.cc
+++ SimG4Core/KillSecondaries/src/KillSecondariesStackingAction.cc
@@ -1,13 +1,24 @@
 #include "SimG4Core/KillSecondaries/interface/KillSecondariesStackingAction.h"
+#include "SimG4Core/Notification/interface/CurrentG4Track.h"
+#include "SimG4Core/Notification/interface/NewTrackAction.h"
 
 #include "G4Track.hh"
 
 G4ClassificationOfNewTrack KillSecondariesStackingAction::ClassifyNewTrack(const G4Track * aTrack) 
 {
-    std::cout << " in KillSecondariesStackingAction::ClassifyNewTrack " << std::endl;
-    int parentId = aTrack->GetParentID();
-    if (parentId != 0) return fKill;
-    else return fUrgent;
+    NewTrackAction newTA;
+    if (aTrack->GetCreatorProcess()==0 || aTrack->GetParentID()==0)     
+    {
+	newTA.primary(aTrack);
+	return fUrgent;
+    }
+    else
+    {
+        const G4Track * mother = CurrentG4Track::track();
+        newTA.secondary(aTrack, *mother);
+	return fKill;
+    }
+
 }
 
 
