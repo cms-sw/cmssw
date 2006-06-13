db10a854c84be674e0f1cda2141458389650d696
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-06-13 11:36:55 +0000

    Temporarily added alignment position error to AlignTransform.

--- CondFormats/Alignment/interface/AlignTransform.h
+++ CondFormats/Alignment/interface/AlignTransform.h
@@ -3,36 +3,49 @@
 #include "CLHEP/Vector/EulerAngles.h"
 #include "CLHEP/Vector/Rotation.h"
 #include "CLHEP/Vector/ThreeVector.h"
+#include "CLHEP/Matrix/SymMatrix.h"   // TEMPORARILY also include alignment error
 #include <boost/cstdint.hpp>
 
 
 /// Class holding data for an Alignment transformation
 /// It contains the raw detector id, its global position and global orientation.
+/// The alignment error is also stored as a temporary workaround.
 /// It is optimized for storage.
-class  AlignTransform {
+class  AlignTransform 
+{
 public:
   typedef CLHEP::HepEulerAngles EulerAngles;
-  typedef CLHEP::Hep3Vector ThreeVector;
-  typedef CLHEP::HepRotation Rotation;
+  typedef CLHEP::Hep3Vector     ThreeVector;
+  typedef CLHEP::HepRotation    Rotation;
+  typedef CLHEP::HepSymMatrix   SymMatrix;
   typedef ThreeVector Translation;
 
-
+  /// Default constructor
   AlignTransform(){}
+
+  /// Constructor from Euler angles and error matrix
   AlignTransform( const Translation & itranslation, 
-		  const EulerAngles & ieulerAngles,
+				  const EulerAngles & ieulerAngles,
+				  const SymMatrix   & ierrorMatrix,
                   const uint32_t & irawId ) :
-    m_translation(itranslation),
+	m_translation(itranslation),
     m_eulerAngles(ieulerAngles),
-    m_rawId(irawId) {}
-  AlignTransform( const Translation & itranslation, 
-		  const Rotation & irotation,
-                  const uint32_t & irawId ) :
-    m_translation(itranslation),
-    m_eulerAngles(irotation.eulerAngles()),
+	m_errorMatrix(ierrorMatrix),
     m_rawId(irawId) {}
   
+  /// Constructor from Rotation and error matrix
+  AlignTransform( const Translation & itranslation, 
+				  const Rotation    & irotation,
+				  const SymMatrix   & ierrorMatrix,
+                  const uint32_t & irawId ) :
+	m_translation(itranslation),
+	m_eulerAngles(irotation.eulerAngles()),
+	m_errorMatrix(ierrorMatrix),
+    m_rawId(irawId) {}
+
   const Translation & translation() const { return m_translation; }
   const EulerAngles & eulerAngles() const { return m_eulerAngles; }
+  const SymMatrix   & errorMatrix() const { return m_errorMatrix; }
   const uint32_t & rawId() const { return m_rawId; }
   Rotation rotation() const { return Rotation(m_eulerAngles); }
 
@@ -40,7 +53,8 @@ private:
 
   Translation m_translation;
   EulerAngles m_eulerAngles;
-  uint32_t m_rawId;
+  SymMatrix   m_errorMatrix;
+  uint32_t    m_rawId;
 
 
 };
