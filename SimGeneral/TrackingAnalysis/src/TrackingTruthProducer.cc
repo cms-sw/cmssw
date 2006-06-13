4dd3e482a1ac250ea03e7265464ce1cd67cd9ae1
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2006-06-13 19:09:11 +0000

    Cleaned up version

--- SimGeneral/TrackingAnalysis/src/TrackingTruthProducer.cc
+++ SimGeneral/TrackingAnalysis/src/TrackingTruthProducer.cc
@@ -25,14 +25,10 @@ TrackingTruthProducer::TrackingTruthProducer(const edm::ParameterSet &conf) {
 }
 
 void TrackingTruthProducer::produce(Event &event, const EventSetup &) {
-  edm::Handle<edm::HepMCProduct>           hepMC;
-  edm::Handle<EmbdSimVertexContainer>      G4VtxContainer;
-  edm::Handle<edm::EmbdSimTrackContainer>  G4TrkContainer;
-  CLHEP::HepLorentzVector                  position;
-  auto_ptr<TrackingVertexContainer>        tVC( new TrackingVertexContainer );  
 
   // Get information out of event record
   
+  edm::Handle<edm::HepMCProduct>           hepMC;
   for (vector<string>::const_iterator source = dataLabels_.begin(); source !=
       dataLabels_.end(); ++source) {
     try {
@@ -43,11 +39,13 @@ void TrackingTruthProducer::produce(Event &event, const EventSetup &) {
       
     }    
   }
+  const edm::HepMCProduct          *mcp =          hepMC.product();
   
+  edm::Handle<EmbdSimVertexContainer>      G4VtxContainer;
+  edm::Handle<edm::EmbdSimTrackContainer>  G4TrkContainer;
   event.getByType(G4VtxContainer);
   event.getByType(G4TrkContainer);
-  const edm::HepMCProduct          *mcp =          hepMC.product();
-  const edm::EmbdSimTrackContainer *etc = G4TrkContainer.product();
+//  const edm::EmbdSimTrackContainer *etc = G4TrkContainer.product();
 
   if (mcp == 0) {
     edm::LogWarning (MessageCategory) << "No HepMC source found";
@@ -55,68 +53,70 @@ void TrackingTruthProducer::produce(Event &event, const EventSetup &) {
   }  
   
   // Find and loop over vertices from HepMC
-  const HepMC::GenEvent *hme = mcp -> GetEvent();
+//  const HepMC::GenEvent *hme = mcp -> GetEvent();
 //  hme -> print();
   
   // Find and loop over EmbdSimVertex vertices
     
+  auto_ptr<TrackingVertexContainer> tVC( new TrackingVertexContainer );  
+
   int index = 0;
   for (edm::EmbdSimVertexContainer::const_iterator itVtx = G4VtxContainer->begin(); 
        itVtx != G4VtxContainer->end(); 
        ++itVtx) {
+    bool InVolume = false;
          
-    position = itVtx -> position();  // Get position of ESV
+    CLHEP::HepLorentzVector position = itVtx -> position();  // Get position of ESV
     math::XYZPoint mPosition = math::XYZPoint(position.x(),position.y(),position.z());
-    int vtxParent = itVtx -> parentIndex(); // Get incoming track (EST)
+//    int vtxParent = itVtx -> parentIndex(); // Get incoming track (EST)
     
-    string InOut;
-    int partHepMC = -1;
+//    int partHepMC = -1;
     
     if (position.perp() < 1200 && abs(position.z()) < 3000) { // In or out of Tracker
-      InOut = "I";
-    } else {
-      InOut = "O";
+      InVolume = true;
     }
     
-    if (vtxParent >= 0) {                     // If there is parent track, figure out HEPMC Vertex 
-      EmbdSimTrack est = etc->at(vtxParent);  // Pull track out from vector
-      partHepMC =     est.genpartIndex(); // Get HepMC particle barcode
-      HepMC::GenParticle *hmp = hme -> barcode_to_particle(partHepMC); // Convert barcode
-      if (hmp != 0) {
-       HepMC::GenVertex *hmpv = hmp -> production_vertex(); 
-       if (hmpv != 0) {
-         int  vb = hmpv  -> barcode();
-       }  
-      }  
-    }  
+//    if (vtxParent >= 0) {                     // If there is parent track, figure out HEPMC Vertex 
+//      EmbdSimTrack est = etc->at(vtxParent);  // Pull track out from vector
+//      partHepMC =     est.genpartIndex(); // Get HepMC particle barcode
+//      HepMC::GenParticle *hmp = hme -> barcode_to_particle(partHepMC); // Convert barcode
+//      if (hmp != 0) {
+//      HepMC::GenVertex *hmpv = hmp -> production_vertex(); 
+//       if (hmpv != 0) {
+//         int  vb = hmpv  -> barcode();
+//       }  
+//      }  
+//    }  
+
+// Find closest vertex to this one
 
     double closest = 9e99;
-    int iTVC = 0;
-    int cTVC = 0;
-    for (TrackingVertexContainer::const_iterator v =
+    TrackingVertexContainer::iterator nearestVertex;
+
+    for (TrackingVertexContainer::iterator v =
         tVC -> begin();
         v != tVC ->end(); ++v) {
       math::XYZPoint vPosition = v->position();   
       double distance = sqrt(pow(vPosition.X()-mPosition.X(),2) +  
                              pow(vPosition.Y()-mPosition.Y(),2) + 
                              pow(vPosition.Z()-mPosition.Z(),2)); 
-      if (distance < closest) {
+      if (distance < closest) { // flag which one so we can associate them
         closest = distance;
-        cTVC = iTVC;
-        // flag which one so we can associate them
+        nearestVertex = v; 
       }   
-      ++iTVC;      
     }
 
+// If outside cutoff, create another TrackingVertex,
+// otherwise add G4 vertex to closest one     
+    
     if (closest > distanceCut_) {
       TrackingVertex tV = TrackingVertex(mPosition);
       tV.addG4Vertex(EmbdSimVertexRef(G4VtxContainer, index) ); // Add G4 vertex
       // Add HepMC vertex
       // Add TrackingParticle (or maybe elsewhere)
       tVC -> push_back(tV);
-      cTVC = tVC->size() - 1;
     } else {
-      (tVC -> at(cTVC)).addG4Vertex(EmbdSimVertexRef(G4VtxContainer, index) ); // Add G4 vertex
+      (*nearestVertex).addG4Vertex(EmbdSimVertexRef(G4VtxContainer, index) ); // Add G4 vertex
       // Add HepMC vertex
       // Add TrackingParticle
     }  
@@ -126,30 +126,30 @@ void TrackingTruthProducer::produce(Event &event, const EventSetup &) {
 
   edm::LogInfo (MessageCategory) << "TrackingTruth found " << tVC->size() << " unique vertices";
   
-  index = 0;
-  for (edm::EmbdSimTrackContainer::const_iterator p = G4TrkContainer->begin(); 
-       p != G4TrkContainer->end(); 
-       ++p) {
-         
-    int partHepMC =    p -> genpartIndex();  
-    HepMC::GenParticle *hmp = hme -> barcode_to_particle(partHepMC);
+//  index = 0;
+//  for (edm::EmbdSimTrackContainer::const_iterator p = G4TrkContainer->begin(); 
+//       p != G4TrkContainer->end(); 
+//       ++p) {
+//         
+//    int partHepMC =    p -> genpartIndex();  
+//    HepMC::GenParticle *hmp = hme -> barcode_to_particle(partHepMC);
     
-    if (hmp != 0) {
-      HepMC::GenVertex *hmpv = hmp -> production_vertex(); 
-      if (hmpv != 0) {
-        int vb = hmpv  -> barcode();
-      }  
-    }  
-    ++index;  
-  }
+//    if (hmp != 0) {
+//      HepMC::GenVertex *hmpv = hmp -> production_vertex(); 
+//      if (hmpv != 0) {
+//        int vb = hmpv  -> barcode();
+//      }  
+//    }  
+//    ++index;  
+//  }
 
-  index = 0;
-  for (TrackingVertexContainer::const_iterator v =
-       tVC -> begin();
-       v != tVC ->end(); ++v) {
+//  index = 0;
+//  for (TrackingVertexContainer::const_iterator v =
+//       tVC -> begin();
+//       v != tVC ->end(); ++v) {
 //    edm::LogInfo (MessageCategory) << "TrackingVertex " << index << " has " << (v -> g4Vertices()).size() << " G4 vertices";
-    ++index;  
-  }        
+//    ++index;  
+//  }        
   
   // Put new info into event record  
   
