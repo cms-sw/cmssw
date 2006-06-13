580ba69a49048b8b739e4566dba686f745d48d8c
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2006-06-13 20:12:24 +0000

    update

--- CondTools/Hcal/test/makeDefaultConsts.sh
+++ CondTools/Hcal/test/makeDefaultConsts.sh
@@ -1,8 +1,10 @@
+
 destination='sqlite:hcal_hardcoded_conditions.db'
+#destination='oracle://devdb10/CMS_COND_HCAL'
 
 hcalCalibrationsCopy pedestals -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_pedestals
-hcalCalibrationsCopy pwidth -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_pedestal_widths
+hcalCalibrationsCopy pwidths -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_pedestal_widths
 hcalCalibrationsCopy gains -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_gains
-hcalCalibrationsCopy gwidth -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_gain_widths
+hcalCalibrationsCopy gwidths -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_gain_widths
 hcalCalibrationsCopy qie -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_qie
 hcalCalibrationsCopy emap -input defaults -output $destination -outputrun 999999 -outputtag hardcoded_emap
