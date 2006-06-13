5eb39735c66c69d0e8c554bd9b8f384721ffa306
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2006-06-13 04:59:16 +0000

    Preliminary TT flag computation. Need to be fixed: several hardcoded parameters and broken for endcaps

--- SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormat.cc
+++ SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormat.cc
@@ -11,15 +11,34 @@ namespace tpg {
 
   std::vector<int> EcalFenixTcpFormat::process(std::vector<int> Et, std::vector<int> fgvb){
     vector<int> out (Et.size());
+    
+    //FIXME: must configurabled
+    const double towerLowThreshold = 2.5; //in GeV
+    const double towerHighThreshold = 5.; //in GeV
 
-    int ttFlag=0;  //to be fixed, taken from Lookup table
+    //FIXME: should not be hardcoded!!
+    //FIXME: value valid only for barrel! (GeV/ADC~0.06 for EE)
+    double adc2GeV = 0.035;
+    
     for (unsigned int i=0; i<Et.size();++i) {
       if (Et[i]>0xFF) Et[i]=0xFF;
-      out[i]=((ttFlag&0x3)<<9)|(Et[i]&0xFF);
+      //computes trigger tower flag:
+      //FIXME: check if < or <=.
+      int ttFlag;
+      double etGeV=Et[i]*adc2GeV;
+      //FIXME: flag code should go to some enum ?
+      if(etGeV<towerLowThreshold){ //Low interest
+	ttFlag = 0;
+      } else if(etGeV < towerHighThreshold){ //Mid interest
+	ttFlag = 0x1;
+      } else{ //etGeV>=towerHighThreshold => High interest
+	ttFlag = 0x3;
+      }
+      
+      out[i]=((ttFlag&0x7)<<9)|(Et[i]&0xFF);
       out[i]=out[i] | fgvb[i]<<8;
     }
     return out;
   }
-
 } /* End of namespace tpg */
 
