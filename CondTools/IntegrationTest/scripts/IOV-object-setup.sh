e35a1f87a960a90d9073e0c5d0758d8f35c8f8f9
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-06-14 07:42:35 +0000

    Updated to IOV object 2.0

--- CondTools/IntegrationTest/scripts/IOV-object-setup.sh
+++ CondTools/IntegrationTest/scripts/IOV-object-setup.sh
@@ -1,4 +1,4 @@
-MAPPING_FILE=${MAPPING_PATH}/CondIOV-mapping-custom.xml
+MAPPING_FILE=${MAPPING_PATH}/CondIOV-mapping-custom_2.0.xml
 OBJECT_LIBRARY=CondCoreIOVService
 OBJECT_NAME=IOV
 # Unneccessary for IOV object
