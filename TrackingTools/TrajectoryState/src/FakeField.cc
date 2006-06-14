dfa24a5651f1f47ec06bd62dba58d9c70107674b
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2006-06-14 14:23:02 +0000

    Not needed anymore - Field in TT

--- TrackingTools/TrajectoryState/src/FakeField.cc
+++
@@ -1,4 +0,0 @@
-#include "TrackingTools/TrajectoryState/interface/FakeField.h"
-
-// FIXME VERTEX leaks memory... only for testing
-MagneticField* TrackingTools::FakeField::Field::theField = new ConcreteField;
