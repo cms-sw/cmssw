56ccd7a8df79254ee4cb3319f18614df2db10b77
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-06-14 06:32:51 +0000

    restored previous version

--- DataFormats/TrackReco/interface/TrackBase.h
+++ DataFormats/TrackReco/interface/TrackBase.h
@@ -8,27 +8,23 @@
  *
  * \author Luca Lista, INFN
  *
- * \version $Id: TrackBase.h,v 1.9 2006/04/28 08:05:25 llista Exp $
+ * \version $Id: TrackBase.h,v 1.12 2006/05/08 07:52:54 llista Exp $
  *
  */
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "DataFormats/Math/interface/Point3D.h"
 #include "DataFormats/Math/interface/Error.h"
-#include "DataFormats/TrackReco/interface/HelixUtils.h"
-#include "DataFormats/TrackReco/interface/HitPattern.h"
+#include "DataFormats/TrackReco/interface/PerigeeParameters.h"
+#include "DataFormats/TrackReco/interface/PerigeeCovariance.h"
 
 namespace reco {
 
   class TrackBase {
   public:
-    /// helix fit parameters
-    typedef helix::Parameters Parameters;
-    /// helix parameters covariance matrix (5x5)
-    typedef helix::Covariance Covariance;
-    /// position-momentum covariance matrix (6x6).
-    /// This type will be replaced by a MathCore symmetric
-    /// matrix, as soon as available
-    typedef math::Error<6>::type PosMomError;
+    /// perigee fit parameters
+    typedef perigee::Parameters Parameters;
+    /// perigee parameters covariance matrix (5x5)
+    typedef perigee::Covariance Covariance;
     /// spatial vector
     typedef math::XYZVector Vector;
     /// point in the space
@@ -38,27 +34,15 @@ namespace reco {
     TrackBase() { }
     /// constructor from fit parameters and error matrix
     TrackBase( double chi2, double ndof, 
+	       int found, int invalid, int lost,
 	       const Parameters &, const Covariance & );
-    /// constructor from cartesian coordinates and covariance matrix.
-    /// notice that the vertex passed must be 
-    /// the point of closest approch to the beamline.
-    TrackBase( double chi2, double ndof, 
-	       int q, const Point & v, const Vector & p, 
-	       const PosMomError & err );
-
-   
-    void setHitPattern(const TrackingRecHitRefVector & hitlist)
-      {
-	hitPattern_.set(hitlist);
-      }
-
     /// chi-squared of the fit
     double chi2() const { return chi2_; }
     /// number of degrees of freedom of the fit
     double ndof() const { return ndof_; }
     /// chi-squared divided by n.d.o.f.
     double normalizedChi2() const { return chi2_ / ndof_; }
-    /// helix fit parameters
+    /// perigee fit parameters
     const Parameters & parameters() const { return par_; }
     /// i-th fit parameter ( i = 0, ... 4 )
     double parameter( int i ) const { return par_( i ); }
@@ -66,34 +50,48 @@ namespace reco {
     const Covariance & covariance() const { return cov_; }
     /// (i,j)-th element of covarianve matrix ( i, j = 0, ... 4 )
     double covariance( int i, int j ) const { return cov_( i, j ); }
+
     /// track electric charge
     int charge() const { return par_.charge(); }
+
     /// track transverse momentum
     double pt() const { return par_.pt(); }
-    /// track impact parameter (distance of closest approach to beamline)
-    double d0() const { return par_.d0(); }
+
+    /// The signed transverse curvature
+    double transverseCurvature() const { return par_.transverseCurvature(); }
     /// track azimutal angle of point of closest approach to beamline
     double phi0() const { return par_.phi0(); }
-    /// e / pt (electric charge divided by transverse momentum)
-    double omega() const { return par_.omega(); }
+    /// polar angle  
+    double theta() const { return par_.theta(); }
+    /// track impact parameter (distance of closest approach to beamline)
+    double d0() const { return par_.d0(); }
     /// z coordniate of point of closest approach to beamline
     double dz() const { return par_.dz(); }
-    /// tangent of the dip angle ( tanDip = pz / pt )
-    double tanDip() const { return par_.tanDip(); }
+
+    /// error on specified element
+    double error( int i ) const { return cov_.error( i ); }
+
+    /// error on signed transverse curvature
+    double transverseCurvatureError() const { return cov_.transverseCurvatureError(); }
+    /// error on theta
+    double thetaError() const { return cov_.thetaError(); }
+    /// error on phi0
+    double phi0Error() const { return cov_.phi0Error(); }
+    /// error on d0
+    double d0Error() const { return cov_.d0Error(); }
+    /// error on dx
+    double dzError() const { return cov_.dzError(); }
+
     /// track momentum vector
     Vector momentum() const { return par_.momentum(); }
     /// position of point of closest approach to the beamline
     Point vertex() const { return par_.vertex(); }
-    //  hit pattern
-    HitPattern hitPattern() const { return hitPattern_; }
-    /// position-momentum error matrix ( 6x6, degenerate )
-    PosMomError posMomError() const;
     /// number of hits found 
-    unsigned short found() const { return hitPattern_.numberOfValidHits(); }
+    unsigned short found() const { return found_; }
     /// number of hits lost
-    unsigned short lost() const { return hitPattern_.numberOfLostHits(); }
+    unsigned short lost() const { return lost_; }
     /// number of invalid hits
-    unsigned short invalid() const { return hitPattern_.numberOfLostHits(); }
+    unsigned short invalid() const { return invalid_; }
     /// momentum vector magnitude
     double p() const { return momentum().R(); }
     /// x coordinate of momentum vector
@@ -106,8 +104,6 @@ namespace reco {
     double phi() const { return momentum().Phi(); }
     /// pseudorapidity of momentum vector
     double eta() const { return momentum().Eta(); }
-    /// polar angle of momentum vector
-    double theta() const { return momentum().Theta(); }
     /// x coordinate of point of closest approach to the beamline
     double x() const { return vertex().X(); }
     /// y coordinate of point of closest approach to the beamline
@@ -118,23 +114,21 @@ namespace reco {
     double rho() const { return d0(); }
 
   private:
-
-    HitPattern hitPattern_;
-
-    
     /// chi-squared
     Double32_t chi2_;
     /// number of degrees of freedom
     Double32_t ndof_;
-    /// helix 5 parameters
+    /// number of hits found
+    unsigned short found_;
+    /// number of hits lost
+    unsigned short lost_;
+    /// number of invalid hits
+    unsigned short invalid_;
+    /// perigee 5 parameters
     Parameters par_;
-    /// helix 5x5 covariance matrix
+    /// perigee 5x5 covariance matrix
     Covariance cov_;
   };
-
-  inline TrackBase::PosMomError TrackBase::posMomError() const { 
-    return helix::posMomError( par_, cov_ ); 
-  }
 
 }
 
