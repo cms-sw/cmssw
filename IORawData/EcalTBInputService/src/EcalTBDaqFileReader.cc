9da99142f866f425129d97b0c9e0924ecc6c7152
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-06-14 12:49:17 +0000

    Decoupled file reading in EcalTBDaqFile

--- IORawData/EcalTBInputService/src/EcalTBDaqFileReader.cc
+++ IORawData/EcalTBInputService/src/EcalTBDaqFileReader.cc
@@ -2,33 +2,37 @@
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
 
 #include "EcalTBDaqFileReader.h"
+#include "EcalTBDaqSimpleFile.h"
+#include "EcalTBDaqRFIOFile.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include<iostream>
 #include<vector>
 #include<fstream>
 #include <string>
 
+
 using namespace std;
 using namespace edm;
 //using namespace raw;
 
-EcalTBDaqFileReader * EcalTBDaqFileReader::instance_=0;
+// EcalTBDaqFileReader * EcalTBDaqFileReader::instance_=0;
 
-EcalTBDaqFileReader * EcalTBDaqFileReader::instance(){
+// EcalTBDaqFileReader * EcalTBDaqFileReader::instance(){
 
-  if (instance_== 0) {
-    instance_ = new EcalTBDaqFileReader();
-  }
-  return instance_;
+//   if (instance_== 0) {
+//     instance_ = new EcalTBDaqFileReader();
+//   }
+//   return instance_;
 
-}
+// }
 
 
-EcalTBDaqFileReader::EcalTBDaqFileReader(): initialized_(false) { 
+EcalTBDaqFileReader::EcalTBDaqFileReader(): initialized_(false), inputFile_(0) { 
 
   LogDebug("EcalTBInputService") << "@SUB=EcalTBDaqFileReader";
-  if(instance_ == 0) instance_ = this;
+  //   if(instance_ == 0) instance_ = this;
 
 }
 
@@ -37,11 +41,11 @@ EcalTBDaqFileReader::~EcalTBDaqFileReader(){
 
   LogDebug("EcalTBInputService") << "@SUB=EcalTBDaqFileReader";
 
-  if(inputFile) {
-    inputFile.close();
-    //delete input_;
+  if(inputFile_) {
+    inputFile_->close();
+    delete inputFile_;
   }
-  instance_=0;
+  //   instance_=0;
 
   //Cleaning the event
   if (cachedData_.fedData)
@@ -61,125 +65,28 @@ bool EcalTBDaqFileReader::isInitialized(){
 void EcalTBDaqFileReader::initialize(const string & file, bool isBinary){
 
   isBinary_ = isBinary;
+  std::string protocol=file.substr( 0, file.find( ":" )) ;
   std::string filename=file.substr( file.find( ":" )+1, file.size());
-
+  
   // case of ASCII input data file
   if (initialized_) {
     //    cout << "EcalTB DaqFileReader was already initialized... reinitializing it " << endl;
-    if(inputFile) {
-      inputFile.close();
+    if(inputFile_) {
+      inputFile_->close();
+      delete inputFile_;
     }
   }
-  
-  if ( isBinary_){
-    // case of binary input data file
-    LogInfo("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::initialize" << "Opening binary data file " << filename;
-    inputFile.open( filename.c_str(), ios::binary );
-    if( inputFile.fail() ) {
-      LogError("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::initialize" << "the input file: " << filename << " is not present. Exiting program... " ;
-      exit(1);
-    }
-  }// end if binary
-  
-  else{
-    LogInfo("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::initialize" << "Opening ASCII file " << filename;
-    inputFile.open(filename.c_str());
-    if( inputFile.fail() ) {
-      LogError("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::initialize" << "the input file: " << filename << " is not present. Exiting program... " ;
-      exit(1);
-    }
-  }// end if not binary
+
+  if (protocol == "file")
+    inputFile_ = dynamic_cast<EcalTBDaqFile*>(new EcalTBDaqSimpleFile(filename, isBinary_));
+  else if (protocol == "rfio")
+    inputFile_ = dynamic_cast<EcalTBDaqFile*>(new EcalTBDaqRFIOFile(filename, isBinary_));
 
   // Initialize cachedData_
   cachedData_.len=0;
   cachedData_.fedData=0;
   initialized_ = true;
 }
-
-
-
-
-
-//Fill the event
-void EcalTBDaqFileReader::getEventTrailer() {
-  
-  if (isBinary_){
-    ulong dataStore=1;
-    
-    // read first words of event, seeking for event size 
-    inputFile.read(reinterpret_cast<char *>(&dataStore),sizeof(ulong));
-    inputFile.read(reinterpret_cast<char *>(&dataStore),sizeof(ulong));
-    inputFile.read(reinterpret_cast<char *>(&dataStore),sizeof(ulong));
-    int size =  dataStore & 0xffffff ;
-//     cout << "[EcalTB DaqFileReader::getEventTrailer] event size masked  "<<  size 
-// 	 << " (max size in byte is " << maxEventSizeInBytes_ << ")"<< endl;
-    if (size > maxEventSizeInBytes_){
-      LogWarning("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::getEventTrailer" << "event size larger than allowed";
-    }
-    
-    inputFile.seekg(-2*4,ios::cur);
-    inputFile.read(reinterpret_cast<char *>(&dataStore),sizeof(ulong));
-    //cout << "is it beginning?\t"<< a << endl;
-    inputFile.seekg(-2*4,ios::cur);
-    
-    // 
-    char   * bufferChar =  new  char [8* size];
-    inputFile.read (bufferChar,size*8);
-    
-    cachedData_.len         = size*8;
-    cachedData_.fedData = reinterpret_cast<unsigned char*>(bufferChar);
-    
-  }// end in case of binary file
-  
-
-  else{
-    string myWord;
-
-    // allocate max memory allowed, use only what needed
-    int len=0;
-    ulong* buf = new ulong [maxEventSizeInBytes_];
-    ulong* tmp=buf;
-
-    // importing data word by word
-    for ( int i=0; i< maxEventSizeInBytes_/4; ++i) {
-      inputFile >> myWord;
-      sscanf( myWord.c_str(),"%x",buf);
-      int val= *buf >> 28;
-
-//       if (i<4)
-// 	cout << " myWord " << myWord << " myWord >> 28 " << val << endl;
-
-
-      if ( len%2!=0 ) {
-	// looking for end of event searching a tag
-	if ( val  == EOE_ ) {
-	  //        cout << " EcalTBDaqFileReader::getEventTrailer EOE_ reached " <<   endl;
-	  len++;
-	  break;
-	}
-      }
-      buf+=1;
-      len++;
-
-    }// end loop importing data word by word
-
-    //    cout << " Number of 32 words in the event " << len << endl;
-    len*=4;
-    //    cout << " Length in bytes " << len << endl;
-
-    cachedData_.len = len;
-    //fedData = reinterpret_cast<unsigned char*>(tmp);
-    cachedData_.fedData = reinterpret_cast<unsigned char*>(tmp);
-    
-
-  }// end in case of ASCII file
-
-}
-
-
-
-
-
 
 bool EcalTBDaqFileReader::fillDaqEventData() {
   //  cout<< "EcalTBDaqFileReader::fillDaqEventData() beginning " << endl;      
@@ -190,19 +97,19 @@ bool EcalTBDaqFileReader::fillDaqEventData() {
     delete[] cachedData_.fedData;
 
   pair<int,int> fedInfo;  //int =FED id, int = event data length 
-
   try 
     {
       
       //checkEndOfEvent()replaced by tru + getEventTrailer();
-      
-      if( checkEndOfFile() ) throw 1;
+
+      if( inputFile_->checkEndOfFile() ) throw 1;
       
       // FedDataPair is struct: event lenght + pointer to beginning of event 
-      getEventTrailer();
+      if (!inputFile_->getEventData(cachedData_))
+	throw 2;
       // extracting header information from event
       setFEDHeader();
-      
+
       fedInfo.first=getFedId();
       fedInfo.second = cachedData_.len;
       
@@ -231,12 +138,13 @@ bool EcalTBDaqFileReader::fillDaqEventData() {
       if (i==1)
 	{
 	  LogInfo("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::fillDaqEventData" << "END OF FILE REACHED. No information read for the requested event";
+	  return false;
 	} 
       else 
 	{
 	  LogError("EcalTBInputService") << "@SUB=EcalTBDaqFileReader::fillDaqEventData" << "unkown exception";
+	  return false;
 	}
-      return false;
     }
 }
 
@@ -273,14 +181,8 @@ void  EcalTBDaqFileReader::setFEDHeader() {
 
 }
 
-
-bool EcalTBDaqFileReader::checkEndOfFile() {
-
-  //  cout << "  EcalTBDaqFileReader::checkEndOfFile " << endl;
-  //unsigned char * buf = new unsigned char;
-
-  bool retval=false;
-  if ( inputFile.eof() ) retval=true;
-  return retval;
+bool EcalTBDaqFileReader::checkEndOfFile() const
+{ 
+  return inputFile_->checkEndOfFile(); 
 }
 
--- IORawData/EcalTBInputService/src/EcalTBDaqFileReader.h
+++ IORawData/EcalTBInputService/src/EcalTBDaqFileReader.h
@@ -1,24 +1,28 @@
 #ifndef EcalTBDaqFileReader_H
 #define EcalTBDaqFileReader_H
 
-#include "IORawData/EcalTBInputService/src/EcalTBDaqFileReader.h"
 
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 
 #include <iosfwd>
 #include <string>
-#include <fstream>
-#include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 //namespace raw {class FEDRawDataCollection; }
 
+//forward declaration
+
+class EcalTBDaqFile;
+
 struct FedDataPair {
   unsigned char* fedData;
   int len;
 };
+
+
 
 class EcalTBDaqFileReader  {
 
@@ -30,31 +34,39 @@ class EcalTBDaqFileReader  {
   virtual ~EcalTBDaqFileReader();
 
   //Return the instance of the reader
-  static EcalTBDaqFileReader * instance();
+  //  static EcalTBDaqFileReader * instance();
 
   //Set the initialization bit
   void setInitialized(bool value);
   //Check if initialized or not
   bool isInitialized();
+
   //Initialization 
   void initialize(const std::string & filename, bool isBinary);
-
-  //Check if the position in file is EOF
-  bool checkEndOfFile();
 
   //Fill Data for an event from input file
   bool fillDaqEventData();
 
+  //Return the DAQ File
+  const EcalTBDaqFile* getDaqFile() const { return inputFile_; }
+
+  //Check if the position in file is EOF
+  bool checkEndOfFile() const;  
+
   //Return cachedData for the event 
-  const FedDataPair& getFedData() { return cachedData_;} 
+  const FedDataPair& getFedData() const { return cachedData_;} 
+
   //Return event FedId
-  int getFedId() {return headValues_[0];}
+  int getFedId() const {return headValues_[0];} 
+
   //Return event number
-  int getEventNumber() {return headValues_[1];}
+  int getEventNumber() const {return headValues_[1];}
+
   //Return event size
-  int getEventLength() {return headValues_[2];}
+  int getEventLength() const {return headValues_[2];}
+
   //Return run number
-  int getRunNumber() {return headValues_[3];}
+  int getRunNumber() const {return headValues_[3];}
 
 protected:
 
@@ -65,22 +77,11 @@ private:
 
   //Fill event header information
   void setFEDHeader();
-  //Seek in the file for the event 
-  void getEventTrailer();
 
-  ifstream inputFile;
-
-  static EcalTBDaqFileReader * instance_;
-
-  //static const int maxEventSizeInBytes_=42640;
-  static const int maxEventSizeInBytes_=100000;
-  static const int EOE_=10;
-  static const int BOE_=5;
+  EcalTBDaqFile* inputFile_;
 
   vector<int> headValues_;
   FedDataPair cachedData_;
-
-
 
 };
 #endif
