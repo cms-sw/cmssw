de8172b05a8958d71849fe835ecdd7f2a6fce5e1
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-06-15 12:45:36 +0000

    first version of test beam readout

---
+++ SimCalorimetry/EcalTestBeamAlgos/interface/EcalTBReadout.h
@@ -0,0 +1,56 @@
+#ifndef EcalTestBeamAlgos_EcalTBReadout_h
+#define EcalTestBeamAlgos_EcalTBReadout_h
+
+/*
+ *
+ * $Id:$
+ *
+ */
+
+#include <string>
+#include <vector>
+
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EcalTrigTowerDetId.h"
+#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloTopology/interface/EcalTrigTowerConstituentsMap.h"
+
+class EcalTBReadout {
+
+public:
+
+  // The following is not yet used, but will be the primary
+  // constructor when the parameter set system is available.
+  //
+  EcalTBReadout(const std::string theEcalTBInfoLabel);
+  ~EcalTBReadout(){};
+
+  /// tell the readout which cells exist
+  void setDetIds(const std::vector<DetId> & detIds) {theDetIds = detIds;}
+
+  /// search for the TT to be read
+  void findTTlist(const int & crysId, const EcalTrigTowerConstituentsMap& etmap);
+
+  /// read only the digis from the selected TT
+  void readOut(EBDigiCollection & input, EBDigiCollection & output, const EcalTrigTowerConstituentsMap& etmap);
+
+  /// master function to be called once per event
+  void performReadout(edm::Event& event, const EcalTrigTowerConstituentsMap & theTTmap, EBDigiCollection & input, EBDigiCollection & output);
+
+private:
+
+  int theTargetCrystal_;
+
+  std::vector<EcalTrigTowerDetId> theTTlist_;
+
+  static const int NCRYMATRIX = 7;
+  
+  std::vector<DetId> theDetIds;
+
+  std::string ecalTBInfoLabel_;
+
+};
+
+#endif 
---
+++ SimCalorimetry/EcalTestBeamAlgos/src/EcalTBReadout.cc
@@ -0,0 +1,140 @@
+#include <algorithm>
+
+#include "SimCalorimetry/EcalTestBeamAlgos/interface/EcalTBReadout.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+
+EcalTBReadout::EcalTBReadout(const std::string theEcalTBInfoLabel) 
+: ecalTBInfoLabel_(theEcalTBInfoLabel) {
+
+  theTargetCrystal_ = -1;
+  theTTlist_.reserve(1);
+
+}
+
+void EcalTBReadout::findTTlist(const int & crysId, const EcalTrigTowerConstituentsMap& etmap) {
+
+  // search for the TT involved in the NCRYMATRIX x NCRYMATRIX
+  // around the target crystal if a new target, otherwise use
+  // the list already filled
+
+  if ( crysId == theTargetCrystal_ ) { return; }
+
+  theTTlist_.clear();
+
+  /// step 1:
+  /// find the correspondence between the crystal identified in module and its DetId
+  /// assuming only 1 SM present
+  
+  EBDetId theTargetId;
+  std::vector<DetId>::const_iterator idItr = theDetIds.begin();
+  unsigned int ncount = 0;
+  bool found = false;
+  
+  while  ( (ncount <= theDetIds.size()) && !found ) 
+  {
+    ++idItr;
+    ++ncount;
+    EBDetId thisEBdetid(idItr->rawId());
+    if (thisEBdetid.ic() == crysId) {
+      theTargetId = thisEBdetid;
+      found = true;
+    }
+  }
+  if ( !found ) {
+    throw cms::Exception("ObjectNotFound", "Ecal TB target crystal not found in geometry");
+    return;
+  }
+  theTargetCrystal_ = theTargetId.ic();
+
+  /// step 2:
+  /// find the crystals in the matrix and fill the TT list
+
+  int myEta = theTargetId.ieta();
+  int myPhi = theTargetId.iphi();
+
+
+  for ( int icrysEta = (myEta-(NCRYMATRIX-1)/2) ; icrysEta <= (myEta+(NCRYMATRIX-1)/2) ; ++icrysEta ) {
+    for ( int icrysPhi = (myPhi-(NCRYMATRIX-1)/2) ; icrysPhi <= (myPhi+(NCRYMATRIX-1)/2) ; ++icrysPhi ) {
+      
+      /// loop on all the valid DetId and search for the good ones
+
+      EBDetId thisEBdetid;
+
+      idItr = theDetIds.begin();
+      ncount = 0;
+      found = false;
+  
+      while  ( (ncount <= theDetIds.size()) && !found ) 
+        {
+          ++idItr;
+          ++ncount;
+          EBDetId myEBdetid(idItr->rawId());
+          if ( (myEBdetid.ieta() == icrysEta) && (myEBdetid.iphi() == icrysPhi) ) {
+            thisEBdetid = myEBdetid;
+            found = true;
+          }
+        }
+
+      if ( found ) {
+
+        EcalTrigTowerDetId thisTTdetId=etmap.towerOf(thisEBdetid);
+
+        LogDebug("EcalDigi") << "Crystal to be readout: sequential id = " << thisEBdetid.ic() << " eta = " << icrysEta << " phi = " << icrysPhi << " from TT = " << thisTTdetId;
+
+        if ( theTTlist_.size() == 0 || ( theTTlist_.size() == 1 && theTTlist_[0] != thisTTdetId )) {
+          theTTlist_.push_back(thisTTdetId);
+        }
+        else {
+          std::vector<EcalTrigTowerDetId>::iterator ttFound = find(theTTlist_.begin(), theTTlist_.end(), thisTTdetId);
+          if ( theTTlist_.size() > 1 && ttFound == theTTlist_.end() && *(theTTlist_.end()) != thisTTdetId ) { 
+            theTTlist_.push_back(thisTTdetId);
+          }
+        }
+      }
+
+    }
+  }
+
+  edm::LogInfo("EcalDigi") << " TT to be read: ";
+  for ( unsigned int i = 0 ; i < theTTlist_.size() ; ++i ) {
+    edm::LogInfo("EcalDigi") << " TT " << i << " " << theTTlist_[i];
+  }
+
+}
+
+void EcalTBReadout::readOut(EBDigiCollection & input, EBDigiCollection & output, const EcalTrigTowerConstituentsMap& etmap) {
+
+  for(EBDigiCollection::const_iterator digiItr = input.begin();
+      digiItr != input.end(); ++digiItr)
+    {
+      EcalTrigTowerDetId thisTTdetId=etmap.towerOf(digiItr->id());
+      std::vector<EcalTrigTowerDetId>::iterator ttFound = find(theTTlist_.begin(), theTTlist_.end(), thisTTdetId);
+      if ((ttFound != theTTlist_.end()) || *(theTTlist_.end()) == thisTTdetId) { 
+        output.push_back(*digiItr);
+      }
+    }
+  edm::LogInfo("EcalDigi") << "Read EB Digis: " << output.size();
+  
+}
+
+void EcalTBReadout::performReadout(edm::Event& event, const EcalTrigTowerConstituentsMap & theTTmap, EBDigiCollection & input, EBDigiCollection & output) {
+
+  // TB readout
+  // step 1: get the target crystal index
+
+  edm::Handle<PEcalTBInfo> theEcalTBInfo;
+  event.getByLabel(ecalTBInfoLabel_,theEcalTBInfo);
+
+  int crysId = theEcalTBInfo->nCrystal();
+
+  // step 2: update (if needed) the TT list to be read
+
+  findTTlist(crysId, theTTmap);
+
+  // step 3: perform the readout
+
+  readOut(input, output, theTTmap);
+
+}
