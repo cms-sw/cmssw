6ed1cac2566a9c1c235e43410af5d6efaefc0ca3
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-06-16 16:43:38 +0000

    Added Other category to allow chaining of ids (for calibration, ZDC, etc)

--- DataFormats/HcalDetId/interface/HcalSubdetector.h
+++ DataFormats/HcalDetId/interface/HcalSubdetector.h
@@ -1,6 +1,8 @@
 #ifndef DATAFORMATS_HCALDETID_HCALSUBDETECTOR_H
 #define DATAFORMATS_HCALDETID_HCALSUBDETECTOR_H 1
 
-enum HcalSubdetector { HcalEmpty=0, HcalBarrel=1, HcalEndcap=2, HcalOuter=3, HcalForward=4, HcalTriggerTower=5, HcalCalibration=6, HcalComposite=7 };
+enum HcalSubdetector { HcalEmpty=0, HcalBarrel=1, HcalEndcap=2, HcalOuter=3, HcalForward=4, HcalTriggerTower=5, HcalOther=7 };
+
+enum HcalOtherSubdetector { HcalOtherEmpty=0, HcalZDC=1, HcalCalibration=2 };
 
 #endif
