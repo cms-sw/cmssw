895c4248779c2b9e7dd7fa87a2e2ee85a4e2670e
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-06-16 09:19:38 +0000

    added noise matrix record

--- CondCore/CSCPlugins/src/plugin.cc
+++ CondCore/CSCPlugins/src/plugin.cc
@@ -15,11 +15,15 @@
 #include "CondFormats/DataRecord/interface/CSCcrosstalkRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
 #include "CondFormats/DataRecord/interface/CSCIdentifierRcd.h"
+#include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
+#include "CondFormats/DataRecord/interface/CSCNoiseMatrixRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCReadoutMapping.h"
 #include "CondFormats/DataRecord/interface/CSCReadoutMappingRcd.h"
+
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(CSCPedestalsRcd,CSCPedestals);
 REGISTER_PLUGIN(CSCGainsRcd,CSCGains);
 REGISTER_PLUGIN(CSCcrosstalkRcd,CSCcrosstalk);
+REGISTER_PLUGIN(CSCNoiseMatrixRcd,CSCNoiseMatrix);
 REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
 REGISTER_PLUGIN(CSCReadoutMappingRcd,CSCReadoutMapping);
