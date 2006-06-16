508e2be57a47e53fa78b32c2d55bd5f1eea72dde
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-06-16 15:46:42 +0000

    Moved from ProductTag

---
+++ FWCore/ParameterSet/src/InputTag.cc
@@ -0,0 +1,45 @@
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/parse.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace edm {
+
+  InputTag::InputTag()
+  : label_(""),
+    instance_("")
+  {
+  }
+
+
+  InputTag::InputTag(const std::string & label, const std::string & instance)
+  : label_(label),
+    instance_(instance)
+  {
+  }
+
+  InputTag::InputTag(const std::string & s) 
+  : label_(""),
+    instance_("")
+  {
+    // string is delimited by colons
+    std::vector<std::string> tokens = edm::pset::tokenize(s, ":");
+    int nwords = tokens.size();
+    if(nwords == 0 || nwords > 2)
+    {
+      throw edm::Exception(errors::Configuration,"InputTag")
+        << "Input tag " << s << " has " << nwords << " tokens";
+    }
+    label_ = tokens[0];
+
+    if(nwords > 1) instance_ = tokens[1];
+  }
+
+
+  std::string InputTag::encode() const
+  {
+    return label_ + ":" + instance_;
+  }
+}
+
+
+
