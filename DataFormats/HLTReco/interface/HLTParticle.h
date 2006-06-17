0d8e3a6afbc5b4feddbc58faae9193c6b982b35b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2006-06-17 21:24:54 +0000

    *** empty log message ***

--- DataFormats/HLTReco/interface/HLTParticle.h
+++ DataFormats/HLTReco/interface/HLTParticle.h
@@ -9,30 +9,30 @@
  *  an HLT filter decision.
  *
  *
- *  $Date: 2006/06/16 18:55:55 $
- *  $Revision: 1.3 $
+ *  $Date: 2006/06/17 04:02:16 $
+ *  $Revision: 1.4 $
  *
  *  \author Martin Grunewald
  *
  */
 
 #include "DataFormats/Candidate/interface/Particle.h"
-#include <cassert>
 
 namespace reco
 {
 
   class HLTParticle : public Particle {
-    // Currently, we re-use the Particle class from the Candidate Model
-    // Later, we may use our own (more compact) particle (base) class
+    // Currently, we extend the Particle class from the Candidate Model with:
 
   protected:
-    char id_; // id of particle (PDG scheme - fundamental particles only!)
+    char id_; // numerical PDG id of particle ("fundamental" particles only!)
 
   public:
     HLTParticle(): Particle(), id_() { }
-    HLTParticle(const Particle& p, int i=0) : Particle(p), id_(i) {
-      assert ( (-128<=i) && (i<=127) ) ; }
+
+    HLTParticle(const Particle& p, int id=0) : Particle(p), id_() {
+      if (id<-100) {id_=-101;} else if (id>+100) {id_=+101;} else {id_=char(id);}
+    }
 
     int id() const { return (int) (id_); }
 
