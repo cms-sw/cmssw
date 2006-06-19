8344019db9caf55070ff253ed4ca28490bffc11b
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-06-19 15:53:35 +0000

    First release

---
+++ SimG4CMS/EcalTestBeam/interface/FakeTBEventHeaderProducer.h
@@ -0,0 +1,58 @@
+#ifndef FakeTBEventHeaderProducer_H
+#define FakeTBEventHeaderProducer_H
+/*
+ * \file FakeTBEventHeaderProducer.h
+ *
+ * $Id: FakeTBEventHeaderProducer.h,v 1.1 2006/05/31 09:31:57 fabiocos Exp $
+ *
+ * Mimic the event header information
+ * for the test beam simulation 
+ *
+ */
+
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBEventHeader.h"
+#include "Geometry/EcalTestBeam/interface/EcalTBHodoscopeGeometry.h"
+
+using namespace cms;
+using namespace std;
+
+class FakeTBEventHeaderProducer: public edm::EDProducer{
+
+  
+ public:
+  
+  /// Constructor
+  FakeTBEventHeaderProducer(const edm::ParameterSet& ps);
+  
+  /// Destructor
+  virtual ~FakeTBEventHeaderProducer();
+  
+  /// Produce digis out of raw data
+  void produce(edm::Event & event, const edm::EventSetup& eventSetup);
+  
+  // BeginJob
+  //void beginJob(const edm::EventSetup& c);
+  
+  // EndJob
+  //void endJob(void);
+  
+
+private:
+
+  string ecalTBInfoLabel_;
+
+};
+
+#endif
---
+++ SimG4CMS/EcalTestBeam/src/FakeTBEventHeaderProducer.cc
@@ -0,0 +1,53 @@
+/*
+ * \file FakeTBEventHeaderProducer.cc
+ *
+ * $Id: FakeTBEventHeaderProducer.cc,v 1.2 2006/06/05 13:57:23 fabiocos Exp $
+ *
+ */
+
+#include "SimG4CMS/EcalTestBeam/interface/FakeTBEventHeaderProducer.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+
+FakeTBEventHeaderProducer::FakeTBEventHeaderProducer(const edm::ParameterSet& ps) {
+  produces<EcalTBEventHeader>();
+  ecalTBInfoLabel_ = ps.getUntrackedParameter<string>("EcalTBInfoLabel","SimEcalTBG4Object");
+
+}
+
+ 
+FakeTBEventHeaderProducer::~FakeTBEventHeaderProducer() 
+{
+}
+
+ void FakeTBEventHeaderProducer::produce(edm::Event & event, const edm::EventSetup& eventSetup)
+{
+  auto_ptr<EcalTBEventHeader> product(new EcalTBEventHeader());
+
+  // get the vertex information from the event
+
+  const PEcalTBInfo* theEcalTBInfo=0;
+  try
+    {
+      edm::Handle<PEcalTBInfo> EcalTBInfo;
+      event.getByLabel(ecalTBInfoLabel_,EcalTBInfo);
+      theEcalTBInfo = EcalTBInfo.product(); 
+    }
+  catch ( std::exception& ex ) 
+    {
+      //edm::LogError("EcalRecHitError") << "Error! can't get the product " << EBuncalibRecHitCollection_.c_str() ;
+    }
+
+  if (!theEcalTBInfo)
+    return;
+  
+  product->setEventNumber(event.id().event());
+  product->setBurstNumber(1);
+  product->setTriggerMask(0x1);
+  product->setCrystalInBeam(EBDetId(1,theEcalTBInfo->nCrystal(),EBDetId::SMCRYSTALMODE));
+  
+//   LogDebug("FakeTBHeader") << (*product);
+//   LogDebug("FakeTBHeader") << (*product).eventType();
+//   LogDebug("FakeTBHeader") << (*product).crystalInBeam();
+  event.put(product);
+  
+}
