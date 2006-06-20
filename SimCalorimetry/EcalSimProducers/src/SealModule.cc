f0ae4909344e405623112dc7030f0925add2126b
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-06-20 16:30:20 +0000

    obsolete test code removed

--- SimCalorimetry/EcalSimProducers/src/SealModule.cc
+++ SimCalorimetry/EcalSimProducers/src/SealModule.cc
@@ -6,9 +6,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "SimCalorimetry/EcalSimProducers/interface/EcalDigiProducer.h"
-#include "SimCalorimetry/EcalSimProducers/interface/EcalDigiTester.h"
 
 DEFINE_SEAL_MODULE () ;
 DEFINE_ANOTHER_FWK_MODULE (EcalDigiProducer) ;
-DEFINE_ANOTHER_FWK_MODULE (EcalDigiTester) ;
 
