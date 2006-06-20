9459307330ed3699ac18d45307ae7f12d12f9c0d
Oliver Gutsche <sha1-2307d037a116d9bb0c52272fc9aa8ec9cc58a51e@cern.ch>
2006-06-20 09:09:19 +0000

    simple track parameter reader

---
+++ RecoTracker/RoadSearchHelixMaker/test/RoadSearchTrackReaderAlgorithm.cc
@@ -0,0 +1,46 @@
+//
+// Package:         RecoTracker/RoadSearchHelixMaker/test
+// Class:           RoadSearchTrackReaderAlgorithm
+// 
+// Description:     read and analyze Tracks
+//
+// Original Author: Oliver Gutsche, gutsche@fnal.gov
+// Created:         Wed Mar 29 20:00:00 UTC 2006
+//
+// $Author: gutsche $
+// $Date: 2006/03/29 20:10:38 $
+// $Revision: 1.1 $
+//
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#include "RecoTracker/RoadSearchHelixMaker/test/RoadSearchTrackReaderAlgorithm.h"
+
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+RoadSearchTrackReaderAlgorithm::RoadSearchTrackReaderAlgorithm(const edm::ParameterSet& conf) : conf_(conf) { 
+}
+
+RoadSearchTrackReaderAlgorithm::~RoadSearchTrackReaderAlgorithm() {
+}
+
+
+void RoadSearchTrackReaderAlgorithm::run(const reco::TrackCollection* input)
+{
+  
+  edm::LogInfo("RoadSearch") << "number of Tracks: " << input->size();
+  
+  for ( reco::TrackCollection::const_iterator track = input->begin(); track != input->end(); ++track ) {
+    edm::LogInfo("RoadSearch") << "Perigee-Parameter: transverseCurvature: " << track->transverseCurvature();
+    edm::LogInfo("RoadSearch") << "Perigee-Parameter: theta: " << track->theta();
+    edm::LogInfo("RoadSearch") << "Perigee-Parameter: phi0: " << track->phi0();
+    edm::LogInfo("RoadSearch") << "Perigee-Parameter: d0: " << track->d0();
+    edm::LogInfo("RoadSearch") << "Perigee-Parameter: dz: " << track->dz();
+  }
+  
+};
---
+++ RecoTracker/RoadSearchHelixMaker/test/SealModules.cc
@@ -0,0 +1,12 @@
+
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoTracker/RoadSearchHelixMaker/test/RoadSearchTrackReader.h"
+
+using cms::RoadSearchTrackReader;
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(RoadSearchTrackReader)
+
