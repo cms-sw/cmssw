7d3504f4a677602b00b2c2da8d523b420180eed7
Christos Leonidopoulos <sha1-53cc0b6cc69397b8d4d0d7155db624dc88e57885@cern.ch>
2006-06-21 12:40:04 +0000

    removed option of specifying profile uncertainty in profile collations (to be determined by first profile to be received at client)

--- DQMServices/Core/interface/MonitorUserInterface.h
+++ DQMServices/Core/interface/MonitorUserInterface.h
@@ -205,18 +205,15 @@ class MonitorUserInterface : public StringUtil
 					    const std::string title, 
 					    const std::string pathname)=0;
   // collate profiles, store in <pathname>;
-  // option is one of: " ", "s" (default), "i", "G" (see TProfile::BuildOptions)
   virtual CollateMonitorElement* collateProf(const std::string name, 
 					     const std::string title, 
-					     const std::string pathname,
-					     char * option = "s")=0;
+					     const std::string pathname)=0;
   
   // collate profiles, store in <pathname>;
-  // option is one of: " ", "s" (default), "i", "G" (see TProfile::BuildOptions)
   virtual CollateMonitorElement* collateProf2D(const std::string name, 
 					       const std::string title, 
-					       const std::string pathname,
-					       char * option = "s") = 0;
+					       const std::string pathname)
+    = 0;
   
   // add <search_string> to summary ME; 
   // <search_string> could : (a) be exact pathname (e.g. A/B/C/histo)
