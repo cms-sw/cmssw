6472eb21970866dd052b229bde63bf78cb41f75a
xhuang <sha1-4bd1aee960316b9472cdc249dc0e7db0c29cbfd7@cern.ch>
2006-06-22 20:35:54 +0000

    first version

---
+++ Validation/EcalHits/test/DoCompare.C
@@ -0,0 +1,137 @@
+void DoCompare( char* Energy ){
+
+ static const int NHisto = 34;
+ static const int NHisto2 = 4;
+ static const int NHisto3 = 2;
+
+ TText* te = new TText();
+ te->SetTextSize(0.1);
+
+ gROOT->ProcessLine(".x HistoCompare.C");
+ HistoCompare * myPV = new HistoCompare();
+
+ char*  reffilename  = "${REFFILE}";//"../data/EcalSimHitHisto_30GeV.root";
+ char*  curfilename  = "${CURFILE}";//"../data/EcalSimHitHisto_30GeV.root";
+
+ TFile * reffile = new TFile(reffilename);
+ TFile * curfile = new TFile(curfilename);
+
+ //1-Dimension Histogram
+ char* label[NHisto];
+ label[0] = "Barrel_E1x1";
+ label[1] = "Barrel_E2x2";
+ label[2] = "Barrel_E3x3";
+ label[3] = "Barrel_E4x4";
+ label[4] = "Barrel_E5x5";
+ label[5] = "Barrel_E1OverE4";
+ label[6] = "Barrel_E4OverE9";
+ label[7] = "Barrel_E9OverE16";
+ label[8] = "Barrel_E16OverE25";
+ label[9] = "Barrel_E1OverE25";
+ label[10] = "Barrel_E9OverE25";
+ label[11] = "Ecal_EBOverETotal";
+ label[12] = "Ecal_EEOverETotal";
+ label[13] = "Ecal_EPOverETotal";
+ label[14] = "Ecal_EBEEEPOverETotal";
+ label[15] = "PreShower_EHit_L1zp";
+ label[16] = "PreShower_EHit_L2zp";
+ label[17] = "Preshower_NHit_L1zp";
+ label[18] = "Preshower_NHit_L2zp";
+ label[19] = "Endcap_E1x1";
+ label[20] = "Endcap_E2x2";
+ label[21] = "Endcap_E3x3";
+ label[22] = "Endcap_E4x4";
+ label[23] = "Endcap_E5x5";
+ label[24] = "Endcap_E1OverE4";
+ label[25] = "Endcap_E4OverE9";
+ label[26] = "Endcap_E9OverE16";
+ label[27] = "Endcap_E16OverE25";
+ label[28] = "Endcap_E1OverE25";
+ label[29] = "Endcap_E9OverE25";
+ label[30] = "PreShower_EHit_L1zm";
+ label[31] = "PreShower_EHit_L2zm";
+ label[32] = "Preshower_NHit_L1zm";
+ label[33] = "Preshower_NHit_L2zm";
+
+ TH1F* htemp1[NHisto];
+ TH1F* htemp2[NHisto];
+ for ( int i = 0; i< NHisto ; i++ ) {
+   char title[50];
+   TCanvas c1;
+   htemp1[i]  = dynamic_cast<TH1F*>(reffile->Get(label[i]));
+   htemp2[i]  = dynamic_cast<TH1F*>(curfile->Get(label[i]));
+   assert(htemp1[i] != 0);
+   assert(htemp2[i] != 0);
+   htemp1[i]->SetLineColor(2);
+   htemp2[i]->SetLineColor(4);
+   htemp1[i]->SetLineStyle(3);
+   htemp2[i]->SetLineStyle(5);
+  
+   htemp1[i]->Draw();
+   htemp2[i]->Draw("Same"); 
+   myPV->PVCompute(htemp1[i],htemp2[i], te);
+   sprintf(title,"%s%s%s", Energy, label[i],".eps");
+   c1.Print(title);
+ }
+
+ 
+ //2-Dimention Histograms
+ char* label2[NHisto2];
+ label2[0] = "Barrel_Longitudinal";
+ label2[1] = "Barrel_Occupancy";
+ label2[2] = "Endcap_Longitudinal";
+ label2[3] = "Endcap_Occupancy";
+
+ TH2F* h2temp1[NHisto2];
+ TH2F* h2temp2[NHisto2];
+
+ for ( int i = 0; i< NHisto2 ; i++ ) {
+   char title[50];
+   TCanvas c1;
+   c1.Divide(2,1);
+
+   h2temp1[i]  = dynamic_cast<TH2F*>(reffile->Get(label2[i]));
+   h2temp2[i]  = dynamic_cast<TH2F*>(curfile->Get(label2[i]));
+   assert(h2temp1[i] != 0);
+   assert(h2temp2[i] != 0);
+   c1.cd(1);
+   h2temp1[i]->Draw();
+   c1.cd(2);
+   h2temp2[i]->Draw();
+   myPV->PVCompute(h2temp1[i],h2temp2[i], te);
+   sprintf(title,"%s%s%s", Energy, label2[i],".eps");
+   c1.Print(title);
+ }
+
+
+
+
+ //TProfiles
+  char* label3[NHisto3];
+  label3[0] = "Preshower_EEOverES_zp";
+  label3[1] = "Preshower_EEOverES_zm";
+
+ TProfile* hpro1[NHisto3];
+ TProfile* hpro2[NHisto3];
+
+ for ( int i = 0; i< NHisto3 ; i++ ) {
+   char title[50];
+   TCanvas c1;
+   c1.Divide(2,1);
+
+   hpro1[i]  = dynamic_cast<TProfile*>(reffile->Get(label3[i]));
+   hpro2[i]  = dynamic_cast<TProfile*>(curfile->Get(label3[i]));
+   assert(hpro1[i] != 0);
+   assert(hpro2[i] != 0);
+   c1.cd(1);
+   hpro1[i]->Draw();
+   c1.cd(2);
+   hpro2[i]->Draw();
+   myPV->PVCompute(hpro1[i],hpro2[i], te);
+   sprintf(title,"%s%s%s", Energy, label3[i],".eps");
+   c1.Print(title);
+ }
+
+}
+
+
