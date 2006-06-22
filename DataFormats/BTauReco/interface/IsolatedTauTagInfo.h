c57e982ca8daef746267b9420b0ce8b574f5dcbd
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2006-06-22 09:32:23 +0000

    *** empty log message ***

--- DataFormats/BTauReco/interface/IsolatedTauTagInfo.h
+++ DataFormats/BTauReco/interface/IsolatedTauTagInfo.h
@@ -1,14 +1,12 @@
 #ifndef BTauReco_TauTagIsolation_h
 #define BTauReco_TauTagIsolation_h
 //
-// \class TauJetTagIsolation
-// \short concrete class for the description of the result of the tau-tagging with the isolation algorithm 
-//
-// concrete class inherits from JetTag
-// contains the result of the tracker based tau-tagging algorithm 
+// \class IsolatedTauTagInfo
+// \short Extended object for the Tau Isolation algorithm.
+// contains the result and the methods used in the ConeIsolation Algorithm, to create the 
 // object to be made persistent on RECO
 //
-// \author: Marcel Vos and Simone Gennai, based on ORCA class by S. Gennai and F. Moortgat
+// \author: Simone Gennai, based on ORCA class by S. Gennai and F. Moortgat
 //
 
 
@@ -42,7 +40,10 @@ struct SortByDescendingTrackPt
   class IsolatedTauTagInfo{
 
   public:
+    //default constructor
     IsolatedTauTagInfo() {}
+
+
     IsolatedTauTagInfo(edm::RefVector<TrackCollection> tracks) 
       {    
 	track_iterator it = tracks.begin();
@@ -52,31 +53,36 @@ struct SortByDescendingTrackPt
 	  }
 	
     }
-    
+    //destructor
     virtual ~IsolatedTauTagInfo() {};
-    
+    //method used to set the reference to the JetTag
     void setJetTag(const JetTagRef myRef) { 
        m_jetTag = myRef;
      }
+    //get the jet from the jetTag
     const Jet & jet() const { return m_jetTag->jet(); }
     
+    //get the tracks from the jetTag
     const TrackRefVector & allTracks() const { return (*m_jetTag).tracks(); }
 
-
+    //get the selected tracks used to computed the isolation
     const TrackRefVector & selectedTracks() const {return selectedTracks_;}
     
+    //get the reference to the jet
     const JetTagRef & jetRef() const { return m_jetTag; }
   
+    //default discriminator: returns the value of the discriminator of the jet tag, i.e. the one computed with the parameters taken from the cfg file
      double discriminator() const { 
        double myDiscr = m_jetTag->discriminator();
        return myDiscr; }
+     //methids to be used to recomputed the isolation with a new set of parameters
      double discriminator(float m_cone, float sig_cone, float iso_con, float pt_min_lt, float pt_min_tk, int nTracksIsoRing=0) const;
      double discriminator( math::XYZVector myVector, float m_cone, float sig_cone, float iso_con, float pt_min_lt, float pt_min_tk, int nTracksIsoRing=0) const;
     virtual IsolatedTauTagInfo* clone() const { return new IsolatedTauTagInfo( *this ); }
     // return all tracks in a cone of size "size" around a direction "direction" 
     const edm::RefVector<TrackCollection> tracksInCone(const math::XYZVector myVector,const float size,  const float pt_min ) const;
  
-    // matchingConeSize_  centred on the jet direction
+    //return the leading track in a given cone around the jet axis or a given direction
     const TrackRef leadingSignalTrack(const float rm_cone, const float pt_min) const;
     const TrackRef leadingSignalTrack(math::XYZVector myVector, const float rm_cone, const float pt_min) const;
     
