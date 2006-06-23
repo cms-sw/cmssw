cfe302cc6c35a1554bef956f24ee33dc994417c8
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2006-06-23 09:20:41 +0000

    - oopps ...missed two files ...

---
+++ DQM/EcalBarrelMonitorClient/interface/EBBeamCaloClient.h
@@ -0,0 +1,98 @@
+#ifndef EBBeamCaloClient_H
+#define EBBeamCaloClient_H
+
+/*
+ * \file EBBeamCaloClient.h
+ *
+ * $Date: 2006/06/22 14:47:05 $
+ * $Revision: 1.11 $
+ * \author G. Della Ricca
+ * \author F. Cossutti
+ *
+*/
+
+#include <vector>
+#include <string>
+
+#include "TROOT.h"
+#include "TProfile2D.h"
+#include "TH1F.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DQM/EcalBarrelMonitorClient/interface/EBClient.h"
+#include "OnlineDB/EcalCondDB/interface/EcalCondDBInterface.h"
+#include "OnlineDB/EcalCondDB/interface/MonRunIOV.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "DQMServices/Core/interface/MonitorUserInterface.h"
+#include "DQMServices/Core/interface/CollateMonitorElement.h"
+
+#include "DQMServices/Core/interface/QTestStatus.h"
+#include "DQMServices/QualityTests/interface/QCriterionRoot.h"
+
+using namespace cms;
+using namespace edm;
+using namespace std;
+
+class EBBeamCaloClient : public EBClient {
+
+public:
+
+/// Constructor
+EBBeamCaloClient(const ParameterSet& ps, MonitorUserInterface* mui);
+
+/// Destructor
+virtual ~EBBeamCaloClient();
+
+/// Subscribe/Unsubscribe to Monitoring Elements
+void subscribe(void);
+void subscribeNew(void);
+void unsubscribe(void);
+
+/// Analyze
+void analyze(void);
+
+/// BeginJob
+void beginJob(void);
+
+/// EndJob
+void endJob(void);
+
+/// BeginRun
+void beginRun(void);
+
+/// EndRun
+void endRun(void);
+
+/// Setup
+void setup(void);
+
+/// Cleanup
+void cleanup(void);
+
+/// HtmlOutput
+void htmlOutput(int run, string htmlDir, string htmlName);
+
+/// WriteDB
+void writeDb(EcalCondDBInterface* econn, MonRunIOV* moniov, int ism);
+
+private:
+
+int ievt_;
+int jevt_;
+
+bool collateSources_;
+bool cloneME_;
+
+bool verbose_;
+
+bool enableMonitorDaemon_;
+
+string prefixME_;
+
+vector<int> superModules_;
+
+MonitorUserInterface* mui_;
+
+};
+
+#endif
