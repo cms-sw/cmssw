5f0e9ac947595561425d11344bb58956ba28dd08
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2006-06-24 20:12:25 +0000

    now just one PixelSeedLayerPairs instance is used

--- RecoTracker/TkSeedGenerator/src/CombinatorialSeedGeneratorFromPixel.cc
+++ RecoTracker/TkSeedGenerator/src/CombinatorialSeedGeneratorFromPixel.cc
@@ -1,19 +1,18 @@
 #include "RecoTracker/TkSeedGenerator/interface/CombinatorialSeedGeneratorFromPixel.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "RecoTracker/TkHitPairs/interface/PixelSeedLayerPairs.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-void CombinatorialSeedGeneratorFromPixel::init(const SiPixelRecHitCollection &coll ,const edm::EventSetup& iSetup)
-{
 
- PixelSeedLayerPairs pixellayers;
-  pixellayers.init(coll,iSetup);
-  initPairGenerator(&pixellayers,iSetup);
+void CombinatorialSeedGeneratorFromPixel::init(const SiPixelRecHitCollection &coll ,
+					       const edm::EventSetup& iSetup)
+{
+  pixelLayers->init(coll,iSetup);
+  initPairGenerator(pixelLayers,iSetup);
 }
 
-CombinatorialSeedGeneratorFromPixel::CombinatorialSeedGeneratorFromPixel(edm::ParameterSet const& conf): SeedGeneratorFromLayerPairs(conf)
+CombinatorialSeedGeneratorFromPixel::CombinatorialSeedGeneratorFromPixel(edm::ParameterSet const& conf)
+  : SeedGeneratorFromLayerPairs(conf)
 {  
-
   edm::ParameterSet conf_ = pSet();
   float ptmin=conf_.getParameter<double>("ptMin");
   float originradius=conf_.getParameter<double>("originRadius");
@@ -25,8 +24,10 @@ CombinatorialSeedGeneratorFromPixel::CombinatorialSeedGeneratorFromPixel(edm::Pa
   edm::LogInfo("CombinatorialSeedGeneratorFromPixel")<<" PtMin of track is "<<ptmin<< 
     " The Radius of the cylinder for seeds is "<<originradius <<"cm" ;
 
+  pixelLayers = new PixelSeedLayerPairs();
 }
 
-void CombinatorialSeedGeneratorFromPixel::run(TrajectorySeedCollection &output,const edm::EventSetup& iSetup){
+void CombinatorialSeedGeneratorFromPixel::run(TrajectorySeedCollection &output,
+					      const edm::EventSetup& iSetup){
   seeds(output,iSetup,region);
 }
