3cadf94c56385d5c72657fb27c0e0480d18985c2
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2006-06-26 19:11:06 +0000

    added ZDC classes

--- DataFormats/HcalRecHit/src/classes.h
+++ DataFormats/HcalRecHit/src/classes.h
@@ -1,6 +1,7 @@
 #include "DataFormats/HcalRecHit/interface/HBHERecHit.h"
 #include "DataFormats/HcalRecHit/interface/HFRecHit.h"
 #include "DataFormats/HcalRecHit/interface/HORecHit.h"
+#include "DataFormats/HcalRecHit/interface/ZDCRecHit.h"
 #include "DataFormats/HcalRecHit/interface/HcalTriggerPrimitiveRecHit.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/HcalRecHit/interface/HcalSourcePositionData.h"
@@ -11,17 +12,20 @@ namespace {
     std::vector<HBHERecHit> vHBHE_;
     std::vector<HORecHit> vHO_;
     std::vector<HFRecHit> vHF_;
+    std::vector<ZDCRecHit> vZDC_;
     std::vector<HcalTriggerPrimitiveRecHit> vHTP_;
 
     HBHERecHitCollection theHBHE_;
     HORecHitCollection theHO_;
     HFRecHitCollection theHF_;
+    ZDCRecHitCollection theZDC_;
     HcalTrigPrimRecHitCollection theHTP_;
     HcalSourcePositionData theSPD_;
 
     edm::Wrapper<HBHERecHitCollection> theHBHEw_;
     edm::Wrapper<HORecHitCollection> theHOw_;
     edm::Wrapper<HFRecHitCollection> theHFw_;
+    edm::Wrapper<ZDCRecHitCollection> theZDCw_;
     edm::Wrapper<HcalTrigPrimRecHitCollection> theHTPw_;
     edm::Wrapper<HcalSourcePositionData> theSPDw_;
  }
