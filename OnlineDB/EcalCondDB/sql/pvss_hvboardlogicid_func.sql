ccda42084a1c429cdd24ed7929f75c09215b5774
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-06-26 09:20:13 +0000

    Updated for new string format

--- OnlineDB/EcalCondDB/sql/pvss_hvboardlogicid_func.sql
+++ OnlineDB/EcalCondDB/sql/pvss_hvboardlogicid_func.sql
@@ -7,21 +7,21 @@ function HVBOARDLOGICID(DPName in varchar2) return varchar2 is
 
  SS = SM number 1-36
  CC = board number 0, 2, 4 or 6
- source string will be something like CAEN/supermodule001/board11 for a 
-board.*/
+ source string will be soemthing like ECAL_HV/SM11/board11  
+*/
  DPNameWithoutSystem varchar2(1000);
- moduleNumber number;
+ superModuleNumber number;
  boardNumber number;
  invalid_board_name exception; --there probably isn't much point defining my own exception type since it will be out of scope by the time anything sees it and will just show as a user defined exception, but it's better than throwing some unrelated predefined exception
 
 begin
         DPNameWithoutSystem:=substr(DPName,instr(DPName,':')+1);
 
-moduleNumber:=to_number(regexp_substr(DPNameWithoutSystem,'[[:digit:]]+'));
+superModuleNumber:=to_number(regexp_substr(DPNameWithoutSystem,'[[:digit:]]+'));
 
 boardNumber:=to_number(regexp_substr(DPNameWithoutSystem,'[[:digit:]]+',1,2));
-        if moduleNumber is null or boardNumber is null then
+        if superModuleNumber is null or boardNumber is null then
                 raise invalid_board_name;
         end if;
-        return 1061000000+10000*moduleNumber+boardNumber;
+        return 1061000000+10000*superModuleNumber+boardNumber;
 end HVBOARDLOGICID;
