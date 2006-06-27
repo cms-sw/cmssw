b7ac198fa938e5b84fa6002cb6bf8887301a7351
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2006-06-27 00:11:12 +0000

    Remove old summary object makers.

--- HLTrigger/HLTcore/src/SealModule.cc
+++ HLTrigger/HLTcore/src/SealModule.cc
@@ -4,14 +4,10 @@
 #include "HLTrigger/HLTcore/interface/HLTPrescaler.h"
 #include "HLTrigger/HLTcore/interface/HLTAnalCand.h"
 #include "HLTrigger/HLTcore/interface/HLTProdCand.h"
-#include "HLTrigger/HLTcore/interface/HLTMakePathObject.h"
-#include "HLTrigger/HLTcore/interface/HLTMakeGlobalObject.h"
 #include "HLTrigger/HLTcore/interface/HLTMakeSummaryObjects.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTPrescaler)
 DEFINE_ANOTHER_FWK_MODULE(HLTAnalCand)
 DEFINE_ANOTHER_FWK_MODULE(HLTProdCand)
-DEFINE_ANOTHER_FWK_MODULE(HLTMakePathObject)
-DEFINE_ANOTHER_FWK_MODULE(HLTMakeGlobalObject)
 DEFINE_ANOTHER_FWK_MODULE(HLTMakeSummaryObjects)
