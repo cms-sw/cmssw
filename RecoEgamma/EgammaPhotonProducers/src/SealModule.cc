5bee896058538f80fdf1b7cfc876c3ccce723f0c
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2006-06-27 14:04:18 +0000

    Updated to include the new modules

--- RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
+++ RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
@@ -1,8 +1,12 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoEgamma/EgammaPhotonProducers/interface/PhotonProducer.h"
+#include "RecoEgamma/EgammaPhotonProducers/interface/PhotonCorrectionProducer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/ConvertedPhotonProducer.h"
 
 DEFINE_SEAL_MODULE();
 
 
+DEFINE_ANOTHER_FWK_MODULE(PhotonProducer);
+DEFINE_ANOTHER_FWK_MODULE(PhotonCorrectionProducer);
 DEFINE_ANOTHER_FWK_MODULE(ConvertedPhotonProducer);
