a9b8783cd30b208ef83993791b9dc33df32aed98
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-06-27 12:39:45 +0000

    add CSC test

--- RecoMuon/DetLayers/test/MuonRecoGeometryAnalyzer.cc
+++ RecoMuon/DetLayers/test/MuonRecoGeometryAnalyzer.cc
@@ -1,7 +1,7 @@
 /** \file
  *
- *  $Date: 2006/06/13 08:46:03 $
- *  $Revision: 1.1 $
+ *  $Date: 2006/06/26 13:24:25 $
+ *  $Revision: 1.2 $
  */
 
 #include <FWCore/Framework/interface/Frameworkfwd.h>
@@ -23,8 +23,11 @@
 
 #include "RecoMuon/DetLayers/interface/MuRodBarrelLayer.h"
 #include "RecoMuon/DetLayers/interface/MuDetRod.h"
+#include "RecoMuon/DetLayers/interface/MuRingForwardLayer.h"
+#include "RecoMuon/DetLayers/interface/MuDetRing.h"
 
 #include <DataFormats/MuonDetId/interface/DTWireId.h>
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
 
 //#include "Geometry/Vector/interface/CoordinateSets.h"
 
@@ -42,6 +45,7 @@ class MuonRecoGeometryAnalyzer : public EDAnalyzer {
   virtual void analyze( const Event& ev, const EventSetup& es);
 
   void testDTLayers(const MuonDetLayerGeometry*, const MagneticField* field);
+  void testCSCLayers(const MuonDetLayerGeometry*, const MagneticField* field);
 
   MeasurementEstimator *theEstimator;
 };
@@ -66,7 +70,8 @@ void MuonRecoGeometryAnalyzer::analyze( const Event& ev,
   ESHandle<MagneticField> magfield;
   es.get<IdealMagneticFieldRecord>().get(magfield);
   
-  testDTLayers(geo.product(),magfield.product());
+  //  testDTLayers(geo.product(),magfield.product());
+  testCSCLayers(geo.product(),magfield.product());
 }
 
 
@@ -96,6 +101,7 @@ void MuonRecoGeometryAnalyzer::testDTLayers(const MuonDetLayerGeometry* geo,cons
     TrajectoryStateOnSurface tsos(gtp, cyl);
     cout << "testDTLayers: at " << tsos.globalPosition()
 	 << " R=" << tsos.globalPosition().perp()
+	 << " phi=" << tsos.globalPosition().phi()
 	 << " Z=" << tsos.globalPosition().z()
 	 << " p = " << tsos.globalMomentum()
 	 << endl;
@@ -106,6 +112,7 @@ void MuonRecoGeometryAnalyzer::testDTLayers(const MuonDetLayerGeometry* geo,cons
     pair<bool, TrajectoryStateOnSurface> comp = layer->compatible(tsos,prop,*theEstimator);
     cout << "is compatible: " << comp.first
 	 << " at: R=" << comp.second.globalPosition().perp()
+	 << " phi=" << comp.second.globalPosition().phi()
 	 << " Z=" <<  comp.second.globalPosition().z()
 	 << endl;
   
@@ -125,6 +132,63 @@ void MuonRecoGeometryAnalyzer::testDTLayers(const MuonDetLayerGeometry* geo,cons
     }    
   }
 }
+
+void MuonRecoGeometryAnalyzer::testCSCLayers(const MuonDetLayerGeometry* geo,const MagneticField* field) {
+
+  const vector<DetLayer*>& layers = geo->allCSCLayers();
+
+  for (vector<DetLayer*>::const_iterator ilay = layers.begin(); ilay!=layers.end(); ++ilay) {
+    const MuRingForwardLayer* layer = (const MuRingForwardLayer*) (*ilay);
+  
+    const BoundDisk& disk = layer->specificSurface();
+
+    // Generate a random point on the disk
+    double aPhi = RandFlat::shoot(-Geom::pi(),Geom::pi());
+    double aR = RandFlat::shoot(disk.innerRadius(), disk.outerRadius());
+    GlobalPoint gp(GlobalPoint::Cylindrical(aR, aPhi, disk.position().z()));  
+
+    // Momentum: 10 GeV, straight from the origin
+    GlobalVector gv(GlobalVector::Spherical(gp.theta(), aPhi, 10.));
+
+    //FIXME: only negative charge
+    int charge = -1;
+
+    GlobalTrajectoryParameters gtp(gp,gv,charge,field);
+    TrajectoryStateOnSurface tsos(gtp, disk);
+    cout << "testCSCLayers: at " << tsos.globalPosition()
+	 << " R=" << tsos.globalPosition().perp()
+	 << " phi=" << tsos.globalPosition().phi()
+	 << " Z=" << tsos.globalPosition().z()
+	 << " p = " << tsos.globalMomentum()
+	 << endl;
+
+
+    SteppingHelixPropagator prop(field,alongMomentum);
+
+    pair<bool, TrajectoryStateOnSurface> comp = layer->compatible(tsos,prop,*theEstimator);
+    cout << "is compatible: " << comp.first
+	 << " at: R=" << comp.second.globalPosition().perp()
+	 << " phi=" << comp.second.globalPosition().phi()
+	 << " Z=" <<  comp.second.globalPosition().z()
+	 << endl;
+  
+    vector<DetLayer::DetWithState> compDets = layer->compatibleDets(tsos,prop,*theEstimator);
+    if (compDets.size()) {
+      cout << "compatibleDets: " << compDets.size() << endl
+
+	   << "  final state pos: " << compDets.front().second.globalPosition() << endl 
+	   << "  det         pos: " << compDets.front().first->position()
+	   << " id: " << CSCDetId(compDets.front().first->geographicalId().rawId()) << endl 
+	   << "  distance " << (tsos.globalPosition()-compDets.front().first->position()).mag()
+
+	   << endl
+	   << endl;
+    } else {
+      cout << " ERROR : no compatible det found" << endl;
+    }
+  }
+}
+
 
 
 
