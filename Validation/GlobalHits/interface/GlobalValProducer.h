f66e11270fb95d61e7542dfc2cfc41815164b588
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2006-06-27 17:13:48 +0000

    modified to replace EmbdSim* with Sim*

--- Validation/GlobalHits/interface/GlobalValProducer.h
+++ Validation/GlobalHits/interface/GlobalValProducer.h
@@ -41,8 +41,8 @@
 // data in edm::event
 #include "SimDataFormats/GlobalValidation/interface/PGlobalSimHit.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
-#include "SimDataFormats/Vertex/interface/EmbdSimVertexContainer.h"
-#include "SimDataFormats/Track/interface/EmbdSimTrackContainer.h"
+#include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
 #include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 //#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
