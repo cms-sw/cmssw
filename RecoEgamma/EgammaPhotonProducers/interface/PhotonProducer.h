51361f4ccce63577f0718ce965258045f8627e9a
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2006-06-27 14:00:05 +0000

    Initial commit. Very basic producer

---
+++ RecoEgamma/EgammaPhotonProducers/interface/PhotonProducer.h
@@ -0,0 +1,50 @@
+#ifndef RecoEgamma_EgammaPhotonProducers_PhotonProducer_h
+#define RecoEgamma_EgammaPhotonProducers_PhotonProducer_h
+/** \class PhotonProducer
+ **  
+ **
+ **  $Id: PhotonProducer.h,v 1.1 2006/06/09 15:55:49 nancy Exp $ 
+ **  $Date: 2006/06/09 15:55:49 $ 
+ **  $Revision: 1.1 $
+ **  \author Nancy Marinelli, U. of Notre Dame, US
+ **
+ ***/
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+
+
+
+// PhotonProducer inherits from EDProducer, so it can be a module:
+class PhotonProducer : public edm::EDProducer {
+
+ public:
+
+  PhotonProducer (const edm::ParameterSet& ps);
+  ~PhotonProducer();
+
+
+  virtual void beginJob (edm::EventSetup const & es);
+  virtual void produce(edm::Event& evt, const edm::EventSetup& es);
+
+ private:
+
+  
+
+  
+  std::string PhotonCollection_;
+  std::string scProducer_;
+  std::string scCollection_;
+  edm::ParameterSet conf_;
+
+
+  edm::ESHandle<MagneticField> theMF_;
+
+
+};
+#endif
