580313226f4be6a1b120af80f22aa4d70bfde795
Wolfgang Waltenberger <sha1-9c4ada266a8352c2428af94857b7ab82e25b3b37@cern.ch>
2006-06-28 08:54:56 +0000

    merger test

--- CommonTools/Clustering1D/test/ClusteringTest.cpp
+++ CommonTools/Clustering1D/test/ClusteringTest.cpp
@@ -1,4 +1,6 @@
 #include "CommonTools/Clustering1D/test/Input.cpp"
+#include "CommonTools/Clustering1D/interface/Cluster1DMerger.h"
+#include "CommonTools/Clustering1D/interface/TrivialWeightEstimator.h"
 
 #define HaveMtv
 #define HaveFsmw
@@ -16,85 +18,121 @@
 #include <string>
 #include <iostream>
 
+using namespace std;
+
 namespace
 {
-  void print ( const std::vector < Cluster1D<string> > & obj )
+  void print ( const Cluster1D<string> & obj )
   {
-      for ( std::vector< Cluster1D<string> >::const_iterator i=obj.begin();
+      cout << "   Cluster1D ";
+      vector < const string * > names = obj.tracks();
+      for ( vector< const string * >::iterator nm=names.begin();
+              nm!=names.end() ; ++nm )
+      {
+          cout << **nm;
+      };
+      cout << " at " << obj.position().value() << " +/- "
+           << obj.position().error() << " weight " << obj.weight();
+      cout << endl;
+  }
+    
+  void print ( const vector < Cluster1D<string> > & obj )
+  {
+      for ( vector< Cluster1D<string> >::const_iterator i=obj.begin();
               i!=obj.end() ; ++i )
       {
-          std::cout << "   Cluster1D ";
-          std::vector < const string * > names = i->tracks();
-          for ( std::vector< const string * >::iterator nm=names.begin();
-                  nm!=names.end() ; ++nm )
-          {
-              std::cout << **nm;
-          };
-          std::cout << " at " << i->position().value() << " +/- "
-          << i->position().error() << " weight " << i->weight();
-          std::cout << std::endl;
+        print ( *i );
       }
   }
 
-  void print ( const pair < std::vector < Cluster1D<string> >,
-               std::vector < const string * > > & obj )
+  void print ( const pair < vector < Cluster1D<string> >,
+               vector < const string * > > & obj )
   {
-      std::vector < Cluster1D<string > > sol = obj.first;
-      for ( std::vector< Cluster1D<string> >::const_iterator i=sol.begin();
+      vector < Cluster1D<string > > sol = obj.first;
+      for ( vector< Cluster1D<string> >::const_iterator i=sol.begin();
               i!=sol.end() ; ++i )
       {
-          std::cout << "   Cluster1D: ";
-          std::vector < const string * > names = i->tracks();
-          for ( std::vector< const string * >::iterator nm=names.begin();
+          cout << "   Cluster1D: ";
+          vector < const string * > names = i->tracks();
+          for ( vector< const string * >::iterator nm=names.begin();
                   nm!=names.end() ; ++nm )
           {
-              std::cout << **nm;
+              cout << **nm;
           };
-          std::cout << " at " << i->position().value() << " +/- "
-          << i->position().error() << " weight " << i->weight() << std::endl;
+          cout << " at " << i->position().value() << " +/- "
+          << i->position().error() << " weight " << i->weight() << endl;
       }
 
-      std::cout << " Discarded: ";
-      std::vector <const string * > disc = obj.second;
-      for ( std::vector< const string * >::const_iterator i=disc.begin();
+      cout << " Discarded: ";
+      vector <const string * > disc = obj.second;
+      for ( vector< const string * >::const_iterator i=disc.begin();
               i!=disc.end() ; ++i )
       {
-          std::cout << **i;
+          cout << **i;
       }
-      std::cout << std::endl;
+      cout << endl;
   }
 
-  void run ( const std::vector < Cluster1D<string> > & input )
+  void run ( const vector < Cluster1D<string> > & input )
   {
-    std::cout << std::endl << "Input: " << std::endl;
+    cout << endl << "Input: " << endl;
     print ( input );
-    std::cout << std::endl;
+    cout << endl;
 
 
     /* Cluster1Dize */
     #ifdef HaveFsmw
     FsmwClusterizer1D<string> fsmw;
-    pair < std::vector < Cluster1D<string> >, std::vector < const string * > > ret=
+    pair < vector < Cluster1D<string> >, vector < const string * > > ret=
         fsmw( input );
-    std::cout << std::endl << "Fsmw finds: " << std::endl;
+    cout << endl << "Fsmw finds: " << endl;
     print ( ret );
     #endif
 
     #ifdef HaveMtv
     MtvClusterizer1D<string> mtv;
     ret= mtv( input );
-    std::cout << std::endl << "Mtv finds: " << std::endl;
+    cout << endl << "Mtv finds: " << endl;
     print ( ret );
     #endif
 
     #ifdef HaveDivisive
     DivisiveClusterizer1D<string> div(50., 1,true, 10.,true);
     ret = div( input );
-    std::cout << std::endl << "Divisive finds: " << std::endl;
+    cout << endl << "Divisive finds: " << endl;
     print ( ret );
     #endif
 
-    std::cout << std::endl;
+    cout << endl;
+  }
+
+  void mergingResult ( const Cluster1D<string> & one,
+                       const Cluster1D<string> & two )
+  {
+    cout << "Merger test:" << endl;
+    print ( one );
+    print ( two );
+    
+    Cluster1D<string> result = Cluster1DMerger<string>( TrivialWeightEstimator < string > () )
+      ( one, two );
+    cout << "Merge result: " << endl;
+    print ( result );
+  }
+  
+  void mergerTest()
+  {
+    string one_s="a";
+    vector < const string * > one_names;
+    one_names.push_back ( &one_s );
+    Cluster1D<string> one ( Measurement1D ( 1.0, 0.1 ), one_names, 1.0 );
+
+    
+    vector < const string * > two_names;
+    string two_s="b";
+    two_names.push_back ( &two_s );
+    Cluster1D<string> two ( Measurement1D ( 2.0, 0.2 ), two_names, 1.0 );
+
+    mergingResult ( one, two );
   }
 }
 
@@ -102,19 +140,24 @@ namespace
 
 int main( int argc, char ** argv )
 {
+    mergerTest();
+    /*
 
-    std::cout << "Three Items:" << std::endl
-              << "============" << std::endl;
-    std::vector < Cluster1D<string> > input = threeItems();
+    cout << "Three Items:" << endl
+              << "============" << endl;
+    vector < Cluster1D<string> > input = threeItems();
     run ( input );
-    std::cout << std::endl
-              << "Four Items:" << std::endl
-              << "============" << std::endl;
+    cout << endl
+              << "Four Items:" << endl
+              << "============" << endl;
     input = fourItems();
     run ( input );
-    std::cout << std::endl
-              << "Seven:" << std::endl
-              << "============" << std::endl;
+    */
+
+    /*
+    cout << endl
+              << "Seven:" << endl
+              << "============" << endl;
     input = createInput( 7 );
-    run ( input );
+    run ( input );*/
 }
