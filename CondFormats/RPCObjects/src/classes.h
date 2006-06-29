d9d81824918db266b9b8e1cd8de6783436b8f10b
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-06-29 10:07:54 +0000

    adopt POOL_2_4_4 removing iterator

--- CondFormats/RPCObjects/src/classes.h
+++ CondFormats/RPCObjects/src/classes.h
@@ -2,10 +2,9 @@
 #include "CondFormats/RPCObjects/interface/RPCdeteIndex.h"
 #include "CondFormats/RPCObjects/interface/RPCelecIndex.h"
 
-// Declaration of the iterator (necessary for the generation of the dictionary)
-template std::vector<RPCReadOutLink>::iterator;
-template std::vector<RPCReadOutLink>::const_iterator;
-template std::map<RPCdeteIndex, RPCelecIndex>::iterator;
-template std::map<RPCdeteIndex, RPCelecIndex>::const_iterator;
-template std::map<RPCelecIndex, RPCdeteIndex>::iterator;
-template std::map<RPCelecIndex, RPCdeteIndex>::const_iterator;
+namespace{
+  std::map<RPCdeteIndex, RPCelecIndex> cmap;
+}
+namespace{
+  std::map<RPCelecIndex, RPCdeteIndex> dmap;
+}
