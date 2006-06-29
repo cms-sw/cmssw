9733ae98081e1fe6e0ef5318d50c8933aa04d0e1
Dmitry Bandurin <sha1-6937735a9c40f19bd812e28cbcccd5baff65778d@cern.ch>
2006-06-29 20:57:22 +0000

    Adding PreshowerClusterProducer.h

--- RecoEcal/EgammaClusterProducers/interface/PreshowerClusterProducer.h
+++ RecoEcal/EgammaClusterProducers/interface/PreshowerClusterProducer.h
@@ -4,13 +4,19 @@
 #include <memory>
 
 #include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/EcalDetId/interface/ESDetId.h"
 
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+
 #include "DataFormats/EgammaReco/interface/PreshowerCluster.h"
 #include "RecoEcal/EgammaClusterAlgos/interface/PreshowerClusterAlgo.h"
+
+#include "TH1.h"
+class TFile;
 
 
 class PreshowerClusterProducer : public edm::EDProducer {
@@ -19,32 +25,51 @@ class PreshowerClusterProducer : public edm::EDProducer {
 
   typedef math::XYZPoint Point;
 
-  PreshowerClusterProducer (const edm::ParameterSet& ps);
+  explicit PreshowerClusterProducer (const edm::ParameterSet& ps);
 
   ~PreshowerClusterProducer();
 
-  virtual void produce(edm::Event& evt, const edm::EventSetup& es);
-  const ESDetId getClosestCellInPlane(Point &point, const int plane) const;
+  virtual void produce( edm::Event& evt, const edm::EventSetup& es);
+  const ESDetId getClosestCellInPlane_(const GlobalPoint &point, const int plane) const;
 
  private:
 
   int nEvt_;         // internal counter of events
 
-  std::string hitProducer_;   // name of module/plugin/producer producing hits
-  std::string hitCollection_; // secondary name given to collection of hits by hitProducer
-  std::string clusterCollection1_;  // secondary name to be given to collection of cluster produced in this module
-  std::string clusterCollection2_;  
-  std::string superclusterCollection_;
+  //clustering parameters:
+  std::string preshHitProducer_;   // name of module/plugin/producer producing hits
+  std::string preshHitCollection_; // secondary name given to collection of hits by hitProducer
+  std::string preshClusterCollectionX_;  // secondary name to be given to collection of cluster produced in this module
+  std::string preshClusterCollectionY_;  
+  std::string endcapSClusterCollection_;
+  std::string endcapSClusterProducer_;
 
-  int PreshNclust_;
+  int preshNclust_;
 
-  double calib_plane1_;
-  double calib_plane2_;
+  // association parameters:
+  std::string assocSClusterCollection_;    // name of super cluster output collection
+
+  double calib_planeX_;
+  double calib_planeY_;
   double miptogev_;
 
   PreshowerClusterAlgo * presh_algo; // algorithm doing the real work
 
-  std::string SClusterCollection_;    // name of super cluster output collection
+  DebugLevel debugL;
+
+  virtual void beginJob(edm::EventSetup const&);
+  virtual void endJob();
+  TH1F* h1_esE_x;
+  TH1F* h1_esE_y;
+  TH1F* h1_esEta_x;
+  TH1F* h1_esEta_y;
+  TH1F* h1_esPhi_x;
+  TH1F* h1_esPhi_y;
+  TH1F* h1_esNhits_x;
+  TH1F* h1_esNhits_y;
+  TH1F* h1_esDeltaE;
+  std::string outputFile_; // output file
+  TFile*  rootFile_;
 
 };
 #endif
