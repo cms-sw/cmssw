18f17a4dea03b46eb8e7903c8e06ea57cc10879a
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-06-29 15:17:42 +0000

    TrackerDigiGeometryRecord now depends on Alignments record as well.

--- Geometry/Records/interface/TrackerDigiGeometryRecord.h
+++ Geometry/Records/interface/TrackerDigiGeometryRecord.h
@@ -16,17 +16,20 @@
 //
 // Author:      
 // Created:     Mon Jul 25 11:05:09 EDT 2005
-// $Id: IdealGeometryRecord.h,v 1.1 2005/07/25 15:11:22 chrjones Exp $
+// $Id: TrackerDigiGeometryRecord.h,v 1.1 2005/08/09 10:37:47 tboccali Exp $
 //
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
 #include "boost/mpl/vector.hpp"
 
 
-class TrackerDigiGeometryRecord : public edm::eventsetup::DependentRecordImplementation<TrackerDigiGeometryRecord,
-  boost::mpl::vector<IdealGeometryRecord> > {};
+class TrackerDigiGeometryRecord : 
+  public edm::eventsetup::DependentRecordImplementation<TrackerDigiGeometryRecord,
+														boost::mpl::vector<IdealGeometryRecord,
+																		   TrackerAlignmentRcd > > {};
 
 #endif /* RECORDS_TRACKERDIGIGEOMETRYRECORD_H */
 
