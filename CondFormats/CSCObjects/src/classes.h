6cc684f55e3bab96aa77032a1d7d5a9f5802b3bf
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-06-29 09:49:31 +0000

    adopt to POOL_2_4_4 removing iterator

--- CondFormats/CSCObjects/src/classes.h
+++ CondFormats/CSCObjects/src/classes.h
@@ -1,35 +1,19 @@
 #include "CondFormats/CSCObjects/interface/CSCPedestals.h"
-template std::map< int, std::vector<CSCPedestals::Item> >::iterator;
-template std::map< int, std::vector<CSCPedestals::Item> >::const_iterator;
-template std::vector< CSCPedestals::Item >::iterator;
-template std::vector< CSCPedestals::Item >::const_iterator;
-
+namespace{
+ std::map< int, std::vector<CSCPedestals::Item> > pedmap;
+}
 #include "CondFormats/CSCObjects/interface/CSCGains.h"
-template std::map< int, std::vector< CSCGains::Item> >::iterator;
-template std::map< int, std::vector< CSCGains::Item> >::const_iterator;
-template std::vector< CSCGains::Item >::iterator;
-template std::vector< CSCGains::Item >::const_iterator;
-
+namespace{
+  std::map< int, std::vector< CSCGains::Item> > gmap;
+}
 #include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
-template std::map< int, std::vector< CSCNoiseMatrix::Item> >::iterator;
-template std::map< int, std::vector< CSCNoiseMatrix::Item> >::const_iterator;
-template std::vector< CSCNoiseMatrix::Item >::iterator;
-template std::vector< CSCNoiseMatrix::Item >::const_iterator;
-
+namespace{
+  std::map< int, std::vector< CSCNoiseMatrix::Item> > mmap;
+}
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
-template std::map< int, std::vector< CSCcrosstalk::Item> >::iterator;
-template std::map< int, std::vector< CSCcrosstalk::Item> >::const_iterator;
-template std::vector< CSCcrosstalk::Item >::iterator;
-template std::vector< CSCcrosstalk::Item >::const_iterator;
-
+namespace{
+  std::map< int, std::vector< CSCcrosstalk::Item> > cmap;
+}
 #include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
-template std::vector< CSCIdentifier::Item >::iterator;
-template std::vector< CSCIdentifier::Item >::const_iterator;
-
 #include "CondFormats/CSCObjects/interface/CSCReadoutMapping.h"
-template std::vector< CSCReadoutMapping::CSCLabel >::iterator;
-template std::vector< CSCReadoutMapping::CSCLabel >::const_iterator;
-
 #include "CondFormats/CSCObjects/interface/CSCTriggerMapping.h"
-template std::vector< CSCTriggerMapping::CSCTriggerConnection >::iterator;
-template std::vector< CSCTriggerMapping::CSCTriggerConnection >::const_iterator;
