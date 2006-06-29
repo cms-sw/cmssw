a9b47a84c22ac6712f05a4323b4ee5ffd9d46aa7
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-06-29 12:02:51 +0000

    remove iterator

--- CondFormats/SiPixelObjects/src/classes.h
+++ CondFormats/SiPixelObjects/src/classes.h
@@ -1,9 +1,4 @@
 #include "CondFormats/SiPixelObjects/interface/SiPixelPedestals.h"
-
-// Declaration of the iterator (necessary for the generation of the dictionary)
-
-template std::vector<SiPixelDbItem>::iterator;
-template std::vector<SiPixelDbItem>::const_iterator;
-
-template std::map< unsigned int, SiPixelPedestals::SiPixelPedestalsVector>::iterator;
-template std::map< unsigned int, SiPixelPedestals::SiPixelPedestalsVector>::const_iterator;
+namespace{
+  std::map< unsigned int, SiPixelPedestals::SiPixelPedestalsVector> sipixped;
+}
