80b3b6fab6607f8c975a2efdbaa86761e2eb6db4
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-06-30 07:24:51 +0000

    removed records for SiStrip Control and Readout Cabling classes

--- CondFormats/DataRecord/src/SiStripControlCablingRcd.cc
+++
@@ -1,4 +0,0 @@
-#include "CondFormats/DataRecord/interface/SiStripControlCablingRcd.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
-
-EVENTSETUP_RECORD_REG(SiStripControlCablingRcd);
