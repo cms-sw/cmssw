c7c3fce33a41e643865fa5770d3531565221ff2c
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2006-07-01 13:08:14 +0000

    Interface is finished

--- EventFilter/CSCTFRawToDigi/src/CSCSPTrailer.h
+++ EventFilter/CSCTFRawToDigi/src/CSCSPTrailer.h
@@ -10,7 +10,7 @@ private:
 	/////// word 2 ///////
 	unsigned trailer_mark_2 : 4;  // constant, should be 1111 = 0xF
 	unsigned trailer_mark_3 : 3;  // constant, should be  111 = 0x7
-	unsigned l1a_fifo_full  : 1;
+	unsigned l1a_fifo_full_ : 1;
 	unsigned word_count_high: 4;
 	unsigned trailer_mark_4 : 4;  // constant, should be 1111 = 0xF
 	/////// word 3 ///////
@@ -46,6 +46,20 @@ public:
 			trailer_mark_1!=0xF || trailer_mark_2!=0xF || trailer_mark_3!=0x7 || trailer_mark_4!=0xF || trailer_mark_5!=0xF || trailer_mark_6!=0xF ||
 			trailer_mark_7!=0xE || trailer_mark_8!=0xE || trailer_mark_9!=0xE || trailer_mark_10!=0xE;
 	}
+
+	unsigned int l1a_7bits     (void) const throw() { return l1a_; }
+	unsigned int l1a_queue_size(void) const throw() { return (word_count_high<<4)|word_count_low; }
+	bool         l1a_fifo_full (void) const throw() { return l1a_fifo_full_; }
+
+	unsigned int firmware_year (void) const throw() { return core_release_year;  }
+	unsigned int firmware_month(void) const throw() { return core_release_month; }
+	unsigned int firmware_day  (void) const throw() { return core_release_day;   }
+	unsigned int configuration (void) const throw() { return core_configuraton;  }
+
+	unsigned int slot    (void) const throw() { return  board_id_&0x1F;       }
+	unsigned int sector  (void) const throw() { return (board_id_&0x700)>>8;  }
+	unsigned int endcap  (void) const throw() { return (board_id_&0x800)>>11; }
+	unsigned int board_id(void) const throw() { return board_id_;             }
 
 	bool unpack(const unsigned short *&buf) throw()  { memcpy(this, buf, 8*sizeof(short)); buf+=8; return check(); }
 
