00e4fbeac64e5b680b8cd53cc14c8b65c1e7c86f
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2006-07-03 09:45:42 +0000

    complete seed analyzer

--- RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedAnalyzer.h
+++ RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedAnalyzer.h
@@ -9,7 +9,7 @@
 //
 // Original Author:  Ursula Berthon, Claude Charlot
 //         Created:  Mon Mar 27 13:22:06 CEST 2006
-// $Id: ElectronPixelSeedAnalyzer.h,v 1.1 2006/06/02 15:32:44 uberthon Exp $
+// $Id: ElectronPixelSeedAnalyzer.h,v 1.2 2006/06/08 16:54:41 uberthon Exp $
 //
 //
   
@@ -31,6 +31,7 @@ class MagneticField;
 class TFile;
 class TH1F;
 class TH1I;
+class TTree;
 
 class ElectronPixelSeedAnalyzer : public edm::EDAnalyzer
 {
@@ -49,8 +50,26 @@ class ElectronPixelSeedAnalyzer : public edm::EDAnalyzer
   edm::ESHandle<TrackerGeometry> pDD;
   edm::ESHandle<MagneticField> theMagField;
   TFile *histfile_;
+  TTree *tree_;
+  float mcEnergy[10], mcEta[10], mcPhi[10], mcPt[10], mcQ[10];
+  float superclusterEnergy[10], superclusterEta[10], superclusterPhi[10], superclusterEt[10];
+  float seedMomentum[10], seedEta[10], seedPhi[10], seedPt[10], seedQ[10];
+  TH1F *histeMC_;
+  TH1F *histp_;
+  TH1F *histeclu_;
   TH1F *histpt_;
+  TH1F *histptMC_;
+  TH1F *histetclu_;
+  TH1F *histeffpt_;
+  TH1F *histeta_;
+  TH1F *histetaMC_;
+  TH1F *histetaclu_;
+  TH1F *histeffeta_;
+  TH1F *histq_;
+  TH1F *histeoverp_;
   TH1I *histnrseeds_;
+  TH1I *histnbseeds_;
+  TH1I *histnbclus_;
  };
   
 #endif
--- RecoEgamma/EgammaElectronProducers/src/ElectronPixelSeedAnalyzer.cc
+++ RecoEgamma/EgammaElectronProducers/src/ElectronPixelSeedAnalyzer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Ursula Berthon
 //         Created:  Mon Mar 27 13:22:06 CEST 2006
-// $Id: ElectronPixelSeedAnalyzer.cc,v 1.1 2006/06/02 15:32:45 uberthon Exp $
+// $Id: ElectronPixelSeedAnalyzer.cc,v 1.2 2006/06/08 16:54:41 uberthon Exp $
 //
 //
 
@@ -44,23 +44,28 @@
 
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
 #include <iostream>
 #include "TFile.h"
 #include "TH1F.h"
 #include "TH1I.h"
+#include "TTree.h"
 
 using namespace reco;
  
 ElectronPixelSeedAnalyzer::ElectronPixelSeedAnalyzer(const edm::ParameterSet& iConfig)
 {
-  histfile_ = new TFile("histos.root","UPDATE");
-}
-
+  histfile_ = new TFile("electronpixelseeds.root","RECREATE");
+}  
+  
 ElectronPixelSeedAnalyzer::~ElectronPixelSeedAnalyzer()
 {
  
   // do anything here that needs to be done at desctruction time
   // (e.g. close files, deallocate resources etc.)
+  tree_->Print();
   histfile_->Write();
   histfile_->Close();
 }
@@ -68,59 +73,159 @@ ElectronPixelSeedAnalyzer::~ElectronPixelSeedAnalyzer()
 void ElectronPixelSeedAnalyzer::beginJob(edm::EventSetup const&iSetup){
   iSetup.get<TrackerDigiGeometryRecord> ().get (pDD); 
   iSetup.get<IdealMagneticFieldRecord>().get(theMagField);
-  histpt_ = new TH1F("pt","pt of seed ",100,0.,100.);
-  histnrseeds_ = new TH1I("nrs","Nr seeds/evt",50,0.,50.);
+  tree_ = new TTree("ElectronPixelSeeds","ElectronPixelSeed validation ntuple");
+  tree_->Branch("mcEnergy",mcEnergy,"mcEnergy[10]/F");
+  tree_->Branch("mcEta",mcEta,"mcEta[10]/F");
+  tree_->Branch("mcPhi",mcPhi,"mcPhi[10]/F");
+  tree_->Branch("mcPt",mcPt,"mcPt[10]/F");
+  tree_->Branch("mcQ",mcQ,"mcQ[10]/F");
+  tree_->Branch("superclusterEnergy",superclusterEnergy,"superclusterEnergy[10]/F");
+  tree_->Branch("superclusterEta",superclusterEta,"superclusterEta[10]/F");
+  tree_->Branch("superclusterPhi",superclusterPhi,"superclusterPhi[10]/F");
+  tree_->Branch("superclusterEt",superclusterEt,"superclusterEt[10]/F");
+  tree_->Branch("seedMomentum",seedMomentum,"seedMomentum[10]/F");
+  tree_->Branch("seedEta",seedEta,"seedEta[10]/F");
+  tree_->Branch("seedPhi",seedPhi,"seedPhi[10]/F");
+  tree_->Branch("seedPt",seedPt,"seedPt[10]/F");
+  tree_->Branch("seedQ",seedQ,"seedQ[10]/F");
+  histeMC_ = new TH1F("eMC","MC particle energy",100,0.,100.);
+  histp_ = new TH1F("p","seed p",100,0.,100.);
+  histeclu_ = new TH1F("clus energy","supercluster energy",100,0.,100.);
+  histpt_ = new TH1F("pt","seed pt",100,0.,100.);
+  histptMC_ = new TH1F("ptMC","MC particle pt",100,0.,100.);
+  histetclu_ = new TH1F("Et","supercluster Et",100,0.,100.);
+  histeffpt_ = new TH1F("pt eff","seed effciency vs pt",100,0.,100.);
+  histeta_ = new TH1F("seed eta","seed eta",100,-2.5,2.5);
+  histetaMC_ = new TH1F("etaMC","MC particle eta",100,-2.5,2.5);
+  histetaclu_ = new TH1F("clus eta","supercluster eta",100,-2.5,2.5);
+  histeffeta_ = new TH1F("eta eff","seed effciency vs eta",100,-2.5,2.5);
+  histq_ = new TH1F("q","seed charge",100,-2.5,2.5);
+  histeoverp_ = new TH1F("E/p","seed E/p",100,0.,10.);
+  histnbseeds_ = new TH1I("nrs","Nr of seeds ",50,0.,25.);
+  histnbclus_ = new TH1I("nrclus","Nr of superclusters ",50,0.,25.);
+  histnrseeds_ = new TH1I("ns","Nr of seeds if clusters",50,0.,25.);
 }     
 
 void
 ElectronPixelSeedAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& iSetup)
 {
+  // seems to break due to pDD and or theMagField from the setup
+  
   // rereads the seeds for test purposes
   typedef edm::OwnVector<TrackingRecHit> recHitContainer;
   typedef recHitContainer::const_iterator const_iterator;
   typedef std::pair<const_iterator,const_iterator> range;
 
+  // get seeds
+  
   edm::Handle<ElectronPixelSeedCollection> elSeeds;
   e.getByType(elSeeds); 
-  std::cout <<"\n\n =================> Treating event "<<e.id()<<" Number of seeds "<<elSeeds.product()->size()<<std::endl;
+  edm::LogInfo("")<<"\n\n =================> Treating event "<<e.id()<<" Number of seeds "<<elSeeds.product()->size();
   int is=0;
+
   for( ElectronPixelSeedCollection::const_iterator MyS= (*elSeeds).begin(); MyS != (*elSeeds).end(); ++MyS) {
-    std::cout <<"\nSeed nr "<<is++<<": "<<std::endl;
+    
+    LogDebug("") <<"\nSeed nr "<<is<<": ";
     range r=(*MyS).recHits();
-    const GeomDet *det = pDD->idToDet(((*r.first)).geographicalId());
-    std::cout<<" Number of RecHits= "<<(*MyS).nHits()<<std::endl;
+     LogDebug("")<<" Number of RecHits= "<<(*MyS).nHits();
+    const GeomDet *det=0;
     for (TrackingRecHitCollection::const_iterator rhits=r.first; rhits!=r.second; rhits++) {
-      std::cout <<" SiPixelHit   local x,y,z "<<(*rhits).localPosition()<<" det "<<(*rhits).geographicalId().det()<<" subdet "<<(*rhits).geographicalId().subdetId()<<std::endl;
-      std::cout <<" SiPixelHit   global  "<<det->toGlobal((*rhits).localPosition())<<std::endl;
-      //       const DetLayer *ilayer=theGeometricSearchTracker->detLayer((*rhits).geographicalId());
-
-      //       if ((*rhits).geographicalId().subdetId()==1) {
-      // 	const BarrelDetLayer *bdetl = dynamic_cast<const BarrelDetLayer *>(ilayer);
-      // 	if (bdetl) {
-      // 	  std::cout <<" PixelHit layer radius "<<bdetl->specificSurface().radius()<<std::endl;
-      // 	}
-      // 	else printf("Could not downcast to barrel!!\n");
-      //       }else {
-      // 	const ForwardDetLayer *fdetl = dynamic_cast<const ForwardDetLayer *>(ilayer);
-      // 	if (!fdetl)
-      // 	  printf("Could not downcast to forward!!\n");
-      //       }
-    }
-
-    std::cout<<" ElectronPixelSeed charge: "<<(*MyS).getCharge()<<std::endl;
-
+      det = pDD->idToDet(((*rhits)).geographicalId());
+       LogDebug("") <<" SiPixelHit   local x,y,z "<<(*rhits).localPosition()<<" det "<<(*rhits).geographicalId().det()<<" subdet "<<(*rhits).geographicalId().subdetId();
+       LogDebug("") <<" SiPixelHit   global  "<<det->toGlobal((*rhits).localPosition());
+    }   
+    
+    // state on last det
     TrajectoryStateOnSurface t= transformer_.transientState((*MyS).startingState(), &(det->surface()), &(*theMagField));
 
-    std::cout<<" TSOS, position: "<<t.globalPosition()<<std::endl;
-    std::cout<<" momentum: "<<t.globalMomentum()<<std::endl;
-    const SuperCluster *theClus=(*MyS).superCluster();
-    std::cout<<" SuperCluster energy: "<<theClus->energy()<<", position: "<<theClus->position()<<std::endl;
-  histpt_->Fill(TMath::Sqrt(t.globalMomentum().x()*t.globalMomentum().x() + t.globalMomentum().y()*t.globalMomentum().y()));
+    // debug
+    
+     LogDebug("")<<" ElectronPixelSeed outermost state position: "<<t.globalPosition();
+     LogDebug("")<<" ElectronPixelSeed outermost state momentum: "<<t.globalMomentum();
+     const SuperCluster *theClus=(*MyS).superCluster();
+     LogDebug("")<<" ElectronPixelSeed superCluster energy: "<<theClus->energy()<<", position: "<<theClus->position();
+     LogDebug("")<<" ElectronPixelSeed outermost state Pt: "<<t.globalMomentum().perp();
+     LogDebug("")<<" ElectronPixelSeed supercluster Et: "<<theClus->energy()*sin(2.*atan(exp(-theClus->position().eta())));
+     LogDebug("")<<" ElectronPixelSeed outermost momentum direction eta: "<<t.globalMomentum().eta();
+     LogDebug("")<<" ElectronPixelSeed supercluster eta: "<<theClus->position().eta();
+     LogDebug("")<<" ElectronPixelSeed seed charge: "<<(*MyS).getCharge();
+     LogDebug("")<<" ElectronPixelSeed E/p: "<<theClus->energy()/t.globalMomentum().mag();
+
+    // fill the tree and histos
+    
+    histpt_->Fill(t.globalMomentum().perp());
+    histetclu_->Fill(theClus->energy()*sin(2.*atan(exp(-theClus->position().eta()))));
+    histeta_->Fill(t.globalMomentum().eta());
+    histetaclu_->Fill(theClus->position().eta());
+    histq_->Fill((*MyS).getCharge());
+    histeoverp_->Fill(theClus->energy()/t.globalMomentum().mag());   
+    
+    if (is<10) {
+      superclusterEnergy[is] = theClus->energy();
+      superclusterEta[is] = theClus->position().eta();
+      superclusterPhi[is] = theClus->position().phi();
+      superclusterEt[is] = theClus->energy()*sin(2.*atan(exp(-theClus->position().eta())));
+      seedMomentum[is] = t.globalMomentum().mag();
+      seedEta[is] = t.globalMomentum().eta();
+      seedPhi[is] = t.globalMomentum().phi();
+      seedPt[is] = t.globalMomentum().perp();
+      seedQ[is] = (*MyS).getCharge();
+    }
+    
+    is++;
+    
   }
+  
+  histnbseeds_->Fill(elSeeds.product()->size());
+
   // get input clusters 
+
   edm::Handle<SuperClusterCollection> clusters;
-  e.getByType(clusters);
-  if (clusters.product()->size()>0)      histnrseeds_->Fill(elSeeds.product()->size());
+  //CC to be changed according to supercluster input
+  e.getByLabel("correctedHybridSuperClusterProducer", "correctedHybridSuperClusterCollection", clusters); 
+  histnbclus_->Fill(clusters.product()->size());
+  if (clusters.product()->size()>0) histnrseeds_->Fill(elSeeds.product()->size());
+  
+  // get MC information
+  
+  edm::Handle<edm::HepMCProduct> HepMCEvt;
+  // this one is empty branch in current test files
+  //e.getByLabel("VtxSmeared", "", HepMCEvt);
+  e.getByLabel("source", "", HepMCEvt);
+  
+  const HepMC::GenEvent* MCEvt = HepMCEvt->GetEvent();
+  int ip=0;
+  for (HepMC::GenEvent::particle_const_iterator partIter = MCEvt->particles_begin();
+   partIter != MCEvt->particles_end(); ++partIter) { 
+
+    for (HepMC::GenEvent::vertex_const_iterator vertIter = MCEvt->vertices_begin();
+     vertIter != MCEvt->vertices_end(); ++vertIter) {
+
+      CLHEP::HepLorentzVector creation = (*partIter)->CreationVertex();
+      CLHEP::HepLorentzVector momentum = (*partIter)->Momentum();
+      HepPDT::ParticleID id = (*partIter)->particleID();  // electrons and positrons are 11 and -11
+      LogDebug("")  << "MC particle id " << id.pid() << ", creationVertex " << creation << " cm, initialMomentum " << momentum << " GeV/c" << std::endl;
+      if (id == 11 || id == -11) {
+	histptMC_->Fill(momentum.perp());
+	histetaMC_->Fill(momentum.pseudoRapidity());
+	histeMC_->Fill(momentum.rho());
+	if (ip<10) {
+	  mcEnergy[ip] = momentum.rho();
+	  mcEta[ip] = momentum.pseudoRapidity();
+	  mcPhi[ip] = momentum.phi();
+	  mcPt[ip] = momentum.perp();
+	  mcQ[ip] = ((id == 11) ? -1.: +1.);
+	}
+      }
+
+    }
+    
+    ip++;
+   
+  }  
+    
+  tree_->Fill();
+  
 }
 
 
