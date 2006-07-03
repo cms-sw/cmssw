19a6660083b462bc9fcee2c2e47e090c0d3e4ad2
Norbert Neumeister <sha1-7abc839d53ede37ea0c445f89ed7dc2d61338f2b@cern.ch>
2006-07-03 12:01:58 +0000

    *** empty log message ***

--- RecoMuon/GlobalTrackFinder/interface/GlobalMuonTrackMatcher.h
+++ RecoMuon/GlobalTrackFinder/interface/GlobalMuonTrackMatcher.h
@@ -4,33 +4,43 @@
 /** \class GlobalMuonTrackMatcher
  *  match standalone muon track with tracker track
  *
- *  $Date: $
- *  $Revision:  $
+ *  $Date: 2006/07/02 03:00:36 $
+ *  $Revision: 1.1 $
  *  \author Chang Liu  - Purdue University
  */
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "FWCore/Framework/interface/Handle.h"
 
-
 class TrajectoryStateOnSurface;
 
+//              ---------------------
+//              -- Class Interface --
+//              ---------------------
+
 class GlobalMuonTrackMatcher {
-public:
-  /// constructor
-  GlobalMuonTrackMatcher(const double& chi2);
 
-  /// destructor
-  virtual ~GlobalMuonTrackMatcher(){};
+  public:
 
-  std::pair<bool, reco::TrackRef> match(const reco::TrackRef&, const edm::Handle<reco::TrackCollection>&);
+    /// constructor
+    GlobalMuonTrackMatcher(double chi2);
 
-  std::pair<bool,double> match(const reco::TrackRef&, const reco::TrackRef&); 
+    /// destructor
+    virtual ~GlobalMuonTrackMatcher() {};
 
-  std::pair<bool,double> match(const TrajectoryStateOnSurface&, const TrajectoryStateOnSurface&);
+    ///
+    std::pair<bool, reco::TrackRef> match(const reco::TrackRef&, const edm::Handle<reco::TrackCollection>&) const;
 
-private:
-  double theMaxChi2;
+    ///
+    std::pair<bool,double> match(const reco::TrackRef&, const reco::TrackRef&) const; 
+
+    ///
+    std::pair<bool,double> match(const TrajectoryStateOnSurface&, const TrajectoryStateOnSurface&) const;
+
+  private:
+
+    double theMaxChi2;
 
 };
+
 #endif
--- RecoMuon/GlobalTrackFinder/src/GlobalMuonTrackMatcher.cc
+++ RecoMuon/GlobalTrackFinder/src/GlobalMuonTrackMatcher.cc
@@ -3,16 +3,14 @@
 /** \class GlobalMuonTrackMatcher
  *  match standalone muon track with a tracker track
  *
- *  $Date: $
- *  $Revision:  $
+ *  $Date: 2006/07/02 03:01:37 $
+ *  $Revision: 1.1 $
  *  \author Chang Liu  - Purdue University
  */
 #include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 
 
-GlobalMuonTrackMatcher::GlobalMuonTrackMatcher(const double& chi2) {
-
-  theMaxChi2 = chi2;
+GlobalMuonTrackMatcher::GlobalMuonTrackMatcher(double chi2) : theMaxChi2(chi2) {
 
 }
 
@@ -20,7 +18,8 @@ GlobalMuonTrackMatcher::GlobalMuonTrackMatcher(const double& chi2) {
    a given standalone Track
  */
 std::pair<bool, reco::TrackRef> 
-GlobalMuonTrackMatcher::match(const reco::TrackRef& staT, const edm::Handle<reco::TrackCollection>& tkTs){
+GlobalMuonTrackMatcher::match(const reco::TrackRef& staT, const edm::Handle<reco::TrackCollection>& tkTs) const {
+
   bool hasMatchTk = false;
   reco::TrackRef result;
   double minChi2 = theMaxChi2;
@@ -34,14 +33,16 @@ GlobalMuonTrackMatcher::match(const reco::TrackRef& staT, const edm::Handle<reco
       result = tkTRef;
     }
   } 
+
   return(std::pair<bool, reco::TrackRef>(hasMatchTk, result));
+
 }
 
 /** determine if the tk and standalone Tracks are compatible
   by comparing their TSOSs on outer Tracker surface
  */
 std::pair<bool,double> 
-GlobalMuonTrackMatcher::match(const reco::TrackRef&, const reco::TrackRef&){
+GlobalMuonTrackMatcher::match(const reco::TrackRef&, const reco::TrackRef&) const {
 
   return(std::pair<bool,double>(false,0));
 
@@ -50,9 +51,16 @@ GlobalMuonTrackMatcher::match(const reco::TrackRef&, const reco::TrackRef&){
 /** determine if two TSOSs are compatible
  */
 std::pair<bool,double> 
-GlobalMuonTrackMatcher::match(const TrajectoryStateOnSurface&, const TrajectoryStateOnSurface&){
+GlobalMuonTrackMatcher::match(const TrajectoryStateOnSurface& tsos1, const TrajectoryStateOnSurface& tsos2) const {
 
-  return(std::pair<bool,double>(false,0));
+  AlgebraicVector v(tsos1.globalParameters().vector() - tsos2.globalParameters().vector());
+  AlgebraicSymMatrix m(tsos1.curvilinearError().matrix() + tsos2.curvilinearError().matrix());
+  int ierr;
+  m.invert(ierr);
+  // if (ierr != 0) ...
+  double est = m.similarity(v);
+
+  return ( est > theMaxChi2 ) ? std::pair<bool,double>(false,est) : std::pair<bool,double>(true,est);
 
 }
 
