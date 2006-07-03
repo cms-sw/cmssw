e2b334d522c8ef6ffe08d53edbbb7cb51ab5e050
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2006-07-03 07:25:18 +0000

    intial position releted stuff removed. setSurface() and initialize() are now protected

--- TrackingTools/DetLayers/interface/BarrelDetLayer.h
+++ TrackingTools/DetLayers/interface/BarrelDetLayer.h
@@ -21,12 +21,8 @@ class BarrelDetLayer : public DetLayer {
  public:
 
   BarrelDetLayer() : 
-    theCylinder(0), theInitialPosition(0) {}
+    theCylinder(0){}
   
-  BarrelDetLayer( float initPos) : 
-    theCylinder(0), theInitialPosition(initPos) {}
-  
-
   virtual ~BarrelDetLayer();
 
   // GeometricSearchDet interface
@@ -43,26 +39,21 @@ class BarrelDetLayer : public DetLayer {
   // Extension of the interface
   virtual const BoundCylinder&  specificSurface() const { return *theCylinder;}
 
-  void setSurface( BoundCylinder* cp);
-
   bool contains( const Local3DPoint& p) const;
 
-  virtual float initialPosition() const { return theInitialPosition;}
-  
-  virtual void initialize();
-  
 
 
 protected:
+  void setSurface( BoundCylinder* cp);
+
+  virtual void initialize();
 
   virtual BoundCylinder* computeSurface();
 
-private:
 
+private:
+  //float theRmin, theRmax, theZmin, theZmax;
   ReferenceCountingPointer<BoundCylinder>  theCylinder;
-  float theInitialPosition;
-  
-  float theRmin, theRmax, theZmin, theZmax;
 
 };
 
