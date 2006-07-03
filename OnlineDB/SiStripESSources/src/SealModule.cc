4fb9e49a4f10d0e126d195ef890db89e6bb73229
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-03 18:31:13 +0000

    moved populate db code to test

--- OnlineDB/SiStripESSources/src/SealModule.cc
+++ OnlineDB/SiStripESSources/src/SealModule.cc
@@ -1,11 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 DEFINE_SEAL_MODULE();
 
-// #include "FWCore/Framework/interface/MakerMacros.h"
-// #include "OnlineDB/SiStripESSources/interface/SiStripPopulateConfigDb.h"
-// DEFINE_ANOTHER_FWK_MODULE(SiStripPopulateConfigDb)
-
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "OnlineDB/SiStripESSources/interface/SiStripFedCablingBuilderFromDb.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripFedCablingBuilderFromDb)
+
 
