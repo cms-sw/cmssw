395c18ac8d44f7c474c988d7822d88cbb4e81eeb
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-07-04 08:12:31 +0000

    Changed weights for ESRecHitSimAlgo

--- RecoLocalCalo/EcalRecAlgos/src/ESRecHitSimAlgo.cc
+++ RecoLocalCalo/EcalRecAlgos/src/ESRecHitSimAlgo.cc
@@ -15,14 +15,14 @@ ESRecHitSimAlgo::ESRecHitSimAlgo(int gain, int pedestal, double MIPADC, double M
     pw[2] = 0.247238;
   }
   else if (gain_ == 1) {
-    pw[0] = -0.085;
-    pw[1] = 0.909;
-    pw[2] = 0.176;
+    pw[0] = -0.0739126;
+    pw[1] = 0.7815982;
+    pw[2] = 0.3691372;
   }
   else if (gain_ == 2) {
-    pw[0] = -0.097;
-    pw[1] = 0.805;
-    pw[2] = 0.292;
+    pw[0] = -0.01632344;
+    pw[1] = 0.7515172;
+    pw[2] = 0.4028312;
   }
 
   LogDebug("ESRecHitSimAlgo") << "ESRecHitSimAlgo : Gain "<<gain_<<" Weights : "<<pw[0]<<" "<<pw[1]<<" "<<pw[2];
