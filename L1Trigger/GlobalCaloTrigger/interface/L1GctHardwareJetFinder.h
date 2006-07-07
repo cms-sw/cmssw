3d4042b7caaaceaf7ed1726d3677f8cc06ff326b
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2006-07-07 13:59:53 +0000

    First version of hardwareJetFinder

---
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctHardwareJetFinder.h
@@ -0,0 +1,101 @@
+#ifndef L1GCTHARDWAREJETFINDER_H_
+#define L1GCTHARDWAREJETFINDER_H_
+
+#include "L1Trigger/GlobalCaloTrigger/interface/L1GctJetFinderBase.h"
+
+#include <boost/cstdint.hpp> //for uint16_t
+#include <vector>
+
+/*! \class L1GctHardwareJetFinder
+ * \brief Emulation of the hardware jet finder.
+ *
+ *  
+ */
+/*
+ * \author Greg Heath
+ * \date June 2006
+ */
+
+
+
+class L1GctHardwareJetFinder : public L1GctJetFinderBase
+{
+ public:
+
+  /// id is 0-8 for -ve Eta jetfinders, 9-17 for +ve Eta, for increasing Phi.
+  L1GctHardwareJetFinder(int id, std::vector<L1GctSourceCard*> sourceCards,
+			 L1GctJetEtCalibrationLut* jetEtCalLut);
+                 
+  ~L1GctHardwareJetFinder();
+   
+  /// Overload << operator
+  friend std::ostream& operator << (std::ostream& os, const L1GctHardwareJetFinder& algo);
+
+  /// include additional reset functionality
+  virtual void reset();
+
+  /// get input data from sources
+  virtual void fetchInput();
+
+  /// process the data, fill output buffers
+  virtual void process();
+
+ protected:
+
+  // Each jetFinder must define the constants as private and copy the
+  // function definitions below.
+  virtual unsigned maxRegionsIn() const { return MAX_REGIONS_IN; }
+  virtual unsigned centralCol0() const { return CENTRAL_COL0; }
+  virtual int nCols() const { return N_COLS; }
+
+private:
+
+  /// The real jetFinders must define these constants
+  static const unsigned int MAX_REGIONS_IN; ///< Dependent on number of rows and columns.
+  static const int N_COLS;
+  static const unsigned int CENTRAL_COL0;
+
+  /// Threshold value to find a local et maximum
+  static const unsigned int JET_THRESHOLD;
+
+  /// Vector of pre-clustered jets formatted as a search array for the final clustering
+  RegionsVector m_protoJetRegions;
+
+  /// Local vectors used during both stages of clustering
+  RegionsVector m_localMaxima;
+  /// Each local maximum becomes a cluster
+  RegionsVector m_clusters;
+
+  /// The number of local Maxima/clusters found at each stage of clustering
+  unsigned m_numberOfClusters;
+  
+  /// The first stage of clustering, called by fetchInput()
+  void findProtoJets();  
+  /// The second stage of clustering, called by process()
+  void findJets();  
+
+  /// Both clustering stages need to find local maxima in the search array
+  void findLocalMaxima(const RegionsVector rgv);
+  /// Both clustering stages need to convert local maxima to clusters
+  void findClusters(const RegionsVector rgv, const bool preClusterLogic);
+
+  // comparison operator for sorting local maxima
+  struct etGreaterThan : public std::binary_function<L1CaloRegion, L1CaloRegion, bool> 
+  {
+    bool operator()(const L1CaloRegion& x, const L1CaloRegion& y) {
+      return ( x.et() > y.et() ) ;
+    }
+  };
+
+  /// Fill search array for the second stage of clustering based on the pre-clustered jets
+  void fillRegionsFromProtoJets();
+  /// Organise the pre-clustered jets into the ones we keep and those we send to the neighbour
+  void convertClustersToProtoJets();
+  /// Organise the final clustered jets into L1GctJets
+  void convertClustersToOutputJets();
+
+};
+
+std::ostream& operator << (std::ostream& os, const L1GctHardwareJetFinder& algo);
+
+#endif /*L1GCTHARDWAREJETFINDER_H_*/
---
+++ L1Trigger/GlobalCaloTrigger/src/L1GctHardwareJetFinder.cc
@@ -0,0 +1,237 @@
+#include "L1Trigger/GlobalCaloTrigger/interface/L1GctHardwareJetFinder.h"
+ 
+#include "FWCore/Utilities/interface/Exception.h"  
+
+#include <iostream>
+using namespace std;
+
+//DEFINE STATICS
+const unsigned int L1GctHardwareJetFinder::MAX_REGIONS_IN = (((L1CaloRegionDetId::N_ETA)/2)+1)*L1GctHardwareJetFinder::N_COLS;
+
+const int L1GctHardwareJetFinder::N_COLS = 2;
+const unsigned int L1GctHardwareJetFinder::CENTRAL_COL0 = 0;
+
+const unsigned int L1GctHardwareJetFinder::JET_THRESHOLD = 1;
+
+L1GctHardwareJetFinder::L1GctHardwareJetFinder(int id, vector<L1GctSourceCard*> sourceCards,
+					   L1GctJetEtCalibrationLut* jetEtCalLut):
+  L1GctJetFinderBase(id, sourceCards, jetEtCalLut),
+  m_protoJetRegions(MAX_REGIONS_IN)
+{
+  // Setup the position info in protoJetRegions;
+  for (unsigned column=0; column<2; ++column) {
+    for (unsigned row=0; row<COL_OFFSET; ++row) {
+      unsigned ieta;
+      unsigned iphi;
+      if (id<static_cast<int>(L1CaloRegionDetId::N_PHI/2)) {
+	ieta = (L1CaloRegionDetId::N_ETA/2-1+row);
+	iphi = (2*m_id+column);
+      } else {
+	ieta = (L1CaloRegionDetId::N_ETA/2-row);
+	iphi = (2*m_id+column-L1CaloRegionDetId::N_PHI);
+      }
+      L1CaloRegion temp(0, ieta, iphi);
+      m_protoJetRegions.at(column*COL_OFFSET+row) = temp;
+    }
+  }
+  this->reset();
+}
+
+L1GctHardwareJetFinder::~L1GctHardwareJetFinder()
+{
+}
+
+ostream& operator << (ostream& os, const L1GctHardwareJetFinder& algo)
+{
+  os << "===L1GctHardwareJetFinder===" << endl;
+  const L1GctJetFinderBase* temp = &algo;
+  os << *temp;
+  return os;
+}
+
+void L1GctHardwareJetFinder::reset()
+{
+  L1GctJetFinderBase::reset();
+  // Reset m_protoJetRegions without disturbing the position information
+  for (unsigned j=0; j<m_protoJetRegions.size(); ++j) {
+    m_protoJetRegions.at(j).reset();
+  }
+}
+
+void L1GctHardwareJetFinder::fetchInput()
+{
+  fetchCentreStripsInput();
+  findProtoJets();
+}
+
+void L1GctHardwareJetFinder::process() 
+{
+  fetchProtoJetsFromNeighbour();
+  findJets();
+  sortJets();
+  doEnergySums();
+}
+
+/// HERE IS THE JETFINDER CODE
+
+/// The first stage of clustering, called by fetchInput()
+void L1GctHardwareJetFinder::findProtoJets()
+{
+  findLocalMaxima(m_inputRegions);
+  findClusters(m_inputRegions, true);
+  convertClustersToProtoJets();
+}
+
+/// The second stage of clustering, called by process()
+void L1GctHardwareJetFinder::findJets()
+{
+  fillRegionsFromProtoJets();
+  findLocalMaxima(m_protoJetRegions);
+  findClusters(m_protoJetRegions, false);
+  convertClustersToOutputJets();
+}
+
+/// Both clustering stages need to find local maxima in the search array
+//  Find the local et maxima in the 2x11 array of regions
+void L1GctHardwareJetFinder::findLocalMaxima(const RegionsVector rgv)
+{
+  m_localMaxima.clear();
+  m_localMaxima.resize(MAX_JETS_OUT);
+  UShort jetNum = 0; //holds the number of jets currently found
+  UShort centreIndex = COL_OFFSET*this->centralCol0();
+  for(UShort column = 0; column <2; ++column)  //Find jets in the central search region
+  {
+    //don't include row zero as it is not in the search region
+    ++centreIndex;
+    for (UShort row = 1; row < COL_OFFSET; ++row)  
+    {
+      // Here's the array of greater-than and greater-or-equal tests
+      // to ensure each localMaximum appears once and only once in the list
+      bool localMax = (rgv.at(centreIndex).et()>=JET_THRESHOLD);
+      localMax     &= (rgv.at(centreIndex).et() >  rgv.at(centreIndex-1).et());
+      if (row < (COL_OFFSET-1)) {
+	localMax   &= (rgv.at(centreIndex).et() >= rgv.at(centreIndex+1).et());
+      }
+      if (column==0) {
+	localMax   &= (rgv.at(centreIndex).et() >= rgv.at(centreIndex+COL_OFFSET).et());
+	localMax   &= (rgv.at(centreIndex).et() >  rgv.at(centreIndex+COL_OFFSET-1).et());
+	if (row < (COL_OFFSET-1)) {
+	  localMax &= (rgv.at(centreIndex).et() >  rgv.at(centreIndex+COL_OFFSET-1).et());
+	}
+      } else {
+	localMax   &= (rgv.at(centreIndex).et() >  rgv.at(centreIndex-COL_OFFSET).et());
+	localMax   &= (rgv.at(centreIndex).et() >= rgv.at(centreIndex-COL_OFFSET-1).et());
+	if (row < (COL_OFFSET-1)) {
+	  localMax &= (rgv.at(centreIndex).et() >= rgv.at(centreIndex-COL_OFFSET-1).et());
+	}
+      }
+      if (localMax) {
+        assert(jetNum < MAX_JETS_OUT);
+                
+        m_localMaxima.at(jetNum) = rgv.at(centreIndex);
+        ++jetNum;
+      }
+      ++centreIndex;
+    }
+  }
+
+  // Sort the maxima by et at this point
+  sort(m_localMaxima.begin(), m_localMaxima.end(), etGreaterThan());
+
+  m_numberOfClusters = jetNum;
+
+}
+
+/// Both clustering stages need to convert local maxima to clusters
+//  For each local maximum, find the cluster et in a 2x3 region.
+//  The logic ensures that a given region et cannot be used in more than one cluster.
+//  The sorting of the local maxima ensures the highest et maximum has priority.
+void L1GctHardwareJetFinder::findClusters(const RegionsVector rgv, const bool preClusterLogic)
+{
+  m_clusters.clear();
+  m_clusters.resize(MAX_JETS_OUT);
+
+  // Use each row in the array once only; remember which ones we have used
+  vector<bool> usedThisRow(COL_OFFSET, false);
+
+  // Loop over local maxima
+  for (unsigned j=0; j<m_numberOfClusters; ++j) {
+    unsigned localEta = m_localMaxima.at(j).rctEta();
+    unsigned localPhi = m_localMaxima.at(j).rctPhi();
+
+    unsigned etCluster = 0;
+    bool tauVetoOr = false;
+    bool ovrFlowOr = false;
+
+    for (unsigned row=localEta; ((row<(localEta+3)) && (row<COL_OFFSET)); ++row) {
+      if (!usedThisRow.at(row)) {
+	for (unsigned column=0; column<2; ++column) {
+	  if ((preClusterLogic) || (column != localPhi) || (row == (localEta+1))) {
+	    unsigned index = column*COL_OFFSET + row;
+	    etCluster += rgv.at(index).et();
+	    tauVetoOr |= rgv.at(index).tauVeto();
+	    ovrFlowOr |= rgv.at(index).overFlow();
+	  }
+	}
+	usedThisRow.at(row) = true;
+      }
+    }
+    unsigned eta = m_localMaxima.at(j).gctEta();
+    unsigned phi = m_localMaxima.at(j).gctPhi();
+
+    // In the precluster phase, we shift the phi
+    // position of the region before sending the odd
+    // phi value out to the neighbour
+    if (preClusterLogic) {
+      phi = (L1CaloRegionDetId::N_PHI + phi + 1)%L1CaloRegionDetId::N_PHI;
+    }
+
+    L1CaloRegion temp(etCluster, ovrFlowOr, tauVetoOr, false, false, eta, phi);
+    m_clusters.at(j) = temp;
+  }
+}
+
+/// Fill search array for the second stage of clustering based on the pre-clustered jets
+void L1GctHardwareJetFinder::fillRegionsFromProtoJets()
+{
+  for (unsigned j=0; j<MAX_JETS_OUT; ++j) {
+    if (m_keptProtoJets.at(j).et()>=JET_THRESHOLD) {
+      unsigned eta0 = m_keptProtoJets.at(j).rctEta();
+      m_protoJetRegions.at(eta0+1) = m_keptProtoJets.at(j);
+    }
+    if (m_rcvdProtoJets.at(j).et()>=JET_THRESHOLD) {
+      unsigned eta1 = m_rcvdProtoJets.at(j).rctEta();
+      m_protoJetRegions.at(eta1+1+COL_OFFSET) = m_rcvdProtoJets.at(j);
+    }
+  }
+}
+
+/// Organise the pre-clustered jets into the ones we keep and those we send to the neighbour
+void L1GctHardwareJetFinder::convertClustersToProtoJets()
+{
+  unsigned numberOfSentJets = 0;
+  unsigned numberOfKeptJets = 0;
+  for (unsigned j=0; j<MAX_JETS_OUT; ++j) {
+    if (m_clusters.at(j).et()>=JET_THRESHOLD) {
+      if (m_clusters.at(j).rctPhi()==0) {
+	m_keptProtoJets.at(numberOfKeptJets++) = m_clusters.at(j);
+      } else {
+	m_sentProtoJets.at(numberOfSentJets++) = m_clusters.at(j);
+      }
+    }
+  }
+}
+
+/// Organise the final clustered jets into L1GctJets
+void L1GctHardwareJetFinder::convertClustersToOutputJets()
+{
+  for (unsigned j=0; j<MAX_JETS_OUT; ++j) {
+    if (m_clusters.at(j).et()>=JET_THRESHOLD) {
+      unsigned rawsum = m_clusters.at(j).et();
+      if (m_clusters.at(j).overFlow()) { rawsum = rawsum | (1<<L1GctJet::RAWSUM_BITWIDTH); }
+      L1GctJet temp(rawsum, m_clusters.at(j).gctEta(), m_clusters.at(j).gctPhi(), m_clusters.at(j).tauVeto(), m_jetEtCalLut);
+      m_outputJets.at(j) = temp;
+    }
+  }
+}
+
