6b74b5ebf5211b3022421a72d4d09c2c5d4441ff
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2006-07-10 05:46:48 +0000

    better version (0.7.0 compatible)

--- HLTrigger/HLTfilters/utilities/hlt.py
+++ HLTrigger/HLTfilters/utilities/hlt.py
@@ -3,18 +3,36 @@
 #
 # Doug Wright
 
+import os,sys,commands,re
+
 hlt = {} # trigger
 
 #
 #....add trigger to the list, check that trigger name is unique
 #
-def trig(name,prescale,l1bits):
+def trig(name,prescale):
  if name not in hlt:
     hlt[name] = prescale
  else:
     print "Error:", name,\
           "is already defined! Did not change prescale to", prescale
 
+# look for trigger path include file 
+def findInc(name):
+    env= os.environ.get('CMSSW_SEARCH_PATH')
+    dirList=env.split(':')
+
+    if not dirList: return 0
+
+    for dir in dirList:
+        if not os.path.isdir(dir): continue
+        fname=dir+'/'+name
+        print fname
+        if os.path.isfile(fname):
+            return 1
+
+    return 0
+    
 #
 #....use trigger list to create the .cfg file to load all of the triggers
 #    with prescale values
@@ -24,15 +42,24 @@ def make_cfg_file(file):
   f = open(file,'w')
   for name in hlt.keys():
     prescale=str(hlt[name])
-    name_prescale = name + "Prescale"
-    name_sequence = name + "Sequence"
-    hlt_sequence_path = "HLTCore/HLTfilters/data/"
+    name_prescale = "HLT" + name + "Prescale"
+    name_sequence = "HLT" + name + "Sequence"
+    hlt_sequence_path = "HLTrigger/HLTfilters/data/" + name_sequence + ".cfi"
     f.write("\n")
-    f.write("include \"" + hlt_sequence_path + name +".cfi\"\n")
+    foundValidIncFile=findInc(hlt_sequence_path)
+    if ( foundValidIncFile == 1 ):
+        f.write("include \"" + hlt_sequence_path + "\"\n")
+    else:
+        print "Skipping missing include " + hlt_sequence_path + "\n"
+    
     f.write("module   " + name_prescale +"= Prescaler { int32 prescaleFactor = " + prescale + " }\n")
     f.write("path     " + name + " =")
-    f.write(", Hlt" + name_prescale)
-    f.write(", Hlt" + name_sequence + "}\n")
+    f.write("{" + name_prescale)
+    if ( foundValidIncFile == 1 ):
+        f.write(", Hlt" + name_sequence + "}\n")
+    else:
+        f.write("}\n")
+        
   f.write("\n")
   f.close()
 
