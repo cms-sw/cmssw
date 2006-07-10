595e984f28e3933b5fbda7a4bc558e8fc2dcd12c
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-07-10 09:28:21 +0000

    Added TypeId <-> Name interface (now a class).

--- Alignment/CommonAlignment/interface/AlignableObjectId.h
+++ Alignment/CommonAlignment/interface/AlignableObjectId.h
@@ -1,9 +1,18 @@
 #ifndef Alignment_CommonAlignment_AlignableObjectId_h
 #define Alignment_CommonAlignment_AlignableObjectId_h
 
+#include <map>
+#include <string>
+
 /// Type Identifier of Alignable objects (Det's, Rod's etc.).
-namespace AlignableObjectId 
+/// Allows conversion between type and name, and vice-versa
+class AlignableObjectId 
 {
+
+public:
+  
+  /// Constructor (defines names)
+  AlignableObjectId();
 
   enum AlignableObjectIdType 
 	{ 
@@ -36,7 +45,31 @@ namespace AlignableObjectId
 	  AlignableMuon
 	  
 	};
+  
+  typedef std::map<AlignableObjectIdType, std::string> MapEnumType;
+  typedef std::map<std::string, AlignableObjectIdType> ReverseMapEnumType;
+  typedef std::pair<AlignableObjectIdType, std::string> PairEnumType;
+  typedef std::pair<std::string, AlignableObjectIdType> PairEnumReverseType;
 
-}
+  /// Convert name to type
+  const AlignableObjectIdType nameToType( const std::string name ) const;
+
+  /// Convert type to name
+  const std::string typeToName( const int type ) const;
+
+private:
+  MapEnumType theMap;
+  ReverseMapEnumType theReverseMap;
+
+  // Reverse functor
+  struct reverse_pair {
+	PairEnumReverseType operator()( const PairEnumType& pair ) const 
+	{ 
+	  return PairEnumReverseType( pair.second, pair.first ); 
+	}
+  };
+
+
+};
 
 #endif
