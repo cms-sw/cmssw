f917cd8c427a288e345dca1cca89eba94aef73e4
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2006-07-10 18:09:44 +0000

    Add SimplePhotonAnalyzer

--- RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
+++ RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
@@ -1,6 +1,7 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/PhotonProducer.h"
+#include "RecoEgamma/EgammaPhotonProducers/interface/SimplePhotonAnalyzer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/PhotonCorrectionProducer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/ConvertedPhotonProducer.h"
 
@@ -8,5 +9,6 @@ DEFINE_SEAL_MODULE();
 
 
 DEFINE_ANOTHER_FWK_MODULE(PhotonProducer);
+DEFINE_ANOTHER_FWK_MODULE(SimplePhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PhotonCorrectionProducer);
 DEFINE_ANOTHER_FWK_MODULE(ConvertedPhotonProducer);
