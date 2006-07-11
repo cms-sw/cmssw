a16706c11cc7d76a66a863ac317aafa6c51e7706
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-07-11 14:54:54 +0000

    order

--- CondTools/IntegrationTest/monitoring/poll_db.sql
+++ CondTools/IntegrationTest/monitoring/poll_db.sql
@@ -10,7 +10,7 @@ cnt number(10);
 BEGIN
   SELECT systimestamp INTO ts FROM dual;
   dbms_output.put_line('Time:  ' || ts);
-  FOR result IN (SELECT table_name FROM user_tables)
+  FOR result IN (SELECT table_name FROM user_tables ORDER BY table_name)
   LOOP
     EXECUTE IMMEDIATE 'SELECT ''' || result.table_name || ''', count(*) FROM ' || result.table_name INTO tbl, cnt;
     dbms_output.put_line(tbl || ' ' || cnt || ' rows');
