1268565cae31a711610a8620e492131e937d18ce
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-07-11 21:38:54 +0000

    Change default c'tor, add new c'tor and addition functions

--- DataFormats/Common/interface/FileFormatVersion.h
+++ DataFormats/Common/interface/FileFormatVersion.h
@@ -1,13 +1,39 @@
 #ifndef Common_FileFormatVersion_h
 #define Common_FileFormatVersion_h
 
-#include "FWCore/Utilities/interface/GetFileFormatVersion.h"
+#include <ostream>
 
-namespace edm {
-  struct FileFormatVersion {
-    explicit FileFormatVersion() : value_(getFileFormatVersion()) {}
+namespace edm 
+{
+  struct FileFormatVersion 
+  {
+    FileFormatVersion() : value_(-1) { }
+    explicit FileFormatVersion(int vers) : value_(vers)  { }
+    bool isValid() { return value_ >= 0; }
+    
     int value_;
   };
+
+  inline
+  bool operator== (FileFormatVersion const& a, FileFormatVersion const& b)
+  {
+    return a.value_ == b.value_;
+  }
+
+  inline
+  bool operator!= (FileFormatVersion const& a, FileFormatVersion const& b)
+  {
+    return !(a==b);
+  }
+
+  inline
+  std::ostream&
+  operator<< (std::ostream& os, FileFormatVersion const& ff)
+  {
+    os << ff.value_;
+    return os;
+  }
+
 /*
   inline
   FileFormatVersion getFileFormatVersion() {
