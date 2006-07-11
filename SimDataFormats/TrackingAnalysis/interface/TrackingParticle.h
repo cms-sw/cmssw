8806f5b4457d37334b76d9292b76985cf73b2389
Eduardo Luiggi Lopez <sha1-3fc92667b1684b58ee3cf7c3d0fd548d6ae2812f@cern.ch>
2006-07-11 16:12:34 +0000

    added iterators and getters

--- SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h
+++ SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h
@@ -18,6 +18,9 @@ public:
   /// reference to HepMC::GenParticle
   typedef edm::RefVector<edm::HepMCProduct, HepMC::GenParticle > GenParticleRefVector;
   typedef edm::Ref<edm::HepMCProduct, HepMC::GenParticle >       GenParticleRef;
+  typedef GenParticleRefVector::iterator		         genp_iterator;
+  typedef SimTrackRefVector::iterator				 g4t_iterator;
+ 
   /// default constructor
   TrackingParticle() { }
   // destructor
@@ -25,27 +28,37 @@ public:
   /// constructor from pointer to generator particle
   TrackingParticle( Charge q, const LorentzVector & p4, const Point & vtx,
 		    double t, const int pdgId,  const int source, const int crossing );
-  /// PDG identifier  
+  
+  /// PDG id, signal source, crossing number  
   int pdgId() const { return pdgId_; }
   int source() const { return signalSource_ % 4 == 0; }
   int crossing() const { return crossing_; }
+  
+  ///iterators
+  genp_iterator genParticle_begin() const;
+  genp_iterator genParticle_end() const;
+  g4t_iterator  g4Track_begin() const;
+  g4t_iterator  g4Track_end() const;
+  
 
-// Setters for Embd and Sim Tracks
+// Setters for G4 and HepMC
   void addG4Track(const SimTrackRef&);
   void addGenParticle(const GenParticleRef&);
   
-// Need Getters for Embd and Sim Tracks
-
+// Getters for Embd and Sim Tracks
+  GenParticleRefVector	genParticle() const { return genParticles_; }
+  SimTrackRefVector	g4Tracks() const { return g4Tracks_ ; }
+  
 private:
   /// production time
   double t_;
-  /// PDG identifier
+  /// PDG identifier, signal source, crossing number
   int pdgId_;
+  int signalSource_; 
+  int crossing_;
   /// references to G4 and HepMC tracks
   SimTrackRefVector g4Tracks_;
   GenParticleRefVector  genParticles_;
-  int signalSource_; 
-  int crossing_;
 };
 
 #endif // SimDataFormats_TrackingParticle_H
--- SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
+++ SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
@@ -17,4 +17,19 @@ void TrackingParticle::addG4Track( const SimTrackRef &ref) {
   g4Tracks_.push_back(ref);
 }
 
+TrackingParticle::genp_iterator TrackingParticle::genParticle_begin() const {
+   return genParticles_.begin();
+}
+
+TrackingParticle::genp_iterator TrackingParticle::genParticle_end() const {
+   return genParticles_.end();
+}
+
+TrackingParticle::g4t_iterator TrackingParticle::g4Track_begin()const {
+    return g4Tracks_.begin();
+}
+
+TrackingParticle::g4t_iterator TrackingParticle::g4Track_end()const {
+    return g4Tracks_.end();
+}
 //int TrackingParticle::source(){}
