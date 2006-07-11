fbffa9dce7e49bc42c34d4e5737eb8a7c5e51590
Filip Moortgat <sha1-236ec69c42ab223e0df5c43733dcda6ad72fa5d0@cern.ch>
2006-07-11 16:09:06 +0000

    split in .h and .cc

--- SimGeneral/HepPDTESSource/src/HepPDTESSource.cc
+++ SimGeneral/HepPDTESSource/src/HepPDTESSource.cc
@@ -1,53 +1,5 @@
-// -*- C++ -*-
-//
-// Package:    HepPDTESSource
-// Class:      HepPDTESSource
-// 
-/**\class HepPDTESSource HepPDTESSource.h PhysicsTools/HepPDTESSource/interface/HepPDTESSource.h
+#include "SimGeneral/HepPDTESSource/interface/HepPDTESSource.h"
 
- Description: HepPDT particle data table ESSource
-
- Implementation:
-    <Notes on implementation>
-*/
-//
-// Original Author:  Luca Lista
-//         Created:  Fri Mar 10 15:58:18 CET 2006
-// $Id: HepPDTESSource.cc,v 1.1 2006/03/14 16:33:58 llista Exp $
-//
-#include <memory>
-#include <fstream>
-#include "boost/shared_ptr.hpp"
-#include <CLHEP/HepPDT/DefaultConfig.hh>
-#include <CLHEP/HepPDT/TableBuilder.hh>
-#include <CLHEP/HepPDT/ParticleDataTableT.hh>
-#include "FWCore/Framework/interface/ESProducer.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EventSetupRecordIntervalFinder.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-#include "SimGeneral/HepPDTRecord/interface/PDTRecord.h"
-#include <climits>
-
-class HepPDTESSource : public edm::ESProducer, public  edm::EventSetupRecordIntervalFinder {
-public:
-  /// constructor from parameter set
-  HepPDTESSource( const edm::ParameterSet& );
-  /// destructor
-  ~HepPDTESSource();
-  /// define the particle data table type
-  typedef DefaultConfig::ParticleDataTable PDT;
-  /// define the return type
-  typedef std::auto_ptr<PDT> ReturnType;
-  /// return the particle table
-  ReturnType produce( const PDTRecord & );
-  /// set validity interval
-  void setIntervalFor( const edm::eventsetup::EventSetupRecordKey &,
-		       const edm::IOVSyncValue &,
-		       edm::ValidityInterval & );
-
-private:
-  std::string pdtFileName;
-};
 
 HepPDTESSource::HepPDTESSource( const edm::ParameterSet& cfg ) :
   pdtFileName( cfg.getParameter<std::string>( "pdtFileName" ) ) {
@@ -88,4 +40,4 @@ void HepPDTESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey&
 }
 
 //define this as a plug-in
-DEFINE_FWK_EVENTSETUP_SOURCE( HepPDTESSource )
+//DEFINE_FWK_EVENTSETUP_SOURCE( HepPDTESSource )
