5475d853116a8ec45e37247cb65f2fd24d337e7c
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-07-14 03:20:08 +0000

    independent implementation

--- RecoMuon/MuonSeedGenerator/src/CosmicMuonSeedGenerator.h
+++ RecoMuon/MuonSeedGenerator/src/CosmicMuonSeedGenerator.h
@@ -3,24 +3,20 @@
 
 /** \class CosmicMuonSeedGenerator
  *  SeedGenerator for Cosmic Muon
- *  some changes from MuonSeedGenerator 
- *  \author A. Vitelli, V.Palichik, ported by: R. Bellan 
- *  to cover more stations and have looser requirements
  *
- *  $Date: $
- *  $Revision: $
+ *  $Date: 2006/07/07 19:26:12 $
+ *  $Revision: 1.1 $
  *  \author Chang Liu - Purdue University 
  */
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
+#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
 
 #include <vector>
 
 namespace edm {class ParameterSet; class Event; class EventSetup;}
 
-class MuonSeedFinder;
-class MuonTransientTrackingRecHit;
 
 class CosmicMuonSeedGenerator: public edm::EDProducer {
 
@@ -42,20 +38,35 @@ class CosmicMuonSeedGenerator: public edm::EDProducer {
   /// reconstruct muon's seeds
   virtual void produce(edm::Event&, const edm::EventSetup&);
 
- protected:
-
  private:
-  void complete(MuonSeedFinder& seed,RecHitContainer &recHits, bool* used=0) const;
-  void checkAndFill(MuonSeedFinder& Theseed, const edm::EventSetup& eSetup);
 
-  // FIXME: change in OwnVector?
-  std::vector<TrajectorySeed> theSeeds;
+  /// generate TrajectorySeeds and put them into results
+  void createSeeds(TrajectorySeedCollection& results,
+                   int nseeds,
+                   const RecHitContainer& hits,
+                   const edm::EventSetup& eSetup) const;
 
+  /// determine if a MuonTransientTrackingRecHit is qualified to build seed
+  bool checkQuality(MuonTransientTrackingRecHit *) const;
+
+  /// create TrajectorySeed from MuonTransientTrackingRecHit 
+  std::vector<TrajectorySeed> createSeed(MuonTransientTrackingRecHit *,
+                                         const edm::EventSetup&) const;
+
+ private: 
   /// the name of the DT rec hits collection
   std::string theDTRecSegmentLabel;
 
   /// the name of the CSC rec hits collection
   std::string theCSCRecSegmentLabel;
+
+  /// the maximum number of Seeds
+  int theMaxSeeds;
+  
+  /// the maximum chi2 required for dt and csc rechits
+  double theMaxDTChi2;
+  double theMaxCSCChi2;
+ 
 };
 #endif
 
