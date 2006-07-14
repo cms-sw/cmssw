d0d5d161dfa46955c983639255ec3282a4ed9eb5
Stephen Wagner <sha1-007cb192666c25c72e6f07a7e13df677568d86c7@cern.ch>
2006-07-14 01:02:22 +0000

    added 1st version of module to remove duplicate tracks (RoadSearchTrackListCleaner)

---
+++ RecoTracker/RoadSearchHelixMaker/src/RoadSearchTrackListCleaner.cc
@@ -0,0 +1,180 @@
+//
+// Package:         RecoTracker/RoadSearchTrackListCleaner
+// Class:           RoadSearchHelixMaker
+// 
+// Description:     TrackListCleaner
+//
+// Original Author: Steve Wagner, stevew@pizero.colorado.edu
+// Created:         Sat Jan 14 22:00:00 UTC 2006
+//
+// $Author: gutsche $
+// $Date: 2006/03/28 23:08:40 $
+// $Revision: 1.4 $
+//
+
+#include <memory>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "RecoTracker/RoadSearchHelixMaker/interface/RoadSearchTrackListCleaner.h"
+
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2DMatchedLocalPosCollection.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2DLocalPosCollection.h"
+#include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
+#include "DataFormats/TrackCandidate/interface/TrackCandidateCollection.h"
+
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
+#include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "DataFormats/TrackReco/src/classes.h"
+
+namespace cms
+{
+
+  RoadSearchTrackListCleaner::RoadSearchTrackListCleaner(edm::ParameterSet const& conf) : 
+    conf_(conf)
+  {
+    produces<reco::TrackCollection>();
+//    produces<reco::TrackExtraCollection>();
+  }
+
+
+  // Virtual destructor needed.
+  RoadSearchTrackListCleaner::~RoadSearchTrackListCleaner() { }  
+
+  // Functions that gets called by framework every event
+  void RoadSearchTrackListCleaner::produce(edm::Event& e, const edm::EventSetup& es)
+  {
+    // retrieve producer name of input SiStripRecHit2DLocalPosCollection
+    std::string trackProducer = conf_.getParameter<std::string>("TrackProducer");
+  
+    //
+    // extract tracker geometry
+    //
+    edm::ESHandle<TrackerGeometry> theG;
+    es.get<TrackerDigiGeometryRecord>().get(theG);
+
+//    using namespace reco;
+
+    // get Inputs 
+    edm::Handle<reco::TrackCollection> trackCollection;
+    e.getByLabel(trackProducer, trackCollection);
+//    edm::Handle<SiStripRecHit2DLocalPosCollection> rphirecHits;
+//    e.getByLabel(recHitProducer, "rphiRecHit", rphirecHits);
+//    edm::Handle<SiStripRecHit2DLocalPosCollection> stereorecHits;
+//    e.getByLabel(recHitProducer, "stereoRecHit", stereorecHits);
+//    edm::Handle<SiStripRecHit2DMatchedLocalPosCollection> matchedrecHits;
+//    e.getByLabel(recHitProducer, "matchedRecHit", matchedrecHits);
+
+    const reco::TrackCollection tC = *(trackCollection.product());
+
+    std::cout << "Reconstructed "<< tC.size() << " tracks" << std::endl ;
+
+    // Step B: create empty output collection
+    std::auto_ptr<reco::TrackCollection> output(new reco::TrackCollection);
+
+  //
+  //  no input tracks
+  //
+
+    if ( tC.empty() ){
+//      LogDebug("RoadSearch") << "Found " << output.size() << " clouds.";
+      e.put(output);
+      return;  
+    }
+
+  //
+  //  1 raw cloud - nothing to try merging, but one cloud to duplicate
+  //
+
+    if ( 1==tC.size() ){
+      for (reco::TrackCollection::const_iterator track=tC.begin(); track!=tC.end(); track++){
+        reco::Track * theTrack = new reco::Track(track->chi2(),(short unsigned)track->ndof(),track->found(),track->lost(),track->invalid(),track->parameters(),track->covariance());    
+//        reco::TrackExtra * theTrackExtra = new reco::TrackExtra(track->outerPosition(), track->outerMomentum(), true);
+        //create a TrackExtraRef
+//        reco::TrackExtraRef  theTrackExtraRef(ohTE,cc);    
+        //use the TrackExtraRef to assign the TrackExtra to the Track
+//        theTrack->setExtra(theTrackExtraRef);    
+        //fill the TrackCollection
+        reco::TrackExtraRef theTrackExtraRef=track->extra();    
+        theTrack->setExtra(theTrackExtraRef);    
+        output->push_back(*theTrack);
+        delete theTrack;
+      }//end faux loop over tracks
+//      LogDebug("RoadSearchTrackListCleaner") << " output " << output.size() << " tracks.";
+      e.put(output);
+      return;
+    }  
+  //
+  //  > 1 track - try merging
+  //
+    std::vector<int> not_dup; for (int i=0; i<tC.size(); ++i){not_dup.push_back(1);}
+    int i=-1;
+    for (reco::TrackCollection::const_iterator track=tC.begin(); track!=tC.end(); track++){
+      i++; std::cout << "Track number "<< i << std::endl ; if (!not_dup[i])continue;
+      int j=-1;
+      for (reco::TrackCollection::const_iterator track2=tC.begin(); track2!=tC.end(); track2++){
+        j++;
+        if ((j<=i)||(!not_dup[j])||(!not_dup[i]))continue;
+//        if (j<=i)continue;
+        int noverlap=0;
+        for (trackingRecHit_iterator it = track->recHitsBegin();  it != track->recHitsEnd(); it++){
+          if ((*it)->isValid()){
+            for (trackingRecHit_iterator jt = track2->recHitsBegin();  jt != track2->recHitsEnd(); jt++){
+	      if ((*jt)->isValid()){
+                if (((*it)->geographicalId()==(*jt)->geographicalId())&&((*it)->localPosition().x()==(*jt)->localPosition().x()))noverlap++;
+              }
+            }
+          }
+        }
+        float fi=float(noverlap)/float(track->recHitsSize()); float fj=float(noverlap)/float(track2->recHitsSize());
+        std::cout << " trk1 trk2 nhits1 nhits2 nover " << i << " " << j << " " << track->recHitsSize() << " " 
+                  << track2->recHitsSize() << " " << noverlap << " " << fi << " " << fj  <<std::endl;
+        if ((fi>0.66)||(fj>0.66)){
+          if (fi<fj){
+            not_dup[j]=0; std::cout << " removing 2nd trk in pair " << std::endl;
+          }else{
+            if (fi>fj){
+              not_dup[i]=0; std::cout << " removing 1st trk in pair " << std::endl;
+            }else{
+              std::cout << " removing worst chisq in pair " << std::endl;
+              if (track->chi2() > track2->chi2()){not_dup[i]=0;}else{not_dup[j]=0;}
+            }//end fi > or = fj
+          }//end fi < fj
+        }//end got a duplicate
+      }//end track2 loop
+    }//end track loop
+    i=-1;
+    for (reco::TrackCollection::const_iterator track=tC.begin(); track!=tC.end(); track++){
+      i++;  if (!not_dup[i])continue;
+      reco::Track * theTrack = new reco::Track(track->chi2(),(short unsigned)track->ndof(),track->found(),track->lost(),track->invalid(),track->parameters(),track->covariance());    
+//      reco::TrackExtra * theTrackExtra = new reco::TrackExtra(track->outerPosition(), track->outerMomentum(), true);
+      //create a TrackExtraRef
+//      reco::TrackExtraRef  theTrackExtraRef(ohTE,cc);    
+      //use the TrackExtraRef to assign the TrackExtra to the Track
+//      theTrack->setExtra(theTrackExtraRef);    
+      //fill the TrackCollection
+      reco::TrackExtraRef theTrackExtraRef=track->extra();    
+      theTrack->setExtra(theTrackExtraRef);    
+      output->push_back(*theTrack);
+      delete theTrack;
+    }//end faux loop over tracks
+//    LogDebug("RoadSearchTrackListCleaner") << " output " << output.size() << " tracks.";
+    e.put(output);
+    return;
+
+  }//end produce
+
+}
--- RecoTracker/RoadSearchHelixMaker/src/SealModules.cc
+++ RecoTracker/RoadSearchHelixMaker/src/SealModules.cc
@@ -4,7 +4,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoTracker/RoadSearchHelixMaker/interface/RoadSearchHelixMaker.h"
+#include "RecoTracker/RoadSearchHelixMaker/interface/RoadSearchTrackListCleaner.h"
 
 using cms::RoadSearchHelixMaker;
+using cms::RoadSearchTrackListCleaner;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RoadSearchHelixMaker)
+DEFINE_ANOTHER_FWK_MODULE(RoadSearchTrackListCleaner)
