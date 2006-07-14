b0df756a125bcf2851bba7c12bcceecb0a2b8be8
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2006-07-14 07:01:53 +0000

    understand new turl

--- Utilities/StorageFactory/src/RFIOStorageMaker.cc
+++ Utilities/StorageFactory/src/RFIOStorageMaker.cc
@@ -14,25 +14,41 @@
 //<<<<<< PUBLIC FUNCTION DEFINITIONS                                    >>>>>>
 //<<<<<< MEMBER FUNCTION DEFINITIONS                                    >>>>>>
 
+
+namespace {
+
+std::string normalizeURL(const std::string &path) {
+    std::string ret;
+    size_t p = path.find("?path");
+    if (p==std::string::npos)
+	p=0;
+    else
+	ret = "rfio:///";
+
+     return ret+path.substr(p);
+}
+
+}
+
 seal::Storage *
 RFIOStorageMaker::open (const std::string & /* proto */,
 		        const std::string &path,
 			int mode,
 		        const std::string & /* tmpdir */)
-{ return new RFIOFile (path, mode); }
+{ return new RFIOFile (normalizeURL(path), mode); }
 
 bool
 RFIOStorageMaker::check (const std::string &proto,
 		         const std::string &path,
 		         seal::IOOffset *size /* = 0 */)
 {
-    if (rfio_access (path.c_str (), R_OK) != 0)
+    if (rfio_access (normalizeURL(path).c_str (), R_OK) != 0)
 	return false;
 
     if (size)
     {
 	struct stat64 buf;
-	if (rfio_stat64 (path.c_str (), &buf) != 0)
+	if (rfio_stat64 (normalizeURL(path).c_str (), &buf) != 0)
 	    return false;
 
 	*size = buf.st_size;
