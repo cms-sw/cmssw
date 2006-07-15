64897071e1135e78cf3b8634684f79570255d1e3
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2006-07-15 09:29:50 +0000

    for DT segments extrapolation

--- DQM/RPCMonitorDigi/interface/RPCMonitorEfficiency.h
+++ DQM/RPCMonitorDigi/interface/RPCMonitorEfficiency.h
@@ -5,8 +5,8 @@
  *
  * Class for RPC Monitoring using RPCDigi and RPCRecHit.
  *
- *  $Date: 2006/06/26 13:25:30 $
- *  $Revision: 1.1 $
+ *  $Date: 2006/06/27 07:57:31 $
+ *  $Revision: 1.2 $
  *
  * \author Ilaria Segoni (CERN)
  *
@@ -22,30 +22,53 @@
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
 #include "DQMServices/Daemon/interface/MonitorDaemon.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-
 #include<string>
 #include<map>
 
+/* Base Class Headers */
+#include <DataFormats/TrajectoryState/interface/LocalTrajectoryParameters.h> 
+#include <DataFormats/DTRecHit/interface/DTRecSegment4D.h> 
+
+/* Collaborating Class Declarations */
+#include "FWCore/Framework/interface/Handle.h"
+#include <Geometry/Surface/interface/Surface.h>
+#include <Geometry/Surface/interface/BoundPlane.h>
+#include <MagneticField/Engine/interface/MagneticField.h>
+
+
 class RPCDetId;
+class TFile;
+class TH1F;
 
 class RPCMonitorEfficiency : public edm::EDAnalyzer {
-   public:
-	explicit RPCMonitorEfficiency( const edm::ParameterSet& );
-	~RPCMonitorEfficiency();
-   
-	virtual void analyze( const edm::Event&, const edm::EventSetup& );
-
-	virtual void endJob(void);
-        
-
-   private:
-	int counter;
-	std::string nameInLog;
-	bool saveRootFile;
-	int  saveRootFileEventsInterval;
-	std::string RootFileName;
-	/// back-end interface
-	DaqMonitorBEInterface * dbe;
+ public:
+  
+  ///Constructor
+  explicit RPCMonitorEfficiency( const edm::ParameterSet& );
+  
+  ///Destructor
+  ~RPCMonitorEfficiency();
+  
+  //Operations
+  virtual void analyze( const edm::Event&, const edm::EventSetup& );
+  virtual void endJob(void);
+  
+  const BoundPlane makeSurface(const edm::EventSetup & eventSetup,const DTRecSegment4D & theSegment);
+  const MagneticField *makeMagneticField(const edm::EventSetup& eventSetup);
+	
+ private:
+  int counter;
+  std::string nameInLog;
+  bool saveRootFile;
+  int  saveRootFileEventsInterval;
+  std::string RootFileName;
+  /// back-end interface
+  DaqMonitorBEInterface * dbe;
+  LocalTrajectoryParameters makeLocalTrajectory(DTRecSegment4D theSegment);
+  bool debug;
+  TFile* theFile;
+  std::string theRecHits4DLabel;
+  TH1F *hPositionX;
 };
 
 #endif
--- DQM/RPCMonitorDigi/src/RPCMonitorEfficiency.cc
+++ DQM/RPCMonitorDigi/src/RPCMonitorEfficiency.cc
@@ -1,62 +1,108 @@
+
 /** \file
  *
  *  implementation of RPCMonitorEfficiency class
  *
- *  $Date: 2006/06/23 12:29:22 $
- *  $Revision: 1.8 $
+ *  $Date: 2006/06/26 13:25:29 $
+ *  $Revision: 1.1 $
  *
- * \author Ilaria Segoni
+ * \author  Camilo Carrillo
  */
 
 #include <map>
 #include <string>
-
 #include <DQM/RPCMonitorDigi/interface/RPCMonitorEfficiency.h>
-
-
-
 ///Log messages
 #include <FWCore/ServiceRegistry/interface/Service.h>
 #include <FWCore/MessageLogger/interface/MessageLogger.h>
 
+/* This Class Header */
+#include "RecoLocalMuon/DTSegment/test/DTRecSegment4DReader.h"
+
+/* Collaborating Class Header */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/DTRecHit/interface/DTRecSegment4DCollection.h"
+#include "TFile.h"
+#include "TH1F.h"
+
+/// for DQM ILA
+#include <Geometry/DTGeometry/interface/DTGeometry.h>
+#include <Geometry/Records/interface/MuonGeometryRecord.h>
+#include <FWCore/Framework/interface/ESHandle.h>
+#include <FWCore/Framework/interface/eventSetupGetImplementation.h>
+#include <MagneticField/Records/interface/IdealMagneticFieldRecord.h>
+#include <TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryError.h>
+#include <TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h>
 
 RPCMonitorEfficiency::RPCMonitorEfficiency( const edm::ParameterSet& pset ):counter(0){
-
   nameInLog = pset.getUntrackedParameter<std::string>("moduleLogName", "RPCEfficiency");
   saveRootFile  = pset.getUntrackedParameter<bool>("EfficDQMSaveRootFile", false); 
-  saveRootFileEventsInterval  = pset.getUntrackedParameter<int>("EfficEventsInterval", 10000); 
+  saveRootFileEventsInterval  = pset.getUntrackedParameter<int>("EfficEventsInterval", 100000); 
   RootFileName  = pset.getUntrackedParameter<std::string>("RootFileNameEfficiency", "RPCMonitorEfficiency.root"); 
-  
   /// get hold of back-end interface
   dbe = edm::Service<DaqMonitorBEInterface>().operator->();
-  
   edm::Service<MonitorDaemon> daemon;
   daemon.operator->();
-
-
 }
-
 
 RPCMonitorEfficiency::~RPCMonitorEfficiency(){
 }
 
-void RPCMonitorEfficiency::endJob(void)
-{
+void RPCMonitorEfficiency::endJob(void){
   dbe->save(RootFileName);  
 }
 
 
-void RPCMonitorEfficiency::analyze(const edm::Event& iEvent, 
-			       const edm::EventSetup& iSetup ){
- edm::LogInfo (nameInLog) <<"Beginning event efficiency evaluation " << counter;
-
-
+void RPCMonitorEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup ){
+  edm::LogInfo (nameInLog) <<"Beginning event efficiency evaluation " << counter;
+  
   if((!(counter%saveRootFileEventsInterval))&&(saveRootFile) ) {
     dbe->save(RootFileName);
   }
   
+  edm::Handle<DTRecSegment4DCollection> all4DSegments;
+  iEvent.getByLabel(theRecHits4DLabel, all4DSegments);
+  DTRecSegment4DCollection::const_iterator segment;
+  //std::cout<<"Reconstructed segments: "<<std::endl;
+  
+  for (segment = all4DSegments->begin(); segment != all4DSegments->end(); ++segment){
+    //std::cout<<"Chamber ID= "<<(*segment).chamberId()<<std::endl;
+    hPositionX->Fill( (*segment).localPosition().x());
+    LocalTrajectoryParameters localTrajectParameters=this->makeLocalTrajectory(*segment);
+    LocalTrajectoryError localError;
+    const BoundPlane  theSurface= this->makeSurface(iSetup,*segment);
+    const MagneticField * magField= this->makeMagneticField(iSetup);
+    edm::ESHandle<MagneticField> magfield;
+    iSetup.get<IdealMagneticFieldRecord>().get(magfield);
+    const MagneticField *field = magfield.product();
+    TrajectoryStateOnSurface*tsos=new TrajectoryStateOnSurface(localTrajectParameters,localError,theSurface,field,1);
+  }
+  //std::cout<<"---"<<std::endl;
   counter++;
 }
- 
- 
+
+LocalTrajectoryParameters RPCMonitorEfficiency::makeLocalTrajectory(DTRecSegment4D theSegment){
+  LocalPoint  thePoint= theSegment.localPosition();  
+  LocalVector theDirection=theSegment.localDirection();
+  LocalTrajectoryParameters theTrajectoryParameters(thePoint,theDirection,1);
+  return theTrajectoryParameters;
+}
+
+const BoundPlane RPCMonitorEfficiency::makeSurface(const edm::EventSetup&  eventSetup,const DTRecSegment4D &  theSegment){
+  edm::ESHandle<DTGeometry> pDD;
+  eventSetup.get<MuonGeometryRecord>().get(pDD);
+  const GeomDet* gdet=pDD->idToDet(theSegment.geographicalId());
+  const BoundPlane & surface = gdet->surface();
+  return  surface;
+}
+
+const MagneticField * RPCMonitorEfficiency::makeMagneticField(const edm::EventSetup& eventSetup){
+  edm::ESHandle<MagneticField> magfield;
+  eventSetup.get<IdealMagneticFieldRecord>().get(magfield);
+  const MagneticField *field = magfield.product();
+  return field;
+}
+
 
