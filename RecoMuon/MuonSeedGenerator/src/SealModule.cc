1ac8ca793489ae2cc8c46313fb505371d2756eaa
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-07-15 18:52:44 +0000

    cosmic muon seed generator

--- RecoMuon/MuonSeedGenerator/src/SealModule.cc
+++ RecoMuon/MuonSeedGenerator/src/SealModule.cc
@@ -2,8 +2,9 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+#include "RecoMuon/MuonSeedGenerator/src/CosmicMuonSeedGenerator.h"
 #include "RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(MuonSeedGenerator)
-
+DEFINE_ANOTHER_FWK_MODULE(CosmicMuonSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(MuonSeedGenerator);
