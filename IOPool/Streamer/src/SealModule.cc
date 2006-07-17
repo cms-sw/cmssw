df9be832347315d10fd3354669dc534b64527cf9
afaq <sha1-85f47aa78e37d28203e5c3f51090413d817de089@cern.ch>
2006-07-17 21:58:31 +0000

    SealModule.cc is changed to refelect newly added Templete classes for Streamer input and output

--- IOPool/Streamer/src/SealModule.cc
+++ IOPool/Streamer/src/SealModule.cc
@@ -4,37 +4,40 @@
 #include "IOPool/Streamer/src/TestConsumer.h"
 #include "IOPool/Streamer/interface/FragmentInput.h"
 #include "IOPool/Streamer/interface/EventStreamOutput.h"
-#include "IOPool/Streamer/src/EventStreamFileReader.h"
 #include "IOPool/Streamer/interface/HLTInfo.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-//#include "IOPool/Streamer/src/EventStreamFileWriter.h"
+#include "IOPool/Streamer/src/EventStreamFileReader.h"
 
-
+//New module to write events from Streamer files
+#include "IOPool/Streamer/interface/StreamerOutputModule.h"
 #include "IOPool/Streamer/src/StreamerFileWriter.h"
+
+//new module to read events from Streamer files
+#include "IOPool/Streamer/interface/StreamerInputModule.h"
 #include "IOPool/Streamer/src/StreamerFileReader.h"
 
-//typedef edm::EventStreamFileWriter StreamTestConsumer;
+typedef edm::StreamerOutputModule<edm::StreamerFileWriter> EventStreamFileWriter;
+typedef edm::StreamerInputModule<edm::StreamerFileReader> NewEventStreamFileReader;
 
-typedef edm::EventStreamingModule<edmtest::TestConsumer> StreamTestConsumer;
-
-//using edm::EventStreamFileWriter;
-
-using edmtestp::StreamerFileReader;
+using edm::StreamerFileReader;
 using edm::StreamerFileWriter;
-using stor::FragmentInput;
+
+//Old way of reading and writting Streamer files
+typedef edm::EventStreamingModule<edmtest::TestConsumer> StreamTestConsumer;
 using edmtestp::EventStreamFileReader;
+using stor::FragmentInput;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(StreamTestConsumer);
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(FragmentInput);
 
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(EventStreamFileReader);
-DEFINE_ANOTHER_FWK_INPUT_SOURCE(StreamerFileReader);
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(NewEventStreamFileReader);
 
-//DEFINE_ANOTHER_FWK_MODULE(EventStreamFileWriter);
-DEFINE_ANOTHER_FWK_MODULE(StreamerFileWriter);
+DEFINE_ANOTHER_FWK_MODULE(EventStreamFileWriter);
 
 using namespace edm::serviceregistry;
 using stor::HLTInfo;
 
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(HLTInfo,ParameterSetMaker<HLTInfo>)
+
