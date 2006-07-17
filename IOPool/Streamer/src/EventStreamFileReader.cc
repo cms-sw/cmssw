c245a1a859c0771277ac08c8bd276364ab939343
afaq <sha1-85f47aa78e37d28203e5c3f51090413d817de089@cern.ch>
2006-07-17 19:31:14 +0000

    Oh well, i renamed them by mistake renaming them back to correct compileable versions

---
+++ IOPool/Streamer/src/EventStreamFileReader.cc
@@ -0,0 +1,51 @@
+
+#include "IOPool/Streamer/src/EventStreamFileReader.h"
+#include "IOPool/Streamer/interface/BufferArea.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "IOPool/Streamer/interface/ClassFiller.h"
+#include "IOPool/Streamer/interface/Utilities.h"
+
+
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+using namespace edm;
+
+namespace edmtestp
+{  
+  // ----------------------------------
+
+  EventStreamFileReader::EventStreamFileReader(edm::ParameterSet const& pset,
+					       edm::InputSourceDescription const& desc):
+    edm::InputSource(pset, desc),
+    filename_(pset.getParameter<string>("fileName")),
+    ist_(filename_.c_str(),ios_base::binary | ios_base::in),
+    reader_(ist_)
+  {
+    if(!ist_)
+      {
+	throw cms::Exception("Configuration","EventStreamFileReader")
+	  << "cannot open file " << filename_;
+      }
+
+    std::auto_ptr<SendJobHeader> p = readHeaderFromStream(ist_);
+    SendDescs & descs = p->descs_;
+    edm::mergeWithRegistry(descs, productRegistry());
+
+    // jbk - the next line should not be needed
+    edm::declareStreamers(descs);
+    edm::buildClassCache(descs);
+    edm::loadExtraClasses();
+  }
+
+  EventStreamFileReader::~EventStreamFileReader()
+  {
+  }
+
+  std::auto_ptr<edm::EventPrincipal> EventStreamFileReader::read()
+  {
+    return reader_.read(productRegistry());
+  }
+
+}
