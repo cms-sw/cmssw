1216b274250f6cd5afe03bd3e5e4f22b37d6bf80
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-07-17 18:30:35 +0000

    Clear files to change permissions.

--- CondTools/IntegrationTest/scripts/EcalPedestals-object-setup.sh
+++
@@ -1,5 +0,0 @@
-MAPPING_FILE=${MAPPING_PATH}/EcalPedestals-mapping-custom_2.0.xml
-OBJECT_LIBRARY=CondFormatsEcalObjects
-OBJECT_NAME=EcalPedestals
-OBJECT_TABLE=ECALPEDESTALS
-TAG=${OBJECT_NAME}_from_online
