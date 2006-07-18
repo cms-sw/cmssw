08549ea65d3810e775ad078a06dd1aa2fb3c6ad2
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2006-07-18 20:49:21 +0000

    Add Map

---
+++ SimDataFormats/TrackingAnalysis/src/TrackVertexMap.cc
@@ -0,0 +1,8 @@
+#include "SimDataFormats/TrackingAnalysis/interface/TrackVertexMap.h"
+
+/*
+void TrackVertexMap::addVertexToTrack(TrackingParticleRef &t,TrackingVertexRef &v){
+  trackVertexMap_.insert(t,v);
+} 
+
+*/
