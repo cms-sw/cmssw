9171aec9b282e07a32170644942206cdc3b6a302
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2006-07-19 15:13:11 +0000

    add cfeb and use to id ME1a

--- CondFormats/CSCObjects/src/CSCReadoutMapping.cc
+++ CondFormats/CSCObjects/src/CSCReadoutMapping.cc
@@ -1,5 +1,6 @@
 #include <CondFormats/CSCObjects/interface/CSCReadoutMapping.h>
 #include <DataFormats/MuonDetId/interface/CSCDetId.h>
+#include <FWCore/MessageLogger/interface/MessageLogger.h>
 #include <iostream>
 
 CSCReadoutMapping::CSCReadoutMapping() : myName_("CSCReadoutMapping"), debugV_( false ) {}
@@ -14,20 +15,32 @@ int CSCReadoutMapping::chamber( int endcap, int station, int vme, int dmb, int t
   std::map<int,int>::const_iterator it = hw2sw_.find( hid );
   if ( it != hw2sw_.end() ) {
     cid = it->second;
-    if ( debugV() ) std::cout << myName_ << ": for requested hw id = " << hid <<
-       ", found sw id = " << cid << std::endl;
+    std::cout << "hwid = " << hid << ", swid = " << cid << std::endl;
+    //    LogDebug("CSC") << " for requested hw id = " << hid << ", found sw id = " << cid;
   }
   else {
-    std::cout << myName_ << ": ERROR, cannot find requested hw id = " << hid <<
-      " in mapping." << std::endl;
+    edm::LogError("CSC") << " cannot find requested hw id = " << hid << " in mapping.";
   }
   return cid;
 }
 
-CSCDetId CSCReadoutMapping::detId( int endcap, int station, int vme, int dmb, int tmb, int layer ) const {
+CSCDetId CSCReadoutMapping::detId( int endcap, int station, int vme, int dmb, int tmb, int cfeb, 
+        int layer ) const {
+
+  // Find CSCDetId index of chamber corresponding to the hardware readout arguments
   int cid = chamber( endcap, station, vme, dmb, tmb );
-  int lid = cid + layer;
-  return CSCDetId( lid );
+
+  // Decode the individual labels
+  int chamber = CSCDetId::chamber( cid );
+  int ring    = CSCDetId::ring( cid );
+
+  // Now sort out ME1a from ME11-combined
+  // cfeb =0-3 for ME1b, cfeb=4 for ME1a
+  if ( station == 1  && ring == 1 && cfeb == 4 ) {
+      // This is ME1a region
+      ring = 4; // reset from 1 to 4 which flags ME1a
+  }
+  return CSCDetId( endcap, station, ring, chamber, layer );
 }
 
 void CSCReadoutMapping::addRecord( int endcap, int station, int ring, int chamber, 
@@ -37,16 +50,16 @@ void CSCReadoutMapping::addRecord( int endcap, int station, int ring, int chambe
   mapping_.push_back( newRecord );
   int hid = hwId( endcap, station, vmecrate, dmb, tmb );
   int sid = swId( endcap, station, ring, chamber);
-  if ( debugV() ) std::cout << myName_ << ": map hw " << hid << " to sw " << sid << std::endl;
-  if ( hw2sw_.insert( std::make_pair( hid, sid) ).second ) {
-    if ( debugV() ) std::cout << myName_ << ": insert pair succeeded." << std::endl;
+  // LogDebug("CSC") << " map hw " << hid << " to sw " << sid;
+  if ( hw2sw_.insert( std::make_pair(hid, sid) ).second ) {
+    // LogDebug("CSC") << " insert pair succeeded.";
   }
   else {
-    std::cout << myName_ << ": ERROR, already have key = " << hid << std::endl;
+    edm::LogError("CSC") << " already have key = " << hid;
   }
 } 
 
 int CSCReadoutMapping::swId( int endcap, int station, int ring, int chamber ) const {
-  // Software id is just CSCDetId for the chamber
+  // Software id is just CSCDetId for the chamber - but no distinction within ME11
   return CSCDetId::rawIdMaker( endcap, station, ring, chamber, 0 ); // usual detid for chamber, i.e. layer=0
 }
