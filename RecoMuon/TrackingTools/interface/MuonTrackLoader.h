c2cb1f525177a35d63a49872c94cad55563ac454
Norbert Neumeister <sha1-7abc839d53ede37ea0c445f89ed7dc2d61338f2b@cern.ch>
2006-07-20 16:43:27 +0000

    *** empty log message ***

--- RecoMuon/TrackingTools/interface/MuonTrackLoader.h
+++ RecoMuon/TrackingTools/interface/MuonTrackLoader.h
@@ -5,42 +5,50 @@
  *  Class to load the tracks in the event, it provide some common functionalities
  *  both for all the RecoMuon producers.
  *
- *  $Date: 2006/07/06 09:19:05 $
- *  $Revision: 1.2 $
+ *  $Date: 2006/07/12 16:33:04 $
+ *  $Revision: 1.3 $
  *  \author R. Bellan - INFN Torino <riccardo.bellan@cern.ch>
  */
 
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
 #include "FWCore/Framework/interface/OrphanHandle.h"
 
 #include <vector>
 
-class Trajectory;
 namespace edm {class Event;}
 
+class Trajectory;
+
 class MuonTrackLoader {
- public:
-  typedef std::vector<Trajectory> TrajectoryContainer;
-  
-  /// Constructor
-  MuonTrackLoader() {};
 
-  /// Destructor
-  virtual ~MuonTrackLoader(){};
+  public:
 
-  // Operations
-  
-  /// Convert the trajectories in tracks and load the tracks in the event
-  edm::OrphanHandle<reco::TrackCollection> loadTracks(const TrajectoryContainer &trajectories, 
-						      edm::Event& event);
-  
- protected:
-  
- private:
-  reco::Track buildTrack (const Trajectory& trajectory) const;
-  reco::TrackExtra buildTrackExtra(const Trajectory& trajectory) const;
+    typedef std::vector<Trajectory> TrajectoryContainer;
+    typedef std::pair<Trajectory, reco::TrackRef> MuonCandidate;
+    typedef std::vector<MuonCandidate> CandidateContainer;
 
+  public:
+    
+    /// Constructor
+    MuonTrackLoader() {}
+
+    /// Destructor
+    virtual ~MuonTrackLoader() {}
+  
+    /// Convert the trajectories into tracks and load the tracks in the event
+    edm::OrphanHandle<reco::TrackCollection> loadTracks(const TrajectoryContainer&, 
+                                                        edm::Event&);
+
+    /// Convert the trajectories into tracks and load the tracks in the event
+    edm::OrphanHandle<reco::MuonCollection> loadTracks(const CandidateContainer&, 
+                                                       edm::Event&); 
+  
+  private:
+ 
+    reco::Track buildTrack (const Trajectory&) const;
+    reco::TrackExtra buildTrackExtra(const Trajectory&) const;
 
 };
 #endif
-
