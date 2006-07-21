ef98a7068491de0ab4f5124a3c474415ff802a96
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-07-21 14:33:55 +0000

    all modules migrated to templates

--- PhysicsTools/RecoAlgos/interface/SingleElementCollectionSelector.h
+++ PhysicsTools/RecoAlgos/interface/SingleElementCollectionSelector.h
@@ -7,9 +7,9 @@
  * 
  * \author Luca Lista, INFN
  *
- * \version $Revision: 1.2 $
+ * \version $Revision: 1.1 $
  *
- * $Id: SingleTrackSelector.h,v 1.2 2006/07/21 12:38:50 llista Exp $
+ * $Id: SingleElementCollectionSelector.h,v 1.1 2006/07/21 14:11:26 llista Exp $
  *
  */
 
@@ -22,15 +22,15 @@ struct SingleElementCollectionSelector {
   typedef typename container::const_iterator const_iterator;
   SingleElementCollectionSelector( const edm::ParameterSet & cfg ) : 
     select_( cfg ) { }
-  const_iterator begin() const { return c_.begin(); }
-  const_iterator end() const { return c_.end(); }
+  const_iterator begin() const { return selected_.begin(); }
+  const_iterator end() const { return selected_.end(); }
   void select( const reco::TrackCollection & c ) {
-    c_.clear();
+    selected_.clear();
     for( typename C::const_iterator i = c.begin(); i != c.end(); ++ i )
-      if ( select_( * i ) ) c_.push_back( & * i );
+      if ( select_( * i ) ) selected_.push_back( & * i );
   }
 private:
-  container c_;
+  container selected_;
   S select_;
 };
 
--- PhysicsTools/RecoAlgos/src/SealModule.cc
+++ PhysicsTools/RecoAlgos/src/SealModule.cc
@@ -1,11 +1,16 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "PhysicsTools/RecoAlgos/interface/TrackSelector.h"
 #include "PhysicsTools/RecoAlgos/interface/SingleTrackSelector.h"
 #include "PhysicsTools/RecoAlgos/interface/AnySelector.h"
-#include "PhysicsTools/RecoAlgos/src/LargestPtTrackSelector.h"
+#include "PhysicsTools/RecoAlgos/interface/SortCollectionSelector.h"
+#include "PhysicsTools/RecoAlgos/interface/PtComparator.h"
 #include "PhysicsTools/RecoAlgos/src/RecoModules.h"
 
 typedef SingleTrackSelector<AnySelector<reco::Track> > AnyTrackSelector;
+typedef TrackSelector<SortCollectionSelector<reco::TrackCollection, 
+					     PtInverseComparator<reco::Track> > 
+                     > LargestPtTrackSelector;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( AnyTrackSelector );
