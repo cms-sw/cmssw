001b91fcfac656c34820212a3452352a7735f84e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-07-21 18:55:28 +0000

    put back for backward compatibility

--- FWCore/Framework/interface/ModuleDescriptionRegistry.h
+++ FWCore/Framework/interface/ModuleDescriptionRegistry.h
@@ -1,1 +1,14 @@
-#include "DataFormats/Common/interface/ModuleDescriptionRegistry.h"
+#ifndef FWCoreFrameworkModuleDescriptionRegistry_h
+#define FWCoreFrameworkModuleDescriptionRegistry_h
+
+#include "FWCore/Utilities/interface/ThreadSafeRegistry.h"
+#include "DataFormats/Common/interface/ModuleDescription.h"
+#include "DataFormats/Common/interface/ModuleDescriptionID.h"
+
+namespace edm
+{
+  typedef edm::detail::ThreadSafeRegistry<edm::ModuleDescriptionID, edm::ModuleDescription> ModuleDescriptionRegistry;
+  typedef ModuleDescriptionRegistry::collection_type ModuleDescriptionMap;
+}
+
+#endif
--- FWCore/Framework/interface/ProcessHistoryRegistry.h
+++ FWCore/Framework/interface/ProcessHistoryRegistry.h
@@ -1,1 +1,14 @@
-#include "DataFormats/Common/interface/ProcessHistoryRegistry.h"
+#ifndef FWCoreFrameworkProcessHistoryRegistry_h
+#define FWCoreFrameworkProcessHistoryRegistry_h
+
+#include "FWCore/Utilities/interface/ThreadSafeRegistry.h"
+#include "DataFormats/Common/interface/ProcessHistory.h"
+#include "DataFormats/Common/interface/ProcessHistoryID.h"
+
+namespace edm
+{
+  typedef edm::detail::ThreadSafeRegistry<edm::ProcessHistoryID,edm::ProcessHistory> ProcessHistoryRegistry;
+  typedef ProcessHistoryRegistry::collection_type ProcessHistoryMap;
+}
+
+#endif
