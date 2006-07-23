76fdd538e74ec94a70f04103d9b5233d60e804e8
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-07-23 02:38:46 +0000

    fixes

--- TrackingTools/TransientTrackingRecHit/src/module.cc
+++ TrackingTools/TransientTrackingRecHit/src/module.cc
@@ -1,10 +1,6 @@
 #include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
 #include "TrackingTools/TransientTrackingRecHit/src/GenericTransientTrackingRecHitBuilderESProducer.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
-#include "FWCore/Framework/interface/ESProducer.h"
-#include "TrackingTools/PatternTools/interface/TrajectoryStateUpdator.h" 
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 EVENTSETUP_DATA_REG(TransientTrackingRecHitBuilder);
