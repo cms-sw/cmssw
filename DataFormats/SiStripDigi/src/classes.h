d1ff65b19a34ade250ecc1330c9dedd3b60df508
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-24 17:36:02 +0000

    changes concerning SiStripDigis class

--- DataFormats/SiStripDigi/src/classes.h
+++ DataFormats/SiStripDigi/src/classes.h
@@ -20,6 +20,7 @@ namespace {
 }
 
 #include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
+#include "DataFormats/Common/interface/traits.h"
 namespace {
   namespace {
     edm::Wrapper< SiStripRawDigi > raw0;
@@ -27,25 +28,40 @@ namespace {
     edm::Wrapper< edm::DetSet<SiStripRawDigi> > raw2;
     edm::Wrapper< std::vector<edm::DetSet<SiStripRawDigi> > > raw3;
     edm::Wrapper< edm::DetSetVector<SiStripRawDigi> > raw4;
+    edm::Wrapper< edm::DoNotSortUponInsertion > raw5;
   }
 }
 
 #include "DataFormats/SiStripDigi/interface/SiStripDigis.h"
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h"
+#include "boost/cstdint.hpp"
 namespace {
   namespace {
-    edm::Wrapper< SiStripDigis > digis;
-    edm::Wrapper< FEDRawDataCollection > fed_buffers;
-    edm::RefProd< FEDRawDataCollection > ref_to_fed_buffers;
-    edm::Wrapper< std::vector< bool > > list_of_fed_id;
-/*     edm::Wrapper< unsigned long > appended_bytes;  */
-/*     edm::Wrapper< unsigned long > daq_hdr_pos;  */
-/*     edm::Wrapper< unsigned long > trk_hdr_pos;  */
-/*     edm::Wrapper< unsigned long > apv_error_hdr_pos;  */
-/*     edm::Wrapper< unsigned long > full_debug_hdr_pos;  */
-/*     edm::Wrapper< unsigned long > apv_error_hdr_size;  */
-/*     edm::Wrapper< unsigned long > full_debug_hdr_size;  */
-/*     edm::Wrapper< std::string >   error;  */
+
+    edm::Wrapper<SiStripDigis> digis;
+
+    edm::Wrapper<FEDRawDataCollection> fed_buffers;
+    edm::RefProd<FEDRawDataCollection> ref_to_fed_buffers;
+
+    edm::Wrapper< std::vector<bool> > list_of_fed_id;
+
+    edm::Wrapper<uint8_t*> ptr_to_buffer;
+    edm::Wrapper< std::vector<uint8_t*> > ptrs_to_buffers;
+
+    edm::Wrapper<uint32_t> size_of_buffer;
+    edm::Wrapper< std::vector<uint32_t> > size_of_buffers;
+
+    edm::Wrapper<sistrip::FedReadoutMode> fed_readout_mode;
+    edm::Wrapper< std::vector<sistrip::FedReadoutMode> > fed_readout_modes;
+
+    edm::Wrapper<sistrip::FedReadoutPath> fed_readout_path;
+    edm::Wrapper< std::vector<sistrip::FedReadoutPath> > fed_readout_paths;
+
+    edm::Wrapper<uint16_t> payload_position;
+    edm::Wrapper< std::vector<uint16_t> > payload_positions;
+    edm::Wrapper< std::vector< std::vector<uint16_t> > > more_payload_positions;
+
   }
 }
 
