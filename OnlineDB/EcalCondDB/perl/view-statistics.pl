41687b14468e7e391d802c9ba6292c3c19adcad9
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-07-24 20:49:56 +0000

    Adding extents of IDs

--- OnlineDB/EcalCondDB/perl/view-statistics.pl
+++ OnlineDB/EcalCondDB/perl/view-statistics.pl
@@ -12,8 +12,12 @@ my $condDB = ConnectionFile::connect();
 
 my $dbh = $condDB->{dbh};
 
-my $sql = qq[  select cv.name, cv.maps_to, count(cv.logic_id), min(cv.logic_id) m,
-	       cd.id1name, cd.id2name, cd.id3name, cd.description 
+my $sql = qq[  select cv.name, cv.maps_to, count(cv.logic_id), 
+               min(cv.logic_id) m, max(cv.logic_id),
+	       cd.id1name, min(cv.id1), max(cv.id1), 
+               cd.id2name, min(cv.id2), max(cv.id2), 
+               cd.id3name, min(cv.id3), max(cv.id3), 
+               cd.description 
 	       from channelView cv, viewDescription cd 
 	       where cv.name = cd.name AND cv.name = cv.maps_to
 	       group by cv.name, cv.maps_to, cd.id1name, 
@@ -23,7 +27,10 @@ my $sql = qq[  select cv.name, cv.maps_to, count(cv.logic_id), min(cv.logic_id) 
 
 my @data = @{$dbh->selectall_arrayref($sql)};
 
-my @headers = qw(name maps_to count min_logic_id id1name id2name id3name
+my @headers = qw(name maps_to count min_logic_id max_logic_id 
+                 id1name min_id1 max_id1 
+                 id2name min_id2 max_id2
+                 id3name min_id3 max_id3
 		 description);
 
 my $head = join(",", map("\"$_\"", @headers));
@@ -40,13 +47,17 @@ foreach (@data) {
 print "\n";
 print "CHANNEL VIEW MAPPINGS\n";
 print $head, "\n";
-$sql = qq[  select cv.name, cv.maps_to, count(cv.logic_id), min(cv.logic_id) m,
-	    cd.id1name, cd.id2name, cd.id3name, cd.description 
-	    from channelView cv, viewDescription cd 
-	    where cv.name = cd.name AND cv.name != cv.maps_to
-	    group by cv.name, cv.maps_to, cd.id1name, 
-	    cd.id2name, cd.id3name, cd.description 
-            order by m
+$sql = qq[ select cv.name, cv.maps_to, count(cv.logic_id), 
+           min(cv.logic_id) m, max(cv.logic_id),
+	   cd.id1name, min(cv.id1), max(cv.id1), 
+           cd.id2name, min(cv.id2), max(cv.id2), 
+           cd.id3name, min(cv.id3), max(cv.id3), 
+           cd.description 
+	   from channelView cv, viewDescription cd 
+	   where cv.name = cd.name AND cv.name != cv.maps_to
+	   group by cv.name, cv.maps_to, cd.id1name, 
+	   cd.id2name, cd.id3name, cd.description 
+           order by m 
          ];
 
 @data = @{$dbh->selectall_arrayref($sql)};
