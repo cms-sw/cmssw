c61a7b1bfe4229f1a634cf8ed75e8bdb9ddb4919
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-25 15:11:35 +0000

    added Monitorables struct

--- DQM/SiStripCommissioningAnalysis/interface/ApvTimingAnalysis.h
+++ DQM/SiStripCommissioningAnalysis/interface/ApvTimingAnalysis.h
@@ -2,7 +2,11 @@
 #define DQM_SiStripCommissioningAnalysis_ApvTimingAnalysis_H
 
 #include "DQM/SiStripCommissioningAnalysis/interface/CommissioningAnalysis.h"
+#include <boost/cstdint.hpp>
+#include <sstream>
 #include <vector>
+
+class TProfile;
 
 /**
    @class : ApvTimingAnalysis
@@ -13,19 +17,38 @@
 class ApvTimingAnalysis : public CommissioningAnalysis {
   
  public:
-
-  /** Constructor */
+  
   ApvTimingAnalysis() {;}
-
-  /** Destructor */
   virtual ~ApvTimingAnalysis() {;}
+
+  /** Simple container class that holds various parameter values that
+      are extracted from the "tick mark" histogram by the analysis. */
+  class Monitorables {
+  public:
+    uint16_t coarse_; // PLL coarse delay setting
+    uint16_t fine_;   // PLL fine delay setting
+    float delay_;     // Time delay (from coarse and fine values) [ns]
+    float error_;     // Error on time delay [ns]
+    float base_;      // Level of tick mark "base" [adc]
+    float peak_;      // Level of tick mark "peak" [adc]
+    float height_;    // Tick mark height [ADC]
+    Monitorables() : 
+      coarse_(0), fine_(0), 
+      delay_(0.), error_(0.), 
+      base_(0.), peak_(0.), height_(0.) {;}
+    void print( std::stringstream& );
+  };
+  
+  /** Takes a TProfile histo containing an APV tick mark, extracts
+      various parameter values from the histogram, and fills the
+      Monitorables object. */
+  static void analysis( const TProfile* const, Monitorables& ); 
   
   /** Takes a vector containing one TProfile of a tick mark and fills a vector of 2 unsigned shorts representing the rise time of the tick. The first is a coarse-time measurement (units of 25ns), the second a fine-time (units 1/24th of the coarse). */
-  virtual void analysis( const vector<const TProfile*>& histos, 
-			      vector<unsigned short>& monitorables);
+  static void analysis( const std::vector<const TProfile*>& histos, 
+			std::vector<unsigned short>& monitorables );
+  
 };
-
-
 
 #endif // DQM_SiStripCommissioningAnalysis_ApvTimingAnalysis_H
 
