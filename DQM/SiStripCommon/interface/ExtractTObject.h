f0f05ebcb8f90f5ad8692563cda6d0bd748551cb
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-25 15:03:47 +0000

    added method to insert root object within dqm fwk

--- DQM/SiStripCommon/interface/ExtractTObject.h
+++ DQM/SiStripCommon/interface/ExtractTObject.h
@@ -4,6 +4,7 @@
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "DQMServices/Core/interface/MonitorElementT.h"
 #include "TNamed.h"
+#include <string>
 
 /** */
 template <class T> 
@@ -11,12 +12,26 @@ class ExtractTObject {
   
  public:
   
-  T* operator() ( MonitorElement* me ) { 
+  T* extract( MonitorElement* me ) { 
     if ( me ) {
       MonitorElementT<TNamed>* tnamed = dynamic_cast< MonitorElementT<TNamed>* >( me );
       if ( tnamed ) {
 	T* histo = dynamic_cast<T*>( tnamed->operator->() );
-	if( histo ) { return histo; }
+	if ( histo ) { return histo; }
+	else { return 0; }
+      } else { return 0; }
+    } else { return 0; }
+  }
+  
+  T* insert( MonitorElement* me, T* new_histo, std::string new_name = "" ) { 
+    if ( me ) {
+      MonitorElementT<TNamed>* tnamed = dynamic_cast< MonitorElementT<TNamed>* >( me );
+      if ( tnamed ) {
+	T* old_histo = dynamic_cast<T*>( tnamed->operator->() );
+	if ( old_histo ) { 
+	  tnamed->insert( new_histo, new_name );
+	  return tnamed->operator->(); 
+	}
 	else { return 0; }
       } else { return 0; }
     } else { return 0; }
@@ -25,3 +40,4 @@ class ExtractTObject {
 };
 
 #endif // DQM_SiStripCommon_ExtractTObject_H
+
