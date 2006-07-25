6a5d2af77b591f062cdfcb0e0eafb060def235a0
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-25 18:28:16 +0000

    impl for commissioning summary factory

---
+++ DQM/SiStripCommissioningSummary/src/SummaryFactory.cc
@@ -0,0 +1,12 @@
+#include "DQM/SiStripCommissioningSummary/interface/SummaryFactory.h"
+#include "DQM/SiStripCommissioningSummary/interface/CommissioningSummary.h"
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+//
+CommissioningSummary* SummaryFactory::book( const SummaryFactory::Histo& histo, 
+					    const string& directory ) {
+  //@@ something here...
+}
+
