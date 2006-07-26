2768d0f14bc0f9796e726cb102eeef8b4129ea19
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-07-26 12:06:52 +0000

    trivial change

--- DQM/SiStripCommissioningSummary/src/SummaryFactory.cc
+++ DQM/SiStripCommissioningSummary/src/SummaryFactory.cc
@@ -7,6 +7,7 @@ using namespace std;
 //
 CommissioningSummary* SummaryFactory::book( const SummaryFactory::Histo& histo, 
 					    const string& directory ) {
+  return 0;
   //@@ something here...
 }
 
