6fc5ec4294de287992a4bb90a375975970c8033b
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-07-27 23:55:30 +0000

    Adding a simple dumper for tdc raw information

--- RecoTBCalo/EcalTBTDCReconstructor/src/SealModule.cc
+++ RecoTBCalo/EcalTBTDCReconstructor/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTBCalo/EcalTBTDCReconstructor/interface/EcalTBTDCRecInfoProducer.h"
+#include "RecoTBCalo/EcalTBTDCReconstructor/interface/EcalTBTDCRawInfoDumper.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalTBTDCRecInfoProducer );
+DEFINE_ANOTHER_FWK_MODULE( EcalTBTDCRawInfoDumper );
 
