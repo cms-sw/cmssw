f3020d78ae80d29fd36a4c6f7f8407aa69f5f828
Jordan Damgov <sha1-c571eab30c7a5b3fdf0a5cbf96ae23d3d4909587@cern.ch>
2006-07-27 03:01:18 +0000

    Separated setADSs04 and setADSs06, new counters.

--- TBDataFormats/HcalTBObjects/interface/HcalTBBeamCounters.h
+++ TBDataFormats/HcalTBObjects/interface/HcalTBBeamCounters.h
@@ -13,47 +13,81 @@
     // Getter methods
 
     /// Muon Veto adc 
-    unsigned short VMadc()     const { return VMadc_;     }
-    unsigned short V3adc()     const { return V3adc_;     }
-    unsigned short V6adc()     const { return V6adc_;     }
-    unsigned short VH1adc()     const { return VH1adc_;     }
-    unsigned short VH2adc()     const { return VH2adc_;     }
-    unsigned short VH3adc()     const { return VH3adc_;     }
-    unsigned short VH4adc()     const { return VH4adc_;     }
-    unsigned short CK2adc()     const { return CK2adc_;     }
-    unsigned short CK3adc()     const { return CK3adc_;     }
-    unsigned short SciVLEadc()     const { return SciVLEadc_;     }
-    unsigned short Sci521adc()     const { return Sci521adc_;     }
-    unsigned short Sci528adc()     const { return Sci528adc_;     }
-    unsigned short S1()     const { return S1_;     }
-    unsigned short S2()     const { return S2_;     }
-    unsigned short S3()     const { return S3_;     }
-    unsigned short S4()     const { return S4_;     }
+    double VMadc()     const { return VMadc_;     }
+    double V3adc()     const { return V3adc_;     }
+    double V6adc()     const { return V6adc_;     }
+    double VH1adc()     const { return VH1adc_;     }
+    double VH2adc()     const { return VH2adc_;     }
+    double VH3adc()     const { return VH3adc_;     }
+    double VH4adc()     const { return VH4adc_;     }
+    double CK2adc()     const { return CK2adc_;     }
+    double CK3adc()     const { return CK3adc_;     }
+    double SciVLEadc()     const { return SciVLEadc_;     }
+    double Sci521adc()     const { return Sci521adc_;     }
+    double Sci528adc()     const { return Sci528adc_;     }
+    double S1adc()     const { return S1adc_;     }
+    double S2adc()     const { return S2adc_;     }
+    double S3adc()     const { return S3adc_;     }
+    double S4adc()     const { return S4adc_;     }
+    double VMFadc()     const { return VMFadc_;     }
+    double VMBadc()     const { return VMFadc_;     }
+    double VM1adc()     const { return VM1adc_;     }
+    double VM2adc()     const { return VM2adc_;     }
+    double VM3adc()     const { return VM3adc_;     }
+    double VM4adc()     const { return VM4adc_;     }
+    double VM5adc()     const { return VM5adc_;     }
+    double VM6adc()     const { return VM6adc_;     }
+    double VM7adc()     const { return VM7adc_;     }
+    double VM8adc()     const { return VM8adc_;     }
+    double TOF1adc()     const { return TOF1adc_;     }
+    double TOF2adc()     const { return TOF2adc_;     }
 
     // Setter methods
-    void   setADCs (uint16_t VMadc,uint16_t V3adc,uint16_t V6adc,
-                                     uint16_t VH1adc ,uint16_t VH2adc,uint16_t VH3adc,uint16_t VH4adc,
-                                     uint16_t CK2adc,uint16_t CK3adc,uint16_t SciVLEadc,
-                                     uint16_t Sci521adc,uint16_t Sci528adc,
-                                     uint16_t S1,uint16_t S2,uint16_t S3,uint16_t S4);
+    void   setADCs04 (double VMadc,double V3adc,double V6adc,
+                                     double VH1adc ,double VH2adc,double VH3adc,double VH4adc,
+                                     double CK2adc,double CK3adc,double SciVLEadc,
+                                     double Sci521adc,double Sci528adc,
+                                     double S1,double S2,double S3,double S4);
+    void   setADCs06 (double VMFadc,double VMBadc,
+                                     double VM1adc ,double VM2adc,double VM3adc,double VM4adc,
+                                     double VM5adc ,double VM6adc,double VM7adc,double VM8adc,
+                                     double CK2adc,double CK3adc,double SciVLEadc,
+                                     double S1adc,double S2adc,double S3adc,double S4adc,
+				     double TOF1adc,double TOF2adc);
 
   private:
-	uint16_t    VMadc_ ;
-	uint16_t    V3adc_ ;
-	uint16_t    V6adc_ ;
-	uint16_t    VH1adc_ ;
-	uint16_t    VH2adc_ ;
-	uint16_t    VH3adc_ ;
-	uint16_t    VH4adc_ ;
-	uint16_t    CK2adc_ ;
-	uint16_t    CK3adc_ ;
-	uint16_t    SciVLEadc_ ; 
-	uint16_t    Sci521adc_ ;
-	uint16_t    Sci528adc_ ;
-	uint16_t    S1_ ;
-	uint16_t    S2_ ;
-	uint16_t    S3_ ;
-	uint16_t    S4_ ;
+//    TB2004 specific
+	double    VMadc_ ; // behind HO
+	double    V3adc_ ; // behind HB at (eta,phi)=(7,3)
+	double    V6adc_ ;  // behind HB at (eta,phi)=(7,6)
+	double    VH1adc_ ; // part of extra muon veto wall - the end of TB04 data taking
+	double    VH2adc_ ; // part of extra muon veto wall - the end of TB04 data taking
+	double    VH3adc_ ; // part of extra muon veto wall - the end of TB04 data taking
+	double    VH4adc_ ; // part of extra muon veto wall - the end of TB04 data taking
+	double    Sci521adc_ ; // Scintilator at 521m (see beam line drawings)
+	double    Sci528adc_ ; // Scintilator at 522m (see beam line drawings)
+//   Common for TB2004 and TB2006
+	double    CK2adc_ ; // Cerenkov 2 : electron id 
+	double    CK3adc_ ; // Cerenkov 3 : pi/proton separation
+	double    SciVLEadc_ ; // Scintillator in VLE beam line 
+	double    S1adc_ ; // Trigger scintilator 14x14 cm
+	double    S2adc_ ; // Trigger scintilator 4x4 cm
+	double    S3adc_ ; // Trigger scintilator 2x2 cm
+	double    S4adc_ ; // Trigger scintilator 14x14 cm
+//    TB2006 specific
+	double    VMFadc_ ; // VM front
+	double    VMBadc_ ; // VM back
+	double    VM1adc_ ; // Muon veto wall
+	double    VM2adc_ ; // Muon veto wall
+	double    VM3adc_ ; // Muon veto wall
+	double    VM4adc_ ; // Muon veto wall
+	double    VM5adc_ ; // Muon veto wall
+	double    VM6adc_ ; // Muon veto wall
+	double    VM7adc_ ; // Muon veto wall
+	double    VM8adc_ ; // Muon veto wall
+	double    TOF1adc_ ; // TOF1
+	double    TOF2adc_ ; // TOF2
+
 
   };
 
--- TBDataFormats/HcalTBObjects/src/HcalTBBeamCounters.cc
+++ TBDataFormats/HcalTBObjects/src/HcalTBBeamCounters.cc
@@ -4,33 +4,63 @@ using namespace std;
 
   HcalTBBeamCounters::HcalTBBeamCounters() :
     VMadc_(0),V3adc_(0),V6adc_(0),VH1adc_(0),VH2adc_(0),VH3adc_(0),VH4adc_(0),
-    CK2adc_(0),CK3adc_(0),SciVLEadc_(0),Sci521adc_(0),Sci528adc_(0),
-    S1_(0),S2_(0),S3_(0),S4_(0){}
+    Sci521adc_(0),Sci528adc_(0),CK2adc_(0),CK3adc_(0),SciVLEadc_(0),
+    S1adc_(0),S2adc_(0),S3adc_(0),S4adc_(0),VMFadc_(0),VMBadc_(0),
+    VM1adc_(0),VM2adc_(0),VM3adc_(0),VM4adc_(0),VM5adc_(0),VM6adc_(0),VM7adc_(0),VM8adc_(0),
+    TOF1adc_(0),TOF2adc_(0){}
 
-  void HcalTBBeamCounters::setADCs  (uint16_t VMadc,uint16_t V3adc,uint16_t V6adc,
-				     uint16_t VH1adc ,uint16_t VH2adc,uint16_t VH3adc,uint16_t VH4adc,
-				     uint16_t CK2adc,uint16_t CK3adc,uint16_t SciVLEadc,
-				     uint16_t Sci521adc,uint16_t Sci528adc,
-                                     uint16_t S1,uint16_t S2,uint16_t S3,uint16_t S4)
+  void HcalTBBeamCounters::setADCs04  (double VMadc,double V3adc,double V6adc,
+				     double VH1adc ,double VH2adc,double VH3adc,double VH4adc,
+				     double CK2adc,double CK3adc,double SciVLEadc,
+				     double Sci521adc,double Sci528adc,
+                                     double S1adc,double S2adc,double S3adc,double S4adc)
 
   {
-    VMadc_ = VMadc % 0x4000;
-    V3adc_ = V3adc % 0x4000;
-    V6adc_ = V6adc % 0x4000;
-    VH1adc_ = VH1adc % 0x4000;
-    VH2adc_ = VH2adc % 0x4000;
-    VH3adc_ = VH3adc % 0x4000;
-    VH4adc_ = VH4adc % 0x4000;
-    CK2adc_ = CK2adc % 0x4000;
-    CK3adc_ = CK3adc % 0x4000;
-    SciVLEadc_ = SciVLEadc % 0x4000;
-    Sci521adc_ = Sci521adc % 0x4000;
-    Sci528adc_ = Sci528adc % 0x4000;
-    S1_ = S1 % 0x4000;
-    S2_ = S2 % 0x4000;
-    S3_ = S3 % 0x4000;
-    S4_ = S4 % 0x4000;
+    VMadc_ = VMadc ;
+    V3adc_ = V3adc ;
+    V6adc_ = V6adc ;
+    VH1adc_ = VH1adc ;
+    VH2adc_ = VH2adc ;
+    VH3adc_ = VH3adc ;
+    VH4adc_ = VH4adc ;
+    CK2adc_ = CK2adc ;
+    CK3adc_ = CK3adc ;
+    SciVLEadc_ = SciVLEadc ;
+    Sci521adc_ = Sci521adc ;
+    Sci528adc_ = Sci528adc ;
+    S1adc_ = S1adc ;
+    S2adc_ = S2adc ;
+    S3adc_ = S3adc ;
+    S4adc_ = S4adc ;
   }
+   void  HcalTBBeamCounters::setADCs06 (double VMFadc,double VMBadc,
+                                     double VM1adc ,double VM2adc,double VM3adc,double VM4adc,
+                                     double VM5adc ,double VM6adc,double VM7adc,double VM8adc,
+                                     double CK2adc,double CK3adc,double SciVLEadc,
+                                     double S1adc,double S2adc,double S3adc,double S4adc,
+                                     double TOF1adc,double TOF2adc)
+  {
+   VMFadc_    = VMFadc ;
+   VMBadc_    = VMBadc ;
+   VM1adc_    = VM1adc ;
+   VM2adc_    = VM2adc ;
+   VM3adc_    = VM3adc ;
+   VM4adc_    = VM4adc ;
+   VM5adc_    = VM5adc ;
+   VM6adc_    = VM6adc ;
+   VM7adc_    = VM7adc ;
+   VM8adc_    = VM8adc ;
+   CK2adc_    = CK2adc ;
+   CK3adc_    = CK3adc ;
+   SciVLEadc_ = SciVLEadc ;
+   S1adc_     = S1adc ;
+   S2adc_     = S2adc ;
+   S3adc_     = S3adc ;
+   S4adc_     = S4adc ;
+   TOF1adc_   = TOF1adc ;
+   TOF2adc_   = TOF2adc ;
+  }
+
 
   ostream& operator<<(ostream& s, const HcalTBBeamCounters& htbcnt) {
 
