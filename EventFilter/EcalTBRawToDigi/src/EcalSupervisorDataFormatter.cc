64b0bcfda1383b753b3136b6b8d934c0721a0304
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-07-27 23:43:04 +0000

    Adding EcalSupervisor unpacker

---
+++ EventFilter/EcalTBRawToDigi/src/EcalSupervisorDataFormatter.cc
@@ -0,0 +1,79 @@
+#include "EcalSupervisorDataFormatter.h"
+
+using namespace edm;
+using namespace std;
+
+#include <iostream>
+
+EcalSupervisorDataFormatter::EcalSupervisorDataFormatter () {
+}
+
+void EcalSupervisorDataFormatter::interpretRawData( const FEDRawData & fedData, 
+					   EcalTBEventHeader& tbEventHeader)
+{
+  const ulong * buffer = ( reinterpret_cast<ulong*>(const_cast<unsigned char*> ( fedData.data())));
+  int fedLenght                        = fedData.size(); // in Bytes
+  
+  // check ultimate fed size and strip off fed-header and -trailer
+  if (fedLenght != (nWordsPerEvent *4) )
+    {
+      LogError("EcalSupervisorDataFormatter") << "EcalSupervisorData has size "  <<  fedLenght
+				       <<" Bytes as opposed to expected " 
+				       << (nWordsPerEvent *4)
+				       << ". Returning."<< endl;
+      return;
+    }
+
+  ulong a=1; // used to extract an 8 Bytes word from fed 
+  ulong b=1; // used to manipulate the 8 Bytes word and get what needed
+
+  int wordCounter =0;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a & 0xfff00000);
+  b = b >> 20;
+  tbEventHeader.setBurstNumber(b);
+  LogDebug("EcalSupervisorDataFormatter") << "Burst number:\t" << b << endl;
+  //Skipping the second word
+  wordCounter +=1;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0x80000000);
+  b = b >> 31;
+  tbEventHeader.setSyncError(b & 0x1);
+  LogDebug("EcalSupervisorDataFormatter") << "Sync Error:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffff);
+  tbEventHeader.setRunNumber(b);
+  LogDebug("EcalSupervisorDataFormatter") << "Run Number:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xff);
+  int version = b;
+  LogDebug("EcalSupervisorDataFormatter") << "Version Number:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setEventNumber(b);
+  LogDebug("EcalSupervisorDataFormatter") << "Event Number:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setBegBurstTimeSec(b);
+  LogDebug("EcalSupervisorDataFormatter") << "BegBurstTimeSec:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setBegBurstTimeMsec(b);
+  LogDebug("EcalSupervisorDataFormatter") << "BegBurstTimeMsec:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setEndBurstTimeSec(b);
+  LogDebug("EcalSupervisorDataFormatter") << "EndBurstTimeSec:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setEndBurstTimeMsec(b);
+  LogDebug("EcalSupervisorDataFormatter") << "EndBurstTimeMsec:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setBegBurstLV1A(b);
+  LogDebug("EcalSupervisorDataFormatter") << "BegBurstLV1A:\t" << b << endl;
+  a = buffer[wordCounter];wordCounter++;
+  b = (a& 0xffffffff);
+  tbEventHeader.setEndBurstLV1A(b);
+  LogDebug("EcalSupervisorDataFormatter") << "EndBurstLV1A:\t" << b << endl;
+}
--- EventFilter/EcalTBRawToDigi/src/EcalSupervisorDataFormatter.h
+++ EventFilter/EcalTBRawToDigi/src/EcalSupervisorDataFormatter.h
@@ -2,14 +2,11 @@
 #define EcalSupervisorDataFormatter_H
 /** \class EcalSupervisorDataFormatter
  *
- *  $Id: $
+ *  $Id: EcalSupervisorDataFormatter.h,v 1.1 2006/07/21 12:36:25 meridian Exp $
  */
 
 #include <TBDataFormats/EcalTBObjects/interface/EcalTBCollections.h>
-
-#include <vector> 
-#include <map>
-#include <iostream>
+#include <DataFormats/FEDRawData/interface/FEDRawData.h>
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -22,12 +19,15 @@ class EcalSupervisorDataFormatter   {
 
  public:
 
-  EcalSupervisorDataFormatter() {};
+  EcalSupervisorDataFormatter() ;
   virtual ~EcalSupervisorDataFormatter(){LogDebug("EcalTBRawToDigi") << "@SUB=EcalSupervisorDataFormatter" << "\n"; };
 
   //Method to be implemented
-  void  interpretRawData( const FEDRawData & data, EcalTBEventHeader& tbEventHeader ) {};
+  void  interpretRawData( const FEDRawData & data, EcalTBEventHeader& tbEventHeader ) ;
+
  private:
+
+  static const int nWordsPerEvent = 14;    
 
 };
 #endif
