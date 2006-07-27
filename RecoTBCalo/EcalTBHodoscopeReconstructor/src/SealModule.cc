729e565a72cdd2627004d5b5082b4c753418a2d5
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-07-27 23:54:30 +0000

    Adding simple dumper of raw hodoscope information

--- RecoTBCalo/EcalTBHodoscopeReconstructor/src/SealModule.cc
+++ RecoTBCalo/EcalTBHodoscopeReconstructor/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTBCalo/EcalTBHodoscopeReconstructor/interface/EcalTBHodoscopeRecInfoProducer.h"
+#include "RecoTBCalo/EcalTBHodoscopeReconstructor/interface/EcalTBHodoscopeRawInfoDumper.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalTBHodoscopeRecInfoProducer );
+DEFINE_ANOTHER_FWK_MODULE( EcalTBHodoscopeRawInfoDumper );
 
