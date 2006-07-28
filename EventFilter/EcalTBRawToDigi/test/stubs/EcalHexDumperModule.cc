95a31a31133a816635573f35673274616eb66159
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2006-07-28 17:39:45 +0000

    HexDumperModule can now also dump DCC feds to binary file

--- EventFilter/EcalTBRawToDigi/test/stubs/EcalHexDumperModule.cc
+++ EventFilter/EcalTBRawToDigi/test/stubs/EcalHexDumperModule.cc
@@ -17,6 +17,10 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
+#include <stdio.h>
+#include <fstream>
 
 #include <iomanip> 
 
@@ -29,19 +33,28 @@ class EcalHexDumperModule: public edm::EDAnalyzer{
  public:
 
   EcalHexDumperModule(const edm::ParameterSet& ps){  
-    verbosity= ps.getUntrackedParameter<int>("verbosity",1);
+    verbosity_= ps.getUntrackedParameter<int>("verbosity",1);
+    
+    beg_DCC_fed_id_= ps.getUntrackedParameter<int>("beg_DCC_fed_id",600);
+    end_DCC_fed_id_= ps.getUntrackedParameter<int>("end_DCC_fed_id",670);
+
+    writeDcc_ =ps.getUntrackedParameter<bool>("writeDCC",false);
+    filename_  =ps.getUntrackedParameter<string>("filename","dump.bin");
     event_ =0;
   }
 
   
  protected:
-  int verbosity;
-  int event_;
+  int       verbosity_;
+  bool   writeDcc_;
+  int      beg_DCC_fed_id_;
+  int      end_DCC_fed_id_;
+  string filename_;
+  int      event_;
 
   void analyze( const edm::Event & e, const  edm::EventSetup& c);
 
-
-};// class EcalHexDumperModule
+};
 
 
 
@@ -49,15 +62,17 @@ void EcalHexDumperModule::analyze( const edm::Event & e, const  edm::EventSetup&
   
   edm::Handle<FEDRawDataCollection> rawdata;
   e.getByType(rawdata);  
+
+  ofstream dumpFile (filename_.c_str(),ios::app );
   
   event_++;
-
+  
   for (int id= 0; id<=FEDNumbering::lastFEDId(); ++id){ 
 
     const FEDRawData& data = rawdata->FEDData(id);
-
+    
     if (data.size()>4){      
- 
+      
       cout << "\n\n\n[EcalHexDumperModule] Event: " 
 	   << dec << event_ 
 	   << " fed: " << id 
@@ -69,12 +84,10 @@ void EcalHexDumperModule::analyze( const edm::Event & e, const  edm::EventSetup&
 	  cout<< "Fed size in bits not multiple of 64, strange." << endl;
 	  cout << "***********************************************" << endl;
 	}
-
-
+      
+      
       int length = data.size();
-      const ulong * pData = ( reinterpret_cast<ulong*>(const_cast<unsigned char*> ( data.data())));
-
-
+      const ulong               * pData     = ( reinterpret_cast<ulong*>(const_cast<unsigned char*> ( data.data())));
       cout << setfill('0');
       for (int words=0; words < length/4; (words+=2)  )
 	{
@@ -83,14 +96,18 @@ void EcalHexDumperModule::analyze( const edm::Event & e, const  edm::EventSetup&
 	}
 
       cout << "\n";
-            
+
+
+      if (beg_DCC_fed_id_ <= id && id <= end_DCC_fed_id_ && writeDcc_)
+	{
+	  dumpFile.write( reinterpret_cast <const char *> (pData), length);
+	}
     }
-
+    
   }
-
-
+  dumpFile.close();    
+  if (! writeDcc_) remove(filename_.c_str()); 
 }
-
 
 
 DEFINE_FWK_MODULE(EcalHexDumperModule)
