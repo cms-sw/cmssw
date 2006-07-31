a62740511f50dce658e10c2a5b9f622065ae9693
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2006-07-31 14:34:36 +0000

    Added L1TriggerObject class to classes_def.xml

--- DataFormats/L1GlobalTrigger/src/classes.h
+++ DataFormats/L1GlobalTrigger/src/classes.h
@@ -1,13 +1,15 @@
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1TriggerObject.h"
 #include <vector>
 
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace { namespace {
 
-    edm::Wrapper<std::vector<bool> > dummy1;
-       
+    edm::Wrapper<std::vector<bool> > dummy0;
+
     // dictionary for L1 Global Trigger Readout Record
-    edm::Wrapper<L1GlobalTriggerReadoutRecord> dummy2;
+    L1GlobalTriggerReadoutRecord dummy10;
+    edm::Wrapper<L1GlobalTriggerReadoutRecord> dummy11;
 } }
 
