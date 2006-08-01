05750cfd95169c7018f2c14c48a52086f7391515
Katarzyna Maria Dziedziniewicz-Wojcik <sha1-4895fc69171a71d75d30db9cda108018f786222b@cern.ch>
2006-08-01 12:25:50 +0000

    *** empty log message ***

--- CondCore/OptAlignPlugins/src/plugin.cc
+++ CondCore/OptAlignPlugins/src/plugin.cc
@@ -10,12 +10,16 @@
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignments.h"
 #include "CondFormats/OptAlignObjects/interface/CSCZSensors.h"
 #include "CondFormats/OptAlignObjects/interface/CSCRSensors.h"
+#include "CondFormats/OptAlignObjects/interface/Inclinometers.h"
+#include "CondFormats/OptAlignObjects/interface/PX_sensors.h"
 
 #include "CondFormats/OptAlignObjects/interface/MBAChBenchCalPlate.h"
 #include "CondFormats/OptAlignObjects/interface/MBAChBenchSurveyPlate.h"
 #include "CondFormats/DataRecord/interface/OpticalAlignmentsRcd.h"
 #include "CondFormats/DataRecord/interface/CSCZSensorsRcd.h"
 #include "CondFormats/DataRecord/interface/CSCRSensorsRcd.h"
+#include "CondFormats/DataRecord/interface/InclinometersRcd.h"
+#include "CondFormats/DataRecord/interface/PX_sensorsRcd.h"
 
 #include "CondFormats/DataRecord/interface/MBAChBenchCalPlateRcd.h"
 #include "CondFormats/DataRecord/interface/MBAChBenchSurveyPlateRcd.h"
@@ -25,3 +29,5 @@ REGISTER_PLUGIN(CSCZSensorsRcd,CSCZSensors);
 REGISTER_PLUGIN(CSCRSensorsRcd,CSCRSensors);
 REGISTER_PLUGIN(MBAChBenchCalPlateRcd,MBAChBenchCalPlate);
 REGISTER_PLUGIN(MBAChBenchSurveyPlateRcd,MBAChBenchSurveyPlate);
+REGISTER_PLUGIN(InclinometersRcd, Inclinometers);
+REGISTER_PLUGIN(PX_sensorsRcd, PX_sensors);
