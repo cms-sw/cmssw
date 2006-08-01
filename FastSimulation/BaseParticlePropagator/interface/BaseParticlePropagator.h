82d88ba74850b8822c2a9f1f6d42122a8c1be71b
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2006-08-01 16:26:02 +0000

    New package to reduce dependencies

---
+++ FastSimulation/BaseParticlePropagator/interface/BaseParticlePropagator.h
@@ -0,0 +1,213 @@
+/*Emacs: -*- C++ -*- */
+#ifndef BASEPARTICLEPROPAGATOR_H
+#define BASEPARTICLEPROPAGATOR_H
+
+/**
+* This class is aimed at propagating charged and neutral 
+* particles (yet under the form of a RawParticle) from 
+* a given vertex to a cylinder, defined by a radius rCyl 
+* and a length 2*zCyl, centered in (0,0,0) and whose axis
+* is parallel to the B field (B is oriented along z, by 
+* definition of the z axis).
+*
+* The main method
+*
+*     bool propagate()
+*
+* returns true if an intersection between the propagated 
+* RawParticle and the cylinder is found. The location of 
+* the intersection is given by the value of success:
+*
+*   * success = 2 : an intersection is found forward in 
+*                   the cylinder endcaps. The RawParticle
+*                   vertex and momentum are overwritten by
+*                   the values at this intersection.
+*
+*   * success = 1 : an intersection is found forward in 
+*                   the cylinder barrel. The RawParticle
+*                   vertex and momentum are overwritten by
+*                   the values at this intersection.
+*
+*   * success =-2 : an intersection is found backward in 
+*                   the cylinder endcaps. The RawParticle
+*                   vertex and momentum are NOT overwritten
+*                   by the values at this intersection.
+*
+*   * success =-1 : an intersection is found backward in 
+*                   the cylinder barrel. The RawParticle
+*                   vertex and momentum are NOT overwritten
+*                   by the values at this intersection.
+*
+*   * success = 0 : No intersection is found after half a
+*                   loop (only possible if firstLoop=true).
+*                   The vertex and momentum are overwritten
+*                   by the values after this half loop. 
+*
+* 
+* The method
+*
+*     propagated()
+*
+* returns a new RawParticle with the propagated coordinates, 
+* if overwriting is not considered an advantage by the user.
+*
+* Particles can be propagated backward with the method
+*
+*     backPropagate()
+*
+* Member functions 
+*
+*  o propagateToPreshowerLayer1(),  
+*  o propagateToPreshowerLayer2(),  
+*  o propagateToEcalEntrance(),  
+*  o propagateToHcalEntrance(),  
+*  o propagateToHcalExit(),  
+*  o propagateToClosestApproach(),  
+*
+* only need a momentum, a vertex and an electric charge
+* to operate. Radii, half-lengths and default B field (4T)
+* are set therein by default.
+*
+* As of today, no average loss of energy (dE/dx, Brems) is
+* considered in the propagation. No uncertainty (multiple
+* scattering, dE/dx, Brems) is yet implemented.
+*
+* \author Patrick Janot 
+* $Date : 19-Aug-2002, with subsequent modification for FAMOS
+* \version 15-Dec-2003 */
+
+//FAMOS
+#include "FastSimulation/Particle/interface/RawParticle.h"
+
+//CLHEP
+#include "CLHEP/Units/PhysicalConstants.h" // for c_light
+
+class BaseParticlePropagator : public RawParticle {
+  
+public:
+
+  /// Default c'tor
+  BaseParticlePropagator();
+
+  /** Constructor taking as arguments a RawParticle, as well as the radius,
+      half-height and magnetic field defining the cylinder for which 
+      propagation is to be performed */
+  BaseParticlePropagator(const RawParticle& myPart, double r, double z, double B);
+
+  /// Initialize internal switches and quantities
+  void init();
+
+  /** Update the current instance, after the propagation of the particle 
+      to the surface of the cylinder */
+  bool propagate();
+
+  /** Update the current instance, after the back-propagation of the particle 
+      to the surface of the cylinder */
+  bool backPropagate();
+
+  /** Return a new instance, corresponding to the particle propagated
+      to the surface of the cylinder */
+  BaseParticlePropagator propagated() const;
+
+  /** Update the particle after propagation to the closest approach from 
+      Z axis, to the preshower layer 1 & 2, to the ECAL entrance, to the 
+      HCAL entrance, the HCAL 2nd and 3rd layer (not coded yet), the VFCAL 
+      entrance, or any BoundSurface(disk or cylinder)*/
+  bool propagateToClosestApproach(bool first=true);
+  bool propagateToEcal(bool first=true);
+  bool propagateToPreshowerLayer1(bool first=true);
+  bool propagateToPreshowerLayer2(bool first=true);
+  bool propagateToEcalEntrance(bool first=true);
+  bool propagateToHcalEntrance(bool first=true);
+  bool propagateToVFcalEntrance(bool first=true);
+  bool propagateToHcalExit(bool first=true);
+  bool propagateToNominalVertex(
+      const HepLorentzVector& hit2=HepLorentzVector(0.,0.,0.,0.));
+
+  /// Return position (hereafter called "vertex") three-vector of particle
+  Hep3Vector position() const;
+  /// Return momentum four-vector of particle
+  HepLorentzVector momentum() const;
+
+  /// Transverse impact parameter
+  double xyImpactParameter() const;
+  /// Longitudinal impact parameter
+  double zImpactParameter() const;
+
+  /// Is the vertex inside the cylinder ? (stricly inside : true) 
+  bool           inside() const;
+  bool           inside(double rPos) const;
+  /// Is the vertex already on the cylinder surface ? 
+  bool           onSurface() const;
+  bool           onSurface(double rPos) const;
+  /// Is the vertex already on the cylinder barrel ? 
+  bool           onBarrel() const;
+  bool           onBarrel(double rPos) const;
+  /// Is the vertex already on the cylinder endcap ? 
+  bool           onEndcap() const;
+  bool           onEndcap(double rPos) const;
+  /// Is the vertex on some material ?
+  bool           onFiducial() const { return fiducial; }
+  /// Has the particle decayed while propagated ?
+  bool           hasDecayed() const { return decayed; }
+  /// Has propagation been performed and was barrel or endcap reached ?
+  int           getSuccess() const { return success;  }
+  /// Set the magnetic field
+  inline void setMagneticField(double b) {  bField=b; };
+  /// Get the magnetic field 
+  inline double getMagneticField() const {  return bField; };
+    
+  /// Set the propagation characteristics (rCyl, zCyl and first loop only)
+  void setPropagationConditions(double r, double z, bool firstLoop=true);
+
+  void increaseRCyl(double delta) {rCyl = rCyl + delta;}
+
+private:
+  /// Simulated particle that is to be resp has been propagated
+  //  RawParticle   particle;
+  /// Radius of the cylinder (centred at 0,0,0) to which propagation is done
+  double rCyl;
+  /// Half-height of the cylinder (centred at 0,0,0) to which propagation is done
+  double zCyl;
+  /// Magnetic field in the cylinder, oriented along the Z axis
+  double bField;
+
+protected:
+  /// 0:propagation still be done, 1:reached 'barrel', 2:reached 'endcaps'
+  int success;
+  /// The particle traverses some real material
+  bool fiducial;
+
+private:
+  /// Do only the first half-loop
+  bool firstLoop;
+  /// The particle decayed while propagated !
+  bool decayed;
+  /// The proper time of the particle
+  double properTime;
+  /// The proper decay time of the particle
+  double properDecayTime;
+  /// The propagation direction 
+  int propDir;
+
+public:
+  /// The helix Radius
+  double        helixRadius() const;
+  double        helixRadius(double pT) const;
+  /// The azimuth of the momentum at the vertex
+  double        helixStartPhi() const;
+  /// The x coordinate of the helix axis
+  double        helixCentreX() const;
+  double        helixCentreX(double radius, double phi) const;
+  /// The y coordinate of the helix axis
+  double        helixCentreY() const;
+  double        helixCentreY(double radius, double phi) const;
+  /// The distance between the cylinder and the helix axes
+  double        helixCentreDistToAxis() const;
+  double        helixCentreDistToAxis(double xC, double yC) const;
+  /// The azimuth if the vector joining the cylinder and the helix axes
+  double        helixCentrePhi() const;
+  double        helixCentrePhi(double xC, double yC) const;
+};
+
+#endif
---
+++ FastSimulation/BaseParticlePropagator/src/BaseParticlePropagator.cc
@@ -0,0 +1,618 @@
+//FAMOS headers
+#include "FastSimulation/ParticlePropagator/interface/BaseParticlePropagator.h"
+
+//CLHEP Headers
+#include "CLHEP/Random/Random.h"
+#include "CLHEP/Random/RandFlat.h"
+
+#include <iomanip>
+//#include <fstream>
+//#include <iostream>
+
+using namespace std;
+
+BaseParticlePropagator::BaseParticlePropagator() :
+  RawParticle(), rCyl(0.), zCyl(0.), bField(0)
+{;}
+
+BaseParticlePropagator::BaseParticlePropagator( 
+         const RawParticle& myPart,double R, double Z, double B ) :
+  RawParticle(myPart), rCyl(R), zCyl(Z), bField(B) 
+{
+  init();
+}
+
+void 
+BaseParticlePropagator::init() {
+  
+  // The status of the propagation
+  success = 0;
+  // Propagate only for the first half-loop
+  firstLoop = true;
+  //
+  fiducial = true;
+  // The particle has not yet decayed
+  decayed = false;
+  // The proper time is set to zero
+  properTime = 0.;
+  // The propagation direction is along the momentum
+  propDir = 1;
+  // And this is the proper time at which the particle will decay
+  properDecayTime = 
+    (pid()==0||pid()==22||abs(pid())==11||abs(pid())==2112||abs(pid())==2212) ?
+    1E99 : -PDGcTau() * log(RandFlat::shoot());
+
+}
+
+bool 
+BaseParticlePropagator::propagate() { 
+  //
+  // Check that the particle is not already on the cylinder surface
+  //
+  double rPos = position().perp();
+
+  if ( onBarrel(rPos) ) { 
+    success = 1;
+    return true;
+  }
+  //
+  if ( onEndcap(rPos) ) { 
+    success = 2;
+    return true;
+  }
+  //
+  // Treat neutral particles (or no magnetic field) first 
+  //
+  if ( charge() == 0.0 || bField == 0.0 ) {
+    //
+    // First check that the particle crosses the cylinder
+    //
+    double pT = perp();
+    double pT2 = pT*pT;
+    double b2 = rCyl * pT;
+    double ac = fabs ( x()*py() - y()*px() );
+    //
+    // The particle never crosses (or never crossed) the cylinder
+    //
+    if ( ac > b2 ) return false;
+
+    double delta  = sqrt(b2*b2 - ac*ac);
+    double tplus  = -( x()*px()+y()*py() ) + delta;
+    double tminus = -( x()*px()+y()*py() ) - delta;
+    //
+    // Find the first (time-wise) intersection point with the cylinder
+    //
+    double solution = tminus < 0 ? tplus/pT2 : tminus/pT2;
+    if ( solution < 0. ) return false;
+    //
+    // Check that the particle does (did) not exit from the endcaps first.
+    //
+    double zProp = z() + pz() * solution;
+    if ( fabs(zProp) > zCyl ) {
+      tplus  = ( zCyl - z() ) / pz();
+      tminus = (-zCyl - z() ) / pz();
+      solution = tminus < 0 ? tplus : tminus;
+      if ( solution < 0. ) return false;
+      zProp = z() + pz() * solution;
+      success = 2;
+    }
+    else {
+      success = 1;
+    }
+    //
+    // Check the decay time
+    //
+    double delTime = propDir * (zProp-z())*mass()/pz();
+    double factor = 1.;
+    properTime += delTime;
+    if ( properTime > properDecayTime ) {
+      factor = 1.-(properTime-properDecayTime)/delTime;
+      properTime = properDecayTime;
+      decayed = true;
+    }
+
+    //
+    // Compute the coordinates of the RawParticle after propagation
+    //
+    double xProp = x() + px() * solution * factor;
+    double yProp = y() + py() * solution * factor;
+           zProp = z() + pz() * solution * factor;
+    double tProp = t() + e()  * solution * factor;
+  //
+  // ... and update the particle with its propagated coordinates
+  //
+    setVertex( HepLorentzVector(xProp,yProp,zProp,tProp) );
+
+    return true;
+  }
+  //
+  // Treat charged particles with magnetic field next.
+  //
+  else {
+    //
+    // First check that the particle can cross the cylinder
+    //
+    double pT = perp();
+    double radius = helixRadius(pT);
+    double phi0 = helixStartPhi();
+    double xC = helixCentreX(radius,phi0);
+    double yC = helixCentreY(radius,phi0);
+    double dist = helixCentreDistToAxis(xC,yC);
+    //
+    // The helix either is outside or includes the cylinder -> no intersection
+    //
+    if ( fabs ( fabs(radius) - dist ) > rCyl ) return false;
+    //
+    // The particle is already away from the endcaps, and will never come back
+    // Could be back-propagated, so warn the user that it is so.
+    // 
+    if ( z() * pz() > zCyl * fabs(pz()) ) { 
+      success = -2;
+      return true;
+    }
+    //
+    // The particle is already on the barrel and needs not be propgated again
+    //
+    // if ( vertex().perp() == rCyl ) return false;
+    //
+    //    double pT = perp();
+    double pZ = pz();
+    double phiProp, zProp;
+    //
+    // Does the helix cross the cylinder barrel ? If not, do the first half-loop
+    //
+    double rProp = min(fabs(radius)+dist-0.000001, rCyl);
+    //
+    // Compute phi after propagation (two solutions, but the asin definition
+    // (between -pi/2 and +pi/2) takes care of the wrong one.
+    //
+    phiProp = helixCentrePhi(xC,yC) + ( inside(rPos) || onSurface(rPos) ? 
+	    asin ( (rProp*rProp-radius*radius-dist*dist) / (2*dist*radius) ) :
+       M_PI-asin ( (rProp*rProp-radius*radius-dist*dist) / (2*dist*radius) ) );
+    //
+    // Solve the obvious two-pi ambiguities: more than one turn!
+    //
+    if ( abs(phiProp - phi0) > 2.*M_PI )
+      phiProp += phiProp > phi0 ? -2.*M_PI : +2.*M_PI;
+    //
+    // Check that the phi difference has the right sign, according to Q*B
+    // (Another two pi ambuiguity, slightly more subtle)
+    //
+    if ( (phiProp-phi0)*radius < 0.0 )
+      radius > 0.0 ? phiProp += 2 * M_PI : phiProp -= 2 * M_PI;
+    //
+    // Check that the particle does not exit from the endcaps first.
+    //
+    zProp = z() + ( phiProp - phi0 ) * pZ * radius / pT ;
+    if ( fabs(zProp) > zCyl ) {
+
+      zProp = zCyl * fabs(pZ)/pZ;
+      phiProp = phi0 + (zProp - z()) / radius * pT / pZ;
+      success = 2;
+
+    } else {
+
+      //
+      // If the particle does not cross the barrel, either process the
+      // first-half loop only or propagate all the way down to the endcaps
+      //
+      if ( rProp < rCyl ) {
+
+	if ( firstLoop ) {
+
+	  success = 0;
+
+	} else {
+
+	  zProp = zCyl * fabs(pZ)/pZ;
+	  phiProp = phi0 + (zProp - z()) / radius * pT / pZ;
+	  success = 2;
+
+	}
+      //
+      // The particle crossed the barrel 
+      // 
+      } else {
+
+	success = 1;
+
+      }
+    }
+    //
+    // Compute the coordinates of the RawParticle after propagation
+    //
+    //
+    // Check the decay time
+    //
+    double delTime = propDir * (zProp-z())*mass()/pz();
+    double factor = 1.;
+    properTime += delTime;
+    if ( properTime > properDecayTime ) {
+      factor = 1.-(properTime-properDecayTime)/delTime;
+      properTime = properDecayTime;
+      decayed = true;
+    }
+
+    zProp = z() + (zProp-z())*factor;
+    phiProp = phi0 + (zProp - z()) / radius * pT / pZ;
+
+    double sProp = sin(phiProp);
+    double cProp = cos(phiProp);
+    double xProp = xC + radius * sProp;
+    double yProp = yC - radius * cProp;
+    double tProp = t() + (zProp-z())*e()/pz();
+    double pxProp = pT * cProp;
+    double pyProp = pT * sProp;
+    //
+    // Last check : Although propagated to the endcaps, R could still be
+    // larger than rCyl because the cylinder was too short...
+    //
+    if ( success == 2 && sqrt(xProp*xProp+yProp*yProp) > rCyl ) { 
+      success = -1;
+      return true;
+    }
+    //
+    // ... and update the particle vertex and momentum
+    //
+    setVertex( HepLorentzVector(xProp,yProp,zProp,tProp) );
+    setPx(pxProp);
+    setPy(pyProp);
+    return true;
+ 
+  }
+}
+
+bool
+BaseParticlePropagator::backPropagate() {
+
+  // Backpropagate
+  setPx(-px()); setPy(-py()); setPz(-pz()); setCharge(-charge());
+  propDir = -1;
+  bool done = propagate();
+  setPx(-px()); setPy(-py()); setPz(-pz()); setCharge(-charge());
+  propDir = +1;
+
+  return done;
+}
+
+BaseParticlePropagator
+BaseParticlePropagator::propagated() const {
+  // Copy the input particle in a new instance
+  BaseParticlePropagator myPropPart(*this);
+  // Allow for many loops
+  myPropPart.firstLoop=false;
+  // Propagate the particle ! 
+  myPropPart.propagate();
+  // Back propagate if the forward propagation was not a success
+  if (myPropPart.success < 0 )
+    myPropPart.backPropagate();
+  // Return the new instance
+  return myPropPart;
+}
+
+void 
+BaseParticlePropagator::setPropagationConditions(double R, double Z, bool f) { 
+  rCyl = R;
+  zCyl = Z;
+  firstLoop = f;
+}
+
+bool
+BaseParticlePropagator::propagateToClosestApproach(bool first) {
+  //
+  // Propagation to point of clostest approach to z axis
+  //
+  double r,z;
+  if ( charge() != 0.0 && bField != 0.0 ) {
+    r = fabs ( fabs( helixRadius() )
+	       - helixCentreDistToAxis() ) + 0.0000001;
+  }
+  else {
+    r = fabs( px() * y() - py() * x() ) / perp(); 
+  }
+
+  z = 999999.;
+
+  setPropagationConditions(r , z, first);
+
+  return backPropagate();
+
+}
+
+bool
+BaseParticlePropagator::propagateToEcal(bool first) {
+  //
+  // Propagation to Ecal (including preshower) after the
+  // last Tracker layer
+  // TODO: include proper geometry
+  // Geometry taken from CMS ECAL TDR
+  //
+  // First propagate to global barrel / endcap cylinder 
+  //  setPropagationConditions(1290. , 3045 , first);
+  setPropagationConditions(129.0 , 303.16 , first);
+  return propagate();
+
+}
+
+bool
+BaseParticlePropagator::propagateToPreshowerLayer1(bool first) {
+  //
+  // Propagation to Preshower Layer 1
+  // TODO: include proper geometry
+  // Geometry taken from CMS ECAL TDR
+  //
+  // First propagate to global barrel / endcap cylinder 
+  //  setPropagationConditions(1290., 3045 , first);
+  setPropagationConditions(129.0, 303.16 , first);
+  bool done = propagate();
+
+  // Check that were are on the Layer 1 
+  if ( done && (vertex().perp() > 125.0 || vertex().perp() < 45.0) ) 
+    success = 0;
+  
+  return done;
+}
+
+bool
+BaseParticlePropagator::propagateToPreshowerLayer2(bool first) {
+  //
+  // Propagation to Preshower Layer 2
+  // TODO: include proper geometry
+  // Geometry taken from CMS ECAL TDR
+  //
+  // First propagate to global barrel / endcap cylinder 
+  //  setPropagationConditions(1290. , 3090 , first);
+  setPropagationConditions(129.0 , 307.13 , first);
+  bool done = propagate();
+
+  // Check that we are on Layer 2 
+  if ( done && (vertex().perp() > 125.0 || vertex().perp() < 45.0 ))
+    success = 0;
+
+  return done;
+
+}
+
+bool
+BaseParticlePropagator::propagateToEcalEntrance(bool first) {
+  //
+  // Propagation to ECAL entrance
+  // TODO: include proper geometry
+  // Geometry taken from CMS ECAL TDR
+  //
+  // First propagate to global barrel / endcap cylinder 
+  setPropagationConditions(129.0 , 317.0,first);
+  bool done = propagate();
+
+  // Where are we ?
+  double eta = fabs ( position().eta() );
+
+  // Go to endcap cylinder in the "barrel cut corner" 
+  if ( done && eta > 1.479 && success == 1 ) {
+    setPropagationConditions(171.1 , 317.0, first);
+    done = propagate();
+  }
+
+  // We are not in the ECAL acceptance
+  if ( fabs ( position().eta() ) > 3.0 ) success = 0;
+
+  return done;
+}
+
+bool
+BaseParticlePropagator::propagateToHcalEntrance(bool first) {
+  //
+  // Propagation to HCAL entrance
+  // TODO: include proper geometry
+  // Geometry taken from DAQ TDR Chapter 13
+  //
+
+  // First propagate to global barrel / endcap cylinder 
+  setPropagationConditions(183.0 , 388.0, first);
+  propDir = 0;
+  bool done = propagate();
+  propDir = 1;
+
+  // We are not in the HCAL acceptance
+  if ( done && fabs ( position().eta() ) > 3.0 ) success = 0;
+
+  return done;
+}
+
+bool
+BaseParticlePropagator::propagateToVFcalEntrance(bool first) {
+  //
+  // Propagation to VFCAL entrance
+  // TODO: include proper geometry
+  // Geometry taken from DAQ TDR Chapter 13
+
+  setPropagationConditions(200.0 , 1110.0, first);
+  propDir = 0;
+  bool done = propagate();
+  propDir = 1;
+
+  // We are not in the VFCAL acceptance
+  double feta = fabs ( position().eta() );
+  if ( done && ( feta < 3 || feta > 5.0 ) ) success = 0;
+
+  return done;
+}
+
+bool
+BaseParticlePropagator::propagateToHcalExit(bool first) {
+  //
+  // Propagation to HCAL exit
+  // TODO: include proper geometry
+  // Geometry taken from DAQ TDR Chapter 13
+  //
+
+  // Approximate it to a single cylinder as it is not that crucial.
+  setPropagationConditions(285.0 , 560.0, first);
+  //  this->rawPart().setCharge(0.0); ?? Shower Propagation ??
+  propDir = 0;
+  bool done = propagate();
+  propDir = 1;
+
+  return done;
+}
+
+bool
+BaseParticlePropagator::propagateToNominalVertex(const HepLorentzVector& v) 
+{
+
+  // Not implemented for neutrals (used for electrons only)
+  if ( charge() == 0. || bField == 0.) return false;
+
+  // Define the proper pT direction to meet the point (vx,vy) in (x,y)
+  double dx = x()-v.x();
+  double dy = y()-v.y();
+  double phi = atan2(dy,dx) + asin ( sqrt(dx*dx+dy*dy)/(2.*helixRadius()) );
+
+  // The absolute pT (kept to the original value)
+  double pT = perp();
+
+  // Set the new pT
+  setPx(pT*cos(phi));
+  setPy(pT*sin(phi));
+
+  return propagateToClosestApproach();
+  
+}
+
+double
+BaseParticlePropagator::xyImpactParameter() const {
+  // Transverse impact parameter
+  return ( charge() != 0.0 && bField != 0.0 ) ? 
+    helixCentreDistToAxis() - fabs( helixRadius() ) :
+    fabs( px() * y() - py() * x() ) / perp(); 
+}
+
+double
+BaseParticlePropagator::zImpactParameter() const {
+  // Longitudinal impact parameter
+  return vertex().z() - vertex().perp() * pz() / perp();
+}
+
+double 
+BaseParticlePropagator::helixRadius() const { 
+  // The helix Radius
+  //
+  // The helix' Radius sign accounts for the orientation of the magnetic field 
+  // (+ = along z axis) and the sign of the electric charge of the particle. 
+  // It signs the rotation of the (charged) particle around the z axis: 
+  // Positive means anti-clockwise, negative means clockwise rotation.
+  //
+  // The radius is returned in cm to match the units in RawParticle.
+  return charge() == 0 ? 0.0 : - perp() / ( c_light * 1e-5 * bField * charge() );
+}
+
+double 
+BaseParticlePropagator::helixRadius(double pT) const { 
+  // a faster version of helixRadius, once perp() has been computed
+  return charge() == 0 ? 0.0 : - pT / ( c_light * 1e-5 * bField * charge() );
+}
+
+double 
+BaseParticlePropagator::helixStartPhi() const { 
+  // The azimuth of the momentum at the vertex
+  return px() == 0.0 && py() == 0.0 ? 0.0 : atan2(py(),px());
+}
+
+double 
+BaseParticlePropagator::helixCentreX() const { 
+  // The x coordinate of the helix axis
+  return x() - helixRadius() * sin ( helixStartPhi() );
+}
+
+double 
+BaseParticlePropagator::helixCentreY() const { 
+  // The y coordinate of the helix axis
+  return y() + helixRadius() * cos ( helixStartPhi() );
+}
+
+double 
+BaseParticlePropagator::helixCentreX(double radius, double phi) const { 
+  // Fast version of helixCentreX()
+  return x() - radius * sin (phi);
+}
+
+double 
+BaseParticlePropagator::helixCentreY(double radius, double phi) const { 
+  // Fast version of helixCentreX()
+  return y() + radius * cos (phi);
+}
+
+double 
+BaseParticlePropagator::helixCentreDistToAxis() const { 
+  // The distance between the cylinder and the helix axes
+  double xC = helixCentreX();
+  double yC = helixCentreY();
+  return sqrt( xC*xC + yC*yC );
+}
+
+double 
+BaseParticlePropagator::helixCentreDistToAxis(double xC, double yC) const { 
+  // Faster version of helixCentreDistToAxis
+  //  double xC = helixCentreX();
+  //  double yC = helixCentreY();
+  return sqrt( xC*xC + yC*yC );
+}
+
+double 
+BaseParticlePropagator::helixCentrePhi() const { 
+  // The azimuth if the vector joining the cylinder and the helix axes
+  double xC = helixCentreX();
+  double yC = helixCentreY();
+  return xC == 0.0 && yC == 0.0 ? 0.0 : atan2(yC,xC);
+}
+
+double 
+BaseParticlePropagator::helixCentrePhi(double xC, double yC) const { 
+  // Faster version of helixCentrePhi() 
+  //  double xC = helixCentreX();
+  //  double yC = helixCentreY();
+  return xC == 0.0 && yC == 0.0 ? 0.0 : atan2(yC,xC);
+}
+
+Hep3Vector
+BaseParticlePropagator::position() const { return vertex().vect(); }
+
+HepLorentzVector
+BaseParticlePropagator::momentum() const { 
+  return HepLorentzVector(px(),py(),pz(),e()); 
+}
+
+bool
+BaseParticlePropagator::inside() const {
+  return (position().perp()<rCyl-0.00001 && fabs(z())<zCyl-0.00001);}
+
+bool BaseParticlePropagator::onSurface() const {
+  return ( onBarrel() || onEndcap() ); 
+}
+
+bool BaseParticlePropagator::onBarrel() const {
+  return ( fabs(position().perp()-rCyl) < 0.00001 && fabs(z()) <= zCyl );
+}
+
+bool BaseParticlePropagator::onEndcap() const {
+  return ( fabs(fabs(z())-zCyl) < 0.00001 && position().perp() <= rCyl ); 
+}
+
+// Faster versions
+bool
+BaseParticlePropagator::inside(double rPos) const {
+  return (rPos<rCyl-0.00001 && fabs(z())<zCyl-0.00001);}
+
+bool BaseParticlePropagator::onSurface(double rPos) const {
+  return ( onBarrel(rPos) || onEndcap(rPos) ); 
+}
+
+bool BaseParticlePropagator::onBarrel(double rPos) const {
+  return ( fabs(rPos-rCyl) < 0.00001 && fabs(z()) <= zCyl );
+}
+
+bool BaseParticlePropagator::onEndcap(double rPos) const {
+  return ( fabs(fabs(z())-zCyl) < 0.00001 && rPos <= rCyl ); 
+}
+
+
