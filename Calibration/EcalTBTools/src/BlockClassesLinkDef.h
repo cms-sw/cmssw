b4e81d884fb71086f22cd2e684b6597e6afe27f5
Pietro Govoni <sha1-b5f71fec85fef77f7f4bdfe5a60a81247d80ee44@cern.ch>
2006-08-01 09:45:13 +0000

    first commit

---
+++ Calibration/EcalTBTools/src/BlockClassesLinkDef.h
@@ -0,0 +1,12 @@
+#include "Calibration/EcalTBTools/interface/BlockClasses.h"
+#include "Calibration/EcalTBTools/interface/TB06Reco.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TB06Reco+ ;
+
+#endif
