fa2489d6233eea660b91344d9a83a79f5079e347
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2006-08-02 16:36:44 +0000

    associate fitted tracks with electrons

--- RecoEgamma/EgammaElectronProducers/src/SealModule.cc
+++ RecoEgamma/EgammaElectronProducers/src/SealModule.cc
@@ -1,6 +1,7 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/SiStripElectronProducer.h"
+#include "RecoEgamma/EgammaElectronProducers/interface/SiStripElectronAssociator.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronProducer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedProducer.h"
@@ -11,6 +12,7 @@ DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(ElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronProducer);
+DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAssociator);
 DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedProducer)
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronProducer)
