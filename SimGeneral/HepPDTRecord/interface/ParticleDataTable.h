281422cf64b3a5be0e7386584aacc78649ca8245
Filip Moortgat <sha1-236ec69c42ab223e0df5c43733dcda6ad72fa5d0@cern.ch>
2006-08-03 08:23:34 +0000

    add  typedefs

--- SimGeneral/HepPDTRecord/interface/ParticleDataTable.h
+++ SimGeneral/HepPDTRecord/interface/ParticleDataTable.h
@@ -1,11 +1,15 @@
 #ifndef HepPDTProducer_ParticleDataTable_h
 #define HepPDTProducer_ParticleDataTable_h
-// $Id: ParticleDataTable.h,v 1.1 2006/03/14 16:22:30 llista Exp $
+// $Id: ParticleDataTable.h,v 1.2 2006/03/14 17:01:38 llista Exp $
 
 #include "FWCore/Framework/interface/data_default_record_trait.h"
 #include <CLHEP/HepPDT/DefaultConfig.hh>
 #include <CLHEP/HepPDT/ParticleDataTableT.hh>
 #include "SimGeneral/HepPDTRecord/interface/PDTRecord.h"
+
+typedef DefaultConfig::ParticleDataTable ParticleDataTable;
+typedef DefaultConfig::ParticleData ParticleData;
+
 
 EVENTSETUP_DATA_DEFAULT_RECORD( DefaultConfig::ParticleDataTable, PDTRecord );
 
