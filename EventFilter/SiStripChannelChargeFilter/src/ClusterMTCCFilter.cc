41961c45266c5f9ce7029cbe7beb254e3394d1d9
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-08-04 13:39:01 +0000

    update to ClusterMTCCFilter

--- EventFilter/SiStripChannelChargeFilter/src/ClusterMTCCFilter.cc
+++ EventFilter/SiStripChannelChargeFilter/src/ClusterMTCCFilter.cc
@@ -11,6 +11,7 @@
 #include "DataFormats/SiStripDetId/interface/TOBDetId.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
+using namespace std;
 
 namespace cms
 {
@@ -20,11 +21,11 @@ ClusterMTCCFilter::ClusterMTCCFilter(const edm::ParameterSet& ps){
    ChargeThresholdTOB=ps.getParameter<int>("ChargeThresholdTOB");
    ChargeThresholdTEC=ps.getParameter<int>("ChargeThresholdTEC");
    MinClustersDiffComponents=ps.getParameter<int>("MinClustersDiffComponents");
-   clusterProducer = ps.getParameter<std::string>("ClusterProducer");
+   clusterProducer = ps.getParameter<string>("ClusterProducer");
    //
    produces <int>();
    produces <unsigned int >();
-   produces < std::map<uint,std::pair<SiStripCluster,uint32_t> > >();
+   produces < map<uint,vector< pair<SiStripCluster, uint32_t> > > >();
 }
 
 bool ClusterMTCCFilter::filter(edm::Event & e, edm::EventSetup const& c) {
@@ -32,16 +33,16 @@ bool ClusterMTCCFilter::filter(edm::Event & e, edm::EventSetup const& c) {
   edm::Handle< edm::DetSetVector<SiStripCluster> > h;
   e.getByLabel(clusterProducer,h);
 
+
   //
   unsigned int sum_of_cluster_charges=0;
   clusters_in_subcomponents.clear();
   // first find all clusters that are over the threshold
-  // uint generalized_layer:  31 = TIB1, 32 = TIB2, 33 = TIB3, 50 = TOB, 60 = TEC
   for (edm::DetSetVector<SiStripCluster>::const_iterator it=h->begin();it!=h->end();it++) {
-    for(std::vector<SiStripCluster>::const_iterator vit=(it->data).begin(); vit!=(it->data).end(); vit++){
+    for(vector<SiStripCluster>::const_iterator vit=(it->data).begin(); vit!=(it->data).end(); vit++){
       // calculate sum of amplitudes
       unsigned int amplclus=0;
-      for(std::vector<short>::const_iterator ia=vit->amplitudes().begin(); ia!=vit->amplitudes().end(); ia++) {
+      for(vector<short>::const_iterator ia=vit->amplitudes().begin(); ia!=vit->amplitudes().end(); ia++) {
         if ((*ia)>0) amplclus+=(*ia); // why should this be negative?
       }
       sum_of_cluster_charges += amplclus;
@@ -52,12 +53,13 @@ bool ClusterMTCCFilter::filter(edm::Event & e, edm::EventSetup const& c) {
         || ( thedetId.subdetId()==StripSubdetector::TOB && amplclus>ChargeThresholdTOB )
         || ( thedetId.subdetId()==StripSubdetector::TEC && amplclus>ChargeThresholdTEC )
         ){
+        // calculate generalized_layer:  31 = TIB1, 32 = TIB2, 33 = TIB3, 50 = TOB, 60 = TEC
         if(thedetId.subdetId()==StripSubdetector::TIB){
            TIBDetId ptib = TIBDetId(thedetId.rawId());
            generalized_layer = 10*thedetId.subdetId() + ptib.layer() + ptib.stereo();
 	   if (ptib.layer()==2){
 	     generalized_layer++;
-	     if (ptib.glued()) std::cout<<"WRONGGGG"<<std::endl;
+	     if (ptib.glued()) cout<<"WRONGGGG"<<endl;
 	   }
 
         }else{
@@ -67,24 +69,31 @@ bool ClusterMTCCFilter::filter(edm::Event & e, edm::EventSetup const& c) {
 	    generalized_layer += ptob.layer();
 	  }
         }
-        clusters_in_subcomponents.insert( std::make_pair(generalized_layer,  std::make_pair(*vit,it->detId())) );
+        // fill clusters_in_subcomponents
+        map<uint,vector<pair<SiStripCluster,uint32_t> > >::iterator layer_it = clusters_in_subcomponents.find(generalized_layer);
+        if(layer_it==clusters_in_subcomponents.end()){ // if layer not found yet, create DATA vector and generate map KEY + DATA
+          vector<pair<SiStripCluster, uint32_t> > local_vector;
+          local_vector.push_back( std::make_pair(*vit,it->detId()) );
+          clusters_in_subcomponents.insert( std::make_pair( generalized_layer, local_vector) );
+        }else{ // push into already existing vector
+           (layer_it->second).push_back( std::make_pair(*vit,it->detId()) );
+        }
       }
     }
   }
 
   bool decision=false; // default value, only accept if set true in this loop
   uint nr_of_subcomps_with_clusters=0;
-  if( clusters_in_subcomponents.count(31)>0 ) nr_of_subcomps_with_clusters++; // TIB1
-  if( clusters_in_subcomponents.count(32)>0 ) nr_of_subcomps_with_clusters++; // TIB2
-  if( clusters_in_subcomponents.count(33)>0 ) nr_of_subcomps_with_clusters++; // TIB3
-  if( clusters_in_subcomponents.count(51)>0 ) nr_of_subcomps_with_clusters++; // TOB1
-  if( clusters_in_subcomponents.count(52)>0 ) nr_of_subcomps_with_clusters++; // TOB2
-  if( clusters_in_subcomponents.count(60)>0 ) nr_of_subcomps_with_clusters++; // TEC
+  if( clusters_in_subcomponents[31].size()>0 ) nr_of_subcomps_with_clusters++; // TIB1
+  if( clusters_in_subcomponents[32].size()>0 ) nr_of_subcomps_with_clusters++; // TIB2
+  if( clusters_in_subcomponents[33].size()>0 ) nr_of_subcomps_with_clusters++; // TIB3
+  if( clusters_in_subcomponents[51].size()>0 ) nr_of_subcomps_with_clusters++; // TOB1
+  if( clusters_in_subcomponents[52].size()>0 ) nr_of_subcomps_with_clusters++; // TOB2
+  if( clusters_in_subcomponents[60].size()>0 ) nr_of_subcomps_with_clusters++; // TEC
   if(
      nr_of_subcomps_with_clusters >= MinClustersDiffComponents // more than 'MinClustersDiffComponents' components have at least 1 cluster
      ) {
       decision = true; // accept event
-//      std::cout<<"Nr of layers with clusters: "<<nr_of_subcomps_with_clusters<<" "<< clusters_in_subcomponents.count(31)<<" "<< clusters_in_subcomponents.count(32)<<" "<< clusters_in_subcomponents.count(33)<<" "<< clusters_in_subcomponents.count(51)<<" "<< clusters_in_subcomponents.count(51)<<" "<< clusters_in_subcomponents.count(60)<<std::endl;
   }
 
   std::auto_ptr< int > output_decision( new int(decision) );
@@ -93,11 +102,9 @@ bool ClusterMTCCFilter::filter(edm::Event & e, edm::EventSetup const& c) {
   std::auto_ptr< unsigned int > output_sumofcharges( new unsigned int(sum_of_cluster_charges) );
   e.put(output_sumofcharges);
 
-  std::auto_ptr< std::map< uint, std::pair<SiStripCluster,uint32_t> >  > output_clusters(new std::map< uint, std::pair<SiStripCluster,uint32_t> > (clusters_in_subcomponents));
+  std::auto_ptr< map< uint, vector< pair<SiStripCluster,uint32_t> > > > output_clusters(new map< uint, vector< pair<SiStripCluster,uint32_t> > > (clusters_in_subcomponents));
   e.put(output_clusters);
 
   return decision;
-
 }
 }
-
