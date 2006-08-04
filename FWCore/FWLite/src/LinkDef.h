91aa21a2498d93209f7c5cc97b9454fdde919c10
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-08-04 18:20:22 +0000

    added branchToClass

--- FWCore/FWLite/src/LinkDef.h
+++ FWCore/FWLite/src/LinkDef.h
@@ -1,4 +1,6 @@
 #include "FWCore/FWLite/src/AutoLibraryLoader.h"
+#include "FWCore/FWLite/src/branchToClass.h"
 #ifdef __CINT__
 #pragma link C++ class AutoLibraryLoader;
+#pragma link C++ function branchToClass;
 #endif
