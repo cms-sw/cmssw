8bdc6a48674aa04908d5d2dbd42d915b83593cbf
Danek Kotlinski <sha1-3a033e6407e48eb9fbe364eec828608ee1ba03ae@cern.ch>
2006-08-07 11:07:18 +0000

    update the PixelTrackProducer to store rechits

--- RecoPixelVertexing/PixelTrackFitting/src/PixelTrackProducer.cc
+++ RecoPixelVertexing/PixelTrackFitting/src/PixelTrackProducer.cc
@@ -20,12 +20,16 @@
 #include "RecoPixelVertexing/PixelTriplets/interface/OrderedHitTriplets.h"
 #include "RecoTracker/TkTrackingRegions/interface/GlobalTrackingRegion.h"
 #include "RecoPixelVertexing/PixelTriplets/interface/PixelHitTripletGenerator.h"
+#include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "FWCore/Framework/interface/OrphanHandle.h"
 
 PixelTrackProducer::PixelTrackProducer(const edm::ParameterSet& conf)
   : theConfig(conf)
 {
   edm::LogInfo("PixelTrackProducer")<<" constuction...";
   produces<reco::TrackCollection>();
+  produces<TrackingRecHitCollection>();
+  produces<reco::TrackExtraCollection>();
 }
 
 PixelTrackProducer::~PixelTrackProducer()
@@ -33,7 +37,7 @@ PixelTrackProducer::~PixelTrackProducer()
 
 void PixelTrackProducer::produce(edm::Event& ev, const edm::EventSetup& es)
 {
-  LogDebug("PixelTrackProducer, produce")<<"event# :"<<ev.id(); 
+  LogDebug("PixelTrackProducer, produce")<<"event# :"<<ev.id();
 
   edm::Handle<SiPixelRecHitCollection> pixelHits;
   ev.getByType(pixelHits);
@@ -50,20 +54,66 @@ void PixelTrackProducer::produce(edm::Event& ev, const edm::EventSetup& es)
   std::string fitterName = theConfig.getParameter<std::string>("Fitter");
   edm::ESHandle<PixelFitter> fitter;
   es.get<TrackingComponentsRecord>().get(fitterName,fitter);
- 
+
 
   std::auto_ptr<reco::TrackCollection> result(new reco::TrackCollection);
-  
+  std::auto_ptr<TrackingRecHitCollection> outputRHColl(new TrackingRecHitCollection);
+  std::auto_ptr<reco::TrackCollection> outputTColl(new reco::TrackCollection);
+  std::auto_ptr<reco::TrackExtraCollection> outputTEColl(new reco::TrackExtraCollection);
+
   typedef OrderedHitTriplets::const_iterator IT;
+  int nTracks = 0;
   for (IT it = triplets.begin(); it != triplets.end(); it++) {
     std::vector<const TrackingRecHit *> hits;
     hits.push_back( (*it).inner() );
     hits.push_back( (*it).middle() );
     hits.push_back( (*it).outer() );
-    const reco::Track *track = fitter->run(es,hits,region); 
-    if (track) result->push_back(*track);
-//    delete track;
-  } 
-  
+    const reco::Track *track = fitter->run(es,hits,region);
+    if (track) {
+      result->push_back(*track);
+
+      outputRHColl->push_back( ( (*it).inner() )->clone() );
+      outputRHColl->push_back( ( (*it).middle() )->clone() );
+      outputRHColl->push_back( ( (*it).outer() )->clone() );
+      nTracks++;
+    }
+  }
+  cout << "nTracks" << nTracks << endl;
+
+  LogDebug("TrackProducer") << "put the collection of TrackingRecHit in the event" << "\n";
+  edm::OrphanHandle <TrackingRecHitCollection> ohRH = ev.put( outputRHColl );
+
+  int cc = 0;
+  for (int k = 0; k < nTracks; k++)
+  {
+    reco::TrackExtra* theTrackExtra = new reco::TrackExtra();
+
+    //fill the TrackExtra with TrackingRecHitRef
+    for(int i = 0; i < 3; i++)
+    {
+      theTrackExtra->add(TrackingRecHitRef(ohRH,cc));
+      cc++;
+    }
+
+    outputTEColl->push_back(*theTrackExtra);
+    delete theTrackExtra;
+  }
+
+  //put the collection of TrackExtra in the event
+  LogDebug("TrackProducer") << "put the collection of TrackExtra in the event" << "\n";
+  edm::OrphanHandle<reco::TrackExtraCollection> ohTE = ev.put(outputTEColl);
+
+  for (int k = 0; k < nTracks; k++)
+  {
+    //create a TrackExtraRef
+    const reco::TrackExtraRef theTrackExtraRef(ohTE,k);
+
+    //use the TrackExtraRef to assign the TrackExtra to the Track
+    (result->at(k)).setExtra(theTrackExtraRef);
+
+  }
+//
+
+
   ev.put(result);
 }
