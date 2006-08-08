f575cd92bb46a1c3e916222d211055ace2dcff06
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-08-08 16:13:48 +0000

    Switch back to iphi=1 at phi=0 Correct crystal id methods to keep with ECAL convention

--- DataFormats/HcalDetId/src/HcalDetId.cc
+++ DataFormats/HcalDetId/src/HcalDetId.cc
@@ -2,22 +2,15 @@
 
 const HcalDetId HcalDetId::Undefined(HcalEmpty,0,0,0);
 
-static const int EncodingVersion = 1;
-
 HcalDetId::HcalDetId() : DetId() {
 }
 
 HcalDetId::HcalDetId(uint32_t rawid) : DetId(rawid) {
-  if (!null() && encodingVersion()==0) {
-    id_=(id_&0xFFFFFF80u)|(iphi())|((EncodingVersion&0x3)<<17);
-  }
 }
 
 HcalDetId::HcalDetId(HcalSubdetector subdet, int tower_ieta, int tower_iphi, int depth) : DetId(Hcal,subdet) {
   // (no checking at this point!)
-  id_ |=
-    ((EncodingVersion&0x3)<<17) |
-    ((depth&0x7)<<14) |
+  id_ |= ((depth&0x7)<<14) |
     ((tower_ieta>0)?(0x2000|(tower_ieta<<7)):((-tower_ieta)<<7)) |
     (tower_iphi&0x7F);
 }
@@ -33,9 +26,6 @@ HcalDetId::HcalDetId(const DetId& gen) {
       }  
   }
   id_=gen.rawId();
-  if (!null() && encodingVersion()==0) {
-    id_=(id_&0xFFFFFF80u)|(iphi())|((EncodingVersion&0x3)<<17);
-  }
 }
 
 HcalDetId& HcalDetId::operator=(const DetId& gen) {
@@ -49,33 +39,19 @@ HcalDetId& HcalDetId::operator=(const DetId& gen) {
       }  
   }
   id_=gen.rawId();
-  if (!null() && encodingVersion()==0) {
-    id_=(id_&0xFFFFFF80u)|(iphi())|((EncodingVersion&0x3)<<17);
-  }
   return (*this);
 }
 
-
-HcalDetId::HcalDetId(const HcalDetId& gen) {
-  id_=gen.rawId();
-  if (!null() && encodingVersion()==0) {
-    id_=(id_&0xFFFFFF80u)|(iphi())|((EncodingVersion&0x3)<<17);
-  }
+int HcalDetId::crystal_iphi_low() const { 
+  int simple_iphi=((iphi()-1)*5)+1; 
+  simple_iphi+=10;
+  return ((simple_iphi>360)?(simple_iphi-360):(simple_iphi));
 }
 
-HcalDetId& HcalDetId::operator=(const HcalDetId& gen) {
-  id_=gen.rawId();
-  if (!null() && encodingVersion()==0) {
-    id_=(id_&0xFFFFFF80u)|(iphi())|((EncodingVersion&0x3)<<17);
-  }
-  return (*this);
-}
-
-int HcalDetId::iphi() const {
-  int retval=(id_&0x7F);
-  if (!null() && encodingVersion()==0 && ietaAbs()<40) 
-    retval=((retval+1)%72)+1;
-  return retval;
+int HcalDetId::crystal_iphi_high() const { 
+  int simple_iphi=((iphi()-1)*5)+5; 
+  simple_iphi+=10;
+  return ((simple_iphi>360)?(simple_iphi-360):(simple_iphi));
 }
 
 std::ostream& operator<<(std::ostream& s,const HcalDetId& id) {
