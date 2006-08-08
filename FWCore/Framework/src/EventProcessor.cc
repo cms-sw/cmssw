576e130cdc8e002350d5f28c53396003296e4017
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-08-08 00:40:51 +0000

    use 'copySlotsTo' rather than connectTo for the ActivityRegistry in order to improve performance

--- FWCore/Framework/src/EventProcessor.cc
+++ FWCore/Framework/src/EventProcessor.cc
@@ -598,8 +598,14 @@ namespace edm {
 
     //create the services
     ServiceToken tempToken(ServiceRegistry::createSet(*pServiceSets,iToken,iLegacy));
-    tempToken.connectTo(*actReg_);
-     
+
+    //using copySlotsTo rather than just connectTo allows a performance improvement
+    // since slots are called directly rather than indirectly through another ActivityRegistry
+    // The downside is if items were added to 'tempToken' then 'actReg_' would not see the changes
+    // However, in this case since tempToken is a temporary, the use of copySlotsTo is safe
+    //tempToken.connectTo(*actReg_);
+    tempToken.copySlotsTo(*actReg_); 
+    
     //add the ProductRegistry as a service ONLY for the construction phase
     typedef serviceregistry::ServiceWrapper<ConstProductRegistry> w_CPR;
     shared_ptr<w_CPR>
