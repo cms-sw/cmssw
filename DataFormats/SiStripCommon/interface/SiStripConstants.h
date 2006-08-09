7d0fc213b016bc83958537981524c714e7f725f1
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-08-09 16:17:20 +0000

    reorganised consts and enums within files

--- DataFormats/SiStripCommon/interface/SiStripConstants.h
+++ DataFormats/SiStripCommon/interface/SiStripConstants.h
@@ -5,22 +5,12 @@
 #include <string>
 
 // -----------------------------------------------------------------------------
-// Constants related to error conditions
-namespace sistrip { 
-
-  static const std::string sistrip_ = "SiStrip|";
-  
-  // Error categories
-  static const std::string DQM_FWK    = sistrip_ + "DQM_FWK";
-  static const std::string DQM_HISTOS = sistrip_ + "DQM_HISTOS";
-  
-}
-
-// -----------------------------------------------------------------------------
 // Useful constants associated with the FED
 namespace sistrip { 
 
-  // FED = Front-End Driver, FEUNIT = Front-End Unit, FEDCH = Fed Channel
+  // FED = Front-End Driver
+  // FEUNIT = Front-End Unit
+  // FEDCH = Fed Channel
   static const uint16_t FEDCH_PER_FEUNIT = 12;
   static const uint16_t FEUNITS_PER_FED  = 8;
   static const uint16_t FEDCH_PER_FED    = FEDCH_PER_FEUNIT * FEUNITS_PER_FED; // 96
@@ -36,7 +26,7 @@ namespace sistrip {
   static const uint16_t STRIPS_PER_FEUNIT = STRIPS_PER_FEDCH * FEDCH_PER_FEUNIT; // 3072
   static const uint16_t STRIPS_PER_FED    = STRIPS_PER_FEUNIT * FEUNITS_PER_FED; // 24576
 
-  // 
+  // Constants related to FED buffer formatting
   static const uint16_t DAQ_HDR_SIZE        = 8;
   static const uint16_t TRK_HDR_SIZE        = 8;
   static const uint16_t FE_HDR_SIZE         = 16;
@@ -46,68 +36,17 @@ namespace sistrip {
 }
 
 // -----------------------------------------------------------------------------
-// Constants associated with the naming of DQM histograms
+// Constants related to error conditions
 namespace sistrip { 
+
+  static const std::string sistrip_ = "SiStrip|";
   
-  // generic constants
-  static const uint16_t all_     = 0xFFFF;
-  static const std::string root_ = "SiStrip";
-  static const std::string dir_  = "/";
-  static const std::string sep_  = "_";
-  static const std::string commissioningTask_ = "SiStripCommissioningTask";
-  
-  // views
-  static const std::string controlView_  = "ControlView";
-  static const std::string readoutView_  = "ReadoutView";
-  static const std::string detectorView_ = "DetectorView";
-  static const std::string unknownView_  = "UnknownView";
-
-  // control and readout parameters
-  static const std::string fecCrate_    = "FecCrate";
-  static const std::string fecSlot_     = "FecSlot";
-  static const std::string fecRing_     = "FecRing";
-  static const std::string ccuAddr_     = "CcuAddr";
-  static const std::string ccuChan_     = "CcuChan";
-  static const std::string fedId_       = "FedId";
-  static const std::string fedChannel_  = "FedChannel";
-
-  // commissioning task
-  static const std::string fedCabling_  = "FedCabling";
-  static const std::string apvTiming_   = "ApvTiming";
-  static const std::string fedTiming_   = "FedTiming";
-  static const std::string optoScan_    = "OptoScan";
-  static const std::string vpspScan_    = "VpspScan";
-  static const std::string pedestals_   = "Pedestals";
-  static const std::string apvLatency_  = "ApvLatency";
-  static const std::string unknownTask_ = "UnknownTask";
-
-  // histo contents
-  static const std::string sum2_            = "SumOfSquares";
-  static const std::string sum_             = "SumOfContents";
-  static const std::string num_             = "NumOfEntries";
-  static const std::string unknownContents_ = "UnknownContents";
-
-  // key 
-  static const std::string fedKey_     = "FedKey";
-  static const std::string fecKey_     = "FecKey";
-  static const std::string detKey_     = "DetId"; //@@ necessary?
-  static const std::string unknownKey_ = "UnknownKey";
-
-  // granularity
-  static const std::string lldChan_            = "LldChan";
-  static const std::string apvPair_            = "ApvPair";
-  static const std::string apv_                = "Apv";
-  static const std::string unknownGranularity_ = "UnknownGranularity";
-
-  // extra histogram information 
-  static const std::string gain_              = "Gain";
-  static const std::string digital_           = "Digital";
-  static const std::string pedsAndRawNoise_   = "PedsAndRawNoise";
-  static const std::string residualsAndNoise_ = "ResidualsAndNoise";
-  static const std::string commonMode_        = "CommonMode";
+  // Error categories
+  static const std::string DQM_FWK    = sistrip_ + "DQM_FWK";
+  static const std::string DQM_HISTOS = sistrip_ + "DQM_HISTOS";
   
 }
-
+  
 #endif // DataFormats_SiStripCommon_SiStripConstants_H
 
 
--- DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h
+++ DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h
@@ -1,45 +1,8 @@
 #ifndef DataFormats_SiStripCommon_SiStripEnumeratedTypes_H
 #define DataFormats_SiStripCommon_SiStripEnumeratedTypes_H
 
-#include "boost/cstdint.hpp"
-#include <vector>
-
 namespace sistrip { 
 
-  /** */
-  enum FedReadoutMode { UNKNOWN_FED_READOUT_MODE,
-			UNDEFINED_FED_READOUT_MODE,
-			SCOPE_MODE, 
-			VIRGIN_RAW, 
-			PROC_RAW, 
-			ZERO_SUPPR,
-			ZERO_SUPPR_LITE };
-
-  /** */
-  enum FedReadoutPath { UNKNOWN_FED_READOUT_PATH,
-			UNDEFINED_FED_READOUT_PATH,
-			VME_READOUT, 
-			SLINK_READOUT };
-  
-  /** */
-  enum FedBufferFormat { UNKNOWN_FED_BUFFER_FORMAT,
-			 UNDEFINED_FED_BUFFER_FORMAT,
-			 FULL_DEBUG_FORMAT, 
-			 APV_ERROR_FORMAT };
-  
-  /** */
-  enum FedSuperMode { UNKNOWN_FED_SUPER_MODE,
-		      UNDEFINED_FED_SUPER_MODE,
-		      REAL, 
-		      FAKE };
-  
-  /** Detector views. */
-  enum View { UNKNOWN_VIEW, 
-	      NO_VIEW, 
-	      READOUT, 
-	      CONTROL, 
-	      DETECTOR };
-  
   /** 
    * Commissioning Tasks (equivalent "TrackerSupervisor" enums in brackets): 
    * unknown commissioning task,
@@ -69,23 +32,40 @@ namespace sistrip {
 	      VPSP_SCAN,
 	      PEDESTALS,
 	      APV_LATENCY,
-	      PHYSICS };
-  
-  /** 
-   * Histogram contents:
-   */
-  enum Contents { UNKNOWN_CONTENTS, COMBINED, SUM2, SUM, NUM };
+	      PHYSICS 
+  };
 
-  /**
-   * Key type used as an identifier:
-   */
-  enum KeyType { UNKNOWN_KEY, NO_KEY, FED, FEC, DET };
-  
-  /**
-   * Histogram granularity
-   */
-  enum Granularity { UNKNOWN_GRAN, MODULE, LLD_CHAN, APV_PAIR, APV };
+  /** */
+  enum FedReadoutMode { UNKNOWN_FED_READOUT_MODE,
+			UNDEFINED_FED_READOUT_MODE,
+			SCOPE_MODE, 
+			VIRGIN_RAW, 
+			PROC_RAW, 
+			ZERO_SUPPR,
+			ZERO_SUPPR_LITE 
+  };
 
+  /** */
+  enum FedReadoutPath { UNKNOWN_FED_READOUT_PATH,
+			UNDEFINED_FED_READOUT_PATH,
+			VME_READOUT, 
+			SLINK_READOUT 
+  };
+  
+  /** */
+  enum FedBufferFormat { UNKNOWN_FED_BUFFER_FORMAT,
+			 UNDEFINED_FED_BUFFER_FORMAT,
+			 FULL_DEBUG_FORMAT, 
+			 APV_ERROR_FORMAT 
+  };
+  
+  /** */
+  enum FedSuperMode { UNKNOWN_FED_SUPER_MODE,
+		      UNDEFINED_FED_SUPER_MODE,
+		      REAL, 
+		      FAKE 
+  };
+  
 }
 
 #endif // DataFormats_SiStripCommon_SiStripEnumeratedTypes_H
