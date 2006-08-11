a9efcdb0a6169de6e2705d480a5f0543716129a0
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-08-11 10:55:32 +0000

    icc file moved in DataFormats/MuonData

--- SimMuon/DTDigitizer/test/stubs/DTTime2DriftParametrization.cc
+++ SimMuon/DTDigitizer/test/stubs/DTTime2DriftParametrization.cc
@@ -20,10 +20,10 @@
 
 #ifndef MB_DT_STANDALONE
 #include "DTTime2DriftParametrization.h"
-#include "SimMuon/DTDigitizer/src/DTDriftTimeParameters.icc"
+#include "DataFormats/MuonData/src/DTDriftTimeParameters.icc"
 #else
 #include "DTTime2DriftParametrization.h"
-#include "SimMuon/DTDigitizer/src/DTDriftTimeParameters.icc"
+#include "DataFormats/MuonData/src/DTDriftTimeParameters.icc"
 #endif
 
 DTTime2DriftParametrization::DTTime2DriftParametrization(){}
