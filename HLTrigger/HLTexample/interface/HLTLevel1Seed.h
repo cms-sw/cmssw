b5f4e41ec1ed9543867f3bbcc48b15e72c34bc16
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2006-08-11 14:58:33 +0000

    More detailed Level-1 treatment.

--- HLTrigger/HLTexample/interface/HLTLevel1Seed.h
+++ HLTrigger/HLTexample/interface/HLTLevel1Seed.h
@@ -7,7 +7,7 @@
  *  This class is an HLTFilter (-> EDFilter) implementing filtering on
  *  Level-1 bits and extraction of seed objects.
  *
- *  $Date: 2006/06/26 23:39:24 $
+ *  $Date: 2006/08/10 17:09:24 $
  *  $Revision: 1.1 $
  *
  *  \author Martin Grunewald
@@ -24,16 +24,27 @@
 
 class HLTLevel1Seed : public HLTFilter {
 
-   public:
-      explicit HLTLevel1Seed(const edm::ParameterSet&);
-      ~HLTLevel1Seed();
-      virtual bool filter(edm::Event&, const edm::EventSetup&);
+  public:
 
-   private:
-      bool                      andOr_;  // false=and-mode (all), true=or-mode(at least one)
-      bool                      byName_; // list of l1 triggers provided by Name or not
-      std::vector<unsigned int> L1SeedsByType_; // list of required L1 triggers by type/number
-      std::vector<std::string > L1SeedsByName_; // list of required L1 triggers by name
+    explicit HLTLevel1Seed(const edm::ParameterSet&);
+    ~HLTLevel1Seed();
+    virtual bool filter(edm::Event&, const edm::EventSetup&);
+
+  private:
+
+    edm::InputTag L1ExtraTag_;
+    // L1 Extra EDProducts
+
+    bool andOr_;
+    // false=and-mode (all), true=or-mode(at least one)
+
+    bool byName_;
+    // list of l1 triggers provided by Name or not
+
+    std::vector<unsigned int> L1SeedsByType_;
+    std::vector<std::string > L1SeedsByName_;
+    // list of required L1 triggers by type/number and by name
+
 };
 
 #endif //HLTLevel1Seed_h
--- HLTrigger/HLTexample/src/HLTLevel1Seed.cc
+++ HLTrigger/HLTexample/src/HLTLevel1Seed.cc
@@ -2,8 +2,8 @@
  *
  * See header file for documentation
  *
- *  $Date: 2006/08/10 17:31:14 $
- *  $Revision: 1.2 $
+ *  $Date: 2006/08/10 21:19:59 $
+ *  $Revision: 1.3 $
  *
  *  \author Martin Grunewald
  *
@@ -24,10 +24,11 @@
 // constructors and destructor
 //
 HLTLevel1Seed::HLTLevel1Seed(const edm::ParameterSet& iConfig) :
-  andOr_  (iConfig.getParameter<bool> ("andOr" )),
-  byName_ (iConfig.getParameter<bool> ("byName"))
+  L1ExtraTag_(iConfig.getParameter<edm::InputTag> ("L1ExtraTag")),
+  andOr_     (iConfig.getParameter<bool> ("andOr" )),
+  byName_    (iConfig.getParameter<bool> ("byName"))
 {
-   unsigned int n;
+   unsigned int n(0);
 
    if (byName_) {
      // have names, need to get slot numbers
@@ -44,14 +45,30 @@ HLTLevel1Seed::HLTLevel1Seed(const edm::ParameterSet& iConfig) :
      L1SeedsByType_= iConfig.getParameter<std::vector<unsigned int> >("L1SeedsByType");
      n=L1SeedsByType_.size();
      L1SeedsByName_.resize(n);
-     l1extra::L1ParticleMap::L1TriggerType type;
      for (unsigned int i=0; i!=n; i++) {
+       l1extra::L1ParticleMap::L1TriggerType type;
        type=(l1extra::L1ParticleMap::L1TriggerType) (L1SeedsByType_[i]);
        L1SeedsByName_[i]=l1extra::L1ParticleMap::triggerName(type);
      }
    }
 
-   LogDebug("") << "Level-1 triggers requested: " << n << " - andOr mode: " << andOr_ << " - byName: " << byName_;
+   // for empty input vectors (n=0), default to all triggers!
+   if (n==0) {
+     n=(unsigned int) (l1extra::L1ParticleMap::kNumOfL1TriggerTypes);
+     L1SeedsByName_.resize(n);
+     L1SeedsByType_.resize(n);
+     for (unsigned int i=0; i!=n; i++) {
+       L1SeedsByType_[i]=i;
+       l1extra::L1ParticleMap::L1TriggerType type;
+       type=(l1extra::L1ParticleMap::L1TriggerType) (L1SeedsByType_[i]);
+       L1SeedsByName_[i]=l1extra::L1ParticleMap::triggerName(type);
+     }
+   }
+
+   LogDebug("") << "Level-1 triggers: " +L1ExtraTag_.encode()
+		<< " - Number requested: " << n 
+		<< " - andOr mode: " << andOr_
+		<< " - byName: " << byName_;
    if (n>0) {
      LogDebug("") << "  Level-1 triggers requestd: type and name:";
      for (unsigned int i=0; i!=n; i++) {
@@ -90,22 +107,47 @@ HLTLevel1Seed::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    // Ref to Candidate object to be recorded in filter object
    RefToBase<Candidate> ref;
 
-   // get hold of (single?) L1ParticleMapCollection
+
+   // get hold of L1GlobalReadoutRecord
+   Handle<L1GlobalTriggerReadoutRecord> l1gtrrh;
+   try {iEvent.getByLabel(L1ExtraTag_,l1gtrrh);}
+   catch (...) {
+     LogDebug("") << "L1GlobalTriggerReadoutRecord with label ["+L1ExtraTag_.encode()+"] not found!";
+     //     iEvent.put(filterobject);
+     //     return false;
+   }
+   if (l1gtrrh.isValid()) {
+     LogDebug("") << "L1GlobalTriggerReadoutRecord decision: " << l1gtrrh->decision();
+   } else {
+     LogDebug("") << "L1GlobalTriggerReadoutRecord with label ["+L1ExtraTag_.encode()+"] not valid!";
+     //     iEvent.put(filterobject);
+     //     return false;
+   }
+
+   // get hold of L1ParticleMapCollection
    Handle<L1ParticleMapCollection> l1pmch;
-   iEvent.getByType(l1pmch);
-   if (!l1pmch.isValid()) {
+   try {iEvent.getByLabel(L1ExtraTag_,l1pmch);}
+   catch (...) {
+     LogDebug("") << "L1ParticleMapCollection with label ["+L1ExtraTag_.encode()+"] not found!";
      iEvent.put(filterobject);
-     LogDebug("") << "No L1ParticleMapCollection found!";
+     return false;
+   }
+   if (l1pmch.isValid()) {
+     LogDebug("") << "L1ParticleMapCollection contains " << l1pmch->size() << " maps.";
+   } else {
+     LogDebug("") << "L1ParticleMapCollection with label ["+L1ExtraTag_.encode()+"] not valid!";
+     iEvent.put(filterobject);
      return false;
    }
    const unsigned int m(l1pmch->size());
 
-   // get index into L1ParticleMapCollection for requested L1 triggers
+
+   // get indices into L1ParticleMapCollection for requested 
+   // L1 triggers which have fired this event
    const unsigned int n(L1SeedsByName_.size());
-   vector<int> index(n);
+   vector<int> index(n,-1);
    L1ParticleMap::L1TriggerType l1tt;
    for (unsigned int i=0; i!=n; i++) {
-     index[i]=-1;
      l1tt= (L1ParticleMap::L1TriggerType) (L1SeedsByType_[i]);
      for (unsigned int j=0; j!=m; j++) {
        const L1ParticleMap& l1pm((*l1pmch)[j]);
@@ -116,125 +158,156 @@ HLTLevel1Seed::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
      }
    }
 
-   // filter result: have all (and-mode) / at least one of (or-mode)
-   // the requested L1 triggers fired?
+
+   // HLT Level1Seed Filter Result:
+   // have all (and-mode) / at least one (or-mode)
+   // of the requested L1 triggers fired this event?
    unsigned int fired(0);
    for (unsigned int i=0; i!=n; i++) if (index[i]>=0) fired++;
    const bool accept( ((!andOr_) && (fired==n)) ||
 		      (( andOr_) && (fired!=0)) );
+   LogDebug("") << "Accept = " << accept;
 
    // number of of particles of each type eventually recorded in filter object
-   unsigned int ne(0);
-   unsigned int nm(0);
-   unsigned int nt(0);
-   unsigned int nj(0);
-   unsigned int nM(0);
+   unsigned int ne(0); // em
+   unsigned int nm(0); // muon
+   unsigned int nt(0); // tau
+   unsigned int nj(0); // jets
+   unsigned int nM(0); // mets
 
    // in case of accept, record all particles used
-   // by any requested L1 triggers which fired
+   // by any requested L1 triggers which has fired
    if (accept) {
 
      // we do not want to store duplicates, hence we determine
-     // which L1 particle is used by one of the requested triggers
+     // which L1 particle is used by the requested triggers
      // and then store the used L1 particles only once
 
      // get hold of overall collections of particles of each type
      // will check later which are used by which requested L1 trigger
      Handle<L1EmParticleCollection  > l1eh;
-     iEvent.getByType(l1eh);
+     try {iEvent.getByLabel(L1ExtraTag_,l1eh);} catch(...) {;}
      unsigned int Ne(0);
      if (l1eh.isValid()) Ne=l1eh->size();
+     //     LogDebug("") << "L1EmParticleCollection size = " << Ne;
      vector<unsigned int> ve(Ne,0);
      // keeps track how often each Em particle is used
 
      Handle<L1MuonParticleCollection> l1mh;
-     iEvent.getByType(l1mh);
+     try {iEvent.getByLabel(L1ExtraTag_,l1mh);} catch (...) {;}
      unsigned int Nm(0);
      if (l1mh.isValid()) Nm=l1mh->size();
+     //     LogDebug("") << "L1MuonParticleCollection size = " << Nm;
      vector<unsigned int> vm(Nm,0);
      // keeps track how often each Muon particle is used
 
      Handle<L1JetParticleCollection>  l1th; // taus are stored as jets
-     iEvent.getByType(l1th);
+     InputTag L1ExtraTauTag(InputTag(L1ExtraTag_.label(),"Tau"));
+     try {iEvent.getByLabel(L1ExtraTauTag,l1th);} catch (...) {;}
      unsigned int Nt(0);
      if (l1th.isValid()) Nt=l1th->size();
+     //     LogDebug("") << "L1TauParticleCollection size = " << Nt;
      vector<unsigned int> vt(Nt,0);
      // keeps track how often each Tau particle is used
 
      Handle<L1JetParticleCollection>  l1jh;
-     iEvent.getByType(l1jh);
+     InputTag L1ExtraJetTag(InputTag(L1ExtraTag_.label(),"ForCen"));
+     try {iEvent.getByLabel(L1ExtraJetTag,l1jh);} catch (...) {;}
      unsigned int Nj(0);
-     if (l1jh.isValid()) Nj=l1th->size();
+     if (l1jh.isValid()) Nj=l1jh->size();
+     //     LogDebug("") << "L1JetParticleCollection size = " << Nj;
      vector<unsigned int> vj(Nj,0);
      // keeps track how often each Jet particle is used
 
      Handle<L1EtMissParticle> l1Mh;
-     iEvent.getByType(l1Mh);
+     try {iEvent.getByLabel(L1ExtraTag_,l1Mh);} catch (...) {;}
      unsigned int NM(0);
      if (l1Mh.isValid()) NM=1;
+     //     LogDebug("") << "L1EtMissParticle size = " << NM;
      vector<unsigned int> vM(NM,0);
      // keeps track how often the global EtMiss "particle" is used
 
-     // loop over requested triggers and count which particles are used
+     // loop over requested triggers
      for (unsigned int i=0; i!=n; i++) {
-       const L1ParticleMap& l1pm((*l1pmch)[index[i]]);
+       //       LogDebug("") << "Accessing L1 trigger: " 
+       //		    << i << "=" << L1SeedsByName_[i] << " " << index[i];
+       // has requested trigger fired?
        if (index[i]>=0) { // requested and fired!
+	 // if yes, count which particles of each type have been used
+	 const L1ParticleMap& l1pm((*l1pmch)[index[i]]);
          unsigned int m(0);
+
 	 // em particles (gamma+electron)
 	 m=l1pm.emParticles().size();
-         for (unsigned int j=0; j!=m; i++) ve[l1pm.emParticles()[j].key()]++;
+	 //	 LogDebug("") << "e " << m;
+         for (unsigned int j=0; j!=m; j++) ve[l1pm.emParticles()[j].key()]++;
+
 	 // muon particles
 	 m=l1pm.muonParticles().size();
-         for (unsigned int j=0; j!=m; i++) vm[l1pm.muonParticles()[j].key()]++;
+	 //	 LogDebug("") << "m " << m;
+         for (unsigned int j=0; j!=m; j++) vm[l1pm.muonParticles()[j].key()]++;
+
 	 // tau particles
 	 m=l1pm.tauParticles().size();
-         for (unsigned int j=0; j!=m; i++) vt[l1pm.tauParticles()[j].key()]++;
+	 //	 LogDebug("") << "t " << m;
+         for (unsigned int j=0; j!=m; j++) vt[l1pm.tauParticles()[j].key()]++;
+
 	 // jet particles
 	 m=l1pm.jetParticles().size();
-         for (unsigned int j=0; j!=m; i++) vj[l1pm.jetParticles()[j].key()]++;
+	 //	 LogDebug("") << "j " << m;
+         for (unsigned int j=0; j!=m; j++) vj[l1pm.jetParticles()[j].key()]++;
+
 	 // (single global) met "particle"
+	 //	 LogDebug("") << "Ma " << vM[0];
          if (l1pm.etMissParticle() != L1EtMissParticleRefProd() ) vM[0]++;
+	 //	 LogDebug("") << "Mb " << vM[0];
        }
      }
 
      // record used physics objects in filterobject
-     for (unsigned int i=0; i!=Ne; i++) {
-       if (ve[i]>0) {
-	 ref=RefToBase<Candidate>(L1EmParticleRef  (l1eh,i));
-	 filterobject->putParticle(ref);
-	 ne++;
-       }
-     }
-     for (unsigned int i=0; i!=Nm; i++) {
-       if (vm[i]>0) {
-	 ref=RefToBase<Candidate>(L1MuonParticleRef(l1mh,i));
-	 filterobject->putParticle(ref);
-	 nm++;
-       }
-     }
-     for (unsigned int i=0; i!=Nt; i++) {
-       if (vt[i]>0) {             // taus are stored as jets
-	 ref=RefToBase<Candidate>(L1JetParticleRef (l1th,i));
-	 filterobject->putParticle(ref);
-	 nt++;
-       }
-     }
-     for (unsigned int i=0; i!=Nj; i++) {
-       if (vj[i]>0) {
-	 ref=RefToBase<Candidate>(L1JetParticleRef (l1jh,i));
-	 filterobject->putParticle(ref);
-	 nj++;
-       }
-     }
-     for (unsigned int i=0; i!=NM; i++) {
-       if (vM[i]>0) {
-	 ref=RefToBase<Candidate>(L1EtMissParticleRefProd(l1Mh));
-	 filterobject->putParticle(ref);
-	 nM++;
-       }
-     }
+     //     LogDebug("") << "Inserting into filter object:";
 
-   }
+     // em particles (gamma+electron)
+     for (unsigned int i=0; i!=Ne; i++) if (ve[i]>0) {
+       ref=RefToBase<Candidate>(L1EmParticleRef  (l1eh,i));
+       filterobject->putParticle(ref);
+       ne++;
+     }
+     //     LogDebug("") << "Inserted e: " << ne;
+
+     // muon particles
+     for (unsigned int i=0; i!=Nm; i++) if (vm[i]>0) {
+       ref=RefToBase<Candidate>(L1MuonParticleRef(l1mh,i));
+       filterobject->putParticle(ref);
+       nm++;
+     }
+     //     LogDebug("") << "Inserted m: " << nm;
+
+     // tau particles (taus are stored as jets!)
+     for (unsigned int i=0; i!=Nt; i++) if (vt[i]>0) {
+       ref=RefToBase<Candidate>(L1JetParticleRef (l1th,i));
+       filterobject->putParticle(ref);
+       nt++;
+     }
+     //     LogDebug("") << "Inserted t: " << nt;
+
+     // jet particles
+     for (unsigned int i=0; i!=Nj; i++) if (vj[i]>0) {
+       ref=RefToBase<Candidate>(L1JetParticleRef (l1jh,i));
+       filterobject->putParticle(ref);
+       nj++;
+     }
+     //     LogDebug("") << "Inserted j: " << nj;
+
+     // (single global) met "particle"
+     for (unsigned int i=0; i!=NM; i++) if (vM[i]>0) {
+       ref=RefToBase<Candidate>(L1EtMissParticleRefProd(l1Mh));
+       filterobject->putParticle(ref);
+       nM++;
+     }
+     //     LogDebug("") << "Inserted M: " << nM;
+
+   } // if (accept)
 
    // put filter object into the Event
    iEvent.put(filterobject);
