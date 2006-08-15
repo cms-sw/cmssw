cb44d36d5b31dde0594f0e76021aa6cdfbb3c153
Pietro Govoni <sha1-b5f71fec85fef77f7f4bdfe5a60a81247d80ee44@cern.ch>
2006-08-15 09:32:35 +0000

    EDfilter to select only runs of a given type

---
+++ RecoTBCalo/EcalTBRecProducers/src/IsTBH4Type.cc
@@ -0,0 +1,78 @@
+//
+// Original Author:  Pietro Govoni
+//         Created:  Thu Aug 10 16:21:22 CEST 2006
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoTBCalo/EcalTBRecProducers/interface/IsTBH4Type.h"
+#include "TBDataFormats/EcalTBObjects/interface/EcalTBEventHeader.h"
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+IsTBH4Type::IsTBH4Type(const edm::ParameterSet& iConfig)
+{
+   //now do what ever initialization is needed
+   eventHeaderCollection_ = iConfig.getParameter<std::string> ("eventHeaderCollection") ;
+   eventHeaderProducer_   = iConfig.getParameter<std::string> ("eventHeaderProducer") ;
+   typeToFlag_            = iConfig.getParameter<std::string> ("typeToFlag") ;
+   notFound_              = iConfig.getUntrackedParameter<bool> ("ifHeaderNotFound","false") ;
+}
+
+
+IsTBH4Type::~IsTBH4Type()
+{
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+bool
+IsTBH4Type::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+
+   using namespace edm;
+
+   Handle<EcalTBEventHeader> pEventHeader ;
+   const EcalTBEventHeader* evtHeader=0 ;
+   try {
+     iEvent.getByLabel ( eventHeaderProducer_ , pEventHeader ) ;
+     evtHeader = pEventHeader.product () ; // get a ptr to the product
+   } catch ( std::exception& ex ) {
+     std::cerr << ex.what () << std::endl ;
+   }
+   if (!evtHeader) return notFound_ ;
+//   std::cout << "PIETRO " << evtHeader->eventType () << std::endl ;
+//   std::cout << "PIETRO " << (evtHeader->eventType () != typeToFlag_) << std::endl ;
+   if (evtHeader->eventType () != typeToFlag_) return false ;
+
+   return true;
+}
+
--- RecoTBCalo/EcalTBRecProducers/src/SealModule.cc
+++ RecoTBCalo/EcalTBRecProducers/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTBCalo/EcalTBRecProducers/interface/EcalTBWeightUncalibRecHitProducer.h"
+#include "RecoTBCalo/EcalTBRecProducers/interface/IsTBH4Type.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalTBWeightUncalibRecHitProducer );
+DEFINE_ANOTHER_FWK_MODULE( IsTBH4Type );
