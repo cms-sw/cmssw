40d30a985bc0ebd7ca81c3ea480a9e490dac8bf4
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-08-15 22:13:56 +0000

    Adding rootscripts

--- OnlineDB/EcalCondDB/rootscripts/test_PedestalMapGain12.cpp
+++ OnlineDB/EcalCondDB/rootscripts/test_PedestalMapGain12.cpp
@@ -1,7 +1,7 @@
 void test_PedestalMapGain12(){
 
 /*connect with oracle server */
-TSQLServer *db=TSQLServer::Connect("oracle://pccmsecdb:1521/ecalh4db","cond01","XXXpasswordXXX");
+TSQLServer *db=TSQLServer::Connect("oracle://pccmsecdb:1521/ecalh4db","read01","XXXpasswordXXX");
 
  gStyle->SetOptStat(0);
  gStyle->SetOptFit();
--- OnlineDB/EcalCondDB/rootscripts/test_TempMap.cpp
+++ OnlineDB/EcalCondDB/rootscripts/test_TempMap.cpp
@@ -1,7 +1,7 @@
 void test_TempMap(){
 
 /*connect with oracle server */
-TSQLServer *db=TSQLServer::Connect("oracle://pccmsecdb:1521/ecalh4db","cond01","XXXpasswordXXX");
+TSQLServer *db=TSQLServer::Connect("oracle://pccmsecdb:1521/ecalh4db","read01","XXXpasswordXXX");
 
  gStyle->SetOptStat(0);
  gStyle->SetOptFit();
