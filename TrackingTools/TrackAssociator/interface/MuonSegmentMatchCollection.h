257c08d0f448c856b795fcc4e95e9817edef27ad
jribnik <sha1-1f7b1d94f19f23f67adbbb4f5dcb8ced97706511@cern.ch>
2006-08-16 21:57:55 +0000

    added for TestTrackAssociatorProducer

---
+++ TrackingTools/TrackAssociator/interface/MuonSegmentMatchCollection.h
@@ -0,0 +1,9 @@
+#ifndef TrackAssociator_MuonSegmentMatchCollection_h
+#define TrackAssociator_MuonSegmentMatchCollection_h
+
+#include <vector>
+#include "TrackingTools/TrackAssociator/interface/MuonSegmentMatch.h"
+
+typedef std::vector<MuonSegmentMatch> MuonSegmentMatchCollection;
+
+#endif
---
+++ TrackingTools/TrackAssociator/interface/TrackDetMatchInfoCollection.h
@@ -0,0 +1,9 @@
+#ifndef TrackAssociator_TrackDetMatchInfoCollection_h
+#define TrackAssociator_TrackDetMatchInfoCollection_h
+
+#include <vector>
+#include "TrackingTools/TrackAssociator/interface/TrackDetMatchInfo.h"
+
+typedef std::vector<TrackDetMatchInfo> TrackDetMatchInfoCollection;
+
+#endif
