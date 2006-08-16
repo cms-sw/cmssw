56886d32920d1c9ce8a65351f4421d2932bd8717
jribnik <sha1-1f7b1d94f19f23f67adbbb4f5dcb8ced97706511@cern.ch>
2006-08-16 21:56:00 +0000

    added various accessors; unsigned int id -> DetId

--- TrackingTools/TrackAssociator/interface/MuonSegmentMatch.h
+++ TrackingTools/TrackAssociator/interface/MuonSegmentMatch.h
@@ -2,9 +2,19 @@
 #define TrackAssociator_MuonSegmentMatch_h
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "DataFormats/Math/interface/Point3D.h"
+#include "DataFormats/DetId/interface/DetId.h"
 
 class MuonSegmentMatch {
  public:
+   int station() const;
+   int detector() const { return id.subdetId(); }
+   double pullX(bool scaled = true) const;
+   double pullY(bool scaled = true) const;
+   double scaledSegmentLocalErrorXX() const;
+   double scaledSegmentLocalErrorYY() const;
+   double scaledTrajectoryLocalErrorXX() const;
+   double scaledTrajectoryLocalErrorYY() const;
+
    math::XYZPoint  segmentGlobalPosition;
    math::XYZPoint  segmentLocalPosition;
    math::XYZVector segmentLocalDirection;
@@ -23,6 +33,6 @@ class MuonSegmentMatch {
    float  trajectoryLocalErrorDxDz;
    float  trajectoryLocalErrorDyDz;
 
-   unsigned int id;
+   DetId id;
 };
 #endif
