e813d8d0129836e3c6fcc49c72cc5a61b70e89c4
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2006-08-16 15:29:57 +0000

    monitor selector now deals with non-TB data files...

--- DQM/HcalMonitorModule/src/HcalMonitorSelector.cc
+++ DQM/HcalMonitorModule/src/HcalMonitorSelector.cc
@@ -3,8 +3,8 @@
 /*
  * \file HcalMonitorSelector.cc
  * 
- * $Date: 2006/04/04 19:26:04 $
- * $Revision: 1.2 $
+ * $Date: 2006/04/18 19:24:15 $
+ * $Revision: 1.3 $
  * \author W Fisher
  *
 */
@@ -22,11 +22,16 @@ HcalMonitorSelector::~HcalMonitorSelector(){
 void HcalMonitorSelector::processEvent(const edm::Event& e){
   m_eventMask = 0;
 
+  ///Just pass these through for now...
+  m_eventMask = m_eventMask|DO_HCAL_DIGIMON;
+  m_eventMask = m_eventMask|DO_HCAL_DFMON;
+  m_eventMask = m_eventMask|DO_HCAL_RECHITMON;
+
   edm::Handle<HcalTBTriggerData> triggerD;
   try{
     e.getByType(triggerD);
   }
-  catch(...) { m_runNum=-1; return; }
+  catch(...) { m_runNum=-1; m_eventMask = m_eventMask|DO_HCAL_PED_CALIBMON|DO_HCAL_LED_CALIBMON; return; }
   const HcalTBTriggerData trigger = *triggerD;
   
   m_runNum = trigger.runNumber();
@@ -37,11 +42,6 @@ void HcalMonitorSelector::processEvent(const edm::Event& e){
   if (trigger.wasInSpillPedestalTrigger()) m_eventMask = m_eventMask|DO_HCAL_PED_CALIBMON;
   if (trigger.wasLEDTrigger()) m_eventMask = m_eventMask|DO_HCAL_LED_CALIBMON;
   if (trigger.wasLaserTrigger()) m_eventMask = m_eventMask|DO_HCAL_LASER_CALIBMON;
-
-  ///Just pass these through for now...
-  m_eventMask = m_eventMask|DO_HCAL_DIGIMON;
-  m_eventMask = m_eventMask|DO_HCAL_DFMON;
-  m_eventMask = m_eventMask|DO_HCAL_RECHITMON;
 
   return;
 }
