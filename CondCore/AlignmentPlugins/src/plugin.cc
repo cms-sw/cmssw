43a5595b570f43cc570b39171f3f4b639f758d7f
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-08-17 14:52:00 +0000

    Added records for muon system

--- CondCore/AlignmentPlugins/src/plugin.cc
+++ CondCore/AlignmentPlugins/src/plugin.cc
@@ -8,11 +8,18 @@
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/Alignment/interface/Alignments.h"
-#include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
-
 #include "CondFormats/Alignment/interface/AlignmentErrors.h"
+#include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/TrackerAlignmentErrorRcd.h"
+#include "CondFormats/DataRecord/interface/DTAlignmentRcd.h"
+#include "CondFormats/DataRecord/interface/DTAlignmentErrorRcd.h"
+#include "CondFormats/DataRecord/interface/CSCAlignmentRcd.h"
+#include "CondFormats/DataRecord/interface/CSCAlignmentErrorRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(TrackerAlignmentRcd,Alignments);
 REGISTER_PLUGIN(TrackerAlignmentErrorRcd,AlignmentErrors);
+REGISTER_PLUGIN(DTAlignmentRcd,Alignments);
+REGISTER_PLUGIN(DTAlignmentErrorRcd,AlignmentErrors);
+REGISTER_PLUGIN(CSCAlignmentRcd,Alignments);
+REGISTER_PLUGIN(CSCAlignmentErrorRcd,AlignmentErrors);
