379095540be5ff9e7a4717e1a3fc79c95aa899b9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-08-20 22:41:11 +0000

    Add FEDRawDataCollection::swap

--- DataFormats/FEDRawData/interface/FEDRawDataCollection.h
+++ DataFormats/FEDRawData/interface/FEDRawDataCollection.h
@@ -4,14 +4,15 @@
 /** \class FEDRawDataCollection
  *  An EDCollection storing the raw data for all  FEDs in a Event.
  *  
- *  Reference: DaqPrototype/DaqPersistentData/interface/DaqFedOpaqueData.h
+ *  Reference: DaqPrototype/DaqPersistentData/interface/DaqFEDOpaqueData.h
  *
- *  $Date: 2005/10/06 18:25:22 $
- *  $Revision: 1.4 $
+ *  $Date: 2005/10/18 13:28:12 $
+ *  $Revision: 1.5 $
  *  \author N. Amapane - S. Argiro'
  */
 
 #include <DataFormats/FEDRawData/interface/FEDRawData.h>
+#include "DataFormats/Common/interface/traits.h"
 #include <vector>
 
 
@@ -29,12 +30,27 @@ class FEDRawDataCollection  {
 
   FEDRawDataCollection(const FEDRawDataCollection &);
 
+  void swap(FEDRawDataCollection & other) {
+    data_.swap(other.data_);
+  }
+
  private:
 
   std::vector<FEDRawData> data_; ///< the raw data 
 
 };
 
+inline
+void swap(FEDRawDataCollection & a, FEDRawDataCollection & b) {
+  a.swap(b);
+}
+
+namespace edm {
+  template <>
+  struct has_swap<FEDRawDataCollection> {
+    static bool const value = true;
+  };
+}
 
 #endif
 
