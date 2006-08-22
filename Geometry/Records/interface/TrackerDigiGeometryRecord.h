28cb3264f809c03ba19d7ad8ef4d7ae17fddfce8
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-08-22 15:45:44 +0000

    Added dependency on alignment errors record

--- Geometry/Records/interface/TrackerDigiGeometryRecord.h
+++ Geometry/Records/interface/TrackerDigiGeometryRecord.h
@@ -16,20 +16,22 @@
 //
 // Author:      
 // Created:     Mon Jul 25 11:05:09 EDT 2005
-// $Id: TrackerDigiGeometryRecord.h,v 1.1 2005/08/09 10:37:47 tboccali Exp $
+// $Id: TrackerDigiGeometryRecord.h,v 1.2 2006/06/29 15:17:42 fronga Exp $
 //
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
+#include "CondFormats/DataRecord/interface/TrackerAlignmentErrorRcd.h"
 #include "boost/mpl/vector.hpp"
 
 
 class TrackerDigiGeometryRecord : 
   public edm::eventsetup::DependentRecordImplementation<TrackerDigiGeometryRecord,
-														boost::mpl::vector<IdealGeometryRecord,
-																		   TrackerAlignmentRcd > > {};
+							boost::mpl::vector<IdealGeometryRecord, 
+									   TrackerAlignmentRcd, 
+									   TrackerAlignmentErrorRcd> > {};
 
 #endif /* RECORDS_TRACKERDIGIGEOMETRYRECORD_H */
 
