34f8daff2f544dfdc49f86f429a2d56bcea737ab
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-08-23 19:16:13 +0000

    Add reference support

--- DataFormats/HcalRecHit/src/classes.h
+++ DataFormats/HcalRecHit/src/classes.h
@@ -5,7 +5,12 @@
 #include "DataFormats/HcalRecHit/interface/HcalTriggerPrimitiveRecHit.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/HcalRecHit/interface/HcalSourcePositionData.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
+#include "DataFormats/Common/interface/RefVector.h"
+#include "DataFormats/Common/interface/SortedCollection.h"
 
 namespace {
   namespace {
@@ -31,6 +36,27 @@ namespace {
     edm::Wrapper<HcalCalibRecHitCollection> theCalibw_;
     edm::Wrapper<HcalTrigPrimRecHitCollection> theHTPw_;
     edm::Wrapper<HcalSourcePositionData> theSPDw_;
- }
+
+    edm::Ref<HBHERecHitCollection> theHBHEr_;
+    edm::Ref<HORecHitCollection> theHOr_;
+    edm::Ref<HFRecHitCollection> theHFr_;
+    edm::Ref<ZDCRecHitCollection> theZDCr_;
+    edm::Ref<HcalCalibRecHitCollection> theCalibr_;
+    edm::Ref<HcalTrigPrimRecHitCollection> theHTPr_;
+
+    edm::RefProd<HBHERecHitCollection> theHBHErp_;
+    edm::RefProd<HORecHitCollection> theHOrp_;
+    edm::RefProd<HFRecHitCollection> theHFrp_;
+    edm::RefProd<ZDCRecHitCollection> theZDCrp_;
+    edm::RefProd<HcalCalibRecHitCollection> theCalibrp_;
+    edm::RefProd<HcalTrigPrimRecHitCollection> theHTPrp_;
+
+    edm::RefVector<HBHERecHitCollection> theHBHErv_;
+    edm::RefVector<HORecHitCollection> theHOrv_;
+    edm::RefVector<HFRecHitCollection> theHFrv_;
+    edm::RefVector<ZDCRecHitCollection> theZDCrv_;
+    edm::RefVector<HcalCalibRecHitCollection> theCalibrv_;
+    edm::RefVector<HcalTrigPrimRecHitCollection> theHTPrv_;
+  }
 }
 
