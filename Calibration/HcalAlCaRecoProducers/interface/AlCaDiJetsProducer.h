68c09ee3ce2324d05ba8522fb738241da0033d64
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2006-08-23 13:12:44 +0000

    *** empty log message ***

---
+++ Calibration/HcalAlCaRecoProducers/interface/AlCaDiJetsProducer.h
@@ -0,0 +1,64 @@
+#ifndef AlCaDiJetsProducer_AlCaHcalProducers_h
+#define AlCaDiJetsProducer_AlCaHcalProducers_h
+
+
+// -*- C++ -*-
+
+
+// system include files
+#include <memory>
+#include <string>
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/Vector/interface/GlobalPoint.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "RecoTracker/TrackProducer/interface/TrackProducerBase.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+
+//
+// class declaration
+//
+namespace edm {
+  class ParameterSet;
+  class Event;
+  class EventSetup;
+}
+
+namespace cms
+{
+
+class AlCaDiJetsProducer : public edm::EDProducer {
+   public:
+      explicit AlCaDiJetsProducer(const edm::ParameterSet&);
+      ~AlCaDiJetsProducer();
+
+
+      virtual void produce(edm::Event &, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+   std::string mInput;
+};
+}// end namespace cms
+#endif
---
+++ Calibration/HcalAlCaRecoProducers/src/AlCaDiJetsProducer.cc
@@ -0,0 +1,95 @@
+#include "Calibration/HcalAlCaRecoProducers/interface/AlCaDiJetsProducer.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+using namespace edm;
+using namespace std;
+using namespace reco;
+
+namespace cms
+{
+
+AlCaDiJetsProducer::AlCaDiJetsProducer(const edm::ParameterSet& iConfig):
+       mInput(iConfig.getParameter<std::string>("src"))
+{
+   
+   //register your products
+
+   produces<reco::TrackCollection>();
+   produces<CaloJetCollection>();
+}
+
+
+AlCaDiJetsProducer::~AlCaDiJetsProducer()
+{
+ 
+
+}
+
+
+// ------------ method called to produce the data  ------------
+void
+AlCaDiJetsProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+   using namespace std;
+// Jet Collection
+   edm::Handle<CaloJetCollection> jets;                        //Define Inputs
+   iEvent.getByLabel(mInput, jets);                            //Get Inputs
+   auto_ptr<CaloJetCollection> result (new CaloJetCollection); //Corrected jets
+   CaloJetCollection::const_iterator jet = jets->begin ();
+   double myvalue = 4.*atan(1.);
+   double twomyvalue = 8.*atan(1.); 
+   CaloJet fJet1,fJet2;
+   if(jets->size() > 1 )
+   {
+     fJet1 = (*jets)[0];
+     fJet2 = (*jets)[1];
+     double phi1=fabs(fJet1.eta());
+     double phi2=fabs(fJet2.eta());
+     double dphi = fabs(phi1-phi2);
+     if (dphi > myvalue) dphi = twomyvalue-dphi;
+     double degreedphi = dphi*180./myvalue;
+     if (fabs(degreedphi-180.) > 10. ) return;
+     result->push_back (fJet1);
+     result->push_back (fJet2);
+   } else
+   {
+     return;
+   }
+
+// Track Collection    
+   edm::Handle<reco::TrackCollection> trackCollection;
+
+   try {
+     iEvent.getByType(trackCollection);
+   } catch ( std::exception& ex ) {
+     LogDebug("") << "AlCaIsoTracksProducer: Error! can't get product!" << std::endl;
+   }
+   const reco::TrackCollection tC = *(trackCollection.product());
+  
+   //Create empty output collections
+
+   std::auto_ptr<reco::TrackCollection> outputTColl(new reco::TrackCollection);
+
+   for (reco::TrackCollection::const_iterator track=tC.begin(); track!=tC.end()-1; track++)
+   {
+               
+               double deta = track->momentum().eta() - fJet1.eta();  
+               double dphi = fabs(track->momentum().phi() - fJet1.phi());
+               if (dphi > atan(1.)*4.) dphi = 8.*atan(1.) - dphi;
+               double ddir1 = sqrt(deta*deta-dphi*dphi);
+               deta = track->momentum().eta() - fJet2.eta();
+               dphi = fabs(track->momentum().phi() - fJet2.phi());
+               if (dphi > atan(1.)*4.) dphi = 8.*atan(1.) - dphi;
+               double ddir2 = sqrt(deta*deta-dphi*dphi);
+
+      if( ddir1 < 0.5  || ddir2 < 0.5)      
+      {
+         outputTColl->push_back(*track);
+      } 
+   }
+  //Put selected information in the event
+  iEvent.put( outputTColl, "JetTracksCollection");
+  iEvent.put( result, "DijetBackToBackCollection");
+}
+}
