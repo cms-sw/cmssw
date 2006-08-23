213e0bb8fb8d54be8a6a2a3aecf836d9fe1f36e6
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2006-08-23 16:09:16 +0000

    first submission

---
+++ FWCore/FWLite/test/autoload_with_std.C
@@ -0,0 +1,17 @@
+{
+//NOTE: this test causes the macro to abort since
+// edmtest is unknown at this point
+//if( TClass::GetClass("std::vector<edmtest::Thing>") ) {
+//   cout <<"class already exists!"<<endl;
+//   exit(1);
+//}
+//cout <<"class not present yet"<<endl;
+gSystem->Load("libFWCoreFWLite");
+AutoLibraryLoader::enable();
+if( !TClass::GetClass("std::vector<edmtest::Thing>") ) {
+   cout <<"class still missing"<<endl;
+   exit(1);
+}
+cout <<"class loaded"<<endl;
+exit(0);
+}
