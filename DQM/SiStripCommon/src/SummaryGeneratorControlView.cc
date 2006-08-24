60cad644521fd96140c819fc5b038efb98bd0e1d
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-08-24 08:43:08 +0000

    bug fix

--- DQM/SiStripCommon/src/SummaryGeneratorControlView.cc
+++ DQM/SiStripCommon/src/SummaryGeneratorControlView.cc
@@ -2,6 +2,7 @@
 #include "DQM/SiStripCommon/interface/SiStripHistoNamingScheme.h"
 #include "DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h"
 #include "DataFormats/SiStripDetId/interface/SiStripControlKey.h"
+#include <iostream>
 #include <sstream>
 #include <cmath>
 
@@ -14,18 +15,15 @@ void SummaryGeneratorControlView::fillMap( const string& directory,
 					   const float& value,
 					   const float& error ) {
   
-  // Clear map
-  map_.clear();
-
   // Create control path structs for both histo level and key
   SiStripHistoNamingScheme::ControlPath level = SiStripHistoNamingScheme::controlPath( directory );
   SiStripControlKey::ControlPath path = SiStripControlKey::path( key );
-
-  if ( ( path.fecCrate_ && level.fecCrate_ == level.fecCrate_ ) && 
-       ( path.fecSlot_  && level.fecSlot_  == level.fecSlot_  ) && 
-       ( path.fecRing_  && level.fecRing_  == level.fecRing_  ) && 
-       ( path.ccuAddr_  && level.ccuAddr_  == level.ccuAddr_  ) && 
-       ( path.ccuChan_  && level.ccuChan_  == level.ccuChan_  ) ) { 
+  
+  if ( ( ( path.fecCrate_ == level.fecCrate_ ) || ( level.fecCrate_ == sistrip::invalid_ ) ) &&
+       ( ( path.fecSlot_  == level.fecSlot_  ) || ( level.fecSlot_  == sistrip::invalid_ ) ) &&
+       ( ( path.fecRing_  == level.fecRing_  ) || ( level.fecRing_  == sistrip::invalid_ ) ) && 
+       ( ( path.ccuAddr_  == level.ccuAddr_  ) || ( level.ccuAddr_  == sistrip::invalid_ ) ) &&
+       ( ( path.ccuChan_  == level.ccuChan_  ) || ( level.ccuChan_  == sistrip::invalid_ ) ) ) { 
     
     stringstream bin;
     if ( path.fecCrate_ != sistrip::invalid_ ) { bin << path.fecCrate_ << sistrip::pipe_; }
@@ -34,13 +32,18 @@ void SummaryGeneratorControlView::fillMap( const string& directory,
     if ( path.ccuAddr_  != sistrip::invalid_ ) { bin << path.ccuAddr_  << sistrip::pipe_; }
     if ( path.ccuChan_  != sistrip::invalid_ ) { bin << path.ccuChan_  << sistrip::pipe_; }
     if ( path.channel_  != sistrip::invalid_ ) { bin << path.channel_; }
-    
+
     if ( map_.find( bin.str() ) == map_.end() ) { 
       map_[bin.str()].first = value; 
       map_[bin.str()].second = error; 
+//       cout << " bin: " << bin.str()
+// 	   << " value: " << value 
+// 	   << " error: " << error 
+// 	   << endl;
     } else { /* warning here? */ }
   
   }
+//   cout << "map_.size(): " << map_.size() << endl;
   
 }
 
@@ -53,14 +56,15 @@ void SummaryGeneratorControlView::logicalView( TH1& histo ) {
   
   // Set histogram number of bins and min/max
   histo.SetBins( map_.size(), 0., (Double_t)map_.size() );
-
+  
   // Iterate through map, set bin labels and fill histogram
-  uint16_t ibin = 0;
+  uint16_t ibin = 1;
   map< string, pair<float,float> >::const_iterator idevice = map_.begin();
   for ( ; idevice != map_.end(); idevice++ ) {
     histo.GetXaxis()->SetBinLabel( (Int_t)ibin, idevice->first.c_str() );
     histo.SetBinContent( (Int_t)ibin, idevice->second.first );
     histo.SetBinError( (Int_t)ibin, idevice->second.second );
+    ibin++;
   }
   
 }
@@ -68,7 +72,7 @@ void SummaryGeneratorControlView::logicalView( TH1& histo ) {
 //------------------------------------------------------------------------------
 //
 void SummaryGeneratorControlView::simpleDistr( TH1& histo ) {
-  
+
   // Check number of entries in map
   if ( map_.empty() ) { return; }
   
@@ -77,11 +81,10 @@ void SummaryGeneratorControlView::simpleDistr( TH1& histo ) {
   int high = (int)ceil( fabs(ran.first) );
   int low  = (int)ceil( fabs(ran.second) );
   int diff = abs(high) - abs(low);
-  
+
   // Set histogram binning
-  if ( diff ) {
-    histo.SetBins( int(diff+abs(0.2*diff)), low-abs(0.1*diff), high+abs(0.1*diff) );
-  } else { histo.SetBins( 2, low-1., low+1. ); }
+  if ( diff ) { histo.SetBins( int(diff+abs(0.2*diff)), low-abs(0.1*diff), high+abs(0.1*diff) ); }
+  else { histo.SetBins( 2, low-1., low+1. ); }
   
   // Fill histogram
   map< string, pair<float,float> >::const_iterator idevice = map_.begin();
@@ -90,6 +93,8 @@ void SummaryGeneratorControlView::simpleDistr( TH1& histo ) {
   }
 
 }
+
+
 
 
 
