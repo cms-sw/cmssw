a77f6270ddc4a3c7a16df6c3736b033bd8905db1
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-08-24 05:38:14 +0000

    use default 345 clusters.

--- DQM/SiStripMonitorCluster/test/lscmsdisk0.sh
+++ DQM/SiStripMonitorCluster/test/lscmsdisk0.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 RUNNR=$1
-CMSDISK0_MTCC_DIR="/data0/mtcc_test/";
+CMSDISK0_MTCC_DIR="/data0/mtcc_0_9_0";
+echo $CMSDISK0_MTCC_DIR
 
 case $# in
 0)
-        ssh `whoami`@cmsdisk0.cern.ch "ls ${CMSDISK0_MTCC_DIR}";
+        ssh `whoami`@cmsdisk0.cern.ch "ls -lh ${CMSDISK0_MTCC_DIR}";
 	;;
 1)
         RUNNR=$1;
-        ssh `whoami`@cmsdisk0.cern.ch "ls ${CMSDISK0_MTCC_DIR}" | grep ${RUNNR} | grep '\.root'
+        ssh `whoami`@cmsdisk0.cern.ch "ls -lh ${CMSDISK0_MTCC_DIR}" | grep ${RUNNR} | grep '\.dat'
         ;;
 *)
         ;;
