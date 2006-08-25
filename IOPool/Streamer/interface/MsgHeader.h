b74be99c6f0a692ae8f9e2b17a0b43b6d694f1be
Kurt Biery <sha1-a0b81a63ebfa22b3eb476206725e29d6b356d7db@cern.ch>
2006-08-25 14:10:51 +0000

    Added event request message code

--- IOPool/Streamer/interface/MsgHeader.h
+++ IOPool/Streamer/interface/MsgHeader.h
@@ -13,7 +13,8 @@ struct Header
 
   // 20-Jul-2006, KAB: added enumeration for message types
   enum Codes { INVALID = 0, INIT = 1, EVENT = 2, DONE = 3, EOFRECORD = 4,
-               HEADER_REQUEST = 5,CONS_REG_REQUEST = 6,CONS_REG_RESPONSE = 7 };
+               HEADER_REQUEST = 5, EVENT_REQUEST = 6,
+               CONS_REG_REQUEST = 7, CONS_REG_RESPONSE = 8 };
 };
 
 // as we need to see it
