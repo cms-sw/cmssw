649c0ba62b2b4ecd349a1ce2813e6ae6f2cdf149
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-08-26 10:10:13 +0000

    add LTC filter

--- EventFilter/SiStripChannelChargeFilter/src/SealModules.cc
+++ EventFilter/SiStripChannelChargeFilter/src/SealModules.cc
@@ -6,11 +6,14 @@
 #include "EventFilter/SiStripChannelChargeFilter/interface/MTCCHLTrigger.h"
 #include "EventFilter/SiStripChannelChargeFilter/interface/ClusterMTCCFilter.h"
 #include "EventFilter/SiStripChannelChargeFilter/interface/TrackMTCCFilter.h"
+#include "EventFilter/SiStripChannelChargeFilter/interface/LTCTriggerBitsFilter.h"
 
 using cms::MTCCHLTrigger;
 using cms::ClusterMTCCFilter;
 using cms::TrackMTCCFilter;
+using cms::LTCTriggerBitsFilter;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MTCCHLTrigger)
 DEFINE_ANOTHER_FWK_MODULE(ClusterMTCCFilter)
 DEFINE_ANOTHER_FWK_MODULE(TrackMTCCFilter)
+DEFINE_ANOTHER_FWK_MODULE(LTCTriggerBitsFilter)
