187c4f5f18c05aa42a9f8dca9a2129e4010f1cc9
Norbert Neumeister <sha1-7abc839d53ede37ea0c445f89ed7dc2d61338f2b@cern.ch>
2006-08-29 22:29:02 +0000

    delete trajectory

--- RecoMuon/TrackingTools/interface/MuonCandidate.h
+++ RecoMuon/TrackingTools/interface/MuonCandidate.h
@@ -4,8 +4,8 @@
 /** \class MuonCandidate
  *  Auxiliary class for muon candidates
  *
- *  $Date: 2006/07/25 13:10:47 $
- *  $Revision: 1.3 $
+ *  $Date: 2006/07/25 19:31:25 $
+ *  $Revision: 1.4 $
  *  \author N. Neumeister	Purdue University 
  */
 
@@ -30,7 +30,7 @@ class MuonCandidate {
       theTrajectory(traj), theMuonTrack(muon), theTrackerTrack(tracker) {} 
   
     /// destructor
-    virtual ~MuonCandidate() {}
+    virtual ~MuonCandidate() { if (theTrajectory) delete theTrajectory; }
   
     /// return trajectory
     Trajectory* trajectory() const { return theTrajectory; }
