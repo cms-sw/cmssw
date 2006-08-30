ab2bae8d2c48f67202e37e922f227fcc42054f03
Andre Sznajder <sha1-641e645e23fa286db91b280caea731b827d8f661@cern.ch>
2006-08-30 12:18:51 +0000

    change of names

--- Alignment/MuonAlignment/test/TestMisalign.cpp
+++ Alignment/MuonAlignment/test/TestMisalign.cpp
@@ -7,9 +7,6 @@
 // Description: Module to test the Alignment software
 //
 //
-// Original Author:  Frederic Ronga
-//         Created:  March 16, 2006
-//
 
 
 // system include files
@@ -34,11 +31,11 @@
 #include "Alignment/MuonAlignment/interface/AlignableDTChamber.h"
 #include "Alignment/MuonAlignment/interface/AlignableCSCChamber.h"
 
-#include "Geometry/DTGeometryBuilder/src/DTGeometryBuilderFromDDD.h"
-#include "Geometry/CSCGeometryBuilder/src/CSCGeometryBuilderFromDDD.h"
-
-#include "Geometry/Records/interface/MuonGeometryRecord.h"
-
+#include "Geometry/DTGeometry/interface/DTGeometry.h"
+#include "Geometry/DTGeometry/interface/DTChamber.h"
+#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
+#include "Geometry/CSCGeometry/interface/CSCChamber.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 
 //
 //
@@ -54,6 +51,15 @@ public:
   virtual void analyze( const edm::Event&, const edm::EventSetup& );
 private:
 
+  void fillTree( const GeomDet* geomDet );
+
+  // ----------member data ---------------------------
+  TTree* theTree;
+  TFile* theFile;
+  float x,y,z,phi,theta,length,thick,width;
+  int Id_;
+  TRotMatrix* rot;
+
 };
 
 //
@@ -62,12 +68,31 @@ private:
 TestMisalign::TestMisalign( const edm::ParameterSet& iConfig ) 
 { 
 
+  // Open root file and define tree
+  std::string fileName = iConfig.getUntrackedParameter<std::string>("fileName","test.root");
+  theFile = new TFile( fileName.c_str(), "RECREATE" );
+  theTree = new TTree( "theTree", "Detector units positions" );
+
+  theTree->Branch("Id",     &Id_,    "Id/I"     );
+  theTree->Branch("x",      &x,      "x/F"      );
+  theTree->Branch("y",      &y,      "y/F"      );
+  theTree->Branch("z",      &z,      "z/F"      );
+  theTree->Branch("phi",    &phi,    "phi/F"    );
+  theTree->Branch("theta",  &theta,  "theta/F"  );
+  theTree->Branch("length", &length, "length/F" );
+  theTree->Branch("width",  &width,  "width/F"  );
+  theTree->Branch("thick",  &thick,  "thick/F"  );
+  rot = 0;
+  theTree->Branch("rot",    "TRotMatrix", &rot  );
 
 }
 
 
 TestMisalign::~TestMisalign()
 { 
+  
+  theTree->Write();
+  theFile->Close();
   
 }
 
@@ -79,21 +104,56 @@ TestMisalign::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup )
    
   edm::LogInfo("MuonAlignment") << "Starting!";
 
-
-
   //
-  // Retrieve ideal geometry from event setup
+  // Retrieve muon geometry from event setup
   //
-  edm::ESHandle<DTGeometry> dtGeometry;
-  edm::ESHandle<CSCGeometry> cscGeometry;
-  iSetup.get<MuonGeometryRecord>().get( dtGeometry );
-  iSetup.get<MuonGeometryRecord>().get( cscGeometry );
+  edm::ESHandle<DTGeometry> pDT;
+  edm::ESHandle<CSCGeometry> pCSC;
 
-  AlignableMuon* theAlignableMuon = new AlignableMuon( &(*dtGeometry), &(*cscGeometry) );
+  iSetup.get<MuonGeometryRecord>().get( pDT );     
+  iSetup.get<MuonGeometryRecord>().get( pCSC ); 
 
 
-  
+  // Now loop on detector units, and store position and orientation
+  for ( std::vector<GeomDet*>::const_iterator iGeomDet = pDT->dets().begin();
+		iGeomDet != pDT->dets().end(); iGeomDet++ )
+	this->fillTree( *iGeomDet );
+  for ( std::vector<GeomDet*>::const_iterator iGeomDet = pCSC->dets().begin();
+		iGeomDet != pCSC->dets().end(); iGeomDet++ )
+	this->fillTree( *iGeomDet );
+
   edm::LogInfo("MuonAlignment") << "Done!";
+
+}
+
+
+//__________________________________________________________________________________________________
+void TestMisalign::fillTree( const GeomDet* geomDet )
+{
+  Id_     = geomDet->geographicalId().rawId();
+  x      = geomDet->position().x();
+  y      = geomDet->position().y();
+  z      = geomDet->position().z();
+  phi    = geomDet->surface().normalVector().phi();
+  theta  = geomDet->surface().normalVector().theta();
+  length = geomDet->surface().bounds().length();
+  width  = geomDet->surface().bounds().width();
+  thick  = geomDet->surface().bounds().thickness();
+
+  double matrix[9] = { 
+	geomDet->rotation().xx(),
+	geomDet->rotation().xy(),
+	geomDet->rotation().xz(),
+	geomDet->rotation().yx(),
+	geomDet->rotation().yy(),
+	geomDet->rotation().yz(),
+	geomDet->rotation().zx(),
+	geomDet->rotation().zy(),
+	geomDet->rotation().zz()
+  };
+  rot = new TRotMatrix( "rot", "rot", matrix );
+
+  theTree->Fill();
 
 }
 
