0c7ec253fe8e2863662c401155fdfbec0c9970ce
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-08-31 09:38:47 +0000

    first upload

---
+++ CondTools/SiStrip/scripts/SiStripCondObjDisplay.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+function usage(){
+    echo -e "\n[usage] SiStripCondObjDisplay.sh [options]"
+    echo -e " -run=<runNb>"
+    echo -e " -CondDb=<sqlite>, <devdb10>, <orcon> (default is sqlite)"
+    echo -e " -dbfile=<dbfile> (needed for CondDb=sqlite)"
+    echo -e " -dbcatalog=<dbcatalog> (needed for CondDb=sqlite)"
+    exit
+}
+
+function getParameter(){
+    what=$1
+    eval $what=\$$#
+    shift
+    where=$@
+    if [ `echo $where | grep -c "\-$what="` = 1 ]; then
+        eval $what=`echo $where | awk -F"${what}=" '{print $2}' | awk '{print $1}'`
+    elif [ `echo $where | grep -c "\-$what"` = 1 ]; then
+	eval $what=1
+    fi
+}
+
+#################
+## MAIN
+#################
+
+[ `echo $@ | grep -c "\-help"` = 1 ] && usage;
+
+getParameter run  $@ 1
+getParameter dbfile $@ ""
+getParameter dbcatalog $@ ""
+getParameter CondDb $@ sqlite
+
+if [ "$CondDb" == "sqlite" ] && [ "$dbfile" != "" ] && [ "$dbcatalog" != "" ]; 
+    then
+    DBfile="sqlite_file:${dbfile}"
+    DBcatalog="file:${dbcatalog}"
+elif [ "$CondDb" == "devdb10" ];  then
+    DBfile="oracle://devdb10/CMS_COND_STRIP"
+    DBcatalog="relationalcatalog_oracle://devdb10/CMS_COND_GENERAL"
+elif [ "$CondDb" == "orcon" ]; then
+    DBfile="oracle://orcon/CMS_COND_STRIP"
+    DBcatalog="relationalcatalog_oracle://orcon/CMS_COND_GENERAL"
+else
+    echo -e "\nWrong options"
+    usage
+fi
+
+
+test_area=/tmp/$USER/Display
+[ ! -e ${test_area} ] && mkdir -p ${test_area}
+
+output_file_name=${test_area}/Display_PedNoise_RunNb_${run}.root 
+ps_file_name=${test_area}/Display_PedNoise_RunNb_${run}.ps 
+cfg_file=${test_area}/SiStripCondObjDisplay_RunNb_${run}.cfg
+
+echo ${cfg_file}
+
+export TNS_ADMIN=/afs/cern.ch/project/oracle/admin
+export CORAL_AUTH_PATH=/afs/cern.ch/cms/DB/conddb
+
+# scramv1 setup -i oracle <<EOF > /dev/null
+
+
+
+# /afs/cern.ch/project/oracle/admin
+
+# EOF 
+
+eval `scramv1 runtime -sh`
+
+cat template_SiStripCondObjDisplay.cfg | sed -e "s#insert_DBfile#$DBfile#" -e "s#insert_DBcatalog#$DBcatalog#" -e "s#insert_output_filename#${output_file_name}#" -e "s#insert_ps_filename#${ps_file_name}#" -e "s#insert_runNb#${run}#" > ${cfg_file}
+echo "cmsRun ${cfg_file}"
+cmsRun ${cfg_file} > ${test_area}/out_diplay_${run}
+
+grep "SiStripCondObjDisplay::geometry_Vs_Cabling"  ${test_area}/out_diplay_${run}
+echo
+echo
+echo "Empty Pedestal Histos" `grep "Empty Ped"  ${test_area}/out_diplay_${run} | wc -l`
+echo "Empty Noise Histos" `grep "Empty Nois"  ${test_area}/out_diplay_${run} | wc -l`
+echo "Empty BadStrip Histos" `grep "Empty Bad"  ${test_area}/out_diplay_${run} | wc -l`
+echo "Pedestal Histos with all channel at zero" `grep "All channel at zero in Ped"  ${test_area}/out_diplay_${run} | wc -l`
+echo "Noise Histos with all channel at zero" `grep "All channel at zero in Nois"  ${test_area}/out_diplay_${run} | wc -l`
+echo "BadStrips Histos with all channel at zero" `grep "All channel at zero in Bad"  ${test_area}/out_diplay_${run} | wc -l`
+
+echo -e "\nroot file and postscript file with histos can be found in  ${test_area}"
+echo -e "to see .ps file do\ngv  ${ps_file_name}&"
---
+++ CondTools/SiStrip/scripts/SiStripO2O.sh
@@ -0,0 +1,159 @@
+#!/bin/sh
+
+function usage () {
+    echo -e "\n[usage]\n SiStripO2O.sh [options]"
+    echo -e " -IOV=<runNb> (default is $default_IOV )"
+    echo -e " -ConfigDb=<user/passwd@path> (default is ${default_ConfigDb} )"
+    echo -e " -ConfigDbVersion=<Major.Minor> (default is ${default_ConfigDb} )"
+    echo -e " -ConfigDbPartition=<partitionName> (default is ${default_ConfigDbPartition} )"
+    echo -e " -doPedNoiseTransfer (default is $default_doPedNoiseTransfer )"
+    echo -e " -doFedCablingTransfer (default is $default_doFedCablingTransfer )" 
+    echo -e " -CondDb=<sqlite>, <devdb10>, <orcon> (default is sqlite)"
+    echo -e " -sqliteDb=<path_name> (default is /tmp/\$user/o2o/dummy_IOV.db)"
+    echo -e " -sqliteCatalog=<path_name> (default is /tmp/\$user/o2o/dummy_IOV.xml)"
+    echo -e " -firstUpload (otherwise works in append mode) "
+
+    
+    exit
+}
+
+function getParameter(){
+    what=$1
+    eval $what=\$$#
+    shift
+    where=$@
+    if [ `echo $where | grep -c "\-$what="` = 1 ]; then
+        eval $what=`echo $where | awk -F"${what}=" '{print $2}' | awk '{print $1}'`
+    elif [ `echo $where | grep -c "\-$what"` = 1 ]; then
+	eval $what=1
+    fi
+}
+
+function settings (){
+    export TNS_ADMIN=/afs/cern.ch/project/oracle/admin
+    export CORAL_AUTH_PATH=/afs/cern.ch/cms/DB/conddb
+    export test_area=/tmp/$USER/o2o
+
+    default_IOV=1
+    default_doPedNoiseTransfer=0
+    default_doFedCablingTransfer=0
+    default_ConfigDb=cms_mtcc_sitracker/cms_mtcc@omds
+    default_ConfigDbVersion=8.189
+    default_ConfigDbPartition="MTCC_DEMO"
+    default_CondDb="sqlite"
+    default_firstUpload=0
+    
+    getParameter help $@ 0
+    [ "$help" = 1 ] && usage
+
+    getParameter doPedNoiseTransfer   $@ ${default_doPedNoiseTransfer}
+    getParameter doFedCablingTransfer $@ ${default_doFedCablingTransfer}
+    getParameter ConfigDb             $@ ${default_ConfigDb}
+    getParameter ConfigDbVersion      $@ ${default_ConfigDbVersion}
+    getParameter ConfigDbPartition    $@ ${default_ConfigDbPartition}
+    getParameter IOV                  $@ ${default_IOV}
+    getParameter CondDb               $@ ${default_CondDb}
+    getParameter firstUpload          $@ ${default_firstUpload} 
+
+    default_sqliteDb=${test_area}/dummy_${default_IOV}.db
+    default_sqliteCatalog=${test_area}/dummy_${default_IOV}.xml
+    getParameter sqliteDb             $@ ${default_sqliteDb}
+    getParameter sqliteCatalog        $@ ${default_sqliteCatalog}
+
+
+    append=1
+    [ "$firstUpload" = 1 ] && append=0
+    export append
+    
+    ConfigDbUser=`echo ${ConfigDb}| awk -F'/' '{print $1}'`
+    ConfigDbPasswd=`echo ${ConfigDb}| awk -F'/' '{print $2}' | awk -F'@' '{print $1}'`
+    ConfigDbPath=`echo ${ConfigDb}| awk -F'@' '{print $2}'`
+    ConfigDbMajorVersion=`echo ${ConfigDbVersion}| awk -F'.' '{print $1}'`
+    ConfigDbMinorVersion=`echo ${ConfigDbVersion}| awk -F'.' '{print $2}'`
+
+    echo -e " -IOV=$IOV"
+    echo -e " -ConfigDb=${ConfigDb}"
+    echo -e " -ConfigDbVersion=${ConfigDbVersion}"
+    echo -e " -ConfigDbPartition=${ConfigDbPartition}"
+    echo -e " -CondDb=$CondDb"
+    echo -e " -firstUpload $firstUpload"
+    echo -e " -doPedNoiseTransfer $doPedNoiseTransfer"
+    echo -e " -doFedCablingTransfer $doFedCablingTransfer"
+    if [ "$CondDb" = "sqlite" ]; then
+	echo -e " -sqliteDb=${sqliteDb}"
+	echo -e " -sqliteCatalog=${sqliteCatalog}"
+    fi
+}
+
+#################
+## MAIN
+#################
+
+
+settings "$@"
+
+
+[ ! -e ${test_area} ] && mkdir -p ${test_area}
+
+scramv1 setup -i oracle <<EOF > /dev/null
+
+
+
+/afs/cern.ch/project/oracle/admin
+
+EOF
+
+eval `scramv1 runtime -sh`
+
+  
+if [ "$CondDb" == "devdb10" ]; then
+    DBfile="oracle://devdb10/CMS_COND_STRIP"
+    DBcatalog="relationalcatalog_oracle://devdb10/CMS_COND_GENERAL"
+elif [ "$CondDb" == "orcon" ]; then
+    DBfile="oracle://orcon/CMS_COND_STRIP"
+    DBcatalog="relationalcatalog_oracle://orcon/CMS_COND_GENERAL"
+elif [ "$CondDb" == "sqlite" ]; then
+    DBfile="sqlite_file:${sqliteDb}"
+    DBcatalog="file:${sqliteCatalog}"
+
+    if [ "${append}" == 0 ];
+	then 
+	rm -f ${sqliteDb}
+	rm -f ${sqliteCatalog}
+
+	echo "OracleDBA/scripts/cmscond_bootstrap_detector.pl --offline_connect $DBfile --auth ${CORAL_AUTH_PATH}/authentication.xml STRIP "
+	../../OracleDBA/scripts/cmscond_bootstrap_detector.pl --offline_connect $DBfile --auth ${CORAL_AUTH_PATH}/authentication.xml STRIP 
+    fi
+else
+    echo "ERROR: wrong argument value: -CondDb=<sqlite>, <devdb10>, <orcon> "
+    echo "EXIT"
+    exit
+fi
+
+
+o2otrans="dummy"
+if [ "$doPedNoiseTransfer" == "1" ] && [ "$doFedCablingTransfer" == "1" ];
+    then 
+    o2otrans="#o2o_all"
+else
+    [ "$doPedNoiseTransfer" == "1" ]    && o2otrans="#o2o_pednoi"
+    [ "$doFedCablingTransfer" == "1" ] && o2otrans="#o2o_cabl"
+fi
+
+
+cfg_file=${test_area}/SiStripO2O_IOV_${IOV}.cfg
+
+
+echo DBfile $DBfile
+echo DBcatalog $DBcatalog
+
+cat template_SiStripO2O.cfg | sed -e "s#insert_DBfile#$DBfile#g" -e "s#insert_DBcatalog#$DBcatalog#g"  -e "s#insert_IOV#${IOV}#" -e "s#insert_appendflag#${append}#g" -e "s@#appendMode_${append}@@g" \
+-e "s#insert_ConfigDbUser#${ConfigDbUser}#" -e "s#insert_ConfigDbPasswd#${ConfigDbPasswd}#" -e "s#insert_ConfigDbPath#${ConfigDbPath}#"	-e "s#insert_ConfigDbPartition#${ConfigDbPartition}#" -e "s#insert_ConfigDbMajorVersion#${ConfigDbMajorVersion}#" -e "s#insert_ConfigDbMinorVersion#${ConfigDbMinorVersion}#" \
+-e "s#insert_doPedNoiseTransfer#$doPedNoiseTransfer#" -e "s#insert_doFedCablingTransfer#$doFedCablingTransfer#" -e "s@${o2otrans}@@"> ${cfg_file}
+
+
+
+echo "cmsRun ${cfg_file}"
+cmsRun ${cfg_file} > ${test_area}/out_o2o_${IOV}
+
+
