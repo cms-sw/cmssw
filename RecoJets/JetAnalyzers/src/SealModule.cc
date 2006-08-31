eeb5b18a40b1bebfddc77495f86583e6aac36016
Robert Harris <sha1-bef59cd0d27f2c81b33bddad0d65ff99d43c90a4@cern.ch>
2006-08-31 15:08:26 +0000

    First commit of Jet Analyzer and supporting files from Anwar Bhatti and Lenny Apanasevich.

--- RecoJets/JetAnalyzers/src/SealModule.cc
+++ RecoJets/JetAnalyzers/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoJets/JetAnalyzers/interface/JetPlotsExample.h"
+#include "RecoJets/JetAnalyzers/interface/JetAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( JetPlotsExample );
+DEFINE_ANOTHER_FWK_MODULE( JetAnalyzer );
