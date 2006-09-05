3c3a641a49a58b341802007793f99e20c5b89a76
Robert Harris <sha1-bef59cd0d27f2c81b33bddad0d65ff99d43c90a4@cern.ch>
2006-09-05 16:00:39 +0000

    Added calorimeter subsystem energies, EM fraction, and response in barrel w and w/o corrections.

--- RecoJets/JetAnalyzers/interface/JetValidation.h
+++ RecoJets/JetAnalyzers/interface/JetValidation.h
@@ -1,6 +1,7 @@
 #ifndef RecoExamples_JetValidation_h
 #define RecoExamples_JetValidation_h
 #include <TH1.h>
+#include <TProfile.h>
 /* \class JetValidation
  *
  * \author Robert Harris
@@ -24,12 +25,24 @@ private:
   double PtHistMax;
   
   // Root objects
+  
+  //Simple Hists
   TH1F ptMC5cal, etaMC5cal, phiMC5cal, m2jMC5cal;
   TH1F ptMC5gen, etaMC5gen, phiMC5gen, m2jMC5gen;
   TH1F ptIC5cal, etaIC5cal, phiIC5cal, m2jIC5cal;
   TH1F ptIC5gen, etaIC5gen, phiIC5gen, m2jIC5gen;
   TH1F ptKT10cal, etaKT10cal, phiKT10cal, m2jKT10cal;
   TH1F ptKT10gen, etaKT10gen, phiKT10gen, m2jKT10gen;
+  
+  //Calorimeter Sub-System Analysis Histograms for IC5 CaloJets only
+  TH1F emEnergyFraction, emEnergyInEB, emEnergyInEE, emEnergyInHF;
+  TH1F hadEnergyInHB, hadEnergyInHE, hadEnergyInHF, hadEnergyInHO;
+
+  //Matched jets Analysis Histograms for MC5 CaloJets only
+  TH1F dR, dRcor;
+  TProfile respVsPt, corRespVsPt;
+
+  //Histo File 
   TFile* m_file;
 
   //Internal parameters
--- RecoJets/JetAnalyzers/src/JetValidation.cc
+++ RecoJets/JetAnalyzers/src/JetValidation.cc
@@ -4,6 +4,7 @@
 // Date:  30 - August - 2006
 // 
 #include "RecoJets/JetAnalyzers/interface/JetValidation.h"
+#include "RecoJets/JetAlgorithms/interface/JetAlgoHelper.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
@@ -71,15 +72,31 @@ void JetValidation::beginJob( const EventSetup & ) {
   phiKT10gen = TH1F("phiKT10gen","#phi of leading GenJets (KT10)",72,-M_PI, M_PI);
   m2jKT10gen = TH1F("m2jKT10gen","Dijet Mass of leading GenJets (KT10)",100,0.0,2*PtHistMax);
 
+  //Calorimeter Sub-System Analysis Histograms for IC5 CaloJets only
+  emEnergyFraction =  TH1F("emEnergyFraction","Leading Jets EM Fraction",100,0.0,1.0);
+  emEnergyInEB = TH1F("emEnergyInEB","Leading Jets emEnergyInEB",100,0.0,2*PtHistMax);
+  emEnergyInEE = TH1F("emEnergyInEE","Leading Jets emEnergyInEE",100,0.0,2*PtHistMax);
+  emEnergyInHF = TH1F("emEnergyInHF","Leading Jets emEnergyInHF",100,0.0,2*PtHistMax);
+  hadEnergyInHB = TH1F("hadEnergyInHB","Leading Jets hadEnergyInHB",100,0.0,2*PtHistMax);
+  hadEnergyInHE = TH1F("hadEnergyInHE","Leading Jets hadEnergyInHE",100,0.0,2*PtHistMax);
+  hadEnergyInHF = TH1F("hadEnergyInHF","Leading Jets hadEnergyInHF",100,0.0,2*PtHistMax);
+  hadEnergyInHO = TH1F("hadEnergyInHO","Leading Jets hadEnergyInHO",100,0.0,0.5*PtHistMax);
+  
+  //Matched jets Analysis Histograms for MC5 CaloJets only
+  dR = TH1F("dR","Leading GenJets dR with matched CaloJet",100,0,0.5);
+  respVsPt = TProfile("respVsPt","CaloJet Response of Leading GenJets in Barrel",100,0.0,PtHistMax/2); 
+  dRcor = TH1F("dRcor","CorJets dR with matched CaloJet",100,0.0,0.01);
+  corRespVsPt = TProfile("corRespVsPt","Corrected CaloJet Response of Leading GenJets in Barrel",100,0.0,PtHistMax/2); 
+
 }
 
 void JetValidation::analyze( const Event& evt, const EventSetup& es ) {
-  math::XYZTLorentzVector p4jet[2];
+  math::XYZTLorentzVector p4jet[2], p4gen[2], p4cal[2], p4cor[2];
   int jetInd;
   Handle<CaloJetCollection> caloJets;
   Handle<GenJetCollection> genJets;
 
-  //Fill Basic Hists
+  //Fill Simple Histos
   
   //MC5 cal
   evt.getByLabel( "midPointCone5CaloJets", caloJets );
@@ -153,6 +170,69 @@ void JetValidation::analyze( const Event& evt, const EventSetup& es ) {
   }
   if(jetInd==2)m2jKT10gen.Fill( (p4jet[0]+p4jet[1]).mass() ); 
 
+ //Calorimeter Sub-System Analysis Histograms for IC5 CaloJets only
+  evt.getByLabel( "iterativeCone5CaloJets", caloJets );
+  jetInd = 0;
+  for( CaloJetCollection::const_iterator cal = caloJets->begin(); cal != caloJets->end() && jetInd<2; ++ cal ) {
+    emEnergyFraction.Fill(cal->emEnergyFraction()); 
+    emEnergyInEB.Fill(cal->emEnergyInEB()); 
+    emEnergyInEE.Fill(cal->emEnergyInEE()); 
+    emEnergyInHF.Fill(cal->emEnergyInHF()); 
+    hadEnergyInHB.Fill(cal->hadEnergyInHB()); 
+    hadEnergyInHE.Fill(cal->hadEnergyInHE()); 
+    hadEnergyInHF.Fill(cal->hadEnergyInHF()); 
+    hadEnergyInHO.Fill(cal->hadEnergyInHO()); 
+    jetInd++;
+  }
+
+  //Matching for MC5 Jets: leading genjets matched to any CaloJet
+  evt.getByLabel( "midPointCone5GenJets", genJets );
+  evt.getByLabel( "midPointCone5CaloJets", caloJets );
+  jetInd = 0;
+  double dRmin[2];
+  for( GenJetCollection::const_iterator gen = genJets->begin(); gen != genJets->end() && jetInd<2; ++ gen ) { //leading genJets
+    p4gen[jetInd] = gen->p4();    //Gen 4-vector
+    dRmin[jetInd]=1000.0;
+    for( CaloJetCollection::const_iterator cal = caloJets->begin(); cal != caloJets->end(); ++ cal ) { //all CaloJets
+       double delR = deltaR( cal->eta(), cal->phi(), gen->eta(), gen->phi() ); 
+       if(delR<dRmin[jetInd]){
+         dRmin[jetInd]=delR;           //delta R of match
+	 p4cal[jetInd] = cal->p4();  //Matched Cal 4-vector
+       }
+    }
+    dR.Fill(dRmin[jetInd]);
+    if(dRmin[jetInd]>0.5)cout << "Warning: dR=" <<dRmin<<", GenPt="<<p4gen[jetInd].Pt()<<", CalPt="<<p4cal[jetInd].Pt()<<endl;
+    jetInd++;    
+  }
+  //Fill Resp vs Pt profile histogram with response of two leading gen jets
+  for( jetInd=0; jetInd<2; ++jetInd ){
+    if(fabs(p4gen[jetInd].eta())<1.){
+      respVsPt.Fill(p4gen[jetInd].Pt(), p4cal[jetInd].Pt()/p4gen[jetInd].Pt() );
+    }
+  }
+
+  //Find the Corrected CaloJets that match the two uncorrected CaloJets
+  evt.getByLabel( "corJetMcone5", caloJets );
+  for( jetInd=0; jetInd<2; ++jetInd ){
+    bool found=kFALSE;
+    for( CaloJetCollection::const_iterator cor = caloJets->begin(); cor != caloJets->end() && !found; ++ cor ) { //all corrected CaloJets
+       double delR = deltaR( cor->eta(), cor->phi(), p4cal[jetInd].eta(),  p4cal[jetInd].phi()); 
+       if(delR<0.01){
+         dRmin[jetInd]=delR;           //delta R of match
+	 p4cor[jetInd] = cor->p4();  //Matched Cal 4-vector
+	 found=kTRUE;
+         dRcor.Fill(dRmin[jetInd]);
+       }
+    }
+    if(!found)cout << "Warning: corrected jet not found. jetInd=" << jetInd << endl;
+  }
+  //Fill Resp vs Pt profile histogram with corrected response of two leading gen jets
+  for( jetInd=0; jetInd<2; ++jetInd ){
+    if(fabs(p4gen[jetInd].eta())<1.){
+     corRespVsPt.Fill(p4gen[jetInd].Pt(), p4cor[jetInd].Pt()/p4gen[jetInd].Pt() ); 
+    }
+  }
+   
 }
 
 void JetValidation::endJob() {
