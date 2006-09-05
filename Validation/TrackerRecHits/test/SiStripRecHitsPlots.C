8ffffdca1f8e03894c191a5c3fb86809d1c85f93
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2006-09-05 08:07:56 +0000

    *** empty log message ***

--- Validation/TrackerRecHits/test/SiStripRecHitsPlots.C
+++ Validation/TrackerRecHits/test/SiStripRecHitsPlots.C
@@ -342,7 +342,7 @@ void SiStripRecHitsPlots()
  SiStrip->cd(6);
  Chi2_matched_layer2tob->Draw();
  
- SiStrip->Print("MatcheResOfTOB.eps");
+ SiStrip->Print("MatchedResOfTOB.eps");
 
 ////////////////////////////////////
 //            TID                 //
