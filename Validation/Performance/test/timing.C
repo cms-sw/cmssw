75c13d69dcf088bd38bef4ae279754a011801d18
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2006-09-07 18:14:44 +0000

    add RPC histo, adjust histo limits, save root histo output file

--- Validation/Performance/test/timing.C
+++ Validation/Performance/test/timing.C
@@ -18,15 +18,18 @@
    
    in.open("timing_minbias.log") ;
    
-   TH1F* Vtx = new TH1F( "VtxSmeared", "VertexGenerator", 100, 0., 1. ) ;
+   TFile* OutFile = new TFile("timing_minbias.root", "RECREATE") ;
+    
+   TH1F* Vtx = new TH1F( "VtxSmeared", "VertexGenerator", 100, 0., 0.1 ) ;
    TH1F* OPr = new TH1F( "SimG4Object", "OscarProducer", 100, 0., 500. ) ;
-   TH1F* Mix = new TH1F( "mix", "MixingModule",  100, 0., 1. ) ;
-   TH1F* Pix = new TH1F( "pixdigi", "SiPixelDigitizer", 100, 0., 1. ) ;
-   TH1F* SiS = new TH1F( "sistripdigi", "SiStripDigitizer", 100, 0., 1. ) ;
-   TH1F* Eca = new TH1F( "ecaldigi", "EcalDigiProducer", 100, 0., 5. ) ;
-   TH1F* Hca = new TH1F( "hcaldigi", "HcalDigiProcucer", 100, 0., 1. ) ;
+   TH1F* Mix = new TH1F( "mix", "MixingModule",  100, 0., 0.5 ) ;
+   TH1F* Pix = new TH1F( "pixdigi", "SiPixelDigitizer", 100, 0., 0.5 ) ;
+   TH1F* SiS = new TH1F( "sistripdigi", "SiStripDigitizer", 100, 0., 2. ) ;
+   TH1F* Eca = new TH1F( "ecaldigi", "EcalDigiProducer", 100, 0., 2. ) ;
+   TH1F* Hca = new TH1F( "hcaldigi", "HcalDigiProcucer", 100, 0., 0.5 ) ;
    TH1F* CSC = new TH1F( "muoncscdigi", "CSCDigiProducer", 100, 0., 0.1 ) ;
-   TH1F* DT  = new TH1F( "muondtdigi",  "DTDigitizer", 100, 0., 0.1 ) ;
+   TH1F* DT  = new TH1F( "muondtdigi",  "DTDigitizer",     100, 0., 0.1 ) ;
+   TH1F* RPC = new TH1F( "muonrpcdigi", "RPCDigiProducer", 100, 0., 0.1 ) ;
    //
    // no PoolOutputModule in this chain 
    //
@@ -36,15 +39,16 @@
       in >> Logo >> Evt >> Run >> ModLabel >> ModName >> Time ;
       if ( !in.good() ) break ;
       if ( Evt == 1 ) continue ; // skip init CPU for now...
-      if ( ModName == "VertexGenerator" ) Vtx->Fill(Time) ;
-      if ( ModName == "OscarProducer" ) OPr->Fill(Time) ;
-      if ( ModName == "MixingModule" )  Mix->Fill(Time) ;
+      if ( ModName == "VertexGenerator" )  Vtx->Fill(Time) ;
+      if ( ModName == "OscarProducer" )    OPr->Fill(Time) ;
+      if ( ModName == "MixingModule" )     Mix->Fill(Time) ;
       if ( ModName == "SiPixelDigitizer" ) Pix->Fill(Time) ;
       if ( ModName == "SiStripDigitizer" ) SiS->Fill(Time) ;
       if ( ModName == "EcalDigiProducer" ) Eca->Fill(Time) ;
       if ( ModName == "HcalDigiProducer" ) Hca->Fill(Time) ;
-      if ( ModName == "CSCDigiProducer" ) CSC->Fill(Time) ;
-      if ( ModName == "DTDigitizer" ) DT->Fill(Time) ;
+      if ( ModName == "CSCDigiProducer" )  CSC->Fill(Time) ;
+      if ( ModName == "DTDigitizer" )      DT->Fill(Time) ;
+      if ( ModName == "RPCDigiProducer" )  RPC->Fill(Time) ;
    }
    
    in.close() ;
@@ -59,6 +63,8 @@
    cout << "[OVAL] : HcalDigiProducer - MEAN CPU = " << Hca->GetMean() << " sec/evt" << endl ;
    cout << "[OVAL] : CSCDigiProducer - MEAN CPU = " << CSC->GetMean() << " sec/evt" << endl ;
    cout << "[OVAL] : DTDigitizer - MEAN CPU = " << DT->GetMean() << " sec/evt" << endl ;
+   cout << "[OVAL] : RPCDigiProducer - MEAN CPU = " << RPC->GetMean() << " sec/evt" << endl ;
+   cout << "[OVAL] : MixingModule - MEAN CPU = " << Mix->GetMean() << " sec/evt" << endl ;
    
    TCanvas* c2 = new TCanvas("c2") ;
    OPr->Draw() ;
@@ -68,22 +74,24 @@
    TCanvas* c3 = new TCanvas( "c3", " ", 900, 900 ) ;
    c3->Divide(2,4) ;
    c3->cd(1) ;
-   Vtx->Draw() ;
-   c3->cd(2) ;
    Pix->Draw() ;
-   c3->cd(3) ;
+   c3->cd(2) ;
    SiS->Draw() ;
-   c3->cd(4) ;
+   c3->cd(3) ;
    Eca->Draw() ;
-   c3->cd(5) ;
+   c3->cd(4) ;
    Hca->Draw() ;
-   c3->cd(6) ;
+   c3->cd(5) ;
    CSC->Draw() ;
-   c3->cd(7) ;
+   c3->cd(6) ;
    DT->Draw() ;
+   c3->cd(7) ;
+   RPC->Draw() ;
    c3->cd(8) ;
    Mix->Draw() ;
 
    c3->SaveAs("Modules_minbias.ps")  ;
+   
+   OutFile->Write() ;
          
 }
