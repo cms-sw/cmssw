cc05e40353d2522ecb98c885e2d17f9263e8c2c2
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-09-08 01:03:56 +0000

    Add an okToRemodify_ field, because ReplaceNodes created interactively aren't subject to the no-changing-twice rule

--- FWCore/ParameterSet/interface/ReplaceNode.h
+++ FWCore/ParameterSet/interface/ReplaceNode.h
@@ -18,12 +18,15 @@ namespace edm {
     {
     public:
       ReplaceNode(const std::string & type, const std::string& name,
-                  NodePtr value, int line=-1)
-      : Node(name, line), type_(type), value_(value) {}
+                  NodePtr value, bool okToRemodify, int line=-1)
+      : Node(name, line), type_(type), value_(value), okToRemodify_(okToRemodify) {}
       /// deep copy
       ReplaceNode(const ReplaceNode & n);
       virtual Node * clone() const { return new ReplaceNode(*this);}
       virtual std::string type() const {return type_;}
+      /// batch jobs don't want to modify the same variable twice.  OK to do
+      /// that interactively, though.
+      bool okToRemodify() const {return okToRemodify_;}
       virtual void print(std::ostream& ost, PrintOptions options) const;
       virtual void accept(Visitor& v) const;
       NodePtr value() const {return value_;}
@@ -35,6 +38,7 @@ namespace edm {
     private:
       std::string type_;
       NodePtr value_;
+      bool okToRemodify_;
     };
 
   }
