40e8bc0e08c88e510db1cb176c90d0c54c19a19d
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-09-08 13:55:36 +0000

    tidy up of cfi, cff and cfg files, moved source files to stubs

---
+++ OnlineDB/SiStripESSources/test/stubs/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "PluginManager/ModuleDef.h"
+DEFINE_SEAL_MODULE();
+
+#include "OnlineDB/SiStripESSources/test/stubs/SiStripPopulateConfigDb.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripPopulateConfigDb)
+
+#include "OnlineDB/SiStripESSources/test/stubs/SiStripAnalyzeFedCabling.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripAnalyzeFedCabling)
