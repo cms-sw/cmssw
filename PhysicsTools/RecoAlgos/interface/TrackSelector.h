703b2b2d5711425853cc495208738d9b2074c340
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-09-08 09:37:06 +0000

    updated for backward compatibility witn 1_1_0 tag

--- PhysicsTools/RecoAlgos/interface/TrackSelector.h
+++ PhysicsTools/RecoAlgos/interface/TrackSelector.h
@@ -7,9 +7,9 @@
  * 
  * \author Luca Lista, INFN
  *
- * \version $Revision: 1.8 $
+ * \version $Revision: 1.9 $
  *
- * $Id: TrackSelector.h,v 1.8 2006/07/25 09:35:48 llista Exp $
+ * $Id: TrackSelector.h,v 1.9 2006/09/01 09:42:24 llista Exp $
  *
  */
 #include "DataFormats/TrackReco/interface/Track.h"
@@ -40,8 +40,8 @@ namespace helper {
 	selTracks_->back().setExtra( TrackExtraRef( rTrackExtras, idx ++ ) );
 	selTrackExtras_->push_back( TrackExtra( trk.outerPosition(), trk.outerMomentum(), trk.outerOk(),
 						trk.innerPosition(), trk.innerMomentum(), trk.innerOk(),
-						trk.outerStateCovariance(), trk.outerDetId(),
-						trk.innerStateCovariance(), trk.innerDetId() ) );
+						trk.extra()->outerStateCovariance(), trk.extra()->outerDetId(),
+						trk.extra()->innerStateCovariance(), trk.extra()->innerDetId() ) );
 	TrackExtra & tx = selTrackExtras_->back();
 	for( trackingRecHit_iterator hit = trk.recHitsBegin(); hit != trk.recHitsEnd(); ++ hit ) {
 	  selHits_->push_back( (*hit)->clone() );
