6eda947260169f2ae73f2bd572d9440b7e047023
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2006-09-12 08:11:07 +0000

    DTTTrigWriter added

--- CalibMuon/DTCalibration/src/SealModule.cc
+++ CalibMuon/DTCalibration/src/SealModule.cc
@@ -3,6 +3,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "CalibMuon/DTCalibration/src/DTTTrigCalibration.h"
+#include "CalibMuon/DTCalibration/src/DTTTrigWriter.h"
 #include "CalibMuon/DTCalibration/src/DTT0Calibration.h"
 #include "CalibMuon/DTCalibration/src/DTVDriftCalibration.h"
 #include "CalibMuon/DTCalibration/src/DTVDriftWriter.h"
@@ -10,6 +11,7 @@
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTTTrigCalibration);
+DEFINE_ANOTHER_FWK_MODULE(DTTTrigWriter);
 DEFINE_ANOTHER_FWK_MODULE(DTT0Calibration);
 DEFINE_ANOTHER_FWK_MODULE(DTVDriftCalibration);
 DEFINE_ANOTHER_FWK_MODULE(DTVDriftWriter);
