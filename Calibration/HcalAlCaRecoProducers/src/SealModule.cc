ec5b0fc12785bd4e4f242e9b54fdc8d53ac69a56
Michal Szleper <sha1-8dc5ee44d1f0d27d89108086e7cf38e4a70bd448@cern.ch>
2006-09-12 21:56:42 +0000

    Add AlCaDiJetsProducer

--- Calibration/HcalAlCaRecoProducers/src/SealModule.cc
+++ Calibration/HcalAlCaRecoProducers/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "Calibration/HcalAlCaRecoProducers/interface/AlCaEcalHcalReadoutsProducer.h"
 #include "Calibration/HcalAlCaRecoProducers/interface/AlCaIsoTracksProducer.h"
+#include "Calibration/HcalAlCaRecoProducers/interface/AlCaEcalHcalReadoutsProducer.h"
+#include "Calibration/HcalAlCaRecoProducers/interface/AlCaDiJetsProducer.h"
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(AlCaIsoTracksProducer)
-DEFINE_ANOTHER_FWK_MODULE(AlCaEcalHcalReadoutsProducer)
+DEFINE_ANOTHER_FWK_MODULE(AlCaIsoTracksProducer);
+DEFINE_ANOTHER_FWK_MODULE(AlCaEcalHcalReadoutsProducer);
+DEFINE_ANOTHER_FWK_MODULE(AlCaDiJetsProducer)
