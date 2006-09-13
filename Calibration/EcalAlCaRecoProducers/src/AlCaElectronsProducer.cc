376e56f5adde78cfeb19eba037ab5bc1ea7a22e8
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2006-09-13 15:18:41 +0000

    bugfix

--- Calibration/EcalAlCaRecoProducers/src/AlCaElectronsProducer.cc
+++ Calibration/EcalAlCaRecoProducers/src/AlCaElectronsProducer.cc
@@ -2,26 +2,48 @@
 #include "DataFormats/EgammaCandidates/interface/SiStripElectron.h"
 #include "DataFormats/EgammaCandidates/interface/Electron.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EgammaReco/interface/BasicCluster.h"
 
 AlCaElectronsProducer::AlCaElectronsProducer(const edm::ParameterSet& iConfig)
 {
-
-pixelMatchElectronProducer_ = iConfig.getParameter< std::string > ("pixelMatchElectronProducer");
+//input collections
 siStripElectronProducer_ = iConfig.getParameter< std::string > ("siStripElectronProducer");
 
-pixelMatchElectronCollection_ = iConfig.getParameter<std::string>("pixelMatchElectronCollection");
 siStripElectronCollection_ = iConfig.getParameter<std::string>("siStripElectronCollection");
 
-alcaPixelMatchElectronCollection_ = iConfig.getParameter<std::string>("alcaPixelMatchElectronCollection");
+BarrelHitsCollection_ = iConfig.getParameter< std::string > ("barrelHitCollection");
+
+ecalHitsProducer_ = iConfig.getParameter< std::string > ("ecalRecHitsProducer");
+
+
+//output collections
+
+
 alcaSiStripElectronCollection_ = iConfig.getParameter<std::string>("alcaSiStripElectronCollection");
+
+alcaBarrelHitsCollection_ = iConfig.getParameter<std::string>("alcaBarrelHitCollection");
+
+alcaHybridSuperClusterCollection_ = iConfig.getParameter<std::string>("alcaHybridSuperClusterCollection");
+
+//alcaCorrectedHybridSuperClusterCollection_ = iConfig.getParameter<std::string>("alcaCorrectedHybridSuperClusterCollection");
 
 ptCut_ = iConfig.getParameter< double > ("ptCut");
 
 
    //register your products
+
    produces<reco::SiStripElectronCollection>(alcaSiStripElectronCollection_);
-   produces<reco::ElectronCollection>(alcaPixelMatchElectronCollection_);
+
+   produces< EBRecHitCollection >(alcaBarrelHitsCollection_);
+
+   produces<reco::SuperClusterCollection>(alcaHybridSuperClusterCollection_);
+
+//   produces<reco::SuperClusterCollection>(alcaCorrectedHybridSuperClusterCollection_);
+
+
 
 }
 
@@ -40,49 +62,115 @@ AlCaElectronsProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
    using namespace edm;
    using namespace std;
 
-
-   Handle<reco::SiStripElectronCollection> pSiStripElectrons;
-   Handle<reco::ElectronCollection> pPixelMatchElectrons;
-
-//   std::cout << "pixelMatchElectronProducer: "<< pixelMatchElectronProducer_<<std::endl;
-//   std::cout << "pixelMatchElectronCollection: "<< pixelMatchElectronCollection_<<std::endl;
-//   std::cout << "siStripElectronProducer: "<< siStripElectronProducer_<<std::endl;
-//   std::cout << "siStripElectronCollection: "<< siStripElectronCollection_<<std::endl;
-
-
-   try {
-     iEvent.getByLabel(pixelMatchElectronProducer_, pixelMatchElectronCollection_, pPixelMatchElectrons);
+  // Get siStripElectrons
+  Handle<reco::SiStripElectronCollection> pSiStripElectrons;
+  try {
      iEvent.getByLabel(siStripElectronProducer_, siStripElectronCollection_, pSiStripElectrons);
    } catch ( std::exception& ex ) {
      LogDebug("") << "AlCaElectronsProducer: Error! can't get product!" << std::endl;
-//     std::cout <<  "AlCaElectronsProducer: Error! can't get product!" << std::endl;
    }
+  const reco::SiStripElectronCollection* siStripElectronCollection = pSiStripElectrons.product();
+
+// get RecHits
+  Handle<EBRecHitCollection> barrelRecHitsHandle;
+  try {
+    iEvent.getByLabel(ecalHitsProducer_,BarrelHitsCollection_,barrelRecHitsHandle);
+  } catch ( std::exception& ex ) {
+    std::cout << "AlCaElectronsProducer: Error! can't get product!" << std::endl;
+  }
+  const EBRecHitCollection* barrelHitsCollection = barrelRecHitsHandle.product();
+
+
 
   //Create empty output collections
 
     std::auto_ptr<reco::SiStripElectronCollection> miniSiStripElectronCollection(new reco::SiStripElectronCollection);
-    std::auto_ptr<reco::ElectronCollection> miniPixelMatchElectronCollection(new reco::ElectronCollection);
 
-  //Select interesting electrons
+    std::auto_ptr< EBRecHitCollection > miniEBRecHitCollection( new EBRecHitCollection );
+
+    std::auto_ptr< reco::SuperClusterCollection > miniSuperClusterCollection( new reco::SuperClusterCollection );
+
+
+//  loop on SiStrip Electrons
+
     reco::SiStripElectronCollection::const_iterator siStripEleIt;
-
-    for (siStripEleIt=pSiStripElectrons->begin(); siStripEleIt!=pSiStripElectrons->end(); siStripEleIt++) {
+    int ii=0;
+ 
+    for (siStripEleIt=siStripElectronCollection->begin(); siStripEleIt!=siStripElectronCollection->end(); siStripEleIt++) {
       if (siStripEleIt->pt() >= ptCut_) {
-        miniSiStripElectronCollection->push_back(*siStripEleIt);
-      }
+
+        ii++;         
+        
+	 const reco::SuperCluster& sc = *(siStripEleIt->superCluster()) ;
+	              
+	       int yy=0;
+               double mySCenergy=0.0;
+    	       double currEnergy =0.;
+ 	       EBDetId maxHit;
+    	       vector<EBDetId> scXtals;
+	       scXtals.clear();
+               const std::vector<DetId> & v1 = sc.getHitsByDetId();
+
+               for( std::vector<DetId>::const_iterator idsIt = v1.begin(); idsIt != v1.end(); ++idsIt) {
+		    yy++;
+                    
+                    if((*idsIt).subdetId()!=1 || (*idsIt).det()!=3) continue;
+		    
+	   	    if((barrelHitsCollection->find(*idsIt))->energy() > currEnergy) {
+	   		currEnergy=(barrelHitsCollection->find(*idsIt))->energy();
+	   		maxHit=*idsIt;
+	   	    }
+ 		   
+		    miniEBRecHitCollection->push_back(*(barrelHitsCollection->find(*idsIt)));
+                    scXtals.push_back(*idsIt);
+                }
+ 
+
+    
+	       for (unsigned int icry=0;icry<25;icry++)
+	         {
+ 		       unsigned int row = icry / 5;
+ 		       unsigned int column= icry %5;
+		       int curr_eta=maxHit.ieta()+column-2;
+		       int curr_phi=maxHit.iphi()+row-2;
+		       
+                       if (curr_eta * maxHit.ieta() <= 0) {if (maxHit.ieta() > 0) curr_eta--; else curr_eta++; }  // JUMP over 0
+                       if (curr_phi < 1) curr_phi += 360;
+                       if (curr_phi > 360) curr_phi -= 360;
+
+		       if(curr_eta > 85) continue;
+		       if(curr_eta < -85) continue;
+		       if(curr_phi > 360) continue;
+		       if(curr_phi < 0) continue;
+
+			         EBDetId det = EBDetId(curr_eta,curr_phi,EBDetId::ETAPHIMODE);
+
+ 			         std::vector<EBDetId>::const_iterator usedIds;
+				 
+				 bool HitAlreadyUsed=false;
+ 				    for(usedIds=scXtals.begin(); usedIds!=scXtals.end(); usedIds++)
+				    {
+ 				       if(*usedIds==det){
+                                           HitAlreadyUsed=true;
+					}
+				    }
+				  
+				 if(!HitAlreadyUsed){
+				      miniEBRecHitCollection->push_back(*(barrelHitsCollection->find(det)));
+				  }
+				       
+	         }
+	  
+
+         miniSuperClusterCollection->push_back(sc);
+         miniSiStripElectronCollection->push_back(*siStripEleIt);
+       }
     }
 
-    reco::ElectronCollection::const_iterator pixelMatchEleIt;
-
-    for (pixelMatchEleIt=pPixelMatchElectrons->begin(); pixelMatchEleIt!=pPixelMatchElectrons->end(); pixelMatchEleIt++) {
-      if (pixelMatchEleIt->pt() >= ptCut_) {
-        miniPixelMatchElectronCollection->push_back(*pixelMatchEleIt);
-      }
-    }
 
   //Put selected information in the event
-  iEvent.put( miniPixelMatchElectronCollection,alcaPixelMatchElectronCollection_ );
-  iEvent.put( miniSiStripElectronCollection,alcaSiStripElectronCollection_ );
-  
+   iEvent.put( miniEBRecHitCollection,alcaBarrelHitsCollection_ );
+   iEvent.put( miniSuperClusterCollection,alcaHybridSuperClusterCollection_ );
+   iEvent.put( miniSiStripElectronCollection,alcaSiStripElectronCollection_ );
  
 }
