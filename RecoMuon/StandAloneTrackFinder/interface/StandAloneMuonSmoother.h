e7df2ba25b62b510f0c3f2dfe034226b6052239e
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-09-13 10:42:23 +0000

    little specialization

--- RecoMuon/StandAloneTrackFinder/interface/StandAloneMuonSmoother.h
+++ RecoMuon/StandAloneTrackFinder/interface/StandAloneMuonSmoother.h
@@ -4,32 +4,72 @@
 /** \class StandAloneMuonSmoother
  *  The outward-inward fitter (starts from StandAloneMuonBackwardFilter innermost state).
  *
- *  $Date: 2006/08/30 12:56:18 $
- *  $Revision: 1.6 $
+ *  $Date: 2006/08/31 18:28:04 $
+ *  $Revision: 1.7 $
  *  \author R. Bellan - INFN Torino <riccardo.bellan@cern.ch>
  */
 
+// #include "TrackingTools/TrackFitters/interface/KFTrajectorySmoother.h"
+#include "TrackingTools/PatternTools/interface/Trajectory.h"
+
 class MuonServiceProxy;
+class TrajectoryStateUpdator;
+class MeasurementEstimator;
+class Propagator;
 
-namespace edm {class ParameterSet; class EventSetup; class Event;}
+#include <string>
 
-class StandAloneMuonSmoother {
-public:
+namespace edm {class ParameterSet; class Event;}
+
+//class StandAloneMuonSmoother: public KFTrajectorySmoother {
+class StandAloneMuonSmoother{
+
+  typedef std::pair<bool,Trajectory> SmoothingResult;
+
+ public:
   /// Constructor
   StandAloneMuonSmoother(const edm::ParameterSet& par, const MuonServiceProxy* service);
 
   /// Destructor
-  virtual ~StandAloneMuonSmoother(){};
-
-  // Operations
+  virtual ~StandAloneMuonSmoother();
   
-  /// Pass the Event to the algo at each event
-  virtual void setEvent(const edm::Event& event);
+  // Operations 
 
+  /// Smoothes the trajectories
+  SmoothingResult smooth(const Trajectory&);
+  
+  /// return the KFUpdator
+  TrajectoryStateUpdator *updator() const {return theUpdator;}
 
-protected:
+  /// access at the propagator
+  const Propagator *propagator() const;
 
-private:
+  /// access at the estimator
+  MeasurementEstimator *estimator() const {return theEstimator;}
+    
+ protected:
+  
+ private:
+  std::string thePropagatorName;
+
+  /// The max allowed chi2 to accept a rechit in the fit
+  double theMaxChi2;
+
+  /// The errors of the trajectory state are multiplied by nSigma 
+  /// to define acceptance of BoundPlane and maximalLocalDisplacement
+  double theNSigma;
+
+  /// The estimator: makes the decision wheter a measure is good or not
+  /// it isn't used by the updator which does the real fit. In fact, in principle,
+  /// a looser request onto the measure set can be requested 
+  /// (w.r.t. the request on the accept/reject measure in the fit)
+  MeasurementEstimator *theEstimator;
+
+  double theErrorRescaling;
+
+  TrajectoryStateUpdator *theUpdator;
+
+  const MuonServiceProxy* theService;
 
 };
 #endif
--- RecoMuon/StandAloneTrackFinder/src/StandAloneMuonSmoother.cc
+++ RecoMuon/StandAloneTrackFinder/src/StandAloneMuonSmoother.cc
@@ -1,18 +1,65 @@
 /** \class StandAloneMuonSmoother
  *  The outward-inward fitter (starts from StandAloneMuonBackwardFilter innermost state).
  *
- *  $Date: 2006/08/30 12:56:19 $
- *  $Revision: 1.4 $
+ *  $Date: 2006/08/31 18:28:04 $
+ *  $Revision: 1.5 $
  *  \author R. Bellan - INFN Torino <riccardo.bellan@cern.ch>
  */
 
-// #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "RecoMuon/StandAloneTrackFinder/interface/StandAloneMuonSmoother.h"
-#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
+
+#include "TrackingTools/KalmanUpdators/interface/Chi2MeasurementEstimator.h"
+#include "TrackingTools/KalmanUpdators/interface/KFUpdator.h"
+#include "TrackingTools/PatternTools/interface/TrajectoryMeasurement.h"
+
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/TrackFitters/interface/TrajectoryStateCombiner.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 using namespace edm;
+using namespace std;
 
-StandAloneMuonSmoother::StandAloneMuonSmoother(const ParameterSet& par, const MuonServiceProxy* service){}
 
-void StandAloneMuonSmoother::setEvent(const Event& event){}
+StandAloneMuonSmoother::StandAloneMuonSmoother(const ParameterSet& par, 
+					       const MuonServiceProxy* service):theService(service){
+
+  // The max allowed chi2 to accept a rechit in the fit
+  theMaxChi2 = par.getParameter<double>("MaxChi2");
+  
+  // The errors of the trajectory state are multiplied by nSigma 
+  // to define acceptance of BoundPlane and maximalLocalDisplacement
+  theNSigma = par.getParameter<double>("NumberOfSigma"); // default = 3.
+  
+  // The estimator: makes the decision wheter a measure is good or not
+  // it isn't used by the updator which does the real fit. In fact, in principle,
+  // a looser request onto the measure set can be requested 
+  // (w.r.t. the request on the accept/reject measure in the fit)
+  theEstimator = new Chi2MeasurementEstimator(theMaxChi2,theNSigma);
+  
+  theErrorRescaling = par.getParameter<double>("ErrorRescalingFactor");
+  
+  thePropagatorName = par.getParameter<string>("Propagator");
+  
+  theUpdator = new KFUpdator();
+}
+
+StandAloneMuonSmoother::~StandAloneMuonSmoother(){
+  if (theEstimator) delete theEstimator;
+  if (theUpdator) delete theUpdator;
+}
+
+const Propagator* StandAloneMuonSmoother::propagator() const{ 
+  return &*theService->propagator(thePropagatorName); 
+}
+
+StandAloneMuonSmoother::SmoothingResult StandAloneMuonSmoother::smooth(const Trajectory& inputTrajectory){
+
+  const string metname = "Muon|RecoMuon|StandAloneMuonSmoother";
+  
+  Trajectory smoothed(inputTrajectory.seed());
+
+  return SmoothingResult(true,smoothed); 
+}
