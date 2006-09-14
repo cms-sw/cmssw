43b5bcd2d0dccd0dac1c1084fc8790737488e0d8
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-09-14 16:59:05 +0000

    Make a record and allow the HcalTPGCoder to be put into it.

---
+++ CalibFormats/HcalObjects/interface/HcalTPGRecord.h
@@ -0,0 +1,26 @@
+#ifndef HcalObjects_HcalTPGRecord_h
+#define HcalObjects_HcalTPGRecord_h
+// -*- C++ -*-
+//
+// Package:     HcalObjects
+// Class  :     HcalTPGRecord
+// 
+/**\class HcalTPGRecord HcalTPGRecord.h CalibFormats/HcalObjects/interface/HcalTPGRecord.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Thu Sep 14 11:54:26 CDT 2006
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class HcalTPGRecord : public edm::eventsetup::EventSetupRecordImplementation<HcalTPGRecord> {};
+
+#endif
---
+++ CalibFormats/HcalObjects/src/ES_HcalTPGCoder.cc
@@ -0,0 +1,4 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "CalibFormats/HcalObjects/interface/HcalTPGCoder.h"
+
+EVENTSETUP_DATA_REG(HcalTPGCoder)
