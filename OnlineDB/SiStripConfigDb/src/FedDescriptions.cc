f86f7f0f64084b1efdb5dacea2ff43bfdcc74f74
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-09-14 07:54:10 +0000

    temp fix? fed factory needs -1 to get 'latest' version

--- OnlineDB/SiStripConfigDb/src/FedDescriptions.cc
+++ OnlineDB/SiStripConfigDb/src/FedDescriptions.cc
@@ -1,5 +1,5 @@
-// Last commit: $Id: FedDescriptions.cc,v 1.2 2006/07/26 11:27:19 bainbrid Exp $
-// Latest tag:  $Name: V00-01-02 $
+// Last commit: $Id: FedDescriptions.cc,v 1.3 2006/08/31 19:49:41 bainbrid Exp $
+// Latest tag:  $Name:  $
 // Location:    $Source: /cvs_server/repositories/CMSSW/CMSSW/OnlineDB/SiStripConfigDb/src/FedDescriptions.cc,v $
 
 #include "OnlineDB/SiStripConfigDb/interface/SiStripConfigDb.h"
@@ -20,8 +20,7 @@ const SiStripConfigDb::FedDescriptions& SiStripConfigDb::getFedDescriptions() {
   try {
     deviceFactory(__FUNCTION__)->setUsingStrips( usingStrips_ );
     feds_ = *( deviceFactory(__FUNCTION__)->getFed9UDescriptions( partition_.name_, 
-								  partition_.major_, 
-								  partition_.minor_ ) );
+								  -1, -1 ) ); //partition_.major_, partition_.minor_ ) );
     resetFeds_ = false;
   }
   catch (... ) {
