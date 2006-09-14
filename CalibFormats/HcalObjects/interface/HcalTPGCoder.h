6bb465a4a29562c6473eb6911846ff25533b3f60
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2006-09-14 20:01:37 +0000

    Make setup functions const as required by ES

--- CalibFormats/HcalObjects/interface/HcalTPGCoder.h
+++ CalibFormats/HcalObjects/interface/HcalTPGCoder.h
@@ -5,6 +5,11 @@
 #include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HFDataFrame.h"
 
+// forward declaration of EventSetup is all that is needed here
+namespace edm {
+  class EventSetup; 
+}
+
 /** \class HcalTPGCoder
   *  
   * Converts ADC to linear E or ET for use in the TPG path
@@ -12,12 +17,14 @@
   * Note : whether the coder produces E or ET is determined by the specific
   * implementation of the coder.
   *
-  * $Date: 2006/03/30 16:49:18 $
-  * $Revision: 1.2 $
+  * $Date: 2006/09/14 16:58:37 $
+  * $Revision: 1.3 $
   * \author J. Mans - Minnesota
   */
 class HcalTPGCoder {
 public:
+  virtual void getConditions(const edm::EventSetup& es) const { }
+  virtual void releaseConditions() const { }
   virtual void adc2Linear(const HBHEDataFrame& df, IntegerCaloSamples& ics) const = 0;
   virtual void adc2Linear(const HFDataFrame& df, IntegerCaloSamples& ics) const = 0;
 };
