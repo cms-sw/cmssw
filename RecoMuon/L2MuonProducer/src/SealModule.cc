d0922bf105439b68912012f7a9e309ce6e6dbf2e
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-09-14 14:13:01 +0000

    L2MuonCandidateProducer module

--- RecoMuon/L2MuonProducer/src/SealModule.cc
+++ RecoMuon/L2MuonProducer/src/SealModule.cc
@@ -3,7 +3,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoMuon/L2MuonProducer/src/L2MuonProducer.h"
+#include "RecoMuon/L2MuonProducer/src/L2MuonCandidateProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L2MuonProducer)
-
+DEFINE_ANOTHER_FWK_MODULE(L2MuonCandidateProducer)
