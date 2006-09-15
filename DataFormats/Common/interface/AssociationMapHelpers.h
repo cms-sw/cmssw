d99020d990b35b9fd01c13402d15f48cb78d03a9
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-09-15 07:30:50 +0000

    added missing file

---
+++ DataFormats/Common/interface/AssociationMapHelpers.h
@@ -0,0 +1,43 @@
+#ifndef Common_AssociationMapKeyVal_h
+#define Common_AssociationMapKeyVal_h
+/*
+ *
+ * helper classes for AssociationMap
+ *
+ * \author Luca Lista, INFN
+ *
+ * $Id: AssociationMap.h,v 1.22 2006/08/28 10:03:47 llista Exp $
+ *
+ */
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace edm {
+  namespace helpers {
+    template<typename K, typename V>
+    struct KeyVal {
+      typedef K key_type;
+      typedef V value_type;
+      KeyVal() { }
+      KeyVal( const K & k, const V & v ) : key( k ), val( v ) { }
+      K key;
+      V val;
+    };
+    
+    template<typename K>
+    struct Key {
+      typedef K key_type;
+      Key() { }
+      Key( const K & k ) : key( k ) { }
+      K key;
+    };
+    
+    /// throw if r hasn't the same id as rp
+    template<typename RP, typename R>
+    void checkRef( const RP & rp, const R & r ) {
+      if ( rp.id() != r.id() )
+	throw edm::Exception( edm::errors::InvalidReference, "invalid reference" );
+    }
+  }
+}
+
+#endif
