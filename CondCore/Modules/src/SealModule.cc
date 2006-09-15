dd2376aafba2f2bce3aa8b245f1dbabcdb4e63b7
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-09-15 11:44:03 +0000

    add EMptyIOVSource

--- CondCore/Modules/src/SealModule.cc
+++ CondCore/Modules/src/SealModule.cc
@@ -3,6 +3,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "CondCore/Modules/src/IOVSource.h"
+#include "CondCore/Modules/src/EmptyIOVSource.h"
 using cond::IOVSource;
+using cond::EmptyIOVSource;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(IOVSource)
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(EmptyIOVSource)
