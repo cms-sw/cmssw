4ebb0488210893dff115760c3d3b164c59e5b126
Pascal Vanlaer <sha1-589d8756fa197c797cb1cfaa739416398ec997b9@cern.ch>
2006-09-15 15:33:23 +0000

    cleanup to restart from scratch

--- Validation/RecoVertex/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "Validation/RecoVertex/interface/TrackParameterAnalyzer.h"
-#include "Validation/RecoVertex/interface/PrimaryVertexAnalyzer.h"
-
-DEFINE_SEAL_MODULE();
-
-DEFINE_ANOTHER_FWK_MODULE(PrimaryVertexAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(TrackParameterAnalyzer);
