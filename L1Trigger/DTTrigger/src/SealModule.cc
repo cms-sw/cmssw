100afdf7bc8472f55044335fef9b6211e821bbef
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2006-09-18 10:46:13 +0000

    New Alalyzers introduced and headers moved

--- L1Trigger/DTTrigger/src/SealModule.cc
+++ L1Trigger/DTTrigger/src/SealModule.cc
@@ -1,7 +1,10 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "L1Trigger/DTTrigger/test/DTTrigTest.cc"
-#include "L1Trigger/DTTrigger/src/DTTrigProd.cc"
+#include "L1Trigger/DTTrigger/interface/DTTrigTest.h"
+#include "L1Trigger/DTTrigger/interface/DTTrigProd.h"
+#include "L1Trigger/DTTrigger/interface/DTTrigFineSync.h"
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTTrigTest);
+DEFINE_ANOTHER_FWK_MODULE(DTTrigFineSync);
 DEFINE_ANOTHER_FWK_MODULE(DTTrigProd);
