191f1c4b54a2a72cddd57b145312259820fa6f4b
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2006-09-19 09:11:49 +0000

    updated

--- Calibration/EcalCalibAlgos/src/SealModule.cc
+++ Calibration/EcalCalibAlgos/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Calibration/EcalCalibAlgos/interface/miscalibExample.h"
+#include "Calibration/EcalCalibAlgos/interface/ElectronCalibration.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(miscalibExample);
+DEFINE_ANOTHER_FWK_MODULE(ElectronCalibration);
