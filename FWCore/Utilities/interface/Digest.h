2cdf3979f7d0ddc7e5499823ae7ad232fe3208b0
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-09-19 14:53:46 +0000

    Prepare Digest and MD5Result classes, to allow removal of Seal's MD5 digest

--- FWCore/Utilities/interface/Digest.h
+++ FWCore/Utilities/interface/Digest.h
@@ -1,19 +1,53 @@
 #ifndef FWCORE_FWUTILITIES_DIGEST_INC
 #define FWCORE_FWUTILITIES_DIGEST_INC
 
-#include <string>
 #include "md5.h"
+
+#include <ostream>
+#include <string>
 
 
 namespace cms
 {
+
   struct MD5Result
   {
+    // The default-constructed MD5Result is invalid; all others are
+    // valid. The MD5 digest of the empty string is the value of the
+    // default-constructed MD5Result.
+    MD5Result();
+
+    // This is the MD5 digest.
     unsigned char bytes[16];
+
+    // Convert the digest to a printable string (the 'hexdigest')
     std::string toString() const;
+
+    // The MD5 digest (not hexdigest) in string form
+    // 'std::basic_string<char>', rather than
+    // 'unsigned char [16]'
+    std::string compactForm() const;
+
+    bool isValid() const;
   };
 
+  bool operator==(MD5Result const& a, MD5Result const& b);
+  bool operator< (MD5Result const& a, MD5Result const& b);
 
+  inline bool operator!=(MD5Result const& a, MD5Result const& b)
+  {
+    return !(a==b);
+  }
+
+  inline
+  std::ostream& operator<< (std::ostream& os, MD5Result const& r)
+  {
+    os << r.toString();
+    return os;
+  }
+
+  // Digest creates an MD5 digest of the given string. The digest can
+  // be updated by using 'append'.
   class Digest
   {
   public:
--- FWCore/Utilities/src/Digest.cc
+++ FWCore/Utilities/src/Digest.cc
@@ -1,18 +1,73 @@
-#include "FWCore/Utilities/interface/Digest.h"
+#include <algorithm>
 #include <iomanip>
 #include <sstream>
 #include <string>
 
+#include "FWCore/Utilities/interface/Digest.h"
+
 namespace cms
 {
+  namespace
+  {
+    MD5Result invalidResult = Digest().digest();
+  }
+
+  //--------------------------------------------------------------------
+  //
+  // MD5Result and associated free functions
+  //
+
+  MD5Result::MD5Result() 
+  {
+    std::copy(invalidResult.bytes,
+	      invalidResult.bytes + sizeof(invalidResult.bytes),
+	      bytes);
+  }
+
 
   std::string MD5Result::toString() const
   {
     std::ostringstream os;
     os << std::hex << std::setfill('0');
-    for (int i = 0 ; i < 16 ; ++i) os << std::setw(2) << static_cast<int>(bytes[i]);
+    for (size_t i = 0 ; i < sizeof(bytes) ; ++i) 
+      os << std::setw(2) << static_cast<int>(bytes[i]);
     return os.str();
   }
+
+  std::string MD5Result::compactForm() const
+  {
+    // This is somewhat dangerous, because the conversion of 'unsigned
+    // char' to 'char' may be undefined if 'char' is a signed type
+    // (4.7p3 in the Standard).
+    const char* p = reinterpret_cast<const char*>(&bytes[0]);
+    return std::string(p, p+sizeof(bytes));
+  }
+
+  bool MD5Result::isValid() const
+  {
+    return (*this != invalidResult);
+  }
+
+  bool operator==(MD5Result const& a, MD5Result const& b)
+  {
+    return std::equal(a.bytes, a.bytes+sizeof(a.bytes), b.bytes);
+  }
+
+
+  bool operator< (MD5Result const& a, MD5Result const& b)
+  {
+    return std::lexicographical_compare(a.bytes, 
+					a.bytes+sizeof(a.bytes), 
+					b.bytes,
+					b.bytes+sizeof(b.bytes));
+  }
+
+
+
+  //--------------------------------------------------------------------
+  //
+  // Digest
+  //
 
   Digest::Digest() :
     state_()
