fb85d68bfed5d3df524b9ddf1de411ccbb8d942f
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-09-20 16:42:13 +0000

    Make InvalidHash a function, rather than data member, and fix printing function

--- DataFormats/Common/interface/Hash.h
+++ DataFormats/Common/interface/Hash.h
@@ -1,7 +1,9 @@
 #ifndef Common_Hash_h
 #define Common_Hash_h
 
+#include <algorithm>
 #include <string>
+
 #include "FWCore/Utilities/interface/Digest.h"
 #include "FWCore/Utilities/interface/EDMException.h"
 
@@ -9,42 +11,14 @@
   
 Hash:
 
-$Id: Hash.h,v 1.5 2006/09/11 21:28:34 paterno Exp $
+$Id: Hash.h,v 1.6 2006/09/20 16:07:08 paterno Exp $
 ----------------------------------------------------------------------*/
 namespace edm {
 
-
-  //   namespace detail {
-  //     inline
-  //     std::string::value_type
-  //     unhexify(std::string::value_type hexed)
-  //     {
-  //       switch (hexed) {
-  //       case '0': case '1': case '2': case '3': case '4':
-  //       case '5': case '6': case '7': case '8': case '9':
-  // 	return hexed - '0';
-  //       case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
-  // 	return hexed - 'a' + 10;
-  //       case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-  // 	return hexed - 'A' + 10;
-  //       default:
-  // 	throw edm::Exception(edm::errors::LogicError)
-  // 	  << "Non-hex character in Hash "
-  // 	  << "Please report this to the core framework developers";
-  //       }
-  //       // We never get here.
-  //       return '\0';
-  //     }
-  //   } // namespace detail
-
-  namespace
+  namespace detail
   {
     // This string is the 16-byte, non-printable version.
-    std::string const& InvalidHash()
-    {
-      static const std::string invalid = cms::MD5Result().compactForm();
-      return invalid;
-    }
+    std::string const& InvalidHash();
   }
 
   template <int I>
@@ -71,7 +45,7 @@ namespace edm {
     std::ostream& print(std::ostream& os) const;
     void swap(Hash<I>& other);
 
-    // Return the 16-byte (non-printable) string form
+    // Return the 16-byte (non-printable) string form.
     value_type compactForm() const;
     
   private:
@@ -116,7 +90,7 @@ namespace edm {
   bool 
   Hash<I>::isValid() const
   {
-    return hash_ != InvalidHash();
+    return hash_ != edm::detail::InvalidHash();
   }
 
   template <int I>
@@ -156,7 +130,10 @@ namespace edm {
   std::ostream& 
   Hash<I>::print(std::ostream& os) const
   {
-    return os << hash_;
+    cms::MD5Result temp;
+    std::copy(hash_.begin(), hash_.end(), temp.bytes);
+    os << temp.toString();
+    return os;
   }
 
   template <int I>
@@ -173,21 +150,6 @@ namespace edm {
   Hash<I>::compactForm() const
   {
     return hash_;
-    //     value_type v;
-    //     if (! hash_.empty() )
-    //       {
-    // 	value_type::size_type nloop = hash_.size()/2;
-    // 	v.resize(nloop);
-    // 	value_type::const_iterator it = hash_.begin();
-    // 	for (value_type::size_type i = 0; i != nloop; ++i )
-    // 	  {
-    // 	    // first nybble
-    // 	    v[i] = ( detail::unhexify(*it++) << 4 );
-    // 	    // second nybble
-    // 	    v[i] += ( detail::unhexify(*it++) );
-    // 	  }
-    //       }
-    //     return v;
   }
 
   template <int I>
@@ -212,24 +174,7 @@ namespace edm {
     switch (hash_.size() ) {
     case 0:
       {
-	hash_ = InvalidHash();
-	// 	  hash_.resize(16);
-	// 	  hash_[0] = 0xd4;
-	// 	  hash_[1] = 0x1d;
-	// 	  hash_[2] = 0x8c;
-	// 	  hash_[3] = 0xd9;
-	// 	  hash_[4] = 0x8f;
-	// 	  hash_[5] = 0x00;
-	// 	  hash_[6] = 0xb2;
-	// 	  hash_[7] = 0x04;
-	// 	  hash_[8] = 0xe9;
-	// 	  hash_[9] = 0x80;
-	// 	  hash_[10] = 0x09;
-	// 	  hash_[11] = 0x98;
-	// 	  hash_[12] = 0xec;
-	// 	  hash_[13] = 0xf8;
-	// 	  hash_[14] = 0x42;
-	// 	  hash_[15] = 0x7e;
+	hash_ = edm::detail::InvalidHash();
 	break;
       }	
     case 16: 
@@ -242,14 +187,6 @@ namespace edm {
 	cms::MD5Result temp;
 	temp.fromHexifiedString(hash_);
 	hash_ = temp.compactForm();
-	// 	  std::string unhexified(16, ' ');
-	// 	  std::string::const_iterator it = hash_.begin();
-	// 	  for (std::string::size_type i = 0; i != hash_.size(); ++i)
-	// 	    {
-	// 	      unhexified[i]  = (detail::unhexify(*it++) << 4);
-	// 	      unhexified[i] +=  detail::unhexify(*it++);
-	// 	    }
-	// 	  hash_.swap(unhexified);
 	break;
       }
 	  
