dbaf2bd9fe915675d953719d6b6b776ce05ac3e0
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-09-20 15:49:36 +0000

    added more selectors

---
+++ PhysicsTools/Utilities/interface/OrSelector.h
@@ -0,0 +1,27 @@
+#ifndef RecoAlgos_OrSelector_h
+#define RecoAlgos_OrSelector_h
+/* \class OrSelector
+ *
+ * \author Luca Lista, INFN
+ *
+ * $Id: PtMinSelector.h,v 1.2 2006/07/25 17:21:31 llista Exp $
+ */
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+template<typename S1, typename S2>
+struct OrSelector {
+  BOOST_STATIC_ASSERT( ( boost::is_same<typename S1::value_type, typename S2::value_type>::value ) );
+  typedef typename S1::value_type value_type;
+  OrSelector( const edm::ParameterSet & cfg ) : 
+    s1_( cfg ), s2_( cfg ) { }
+  bool operator()( const value_type & t ) const { 
+    return s1_( t ) || s2_( t );
+  }
+private:
+  S1 s1_;
+  S2 s2_;
+};
+
+#endif
