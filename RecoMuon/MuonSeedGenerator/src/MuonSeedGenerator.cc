7af20d63a2dc4b2993592aff54afd356de4ead45
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-09-21 15:49:27 +0000

    bug fix in seed production

--- RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.cc
+++ RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.cc
@@ -3,8 +3,8 @@
  *  
  *  All the code is under revision
  *
- *  $Date: 2006/08/01 15:53:04 $
- *  $Revision: 1.10 $
+ *  $Date: 2006/08/28 16:14:25 $
+ *  $Revision: 1.11 $
  *
  *  \author A. Vitelli - INFN Torino, V.Palichik
  *  \author ported by: R. Bellan - INFN Torino
@@ -522,16 +522,27 @@ void MuonSeedGenerator::produce(edm::Event& event, const edm::EventSetup& eSetup
   if ( MB2 ) delete [] MB2;
   if ( MB1 ) delete [] MB1;
 
-  //>> NEW
-
-  // what is the id??
-  //  output->put(chamberId, theSeeds.begin(),theSeeds.end());
-  for(vector<TrajectorySeed>::iterator seed = theSeeds.begin();
-      seed != theSeeds.end(); ++seed)
-    output->push_back(*seed);
+  
+  if(theSeeds.size() <= 1)
+    output->push_back(theSeeds.front());
+  
+  else{
+    for(vector<TrajectorySeed>::iterator seed = theSeeds.begin();
+	seed != theSeeds.end(); ++seed){
+      int counter =0;
+      for(vector<TrajectorySeed>::iterator seed2 = seed;
+	  seed2 != theSeeds.end(); ++seed2) 
+	if( seed->startingState().parameters().vector() ==
+	    seed2->startingState().parameters().vector() )
+	  ++counter;
+      
+      if( counter > 1 ) theSeeds.erase(seed--);
+      else output->push_back(*seed);
+    }
+  }
   
   event.put(output);
-  //<<
+  
 }
 
 
