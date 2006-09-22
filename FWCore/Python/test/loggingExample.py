8132d174886ca0f8264babd42e86fbe260ab2958
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2006-09-22 14:57:18 +0000

    fixed to be used as unit test

--- FWCore/Python/test/loggingExample.py
+++ FWCore/Python/test/loggingExample.py
@@ -1,10 +1,19 @@
+# Service example file (called by serviceTest.cfg)
+# has to be used together with loggingServer.py
+
 import libFWCorePython as edm
+import xmlrpclib
 
 
 class Service:
  
   def __init__(self):
     print "PythonService: constructor called"
+    try:
+      self.server =  xmlrpclib.Server('http://localhost:8000')
+      self.server.logme("Job started")
+    except:
+      print "Can't connect to RPC server"
 
 
   def postBeginJob(self):
@@ -20,7 +29,10 @@ class Service:
     handle = edm.Handle("edmtest::IntProduct")
     event.getByLabel("int",handle)
     print handle.get().value
+    try: 
+      print self.server.logme("executing PostProcessEvent")
+    except:
+      pass
 
 
-##################
 service = Service()
