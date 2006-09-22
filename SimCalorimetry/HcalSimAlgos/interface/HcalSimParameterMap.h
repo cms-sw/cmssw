a48af958284584474111bf8655c294c7db832a0b
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-09-22 03:49:46 +0000

    Separate parameters for Barrel and Endcap

--- SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h
+++ SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h
@@ -3,23 +3,30 @@
 
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloVSimParameterMap.h"
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 class HcalSimParameterMap : public CaloVSimParameterMap
 {
 public:
+  /// hardcoded default parameters
   HcalSimParameterMap();
+  /// configurable parameters
+  HcalSimParameterMap(const edm::ParameterSet & p);
+
   virtual ~HcalSimParameterMap() {}
 
   virtual const CaloSimParameters & simParameters(const DetId & id) const;
 
   /// accessors
-  CaloSimParameters hbheParameters() const {return theHBHEParameters;}
+  CaloSimParameters hbParameters() const {return theHBParameters;}
+  CaloSimParameters heParameters() const {return theHEParameters;}
   CaloSimParameters hoParameters() const  {return theHOParameters;}
   CaloSimParameters hfParameters1() const  {return theHFParameters1;}
   CaloSimParameters hfParameters2() const  {return theHFParameters2;}
 
 private:
-  CaloSimParameters theHBHEParameters;
+  CaloSimParameters theHBParameters;
+  CaloSimParameters theHEParameters;
   CaloSimParameters theHOParameters;
   CaloSimParameters theHFParameters1;
   CaloSimParameters theHFParameters2;
--- SimCalorimetry/HcalSimAlgos/src/HcalSimParameterMap.cc
+++ SimCalorimetry/HcalSimAlgos/src/HcalSimParameterMap.cc
@@ -8,8 +8,11 @@
 // and 1/(0.3 pe/fC) in the endcap.  I'll change them to make them
 // consistent with the current default calibrations.
 HcalSimParameterMap::HcalSimParameterMap() :
-  theHBHEParameters(2000., 0.3305,
+  theHBParameters(2000., 0.3305,
                    117, 12, 
+                   10, 5, true),
+  theHEParameters(2000., 0.3305,
+                   117, 12,
                    10, 5, true),
   theHOParameters( 4000., 0.3065, 217., 12, 10, 5, true),
   theHFParameters1(1., 18.93,
@@ -27,11 +30,53 @@ HcalSimParameterMap::HcalSimParameterMap() :
                  bool doPhotostatistics)
 */
 
+HcalSimParameterMap::HcalSimParameterMap(const edm::ParameterSet & p)
+: theHBParameters(p.getParameter<double>("photomultiplierGainHB"), 
+                  p.getParameter<double>("amplifierGainHB"),
+                  p.getParameter<double>("samplingFactorHB"),
+                  p.getParameter<double>("timePhaseHB"),
+                  p.getParameter<int>("readoutFrameSizeHB"),
+                  p.getParameter<int>("binOfMaximumHB"),
+                  p.getParameter<bool>("doPhotostatisticsHB")),
+  theHEParameters(p.getParameter<double>("photomultiplierGainHE"),
+                  p.getParameter<double>("amplifierGainHE"),
+                  p.getParameter<double>("samplingFactorHE"),
+                  p.getParameter<double>("timePhaseHE"),
+                  p.getParameter<int>("readoutFrameSizeHE"),
+                  p.getParameter<int>("binOfMaximumHE"),
+                  p.getParameter<bool>("doPhotostatisticsHE")),
+  theHOParameters(p.getParameter<double>("photomultiplierGainHO"),
+                  p.getParameter<double>("amplifierGainHO"),
+                  p.getParameter<double>("samplingFactorHO"),
+                  p.getParameter<double>("timePhaseHO"),
+                  p.getParameter<int>("readoutFrameSizeHO"),
+                  p.getParameter<int>("binOfMaximumHO"),
+                  p.getParameter<bool>("doPhotostatisticsHO")),
+  theHFParameters1(p.getParameter<double>("photomultiplierGainHF1"),
+                   p.getParameter<double>("amplifierGainHF1"),
+                   p.getParameter<double>("samplingFactorHF1"),
+                   p.getParameter<double>("timePhaseHF1"),
+                   p.getParameter<int>("readoutFrameSizeHF1"),
+                   p.getParameter<int>("binOfMaximumHF1"),
+                   p.getParameter<bool>("doPhotostatisticsHF1")),
+  theHFParameters2(p.getParameter<double>("photomultiplierGainHF2"),
+                   p.getParameter<double>("amplifierGainHF2"),
+                   p.getParameter<double>("samplingFactorHF2"),
+                   p.getParameter<double>("timePhaseHF2"),
+                   p.getParameter<int>("readoutFrameSizeHF2"),
+                   p.getParameter<int>("binOfMaximumHF2"),
+                   p.getParameter<bool>("doPhotostatisticsHF2"))
+{
+}
+
+
 
 const CaloSimParameters & HcalSimParameterMap::simParameters(const DetId & detId) const {
   HcalDetId hcalDetId(detId);
-  if(hcalDetId.subdet() == HcalBarrel || hcalDetId.subdet() == HcalEndcap) {
-     return theHBHEParameters;
+  if(hcalDetId.subdet() == HcalBarrel) {
+     return theHBParameters;
+  } else if(hcalDetId.subdet() == HcalEndcap) {
+     return theHEParameters;
   } else if(hcalDetId.subdet() == HcalOuter) {
      return theHOParameters;
   } else { // HF
