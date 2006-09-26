7cb0038ce55d9228fa704f674908ee56e313c46d
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2006-09-26 10:04:19 +0000

    fixed typos, compiles with CMSSW_1_1_0_pre2

--- EventFilter/Utilities/src/RunBase.cc
+++ EventFilter/Utilities/src/RunBase.cc
@@ -1,4 +1,4 @@
 #include "EventFilter/Utilities/interface/RunBase.h"
 
-xdata::UnsignedIntger32 evf::RunBase::runNumber_ = 0;
-std::string evf::RunBase::sourceId_ = "Offline";
+xdata::UnsignedInteger32 evf::RunBase::runNumber_=0;
+std::string              evf::RunBase::sourceId_ ="Offline";
