d1145ca9329101b07872c95fd54f916a5f43aef1
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2006-09-27 11:45:05 +0000

    Using electronselector

--- Calibration/EcalAlCaRecoProducers/interface/AlCaElectronsProducer.h
+++ Calibration/EcalAlCaRecoProducers/interface/AlCaElectronsProducer.h
@@ -9,11 +9,12 @@
 
  Implementation:
      <Notes on implementation>
+
 */
 //
 // Original Author:  Lorenzo AGOSTINO
 //         Created:  Mon Jul 17 18:07:01 CEST 2006
-// $Id: AlCaElectronsProducer.h,v 1.3 2006/09/13 15:20:29 lorenzo Exp $
+// $Id: AlCaElectronsProducer.h,v 1.4 2006/09/25 16:49:22 meridian Exp $
 //
 //
 
@@ -27,13 +28,12 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
+#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 //
 // class decleration
 //
-
 
 class AlCaElectronsProducer : public edm::EDProducer {
    public:
@@ -45,56 +45,12 @@ class AlCaElectronsProducer : public edm::EDProducer {
    private:
       // ----------member data ---------------------------
 
+  
+  edm::InputTag ebRecHitsLabel_;
+  edm::InputTag electronLabel_;
+  std::string alcaBarrelHitsCollection_;
+  int etaSize_;
+  int phiSize_;
+  
 
- std::string pixelMatchElectronProducer_;
- std::string siStripElectronProducer_;
- 
- std::string pixelMatchElectronCollection_;
- std::string siStripElectronCollection_;
-
- std::string BarrelHitsCollection_;
- std::string EndcapHitsCollection_;
- std::string ecalHitsProducer_;
-
- std::string basicClusterCollection_;
- std::string basicClusterProducer_;
-
-// std::string islandBarrelBasicClusterCollection_;
-// std::string islandBarrelBasicClusterProducer_;
-
-// std::string islandBarrelSuperClusterCollection_;
-// std::string islandBarrelSuperClusterProducer_;
-
-// std::string correctedIslandBarrelSuperClusterCollection_;
- //std::string correctedIslandBarrelSuperClusterProducer_;
-
- std::string hybridSuperClusterCollection_;
- std::string hybridSuperClusterProducer_;
- 
- std::string correctedHybridSuperClusterCollection_;
- std::string correctedHybridSuperClusterProducer_ ;
-
-// output collections
-
- std::string alcaPixelMatchElectronCollection_;
-
- std::string alcaSiStripElectronCollection_;
- 
- std::string alcaBarrelHitsCollection_;
-
-// std::string alcaEndcapHitsCollection_;
- 
- std::string alcaBasicClusterCollection_;
- 
-// std::string alcaIslandBarrelSuperClusterCollection_;
- 
-// std::string alcaCorrectedIslandBarrelSuperClusterCollection_;
- 
- std::string alcaHybridSuperClusterCollection_;
- 
- std::string alcaCorrectedHybridSuperClusterCollection_;
- 
- double ptCut_;
-   
-// int evento;
 };
