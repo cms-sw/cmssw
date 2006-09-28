6b6dd60d60d274784cfbe646e05189f17e1e4d48
Ferenc Sikler <sha1-753db23f0bb46b999a71a8a004b971875b5ec09c@cern.ch>
2006-09-28 10:31:44 +0000

    First version.

---
+++ RecoVZero/VZeroFinding/interface/VZeroFinder.h
@@ -0,0 +1,47 @@
+#ifndef _VZeroFinder_h_
+#define _VZeroFinder_h_
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+
+#include "DataFormats/VZero/interface/VZero.h"
+#include "DataFormats/VZero/interface/VZeroFwd.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+
+#include "RecoVertex/VertexTools/interface/ClosestApproachInRPhi.h"
+
+class VZeroFinder
+{
+ public:
+   VZeroFinder(const edm::EventSetup& es,
+                    const edm::ParameterSet& pset);
+   ~VZeroFinder();
+
+   bool checkTrackPair(const reco::Track& posTrack,
+                       const reco::Track& negTrack,
+                       reco::VZeroData& data);
+
+ private:
+   FreeTrajectoryState getTrajectory(const reco::Track& track);
+
+   ClosestApproachInRPhi * theApproach;
+
+   float maxDcaR,
+         maxDcaZ,
+         minCrossingRadius,
+         maxCrossingRadius,
+         maxImpactMother; 
+
+   const MagneticField * theMagField;
+};
+
+#endif
+
---
+++ RecoVZero/VZeroFinding/src/VZeroFinder.cc
@@ -0,0 +1,123 @@
+#include "RecoVZero/VZeroFinding/interface/VZeroFinder.h"
+
+#include "Geometry/Vector/interface/GlobalVector.h"
+
+/*****************************************************************************/
+VZeroFinder::VZeroFinder
+  (const edm::EventSetup& es,
+   const edm::ParameterSet& pset)
+{
+  // Get track-pair level cuts
+  maxDcaR = pset.getParameter<double>("maxDcaR");
+  maxDcaZ = pset.getParameter<double>("maxDcaZ");
+
+  // Get mother cuts
+  minCrossingRadius = pset.getParameter<double>("minCrossingRadius");
+  maxCrossingRadius = pset.getParameter<double>("maxCrossingRadius");
+  maxImpactMother   = pset.getParameter<double>("maxImpactMother");
+
+  // Get magnetic field
+  edm::ESHandle<MagneticField> magField;
+  es.get<IdealMagneticFieldRecord>().get(magField);
+  theMagField = magField.product();
+
+  // Get closest approach finder
+  theApproach = new ClosestApproachInRPhi();
+}
+
+/*****************************************************************************/
+VZeroFinder::~VZeroFinder()
+{
+  delete theApproach;
+}
+
+/*****************************************************************************/
+FreeTrajectoryState VZeroFinder::getTrajectory(const reco::Track& track)
+{ 
+  GlobalPoint position(track.vertex().x(),
+                       track.vertex().y(),
+                       track.vertex().z());
+
+  GlobalVector momentum(track.momentum().x(),
+                        track.momentum().y(),
+                        track.momentum().z());
+
+  GlobalTrajectoryParameters gtp(position,momentum,
+                                 track.charge(),theMagField);
+  
+  FreeTrajectoryState fts(gtp);
+
+  return fts; 
+} 
+
+/*****************************************************************************/
+bool VZeroFinder::checkTrackPair(const reco::Track& posTrack,
+                                      const reco::Track& negTrack,
+                                      reco::VZeroData& data)
+{
+  // Get trajectories
+  FreeTrajectoryState posTraj = getTrajectory(posTrack);
+  FreeTrajectoryState negTraj = getTrajectory(negTrack);
+
+  // Closest approach
+  pair<GlobalTrajectoryParameters, GlobalTrajectoryParameters>  
+    gtp = theApproach->trajectoryParameters(posTraj,negTraj);
+
+  // Closest points
+  pair<GlobalPoint, GlobalPoint>
+    points(gtp.first.position(), gtp.second.position());
+
+  // Momenta at closest point
+  pair<GlobalVector,GlobalVector>
+    momenta(gtp.first.momentum(), gtp.second.momentum());
+
+  // dcaR
+  float dcaR = (points.first - points.second).perp();
+  GlobalPoint crossing(0.5*(points.first.x() + points.second.x()),
+                       0.5*(points.first.y() + points.second.y()),
+                       0.5*(points.first.z() + points.second.z()));
+
+  if(dcaR < maxDcaR &&
+     crossing.perp() > minCrossingRadius &&
+     crossing.perp() < maxCrossingRadius)
+  {
+    // dcaZ
+    float theta = 0.5*(posTrack.theta() + negTrack.theta());
+    float dcaZ = fabs((points.first - points.second).z()) * sin(theta); 
+
+    if(dcaZ < maxDcaZ)
+    {
+      // Momentum of the mother
+      GlobalVector momentum = momenta.first + momenta.second;
+
+      // Impact parameter of the mother in the plane
+      GlobalVector r_(crossing.x(),crossing.y(),0);
+      GlobalVector p_(momentum.x(),momentum.y(),0);
+
+      GlobalVector b_ = r_ - (r_*p_)*p_ / p_.mag2();
+      float impact = b_.mag(); 
+
+      if(impact < maxImpactMother)
+      {
+        // Armenteros variables
+        float pt =
+          (momenta.first.cross(momenta.second)).mag()/momentum.mag();
+        float alpha =
+          (momenta.first.mag2() - momenta.second.mag2())/momentum.mag2();
+
+        // Fill data
+        data.dcaR = dcaR;
+        data.dcaZ = dcaZ;
+        data.crossingPoint = crossing;
+        data.impactMother  = impact;
+        data.armenterosPt    = pt;
+        data.armenterosAlpha = alpha;
+
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
