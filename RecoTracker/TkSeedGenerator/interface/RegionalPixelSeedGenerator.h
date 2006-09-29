dd8a74d434d5695dea359013275176750df3ee41
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2006-09-29 11:03:17 +0000

    *** empty log message ***

---
+++ RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGenerator.h
@@ -0,0 +1,48 @@
+#ifndef RegionalPixelSeedGenerator_h
+#define RegionalPixelSeedGenerator_h
+
+//
+// Package:         RecoTracker/RegionalPixelSeedGenerator
+// Class:           RegionalPixelSeedGenerator
+// 
+// Description:     Calls RoadSeachSeedFinderAlgorithm
+//                  to find TrackingSeeds.
+
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "DataFormats/Common/interface/EDProduct.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "RecoTracker/TkSeedGenerator/interface/CombinatorialRegionalSeedGeneratorFromPixel.h"
+
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/JetReco/interface/Jet.h"
+
+class RegionalPixelSeedGenerator : public edm::EDProducer
+{
+ public:
+
+  explicit RegionalPixelSeedGenerator(const edm::ParameterSet& conf);
+
+  virtual ~RegionalPixelSeedGenerator();
+
+  virtual void produce(edm::Event& e, const edm::EventSetup& c);
+
+ private:
+  edm::ParameterSet conf_;
+  CombinatorialRegionalSeedGeneratorFromPixel  combinatorialSeedGenerator;
+  float ptmin;
+  float originradius;
+  float halflength;
+  float originz;
+  float deltaEta;
+  float deltaPhi;
+  std::string srcDirection;
+  std::string vertexSrc;
+};
+
+#endif
--- RecoTracker/TkSeedGenerator/src/SealModules.cc
+++ RecoTracker/TkSeedGenerator/src/SealModules.cc
@@ -3,12 +3,14 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoTracker/TkSeedGenerator/interface/GlobalPixelSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGenerator.h"
 #include "RecoTracker/TkSeedGenerator/interface/GlobalPixelLessSeedGenerator.h"
 #include "RecoTracker/TkSeedGenerator/interface/GlobalMixedSeedGenerator.h"
 #include "RecoTracker/TkSeedGenerator/interface/CosmicSeedGenerator.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(GlobalPixelSeedGenerator)
+DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGenerator)
 DEFINE_ANOTHER_FWK_MODULE(GlobalPixelLessSeedGenerator)
 DEFINE_ANOTHER_FWK_MODULE(GlobalMixedSeedGenerator)
 DEFINE_ANOTHER_FWK_MODULE(CosmicSeedGenerator)
