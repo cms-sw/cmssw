ff7ca8698c0dbec3285699e8a33071378af71537
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2006-10-01 15:31:48 +0000

    added PhiSymmetryCalibration

--- Calibration/EcalCalibAlgos/src/SealModule.cc
+++ Calibration/EcalCalibAlgos/src/SealModule.cc
@@ -2,7 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Calibration/EcalCalibAlgos/interface/miscalibExample.h"
 #include "Calibration/EcalCalibAlgos/interface/ElectronCalibration.h"
+#include "Calibration/EcalCalibAlgos/interface/PhiSymmetryCalibration.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(miscalibExample);
 DEFINE_ANOTHER_FWK_MODULE(ElectronCalibration);
+DEFINE_ANOTHER_FWK_LOOPER(PhiSymmetryCalibration)
