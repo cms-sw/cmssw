f153ff4d12c55a489ed53f7d556ea235fafe04ec
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-10-02 15:43:30 +0000

    new modules for the algorithmic geometry added

--- Geometry/EcalCommonData/src/module.cc
+++ Geometry/EcalCommonData/src/module.cc
@@ -1,10 +1,14 @@
 //<<<<<< INCLUDES                                                       >>>>>>
 
 //#include "Geometry/EcalPreshowerData/interface/DDTestAlgorithm.h"
-#include "Geometry/EcalCommonData/interface/Preshower.h"
+#include "Geometry/EcalCommonData/interface/DDEcalBarrelAlgo.h"
+#include "Geometry/EcalCommonData/interface/DDEcalPreshowerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 #include "PluginManager/ModuleDef.h"
 
 DEFINE_SEAL_MODULE ();
 //DEFINE_SEAL_PLUGIN (DDAlgorithmFactory, DDTestAlgorithm, "DDTestAlgorithm");
-DEFINE_SEAL_PLUGIN (DDAlgorithmFactory, Preshower, "Preshower");
+
+DEFINE_SEAL_PLUGIN (DDAlgorithmFactory, DDEcalBarrelAlgo, "DDEcalBarrelAlgo");
+
+DEFINE_SEAL_PLUGIN (DDAlgorithmFactory, DDEcalPreshowerAlgo, "DDEcalPreshowerAlgo");
