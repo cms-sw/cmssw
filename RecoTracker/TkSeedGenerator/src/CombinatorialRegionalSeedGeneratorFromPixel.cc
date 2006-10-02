e583c07037fc01f4719f1a6f812f4f89a9c1e2ba
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2006-10-02 14:51:43 +0000

    *** empty log message ***

---
+++ RecoTracker/TkSeedGenerator/src/CombinatorialRegionalSeedGeneratorFromPixel.cc
@@ -0,0 +1,28 @@
+#include "RecoTracker/TkSeedGenerator/interface/CombinatorialRegionalSeedGeneratorFromPixel.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+void CombinatorialRegionalSeedGeneratorFromPixel::init(const SiPixelRecHitCollection &coll ,
+					       const edm::EventSetup& iSetup)
+{
+  pixelLayers->init(coll,iSetup);
+  initPairGenerator(pixelLayers,iSetup);
+}
+
+CombinatorialRegionalSeedGeneratorFromPixel::CombinatorialRegionalSeedGeneratorFromPixel(edm::ParameterSet const& conf)
+  : SeedGeneratorFromLayerPairs(conf)
+{  
+  edm::ParameterSet conf_ = pSet();
+
+
+
+  pixelLayers = new PixelSeedLayerPairs();
+}
+
+void CombinatorialRegionalSeedGeneratorFromPixel::run(RectangularEtaPhiTrackingRegion& etaphiRegion, TrajectorySeedCollection &output,
+					      const edm::EventSetup& iSetup){
+
+  region = etaphiRegion;
+  seeds(output,iSetup,region);
+}
---
+++ RecoTracker/TkSeedGenerator/src/RegionalPixelSeedGenerator.cc
@@ -0,0 +1,106 @@
+//
+// Package:         RecoTracker/TkSeedGenerator
+// Class:           RegionalPixelSeedGenerator
+// 
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGenerator.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
+#include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "RecoTracker/TkTrackingRegions/interface/RectangularEtaPhiTrackingRegion.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include <DataFormats/VertexReco/interface/Vertex.h>
+#include "DataFormats/Math/interface/Vector3D.h"
+// Math
+#include "Math/GenVector/VectorUtil.h"
+#include "Math/GenVector/PxPyPzE4D.h"
+
+using namespace std;
+using namespace reco;
+
+RegionalPixelSeedGenerator::RegionalPixelSeedGenerator(edm::ParameterSet const& conf) : 
+  conf_(conf),combinatorialSeedGenerator(conf)
+{
+  edm::LogInfo ("RegionalPixelSeedGenerator")<<"Enter the RegionalPixelSeedGenerator";
+  produces<TrajectorySeedCollection>();
+
+  ptmin=conf_.getParameter<double>("ptMin");
+  originradius=conf_.getParameter<double>("originRadius");
+  halflength=conf_.getParameter<double>("originHalfLength");
+  vertexSrc=conf_.getParameter<string>("vertexSrc");
+  deltaEta = conf_.getParameter<double>("deltaEtaRegion");
+  deltaPhi = conf_.getParameter<double>("deltaPhiRegion");
+  srcDirection = conf_.getParameter<string>("sourceDirection");
+
+}
+
+// Virtual destructor needed.
+RegionalPixelSeedGenerator::~RegionalPixelSeedGenerator() { }  
+
+// Functions that gets called by framework every event
+void RegionalPixelSeedGenerator::produce(edm::Event& e, const edm::EventSetup& es)
+{
+  // get Inputs
+  edm::Handle<SiPixelRecHitCollection> pixelHits;
+
+  edm::ESHandle<TrackerGeometry> tracker;
+  es.get<TrackerDigiGeometryRecord>().get(tracker);
+  edm::Handle<reco::VertexCollection> vertices;
+  e.getByLabel(vertexSrc,vertices);
+  const reco::VertexCollection vertCollection = *(vertices.product());
+  reco::VertexCollection::const_iterator ci = vertCollection.begin();
+  if(vertCollection.size() == 0) return;
+  originz = ci->z();
+
+
+
+  //
+  // get the pixel Hits
+  //
+  std::string hitProducer = conf_.getParameter<std::string>("HitProducer");
+  e.getByLabel(hitProducer, pixelHits);
+  //  e.getByType(pixelHits);
+
+
+
+  std::auto_ptr<TrajectorySeedCollection> output(new TrajectorySeedCollection);
+  //
+  
+  //Get the jet direction
+  edm::Handle<CaloJetCollection> jets;
+  e.getByLabel(srcDirection, jets);
+
+  GlobalVector globalVector(0,0,1);
+  if(jets->size() != 0)
+    {
+      GlobalVector jetVector((jets->begin())->px(),(jets->begin())->py(),(jets->begin())->pz());
+      globalVector = jetVector;
+    }
+
+
+  RectangularEtaPhiTrackingRegion* etaphiRegion = new  RectangularEtaPhiTrackingRegion(globalVector,
+										       GlobalPoint(0,0,originz), 
+										       ptmin,
+										       originradius,
+										       halflength,
+										       deltaEta,
+										       deltaPhi);
+  
+  combinatorialSeedGenerator.init(*pixelHits,es);
+  combinatorialSeedGenerator.run(*etaphiRegion,*output,es);
+
+  // write output to file
+  LogDebug("Algorithm Performance")<<" number of seeds = "<< output->size();
+
+
+  e.put(output);
+}
