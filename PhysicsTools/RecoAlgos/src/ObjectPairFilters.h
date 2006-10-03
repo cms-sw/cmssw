9a8dc0e920549849a824379ff6830f3258e2d6e3
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-10-03 10:34:32 +0000

    fixed pair selectors

--- PhysicsTools/RecoAlgos/src/ObjectPairFilters.h
+++ PhysicsTools/RecoAlgos/src/ObjectPairFilters.h
@@ -2,6 +2,7 @@
 #define RecoAlgos_ObjectPairFilters_h
 #include "PhysicsTools/UtilAlgos/interface/ObjectPairFilter.h"
 #include "PhysicsTools/Utilities/interface/MasslessInvariantMass.h"
+#include "PhysicsTools/Utilities/interface/RangeObjectPairSelector.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/EgammaCandidates/interface/Electron.h"
 
@@ -10,13 +11,17 @@ namespace reco {
     /// filter on electron pairs based on invariant mass
     typedef ObjectPairFilter<
               reco::ElectronCollection,
-              MasslessInvariantMass<reco::Electron> 
+              RangeObjectPairSelector<
+                reco::Electron, 
+                MasslessInvariantMass<reco::Electron> >
             > ElectronPairMassFilter;
 
     /// filter on muon pairs based on invariant mass
     typedef ObjectPairFilter<
               reco::MuonCollection,
-              MasslessInvariantMass<reco::Muon> 
+              RangeObjectPairSelector<
+                reco::Muon, 
+                MasslessInvariantMass<reco::Muon> >
             > MuonPairMassFilter;
   }
 }
