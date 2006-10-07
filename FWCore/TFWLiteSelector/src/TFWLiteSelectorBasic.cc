edb5cd4348c2562a94d19ca9af4b2e70cb772925
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-10-07 07:26:11 +0000

    moved processHistory() from EventAux to EventPrincipal

--- FWCore/TFWLiteSelector/src/TFWLiteSelectorBasic.cc
+++ FWCore/TFWLiteSelector/src/TFWLiteSelectorBasic.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Tue Jun 27 17:58:10 EDT 2006
-// $Id: TFWLiteSelectorBasic.cc,v 1.2 2006/07/26 12:22:19 chrjones Exp $
+// $Id: TFWLiteSelectorBasic.cc,v 1.3 2006/08/26 18:37:13 chrjones Exp $
 //
 
 // system include files
@@ -236,7 +236,6 @@ TFWLiteSelectorBasic::Process(Long64_t iEntry) {
       m_->metaTree_->GetEntry(iEntry);
 
 //NEW      m_->processNames_ = aux.processHistory();
-      m_->processNames_ = aux.processHistory();
 
 //      std::cout <<"ProcessNames\n";
 //      for(edm::ProcessNameList::const_iterator itName = m_->processNames_.begin();
@@ -248,6 +247,7 @@ TFWLiteSelectorBasic::Process(Long64_t iEntry) {
       try {
 	 m_->reader_->setEntry(iEntry);
 	 edm::EventPrincipal ep(aux.id(), aux.time(),m_->reg_, aux.luminosityBlockID(),  aux.processHistoryID(), m_->reader_);
+         m_->processNames_ = ep.processHistory();
 //OLD	 edm::EventPrincipal ep(aux.id_, aux.time_,m_->reg_, m_->processNames_, m_->reader_);
 
 	 using namespace edm;
