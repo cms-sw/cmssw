a1fe7f2d3ebd242aa5a60efa9969995973775b20
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-10-09 16:45:34 +0000

    extensions requested by Ursula

--- DataFormats/TrajectorySeed/src/classes.h
+++ DataFormats/TrajectorySeed/src/classes.h
@@ -1,12 +1,20 @@
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/RefVectorIterator.h"
+#include "DataFormats/Common/interface/Ref.h"
 
 namespace {
   namespace {
     std::vector<TrajectorySeed> v1;
     TrajectorySeedCollection c1;
     edm::Wrapper<TrajectorySeedCollection> w1;
-    edm::RefVectorIterator<std::vector<TrajectorySeed>,TrajectorySeed,edm::refhelper::FindUsingAdvance<std::vector<TrajectorySeed>,TrajectorySeed> > rfitr1;
+
+edm::RefVectorIterator<std::vector<TrajectorySeed>,TrajectorySeed,edm::refhelper::FindUsingAdvance<std::vector<TrajectorySeed>,TrajectoryS
+eed> > rfitr1;
+    edm::Ref<TrajectorySeedCollection> s1;
+    edm::RefProd<TrajectorySeedCollection> s2;
+    edm::RefVector<TrajectorySeedCollection> s3;
   }
 }
+
+
