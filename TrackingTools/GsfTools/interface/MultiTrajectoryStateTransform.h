c75532ef15c8755892ef2e4dbb84c5d12fa6a0aa
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2006-10-09 16:59:43 +0000

    helper class for accessing multiTSOSes

---
+++ TrackingTools/GsfTools/interface/MultiTrajectoryStateTransform.h
@@ -0,0 +1,41 @@
+#ifndef MultiTrajectoryStateTransform_H
+#define MultiTrajectoryStateTransform_H
+
+/** Extracts innermost and  outermost states from a GsfTrack
+ *  in form of a TrajectoryStateOnSurface */
+
+#include "DataFormats/TrackReco/interface/GsfTrack.h"
+#include "DataFormats/TrackReco/interface/GsfTrackExtra.h"
+
+class TrajectoryStateOnSurface;
+class TrackingGeometry;
+class Surface;
+class MagneticField;
+
+class MultiTrajectoryStateTransform {
+private:
+  typedef reco::GsfTrackExtra::LocalParameterVector ParameterVector;
+  typedef reco::GsfTrackExtra::LocalCovarianceMatrix CovarianceMatrix;
+  enum { dimension = reco::GsfTrackExtra::dimension };
+
+public:
+
+  /** Construct a TrajectoryStateOnSurface from the reco::GsfTrack 
+   *  innermost or outermost state, requires access to tracking geometry */
+  TrajectoryStateOnSurface innerStateOnSurface( const reco::GsfTrack& tk, 
+						const TrackingGeometry& geom,
+						const MagneticField* field) const;
+  TrajectoryStateOnSurface outerStateOnSurface( const reco::GsfTrack& tk, 
+						const TrackingGeometry& geom,
+						const MagneticField* field) const;
+
+private:
+  TrajectoryStateOnSurface stateOnSurface (const std::vector<double>& weights,
+					   const std::vector<ParameterVector>& parameters,
+					   const std::vector<CovarianceMatrix>& covariances,
+					   const double& pzSign,
+					   const Surface& surface,
+					   const MagneticField* field) const;
+};
+
+#endif
---
+++ TrackingTools/GsfTools/src/MultiTrajectoryStateTransform.cc
@@ -0,0 +1,74 @@
+#include "TrackingTools/GsfTools/interface/MultiTrajectoryStateTransform.h"
+#include "DataFormats/TrackReco/interface/GsfTrackExtraFwd.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "TrackingTools/GsfTools/interface/BasicMultiTrajectoryState.h"
+#include "TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryParameters.h"
+#include "TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryError.h"
+#include "Geometry/CommonDetUnit/interface/TrackingGeometry.h"
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+
+
+TrajectoryStateOnSurface 
+MultiTrajectoryStateTransform::outerStateOnSurface( const reco::GsfTrack& tk, 
+						    const TrackingGeometry& geom,
+						    const MagneticField* field) const
+{
+  const Surface& surface = geom.idToDet( DetId( tk.extra()->outerDetId()))->surface();
+
+  const reco::GsfTrackExtraRef& extra(tk.extra());
+  return stateOnSurface(extra->outerStateWeights(),
+			extra->outerStateLocalParameters(),
+			extra->outerStateCovariances(),
+			extra->outerStateLocalPzSign(),
+			surface,field);
+}
+
+TrajectoryStateOnSurface 
+MultiTrajectoryStateTransform::innerStateOnSurface( const reco::GsfTrack& tk, 
+						    const TrackingGeometry& geom,
+						    const MagneticField* field) const
+{
+  const Surface& surface = geom.idToDet( DetId( tk.extra()->innerDetId()))->surface();
+
+  const reco::GsfTrackExtraRef& extra(tk.extra());
+  return stateOnSurface(extra->innerStateWeights(),
+			extra->innerStateLocalParameters(),
+			extra->innerStateCovariances(),
+			extra->innerStateLocalPzSign(),
+			surface,field);
+}
+
+TrajectoryStateOnSurface 
+MultiTrajectoryStateTransform::stateOnSurface (const std::vector<double>& weights,
+					       const std::vector<ParameterVector>& parameters,
+					       const std::vector<CovarianceMatrix>& covariances,
+					       const double& pzSign,
+					       const Surface& surface,
+					       const MagneticField* field) const
+{
+  if ( weights.empty() )  return TrajectoryStateOnSurface();
+  
+  unsigned int nc(weights.size());
+  AlgebraicVector pars(dimension);
+  AlgebraicSymMatrix cov(dimension);
+  
+  std::vector<TrajectoryStateOnSurface> components;
+  components.reserve(nc);
+  
+  // create components TSOSs
+  for ( unsigned int i=0; i<nc; i++ ) {
+    // convert parameter vector and covariance matrix
+    for ( unsigned int j1=0; j1<dimension; j1++ ) {
+      pars[j1] = parameters[i](j1);
+      for ( unsigned int j2=0; j2<=j1; j2++ ) 
+	cov[j1][j2] = covariances[i](j1,j2);
+    }
+    // create local parameters & errors
+    LocalTrajectoryParameters lp(pars,pzSign);
+    LocalTrajectoryError le(cov);
+    // create component
+    components.push_back(TrajectoryStateOnSurface(lp,le,surface,field,weights[i]));
+  }
+  return 
+    TrajectoryStateOnSurface(new BasicMultiTrajectoryState(components));
+}
