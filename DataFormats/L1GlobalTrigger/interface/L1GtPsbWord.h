a60190336d34eda4cb02ad094f554a56a0b6fe6f
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2006-10-10 11:34:18 +0000

    First version of PSB word class

---
+++ DataFormats/L1GlobalTrigger/interface/L1GtPsbWord.h
@@ -0,0 +1,108 @@
+#ifndef L1GlobalTrigger_L1GtPsbWord_h
+#define L1GlobalTrigger_L1GtPsbWord_h
+
+/**
+ * \class L1GtPsbWord
+ * 
+ * 
+ * 
+ * Description: L1 Global Trigger - PSB composed word in the readout record 
+ * Implementation:
+ *    <TODO: enter implementation details>
+ *   
+ * \author: Vasile Mihai Ghete - HEPHY Vienna
+ * 
+ * $Date$
+ * $Revision$
+ *
+ */
+
+// system include files
+#include <boost/cstdint.hpp>
+
+// user include files
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+
+// forward declarations
+
+class L1GtPsbWord
+{
+
+public:
+
+    static const int NumberAData = 8;
+    static const int NumberBData = 8;
+
+public:
+    /// constructors
+    L1GtPsbWord();    // empty constructor, all members set to zero;
+
+    // constructor from unpacked values;
+    L1GtPsbWord(
+        uint16_t boardIdValue,
+        uint16_t bxInEventValue,
+        uint16_t bxNrValue,
+        uint32_t eventNrValue,
+        uint16_t aDataValue[NumberAData],
+        uint16_t bDataValue[NumberBData],
+        uint16_t localBxNrValue
+    ); 
+    
+
+    /// destructor
+    virtual ~L1GtPsbWord();
+
+public:
+
+    /// get/set board ID
+    inline const uint16_t boardId() const { return m_boardId; }
+    void setBoardId(uint16_t boardIdValue) { m_boardId = boardIdValue; }
+          
+    /// get/set bunch cross in the GT event record 
+    inline const uint16_t bxInEvent() const { return m_bxInEvent; }
+    void setBxInEvent(uint16_t bxInEventValue) { m_bxInEvent = bxInEventValue; }
+
+    /// get/set bunch cross number of the actual bx
+    inline const uint16_t bxNr() const { return m_bxNr; }
+    void setBxNr(uint16_t bxNrValue) { m_bxNr = bxNrValue; }
+
+    /// get/set event number since last L1 reset generated in PSB
+    inline const uint32_t eventNr() const { return m_eventNr; } 
+    void setEventNr(uint32_t eventNrValue) { m_eventNr = eventNrValue; }
+
+    /// get/set A_DATA_CH_IA 
+    const uint16_t aData(int iA) const;
+    void setAData(uint16_t aDataVal, int iA);
+
+    /// get/set B_DATA_CH_IB 
+    const uint16_t bData(int iB) const;
+    void setBData(uint16_t bDataVal, int iB);
+    
+    /// get/set local bunch cross number of the actual bx
+    inline const uint16_t localBxNr() const { return m_localBxNr; }     
+    void setLocalBxNr(uint16_t localBxNrValue) { m_localBxNr = localBxNrValue; }
+            
+private:
+
+                               // first number in the comment represents number of bits
+
+    uint16_t m_boardId;        // 16: board identifier
+                               //
+    uint16_t m_bxInEvent;      //  4: bunch cross in the GT event record 
+                               //     one keeps 3 bx (F, 0, 1) for standard record, 
+                               //               5 bx (E, F, 0, 1) for debug record  
+                               //
+    uint16_t m_bxNr;           // 12: bunch cross number of the actual bx
+    uint32_t m_eventNr;        // 24: event number since last L1 reset generated in PSB
+//
+    uint16_t m_aData[NumberAData];    // 16: A_Data_ChX
+//
+    uint16_t m_bData[NumberBData];    // 16: B_Data_ChX
+//
+    uint16_t m_localBxNr;      // 12: local bunch cross number of the actual bx
+                               //     bx number at which the data were written into the ringbuffer
+
+        
+};
+
+#endif /*L1GlobalTrigger_L1GtPsbWord_h*/
---
+++ DataFormats/L1GlobalTrigger/src/L1GtPsbWord.cc
@@ -0,0 +1,143 @@
+/**
+ * \class L1GtPsbWord
+ * 
+ * 
+ * 
+ * Description: see header file 
+ * Implementation:
+ *    <TODO: enter implementation details>
+ *   
+ * \author: Vasile Mihai Ghete - HEPHY Vienna
+ * 
+ * $Date$
+ * $Revision$
+ *
+ */
+
+// this class header
+#include "DataFormats/L1GlobalTrigger/interface/L1GtPsbWord.h"
+
+// system include files
+
+// user include files
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+
+#include "FWCore/Utilities/interface/EDMException.h"
+
+
+// constructors
+
+// empty constructor, all members set to zero;
+L1GtPsbWord::L1GtPsbWord() {
+
+    m_boardId = 0;
+    m_bxInEvent = 0;
+    m_bxNr = 0;
+    m_eventNr = 0;
+
+    for (int iA = 0; iA < NumberAData; ++iA) {
+        m_aData[iA] = 0;            
+    }
+
+    for (int iB = 0; iB < NumberBData; ++iB) {
+        m_bData[iB] = 0;            
+    }
+
+    m_localBxNr = 0;
+
+}
+
+// constructor from unpacked values;
+L1GtPsbWord::L1GtPsbWord(
+    uint16_t boardIdValue,
+    uint16_t bxInEventValue,
+    uint16_t bxNrValue,
+    uint32_t eventNrValue,
+    uint16_t aDataValue[NumberAData],
+    uint16_t bDataValue[NumberBData],
+    uint16_t localBxNrValue
+    ) 
+{
+
+    m_boardId = boardIdValue;
+    m_bxInEvent = bxInEventValue;
+    m_bxNr = bxNrValue;
+    m_eventNr = eventNrValue;
+
+    for (int iA = 0; iA < NumberAData; ++iA) {
+        m_aData[iA] = aDataValue[iA];            
+    }
+
+    for (int iB = 0; iB < NumberBData; ++iB) {
+        m_bData[iB] = bDataValue[iB];            
+    }
+
+    m_localBxNr = localBxNrValue;
+    
+}
+
+// destructor
+L1GtPsbWord::~L1GtPsbWord() {
+}
+
+// methods
+
+// get/set A_DATA_CH_IA 
+
+const uint16_t L1GtPsbWord::aData(int iA) const {
+
+    if (iA < 0 || iA > NumberAData) {
+        throw cms::Exception("aDataIndexError")
+            << "\nError: index for A_DATA array out of range. Allowed range: [0, "
+            << NumberAData << ") " << std::endl;
+        
+    } else {
+         return m_aData[iA];         
+    }     
+     
+}
+void L1GtPsbWord::setAData(uint16_t aDataVal, int iA) { 
+
+    if (iA < 0 || iA > NumberAData) {
+        throw cms::Exception("aDataIndexError")
+            << "\nError: index for A_DATA array out of range. Allowed range: [0, "
+            << NumberAData << ") " << std::endl;
+        
+    } else {
+        m_aData[iA] = aDataVal; 
+    }     
+    
+}
+
+// get/set B_DATA_CH_IB 
+
+const uint16_t L1GtPsbWord::bData(int iB) const {
+
+    if (iB < 0 || iB > NumberBData) {
+        throw cms::Exception("bDataIndexError")
+            << "\nError: index for B_DATA array out of range. Allowed range: [0, "
+            << NumberBData << ") " << std::endl;
+        
+    } else {
+         return m_bData[iB];         
+    }     
+     
+}
+void L1GtPsbWord::setBData(uint16_t bDataVal, int iB) { 
+
+    if (iB < 0 || iB > NumberBData) {
+        throw cms::Exception("bDataIndexError")
+            << "\nError: index for B_DATA array out of range. Allowed range: [0, "
+            << NumberBData << ") " << std::endl;
+        
+    } else {
+        m_bData[iB] = bDataVal; 
+    }     
+    
+}
+
+// static class members
+const int L1GtPsbWord::NumberAData;
+const int L1GtPsbWord::NumberBData;
+
+
