a89051c0e420ca376a1b307a914e0443577c7663
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-10-10 09:02:51 +0000

    fix return

--- DataFormats/TrajectorySeed/src/classes.h
+++ DataFormats/TrajectorySeed/src/classes.h
@@ -9,8 +9,7 @@ namespace {
     TrajectorySeedCollection c1;
     edm::Wrapper<TrajectorySeedCollection> w1;
 
-edm::RefVectorIterator<std::vector<TrajectorySeed>,TrajectorySeed,edm::refhelper::FindUsingAdvance<std::vector<TrajectorySeed>,TrajectoryS
-eed> > rfitr1;
+    edm::RefVectorIterator<std::vector<TrajectorySeed>,TrajectorySeed,edm::refhelper::FindUsingAdvance<std::vector<TrajectorySeed>,TrajectorySeed> > rfitr1;
     edm::Ref<TrajectorySeedCollection> s1;
     edm::RefProd<TrajectorySeedCollection> s2;
     edm::RefVector<TrajectorySeedCollection> s3;
