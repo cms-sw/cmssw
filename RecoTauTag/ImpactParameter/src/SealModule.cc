1e54190fc45f1a56bdfd7a55c66a3f838926b366
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2006-10-10 08:01:05 +0000

    *** empty log message ***

--- RecoTauTag/ImpactParameter/src/SealModule.cc
+++ RecoTauTag/ImpactParameter/src/SealModule.cc
@@ -1,11 +1,11 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTauTag/ImpactParameter/interface/ImpactParameter.h"
-#include "RecoTauTag/ImpactParameter/interface/TauImpactParameterTest.h"
+//#include "RecoTauTag/ImpactParameter/interface/TauImpactParameterTest.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ImpactParameter)
-#DEFINE_ANOTHER_FWK_MODULE(TauImpactParameterTest)
+//DEFINE_ANOTHER_FWK_MODULE(TauImpactParameterTest)
 
 
 
