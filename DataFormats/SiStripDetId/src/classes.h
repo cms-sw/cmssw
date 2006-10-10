04a55846218ddfb385e4e1397aec17005ac162a9
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-10-10 14:25:49 +0000

    added key classes

--- DataFormats/SiStripDetId/src/classes.h
+++ DataFormats/SiStripDetId/src/classes.h
@@ -1,4 +1,8 @@
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
 #include "DataFormats/SiStripDetId/interface/TIBDetId.h"
 #include "DataFormats/SiStripDetId/interface/TIDDetId.h"
 #include "DataFormats/SiStripDetId/interface/TOBDetId.h"
 #include "DataFormats/SiStripDetId/interface/TECDetId.h"
+#include "DataFormats/SiStripDetId/interface/SiStripFecKey.h"
+#include "DataFormats/SiStripDetId/interface/SiStripFedKey.h"
+#include "DataFormats/SiStripDetId/interface/SiStripDetKey.h"
