0e20dc5e11ccc848132591265a33375dd9c6a76e
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2006-10-11 08:28:39 +0000

    Make addition for TB2006 simulation for H2 ECal+HCal

--- SimDataFormats/HcalTestBeam/src/classes.h
+++ SimDataFormats/HcalTestBeam/src/classes.h
@@ -1,12 +1,17 @@
 #include "SimDataFormats/HcalTestBeam/interface/PHcalTB04Info.h"
+#include "SimDataFormats/HcalTestBeam/interface/PHcalTB06Info.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include <vector>
 
 namespace {
   namespace {
-    std::vector<int>            dumI;
-    std::vector<float>          dumF;
-    PHcalTB04Info               theInfo;
-    edm::Wrapper<PHcalTB04Info> theTB04Info;
+    std::vector<int>                dumI;
+    std::vector<float>              dumF;
+    PHcalTB04Info                   theInfo4;
+    edm::Wrapper<PHcalTB04Info>     theTB04Info;
+    std::vector<PHcalTB06Info::Vtx> dummy1;
+    std::vector<PHcalTB06Info::Hit> dummy2;
+    PHcalTB06Info                   theInfo6;
+    edm::Wrapper<PHcalTB06Info>     theTB06Info;
   }
 }
