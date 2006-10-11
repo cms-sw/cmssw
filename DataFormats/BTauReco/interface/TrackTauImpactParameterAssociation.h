19fa035bfc015ddc917910c1e5e50d87d81ae207
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-10-11 11:35:15 +0000

    added association map typedef

---
+++ DataFormats/BTauReco/interface/TrackTauImpactParameterAssociation.h
@@ -0,0 +1,17 @@
+#ifndef BTauReco_TrackTauImpactParameterAssociation_h
+#define BTauReco_TrackTauImpactParameterAssociation_h
+
+#include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/BTauReco/interface/TauImpactParameterInfo.h"
+
+namespace reco {
+  typedef edm::AssociationMap <
+            edm::OneToValue<
+              reco::TrackCollection,
+              reco::TauImpactParameterTrackData
+            > 
+          > TrackTauImpactParameterAssociationCollection;
+}
+
+#endif
