2dceaca6aebb53c46c0f8072ae7dc09e89931125
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-10-12 19:58:30 +0000

    Adding dependency for MuonNumberingRecord so that it can be retrieved where needed for access to MuonNumberingInitialization.

--- Geometry/Records/interface/MuonGeometryRecord.h
+++ Geometry/Records/interface/MuonGeometryRecord.h
@@ -4,22 +4,22 @@
 /** \class MuonGeometryRecord
  *  The Muon DetUnit geometry.
  *
- *  $Date: 2005/10/25 14:10:07 $
- *  $Revision: 1.1 $
+ *  $Date: 2006/08/22 15:44:05 $
+ *  $Revision: 1.2 $
  *  \author N. Amapane - CERN
  */
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/Records/interface/MuonNumberingRecord.h"
 #include "boost/mpl/vector.hpp"
 #include "CondFormats/DataRecord/interface/DTAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/DTAlignmentErrorRcd.h"
 #include "CondFormats/DataRecord/interface/CSCAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/CSCAlignmentErrorRcd.h"
 
-
-class MuonGeometryRecord : public edm::eventsetup::DependentRecordImplementation<MuonGeometryRecord,boost::mpl::vector<IdealGeometryRecord, DTAlignmentRcd, DTAlignmentErrorRcd, CSCAlignmentRcd, CSCAlignmentErrorRcd> > {};
+class MuonGeometryRecord : public edm::eventsetup::DependentRecordImplementation<MuonGeometryRecord,boost::mpl::vector<IdealGeometryRecord, MuonNumberingRecord, DTAlignmentRcd, DTAlignmentErrorRcd, CSCAlignmentRcd, CSCAlignmentErrorRcd> > {};
 
 #endif
 
