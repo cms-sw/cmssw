effedb18603055711165e7f5cee097d37204b2ed
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2006-10-13 14:34:13 +0000

    V2.0

--- Calibration/Tools/interface/MinL3Algorithm.h
+++ Calibration/Tools/interface/MinL3Algorithm.h
@@ -4,9 +4,10 @@
 /** \class MinL3Algorithm
  *  Implementation of the L3 Collaboration algorithm to solve a system Ax = B
  *  by minimization of |Ax-B| using an iterative linear approach
+ *  This class is specific for the ECAL calibration
  *
- * $Date: 2006/08/25 $
- * $Revision: 1.2 $
+ * $Date: 2006/10/13 $
+ * $Revision: 2.0 $
  * \author R.Ofierzynski, CERN
  */
 
@@ -20,25 +21,41 @@ class MinL3Algorithm
 {
 public:
   /// Default constructor
-  /// CAVEAT: use normalise = true only if you know what you're doing!
-  MinL3Algorithm(bool normalise = false);
+  MinL3Algorithm(int squareMode_ = 5, int mineta_ = -85, int maxeta_ = 85, int minphi_ = 1, int maxphi_ = 360);
 
   /// Destructor
   ~MinL3Algorithm();
 
-  /// run the Minimization L3 Algorithm "nIter" number of times, recalibrating the event matrix after each iteration with the new solution
+  /// method doing the full calibration running nIter number of times, recalibrating the event matrix after each iteration with the new solution
   /// returns the vector of calibration coefficients built from all iteration solutions
-  vector<float> iterate(const vector<vector<float> >& eventMatrix, const vector<float>& energyVector, int nIter);
+  /// >> to be used also as recipe on how to use the calibration methods one-by-one <<
+  vector<float> iterate(const vector<vector<float> >& eventMatrix, const vector<int>& VmaxCeta, const vector<int>& VmaxCphi, const vector<float>& energyVector, const int& nIter, const bool& normalizeFlag = false);
 
-  /// perform one iteration using the Minimization L3 Algorithm
-  /// returns the vector of calibration coefficients
-  vector<float> iterate(const vector<vector<float> >& eventMatrix, const vector<float>& energyVector);
 
-protected:
+  /// add event to the calculation of the calibration vector
+  void addEvent(const vector<float>& eventSquare, const int& maxCeta, const int& maxCphi, const float& energy);
+
+  /// recalibrate before next iteration: give previous solution vector as argument
+  vector<float> recalibrateEvent(const vector<float>& eventSquare, const int& maxCeta, const int& maxCphi, const vector<float>& recalibrateVector); 
+
+  /// get the solution at the end of the calibration
+  vector<float> getSolution(bool resetsolution=true);
+
+  /// reset for new iteration
+  void resetSolution(); 
+
+  /// method to translate from square indices to region indices
+  int indexSqr2Reg(const int& sqrIndex, const int& maxCeta, const int& maxCphi);
 
 
 private:
-  bool normaliseFlag;
+
+  int squareMode;
+  int mineta, maxeta, minphi, maxphi;
+  int countEvents;
+  int Nchannels, Nxtals;
+  vector<float> wsum;
+  vector<float> Ewsum;
 
 };
 
--- Calibration/Tools/src/MinL3Algorithm.cc
+++ Calibration/Tools/src/MinL3Algorithm.cc
@@ -1,19 +1,28 @@
 
-/** \file MinL3Algorithm.cc
+/** \file GenericMinL3Algorithm.cc
  *
- * $Date: 2006/08/25 $
- * $Revision: 1.2 $
- *
+ * $Date: 2006/10/13 $
+ * $Revision: 2.0 $
  * \author R.Ofierzynski, CERN
  */
 
 #include "Calibration/Tools/interface/MinL3Algorithm.h"
 
 
-MinL3Algorithm::MinL3Algorithm(bool normalise)
-  :normaliseFlag(normalise)
-// if normalize=true: for all events sum the e25ovTRP. then take the mean value and rescale all TrP
+MinL3Algorithm::MinL3Algorithm(int squareMode_, int mineta_, int maxeta_, int minphi_, int maxphi_)
+  :squareMode(squareMode_), mineta(mineta_), maxeta(maxeta_), minphi(minphi_), maxphi(maxphi_), countEvents(0)
 {
+  int Neta = maxeta - mineta + 1;
+  if (mineta * maxeta < 0) Neta--; // there's no eta index = 0
+  int Nphi = maxphi - minphi + 1;
+  if (Nphi <0) Nphi += 360;
+  
+  Nchannels = Neta * Nphi; // no. of channels, get it from edges of the region
+
+  Nxtals = squareMode*squareMode; // no. of xtals in one event
+
+  wsum.assign(Nchannels,0.);
+  Ewsum.assign(Nchannels,0.);
 }
 
 
@@ -22,119 +31,161 @@ MinL3Algorithm::~MinL3Algorithm()
 }
 
 
-vector<float> MinL3Algorithm::iterate(const vector<vector<float> >& eventMatrix, const vector<float>& energyVector, const int nIter)
+vector<float> MinL3Algorithm::iterate(const vector<vector<float> >& eventMatrix, const vector<int>& VmaxCeta, const vector<int>& VmaxCphi, const vector<float>& energyVector, const int& nIter, const bool& normalizeFlag)
 {
-  vector<float> solution;
-  vector<float> theCalibVector(energyVector.size(),1.);
-  vector<vector<float> > myEventMatrix(eventMatrix);
   int Nevents = eventMatrix.size(); // Number of events to calibrate with
-  int Nchannels = eventMatrix[0].size(); // Number of channel coefficients
+
+  vector<float> totalSolution(Nchannels,1.);
+  vector<float> iterSolution;
+  vector<vector<float> > myEventMatrix(eventMatrix);
+  vector<float> myEnergyVector(energyVector);
+
+  int i, j;
 
   // Iterate the correction
   for (int iter=1;iter<=nIter;iter++) 
     {
-      // make one iteration
-      solution = iterate(myEventMatrix, energyVector);
 
-      if (solution.empty()) return solution;
-      // R.O.: or throw an exception, what's the standard CMS way ?
+      // if normalization flag is set, normalize energies
+      float sumOverEnergy;
+      if (normalizeFlag)
+	{
+	  float scale = 0.;
+	  
+	  for (i=0; i<Nevents; i++)
+	    {
+	      sumOverEnergy = 0.;
+	      for (j=0; j<Nxtals; j++) {sumOverEnergy += myEventMatrix[i][j];}
+	      sumOverEnergy /= myEnergyVector[i];
+	      scale += sumOverEnergy;
+	    }
+	  scale /= Nevents;
+	  
+	  for (i=0; i<Nevents; i++) {myEnergyVector[i] *= scale;}	  
+	} // end normalize energies
+
+      // now the real work starts:
+      for (int iEvt=0; iEvt < Nevents; iEvt++)
+	{
+	  addEvent(myEventMatrix[iEvt], VmaxCeta[iEvt], VmaxCphi[iEvt], myEnergyVector[iEvt]);
+	}
+      iterSolution = getSolution();
+      if (iterSolution.empty()) return iterSolution;
 
       // re-calibrate eventMatrix with solution
-      for (int i=0; i<Nchannels; i++) 
+      for (int ievent = 0; ievent<Nevents; ievent++)
 	{
-	  for (int ievent = 0; ievent<Nevents; ievent++)
-	    {
-	      myEventMatrix[ievent][i] *= solution[i];
-	    }
-	  // save solution into theCalibVector
-	  theCalibVector[i] *= solution[i];
+	  myEventMatrix[ievent] = recalibrateEvent(myEventMatrix[ievent], VmaxCeta[ievent], VmaxCphi[ievent], iterSolution);
 	}
 
-    } // end iterate the correction
+      for (int i=0; i<Nchannels; i++) 
+	{
+	  // save solution into theCalibVector
+	  totalSolution[i] *= iterSolution[i];
+	}
+      //      resetSolution(); // reset for new iteration, now: getSolution does it automatically if not vetoed
+    } // end iterate correction
 
-  return theCalibVector;
+  return totalSolution;
 }
 
 
-vector<float> MinL3Algorithm::iterate(const vector<vector<float> >& eventMatrix, const vector<float>& energyVector)
+void MinL3Algorithm::addEvent(const vector<float>& eventSquare, const int& maxCeta, const int& maxCphi, const float& energy)
 {
-  vector<float> solution;
-  vector<float> myEnergyVector(energyVector);
+  countEvents++;
 
-  int Nevents = eventMatrix.size(); // Number of events to calibrate with
-  int Nchannels = eventMatrix[0].size(); // Number of channel coefficients
-
-  // Sanity check
-  if (Nevents != myEnergyVector.size()) 
+  float w, invsumXmatrix;
+  int iFull, i;
+  // Loop over the crystal matrix to find the sum
+  float sumXmatrix=0.;
+      
+  for (i=0; i<Nxtals; i++) { sumXmatrix+=eventSquare[i]; }
+      
+  if (sumXmatrix != 0.)
     {
-      cout << "MinL3Algorithm::iterate(): Error: bad matrix dimensions. Dropping out." << endl;
-      return solution; // empty vector !
-    }
+      invsumXmatrix = 1/sumXmatrix;
+      // Loop over the crystal matrix (3x3,5x5,7x7) again and calculate the weights for each xtal
+      for (i=0; i<Nxtals; i++) 
+	{		
+	  w = eventSquare[i] * invsumXmatrix;
 
-  // initialize the solution vector with 1.
-  solution.assign(Nchannels,1.);
-
-  int ievent, i, j;
-
-  // if normalization flag is set, normalize energies
-  float sumOverEnergy;
-  if (normaliseFlag)
-    {
-      float scale = 0.;
-      
-      cout << "MinL3Algorithm::iterate(): Normalising event data" << endl;
-
-      for (i=0; i<Nevents; i++)
-	{
-	  sumOverEnergy = 0.;
-	  for (j=0; j<Nchannels; j++) {sumOverEnergy += eventMatrix[i][j];}
-	  sumOverEnergy /= myEnergyVector[i];
-	  scale += sumOverEnergy;
-	}
-      scale /= Nevents;
-      cout << "  Normalisation = " << scale << endl;
-      
-      for (i=0; i<Nevents; i++) {myEnergyVector[i] *= scale;}	  
-    } // end normalize energies
-
-
-  // This is where the real work goes on...
-  float sum25, invsum25;
-  float w; // weight for event
-  vector<float> wsum(Nchannels,0.); // sum of weights for a crystal
-  vector<float> Ewsum(Nchannels,0.); // sum of products of weight*Etrue/E25
-
-  // Loop over events
-  for (ievent = 0; ievent<Nevents; ievent++)
-    {
-      // Loop over the 5x5 to find the sum
-      sum25=0.;
-      
-      for (i=0; i<Nchannels; i++) { sum25+=eventMatrix[ievent][i]; } //*calibs[i];
-      
-      if (sum25 != 0.)
-	{
-	  invsum25 = 1/sum25;
-	  // Loop over the 5x5 again and calculate the weights for each xtal
-	  for (i=0; i<Nchannels; i++) 
-	    {		
-	      w = eventMatrix[ievent][i] * invsum25; // * calibs[i]
-	      wsum[i] += w;
-	      Ewsum[i] += (w * myEnergyVector[ievent] * invsum25);	
+	  iFull = indexSqr2Reg(i, maxCeta, maxCphi);
+	  if (iFull >= 0)
+	    {
+	      wsum[iFull] += w;
+	      Ewsum[iFull] += (w * energy * invsumXmatrix);
 	    }
 	}
-      else {cout << " Debug: dropping null event: " << ievent << endl;}
-      
-    } // end Loop over events
-  
-  // Apply correction factors to all channels not in the margin
-  for (i=0; i<Nchannels; i++) 
+    }
+  //  else {cout << " Debug: dropping null event: " << countEvents << endl;}
+}
+
+
+vector<float> MinL3Algorithm::getSolution(bool resetsolution)
+{
+  vector<float> solution(Nchannels,1.);
+
+  for (int i=0; i<Nchannels; i++) 
     {
       if (wsum[i] != 0.) 
-	{ solution[i]*=Ewsum[i]/wsum[i]; }
-      else 
-	{ cout << "warning - no event data for crystal index " << i << endl; }
+	{ solution[i]*=Ewsum[i]/wsum[i];}
+      //      else 
+      //	{ cout << "warning - no event data for crystal index (reduced region) " << i << endl; }
     }
+  
+  if (resetsolution) resetSolution();
 
   return solution;
+}
+
+
+void MinL3Algorithm::resetSolution()
+{
+  wsum.assign(Nchannels,0.);
+  Ewsum.assign(Nchannels,0.);
+}
+
+
+vector<float> MinL3Algorithm::recalibrateEvent(const vector<float>& eventSquare, const int& maxCeta, const int& maxCphi, const vector<float>& recalibrateVector)
+{
+  vector<float> newEventSquare(eventSquare);
+  int iFull;
+
+  for (int i=0; i<Nxtals; i++) 
+    {
+      iFull = indexSqr2Reg(i, maxCeta, maxCphi);
+      if (iFull >=0)
+	newEventSquare[i] *= recalibrateVector[iFull];
+    }
+  return newEventSquare;
+}
+
+
+int MinL3Algorithm::indexSqr2Reg(const int& sqrIndex, const int& maxCeta, const int& maxCphi)
+{
+  int regionIndex;
+
+  // get the current eta, phi indices
+  int curr_eta = maxCeta - squareMode/2 + sqrIndex%squareMode;
+  if (curr_eta * maxCeta <= 0) {if (maxCeta > 0) curr_eta--; else curr_eta++; }  // JUMP over 0
+
+  int curr_phi = maxCphi - squareMode/2 + sqrIndex/squareMode;
+  if (curr_phi < 1) curr_phi += 360;
+  if (curr_phi > 360) curr_phi -= 360;
+
+  bool negPhiDirection = (maxphi < minphi);
+  int iFullphi;
+
+  regionIndex = -1;
+
+  if (curr_eta >= mineta && curr_eta <= maxeta)
+    if ( (!negPhiDirection && (curr_phi >= minphi && curr_phi <= maxphi)) ||
+	 (negPhiDirection && !(curr_phi >= minphi && curr_phi <= maxphi))      ) 
+      {
+	iFullphi = curr_phi - minphi;
+	if (iFullphi < 0) iFullphi += 360;
+	regionIndex = (curr_eta - mineta) * (maxphi - minphi + 1 + 360*negPhiDirection) + iFullphi;
+      }
+
+  return regionIndex;
 }
