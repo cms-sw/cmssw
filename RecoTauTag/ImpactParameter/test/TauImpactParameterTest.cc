f52f1be9cf7c399c5f334d1409b5b284b7c8bbbb
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2006-10-13 07:34:00 +0000

    *** empty log message ***

---
+++ RecoTauTag/ImpactParameter/test/TauImpactParameterTest.cc
@@ -0,0 +1,101 @@
+// -*- C++ -*-
+//
+// Package:    TauImpactParameterTest
+// Class:      TauImpactParameterTest
+//
+/**\class TauImpactParameterTest TauImpactParameterTest.cc RecoTauTag/ImpactParameter/test/TauImpactParameterTest.cc
+
+ Description: EDAnalyzer to show how to get the tau impact parameter
+ Implementation:
+
+*/
+//
+// Original Author:  Sami Lehti
+//
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/BTauReco/interface/TauImpactParameterInfo.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+class TauImpactParameterTest : public edm::EDAnalyzer {
+
+  public:
+        TauImpactParameterTest(const edm::ParameterSet&);
+        ~TauImpactParameterTest();
+
+        virtual void analyze(const edm::Event&, const edm::EventSetup&);
+        virtual void beginJob();
+        virtual void endJob();
+
+  private:
+        string jetTagSrc;
+};
+
+
+TauImpactParameterTest::TauImpactParameterTest(const edm::ParameterSet& iConfig){
+	jetTagSrc = iConfig.getParameter<string>("JetTagProd");
+}
+
+TauImpactParameterTest::~TauImpactParameterTest(){
+
+}
+
+void TauImpactParameterTest::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+
+	Handle<TauImpactParameterInfoCollection> tauHandle;
+	iEvent.getByLabel(jetTagSrc,tauHandle);
+
+	const TauImpactParameterInfoCollection & tauIpInfo = *(tauHandle.product());
+	cout << "Found " << tauIpInfo.size() << " Tau candidates" << endl;
+
+	TauImpactParameterInfoCollection::const_iterator iJet;
+	int i = 0;
+	for (iJet = tauIpInfo.begin(); iJet != tauIpInfo.end(); iJet++) {
+
+	    cout << "  Candidate " << i << endl;
+
+	    const TrackRefVector& tracks = iJet->getIsolatedTauTag()->selectedTracks();
+
+            RefVector<TrackCollection>::const_iterator iTrack;
+	    for (iTrack = tracks.begin(); iTrack != tracks.end(); iTrack++){
+
+                cout << "    track pt, eta " << (*iTrack)->pt() << " " 
+                                             << (*iTrack)->eta() << endl;
+cout << "check track sip " << (*iTrack)->d0() << endl;
+		const reco::TauImpactParameterTrackData* trackData = iJet->getTrackData(*iTrack);
+cout << "check loop 1" << endl;
+		if(! trackData == 0){
+		  Measurement1D tip = trackData->transverseIp;
+cout << "check loop 2" << endl;
+		  cout << "    track ip,sip,err " << tip.value()
+		       << " " 			<< tip.significance()
+                       << " "                     << tip.error() << endl; 
+		}else{
+		  cout << "    track data = 0! " << endl;
+		}
+	    }
+
+	    cout <<"    discriminator = "<< iJet->discriminator() <<endl;
+	    i++;
+	}
+}
+
+void TauImpactParameterTest::beginJob(){
+}
+
+void TauImpactParameterTest::endJob(){
+}
+
+//define this as a plug-in
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(TauImpactParameterTest)
