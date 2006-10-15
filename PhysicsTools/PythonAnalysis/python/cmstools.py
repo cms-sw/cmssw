4b6132584d19c47999be151db589f1adbfa424b6
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2006-10-15 20:57:26 +0000

    implemented new access method to event tree

--- PhysicsTools/PythonAnalysis/python/cmstools.py
+++ PhysicsTools/PythonAnalysis/python/cmstools.py
@@ -3,7 +3,9 @@
 benedikt.hegner@cern.ch
 
 """
-
+import re
+import ROOT
+import exceptions
 ### define tab completion
 try:
   import readline, cmscompleter
@@ -32,3 +34,77 @@ def all(container):
         yield container[entry]
     except:
       pass
+
+
+### auto branch types (Chris Jones)
+def createBranchBuffer(branch):
+    reColons = re.compile(r'::')
+    reCloseTemplate =re.compile(r'>')
+    reOpenTemplate =re.compile(r'<')
+    branchType = ROOT.branchToClass(branch)
+    buffer = eval ('ROOT.'+reColons.sub(".",reOpenTemplate.sub("(ROOT.",reCloseTemplate.sub(")",branchType.GetName())))+'()')
+    if( branch.GetName()[-1] != '.'):
+        branch.SetAddress(buffer)
+    else:
+        branch.SetAddress(ROOT.AddressOf(buffer))
+    return buffer
+
+class EventTree(object):
+      def __init__(self,ttree):
+          self._tree = ttree
+          self._usedBranches = dict()
+          self._index = -1
+      def branch(self,name):
+          # support for aliases
+          alias = self._tree.GetAlias(name)
+          if alias != '': name = alias 
+          # access the branch in ttree
+          if name in self._usedBranches:
+              return self._usedBranches[name]
+          self._usedBranches[name]=EventBranch(self,name)
+          return self._usedBranches[name]
+      def tree(self):
+          return self._tree
+      def index(self):
+          return self._index
+      def __setBranchIndicies(self):
+          for branch in self._usedBranches.itervalues():
+              branch.setIndex(self._index)
+      def __getitem__(self,key):
+          if key <0 or key > self._tree.GetEntries():
+              raise IndexError
+          self._index = key
+          self.__setBranchIndicies()
+      def __iter__(self):
+          for entry in xrange(self._tree.GetEntries()):
+              self._index = entry
+              self.__setBranchIndicies()
+              yield entry
+
+
+class EventBranch(object):
+    def __init__(self,parent,name):
+        self._branch = parent.tree().GetBranch(name)
+        if self._branch == None:
+            raise cmserror("Unknown branch "+name)
+        self._buffer = createBranchBuffer(self._branch)
+        self._index = parent.index()
+        self._readData = False
+    def setIndex(self,index):
+        self._index = index
+        self._readData = False
+    def __readData(self):
+        self._branch.GetEntry(self._index)
+        self._readData = True
+
+    # replace this by __getattr__ to allow branch.attr instead of branch().attr
+    def __call__(self):
+        if not self._readData:
+            self.__readData()
+        return self._buffer
+
+class cmserror(exceptions.StandardError):
+    def __init__(self, message):
+          print "========================================"
+          print "ERROR:", message
+          print "========================================"
