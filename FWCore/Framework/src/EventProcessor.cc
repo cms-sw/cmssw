d41e35177d4bd14f7ba4eca08b20ce4ae22fc7c2
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-10-16 22:55:41 +0000

    Turn on AdaptorConfig service for all cmsRun jobs

--- FWCore/Framework/src/EventProcessor.cc
+++ FWCore/Framework/src/EventProcessor.cc
@@ -603,6 +603,7 @@ namespace edm {
     adjustForDefaultService(*(pServiceSets.get()), "InitRootHandlers");
     adjustForDefaultService(*(pServiceSets.get()), "MessageLogger");
     adjustForDefaultService(*(pServiceSets.get()), "LoadAllDictionaries");
+    adjustForDefaultService(*(pServiceSets.get()), "AdaptorConfig");
     adjustForService(*(pServiceSets.get()), "JobReportService");
     adjustForService(*(pServiceSets.get()), "SiteLocalConfigService");
 
