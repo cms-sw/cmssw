65c1b5e1fb658a02bcf93f6f9da9bde1ab725f2c
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-10-17 16:08:31 +0000

    seal plugin

---
+++ RecoMuon/L2MuonSeedGenerator/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoMuon/L2MuonSeedGenerator/src/L2MuonSeedGenerator.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(L2MuonSeedGenerator)
