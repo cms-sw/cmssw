975379516d5c6f822de56afe292c0ad0a44fbd0f
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2006-10-18 15:29:56 +0000

    add PixelMatchGsfElectron class interface

---
+++ DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h
@@ -0,0 +1,155 @@
+#ifndef PixelMatchGsfElectron_h
+#define PixelMatchGsfElectron_h
+//**********************************************************
+// For the moment, this is just the interface definition
+// Implementation to come
+//**********************************************************
+//-------------------------------------------------------------------
+//
+// Package EgammaCandidates
+//
+/** \class PixelMatchGsfElectron
+*/
+//  adapted from the TRecElectron class in ORCA
+//
+// Author:
+//
+// Claude Charlot - CNRS & IN2P3, LLR Ecole polytechnique
+// Ursula Berthon - LLR Ecole polytechnique
+// 
+// $Log: PixelMatchGsfElectron.h,v $
+// initial version
+//
+//
+//-------------------------------------------------------------------
+
+#include "DataFormats/EgammaCandidates/interface/Electron.h"
+#include "DataFormats/TrackReco/interface/GsfTrack.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
+#include "DataFormats/Math/interface/Point3D.h"
+
+#include <vector>
+
+class TrajectoryStateOnSurface; 
+
+class ElectronMomentumCorrector;
+class ElectronEnergyCorrector;
+class ElectronClassification;
+
+namespace reco {
+
+class PixelMatchGsfElectron : public Electron {
+
+ public:
+  
+  PixelMatchGsfElectron(): Electron(){};
+  //  PixelMatchGsfElectron(const SuperCluster *, const GsfTrack *, 
+  PixelMatchGsfElectron(const GsfTrackRef gsft,
+	       const TrajectoryStateOnSurface& tssuper,
+			  const TrajectoryStateOnSurface& tsseed) :Electron(), gsfTrack_(gsft) {;} 
+
+  virtual ~PixelMatchGsfElectron(){};
+
+  //Public methods
+
+  // particle behaviour
+   /** The electron classification.
+      barrel  :   0: golden,  10: bigbrem,  20: narrow, 30-34: showering, 
+                (30: showering nbrem=0, 31: showering nbrem=1, 32: showering nbrem=2 ,33: showering nbrem=3, 34: showering nbrem>=4)
+                 40: crack
+      endcaps : 100: golden, 110: bigbrem, 120: narrow, 130-134: showering
+               (130: showering nbrem=0, 131: showering nbrem=1, 132: showering nbrem=2 ,133: showering nbrem=3, 134: showering nbrem>=4)
+   */
+  int classification() const {return electronClass_;}
+
+  // supercluster and electron track related quantities
+  //! the super cluster energy corrected by EnergyScaleFactor
+  float caloEnergy() const {return superCluster()->energy();}
+  //! the super cluster position
+  math::XYZPoint caloPosition() const {return superCluster()->position();}
+  //! the track momentum at vertex
+  math::XYZPoint trackMomentumAtVtx() const {return trackMomentumAtVtx_;}
+  //! the track impact point state position
+  math::XYZPoint TrackPositionAtVtx() const {return trackPositionAtVtx_;}
+  //! the track momentum extrapolated at the supercluster position
+  math::XYZPoint trackMomentumAtCalo() const {return trackMomentumAtCalo_;}
+  //! the track extrapolated position at min distance to the supercluster position
+  math::XYZPoint TrackPositionAtCalo() const {return trackPositionAtCalo_;}
+  //! the supercluster energy / track momentum at impact point
+  float eSuperClusterOverP() const {return eSuperClusterOverP_;}
+  //! the seed cluster energy / track momentum at calo from outermost state
+  float eSeedClusterOverPout() const {return eSeedClusterOverPout_;}
+  //! the supercluster eta - track eta from helix extrapolation from impact point
+  float deltaEtaSuperClusterTrackAtVtx() const {return deltaEtaSuperClusterAtVtx_;}
+  //! the seed cluster eta - track eta at calo from outermost state
+  float deltaEtaSeedClusterTrackAtCalo() const {return deltaEtaSeedClusterAtCalo_;}
+  //! the supercluster phi - track phi from helix extrapolation from impact point
+  float deltaPhiSuperClusterTrackAtVtx() const {return deltaPhiSuperClusterAtVtx_;}
+  //! the seed cluster phi - track phi at calo from outermost state
+  float deltaPhiSeedClusterTrackAtCalo() const {return deltaPhiSeedClusterAtCalo_;}
+
+  //! the hadronic over electromagnetic fraction
+  float hadronicOverEm() const {return hadOverEm_;}
+
+  // corrections
+  //! tell if class dependant escale correction have been applied
+  bool isEnergyScaleCorrected() const {return energyScaleCorrected_;}
+  //! tell if class dependant E-p combination has been determined
+  bool isMomentumCorrected() const {return momentumFromEpCombination_;}
+  //! handle electron energy correction.  Rescales 4 momentum from corrected 
+  //! energy value and sets momentumFromEpCombination_ to true
+  void correctElectronFourMomentum(const ElectronMomentumCorrector *thecorr) {;}
+  //! handle electron supercluster energy scale correction.  Propagates new 
+  //! energy value to all electron attributes and sets energyScaleCorrected_ to true
+  void correctElectronEnergyScale(const ElectronEnergyCorrector *thecorr) {;}
+  //! determine the class of the electron
+  void classifyElectron(const ElectronClassification *theclassifier) {;}
+
+  //! the errors on the supercluster energy and track momentum
+  float caloEnergyError() const {return energyError_;}
+  float trackMomentumError() const {return trackMomentumError_;}
+
+  //! get associated GsfTrack pointer
+  const GsfTrackRef getGsfTrack() const { return gsfTrack_; } 
+
+  //! number of related brem clusters
+  int numberOfBrems() const {return numberOfBrems_;}
+  //! array of pointers to the related brem clusters
+  BasicClusterRefVector getBremClusters() const {;}
+
+
+ private:
+
+  void setNumberOfBrems();
+
+  math::XYZPoint trackMomentumAtVtx_;
+  math::XYZPoint trackPositionAtVtx_;
+  math::XYZPoint trackMomentumAtCalo_;
+  math::XYZPoint trackPositionAtCalo_;
+
+  float energyError_;
+  float trackMomentumError_;
+
+  int electronClass_;
+
+  float eSuperClusterOverP_;
+  float eSeedClusterOverPout_;
+
+  float deltaEtaSuperClusterAtVtx_;
+  float deltaEtaSeedClusterAtCalo_;
+  float deltaPhiSuperClusterAtVtx_;
+  float deltaPhiSeedClusterAtCalo_;
+
+  float hadOverEm_;
+  int numberOfBrems_;
+
+  GsfTrackRef gsfTrack_;
+
+  bool energyScaleCorrected_;
+  bool momentumFromEpCombination_;
+
+};
+
+}
+#endif
