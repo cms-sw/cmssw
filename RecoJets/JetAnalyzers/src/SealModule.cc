d7d481a64bcddacbcf26d93ce9bf483fccef4a5e
Robert Harris <sha1-bef59cd0d27f2c81b33bddad0d65ff99d43c90a4@cern.ch>
2006-10-19 19:19:10 +0000

    Add module JetToDigiDump which does nested printout of jets, constituent calotowers, constituent rechits and digis

--- RecoJets/JetAnalyzers/src/SealModule.cc
+++ RecoJets/JetAnalyzers/src/SealModule.cc
@@ -4,9 +4,11 @@
 #include "RecoJets/JetAnalyzers/interface/JetAnalyzer.h"
 #include "RecoJets/JetAnalyzers/interface/JetValidation.h"
 #include "RecoJets/JetAnalyzers/interface/CaloTowersExample.h"
+#include "RecoJets/JetAnalyzers/interface/JetToDigiDump.h"
  
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( JetPlotsExample );
 DEFINE_ANOTHER_FWK_MODULE( JetAnalyzer );
 DEFINE_ANOTHER_FWK_MODULE( JetValidation );
 DEFINE_ANOTHER_FWK_MODULE( CaloTowersExample );
+DEFINE_ANOTHER_FWK_MODULE( JetToDigiDump );
