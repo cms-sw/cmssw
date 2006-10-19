6a0bab9c0503a82616a11eb6004884b8d67a2013
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2006-10-19 21:41:19 +0000

    inputTag instead of string

--- RecoMuon/CosmicMuonProducer/src/GlobalCosmicMuonProducer.h
+++ RecoMuon/CosmicMuonProducer/src/GlobalCosmicMuonProducer.h
@@ -6,12 +6,13 @@
  *  reconstruct muons using dt,csc,rpc and tracker starting from cosmic muon 
  *  tracks
  *
- *  $Date: $
- *  $Revision: $
+ *  $Date: 2006/09/20 00:09:09 $
+ *  $Revision: 1.1 $
  *  \author Chang Liu  -  Purdue University <Chang.Liu@cern.ch>
  */
 
 #include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
 
 class MuonTrackFinder;
 class MuonServiceProxy;
@@ -25,7 +26,7 @@ public:
   virtual void produce(edm::Event&, const edm::EventSetup&);
 
 private:
-  std::string theTrackCollectionLabel;
+  edm::InputTag theTrackCollectionLabel;
   MuonTrackFinder* theTrackFinder;
 
   /// the event setup proxy, it takes care the services update
