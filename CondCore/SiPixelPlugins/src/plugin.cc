950109fc5e1697438c3c85e6462bebfd87eda2af
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2006-10-20 09:15:36 +0000

    First release of Pixel gain calibration

--- CondCore/SiPixelPlugins/src/plugin.cc
+++ CondCore/SiPixelPlugins/src/plugin.cc
@@ -2,6 +2,9 @@
 
 #include "CondFormats/SiPixelObjects/interface/SiPixelFedCablingMap.h"
 #include "CondFormats/DataRecord/interface/SiPixelFedCablingMapRcd.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibration.h"
+#include "CondFormats/DataRecord/interface/SiPixelGainCalibrationRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(SiPixelFedCablingMapRcd,SiPixelFedCablingMap);
+REGISTER_PLUGIN(SiPixelGainCalibrationRcd,SiPixelGainCalibration);
