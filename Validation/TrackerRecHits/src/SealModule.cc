30ddc572329aa6ae7eafd7b83b3542f51f4675e6
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2006-10-24 10:21:41 +0000

    Add semicolon for SLC4

--- Validation/TrackerRecHits/src/SealModule.cc
+++ Validation/TrackerRecHits/src/SealModule.cc
@@ -4,5 +4,5 @@
 #include "Validation/TrackerRecHits/interface/SiPixelRecHitsValid.h"
 
 DEFINE_SEAL_MODULE ();
-DEFINE_ANOTHER_FWK_MODULE(SiStripRecHitsValid)
-DEFINE_ANOTHER_FWK_MODULE(SiPixelRecHitsValid)
+DEFINE_ANOTHER_FWK_MODULE(SiStripRecHitsValid);
+DEFINE_ANOTHER_FWK_MODULE(SiPixelRecHitsValid);
