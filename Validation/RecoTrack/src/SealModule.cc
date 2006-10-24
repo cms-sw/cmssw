eadd7933863c4c9d34e3a7d66334ac51fb719904
Arnaud Gay <sha1-ffc0f44b10535426b6e80e730966fc0636a397a8@cern.ch>
2006-10-24 08:57:25 +0000

    *** empty log message ***

--- Validation/RecoTrack/src/SealModule.cc
+++ Validation/RecoTrack/src/SealModule.cc
@@ -6,5 +6,5 @@
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MultiTrackValidator);
-DEFINE_ANOTHER_FWK_MODULE(SiStripTrackingRecHitsValid)
+DEFINE_ANOTHER_FWK_MODULE(SiStripTrackingRecHitsValid);
 
