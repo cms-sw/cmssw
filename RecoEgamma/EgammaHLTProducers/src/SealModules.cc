18fea4e3b3fb6ee47477e861868c3ca1f66d3615
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2006-10-26 20:54:39 +0000

    add electron track isolation

--- RecoEgamma/EgammaHLTProducers/src/SealModules.cc
+++ RecoEgamma/EgammaHLTProducers/src/SealModules.cc
@@ -3,10 +3,12 @@
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTEcalIsolationProducers.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTHcalIsolationProducers.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTPhotonTrackIsolationProducers.h"
+#include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTElectronTrackIsolationProducers.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTRecoEcalCandidateProducers.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTEcalIsolationProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTHcalIsolationProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTPhotonTrackIsolationProducers);
+DEFINE_ANOTHER_FWK_MODULE(EgammaHLTElectronTrackIsolationProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTRecoEcalCandidateProducers);
