117d7dbe62bf740054c5423efb203718a69d4ab7
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2006-10-26 20:51:11 +0000

    Electron Track Isolation

---
+++ RecoEgamma/EgammaHLTProducers/interface/EgammaHLTElectronTrackIsolationProducers.h
@@ -0,0 +1,58 @@
+// -*- C++ -*-
+//
+// Package:    EgammaHLTProducers
+// Class:      EgammaHLTElectronTrackIsolationProducers
+// 
+/**\class EgammaHLTElectronTrackIsolationProducers EgammaHLTElectronTrackIsolationProducers.cc RecoEgamma/EgammaHLTProducers/interface/EgammaHLTElectronTrackIsolationProducers.h
+*/
+//
+// Original Author:  Monica Vazquez Acosta (CERN)
+//
+// $Id: $
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoEgamma/EgammaHLTAlgos/interface/EgammaHLTTrackIsolation.h"
+
+//
+// class declaration
+//
+
+class EgammaHLTElectronTrackIsolationProducers : public edm::EDProducer {
+   public:
+      explicit EgammaHLTElectronTrackIsolationProducers(const edm::ParameterSet&);
+      ~EgammaHLTElectronTrackIsolationProducers();
+
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+
+  edm::InputTag electronProducer_;
+  edm::InputTag trackProducer_;
+
+  edm::ParameterSet conf_;
+
+  double egTrkIsoPtMin_; 
+  double egTrkIsoConeSize_;
+  double egTrkIsoZSpan_;   
+  double egTrkIsoRSpan_;  
+  double egTrkIsoVetoConeSize_;
+
+  EgammaHLTTrackIsolation* test_;
+
+};
+
