ac7648362c1025ece48a82a80ec1faa064e6e15c
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-10-30 21:07:50 +0000

    *** empty log message ***

--- OnlineDB/SiStripESSources/src/SealModule.cc
+++ OnlineDB/SiStripESSources/src/SealModule.cc
@@ -1,6 +1,6 @@
+#include "FWCore/Framework/interface/SourceFactory.h"
 #include "PluginManager/ModuleDef.h"
 DEFINE_SEAL_MODULE();
 
-#include "FWCore/Framework/interface/SourceFactory.h"
 #include "OnlineDB/SiStripESSources/interface/SiStripFedCablingBuilderFromDb.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripFedCablingBuilderFromDb);
