26a7264a3612cc215bd89f782f402dbab9edc888
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-10-30 21:29:41 +0000

    tidy up of interface

--- DQM/SiStripCommissioningSources/interface/SiStripCommissioningSource.h
+++ DQM/SiStripCommissioningSources/interface/SiStripCommissioningSource.h
@@ -1,12 +1,16 @@
 #ifndef DQM_SiStripCommissioningSources_SiStripCommissioningSource_H
 #define DQM_SiStripCommissioningSources_SiStripCommissioningSource_H
 
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h"
+#include "DataFormats/SiStripCommon/interface/SiStripHistoNamingScheme.h"
+#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
+#include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
 #include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
 #include "CalibFormats/SiStripObjects/interface/SiStripFecCabling.h"
-#include "DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h"
-#include "DQM/SiStripCommon/interface/SiStripHistoNamingScheme.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include <string>
+#include <vector>
 #include <map>
 
 class DaqMonitorBEInterface;
@@ -23,6 +27,8 @@ class SiStripCommissioningSource : public edm::EDAnalyzer {
   
   /** Map of task objects, identified through FedChanelId */
   typedef std::map<unsigned int, CommissioningTask*> TaskMap;
+  typedef std::vector<CommissioningTask*> VecOfTasks;
+  typedef std::vector<VecOfTasks> VecOfVecOfTasks;
   
   SiStripCommissioningSource( const edm::ParameterSet& );
   ~SiStripCommissioningSource();
@@ -42,35 +48,69 @@ class SiStripCommissioningSource : public edm::EDAnalyzer {
   /** */
   DaqMonitorBEInterface* const dqm( std::string method = "" ) const;
   
- private: // ----- data members -----
+ private: // ----- methods -----
 
-  std::string inputModuleLabel_;
+  void createCablingTasks();
+  void createTasks();
+
+  void clearCablingTasks();
+  void clearTasks();
+
+  void fillCablingHistos( const SiStripEventSummary* const,
+			  const edm::DetSetVector<SiStripRawDigi>& );
+  void fillHistos( const SiStripEventSummary* const,
+		   const edm::DetSetVector<SiStripRawDigi>& );
+  
+ private:
+
+  // ---------- DQM fwk and cabling ----------
+
   /** Interface to Data Quality Monitoring framework. */
   DaqMonitorBEInterface* dqm_;
-  /** Identifies commissioning task read from cfg file. */
-  std::string taskFromCfg_; 
-  /** Identifies commissioning task. */
-  sistrip::Task task_; 
-  /** Map of task objects, identified through FedChanKey. */
-  TaskMap tasks_;
-  /** */
-  int updateFreq_;
-
-  /** */
-  std::string filename_;
-
-  /** */
-  uint32_t run_;
-  /** */
-  bool createTask_;
 
   /** */
   SiStripFedCabling* fedCabling_;
+
   /** */
   SiStripFecCabling* fecCabling_;
-  /** */
+  
+  // ---------- Input / output ----------
+
+  /** Name of digi input module. */
+  std::string inputModuleLabel_;
+
+  /** Filename of output root file containing source histos. */
+  std::string filename_;
+
+  /** Run number used for naming of root file. */
+  uint32_t run_;
+
+  /** Record of time used to calculate event rate. */
+  uint32_t time_;
+
+  // ---------- Histogram-related ----------
+
+  /** Identifies commissioning task read from cfg file. */
+  std::string taskConfigurable_; 
+
+  /** Identifies commissioning task. */
+  sistrip::Task task_; 
+
+  /** Vector of vector of task objects (indexed using FED id.ch. */
+  VecOfVecOfTasks tasks_;
+
+  /** Map of cabling task objects (indexed using FEC key). */
+  TaskMap cablingTasks_;
+
+  /** Flag to indicate whether histo objects exist or not. */
+  bool tasksExist_;
+
+  /** Flag to indicate whether task is FED cabling or not. */
   bool cablingTask_;
   
+  /** Update frequency for histograms (ignored for cabling). */
+  int updateFreq_;
+
 };
 
 #endif // DQM_SiStripCommissioningSources_SiStripCommissioningSource_H
