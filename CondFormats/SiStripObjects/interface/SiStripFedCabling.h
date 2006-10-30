2a88a80e762dc419c201884a54a9a73cf4e6f67b
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-10-30 20:16:41 +0000

    added detected and undetected methods, added print method

--- CondFormats/SiStripObjects/interface/SiStripFedCabling.h
+++ CondFormats/SiStripObjects/interface/SiStripFedCabling.h
@@ -5,6 +5,12 @@
 #include <boost/cstdint.hpp>
 #include <vector>
 #include <string>
+#include <sstream>
+
+class SiStripFedCabling;
+
+/** Debug info for SiStripFedCabling class. */
+std::ostream& operator<< ( std::ostream&, const SiStripFedCabling& );
 
 /** 
     \class SiStripFedCabling 
@@ -17,23 +23,34 @@ class SiStripFedCabling {
   
   /** Constructor taking FED channel connection objects as input. */
   SiStripFedCabling( const std::vector<FedChannelConnection>& connections );
-  
+
   /** Public default constructor. */
   SiStripFedCabling();
-  
+
   /** Virtual destructor. */
   virtual ~SiStripFedCabling();
-
+  
   /** Builds FED cabling from vector of FED connections. */
   void buildFedCabling( const std::vector<FedChannelConnection>& connections );
-  
-  /** Active FEDs. */
+
+  /** Retrieve active FEDs. */
   const std::vector<uint16_t>& feds() const;
-  /** Connection info for FE devices connected to a given FED id and channel. */
+  
+  /** Connection info for devices connected to a given FED id and channel. */
   const FedChannelConnection& connection( uint16_t fed_id,
 					  uint16_t fed_chan ) const; 
-  /** Connection info for FE devices connected to a given FED. */
+
+  /** Connection info for devices connected to a given FED. */
   const std::vector<FedChannelConnection>& connections( uint16_t fed_id ) const; 
+  
+  /** Information for detected devices that are not connected. */
+  inline const std::vector<FedChannelConnection>& detected() const; 
+  
+  /** Information for undetected devices. */
+  inline const std::vector<FedChannelConnection>& undetected() const; 
+  
+  /** Prints all connection information for this FED cabling object. */
+  void print( std::stringstream& ) const;
   
  private:
   
@@ -65,10 +82,17 @@ class SiStripFedCabling {
   */
   std::vector<FedChannelConnection> undetected_;
 
-  /** */
-  static const std::string logCategory_;
-
 };
+
+// ---------- Inline methods ----------
+
+const std::vector<FedChannelConnection>& SiStripFedCabling::detected() const {
+  return detected_;
+} 
+
+const std::vector<FedChannelConnection>& SiStripFedCabling::undetected() const{
+  return undetected_;
+}
 
 #endif // CondFormats_SiStripObjects_SiStripFedCabling_H
 
