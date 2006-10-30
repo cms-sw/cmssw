55322fd61fd6e82fd7da2ae11f95a9909eb46c38
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-10-30 20:21:55 +0000

    changes concerning message logger, added operator<< method

--- CondFormats/SiStripObjects/src/FedChannelConnection.cc
+++ CondFormats/SiStripObjects/src/FedChannelConnection.cc
@@ -2,38 +2,10 @@
 #include "DataFormats/SiStripCommon/interface/SiStripConstants.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <iomanip>
+#include <sstream>
 
 using namespace std;
-
-// -----------------------------------------------------------------------------
-//
-ostream& operator<< ( ostream& os, const FedChannelConnection& conn ) {
-  return os << "[FedChannelConnection]" << endl
-	    << "  FedId/Ch: "
-	    << conn.fedId() << "/"
-	    << conn.fedCh() << endl
-	    << "  crate/FEC/CCU/Module/LLDchan/APV0/1: "
-	    << conn.fecCrate() << "/"
-	    << conn.fecSlot() << "/"
-	    << conn.fecRing() << "/"
-	    << conn.ccuAddr() << "/"
-	    << conn.ccuChan() << "/"
-	    << conn.lldChannel() << "/"
-	    << conn.i2cAddr(0) << "/"
-	    << conn.i2cAddr(1) << endl
-	    << "  DcuId/DetId/nPairs/pairNum: "
-	    << hex
-	    << "0x" << setfill('0') << setw(8) << conn.dcuId() << "/"
-	    << "0x" << setfill('0') << setw(8) << conn.detId() << "/"
-	    << dec
-	    << conn.nApvPairs() << "/"
-	    << conn.apvPairNumber() << endl
-	    << "  DCU/MUX/PLL/LLD found: "
-	    << conn.dcu() << "/"
-	    << conn.mux() << "/"
-	    << conn.pll() << "/"
-	    << conn.lld();
-}
+using namespace sistrip;
 
 // -----------------------------------------------------------------------------
 //
@@ -41,8 +13,9 @@ const uint16_t& FedChannelConnection::i2cAddr( const uint16_t& apv ) const {
   if      ( apv == 0 ) { return apv0_; }
   else if ( apv == 1 ) { return apv1_; }
   else {
-    edm::LogWarning(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::i2cAddr]"
-					 << " Unexpected APV I2C address!" << apv;
+    edm::LogWarning(mlCabling_)
+      << "[FedChannelConnection::" << __func__ << "]"
+      << " Unexpected APV I2C address!" << apv;
     static const uint16_t i2c_addr = 0;
     return i2c_addr;
   }
@@ -55,10 +28,11 @@ uint16_t FedChannelConnection::lldChannel() const {
   else if ( apv0_ == 34 || apv1_ == 35 ) { return 1; }
   else if ( apv0_ == 36 || apv1_ == 37 ) { return 2; }
   else {
-    edm::LogWarning(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::lldChannel]"
-					 << " Unexpected APV I2C addresses!" 
-					 << " Apv0: " << apv0_
-					 << " Apv1: " << apv1_;
+    edm::LogWarning(mlCabling_)
+      << "[FedChannelConnection::" << __func__ << "]"
+      << " Unexpected APV I2C addresses!" 
+      << " Apv0: " << apv0_
+      << " Apv1: " << apv1_;
   }
   return 0;
 }
@@ -70,28 +44,29 @@ uint16_t FedChannelConnection::apvPairNumber() const {
     if      ( apv0_ == 32 || apv1_ == 33 ) { return 0; }
     else if ( apv0_ == 36 || apv1_ == 37 ) { return 1; }
     else { 
-      edm::LogWarning(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::apvPairNumber]"
-					   << " Incompatible data!" 
-					   << " APV pairs: " << nApvPairs_
-					   << " APV0: " << apv0_
-					   << " APV1: " << apv1_;
+      edm::LogWarning(mlCabling_)
+	<< "[FedChannelConnection::" << __func__ << "]"
+	<< " Incompatible data!" 
+	<< " APV pairs: " << nApvPairs_
+	<< " APV0: " << apv0_
+	<< " APV1: " << apv1_;
     }
   } else if ( nApvPairs_ == 3 ) {
     if      ( apv0_ == 32 || apv1_ == 33 ) { return 0; }
     else if ( apv0_ == 34 || apv1_ == 35 ) { return 1; }
     else if ( apv0_ == 36 || apv1_ == 37 ) { return 2; }
     else { 
-      edm::LogWarning(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::apvPairNumber]"
-					   << " Incompatible data!" 
-					   << " APV pairs: " << nApvPairs_
-					   << " APV0: " << apv0_
-					   << " APV1: " << apv1_;
+      edm::LogWarning(mlCabling_)
+	<< "[FedChannelConnection::" << __func__ << "]"
+	<< " Incompatible data!" 
+	<< " APV pairs: " << nApvPairs_
+	<< " APV0: " << apv0_
+	<< " APV1: " << apv1_;
     }
-  } else if ( nApvPairs_ == 0 ) {
-    // edm::LogError(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::apvPairNumber] Zero APV pairs";
   } else {
-    edm::LogWarning(/* sistrip::mlCabling_ */ "") << "[FedChannelConnection::apvPairNumber]"
-					 << " Unexpected number of APV pairs: " << nApvPairs_;
+    edm::LogWarning(mlCabling_) 
+      << "[FedChannelConnection::" << __func__ << "]"
+      << " Unexpected number of APV pairs: " << nApvPairs_;
   }
   return 0;
 }
@@ -99,29 +74,38 @@ uint16_t FedChannelConnection::apvPairNumber() const {
 // -----------------------------------------------------------------------------
 /** */
 void FedChannelConnection::print( stringstream& ss ) const {
-  ss << "FedId/FedCh: "
+  ss << "  FedId/Ch: "
      << fedId() << "/"
-     << fedCh() 
-     << ", FecCrate/FecSlot/CcuAddr/CcuChan/APV0/APV1: "
+     << fedCh() << endl
+     << "  Crate/FEC/CCU/Module/LLDchan/APV0/1: "
      << fecCrate() << "/"
      << fecSlot() << "/"
      << fecRing() << "/"
      << ccuAddr() << "/"
      << ccuChan() << "/"
+     << lldChannel() << "/"
      << i2cAddr(0) << "/"
-     << i2cAddr(1) 
-     << ", DcuId/DetId/nPairs: "
+     << i2cAddr(1) << endl
+     << "  DcuId/DetId/nPairs/pairNum: "
      << hex
      << "0x" << setfill('0') << setw(8) << dcuId() << "/"
      << "0x" << setfill('0') << setw(8) << detId() << "/"
      << dec
-     << nApvPairs() 
-     << ", LldChan/PairNumber: "
-     << lldChannel() << "/"
-     << apvPairNumber() 
-     << ", DCU/MUX/PLL/LLD: "
+     << nApvPairs() << "/"
+     << apvPairNumber() << endl
+     << "  DCU/MUX/PLL/LLD found: "
      << dcu() << "/"
      << mux() << "/"
      << pll() << "/"
      << lld();
 }
+
+// -----------------------------------------------------------------------------
+//
+ostream& operator<< ( ostream& os, const FedChannelConnection& conn ) {
+  stringstream ss;
+  conn.print(ss);
+  os << ss.str();
+  return os;
+}
+
