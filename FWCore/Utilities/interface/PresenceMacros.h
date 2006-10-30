eacf972cd454d3ee0a5f0647453b6974978f2866
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-10-30 23:05:34 +0000

    Remove unnecessary conditional compilation

--- FWCore/Utilities/interface/PresenceMacros.h
+++ FWCore/Utilities/interface/PresenceMacros.h
@@ -5,24 +5,11 @@
 #include "FWCore/Utilities/interface/PresenceFactory.h"
 #include "FWCore/Utilities/interface/Presence.h"
 
-#if __GNUC_PREREQ (3,4)
-
 #define DEFINE_FWK_PRESENCE(type) \
   DEFINE_SEAL_MODULE (); \
   DEFINE_SEAL_PLUGIN (edm::PresenceFactory,type,#type)
 
 #define DEFINE_ANOTHER_FWK_PRESENCE(type) \
   DEFINE_SEAL_PLUGIN (edm::PresenceFactory,type,#type)
-
-#else
-
-#define DEFINE_FWK_PRESENCE(type) \
-  DEFINE_SEAL_MODULE (); \
-  DEFINE_SEAL_PLUGIN (edm::PresenceFactory,type,#type);
-
-#define DEFINE_ANOTHER_FWK_PRESENCE(type) \
-  DEFINE_SEAL_PLUGIN (edm::PresenceFactory,type,#type);
-
-#endif
 
 #endif
