7d12f3305c8070f1f302bf96433a28bec05b4752
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-11-02 11:34:05 +0000

    removed unneeded dictionaries

--- DataFormats/HepMCCandidate/src/classes.h
+++ DataFormats/HepMCCandidate/src/classes.h
@@ -4,12 +4,6 @@
 
 namespace {
   namespace {
-    // this is needed to fix a "missing dictionary" problem.
-    // see the following thread:
-    //   https://hypernews.cern.ch/HyperNews/CMS/get/physTools/63.html
-    edm::Wrapper<reco::HepMCCandidate> w1;
-
-    edm::Wrapper<reco::GenParticleCandidate> w2;
     reco::GenParticleCandidateCollection c3;
     reco::GenParticleCandidateRef r1;
     reco::GenParticleCandidateRefVector rv1;
