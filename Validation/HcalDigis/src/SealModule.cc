d3a862fccd1fbbe9e126363ee4f6f32389c4bf23
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-11-03 15:51:34 +0000

    Factored into its own file

---
+++ Validation/HcalDigis/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "Validation/HcalDigis/interface/HcalDigiTester.h"
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_ANOTHER_FWK_MODULE (HcalDigiTester) ;
+
