52c3f6fba16a089ffc7e568f2fe0426e5f3f7d31
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-11-03 15:52:00 +0000

    Uses new HcalSubdet monitors

--- Validation/HcalDigis/interface/HcalDigiTester.h
+++ Validation/HcalDigis/interface/HcalDigiTester.h
@@ -1,60 +1,18 @@
 //#ifndef HCALDIGITESTER_H
 //#define HCALDIGITESTER_H
 
-#include <memory>
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
 // user include files
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 
-#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
-#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
-#include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
-#include "DataFormats/HcalDetId/interface/HcalDetId.h"
-#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
-#include "DataFormats/HcalDigi/interface/HcalQIESample.h"
-
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
-#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
-#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
-#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
-
 #include "CalibFormats/HcalObjects/interface/HcalDbService.h"
-#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
-#include "CalibFormats/HcalObjects/interface/HcalCoderDb.h"
-#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
-
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
 #include "DQMServices/Daemon/interface/MonitorDaemon.h"
-
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
-#include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
-#include "DataFormats/HcalDigi/interface/HFDataFrame.h"
-#include "DataFormats/HcalDigi/interface/HODataFrame.h"
-
-
-#include <vector>
-#include <utility>
-#include <ostream>
-#include <string>
-#include <algorithm>
-#include <cmath>
-
-
-//
-// class declaration
-//
-
-using namespace cms;
-using namespace edm;
-using namespace std;
-
+#include <map>
+#include "Validation/HcalDigis/src/HcalSubdetDigiMonitor.h"
 
 class HcalDigiTester : public edm::EDAnalyzer {
 public:
@@ -62,60 +20,24 @@ public:
   ~HcalDigiTester();
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void beginJob(const edm::EventSetup&) ;
-  //  virtual void fill(const HBHEDataFrame& digi) const;
-  //  virtual void fill(const HODataFrame& digi) const;
-  // virtual void fill(const HFDataFrame& digi) const;
  template<class Digi>  void reco(const edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
  
 
  private:
+  // choose the correct subdet
+  HcalSubdetDigiMonitor * monitor();
 
   DaqMonitorBEInterface* dbe_;
   
-  string outputFile_;
-  string hcalselector_;
+  std::string outputFile_;
+  std::string hcalselector_;
   bool subpedvalue_;
 
-  //  HE Monitor Elements
-  MonitorElement* meEtaHE;
-  MonitorElement* mePhiHE;
-  MonitorElement* meDigiSimhitHE;
-  MonitorElement* meRatioDigiSimhitHE;
-  MonitorElement* meDigiSimhitHEprofile;
-  MonitorElement* menDigisHE;
-  MonitorElement* meSumDigisHE;
-  MonitorElement* meSumDigis_noise_HE;
+  edm::ESHandle<CaloGeometry> geometry ;
+  edm::ESHandle<HcalDbService> conditions;
+  float pedvalue;
 
- //  HE Monitor Elements
-  MonitorElement* meEtaHB;
-  MonitorElement* mePhiHB;
-  MonitorElement* meDigiSimhitHB;
-  MonitorElement* meRatioDigiSimhitHB;
-  MonitorElement* meDigiSimhitHBprofile;
-  MonitorElement* menDigisHB;
-  MonitorElement* meSumDigisHB;
-  MonitorElement* meSumDigis_noise_HB;
-
-  //   HF Monitor Elements
-  MonitorElement* meEtaHF;
-  MonitorElement* mePhiHF;
-  MonitorElement* meDigiSimhitHF;
-  MonitorElement* meRatioDigiSimhitHF;
-  MonitorElement* meDigiSimhitHFprofile;
-  MonitorElement* menDigisHF;
-  MonitorElement* meSumDigisHF;
-  MonitorElement* meSumDigis_noise_HF;
- //   HO Monitor Elements
-  MonitorElement* meEtaHO;
-  MonitorElement* mePhiHO;
-  MonitorElement* meDigiSimhitHO;
-  MonitorElement* meRatioDigiSimhitHO;
-  MonitorElement* meDigiSimhitHOprofile; 
-  MonitorElement* menDigisHO;
-  MonitorElement* meSumDigisHO;
-  MonitorElement* meSumDigis_noise_HO;
-ESHandle<CaloGeometry> geometry ;
-ESHandle<HcalDbService> conditions;
- float pedvalue;
+  std::map<std::string, HcalSubdetDigiMonitor*> monitors_;
 };
+
--- Validation/HcalDigis/src/HcalDigiTester.cc
+++ Validation/HcalDigis/src/HcalDigiTester.cc
@@ -1,4 +1,40 @@
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
 #include "Validation/HcalDigis/interface/HcalDigiTester.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+#include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/HcalDigi/interface/HcalQIESample.h"
+
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
+
+#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
+#include "CalibFormats/HcalObjects/interface/HcalCoderDb.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/Daemon/interface/MonitorDaemon.h"
+
+#include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HFDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HODataFrame.h"
+
+#include <vector>
+#include <utility>
+#include <ostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace cms;
+using namespace edm;
+using namespace std;
+
 
 
 
@@ -36,36 +72,66 @@ void HcalDigiTester::reco(const edm::Event& iEvent, const edm::EventSetup& iSetu
   if (hcalselector_ == "HO"  ) subdet = 3;
   if (hcalselector_ == "HF"  ) subdet = 4; 
 
-
-      for (ihbhe=hbhe->begin();ihbhe!=hbhe->end();ihbhe++)
+  for (ihbhe=hbhe->begin();ihbhe!=hbhe->end();ihbhe++)
 	{
 	  HcalDetId cell(ihbhe->id()); 
 	  if (cell.subdet()== subdet  ) 
 	    {
 	      const CaloCellGeometry* cellGeometry =
-		geometry->getSubdetectorGeometry (cell)->getGeometry (cell) ;
+          geometry->getSubdetectorGeometry (cell)->getGeometry (cell) ;
 	      double fEta = cellGeometry->getPosition ().eta () ;
 	      double fPhi = cellGeometry->getPosition ().phi () ;
-	      if (hcalselector_ == "HB"  ){ if (meEtaHB) meEtaHB->Fill(fEta) ; if (mePhiHB) mePhiHB->Fill(fPhi) ;}
-	      if (hcalselector_ == "HE"  ){ if (meEtaHE) meEtaHE->Fill(fEta) ; if (mePhiHE) mePhiHE->Fill(fPhi) ;}
-	      if (hcalselector_ == "HO"  ){ if (meEtaHO) meEtaHO->Fill(fEta) ; if (mePhiHO) mePhiHO->Fill(fPhi) ;}
-	      if (hcalselector_ == "HF"  ){ if (meEtaHF) meEtaHF->Fill(fEta) ; if (mePhiHF) mePhiHF->Fill(fPhi) ;}
+        monitor()->fillEta(fEta);
+        monitor()->fillPhi(fPhi);
 	      
 	      conditions->makeHcalCalibration(cell, &calibrations);
 	      const HcalQIECoder* channelCoder = conditions->getHcalCoder(cell);
 	      HcalCoderDb coder (*channelCoder, *shape);
 	      coder.adc2fC(*ihbhe,tool);
 	      
-
+        float fDigiSum = 0;
 	      for  (int ii=0;ii<tool.size();ii++)
-		{
-		  int capid = (*ihbhe)[ii].capid();
-		  if (subpedvalue_) fAdcSum+=(tool[ii]-calibrations.pedestal(capid));
-		  if (!subpedvalue_) fAdcSum+=(tool[ii] - pedvalue);
-		}
+        {
+          int capid = (*ihbhe)[ii].capid();
+          if (subpedvalue_) fDigiSum+=(tool[ii]-calibrations.pedestal(capid));
+          if (!subpedvalue_) fDigiSum+=(tool[ii] - pedvalue);
+        }
+        fAdcSum += fDigiSum;
+
+        monitor()->fillPedestal((*ihbhe)[0]);
+        monitor()->fillPedestal((*ihbhe)[1]);
+ 
+        if(fDigiSum > 50.)
+        {
+          // now do a few selected individual bins, if it's big enough
+          float fBin4  = tool[3];
+          float fBin56 = tool[4] + tool[5];
+
+          if(subpedvalue_)
+          {
+             fBin4 -= calibrations.pedestal((*ihbhe)[3].capid());
+             fBin56 -= (calibrations.pedestal((*ihbhe)[0].capid())
+                      + calibrations.pedestal((*ihbhe)[1].capid()));
+          }
+          else 
+          {
+            fBin4 -= pedvalue;
+            fBin56 -= 2*pedvalue;
+          }
+
+          //fBin12 is a pedestal, others are percentages
+          if(fDigiSum > 0)
+          {
+            fBin4 /= fDigiSum;
+            fBin56 /= fDigiSum;
+          }
+
+          monitor()->fillBin4Frac(fBin4);
+          monitor()->fillBin56Frac(fBin56);
+        }
 	      ndigis++;
 	    }
-	}
+    }
 
         
     
@@ -77,205 +143,42 @@ void HcalDigiTester::reco(const edm::Event& iEvent, const edm::EventSetup& iSetu
       
       float fEnergySimHits = 0; 
       for (std::vector<PCaloHit>::const_iterator simhits = simhitResult->begin () ;
-	   simhits != simhitResult->end () ;
-	   ++simhits)
-	{    
-	  HcalDetId detId(simhits->id());
-	  //  1 == HB
-	  if (detId.subdet()== subdet  ){  fEnergySimHits += simhits->energy(); }
-	}
-
-      if (hcalselector_ == "HB"  ) {
-	if (meDigiSimhitHB) meDigiSimhitHB->Fill( fEnergySimHits, fAdcSum);
-	if (meRatioDigiSimhitHB) meRatioDigiSimhitHB->Fill(fAdcSum/fEnergySimHits);
-	if (meDigiSimhitHBprofile) meDigiSimhitHBprofile->Fill( fEnergySimHits, fAdcSum);
-	if (meSumDigisHB) meSumDigisHB->Fill(fAdcSum);
-	if (menDigisHB) menDigisHB->Fill(ndigis);
-	if (meSumDigis_noise_HB) meSumDigis_noise_HB->Fill(fAdcSum);
-      }
-      if (hcalselector_ == "HE"  ) {
-	if (meDigiSimhitHE) meDigiSimhitHE->Fill( fEnergySimHits, fAdcSum);
-	if (meRatioDigiSimhitHE) meRatioDigiSimhitHE->Fill(fAdcSum/fEnergySimHits);
-	if (meDigiSimhitHEprofile) meDigiSimhitHEprofile->Fill( fEnergySimHits, fAdcSum);
-	if (meSumDigisHE) meSumDigisHE->Fill(fAdcSum);
-	if (menDigisHE) menDigisHE->Fill(ndigis);
-	if (meSumDigis_noise_HE) meSumDigis_noise_HE->Fill(fAdcSum);
-      }
-      if (hcalselector_ == "HF"  ) {
-	if (meDigiSimhitHF) meDigiSimhitHF->Fill( fEnergySimHits, fAdcSum);
-	if (meRatioDigiSimhitHF) meRatioDigiSimhitHF->Fill(fAdcSum/fEnergySimHits);
-	if (meDigiSimhitHFprofile) meDigiSimhitHFprofile->Fill( fEnergySimHits, fAdcSum);
-	if (meSumDigisHF) meSumDigisHF->Fill(fAdcSum);
-	if (menDigisHF) menDigisHF->Fill(ndigis);
-	if (meSumDigis_noise_HF) meSumDigis_noise_HF->Fill(fAdcSum);
-      }
-      if (hcalselector_ == "HO"  ) {
-	if (meDigiSimhitHO) meDigiSimhitHO->Fill( fEnergySimHits, fAdcSum);
-	if (meRatioDigiSimhitHO) meRatioDigiSimhitHO->Fill(fAdcSum/fEnergySimHits);
-	if (meDigiSimhitHOprofile) meDigiSimhitHOprofile->Fill( fEnergySimHits, fAdcSum);
-	if (meSumDigisHO) meSumDigisHO->Fill(fAdcSum);
-	if (menDigisHO) menDigisHO->Fill(ndigis);
-	if (meSumDigis_noise_HO) meSumDigis_noise_HO->Fill(fAdcSum);
+       simhits != simhitResult->end () ;
+       ++simhits)
+      {    
+	      HcalDetId detId(simhits->id());
+        //  1 == HB
+        if (detId.subdet()== subdet  ){  
+          fEnergySimHits += simhits->energy(); 
+        }
       }
 
-      ndigis=0;
+  monitor()->fillDigiSimhit(fEnergySimHits, fAdcSum);
+  monitor()->fillRatioDigiSimhit(fAdcSum/fEnergySimHits);
+  monitor()->fillDigiSimhitProfile(fEnergySimHits, fAdcSum);
+  monitor()->fillSumDigis(fAdcSum);
+  monitor()->fillSumDigis_noise(fAdcSum);
+  monitor()->fillNDigis(ndigis);
 }
 
 
 
 HcalDigiTester::HcalDigiTester(const edm::ParameterSet& iConfig)
+: dbe_(edm::Service<DaqMonitorBEInterface>().operator->()),
+  outputFile_(iConfig.getUntrackedParameter<string>("outputFile", "")),
+  hcalselector_(iConfig.getUntrackedParameter<string>("hcalselector", "all")),
+  subpedvalue_(iConfig.getUntrackedParameter<bool>("subpedvalue", "true")),
+  monitors_()
 {
-  // DQM ROOT output
-  outputFile_ = iConfig.getUntrackedParameter<string>("outputFile", "");
-
   if ( outputFile_.size() != 0 ) {
-    LogInfo("OutputInfo") << " Hcal Digi Task histograms will be saved to '" << outputFile_.c_str() << "'";
+    edm::LogInfo("OutputInfo") << " Hcal Digi Task histograms will be saved to '" << outputFile_.c_str() << "'";
   } else {
     LogInfo("OutputInfo") << " Hcal Digi Task histograms will NOT be saved";
   }
-  
-  dbe_ = 0;
 
-  // get hold of back-end interface
-  dbe_ = Service<DaqMonitorBEInterface>().operator->();
-  
-  
-  meEtaHE = 0;
-  mePhiHE = 0;
-
- 
-  meEtaHB = 0;
-  mePhiHB = 0;
- 
-  meEtaHF = 0;
-  mePhiHF = 0;
-
-  meEtaHO = 0;
-  mePhiHO = 0;
-
-  meDigiSimhitHB = 0;
-  meDigiSimhitHF = 0;
-  meDigiSimhitHE = 0;
-  meDigiSimhitHO = 0;
-
-
-  meDigiSimhitHBprofile = 0;
-  meDigiSimhitHEprofile = 0;
-  meDigiSimhitHFprofile = 0;
-  meDigiSimhitHOprofile = 0;
-
-  meRatioDigiSimhitHB = 0;
-  meRatioDigiSimhitHE = 0;
-  meRatioDigiSimhitHF = 0;
-  meRatioDigiSimhitHO = 0;
-
-  meSumDigisHB = 0;
-  meSumDigisHE = 0;
-  meSumDigisHF = 0;
-  meSumDigisHO = 0;
-
-  Char_t histo[20];
- 
-  hcalselector_ = iConfig.getUntrackedParameter<string>("hcalselector", "all");
-  subpedvalue_ = iConfig.getUntrackedParameter<bool>("subpedvalue", "true");
 
  if ( dbe_ ) {
    dbe_->setCurrentFolder("HcalDigiTask");
-   
-   if (hcalselector_ == "HE" || hcalselector_ == "noise" ) {
-   sprintf (histo, "HcalDigiTask_Eta_of_digis_HE" ) ;
-   meEtaHE = dbe_->book1D(histo, histo, 60 , -3. , 3.);
-   sprintf (histo, "HcalDigiTask_Phi_of_digis_HE" ) ;
-   mePhiHE = dbe_->book1D(histo, histo, 36 , -3.14159, 3.14159);
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HE");
-   meDigiSimhitHE = dbe_->book2D(histo, histo, 50, 0.,1.,  8000, 0., 800.);
-
-   sprintf (histo, "HcalDigiTask_Ratio_energy_digis_vs_simhits_HE");
-   meRatioDigiSimhitHE = dbe_->book1D(histo, histo,  200, 0., 1000.);   
-
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HE(profile)");
-   meDigiSimhitHEprofile = dbe_->bookProfile(histo, histo, 50, 0.,1.,  8000, 0., 800.);
-   sprintf (histo, "HcalDigiTask_number_of_digis_HE");
-   menDigisHE = dbe_->book1D(histo, histo,  40, 0., 200.);  
-
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_HE");
-   meSumDigisHE = dbe_->book1D(histo, histo,  100, 0., 800.);  
-
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_noise_HE");
-   meSumDigis_noise_HE = dbe_->book1D(histo, histo,  50, -500., 500.);  
-   }
-
-   if (hcalselector_ == "HB" || hcalselector_ == "noise"  ) {
-   sprintf (histo, "HcalDigiTask_Eta_of_digis_HB" ) ;
-   meEtaHB = dbe_->book1D(histo, histo, 40, -1.74 , 1.74);
-   sprintf (histo, "HcalDigiTask_Phi_of_digis_HB" ) ;
-   mePhiHB = dbe_->book1D(histo, histo, 72, -3.14159, 3.14159);
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HB");
-   meDigiSimhitHB = dbe_->book2D(histo, histo, 50, 0.,1.5,  8000, 0., 800.);
-
-   sprintf (histo, "HcalDigiTask_Ratio_energy_digis_vs_simhits_HB");
-   meRatioDigiSimhitHB = dbe_->book1D(histo, histo,  200, 0., 1000.);   
-
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HB(profile)");
-   meDigiSimhitHBprofile = dbe_->bookProfile(histo, histo, 50, 0.,1.5,  8000, 0., 800.); 
-
-   sprintf (histo, "HcalDigiTask_number_of_digis_HB");
-   menDigisHB = dbe_->book1D(histo, histo,  40 , 0, 80);  
-   
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_HB");
-   meSumDigisHB = dbe_->book1D(histo, histo,  100, 0., 800.);  
-
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_noise_HB");
-   meSumDigis_noise_HB = dbe_->book1D(histo, histo,  50, -500., 500.);  
-   }
-
-   if (hcalselector_ == "HF" || hcalselector_ == "noise"  ) {
-   sprintf (histo, "HcalDigiTask_Eta_of_digis_HF" ) ;
-   meEtaHF = dbe_->book1D(histo, histo, 100, -5. , 5.);
-   sprintf (histo, "HcalDigiTask_Phi_of_digis_HF" ) ;
-   mePhiHF = dbe_->book1D(histo, histo, 36, -3.14159, 3.14159);
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HF");
-   meDigiSimhitHF = dbe_->book2D(histo, histo, 30, 0.,60.,  3500, 0., 350.);
-
-   sprintf (histo, "HcalDigiTask_Ratio_energy_digis_vs_simhits_HF");
-   meRatioDigiSimhitHF = dbe_->book1D(histo, histo,  40, 3., 7.);   
-
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HF(profile)");
-   meDigiSimhitHFprofile = dbe_->bookProfile(histo, histo, 30, 0.,60.,  3500, 0., 350.);
-   sprintf (histo, "HcalDigiTask_number_of_digis_HF");
-   menDigisHF = dbe_->book1D(histo, histo,  20, 0., 20.);  
-
-  sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_HF");
-   meSumDigisHF = dbe_->book1D(histo, histo,  100, 0., 350.); 
-
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_noise_HF");
-   meSumDigis_noise_HF = dbe_->book1D(histo, histo,  50, -150., 150.);   
-   }  
-
-   if (hcalselector_ == "HO" || hcalselector_ == "noise"  ) {
-   sprintf (histo, "HcalDigiTask_Eta_of_digis_HO" ) ;
-   meEtaHO = dbe_->book1D(histo, histo, 40, -1.74 , 1.74);
-   sprintf (histo, "HcalDigiTask_Phi_of_digis_HO" ) ;
-   mePhiHO = dbe_->book1D(histo, histo, 72, -3.14159, 3.14159);
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HO");
-   meDigiSimhitHO = dbe_->book2D(histo, histo, 50, 0.,0.2,  1500, 0., 150.);
-
-   sprintf (histo, "HcalDigiTask_Ratio_energy_digis_vs_simhits_HO");
-   meRatioDigiSimhitHO = dbe_->book1D(histo, histo,  140, 0., 1400.);   
-
-   sprintf (histo, "HcalDigiTask_energy_digis_vs_simhits_HO(profile)");
-   meDigiSimhitHOprofile = dbe_->bookProfile(histo, histo, 50, 0.,0.2,  1500, 0., 150.);
-   sprintf (histo, "HcalDigiTask_number_of_digis_HO");
-   menDigisHO = dbe_->book1D(histo, histo,  50, 0., 50.);  
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_HO");
-   meSumDigisHO = dbe_->book1D(histo, histo,  100, 0., 150.);  
-  
-   sprintf (histo, "HcalDigiTask_sum_over_digis(fC)_noise_HO");
-   meSumDigis_noise_HO = dbe_->book1D(histo, histo,  50, -500., 500.);  
-   }  
-
- 
-    
  }
 }
    
@@ -295,15 +198,30 @@ void HcalDigiTester::endJob() {
   cout << " dbe_ = " << dbe_ << endl; 
  if ( outputFile_.size() != 0 && dbe_ ) dbe_->save(outputFile_);
 }
+
 void HcalDigiTester::beginJob(const edm::EventSetup& c){
 
 }
 
 
+HcalSubdetDigiMonitor * HcalDigiTester::monitor()
+{
+  std::map<std::string, HcalSubdetDigiMonitor*>::iterator monitorItr
+   = monitors_.find(hcalselector_);
+
+  if(monitorItr == monitors_.end())
+  {
+    HcalSubdetDigiMonitor* m = new HcalSubdetDigiMonitor(dbe_, hcalselector_);
+    std::pair<std::string, HcalSubdetDigiMonitor*> mapElement(
+      hcalselector_, m);
+    monitorItr = monitors_.insert(mapElement).first;
+  }
+  return monitorItr->second;
+}
+
 void
 HcalDigiTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  using namespace edm;
   iSetup.get<IdealGeometryRecord>().get (geometry);
   iSetup.get<HcalDbRecord>().get(conditions);
   //  reco<HBHEDataFrame>(iEvent,iSetup);
@@ -331,6 +249,3 @@ HcalDigiTester::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 }
 
-
-DEFINE_SEAL_MODULE ();
-DEFINE_ANOTHER_FWK_MODULE (HcalDigiTester) ;
