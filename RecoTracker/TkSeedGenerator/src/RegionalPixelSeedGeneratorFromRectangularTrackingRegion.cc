0a5cc0b12dac0a54a335cc9425f217fe4e41ebca
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2006-11-06 13:31:40 +0000

    *** empty log message ***

--- RecoTracker/TkSeedGenerator/src/RegionalPixelSeedGeneratorFromRectangularTrackingRegion.cc
+++ RecoTracker/TkSeedGenerator/src/RegionalPixelSeedGeneratorFromRectangularTrackingRegion.cc
@@ -1,13 +1,13 @@
 //
-// Package:         RecoTracker/TkSeedGeneratorFromMuon
-// Class:           RegionalPixelSeedGeneratorFromMuon
+// Package:         RecoTracker/TkSeedGeneratorFromRectangularTrackingRegion
+// Class:           RegionalPixelSeedGeneratorFromRectangularTrackingRegion
 // 
 
 #include <iostream>
 #include <memory>
 #include <string>
 
-#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromMuon.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromRectangularTrackingRegion.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
 #include "FWCore/Framework/interface/Handle.h"
@@ -27,41 +27,32 @@
 using namespace std;
 using namespace reco;
 
-RegionalPixelSeedGeneratorFromMuon::RegionalPixelSeedGeneratorFromMuon(edm::ParameterSet const& conf) : 
+RegionalPixelSeedGeneratorFromRectangularTrackingRegion::RegionalPixelSeedGeneratorFromRectangularTrackingRegion(edm::ParameterSet const& conf) : 
   conf_(conf),combinatorialSeedGenerator(conf)
 {
-  edm::LogInfo ("RegionalPixelSeedGeneratorFromMuon")<<"Enter the RegionalPixelSeedGeneratorFromMuon";
+  edm::LogInfo ("RegionalPixelSeedGeneratorFromRectangularTrackingRegion")<<"Enter the RegionalPixelSeedGeneratorFromRectangularTrackingRegion";
   produces<TrajectorySeedCollection>();
 
   ptmin=conf_.getParameter<double>("ptMin");
   originradius=conf_.getParameter<double>("originRadius");
   halflength=conf_.getParameter<double>("originHalfLength");
-  vertexSrc=conf_.getParameter<string>("vertexSrc");
   deltaEta = conf_.getParameter<double>("deltaEtaRegion");
   deltaPhi = conf_.getParameter<double>("deltaPhiRegion");
-  jetSrc = conf_.getParameter<edm::InputTag>("MuonSrc");
+  regSrc = conf_.getParameter<edm::InputTag>("RegionSrc");
 
 }
 
 // Virtual destructor needed.
-RegionalPixelSeedGeneratorFromMuon::~RegionalPixelSeedGeneratorFromMuon() { }  
+RegionalPixelSeedGeneratorFromRectangularTrackingRegion::~RegionalPixelSeedGeneratorFromRectangularTrackingRegion() { }  
 
 // Functions that gets called by framework every event
-void RegionalPixelSeedGeneratorFromMuon::produce(edm::Event& e, const edm::EventSetup& es)
+void RegionalPixelSeedGeneratorFromRectangularTrackingRegion::produce(edm::Event& e, const edm::EventSetup& es)
 {
   // get Inputs
   edm::Handle<SiPixelRecHitCollection> pixelHits;
 
   edm::ESHandle<TrackerGeometry> tracker;
   es.get<TrackerDigiGeometryRecord>().get(tracker);
-  edm::Handle<reco::VertexCollection> vertices;
-  e.getByLabel(vertexSrc,vertices);
-  const reco::VertexCollection vertCollection = *(vertices.product());
-  reco::VertexCollection::const_iterator ci = vertCollection.begin();
-  if(vertCollection.size() == 0) return;
-  originz = ci->z();
-
-
 
   //
   // get the pixel Hits
@@ -75,31 +66,17 @@ void RegionalPixelSeedGeneratorFromMuon::produce(edm::Event& e, const edm::Event
   //
   
   //Get the jet direction
-  edm::Handle<MuonCollection> jets;
-  e.getByLabel(jetSrc, jets);
-
-  GlobalVector globalVector(0,0,1);
-
-  //    if(jets->size() > 0)
-  //    {
-     MuonCollection::const_iterator iJet = jets->begin();
-      for(;iJet != jets->end();iJet++)
+  typedef std::vector<RectangularEtaPhiTrackingRegion> RectangularEtaPhiTrackingRegionCollection;   
+  edm::Handle<RectangularEtaPhiTrackingRegionCollection> regions;
+  e.getByLabel(regSrc, regions);
+  
+  RectangularEtaPhiTrackingRegionCollection::const_iterator iReg = regions->begin();
+  for(;iReg != regions->end();iReg++)
 	{
 	  
-	  GlobalVector jetVector((iJet)->p4().x(),(iJet)->p4().y(),(iJet)->p4().z());
-	  globalVector = jetVector;
-	  
-	  
-	  RectangularEtaPhiTrackingRegion* etaphiRegion = new  RectangularEtaPhiTrackingRegion(globalVector,
-											       GlobalPoint(0,0,originz), 
-											       ptmin,
-											       originradius,
-											       halflength,
-											       deltaEta,
-											       deltaPhi);
-	  
+	  RectangularEtaPhiTrackingRegion* region = const_cast<RectangularEtaPhiTrackingRegion*>(&(*iReg));
 	  combinatorialSeedGenerator.init(*pixelHits,es);
-	  combinatorialSeedGenerator.run(*etaphiRegion,*output,es);
+	  combinatorialSeedGenerator.run(*region,*output,es);
 	  // write output to file
 	}
       //    }   
