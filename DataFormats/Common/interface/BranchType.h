61bec23aa0181083a01b827a816cd83e7aa3924e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-11-07 00:30:43 +0000

    Use branch type to dervie tree and branch names.

--- DataFormats/Common/interface/BranchType.h
+++ DataFormats/Common/interface/BranchType.h
@@ -6,21 +6,43 @@
   
 BranchType: The type of a Branch (Event, LuminosityBlock, or Run)
 
-$Id: BranchType.h,v 1.1 2006/11/04 00:35:22 wmtan Exp $
+$Id: BranchType.h,v 1.2 2006/11/04 07:16:17 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 namespace edm {
+  // Note: These enum values are used as subscripts for a fixed size array, so they must not change.
   enum BranchType {
     InEvent = 0,
-    InLumi,
-    InRun
+    InLumi = 1,
+    InRun = 2,
+    EndBranchType = 3
   };
 
   inline
+  std::string BranchTypeToString(BranchType const& branchType) {
+    std::string st = ((branchType == InEvent) ? "Event" : ((branchType == InRun) ? "Run" : "LuminosityBlock"));
+    return st;
+  }
+
+  inline
+  std::string BranchTypeToProductTreeName(BranchType const& branchType) {
+    return BranchTypeToString(branchType) + "s";
+  }
+
+  inline
+  std::string BranchTypeToMetaDataTreeName(BranchType const& branchType) {
+    return BranchTypeToString(branchType) + "MetaData";
+  }
+
+  inline
+  std::string BranchTypeToAuxiliaryBranchName(BranchType const& branchType) {
+    return BranchTypeToString(branchType) + "Aux";
+  }
+
+  inline
   std::ostream&
-  operator<<(std::ostream& os, BranchType const& p) {
-    std::string const type = ((p == InEvent) ? "Event" : ((p == InRun) ? "Run" : "LuminosityBlock"));
-    os << type;
+  operator<<(std::ostream& os, BranchType const& branchType) {
+    os << BranchTypeToString(branchType);
     return os;
   }
 
