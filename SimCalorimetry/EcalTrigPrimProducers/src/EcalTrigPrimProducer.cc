4469917a5234760d0486f2c5b212373a86dfa0bd
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2006-11-07 16:29:57 +0000

    add Protection for missing input data

--- SimCalorimetry/EcalTrigPrimProducers/src/EcalTrigPrimProducer.cc
+++ SimCalorimetry/EcalTrigPrimProducers/src/EcalTrigPrimProducer.cc
@@ -2,7 +2,7 @@
  *
  * EcalTrigPrimProducer produces a EcalTrigPrimDigiCollection
  * The barrel code does a detailed simulation
- * The code for the endcap is simulated in a rough way, due to missing strip geometry
+ * The code for the endcap is simulated in a rough way, due to missing  strip geometry
  *
  *
  * \author Ursula Berthon, Stephanie Baffioni,  LLR Palaiseau
@@ -25,14 +25,14 @@
 #include "DataFormats/Common/interface/Provenance.h"
 #include "DataFormats/Common/interface/BranchDescription.h"
 
-#include "SimCalorimetry/EcalTrigPrimProducers/interface/EcalTrigPrimProducer.h"
-#include "SimCalorimetry/EcalTrigPrimAlgos/interface/EcalTrigPrimFunctionalAlgo.h"
+#include  "SimCalorimetry/EcalTrigPrimProducers/interface/EcalTrigPrimProducer.h"
+#include  "SimCalorimetry/EcalTrigPrimAlgos/interface/EcalTrigPrimFunctionalAlgo.h"
 #include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
 #include "TFile.h"
 #include "TTree.h"
 
-  
-EcalTrigPrimProducer::EcalTrigPrimProducer(const edm::ParameterSet& iConfig)
+
+EcalTrigPrimProducer::EcalTrigPrimProducer(const edm::ParameterSet&  iConfig)
 {
   //register your products
   produces <EcalTrigPrimDigiCollection >();
@@ -45,7 +45,7 @@ EcalTrigPrimProducer::EcalTrigPrimProducer(const edm::ParameterSet& iConfig)
     histfile_=NULL;
     valTree_=NULL;
   }
-  
+
   label_= iConfig.getParameter<std::string>("Label");
   instanceNameEB_ = iConfig.getParameter<std::string>("InstanceEB");;
   instanceNameEE_ = iConfig.getParameter<std::string>("InstanceEE");;
@@ -54,7 +54,7 @@ EcalTrigPrimProducer::EcalTrigPrimProducer(const edm::ParameterSet& iConfig)
   ttfThreshHigh_=  iConfig.getParameter<double>("TTFHighEnergy");
   algo_=NULL;
   //FIXME: add configuration
-					      
+                   
 }
 
 void EcalTrigPrimProducer::beginJob(edm::EventSetup const& setup) {
@@ -65,34 +65,33 @@ void EcalTrigPrimProducer::beginJob(edm::EventSetup const& setup) {
     binOfMaximum_=0;
     edm::Service<edm::ConstProductRegistry> reg;
     // Loop over provenance of products in registry.
-    for (edm::ProductRegistry::ProductList::const_iterator it = reg->productList().begin();
-	 it != reg->productList().end(); ++it) {
+    for (edm::ProductRegistry::ProductList::const_iterator it =  reg->productList().begin();
+     it != reg->productList().end(); ++it) {
       edm::BranchDescription desc = it->second;
-      printf("Classname %s, label %s\n",desc.friendlyClassName().c_str(), desc.moduleLabel().c_str());
-      if (!desc.friendlyClassName().compare(0,18,"EBDataFramesSorted") & desc.moduleLabel()==label_ ) {
-	edm::ParameterSet result = getParameterSet(desc.psetID());
-	binOfMaximum_=result.getParameter<int>("binOfMaximum");
-	break;
+      if (!desc.friendlyClassName().compare(0,18,"EBDataFramesSorted")  & desc.moduleLabel()==label_ ) {
+    edm::ParameterSet result = getParameterSet(desc.psetID());
+    binOfMaximum_=result.getParameter<int>("binOfMaximum");
+    break;
       }
     }
   }
   catch(cms::Exception& e) {
     // segv in case product was found but not parameter..
-    edm::LogWarning("")<<"Could not find parameter binOfMaximum in product registry for EBDataFramesSorted, had to set binOfMaximum by Hand";
+    edm::LogWarning("")<<"Could not find parameter binOfMaximum in  product registry for EBDataFramesSorted, had to set binOfMaximum by  Hand";
     binOfMaximum_=6;
   }
   if (binOfMaximum_==0) {
-    edm::LogWarning("")<<"Could not find product registry of EBDataFramesSorted, had to set binOfMaximum by Hand";
+    edm::LogWarning("")<<"Could not find product registry of  EBDataFramesSorted, had to set binOfMaximum by Hand";
     binOfMaximum_=6;
   }
 
-  algo_ = new EcalTrigPrimFunctionalAlgo(setup, valTree_,binOfMaximum_,nrSamples_,ttfThreshLow_,ttfThreshHigh_);
-  edm::LogInfo("constructor") <<"EcalTrigPrimProducer will write: "<<nrSamples_<<" samples for each digi,  binOfMaximum used: "<<binOfMaximum_;
+  algo_ = new EcalTrigPrimFunctionalAlgo(setup,  valTree_,binOfMaximum_,nrSamples_,ttfThreshLow_,ttfThreshHigh_);
+  edm::LogInfo("constructor") <<"EcalTrigPrimProducer will write:  "<<nrSamples_<<" samples for each digi,  binOfMaximum used:  "<<binOfMaximum_;
 }
 
 EcalTrigPrimProducer::~EcalTrigPrimProducer()
 {
- 
+
   // do anything here that needs to be done at desctruction time
   // (e.g. close files, deallocate resources etc.)
   delete algo_;
@@ -106,30 +105,40 @@ EcalTrigPrimProducer::~EcalTrigPrimProducer()
 
 // ------------ method called to produce the data  ------------
 void
-EcalTrigPrimProducer::produce(edm::Event& e, const edm::EventSetup& iSetup)
+EcalTrigPrimProducer::produce(edm::Event& e, const edm::EventSetup&  iSetup)
 {
 
   edm::Handle<EBDigiCollection> ebDigis;
-  e.getByLabel(label_,instanceNameEB_,ebDigis);
   edm::Handle<EEDigiCollection> eeDigis;
-  e.getByLabel(label_,instanceNameEE_,eeDigis);
+  bool barrel=true, endcap=true;
+  try{e.getByLabel(label_,instanceNameEB_,ebDigis);}
+  catch(cms::Exception &e) {
+    barrel=false;
+    edm::LogWarning("produce") <<" Couldnt find Barrel dataframes";
+  }
+  try{e.getByLabel(label_,instanceNameEE_,eeDigis);}
+  catch(cms::Exception &e) {
+    endcap=false;
+    edm::LogWarning("produce") <<" Couldnt find Endcap dataframes";
+  }
 
-  LogDebug("Startproduce") <<" =================> Treating event "<<e.id()<<", Number of EBDFataFrames "<<ebDigis.product()->size() ;
-  std::auto_ptr<EcalTrigPrimDigiCollection> pOut(new EcalTrigPrimDigiCollection);
-  
+  LogDebug("Startproduce") <<" =================> Treating event  "<<e.id()<<", Number of EBDFataFrames "<<ebDigis.product()->size() ;
+  std::auto_ptr<EcalTrigPrimDigiCollection> pOut(new  EcalTrigPrimDigiCollection);
 
-  // invoke algorithm  //FIXME: better separation 
-  //   algo_->setupES(iSetup);
-  algo_->run(ebDigis.product(),eeDigis.product(),*pOut, fgvbMinEnergy_);
+
+  // invoke algorithm  //FIXME: better separation
+  const EBDigiCollection *ebdc=NULL;
+  const EEDigiCollection *eedc=NULL;
+  if (barrel) ebdc=ebDigis.product();
+  if (endcap) eedc=eeDigis.product();
+  algo_->run(ebdc,eedc,*pOut, fgvbMinEnergy_);
   for (unsigned int i=0;i<pOut->size();++i) {
     for (int isam=0;isam<(*pOut)[i].size();++isam) {
-      if ((*pOut)[i][isam].raw()) LogDebug("Produced for ") <<" Tower "<<i<<", sample "<<isam<<", value "<<(*pOut)[i][isam].raw();
+      if ((*pOut)[i][isam].raw()) LogDebug("Produced for ") <<" Tower  "<<i<<", sample "<<isam<<", value "<<(*pOut)[i][isam].raw();
     }
   }
-  edm::LogInfo("produce") <<"For Barrel + Endcap, "<<pOut->size()<<" TP Digis were produced";
-	
+  edm::LogInfo("produce") <<"For Barrel + Endcap, "<<pOut->size()<<" TP  Digis were produced";
+    
   // put result into the Event
   e.put(pOut);
 }
-
-
