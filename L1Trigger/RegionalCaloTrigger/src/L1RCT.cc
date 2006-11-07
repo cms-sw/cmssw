ab279929b221f6db0c0bbbe3a1db0c3f3ee23745
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2006-11-07 18:27:12 +0000

    Changed L1RCT::calcCard() algorithm

--- L1Trigger/RegionalCaloTrigger/src/L1RCT.cc
+++ L1Trigger/RegionalCaloTrigger/src/L1RCT.cc
@@ -611,7 +611,8 @@ unsigned short L1RCT::calcCard(unsigned short rct_iphi, unsigned short absIeta){
   unsigned short card = 999;
   // Note absIeta counts from 1-32 (not 0-31)
   if (absIeta <= 24){
-    card =  ((absIeta-1)/8) + ((rct_iphi / 4) % 2) * 3;
+    // card =  ((absIeta-1)/8) + ((rct_iphi / 4) % 2) * 3;  // Sridhara's line
+    card =  ((absIeta-1)/8)*2 + regionPhi; // Jessica's line
   }
   // 25 <= absIeta <= 28 (card 6)
   else if ((absIeta >= 25) && (absIeta <= 28)){
