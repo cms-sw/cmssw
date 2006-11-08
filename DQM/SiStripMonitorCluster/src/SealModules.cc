c42dcb8890beb37510bbb158f09558e7a58f905f
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2006-11-08 16:51:51 +0000

    clean up filter monitoring

--- DQM/SiStripMonitorCluster/src/SealModules.cc
+++ DQM/SiStripMonitorCluster/src/SealModules.cc
@@ -1,6 +1,7 @@
 #include "PluginManager/ModuleDef.h"
 #include "DQM/SiStripMonitorCluster/interface/SiStripMonitorCluster.h"
 #include "DQM/SiStripMonitorCluster/interface/SiStripMonitorHLT.h"
+#include "DQM/SiStripMonitorCluster/interface/SiStripMonitorFilter.h"
 #include "DQM/SiStripMonitorCluster/interface/MonitorLTC.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
@@ -9,4 +10,5 @@
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripMonitorCluster);
 DEFINE_ANOTHER_FWK_MODULE(SiStripMonitorHLT);
+DEFINE_ANOTHER_FWK_MODULE(SiStripMonitorFilter);
 DEFINE_ANOTHER_FWK_MODULE(MonitorLTC);
---
+++ DQM/SiStripMonitorCluster/src/SiStripMonitorFilter.cc
@@ -0,0 +1,51 @@
+// -*- C++ -*-
+//
+// Package:    SiStripMonitorCluster
+// Class:      SiStripMonitorFilter
+// 
+//class SiStripMonitorFilter SiStripMonitorFilter.cc DQM/SiStripMonitorCluster/src/SiStripMonitorFilter.cc
+#include <vector>
+
+#include <numeric>
+#include <iostream>
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "DQM/SiStripMonitorCluster/interface/SiStripMonitorFilter.h"
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+
+using namespace edm;
+
+SiStripMonitorFilter::SiStripMonitorFilter(const edm::ParameterSet& iConfig)
+{
+  FilterDirectory="FilterResults";
+  dbe_  = Service<DaqMonitorBEInterface>().operator->();
+  conf_ = iConfig;
+}
+
+void SiStripMonitorFilter::beginJob(const edm::EventSetup& es){
+  dbe_->setCurrentFolder(FilterDirectory);
+  std::string FilterProducer = conf_.getParameter<std::string>("FilterProducer");
+  FilterDecision = dbe_->book1D(FilterProducer+"_FilterDecision", FilterProducer+"FilterDecision", 2, -0.5, 1.5);
+}
+
+void SiStripMonitorFilter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  // get from event
+  std::string FilterProducer = conf_.getParameter<std::string>("FilterProducer");
+  Handle<int> filter_decision; iEvent.getByLabel(FilterProducer, "", filter_decision); // filter decision
+  // trigger decision
+  FilterDecision->Fill(*filter_decision);
+}
+
+void SiStripMonitorFilter::endJob(void){
+  LogInfo("DQM|SiStripMonitorFilter")<<"Events rejected/accepted "<<FilterDecision->getBinContent(1)<<"/"<<FilterDecision->getBinContent(2);
+  bool outputMEsInRootFile = conf_.getParameter<bool>("OutputMEsInRootFile");
+  std::string outputFileName = conf_.getParameter<std::string>("OutputFileName");
+  if(outputMEsInRootFile){
+    dbe_->save(outputFileName);
+  }
+}
+
