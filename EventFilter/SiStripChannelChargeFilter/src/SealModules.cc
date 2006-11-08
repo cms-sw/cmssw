631aee3cf9035eede987aaaa6d8f0ec5a4d138ad
sfricke <sha1-a3e9314907daa7d8818c7915e5e43f36adf46966@cern.ch>
2006-11-08 17:56:20 +0000

    add TEC Clusters filter

--- EventFilter/SiStripChannelChargeFilter/src/SealModules.cc
+++ EventFilter/SiStripChannelChargeFilter/src/SealModules.cc
@@ -5,15 +5,18 @@
 
 #include "EventFilter/SiStripChannelChargeFilter/interface/MTCCHLTrigger.h"
 #include "EventFilter/SiStripChannelChargeFilter/interface/ClusterMTCCFilter.h"
+#include "EventFilter/SiStripChannelChargeFilter/interface/TECClusterFilter.h"
 #include "EventFilter/SiStripChannelChargeFilter/interface/TrackMTCCFilter.h"
 #include "EventFilter/SiStripChannelChargeFilter/interface/LTCTriggerBitsFilter.h"
 
 using cms::MTCCHLTrigger;
 using cms::ClusterMTCCFilter;
+using cms::TECClusterFilter;
 using cms::TrackMTCCFilter;
 using cms::LTCTriggerBitsFilter;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MTCCHLTrigger);
 DEFINE_ANOTHER_FWK_MODULE(ClusterMTCCFilter);
+DEFINE_ANOTHER_FWK_MODULE(TECClusterFilter);
 DEFINE_ANOTHER_FWK_MODULE(TrackMTCCFilter);
 DEFINE_ANOTHER_FWK_MODULE(LTCTriggerBitsFilter);
