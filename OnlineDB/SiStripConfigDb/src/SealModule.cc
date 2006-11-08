9b3a9277146d2a50de275572e3e2d36f6d9d1860
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-11-08 16:02:55 +0000

    new service

---
+++ OnlineDB/SiStripConfigDb/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "PluginManager/ModuleDef.h"
+DEFINE_SEAL_MODULE();
+
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "OnlineDB/SiStripConfigDb/interface/SiStripConfigDb.h"
+DEFINE_ANOTHER_FWK_SERVICE(SiStripConfigDb);
