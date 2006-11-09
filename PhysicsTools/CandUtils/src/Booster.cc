24c2848af6143e1934fbcc38e0c2e14f99e841b2
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-11-09 09:24:51 +0000

    Boost -> boost (new ROOT version)

--- PhysicsTools/CandUtils/src/Booster.cc
+++ PhysicsTools/CandUtils/src/Booster.cc
@@ -1,4 +1,4 @@
-// $Id: Booster.cc,v 1.6 2006/02/21 10:37:31 llista Exp $
+// $Id: Booster.cc,v 1.7 2006/07/26 08:48:06 llista Exp $
 #include "PhysicsTools/CandUtils/interface/Booster.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include <Math/VectorUtil.h>
@@ -6,7 +6,7 @@ using namespace std;
 using namespace reco;
 
 void Booster::set( Candidate& c ) {
-  c.setP4( ROOT::Math::VectorUtil::Boost( c.p4(), boost ) );
+  c.setP4( ROOT::Math::VectorUtil::boost( c.p4(), boost ) );
   for( Candidate::iterator d = c.begin(); d != c.end(); ++ d )
     set( * d );
 }
