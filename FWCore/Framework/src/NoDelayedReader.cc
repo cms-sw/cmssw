21d7ced75ad6589fd176c0a8391f5c746be4b641
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-11-10 20:51:10 +0000

    Handle the run and luminosity block cases

--- FWCore/Framework/src/NoDelayedReader.cc
+++ FWCore/Framework/src/NoDelayedReader.cc
@@ -1,9 +1,11 @@
 /*----------------------------------------------------------------------
-$Id: NoDelayedReader.cc,v 1.2 2005/12/01 22:14:54 wmtan Exp $
+$Id: NoDelayedReader.cc,v 1.3 2006/01/06 00:29:32 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Framework/interface/NoDelayedReader.h"
 #include "FWCore/Framework/interface/EventPrincipal.h"
+#include "FWCore/Framework/interface/RunPrincipal.h"
+#include "FWCore/Framework/interface/LuminosityBlockPrincipal.h"
 
 namespace edm {
   NoDelayedReader::~NoDelayedReader() {}
@@ -11,7 +13,21 @@ namespace edm {
   std::auto_ptr<EDProduct>
   NoDelayedReader::get(BranchKey const& k, EDProductGetter const* ep) const {
     EventPrincipal const* epr = dynamic_cast<EventPrincipal const*>(ep);
+    if (epr) {
+      throw cms::Exception("LogicError","NoDelayedReader")
+        << "get() called for branchkey: " << k << " EventID: " << epr->id() << "\n";
+    }
+    RunPrincipal const* rpr = dynamic_cast<RunPrincipal const*>(ep);
+    if (rpr) {
+      throw cms::Exception("LogicError","NoDelayedReader")
+        << "get() called for branchkey: " << k << " RunID: " << epr->id() << "\n";
+    }
+    LuminosityBlockPrincipal const* lpr = dynamic_cast<LuminosityBlockPrincipal const*>(ep);
+    if (lpr) {
+      throw cms::Exception("LogicError","NoDelayedReader")
+        << "get() called for branchkey: " << k << " LuminosityBlockID: " << lpr->id() << "\n";
+    }
     throw cms::Exception("LogicError","NoDelayedReader")
-      << "get() called for branchkey: " << k << " EventID: " << (epr ? epr->id() : EventID()) << "\n";
+      << "get() called for branchkey: " << k << "\n";
   }
 }
