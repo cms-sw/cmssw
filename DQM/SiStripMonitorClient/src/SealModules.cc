859575794c094a2155b0171452cce612844925b0
Samvel Khalatyan <sha1-ae95162fd70683bc31ca18885c4c50db0feed2df@cern.ch>
2006-11-10 23:31:29 +0000

    Fixed includes

--- DQM/SiStripMonitorClient/src/SealModules.cc
+++ DQM/SiStripMonitorClient/src/SealModules.cc
@@ -1,5 +1,6 @@
 #include "PluginManager/ModuleDef.h"
 #include "DQM/SiStripMonitorClient/interface/SiStripOfflineDQM.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripOfflineDQM)
