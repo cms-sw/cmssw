881840f1473b628d873d7bdd412af8c402ab652d
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2006-11-13 11:39:35 +0000

    Moved some of the persistent classes of SimG4CMS/Calo

---
+++ SimDataFormats/CaloTest/src/classes.h
@@ -0,0 +1,8 @@
+#include "SimDataFormats/CaloTest/interface/HcalTestHistoClass.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
+namespace {
+  namespace {
+    HcalTestHistoClass theHcalTestHistoClass;
+  }
+}
