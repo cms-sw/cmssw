7af9038a62be686b77af7336fdc537301eaf4c35
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-11-13 10:07:32 +0000

    fix for gcc345

--- CondFormats/Calibration/src/T_EventSetup_Pedestals.cc
+++ CondFormats/Calibration/src/T_EventSetup_Pedestals.cc
@@ -8,7 +8,7 @@
 //
 // Author:      Chris Jones
 // Created:     Mon Apr 18 16:42:52 EDT 2005
-// $Id: T_EventSetup_Pedestals.cc,v 1.1 2005/07/22 14:26:16 xiezhen Exp $
+// $Id: T_EventSetup_Pedestals.cc,v 1.2 2006/07/18 17:57:43 xiezhen Exp $
 //
 
 // system include files
@@ -17,4 +17,4 @@
 #include "CondFormats/Calibration/interface/Pedestals.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
-EVENTSETUP_DATA_REG(Pedestals)
+EVENTSETUP_DATA_REG(Pedestals);
