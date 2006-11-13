f7ac9db29664016e923d5de9d294e8f6a762e853
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2006-11-13 12:59:36 +0000

    *** empty log message ***

--- RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
+++ RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
@@ -5,5 +5,5 @@
 #include "RecoMuon/L3MuonIsolationProducer/src/L3MuonIsolationProducer.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(L3MuonIsolationProducer)
+DEFINE_ANOTHER_FWK_MODULE(L3MuonIsolationProducer);
 
