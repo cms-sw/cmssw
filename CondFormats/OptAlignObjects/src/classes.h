4f74e493b03970031aff75e7a270081f155b8fbc
Pedro Arce <sha1-a7e83fd644adcfec94499d67c7f8f23354d9a549@cern.ch>
2006-11-14 10:36:23 +0000

    go back to r 1.11

--- CondFormats/OptAlignObjects/src/classes.h
+++ CondFormats/OptAlignObjects/src/classes.h
@@ -8,24 +8,41 @@
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignMeasurements.h"
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignMeasurementInfo.h"
 #include "CondFormats/OptAlignObjects/interface/CSCZSensors.h"
+#include "CondFormats/OptAlignObjects/interface/CSCRSensors.h"
 
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "CondFormats/OptAlignObjects/interface/Inclinometers.h"
+#include "CondFormats/OptAlignObjects/interface/PXsensors.h"
 
 #include <string>
 #include <vector>
 
-template std::vector<OpticalAlignInfo>::iterator;
-
+//template std::vector<OpticalAlignInfo>::iterator;
 //template std::vector<MBAForkData>::iterator;
-template std::vector<MBAChBenchCalPlateData>::iterator;
-template std::vector<MBAChBenchSurveyPlateData>::iterator;
+//template std::vector<MBAChBenchCalPlateData>::iterator;
+//template std::vector<MBAChBenchSurveyPlateData>::iterator;
+//template std::vector<OpticalAlignMeasurementInfo>::iterator;
+//template std::vector<CSCZSensorData>::iterator;
+//template std::vector<OpticalAlignParam>::iterator;
+///template std::vector< int >::iterator;
+//template std::vector< int >::const_iterator;
+//template std::vector< Inclinometers::Item >::iterator;
+//template std::vector< Inclinometers::Item >::const_iterator;
+//template std::vector< PXsensors::Item >::iterator;
+//template std::vector< PXsensors::Item >::const_iterator;
 
-template std::vector<OpticalAlignMeasurementInfo>::iterator;
-template std::vector<CSCZSensorData>::iterator;
+namespace{
+  std::vector<OpticalAlignInfo> optaligninfovec;
+  std::vector<MBAChBenchCalPlateData> mbacalvec;
+  std::vector<MBAChBenchSurveyPlateData> mbasurveyvec;
+  std::vector<OpticalAlignMeasurementInfo> optmeasureinfovec;
+  std::vector<CSCZSensorData> zsensorvec;
+  std::vector<CSCRSensorData> rsensorvec;
+  std::vector<OpticalAlignParam> OpticalAlignParamvec;
+  std::vector< Inclinometers::Item > incvec;
+  std::vector< PXsensors::Item > pxvec;
+}
 
-template std::vector<OpticalAlignParam>::iterator;
-template std::vector< int >::iterator;
-template std::vector< int >::const_iterator;
 //template edm::Wrapper<OpticalAlignments>;
 namespace {
   namespace {
