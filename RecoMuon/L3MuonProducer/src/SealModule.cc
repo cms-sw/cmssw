db3a9cfc37556d372d2bbf627fabbbfd0989d763
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-11-15 08:30:45 +0000

    Candidate producer

--- RecoMuon/L3MuonProducer/src/SealModule.cc
+++ RecoMuon/L3MuonProducer/src/SealModule.cc
@@ -3,7 +3,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoMuon/L3MuonProducer/src/L3MuonProducer.h"
+#include "RecoMuon/L3MuonProducer/src/L3MuonCandidateProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L3MuonProducer);
-
+DEFINE_ANOTHER_FWK_MODULE(L3MuonCandidateProducer);
