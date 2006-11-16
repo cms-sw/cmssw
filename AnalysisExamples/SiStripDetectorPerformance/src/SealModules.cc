580d3a2c667c93a57e440f51340bb180a121d8e1
Samvel Khalatyan <sha1-ae95162fd70683bc31ca18885c4c50db0feed2df@cern.ch>
2006-11-16 22:15:38 +0000

    MTCCNtupleMaker added

--- AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
+++ AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
@@ -3,6 +3,7 @@
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/AnalyzeTracksClusters.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysis.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysisFilter.h"
+#include "AnalysisExamples/SiStripDetectorPerformance/interface/MTCCNtupleMaker.h"
 
 using cms::ClusterAnalysis;
 using cms::ClusterAnalysisFilter;
@@ -11,3 +12,4 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(AnalyzeTracksClusters);
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysisFilter);
+DEFINE_ANOTHER_FWK_MODULE(MTCCNtupleMaker);
