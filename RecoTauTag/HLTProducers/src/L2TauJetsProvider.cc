fa9f57cba64d1146d842770da07fc8d2decfb007
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2006-11-16 12:13:12 +0000

    *** empty log message ***

--- RecoTauTag/HLTProducers/src/L2TauJetsProvider.cc
+++ RecoTauTag/HLTProducers/src/L2TauJetsProvider.cc
@@ -10,6 +10,7 @@ using namespace edm;
 L2TauJetsProvider::L2TauJetsProvider(const edm::ParameterSet& iConfig)
 {
   jetSrc = iConfig.getParameter<InputTag>("JetSrc");
+  l1ParticleMap = iConfig.getParameter<InputTag>("L1ParticleMap");
 
   produces<CaloJetCollection>("SingleTau");
   produces<CaloJetCollection>("DoubleTau");
@@ -25,16 +26,28 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
  using namespace edm;
  using namespace std;
  using namespace reco;
+ using namespace l1extra;
  Handle<CaloJetCollection> jets;
  iEvent.getByLabel(jetSrc,jets);
  CaloJetCollection* singleTauTmp = new CaloJetCollection();
  CaloJetCollection* doubleTauTmp= new CaloJetCollection();
  CaloJetCollection* leptonTauTmp= new CaloJetCollection();
 
+  Handle< L1ParticleMapCollection > mapColl ;
+  iEvent.getByLabel( l1ParticleMap, mapColl ) ;
+  const L1ParticleMap& singleTauMap = ( *mapColl )[L1ParticleMap::kSingleTau ] ;
+  const L1ParticleMap& doubleTauMap = ( *mapColl )[L1ParticleMap::kDoubleTau ] ;
 
-	mEt_SingleTau = 30.;
-	mEt_DoubleTau = 66.;
-	mEt_CombTau = 50.;
+  bool singleTauFired = singleTauMap.triggerDecision() ;
+  bool doubleTauFired = doubleTauMap.triggerDecision() ;
+
+
+  //FIXEM:
+  //To be fixed with L1 energy and thresholds
+  //TO BE CAREFULL VERY DANGEROUS AND ERROR PRONE
+  mEt_SingleTau = 30.;
+  mEt_DoubleTau = 12.;
+  mEt_CombTau = 50.;
 	  
   //Loop over the jetSrc to split the jets
   //Et cut will be replaced with L1 Trigger bit
@@ -51,14 +64,14 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   for(;iJet!=jets->end();iJet++)
     {
       //Fill doubleTauJet collection if the jet is above L1 Double Thresholds
-      if(iJet->pt() >= mEt_DoubleTau && iJet->pt() < mEt_SingleTau) doubleTauTmp->push_back(*(iJet));
-
+      if(doubleTauFired){
+	if(iJet->pt() >= mEt_DoubleTau && iJet->pt() < mEt_SingleTau) doubleTauTmp->push_back(*(iJet));
+      }else{
       //Fill doubleTauJet collection if the jet is below L1 Double Thresholds but there is at least one Tau triggering single Tau
-      if(singleTauTmp->size() > 0 && iJet->pt() < mEt_DoubleTau && iJet->pt() >= mEt_CombTau) doubleTauTmp->push_back(*(iJet));
-
-      //Otherwise fill Lepton+Tau collection
-      if(singleTauTmp->size() == 0 && iJet->pt() < mEt_DoubleTau && iJet->pt() >= mEt_CombTau) leptonTauTmp->push_back(*(iJet));
-
+	if(singleTauFired && iJet->pt() < mEt_DoubleTau && iJet->pt() >= mEt_CombTau) doubleTauTmp->push_back(*(iJet));
+      }
+	//Otherwise fill Lepton+Tau collection
+      if(iJet->pt() < mEt_DoubleTau && iJet->pt() >= mEt_CombTau) leptonTauTmp->push_back(*(iJet));
     }
   auto_ptr<CaloJetCollection> singleTaujets(singleTauTmp);
   auto_ptr<CaloJetCollection> doubleTaujets(doubleTauTmp);
@@ -67,21 +80,35 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   cout <<"Size of SingleTau "<<singleTauTmp->size()<<endl;
   cout <<"Size of DoubleTau "<<doubleTauTmp->size()<<endl;
 
-   int l1Decision =0;
+  int l1Decision =0;
   int singleTauSize = singleTauTmp->size();
   int doubleTauSize = doubleTauTmp->size();
 
 
-  //This have to be modified taking L1 bits
 
-
-  if(singleTauSize < 2 && doubleTauSize == 0)    l1Decision = 1; //Pure SingleTau Trigger
-  if(singleTauSize >1 && doubleTauSize == 0)     l1Decision = 2;//Single && DoubleTau
-  if(singleTauSize >1 && doubleTauSize > 0) l1Decision = 3;//Single && DoubleTau      
-  if(singleTauSize == 1 && doubleTauSize > 0) l1Decision = 4;//Single && DoubleTau, To check the mixed case 
-  if(singleTauSize == 0 && doubleTauSize >1) l1Decision = 6;//Pure Double
+  //Coding decisions:
+  // 0: No doubleTau is possible
+  // 1: L1 singleTau candidate has  to be confirmed in at least 1 jet
+  // 2: L1 singleTau candidate has  to be confirmed in at least 2 jets
+  // 3: L1 singleTau has NOT to be confirmed
   
-  //There is missing the mixed case!!!!!!!!
+
+
+  if(singleTauFired){
+    if(!doubleTauFired) {
+      if(doubleTauSize ==0) l1Decision =0;
+      if(doubleTauSize>0) l1Decision =1; //Mixed Tau case
+    }else{
+      //Single and DoubleTau fired
+      if(doubleTauSize == 0) l1Decision=2;
+      if(singleTauSize == 1 && doubleTauSize ==1) l1Decision =1;
+      if(doubleTauSize >1) l1Decision =3;
+    }
+  }
+  if(!singleTauFired){
+    l1Decision =3;
+  }
+
 
   auto_ptr< vector<int> > myL1BookKeep(new vector<int>);
   myL1BookKeep->push_back(l1Decision);
