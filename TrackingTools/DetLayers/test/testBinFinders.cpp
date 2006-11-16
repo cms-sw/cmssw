3485d888ccfff26ede3043e65cc65eb4fbd737a9
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2006-11-16 13:07:51 +0000

    force compilation of all headers

---
+++ TrackingTools/DetLayers/test/testBinFinders.cpp
@@ -0,0 +1,10 @@
+// For the time being, just try to compile the headers.
+
+#include <TrackingTools/DetLayers/interface/PeriodicBinFinderInZ.h>
+#include <TrackingTools/DetLayers/interface/GeneralBinFinderInZforGeometricSearchDet.h>
+#include <TrackingTools/DetLayers/interface/simple_stat.h>
+
+
+int main(){
+  return 0;
+}
