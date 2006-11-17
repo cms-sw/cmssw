044b6c47125d4f7d80859bebb66e29816651ec33
Wolfgang Waltenberger <sha1-9c4ada266a8352c2428af94857b7ab82e25b3b37@cern.ch>
2006-11-17 10:14:35 +0000

    make it compile with newer compiler, move two lines from .h to .cc

--- CommonTools/Statistics/interface/ChiSquaredProbability.h
+++ CommonTools/Statistics/interface/ChiSquaredProbability.h
@@ -1,18 +1,13 @@
 #ifndef ChiSquaredProbability_H
 #define ChiSquaredProbability_H
 
-#include "CommonTools/Statistics/src/IncompleteGammaComplement.h"
-
 /** Returns the probability that an observation, correctly described by
  *  a model with nrDOF, will give rise to a chi-squared larger than the one
  *  observed; from this, one can interpret this probability as how likely
  *  it is to observe as high (or higher) a chi-squared. 
  *  source: Numerical Recipes
  */
-inline float ChiSquaredProbability( double chiSquared, double nrDOF )
-{ return IncompleteGammaComplement::value( nrDOF / 2 , chiSquared / 2 ); }
-
-inline float LnChiSquaredProbability( double chiSquared, double nrDOF )
-{ return IncompleteGammaComplement::ln( nrDOF / 2 , chiSquared / 2 ); }
+float ChiSquaredProbability( double chiSquared, double nrDOF );
+float LnChiSquaredProbability( double chiSquared, double nrDOF );
 
 #endif
