f4f0b69eaa481ab96af196995f8c78a191bb8d3e
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-11-20 10:14:08 +0000

    add back all

--- CondFormats/OptAlignObjects/src/T_EventSetup_OpticalAlignments.cc
+++ CondFormats/OptAlignObjects/src/T_EventSetup_OpticalAlignments.cc
@@ -8,7 +8,7 @@
 //
 // Author:      Chris Jones
 // Created:     Mon Apr 18 16:42:52 EDT 2005
-// $Id: T_EventSetup_OpticalAlignments.cc,v 1.3 2006/04/07 01:09:58 case Exp $
+// $Id: T_EventSetup_OpticalAlignments.cc,v 1.6 2006/11/07 22:23:43 arce Exp $
 //
 
 // system include files
@@ -16,12 +16,18 @@
 // user include files
 #include "CondFormats/OptAlignObjects/interface/OpticalAlignments.h"
 #include "CondFormats/OptAlignObjects/interface/CSCZSensors.h"
+#include "CondFormats/OptAlignObjects/interface/CSCRSensors.h"
 #include "CondFormats/OptAlignObjects/interface/MBAChBenchCalPlate.h"
 #include "CondFormats/OptAlignObjects/interface/MBAChBenchSurveyPlate.h"
+#include "CondFormats/OptAlignObjects/interface/Inclinometers.h"
+ #include "CondFormats/OptAlignObjects/interface/PXsensors.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 //using Alignments;
 EVENTSETUP_DATA_REG(OpticalAlignments);
 EVENTSETUP_DATA_REG(CSCZSensors);
+EVENTSETUP_DATA_REG(CSCRSensors);
 EVENTSETUP_DATA_REG(MBAChBenchCalPlate);
 EVENTSETUP_DATA_REG(MBAChBenchSurveyPlate);
+EVENTSETUP_DATA_REG(Inclinometers);
+EVENTSETUP_DATA_REG(PXsensors);
