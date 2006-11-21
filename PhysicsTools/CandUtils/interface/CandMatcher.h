e1b3dd584b58cfca4cb9561419886616668f71d4
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-11-21 15:49:02 +0000

    added candidate matcher navigating among mothers

---
+++ PhysicsTools/CandUtils/interface/CandMatcher.h
@@ -0,0 +1,33 @@
+#ifndef CandUtils_CandMatcher_h
+#define CandUtils_CandMatcher_h
+/* class CandMatcher
+ *
+ * \author Luca Lista, INFN
+ *
+ */
+#include "DataFormats/Candidate/interface/CandMatchMap.h"
+#include <set>
+
+class CandMatcher {
+public:
+  /// constructor
+  explicit CandMatcher( const reco::CandMatchMap & map );
+  /// get match from persistent reference
+  reco::CandidateRef operator()( const reco::CandidateRef & ) const;
+  /// get match from transient reference
+  reco::CandidateRef operator()( const reco::Candidate & ) const;
+
+private:
+  /// reference to match map, typically taken from the event
+  const reco::CandMatchMap & map_;
+  /// pointer map type
+  typedef std::map<const reco::Candidate *, reco::CandidateRef> RefMap;
+  /// pointer map of candidates (e.g.: reco)
+  RefMap candRefs_;
+  /// pointer map of matched candidates (e.g.: MC truth)
+  RefMap matchedRefs_;
+  /// mother indices
+  std::vector<std::vector<size_t> > mothers_;
+};
+
+#endif
