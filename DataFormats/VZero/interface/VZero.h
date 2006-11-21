46b5f3ab6a0f3ff387576418b047c09aef2b3e43
Ferenc Sikler <sha1-753db23f0bb46b999a71a8a004b971875b5ec09c@cern.ch>
2006-11-21 11:40:23 +0000

    Fixing missing library, use of DataFormats/Math.

--- DataFormats/VZero/interface/VZero.h
+++ DataFormats/VZero/interface/VZero.h
@@ -4,8 +4,7 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/VZero/interface/VZeroFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
-#include "Geometry/Vector/interface/GlobalPoint.h"
-#include "Geometry/Vector/interface/GlobalVector.h"
+#include "DataFormats/Math/interface/Point3D.h"
 
 namespace reco {
 
@@ -13,27 +12,40 @@ namespace reco {
   {
    public:
      float dcaR,dcaZ, impactMother, armenterosPt,armenterosAlpha;
-     GlobalPoint crossingPoint;
+     math::GlobalPoint crossingPoint;
   };
 
   class VZero
   {
    public:
-     // default construction
+     // default constructor
      VZero() { }
 
      // constructor from parameters
      VZero(Vertex vertex, VZeroData data);
 
+     // decay/conversion vertex
      Vertex vertex() const { return vertex_; }
+
+     // position of vertex     
      Vertex::Point crossingPoint() const { return vertex_.position(); }
 
+     // reference to positive daughter
      TrackRef positiveDaughter() const { return *(vertex_.tracks_begin()  ); }
+
+     // reference to negative daughter
      TrackRef negativeDaughter() const { return *(vertex_.tracks_begin()+1); }
 
+     // distance of closest approach (radial)
      float dcaR() const { return data_.dcaR; }
+
+     // distance of closest approach (z)
      float dcaZ() const { return data_.dcaZ; }
+
+     // impact parameter of the mother particle
      float impactMother() const { return data_.impactMother; }
+
+     // Armenteros variables
      float armenterosPt() const { return data_.armenterosPt; }
      float armenterosAlpha() const { return data_.armenterosAlpha; }
 
