7c6b30b291005308e444358db62613b81947b685
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-11-22 11:22:16 +0000

    added daq scope mode task

---
+++ DQM/SiStripCommissioningSources/src/DaqScopeModeTask.cc
@@ -0,0 +1,70 @@
+#include "DQM/SiStripCommissioningSources/interface/DaqScopeModeTask.h"
+#include "DataFormats/SiStripCommon/interface/SiStripConstants.h"
+#include "DataFormats/SiStripCommon/interface/SiStripHistoNamingScheme.h"
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripFecCabling.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+using namespace std;
+using namespace sistrip;
+
+// -----------------------------------------------------------------------------
+//
+DaqScopeModeTask::DaqScopeModeTask( DaqMonitorBEInterface* dqm,
+				    const FedChannelConnection& conn ) :
+  CommissioningTask( dqm, conn, "DaqScopeModeTask" ),
+  scope_(),
+  nBins_(sistrip::maximum_) // ADC range (0->1023)
+{}
+
+// -----------------------------------------------------------------------------
+//
+DaqScopeModeTask::~DaqScopeModeTask() {
+}
+
+// -----------------------------------------------------------------------------
+//
+void DaqScopeModeTask::book() {
+  LogTrace(mlDqmSource_) << "[CommissioningTask::" << __func__ << "]";
+  
+  string title = SiStripHistoNamingScheme::histoTitle( HistoTitle( sistrip::DAQ_SCOPE_MODE, 
+								   sistrip::FED_KEY, 
+								   fedKey(),
+								   sistrip::LLD_CHAN, 
+								   connection().lldChannel() ) );
+  
+  scope_.histo_ = dqm()->book1D( title, title, 
+				 nBins_, -0.5, nBins_-0.5 );
+  
+  scope_.vNumOfEntries_.resize(nBins_,0);
+  scope_.vSumOfContents_.resize(nBins_,0);
+  scope_.vSumOfSquares_.resize(nBins_,0);
+  scope_.isProfile_ = false; //@@ simple 1D histo
+  
+}
+
+// -----------------------------------------------------------------------------
+//
+void DaqScopeModeTask::fill( const SiStripEventSummary& summary,
+			     const edm::DetSet<SiStripRawDigi>& digis ) {
+
+  if ( digis.data.size() == 0 ) { //@@ check scope mode length?  
+    edm::LogWarning(mlDqmSource_)
+      << "[DaqScopeModeTask::" << __func__ << "]"
+      << " Unexpected number of digis! " 
+      << digis.data.size(); 
+  } else {
+    for ( uint16_t ibin = 0; ibin < digis.data.size(); ibin++ ) {
+      updateHistoSet( scope_, digis.data[ibin].adc() );
+    }
+  }
+  
+}
+
+// -----------------------------------------------------------------------------
+//
+void DaqScopeModeTask::update() {
+  updateHistoSet( scope_ );
+}
+
+
