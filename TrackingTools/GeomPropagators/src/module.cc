cf44720a3c42d55dc6dfb4eb2a5beec101f43c9b
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-11-23 11:48:37 +0000

    SmartPropagatorESProducer

--- TrackingTools/GeomPropagators/src/module.cc
+++ TrackingTools/GeomPropagators/src/module.cc
@@ -1,6 +1,7 @@
 #include "TrackingTools/GeomPropagators/interface/Propagator.h"
 #include "TrackingTools/GeomPropagators/interface/AnalyticalPropagatorESProducer.h"
 #include "TrackingTools/GeomPropagators/interface/StraightLinePropagatorESProducer.h"
+#include "TrackingTools/GeomPropagators/interface/SmartPropagatorESProducer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -12,4 +13,4 @@
 EVENTSETUP_DATA_REG(Propagator);
 DEFINE_FWK_EVENTSETUP_MODULE(StraightLinePropagatorESProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(AnalyticalPropagatorESProducer);
-
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(SmartPropagatorESProducer);
