328faf4f0b6300775ff64f22232a2d1887c0a4c6
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-11-23 11:44:36 +0000

    cleaning and GlobalTrackingGeometryRecord added to the record list

--- TrackingTools/Records/interface/TransientRecHitRecord.h
+++ TrackingTools/Records/interface/TransientRecHitRecord.h
@@ -1,16 +1,16 @@
-#ifndef RecoTracker_Record_TransientRecHitRecord_h
-#define RecoTracker_Record_TransientRecHitRecord_h
+#ifndef TrackingTools_Record_TransientRecHitRecord_h
+#define TrackingTools_Record_TransientRecHitRecord_h
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "RecoLocalTracker/Records/interface/TrackerCPERecord.h"
-
+#include "Geometry/Records/interface/GlobalTrackingGeometryRecord.h"
 
 #include "boost/mpl/vector.hpp"
 
 class TransientRecHitRecord : public edm::eventsetup::DependentRecordImplementation<TransientRecHitRecord,
-  boost::mpl::vector<IdealGeometryRecord,TrackerDigiGeometryRecord,TrackerCPERecord> > {};
+			      boost::mpl::vector<IdealGeometryRecord,TrackerDigiGeometryRecord,TrackerCPERecord,GlobalTrackingGeometryRecord> > {};
 #endif 
 
