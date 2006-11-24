bf331961f82c249ceee920c9e4d1a26a1048c45b
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-11-24 11:46:06 +0000

    added null string

--- DataFormats/SiStripCommon/interface/SiStripConstants.h
+++ DataFormats/SiStripCommon/interface/SiStripConstants.h
@@ -12,7 +12,7 @@ namespace sistrip {
   static const uint16_t invalid_ = 0xFFFF; // 65535
   static const uint16_t unknown_ = 0xFFFE; // 65534
   static const uint16_t maximum_ = 0x3FF;  // 1023
-
+  static const std::string null_ = "null";
 }
 
 // -------------------- MessageLogger categories --------------------
