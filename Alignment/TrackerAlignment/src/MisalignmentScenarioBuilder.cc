a3018b0dcf4a69c96e550807013bee743902d1bc
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-11-24 15:35:40 +0000

    Bug fix in previous fix (PSets with N>9)

--- Alignment/TrackerAlignment/src/MisalignmentScenarioBuilder.cc
+++ Alignment/TrackerAlignment/src/MisalignmentScenarioBuilder.cc
@@ -322,15 +322,18 @@ MisalignmentScenarioBuilder::rootName_( const std::string& parameterSetName ) co
 
   std::string result = parameterSetName; // Initialise to full string
   
-  // String ends with 's'
+  // Check if string ends with 's'
   const int lastChar = parameterSetName.length()-1;
   if ( parameterSetName[lastChar] == 's' ) 
-    result =  parameterSetName.substr( 0, lastChar );
+      result =  parameterSetName.substr( 0, lastChar );
   else
-    // Otherwise, look for numbers (assumes names have no numbers...)
+    // Otherwise, look for numbers (assumes names have no numbers inside...)
     for ( unsigned int ichar = 0; ichar<parameterSetName.length(); ichar++ )
       if ( isdigit(parameterSetName[ichar]) )
-        result = parameterSetName.substr( 0, ichar );
+        {
+          result = parameterSetName.substr( 0, ichar );
+          break; // Stop at first digit
+        }
 
   LogDebug("PrintParameters") << "Name was " << parameterSetName << ", root is " << result;
 
