cae92e7282edd24c1bb12e01e8aa310f4f83443f
spitsky <sha1-018681df6f29f6cf50968c831efcf13efd2a868f@cern.ch>
2006-11-24 08:26:12 +0000

    *** empty log message ***

---
+++ GeneratorInterface/AlpgenInterface/src/SealModule.cc
@@ -0,0 +1,10 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "GeneratorInterface/AlpgenInterface/interface/AlpgenSource.h"
+
+  using edm::AlpgenSource;
+
+  DEFINE_SEAL_MODULE();
+  DEFINE_ANOTHER_FWK_INPUT_SOURCE(AlpgenSource)
+
