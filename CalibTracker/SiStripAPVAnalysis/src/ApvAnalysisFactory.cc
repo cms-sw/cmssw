9a2d2a72a53d5ffc321e731f7ecabba2595fc53f
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2006-11-26 17:35:12 +0000

    *** empty log message ***

--- CalibTracker/SiStripAPVAnalysis/src/ApvAnalysisFactory.cc
+++ CalibTracker/SiStripAPVAnalysis/src/ApvAnalysisFactory.cc
@@ -130,7 +130,7 @@ void ApvAnalysisFactory::update(uint32_t detId, const edm::DetSet<SiStripRawDigi
 	   
 	   for(int istrip = startStrip; istrip < stopStrip;istrip++)
 	     {
-               if (in.data.size() < istrip) tmpRawDigi.data.push_back(0);
+               if (in.data.size() <= istrip) tmpRawDigi.data.push_back(0);
 	       else tmpRawDigi.data.push_back(in.data[istrip]); //maybe dangerous
 	     }
 
