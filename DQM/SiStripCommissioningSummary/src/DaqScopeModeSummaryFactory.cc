78c6b6ac8324de5df2646f701fe3b4d837dcc770
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-11-27 09:13:55 +0000

    added new daq scope mode task

---
+++ DQM/SiStripCommissioningSummary/src/DaqScopeModeSummaryFactory.cc
@@ -0,0 +1,134 @@
+#include "DQM/SiStripCommissioningSummary/interface/DaqScopeModeSummaryFactory.h"
+#include "DQM/SiStripCommissioningSummary/interface/SummaryGenerator.h"
+#include "DataFormats/SiStripCommon/interface/SiStripHistoNamingScheme.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+// -----------------------------------------------------------------------------
+//
+SummaryHistogramFactory<DaqScopeModeAnalysis>::SummaryHistogramFactory() :
+  histo_(sistrip::UNKNOWN_SUMMARY_HISTO),
+  type_(sistrip::UNKNOWN_SUMMARY_TYPE),
+  view_(sistrip::UNKNOWN_VIEW),
+  level_(sistrip::root_),
+  gran_(sistrip::UNKNOWN_GRAN),
+  generator_(0) 
+{
+} 
+
+
+// -----------------------------------------------------------------------------
+//
+SummaryHistogramFactory<DaqScopeModeAnalysis>::~SummaryHistogramFactory() {
+  if ( generator_ ) { delete generator_; }
+}
+
+// -----------------------------------------------------------------------------
+//
+void SummaryHistogramFactory<DaqScopeModeAnalysis>::init( const sistrip::SummaryHisto& histo, 
+							  const sistrip::SummaryType& type,
+							  const sistrip::View& view, 
+							  const string& top_level_dir, 
+							  const sistrip::Granularity& gran ) {
+  histo_ = histo;
+  type_ = type;
+  view_ = view;
+  level_ = top_level_dir;
+  gran_ = gran;
+
+  // Retrieve utility class used to generate summary histograms
+  if ( generator_ ) { delete generator_; generator_ = 0; }
+  generator_ = SummaryGenerator::instance( view );
+  
+}
+
+//------------------------------------------------------------------------------
+//
+uint32_t SummaryHistogramFactory<DaqScopeModeAnalysis>::extract( const map<uint32_t,DaqScopeModeAnalysis>& data ) {
+  
+  // Check if data are present
+  if ( data.empty() ) { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " No data in monitorables map!" << endl;
+    return 0; 
+  }
+  
+  // Check if instance of generator class exists
+  if ( !generator_ ) { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " NULL pointer to SummaryGenerator object!" << endl;
+    return 0;
+  }
+  
+  // Transfer appropriate monitorables info to generator object
+  generator_->clearMap();
+  map<uint32_t,DaqScopeModeAnalysis>::const_iterator iter = data.begin();
+  for ( ; iter != data.end(); iter++ ) {
+    if ( histo_ == sistrip::DAQ_SCOPE_MODE_MEAN_SIGNAL ) { 
+      generator_->fillMap( level_, gran_, iter->first, iter->second.mean() ); 
+    } else { continue; }
+  }
+  return generator_->size();
+}
+
+//------------------------------------------------------------------------------
+//
+void SummaryHistogramFactory<DaqScopeModeAnalysis>::fill( TH1& summary_histo ) {
+
+  // Check if instance of generator class exists
+  if ( !generator_ ) { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " NULL pointer to SummaryGenerator object!" << endl;
+    return;
+  }
+
+  // Check if instance of generator class exists
+  if ( !(&summary_histo) ) { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " NULL pointer to SummaryGenerator object!" << endl;
+    return;
+  }
+
+  // Check if map is filled
+  if ( !generator_->size() ) { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " No data in the monitorables map!" << endl;
+    return; 
+  } 
+
+  // Generate appropriate summary histogram 
+  if ( type_ == sistrip::SUMMARY_DISTR ) {
+    generator_->summaryDistr( summary_histo );
+  } else if ( type_ == sistrip::SUMMARY_1D ) {
+    generator_->summary1D( summary_histo );
+  } else if ( type_ == sistrip::SUMMARY_2D ) {
+    generator_->summary2D( summary_histo );
+  } else if ( type_ == sistrip::SUMMARY_PROF ) {
+    generator_->summaryProf( summary_histo );
+  } else { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " Unexpected SummaryType value:"
+	 << SiStripHistoNamingScheme::summaryType( type_ ) 
+	 << endl;
+    return; 
+  }
+  
+  // Histogram formatting
+  if ( histo_ == sistrip::DAQ_SCOPE_MODE_MEAN_SIGNAL ) { 
+    generator_->axisLabel( "Mean signal [adc]" );
+  } else { 
+    cerr << "[" << __PRETTY_FUNCTION__ << "]" 
+	 << " Unexpected SummaryHisto value:"
+	 << SiStripHistoNamingScheme::summaryHisto( histo_ ) 
+	 << endl;
+  } 
+  generator_->format( sistrip::APV_TIMING, histo_, type_, view_, level_, gran_, summary_histo );
+  
+}
+
+// -----------------------------------------------------------------------------
+//
+template class SummaryHistogramFactory<DaqScopeModeAnalysis>;
+
