d459f754cda6be1e8a942ce4454b1e27dd0e7c06
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2006-11-27 16:43:40 +0000

    improve DBInterface

--- SimCalorimetry/EcalTrigPrimAlgos/interface/DBInterface.h
+++ SimCalorimetry/EcalTrigPrimAlgos/interface/DBInterface.h
@@ -3,18 +3,19 @@
 
 #include <vector>
 #include <map>
-#include <string>
 
 class DBInterface {
   
  public:
   DBInterface() {;}
   DBInterface(std::string dbFileEB,std::string dbFileEE);
-  std::vector<unsigned int> getTowerParameters(int SM, int towerInSM) ;
-  std::vector<unsigned int> getStripParameters(int SM, int towerInSM, int stripInTower) ;
-  std::vector<unsigned int> getXtalParameters(int SM, int towerInSM, int stripInTower, int xtalInStrip) ;
+   std::vector<unsigned int> getTowerParameters(int SM, int towerInSM, bool print = false) ;
+  std::vector<unsigned int> getStripParameters(int SM, int towerInSM, int stripInTower, bool print = false) ;
+  std::vector<unsigned int> getXtalParameters(int SM, int towerInSM, int stripInTower, int xtalInStrip, bool print = false) ;
 
  private:
+  std::vector<int> getRange(int smNb, int towerNbInSm, int stripNbInTower=0, int xtalNbInStrip=0) ;
+
   std::map <int, std::vector<unsigned int> > towerParam_ ;
   std::map <int, std::vector<unsigned int> > stripParam_ ;
   std::map <int, std::vector<unsigned int> > xtalParam_ ;
