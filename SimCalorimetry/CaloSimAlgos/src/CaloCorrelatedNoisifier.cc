ea7bfffd535ef2ee8dbd94e39ff0b69c3109f386
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2006-11-27 17:58:05 +0000

    optimization for sparse matrix handling (courtesy of V. Innocente) added check on the matrix being diagonal or not

--- SimCalorimetry/CaloSimAlgos/src/CaloCorrelatedNoisifier.cc
+++ SimCalorimetry/CaloSimAlgos/src/CaloCorrelatedNoisifier.cc
@@ -4,94 +4,117 @@
 #include <stdexcept>
 
 CaloCorrelatedNoisifier::CaloCorrelatedNoisifier(int nFrames)
-: theMatrix(nFrames, 1),
+: theMatrix(nFrames, 1.0),
   theRandomGaussian(*(HepRandom::getTheEngine())),
-  theSize(nFrames)
+  theSize(nFrames),
+  theNorma(0.,theSize)
 {
-  theNorma.reserve(theSize);
-  theNorma.clear();
-  normaDone = false;
+  computeNormalization(); 
+
+  isDiagonal_ = true;
+  checkOffDiagonal(isDiagonal_);
+
 }
 
 
 CaloCorrelatedNoisifier::CaloCorrelatedNoisifier(const HepSymMatrix & matrix)
-: theMatrix(matrix),
+: theMatrix(matrix.num_row(),matrix),
   theRandomGaussian(*(HepRandom::getTheEngine())),
-  theSize(theMatrix.num_row())
+  theSize(theMatrix.rank()),
+  theNorma(0.,theSize)
 {
-  theNorma.reserve(theSize);
-  theNorma.clear();
-  normaDone = false;
+  computeNormalization(); 
+
+  isDiagonal_ = true;
+  checkOffDiagonal(isDiagonal_);
+
 }
+
 
 void CaloCorrelatedNoisifier::setDiagonal(double value) 
 {
   for(int i = 0; i < theSize; ++i) 
   {
-    theMatrix[i][i] = value;
+    theMatrix(i,i) = value;
   }
 
-  computeNormalization(theNorma); 
+  computeNormalization(); 
+
+  isDiagonal_ = true;
+  checkOffDiagonal(isDiagonal_);
+
 } 
 
 void CaloCorrelatedNoisifier::setOffDiagonal(int distance, double value)
 {
   for(int column = 0; column < theSize; ++column)
   {
-    // first do the upper neighbor
     int row = column - distance;
-    if(row < 0) row += theSize;
-    theMatrix[row][column] = value;
+    if(row < 0) continue;
+    theMatrix(row,column) = value;
+    theMatrix(column,row) = value;
 
-    // and the lower neighbor
-    // probably unnecessary in a symmetric matrix
-    row = column + distance;
-    if(row >= theSize) row -= theSize;
-    theMatrix[row][column] = value;
   }
 
-  computeNormalization(theNorma); 
+  computeNormalization(); 
+
+  isDiagonal_ = true;
+  checkOffDiagonal(isDiagonal_);
+
 }
 
 #include <iostream>
 void CaloCorrelatedNoisifier::noisify(CaloSamples & frame) 
 {
 
-  //check if the normalizations are avilable, otherwise compute them
-  if ( ! normaDone) computeNormalization(theNorma); 
-
-  // make a vector of random values
+ // make a vector of random values
   assert(frame.size() == theSize);
-  HepVector uncorrelated(theSize, theRandomGaussian);
-  
-  // rotate them to make a correlated noise vector
-  HepVector correlated = theMatrix * uncorrelated; 
+  std::valarray<double> uncorrelated(0.,theSize);
+  for (int i=0; i<theSize; i++)
+    uncorrelated[i]=theRandomGaussian.shoot();
 
-  // stuff 'em in the frame
-  for(int i = 0; i < theSize; ++i)
-  {
-    frame[i] += (correlated[i]/theNorma[i]);
+  if ( isDiagonal_ ) {     
+    for(int i = 0; i < theSize; ++i)
+      { frame[i] = uncorrelated[i]; }
+  }
+  
+  else {
+    
+    // rotate them to make a correlated noise vector
+    std::valarray<double> correlated = theMatrix * uncorrelated; 
+    
+    // stuff 'em in the frame
+    for(int i = 0; i < theSize; ++i)
+      {
+        frame[i] += (correlated[i]*theNorma[i]);
+      }
   }
 }
 
-void CaloCorrelatedNoisifier::computeNormalization(std::vector<double> & norma) 
+void CaloCorrelatedNoisifier::computeNormalization() 
 {
-  normaDone = false;
+  theNorma = 0;
+  caloMath::SparseMatrix<double>::const_iterator p = theMatrix.values().begin();
+  caloMath::SparseMatrix<double>::const_iterator e = theMatrix.values().end();
+  for (;p!=e;p++)
+    theNorma[(*p).i] += (*p).v*(*p).v;
+  for (int i=0; i<theSize; i++)
+    if (theNorma[i] <= 0. ) throw(std::runtime_error("CaloCorrelatedNoisifier:  normalization equal to zero."));
+  theNorma = 1./std::sqrt(theNorma);
 
-  for (int row = 0; row < theSize; ++row) {
+}
 
-    double part1 = 0.;
+void CaloCorrelatedNoisifier::checkOffDiagonal(bool & isDiagonal_){
 
-    for (int j = 0; j < theSize; ++j ) {
-      part1 += theMatrix[row][j]*theMatrix[row][j];
+  isDiagonal_ = true;
+
+  for ( int i = 0 ; i < theSize ; i++ ) {
+    for ( int j = 0 ; j < theSize ; j++ ) {
+
+      if ( i != j && theMatrix(i,j) != 0. ) { isDiagonal_ = false ; return ; }
+      
     }
-    
-    if ( part1 <= 0. ) throw(std::runtime_error("CaloCorrelatedNoisifier:  normalization equal to zero."));
-
-    theNorma[row] = sqrt(part1);
-
   }
-
-  normaDone = true;
+  
 
 }
