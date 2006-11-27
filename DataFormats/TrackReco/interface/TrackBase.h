b1d6a2f8ebbfb322eba9c7f916bf9c844026b1f2
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-11-27 12:06:58 +0000

    added JA changed

--- DataFormats/TrackReco/interface/TrackBase.h
+++ DataFormats/TrackReco/interface/TrackBase.h
@@ -8,23 +8,21 @@
  * and summary information of the hit pattern. Transverse momentum is
  * also stored to avoid access to magnetic field.
  *
- * Model of 5 perigee parameters for Track fit:<BR>
- * <B> (kappa, theta, phi_0, d_0, z_0) </B><BR>
+ * Model of 5 curvilinear parameters for Track fit:<BR>
+ * <B> (qoverp, lambda, phi_0, d_xy, d_sz) </B><BR>
  * defined as:  <BR>
- *   <DT> kappa = q / p_T = signed transverse curvature </DT> 
- *   <DT> theta = polar angle at pca. to the beam line </DT>
- *   <DT> phi_0 = azimuth angle at pca. to the beam line </DT>
- *   <DT> d_0 = signed transverse dca. to the beam line (positive if the beam is outside the circle) </DT>
- *   <DT> z_0 = z-coordinate of pca. to the beam line </DT>
+ *   <DT> qoverp = q / abs(p) = signed inverse of momentum </DT> 
+ *   <DT> lambda = pi/2 - polar angle at the given point </DT>
+ *   <DT> phi_0 = azimuth angle at the given point </DT>
+ *   <DT> d_xy = transverse distance to beam spot (from "straight line" extrapolation if (x,y,z) is not at dca) =  - x * sin(phi_0) + y * cos(phi_0) </DT>
+ *   <DT> d_sz = distance in SZ plane to beam spot (from "straight line" extrapolation if (x,y,z) is not at dca)  = z * cos(lambda) </DT>
  *
- * according to how described in the following documents: <BR>
+ * according to the definitions given in the following document: <BR>
  * <a href="http://cms.cern.ch/iCMS/jsp/openfile.jsp?type=NOTE&year=2006&files=NOTE2006_001.pdf">A. Strandlie, W. Wittek, "Propagation of Covariance Matrices...", CMS Note 2006/001</a> <BR>
- * P. Billoir, S. Qian, "Fast vertex fitting...", NIM A311 (1992) 139. <BR>
- * <a href="http://cmsdoc.cern.ch/cms/Physics/btau/management/activities/reconstruction/vertex/tutorial041112.d/node5.html#SECTION00053000000000000000">Track parametrization in vertex fitting (Vertex reconstruction tutorial)</a> <BR>
  * 
- * \author Thomas Speer, Luca Lista, Pascal Vanlaer
+ * \author Thomas Speer, Luca Lista, Pascal Vanlaer, Juan Alcaraz
  *
- * \version $Id: TrackBase.h,v 1.43 2006/10/31 19:23:28 llista Exp $
+ * \version $Id: TrackBase.h,v 1.44 2006/11/27 09:03:05 llista Exp $
  *
  */
 
@@ -54,7 +52,7 @@ namespace reco {
     /// point in the space
     typedef math::XYZPoint Point;
     /// enumerator provided indices to the five parameters
-    enum { i_transverseCurvature = 0 , i_theta, i_phi0, i_d0, i_dz }; 
+    enum { i_qoverp = 0 , i_lambda, i_phi0, i_dxy, i_dsz }; 
     /// spatial vector
     typedef math::XYZVector Vector;
     /// point in the space
@@ -77,35 +75,53 @@ namespace reco {
     /// track electric charge
     int charge() const { return charge_; }
     /// transverse curvature
-    double transverseCurvature() const { return charge() / pt(); }
+    double qoverp() const { return charge() / p(); }
     /// track azimutal angle at vertex
     double phi0() const { return momentum_.phi(); }
     /// polar angle  
     double theta() const { return momentum_.theta(); }
+    /// Lambda angle
+    double lambda() const { return M_PI/2 - momentum_.theta(); }
     /// track impact parameter (distance of closest approach to beamline)
-    double d0() const {  return ( vx() * py() - vy() * px() ) / p (); }
-    /// z coordniate of point of closest approach to beamline
-    double dz() const { return vz() * pz() / p(); }
+    double dxy() const { return ( - vx() * py() + vy() * px() ) / p (); }
+    /// track impact parameter in perigee convention (d0 = - dxy)
+    double d0() const { return - dxy(); }
+    /// sz distance to beamline
+    double dsz() const { return vz() * pt() / p(); }
+    /// z distance to beamline
+    double dz() const { return vz(); }
 
     /// track momentum vector
     const Vector & momentum() const { return momentum_; }
     /// position of point of closest approach to the beamline
     const Point & vertex() const { return vertex_; }
-    /// (i,j)-th element of covarianve matrix ( i, j = 0, ... 4 )
 
+    /// Parameters with one-to-one corerspondence to the covariance matrix
+    ParameterVector parameters() const { 
+      return ParameterVector(qoverp(),lambda(),phi(),dxy(),dsz());
+    }
+    /// (i,j)-th element of covarianve matrix ( i, j = 0, ... 4 )
     double covariance( int i, int j ) const { return covariance_[ covIndex( i, j ) ]; }
     /// error on specified element
     double error( int i ) const { return sqrt( covariance_[ covIndex( i, i ) ] ); }
     /// error on signed transverse curvature
-    double transverseCurvatureError() const { return error( i_transverseCurvature ); }
+    double qoverpError() const { return error( i_qoverp ); }
     /// error on theta
-    double thetaError() const { return error( i_theta ); }
+    double thetaError() const { return error( i_lambda ); }
+    /// error on lambda
+    double lambdaError() const { return error( i_lambda ); }
+    /// error on eta
+    double etaError() const { return error( i_lambda ) / sin(theta()); }
     /// error on phi0
     double phi0Error() const { return error( i_phi0 ); }
+    /// error on dxy
+    double dxyError() const { return error( i_dxy ); }
     /// error on d0
-    double d0Error() const { return error( i_d0 ); }
-    /// error on dx
-    double dzError() const { return error( i_dz ); }
+    double d0Error() const { return error( i_dxy ); }
+    /// error on dsz
+    double dszError() const { return error( i_dsz ); }
+    /// error on dz
+    double dzError() const { return error( i_dsz ) * p()/pt(); }
     /// return SMatrix
     CovarianceMatrix covariance() const { CovarianceMatrix m; fill( m ); return m; }
     /// fill SMatrix
