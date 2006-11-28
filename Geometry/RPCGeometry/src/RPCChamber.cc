c87d03a6079590048edd4be99e282570646cea34
Raffaello Trentadue <sha1-9504fe7d3bbc4a07e3333b20772b63251c823b73@cern.ch>
2006-11-28 10:22:01 +0000

    RPCChamber added

--- Geometry/RPCGeometry/src/RPCChamber.cc
+++ Geometry/RPCGeometry/src/RPCChamber.cc
@@ -1,8 +1,9 @@
 /** \file
  *
- *  $Date: 2006/09/21 15:33:47 $
- *  $Revision: 1.1 $
- *  \author Stefano Lacaprara - INFN Padova <stefano.lacaprara@pd.infn.it>
+ *  $Date: 2006/09/21 15:39:48 $
+ *  $Revision: 1.2 $
+ *  Author: Raffaello Trentadue - Università Bari 
+ *  Mail:     <raffaello.trentadue@ba.infn.it>
  */
 
 /* This Class Header */
@@ -11,7 +12,7 @@
 /* Collaborating Class Header */
 #include "Geometry/RPCGeometry/interface/RPCRoll.h"
 #include "DataFormats/MuonDetId/interface/RPCDetId.h"
-#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+
 
 /* C++ Headers */
 #include <iostream>
@@ -19,59 +20,82 @@
 /* ====================================================================== */
 
 /* Constructor */ 
-RPCChamber::RPCChamber(RPCDetId id, const BoundPlane::BoundPlanePointer& plane) :
-  GeomDet(plane), 
-  theId(id) {
-}
+RPCChamber::RPCChamber(RPCDetId id, 
+		       const ReferenceCountingPointer<BoundPlane> & plane) :
+  GeomDet(plane), theId(id)
+{}
 
 /* Destructor */ 
 RPCChamber::~RPCChamber() {
-    // delete all rolls associated to this chamber
-  for (std::vector<const RPCRoll*>::const_iterator iroll = theRolls.begin();
-       iroll != theRolls.end(); ++iroll){
-    delete (*iroll);
-  }
+}
+
+DetId 
+RPCChamber::geographicalId() const
+{
+  return theId;
+}
+
+RPCDetId
+RPCChamber::id() const
+{
+  return theId;
 }
 
 /* Operations */ 
-DetId RPCChamber::geographicalId() const {
-  return theId;
+
+bool 
+RPCChamber::operator==(const RPCChamber& ch) const {
+  return this->id()==ch.id();
 }
 
-RPCDetId RPCChamber::id() const {
-  return theId;
-}
 
-bool RPCChamber::operator==(const RPCChamber& ch) const {
-  return id()==ch.id();
-}
 
-void RPCChamber::add(RPCRoll* rl) {
+void 
+RPCChamber::add(RPCRoll* rl) {
   theRolls.push_back(rl);
 }
 
-std::vector<const GeomDet*> RPCChamber::components() const {
+
+
+std::vector<const GeomDet*> 
+RPCChamber::components() const {
   return  std::vector<const GeomDet*>(theRolls.begin(), theRolls.end());
 }
 
-const GeomDet* RPCChamber::component(DetId id) const {
+
+
+const GeomDet* 
+RPCChamber::component(DetId id) const {
   return roll(RPCDetId(id.rawId()));
 }
 
-const std::vector<const RPCRoll*>& RPCChamber::rolls() const {
+
+const std::vector<const RPCRoll*>& 
+RPCChamber::rolls() const 
+{
   return theRolls;
 }
 
-const RPCRoll* RPCChamber::roll(RPCDetId id) const{
+const int RPCChamber::nrolls() const
+{
+  return theRolls.size();
+}
+
+const RPCRoll* 
+RPCChamber::roll(RPCDetId id) const
+{
   if (id.chamberId()!=theId) return 0; // not in this Roll!
   return roll(id.roll());
 }
 
 
-const RPCRoll* RPCChamber::roll(int isl) const {
+const RPCRoll* 
+RPCChamber::roll(int isl) const 
+{
   for (std::vector<const RPCRoll*>::const_iterator i = theRolls.begin();
        i!= theRolls.end(); ++i) {
-    if ((*i)->id().roll()==isl) return (*i);
+    if ((*i)->id().roll()==isl) 
+      return (*i);
   }
   return 0;
 }
