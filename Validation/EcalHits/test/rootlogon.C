e69d8c43b9b30790abc5d1bec8a48ef7582af58c
xhuang <sha1-4bd1aee960316b9472cdc249dc0e7db0c29cbfd7@cern.ch>
2006-11-28 21:06:36 +0000

    temporary solution for running root macros

--- Validation/EcalHits/test/rootlogon.C
+++ Validation/EcalHits/test/rootlogon.C
@@ -4,5 +4,7 @@ Cintex::Enable();
 cout << "Loading FWLite..." << endl;
 gSystem->Load("libFWCoreFWLite");
 AutoLibraryLoader::enable();
-
+gSystem->Load("libSimDataFormatsEcalValidation.so");
+gSystem->Load("libSimDataFormatsTrack.so");
+gSystem->Load("libSimDataFormatsVertex.so");
 }
