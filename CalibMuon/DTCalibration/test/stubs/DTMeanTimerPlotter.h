ef4142da8f0452e9a2798f9c6c0bcd5ed7133850
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2006-11-28 14:31:59 +0000

    meantimer plotter

---
+++ CalibMuon/DTCalibration/test/stubs/DTMeanTimerPlotter.h
@@ -0,0 +1,69 @@
+#ifndef DTMeanTimerPlotter_H
+#define DTMeanTimerPlotter_H
+
+/** \class DTMeanTimerPlotter
+ *  Utility class to plot MeanTimer produced with DTVDriftCalibration.
+ *  The tmax histograms can be fitted using exactly the same code that is
+ *  used by the calibration application.
+ *
+ *  $Date: 2006/11/27 12:26:32 $
+ *  $Revision: 1.0 $
+ *  \author S. Bolognesi - INFN Torino
+ */
+
+#include "TString.h"
+#include <vector>
+
+
+class TFile;
+class TCanvas;
+class TH1D;
+class TF1;
+
+class DTMeanTimerPlotter {
+public:
+  /// Constructor
+  DTMeanTimerPlotter(TFile *file);
+
+
+  /// Destructor
+  virtual ~DTMeanTimerPlotter();
+
+  // Operations
+
+  /// Plot the time box of a given superlayer.
+  /// Options: "same" -> histo drawn in the active canvas, 
+  ///          "SingleDeltaT0" or "SingleFormula" -> fit and draw each TMax histo separately (in the same canvas) 
+  ///          "fit" -> draw and fit the histos
+  void plotMeanTimer(int wheel, int station, int sector, int sl,
+		    const TString& drawOptions = "");
+ 
+   /// Set the verbosity of the output: 0 = silent, 1 = info, 2 = debug
+  void setVerbosity(unsigned int lvl);
+  /// Set rebin number
+  void setRebinning(unsigned int rebin);
+  /// Reset the counter for histos color
+  void resetColor();
+
+protected:
+
+private:
+
+  //Plot the TMax histogram for each deltaT0: THESE ARE NOT USED TO COMPUTE VDRIFT
+  std::vector<TH1D*>  plotSingleTMaxDeltaT0 (TString& name);
+  //Plot the TMax histogram for each formula
+  std::vector<TH1D*> plotSingleTMaxFormula (TString& name);
+  //Plot the total TMax histograms: THIS IS NOT USED TO COMPUTE VDRIFT
+  std::vector<TH1D*> plotTotalTMax (TString& name);
+
+  void plotHistos(std::vector<TH1D*> hTMaxes, TString& name, const TString& drawOptions);
+  TString getHistoNameSuffix(int wheel, int station, int sector, int sl);
+  std::vector<TF1*> fitTMaxes(std::vector<TH1D*> histo);
+  double getMaximum(std::vector<TH1D*> hTMaxes);
+
+  TFile *theFile;
+  unsigned int theVerbosityLevel;
+  unsigned int theRebinning;
+  unsigned int color;
+};
+#endif
