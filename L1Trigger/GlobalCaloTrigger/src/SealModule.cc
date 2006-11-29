19ea3302591da5d752a3e9925c693dbcf4faf187
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2006-11-29 18:13:21 +0000

    *** empty log message ***

--- L1Trigger/GlobalCaloTrigger/src/SealModule.cc
+++ L1Trigger/GlobalCaloTrigger/src/SealModule.cc
@@ -3,9 +3,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctEmulator.h"
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctTestAnalyzer.h"
-#include "L1Trigger/GlobalCaloTrigger/src/L1FakeRctProducer.h"
+#include "L1Trigger/GlobalCaloTrigger/src/FakeGctInputProducer.h"
+#include "L1Trigger/GlobalCaloTrigger/src/FakeGctInputTester.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L1GctEmulator);
 DEFINE_ANOTHER_FWK_MODULE(L1GctTestAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(L1FakeRctProducer);
+DEFINE_ANOTHER_FWK_MODULE(FakeGctInputProducer);
+DEFINE_ANOTHER_FWK_MODULE(FakeGctInputTester);
