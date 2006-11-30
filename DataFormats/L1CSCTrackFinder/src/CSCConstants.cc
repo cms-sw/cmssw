4d273e54cb1f511c056a61244c079f533c3716b6
Lindsey Gray <sha1-3ec7abf2a9e8e001c55b41ebd09d9de6b752a31e@cern.ch>
2006-11-30 06:40:56 +0000

    Moved TrackStubs here for persistence.

---
+++ DataFormats/L1CSCTrackFinder/src/CSCConstants.cc
@@ -0,0 +1,9 @@
+#include <L1Trigger/CSCCommonTrigger/interface/CSCConstants.h>
+
+const double CSCConstants::RAD_PER_DEGREE = M_PI/180.;
+const double CSCConstants::SECTOR1_CENT_DEG = 45;
+const double CSCConstants::SECTOR1_CENT_RAD = CSCConstants::SECTOR1_CENT_DEG * CSCConstants::RAD_PER_DEGREE;
+const double CSCConstants::SECTOR_DEG = 62.;
+const double CSCConstants::SECTOR_RAD = CSCConstants::SECTOR_DEG*CSCConstants::RAD_PER_DEGREE; // radians
+const double CSCConstants::minEta = .9;
+const double CSCConstants::maxEta = 2.5;
--- DataFormats/L1CSCTrackFinder/src/classes.h
+++ DataFormats/L1CSCTrackFinder/src/classes.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <DataFormats/L1CSCTrackFinder/interface/L1TrackId.h>
 #include <DataFormats/L1CSCTrackFinder/interface/L1Track.h>
+#include <DataFormats/L1CSCTrackFinder/interface/CSCTrackStub.h>
+#include <DataFormats/L1CSCTrackFinder/interface/CSCTriggerContainer.h>
 #include <DataFormats/Common/interface/Wrapper.h>
 
 namespace
@@ -9,14 +11,18 @@ namespace
     {
       csc::L1Track cL1TRK;
       csc::L1TrackId cL1TRKID;
-
+      CSCTrackStub cTrkStb;
+      
       std::pair<csc::L1Track,MuonDigiCollection<CSCDetId,CSCCorrelatedLCTDigi> > vL1Trk;
 
       std::vector<csc::L1Track> vL1TRK;
       std::vector<csc::L1TrackId> vL1TRKID;
       std::vector<std::pair<csc::L1Track,MuonDigiCollection<CSCDetId,CSCCorrelatedLCTDigi> > > vL1TrkColl;
-      
+      std::vector<CSCTrackStub> vTrkStb;
+      CSCTriggerContainer<CSCTrackStub> tcTrkStb;
+
       edm::Wrapper<std::vector<csc::L1Track> > wL1TRK;
       edm::Wrapper<std::vector<std::pair<csc::L1Track,MuonDigiCollection<CSCDetId,CSCCorrelatedLCTDigi> > > > wL1TrkColl;
+      edm::Wrapper<CSCTriggerContainer<CSCTrackStub> > wTrkStb;
     }
 }
