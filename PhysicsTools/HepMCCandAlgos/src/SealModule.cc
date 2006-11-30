4152cd466ed3d490be91fe2bf8fd8684eb744706
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-11-30 12:00:28 +0000

    fixed mother references

--- PhysicsTools/HepMCCandAlgos/src/SealModule.cc
+++ PhysicsTools/HepMCCandAlgos/src/SealModule.cc
@@ -4,7 +4,6 @@
 #include "PhysicsTools/HepMCCandAlgos/src/GenParticleCandidateProducer.h"
 #include "PhysicsTools/HepMCCandAlgos/src/GenParticleCandidateSelector.h"
 #include "PhysicsTools/HepMCCandAlgos/src/HepMCCandidateSelector.h"
-#include "PhysicsTools/HepMCCandAlgos/src/SetGenParticleMotherReference.h"
 #include "PhysicsTools/HepMCCandAlgos/src/ParticleTreeDrawer.h"
 #include "PhysicsTools/HepMCCandAlgos/src/MCTruthDeltaRMatcher.h"
 #include "PhysicsTools/HepMCCandAlgos/src/MCTruthCompositeMatcher.h"
@@ -14,7 +13,6 @@ DEFINE_ANOTHER_FWK_MODULE( HepMCCandidateProducer );
 DEFINE_ANOTHER_FWK_MODULE( GenParticleCandidateProducer );
 DEFINE_ANOTHER_FWK_MODULE( GenParticleCandidateSelector );
 DEFINE_ANOTHER_FWK_MODULE( HepMCCandidateSelector );
-DEFINE_ANOTHER_FWK_MODULE( SetGenParticleMotherReference );
 DEFINE_ANOTHER_FWK_MODULE( ParticleTreeDrawer );
 DEFINE_ANOTHER_FWK_MODULE( MCTruthDeltaRMatcher );
 DEFINE_ANOTHER_FWK_MODULE( MCTruthCompositeMatcher );
