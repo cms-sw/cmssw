86ff0d73934d1cca0d4893462ceab60c712286fe
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-12-01 03:23:55 +0000

    Replace deprecated Reflex function HasKey() with HasProperty()

--- DataFormats/Common/src/BranchDescription.cc
+++ DataFormats/Common/src/BranchDescription.cc
@@ -6,7 +6,7 @@
 
 /*----------------------------------------------------------------------
 
-$Id: BranchDescription.cc,v 1.20 2006/11/04 07:16:18 wmtan Exp $
+$Id: BranchDescription.cc,v 1.21 2006/11/07 00:29:38 wmtan Exp $
 
 ----------------------------------------------------------------------*/
 
@@ -90,7 +90,7 @@ namespace edm {
 
     ROOT::Reflex::Type t = ROOT::Reflex::Type::ByName(fullClassName());
     ROOT::Reflex::PropertyList p = t.Properties();
-    transient_ = (p.HasKey("persistent") ? p.PropertyAsString("persistent") == std::string("false") : false);
+    transient_ = (p.HasProperty("persistent") ? p.PropertyAsString("persistent") == std::string("false") : false);
   }
 
   ParameterSetID const&
