84e9f6ba281a76ec178ed019865702a16cfa79d6
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2006-12-06 16:41:55 +0000

    Make the schedule validator properly handle - and ! symbols which may preceed module labels in paths, this was not working correctly before

--- FWCore/ParameterSet/src/ScheduleValidator.cc
+++ FWCore/ParameterSet/src/ScheduleValidator.cc
@@ -3,11 +3,11 @@
    Implementation of class ScheduleValidator
 
    \author Stefano ARGIRO
-   \version $Id: ScheduleValidator.cc,v 1.11 2006/08/07 23:52:36 rpw Exp $
+   \version $Id: ScheduleValidator.cc,v 1.12 2006/08/28 19:14:58 rpw Exp $
    \date 10 Jun 2005
 */
 
-static const char CVSId[] = "$Id: ScheduleValidator.cc,v 1.11 2006/08/07 23:52:36 rpw Exp $";
+static const char CVSId[] = "$Id: ScheduleValidator.cc,v 1.12 2006/08/28 19:14:58 rpw Exp $";
 
 #include "FWCore/ParameterSet/src/ScheduleValidator.h"
 #include "FWCore/Utilities/interface/EDMException.h"
@@ -81,6 +81,14 @@ void ScheduleValidator::validate(){
 
   dependencies_.clear();
 
+  std::string leafName;
+  std::string sonName;
+  std::string leftName;
+
+  leafName.reserve(100);
+  sonName.reserve(100);
+  leftName.reserve(100);
+
   // iterate on leaf nodes
   std::list<NodePtr>::iterator leafIt;
   for (leafIt=leaves_.begin(); leafIt!=leaves_.end(); ++leafIt){
@@ -103,7 +111,16 @@ void ScheduleValidator::validate(){
 
       OperatorNode* node = dynamic_cast<OperatorNode*>(p);
       // make sure we don't redescend where we came from
-      if (son->name() != node->left()->name()) findDeps(node->left(), dep);
+
+      sonName = son->name();
+      if (sonName.size() > 0 &&
+          (sonName[0] == '!' || sonName[0] == '-')) sonName.erase(0,1);
+
+      leftName = node->left()->name();
+      if (leftName.size() > 0 &&
+          (leftName[0] == '!' || leftName[0] == '-')) leftName.erase(0,1);
+
+      if (sonName != leftName) findDeps(node->left(), dep);
       
       son=p;
       p= p->getParent();
@@ -113,7 +130,12 @@ void ScheduleValidator::validate(){
     dep.unique(); // removes duplicates
 
     // insert the list of deps
-    Dependencies::iterator depIt= dependencies_.find((*leafIt)->name());
+
+    leafName = (*leafIt)->name();
+    if (leafName.length() > 0 && 
+        (leafName[0] == '!' || leafName[0] == '-')) leafName.erase(0,1);
+
+    Dependencies::iterator depIt= dependencies_.find(leafName);
     if (depIt!= dependencies_.end()){
       DependencyList& old_deplist = (*depIt).second;
     
@@ -129,7 +151,7 @@ void ScheduleValidator::validate(){
 
 	throw edm::Exception(errors::Configuration,"InconsistentSchedule")
 	  << "Inconsistent schedule for module "
-	  << (*leafIt)->name()
+	  << leafName
 	  << "\n"
 	  << "Depends on " << olddepstr.str() 
 	  << " but also on " << newdepstr.str()
@@ -137,7 +159,7 @@ void ScheduleValidator::validate(){
       }
     }
     else {
-      dependencies_[(*leafIt)->name()] = dep;
+      dependencies_[leafName] = dep;
     }
 
   }//for leaf
@@ -147,7 +169,16 @@ void ScheduleValidator::validate(){
 void ScheduleValidator::findDeps(NodePtr& node, DependencyList& dep){
 
   // if we have an operand, add it to the list of dependencies
-  if (node->type() =="operand") dep.push_back(node->name());
+  if (node->type() =="operand") {
+
+    if (node->name().size() > 0 &&
+        (node->name()[0] == '!' || node->name()[0] == '-')) {
+      std::string nodeName = node->name();
+      nodeName.erase(0,1);
+      dep.push_back(nodeName);
+    }
+    else dep.push_back(node->name());
+  }
   // else follow the tree, unless the leaf is contained in the node
   else{
 
