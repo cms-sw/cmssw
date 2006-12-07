c4f1139d36b5ca077ddd6e0fc7835523457acfbe
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2006-12-07 01:03:44 +0000

    New directory: Records

---
+++ RecoBTag/Records/interface/SoftLeptonBTagRecord.h
@@ -0,0 +1,10 @@
+#ifndef RecoBTag_Records_SoftLeptonBTagRecord_h
+#define RecoBTag_Records_SoftLeptonBTagRecord_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+
+class SoftLeptonBTagRecord : public edm::eventsetup::EventSetupRecordImplementation<SoftLeptonBTagRecord> {};
+
+#endif 
+
