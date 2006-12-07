ed2c0f8012c40798524259a502edad3902161253
Juan Alcaraz Maestre <sha1-00c52069b91662a716b700699899914afb62bf4e@cern.ch>
2006-12-07 10:21:09 +0000

    Drop L3MuonAltProducer

--- RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
+++ RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
@@ -2,10 +2,8 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoMuon/L3MuonIsolationProducer/src/L3MuonAltProducer.h"
 #include "RecoMuon/L3MuonIsolationProducer/src/L3MuonIsolationProducer.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(L3MuonAltProducer);
 DEFINE_ANOTHER_FWK_MODULE(L3MuonIsolationProducer);
 
