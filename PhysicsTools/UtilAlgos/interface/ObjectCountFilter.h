863b6ef301735b9454ab709a3e740e8600429a90
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-12-07 10:53:14 +0000

    generalized template module

--- PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
+++ PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
@@ -6,9 +6,9 @@
  * 
  * \author Luca Lista, INFN
  *
- * \version $Revision: 1.4 $
+ * \version $Revision: 1.5 $
  *
- * $Id: ObjectCountFilter.h,v 1.4 2006/10/03 09:02:10 llista Exp $
+ * $Id: ObjectCountFilter.h,v 1.5 2006/11/23 14:27:47 llista Exp $
  *
  */
 
@@ -18,75 +18,61 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 #include "PhysicsTools/Utilities/interface/AnySelector.h"
-#include <algorithm>
+#include "PhysicsTools/Utilities/interface/MinNumberSelector.h"
 
-
-template<typename S>
-class ObjectCountFilterBase : public edm::EDFilter {
-public:
-  /// constructor 
-  explicit ObjectCountFilterBase( const edm::ParameterSet & cfg ) :
-    src_( cfg.template getParameter<edm::InputTag>( "src" ) ),
-    minNumber_( 1 ) {
-    std::vector<std::string> ints = cfg.template getParameterNamesForType<unsigned int>();
-    const std::string minNumber( "minNumber" );
-    bool foundMinNumber = std::find( ints.begin(), ints.end(), minNumber ) != ints.end();
-    if ( foundMinNumber )
-      minNumber_ = cfg.template getParameter<unsigned int>( minNumber );
-  }
-  
-protected:
-  /// source collection label
-  edm::InputTag src_;
-  /// minimum number of entries in a collection
-  unsigned int minNumber_;
-};
-
-template<typename C, typename S = AnySelector<typename C::value_type> >
-class ObjectCountFilter : 
-  public ObjectCountFilterBase<S> {
+template<typename C, 
+	 typename S = AnySelector<typename C::value_type>, 
+	 typename N = MinNumberSelector>
+class ObjectCountFilter : public edm::EDFilter {
 public:
   /// constructor 
   explicit ObjectCountFilter( const edm::ParameterSet & cfg ) :
-    ObjectCountFilterBase<S>( cfg ),
-    select_( cfg ) {
+    src_( cfg.template getParameter<edm::InputTag>( "src" ) ),
+    select_( cfg ),
+    sizeSelect_( cfg ) {
   }
   
 private:
-  typedef ObjectCountFilterBase<S> base;
   /// process one event
   bool filter( edm::Event& evt, const edm::EventSetup& ) {
     edm::Handle<C> source;
-    evt.getByLabel( base::src_, source );
+    evt.getByLabel( src_, source );
     size_t n = 0;
     for( typename C::const_iterator i = source->begin(); i != source->end(); ++ i ) {
       if ( select_( * i ) ) n ++;
-      if ( n >= base::minNumber_ ) return true;
+      if ( sizeSelect_( n ) ) return true;
     }
     return false;
   }
+  /// source collection label
+  edm::InputTag src_;
   /// object filter
   S select_;
+  /// minimum number of entries in a collection
+  N sizeSelect_;
 };
 
 
-template<typename C>
-class ObjectCountFilter<C, AnySelector<typename C::value_type> > : 
-  public ObjectCountFilterBase<AnySelector<typename C::value_type> > {
+template<typename C, typename N>
+class ObjectCountFilter<C, AnySelector<typename C::value_type>, N> : public edm::EDFilter {
 public:
   /// constructor 
   explicit ObjectCountFilter( const edm::ParameterSet & cfg ) :
-    ObjectCountFilterBase<AnySelector<typename C::value_type> >( cfg ) {
+    src_( cfg.template getParameter<edm::InputTag>( "src" ) ),
+    sizeSelect_( cfg ) {
   }
   
 private:
-  typedef ObjectCountFilterBase<AnySelector<typename C::value_type> > base;
   /// process one event
   bool filter( edm::Event& evt, const edm::EventSetup& ) {
     edm::Handle<C> source;
-    evt.getByLabel( base::src_, source );
-    return source->size() >= base::minNumber_;
+    evt.getByLabel( src_, source );
+    return sizeSelect_( source->size() );
   }
+  /// source collection label
+  edm::InputTag src_;
+  /// minimum number of entries in a collection
+  N sizeSelect_;
 };
 
 #endif
