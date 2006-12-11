442f7beb121a65c83b4bd250f662f12120621892
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2006-12-11 20:42:28 +0000

    first update

---
+++ Validation/GlobalDigis/src/GlobalDigisProducer.cc
@@ -0,0 +1,201 @@
+#include "Validation/GlobalDigis/interface/GlobalDigisProducer.h"
+
+GlobalDigisProducer::GlobalDigisProducer(const edm::ParameterSet& iPSet) :
+  fName(""), verbosity(0), frequency(0), label(""), getAllProvenances(false),
+  printProvenanceInfo(false), count(0)
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_GlobalDigisProducer";
+
+  // get information from parameter set
+  fName = iPSet.getUntrackedParameter<std::string>("Name");
+  verbosity = iPSet.getUntrackedParameter<int>("Verbosity");
+  frequency = iPSet.getUntrackedParameter<int>("Frequency");
+  label = iPSet.getParameter<std::string>("Label");
+  edm::ParameterSet m_Prov =
+    iPSet.getParameter<edm::ParameterSet>("ProvenanceLookup");
+  getAllProvenances = 
+    m_Prov.getUntrackedParameter<bool>("GetAllProvenances");
+  printProvenanceInfo = 
+    m_Prov.getUntrackedParameter<bool>("PrintProvenanceInfo");
+
+  //get Labels to use to extract information
+  ECalEBSrc_ = iPSet.getParameter<edm::InputTag>("ECalEBSrc");
+  ECalEESrc_ = iPSet.getParameter<edm::InputTag>("ECalEESrc");
+
+  // use value of first digit to determine default output level (inclusive)
+  // 0 is none, 1 is basic, 2 is fill output, 3 is gather output
+  verbosity %= 10;
+
+  // create persistent object
+  produces<PGlobalDigi>(label);
+
+  // print out Parameter Set information being used
+  if (verbosity >= 0) {
+    edm::LogInfo(MsgLoggerCat) 
+      << "\n===============================\n"
+      << "Initialized as EDProducer with parameter values:\n"
+      << "    Name          = " << fName << "\n"
+      << "    Verbosity     = " << verbosity << "\n"
+      << "    Frequency     = " << frequency << "\n"
+      << "    Label         = " << label << "\n"
+      << "    GetProv       = " << getAllProvenances << "\n"
+      << "    PrintProv     = " << printProvenanceInfo << "\n"
+      << "    ECalEBSrc     = " << ECalEBSrc_.label() 
+      << ":" << ECalEBSrc_.instance() << "\n"
+      << "    ECalEESrc     = " << ECalEESrc_.label() 
+      << ":" << ECalEESrc_.instance() << "\n"
+      << "===============================\n";
+  }
+}
+
+GlobalDigisProducer::~GlobalDigisProducer() 
+{
+}
+
+void GlobalDigisProducer::beginJob(const edm::EventSetup& iSetup)
+{
+  clear();
+  return;
+}
+
+void GlobalDigisProducer::endJob()
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_endJob";
+  if (verbosity >= 0)
+    edm::LogInfo(MsgLoggerCat) 
+      << "Terminating having processed " << count << " events.";
+  return;
+}
+
+void GlobalDigisProducer::produce(edm::Event& iEvent, 
+				  const edm::EventSetup& iSetup)
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_produce";
+
+  // keep track of number of events processed
+  ++count;
+
+  // get event id information
+  int nrun = iEvent.id().run();
+  int nevt = iEvent.id().event();
+
+  if (verbosity > 0) {
+    edm::LogInfo(MsgLoggerCat)
+      << "Processing run " << nrun << ", event " << nevt
+      << " (" << count << " events total)";
+  } else if (verbosity == 0) {
+    if (nevt%frequency == 0 || nevt == 1) {
+      edm::LogInfo(MsgLoggerCat)
+	<< "Processing run " << nrun << ", event " << nevt
+	<< " (" << count << " events total)";
+    }
+  }
+
+  // clear event holders
+  clear();
+
+  // look at information available in the event
+  if (getAllProvenances) {
+
+    std::vector<const edm::Provenance*> AllProv;
+    iEvent.getAllProvenance(AllProv);
+
+    if (verbosity >= 0)
+      edm::LogInfo(MsgLoggerCat)
+	<< "Number of Provenances = " << AllProv.size();
+
+    if (printProvenanceInfo && (verbosity >= 0)) {
+      TString eventout("\nProvenance info:\n");      
+
+      for (unsigned int i = 0; i < AllProv.size(); ++i) {
+	eventout += "\n       ******************************";
+	eventout += "\n       Module       : ";
+	eventout += (AllProv[i]->product).moduleLabel();
+	eventout += "\n       ProductID    : ";
+	eventout += (AllProv[i]->product).productID_.id_;
+	eventout += "\n       ClassName    : ";
+	eventout += (AllProv[i]->product).fullClassName_;
+	eventout += "\n       InstanceName : ";
+	eventout += (AllProv[i]->product).productInstanceName_;
+	eventout += "\n       BranchName   : ";
+	eventout += (AllProv[i]->product).branchName_;
+      }
+      eventout += "\n       ******************************\n";
+      edm::LogInfo(MsgLoggerCat) << eventout << "\n";
+      printProvenanceInfo = false;
+    }
+    getAllProvenances = false;
+  }
+
+  // call fill functions
+  // gather Ecal information from event
+  fillECal(iEvent, iSetup);
+
+  if (verbosity > 0)
+    edm::LogInfo (MsgLoggerCat)
+      << "Done gathering data from event.";
+
+  // produce object to put into event
+  std::auto_ptr<PGlobalDigi> pOut(new PGlobalDigi);
+
+  if (verbosity > 2)
+    edm::LogInfo (MsgLoggerCat)
+      << "Saving event contents:";
+
+  // call store functions
+  // store ECal information in produce
+  storeECal(*pOut);
+
+  // store information in event
+  iEvent.put(pOut,label);
+
+  return;
+}
+
+void GlobalDigisProducer::fillECal(edm::Event& iEvent, 
+				   const edm::EventSetup& iSetup)
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_fillECal";
+
+  TString eventout;
+  if (verbosity > 0)
+    eventout = "\nGathering info:";  
+
+  if (verbosity > 0)
+    edm::LogInfo(MsgLoggerCat) << eventout << "\n";
+
+  return;
+}
+
+void GlobalDigisProducer::storeECal(PGlobalDigi& product)
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_storeECal";
+
+  return;
+}
+
+void GlobalDigisProducer::clear()
+{
+  std::string MsgLoggerCat = "GlobalDigisProducer_clear";
+
+  if (verbosity > 0)
+    edm::LogInfo(MsgLoggerCat)
+      << "Clearing event holders"; 
+
+  // reset electromagnetic info
+  // EE info
+  EECalADC.clear();
+  EECalmaxPos.clear(); 
+  EECalAEE.clear(); 
+  EECalSHE.clear();
+  // EB info
+  EBCalADC.clear();
+  EBCalmaxPos.clear(); 
+  EBCalAEE.clear(); 
+  EBCalSHE.clear();
+
+  return;
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(GlobalDigisProducer);
