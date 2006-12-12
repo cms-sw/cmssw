8a68f5faf7ab2e7c8c918f2a79276ff1471e2166
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-12-12 14:13:52 +0000

    added TFileService

---
+++ PhysicsTools/Utilities/src/SealPlugin.cc
@@ -0,0 +1,5 @@
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "PhysicsTools/Utilities/interface/TFileService.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_SERVICE( TFileService );
