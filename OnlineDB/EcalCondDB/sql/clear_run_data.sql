9a2a069c89c13d1eafa9006f29062eafc7ca5477
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2006-12-12 17:07:44 +0000

    Add channel masking tables

--- OnlineDB/EcalCondDB/sql/clear_run_data.sql
+++ OnlineDB/EcalCondDB/sql/clear_run_data.sql
@@ -6,3 +6,9 @@
 
 DROP TABLE run_dat;
 DROP TABLE run_config_dat;
+DROP TABLE run_crystal_ch_errors_dat;
+DROP TABLE run_pn_ch_errors_dat;
+DROP TABLE run_mem_ch_errors_dat;
+DROP TABLE run_tt_ch_errors_dat;
+DROP TABLE error_dictionary_def;
+DROP SEQUENCE error_dictionary_def_sq;
--- OnlineDB/EcalCondDB/sql/create_run_data.sql
+++ OnlineDB/EcalCondDB/sql/create_run_data.sql
@@ -37,3 +37,61 @@ CREATE TABLE run_h4_table_position_dat (
 
 ALTER TABLE run_h4_table_position_dat ADD CONSTRAINT run_h4_table_position_dat_pk PRIMARY KEY (iov_id, logic_id);
 ALTER TABLE run_h4_table_position_dat ADD CONSTRAINT run_h4_table_position_dat_fk FOREIGN KEY (iov_id) REFERENCES run_iov (iov_id);
+
+
+
+
+-- Tables for channel masking
+CREATE TABLE run_crystal_ch_errors_dat (
+  iov_id		NUMBER(10),
+  logic_id		NUMBER(10),
+  error_bits		NUMBER(38)
+);
+
+ALTER TABLE run_crystal_ch_errors_dat ADD CONSTRAINT run_crystal_ch_errors_dat_pk PRIMARY KEY (iov_id, logic_id);
+ALTER TABLE run_crystal_ch_errors_dat ADD CONSTRAINT run_crystal_ch_errors_dat_fk FOREIGN KEY (iov_id) REFERENCES run_iov (iov_id);
+
+
+
+CREATE TABLE run_pn_ch_errors_dat (
+  iov_id		NUMBER(10),
+  logic_id		NUMBER(10),
+  error_bits		NUMBER(38)
+);
+
+ALTER TABLE run_pn_ch_errors_dat ADD CONSTRAINT run_pn_ch_errors_dat_pk PRIMARY KEY (iov_id, logic_id);
+ALTER TABLE run_pn_ch_errors_dat ADD CONSTRAINT run_pn_ch_errors_dat_fk FOREIGN KEY (iov_id) REFERENCES run_iov (iov_id);
+
+
+
+CREATE TABLE run_mem_ch_errors_dat (
+  iov_id		NUMBER(10),
+  logic_id		NUMBER(10),
+  error_bits		NUMBER(38)
+);
+
+ALTER TABLE run_mem_ch_errors_dat ADD CONSTRAINT run_mem_ch_errors_dat_pk PRIMARY KEY (iov_id, logic_id);
+ALTER TABLE run_mem_ch_errors_dat ADD CONSTRAINT run_mem_ch_errors_dat_fk FOREIGN KEY (iov_id) REFERENCES run_iov (iov_id);
+
+
+
+CREATE TABLE run_tt_ch_errors_dat (
+  iov_id		NUMBER(10),
+  logic_id		NUMBER(10),
+  error_bits		NUMBER(38)
+);
+
+ALTER TABLE run_tt_ch_errors_dat ADD CONSTRAINT run_tt_ch_errors_dat_pk PRIMARY KEY (iov_id, logic_id);
+ALTER TABLE run_tt_ch_errors_dat ADD CONSTRAINT run_tt_ch_errors_dat_fk FOREIGN KEY (iov_id) REFERENCES run_iov (iov_id);
+
+CREATE TABLE error_dictionary_def (
+  def_id		NUMBER(10),
+  error_mask		NUMBER(38),
+  short_desc		VARCHAR2(100),
+  long_desc		VARCHAR2(1000)
+);
+
+CREATE SEQUENCE error_dictionary_def_sq INCREMENT BY 1 START WITH 1;
+ALTER TABLE error_dictionary_def ADD CONSTRAINT error_dictionary_def_pk PRIMARY KEY (def_id);
+ALTER TABLE error_dictionary_def ADD CONSTRAINT error_dictionary_def_uk UNIQUE (error_mask);
+CREATE INDEX error_dictionary_def_ix ON error_dictionary_def(short_desc);
