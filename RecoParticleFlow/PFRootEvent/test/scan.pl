887948f28596497ab07f6d2a2cd81af6b6631898
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-12-12 15:39:15 +0000

    works on LSF for parameter scans \!

--- RecoParticleFlow/PFRootEvent/test/scan.pl
+++ RecoParticleFlow/PFRootEvent/test/scan.pl
@@ -87,15 +87,21 @@ my @values = split(" ",$svalues);
 my $masterprocess = "Macros/process.C";
 
 
-foreach my $rootfile (@rootfiles) {
-    chomp $rootfile;
-    my $fullfilename = "$basedir/$rootfile";
-    print "processing :  $fullfilename\n";
+foreach my $value (@values) {
+    my $valdir = "$tag1\_$tag2\_$value";
+    my $outdir = "$pwd/$scandir/$valdir";
     
-    if($svalues == 0) {
-	my $optfile = "$pwd/$scandir/auto_$rootfile.opt";
-	my $outrootfile = "$pwd/$scandir/out_$rootfile";
+    `mkdir $outdir`;
 
+    foreach my $rootfile (@rootfiles) {
+	chomp $rootfile;
+	my $fullfilename = "$basedir/$rootfile";
+	print "processing :  $fullfilename : $tag1 $tag2 $value\n";
+	
+#    if($svalues == 0) {
+	my $optfile = "$outdir/auto_$rootfile.opt";
+	my $outrootfile = "$outdir/out_$rootfile";
+	
 	open(IN, "<$masterfile");
 	open(OUT, ">$optfile");
 	while ( <IN> ) {
@@ -108,6 +114,10 @@ foreach my $rootfile (@rootfiles) {
 	    elsif($line =~ m!root\s+outfile\s+! ) {
 		print OUT "root outfile $outrootfile\n";
 	    }
+	    
+	    elsif($line =~ /$tag1\s+$tag2\s+/ && $line !~ /\s*\/\//) {
+		print OUT "$tag1 $tag2 $value\n";
+	    }
 	    else {
 		print OUT "$line";
 	    }
@@ -115,7 +125,7 @@ foreach my $rootfile (@rootfiles) {
 	close(IN);
 	close(OUT);
 	
-	my $macro = "$pwd/$scandir/auto_$rootfile.C";
+	my $macro = "$outdir/auto_$rootfile.C";
 	open(IN, "<$masterprocess");
 #	`rm -f tmpprocess.C`;
 	open(OUT, ">$macro");
@@ -144,63 +154,7 @@ foreach my $rootfile (@rootfiles) {
 	    #`cp $macro $scandir`;
 	    #`cp $optfile $scandir`;	    
 	}
-    }
-    
-    else {
-	foreach my $val (@values) {
-	    
-	    print "$masterfile: $tag1 $tag2 $val\n";
-	    my $outfile = "auto_$masterfile\_$tag1\_$tag2\_$val.opt";
-	    
-	    open(IN, "<$masterfile");
-	    open(OUT, ">$outfile");
-	    while ( <IN> ) {
-		my $line = $_;
-
-		if($line =~ /$tag1\s+$tag2\s+/ && $line !~ /\s*\/\//) {
-		    print OUT "$tag1 $tag2 $val\n";
-		}
-		elsif($line =~ /root\s+file\s+/ && $line !~ /\s*\/\//) {
-		    print OUT "root file $fullfilename\n";
-		}
-		else {
-		    print OUT "$line";
-		}
-	    }
-	    close(IN);
-	    close(OUT);
-	    
-	    open(IN, "<$masterprocess");
-	    `rm -f tmpprocess.C`;
-	    open(OUT, ">tmpprocess.C");
-	    while ( <IN> ) {
-		my $line = $_;
-		
-		if($line =~ /^PFRootEventManagerColin\s+em/ && $line !~ /\s*\/\//) {
-		    print OUT "PFRootEventManagerColin em(\"$outfile\");\n";
-		}
-		elsif($line =~ /(.*)<<val<<(.*)/ && $line !~ /\s*\/\//) {
-		    print OUT "$1<<$val<<$2\n"; 
-		}
-		else {
-		    print OUT "$line";
-		}
-	    }
-	    if(! $doNotSubmit) {
-		`nice -n 5 root -b tmpprocess.C`;
-		my $outrootname = "$tag1\_$tag2\_$val.root";
-		`mv out.root out_$outrootname`;
-	    }
-#    print "done $val\n";
-	}
 	
-	my $outdirname = "Out\_$tag1\_$tag2\_$rootfile";
-	$outdirname =~ s/\.root//;
-	`mkdir $outdirname`;
-	my $outpattern = "out_$tag1\_$tag2\_*.root";
-	`mv $outpattern $outdirname`;
-
     }
-
 }
 
