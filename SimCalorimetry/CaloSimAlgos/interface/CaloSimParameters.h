3142b431067e1d625f3eb4fb85e4ecb66975cf5b
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-12-13 01:04:57 +0000

    Get rid of sampling factor, and make a ctor from a ParameterSet

--- SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h
+++ SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h
@@ -2,7 +2,7 @@
 #define CaloSimAlgos_CaloSimParameters_h
 
 #include <iosfwd>
-
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 /**
 
    \class CaloSimParameters
@@ -16,17 +16,9 @@ public:
   CaloSimParameters(double simHitToPhotoelectrons, double photoelectronsToAnalog, 
                  double samplingFactor, double timePhase,
                  int readoutFrameSize, int binOfMaximum,
-                 bool doPhotostatistics, bool syncPhase=true)
-  : simHitToPhotoelectrons_(simHitToPhotoelectrons),
-    photoelectronsToAnalog_(photoelectronsToAnalog),
-    samplingFactor_(samplingFactor),
-    timePhase_(timePhase),
-    readoutFrameSize_(readoutFrameSize),
-    binOfMaximum_(binOfMaximum),
-    doPhotostatistics_(doPhotostatistics),
-    syncPhase_(syncPhase)
-  {
-  }
+                 bool doPhotostatistics, bool syncPhase=true);
+
+  CaloSimParameters(const edm::ParameterSet & p);
 
   ~CaloSimParameters() {};
 
@@ -38,18 +30,8 @@ public:
   /// the factor which goes from photoelectrons to whatever gets read by ADCs
  double photoelectronsToAnalog() const {return photoelectronsToAnalog_;}
 
-  /// the ratio of actual incident energy to deposited energy
-  /// in the SimHit
-  double samplingFactor() const {return samplingFactor_;}
-
   /// the adjustment you need to apply to get the signal where you want it
   double timePhase() const {return timePhase_;}
-
-  /// the factor that goes from linearized ADC counts to energy
-  /// (GeV/dGeV) / (pe/dGeV) / (signal/pe)
-  double calibrationConstant() const {
-    return samplingFactor_ / simHitToPhotoelectrons_ / photoelectronsToAnalog_;
-  }
 
   /// for now, the LinearFrames and trhe digis will be one-to-one.
   int readoutFrameSize() const {return readoutFrameSize_;}
@@ -66,7 +48,6 @@ public:
 private:
   double simHitToPhotoelectrons_;
   double photoelectronsToAnalog_;
-  double samplingFactor_;
   double timePhase_;
   int readoutFrameSize_;
   int binOfMaximum_;
--- SimCalorimetry/CaloSimAlgos/src/CaloSimParameters.cc
+++ SimCalorimetry/CaloSimAlgos/src/CaloSimParameters.cc
@@ -1,11 +1,39 @@
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h"
 #include<iostream>
   
+CaloSimParameters::CaloSimParameters(double simHitToPhotoelectrons, double photoelectronsToAnalog,
+                 double samplingFactor, double timePhase,
+                 int readoutFrameSize, int binOfMaximum,
+                 bool doPhotostatistics, bool syncPhase)
+: simHitToPhotoelectrons_(simHitToPhotoelectrons),
+  photoelectronsToAnalog_(photoelectronsToAnalog),
+  timePhase_(timePhase),
+  readoutFrameSize_(readoutFrameSize),
+  binOfMaximum_(binOfMaximum),
+  doPhotostatistics_(doPhotostatistics),
+  syncPhase_(syncPhase)
+{
+}
+
+
+
+CaloSimParameters::CaloSimParameters(const edm::ParameterSet & p)
+: simHitToPhotoelectrons_( p.getParameter<double>("simHitToPhotoelectrons") ),
+  photoelectronsToAnalog_( p.getParameter<double>("photoelectronsToAnalog") ),
+  timePhase_( p.getParameter<double>("timePhase") ),
+  readoutFrameSize_( p.getParameter<int>("readoutFrameSize") ),
+  binOfMaximum_( p.getParameter<int>("binOfMaximum") ),
+  doPhotostatistics_( p.getParameter<bool>("doPhotoStatistics") ),
+  syncPhase_( p.getParameter<double>("syncPhase") )
+{
+}
+
+
 std::ostream & operator<<(std::ostream & os, const CaloSimParameters & p) {
   os << "CALO SIM PARAMETERS" << std::endl;
   os << p.simHitToPhotoelectrons() << " pe per SimHit energy " << std::endl;
   os << p.photoelectronsToAnalog() << " Analog signal to be digitized per pe" << std::endl;
-  os << "Sampling Factor:      " << p.samplingFactor() << " Incident energy / SimHit Energy " << std::endl;
+  os << " Incident energy / SimHit Energy " << std::endl;
   return os;
 }
 
