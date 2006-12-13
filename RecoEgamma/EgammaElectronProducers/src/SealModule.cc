c86184ed54accd9f5dc958dfcba77d751da26401
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2006-12-13 18:36:43 +0000

    correct cfg files for Gsf tracking

--- RecoEgamma/EgammaElectronProducers/src/SealModule.cc
+++ RecoEgamma/EgammaElectronProducers/src/SealModule.cc
@@ -9,7 +9,7 @@
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedProducer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedAnalyzer.h"
-//#include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronProducer.h"
+//temporary: #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronProducer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchGsfElectronProducer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchGsfElectronAnalyzer.h"
@@ -28,7 +28,7 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAssociator);
 DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedProducer);
-//DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronProducer);
+//temporary: DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
