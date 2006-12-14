057173fd6f7caba444e28afd7ea8c9f99c9de95b
Claudio Viviani <sha1-5a2d4e7ace4f17d1a8c54594122228789f798ee0@cern.ch>
2006-12-14 10:15:29 +0000

    *** empty log message ***

---
+++ L1Trigger/RPCTechnicalTrigger/src/RBCPolicy.cc
@@ -0,0 +1,46 @@
+#include "L1Trigger/RBCEmulator/interface/RBCPolicy.h"
+#include "L1Trigger/RBCEmulator/src/RBCLogic.h"
+#include "L1Trigger/RBCEmulator/src/RBCPatternLogic.h"
+#include "L1Trigger/RBCEmulator/src/RBCChamberORLogic.h"
+
+
+RBCPolicy::RBCPolicy(RBCPolicy::Policy p) : pol(p), l(0)    
+{
+}
+
+
+RBCPolicy::~RBCPolicy()
+{ 
+  std::cout<<"bye policy"<<std::endl;
+  if (l)
+    delete l;
+  l=0;
+}
+
+
+
+std::string 
+RBCPolicy::message()
+{
+    if (pol == Patterns)
+      return "Patterns";
+    else if ( pol == ChamberOR )
+      return "ChamberOR";
+    else
+      return "No idea";
+}
+  
+
+
+RBCLogic* 
+RBCPolicy::instance()
+{
+  if (l)
+    delete l;
+  l=0;
+  if (pol == Patterns)
+    l = new RBCPatternLogic();
+  else if ( pol == ChamberOR )
+    l =  new RBCChamberORLogic();
+  return l;
+}
