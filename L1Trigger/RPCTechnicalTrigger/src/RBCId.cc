9fe1e8bf64bf74c5b3342e8dc02b769ce4e104f2
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2006-12-14 10:13:23 +0000

    first release

--- L1Trigger/RPCTechnicalTrigger/src/RBCId.cc
+++ L1Trigger/RPCTechnicalTrigger/src/RBCId.cc
@@ -1,1 +1,24 @@
-Empty file.
+#include "L1Trigger/RBCEmulator/interface/RBCId.h"
+
+RBCId::RBCId() : w(-9),s(0)
+{}
+
+RBCId::RBCId(int wheel, int sector) : w(wheel), s(sector)
+{}
+
+RBCId::~RBCId(){}
+
+
+int
+RBCId::wheel() const
+{
+  return w;
+}
+
+
+
+int
+RBCId::sector() const
+{
+  return s;
+}
