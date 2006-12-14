758be3bf9c5b2c7d98c45192ddf92fec2a61b2b2
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-12-14 01:41:25 +0000

    Needed a smarter Parameters class, to apply calibrations to simHitToPhotoelectrons

---
+++ SimCalorimetry/HcalSimAlgos/interface/HcalSimParameters.h
@@ -0,0 +1,38 @@
+#ifndef HcalSimAlgos_HcalSimParameters_h
+#define HcalSimAlgos_HcalSimParameters_h
+
+#include "SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+
+class HcalSimParameters : public CaloSimParameters
+{
+public:
+  HcalSimParameters(double simHitToPhotoelectrons, double photoelectronsToAnalog,
+                 double samplingFactor, double timePhase,
+                 int readoutFrameSize, int binOfMaximum,
+                 bool doPhotostatistics, bool syncPhase,
+                 int firstRing, const std::vector<double> & samplingFactors);
+  HcalSimParameters(const edm::ParameterSet & p);
+
+  virtual ~HcalSimParameters() {}
+
+  void setDbService(const HcalDbService * service) {theDbService = service;}
+
+  virtual double simHitToPhotoelectrons(const DetId & detId) const;
+
+  double fCtoGeV(const DetId & detId) const;
+
+  /// the ratio of actual incident energy to deposited energy
+  /// in the SimHit
+  virtual double samplingFactor(const DetId & detId) const;
+
+
+private:
+  const HcalDbService * theDbService;
+  int theFirstRing;
+  std::vector<double> theSamplingFactors;
+};
+
+#endif
+  
---
+++ SimCalorimetry/HcalSimAlgos/src/HcalSimParameters.cc
@@ -0,0 +1,77 @@
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameters.h"
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "CondFormats/HcalObjects/interface/HcalGain.h"
+#include "CondFormats/HcalObjects/interface/HcalGainWidth.h"
+#include "CLHEP/Random/RandGaussQ.h"
+
+
+HcalSimParameters::HcalSimParameters(double simHitToPhotoelectrons, double photoelectronsToAnalog,
+                 double samplingFactor, double timePhase,
+                 int readoutFrameSize, int binOfMaximum,
+                 bool doPhotostatistics, bool syncPhase,
+                 int firstRing, const std::vector<double> & samplingFactors)
+: CaloSimParameters(simHitToPhotoelectrons, photoelectronsToAnalog, samplingFactor, timePhase,
+                    readoutFrameSize, binOfMaximum, doPhotostatistics, syncPhase),
+  theDbService(0),
+  theFirstRing(firstRing),
+  theSamplingFactors(samplingFactors)
+{
+}
+
+
+
+
+HcalSimParameters::HcalSimParameters(const edm::ParameterSet & p)
+:  CaloSimParameters(p),
+   theDbService(0),
+   theFirstRing( p.getParameter<int>("firstRing") ),
+   theSamplingFactors( p.getParameter<std::vector<double> >("samplingFactors") )
+{
+}
+
+
+double HcalSimParameters::simHitToPhotoelectrons(const DetId & detId) const 
+{
+  // the gain is in units of GeV/fC.  We want a constant with pe/dGeV
+  // pe/dGeV = (GeV/dGeV) / (GeV/fC) / (fC/pe)
+  
+  return samplingFactor(detId) / fCtoGeV(detId) / photoelectronsToAnalog();
+}
+
+
+double HcalSimParameters::fCtoGeV(const DetId & detId) const
+{
+  assert(theDbService != 0);
+  HcalDetId hcalDetId(detId);
+  const HcalGain* gains = theDbService->getGain  (hcalDetId);
+  const HcalGainWidth* gwidths = theDbService->getGainWidth  (hcalDetId);
+  if (!gains || !gwidths )
+  {
+    edm::LogError("HcalAmplifier") << "Could not fetch HCAL conditions for channel " << hcalDetId;
+  }
+  // only one gain will be recorded per channel, so just use capID 0 for now
+  
+  double result = gains->getValue(0);
+//  if(doNoise_)
+///  {
+//    result += RandGaussQ::shoot(0.,  gwidths->getValue(0));
+//  }
+  return result;
+}
+
+/*
+double HcalSimParameters::photoelectronsToAnalog(const DetId & detId) const
+{ 
+  // multiply amplification factor by electron charge to get fC
+  return amplification_ * (1.6e-4);
+}
+*/
+
+double HcalSimParameters::samplingFactor(const DetId & detId) const
+{
+  HcalDetId hcalDetId(detId);
+  return theSamplingFactors[hcalDetId.ietaAbs()-theFirstRing];
+}
+
