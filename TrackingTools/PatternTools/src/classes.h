09b5787f36db055010b275103214c0338c113829
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2006-12-14 16:43:53 +0000

    Definitions needed to put trajectory <-> track association map in the event

--- TrackingTools/PatternTools/src/classes.h
+++ TrackingTools/PatternTools/src/classes.h
@@ -1,9 +1,21 @@
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "TrackingTools/PatternTools/interface/Trajectory.h"
+#include "TrackingTools/PatternTools/interface/TrajTrackAssociation.h"
 #include <vector>
 
 namespace {
   namespace {
     std::vector<Trajectory> kk;
+    edm::Wrapper< std::vector<Trajectory> > trajCollWrapper;
+    
+    TrajTrackAssociationCollection ttam;
+    edm::Wrapper<TrajTrackAssociationCollection> wttam;
+    TrajTrackAssociation vttam;
+    TrajTrackAssociationRef rttam;
+    TrajTrackAssociationRefProd rpttam;
+    TrajTrackAssociationRefVector rvttam;
+  
   }
 }
