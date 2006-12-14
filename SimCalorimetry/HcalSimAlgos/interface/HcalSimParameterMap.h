6bd4c582a68fc521b347353f53470b3691369771
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-12-14 01:43:13 +0000

    Use the new HcalSimParameters for HB, HE, & HO

--- SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h
+++ SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h
@@ -2,7 +2,7 @@
 #define HcalSimAlgos_HcalSimParameterMap_h
 
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloVSimParameterMap.h"
-#include "SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h"
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameters.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
 
@@ -19,25 +19,20 @@ public:
   virtual const CaloSimParameters & simParameters(const DetId & id) const;
 
   /// accessors
-  CaloSimParameters hbParameters() const {return theHBParameters;}
-  CaloSimParameters heParameters() const {return theHEParameters;}
-  CaloSimParameters hoParameters() const  {return theHOParameters;}
+  HcalSimParameters hbParameters() const {return theHBParameters;}
+  HcalSimParameters heParameters() const {return theHEParameters;}
+  HcalSimParameters hoParameters() const  {return theHOParameters;}
   CaloSimParameters hfParameters1() const  {return theHFParameters1;}
   CaloSimParameters hfParameters2() const  {return theHFParameters2;}
 
-  /// might move out of here eventually
-  double samplingFactor(const HcalDetId & detId) const;
+  void setDbService(const HcalDbService * service);
 
 private:
-  CaloSimParameters theHBParameters;
-  CaloSimParameters theHEParameters;
-  CaloSimParameters theHOParameters;
+  HcalSimParameters theHBParameters;
+  HcalSimParameters theHEParameters;
+  HcalSimParameters theHOParameters;
   CaloSimParameters theHFParameters1;
   CaloSimParameters theHFParameters2;
-
-  std::vector<double> theSamplingFactors;
-  double theSamplingFactorHF1;
-  double theSamplingFactorHF2;
 };
 
 
--- SimCalorimetry/HcalSimAlgos/src/HcalSimParameterMap.cc
+++ SimCalorimetry/HcalSimAlgos/src/HcalSimParameterMap.cc
@@ -4,35 +4,26 @@
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
   
 
-// the second value, amplifierGain, maybe should be 1/(4 pe/fC) in barrel,
-// and 1/(0.3 pe/fC) in the endcap.  I'll change them to make them
-// consistent with the current default calibrations.
 HcalSimParameterMap::HcalSimParameterMap() :
   theHBParameters(2000., 0.3305,
                    117, 5, 
-                   10, 5, true),
+                   10, 5, true, true,
+                   1, std::vector<double>(16, 117.)),
   theHEParameters(2000., 0.3305,
                    178, 5,
-                   10, 5, true),
-  theHOParameters( 4000., 0.3065, 217., 5, 10, 5, true),
-  theHFParameters1(1., 18.93,
+                   10, 5, true, true,
+                   16, std::vector<double>(16, 178.)),
+  theHOParameters( 4000., 0.3065, 
+                   217., 5, 
+                   10, 5, true, true,
+                   1, std::vector<double>(16, 217.)),
+  theHFParameters1(1., 5.917,
                  2.84 , -4,
                 6, 4, false),
-  theHFParameters2(1., 13.93,
+  theHFParameters2(1., 4.354,
                  2.09 , -4,
-                6, 4, false),
-  theSamplingFactors(29),
-  theSamplingFactorHF1(2.84),
-  theSamplingFactorHF2(2.09)
+                6, 4, false)
 {
-  for(unsigned i = 0; i <16; ++i)
-  {
-    theSamplingFactors[i] = 117.;
-  }
-  for(unsigned i = 16; i <29; ++i)
-  {
-    theSamplingFactors[i] = 178.;
-  }
 }
 /*
   CaloSimParameters(double photomultiplierGain, double amplifierGain,
@@ -46,10 +37,7 @@ HcalSimParameterMap::HcalSimParameterMap(const edm::ParameterSet & p)
   theHEParameters(  p.getParameter<edm::ParameterSet>("he") ),
   theHOParameters(  p.getParameter<edm::ParameterSet>("ho") ),
   theHFParameters1( p.getParameter<edm::ParameterSet>("hf1") ),
-  theHFParameters2( p.getParameter<edm::ParameterSet>("hf2") ),
-  theSamplingFactors( p.getParameter<std::vector<double> >("samplingFactors") ),
-  theSamplingFactorHF1( p.getParameter<double>("samplingFactorHF1") ),
-  theSamplingFactorHF2( p.getParameter<double>("samplingFactorHF2") )
+  theHFParameters2( p.getParameter<edm::ParameterSet>("hf2") )
 {
 }
 
@@ -73,17 +61,9 @@ const CaloSimParameters & HcalSimParameterMap::simParameters(const DetId & detId
 }
 
 
-double HcalSimParameterMap::samplingFactor(const HcalDetId & detId) const
+void HcalSimParameterMap::setDbService(const HcalDbService * dbService)
 {
-  double result;
-  if(detId.subdet() == HcalForward)
-  {
-    result = (detId.depth() == 1) ? theSamplingFactorHF1 : theSamplingFactorHF2;
-  }
-  else
-  {
-    result = theSamplingFactors[detId.ietaAbs()-1];
-  }
-  return result;
+  theHBParameters.setDbService(dbService);
+  theHEParameters.setDbService(dbService);
+  theHOParameters.setDbService(dbService);
 }
-
