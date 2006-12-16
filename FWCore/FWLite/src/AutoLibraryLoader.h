c6ada2113b7a01ed39fb22a9dd3c52093cf94e36
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-12-16 13:33:29 +0000

    FWCore/RootAutoLibraryLoader package split off from FWCore/FWLite

--- FWCore/FWLite/src/AutoLibraryLoader.h
+++ FWCore/FWLite/src/AutoLibraryLoader.h
@@ -5,24 +5,19 @@
  * ROOT helper class which can automatically load the 
  * proper shared library when ROOT needs a new class dictionary
  *
- * \author Chirs Jones, Cornell
+ * \author Chris Jones, Cornell
  *
- * $Id: AutoLibraryLoader.h,v 1.3 2006/08/11 19:41:49 chrjones Exp $
+ * $Id: AutoLibraryLoader.h,v 1.4 2006/09/29 00:54:16 chrjones Exp $
  *
  */
-#include "TClassGenerator.h"
-
+#include "TObject.h"
 class DummyClassToStopCompilerWarning;
 
-class AutoLibraryLoader : public TClassGenerator {
+class AutoLibraryLoader: public TObject {
   friend class DummyClassToStopCompilerWarning;
 public:
-  /// return class type
-  virtual TClass *GetClass(const char* classname, Bool_t load);
-  /// return class type
-  virtual TClass *GetClass(const type_info& typeinfo, Bool_t load);
   /// interface for TClass generators
-  ClassDef(AutoLibraryLoader,1);
+  ClassDef(AutoLibraryLoader,2);
   /// enable automatic library loading  
   static void enable();
   
@@ -30,7 +25,6 @@ public:
   static void loadAll();
 
 private:
-  const char* classNameAttemptingToLoad_; //!
   AutoLibraryLoader();
   AutoLibraryLoader(const AutoLibraryLoader&); // stop default
   const AutoLibraryLoader& operator=(const AutoLibraryLoader&); // stop default
