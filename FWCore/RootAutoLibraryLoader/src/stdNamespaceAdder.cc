a7869bc8d1298b29ceba4a2160e4ef906d5bc155
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-12-20 00:23:40 +0000

    Fix missing new-line at end of file

--- FWCore/RootAutoLibraryLoader/src/stdNamespaceAdder.cc
+++ FWCore/RootAutoLibraryLoader/src/stdNamespaceAdder.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  
 //         Created:  Tue Dec  6 09:18:05 EST 2005
-// $Id: stdNamespaceAdder.cc,v 1.1 2006/05/17 16:43:44 wmtan Exp $
+// $Id: stdNamespaceAdder.cc,v 1.1 2006/12/16 13:31:42 wmtan Exp $
 //
 
 // system include files
@@ -22,15 +22,15 @@
 // constants, enums and typedefs
 //
 namespace edm {
-namespace root {
-   std::string stdNamespaceAdder(const std::string& iClassName)
-   {
+  namespace root {
+    std::string stdNamespaceAdder(const std::string& iClassName)
+    {
       //adds the std:: prefix to vector, string, map, list or deque if it is not
       // already there
       static const boost::regex e("(^|[^[:alnum:]_:])((?:vector)|(?:string)|(?:map)|(?:list)|(?:deque))");
       const std::string format("\\1std::\\2");
       
       return regex_replace(iClassName, e, format, boost::match_default | boost::format_sed);
-   }
-}
+    }
+  }
 }
