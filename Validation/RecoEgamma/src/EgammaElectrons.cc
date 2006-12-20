71989830dfc9c436e5f24fea87fe6bb724b85d25
mabalazs <sha1-814c98c95c0331e45c899668854de808bc93186d@cern.ch>
2006-12-20 16:53:41 +0000

    Bug Fix: Wrong Partilce Id in EgammaElectrons.cc NOTE: Bug only in V00-00-04 tag
    Michael A. Balazs UVa 12-20-06

--- Validation/RecoEgamma/src/EgammaElectrons.cc
+++ Validation/RecoEgamma/src/EgammaElectrons.cc
@@ -204,7 +204,7 @@ void EgammaElectrons::analyze( const edm::Event& evt, const edm::EventSetup& es 
   for(HepMC::GenEvent::particle_const_iterator currentParticle = genEvent->particles_begin(); 
     currentParticle != genEvent->particles_end(); currentParticle++ )
   {
-    if(abs((*currentParticle)->pdg_id())==22 && (*currentParticle)->status()==1) 
+    if(abs((*currentParticle)->pdg_id())==11 && (*currentParticle)->status()==1) 
     {
       double etaTrue = (*currentParticle)->momentum().eta();
       double phiTrue = (*currentParticle)->momentum().phi();
