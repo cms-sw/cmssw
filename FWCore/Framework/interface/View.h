8bfd6b5f45fe9e00862ca147388e0ac0770e73aa
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-12-20 22:54:11 +0000

    Add INCOMPLETE View template and first tests. ITERATION IS NOT YET CORRECT

---
+++ FWCore/Framework/interface/View.h
@@ -0,0 +1,162 @@
+#ifndef Framework_View_h
+#define Framework_View_h
+// -*- C++ -*-
+//
+// Package:     Framework
+// Class  :     View
+// 
+/**\class View View.h FWCore/Framework/interface/View.h
+
+Description: Provide access to any EDProduct that is a sequence.
+
+
+*/
+//
+// Original Author:  
+//         Created:  Mon Dec 18 09:48:30 CST 2006
+// $Id$
+//
+
+#include <algorithm>
+#include <vector>
+
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+namespace edm
+{
+  template <class T>
+  class View
+  {
+    typedef std::vector<T*>  seq_t;
+  public:
+    typedef T*         pointer;
+    typedef T const*   const_pointer;
+
+    typedef T&         reference;
+    typedef T const&   const_reference;
+
+    typedef T          value_type;
+
+    typedef typename seq_t::const_iterator const_iterator; // FIXME
+
+    typedef typename seq_t::size_type size_type;
+    typedef typename seq_t::difference_type difference_type;
+
+    typedef typename seq_t::const_reverse_iterator const_reverse_iterator; // FIXME
+
+    // Compiler-generated default c'tor, copy, assignment, and d'tor
+    // each does the right thing.
+
+    size_type capacity() const { return data_.capacity(); }
+
+    // Most non-const member functions not present.
+    // No access to non-const contents provided.
+
+    const_iterator begin() const { return data_.begin(); }
+    const_iterator end() const { return data_.end(); }
+
+    const_reverse_iterator rbegin() const { return data_.rbegin(); }
+    const_reverse_iterator rend() const { return data_.rend(); }
+
+    size_type size() const { return data_.size(); }
+
+    size_type max_size() const { return data_.max_size(); }
+
+    bool empty() const { return data_.empty(); }
+
+    const_reference at(size_type pos) const { return *data_.at(pos); }
+
+    const_reference operator[](size_type pos) const { return *data_[pos]; }
+
+    const_reference front() const { return *data_.front(); }
+
+    const_reference back() const {return *data_.back(); }
+
+    void pop_back() { data_.pop_back(); }
+
+    // No erase, because erase is required to return an *iterator*,
+    // not a *const_iterator*.
+
+    // The following are for testing.
+    friend void fill_from_range<T>(T* first, T* last, View& output);   
+
+  private:
+
+    seq_t  data_;
+
+  };
+
+  
+  template <class T>
+  void
+  fill_from_range(T* first, T* last, View<T>& output)
+  {
+    output.data_.resize(std::distance(first,last));
+    for (typename View<T>::size_type i = 0; first != last; ++i, ++first)
+      output.data_[i] = first;
+  }
+
+  // Associated free functions (same as for std::vector)
+  template <class T> bool operator==(View<T> const&, View<T> const&);
+  template <class T> bool operator!=(View<T> const&, View<T> const&);
+  template <class T> bool operator< (View<T> const&, View<T> const&);
+  template <class T> bool operator<=(View<T> const&, View<T> const&);
+  template <class T> bool operator> (View<T> const&, View<T> const&);
+  template <class T> bool operator>=(View<T> const&, View<T> const&);
+
+
+  template <class T>
+  inline
+  bool
+  operator== (View<T> const& lhs, View<T> const& rhs)
+  {
+    return 
+      lhs.size() == rhs.size() &&
+      std::equal(lhs.begin(), lhs.end(), rhs.begin());
+  }
+
+  template <class T>
+  inline
+  bool 
+  operator!=(View<T> const& lhs, View<T> const& rhs)
+  {
+    return !(lhs==rhs);
+  }
+
+  template <class T>
+  inline 
+  bool 
+  operator< (View<T> const& lhs, View<T> const& rhs)
+  {
+    return 
+      std::lexicographical_compare(lhs.begin(), lhs.end(),
+				   rhs.begin(), rhs.end());
+  }
+
+  template <class T> 
+  inline
+  bool
+  operator<=(View<T> const& lhs, View<T> const& rhs)
+  {
+    return !(rhs<lhs);
+  }
+
+  template <class T> 
+  inline 
+  bool operator> (View<T> const& lhs, View<T> const& rhs)
+  {
+    return rhs<lhs;
+  }
+
+  template <class T>
+  inline
+  bool operator>=(View<T> const& lhs, View<T> const& rhs)
+  {
+    return !(lhs<rhs);
+  }
+
+}
+
+
+#endif
---
+++ FWCore/Framework/test/View_t.cpp
@@ -0,0 +1,81 @@
+#include <Utilities/Testing/interface/CppUnit_testdriver.icpp>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "FWCore/Framework/interface/View.h"
+
+class testView: public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(testView);
+  CPPUNIT_TEST(basic);
+  CPPUNIT_TEST(createFromArray);
+  CPPUNIT_TEST(directAccess);
+  CPPUNIT_TEST_SUITE_END();
+
+ public:
+  testView() {}
+  ~testView() {}
+  void setUp() {}
+  void tearDown() {}
+
+  void basic();
+  void createFromArray();
+  void directAccess();
+
+ private:
+  typedef int  value_type;
+  typedef edm::View<value_type> view;
+  typedef view::const_iterator const_iterator;
+  typedef view::const_reverse_iterator const_reverse_iterator;
+  typedef view::const_reference const_reference;
+  typedef view::size_type size_type;
+};
+
+///registration of the test so that the runner can find it
+CPPUNIT_TEST_SUITE_REGISTRATION(testView);
+
+void testView::basic()
+{
+  view v1;
+  CPPUNIT_ASSERT(v1.size() == 0);
+  CPPUNIT_ASSERT(v1.empty());
+  view v2(v1);
+  CPPUNIT_ASSERT(v1==v2);
+  view v3;
+  v3 = v1;
+  CPPUNIT_ASSERT(v3==v1);
+
+  CPPUNIT_ASSERT(! (v1 < v2));
+  CPPUNIT_ASSERT(v1 <= v2);
+  CPPUNIT_ASSERT(! (v1 > v2));
+  CPPUNIT_ASSERT(v1 >= v2);
+  CPPUNIT_ASSERT(! (v1 != v2));
+}
+
+void testView::createFromArray()
+{
+  value_type vals[] = { 1, 2, 3, 4, 5 };
+  size_t sz = sizeof(vals)/sizeof(value_type);
+
+  view v1;
+  edm::fill_from_range(vals, vals+sz, v1);
+  CPPUNIT_ASSERT(v1.size() == 5);
+  view v2;
+  edm::fill_from_range(vals, vals+sz, v2);
+  CPPUNIT_ASSERT(v1==v2);
+}
+
+void testView::directAccess()
+{
+  value_type vals[] = { 1, 2, 3, 4, 5 };
+  size_t sz = sizeof(vals)/sizeof(value_type);
+
+  view v1;
+  edm::fill_from_range(vals, vals+sz, v1);
+  for (size_type i = 0; i < v1.size(); ++i)
+    {
+      CPPUNIT_ASSERT(v1[i] == vals[i]);
+    }  
+}
+
+
+
