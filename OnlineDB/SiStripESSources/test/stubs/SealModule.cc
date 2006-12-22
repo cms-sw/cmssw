d33b900da4598082fbf06f40b8fec031f93246d5
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2006-12-22 12:23:40 +0000

    removed old tests, added new tests for peds and noise builders

--- OnlineDB/SiStripESSources/test/stubs/SealModule.cc
+++ OnlineDB/SiStripESSources/test/stubs/SealModule.cc
@@ -1,15 +1,19 @@
+// Last commit: $Id: $
+// Latest tag:  $Name:  $
+// Location:    $Source: $
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "PluginManager/ModuleDef.h"
 DEFINE_SEAL_MODULE();
 
-#include "OnlineDB/SiStripESSources/test/stubs/PopulateConfigDb.h"
-DEFINE_ANOTHER_FWK_MODULE(PopulateConfigDb);
-
 #include "OnlineDB/SiStripESSources/test/stubs/AnalyzeFedCabling.h"
 DEFINE_ANOTHER_FWK_MODULE(AnalyzeFedCabling);
 
+#include "OnlineDB/SiStripESSources/test/stubs/AnalyzePedestals.h"
+DEFINE_ANOTHER_FWK_MODULE(AnalyzePedestals);
+
+#include "OnlineDB/SiStripESSources/test/stubs/AnalyzeNoise.h"
+DEFINE_ANOTHER_FWK_MODULE(AnalyzeNoise);
+
 #include "OnlineDB/SiStripESSources/test/stubs/test_FedCablingBuilderFromDb.h"
 DEFINE_ANOTHER_FWK_MODULE(test_FedCablingBuilderFromDb);
-
-#include "OnlineDB/SiStripESSources/test/stubs/test_DatabaseService.h"
-DEFINE_ANOTHER_FWK_MODULE(test_DatabaseService);
