fb593e82868fe75bf8d20f2b7f1f82c4e7b43399
Sridhara Dasu <sha1-9a11d2b71511b461682d02cb386f4219547e099a@cern.ch>
2006-12-23 01:09:13 +0000

    Werner's latest

--- CalibCalorimetry/CaloTPG/src/CaloTPGTranscoderULUT.cc
+++ CalibCalorimetry/CaloTPG/src/CaloTPGTranscoderULUT.cc
@@ -84,8 +84,8 @@ void CaloTPGTranscoderULUT::loadhcalUncompress(const std::string& filename) {
   if( userfile ) 
     {
      for (int i=0; i<tpgmax; i++) { 
-      for(int j = 1; j <etabound; j++) {
-	  userfile >> hcaluncomp_[j][i];}
+      for(int j = 1; j <=etabound; j++) {
+	 userfile >> hcaluncomp_[j][i];}
     }
      //  cout<<"test hcal"<<endl;
     userfile.close();
