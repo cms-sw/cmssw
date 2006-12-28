7ad7cdee38fefd66232e1f05b019537dcdffc6b1
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-12-28 18:51:12 +0000

    Add support to be used for filling Views

--- DataFormats/Common/interface/EDProduct.h
+++ DataFormats/Common/interface/EDProduct.h
@@ -6,9 +6,11 @@
 EDProduct: The base class of all things that will be inserted into the
 Event.
 
-$Id: EDProduct.h,v 1.2 2006/07/21 21:55:12 wmtan Exp $
+$Id: EDProduct.h,v 1.3 2006/08/01 20:54:03 wmtan Exp $
 
 ----------------------------------------------------------------------*/
+
+#include <vector>
 
 namespace edm {
 
@@ -17,11 +19,18 @@ namespace edm {
     EDProduct();
     virtual ~EDProduct();
     bool isPresent() const {return isPresent_();}
+
+    // We have to use vector<void*> to keep the type information out
+    // of the EDProduct class.
+    void fillView(std::vector<void*>& view) const;
+
   private:
     // This will never be called.
     // For technical ROOT related reasons, we cannot
     // declare it = 0.
     virtual bool isPresent_() const {return true;}
+
+    virtual void do_fillView(std::vector<void*>& /* unused */) const;
   };
 }
 #endif
--- DataFormats/Common/src/EDProduct.cc
+++ DataFormats/Common/src/EDProduct.cc
@@ -1,15 +1,20 @@
 /*----------------------------------------------------------------------
   
-$Id: EDProduct.cc,v 1.2 2005/10/11 21:32:24 wmtan Exp $
+$Id: EDProduct.cc,v 1.1 2006/02/07 07:01:51 wmtan Exp $
 
 ----------------------------------------------------------------------*/
 
-
 #include "DataFormats/Common/interface/EDProduct.h"
-
 
 namespace edm {
   EDProduct::EDProduct() {}
 
   EDProduct::~EDProduct() {}
+
+  void EDProduct::fillView(std::vector<void*>& pointers) const
+  {
+    do_fillView(pointers);
+  }
+
+  void EDProduct::do_fillView(std::vector<void*>&) const { }
 }
