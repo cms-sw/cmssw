62223f646f3ea3d5566d19362d2caf255d2483bf
Robert Harris <sha1-bef59cd0d27f2c81b33bddad0d65ff99d43c90a4@cern.ch>
2007-01-03 19:34:06 +0000

    Removing cout lines

--- JetMETCorrections/MCJet/src/MCJetProducer.cc
+++ JetMETCorrections/MCJet/src/MCJetProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Olga Kodolova
 //         Created:  Wed Feb  1 17:04:23 CET 2006
-// $Id: MCJetProducer.cc,v 1.4 2006/06/28 16:54:14 kodolova Exp $
+// $Id: MCJetProducer.cc,v 1.5 2006/09/29 14:04:59 kodolova Exp $
 //
 //
 
@@ -64,15 +64,15 @@ MCJet::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    iEvent.getByLabel(mInput, jets);                              //Get Inputs
    auto_ptr<CaloJetCollection> result (new CaloJetCollection);  //Corrected jets
    CaloJetCollection::const_iterator jet = jets->begin ();
-      cout<<" Size of jets "<<jets->size()<<endl;
+      //cout<<" Size of jets "<<jets->size()<<endl;
    if(jets->size() > 0 )
    { 
    for (; jet != jets->end (); jet++) {
       result->push_back (mAlgorithm.applyCorrection (*jet));
-      cout<<" Size of the result "<<result->size()<<endl;
+      //cout<<" Size of the result "<<result->size()<<endl;
    }
    }
-      cout<<" Put result "<<result->size()<<endl;
+      //cout<<" Put result "<<result->size()<<endl;
    iEvent.put(result);  //Puts Corrected Jet Collection into event
    
 }
