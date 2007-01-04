e437c7b6e9c400c30f3c8352256b601a9a7dcfe1
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-04 00:19:43 +0000

    New package for CLHEP data dictionaries

---
+++ DataFormats/CLHEP/src/classes.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include "CLHEP/HepMC/Flow.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include "CLHEP/HepMC/GenParticle.h"
+#include "CLHEP/HepMC/GenVertex.h"
+#include "CLHEP/HepMC/HepMCConfig.h"
+#include "CLHEP/HepMC/Polarization.h"
+#include "CLHEP/HepMC/ReadHepMC.h"
+#include "CLHEP/HepMC/WeightContainer.h"
+#include "CLHEP/HepPDT/ParticleID.hh"
+#include "CLHEP/Matrix/Matrix.h"
+#include "CLHEP/Matrix/SymMatrix.h"
+#include "CLHEP/Matrix/Vector.h"
+#include "CLHEP/Vector/EulerAngles.h"
+#include "CLHEP/Vector/LorentzVector.h"
+#include "CLHEP/Vector/Rotation.h"
+#include "CLHEP/Vector/ThreeVector.h"
+
+namespace {
+  namespace {
+    std::vector<CLHEP::HepLorentzVector> dummy0;
+  }
+}
