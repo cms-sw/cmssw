c13b1cff99075865cbf81d19ae3b476205df02fe
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-05 19:35:55 +0000

    Add more dictionaries

--- DataFormats/Common/src/classes.h
+++ DataFormats/Common/src/classes.h
@@ -46,13 +46,17 @@ namespace { namespace {
   edm::Wrapper<std::vector<long double> > dummy11;
   edm::Wrapper<std::vector<float> > dummy12;
   edm::Wrapper<std::vector<bool> > dummy13;
+  edm::Wrapper<std::vector<unsigned long long> > dummy14;
+  edm::Wrapper<std::vector<long long> > dummy15;
 
   edm::Wrapper<std::pair<unsigned long, unsigned long> > dymmywp1;
   edm::Wrapper<std::pair<unsigned int, unsigned int> > dymmywp2;
   edm::Wrapper<std::pair<unsigned short, unsigned short> > dymmywp3;
   edm::Wrapper<std::pair<int, int> > dymmywp4;
   edm::Wrapper<std::pair<unsigned int, bool> > dymmywp5;
-  edm::Wrapper<std::pair<double,double> > dymmywp6;
+  edm::Wrapper<std::pair<unsigned int, float> > dymmywp6;
+  edm::Wrapper<std::pair<double, double> > dymmywp7;
+  edm::Wrapper<std::pair<unsigned long long, std::basic_string<char> > > dymmywp8;
   edm::Wrapper<std::map<unsigned long, unsigned long> > dymmywm1;
   edm::Wrapper<std::map<unsigned int, unsigned int> > dymmywm2;
   edm::Wrapper<std::map<unsigned short, unsigned short> > dymmywm3;
@@ -62,7 +66,8 @@ namespace { namespace {
   edm::Wrapper<std::map<unsigned int, std::vector<unsigned int> > > dymmywmv2;
   edm::Wrapper<std::map<unsigned short, std::vector<unsigned short> > > dymmypwmv3;
   edm::Wrapper<std::map<unsigned int, float> > dummyypwmv4;
-  edm::Wrapper<std::multimap<double, double> > dummyypwmv5;
+  edm::Wrapper<std::map<unsigned long long, std::basic_string<char> > > dummyypwmv5;
+  edm::Wrapper<std::multimap<double, double> > dummyypwmv6;
 
   edm::Wrapper<unsigned long> dummyw1;
   edm::Wrapper<unsigned int> dummyw2;
@@ -77,6 +82,8 @@ namespace { namespace {
   edm::Wrapper<long double> dummyw11;
   edm::Wrapper<float> dummyw12;
   edm::Wrapper<bool> dummyw13;
+  edm::Wrapper<unsigned long long> dummyw14;
+  edm::Wrapper<long long> dummyw15;
 
   edm::Wrapper<edm::HLTPathStatus> dummyx16;
   edm::Wrapper<std::vector<edm::HLTPathStatus> > dummyx17;
