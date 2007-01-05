3c155e30e684e7671ebc506bb96d64bc5f322d82
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-05 00:26:51 +0000

    To avoid duplication, define RefProd<> here, and remove unnecessary duplicate of vector<unsigned char>

--- DataFormats/FEDRawData/src/classes.h
+++ DataFormats/FEDRawData/src/classes.h
@@ -2,14 +2,14 @@
 #include <DataFormats/FEDRawData/interface/FEDRawData.h>
 #include <DataFormats/FEDRawData/interface/FEDRawDataCollection.h>
 #include <DataFormats/Common/interface/Wrapper.h>
+#include <DataFormats/Common/interface/RefProd.h>
 
  namespace{ 
    namespace {
-
-     std::vector<unsigned char> v;  
      FEDRawData              a1; 
      std::vector<FEDRawData> a2;
      FEDRawDataCollection    a3;
      edm::Wrapper<FEDRawDataCollection> d;
+     edm::RefProd<FEDRawDataCollection> r;
    }
  }
