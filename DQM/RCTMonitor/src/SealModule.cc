d8f2c167d89d5b74b6b3c0f561cfb5a917aa3d58
Sridhara Dasu <sha1-9a11d2b71511b461682d02cb386f4219547e099a@cern.ch>
2007-01-09 01:11:38 +0000

    Under development

--- DQM/RCTMonitor/src/SealModule.cc
+++ DQM/RCTMonitor/src/SealModule.cc
@@ -1,6 +1,8 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DQM/RCTMonitor/src/checkTPGs.h"
+#include "DQM/RCTMonitor/src/makeEfficiencyPlots.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(checkTPGs);
+DEFINE_ANOTHER_FWK_MODULE(makeEfficiencyPlots);
--- DQM/RCTMonitor/src/checkTPGs.cc
+++ DQM/RCTMonitor/src/checkTPGs.cc
@@ -6,6 +6,9 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+using namespace reco;
+
 #include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
 #include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
 #include "CalibFormats/CaloTPG/interface/CaloTPGTranscoder.h"
@@ -15,6 +18,12 @@
 using std::cerr;
 using std::cout;
 using std::endl;
+
+// bin 0 is dummy.  absIeta runs 1-32
+const float etaLUT[] = {0.0000, 0.0435, 0.1305, 0.2175, 0.3045, 0.3915, 0.4785, 0.5655, 0.6525, 0.7395, 
+			0.8265, 0.9135, 1.0005, 1.0875, 1.1745, 1.2615, 1.3485, 1.4355, 1.5225, 1.6095, 
+			1.6965, 1.7850, 1.8800, 1.9865, 2.1075, 2.2470, 2.4110, 2.5750, 2.8250, 3.3250, 
+			3.8250, 4.3250, 4.825};
 
 checkTPGs::checkTPGs(const edm::ParameterSet& iConfig)
 {
@@ -78,4 +87,64 @@ void checkTPGs::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   cout << "ecalSum = " << ecalSum << "\thbSum = " << hbSum << "\theSum = " << heSum << "\thfSum = " << hfSum << endl;
   cout << "ecalMax = " << ecalMax << "\thbMax = " << hbMax << "\theMax = " << heMax << "\thfMax = " << hfMax << endl;
   cout << endl;
+  edm::Handle<CandidateCollection> genParticlesHandle;
+  iEvent.getByLabel( "genParticleCandidates", genParticlesHandle);
+  CandidateCollection genParticles = *genParticlesHandle;
+  for(size_t i = 0; i < genParticles.size(); ++ i ) {
+    const Candidate & p = genParticles[ i ];
+    int id = pdgId( p );
+    int st = status( p );
+    double pt = p.pt(), eta = p.eta(), phi = p.phi(), mass = p.mass();
+    double vx = p.vx(), vy = p.vy(), vz = p.vz();
+    int charge = p.charge();
+    int n = p.numberOfDaughters();
+    cout << "Found particle: " << id << " with status " << st << " pt=" << pt << " eta=" << eta << " phi=" << phi
+	 << " mass=" << mass << " charge=" << charge << endl;
+    float towerEtSum = 0;
+    for (int i = 0; i < nEcalDigi; i++){
+      unsigned short energy = ecalCollection[i].compressedEt();
+      float towerEt = float(energy)/2.;  // Temporarily ET is hardcoded to be in 0.5 GeV steps in linear scale
+      short ieta = (short) ecalCollection[i].id().ieta(); 
+      unsigned short absIeta = (unsigned short) abs(ieta);    // absIeta runs 1-32
+      float towerEta = (ieta / absIeta) * etaLUT[absIeta];    // Lookup bin centers
+      unsigned short cal_iphi = (unsigned short) ecalCollection[i].id().iphi();
+      float towerPhi = float(cal_iphi) * 3.1415927 / 36.;
+      if(towerPhi > 3.1415927) towerPhi -= 3.1415927;
+      if(towerEt > 2.)
+	{
+	  cout << "ECAL towerEt=" << towerEt << " towerEta=" << towerEta << " towerPhi=" << towerPhi << endl;
+	}
+      float deltaRSq = (eta - towerEta) * (eta - towerEta) + (phi - towerPhi) * (phi - towerPhi);
+      if(deltaRSq < 0.1 * 0.1)
+	{
+	  towerEtSum += towerEt;
+	}
+    }
+    float hcalTowerEtSum = 0.;
+    for (int i = 0; i < nHcalDigi; i++){
+      unsigned short energy = hcalCollection[i].SOI_compressedEt();     // access only sample of interest
+      short ieta = (short) hcalCollection[i].id().ieta(); 
+      unsigned short absIeta = (unsigned short) abs(ieta);
+      float towerEt = transcoder->hcaletValue(absIeta, energy);
+      float towerEta = (ieta / absIeta) * etaLUT[absIeta];    // Lookup bin centers
+      unsigned short cal_iphi = (unsigned short) hcalCollection[i].id().iphi();
+      float towerPhi = float(cal_iphi) * 3.1415927 / 36.;
+      if(towerPhi > 3.1415927) towerPhi -= 3.1415927;
+      if(towerEt > 2.)
+	{
+	  cout << "HCAL towerEt=" << towerEt << " towerEta=" << towerEta << " towerPhi=" << towerPhi << endl;
+	}
+      float deltaRSq = (eta - towerEta) * (eta - towerEta) + (phi - towerPhi) * (phi - towerPhi);
+      if(deltaRSq < 0.1 * 0.1)
+	{
+	  towerEtSum += towerEt;
+	  hcalTowerEtSum += towerEt;
+	}
+    }
+    cout << "Trigger tower primitives neighborhood sum ET =" << towerEtSum;
+    if(towerEtSum > 0)
+      cout << " HCAL fraction =" << (100. * hcalTowerEtSum / towerEtSum) << "%" << endl;
+    else
+      cout << endl;
+  }
 }
