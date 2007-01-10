68b7c8304cb62450e60dcfee7c6e2ddad822d633
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-01-10 10:42:59 +0000

    proxy to a JetTag class, to be used as a base for extended collections

---
+++ DataFormats/BTauReco/interface/JetTagProxy.h
@@ -0,0 +1,34 @@
+#ifndef DataFormats_BTauReco_JetTagProxy_h
+#define DataFormats_BTauReco_JetTagProxy_h
+
+#include "DataFormats/BTauReco/interface/JetTag.h"
+
+namespace reco {
+ 
+class JetTagProxy {
+public:
+  JetTagProxy() : m_jetTag() { }
+
+  virtual ~JetTagProxy(void) {}
+  
+  virtual JetTagProxy* clone(void) const { return new JetTagProxy(*this); }
+
+  // set the JetTag reference
+  void setJetTag(const JetTagRef & ref) { m_jetTag = ref; }
+
+  // get the JetTag reference
+  const JetTagRef & getJetTag(void) const { return m_jetTag; }
+
+  // forward to JetTag methods
+  double discriminator(void)                           const { return m_jetTag->discriminator(); }
+  const Jet & jet(void)                                const { return m_jetTag->jet(); }
+  const edm::RefVector<TrackCollection> & tracks(void) const { return m_jetTag->tracks(); }
+  const JetTracksAssociationRef & jtaRef(void)         const { return m_jetTag->jtaRef(); }
+  
+potected:
+  JetTagRef m_jetTag;
+};
+
+}
+
+#endif // DataFormats_BTauReco_JetTagProxy_h
