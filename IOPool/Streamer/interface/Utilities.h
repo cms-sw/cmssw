4b67625e9944f8c44f25a5470084cb97b17255a7
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-11 23:25:32 +0000

    Remove functions duplicated in StreamerInputSource

--- IOPool/Streamer/interface/Utilities.h
+++ IOPool/Streamer/interface/Utilities.h
@@ -7,33 +7,27 @@
 //  Streamer package.
 //
 
-#include "DataFormats/Streamer/interface/StreamedProducts.h"
-#include "IOPool/Streamer/interface/Messages.h"
-#include "IOPool/Streamer/interface/EventBuffer.h"
-#include "FWCore/Framework/interface/EventPrincipal.h"
-#include "DataFormats/Common/interface/ProductRegistry.h"
-#include "FWCore/Utilities/interface/DebugMacros.h"
-
 #include "TBuffer.h"
 #include "TClass.h"
 
-#include <memory>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <utility>
+#include "FWCore/Utilities/interface/DebugMacros.h"
+#include "DataFormats/Streamer/interface/StreamedProducts.h"
+#include "DataFormats/Common/interface/ProductRegistry.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EventPrincipal.h"
+#include "IOPool/Streamer/interface/EventBuffer.h"
+
 #include <iostream>
 
 namespace edm
 {
-  edm::ProductRegistry getRegFromFile(const std::string& filename);
+  class InitMsg;
+  void mergeIntoRegistry(SendDescs const& descs, ProductRegistry& reg);
+  edm::ProductRegistry getRegFromFile(std::string const& filename);
   std::auto_ptr<SendJobHeader> readHeaderFromStream(std::ifstream& ist);
-  bool registryIsSubset(const SendJobHeader&, const ProductRegistry& reg);
-  bool registryIsSubset(const SendJobHeader& sd, const SendJobHeader& ref);
-  //bool registryIsSame(const SendJobHeader& sd, const SendJobHeader& ref);
-  void mergeWithRegistry(const SendDescs& descs, ProductRegistry& reg);
-  void declareStreamers(const SendDescs& descs);
-  void buildClassCache(const SendDescs& descs);
+  bool registryIsSubset(SendJobHeader const&, ProductRegistry const& reg);
+  bool registryIsSubset(SendJobHeader const& sd, SendJobHeader const& ref);
+  //bool registryIsSame(SendJobHeader const& sd, SendJobHeader const& ref);
 
   class JobHeaderDecoder
   {
@@ -41,30 +35,12 @@ namespace edm
     JobHeaderDecoder();
     ~JobHeaderDecoder();
 
-    std::auto_ptr<SendJobHeader> decodeJobHeader(const InitMsg& msg);
+    std::auto_ptr<SendJobHeader> decodeJobHeader(InitMsg const& msg);
 
   private:
     TClass* desc_;
     TBuffer buf_;
   };
-
-#if 0
-  class EventDecoder
-  {
-  public:
-    EventDecoder();
-    ~EventDecoder();
-
-    std::auto_ptr<EventPrincipal> decodeEvent(const EventMsg&, 
-					      const ProductRegistry&);
-
-  private:
-    //std::auto_ptr<SendEvent> decodeMsg(const EventMsg& msg);
-
-    TClass* desc_;
-    TBuffer buf_;
-  };
-#endif
 
   // ------------------------------------------------------------
   // Below are a few utilities for putting event pointers and job
@@ -73,8 +49,8 @@ namespace edm
   class JobHeaderInserter
   {
   public:
-    explicit JobHeaderInserter(EventBuffer& b):buf_(&b) { }
-    void insert(const InitMsg& msg)
+    explicit JobHeaderInserter(EventBuffer& b) : buf_(&b) {}
+    void insert(InitMsg const& msg)
     {
       std::auto_ptr<SendJobHeader> p = decoder_.decodeJobHeader(msg);
       EventBuffer::ProducerBuffer b(*buf_);
@@ -103,39 +79,6 @@ namespace edm
 
   // -----------------
 
-#if 0
-  class EventInserter
-  {
-  public:
-    explicit EventInserter(EventBuffer& b):buf_(&b) { }
-    void insert(const EventMsg& msg,const ProductRegistry& prods)
-    {
-      std::auto_ptr<EventPrincipal> p = decoder_.decodeEvent(msg,prods);
-	  send(p);
-    }
-
-	std::auto_ptr<EventPrincipal> decode(const EventMsg& msg,
-	                                     const ProductRegistry& prods)
-	{
-      return decoder_.decodeEvent(msg,prods);
-	}
-
-	void send(std::auto_ptr<EventPrincipal> p)
-	{
-      EventBuffer::ProducerBuffer b(*buf_);
-      void** v = (void**)b.buffer();
-	  FDEBUG(2) << "Insert: event ptr = " << (void*)p.get() << std::endl;
-      *v = p.release();
-	  FDEBUG(2) << "Insert: " << b.buffer() << " " << b.size() << std::endl;
-      b.commit(sizeof(void*));
-	}
-
-  private:
-    EventBuffer* buf_;
-    EventDecoder decoder_;
-  };
-#endif
-
   class EventExtractor
   {
   public:
@@ -151,25 +94,6 @@ namespace edm
   private:
     EventBuffer* buf_;
   };
-
-#if 0
-  class EventReader
-  {
-  public:
-    typedef std::vector<char> Buf;
-
-    EventReader(std::ifstream& ist):
-      ist_(&ist),b_(1000*1000*7) { }
-    std::auto_ptr<EventPrincipal> read(const ProductRegistry& prods);
-    int readMessage(Buf& here);
-
-  private:
-
-    std::ifstream* ist_;
-    Buf b_;
-    EventDecoder decoder_;
-  };
-#endif
 
 }
 
--- IOPool/Streamer/src/Utilities.cc
+++ IOPool/Streamer/src/Utilities.cc
@@ -1,14 +1,15 @@
 
 #include "IOPool/Streamer/interface/Utilities.h"
-#include "IOPool/Streamer/interface/ClassFiller.h"
 #include "IOPool/Streamer/interface/InitMessage.h"
-#include "IOPool/Streamer/interface/MsgHeader.h"
+#include "IOPool/Streamer/interface/ClassFiller.h"
+#include "IOPool/Streamer/interface/Messages.h"
 #include "IOPool/Streamer/interface/StreamDeserializer.h"
+#include "IOPool/Streamer/interface/StreamerInputSource.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/Utilities/interface/DebugMacros.h"
-#include "DataFormats/Common/interface/Wrapper.h"
 
-#include <typeinfo>
+//#include <typeinfo>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -60,7 +61,7 @@ namespace edm
   JobHeaderDecoder::~JobHeaderDecoder() { }
 
   std::auto_ptr<SendJobHeader>
-  JobHeaderDecoder::decodeJobHeader(const InitMsg& msg)
+  JobHeaderDecoder::decodeJobHeader(InitMsg const& msg)
   {
     FDEBUG(6) << "StreamInput: decodeRegistry" << endl;
 
@@ -84,8 +85,8 @@ namespace edm
     return sd;
   }
 
-  bool registryIsSubset(const SendJobHeader& sd,
-			const ProductRegistry& reg)
+  bool registryIsSubset(SendJobHeader const& sd,
+			ProductRegistry const& reg)
   {
     bool rc = true;
     SendDescs::const_iterator i(sd.descs_.begin()),e(sd.descs_.end());
@@ -119,8 +120,8 @@ namespace edm
     return rc;
   }
 
-  bool registryIsSubset(const SendJobHeader& sd,
-			const SendJobHeader& ref)
+  bool registryIsSubset(SendJobHeader const& sd,
+			SendJobHeader const& ref)
   {
     bool rc = true;
     SendDescs::const_iterator i(sd.descs_.begin()),e(sd.descs_.end());
@@ -163,146 +164,24 @@ namespace edm
     return rc;
   }
 
-  void mergeWithRegistry(const SendDescs& descs,
-			 ProductRegistry& reg)
-  {
+  // ---------------------------------------
+
+  void mergeIntoRegistry(SendDescs const& descs,
+			 ProductRegistry& reg) {
     SendDescs::const_iterator i(descs.begin()), e(descs.end());
 
     // the next line seems to be not good.  what if the productdesc is
     // already there? it looks like I replace it.  maybe that it correct
 
-    FDEBUG(6) << "mergeWithRegistry: Product List: " << endl;
+    FDEBUG(6) << "mergeIntoRegistry: Product List: " << std::endl;
     for(; i != e; ++i) {
 	reg.copyProduct(*i);
-	FDEBUG(6) << "StreamInput prod = " << i->className() << endl;
+	FDEBUG(6) << "StreamInput prod = " << i->className() << std::endl;
     }
 
     // not needed any more
     // fillStreamers(*pr_);
   }
-
-  void declareStreamers(const SendDescs& descs)
-  {
-    SendDescs::const_iterator i(descs.begin()), e(descs.end());
-
-    for(; i != e; ++i) {
-	//pi->init();
-	string real_name = wrappedClassName(i->className());
-	FDEBUG(6) << "declare: " << real_name << endl;
-	edm::loadCap(real_name);
-    }
-  }
-
-  void buildClassCache(const SendDescs& descs)
-  {
-    SendDescs::const_iterator i(descs.begin()), e(descs.end());
-
-    for(; i != e; ++i) {
-	//pi->init();
-	string real_name = wrappedClassName(i->className());
-	FDEBUG(6) << "BuildReadData: " << real_name << endl;
-	edm::doBuildRealData(real_name);
-    }
-  }
-
-  // ---------------------------------------
-
-#if 0
-  EventDecoder::EventDecoder():
-    desc_(getTClassFor<SendEvent>()),
-    buf_(TBuffer::kRead)
-  {
-  }
-
-  EventDecoder::~EventDecoder() { }
-
-  std::auto_ptr<EventPrincipal>
-  EventDecoder::decodeEvent(const EventMsg& msg, const ProductRegistry& pr)
-  {
-    FDEBUG(5) << "Decide event: "
-	      << msg.getEventNumber() << " "
-	      << msg.getRunNumber() << " "
-	      << msg.getTotalSegs() << " "
-	      << msg.getWhichSeg() << " "
-	      << msg.msgSize() << " "
-	      << msg.getDataSize() << " "
-	      << msg.data()
-	      << endl;
-
-    // This "SetBuffer" stuff does not appear to work or I don't understand
-    // what needs to be done to actually make it go. (JBK)
-    //buf_.SetBuffer((char*)msg.data(),msg.getDataSize(),kFALSE);
-    TBuffer xbuf(TBuffer::kRead,msg.getDataSize(),(char*)msg.data(),kFALSE);
-    RootDebug tracer(10,10);
-    auto_ptr<SendEvent> sd((SendEvent*)xbuf.ReadObjectAny(desc_));
-
-    if(sd.get()==0)
-      {
-	throw cms::Exception("EventInput","Read")
-	  << "got a null event from input stream\n";
-      }
-
-    FDEBUG(5) << "Got event: " << sd->id_ << " " << sd->prods_.size() << endl;
-
-    auto_ptr<EventPrincipal> ep(new EventPrincipal(sd->id_,
-						   sd->time_,
-						   pr));
-
-    // no process name list handling
-
-    SendProds::iterator spi(sd->prods_.begin()),spe(sd->prods_.end());
-    for(;spi!=spe;++spi)
-      {
-	FDEBUG(10) << "check prodpair" << endl;
-	if(spi->prov()==0)
-	  throw cms::Exception("NoData","EmptyProvenance");
-	if(spi->desc()==0)
-	  throw cms::Exception("NoData","EmptyDesc");
-
-	FDEBUG(5) << "Prov:"
-	     << " " << spi->desc()->className()
-             << " " << spi->desc()->productInstanceName()
-	  // << " " << spi->prod()->id()
-	     << " " << spi->desc()->productID()
-	     << " " << spi->prov()->productID_
-	     << endl;
-
-	if(spi->prod()==0)
-	  {
-	    FDEBUG(10) << "Product is null" << endl;
-	    continue;
-	    throw cms::Exception("NoData","EmptyProduct");
-	  }
-
-	auto_ptr<EDProduct>
-	  aprod(const_cast<EDProduct*>(spi->prod()));
-	auto_ptr<BranchEntryDescription>
-	  aedesc(const_cast<BranchEntryDescription*>(spi->prov()));
-	auto_ptr<BranchDescription>
-	  adesc(const_cast<BranchDescription*>(spi->desc()));
-
-	auto_ptr<Provenance> aprov(new Provenance);
-	aprov->event   = *(aedesc.get());
-	aprov->product = *(adesc.get());
-        if(aprov->isPresent()) {
-          FDEBUG(10) << "addgroup next " << aprov->productID() << endl;
-          FDEBUG(10) << "addgroup next " << aprov->event.productID_ << endl;
-          ep->addGroup(auto_ptr<Group>(new Group(aprod,aprov)));
-          FDEBUG(10) << "addgroup done" << endl;
-        } else {
-          FDEBUG(10) << "addgroup empty next " << aprov->productID() << endl;
-          FDEBUG(10) << "addgroup empty next " << aprov->event.productID_ << endl;
-          ep->addGroup(auto_ptr<Group>(new Group(aprov, false)));
-          FDEBUG(10) << "addgroup empty done" << endl;
-        }
-	spi->clear();
-      }
-
-    FDEBUG(10) << "Size = " << ep->numEDProducts() << endl;
-
-    return ep;
-  }
-#endif
 
   std::auto_ptr<SendJobHeader> readHeaderFromStream(ifstream& ist)
   {
@@ -346,7 +225,7 @@ namespace edm
     return p;
   }
 
-  edm::ProductRegistry getRegFromFile(const std::string& filename)
+  edm::ProductRegistry getRegFromFile(std::string const& filename)
   {
     edm::ProductRegistry pr;
     ifstream ist(filename.c_str(),ios_base::binary | ios_base::in);
@@ -358,33 +237,8 @@ namespace edm
       }
 
     std::auto_ptr<SendJobHeader> p = readHeaderFromStream(ist);
-    mergeWithRegistry(p->descs_,pr);
+    edm::mergeIntoRegistry(p->descs_,pr);
     return pr;
   }
-
-#if 0
-  int EventReader::readMessage(Buf& here)
-  {
-    int len=0;
-    ist_->read((char*)&len,sizeof(int));
-
-    if(!*ist_ || len==0) return 0;
-
-    here.resize(len);
-    ist_->read(&here[0],len);
-    return len;
-  }
-
-  std::auto_ptr<EventPrincipal> EventReader::read(const ProductRegistry& prods)
-  {
-    int len = readMessage(b_);
-    if(len==0)
-	return std::auto_ptr<edm::EventPrincipal>();
-
-    edm::EventMsg msg(&b_[0],len);
-    return decoder_.decodeEvent(msg,prods);
-
-  }
-#endif
 
 }
