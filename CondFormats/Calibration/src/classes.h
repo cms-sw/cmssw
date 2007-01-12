9e09558881f942483b27421b20b0c980a6afa065
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-01-12 15:38:38 +0000

    add iterator

--- CondFormats/Calibration/src/classes.h
+++ CondFormats/Calibration/src/classes.h
@@ -2,15 +2,20 @@
 #include "CondFormats/Calibration/interface/BlobPedestals.h"
 #include "CondFormats/Calibration/interface/BlobNoises.h"
 #include "CondFormats/Calibration/interface/mySiStripNoises.h"
-// Declaration of the iterator (necessary for the generation of the dictionary)
-/*template std::vector< Pedestals::Item >::iterator;
-template std::vector< Pedestals::Item >::const_iterator;
-template std::vector< unsigned int >::iterator;
-template std::vector< unsigned int >::const_iterator;
-template std::vector< BlobNoises::DetRegistry >::iterator;
-template std::vector< BlobNoises::DetRegistry >::const_iterator;
-template std::vector< short >::iterator;
-template std::vector< short >::const_iterator;
-template std::vector< mySiStripNoises::DetRegistry >::iterator;
-template std::vector< mySiStripNoises::DetRegistry >::const_iterator;
-*/
+#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+#include "CondFormats/SiStripObjects/interface/FedChannelConnection.h"
+#include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+namespace {
+  std::vector< Pedestals::Item >::iterator tmp0;
+  std::vector< Pedestals::Item >::const_iterator tmp1;
+  std::vector< std::vector<BlobNoises::DetRegistry> >::iterator tmp2;
+  std::vector< std::vector<BlobNoises::DetRegistry> >::const_iterator tmp3; 
+  std::vector< mySiStripNoises::SiStripData >::iterator tmp4;
+  std::vector< mySiStripNoises::SiStripData >::const_iterator tmp5;
+  std::vector< mySiStripNoises::DetRegistry >::iterator tmp6;
+  std::vector< mySiStripNoises::DetRegistry >::const_iterator tmp7;   
+  std::vector<unsigned int>::iterator tmp8;
+  std::vector<unsigned int>::const_iterator tmp9;
+}
