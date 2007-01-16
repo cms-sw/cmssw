9cff193b2959bc22b3d3380fe964b808a194bd05
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-01-16 17:05:41 +0000

    Added include from DataFormats

--- TrackingTools/PatternTools/interface/TrajTrackAssociation.h
+++ TrackingTools/PatternTools/interface/TrajTrackAssociation.h
@@ -1,6 +1,8 @@
 #ifndef TrackingTools_PatternTools_TrajTrackAssociation_h
 #define TrackingTools_PatternTools_TrajTrackAssociation_h
 
+#include "DataFormats/Common/interface/AssociationMap.h"
+
 typedef edm::AssociationMap<edm::OneToOne<std::vector<Trajectory>,
                                           reco::TrackCollection,unsigned short> > TrajTrackAssociationCollection;
 typedef TrajTrackAssociationCollection::value_type TrajTrackAssociation;
