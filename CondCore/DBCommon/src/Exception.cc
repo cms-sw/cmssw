2cf8708a4f6049fbb6020610f7f878cd7d59bf09
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-01-16 17:52:58 +0000

    remove all

--- CondCore/DBCommon/src/Exception.cc
+++
@@ -1,8 +0,0 @@
-#include "CondCore/DBCommon/interface/Exception.h"
-cond::Exception::Exception( const std::string& message ):cms::Exception("Conditions",message){}
-cond::Exception::~Exception() throw(){}
-
-cond::noDataForRequiredTimeException::noDataForRequiredTimeException(
-const std::string& from, const std::string& rcd, const std::string& current ):
-  cond::Exception(from+":  no data available in "+rcd+" at time "+current){}
-cond::noDataForRequiredTimeException::~noDataForRequiredTimeException() throw(){}
