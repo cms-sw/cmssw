96edddcf9159549ed72aba43a1cafc8223747cc0
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-01-16 18:08:20 +0000

    added

---
+++ CondCore/IOVService/test/testPayloadObj.h
@@ -0,0 +1,10 @@
+#ifndef testPayloadObj_H
+#define testPayloadObj_H
+#include <vector>
+  class testPayloadObj {
+  public:
+    testPayloadObj(){}
+    virtual ~testPayloadObj(){}
+    std::vector<int> data;
+  };
+#endif
