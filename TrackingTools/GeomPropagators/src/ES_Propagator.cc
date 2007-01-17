9718d69b64a26f5cd03c23aeaacd2c25cbbb7d5c
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2007-01-17 19:49:18 +0000

    ESProducers moved to TT/Producers package

---
+++ TrackingTools/GeomPropagators/src/ES_Propagator.cc
@@ -0,0 +1,8 @@
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(Propagator);
