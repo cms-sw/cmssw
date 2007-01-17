9313482481dd4a8c3b7706c0555bd2c6527ca47b
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-01-17 16:28:23 +0000

    fix blobnoise iterator

--- CondFormats/Calibration/src/classes.h
+++ CondFormats/Calibration/src/classes.h
@@ -2,11 +2,12 @@
 #include "CondFormats/Calibration/interface/BlobPedestals.h"
 #include "CondFormats/Calibration/interface/BlobNoises.h"
 #include "CondFormats/Calibration/interface/mySiStripNoises.h"
-#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+/*#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
 #include "CondFormats/SiStripObjects/interface/FedChannelConnection.h"
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
 #include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
 #include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+*/
 namespace {
   std::vector< Pedestals::Item >::iterator tmp0;
   std::vector< Pedestals::Item >::const_iterator tmp1;
@@ -18,4 +19,6 @@ namespace {
   std::vector< mySiStripNoises::DetRegistry >::const_iterator tmp7;   
   std::vector<unsigned int>::iterator tmp8;
   std::vector<unsigned int>::const_iterator tmp9;
+  std::vector<short>::iterator tmp10;
+  std::vector<short>::const_iterator tmp11;
 }
