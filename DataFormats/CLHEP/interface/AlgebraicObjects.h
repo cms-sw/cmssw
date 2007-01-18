dbbb4618ad352f3b561d39ebf19f2dd0f9ddda4a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-18 00:05:33 +0000

    Moved from Geometry/CommonDetAlgo

---
+++ DataFormats/CLHEP/interface/AlgebraicObjects.h
@@ -0,0 +1,16 @@
+#ifndef _TRACKER_ALGEBRAICOBJECTS_H_
+#define _TRACKER_ALGEBRAICOBJECTS_H_
+
+#ifdef HEP_SHORT_NAMES
+#undef HEP_SHORT_NAMES
+#endif
+
+#include "CLHEP/Matrix/Vector.h"
+#include "CLHEP/Matrix/Matrix.h"
+#include "CLHEP/Matrix/SymMatrix.h"
+
+typedef HepVector      AlgebraicVector;
+typedef HepMatrix      AlgebraicMatrix;
+typedef HepSymMatrix   AlgebraicSymMatrix;
+
+#endif
