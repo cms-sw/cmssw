de355ff6a1bf721fc229fa9a8cb3f2397dc16f2f
Samvel Khalatyan <sha1-ae95162fd70683bc31ca18885c4c50db0feed2df@cern.ch>
2007-01-19 23:05:32 +0000

    Added AnaSiStripClusters

--- AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
+++ AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
@@ -1,15 +1,16 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/AnalyzeTracksClusters.h"
-#include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysis.h"
+//#include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysis.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysisFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/MTCCNtupleMaker.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/CosmicTIFFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/CosmicGenFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/TrackerMuFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/MTCCAmplifyDigis.h"
+#include "AnalysisExamples/SiStripDetectorPerformance/interface/AnaSiStripClusters.h"
 
-using cms::ClusterAnalysis;
+//using cms::ClusterAnalysis;
 using cms::ClusterAnalysisFilter;
 using cms::CosmicTIFFilter;
 using cms::CosmicGenFilter;
@@ -17,10 +18,11 @@ using cms::TrackerMuFilter;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(AnalyzeTracksClusters);
-DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
+//DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysisFilter);
 DEFINE_ANOTHER_FWK_MODULE(MTCCNtupleMaker);
 DEFINE_ANOTHER_FWK_MODULE(CosmicTIFFilter);
 DEFINE_ANOTHER_FWK_MODULE(CosmicGenFilter);
 DEFINE_ANOTHER_FWK_MODULE(TrackerMuFilter);
 DEFINE_ANOTHER_FWK_MODULE(MTCCAmplifyDigis);
+DEFINE_ANOTHER_FWK_MODULE(AnaSiStripClusters);
