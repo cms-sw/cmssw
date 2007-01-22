d802f19759a05f8a76945021c7176130addeb019
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-01-22 17:02:54 +0000

    Btag records added

---
+++ CondFormats/DataRecord/src/BTagTrackProbability3DRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     BTagTrackProbability3DRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Thu Jan 11 09:36:33 CET 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/BTagTrackProbability3DRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(BTagTrackProbability3DRcd);
