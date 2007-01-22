0e8e17f03158e36acbd786c2d421de959e97e6a9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-01-22 23:04:27 +0000

    Avoid unnecessary 'using' directives in header files, even if harmless, as in this case.

--- FWCore/ServiceRegistry/interface/connect_but_block_self.h
+++ FWCore/ServiceRegistry/interface/connect_but_block_self.h
@@ -17,7 +17,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Thu Sep 22 20:42:32 CEST 2005
-// $Id: connect_but_block_self.h,v 1.1 2005/09/26 23:15:03 chrjones Exp $
+// $Id: connect_but_block_self.h,v 1.2 2006/08/16 13:37:19 chrjones Exp $
 //
 
 // system include files
@@ -83,7 +83,7 @@ namespace edm {
          }
       template<class T, class TArg>
          BlockingWrapper<T,TArg> make_blockingwrapper(T iT,
-                                                      const sigc::slot<void,TArg>*){
+                                                      const sigc::slot<void,TArg>*) {
             return BlockingWrapper<T,TArg>(iT);
          }
       
@@ -91,10 +91,9 @@ namespace edm {
       template<class Func, class Signal>
       void 
       connect_but_block_self(Signal& oSignal, const Func& iFunc) {
-         using sigc::connection;
-         boost::shared_ptr<boost::shared_ptr<connection> > holder(new boost::shared_ptr<connection>());
-         *holder = boost::shared_ptr<connection>(
-                    new connection(oSignal.connect(
+         boost::shared_ptr<boost::shared_ptr<sigc::connection> > holder(new boost::shared_ptr<sigc::connection>());
+         *holder = boost::shared_ptr<sigc::connection>(
+                    new sigc::connection(oSignal.connect(
                                                    make_blockingwrapper(iFunc,
                                                                         static_cast<const typename Signal::slot_type*>(0)))));
       }
