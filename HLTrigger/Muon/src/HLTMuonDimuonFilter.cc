cce6992357e809990f5d07ab00e73b42fe23d4ca
Juan Alcaraz Maestre <sha1-00c52069b91662a716b700699899914afb62bf4e@cern.ch>
2007-01-22 13:20:24 +0000

    First version of a specific muon pair filter

---
+++ HLTrigger/Muon/src/HLTMuonDimuonFilter.cc
@@ -0,0 +1,223 @@
+/** \class HLTMuonDimuonFilter
+ *
+ * See header file for documentation
+ *
+ *  \author J. Alcaraz
+ *
+ */
+
+#include "HLTrigger/Muon/interface/HLTMuonDimuonFilter.h"
+
+#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/HLTReco/interface/HLTFilterObject.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
+
+//
+// constructors and destructor
+//
+HLTMuonDimuonFilter::HLTMuonDimuonFilter(const edm::ParameterSet& iConfig) :
+   candTag_     (iConfig.getParameter< edm::InputTag > ("CandTag")),
+   max_Eta_     (iConfig.getParameter<double> ("MaxEta")),
+   min_Nhits_   (iConfig.getParameter<int> ("MinNhits")),
+   max_Dr_      (iConfig.getParameter<double> ("MaxDr")),
+   max_Dz_      (iConfig.getParameter<double> ("MaxDz")),
+   chargeOpt_     (iConfig.getParameter<int> ("ChargeOpt")),
+   min_PtPair_     (iConfig.getParameter<double> ("MinPtPair")),
+   min_PtMax_     (iConfig.getParameter<double> ("MinPtMax")),
+   min_PtMin_     (iConfig.getParameter<double> ("MinPtMin")),
+   min_InvMass_ (iConfig.getParameter<double> ("MinInvMass")),
+   max_InvMass_ (iConfig.getParameter<double> ("MaxInvMass")),
+   min_Acop_    (iConfig.getParameter<double> ("MinAcop")),
+   max_Acop_    (iConfig.getParameter<double> ("MaxAcop")),
+   nsigma_Pt_   (iConfig.getParameter<double> ("NSigmaPt")) 
+{
+
+   LogDebug("HLTMuonDimuonFilter")
+      << " CandTag/MinN/MaxEta/MinNhits/MaxDr/MaxDz/MinPt1/MinPt2/MinInvMass/MaxInvMass/MinAcop/MaxAcop/NSigmaPt : " 
+      << candTag_.encode()
+      << " " << max_Eta_
+      << " " << min_Nhits_
+      << " " << max_Dr_
+      << " " << max_Dz_
+      << " " << chargeOpt_ << " " << min_PtPair_
+      << " " << min_PtMax_ << " " << min_PtMin_
+      << " " << min_InvMass_ << " " << max_InvMass_
+      << " " << min_Acop_ << " " << max_Acop_
+      << " " << nsigma_Pt_;
+
+   //register your products
+   produces<reco::HLTFilterObjectWithRefs>();
+}
+
+HLTMuonDimuonFilter::~HLTMuonDimuonFilter()
+{
+}
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+bool
+HLTMuonDimuonFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace std;
+   using namespace edm;
+   using namespace reco;
+
+   // All HLT filters must create and fill an HLT filter object,
+   // recording any reconstructed physics objects satisfying (or not)
+   // this HLT filter, and place it in the Event.
+
+   // The filter object
+   auto_ptr<HLTFilterObjectWithRefs>
+     filterproduct (new HLTFilterObjectWithRefs(path(),module()));
+
+   // Ref to Candidate object to be recorded in filter object
+   RefToBase<Candidate> ref1;
+   RefToBase<Candidate> ref2;
+
+   // get hold of trks
+   Handle<RecoChargedCandidateCollection> mucands;
+   iEvent.getByLabel (candTag_,mucands);
+
+   // look at all mucands,  check cuts and add to filter object
+   int n = 0;
+   RecoChargedCandidateCollection::const_iterator cand1;
+   RecoChargedCandidateCollection::const_iterator cand2;
+   for (cand1=mucands->begin(); cand1!=mucands->end(); cand1++) {
+      TrackRef tk1 = cand1->get<TrackRef>();
+      // eta cut
+      LogDebug("HLTMuonDimuonFilter") << " Muon in loop, eta= "
+            << tk1->eta() << ", hits= " << tk1->numberOfValidHits();
+      if (fabs(tk1->eta())>max_Eta_) continue;
+
+      // cut on number of hits
+      if (tk1->numberOfValidHits()<min_Nhits_) continue;
+
+      //dr cut
+      if (fabs(tk1->d0())>max_Dr_) continue;
+
+      //dz cut
+      if (fabs(tk1->dz())>max_Dz_) continue;
+
+      // Pt threshold cut
+      double pt1 = tk1->pt();
+      double err1 = tk1->error(0);
+      double abspar1 = fabs(tk1->parameter(0));
+      double ptLx1 = pt1;
+      // convert 50% efficiency threshold to 90% efficiency threshold
+      //if (abscur>0 && errcur>0) ptLx += 3.9*errcur/abscur*pt;
+      if (abspar1>0) ptLx1 += nsigma_Pt_*err1/abspar1*pt1;
+      LogDebug("HLTMuonDimuonFilter") << " ...Muon in loop, pt1= "
+            << pt1 << ", ptLx1= " << ptLx1;
+
+      for (cand2=cand1++; cand2!=mucands->end(); cand2++) {
+            TrackRef tk2 = cand2->get<TrackRef>();
+
+            // eta cut
+            LogDebug("HLTMuonDimuonFilter") << " Muon in loop, eta= "
+                  << tk2->eta() << ", hits= " << tk2->numberOfValidHits();
+            if (fabs(tk2->eta())>max_Eta_) continue;
+
+            // cut on number of hits
+            if (tk2->numberOfValidHits()<min_Nhits_) continue;
+
+            //dr cut
+            if (fabs(tk2->d0())>max_Dr_) continue;
+
+            //dz cut
+            if (fabs(tk2->dz())>max_Dz_) continue;
+
+            // Pt threshold cut
+            double pt2 = tk2->pt();
+            double err2 = tk2->error(0);
+            double abspar2 = fabs(tk2->parameter(0));
+            double ptLx2 = pt2;
+            // convert 50% efficiency threshold to 90% efficiency threshold
+            //if (abscur>0 && errcur>0) ptLx += 3.9*errcur/abscur*pt;
+            if (abspar2>0) ptLx2 += nsigma_Pt_*err2/abspar2*pt2;
+            LogDebug("HLTMuonDimuonFilter") << " ...Muon in loop, pt2= "
+                  << pt2 << ", ptLx2= " << ptLx2;
+
+            if (ptLx1>ptLx2) {
+                  if (ptLx1<min_PtMax_) continue;
+                  if (ptLx2<min_PtMin_) continue;
+            } else {
+                  if (ptLx1<min_PtMax_) continue;
+                  if (ptLx2<min_PtMin_) continue;
+            }
+
+            if (chargeOpt_<0) {
+                  if (tk1->charge()*tk2->charge()>0) continue;
+            } else if (chargeOpt_>0) {
+                  if (tk1->charge()*tk2->charge()<0) continue;
+            }
+
+            double acop = fabs(tk1->phi()-tk2->phi());
+            if (acop>M_PI) acop = 2*M_PI - acop;
+            acop = M_PI - acop;
+            if (acop<min_Acop_) continue;
+            if (acop>max_Acop_) continue;
+
+            double px12 = tk1->px() + tk2->px();
+            double py12 = tk1->py() + tk2->py();
+            double pt12 = sqrt(px12*px12+py12*py12);
+            if (pt12<min_PtPair_) continue;
+
+            double e1 = sqrt(tk1->p()*tk1->p()+0.106*0.106);
+            double e2 = sqrt(tk2->p()*tk2->p()+0.106*0.106);
+            double e12 = e1 + e2;
+            double pz12 = tk1->pz() + tk2->pz();
+            double invmass = e12*e12 - pt12*pt12 - pz12*pz12;
+            if (invmass>0) invmass = sqrt(invmass); else invmass = 0;
+            if (invmass<min_InvMass_) continue;
+            if (invmass>max_InvMass_) continue;
+
+            // Add this pair
+            n++;
+            LogDebug("HLTMuonDimuonFilter") << " Track1 passing filter: eta= " << tk1->eta() << ", pt: " << tk1->pt();
+            LogDebug("HLTMuonDimuonFilter") << " Track2 passing filter: eta= " << tk2->eta() << ", pt: " << tk2->pt();
+            LogDebug("HLTMuonDimuonFilter") << " Invmass= " << sqrt(invmass);
+
+            bool i1done = false;
+            bool i2done = false;
+            for (unsigned int i=0; i<filterproduct->size(); i++) {
+                  RefToBase<Candidate> candref = filterproduct->getParticleRef(i);
+                  TrackRef tktmp = candref->get<TrackRef>();
+                  if (tktmp==tk1) {
+                        i1done = true;
+                  } else if (tktmp==tk2) {
+                        i2done = true;
+                  }
+                  if (i1done && i2done) break;
+            }
+
+            if (!i1done) { 
+                  ref1=RefToBase<Candidate>( Ref<RecoChargedCandidateCollection>
+                     (mucands,distance(mucands->begin(), cand1)));
+                  filterproduct->putParticle(ref1);
+            }
+            if (!i2done) { 
+                  ref2=RefToBase<Candidate>( Ref<RecoChargedCandidateCollection>
+                     (mucands,distance(mucands->begin(), cand2)));
+                  filterproduct->putParticle(ref2);
+            }
+
+      }
+
+   }
+
+   // filter decision
+   const bool accept (n >= 1);
+
+   // put filter object into the Event
+   iEvent.put(filterproduct);
+
+   return accept;
+}
--- HLTrigger/Muon/src/SealModule.cc
+++ HLTrigger/Muon/src/SealModule.cc
@@ -4,8 +4,10 @@
 #include "HLTrigger/Muon/interface/HLTMuonL1Filter.h"
 #include "HLTrigger/Muon/interface/HLTMuonPreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonIsoFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonDimuonFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonL1Filter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonPreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonIsoFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonFilter);
