5e078e8bd08ad3efd6171cfba6d3f530bd02fc76
egeland <sha1-f6919fe6227703e11fc3407143274a6a419ae791@cern.ch>
2007-01-23 09:34:07 +0000

    Moving to CondTools/Ecal

--- CalibCalorimetry/EcalTBCondTools/src/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "CalibCalorimetry/EcalTBCondTools/interface/StoreEcalCondition.h"
-#include "CalibCalorimetry/EcalTBCondTools/interface/ReprocessEcalPedestals.h"
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(StoreEcalCondition);
-DEFINE_ANOTHER_FWK_MODULE(ReprocessEcalPedestals);
