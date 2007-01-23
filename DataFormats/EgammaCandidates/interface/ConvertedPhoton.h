4669f8be419270311c32744b41b5ff073b2d7b42
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-01-23 18:12:48 +0000

    Add the conversion vertex position in the constructor + a setter and a getter of RefVector to tracks

--- DataFormats/EgammaCandidates/interface/ConvertedPhoton.h
+++ DataFormats/EgammaCandidates/interface/ConvertedPhoton.h
@@ -6,7 +6,7 @@
  *
  * \author N.Marinelli  University of Notre Dame, US
  *
- * \version $Id: ConvertedPhoton.h,v 1.1 2006/06/22 12:41:33 nancy Exp $
+ * \version $Id: ConvertedPhoton.h,v 1.2 2006/12/19 18:12:53 nancy Exp $
  *
  */
 #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
@@ -15,19 +15,13 @@
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
 
 namespace reco {
-
-  class ConvertedPhoton : public RecoCandidate {
+    class ConvertedPhoton : public RecoCandidate {
   public:
     /// default constructor
     ConvertedPhoton() : RecoCandidate() { }
-    /// constructor from values
-    ConvertedPhoton( Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ) : 
-      RecoCandidate( q, p4, vtx ) { }
-    // 
-    ConvertedPhoton( const reco::SuperClusterRef scl,  const reco::TrackRefVector trkRefs , Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ):   RecoCandidate( q, p4, vtx ), superCluster_(scl), tracks_(trkRefs)  { }
-    //
 
-    ConvertedPhoton( const reco::SuperClusterRef scl,  Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ) ):   RecoCandidate( q, p4, vtx ), superCluster_(scl) { }
+
+   ConvertedPhoton( Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ), const Point & convVtx = Point( 0, 0, 0 )  ):  theConversionVertex_(convVtx) { }
 
    
     /// destructor
@@ -38,15 +32,23 @@ namespace reco {
     reco::SuperClusterRef superCluster() const ;
     /// set refrence to ConvertedPhoton component
     void setSuperCluster( const reco::SuperClusterRef & r ) { superCluster_ = r; }
+    // set reference to a pair of Tracks
+    void setTrackPairRef( const reco::TrackRefVector & pair ) { tracks_ = pair; }
     // reference to a vector of tracks
     reco::TrackRefVector tracks() const ;  
+    // returns the position of the conversion vertex
+    const Point & convVertexPosition() const { return theConversionVertex_ ; }
 
   private:
     /// check overlap with another candidate
     virtual bool overlap( const Candidate & ) const;
+   
+
     /// reference to a SuperCluster
     reco::SuperClusterRef superCluster_;
     reco::TrackRefVector  tracks_;
+    reco::Particle::Point theConversionVertex_;
+
   };
   
 }
