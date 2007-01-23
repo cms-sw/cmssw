35e4a18e7ae53862fb1c83abfd04fc4c4145b433
Kevin Burkett <sha1-d3b62d5921247fe185d98f6705fe0f56064ef84b@cern.ch>
2007-01-23 22:01:13 +0000

    Use LogDebug in place of std::cout

--- RecoTracker/RoadSearchTrackCandidateMaker/src/RoadSearchPairLess.cc
+++ RecoTracker/RoadSearchTrackCandidateMaker/src/RoadSearchPairLess.cc
@@ -5,6 +5,8 @@
 #include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
 #include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 bool RoadSearchPairLess::InsideOutCompare(const std::pair<TransientTrackingRecHit::ConstRecHitPointer, TrajectoryMeasurement*> HitTM1 ,
 					  const std::pair<TransientTrackingRecHit::ConstRecHitPointer, TrajectoryMeasurement*> HitTM2 ) const
      {
@@ -14,16 +16,16 @@ bool RoadSearchPairLess::InsideOutCompare(const std::pair<TransientTrackingRecHi
        DetId idb(HitTM2.first->det()->geographicalId());
 
        
-       std::cout<<" Comparing (r/phi/z) Hit 1 on DetID "
-		<< ida.rawId() << " : "
-		<< HitTM1.first->globalPosition().perp() << " / "
-		<< HitTM1.first->globalPosition().phi() << " / "
-		<< HitTM1.first->globalPosition().z()
-		<< " and Hit 2 on DetID "
-		<< idb.rawId() << " : "
-		<< HitTM2.first->globalPosition().perp() << " / "
-		<< HitTM2.first->globalPosition().phi() << " / "
-		<< HitTM2.first->globalPosition().z() << std::endl;
+       LogDebug("RoadSearch")<<" Comparing (r/phi/z) Hit 1 on DetID "
+			     << ida.rawId() << " : "
+			     << HitTM1.first->globalPosition().perp() << " / "
+			     << HitTM1.first->globalPosition().phi() << " / "
+			     << HitTM1.first->globalPosition().z()
+			     << " and Hit 2 on DetID "
+			     << idb.rawId() << " : "
+			     << HitTM2.first->globalPosition().perp() << " / "
+			     << HitTM2.first->globalPosition().phi() << " / "
+			     << HitTM2.first->globalPosition().z() ;
        
 
        if( (ida.subdetId() == StripSubdetector::TIB || ida.subdetId() == StripSubdetector::TOB || ida.subdetId() == PixelSubdetector::PixelBarrel) &&
@@ -46,9 +48,9 @@ bool RoadSearchPairLess::InsideOutCompare(const std::pair<TransientTrackingRecHi
        
        if( (ida.subdetId() == StripSubdetector::TIB || ida.subdetId() == StripSubdetector::TOB || ida.subdetId() == PixelSubdetector::PixelBarrel) &&
 	   (idb.subdetId() == StripSubdetector::TEC || idb.subdetId() == StripSubdetector::TID || idb.subdetId() == PixelSubdetector::PixelEndcap)) {  // barrel with barrel
-	 std::cout<<"*** How did this happen ?!?!? ***" << std::endl;
+	 LogDebug("RoadSearch") <<"*** How did this happen ?!?!? ***" ;
        }else{
-	 std::cout<<"*** How did this happen ?!?!? ***" << std::endl;
+	 LogDebug("RoadSearch") <<"*** How did this happen ?!?!? ***" ;
        }
        
        //throw DetLogicError("GeomDetLess: arguments are not Barrel or Forward GeomDets");
