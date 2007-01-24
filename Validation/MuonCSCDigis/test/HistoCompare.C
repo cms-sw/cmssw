12e34543c0952f0c5d9e33f26db8ff18f106fba9
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-01-24 00:42:40 +0000

    copied, but improved

---
+++ Validation/MuonCSCDigis/test/HistoCompare.C
@@ -0,0 +1,111 @@
+#include "TCanvas.h"
+#include <strstream>
+#include "TString.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
+#include "TPostScript.h"
+#include "TText.h"
+#include "TCanvas.h"
+#include <iostream>
+
+class HistoCompare {
+public:
+  virtual void compare(TH1 * oldHisto , TH1 * newHisto)
+  {
+    Double_t *res;
+    Double_t mypv = oldHisto->Chi2Test( newHisto,"UU", res);
+
+    std::strstream buf;
+    std::string value;
+    buf<<"PV="<<mypv<<std::endl;
+    buf>>value;
+
+    //  myte->DrawTextNDC(0.2,0.7, value.c_str());
+
+    std::cout << "[OVAL] " << oldHisto->GetName() << " PV = " << mypv << std::endl;
+    return;
+  }
+
+  virtual void openPage(string name, int nx=1, int ny=1) {}
+  virtual void closePage() {}
+
+};
+
+
+class HistoCompareDraw : public HistoCompare
+{
+public:
+  HistoCompareDraw() : theCanvas(0), subpad(0) {}
+
+  virtual void compare(TH1 * oldHisto , TH1 * newHisto) { 
+    theCanvas->cd(++subpad);
+    oldHisto->UseCurrentStyle();
+    newHisto->UseCurrentStyle();
+    oldHisto->SetLineColor(45);
+    newHisto->SetLineColor(47);
+    oldHisto->Draw();
+    newHisto->Draw("same");
+    HistoCompare::compare(oldHisto, newHisto);
+  }
+
+protected:
+  TCanvas * theCanvas;
+  int subpad;
+};
+
+
+class HistoCompareGif : public HistoCompareDraw
+{
+public:
+  HistoCompareGif(string title)
+  : theTitle(title),
+    theName("")
+  {
+  }
+
+  virtual void openPage(string name, int nx, int ny) {
+    theCanvas =  new TCanvas(theTitle.c_str(), theTitle.c_str(), 800, 600);
+    theCanvas->Divide(nx, ny);
+    theName = name + ".gif";
+    subpad = 0;
+  }
+  
+  virtual void closePage() {
+    theCanvas->Print(theName.c_str());
+  }
+
+private:
+  string theTitle;
+  string theName;
+};
+
+
+class HistoComparePS: public HistoCompareDraw
+{
+public:
+  HistoComparePS(string filename)
+  : thePostscript( new TPostScript(filename , -112) )
+  {
+    theCanvas = new TCanvas("ps", "ps", 800, 600);
+    thePostscript->Range(29.7 , 21.0);
+  }
+
+  HistoComparePS::~HistoComparePS()
+  {
+    thePostscript->Close();
+  }
+
+  virtual void openPage(string name, int nx=1, int ny=1) {
+    subpad = 0;
+  }
+    
+  virtual void closePage() {
+    theCanvas->Update();
+    thePostscript->NewPage();
+  }
+
+private:
+  TPostScript * thePostscript;
+};
+
