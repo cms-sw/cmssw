7c831f92cf1bad501d69c18ab093a4910513968d
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2007-01-26 10:56:59 +0000

    Moved Gsf* classes to GsfTrackReco

--- DataFormats/TrackReco/src/GsfTrack.cc
+++
@@ -1,7 +0,0 @@
-#include "DataFormats/TrackReco/interface/GsfTrack.h"
-using namespace reco;
-
-GsfTrack::GsfTrack( double chi2, double ndof, const Point & vertex, const Vector & momentum, int charge,
-		    const CovarianceMatrix & cov ) :
-  TrackBase( chi2, ndof, vertex, momentum, charge, cov ) {
-}
--- DataFormats/TrackReco/src/classes.h
+++ DataFormats/TrackReco/src/classes.h
@@ -1,10 +1,7 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
-#include "DataFormats/TrackReco/interface/GsfTrack.h"
-#include "DataFormats/TrackReco/interface/GsfTrackExtra.h"
 #include "DataFormats/Common/interface/AssociationMap.h"
-// #include "DataFormats/TrackReco/interface/GsfComponent5D.h"
 #include <vector>
 
 namespace {
@@ -22,23 +19,7 @@ namespace {
     reco::TrackRefVector rv1;
     edm::Wrapper<reco::TrackRefVector> wv1;
 
-    reco::GsfTrackExtraCollection v4;
-    edm::Wrapper<reco::GsfTrackExtraCollection> c4;
-    edm::Ref<reco::GsfTrackExtraCollection> r4;
-    edm::RefProd<reco::GsfTrackExtraCollection> rp4;
-    edm::RefVector<reco::GsfTrackExtraCollection> rv4;
-
-    reco::GsfTrackCollection v2;
-    edm::Wrapper<reco::GsfTrackCollection> c2;
-    edm::Ref<reco::GsfTrackCollection> r2;
-    edm::RefProd<reco::GsfTrackCollection> rp2;
-    edm::RefVector<reco::GsfTrackCollection> rv2;
-
     edm::helpers::Key< edm::RefProd < std::vector < reco::Track > > > rpt1;
     edm::AssociationMap<edm::OneToValue< std::vector<reco::Track>, double, unsigned int > > am1;
-    edm::helpers::Key< edm::RefProd < std::vector < reco::GsfTrack > > > rpt11;
-    edm::AssociationMap<edm::OneToValue< std::vector<reco::GsfTrack>, double, unsigned int > > am11;
-
-//     reco::GsfComponent5D s5;
   }
 }
