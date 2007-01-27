e28784a5391e39a869bd48ada92c923cf2f8b268
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-01-27 19:26:25 +0000

    Give an operator[]

--- SimMuon/CSCDigitizer/src/CSCAnalogSignal.h
+++ SimMuon/CSCDigitizer/src/CSCAnalogSignal.h
@@ -26,6 +26,8 @@
 #include <vector>
 #include <iosfwd>
 
+//TODO remove
+#include <iostream>
 
 class CSCAnalogSignal
 {
@@ -103,6 +105,13 @@ public:
     assert(i>=0 && i < getSize());
     return theBinValues[i];
   }
+
+  const float & operator[](int i) const {
+    assert(i>=0 && i < getSize());
+    return theBinValues[i];
+  }
+
+  unsigned size() const {return theBinValues.size();}
 
 private:
   int theElement;
