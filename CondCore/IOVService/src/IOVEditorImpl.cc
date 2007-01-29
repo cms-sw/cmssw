180f4d806f77cc80462316f47796d8ea1b14c690
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-01-29 15:22:46 +0000

    fix append boundary value

--- CondCore/IOVService/src/IOVEditorImpl.cc
+++ CondCore/IOVService/src/IOVEditorImpl.cc
@@ -9,14 +9,12 @@ cond::IOVEditorImpl::IOVEditorImpl( cond::PoolStorageManager& pooldb,
 				    ):m_pooldb(pooldb),m_token(token),m_globalSince(globalSince),m_globalTill(globalTill),m_isActive(false){
 }
 void cond::IOVEditorImpl::init(){
-  //m_pooldb.startTransaction(cond::ReadOnly);
   if(!m_token.empty()){
     m_iov=cond::Ref<cond::IOV>(m_pooldb, m_token); 
   }else{
     m_iov=cond::Ref<cond::IOV>(m_pooldb,new cond::IOV);
   }
   *m_iov;
-  //m_pooldb.commit();
   m_isActive=true;
 }
 cond::IOVEditorImpl::~IOVEditorImpl(){
@@ -27,18 +25,15 @@ void cond::IOVEditorImpl::insert( cond::Time_t tillTime,
   if(!m_isActive) this->init();
   //fix me: throw if beyond global range!!! 
   m_iov->iov.insert(std::make_pair<cond::Time_t, std::string>(tillTime, payloadToken));
-  //m_pooldb.startTransaction(cond::ReadWriteCreate);
   if(m_token.empty()){
     m_iov.markWrite(cond::IOVNames::container());
   }else{
     m_iov.markUpdate();
   }
   m_token=m_iov.token();
-  //m_pooldb.commit();
 }
 void cond::IOVEditorImpl::bulkInsert(std::vector< std::pair<cond::Time_t,std::string> >& values){
     if(!m_isActive) this->init();
-    //m_pooldb.startTransaction(cond::ReadWriteCreate);
     for(std::vector< std::pair<cond::Time_t,std::string> >::iterator it=values.begin(); it!=values.end(); ++it){
       m_iov->iov.insert(*it);
     }
@@ -48,7 +43,6 @@ void cond::IOVEditorImpl::bulkInsert(std::vector< std::pair<cond::Time_t,std::st
     }else{
       m_iov.markUpdate();   
     }
-    //m_pooldb.commit();
 }
 void cond::IOVEditorImpl::updateClosure( cond::Time_t newtillTime ){
   if( m_token.empty() ) throw cond::Exception("cond::IOVEditorImpl::updateClosure cannot change non-existing IOV index");
@@ -57,9 +51,7 @@ void cond::IOVEditorImpl::updateClosure( cond::Time_t newtillTime ){
   std::string closePayload=m_iov->iov.rbegin()->second;
   m_iov->iov.insert( std::make_pair(newtillTime,closePayload) );
   m_iov->iov.erase( m_iov->iov.find(closeIOV) );
-  // m_pooldb.startTransaction(cond::ReadWriteCreate);
   m_iov.markUpdate();
-  //m_pooldb.commit();
 }
 void cond::IOVEditorImpl::append(  cond::Time_t sinceTime ,
 				   const std::string& payloadToken
@@ -70,23 +62,17 @@ void cond::IOVEditorImpl::append(  cond::Time_t sinceTime ,
   cond::Time_t lastIOV=m_iov->iov.rbegin()->first;
   std::string lastPayload=m_iov->iov.rbegin()->second;
   m_iov->iov[lastIOV]=payloadToken;
-  m_iov->iov.insert( std::make_pair((sinceTime+1),lastPayload) );
-  //m_pooldb.startTransaction(cond::ReadWriteCreate);
+  m_iov->iov.insert( std::make_pair((sinceTime-1),lastPayload) );
   m_iov.markUpdate();
-  //m_pooldb.commit();
 }
 void cond::IOVEditorImpl::deleteEntries(){
   if( m_token.empty() ) throw cond::Exception("cond::IOVEditorImpl::deleteEntries cannot delete to non-existing IOV index");
   if(!m_isActive) this->init();
-  //m_pooldb.startTransaction(cond::ReadWriteCreate);
   m_iov.markDelete();
-  //m_pooldb.commit();
 }
 void cond::IOVEditorImpl::import( const std::string& sourceIOVtoken ){
   if( !m_token.empty() ) throw cond::Exception("cond::IOVEditorImpl::import IOV index already exists, cannot import");
-  //m_pooldb.startTransaction(cond::ReadWriteCreate);
   m_iov=cond::Ref<cond::IOV>(m_pooldb,sourceIOVtoken);
   m_iov.markWrite(cond::IOVNames::container());
-  //m_pooldb.commit();
   m_token=m_iov.token();
 }
