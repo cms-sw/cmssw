c40b9ece7147d782c3c852d14f4745a62ef11a50
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-01-29 20:18:45 +0000

    Move TrigPrim to own packages

--- SimCalorimetry/HcalSimProducers/src/SealModule.cc
+++ SimCalorimetry/HcalSimProducers/src/SealModule.cc
@@ -1,14 +1,12 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalDigiProducer.h"
-#include "SimCalorimetry/HcalSimProducers/src/HcalTrigPrimDigiProducer.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalHitAnalyzer.h"
 #include "SimCalorimetry/HcalSimProducers/src/HcalDigiAnalyzer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalDigiProducer);
-DEFINE_ANOTHER_FWK_MODULE(HcalTrigPrimDigiProducer);
 DEFINE_ANOTHER_FWK_MODULE(HcalHitAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(HcalDigiAnalyzer);
 
