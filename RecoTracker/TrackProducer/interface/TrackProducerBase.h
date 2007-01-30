bb2a365845413ffa849e66d95d51238538fff053
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2007-01-30 09:24:35 +0000

    moving ESHandle from FWCore to DataFormats

--- RecoTracker/TrackProducer/interface/TrackProducerBase.h
+++ RecoTracker/TrackProducer/interface/TrackProducerBase.h
@@ -16,7 +16,7 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/Common/interface/ESHandle.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
