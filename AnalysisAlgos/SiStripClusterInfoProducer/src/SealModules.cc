1d8dc764e31bc93d34e1f1b2bf0abaa669069b7a
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-02-02 18:16:50 +0000

    commit of a fake module to make SiStripClusterInfoProducer works on SimData

---
+++ AnalysisAlgos/SiStripClusterInfoProducer/src/SealModules.cc
@@ -0,0 +1,14 @@
+
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+//#include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripClusterInfoProducer.h"
+#include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripFakeRawDigiModule.h"
+
+//using cms::SiStripClusterInfoProducer;
+SiStriFakeRawDigiModule;
+
+DEFINE_SEAL_MODULE();
+//DEFINE_ANOTHER_FWK_MODULE(SiStripClusterInfoProducer);
+DEFINE_ANOTHER_FWK_MODULE(SiStripFakeRawDigiModule);
