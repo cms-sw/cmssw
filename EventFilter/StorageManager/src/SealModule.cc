dbac86278e66bdb62782bc1bc4abd39d5c5476e8
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2007-02-04 06:33:52 +0000

    removed StreamerI2OWriter to different package, EventFilter/FUSender

--- EventFilter/StorageManager/src/SealModule.cc
+++ EventFilter/StorageManager/src/SealModule.cc
@@ -1,16 +1,10 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "IOPool/Streamer/interface/StreamerOutputModule.h"
-#include "EventFilter/StorageManager/src/StreamerI2OWriter.h"
 #include "EventFilter/StorageManager/src/EventStreamHttpReader.h"
 
-typedef edm::StreamerOutputModule<edm::StreamerI2OWriter> I2OStreamConsumer;
-
-using edm::StreamerI2OWriter;
 using edm::EventStreamHttpReader;
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(I2OStreamConsumer);
 DEFINE_ANOTHER_FWK_INPUT_SOURCE(EventStreamHttpReader);
 
