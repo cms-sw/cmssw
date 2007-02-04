9422ed47d902b34ef6f0639e133e485f6ce4a21b
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-02-04 21:05:42 +0000

    SiStripClusterInfoProducer moved here from RecoLocalTracker/SiStripClusterizer

--- AnalysisAlgos/SiStripClusterInfoProducer/src/SealModules.cc
+++ AnalysisAlgos/SiStripClusterInfoProducer/src/SealModules.cc
@@ -3,12 +3,12 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-//#include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripClusterInfoProducer.h"
+#include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripClusterInfoProducer.h"
 #include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripFakeRawDigiModule.h"
 
-//using cms::SiStripClusterInfoProducer;
+using cms::SiStripClusterInfoProducer;
 SiStriFakeRawDigiModule;
 
 DEFINE_SEAL_MODULE();
-//DEFINE_ANOTHER_FWK_MODULE(SiStripClusterInfoProducer);
+DEFINE_ANOTHER_FWK_MODULE(SiStripClusterInfoProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripFakeRawDigiModule);
