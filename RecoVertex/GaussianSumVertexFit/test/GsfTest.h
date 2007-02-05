eeaad5177e6bd237e7864810d57250b37834f5ed
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-02-05 17:02:48 +0000

    First commit of GSF vertex fitter

---
+++ RecoVertex/GaussianSumVertexFit/test/GsfTest.h
@@ -0,0 +1,64 @@
+// -*- C++ -*-
+//
+// Package:    GsfTest
+// Class:      GsfTest
+// 
+/**\class GsfTest GsfTest.cc RecoVertex/GsfTest/src/GsfTest.cc
+
+ Description: steers tracker primary vertex reconstruction and storage
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Pascal Vanlaer
+//         Created:  Tue Feb 28 11:06:34 CET 2006
+// $Id: GsfTest.h,v 1.3 2006/10/31 16:52:03 speer Exp $
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "SimDataFormats/Vertex/interface/SimVertex.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
+#include "RecoVertex/KalmanVertexFit/test/SimpleVertexTree.h"
+#include "SimTracker/TrackAssociation/interface/TrackAssociatorByChi2.h"
+#include <TFile.h>
+
+  /**
+   * This is a very simple test analyzer mean to test the KalmanVertexFitter
+   */
+
+class GsfTest : public edm::EDAnalyzer {
+public:
+  explicit GsfTest(const edm::ParameterSet&);
+  ~GsfTest();
+  
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+
+  virtual void beginJob(edm::EventSetup const&);
+  virtual void endJob();
+
+private:
+
+  TrackingVertex getSimVertex(const edm::Event& iEvent) const;
+
+  edm::ParameterSet theConfig;
+  edm::ParameterSet gsfPSet;
+  TrackAssociatorByChi2 * associatorForParamAtPca;
+  SimpleVertexTree *tree;
+  TFile*  rootFile_;
+
+  std::string outputFile_; // output file
+  std::string trackLabel_; // label of track producer
+};
