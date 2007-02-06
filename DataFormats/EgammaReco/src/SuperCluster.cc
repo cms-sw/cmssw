06e9fc0f76b8eba69a0f6457ad154b5346343f7d
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-02-06 23:58:53 +0000

    Remove shape accessor methods

--- DataFormats/EgammaReco/src/SuperCluster.cc
+++ DataFormats/EgammaReco/src/SuperCluster.cc
@@ -1,4 +1,4 @@
-// $Id: SuperCluster.cc,v 1.5 2006/05/23 16:28:06 askew Exp $
+// $Id: SuperCluster.cc,v 1.6 2006/09/12 23:53:40 rahatlou Exp $
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
 #include "DataFormats/EgammaReco/interface/ClusterShape.h"
@@ -33,56 +33,3 @@ SuperCluster::SuperCluster( double energy, const math::XYZPoint& position,
   } // loop over basic clusters
 
 }
-
-std::vector<DetId>
-SuperCluster::getHitsByDetId() const {
-  return usedHits_;
-}
-
-
-
-double SuperCluster::eMax() const{
-  return shape_->eMax();
-}
-
-double SuperCluster::e2x2() const{
-  return shape_->e2x2();
-}
-
-double SuperCluster::e3x3() const{
-  return shape_->e3x3();
-}
-
-double SuperCluster::e5x5() const{
-  return shape_->e5x5();
-}
-
-double SuperCluster::covEtaEta() const{
-  return shape_->covEtaEta();
-}
-
-double SuperCluster::covEtaPhi() const{
-  return shape_->covEtaPhi();
-}
-
-double SuperCluster::covPhiPhi() const{
-  return shape_->covPhiPhi();
-}
-
-// double SuperCluster::hadOverEcal() const{
-//   return shape_->hadOverEcal();
-// }
-
-/**
-double SuperCluster::disc1() const { 
-  return pi0Discriminator_->disc1(); 
-}
-
-double SuperCluster::disc2() const { 
-  return pi0Discriminator_->disc2(); 
-}
-
-double SuperCluster::disc3() const { 
-  return pi0Discriminator_->disc3(); 
-}
-**/
