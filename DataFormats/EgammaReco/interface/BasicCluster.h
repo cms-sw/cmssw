aff6ff4a68b60a6b6500e80d2f6e063a7abb6322
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-02-06 23:51:27 +0000

    Remove ClusterShape data member and set and access methods (replaced by AssociationMap).  Remove declaration of obsolete class EcalRecHitData.  Remove redundant method superClusterId()

--- DataFormats/EgammaReco/interface/BasicCluster.h
+++ DataFormats/EgammaReco/interface/BasicCluster.h
@@ -7,62 +7,30 @@
  *
  * \author Luca Lista, INFN
  *
- * \version $Id: BasicCluster.h,v 1.6 2006/05/10 12:45:59 tsirig Exp $
+ * \version $Id: BasicCluster.h,v 1.7 2006/05/23 16:25:46 askew Exp $
  *
  */
 #include "DataFormats/Math/interface/Point3D.h"
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
 #include "DataFormats/EgammaReco/interface/EcalCluster.h"
-#include "DataFormats/EgammaReco/interface/ClusterShape.h"
 
 namespace reco {
   
-  class EcalRecHitData {
-  public:
-    EcalRecHitData() { }
-    EcalRecHitData( float e, float chi2, DetId id, float frac=1 ) :
-      energy_(e), chi2_(chi2), detId_(id), fraction_(frac) { }
-    ~EcalRecHitData() { }
-    float energy() const {return energy_;}
-    float chi2() const {return chi2_;}
-    DetId detId() const {return detId_;}
-    float fraction() const {return fraction_;}
-  private:
-    float energy_;
-    float chi2_;
-    DetId detId_;
-    float fraction_;
-  };
-
   enum AlgoId { island = 0, hybrid = 1 };
 
   class BasicCluster : public EcalCluster {
   public:
-    /// point in the space
+
     typedef math::XYZPoint Point;
-    /// index type used as reference to RecHit.
-    //typedef unsigned short RecHitIndex;
-    /// collection of (indices to) RecHit with assoxciated energy fraction
-    //typedef std::vector<std::pair<RecHitIndex, float> > RecHitCollection;
 
     /// default constructor
     BasicCluster() : EcalCluster(0., Point(0.,0.,0.)), chi2_(-1.) { }
 
-    /// constructor from EcalRecHits
-    //BasicCluster( const std::vector<EcalRecHitData>& recHits,
-    //              int superClusterId,
-    //             const Point & position = Point( 0, 0, 0 ) );
-
     BasicCluster( const double energy, const Point& position, const double chi2, const std::vector<DetId> usedHits, AlgoId algoID = hybrid);
 
+    /// DetIds of component RecHits
     virtual std::vector<DetId> getHitsByDetId() const { return usedHits_; }
-
-    void SetClusterShapeRef(ClusterShapeRef ref){ clusref=ref;}
-    /// cluster centriod position
-    //const Point & position() const { return position_; }
-    /// cluster energy
-    //double energy() const { return energy_; }
 
     /// chi-squared
     double chi2() const { return chi2_; }
@@ -70,34 +38,20 @@ namespace reco {
     /// identifier of the algorithm
     AlgoId algo() const { return algoId_; }
 
-    /// identifier of supercluster, or -1 in case of island algorithm
-    int superClusterId() const { return algoId_; }
-
-    /// Access to ECAL RecHit information
-    //std::vector<EcalRecHitData> recHits() const { return recHits_; }
-
     /// this method is needed to sort the BasicClusters by energy
     bool operator<(const reco::BasicCluster &otherCluster) const;
     bool operator==(const BasicCluster& rhs) const;
 
   private:
-    /// cluster centroid position
-    //Point position_;
-    /// cluster energy
-    //Double32_t energy_;
 
     /// chi-squared
     Double32_t chi2_;
 
     /// 0 in case of island algorithm, 1 in case of hybrid
     AlgoId algoId_;
-    //Associated clustershape information;
-    ClusterShapeRef clusref;
-    /// ECAL RecHit information
-    //std::vector<EcalRecHitData> recHits_;
 
-   /// used hits by detId
-   std::vector<DetId> usedHits_;
+    /// used hits by detId
+    std::vector<DetId> usedHits_;
 
   };
 
