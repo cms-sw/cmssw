14af654ef85c384d97900768ef75b284ec643bbc
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2007-02-07 09:34:07 +0000

    Small fix

--- AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
+++ AnalysisExamples/SiStripDetectorPerformance/src/SealModules.cc
@@ -1,21 +1,16 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/AnalyzeTracksClusters.h"
-//#include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysis.h"
+#include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysis.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/ClusterAnalysisFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/MTCCNtupleMaker.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/CosmicTIFFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/CosmicGenFilter.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/TrackerMuFilter.h"
-<<<<<<< SealModules.cc
 // M. De Mattia 25/1/2007
 #include "AnalysisExamples/SiStripDetectorPerformance/interface/TIFNtupleMaker.h"
 // M. De Mattia 30/1/2007
 #include "AnalysisAlgos/SiStripClusterInfoProducer/interface/SiStripFakeRawDigiModule.h"
-=======
-#include "AnalysisExamples/SiStripDetectorPerformance/interface/MTCCAmplifyDigis.h"
-#include "AnalysisExamples/SiStripDetectorPerformance/interface/AnaSiStripClusters.h"
->>>>>>> 1.7
 
 //using cms::ClusterAnalysis;
 using cms::ClusterAnalysisFilter;
@@ -25,18 +20,13 @@ using cms::TrackerMuFilter;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(AnalyzeTracksClusters);
-//DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
+DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysisFilter);
 DEFINE_ANOTHER_FWK_MODULE(MTCCNtupleMaker);
 DEFINE_ANOTHER_FWK_MODULE(CosmicTIFFilter);
 DEFINE_ANOTHER_FWK_MODULE(CosmicGenFilter);
 DEFINE_ANOTHER_FWK_MODULE(TrackerMuFilter);
-<<<<<<< SealModules.cc
 // 25/1/2007
 DEFINE_ANOTHER_FWK_MODULE(TIFNtupleMaker);
 // 25/1/2007
 DEFINE_ANOTHER_FWK_MODULE(SiStripFakeRawDigiModule);
-=======
-DEFINE_ANOTHER_FWK_MODULE(MTCCAmplifyDigis);
-DEFINE_ANOTHER_FWK_MODULE(AnaSiStripClusters);
->>>>>>> 1.7
