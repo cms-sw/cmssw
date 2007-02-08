ea50505f015bc5ea4263f5043705597ac71c0a60
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-02-08 17:13:04 +0000

    Some fixes + adaptation to new DB interface.

--- Alignment/TrackerAlignment/interface/TrackerAlignment.h
+++ Alignment/TrackerAlignment/interface/TrackerAlignment.h
@@ -2,48 +2,42 @@
 #define Alignment_TrackerAlignment_TrackerAlignment_H
 
 /** \class TrackerAlignment
-*  The TrackerAlignment helper class for alignment jobs
-*  - Rotates and Translates any module for the tracker based on rawId
-*
-*  \author Nhan Tran
-*/
+ *  The TrackerAlignment helper class for alignment jobs
+ *  - Rotates and Translates any module for the tracker based on rawId
+ *
+ *  \author Nhan Tran
+ */
 
 #include "Alignment/TrackerAlignment/interface/AlignableTracker.h"
 
 class TrackerAlignment{
  
 public:
-	//Define Return Type
-	typedef boost::shared_ptr<TrackerGeometry> ReturnType;
-	//Produce the geometry
-	//	virtual ReturnType produce(const TrackerDigiGeometryRecord& iRecord);
+  TrackerAlignment( const edm::EventSetup& setup );
 	
-	TrackerAlignment( const edm::EventSetup& setup );
+  ~TrackerAlignment();
 	
-	~TrackerAlignment(){};
+  AlignableTracker* getAlignableTracker() { return theAlignableTracker; }
 	
-	AlignableTracker* getAlignableTracker() { return theAlignableTracker; }
+  void moveAlignablePixelEndCaps( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
+  void moveAlignableEndCaps( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
+  void moveAlignablePixelHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
+  void moveAlignableInnerHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
+  void moveAlignableOuterHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );	
+  void moveAlignableTIDs( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
+  
+  void saveToDB();
+  int rawid;
+  
+  std::vector<float> local_displacements;
+  
+  std::vector<float> local_rotations;
 	
-	void moveAlignablePixelEndCaps( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
-	void moveAlignableEndCaps( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
-	void moveAlignablePixelHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
-	void moveAlignableInnerHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
-	void moveAlignableOuterHalfBarrels( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );	
-	void moveAlignableTIDs( int rawId, std::vector<float> localDisplacements, std::vector<float> localRotations  );
-	
-	void saveToDB();
-	int rawid;
-	
-	std::vector<float> local_displacements;
-	
-	std::vector<float> local_rotations;
-	
-	
-	AlignableTracker* theAlignableTracker;
-	
-	
-private:
-	ReturnType theTracker;
+  
+private:  
+  AlignableTracker* theAlignableTracker;
 
+  std::string theAlignRecordName, theErrorRecordName;
+  
 };
 #endif //TrackerAlignment_H
--- Alignment/TrackerAlignment/src/TrackerAlignment.cc
+++ Alignment/TrackerAlignment/src/TrackerAlignment.cc
@@ -11,8 +11,7 @@
  
 // Tracker geom
 #include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
-#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeomBuilderFromGeometricDet.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 
 // Alignment
 #include "CondFormats/Alignment/interface/Alignments.h"
@@ -23,48 +22,61 @@
 
 //__________________________________________________________________
 //
-TrackerAlignment::TrackerAlignment(const edm::EventSetup& setup  ){
+TrackerAlignment::TrackerAlignment(const edm::EventSetup& setup  ) :
+  theAlignRecordName( "TrackerAlignmentRcd" ),
+  theErrorRecordName( "TrackerAlignmentErrorRcd" )
+{
 	//Create the tracker geometry from the ideal geometry
 	// Create the tracker geometry from ideal geometry (first time only)
-	edm::ESHandle<DDCompactView> cpv;
+	edm::ESHandle<TrackerGeometry> trackerGeometry;
 	edm::ESHandle<GeometricDet> gD;
-	setup.get<IdealGeometryRecord>().get( cpv );
-	setup.get<IdealGeometryRecord>().get( gD );
-	TrackerGeomBuilderFromGeometricDet trackerBuilder;
-	theTracker  = boost::shared_ptr<TrackerGeometry>( trackerBuilder.build(&(*cpv),&(*gD)) );
-	
+	setup.get<TrackerDigiGeometryRecord>().get( trackerGeometry );
 	
 	//Retrieve alignable units
-	theAlignableTracker = new AlignableTracker(&(*gD),  &(*theTracker));
+	theAlignableTracker = new AlignableTracker(&(*gD),  &(*trackerGeometry));
 }
+
+
+//__________________________________________________________________
+//
+TrackerAlignment::~TrackerAlignment( void )
+{
+
+  delete theAlignableTracker;
+
+}
+
+
 //__________________________________________________________________
 //
 void TrackerAlignment::moveAlignablePixelEndCaps( int rawid , std::vector<float> local_displacements,  std::vector<float> local_rotations  ){
 	
 	// Displace and rotate pixelEndCaps
 	std::vector<Alignable*> thePixelEndCapsAlignables = theAlignableTracker->pixelEndcapGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = thePixelEndCapsAlignables.begin(); iter != thePixelEndCapsAlignables.end(); iter++ ){ 
-
+	for ( std::vector<Alignable*>::iterator iter = thePixelEndCapsAlignables.begin(); 
+          iter != thePixelEndCapsAlignables.end(); ++iter )
+      { 
+        
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
 		
 		// Select the given module
 		if ( id == rawid ){
-			
-			// Convert local to global diplacements
-			LocalVector lvector( local_displacements.at(0), local_displacements.at(1), local_displacements.at(2)); 
-			GlobalVector gvector = ((*iter)->surface()).toGlobal( lvector );
-			
-			// global displacement 
-			(*iter)->move( gvector );
-			
-			// local rotation 
-			(*iter)->rotateAroundLocalX( local_rotations.at(0) ); // Local X axis rotation
-			(*iter)->rotateAroundLocalY( local_rotations.at(1) ); // Local Y axis rotation
-			(*iter)->rotateAroundLocalZ( local_rotations.at(2) ); // Local Z axis rotation
-			
+          
+          // Convert local to global diplacements
+          LocalVector lvector( local_displacements.at(0), local_displacements.at(1), local_displacements.at(2)); 
+          GlobalVector gvector = ((*iter)->surface()).toGlobal( lvector );
+          
+          // global displacement 
+          (*iter)->move( gvector );
+          
+          // local rotation 
+          (*iter)->rotateAroundLocalX( local_rotations.at(0) ); // Local X axis rotation
+          (*iter)->rotateAroundLocalY( local_rotations.at(1) ); // Local Y axis rotation
+          (*iter)->rotateAroundLocalZ( local_rotations.at(2) ); // Local Z axis rotation
+          
 		}
-	}
+      }
 }
 //__________________________________________________________________
 //
@@ -72,7 +84,8 @@ void TrackerAlignment::moveAlignableEndCaps( int rawid , std::vector<float> loca
 	
 	// Displace and rotate EndCaps
 	std::vector<Alignable*> theEndCapsAlignables = theAlignableTracker->endcapGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = theEndCapsAlignables.begin(); iter != theEndCapsAlignables.end(); iter++ ){ 
+	for ( std::vector<Alignable*>::iterator iter = theEndCapsAlignables.begin(); 
+          iter != theEndCapsAlignables.end(); ++iter ){ 
 		
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
@@ -101,7 +114,8 @@ void TrackerAlignment::moveAlignablePixelHalfBarrels( int rawid , std::vector<fl
 	
 	// Displace and rotate PixelHalfBarrels
 	std::vector<Alignable*> thePixelHalfBarrelsAlignables = theAlignableTracker->pixelHalfBarrelGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = thePixelHalfBarrelsAlignables.begin(); iter != thePixelHalfBarrelsAlignables.end(); iter++ ){ 
+	for ( std::vector<Alignable*>::iterator iter = thePixelHalfBarrelsAlignables.begin(); 
+          iter != thePixelHalfBarrelsAlignables.end(); ++iter ){ 
 		
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
@@ -130,7 +144,8 @@ void TrackerAlignment::moveAlignableOuterHalfBarrels( int rawid , std::vector<fl
 	
 	// Displace and rotate OuterHalfBarrels
 	std::vector<Alignable*> theOuterHalfBarrelsAlignables = theAlignableTracker->outerBarrelGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = theOuterHalfBarrelsAlignables.begin(); iter != theOuterHalfBarrelsAlignables.end(); iter++ ){ 
+	for ( std::vector<Alignable*>::iterator iter = theOuterHalfBarrelsAlignables.begin(); 
+          iter != theOuterHalfBarrelsAlignables.end(); ++iter ){ 
 		
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
@@ -159,7 +174,8 @@ void TrackerAlignment::moveAlignableInnerHalfBarrels( int rawid , std::vector<fl
 	
 	// Displace and rotate InnerHalfBarrels
 	std::vector<Alignable*> theInnerHalfBarrelsAlignables = theAlignableTracker->innerBarrelGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = theInnerHalfBarrelsAlignables.begin(); iter != theInnerHalfBarrelsAlignables.end(); iter++ ){ 
+	for ( std::vector<Alignable*>::iterator iter = theInnerHalfBarrelsAlignables.begin(); 
+          iter != theInnerHalfBarrelsAlignables.end(); ++iter ){ 
 		
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
@@ -188,7 +204,8 @@ void TrackerAlignment::moveAlignableTIDs( int rawid , std::vector<float> local_d
 	
 	// Displace and rotate TIDs
 	std::vector<Alignable*> theTIDsAlignables = theAlignableTracker->TIDGeomDets();
-	for ( std::vector<Alignable*>::iterator iter = theTIDsAlignables.begin(); iter != theTIDsAlignables.end(); iter++ ){ 
+	for ( std::vector<Alignable*>::iterator iter = theTIDsAlignables.begin(); 
+          iter != theTIDsAlignables.end(); ++iter ){ 
 		
 		// Get the raw ID of the associated GeomDet
 		int id = (*iter)->geomDetId().rawId();
@@ -211,6 +228,10 @@ void TrackerAlignment::moveAlignableTIDs( int rawid , std::vector<float> local_d
 		}
 	}
 }
+
+
+//__________________________________________________________________
+//
 void TrackerAlignment::saveToDB(void){
 	
 	// Output POOL-ORA objects
@@ -218,23 +239,25 @@ void TrackerAlignment::saveToDB(void){
   if( !poolDbService.isAvailable() ) // Die if not available
 	throw cms::Exception("NotAvailable") << "PoolDBOutputService not available";
 
-	 // Retrieve, sort and store
+  // Retrieve and store
   Alignments* alignments = theAlignableTracker->alignments();
-	AlignmentErrors* alignmentErrors = theAlignableTracker->alignmentErrors();
-	std::sort( alignments->m_align.begin(), alignments->m_align.end(), 
-			 lessAlignmentDetId<AlignTransform>() );
-  std::sort( alignmentErrors->m_alignError.begin(), alignmentErrors->m_alignError.end(), 
-			 lessAlignmentDetId<AlignTransformError>() );
+  AlignmentErrors* alignmentErrors = theAlignableTracker->alignmentErrors();
 
-	// Define callback tokens for the payloads
-  size_t alignmentsToken = poolDbService->callbackToken("Alignments");
-  size_t alignmentErrorsToken = poolDbService->callbackToken("AlignmentErrors");
 
-	poolDbService->newValidityForNewPayload<Alignments>( alignments, 
-		poolDbService->endOfTime(),
-		alignmentsToken );
-  poolDbService->newValidityForNewPayload<AlignmentErrors>( alignmentErrors, 
-		poolDbService->endOfTime(),
-		alignmentErrorsToken );
+  if ( poolDbService->isNewTagRequest(theAlignRecordName) )
+    poolDbService->createNewIOV<Alignments>( alignments, poolDbService->endOfTime(), 
+                                             theAlignRecordName );
+  else
+    poolDbService->appendSinceTime<Alignments>( alignments, poolDbService->currentTime(), 
+                                                theAlignRecordName );
+  if ( poolDbService->isNewTagRequest(theErrorRecordName) )
+    poolDbService->createNewIOV<AlignmentErrors>( alignmentErrors,
+                                                  poolDbService->endOfTime(), 
+                                                  theErrorRecordName );
+  else
+    poolDbService->appendSinceTime<AlignmentErrors>( alignmentErrors,
+                                                     poolDbService->currentTime(), 
+                                                     theErrorRecordName );
+
 }
 
