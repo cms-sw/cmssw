e2e06099696d6eace52d1a933f00234807a22843
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2007-02-09 10:47:54 +0000

    DetId for a SC in EE needed for SR flags

---
+++ DataFormats/EcalDetId/interface/EcalScDetId.h
@@ -0,0 +1,80 @@
+// -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: t; tab-width: 8; -*-
+//$Id$
+
+#ifndef ECALSDDETID_H
+#define ECALSCDETID_H
+
+#include <ostream>
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
+
+
+/** \class EcalScDetId
+ *  Supercrystal identifier class for the ECAL endcap.
+ *  <P>Note: internal representation of ScDetId:
+ *  <CODE>
+ *  31              .               15              .              0
+ *  |-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-| 
+ *  |  det  |sudet|         0       |1|z|     ix      |     iy      |
+ *  +-------+-----+-----------------+-+-+-------------+-------------+
+ *  </CODE>
+ */
+
+class EcalScDetId : public DetId {
+ public:
+  /** Constructor of a null id */
+  EcalScDetId();
+  /** Constructor from a raw value */
+  EcalScDetId(uint32_t rawid);
+  /** Constructor from crystal ix,iy,iz (iz=+1/-1) 
+   or from sc,cr,iz */
+  EcalScDetId(int ix, int iy, int iz);  
+  /** Constructor from a generic cell id */
+  EcalScDetId(const DetId& id);
+  /// assignment operator
+  EcalScDetId& operator=(const DetId& id);
+
+  /// Gets the subdetector
+  EcalSubdetector subdet() const { return EcalSubdetector(subdetId()); }
+  /// Gets the z-side of the crystal (1/-1)
+  int zside() const { return (id_&0x4000)?(1):(-1); }
+  /// Gets the crystal ix
+  int ix() const { return (id_>>7)&0x7F; }
+  /// Gets the crystal iy
+  int iy() const { return id_&0x7F; }  
+  /// Gets the SuperCrystal number
+  int isc() const;
+  /// Gets the quadrant of the DetId
+  int iquadrant() const ;
+  /// Gets a compact index for arrays
+  int hashedIndex() const;
+
+  static const int IX_MIN=1;
+  static const int IY_MIN=1;
+  static const int IX_MAX=20;
+  static const int IY_MAX=20;
+  static const int ISC_MIN=1;
+  static const int ISC_MAX=316;
+
+ private:
+
+  //Functions adapted from similar B. Kennedy's code of EEDetId class to
+  //retrieve ix and iy from SC and Crystal number
+
+  static const int nCols = 10;
+  static const int nCrys = 5; /* Number of crystals per row in SC */
+  static const int QuadColLimits[nCols+1];
+  static const int iYoffset[nCols+1];
+  int ix(int iSC,int iCrys) const;
+  int iy(int iSC,int iCrys) const;
+  int ixQuadrantOne() const;
+  int iyQuadrantOne() const;
+  /// check if a valid index combination
+  bool validDetId(int i, int j, int iz) const;
+};
+
+
+std::ostream& operator<<(std::ostream& s,const EcalScDetId& id);
+
+
+#endif //ECALSCDETID not defined
---
+++ DataFormats/EcalDetId/src/EcalScDetId.cc
@@ -0,0 +1,138 @@
+#include "DataFormats/EcalDetId/interface/EcalScDetId.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include <iostream>
+const int EcalScDetId::QuadColLimits[EcalScDetId::nCols+1] = { 0, 8,17,27,36,45,54,62,70,76,79 };
+const int EcalScDetId::iYoffset[EcalScDetId::nCols+1]      = { 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+EcalScDetId::EcalScDetId() : DetId() {
+}
+EcalScDetId::EcalScDetId(uint32_t rawid) : DetId(rawid) {
+}
+EcalScDetId::EcalScDetId(int ix, int iy, int iz) : DetId(Ecal,EcalEndcap) 
+{
+  if(!validDetId(ix,iy,iz))
+    {
+      throw cms::Exception("InvalidDetId") << "EcalScDetId:  Cannot create object.  Indexes out of bounds.";
+    }
+  const int scBit = 1<<15; //bit set to 1 to distinguish from crystal id (EEDetId)
+  //                         and for a reasonale behaviour of DetId ccomparison operators.
+  id_|=(iy&0x7f)|((ix&0x7f)<<7)|((iz>0)?(1<<14):(0))|scBit;
+}
+  
+EcalScDetId::EcalScDetId(const DetId& gen) {
+  if (!gen.null() && (gen.det()!=Ecal || gen.subdetId()!=EcalEndcap)) {
+    throw cms::Exception("InvalidDetId"); 
+  }
+  id_=gen.rawId();
+}
+  
+EcalScDetId& EcalScDetId::operator=(const DetId& gen) {
+  if (!gen.null() && ( gen.det()!=Ecal || gen.subdetId()!=EcalEndcap )) {
+    throw cms::Exception("InvalidDetId"); 
+  }
+  id_=gen.rawId();
+  return *this;
+}
+  
+int EcalScDetId::hashedIndex() const {
+  return ((zside()>0)?(IX_MAX*IY_MAX):(0))+(iy()-1)*IX_MAX+(ix()-1);
+}
+
+int EcalScDetId::ixQuadrantOne() const
+{ 
+  int iQuadrant = iquadrant();
+  if ( iQuadrant == 1 || iQuadrant == 4)
+    return (ix() - 10);
+  else if ( iQuadrant == 2 || iQuadrant == 3)
+    return (11 - ix());
+  //Should never be reached
+  return -1;
+}
+
+int EcalScDetId::iyQuadrantOne() const
+{ 
+  int iQuadrant = iquadrant();
+  if ( iQuadrant == 1 || iQuadrant == 2)
+    return (iy() - 10);
+  else if ( iQuadrant == 3 || iQuadrant == 4)
+    return 11 - iy();
+  //Should never be reached
+  return -1;
+}
+
+int EcalScDetId::iquadrant() const {
+  if (ix()>10)
+    {
+      if(iy()>10)
+	return 1;
+      else
+	return 4;
+    }
+  else
+    {
+      if(iy()>10)
+	return 2;
+      else
+	return 3;
+    }
+  //Should never be reached
+  return -1;
+}  
+
+int EcalScDetId::isc() const 
+{
+  /*
+   *  Return SC number from (x,y) coordinates.
+   *
+   *  Copied from code originally written by B W Kennedy
+   */
+  
+  int iCol = ix();
+  int iRow = iy();
+  int nSCinQuadrant = QuadColLimits[nCols];
+  int iSC;
+  
+  if (iRow <= iYoffset[iCol]) 
+    return -1;
+  else 
+    iSC = QuadColLimits[iCol-1] + iRow - iYoffset[iCol];
+
+  if (iSC > QuadColLimits[iCol]) 
+    return -2;
+  
+  if (iSC>0) 
+      iSC += nSCinQuadrant*(iquadrant()-1);
+  
+  return iSC;
+}  
+
+bool EcalScDetId::validDetId(int crystal_ix, int crystal_iy, int iz) const {
+  static const char endcapMap[401] = {
+    "       XXXXXX       "
+    "    XXXXXXXXXXXX    "
+    "   XXXXXXXXXXXXXX   "
+    "  XXXXXXXXXXXXXXXX  "
+    " XXXXXXXXXXXXXXXXXX "
+    " XXXXXXXXXXXXXXXXXX "             //    Z
+    " XXXXXXXXXXXXXXXXXX "             //     x-----> X
+    "XXXXXXXXXXXXXXXXXXXX"             //     |
+    "XXXXXXXXX  XXXXXXXXX"             //     |
+    "XXXXXXXX    XXXXXXXX"//_          //     |
+    "XXXXXXXX    XXXXXXXX"             //     V Y
+    "XXXXXXXXX  XXXXXXXXX"
+    "XXXXXXXXXXXXXXXXXXXX"
+    " XXXXXXXXXXXXXXXXXX "
+    " XXXXXXXXXXXXXXXXXX "
+    " XXXXXXXXXXXXXXXXXX "
+    "  XXXXXXXXXXXXXXXX  "
+    "   XXXXXXXXXXXXXX   "
+    "    XXXXXXXXXXXX    "
+    "       XXXXXX       "};
+
+  return abs(iz)==1 && endcapMap[ix()-1+(iy()-1)*20]!=' ';
+}
+
+std::ostream& operator<<(std::ostream& s,const EcalScDetId& id) {
+  return s << "(EE iz " << ((id.zside()>0)?("+ "):("- ")) << " ix " << id.ix() << " , iy " << id.iy() << ')';
+}
