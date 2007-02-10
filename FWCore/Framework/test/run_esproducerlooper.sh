811adb43d0644a371e6be30e8871193f29ce903b
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-02-10 19:22:54 +0000

    tests for interaction between Looper and ESProducer

--- FWCore/Framework/test/run_esproducerlooper.sh
+++ FWCore/Framework/test/run_esproducerlooper.sh
@@ -4,3 +4,6 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 (cmsRun ${LOCAL_TEST_DIR}/test_esproducerlooper.cfg ) || die 'Failure using test_esproducerlooper.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_esproducerlooper_override.cfg ) || die 'Failure using test_esproducerlooper_override.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_esproducerlooper_prefer.cfg ) || die 'Failure using test_esproducerlooper_prefer.cfg' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_esproducerlooper_prefer_producer.cfg ) || die 'Failure using test_esproducerlooper_prefer_producer.cfg' $?
