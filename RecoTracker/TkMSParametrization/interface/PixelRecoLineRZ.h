8d1dc499463e19c9545076adf79b5830c84d59ed
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2007-02-10 20:34:28 +0000

    *** empty log message ***

--- RecoTracker/TkMSParametrization/interface/PixelRecoLineRZ.h
+++ RecoTracker/TkMSParametrization/interface/PixelRecoLineRZ.h
@@ -5,6 +5,7 @@
     and cotangent */
 
 #include "RecoTracker/TkMSParametrization/interface/PixelRecoPointRZ.h"
+#include "Geometry/Vector/interface/GlobalPoint.h"
 
 class PixelRecoLineRZ {
 public:
@@ -12,23 +13,33 @@ public:
   typedef PixelRecoPointRZ LineOrigin;
 
   PixelRecoLineRZ() { }
+
+  PixelRecoLineRZ(const GlobalPoint & p1, const GlobalPoint & p2) 
+    : theOrigin ( LineOrigin( p1.perp(), p1.z()) ),
+      theCotLine ( initCot(theOrigin.z()-p2.z(), theOrigin.r()-p2.perp()) ) 
+  { }
+
+
   PixelRecoLineRZ(const LineOrigin & aOrigin, float aCotLine)
     : theOrigin(aOrigin), theCotLine(aCotLine) { }
-  PixelRecoLineRZ(const LineOrigin & aOrigin, 
-                          const PixelRecoPointRZ & aPoint) 
+
+  PixelRecoLineRZ(const LineOrigin & aOrigin, const PixelRecoPointRZ & aPoint) 
     : theOrigin(aOrigin),
-      theCotLine( (aPoint.z()-aOrigin.z())/(aPoint.r()-aOrigin.r()) )
+      theCotLine( initCot( aPoint.z()-aOrigin.z(), aPoint.r()-aOrigin.r() ) ) 
       { } 
 
   float  cotLine() const {return theCotLine; }
   const LineOrigin & origin() const  { return theOrigin; }
-  
 
   float zAtR (float r) const 
     { return theOrigin.z()+(r-theOrigin.r())*theCotLine; }
   float rAtZ (float z) const 
     { return theOrigin.r()+(z-theOrigin.z())/theCotLine; }
 
+private:
+  float initCot (float dz, float dr) {
+    return (fabs(dr) > 1.e-4)  ? dz/dr : 99999.;
+  }
 private:
   LineOrigin theOrigin;
   float theCotLine;
