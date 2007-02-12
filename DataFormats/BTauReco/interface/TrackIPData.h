072771e85afe2b53b87da199801965c7482a5a23
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-02-12 00:50:08 +0000

    fixed DataFormats/GeometryVector in place of Geometry/Vector

--- DataFormats/BTauReco/interface/TrackIPData.h
+++ DataFormats/BTauReco/interface/TrackIPData.h
@@ -5,8 +5,7 @@
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/Math/interface/Vector3D.h"
 #include "DataFormats/Common/interface/Ref.h"
-#include "Geometry/Vector/interface/GlobalVector.h"
-
+#include "DataFormats/GeometryVector/interface/GlobalVector.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
 
 #include "DataFormats/BTauReco/interface/JetTracksAssociation.h"
