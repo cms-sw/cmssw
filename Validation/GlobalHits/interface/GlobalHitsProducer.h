e26fd31ad0eaa7015c7c7d31f59aa0231ad44781
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2007-02-12 19:41:23 +0000

    reflect change in Handle.h location

--- Validation/GlobalHits/interface/GlobalHitsProducer.h
+++ Validation/GlobalHits/interface/GlobalHitsProducer.h
@@ -6,7 +6,7 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "DataFormats/Common/interface/Provenance.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
