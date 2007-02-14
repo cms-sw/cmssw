39436d65c8a30484ddb4aa5c88c98685c5d0f513
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2007-02-14 09:54:15 +0000

    removed using directive

--- EventFilter/Utilities/interface/Css.h
+++ EventFilter/Utilities/interface/Css.h
@@ -5,7 +5,7 @@
 #include "xgi/include/xgi/Output.h"
 #include "xgi/include/xgi/exception/Exception.h"
 
-using namespace std;
+
 
 namespace evf{
 
@@ -15,47 +15,48 @@ namespace evf{
       void css(xgi::Input  *in,
 	       xgi::Output *out) throw (xgi::exception::Exception)
 	{
-	      out->getHTTPResponseHeader().addHeader("Content-Type", "text/css");
-
-	      *out << "body"                                   << endl;
-	      *out << "{"                                      << endl;
-	      *out << "background-color: white;"               << endl;
-	      *out << "}"                                      << endl;
-	      *out                                             << endl;
-	      *out << "table.modules"                          << endl;
-	      *out << "{"                                      << endl;
-	      *out << "font-family: Arial;"                    << endl;
-	      *out << "border: thin dotted;"                   << endl;
-	      *out << "}"                                      << endl;
-	      *out                                             << endl;
-	      *out << "table.modules colgroup"                 << endl;
-	      *out << "{"                                      << endl;
-	      *out << "width: 30%;"                            << endl;
-	      *out << "border: solid;"                         << endl;
-	      *out << "}"                                      << endl;
-	      *out                                             << endl;
-	      *out << "table.modules th"                       << endl;
-	      *out << "{"                                      << endl;
-	      *out << "color: white;"                          << endl;
-	      *out << "background-color: #63F;"                << endl;
-	      *out << "}"                                      << endl;
-
-	      *out << "table.states"                           << endl;
-	      *out << "{"                                      << endl;
-	      *out << "font-family: Arial;"                    << endl;
-	      *out << "border: thin dotted;"                   << endl;
-	      *out << "}"                                      << endl;
-	      *out                                             << endl;
-	      *out << "table.states th"                        << endl;
-	      *out << "{"                                      << endl;
-	      *out << "color: white;"                          << endl;
-	      *out << "background-color: #63F;"                << endl;
-	      *out << "}"                                      << endl;
-
-	      *out                                             << endl;
-
-
-
+	  using std::endl;
+	  out->getHTTPResponseHeader().addHeader("Content-Type", "text/css");
+	  
+	  *out << "body"                                   << endl;
+	  *out << "{"                                      << endl;
+	  *out << "background-color: white;"               << endl;
+	  *out << "}"                                      << endl;
+	  *out                                             << endl;
+	  *out << "table.modules"                          << endl;
+	  *out << "{"                                      << endl;
+	  *out << "font-family: Arial;"                    << endl;
+	  *out << "border: thin dotted;"                   << endl;
+	  *out << "}"                                      << endl;
+	  *out                                             << endl;
+	  *out << "table.modules colgroup"                 << endl;
+	  *out << "{"                                      << endl;
+	  *out << "width: 30%;"                            << endl;
+	  *out << "border: solid;"                         << endl;
+	  *out << "}"                                      << endl;
+	  *out                                             << endl;
+	  *out << "table.modules th"                       << endl;
+	  *out << "{"                                      << endl;
+	  *out << "color: white;"                          << endl;
+	  *out << "background-color: #63F;"                << endl;
+	  *out << "}"                                      << endl;
+	  
+	  *out << "table.states"                           << endl;
+	  *out << "{"                                      << endl;
+	  *out << "font-family: Arial;"                    << endl;
+	  *out << "border: thin dotted;"                   << endl;
+	  *out << "}"                                      << endl;
+	  *out                                             << endl;
+	  *out << "table.states th"                        << endl;
+	  *out << "{"                                      << endl;
+	  *out << "color: white;"                          << endl;
+	  *out << "background-color: #63F;"                << endl;
+	  *out << "}"                                      << endl;
+	  
+	  *out                                             << endl;
+	  
+	  
+	  
 	}
 
     private: 
