d5c2667ad9777b7e53e3a067a10b6b3dab55a594
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-02-14 23:44:20 +0000

    Calculate r9, r19 and e5x5 to store in Photon.  Read in ClusterShape association map in order to do this.  New method fillPhotonCollection to avoid duplication of code.

--- RecoEgamma/EgammaPhotonProducers/src/PhotonProducer.cc
+++ RecoEgamma/EgammaPhotonProducers/src/PhotonProducer.cc
@@ -5,14 +5,9 @@
 // Framework
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/Handle.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Utilities/interface/Exception.h"
 //
-#include "DataFormats/EgammaReco/interface/BasicCluster.h"
-#include "DataFormats/EgammaReco/interface/SuperCluster.h"
-#include "DataFormats/EgammaCandidates/interface/Photon.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 
 #include "RecoEgamma/EgammaPhotonProducers/interface/PhotonProducer.h"
@@ -29,6 +24,10 @@ PhotonProducer::PhotonProducer(const edm::ParameterSet& config) :
 
   scHybridBarrelCollection_     = conf_.getParameter<std::string>("scHybridBarrelCollection");
   scIslandEndcapCollection_     = conf_.getParameter<std::string>("scIslandEndcapCollection");
+  barrelClusterShapeMapProducer_   = conf_.getParameter<std::string>("barrelClusterShapeMapProducer");
+  barrelClusterShapeMapCollection_ = conf_.getParameter<std::string>("barrelClusterShapeMapCollection");
+  endcapClusterShapeMapProducer_   = conf_.getParameter<std::string>("endcapClusterShapeMapProducer");
+  endcapClusterShapeMapCollection_ = conf_.getParameter<std::string>("endcapClusterShapeMapCollection");
   vertexProducer_       = conf_.getParameter<std::string>("primaryVertexProducer");
   PhotonCollection_ = conf_.getParameter<std::string>("photonCollection");
 
@@ -72,6 +71,15 @@ void PhotonProducer::produce(edm::Event& theEvent, const edm::EventSetup& theEve
 
   reco::SuperClusterCollection scEndcapCollection = *(scEndcapHandle.product());
   edm::LogInfo("PhotonProducer") << " Accessing Endcap SC collection with size : " << scEndcapCollection.size()  << "\n";
+  
+  // Get ClusterShape association maps
+  edm::Handle<reco::BasicClusterShapeAssociationCollection> barrelClShpHandle;
+  theEvent.getByLabel(barrelClusterShapeMapProducer_, barrelClusterShapeMapCollection_, barrelClShpHandle);
+  const reco::BasicClusterShapeAssociationCollection& barrelClShpMap = *barrelClShpHandle;
+
+  edm::Handle<reco::BasicClusterShapeAssociationCollection> endcapClShpHandle;
+  theEvent.getByLabel(endcapClusterShapeMapProducer_, endcapClusterShapeMapCollection_, endcapClShpHandle);
+  const reco::BasicClusterShapeAssociationCollection& endcapClShpMap = *endcapClShpHandle;
 
   // Get the primary event vertex
   Handle<reco::VertexCollection> vertexHandle;
@@ -85,51 +93,59 @@ void PhotonProducer::produce(edm::Event& theEvent, const edm::EventSetup& theEve
 
   edm::LogInfo("PhotonProducer") << "Constructing Photon 4-vectors assuming primary vertex position: " << vtx << std::endl;
 
-  //  Loop over barrel SC and fill the  photon collection
   int iSC=0; // index in photon collection
-  int lSC=0; // local index on barrel
+  // Loop over barrel and endcap SC collections and fill the  photon collection
+  fillPhotonCollection(scBarrelHandle,barrelClShpMap,vtx,outputPhotonCollection,iSC);
+  fillPhotonCollection(scEndcapHandle,endcapClShpMap,vtx,outputPhotonCollection,iSC);
+
+  // put the product in the event
+  edm::LogInfo("PhotonProducer") << " Put in the event " << iSC << " Photon Candidates \n";
+  outputPhotonCollection_p->assign(outputPhotonCollection.begin(),outputPhotonCollection.end());
+  theEvent.put( outputPhotonCollection_p, PhotonCollection_);
+
+}
+
+void PhotonProducer::fillPhotonCollection(
+		   const edm::Handle<reco::SuperClusterCollection> & scHandle,
+		   const reco::BasicClusterShapeAssociationCollection& clshpMap,
+		   math::XYZPoint & vtx,
+		   reco::PhotonCollection & outputPhotonCollection, int iSC) {
+
+  reco::SuperClusterCollection scCollection = *(scHandle.product());
   reco::SuperClusterCollection::iterator aClus;
-  for(aClus = scBarrelCollection.begin(); aClus != scBarrelCollection.end(); aClus++) {
+  reco::BasicClusterShapeAssociationCollection::const_iterator seedShpItr;
 
-    // compute correctly the momentum vector of the photon from primary vertex and cluster position
-    math::XYZVector direction =aClus->position() - vtx;
+  std::cout << "h1" << std::endl;
+  int lSC=0; // reset local supercluster index
+  for(aClus = scCollection.begin(); aClus != scCollection.end(); aClus++) {
+
+    std::cout << "h2" << std::endl;
+    // compute R9=E3x3/ESC
+    seedShpItr = clshpMap.find(aClus->seed());
+    assert(seedShpItr != clshpMap.end());
+    const reco::ClusterShapeRef& seedShapeRef = seedShpItr->val;
+    double r9 = seedShapeRef->e3x3()/(aClus->rawEnergy()+aClus->preshowerEnergy());
+    double r19 = seedShapeRef->eMax()/seedShapeRef->e3x3();
+    double e5x5 = seedShapeRef->e5x5();
+
+    // compute position of ECAL shower
+    math::XYZPoint caloPosition;
+    caloPosition = aClus->position();
+
+    // compute momentum vector of photon from primary vertex and cluster position
+    math::XYZVector direction = caloPosition - vtx;
     math::XYZVector momentum = direction.unit() * aClus->energy();
     const reco::Particle::LorentzVector  p4(momentum.x(), momentum.y(), momentum.z(), aClus->energy() );
 
-    reco::Photon newCandidate(0, p4, vtx);
+    reco::Photon newCandidate(0, p4, r9, r19, e5x5, vtx);
 
     outputPhotonCollection.push_back(newCandidate);
-    reco::SuperClusterRef scRef(reco::SuperClusterRef(scBarrelHandle, lSC));
-    outputPhotonCollection[iSC].setSuperCluster(scRef);
-
-
-    lSC++;
-    iSC++;
-
-  }
-
-  //  Loop over Endcap SC and fill the  photon collection
-  lSC=0; // reset local index for endcap
-  for(aClus = scEndcapCollection.begin(); aClus != scEndcapCollection.end(); aClus++) {
-
-    math::XYZVector direction =aClus->position() - vtx;
-    math::XYZVector momentum = direction.unit() * aClus->energy();
-    const reco::Particle::LorentzVector  p4(momentum.x(), momentum.y(), momentum.z(), aClus->energy() );
-
-    reco::Photon newCandidate(0, p4, vtx);
-
-    outputPhotonCollection.push_back(newCandidate);
-    reco::SuperClusterRef scRef(reco::SuperClusterRef(scEndcapHandle, lSC));
+    reco::SuperClusterRef scRef(reco::SuperClusterRef(scHandle, lSC));
     outputPhotonCollection[iSC].setSuperCluster(scRef);
  
     iSC++;
     lSC++;
 
   }
-
-  // put the product in the event
-  edm::LogInfo("PhotonProducer") << " Put in the event " << iSC << " Photon Candidates \n";
-  outputPhotonCollection_p->assign(outputPhotonCollection.begin(),outputPhotonCollection.end());
-  theEvent.put( outputPhotonCollection_p, PhotonCollection_);
 
 }
