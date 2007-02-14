f9924865d91353f3c25ac69d78e9cd4e801ad63c
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2007-02-14 16:52:20 +0000

    A little bit of tuning for nuclear interactions

--- FastSimulation/MaterialEffects/src/MaterialEffects.cc
+++ FastSimulation/MaterialEffects/src/MaterialEffects.cc
@@ -218,7 +218,7 @@ void MaterialEffects::interact(FSimEvent& mySimEvent,
     if ( !layer.sensitive() ) { 
       if ( layer.layerNumber() == 107 ) { 
 	double eta = myTrack.vertex().eta();
-	factor = eta > 2.2 ? 1.0 +(eta-2.2)*3.0 : 1.0;
+	factor = eta > 2.2 ? 1.0 +(eta-2.2)*2.4 : 1.0;
       }	else if ( layer.layerNumber() == 113 ) { 
 	double zed = fabs(myTrack.vertex().z());
 	factor = zed > 116. ? 0.6 : 1.4;
