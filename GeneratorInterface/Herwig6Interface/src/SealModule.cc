91ff6c238885cd509a28535973ac01dd2b45d613
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2007-02-14 15:29:53 +0000

    bug-fix in SealModule

--- GeneratorInterface/Herwig6Interface/src/SealModule.cc
+++ GeneratorInterface/Herwig6Interface/src/SealModule.cc
@@ -3,7 +3,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "GeneratorInterface/Herwig6Interface/interface/Herwig6Source.h"
 
-  using edm::Herwig6Source;
+using edm::Herwig6Source;
 
-  DEFINE_SEAL_MODULE();
-  DEFINE_ANOTHER_FWK_INPUT_SOURCE(Herwig6Source)
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_INPUT_SOURCE(Herwig6Source);
+
