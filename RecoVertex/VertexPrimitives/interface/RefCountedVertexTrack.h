59b925d1834420ec84df005f0982ea45f5a25b0b
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-02-15 16:44:54 +0000

    Added RefCountedVertexTrack comparison predicate

--- RecoVertex/VertexPrimitives/interface/RefCountedVertexTrack.h
+++ RecoVertex/VertexPrimitives/interface/RefCountedVertexTrack.h
@@ -6,4 +6,13 @@
 
 typedef ReferenceCountingPointer<VertexTrack> RefCountedVertexTrack;
 
+class VertexTrackEqual {
+  public:
+    VertexTrackEqual( const RefCountedVertexTrack & t) : track_( t ) { }
+    bool operator()( const RefCountedVertexTrack & t ) const { return t->operator==(*track_);}
+  private:
+    const RefCountedVertexTrack & track_;
+};
+
+
 #endif
