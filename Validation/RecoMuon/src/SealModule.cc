f83188a6e960fce9b255a3152a4118542bf1fcbd
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2007-02-16 15:24:11 +0000

    added RecoMuonValidator

--- Validation/RecoMuon/src/SealModule.cc
+++ Validation/RecoMuon/src/SealModule.cc
@@ -4,7 +4,9 @@
 
 #include "Validation/RecoMuon/src/MuonTrackAnalyzer.h"
 #include "Validation/RecoMuon/src/MuonTrackResidualAnalyzer.h"
+#include "Validation/RecoMuon/src/RecoMuonValidator.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MuonTrackAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MuonTrackResidualAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(RecoMuonValidator);
