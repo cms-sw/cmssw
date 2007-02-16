2ae3e9addad47698eab34ea3d5236396994b7c59
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-02-16 08:29:56 +0000

    fix nth bug tag vs tau

--- CondCore/BTauPlugins/src/plugin.cc
+++ CondCore/BTauPlugins/src/plugin.cc
@@ -1,6 +1,6 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 
-#include "CondFormats/BTagObjects/interface/TrackProbabilityCalibration.h"
+#include "CondFormats/BTauObjects/interface/TrackProbabilityCalibration.h"
 #include "CondFormats/DataRecord/interface/BTagTrackProbability2DRcd.h"
 #include "CondFormats/DataRecord/interface/BTagTrackProbability3DRcd.h"
 
