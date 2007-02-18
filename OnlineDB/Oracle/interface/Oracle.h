ffe8eaf171be51d80336e75b63c1e0a9be98fa6e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-02-18 23:10:09 +0000

    Protect against #defines in occi.h

---
+++ OnlineDB/Oracle/interface/Oracle.h
@@ -0,0 +1,12 @@
+#ifndef OnlineDBOracle_Oracle_h
+#define OnlineDBOracle_Oracle_h
+
+// Isolate the dependency on Oracle to this header.
+#include "occi.h"
+
+// Unddefine any dangerous defines in occi.h.
+#ifdef CONST
+#undef CONST
+#endif
+
+#endif
