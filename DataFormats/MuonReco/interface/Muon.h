edccc03fd425aceff72373865dd7751eeeebb0d3
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2007-02-19 18:49:17 +0000

    removed the SuperClusterRef

--- DataFormats/MuonReco/interface/Muon.h
+++ DataFormats/MuonReco/interface/Muon.h
@@ -10,7 +10,7 @@
  *
  * \author Luca Lista, INFN
  *
- * \version $Id: Muon.h,v 1.18 2006/05/02 10:19:13 llista Exp $
+ * \version $Id: Muon.h,v 1.19 2007/01/17 10:23:30 llista Exp $
  *
  */
 #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
@@ -28,16 +28,12 @@ namespace reco {
     TrackRef standAloneMuon() const { return standAloneMuon_; }
     /// reference to Track reconstructed in both tracked and muon detector
     TrackRef combinedMuon() const { return combinedMuon_; }
-    /// reference to associated Ecal SuperCluster
-    SuperClusterRef superCluster() const { return superCluster_; }
     /// set reference to Track
     void setTrack( const TrackRef & t ) { track_ = t; }
     /// set reference to Track
     void setStandAlone( const TrackRef & t ) { standAloneMuon_ = t; }
     /// set reference to Track
     void setCombined( const TrackRef & t ) { combinedMuon_ = t; }
-    /// set reference to associated Ecal SuperCluster
-    void setSuperCluster( const SuperClusterRef & ref ) { superCluster_ = ref; }
     /// PDG identifier
     virtual int pdgId() const { return - 13 * charge(); }
 
@@ -50,8 +46,6 @@ namespace reco {
     TrackRef standAloneMuon_;
     /// reference to Track reconstructed in both tracked and muon detector
     TrackRef combinedMuon_;
-    /// reference to associated Ecal SuperCluster
-    SuperClusterRef superCluster_;
 };
 
 }
