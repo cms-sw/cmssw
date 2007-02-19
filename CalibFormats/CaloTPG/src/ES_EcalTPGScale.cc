acd9fc488e55d4d8ae0f09c976efb41296ee60d3
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-02-19 16:24:21 +0000

    Added HcalTPGScale and EcalTPGScale abstract interfaces

---
+++ CalibFormats/CaloTPG/src/ES_EcalTPGScale.cc
@@ -0,0 +1,5 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "CalibFormats/CaloTPG/interface/EcalTPGScale.h"
+
+
+EVENTSETUP_DATA_REG(EcalTPGScale);
---
+++ CalibFormats/CaloTPG/src/ES_HcalTPGScale.cc
@@ -0,0 +1,4 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "CalibFormats/CaloTPG/interface/HcalTPGScale.h"
+
+EVENTSETUP_DATA_REG(HcalTPGScale);
