18bc52b3ee554265b004d9faee362629b8841e2c
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2007-02-19 12:03:43 +0000

    cleanup

--- TrackingTools/TrackAssociator/src/MuonSegmentMatch.cc
+++ TrackingTools/TrackAssociator/src/MuonSegmentMatch.cc
@@ -2,19 +2,3 @@
 #include "DataFormats/MuonDetId/interface/DTChamberId.h"
 #include "DataFormats/MuonDetId/interface/CSCDetId.h"
 #include "DataFormats/MuonDetId/interface/RPCDetId.h"
-
-double MuonSegmentMatch::pullX() const {
-   double deltaX, errorX;
-   deltaX = segmentLocalPosition.X()-trajectoryLocalPosition.X();
-   errorX = sqrt(segmentLocalErrorXX+trajectoryLocalErrorXX);
-
-   return deltaX/errorX;
-}
-
-double MuonSegmentMatch::pullY() const {
-   double deltaY, errorY;
-   deltaY = segmentLocalPosition.Y()-trajectoryLocalPosition.Y();
-   errorY = sqrt(segmentLocalErrorYY+trajectoryLocalErrorYY);
-
-   return deltaY/errorY;
-}
