6890a8262773a4cc00a177a45e27a98d84477f40
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-02-19 15:34:55 +0000

    Add compress code

--- CalibFormats/HcalObjects/interface/HcalTPGCoder.h
+++ CalibFormats/HcalObjects/interface/HcalTPGCoder.h
@@ -4,6 +4,7 @@
 #include "CalibFormats/CaloObjects/interface/IntegerCaloSamples.h"
 #include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HFDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HcalTriggerPrimitiveDigi.h"
 
 // forward declaration of EventSetup is all that is needed here
 namespace edm {
@@ -13,12 +14,13 @@ namespace edm {
 /** \class HcalTPGCoder
   *  
   * Converts ADC to linear E or ET for use in the TPG path
+  * Also compresses linear scale for transmission to RCT
   * 
   * Note : whether the coder produces E or ET is determined by the specific
   * implementation of the coder.
   *
-  * $Date: 2006/09/14 16:58:37 $
-  * $Revision: 1.3 $
+  * $Date: 2006/09/14 20:01:37 $
+  * $Revision: 1.4 $
   * \author J. Mans - Minnesota
   */
 class HcalTPGCoder {
@@ -27,6 +29,7 @@ public:
   virtual void releaseConditions() const { }
   virtual void adc2Linear(const HBHEDataFrame& df, IntegerCaloSamples& ics) const = 0;
   virtual void adc2Linear(const HFDataFrame& df, IntegerCaloSamples& ics) const = 0;
+  virtual void compress(const IntegerCaloSamples& ics, const std::vector<bool>& featureBits, HcalTriggerPrimitiveDigi& tp) const = 0;
 };
 
 #endif
