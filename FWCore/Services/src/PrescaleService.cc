5c72c489a3a3722c32e6da8105a3c9a261388c0b
youngman <sha1-70f538cceae4828b7cf23a74f22a96dcfa70321a@cern.ch>
2007-02-19 19:00:13 +0000

    Test version

---
+++ FWCore/Services/src/PrescaleService.cc
@@ -0,0 +1,173 @@
+
+
+#include "FWCore/Services/src/PrescaleService.h"
+#include "DataFormats/Common/interface/ModuleDescription.h"
+#include "DataFormats/Common/interface/EventID.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+namespace edm {
+  namespace service {
+
+    PrescaleService::PrescaleService(const ParameterSet& iPS, ActivityRegistry&iReg)
+    {
+      
+      LogDebug("PrescaleService") << "PrescaleService::PrescaleService";
+
+      iReg.watchPostBeginJob(this,&PrescaleService::postBeginJob);
+      iReg.watchPostEndJob(this,&PrescaleService::postEndJob);
+      
+      iReg.watchPreProcessEvent(this,&PrescaleService::preEventProcessing);
+      iReg.watchPostProcessEvent(this,&PrescaleService::postEventProcessing);
+
+      iReg.watchPreModule(this,&PrescaleService::preModule);
+      iReg.watchPostModule(this,&PrescaleService::postModule);
+
+      count_ = 0;
+      fu_ = 0;
+      lsnr_ = -1;
+    }
+
+    PrescaleService::~PrescaleService()
+    {
+    }
+
+    void PrescaleService::postBeginJob()
+    {
+    }
+
+    void PrescaleService::postEndJob()
+    {
+    }
+
+    void PrescaleService::preEventProcessing(const edm::EventID& iID,
+					       const edm::Timestamp& iTime)
+    {
+      //      std::cout << "!!! PrescaleService::preEventProcessing count " << count_ << std::endl;
+      if (count_%100 == 0) {
+	if (fu_ != 0) {
+	  //	  std::cout << "!!! get trigger report" << std::endl;
+	  fu_->getTriggerReport(tr_);
+	  lsnr_ += 1;
+	  //	  std::cout << "!!! trigger report done" << std::endl;
+
+	  ostringstream oss;
+	  string ARRAY_LEN = "_";
+	  string SEPARATOR = " ";
+	  // Lumi section number
+	  oss << lsnr_ << ":";
+	  //TriggerReport::eventSummary
+	  oss << tr_.eventSummary.totalEvents << SEPARATOR
+	      << tr_.eventSummary.totalEventsPassed << SEPARATOR
+	      << tr_.eventSummary.totalEventsFailed << SEPARATOR;
+	  //TriggerReport::trigPathSummaries
+	  oss << ARRAY_LEN << tr_.trigPathSummaries.size() << SEPARATOR;
+	  for(unsigned int i=0; i<tr_.trigPathSummaries.size(); i++)
+	    {
+	      oss << tr_.trigPathSummaries[i].bitPosition << SEPARATOR
+		  << tr_.trigPathSummaries[i].timesRun << SEPARATOR
+		  << tr_.trigPathSummaries[i].timesPassed << SEPARATOR
+		  << tr_.trigPathSummaries[i].timesFailed << SEPARATOR
+		  << tr_.trigPathSummaries[i].timesExcept << SEPARATOR
+		  << tr_.trigPathSummaries[i].name << SEPARATOR;
+	      //TriggerReport::trigPathSummaries::moduleInPathSummaries
+	      oss << ARRAY_LEN << tr_.trigPathSummaries[i].moduleInPathSummaries.size() << SEPARATOR;
+	      for(unsigned int j=0; j<tr_.trigPathSummaries[i].moduleInPathSummaries.size(); j++)
+		{
+		  oss << tr_.trigPathSummaries[i].moduleInPathSummaries[j].timesVisited << SEPARATOR
+		      << tr_.trigPathSummaries[i].moduleInPathSummaries[j].timesPassed << SEPARATOR
+		      << tr_.trigPathSummaries[i].moduleInPathSummaries[j].timesFailed << SEPARATOR
+		      << tr_.trigPathSummaries[i].moduleInPathSummaries[j].timesExcept << SEPARATOR
+		      << tr_.trigPathSummaries[i].moduleInPathSummaries[j].moduleLabel << SEPARATOR;
+		}
+	    }
+
+	  //TriggerReport::endPathSummaries
+	  oss << ARRAY_LEN << tr_.endPathSummaries.size() << SEPARATOR;
+	  for(unsigned int i=0; i<tr_.endPathSummaries.size(); i++)
+	    {
+	      oss << tr_.endPathSummaries[i].bitPosition << SEPARATOR
+		  << tr_.endPathSummaries[i].timesRun << SEPARATOR
+		  << tr_.endPathSummaries[i].timesPassed << SEPARATOR
+		  << tr_.endPathSummaries[i].timesFailed << SEPARATOR
+		  << tr_.endPathSummaries[i].timesExcept << SEPARATOR
+		  << tr_.endPathSummaries[i].name << SEPARATOR;
+	      //TriggerReport::endPathSummaries::moduleInPathSummaries
+	      oss << ARRAY_LEN << tr_.endPathSummaries[i].moduleInPathSummaries.size() << SEPARATOR;
+	      for(unsigned int j=0; j<tr_.endPathSummaries[i].moduleInPathSummaries.size(); j++)
+		{
+		  oss << tr_.endPathSummaries[i].moduleInPathSummaries[j].timesVisited << SEPARATOR
+		      << tr_.endPathSummaries[i].moduleInPathSummaries[j].timesPassed << SEPARATOR
+		      << tr_.endPathSummaries[i].moduleInPathSummaries[j].timesFailed << SEPARATOR
+		      << tr_.endPathSummaries[i].moduleInPathSummaries[j].timesExcept << SEPARATOR
+		      << tr_.endPathSummaries[i].moduleInPathSummaries[j].moduleLabel << SEPARATOR;
+		}
+	    }
+
+	  //TriggerReport::workerSummaries
+	  oss << ARRAY_LEN << tr_.workerSummaries.size() << SEPARATOR;
+	  for(unsigned int i=0; i<tr_.workerSummaries.size(); i++)
+	    {
+	      oss << tr_.workerSummaries[i].timesVisited << SEPARATOR
+		  << tr_.workerSummaries[i].timesRun     << SEPARATOR
+		  << tr_.workerSummaries[i].timesPassed  << SEPARATOR
+		  << tr_.workerSummaries[i].timesFailed  << SEPARATOR
+		  << tr_.workerSummaries[i].timesExcept  << SEPARATOR
+		  << tr_.workerSummaries[i].moduleLabel  << SEPARATOR;
+	    }
+
+	  trs_ = oss.str();
+	  //	  std::cout << "!!! trigger string " << trs_ << std::endl;
+	}
+      }
+    }
+
+    void PrescaleService::postEventProcessing(const Event& e,
+						const EventSetup&)
+    {
+	++count_;
+    }
+
+    void PrescaleService::preModule(const ModuleDescription& md)
+    {
+    }
+
+    void PrescaleService::postModule(const ModuleDescription& md)
+    {
+    }
+
+    unsigned int PrescaleService::getPrescale(unsigned int ls, string module)
+    {
+      return store.get(ls, module);
+    }
+
+    int PrescaleService::putPrescale(string s)
+    {
+      store.add(s);
+      return store.size();
+    }
+
+    int PrescaleService::sizePrescale()
+    {
+      return store.size();
+    }
+
+    void PrescaleService::putHandle(edm::EventProcessor *proc_)
+    {
+      fu_ = proc_;
+    }
+
+    string PrescaleService::getTriggerCounters()
+    {
+      //      std::cout << "PrescaleService::getTriggerCounters lsnr_ " << lsnr_ << std::endl;
+      return trs_;
+    }
+  }
+}
---
+++ FWCore/Services/src/PrescaleService.h
@@ -0,0 +1,201 @@
+#ifndef Services_PrescaleService_h
+#define Services_PrescaleService_h
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/ActivityRegistry.h"
+#include "DataFormats/Common/interface/EventID.h"
+
+#include "FWCore/Framework/interface/EventProcessor.h"
+#include "FWCore/Framework/interface/TriggerReport.h"
+
+#include "boost/thread/mutex.hpp"
+
+using namespace std;
+
+#include <vector>
+#include <iostream>
+
+
+namespace edm {
+  namespace service {
+
+    // cache structure used to store prescalers
+    struct element {
+      string path;
+      string module;
+      unsigned int value;
+    };
+    typedef element element;
+
+    struct cache {
+      unsigned int ls;
+      unsigned int touch;
+      vector<element> modules;
+    };
+    typedef cache cache;
+
+    // cache class
+    class Cache {
+      boost::mutex mutex;
+      vector<cache> members;
+      unsigned int updcnt;
+      unsigned int badcnt;
+      unsigned int baddec;
+      unsigned int getcnt;
+      unsigned int getbad;
+      int lastls;
+
+    public:
+      Cache() { 
+	updcnt = 0;
+	badcnt = 0;
+	baddec = 0;
+	getcnt = 0;
+	getbad = 0;
+        lastls = 0;
+      }
+
+      // add complete member to cache
+      // input string format = "LS# path module prescaler ... path module prescaler" 
+      void add(const string s) {
+        vector<string> tokens;
+        string delims = " ";
+
+        // tokenize everything
+	string::size_type last = s.find_first_not_of(delims, 0);
+	string::size_type posn = s.find_first_of(delims, last);
+        while (string::npos != posn || string::npos != last) {
+	  tokens.push_back(s.substr(last, posn - last));
+	  last = s.find_first_not_of(delims, posn);
+	  posn = s.find_first_of(delims, last);
+        }
+
+	// lock to update counters and cache
+	boost::mutex::scoped_lock scoped_lock(mutex);
+
+        // validate tokens
+        int value;
+        if (tokens.size() < 4 || (tokens.size()%3) != 1) badcnt++;
+        for (unsigned int i=0; i<tokens.size(); i += 3) {
+	  if(sscanf(tokens[i].c_str(), "%d", &value) != 1) baddec++;
+        }
+
+        // cache tokens
+        int ls;
+	updcnt++;
+        sscanf(tokens[0].c_str(), "%d", &ls);
+        for (unsigned int i=1; i<tokens.size(); i += 3) {
+	  sscanf(tokens[i+2].c_str(), "%d", &value);
+	  add(ls, tokens[i], tokens[i+1], value);
+        }
+	lastls = ls;
+
+      }
+
+      // add single element to cache member - should not lock the mutex
+      void add(unsigned int ls, string path, string module, unsigned int value) {
+	vector<cache>::iterator p;
+	for(p=members.begin(); p != members.end(); p++) {
+	  if (p->ls == ls) {
+	    element e;
+	    e.path = path;
+	    e.module = module;
+	    e.value = value;
+	    p->modules.push_back(e);
+	    return;
+	  }
+	}
+
+	cache c;
+	c.ls = ls;
+	c.touch = 0;
+	element e;
+	e.path = path;
+        e.module = module;
+        e.value = value;
+	c.modules.push_back(e);
+	members.push_back(c);
+	for(p=members.begin(); p != members.end(); p++) p->touch += 1;
+	if (members.size() > 5) {
+	  for(p=members.begin(); p != members.end(); p++) {
+	    if (p->touch > 5) {
+	      members.erase(p);
+	    }
+	  }
+	}
+      }
+
+      // get the prescale value associated with LS# and module name
+      int get(unsigned int ls, string module) {
+	boost::mutex::scoped_lock scoped_lock(mutex);
+
+	vector<cache>::iterator p;
+	for(p=members.begin(); p != members.end(); p++) {
+	  if (ls == p->ls) {
+	    vector<element>::iterator q;
+	    for(q=p->modules.begin(); q != p->modules.end(); q++) {
+	      if (module == q->module) return q->value;
+	    }
+	    break;
+	  }
+	}
+	return lastls;
+      }
+
+      // prints cache status to stdout
+      void show() {
+	boost::mutex::scoped_lock scoped_lock(mutex);
+
+	vector<cache>::iterator p;
+	for(p=members.begin(); p != members.end(); p++) {
+	  cout << " member " << p->ls;
+	  vector<element>::iterator q;
+	  for(q=p->modules.begin(); q != p->modules.end(); q++) {
+	    cout << " p " << q->path << " m " << q->module << " v " << q->value;
+	  }
+	  cout << endl;
+	}
+      }
+
+      // return number of members in the cache
+      int size() {
+        return members.size();
+      }
+
+    };
+
+    class PrescaleService
+    {
+      edm::EventID curr_event_;
+      int count_;
+      Cache store;
+      edm::EventProcessor *fu_;
+      edm::TriggerReport tr_;
+      string trs_;
+      int lsnr_;
+
+    public:
+      PrescaleService(const ParameterSet&,ActivityRegistry&);
+      ~PrescaleService();
+      
+      void postBeginJob();
+      void postEndJob();
+      
+      void preEventProcessing(const edm::EventID&, const edm::Timestamp&);
+      void postEventProcessing(const Event&, const EventSetup&);
+      
+      void preModule(const ModuleDescription&);
+      void postModule(const ModuleDescription&);
+
+      unsigned int getPrescale(unsigned int ls, string module);
+      int putPrescale(string s);
+      int sizePrescale();
+      void putHandle(edm::EventProcessor *proc_);
+      string getTriggerCounters();
+	
+    };
+  }
+}
+
+
+#endif
