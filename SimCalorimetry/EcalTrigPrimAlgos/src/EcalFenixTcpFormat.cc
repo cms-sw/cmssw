e7affdc88ab3afe32ed6572e6de9c583ecec47b7
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2007-02-20 13:42:40 +0000

    correct Tcp formatted output

--- SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormat.cc
+++ SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormat.cc
@@ -11,17 +11,20 @@ EcalFenixTcpFormat::~EcalFenixTcpFormat() {
 
  
 void EcalFenixTcpFormat::process(std::vector<int> &Et, std::vector<int> &fgvb, 
-std::vector<EcalTriggerPrimitiveSample> & out,
-std::vector<EcalTriggerPrimitiveSample> & out2){
+				 std::vector<EcalTriggerPrimitiveSample> & out,
+				 std::vector<EcalTriggerPrimitiveSample> & out2){
   // put TP-s in the output
   // on request also in TcpFormat    
   for (unsigned int i=0; i<Et.size();++i) {
     Et[i]=Et[i]>>2;
-    if (Et[i]>0xfff) Et[i]=0xfff ;
-    if (tcpFormat_)  out2.push_back(EcalTriggerPrimitiveSample( Et[i],fgvb[i],0)); 
+    if (Et[i]>0x3ff) Et[i]=0x3ff ;
     int lut_out = lut_[Et[i]] ;
-    Et[i] = lut_out & 0xff ;
     int ttFlag = (lut_out & 0x700) >> 8 ;
+    if (tcpFormat_)  {
+      int data = ((ttFlag&0x7)<<11) | ((fgvb[i] & 0x1)<<10) |  (Et [i] & 0x3ff) ;
+      out2.push_back(EcalTriggerPrimitiveSample(data));
+    }
+    Et[i] = lut_out & 0xff ;
     out.push_back(EcalTriggerPrimitiveSample( Et[i],fgvb[i],ttFlag)); 
   }
 }
