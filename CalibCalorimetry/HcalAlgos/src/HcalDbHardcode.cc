e42538547f316c359c42a4b1b2deb7258095dafd
Michal Szleper <sha1-8dc5ee44d1f0d27d89108086e7cf38e4a70bd448@cern.ch>
2007-02-20 00:25:36 +0000

    Use realistic constants (HF mainly)

--- CalibCalorimetry/HcalAlgos/src/HcalDbHardcode.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalDbHardcode.cc
@@ -10,9 +10,8 @@
 
 
 HcalPedestal HcalDbHardcode::makePedestal (HcalDetId fId, bool fSmear) {
-  HcalGain gain = HcalDbHardcode::makeGain (fId, fSmear);
   HcalPedestalWidth width = makePedestalWidth (fId);
-  float value0 = fId.subdet () == HcalForward ? 5. : 0.75; // GeV(!)
+  float value0 = fId.subdet () == HcalForward ? 11. : 4.;  // fC
   float value [4] = {value0, value0, value0, value0};
   if (fSmear) {
     for (int i = 0; i < 4; i++) {
@@ -21,20 +20,19 @@ HcalPedestal HcalDbHardcode::makePedestal (HcalDetId fId, bool fSmear) {
     }
   }
   HcalPedestal result (fId.rawId (), 
-		       value[0] / gain.getValue (0),
-		       value[1] / gain.getValue (1),
-		       value[2] / gain.getValue (2),
-		       value[3] / gain.getValue (3)
+		       value[0], value[1], value[2], value[3]
 		       );
   return result;
 }
 
 HcalPedestalWidth HcalDbHardcode::makePedestalWidth (HcalDetId fId) {
-  HcalGain gain = HcalDbHardcode::makeGain (fId);
-  float value = fId.subdet () == HcalForward ? 1. : 0.1;
+  float value = 0;
+  if (fId.subdet() == HcalBarrel || fId.subdet() == HcalOuter) value = 0.7;
+  else if (fId.subdet() == HcalEndcap) value = 0.9;
+  else if (fId.subdet() == HcalForward) value = 2.5;  // everything in fC
   HcalPedestalWidth result (fId.rawId ());
   for (int i = 0; i < 4; i++) {
-    double width = value / gain.getValue (i);
+    double width = value;
     for (int j = 0; j <= i; j++) {
       result.setSigma (i, j, i == j ? width * width : 0);
     }
@@ -44,7 +42,12 @@ HcalPedestalWidth HcalDbHardcode::makePedestalWidth (HcalDetId fId) {
 
 HcalGain HcalDbHardcode::makeGain (HcalDetId fId, bool fSmear) {
   HcalGainWidth width = makeGainWidth (fId);
-  float value0 = fId.subdet () == HcalForward ? 0.48 : 0.177; // GeV/fC
+  float value0 = 0;
+  if (fId.subdet() != HcalForward) value0 = 0.177;  // GeV/fC
+  else {
+    if (fId.depth() == 1) value0 = 0.2146;
+    else if (fId.depth() == 2) value0 = 0.3375;
+  }
   float value [4] = {value0, value0, value0, value0};
   if (fSmear) for (int i = 0; i < 4; i++) value [i] = RandGauss::shoot (value0, width.getValue (i)); 
   HcalGain result (fId.rawId (), value[0], value[1], value[2], value[3]);
@@ -60,7 +63,7 @@ HcalGainWidth HcalDbHardcode::makeGainWidth (HcalDetId fId) {
 HcalQIECoder HcalDbHardcode::makeQIECoder (HcalDetId fId) {
   HcalQIECoder result (fId.rawId ());
   float offset = 0;
-  float slope = fId.subdet () == HcalForward ? 0.36 : 1.;
+  float slope = fId.subdet () == HcalForward ? 0.36 : 0.92;  // ADC/fC
   for (unsigned range = 0; range < 4; range++) {
     for (unsigned capid = 0; capid < 4; capid++) {
       result.setOffset (capid, range, offset);
