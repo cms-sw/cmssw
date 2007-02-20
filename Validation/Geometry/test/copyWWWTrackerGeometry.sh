704d0988b84e0bd4e25cbbadb9fe696d5c9c2455
riccardo <sha1-a8fe582e28bc10906e7eb2fd4c1098a4a0c0276f@cern.ch>
2007-02-20 16:40:56 +0000

    copy the plots into web (Validation Suite)

---
+++ Validation/Geometry/test/copyWWWTrackerGeometry.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+export RELEASE=$CMSSW_VERSION
+
+#export wwwDir=/afs/cern.ch/cms/cpt/Software/html/General/Validation/SVSuite/TrackerBTau/$RELEASE/
+
+export wwwDir=./prova
+
+if [ ! -d $wwwDir ]; then
+    echo Creating directory $wwwDir
+    mkdir $wwwDir
+fi
+
+
+export geomDir=$dir/TrackerGeometry
+
+if [ ! -d $geomDir ]; then
+    echo Creating directory $geomDir
+    mkdir $geomDir
+fi
+
+# Material Budget
+export mbDir=$geomDir/MaterialBudget
+# Geometry: Positioning and Numbering Scheme
+export nsDir=$geomDir/Geometry
+
+# make Material Budget directories
+mkdir $mbDir
+mkdir $mbDir/Comparison
+mkdir $mbDir/Comparison/eps
+mkdir $mbDir/Comparison/gif
+mkdir $mbDir/Plots
+mkdir $mbDir/Plots/eps
+mkdir $mbDir/Plots/gif
+
+# make Geometry
+mkdir $nsDir
+
+
+# move Material Budget plots
+mv Images/*Comparison*.eps $mbDir/Comparison/eps/.
+mv Images/*Comparison*.gif $mbDir/Comparison/gif/.
+mv Images/*.eps            $mbDir/Plots/eps/.
+mv Images/*.gif            $mbDir/Plots/gif/.
+
+# move geometry diff files
+mv diff_info.temp $nsDir/ModulePositioning.diff
+mv diff_num.temp  $nsDir/ModuleNumbering.diff
