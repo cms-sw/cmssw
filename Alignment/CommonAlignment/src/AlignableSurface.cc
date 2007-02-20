5e4d66c9eeb8614ebec55aca65681e90fe1b822d
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-02-20 22:27:26 +0000

    Missed adding this file earlier.

---
+++ Alignment/CommonAlignment/src/AlignableSurface.cc
@@ -0,0 +1,28 @@
+#include "Alignment/CommonAlignment/interface/AlignableSurface.h"
+
+AlignableSurface::AlignableSurface(const PositionType& pos,
+				   const RotationType& rot):
+  GloballyPositioned<float>(pos, rot),
+  theWidth(0.),
+  theLength(0.)
+{
+}
+
+std::vector<AlignableSurface::GlobalPoint> AlignableSurface::toGlobal(const std::vector<LocalPoint>& localPoints) const
+{
+  std::vector<GlobalPoint> globalPoints;
+
+  globalPoints.reserve( localPoints.size() );
+
+  globalPoints.push_back( toGlobal(localPoints[0]) );
+  globalPoints.push_back( toGlobal(localPoints[1]) );
+  globalPoints.push_back( toGlobal(localPoints[2]) );
+  globalPoints.push_back( toGlobal(localPoints[3]) );
+  globalPoints.push_back( toGlobal(localPoints[4]) );
+  globalPoints.push_back( toGlobal(localPoints[5]) );
+  globalPoints.push_back( toGlobal(localPoints[6]) );
+  globalPoints.push_back( toGlobal(localPoints[7]) );
+  globalPoints.push_back( toGlobal(localPoints[8]) );
+
+  return globalPoints;
+}
