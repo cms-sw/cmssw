eeabfdb71b2dbce4cd31cc97f61002927121581f
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2007-02-21 09:48:16 +0000

    *** empty log message ***

--- Validation/RecoTau/src/SealModule.cc
+++ Validation/RecoTau/src/SealModule.cc
@@ -1,7 +1,11 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoTau/interface/TauTagVal.h"
+#include "Validation/RecoTau/interface/TauTagVal_BKG.h"
+#include "Validation/RecoTau/interface/TauTagVal_EMIso.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( TauTagVal );
+DEFINE_ANOTHER_FWK_MODULE( TauTagVal_BKG );
+DEFINE_ANOTHER_FWK_MODULE( TauTagVal_EMIso );
 
