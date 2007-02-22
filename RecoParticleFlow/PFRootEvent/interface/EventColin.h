38e602b1d967fafc4eec24b790fd6f0976b39795
Alexandre Zabi <sha1-317a4bf0815cf2e6e714c0614a0f72cd31dbdecf@cern.ch>
2007-02-22 13:58:16 +0000

    Adding jets class

--- RecoParticleFlow/PFRootEvent/interface/EventColin.h
+++ RecoParticleFlow/PFRootEvent/interface/EventColin.h
@@ -13,6 +13,9 @@ class EventColin {
     particles_.clear();
     clusters_.clear();
     clustersIsland_.clear();
+    jetsMC_.clear();
+    jetsEHT_.clear();
+    jetsPF_.clear();
   }
 
   class Particle {
@@ -35,6 +38,15 @@ class EventColin {
     int type;
   };   
 
+  class Jets {
+  public:
+    Jets() : eta(0),phi(0),et(0),e(0) {}
+    virtual ~Jets() {}
+    double eta;
+    double phi;
+    double et;
+    double e;
+  };
 
   void addParticle( const Particle& ptc ) {
     particles_.push_back(ptc);
@@ -48,6 +60,18 @@ class EventColin {
     clustersIsland_.push_back(ptc);
   }
 
+  void addJetsMC( const Jets & jets ) {
+    jetsMC_.push_back(jets);
+  }
+
+  void addJetsEHT( const Jets & jets ) {
+    jetsEHT_.push_back(jets);
+  }
+
+  void addJetsPF( const Jets & jets ) {
+    jetsPF_.push_back(jets);
+  }
+
   const std::vector<EventColin::Particle>& particles() 
     {return particles_;}
   const std::vector<EventColin::Cluster>& clusters() 
@@ -59,8 +83,11 @@ class EventColin {
 
 
   std::vector<EventColin::Particle> particles_;
-  std::vector<EventColin::Cluster> clusters_;
-  std::vector<EventColin::Cluster> clustersIsland_;
+  std::vector<EventColin::Cluster>  clusters_;
+  std::vector<EventColin::Cluster>  clustersIsland_;
+  std::vector<EventColin::Jets>     jetsMC_;
+  std::vector<EventColin::Jets>     jetsEHT_;
+  std::vector<EventColin::Jets>     jetsPF_;
   
 };
 
