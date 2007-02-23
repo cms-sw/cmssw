53ced67a28827a0f338c4b7eaecb9e44f38c2f8e
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-02-23 15:50:58 +0000

    fixed RefVector object selector

--- PhysicsTools/UtilAlgos/interface/ObjectRefVectorSelector.h
+++ PhysicsTools/UtilAlgos/interface/ObjectRefVectorSelector.h
@@ -12,6 +12,7 @@ namespace helper {
   
   template<typename C>
   struct RefVectorStoreMananger {
+    typedef edm::RefVector<C> collection;
     RefVectorStoreMananger() : selected_( new edm::RefVector<C> ) { 
     }
     template<typename I>
@@ -19,10 +20,10 @@ namespace helper {
       for( I i = begin; i != end; ++ i )
 	selected_->push_back( * i );
     }
-    void put( edm::Event & evt ) {
-      evt.put( selected_ );
+     edm::OrphanHandle<collection> put( edm::Event & evt ) {
+      return evt.put( selected_ );
     }
-    bool empty() const { return selected_->empty(); }
+    size_t size() const { return selected_->size(); }
   private:
     std::auto_ptr<edm::RefVector<C> > selected_;
   };
@@ -37,9 +38,9 @@ namespace helper {
 
 template<typename S, 
 	 typename N = NonNullNumberSelector,
-         typename P = reco::helpers::NullPostProcessor<typename S::collection>,
-	 typename M = typename helper::CollectionStoreManager<typename S::collection>::type, 
-	 typename B = typename helper::CollectionStoreManager<typename S::collection>::base>
+         typename P = reco::helpers::NullPostProcessor<edm::RefVector<typename S::collection> >,
+	 typename M = typename helper::RefVectorCollectionStoreManager<typename S::collection>::type, 
+	 typename B = typename helper::RefVectorCollectionStoreManager<typename S::collection>::base>
 class ObjectRefVectorSelector : public ObjectSelector<S, N, P, M, B> {
 public:
   explicit ObjectRefVectorSelector( const edm::ParameterSet & cfg ) :
