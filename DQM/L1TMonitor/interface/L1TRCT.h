48096fc53c63cbf99a538cfaf1ea66e14d98ddd4
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2007-02-23 21:58:43 +0000

    change getByType to getByLabel and add InputTag

--- DQM/L1TMonitor/interface/L1TRCT.h
+++ DQM/L1TMonitor/interface/L1TRCT.h
@@ -5,11 +5,14 @@
 /*
  * \file L1TRCT.h
  *
- * $Date: 2007/02/19 21:11:23 $
- * $Revision: 1.2 $
+ * $Date: 2007/02/19 22:49:53 $
+ * $Revision: 1.1 $
  * \author P. Wittich
- * $Id: L1TRCT.h,v 1.2 2007/02/19 21:11:23 wittich Exp $
+ * $Id: L1TRCT.h,v 1.1 2007/02/19 22:49:53 wittich Exp $
  * $Log: L1TRCT.h,v $
+ * Revision 1.1  2007/02/19 22:49:53  wittich
+ * - Add RCT monitor
+ *
  *
  *
 */
@@ -92,7 +95,7 @@ private:
   bool monitorDaemon_;
   ofstream logFile_;
 
-  //edm::InputTag l1ExtraLabel_;
+  edm::InputTag rctSource_;
 
 
 };
