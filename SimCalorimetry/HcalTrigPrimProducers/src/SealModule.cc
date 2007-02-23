53a937b0915bed2b96f87a9933835f6352d18ea2
mlw <sha1-db0d09810bc23ac93748e1866784d92075619538@cern.ch>
2007-02-23 17:17:31 +0000

    Added TPGntupler and HcalDummyHitProducer

--- SimCalorimetry/HcalTrigPrimProducers/src/SealModule.cc
+++ SimCalorimetry/HcalTrigPrimProducers/src/SealModule.cc
@@ -1,10 +1,11 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimCalorimetry/HcalTrigPrimProducers/src/HcalTrigPrimDigiProducer.h"
-
+#include "SimCalorimetry/HcalTrigPrimProducers/src/HcalDummyHitProducer.h"
+#include "SimCalorimetry/HcalTrigPrimProducers/src/TPGntupler.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalTrigPrimDigiProducer);
-
-
+DEFINE_ANOTHER_FWK_MODULE(HcalDummyHitProducer);
+DEFINE_ANOTHER_FWK_MODULE(TPGntupler);
