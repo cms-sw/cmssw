3894a59a90295e1f3b72b41012f18e015c98bea5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-02-24 00:36:38 +0000

    Remove duplicate data dictionaries

--- DataFormats/VertexReco/src/classes.h
+++ DataFormats/VertexReco/src/classes.h
@@ -1,21 +1,18 @@
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
+#include "DataFormats/Common/interface/RefVector.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/Common/interface/AssociationMap.h"
 #include <vector>
 #include <utility>
 
 namespace {
   namespace {
-    std::pair<unsigned int,double> puid;
     reco::Vertex rv1;
     std::vector<reco::Vertex> v1;
     edm::Wrapper<std::vector<reco::Vertex> > wc1;
     edm::Ref<std::vector<reco::Vertex> > r1;
     edm::RefProd<std::vector<reco::Vertex> > rp1;
     edm::RefVector<std::vector<reco::Vertex> > rvv1;
-
-    edm::AssociationMap<edm::OneToValue< std::vector<reco::Track>, float, unsigned int > > am2;
   }
 }
