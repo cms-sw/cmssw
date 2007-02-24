3af7ed36b42e8f9472531a937649b86b101ea048
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-02-24 00:51:41 +0000

    Remove duplicate dictionaries

--- DataFormats/SiStripDigi/src/classes.h
+++ DataFormats/SiStripDigi/src/classes.h
@@ -11,24 +11,22 @@
 #include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
 namespace {
   namespace {
-    edm::Wrapper< SiStripDigi > zs0;
-    edm::Wrapper< std::vector<SiStripDigi>  > zs1;
-    edm::Wrapper< edm::DetSet<SiStripDigi> > zs2;
-    edm::Wrapper< std::vector<edm::DetSet<SiStripDigi> > > zs3;
-    edm::Wrapper< edm::DetSetVector<SiStripDigi> > zs4;
+    edm::Wrapper<SiStripDigi > zs0;
+    edm::Wrapper<std::vector<SiStripDigi> > zs1;
+    edm::Wrapper<edm::DetSet<SiStripDigi> > zs2;
+    edm::Wrapper<std::vector<edm::DetSet<SiStripDigi> > > zs3;
+    edm::Wrapper<edm::DetSetVector<SiStripDigi> > zs4;
   }
 }
 
 #include "DataFormats/SiStripDigi/interface/SiStripRawDigi.h"
-#include "DataFormats/Common/interface/traits.h" // DoNotSortUponInsertion
 namespace {
   namespace {
-    edm::Wrapper< SiStripRawDigi > raw0;
-    edm::Wrapper< std::vector<SiStripRawDigi>  > raw1;
-    edm::Wrapper< edm::DetSet<SiStripRawDigi> > raw2;
-    edm::Wrapper< std::vector<edm::DetSet<SiStripRawDigi> > > raw3;
-    edm::Wrapper< edm::DetSetVector<SiStripRawDigi> > raw4;
-    edm::Wrapper< edm::DoNotSortUponInsertion > raw5;
+    edm::Wrapper<SiStripRawDigi > raw0;
+    edm::Wrapper<std::vector<SiStripRawDigi> > raw1;
+    edm::Wrapper<edm::DetSet<SiStripRawDigi> > raw2;
+    edm::Wrapper<std::vector<edm::DetSet<SiStripRawDigi> > > raw3;
+    edm::Wrapper<edm::DetSetVector<SiStripRawDigi> > raw4;
   }
 }
 
@@ -40,23 +38,23 @@ namespace {
 
     edm::Wrapper<SiStripDigiCollection> digis;
 
-    edm::Wrapper< std::vector<bool> > list_of_fed_id;
+    edm::Wrapper<std::vector<bool> > list_of_fed_id;
 
     edm::Wrapper<uint8_t*> ptr_to_buffer;
-    edm::Wrapper< std::vector<uint8_t*> > ptrs_to_buffers;
+    edm::Wrapper<std::vector<uint8_t*> > ptrs_to_buffers;
 
     edm::Wrapper<uint32_t> size_of_buffer;
-    edm::Wrapper< std::vector<uint32_t> > size_of_buffers;
+    edm::Wrapper<std::vector<uint32_t> > size_of_buffers;
 
     edm::Wrapper<sistrip::FedReadoutMode> fed_readout_mode;
-    edm::Wrapper< std::vector<sistrip::FedReadoutMode> > fed_readout_modes;
+    edm::Wrapper<std::vector<sistrip::FedReadoutMode> > fed_readout_modes;
 
     edm::Wrapper<sistrip::FedReadoutPath> fed_readout_path;
-    edm::Wrapper< std::vector<sistrip::FedReadoutPath> > fed_readout_paths;
+    edm::Wrapper<std::vector<sistrip::FedReadoutPath> > fed_readout_paths;
 
     edm::Wrapper<uint16_t> payload_position;
-    edm::Wrapper< std::vector<uint16_t> > payload_positions;
-    edm::Wrapper< std::vector< std::vector<uint16_t> > > more_payload_positions;
+    edm::Wrapper<std::vector<uint16_t> > payload_positions;
+    edm::Wrapper<std::vector<std::vector<uint16_t> > > more_payload_positions;
 
   }
 }
@@ -65,9 +63,9 @@ namespace {
 #include "DataFormats/SiStripCommon/interface/SiStripEnumeratedTypes.h"
 namespace {
   namespace {
-    edm::Wrapper< sistrip::Task > task;
-    edm::Wrapper< sistrip::FedReadoutMode > fed_mode;
-    edm::Wrapper< SiStripEventSummary > summary;
+    edm::Wrapper<sistrip::Task > task;
+    edm::Wrapper<sistrip::FedReadoutMode > fed_mode;
+    edm::Wrapper<SiStripEventSummary > summary;
 
   }
 }
