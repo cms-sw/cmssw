b156a5b6b405bbbe51e22f45035da34e712832c5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-02-25 08:26:37 +0000

    ok

---
+++ PerfTools/Callgrind/interface/ProfilerService.h
@@ -0,0 +1,52 @@
+#ifndef ProfilerService_H
+#define ProfilerService_H
+
+
+//FIXME only forward declarations???
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/ActivityRegistry.h"
+
+#include <vector>
+#include <string>
+
+
+/* A Service to start and stop calgrind profiling on demand...
+ * act also as profiler watchdog 
+ * (in the same service to avoid dependency between service)
+ */
+class ProfilerService {
+public:
+  ProfilerService(edm::ParameterSet const& pset, 
+		  edm::ActivityRegistry  & activity);
+
+  ~ProfilerService();
+
+  bool startInstrumentation();
+  bool stopInstrumentation();
+  bool forceStopInstrumentation();
+  bool dumpStat();
+
+  void beginEvent(const edm::EventID&, const edm::Timestamp&);
+  void endEvent(const edm::Event&, const edm::EventSetup&);
+  
+  void beginPath(std::string const & path);
+  void endPath(std::string const & path,  const edm::HLTPathStatus&);
+
+  
+
+private:
+
+  // configurable
+  int m_firstEvent; 
+  int m_lastEvent;
+  std::vector<std::string> m_paths; 
+
+  // internal state
+  int m_evtCount;
+  bool m_doEvent;
+  int m_active;
+  std::string m_activePath;
+
+}; 
+
+#endif // ProfilerService_H
---
+++ PerfTools/Callgrind/src/ProfilerService.cc
@@ -0,0 +1,79 @@
+#include "PerfTools/Callgrind/interface/ProfilerService.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <algorithm>
+
+#include "valgrind/callgrind.h"
+
+ProfilerService::ProfilerService(edm::ParameterSet const& pset, 
+				 edm::ActivityRegistry  & activity) :
+  
+  m_firstEvent(pset.getUntrackedParameter<int>("FirstEvent",0 )),
+  m_lastEvent(pset.getUntrackedParameter<int>("LastEvent",-1)),
+  m_paths(pset.getUntrackedParameter<std::vector<std::string> >("Paths",std::vector<std::string>() )),
+  m_evtCount(0),
+  m_doEvent(false);
+  m_active(0){
+    activity.watchPreProcessEvent)(this,&ProfilerService::beginEvent);
+    activity.watchPostProcessEvent)(this,&ProfilerService::endEvent);
+    activity.watchPreProcessPath)(this,&ProfilerService::beginPath);
+    activity.watchPostProcessPath)(this,&ProfilerService::endPath);
+
+}
+
+ProfilerService::~ProfilerService(){}
+
+bool ProfilerService::startInstrumentation(){
+  if (m_active==0) {
+    CALLGRIND_START_INSTRUMENTATION;
+    CALLGRIND_DUMP_STATS;
+  }
+  // support nested start/stop
+  ++m_active;
+  return m_active==1;
+}
+
+bool ProfilerService::stopInstrumentation() {
+  if (m_active==0) return false;
+  CALLGRIND_STOP_INSTRUMENTATION;
+  --m_active();
+  return m_active==0;
+}
+
+bool forceStopInstrumentation() {
+  if (m_active==0) return false;
+  CALLGRIND_STOP_INSTRUMENTATION;
+  m_active=0;
+  return true;
+}
+
+
+bool ProfilerService::dumpStat() {
+     CALLGRIND_DUMP_STATS;
+}
+
+void  ProfilerService::beginEvent(const edm::EventID&, const edm::Timestamp&) {
+  ++m_evtCount;
+  doEvent = m_evtCount >= m_firstEvent && m_evtCount <= m_lastEvent;
+}
+
+void  ProfilerService::endEvent(const edm::Event&, const edm::EventSetup&) {
+  // force, a nested instrumentation may fail to close in presence of filters
+  forceStopInstrumentation();
+}
+
+void  ProfilerService::beginPath(std::string const & path) {
+  // assume less than 5-6 path to instrument ....
+  if (std::find(m_path.begin(),m_path.end(),path) == m_path.end()) return; 
+    m_activePath=path;
+    startInstrumentation();
+}
+
+void  ProfilerService::endPath(std::string const & /* path */,  const edm::HLTPathStatus&) {
+  // force, a nested instrumentation may fail to close in presence of filters
+  forceStopInstrumentation();
+  m_activePath.clear();
+}
