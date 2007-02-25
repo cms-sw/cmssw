1e97779f2e6085ae912fd383ace4d661baa0cf3b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-02-25 02:43:12 +0000

    Remove stdNamespaceAdder.*, which can be used from RootAutoLibraryLoader

--- FWCore/FWLite/test/stdnamespaceadder_t.cppunit.cpp
+++ FWCore/FWLite/test/stdnamespaceadder_t.cppunit.cpp
@@ -3,14 +3,14 @@
 Test program for edm::TypeIDBase class.
 Changed by Viji on 29-06-2005
 
-$Id: stdnamespaceadder_t.cppunit.cpp,v 1.1 2006/05/17 16:43:44 wmtan Exp $
+$Id: stdnamespaceadder_t.cppunit.cpp,v 1.2 2006/05/30 21:48:43 chrjones Exp $
  ----------------------------------------------------------------------*/
 
 #include <cassert>
 #include <iostream>
 #include <string>
 #include <cppunit/extensions/HelperMacros.h>
-#include "FWCore/FWLite/src/stdNamespaceAdder.h"
+#include "FWCore/RootAutoLibraryLoader/src/stdNamespaceAdder.h"
 
 class testSTDNamespaceAdder: public CppUnit::TestFixture
 {
@@ -35,7 +35,7 @@ using std::endl;
 void testSTDNamespaceAdder::tests()
 {
    using std::string;
-   using namespace fwlite;
+   using edm::root::stdNamespaceAdder;
 
    static const string kVectorBlahNoNS("vector<Blah>");
    static const string kVectorBlahNS("std::vector<Blah>");
