3eb77f01b6ea1050f40f6b1bf3a7f884cf90d65c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-02-25 03:47:33 +0000

    Add dictionaries that were mistakenly in DataFormats/MuonReco

--- DataFormats/TrackReco/src/classes.h
+++ DataFormats/TrackReco/src/classes.h
@@ -2,6 +2,10 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
 #include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
+#include "DataFormats/Common/interface/RefVector.h"
+#include "DataFormats/Common/interface/OneToValue.h"
 #include <vector>
 
 namespace {
@@ -20,8 +24,14 @@ namespace {
     edm::Wrapper<reco::TrackRefVector> wv1;
     std::vector<reco::TrackRef> vr1;
 
-    edm::helpers::Key< edm::RefProd < std::vector < reco::Track > > > rpt1;
-    edm::AssociationMap<edm::OneToValue< std::vector<reco::Track>, double, unsigned int > > am1;
-    edm::AssociationMap<edm::OneToValue< std::vector<reco::Track>, float, unsigned int > > am2;
+    edm::helpers::Key<edm::RefProd<std::vector<reco::Track> > > rpt1;
+    edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, double, unsigned int> > am1;
+    edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, float, unsigned int> > am2;
+    edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, int, unsigned int> > am3;
+    edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, bool, unsigned int> > am4;
+    edm::Wrapper<edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, double, unsigned int> > > wam1;
+    edm::Wrapper<edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, float, unsigned int> > > wam2;
+    edm::Wrapper<edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, int, unsigned int> > >  wam3;
+    edm::Wrapper<edm::AssociationMap<edm::OneToValue<std::vector<reco::Track>, bool, unsigned int> > > wam4;
   }
 }
