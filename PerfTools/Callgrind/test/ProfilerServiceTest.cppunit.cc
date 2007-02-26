96cf79d978a601fcd2f1b37e7d3f621956fb8261
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-02-26 11:09:09 +0000

    more tests

--- PerfTools/Callgrind/test/ProfilerServiceTest.cppunit.cc
+++ PerfTools/Callgrind/test/ProfilerServiceTest.cppunit.cc
@@ -9,7 +9,8 @@
 #include <boost/assign/std/vector.hpp>
 #include <boost/assign/list_of.hpp>
 using namespace boost::assign;
-
+#include <boost/ref.hpp>
+#include <boost/bind.hpp>
 
 #include <limits>
 #include<vector>
@@ -210,14 +211,14 @@ void TestProfilerService::check_Path() {
   ps.beginEvent();
   CPPUNIT_ASSERT(ps.m_active==0);
   CPPUNIT_ASSERT(!ps.doEvent());
-  std::for_each(allPaths.begin(),allPaths.end(),cp);
+  std::for_each(allPaths.begin(),allPaths.end(),boost::ref(cp));
   CPPUNIT_ASSERT(cp.done==0);
   ps.endEvent();
 
   ps.beginEvent();
   CPPUNIT_ASSERT(ps.m_active==0);
   CPPUNIT_ASSERT(ps.doEvent());
-  std::for_each(allPaths.begin(),allPaths.end(),cp);
+  std::for_each(allPaths.begin(),allPaths.end(),boost::ref(cp));
   CPPUNIT_ASSERT(cp.done==2);
   ps.endEvent();
 
@@ -245,7 +246,7 @@ void TestProfilerService::check_Nesting() {
   ps.beginEvent();
   CPPUNIT_ASSERT(ps.m_active==0);
   CPPUNIT_ASSERT(!ps.doEvent());
-  std::for_each(allPaths.begin(),allPaths.end(),cp0);
+  std::for_each(allPaths.begin(),allPaths.end(),boost::ref(cp0));
   CPPUNIT_ASSERT(cp0.done==0);
   ps.endEvent();
   CPPUNIT_ASSERT(ps.m_active==0);
@@ -253,7 +254,7 @@ void TestProfilerService::check_Nesting() {
   ps.beginEvent();
   CPPUNIT_ASSERT(ps.m_active==1);
   CPPUNIT_ASSERT(ps.doEvent());
-  std::for_each(allPaths.begin(),allPaths.end(),cp1);
+  std::for_each(allPaths.begin(),allPaths.end(),boost::ref(cp1));
   CPPUNIT_ASSERT(cp1.done==2);
   ps.endEvent();
   CPPUNIT_ASSERT(ps.m_active==0);
