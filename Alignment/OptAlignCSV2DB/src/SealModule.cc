0f7b13e514cbfbe3290d284cd9bdc4b1fd90c971
Pedro Arce <sha1-a7e83fd644adcfec94499d67c7f8f23354d9a549@cern.ch>
2007-02-27 17:07:28 +0000

    update to new DB schema

--- Alignment/OptAlignCSV2DB/src/SealModule.cc
+++ Alignment/OptAlignCSV2DB/src/SealModule.cc
@@ -1,7 +1,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Alignment/OptAlignCSV2DB/interface/OptAlignDataConverter.h"
 #include "Alignment/OptAlignCSV2DB/interface/OptAlignDBAnalyzer.h"
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(OptAlignDataConverter)
-
-DEFINE_ANOTHER_FWK_MODULE(OptAlignDBAnalyzer)
+//DEFINE_SEAL_MODULE();
+DEFINE_FWK_MODULE(OptAlignDataConverter);
+DEFINE_ANOTHER_FWK_MODULE(OptAlignDBAnalyzer);
