30bae796d22f78f519bda0924482e7bc7ce82146
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2007-02-28 14:39:09 +0000

    Correct a mm -> cm bug. Optimize timing

--- FastSimulation/MaterialEffects/src/MultipleScatteringUpdator.cc
+++ FastSimulation/MaterialEffects/src/MultipleScatteringUpdator.cc
@@ -1,8 +1,7 @@
 #include "FastSimulation/MaterialEffects/interface/MultipleScatteringUpdator.h"
 
-#include "CLHEP/Geometry/Vector3D.h"
-#include "CLHEP/Geometry/Transform3D.h"
 #include "CLHEP/Vector/LorentzVector.h"
+#include "CLHEP/Vector/Rotation.h"
 #include "CLHEP/Units/PhysicalConstants.h"
 
 #include <iostream>
@@ -38,36 +37,26 @@ void MultipleScatteringUpdator::compute(ParticlePropagator &Particle)
   double theta1 = random->gaussShoot(0.,theta0); 
   double theta2 = random->gaussShoot(0.,theta0); 
 
-  HepVector3D theP(Particle.px(),Particle.py(),Particle.pz());
-  HepVector3D perpP1 = theP.orthogonal();
-  HepVector3D perpP2 = theP.cross(perpP1);
-  HepTransform3D Rotation1 = HepRotate3D(theta1,perpP1);
-  HepTransform3D Rotation2 = HepRotate3D(theta2,perpP2);
-  HepTransform3D theRotation = Rotation2*Rotation1;
-  HepVector3D newP(theRotation*theP);
-
-  Particle.setPx(newP.x()); 
-  Particle.setPy(newP.y()); 
-  Particle.setPz(newP.z());
+  Hep3Vector axis1 = Particle.vect().orthogonal();
+  Hep3Vector axis2 = Particle.vect().cross(axis1);
+  HepRotation theRotation1(axis1,theta1);
+  HepRotation theRotation2(axis2,theta2);
+  Particle *= theRotation2*theRotation1;
 
   // Generate mutiple scattering displacements in mm (assuming the detectors
   // are silicon only to determine the thickness) in the directions orthogonal
   // to the vector normal to the surface
-
   double xp = (theta1/2. + random->gaussShoot(0.,theta0)/sqrt(12.))
-                         * radLengths * radLenIncm() * 10.;
+                         * radLengths * radLenIncm();		 
   double yp = (theta2/2. + random->gaussShoot(0.,theta0)/sqrt(12.))
-                         * radLengths * radLenIncm() * 10.;
+                         * radLengths * radLenIncm();
   
-  HepVector3D 
+  Hep3Vector 
     normal(theNormalVector.x(),theNormalVector.y(),theNormalVector.z());
-  HepVector3D tangent1 = normal.orthogonal();
-  HepVector3D tangent2 = normal.cross(tangent1);
-  HepVector3D thePos(Particle.vertex().x(),
-		     Particle.vertex().y(),
-		     Particle.vertex().z());
+  Hep3Vector tangent1 = normal.orthogonal();
+  Hep3Vector tangent2 = normal.cross(tangent1);
 
-  HepVector3D newPos = thePos + tangent1*xp + tangent2*yp;
+  Hep3Vector newPos = Particle.vertex().vect() + tangent1*xp + tangent2*yp;
 
   HepLorentzVector newVertex(newPos,Particle.vertex().t());
 
