8278885707fa022b9375ae491d40e7533d8996ef
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-03-01 15:56:30 +0000

    removed obsolete code

--- DataFormats/HepMCCandidate/interface/GenParticleCandidate.h
+++ DataFormats/HepMCCandidate/interface/GenParticleCandidate.h
@@ -20,52 +20,20 @@ namespace reco {
   public:
     /// default constructor
     GenParticleCandidate() { }
-    /// constroctor from pointer to generator particle
-    GenParticleCandidate( const HepMC::GenParticle * );
     /// constrocturo from values
     GenParticleCandidate( Charge q, const LorentzVector & p4, const Point & vtx, 
-			  int pdgId, int status, bool integerCharge = true );
+			  int pdgId, int status );
     /// destructor
     virtual ~GenParticleCandidate();
     /// return a clone
     GenParticleCandidate * clone() const;
-    /// PDG code
-    virtual int pdgId() const { return pdgId_; }
-    /// status code
-    int status() const { return status_; }
 
   private:
     /// post-read fixup
     virtual void fixup() const;
     /// checp overlap with another candidate
     bool overlap( const Candidate & ) const;
-    /// PDG code
-    int pdgId_;
-    /// status code
-    int status_;
  };
-
-  /// PDG id component tag
-  struct PdgIdTag { };
-
-  /// status code component tag
-  struct StatusTag { };
-
-  /// get PDG id component
-  GET_CANDIDATE_COMPONENT( GenParticleCandidate, int, pdgId, PdgIdTag );
-
-  /// get status code component
-  GET_CANDIDATE_COMPONENT( GenParticleCandidate, int, status, StatusTag );
-
-  /// left for backward compatibility. Can now be replaced by
-  /// an equivalent member function
-  inline int pdgId( const Candidate & c ) {
-    return c.pdgId();
-  }
-
-  inline int status( const Candidate & c ) {
-    return c.get<int, StatusTag>();
-  }
 
 }
 
--- DataFormats/HepMCCandidate/src/GenParticleCandidate.cc
+++ DataFormats/HepMCCandidate/src/GenParticleCandidate.cc
@@ -1,33 +1,11 @@
 // $Id: GenParticleCandidate.cc,v 1.8 2007/02/19 12:59:05 llista Exp $
 #include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
-#include <CLHEP/HepMC/GenParticle.h>
-#include <CLHEP/HepMC/GenVertex.h>
 #include <iostream>
 using namespace reco;
 
-GenParticleCandidate::GenParticleCandidate( const HepMC::GenParticle * p ) : 
-  CompositeRefCandidate(), 
-  pdgId_( p->pdg_id() ), 
-  status_( p->status() ) {
-  qx3_ = p->particleID().threeCharge();
-  CLHEP::HepLorentzVector p4 =p->momentum();
-  p4_ = LorentzVector( p4.x(), p4.y(), p4.z(), p4.t() );
-  const HepMC::GenVertex * v = p->production_vertex();
-  if ( v != 0 ) {
-    HepGeom::Point3D<double> vtx = v->point3d();
-    vertex_ = Point( vtx.x() / 10. , vtx.y() / 10. , vtx.z() / 10. );
-  } else {
-    vertex_.SetXYZ( 0, 0, 0 );
-  }
-  // don't fill references to daughters at this level
-}
-
 GenParticleCandidate::GenParticleCandidate( Charge q, const LorentzVector & p4, 
-					    const Point & vtx, int pdgId, int status, 
-					    bool integerCharge ) : 
-  CompositeRefCandidate( q, p4, vtx, integerCharge ), 
-  pdgId_( pdgId ), 
-  status_( status ) {
+					    const Point & vtx, int pdgId, int status ) : 
+  CompositeRefCandidate( q, p4, vtx, pdgId, status, false ) {
 }
 
 GenParticleCandidate::~GenParticleCandidate() { }
