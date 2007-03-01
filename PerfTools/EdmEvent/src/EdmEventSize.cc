b469170b09ad50856af18993ec83b9be3b1dfa56
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-03-01 12:53:21 +0000

    greater

--- PerfTools/EdmEvent/src/EdmEventSize.cc
+++ PerfTools/EdmEvent/src/EdmEventSize.cc
@@ -96,7 +96,7 @@ namespace perftools {
       m_branches.push_back( BranchRecord(name, s[0], s[1]) );
     }
     std::sort(m_branches.begin(),m_branches.end(), 
-	      boost::bind(std::less<size_t>(),
+	      boost::bind(std::greater<size_t>(),
 			  boost::bind(&BranchRecord::compr_size,_1),
 			  boost::bind(&BranchRecord::compr_size,_2))
 	      );
