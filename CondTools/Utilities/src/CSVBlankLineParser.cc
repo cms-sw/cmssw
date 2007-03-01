a8c16bd88f341f60e12e450499eab763c2ece04b
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-03-01 13:42:40 +0000

    moved to CondCore

--- CondTools/Utilities/src/CSVBlankLineParser.cc
+++
@@ -1,8 +0,0 @@
-#include "CondTools/Utilities/interface/CSVBlankLineParser.h"
-#include <boost/spirit/core.hpp>
-using namespace boost::spirit;
-
-bool CSVBlankLineParser::isBlank( const std::string& inputLine){
-  boost::spirit::rule<> blankparser=*blank_p;
-  return boost::spirit::parse(inputLine.c_str(),blankparser).full;
-}
