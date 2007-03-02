e5f38659408402c36865600f43c1aba59bddc8e4
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-03-02 15:49:19 +0000

    put them back

---
+++ L1Trigger/GlobalCaloTrigger/interface/L1GctTestAnalyzer.h
@@ -0,0 +1,75 @@
+// -*- C++ -*-
+//
+// Package:    L1GctTestAnalyzer
+// Class:      L1GctTestAnalyzer
+// 
+/**\class L1GctTestAnalyzer L1GctTestAnalyzer.cc L1Trigger/GlobalCaloTrigger/test/L1GctTestAnalyzer.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Jim Brooke
+//         Created:  Thu May 18 16:45:23 CEST 2006
+// $Id: L1GctTestAnalyzer.h,v 1.5 2007/03/02 14:00:48 jbrooke Exp $
+//
+//
+
+
+// user include files
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+
+#include <iostream>
+#include <fstream>
+
+//
+// class decleration
+//
+
+class L1GctTestAnalyzer : public edm::EDAnalyzer {
+public:
+  explicit L1GctTestAnalyzer(const edm::ParameterSet&);
+  ~L1GctTestAnalyzer();
+  
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  
+  void doRctEM(const edm::Event&, edm::InputTag label);
+  void doInternEM(const edm::Event&, edm::InputTag label);
+  void doEM(const edm::Event&, edm::InputTag label);
+  void doJets(const edm::Event&, edm::InputTag label);
+  
+private:
+  // ----------member data ---------------------------
+  
+  edm::InputTag rawLabel_;
+  edm::InputTag emuLabel_;
+  std::string outFilename_;
+  std::ofstream outFile_;
+
+  bool doHW_;
+  bool doEmu_;
+  bool doRctEM_;
+  bool doInternEM_;
+  bool doEM_;
+  bool doJets_;
+
+  unsigned rctEmMinRank_;
+  
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
