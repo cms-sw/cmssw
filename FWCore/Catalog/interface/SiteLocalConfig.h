701803d7e218f1b91df95cdb3a04c60cfd10037b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-03-04 04:43:30 +0000

    Moved here from FWCore/Framework

---
+++ FWCore/Catalog/interface/SiteLocalConfig.h
@@ -0,0 +1,42 @@
+#ifndef FWCore_Catalog_SiteLocalConfig_h
+#define FWCore_Catalog_SiteLocalConfig_h
+
+// INCLUDES
+# include <string>
+# include <list>
+
+// PUBLIC DEFINES
+// PUBLIC CONSTANTS
+// PUBLIC TYPES
+namespace edm 
+{
+    class ParameterSet;
+    class ActivityRegistry;
+}
+
+// PUBLIC VARIABLES
+// PUBLIC FUNCTIONS
+// CLASS DECLARATIONS
+
+namespace edm 
+{
+  class SiteLocalConfig
+  {
+  public:
+    SiteLocalConfig () {}
+    virtual ~SiteLocalConfig() {}
+    
+    virtual const std::string dataCatalog (void) const = 0;
+    virtual const std::string calibCatalog (void) const = 0;
+    virtual const std::string lookupCalibConnect (const std::string& input) const = 0;
+    
+    // implicit copy constructor
+    // implicit assignment operator
+  private:
+  };
+}
+    
+// INLINE PUBLIC FUNCTIONS
+// INLINE MEMBER FUNCTIONS
+
+#endif //FWCore_Catalog_SiteLocalConfig_h
