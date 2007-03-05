642ad6a4577f1bfd4043a57237e8aa93067bb629
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-03-05 13:09:39 +0000

    Modified headers (some were missed in previous step)

--- Alignment/LaserDQM/plugins/LaserDQMStatistics.cc
+++ Alignment/LaserDQM/plugins/LaserDQMStatistics.cc
@@ -5,7 +5,7 @@
 #include "Alignment/LaserDQM/plugins/LaserDQM.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 
 #include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
