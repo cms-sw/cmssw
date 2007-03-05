0839eecc061095511d6f44837719f641c486a757
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2007-03-05 07:20:44 +0000

    cleaned

--- Validation/RecoMuon/test/run.sh
+++ Validation/RecoMuon/test/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 
-#WORKDIR=/afs/cern.ch/user/j/jhgoh/Work/Validation/CMSSW_1_2_0/src/Validation/RecoMuon
-#OUTDIR=$WORKDIR/data/`date %Y%m%d%I%M`
-
 cmsRun $PKGDIR/test/${1}.cfg >& ${1}.log
 RETVAL=$?
 
