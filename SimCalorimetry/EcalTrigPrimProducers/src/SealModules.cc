6582c325dc2071987766e085eaf5d865559d537f
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2007-03-06 13:14:53 +0000

    add input analyzer

--- SimCalorimetry/EcalTrigPrimProducers/src/SealModules.cc
+++ SimCalorimetry/EcalTrigPrimProducers/src/SealModules.cc
@@ -5,10 +5,11 @@
 
 #include "SimCalorimetry/EcalTrigPrimProducers/interface/EcalTrigPrimProducer.h"
 #include "SimCalorimetry/EcalTrigPrimProducers/interface/EcalTrigPrimAnalyzer.h"
+#include "SimCalorimetry/EcalTrigPrimProducers/interface/EcalTPInputAnalyzer.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EcalTrigPrimProducer);
-
+DEFINE_ANOTHER_FWK_MODULE(EcalTPInputAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(EcalTrigPrimAnalyzer);
 
