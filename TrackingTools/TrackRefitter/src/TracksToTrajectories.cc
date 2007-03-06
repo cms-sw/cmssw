ecb2b7effec4ab76951c5b10ff1a508d4399e726
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2007-03-06 17:00:32 +0000

    Association between Trajectory and Track

--- TrackingTools/TrackRefitter/src/TracksToTrajectories.cc
+++ TrackingTools/TrackRefitter/src/TracksToTrajectories.cc
@@ -9,6 +9,7 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "TrackingTools/PatternTools/interface/Trajectory.h"
+#include "TrackingTools/PatternTools/interface/TrajTrackAssociation.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
@@ -24,6 +25,7 @@ TracksToTrajectories::TracksToTrajectories(const ParameterSet& parameterSet){
   theTrackTransformer = new TrackTransformer(parameterSet.getParameter<ParameterSet>("TrackTransformer"));
   
   produces<vector<Trajectory> >();
+  produces<TrajTrackAssociationCollection>();
 }
 
 /// Destructor
@@ -35,25 +37,44 @@ TracksToTrajectories::~TracksToTrajectories(){
 void TracksToTrajectories::produce(Event& event, const EventSetup& setup){
 
   const string metname = "Reco|TrackingTools|TracksToTrajectories";
-  
-  auto_ptr<vector<Trajectory> > trajectoryCollection(new std::vector<Trajectory>);
-  
+
   theTrackTransformer->setServices(setup);
   
+  // Collection of Trajectory
+  auto_ptr<vector<Trajectory> > trajectoryCollection(new vector<Trajectory>);
+  
+  // Get the reference
+  RefProd<vector<Trajectory> > trajectoryCollectionRefProd 
+    = event.getRefBeforePut<vector<Trajectory> >();
+  
+  // Association map between Trajectory and Track
+  auto_ptr<TrajTrackAssociationCollection> trajTrackMap(new TrajTrackAssociationCollection);
+ 
   // Get the RecTrack collection from the event
   Handle<reco::TrackCollection> tracks;
   event.getByLabel(theTracksLabel,tracks);
   
+  Ref<vector<Trajectory> >::key_type trajectoryIndex = 0;
+  reco::TrackRef::key_type trackIndex = 0;
+
   // Loop over the Rec tracks
   for (reco::TrackCollection::const_iterator newTrack = tracks->begin(); 
        newTrack != tracks->end(); ++newTrack) {
     
     vector<Trajectory> trajectoriesSM = theTrackTransformer->transform(*newTrack);
     
-    if(!trajectoriesSM.empty())
+    if(!trajectoriesSM.empty()){
+      // Load the trajectory in the Trajectory Container
       trajectoryCollection->push_back(trajectoriesSM.front());
-    
+
+      // Make the association between the Trajectory and the original Track
+      trajTrackMap->insert(Ref<vector<Trajectory> >(trajectoryCollectionRefProd,trajectoryIndex++),
+			   reco::TrackRef(tracks,trackIndex++));
+    }
+    else
+      LogError(metname) << "Error in the Track refitting. This must not happen!";
   }
   LogDebug(metname)<<"Load the Trajectory Collection";
   event.put(trajectoryCollection);
+  event.put(trajTrackMap);
 }
