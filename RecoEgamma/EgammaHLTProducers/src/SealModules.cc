945dcd528074dedbde5321d11de0e9ef93bb36f3
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2007-03-06 10:57:39 +0000

    Adapt to tracking regional reconstruction seeded by SC direction with nominal vertex

--- RecoEgamma/EgammaHLTProducers/src/SealModules.cc
+++ RecoEgamma/EgammaHLTProducers/src/SealModules.cc
@@ -6,6 +6,7 @@
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTElectronTrackIsolationProducers.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTRecoEcalCandidateProducers.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTPixelMatchElectronProducers.h"
+#include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTRegionalPixelSeedGeneratorProducers.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTEcalIsolationProducers);
@@ -14,3 +15,4 @@ DEFINE_ANOTHER_FWK_MODULE(EgammaHLTPhotonTrackIsolationProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTElectronTrackIsolationProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTRecoEcalCandidateProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTPixelMatchElectronProducers);
+DEFINE_ANOTHER_FWK_MODULE(EgammaHLTRegionalPixelSeedGeneratorProducers);
