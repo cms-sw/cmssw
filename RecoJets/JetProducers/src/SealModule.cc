52d6a774a9ae9a4001ff3dba1c031ad6d5f6d29b
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2007-03-06 01:02:38 +0000

    Clean up old Kt stuff Temorary remove new CDF MCone wrapper Disable obsolete CaloTowers data file download

--- RecoJets/JetProducers/src/SealModule.cc
+++ RecoJets/JetProducers/src/SealModule.cc
@@ -3,7 +3,6 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoJets/JetProducers/interface/KtJetProducer.h"
 #include "RecoJets/JetProducers/interface/MidpointJetProducer.h"
 #include "RecoJets/JetProducers/interface/IterativeConeJetProducer.h"
 #include "RecoJets/JetProducers/interface/ToyJetCorrector.h"
@@ -12,14 +11,12 @@
 
 
 using cms::MidpointJetProducer;
-using cms::KtJetProducer;
 using cms::IterativeConeJetProducer;
 using cms::ToyJetCorrector;
 using cms::FastJetProducer;
 using cms::ExtKtJetProducer;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MidpointJetProducer);
-DEFINE_ANOTHER_FWK_MODULE(KtJetProducer);
 DEFINE_ANOTHER_FWK_MODULE(IterativeConeJetProducer);
 DEFINE_ANOTHER_FWK_MODULE(ToyJetCorrector);
 DEFINE_ANOTHER_FWK_MODULE(FastJetProducer);
