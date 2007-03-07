844aeca2262a7152a9f06372db9e25cbe2757691
Phillip Russell Dudero <sha1-27fba2aad3045c4283acebe73f4342e8a8c68f05@cern.ch>
2007-03-07 20:05:51 +0000

    Fix moved header #include for 140 series

--- RecoTBCalo/HcalPlotter/src/HcalQLPlotAnal.cc
+++ RecoTBCalo/HcalPlotter/src/HcalQLPlotAnal.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Phillip R. Dudero
 //         Created:  Tue Jan 16 21:11:37 CST 2007
-// $Id: HcalQLPlotAnal.cc,v 1.1 2007/02/12 05:38:42 dudero Exp $
+// $Id: HcalQLPlotAnal.cc,v 1.2 2007/02/22 15:44:12 dudero Exp $
 //
 //
 
@@ -25,7 +25,7 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
