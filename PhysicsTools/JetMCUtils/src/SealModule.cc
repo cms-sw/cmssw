daa255de065e3c71d36c491ea8da721ca47382a8
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2007-03-07 18:23:58 +0000

    moved to JetMCAlgos

--- PhysicsTools/JetMCUtils/src/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "PhysicsTools/JetMCUtils/interface/GenJetRecoJetMatcher.h"
-#include "PhysicsTools/JetMCUtils/src/jetTest.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE( GenJetRecoJetMatcher );
-DEFINE_ANOTHER_FWK_MODULE( jetTest );
