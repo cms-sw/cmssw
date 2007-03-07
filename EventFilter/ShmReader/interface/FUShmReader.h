ae2d96316be58871669b053386a3a3e4f8a526b1
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2007-03-07 12:59:03 +0000

    tag V00-01-04: V00-01-01 + adjust to FWCore reorganziation.

--- EventFilter/ShmReader/interface/FUShmReader.h
+++ EventFilter/ShmReader/interface/FUShmReader.h
@@ -4,7 +4,7 @@
 
 #include "EventFilter/ResourceBroker/interface/FUShmBuffer.h"
 #include "IORawData/DaqSource/interface/DaqBaseReader.h"
-#include "DataFormats/Common/interface/EventID.h"
+#include "DataFormats/Provenance/interface/EventID.h"
 
 
 class FUShmReader : public DaqBaseReader
