66552f19367c62f7f03261022171a97d9a60a4b6
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2007-03-07 10:07:27 +0000

    Fix for new includes

--- SimCalorimetry/EcalSimProducers/interface/EcalDigiProducer.h
+++ SimCalorimetry/EcalSimProducers/interface/EcalDigiProducer.h
@@ -3,7 +3,6 @@
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Handle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloHitResponse.h"
