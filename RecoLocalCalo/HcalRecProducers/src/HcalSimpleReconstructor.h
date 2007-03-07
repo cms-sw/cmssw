2ea9a7eb72b1877d0e75a1d6169a75e2f027341c
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-03-07 19:29:02 +0000

    Moved into src directory

---
+++ RecoLocalCalo/HcalRecProducers/src/HcalSimpleReconstructor.h
@@ -0,0 +1,33 @@
+#ifndef HCALSIMPLERECONSTRUCTOR_H
+#define HCALSIMPLERECONSTRUCTOR_H 1
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/Common/interface/EDProduct.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalSimpleRecAlgo.h"
+
+
+    /** \class HcalSimpleReconstructor
+	
+    $Date: 2006/09/19 18:19:46 $
+    $Revision: 1.7 $
+    \author J. Mans - Minnesota
+    */
+    class HcalSimpleReconstructor : public edm::EDProducer {
+    public:
+      explicit HcalSimpleReconstructor(const edm::ParameterSet& ps);
+      virtual ~HcalSimpleReconstructor();
+      virtual void produce(edm::Event& e, const edm::EventSetup& c);
+    private:
+      HcalSimpleRecAlgo reco_;
+      HcalSubdetector subdet_;
+      HcalOtherSubdetector subdetOther_;
+      edm::InputTag inputLabel_;
+    };
+
+#endif
