45b01abd20363aa31be34715a46074e4670340db
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2007-03-08 17:14:30 +0000

    tag V00-12-00: compliant with FWCore reorganization in 140 series; evf::StateMachine to be implemented by all FilterFarm apps, see TWIKI page

--- EventFilter/Utilities/src/WebGUI.cc
+++ EventFilter/Utilities/src/WebGUI.cc
@@ -29,7 +29,7 @@ using namespace cgicc;
 ////////////////////////////////////////////////////////////////////////////////
 
 //______________________________________________________________________________
-WebGUI::WebGUI(xdaq::Application* app,toolbox::fsm::FiniteStateMachine* fsm)
+WebGUI::WebGUI(xdaq::Application* app,StateMachine* fsm)
   : app_(app)
   , fsm_(fsm)
   , log_(app->getApplicationContext()->getLogger())
@@ -410,8 +410,7 @@ void WebGUI::htmlHead(Input_t *in,Output_t* out,CString_t& pageTitle)
 //______________________________________________________________________________
 void WebGUI::htmlHeadline(Input_t *in,Output_t *out,CString_t& link,CString_t& icon)
 {
-  int    state    =fsm_->getCurrentState();
-  string stateName=fsm_->getStateName(state);
+  string stateName=fsm_->stateName()->toString();
   
   *out<<table().set("border","0").set("width","100%")<<endl
       <<tr()<<td().set("align","left")<<endl
