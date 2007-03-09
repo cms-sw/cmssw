d51175ad96a865366c5d7031bbd9a672f7000d67
Emmanuelle Perez <sha1-c810833a342e16d9bed5b24baa568142a413d377@cern.ch>
2007-03-09 07:31:42 +0000

    first version

---
+++ Geometry/EcalMapping/interface/EcalMappingRcd.h
@@ -0,0 +1,10 @@
+#ifndef ECALMAPPINGRCD_H
+#define ECALMAPPINGRCD_H
+                                                                                                                                            
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+                                                                                                                                            
+class EcalMappingRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalMappingRcd> {};
+                                                                                                                                            
+                                                                                                                                            
+#endif
+
---
+++ Geometry/EcalMapping/src/ES_EcalElectronicsMapping.cc
@@ -0,0 +1,6 @@
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "Geometry/EcalMapping/interface/EcalElectronicsMapping.h"
+
+EVENTSETUP_DATA_REG(EcalElectronicsMapping);
+
+
