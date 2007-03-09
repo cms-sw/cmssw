81f87704e1000bdcc8abcee1d603b1c837ee37fd
Kevin.Klapoetke Klapoetke <sha1-00ae6b1fd57e2673af62ea5af6540d64f8b247c9@cern.ch>
2007-03-09 11:40:54 +0000

    Improved version capable of comparing reconstructed ID to DetID and elecID.

--- Calibration/HcalConnectivity/src/HcalCableMapper.cc
+++ Calibration/HcalConnectivity/src/HcalCableMapper.cc
@@ -1,6 +1,6 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
