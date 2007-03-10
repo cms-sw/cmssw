82c5814dd71880afaa245da635e040df042ea27e
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2007-03-10 10:06:35 +0000

    *** empty log message ***

--- JetMETCorrections/Modules/src/JetCorrectionProducer.cc
+++ JetMETCorrections/Modules/src/JetCorrectionProducer.cc
@@ -6,7 +6,7 @@
 #include "JetCorrectionProducer.h"
 
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
