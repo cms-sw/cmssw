d909eb65dcfff046483546bbe238faf2610dc017
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-03-11 02:20:23 +0000

    header fix

--- RecoTBCalo/HcalTBObjectUnpacker/interface/HcalTBObjectUnpacker.h
+++ RecoTBCalo/HcalTBObjectUnpacker/interface/HcalTBObjectUnpacker.h
@@ -17,7 +17,7 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "DataFormats/Common/interface/EDProduct.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
