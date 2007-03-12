0f1812f831dd0b6ef64168aaa662bc2325ba3f48
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-03-12 13:54:37 +0000

    Save Energy deposit due to EM and Hadronic component separately in persistent CaloHit

--- SimDataFormats/CaloHit/interface/PCaloHit.h
+++ SimDataFormats/CaloHit/interface/PCaloHit.h
@@ -5,62 +5,63 @@
 
 // Persistent Calorimeter hit
 
-  class PCaloHit 
-  { 
-  public: 
+class PCaloHit { 
 
-    PCaloHit(float e = 0., float t = 0., int i = 0) : 
-      myEnergy (e), myTime (t), myItra (i) { }
+public: 
 
-    PCaloHit(unsigned int id, float e = 0., float t = 0., int i = 0) : 
-      myEnergy (e), myTime (t), myItra (i), detId(id) { }
+  PCaloHit(float eEM = 0., float eHad=0., float t = 0., int i = 0);
 
+  PCaloHit(unsigned int id, float eEM = 0., float eHad =0., float t = 0., 
+	   int i = 0);
 
-    //Names
-    static const char *name() { return "Hit"; }
+  
+  //Names
+  static const char *name() { return "Hit"; }
 
-    const char * getName() const { return name (); }
+  const char * getName() const { return name (); }
 
-    //Energy deposit of the Hit
-    double energy() const { return myEnergy; }
+  //Energy deposit of the Hit
+  double energy()    const { return myEnergyEM+myEnergyHad; }
+  double energyEM()  const { return myEnergyEM; }
+  double energyHad() const { return myEnergyHad; }
 
-    //Time of the deposit
-    double time() const { return myTime; }
+  //Time of the deposit
+  double time() const { return myTime; }
 
-    //G3 track number
-    int geantTrackId() const { return myItra; }
+  //Geant track number
+  int geantTrackId() const { return myItra; }
 
-    //DetId where the Hit is recorded
-    unsigned int  id() const { return detId; }
+  //DetId where the Hit is recorded
+  unsigned int  id() const { return detId; }
 
-    //Event Id (for signal/pileup discrimination)
+  //Event Id (for signal/pileup discrimination)
 
-    void setEventId(EncodedEventId e) { theEventId = e; }
+  void setEventId(EncodedEventId e) { theEventId = e; }
 
-    EncodedEventId eventId() const {return theEventId;}
-
-
-    //Comparisons
-
-    bool operator<(const PCaloHit &d) const 
-    { return myEnergy < d.myEnergy; }
-
-    //Same Hit (by value)
-
-    bool operator==(const PCaloHit &d) const 
-    { return myEnergy == d.myEnergy && detId == d.detId; }
+  EncodedEventId eventId() const {return theEventId;}
 
 
+  //Comparisons
 
-  protected: 
-    float myEnergy; 
-    float myTime; 
-    int myItra; 
-    unsigned int detId; 
-    EncodedEventId  theEventId;
-  }; 
+  bool operator<(const PCaloHit &d) const 
+  { return (myEnergyEM+myEnergyHad) < (d.myEnergyEM+d.myEnergyHad); }
 
-  #include<iosfwd>
-  std::ostream &operator<<(std::ostream &, const PCaloHit &); 
+  //Same Hit (by value)
+  bool operator==(const PCaloHit &d) const 
+  { return ((myEnergyEM+myEnergyHad) == (d.myEnergyEM+d.myEnergyHad) &&
+	    detId == d.detId); }
+
+
+protected: 
+  float myEnergyEM;
+  float myEnergyHad; 
+  float myTime; 
+  int   myItra; 
+  unsigned int detId; 
+  EncodedEventId  theEventId;
+}; 
+
+#include<iosfwd>
+std::ostream &operator<<(std::ostream &, const PCaloHit &); 
 
 #endif // _SimDataFormats_SimCaloHit_PCaloHit_h_
--- SimDataFormats/CaloHit/src/PCaloHit.cc
+++ SimDataFormats/CaloHit/src/PCaloHit.cc
@@ -1,12 +1,21 @@
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include<iostream>
 
-  std::ostream & operator<<(std::ostream& o,const PCaloHit& hit)
-  {
-    o << "0x"<<std::hex<< hit.id() << std::dec
-      << ": Energy " << hit.energy() << " GeV "
-      << " Tof " << hit.time() << " ns "
-      << " Geant track #" << hit.geantTrackId();
+PCaloHit::PCaloHit(float eEM, float eHad, float t, int i) : myEnergyEM(eEM), 
+							    myEnergyHad(eHad), 
+							    myTime(t), 
+							    myItra(i) { }
 
-    return o;
-  }
+PCaloHit::PCaloHit(unsigned int id, float eEM, float eHad, float t, int i) :
+  myEnergyEM(eEM), myEnergyHad(eHad), myTime(t), myItra(i), detId(id) { }
+
+
+std::ostream & operator<<(std::ostream& o,const PCaloHit& hit)  {
+  o << "0x"<<std::hex<< hit.id() << std::dec
+    << ": Energy (EM) " << hit.energyEM() << " GeV "
+    << ": Energy (Had) " << hit.energyHad() << " GeV "
+    << " Tof " << hit.time() << " ns "
+    << " Geant track #" << hit.geantTrackId();
+
+  return o;
+}
