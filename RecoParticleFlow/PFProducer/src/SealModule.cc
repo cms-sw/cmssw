7916ff748d1cb3d0b0ac32395dc2dae28bdbf372
Joanna Weng <sha1-e1d5fb97f74876fe25d59750eed1d5611df817d0@cern.ch>
2007-03-12 10:40:11 +0000

    added PFJetCandidateCreator

--- RecoParticleFlow/PFProducer/src/SealModule.cc
+++ RecoParticleFlow/PFProducer/src/SealModule.cc
@@ -1,13 +1,14 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
-
 #include "RecoParticleFlow/PFProducer/interface/PFProducer.h"
 #include "RecoParticleFlow/PFProducer/interface/TauHadronDecayFilter.h"
 #include "RecoParticleFlow/PFProducer/interface/EFilter.h"
+#include "RecoParticleFlow/PFProducer/interface/PFJetCandidateCreator.h"
 
 DEFINE_SEAL_MODULE();
+
 DEFINE_ANOTHER_FWK_MODULE(PFProducer);
 DEFINE_ANOTHER_FWK_MODULE(TauHadronDecayFilter);
 DEFINE_ANOTHER_FWK_MODULE(EFilter);
+DEFINE_ANOTHER_FWK_MODULE(PFJetCandidateCreator);
