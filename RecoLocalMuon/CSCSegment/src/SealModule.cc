057c0db0002e0be20f414c00a014ec299d6b555e
Ingo Bloch <sha1-424bb6d4ccefa9b8e891c94b5da98cc3068377ed@cern.ch>
2007-03-13 15:47:11 +0000

    add the ST segment finder

--- RecoLocalMuon/CSCSegment/src/SealModule.cc
+++ RecoLocalMuon/CSCSegment/src/SealModule.cc
@@ -7,10 +7,12 @@
 #include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoSK.h>
 #include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoTC.h>
 #include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoDF.h>
+#include <RecoLocalMuon/CSCSegment/src/CSCSegAlgoST.h>
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCSegmentProducer);
 DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoSK, "CSCSegAlgoSK");
 DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoTC, "CSCSegAlgoTC");
 DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoDF, "CSCSegAlgoDF");
+DEFINE_SEAL_PLUGIN(CSCSegmentBuilderPluginFactory, CSCSegAlgoST, "CSCSegAlgoST");
 
