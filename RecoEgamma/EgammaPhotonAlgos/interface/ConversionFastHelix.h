48942beb7d00e62b9ab8971850d6a17398227392
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-03-14 20:30:55 +0000

    Method isValid returns now the state of the Helix

--- RecoEgamma/EgammaPhotonAlgos/interface/ConversionFastHelix.h
+++ RecoEgamma/EgammaPhotonAlgos/interface/ConversionFastHelix.h
@@ -11,17 +11,6 @@
 
 /**
    Generation of track parameters at a vertex using two hits and a vertex.
-   It is used e.g. by a seed generator.
-
-   21.02.2001: Old FastHelix is now called FastHelixFit. Replace FastLineFit
-               by FastLine (z0, dz/drphi calculated without vertex and errors)
-   14.02.2001: Replace general Circle by FastCircle.
-   13.02.2001: LinearFitErrorsInTwoCoordinates replaced by FastLineFit
-   29.11.2000: (Pascal Vanlaer) Modification of calculation of sign of px,py
-               and change in calculation of pz, z0.
-   29.11.2000: (Matthias Winkler) Split stateAtVertex() in two parts (Circle 
-               is valid or not): helixStateAtVertex() and 
-	                         straightLineStateAtVertex()
  */
 
 class ConversionFastHelix {
@@ -35,25 +24,32 @@ public:
   ConversionFastHelix(const GlobalPoint& outerHit, 
 	    const GlobalPoint& middleHit,
 	    const GlobalPoint& aVertex,
-	    const MagneticField* field);
+	    const MagneticField* field
+            );
   
   ~ConversionFastHelix() {}
+
+  void makeHelix();
   
-  bool isValid() const {return theCircle.isValid();}
 
-  FTS stateAtVertex() const;
+  bool isValid() {return validStateAtVertex;  }
 
-  FTS helixStateAtVertex() const;
+  FTS stateAtVertex() ;
 
-  FTS straightLineStateAtVertex() const;
+  FTS helixStateAtVertex() ;
+
+  FTS straightLineStateAtVertex() ;
 
 private:
-  
+
+  FTS theHelix_; 
+  bool validStateAtVertex; 
   GlobalPoint theOuterHit;
   GlobalPoint theMiddleHit;
   GlobalPoint theVertex;
   FastCircle theCircle;
   const MagneticField* mField;
+ 
 };
 
 #endif //Egamma_ConversionFastHelix_H_
