3a16a9adcd110dc22b2b8989e58745ef61e748ad
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-15 17:11:21 +0000

    significant updates to key classes

---
+++ DataFormats/SiStripCommon/src/SiStripNullKey.cc
@@ -0,0 +1,83 @@
+// Last commit: $Id: $
+// Latest tag:  $Name: $
+// Location:    $Source: $
+
+#include "DataFormats/SiStripCommon/interface/SiStripNullKey.h"
+#include "DataFormats/SiStripCommon/interface/SiStripEnumsAndStrings.h"
+#include <iomanip>
+#include <sstream>
+
+// -----------------------------------------------------------------------------
+// 
+SiStripNullKey::SiStripNullKey() : SiStripKey() {;}
+
+// -----------------------------------------------------------------------------
+// 
+bool SiStripNullKey::isEqual( const SiStripKey& input ) const {
+  SiStripKey& temp = const_cast<SiStripKey&>(input);
+  if ( &dynamic_cast<SiStripNullKey&>(temp) ) { return true; }
+  else { return false; }
+}
+
+// -----------------------------------------------------------------------------
+// 
+bool SiStripNullKey::isConsistent( const SiStripKey& input ) const {
+  return isEqual(input);
+}
+
+// -----------------------------------------------------------------------------
+//
+bool SiStripNullKey::isValid() const { 
+  return false;
+}
+
+// -----------------------------------------------------------------------------
+//
+bool SiStripNullKey::isValid( const sistrip::Granularity& gran ) const {
+  return false; 
+}
+
+// -----------------------------------------------------------------------------
+//
+bool SiStripNullKey::isInvalid() const { 
+  return true;
+}
+
+// -----------------------------------------------------------------------------
+//
+bool SiStripNullKey::isInvalid( const sistrip::Granularity& gran ) const {
+  return true;
+}
+
+// -----------------------------------------------------------------------------
+// 
+void SiStripNullKey::initFromValue() {;}
+
+// -----------------------------------------------------------------------------
+//
+void SiStripNullKey::initFromKey() {;}
+
+// -----------------------------------------------------------------------------
+// 
+void SiStripNullKey::initFromPath() {;}
+
+// -----------------------------------------------------------------------------
+// 
+void SiStripNullKey::initGranularity() {;}
+
+// -----------------------------------------------------------------------------
+//
+std::ostream& operator<< ( std::ostream& os, const SiStripNullKey& input ) {
+  return os << "[SiStripNullKey::print]" << std::endl
+	    << std::hex
+	    << " 32-bit key  : 0x" 
+	    << std::setfill('0') 
+	    << std::setw(8) << input.key() << std::endl
+	    << std::setfill(' ') 
+	    << std::dec
+	    << " Directory   : " << input.path() << std::endl
+	    << " Granularity : "
+	    << SiStripEnumsAndStrings::granularity( input.granularity() ) << std::endl
+ 	    << " Channel     : " << input.channel() << std::endl
+	    << " isValid    : " << input.isValid();
+}
