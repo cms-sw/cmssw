0977d02f1a1465d75f04c8e1c87330d64155b250
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-15 17:04:42 +0000

    re-organisation of constants and enumerated types

---
+++ DataFormats/SiStripCommon/interface/ConstantsForSummaryPlots.h
@@ -0,0 +1,9 @@
+#ifndef DataFormats_SiStripCommon_ConstantsForSummaryPlots_H
+#define DataFormats_SiStripCommon_ConstantsForSummaryPlots_H
+
+#include "DataFormats/SiStripCommon/interface/ConstantsForMonitorable.h"
+#include "DataFormats/SiStripCommon/interface/ConstantsForPresentation.h"
+  
+#endif // DataFormats_SiStripCommon_ConstantsForSummaryPlots_H
+
+
