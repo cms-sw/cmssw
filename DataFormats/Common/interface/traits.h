5ed00c9751d4156b9e51b402cb80e0af4a6050e0
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2007-03-15 13:32:24 +0000

    Add key_traits struct template and simple tests

--- DataFormats/Common/interface/traits.h
+++ DataFormats/Common/interface/traits.h
@@ -5,11 +5,12 @@
 
 Definition of traits templates used in the EDM.  
 
-$Id: traits.h,v 1.11 2007/01/23 00:25:52 wmtan Exp $
+$Id: traits.h,v 1.12 2007/02/07 23:01:43 paterno Exp $
 
 ----------------------------------------------------------------------*/
 
 #include <deque>
+#include <limits>
 #include <list>
 #include <map>
 #include <set>
@@ -20,6 +21,43 @@ $Id: traits.h,v 1.11 2007/01/23 00:25:52 wmtan Exp $
 
 namespace edm
 {
+  //------------------------------------------------------------
+  //
+  // The trait struct template key_traits<K> is used to carry
+  // information relevant to the type K when used as a 'key' in
+  // RefVector and its related classes and templates.
+  //
+  // The general case works only for integral types K; for more
+  // 'esoteric' types, one must introduce an explicit specialization.
+  // That specialization must initialize the static data member
+  // 'value'.
+
+  template <class K>
+  struct key_traits
+  {
+    typedef K key_type;
+    static const key_type value;
+  };
+
+  template <class K> 
+  typename  key_traits<K>::key_type const
+  key_traits<K>::value =
+    std::numeric_limits<typename key_traits<K>::key_type>::max();
+
+  // If we ever need to support instantiations of std::basic_string
+  // other than std::string, this is the place to do it.
+
+  // For value, we make a 1-character long string that contains an
+  // unprintable character; we are hoping nobody ever uses such a
+  // string as a legal key.
+  template <> 
+  struct key_traits<std::string>
+  {
+    typedef std::string key_type;
+    static const key_type value;
+  };
+
+
   //------------------------------------------------------------
   //
   // DoNotSortUponInsertion is a base class. Derive your own class X
