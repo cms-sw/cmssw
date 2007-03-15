67e0912cb73beba92967b960a4d1fcfe85c7264d
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-15 17:12:19 +0000

    changes to root dict for new key classes

--- DataFormats/SiStripCommon/src/classes.h
+++ DataFormats/SiStripCommon/src/classes.h
@@ -1,3 +1,20 @@
+#ifndef DataFormats_SiStripCommon_Classes_H
+#define DataFormats_SiStripCommon_Classes_H
+
+#include "DataFormats/Common/interface/Wrapper.h"
+
 #include "DataFormats/SiStripCommon/interface/SiStripFecKey.h"
 #include "DataFormats/SiStripCommon/interface/SiStripFedKey.h"
 #include "DataFormats/SiStripCommon/interface/SiStripDetKey.h"
+#include "DataFormats/SiStripCommon/interface/SiStripNullKey.h"
+
+namespace { 
+  namespace { 
+    // edm::Wrapper< SiStripFecKey > fec;
+    // edm::Wrapper< SiStripFedKey > fed;
+    // edm::Wrapper< SiStripDetKey > det;
+    // edm::Wrapper< SiStripNullKey > null;
+  }
+}
+
+#endif // DataFormats_SiStripCommon_Classes_H
