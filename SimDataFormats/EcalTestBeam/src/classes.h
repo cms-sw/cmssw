81164721b25f82e800013fadc85ef72e6257a484
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2007-03-16 19:36:13 +0000

    Update for 1_2_0 with new algorithmic geometry and beam line

--- SimDataFormats/EcalTestBeam/src/classes.h
+++ SimDataFormats/EcalTestBeam/src/classes.h
@@ -1,4 +1,6 @@
 #include "SimDataFormats/EcalTestBeam/interface/PEcalTBInfo.h"
+#include "SimDataFormats/EcalTestBeam/interface/HodoscopeDetId.h"
+
 #include "DataFormats/Common/interface/Wrapper.h"
 #include <vector>
 
