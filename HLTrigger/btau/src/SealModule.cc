9f871fedf7c237e4a9c6579b33d09bda25d187cd
wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch>
2007-03-16 14:49:57 +0000

    added DisplacedmumuFilter

--- HLTrigger/btau/src/SealModule.cc
+++ HLTrigger/btau/src/SealModule.cc
@@ -3,7 +3,11 @@
 
 #include "HLTrigger/btau/interface/HLTJetTag.h"
 #include "HLTrigger/btau/interface/HLTTauL25DoubleFilter.h"
+#include "HLTrigger/btau/interface/HLTDisplacedmumuFilter.h"
+#include "HLTrigger/btau/interface/CandidateFromHLTFOWRProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTJetTag);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauL25DoubleFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTDisplacedmumuFilter);
+DEFINE_ANOTHER_FWK_MODULE(CandidateFromHLTFOWRProducer);
