4a8aa512d89918e52d331cd9b4e2f77353c4aa9c
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2007-03-16 13:51:29 +0000

    First commit of L1Trigger records

---
+++ CondFormats/DataRecord/interface/L1EmEtScaleRcd.h
@@ -0,0 +1,26 @@
+#ifndef L1Scales_L1EmEtScaleRcd_h
+#define L1Scales_L1EmEtScaleRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     L1EmEtScaleRcd
+// 
+/**\class L1EmEtScaleRcd L1EmEtScaleRcd.h CondFormats/DataRecord/interface/L1EmEtScaleRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Jim Brooke
+// Created:     Wed Oct  4 16:49:43 CEST 2006
+// $Id: 
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1EmEtScaleRcd : public edm::eventsetup::EventSetupRecordImplementation<L1EmEtScaleRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/interface/L1JetEtScaleRcd.h
@@ -0,0 +1,26 @@
+#ifndef L1Scales_L1JetEtScaleRcd_h
+#define L1Scales_L1JetEtScaleRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     L1JetEtScaleRcd
+// 
+/**\class L1JetEtScaleRcd L1JetEtScaleRcd.h CondFormats/DataRecord/interface/L1JetEtScaleRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Wed Oct  4 16:49:43 CEST 2006
+// $Id: L1JetEtScaleRcd.h,v 1.3 2006/10/06 12:07:28 jbrooke Exp $
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1JetEtScaleRcd : public edm::eventsetup::EventSetupRecordImplementation<L1JetEtScaleRcd> {};
+
+#endif
