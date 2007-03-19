e900ffc150cec0f8fe390b95f6e5850554e276f1
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-19 09:44:47 +0000

    added logger category

--- DataFormats/SiStripCommon/interface/ConstantsForLogger.h
+++ DataFormats/SiStripCommon/interface/ConstantsForLogger.h
@@ -10,17 +10,18 @@
 
 namespace sistrip { 
   
-  static const std::string mlDigis_        = "SiStripDigis";
-  static const std::string mlCabling_      = "SiStripCabling";
-  static const std::string mlConfigDb_     = "SiStripConfigDb";
-  static const std::string mlRawToDigi_    = "SiStripRawToDigi";
-  static const std::string mlRawToCluster_ = "SiStripRawToCluster";
-  static const std::string mlDqmCommon_    = "SiStripDqmCommon";
-  static const std::string mlDqmSource_    = "SiStripDqmSource";
-  static const std::string mlDqmClient_    = "SiStripDqmClient";
-  static const std::string mlInputSource_  = "SiStripInputSource";
-  static const std::string mlESSources_    = "SiStripESSources";
-  static const std::string mlO2O_          = "SiStripO2O";
+  static const std::string mlCabling_       = "SiStripCabling";
+  static const std::string mlCommissioning_ = "SiStripCommissioning";
+  static const std::string mlConfigDb_      = "SiStripConfigDb";
+  static const std::string mlDigis_         = "SiStripDigis";
+  static const std::string mlDqmCommon_     = "SiStripDqmCommon";
+  static const std::string mlDqmClient_     = "SiStripDqmClient";
+  static const std::string mlDqmSource_     = "SiStripDqmSource";
+  static const std::string mlESSources_     = "SiStripESSources";
+  static const std::string mlInputSource_   = "SiStripInputSource";
+  static const std::string mlO2O_           = "SiStripO2O";
+  static const std::string mlRawToCluster_  = "SiStripRawToCluster";
+  static const std::string mlRawToDigi_     = "SiStripRawToDigi";
 
 }
 
