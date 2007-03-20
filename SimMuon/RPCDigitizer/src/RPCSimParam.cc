c49deecdcb8ae749ec44547126a7a624389ad19d
Raffaello Trentadue <sha1-9504fe7d3bbc4a07e3333b20772b63251c823b73@cern.ch>
2007-03-20 10:01:11 +0000

    Real Cluster size added

--- SimMuon/RPCDigitizer/src/RPCSimParam.cc
+++ SimMuon/RPCDigitizer/src/RPCSimParam.cc
@@ -9,6 +9,14 @@
 #include <CLHEP/Random/RandGaussQ.h>
 #include <CLHEP/Random/RandFlat.h>
 
+#include<cstring>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<stdlib.h>
+
+using namespace std;
 
 
 RPCSimParam::RPCSimParam(const edm::ParameterSet& config) : RPCSim(config){
@@ -31,12 +39,109 @@ RPCSimParam::RPCSimParam(const edm::ParameterSet& config) : RPCSim(config){
     std::cout <<"Signal propagation time   = "<<sspeed<<" x c"<<std::endl;
     std::cout <<"Link Board Gate Width     = "<<lbGate<<" ns"<<std::endl;
   }
+
+  string ifile="ClSizeTot.dat";
+
+  MyOutput1 = new fstream("clsizeOut1.dat",ios::out);
+  MyOutput2 = new fstream("clsizeOut2.dat",ios::out);
+  MyOutput3 = new fstream("clsizeOut3.dat",ios::out);
+
+  infile = new ifstream(ifile.c_str(), ios::in);
+  if(! *infile) {
+    cerr << "error: unable to open input file: "
+	 <<  ifile  << endl;
+    //    return -1;
+  }
+
+  string buffer;
+  double sum = 0;
+  unsigned int counter = 1;
+  int row = 1;
+  std::vector<double> sum_clsize;
+
+  while ( *infile >> buffer ) {
+    const char *buffer1 = buffer.c_str();
+    double dato = atof(buffer1);
+    sum += dato;
+    sum_clsize.push_back(sum);
+
+    std::cout<<dato<<std::endl;
+    
+    if(counter == row*20) {
+
+      for(int i = 0; i < sum_clsize.size(); ++i){
+	std::cout<<sum_clsize[i]<<std::endl;
+      }
+
+      clsMap[row] = sum_clsize;
+      row++;
+      sum = 0;
+      sum_clsize.clear();
+    }
+    counter++;
+  }
+  std::cout<<"Somma = "<<(clsMap[1])[0]<<"  "<<(clsMap[2])[0]<<"  "<<(clsMap[3])[0]<<"  "<<(clsMap[4])[0]<<std::endl;
+
+}
+
+int RPCSimParam::getClSize(float posX)
+{
+
+// //     string ifile = getenv("ORCA_DATA_PATH");
+// //     int pos = ifile.find_first_of(':');
+// //     if (pos != string::npos){
+// //       ifile.erase(pos, ifile.size() -1);
+// //       ifile=ifile + "/Param_Clust/ClSize_tot_9600.00.dat";
+// //     }
+
+  int cnt = 1;
+  int min = 1;
+  int max = 1;
+  double func;
+  std::vector<double> sum_clsize;
+
+  double rr_cl = RandFlat::shoot();
+  if(0.0 <= posX && posX < 0.2)  {
+    func = (clsMap[1])[(clsMap[1]).size()-1]*(rr_cl);
+    sum_clsize = clsMap[1];
+  }
+  if(0.2 <= posX && posX < 0.4) {
+    func = (clsMap[2])[(clsMap[2]).size()-1]*(rr_cl);
+    sum_clsize = clsMap[2];
+  }
+  if(0.4 <= posX && posX < 0.6) {
+    func = (clsMap[3])[(clsMap[3]).size()-1]*(rr_cl);
+    sum_clsize = clsMap[3];
+  }
+  if(0.6 <= posX && posX < 0.8) {
+    func = (clsMap[4])[(clsMap[4]).size()-1]*(rr_cl);
+    sum_clsize = clsMap[4];
+  }
+  if(0.8 <= posX && posX < 1.0)  {
+    func = (clsMap[5])[(clsMap[5]).size()-1]*(rr_cl);
+    sum_clsize = clsMap[5];
+  }
+  cout<<"rr_cl = "<<rr_cl<<"  "<<"SumCl = "<<(clsMap[1])[(clsMap[1]).size()-1]<<"  "<<"func = "<<func<<endl;
+  
+  for(vector<double>::iterator iter = sum_clsize.begin();
+      iter != sum_clsize.end(); ++iter){
+      cnt++;
+      if(func > (*iter)){
+        min = cnt;
+      }
+      else if(func < (*iter)){
+        max = cnt;
+        break;
+      }
+  }
+  //  *MyOutput<<min<<endl;
+  return min;
 }
 
 
 void
 RPCSimParam::simulate(const RPCRoll* roll,
-		       const edm::PSimHitContainer& rpcHits )
+		      const edm::PSimHitContainer& rpcHits )
 {
   const Topology& topology=roll->specs()->topology();
   for (edm::PSimHitContainer::const_iterator _hit = rpcHits.begin();
@@ -47,6 +152,8 @@ RPCSimParam::simulate(const RPCRoll* roll,
     const LocalPoint& entr=_hit->entryPoint();
     //    const LocalPoint& exit=_hit->exitPoint();
 
+    float posX = roll->strip(_hit->localPosition()) - static_cast<int>(roll->strip(_hit->localPosition()));
+
     // Effinciecy
     if (RandFlat::shoot() < aveEff) {
       int centralStrip = topology.channel(entr)+1;  
@@ -55,7 +162,16 @@ RPCSimParam::simulate(const RPCRoll* roll,
       // Compute the cluster size
       double w = RandFlat::shoot();
       if (w < 1.e-10) w=1.e-10;
-      int clsize = static_cast<int>( -1.*aveCls*log(w)+1.);
+      int clsize = RPCSimParam::getClSize(posX);//static_cast<int>( -1.*aveCls*log(w)+1.);
+
+      //---------------------VERIFICA------------------------------
+
+      if(clsize == 1) *MyOutput1<<posX<<endl;
+      if(clsize == 2) *MyOutput2<<posX<<endl;
+      if(clsize == 3) *MyOutput3<<posX<<endl;
+
+      //-----------------------------------------------------------
+
       std::vector<int> cls;
       cls.push_back(centralStrip);
       if (clsize > 1){
