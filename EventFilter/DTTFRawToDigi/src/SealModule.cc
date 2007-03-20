e5df2dcc31862448bf1152ccae04cbbf1bf5a62c
Jorge Fernandez De Troconiz <sha1-75dae295003aa1cfa4337639ae3380ba6ad18aa2@cern.ch>
2007-03-20 12:18:42 +0000

    Package doc and PluginManager transition

--- EventFilter/DTTFRawToDigi/src/SealModule.cc
+++ EventFilter/DTTFRawToDigi/src/SealModule.cc
@@ -1,4 +1,4 @@
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "EventFilter/DTTFRawToDigi/interface/DTTFFEDReader.h"
 #include "EventFilter/DTTFRawToDigi/interface/DTTFFEDSim.h"
