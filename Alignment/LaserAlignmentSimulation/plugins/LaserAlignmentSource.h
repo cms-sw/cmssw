561c58aa58edf8b3388f80d6c3d3c38dfcbc124e
mthomas <sha1-c3ddb5d317a38815e0d99b0f7ef83c94092833ba@cern.ch>
2007-03-20 12:00:32 +0000

    made changes due to the use of new HepMC version

--- Alignment/LaserAlignmentSimulation/plugins/LaserAlignmentSource.h
+++ Alignment/LaserAlignmentSimulation/plugins/LaserAlignmentSource.h
@@ -1,11 +1,15 @@
-/* 
- * Source to be used for the Simulation of the Laser Alignment System
- * an empty MCHepEvent will be generated (needed by OscarProducer). The actual simulation of 
- * the laser beams is done in the SimWatcher attached to OscarProducer
- */
-
 #ifndef LaserAlignmentSimulation_LaserAlignmentSource_H
 #define LaserAlignmentSimulation_LaserAlignmentSource_H
+
+/** \class LaserAlignmentSource
+ *  Source to be used for the Simulation of the Laser Alignment System
+ *  an empty MCHepEvent will be generated (needed by OscarProducer). The actual simulation of 
+ *  the laser beams is done in the SimWatcher attached to OscarProducer
+ *
+ *  $Date: Mon Mar 19 12:26:06 CET 2007 $
+ *  $Revision: 1.1 $
+ *  \author Maarten Thomas
+ */
 
 // system include files
 #include <memory>
@@ -16,19 +20,21 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "CLHEP/HepMC/GenEvent.h"
+#include "HepMC/GenEvent.h"
 
 //
 // class decleration
 //
-
 class LaserAlignmentSource : public edm::GeneratedInputSource {
  public:
+	/// constructor
   explicit LaserAlignmentSource(const edm::ParameterSet&, const edm::InputSourceDescription&);
+	/// destructor
   ~LaserAlignmentSource();
 
   
  private:
+	/// produce the HepMCProduct
   virtual bool produce(edm::Event&);
   
   // the event format itself
