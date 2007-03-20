b02b9bb6034d516891701fdffe370f426508e098
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-03-20 17:34:31 +0000

    Add option of "ALL" detectors -- but using the same cuts for all!

--- SimCalorimetry/HcalZeroSuppressionProducers/src/SealModule.cc
+++ SimCalorimetry/HcalZeroSuppressionProducers/src/SealModule.cc
@@ -1,6 +1,6 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimCalorimetry/HcalZeroSuppressionProducers/interface/HcalSimpleAmplitudeZS.h"
+#include "SimCalorimetry/HcalZeroSuppressionProducers/src/HcalSimpleAmplitudeZS.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalSimpleAmplitudeZS);
