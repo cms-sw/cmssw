988447e1bda324029afb50db6c64e2bb7d212e32
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-21 16:32:23 +0000

    added sistripeventsummary object to this package from sistripdigi package

--- DataFormats/SiStripCommon/src/classes.h
+++ DataFormats/SiStripCommon/src/classes.h
@@ -7,13 +7,22 @@
 #include "DataFormats/SiStripCommon/interface/SiStripFedKey.h"
 #include "DataFormats/SiStripCommon/interface/SiStripDetKey.h"
 #include "DataFormats/SiStripCommon/interface/SiStripNullKey.h"
-
 namespace { 
   namespace { 
     // edm::Wrapper< SiStripFecKey > fec;
     // edm::Wrapper< SiStripFedKey > fed;
     // edm::Wrapper< SiStripDetKey > det;
     // edm::Wrapper< SiStripNullKey > null;
+  }
+}
+
+#include "DataFormats/SiStripCommon/interface/SiStripEventSummary.h"
+namespace {
+  namespace {
+    edm::Wrapper< sistrip::RunType > run_type; 
+    edm::Wrapper< sistrip::ApvReadoutMode > apv_mode; 
+    edm::Wrapper< sistrip::FedReadoutMode > fed_mode; 
+    edm::Wrapper< SiStripEventSummary > summary;
   }
 }
 
