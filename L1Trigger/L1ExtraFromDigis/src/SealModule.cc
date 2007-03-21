1751d50457a64e4df274d467a6fd1f920e84d2a3
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2007-03-21 20:16:04 +0000

    PluginManager transition.

--- L1Trigger/L1ExtraFromDigis/src/SealModule.cc
+++ L1Trigger/L1ExtraFromDigis/src/SealModule.cc
@@ -1,4 +1,4 @@
-#include "PluginManager/ModuleDef.h"
+//#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "L1Trigger/L1ExtraFromDigis/interface/L1ExtraParticlesProd.h"
 #include "L1Trigger/L1ExtraFromDigis/interface/L1ExtraParticleMapProd.h"
