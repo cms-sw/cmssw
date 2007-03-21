435aaa3b27d2b62edd13151869f595170631ded2
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2007-03-21 15:07:11 +0000

    Plugin Manager Transition

--- L1Trigger/DTTrigger/src/SealModule.cc
+++ L1Trigger/DTTrigger/src/SealModule.cc
@@ -1,4 +1,4 @@
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "L1Trigger/DTTrigger/interface/DTTrigTest.h"
 #include "L1Trigger/DTTrigger/interface/DTTrigProd.h"
