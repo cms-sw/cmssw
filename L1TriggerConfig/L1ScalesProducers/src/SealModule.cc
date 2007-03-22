25b59bbcdffe7ad351eb8783cc1cde2d4e421fbb
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2007-03-22 17:54:11 +0000

    Plugin manager updates

--- L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
+++ L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
@@ -1,9 +1,8 @@
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTrivialProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTester.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1ScalesTrivialProducer);
+DEFINE_FWK_EVENTSETUP_MODULE(L1ScalesTrivialProducer);
 DEFINE_ANOTHER_FWK_MODULE(L1ScalesTester);
