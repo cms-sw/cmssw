e8b4f5b6c25f13ea10e12ce2cf41ff92b37ff772
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-03-22 16:41:37 +0000

    remove analyzer modules (moved to RecoEgamma/Examples)

--- RecoEgamma/EgammaElectronProducers/src/SealModule.cc
+++ RecoEgamma/EgammaElectronProducers/src/SealModule.cc
@@ -5,15 +5,10 @@
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/SiStripElectronProducer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/SiStripElectronAssociator.h"
-#include "RecoEgamma/EgammaElectronProducers/interface/SiStripElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronProducer.h"
-#include "RecoEgamma/EgammaElectronProducers/interface/ElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedProducer.h"
-#include "RecoEgamma/EgammaElectronProducers/interface/ElectronPixelSeedAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronProducer.h"
-#include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchGsfElectronProducer.h"
-#include "RecoEgamma/EgammaElectronProducers/interface/PixelMatchGsfElectronAnalyzer.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/TrackProducerWithSeedAssoc.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/GsfTrackProducerWithSeedAssoc.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/CkfTrackCandidateMakerWithSeedAssoc.h"
@@ -27,14 +22,9 @@ DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(CkfTrajectoryBuilderWithSeedAssocESProducer
 DEFINE_ANOTHER_FWK_MODULE(ElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAssociator);
-DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedProducer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronProducer);
-DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronProducer);
-DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(TrackProducerWithSeedAssoc);
 DEFINE_ANOTHER_FWK_MODULE(GsfTrackProducerWithSeedAssoc);
 DEFINE_ANOTHER_FWK_MODULE(CkfTrackCandidateMakerWithSeedAssoc);
