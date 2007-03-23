ee25883e6dfbaafc2c2498a5d26707bf397f3066
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-03-23 10:20:10 +0000

    added GMT records from Ivan

---
+++ CondFormats/DataRecord/interface/L1MuGMTScalesRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormatsDataRecord_L1MuGMTScalesRcd_h
+#define CondFormatsDataRecord_L1MuGMTScalesRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1MuGMTScalesRcd : public edm::eventsetup::EventSetupRecordImplementation<L1MuGMTScalesRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/interface/L1MuTriggerScalesRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormatsDataRecord_L1MuTriggerScalesRcd_h
+#define CondFormatsDataRecord_L1MuTriggerScalesRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1MuTriggerScalesRcd : public edm::eventsetup::EventSetupRecordImplementation<L1MuTriggerScalesRcd> {};
+
+#endif
