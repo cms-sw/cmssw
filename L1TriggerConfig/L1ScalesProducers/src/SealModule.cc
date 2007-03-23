62d307c9cc1224868b820c295c75f7f10a8b28e9
Ivan Mikulec <sha1-8d77057744976da3e68d7e182804494ea5541838@cern.ch>
2007-03-23 15:43:08 +0000

    add muon scales

--- L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
+++ L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
@@ -2,7 +2,14 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTrivialProducer.h"
+#include "L1TriggerConfig/L1ScalesProducers/interface/L1MuTriggerScalesProducer.h"
+#include "L1TriggerConfig/L1ScalesProducers/interface/L1MuGMTScalesProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTester.h"
+#include "L1TriggerConfig/L1ScalesProducers/interface/L1MuScalesTester.h"
 
 DEFINE_FWK_EVENTSETUP_MODULE(L1ScalesTrivialProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuTriggerScalesProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuGMTScalesProducer);
+
 DEFINE_ANOTHER_FWK_MODULE(L1ScalesTester);
+DEFINE_ANOTHER_FWK_MODULE(L1MuScalesTester);
