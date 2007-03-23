04eb0133cd10e2c169da279dfea3a99068c255fe
Ivan Mikulec <sha1-8d77057744976da3e68d7e182804494ea5541838@cern.ch>
2007-03-23 15:22:12 +0000

    add muon scales

--- CondFormats/L1TObjects/src/classes.h
+++ CondFormats/L1TObjects/src/classes.h
@@ -1,2 +1,4 @@
+#include "CondFormats/L1TObjects/interface/L1MuTriggerScales.h"
+#include "CondFormats/L1TObjects/interface/L1MuGMTScales.h"
 #include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
 #include "CondFormats/L1TObjects/interface/L1GctJetEtCalibrationFunction.h"
