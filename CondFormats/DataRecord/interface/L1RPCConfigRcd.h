f0f9aed662027a803a67ffa53f7da0603515771c
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2007-03-23 14:36:42 +0000

    First submission.

---
+++ CondFormats/DataRecord/interface/L1RPCConfigRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_L1RPCConfigRcd_h
+#define DataRecord_L1RPCConfigRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     L1RPCConfigRcd
+// 
+/**\class L1RPCConfigRcd L1RPCConfigRcd.h CondFormats/DataRecord/interface/L1RPCConfigRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Tue Mar 20 14:39:09 CET 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1RPCConfigRcd : public edm::eventsetup::EventSetupRecordImplementation<L1RPCConfigRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/L1RPCConfigRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     L1RPCConfigRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Tue Mar 20 14:39:09 CET 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/L1RPCConfigRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(L1RPCConfigRcd);
