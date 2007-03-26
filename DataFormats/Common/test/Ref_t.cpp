128bae2c6226450b40b81c49ae506f4b278a7eca
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2007-03-26 16:06:02 +0000

    Enhance SimpleEDProductGetter and provide test for dereferencing edm::Ref

--- DataFormats/Common/test/Ref_t.cpp
+++ DataFormats/Common/test/Ref_t.cpp
@@ -7,6 +7,8 @@
 
 #include "SimpleEDProductGetter.h"
 
+#include <iostream>
+
 class TestRef: public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(TestRef);
@@ -16,7 +18,9 @@ class TestRef: public CppUnit::TestFixture
   CPPUNIT_TEST_SUITE_END();
 
  public:
-  typedef edm::Ref<std::vector<int> > ref_t;
+  typedef std::vector<int>        product_t;
+  typedef edm::Wrapper<product_t> wrapper_t;
+  typedef edm::Ref<product_t>     ref_t;
 
   TestRef() { } 
   ~TestRef() {}
@@ -34,38 +38,56 @@ CPPUNIT_TEST_SUITE_REGISTRATION(TestRef);
 
 void TestRef::default_ctor_without_active_getter()
 {
-  ref_t  default_refcore;
-  CPPUNIT_ASSERT(default_refcore.isNull());
-  CPPUNIT_ASSERT(default_refcore.isNonnull()==false);
-  CPPUNIT_ASSERT(!default_refcore);
-  CPPUNIT_ASSERT(default_refcore.productGetter()==0);
-  CPPUNIT_ASSERT(default_refcore.id().isValid()==false);
+  ref_t  default_ref;
+  CPPUNIT_ASSERT(default_ref.isNull());
+  CPPUNIT_ASSERT(default_ref.isNonnull()==false);
+  CPPUNIT_ASSERT(!default_ref);
+  CPPUNIT_ASSERT(default_ref.productGetter()==0);
+  CPPUNIT_ASSERT(default_ref.id().isValid()==false);
 }
 
 void TestRef::default_ctor_with_active_getter()
 {
   SimpleEDProductGetter getter;
   edm::EDProductGetter::Operate op(&getter);
-  ref_t  default_refcore;
-  CPPUNIT_ASSERT(default_refcore.isNull());
-  CPPUNIT_ASSERT(default_refcore.isNonnull()==false);
-  CPPUNIT_ASSERT(!default_refcore);
-  CPPUNIT_ASSERT(default_refcore.productGetter()==&getter);
-  CPPUNIT_ASSERT(default_refcore.id().isValid()==false);
+  ref_t  default_ref;
+  CPPUNIT_ASSERT(default_ref.isNull());
+  CPPUNIT_ASSERT(default_ref.isNonnull()==false);
+  CPPUNIT_ASSERT(!default_ref);
+  CPPUNIT_ASSERT(default_ref.productGetter()==&getter);
+  CPPUNIT_ASSERT(default_ref.id().isValid()==false);
+  CPPUNIT_ASSERT_THROW(default_ref.operator->(), edm::Exception);
+  CPPUNIT_ASSERT_THROW(*default_ref, edm::Exception);
 }
 
 void TestRef::nondefault_ctor()
 {
   SimpleEDProductGetter getter;
+  
   edm::EDProductGetter::Operate op(&getter);
   edm::ProductID id(201U);
   CPPUNIT_ASSERT(id.isValid());
 
-  ref_t  refcore(id, 0, &getter);
-  CPPUNIT_ASSERT(refcore.isNull()==false);
-  CPPUNIT_ASSERT(refcore.isNonnull());
-  CPPUNIT_ASSERT(!!refcore);
-  CPPUNIT_ASSERT(refcore.productGetter()==&getter);
-  CPPUNIT_ASSERT(refcore.id().isValid());
+  std::auto_ptr<product_t> prod(new product_t);
+  prod->push_back(1);
+  prod->push_back(2);
+  getter.addProduct(id, prod);
+
+
+  ref_t  ref0(id, 0, &getter);
+  CPPUNIT_ASSERT(ref0.isNull()==false);
+  CPPUNIT_ASSERT(ref0.isNonnull());
+  CPPUNIT_ASSERT(!!ref0);
+  CPPUNIT_ASSERT(ref0.productGetter()==&getter);
+  CPPUNIT_ASSERT(ref0.id().isValid());
+  CPPUNIT_ASSERT(*ref0 == 1);
+
+  ref_t  ref1(id, 1, &getter);
+  CPPUNIT_ASSERT(ref1.isNonnull());
+  CPPUNIT_ASSERT(*ref1 == 2);
+
+  // Note that nothing stops one from making an edm::Ref into a
+  // collection using an index that is invalid. So there is no testing
+  // of such use to be done.
 }
 
--- DataFormats/Common/test/SimpleEDProductGetter.h
+++ DataFormats/Common/test/SimpleEDProductGetter.h
@@ -1,20 +1,42 @@
 #ifndef DATAFORMAT_COMMON_TEST_SIMPLEEDPRODUCTGETTER_H
 #define DATAFORMAT_COMMON_TEST_SIMPLEEDPRODUCTGETTER_H
 
-#include "DataFormats/Common/interface/EDProductGetter.h"
-
 #include <map>
+#include <memory>
+
 #include "boost/shared_ptr.hpp"
+
 #include "DataFormats/Common/interface/EDProduct.h"
+#include "DataFormats/Common/interface/EDProductGetter.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
 
 class SimpleEDProductGetter : public edm::EDProductGetter
 {
  public:
-  virtual edm::EDProduct const* getIt(edm::ProductID const& id) const { return 0; }
+  template <class T>
+  void 
+  addProduct(edm::ProductID const& id, std::auto_ptr<T> p)
+  {
+    typedef edm::Wrapper<T> wrapper_t;
+
+    boost::shared_ptr<wrapper_t> product(new wrapper_t(p));
+    database[id] = product;    
+  }
+
+
+  size_t size() const 
+  { return database.size(); }
+
+  virtual edm::EDProduct const* getIt(edm::ProductID const& id) const
+  { 
+    boost::shared_ptr<edm::EDProduct> pprod = database[id];
+    return &*pprod;
+  }
 
  private:
 
-  std::map<edm::ProductID, boost::shared_ptr<edm::EDProduct> > database;
+  mutable std::map<edm::ProductID, boost::shared_ptr<edm::EDProduct> > database;
 };
 
 #endif
