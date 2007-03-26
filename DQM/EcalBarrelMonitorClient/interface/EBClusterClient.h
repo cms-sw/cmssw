ab860c61e97240bbc25685fc2191369158f72e3d
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2007-03-26 14:14:07 +0000

    added more histograms

--- DQM/EcalBarrelMonitorClient/interface/EBClusterClient.h
+++ DQM/EcalBarrelMonitorClient/interface/EBClusterClient.h
@@ -4,12 +4,12 @@
 /*
  * \file EBClusterClient.h
  *
- * $Date: 2007/02/20 15:42:16 $
- * $Revision: 1.7 $
+ * $Date: 2007/03/13 10:14:25 $
+ * $Revision: 1.8 $
  * \author G. Della Ricca
  * \author F. Cossutti
  *
-*/
+ */
 
 #include <vector>
 #include <string>
@@ -31,89 +31,97 @@
 
 class EBClusterClient : public EBClient {
 
-friend class EBSummaryClient;
+  friend class EBSummaryClient;
 
-public:
+ public:
 
-/// Constructor
-EBClusterClient(const edm::ParameterSet& ps);
+  /// Constructor
+  EBClusterClient(const edm::ParameterSet& ps);
 
-/// Destructor
-virtual ~EBClusterClient();
+  /// Destructor
+  virtual ~EBClusterClient();
 
-/// Subscribe/Unsubscribe to Monitoring Elements
-void subscribe(void);
-void subscribeNew(void);
-void unsubscribe(void);
+  /// Subscribe/Unsubscribe to Monitoring Elements
+  void subscribe(void);
+  void subscribeNew(void);
+  void unsubscribe(void);
 
-/// softReset
-void softReset(void);
+  /// softReset
+  void softReset(void);
 
-/// Analyze
-void analyze(void);
+  /// Analyze
+  void analyze(void);
 
-/// BeginJob
-void beginJob(MonitorUserInterface* mui);
+  /// BeginJob
+  void beginJob(MonitorUserInterface* mui);
 
-/// EndJob
-void endJob(void);
+  /// EndJob
+  void endJob(void);
 
-/// BeginRun
-void beginRun(void);
+  /// BeginRun
+  void beginRun(void);
 
-/// EndRun
-void endRun(void);
+  /// EndRun
+  void endRun(void);
 
-/// Setup
-void setup(void);
+  /// Setup
+  void setup(void);
 
-/// Cleanup
-void cleanup(void);
+  /// Cleanup
+  void cleanup(void);
 
-/// HtmlOutput
-void htmlOutput(int run, string htmlDir, string htmlName);
+  /// HtmlOutput
+  void htmlOutput(int run, string htmlDir, string htmlName);
 
-/// WriteDB
-bool writeDb(EcalCondDBInterface* econn, RunIOV* runiov, MonRunIOV* moniov, int ism);
+  /// WriteDB
+  bool writeDb(EcalCondDBInterface* econn, RunIOV* runiov, MonRunIOV* moniov, int ism);
 
-/// Get Functions
- inline int getEvtPerJob() { return ievt_; }
- inline int getEvtPerRun() { return jevt_; }
+  /// Get Functions
+  inline int getEvtPerJob() { return ievt_; }
+  inline int getEvtPerRun() { return jevt_; }
 
-private:
+ private:
 
-int ievt_;
-int jevt_;
+  int ievt_;
+  int jevt_;
 
-bool collateSources_;
-bool cloneME_;
-bool enableQT_;
+  bool collateSources_;
+  bool cloneME_;
+  bool enableQT_;
 
-bool verbose_;
+  bool verbose_;
 
-bool enableMonitorDaemon_;
+  bool enableMonitorDaemon_;
 
-string prefixME_;
+  string prefixME_;
 
-vector<int> superModules_;
+  vector<int> superModules_;
 
-MonitorUserInterface* mui_;
+  MonitorUserInterface* mui_;
 
-CollateMonitorElement* me_h01_[3];
-CollateMonitorElement* me_h02_;
-CollateMonitorElement* me_h03_;
+  CollateMonitorElement* me_h01_[3];
+  CollateMonitorElement* me_h02_[2];
+  CollateMonitorElement* me_h03_;
+  CollateMonitorElement* me_h04_;
 
-CollateMonitorElement* me_i01_[3];
-CollateMonitorElement* me_i02_;
-CollateMonitorElement* me_i03_;
+  CollateMonitorElement* me_i01_[3];
+  CollateMonitorElement* me_i02_[2];
+  CollateMonitorElement* me_i03_;
+  CollateMonitorElement* me_i04_;
 
-TH1F* h01_[3];
-TProfile2D* h02_;
-TH2F* h03_;
+  CollateMonitorElement* me_s01_[2];
 
-TH1F* i01_[3];
-TProfile2D* i02_;
-TH2F* i03_;
+  TH1F* h01_[3];
+  TProfile2D* h02_[2];
+  TH2F* h03_;
+  TProfile2D* h04_;
+
+  TH1F* i01_[3];
+  TProfile2D* i02_[2];
+  TH2F* i03_;
+  TProfile2D* i04_;
+
+  TH1F* s01_[2];
 
 };
 
