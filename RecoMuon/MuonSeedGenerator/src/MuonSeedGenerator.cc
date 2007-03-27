770aec0339323a580305ada20d498b8164938e88
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2007-03-27 08:04:08 +0000

    bug in the number of segments check

--- RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.cc
+++ RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.cc
@@ -3,8 +3,8 @@
  *  
  *  All the code is under revision
  *
- *  $Date: 2006/11/17 09:04:32 $
- *  $Revision: 1.15 $
+ *  $Date: 2007/03/07 13:20:55 $
+ *  $Revision: 1.16 $
  *
  *  \author A. Vitelli - INFN Torino, V.Palichik
  *  \author ported by: R. Bellan - INFN Torino
@@ -489,9 +489,7 @@ void MuonSeedGenerator::produce(edm::Event& event, const edm::EventSetup& eSetup
 	complete(theSeed, list6, MB3);
 	if (theSeed.nrhit()>1 || (theSeed.nrhit()==1 &&
 				  theSeed.firstRecHit()->dimension()==4) ) {
-	  checkAndFill(theSeed,eSetup);
-
-
+	  fill(theSeed,eSetup);
 	}
       }
     }
@@ -508,7 +506,7 @@ void MuonSeedGenerator::produce(edm::Event& event, const edm::EventSetup& eSetup
 	complete(theSeed, list7, MB2);
 	if (theSeed.nrhit()>1 || (theSeed.nrhit()==1 &&
 				  theSeed.firstRecHit()->dimension()==4) ) {
-	  checkAndFill(theSeed,eSetup);
+	  fill(theSeed,eSetup);
 	}
       }
     }
@@ -712,5 +710,16 @@ void MuonSeedGenerator::checkAndFill(MuonSeedFinder& theSeed, const edm::EventSe
       //if ( (*the_seed).isValid() )
       theSeeds.push_back(*the_seed);
     }
+  }
+}
+
+void MuonSeedGenerator::fill(MuonSeedFinder& theSeed, const edm::EventSetup& eSetup){
+  
+  vector<TrajectorySeed> the_seeds =  theSeed.seeds(eSetup);
+  for (vector<TrajectorySeed>::const_iterator
+	 the_seed=the_seeds.begin(); the_seed!=the_seeds.end(); ++the_seed) {
+    // FIXME, ask for this method
+    //if ( (*the_seed).isValid() )
+    theSeeds.push_back(*the_seed);
   }
 }
