1901d49ce73e4f31f3421ddc45067a62c9ac2af5
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-03-27 22:23:50 +0000

    added SiStripApvGain

--- CondCore/SiStripPlugins/src/plugin.cc
+++ CondCore/SiStripPlugins/src/plugin.cc
@@ -13,9 +13,12 @@
 #include "CondFormats/DataRecord/interface/SiStripNoisesRcd.h"
 #include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
 #include "CondFormats/DataRecord/interface/SiStripFedCablingRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+#include "CondFormats/DataRecord/interface/SiStripApvGainRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(SiStripPedestalsRcd,SiStripPedestals);
 REGISTER_PLUGIN(SiStripNoisesRcd,SiStripNoises);
 REGISTER_PLUGIN(SiStripFedCablingRcd,SiStripFedCabling);
+REGISTER_PLUGIN(SiStripApvGainRcd,SiStripApvGain);
 
