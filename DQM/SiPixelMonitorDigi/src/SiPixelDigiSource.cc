fcf44a35262c56221bf14a9883b2ff642808eee3
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2007-03-28 14:42:30 +0000

    Use folder organizer class

--- DQM/SiPixelMonitorDigi/src/SiPixelDigiSource.cc
+++ DQM/SiPixelMonitorDigi/src/SiPixelDigiSource.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Vincenzo Chiochia
 //         Created:  
-// $Id: SiPixelDigiSource.cc,v 1.9 2007/03/07 18:56:40 chiochia Exp $
+// $Id: SiPixelDigiSource.cc,v 1.10 2007/03/09 08:35:49 chiochia Exp $
 //
 //
 #include "DQM/SiPixelMonitorDigi/interface/SiPixelDigiSource.h"
@@ -24,18 +24,14 @@
 #include "FWCore/Framework/interface/ModuleFactory.h"
 // DQM Framework
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQM/SiPixelCommon/interface/SiPixelFolderOrganizer.h"
 // Geometry
 #include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
 // DataFormats
 #include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
-#include "DataFormats/SiPixelDetId/interface/PXBDetId.h"
-#include "DataFormats/SiPixelDetId/interface/PXFDetId.h"
-#include "DataFormats/SiPixelDetId/interface/PixelBarrelName.h"
-#include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
 #include "DataFormats/SiPixelDigi/interface/PixelDigiCollection.h"
-#include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 //
 #include <boost/cstdint.hpp>
@@ -113,10 +109,6 @@ SiPixelDigiSource::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 //------------------------------------------------------------------
 void SiPixelDigiSource::buildStructure(const edm::EventSetup& iSetup){
 
-  //
-  // Later on the structure will be built from cabling and
-  // *not* from geometry
-  //
   std::cout <<" *** SiPixelDigiSource::buildStructure" << std::endl;
   edm::ESHandle<TrackerGeometry> pDD;
   iSetup.get<TrackerDigiGeometryRecord>().get( pDD );
@@ -150,73 +142,22 @@ void SiPixelDigiSource::buildStructure(const edm::EventSetup& iSetup){
 // Book MEs
 //------------------------------------------------------------------
 void SiPixelDigiSource::bookMEs(){
-
+  
   std::map<uint32_t,SiPixelDigiModule*>::iterator struct_iter;
-  string rootDir = "Tracker";
   theDMBE->setVerbose(0);
-
+    
+  SiPixelFolderOrganizer theSiPixelFolder;
+  
   for(struct_iter = thePixelStructure.begin(); struct_iter != thePixelStructure.end(); struct_iter++){
-    if(DetId::DetId((*struct_iter).first).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) {
-
-      
-
-      PixelBarrelName::Shell DBshell = PixelBarrelName::PixelBarrelName(DetId::DetId((*struct_iter).first)).shell();
-      int DBlayer  = PixelBarrelName::PixelBarrelName(DetId::DetId((*struct_iter).first)).layerName();
-      int DBladder = PixelBarrelName::PixelBarrelName(DetId::DetId((*struct_iter).first)).ladderName();
-      int DBmodule = PixelBarrelName::PixelBarrelName(DetId::DetId((*struct_iter).first)).moduleName();
-
-      // std::cout << " Shell " << DBshell <<
-// 	" Lay " << DBlayer <<
-// 	" Lad " << DBladder <<
-// 	" Mod " << DBmodule << std::endl;
-
-//       int layer  = PXBDetId::PXBDetId((*struct_iter).first).layer();
-//       int ladder = PXBDetId::PXBDetId((*struct_iter).first).ladder();
-//       int module = PXBDetId::PXBDetId((*struct_iter).first).module();
-      
-      string ssubdet = "PixelBarrel"; 
-      char slayer[80];  sprintf(slayer, "Layer_%i",DBlayer);
-      char sladder[80]; sprintf(sladder,"Ladder_%02i",DBladder);
-      char smodule[80]; sprintf(smodule,"Module_%i",DBmodule);
-
-      std::ostringstream sfolder;
-      
-      sfolder << rootDir << "/" << ssubdet << "/Shell_" <<DBshell<< "/" << slayer << "/" << sladder;
-      if ( PixelBarrelName::PixelBarrelName(DetId::DetId((*struct_iter).first)).isHalfModule() ) sfolder <<"H"; 
-      else sfolder <<"F";
-      sfolder << "/" <<smodule;
-
-      //string sfolder = rootDir + "/" + ssubdet + "/Shell_" + DBshell + "/" + slayer + "/" + sladder + "/" + smodule;
-      theDMBE->setCurrentFolder(sfolder.str().c_str());
+    
+    /// Create folder tree and book histograms 
+    if(theSiPixelFolder.setModuleFolder((*struct_iter).first)){
       (*struct_iter).second->book();
-
-    } else 
-      if(DetId::DetId((*struct_iter).first).subdetId() == static_cast<int>(PixelSubdetector::PixelEndcap)) {
-      string ssubdet = "PixelEndcap";
-      PixelEndcapName::HalfCylinder side = PixelEndcapName::PixelEndcapName(DetId::DetId((*struct_iter).first)).halfCylinder();
-      int disk   = PixelEndcapName::PixelEndcapName(DetId::DetId((*struct_iter).first)).diskName();
-      int blade  = PixelEndcapName::PixelEndcapName(DetId::DetId((*struct_iter).first)).bladeName();
-      int panel  = PixelEndcapName::PixelEndcapName(DetId::DetId((*struct_iter).first)).pannelName();
-      int module = PixelEndcapName::PixelEndcapName(DetId::DetId((*struct_iter).first)).plaquetteName();
-
- //      int side   =  PXFDetId::PXFDetId((*struct_iter).first).side();
-//       int disk   =  PXFDetId::PXFDetId((*struct_iter).first).disk();
-//       int blade  =  PXFDetId::PXFDetId((*struct_iter).first).blade();
-//       int panel  =  PXFDetId::PXFDetId((*struct_iter).first).panel();
-//       int module =  PXFDetId::PXFDetId((*struct_iter).first).module();
-      //char sside[80];  sprintf(sside, "Side_%i",side);
-
-      char sdisk[80];  sprintf(sdisk,  "Disk_%i",disk);
-      char sblade[80]; sprintf(sblade, "Blade_%02i",blade);
-      char spanel[80]; sprintf(spanel, "Panel_%i",panel);
-      char smodule[80];sprintf(smodule,"Module_%i",module);
-
-      std::ostringstream sfolder;
-
-      sfolder <<rootDir <<"/" << ssubdet << "/HalfCylinder_" << side << "/" << sdisk << "/" << sblade << "/" << spanel << "/" << smodule;
-      theDMBE->setCurrentFolder(sfolder.str().c_str());
-      (*struct_iter).second->book();
+    } else {
+      throw cms::Exception("LogicError")
+	<< "[SiPixelDigiSource::bookMEs] Creation of DQM folder failed";
     }
+    
   }
 
 }
