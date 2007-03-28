96a8abdf37aa41e81368176b3db3308de488ce82
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-28 14:56:35 +0000

    analyzer for cabling

--- OnlineDB/SiStripESSources/test/stubs/SealModule.cc
+++ OnlineDB/SiStripESSources/test/stubs/SealModule.cc
@@ -1,6 +1,4 @@
-// Last commit: $Id: SealModule.cc,v 1.8 2006/12/22 12:30:40 bainbrid Exp $
-// Latest tag:  $Name: TIF_210307 $
-// Location:    $Source: /cvs_server/repositories/CMSSW/CMSSW/OnlineDB/SiStripESSources/test/stubs/SealModule.cc,v $
+// Last commit: $Id: $
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "PluginManager/ModuleDef.h"
@@ -9,8 +7,8 @@ DEFINE_SEAL_MODULE();
 #include "OnlineDB/SiStripESSources/test/stubs/test_FedCablingBuilder.h"
 DEFINE_ANOTHER_FWK_MODULE(test_FedCablingBuilder);
 
-#include "OnlineDB/SiStripESSources/test/stubs/test_AnalyzeFecCabling.h"
-DEFINE_ANOTHER_FWK_MODULE(test_AnalyzeFecCabling);
+#include "OnlineDB/SiStripESSources/test/stubs/test_AnalyzeCabling.h"
+DEFINE_ANOTHER_FWK_MODULE(test_AnalyzeCabling);
 
 #include "OnlineDB/SiStripESSources/test/stubs/test_PedestalsBuilder.h"
 DEFINE_ANOTHER_FWK_MODULE(test_PedestalsBuilder);
