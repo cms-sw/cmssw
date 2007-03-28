341a79f708be25c144edd5279e98b60aa3a63e11
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2007-03-28 17:19:56 +0000

    add DTT0Analyzer

--- CalibMuon/DTCalibration/test/DBTools/SealModule.cc
+++ CalibMuon/DTCalibration/test/DBTools/SealModule.cc
@@ -7,14 +7,14 @@
 
 #include "DumpDBToFile.h"
 #include "DumpFileToDB.h"
-//#include "DTT0Analyzer.h"
+#include "DTT0Analyzer.h"
 #include "ProduceFakeDB.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DumpDBToFile);
 DEFINE_ANOTHER_FWK_MODULE(DumpFileToDB);
-//DEFINE_ANOTHER_FWK_MODULE(DTT0Analyzer);
+DEFINE_ANOTHER_FWK_MODULE(DTT0Analyzer);
 DEFINE_ANOTHER_FWK_MODULE(ProduceFakeDB);
 
 
