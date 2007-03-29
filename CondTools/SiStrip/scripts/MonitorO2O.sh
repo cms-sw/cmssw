edbf64d602c5acfca7c8166d22e8f5c79b1d8eaa
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-03-29 16:06:33 +0000

    fix bug

--- CondTools/SiStrip/scripts/MonitorO2O.sh
+++ CondTools/SiStrip/scripts/MonitorO2O.sh
@@ -11,18 +11,18 @@ function getLastIOV(){
 }
 
 function getFedVersionFromRunSummaryTIF(){
-    echo  "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=$1&RUN_END=$1&TEXT=1&DB=omds"
-    wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=$1&RUN_END=$1&TEXT=1&DB=omds" -O out.txt
+#    echo  "wget -q -r http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=$1&RUN_END=$1&TEXT=1&DB=omds -O lastiov.txt"
+    wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=$1&RUN_END=$1&TEXT=1&DB=omds" -O lastiov.txt
 
     #Verify query integrity
-    if [ `grep -c "STOPTIME" out.txt` != 1 ]; then
+    if [ `grep -c "STOPTIME" lastiov.txt` != 1 ]; then
 	echo -e "ERROR: RunSummaryTIF provided a strange output"
-	cat out.txt	
+	cat lastiov.txt	
 	rm -f lockFile
 	exit
     fi
 
-    FEDVersion_lastIOV=`cat out.txt | awk -F"\t" '{if (NR>1) print $11}'` 
+    FEDVersion_LastIOV=`cat lastiov.txt | awk -F"\t" '{if (NR>1) print $11}'` 
 }
 
 function CheckIOV(){
@@ -44,11 +44,11 @@ function CheckIOV(){
 	return 0
     fi
 
-    [ "${FEDVersion_lastIOV}" == "" ] && return 12  #//FedVersion NULL: perhaps you are asking for a not existing runNumber 
+    [ "${FEDVersion_LastIOV}" == "" ] && return 12  #//FedVersion NULL: perhaps you are asking for a not existing runNumber 
     
     #//Check FEDVersion of $Run and $lastIOV//
     #echo ${FEDVersion_lastIOV}
-    [ "${FEDVersion_lastIOV}" == "${FEDVersion_Run}" ] && return 0
+    [ "${FEDVersion_LastIOV}" == "${FEDVersion_Run}" ] && return 0
     return 3
 }
 
@@ -102,16 +102,13 @@ nextRun=5000
 [ "$3" != "" ] && nextRun=$3 
 
 export lastIOV
-export Fedversion_LastIOV
+export FEDVersion_LastIOV
 export nextRun
 
 eval `scramv1 runtime -sh`
 
-rm -f RunToBeSubmitted.tmp  RunToDoO2O.tmp
-
-#[ -e RunToBeSubmitted ] && [ `cat RunToBeSubmitted | wc -l` != "0" ] && mv -f RunToBeSubmitted RunToBeSubmitted.bkp
-#[ -e RunToDoO2O ] && mv -f RunToDoO2O RunToDoO2O.bkp
-#touch  RunToBeSubmitted  RunToDoO2O
+rm -f RunToBeSubmitted_${CondDB}.tmp  RunToDoO2O_${CondDB}.tmp
+touch  RunToBeSubmitted_${CondDB}  RunToDoO2O_${CondDB}
 
 GetPhysicsRuns
 
@@ -151,9 +148,9 @@ for Run in `cat AddedRuns | awk '{print $1}'`
   if [ "${oldtagPN}" != "${tagPN}" ]; then
       oldtagPN=${tagPN}
       getLastIOV $tagPN $CondDB
-      Fedversion_LastIOV=""
+      FEDVersion_LastIOV=""
       [ "$lastIOV" != "" ] && getFedVersionFromRunSummaryTIF $lastIOV
-      echo -e "[MonitorO2O.sh] DB=$CondDB \ttag=$vTag \tlastIOV=$lastIOV \tFedversion_LastIOV=${Fedversion_LastIOV}"
+      echo -e "[MonitorO2O.sh] DB=$CondDB \ttag=$vTag \tlastIOV=$lastIOV \tFEDVersion_LastIOV=${FEDVersion_LastIOV}"
   fi
   
   CheckIOV $Run 
@@ -162,11 +159,11 @@ for Run in `cat AddedRuns | awk '{print $1}'`
   #echo status $status
   if [ "$status" == "0" ];
       then
-      echo -e "$Run \t $CondDB \t $vTag \t \t ${FedVer}">> RunToBeSubmitted.tmp
+      echo -e "$Run \t ${FedVer} \t $CondDB \t $vTag">> RunToBeSubmitted_${CondDB}.tmp
   elif [ $status -lt 10 ]; then
       if [ "${FedVer}" != "${oldFedVer}" ] ; then
 	  oldFedVer=${FedVer}
-	  echo -e "$Run \t $status \t $CondDB \t $vTag \t ${FedVer} \t $ConfigDb" >> RunToDoO2O.tmp
+	  echo -e "$Run \t $status \t $CondDB \t $vTag \t ${FedVer} \t $ConfigDb" >> RunToDoO2O_${CondDB}.tmp
       fi
   else
       # case of error 11 and 12 in CheckIOV
@@ -176,22 +173,32 @@ for Run in `cat AddedRuns | awk '{print $1}'`
   fi
 done
 
-if [ `cat AddedRuns | wc -l ` != "0" ]; then
-    if [ -e RunToBeSubmitted.tmp ] && [ "`diff -q RunToBeSubmitted RunToBeSubmitted.tmp`" != "" ]; then
-	mv -f RunToBeSubmitted.tmp RunToBeSubmitted
-	#cat RunToBeSubmitted | mail -s "MonitorO2O cron: Submit Runs" "domenico.giordano@cern.ch noeding@fnal.gov Nicola.Defilippis@ba.infn.it sdutta@mail.cern.ch"
-	cat RunToBeSubmitted | mail -s "MonitorO2O cron: Submit Runs" "domenico.giordano@cern.ch sdutta@mail.cern.ch"
-#	echo "There are jobs to be Submitted" | mail -s " " 00393402949274@sms.switch.ch
+#if [ `cat AddedRuns | wc -l ` != "0" ]; then
+if [ -e RunToBeSubmitted_${CondDB}.tmp ]; then
+    if [ "`diff -q RunToBeSubmitted_${CondDB} RunToBeSubmitted_${CondDB}.tmp`" != "" ]; then
+	mv -f RunToBeSubmitted_${CondDB}.tmp RunToBeSubmitted_${CondDB}
+	cat RunToBeSubmitted_${CondDB} | mail -s "MonitorO2O cron: Submit Runs" "domenico.giordano@cern.ch noeding@fnal.gov Nicola.Defilippis@ba.infn.it sdutta@mail.cern.ch"
+	#cat RunToBeSubmitted_${CondDB} | mail -s "MonitorO2O cron: Submit Runs" "domenico.giordano@cern.ch sdutta@mail.cern.ch"
+	#echo "There are jobs to be Submitted" | mail -s " " 00393402949274@sms.switch.ch
     fi
-    
-#    if [ `cat RunToDoO2O.tmp | wc -l` != "0" ] && [ "`diff -q RunToDoO2O RunToDoO2O.tmp`" != "" ]; then
-    if [ -e RunToDoO2O.tmp ] && [ "`diff -q RunToDoO2O RunToDoO2O.tmp`" != "" ]; then
-	mv -f RunToDoO2O.tmp RunToDoO2O
-	if [ `cat RunToDoO2O | wc -l` != "0" ]; then
-	    cat RunToDoO2O | mail -s "MonitorO2O cron: Do o2o" domenico.giordano@cern.ch
-#	echo "There is o2o to be done" | mail -s " " 00393402949274@sms.switch.ch
-	fi
-    fi
+else 
+    rm -f RunToBeSubmitted_${CondDB}
+    touch RunToBeSubmitted_${CondDB}
 fi
+#    if [ `cat RunToDoO2O.tmp | wc -l` != "0" ] && [ "`diff -q RunToDoO2O RunToDoO2O.tmp`" != "" ]; then
+if [ -e RunToDoO2O_${CondDB}.tmp ]; then
+    if [ "`diff -q RunToDoO2O_${CondDB} RunToDoO2O_${CondDB}.tmp`" != "" ]; then
+	mv -f RunToDoO2O_${CondDB}.tmp RunToDoO2O_${CondDB}
+    fi
+else
+    rm -f  RunToDoO2O_${CondDB}
+    touch  RunToDoO2O_${CondDB}
+fi
+if [ `cat RunToDoO2O_${CondDB} | wc -l` != "0" ]; then
+    cat RunToDoO2O_${CondDB} | mail -s "MonitorO2O cron: Do o2o" domenico.giordano@cern.ch
+#    echo "There is o2o to be done" | mail -s " " 00393402949274@sms.switch.ch
+fi
+#fi
+#fi
 
 rm -f lockFile
