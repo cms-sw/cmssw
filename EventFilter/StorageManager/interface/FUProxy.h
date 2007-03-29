f73ca5e333c79e09f726cd0d61bcb3e6d6376556
Markus Klute <sha1-ebbada096308039c1891ac11da0ca166ff64d551@cern.ch>
2007-03-29 07:18:11 +0000

    class to send back discards to filter units

---
+++ EventFilter/StorageManager/interface/FUProxy.h
@@ -0,0 +1,47 @@
+#if !defined(STOR_FUPROXY_H)
+#define STOR_FUPROXY_H
+
+////////////////////////////////////////////////////////////////////////////////
+// Created by Markus Klute on 2007 Mar 22.
+// $Id:$
+////////////////////////////////////////////////////////////////////////////////
+// send back discards to filter units
+//
+// following the example in EventFilter/ResourceBroker for BUProxy and SMProxy
+////////////////////////////////////////////////////////////////////////////////
+
+#include "EventFilter/Utilities/interface/i2oEvfMsgs.h"
+#include "EventFilter/Utilities/interface/Exception.h"
+#include "xdaq/include/xdaq/Application.h"
+
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace stor 
+{
+  class FUProxy
+    {     
+    public:
+      FUProxy(xdaq::ApplicationDescriptor *,
+	      xdaq::ApplicationDescriptor *,
+	      xdaq::ApplicationContext    *,
+	      toolbox::mem::Pool          *);
+
+      virtual ~FUProxy();
+      
+      int sendDataDiscard(int);
+      int sendDQMDiscard(int);
+
+    private:
+      int sendDiscard(int, int)   throw (evf::Exception);
+
+      xdaq::ApplicationDescriptor *smAppDesc_;
+      xdaq::ApplicationDescriptor *fuAppDesc_;
+      xdaq::ApplicationContext    *smAppContext_;
+      toolbox::mem::Pool          *i2oPool_;
+      
+    };
+
+} // namespace stor
+
+#endif
---
+++ EventFilter/StorageManager/src/FUProxy.cc
@@ -0,0 +1,115 @@
+// Created by Markus Klute on 2007 Mar 22.
+// $Id:$
+////////////////////////////////////////////////////////////////////////////////
+
+#include <EventFilter/StorageManager/interface/FUProxy.h>
+
+#include <toolbox/include/toolbox/mem/Reference.h>
+#include <toolbox/include/toolbox/mem/MemoryPoolFactory.h>
+#include <toolbox/include/toolbox/mem/exception/Exception.h>
+
+#include <i2o/include/i2o/Method.h>
+#include <i2o/utils/include/i2o/utils/AddressMap.h>
+
+#include <xcept/include/xcept/tools.h>
+
+#include <iostream> 
+#include <sstream>
+
+using stor::FUProxy;
+
+
+////////////////////////////////////////////////////////////////////////////////
+FUProxy::FUProxy(xdaq::ApplicationDescriptor *smAppDesc,
+		 xdaq::ApplicationDescriptor *fuAppDesc,
+		 xdaq::ApplicationContext    *smAppContext,
+		 toolbox::mem::Pool          *i2oPool) 
+  : smAppDesc_(smAppDesc)
+  , fuAppDesc_(fuAppDesc)
+  , smAppContext_(smAppContext)
+  , i2oPool_(i2oPool)
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+FUProxy::~FUProxy()
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+int FUProxy::sendDQMDiscard(int fuId)
+{
+  return sendDiscard(fuId, I2O_FU_DQM_DISCARD);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+int FUProxy::sendDataDiscard(int fuId)
+{
+  return sendDiscard(fuId, I2O_FU_DATA_DISCARD);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+int FUProxy::sendDiscard(int fuId, int msgId)
+  throw (evf::Exception)
+{
+  try {
+    size_t msgSize=sizeof(I2O_FU_DATA_DISCARD_MESSAGE_FRAME);
+    
+    toolbox::mem::Reference *bufRef=
+      toolbox::mem::getMemoryPoolFactory()->getFrame(i2oPool_,msgSize);
+
+    // message pointer
+    I2O_MESSAGE_FRAME *stdMsg = 
+      (I2O_MESSAGE_FRAME*)bufRef->getDataLocation();
+    I2O_PRIVATE_MESSAGE_FRAME *pvtMsg =
+      (I2O_PRIVATE_MESSAGE_FRAME*)stdMsg;
+    I2O_FU_DATA_DISCARD_MESSAGE_FRAME *msg =
+      (I2O_FU_DATA_DISCARD_MESSAGE_FRAME*)stdMsg;
+    
+    // set message frame
+    stdMsg->MessageSize     = msgSize >> 2;
+    stdMsg->InitiatorAddress= i2o::utils::getAddressMap()->getTid(smAppDesc_);
+    stdMsg->TargetAddress   = i2o::utils::getAddressMap()->getTid(fuAppDesc_);
+    stdMsg->Function        = I2O_PRIVATE_MESSAGE;
+    stdMsg->VersionOffset   = 0;
+    stdMsg->MsgFlags        = 0;  
+ 
+    // set private message frame
+    pvtMsg->XFunctionCode   =  msgId;
+    pvtMsg->OrganizationID  = XDAQ_ORGANIZATION_ID;
+
+    // set fu data discard message frame
+    msg->fuID               = fuId;
+
+     // set buffer size
+    bufRef->setDataSize(msgSize);
+
+    // post frame
+    smAppContext_->postFrame(bufRef,smAppDesc_,fuAppDesc_);
+  }
+  catch (xdaq::exception::Exception &e) {
+    std::ostringstream oss;
+    oss << "FUProxy failed to post message! " 
+	<< " What: "              << e.what()
+	<< " Exception history: " << xcept::stdformat_exception_history(e);
+    LOG4CPLUS_ERROR(smAppContext_->getLogger(), oss.str());
+    XCEPT_RETHROW(evf::Exception, oss.str(), e);
+  }
+  catch(toolbox::mem::exception::Exception& e)	{
+    std::ostringstream oss;
+    oss << "FUProxy failed to allocate buffer reference! " 
+	<< " What: "              << e.what()
+	<< " Exception history: " << xcept::stdformat_exception_history(e);
+    LOG4CPLUS_ERROR(smAppContext_->getLogger(), oss.str());
+    XCEPT_RETHROW(evf::Exception, oss.str(), e);
+  }
+  return 0;
+}
+
+
