b732add74bee67b3c9bd589712651bdbbaf06f20
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2007-03-29 10:10:53 +0000

    with the new client DTEfficiency

--- DQM/DTMonitorClient/src/SealModule.cc
+++ DQM/DTMonitorClient/src/SealModule.cc
@@ -11,5 +11,8 @@ DEFINE_ANOTHER_FWK_MODULE(DTtTrigCalibrationTest);
 #include <DQM/DTMonitorClient/src/DTResolutionTest.h>
 DEFINE_ANOTHER_FWK_MODULE(DTResolutionTest);
 
+#include <DQM/DTMonitorClient/src/DTEfficiencyTest.h>
+DEFINE_ANOTHER_FWK_MODULE(DTEfficiencyTest);
+
 #include <DQM/DTMonitorClient/src/DTDataIntegrityTest.h>
 DEFINE_ANOTHER_FWK_MODULE(DTDataIntegrityTest);
