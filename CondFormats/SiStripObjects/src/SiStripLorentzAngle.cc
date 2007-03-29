0b745283be545e229bde669e81786d3d282534f5
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-03-29 17:26:19 +0000

    first commit

---
+++ CondFormats/SiStripObjects/src/SiStripLorentzAngle.cc
@@ -0,0 +1,21 @@
+#include "CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+bool SiStripLorentzAngle::putLorentzAngle(const uint32_t& detid, float& value){
+  std::map<unsigned int,float>::const_iterator id=m_LA.find(detid);
+  if(id!=m_LA.end()){
+    edm::LogError("SiStripLorentzAngle") << "SiStripLorentzAngle for DetID " << detid << " is already stored. Skippig this put" << std::endl;
+    return false;
+  }
+  else m_LA[detid]=value;
+  return true;
+}
+const float& SiStripLorentzAngle::getLorentzAngle(const uint32_t& detid) const  {
+  std::map<unsigned int,float>::const_iterator id=m_LA.find(detid);
+  if(id!=m_LA.end()) return id->second;
+  else {
+    edm::LogError("SiStripLorentzAngle") << "SiStripLorentzAngle for DetID " << detid << " is not stored" << std::endl; 
+  }
+  return 0;
+}
