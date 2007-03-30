a9844bfee0dc7f4209542ef6fe202eae2814c35c
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-03-30 08:49:57 +0000

    set cuts/ranges from PSet; print method

--- RecoMuon/MuonIsolation/interface/Cuts.h
+++ RecoMuon/MuonIsolation/interface/Cuts.h
@@ -3,6 +3,9 @@
 
 #include "RecoMuon/MuonIsolation/interface/Range.h"
 #include <vector>
+#include <string>
+
+namespace edm { class ParameterSet; }
 
 namespace muonisolation {
 
@@ -11,6 +14,13 @@ public:
 
   struct CutSpec { muonisolation::Range<double> etaRange; double conesize; double threshold; }; 
 
+  /// dummy constructor
+  Cuts(){}
+
+  /// ctor by PSet
+  Cuts(const edm::ParameterSet & pset);
+
+  /// constructor from valid parameters 
   Cuts( const std::vector<double> & etaBounds, 
         const std::vector<double> & coneSizes, 
         const std::vector<double> & thresholds);
@@ -21,7 +31,14 @@ public:
 
   unsigned int size() {return theCuts.size();};
 
+  std::string print() const;
+
 private:
+  void init(
+      const std::vector<double> & etaBounds,
+      const std::vector<double> & coneSizes,
+      const std::vector<double> & thresholds);
+
   std::vector<CutSpec> theCuts;
 };
 
--- RecoMuon/MuonIsolation/src/Cuts.cc
+++ RecoMuon/MuonIsolation/src/Cuts.cc
@@ -1,10 +1,28 @@
 #include "RecoMuon/MuonIsolation/interface/Cuts.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include <math.h>
+#include <sstream>
 
 using namespace std;
 using namespace muonisolation;
 
+Cuts::Cuts(const edm::ParameterSet & pset)
+{
+  vector<double> etaBounds  = pset.getParameter<std::vector<double> >("EtaBounds");
+  vector<double> coneSizes  = pset.getParameter<std::vector<double> >("ConeSizes");
+  vector<double> thresholds = pset.getParameter<std::vector<double> >("Thresholds");
+  init(etaBounds,coneSizes,thresholds);
+}
+
 Cuts::Cuts(const vector<double> & etaBounds, const vector<double> & coneSizes,
+      const vector<double> & thresholds) 
+{
+  init(etaBounds,coneSizes,thresholds);
+}
+
+
+void Cuts::init(const vector<double> & etaBounds, const vector<double> & coneSizes,
       const vector<double> & thresholds)
 {
   double minEta = 0.;
@@ -32,4 +50,18 @@ const Cuts::CutSpec & Cuts::operator()(double eta) const
     if (absEta < theCuts[i].etaRange.max() ) { idx_eta = i; break; }
   }
   return theCuts[idx_eta];
+}
+
+std::string Cuts::print() const
+{
+  std::ostringstream result;
+  typedef std::vector<CutSpec>::const_iterator IT;
+  result << "Cuts : " << std::endl;
+  for (IT it = theCuts.begin(), itEnd = theCuts.end(); it < itEnd; ++it) {
+    result << "eta: "<<(*it).etaRange
+           <<", cone: "<< (*it).conesize
+           <<", cut: "<<(*it).threshold
+           <<std::endl;
+  }  
+  return result.str();
 }
