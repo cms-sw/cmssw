e412c9ab5c18dbb578488c8958beab5096c9aed3
Mika Huhtinen <sha1-e4ace60bb85ca6214788a8c677bc0fec21d76610@cern.ch>
2007-03-30 15:56:11 +0000

    Implementation of Minimum Bias Trigger at HLT using track information from Pixel (only) reconstruction. It is assumed that L1 passes ZeroBias.

---
+++ HLTrigger/special/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "HLTrigger/special/interface/HLTPixlMBFilt.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
+				   
