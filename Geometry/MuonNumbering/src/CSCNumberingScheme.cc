5c636fb37d6069e105354284c2c3a48bf6afa6e8
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2007-03-30 14:16:31 +0000

    From Tim Cox, fixes to code. Numbering is the same as always.

--- Geometry/MuonNumbering/src/CSCNumberingScheme.cc
+++ Geometry/MuonNumbering/src/CSCNumberingScheme.cc
@@ -3,6 +3,7 @@
 #include "Geometry/MuonNumbering/interface/MuonBaseNumber.h"
 #include "Geometry/MuonNumbering/interface/MuonDDDConstants.h"
 #include "DetectorDescription/Core/interface/DDCompactView.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include <iostream>
 
@@ -24,15 +25,15 @@ void CSCNumberingScheme::initMe (  const MuonDDDConstants& muonConstants ) {
   theStationLevel=muonConstants.getValue("me_station")/theLevelPart;
   theSubringLevel=muonConstants.getValue("me_subring")/theLevelPart;
   theSectorLevel=muonConstants.getValue("me_sector")/theLevelPart;
-  theLayerLevel=muonConstants.getValue("me_layer")/theLevelPart;
   theRingLevel=muonConstants.getValue("me_ring")/theLevelPart;
+  theLayerLevel=muonConstants.getValue("me_layer")/theLevelPart;
 #ifdef LOCAL_DEBUG
   std::cout << "theRegionLevel " << theRegionLevel <<std::endl;
   std::cout << "theStationLevel " << theStationLevel <<std::endl;
   std::cout << "theSubringLevel " << theSubringLevel <<std::endl;
   std::cout << "theSectorLevel " << theSectorLevel <<std::endl;
-  std::cout << "theLayerLevel " << theLayerLevel <<std::endl;
   std::cout << "theRingLevel " << theRingLevel <<std::endl;
+  std::cout << "theLayerLevel " << theLayerLevel <<std::endl;
 #endif
 }
 
@@ -46,13 +47,6 @@ int CSCNumberingScheme::baseNumberToUnitNumber(const MuonBaseNumber num){
   }
 #endif
 
-  if (num.getLevels()!=theRingLevel) {
-    std::cout << "CSCNS::BNToUN "
-	 << "BaseNumber has " << num.getLevels() << " levels,"
-	 << "need "<<theRingLevel<<std::endl;
-    return 0;
-  }
-
   int fwbw_id=0;
   int station_id=0;
   int ring_id=0;
@@ -60,79 +54,73 @@ int CSCNumberingScheme::baseNumberToUnitNumber(const MuonBaseNumber num){
   int sector_id=0;
   int layer_id=0;
 
-
-  //decode 6 significant endcap levels
+  // Decode endcap levels
+  // We should be able to work with 6 (layer-level) or 5 (chamber-level)
   
-  for (int level=1;level<=theRingLevel;level++) {
+    for (int level=1;level<=num.getLevels();level++) {
 
-    //decode
     if (level==theRegionLevel) {
       const int copyno=num.getBaseNo(level);
       fwbw_id=copyno+1;
+      LogDebug("CSCNumbering") << "endcap=" << fwbw_id ;
 
     } else if (level==theStationLevel) {
       const int station_tag = num.getSuperNo(level);      
       station_id=station_tag;
+      LogDebug("CSCNumbering")<< "station=" << station_id ;
       
     } else if (level==theSubringLevel) {
       const int copyno=num.getBaseNo(level);
       subring_id=copyno+1;
+      LogDebug("CSCNumbering")<<"subring=" << subring_id ;
 
     } else if (level==theSectorLevel) {
       const int copyno=num.getBaseNo(level);
       sector_id=copyno+1;
+      LogDebug("CSCNumbering")<< "sector=" << sector_id ;
 
     } else if (level==theLayerLevel) {
       const int copyno=num.getBaseNo(level);
       layer_id=copyno+1;
+      LogDebug("CSCNumbering") << "layer=" << layer_id ;
 
     } else if (level==theRingLevel) {
       const int ring_tag = num.getSuperNo(level);      
       ring_id=ring_tag;
+      LogDebug("CSCNumbering") << "ring=" << ring_id ;
     }
   }      
 
   // check validity 
     
   if ((fwbw_id < 1) || (fwbw_id > 2)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "forward/backward id out of range: ";
-    std::cout << fwbw_id <<std::endl;
+    edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "forward/backward id out of range:" << fwbw_id;
   }
     
   if ((station_id < 1) || (station_id > 4)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "station id out of range: ";
-    std::cout << station_id <<std::endl;
+    edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "station id out of range:" << station_id;
   }
     
-  if ((ring_id < 1) || (ring_id > 4)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "ring id out of range: ";
-    std::cout << ring_id <<std::endl;
-  }
+   if ((ring_id < 1) || (ring_id > 4)) {
+     edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "ring id out of range:" << ring_id ;
+   }
     
   if ((subring_id < 1) || (subring_id > 2)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "subring id out of range: ";
-    std::cout << subring_id <<std::endl;
+    edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "subring id out of range:" << subring_id ;
   }
     
   if ((sector_id < 1) || (sector_id > 36)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "sector id out of range: ";
-    std::cout << sector_id <<std::endl;
+    edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "sector id out of range:" << sector_id ;
   }
-    
-  if ((layer_id < 1) || (layer_id > 6)) {
-    std::cout << "CSCNumberingScheme: ";
-    std::cout << "layer id out of range: ";
-    std::cout << layer_id <<std::endl;
+
+  // Allow id=0 since that means a chamber    
+  if ((layer_id < 0) || (layer_id > 6)) {
+    edm::LogError("CSCNumbering") << "@SUB=CSCNumberingScheme::baseNumberToUnitNumber" << "layer id out of range" << layer_id ;
   }
     
   // find appropriate chamber label
     
-  int chamber_id=chamberIndex(station_id, ring_id,
+   int chamber_id=chamberIndex(station_id, ring_id,
 			      subring_id, sector_id);
     
   // convert into raw id of appropriate DetId
@@ -158,13 +146,11 @@ int CSCNumberingScheme::baseNumberToUnitNumber(const MuonBaseNumber num){
 int CSCNumberingScheme::chamberIndex(int station_id,  
            int ring_id, int subring_id, int sector_id) const {
 
-  //@@ FIXME Nov-2005 THIS NO LONGER MATCHES HARDWARE
-  //@@ CHAMBER ZERO IS ROTATED BY 20 DEG (I THINK!)
-
   int chamber_id=0;
 
   // chamber label is related to sector_id but we need to
   // adjust to real hardware labelling
+  // Tim confirms this works properly according to CMS IN 2000/004 Version 2.5 March 2007.
 
   if (ring_id == 3) {
     chamber_id=sector_id;
@@ -175,18 +161,6 @@ int CSCNumberingScheme::chamberIndex(int station_id,
       chamber_id=2*sector_id;
     }
   }
-
-//   if (station_id == 1) {
-//     if (ring_id == 3) {
-//       chamber_id--;
-//       if (chamber_id < 1) chamber_id=36;
-//     }
-//   } else {
-//     if (ring_id == 1) {
-//       chamber_id++;
-//       if (chamber_id > 18) chamber_id=1;
-//     }
-//   }
 
   return chamber_id;
 
