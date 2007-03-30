8a9272d10eea1e67a0b1146b9379592d643755f4
Marco Zanetti <sha1-fba42c5becc5876495df0d6b3436fa25443a9c62@cern.ch>
2007-03-30 16:29:38 +0000

    TestClient removed

--- DQM/DTMonitorClient/src/SealModule.cc
+++ DQM/DTMonitorClient/src/SealModule.cc
@@ -2,11 +2,8 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include <DQM/DTMonitorClient/interface/TestClient.h>
-DEFINE_FWK_MODULE(TestClient);
-
 #include <DQM/DTMonitorClient/interface/DTtTrigCalibrationTest.h>
-DEFINE_ANOTHER_FWK_MODULE(DTtTrigCalibrationTest);
+DEFINE_FWK_MODULE(DTtTrigCalibrationTest);
 
 #include <DQM/DTMonitorClient/src/DTResolutionTest.h>
 DEFINE_ANOTHER_FWK_MODULE(DTResolutionTest);
