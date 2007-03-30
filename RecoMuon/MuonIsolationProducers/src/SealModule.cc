ca46b7c38e45f66aef16eda0ea3f0cf273876d58
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-03-30 10:22:53 +0000

    define producers here

---
+++ RecoMuon/MuonIsolationProducers/src/SealModule.cc
@@ -0,0 +1,10 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+
+#include "MuIsoDepositProducer.h"
+DEFINE_ANOTHER_FWK_MODULE(MuIsoDepositProducer);
+
+#include "MuIsolationSumInConesProducer.h"
+DEFINE_ANOTHER_FWK_MODULE(MuIsolationSumInConesProducer);
+
