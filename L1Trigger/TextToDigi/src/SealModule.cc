98b64abb550a61ce39a851ef45f1aefccbcb650d
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2007-04-01 12:08:42 +0000

    added RCT digi to text converter module

--- L1Trigger/TextToDigi/src/SealModule.cc
+++ L1Trigger/TextToDigi/src/SealModule.cc
@@ -6,10 +6,12 @@
 #include "L1Trigger/TextToDigi/src/RctDigiToSourceCardText.h"
 #include "L1Trigger/TextToDigi/src/SourceCardTextToRctDigi.h"
 #include "L1Trigger/TextToDigi/src/RctTextToRctDigi.h"
+#include "L1Trigger/TextToDigi/src/RctDigiToRctText.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TextToRaw);
 DEFINE_ANOTHER_FWK_MODULE(RctDigiToSourceCardText);
 DEFINE_ANOTHER_FWK_MODULE(SourceCardTextToRctDigi);
 DEFINE_ANOTHER_FWK_MODULE(RctTextToRctDigi);
+DEFINE_ANOTHER_FWK_MODULE(RctDigiToRctText);
 
