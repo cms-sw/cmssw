108ecfd9259cec158ca1536bf109250fd0013238
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2007-04-02 10:59:13 +0000

    *** empty log message ***

--- RecoTauTag/HLTProducers/src/L2TauJetsProvider.cc
+++ RecoTauTag/HLTProducers/src/L2TauJetsProvider.cc
@@ -74,12 +74,12 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
  bool singleTauFired = singleTauMap.triggerDecision() ;
  bool doubleTauFired = doubleTauMap.triggerDecision() ;
 
- 
+ /*
  cout <<"Trigger SingleTau "<<singleTauFired<<endl;
  cout <<"Trigger DoubleTau "<<doubleTauFired<<endl;
  cout <<"SingleTau objects: "<<myL1SingleTaus.size()<<endl;
  cout <<"DoubleTau objects: "<<myL1DoubleTaus.size()<<endl;
- 
+ */
 
  
 
@@ -102,8 +102,6 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
 	  {
 	    //Calculate the DeltaR between L1TauCandidate and L1Tau which fired the trigger
 	    deltaR = ROOT::Math::VectorUtil::DeltaR(myL1Tau[iJet].p4().Vect(), (*myTau1)->p4().Vect());
-	    //	    cout <<"SingleTau "<<(*myTau1)->pt() << " "<<(*myTau1)->eta() << " " << (*myTau1)->phi() <<endl;
-	    //	    cout <<"deltaR "<<deltaR<<endl;
 	    if(deltaR < matchingR) {
 	      //Getting back from the map the L2TauJet
 	      const CaloJet myL2TauJet = myL2itr->second;
@@ -117,7 +115,6 @@ void L2TauJetsProvider::produce(edm::Event& iEvent, const edm::EventSetup& iES)
 	L1JetParticleVectorRef::const_iterator myTau2 = myL1DoubleTaus.begin();
 	for(;myTau2 != myL1DoubleTaus.end();myTau2++)
 	  {
-	    //	    cout <<"DoubleTau "<<(*myTau2)->pt() << " "<<(*myTau2)->eta() << " " << (*myTau2)->phi() <<endl;
 	    deltaR = ROOT::Math::VectorUtil::DeltaR(myL1Tau[iJet].p4().Vect(), (*myTau2)->p4().Vect());
 	    if(deltaR < matchingR) {
 	      const CaloJet myL2TauJet = myL2itr->second;
