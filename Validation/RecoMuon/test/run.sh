57a12a359168de22ef827ea041ccc95f865e9be5
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2007-04-02 14:46:06 +0000

    Re-reconstruction

--- Validation/RecoMuon/test/run.sh
+++ Validation/RecoMuon/test/run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash 
 
-cmsRun $PKGDIR/test/${1}.cfg 2>&1 | gzip > $OUTDIR/${1}.log.gz
-RETVAL=$?
+RETVAL=0
 
+if [ "_"$DORECO == "_yes" ]; then
+	cmsRun reco_$1.cfg 2>&1 | gzip > $OUTDIR/reco_$1.log.gz
+	RETVAL=$?
+fi
+
+if [ $RETVAL == 0 ]; then
+	cmsRun $1.cfg 2>&1 | gzip > $OUTDIR/$1.log.gz
+	RETVAL=$?
+fi
+
+rm -f reco.root
 \mv -f *.root $OUTDIR/
 
 exit $RETVAL
