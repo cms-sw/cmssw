4b8c8c76cebb041cf4c4c62c38f58efdcaec1c9a
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2007-04-02 10:36:17 +0000

    RPC trigger files moved to CondFormats/RPCObjects

--- CondFormats/L1TObjects/src/classes.h
+++ CondFormats/L1TObjects/src/classes.h
@@ -2,7 +2,6 @@
 #include "CondFormats/L1TObjects/interface/L1MuGMTScales.h"
 #include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
 #include "CondFormats/L1TObjects/interface/L1GctJetEtCalibrationFunction.h"
-#include "CondFormats/L1TObjects/interface/L1RPCConfig.h"
 #include "CondFormats/L1TObjects/interface/L1MuDTExtLut.h"
 #include "CondFormats/L1TObjects/interface/L1MuDTPhiLut.h"
 #include "CondFormats/L1TObjects/interface/L1MuDTPtaLut.h"
