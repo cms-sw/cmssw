67f7560e535d0bdff88d9e794ca65d3a7a7c7b9c
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2007-04-02 13:02:57 +0000

    version with flavour tools

--- PhysicsTools/JetMCAlgos/src/SealModule.cc
+++ PhysicsTools/JetMCAlgos/src/SealModule.cc
@@ -3,8 +3,10 @@
 #include "PhysicsTools/JetMCAlgos/interface/CandOneToOneDeltaRMatcher.h"
 #include "PhysicsTools/JetMCAlgos/interface/CandOneToManyDeltaRMatcher.h"
 #include "PhysicsTools/JetMCAlgos/src/jetTest.h"
+#include "PhysicsTools/JetMCAlgos/interface/printGenEvent.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( CandOneToOneDeltaRMatcher );
 DEFINE_ANOTHER_FWK_MODULE( CandOneToManyDeltaRMatcher );
+DEFINE_ANOTHER_FWK_MODULE( printGenEvent );
 DEFINE_ANOTHER_FWK_MODULE( jetTest );
