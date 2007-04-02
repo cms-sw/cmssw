bd847f259e47e8000974933177594e68f6e7eed3
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2007-04-02 17:54:37 +0000

    First Version

---
+++ L1TriggerConfig/DTTPGConfig/src/ES_DTConfig.cc
@@ -0,0 +1,5 @@
+
+#include "L1TriggerConfig/DTTPGConfig/interface/DTConfigManager.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(DTConfigManager);
