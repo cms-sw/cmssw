3d11c0147d4e85f4bebd85d7eae4fb16d393f6c5
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2007-04-02 12:40:30 +0000

    first commit

---
+++ PhysicsTools/JetMCUtils/interface/JetMCTag.h
@@ -0,0 +1,16 @@
+#ifndef JetMCTag_H
+#define JetMCTag_h
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+namespace JetMCTagUtils {
+
+  double EnergyRatioFromBHadrons(const reco::Candidate &c);
+  double EnergyRatioFromCHadrons(const reco::Candidate &c);
+  int    BTagPhysicsFlavour();
+  int    BTagAlgoFlavour();
+  bool   decayFromBHadron(const reco::Candidate &c);
+  bool   decayFromCHadron(const reco::Candidate &c);
+
+}
+#endif
