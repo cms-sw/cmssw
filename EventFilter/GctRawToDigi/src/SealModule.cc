aa9d4fd230696d1f46385a287bf9f05157e9821c
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-04-02 15:45:15 +0000

    moved GctVmeToRaw

--- EventFilter/GctRawToDigi/src/SealModule.cc
+++ EventFilter/GctRawToDigi/src/SealModule.cc
@@ -5,6 +5,5 @@
 #include "EventFilter/GctRawToDigi/src/GctRawToDigi.h"
 #include "EventFilter/GctRawToDigi/src/GctVmeToRaw.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(GctVmeToRaw);
-DEFINE_ANOTHER_FWK_MODULE(GctRawToDigi);
+DEFINE_FRAMEWORK_MODULE(GctRawToDigi);
+
