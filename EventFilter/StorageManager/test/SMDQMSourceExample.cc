f50cee03366d826ef357d6374f4d9f5d5c4a21c2
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2007-04-02 19:24:34 +0000

    Add version of DQMSourceExample called SMDQMSourceExample that has no 1 sec wait, does fitting quietly, and creates an additional top level folder.

---
+++ EventFilter/StorageManager/test/SMDQMSourceExample.cc
@@ -0,0 +1,220 @@
+// -*- C++ -*-
+//
+// Package:    DQMServices/Daemon
+// Class:      SMDQMSourceExample
+// 
+/**\class SMDQMSourceExample
+
+  Description: Example DQM Source with multiple top level folders for
+  testing in the Storage manager. This started from the DQMSourceExample.cc
+  file in DQMServices/Daemon/test, but modified to include another top level
+  folder, to remove the 1 sec wait, and to do the fitting without printout.
+
+  $Id$
+
+*/
+
+
+// system include files
+#include <memory>
+#include <iostream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/Daemon/interface/MonitorDaemon.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <TRandom.h> // this is just the random number generator
+
+using std::cout; using std::endl;
+
+//
+// class declaration
+//
+
+class SMDQMSourceExample : public edm::EDAnalyzer {
+public:
+   explicit SMDQMSourceExample( const edm::ParameterSet& );
+   ~SMDQMSourceExample();
+   
+  virtual void analyze( const edm::Event&, const edm::EventSetup& );
+
+  virtual void endJob(void);
+
+private:
+      // ----------member data ---------------------------
+
+  MonitorElement * h1;
+  MonitorElement * h2;
+  MonitorElement * h3;
+  MonitorElement * h4;
+  MonitorElement * h5;
+  MonitorElement * h6;
+  MonitorElement * h7;
+  MonitorElement * h8;
+  MonitorElement * h9;
+  MonitorElement * i1;
+  MonitorElement * f1;
+  MonitorElement * s1;
+  float XMIN; float XMAX;
+  // event counter
+  int counter;
+  // back-end interface
+  DaqMonitorBEInterface * dbe;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+SMDQMSourceExample::SMDQMSourceExample( const edm::ParameterSet& iConfig )
+  : counter(0)
+{
+  // get hold of back-end interface
+  dbe = edm::Service<DaqMonitorBEInterface>().operator->();
+  
+  //We put this here for the moment since there is no better place 
+  edm::Service<MonitorDaemon> daemon;
+  daemon.operator->();
+
+  const int NBINS = 50; XMIN = 0; XMAX = 50;
+
+  // book some histograms here
+
+  // create and cd into new folder
+  dbe->setCurrentFolder("C1");
+  h1 = dbe->book1D("histo", "Example 1D histogram.", NBINS, XMIN, XMAX);
+  h2 = dbe->book2D("histo2", "Example 2 2D histogram.", NBINS, XMIN, XMAX, 
+		   NBINS, XMIN, XMAX);
+  // create and cd into new folder
+  dbe->setCurrentFolder("C1/C2");
+  h3 = dbe->book1D("histo3", "Example 3 1D histogram.", NBINS, XMIN, XMAX);
+  h4 = dbe->book1D("histo4", "Example 4 1D histogram.", NBINS, XMIN, XMAX);
+  h5 = dbe->book1D("histo5", "Example 5 1D histogram.", NBINS, XMIN, XMAX);
+  h6 = dbe->book1D("histo6", "Example 6 1D histogram.", NBINS, XMIN, XMAX);
+  // create and cd into new folder
+  dbe->setCurrentFolder("C1/C3");
+  const int NBINS2 = 10;
+  h7 = dbe->book1D("histo7", "Example 7 1D histogram.", NBINS2, XMIN, XMAX);
+  char temp[1024];
+  for(int i = 1; i <= NBINS2; ++i)
+    {
+      sprintf(temp, " bin no. %d", i);
+      h7->setBinLabel(i, temp);
+    }
+  i1 = dbe->bookInt("int1");
+  f1 = dbe->bookFloat("float1");
+  s1 = dbe->bookString("s1", "my string");
+
+  // create and cd into a new top level folder
+  dbe->setCurrentFolder("D1");
+  h8 = dbe->book1D("histo8", "Example 8 1D histogram.", NBINS, XMIN, XMAX);
+  // create and cd into new sublevel folder
+  dbe->setCurrentFolder("D1/D2");
+  h9 = dbe->book1D("histo9", "Example 9 1D histogram.", NBINS, XMIN, XMAX);
+
+  h2->setAxisTitle("Customized x-axis", 1);
+  h2->setAxisTitle("Customized y-axis", 2);
+
+  // assign tag to MEs h1, h2 and h7
+  const unsigned int detector_id = 17;
+  dbe->tag(h1, detector_id);
+  dbe->tag(h2, detector_id);
+  dbe->tag(h7, detector_id);
+  // tag full directory
+  dbe->tagContents("C1/C3", detector_id);
+
+  // assign tag to MEs h4 and h6
+  const unsigned int detector_id2 = 25;
+  const unsigned int detector_id3 = 50;
+  dbe->tag(h4, detector_id2);
+  dbe->tag(h6, detector_id3);
+
+  // contents of h5 & h6 will be reset at end of monitoring cycle
+  h5->setResetMe(true);
+  h6->setResetMe(true);
+  dbe->showDirStructure();
+}
+
+
+SMDQMSourceExample::~SMDQMSourceExample()
+{
+   
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+  
+}
+
+void SMDQMSourceExample::endJob(void)
+{
+  dbe->save("test.root");  
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void SMDQMSourceExample::analyze(const edm::Event& iEvent, 
+			       const edm::EventSetup& iSetup )
+{   
+  i1->Fill(4);
+  f1->Fill(-3.14);
+ 
+   // Filling the histogram with random data
+  srand( 0 );
+
+  if(counter%1000 == 0)
+    cout << " # of cycles = " << counter << endl;
+
+  for(int i = 0; i != 20; ++i ) 
+    {
+      float x = gRandom->Uniform(XMAX);
+      h1->Fill(x,1./log(x+1));
+      h3->Fill(x, 1);
+      h4->Fill(gRandom->Gaus(30, 3), 1.0);
+      h5->Fill(gRandom->Poisson(15), 0.5);
+      h6->Fill(gRandom->Gaus(25, 15), 1.0);
+      h7->Fill(gRandom->Gaus(25, 8), 1.0);
+      h8->Fill(gRandom->Gaus(25, 7), 1.0);
+      h9->Fill(gRandom->Gaus(25, 6), 1.0);
+    }
+
+  // fit h4 to gaussian
+  MonitorElementT<TNamed>* ob = dynamic_cast<MonitorElementT<TNamed>*> (h4);
+  if(ob)
+    {
+      TH1F * root_ob = dynamic_cast<TH1F *> (ob->operator->());
+      if(root_ob)root_ob->Fit("gaus","Q");
+    }
+  
+  for ( int i = 0; i != 10; ++i ) 
+    {
+      float x = gRandom->Gaus(15, 7);
+      float y = gRandom->Gaus(20, 5);
+      h2->Fill(x,y);
+    }
+  //      (*(*i1))++;
+  //usleep(1000000);
+
+  ++counter;
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(SMDQMSourceExample);
