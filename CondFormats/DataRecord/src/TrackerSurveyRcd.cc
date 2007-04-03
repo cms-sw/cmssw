ebc8f97d5050279e09384d511cf1205e1b4dd8d6
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-04-03 15:24:57 +0000

    Added records needed for alignment with constraints (contain survey info.).

---
+++ CondFormats/DataRecord/src/TrackerSurveyRcd.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/DataRecord/interface/TrackerSurveyRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(TrackerSurveyRcd);
