a980b7e2ca7519b8fa8f929b1af8269c42748fa3
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2007-04-03 02:34:07 +0000

    add record for beam spot objects

---
+++ CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_BeamSpotObjectsRcd_h
+#define DataRecord_BeamSpotObjectsRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     BeamSpotObjectsRcd
+// 
+/**\class BeamSpotObjectsRcd BeamSpotObjectsRcd.h CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Tue Mar  6 19:34:33 CST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class BeamSpotObjectsRcd : public edm::eventsetup::EventSetupRecordImplementation<BeamSpotObjectsRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/BeamSpotObjectsRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     BeamSpotObjectsRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Tue Mar  6 19:34:33 CST 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/BeamSpotObjectsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(BeamSpotObjectsRcd);
