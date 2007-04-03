8fccdbb55c0e9a8932d93e2af8af0a39433765d3
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-04-03 15:48:26 +0000

    Create survey data objects for alignment.

--- CondCore/AlignmentPlugins/src/plugin.cc
+++ CondCore/AlignmentPlugins/src/plugin.cc
@@ -9,12 +9,15 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/Alignment/interface/Alignments.h"
 #include "CondFormats/Alignment/interface/AlignmentErrors.h"
+#include "CondFormats/Alignment/interface/SurveyErrors.h"
 #include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/TrackerAlignmentErrorRcd.h"
 #include "CondFormats/DataRecord/interface/DTAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/DTAlignmentErrorRcd.h"
 #include "CondFormats/DataRecord/interface/CSCAlignmentRcd.h"
 #include "CondFormats/DataRecord/interface/CSCAlignmentErrorRcd.h"
+#include "CondFormats/DataRecord/interface/TrackerSurveyRcd.h"
+#include "CondFormats/DataRecord/interface/TrackerSurveyErrorRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(TrackerAlignmentRcd,Alignments);
@@ -23,3 +26,5 @@ REGISTER_PLUGIN(DTAlignmentRcd,Alignments);
 REGISTER_PLUGIN(DTAlignmentErrorRcd,AlignmentErrors);
 REGISTER_PLUGIN(CSCAlignmentRcd,Alignments);
 REGISTER_PLUGIN(CSCAlignmentErrorRcd,AlignmentErrors);
+REGISTER_PLUGIN(TrackerSurveyRcd,Alignments);
+REGISTER_PLUGIN(TrackerSurveyErrorRcd,SurveyErrors);
