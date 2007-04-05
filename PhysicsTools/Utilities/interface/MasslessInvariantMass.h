e0bc2d5b5994625f8c697b198a897a65f779e852
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-04-05 09:01:02 +0000

    added second parameter to binary functors

--- PhysicsTools/Utilities/interface/MasslessInvariantMass.h
+++ PhysicsTools/Utilities/interface/MasslessInvariantMass.h
@@ -3,9 +3,9 @@
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "DataFormats/Math/interface/Vector3D.h"
 
-template<typename T>
+template<typename T1, typename T2 = T1>
 struct MasslessInvariantMass {
-  double operator()( const T & t1, const T & t2 ) const {
+  double operator()( const T1 & t1, const T2 & t2 ) const {
     math::XYZVector p1 = t1.momentum(), p2 = t2.momentum();
     math::XYZTLorentzVector v1( p1.x(), p1.y(), p1.z(), p1.r() ), v2( p2.x(), p2.y(), p2.z(), p2.r() );
     return ( v1 + v2 ).mass();
