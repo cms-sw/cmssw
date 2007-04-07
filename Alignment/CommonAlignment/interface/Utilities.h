d884b9704f6c560eb33f2327d9bc7b0b6abf3f53
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-04-07 03:29:04 +0000

    Move type definitions to CondFormats/Alignment.

--- Alignment/CommonAlignment/interface/Utilities.h
+++ Alignment/CommonAlignment/interface/Utilities.h
@@ -3,41 +3,17 @@
 
 /** \namespace align
  *
- *  Namespace for common type definitions and calculations in alignment.
+ *  Namespace for common calculations in alignment.
  *
- *  $Date: 2007/03/16 16:12:45 $
- *  $Revision: 1.4 $
+ *  $Date: 2007/03/16 17:59:58 $
+ *  $Revision: 1.5 $
  *  \author Chung Khim Lae
  */
 
-#include <vector>
-
-#include "DataFormats/CLHEP/interface/AlgebraicObjects.h"
-#include "DataFormats/GeometrySurface/interface/TkRotation.h"
-#include "DataFormats/GeometryVector/interface/GlobalTag.h"
-#include "DataFormats/GeometryVector/interface/LocalTag.h"
-#include "DataFormats/GeometryVector/interface/Point3DBase.h"
-#include "DataFormats/GeometryVector/interface/Vector3DBase.h"
+#include "CondFormats/Alignment/interface/Definitions.h"
 
 namespace align
 {
-  typedef double Scalar;
-
-  typedef   TkRotation<Scalar>            RotationType;
-  typedef  Point3DBase<Scalar, GlobalTag> PositionType;
-  typedef  Point3DBase<Scalar, GlobalTag> GlobalPoint;
-  typedef  Point3DBase<Scalar,  LocalTag> LocalPoint;
-  typedef Vector3DBase<Scalar, GlobalTag> GlobalVector;
-  typedef Vector3DBase<Scalar,  LocalTag> LocalVector;
-
-  typedef AlgebraicVector    EulerAngles;
-  typedef AlgebraicSymMatrix ErrorMatrix;
-
-  typedef std::vector<GlobalPoint>  GlobalPoints;
-  typedef std::vector<GlobalVector> GlobalVectors;
-  typedef std::vector<LocalPoint>   LocalPoints;
-  typedef std::vector<LocalVector>  LocalVectors;
-
   /// Convert rotation matrix to angles about x-, y-, z-axes (frame rotation).
   EulerAngles toAngles(
 		       const RotationType&
