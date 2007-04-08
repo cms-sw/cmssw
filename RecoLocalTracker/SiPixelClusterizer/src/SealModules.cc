e440f568646cdd984c04a8bb26446d94c7c248f3
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-08 21:51:33 +0000

    Adapt to new plug-in manager

--- RecoLocalTracker/SiPixelClusterizer/src/SealModules.cc
+++ RecoLocalTracker/SiPixelClusterizer/src/SealModules.cc
@@ -1,5 +1,5 @@
 
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalTracker/SiPixelClusterizer/interface/SiPixelClusterProducer.h"
