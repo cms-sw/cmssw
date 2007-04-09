5fa0efed536fc0a1b38d240c60791cc2fd23a808
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-04-09 21:02:58 +0000

    removed PluginManager/...

--- EventFilter/EcalRawToDigi/src/SealModule.cc
+++ EventFilter/EcalRawToDigi/src/SealModule.cc
@@ -1,6 +1,5 @@
 #include <FWCore/Framework/interface/MakerMacros.h>
 #include "EventFilter/EcalRawToDigi/interface/EcalRawToDigi.h"
-#include "PluginManager/ModuleDef.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EcalRawToDigi);
