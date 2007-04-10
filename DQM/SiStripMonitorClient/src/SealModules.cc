b3f6e504d5936e822d077ab2ff391f57af378b69
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2007-04-10 21:07:19 +0000

    changes due to plugin migration

--- DQM/SiStripMonitorClient/src/SealModules.cc
+++ DQM/SiStripMonitorClient/src/SealModules.cc
@@ -1,4 +1,4 @@
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "DQM/SiStripMonitorClient/interface/SiStripOfflineDQM.h"
