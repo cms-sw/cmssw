f16e5c7b8d0524460b91f4a588ece60a36ed77c2
Pedro Arce <sha1-a7e83fd644adcfec94499d67c7f8f23354d9a549@cern.ch>
2007-04-10 09:33:25 +0000

    no using namespace CLHEP;

--- Alignment/CocoaUtilities/interface/CocoaGlobals.h
+++ Alignment/CocoaUtilities/interface/CocoaGlobals.h
@@ -1,10 +1,7 @@
 #ifndef __GLOBALS_HH
 #define __GLOBALS_HH
 
-//#include <ospace/std/string>
 #include <string>
-#include <float.h>
-#include <math.h>
 
 typedef std::string ALIstring;
 
@@ -18,13 +15,9 @@ typedef unsigned int ALIuint;
 
 typedef bool ALIbool;
 
-//const double ZERO = 1.E-50
-const ALIdouble PI = M_PI; //2 * acos(0.0);
+const ALIdouble PI = 3.1415926;
 
-const double ALI_DBL_MAX = DBL_MAX;
-const double ALI_DBL_MIN = 10./ALI_DBL_MAX;
-
-namespace CLHEP{}
-using namespace CLHEP;
+const double ALI_DBL_MAX = 1.E99;
+const double ALI_DBL_MIN = -1.E99;
 
 #endif
