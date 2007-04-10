b06221514f24762ed407f5600d63ebeb39656056
Michal Szleper <sha1-8dc5ee44d1f0d27d89108086e7cf38e4a70bd448@cern.ch>
2007-04-10 20:58:44 +0000

    Include pi/e ratio applied at RecHit level

--- RecoLocalCalo/CaloTowersCreator/interface/EScales.h
+++ RecoLocalCalo/CaloTowersCreator/interface/EScales.h
@@ -2,6 +2,12 @@
 #define EScales_h
 
 struct EScales{
+  double HBPiOvere;
+  double HESPiOvere;
+  double HEDPiOvere;
+  double HOPiOvere;
+  double HF1PiOvere;
+  double HF2PiOvere;
   double EBScale;
   double EEScale;
   double HBScale;
