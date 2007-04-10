d71a44dca72fa1dcced3f1ef856b7ced1516a6cf
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-04-10 10:07:23 +0000

    adding PHYSICS=13

--- DataFormats/EcalRawData/interface/EcalDCCHeaderBlock.h
+++ DataFormats/EcalRawData/interface/EcalDCCHeaderBlock.h
@@ -7,7 +7,7 @@
  *  Container for ECAL specific DCC Header information
  *
  *
- *  $Id: EcalDCCHeaderBlock.h,v 1.7 2007/01/08 15:30:49 meridian Exp $
+ *  $Id: EcalDCCHeaderBlock.h,v 1.8 2007/04/03 17:04:37 franzoni Exp $
  */
 
 #include <vector>
@@ -37,7 +37,8 @@ class EcalDCCHeaderBlock
     PEDESTAL_STD = 9,
     PEDESTAL_OFFSET_SCAN = 10,
     PEDESTAL_25NS_SCAN = 11,
-    LED_STD= 12
+    LED_STD= 12,
+    PHYSICS= 12
   };
   
   enum EcalBasicTriggerType{
