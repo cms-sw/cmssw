830e7f14be651384b00290464307c8cb438567fd
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2007-04-11 23:43:07 +0000

    Added dependence of CkfComponentsRecord from TkStripCPERecord, TkPixelCPERecord

--- RecoTracker/Record/interface/CkfComponentsRecord.h
+++ RecoTracker/Record/interface/CkfComponentsRecord.h
@@ -4,6 +4,8 @@
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
 #include "RecoLocalTracker/Records/interface/TrackerCPERecord.h"
+#include "RecoLocalTracker/Records/interface/TkStripCPERecord.h"
+#include "RecoLocalTracker/Records/interface/TkPixelCPERecord.h"
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "RecoTracker/Record/interface/TrackerRecoGeometryRecord.h"
 #include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
@@ -17,6 +19,8 @@
 
 class CkfComponentsRecord : public edm::eventsetup::DependentRecordImplementation<CkfComponentsRecord,
 			    boost::mpl::vector<TrackerDigiGeometryRecord,
+                                               TkPixelCPERecord,
+                                               TkStripCPERecord,
 			                       TrackerCPERecord,
 			                       TransientRecHitRecord,
                                                TrackingComponentsRecord,
