24a6f0b39841782aa0257b5617233e8a8e31fca5
Javier Fernandez Menendez <sha1-96a07b193caeb9d667466687d23e726533fc5d4f@cern.ch>
2007-04-11 11:44:54 +0000

    First creation of class AlignmentMuonSelector

---
+++ Alignment/CommonAlignmentProducer/interface/AlignmentMuonSelectorModule.h
@@ -0,0 +1,52 @@
+/** \class AlignmentMuonSelectorModule
+ *
+ * selects a subset of a muon collection and clones
+ * Track, TrackExtra parts and RecHits collection
+ * for SA, GB and Tracker Only options
+ * 
+ * \author Javier Fernandez, IFCA
+ *
+ * \version $Revision: 1.1 $
+ *
+ * $Id: AlignmentMuonSelectorModule.h,v 1.0 2007/03/22 12:22:11 jfernan2 Exp $
+ *
+ */
+
+#include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
+#include "Alignment/CommonAlignmentProducer/interface/AlignmentMuonSelector.h"
+
+// the following include is necessary to clone all track branches
+// including recoTrackExtras and TrackingRecHitsOwned.
+// if you remove it the code will compile, but the cloned
+// tracks have only the recoMuons branch!
+#include "PhysicsTools/RecoAlgos/interface/MuonSelector.h"
+
+struct MuonConfigSelector {
+
+  typedef std::vector<const reco::Muon*> container;
+  typedef container::const_iterator const_iterator;
+  typedef reco::MuonCollection collection; 
+
+  MuonConfigSelector( const edm::ParameterSet & cfg ) :
+    theSelector(cfg) {}
+
+  const_iterator begin() const { return selected_.begin(); }
+  const_iterator end() const { return selected_.end(); }
+  size_t size() const { return selected_.size(); }
+
+  void select( const edm::Handle<reco::MuonCollection> & c,  const edm::Event & evt) {
+    all_.clear();
+    selected_.clear();
+    for( reco::MuonCollection::const_iterator i=c.product()->begin();i!=c.product()->end();++i){
+      all_.push_back(& * i );
+    }
+    selected_=theSelector.select(all_,evt);
+  }
+
+private:
+  container all_,selected_;
+  AlignmentMuonSelector theSelector;
+};
+
+typedef ObjectSelector<MuonConfigSelector>  AlignmentMuonSelectorModule;
+
--- Alignment/CommonAlignmentProducer/src/SealModule.cc
+++ Alignment/CommonAlignmentProducer/src/SealModule.cc
@@ -3,8 +3,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "Alignment/CommonAlignmentProducer/interface/AlignmentProducer.h"
+#include "Alignment/CommonAlignmentProducer/interface/AlignmentMuonSelectorModule.h"
 #include "Alignment/CommonAlignmentProducer/interface/AlignmentTrackSelectorModule.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_LOOPER( AlignmentProducer );
 DEFINE_ANOTHER_FWK_MODULE( AlignmentTrackSelectorModule );
+DEFINE_ANOTHER_FWK_MODULE( AlignmentMuonSelectorModule );
