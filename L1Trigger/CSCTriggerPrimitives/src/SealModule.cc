3efab8d250452d65d412405f7e75ba74525191b2
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2007-04-11 10:04:22 +0000

    Adapt to new plug-in manager.

--- L1Trigger/CSCTriggerPrimitives/src/SealModule.cc
+++ L1Trigger/CSCTriggerPrimitives/src/SealModule.cc
@@ -1,5 +1,5 @@
-#include <PluginManager/ModuleDef.h>
-#include <FWCore/Framework/interface/MakerMacros.h>
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <L1Trigger/CSCTriggerPrimitives/src/CSCTriggerPrimitivesProducer.h>
 
