1aeaf5f1cafe584850974978c542df33aa4ff93b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-11 23:23:45 +0000

    For backward compatibility

--- FWCore/Framework/interface/MakerMacros.h
+++ FWCore/Framework/interface/MakerMacros.h
@@ -10,4 +10,6 @@
 #define DEFINE_ANOTHER_FWK_MODULE(type) \
   DEFINE_EDM_PLUGIN (edm::MakerPluginFactory,edm::WorkerMaker<type>,#type)
 
+// for backward comatibility
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #endif
