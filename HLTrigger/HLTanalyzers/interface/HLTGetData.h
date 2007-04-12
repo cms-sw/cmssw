2d54494bc3e62a21981c8a01c9d1a3ef2b70f698
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-04-12 09:57:13 +0000

    Extension to HLTGetData from Bryan (added missing digi products).

--- HLTrigger/HLTanalyzers/interface/HLTGetData.h
+++ HLTrigger/HLTanalyzers/interface/HLTGetData.h
@@ -7,7 +7,7 @@
  *  This class is an EDAnalyzer implementing a "get data into RAM"
  *  functionality, to simulate online FF running/timimg.
  *
- *  $Date: 2007/03/28 12:58:54 $
+ *  $Date: 2007/04/11 17:51:05 $
  *  $Revision: 1.1 $
  *
  *  \author various
@@ -33,6 +33,15 @@ class HLTGetData : public edm::EDAnalyzer {
   edm::InputTag EBdigiCollection_;
   edm::InputTag EEdigiCollection_;
   edm::InputTag ESdigiCollection_;      
+  edm::InputTag HBHEdigiCollection_;
+  edm::InputTag HOdigiCollection_;
+  edm::InputTag HFdigiCollection_;      
+  edm::InputTag PXLdigiCollection_;      
+  edm::InputTag SSTdigiCollection_;      
+  edm::InputTag CSCStripdigiCollection_;      
+  edm::InputTag CSCWiredigiCollection_;      
+  edm::InputTag DTdigiCollection_;      
+  edm::InputTag RPCdigiCollection_;      
 
 };
 
--- HLTrigger/HLTanalyzers/src/HLTGetData.cc
+++ HLTrigger/HLTanalyzers/src/HLTGetData.cc
@@ -2,8 +2,8 @@
  *
  * See header file for documentation
  *
- *  $Date: 2007/03/28 20:33:07 $
- *  $Revision: 1.2 $
+ *  $Date: 2007/04/11 17:51:06 $
+ *  $Revision: 1.1 $
  *
  *  \author various
  *
@@ -28,6 +28,19 @@
 #include "DataFormats/EcalDigi/interface/EBDataFrame.h"
 #include "DataFormats/EcalDigi/interface/EEDataFrame.h"
 #include "DataFormats/EcalDigi/interface/ESDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
+#include "DataFormats/HcalDigi/interface/HODataFrame.h"
+#include "DataFormats/HcalDigi/interface/HFDataFrame.h"
+#include "DataFormats/MuonData/interface/MuonDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCWireDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCWireDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
+#include "DataFormats/DTDigi/interface/DTDigi.h"
+#include "DataFormats/DTDigi/interface/DTDigiCollection.h"
+#include "DataFormats/RPCDigi/interface/RPCDigi.h"
+#include "DataFormats/RPCDigi/interface/RPCDigiCollection.h"
 
 using namespace edm;
 using namespace std;
@@ -40,6 +53,15 @@ HLTGetData::HLTGetData(const edm::ParameterSet& ps)
   EBdigiCollection_ = ps.getParameter<edm::InputTag>("EBdigiCollection");
   EEdigiCollection_ = ps.getParameter<edm::InputTag>("EEdigiCollection");
   ESdigiCollection_ = ps.getParameter<edm::InputTag>("ESdigiCollection");
+  HBHEdigiCollection_ = ps.getParameter<edm::InputTag>("HBHEdigiCollection");
+  HOdigiCollection_   = ps.getParameter<edm::InputTag>("HOdigiCollection");
+  HFdigiCollection_   = ps.getParameter<edm::InputTag>("HFdigiCollection");
+  PXLdigiCollection_ = ps.getParameter<edm::InputTag>("SiPixeldigiCollection");
+  SSTdigiCollection_ = ps.getParameter<edm::InputTag>("SiStripdigiCollection");
+  CSCStripdigiCollection_ = ps.getParameter<edm::InputTag>("CSCStripdigiCollection");
+  CSCWiredigiCollection_ = ps.getParameter<edm::InputTag>("CSCWiredigiCollection");
+  DTdigiCollection_ = ps.getParameter<edm::InputTag>("DTdigiCollection");
+  RPCdigiCollection_ = ps.getParameter<edm::InputTag>("RPCdigiCollection");
 }
 
 HLTGetData::~HLTGetData()
@@ -53,42 +75,111 @@ HLTGetData::~HLTGetData()
 void
 HLTGetData::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+    using namespace edm;
 
     edm::Handle< DetSetVector<PixelDigi> >  input;
-    iEvent.getByLabel("siPixelDigis", input);
-     auto_ptr<DetSetVector<PixelDigi> > NewPixelDigi(new DetSetVector<PixelDigi> );
-     DetSetVector<PixelDigi>* tt = NewPixelDigi.get();
-     *tt = *input.product();
+    iEvent.getByLabel(PXLdigiCollection_, input);
+    auto_ptr<DetSetVector<PixelDigi> > NewPixelDigi(new DetSetVector<PixelDigi> );
+    DetSetVector<PixelDigi>* tt = NewPixelDigi.get();
+    *tt = *input.product();
 
-     edm::Handle< edm::DetSetVector<SiStripDigi> >  input2;
-     iEvent.getByLabel("siStripDigis",input2);
-     auto_ptr<DetSetVector<SiStripDigi> > NewSiDigi(new DetSetVector<SiStripDigi> );
-     DetSetVector<SiStripDigi>* uu = NewSiDigi.get();
-     *uu = *input2.product();
+    edm::Handle< edm::DetSetVector<SiStripDigi> >  input2;
+    iEvent.getByLabel(SSTdigiCollection_, input2);
+    auto_ptr<DetSetVector<SiStripDigi> > NewSiDigi(new DetSetVector<SiStripDigi> );
+    DetSetVector<SiStripDigi>* uu = NewSiDigi.get();
+    *uu = *input2.product();
 
-     
-     Handle<EBDigiCollection> EcalDigiEB;
-     Handle<EEDigiCollection> EcalDigiEE;
-     Handle<ESDigiCollection> EcalDigiES;
-     const EBDigiCollection* EBdigis =0;
-     const EEDigiCollection* EEdigis =0;
-     const ESDigiCollection* ESdigis =0; 
+    Handle<EBDigiCollection> EcalDigiEB;
+    Handle<EEDigiCollection> EcalDigiEE;
+    Handle<ESDigiCollection> EcalDigiES;
+    const EBDigiCollection* EBdigis = 0;
+    const EEDigiCollection* EEdigis = 0;
+    const ESDigiCollection* ESdigis = 0; 
   
-  
-     iEvent.getByLabel( EBdigiCollection_, EcalDigiEB );
-     EBdigis = EcalDigiEB.product();
-     LogDebug("DigiInfo") << "total # EBdigis: " << EBdigis->size() ;
+    iEvent.getByLabel( EBdigiCollection_, EcalDigiEB );
+    EBdigis = EcalDigiEB.product();
+    LogDebug("DigiInfo") << "total # EBdigis: " << EBdigis->size() ;
      
-     iEvent.getByLabel( EEdigiCollection_, EcalDigiEE );
-     EEdigis = EcalDigiEE.product();
-     LogDebug("DigiInfo") << "total # EEdigis: " << EEdigis->size() ;
+    iEvent.getByLabel( EEdigiCollection_, EcalDigiEE );
+    EEdigis = EcalDigiEE.product();
+    LogDebug("DigiInfo") << "total # EEdigis: " << EEdigis->size() ;
     
     iEvent.getByLabel( ESdigiCollection_, EcalDigiES );
     ESdigis = EcalDigiES.product();
     LogDebug("DigiInfo") << "total # ESdigis: " << ESdigis->size() ;
-   
 
+    Handle<HBHEDigiCollection> HcalDigiHBHE ; 
+    Handle<HODigiCollection> HcalDigiHO ; 
+    Handle<HFDigiCollection> HcalDigiHF ; 
+    const HBHEDigiCollection* HBHEdigis = 0 ;
+    const HODigiCollection* HOdigis = 0 ;
+    const HFDigiCollection* HFdigis = 0 ; 
+    
+    iEvent.getByLabel( HBHEdigiCollection_, HcalDigiHBHE );
+    HBHEdigis = HcalDigiHBHE.product();
+    LogDebug("DigiInfo") << "total # HBHEdigis: " << HBHEdigis->size() ;
+     
+    iEvent.getByLabel( HOdigiCollection_, HcalDigiHO );
+    HOdigis = HcalDigiHO.product();
+    LogDebug("DigiInfo") << "total # HOdigis: " << HOdigis->size() ;
+    
+    iEvent.getByLabel( HFdigiCollection_, HcalDigiHF );
+    HFdigis = HcalDigiHF.product();
+    LogDebug("DigiInfo") << "total # HFdigis: " << HFdigis->size() ;
+    
+    Handle<CSCStripDigiCollection> CSCDigiStrip ; 
+    Handle<CSCWireDigiCollection> CSCDigiWire ; 
+    const CSCStripDigiCollection* CSCstripdigis ; 
+    const CSCWireDigiCollection* CSCwiredigis ; 
 
+    iEvent.getByLabel( CSCStripdigiCollection_, CSCDigiStrip );
+    CSCstripdigis = CSCDigiStrip.product();
+    iEvent.getByLabel( CSCWiredigiCollection_, CSCDigiWire );
+    CSCwiredigis = CSCDigiWire.product();
 
+    int numDigis = 0 ; 
+    for (CSCStripDigiCollection::DigiRangeIterator iter=CSCDigiStrip->begin();
+         iter!=CSCDigiStrip->end(); iter++) {
+        for ( vector<CSCStripDigi>::const_iterator digiIter = (*iter).second.first;
+              digiIter != (*iter).second.second; digiIter++) numDigis++ ;
+    }
+    LogDebug("DigiInfo") << "total # CSCstripdigis: " << numDigis ;
+    numDigis = 0 ; 
+    for (CSCWireDigiCollection::DigiRangeIterator iter=CSCDigiWire->begin();
+         iter!=CSCDigiWire->end(); iter++) {
+        for ( vector<CSCWireDigi>::const_iterator digiIter = (*iter).second.first;
+              digiIter != (*iter).second.second; digiIter++) numDigis++ ;
+    }
+    LogDebug("DigiInfo") << "total # CSCwiredigis: " << numDigis ;
+
+    Handle<DTDigiCollection> DTDigiHandle ; 
+    const DTDigiCollection* DTdigis ; 
+
+    iEvent.getByLabel( DTdigiCollection_, DTDigiHandle );
+    DTdigis = DTDigiHandle.product();
+
+    numDigis = 0 ; 
+    for (DTDigiCollection::DigiRangeIterator iter=DTDigiHandle->begin();
+         iter!=DTDigiHandle->end(); iter++) {
+        for ( vector<DTDigi>::const_iterator digiIter = (*iter).second.first;
+              digiIter != (*iter).second.second; digiIter++) numDigis++ ;
+    }
+    LogDebug("DigiInfo") << "total # DTdigis: " << numDigis ;
+
+    Handle<RPCDigiCollection> RPCDigiHandle ; 
+    const RPCDigiCollection* RPCdigis ; 
+
+    iEvent.getByLabel( RPCdigiCollection_, RPCDigiHandle );
+    RPCdigis = RPCDigiHandle.product();
+
+    numDigis = 0 ; 
+    for (RPCDigiCollection::DigiRangeIterator iter=RPCDigiHandle->begin();
+         iter!=RPCDigiHandle->end(); iter++) {
+        for ( vector<RPCDigi>::const_iterator digiIter = (*iter).second.first;
+              digiIter != (*iter).second.second; digiIter++) numDigis++ ;
+    }
+    LogDebug("DigiInfo") << "total # RPCdigis: " << numDigis ;
+
+    LogDebug("DigiInfo") << "***--------------- End of Event -----------------***" ;  
+    
 }
