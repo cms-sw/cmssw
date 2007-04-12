e33f214e752cfbc911efa30acaf0f3e8d3f68b8d
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2007-04-12 00:45:41 +0000

    adapted to new Records for strip and pixel CPE

--- TrackingTools/Records/interface/TransientRecHitRecord.h
+++ TrackingTools/Records/interface/TransientRecHitRecord.h
@@ -6,11 +6,13 @@
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "RecoLocalTracker/Records/interface/TrackerCPERecord.h"
+#include "RecoLocalTracker/Records/interface/TkStripCPERecord.h"
+#include "RecoLocalTracker/Records/interface/TkPixelCPERecord.h"
 #include "Geometry/Records/interface/GlobalTrackingGeometryRecord.h"
 
 #include "boost/mpl/vector.hpp"
 
 class TransientRecHitRecord : public edm::eventsetup::DependentRecordImplementation<TransientRecHitRecord,
-			      boost::mpl::vector<IdealGeometryRecord,TrackerDigiGeometryRecord,TrackerCPERecord,GlobalTrackingGeometryRecord> > {};
+			      boost::mpl::vector<IdealGeometryRecord,TrackerDigiGeometryRecord,TrackerCPERecord,TkStripCPERecord,TkPixelCPERecord,GlobalTrackingGeometryRecord> > {};
 #endif 
 
