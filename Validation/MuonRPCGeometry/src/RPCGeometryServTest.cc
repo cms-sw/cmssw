da61f5233e819fbade08f2296f4f6938f28e8ab7
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2007-04-17 14:16:27 +0000

    upgrate for eta partition printout

--- Validation/MuonRPCGeometry/src/RPCGeometryServTest.cc
+++ Validation/MuonRPCGeometry/src/RPCGeometryServTest.cc
@@ -22,6 +22,7 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <map>
 #include <iomanip>
 #include <set>
 
@@ -43,6 +44,9 @@ class RPCGeometryServTest : public edm::EDAnalyzer {
   const int dashedLineWidth_;
   const std::string dashedLine_;
   const std::string myName_;
+  std::map<int, std::pair<double, double> > barzranges;
+  std::map<int, std::pair<double, double> > forRranges;
+  std::map<int, std::pair<double, double> > bacRranges;
 };
 
 RPCGeometryServTest::RPCGeometryServTest( const edm::ParameterSet& iConfig )
@@ -107,10 +111,78 @@ RPCGeometryServTest::analyze( const edm::Event& iEvent, const edm::EventSetup& i
 	std::cout <<s.name()<<" eta partition "<<s.eta_partition()<< " nroll="<<ch->nrolls()
 		  <<" z="<<g.z()<<" phi="<<g.phi()<<" R="<<g.perp()
 		  <<std::endl;
+
+	if((*r)->id().region() ==  0 ){
+	  if (barzranges.find(s.eta_partition()) != barzranges.end()){
+	    std::pair<double, double> cic=barzranges.find(s.eta_partition())->second;
+	    double cmin = cic.first;
+	    double cmax = cic.second;
+	    if (g.z()<cmin)
+	      cmin = g.z();
+	    if (g.z()>cmax)
+	      cmax = g.z();
+	    std::pair<double,double>  cic2(cmin,cmax);
+	    barzranges[s.eta_partition()]=cic2;
+	  }else{
+	    std::pair<double, double> cic(g.z(),g.z());
+	    barzranges[s.eta_partition()]=cic;
+	  }
+	}
+	else if((*r)->id().region() ==  +1 ){
+	  if (forRranges.find(s.eta_partition()) != forRranges.end()){
+	    std::pair<double, double> cic=forRranges.find(s.eta_partition())->second;
+	    double cmin = cic.first;
+	    double cmax = cic.second;
+	    if (g.perp()<cmin)
+	      cmin = g.perp();
+	    if (g.perp()>cmax)
+	      cmax = g.perp();
+	    std::pair<double,double>  cic2(cmin,cmax);
+	    forRranges[s.eta_partition()]=cic2;
+	  }else{
+	    std::pair<double, double> cic(g.perp(),g.perp());
+	    forRranges[s.eta_partition()]=cic;
+	  }
+	}
+	else if((*r)->id().region() ==  -1 ){
+ 	  if (bacRranges.find(s.eta_partition()) != bacRranges.end()){
+	    std::pair<double, double> cic=bacRranges.find(s.eta_partition())->second;
+	    double cmin = cic.first;
+	    double cmax = cic.second;
+	    if (g.perp()<cmin)
+	      cmin = g.perp();
+	    if (g.perp()>cmax)
+	      cmax = g.perp();
+	    std::pair<double,double>  cic2(cmin,cmax);
+	    bacRranges[s.eta_partition()]=cic2;
+	  }else{
+	    std::pair<double, double> cic(g.perp(),g.perp());
+	    bacRranges[s.eta_partition()]=cic;
+	  }
+	}
       }
     }
   }
+
   std::cout <<std::endl;
+  std::map<int, std::pair<double, double> >::iterator ieta;
+
+  for (ieta=bacRranges.begin(); ieta != bacRranges.end(); ieta++){
+    std::cout <<" Eta "<<ieta->first<<" Radii = ( "<<ieta->second.first
+	      <<", "<<ieta->second.second<<")"<<std::endl;
+  }
+
+  for (ieta=barzranges.begin(); ieta != barzranges.end(); ieta++){
+    std::cout <<" Eta "<<ieta->first<<" Z = ( "<<ieta->second.first
+	      <<", "<<ieta->second.second<<")"<<std::endl;
+  }
+
+  for (ieta=forRranges.begin(); ieta != forRranges.end(); ieta++){
+    std::cout <<" Eta "<<ieta->first<<" Radii = ( "<<ieta->second.first
+	      <<", "<<ieta->second.second<<")"<<std::endl;
+  }
+
+
   std::cout << dashedLine_ << " end" << std::endl;
 }
 
