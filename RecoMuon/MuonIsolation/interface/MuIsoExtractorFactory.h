6bb92c981880de049fd6217388777c3108444f4d
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-04-17 02:36:53 +0000

    plugin migration

--- RecoMuon/MuonIsolation/interface/MuIsoExtractorFactory.h
+++ RecoMuon/MuonIsolation/interface/MuIsoExtractorFactory.h
@@ -6,11 +6,12 @@ namespace edm {class ParameterSet;}
 namespace muonisolation { class MuIsoExtractor; }
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 
-class MuIsoExtractorFactory : public 
-    seal::PluginFactory< muonisolation::MuIsoExtractor* (const edm::ParameterSet&) > { 
-public:
-  MuIsoExtractorFactory();
-  virtual ~MuIsoExtractorFactory();
-  static MuIsoExtractorFactory * get();
-};
+//class MuIsoExtractorFactory : public 
+//    edmplugin::PluginFactory< muonisolation::MuIsoExtractor* (const edm::ParameterSet&) > { 
+//public:
+//  MuIsoExtractorFactory();
+//  virtual ~MuIsoExtractorFactory();
+//  static MuIsoExtractorFactory * get();
+//};
+typedef edmplugin::PluginFactory< muonisolation::MuIsoExtractor* (const edm::ParameterSet&) > MuIsoExtractorFactory;
 #endif
