396c38c67fe91e5e6eb2beb379e4ffb901a0337f
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-04-17 02:40:46 +0000

    use pluginfactory macro

--- RecoMuon/MuonIsolation/src/MuIsoExtractorFactory.cc
+++ RecoMuon/MuonIsolation/src/MuIsoExtractorFactory.cc
@@ -1,19 +1,4 @@
 #include "RecoMuon/MuonIsolation/interface/MuIsoExtractorFactory.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "RecoMuon/MuonIsolation/interface/MuIsoExtractor.h"
 
-
-MuIsoExtractorFactory::MuIsoExtractorFactory()
-  : seal::PluginFactory<muonisolation::MuIsoExtractor*(
-        const edm::ParameterSet & p)>("MuIsoExtractorFactory")
-{ }
-
-MuIsoExtractorFactory::~MuIsoExtractorFactory()
-{ }
-
-MuIsoExtractorFactory * MuIsoExtractorFactory::get()
-{
-  static MuIsoExtractorFactory theMuIsoExtractorFactory;
-  return & theMuIsoExtractorFactory;
-}
+EDM_REGISTER_PLUGINFACTORY(MuIsoExtractorFactory, "MuIsoExtractorFactory");
 
