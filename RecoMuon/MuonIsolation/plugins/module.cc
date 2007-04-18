50b0a2ddc85efdc908f09a2edbd9431413a43754
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-04-18 21:57:52 +0000

    renamed from SealModule.cc; adapt to move of concrete extractor headers

---
+++ RecoMuon/MuonIsolation/plugins/module.cc
@@ -0,0 +1,14 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+
+#include "RecoMuon/MuonIsolation/interface/MuIsoExtractor.h"
+#include "RecoMuon/MuonIsolation/interface/MuIsoExtractorFactory.h"
+#include "TrackExtractor.h"
+#include "CaloExtractor.h"
+#include "CaloExtractorByAssociator.h"
+#include "ExtractorFromDeposits.h"
+DEFINE_EDM_PLUGIN(MuIsoExtractorFactory, muonisolation::TrackExtractor, "TrackExtractor");
+DEFINE_EDM_PLUGIN(MuIsoExtractorFactory, muonisolation::CaloExtractor, "CaloExtractor");
+DEFINE_EDM_PLUGIN(MuIsoExtractorFactory, muonisolation::CaloExtractorByAssociator, "CaloExtractorByAssociator");
+DEFINE_EDM_PLUGIN(MuIsoExtractorFactory, muonisolation::ExtractorFromDeposits, "ExtractorFromDeposits");
