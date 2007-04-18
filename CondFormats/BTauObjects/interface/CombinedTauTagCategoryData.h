1ef3e5faab1e09c92be6ddbf442d4b6a90680abb
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2007-04-18 08:45:55 +0000

    removed mistaken lines

--- CondFormats/BTauObjects/interface/CombinedTauTagCategoryData.h
+++ CondFormats/BTauObjects/interface/CombinedTauTagCategoryData.h
@@ -4,9 +4,5 @@
 struct CombinedTauTagCategoryData {
   int truthmatched1orfake0candidates,theTagVar,signaltks_n;
   float EtMin,EtMax;
-<<<<<<< CombinedTauTagCategoryData.h
-=======
-  reco::TaggingVariableName theTagVarName;
->>>>>>> 1.2
 };
 #endif
