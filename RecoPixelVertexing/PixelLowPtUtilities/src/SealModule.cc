25b12891364613cefb421899adb336d84e28f31c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-18 23:44:24 +0000

    Use new plug-in manager

--- RecoPixelVertexing/PixelLowPtUtilities/src/SealModule.cc
+++ RecoPixelVertexing/PixelLowPtUtilities/src/SealModule.cc
@@ -6,16 +6,16 @@ DEFINE_SEAL_MODULE();
 #include "RecoPixelVertexing/PixelTriplets/interface/HitTripletGeneratorFromPairAndLayers.h"
 #include "RecoPixelVertexing/PixelTriplets/interface/HitTripletGeneratorFromPairAndLayersFactory.h"
 #include "RecoPixelVertexing/PixelLowPtUtilities/interface/PixelTripletLowPtGenerator.h"
-DEFINE_SEAL_PLUGIN(HitTripletGeneratorFromPairAndLayersFactory, PixelTripletLowPtGenerator,"PixelTripletLowPtGenerator");
+DEFINE_EDM_PLUGIN(HitTripletGeneratorFromPairAndLayersFactory, PixelTripletLowPtGenerator,"PixelTripletLowPtGenerator");
 
 // Fitter
 #include "RecoPixelVertexing/PixelTrackFitting/interface/PixelFitter.h"
 #include "RecoPixelVertexing/PixelTrackFitting/interface/PixelFitterFactory.h"
 #include "RecoPixelVertexing/PixelLowPtUtilities/interface/LowPtPixelFitterByHelixProjections.h"
-DEFINE_SEAL_PLUGIN(PixelFitterFactory, LowPtPixelFitterByHelixProjections, "LowPtPixelFitterByHelixProjections");
+DEFINE_EDM_PLUGIN(PixelFitterFactory, LowPtPixelFitterByHelixProjections, "LowPtPixelFitterByHelixProjections");
 
 // Cleaner
 #include "RecoPixelVertexing/PixelTrackFitting/interface/PixelTrackCleaner.h"
 #include "RecoPixelVertexing/PixelTrackFitting/interface/PixelTrackCleanerFactory.h"
 #include "RecoPixelVertexing/PixelLowPtUtilities/interface/LowPtPixelTrackCleanerBySharedHits.h"
-DEFINE_SEAL_PLUGIN(PixelTrackCleanerFactory, LowPtPixelTrackCleanerBySharedHits, "LowPtPixelTrackCleanerBySharedHits");
+DEFINE_EDM_PLUGIN(PixelTrackCleanerFactory, LowPtPixelTrackCleanerBySharedHits, "LowPtPixelTrackCleanerBySharedHits");
