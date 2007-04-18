afe2969881d2f948e28898c64242760236a9069e
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-04-18 15:45:18 +0000

    Small further fix with new plugin manager.

--- Alignment/HIPAlignmentAlgorithm/src/SealModule.cc
+++ Alignment/HIPAlignmentAlgorithm/src/SealModule.cc
@@ -3,8 +3,5 @@
 #include "Alignment/HIPAlignmentAlgorithm/interface/HIPAlignmentAlgorithm.h"
 #include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentAlgorithmPluginFactory.h"
 
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-DEFINE_EDM_PLUGIN( AlignmentAlgorithmPluginFactory,
-		   HIPAlignmentAlgorithm, "HIPAlignmentAlgorithm" );
+DEFINE_EDM_PLUGIN( AlignmentAlgorithmPluginFactory, HIPAlignmentAlgorithm, "HIPAlignmentAlgorithm" );
 
