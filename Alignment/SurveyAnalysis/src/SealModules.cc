3c4ecf6c511e33eb609a2e9d3028c0c83eb5c9ad
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-04-19 22:50:17 +0000

    Make a version for Nhan to test.

---
+++ Alignment/SurveyAnalysis/src/SealModules.cc
@@ -0,0 +1,6 @@
+#include "Alignment/SurveyAnalysis/interface/SurveyDBUploader.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(SurveyDBUploader);
