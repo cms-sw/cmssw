0c5b74950199990b17c9ef110c0d88f75ac2b1ea
wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch>
2007-04-19 15:10:37 +0000

    added HLTFilterObjectWithRefs

--- HLTrigger/btau/src/HLTDisplacedmumuFilter.cc
+++ HLTrigger/btau/src/HLTDisplacedmumuFilter.cc
@@ -17,11 +17,17 @@
 #include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/HLTReco/interface/HLTFilterObject.h"
+
+//#include "DataFormats/Common/interface/AssociationVector.h"
 
 
 using namespace edm;
 using namespace reco;
 using namespace std; 
+
+//typedef edm::AssociationVector<CandidateCollection, VertexCollection > CandidateVertexCollection;
 
 //
 // constructors and destructor
@@ -34,6 +40,8 @@ HLTDisplacedmumuFilter::HLTDisplacedmumuFilter(const edm::ParameterSet& iConfig)
 	minCosinePointingAngle_ = iConfig.getParameter<double>("MinCosinePointingAngle");
 	src_ = iConfig.getParameter<edm::InputTag>("Src");
 	produces<VertexCollection>();
+	produces<HLTFilterObjectWithRefs>();
+// 	produces<CandidateVertexCollection>();
 }
 
 
@@ -64,15 +72,24 @@ bool HLTDisplacedmumuFilter::filter(edm::Event& iEvent, const edm::EventSetup& i
 
 			
 	//get the jpsi collection 
-	
 	Handle<CandidateCollection> candCollection;
 	iEvent.getByLabel(src_,candCollection);
+	
+	// All HLT filters must create and fill an HLT filter object,
+	// recording any reconstructed physics objects satisfying (or not)
+	// this HLT filter, and place it in the Event.
+
+	// The filter object
+	auto_ptr<HLTFilterObjectWithRefs> filterproduct (new HLTFilterObjectWithRefs(path(),module()));
+	
+	// reference to the candidate to be stored in the HLT filter object
+	RefToBase<Candidate> candref;
 	
 	//get the transient track builder:
 	edm::ESHandle<TransientTrackBuilder> theB;
 	iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",theB);
 	std::auto_ptr<VertexCollection> vertexCollection(new VertexCollection());
-	
+// 	std::auto_ptr<CandidateVertexCollection> candidatevertexCollection( new CandidateVertexCollection( CandidateRefProd( candCollection )));
 	if(candCollection->size()>0) {
 		CandidateCollection::const_iterator it=(*candCollection).begin();
 		
@@ -95,8 +112,7 @@ bool HLTDisplacedmumuFilter::filter(edm::Event& iEvent, const edm::EventSetup& i
 				
 				}
 				
-				
-				// Call the KalmanVertexFitter if 2 tracks
+				// Call the KalmanVertexFitter if there are 2 tracks
 				if (t_tks.size()==2){
 				
 					KalmanVertexFitter kvf;
@@ -104,33 +120,42 @@ bool HLTDisplacedmumuFilter::filter(edm::Event& iEvent, const edm::EventSetup& i
 					if (tv.isValid()) {
 						Vertex vertex = tv;
 					
-						
+						// get vertex position and error to calculate the decay length significance
 						GlobalPoint v = tv.position();
 						GlobalError err = tv.positionError();
-						
 						float lxy = v.perp();
 						float lxyerr = err.rerr(v);
 						
+						// get normalizes chi2
 						float normChi2 = tv.normalisedChiSquared();
 					
-						
-					//     float cosAlpha = v.Dot((*it).momentum())/(v.R()*(*it).momentum().R());
-					
+						//calculate the angle between the decay length and the mumu momentum
 						Vertex::Point vperp(v.x(),v.y(),0.);
 						math::XYZVector pperp((*it).momentum().x(),(*it).momentum().y(),0.);
 					
 						float cosAlpha = vperp.Dot(pperp)/(vperp.R()*pperp.R());
+						
 						if( (cosAlpha > minCosinePointingAngle_) && (normChi2 < maxNormalisedChi2_) && ( lxy/lxyerr > minLxySignificance_ ) ){
-	// 						Vertex vertex = tv.vertex();
+							
+							// put vertex in the event
 							vertexCollection->push_back(vertex);
+// 							candidatevertexCollection->push_back(vertex);
 							iEvent.put(vertexCollection);
+// 							iEvent.put(candidatevertexCollection);
+							
+							// put filter object into the Event
+							candref=RefToBase<Candidate>( Ref<CandidateCollection>
+                     		(candCollection,distance(candCollection->begin(), it)));
+							filterproduct->putParticle(candref);
+   							iEvent.put(filterproduct);
+							
 							return true;
 						}
 					}
 				}
 			}	
 		}	
-	}
+	} 
 	return false;
 }
 
