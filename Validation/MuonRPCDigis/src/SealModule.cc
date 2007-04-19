22155a04a6e2164a01cc484e1b42abd501f8c3de
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2007-04-19 13:55:56 +0000

    first release

---
+++ Validation/MuonRPCDigis/src/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "Validation/MuonRPCDigis/src/RPCDigiQuality.h"
+
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(RPCDigiQuality);
