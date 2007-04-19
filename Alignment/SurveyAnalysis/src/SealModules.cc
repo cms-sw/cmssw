a1f15f5436022ca75a06488892fa9e366f0e06a9
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-04-19 23:00:09 +0000

    Return to head (remove file).

--- Alignment/SurveyAnalysis/src/SealModules.cc
+++
@@ -1,6 +0,0 @@
-#include "Alignment/SurveyAnalysis/interface/SurveyDBUploader.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(SurveyDBUploader);
