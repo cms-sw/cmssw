31b2d72b9c5a8456956c398d7b04e7b91ae92854
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2007-04-20 09:56:17 +0000

    *** empty log message ***

--- RecoTracker/TkSeedGenerator/plugins/SealModules.cc
+++ RecoTracker/TkSeedGenerator/plugins/SealModules.cc
@@ -5,26 +5,26 @@
 #include "SeedGeneratorFromRegionHitsEDProducer.h"
 #include "SeedGeneratorFromProtoTracksEDProducer.h"
 
-//#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelSeedGenerator.h"
-//#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelSeedGeneratorWithVertex.h"
-//#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGenerator.h"
-//#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromRectangularTrackingRegion.h"
-//#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelLessSeedGenerator.h"
-//#include "RecoTracker/TkSeedGenerator/interface/GlobalMixedSeedGenerator.h"
-//#include "RecoTracker/TkSeedGenerator/interface/CosmicSeedGenerator.h"
-//#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromTrk.h"
-//#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromCandidate.h"
+#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelSeedGeneratorWithVertex.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromRectangularTrackingRegion.h"
+#include "RecoTracker/TkSeedGenerator/interface/GlobalPixelLessSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/GlobalMixedSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/CosmicSeedGenerator.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromTrk.h"
+#include "RecoTracker/TkSeedGenerator/interface/RegionalPixelSeedGeneratorFromCandidate.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SeedGeneratorFromRegionHitsEDProducer);
 DEFINE_ANOTHER_FWK_MODULE(SeedGeneratorFromProtoTracksEDProducer);
-//DEFINE_ANOTHER_FWK_MODULE(GlobalPixelSeedGenerator);
-//DEFINE_ANOTHER_FWK_MODULE(GlobalPixelSeedGeneratorWithVertex);
-//DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGenerator);
-//DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGeneratorFromCandidate);
-//DEFINE_ANOTHER_FWK_MODULE(GlobalPixelLessSeedGenerator);
-//DEFINE_ANOTHER_FWK_MODULE(GlobalMixedSeedGenerator);
-//DEFINE_ANOTHER_FWK_MODULE(CosmicSeedGenerator);
-//DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGeneratorFromTrk);
+DEFINE_ANOTHER_FWK_MODULE(GlobalPixelSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(GlobalPixelSeedGeneratorWithVertex);
+DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGeneratorFromCandidate);
+DEFINE_ANOTHER_FWK_MODULE(GlobalPixelLessSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(GlobalMixedSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(CosmicSeedGenerator);
+DEFINE_ANOTHER_FWK_MODULE(RegionalPixelSeedGeneratorFromTrk);
 
