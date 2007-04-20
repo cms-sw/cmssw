676d581a6974e5ab79469fd8c025145c6623c481
Wolfgang Waltenberger <sha1-9c4ada266a8352c2428af94857b7ab82e25b3b37@cern.ch>
2007-04-20 13:48:50 +0000

    starting to add combined btagging

---
+++ CondFormats/BTauObjects/interface/CombinedSVCategoryData.h
@@ -0,0 +1,11 @@
+#ifndef CombinedSVCategoryData_H
+#define CombinedSVCategoryData_H
+
+struct CombinedSVCategoryData {
+  float JetEtaMax, JetEtaMin, JetPtMax, JetPtMin;
+  int PartonType;
+  int TaggingVariable;
+  int VertexType;
+};
+
+#endif
