7639508f503149fd52716e2654a809e3477350fc
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-04-21 05:54:28 +0000

    fixed module stub and integration test

--- FWCore/Framework/test/stubs/ToyModules.cc
+++ FWCore/Framework/test/stubs/ToyModules.cc
@@ -251,21 +251,65 @@ namespace edmtest {
     // Put the product into the Event
     e.put(p);
 
-
     // Fill up a collection of SimpleDerived objects
     std::auto_ptr<OVSimpleDerivedProduct> pd(new OVSimpleDerivedProduct());
-
+    
     for (int i = 0; i < size_; ++i)
       {
 	std::auto_ptr<SimpleDerived> simpleDerived(new SimpleDerived()); 
 	simpleDerived->key = size_ - i;
 	simpleDerived->value = 1.5 * i + 100.0;
-        simpleDerived->dummy = 0.0;
-        pd->push_back(simpleDerived);
+	simpleDerived->dummy = 0.0;
+	pd->push_back(simpleDerived);
       }
     
     // Put the product into the Event
     e.put(pd, "derived");
+  }
+    
+  //--------------------------------------------------------------------
+  //
+  // Produces and OVSimpleProduct product instance.
+  //
+  class VSimpleProducer : public edm::EDProducer {
+  public:
+    explicit VSimpleProducer(edm::ParameterSet const& p) : 
+      size_(p.getParameter<int>("size")) 
+    {
+      produces<VSimpleProduct>();
+      assert ( size_ > 1 );
+    }
+
+    explicit VSimpleProducer(int i) : size_(i) 
+    {
+      produces<VSimpleProduct>();
+      assert ( size_ > 1 );
+    }
+
+    virtual ~VSimpleProducer() { }
+    virtual void produce(edm::Event& e, edm::EventSetup const& c);
+    
+  private:
+    int size_;  // number of Simples to put in the collection
+  };
+
+  void
+  VSimpleProducer::produce(edm::Event& e, 
+			   edm::EventSetup const& /* unused */) 
+  {
+    // Fill up a collection
+    std::auto_ptr<VSimpleProduct> p(new VSimpleProduct());
+
+    for (int i = 0; i < size_; ++i)
+      {
+	Simple simple;
+	simple.key = size_ - i;
+	simple.value = 1.5 * i;
+        p->push_back(simple);
+      }
+    
+    // Put the product into the Event
+    e.put(p);
   }
 
   //--------------------------------------------------------------------
@@ -276,25 +320,16 @@ namespace edmtest {
   class AVSimpleProducer : public edm::EDProducer {
   public:
 
-    explicit AVSimpleProducer(edm::ParameterSet const& p) : 
-      size_(p.getParameter<int>("size")) 
-    {
+    explicit AVSimpleProducer(edm::ParameterSet const& p) :
+    src_(p.getParameter<edm::InputTag>("src")) { 
       produces<AVSimpleProduct>();
-      assert ( size_ > 1 );
-    }
-
-    explicit AVSimpleProducer(int i) : size_(i) 
-    {
-      produces<AVSimpleProduct>();
-      assert ( size_ > 1 );
     }
 
     virtual ~AVSimpleProducer() { }
     virtual void produce(edm::Event& e, edm::EventSetup const& c);
     
   private:
-    edm::InputTag src_; // std::vector<edmtet::Simple> to refer to
-    int size_;  // number of Simple objects to put in the collection
+    edm::InputTag src_;
   };
 
   void
@@ -306,7 +341,7 @@ namespace edmtest {
     // Fill up a collection
     std::auto_ptr<AVSimpleProduct> p(new AVSimpleProduct(edm::RefProd<std::vector<edmtest::Simple> >(vs)));
 
-    for (int i = 0; i < size_; ++i)
+    for (unsigned int i = 0; i < vs->size(); ++i)
       {
         edmtest::Simple simple;
         simple.key = 100 + i;  // just some arbitrary number for testing
@@ -691,6 +726,7 @@ using edmtest::IntProducer;
 using edmtest::DoubleProducer;
 using edmtest::SCSimpleProducer;
 using edmtest::OVSimpleProducer;
+using edmtest::VSimpleProducer;
 using edmtest::AVSimpleProducer;
 using edmtest::DSVProducer;
 using edmtest::IntTestAnalyzer;
@@ -707,6 +743,7 @@ DEFINE_FWK_MODULE(IntProducer);
 DEFINE_FWK_MODULE(DoubleProducer);
 DEFINE_FWK_MODULE(SCSimpleProducer);
 DEFINE_FWK_MODULE(OVSimpleProducer);
+DEFINE_FWK_MODULE(VSimpleProducer);
 DEFINE_FWK_MODULE(AVSimpleProducer);
 DEFINE_FWK_MODULE(DSVProducer);
 DEFINE_FWK_MODULE(IntTestAnalyzer);
