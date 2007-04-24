942bad89f32e1a1fa9ed2990dbed2e1305bd447e
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2007-04-24 09:38:15 +0000

    with the new test DTChamberEfficiencyTest

--- DQM/DTMonitorClient/src/SealModule.cc
+++ DQM/DTMonitorClient/src/SealModule.cc
@@ -11,6 +11,9 @@ DEFINE_ANOTHER_FWK_MODULE(DTResolutionTest);
 #include <DQM/DTMonitorClient/src/DTEfficiencyTest.h>
 DEFINE_ANOTHER_FWK_MODULE(DTEfficiencyTest);
 
+#include <DQM/DTMonitorClient/src/DTChamberEfficiencyTest.h>
+DEFINE_ANOTHER_FWK_MODULE(DTChamberEfficiencyTest);
+
 #include <DQM/DTMonitorClient/src/DTDataIntegrityTest.h>
 DEFINE_ANOTHER_FWK_MODULE(DTDataIntegrityTest);
 
