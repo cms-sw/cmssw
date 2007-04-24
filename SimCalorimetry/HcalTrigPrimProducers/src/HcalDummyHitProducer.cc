ceec5097bfe6b8d46f01f0202921a5bf3deb54fa
aurisano <sha1-1c49d782f81e1a091730a40c3f9078e92cab591e@cern.ch>
2007-04-24 12:45:37 +0000

    bugfixes and added hardcoded calibrations to make the energies work out properly

--- SimCalorimetry/HcalTrigPrimProducers/src/HcalDummyHitProducer.cc
+++ SimCalorimetry/HcalTrigPrimProducers/src/HcalDummyHitProducer.cc
@@ -18,17 +18,65 @@ HcalDummyHitProducer::HcalDummyHitProducer(const edm::ParameterSet& ps)
   : theParameterMap(new HcalSimParameterMap())   
 {
   energyEM = 0;
-  energyHad= 0;
   produces<edm::PCaloHitContainer>("HcalHits");
   step_size = ps.getParameter<double>("step_size");
+  energyHad = ps.getParameter<double>("energy_start");
 }
 
 HcalDummyHitProducer::~HcalDummyHitProducer() {}
 
+float sample_factor(HcalDetId id)
+{
+  float output;
+  if (id.subdet() == 1)
+    {
+      if (id.ietaAbs() == 1) {output = 120.49;}
+      else if (id.ietaAbs() == 2) {output = 120.49;}
+      else if (id.ietaAbs() == 3) {output = 120.49;}
+      else if (id.ietaAbs() == 4) {output = 120.58;}
+      else if (id.ietaAbs() == 5) {output = 120.67;}
+      else if (id.ietaAbs() == 6) {output = 120.76;}
+      else if (id.ietaAbs() == 7) {output = 120.87;}
+      else if (id.ietaAbs() == 8) {output = 120.98;}
+      else if (id.ietaAbs() == 9) {output = 121.08;}
+      else if (id.ietaAbs() == 10) {output = 121.44;}
+      else if (id.ietaAbs() == 11) {output = 121.81;}
+      else if (id.ietaAbs() == 12) {output = 122.17;}
+      else if (id.ietaAbs() == 13) {output = 123.37;}
+      else if (id.ietaAbs() == 14) {output = 124.57;}
+      else if (id.ietaAbs() == 15) {output = 125.76;}
+      else {output = 130.0;}
+    }
+  else if (id.subdet() == 2)
+    {
+      if (id.ietaAbs() == 15) {output = 151.05;}
+      else if (id.ietaAbs() == 16) {output = 189.25;}
+      else if (id.ietaAbs() == 17) {output = 174.51;}
+      else if (id.ietaAbs() == 18) {output = 174.51;}
+      else if (id.ietaAbs() == 19) {output = 174.51;}
+      else if (id.ietaAbs() == 20) {output = 174.51;}
+      else if (id.ietaAbs() == 21) {output = 174.51;}
+      else if (id.ietaAbs() == 22) {output = 174.51;}
+      else if (id.ietaAbs() == 23) {output = 174.51;}
+      else if (id.ietaAbs() == 24) {output = 174.51;}
+      else if (id.ietaAbs() == 25) {output = 174.51;}
+      else if (id.ietaAbs() == 26) {output = 174.51;}
+      else if (id.ietaAbs() == 27) {output = 174.51;}
+      else {output = 174.51;}
+    }
+  else if (id.subdet() == 4)
+    {
+      if(id.depth() == 1) {output = 2.79;}
+      else {output =  1.842;}
+    }
+  else
+    {
+      output =  0.0;
+    }
+  return output;
+}
 
 void HcalDummyHitProducer::produce(edm::Event& e, const edm::EventSetup& eventSetup) {
-  //edm::Handle<edm::PCaloHitContainer> pcalo;
-  //e.getByLabel("g4SimHits","HcalHits",pcalo);
   // get the appropriate gains, noises, & widths for this event
   edm::ESHandle<HcalDbService> conditions;
   eventSetup.get<HcalDbRecord>().get(conditions);
@@ -74,50 +122,48 @@ void HcalDummyHitProducer::produce(edm::Event& e, const edm::EventSetup& eventSe
   double calib;
   int ncells;
   int ntowers;
-  //  vector<HcalTrigTowerDetId> towerids;  
   double cell_energyEM, cell_energyHad;
 
   for(vector<DetId>::const_iterator itr = hbCells.begin(); itr != hbCells.end(); ++itr)
     {
-      calib = 117;
       time = 8.4;
       HcalDetId barrelId(*itr);
+      calib = sample_factor(barrelId);
       towerids = theTrigTowerGeometry.towerIds(barrelId);
       ncells = Tower_map.count(towerids[0]);
       ntowers = towerids.size();
-      cell_energyEM = (energyEM*ntowers)/(ncells*calib);
+      cell_energyEM = 0;
       cell_energyHad= (energyHad*ntowers)/(ncells*calib);
       PCaloHit barrelHit(barrelId.rawId(), cell_energyEM, cell_energyHad, time, 0);
       result->push_back(barrelHit);
     }
   for(vector<DetId>::const_iterator itr = heCells.begin(); itr != heCells.end(); ++itr)
     {
-      calib = 178;
       time = 13.0;
       HcalDetId endcapId(*itr);
+      calib = sample_factor(endcapId);
       towerids = theTrigTowerGeometry.towerIds(endcapId);
       ncells = Tower_map.count(towerids[0]);
       ntowers = towerids.size();
-      cell_energyEM = (energyEM*ntowers)/(ncells*calib);
+      cell_energyEM =  0;
       cell_energyHad= (energyHad*ntowers)/(ncells*calib);
       PCaloHit endcapHit(endcapId.rawId(), cell_energyEM, cell_energyHad, time, 0);
       result->push_back(endcapHit);
     }
   for(vector<DetId>::const_iterator itr = hfCells.begin(); itr != hfCells.end(); ++itr)
     {
-      calib = 2.84;
       time = 37.0;
       HcalDetId forwardId(*itr);
+      calib = sample_factor(forwardId);
       towerids = theTrigTowerGeometry.towerIds(forwardId);
       ncells = Tower_map.count(towerids[0]);
       ntowers = towerids.size();
-      cell_energyEM = (energyEM*ntowers)/(ncells*calib);
+      cell_energyEM = 0;
       cell_energyHad= (energyHad*ntowers)/(ncells*calib);
       PCaloHit forwardHit(forwardId.rawId(), cell_energyEM, cell_energyHad, time, 0);
       result->push_back(forwardHit);
     }
   e.put(result,"HcalHits");
-  energyEM  += step_size;
   energyHad += step_size;
 }
 
