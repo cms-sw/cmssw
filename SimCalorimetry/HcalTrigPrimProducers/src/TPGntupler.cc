b8ed9c4e7bc00a8be76c99a71adce2ca6df1113e
aurisano <sha1-1c49d782f81e1a091730a40c3f9078e92cab591e@cern.ch>
2007-04-24 11:57:35 +0000

    removed hit energy from ntuple

--- SimCalorimetry/HcalTrigPrimProducers/src/TPGntupler.cc
+++ SimCalorimetry/HcalTrigPrimProducers/src/TPGntupler.cc
@@ -13,47 +13,39 @@
 //
 // Original Author:  Adam Aurisano
 //         Created:  Thur Jan 18 2007
-// $Id$
+// $Id: TPGntupler.cc,v 1.3 2007/03/26 09:25:29 aurisano Exp $
 //
 //
 
 #include "TPGntupler.h"
 #include "CalibCalorimetry/CaloTPG/src/CaloTPGTranscoderULUT.h"
 #include "TMath.h"
+#include <fstream>
 
 using namespace std;
 using namespace edm;
-
 
 TPGntupler::TPGntupler(const edm::ParameterSet& iConfig) : 
   //now do what ever initialization is needed
   file("tpg_ntuple.root", "RECREATE" ),
   tree("TPGntuple","Trigger Primitive Ntuple")
 {
-  //  gROOT->ProcessLine(".L TPinfo.cc+");
-  //infoarray = new TClonesArray("TPinfo",4176);
-  //tree.Branch("tpinfoarray","TClonesArray",&infoarray,32000,99);
   tree.Branch("run",&run_num,"run/I");
   tree.Branch("event",&event_num,"event/I");
   tree.Branch("ieta",ieta,"ieta[4176]/I");
   tree.Branch("iphi",iphi,"iphi[4176]/I");
   tree.Branch("tpg_energy",tpg_energy,"tpg_energy[4176]/F");
-  tree.Branch("hit_energy",hit_energy,"hit_energy[4176]/F");
+  //tree.Branch("hit_energy",hit_energy,"hit_energy[4176]/F");
   tree.Branch("tpg_uncompressed",tpg_uncompressed,"tpg_uncompressed[4176]/F");
   tree.Branch("tpg_index",index,"index[4176]/I");
-  //transcoder_ = transcoder;
+  tree.Branch("rec_energy",rec_energy,"rec_energy[4176]/F");
 }
-
-
 
 TPGntupler::~TPGntupler()
 {
   file.cd();
   tree.Write();
-  // do anything here that needs to be done at desctruction time
-  // (e.g. close files, deallocate resources etc.)
 }
-
 
 //
 // member functions
@@ -66,114 +58,127 @@ TPGntupler::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   using namespace edm;
   using namespace std;
 
+  ofstream out;
+  out.open("outfile.txt");
+  char semicolon = 59;
+
   edm::ESHandle<CaloTPGTranscoder> _transcoder;
   iSetup.get<CaloTPGRecord>().get(_transcoder);
   // get the appropriate gains, noises, & widths for this event
-  edm::ESHandle<HcalDbService> conditions;
-  iSetup.get<HcalDbRecord>().get(conditions);
+  //edm::ESHandle<HcalDbService> conditions;
+  //iSetup.get<HcalDbRecord>().get(conditions);
   // get the correct geometry
-  edm::ESHandle<CaloGeometry> geometry;
-  iSetup.get<IdealGeometryRecord>().get(geometry);
-  vector<DetId> hbCells =  geometry->getValidDetIds(DetId::Hcal, HcalBarrel);
-  vector<DetId> heCells =  geometry->getValidDetIds(DetId::Hcal, HcalEndcap);
-  vector<DetId> hfCells =  geometry->getValidDetIds(DetId::Hcal, HcalForward);
-
+  //edm::ESHandle<CaloGeometry> geometry;
+  //iSetup.get<IdealGeometryRecord>().get(geometry);
+  //vector<DetId> hbCells =  geometry->getValidDetIds(DetId::Hcal, HcalBarrel);
+  //vector<DetId> heCells =  geometry->getValidDetIds(DetId::Hcal, HcalEndcap);
+  //vector<DetId> hfCells =  geometry->getValidDetIds(DetId::Hcal, HcalForward);
 
   Handle<HcalTrigPrimDigiCollection> tpg;
-  iEvent.getByLabel("hcalTriggerPrimitiveDigis", tpg);  
+  iEvent.getByType(tpg);  
 
-  Handle<CrossingFrame> cf;
-  iEvent.getByType(cf);
+  //  Handle<CrossingFrame> cf;
+  //iEvent.getByType(cf);
+
+  Handle<HBHERecHitCollection> hbhe_rec;
+  iEvent.getByType(hbhe_rec);
+  
+  Handle<HFRecHitCollection> hf_rec;
+  iEvent.getByType(hf_rec);
 
   run_num = iEvent.id().run();
   event_num = iEvent.id().event();
 
+  //auto_ptr<MixCollection<PCaloHit> > pcalo(new MixCollection<PCaloHit>(cf.product(),"HcalHits"));
 
-  auto_ptr<MixCollection<PCaloHit> > pcalo(new MixCollection<PCaloHit>(cf.product(),"HcalHits"));
-
-
-  double hit_e;
-  double tpg_e;
-  double temp_e;
+  //float hit_e;
+  float tpg_e;
+  float rec_e;
   double eta1, eta2;
-  //double hit_energy, tpg_energy;
+  float calib;
   std::vector<HcalTrigTowerDetId> towerids;
-  std::vector<HcalDetId> cellids;
-  hit_map.clear();
-  Hit_cells.clear();
-  Hit_towers.clear();
-  TP_towers.clear();
+  double eta;
+  //  Hit_towers.clear();
+  Rec_towers.clear();
 
-  assert(hit_map.size() == 0);
   //Construct map of hit cells vs. trigger tower id 
-  for(MixCollection<PCaloHit>::MixItr j = pcalo->begin(); j != pcalo->end(); ++j)
+  //for(MixCollection<PCaloHit>::MixItr sim_iter = pcalo->begin(); sim_iter != pcalo->end(); ++sim_iter)
+  //{
+  //HcalDetId hcalid = HcalDetId(sim_iter->id());
+  //if (hcalid.subdet() == 1) 
+  //{
+  //calib = 117.0;
+  //}
+  //else if (hcalid.subdet() == 2) 
+  //{
+  //calib = 178.0;
+  //}
+  //else if (hcalid.subdet() == 3) 
+  //{
+  // calib = 2.84;
+  //}
+  //else
+  //{
+  //calib = 0.0;
+  //}
+  //towerids = theTrigTowerGeometry.towerIds(hcalid);
+  //assert(towerids.size() == 2 || towerids.size() == 1);
+  //for(unsigned int n = 0; n < towerids.size(); n++)
+  //{
+  //Hit_towers.insert(IdtoEnergy::value_type(towerids[n],(calib*sim_iter->energy())/towerids.size()));
+  //}
+  //}
+
+  for(HBHERecHitCollection::const_iterator hbhe_iter = hbhe_rec->begin(); hbhe_iter != hbhe_rec->end(); ++hbhe_iter)
     {
-      PCaloHit cell(j->id(),j->energyEM(),j->energyHad(),j->time(),j->geantTrackId());
-      HcalDetId hcalid = HcalDetId(j->id());
-      //fill energies vs. hcal ids
-      Hit_cells.insert(Cell_Map::value_type(hcalid,j->energy()));
-      towerids = theTrigTowerGeometry.towerIds(hcalid);
+      towerids = theTrigTowerGeometry.towerIds(hbhe_iter->id());
       assert(towerids.size() == 2 || towerids.size() == 1);
-      //fill map with cells vs tower ids
       for(unsigned int n = 0; n < towerids.size(); n++)
 	{
-	  hit_map.insert(IdtoHit::value_type(towerids[n],cell));
+	  Rec_towers.insert(IdtoEnergy::value_type(towerids[n],hbhe_iter->energy()/towerids.size()));
 	}
     }
 
-  for(HcalTrigPrimDigiCollection::const_iterator j = tpg->begin(); j != tpg->end(); ++j )
+  for(HFRecHitCollection::const_iterator hf_iter = hf_rec->begin(); hf_iter != hf_rec->end(); ++hf_iter)
     {
-      temp_e = 0.0;
-      hit_e = 0.0;
-      tpg_e = 0.0;
-      
-      for(IdtoHit::iterator i = hit_map.lower_bound(j->id()); i != hit_map.upper_bound(j->id()); ++i)
-	{
-	  towerids = theTrigTowerGeometry.towerIds((i->second).id());
-	  assert(towerids.size() == 2 || towerids.size() == 1);
-	  HcalDetId id = HcalDetId((i->second).id());
-	  if (id.subdet() == 1)
-	    {
-	      temp_e += 117*((i->second).energy()/double(towerids.size()));
-	    }
-	  else if (id.subdet() == 2)
-	    {
-	      temp_e += 178*((i->second).energy()/double(towerids.size()));
-	    }
-	  else 
-	    {
-	      temp_e += 2.84*((i->second).energy()/double(towerids.size()));
-	    }
-	}
-      hit_e = temp_e;  //hit_e is sum of all hit energy for cells in tower
-      //Fill Hit_towers
-      Hit_towers.insert(IdtoEnergy::value_type(j->id(),hit_e));
-
-      tpg_e = double(j->SOI_compressedEt());
-      TP_towers.insert(IdtoEnergy::value_type(j->id(),tpg_e));
+      towerids = theTrigTowerGeometry.towerIds(hf_iter->id());
+      assert(towerids.size() == 2 || towerids.size() == 1);
+      for(unsigned int n = 0; n < towerids.size(); n++)
+        {
+          Rec_towers.insert(IdtoEnergy::value_type(towerids[n],hf_iter->energy()/towerids.size()));
+        }
     }
 
   int ntower = 0;
-  double eta;
-  for(IdtoEnergy::const_iterator j = TP_towers.begin(); j != TP_towers.end(); ++j)
+  for(HcalTrigPrimDigiCollection::const_iterator tpg_iter = tpg->begin(); tpg_iter != tpg->end(); ++tpg_iter )
     {
-      ieta[ntower] = j->first.ieta();
-      iphi[ntower] = j->first.iphi();
-      tpg_energy[ntower] = j->second;
-      hit_energy[ntower] = (Hit_towers.find(j->first))->second;
+      //hit_e = 0.0;
+      tpg_e = 0.0;
+      rec_e = 0.0;
+      //for(IdtoEnergy::iterator i = Hit_towers.lower_bound(tpg_iter->id()); i != Hit_towers.upper_bound(tpg_iter->id()); ++i)
+      //{
+      //hit_e += i->second;
+      ///}
+      for(IdtoEnergy::iterator i = Rec_towers.lower_bound(tpg_iter->id()); i != Rec_towers.upper_bound(tpg_iter->id()); ++i)
+	{
+	  rec_e += i->second;
+	}
+      tpg_e = double(tpg_iter->SOI_compressedEt());
+      ieta[ntower] = tpg_iter->id().ieta();
+      iphi[ntower] = tpg_iter->id().iphi();
+      tpg_energy[ntower] = tpg_e;
+      //hit_energy[ntower] = hit_e;
+      rec_energy[ntower] = rec_e;
       theTrigTowerGeometry.towerEtaBounds(ieta[ntower],eta1,eta2);
       eta = (eta1+eta2)/2;
-      //      cout << "Eta value " << eta << " ieta value " << ieta[ntower] << "\n";
-      tpg_uncompressed[ntower] = float(_transcoder->hcaletValue(j->first.ietaAbs(),int(j->second)));
-      index[ntower] = 100*ieta[ntower]+iphi[ntower];
-      //cout << "Hit energy = " << hit_energy[ntower] << " Uncommpressed = " << tpg_uncompressed[ntower] << "\n";
-      ++ntower;
+      tpg_uncompressed[ntower] = float(_transcoder->hcaletValue(tpg_iter->id().ietaAbs(),int(tpg_e)));
+      index[ntower] = tpg_iter->id().zside()*(100*tpg_iter->id().ietaAbs()+tpg_iter->id().iphi());
+      out << "output[" <<index[ntower] << "] = " << ntower << semicolon <<"\n";
+      ntower++;
     }
   tree.Fill();
+  out.close();
 }
-
-
-
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
