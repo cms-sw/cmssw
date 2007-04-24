3456cd9e331102f8bc72d2fc62075d452f5f44ad
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-04-24 07:07:14 +0000

    added new utility

---
+++ PhysicsTools/Utilities/interface/DeltaPhi.h
@@ -0,0 +1,19 @@
+#ifndef Utilities_DeltaPhi_h
+#define Utilities_DeltaPhi_h
+/* \class DeltaPhi
+ *
+ * returns DeltaPhi between two objects
+ *
+ * \author Luca Lista, INFN
+ */
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "PhysicsTools/Utilities/interface/Math.h"
+
+template<typename T1, typename T2 = T1>
+struct DeltaPhi {
+  double operator()( const T1 & t1, const T2 & t2 ) const {
+    return deltaPhi( t1, t2 );
+  }
+};
+
+#endif
