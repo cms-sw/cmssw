d8311b0117854965aa1eed98648c7a8562ac7231
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2007-04-24 22:32:57 +0000

    - removed TAUOLA test program

--- GeneratorInterface/Pythia6Interface/src/SealModule.cc
+++ GeneratorInterface/Pythia6Interface/src/SealModule.cc
@@ -3,11 +3,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "GeneratorInterface/Pythia6Interface/interface/PythiaSource.h"
 #include "GeneratorInterface/Pythia6Interface/interface/HepMCProductAnalyzer.h"
-#include "GeneratorInterface/Pythia6Interface/interface/TauDecayAnalyzer.h"
 
   using edm::PythiaSource;
 
   DEFINE_SEAL_MODULE();
   DEFINE_ANOTHER_FWK_INPUT_SOURCE(PythiaSource);
   DEFINE_ANOTHER_FWK_MODULE(HepMCProductAnalyzer);
-  DEFINE_ANOTHER_FWK_MODULE(TauDecayAnalyzer);
