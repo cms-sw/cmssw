aac990b72f1d4f3405dc5b2f8dd3b4737b4a3597
afaq <sha1-85f47aa78e37d28203e5c3f51090413d817de089@cern.ch>
2007-04-25 21:37:43 +0000

    Class to read DQM Event files

---
+++ IOPool/Streamer/interface/StreamDQMInputFile.h
@@ -0,0 +1,34 @@
+#ifndef _StreamDQMInputFile_h
+#define _StreamDQMInputFile_h
+
+#include "IOPool/Streamer/interface/MsgTools.h"
+#include "IOPool/Streamer/interface/DQMEventMessage.h"
+
+#include<string>
+#include<vector>
+#include<fstream>
+
+  class StreamDQMInputFile
+  {
+  public:
+
+    /**Reads a Streamer file */
+    explicit StreamDQMInputFile(const std::string& name);
+
+    ~StreamDQMInputFile();
+
+    bool next() ; /** Moves the handler to next Event Record */
+
+    const DQMEventMsgView*  currentRecord() const { return currentEvMsg_; }
+    /** Points to current Record */
+
+  private:
+
+    int readDQMEventMessage();
+
+    DQMEventMsgView* currentEvMsg_;
+    std::auto_ptr<std::ifstream> ist_;
+    std::vector<char> eventBuf_;  /** Buffer to store Event Data */
+  };
+
+#endif
