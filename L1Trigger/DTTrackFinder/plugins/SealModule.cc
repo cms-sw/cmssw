3fd722504967e085aa12fa6afd7a746864946b2c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-25 23:16:18 +0000

    Use quotes, not angle brackets, for local includes

--- L1Trigger/DTTrackFinder/plugins/SealModule.cc
+++ L1Trigger/DTTrackFinder/plugins/SealModule.cc
@@ -1,5 +1,5 @@
+#include "DTTrackFinder.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include <DTTrackFinder.h>
 
 DEFINE_FWK_MODULE(DTTrackFinder);
