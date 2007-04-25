ac1b9abef209e578fc987f9f6f231bb8a90f170a
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-04-25 15:49:59 +0000

    plugin migration

--- DQM/SiStripCommissioningDbClients/plugins/module.cc
+++ DQM/SiStripCommissioningDbClients/plugins/module.cc
@@ -1,6 +1,4 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 DEFINE_SEAL_MODULE();
 
 #include "DQM/SiStripCommissioningDbClients/interface/SiStripCommissioningOfflineDbClient.h"
