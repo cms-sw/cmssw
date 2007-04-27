bb7132bae6024ea315e5a62d46de4f688239057c
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2007-04-27 09:16:55 +0000

    allowed use of PFCandidateRef, PFCandidateRefProd, PFCandidateRefVector.

--- DataFormats/ParticleFlowCandidate/src/classes.h
+++ DataFormats/ParticleFlowCandidate/src/classes.h
@@ -1,8 +1,12 @@
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   namespace {
+    reco::PFCandidateRef c_r;
+    reco::PFCandidateRefProd c_rp;
+    reco::PFCandidateRefVector c_rv;
     edm::Wrapper<std::vector<reco::PFCandidate> > w1;
   }
 }
