035920170ada9427323134b7f58f959e6faec800
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-04-27 14:10:39 +0000

    removed 'cms.' from .py files

--- PhysicsTools/HepMCCandAlgos/test/mccandidates.py
+++ PhysicsTools/HepMCCandAlgos/test/mccandidates.py
@@ -1,6 +1,6 @@
-import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.Config import *
 
-process = cms.Process("MCCand")
+process = Process("MCCand")
 
 process.include( "FWCore/MessageLogger/data/MessageLogger.cfi" )
 process.include( "SimGeneral/HepPDTESSource/data/pythiapdt.cfi")
@@ -12,30 +12,30 @@ process.include( "PhysicsTools/HepMCCandAlgos/data/genParticleCandidatesFast.cfi
 # include "PhysicsTools/HepMCCandAlgos/test/h4l.cff"
 # include "PhysicsTools/HepMCCandAlgos/test/herwig.cff"
 
-process.add_(cms.Service("RandomNumberGeneratorService",
-             sourceSeed= cms.untracked.uint32( 123456789 )))
+process.add_(Service("RandomNumberGeneratorService",
+             sourceSeed= untracked.uint32( 123456789 )))
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50) )
+process.maxEvents = untracked.PSet( input = untracked.int32(50) )
 
 from h4l import pythiaSource
 
 process.source = pythiaSource
 
-process.out = cms.OutputModule("PoolOutputModule",
-  fileName = cms.untracked.string("genevents.root"),
-  outputCommands= cms.untracked.vstring(
+process.out = OutputModule("PoolOutputModule",
+  fileName = untracked.string("genevents.root"),
+  outputCommands= untracked.vstring(
     "drop *",
     "keep *_genParticleCandidates_*_*"
  )
 )
   
-process.printEventNumber = cms.OutputModule( "AsciiOutputModule" )
+process.printEventNumber = OutputModule( "AsciiOutputModule" )
   
-process.p = cms.Path( 
+process.p = Path( 
   process.genParticleCandidates 
 )
 
-process.o = cms.EndPath( 
+process.o = EndPath( 
   process.out * 
   process.printEventNumber 
 )
