ec8be5febf89f0fc35c32e879cdf3206412e1e84
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2007-04-29 22:37:29 +0000

    Adding typdefs, conversion macros and includes for tracking SMatrix migration

--- DataFormats/CLHEP/interface/AlgebraicObjects.h
+++ DataFormats/CLHEP/interface/AlgebraicObjects.h
@@ -13,4 +13,6 @@ typedef HepVector      AlgebraicVector;
 typedef HepMatrix      AlgebraicMatrix;
 typedef HepSymMatrix   AlgebraicSymMatrix;
 
+#include "DataFormats/CLHEP/interface/Migration.h"
+
 #endif
