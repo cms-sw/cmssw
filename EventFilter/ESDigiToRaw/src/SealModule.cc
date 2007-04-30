f7b724dcd93577fa77093907c0858c9de8292aef
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2007-04-30 19:24:21 +0000

    first commit

---
+++ EventFilter/ESDigiToRaw/src/SealModule.cc
@@ -0,0 +1,10 @@
+//#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "EventFilter/ESDigiToRaw/interface/ESDigiToRaw.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(ESDigiToRaw);
+
+
