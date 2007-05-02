c6820a4f53abb053565a5426faf75b60e4821b49
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-05-02 15:11:28 +0000

    Revised constructor. Now reference to SC and to tracks are given in the constructor. This was done to allow to have the references available at object cosntruction time and related quanties can be calculated and made available. A series of method give access to pair invariant mass, pair mommentum, ration between SC energy and track pair momentum etc. etc.

--- DataFormats/EgammaCandidates/interface/ConvertedPhoton.h
+++ DataFormats/EgammaCandidates/interface/ConvertedPhoton.h
@@ -6,13 +6,15 @@
  *
  * \author N.Marinelli  University of Notre Dame, US
  *
- * \version $Id: ConvertedPhoton.h,v 1.5 2007/03/16 13:59:37 llista Exp $
+ * \version $Id: ConvertedPhoton.h,v 1.4 2007/01/26 16:24:39 nancy Exp $
  *
  */
 #include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
 #include "DataFormats/EgammaCandidates/interface/ConvertedPhotonFwd.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/GeometryVector/interface/GlobalVector.h"
+
 
 namespace reco {
     class ConvertedPhoton : public RecoCandidate {
@@ -20,26 +22,40 @@ namespace reco {
     /// default constructor
     ConvertedPhoton() : RecoCandidate() { }
 
-   ConvertedPhoton( Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ), const Point & convVtx = Point( 0, 0, 0 ) ):  RecoCandidate( q, p4, vtx, 22 * q ), theConversionVertex_(convVtx) { }
-
+  
+    ConvertedPhoton( const reco::SuperClusterRef sc, const std::vector<reco::TrackRef> tr, Charge q, const LorentzVector & p4, const Point & vtx, const Point & convVtx  );
     /// destructor
     virtual ~ConvertedPhoton();
     /// returns a clone of the candidate
     ConvertedPhoton * clone() const;
     /// reference to a SuperCluster
     reco::SuperClusterRef superCluster() const ;
-    /// set refrence to ConvertedPhoton component
-    void setSuperCluster( const reco::SuperClusterRef & r ) { superCluster_ = r; }
 
-
-    // set reference to a pair of Tracks
-     void setTrackPairRef( const  std::vector<reco::TrackRef>  & pair ) { tracks_ = pair; }
-    // reference to a vector of tracks
-    const  std::vector<reco::TrackRef>& tracks() const ; 
+    // vector of references to  tracks
+    std::vector<reco::TrackRef> tracks() const ; 
+    // Bool flagging objects having track size >0
+    bool isConverted() const;
+    // Number of tracks= 0,1,2
+    unsigned int nTracks() const {return  tracks().size(); }
+    // if nTracks=2 
+    double pairInvariantMass() const {return invMass_;}
+    // Delta cotg(Theta) where Theta is the angle in the (y,z) plane between the two tracks 
+    double pairCotThetaSeparation() const {return dCotTheta_;}
+    // Conversion tracks momentum 
+    GlobalVector  pairMomentum() const {return momTracks_;}
+    // Phi  
+    double pairMomentumPhi() const {return  phiTracks_;}
+    // Eta 
+    double pairMomentumEta() const {return etaTracks_;}
+    // Pt from tracks divided by the super cluster transverse energy
+    double pairPtOverEtSC() const {return ptOverEtSC_;}
+    // Super Cluster energy divided by tracks momentum
+    double EoverP() const {return ep_;}
+    // returns the position of the conversion vertex
+    const Point & convVertexPosition() const { return theConversionVertex_ ; }
    /// reference to one of multiple Tracks: implements the method inherited from RecoCandidate
      reco::TrackRef track( size_t ) const;
-    // returns the position of the conversion vertex
-     const Point & convVertexPosition() const { return theConversionVertex_ ; }
+
 
   private:
     /// check overlap with another candidate
@@ -48,9 +64,25 @@ namespace reco {
 
     /// reference to a SuperCluster
     reco::SuperClusterRef superCluster_;
-    //    reco::TrackRefVector  tracks_;
     std::vector<reco::TrackRef>  tracks_;
     reco::Particle::Point theConversionVertex_;
+
+    void makePairInvariantMass() ;
+    void makePairCotThetaSeparation();
+    void makePairMomentum() ;
+    void makePairMomentumEta() ;
+    void makePairMomentumPhi() ;
+    void makePairPtOverEtSC() ;
+    void makeEoverP() ;
+
+    double invMass_;
+    double dCotTheta_;
+    double etaTracks_;
+    double phiTracks_;
+    GlobalVector  momTracks_;
+    double ptOverEtSC_;
+    double ep_;
+
 
   };
   
--- DataFormats/EgammaCandidates/src/ConvertedPhoton.cc
+++ DataFormats/EgammaCandidates/src/ConvertedPhoton.cc
@@ -3,6 +3,23 @@
 
 using namespace reco;
 
+
+
+
+ConvertedPhoton::ConvertedPhoton( const reco::SuperClusterRef sc, const std::vector<reco::TrackRef> tr, Charge q, const LorentzVector & p4, const Point & vtx = Point( 0, 0, 0 ), const Point & convVtx = Point( 0, 0, 0 ) ):  
+    RecoCandidate( q, p4, vtx, 22 * q ), superCluster_(sc), tracks_(tr), theConversionVertex_(convVtx) {
+
+  makePairInvariantMass();
+  makePairCotThetaSeparation();
+  makePairMomentum();
+  makePairMomentumEta();
+  makePairMomentumPhi();
+  makePairPtOverEtSC();
+  makeEoverP() ;
+
+ }
+
+
 ConvertedPhoton::~ConvertedPhoton() { }
 
 ConvertedPhoton * ConvertedPhoton::clone() const { 
@@ -16,7 +33,7 @@ reco::SuperClusterRef ConvertedPhoton::superCluster() const {
 
 
 
-const std::vector<reco::TrackRef>&  ConvertedPhoton::tracks() const { 
+std::vector<reco::TrackRef>  ConvertedPhoton::tracks() const { 
    return tracks_;
 }
 
@@ -25,6 +42,114 @@ TrackRef ConvertedPhoton::track( size_t ind ) const {
   return tracks_[ind];
 }
 
+
+
+bool ConvertedPhoton::isConverted() const {
+  
+  if ( this->nTracks() > 0) 
+    return true;
+  else
+    return false;
+}
+
+
+void  ConvertedPhoton::makePairInvariantMass() {
+  invMass_=-99.;
+  const float mElec= 0.000511;
+  if ( nTracks()==2 ) {
+    double px= tracks()[0]->innerMomentum().x() + tracks()[1]->innerMomentum().x();
+    double py= tracks()[0]->innerMomentum().y() + tracks()[1]->innerMomentum().y();
+    double pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();  
+    double mom1=tracks()[0]->innerMomentum().Mag2() ;
+    double mom2=tracks()[1]->innerMomentum().Mag2() ;
+    double e = sqrt( mom1+ mElec*mElec ) + sqrt( mom2 + mElec*mElec );
+    invMass_= ( e*e - px*px -py*py - pz*pz);
+  }
+  
+
+ 
+
+}
+
+void  ConvertedPhoton::makePairCotThetaSeparation()  {
+  dCotTheta_=-99.;
+  
+  if ( nTracks()==2 ) {
+    double theta1=tracks()[0]->innerMomentum().Theta();
+    double theta2=tracks()[1]->innerMomentum().Theta();
+    dCotTheta_ =  1./tan(theta1) - 1./tan(theta2) ;
+  }
+}
+
+
+void  ConvertedPhoton::makePairMomentum()  {
+
+  double px=0.;
+  double py=0.;
+  double pz=0.;
+  
+  if ( nTracks()==2 ) {
+    px= tracks()[0]->innerMomentum().x() + tracks()[1]->innerMomentum().x();
+    py= tracks()[0]->innerMomentum().y() + tracks()[1]->innerMomentum().y();
+    pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();  
+
+  } else if (  nTracks()==1 ) {
+    px= tracks()[0]->innerMomentum().x() ;
+    py= tracks()[0]->innerMomentum().y() ;
+    pz= tracks()[0]->innerMomentum().z() ;
+  }
+
+
+  momTracks_= GlobalVector(px,py,pz);
+
+}
+
+
+void  ConvertedPhoton::makePairMomentumEta()  {
+
+  etaTracks_=-99.;
+  
+  if ( nTracks() > 0 ) {
+    etaTracks_= pairMomentum().eta();
+  } 
+
+}
+
+void  ConvertedPhoton::makePairMomentumPhi()  {
+
+  phiTracks_=-99.;
+  
+  if ( nTracks() > 0 ) {
+    phiTracks_= pairMomentum().phi();
+  } 
+
+}
+
+
+
+
+void  ConvertedPhoton::makePairPtOverEtSC()  {
+
+  ptOverEtSC_=-99.;
+  double etaSC=superCluster()->eta();
+  double EtSC= superCluster()->energy()/cosh(etaSC);
+  
+   if ( nTracks() ==2 ) {
+    ptOverEtSC_= (pairMomentum().perp()) /EtSC; 
+   }
+
+}
+
+
+
+
+void  ConvertedPhoton::makeEoverP()  {
+
+  ep_=-99.;
+  if ( nTracks() > 0  ) 
+    ep_=  this->superCluster()->energy()/this->pairMomentum().mag();
+  
+}
 
 
 bool ConvertedPhoton::overlap( const Candidate & c ) const {
