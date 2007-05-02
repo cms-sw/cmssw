09a29256a1175942b85af022834fb14d3ecdea44
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2007-05-02 16:04:06 +0000

    fixing bug failing with new plugin manager, slipped in from backports to 14X

--- GeneratorInterface/GenFilters/src/SealModule.cc
+++ GeneratorInterface/GenFilters/src/SealModule.cc
@@ -1,4 +1,3 @@
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "GeneratorInterface/GenFilters/interface/PythiaFilter.h"
