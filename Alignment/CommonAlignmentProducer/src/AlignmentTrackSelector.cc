528fb8dc9551d49d9f637a3a8d944d6c00ab1fa4
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2007-05-03 16:23:21 +0000

    Fix for AlignmentTrackSelectorModule

--- Alignment/CommonAlignmentProducer/src/AlignmentTrackSelector.cc
+++ Alignment/CommonAlignmentProducer/src/AlignmentTrackSelector.cc
@@ -38,13 +38,16 @@ AlignmentTrackSelector::AlignmentTrackSelector(const edm::ParameterSet & cfg) :
 
   if (applyMultiplicityFilter)
 	edm::LogInfo("AlignmentTrackSelector") 
-	  << "apply multiplicity filter N>=" << minMultiplicity;
+	  << "apply multiplicity filter N>= " << minMultiplicity << "and N<= " << maxMultiplicity;
 
   edm::ParameterSet minHitsPerSubdet = conf_.getParameter<edm::ParameterSet>( "minHitsPerSubDet" );
   minHitsinTIB = minHitsPerSubdet.getUntrackedParameter<int>( "inTIB" , 0 );
   minHitsinTOB = minHitsPerSubdet.getUntrackedParameter<int>( "inTOB" , 0 );
   minHitsinTID = minHitsPerSubdet.getUntrackedParameter<int>( "inTID" , 0 );
   minHitsinTEC = minHitsPerSubdet.getUntrackedParameter<int>( "inTEC" , 0 );
+  
+  edm::LogInfo("AlignmentTrackSelector") 
+    << "Minimum number of hits in TIB/TID/TOB/TEC = " << minHitsinTIB << "/" << minHitsinTID << "/" << minHitsinTOB << "/" << minHitsinTEC;
 
   TkMap = new TrackerAlignableId();
 }
