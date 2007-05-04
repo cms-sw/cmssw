478043232bdab393fca092d36b9700a6b2f9ae66
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-05-04 01:14:36 +0000

    Additional persistency for lepton classes + cleanup.

--- AnalysisDataFormats/TopObjects/src/classes.h
+++ AnalysisDataFormats/TopObjects/src/classes.h
@@ -4,31 +4,44 @@
 #include "AnalysisDataFormats/TopObjects/interface/TopMETObject.h"
 #include "AnalysisDataFormats/TopObjects/interface/TtGenEvent.h"
 #include "AnalysisDataFormats/TopObjects/interface/TtSemiEvtSolution.h"
+
 #include "DataFormats/Common/interface/Wrapper.h"
+
 #include <vector>
 
-namespace { namespace {
-  //say which template classes should have dictionaries
-  edm::Wrapper<TopElectronObject> dummy1;
-  edm::Wrapper<TopMuonObject> dummy2;
-  edm::Wrapper<TopJetObject> dummy3;
-  edm::Wrapper<TopMETObject> dummy4;
-  edm::Wrapper<std::vector<TopElectronObject> > dummy5;
-  edm::Wrapper<std::vector<TopMuonObject> > dummy6;
-  edm::Wrapper<std::vector<TopJetObject> > dummy7;
-  edm::Wrapper<std::vector<TopMETObject> > dummy8;
-  edm::Wrapper<TtGenEvent> dummy9;
-  edm::Wrapper<TtSemiEvtSolution> dummy10;
-  edm::Wrapper<std::vector<TtSemiEvtSolution> > dummy11;
-  edm::Wrapper<TopElectron> dummy12;
-  edm::Wrapper<TopMuon> dummy13;
-  edm::Wrapper<TopMET> dummy14;
-  edm::Wrapper<TopJet> dummy15;
-  edm::Wrapper<TopParticle> dummy16;
-  edm::Wrapper<std::vector<TopElectron> > dummy17;
-  edm::Wrapper<std::vector<TopMuon> > dummy18;
-  edm::Wrapper<std::vector<TopMET> > dummy19;
-  edm::Wrapper<std::vector<TopJet> > dummy20;
-  edm::Wrapper<std::vector<TopParticle> > dummy21;
-} }
 
+namespace {
+  namespace {
+
+    edm::Wrapper<std::vector<std::pair<double, double> > > dummy0;
+
+    edm::Wrapper<TopElectron>                              dummy10;
+    edm::Wrapper<TopMuon>                                  dummy11;
+    edm::Wrapper<TopJet>                                   dummy13;
+    edm::Wrapper<TopMET>                                   dummy12;
+    edm::Wrapper<TopParticle>                              dummy14;
+    edm::Wrapper<TopObject<ElectronType> >                 dummy15;
+    edm::Wrapper<TopObject<MuonType> >                     dummy16;
+    edm::Wrapper<TopElectronObject>                        dummy17;
+    edm::Wrapper<TopMuonObject>                            dummy18;
+    edm::Wrapper<TopJetObject>                             dummy19;
+    edm::Wrapper<TopMETObject>                             dummy110;
+
+    edm::Wrapper<std::vector<TopElectron> >                dummy20;
+    edm::Wrapper<std::vector<TopMuon> >                    dummy21;
+    edm::Wrapper<std::vector<TopJet> >                     dummy23;
+    edm::Wrapper<std::vector<TopMET> >                     dummy22;
+    edm::Wrapper<std::vector<TopParticle> >                dummy24;
+    edm::Wrapper<std::vector<TopObject<ElectronType> > >   dummy25;
+    edm::Wrapper<std::vector<TopObject<MuonType> > >       dummy26;
+    edm::Wrapper<std::vector<TopElectronObject> >          dummy27;
+    edm::Wrapper<std::vector<TopMuonObject> >              dummy28;
+    edm::Wrapper<std::vector<TopJetObject> >               dummy29;
+    edm::Wrapper<std::vector<TopMETObject> >               dummy210;
+
+    edm::Wrapper<TtGenEvent>                               dummy30;
+    edm::Wrapper<TtSemiEvtSolution>                        dummy31;
+    edm::Wrapper<std::vector<TtSemiEvtSolution> >          dummy32;
+
+  }
+}
