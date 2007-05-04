448ba8b2e784df7911fa2985111bf0b1a6bed329
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-05-04 23:17:40 +0000

    Electronics id extended to allow definition of trigger-chain electronics ids (with SLB site information)

--- DataFormats/HcalDetId/interface/HcalElectronicsId.h
+++ DataFormats/HcalDetId/interface/HcalElectronicsId.h
@@ -2,17 +2,19 @@
 #define DATAFORMATS_HCALDETID_HCALELECTRONICSID_H 1
 
 #include <boost/cstdint.hpp>
+#include <string>
 #include <ostream>
 
 /** \brief Readout chain identification for Hcal
-    [31:25] Unused (so far)
+    [31:26] Unused (so far)
+    [25]    Trigger-chain id flag
     [24:20] Readout Crate Id
     [19] HTR FPGA selector [t/b]
     [18:14] HTR Slot
     [13:9]  DCC id
     [8:5]   Spigot
-    [4:2]   FiberIndex
-    [1:0]   FiberChanId
+    [4:2]   FiberIndex or SLB site
+    [1:0]   FiberChanId or SLB channel
  */
 class HcalElectronicsId {
 public:
@@ -22,17 +24,29 @@ public:
   HcalElectronicsId(uint32_t);
   /** Constructor from fiberchan,fiber index,spigot,dccid */
   HcalElectronicsId(int fiberChan, int fiberIndex, int spigot, int dccid);
+  /** Constructor from slb channel,slb site,spigot,dccid */
+  HcalElectronicsId(int slbChan, int slbSite, int spigot, int dccid, int crate, int slot, int tb);
   uint32_t operator()() { return hcalElectronicsId_; }
 
   uint32_t rawId() const { return hcalElectronicsId_; }
 
+  bool isTriggerChainId() const { return (hcalElectronicsId_&0x04000000)!=0; }
+
   /** Set the htr-related information 1=top, 0=bottom*/
   void setHTR(int crate, int slot, int tb);
 
-  /// get the fiber channel id (which of three channels on a readout fiber)
+  /// get the fiber channel id (which of three channels on a readout fiber) (valid only for non-trigger-chain ids)
   int fiberChanId() const { return hcalElectronicsId_&0x3; }
-  /// get the fiber index [1-8] (which of eight fibers carried by a spigot)
+  /// get the fiber index [1-8] (which of eight fibers carried by a spigot) (valid only for non-trigger-chain ids)
   int fiberIndex() const { return ((hcalElectronicsId_>>2)&0x7)+1; }
+  /// get the SLB channel index  (valid only for trigger-chain ids)
+  int slbChannelIndex() const { return hcalElectronicsId_&0x3; }
+  /// get the SLB site number (valid only for trigger-chain ids)
+  int slbSiteNumber() const { return ((hcalElectronicsId_>>2)&0x7)+1; }
+
+  /// get the HTR-wide slb channel code (letter plus number)
+  std::string slbChannelCode() const;
+
   /// get the HTR channel id (1-24)
   int htrChanId() const { return (fiberChanId()+1)+((fiberIndex()-1)*3); }
   /// get the spigot (input number on DCC)
