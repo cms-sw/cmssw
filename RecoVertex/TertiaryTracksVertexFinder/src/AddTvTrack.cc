7994750dce1a54c53cf4173d792b9b70395bf2fa
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-05-04 13:04:20 +0000

    Revert to CLHEP, with correct SMatrix-track interface

--- RecoVertex/TertiaryTracksVertexFinder/src/AddTvTrack.cc
+++ RecoVertex/TertiaryTracksVertexFinder/src/AddTvTrack.cc
@@ -48,8 +48,7 @@ vector<TransientVertex> AddTvTrack::getSecondaryVertices(const vector<TransientT
 
     TrajectoryStateClosestToPoint tscp=(*itT).trajectoryStateClosestToPoint(vtxPoint);
     double val=tscp.perigeeParameters().transverseImpactParameter();
-    //double error=sqrt((tscp.perigeeError().covarianceMatrix_old())[3][3]); // FIXME
-    double error=sqrt((tscp.perigeeError().covarianceMatrix())(3,3)); // FIXME
+    double error=sqrt((tscp.perigeeError().covarianceMatrix_old())[3][3]);
 
     if (debug) cout <<"[AddTvTrack] tip val,err"<<val<<","<<error<<endl;
 
@@ -233,8 +232,7 @@ vector<TransientVertex> AddTvTrack::getSecondaryVertices(const vector<TransientT
       GlobalPoint vtxPoint((*thePrimaryVertices)[0].position());
       TrajectoryStateClosestToPoint tscp=(*itT).trajectoryStateClosestToPoint(vtxPoint);
       double IP=tscp.perigeeParameters().transverseImpactParameter();
-      //double error=sqrt((tscp.perigeeError().covarianceMatrix_old())[3][3]); // FIXME
-      double error=sqrt((tscp.perigeeError().covarianceMatrix())(3,3)); // FIXME
+      double error=sqrt((tscp.perigeeError().covarianceMatrix_old())[3][3]);
       double err=sqrt(pow(error,2)+pow(0.0015,2));
       double IPSig = IP/err;
 
