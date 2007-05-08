5c9e55c49892866eb0b4a1f7b96077880f62c528
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2007-05-08 14:53:44 +0000

    add ttrig analyzer

--- CalibMuon/DTCalibration/test/DBTools/SealModule.cc
+++ CalibMuon/DTCalibration/test/DBTools/SealModule.cc
@@ -8,6 +8,7 @@
 #include "DumpDBToFile.h"
 #include "DumpFileToDB.h"
 #include "DTT0Analyzer.h"
+#include "DTTTrigAnalyzer.h"
 #include "ProduceFakeDB.h"
 
 
@@ -15,6 +16,7 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DumpDBToFile);
 DEFINE_ANOTHER_FWK_MODULE(DumpFileToDB);
 DEFINE_ANOTHER_FWK_MODULE(DTT0Analyzer);
+DEFINE_ANOTHER_FWK_MODULE(DTTTrigAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ProduceFakeDB);
 
 
