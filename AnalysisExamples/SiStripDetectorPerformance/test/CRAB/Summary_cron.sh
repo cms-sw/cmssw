962e28cd010e43777f653e3484bafeab67380b3e
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-05-08 07:12:25 +0000

    summary cron moved here

---
+++ AnalysisExamples/SiStripDetectorPerformance/test/CRAB/Summary_cron.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+####################################
+#//////////////////////////////////#
+####################################
+##           LOCAL PATHS          ##
+## change this for your local dir ##
+####################################
+
+## Where to find all the templates and to write all the logs
+export LOCALHOME=/analysis/sw/CRAB
+## Where to copy all the results
+export MainStoreDir=/data1/CrabAnalysis
+## Where to create crab jobs
+export WorkingDir=/tmp/${USER}
+## Leave python path as it is to source in standard (local) area
+export python_path=/analysis/sw/CRAB
+
+####################################
+#//////////////////////////////////#
+####################################
+
+########################################
+## Patch to make it work with crontab ##
+###########################################
+export MYHOME=/analysis/sw/CRAB
+# the scritps will source ${MYHOME}/crab.sh
+###########################################
+
+[ -e ${LOCALHOME}/lock_summ ] && exit
+
+touch ${LOCALHOME}/lock_summ
+
+echo -e "\n...Running BadStripsFromPosition"
+${LOCALHOME}/macros/BadStripsFromPosition.sh
+
+echo -e "\n...Creating Summaries"
+${LOCALHOME}/getSummary.sh
+
+rm -f ${LOCALHOME}/lock_summ
