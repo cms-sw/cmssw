33ef23703f2cdfae93361aca7843b655bf7ca2f5
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2007-05-08 13:22:44 +0000

    Corrected a bug: non isolated candidate were not checked. Now it take in inputs also the SeedSuperClusterAssociationCollection maps (barrel and endcap) for L1NonIsolated

--- HLTrigger/Egamma/interface/HLTElectronPixelMatchFilter.h
+++ HLTrigger/Egamma/interface/HLTElectronPixelMatchFilter.h
@@ -22,10 +22,17 @@ class HLTElectronPixelMatchFilter : public HLTFilter {
 
    private:
       edm::InputTag candTag_;     // input tag identifying product contains filtered egammas
-      edm::InputTag pixelmapbarrelTag_; // input tag for the pixel seed - supercluster map
-      edm::InputTag pixelmapendcapTag_; // input tag for the pixel seed - supercluster map
+
+      edm::InputTag L1IsoPixelmapbarrelTag_; // input tag for the pixel seed - supercluster map
+      edm::InputTag L1IsoPixelmapendcapTag_; // input tag for the pixel seed - supercluster map
+
+      edm::InputTag L1NonIsoPixelmapbarrelTag_; // input tag for the pixel seed - supercluster map
+      edm::InputTag L1NonIsoPixelmapendcapTag_; // input tag for the pixel seed - supercluster map
+
       double npixelmatchcut_;     // number of pixelmatch hits
       int    ncandcut_;           // number of electrons required
+      
+      bool doIsolated_;
 
 };
 
