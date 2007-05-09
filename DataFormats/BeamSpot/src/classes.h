c38e40f8106a1e711858651fe5b02652488d3175
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2007-05-09 20:17:31 +0000

    add a edm::wrapper type

--- DataFormats/BeamSpot/src/classes.h
+++ DataFormats/BeamSpot/src/classes.h
@@ -1,9 +1,11 @@
 
+#include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/BeamSpot/interface/BeamSpot.h"
 
 
 namespace {
 
 	reco::BeamSpot b;
-
+	edm::Wrapper<reco::BeamSpot> b_w;
+	
 }
