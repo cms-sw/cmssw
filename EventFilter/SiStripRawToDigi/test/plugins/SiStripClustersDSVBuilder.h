1fac377fd19349bbd21edbc98964b911f24434c3
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2007-05-09 10:09:48 +0000

    loops SiStripRefGetter<SiStripCluster> object from event and writes DetSetVector<SiStripCluster>

---
+++ EventFilter/SiStripRawToDigi/test/plugins/SiStripClustersDSVBuilder.h
@@ -0,0 +1,45 @@
+#ifndef EventFilter_SiStripRawToDigi_SiStripClustersDSVBuilder_H
+#define EventFilter_SiStripRawToDigi_SiStripClustersDSVBuilder_H
+
+//FWCore
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//Data Formats
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/SiStripCommon/test/stubs/SiStripLazyGetter.h"
+#include "DataFormats/SiStripCommon/test/stubs/SiStripRefGetter.h"
+#include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+
+//stl
+#include <memory>
+#include "boost/bind.hpp"
+
+class SiStripClustersDSVBuilder : public edm::EDProducer {
+  
+ public:
+
+  typedef edm::DetSet<SiStripCluster> DetSet;
+  typedef edm::DetSetVector<SiStripCluster> DSV;
+  typedef edm::SiStripRefGetter< SiStripCluster, edm::SiStripLazyGetter<SiStripCluster> > RefGetter;
+
+  SiStripClustersDSVBuilder( const edm::ParameterSet& );
+  ~SiStripClustersDSVBuilder();
+  
+  virtual void beginJob( const edm::EventSetup& );
+  virtual void endJob();
+  virtual void produce( edm::Event&, const edm::EventSetup& );
+  
+ private: 
+
+  /** Input label */
+  std::string inputModuleLabel_;
+
+  /** Output Product label */
+  std::string outputProductLabel_;
+};
+
+#endif //  EventFilter_SiStripRawToDigi_SiStripClustersDSVBuilder_H
