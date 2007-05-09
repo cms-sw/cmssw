f555a00c2fcb8697e9dc50b3246ed34cf2539a5f
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-05-09 15:28:17 +0000

    Clean the olde stuff from ORCA which ws commented out

--- RecoEgamma/EgammaMCTools/interface/PhotonMCTruth.h
+++ RecoEgamma/EgammaMCTools/interface/PhotonMCTruth.h
@@ -12,8 +12,8 @@
  *  This class stores all the MC truth information needed about the
  *  conversion
  * 
- *  $Date: 2007/04/13 12:28:01 $
- *  $Revision: 1.1 $
+ *  $Date: 2007/04/17 08:55:16 $
+ *  $Revision: 1.2 $
  *  \author N. Marinelli  IASA-Athens
  *
  */
@@ -34,59 +34,15 @@ public:
 
 
  HepLorentzVector primaryVertex() const {return thePrimaryVertex_;}
- // const vector<const TkSimTrack*> &convSimTracks()  { return theTracks_;}
  int isAConversion() const { return isAConversion_;}
  float radius() const {return theR_;}
  float z() const {return theZ_;}
  HepLorentzVector fourMomentum() const {return thePhoton_;}
  HepLorentzVector vertex() const {return theConvVertex_;}
- std::vector<const SimTrack *> simTracks() const {return tracks_;} 
- float ptTrack1() const;
- float ptTrack2() const;
- float invMass() const;
- 
 
- /*
- vector<GlobalPoint> track1Hits() const {
-   if(theTracks_.size() > 0) {
-     return getHitPositions(theTracks_[0]);
-   } else {
-     vector<GlobalPoint> theHitPositions;
-     return theHitPositions;
-   }
- };
- vector<GlobalPoint> track2Hits() const {
-   if(theTracks_.size() > 1) {
-     return getHitPositions(theTracks_[1]);
-   } else {
-     vector<GlobalPoint> theHitPositions;
-     return theHitPositions;
-   }
- };
- */
- 
- //Get nearest basic cluster to point where SimTrack impacts ECAL
- // const EgammaBasicCluster * bcFromTrack1() const{
- // return (theBasicClusters_.size() > 0) ? theBasicClusters_[0] : 0;};
-
- // const EgammaBasicCluster * bcFromTrack2() const{
- // return (theBasicClusters_.size() > 1) ? theBasicClusters_[1] : 0;};
- 
- GlobalPoint ecalImpactPosTrack1() const {return ecalPosTrack1_;};
- GlobalPoint ecalImpactPosTrack2() const {return ecalPosTrack2_;};
-
- // vector<GlobalPoint> getHitPositions(const TkSimTrack*  const &theTkSimTrack) ;
  
  private:
 
- //  void basicClusters();
-  //Find nearest basic cluster to point where each SimTrack impacts ECAL
- // const EgammaBasicCluster * bcFromTrack(const TkSimTrack * theTkSimTrack) const;
- // void printTrackHits (const TkSimTrack * theTkSimTrack) const;
-  //  vector<GlobalPoint> getHitPositions(const TkSimTrack*  &theTkSimTrack) const ;
-
-  //Find point where each SimTrack impacts ECAL
- //  void ecalImpactPositions ();
 
 
   int isAConversion_;
@@ -97,9 +53,6 @@ public:
   HepLorentzVector thePrimaryVertex_;
   std::vector<const SimTrack *> tracks_;
 
-
-  GlobalPoint ecalPosTrack1_;
-  GlobalPoint ecalPosTrack2_;
 };
 
 #endif
