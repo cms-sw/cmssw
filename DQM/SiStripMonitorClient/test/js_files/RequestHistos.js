6ccb7fa13e5d08d0fd055a635bcc83e86a55aebb
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2007-05-11 20:02:34 +0000

    added editable select menu in the html

--- DQM/SiStripMonitorClient/test/js_files/RequestHistos.js
+++ DQM/SiStripMonitorClient/test/js_files/RequestHistos.js
@@ -59,6 +59,15 @@ function FillModuleHistoList() {
             aobj.add(aoption, -1);
           }
         }
+	var cobj = document.getElementById("module_number_edit");
+   	if (cobj != null) {
+          cobj.value = aobj.options[0].value;
+        } 
+        // Select the first option and set to editable text  
+        var cobj = document.getElementById("module_number_edit");
+        if (cobj != null) {
+          cobj.value = aobj.options[0].value;;
+        }   
 
         // Histogram  select box
         var bobj = document.getElementById("histolistarea");
