5d3cb8e222a3af89c7ff2c33e169d8b625083539
Ted Ritchie Kolberg <sha1-dd045d023f2251ade8f4868cee28aa7a221d58be@cern.ch>
2007-05-13 23:04:23 +0000

    get decay data

--- RecoEgamma/EgammaMCTools/interface/PizeroMCTruth.h
+++ RecoEgamma/EgammaMCTools/interface/PizeroMCTruth.h
@@ -14,6 +14,13 @@ class PizeroMCTruth {
     void SetDecay(float r, float z, CLHEP::HepLorentzVector momentum1, CLHEP::HepLorentzVector momentum2);
     void SetDalitzDecay(float r, float z, CLHEP::HepLorentzVector momentum1, CLHEP::HepLorentzVector momentum2, CLHEP::HepLorentzVector momentum3);
 
+    bool IsDalitz() { return dalitz_; };
+    float GetR() { return r_; };
+    float GetZ() { return z_; };
+    CLHEP::HepLorentzVector GetMomentum1() { return momentum1_; };
+    CLHEP::HepLorentzVector GetMomentum2() { return momentum2_; };
+    CLHEP::HepLorentzVector GetMomentum3() { return momentum3_; };
+
   private:
     bool dalitz_;
     float r_;
