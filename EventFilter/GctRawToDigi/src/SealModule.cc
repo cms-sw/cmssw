aa6615ec7faa95d8202701d70f86e773f4c1af13
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-05-14 10:05:49 +0000

    updated to new plugin manager

--- EventFilter/GctRawToDigi/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "EventFilter/GctRawToDigi/src/GctRawToDigi.h"
-#include "EventFilter/GctRawToDigi/src/GctVmeToRaw.h"
-
-DEFINE_FRAMEWORK_MODULE(GctRawToDigi);
-
