817f4a54c571aefa037e2e2c5ccd749cec67aa56
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-05-14 22:50:08 +0000

    Moved files back into src

--- SimMuon/CSCDigitizer/plugins/SealModule.cc
+++ SimMuon/CSCDigitizer/plugins/SealModule.cc
@@ -1,8 +1,8 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimMuon/CSCDigitizer/plugins/CSCDigiProducer.h"
-#include "SimMuon/CSCDigitizer/plugins/CSCDigiDump.h"
-#include "SimMuon/CSCDigitizer/plugins/CSCNeutronWriter.h"
+#include "SimMuon/CSCDigitizer/src/CSCDigiProducer.h"
+#include "SimMuon/CSCDigitizer/src/CSCDigiDump.h"
+#include "SimMuon/CSCDigitizer/src/CSCNeutronWriter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiProducer);
