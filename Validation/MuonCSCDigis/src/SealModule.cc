dfbf815e33bc8577db1461806edbcee989b73b06
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-05-14 19:51:58 +0000

    Should have moved to plugins

--- Validation/MuonCSCDigis/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "Validation/MuonCSCDigis/src/CSCDigiValidation.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-
-DEFINE_SEAL_MODULE ();
-DEFINE_ANOTHER_FWK_MODULE (CSCDigiValidation) ;
-
-
