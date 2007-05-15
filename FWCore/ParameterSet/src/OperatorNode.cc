c1a7b015881af3a41a0e4002ca481dec4ffb7b50
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-05-15 21:54:42 +0000

    Factored out, added a clone() method

---
+++ FWCore/ParameterSet/src/OperatorNode.cc
@@ -0,0 +1,100 @@
+
+#include "FWCore/ParameterSet/interface/OperatorNode.h"
+#include "FWCore/ParameterSet/interface/OperandNode.h"
+#include "FWCore/ParameterSet/interface/Visitor.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <typeinfo>
+#include <iterator>
+
+using namespace std;
+
+namespace edm {
+
+  namespace pset {
+
+
+    // -------------------------
+
+    string makeOpName()
+    {
+      static int opcount = 0;
+      ostringstream ost;
+      ++opcount;
+      ost << "op" << opcount;
+      return ost.str();
+    }
+
+    bool operator_or_operand(NodePtr n)
+    {
+      Node* p = n.operator->();
+      return 
+	( dynamic_cast<OperatorNode*>(p) != 0 ||
+	  dynamic_cast<OperandNode*>(p) != 0 );
+    }
+
+    //--------------------------------------------------
+    // OperatorNode
+    //--------------------------------------------------
+
+    OperatorNode::OperatorNode(const string& type,
+			       NodePtr left, 
+			       NodePtr right,
+			       int line):
+      Node(makeOpName(), line),
+      type_(type),
+      left_(left),
+      right_(right)
+    {   
+      assert( operator_or_operand(left) );
+      assert( operator_or_operand(right) );
+      left_->setParent(this);
+      right_->setParent(this);
+    }
+
+    string OperatorNode::type() const { return type_; }
+
+
+    Node * OperatorNode::clone() const 
+    { 
+      OperatorNode * newNode =  new OperatorNode(*this);
+      newNode->left_ = NodePtr( left_->clone() );
+      newNode->right_ = NodePtr( right_->clone() );
+      newNode->left_->setParent(newNode);
+      newNode->right_->setParent(newNode);
+      return newNode;
+    }
+
+
+    void OperatorNode::print(ostream& ost, Node::PrintOptions options) const
+    {
+      ost << "( " << left_ << " " << type_ << " " << right_ << " )";
+    }
+
+
+    bool OperatorNode::findChild(const std::string & childName, NodePtr & result)
+    {
+      bool foundLeft  = left()->findChild(childName, result);
+      bool foundRight = right()->findChild(childName, result);
+
+      if(foundLeft && foundRight)
+      {
+        throw edm::Exception(errors::Configuration)
+         << "A child named " << childName 
+         << " was found on both sides of an operator";
+      }
+      return (foundLeft || foundRight);
+    }
+        
+
+    void OperatorNode::accept(Visitor& v) const
+    {
+      v.visitOperator(*this);
+      //throw runtime_error("OperatorNodes cannot be visited");
+    }
+
+  }
+}
