a765c3bddbda878b35d5d69a68389b8bd29c00af
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2007-05-15 00:26:56 +0000

    Fixes for generation of dictionaries for 64-bit build

--- DataFormats/RPCRecHit/src/classes.h
+++ DataFormats/RPCRecHit/src/classes.h
@@ -4,6 +4,15 @@
 
 namespace{ 
   namespace {
+    std::pair<unsigned int, unsigned int> dummyrpc1;
+    std::pair<unsigned long, unsigned long> dummyrpc2;
+    std::map<RPCDetId, std::pair<unsigned int, unsigned int> > dummyrpcdetid1;
+    std::map<RPCDetId, std::pair<unsigned long, unsigned long> > dummyrpcdetid2;
+  }
+}
+
+namespace{ 
+  namespace {
     RPCRecHit rrh;
     std::vector<RPCRecHit> vrh;
     RPCRecHitCollection c;
