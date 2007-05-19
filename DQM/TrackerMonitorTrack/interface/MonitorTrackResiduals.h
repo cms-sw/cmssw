447ffcae258312a4d0bc99a9bf218ce47ee0aa59
Dorian Kcira <sha1-4b05586558d85b59db96d5e5f9039f53ad1d4a66@cern.ch>
2007-05-19 10:35:57 +0000

    add class description

--- DQM/TrackerMonitorTrack/interface/MonitorTrackResiduals.h
+++ DQM/TrackerMonitorTrack/interface/MonitorTrackResiduals.h
@@ -7,40 +7,21 @@
 // Class:      MonitorTrackResiduals
 // 
 /**\class MonitorTrackResiduals MonitorTrackResiduals.h DQM/TrackerMonitorTrack/interface/MonitorTrackResiduals.cc
-
- Description: <one line class summary>
-
- Implementation:
-     <Notes on implementation>
+Monitoring source for track residuals on each detector module
 */
-//
 // Original Author:  Israel Goitom
 //         Created:  Fri May 26 14:12:01 CEST 2006
-// $Id: MonitorTrackResiduals.h,v 1.8 2007/02/13 16:48:01 dkcira Exp $
-//
-//
-
-
-// system include files
+// $Id: MonitorTrackResiduals.h,v 1.9 2007/02/28 13:40:04 goitom Exp $
 #include <memory>
 #include <fstream>
- 
-// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
 #include "DQMServices/Daemon/interface/MonitorDaemon.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
-
-//
-// class decleration
-//
 
 typedef std::map<int, MonitorElement *> HistoClass;
 
@@ -54,13 +35,10 @@ class MonitorTrackResiduals : public edm::EDAnalyzer {
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
    private:
   std::map<uint32_t, MonitorElement *> detModules;
-
 //  unsigned int minTracks_;
-
   DaqMonitorBEInterface * dbe;
   edm::ParameterSet conf_;
 
   HistoClass HitResidual;
 };
-
 #endif
