7de30888eac9abf8b0a2d5e39eb5187dce08a982
Ian Tomalin <sha1-3fa43f967df83df8ba717ce5a271c8449420497d@cern.ch>
2007-05-21 11:12:09 +0000

    for CMSSW 1.5.x

--- HLTrigger/btau/src/SealModule.cc
+++ HLTrigger/btau/src/SealModule.cc
@@ -1,16 +1,14 @@
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/btau/interface/HLTJetTag.h"
 #include "HLTrigger/btau/interface/HLTTauL25DoubleFilter.h"
 #include "HLTrigger/btau/interface/HLTDisplacedmumuFilter.h"
 #include "HLTrigger/btau/interface/HLTL1MuonCorrector.h"
-#include "HLTrigger/btau/interface/GetJetsFromHLTobject.h"
+#include "HLTrigger/btau/interface/HLTmumuGammaFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTJetTag);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauL25DoubleFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTDisplacedmumuFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTL1MuonCorrector);
-DEFINE_ANOTHER_FWK_MODULE(GetJetsFromHLTobject);
-
+DEFINE_ANOTHER_FWK_MODULE(HLTmumuGammaFilter);
