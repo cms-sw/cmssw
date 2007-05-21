379b30b7a413ea1f98178b5a7325138baecacb1c
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2007-05-21 21:59:51 +0000

    change from my own conversion to using stringstream. also documented a bit more clearly in header.

--- DetectorDescription/Base/interface/DDutils.h
+++ DetectorDescription/Base/interface/DDutils.h
@@ -5,11 +5,13 @@
 #include <vector>
 #include <iostream>
 
+/// Converts a std::vector of doubles to a std::vector of int
 std::vector<int>  dbl_to_int ( const std::vector<double> & vecdbl);
-//Converts a std::vector of doubles to a std::vector of int
 
+/// Converts only the integer part of a double to a string.
 std::string dbl_to_string (const double& in );
-std::string int_to_string (const int& in );
+
 //Converts int or double to std::string
+std::string int_to_string (const int& in );
 
 #endif
--- DetectorDescription/Base/src/DDutils.cc
+++ DetectorDescription/Base/src/DDutils.cc
@@ -1,8 +1,7 @@
 #include "DetectorDescription/Base/interface/DDutils.h"
-#include <fstream>
-#include <cmath>
 #include <vector>
 #include <string>
+#include <sstream>
 
 std::vector<int> dbl_to_int ( const std::vector<double> & vecdbl) {
   std::vector<int> tInt;
@@ -17,53 +16,9 @@ std::string dbl_to_string (const double& in) {
 }
 
 std::string int_to_string(const int& in) {
-  if (in < 0) return std::string("-") + int_to_string(in * -1);
-  
-  if (in < 10) {
-    switch (in) {
-    case 0: 
-      return std::string("0");
-      break;
 
-    case 1:
-      return std::string("1");
-      break;
+  std::ostringstream ostr;
+  ostr << in;
+  return ostr.str();
 
-    case 2:
-      return std::string("2");
-      break;
-
-    case 3:
-      return std::string("3");
-      break;
-
-    case 4:
-      return std::string("4");
-      break;
-
-    case 5:
-      return std::string("5");
-      break;
-
-    case 6:
-      return std::string("6");
-      break;
-
-    case 7:
-      return std::string("7");
-      break;
-
-    case 8:
-      return std::string("8");
-      break;
-      
-    case 9:
-      return std::string("9");
-      break;
-
-    default:
-      return std::string(" ");
-    }
-  }
-  return int_to_string(in / 10) + int_to_string(in % 10);
 }
