abd84fddb1e4d369e519e1258cc09b14655990b0
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2007-05-22 20:40:27 +0000

    make code compile

--- Geometry/EcalMapping/plugins/SealModule.cc
+++ Geometry/EcalMapping/plugins/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "Geometry/EcalMapping/interface/EcalElectronicsMappingBuilder.h"
+#include "Geometry/EcalMapping/plugins/EcalElectronicsMappingBuilder.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(EcalElectronicsMappingBuilder);
