326ab525a8256d01dcac635f00e7e6ed0a23d4ea
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-05-22 17:13:26 +0000

    add explicit constructor add operator()

--- RecoBTau/JetTagComputer/interface/JetTagComputer.h
+++ RecoBTau/JetTagComputer/interface/JetTagComputer.h
@@ -1,11 +1,25 @@
 #ifndef RecoBTau_JetTagComputer_h
 #define RecoBTau_JetTagComputer_h
+
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/BTauReco/interface/BaseTagInfo.h"
-class  JetTagComputer
-{
- public:
+
+class JetTagComputer {
+public:
+  // default constructor
+  JetTagComputer(void) { }
+
+  // explicit constructor accepting a ParameterSet for configuration
+  explicit JetTagComputer(const edm::ParameterSet & configuration) { }
+  
   virtual float discriminator(const reco::BaseTagInfo &) const = 0;
-  virtual void setEventSetup(const edm::EventSetup &) const {}
+  virtual void  setEventSetup(const edm::EventSetup &) const { }
+
+  float operator()(const reco::BaseTagInfo & info) const {
+    return discriminator(info);
+  }
+  
 };
-#endif
+
+#endif // RecoBTau_JetTagComputer_h
