6f89548bfbcfb8e6721e9e6bc2f94257158ea6ec
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-05-22 17:15:43 +0000

    *** empty log message ***

--- RecoBTau/JetTagComputer/src/JetTagComputer.cc
+++ RecoBTau/JetTagComputer/src/JetTagComputer.cc
@@ -1,5 +1,4 @@
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
 #include "RecoBTau/JetTagComputer/interface/JetTagComputer.h"
 
 EVENTSETUP_DATA_REG(JetTagComputer);
--- RecoBTau/JetTagComputer/src/JetTagComputerRecord.cc
+++ RecoBTau/JetTagComputer/src/JetTagComputerRecord.cc
@@ -1,4 +1,4 @@
-#include "RecoBTau/JetTagComputer/interface/JetTagComputerRecord.h"
 #include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+#include "RecoBTau/JetTagComputer/interface/JetTagComputerRecord.h"
 
 EVENTSETUP_RECORD_REG(JetTagComputerRecord);
