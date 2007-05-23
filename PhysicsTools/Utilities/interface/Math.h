89237a018bece65c0d9a3a43a6036ef5d09c13e5
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2007-05-23 19:08:12 +0000

    added calculation of three-dimensional spatial angle between two vectors

--- PhysicsTools/Utilities/interface/Math.h
+++ PhysicsTools/Utilities/interface/Math.h
@@ -1,7 +1,7 @@
 #ifndef Utilities_Math_h
 #define Utilities_Math_h
 /*
- * Ported from orignla code in RecoJets 
+ * Ported from original code in RecoJets 
  * by Fedor Ratnikov, FNAL
  */
 #include <cmath>
@@ -13,6 +13,10 @@ T deltaPhi (T phi1, T phi2) {
   while (result <= -M_PI) result += 2*M_PI;
   return result;
 }
+
+//
+//-------------------------------------------------------------------------------
+//
 
 template <class T>
 T deltaR2 (T eta1, T phi1, T eta2, T phi2) {
@@ -34,6 +38,20 @@ double deltaR2( const T1 & t1, const T2 & t2 ) {
 template<typename T1, typename T2>
 double deltaR( const T1 & t1, const T2 & t2 ) {
   return deltaR( t1.eta(), t1.phi(), t2.eta(), t2.phi() );
+} 
+
+//
+//-------------------------------------------------------------------------------
+//
+
+template <class T>
+T angle (T x1, T y1, T z1, T x2, T y2, T z2) {
+  return acos((x1*x2 + y1*y2 + z1*z2)/sqrt((x1*x1 + y1*y1 + z1*z1)*(x2*x2 + y2*y2 + z2*z2)));
+}
+
+template<typename T1, typename T2>
+double angle( const T1 & t1, const T2 & t2 ) {
+  return angle( t1.x(), t1.y(), t1.z(), t2.x(), t2.y(), t2.z() );
 } 
 
 #endif
