f825ce273e580e2aeadad7e8bf05703eeb70a8f5
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2007-05-23 19:08:53 +0000

    added calculation of three-dimensional spatial distance between two vectors

---
+++ PhysicsTools/Utilities/interface/Angle.h
@@ -0,0 +1,23 @@
+#ifndef PhysicsTools_Utilities_Angle_h
+#define PhysicsTools_Utilities_Angle_h
+
+/* \class Angle
+ *
+ * returns three-dimensional Angle between two objects;
+ * defined via scalar product: 
+ *   angle = acos((v1 * v2)/(|v1| * |v2|))
+ *
+ * \author Christian Veelken, UC Davis
+ */
+
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "PhysicsTools/Utilities/interface/Math.h"
+
+template<typename T1, typename T2 = T1>
+struct Angle {
+  double operator()( const T1 & t1, const T2 & t2 ) const {
+    return angle( t1, t2 );
+  }
+};
+
+#endif
