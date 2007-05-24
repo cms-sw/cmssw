8863e1eb92188b8249d864c9cbdae499e7d43dfe
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2007-05-24 11:48:57 +0000

    more general pT alghorithm

--- PhysicsTools/IsolationUtils/interface/PtIsolationAlgo.h
+++ PhysicsTools/IsolationUtils/interface/PtIsolationAlgo.h
@@ -11,12 +11,14 @@ class PtIsolationAlgo {
 public:
   typedef double value_type;
   PtIsolationAlgo() { }
-  PtIsolationAlgo( double dRMin, double dRMax, double dzMax ) :
-    dRMin_( dRMin ), dRMax_( dRMax ), dzMax_( dzMax ) { }
+  PtIsolationAlgo( double dRMin, double dRMax, double dzMax,
+		   double d0Max, double ptMin ) :
+    dRMin_( dRMin ), dRMax_( dRMax ), dzMax_( dzMax ),
+    d0Max_( d0Max ), ptMin_( ptMin ) { }
   double operator()(const T &, const C &) const;
 
 private:
-  double dRMin_, dRMax_, dzMax_;
+  double dRMin_, dRMax_, dzMax_, d0Max_, ptMin_;
 };
 
 template <typename T, typename C>
@@ -26,11 +28,17 @@ double PtIsolationAlgo<T, C>::operator()(const T & cand, const C & elements) con
   double candEta = cand.eta();
   double candPhi = cand.phi();
   for( typename C::const_iterator elem = elements.begin(); elem != elements.end(); ++ elem ) {
+    double elemPt = elem->pt();
+    if ( elemPt < ptMin_ ) continue;
+    double elemVx = elem->vx();
+    double elemVy = elem->vy();
+    double elemD0 = sqrt( elemVx * elemVx + elemVy * elemVy );
+    if ( elemD0 > d0Max_ ) continue;
     double dz = fabs( elem->vz() - candVz );
+    if ( dz > dzMax_ ) continue;
     double dR = deltaR( elem->eta(), elem->phi(), candEta, candPhi );
-    if ( dz < dzMax_ &&  dR < dRMax_ && dR > dRMin_ ) {
-      ptSum += elem->pt();
-    }
+    if ( (dR > dRMax_) || (dR < dRMin_) ) continue;
+    ptSum += elemPt;
   }
   return ptSum;
 }
