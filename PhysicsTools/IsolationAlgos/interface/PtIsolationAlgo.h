d77431bad1dc727081cabe5a04a9812c43424b12
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2007-05-24 11:48:02 +0000

    more general pT alghoritm

--- PhysicsTools/IsolationAlgos/interface/PtIsolationAlgo.h
+++ PhysicsTools/IsolationAlgos/interface/PtIsolationAlgo.h
@@ -15,7 +15,9 @@ namespace reco {
       static PtIsolationAlgo<T, C> make( const edm::ParameterSet & cfg ) {
 	  return PtIsolationAlgo<T, C>( cfg.template getParameter<double>( "dRMin" ), 
 					cfg.template getParameter<double>( "dRMax" ),
-					cfg.template getParameter<double>( "dzMax" ) );
+					cfg.template getParameter<double>( "dzMax" ),
+					cfg.template getParameter<double>( "d0Max" ),
+					cfg.template getParameter<double>( "ptMin" ) );
       }
     };
   }
