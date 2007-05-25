a29fc9c17ca03f11f4f2c2ec3d33279fa652e3df
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-05-25 18:20:19 +0000

    switch to using a struct instead of a namespace in order to avoid instantiating unneeded dummy objects

--- FWCore/Skeletons/scripts/mkTemplates/DataPkg/classes.h
+++ FWCore/Skeletons/scripts/mkTemplates/DataPkg/classes.h
@@ -1,11 +1,11 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 
 //Add includes for your classes here
-#include "skelsubsys/datapkgname/YOUR_CLASS_GOES_HERE.h"
+#include "skelsubsys/datapkgname/interface/YOUR_CLASS_GOES_HERE.h"
 
 namespace {
-   namespace {
+   struct skelsubsys_datapkgname {
       //add 'dummy' Wrapper variable for each class type you put into the Event
       edm::Wrapper<YOUR_CLASS_GOES_HERE> dummy1;
-   }
+   };
 }
