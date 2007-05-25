a73c9da140b43e92664e3dfdf355260ef63aff8a
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-05-25 22:13:47 +0000

    Add a tool to print positions of all sensitive volumes of a detector + correction to some cfg files

--- SimG4Core/PrintGeomInfo/src/module.cc
+++ SimG4Core/PrintGeomInfo/src/module.cc
@@ -1,8 +1,10 @@
 #include "SimG4Core/PrintGeomInfo/interface/PrintGeomInfoAction.h"
 #include "SimG4Core/PrintGeomInfo/interface/PrintMaterialBudgetInfo.h"
+#include "SimG4Core/PrintGeomInfo/interface/PrintSensitive.h"
 #include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
    
 DEFINE_SEAL_MODULE ();
 DEFINE_SIMWATCHER(PrintGeomInfoAction);
 DEFINE_SIMWATCHER(PrintMaterialBudgetInfo);
+DEFINE_SIMWATCHER(PrintSensitive);
