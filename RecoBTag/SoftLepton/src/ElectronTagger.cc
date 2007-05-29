3234aca9a88525a32defc5c395aba184de8f3dfc
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-05-29 20:27:34 +0000

    move return statements out of try catch block

--- RecoBTag/SoftLepton/src/ElectronTagger.cc
+++ RecoBTag/SoftLepton/src/ElectronTagger.cc
@@ -5,10 +5,10 @@
 
 /// b-tag a jet based on track-to-jet parameters in the extened info collection
 float ElectronTagger::discriminator(const reco::BaseTagInfo & tagInfo) const {
+  // default value, used if there are no leptons associated to this jet
+  float bestTag = -1.;
   try {
     const reco::SoftLeptonTagInfo & info = dynamic_cast<const reco::SoftLeptonTagInfo &>(tagInfo);
-    // default value, used if there are no leptons associated to this jet
-    float bestTag = -1.;
     // if there are multiple leptons, look for the highest tag result
     for (unsigned int i = 0; i < info.leptons(); i++) {
       const reco::SoftLeptonProperties & properties = info.properties(i);
@@ -16,9 +16,9 @@ float ElectronTagger::discriminator(const reco::BaseTagInfo & tagInfo) const {
       if (tag > bestTag) 
         bestTag = tag;
     }
-    return bestTag;
   } catch(std::bad_cast e) {
     // ERROR - trying to use the wrong XxxTagInfo
     throw edm::Exception(edm::errors::LogicError) << "Wrong reco::BaseTagInfo-derived collection passed, expected reco::SoftLeptonTagInfo";
   }
+  return bestTag;
 }
--- RecoBTag/SoftLepton/src/LeptonTaggerDistance.cc
+++ RecoBTag/SoftLepton/src/LeptonTaggerDistance.cc
@@ -12,10 +12,10 @@ float LeptonTaggerDistance::discriminator(const reco::BaseTagInfo & tagInfo) con
       if (info.properties(i).deltaR <= m_maxDistance)
         return 1.0;
     }
-    // default value, used if there are no leptons associated to this jet
-    return -1.0;
   } catch(std::bad_cast e) {
     // ERROR - trying to use the wrong XxxTagInfo
     throw edm::Exception(edm::errors::LogicError) << "Wrong reco::BaseTagInfo-derived collection passed, expected reco::SoftLeptonTagInfo";
   }
+  // default value, used if there are no leptons associated to this jet
+  return -1.0;
 }
