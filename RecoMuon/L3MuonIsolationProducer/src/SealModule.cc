45475ee0e3172273856732fb34e61980dab92dde
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2007-05-30 11:09:12 +0000

    *** empty log message ***

--- RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
+++ RecoMuon/L3MuonIsolationProducer/src/SealModule.cc
@@ -8,3 +8,9 @@ DEFINE_SEAL_MODULE();
 #include "L3MuonIsolationProducer.h"
 DEFINE_ANOTHER_FWK_MODULE(L3MuonIsolationProducer);
 
+#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducerFactory.h"
+#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducer.h"
+#include "IsolationRegionAroundL3Muon.h"
+
+DEFINE_EDM_PLUGIN(TrackingRegionProducerFactory, IsolationRegionAroundL3Muon, "IsolationRegionAroundL3Muon");
+
