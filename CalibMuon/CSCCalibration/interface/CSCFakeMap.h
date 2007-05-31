a84a49a26f29242f707c790b7a76ded03c417794
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-05-31 12:48:57 +0000

    modified

--- CalibMuon/CSCCalibration/interface/CSCFakeMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeMap.h
@@ -3,17 +3,49 @@
 
 #include <iostream>
 #include "DataFormats/DetId/interface/DetId.h"
+#include <Geometry/CSCGeometry/interface/CSCLayer.h>
+#include <Geometry/CSCGeometry/interface/CSCChamberSpecs.h>
+#include <Geometry/CSCGeometry/interface/CSCLayerGeometry.h>
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
+#include <CondFormats/CSCObjects/interface/CSCReadoutMapping.h>
+
 #include <map>
+#include <vector>
+#include <iostream>
 
 class CSCFakeMap{
 
 public:
 CSCFakeMap(){}
-virtual ~CSCFakeMap(){}
 
-public:
+ void prefillMap(){
 
-
+   if(detId.station() < 4 && (detId.ring() <=4) )
+     {
+       map_.setValue(gains.rawId(),1.0);
+       map_.setValue(crosstalk.rawId(),1.0);
+       map_.setValue(matrix.rawId(),1.0);
+     }
+   
+ }
+ 
+ void print()
+   {
+     
+     std::map<uint32_t,float>::const_iterator it;
+     
+     for(it=map_.getMap().begin();it!=map_.getMap().end();it++){
+     }
+     
+   }
+ 
+ const CSCobject & get(){
+   return map_;
+ }
+ 
+ public:
+ 
+ 
 };
 
 
