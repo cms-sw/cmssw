166587699e2bc9f827484e4a97138f0cc5b5e51b
biallass <sha1-7a1bc73326736ef8230c83e0a5d14ba2577a78fe@cern.ch>
2007-05-31 09:48:59 +0000

    new version of CMSCGEN using improved parametrizations, see internal note

--- GeneratorInterface/CosmicMuonGenerator/src/CMSCGEN.cc
+++ GeneratorInterface/CosmicMuonGenerator/src/CMSCGEN.cc
@@ -1,16 +1,16 @@
 //
-// CMSCGEN.cc       T.Hebbeker 2006
+// CMSCGEN.cc   version 3.0    Thomas Hebbeker 2007-05-15  
 //
-// implemented in CMSSW by P. Biallass 29.03.2006  
-// see header for documentation
+// implemented in CMSSW by P. Biallass 2007-05-28 
+// see header for documentation and CMS internal note 2007 "Improved Parametrization of the Cosmic Muon Flux for the generator CMSCGEN" by Biallass + Hebbeker
 //
 
 
 #include "GeneratorInterface/CosmicMuonGenerator/interface/CMSCGEN.h"
-
-
-int CMSCGEN::initialize(float Emin_in, float Emax_in, float thetamin_in, float thetamax_in, int RanSeed, bool TIFOnly_constant, bool TIFOnly_linear) 
+ 
+int CMSCGEN::initialize(double pmin_in, double pmax_in, double thetamin_in, double thetamax_in, int RanSeed, bool TIFOnly_constant, bool TIFOnly_linear)  
 {
+
   //set seed for Random Generator (seed can be controled by config-file), P.Biallass 2006
   RanGen2.SetSeed(RanSeed);
 
@@ -18,142 +18,199 @@ int CMSCGEN::initialize(float Emin_in, float Emax_in, float thetamin_in, float t
   TIFOnly_const = TIFOnly_constant;
   TIFOnly_lin = TIFOnly_linear;
 
+
   // units: GeV
 
-  // muon coming from above, vertically: cos = 1
+  // WARNING: coordinate system: 
+  //   - to outside world define z axis downwards, i.e.
+  //          muon coming from above, vertically: cos = 1
+  //      (used for backward compatibility)
+  //   - internally use frame with z axis upwards, i.e.
+  //          muon coming from above, vertically: cos = -1
+  //     (corresponds to CMS note definition)
+
+  //set cmin and cmax, here convert between coordinate systems:
+  cmin_in = - TMath::Cos(thetamin_in);//input angle already converted from Deg to Rad!
+  cmax_in = - TMath::Cos(thetamax_in);//input angle already converted from Deg to Rad!
+
 
   //allowed energy range
-  Emin_min=2.;
-  Emin_max=1000.;
-  Emax_max=10000.;   
+  pmin_min = 3.;
+  pmin_max = 100.;
+  pmax = 3000.;
   //allowed angular range
-  cmin_min=0.0348994;
-  cmin_max=0.9999;
-  cmax = 1.;
+  cmax_max = -0.1,
+  cmax_min = -0.9999;
 
-  // set Emin  
+ if(TIFOnly_const == true || TIFOnly_lin == true) pmin_min = 0.; //forTIF
 
-  if(TIFOnly_constant == true || TIFOnly_linear == true) Emin_min = 0.; //forTIF
-
-  if(Emin_in < Emin_min || Emin_in > Emin_max)
-  { 
-    std::cout << " >>> CMSCGEN.initialize <<< warning: illegal Emin_in =" << Emin_in;
+  // set pmin  
+  if(pmin_in < pmin_min || pmin_in > pmin_max){ 
+    std::cout << " >>> CMSCGEN.initialize <<< warning: illegal pmin_in =" << pmin_in;
     return(-1);
-  }
-  else if(Emax_in > Emax_max )
-  {
-   std::cout << " >>> CMSCGEN.initialize <<< warning: illegal Emax_in =" << Emax_in;
+  } else if(pmax_in > pmax ){
+    std::cout << " >>> CMSCGEN.initialize <<< warning: illegal pmax_in =" << pmax_in;
     return(-1);
-  }else{   
-    Emin = Emin_in;
-    Emax = Emax_in;
-    xemax = 1./(Emin*Emin);
-    xemin = 1./(Emax*Emax); 
+  }else{     
+    pmin = pmin_in;
+    pmax = pmax_in;
+    xemax = 1./(pmin*pmin);
+    xemin = 1./(pmax*pmax); 
   }
 
-  //set cmin and cmax
- if( TMath::Cos(thetamin_in) < cmax )
-  { 
-    std::cout << " >>> CMSCGEN.initialize <<< warning: illegal thetamin_in =" << thetamin_in;
-    return(-1);
-  }
-  else if(TMath::Cos(thetamax_in) < cmin_min || TMath::Cos(thetamax_in) > cmin_max)
-  {
-    std::cout << " >>> CMSCGEN.initialize <<< warning: illegal thetamax_in =" << thetamax_in;
-    return(-1);
-  }else{   
-    cmin = TMath::Cos(thetamax_in);//input angle already converted from Deg to Rad!
-    cmax = TMath::Cos(thetamin_in);//input angle already converted from Deg to Rad!
-  }
+
+  // set cmax and cmin
+  if(cmax_in < cmax_min || cmax_in > cmax_max)
+    { 
+      std::cout << " >>> CMSCGEN.initialize <<< warning: illegal cmax_in =" << cmax_in;
+      return(-1);
+    }
+  else 
+    {
+      cmax = cmax_in;
+      cmin = cmin_in;
+    }
+
 
   initialization = 1;
 
-  if(TIFOnly_constant == true || TIFOnly_linear == true) Emin_min = 2.; //forTIF
-  elmin = log10(Emin_min);
-  elmax = log10(Emax_max);
-  elfac = 100./(elmax-elmin);
+  if(TIFOnly_const == true || TIFOnly_lin == true) pmin_min = 3.; //forTIF
 
-//
-// +++ calculate coefficients for energy spectrum
-//
+  //  Lmin = log10(pmin_min);
+  Lmin = log10(pmin);
+  Lmax = log10(pmax);
+  Lfac = 100./(Lmax-Lmin);
 
-  pe[0] = -1.472;
-  pe[1] = 14.42;
-  pe[2] = -47.92;
-  pe[3] = 80.94;
-  pe[4] = -62.84;
-  pe[5] = 25.95;
-  pe[6] = -6.014;
-  pe[7] = 0.7456;
-  pe[8] = -0.0387;
+  //
+  // +++ coefficients for energy spectrum
+  //
 
-//  normalisation !  Note: 2 = log10(100)   
+  pe[0] = -1.;
+  pe[1] = 6.22176;
+  pe[2] = -13.9404;
+  pe[3] = 18.1643;
+  pe[4] = -9.22784;
+  pe[5] = 1.99234;
+  pe[6] = -0.156434;
+  pe[7] = 0.;
+  pe[8] = 0.;
 
-  float ce;
+  //
+  // +++ coefficients for cos theta distribution
+  //
 
+  b0c[0] = 0.6639;
+  b0c[1] = -0.9587;
+  b0c[2] = 0.2772;
+  
+  b1c[0] = 5.820;
+  b1c[1] = -6.864;
+  b1c[2] = 1.367;
+  
+  b2c[0] = 10.39;
+  b2c[1] = -8.593;
+  b2c[2] = 1.547;
+  
+  //
+  // +++ calculate correction table for different cos theta dependence!
+  //     reference range: c1  to  c2 
+  //
+  //  explanation: the parametrization of the energy spectrum as used above
+  //    is the integral over the c = cos(zenith angle) range -1...-0.1 
+  //    since the c distribution depends on energy, the integrated energy
+  //    spectrum depends on this range. Here a correction factor is determined,
+  //    based on the linear c dependence of the c distribution.
+  //    The correction is calculated for 100 bins in L = log10(energy).
+  //
+  // +++ in same loop calculate integrated flux 
+  //      (integrated over angles and momentum)
+
+  c1 = -1.;
+  c2 = -0.1;
+  
+  double cemax0 = 1.0;
+  double L, L2; 
+  double s;
+  double p, p1, p2;
+  double integral_here, integral_ref;
+
+  integrated_flux = 0.;
+  
+  for(int k=1; k<=100; k++)
+    {
+      L = Lmin + (k-0.5)/Lfac;
+      L2 = L*L;   
+      p = pow(10,L);
+      p1 = pow(10,L-0.5/Lfac);
+      p2 = pow(10,L+0.5/Lfac);
+      
+      b0 = b0c[0] + b0c[1] * L + b0c[2]* L2;
+      b1 = b1c[0] + b1c[1] * L + b1c[2]* L2;
+      b2 = b2c[0] + b2c[1] * L + b2c[2]* L2;
+      
+      integral_ref = b0 * (c2 - c1) 
+	+ b1/2. * (c2*c2 - c1*c1) 
+	+ b2/3. * (c2*c2*c2 - c1*c1*c1);
+      integral_here = b0 * (cmax - cmin) 
+	+ b1/2. * (cmax*cmax - cmin*cmin) 
+	+ b2/3. * (cmax*cmax*cmax - cmin*cmin*cmin);
+      
+      corr[k] = integral_here/integral_ref;
+      
+      s = (((((((pe[8]*L
+		 +pe[7])*L
+		+pe[6])*L
+	       +pe[5])*L
+	      +pe[4])*L
+	     +pe[3])*L
+	    +pe[2])*L
+	   +pe[1])*L
+	+pe[0];
+    
+      integrated_flux += 1./pow(p,3) * s * corr[k] * (p2-p1);
+
+      /*
+	std::cout << k << " " 
+	<< corr[k] << " " 
+	<< p << " " 
+	<< s << " " 
+	<< p1 << " " 
+	<< p2 << " " 
+	<< integrated_flux << " " 
+	<< std::endl;
+      */
+      
+      // std::cout << k << " " << corr[k] << " " << std::endl;
+    }
+
+  integrated_flux *= 1.27E3;
+  std::cout << " >>> CMSCGEN.initialize <<< " <<
+    " Integrated flux = " << integrated_flux << " /m**2/s " << std::endl;
+  
+  //  find approximate peak value, for Monte Carlo sampling
+  //      peak is near L = 2 
+
+  double ce;
+  
   ce = (((((((pe[8]*2.
-            +pe[7])*2.
-            +pe[6])*2.
+	      +pe[7])*2.
+	     +pe[6])*2.
             +pe[5])*2.
-            +pe[4])*2.
-            +pe[3])*2.
-            +pe[2])*2.
-            +pe[1])*2.
-            +pe[0];
+	   +pe[4])*2.
+	  +pe[3])*2.
+	 +pe[2])*2.
+	+pe[1])*2.
+    +pe[0];
 
-  ce = 0.28/ce;
+  // normalize to 0.5 (not 1) to have some margin if peak is not at L=2
+  //
+  ce = 0.5/ce;
 
   for (int k=0; k<9; k++)
-  {
-    pe[k] = pe[k]*ce;
-  }
+    {
+      pe[k] = pe[k]*ce;
+    }
 
-  pc[0] = -1.903;
-  pc[1] = 0.1434;
-  pc[2] = 0.01450;
-
-//
-// +++ calculate correction table for different cos theta dependence!
-//     reference range: c1  to  c2 
-//
-//  explanation: the parametrization of the energy spectrum as used above
-//    is the integral over the c = cos(zenith angle) range 0.4-1
-//    since the c distribution depends on energy, the integrated energy
-//    spectrum depends on this range. Here a correction factor is determined,
-//    based on the linear c dependence of the c distribution.
-//    The correction is calculated for 100 bins in log10(energy).
-//
-
-  c1 = 0.4;
-  c2 = 1.;
-
-  float cemax0 = 0.3;
-  float e10, ee, cax, ca, here, ref;
-
-  for(int k=1; k<=100; k++)
-  {
-    e10 = elmin + (elmax-elmin)/100.*(k-0.5);
-    ee = e10*log(10.);
-    cax = (pc[2]*ee+pc[1])*ee+pc[0];
-    ca = -cax/(1.+cax);
-
-    // Integration for a specific energy should be done from theta_min to 
-    // angle where flux becomes zero (= cmin_allowed).
-    // Decide here if this angle is lower than theta_max defined earlier.
-    // Large energies lead to unphysical cmin_allowed (if cax>0), choose cmin in this case
-    // as high energies can have very large angles, so flux never zero.
-    cmin_allowed = 0.;
-    if(cax < 0.) cmin_allowed = 1 + 1./cax;
-    cmin_allowed = TMath::Max(cmin, cmin_allowed);
-
-    // TH  version 1.1   following two lines were wrong in version 1.0
-    //   ( instead of   +0.5*ca  there was written   *0.5*ca  )
-    ref = (c2-c1)+0.5*ca*(c2*c2-c1*c1);
-    here = (cmax-cmin_allowed)+0.5*ca*(cmax*cmax-cmin_allowed*cmin_allowed);
-    // 
-    corr[k] = here/ref;
-  }
-   
   cemax = cemax0*corr[50];      
 
   return initialization;
@@ -169,214 +226,227 @@ int CMSCGEN::generate()
       return -1;
     }
 
-// note: use historical notation (fortran version l3cgen.f)
-  
-//
-// +++ determine x = 1/e**2
-//
-//  explanation: the energy distribution 
-//        dn/d(1/e**2) = dn/de * e**3 = dn/dlog10(e) * e**2
-//     is parametrized by a polynomial. accordingly xe = 1/e**2 is sampled
-//     and e calculated 
-//
-//     need precise random variable with high precison since for 
-//     emin = 2 GeV energies around 10000 GeV are very rare!
-//     [roughly (2/10000)**3 = 8E-12]
-//
-  
-  bool accept = 0;
-  float r1, r2, r3, r4;
-  float xe, e, ce, e10;
+  // note: use historical notation (fortran version l3cgen.f)
+
+  //
+  // +++ determine x = 1/e**2
+  //
+  //  explanation: the energy distribution 
+  //        dn/d(1/e**2) = dn/de * e**3 = dn/dlog10(e) * e**2
+  //     is parametrized by a polynomial. accordingly xe = 1/e**2 is sampled
+  //     and e calculated 
+  //
+  //     need precise random variable with high precison since for 
+  //     emin = 3 GeV energies around 3000 GeV are very rare!
+  //
+
+  double r1, r2, r3;
+  double xe, e, ce, L, L2;
   int k;    
   double prob; 
-
-  while (accept==0)
+  
+  double c_max;
+  double z, z_max;
+  
+  while (1)
     {
-      
-      // P. Biallass, 2006:
       prob = RanGen2.Rndm();
-      r1 = float(prob);
+      r1 = double(prob);
       prob = RanGen2.Rndm();
-      r2 = float(prob);
+      r2 = double(prob);
       
       xe = xemin+r1*(xemax-xemin);
-      if( (1./sqrt(xe)<2) && TIFOnly_const == true) { //generate constant energy dependence for E<2GeV, only used for TIF
+      
+      if( (1./sqrt(xe)<3) && TIFOnly_const == true) { //generate constant energy dependence for E<2GeV, only used for TIF
 	//compute constant to match to CMSCGEN spectrum
-	e=2.;      
-	e10 = log10(e);
+	e=3.;      
+	L = log10(e);
+	L2 = L*L;
 	
-	ce = (((((((pe[8]*e10
-		    +pe[7])*e10
-		   +pe[6])*e10
-		  +pe[5])*e10
-		 +pe[4])*e10
-		+pe[3])*e10
-	       +pe[2])*e10
-	      +pe[1])*e10
+	ce = (((((((pe[8]*L
+		    +pe[7])*L
+		   +pe[6])*L
+		  +pe[5])*L
+		 +pe[4])*L
+		+pe[3])*L
+	       +pe[2])*L
+	      +pe[1])*L
 	  +pe[0];
 	
-	k = int ((e10-elmin)*elfac+1.);
+	k = int ((L-Lmin)*Lfac+1.);
 	k = TMath::Max(1,TMath::Min(k,100));
 	ce = ce * corr[k];
 	
 	e = 1./sqrt(xe);  
-	if(r2 < ( e*e*e*ce/(cemax*2.*2.*2.) ))
-	  {
-	    accept = 1;
-	  }
-      }else if( (1./sqrt(xe)<2) && TIFOnly_lin == true) { //generate linear energy dependence for E<2GeV, only used for TIF
+	if(r2 < ( e*e*e*ce/(cemax*3.*3.*3.) )) break;
+      
+      }else if( (1./sqrt(xe)<3) && TIFOnly_lin == true) { //generate linear energy dependence for E<2GeV, only used for TIF
 	//compute constant to match to CMSCGEN spectrum
-	e=2.;      
-	e10 = log10(e);
+	e=3.;      
+	L = log10(e);
+	L2 = L*L;
 	
-	ce = (((((((pe[8]*e10
-		    +pe[7])*e10
-		   +pe[6])*e10
-		  +pe[5])*e10
-		 +pe[4])*e10
-		+pe[3])*e10
-	       +pe[2])*e10
-	      +pe[1])*e10
+	ce = (((((((pe[8]*L
+		    +pe[7])*L
+		   +pe[6])*L
+		  +pe[5])*L
+		 +pe[4])*L
+		+pe[3])*L
+	       +pe[2])*L
+	      +pe[1])*L
 	  +pe[0];
       
-	k = int ((e10-elmin)*elfac+1.);
+	k = int ((L-Lmin)*Lfac+1.);
 	k = TMath::Max(1,TMath::Min(k,100));
 	ce = ce * corr[k];
       
 	e = 1./sqrt(xe);  
-	if(r2 < ( e*e*e*e*ce/(cemax*2.*2.*2.*2.) ))
-	  {
-	    accept = 1;
-	  }
+	if(r2 < ( e*e*e*e*ce/(cemax*3.*3.*3.*3.) )) break;
+      
       }else{ //this is real CMSCGEN energy-dependence
+
 	e = 1./sqrt(xe);       
-	e10 = log10(e);
+	L = log10(e);
+	L2 = L*L;
 	
-	ce = (((((((pe[8]*e10
-		    +pe[7])*e10
-		   +pe[6])*e10
-		  +pe[5])*e10
-		 +pe[4])*e10
-		+pe[3])*e10
-	       +pe[2])*e10
-	      +pe[1])*e10
+	ce = (((((((pe[8]*L
+		    +pe[7])*L
+		   +pe[6])*L
+		  +pe[5])*L
+		 +pe[4])*L
+		+pe[3])*L
+	       +pe[2])*L
+	      +pe[1])*L
 	  +pe[0];
 	
-	k = int ((e10-elmin)*elfac+1.);
+	k = int ((L-Lmin)*Lfac+1.);
 	k = TMath::Max(1,TMath::Min(k,100));
 	ce = ce * corr[k];
-      
-	if(cemax*r2 < ce)
-	  {
-	    accept = 1;
-	  }
+	
+	if(cemax*r2 < ce) break;
+
       } //end of CMSCGEN energy-dependence
     } //end of while
-     
-  pq = e;
-
-//
-// +++ charge ratio 1.3
-//
-  prob = RanGen2.Rndm();
-  r3 = float(prob);
-
-  float charg = 1.;
-  if(r3 < 0.43) charg=-1.;
-       
-  pq = pq*charg;
-
-//
-//  +++ determine cos(angle)
-//
-//     here we can calculate c analytically, since cos(angle) = c
-//     distribution is linear (to a good approximation)
-//      ca is linear coefficient in c distribution:
-//     dN/dc = 1 + ca * c
-//      integration:  int ~ (c-cmin) + 0.5*ca*(c**2-cmin**2)
-//     normalization:  factor 1/[(cmax-cmin)+0.5*ca*(cmax**2-cmin**2)]
-//     inversion: int = r   yields  solution of equation c**2 + pc + q = 0:
-//
   
-  float eln, cax, fac, p, q, ca; 
-
-  if(TIFOnly_const == true && e<2.) e = 2.; //forTIF (when E<2GeV use angles of 2GeV cosmic)
-  if(TIFOnly_lin == true && e<2.) e = 2.; //forTIF (when E<2GeV use angles of 2GeV cosmic)
-
-  eln = log(e);
-  cax = (pc[2]*eln+pc[1])*eln+pc[0];
-
-  if (fabs(1.+cax) < 0.001) 
-  {
-    ca = -1000.;
-  } 
-  else 
-  {       
-    ca = -cax/(1.+cax);
-  }
-
-// Volker Schmitt:
-  if(fabs(ca) < 0.001) 
-  {
-    if (ca < 0) ca=0.001;
-    if (ca >=0) ca=-0.001;
-// TH march 1999:
-    cax = -ca;
-  }
-
-  // Integration for a specific energy should be done from theta_min to 
-  // angle where flux becomes zero (= cmin_allowed).
-  // Decide here if this angle is lower than theta_max defined earlier.
-  // Large energies lead to unphysical cmin_allowed (if cax>0), choose cmin in this case
-  // as high energies can have very large angles, so flux never zero.
-  cmin_allowed = 0.;
-  if(cax < 0.) cmin_allowed = 1 + 1./cax;
-  cmin_allowed = TMath::Max(cmin, cmin_allowed);
-
-  //finally dice angle    
+  pq = e;
+  
+  //
+  // +++ charge ratio 1.280
+  //
   prob = RanGen2.Rndm();
-  r4 = float(prob);
-  fac = (cmax-cmin_allowed)+0.5*ca*(cmax*cmax-cmin_allowed*cmin_allowed);
-  p = 2./ca;
-  q = -p*cmin_allowed - cmin_allowed*cmin_allowed - p*fac*r4;
-  if(cax >= 0.) c = -0.5*p - sqrt(0.25*p*p-q);
-  if(cax < 0.) c = -0.5*p + sqrt(0.25*p*p-q);
+  r3 = double(prob);
+  
+  double charg = 1.;
+  if(r3 < 0.439) charg=-1.;
+  
+  pq = pq*charg;
+  
+  //
+  //  +++ determine cos(angle)
+  //
+  //  simple trial and rejection method
+  //
+  
+  // first calculate energy dependent coefficients b_i
 
-  if(c>cmax || c<cmin) std::cout << " >>> CMSCGEN <<< warning: generated cosmic with cos(theta) = " << c << " --> out of range "<< std::endl;
-
+  if(TIFOnly_const == true && e<3.){ //forTIF (when E<2GeV use angles of 2GeV cosmic)
+    L = log10(3.);
+    L2 = L*L;
+  }
+  if(TIFOnly_lin == true && e<3.){ //forTIF (when E<2GeV use angles of 2GeV cosmic)
+    L = log10(3.);
+    L2 = L*L; 
+  }
+  
+  b0 = b0c[0] + b0c[1] * L + b0c[2]* L2;
+  b1 = b1c[0] + b1c[1] * L + b1c[2]* L2;
+  b2 = b2c[0] + b2c[1] * L + b2c[2]* L2;
+  
+  //
+  // need to know the maximum of z(c)
+  //
+  // first calculate c for which c distribution z(c) = maximum 
+  // 
+  // (note: maximum of curve is NOT always at c = -1, but never at c = -0.1)   
+  //
+  
+  // try extremal value (from z'(c) = 0), but only if z''(c) < 0  
+  //
+  // z'(c) = b1 + b2 * c   =>  at c_max = - b1 / (2 b_2) is z'(c) = 0
+  //
+  // z''(c) = b2 
+  
+  c_max = -1.;
+  
+  if(b2<0.) {
+    c_max = - 0.5 * b1/b2;
+    if(c_max < -1.) c_max = -1.;
+    if(c_max > -0.1) c_max = -0.1;
+  } 
+  
+  z_max = b0 + b1 * c_max + b2 * c_max * c_max;
+  
+  // now we throw dice:
+  
+  while (1)
+    {
+      prob = RanGen2.Rndm();
+      r1 = double(prob);
+      prob = RanGen2.Rndm();
+      r2 = double(prob);
+      c = cmin + (cmax-cmin)*r1;   
+      z = b0 + b1 * c + b2 * c*c;
+      if (z > z_max*r2) break;
+    }    
+  
   return 0;
-
+  
 }
 
 
-float CMSCGEN::energy_times_charge()
+double CMSCGEN::momentum_times_charge()
 {
-
+  
   if(initialization==1)
-  { 
-    return pq;
-  }
+    { 
+      return pq;
+    }
   else
-  {
-    std::cout << " >>> CMSCGEN <<< warning: not initialized" << std::endl;
-    return -9999.;
-  }
+    {
+      std::cout << " >>> CMSCGEN <<< warning: not initialized" << std::endl;
+      return -9999.;
+    }
 
 }
 
-float CMSCGEN::cos_theta()
+double CMSCGEN::cos_theta()
 {
-
+  
   if(initialization==1)
-  { 
-    return c;
-  }
+    { 
+      // here convert between coordinate systems:
+      return -c;
+    }
   else
-  {
-    std::cout << " >>> CMSCGEN <<< warning: not initialized" << std::endl;
-    return -0.9999;
-  }
+    {
+      std::cout << " >>> CMSCGEN <<< warning: not initialized" << std::endl;
+      return -0.9999;
+    }
 
 }
 
+double CMSCGEN::flux()
+{
+  
+  if(initialization==1)
+    { 
+      return integrated_flux;
+    }
+  else
+    {
+      std::cout << " >>> CMSCGEN <<< warning: not initialized" << std::endl;
+      return -0.9999;
+    }
+  
+}
 
