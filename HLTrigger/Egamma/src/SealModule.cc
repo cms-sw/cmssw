fda4d2b5f5aebb3c41732f85dd73e0e9ef8ddc8f
Marco Pieri <sha1-0c70a541ce30dda16c91989063058bef3a5a7ba0@cern.ch>
2007-05-31 19:35:22 +0000

    Introduced regional unpacking and two more HLT paths for High Energy EM objects + changes in L1 bits used

--- HLTrigger/Egamma/src/SealModule.cc
+++ HLTrigger/Egamma/src/SealModule.cc
@@ -7,12 +7,15 @@
 #include "HLTrigger/Egamma/interface/HLTEgammaDoubleEtFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaEcalIsolFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaHcalIsolFilter.h"
+#include "HLTrigger/Egamma/interface/HLTEgammaHcalDBCFilter.h"
+#include "HLTrigger/Egamma/interface/HLTEgammaHOEFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPhotonTrackIsolFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronTrackIsolFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronPixelMatchFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronEoverpFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPMMassFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPMDocaFilter.h"
+
 
 #include "HLTrigger/Egamma/interface/HLTEgammaL1MatchFilterRegional.h"
 #include "HLTrigger/Egamma/interface/HLTElectronEoverpFilterRegional.h"
@@ -25,6 +28,8 @@ DEFINE_ANOTHER_FWK_MODULE(HLTEgammaEtFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaDoubleEtFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaEcalIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHcalIsolFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHcalDBCFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHOEFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTPhotonTrackIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronTrackIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronPixelMatchFilter);
