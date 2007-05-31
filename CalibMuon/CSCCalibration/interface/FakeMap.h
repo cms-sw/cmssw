281745b2cd65bd792c83a236bba4636fdbdad4f4
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-05-31 13:30:32 +0000

    added

---
+++ CalibMuon/CSCCalibration/interface/FakeMap.h
@@ -0,0 +1,22 @@
+#ifndef _FAKE_MAP_H
+#define _FAKE_MAP_H
+
+#include <iostream>
+#include "DataFormats/DetId/interface/DetId.h"
+#include <map>
+
+class FakeMap{
+
+public:
+FakeMap(){}
+virtual ~FakeMap(){}
+
+public:
+
+
+virtual void csc(const DetId &cell, float scaling_factor)=0;
+
+};
+
+
+#endif
