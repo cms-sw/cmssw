405550fbc0df75f11c4208f58a66cf3b8563675c
heyninck <sha1-082da020e7327f374b7da66709d8d266b70884a2@cern.ch>
2007-06-01 09:11:46 +0000

    correlation plots added + some extra members

--- TopQuarkAnalysis/TopTools/interface/LRHelpFunctions.h
+++ TopQuarkAnalysis/TopTools/interface/LRHelpFunctions.h
@@ -3,7 +3,7 @@
 // Author:  Jan Heyninck
 // Created: Tue Apr  3 17:33:23 PDT 2007
 //
-// $Id: LRHelpFunctions.h,v 1.2 2007/05/22 16:43:36 heyninck Exp $
+// $Id: LRHelpFunctions.h,v 1.1 2007/05/24 13:19:46 heyninck Exp $
 //
 
 #ifndef LRHelpFunctions_h
@@ -14,7 +14,7 @@
   \brief    Help functionalities to implement and evaluate LR ratio method
 
   \author   Jan Heyninck
-  \version  $Id: LRHelpFunctions.h,v 1.2 2007/05/22 16:43:36 heyninck Exp $
+  \version  $Id: LRHelpFunctions.h,v 1.1 2007/05/24 13:19:46 heyninck Exp $
 */
 
 #include "TString.h"
@@ -25,6 +25,8 @@
 #include "TF1.h"
 #include "TGraph.h"
 #include "TList.h"
+#include "TPaveText.h"
+#include "TText.h"
 #include "TCanvas.h"
 #include <iostream>
 using namespace std;
@@ -40,18 +42,15 @@ class LRHelpFunctions {
     void 	fillToSignalHists(vector<double>);
     void 	fillToBackgroundHists(vector<double>);
     void        makeAndFitSoverSplusBHists();
-    void        readObsHistsAndFits(TString);
+    void        readObsHistsAndFits(TString,bool);
     void        storeToROOTfile(TString);
     void        storeControlPlots(TString); 
     void        fillLRSignalHist(double);
     void        fillLRBackgroundHist(double);
     void        makeAndFitPurityHists(); 
     double 	calcLRval(vector<double>);
-/*    vector<TF1> getObsFits(TString fileName);
-    TF1 	getPurVsLRtotFit(TString fileName);
-    TH1F makePurityPlot(TH1F *hLRtotS, TH1F *hLRtotB);
-    TGraph makeEffVsPurGraph(TH1F *hLRtotS, TF1 *fPurity);
-*/
+    bool 	isIncluded(int);
+    double 	calcProb(double);
    
   private:
     vector<TH1F*> hObsS, hObsB, hObsSoverSplusB;
@@ -59,6 +58,7 @@ class LRHelpFunctions {
     vector<TF1*>  fObsSoverSplusB;
     TH1F 	  *hLRtotS, *hLRtotB, *hLRtotSoverSplusB;
     TF1		  *fLRtotSoverSplusB;
+    vector<int>   selObs;
 };
 
 #endif
--- TopQuarkAnalysis/TopTools/src/LRHelpFunctions.cc
+++ TopQuarkAnalysis/TopTools/src/LRHelpFunctions.cc
@@ -2,14 +2,17 @@
 // Author:  Jan Heyninck
 // Created: Tue Apr  3 17:33:23 PDT 2007
 //
-// $Id: LRHelpFunctions.cc,v 1.1 2007/05/08 14:03:05 heyninck Exp $
+// $Id: LRHelpFunctions.cc,v 1.1 2007/05/24 13:19:47 heyninck Exp $
 //
 #include "TopQuarkAnalysis/TopTools/interface/LRHelpFunctions.h"
+#include "TopQuarkAnalysis/TopEventProducers/bin/tdrstyle.C"
 
 // constructors
 LRHelpFunctions::LRHelpFunctions() {}
 LRHelpFunctions::LRHelpFunctions(vector<int> obsNr, int nrBins, vector<double> obsMin, vector<double> obsMax,
-                                 vector<const char*> functions, int nrLRbins, double LRmin, double LRmax, const char* LRfunction) {
+                                 vector<const char*> functions, int nrLRbins, double LRmin, double LRmax, const char* LRfunction) { 
+  setTDRStyle();
+  gStyle->SetCanvasDefW(900);
   for(size_t o=0; o<obsNr.size(); o++){
     // create Signal, background and s/(s+b) histogram
     TString htS  = "Obs"; htS  += obsNr[o]; htS += "_S"; 
@@ -20,7 +23,7 @@ LRHelpFunctions::LRHelpFunctions(vector<int> obsNr, int nrBins, vector<double> o
     hObsSoverSplusB.push_back( new TH1F(htSB,htSB,nrBins,obsMin[o],obsMax[o]) );
     
     // create the correlation 2D plots among the observables (for signal)
-    for (size_t o2 = o; o2 < obsNr.size(); o2++) {
+    for (size_t o2 = o+1; o2 < obsNr.size(); o2++) {
       TString hcorr  = "Corr_Obs"; hcorr  += obsNr[o]; hcorr += "_Obs";  hcorr  += obsNr[o2]; 
       hObsCorr.push_back( new TH2F(hcorr,hcorr,nrBins,obsMin[o],obsMax[o],nrBins,obsMin[o2],obsMax[o2]) );
     }    
@@ -47,7 +50,14 @@ LRHelpFunctions::~LRHelpFunctions() {}
 
 // member function to add observable values to the signal histograms
 void LRHelpFunctions::fillToSignalHists(vector<double> obsVals){
-  for(size_t o=0; o<obsVals.size(); o++) hObsS[o]->Fill(obsVals[o]);
+  int hIndex = 0;
+  for(size_t o=0; o<obsVals.size(); o++) {
+    hObsS[o]->Fill(obsVals[o]);
+    for(size_t o2=o+1; o2<obsVals.size(); o2++) {
+      hObsCorr[hIndex] -> Fill(obsVals[o],obsVals[o2]);
+      ++hIndex;
+    }
+  }
 }
   
   
@@ -74,7 +84,7 @@ void LRHelpFunctions::makeAndFitSoverSplusBHists(){
 
 
 // member function to read the observable hists & fits from a root-file
-void LRHelpFunctions::readObsHistsAndFits(TString fileName){
+void LRHelpFunctions::readObsHistsAndFits(TString fileName, bool readLRplots){
   hObsS.clear();
   hObsB.clear();
   hObsSoverSplusB.clear();
@@ -90,6 +100,9 @@ void LRHelpFunctions::readObsHistsAndFits(TString fileName){
       hObsSoverSplusB.push_back( new TH1F(tmp));
       TString ft = "F_"; ft += keyName;
       fObsSoverSplusB.push_back( new TF1(*(((TH1F*) el -> ReadObj()) -> GetFunction(ft))) );
+      keyName.Remove(keyName.Index("_"),keyName.Length());
+      keyName.Remove(0,3);
+      selObs.push_back(keyName.Atoi());
     }
     else if(keyName.Contains("S") && keyName.Contains("Obs")){
       TH1F tmp =  *((TH1F*) el -> ReadObj());
@@ -100,7 +113,15 @@ void LRHelpFunctions::readObsHistsAndFits(TString fileName){
       hObsB.push_back( new TH1F(tmp));
     }
   }
+  
+  if(readLRplots){
+    hLRtotS = new TH1F(*((TH1F*)fitFile->GetKey("hLRtotS")->ReadObj()));
+    hLRtotB = new TH1F(*((TH1F*)fitFile->GetKey("hLRtotB")->ReadObj()));
+    hLRtotSoverSplusB = new TH1F(*((TH1F*)fitFile->GetKey("hLRtotSoverSplusB")->ReadObj()));
+    fLRtotSoverSplusB = new TF1(*((TF1*)(((TH1F*)fitFile->GetKey("hLRtotSoverSplusB")->ReadObj())->GetFunction("fLRtotSoverSplusB"))));
+  }
 }
+
 
 
 
@@ -112,6 +133,13 @@ void  LRHelpFunctions::storeToROOTfile(TString fname){
     hObsS[o] 		-> Write();
     hObsB[o] 		-> Write();
     hObsSoverSplusB[o] 	-> Write();
+  }
+  for(size_t o=0; o<hObsS.size(); o++) {
+    int hIndex = 0;
+    for(size_t o2=o+1; o2<hObsS.size(); o2++) {
+      hObsCorr[hIndex] -> Write();
+      ++ hIndex;
+    }
   }
   hLRtotS 		-> Write();
   hLRtotB 		-> Write();
@@ -126,7 +154,6 @@ void  LRHelpFunctions::storeToROOTfile(TString fname){
 void  LRHelpFunctions::storeControlPlots(TString fname){  
   TCanvas c("dummy","",1);
   c.Print(fname + "[","landscape");
-  //TCanvas *cObs[10][2], *cLR, *cPur;
   for(size_t o=0; o<hObsS.size(); o++) {
      TCanvas c2("c2","",1);
      c2.Divide(2,1);
@@ -139,6 +166,24 @@ void  LRHelpFunctions::storeControlPlots(TString fname){
      fObsSoverSplusB[o] -> Draw("same");
      c2.Print(fname,"Landscape");
   }
+  
+  for(size_t o=0; o<hObsS.size(); o++) {
+    int hIndex = 0;
+    for(size_t o2=o+1; o2<hObsS.size(); o2++) {
+      TCanvas cc("cc","",1);
+      hObsCorr[hIndex] -> Draw("box");
+      TPaveText pt(0.5,0.87,0.98,0.93,"blNDC");
+      pt.SetFillStyle(1);
+      pt.SetFillColor(0);
+      pt.SetBorderSize(0);
+      TString tcorr = "Corr. of "; tcorr += (int)(100.*hObsCorr[hIndex] -> GetCorrelationFactor()); tcorr += " %";
+      TText *text = pt.AddText(tcorr);
+      pt.Draw("same");
+      ++hIndex;
+      cc.Print(fname,"Landscape");
+    }
+  }
+  
   TCanvas c3("c3","",1);
   c3.Divide(2,1);
   c3.cd(1);
@@ -192,4 +237,22 @@ void  LRHelpFunctions::makeAndFitPurityHists(){
       hLRtotSoverSplusB->SetBinError(b,error);
     }
   }
+  hLRtotSoverSplusB -> Fit(fLRtotSoverSplusB->GetName(),"RQ");
 }   
+
+
+
+// member function to calculate the probability for signal given a logLR value
+double  LRHelpFunctions::calcProb(double logLR){
+  return fLRtotSoverSplusB -> Eval(logLR);
+}
+
+
+// member function to check if a certain observable was used
+bool  LRHelpFunctions::isIncluded(int obs){
+  bool found = false;
+  for(size_t o=0; o<selObs.size(); o++){
+    if(selObs[o] == obs) found = true;
+  }
+  return found;
+}
