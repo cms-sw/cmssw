3d2778e0c03cfe0b1442d77e28719f8b4edc7d05
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-06-01 18:34:02 +0000

    adding BeamHaloNavigationSchool

---
+++ RecoTracker/TkNavigation/interface/BeamHaloNavigationSchool.h
@@ -0,0 +1,25 @@
+#ifndef TkNavigation_BeamHaloNavigationSchool_H
+#define TkNavigation_BeamHaloNavigationSchool_H
+
+#include "RecoTracker/TkNavigation/interface/SimpleNavigationSchool.h"
+#include "RecoTracker/TkDetLayers/interface/GeometricSearchTracker.h"
+
+#include <vector>
+
+/** Concrete navigation school for the Tracker, connecting disks only for traversing tracks : moslty beam halo muon 
+ */
+
+class BeamHaloNavigationSchool : public SimpleNavigationSchool {
+public:
+  
+  BeamHaloNavigationSchool(const GeometricSearchTracker* theTracker,
+			 const MagneticField* field);
+
+  //addon to SimpleNavigationSchool
+  void linkOtherEndLayers( SymmetricLayerFinder& symFinder);
+  void addInward(DetLayer * det, FDLC news);
+  void addInward(DetLayer * det, ForwardDetLayer * newF);
+  FDLC reachableFromHorizontal();
+};
+
+#endif // BeamHaloNavigationSchool_H
--- RecoTracker/TkNavigation/plugins/modules.cc
+++ RecoTracker/TkNavigation/plugins/modules.cc
@@ -6,6 +6,8 @@ DEFINE_SEAL_MODULE();
 #include "TrackingTools/DetLayers/interface/NavigationSchool.h"
 #include "RecoTracker/TkNavigation/interface/SimpleNavigationSchool.h"
 #include "RecoTracker/TkNavigation/interface/CosmicNavigationSchool.h"
+#include "RecoTracker/TkNavigation/interface/BeamHaloNavigationSchool.h"
  
 DEFINE_EDM_PLUGIN(NavigationSchoolFactory, SimpleNavigationSchool, "SimpleNavigationSchool");
 DEFINE_EDM_PLUGIN(NavigationSchoolFactory, CosmicNavigationSchool, "CosmicNavigationSchool");
+DEFINE_EDM_PLUGIN(NavigationSchoolFactory, BeamHaloNavigationSchool, "BeamHaloNavigationSchool");
