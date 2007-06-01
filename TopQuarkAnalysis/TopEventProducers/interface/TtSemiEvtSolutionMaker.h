97bedcc62b0d7e7e926d12a1f1089aa80409eb79
heyninck <sha1-082da020e7327f374b7da66709d8d266b70884a2@cern.ch>
2007-06-01 09:08:15 +0000

    add event selection and JetComLR calculation possibility

--- TopQuarkAnalysis/TopEventProducers/interface/TtSemiEvtSolutionMaker.h
+++ TopQuarkAnalysis/TopEventProducers/interface/TtSemiEvtSolutionMaker.h
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jan Heyninck
 //         Created:  Thu May 18 18:11:01 CEST 2006
-// $Id: TtSemiEvtSolutionMaker.h,v 1.3 2007/05/22 16:43:34 heyninck Exp $
+// $Id: TtSemiEvtSolutionMaker.h,v 1.4 2007/05/23 09:04:58 heyninck Exp $
 //
 //
 
@@ -36,11 +36,16 @@
 #include "AnalysisDataFormats/TopObjects/interface/BestMatching.h"
 #include "TopQuarkAnalysis/TopJetCombination/interface/TtSemiSimpleBestJetComb.h"
 #include "TopQuarkAnalysis/TopJetCombination/interface/TtSemiLRJetCombObservables.h"
+#include "TopQuarkAnalysis/TopJetCombination/interface/TtSemiLRJetCombCalc.h"
+#include "PhysicsTools/Utilities/interface/MinSelector.h"
+#include "PhysicsTools/Utilities/interface/EtaRangeSelector.h"
+#include "PhysicsTools/Utilities/interface/EtMinSelector.h"
+#include "PhysicsTools/Utilities/interface/PtMinSelector.h"
+#include "PhysicsTools/Utilities/interface/DeltaR.h"
 #include <string>
 #include <vector>
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
 
@@ -56,17 +61,37 @@ class TtSemiEvtSolutionMaker : public edm::EDProducer {
 
       virtual void produce(edm::Event&, const edm::EventSetup&);
    private:
-      TtSemiKinFitterEtThetaPhi * myKinFitterEtThetaPhi;
-      TtSemiKinFitterEtEtaPhi   * myKinFitterEtEtaPhi;
-      TtSemiKinFitterEMom       * myKinFitterEMom;
-      //std::vector<TtJetCombinationProbability> jetCombProbs;
       string leptonFlavour_;
-      string lJetInput_,bJetInput_;
+      string lJetInput_, bJetInput_, lrJetCombFile_;
       bool addLRJetComb_, doKinFit_, matchToGenEvt_;
       int maxNrIter_;
       double maxDeltaS_, maxF_;
       int param_;
       vector<int> constraints_;
-      TtSemiSimpleBestJetComb      *mySimpleBestJetComb;
-      TtSemiLRJetCombObservables   *myLRJetCombObservables;
+      double jetEtaCut_;	
+      double recJetETCut_;	
+      double calJetETCut_;	
+      double jetLRCut_;
+      double muonEtaCut_;	
+      double muonPtCut_;	
+      double muonLRCut_;
+      double electronEtaCut_;	
+      double electronPtCut_;	
+      double electronLRCut_;	
+      double metCut_;	
+      TtSemiKinFitterEtThetaPhi     *myKinFitterEtThetaPhi;
+      TtSemiKinFitterEtEtaPhi       *myKinFitterEtEtaPhi;
+      TtSemiKinFitterEMom           *myKinFitterEMom;
+      TtSemiSimpleBestJetComb       *mySimpleBestJetComb;
+      TtSemiLRJetCombObservables    *myLRJetCombObservables;
+      TtSemiLRJetCombCalc	    *myLRJetCombCalc;
+      EtaRangeSelector<TopJet>      *jetEtaRangeSelector;
+      EtMinSelector<TopJet>    	    *recJetEtMinSelector;
+      EtMinSelector<TopJet>         *calJetEtMinSelector;
+      EtaRangeSelector<TopMuon>     *muonEtaRangeSelector;
+      PtMinSelector<TopMuon>	    *muonPtMinSelector;
+      EtaRangeSelector<TopElectron> *electronEtaRangeSelector;
+      PtMinSelector<TopElectron>    *electronPtMinSelector;
+      EtMinSelector<TopMET>         *metEtMinSelector;
+      //MinSelector		
 };
--- TopQuarkAnalysis/TopEventProducers/src/TtSemiEvtSolutionMaker.cc
+++ TopQuarkAnalysis/TopEventProducers/src/TtSemiEvtSolutionMaker.cc
@@ -1,21 +1,44 @@
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtSemiEvtSolutionMaker.h"
 
 //
-// constructors and destructor
+// constructors
 //
+
 TtSemiEvtSolutionMaker::TtSemiEvtSolutionMaker(const edm::ParameterSet& iConfig)
 {
    leptonFlavour_   = iConfig.getParameter< string > 	  ("leptonFlavour");
    lJetInput_       = iConfig.getParameter< string > 	  ("lJetInput");
    bJetInput_       = iConfig.getParameter< string > 	  ("bJetInput");
+   jetEtaCut_  	    = iConfig.getParameter< double >      ("jetEtaCut");	
+   recJetETCut_     = iConfig.getParameter< double >      ("recJetETCut");	
+   calJetETCut_     = iConfig.getParameter< double >      ("calJetETCut");	
+   jetLRCut_        = iConfig.getParameter< double >      ("jetLRCut");
+   muonEtaCut_      = iConfig.getParameter< double >      ("muonEtaCut");	
+   muonPtCut_       = iConfig.getParameter< double >      ("muonPtCut");	
+   muonLRCut_       = iConfig.getParameter< double >      ("muonLRCut");	
+   electronEtaCut_  = iConfig.getParameter< double >      ("electronEtaCut");	
+   electronPtCut_   = iConfig.getParameter< double >      ("electronPtCut");	
+   electronLRCut_   = iConfig.getParameter< double >      ("electronLRCut");	
+   metCut_          = iConfig.getParameter< double >      ("metCut");	
    doKinFit_        = iConfig.getParameter< bool >        ("doKinFit");
    addLRJetComb_    = iConfig.getParameter< bool >        ("addLRJetComb");
+   lrJetCombFile_   = iConfig.getParameter< string >      ("lrJetCombFile");
    maxNrIter_       = iConfig.getParameter< int >         ("maxNrIter");
    maxDeltaS_       = iConfig.getParameter< double >      ("maxDeltaS");
    maxF_            = iConfig.getParameter< double >      ("maxF");
    param_           = iConfig.getParameter< int >         ("parametrisation");
    constraints_     = iConfig.getParameter< vector<int> > ("constraints");
    matchToGenEvt_   = iConfig.getParameter< bool > 	  ("matchToGenEvt");
+   
+   // define kinematic selection cuts
+   jetEtaRangeSelector      = new EtaRangeSelector<TopJet>(-1.*jetEtaCut_,jetEtaCut_);
+   recJetEtMinSelector      = new EtMinSelector<TopJet>(recJetETCut_);
+   calJetEtMinSelector      = new EtMinSelector<TopJet>(calJetETCut_);
+   muonEtaRangeSelector     = new EtaRangeSelector<TopMuon>(-1.*muonEtaCut_,muonEtaCut_);
+   muonPtMinSelector        = new PtMinSelector<TopMuon>(muonPtCut_);
+   electronEtaRangeSelector = new EtaRangeSelector<TopElectron>(-1.*electronEtaCut_,electronEtaCut_);
+   electronPtMinSelector    = new PtMinSelector<TopElectron>(electronPtCut_);
+   metEtMinSelector         = new EtMinSelector<TopMET>(metCut_);
    
    // define kinfitter
    if(doKinFit_){
@@ -27,15 +50,40 @@ TtSemiEvtSolutionMaker::TtSemiEvtSolutionMaker(const edm::ParameterSet& iConfig)
    // define jet combinations related calculators
    mySimpleBestJetComb      = new TtSemiSimpleBestJetComb();
    myLRJetCombObservables   = new TtSemiLRJetCombObservables();
-   
+   if(addLRJetComb_) myLRJetCombCalc = new TtSemiLRJetCombCalc(lrJetCombFile_);
    produces<vector<TtSemiEvtSolution> >();
 }
 
 
+
+
+
+
+
+
+//
+// destructor
+//
+
 TtSemiEvtSolutionMaker::~TtSemiEvtSolutionMaker() {
+   delete recJetEtMinSelector;
+   delete calJetEtMinSelector;
+   delete jetEtaRangeSelector;
+   delete muonPtMinSelector;
+   delete muonEtaRangeSelector;
+   delete electronPtMinSelector;
+   delete electronEtaRangeSelector;
+   delete metEtMinSelector;
    delete mySimpleBestJetComb;
    delete myLRJetCombObservables;
+   if(addLRJetComb_) delete myLRJetCombCalc;
 }
+
+
+
+
+
+
 
 
 //
@@ -45,39 +93,82 @@ TtSemiEvtSolutionMaker::~TtSemiEvtSolutionMaker() {
 // ------------ method called to produce the data  ------------
 void TtSemiEvtSolutionMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   edm::Handle<vector<TopMuon> >  muons;
-   if(leptonFlavour_ == "muon") iEvent.getByType(muons);
-   edm::Handle<vector<TopElectron> >  electrons;
-   if(leptonFlavour_ == "electron") iEvent.getByType(electrons);
-   edm::Handle<vector<TopMET> >  mets;
-   iEvent.getByType(mets);
+
+  //
+  //  TopObject Selection
+  //
+
+   // select lepton (the TtLepton vectors are, for the moment, sorted on pT)
+   bool leptonFound = false;
+   TopMuon selMuon;
+   if(leptonFlavour_ == "muon"){
+     edm::Handle<vector<TopMuon> >  muons;
+     iEvent.getByType(muons);
+     vector<TopMuon> selMuons;
+     for(size_t m=0; m < muons->size(); m++) { 
+       if( (*muonEtaRangeSelector)((*muons)[m]) && (*muonPtMinSelector)((*muons)[m]) ) selMuons.push_back((*muons)[m]);
+     }
+     if( selMuons.size() > 0 ){
+       //select highest pT muon
+       selMuon = selMuons[0];
+       leptonFound = true;
+     }    
+   }  
+   TopElectron selElectron;
+   if(leptonFlavour_ == "electron"){
+     edm::Handle<vector<TopElectron> >  electrons;
+     iEvent.getByType(electrons);
+     vector<TopElectron> selElectrons;
+     for(size_t e=0; e < electrons->size(); e++) { 
+       if( (*electronEtaRangeSelector)((*electrons)[e]) && (*electronPtMinSelector)((*electrons)[e]) ) selElectrons.push_back((*electrons)[e]);
+     }
+     if( selElectrons.size() > 0 ){
+       //select highest pT electron
+       selElectron = selElectrons[0];
+       leptonFound = true;
+     }       
+   }  
+
+   // select MET (TopMET vector is sorted on ET)
+   bool METFound = false;
+   TopMET selMET;
+   edm::Handle<vector<TopMET> >  METs;
+   iEvent.getByType(METs);
+   vector<TopMET> selMETs;
+   for(size_t m=0; m < METs->size(); m++) { 
+     if( (*metEtMinSelector)((*METs)[m]) ) selMETs.push_back((*METs)[m]);
+   }
+   if( selMETs.size() > 0 ){
+     //select highest Et MET
+     selMET = selMETs[0];
+     METFound = true;
+   }
+
+   // select Jets (TopJet vector is sorted on recET, so four first elements in both the lJets and bJets vector are the same )
+   bool jetsFound = false;
+   vector<TopJet> lSelJets, bSelJets;
    edm::Handle<vector<TopJet> >  lJets;
    iEvent.getByLabel(lJetInput_,lJets);
    edm::Handle<vector<TopJet> >  bJets;
    iEvent.getByLabel(bJetInput_,bJets);
-
-   //select lepton (the TtLepton vectors are, for the moment, sorted on pT)
-   TopMuon selMuon;
-   TopElectron selElectron;
-   bool leptonFound = false;
-   if(leptonFlavour_ == "muon"     &&     muons->size()>=1) { selMuon     = (*muons)[0];     leptonFound = true;};    
-   if(leptonFlavour_ == "electron" && electrons->size()>=1) { selElectron = (*electrons)[0]; leptonFound = true;};  
-
-   //select MET (TopMET vector is sorted on ET)
-   TopMET selMET;
-   bool METFound = false;
-   if(mets -> size()>=1) { selMET = (*mets)[0]; METFound = true;};  
-
-   //select Jets (TopJet vector is sorted on recET, so four first elements in both the lJets and bJets vector are the same )
-   vector<TopJet> lSelJets, bSelJets;
-   bool jetsFound = false;
-   if(lJets -> size()>=4 && bJets -> size()>=4 ) { 
-     for(int j=0; j<4; j++) {
+   // vectors were sorted on recET
+   for(size_t j=0; j < min(lJets -> size(),bJets -> size()); j++) {
+     if( (*recJetEtMinSelector)((*lJets)[j].getRecJet()) && (*jetEtaRangeSelector)((*lJets)[j].getRecJet())
+      && (*calJetEtMinSelector)((*lJets)[j])             && (*calJetEtMinSelector)((*bJets)[j]) 
+      && true){
        lSelJets.push_back((*lJets)[j]);
        bSelJets.push_back((*bJets)[j]);
      }
-     jetsFound = true;
    }
+   if( lSelJets.size() >= 4 ) jetsFound = true;
+   
+   
+   
+   
+   
+   //
+   // Build Event solutions according to the ambiguity in the jet combination
+   //
    
    vector<TtSemiEvtSolution> *evtsols = new vector<TtSemiEvtSolution>();
    if(leptonFound && METFound && jetsFound){
@@ -104,10 +195,10 @@ void TtSemiEvtSolutionMaker::produce(edm::Event& iEvent, const edm::EventSetup& 
 		   asol.setLeptonParametrisation(param_);
 		   asol.setMETParametrisation(param_);
       		 }
-		 // these lines calculate the observables to be used in the JetCombination
+		 // these lines calculate the observables to be used in the JetCombination LR
 		 (*myLRJetCombObservables)(asol);
 		 //if asked for, calculate with these observable values the LRvalue and probability a jet combination is correct
-		 //if(addLRJetComb_) ...
+		 if(addLRJetComb_) (*myLRJetCombCalc)(asol);
 	         evtsols->push_back(asol);
 	       } 
 	     }
