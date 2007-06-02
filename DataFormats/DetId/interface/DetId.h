07f6868fd898ef292a46e2ae4f90e56558060e2b
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-06-02 06:05:11 +0000

    inline, auto conversion to rawid

--- DataFormats/DetId/interface/DetId.h
+++ DataFormats/DetId/interface/DetId.h
@@ -1,8 +1,8 @@
 #ifndef DATAFORMATS_DETID_H
-#define DATAFORMATS_DETID_H 1
+#define DATAFORMATS_DETID_H
 
 #include <boost/cstdint.hpp>
-#include <ostream>
+// #include <ostream>
 
 /** \class DetId
 
@@ -12,8 +12,8 @@ the large-scale detector (e.g. Tracker or Ecal) while the next three
 bits ([27:25]) identify a part of the detector (such as HcalBarrel
 (HB) for Hcal).
 
-$Date: 2005/10/06 00:23:46 $
-$Revision: 1.4 $
+$Date: 2006/10/13 19:38:48 $
+$Revision: 1.5 $
 */
 class DetId {
 public:
@@ -23,18 +23,21 @@ public:
 
   enum Detector { Tracker=1,Muon=2,Ecal=3,Hcal=4,Calo=5 };
   /// Create an empty or null id (also for persistence)
-  DetId();
+  DetId()  : id_(0) { }
   /// Create an id from a raw number
-  explicit DetId(uint32_t id);
+  DetId(uint32_t id) : id_(id) { }
   /// Create an id, filling the detector and subdetector fields as specified
-  DetId(Detector det, int subdet);
+  DetId(Detector det, int subdet)  {
+    id_=((det&0xF)<<28)|((subdet&0x7)<<25);
+  }
 
   /// get the detector field from this detid
   Detector det() const { return Detector((id_>>kDetOffset)&0xF); }
   /// get the contents of the subdetector field (not cast into any detector's numbering enum)
   int subdetId() const { return ((id_>>kSubdetOffset)&0x7); }
 
-  uint32_t operator()() { return id_; }
+  uint32_t operator()() const { return id_; }
+  operator uint32_t() const { return id_; }
 
   /// get the raw id 
   uint32_t rawId() const { return id_; }
@@ -42,15 +45,26 @@ public:
   bool null() const { return id_==0; }
   
   /// equality
-  int operator==(const DetId& id) const { return id_==id.id_; }
+  bool operator==(DetId id) const { return id_==id.id_; }
   /// inequality
-  int operator!=(const DetId& id) const { return id_!=id.id_; }
+  bool operator!=(DetId id) const { return id_!=id.id_; }
   /// comparison
-  int operator<(const DetId& id) const { return id_<id.id_; }
+  bool operator<(DetId id) const { return id_<id.id_; }
 
 protected:
   uint32_t id_;
 };
+
+/// equality
+bool operator==(uint32_t i, DetId id) const { return i==id.id_; }
+bool operator==(DetId id, uint32_t i) const { return i==id.id_; }
+/// inequality
+bool operator!=(uint32_t i, DetId id) const { return i!=id.id_; }
+bool operator!=(DetId id, uint32_t i) const { return i!=id.id_; }
+/// comparison
+bool operator<(uint32_t i, DetId id) const { return i<id.id_; }
+bool operator<(DetId id, uint32_t i) const { return id.id_<i; }
+
 
 //std::ostream& operator<<(std::ostream& s, const DetId& id);
 
