7ff27ede669b592b0a43c6764d770f5181071e56
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-06-03 13:08:04 +0000

    modified

--- CalibMuon/CSCCalibration/src/SealModule.cc
+++ CalibMuon/CSCCalibration/src/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
-#include "CondFormats/CSCObjects/interface/CSCFakeConditionsProducer.h"
+#include "CondFormats/CSCObjects/interface/CSCFakeConditions.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakeConditionsProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakeConditions);
