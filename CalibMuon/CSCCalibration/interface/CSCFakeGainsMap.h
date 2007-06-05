4785dd7875f8be1dfbb5699c4f310a949ffa6358
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-06-05 12:50:36 +0000

    modified id_layer

--- CalibMuon/CSCCalibration/interface/CSCFakeGainsMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeGainsMap.h
@@ -20,47 +20,58 @@ class CSCFakeGainsMap{
   CSCFakeGainsMap(){ 
   }
   
-  
+
   void prefillGainsMap(){
     
     const CSCDetId& detId = CSCDetId();
     cngains = new CSCGains();
-
-    int max_istrip,id_layer;
+    
+    int max_istrip,id_layer,max_ring,max_cham;
+    
     //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
+    
     for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
       for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-	if(istation==1)                detId.maxRingId()==4;
-	if(istation==2 || istation==3) detId.maxRingId()==2;
-	if(istation==4)                detId.maxRingId()==1;
-	
-	for(int iring=detId.minRingId(); iring<=detId.maxRingId(); iring++){
-	  // std::cout <<"Station: "<<iendcap<<" and ring "<<iring<<std::endl;
-	  for(int ichamber=detId.minChamberId(); ichamber<=detId.maxChamberId(); ichamber++){
+	max_ring=detId.maxRingId();
+	//station 4 ring 4 not there(36 chambers*2 missing)
+	//3 rings max this way of counting (ME1a & b)
+	if(istation==1)    max_ring=3;
+	if(istation==2)    max_ring=2;
+	if(istation==3)    max_ring=2;
+	if(istation==4)    max_ring=1;
+
+	for(int iring=detId.minRingId(); iring<=max_ring; iring++){
+	  max_istrip=80;
+	  max_cham=detId.maxChamberId();  
+	  if(istation==1 && iring==1)    max_cham=36;
+	  if(istation==1 && iring==2)    max_cham=36;
+	  if(istation==1 && iring==3)    max_cham=36;
+	  if(istation==2 && iring==1)    max_cham=18;
+	  if(istation==2 && iring==2)    max_cham=36;
+	  if(istation==3 && iring==1)    max_cham=18;
+	  if(istation==3 && iring==2)    max_cham=36;
+	  if(istation==4 && iring==1)    max_cham=18;
+
+	  for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
 	    for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	      if(istation==1 && iring==3){
-		max_istrip=64;
-	      }else{
-		max_istrip=80;
-		std::vector<CSCGains::Item> itemvector;
-		itemvector.resize(max_istrip);
-		
-		for(int istrip=0;istrip<max_istrip;istrip++){
-		  
-		  itemvector[istrip].gain_slope=8.0;
-		  itemvector[istrip].gain_intercept=-10.0;
-		  itemvector[istrip].gain_chi2=1.0;
-		  id_layer = 100000*iendcap+10000*istation+1000*iring+100*ichamber+10*ilayer+ilayer;
-		  std::cout<<" ID is: "<<id_layer<<std::endl;
-		  cngains->gains[id_layer]=itemvector;
-		}
+	      //station 1 ring 3 has 64 strips per layer instead of 80 
+	      if(istation==1 && iring==3)   max_istrip=64;
+	      
+	      std::vector<CSCGains::Item> itemvector;
+	      itemvector.resize(max_istrip);
+	      id_layer = 100000*iendcap + 10000*istation + 1000*iring + 10*ichamber + ilayer;
+
+	      for(int istrip=0;istrip<max_istrip;istrip++){
+		itemvector[istrip].gain_slope    = 7.55;
+		itemvector[istrip].gain_intercept= -10.00;
+		itemvector[istrip].gain_chi2     = 2.00;
+		cngains->gains[id_layer]=itemvector;
 	      }
 	    }
 	  }
 	}
       }
     }
-    
   }
 
   const CSCGains & get(){
@@ -69,10 +80,8 @@ class CSCFakeGainsMap{
   
   
  private:
-  
   CSCGains *cngains ;
-  const CSCGeometry *geometry;
-  
+  const CSCGeometry *geometry; 
 };
 
 #endif
--- CalibMuon/CSCCalibration/interface/CSCFakePedestalsMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakePedestalsMap.h
@@ -26,42 +26,53 @@ class CSCFakePedestalsMap{
     const CSCDetId& detId = CSCDetId();
     cnpedestals = new CSCPedestals();
 
-    int max_istrip,id_layer;
+    int max_istrip,id_layer,max_ring,max_cham;
+
     //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
+
     for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
       for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-	if(istation==1)                detId.maxRingId()==4;
-	if(istation==2 || istation==3) detId.maxRingId()==2;
-	if(istation==4)                detId.maxRingId()==1;
+	max_ring=detId.maxRingId();
+	//station 4 ring 4 not there(36 chambers*2 missing)
+	//3 rings max this way of counting (ME1a & b)
+	if(istation==1)    max_ring=3;
+	if(istation==2)    max_ring=2;
+	if(istation==3)    max_ring=2;
+	if(istation==4)    max_ring=1;
 	
-	for(int iring=detId.minRingId(); iring<=detId.maxRingId(); iring++){
-	  // std::cout <<"Station: "<<iendcap<<" and ring "<<iring<<std::endl;
-	  for(int ichamber=detId.minChamberId(); ichamber<=detId.maxChamberId(); ichamber++){
+	for(int iring=detId.minRingId(); iring<=max_ring; iring++){
+	  max_istrip=80;
+	  max_cham=detId.maxChamberId();  
+	  if(istation==1 && iring==1)    max_cham=36;
+	  if(istation==1 && iring==2)    max_cham=36;
+	  if(istation==1 && iring==3)    max_cham=36;
+	  if(istation==2 && iring==1)    max_cham=18;
+	  if(istation==2 && iring==2)    max_cham=36;
+	  if(istation==3 && iring==1)    max_cham=18;
+	  if(istation==3 && iring==2)    max_cham=36;
+	  if(istation==4 && iring==1)    max_cham=18;
+
+	  for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
 	    for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	      if(istation==1 && iring==3){
-		max_istrip=64;
-	      }else{
-		max_istrip=80;
-		std::vector<CSCPedestals::Item> itemvector;
-		itemvector.resize(max_istrip);
-		
-		for(int istrip=0;istrip<max_istrip;istrip++){
-		  
-		  itemvector[istrip].ped=600.0;
-		  itemvector[istrip].rms=1.5;
-		  id_layer = 100000*iendcap+10000*istation+1000*iring+100*ichamber+10*ilayer+ilayer;
-		  std::cout<<" ID is: "<<id_layer<<std::endl;
-		  cnpedestals->pedestals[id_layer]=itemvector;
-		}
+	      //station 1 ring 3 has 64 strips per layer instead of 80 
+	      if(istation==1 && iring==3)   max_istrip=64;
+	      
+	      std::vector<CSCPedestals::Item> itemvector;
+	      itemvector.resize(max_istrip);
+	      id_layer = 100000*iendcap + 10000*istation + 1000*iring + 10*ichamber + ilayer;
+
+	      for(int istrip=0;istrip<max_istrip;istrip++){
+		itemvector[istrip].ped=650.00;
+		itemvector[istrip].rms=1.5;
+		cnpedestals->pedestals[id_layer]=itemvector;
 	      }
 	    }
 	  }
 	}
       }
     }
-    
-  }
-
+  }  
+  
   const CSCPedestals & get(){
     return (*cnpedestals);
   }
