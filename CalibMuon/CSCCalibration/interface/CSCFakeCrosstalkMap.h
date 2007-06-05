6d79d20d437153fd39d946f0573b14e404435e01
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-06-05 14:45:54 +0000

    removed prefill from header

--- CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkMap.h
@@ -20,70 +20,12 @@ class CSCFakeCrosstalkMap{
   CSCFakeCrosstalkMap(){ 
   }
   
-  
-  void prefillCrosstalkMap(){
-    
-    const CSCDetId& detId = CSCDetId();
-    cncrosstalk = new CSCcrosstalk();
-
-    int max_istrip,id_layer,max_ring,max_cham;
-
-    //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
-
-    for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
-      for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-	max_ring=detId.maxRingId();
-	  //station 4 ring 4 not there(36 chambers*2 missing)
-	//3 rings max this way of counting (ME1a & b)
-	if(istation==1)    max_ring=3;
-	if(istation==2)    max_ring=2;
-	if(istation==3)    max_ring=2;
-	if(istation==4)    max_ring=1;
-	
-	for(int iring=detId.minRingId(); iring<=max_ring; iring++){
-	  max_istrip=80;
-	  max_cham=detId.maxChamberId();
-	  if(istation==1 && iring==1)    max_cham=36;
-	  if(istation==1 && iring==2)    max_cham=36;
-	  if(istation==1 && iring==3)    max_cham=36;
-	  if(istation==2 && iring==1)    max_cham=18;
-	  if(istation==2 && iring==2)    max_cham=36;
-	  if(istation==3 && iring==1)    max_cham=18;
-	  if(istation==3 && iring==2)    max_cham=36;
-	  if(istation==4 && iring==1)    max_cham=18;
-	  
-	  for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
-	    for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	      //station 1 ring 3 has 64 strips per layer instead of 80 
-	      if(istation==1 && iring==3)   max_istrip=64;
-
-	      std::vector<CSCcrosstalk::Item> itemvector;
-	      itemvector.resize(max_istrip);
-	      id_layer = 100000*iendcap + 10000*istation + 1000*iring + 10*ichamber + ilayer;
-	      
-	      for(int istrip=0;istrip<max_istrip;istrip++){		  
-		itemvector[istrip].xtalk_slope_right    = -0.001;
-		itemvector[istrip].xtalk_intercept_right= 0.045;
-		itemvector[istrip].xtalk_chi2_right     = 2.00;
-		itemvector[istrip].xtalk_slope_left     = -0.001;
-		itemvector[istrip].xtalk_intercept_left = 0.045;
-		itemvector[istrip].xtalk_chi2_left      = 2.00;
-		
-		id_layer = 100000*iendcap+10000*istation+1000*iring+100*ichamber+10*ilayer+ilayer;
-		cncrosstalk->crosstalk[id_layer]=itemvector;
-	      }
-	    }
-	  }
-	}
-      }
-    }
-  } 
+  void prefillCrosstalkMap();
 
   const CSCcrosstalk & get(){
     return (*cncrosstalk);
   }
-  
-  
+    
  private:
   
   CSCcrosstalk *cncrosstalk ;
--- CalibMuon/CSCCalibration/interface/CSCFakePedestalsMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakePedestalsMap.h
@@ -20,59 +20,8 @@ class CSCFakePedestalsMap{
   CSCFakePedestalsMap(){ 
   }
   
-  
-  void prefillPedestalsMap(){
+  void prefillPedestalsMap();
     
-    const CSCDetId& detId = CSCDetId();
-    cnpedestals = new CSCPedestals();
-
-    int max_istrip,id_layer,max_ring,max_cham;
-
-    //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
-
-    for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
-      for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-	max_ring=detId.maxRingId();
-	//station 4 ring 4 not there(36 chambers*2 missing)
-	//3 rings max this way of counting (ME1a & b)
-	if(istation==1)    max_ring=3;
-	if(istation==2)    max_ring=2;
-	if(istation==3)    max_ring=2;
-	if(istation==4)    max_ring=1;
-	
-	for(int iring=detId.minRingId(); iring<=max_ring; iring++){
-	  max_istrip=80;
-	  max_cham=detId.maxChamberId();  
-	  if(istation==1 && iring==1)    max_cham=36;
-	  if(istation==1 && iring==2)    max_cham=36;
-	  if(istation==1 && iring==3)    max_cham=36;
-	  if(istation==2 && iring==1)    max_cham=18;
-	  if(istation==2 && iring==2)    max_cham=36;
-	  if(istation==3 && iring==1)    max_cham=18;
-	  if(istation==3 && iring==2)    max_cham=36;
-	  if(istation==4 && iring==1)    max_cham=18;
-
-	  for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
-	    for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	      //station 1 ring 3 has 64 strips per layer instead of 80 
-	      if(istation==1 && iring==3)   max_istrip=64;
-	      
-	      std::vector<CSCPedestals::Item> itemvector;
-	      itemvector.resize(max_istrip);
-	      id_layer = 100000*iendcap + 10000*istation + 1000*iring + 10*ichamber + ilayer;
-
-	      for(int istrip=0;istrip<max_istrip;istrip++){
-		itemvector[istrip].ped=650.00;
-		itemvector[istrip].rms=1.5;
-		cnpedestals->pedestals[id_layer]=itemvector;
-	      }
-	    }
-	  }
-	}
-      }
-    }
-  }  
-  
   const CSCPedestals & get(){
     return (*cnpedestals);
   }
