3e804c29c9a3f9096453d7d83d27790cdde5b769
Paolo Ronchese <sha1-591dc25f43b7b0cdf7a5025ce38797ed3141c081@cern.ch>
2007-06-05 16:02:37 +0000

    *** empty log message ***

--- CondFormats/DataRecord/interface/DTT0GeomRcd.h
+++
@@ -1,9 +0,0 @@
-#ifndef DTT0GEOMRCD_H
-#define DTT0GEOMRCD_H
-
-#include "boost/mpl/vector.hpp"
-#include "Geometry/Records/interface/MuonGeometryRecord.h"
-
-#include "FWCore/Framework/interface/DependentRecordImplementation.h"
-class DTT0GeomRcd : public edm::eventsetup::DependentRecordImplementation<DTT0GeomRcd, boost::mpl::vector<MuonGeometryRecord> > {};
-#endif
