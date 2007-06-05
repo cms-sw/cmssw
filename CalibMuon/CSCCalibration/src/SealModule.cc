4eebabace0b1fe3ffac499bbe62c1804afc69141
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-06-05 16:54:04 +0000

    added all consts

--- CalibMuon/CSCCalibration/src/SealModule.cc
+++ CalibMuon/CSCCalibration/src/SealModule.cc
@@ -6,9 +6,16 @@
 #include "CalibMuon/CSCCalibration/interface/CSCFakePedestalsConditions.h"
 #include "CalibMuon/CSCCalibration/interface/CSCFakeNoiseMatrixConditions.h"
 #include "CalibMuon/CSCCalibration/interface/WriteGainsToDB.h"
+#include "CalibMuon/CSCCalibration/interface/WritePedestalsToDB.h"
+#include "CalibMuon/CSCCalibration/interface/WriteNoiseMatrixToDB.h"
+#include "CalibMuon/CSCCalibration/interface/WriteCrosstalkToDB.h"
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakeGainsConditions);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakeCrosstalkConditions);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakePedestalsConditions);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCFakeNoiseMatrixConditions);
 DEFINE_ANOTHER_FWK_MODULE(WriteGainsToDB);
+DEFINE_ANOTHER_FWK_MODULE(WritePedestalsToDB);
+DEFINE_ANOTHER_FWK_MODULE(WriteNoiseMatrixToDB);
+DEFINE_ANOTHER_FWK_MODULE(WriteCrosstalkToDB);
