1a2521207b3aa6d602220f8920516cc813b9ae25
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-06-05 14:29:12 +0000

    added error codes and isValid

--- DQM/SiStripCommissioningAnalysis/interface/ApvTimingAnalysis.h
+++ DQM/SiStripCommissioningAnalysis/interface/ApvTimingAnalysis.h
@@ -23,6 +23,8 @@ class ApvTimingAnalysis : public CommissioningAnalysis {
   ApvTimingAnalysis();
   virtual ~ApvTimingAnalysis() {;}
   
+  // ---------- Access to analysis and histos ----------
+
   inline const float& time() const; 
   inline const float& maxTime() const; 
   inline const float& delay() const; 
@@ -33,9 +35,16 @@ class ApvTimingAnalysis : public CommissioningAnalysis {
   
   inline const Histo& histo() const;
   
-  void maxTime( const float& time ); 
+  void maxTime( const float& time ); //@@ make this and data static!
+
+  // ---------- Utility methods ----------
   
+  bool isValid();
+
   void print( std::stringstream&, uint32_t not_used = 0 );
+  
+  /** Override base method for public access. */
+  inline void addErrorCode( const std::string& error );
   
  private:
   
@@ -47,16 +56,22 @@ class ApvTimingAnalysis : public CommissioningAnalysis {
   
   /** Time of tick mark rising edge [ns] */
   float time_;
+
   /** Time of sampling point of last tick mark [ns] */
   float maxTime_;
+
   /** Delay required to synchronise */ 
   float delay_;
+
   /** Error on time delay [ns] */
   float error_;
+
   /** Level of tick mark "base" [adc] */
   float base_;
+
   /** Level of tick mark "peak" [adc] */
   float peak_;
+
   /** Tick mark height [adc] */
   float height_;
   
@@ -76,6 +91,7 @@ const float& ApvTimingAnalysis::base() const { return base_; }
 const float& ApvTimingAnalysis::peak() const { return peak_; }
 const float& ApvTimingAnalysis::height() const { return height_; }
 const ApvTimingAnalysis::Histo& ApvTimingAnalysis::histo() const { return histo_; }
+void ApvTimingAnalysis::addErrorCode( const std::string& error ) { CommissioningAnalysis::addErrorCode(error) ;}
 
 #endif // DQM_SiStripCommissioningAnalysis_ApvTimingAnalysis_H
 
