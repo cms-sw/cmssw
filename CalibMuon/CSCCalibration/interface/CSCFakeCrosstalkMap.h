c7442eaacc477a0fc1abac9532eefe9b90f55448
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-06-08 17:40:38 +0000

    Factored out the smearing into a separate method

--- CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkMap.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkMap.h
@@ -1,35 +1,25 @@
 #ifndef _CSC_FAKE_CROSSTALK_MAP
 #define _CSC_FAKE_CROSSTALK_MAP
 
-#include <iostream>
-#include <map>
-#include <vector>
-#include <iomanip>
-
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
-#include <Geometry/CSCGeometry/interface/CSCLayer.h>
-#include <Geometry/CSCGeometry/interface/CSCChamberSpecs.h>
-#include <Geometry/CSCGeometry/interface/CSCLayerGeometry.h>
 #include <Geometry/CSCGeometry/interface/CSCGeometry.h>
-
-#include <DataFormats/MuonDetId/interface/CSCDetId.h>
-#include <CondFormats/CSCObjects/interface/CSCReadoutMapping.h>
 
 class CSCFakeCrosstalkMap{
  public:
-  CSCFakeCrosstalkMap(){ 
-  }
+  CSCFakeCrosstalkMap();
   
-  float mean,min,minchi;
-  int seed;long int M;
-  
-  void prefillCrosstalkMap();
-
   const CSCcrosstalk & get(){
     return (*cncrosstalk);
   }
     
+  void smear();
+
  private:
+  float theMean;
+  float theMin;
+  float theMinChi;
+  int theSeed;
+  long int theM;
   
   CSCcrosstalk *cncrosstalk ;
   const CSCGeometry *geometry;
