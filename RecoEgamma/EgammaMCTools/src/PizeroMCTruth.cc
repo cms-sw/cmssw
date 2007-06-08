56092b438a7ab6682ce2fd72304ae295bf2cc323
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-06-08 10:38:19 +0000

    Complete restructuring. The class PizeroMCTruth now stores the pizero four momentum, the MC trhuth of decay photons

--- RecoEgamma/EgammaMCTools/src/PizeroMCTruth.cc
+++ RecoEgamma/EgammaMCTools/src/PizeroMCTruth.cc
@@ -1,4 +1,7 @@
 #include "RecoEgamma/EgammaMCTools/interface/PizeroMCTruth.h"
+#include "RecoEgamma/EgammaMCTools/interface/PhotonMCTruth.h"
+#include "RecoEgamma/EgammaMCTools/interface/ElectronMCTruth.h"
+
 
 #include <iostream>
 
@@ -6,21 +9,14 @@ PizeroMCTruth::PizeroMCTruth() {
 
 }
 
-void PizeroMCTruth::SetDecay(float r, float z, CLHEP::HepLorentzVector momentum1, CLHEP::HepLorentzVector momentum2)
-{
-  dalitz_ = false;
-  r_ = r;
-  z_ = z;
-  momentum1_ = momentum1;
-  momentum2_ = momentum2;
-}
 
-void PizeroMCTruth::SetDalitzDecay(float r, float z, CLHEP::HepLorentzVector momentum1, CLHEP::HepLorentzVector momentum2, CLHEP::HepLorentzVector momentum3)
-{
-  dalitz_ = true;
-  r_ = r;
-  z_ = z;
-  momentum1_ = momentum1;
-  momentum2_ = momentum2;
-  momentum3_ = momentum3;
-}
+PizeroMCTruth::PizeroMCTruth( const HepLorentzVector& pizeroFourMomentum, 
+			      std::vector<PhotonMCTruth>& photons,
+			      const HepLorentzVector& pV):  
+			      
+  thePizero_(pizeroFourMomentum), thePhotons_(photons), thePrimaryVertex_(pV) {}
+
+
+
+
+ 
