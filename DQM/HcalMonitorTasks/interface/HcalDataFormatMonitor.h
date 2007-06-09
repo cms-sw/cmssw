3209bfee626f949dc254f762443b02df4ba47089
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2007-06-09 21:13:04 +0000

    more inclusive treatment of HTR header

--- DQM/HcalMonitorTasks/interface/HcalDataFormatMonitor.h
+++ DQM/HcalMonitorTasks/interface/HcalDataFormatMonitor.h
@@ -7,44 +7,62 @@
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
 
 /** \class Hcaldataformatmonitor
-  *  
-  * $Date: 2007/04/02 13:19:38 $
-  * $Revision: 1.11 $
-  * \author W. Fisher - FNAL
-  */
+ *
+ * $Date: 2007/04/20 15:11:10 $
+ * $Revision: 1.12 $
+ * \author W. Fisher - FNAL
+ */
 class HcalDataFormatMonitor: public HcalBaseMonitor {
-public:
-  HcalDataFormatMonitor(); 
-  ~HcalDataFormatMonitor(); 
-
+ public:
+  HcalDataFormatMonitor();
+  ~HcalDataFormatMonitor();
+  
   void setup(const edm::ParameterSet& ps, DaqMonitorBEInterface* dbe);
-  void processEvent(const FEDRawDataCollection& rawraw, const HcalUnpackerReport& report, const HcalElectronicsMap& emap);
+  void processEvent(const FEDRawDataCollection& rawraw, const
+		    HcalUnpackerReport& report, const HcalElectronicsMap& emap);
   void unpack(const FEDRawData& raw, const HcalElectronicsMap& emap);
   void clearME();
-
-private: /// Data accessors
-  vector<int> fedUnpackList_;
-  int firstFED_;
-  int ievt_;
-
-private:  ///Monitoring elements
-
-  MonitorElement* meEVT_;
-
-  MonitorElement* meSpigotFormatErrors_;
-  MonitorElement* meBadQualityDigis_;
-  MonitorElement* meUnmappedDigis_;
-  MonitorElement* meUnmappedTPDigis_;
-  MonitorElement* meFEDerrorMap_;
   
-  struct{
-    MonitorElement* ERR_MAP;
-    MonitorElement* DCC_ERRWD;
-    MonitorElement* SpigotMap;
-  } hbHists, heHists, hfHists,hoHists;
-
-  
-
+ private: // Data accessors
+   vector<int> fedUnpackList_;
+   int firstFED_;
+   int ievt_;
+   int lastEvtN_;
+   
+ private:  //Monitoring elements
+   
+   MonitorElement* meEVT_;
+   
+   //MEs for hcalunpacker report info
+   MonitorElement* meSpigotFormatErrors_;
+   MonitorElement* meBadQualityDigis_;
+   MonitorElement* meUnmappedDigis_;
+   MonitorElement* meUnmappedTPDigis_;
+   MonitorElement* meFEDerrorMap_;
+   //Check that evt numbers are synchronized across all HTRs
+   MonitorElement* meEvtNumberSynch_;
+   
+   // The following MEs map specific conditons from the HTR/DCC headers as specified in
+   //   http://cmsdoc.cern.ch/cms/HCAL/document/CountingHouse/HTR/design/Rev4MainFPGA.pdf
+   struct{
+     MonitorElement* DCC_ErrWd;  //16 (12??) bit HTR error word, Ext. Header 3
+     MonitorElement* SpigotMap;  //Map of HTR errors into Spigot/DCCID space
+     MonitorElement* CrateMap;   //Map of HTR errors into Crate/Slot space
+     MonitorElement* ExtHeader5; //16 bits from Ext. Header 5
+     MonitorElement* ExtHeader7; //16 bits from Ext. Header 7     
+     //Map of into HTR error bits into Spigot/DCCID space
+     //  listed in increasing order from LSB of Byte 0
+     MonitorElement* OWMap;      //Overflow warnings
+     MonitorElement* BZMap;      //Internal buffer busy
+     MonitorElement* EEMap;      //Empty event
+     MonitorElement* RLMap;      //Reject L1A
+     MonitorElement* LEMap;      //Latency errors 
+     MonitorElement* LWMap;      //Latency warnings 
+     MonitorElement* ODMap;      //Optical Data errors 
+     MonitorElement* CKMap;      //Clock errors 
+     MonitorElement* BEMap;      //Bunch error
+   } hbHists,heHists,hfHists,hoHists;
+   
 };
 
 #endif
