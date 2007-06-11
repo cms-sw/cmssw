299798a09f4ea34c62d4c7a94e3541e9122ce38b
Shan-Huei Chuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2007-06-11 18:23:40 +0000

    histogram IDs are modified to conform with other parts of pixel dqm.

--- DQM/SiPixelMonitorTrack/src/SiPixelResidualModule.cc
+++ DQM/SiPixelMonitorTrack/src/SiPixelResidualModule.cc
@@ -1,9 +1,28 @@
+// Package:    SiPixelMonitorTrack
+// Class:      SiPixelResidualModule
+// 
+// class SiPixelResidualModule SiPixelResidualModule.cc 
+//       DQM/SiPixelMonitorTrack/src/SiPixelResidualModule.cc
+//
+// Description: SiPixel hit-to-track residual data quality monitoring modules
+// Implementation: prototype -> improved -> never final - end of the 1st step 
+//
+// Original Author: Shan-Huei Chuang
+//         Created: Fri Mar 23 18:41:42 CET 2007
+// $Id: SiPixelResidualModule.cc,v 1.3 2007/05/24 06:11:46 schuang Exp $
+
+
 #include <boost/cstdint.hpp>
 #include <string>
+#include <iostream>
 
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DQM/SiPixelCommon/interface/SiPixelHistogramId.h"
 #include "DQM/SiPixelMonitorTrack/interface/SiPixelResidualModule.h"
+
+
+using namespace std; 
 
 
 SiPixelResidualModule::SiPixelResidualModule() : id_(0) {
@@ -18,14 +37,17 @@ SiPixelResidualModule::~SiPixelResidualModule() {
 }
 
 
-void SiPixelResidualModule::book() {
+void SiPixelResidualModule::book(const edm::ParameterSet& iConfig) {
   DaqMonitorBEInterface* _dbe = edm::Service<DaqMonitorBEInterface>().operator->();
-  char hisID[80]; 
 
-  sprintf(hisID,"residual_x_module_%i",id_);
+  std::string hisID;
+  edm::InputTag src = iConfig.getParameter<edm::InputTag>("src");
+  SiPixelHistogramId* theHistogramId = new SiPixelHistogramId(src.label());
+
+  hisID = theHistogramId->setHistoId("residualX",id_);
   meResidualX_ = _dbe->book1D(hisID,"Hit-to-Track Residual in X",500,-5.,5.);
 
-  sprintf(hisID,"residual_y_module_%i",id_);
+  hisID = theHistogramId->setHistoId("residualY",id_);
   meResidualY_ = _dbe->book1D(hisID,"Hit-to-Track Residual in Y",500,-5.,5.);
 }
 
