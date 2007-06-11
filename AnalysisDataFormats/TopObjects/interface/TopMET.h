1e743eb64becc149785cf8f205ae2f2112096b8a
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2007-06-11 21:00:37 +0000

    Add implementation of W -> l + nu solver for neutrino momentum, first version still needs further tests

--- AnalysisDataFormats/TopObjects/interface/TopMET.h
+++ AnalysisDataFormats/TopObjects/interface/TopMET.h
@@ -2,7 +2,7 @@
 // Author:  Steven Lowette
 // Created: Thu May  3 10:37:17 PDT 2007
 //
-// $Id: TopMET.h,v 1.2 2007/05/22 16:36:50 heyninck Exp $
+// $Id: TopMET.h,v 1.3 2007/06/09 01:18:48 lowette Exp $
 //
 
 #ifndef TopMET_h
@@ -15,7 +15,7 @@
    TopMET contains a missing ET 4-vector as a TopObject
 
   \author   Steven Lowette
-  \version  $Id: TopMET.h,v 1.2 2007/05/22 16:36:50 heyninck Exp $
+  \version  $Id: TopMET.h,v 1.3 2007/06/09 01:18:48 lowette Exp $
 */
 
 
@@ -40,11 +40,22 @@ class TopMET : public TopObject<METType> {
     TopParticle    getFitMET() const;
     void           setGenMET(Particle);
     void           setFitMET(TopParticle);
-
+	// solve for neutrino Pz constraining to the W mass in W -> mu + nu
+	// type defines how to choose the roots:
+	// type = 1: the closest nu_pz to mu_pz if real roots,
+	//           or just the real part if solution is complex.
+	// type = 2: pending
+	// type = 3: pending
+	TopParticle    getPz(TopParticle lepton, int type=0);
+	// return true if the solution is complex
+	bool           isSolutionComplex() { return iscomplex_; }
+	
   protected:
 
     reco::Particle genMET;
     TopParticle    fitMET;
+	bool           iscomplex_;
+	
 
 };
 
--- AnalysisDataFormats/TopObjects/src/TopMET.cc
+++ AnalysisDataFormats/TopObjects/src/TopMET.cc
@@ -8,16 +8,23 @@
 //
 // Original Author:  
 //         Created:  Wed May 10 11:48:25 CEST 2006
-// $Id: TopMET.cc,v 1.1 2007/05/02 15:05:04 lowette Exp $
+// $Id: TopMET.cc,v 1.1 2007/05/22 16:36:50 heyninck Exp $
 //
 
 // system include files
 
 // user include files
 #include "AnalysisDataFormats/TopObjects/interface/TopMET.h"
+#include "TMath.h"
 
-TopMET::TopMET(){ }
-TopMET::TopMET(METType aMet): TopObject<METType>(aMet) {}
+TopMET::TopMET() {
+	iscomplex_ = false;
+}
+
+TopMET::TopMET(METType aMet): TopObject<METType>(aMet) {
+	iscomplex_ = false;
+}
+
 TopMET::~TopMET(){ }
 
 void 		TopMET::setGenMET(reco::Particle gm)     { genMET = gm; }
@@ -25,3 +32,51 @@ void 		TopMET::setFitMET(TopParticle fm)   { fitMET = fm; }
 
 reco::Particle	TopMET::getGenMET() const	    { return genMET; }
 TopParticle	TopMET::getFitMET() const	    { return fitMET; }
+
+//_________________________________________________
+TopParticle TopMET::getPz(TopParticle lepton, int type) {
+
+	// fixme: check you have initialized MET first
+	
+	double M_W  = 80.4;
+	double M_mu =  0.10566;
+	double emu = lepton.energy();
+	double pxmu = lepton.px();
+	double pymu = lepton.py();
+	double pzmu = lepton.pz();
+	double pxnu = fitMET.px();
+	double pynu = fitMET.py();
+	double pznu = 0.;
+
+	double a = M_W*M_W - M_mu*M_mu + 2.0*pxmu*pxnu + 2.0*pymu*pynu;
+	double A = 4.0*(emu*emu - pzmu*pzmu);
+	double B = -4.0*a*pzmu;
+	double C = 4.0*emu*emu*(pxnu*pxnu + pynu*pynu) - a*a;
+
+	double tmproot = B*B - 4.0*A*C;
+
+	if (tmproot<0) {
+		iscomplex_= true;
+		if (type==0) pznu = - B/(2*A); // take real part of complex roots
+
+		//std::cout<< "complex sol. tmproot=" << tmproot << " pznu=" << pznu << std::endl;
+	}
+	else {
+		iscomplex_ = false;
+		double tmpsol1 = (-B + TMath::Sqrt(tmproot))/(2.0*A);
+		double tmpsol2 = (-B - TMath::Sqrt(tmproot))/(2.0*A);
+		
+		if (type == 0 ) {
+			// two real roots, pick one with pz closest to muon
+			if (TMath::Abs(tmpsol2-pzmu) < TMath::Abs(tmpsol1-pzmu)) { pznu = tmpsol2;}
+			else pznu = tmpsol1;
+		}
+	}
+
+	Particle neutrino;
+	neutrino.setP4( LorentzVector(pxnu, pynu, pznu, TMath::Sqrt(pxnu*pxnu + pynu*pynu + pznu*pznu ))) ;
+	
+	return neutrino;
+
+	
+}
