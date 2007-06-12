42be2501de5f77f93d5cc4a040d38c10a0c2db91
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-06-12 08:47:26 +0000

    Added tilt of beam line

--- RecoVertex/VertexPrimitives/src/BeamSpot.cc
+++ RecoVertex/VertexPrimitives/src/BeamSpot.cc
@@ -1,5 +1,6 @@
 #include "RecoVertex/VertexPrimitives/interface/BeamSpot.h"
 #include "RecoVertex/VertexPrimitives/interface/ConvertToFromReco.h"
+#include "DataFormats/GeometrySurface/interface/TkRotation.h"
 
 BeamSpot::BeamSpot() : thePos(0, 0, 0), 
 		       theErr(0.0015*0.0015, 0., 0.0015*0.0015, 
@@ -7,33 +8,49 @@ BeamSpot::BeamSpot() : thePos(0, 0, 0),
 {}
 
 BeamSpot::BeamSpot(const reco::BeamSpot & beamSpot) :
-	 thePos(RecoVertex::convertPos(beamSpot.position())),
-	 theErr(RecoVertex::convertError(beamSpot.covariance3D()))
-{}
+	 thePos(RecoVertex::convertPos(beamSpot.position()))
+{
+  GlobalVector newZ(beamSpot.dxdz(), beamSpot.dydz(), 1);
+  AlgebraicSymMatrix diagError(3,0);
+  diagError(1,1) = pow(beamSpot.BeamWidth(),2) + pow(beamSpot.x0Error(),2);
+  diagError(2,2) = pow(beamSpot.BeamWidth(),2) + pow(beamSpot.y0Error(),2);
+  diagError(3,3) = pow(beamSpot.sigmaZ(),2) + pow(beamSpot.z0Error(),2);
+  theErr = rotateMatrix(newZ, diagError);
+}
 
 BeamSpot::BeamSpot(const BeamSpotObjects * beamSpot)
 {
   thePos=GlobalPoint(beamSpot->GetX(), beamSpot->GetY(), beamSpot->GetZ());
-  double dx=sqrt(pow(beamSpot->GetBeamWidth(),2)+pow(beamSpot->GetXError(),2));
-  double dy=sqrt(pow(beamSpot->GetBeamWidth(),2)+pow(beamSpot->GetYError(),2));
-  double dz=beamSpot->GetSigmaZ();
-  theErr=GlobalError(dx*dx,0.,dy*dy, 0., 0., dz*dz);
+
+  GlobalVector newZ(beamSpot->Getdxdz(), beamSpot->Getdydz(), 1);
+  AlgebraicSymMatrix diagError(3,0);
+  diagError(1,1) = pow(beamSpot->GetBeamWidth(),2)+pow(beamSpot->GetXError(),2);
+  diagError(2,2) = pow(beamSpot->GetBeamWidth(),2)+pow(beamSpot->GetYError(),2);
+  diagError(3,3) = pow(beamSpot->GetSigmaZ(),2) +  pow(beamSpot->GetZError(),2);
+  theErr = rotateMatrix(newZ, diagError);
 }
 
-  // to be replaced by ParameterSet
-  /*
-  ConfigurableVector<float> conf_pos ("BeamSpot:position");
-  if (conf_pos.size() == 3) {
-    thePos = GlobalPoint(conf_pos[0], conf_pos[1], conf_pos[2]);
-  }
+AlgebraicSymMatrix BeamSpot::rotateMatrix(const GlobalVector& newZ,
+	const AlgebraicSymMatrix& diagError) const
+{
+  GlobalVector globalZ(0.,0.,1.);
+  GlobalVector rotationAxis = globalZ.unit().cross(newZ.unit());
+  float rotationAngle = -acos(globalZ.unit().dot(newZ.unit()));
 
-  ConfigurableVector<float> conf_spread ("BeamSpot:spread");
-  if (conf_spread.size() == 3) {
-    theErr = GlobalError(conf_spread[0]*conf_spread[0], 
-			 0., 
-			 conf_spread[1]*conf_spread[1], 
-			 0., 
-			 0.,
-			 conf_spread[2]*conf_spread[2]);
-  }
-  */
+  TkRotation<float> rotation(rotationAxis.basicVector(),rotationAngle);
+  AlgebraicMatrix rotationMatrix(3,3,0);
+  rotationMatrix(1,1) = rotation.xx();
+  rotationMatrix(1,2) = rotation.xy();
+  rotationMatrix(1,3) = rotation.xz();
+  rotationMatrix(2,1) = rotation.yx();
+  rotationMatrix(2,2) = rotation.yy();
+  rotationMatrix(2,3) = rotation.yz();
+  rotationMatrix(3,1) = rotation.zx();
+  rotationMatrix(3,2) = rotation.zy();
+  rotationMatrix(3,3) = rotation.zz();
+
+//  std::cout << rotation;
+
+  return diagError.similarity(rotationMatrix);
+
+}
