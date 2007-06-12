d6a88be3bcb6b0e97bc37102e5de88cfc6e6f00f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-06-12 16:45:30 +0000

    fix for 140

---
+++ L1Trigger/TextToDigi/src/TextToRaw.h
@@ -0,0 +1,66 @@
+#ifndef TextToRaw_h
+#define TextToRaw_h
+
+// -*- C++ -*-
+//
+// Package:    TextToRaw
+// Class:      TextToRaw
+// 
+/**\class TextToRaw TextToRaw.cc L1Triggr/TextToDigi/src/TextToRaw.cc
+
+ Description: Convert ASCII dump of a raw event to FEDRawData format for unpacking
+
+ Implementation:
+     Input format is a 32 bit hex string per line (LSW first). Events separated with blank line.
+*/
+//
+// Original Author:  Jim Brooke
+//         Created:  Wed Nov  1 11:57:10 CET 2006
+// $Id: TextToRaw.h,v 1.1 2007/03/08 14:00:55 jbrooke Exp $
+//
+//
+
+
+// system include files
+#include <memory>
+#include <string>
+#include <fstream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/FEDRawData/interface/FEDRawData.h"
+
+//
+// class decleration
+//
+
+class TextToRaw : public edm::EDProducer {
+ public:
+  explicit TextToRaw(const edm::ParameterSet&);
+  ~TextToRaw();
+  
+ private: // methods
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+ private:
+
+  // ID of the FED to emulate
+  int fedId_;
+
+  // File to read
+  std::string filename_;
+  std::ifstream file_;
+
+  // array to store the data
+  static const int EVT_MAX_SIZE=1024;
+  char data_[1024];
+
+};
+
+#endif
