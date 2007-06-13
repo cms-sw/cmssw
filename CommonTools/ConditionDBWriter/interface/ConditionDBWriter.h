803bf67c0637d75dd73e00a895da593a9f49ce94
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2007-06-13 15:50:54 +0000

    improved documentation and minor fixes

--- CommonTools/ConditionDBWriter/interface/ConditionDBWriter.h
+++ CommonTools/ConditionDBWriter/interface/ConditionDBWriter.h
@@ -5,17 +5,128 @@
 // Package:    ConditionDBWriter
 // Class:      ConditionDBWriter
 // 
-/**\class ConditionDBWriter ConditionDBWriter.cc CalibTracker/SiStripChannelGain/src/ConditionDBWriter.cc
+// \class ConditionDBWriter 
 
- Description: <one line class summary>
+//  Description: 
 
- Implementation:
-     <Notes on implementation>
-*/
+//  Implementation:
+
+// This class can be very useful whenever a CMSSW application needs to store data
+// to the offline DB. Typically such applications require access to event data 
+// and/or need to be notified about the start of Run, Lumi section in order 
+// to set a correct Interval Of Validity (IOV) for the data they have to store.
+// Therefore the FWK EDAnalyzer is an excellent candidate for the implementation
+// of such applications; this is the reason why this class inherits from 
+// the EDAnalyzer class. 
+
+// The user class should inherit from this class. 
+// The templated type must be the type of the object that
+// has to be written on the DB (e.g. MyCalibration).
+
+// The user must implement in his derived class the abstract method below
+
+// virtual MyCalibration * getNewObject()=0;
+
+// in this method, the user must create a new instance of the DB object and 
+// return a pointer to it. The object must be created with "new" and never 
+// be deleted by the user: it will be the FWK that takes control over it.
+
+// The user can optionally implement the following methods 
+
+//   //Will be called at the beginning of the job
+//   virtual void algoBeginJob(const edm::EventSetup&){};
+//   //Will be called at the beginning of each run in the job
+//   virtual void algoBeginRun(const edm::Run &, const edm::EventSetup &){};
+//   //Will be called at the beginning of each luminosity block in the run
+//   virtual void algoBeginLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup &){};
+//   //Will be called at every event
+//   virtual void algoAnalyze(const edm::Event&, const edm::EventSetup&){};
+//   //Will be called at the end of each run in the job
+//   virtual void algoEndRun(const edm::Run &, const edm::EventSetup &){};
+//   //Will be called at the end of the job
+//   virtual void algoEndJob(){};
+
+// where he can access information needed to build his object. For instance, if
+// he is computing a calibration that is computed as the mean of a certain
+// quantity that varies from event to event, he will implement the algoAnalyze 
+// method.
+
+// The important part is the IOV setting. The advantage of using this class is 
+// that this setting is performed almost automatically: the only thing
+// that the user has to do is to pass prescriptions about the IOV setting
+// in the configuration of his module. A typical
+// configuration is as follows:
+
+
+//       module prod =  SiStripGainRandomCalculator {
+
+//       #parameters of the derived class
+// 		double MinPositiveGain = 0.1
+// 		double MeanGain    = 1
+// 		double SigmaGain   = 0
+//                 untracked bool   printDebug = true
+
+//       #parameters of the base class
+// 		string IOVMode	     = "Run"
+// 		bool SinceAppendMode = true
+// 		string Record        = "SiStripApvGainRcd"
+
+//                }
+
+// Two subsets of parameters can be found. The first subset contains the specific
+// parameters of the user class, which is called in this case 
+// SiStripGainRandomCalculator. The second subset contains the parameters of
+// the base class. These are the following:
+
+// 1) string IOVMode
+
+// 4 possible values can be given: "Job", "Run", "LumiBlock" and "AlgoDriven"
+// This card determines the length of the IOV. In other words, together with  
+// the number of Lumysections/runs the user has decided to run his application,
+// this card determines the number of objects that will be stored on the DB
+// (the getNewObject method will be called as many times).
+// For example if the user is running on the events of one Run, which has 
+// 10 luminosity sections and chooses the "LumiBlock" mode, then 10 objects
+// with corresponding IOV will be written. If the "Job" mode is chosen, only one 
+// object will be stored irrespective of the dataset on which the user is 
+// running.
+// The  "AlgoDriven" option is special. If this choice is made, then it is 
+// up to the user to tell in the code when the getNewObject method must be 
+// called. This can be done by calling the method  below  void storeOnDbNow()
+// must be invoked whenever a certain condition that justifies the start/end
+// of an IOV is met.
+
+// 2) bool SinceAppendMode
+
+// If this card is set to true, new objects are appended to the chosen tag (
+// to be specified in the configuration of the  PoolDBOutputService service,
+// which must always be included in the cfg file of your application).
+//     If instead this card is set to false, new objects are appended in till
+//     mode. For example if the user runs on 2 Runs (1 to 2), chooses 
+//     the "Run" IOVMode and the SinceAppendMode, then he will have two objects
+//     stored on the DB, 
+//     Object 1 valid for Run 1
+//     Object 2 valid from Run 2 on (unless new objects are appended later)
+//       If instead he had run in Till mode, then he would get two objects 
+//     stored:
+//     Object 1 valid for Run 1
+//     Object 2 valid for Run 2 only.
+    
+//     WARNING: due to a current misfunctioning of the framework, if
+//     the *Since* mode is chosen, then it is highly recommended to store objects 
+//     in IOV chronological order. If you have 10 runs, then execute your application starting from run 1 and not for example in two steps: first from Run 6 to Run 10 and then from Run 1 to Run 6.
+  
+
+// 3)string Record 
+
+// this is the eventsetup record of your object.
+
+
+
 //
 // Original Author:  Giacomo Bruno
 //         Created:  May 23 10:04:31 CET 2007
-// $Id: ConditionDBWriter.h,v 1.3 2007/05/09 16:10:13 gbruno Exp $
+// $Id: ConditionDBWriter.h,v 1.1 2007/05/25 17:50:53 gbruno Exp $
 //
 //
 
@@ -66,7 +177,7 @@ public:
   }
 
 
-  ~ConditionDBWriter(){
+  virtual ~ConditionDBWriter(){
   
     edm::LogInfo("ConditionDBWriter::~ConditionDBWriter()") << std::endl;
 
@@ -81,19 +192,19 @@ private:
   virtual T * getNewObject()=0;
 
 
-  // Optional methods that may be implemented in the derived classes if needed
+  // Optional methods that may be implemented (technically "overridden") in the derived classes if needed
 
-  //Called at the beginning of the job
+  //Will be called at the beginning of the job
   virtual void algoBeginJob(const edm::EventSetup&){};
-  //Called at the beginning of each run in the job
+  //Will be called at the beginning of each run in the job
   virtual void algoBeginRun(const edm::Run &, const edm::EventSetup &){};
-  //Called at the beginning of each luminosity block in the run
+  //Will be called at the beginning of each luminosity block in the run
   virtual void algoBeginLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup &){};
-  //called at every event
+  //Will be called at every event
   virtual void algoAnalyze(const edm::Event&, const edm::EventSetup&){};
-  //Called at the end of each run in the job
+  //Will be called at the end of each run in the job
   virtual void algoEndRun(const edm::Run &, const edm::EventSetup &){};
-  //Called at the end of the job
+  //Will be called at the end of the job
   virtual void algoEndJob(){};
 
 
