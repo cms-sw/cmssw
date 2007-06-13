f83e191668efd035cb3734af331a73e7093357ab
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-06-13 12:39:26 +0000

    first commit

---
+++ CondFormats/DataRecord/interface/SiStripBadStripRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_SiStripBadStripRcd_h
+#define DataRecord_SiStripBadStripRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiStripBadStripRcd
+// 
+/**\class SiStripBadStripRcd SiStripBadStripRcd.h CondFormats/DataRecord/interface/SiStripBadStripRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Wed May 16 17:15:57 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripBadStripRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripBadStripRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/interface/SiStripModuleHVRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_SiStripModuleHVRcd_h
+#define DataRecord_SiStripModuleHVRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiStripModuleHVRcd
+// 
+/**\class SiStripModuleHVRcd SiStripModuleHVRcd.h CondFormats/DataRecord/interface/SiStripModuleHVRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Wed May 16 17:15:57 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripModuleHVRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripModuleHVRcd> {};
+
+#endif
