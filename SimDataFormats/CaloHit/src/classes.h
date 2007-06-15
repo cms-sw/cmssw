7e4296c83f533aa60cf0da0321f3084c89325976
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-06-15 14:05:40 +0000

    Correct the classes.h which was mistakenly put wrong last time

--- SimDataFormats/CaloHit/src/classes.h
+++ SimDataFormats/CaloHit/src/classes.h
@@ -1,19 +1,19 @@
-#include "SimDataFormats/CaloHit/interface/HFShowerPhoton.h"
 #include "SimDataFormats/CaloHit/interface/HFShowerLibraryEventInfo.h"
+#include "SimDataFormats/CaloHit/interface/HFShowerPhoton.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   namespace {
-    HFShowerPhoton              dummy22;
-    std::vector<HFShowerPhoton> dummy222;
-    edm::Wrapper<std::vector<HFShowerPhoton> > dummy2222;
-    HFShowerLibraryEventInfo    dummy33;
-    std::vector<HFShowerLibraryEventInfo> dummy333;
-    edm::Wrapper<std::vector<HFShowerLibraryEventInfo> > dummy3333;
-    PCaloHit                    dummy44;
-    edm::PCaloHitContainer      dummy777;
-    edm::Wrapper<edm::PCaloHitContainer> dummy7777;
+    HFShowerLibraryEventInfo                             rv1;
+    std::vector<HFShowerLibraryEventInfo>                v1;
+    edm::Wrapper<std::vector<HFShowerLibraryEventInfo> > wc1;
+    HFShowerPhoton                                       rv2;
+    std::vector<HFShowerPhoton>                          v2;
+    edm::Wrapper<std::vector<HFShowerPhoton> >           wc2;
+    PCaloHit                                             rv3;
+    edm::PCaloHitContainer                               v3;
+    edm::Wrapper<edm::PCaloHitContainer>                 wc3;
   }
 }
