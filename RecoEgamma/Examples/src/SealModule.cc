c4d3d8bc3409f76ad81f72f26aa15fddc7096cb9
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-06-17 22:26:46 +0000

    special commit for electron ID in CMSSW_131

--- RecoEgamma/Examples/src/SealModule.cc
+++ RecoEgamma/Examples/src/SealModule.cc
@@ -3,29 +3,9 @@
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
-#include "RecoEgamma/Examples/interface/SiStripElectronAnalyzer.h"
-#include "RecoEgamma/Examples/interface/ElectronAnalyzer.h"
-#include "RecoEgamma/Examples/interface/ElectronPixelSeedAnalyzer.h"
-#include "RecoEgamma/Examples/interface/PixelMatchElectronAnalyzer.h"
-#include "RecoEgamma/Examples/interface/PixelMatchGsfElectronAnalyzer.h"
-#include "RecoEgamma/Examples/interface/SimplePhotonAnalyzer.h"
-#include "RecoEgamma/Examples/interface/SimpleConvertedPhotonAnalyzer.h"
 #include "RecoEgamma/Examples/interface/ElectronIDAnalyzer.h"
-#include "RecoEgamma/Examples/interface/MCPhotonAnalyzer.h"
-#include "RecoEgamma/Examples/interface/MCElectronAnalyzer.h"
-#include "RecoEgamma/Examples/interface/MCPizeroAnalyzer.h"
 
 
 //DEFINE_SEAL_MODULE();
 
-DEFINE_FWK_MODULE(SiStripElectronAnalyzer);
-DEFINE_FWK_MODULE(ElectronAnalyzer);
-DEFINE_FWK_MODULE(PixelMatchElectronAnalyzer);
-DEFINE_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
-DEFINE_FWK_MODULE(ElectronPixelSeedAnalyzer);
-DEFINE_FWK_MODULE(SimplePhotonAnalyzer);
-DEFINE_FWK_MODULE(SimpleConvertedPhotonAnalyzer);
 DEFINE_FWK_MODULE(ElectronIDAnalyzer);
-DEFINE_FWK_MODULE(MCPhotonAnalyzer);
-DEFINE_FWK_MODULE(MCElectronAnalyzer);
-DEFINE_FWK_MODULE(MCPizeroAnalyzer);
