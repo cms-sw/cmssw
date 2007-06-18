7b92611fbe017d8f93fc0bc3b52a6b7abeed8e15
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-06-18 13:52:16 +0000

    doxygen

--- DataFormats/TrackReco/interface/DeDxHit.h
+++ DataFormats/TrackReco/interface/DeDxHit.h
@@ -1,14 +1,19 @@
 #ifndef TrackDeDxHits_H
 #define TrackDeDxHits_H
 namespace reco {
+/**
+ * Class defining the dedx hits, i.e. track hits with only dedx need informations 
+ */
 class DeDxHit {
 public:
   DeDxHit() {}
   DeDxHit(float ch,float dist,int detId):
   m_charge(ch),m_distance(dist),m_subDetId(detId) {}
 
-  //Return the angle and thick normalized, calibrated energy release
+  ///Return the angle and thick normalized, calibrated energy release
   float charge() const {return m_charge;}
+
+  ///Return the distance of the hit from the interaction point
   float distance() const {return m_distance;}
   bool operator< (const DeDxHit & other) const {return m_charge < other.m_charge; }
 
