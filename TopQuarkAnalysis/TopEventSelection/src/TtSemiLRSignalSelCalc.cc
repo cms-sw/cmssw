d1851512e07eb9a297a032e2d9e676c298618474
heyninck <sha1-082da020e7327f374b7da66709d8d266b70884a2@cern.ch>
2007-06-18 14:12:18 +0000

    bug fix

--- TopQuarkAnalysis/TopEventSelection/src/TtSemiLRSignalSelCalc.cc
+++ TopQuarkAnalysis/TopEventSelection/src/TtSemiLRSignalSelCalc.cc
@@ -2,7 +2,7 @@
 // Author:  Jan Heyninck
 // Created: Tue Apr  3 17:33:23 PDT 2007
 //
-// $Id: TtSemiLRSignalSelCalc.cc,v 1.1 2007/06/01 09:10:19 heyninck Exp $
+// $Id: TtSemiLRSignalSelCalc.cc,v 1.1 2007/06/15 08:49:32 heyninck Exp $
 //
 #include "TopQuarkAnalysis/TopEventSelection/interface/TtSemiLRSignalSelCalc.h"
 
@@ -28,16 +28,15 @@ void  TtSemiLRSignalSelCalc::operator()(TtSemiEvtSolution & sol){
   // find the used observables
   std::vector<double> obsVals;
   for(unsigned int o = 0; o<100; o++){
-    if( myLR->obsFitIncluded(o) ) { std::cout<<"uses obs value of obs"<<o<<std::endl; obsVals.push_back(sol.getLRSignalEvtObsVal(o)); };
+    if( myLR->obsFitIncluded(o) ) obsVals.push_back(sol.getLRSignalEvtObsVal(o)); 
   }
   
   // calculate the logLR and the purity
   double logLR = myLR->calcLRval(obsVals);
   double prob  = -999.;
-  if(addPurity) myLR->calcProb(logLR);
+  if(addPurity) prob = myLR->calcProb(logLR);
   
   // fill these values to the members in the TtSemiEvtSolution
   sol.setLRSignalEvtLRval(logLR);
   sol.setLRSignalEvtProb(prob);
-  std::cout<<"  Found logLR = "<<logLR<<" and Prob = "<<prob<<std::endl;
 }
--- TopQuarkAnalysis/TopJetCombination/src/TtSemiLRJetCombCalc.cc
+++ TopQuarkAnalysis/TopJetCombination/src/TtSemiLRJetCombCalc.cc
@@ -2,7 +2,7 @@
 // Author:  Jan Heyninck
 // Created: Tue Apr  3 17:33:23 PDT 2007
 //
-// $Id: TtSemiLRJetCombCalc.cc,v 1.2 2007/06/09 01:17:40 lowette Exp $
+// $Id: TtSemiLRJetCombCalc.cc,v 1.3 2007/06/15 08:53:54 heyninck Exp $
 //
 #include "TopQuarkAnalysis/TopJetCombination/interface/TtSemiLRJetCombCalc.h"
 
@@ -28,16 +28,15 @@ void  TtSemiLRJetCombCalc::operator()(TtSemiEvtSolution & sol){
   // find the used observables
   std::vector<double> obsVals;
   for(unsigned int o = 0; o<100; o++){
-    if( myLR->obsFitIncluded(o) ) { std::cout<<"uses obs value of obs"<<o<<std::endl; obsVals.push_back(sol.getLRSignalEvtObsVal(o)); };
+    if( myLR->obsFitIncluded(o) ) {obsVals.push_back(sol.getLRJetCombObsVal(o)); };
   }
   
   // calculate the logLR and the purity
   double logLR = myLR->calcLRval(obsVals);
   double prob  = -999.;
-  if(addPurity) myLR->calcProb(logLR);
+  if(addPurity) prob = myLR->calcProb(logLR);
   
   // fill these values to the members in the TtSemiEvtSolution
   sol.setLRJetCombLRval(logLR);
   sol.setLRJetCombProb(prob);
-  std::cout<<"  Found logLR = "<<logLR<<" and Prob = "<<prob<<std::endl;
 }
