ef2f0f5bcc63a125a85528d85c1b7955e466c654
heyninck <sha1-082da020e7327f374b7da66709d8d266b70884a2@cern.ch>
2007-06-18 11:53:25 +0000

    back to previous version till for all depending files an alternative is available

--- TopQuarkAnalysis/TopEventProducers/interface/TtGenEventReco.h
+++ TopQuarkAnalysis/TopEventProducers/interface/TtGenEventReco.h
@@ -1,26 +1,50 @@
-#include <memory>
-#include <string>
-#include <vector>
-#include <map>
+// -*- C++ -*-
+//
+// Package:    TtGenEventReco
+// Class:      TtGenEventReco
+// 
+/**\class TtGenEventReco TtGenEventReco.cc AnalysisDataFormats/TopObjectsProducers/src/TtGenEventReco.cc
 
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Jan Heyninck
+//         Created:  Tue Apr  10 12:01:49 CEST 2007
+// $Id: TtGenEventReco.h,v 1.1 2007/05/02 15:10:44 lowette Exp $
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "AnalysisDataFormats/TopObjects/interface/TtGenEvent.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
+#include <vector>
+
+using namespace std;
+using namespace edm;
+
+//
+// class decleration
+//
 
 class TtGenEventReco : public edm::EDProducer {
- public:
-  explicit TtGenEventReco(const edm::ParameterSet&);
-  ~TtGenEventReco();
-  
-  virtual void produce(edm::Event&, const edm::EventSetup&);
-  void fillOutput(const reco::CandidateCollection&, reco::CandidateCollection&);
-  void fillRefs(const reco::CandidateRefProd&, reco::CandidateCollection&);
- private:
-  edm::InputTag src_;  
-  std::map<int,std::vector<int> > refs_;
+   public:
+      explicit TtGenEventReco(const edm::ParameterSet&);
+      ~TtGenEventReco();
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+   private:
 };
--- TopQuarkAnalysis/TopEventProducers/src/TtGenEventReco.cc
+++ TopQuarkAnalysis/TopEventProducers/src/TtGenEventReco.cc
@@ -1,98 +1,126 @@
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtGenEventReco.h"
 
-using namespace std;
-using namespace reco;
-
-static const int stat = 3;
-static const int tId  = 6;
-static const int bId  = 5;
-static const int WId  =24;
-
-TtGenEventReco::TtGenEventReco(const edm::ParameterSet& cfg):
-  src_ ( cfg.getParameter<edm::InputTag>( "src" ) )
+//
+// constructors and destructor
+//
+TtGenEventReco::TtGenEventReco(const edm::ParameterSet& iConfig)
 {
-  produces<reco::CandidateCollection>();
+   produces<TtGenEvent>();
 }
+
 
 TtGenEventReco::~TtGenEventReco()
 {
 }
 
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
 void
-TtGenEventReco::produce(edm::Event& evt, const edm::EventSetup& setup)
+TtGenEventReco::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {     
-  edm::Handle<reco::CandidateCollection> src;
-  evt.getByLabel(src_, src);
- 
-  const reco::CandidateRefProd ref = evt.getRefBeforePut<reco::CandidateCollection>(); 
-  std::auto_ptr<reco::CandidateCollection> sel(new reco::CandidateCollection);
+  
+   // Get the vector of generated particles from the event
+   Handle<CandidateCollection> genParticles;
+   iEvent.getByLabel( "genParticleCandidates", genParticles );
+   if(genParticles->size() == 0) cout<<"No GenParticle Candidates found..."<<endl;  
+   
+   // search top quarks
+   vector<const Candidate *> tvec;
+   for( size_t p=0; (p<genParticles->size() && tvec.size()<2); p++) {
+     if(status((*genParticles)[p]) == 3 && abs((*genParticles)[p].pdgId()) == 6) tvec.push_back(&((*genParticles)[p]));
+   }
+   //cout<<"found "<<tvec.size()<<" top quarks..."<<endl; 
+   if(tvec.size()<2) cout<<"No ttbar process!"<<endl;  
+  
+   
+   // search W-bosons and b-quarks
+   vector<const Candidate *> Wvec, bvec;
+   if(tvec.size() == 2){
+     for( size_t t=0; t<tvec.size(); t++) {
+       for( size_t d=0; d<tvec[t]->numberOfDaughters(); d++) {
+         if(status(*tvec[t]->daughter(d)) == 3 && abs(tvec[t]->daughter(d)->pdgId()) == 24) Wvec.push_back(tvec[t]->daughter(d));
+         if(status(*tvec[t]->daughter(d)) == 3 && abs(tvec[t]->daughter(d)->pdgId()) ==  5) bvec.push_back(tvec[t]->daughter(d));
+       }
+     }
+   }
+   //cout<<"found "<<Wvec.size()<<" W bosons..."<<endl;   
+   //cout<<"found "<<bvec.size()<<" b quarks..."<<endl;   
+   
+   
+   // search W-decay products
+   int Whadr = 0;
+   vector<const Candidate *> qvec, lvec, nvec;
+   if(Wvec.size() == 2 && bvec.size() == 2){
+     for( size_t w=0; w<tvec.size(); w++) {
+       for( size_t d=0; d<Wvec[w]->numberOfDaughters(); d++) {
+         if(status(*Wvec[w]->daughter(d)) == 3 && abs(Wvec[w]->daughter(d)->pdgId()) < 5) {qvec.push_back(Wvec[w]->daughter(d)); Whadr = w;};
+         if(status(*Wvec[w]->daughter(d)) == 3 && abs(Wvec[w]->daughter(d)->pdgId()) ==  11 || abs(Wvec[w]->daughter(d)->pdgId()) ==  13 || abs(Wvec[w]->daughter(d)->pdgId()) ==  15) lvec.push_back(Wvec[w]->daughter(d));
+         if(status(*Wvec[w]->daughter(d)) == 3 && abs(Wvec[w]->daughter(d)->pdgId()) ==  12 || abs(Wvec[w]->daughter(d)->pdgId()) ==  14 || abs(Wvec[w]->daughter(d)->pdgId()) ==  16) nvec.push_back(Wvec[w]->daughter(d));
+       }
+     }
+   } 
+   //cout<<"found "<<qvec.size()<<" light quarks..."<<endl; 
+   //cout<<"found "<<lvec.size()<<" charged leptons..."<<endl; 
+   //cout<<"found "<<nvec.size()<<" neutrinos..."<<endl; 
 
-  //fill output collection
-  fillOutput( *src, *sel );
-  //fill references
-  fillRefs( ref, *sel );
 
-  evt.put( sel );
-}
+   // fill TtGenEvent object depending on decay
+   int decay = -999;
+   vector<const Candidate *> evtvec;
+   //  semilep
+   if(qvec.size() == 2 && lvec.size() == 1 && nvec.size() == 1){
+     decay = 1;
+     evtvec.push_back(qvec[0]); 
+     evtvec.push_back(qvec[1]); 
+     evtvec.push_back(bvec[Whadr]); 
+     evtvec.push_back(bvec[1-Whadr]); 
+     evtvec.push_back(lvec[0]);  
+     evtvec.push_back(nvec[0]); 
+     evtvec.push_back(Wvec[Whadr]);
+     evtvec.push_back(Wvec[1-Whadr]);
+     evtvec.push_back(tvec[Whadr]);
+     evtvec.push_back(tvec[1-Whadr]);
+   }	 
+   //fullylep
+   else if(qvec.size() == 0 && lvec.size() == 2 && nvec.size() == 2){
+     decay = 2;
+     evtvec.push_back(lvec[0]); 
+     evtvec.push_back(nvec[0]); 
+     evtvec.push_back(bvec[0]); 
+     evtvec.push_back(bvec[1]); 
+     evtvec.push_back(lvec[1]);  
+     evtvec.push_back(nvec[1]); 
+     evtvec.push_back(Wvec[0]);
+     evtvec.push_back(Wvec[1]);
+     evtvec.push_back(tvec[0]);
+     evtvec.push_back(tvec[1]);
+   }	
+   //fully hadr
+   else if(qvec.size() == 4 && lvec.size() == 0 && nvec.size() == 0){
+     decay = 0;
+     evtvec.push_back(qvec[0]); 
+     evtvec.push_back(qvec[1]); 
+     evtvec.push_back(bvec[0]); 
+     evtvec.push_back(bvec[1]); 
+     evtvec.push_back(qvec[2]);  
+     evtvec.push_back(qvec[3]); 
+     evtvec.push_back(Wvec[0]);
+     evtvec.push_back(Wvec[1]);
+     evtvec.push_back(tvec[0]);
+     evtvec.push_back(tvec[1]);
+   }
+   else {
+     cout<<"no semilep, fullylep or fullyhadr decay???"<<endl;
+   }
+   
+   // put genEvt object in Event
+   TtGenEvent * genEvt = new TtGenEvent(decay,evtvec);
+   auto_ptr<TtGenEvent> myTtGenEvent(genEvt);
+   iEvent.put(myTtGenEvent);
+   
 
-void TtGenEventReco::fillOutput(const reco::CandidateCollection& src, reco::CandidateCollection& sel)
-{
-  CandidateCollection::const_iterator t=src.begin();
-  for(int idx=-1; t!=src.end(); ++t){
-    if( status( *t )==stat && abs( t->pdgId() )==tId ){
-      GenParticleCandidate* cand = new GenParticleCandidate( t->charge(), t->p4(), 
-							     t->vertex(), t->pdgId(), status( *t ) );
-      auto_ptr<reco::Candidate> ptr( cand );
-      sel.push_back( ptr );
-      ++idx;
-      int inode=idx, ileaf=0;
-      vector<int> node, leaf;
-      Candidate::const_iterator td=t->begin();
-      for( ; td!=t->end(); ++td){
-	if( status( *td )==stat && abs( td->pdgId() )==bId ){
-	  GenParticleCandidate* cand = new GenParticleCandidate( td->charge(), td->p4(), 
-								 td->vertex(), td->pdgId(), status( *td ) );
-	  auto_ptr<Candidate> ptr( cand );
-	  sel.push_back( ptr );
-	  node.push_back( ++idx ); 
-	}
-	if( status( *td )==stat && abs( td->pdgId() )==WId ){
-	  GenParticleCandidate* cand = new GenParticleCandidate( td->charge(), td->p4(), 
-								 td->vertex(), td->pdgId(), status( *td ) );
-	  auto_ptr<Candidate> ptr( cand );
-	  sel.push_back( ptr );
-	  node.push_back( ++idx ); 
-	  ileaf=idx;
-	  Candidate::const_iterator wd=td->begin();
-	  for( ; wd!=td->end(); ++wd){
-	    GenParticleCandidate* cand = new GenParticleCandidate( wd->charge(), wd->p4(), 
-								   wd->vertex(), wd->pdgId(), status( *wd ) );
-	    auto_ptr<Candidate> ptr( cand );
-	    sel.push_back( ptr );
-	    leaf.push_back( ++idx );
-	  }
-	}
-      }
-      refs_[ inode ]=node;
-      refs_[ ileaf ]=leaf;
-    }
-  }
-}
-
-void TtGenEventReco::fillRefs(const reco::CandidateRefProd& ref, reco::CandidateCollection& sel)
-{ 
-  CandidateCollection::iterator p=sel.begin();
-  for(int idx=0; p!=sel.end(); ++p, ++idx){
-    map<int, vector<int> >::const_iterator daughters=refs_.find( idx );
-    if( daughters!=refs_.end() ){
-      vector<int>::const_iterator daughter = daughters->second.begin();
-      for( ; daughter!=daughters->second.end(); ++daughter){
-	GenParticleCandidate* part = dynamic_cast<GenParticleCandidate* > (&(*p));
-	if(part == 0){
-	  throw edm::Exception( edm::errors::InvalidReference, "Not a GenParticleCandidate" );
-	}
-	part->addDaughter( CandidateRef(ref, *daughter) );
-      }
-    }
-  }
 }
