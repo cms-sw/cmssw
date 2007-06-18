3795c12de435eb1dbbdf42d45d643a262760a39f
mthomas <sha1-c3ddb5d317a38815e0d99b0f7ef83c94092833ba@cern.ch>
2007-06-18 09:22:37 +0000

    added forgotten code

--- Alignment/LaserAlignment/test/RecoAnalyzerRecHits.cc
+++ Alignment/LaserAlignment/test/RecoAnalyzerRecHits.cc
@@ -1,8 +1,8 @@
 /** \file RecoAnalyzerRecHits.cc
 *  plots for RecHits
   *
-  *  $Date: 2007/06/17 13:36:54 $
-  *  $Revision: 1.5 $
+  *  $Date: 2007/06/18 08:57:41 $
+  *  $Revision: 1.6 $
   *  \author Maarten Thomas
  */
 
@@ -222,6 +222,155 @@
       // get the DetUnit
     const StripGeomDetUnit* const theStripDet = dynamic_cast<const StripGeomDetUnit*>(theTracker.idToDet((*detId_iter)));
 
+        // some variables we need later on in the program
+      int theBeam     = 0;
+      int theRing     = 0;
+      std::string thePart  = "";
+      int theTIBLayer = 0;
+      int theTOBLayer = 0;
+      int theTECWheel = 0;
+      int theTOBStereoDet = 0;
+
+      switch ((*detId_iter).subdetId())
+      {
+        case StripSubdetector::TIB:
+        {
+          TIBDetId theTIBDetId((*detId_iter).rawId());
+          thePart = "TIB";
+          theTIBLayer = theTIBDetId.layer();
+          break;
+        }
+        case StripSubdetector::TOB:
+        {
+          TOBDetId theTOBDetId((*detId_iter).rawId());
+          thePart = "TOB";
+          theTOBLayer = theTOBDetId.layer();
+          theTOBStereoDet = theTOBDetId.stereo();
+          break;
+        }
+        case StripSubdetector::TEC:
+        {
+          TECDetId theTECDetId((*detId_iter).rawId());
+
+        // is this module in TEC+ or TEC-?
+          if (theTECDetId.side() == 1) { thePart = "TEC-"; }
+          else if (theTECDetId.side() == 2) { thePart = "TEC+"; }
+
+        // in which ring is this module?
+          if ( theStripDet->surface().position().perp() > 55.0 && theStripDet->surface().position().perp() < 59.0 )
+            { theRing = 4; } // Ring 4
+          else if ( theStripDet->surface().position().perp() > 81.0 && theStripDet->surface().position().perp() < 85.0 )
+            { theRing = 6; } // Ring 6
+          else
+            { theRing = -1; } // probably not a Laser Hit!
+
+        // on which disk is this module
+          theTECWheel = theTECDetId.wheel();
+          break;
+        }
+      }
+
+        // which beam belongs these digis to
+      if ( thePart == "TIB" && theTIBLayer == 4 )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTIB))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.29 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 1.29 + theSearchPhiTIB))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.85 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 1.85 + theSearchPhiTIB))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.75 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 2.75 + theSearchPhiTIB))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTIB))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -2.00 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -2.00 + theSearchPhiTIB))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTIB))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTIB))    { theBeam = 7; } // beam 7
+        else
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
+      else if ( thePart == "TOB" && theTOBLayer == 1 )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTOB) 
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTOB))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.29 - theSearchPhiTOB) 
+          && (theStripDet->surface().position().phi() < 1.29 + theSearchPhiTOB))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.85 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < 1.85 + theSearchPhiTOB))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.75 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < 2.75 + theSearchPhiTOB))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTOB))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -2.00 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -2.00 + theSearchPhiTOB))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTOB))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTOB))    { theBeam = 7; } // beam 7
+        else
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
+      else if ( thePart == "TEC+" || thePart == "TEC-" )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTEC))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.18 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.18 + theSearchPhiTEC))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.96 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.96 + theSearchPhiTEC))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.74 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 2.74 + theSearchPhiTEC))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.74 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -2.74 + theSearchPhiTEC))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -1.96 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.96 + theSearchPhiTEC))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.18 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.18 + theSearchPhiTEC))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.39 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -0.39 + theSearchPhiTEC))    { theBeam = 7; } // beam 7
+
+        else if ( (theStripDet->surface().position().phi() > 1.28 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.28 + theSearchPhiTEC))     { theBeam = 21; } // beam 1 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > 1.84 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.84 + theSearchPhiTEC))     { theBeam = 22; } // beam 2 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTEC))    { theBeam = 24; } // beam 4 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTEC))    { theBeam = 26; } // beam 6 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTEC))    { theBeam = 27; } // beam 7 TEC2TEC
+        else 
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
+
       // get the RecHits
     SiStripRecHit2DCollection::range rechitRange = theRPhiRecHitCollection->get((*detId_iter));
     SiStripRecHit2DCollection::const_iterator rechitRangeIteratorBegin = rechitRange.first;
@@ -254,6 +403,155 @@
   {
       // get the DetUnit
     const StripGeomDetUnit* const theStripDet = dynamic_cast<const StripGeomDetUnit*>(theTracker.idToDet((*detId_iter)));
+
+        // some variables we need later on in the program
+      int theBeam     = 0;
+      int theRing     = 0;
+      std::string thePart  = "";
+      int theTIBLayer = 0;
+      int theTOBLayer = 0;
+      int theTECWheel = 0;
+      int theTOBStereoDet = 0;
+
+      switch ((*detId_iter).subdetId())
+      {
+        case StripSubdetector::TIB:
+        {
+          TIBDetId theTIBDetId((*detId_iter).rawId());
+          thePart = "TIB";
+          theTIBLayer = theTIBDetId.layer();
+          break;
+        }
+        case StripSubdetector::TOB:
+        {
+          TOBDetId theTOBDetId((*detId_iter).rawId());
+          thePart = "TOB";
+          theTOBLayer = theTOBDetId.layer();
+          theTOBStereoDet = theTOBDetId.stereo();
+          break;
+        }
+        case StripSubdetector::TEC:
+        {
+          TECDetId theTECDetId((*detId_iter).rawId());
+
+        // is this module in TEC+ or TEC-?
+          if (theTECDetId.side() == 1) { thePart = "TEC-"; }
+          else if (theTECDetId.side() == 2) { thePart = "TEC+"; }
+
+        // in which ring is this module?
+          if ( theStripDet->surface().position().perp() > 55.0 && theStripDet->surface().position().perp() < 59.0 )
+            { theRing = 4; } // Ring 4
+          else if ( theStripDet->surface().position().perp() > 81.0 && theStripDet->surface().position().perp() < 85.0 )
+            { theRing = 6; } // Ring 6
+          else
+            { theRing = -1; } // probably not a Laser Hit!
+
+        // on which disk is this module
+          theTECWheel = theTECDetId.wheel();
+          break;
+        }
+      }
+
+        // which beam belongs these digis to
+      if ( thePart == "TIB" && theTIBLayer == 4 )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTIB))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.29 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 1.29 + theSearchPhiTIB))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.85 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 1.85 + theSearchPhiTIB))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.75 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < 2.75 + theSearchPhiTIB))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTIB))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -2.00 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -2.00 + theSearchPhiTIB))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTIB))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTIB) 
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTIB))    { theBeam = 7; } // beam 7
+        else
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
+      else if ( thePart == "TOB" && theTOBLayer == 1 )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTOB) 
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTOB))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.29 - theSearchPhiTOB) 
+          && (theStripDet->surface().position().phi() < 1.29 + theSearchPhiTOB))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.85 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < 1.85 + theSearchPhiTOB))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.75 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < 2.75 + theSearchPhiTOB))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTOB))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -2.00 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -2.00 + theSearchPhiTOB))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTOB))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTOB)
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTOB))    { theBeam = 7; } // beam 7
+        else
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
+      else if ( thePart == "TEC+" || thePart == "TEC-" )
+      {
+        if ( (theStripDet->surface().position().phi() > 0.39 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 0.39 + theSearchPhiTEC))          { theBeam = 0; } // beam 0 
+
+        else if ( (theStripDet->surface().position().phi() > 1.18 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.18 + theSearchPhiTEC))     { theBeam = 1; } // beam 1
+
+        else if ( (theStripDet->surface().position().phi() > 1.96 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.96 + theSearchPhiTEC))     { theBeam = 2; } // beam 2
+
+        else if ( (theStripDet->surface().position().phi() > 2.74 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 2.74 + theSearchPhiTEC))     { theBeam = 3; } // beam 3
+
+        else if ( (theStripDet->surface().position().phi() > -2.74 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -2.74 + theSearchPhiTEC))    { theBeam = 4; } // beam 4
+
+        else if ( (theStripDet->surface().position().phi() > -1.96 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.96 + theSearchPhiTEC))    { theBeam = 5; } // beam 5
+
+        else if ( (theStripDet->surface().position().phi() > -1.18 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.18 + theSearchPhiTEC))    { theBeam = 6; } // beam 6
+
+        else if ( (theStripDet->surface().position().phi() > -0.39 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -0.39 + theSearchPhiTEC))    { theBeam = 7; } // beam 7
+
+        else if ( (theStripDet->surface().position().phi() > 1.28 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.28 + theSearchPhiTEC))     { theBeam = 21; } // beam 1 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > 1.84 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < 1.84 + theSearchPhiTEC))     { theBeam = 22; } // beam 2 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -2.59 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -2.59 + theSearchPhiTEC))    { theBeam = 24; } // beam 4 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -1.10 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -1.10 + theSearchPhiTEC))    { theBeam = 26; } // beam 6 TEC2TEC
+
+        else if ( (theStripDet->surface().position().phi() > -0.50 - theSearchPhiTEC)
+          && (theStripDet->surface().position().phi() < -0.50 + theSearchPhiTEC))    { theBeam = 27; } // beam 7 TEC2TEC
+        else 
+          { theBeam = -1; } // probably not a Laser Hit!
+      }
 
       // get the RecHits
     SiStripRecHit2DCollection::range rechitRange = theStereoRecHitCollection->get((*detId_iter));
