b8821ba03766124f4d7111cef9add4d310cdee42
Dimitrios Tsirigkas <sha1-7a25099d73dbb3e60e5b1352f9ab2dd1067a4a69@cern.ch>
2007-06-19 09:55:39 +0000

    first version of the PFClusterShapes

---
+++ RecoParticleFlow/PFClusterShapeProducer/src/SealModule.cc
@@ -0,0 +1,6 @@
+#include "PluginManager/ModuleDef.h"
+#include "RecoParticleFlow/PFClusterShapeProducer/interface/PFClusterShapeProducer.h"
+
+DEFINE_SEAL_MODULE();
+
+DEFINE_ANOTHER_FWK_MODULE(PFClusterShapeProducer);
