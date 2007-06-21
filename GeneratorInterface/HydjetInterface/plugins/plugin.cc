d0323d52d6fe5efb0c1be8b0b9cf4648c7e741e1
mballint <sha1-2ecf5fbdb47e0df5b2667dea89dfc563e54addeb@cern.ch>
2007-06-21 13:56:00 +0000

    adapt to EDM_PLUGINS.

---
+++ GeneratorInterface/HydjetInterface/plugins/plugin.cc
@@ -0,0 +1,10 @@
+//
+// $Id:$
+//
+
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "GeneratorInterface/HydjetInterface/interface/HydjetSource.h"
+
+using edm::HydjetSource;
+
+DEFINE_FWK_INPUT_SOURCE(HydjetSource);
---
+++ GeneratorInterface/PyquenInterface/plugins/plugin.cc
@@ -0,0 +1,11 @@
+//
+// $Id:$
+//
+
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "GeneratorInterface/PyquenInterface/interface/PyquenSource.h"
+
+
+using edm::PyquenSource;
+
+DEFINE_FWK_INPUT_SOURCE(PyquenSource);
