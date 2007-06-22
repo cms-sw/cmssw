617e4a0bccb19d454a4cf60dcaf841c33b8aeb3d
Nhan Viet Tran <sha1-8a19d334a4648fb26ba9e7879a577ce46f3df51a@cern.ch>
2007-06-22 17:32:08 +0000

    get rid of if statement for firstdet issue

--- Alignment/SurveyAnalysis/plugins/SurveyInputTrackerFromDB.cc
+++ Alignment/SurveyAnalysis/plugins/SurveyInputTrackerFromDB.cc
@@ -14,6 +14,8 @@ SurveyInputTrackerFromDB::SurveyInputTrackerFromDB(const edm::ParameterSet& cfg)
 
 void SurveyInputTrackerFromDB::beginJob(const edm::EventSetup& setup)
 {
+	std::cout << "***************ENTERING BEGIN JOB******************" << std::endl;
+	
   edm::ESHandle<DDCompactView> view;
   edm::ESHandle<GeometricDet>  geom;
 
@@ -27,55 +29,57 @@ void SurveyInputTrackerFromDB::beginJob(const edm::EventSetup& setup)
 	SurveyInputTextReader dataReader;
   dataReader.readFile( textFileName );
 	uIdMap = dataReader.UniqueIdMap();
-	
+
 	addComponent( new AlignableTracker(&*geom, tracker) );
   addSurveyInfo( detector() );
+	std::cout << "*************END BEGIN JOB***************" << std::endl;
 }
 
 void SurveyInputTrackerFromDB::addSurveyInfo(Alignable* ali)
 {
   const std::vector<Alignable*>& comp = ali->components();
-
+	//std::cout << "in addSurveyInfo" << std::endl;
   unsigned int nComp = comp.size();
-
+	//std::cout << "alOjbId: " << ali->alignableObjectId();
+	//std::cout << " , detId: " << ali->geomDetId().rawId() << std::endl;
+	
   for (unsigned int i = 0; i < nComp; ++i) addSurveyInfo(comp[i]);
-
+	
 	static TrackerAlignableId uid;
 	SurveyInputTextReader::MapType::const_iterator it;
 	align::ErrorMatrix error;
-	if (ali->alignableObjectId() != AlignableObjectId::AlignableDetUnit){
-		TrackerAlignableId::UniqueId id = uid.alignableUniqueId(ali);
-		std::cout << "UniqueId: " << id.first << ", " << id.second << std::endl;
-		it = uIdMap.find(id);
-		if (it != uIdMap.end()){
-			
-			const std::vector<float>& parameters = (it)->second;
-			
-			//move the surface
-			//displacement
-			LocalVector lvector (parameters[0], parameters[1], parameters[2]);
-			GlobalVector gvector = ali->surface().toGlobal(lvector);
-			ali->move(gvector);
-			//rotation
-			const Basic3DVector<float> rot_aa(parameters[3], parameters[4], parameters[5]);
-			align::RotationType rotation(rot_aa, rot_aa.mag());
-			ali->rotateInLocalFrame(rotation);
-			
-			//sets the errors for the hierarchy level
-			double* errorData = error.Array();
-			for (int i = 0; i < 21; i++){errorData[i] = parameters[i+6];}
-			//std::cout << "error: " << std::endl << error << std::endl;
-			ali->setSurvey( new SurveyDet(ali->surface(), error*(1e-3)) );
-		}
-		else {
-			error = ROOT::Math::SMatrixIdentity();
-			ali->setSurvey( new SurveyDet(ali->surface(), error * 1e-3) );
-		}
+	
+	//if (ali->alignableObjectId() != AlignableObjectId::AlignableDetUnit){
+		
+	TrackerAlignableId::UniqueId id = uid.alignableUniqueId(ali);
+	std::cout << "UniqueId: " << id.first << ", " << id.second << std::endl;
+	it = uIdMap.find(id);
+	std::cout << "itID: " << it->first.first << ", " << it->first.second << std::endl;
+	if (it != uIdMap.end()){
+		
+		const std::vector<float>& parameters = (it)->second;
+		
+		//move the surface
+		//displacement
+		LocalVector lvector (parameters[0], parameters[1], parameters[2]);
+		GlobalVector gvector = ali->surface().toGlobal(lvector);
+		ali->move(gvector);
+		//rotation
+		const Basic3DVector<float> rot_aa(parameters[3], parameters[4], parameters[5]);
+		align::RotationType rotation(rot_aa, rot_aa.mag());
+		ali->rotateInLocalFrame(rotation);
+		
+		//sets the errors for the hierarchy level
+		double* errorData = error.Array();
+		for (int i = 0; i < 21; i++){errorData[i] = parameters[i+6];}
+
+		ali->setSurvey( new SurveyDet(ali->surface(), error*(1e-6)) );
 	}
-	else{
+	else {
 		error = ROOT::Math::SMatrixIdentity();
-		ali->setSurvey( new SurveyDet(ali->surface(), error * 1e-3) );
+		ali->setSurvey( new SurveyDet(ali->surface(), error * 1e-6) );
 	}
+		
 }
 // Plug in to framework
 
