3995ab2fe1582b82e162370d61923f70944b89c9
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2007-06-22 12:29:33 +0000

    add ESRawToDigiTB

--- EventFilter/ESRawToDigi/src/SealModule.cc
+++ EventFilter/ESRawToDigi/src/SealModule.cc
@@ -3,8 +3,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigi.h"
+#include "EventFilter/ESRawToDigi/interface/ESRawToDigiTB.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
+DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiTB);
 
 
