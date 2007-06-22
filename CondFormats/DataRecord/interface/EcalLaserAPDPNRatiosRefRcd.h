c90357ff8820dfb6901c96de34bd4a0a938e9053
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2007-06-22 13:30:20 +0000

    ECAL laser objects definition by me and Vladlen

---
+++ CondFormats/DataRecord/interface/EcalLaserAPDPNRatiosRefRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_EcalLaserAPDPNRatiosRefRcd_h
+#define DataRecord_EcalLaserAPDPNRatiosRefRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     EcalLaserAPDPNRatiosRefRcd
+// 
+/**\class EcalLaserAPDPNRatiosRefRcd EcalLaserAPDPNRatiosRefRcd.h CondFormats/DataRecord/interface/EcalLaserAPDPNRatiosRefRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Fri Jun  1 12:31:01 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class EcalLaserAPDPNRatiosRefRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalLaserAPDPNRatiosRefRcd> {};
+
+#endif
