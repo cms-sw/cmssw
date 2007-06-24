d88ec80716bf8c04c0199c116481d93f0b5bfd2e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-06-24 14:40:54 +0000

    direct access to item

--- CondFormats/EcalObjects/interface/EcalPedestals.h
+++ CondFormats/EcalObjects/interface/EcalPedestals.h
@@ -25,16 +25,17 @@ class EcalPedestals {
     float rms_x1;
 
   public:
-    float * mean_rms(int i) {
+    float const * mean_rms(int i) const {
       if (i==0) return &zero.z1;
       return (&mean_x12)+(2*(i-1));
     }
     
-    float mean(int i) {
+    float mean(int i) const {
       if (i==0) return 0.;
       return *(&mean_x12+(2*(i-1)));
     }
-    float rms(int i) {
+
+    float rms(int i) const {
       if (i==0) return 0.;
       return *(&rms_x12+(2*(i-1)));
     }
