d0ec2c93c7dc79c60bcc326c0ae77127aaa04c38
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-06-24 10:57:59 +0000

    use switch

--- CondFormats/EcalObjects/src/EcalPedestals.cc
+++ CondFormats/EcalObjects/src/EcalPedestals.cc
@@ -1,5 +1,6 @@
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
 #include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
 #include <algorithm>
 
 EcalPedestals::EcalPedestals(){}
@@ -20,12 +21,35 @@ namespace {
 
     std::vector<Item> & v;
   };
+
+  template<typename Item>
+  class EcalCalibInserter {
+  public:
+    Inserter(std::vector<Item> & ib, std::vector<Item> & ie ) : b(ib),e(ie){}
+
+    void operator()(std::pair<uint32_t, const Item> const & p) {
+      ::DetId id(p.first);
+      if (id.null() || id.det()!=::DetId::Ecal) return;
+      switch (id.subdetId()) :
+      case (EcalBarrel ) : 
+	EBDetId ib(p.first);
+	b.at(ib.hashedIndex()) = p.second;
+	break;
+      case (EcalBarrel ) : 
+	EEDetId ie(p.first);
+	// e.at(ie.hashedIndex()) = p.second;
+    }
+
+    std::vector<Item> & b;
+    std::vector<Item> & e;
+  };
 }
 
 void EcalPedestals::update() const {
   if (m_barrel.empty()) {
     // FIXME
     m_barrel.resize(m_pedestals.size());
-    std::for_each(m_pedestals.begin(),m_pedestals.end(),Inserter<EBDetId,Item>(m_barrel));
+    std::for_each(m_pedestals.begin(),m_pedestals.end(),
+		  EcalCalibInserter<Item>(m_barrel,m_endcap));
   }
 }
