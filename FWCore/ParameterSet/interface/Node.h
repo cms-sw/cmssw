202d0457b383de331d04264ed7cff7036ba7ae3e
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-06-25 21:22:32 +0000

    Add traceback method to makeit easier to give every exception a traceback

--- FWCore/ParameterSet/interface/Node.h
+++ FWCore/ParameterSet/interface/Node.h
@@ -56,6 +56,9 @@ namespace edm {
       /// Shows where this was included from
       /// default passes up to parent
       virtual void printTrace(std::ostream& ost) const;
+      /// returns a trace of all the nodes above this one
+      std::string traceback() const;
+
       /// will print a trace if the node matches.
       /// good for searching through deep include hierarchies
       virtual void locate(const std::string & s, std::ostream& ost) const;
