7d44c7c5a24fd7e7e2d570e53f8552db11204c51
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2007-06-26 11:47:57 +0000

    add ESUnpackerCT for preshower comic ray test

--- EventFilter/ESRawToDigi/src/SealModule.cc
+++ EventFilter/ESRawToDigi/src/SealModule.cc
@@ -4,9 +4,11 @@
 
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigi.h"
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigiTB.h"
+#include "EventFilter/ESRawToDigi/interface/ESRawToDigiCT.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiTB);
+DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiCT);
 
 
