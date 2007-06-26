a98b9aca8732a8716fc1b4fb3dcf8bab00bd847d
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2007-06-26 07:57:01 +0000

    *** empty log message ***

--- RecoTauTag/RecoTau/src/PFRecoTauAlgorithm.cc
+++ RecoTauTag/RecoTau/src/PFRecoTauAlgorithm.cc
@@ -2,17 +2,20 @@
 #include "DataFormats/Math/interface/Point3D.h"
 Tau PFRecoTauAlgorithm::tag(const PFIsolatedTauTagInfo& myTagInfo)
 {
-  const Jet & jet = *(myTagInfo.jet());
   //Takes the jet
-const  PFJet * pfJet = dynamic_cast<const PFJet*>( &jet );
+  const Jet & jet = *(myTagInfo.jet());
+  const  PFJet * pfJet = dynamic_cast<const PFJet*>( &jet );
+
   //Takes the LeadChargedHadron
   float z_PV = 0;
+  TrackRef myLeadTk;
   PFCandidateRef myLeadPFChargedHadronCand = (myTagInfo).leadPFChargedHadrCand(MatchingConeSize_, LeadCand_minPt_);
   if (myLeadPFChargedHadronCand->blockRef()->elements().size()!=0){
     for (OwnVector<PFBlockElement>::const_iterator iPFBlock=myLeadPFChargedHadronCand->blockRef()->elements().begin();
 	 iPFBlock!=myLeadPFChargedHadronCand->blockRef()->elements().end();iPFBlock++){
-      if ((*iPFBlock).type()==1 && ROOT::Math::VectorUtil::DeltaR(myLeadPFChargedHadronCand->momentum(),(*iPFBlock).trackRef()->momentum())<0.001){
-	TrackRef myLeadTk =(*iPFBlock).trackRef();
+      if ((*iPFBlock).type()==1 &&
+	  ROOT::Math::VectorUtil::DeltaR(myLeadPFChargedHadronCand->momentum(),(*iPFBlock).trackRef()->momentum())<0.001){
+	myLeadTk =(*iPFBlock).trackRef();
 	z_PV = myLeadTk->dz();
       }
     }
@@ -21,6 +24,9 @@ const  PFJet * pfJet = dynamic_cast<const PFJet*>( &jet );
   math::XYZPoint  vtx = math::XYZPoint( 0, 0, z_PV );
   //Qui che ci mettiamo per il vertice
   Tau myTau(pfJet->charge(), pfJet->p4(), vtx );
+  myTau.setLeadingTrack(myLeadTk);
+  myTau.setLeadingChargedHadron(myTagInfo.leadPFChargedHadrCand(MatchingConeSize_, LeadCand_minPt_));
+
   return myTau;
   
 }
