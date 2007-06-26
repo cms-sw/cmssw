26f1091161797aa89723dce469fd2feca8743b2f
Martijn Mulders <sha1-f69d5e2605de9d2f4b58e7ff6c652334c0f234e5@cern.ch>
2007-06-26 15:21:19 +0000

    extended usability of NavPropagator to +z half of detector using internal reflections

--- TrackPropagation/NavPropagator/src/NavPropagator.cc
+++ TrackPropagation/NavPropagator/src/NavPropagator.cc
@@ -4,6 +4,7 @@
 #include  "TrackPropagation/NavGeometry/interface/NavVolume6Faces.h"
 #include "TrackPropagation/RungeKutta/interface/RKPropagatorInS.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/GeometrySurface/interface/PlaneBuilder.h"
 
 using namespace std;
 
@@ -33,22 +34,39 @@ NavPropagator::propagateWithPath(const TrajectoryStateOnSurface& inputState,
   VolumeCrossReturnType exitState( 0, inputState, 0.0);
   TSOS startingState = inputState;
   TSOS TempState = inputState;
-  const NavVolume* currentVolume;
+  const NavVolume* currentVolume = 0; // explicit initialization to get rid of compiler warning
 
   int count = 0;
   int maxCount = 100;
-  while (!destinationCrossed( startingState, exitState.tsos(), targetPlane)) {
+  while (!destinationCrossed( startingState, TempState, targetPlane)) {
 
      LogDebug("NavPropagator") <<  "NavPropagator:: at beginning of while loop at iteration " << count ;
 
     startingState = TempState;
+    bool isReflected = false;
+
     if (exitState.volume() != 0) { // next volume connected
       currentVolume = exitState.volume();
     }
     else {
-      currentVolume = findVolume( startingState);
+      // 1 mm shifted peek is necessary only because geometry is not yet glued and exists only for z<0
+      GlobalPoint gp = startingState.globalPosition() + 0.1*startingState.globalMomentum().unit();
+
+      if (gp.z()>0) { // Reflect in Z (as long as MagVolumes exist only for z<0
+	GlobalTrajectoryParameters gtp( GlobalPoint(startingState.globalPosition().x(),
+						       startingState.globalPosition().y(),
+						    -startingState.globalPosition().z()),
+					GlobalVector(startingState.globalMomentum().x(),
+						       startingState.globalMomentum().y(),
+						       -startingState.globalMomentum().z()),
+					startingState.globalParameters().charge(), theField );
+	FreeTrajectoryState fts(gtp);
+	TempState = TSOS( fts, startingState.surface());
+	isReflected = true;
+      }
+      currentVolume = findVolume( TempState );
       if (currentVolume == 0) {
-	 LogDebug("NavPropagator") <<  "NavPropagator: findVolume failed to find volume containing point "
+	 std::cout <<  "NavPropagator: findVolume failed to find volume containing point "
 	     << startingState.globalPosition() ;
 	return std::pair<TrajectoryStateOnSurface,double>(noNextVolume( startingState), 0);
       }
@@ -57,7 +75,7 @@ NavPropagator::propagateWithPath(const TrajectoryStateOnSurface& inputState,
     PropagatorType currentPropagator( *currentVolume);
 
      LogDebug("NavPropagator") <<  "NavPropagator: calling crossToNextVolume" ;
-    exitState = currentVolume->crossToNextVolume( startingState, currentPropagator);
+    exitState = currentVolume->crossToNextVolume( TempState, currentPropagator);
      LogDebug("NavPropagator") <<  "NavPropagator: crossToNextVolume returned" ;
      LogDebug("NavPropagator") <<  "Volume pointer: " << exitState.volume() << " and new ";
      LogDebug("NavPropagator") <<  exitState.tsos() ;
@@ -66,7 +84,7 @@ NavPropagator::propagateWithPath(const TrajectoryStateOnSurface& inputState,
 
     if ( !exitState.tsos().isValid()) {
       // return propagateInVolume( currentVolume, startingState, targetPlane);
-       LogDebug("NavPropagator") <<  "NavPropagator: failed to crossToNextVolume in volume at pos "
+       std::cout <<  "NavPropagator: failed to crossToNextVolume in volume at pos "
 	   << currentVolume->position();
       break;
     }
@@ -98,14 +116,34 @@ NavPropagator::propagateWithPath(const TrajectoryStateOnSurface& inputState,
 
     }
 
+
+    // reflect back to normal universe if necessary:
+    if (isReflected) { // reflect back... nobody should know we secretely z-reflected the tsos 
+      
+
+      GlobalTrajectoryParameters gtp( GlobalPoint(TempState.globalPosition().x(),
+						     TempState.globalPosition().y(),
+						  -TempState.globalPosition().z()),
+				      GlobalVector(TempState.globalMomentum().x(),
+						   TempState.globalMomentum().y(),
+						   -TempState.globalMomentum().z()),
+				      TempState.globalParameters().charge(), theField );
+
+      FreeTrajectoryState fts(gtp);
+      TSOS ReflectedState( fts, TempState.surface());
+      TempState = ReflectedState;
+      isReflected = false;
+    }
+
     ++count;
     if (count > maxCount) {
        LogDebug("NavPropagator") <<  "Ohoh, NavPropagator in infinite loop, count = " << count;
       return TsosWP();
     }
+
   }
 
-   LogDebug("NavPropagator") <<  "NavPropagator: calling propagateInVolume to reach final destination" ;
+  
 
   // Arriving here only if destinationCrossed or if crossToNextVolume returned invalid state,
   // in which case we should check if the targetPlane is not crossed in the current volume
@@ -120,12 +158,19 @@ const NavVolume* NavPropagator::findVolume( const TrajectoryStateOnSurface& inpu
    LogDebug("NavPropagator") <<  "NavPropagator: calling theField->findVolume ";
 
   GlobalPoint gp = inputState.globalPosition() + 0.1*inputState.globalMomentum().unit();
+  // Next protection only needed when 1 mm linear move crosses the z==0 plane:
+  GlobalPoint gpSym(gp.x(), gp.y(), (gp.z()<0? gp.z() : -gp.z()));
 
-  const MagVolume* magVolume = theField->findVolume( gp);
+  const MagVolume* magVolume = theField->findVolume( gpSym);
 
    LogDebug("NavPropagator") <<  "NavPropagator: got MagVolume* " << magVolume 
-       << " when searching with pos " << gp ;
+       << " when searching with pos " << gpSym ;
 
+
+   if (!magVolume) {
+     cout << "Got invalid volume pointer " << magVolume << " at position " << gpSym;
+     return 0;
+   } 
   return navVolume(magVolume);
 }
 
@@ -148,13 +193,82 @@ NavPropagator::propagateInVolume( const NavVolume* currentVolume,
 				  const Plane& targetPlane) const
 {
   PropagatorType prop( *currentVolume);
-  TsosWP res = prop.propagateWithPath( startingState, targetPlane);
-  if (res.first.isValid()) { 
-    if (currentVolume->inside( res.first.globalPosition())) {
+
+  // OK, fix is rather ugly for now: need to reflect z>0 states to z<0 MM 25/6/07
+  bool isReflected = false;
+  TSOS okState = startingState;
+  PlaneBuilder::ReturnType ReflectedPlane;
+  Plane okPlane = targetPlane;
+  
+  if (startingState.globalPosition().z()>0 || 
+      (fabs(startingState.globalPosition().z())<1.e-4 && startingState.globalMomentum().z()>1.e-4)) {
+    GlobalTrajectoryParameters gtp( GlobalPoint(startingState.globalPosition().x(),
+						startingState.globalPosition().y(),
+						-startingState.globalPosition().z()),
+				    GlobalVector(startingState.globalMomentum().x(),
+						 startingState.globalMomentum().y(),
+						 -startingState.globalMomentum().z()),
+				    startingState.globalParameters().charge(), prop.magneticField() );
+    
+    FreeTrajectoryState fts(gtp);
+    TSOS ReflectedState( fts, startingState.surface());
+    okState = ReflectedState;
+      
+    GlobalVector TempVec = targetPlane.normalVector();
+    GlobalVector ReflectedVector = GlobalVector ( TempVec.x(), TempVec.y(), -TempVec.z());
+    GlobalVector zAxis = ReflectedVector.unit();
+    GlobalVector yAxis( zAxis.y(), -zAxis.x(), 0);
+    GlobalVector xAxis = yAxis.cross( zAxis);
+    Surface::RotationType rot = Surface::RotationType( xAxis, yAxis, zAxis);
+
+    GlobalPoint gp = targetPlane.position();
+    GlobalPoint gpSym(gp.x(), gp.y(), -gp.z());
+    PlaneBuilder pb;
+    ReflectedPlane = pb.plane( gpSym, rot);
+    
+    okPlane =  *ReflectedPlane;
+    isReflected = true;    
+  }
+  
+  // Done Reflecting
+
+
+  TsosWP res = prop.propagateWithPath( okState, okPlane);
+
+  // Now reflect back the result if necessary...
+  if (isReflected && res.first.isValid()) { // reflect back... nobody should know we secretely z-reflected the tsos 
+   
+
+    TSOS TempState = res.first; 
+    GlobalTrajectoryParameters gtp( GlobalPoint(TempState.globalPosition().x(),
+						TempState.globalPosition().y(),
+						-TempState.globalPosition().z()),
+				    GlobalVector(TempState.globalMomentum().x(),
+						 TempState.globalMomentum().y(),
+						 -TempState.globalMomentum().z()),
+				    TempState.globalParameters().charge(), prop.magneticField() );
+    
+    FreeTrajectoryState fts(gtp);
+    TSOS ReflectedState( fts, TempState.surface());
+    res.first = ReflectedState;
+    isReflected = false;
+  }
+  // Done Reflecting back !
+
+  if (res.first.isValid()) {
+
+    GlobalPoint gp = res.first.globalPosition();
+    GlobalPoint gpSym(gp.x(), gp.y(), (gp.z()<0? gp.z() : -gp.z()));
+ 
+    if (currentVolume->inside( gpSym )) {
       return res;
     }
+    // sometimes fails when propagation on plane and surface are less than 0.1 mm apart... 
   } 
-  return TsosWP( TrajectoryStateOnSurface(), 0);
+
+  //  return TsosWP( TrajectoryStateOnSurface(), 0); // Gives 'double free' errors... return res even when Outside volume... 
+  return res;
+
 }
 
 bool  NavPropagator::destinationCrossed( const TSOS& startState,
