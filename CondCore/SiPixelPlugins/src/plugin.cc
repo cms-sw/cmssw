317c3547307b61247e0de454a527679294deb771
wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch>
2007-06-28 12:46:30 +0000

    Add Lorentz Angle object

--- CondCore/SiPixelPlugins/src/plugin.cc
+++ CondCore/SiPixelPlugins/src/plugin.cc
@@ -4,7 +4,10 @@
 #include "CondFormats/DataRecord/interface/SiPixelFedCablingMapRcd.h"
 #include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibration.h"
 #include "CondFormats/DataRecord/interface/SiPixelGainCalibrationRcd.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelLorentzAngle.h"
+#include "CondFormats/DataRecord/interface/SiPixelLorentzAngleRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(SiPixelFedCablingMapRcd,SiPixelFedCablingMap);
 REGISTER_PLUGIN(SiPixelGainCalibrationRcd,SiPixelGainCalibration);
+REGISTER_PLUGIN(SiPixelLorentzAngleRcd,SiPixelLorentzAngle);
