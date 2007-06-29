ee7169e8c3b3f2ee7d83927fcc6654e3a2eb08e7
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-06-29 06:51:37 +0000

    also weights

--- CondFormats/EcalObjects/interface/EcalGainRatios.h
+++ CondFormats/EcalObjects/interface/EcalGainRatios.h
@@ -3,25 +3,45 @@
 /**
  * Author: Shahram Rahatlou, University of Rome & INFN
  * Created: 22 Feb 2006
- * $Id: $
+ * $Id: EcalGainRatios.h,v 1.2 2006/02/23 16:56:34 rahatlou Exp $
  **/
 
-
+#include "DataFormats/EcalDetId/interface/EcalContainer.h"
 #include <map>
 #include <boost/cstdint.hpp>
 #include "CondFormats/EcalObjects/interface/EcalMGPAGainRatio.h"
 
 
 class EcalGainRatios {
-  public:
-    typedef std::map<uint32_t, EcalMGPAGainRatio> EcalGainRatioMap;
+public:
+  typedef EcalMGPAGainRatio Item; 
+  typedef std::map<uint32_t, EcalMGPAGainRatio> EcalGainRatioMap;
 
-    EcalGainRatios();
-    ~EcalGainRatios();
-    void  setValue(const uint32_t& id, const EcalMGPAGainRatio& value);
-    const EcalGainRatioMap& getMap() const { return map_; }
+  EcalGainRatios();
+  ~EcalGainRatios();
+  void  setValue(const uint32_t& id, const EcalMGPAGainRatio& value);
+  const EcalGainRatioMap& getMap() const { return map_; }
+  
+  
+  void update() const;
 
-  private:
-    EcalGainRatioMap map_;
+  Item const & operator()(DetId id) const {
+    return m_hashedCont(id);
+  }
+
+  Item const & barrel(size_t hashid) const {
+    return m_hashedCont.barrel(hashid);
+  }
+  Item const & endcap(size_t hashid) const {
+    return m_hashedCont.endcap(hashid);
+  }
+
+private:
+  void doUpdate();
+  EcalContainer<Item> m_hashedCont;
+
+
+private:
+  EcalGainRatioMap map_;
 };
 #endif
--- CondFormats/EcalObjects/src/EcalGainRatios.cc
+++ CondFormats/EcalObjects/src/EcalGainRatios.cc
@@ -1,7 +1,7 @@
 /**
  * Author: Shahram Rahatlou, University of Rome & INFN
  * Created: 22 Feb 2006
- * $Id: $
+ * $Id: EcalGainRatios.cc,v 1.2 2006/02/23 16:56:35 rahatlou Exp $
  **/
 
 #include "CondFormats/EcalObjects/interface/EcalGainRatios.h"
@@ -18,3 +18,11 @@ EcalGainRatios::setValue(const uint32_t& id, const EcalMGPAGainRatio & value) {
   map_[id] = value;
 }
 
+// safer than mutable vectors...
+void EcalGainRatios::update() const {
+  const_cast<EcalGainRatios&>(*this).doUpdate();
+}
+ 
+void EcalGainRatios::doUpdate(){
+  m_hashedCont.load(getMap().begin(),getMap().end());
+}
