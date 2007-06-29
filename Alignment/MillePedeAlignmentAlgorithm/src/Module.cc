64971c3769fc61b0f8f055de970b949c91ed14db
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2007-06-29 08:41:38 +0000

    remove left over from plugin manager migration

--- Alignment/MillePedeAlignmentAlgorithm/src/Module.cc
+++ Alignment/MillePedeAlignmentAlgorithm/src/Module.cc
@@ -3,8 +3,6 @@
 #include "Alignment/MillePedeAlignmentAlgorithm/interface/MillePedeAlignmentAlgorithm.h"
 #include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentAlgorithmPluginFactory.h"
 
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
 DEFINE_EDM_PLUGIN(AlignmentAlgorithmPluginFactory,
 		   MillePedeAlignmentAlgorithm, "MillePedeAlignmentAlgorithm");
 
