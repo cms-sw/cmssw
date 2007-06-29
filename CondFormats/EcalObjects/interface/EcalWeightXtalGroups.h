538fb916862d8ab3dc2cf30c4b71a8fd6a662e3f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-06-29 07:04:31 +0000

    also weights

--- CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h
+++ CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h
@@ -3,23 +3,43 @@
 /**
  * Author: Shahram Rahatlou, University of Rome & INFN
  * Created: 22 Feb 2006
- * $Id: $
+ * $Id: EcalWeightXtalGroups.h,v 1.2 2006/02/23 16:56:34 rahatlou Exp $
  **/
 
+#include "DataFormats/EcalDetId/interface/EcalContainer.h"
 #include <map>
 #include <boost/cstdint.hpp>
 #include "CondFormats/EcalObjects/interface/EcalXtalGroupId.h"
 
 class EcalWeightXtalGroups {
-  public:
-    typedef std::map<uint32_t, EcalXtalGroupId> EcalXtalGroupsMap;
+public:
+  typedef  EcalXtalGroupId Item; 
+  typedef std::map<uint32_t, EcalXtalGroupId> EcalXtalGroupsMap;
+  
+  EcalWeightXtalGroups();
+  ~EcalWeightXtalGroups();
+  void  setValue(const uint32_t& xtal, const EcalXtalGroupId& group);
+  const EcalXtalGroupsMap& getMap() const { return map_; }
 
-    EcalWeightXtalGroups();
-    ~EcalWeightXtalGroups();
-    void  setValue(const uint32_t& xtal, const EcalXtalGroupId& group);
-    const EcalXtalGroupsMap& getMap() const { return map_; }
+  void update() const;
 
-  private:
-    EcalXtalGroupsMap map_;
+  Item const & operator()(DetId id) const {
+    return m_hashedCont(id);
+  }
+
+  Item const & barrel(size_t hashid) const {
+    return m_hashedCont.barrel(hashid);
+  }
+  Item const & endcap(size_t hashid) const {
+    return m_hashedCont.endcap(hashid);
+  }
+
+  
+private:
+  void doUpdate();
+  EcalContainer<Item> m_hashedCont;
+  
+private:
+  EcalXtalGroupsMap map_;
 };
 #endif
--- CondFormats/EcalObjects/src/EcalWeightXtalGroups.cc
+++ CondFormats/EcalObjects/src/EcalWeightXtalGroups.cc
@@ -1,7 +1,7 @@
 /**
  * Author: Shahram Rahatlou, University of Rome & INFN
  * Created: 22 Feb 2006
- * $Id: $
+ * $Id: EcalWeightXtalGroups.cc,v 1.2 2006/02/23 16:56:35 rahatlou Exp $
  **/
 
 #include "CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h"
@@ -18,3 +18,11 @@ EcalWeightXtalGroups::setValue(const uint32_t& xtal, const EcalXtalGroupId& grou
   map_[xtal] = group;
 }
 
+// safer than mutable vectors...
+void EcalWeightXtalGroups::update() const {
+  const_cast<EcalWeightXtalGroups&>(*this).doUpdate();
+}
+ 
+voidEcalWeightXtalGroups::doUpdate(){
+  m_hashedCont.load(getMap().begin(),getMap().end());
+}
