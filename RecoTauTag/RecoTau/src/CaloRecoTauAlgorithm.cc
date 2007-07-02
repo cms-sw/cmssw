a1c5a3d22f7c2a808cd8d8dd110b66554b653760
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2007-07-02 10:36:39 +0000

    *** empty log message ***

--- RecoTauTag/RecoTau/src/CaloRecoTauAlgorithm.cc
+++ RecoTauTag/RecoTau/src/CaloRecoTauAlgorithm.cc
@@ -1,6 +1,7 @@
 #include "RecoTauTag/RecoTau/interface/CaloRecoTauAlgorithm.h"
 #include "DataFormats/Math/interface/Point3D.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
+#include <Math/GenVector/VectorUtil.h>
 
 Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo)
 {
@@ -9,55 +10,76 @@ Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo)
   //Takes the LeadChargedHadron
   float z_PV = 0;
   TrackRef myLeadTk = (myTagInfo.isolatedtautaginfoRef())->leadingSignalTrack(MatchingConeSize_, LeadCand_minPt_);
-
-  /*
-    myLeadTk = 
   if(myLeadTk.isNonnull()){
     cout <<"Vertex "<<myLeadTk->vz()<<endl;
     cout <<"Z_imp "<<myLeadTk->dz()<<endl;
   }
-  */
+  
   math::XYZPoint  vtx = math::XYZPoint( 0, 0, z_PV );
   
   //create the Tau  
   Tau myTau(jet.charge(),jet.p4(),vtx);
-	
-    //Setting the EmOverHcal energy
 
-    
-    //Setting the SelectedTracks
-    TrackRefVector mySelectedTracks = myTagInfo.selectedTks();
-    myTau.setSelectedTracks(mySelectedTracks);  
-    
-    
-    //Setting the LeadChargedHadrons
-      
-      //Setting the HCalEnergy from the LeadHadron
-
-      
-
+  //Setting the SelectedTracks
+  TrackRefVector mySelectedTracks = myTagInfo.selectedTks();
+  myTau.setSelectedTracks(mySelectedTracks);  
+  
+  //Setting the LeadingTrack
+  myTau.setLeadingTrack(myLeadTk);
+  if(myLeadTk.isNonnull())
+    {
       //Setting the SignalTracks
-      
+      TrackRefVector signalTracks = 
+	(myTagInfo.isolatedtautaginfoRef())->tracksInCone(myLeadTk->momentum(), TrackerSignalConeSize_,Tracks_minPt_ );
+
+      myTau.setSignalTracks(signalTracks);
       
       //Setting charge
-      myTau.setCharge(myTagInfo.signalTks_qsum());
-    
+      int charge = 0.;
+      for( int i=0;i<signalTracks.size();i++){
+	charge = charge+ signalTracks[i]->charge();
+      }
+
+      myTau.setCharge(charge);
+
+      //Setting the IsolationTracks
+      TrackRefVector isolationTracks = 
+	(myTagInfo.isolatedtautaginfoRef())->tracksInCone(myLeadTk->momentum(), TrackerIsolConeSize_,Tracks_minPt_ );
+      TrackRefVector isolationBandTracks;
+      for(int i=0;i<isolationTracks.size();i++){
+	const math::XYZVector trackMomentum = isolationTracks[i]->momentum();
+	const math::XYZVector myVector = myLeadTk->momentum();
+	float deltaR = ROOT::Math::VectorUtil::DeltaR(myVector, trackMomentum);
+	if(deltaR > TrackerSignalConeSize_)
+	  isolationBandTracks.push_back(isolationTracks[i]);
+      }
+
+      //Setting sum of the pT of isolation Annulus charged hadrons
+      float ptSum =0;
+      for(int i=0;i<isolationBandTracks.size();i++){
+	ptSum = ptSum + isolationBandTracks[i]->pt();
+      }
+      myTau.setSumPtIsolation(ptSum);
       
+      //Setting sum of the E_T of isolation Annulus gamma candidates
 
 
       //setting the mass with only Signal objects:
       
-      //Setting sum of the pT of isolation Annulus charged hadrons
-      
-      //Setting sum of the E_T of isolation Annulus gamma candidates
-    
 
+      myTau.setIsolationTracks(isolationBandTracks);
+      
+      //Setting the max HCalEnergy
+
+      //Setting the EmOverHcal energy
+      
+      //Setting the number of EcalClusters
 
 /*
 setLeadTkTIP(const Measurement1D& myIP)  { transverseIp_leadTk_ = myIP;}
 setLeadTk3DIP(const Measurement1D& myIP)  {  ip3D_leadTk_=myIP;}
 */
- 
+    }
 
   return myTau;
   
