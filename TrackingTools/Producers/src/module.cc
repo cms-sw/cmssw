457125c2eabb0c24cd55aef86b76184ea7a2f80d
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-07-03 16:40:49 +0000

    first commit of BeamHaloPropagatorESProducer, added as a module

--- TrackingTools/Producers/src/module.cc
+++ TrackingTools/Producers/src/module.cc
@@ -1,6 +1,7 @@
 #include "TrackingTools/Producers/interface/AnalyticalPropagatorESProducer.h"
 #include "TrackingTools/Producers/interface/StraightLinePropagatorESProducer.h"
 #include "TrackingTools/Producers/interface/SmartPropagatorESProducer.h"
+#include "TrackingTools/Producers/interface/BeamHaloPropagatorESProducer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -12,3 +13,4 @@
 DEFINE_FWK_EVENTSETUP_MODULE(StraightLinePropagatorESProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(AnalyticalPropagatorESProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(SmartPropagatorESProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(BeamHaloPropagatorESProducer);
