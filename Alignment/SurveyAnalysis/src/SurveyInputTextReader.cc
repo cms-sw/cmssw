52c2f17ddd189ec18a60ebb332e3cba7f23c4810
Nhan Viet Tran <sha1-8a19d334a4648fb26ba9e7879a577ce46f3df51a@cern.ch>
2007-07-05 19:35:06 +0000

    Allow for comments in text file, eliminated ObjId column, now in block header

--- Alignment/SurveyAnalysis/src/SurveyInputTextReader.cc
+++ Alignment/SurveyAnalysis/src/SurveyInputTextReader.cc
@@ -1,4 +1,5 @@
 // System
+#include <string>
 #include <fstream>
 #include <iostream>
 
@@ -14,9 +15,10 @@ void SurveyInputTextReader::readFile( const std::string& textFileName )
   std::ifstream myfile( textFileName.c_str() );
   if ( !myfile.is_open() )
         throw cms::Exception("FileAccess") << "Unable to open input text file";
-
+	
 	DetIdType m_detId;
-	int m_alignObjId;
+	int m_alignObjId = 0; //initial value 
+	AlignableObjectId  alignObjId;
   TrackerAlignableId::UniqueId  m_uId;
   std::vector<float> m_inputs(NINPUTS);
   float tmpInput;
@@ -24,19 +26,41 @@ void SurveyInputTextReader::readFile( const std::string& textFileName )
   while ( !myfile.eof() && myfile.good() )
 	{
 		m_inputs.clear();
-		myfile >> m_detId >> m_alignObjId;
-		m_uId.first = m_detId;
-		m_uId.second = m_alignObjId;
 		
-		for ( int i=0; i<NINPUTS; i++ )
-		{
-			myfile >> tmpInput;
-			m_inputs.push_back( tmpInput );
+		char firstchar;
+		firstchar = myfile.peek();
+
+		if(firstchar == '#'){
+			std::string line;
+			getline(myfile,line);
 		}
-		
-		// Check if read succeeded (otherwise, we are at eof)
-		if ( myfile.fail() ) break;
-		
-		theMap.insert( PairType( m_uId, m_inputs));
+		else if (firstchar == '!'){
+			std::string firststring;
+			std::string structure; 
+			myfile >> firststring >> structure;
+			std::string endofline;
+			getline(myfile,endofline);
+			m_alignObjId = alignObjId.nameToType(structure);
+			if (m_alignObjId == 0) std::cout << "=============> invalid Object ID" << std::endl;
+		}
+		else{
+			myfile >> m_detId;
+			m_uId.first = m_detId;
+			m_uId.second = m_alignObjId;
+
+			for ( int i=0; i<NINPUTS; i++ )
+			{
+				myfile >> tmpInput;
+				m_inputs.push_back( tmpInput );
+			}
+			std::string endofline;
+			getline(myfile,endofline);
+			theMap.insert( PairType( m_uId, m_inputs));			
+			
+			// Check if read succeeded (otherwise, we are at eof)
+			if ( myfile.fail() ) break;
+			
+
+		}
 	}
 }
