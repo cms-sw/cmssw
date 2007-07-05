9e208dfc7d98546e1589a8fdd3bf95e7dd992dad
Brian Drell <sha1-c7cbb7ddebb3a56d09a58e9398ceff52182b647f@cern.ch>
2007-07-05 12:12:38 +0000

    added V0candidate class and dictionary entries

---
+++ DataFormats/V0Candidate/src/V0Candidate.cc
@@ -0,0 +1,7 @@
+#include "DataFormats/V0Candidate/interface/V0Candidate.h"
+
+using namespace reco;
+
+void V0Candidate::setVertex( const Vertex & vtxIn ) {
+  recoVertex = vtxIn;
+}
---
+++ DataFormats/V0Candidate/src/classes.h
@@ -0,0 +1,8 @@
+#include "DataFormats/V0Candidate/interface/V0Candidate.h"
+
+namespace {
+  namespace {
+    std::vector<reco::V0Candidate> v01;
+    edm::Wrapper<std::vector<reco::V0Candidate> > wv01;
+  }
+}
