fd3afc2578b8f40fa8d839a1956fa624f324b114
Ivan Mikulec <sha1-8d77057744976da3e68d7e182804494ea5541838@cern.ch>
2007-07-06 15:08:37 +0000

    First commit of GMT configuration via EventSetup

---
+++ L1TriggerConfig/GMTConfigProducers/interface/L1MuGMTParametersProducer.h
@@ -0,0 +1,48 @@
+//-------------------------------------------------
+//
+//   \class L1MuGMTParametersProducer
+//
+//   Description:  A class to produce the L1 GMT emulator parameters in the event setup
+//
+//   $Date: $
+//   $Revision: $
+//
+//   Author :
+//   I. Mikulec
+//
+//--------------------------------------------------
+#ifndef GMTConfigProducers_L1MuGMTParametersProducer_h
+#define GMTConfigProducers_L1MuGMTParametersProducer_h
+
+// system include files
+#include <memory>
+#include <boost/shared_ptr.hpp>
+
+// user include files
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "CondFormats/L1TObjects/interface/L1MuGMTParameters.h"
+#include "CondFormats/DataRecord/interface/L1MuGMTParametersRcd.h"
+
+
+//
+// class declaration
+//
+
+class L1MuGMTParametersProducer : public edm::ESProducer {
+public:
+  L1MuGMTParametersProducer(const edm::ParameterSet&);
+  ~L1MuGMTParametersProducer();
+  
+  std::auto_ptr<L1MuGMTParameters> produceL1MuGMTParameters(const L1MuGMTParametersRcd&);
+
+private:
+  // ----------member data ---------------------------
+  edm::ParameterSet* m_ps;
+  
+};
+
+#endif
