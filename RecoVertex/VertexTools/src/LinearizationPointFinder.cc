52a75ebda8339eeed5ebf540aef5236c04fbcbf1
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-07-06 12:52:54 +0000

    Fix so that it can be used with FTS, without TT objects

--- RecoVertex/VertexTools/src/LinearizationPointFinder.cc
+++ RecoVertex/VertexTools/src/LinearizationPointFinder.cc
@@ -1,28 +1,14 @@
 #include "RecoVertex/VertexTools/interface/LinearizationPointFinder.h"
-// #include "CommonReco/PatternTools/interface/ConcreteRecTrack.h"
 #include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackFromFTSFactory.h"
 
 GlobalPoint LinearizationPointFinder::getLinearizationPoint(
     const std::vector<FreeTrajectoryState> & ftses ) const
 {
   std::vector < reco::TransientTrack > rectracks;
-//   vector < ConcreteRecTrack > concreteTks;
-// 
-//   TrajectorySeed ts;
-//   vector < TrajectoryMeasurement > tm;
-//   for ( vector< FreeTrajectoryState>::const_iterator fts=ftses.begin();
-//         fts!=ftses.end() ; ++fts )
-//   {   
-//    GlobalPoint fPos = fts->position();
-//    ConcreteRecTrack ctrack ( *fts, fPos, 0., 0, 0, ts, tm );
-//    concreteTks.push_back(ctrack);
-//   }
-// 
-//   for (vector< ConcreteRecTrack >::const_iterator itk = concreteTks.begin(); 
-//        itk != concreteTks.end(); itk++) {
-//     RecTrack ntrack(&(*itk));
-//     rectracks.push_back ( ntrack );
-//   }
-
+  TransientTrackFromFTSFactory factory;
+  for ( std::vector< FreeTrajectoryState>::const_iterator fts=ftses.begin();
+        fts!=ftses.end() ; ++fts )
+    rectracks.push_back ( factory.build(*fts));
   return getLinearizationPoint(rectracks);
 }
