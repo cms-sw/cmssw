867dd90a2bd774cf75f4ddea49067a5f8870ecff
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-07-06 18:13:57 +0000

    add GetJetsFromHLTobject

--- HLTrigger/btau/src/SealModule.cc
+++ HLTrigger/btau/src/SealModule.cc
@@ -5,10 +5,11 @@
 #include "HLTrigger/btau/interface/HLTDisplacedmumuFilter.h"
 #include "HLTrigger/btau/interface/HLTL1MuonCorrector.h"
 #include "HLTrigger/btau/interface/HLTmumuGammaFilter.h"
+#include "HLTrigger/btau/interface/GetJetsFromHLTobject.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(HLTJetTag);
-DEFINE_ANOTHER_FWK_MODULE(HLTTauL25DoubleFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTDisplacedmumuFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTL1MuonCorrector);
-DEFINE_ANOTHER_FWK_MODULE(HLTmumuGammaFilter);
+DEFINE_FWK_MODULE(HLTJetTag);
+DEFINE_FWK_MODULE(HLTTauL25DoubleFilter);
+DEFINE_FWK_MODULE(HLTDisplacedmumuFilter);
+DEFINE_FWK_MODULE(HLTL1MuonCorrector);
+DEFINE_FWK_MODULE(HLTmumuGammaFilter);
+DEFINE_FWK_MODULE(GetJetsFromHLTobject);
