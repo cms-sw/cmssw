f3bc59299347b57db08b7eb47c03b9f294f549c1
Oliver Gutsche <sha1-2307d037a116d9bb0c52272fc9aa8ec9cc58a51e@cern.ch>
2007-07-08 20:22:41 +0000

    moved from OwnVector to plain vector with TrackingRecHit pointers

--- DataFormats/RoadSearchCloud/interface/RoadSearchCloud.h
+++ DataFormats/RoadSearchCloud/interface/RoadSearchCloud.h
@@ -13,33 +13,30 @@
 // Created:         Sat Jan 14 22:00:00 UTC 2006
 //
 // $Author: gutsche $
-// $Date: 2006/08/29 22:15:40 $
-// $Revision: 1.4 $
+// $Date: 2007/06/29 23:47:24 $
+// $Revision: 1.5 $
 //
 
 #include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
-#include "DataFormats/Common/interface/OwnVector.h"
-#include "DataFormats/Common/interface/ClonePolicy.h"
 
 class RoadSearchCloud {
 public:
 
-  typedef edm::OwnVector<TrackingRecHit,edm::ClonePolicy<TrackingRecHit> > RecHitOwnVector;
+  typedef std::vector<const TrackingRecHit*> RecHitVector;
 
   RoadSearchCloud() {}
-  RoadSearchCloud(RecHitOwnVector rechits): recHits_(rechits) {}
+  RoadSearchCloud(RecHitVector rechits): recHits_(rechits) {}
 
   inline RoadSearchCloud* clone() const { return new RoadSearchCloud(recHits_); }
-  inline RecHitOwnVector recHits() const { return recHits_;}
-  inline void addHit(TrackingRecHit* input) { recHits_.push_back(input); }
+  inline void addHit(const TrackingRecHit* input) { recHits_.push_back(input); }
   inline unsigned int size() const { return recHits_.size(); }
-  inline RecHitOwnVector::const_iterator begin_hits() const { return recHits_.begin(); }
-  inline RecHitOwnVector::const_iterator end_hits()   const { return recHits_.end();   }
-
+  inline RecHitVector recHits() const { return recHits_; }
+  inline RecHitVector::const_iterator begin_hits() const { return recHits_.begin(); }
+  inline RecHitVector::const_iterator end_hits()   const { return recHits_.end();   }
 
 private:
 
-  RecHitOwnVector recHits_;
+  RecHitVector recHits_;
 
 };
 
