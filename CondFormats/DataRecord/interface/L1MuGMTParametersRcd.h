8e761557d8b70e195f60a2f7769e5acb54cabae7
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2007-07-08 00:45:37 +0000

    First submission.

---
+++ CondFormats/DataRecord/interface/L1MuGMTParametersRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormatsDataRecord_L1MuGMTParametersRcd_h
+#define CondFormatsDataRecord_L1MuGMTParametersRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1MuGMTParametersRcd : public edm::eventsetup::EventSetupRecordImplementation<L1MuGMTParametersRcd> {};
+
+#endif
