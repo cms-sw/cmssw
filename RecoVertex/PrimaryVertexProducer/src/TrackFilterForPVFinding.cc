22a35ff45830a7d6cdc3476e9b125317a3e143cf
Wolfram Erdmann <sha1-3bf1149ebfd3d4f6b688e49eec204911cf01dc05@cern.ch>
2007-07-09 14:17:33 +0000

    workaround for problem with Mag

--- RecoVertex/PrimaryVertexProducer/src/TrackFilterForPVFinding.cc
+++ RecoVertex/PrimaryVertexProducer/src/TrackFilterForPVFinding.cc
@@ -8,7 +8,19 @@ TrackFilterForPVFinding::TrackFilterForPVFinding(const edm::ParameterSet& conf)
 bool 
 TrackFilterForPVFinding::operator() (const reco::TransientTrack & tk) const
 {
-  return tk.stateAtBeamLine().transverseImpactParameter().significance()<maxD0Significance();
+  // return tk.stateAtBeamLine().transverseImpactParameter().significance()<maxD0Significance();
+  // the above doesn't seem to work
+  // temporary fix:
+  AlgebraicSymMatrix33 error = tk.stateAtBeamLine().beamSpot().covariance3D() +
+	tk.stateAtBeamLine().trackStateAtPCA().cartesianError().matrix().Sub<AlgebraicSymMatrix33>(0,0);
+  GlobalPoint impactPoint=tk.stateAtBeamLine().trackStateAtPCA().position();
+  double dx=impactPoint.x()-tk.stateAtBeamLine().beamLinePCA().x();
+  double dy=impactPoint.y()-tk.stateAtBeamLine().beamLinePCA().y();
+  AlgebraicVector3 transverseFlightPath(dx,dy,0.);
+  double ip      = sqrt(dx*dx+dy*dy);
+  double ipError = sqrt( ROOT::Math::Similarity(transverseFlightPath.Unit(),error) );
+  //end of temporary fix
+   return ip<ipError*maxD0Significance();
 }
 
 
