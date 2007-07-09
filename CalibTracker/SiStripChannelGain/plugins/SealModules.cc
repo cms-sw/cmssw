2a403d2f1a928f41da0f133957124059768526e6
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2007-07-09 11:11:00 +0000

    updates

--- CalibTracker/SiStripChannelGain/plugins/SealModules.cc
+++ CalibTracker/SiStripChannelGain/plugins/SealModules.cc
@@ -5,8 +5,12 @@
 //#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 #include "CalibTracker/SiStripChannelGain/plugins/SiStripGainRandomCalculator.h"
+#include "CalibTracker/SiStripChannelGain/plugins/SiStripGainTickMarkCalculator.h"
+#include "CalibTracker/SiStripChannelGain/plugins/SiStripGainCosmicCalculator.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripGainRandomCalculator);
+DEFINE_ANOTHER_FWK_MODULE(SiStripGainTickMarkCalculator);
+DEFINE_ANOTHER_FWK_MODULE(SiStripGainCosmicCalculator);
 
 
