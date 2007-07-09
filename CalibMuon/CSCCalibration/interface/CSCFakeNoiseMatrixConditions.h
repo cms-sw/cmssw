19761933fa9c9ed245fc37bbe58d6fb3709d4f76
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-07-09 08:17:30 +0000

    updated to include Map part

--- CalibMuon/CSCCalibration/interface/CSCFakeNoiseMatrixConditions.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeNoiseMatrixConditions.h
@@ -16,24 +16,38 @@
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
+#include <CondFormats/CSCObjects/interface/CSCReadoutMapping.h>
+#include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
 #include "CondFormats/CSCObjects/interface/CSCobject.h"
 #include "CondFormats/DataRecord/interface/CSCNoiseMatrixRcd.h"
-#include "CalibMuon/CSCCalibration/interface/CSCFakeNoiseMatrixMap.h"
+//#include "CalibMuon/CSCCalibration/interface/CSCFakeNoiseMatrixMap.h"
 
 class CSCFakeNoiseMatrixConditions: public edm::ESProducer, public edm::EventSetupRecordIntervalFinder  {
    public:
       CSCFakeNoiseMatrixConditions(const edm::ParameterSet&);
-      ~CSCFakeNoiseMatrixConditions();
+       void prefillNoiseMatrix();
 
+       /*
+       const CSCNoiseMatrix & get(){
+	 return (*cnmatrix);
+       }
+       */
+
+      ~CSCFakeNoiseMatrixConditions();
+      
       typedef const  CSCNoiseMatrix * ReturnType;
 
       ReturnType produceNoiseMatrix(const CSCNoiseMatrixRcd&);
+      CSCNoiseMatrix *cnmatrix ;
+
 
    private:
+
       // ----------member data ---------------------------
     void setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&, edm::ValidityInterval & );
     
-    CSCFakeNoiseMatrixMap matrix;
+    CSCFakeNoiseMatrixConditions matrix;
 };
 
 #endif
