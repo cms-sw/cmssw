71d46168dcb818d39aa003ee62134ec9aa29b956
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2007-07-11 22:06:37 +0000

    Change to use HcalDbRecord directly (as dependency) for conditions info Should be much more efficient Add getters needed by lumi

--- CalibFormats/HcalObjects/interface/HcalTPGCoder.h
+++ CalibFormats/HcalObjects/interface/HcalTPGCoder.h
@@ -19,17 +19,23 @@ namespace edm {
   * Note : whether the coder produces E or ET is determined by the specific
   * implementation of the coder.
   *
-  * $Date: 2006/09/14 20:01:37 $
-  * $Revision: 1.4 $
+  * $Date: 2007/02/19 15:34:55 $
+  * $Revision: 1.5 $
   * \author J. Mans - Minnesota
   */
 class HcalTPGCoder {
 public:
-  virtual void getConditions(const edm::EventSetup& es) const { }
-  virtual void releaseConditions() const { }
   virtual void adc2Linear(const HBHEDataFrame& df, IntegerCaloSamples& ics) const = 0;
   virtual void adc2Linear(const HFDataFrame& df, IntegerCaloSamples& ics) const = 0;
+  virtual unsigned short adc2Linear(HcalQIESample sample,HcalDetId id) const = 0;
+  unsigned short adc2Linear(unsigned char adc, HcalDetId id) const { return adc2Linear(HcalQIESample(adc,0,0,0),id); }
   virtual void compress(const IntegerCaloSamples& ics, const std::vector<bool>& featureBits, HcalTriggerPrimitiveDigi& tp) const = 0;
+  virtual float getLUTPedestal(HcalDetId id) const = 0;
+  virtual float getLUTGain(HcalDetId id) const = 0;
+  /** \brief Get the full linearization LUT (128 elements).
+      Default implementation just uses adc2Linear to get all values
+  */
+  virtual std::vector<unsigned short> getLinearizationLUT(HcalDetId id) const;
 };
 
 #endif
