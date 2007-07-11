d60a6731ca6f8c391202bc01438f7178c13e1f85
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-07-11 19:50:20 +0000

    TrackAssociatorByPosition for muon association purpose

--- SimTracker/TrackAssociatorESProducer/src/SealModules.cc
+++ SimTracker/TrackAssociatorESProducer/src/SealModules.cc
@@ -3,6 +3,8 @@
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 #include "SimTracker/TrackAssociatorESProducer/src/TrackAssociatorByChi2ESProducer.hh"
 #include "SimTracker/TrackAssociatorESProducer/src/TrackAssociatorByHitsESProducer.hh"
+#include "SimTracker/TrackAssociatorESProducer/src/TrackAssociatorByPositionESProducer.hh"
 
 DEFINE_FWK_EVENTSETUP_MODULE(TrackAssociatorByHitsESProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(TrackAssociatorByChi2ESProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(TrackAssociatorByPositionESProducer);
