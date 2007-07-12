d663a197a90ced3220baa8214b52b656ba4a4c85
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-07-12 09:52:17 +0000

    added ECAL TPG Records for Pascal Paganini

---
+++ CondFormats/DataRecord/interface/EcalTPGFineGrainTowerEERcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALTPGFINEGRAINTOWEREERCD_H
+#define ECALTPGFINEGRAINTOWEREERCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalTPGFineGrainTowerEERcd : public edm::eventsetup::EventSetupRecordImplementation<EcalTPGFineGrainTowerEERcd> {};
+#endif
---
+++ CondFormats/DataRecord/interface/EcalTPGWeightIdMapRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALTPGWEIGHTIDMAPRCD_H
+#define ECALTPGWEIGHTIDMAPRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalTPGWeightIdMapRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalTPGWeightIdMapRcd> {};
+#endif
