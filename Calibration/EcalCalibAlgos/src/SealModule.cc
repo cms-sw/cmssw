32e6d40862ad8b6abd6dbe66ab6426b6ec516b80
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2007-07-12 17:28:34 +0000

    Changes for Zee

--- Calibration/EcalCalibAlgos/src/SealModule.cc
+++ Calibration/EcalCalibAlgos/src/SealModule.cc
@@ -2,9 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Calibration/EcalCalibAlgos/interface/miscalibExample.h"
 #include "Calibration/EcalCalibAlgos/interface/ElectronCalibration.h"
+#include "Calibration/EcalCalibAlgos/interface/ZeeCalibration.h"
 #include "Calibration/EcalCalibAlgos/interface/PhiSymmetryCalibration.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(miscalibExample);
 DEFINE_ANOTHER_FWK_MODULE(ElectronCalibration);
 DEFINE_ANOTHER_FWK_MODULE(PhiSymmetryCalibration);
+DEFINE_ANOTHER_FWK_LOOPER(Pi0FixedMassWindowCalibration);
+DEFINE_ANOTHER_FWK_LOOPER(ZeeCalibration);
+
