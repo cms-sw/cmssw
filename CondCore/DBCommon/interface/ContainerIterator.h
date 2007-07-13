21ee305876dff2911912179f4ff4af19a2496292
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-07-13 16:44:12 +0000

    add container iterator and test

--- CondCore/DBCommon/interface/ContainerIterator.h
+++ CondCore/DBCommon/interface/ContainerIterator.h
@@ -1,7 +1,7 @@
 #ifndef CondCore_DBCommon_ContainerIterator_h
 #define CondCore_DBCommon_ContainerIterator_h
 #include "Collection/Collection.h"
-#include "CondCore/DBCommon/interface/Ref.h"
+#include "CondCore/DBCommon/interface/TypedRef.h"
 namespace cond{
   class Connection;
   /*
@@ -10,16 +10,16 @@ namespace cond{
   template<typename DataT>
     class ContainerIterator{
     public:
-    ContainerIterator( Connection& dbHandle, 
+    ContainerIterator( PoolTransaction& pooldb, 
 		       const std::string& containername):
-      m_d(&dbHandle), m_collection(new pool::Collection<DataT>( &(pooldb.DataSvc()),"ImplicitCollection","PFN:" + pooldb.connectionString(),containername, pool::ICollection::READ )),m_it(m_collection->select()){
+      m_pooldb(&pooldb), m_collection(new pool::Collection<DataT>( &(pooldb.poolDataSvc()),"ImplicitCollection","PFN:" + pooldb.parentConnection().connectStr(),containername, pool::ICollection::READ )),m_it(m_collection->select()){
     }
-    Ref<DataT> dataRef(){
+    TypedRef<DataT> dataRef(){
       return m_data;
     }
     bool next(){ 
       if( m_it.next() ){
-	m_data=cond::Ref<DataT>(*m_pooldb,m_it.ref());
+	m_data=cond::TypedRef<DataT>(*m_pooldb,m_it.ref());
 	return true;
       }
       return false;
@@ -31,8 +31,8 @@ namespace cond{
       delete m_collection;
     }
     private:
-    mutable cond::Ref<DataT> m_data;
-    cond::Connection* m_pooldb;
+    mutable cond::TypedRef<DataT> m_data;
+    cond::PoolTransaction* m_pooldb;
     pool::Collection<DataT>* m_collection;
     typename pool::Collection<DataT>::Iterator m_it;
   };
