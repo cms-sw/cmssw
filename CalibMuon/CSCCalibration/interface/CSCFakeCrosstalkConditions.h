4c7adf335925248c9a64e5baf64b6e2ca843dd52
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-07-13 15:36:24 +0000

    merged Map and Conditions

--- CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkConditions.h
+++ CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkConditions.h
@@ -1,39 +1,41 @@
-#ifndef _CSCFAKECROSSTALKCONDITIONS_H
-#define _CSCFAKECROSSTALKCONDITIONS_H
+#ifndef _CSCFRONTIERCROSSTALKCONDITIONS_H
+#define _CSCFRONTIERCROSSTALKCONDITIONS_H
 
 #include <memory>
 #include "FWCore/Framework/interface/SourceFactory.h"
-
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/ESProducer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "FWCore/Framework/interface/EventSetupRecordIntervalFinder.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "CondFormats/CSCObjects/interface/CSCobject.h"
+#include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
 #include "CondFormats/DataRecord/interface/CSCcrosstalkRcd.h"
-#include "CalibMuon/CSCCalibration/interface/CSCFakeCrosstalkMap.h"
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
 
 class CSCFakeCrosstalkConditions: public edm::ESProducer, public edm::EventSetupRecordIntervalFinder  {
-   public:
-      CSCFakeCrosstalkConditions(const edm::ParameterSet&);
-      ~CSCFakeCrosstalkConditions();
+ public:
+  CSCFakeCrosstalkConditions(const edm::ParameterSet&);
+  ~CSCFakeCrosstalkConditions();
+  
+  float mean,min,minchi;
+  int seed;long int M;
 
-      typedef const  CSCcrosstalk * ReturnType;
-
-      ReturnType produceCrosstalk(const CSCcrosstalkRcd&);
-
-   private:
-      // ----------member data ---------------------------
-    void setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&, edm::ValidityInterval & );
-    
-    CSCFakeCrosstalkMap crosstalk;
+  void prefillFakeCrosstalk();
+  
+  typedef const  CSCcrosstalk * ReturnType;
+  
+  ReturnType produceCrosstalk(const CSCcrosstalkRcd&);
+  
+ private:
+  // ----------member data ---------------------------
+  void setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&, edm::ValidityInterval & );
+  CSCcrosstalk *cncrosstalk ;
+  
 };
 
 #endif
+
