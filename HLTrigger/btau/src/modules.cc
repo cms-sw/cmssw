960260de1d7c80d3716dab296e9650248b60af57
wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch>
2007-07-13 07:09:41 +0000

    fixed error

--- HLTrigger/btau/src/modules.cc
+++ HLTrigger/btau/src/modules.cc
@@ -3,7 +3,6 @@
 #include "HLTrigger/btau/interface/HLTJetTag.h"
 #include "HLTrigger/btau/interface/HLTTauL25DoubleFilter.h"
 #include "HLTrigger/btau/interface/HLTDisplacedmumuFilter.h"
-#include "HLTrigger/btau/interface/HLTL1MuonCorrector.h"
 #include "HLTrigger/btau/interface/HLTmumuGammaFilter.h"
 #include "HLTrigger/btau/interface/GetJetsFromHLTobject.h"
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducerFactory.h"
@@ -14,6 +13,5 @@ DEFINE_EDM_PLUGIN(TrackingRegionProducerFactory, L3MumuTrackingRegion, "L3MumuTr
 DEFINE_FWK_MODULE(HLTJetTag);
 DEFINE_FWK_MODULE(HLTTauL25DoubleFilter);
 DEFINE_FWK_MODULE(HLTDisplacedmumuFilter);
-DEFINE_FWK_MODULE(HLTL1MuonCorrector);
 DEFINE_FWK_MODULE(HLTmumuGammaFilter);
 DEFINE_FWK_MODULE(GetJetsFromHLTobject);
