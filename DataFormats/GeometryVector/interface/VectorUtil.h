ccb547f73dcd3ea8b79010afec3d1899071634bf
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-07-14 04:48:43 +0000

    less

--- DataFormats/GeometryVector/interface/VectorUtil.h
+++ DataFormats/GeometryVector/interface/VectorUtil.h
@@ -13,6 +13,38 @@ namespace Geom {
   using ROOT::Math::VectorUtil::Phi_0_2pi;
   using ROOT::Math::VectorUtil::Phi_mpi_pi;
   
+ 
+  
+  /** Definition of ordering of azimuthal angles.
+   *  phi1 is less than phi2 if the angle covered by a point going from
+   *  phi1 to phi2 in the counterclockwise direction is smaller than pi.
+   *  It makes sense only if ALL phis are in a single hemisphere...
+   */
+  bool phiLess( float phi1, float phi2) const {
+    float dphi = phi2-phi1; 
+    if ( diff < 0) diff += 2*M_PI;
+    return diff < pi;
+  }
+  bool phiLess(double phi1, double phi2) const {
+    double dphi = phi2-phi1; 
+    if ( diff < 0) diff += 2*M_PI;
+    return diff < pi;
+  }
+  template <class Vector1, class Vector2> 
+  bool phiLess(const Vector1 & v1, const Vector2 & v2) const {
+    return phiLess(v1.phi(),v2.phi()); 
+  }
+
+
+
+  /**
+     Find aximutal Angle difference between two generic vectors ( v2.Phi() - v1.Phi() ) 
+     The only requirements on the Vector classes is that they implement the Phi() method
+     \param v1  Vector of any type implementing the Phi() operator
+     \param v2  Vector of any type implementing the Phi() operator
+     \return  Phi difference
+     \f[ \Delta \phi = \phi_2 - \phi_1 \f]
+  */
   inline double deltaPhi(double phi1, double phi2) { 
     double dphi = phi2-phi1; 
     if ( dphi > M_PI ) {
@@ -22,16 +54,6 @@ namespace Geom {
     }
     return dphi;
   }
-  
-  
-  /**
-     Find aximutal Angle difference between two generic vectors ( v2.Phi() - v1.Phi() ) 
-     The only requirements on the Vector classes is that they implement the Phi() method
-     \param v1  Vector of any type implementing the Phi() operator
-     \param v2  Vector of any type implementing the Phi() operator
-     \return  Phi difference
-     \f[ \Delta \phi = \phi_2 - \phi_1 \f]
-  */
   template <class Vector1, class Vector2> 
   double deltaPhi( const Vector1 & v1, const Vector2 & v2) { 
     return deltaPhi(v1.phi(),v2.phi()); 
