4d81ea271e3688d0db1d70f430e88944b22f0875
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2007-07-14 20:35:35 +0000

    Add vector stuff to dictionaries for PhysicsTools::Calibration::MVAComputer stuff (vector of unsigned char and double to be specific), so that the BlobStream can read and write those objects. This does not contain any format changes!

--- CondFormats/PhysicsToolsObjects/src/classes.h
+++ CondFormats/PhysicsToolsObjects/src/classes.h
@@ -6,13 +6,18 @@ namespace { // anonymous
 
 // MVAComputer
 std::vector<unsigned char> v1;
-std::vector<std::string> v2;
-std::vector<Variable> v3;
-std::vector<PDF> v4;
-std::vector<ProcLikelihood::SigBkg> v5;
-std::pair<double, std::vector<double> > v6;
-std::pair<std::vector<ProcMLP::Neuron>, bool> v7;
-MVAComputerContainer::Entry v8;
-std::vector<MVAComputerContainer::Entry> v9;
+std::vector<unsigned char>::iterator v1i;
+std::vector<unsigned char>::const_iterator v1ci;
+std::vector<double> v2;
+std::vector<double>::iterator v2i;
+std::vector<double>::const_iterator v2ci;
+std::vector<std::string> v3;
+std::vector<Variable> v4;
+std::vector<PDF> v5;
+std::vector<ProcLikelihood::SigBkg> v6;
+std::pair<double, std::vector<double> > v7;
+std::pair<std::vector<ProcMLP::Neuron>, bool> v8;
+MVAComputerContainer::Entry v9;
+std::vector<MVAComputerContainer::Entry> v10;
 
 } // anonymous namespace
