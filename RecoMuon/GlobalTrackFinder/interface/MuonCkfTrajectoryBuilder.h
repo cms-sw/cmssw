d019add3dab47de343afe128838e6395e0986b01
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-07-14 00:02:30 +0000

    improving bare TrajectorySeed option

--- RecoMuon/GlobalTrackFinder/interface/MuonCkfTrajectoryBuilder.h
+++ RecoMuon/GlobalTrackFinder/interface/MuonCkfTrajectoryBuilder.h
@@ -9,19 +9,21 @@ class MuonCkfTrajectoryBuilder : public CkfTrajectoryBuilder {
 			   const TrajectoryStateUpdator*         updator,
 			   const Propagator*                     propagatorAlong,
 			   const Propagator*                     propagatorOpposite,
+			   const Propagator*                     propagatorProximity,
 			   const Chi2MeasurementEstimatorBase*   estimator,
 			   const TransientTrackingRecHitBuilder* RecHitBuilder,
 			   const MeasurementTracker*             measurementTracker);
   virtual ~MuonCkfTrajectoryBuilder();
   
  protected:
-  void collectMeasurement(const std::vector<const DetLayer*>& nl,const TrajectoryStateOnSurface & currentState, std::vector<TM>& result,int& invalidHits) const;
+  void collectMeasurement(const std::vector<const DetLayer*>& nl,const TrajectoryStateOnSurface & currentState, std::vector<TM>& result,int& invalidHits,const Propagator *) const;
 
   virtual void findCompatibleMeasurements( const TempTrajectory& traj, std::vector<TrajectoryMeasurement> & result) const;
   
   //and other fields
   bool theUseSeedLayer;
   double theRescaleErrorIfFail;
+  mutable const Propagator * theProximityPropagator;
 };
 
 
--- RecoMuon/GlobalTrackFinder/src/MuonCkfTrajectoryBuilder.cc
+++ RecoMuon/GlobalTrackFinder/src/MuonCkfTrajectoryBuilder.cc
@@ -7,15 +7,18 @@
 #include "RecoTracker/TkDetLayers/interface/GeometricSearchTracker.h"
 #include "TrackingTools/PatternTools/interface/TrajMeasLessEstim.h"
 
+#include <sstream>
 
 MuonCkfTrajectoryBuilder::MuonCkfTrajectoryBuilder(const edm::ParameterSet&              conf,
 						   const TrajectoryStateUpdator*         updator,
 						   const Propagator*                     propagatorAlong,
 						   const Propagator*                     propagatorOpposite,
+						   const Propagator*                     propagatorProximity,
 						   const Chi2MeasurementEstimatorBase*   estimator,
 						   const TransientTrackingRecHitBuilder* RecHitBuilder,
 						   const MeasurementTracker*             measurementTracker): 
-  CkfTrajectoryBuilder(conf,updator,propagatorAlong,propagatorOpposite,estimator,RecHitBuilder,measurementTracker)
+  CkfTrajectoryBuilder(conf,updator,propagatorAlong,propagatorOpposite,estimator,RecHitBuilder,measurementTracker),
+  theProximityPropagator(propagatorProximity)
 {
   //and something specific to me ?
   theUseSeedLayer = conf.getParameter<bool>("useSeedLayer");
@@ -26,11 +29,34 @@ MuonCkfTrajectoryBuilder::~MuonCkfTrajectoryBuilder()
 {  delete (CkfTrajectoryBuilder*)this;}
 
 
-void MuonCkfTrajectoryBuilder::collectMeasurement(const std::vector<const DetLayer*>& nl,const TrajectoryStateOnSurface & currentState, std::vector<TM>& result,int& invalidHits) const{
+std::string dumpMeasurement(const TrajectoryMeasurement & tm)
+{
+  std::stringstream buffer;
+  buffer<<"layer pointer: "<<tm.layer()<<"\n"
+        <<"estimate: "<<tm.estimate()<<"\n"
+        <<"forward state: \n"
+        <<"x: "<<tm.forwardPredictedState().globalPosition()<<"\n"
+        <<"p: "<<tm.forwardPredictedState().globalMomentum()<<"\n"
+        <<"geomdet pointer from rechit: "<<tm.recHit()->det()<<"\n"
+        <<"detId: "<<tm.recHit()->geographicalId().rawId();
+  return buffer.str();
+}
+std::string dumpMeasurements(const std::vector<TrajectoryMeasurement> & v)
+{
+  std::stringstream buffer;
+  buffer<<v.size()<<" total measurements\n";
+  for (std::vector<TrajectoryMeasurement>::const_iterator it = v.begin(); it!=v.end();++it){
+    buffer<<dumpMeasurement(*it);
+    buffer<<"\n";}
+  return buffer.str();
+}
+
+
+void MuonCkfTrajectoryBuilder::collectMeasurement(const std::vector<const DetLayer*>& nl,const TrajectoryStateOnSurface & currentState, std::vector<TM>& result,int& invalidHits, const Propagator * prop) const{
   for (std::vector<const DetLayer*>::const_iterator il = nl.begin();
        il != nl.end(); il++) {
     std::vector<TM> tmp =
-      theLayerMeasurements->measurements((**il),currentState, *theForwardPropagator, *theEstimator);
+      theLayerMeasurements->measurements((**il),currentState, *prop, *theEstimator);
     
     LogDebug("CkfPattern")<<tmp.size()<<" measurements returned by LayerMeasurements";
     
@@ -48,16 +74,12 @@ void MuonCkfTrajectoryBuilder::collectMeasurement(const std::vector<const DetLay
     }
   }
   
-  LogDebug("CkfPattern")<<result.size()<<" total measurements";
-  for (std::vector<TrajectoryMeasurement>::iterator it = result.begin(); it!=result.end();++it){
-    LogDebug("CkfPattern")<<"layer pointer: "<<it->layer()<<"\n"
-                          <<"estimate: "<<it->estimate()<<"\n"
-                          <<"forward state: \n"<<it->forwardPredictedState()
-                          <<"geomdet pointer from rechit: "<<it->recHit()->det()<<"\n"
-                          <<"detId: "<<it->recHit()->geographicalId().rawId();
-  }
-  
+  LogDebug("CkfPattern")<<"starting from:\n"
+                        <<"x: "<<currentState.globalPosition()<<"\n"
+                        <<"p: "<<currentState.globalMomentum()<<"\n"
+                        <<dumpMeasurements(result);
 }
+ 
 
 
 void 
@@ -71,7 +93,7 @@ MuonCkfTrajectoryBuilder::findCompatibleMeasurements( const TempTrajectory& traj
 
   if (traj.empty())
     {
-      edm::LogInfo("CkfPattern")<<"using JR patch for no measurement case";           
+      LogDebug("CkfPattern")<<"using JR patch for no measurement case";           
      //what if there are no measurement on the Trajectory
 
       //set the currentState to be the one from the trajectory seed starting point
@@ -86,50 +108,46 @@ MuonCkfTrajectoryBuilder::findCompatibleMeasurements( const TempTrajectory& traj
       const DetLayer * l=theMeasurementTracker->geometricSearchTracker()->detLayer(id);
 
       if (theUseSeedLayer){
-        //get the measurementsin the the layer first
-        //      if ( traj.direction() == alongMomentum ){
         {
-	  edm::LogInfo("CkfPattern")<<"using the layer of the seed first.";
-          //will fail if the building is outside-in
-          //because the propagator will cross over the barrel and give measurement on the other side of the barrel
-          nl.clear();
+	  //get the measurements on the layer first
+	  LogDebug("CkfPattern")<<"using the layer of the seed first.";
           nl.push_back(l);
-          collectMeasurement(nl,currentState,result,invalidHits);
+          collectMeasurement(nl,currentState,result,invalidHits,theProximityPropagator);
         }
-
-
+	
         //if fails: try to rescale locally the state to find measurements
-        if ((result.size()==0 || ((uint)invalidHits==result.size())) && theRescaleErrorIfFail!=1.0)
+        if ((uint)invalidHits==result.size() && theRescaleErrorIfFail!=1.0 && result.size()!=0)
           {
 	    result.clear();
-	    edm::LogInfo("CkfPattern")<<"using a rescale by "<< theRescaleErrorIfFail <<" to find measurements.";
+	    LogDebug("CkfPattern")<<"using a rescale by "<< theRescaleErrorIfFail <<" to find measurements.";
 	    TrajectoryStateOnSurface rescaledCurrentState = currentState;
 	    rescaledCurrentState.rescaleError(theRescaleErrorIfFail);
 	    invalidHits=0;
-	    collectMeasurement(nl,rescaledCurrentState,result,invalidHits);
+	    result.clear();
+	    collectMeasurement(nl,rescaledCurrentState,result,invalidHits,theProximityPropagator);
           }
       }
 
       //if fails: go to next layers
-      if (result.size()==0 || ((uint)invalidHits==result.size()))
+      if (result.size()==0 || (uint)invalidHits==result.size())
         {
           result.clear();
-	  edm::LogInfo("CkfPattern")<<"using JR patch: need to go to next layer to get measurements";
+	  LogDebug("CkfPattern")<<"Need to go to next layer to get measurements";
           //the following will "JUMP" the first layer measurements
-          nl = l->nextLayers(*currentState.freeState(), traj.direction());
+	  nl = l->nextLayers(*currentState.freeState(), traj.direction());
           invalidHits=0;
-          collectMeasurement(nl,currentState,result,invalidHits);
+          collectMeasurement(nl,currentState,result,invalidHits,theForwardPropagator);
         }
 
       //if fails: this is on the next layers already, try rescaling locally the state
-      if ((result.size()==0 || ((uint)invalidHits==result.size())) && theRescaleErrorIfFail!=1.0)
+      if (result.size()!=0 && (uint)invalidHits==result.size() && theRescaleErrorIfFail!=1.0)
         {
           result.clear();
-	  edm::LogInfo("CkfPattern")<<"using a rescale by "<< theRescaleErrorIfFail <<" to find measurements on next layers.";
+	  LogDebug("CkfPattern")<<"using a rescale by "<< theRescaleErrorIfFail <<" to find measurements on next layers.";
           TrajectoryStateOnSurface rescaledCurrentState = currentState;
           rescaledCurrentState.rescaleError(theRescaleErrorIfFail);
           invalidHits=0;
-          collectMeasurement(nl,rescaledCurrentState, result,invalidHits);
+          collectMeasurement(nl,rescaledCurrentState, result,invalidHits,theForwardPropagator);
         }
 
     }
@@ -139,23 +157,9 @@ MuonCkfTrajectoryBuilder::findCompatibleMeasurements( const TempTrajectory& traj
       TSOS currentState( traj.lastMeasurement().updatedState());
 
       nl = traj.lastLayer()->nextLayers( *currentState.freeState(), traj.direction());
-  
-      if (nl.empty()) return;
+      if (nl.empty()){LogDebug("CkfPattern")<<" no next layers... going "<<traj.direction()<<"\n from: \n"<<currentState<<"\n from detId: "<<traj.lastMeasurement().recHit()->geographicalId().rawId(); return ;}
 
-      for (std::vector<const DetLayer*>::iterator il = nl.begin(); 
-	   il != nl.end(); il++) {
-	std::vector<TM> tmp = 
-	  theLayerMeasurements->measurements((**il),currentState, *theForwardPropagator, *theEstimator);
-
-	if ( !tmp.empty()) {
-	  if ( result.empty()) result = tmp;
-	  else {
-	    // keep one dummy TM at the end, skip the others
-	    result.insert( result.end()-invalidHits, tmp.begin(), tmp.end());
-	  }
-	  invalidHits++;
-	}
-      }
+      collectMeasurement(nl,currentState,result,invalidHits,theForwardPropagator);
     }
 
 
