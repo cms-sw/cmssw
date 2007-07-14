21a4d05b1f289c0715ba23e3530a0b004376dbd4
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-07-14 15:40:23 +0000

    Changed traits for internal RefToBase and RefToBaseVector implementation in order to compile cases where a RefToBase is not initialized from a Ref<C> or RefProd<C> that were previously not supported. This case would create a RefToBase<T> that can't be inserted in a RefToBaseVector<T>, so it could cause crashes at run time if used in this way.
    This was introduced to allow compiling the following packages
    that create a RefToBase<Candidate> from a RefToBase<Jet>:
    
       HLTrigger/btau and
       HLTrigger/xchannel
    
    "up-cast" of RefToBase<T> is not supported as it was intended
    in the above packages. If really needed, a new implementation
    should be provided for those cases.

--- DataFormats/Common/interface/HolderToVectorTrait.h
+++ DataFormats/Common/interface/HolderToVectorTrait.h
@@ -1,17 +1,42 @@
 #ifndef DataFormats_Common_HolderToVectorTrait_h
 #define DataFormats_Common_HolderToVectorTrait_h
-#include <boost/static_assert.hpp>
+#include "FWCore/Utilities/interface/EDMException.h"
+//#include <boost/static_assert.hpp>
 
 namespace edm {
   namespace reftobase {
-    template<typename T, typename REF>
-    struct HolderToVectorTrait {
-      BOOST_STATIC_ASSERT(sizeof(REF) == 0); 
+
+    template <typename T, typename REF>
+    struct InvaidHolderToVector {
+      static  std::auto_ptr<BaseVectorHolder<T> > makeVectorHolder() {
+	throw edm::Exception(errors::InvalidReference)
+	  << "InvaidHolderToVector: trying to use RefToBase built with "
+	  << "an internal type. RefToBase should be built passing an "
+	  << "object of type edm::Ref<C>. This exception should never "
+	  << "be thrown if a RefToBase was built from a RefProd<C>.";
+      }
     };
 
+    template<typename T, typename REF>
+    struct HolderToVectorTrait {
+      //      BOOST_STATIC_ASSERT(sizeof(REF) == 0); 
+      typedef InvaidHolderToVector<T, REF> type;
+    };
+
+    template <typename REF>
+    struct InvalidRefHolderToRefVector {
+      static std::auto_ptr<RefVectorHolderBase> makeVectorHolder() {
+	throw edm::Exception(errors::InvalidReference)
+	  << "InvaidRefHolderToRefVector: trying to use RefToBaseVector built with "
+	  << "an internal type. RefToBase should be built passing an "
+	  << "object of type edm::RefVector<C>";
+      }
+    };
+    
     template<typename REF>
     struct RefHolderToRefVectorTrait {
-      BOOST_STATIC_ASSERT(sizeof(REF) == 0); 
+      //      BOOST_STATIC_ASSERT(sizeof(REF) == 0); 
+      typedef InvalidRefHolderToRefVector<REF> type;
     };
 
   }
