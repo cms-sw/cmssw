15ba32a7c6a040bab6220885e67f874018fd3021
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2007-07-16 10:13:09 +0000

    added TrackMomConstraintAssociationCollection and TrackVtxConstraintAssociation

--- TrackingTools/PatternTools/src/classes.h
+++ TrackingTools/PatternTools/src/classes.h
@@ -3,6 +3,7 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "TrackingTools/PatternTools/interface/Trajectory.h"
 #include "TrackingTools/PatternTools/interface/TrajTrackAssociation.h"
+#include "TrackingTools/PatternTools/interface/TrackConstraintAssociation.h"
 #include <vector>
 
 namespace {
@@ -16,6 +17,26 @@ namespace {
     TrajTrackAssociationRef rttam;
     TrajTrackAssociationRefProd rpttam;
     TrajTrackAssociationRefVector rvttam;
+
+    std::vector<MomentumConstraint> j2;
+    edm::Wrapper<std::vector<MomentumConstraint> > j3;
+  
+    TrackMomConstraintAssociationCollection i1;
+    edm::Wrapper<TrackMomConstraintAssociationCollection> i2;
+    TrackMomConstraintAssociation i3;
+    TrackMomConstraintAssociationRef i4;
+    TrackMomConstraintAssociationRefProd i5;
+    TrackMomConstraintAssociationRefVector i6;
+  
+    std::vector<VertexConstraint> jj2;
+    edm::Wrapper<std::vector<VertexConstraint> > jj3;
+  
+    TrackVtxConstraintAssociationCollection ii1;
+    edm::Wrapper<TrackVtxConstraintAssociationCollection> ii2;
+    TrackVtxConstraintAssociation ii3;
+    TrackVtxConstraintAssociationRef ii4;
+    TrackVtxConstraintAssociationRefProd ii5;
+    TrackVtxConstraintAssociationRefVector ii6;
   
   }
 }
