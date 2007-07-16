0ee5ab4162ca8888165296a044e1491948616c6c
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2007-07-16 17:33:52 +0000

    *** empty log message ***

---
+++ CondFormats/EcalCorrections/src/T_EventSetup_EcalGlobalShowerContainmentCorrectionsVsEta.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/EcalCorrections/interface/EcalGlobalShowerContainmentCorrectionsVsEta.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(EcalGlobalShowerContainmentCorrectionsVsEta);
