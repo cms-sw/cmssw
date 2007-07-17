ba9fa790fd69a8aa65c0706b9417b93462638da7
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-07-17 13:19:42 +0000

    added RCT Records

---
+++ CondFormats/DataRecord/interface/L1RCTParametersRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormatsDataRecord_L1RCTParametersRcd_h
+#define CondFormatsDataRecord_L1RCTParametersRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class L1RCTParametersRcd : public edm::eventsetup::EventSetupRecordImplementation<L1RCTParametersRcd> {};
+
+#endif
