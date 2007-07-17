b2d3df29e2212aac9a0ecd8aa4aff42dfa214ad4
Leonard Apanasevich <sha1-81bf5b6d8589229bad93440d7931007f605561fa@cern.ch>
2007-07-17 19:28:07 +0000

    Update configuration files and paths

---
+++ HLTrigger/JetMET/src/HLTRapGapFilter.cc
@@ -0,0 +1,105 @@
+/** \class HLTRapGapFilter
+ *
+ * $Id: HLTRapGapFilter.cc,v 1.2 2007/03/08 22:46:25 apana Exp $
+ *
+ *  \author Monica Vazquez Acosta (CERN)
+ *
+ */
+
+#include "HLTrigger/JetMET/interface/HLTRapGapFilter.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/HLTReco/interface/HLTFilterObject.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/JetReco/interface/CaloJet.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+
+//
+// constructors and destructor
+//
+HLTRapGapFilter::HLTRapGapFilter(const edm::ParameterSet& iConfig)
+{
+   inputTag_ = iConfig.getParameter< edm::InputTag > ("inputTag");
+   absEtaMin_   = iConfig.getParameter<double> ("minEta");
+   absEtaMax_= iConfig.getParameter<double> ("maxEta"); 
+   caloThresh_= iConfig.getParameter<double> ("caloThresh"); 
+
+   //register your products
+   produces<reco::HLTFilterObjectWithRefs>();
+}
+
+HLTRapGapFilter::~HLTRapGapFilter(){}
+
+
+// ------------ method called to produce the data  ------------
+bool
+HLTRapGapFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  // The filter object
+  std::auto_ptr<reco::HLTFilterObjectWithRefs> filterproduct (new reco::HLTFilterObjectWithRefs(path(),module()));
+  // Ref to Candidate object to be recorded in filter object
+  edm::RefToBase<reco::Candidate> ref;
+  // Get the recoEcalCandidates
+
+  edm::Handle<reco::CaloJetCollection> recocalojets;
+  iEvent.getByLabel(inputTag_,recocalojets);
+
+  // look at all candidates,  check cuts and add to filter object
+  int n(0);
+  
+  std::cout << "Found " << recocalojets->size() << " jets in this event" << std::endl;
+
+  if(recocalojets->size() > 1){
+    // events with two or more jets
+
+    double etjet=0.;
+    double etajet=0.;
+    double sumets=0.;
+    int countjets =0;
+
+    for (reco::CaloJetCollection::const_iterator recocalojet = recocalojets->begin(); 
+	 recocalojet<=(recocalojets->begin()+1); recocalojet++) {
+      
+      etjet = recocalojet->energy();
+      etajet = recocalojet->eta();
+      
+      if(fabs(etajet) > absEtaMin_ && fabs(etajet) < absEtaMax_)
+	{
+	  sumets += etjet;
+	  std::cout << "Adding jet with eta = " << etajet << ", and e = " 
+		    << etjet << std::endl;
+	}
+      countjets++;
+    }
+
+    std::cout << "Sum jet energy = " << sumets << std::endl;
+    if(sumets<=caloThresh_){
+      std::cout << "Passed filter!" << std::endl;
+      for (reco::CaloJetCollection::const_iterator recocalojet = recocalojets->begin(); 
+	   recocalojet<=(recocalojets->begin()+1); recocalojet++) {
+	ref=edm::RefToBase<reco::Candidate>(reco::CaloJetRef(recocalojets,
+							     distance(recocalojets->begin(),recocalojet)));
+	filterproduct->putParticle(ref);
+	n++;
+      }
+    }
+    
+  } // events with two or more jets
+  
+  
+  
+  // filter decision
+  bool accept(n>=0);
+  
+  // put filter object into the Event
+  iEvent.put(filterproduct);
+  
+  return accept;
+}
--- HLTrigger/JetMET/src/SealModule.cc
+++ HLTrigger/JetMET/src/SealModule.cc
@@ -6,6 +6,7 @@
 #include "HLTrigger/JetMET/interface/HLTPhi2METFilter.h"
 #include "HLTrigger/JetMET/interface/HLTAcoFilter.h"
 #include "HLTrigger/JetMET/interface/HLTDiJetAveFilter.h"
+#include "HLTrigger/JetMET/interface/HLTRapGapFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTJetVBFFilter);
@@ -13,3 +14,4 @@ DEFINE_ANOTHER_FWK_MODULE(HLTNVFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTPhi2METFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTAcoFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTDiJetAveFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTRapGapFilter);
