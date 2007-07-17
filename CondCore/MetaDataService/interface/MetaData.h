e256002061bb893786ad10c641c2d48d8083fb8e
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-07-17 16:15:07 +0000

    adopt to new interface

--- CondCore/MetaDataService/interface/MetaData.h
+++ CondCore/MetaDataService/interface/MetaData.h
@@ -2,14 +2,11 @@
 #define CondCore_MetaDataService_METADATA_H
 #include <string>
 #include <vector>
-namespace coral{
-  class ISessionProxy;
-}
 namespace cond{
-  class RelationalStorageManager;
+  class CoralTransaction;
   class MetaData {
   public:
-    explicit MetaData( cond::RelationalStorageManager& coraldb );
+    explicit MetaData( cond::CoralTransaction& coraldb );
     ~MetaData();
     bool addMapping(const std::string& name, const std::string& token);
     bool replaceToken(const std::string& name, const std::string& newtoken);
@@ -21,8 +18,8 @@ namespace cond{
     void deleteEntryByTag( const std::string& tag );
   private:
     void createTable(const std::string& tabname);
-    cond::RelationalStorageManager& m_coraldb;
-    coral::ISessionProxy* m_proxy;
+    cond::CoralTransaction& m_coraldb;
   };
 }
 #endif
+ 
--- CondCore/MetaDataService/src/MetaData.cc
+++ CondCore/MetaDataService/src/MetaData.cc
@@ -2,29 +2,19 @@
 #include "CondCore/MetaDataService/interface/MetaDataNames.h"
 #include "CondCore/MetaDataService/interface/MetaDataExceptions.h"
 #include "CondCore/DBCommon/interface/Exception.h"
-#include "CondCore/DBCommon/interface/RelationalStorageManager.h"
-//#include "RelationalAccess/IRelationalService.h"
-//#include "RelationalAccess/IConnectionService.h"
-//#include "RelationalAccess/IConnectionServiceConfiguration.h"
-//#include "RelationalAccess/RelationalServiceException.h"
-//#include "RelationalAccess/IRelationalDomain.h"
+#include "CondCore/DBCommon/interface/CoralTransaction.h"
 #include "RelationalAccess/SchemaException.h"
-//#include "RelationalAccess/ISession.h"
-#include "RelationalAccess/ISessionProxy.h"
-//#include "RelationalAccess/ITransaction.h"
 #include "RelationalAccess/ISchema.h"
 #include "RelationalAccess/ITable.h"
 #include "RelationalAccess/TableDescription.h"
 #include "RelationalAccess/ITablePrivilegeManager.h"
-//#include "RelationalAccess/IPrimaryKey.h"
 #include "RelationalAccess/ICursor.h"
 #include "RelationalAccess/IQuery.h"
 #include "RelationalAccess/ITableDataEditor.h"
-//#include "CoralBase/Exception.h"
 #include "CoralBase/AttributeList.h"
 #include "CoralBase/AttributeSpecification.h"
 #include "CoralBase/Attribute.h"
-cond::MetaData::MetaData(cond::RelationalStorageManager& coraldb):m_coraldb(coraldb){
+cond::MetaData::MetaData(cond::CoralTransaction& coraldb):m_coraldb(coraldb){
 }
 cond::MetaData::~MetaData(){
 }
@@ -35,7 +25,7 @@ bool cond::MetaData::addMapping(const std::string& name, const std::string& iovt
     }catch( const coral::TableAlreadyExistingException& er ){
       //std::cout<<"table alreay existing, not creating a new one"<<std::endl;
     }
-    coral::ITable& mytable=m_coraldb.sessionProxy().nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
+    coral::ITable& mytable=m_coraldb.nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
     coral::AttributeList rowBuffer;
     coral::ITableDataEditor& dataEditor = mytable.dataEditor();
     dataEditor.rowBuffer( rowBuffer );
@@ -53,10 +43,10 @@ bool cond::MetaData::addMapping(const std::string& name, const std::string& iovt
 }
 bool cond::MetaData::replaceToken(const std::string& name, const std::string& newtoken){
   try{
-    if(!m_coraldb.sessionProxy().nominalSchema().existsTable(cond::MetaDataNames::metadataTable())){
+    if(!m_coraldb.nominalSchema().existsTable(cond::MetaDataNames::metadataTable())){
       throw cond::Exception( "MetaData::replaceToken MetaData table doesnot exist" );
     }
-    coral::ITable& mytable=m_coraldb.sessionProxy().nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
+    coral::ITable& mytable=m_coraldb.nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
     coral::AttributeList inputData;
     coral::ITableDataEditor& dataEditor = mytable.dataEditor();
     inputData.extend<std::string>("newToken");
@@ -80,7 +70,7 @@ bool cond::MetaData::replaceToken(const std::string& name, const std::string& ne
 const std::string cond::MetaData::getToken( const std::string& name ){
   std::string iovtoken;
   try{
-    coral::ITable& mytable=m_coraldb.sessionProxy().nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
+    coral::ITable& mytable=m_coraldb.nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
     std::auto_ptr< coral::IQuery > query(mytable.newQuery());
     query->setRowCacheSize( 100 );
     coral::AttributeList emptyBindVariableList;
@@ -105,7 +95,7 @@ const std::string cond::MetaData::getToken( const std::string& name ){
   return iovtoken;
 }
 void cond::MetaData::createTable(const std::string& tabname){
-  coral::ISchema& schema=m_coraldb.sessionProxy().nominalSchema();
+  coral::ISchema& schema=m_coraldb.nominalSchema();
   coral::TableDescription description;
   description.setName( tabname );
   description.insertColumn(  cond::MetaDataNames::tagColumn(), coral::AttributeSpecification::typeNameForId( typeid(std::string)) );
@@ -120,7 +110,7 @@ void cond::MetaData::createTable(const std::string& tabname){
 bool cond::MetaData::hasTag( const std::string& name ) const{
   bool result=false;
   try{
-    coral::ITable& mytable=m_coraldb.sessionProxy().nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
+    coral::ITable& mytable=m_coraldb.nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
     std::auto_ptr< coral::IQuery > query(mytable.newQuery());
     coral::AttributeList emptyBindVariableList;
     std::string condition=cond::MetaDataNames::tagColumn()+" = '"+name+"'";
@@ -139,7 +129,7 @@ bool cond::MetaData::hasTag( const std::string& name ) const{
 }
 void cond::MetaData::listAllTags( std::vector<std::string>& result ) const{
   try{
-    coral::ITable& mytable=m_coraldb.sessionProxy().nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
+    coral::ITable& mytable=m_coraldb.nominalSchema().tableHandle( cond::MetaDataNames::metadataTable() );
     std::auto_ptr< coral::IQuery > query(mytable.newQuery());
     query->addToOutputList( cond::MetaDataNames::tagColumn() );
     query->setMemoryCacheSize( 100 );
@@ -160,7 +150,7 @@ void cond::MetaData::listAllTags( std::vector<std::string>& result ) const{
 void cond::MetaData::deleteAllEntries(){
   coral::AttributeList emptybinddata;
   try{
-    coral::ITable& table=m_coraldb.sessionProxy().nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
+    coral::ITable& table=m_coraldb.nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
     coral::ITableDataEditor& dataEditor = table.dataEditor();
     dataEditor.deleteRows( "",emptybinddata ); 
   }catch(const coral::TableNotExistingException& er){
@@ -177,7 +167,7 @@ void cond::MetaData::deleteEntryByToken( const std::string& token ){
   deletecondition[cond::MetaDataNames::tokenColumn()].data<std::string>()=token;
   std::string whereClause=cond::MetaDataNames::tokenColumn()+"=:"+cond::MetaDataNames::tokenColumn();
   try{
-    coral::ITable& table=m_coraldb.sessionProxy().nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
+    coral::ITable& table=m_coraldb.nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
     coral::ITableDataEditor& dataEditor = table.dataEditor();
     dataEditor.deleteRows(whereClause,deletecondition ); 
     return;
@@ -195,7 +185,7 @@ void cond::MetaData::deleteEntryByTag( const std::string& tag ){
   deletecondition[cond::MetaDataNames::tagColumn()].data<std::string>()=tag;
   std::string whereClause=cond::MetaDataNames::tagColumn()+"=:"+cond::MetaDataNames::tagColumn();
   try{
-    coral::ITable& table=m_coraldb.sessionProxy().nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
+    coral::ITable& table=m_coraldb.nominalSchema().tableHandle(cond::MetaDataNames::metadataTable());
     coral::ITableDataEditor& dataEditor = table.dataEditor();
     dataEditor.deleteRows(whereClause,deletecondition ); 
   }catch(const coral::TableNotExistingException& er){
