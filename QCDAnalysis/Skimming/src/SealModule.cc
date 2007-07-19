bb53bb62224eb0394d80486dc65c0d1c029255f2
Andreas Oehler <sha1-bf17a625e96eed2dfa3f3b53d79864d479019360@cern.ch>
2007-07-19 16:07:26 +0000

    FilterStream for CSA07Exercise + QCDSingleJetFilter to do it

---
+++ QCDAnalysis/Skimming/src/SealModule.cc
@@ -0,0 +1,9 @@
+//Stupid Framework-stuff
+
+#include <PluginManager/ModuleDef.h>
+#include <FWCore/Framework/interface/MakerMacros.h>
+
+#include "QCDAnalysis/Skimming/interface/QCDSingleJetFilter.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(QCDSingleJetFilter);
