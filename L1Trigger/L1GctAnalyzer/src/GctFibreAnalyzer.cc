265f91f1dd25fe7c0bc648658b585c1eb0067f7f
Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch>
2007-07-19 20:17:19 +0000

    Added checks on the fibre data.

--- L1Trigger/L1GctAnalyzer/src/GctFibreAnalyzer.cc
+++ L1Trigger/L1GctAnalyzer/src/GctFibreAnalyzer.cc
@@ -11,7 +11,7 @@ Description: Analyzer individual fibre channels from the source card.
 //
 // Original Author:  Alex Tapper
 //         Created:  Thu Jul 12 14:21:06 CEST 2007
-// $Id$
+// $Id: GctFibreAnalyzer.cc,v 1.1 2007/07/18 13:14:07 tapper Exp $
 //
 //
 
@@ -38,26 +38,49 @@ void GctFibreAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   Handle<L1GctFibreCollection> fibre;
   iEvent.getByLabel(m_fibreSource,fibre);
 
-  std::cout << "New event! " << fibre->size()<< " fibres" << std::endl;
-   
+  bool bc0= false;
+
   for (L1GctFibreCollection::const_iterator f=fibre->begin(); f!=fibre->end(); f++){
-    std::cout << "data=" << f->data() 
+
+    // This is where we'll check it the logical ID corresponds to what we expect
+    // logical id pattern= (logicalID<<8) | (sfpNo+1) for sfpNo=0,1,2,3
+    // source card type = 0 for electron cards
+    // source card local ID = source card type (+4 if RCt crate >=9)
+    // source card logical ID = 8*(RCT phi region 0-8) + source card local ID
+
+    // For now write them out
+    std::cout << std::hex
+              << "data=" << f->data() 
               << " block=" << f->block() 
               << " index=" << f->index() 
               << " bx=" << f->bx()
               << std::endl;
+
+    // Check that the last bit on cycle 1 is set as it should be
+    if (!(f->data() & 0x80000000)){
+      std::cout << "Error in fibre data, b15 on cycle 1 not set! Data corrupt." << std::endl;
+      std::cout << std::hex
+                << "data=" << f->data() 
+                << " block=" << f->block() 
+                << " index=" << f->index() 
+                << " bx=" << f->bx()
+                << std::endl;
+    }
+
+    // Check for BC0 on this event
+    if ((f->data() & 0x8000) && (f==fibre->begin())) bc0=true;
+
+    // If this event has a BC0 then check this fibre sees it too
+    if (bc0 && !(f->data() & 0x8000)){
+      std::cout << "Error in fibre data, missing BC0! Data corrupt." << std::endl;
+      std::cout << std::hex
+                << "data=" << f->data() 
+                << " block=" << f->block() 
+                << " index=" << f->index() 
+                << " bx=" << f->bx()
+                << std::endl;
+    }
   }   
-
-//   uint32_t data() const { return data_; }
-//   uint16_t block() const { return block_; }
-//   uint16_t index() const { return index_; }
-  
-//   source card type = 0 for electron cards
-
-//   source card local ID = source card type (+4 if RCt crate >=9)
-
-//   source card logical ID = 8*(RCT phi region 0-8) + source card local ID
-
 }
 
 
