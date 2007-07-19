c8ba90d89a4c30c4b244224159df2e22b6e03981
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2007-07-19 12:00:29 +0000

    - fix bug when storing integrity errors to the DB

--- DQM/EcalBarrelMonitorClient/src/EBIntegrityClient.cc
+++ DQM/EcalBarrelMonitorClient/src/EBIntegrityClient.cc
@@ -2,8 +2,8 @@
 /*
  * \file EBIntegrityClient.cc
  *
- * $Date: 2007/05/22 15:05:46 $
- * $Revision: 1.150 $
+ * $Date: 2007/06/24 09:41:11 $
+ * $Revision: 1.151 $
  * \author G. Della Ricca
  * \author G. Franzoni
  *
@@ -747,7 +747,7 @@ bool EBIntegrityClient::writeDb(EcalCondDBInterface* econn, RunIOV* runiov, MonR
       if ( dataset1.size() != 0 ) econn->insertDataArraySet(&dataset1, moniov);
       if ( dataset2.size() != 0 ) econn->insertDataArraySet(&dataset2, moniov);
       if ( dataset3.size() != 0 ) econn->insertDataArraySet(&dataset3, moniov);
-      if ( dataset4.size() != 0 ) econn->insertDataArraySet(&dataset3, moniov);
+      if ( dataset4.size() != 0 ) econn->insertDataArraySet(&dataset4, moniov);
       cout << "done." << endl;
     } catch (runtime_error &e) {
       cerr << e.what() << endl;
