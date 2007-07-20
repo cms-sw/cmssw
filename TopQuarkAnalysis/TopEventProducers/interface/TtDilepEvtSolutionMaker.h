33fe028d403abc648e456d5924a5cc0285fe4358
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-07-20 06:52:09 +0000

    Adapted for new TtDilepEvtSolution format with ref's.

--- TopQuarkAnalysis/TopEventProducers/interface/TtDilepEvtSolutionMaker.h
+++ TopQuarkAnalysis/TopEventProducers/interface/TtDilepEvtSolutionMaker.h
@@ -1,38 +1,41 @@
-#include <memory>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <fstream>
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
-#include "AnalysisDataFormats/TopObjects/interface/TtDilepEvtSolution.h"
-#include "TopQuarkAnalysis/TopKinFitter/interface/TtDilepKinSolver.h"
+
+#include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
+
 
 class TtDilepEvtSolutionMaker : public edm::EDProducer {
- public:
-  explicit TtDilepEvtSolutionMaker(const edm::ParameterSet&);
-  ~TtDilepEvtSolutionMaker();
+
+  public:
+
+    explicit TtDilepEvtSolutionMaker(const edm::ParameterSet & iConfig);
+    ~TtDilepEvtSolutionMaker();
   
-  virtual void produce(edm::Event&, const edm::EventSetup&);
- private:  
-  //next methods are avoidable but they make the code legible
-  bool PTComp(TopElectron e, TopMuon m);
-  bool LepDiffCharge(TopElectron e, TopMuon m);
-  bool LepDiffCharge(TopElectron e1, TopElectron e2);
-  bool LepDiffCharge(TopMuon m1, TopMuon m2);
-  bool HasPositiveCharge(TopMuon m);
-  bool HasPositiveCharge(TopElectron e);
-  
-  edm::InputTag electronSource_;
-  edm::InputTag muonSource_;
-  edm::InputTag metSource_;
-  edm::InputTag jetSource_;
-  bool matchToGenEvt_, calcTopMass_;
-  bool eeChannel_, emuChannel_, mumuChannel_;
-  double tmassbegin_, tmassend_, tmassstep_;
+    virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
+
+  private:  
+
+    // next methods are avoidable but they make the code legible
+    bool PTComp(const TopElectron * e, const TopMuon * m) const;
+    bool LepDiffCharge(const TopElectron * e, const TopMuon * m) const;
+    bool LepDiffCharge(const TopElectron * e1, const TopElectron * e2) const;
+    bool LepDiffCharge(const TopMuon * m1, const TopMuon * m2) const;
+    bool HasPositiveCharge(const TopMuon * m) const;
+    bool HasPositiveCharge(const TopElectron * e) const;
+
+  private:
+
+    edm::InputTag electronSource_;
+    edm::InputTag muonSource_;
+    edm::InputTag metSource_;
+    edm::InputTag jetSource_;
+    bool matchToGenEvt_, calcTopMass_;
+    bool eeChannel_, emuChannel_, mumuChannel_;
+    double tmassbegin_, tmassend_, tmassstep_;
+
 };
