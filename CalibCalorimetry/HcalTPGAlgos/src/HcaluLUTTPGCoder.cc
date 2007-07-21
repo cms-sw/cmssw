25cd138771b43865d18ff9e4cb1c2a95c9df3df7
Tulika Bose <sha1-8b8c25cc70f9a5790e3c425b034aa99249523492@cern.ch>
2007-07-21 05:15:54 +0000

    Proper array deletes

--- CalibCalorimetry/HcalTPGAlgos/src/HcaluLUTTPGCoder.cc
+++ CalibCalorimetry/HcalTPGAlgos/src/HcaluLUTTPGCoder.cc
@@ -28,11 +28,10 @@ void HcaluLUTTPGCoder::compress(const IntegerCaloSamples& ics, const std::vector
 }
 
 HcaluLUTTPGCoder::~HcaluLUTTPGCoder() {
-  for (int i = 0; i < nluts; i++) {
-    if (inputLUT[i] != 0) delete inputLUT[i];
-  }
-  delete _gain;
-  delete _ped;
+  delete [] inputLUT;
+  delete [] _gain;
+  delete [] _ped;
+ 
 }
 
 void HcaluLUTTPGCoder::AllocateLUTs() {
