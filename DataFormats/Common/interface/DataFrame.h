e855f429a4688fe4c3c5551007743a65f291c5db
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-07-24 04:32:10 +0000

    an optimized contanier for rawdata and digis

---
+++ DataFormats/Common/interface/DataFrame.h
@@ -0,0 +1,58 @@
+#ifndef Common_DATAFRAME_H
+#define Common_DATAFRAME_H
+
+namespace edm {
+
+  class DataFrameContainer;
+  
+  /* a proxy to a fixed size array of 16bit words belonging to
+   * a "channel" identified by an 32bit id
+   *
+   * FIXME interface to be finalized once use-cases fully identified
+   * 
+   */
+  class DataFrame {
+  public: 
+    
+    typedef unsigned int id_type;
+    typedef unsigned short data_type;
+    
+    
+    inline
+    DataFrame() : m_id(0), m_data(0), m_size(0){}
+    inline
+    DataFrame(id_type i, data_type const * idata, int isize) :
+      m_id(i), m_data(idata), m_size(isize) {}
+    
+    inline
+    DataFrame(DataFrameContainer const & icont,
+	      size_t i);
+    inline
+    data_type & operator[](size_t i) {
+      return data()[i];
+    }
+    
+    
+    inline
+    data_type operator[](size_t i) const {
+      return m_data[i];
+    }
+    
+    inline
+    id_type id() const { return m_id;}
+    
+    inline
+    int size() const { return m_size; }
+    
+  private:
+    data_type * data() {
+      return const_cast<data_type *>(m_data);
+    }
+    
+    id_type m_id;
+    data_type const * m_data;
+    int m_size;
+  };
+}
+
+#endif // Common_DATAFRAME_H
