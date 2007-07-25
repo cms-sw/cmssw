d667a6744dfdade54608d1d54eb879cebf755b46
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2007-07-25 15:26:07 +0000

    Added correction class Rcd for LogE2E1 method

---
+++ CondFormats/DataRecord/interface/EcalShowerContainmentCorrectionsLogE2E1Rcd.h
@@ -0,0 +1,26 @@
+#ifndef CondFormats_EcalShowerContainmentCorrectionsLogE2E1Rcd_h
+#define CondFormats_EcalShowerContainmentCorrectionsLogE2E1Rcd_h
+// -*- C++ -*-
+//
+// Package:     CondFormats
+// Class  :     EcalShowerContainmentCorrectionsLogE2E1Rcd
+// 
+/**\class EcalShowerContainmentCorrectionsLogE2E1Rcd EcalShowerContainmentCorrectionsLogE2E1Rcd.h src/CondFormats/interface/EcalShowerContainmentCorrectionsLogE2E1Rcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Fri Mar  2 16:50:49 CET 2007
+// $Id: EcalShowerContainmentCorrectionsRcd.h,v 1.1.2.1 2007/05/15 20:55:18 argiro Exp $
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class EcalShowerContainmentCorrectionsLogE2E1Rcd : public edm::eventsetup::EventSetupRecordImplementation<EcalShowerContainmentCorrectionsLogE2E1Rcd> {};
+
+#endif
