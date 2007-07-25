a49a28aa21819804190abd76a76cc5ec8ea33cee
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-07-25 14:52:32 +0000

    Fixed binning

--- CondFormats/BTauObjects/interface/CalibratedHistogram.h
+++ CondFormats/BTauObjects/interface/CalibratedHistogram.h
@@ -6,21 +6,45 @@
 class CalibratedHistogram //:public CalibratedObject
 {
 public:
+  static std::vector<float>  constantBinning(int nBins, float min, float max)
+   {
+    std::vector<float> binULimits;
+    float step=(max-min)/nBins;
+    for(int i=0;i<nBins;i++)
+    {
+       binULimits.push_back(min+step*i);
+    }
+    binULimits.push_back(max); // last bin can be larger due to step approx
+
+  return binULimits;
+  }
   CalibratedHistogram() {} 
   virtual ~CalibratedHistogram() {} 
   CalibratedHistogram(std::vector<float> binLimits) : m_binULimits(binLimits),m_normalization(0) 
   { }
+  CalibratedHistogram(int nBins, float min, float max) : m_binULimits(constantBinning(nBins,min,max)),m_normalization(0)
+  {}
 
   std::vector<float> values() const { return m_binValues;}
   std::vector<float> upperLimits() const {return m_binULimits; }
+
+  ///Set bin limits (values can be meaningless if reset() or setValues() are not called )
   void  setUpperLimits(std::vector<float> limits) { m_binULimits=limits; }
+
+  ///Set values at once from a vector
   void  setValues(std::vector<float> values) { m_binValues=values; normalize(); }
+
+  //recompute normalization
   void normalize() {m_normalization=0; for(size_t i=1;i<m_binValues.size(); i++) m_normalization+=m_binValues[i];}   
 
   /*
-    zero is underflow, size is overflow (the number of good bins is size-1))
+    zero is underflow,1 is first bin,nbins is last bin,  nbins+1 is overflow bin 
   */
   float binContent(int bin) const {return m_binValues[bin];}
+
+  ///return the number of bins
+  int numberOfBins()const {return m_binULimits.size()-1; }
+
   float value(float x) const {return binContent(findBin(x));}
   void  reset();
   void  setBinContent(int b,float v);
