19e1809222a3efc6fe8814ace5fa170453870da7
Mika Huhtinen <sha1-e4ace60bb85ca6214788a8c677bc0fec21d76610@cern.ch>
2007-07-26 13:56:52 +0000

    First implementation of HLT path to select isolated high-Pt tracks among Minimum Bias events. Using only reconstructed Pixel Track info.

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -2,10 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
+#include "HLTrigger/special/interface/HLTPixlMBSelectFilter.h"
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 #include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);		   
-DEFINE_ANOTHER_FWK_MODULE(HLTEcalIsolationFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBSelectFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);				   
+DEFINE_ANOTHER_FWK_MODULE(HLTEcalIsolationFilter);				   
+
