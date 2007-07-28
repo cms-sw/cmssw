c3b09152a68eb9e2fa49069ddfbf9da2458bd107
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2007-07-28 09:21:16 +0000

    Silence a bunch of (somewhat verbose) debug printout on cout Remove two uses of LogDebug for Tracer info

--- RecoEgamma/EgammaPhotonAlgos/src/ConversionTrackEcalImpactPoint.cc
+++ RecoEgamma/EgammaPhotonAlgos/src/ConversionTrackEcalImpactPoint.cc
@@ -31,14 +31,12 @@ ConversionTrackEcalImpactPoint::ConversionTrackEcalImpactPoint(const MagneticFie
 theMF_(field)
 { 
 
-    LogDebug("ConversionTrackEcalImpactPoint") << " CTOR  " <<  "\n";  
     forwardPropagator_ = new PropagatorWithMaterial ( dir_ = alongMomentum, 0.000511, theMF_ );
 
 }
 
 ConversionTrackEcalImpactPoint::~ConversionTrackEcalImpactPoint() {
 
-    LogDebug("ConversionTrackEcalImpactPoint") << " DTOR " <<  "\n";  
     delete    forwardPropagator_ ; 
     
 }
@@ -46,28 +44,13 @@ ConversionTrackEcalImpactPoint::~ConversionTrackEcalImpactPoint() {
 std::vector<math::XYZPoint> ConversionTrackEcalImpactPoint::find( const std::vector<reco::TransientTrack>&  tracks  )   {
   std::vector<math::XYZPoint> result;
   
-  std::cout << "  ConversionTrackEcalImpactPoint::find input tracks size  " << tracks.size() <<  "\n";
-
-    
-  
   for (    std::vector<reco::TransientTrack>::const_iterator iTk=tracks.begin(); iTk!=tracks.end(); ++iTk) {
 
     math::XYZPoint ecalImpactPosition(0.,0.,0.);
     const TrajectoryStateOnSurface myTSOS=(*iTk).innermostMeasurementState();
     if ( !( myTSOS.isValid() ) ) continue; 
 
-    std::cout << "  ConversionTrackEcalImpactPoint::find  myTSOS is valid " <<  myTSOS << "\n";
-
-    
-
     stateAtECAL_= forwardPropagator_->propagate( myTSOS, barrel() );
-
-    if (!stateAtECAL_.isValid() ) {
-      std::cout << "  ConversionTrackEcalImpactPoint::find  Barrel stateAtECAL_ is not valid " << std::endl;
-    } else {
-      std::cout << "  ConversionTrackEcalImpactPoint::find  Barrel stateAtECAL_ eta  " <<  stateAtECAL_.globalPosition().eta()  << " phi " << stateAtECAL_.globalPosition().phi()      << " " << stateAtECAL_.globalPosition() <<  "\n";
-      
-    }    
     
 
     if (!stateAtECAL_.isValid() || ( stateAtECAL_.isValid() && fabs(stateAtECAL_.globalPosition().eta() ) >1.479 )  ) {
@@ -75,38 +58,17 @@ std::vector<math::XYZPoint> ConversionTrackEcalImpactPoint::find( const std::vec
          
       if ( (*iTk).innermostMeasurementState().globalPosition().eta() > 0.) {
 	stateAtECAL_= forwardPropagator_->propagate( myTSOS, positiveEtaEndcap());
-        if (!stateAtECAL_.isValid()  ) {
-	  std::cout << "  ConversionTrackEcalImpactPoint::find  stateAtECAL_ not valid in positive endcap  " <<  "\n";
-	} else {
-	  std::cout << "  ConversionTrackEcalImpactPoint::find  stateAtECAL_ eta  ppsitive endcap  " <<  stateAtECAL_.globalPosition().eta()  << "\n";
-	}	
 
       } else {
 
 	stateAtECAL_= forwardPropagator_->propagate( myTSOS, negativeEtaEndcap());
-        if (!stateAtECAL_.isValid()  ) {
-	  std::cout << "  ConversionTrackEcalImpactPoint::find  stateAtECAL_ not valid in negative endcap  " <<  "\n";
-	} else {
-	  std::cout << "  ConversionTrackEcalImpactPoint::find  stateAtECAL_ eta  negative endcap  " <<  stateAtECAL_.globalPosition().eta()  << "\n";
-	}
 
       }
     }
     
-    
-    if ( stateAtECAL_.isValid() ) {
-      ecalImpactPosition = stateAtECAL_.globalPosition();
-      std::cout << "  ConversionTrackEcalImpactPoint::find  output stateAtECAL_  is valid  eta   " << ecalImpactPosition.eta()  << " phi " << ecalImpactPosition.phi() << " " << ecalImpactPosition << "\n";
-    }
-    
     result.push_back(ecalImpactPosition  );
-    
-
-
 
   }
-
-  std::cout << " ConversionTrackEcalImpactPoint::find result size " << result.size() << std::endl;
 
   return result;
 
