49925159f097fa69247e5dda6396d1b94a9abb9d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-07-28 12:09:17 +0000

    Cleanup.

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -1,4 +1,3 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
@@ -6,9 +5,7 @@
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 #include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_ANOTHER_FWK_MODULE(HLTPixlMBSelectFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTPixelIsolTrackFilter);				   
-DEFINE_ANOTHER_FWK_MODULE(HLTEcalIsolationFilter);				   
-
+DEFINE_FWK_MODULE(HLTPixlMBFilt);
+DEFINE_FWK_MODULE(HLTPixlMBSelectFilter);
+DEFINE_FWK_MODULE(HLTPixelIsolTrackFilter);
+DEFINE_FWK_MODULE(HLTEcalIsolationFilter);
