e4260d1cc92f55fdfa6ee66490eba38e518c966d
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2007-07-28 07:36:06 +0000

    adapted to modified reco::Tau member function names

--- RecoTauTag/RecoTau/src/CaloRecoTauAlgorithm.cc
+++ RecoTauTag/RecoTau/src/CaloRecoTauAlgorithm.cc
@@ -1,10 +1,9 @@
 #include "RecoTauTag/RecoTau/interface/CaloRecoTauAlgorithm.h"
-#include "DataFormats/Math/interface/Point3D.h"
-#include "DataFormats/Math/interface/LorentzVector.h"
-#include <Math/GenVector/VectorUtil.h>
+//#include "DataFormats/Math/interface/Point3D.h"
+//#include "DataFormats/Math/interface/LorentzVector.h"
+//#include <Math/GenVector/VectorUtil.h>
 
-Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo)
-{
+Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo){
   //Takes the jet
   const Jet & jet = * (myTagInfo.jet());
   //Takes the LeadChargedHadron
@@ -19,8 +18,11 @@ Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo)
   math::XYZPoint  vtx = math::XYZPoint( 0, 0, z_PV );
   math::XYZTLorentzVector myVec(myTagInfo.alternatrecJet_HepLV().getX(),myTagInfo.alternatrecJet_HepLV().getY(),myTagInfo.alternatrecJet_HepLV().getZ(),myTagInfo.alternatrecJet_HepLV().getT());
 
-  //create the Tau  with the modified 4 Vector
+  //create the Tau with the modified Lorentz-vector
   Tau myTau(jet.charge(),myVec,vtx);
+
+  CaloJetRef theCaloJet=myTagInfo.isolatedtautaginfoRef()->jet().castTo<CaloJetRef>();
+  myTau.setcalojetRef(theCaloJet);
 
   //Setting the mass
   myTau.setInvariantMass(myTagInfo.alternatrecJet_HepLV().m());
@@ -29,75 +31,64 @@ Tau CaloRecoTauAlgorithm::tag(const CombinedTauTagInfo& myTagInfo)
   TrackRefVector mySelectedTracks = myTagInfo.selectedTks();
   myTau.setSelectedTracks(mySelectedTracks);  
   
-  //Setting the LeadingTrack
-  myTau.setLeadingTrack(myLeadTk);
-  if(myLeadTk.isNonnull())
-    {
-      //Setting the SignalTracks
-      TrackRefVector signalTracks = myTagInfo.signalTks();
-      myTau.setSignalTracks(signalTracks);
-
-      myTau.setCharge(myTagInfo.signalTks_qsum());
-
-      //Setting the IsolationTracks
-      TrackRefVector isolationTracks = myTagInfo.isolTks();
-	/*
-	(myTagInfo.isolatedtautaginfoRef())->tracksInCone(myLeadTk->momentum(), TrackerIsolConeSize_,Tracks_minPt_ );
+  //Setting the LeadTrack
+  myTau.setleadTrack(myLeadTk);
+  if(myLeadTk.isNonnull()){
+    //Setting the SignalTracks
+    TrackRefVector signalTracks = myTagInfo.signalTks();
+    myTau.setSignalTracks(signalTracks);
+    
+    myTau.setCharge(myTagInfo.signalTks_qsum());
+    
+    //Setting the IsolationTracks
+    TrackRefVector isolationTracks = myTagInfo.isolTks();
+    /*
+      (myTagInfo.isolatedtautaginfoRef())->tracksInCone(myLeadTk->momentum(), TrackerIsolConeSize_,Tracks_minPt_ );
       TrackRefVector isolationBandTracks;
       for(int i=0;i<isolationTracks.size();i++){
-	const math::XYZVector trackMomentum = isolationTracks[i]->momentum();
-	const math::XYZVector myVector = myLeadTk->momentum();
-	float deltaR = ROOT::Math::VectorUtil::DeltaR(myVector, trackMomentum);
-	if(deltaR > TrackerSignalConeSize_)
-	  isolationBandTracks.push_back(isolationTracks[i]);
+      const math::XYZVector trackMomentum = isolationTracks[i]->momentum();
+      const math::XYZVector myVector = myLeadTk->momentum();
+      float deltaR = ROOT::Math::VectorUtil::DeltaR(myVector, trackMomentum);
+      if(deltaR > TrackerSignalConeSize_)
+      isolationBandTracks.push_back(isolationTracks[i]);
       }
-	*/
-      myTau.setIsolationTracks(isolationTracks);
-      
-      //Setting sum of the pT of isolation Annulus charged hadrons
-      float ptSum =0;
-      for(int i=0;i<isolationTracks.size();i++){
-	ptSum = ptSum + isolationTracks[i]->pt();
-      }
-      myTau.setSumPtIsolation(ptSum);
-
-      
-      //Setting sum of the E_T of isolation Annulus gamma candidates
-      myTau.setEMIsolation(myTagInfo.isolneutralEtsum());
-      
-      //setting the mass with only Signal objects:
-      myTau.setInvariantMass(myTagInfo.alternatrecJet_HepLV().m());
-      
-      //Setting the max HCalEnergy
-
-      const CaloJet* cj = dynamic_cast<CaloJet*>(const_cast<Jet*>(&jet));
-       // access jet constituents
-     const std::vector<CaloTowerRef>  TauJetTowers = cj->getConstituents();
-     // access towers
-     double THmax = -10.; 
-     for(int iTower = 0; iTower < TauJetTowers.size(); iTower++) {
-       CaloTowerRef theTower = TauJetTowers[iTower];
-       // select max Et HCAL tower
-       if(theTower->hadEt() >= THmax) {
-	 THmax    = theTower->hadEt();
-       }
-     }
-
-     myTau.setMaximumHcalEnergy(THmax);
- //Setting the EmEnergyFraction
-
-     myTau.setEmEnergyFraction(myTagInfo.neutralE_o_TksEneutralE());
-          
-
-     
-      //Setting the number of EcalClusters
-      myTau.setNumberOfEcalClusters(myTagInfo.neutralECALClus_number());
-      
-      //LeadTk TIP
-      myTau.setSignificanceLeadTkTIP(myTagInfo.leadTk_signedip3D_significance());
-
+    */
+    myTau.setIsolationTracks(isolationTracks);
+    
+    //Setting sum of the pT of isolation Annulus charged hadrons
+    float ptSum =0;
+    for(unsigned int i=0;i<isolationTracks.size();i++) ptSum+=isolationTracks[i]->pt();
+    myTau.setSumPtIsolation(ptSum);
+    
+    //Setting sum of the E_T of isolation Annulus gamma candidates
+    myTau.setEMIsolation(myTagInfo.isolneutralEtsum());
+    
+    //setting the mass with only Signal objects:
+    myTau.setInvariantMass(myTagInfo.alternatrecJet_HepLV().m());
+    
+    //Setting the max HCalEnergy
+    
+    const CaloJet* cj = dynamic_cast<CaloJet*>(const_cast<Jet*>(&jet));
+    // access jet constituents
+    const std::vector<CaloTowerRef>  TauJetTowers = cj->getConstituents();
+    // access towers
+    double THmax = -10.; 
+    for(unsigned int iTower = 0; iTower < TauJetTowers.size(); iTower++) {
+      CaloTowerRef theTower = TauJetTowers[iTower];
+      // select max Et HCAL tower
+      if(theTower->hadEt()>=THmax) THmax=theTower->hadEt();
     }
-
-  return myTau;
-  
+    
+    myTau.setMaximumHcalEnergy(THmax);
+    //Setting the EmEnergyFraction
+    
+    myTau.setEmEnergyFraction(myTagInfo.neutralE_o_TksEneutralE());
+    
+    //Setting the number of EcalClusters
+    myTau.setNumberOfEcalClusters(myTagInfo.neutralECALClus_number());
+    
+    //LeadTk TIP
+    myTau.setleadTracksignedSipt(myTagInfo.leadTk_signedipt_significance());
+  }
+  return myTau;  
 }
--- RecoTauTag/RecoTau/src/PFRecoTauAlgorithm.cc
+++ RecoTauTag/RecoTau/src/PFRecoTauAlgorithm.cc
@@ -1,142 +1,115 @@
 #include "RecoTauTag/RecoTau/interface/PFRecoTauAlgorithm.h"
-#include "DataFormats/Math/interface/Point3D.h"
-#include "DataFormats/Math/interface/LorentzVector.h"
 
-Tau PFRecoTauAlgorithm::tag(const PFIsolatedTauTagInfo& myTagInfo)
-{
+Tau PFRecoTauAlgorithm::tag(const PFIsolatedTauTagInfo& myTagInfo){
   //Takes the jet
   //  const Jet & jet = * (myTagInfo.jet());
   PFJetRef myJet = myTagInfo.pfjetRef();
-    //Takes the LeadChargedHadron
-    float z_PV = 0;
-    TrackRef myLeadTk;
-    PFCandidateRef leadPFChargedHadron = (myTagInfo).leadPFChargedHadrCand(MatchingConeSize_, LeadCand_minPt_);
-    if(leadPFChargedHadron.isNonnull()){
-      if (leadPFChargedHadron->blockRef()->elements().size()!=0){
-	for (OwnVector<PFBlockElement>::const_iterator iPFBlock=leadPFChargedHadron->blockRef()->elements().begin();
-	     iPFBlock!=leadPFChargedHadron->blockRef()->elements().end();iPFBlock++){
-	  if ((*iPFBlock).type()==1 &&
+  //Takes the LeadPFChargedHadrCand
+  float z_PV = 0;
+  TrackRef myLeadTk;
+  PFCandidateRef leadPFChargedHadron = (myTagInfo).leadPFChargedHadrCand(MatchingConeSize_, LeadCand_minPt_);
+  if(leadPFChargedHadron.isNonnull()){
+    if (leadPFChargedHadron->blockRef()->elements().size()!=0){
+      for (OwnVector<PFBlockElement>::const_iterator iPFBlock=leadPFChargedHadron->blockRef()->elements().begin();
+	   iPFBlock!=leadPFChargedHadron->blockRef()->elements().end();iPFBlock++){
+	if ((*iPFBlock).type()==1 &&
 	    ROOT::Math::VectorUtil::DeltaR(leadPFChargedHadron->momentum(),(*iPFBlock).trackRef()->momentum())<0.001){
-	    myLeadTk =(*iPFBlock).trackRef();
-	    if(myLeadTk.isNonnull()){
-	      z_PV = myLeadTk->dz();
-	    }
+	  myLeadTk =(*iPFBlock).trackRef();
+	  if(myLeadTk.isNonnull()){
+	    z_PV = myLeadTk->dz();
 	  }
 	}
       }
     }
-
+  }
+  
   math::XYZPoint  vtx = math::XYZPoint( 0, 0, z_PV );
   //create the Tau
-    
-    Tau myTau(myJet->charge(),myJet->p4(),vtx);
-	
-    //Setting the EmOverCharged energy
-    myTau.setEmEnergyFraction((myJet->chargedEmEnergy() + myJet->neutralEmEnergy())/ (myJet->chargedHadronEnergy()  +myJet->neutralHadronEnergy()+myJet->chargedEmEnergy() + myJet->neutralEmEnergy() ));
-    
-    //Setting the ChargedHadrons
-    PFCandidateRefVector myChargedHadrons = myTagInfo.PFChargedHadrCands();
-    myTau.setSelectedChargedHadrons(myChargedHadrons);  
-    
-    //Setting the NeutralHadrons
-    PFCandidateRefVector myNeutralHadrons = myTagInfo.PFNeutrHadrCands();
-    myTau.setSelectedNeutralHadrons(myNeutralHadrons);  
-    
-    //Setting the GammaCandidates
-    PFCandidateRefVector myGammaCandidates = myTagInfo.PFGammaCands();
-    myTau.setSelectedGammaCandidates(myGammaCandidates);  
-    
-    //Setting the LeadChargedHadrons
-    if(!leadPFChargedHadron){
-    }else{
-      math::XYZVector leadPFCand_XYZVector=(*leadPFChargedHadron).momentum() ;
-      myTau.setLeadingChargedHadron(leadPFChargedHadron);
-      
-      //Setting the HCalEnergy from the LeadHadron
-      myTau.setMaximumHcalEnergy(leadPFChargedHadron->energy());
-      
-
-      //Setting the SignalChargedHadrons
-      PFCandidateRefVector mySignalChargedHadrons = 
-	myTagInfo.PFChargedHadrCandsInCone(leadPFCand_XYZVector, TrackerSignalConeSize_,Candidates_minPt_);
-      myTau.setSignalChargedHadrons(mySignalChargedHadrons); 
-      
-      
-      //Setting charge
-      int myCharge = 0.;
-      for(int i=0; i<mySignalChargedHadrons.size();i++)
-	{
-	  myCharge = myCharge + mySignalChargedHadrons[i]->charge();
-	}
-      myTau.setCharge(myCharge);
-    
-      //Setting the IsolationBandChargedHadrons
-      PFCandidateRefVector myIsolationChargedHadrons = 
-	myTagInfo.PFChargedHadrCandsInBand(leadPFCand_XYZVector, TrackerSignalConeSize_,TrackerIsolConeSize_,Candidates_minPt_);
-      myTau.setIsolationChargedHadrons(myIsolationChargedHadrons);  
-      
-
-      //Setting the SignalNeutralHadrons
-      PFCandidateRefVector mySignalNeutralHadrons = 
-	myTagInfo.PFNeutrHadrCandsInCone(leadPFCand_XYZVector, ECALSignalConeSize_ ,Candidates_minPt_);
-      myTau.setSignalNeutralHadrons(mySignalNeutralHadrons);  
-      
-      //Setting the IsolationNeutralHadrons
-      PFCandidateRefVector myIsolationNeutralHadrons = 
-	myTagInfo.PFNeutrHadrCandsInBand(leadPFCand_XYZVector, ECALSignalConeSize_, ECALIsolConeSize_,Candidates_minPt_);
-      myTau.setIsolationNeutralHadrons(myIsolationNeutralHadrons);  
-      
-      //Setting the SignalGammaCandidates
-      PFCandidateRefVector mySignalGammaCandidates = 
-	myTagInfo.PFGammaCandsInCone(leadPFCand_XYZVector, ECALSignalConeSize_ ,Candidates_minPt_);
-      myTau.setSignalGammaCandidates(mySignalGammaCandidates);  
-      
-      //Setting the IsolationGammaCandidates
-      PFCandidateRefVector myIsolationGammaCandidates = 
-	myTagInfo.PFGammaCandsInBand(leadPFCand_XYZVector, ECALSignalConeSize_ , ECALIsolConeSize_ ,Candidates_minPt_);
-      myTau.setIsolationGammaCandidates(myIsolationGammaCandidates);  
-
-      //setting the mass with only Signal objects:
-      math::XYZTLorentzVector signalCandidate;
-      for(int i=0;i<mySignalGammaCandidates.size();i++)
-	{
-	  signalCandidate = signalCandidate + mySignalGammaCandidates[i]->p4();
-	}
-      for(int i=0;i<mySignalNeutralHadrons.size();i++)
-	{
-	  signalCandidate = signalCandidate + mySignalNeutralHadrons[i]->p4();
-	}
-      for(int i=0;i<mySignalChargedHadrons.size();i++)
-	{
-	  signalCandidate = signalCandidate + mySignalChargedHadrons[i]->p4();
-	}
-      myTau.setInvariantMass(signalCandidate.mass());
-      
-      //Setting sum of the pT of isolation Annulus charged hadrons
-      float mySumPt=0.;
-      for(int i=0; i<myIsolationChargedHadrons.size();i++)
-	{
-	  mySumPt= mySumPt + myIsolationChargedHadrons[i]->pt();
-	}
-      myTau.setSumPtIsolation(mySumPt);  
-      
-      //Setting sum of the E_T of isolation Annulus gamma candidates
-      float mySumEt=0.;
-      for(int i=0; i<myIsolationGammaCandidates.size();i++)
-	{
-	  mySumEt= mySumEt + myIsolationGammaCandidates[i]->pt();
-	}
-      myTau.setEMIsolation(mySumEt);
-      
-    
-
-
-/*
-setLeadTkTIP(const Measurement1D& myIP)  { transverseIp_leadTk_ = myIP;}
-setLeadTk3DIP(const Measurement1D& myIP)  {  ip3D_leadTk_=myIP;}
-*/
- 
-  }    
-  return myTau;
   
+  Tau myTau(myJet->charge(),myJet->p4(),vtx);
+  
+  myTau.setpfjetRef(myJet);
+  
+  //Setting the EmOverCharged energy
+  myTau.setEmEnergyFraction((myJet->chargedEmEnergy() + myJet->neutralEmEnergy())/ (myJet->chargedHadronEnergy()  +myJet->neutralHadronEnergy()+myJet->chargedEmEnergy() + myJet->neutralEmEnergy() ));
+  
+  //Setting the PFCands
+  PFCandidateRefVector myPFCands = myTagInfo.PFCands();
+  myTau.setSelectedPFCands(myPFCands);  
+  
+  //Setting the PFChargedHadrCands
+  PFCandidateRefVector myPFChargedHadrCands = myTagInfo.PFChargedHadrCands();
+  myTau.setSelectedPFChargedHadrCands(myPFChargedHadrCands);  
+  
+  //Setting the PFNeutrHadrCands
+  PFCandidateRefVector myPFNeutrHadrCands = myTagInfo.PFNeutrHadrCands();
+  myTau.setSelectedPFNeutrHadrCands(myPFNeutrHadrCands);  
+  
+  //Setting the PFGammaCands
+  PFCandidateRefVector myPFGammaCands = myTagInfo.PFGammaCands();
+  myTau.setSelectedPFGammaCands(myPFGammaCands);  
+  
+  //Setting the LeadPFChargedHadrCands
+  if(!leadPFChargedHadron){
+  }else{
+    math::XYZVector leadPFCand_XYZVector=(*leadPFChargedHadron).momentum() ;
+    myTau.setleadPFChargedHadrCand(leadPFChargedHadron);
+    //Setting the HCalEnergy from the LeadHadron
+    myTau.setMaximumHcalEnergy(leadPFChargedHadron->energy());
+        
+    //Setting the SignalPFChargedHadrCands
+    PFCandidateRefVector mySignalPFChargedHadrCands = 
+      myTagInfo.PFChargedHadrCandsInCone(leadPFCand_XYZVector, TrackerSignalConeSize_,Candidates_minPt_);
+    myTau.setSignalPFChargedHadrCands(mySignalPFChargedHadrCands); 
+    
+    //Setting charge
+    if((int)(mySignalPFChargedHadrCands.size())!=0){
+      int myCharge=0;       
+      for(int i=0;i<(int)mySignalPFChargedHadrCands.size();i++) myCharge+=mySignalPFChargedHadrCands[i]->charge();
+      myTau.setCharge(myCharge);
+    }
+    
+    //Setting the IsolationBandPFChargedHadrCands
+    PFCandidateRefVector myIsolationPFChargedHadrCands = 
+      myTagInfo.PFChargedHadrCandsInBand(leadPFCand_XYZVector, TrackerSignalConeSize_,TrackerIsolConeSize_,Candidates_minPt_);
+    myTau.setIsolationPFChargedHadrCands(myIsolationPFChargedHadrCands);  
+        
+    //Setting the SignalPFNeutrHadrCands
+    PFCandidateRefVector mySignalPFNeutrHadrCands = 
+      myTagInfo.PFNeutrHadrCandsInCone(leadPFCand_XYZVector, ECALSignalConeSize_ ,Candidates_minPt_);
+    myTau.setSignalPFNeutrHadrCands(mySignalPFNeutrHadrCands);  
+    
+    //Setting the IsolationPFNeutrHadrCands
+    PFCandidateRefVector myIsolationPFNeutrHadrCands = 
+      myTagInfo.PFNeutrHadrCandsInBand(leadPFCand_XYZVector, ECALSignalConeSize_, ECALIsolConeSize_,Candidates_minPt_);
+    myTau.setIsolationPFNeutrHadrCands(myIsolationPFNeutrHadrCands);  
+    
+    //Setting the SignalPFGammaCands
+    PFCandidateRefVector mySignalPFGammaCands = 
+      myTagInfo.PFGammaCandsInCone(leadPFCand_XYZVector, ECALSignalConeSize_ ,Candidates_minPt_);
+    myTau.setSignalPFGammaCands(mySignalPFGammaCands);  
+    
+    //Setting the IsolationPFGammaCands
+    PFCandidateRefVector myIsolationPFGammaCands = 
+      myTagInfo.PFGammaCandsInBand(leadPFCand_XYZVector, ECALSignalConeSize_ , ECALIsolConeSize_ ,Candidates_minPt_);
+    myTau.setIsolationPFGammaCands(myIsolationPFGammaCands);  
+    
+    //setting the mass with only Signal objects:
+    math::XYZTLorentzVector signalCandidate;
+    for(int i=0;i<(int)mySignalPFGammaCands.size();i++) signalCandidate+=mySignalPFGammaCands[i]->p4();
+    for(int i=0;i<(int)mySignalPFNeutrHadrCands.size();i++) signalCandidate+=mySignalPFNeutrHadrCands[i]->p4();
+    for(int i=0;i<(int)mySignalPFChargedHadrCands.size();i++) signalCandidate+=mySignalPFChargedHadrCands[i]->p4();
+    myTau.setInvariantMass(signalCandidate.mass());
+    
+    //Setting sum of the pT of isolation Annulus charged hadrons
+    float mySumPt=0.;
+    for(int i=0;i<(int)myIsolationPFChargedHadrCands.size();i++) mySumPt+=myIsolationPFChargedHadrCands[i]->pt();
+    myTau.setSumPtIsolation(mySumPt);  
+    
+    //Setting sum of the E_T of isolation Annulus gamma candidates
+    float mySumEt=0.;
+    for(int i=0;i<(int)myIsolationPFGammaCands.size();i++) mySumEt+=myIsolationPFGammaCands[i]->pt();
+    myTau.setEMIsolation(mySumEt);
+  }    
+  return myTau;  
 }
