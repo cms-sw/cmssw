c599970064da835a45308ff3970e54aaa1f78eea
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-07-29 23:56:13 +0000

    boost::python must not be able to handle default arguments to methods. I guess I'll have to decalre both versions of the InputTag constructor explicitly

--- FWCore/ParameterSet/src/PythonModule.h
+++ FWCore/ParameterSet/src/PythonModule.h
@@ -10,6 +10,7 @@ BOOST_PYTHON_MODULE(libFWCoreParameterSet)
 {
   class_<edm::InputTag>("InputTag", init<std::string>())
       .def(init<std::string, std::string, std::string>())
+     .def(init<std::string, std::string>())
       .def("label",    &edm::InputTag::label)
       .def("instance", &edm::InputTag::instance)
       .def("process",  &edm::InputTag::process)
