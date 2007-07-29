471cf4740a97f3a6318e51f755366de87bce5b06
John Marraffino <sha1-3565b4bb138dc66a8251e997feee5430e8526679@cern.ch>
2007-07-29 17:53:29 +0000

    Allow this source to set the bunch crossing and store number data

--- FWCore/Sources/src/RawInputSource.cc
+++ FWCore/Sources/src/RawInputSource.cc
@@ -1,9 +1,10 @@
 /*----------------------------------------------------------------------
-$Id: RawInputSource.cc,v 1.9 2007/07/23 23:42:35 wmtan Exp $
+$Id: RawInputSource.cc,v 1.10 2007/07/26 23:44:27 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include "FWCore/Sources/interface/RawInputSource.h"
 #include "FWCore/Framework/interface/EventPrincipal.h"
+#include "DataFormats/Provenance/interface/EventAuxiliary.h"
 #include "FWCore/Framework/interface/LuminosityBlockPrincipal.h"
 #include "FWCore/Framework/interface/RunPrincipal.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -82,7 +83,7 @@ namespace edm {
     eventId = EventID(runNumber_, eventId.event());
     ep_ = std::auto_ptr<EventPrincipal>(
 	new EventPrincipal(eventId, Timestamp(tstamp),
-	productRegistry(), lbp_, processConfiguration(), true));
+	productRegistry(), lbp_, processConfiguration(), true, EventAuxiliary::Data));
     std::auto_ptr<Event> e(new Event(*ep_, moduleDescription()));
     return e;
   }
