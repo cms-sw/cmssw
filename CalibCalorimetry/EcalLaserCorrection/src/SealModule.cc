9cb97f72b0e3bfad86f5e3268e732fd95afc7747
Toyoko Orimoto <sha1-e781c8403b871d8e1a8ab9a69e6f5e6f12f2f595@cern.ch>
2007-07-30 17:06:33 +0000

    Changed BuildFile and moved test analyzers to test/ directory.  (This was a fix in order to get RecoLocalCalo/EcalRecProducers to compile with EcalLaserCorrection.)
    Toyoko Orimoto
    toyoko.orimoto@cern.ch
    30 July 2007

--- CalibCalorimetry/EcalLaserCorrection/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-
-#include "CalibCalorimetry/EcalLaserCorrection/interface/EcalLaserCorrectionService.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(EcalLaserCorrectionService);
-
-
