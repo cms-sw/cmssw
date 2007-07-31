e1cea2db93f5d36c912214a7ef2713ea0da8c1ec
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2007-07-31 21:59:17 +0000

    added electronID

--- TopQuarkAnalysis/TopObjectProducers/src/TopElectronProducer.cc
+++ TopQuarkAnalysis/TopObjectProducers/src/TopElectronProducer.cc
@@ -2,179 +2,172 @@
 // Author:  Jan Heyninck, Steven Lowette
 // Created: Tue Apr  10 12:01:49 CEST 2007
 //
-// $Id: TopElectronProducer.cc,v 1.10 2007/07/06 00:18:03 lowette Exp $
+// $Id: TopElectronProducer.cc,v 1.11 2007/07/12 19:36:00 lowette Exp $
 //
 
-#include "TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h"
+#include <vector>
+#include <memory>
 
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
-#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
 #include "PhysicsTools/Utilities/interface/DeltaR.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "TopQuarkAnalysis/TopLeptonSelection/interface/TopLeptonLRCalc.h"
 #include "TopQuarkAnalysis/TopObjectResolutions/interface/TopObjectResolutionCalc.h"
 #include "TopQuarkAnalysis/TopLeptonSelection/interface/TopLeptonTrackerIsolationPt.h"
 #include "TopQuarkAnalysis/TopLeptonSelection/interface/TopLeptonCaloIsolationEnergy.h"
+#include "TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h"
 
 
-#include <vector>
-#include <memory>
-
-
-//
-// constructors and destructor
-//
-
-TopElectronProducer::TopElectronProducer(const edm::ParameterSet & iConfig) {
-  // initialize the configurables
-  electronSrc_      = iConfig.getParameter<edm::InputTag>("electronSource");
-  doGenMatch_       = iConfig.getParameter<bool>         ("doGenMatch");
-  removeEleDupes_   = iConfig.getParameter<bool>         ("removeDuplicates");
-  addResolutions_   = iConfig.getParameter<bool>         ("addResolutions");
-  doIsolation_      = iConfig.getParameter<bool>         ("doIsolation");
-  addLRValues_      = iConfig.getParameter<bool>         ("addLRValues");
-  genPartSrc_       = iConfig.getParameter<edm::InputTag>("genParticleSource");
-  electronResoFile_ = iConfig.getParameter<std::string>  ("electronResoFile");
-  electronLRFile_   = iConfig.getParameter<std::string>  ("electronLRFile");
-  
-  // construct resolution calculator
-  if (addResolutions_) theResoCalc_ = new TopObjectResolutionCalc(electronResoFile_);
-
-  // produces vector of electrons
-  produces<std::vector<TopElectron > >();
+TopElectronProducer::TopElectronProducer(const edm::ParameterSet & cfg):
+  src_   ( cfg.getParameter<edm::InputTag>( "src"    ) ),
+  gen_   ( cfg.getParameter<edm::InputTag>( "gen"    ) ),
+  elecID_( cfg.getParameter<edm::InputTag>( "elecID" ) ),
+  useElecID_       ( cfg.getParameter<bool>( "useElectronID"  ) ),
+  useTrkIso_       ( cfg.getParameter<bool>( "useTrkIsolation") ),
+  useCalIso_       ( cfg.getParameter<bool>( "useCalIsolation") ),
+  useResolution_   ( cfg.getParameter<bool>( "useResolutions" ) ),
+  useLikelihood_   ( cfg.getParameter<bool>( "useLikelihood"  ) ),
+  useGenMatching_  ( cfg.getParameter<bool>( "useGenMatching" ) ),
+  useGhostRemoval_ ( cfg.getParameter<bool>( "useGhostRemoval") ),
+  resolutionInput_( cfg.getParameter<std::string>( "resolutionInput" ) ),
+  likelihoodInput_( cfg.getParameter<std::string>( "likelihoodInput" ) )
+{
+  if( useResolution_){
+    resolution_= new TopObjectResolutionCalc( resolutionInput_);
+  }
+  produces<std::vector<TopElectron> >();
 }
 
-
-TopElectronProducer::~TopElectronProducer() {
-  if (addResolutions_) delete theResoCalc_;
+TopElectronProducer::~TopElectronProducer() 
+{
+  if( useResolution_) 
+    delete resolution_;
 }
 
+void 
+TopElectronProducer::produce(edm::Event& evt, const edm::EventSetup& setup) 
+{ 
+  edm::Handle<TopElectronTypeCollection> elecs; 
+  evt.getByLabel(src_, elecs);
 
-//
-// member functions
-//
+  edm::Handle<reco::CandidateCollection> parts;
+  if( useGenMatching_) evt.getByLabel(gen_, parts);
 
-void TopElectronProducer::produce(edm::Event & iEvent, const edm::EventSetup & iSetup) {
- 
-  // Get the collection of electrons from the event
-  edm::Handle<std::vector<TopElectronType> > electronsHandle; 
-  iEvent.getByLabel(electronSrc_, electronsHandle);
-  std::vector<TopElectronType> electrons = *electronsHandle;
+  edm::Handle<reco::ElectronIDAssociationCollection> elecIDs;
+  if( useElecID_) evt.getByLabel( elecID_, elecIDs );
 
-  //remove any duplicate electrons that might be in the event
-  if (removeEleDupes_) electrons = removeEleDupes(electrons);
+  //prepare isolation calculation
+  if( useTrkIso_) trkIsolation_= new TopLeptonTrackerIsolationPt ( setup );
+  if( useCalIso_) calIsolation_= new TopLeptonCaloIsolationEnergy( setup );
 
-  // Get the vector of generated particles from the event if needed
-  edm::Handle<reco::CandidateCollection> particles;
-  if (doGenMatch_) iEvent.getByLabel(genPartSrc_, particles);
+  //prepare LR calculation
+  if( useLikelihood_) 
+    likelihood_= new TopLeptonLRCalc( setup, likelihoodInput_, "" );
 
-  // prepare for isolation calculation
-  if (doIsolation_) theTrackIsoCalc_ = new TopLeptonTrackerIsolationPt(iSetup);
-  if (doIsolation_) theCaloIsoCalc_ = new TopLeptonCaloIsolationEnergy(iSetup);
+  std::vector<TopElectron>* selected = new std::vector<TopElectron>();
+  TopElectronTypeCollection::const_iterator elec = elecs->begin();
+  for(int idx=0; elec!=elecs->end(); ++elec, ++idx){
+    TopElectron sel( *elec );
 
-  // prepare LR calculation if required
-  if (addLRValues_) theLeptonLRCalc_ = new TopLeptonLRCalc(iSetup, electronLRFile_, "");
-
-  // loop over electrons
-  std::vector<TopElectron> * topElectrons = new std::vector<TopElectron>(); 
-  for (size_t e = 0; e < electrons.size(); ++e) {
-    // construct the TopElectron
-    TopElectron anElectron(electrons[e]);
-    // match to generated final state electrons
-    if (doGenMatch_) {
-      // initialize best match as null
-      reco::GenParticleCandidate bestGenElectron(0, reco::Particle::LorentzVector(0,0,0,0), reco::Particle::Point(0,0,0), 0, 0);
-      float bestDR = 0;
-      // find the closest generated electron
-      for(reco::CandidateCollection::const_iterator itGenElectron = particles->begin(); itGenElectron != particles->end(); ++itGenElectron) {
-        reco::GenParticleCandidate aGenElectron = *(dynamic_cast<reco::GenParticleCandidate *>(const_cast<reco::Candidate *>(&*itGenElectron)));
-        if (abs(aGenElectron.pdgId())==11 && aGenElectron.status()==1 && aGenElectron.charge()==electrons[e].charge()) {
-          float currDR = DeltaR<reco::Candidate>()(aGenElectron, electrons[e]);
-          if (bestDR == 0 || currDR < bestDR) {
-            bestGenElectron = aGenElectron;
-            bestDR = currDR;
-          }
-        }
-        anElectron.setGenLepton(bestGenElectron);
-      }
+    if( useElecID_){
+      sel.setLeptonID( electronID(elecs, elecIDs, idx) );
     }
-    // add resolution info if demanded
-    if (addResolutions_) {
-      (*theResoCalc_)(anElectron);
+
+    if( useTrkIso_){
+      sel.setTrackIso( trkIsolation_->calculate( sel, evt ) );
     }
-    // add isolation info
-    if (doIsolation_) {
-      anElectron.setTrackIso(theTrackIsoCalc_->calculate(anElectron,iEvent));
-      anElectron.setCaloIso(theCaloIsoCalc_->calculate(anElectron,iEvent));
+    
+    if( useCalIso_){
+      sel.setCaloIso ( calIsolation_->calculate( sel, evt ) );
     }
-    // add top lepton id LR info if requested
-    if (addLRValues_) {
-      theLeptonLRCalc_->calcLikelihood(anElectron, iEvent);
+
+    if( useResolution_){
+      (*resolution_)( sel );
     }
-    // add the electron to the vector of TopElectrons
-    topElectrons->push_back(TopElectron(anElectron));
+
+    if( useLikelihood_){
+      likelihood_->calcLikelihood( sel, evt );
+    }
+    
+    if ( useGenMatching_) {
+      sel.setGenLepton( matchTruth( *parts, *elec ) );
+    }
+    
+    //add sel to selected
+    selected->push_back( TopElectron( sel ) );
   }
 
-  // sort electrons in pT
-  std::sort(topElectrons->begin(), topElectrons->end(), pTElectronComparator_);
+  //sort electrons in pt
+  std::sort( selected->begin(), selected->end(), ptComparator_);
+  
+  //remove ghosts if requested
+  if( useGhostRemoval_){
+    removeGhosts( selected );
+  }
+ 
+  //add selected to the event output and clean up
+  std::auto_ptr<std::vector<TopElectron> > ptr( selected );
+  evt.put( ptr );
 
-  // put genEvt object in Event
-  std::auto_ptr<std::vector<TopElectron> > pOutElectron(topElectrons);
-  iEvent.put(pOutElectron);
-
-  // destroy the tools
-  if (doIsolation_) delete theTrackIsoCalc_;
-  if (doIsolation_) delete theCaloIsoCalc_;
-  if (addLRValues_) delete theLeptonLRCalc_;
-
+  if( useTrkIso_ ) 
+    delete trkIsolation_;
+  if( useCalIso_ ) 
+    delete calIsolation_;
+  if( useLikelihood_) 
+    delete likelihood_;
 }
 
-//it is possible that there are multiple electron objects in the collection that correspond to the same
-//real physics object - a supercluster with two tracks reconstructed to it, or a track that points to two different SC
-// (i would guess the latter doesn't actually happen).  Mostly they should be removed by the regular selection but...
-//this function removes these duplicate electrons from the selected electrons vector (and returns the new vector, the input
-//is unmodified).  
-std::vector<TopElectronType> TopElectronProducer::removeEleDupes(const std::vector<TopElectronType> &electrons) {
-  
-  //contains indices of duplicate electrons marked for removal
-  //I do it this way because removal during the loop is more confusing
-  std::vector<size_t> indicesToRemove;
-  
-  for (size_t ie=0;ie<electrons.size();ie++) {
-    if (find(indicesToRemove.begin(),indicesToRemove.end(),ie)!=indicesToRemove.end()) continue;//ignore if already marked for removal
-    
-    reco::GsfTrackRef thistrack=electrons[ie].gsfTrack();
-    reco::SuperClusterRef thissc=electrons[ie].superCluster();
+double
+TopElectronProducer::electronID(edm::Handle<TopElectronTypeCollection>& elecs,
+				edm::Handle<reco::ElectronIDAssociationCollection>& elecIDs, int idx)
+{
+  //find elecID for elec with index idx
+  edm::Ref<TopElectronTypeCollection> elecsRef( elecs, idx );
+  reco::ElectronIDAssociationCollection::const_iterator elecID = elecIDs->find( elecsRef );
 
-    for (size_t je=ie+1;je<electrons.size();je++) {
-      if (find(indicesToRemove.begin(),indicesToRemove.end(),je)!=indicesToRemove.end()) continue;//ignore if already marked for removal
+  //return corresponding elecID (only 
+  //cut based available at the moment)
+  const reco::ElectronIDRef& id = elecID->val;
+  return id->cutBasedDecision();
+}
 
-      if ((thistrack==electrons[je].gsfTrack()) ||
-	  (thissc==electrons[je].superCluster()) ) {//we have a match, arbitrate and mark one for removal
-	//keep the one with E/P closer to unity
-	float diff1=fabs(electrons[ie].eSuperClusterOverP()-1);
-	float diff2=fabs(electrons[je].eSuperClusterOverP()-1);
-	
-	if (diff1<diff2) {
-	  indicesToRemove.push_back(je);
-	} else {
-	  indicesToRemove.push_back(ie);
+reco::GenParticleCandidate
+TopElectronProducer::matchTruth(const reco::CandidateCollection& parts, const TopElectronType& elec)
+{
+  float minR = 0.;
+  reco::GenParticleCandidate gene;
+  reco::CandidateCollection::const_iterator part = parts.begin();
+  for( ; part!=parts.end(); ++part ){
+    if( (abs(part->pdgId())==11) && (status( *part )==1) && part->charge()==elec.charge() ){
+      float dr = DeltaR<reco::Candidate>()( *part, elec );
+      if( minR==0 || dr<minR ){
+	minR = dr;
+	gene = dynamic_cast<const reco::GenParticleCandidate&>(*part);
+      }
+    }
+  }
+  return gene;
+}
+
+void
+TopElectronProducer::removeGhosts(std::vector<TopElectron>* elecs) 
+{
+  std::vector<TopElectron>::iterator cmp = elecs->begin();  
+  std::vector<TopElectron>::iterator ref = elecs->begin();  
+  for( ; ref<elecs->end(); ++ref ){
+    for( ; (cmp!=ref) && cmp<elecs->end(); ++cmp ){
+      if ((cmp->gsfTrack()==ref->gsfTrack()) || (cmp->superCluster()==ref->superCluster()) ){
+	//same track or super cluster is used
+	//keep the one with E/p closer to one	
+	if(fabs(ref->eSuperClusterOverP()-1.) < fabs(cmp->eSuperClusterOverP()-1.)){
+	  elecs->erase( cmp );
+	} 
+	else{
+	  elecs->erase( ref );
 	}
       }
     }
   }
-  std::vector<TopElectronType> output;
-  //now remove the ones marked
-  for (size_t ie=0;ie<electrons.size();ie++) {
-    if (find(indicesToRemove.begin(),indicesToRemove.end(),ie)!=indicesToRemove.end()) {
-      continue;
-    } else {
-      output.push_back(electrons[ie]);
-    }
-  }
-  
-  
-  return output;
+  return;
 }
 
