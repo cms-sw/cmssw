9c002bebe5855fe7facdbbb345f0641b89178f79
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2007-07-31 21:57:30 +0000

    added eletronID

--- TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h
+++ TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h
@@ -2,36 +2,24 @@
 // Author:  Jan Heyninck, Steven Lowette
 // Created: Tue Apr  10 12:01:49 CEST 2007
 //
-// $Id: TopElectronProducer.h,v 1.7 2007/07/06 00:18:04 lowette Exp $
+// $Id: TopElectronProducer.h,v 1.8 2007/07/12 19:36:00 lowette Exp $
 //
 
 #ifndef TopObjectProducers_TopElectronProducer_h
 #define TopObjectProducers_TopElectronProducer_h
 
-/**
-  \class    TopElectronProducer TopElectronProducer.h "TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h"
-  \brief    Produces TopElectron's
-
-   TopElectronProducer produces TopElectron's starting from an ElectronType
-   collection, with possible matching to generator level, adding of resolutions
-   and calculation of a lepton likelihood ratio
-
-  \author   Jan Heyninck, Steven Lowette
-  \version  $Id: TopElectronProducer.h,v 1.7 2007/07/06 00:18:04 lowette Exp $
-*/
-
-
-#include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
-
-#include "PhysicsTools/Utilities/interface/PtComparator.h"
-
-#include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
-
 #include <string>
 
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "PhysicsTools/Utilities/interface/PtComparator.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
+
+#include "AnalysisDataFormats/Egamma/interface/ElectronID.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronIDAssociation.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleCandidate.h"
 
 class TopObjectResolutionCalc;
 class TopLeptonTrackerIsolationPt;
@@ -40,36 +28,33 @@ class TopLeptonLRCalc;
 
 
 class TopElectronProducer : public edm::EDProducer {
+  
+ public:
+  
+  explicit TopElectronProducer(const edm::ParameterSet&);
+  ~TopElectronProducer();  
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  
+ private:
+  
+  void removeGhosts(std::vector<TopElectron>*);
+  reco::GenParticleCandidate matchTruth(const reco::CandidateCollection&, const TopElectronType&);
+  double electronID(edm::Handle<TopElectronTypeCollection>&, 
+		    edm::Handle<reco::ElectronIDAssociationCollection>&, int);
+  
+ private:
 
-  public:
+  edm::InputTag src_, gen_, elecID_;
+  bool useElecID_, useTrkIso_, useCalIso_, useResolution_;
+  bool useLikelihood_, useGenMatching_, useGhostRemoval_;
+  std::string resolutionInput_, likelihoodInput_;
 
-    explicit TopElectronProducer(const edm::ParameterSet & iConfig);
-    ~TopElectronProducer();
-
-    virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
-
-  private:
-
-    std::vector<TopElectronType> removeEleDupes(const std::vector<TopElectronType> &electrons);
-    
-    // configurables
-    edm::InputTag  electronSrc_;
-    bool           doGenMatch_;
-    bool           removeEleDupes_;
-    bool           addResolutions_;
-    bool           doIsolation_;
-    bool           addLRValues_;
-    edm::InputTag  genPartSrc_;
-    std::string    electronResoFile_;
-    std::string    electronLRFile_;
-    // tools
-    TopObjectResolutionCalc *        theResoCalc_;
-    TopLeptonTrackerIsolationPt *    theTrackIsoCalc_;
-    TopLeptonCaloIsolationEnergy *   theCaloIsoCalc_;
-    TopLeptonLRCalc *                theLeptonLRCalc_;
-    PtInverseComparator<TopElectron> pTElectronComparator_;
-
+  TopObjectResolutionCalc *resolution_;
+  TopLeptonTrackerIsolationPt  *trkIsolation_;
+  TopLeptonCaloIsolationEnergy *calIsolation_;
+  TopLeptonLRCalc *likelihood_;
+  
+  PtInverseComparator<TopElectron> ptComparator_;
 };
-
 
 #endif
