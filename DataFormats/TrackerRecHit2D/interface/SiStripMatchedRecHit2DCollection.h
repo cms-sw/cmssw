faca4e3794479e1edc74e63ec45fee923378a876
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-07-31 06:22:31 +0000

    forgot include

--- DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2DCollection.h
+++ DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2DCollection.h
@@ -3,6 +3,8 @@
 
 #include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2D.h"
 
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+
 typedef edmNew::DetSetVector<SiStripMatchedRecHit2D> SiStripMatchedRecHit2DCollection;
 
 #endif
