96f4d3f5e1655dd138eb2b402074923a3a33869f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-07-31 09:06:53 +0000

    added statics for block properties

--- EventFilter/GctRawToDigi/src/GctBlockHeader.cc
+++ EventFilter/GctRawToDigi/src/GctBlockHeader.cc
@@ -17,3 +17,49 @@ std::ostream& operator<<(std::ostream& os, const GctBlockHeader& h) {
   os << "ID " << std::hex << h.id() << " : Samples " << h.nSamples() << " : BX " << h.bcId() << " : Event " << h.eventId() << std::dec;
   return os;
 }
+
+
+// /// setup class statics to lookup block length
+
+// GctBlockHeader::blockLength_[0x00] = 0;
+// GctBlockHeader::blockLength_[0x58] = 0;
+// GctBlockHeader::blockLength_[0x59] = 0;
+// GctBlockHeader::blockLength_[0x5f] = 1;   // ConcJet: Bunch Counter Pattern Test
+// GctBlockHeader::blockLength_[0x68] = 4;   // ConcElec: Output to Global Trigger
+// GctBlockHeader::blockLength_[0x69] = 16;  // ConcElec: Sort Input
+// GctBlockHeader::blockLength_[0x6b] = 2;   // ConcElec: GT Serdes Loopback
+// GctBlockHeader::blockLength_[0x6f] = 1;   // ConcElec: Bunch Counter Pattern Test
+// GctBlockHeader::blockLength_[0x80] = 20;  // Leaf-U1, Elec, NegEta, Sort Input
+// GctBlockHeader::blockLength_[0x81] = 15;  // Leaf-U1, Elec, NegEta, Raw Input
+// GctBlockHeader::blockLength_[0x83] = 4;   // Leaf-U1, Elec, NegEta, Sort Output
+// GctBlockHeader::blockLength_[0x88] = 16;  // Leaf-U2, Elec, NegEta, Sort Input
+// GctBlockHeader::blockLength_[0x89] = 12;  // Leaf-U2, Elec, NegEta, Raw Input
+// GctBlockHeader::blockLength_[0x8b] = 4;   // Leaf-U2, Elec, NegEta, Sort Output
+// GctBlockHeader::blockLength_[0xc0] = 20;  // Leaf-U1, Elec, PosEta, Sort Input
+// GctBlockHeader::blockLength_[0xc1] = 15;  // Leaf-U1, Elec, PosEta, Raw Input
+// GctBlockHeader::blockLength_[0xc3] = 4;   // Leaf-U1, Elec, PosEta, Sort Output
+// GctBlockHeader::blockLength_[0xc8] = 16;  // Leaf-U2, Elec, PosEta, Sort Input
+// GctBlockHeader::blockLength_[0xc9] = 12;  // Leaf-U2, Elec, PosEta, Raw Input
+// GctBlockHeader::blockLength_[0xcb] = 4;   // Leaf-U2, Elec, PosEta, Sort Output
+
+// // setup block names
+// GctBlockHeader::blockName_[0x00] = "null block";
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x59] = ;
+// GctBlockHeader::blockName_[0x5f] = ;
+// GctBlockHeader::blockName_[0x68] = ;
+// GctBlockHeader::blockName_[0x69] = ;
+// GctBlockHeader::blockName_[0x6b] = ;
+// GctBlockHeader::blockName_[0x6f] = ;
+// GctBlockHeader::blockName_[0x80] = ;
+// GctBlockHeader::blockName_[0x81] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
+// GctBlockHeader::blockName_[0x58] = ;
--- EventFilter/GctRawToDigi/src/GctBlockHeader.h
+++ EventFilter/GctRawToDigi/src/GctBlockHeader.h
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <ostream>
+#include <string>
 
 // data block header
 // blockId = 7:0
@@ -27,9 +28,16 @@ class GctBlockHeader {
   unsigned int bcId() const { return (d>>12) & 0xfff; }
   unsigned int eventId() const { return (d>>24) & 0xff; }
 
+  //  unsigned int length() const { return blockLength_.find(id()); }
+
  private:
   
   uint32_t d;
+
+  static std::map<unsigned, unsigned> blockLength_;  // fundamental size of a block (ie for 1 readout sample)
+  static std::map<unsigned, std::string> blockName_;  // block name!
+
+
 
 };
 
