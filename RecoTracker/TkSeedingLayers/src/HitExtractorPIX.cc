17698f087c4163c4c142bcff9b8ead39866b74ac
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-07-31 16:10:17 +0000

    new detset

--- RecoTracker/TkSeedingLayers/src/HitExtractorPIX.cc
+++ RecoTracker/TkSeedingLayers/src/HitExtractorPIX.cc
@@ -18,17 +18,24 @@ HitExtractorPIX::HitExtractorPIX(
   : theSide(side), theIdLayer(idLayer), theHitProducer(hitProducer)
 { }
 
+template <typename A, typename B>
+SiPixelRecHitCollection::Range rangeFromPair(SiPixelRecHitCollection const & v, std::pair<A,B> const & p) {
+  return v.equal_range(p.first,p.second);
+}
+
 vector<SeedingHit> HitExtractorPIX::hits(const SeedingLayer & sl,const edm::Event& ev, const edm::EventSetup& es) const
 {
   TrackerLayerIdAccessor accessor;
   std::vector<SeedingHit> result;
   edm::Handle<SiPixelRecHitCollection> pixelHits;
   ev.getByLabel( theHitProducer, pixelHits);
-  const SiPixelRecHitCollection::range range = (theSide==SeedingLayer::Barrel) ?
-        pixelHits->get(accessor.pixelBarrelLayer(theIdLayer))
-      : pixelHits->get(accessor.pixelForwardDisk(theSide,theIdLayer));
-  for(SiPixelRecHitCollection::const_iterator it = range.first; it != range.second; it++){
+
+  const SiPixelRecHitCollection::Range range = (theSide==SeedingLayer::Barrel) ?
+    rangeFromPair(pixelHits,accessor.pixelBarrelLayer(theIdLayer))
+    :  rangeFromPair(pixelHits,accessor.pixelForwardDisk(theSide,theIdLayer));
+  for(SiPixelRecHitCollection::const_iterator id = range.first; id != range.second; id++)
+    for(SiPixelRecHitCollection::DetSet::const_iterator it = id.begin(); it != id.end(); it++){
       result.push_back( SeedingHit(&(*it), sl, es) );
-  }
+    }
   return result;
 }
