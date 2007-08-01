fae9459afe8b9b8a323c678b8ad521ca9e903600
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2007-08-01 17:19:48 +0000

    new addition of EcalTrendChecker

--- CondTools/Ecal/src/SealModule.cc
+++ CondTools/Ecal/src/SealModule.cc
@@ -4,9 +4,11 @@
 #include "CondTools/Ecal/interface/EcalDBCopy.h"
 #include "CondTools/Ecal/interface/EcalTestDevDB.h"
 #include "CondTools/Ecal/interface/EcalPedestalTransfer.h"
+#include "CondTools/Ecal/interface/EcalTrendChecker.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(EcalDBCopy);
 DEFINE_ANOTHER_FWK_MODULE(EcalTestDevDB);
 DEFINE_ANOTHER_FWK_MODULE(EcalPedestalTransfer);
+DEFINE_ANOTHER_FWK_MODULE(EcalTrendChecker);
