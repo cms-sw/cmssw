67cdd00cad3ef3e149f7e59bb088d890c5531621
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2007-08-01 22:36:24 +0000

    Fix #include for header moved from "src" to "plugins"

--- CalibCalorimetry/EcalLaserCorrection/plugins/SealModule.cc
+++ CalibCalorimetry/EcalLaserCorrection/plugins/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 
-#include "CalibCalorimetry/EcalLaserCorrection/interface/EcalLaserCorrectionService.h"
+#include "CalibCalorimetry/EcalLaserCorrection/plugins/EcalLaserCorrectionService.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_FWK_EVENTSETUP_MODULE(EcalLaserCorrectionService);
