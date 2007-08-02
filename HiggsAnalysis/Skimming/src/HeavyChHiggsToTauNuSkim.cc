63e4464de9b22215f322031c06ef02fc68ee028b
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2007-08-02 10:35:45 +0000

    *** empty log message ***

--- HiggsAnalysis/Skimming/src/HeavyChHiggsToTauNuSkim.cc
+++ HiggsAnalysis/Skimming/src/HeavyChHiggsToTauNuSkim.cc
@@ -9,6 +9,7 @@
 
 #include "HiggsAnalysis/Skimming/interface/HeavyChHiggsToTauNuSkim.h"
 
+#include "DataFormats/BTauReco/interface/IsolatedTauTagInfo.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "DataFormats/BTauReco/interface/JetTag.h"
 #include "DataFormats/BTauReco/interface/JetTagFwd.h"
@@ -28,11 +29,13 @@ HeavyChHiggsToTauNuSkim::HeavyChHiggsToTauNuSkim(const edm::ParameterSet& iConfi
 	// Local Debug flag
 	debug           = iConfig.getParameter<bool>("DebugHeavyChHiggsToTauNuSkim");
 
+	hltTauLabel	= iConfig.getParameter<InputTag>("HLTTauCollection");
 	jetLabel        = iConfig.getParameter<InputTag>("JetTagCollection");
 	minNumberOfjets = iConfig.getUntrackedParameter<int>("minNumberOfJets",3);
 	jetEtMin        = iConfig.getUntrackedParameter<double>("jetEtMin",20.);
 	jetEtaMin       = iConfig.getUntrackedParameter<double>("jetEtaMin",-2.4);
 	jetEtaMax       = iConfig.getUntrackedParameter<double>("jetEtaMax",2.4);
+	minDRFromTau    = iConfig.getUntrackedParameter<double>("minDRFromTau",0.5);
 
         nEvents         = 0;
         nSelectedEvents = 0;
@@ -53,6 +56,33 @@ bool HeavyChHiggsToTauNuSkim::filter(edm::Event& iEvent, const edm::EventSetup& 
 
 	nEvents++;
 
+	// HLT tau
+
+	Handle<IsolatedTauTagInfoCollection> tauTagL3Handle;
+	try{
+//                iEvent.getByLabel("isolatedL3SingleTau", tauTagL3Handle);
+                iEvent.getByLabel(hltTauLabel, tauTagL3Handle);
+        }catch(...) {;}
+
+	Jet theTau;
+	double maxEt = 0;
+	if(tauTagL3Handle.isValid()){
+		const IsolatedTauTagInfoCollection & L3Taus = *(tauTagL3Handle.product());
+		//cout << "L3 tau collection size " << L3Taus.size() << endl;
+		IsolatedTauTagInfoCollection::const_iterator i;
+		for(i = L3Taus.begin(); i != L3Taus.end(); i++){
+		  if(i->discriminator() == 0) continue;
+		  Jet taujet = *(i->jet().get());
+		  if(taujet.et() > maxEt) {
+			maxEt = taujet.et();
+			theTau = taujet;
+		  }
+		}
+	}
+	if(maxEt == 0) return false;
+
+	// jets
+
 	Handle<JetTagCollection> jetTagHandle;
 	try{
 	    iEvent.getByLabel(jetLabel,jetTagHandle);
@@ -68,7 +98,11 @@ bool HeavyChHiggsToTauNuSkim::filter(edm::Event& iEvent, const edm::EventSetup& 
 	                Jet jet = *(iJet->jet().get());
 			if(jet.et()  > jetEtMin  && 
 			   jet.eta() > jetEtaMin &&
-	                   jet.eta() < jetEtaMax ) nJets++;		
+	                   jet.eta() < jetEtaMax ) {
+				double DR = deltaR(theTau.eta(),jet.eta(),
+				                   theTau.phi(),jet.phi());
+				if(DR > minDRFromTau) nJets++;		
+			}
 		}
 
 		if(nJets >= minNumberOfjets) {
