685a1f98a67d2bb5d1f56ba856257c9603838c82
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-08-02 12:37:36 +0000

    first version

---
+++ DataFormats/Common/interface/DetSetAlgorithm.h
@@ -0,0 +1,56 @@
+#ifndef Common_DetSetAlgorithm_H
+#define Common_DetSetAlgorithm_H
+
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include <boost/ref.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+//FIXME remove New when ready
+namespace edmNew {
+
+  // adapt the RecHit accessors to DSTV
+  template <typename DSTV, typename A, typename B>
+  DSTV::Range detsetRangeFromPair(DSVT const & v, std::pair<A,B> const & p) {
+    return v.equal_range(p.first,p.second);
+  }
+
+
+  // invoke f for each object in the range of DataSets selected by sel
+  // to ease use, f is passed by reference
+  template <typename DSTV, typename A, typename B, typename F>
+  void foreachDetSetObject(DSTV const & v, std::pair<A,B> const & sel, F & f) {
+    typedef typename DSTV::data_type data_type;
+    typename DSTV::Range range = rangeFromPair(v,sel);
+    for(typename DSTV::const_iterator id=range.first; id!=range.second; id++)
+      std::for_each((*id).begin(), (*id).end(),
+		    boost::function<void(const data_type &)>(boost::ref(f)));
+  }
+
+  namespace dstvdetails {
+
+    struct Pointer {
+      template<typename H> 
+      H const * operator()(H const& h) const { return &h;}
+    };
+
+  }
+
+  // to write an easy iterator on objects in a range of DataSets selected by sel
+  // is left as exercise to the reader 
+  // here we provide this not optimal solution...
+  template <typename DSTV, typename A, typename B>
+  void copyDetSetRange(DSTV const & dstv,    
+		       std::vector<typename DSTV::data_type const *> v, 
+		       std::pair<A,B> const & sel) {
+    typename DSTV::Range range = dstv.equal_range(p.first,p.second);
+    for(typename DSTV::const_iterator id=range.first; id!=range.second; id++){
+      size_t cs = v.size();
+      v.resize(cs+range.second-range.first);
+      std::transform((*id).begin(), (*id).end(),v.begin()+cs,Pointer());
+    } 
+  }
+}
+
+
+#endif //  Common_DetSetAlgorithm_H
