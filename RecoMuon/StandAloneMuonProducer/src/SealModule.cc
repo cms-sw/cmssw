76e4f1409e5129e2735c27486e6356a4e0ecfc55
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2007-08-03 08:03:03 +0000

    *** empty log message ***

--- RecoMuon/StandAloneMuonProducer/src/SealModule.cc
+++ RecoMuon/StandAloneMuonProducer/src/SealModule.cc
@@ -2,8 +2,8 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoZ/ZAnalyzer/src/ZAnalyzer.h"
+#include "RecoMuon/StandAloneMuonProducer/src/StandAloneMuonProducer.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(ZAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(StandAloneMuonProducer);
 
