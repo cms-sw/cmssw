d26e289c85203e34be32eda917ff9bcb5a996600
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-08-03 16:54:32 +0000

    compile more tests

--- CondCore/IOVService/test/testIOVDelete.cc
+++ CondCore/IOVService/test/testIOVDelete.cc
@@ -1,9 +1,10 @@
 #include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/ConnectionHandler.h"
 #include "CondCore/DBCommon/interface/SessionConfiguration.h"
-#include "CondCore/DBCommon/interface/PoolStorageManager.h"
+#include "CondCore/DBCommon/interface/PoolTransaction.h"
 #include "CondCore/DBCommon/interface/Exception.h"
 #include "CondCore/DBCommon/interface/MessageLevel.h"
-#include "CondCore/DBCommon/interface/Ref.h"
+#include "CondCore/DBCommon/interface/TypedRef.h"
 #include "CondCore/IOVService/interface/IOVService.h"
 #include "CondCore/IOVService/interface/IOVEditor.h"
 #include "testPayloadObj.h"
@@ -11,14 +12,18 @@
 //#include "CondCore/DBCommon/interface/Ref.h"
 int main(){
   try{
-    cond::DBSession* session=new cond::DBSession(true);
-    session->sessionConfiguration().setMessageLevel(cond::Error);
+    cond::DBSession* session=new cond::DBSession;
+    session->configuration().setMessageLevel(cond::Error);
+    session->configuration().setAuthenticationMethod(cond::XML);
+    static cond::ConnectionHandler& conHandler=cond::ConnectionHandler::Instance();
+    conHandler.registerConnection("mytest","sqlite_file:test.db","file:mycatalog.xml",0);
     session->open();
-    cond::PoolStorageManager pooldb("sqlite_file:test.db","file:mycatalog.xml",session);
-    pooldb.connect();
+    conHandler.connect(session);
+    cond::Connection* myconnection=conHandler.getConnection("mytest");  
+    cond::PoolTransaction& pooldb=myconnection->poolTransaction(false);
+    pooldb.start();
     cond::IOVService iovmanager(pooldb);
     cond::IOVEditor* editor=iovmanager.newIOVEditor();
-    pooldb.startTransaction(false);
     for(int i=0; i<5; ++i){
       std::cout<<"creating test payload obj"<<i<<std::endl;
       testPayloadObj* myobj=new testPayloadObj;
@@ -26,22 +31,20 @@ int main(){
       for(int j=0; j<10; ++j){
         myobj->data.push_back(i+j);
       }
-      cond::Ref<testPayloadObj> myobjRef(pooldb,myobj);
+      cond::TypedRef<testPayloadObj> myobjRef(pooldb,myobj);
       myobjRef.markWrite("testPayloadObjRcd");
       editor->insert(i+10, myobjRef.token());
     }
     std::string iovtoken=editor->token();
     std::cout<<"iov token "<<iovtoken<<std::endl;
     pooldb.commit();
-    pooldb.startTransaction(false);
+    pooldb.start();
     iovmanager.deleteAll(true);
     pooldb.commit();
     delete editor;
-    pooldb.disconnect();
-    pooldb.connect();
+    pooldb.start();
     //same data, delete by tag this time
     cond::IOVEditor* editorNew=iovmanager.newIOVEditor();
-    pooldb.startTransaction(false);
     for(int i=0; i<9; ++i){
       std::cout<<"creating test payload obj"<<i<<std::endl;
       testPayloadObj* cid=new testPayloadObj;
@@ -49,7 +52,7 @@ int main(){
       for(int j=0; j<15; ++j){
         cid->data.push_back(i+j);
       }
-      cond::Ref<testPayloadObj> cidRef(pooldb,cid);
+      cond::TypedRef<testPayloadObj> cidRef(pooldb,cid);
       cidRef.markWrite("testPayloadObjRcd");
       std::cout<<"token"<<cidRef.token()<<std::endl;
       editorNew->insert(i+10, cidRef.token());
@@ -59,9 +62,7 @@ int main(){
     std::cout<<"iov token "<<iovtoken<<std::endl;
     pooldb.commit();
     delete editorNew;
-    pooldb.disconnect();
-    pooldb.connect();
-    pooldb.startTransaction(false);
+    pooldb.start();
     cond::IOVEditor* editorNewNew=iovmanager.newIOVEditor();
     for(int i=0; i<10; ++i){
       std::cout<<"creating test payload obj"<<i<<std::endl;
@@ -69,19 +70,17 @@ int main(){
       for(int j=0; j<7; ++j){
         abc->data.push_back(i+j);
       }
-      cond::Ref<testPayloadObj> abcRef(pooldb,abc);
+      cond::TypedRef<testPayloadObj> abcRef(pooldb,abc);
       abcRef.markWrite("testPayloadObjRcd");
       editorNewNew->insert(i+10, abcRef.token());
     }
     iovtoken=editorNewNew->token();
     std::cout<<"iov token "<<iovtoken<<std::endl;
     pooldb.commit();
-    pooldb.startTransaction(false);
+    pooldb.start();
     //editorNewNew->deleteEntries(true);
     pooldb.commit();
     delete editorNewNew;
-    pooldb.disconnect();
-    session->close();
     delete session;
   }catch(const cond::Exception& er){
     std::cout<<"error "<<er.what()<<std::endl;
--- CondCore/IOVService/test/testIOVEditor.cc
+++ CondCore/IOVService/test/testIOVEditor.cc
@@ -1,22 +1,28 @@
 #include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/ConnectionHandler.h"
+#include "CondCore/DBCommon/interface/Connection.h"
 #include "CondCore/DBCommon/interface/SessionConfiguration.h"
 #include "CondCore/DBCommon/interface/PoolStorageManager.h"
 #include "CondCore/DBCommon/interface/Exception.h"
 #include "CondCore/DBCommon/interface/MessageLevel.h"
+#include "CondCore/DBCommon/interface/PoolTransaction.h"
 #include "CondCore/IOVService/interface/IOVService.h"
 #include "CondCore/IOVService/interface/IOVEditor.h"
 #include <iostream>
-//#include "CondCore/DBCommon/interface/Ref.h"
 int main(){
   try{
-    cond::DBSession* session=new cond::DBSession(true);
-    session->sessionConfiguration().setMessageLevel(cond::Error);
+    cond::DBSession* session=new cond::DBSession;
+    session->configuration().setMessageLevel(cond::Error);
+    session->configuration().setAuthenticationMethod(cond::XML);
+    static cond::ConnectionHandler& conHandler=cond::ConnectionHandler::Instance();
+    conHandler.registerConnection("mytest","sqlite_file:test.db","file:mycatalog.xml",0);
     session->open();
-    cond::PoolStorageManager pooldb("sqlite_file:test.db","file:mycatalog.xml",session);
-    pooldb.connect();
-    cond::IOVService iovmanager(pooldb);
+    conHandler.connect(session);
+    cond::Connection* myconnection=conHandler.getConnection("mytest");    
+    cond::PoolTransaction& pooldb=myconnection->poolTransaction(false);
+    cond::IOVService iovmanager(pooldb);  
     cond::IOVEditor* editor=iovmanager.newIOVEditor();
-    pooldb.startTransaction(false);
+    pooldb.start();
     editor->insert(20,"pay1tok");
     editor->insert(40, "pay2tok");
     editor->insert(60, "pay3tok");
@@ -28,8 +34,6 @@ int main(){
     //cond::IOVEditor* bomber=iovmanager.newIOVEditor(token);
     //bomber->deleteEntries();
     pooldb.commit();
-    pooldb.disconnect();
-    session->close();
     delete editor;
     delete session;
   }catch(const cond::Exception& er){
