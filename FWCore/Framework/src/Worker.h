35f5d1a6d41e30d1ae5d83b45b310e83b0502ca4
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-08-03 16:25:59 +0000

    Remove dangling verbiage from exception messages

--- FWCore/Framework/src/Worker.h
+++ FWCore/Framework/src/Worker.h
@@ -6,7 +6,7 @@
 Worker: this is a basic scheduling unit - an abstract base class to
 something that is really a producer or filter.
 
-$Id: Worker.h,v 1.23 2007/06/14 17:52:18 wmtan Exp $
+$Id: Worker.h,v 1.24 2007/08/02 23:16:03 wmtan Exp $
 
 A worker will not actually call through to the module unless it is
 in a Ready state.  After a module is actually run, the state will not
@@ -253,7 +253,7 @@ namespace edm {
 	cached_exception_.reset(new cms::Exception("std::bad_alloc"));
 	*cached_exception_
 	  << "An std::bad_alloc exception occurred during a call to the module ";
-	exceptionContext(md_, ep, *cached_exception_) << "module.\n"
+	exceptionContext(md_, ep, *cached_exception_)
 	  << "The job has probably exhausted the virtual memory available to the process.\n";
 	throw *cached_exception_;
     }
@@ -263,7 +263,7 @@ namespace edm {
 	cached_exception_.reset(new cms::Exception("StdException"));
 	*cached_exception_
 	  << "An std::exception occurred during a call to the module ";
-        exceptionContext(md_, ep, *cached_exception_) << "module and cannot be repropagated.\n"
+        exceptionContext(md_, ep, *cached_exception_) << "and cannot be repropagated.\n"
 	  << "Previous information:\n" << e.what();
 	throw *cached_exception_;
     }
