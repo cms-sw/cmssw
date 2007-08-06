32574b4695905039de5198787f3ab2b9b30d126b
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-08-06 21:23:31 +0000

    Get rid of G4V7 flag in view of going to G4.8 or beyond releases in the future

--- SimG4Core/CustomPhysics/src/CustomPhysics.cc
+++ SimG4Core/CustomPhysics/src/CustomPhysics.cc
@@ -1,55 +1,57 @@
 #include "SimG4Core/CustomPhysics/interface/CustomPhysics.h"
 #include "SimG4Core/CustomPhysics/interface/CustomPhysicsList.h"
-#include "SimG4Core/QGSP/src/HadronPhysicsQGSP.hh"
- 
-#include "SimG4Core/DummyPhysics/interface/DummyEMPhysics.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#ifdef G4V7
-#include "SimG4Core/Packaging/src/G4DataQuestionaire.hh"
-#include "SimG4Core/Packaging/src/GeneralPhysics.hh"
-#include "SimG4Core/Packaging/src/EMPhysics.hh"
-#include "SimG4Core/Packaging/src/MuonPhysics.hh"
-#include "SimG4Core/Packaging/src/IonPhysics.hh"
-#else
 #include "G4DecayPhysics.hh"
 #include "G4EmStandardPhysics71.hh"
 #include "G4EmExtraPhysics.hh"
 #include "G4IonPhysics.hh"
 #include "G4QStoppingPhysics.hh"
 #include "G4HadronElasticPhysics.hh" 
+#include "G4NeutronTrackingCut.hh"
+
 #include "G4DataQuestionaire.hh"
-#endif
+#include "HadronPhysicsQGSP.hh"
  
 CustomPhysics::CustomPhysics(G4LogicalVolumeToDDLogicalPartMap& map,
-			     const edm::ParameterSet & p) :  PhysicsList(map, p) {
-    G4DataQuestionaire it(photon);
-#ifdef G4V7
-    std::cout << "You are using the simulation engine: QGSP 2.8" << std::endl;
-   
-    RegisterPhysics(new GeneralPhysics("general"));
-    RegisterPhysics(new EMPhysics("EM"));
-    RegisterPhysics(new MuonPhysics("muon"));
-    RegisterPhysics(new HadronPhysicsQGSP("hadron"));
-    RegisterPhysics(new IonPhysics("ion"));
-    RegisterPhysics(new CustomPhysicsList("custom",p));
-#else
-    std::cout << "You are using the simulation engine: QGSP_EMV 3.1" << std::endl;
+			     const edm::ParameterSet & p) : PhysicsList(map,p){
 
-    // EM Physics
-    RegisterPhysics(new G4EmStandardPhysics71("standard EM v71"));
-    // Synchroton Radiation & GN Physics
-    RegisterPhysics(new G4EmExtraPhysics("extra EM"));
-    // Decays
-    RegisterPhysics(new G4DecayPhysics("decay"));
-    // Hadron Elastic scattering
-    RegisterPhysics(new G4HadronElasticPhysics("elastic")); 
-    // Hadron Physics
-    RegisterPhysics(new HadronPhysicsQGSP("hadron"));
-    // Stopping Physics
-    RegisterPhysics(new G4QStoppingPhysics("stopping"));
-    // Ion Physics
-    RegisterPhysics(new G4IonPhysics("ion"));
-    // Custom Physics
-    RegisterPhysics(new CustomPhysicsList("custom",p));    
-#endif
+  G4DataQuestionaire it(photon);
+
+  int  ver     = p.getUntrackedParameter<int>("Verbosity",0);
+  bool emPhys  = p.getUntrackedParameter<bool>("EMPhysics",true);
+  bool hadPhys = p.getUntrackedParameter<bool>("HadPhysics",true);
+  edm::LogInfo("PhysicsList") << "You are using the simulation engine: "
+			      << "QGSP_EMV 3.3 with Flags for EM Physics "
+			      << emPhys << " and for Hadronic Physics "
+			      << hadPhys << "\n";
+
+  // EM Physics
+  RegisterPhysics(new G4EmStandardPhysics71("standard EM v71",ver));
+
+  // Synchroton Radiation & GN Physics
+  RegisterPhysics(new G4EmExtraPhysics("extra EM"));
+
+  // Decays
+  RegisterPhysics(new G4DecayPhysics("decay"));
+
+  // Hadron Elastic scattering
+  RegisterPhysics(new G4HadronElasticPhysics("elastic",ver,false)); 
+
+  // Hadron Physics
+  G4bool quasiElastic=true;
+  RegisterPhysics(new HadronPhysicsQGSP("hadron",quasiElastic));
+  //RegisterPhysics(new HadronPhysicsQGSP("hadron"));
+
+  // Stopping Physics
+  RegisterPhysics(new G4QStoppingPhysics("stopping"));
+
+  // Ion Physics
+  RegisterPhysics(new G4IonPhysics("ion"));
+
+  // Neutron tracking cut
+  RegisterPhysics( new G4NeutronTrackingCut("Neutron tracking cut", ver));
+
+  // Custom Physics
+  RegisterPhysics(new CustomPhysicsList("custom",p));    
 }
