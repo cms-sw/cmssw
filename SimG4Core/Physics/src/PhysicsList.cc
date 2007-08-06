ad34608b6b18eb1925f328264416b046ee981f52
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-08-06 20:52:46 +0000

    Get rid of G4V7 flag in view of going to G4.8 or beyond releases in the future

--- SimG4Core/Physics/src/PhysicsList.cc
+++ SimG4Core/Physics/src/PhysicsList.cc
@@ -1,9 +1,7 @@
 #include "SimG4Core/Physics/interface/PhysicsList.h"
 #include "SimG4Core/Physics/interface/DDG4ProductionCuts.h"
 
-#ifndef G4V7
 #include "G4LossTableManager.hh"
-#endif
 
 PhysicsList::PhysicsList(G4LogicalVolumeToDDLogicalPartMap & map,
 			 const edm::ParameterSet & p) 
@@ -15,12 +13,10 @@ PhysicsList::PhysicsList(G4LogicalVolumeToDDLogicalPartMap & map,
  
 PhysicsList::~PhysicsList() 
 {
-#ifndef G4V7
   if (m_pPhysics.getUntrackedParameter<int>("Verbosity",0) > 1)
     std::cout << " G4BremsstrahlungThreshold was " 
 	      << G4LossTableManager::Instance()->BremsstrahlungTh()/GeV 
 	      << " GeV " << std::endl;
-#endif
   if (prodCuts!=0) delete prodCuts;
 }
 
@@ -30,11 +26,9 @@ void PhysicsList::SetCuts()
   SetDefaultCutValue(m_pPhysics.getParameter<double>("DefaultCutValue")*cm);
   SetCutsWithDefault();
 
-#ifndef G4V7
   G4LossTableManager::Instance()->SetBremsstrahlungTh
     (m_pPhysics.getParameter<double>("G4BremsstrahlungThreshold")*GeV);
-#endif
-    
+
   int v =  m_pPhysics.getUntrackedParameter<int>("Verbosity",0);
   if ( m_pPhysics.getParameter<bool>("CutsPerRegion") )
     {
@@ -44,9 +38,7 @@ void PhysicsList::SetCuts()
     }
 
   if ( v > 1) {
-#ifndef G4V7
     G4LossTableManager::Instance()->SetVerbose(v-1);
-#endif
     G4VUserPhysicsList::DumpCutValuesTable();
   }
 
