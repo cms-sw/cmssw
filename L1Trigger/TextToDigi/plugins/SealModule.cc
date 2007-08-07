8f125d47f60febfb777810e1a0e18f9f0ab090b0
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2007-08-07 00:54:35 +0000

    raw to text converter for GCT

--- L1Trigger/TextToDigi/plugins/SealModule.cc
+++ L1Trigger/TextToDigi/plugins/SealModule.cc
@@ -1,12 +1,14 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "TextToRaw.h"
+#include "RawToText.h"
 #include "RctDigiToSourceCardText.h"
 #include "SourceCardTextToRctDigi.h"
 #include "RctTextToRctDigi.h"
 #include "RctDigiToRctText.h"
 
 DEFINE_FWK_MODULE(TextToRaw);
+DEFINE_FWK_MODULE(RawToText);
 DEFINE_FWK_MODULE(RctDigiToSourceCardText);
 DEFINE_FWK_MODULE(SourceCardTextToRctDigi);
 DEFINE_FWK_MODULE(RctTextToRctDigi);
