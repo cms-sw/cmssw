f4518e6e297b83e05496aff15b869f7f28296268
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2007-08-07 12:27:05 +0000

    from Emanuele: added new record to retrieve pdf histogram used by LL based electron ID algorthm

---
+++ CondFormats/DataRecord/interface/ElectronLikelihoodRcd.h
@@ -0,0 +1,15 @@
+#ifndef ElectronLikelihoodRcd_h
+#define ElectronLikelihoodRcd_h
+/**\class ElectronLikelihoodRcd
+ *
+ * Description: Record for Pid Electron Likelihood
+ *
+ */
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class ElectronLikelihoodRcd :
+public edm::eventsetup::EventSetupRecordImplementation<ElectronLikelihoodRcd> {
+};
+
+#endif
