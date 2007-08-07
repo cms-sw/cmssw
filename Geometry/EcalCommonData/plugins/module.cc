f77340a63c438220c211fac234fed38b4befdf51
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2007-08-07 18:41:06 +0000

    Changes to allow non-default testing of endcap algo geometry

--- Geometry/EcalCommonData/plugins/module.cc
+++ Geometry/EcalCommonData/plugins/module.cc
@@ -2,10 +2,13 @@
 
 //#include "Geometry/EcalPreshowerData/interface/DDTestAlgorithm.h"
 #include "Geometry/EcalCommonData/interface/DDEcalBarrelAlgo.h"
+#include "Geometry/EcalCommonData/interface/DDEcalEndcapAlgo.h"
 #include "Geometry/EcalCommonData/interface/DDEcalPreshowerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 
 //DEFINE_SEAL_PLUGIN (DDAlgorithmFactory, DDTestAlgorithm, "DDTestAlgorithm");
+
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDEcalEndcapAlgo, "ecal:DDEcalEndcapAlgo");
 
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDEcalBarrelAlgo, "ecal:DDEcalBarrelAlgo");
 
