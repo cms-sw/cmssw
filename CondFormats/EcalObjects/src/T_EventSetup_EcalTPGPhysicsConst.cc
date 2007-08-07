ad25668339bce6d5f4e23b50f856f203245745c8
Pascal Paganini <sha1-64e1aff7b98a6a4181bcac5ddf06a281e465575f@cern.ch>
2007-08-07 12:54:01 +0000

    adding EcalTPGPhysicsConst class

---
+++ CondFormats/EcalObjects/src/T_EventSetup_EcalTPGPhysicsConst.cc
@@ -0,0 +1,17 @@
+// -*- C++ -*-
+//
+// Implementation:
+//     create all the 'infrastructure' needed to get into the Context
+//
+// Author:      Chris Jones
+// Created:     Mon Apr 18 16:42:52 EDT 2005
+// $Id: T_EventSetup_EcalTPGPhysicsConst.cc,v 1.2 2007/06/22 13:08:29 paganini Exp $
+//
+
+// system include files
+
+// user include files
+#include "CondFormats/EcalObjects/interface/EcalTPGPhysicsConst.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(EcalTPGPhysicsConst);
