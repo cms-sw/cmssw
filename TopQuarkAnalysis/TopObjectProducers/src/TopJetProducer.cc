27cadbc694b575662905fdfd7321309ab25dff83
Dimitrios Tsirigkas <sha1-7a25099d73dbb3e60e5b1352f9ab2dd1067a4a69@cern.ch>
2007-08-08 09:56:06 +0000

    - fix to segmentation fault due to JetTags returning null references to jets

--- TopQuarkAnalysis/TopObjectProducers/src/TopJetProducer.cc
+++ TopQuarkAnalysis/TopObjectProducers/src/TopJetProducer.cc
@@ -1,5 +1,5 @@
 //
-// $Id: TopJetProducer.cc,v 1.14 2007/07/12 19:37:43 lowette Exp $
+// $Id: TopJetProducer.cc,v 1.15 2007/08/06 14:37:41 tsirig Exp $
 //
 
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopJetProducer.h"
@@ -84,8 +84,8 @@ TopJetProducer::~TopJetProducer() {
 // member functions
 //
 
-void TopJetProducer::produce(edm::Event & iEvent, const edm::EventSetup & iSetup) {
- 
+void TopJetProducer::produce(edm::Event & iEvent, const edm::EventSetup & iSetup) 
+{
   // Get the vector of non-calibrated jets
   edm::Handle<std::vector<TopJetType> > recjets;
   iEvent.getByLabel(recJetsLabel_, recjets);
@@ -116,158 +116,161 @@ void TopJetProducer::produce(edm::Event & iEvent, const edm::EventSetup & iSetup
 
   // for jet flavour
   if (getJetMCFlavour_) jetFlavId_->readEvent(iEvent);
-
- if (doJetCleaning_) {
-  // TEMP Jet cleaning from electrons
-  //select isolated leptons to remove from jets collection
-  electrons=selectIsolated(electrons,ELEISOCUT_,iSetup,iEvent);
-  muons=selectIsolated(muons,MUISOCUT_,iSetup,iEvent);
-  // TEMP End
- }
-
-
-  // loop over jets
-  std::vector<TopJet> * topJets = new std::vector<TopJet>(); 
-  for (size_t j = 0; j < recjets->size(); j++) {
- 
-   if (doJetCleaning_) {
-   // TEMP Jet cleaning from electrons
-    //check that the jet doesn't match in deltaR with an isolated lepton
-    //if it does, then it needs to be cleaned (ie don't put it in the TopJet collection)
-    //FIXME: don't do muons until have a sensible cut value on their isolation
-    float mindr=9999.;
-    for (size_t ie=0; ie<electrons.size(); ie++) {
-      float dr=DeltaR<reco::Candidate>()((*recjets)[j],electrons[ie]);
-      if (dr<mindr) {
-	mindr=dr;
-      }
-    }
-    //if the jet is closely matched in dR to electron, skip it
-    if (mindr<LEPJETDR_) {
-      continue;
-    }
-  // TEMP End
+  
+ if (doJetCleaning_) 
+   {
+     // TEMP Jet cleaning from electrons
+     //select isolated leptons to remove from jets collection
+     electrons=selectIsolated(electrons,ELEISOCUT_,iSetup,iEvent);
+     muons=selectIsolated(muons,MUISOCUT_,iSetup,iEvent);
+     // TEMP End
    }
+ 
+ 
+ // loop over jets
+ std::vector<TopJet> * topJets = new std::vector<TopJet>(); 
+ for (size_t j = 0; j < recjets->size(); j++) 
+   {
+     if (doJetCleaning_) 
+       {
+	 // TEMP Jet cleaning from electrons
+	 //check that the jet doesn't match in deltaR with an isolated lepton
+	 //if it does, then it needs to be cleaned (ie don't put it in the TopJet collection)
+	 //FIXME: don't do muons until have a sensible cut value on their isolation
+	 float mindr=9999.;
+	 for (size_t ie=0; ie<electrons.size(); ie++) 
+	   {
+	     float dr=DeltaR<reco::Candidate>()((*recjets)[j],electrons[ie]);
+	     if (dr<mindr) 
+	       {
+		 mindr=dr;
+	       }
+	   }
+	 //if the jet is closely matched in dR to electron, skip it
+	 if (mindr<LEPJETDR_) 
+	   {
+	     continue;
+	   }
+	 // TEMP End
+       }
 
     // construct the TopJet
     TopJet ajet;
+    
     // loop over cal jets to find corresponding jet
     bool cjFound = false;
-    for (size_t cj = 0; cj < calijets->size(); cj++) {
-      // FIXME: is this 0.01 matching fullproof?
-      if (DeltaR<reco::Candidate>()((*recjets)[j], (*calijets)[cj]) < 0.01) {
-        cjFound = true;
-        ajet = TopJet((*calijets)[cj]);
-        ajet.setRecJet((*recjets)[j]);
-      }
-    }
-    // if cal jet found...
-    if (cjFound) {
-      // get the MC flavour information for this jet
-      if (getJetMCFlavour_) {
-        JetFlavour jetFlavour = jetFlavId_->identifyBasedOnPartons((*recjets)[j]);
-        ajet.setPartonFlavour(jetFlavour.flavour());
-      }
-      // add b-tag info if available & required
-      if (storeBTagInfo_) {
-        for(size_t k=0; k<jetTags_testManyByType.size(); k++){
-	  edm::Handle<std::vector<reco::JetTag> > jetTags = jetTags_testManyByType[k];
-
-	  //**************************
-	  //get label and module names
-	  std::string moduleTagInfoName = (jetTags).provenance()->moduleName();	 
-	  std::string moduleLabel = (jetTags).provenance()->moduleLabel();
-	  //********ignore taggers from AOD*********
-	  if(  (moduleLabel == "trackCountingJetTags"    && ignoreTrackCountingFromAOD_    == true ) ) continue;
-	  if(  (moduleLabel == "trackProbabilityJetTags" && ignoreTrackProbabilityFromAOD_ == true ) ) continue;
-	  if(  (moduleLabel == "softMuonJetTags"         && ignoreSoftMuonFromAOD_         == true ) ) continue;
-	  if(  (moduleLabel == "softElectronJetTags    " && ignoreSoftElectronFromAOD_     == true ) ) continue;
-	
-	  for (size_t t = 0; t < jetTags->size(); t++) {
-	    //cout << "jet test " << ajet.getLCalJet().et() << "   " << (*jetTags)[t].jet().et()  << endl;
-	    //cout << "deltaR   " <<  DeltaR<reco::Candidate>()((*recjets)[j], (*jetTags)[t].jet()) << endl;
-
-	    // FIXME: is this 0.0001 matching fullproof?
-	    if (DeltaR<reco::Candidate>()((*recjets)[j], *(*jetTags)[t].jet()) < 0.00001) {
-	    
-	      //FIXME add combined tagger
-	      //********store discriminators*********
-	      if(keepDiscriminators_ == true){
-	        std::pair<std::string, double> pairDiscri;
-	        pairDiscri.first = moduleLabel;
-	        pairDiscri.second = (*jetTags)[t].discriminator();
-	        //drop TauTag!!!
-	        if(moduleTagInfoName == "TrackProbability" || moduleTagInfoName == "TrackCounting" || moduleTagInfoName == "SoftLepton" ){
-		  ajet.addBDiscriminatorPair(pairDiscri);
-	        }
-	      }
-	    
-	      //FIXME add combined tagger
-	      //********store jetTagRef*********
-	      if(keepJetTagRefs_ == true){
-	      
-	        std::pair<std::string, reco::JetTagRef> pairjettagref;
-	        pairjettagref.first = moduleLabel;
-		pairjettagref.second = reco::JetTagRef(jetTags, t);
-		ajet.addBJetTagRefPair(pairjettagref);
-		/*
-	        if(moduleTagInfoName == "TrackProbability"){
-		  //cout << "string module label " << moduleLabel << endl;
-		  iEvent.getByLabel(moduleLabel,jetsInfoHandleTP);  
-		  const  reco::TrackProbabilityTagInfoCollection & tagInfo_prob = *(jetsInfoHandleTP);
-		  //		  pairjettagref.second = tagInfo_prob[t].getJetTag();
-		  //		  ajet.addBJetTagRefPair(pairjettagref);
-	        }
-	        if(moduleTagInfoName == "TrackCounting"){
-		  //cout << "string module label " << moduleLabel << endl;
-		  iEvent.getByLabel(moduleLabel,jetsInfoHandleTC);  
-		  const  reco::TrackCountingTagInfoCollection & tagInfo_prob = *(jetsInfoHandleTC);
-		  //		  pairjettagref.second = tagInfo_prob[t].getJetTag();
-		  //		  ajet.addBJetTagRefPair(pairjettagref);
-	        }  
-	        if(moduleTagInfoName == "SoftLepton"){
-		  // cout << "string module label " << moduleLabel << endl;
-		  iEvent.getByLabel(moduleLabel,jetsInfoHandle_sl);  
-		  const  reco::SoftLeptonTagInfoCollection & tagInfo_prob = *(jetsInfoHandle_sl);
-		  //		  pairjettagref.second = tagInfo_prob[t].getJetTag();
-		  //		  ajet.addBJetTagRefPair(pairjettagref);
-	        }
-		*/
-	      }  
-	    }
+    for (size_t cj = 0; cj < calijets->size(); cj++) 
+      {
+	// FIXME: is this 0.01 matching fullproof?
+	if (DeltaR<reco::Candidate>()((*recjets)[j], (*calijets)[cj]) < 0.01) 
+	  {
+	    cjFound = true;
+	    ajet = TopJet((*calijets)[cj]);
+	    ajet.setRecJet((*recjets)[j]);
 	  }
+      }
+ 
+    // if cal jet found...
+    if (cjFound) 
+      {
+      // get the MC flavour information for this jet
+      if (getJetMCFlavour_) 
+	{
+	  JetFlavour jetFlavour = jetFlavId_->identifyBasedOnPartons((*recjets)[j]);
+	  ajet.setPartonFlavour(jetFlavour.flavour());
 	}
-      }
-      // add resolution info if demanded
-      if (addResolutions_) {
-        (*theResoCalc_)(ajet);
-      }
-    } else {
-      std::cout << "no cal jet found " << std::endl;
-    }
 
+      // add b-tag info if available & required
+      if (storeBTagInfo_) 
+	{
+	  
+	  for(size_t k=0; k<jetTags_testManyByType.size(); k++)
+	    {
+	      edm::Handle<std::vector<reco::JetTag> > jetTags = jetTags_testManyByType[k];
+	      
+	      //**************************
+	      //get label and module names
+	      std::string moduleTagInfoName = (jetTags).provenance()->moduleName();	 
+	      std::string moduleLabel = (jetTags).provenance()->moduleLabel();
+	      //********ignore taggers from AOD*********
+	      if(  (moduleLabel == "trackCountingJetTags"    && ignoreTrackCountingFromAOD_    == true ) ) continue;
+	      if(  (moduleLabel == "trackProbabilityJetTags" && ignoreTrackProbabilityFromAOD_ == true ) ) continue;
+	      if(  (moduleLabel == "softMuonJetTags"         && ignoreSoftMuonFromAOD_         == true ) ) continue;
+	      if(  (moduleLabel == "softElectronJetTags    " && ignoreSoftElectronFromAOD_     == true ) ) continue;
+
+	      for (size_t t = 0; t < jetTags->size(); t++) 
+		{
+		  edm::RefToBase<reco::Jet> jet_p = (*jetTags)[t].jet();
+		  if (jet_p.isNull()) { continue; }
+		  
+		  // FIXME: is this 0.0001 matching fullproof?
+		  if (DeltaR<reco::Candidate>()( (*recjets)[j], *jet_p ) < 0.00001) 
+		    {
+		      //FIXME add combined tagger
+		      //********store discriminators*********
+		      if(keepDiscriminators_ == true)
+			{
+			  std::pair<std::string, double> pairDiscri;
+			  pairDiscri.first = moduleLabel;
+			  pairDiscri.second = (*jetTags)[t].discriminator();
+			  //drop TauTag!!!
+			  if(moduleTagInfoName == "TrackProbability" || moduleTagInfoName == "TrackCounting" || moduleTagInfoName == "SoftLepton" )
+			    {
+			      ajet.addBDiscriminatorPair(pairDiscri);
+			    }
+			}
+		      //FIXME add combined tagger
+		      //********store jetTagRef*********
+		      if(keepJetTagRefs_ == true)
+			{
+			  
+			  std::pair<std::string, reco::JetTagRef> pairjettagref;
+			  pairjettagref.first = moduleLabel;
+			  pairjettagref.second = reco::JetTagRef(jetTags, t);
+			  ajet.addBJetTagRefPair(pairjettagref);
+			}  
+		    }
+		}
+	    }
+	  
+	}
+      // add resolution info if demanded
+      if (addResolutions_) 
+	{
+	  (*theResoCalc_)(ajet);
+	}
+	
+      }
+    else 
+      {
+	std::cout << "no cal jet found " << std::endl;
+      }
+    
     // Associate tracks with jet (at least temporary)
     simpleJetTrackAssociator_.associate(ajet.momentum(), hTracks, ajet.associatedTracks_);
-
+           
     // PUT HERE EVERYTHING WHICH NEEDS TRACKS
-    if (computeJetCharge_) {
-        ajet.jetCharge_ = static_cast<float>(jetCharge_p->charge(ajet.p4(), ajet.associatedTracks_));
-    }
-
+    if (computeJetCharge_) 
+      {
+	ajet.jetCharge_ = static_cast<float>(jetCharge_p->charge(ajet.p4(), ajet.associatedTracks_));
+      }
+    
     // drop jet track association if the user does not want it
-    if (!storeAssociatedTracks_) { ajet.associatedTracks_.clear(); }
-
+    if (!storeAssociatedTracks_) 
+      { 
+	ajet.associatedTracks_.clear(); 
+      }
+    
     // end of TopObjectProducer loop
     topJets->push_back(ajet);
-  }
+    }
 
   // sort jets in ET
   std::sort(topJets->begin(), topJets->end(), eTComparator_);
-
+  
   // put genEvt  in Event
   std::auto_ptr<std::vector<TopJet> > myTopJetProducer(topJets);
   iEvent.put(myTopJetProducer);
-
 }
 
 
