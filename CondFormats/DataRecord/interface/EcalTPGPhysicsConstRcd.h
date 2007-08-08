c9e1fd8e025ff620d8123239d5dfa7585666b36c
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2007-08-08 08:32:33 +0000

    added for Pascal

---
+++ CondFormats/DataRecord/interface/EcalTPGPhysicsConstRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALTPGPHYSICSCONSTRCD_H
+#define ECALTPGPHYSICSCONSTRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalTPGPhysicsConstRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalTPGPhysicsConstRcd> {};
+#endif
