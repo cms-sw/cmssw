c26123e660c3e5454dabb11afe21d360f2a625ce
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-08-09 22:12:45 +0000

    Move exception messages from here to MessageLogger, so they can be invoked from anywhere

--- FWCore/Framework/bin/cmsRun.cpp
+++ FWCore/Framework/bin/cmsRun.cpp
@@ -4,7 +4,7 @@ This is a generic main that can be used with any plugin and a
 PSet script.   See notes in EventProcessor.cpp for details about
 it.
 
-$Id: cmsRun.cpp,v 1.41 2007/08/07 22:10:53 wmtan Exp $
+$Id: cmsRun.cpp,v 1.42 2007/08/08 21:52:43 wmtan Exp $
 
 ----------------------------------------------------------------------*/  
 
@@ -23,6 +23,7 @@ $Id: cmsRun.cpp,v 1.41 2007/08/07 22:10:53 wmtan Exp $
 #include "FWCore/PluginManager/interface/standard.h"
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/Utilities/interface/Presence.h"
+#include "FWCore/MessageLogger/interface/ExceptionMessages.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/MessageLogger/interface/MessageDrop.h"
 #include "FWCore/PluginManager/interface/PresenceFactory.h"
@@ -54,30 +55,16 @@ namespace {
 	  ep_->endJob();
 	}
 	catch (cms::Exception& e) {
-	  std::string shortDesc("CMSException");
-	  std::ostringstream longDesc;
-	  longDesc << "cms::Exception caught in " 
-		   << kProgramName
-		   << "\n"
-		   << e.explainSelf();
-	  edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+	  edm::printCmsException(e, kProgramName);
+	}
+	catch (std::bad_alloc& e) {
+	  edm::printBadAllocException(kProgramName);
 	}
 	catch (std::exception& e) {
-	  std::string shortDesc("StdLibException");
-	  std::ostringstream longDesc;
-	  longDesc << "Standard library exception caught in " 
-		   << kProgramName
-		   << "\n"
-		   << e.what();
-	  edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+	  edm::printStdException(e, kProgramName);
 	}
 	catch (...) {
-	  std::string shortDesc("UnknownException");
-	  std::ostringstream longDesc;
-	  longDesc << "Unknown exception caught in "
-		   << kProgramName
-		   << "\n";
-	  edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+	  edm::printUnknownException(kProgramName);
         }
       }
     }
@@ -274,49 +261,20 @@ int main(int argc, char* argv[])
     rc = 0;
   }
   catch (cms::Exception& e) {
-    std::string shortDesc("CMSException");
-    std::ostringstream longDesc;
-    longDesc << "cms::Exception caught in " 
-	     << kProgramName
-	     << "\n"
-	     << e.explainSelf();
     rc = 8001;
-    jobRep->reportError(shortDesc, longDesc.str(), rc);
-    edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+    edm::printCmsException(e, kProgramName, jobRep.get(), rc);
   }
   catch(std::bad_alloc& bda) {
-    std::string shortDesc("std::bad_allocException");
-    std::ostringstream longDesc;
-    longDesc << "std::bad_alloc exception caught in "
-	     << kProgramName
-	     << "\n"
-	     << "The job has probably exhausted the virtual memory available to the process.\n";
     rc = 8004;
-    jobRep->reportError(shortDesc, longDesc.str(), rc);
-    edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+    edm::printBadAllocException(kProgramName, jobRep.get(), rc);
   }
-
   catch (std::exception& e) {
-    std::string shortDesc("StdLibException");
-    std::ostringstream longDesc;
-    longDesc << "Standard library exception caught in " 
-	     << kProgramName
-	     << "\n"
-	     << e.what();
     rc = 8002;
-    jobRep->reportError(shortDesc, longDesc.str(), rc);
-    edm::LogSystem(shortDesc) << longDesc.str() << "\n";
-         
+    edm::printStdException(e, kProgramName, jobRep.get(), rc);
   }
   catch (...) {
-    std::string shortDesc("UnknownException");
-    std::ostringstream longDesc;
-    longDesc << "Unknown exception caught in "
-	     << kProgramName
-	     << "\n";
     rc = 8003;
-    jobRep->reportError(shortDesc, longDesc.str(), rc);
-    edm::LogSystem(shortDesc) << longDesc.str() << "\n";
+    edm::printUnknownException(kProgramName, jobRep.get(), rc);
   }
   
   return rc;
