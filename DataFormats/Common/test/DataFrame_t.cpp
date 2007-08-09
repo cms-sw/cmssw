1181d63a99677a6be5819ce76532097d2e961a6f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-08-09 15:01:35 +0000

    compiles, does not run

--- DataFormats/Common/test/DataFrame_t.cpp
+++ DataFormats/Common/test/DataFrame_t.cpp
@@ -8,6 +8,9 @@
 #include<vector>
 #include<algorithm>
 #include<cstdlib>
+#include <boost/bind.hpp>
+#include <numeric>
+
 
 class TestDataFrame: public CppUnit::TestFixture
 {
@@ -15,6 +18,7 @@ class TestDataFrame: public CppUnit::TestFixture
   CPPUNIT_TEST(default_ctor);
   CPPUNIT_TEST(filling);
   CPPUNIT_TEST(iterator);
+  CPPUNIT_TEST(sort);
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -27,17 +31,20 @@ class TestDataFrame: public CppUnit::TestFixture
   void default_ctor();
   void filling();
   void iterator();
+  void sort();
 
 public:
-  std::vector<edm::DataFrame::data_type> sv;
+  std::vector<edm::DataFrame::data_type> sv1;
+  std::vector<edm::DataFrame::data_type> sv2;
 
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestDataFrame);
 
-TestDataFrame::TestDataFrame() : sv(10){ 
+TestDataFrame::TestDataFrame() : sv1(10),sv2(10){ 
   edm::DataFrame::data_type v[10] = {0,1,2,3,4,5,6,7,8,9};
-  std::copy(v,v+10,sv.begin());
+  std::copy(v,v+10,sv1.begin());
+  std::transform(sv1.begin(),sv1.end(),sv2.begin(),boost::bind(std::plus<edm::DataFrame::data_type>(),10,_1));
 } 
 
 
@@ -60,8 +67,8 @@ void TestDataFrame::default_ctor() {
 void TestDataFrame::filling() {
 
   edm::DataFrameContainer frames(10,2);
-  for (int n=1;n<5;++n) {
-    int id=20+n;
+  for (unsigned int n=1;n<5;++n) {
+    unsigned int id=20+n;
     frames.push_back(id);
     CPPUNIT_ASSERT(frames.size()==n);
     edm::DataFrame df = frames.back();
@@ -69,13 +76,13 @@ void TestDataFrame::filling() {
     CPPUNIT_ASSERT(df.id()==id); 
     
     if (n%2==0)
-      std::copy(sv.begin(),sv.end(),df.begin());  
+      std::copy(sv1.begin(),sv1.end(),df.begin());  
     else
-      ::memcpy(&sv[0],&df[0], sizeof(edm::DataFrame::data_type)*frames.stride());
+      ::memcpy(&sv1[0],&df[0], sizeof(edm::DataFrame::data_type)*frames.stride());
     
     std::vector<edm::DataFrame::data_type> v2(10);
     std::copy(frames.m_data.begin()+(n-1)*10,frames.m_data.begin()+n*10,v2.begin());
-    CPPUNIT_ASSERT(sv==v2);
+    CPPUNIT_ASSERT(sv1==v2);
   }
 
 }
@@ -86,13 +93,16 @@ namespace {
     
     void operator()(edm::DataFrame const & df) {
       ++n;
-      CPPUNIT_ASSERT(df.id()==20+n); 
+      CPPUNIT_ASSERT(df.id()==2000+n); 
       std::vector<edm::DataFrame::data_type> v2(10);
       std::copy(df.begin(),df.end(),v2.begin());
-      CPPUNIT_ASSERT(test.sv==v2);
+      if (n%2==0) 
+	CPPUNIT_ASSERT(test.sv1==v2);
+      else
+	CPPUNIT_ASSERT(test.sv2==v2);
     }
     
-    int n;
+    unsigned int n;
   TestDataFrame & test;
   };
 }
@@ -100,14 +110,35 @@ namespace {
 void TestDataFrame::iterator() {
   edm::DataFrameContainer frames(10,2);
   for (int n=1;n<5;++n) {
-    int id=20+n;
+    int id=2000+n;
     frames.push_back(id);
     edm::DataFrame df = frames.back();
-    std::copy(sv.begin(),sv.end(),df.begin());
+    if (n%2==0) 
+      std::copy(sv1.begin(),sv1.end(),df.begin());
+    else
+      std::copy(sv2.begin(),sv2.end(),df.begin());
   }
   CPPUNIT_ASSERT(std::for_each(frames.begin(),frames.end(),VerifyIter(this)).n==4);
+}
 
 
+void TestDataFrame::sort() {
+  edm::DataFrameContainer frames(10,2);
+  std::vector<unsigned int> ids(100,1);
+  ids[0]=2001;
+  std::partial_sum(ids.begin(),ids.end(),ids.begin());
+  std::random_shuffle(ids.begin(),ids.end());
+
+  for (int n=0;n<100;++n) {
+    frames.push_back(ids[n]);
+    edm::DataFrame df = frames.back();
+    if (ids[n]%2==0) 
+      std::copy(sv1.begin(),sv1.end(),df.begin());
+    else
+      std::copy(sv2.begin(),sv2.end(),df.begin());
+  }
+  frames.sort();
+  CPPUNIT_ASSERT(std::for_each(frames.begin(),frames.end(),VerifyIter(this)).n==100);
 }
 
 
