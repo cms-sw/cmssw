ae64bd40e701288e3caf123d28797bd9532ee7ca
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-08-14 20:41:29 +0000

    Allow the suppression of heavy secondaries during transfer to the stack

--- SimG4Core/Application/interface/StackingAction.h
+++ SimG4Core/Application/interface/StackingAction.h
@@ -6,16 +6,17 @@
 #include "G4UserStackingAction.hh"
 #include "G4Track.hh"
 
-class StackingAction : public G4UserStackingAction
-{
+class StackingAction : public G4UserStackingAction {
+
 public:
-    StackingAction(const edm::ParameterSet & ps);
-    virtual ~StackingAction();
-    virtual G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track * aTrack);
-    virtual void NewStage();
-    virtual void PrepareNewEvent();
+  StackingAction(const edm::ParameterSet & ps);
+  virtual ~StackingAction();
+  virtual G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track * aTrack);
+  virtual void NewStage();
+  virtual void PrepareNewEvent();
 private:
-    bool savePrimaryDecayProductsAndConversions;
+  bool   savePrimaryDecayProductsAndConversions, suppressHeavy;
+  double pmaxIon, pmaxNeutron, pmaxProton;
 };
 
 #endif
--- SimG4Core/Application/src/StackingAction.cc
+++ SimG4Core/Application/src/StackingAction.cc
@@ -1,25 +1,55 @@
 #include "SimG4Core/Application/interface/StackingAction.h"
 #include "SimG4Core/Notification/interface/CurrentG4Track.h"
 #include "SimG4Core/Notification/interface/NewTrackAction.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
  
-StackingAction::StackingAction(const edm::ParameterSet & p) 
-: savePrimaryDecayProductsAndConversions(p.getUntrackedParameter<bool>("SavePrimaryDecayProductsAndConversions",false)) {}
+StackingAction::StackingAction(const edm::ParameterSet & p) {
+  savePrimaryDecayProductsAndConversions = p.getUntrackedParameter<bool>("SavePrimaryDecayProductsAndConversions",false);
+  suppressHeavy = p.getUntrackedParameter<bool>("SuppressHeavy", false);
+  pmaxIon       = p.getUntrackedParameter<double>("IonThreshold", 50.0)*MeV;
+  pmaxProton    = p.getUntrackedParameter<double>("ProtonThreshold", 50.0)*MeV;
+  pmaxNeutron   = p.getUntrackedParameter<double>("NeutronThreshold", 50.0)*MeV;
+  edm::LogInfo("SimG4CoreApplication") << "StackingAction initiated with"
+				       << " flag for saving decay products "
+				       <<savePrimaryDecayProductsAndConversions
+				       << " Suppression Flag " << suppressHeavy
+				       << " protons below " << pmaxProton 
+				       << " MeV/c, neutrons below "
+				       << pmaxNeutron << " MeV/c and ions"
+				       << " below " << pmaxIon << " MeV/c\n";
+
+}
 
 StackingAction::~StackingAction() {}
 
-G4ClassificationOfNewTrack StackingAction::ClassifyNewTrack(const G4Track * aTrack)
-{
-    NewTrackAction newTA(savePrimaryDecayProductsAndConversions);
-    if (aTrack->GetCreatorProcess()==0 || aTrack->GetParentID()==0)
-        newTA.primary(aTrack);
-    else
-    {
-        const G4Track * mother = CurrentG4Track::track();
-        newTA.secondary(aTrack, *mother);
+G4ClassificationOfNewTrack StackingAction::ClassifyNewTrack(const G4Track * aTrack) {
+
+  // G4 interface part
+  G4ClassificationOfNewTrack classification = fUrgent;
+
+  NewTrackAction newTA(savePrimaryDecayProductsAndConversions);
+  if (aTrack->GetCreatorProcess()==0 || aTrack->GetParentID()==0)
+    newTA.primary(aTrack);
+  else {
+    const G4Track * mother = CurrentG4Track::track();
+    newTA.secondary(aTrack, *mother);
+    if (suppressHeavy) {
+      int    pdg = aTrack->GetDefinition()->GetPDGEncoding();
+      double pp  = aTrack->GetMomentum().mag()/MeV;
+      if (((pdg/1000000000 == 1) && (((pdg/10000)%100) > 0) && 
+	   (((pdg/10)%100) > 0) && (pp<pmaxIon)) || 
+	  ((pdg == 2212) && (pp < pmaxProton)) ||
+	  ((pdg == 2112) && (pp < pmaxNeutron))) classification = fKill;
     }
-    // G4 interface part
-    G4ClassificationOfNewTrack classification = fUrgent;
-    return classification;
+    LogDebug("SimG4CoreApplication") << "StackingAction:Classify Track "
+				     << aTrack->GetTrackID() << " Parent " 
+				     << aTrack->GetParentID() << " Type "
+				     << aTrack->GetDefinition()->GetParticleName() 
+				     << " Momentum " << aTrack->GetMomentum().mag()/MeV
+				     << " MeV/c as " << classification;
+  }
+  return classification;
 }
 
 void StackingAction::NewStage() {}
