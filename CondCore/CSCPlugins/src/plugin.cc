f4132fa64bad6292597b5b35c4d9da8e377400fe
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-08-15 17:08:32 +0000

    added new CSC object for Gains

--- CondCore/CSCPlugins/src/plugin.cc
+++ CondCore/CSCPlugins/src/plugin.cc
@@ -10,7 +10,9 @@
 #include "CondFormats/CSCObjects/interface/CSCPedestals.h"
 #include "CondFormats/DataRecord/interface/CSCPedestalsRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCGains.h"
+#include "CondFormats/CSCObjects/interface/CSCDBGains.h"
 #include "CondFormats/DataRecord/interface/CSCGainsRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBGainsRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
 #include "CondFormats/DataRecord/interface/CSCcrosstalkRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
@@ -23,6 +25,7 @@
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(CSCPedestalsRcd,CSCPedestals);
 REGISTER_PLUGIN(CSCGainsRcd,CSCGains);
+REGISTER_PLUGIN(CSCDBGainsRcd,CSCDBGains);
 REGISTER_PLUGIN(CSCcrosstalkRcd,CSCcrosstalk);
 REGISTER_PLUGIN(CSCNoiseMatrixRcd,CSCNoiseMatrix);
 REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
