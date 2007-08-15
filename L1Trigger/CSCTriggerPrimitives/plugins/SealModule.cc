93522f7050591eaa048089af4e977639b8ba26d5
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2007-08-15 13:34:41 +0000

    Small clean-up.

--- L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
+++ L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
@@ -1,7 +1,5 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <L1Trigger/CSCTriggerPrimitives/plugins/CSCTriggerPrimitivesProducer.h>
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(CSCTriggerPrimitivesProducer);
+DEFINE_FWK_MODULE(CSCTriggerPrimitivesProducer);
