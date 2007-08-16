34b0df394bd5552e3d4c33e692372d786f2c8cce
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-08-16 14:57:22 +0000

    added new plugins for Frontier objects

--- CondCore/CSCPlugins/src/plugin.cc
+++ CondCore/CSCPlugins/src/plugin.cc
@@ -8,25 +8,34 @@
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/CSCObjects/interface/CSCPedestals.h"
+#include "CondFormats/CSCObjects/interface/CSCDBPedestals.h"
 #include "CondFormats/DataRecord/interface/CSCPedestalsRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBPedestalsRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCGains.h"
 #include "CondFormats/CSCObjects/interface/CSCDBGains.h"
 #include "CondFormats/DataRecord/interface/CSCGainsRcd.h"
 #include "CondFormats/DataRecord/interface/CSCDBGainsRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
+#include "CondFormats/CSCObjects/interface/CSCDBCrosstalk.h"
 #include "CondFormats/DataRecord/interface/CSCcrosstalkRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBCrosstalkRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCIdentifier.h"
 #include "CondFormats/DataRecord/interface/CSCIdentifierRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
+#include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
 #include "CondFormats/DataRecord/interface/CSCNoiseMatrixRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBNoiseMatrixRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCReadoutMapping.h"
 #include "CondFormats/DataRecord/interface/CSCReadoutMappingRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(CSCPedestalsRcd,CSCPedestals);
+REGISTER_PLUGIN(CSCDBPedestalsRcd,CSCDBPedestals);
 REGISTER_PLUGIN(CSCGainsRcd,CSCGains);
 REGISTER_PLUGIN(CSCDBGainsRcd,CSCDBGains);
 REGISTER_PLUGIN(CSCcrosstalkRcd,CSCcrosstalk);
+REGISTER_PLUGIN(CSCDBCrosstalkRcd,CSCDBCrosstalk);
 REGISTER_PLUGIN(CSCNoiseMatrixRcd,CSCNoiseMatrix);
+REGISTER_PLUGIN(CSCDBNoiseMatrixRcd,CSCDBNoiseMatrix);
 REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
 REGISTER_PLUGIN(CSCReadoutMappingRcd,CSCReadoutMapping);
