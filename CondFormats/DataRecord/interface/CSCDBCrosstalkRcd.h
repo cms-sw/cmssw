58a56a808a752593c8663457e2e50876c8fb9e54
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-08-16 14:54:22 +0000

    added new records for Frontier objects

---
+++ CondFormats/DataRecord/interface/CSCDBCrosstalkRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCDBCROSSTALKRCD_H
+#define CSCDBCROSSTALKRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCDBCrosstalkRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCDBCrosstalkRcd> {};
+#endif
---
+++ CondFormats/DataRecord/interface/CSCDBPedestalsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCDBPEDESTALSRCD_H
+#define CSCDBPEDESTALSRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCDBPedestalsRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCDBPedestalsRcd> {};
+#endif
