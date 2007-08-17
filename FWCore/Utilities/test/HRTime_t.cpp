59be81c212d34c7402df56bacf14bac7f1084284
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-08-17 07:12:20 +0000

    HR test

--- FWCore/Utilities/test/HRTime_t.cpp
+++ FWCore/Utilities/test/HRTime_t.cpp
@@ -1,10 +1,8 @@
-#include <Utilities/Testing/interface/CppUnit_testdriver.icpp>
-
 
 #include "FWCore/Utilities/interface/HRRealTime.h"
 
-#include <boost/mpl/assert.hpp>
-#include <boost/type_traits/is_same.hpp>
+// #include <boost/mpl/assert.hpp>
+// #include <boost/type_traits/is_same.hpp>
 #include <boost/type_traits/function_traits.hpp>
 
 // #include <boost/function_types/result_type.hpp>
@@ -16,6 +14,7 @@
 #include <iostream>
 
 
+#include <Utilities/Testing/interface/CppUnit_testdriver.icpp>
 #include <cppunit/extensions/HelperMacros.h>
 
 
@@ -47,11 +46,12 @@ CPPUNIT_TEST_SUITE_REGISTRATION(TestTimers);
 
 
 template<typename S>
-void checkTime(S source) {
+void checkTime(S source, bool hr) {
   
-  typedef typename boost::function_traits<S>::result_type T;
+//  typedef typename boost::function_traits<S>::result_type T;
   // typedef typename boost::function_types::result_type<S>::type T;
-  
+  typedef edm::HRTimeDiffType T;
+
   T i = source();
 
   CPPUNIT_ASSERT(!( i<0 ));
@@ -64,7 +64,7 @@ void checkTime(S source) {
   T b = source();
   CPPUNIT_ASSERT(!( (a-i)<0 ));
   CPPUNIT_ASSERT(!( (b-a)<0 ));
-  CPPUNIT_ASSERT(a>i); // not obvious if low resolution
+  if (hr) CPPUNIT_ASSERT(a>i); // not obvious if low resolution
   
   waiste();
   
@@ -76,21 +76,21 @@ void checkTime(S source) {
   T e = source();
   CPPUNIT_ASSERT(!( (c-i)<(b-i) ));
   CPPUNIT_ASSERT(!( (e-i)<(c-i) ));
-  CPPUNIT_ASSERT( (e-i)>(b-i) ); // not obvious if low resolution...
+  if (hr) CPPUNIT_ASSERT( (e-i)>(b-i) ); // not obvious if low resolution...
 
 }
 
-#define CHECKTIME(S) \
+#define CHECKTIME(S,HR) \
   std::cout << "checking source " << #S << std::endl;	\
-  checkTime(&S)
+  checkTime(&S,HR)
 
 
 void TestTimers::check_stdclock() {
-  CHECKTIME(std::clock);
+  CHECKTIME(std::clock,false);
 }
 
 void TestTimers::check_RealTime() {
-  CHECKTIME(edm::hrRealTime);
+  CHECKTIME(edm::hrRealTime,true);
 }
 
 
