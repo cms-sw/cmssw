a9a4d688500d3c196d274d1867e273017fdeebce
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2007-08-20 08:40:35 +0000

    Pixel CPE errors record

---
+++ CondFormats/DataRecord/src/PixelCPEParmErrorsRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     PixelCPEParmErrorsRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Fri Aug 17 08:18:51 CDT 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/PixelCPEParmErrorsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(PixelCPEParmErrorsRcd);
