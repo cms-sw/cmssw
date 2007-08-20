2c958887010542abe19504269186ce18e8418c63
Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch>
2007-08-20 08:59:41 +0000

    Add Pixel CPE errors

--- CondCore/SiPixelPlugins/src/plugin.cc
+++ CondCore/SiPixelPlugins/src/plugin.cc
@@ -6,8 +6,11 @@
 #include "CondFormats/DataRecord/interface/SiPixelGainCalibrationRcd.h"
 #include "CondFormats/SiPixelObjects/interface/SiPixelLorentzAngle.h"
 #include "CondFormats/DataRecord/interface/SiPixelLorentzAngleRcd.h"
+#include "CondFormats/SiPixelObjects/interface/PixelCPEParmErrors.h"
+#include "CondFormats/DataRecord/interface/PixelCPEParmErrorsRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(SiPixelFedCablingMapRcd,SiPixelFedCablingMap);
 REGISTER_PLUGIN(SiPixelGainCalibrationRcd,SiPixelGainCalibration);
 REGISTER_PLUGIN(SiPixelLorentzAngleRcd,SiPixelLorentzAngle);
+REGISTER_PLUGIN(PixelCPEParmErrorsRcd,PixelCPEParmErrors);
