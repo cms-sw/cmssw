4a4bdbf36c092b3c183164e28da077ce2ffa6d20
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2007-08-22 16:54:35 +0000

    reduce memory usage

--- DataFormats/MuonDetId/interface/CSCIndexer.h
+++ DataFormats/MuonDetId/interface/CSCIndexer.h
@@ -2,9 +2,26 @@
 #define MuonDetId_CSCIndexer_h
 
 /** \class CSCIndexer
- * Handle creation and access of a linear index for each layer 
- * and each strip channel of CSC chamber system
- * 
+ * Creates a linear index for various sublevels of the endcap muon CSC system.
+ *
+ * It supplies a linear index for:
+ * 1. Each chamber of the CSC system: range 1-468 <br>
+ * 2. Each layer of the CSC system: range 1-2808 <br>
+ * 3. Each strip channel of the CSC system: range 1-217728 <br>
+ *
+ * The chamber and layer may be specified by CSCDetId or labels for endcap, station, ring, chamber, layer.
+ * The strip channel is a value 1-80 (or 64: ME13 chambers have only 64 channels.)
+ *
+ * The main user interface is the set of functions <br>
+ *   chamberIndex(.) <br>
+ *   layerIndex(.) <br>
+ *   stripChannelIndex(.) <br>
+ * But the other functions are public since they may be useful in contexts other than for
+ * Conditions Data for which the above functions are intended.
+ *
+ * \warning This class is hard-wired for the CSC system at start-up of CMS in 2008.
+ * with rings ME11, ME12, ME13, ME21, ME22, ME31, ME32, ME41 totalling 234 chambers per endcap.
+ *
  * \warning EVERY LABEL COUNTS FROM ONE NOT ZERO.
  *
  */
@@ -12,95 +29,95 @@
 #include <DataFormats/MuonDetId/interface/CSCDetId.h>
 #include <vector>
 #include <iosfwd>
+#include <utility>  // for pair
 
 class CSCIndexer {
 
+  //  typedef unsigned short int IndexType;
+  //  typedef unsigned       int LongIndexType;
+  typedef uint16_t IndexType;
+  typedef uint32_t LongIndexType;
+
 public:
-	CSCIndexer();
-	~CSCIndexer(){};
+  CSCIndexer(){};
+  ~CSCIndexer(){};
 
   /**
-   * Linear index to label each layer of each hardware chamber.
-   * Arguments are the usual csc labels for endcap, station, ring, chamber, layer.
+   * Linear index to label each CSC in CSC system.
+   * Argument is the CSCDetId of some CSCChamber.
    *
-   * Output is 1 to 2808
+   * Output is 1 to 468.
    *
-   * WARNING: Do not input ME1a values (i.e. ring 4): does not consider ME1a and ME1b to be separate,
-   * No sanity checking on input values!
-   */
-   unsigned int layerIndex( unsigned short int ie, unsigned short int is, unsigned short int ir, unsigned short int ic, unsigned short int il) const {
-    const unsigned short int chambersInEndcap = 234;
-    const unsigned short int layersInChamber = 6;
-    return layersInChamber*( (ie-1)*chambersInEndcap + chambersUpToStation(is-1)
-	       + chambersInStationUpToRing(is, ir-1) + (ic-1) )+ il;
-  }
-
-  /**
-   * Linear index to label each layer in CSC subdetectors.
-   * Argument is a CSCDetId.
-   *
-   * Output is 1 to 2808
-   *
-   * WARNING: Do not input ME1a values (i.e. ring 4): does not consider ME1a and ME1b to be separate,
+   * WARNING: Do not input ME1a values (i.e. ring '4'): does not consider ME1a and ME1b to be separate,
    * No sanity checking on input value: if you supply an ME1a CSCDetId then you'll get nonsense.
    */
-   unsigned int layerIndex( CSCDetId& id ) const {
-    return layerIndex(id.endcap(), id.station(), id.ring(), id.chamber(), id.layer());
-  }
 
-	/**
-	 * How many rings are there in station is=1, 2, 3, 4 ?
-	*/
-	unsigned short int ringsInStation( unsigned short int is ){
-		const unsigned short int nrins[5]={0,3,2,2,1}; // rings per station
-		return nrins[is];
-	}
+   IndexType chamberIndex( CSCDetId& id ) const {
+     return chamberIndex( id.endcap(), id.station(), id.ring(), id.chamber() );
+   }
 
   /**
-   * How many chambers are there in a station is=1, 2, 3, 4 ?
+   * Linear index to label each hardware layer in CSC system.
+   * Argument is the CSCDetId of some CSCLayer.
    *
-   * Does not consider ME1a and ME1b to be separate.
+   * Output is 1 to 2808.
+   *
+   * WARNING: Do not input ME1a values (i.e. ring '4'): does not consider ME1a and ME1b to be separate,
+   * No sanity checking on input value: if you supply an ME1a CSCDetId then you'll get nonsense.
    */
-   unsigned short int chambersInStation( unsigned short int is ) const {
-      const unsigned short int ncins[5]={0,108,54,54,18}; // chambers per station (-) 1 2 3 4
-      return ncins[is];
-    }
+
+   IndexType layerIndex( CSCDetId& id ) const {
+     return layerIndex(id.endcap(), id.station(), id.ring(), id.chamber(), id.layer());
+   }
+
+   /** 
+    * Starting index in range 1-234 for first chamber in ring ir of station is
+    */
+   IndexType startChamberIndexInEndcap(IndexType is, IndexType ir) const {
+     const IndexType nschin[12] = {1,37,73, 109,127,0, 163,181,0, 217,0,0};
+     return nschin[(is-1)*3+ir-1];
+   }
+
+   /** 
+    * Starting index in range 1-468 for first chamber in ring ir of station is in endcap ie
+    */
+   IndexType startChamberIndex(IndexType ie, IndexType is, IndexType ir) const {
+     const IndexType nchpere = 234;
+     return (ie-1)*nchpere + startChamberIndexInEndcap(is, ir);
+   }
+
+   /**
+    *  Linear index in range 1-468 for chamber ic in ring ir of station is in endcap ie
+    */
+   IndexType chamberIndex(IndexType ie, IndexType is, IndexType ir, IndexType ic) const {
+     return startChamberIndex(ie,is,ir) + ic - 1; // -1 so start index _is_ ic=1
+   }
+
+   /**
+    * Linear index in 1-2808 for layer il of chamber ic in ring ir of station is in endcap ie
+    */
+   IndexType layerIndex(IndexType ie, IndexType is, IndexType ir, IndexType ic, IndexType il) const {
+    const IndexType layersInChamber = 6;
+     return (chamberIndex(ie,is,ir,ic) - 1 ) * layersInChamber + il;
+   }
 
   /**
-   * How many chambers in stations UP TO AND INCLUDING  is=1, 2, 3, 4 ?
-   *
-   * Does not consider ME1a and ME1b to be separate.
+   * How many rings are there in station is=1, 2, 3, 4 ?
    */
-   unsigned short int chambersUpToStation( unsigned short int is ) const {
-      const unsigned short int ncins[5]={0, 108,162,216,234}; // chambers in: 0, 1, 1+2, 1+2+3, 1+2+3+4
-      return ncins[is];
-    }
+   IndexType ringsInStation( IndexType is ) const {
+      const IndexType nrins[5] = {0,3,2,2,1}; // rings per station
+      return nrins[is];
+   }
 
   /**
    * How many chambers are there in ring ir of station is?
    *
    * Works for ME1a (ring 4 of ME1) too.
    */
-   unsigned short int chambersInRingOfStation(unsigned short int is, unsigned short int ir) const {
-    short int nc = 36; // most rings have 36 chambers
-    if (is >1 && ir<2 ) nc = 18; // but 21, 31, 41 have 18
-    return nc;
-  }
-
-  /**
-   * How many chambers are there in a station is=1, 2, 3, 4, UP TO AND INCLUDING ring ir?
-   *
-   * WARNING: Do not input ME1a values (i.e. ring 4): does not consider ME1a and ME1b to be separate,
-   * No sanity checking on input values!
-   */
-   unsigned short int chambersInStationUpToRing( unsigned short int is, unsigned short int ir) const {
-    //             ir=     1  2    3   
-    // station 1 rings (0),1,1+2,1+2+3  
-    // station 2 rings (0),1,1+2,1+2 (only 2 rings)
-    // station 3 rings (0),1,1+2,1+2 (only 2 rings)
-    // station 4 rings (0),1, 1,  1 (only 1 ring)
-    const unsigned short int nCinSuptoR[16]={0,36,72,108,  0,18,54,54,  0,18,54,54,  0,18,18,18};
-    return nCinSuptoR[(is-1)*4+ir];
+   IndexType chambersInRingOfStation(IndexType is, IndexType ir) const {
+      IndexType nc = 36; // most rings have 36 chambers
+      if (is >1 && ir<2 ) nc = 18; // but 21, 31, 41 have 18
+      return nc;
    }
 
   /** 
@@ -111,10 +128,10 @@ public:
    * WARNING: ME1a channels are the last 16 of the 80 total in each layer of an ME11 chamber, 
    * and an input ir=4 is invalid and will give nonsense.
    */
-   unsigned short int stripChannelsPerLayer( unsigned short int is, unsigned short int ir ) const {
-    const unsigned short int nSCinC[12] = { 80,80,64, 80,80,0, 80,80,0, 80,0,0 };
-    return nSCinC[(is-1)*3 + ir - 1];
-  }
+   IndexType stripChannelsPerLayer( IndexType is, IndexType ir ) const {
+     const IndexType nSCinC[12] = { 80,80,64, 80,80,0, 80,80,0, 80,0,0 };
+     return nSCinC[(is-1)*3 + ir - 1];
+   }
 
   /**
    * Linear index for 1st strip channel in ring ir of station is in endcap ie.
@@ -124,40 +141,54 @@ public:
    * WARNING: ME1a channels are the last 16 of the 80 total in each layer of an ME11 chamber, 
    * and an input ir=4 is invalid and will give nonsense.
    */
-	unsigned int stripChannelStart( unsigned short int ie, unsigned short int is, unsigned short int ir ) const {
-		const unsigned int stripChannelsPerEndcap = 108864;
-		const unsigned int nStart[12] = { 1,17281,34561, 48385,57025,0, 74305,82945,0, 100225,0,0 };
-		return (ie-1)*stripChannelsPerEndcap + nStart[(is-1)*3 + ir - 1];
-	}
+   LongIndexType stripChannelStart( IndexType ie, IndexType is, IndexType ir ) const {
+      const LongIndexType stripChannelsPerEndcap = 108864;
+      const LongIndexType nStart[12] = { 1,17281,34561, 48385,57025,0, 74305,82945,0, 100225,0,0 };
+      return (ie-1)*stripChannelsPerEndcap + nStart[(is-1)*3 + ir - 1];
+   }
 
   /**
    * Linear index for strip channel istrip in layer il of chamber ic of ring ir
    * in station is of endcap ie.
    *
-   * Use at your own risk! You must input labels within hardware ranges.
+   * Output is 1-217728.
+   *
+   * WARNING: Use at your own risk! You must input labels within hardware ranges.
    * No trapping on out-of-range values!
    */
-	unsigned int stripChannelIndex( unsigned short int ie, unsigned short int is, unsigned short int ir, unsigned short int ic, unsigned short int il, unsigned short int istrip ) const {
-		return stripChannelStart(ie,is,ir)+( (ic-1)*6 + il - 1 )*stripChannelsPerLayer(is,ir) + (istrip-1);
-  }
+   LongIndexType stripChannelIndex( IndexType ie, IndexType is, IndexType ir, IndexType ic, IndexType il, IndexType istrip ) const {
+      return stripChannelStart(ie,is,ir)+( (ic-1)*6 + il - 1 )*stripChannelsPerLayer(is,ir) + (istrip-1);
+   }
 
   /**
-   *  Decode CSCDetId from layer linear index
+   * Linear index for strip channel istrip in layer labelled by CSCDetId id.
+   *
+   * Output is 1-217728.
+   *
+   * WARNING: Use at your own risk! The supplied CSCDetId must be a layer id.
+   * No trapping on out-of-range values!
    */
-	CSCDetId detIdFromLayer( unsigned int lin ) const;
+
+   LongIndexType stripChannelIndex( CSCDetId& id, IndexType istrip ) const {
+      return stripChannelIndex(id.endcap(), id.station(), id.ring(), id.chamber(), id.layer(), istrip );
+   }
 
   /**
-   * Decode CSCDetId from strip channel linear index
+   *  Decode CSCDetId from various indexes and labels
    */
-	CSCDetId detIdFromStrip( unsigned int sin ) const;
-  
-  /**
-   * Decode strip channel index from overall strip channel linear index
-   */
-	unsigned short int stripChannel( unsigned int sin ) const;
+  CSCDetId detIdFromLayerIndex( IndexType ili ) const;
+  CSCDetId detIdFromChamberIndex( IndexType ici ) const;
+  CSCDetId detIdFromChamberIndex_OLD( IndexType ici ) const;
+  CSCDetId detIdFromChamberLabel( IndexType ie, IndexType icl ) const;
+  std::pair<CSCDetId, IndexType> detIdFromStripChannelIndex( LongIndexType ichi ) const;
+
+  IndexType checkLabel( IndexType ) const;
 
 private:
-	std::vector<unsigned int> igorIndex;
+  void fillChamberLabel() const; // const so it can be called in const function detIdFromChamberIndex
+
+  mutable std::vector<IndexType> chamberLabel; // mutable so can be filled by fillChamberLabel
+
 
 };
 
--- DataFormats/MuonDetId/src/CSCIndexer.cc
+++ DataFormats/MuonDetId/src/CSCIndexer.cc
@@ -1,107 +1,138 @@
 #include <DataFormats/MuonDetId/interface/CSCIndexer.h>
 #include <iostream>
+
+void CSCIndexer::fillChamberLabel() const {
+  // Fill the member vector which permits decoding of the linear chamber index
+  // Logically const since initializes cache only
+  chamberLabel.resize( 235 ); // one more than #chambers per endcap
+   IndexType count = 0;
+   chamberLabel[count] = 0;
+
+   for ( IndexType is = 1 ; is != 5; ++is ) {
+      IndexType irmax = ringsInStation(is);
+      for ( IndexType ir = 1; ir != irmax+1; ++ir ) {
+         IndexType icmax = chambersInRingOfStation(is, ir);
+         for ( IndexType ic = 1; ic != icmax+1; ++ic ) {
+	   chamberLabel[ ++count ] = is*1000 + ir*100 + ic ;
+         }
+      } 
+   }
+}
+
+CSCDetId CSCIndexer::detIdFromChamberIndex_OLD( IndexType ici ) const {
+  IndexType ie = 1;
+  if (ici > 234 ) {
+     ie = 2;
+     ici -= 234; 
+  }
+  // Now ici is in range 1-234 (assuming valid input in range 1-468)
+
+  // MEij pairs...
+  const IndexType station[] = {0,1,1,1,2,2,3,3,4};
+  const IndexType ring[]    = {0,1,2,3,1,2,1,2,1};
+
+  // MEij preceding a given MEij matching linear index above
+  const IndexType prevs[] =  {0,0,1,1,1,2,2,3,3}; 
+  const IndexType prevr[] =  {0,0,1,2,3,1,2,1,2};
+
+  IndexType is = 4;
+  IndexType ir = 1;
+  for ( IndexType i = 2; i<=8; ++i) {
+    IndexType js = station[i];
+    IndexType jr = ring[i];
+    // if it's before start of MEjs/jr then it's in the previous MEis/ir
+      if ( ici < startChamberIndexInEndcap(js,jr) ) {
+	is = prevs[i];
+	ir = prevr[i];
+	break;
+      }
+      // otherwise it's in ME41
+  }
+  IndexType ic = ici - startChamberIndexInEndcap(is,ir) + 1;
+
+  return CSCDetId( ie, is, ir, ic );
+}
  
-CSCIndexer::CSCIndexer(){
-	// Fill the member vector which permits decoding of the linear index
-	igorIndex.resize(217729);
-	unsigned int id = 0;
+CSCDetId CSCIndexer::detIdFromChamberIndex( IndexType ici ) const {
+  IndexType ie = 1;
+  if ( ici > 234 ) {
+	  ie = 2;
+	  ici -= 234;
+  }
+
+  if (chamberLabel.empty()) fillChamberLabel();
+  IndexType label = chamberLabel[ici];  
+  return detIdFromChamberLabel( ie, label );
+}
+
+CSCDetId CSCIndexer::detIdFromChamberLabel( IndexType ie, IndexType label ) const {
+
+  IndexType is = label/1000;
+  label -= is*1000;
+  IndexType ir = label/100;
+  label -= ir*100;
+  IndexType ic = label;
+
+  return CSCDetId( ie, is, ir, ic );
+}
+
+CSCDetId CSCIndexer::detIdFromLayerIndex( IndexType ili ) const {
+  IndexType ie = 1;
+  if ( ili > 1404 ) {
+     ie = 2;
+     ili -= 1404;
+  }
+  IndexType il = (ili-1)%6 + 1;
+  IndexType ici = (ili-1)/6 + 1;
+  if (chamberLabel.empty()) fillChamberLabel();
+  IndexType label = chamberLabel[ici];
+  CSCDetId id = detIdFromChamberLabel(ie, label);
 	
-	for(unsigned short int ie=1; ie!=3; ++ie){
-	    for(unsigned short int is=1 ; is!=5; ++is){
-		unsigned short int irmax = ringsInStation(is);
-	      	for(unsigned short int ir=1; ir!=irmax+1; ++ir){
-		    unsigned short int ischmax = stripChannelsPerLayer(is, ir);
-		    unsigned short int icmax = chambersInRingOfStation(is, ir);
-		    for(unsigned short int ic=1; ic!=icmax+1; ++ic){
-			for(unsigned short int il=1; il!=7; ++il){
-			    for(unsigned short int isch=1; isch!=ischmax+1; ++isch){
-				id = ie*10000000 + is*1000000 + ir*100000 + ic*1000 + il*100 + isch;
-			        // if ( isch==1 ){
-				//   std::cout << "CSCIndexer ctor: id = " << id << std::endl;
-				// }
-				igorIndex[ stripChannelIndex(ie,is,ir,ic,il,isch) ] = id;
-			    }
-			}
-		    }
-		}
-	    }
-	}	
-
+  return CSCDetId(ie, id.station(), id.ring(), id.chamber(), il);
 }
 
-CSCDetId CSCIndexer::detIdFromLayer( unsigned int lin ) const {
+std::pair<CSCDetId, CSCIndexer::IndexType>  CSCIndexer::detIdFromStripChannelIndex( LongIndexType isi ) const {
 
-	short linl = (lin-1)%6 + 1; // Turn layer 1-6 to 0-5 and then everything else is divisible by 6
-	unsigned int chambersAll = (lin-linl)/6; // split off layer label part
-	short line = chambersAll/234 + 1; // pick off endcap label
-	unsigned int ix = chambersAll%234 + 1; // split off endcap label part
+   const LongIndexType lastPlusEndcap = 108864; // = 217728/2
+   const LongIndexType firstme13  = 34561; // First channel of ME13
+   const LongIndexType lastme13   = 48384; // Last channel of ME13
 
-     // Station label 
-	short lins = 0;
-	if ( ix <= chambersUpToStation(1) ) lins = 1;
-	else if ( ix <= chambersUpToStation(2) ) lins = 2;
-	else if ( ix <= chambersUpToStation(3) )  lins = 3;
-	else lins = 4;
+   const IndexType lastPlusEndcapLayer = 1404; // = 2808/2
+   const IndexType firstme13layer  = 433; // = 72*6 + 1 (ME13 chambers are 72-108 in range 1-234)
+   const IndexType lastme13layer   = 648; // = 108*6
+	
+   IndexType ie = 1;
+   if ( isi > lastPlusEndcap ) {
+      ie = 2;
+      isi -= lastPlusEndcap;
+   }
+	
+   // Defaults: before ME13
+   LongIndexType istart = 0;
+   IndexType nchan = 80;
+   IndexType layerOffset = 0;
+	
+   if ( isi > lastme13 ) { // after ME13
+      istart = lastme13;
+      layerOffset = lastme13layer;
+   }
+   else if ( isi >= firstme13 ) { // ME13
+      istart = firstme13 - 1;
+      layerOffset = firstme13layer - 1;
+      nchan = 64;
+   }
 
-     // Remove the station part
-	unsigned iy = ix - chambersUpToStation(lins-1);
-     
-	short linr = 0;
-	if ( iy <= chambersInStationUpToRing(lins, 1) )  linr = 1;
-	else if ( iy <= chambersInStationUpToRing(lins, 2) ) linr = 2;
-	else linr = 3;
-
-     // Remove the ring part
-	short linc = iy - chambersInStationUpToRing(lins, linr-1);
-
-     //     std::cout << "Decoded id E" << line << " S" << lins << " R" << 
-     //          linr << " C" << linc << " L" << linl << std::endl;
-
-	return CSCDetId( line, lins, linr, linc, linl );
- }
-
-CSCDetId CSCIndexer::detIdFromStrip( unsigned int sin ) const {
-	unsigned int label = igorIndex.at( sin );
-	unsigned short int isch = label%100;
-	label -= isch;
-	unsigned short int ie = label/10000000;
-	label -= ie*10000000;
-	unsigned short int is = label/1000000;
-	label -= is*1000000;
-	unsigned short int ir = label/100000;
-	label -= ir*100000;
-	unsigned short int ic = label/1000;
-	label -= ic*1000;
-	unsigned short int il = label/100;
-	label -= il*100;
-	//	if ( label != 0 ) {
-	//		std::cout << "CSCIndexer:detIdFromStrip: failed to decode input=" << 
-	//		sin << ", last value =" << label << std::endl;
-	//	}
-	return CSCDetId( ie, is, ir, ic, il );
+   isi -= istart; // remove earlier group(s)
+   IndexType ichan = (isi-1)%nchan + 1;
+   IndexType ili = (isi-1)/nchan + 1;
+   ili += layerOffset; // add appropriate offset for earlier group(s)
+   if ( ie != 1 ) ili+= lastPlusEndcapLayer; // add offset to -z endcap
+	
+   return std::pair<CSCDetId, IndexType>(detIdFromLayerIndex(ili), ichan);
 }
 
-unsigned short int CSCIndexer::stripChannel( unsigned int isch ) const { 
-  //  std::cout << "stripChannel for isch=" << isch << " returns " << igorIndex.at(isch) << std::endl;
-  return (igorIndex.at(isch)) % 100;
+ 
+CSCIndexer::IndexType CSCIndexer::checkLabel( IndexType ici ) const {
+  if (chamberLabel.empty()) fillChamberLabel();
+  return chamberLabel[ici];  
 }
-
-// The following implementation is superseded by more cryptic inline version...
-
-/*
-   unsigned short int CSCIndexer::chambersInStationUpToRing(short int is, short int ir) const {
-     short nc = 0 ;
-     if ( ir == 1 ) {
-       if ( is == 1 )  nc = 36;
-       else nc = 18;
-     }
-     else if ( ir == 2 ) {
-       if ( is == 1 ) nc = 72;
-       else nc = 54;
-     }
-     else if ( ir == 3 ) nc = 108;
-     else nc = 0; // for ir==0
-
-     return nc;   
-  } 
-*/
