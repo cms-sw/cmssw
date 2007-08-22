e57c39e3a49b108e6226902434647bcaad3bd006
Sridhara Dasu <sha1-9a11d2b71511b461682d02cb386f4219547e099a@cern.ch>
2007-08-22 18:08:00 +0000

    H/E cut fixed for low H

--- L1Trigger/RegionalCaloTrigger/interface/L1RCTLookupTables.h
+++ L1Trigger/RegionalCaloTrigger/interface/L1RCTLookupTables.h
@@ -1,9 +1,8 @@
 #ifndef L1RCTLookupTables_h
 #define L1RCTLookupTables_h
 
-class L1RCTParameters;
-class CaloTPGTranscoder;
-class L1CaloEtScale;
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "CalibFormats/CaloTPG/interface/CaloTPGTranscoder.h"
 
 class L1RCTLookupTables {
  
@@ -11,59 +10,65 @@ class L1RCTLookupTables {
 
   // constructor
 
-  L1RCTLookupTables() : rctParameters_(0), transcoder_(0), etScale_(0) {}
-  
-  // this needs to be refreshed every event -- constructor inits to zero
-  // to indicate that it cannot be used -- if this set function is
-  // called, lookup after that call will use it.
-  void setRCTParameters(const L1RCTParameters* rctParameters)
-    {
-      rctParameters_ = rctParameters;
-    }
-  // ditto for transcoder
-  void setTranscoder(const CaloTPGTranscoder* transcoder)
-    {
-      transcoder_ = transcoder;
-    }
-  // ditto for caloEtScale
-  void setL1CaloEtScale(const L1CaloEtScale* etScale)
-    {
-      etScale_ = etScale;
-    }
+  L1RCTLookupTables(const std::string& filename);
+  L1RCTLookupTables(const std::string& filename, const std::string& filename2, bool patternTest);
+  L1RCTLookupTables(const std::string& filename, edm::ESHandle<CaloTPGTranscoder> transcoder);
 
-  const L1RCTParameters* rctParameters() const {return rctParameters_;}
-  
-  unsigned int lookup(unsigned short ecalInput,
-		      unsigned short hcalInput,
-		      unsigned short fgbit,
-		      unsigned short crtNo,
-		      unsigned short crdNo,
-		      unsigned short twrNo
-		      ) const;
+  // function needs to output an unsigned long
 
-  unsigned int lookup(unsigned short hfInput, 
-		      unsigned short crtNo,
-		      unsigned short crdNo,
-		      unsigned short twrNo
-		      ) const;
+  unsigned long lookup(unsigned short ecal, 
+		       unsigned short hcal,
+		       unsigned short fgbit, 
+		       unsigned short crtNo, 
+		       unsigned short crdNo, 
+		       unsigned short twrNo
+		       );
+  unsigned short lookup(unsigned short hfenergy,
+			unsigned short crtNo, 
+			unsigned short crdNo, 
+			unsigned short twrNo
+			);
 
-  unsigned int emRank(unsigned short energy) const;
-  unsigned int eGammaETCode(float ecal, float hcal, int iAbsEta) const;
-  unsigned int jetMETETCode(float ecal, float hcal, int iAbsEta) const;
-  bool hOeFGVetoBit(float ecal, float hcal, bool fgbit) const;
-  bool activityBit(float ecal, float hcal) const;
-  
+  // Parameters of RCT LUTs and LSBs which are read from LUT file
+
+  static float eActivityCut() {return eActivityCut_;}
+  static float hActivityCut() {return hActivityCut_;}
+  static float hOeCut() {return hOeCut_;}
+  static float eGammaLSB() {return eGammaLSB_;}
+  static float jetMETLSB() {return jetMETLSB_;}
+
+  //  void setPatternTest(bool patternTestBit) {patternTest_ = patternTestBit;}
+  //  void setIgnoreFineGrain(bool ignoreFineGrainBit) {ignoreFG_ = ignoreFineGrainBit;}
+
  private:
 
-  // helper functions
+  L1RCTLookupTables();  // Do not implement so one cannot instantiate without input file
 
-  float convertEcal(unsigned short ecal, int iAbsEta) const;
-  float convertHcal(unsigned short hcal, int iAbsEta) const;
-  unsigned long convertToInteger(float et, float lsb, int precision) const;
+  float convertEcal(unsigned short ecal, int iAbsEta);
+  float convertHcal(unsigned short hcal, int iAbsEta);
+  bool calcActivityBit(float ecal, float hcal);
+  bool calcHEBit(float ecal,float hcal, bool fgbit);
+  unsigned long convertToInteger(float et, float lsb, int precision);
 
-  const L1RCTParameters* rctParameters_;
-  const CaloTPGTranscoder* transcoder_;
-  const L1CaloEtScale* etScale_;
+  bool useTranscoder_;
+  edm::ESHandle<CaloTPGTranscoder> transcoder_;
 
+  bool patternTest_;
+  bool ignoreFG_;
+
+  // We implement LUTs in code -- real lookup tables are large 2^17 (addresses) x 18 phi bins x 18 bits
+  // So we only read in conversion constants in float given by TPG developers, and implement LUTs on-the-fly
+  // This same code can be used to write out real LUTs and load in the hardware
+  // The read in conversion constants should come from the database, but for the moment we use a flat file
+  void loadLUTConstants(const std::string& filename);
+  void loadHcalLut(const std::string& filename);
+  static float eActivityCut_;
+  static float hActivityCut_;
+  static float eMaxForFGCut_;
+  static float hOeCut_;
+  static float eGammaLSB_;
+  static float jetMETLSB_;
+  static float eGammaSCF_[32];
+  static float hcalSCF_[32];
 };
 #endif
