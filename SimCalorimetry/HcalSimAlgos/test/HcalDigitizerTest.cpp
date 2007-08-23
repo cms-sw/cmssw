d4b3f0eb5c8fb3dddf2b55a57fb77e3e7fb8b3c7
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2007-08-23 15:26:37 +0000

    zdc included

--- SimCalorimetry/HcalSimAlgos/test/HcalDigitizerTest.cpp
+++ SimCalorimetry/HcalSimAlgos/test/HcalDigitizerTest.cpp
@@ -2,6 +2,7 @@
 #include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HODataFrame.h"
 #include "DataFormats/HcalDigi/interface/HFDataFrame.h"
+#include "DataFormats/HcalDigi/interface/ZDCDataFrame.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloHitResponse.h"
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloTDigitizer.h"
@@ -9,6 +10,7 @@
 #include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HcalShape.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HFShape.h"
+#include "SimCalorimetry/HcalSimAlgos/interface/ZDCShape.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HcalElectronicsSim.h"
 #include "CalibCalorimetry/HcalAlgos/interface/HcalDbHardcode.h"
 #include "CalibFormats/HcalObjects/interface/HcalDbService.h"
@@ -17,6 +19,7 @@
 #include "SimCalorimetry/HcalSimAlgos/interface/HBHEHitFilter.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HOHitFilter.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HFHitFilter.h"
+#include "SimCalorimetry/HcalSimAlgos/interface/ZDCHitFilter.h"
 #include "CondFormats/HcalObjects/interface/HcalPedestals.h"
 #include "CondFormats/HcalObjects/interface/HcalPedestalWidths.h"
 #include "CondFormats/HcalObjects/interface/HcalGains.h"
@@ -38,10 +41,14 @@ void testHitCorrection(HcalHitCorrection * correction, MixCollection<PCaloHit> &
   correction->fillChargeSums(hits);
   for(MixCollection<PCaloHit>::MixItr hitItr = hits.begin();
       hitItr != hits.end(); ++hitItr)
-  {
-    std::cout << "HIT charge " << correction->charge(*hitItr) << " delay " << correction->delay(*hitItr)
-              << " Timebin " << correction->timeBin(*hitItr) <<std::endl;
-  }
+    {
+      DetId detId((*hitItr).id());
+      if (detId.det()==DetId::Calo && detId.subdetId()==HcalZDCDetId::SubdetectorId){
+	std::cout<<"ZDC -- ";
+    } 
+      std::cout << "HIT charge " << correction->charge(*hitItr) << " delay " << correction->delay(*hitItr)
+		<< " Timebin " << correction->timeBin(*hitItr) <<std::endl;
+    }
 }
 
 
@@ -64,17 +71,25 @@ int main() {
   HcalDetId forwardDetId2(HcalForward, 30, 1, 2);
   PCaloHit forwardHit2(forwardDetId2.rawId(), 47.8, 0.);
 
-  vector<DetId> hcalDetIds, hoDetIds, hfDetIds;
+  HcalZDCDetId zdcDetId(HcalZDCDetId::Section(1),true,1);
+  PCaloHit zdcHit(zdcDetId.rawId(), 50.0, 0.123);
+ 
+  std::cout<<zdcDetId<<std::endl;
+  std::cout<<zdcHit<<std::endl;
+
+  vector<DetId> hcalDetIds, hoDetIds, hfDetIds, hzdcDetIds;
   hcalDetIds.push_back(barrelDetId);
   hcalDetIds.push_back(endcapDetId);
   hoDetIds.push_back(outerDetId);
   hfDetIds.push_back(forwardDetId1);
   hfDetIds.push_back(forwardDetId2);
+  hzdcDetIds.push_back(zdcDetId);
 
   vector<DetId> allDetIds;
   allDetIds.insert(allDetIds.end(), hcalDetIds.begin(), hcalDetIds.end());
   allDetIds.insert(allDetIds.end(), hoDetIds.begin(), hoDetIds.end());
   allDetIds.insert(allDetIds.end(), hfDetIds.begin(), hfDetIds.end());
+  allDetIds.insert(allDetIds.end(), hzdcDetIds.begin(), hzdcDetIds.end());
 
   vector<PCaloHit> hits;
   hits.push_back(barrelHit);
@@ -82,6 +97,7 @@ int main() {
   hits.push_back(outerHit);
   hits.push_back(forwardHit1);
   hits.push_back(forwardHit2);
+  hits.push_back(zdcHit);
 
   string hitsName = "HcalHits";
   vector<string> caloDets, trackingDets;
@@ -94,9 +110,12 @@ int main() {
   PCaloHit barrelPileup(barrelDetId.rawId(), 0.00855, 0.);
   // 10 GeV pileup hit
   PCaloHit forwardPileup(forwardDetId1.rawId(), 3.52, 0.);
+  PCaloHit zdcPileup(zdcDetId.rawId(), 3.52, 0.);
+
   vector<PCaloHit> pileups;
   pileups.push_back(barrelPileup);
   pileups.push_back(forwardPileup);
+  pileups.push_back(zdcPileup);
   ///TODO fix once the new crossingframe is released
   //crossingFrame.addPileupCaloHits(-3, hitsName, &pileups);
   // -or -
@@ -104,33 +123,37 @@ int main() {
   HcalSimParameterMap parameterMap;
   HcalShape hcalShape;
   HFShape hfShape;
+  ZDCShape zdcShape;
 
   CaloShapeIntegrator hcalShapeIntegrator(&hcalShape);
   CaloShapeIntegrator hfShapeIntegrator(&hfShape);
+  CaloShapeIntegrator zdcShapeIntegrator(&zdcShape);
 
   CaloHitResponse hbheResponse(&parameterMap, &hcalShapeIntegrator);
   CaloHitResponse hoResponse(&parameterMap, &hcalShapeIntegrator);
   CaloHitResponse hfResponse(&parameterMap, &hfShapeIntegrator);
+  CaloHitResponse zdcResponse(&parameterMap, &zdcShapeIntegrator);
 
   HcalHitCorrection hitCorrection(&parameterMap);
   hbheResponse.setHitCorrection(&hitCorrection);
   hoResponse.setHitCorrection(&hitCorrection);
+  zdcResponse.setHitCorrection(&hitCorrection);
 
   CLHEP::HepJamesRandom randomEngine;
   hbheResponse.setRandomEngine(randomEngine);
   hoResponse.setRandomEngine(randomEngine);
   hfResponse.setRandomEngine(randomEngine);
-  
-
-
+  zdcResponse.setRandomEngine(randomEngine);
 
   HBHEHitFilter hbheHitFilter;
   HOHitFilter hoHitFilter;
   HFHitFilter hfHitFilter;
+  ZDCHitFilter zdcHitFilter;
 
   hbheResponse.setHitFilter(&hbheHitFilter);
   hoResponse.setHitFilter(&hoHitFilter);
   hfResponse.setHitFilter(&hfHitFilter);
+  zdcResponse.setHitFilter(&zdcHitFilter);
 
   HcalPedestals pedestals;
   HcalPedestalWidths pedestalWidths;
@@ -143,14 +166,18 @@ int main() {
     gains.addValue(*detItr, HcalDbHardcode::makeGain(*detItr).getValues ());
     gainWidths.addValue(*detItr, HcalDbHardcode::makeGainWidth(*detItr).getValues ());
   }
-
+  
   pedestals.sort();
   pedestalWidths.sort();
   gains.sort();
   gainWidths.sort();
 
-std::cout << "TEST Pedestal " << pedestals.getValue(barrelDetId,  1) << std::endl;
-
+  std::cout << "TEST Pedestal " << pedestals.getValue(barrelDetId,  1) << std::endl;
+  std::cout << "ZDC pedestal " << pedestals.getValue(zdcDetId,  1) << std::endl;
+  std::cout << "ZDC pedestal width " << pedestalWidths.getWidth(zdcDetId,  1) << std::endl;
+  std::cout << "ZDC gain " << gains.getValue(zdcDetId,  1) << std::endl;
+  std::cout << "ZDC gain width " << gainWidths.getValue(zdcDetId,  1) << std::endl;
+  
   HcalDbService calibratorHandle;
   calibratorHandle.setData(&pedestals);
   calibratorHandle.setData(&pedestalWidths);
@@ -171,14 +198,17 @@ std::cout << "TEST Pedestal " << pedestals.getValue(barrelDetId,  1) << std::end
   CaloTDigitizer<HBHEDigitizerTraits> hbheDigitizer(&hbheResponse, &electronicsSim, addNoise);
   CaloTDigitizer<HODigitizerTraits> hoDigitizer(&hoResponse, &electronicsSim, addNoise);
   CaloTDigitizer<HFDigitizerTraits> hfDigitizer(&hfResponse, &electronicsSim, addNoise);
+  CaloTDigitizer<ZDCDigitizerTraits> zdcDigitizer(&zdcResponse, &electronicsSim, addNoise);
+
   hbheDigitizer.setDetIds(hcalDetIds);
   hfDigitizer.setDetIds(hfDetIds);
   hoDigitizer.setDetIds(hoDetIds);
+  zdcDigitizer.setDetIds(hzdcDetIds);
 
   auto_ptr<HBHEDigiCollection> hbheResult(new HBHEDigiCollection);
   auto_ptr<HODigiCollection> hoResult(new HODigiCollection);
   auto_ptr<HFDigiCollection> hfResult(new HFDigiCollection);
-
+  auto_ptr<ZDCDigiCollection> zdcResult(new ZDCDigiCollection);
 
   MixCollection<PCaloHit> hitCollection(&crossingFrame, hitsName);
 
@@ -187,6 +217,7 @@ std::cout << "TEST Pedestal " << pedestals.getValue(barrelDetId,  1) << std::end
   hbheDigitizer.run(hitCollection, *hbheResult);
   hoDigitizer.run(hitCollection, *hoResult);
   hfDigitizer.run(hitCollection, *hfResult);
+  zdcDigitizer.run(hitCollection, *zdcResult);
 
   // print out all the digis
   cout << "HBHE Frames" << endl;
@@ -195,7 +226,10 @@ std::cout << "TEST Pedestal " << pedestals.getValue(barrelDetId,  1) << std::end
   cout << "HF Frames" << endl;
   copy(hfResult->begin(), hfResult->end(), std::ostream_iterator<HFDataFrame>(std::cout, "\n"));
 
-return 0;
+  cout << "ZDC Frames" << endl;
+  copy(zdcResult->begin(), zdcResult->end(), std::ostream_iterator<ZDCDataFrame>(std::cout, "\n"));
+  
+  return 0;
 }
 
 
