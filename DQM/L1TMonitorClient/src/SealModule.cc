474d7f83f6067975e2ffdb54ee95a6d2f5da2ef6
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2007-08-24 16:31:41 +0000

    added dttpg

--- DQM/L1TMonitorClient/src/SealModule.cc
+++ DQM/L1TMonitorClient/src/SealModule.cc
@@ -1,7 +1,13 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+DEFINE_SEAL_MODULE();
+
 #include <DQM/L1TMonitorClient/interface/L1TCaloClient.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TCaloClient);
+
 #include <DQM/L1TMonitorClient/interface/L1TMuonClient.h>
-DEFINE_FWK_MODULE(L1TCaloClient);
 DEFINE_ANOTHER_FWK_MODULE(L1TMuonClient);
+
+#include <DQM/L1TMonitorClient/interface/L1TDTTPGClient.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TDTTPGClient);
