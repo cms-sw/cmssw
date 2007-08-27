597292d37cb1a66494aa0cf8a6e6fc892f76fa16
jaj99 <sha1-70a74b3bd545c31f46bda95f003fce0216dc67d4@cern.ch>
2007-08-27 12:02:26 +0000

    Initial import of DB API

---
+++ RecoLuminosity/LumiDB/src/linux/Linux/DBWriter.d
@@ -0,0 +1,12 @@
+/nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/src/linux/Linux/DBWriter.o /nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/src/linux/Linux/DBWriter.d :   \
+ /nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/src/common/DBWriter.cc \
+  /nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/include/DBWriter.hh \
+  /nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/OracleDBException.hh \
+  /nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/ICException.hh \
+  /nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/ICTypeDefs.hh
+:
+/nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/src/common/DBWriter.cc:
+/nfshome0/jaj99/TriDAS/RecoLuminosity/LumiDBLib/include/DBWriter.hh:
+/nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/OracleDBException.hh:
+/nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/ICException.hh:
+/nfshome0/jaj99/TriDAS/RecoLuminosity/HLXReadOut/CoreUtils/include/ICTypeDefs.hh:
