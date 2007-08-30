71a9a76786374db81290bc39c9d3245d92a21f67
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-08-30 16:14:02 +0000

    Fake values for pedestals

--- CalibMuon/CSCCalibration/src/CSCFakeDBPedestals.cc
+++ CalibMuon/CSCCalibration/src/CSCFakeDBPedestals.cc
@@ -7,58 +7,22 @@
 
 void CSCFakeDBPedestals::prefillDBPedestals()
 {
-  const CSCDetId& detId = CSCDetId();
   cndbpedestals = new CSCDBPedestals();
   
-  int max_istrip,max_ring,max_cham;
   seed = 10000;	
   srand(seed);
   meanped=600.0, meanrms=1.5, M=1000;
-  unsigned int channelCount = 1;
-
-  //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
+   
+  std::vector<CSCDBPedestals::Item> *itemvector;
+  itemvector = new std::vector<CSCDBPedestals::Item> ;
+  itemvector->resize(217728);
   
-  for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
-    for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-      max_ring=detId.maxRingId();
-      //station 4 ring 4 not there(36 chambers*2 missing)
-      //3 rings max this way of counting (ME1a & b)
-      if(istation==1)    max_ring=3;
-      if(istation==2)    max_ring=2;
-      if(istation==3)    max_ring=2;
-      if(istation==4)    max_ring=1;
-      
-      for(int iring=detId.minRingId(); iring<=max_ring; iring++){
-	max_istrip=80;
-	max_cham=detId.maxChamberId();  
-	if(istation==1 && iring==1)    max_cham=36;
-	if(istation==1 && iring==2)    max_cham=36;
-	if(istation==1 && iring==3)    max_cham=36;
-	if(istation==2 && iring==1)    max_cham=18;
-	if(istation==2 && iring==2)    max_cham=36;
-	if(istation==3 && iring==1)    max_cham=18;
-	if(istation==3 && iring==2)    max_cham=36;
-	if(istation==4 && iring==1)    max_cham=18;
-	
-	for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
-	  for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	    //station 1 ring 3 has 64 strips per layer instead of 80 
-	    if(istation==1 && iring==3)   max_istrip=64;
-	    
-	    std::vector<CSCDBPedestals::Item> itemvector;
-	    itemvector.resize(max_istrip);
-	    
-	    for(int istrip=0;istrip<max_istrip;istrip++){
-	      itemvector[istrip].ped=((double)rand()/((double)(RAND_MAX)+(double)(1)))*100+meanped;
-	      itemvector[istrip].rms=((double)rand()/((double)(RAND_MAX)+(double)(1)))+meanrms;
-	    }
-	    std::copy(itemvector.begin(), itemvector.end(), std::back_inserter(cndbpedestals->pedestals));
-            channelCount += max_istrip;
-	  }
-	}
-      }
-    }
+  for(int i=0; i<217728;i++){
+    itemvector->at(i).ped=((double)rand()/((double)(RAND_MAX)+(double)(1)))*100+meanped;
+    itemvector->at(i).rms=((double)rand()/((double)(RAND_MAX)+(double)(1)))+meanrms;
   }
+  std::copy(itemvector->begin(), itemvector->end(), std::back_inserter(cndbpedestals->pedestals));
+  delete itemvector;
 }  
 
 CSCFakeDBPedestals::CSCFakeDBPedestals(const edm::ParameterSet& iConfig)
