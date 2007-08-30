b1af28115394c0beeea67e996d5ad6c8b806933d
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2007-08-30 07:49:57 +0000

    *** empty log message ***

--- DataFormats/TauReco/src/classes.h
+++ DataFormats/TauReco/src/classes.h
@@ -1,5 +1,7 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/AssociationVector.h"
+#include "DataFormats/TauReco/interface/TauTagInfo.h"
+#include "DataFormats/TauReco/interface/PFTauTagInfo.h"
 #include "DataFormats/TauReco/interface/Tau.h"
 #include "DataFormats/TauReco/interface/TauDiscriminatorByIsolation.h"
 
@@ -8,6 +10,18 @@
 
 namespace {
   namespace {
+    std::vector<reco::TauTagInfo> tti_v;
+    edm::Wrapper<std::vector<reco::TauTagInfo> > tti_w;
+    edm::Ref<std::vector<reco::TauTagInfo> > tti_r;
+    edm::RefProd<std::vector<reco::TauTagInfo> > tti_rp;
+    edm::RefVector<std::vector<reco::TauTagInfo> > tti_rv;
+    
+    std::vector<reco::PFTauTagInfo> pftti_v;
+    edm::Wrapper<std::vector<reco::PFTauTagInfo> > pftti_w;
+    edm::Ref<std::vector<reco::PFTauTagInfo> > pftti_r;
+    edm::RefProd<std::vector<reco::PFTauTagInfo> > pftti_rp;
+    edm::RefVector<std::vector<reco::PFTauTagInfo> > pftti_rv;
+    
     std::vector<reco::Tau> v1;
     edm::Wrapper<std::vector<reco::Tau> > c1;
     edm::Ref<std::vector<reco::Tau> > r1;
