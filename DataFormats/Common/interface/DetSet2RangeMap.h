ed80fce36c545dbca0ac73bb1980b56e5fbfc0d5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-08-30 12:00:04 +0000

    transform

---
+++ DataFormats/Common/interface/DetSet2RangeMap.h
@@ -0,0 +1,44 @@
+#ifndef Common_DetSet2RangeMap_H
+#define Common_DetSet2RangeMap_H
+
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include "DataFormats/Common/interface/RangeMap.h"
+// #include "DataFormats/Common/interface/DetSetAlgorithm.h"
+
+#include "DataFormats/DetId/interface/DetId.h"
+
+#include <boost/ref.hpp>
+// #include <boost/bind.hpp>
+// #include <boost/function.hpp>
+#include <algorithm>
+
+//FIXME remove New when ready
+namespace edmNew {
+
+  namespace {
+    // copy from DS to RM
+    template<typename B>
+    struct ToRM {
+      ToRm(edm::RangeMap<DetId, edm::OwnVector<T> > & irm) : rm(irm){}
+      edm::RangeMap<DetId, edm::OwnVector<T> > & rm;
+      template<typename T>
+      void operator()(DetSet<T> const&  ds) {
+	// make it easy
+	// std::vector<T const *> v(ds.size());
+	//std::transform(ds.begin(),ds.end(),v.begin(),dstvdetails::Pointer());
+	rm.put(ds.id(), ds.begin(), ds.end());
+      }
+    };
+  }
+
+  // copy from DSTV to RangeMap
+  template<typename T, typename B=T>
+  copy(DetSetVector<T> const&  dstv,
+       edm::RangeMap<DetId, edm::OwnVector<B> > & rm) {
+    ToRM<B> torm(rm);
+    std::for_each(dstv.begin(), dstv.end(), boost::ref(torm));
+  }
+
+}
+ 
+#endif Common_DetSet2RangeMap_H
