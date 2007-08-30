727b36b7a42fa1fb97138e34096ac9d91ea3376d
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-08-30 14:41:31 +0000

    Fake values for gains

--- CalibMuon/CSCCalibration/src/CSCFakeDBGains.cc
+++ CalibMuon/CSCCalibration/src/CSCFakeDBGains.cc
@@ -6,67 +6,27 @@
 #include "CondFormats/DataRecord/interface/CSCDBGainsRcd.h"
 #include "CalibMuon/CSCCalibration/interface/CSCFakeDBGains.h"
 #include <DataFormats/MuonDetId/interface/CSCDetId.h>
-#include <DataFormats/MuonDetId/interface/CSCIndexer.h>
+//#include <DataFormats/MuonDetId/interface/CSCIndexer.h>
 
 void CSCFakeDBGains::prefillDBGains()
 {
   cndbgains = new CSCDBGains();
-  const CSCDetId& detId = CSCDetId();
-  CSCIndexer* theIndexer = new CSCIndexer;
-  
-  int max_istrip,id_layer,stripIndex,max_ring,max_cham;
-  unsigned layerId;
-
+ 
   seed = 10000;	
   srand(seed);
   mean=6.8, min=-10.0, minchi=1.0, M=1000;
 
-  unsigned int channelCount = 1;
-
- //endcap=1 to 2,station=1 to 4, ring=1 to 4,chamber=1 to 36,layer=1 to 6 
-  for(int iendcap=detId.minEndcapId(); iendcap<=detId.maxEndcapId(); iendcap++){
-    for(int istation=detId.minStationId() ; istation<=detId.maxStationId(); istation++){
-      max_ring=detId.maxRingId();
-
-      //station 4 ring 4 not there(36 chambers*2 missing)
-      //3 rings max this way of counting (ME1a & b)
-      if(istation==1)    max_ring=3;
-      if(istation==2)    max_ring=2;
-      if(istation==3)    max_ring=2;
-      if(istation==4)    max_ring=1;
-      
-      for(int iring=detId.minRingId(); iring<=max_ring; iring++){
-	max_istrip=80;
-	max_cham=detId.maxChamberId();  
-	if(istation==1 && iring==1)    max_cham=36;
-	if(istation==1 && iring==2)    max_cham=36;
-	if(istation==1 && iring==3)    max_cham=36;
-	if(istation==2 && iring==1)    max_cham=18;
-	if(istation==2 && iring==2)    max_cham=36;
-	if(istation==3 && iring==1)    max_cham=18;
-	if(istation==3 && iring==2)    max_cham=36;
-	if(istation==4 && iring==1)    max_cham=18;
-	
-	for(int ichamber=detId.minChamberId(); ichamber<=max_cham; ichamber++){
-	  for(int ilayer=detId.minLayerId(); ilayer<=detId.maxLayerId(); ilayer++){
-	    //station 1 ring 3 has 64 strips per layer instead of 80 
-	    if(istation==1 && iring==3)   max_istrip=64;
-	    
-	    std::vector<CSCDBGains::Item> itemvector;
-	    itemvector.resize(max_istrip);
-	    
-	    for(int istrip=0;istrip<max_istrip;istrip++){
-	      itemvector[istrip].gain_slope=((double)rand()/((double)(RAND_MAX)+(double)(1)))+mean;
-	      itemvector[istrip].gain_intercept=((double)rand()/((double)(RAND_MAX)+(double)(1)))+min;
-	      itemvector[istrip].gain_chi2=((double)rand()/((double)(RAND_MAX)+(double)(1)))+minchi;
-	    }
-	    std::copy(itemvector.begin(), itemvector.end(), std::back_inserter(cndbgains->gains));
-            channelCount += max_istrip;
-	  }
-	}
-      }
-    }
+  std::vector<CSCDBGains::Item> *itemvector;
+  itemvector = new std::vector<CSCDBGains::Item> ;
+  itemvector->resize(217728);
+  
+  for(int i=0; i<217728;i++){
+    itemvector->at(i).gain_slope=((double)rand()/((double)(RAND_MAX)+(double)(1)))+mean;
+    itemvector->at(i).gain_intercept=((double)rand()/((double)(RAND_MAX)+(double)(1)))+min;
+    itemvector->at(i).gain_chi2=((double)rand()/((double)(RAND_MAX)+(double)(1)))+minchi;
   }
+  std::copy(itemvector->begin(), itemvector->end(), std::back_inserter(cndbgains->gains));
+  delete itemvector;
 }  
 
 CSCFakeDBGains::CSCFakeDBGains(const edm::ParameterSet& iConfig)
