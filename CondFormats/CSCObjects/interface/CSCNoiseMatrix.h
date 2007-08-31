1ce792c2bea7399d0ff03157efafc538127eb13f
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-08-31 17:47:13 +0000

    Added a print() method

--- CondFormats/CSCObjects/interface/CSCNoiseMatrix.h
+++ CondFormats/CSCObjects/interface/CSCNoiseMatrix.h
@@ -12,9 +12,11 @@ class CSCNoiseMatrix{
   struct Item{
     float elem33,elem34,elem35,elem44,elem45,elem46,
           elem55,elem56,elem57,elem66,elem67,elem77;
+    std::string print() const;
   };
 
   const Item & item(int cscId, int strip) const;
+  std::string print() const;
   
   typedef std::map< int,std::vector<Item> > NoiseMatrixMap;
   NoiseMatrixMap matrix;
--- CondFormats/CSCObjects/src/CSCNoiseMatrix.cc
+++ CondFormats/CSCObjects/src/CSCNoiseMatrix.cc
@@ -1,5 +1,8 @@
 #include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
 #include "FWCore/Utilities/interface/Exception.h"
+
+#include <iostream>
 
 CSCNoiseMatrix::CSCNoiseMatrix(){}
 CSCNoiseMatrix::~CSCNoiseMatrix(){}
@@ -10,8 +13,33 @@ const CSCNoiseMatrix::Item & CSCNoiseMatrix::item(int cscId, int strip) const
   if(mapItr == matrix.end())
   {
     throw cms::Exception("CSCNoiseMatrix")
-      << "Cannot find CSC conditions for chamber " << cscId;
+      << "Cannot find CSC conditions for chamber " << CSCDetId(cscId);
   }
   return mapItr->second.at(strip-1);
+}
+
+
+std::string CSCNoiseMatrix::Item::print() const
+{
+  std::ostringstream os;
+  os << elem33 << " " << elem34 << " " << elem35 << " " << elem44 << " "
+     << elem45 << " " << elem46 << " " << elem55 << " " << elem56 << " "
+     << elem57 << " " << elem66 << " " << elem67 << " " << elem77 << "\n";
+  return os.str();
+}
+
+std::string CSCNoiseMatrix::print() const
+{
+  std::ostringstream os;
+  for(NoiseMatrixMap::const_iterator mapItr = matrix.begin(); mapItr != matrix.end(); ++mapItr)
+  {
+    os << mapItr->first<< " ";
+    for(std::vector<Item>::const_iterator itemItr = mapItr->second.begin(); 
+        itemItr != mapItr->second.end(); ++itemItr)
+    {
+      os << itemItr->print();
+    }
+  }
+  return os.str();
 }
 
