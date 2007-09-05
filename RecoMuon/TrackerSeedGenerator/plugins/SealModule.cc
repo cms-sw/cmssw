1706c0697410212c02512c7e5c7bd62a5ce4c395
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2007-09-05 13:53:08 +0000

    remove old / obsolete files

--- RecoMuon/TrackerSeedGenerator/plugins/SealModule.cc
+++ RecoMuon/TrackerSeedGenerator/plugins/SealModule.cc
@@ -2,12 +2,10 @@
 
 #include "RecoMuon/TrackerSeedGenerator/interface/TrackerSeedGeneratorFactory.h"
 
-#include "RecoMuon/TrackerSeedGenerator/interface/TrackerSeedGeneratorBC.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGFromOrderedHits.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGForRoadSearch.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGFromPropagation.h"
 
-DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TrackerSeedGeneratorBC, "TrackerSeedGeneratorBC");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGFromOrderedHits, "TSGFromOrderedHits");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGForRoadSearch, "TSGForRoadSearch");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGFromPropagation, "TSGFromPropagation");
