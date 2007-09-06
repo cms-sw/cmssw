3836be435b751a7536b6a230521092ed3fa8e13c
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-09-06 16:12:26 +0000

    added

---
+++ CondCore/DBCommon/interface/TagMetadata.h
@@ -0,0 +1,11 @@
+#ifndef DBCommon_TagMetadata_h
+#define DBCommon_TagMetadata_h
+#include <string>
+namespace cond{
+  struct TagMetadata{
+    std::string pfn;
+    std::string recordname;
+    std::string objectname;
+  };
+}
+#endif
