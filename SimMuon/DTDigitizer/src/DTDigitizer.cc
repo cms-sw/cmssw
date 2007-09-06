e6168d65e57b3701d84aa838bb4a4b349c2a8bd0
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2007-09-06 12:29:22 +0000

    inly 16 bit are indeed used

--- SimMuon/DTDigitizer/src/DTDigitizer.cc
+++ SimMuon/DTDigitizer/src/DTDigitizer.cc
@@ -1,7 +1,7 @@
 /** \file
  *
- *  $Date: 2007/09/05 10:23:16 $
- *  $Revision: 1.29 $
+ *  $Date: 2007/09/05 16:39:34 $
+ *  $Revision: 1.30 $
  *  \authors: G. Bevilacqua, N. Amapane, G. Cerminara, R. Bellan
  */
 
@@ -531,7 +531,7 @@ void DTDigitizer::storeDigis(DTWireId &wireId,
       // theAssociationMap.createLinks(localId, stat->det().simDet());
 
       //************ 7D ***************
-      if(digi.countsTDC() < pow(2.,18)){
+      if(digi.countsTDC() < pow(2.,16)){
 	DTLayerId layerID = wireId.layerId();  //taking the layer in which reside the wire
 	output.insertDigi(layerID, digi); // ordering Digis by layer
 	digiN++;
