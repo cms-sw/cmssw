4c594243fe62e2b90192a153a4e4d088dd3c67c9
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-09-07 18:12:11 +0000

    Put the doxygen header back in (why was this removed???), plus reverted some style changes.

--- TopQuarkAnalysis/TopObjectProducers/interface/TopMuonProducer.h
+++ TopQuarkAnalysis/TopObjectProducers/interface/TopMuonProducer.h
@@ -2,11 +2,23 @@
 // Author:  Jan Heyninck, Steven Lowette
 // Created: Tue Apr  10 12:01:49 CEST 2007
 //
-// $Id: TopMuonProducer.h,v 1.7 2007/08/27 11:04:31 tsirig Exp $
+// $Id: TopMuonProducer.h,v 1.8 2007/08/28 22:35:59 rwolf Exp $
 //
 
 #ifndef TopObjectProducers_TopMuonProducer_h
 #define TopObjectProducers_TopMuonProducer_h
+
+/**
+  \class    TopMuonProducer TopMuonProducer.h "TopQuarkAnalysis/TopObjectProducers/interface/TopMuonProducer.h"
+  \brief    Produces TopMuon's
+
+   TopMuonProducer produces TopMuon's starting from a MuonType collection,
+   with possible matching to generator level, adding of resolutions and
+   calculation of a lepton likelihood ratio
+
+  \author   Jan Heyninck, Steven Lowette
+  \version  $Id: TopMuonProducer.h,v 1.7 2007/08/27 11:04:31 tsirig Exp $
+*/
 
 #include <string>
 
@@ -25,35 +37,36 @@ class TopLeptonLRCalc;
 
 class TopMuonProducer : public edm::EDProducer {
   
- public:
+  public:
   
-  explicit TopMuonProducer(const edm::ParameterSet & iConfig);
-  ~TopMuonProducer();
-  virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
+    explicit TopMuonProducer(const edm::ParameterSet & iConfig);
+    ~TopMuonProducer();
+    virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
   
- private:
-  void matchTruth(const reco::CandidateCollection&, TopMuonTypeCollection&);  
-  reco::GenParticleCandidate findTruth(const reco::CandidateCollection&, const TopMuonType&);  
-  
- private:
-  
-  // configurables
-  edm::InputTag muonSrc_, genPartSrc_, tracksTag_;
-  bool useTrkIso_, useCalIso_; 
-  bool addResolutions_, useNNReso_;  
-  bool addLRValues_;
-  bool doGenMatch_;
-  std::string muonResoFile_;
-  std::string muonLRFile_;
-  double minRecoOnGenEt_, maxRecoOnGenEt_, maxDeltaR_;  
+  private:
 
-  // tools
-  TopObjectResolutionCalc *theResoCalc_;
-  TopLeptonTrackerIsolationPt  *trkIsolation_;
-  TopLeptonCaloIsolationEnergy *calIsolation_;
-  TopLeptonLRCalc *theLeptonLRCalc_;
-  std::vector<std::pair<const reco::Candidate *, TopMuonType*> > pairGenRecoMuonsVector_;
-  GreaterByPt<TopMuon> pTComparator_;
+    void matchTruth(const reco::CandidateCollection&, TopMuonTypeCollection&);  
+    reco::GenParticleCandidate findTruth(const reco::CandidateCollection&, const TopMuonType&);  
+  
+  private:
+  
+    // configurables
+    edm::InputTag muonSrc_, genPartSrc_, tracksTag_;
+    bool useTrkIso_, useCalIso_; 
+    bool addResolutions_, useNNReso_;  
+    bool addLRValues_;
+    bool doGenMatch_;
+    std::string muonResoFile_;
+    std::string muonLRFile_;
+    double minRecoOnGenEt_, maxRecoOnGenEt_, maxDeltaR_;  
+    // tools
+    TopObjectResolutionCalc *theResoCalc_;
+    TopLeptonTrackerIsolationPt  *trkIsolation_;
+    TopLeptonCaloIsolationEnergy *calIsolation_;
+    TopLeptonLRCalc *theLeptonLRCalc_;
+    std::vector<std::pair<const reco::Candidate *, TopMuonType*> > pairGenRecoMuonsVector_;
+    GreaterByPt<TopMuon> pTComparator_;
+
 };
 
 
