35ded1a0bd902f8c2e7938a33c4888ee76b9ce81
Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch>
2007-09-07 00:12:34 +0000

    QA campaign: fix to compensate includes cleanup in DataFormats/EgammaReco

--- EgammaAnalysis/CSA07Skims/src/EgammaProbeSelector.cc
+++ EgammaAnalysis/CSA07Skims/src/EgammaProbeSelector.cc
@@ -12,6 +12,7 @@
 #include "DataFormats/JetReco/interface/Jet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <iostream> 
 #include <TMath.h>
