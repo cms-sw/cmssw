c64b3e74e8f3722fae7977aa1ecfa123448329c8
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2007-09-07 14:41:33 +0000

    As also posted on the pixel software hypernews fora.
    There is now a working version of the CMSSW module IORawData/SiPixelInputSources that can run on files in castor! For anyone interested in this particular feature: please upgrade IORawData/SiPixelInputSources to tag V00-00-12. This version will hopefully also be queued for the CMSS 17X series (however, it can actually be checked out and used in earlier releases like the 16X tree).
    
    However, there are some small caveats (that considering the typical use I've observed up to now should not stop us much)
    
    1. The module can only read one (1) single file from castor, it is not possible to give a list of files
    2. For CMSSW-standards, the module is relatively slow. This has to do with the fact that each separate event has to be streamed, and the rfio protocol is very much tuned at reading large buffers. It would be better to read a large buffer (32K even more) and read then events from there, but unfortunately rfio does not support directly buffered-I/O as it is also used for random-reads (such as root). If someone wants to volunteer, it would be possible to write the buffering code... but it won't be me.
    3. Caching very large files takes long (25 Mbytes/sec is normal, so we can all calculate that our 'typical' S-curve files (>10 Gbyte) should take several minutes to cache). However, this is probably still preferable over copying everything somewhere else from castor. The grid/castor community is aware of this but I have not heard any suggestions on how to improve this (I'll keep you posted)

--- IORawData/SiPixelInputSources/interface/PixelSLinkDataInputSource.h
+++ IORawData/SiPixelInputSources/interface/PixelSLinkDataInputSource.h
@@ -1,14 +1,39 @@
 #ifndef IORawDataSiPixelInputSources_PixelSLinkDataInputSource_h
 #define IORawDataSiPixelInputSources_PixelSLinkDataInputSource_h
+// -*- C++ -*-
+//
+// Package:    SiPixelInputSources
+// Class:      PixelSLinkDataInputSource
+// 
+/**\class PixelSLinkDataInputSource PixelSLinkDataInputSource.h IORawData/SiPixelInputSources/interface/PixelSLinkDataInputSource.h
 
-// PixelSLinkDataInputSource.hh
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Freya Blekman
+//         Created:  Fri Sep  7 15:46:34 CEST 2007
+// $Id$
+//
+//
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Sources/interface/ExternalInputSource.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include "Utilities/StorageFactory/interface/StorageFactory.h"
+#include "Utilities/StorageFactory/interface/StorageAccount.h"
+#include "FWCore/PluginManager/interface/PluginManager.h"
+#include "FWCore/PluginManager/interface/standard.h"
+#include "SealIOTools/StorageStreamBuf.h"
+#include "SealBase/Storage.h"
+#include "SealBase/DebugAids.h"
+#include "SealBase/Signal.h"
 
 class PixelSLinkDataInputSource : public edm::ExternalInputSource {
 
@@ -23,11 +48,9 @@ public:
 
 
 private:
-  
-  bool open_file();
-  std::string m_file_name;
-  std::fstream m_file;
-  uint32_t m_fileindex;
+
   int m_fedid;
+  uint32_t m_fileindex;
+  std::auto_ptr<seal::Storage> storage;
 };
 #endif
--- IORawData/SiPixelInputSources/src/PixelSLinkDataInputSource.cc
+++ IORawData/SiPixelInputSources/src/PixelSLinkDataInputSource.cc
@@ -1,4 +1,21 @@
-// PixelSLinkDataInputSource.cc
+// -*- C++ -*-
+//
+// Package:    SiPixelInputSources
+// Class:      PixelSLinkDataInputSource
+// 
+/**\class PixelSLinkDataInputSource PixelSLinkDataInputSource.cc IORawData/SiPixelInputSources/src/PixelSLinkDataInputSource.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Freya Blekman
+//         Created:  Fri Sep  7 15:46:34 CEST 2007
+// $Id$
+//
+//
 
 #include <memory>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -8,57 +25,64 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "Utilities/StorageFactory/interface/StorageFactory.h"
 #include "Utilities/StorageFactory/interface/StorageAccount.h"
-#include <fstream>
+#include <iostream>
 
+using namespace seal;
+using namespace edm;
 PixelSLinkDataInputSource::PixelSLinkDataInputSource(const edm::ParameterSet& pset, 
 							      const edm::InputSourceDescription& desc) :
   ExternalInputSource(pset,desc),
-  m_fileindex(0),
-  m_fedid(pset.getUntrackedParameter<int>("fedid"))
+  m_fedid(pset.getUntrackedParameter<int>("fedid")),
+  m_fileindex(0)
 {
-  bool result = open_file();
   produces<FEDRawDataCollection>();
+
+  if (m_fileindex>=fileNames().size()) {
+    std::cout << "no more file to read " << std::endl;
+    return;// ???
+  }
+  std::string currentfilename = fileNames()[m_fileindex];
+  edm::LogInfo("") << "now examining file "<< currentfilename ;
+  m_fileindex++;
+  // reading both castor and other ('normal'/dcap) files.
+  IOOffset size = -1;
+  StorageFactory::get()->enableAccounting(true);
+    
+  bool exists = StorageFactory::get() -> check(currentfilename.c_str(), &size);
+  
+  std::cout << "file size " << size << std::endl;
+  
+  if(!exists){
+    edm::LogInfo("") << "file " << currentfilename << " cannot be found.";
+    return;
+  }
+  // now open the file stream:
+  storage.reset(StorageFactory::get()->open(currentfilename.c_str()));
+  // (throw if storage is 0)
+
 }
     
-bool PixelSLinkDataInputSource::open_file(){
-  if(m_fileindex==fileNames().size())
-    return false;
-  std::string fullname = fileNames()[m_fileindex];
-  m_fileindex++;
-  if(fullname.find('file:')){
-    m_file_name = fullname.substr(5);
-  }
-  else if(fullname.find('rfio:')){
-    edm::LogError("") << "Trying to open file " << fullname <<"... from Castor... (not implemented yet)";
-    return false;
-  }
-  else{
-    edm::LogError("") << "Trying to open file " << fullname <<", and do not recognize prefix (only \"rfio:\" and \"file:\" are accepted)";
-    return false;
-  }
-  m_file.open(m_file_name.c_str(),std::ios::in|std::ios::binary);
-  if (!m_file.good()) {
-    edm::LogError("") << "Error opening file " << fullname ;    
-    return false;
-  }
-  return true;
+PixelSLinkDataInputSource::~PixelSLinkDataInputSource() {
+
+
 }
-PixelSLinkDataInputSource::~PixelSLinkDataInputSource() {}
 
 bool PixelSLinkDataInputSource::produce(edm::Event& event) {
   
+  seal::Storage & m_file = *storage;
+
   // create product (raw data)
   std::auto_ptr<FEDRawDataCollection> buffers( new FEDRawDataCollection );
-  
+    
+
   unsigned long long data;
 
   std::vector<unsigned long long> buffer;
-
-  m_file.read((char*)&data,8);
+  IOSize n = m_file.read((char*)&data,8);
   
-  if (m_file.eof()) {
+  if (n==0) {
     edm::LogInfo("") << "End of input file" ;
-    return open_file();
+    return false;
   }
 
 
@@ -71,10 +95,10 @@ bool PixelSLinkDataInputSource::produce(edm::Event& event) {
 			  << std::hex<<data<<std::dec ;
     }
     count++;
-    m_file.read((char*)&data,8);
-    if (m_file.eof()) {
+    n=m_file.read((char*)&data,8);
+    if (n!=8) {
       edm::LogInfo("") << "End of input file" ;
-      return open_file();
+      return false;
     }
   }
 
@@ -94,6 +118,9 @@ bool PixelSLinkDataInputSource::produce(edm::Event& event) {
     m_file.read((char*)&data,8);
     buffer.push_back(data);
   }while((data >> 60) != 0xa);
+
+  std::cout << "read " <<  buffer.size() << " long words" << std::endl;
+
   std::auto_ptr<FEDRawData> rawData(new FEDRawData(8*buffer.size()));
   //  FEDRawData * rawData = new FEDRawData(8*buffer.size());
   unsigned char* dataptr=rawData->data();
@@ -106,7 +133,8 @@ bool PixelSLinkDataInputSource::produce(edm::Event& event) {
   fedRawData=*rawData;
 
   event.put(buffers);
-
+//  StorageAccount::summaryText ();
   return true;
 
 }
+
