49b2face64cfddc3f9f4545daddb5d1e0bade80d
Mika Huhtinen <sha1-e4ace60bb85ca6214788a8c677bc0fec21d76610@cern.ch>
2007-09-11 09:34:51 +0000

    Adding modification of SealModule to comply with new names of the Pixel MinBias codes.

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -1,11 +1,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTrigger/special/interface/HLTPixlMBFilt.h"
-#include "HLTrigger/special/interface/HLTPixlMBSelectFilter.h"
+#include "HLTrigger/special/interface/HLTPixlMBForAlignmentFilter.h"
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 #include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 
 DEFINE_FWK_MODULE(HLTPixlMBFilt);
-DEFINE_FWK_MODULE(HLTPixlMBSelectFilter);
+DEFINE_FWK_MODULE(HLTPixlMBForAlignmentFilter);
 DEFINE_FWK_MODULE(HLTPixelIsolTrackFilter);
 DEFINE_FWK_MODULE(HLTEcalIsolationFilter);
