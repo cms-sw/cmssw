284902caeebe8cf1c28feaf72d2172ced1e51fc2
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-09-12 17:18:01 +0000

    Make this a plugin

---
+++ SimMuon/MCTruth/plugins/Module.cc
@@ -0,0 +1,7 @@
+#include "SimMuon/MCTruth/src/CSCTruthTest.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_SEAL_MODULE ();
+DEFINE_FWK_MODULE(CSCTruthTest);
+
