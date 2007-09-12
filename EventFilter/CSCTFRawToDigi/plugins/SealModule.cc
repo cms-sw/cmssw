473f71462e6f5bf0485f8962a864dd7187b1127e
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2007-09-12 11:17:30 +0000

    Just forgot this file

--- EventFilter/CSCTFRawToDigi/plugins/SealModule.cc
+++ EventFilter/CSCTFRawToDigi/plugins/SealModule.cc
@@ -2,7 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include <EventFilter/CSCTFRawToDigi/interface/CSCTFUnpacker.h>
 #include <EventFilter/CSCTFRawToDigi/interface/CSCTFPacker.h>
+#include <EventFilter/CSCTFRawToDigi/interface/CSCTFAnalyzer.h>
 
 DEFINE_FWK_MODULE(CSCTFUnpacker);
 DEFINE_ANOTHER_FWK_MODULE(CSCTFPacker);
+DEFINE_ANOTHER_FWK_MODULE(CSCTFAnalyzer);
 
