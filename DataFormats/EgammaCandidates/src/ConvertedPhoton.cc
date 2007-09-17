9fa8badf530f0d88db1310d22565846402899321
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-09-17 17:41:44 +0000

    The ConvertedPhoton constructor takes in input the conversion reco vertex object instead of the simple point with the vertex coordinates. It also takes in input a vector containing the Basic Clusters matching the conversion tracks

--- DataFormats/EgammaCandidates/src/ConvertedPhoton.cc
+++ DataFormats/EgammaCandidates/src/ConvertedPhoton.cc
@@ -1,26 +1,23 @@
-
 #include "DataFormats/EgammaCandidates/interface/ConvertedPhoton.h"
 #include "DataFormats/TrackReco/interface/Track.h" 
 #include "DataFormats/EgammaReco/interface/SuperCluster.h" 
 
-
 using namespace reco;
-
-
-
 
 ConvertedPhoton::ConvertedPhoton(  const reco::SuperClusterRef sc, 
                                    const std::vector<reco::TrackRef> tr, 
                                    Charge q, const LorentzVector & p4,
 				   double r9, 
-				   const std::vector<math::XYZPoint> a, 
-                                   const Point & vtx = Point( 0, 0, 0 ), 
-                                   const Point & convVtx = Point( 0, 0, 0 ) ):  
+				   const std::vector<math::XYZPoint> trackPositionAtEcal, 
+				   const reco::Vertex  & convVtx,
+				   const std::vector<reco::BasicCluster> & matchingBC, 
+				   const Point & vtx = Point( 0, 0, 0 ) ):  
   RecoCandidate( q, p4, vtx, 22*q ),  
   superCluster_(sc), tracks_(tr),
   r9_(r9), 
-  theConversionVertex_(convVtx),  
-  thePositionAtEcal_(a) {
+  thePositionAtEcal_(trackPositionAtEcal), 
+  theConversionVertex_(convVtx), 
+  theMatchingBCs_(matchingBC)  {
   
   makePairInvariantMass();
   makePairCotThetaSeparation();
@@ -30,10 +27,13 @@ ConvertedPhoton::ConvertedPhoton(  const reco::SuperClusterRef sc,
   makePairPtOverEtSC();
   makeEoverP() ;
 
+
+ 
  }
 
 
 ConvertedPhoton::~ConvertedPhoton() { }
+
 
 ConvertedPhoton * ConvertedPhoton::clone() const { 
   return new ConvertedPhoton( * this ); 
@@ -43,19 +43,13 @@ reco::SuperClusterRef ConvertedPhoton::superCluster() const {
   return superCluster_;
 }
 
-
-
-
 std::vector<reco::TrackRef>  ConvertedPhoton::tracks() const { 
    return tracks_;
 }
 
-
 TrackRef ConvertedPhoton::track( size_t ind ) const {
   return tracks_[ind];
 }
-
-
 
 void ConvertedPhoton::setVertex(const Point & vertex) {
   math::XYZVector direction = this->superCluster()->position() - vertex;
@@ -65,8 +59,6 @@ void ConvertedPhoton::setVertex(const Point & vertex) {
   vertex_ = vertex;
 }
 
-
-
 bool ConvertedPhoton::isConverted() const {
   
   if ( this->nTracks() > 0) 
@@ -75,24 +67,19 @@ bool ConvertedPhoton::isConverted() const {
     return false;
 }
 
-
-
-
 void  ConvertedPhoton::makePairInvariantMass() {
   invMass_=-99.;
   const float mElec= 0.000511;
   if ( nTracks()==2 ) {
     double px= tracks()[0]->innerMomentum().x() + tracks()[1]->innerMomentum().x();
     double py= tracks()[0]->innerMomentum().y() + tracks()[1]->innerMomentum().y();
-    double pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();  
+    double pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();
     double mom1=tracks()[0]->innerMomentum().Mag2() ;
     double mom2=tracks()[1]->innerMomentum().Mag2() ;
     double e = sqrt( mom1+ mElec*mElec ) + sqrt( mom2 + mElec*mElec );
     invMass_= ( e*e - px*px -py*py - pz*pz);
   }
   
-
- 
 
 }
 
@@ -106,7 +93,6 @@ void  ConvertedPhoton::makePairCotThetaSeparation()  {
   }
 }
 
-
 void  ConvertedPhoton::makePairMomentum()  {
 
   double px=0.;
@@ -116,7 +102,7 @@ void  ConvertedPhoton::makePairMomentum()  {
   if ( nTracks()==2 ) {
     px= tracks()[0]->innerMomentum().x() + tracks()[1]->innerMomentum().x();
     py= tracks()[0]->innerMomentum().y() + tracks()[1]->innerMomentum().y();
-    pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();  
+    pz= tracks()[0]->innerMomentum().z() + tracks()[1]->innerMomentum().z();
 
   } else if (  nTracks()==1 ) {
     px= tracks()[0]->innerMomentum().x() ;
@@ -128,7 +114,6 @@ void  ConvertedPhoton::makePairMomentum()  {
   momTracks_= GlobalVector(px,py,pz);
 
 }
-
 
 void  ConvertedPhoton::makePairMomentumEta()  {
 
@@ -150,9 +135,6 @@ void  ConvertedPhoton::makePairMomentumPhi()  {
 
 }
 
-
-
-
 void  ConvertedPhoton::makePairPtOverEtSC()  {
 
   ptOverEtSC_=-99.;
@@ -165,9 +147,6 @@ void  ConvertedPhoton::makePairPtOverEtSC()  {
 
 }
 
-
-
-
 void  ConvertedPhoton::makeEoverP()  {
 
   ep_=-99.;
@@ -176,7 +155,6 @@ void  ConvertedPhoton::makeEoverP()  {
   
 }
 
-
 bool ConvertedPhoton::overlap( const Candidate & c ) const {
   const RecoCandidate * o = dynamic_cast<const RecoCandidate *>( & c );
   return ( o != 0 && 
@@ -184,4 +162,4 @@ bool ConvertedPhoton::overlap( const Candidate & c ) const {
 	   );
   return false;
 }
-   
+ 
