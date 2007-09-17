2401b2d961fe8d775331a2bcf73f035198427f1b
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-09-17 15:38:11 +0000

    new chamber mapping object

---
+++ CondFormats/CSCObjects/interface/CSCChamberMap.h
@@ -0,0 +1,41 @@
+#ifndef CSCChamberMap_h
+#define CSCChamberMap_h
+
+#include <vector>
+#include <map>
+
+class CSCChamberMap{
+ public:
+  CSCChamberMap();
+  ~CSCChamberMap();
+  
+  struct MapItem{
+    std::string chamberLabel;
+    int chamberId;
+    int endcap;
+    int station;
+    int ring;
+    int chamber;
+    int cscIndex;
+    int layerIndex;
+    int stripIndex;
+    int anodeIndex;
+    int strips;
+    int anodes;
+    std::string crateLabel;
+    int crateid;
+    int sector;
+    int trig_sector;
+    int dmb;
+    int cscid;
+    int ddu;
+    int ddu_input;
+    int slink;
+  };
+
+  typedef std::map< int,MapItem > ChamberMap;
+  ChamberMap ch_map;
+};
+
+#endif
+
