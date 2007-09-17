26e907ed47ee0a02a19fd210903b8b583889d7f1
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2007-09-17 16:52:27 +0000

    Records for pixel calibration configuration (to be queued for 17X)

---
+++ CondFormats/DataRecord/interface/SiPixelCalibConfigurationRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_SiPixelCalibConfigurationRcd_h
+#define DataRecord_SiPixelCalibConfigurationRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiPixelCalibConfigurationRcd
+// 
+/**\class SiPixelCalibConfigurationRcd SiPixelCalibConfigurationRcd.h CondFormats/DataRecord/interface/SiPixelCalibConfigurationRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Freya Blekman
+// Created:     Mon Sep 17 17:00:35 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiPixelCalibConfigurationRcd : public edm::eventsetup::EventSetupRecordImplementation<SiPixelCalibConfigurationRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/SiPixelCalibConfigurationRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiPixelCalibConfigurationRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      Freya Blekman
+// Created:     Mon Sep 17 17:00:35 CEST 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/SiPixelCalibConfigurationRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiPixelCalibConfigurationRcd);
