32a4db8a96be9901125de579124371cafe3ac707
Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch>
2007-09-18 22:30:47 +0000

    QA campaign: fix to compensate includes cleanup in DataFormats/TrackReco. TrackFwd.h included to declare TrackCollection. Otherwise this causes problems in RecoTracker/DebugTools.

--- TrackingTools/PatternTools/interface/TrajTrackAssociation.h
+++ TrackingTools/PatternTools/interface/TrajTrackAssociation.h
@@ -2,6 +2,7 @@
 #define TrackingTools_PatternTools_TrajTrackAssociation_h
 
 #include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
 
 typedef edm::AssociationMap<edm::OneToOne<std::vector<Trajectory>,
                                           reco::TrackCollection,unsigned short> > TrajTrackAssociationCollection;
