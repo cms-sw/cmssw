ac2e7d19e308b26eef1624395665b7bacfd549d3
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2007-09-18 20:16:53 +0000

    register the likelihood PDF's calibration data in the framework as EventSetup. First version.

---
+++ CondFormats/EgammaObjects/src/T_EventSetup_ElectronPdfCalibration.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/EgammaObjects/interface/ElectronLikelihoodCalibration.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(ElectronLikelihoodCalibration);
