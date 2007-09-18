a506eb0dc9313cd2ceb2e94fee81d22d78dabbcb
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2007-09-18 20:18:49 +0000

    dictionary to convert PDFs objects into DB objects

---
+++ CondFormats/EgammaObjects/src/classes.h
@@ -0,0 +1,15 @@
+#include "CondFormats/BTauObjects/interface/CalibratedHistogram.h"
+#include "CondFormats/EgammaObjects/interface/ElectronLikelihoodCategoryData.h"
+#include "CondFormats/EgammaObjects/interface/ElectronLikelihoodCalibration.h"
+
+namespace {
+
+  ElectronLikelihoodCategoryData a;
+
+  ElectronLikelihoodCalibration b;
+  ElectronLikelihoodCalibration::Entry c;
+  std::vector<ElectronLikelihoodCalibration::Entry> d;
+  std::vector<ElectronLikelihoodCalibration::Entry>::iterator d1;
+  std::vector<ElectronLikelihoodCalibration::Entry>::const_iterator d2;
+
+}
