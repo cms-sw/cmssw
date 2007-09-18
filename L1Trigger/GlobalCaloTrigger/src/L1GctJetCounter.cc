46050de26b88afd71affce8477002455166b094a
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2007-09-18 10:09:47 +0000

    New jet counter lut and setup definitions

--- L1Trigger/GlobalCaloTrigger/src/L1GctJetCounter.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctJetCounter.cc
@@ -3,9 +3,6 @@
  
 #include "FWCore/Utilities/interface/Exception.h"  
 
-#include <iostream>
-using namespace std;
-
 //DEFINE STATICS
 const unsigned int L1GctJetCounter::MAX_JETLEAF_CARDS = L1GctWheelJetFpga::MAX_LEAF_CARDS;
 const unsigned int L1GctJetCounter::MAX_JETS_PER_LEAF = L1GctWheelJetFpga::MAX_JETS_PER_LEAF;
@@ -13,7 +10,7 @@ const unsigned int L1GctJetCounter::MAX_JETS_TO_COUNT = L1GctJetCounter::MAX_JET
                                                         L1GctJetCounter::MAX_JETS_PER_LEAF;
 
 
-L1GctJetCounter::L1GctJetCounter(int id, vector<L1GctJetLeafCard*> leafCards,
+L1GctJetCounter::L1GctJetCounter(int id, std::vector<L1GctJetLeafCard*> leafCards,
                                L1GctJetCounterLut* jetCounterLut):
   m_id(id),
   m_jetLeafCards(leafCards),
@@ -58,30 +55,30 @@ L1GctJetCounter::~L1GctJetCounter()
   delete m_jetCounterLut;
 }
 
-ostream& operator << (ostream& os, const L1GctJetCounter& algo)
+std::ostream& operator << (std::ostream& os, const L1GctJetCounter& algo)
 {
   // Try to make this compact since it is probably
   // sitting inside a loop over 12 JetCounter instances
-  os << "===L1GctJetCounter===" << endl;
+  os << "===L1GctJetCounter===" << std::endl;
   if ((algo.m_id/100) == 0) { os << "Minus wheel, "; }
   else { os << "Plus wheel, "; }
-  os << "jet counter no. " << algo.m_id%100 <<  "; ID = " << algo.m_id << endl;
-//   os << "JetCounterLut* = " <<  algo.m_jetCounterLut << endl;
-  os << *algo.m_jetCounterLut << endl;
+  os << "jet counter no. " << algo.m_id%100 <<  "; ID = " << algo.m_id << std::endl;
+//   os << "JetCounterLut* = " <<  algo.m_jetCounterLut << std::endl;
+  os << *algo.m_jetCounterLut << std::endl;
 //   os << "No of Leaf cards " << algo.m_jetLeafCards.size();
-//   os << ". Total input jets " << algo.m_jets.size() << endl;
+//   os << ". Total input jets " << algo.m_jets.size() << std::endl;
 //   for (unsigned i=0; i<algo.m_jetLeafCards.size(); i++) {
 //     // One line of printing per leaf card
 //     os << "JetLeafCard* " << i << " = " << algo.m_jetLeafCards.at(i);
 //     os << " No of jets " << algo.m_jetLeafCards.at(i)->getOutputJetsA().size() ;
 //     os << " + " << algo.m_jetLeafCards.at(i)->getOutputJetsB().size();
-//     os << " + " << algo.m_jetLeafCards.at(i)->getOutputJetsC().size() << endl;
+//     os << " + " << algo.m_jetLeafCards.at(i)->getOutputJetsC().size() << std::endl;
 //   }
 //   for(unsigned i=0; i < algo.m_jets.size(); ++i)
 //     {
 //       os << algo.m_jets.at(i); 
 //     }
-  os << "Current value of counter " << algo.m_value << endl;
+  os << "Current value of counter " << algo.m_value << std::endl;
 
   return os;
 }
@@ -101,8 +98,8 @@ void L1GctJetCounter::fetchInput()
   for (unsigned i=0; i<m_jetLeafCards.size(); i++) {
     if (jetnum+MAX_JETS_PER_LEAF>m_jets.size()) {
       throw cms::Exception("L1GctProcessingError")
-	<< "L1GctJetCounter id= " << m_id << " trying to input too many jets for Leaf Card number " << i << endl
-	<< "current jetnum is " << jetnum << " about to add " << MAX_JETS_PER_LEAF << endl;
+	<< "L1GctJetCounter id= " << m_id << " trying to input too many jets for Leaf Card number " << i << std::endl
+	<< "current jetnum is " << jetnum << " about to add " << MAX_JETS_PER_LEAF << std::endl;
     }
     L1GctJetLeafCard* jlc = m_jetLeafCards.at(i);
     for (unsigned j=0; j<L1GctJetFinderBase::MAX_JETS_OUT; j++) {
@@ -114,19 +111,27 @@ void L1GctJetCounter::fetchInput()
 }
 
 /// set a new lut for this counter
-void L1GctJetCounter::setLut(L1GctJetCounterLut& lut)
+void L1GctJetCounter::setLut(const L1GctJetCounterLut& lut)
 {
   // Get rid of the old one
   delete m_jetCounterLut;
   m_jetCounterLut = new L1GctJetCounterLut(lut);
 }
 
-/// set a new lut for this counter by specifying the cuts
-void L1GctJetCounter::setLut(L1GctJetCounterLut::validCutType cutType, unsigned cutValue1, unsigned cutValue2)
+/// set a new lut for this counter by specifying the cuts - just one cut
+void L1GctJetCounter::setLut(const L1GctJetCounterSetup::cutDescription& cut)
 {
   // Get rid of the old one
   delete m_jetCounterLut;
-  m_jetCounterLut = new L1GctJetCounterLut(cutType, cutValue1, cutValue2);
+  m_jetCounterLut = new L1GctJetCounterLut(cut);
+}
+
+/// set a new lut for this counter by specifying the cuts - list of cuts
+void L1GctJetCounter::setLut(const L1GctJetCounterSetup::cutsListForJetCounter& cutList)
+{
+  // Get rid of the old one
+  delete m_jetCounterLut;
+  m_jetCounterLut = new L1GctJetCounterLut(cutList);
 }
 
 /// set the m_jets vector for test purposes
--- L1Trigger/GlobalCaloTrigger/src/L1GctJetCounterLut.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctJetCounterLut.cc
@@ -1,49 +1,37 @@
 #include "L1Trigger/GlobalCaloTrigger/interface/L1GctJetCounterLut.h"
 
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctJetCand.h"
+
 #include "FWCore/Utilities/interface/Exception.h"  
 
-#include <iostream>
-
-using namespace std;
-
 //DEFINE STATICS
-const unsigned int L1GctJetCounterLut::MAX_CUT_TYPE = nullCutType;
+const int L1GctJetCounterLut::NAddress=JET_COUNTER_LUT_ADD_BITS;
 
-L1GctJetCounterLut::L1GctJetCounterLut(vector<validCutType> cutType, vector<unsigned> cutValue1, vector<unsigned> cutValue2) :
-  m_nCuts(cutType.size()),
-  m_cutType(cutType),
-  m_cutValue1(cutValue1),
-  m_cutValue2(cutValue2)
+L1GctJetCounterLut::L1GctJetCounterLut(const L1GctJetCounterSetup::cutsListForJetCounter& cuts) :
+  L1GctLut<NAddress,1>(),
+  m_cutList(cuts)
 {
-  if (cutValue1.size() != m_nCuts) {
-    throw cms::Exception("L1GctSetupError")
-      << "Error in constructing L1GctJetCounterLut; size of first vector of cut values is " << cutValue1.size()
-      << " but size of cutType vector is " << cutType.size() << endl;
-  }
-  if (cutValue2.size() != m_nCuts) {
-    throw cms::Exception("L1GctSetupError")
-      << "Error in constructing L1GctJetCounterLut; size of second vector of cut values is " << cutValue2.size()
-      << " but size of cutType vector is " << cutType.size() << endl;
-  }
-  for (unsigned i=0; i<m_nCuts; i++) {
-    checkCut(cutType.at(i), cutValue1.at(i), cutValue2.at(i));
+  for (unsigned i=0; i<cuts.size(); i++) {
+    checkCut(cuts.at(i));
   }
 }
 
-L1GctJetCounterLut::L1GctJetCounterLut(validCutType cutType, unsigned cutValue1, unsigned cutValue2) :
-  m_nCuts(1),
-  m_cutType(1, cutType),
-  m_cutValue1(1, cutValue1),
-  m_cutValue2(1, cutValue2)
+L1GctJetCounterLut::L1GctJetCounterLut(const L1GctJetCounterSetup::cutDescription& cut) :
+  L1GctLut<NAddress,1>(),
+  m_cutList(1, cut)
 {
-  checkCut(cutType, cutValue1, cutValue2);
+  checkCut(cut);
+}
+
+L1GctJetCounterLut::L1GctJetCounterLut() :
+  L1GctLut<NAddress,1>(),
+  m_cutList(1)
+{
 }
 
 L1GctJetCounterLut::L1GctJetCounterLut(const L1GctJetCounterLut& lut) :
-  m_nCuts(lut.nCuts()),
-  m_cutType(lut.cutType()),
-  m_cutValue1(lut.cutValue1()),
-  m_cutValue2(lut.cutValue2())
+  L1GctLut<NAddress,1>(),
+  m_cutList(lut.cutList())
 {
 }
 
@@ -57,43 +45,45 @@ L1GctJetCounterLut L1GctJetCounterLut::operator= (const L1GctJetCounterLut& lut)
   return temp;
 }
 
-ostream& operator << (ostream& os, const L1GctJetCounterLut& lut)
+std::ostream& operator << (std::ostream& os, const L1GctJetCounterLut& lut)
 {
 //   os << "===L1GctJetCounterLut===" << endl;
-//   os << "Number of cuts for this Lut is " << lut.m_nCuts << endl;
-  for (unsigned i=0; i<lut.m_nCuts; i++) {
+//   os << "Number of cuts for this Lut is " << lut.nCuts() << endl;
+  for (unsigned i=0; i<lut.nCuts(); i++) {
     if (i==0) {
       os << "Jet passes ";
     } else {
       os << "\nand " ;
     }
-    switch (lut.m_cutType.at(i))
+    switch (lut.m_cutList.at(i).cutType)
       {
-      case L1GctJetCounterLut::minRank:
-	os << "if its rank is at least " << lut.m_cutValue1.at(i) ;
+      case L1GctJetCounterSetup::minRank:
+	os << "if its rank is at least " << lut.m_cutList.at(i).cutValue1 ;
 	break;
 
-      case L1GctJetCounterLut::maxRank:
-	os << "if its rank is at most " << lut.m_cutValue1.at(i) ;
+      case L1GctJetCounterSetup::maxRank:
+	os << "if its rank is at most " << lut.m_cutList.at(i).cutValue1 ;
 	break;
 
-      case L1GctJetCounterLut::centralEta:
-	os << "if it is in the central eta range with max (local) eta " << lut.m_cutValue1.at(i) ;
+      case L1GctJetCounterSetup::centralEta:
+	os << "if it is in the central eta range with max (local) eta " << lut.m_cutList.at(i).cutValue1 ;
 	break;
 
-      case L1GctJetCounterLut::forwardEta:
-	os << "if it is in the forward eta range with min (local) eta " << lut.m_cutValue1.at(i) ;
+      case L1GctJetCounterSetup::forwardEta:
+	os << "if it is in the forward eta range with min (local) eta " << lut.m_cutList.at(i).cutValue1 ;
 	break;
 
-      case L1GctJetCounterLut::phiWindow:
-	if (lut.m_cutValue2.at(i)>lut.m_cutValue1.at(i)) {
-	  os << "if its phi is between " << lut.m_cutValue1.at(i) << " and " << lut.m_cutValue2.at(i) ;
+      case L1GctJetCounterSetup::phiWindow:
+	if (lut.m_cutList.at(i).cutValue2>lut.m_cutList.at(i).cutValue1) {
+	  os << "if its phi is between " << lut.m_cutList.at(i).cutValue1
+                             << " and " << lut.m_cutList.at(i).cutValue2 ;
 	} else {
-	  os << "if its phi is larger than " << lut.m_cutValue1.at(i) << " or smaller than " << lut.m_cutValue2.at(i) ;
+	  os << "if its phi is larger than " << lut.m_cutList.at(i).cutValue1
+                     << " or smaller than " << lut.m_cutList.at(i).cutValue2 ;
 	}
 	break;
 
-      case L1GctJetCounterLut::nullCutType:
+      case L1GctJetCounterSetup::nullCutType:
 	os << "if Hell freezes over " ;
 	break;
 
@@ -101,107 +91,139 @@ ostream& operator << (ostream& os, const L1GctJetCounterLut& lut)
 	break;
       }
   }
+  os << "===Lookup table contents" << std::endl;
+  const L1GctLut<L1GctJetCounterLut::NAddress,1>* temp=&lut;
+  os << *temp;
   return os;
 }
+
+template class L1GctLut<L1GctJetCounterLut::NAddress,1>;
 
 bool L1GctJetCounterLut::passesCut(const L1GctJetCand jet) const
 {
   bool result = !jet.empty();
-  for (unsigned i=0; i<m_nCuts; i++) {
-    if (!result) { break; } // for efficiency
+  for (unsigned i=0; i<nCuts() && result; i++) {
     result &= jetPassesThisCut(jet, i);
   }
   return result;
 }
-  
-void L1GctJetCounterLut::checkCut(const validCutType cutType, const unsigned cutValue1, const unsigned cutValue2) const
+
+bool L1GctJetCounterLut::passesCut(const uint16_t lutAddress) const
 {
-  switch (cutType)
+  // Make a jet out of a 16-bit lut address. 
+  // Note this is intentionally not the same as the raw representation of the jet
+  // We ignore the sign of eta; and we need to encode the type of jet (forward, central or tau)
+  unsigned rank = (lutAddress & 0x3f);
+  unsigned eta  = (lutAddress >> 6) & 0x7;  
+  unsigned phi  = (lutAddress >> 9) & 0x1f;
+  bool isFor    = (lutAddress & 0x4000) != 0;
+  bool isTau    = (lutAddress & 0x8000) != 0;
+
+  // Check the address corresponds to a physical jet
+  if (phi<L1CaloRegionDetId::N_PHI && !(isFor && isTau)) {
+    // Make a jet and see if it passes the cuts
+    L1GctJetCand jc(rank, phi, eta, isTau, isFor);
+    return passesCut(jc);
+  } else {
+    return false;
+  }
+}
+  
+void L1GctJetCounterLut::checkCut(const L1GctJetCounterSetup::cutDescription cut) const
+{
+  switch (cut.cutType)
     {
-    case minRank:
-      if (cutValue1>=(1<<6)) {
+    case L1GctJetCounterSetup::minRank:
+      if (cut.cutValue1>=(1<<6)) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; rank value " << cutValue1
-	  << " should be less than 64 " << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; rank value " << cut.cutValue1
+	  << " should be less than 64 " << std::endl;
       }
       return;
 
-    case maxRank:
-      if (cutValue1>=(1<<6)) {
+    case L1GctJetCounterSetup::maxRank:
+      if (cut.cutValue1>=(1<<6)) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; rank value " << cutValue1
-	  << " should be less than 64 " << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; rank value " << cut.cutValue1
+	  << " should be less than 64 " << std::endl;
       }
       return;
 
-    case centralEta:
-      if (cutValue1>=L1CaloRegionDetId::N_ETA/2) {
+    case L1GctJetCounterSetup::centralEta:
+      if (cut.cutValue1>=L1CaloRegionDetId::N_ETA/2) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; eta value " << cutValue1
-	  << " should be less than " << L1CaloRegionDetId::N_ETA/2 << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; eta value " << cut.cutValue1
+	  << " should be less than " << L1CaloRegionDetId::N_ETA/2 << std::endl;
       }
       return;
 
-    case forwardEta:
-      if (cutValue1>=L1CaloRegionDetId::N_ETA/2) {
+    case L1GctJetCounterSetup::forwardEta:
+      if (cut.cutValue1>=L1CaloRegionDetId::N_ETA/2) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; eta value " << cutValue1
-	  << " should be less than " << L1CaloRegionDetId::N_ETA/2 << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; eta value " << cut.cutValue1
+	  << " should be less than " << L1CaloRegionDetId::N_ETA/2 << std::endl;
       }
       return;
 
-    case phiWindow:
-      if (cutValue1>=L1CaloRegionDetId::N_PHI) {
+    case L1GctJetCounterSetup::phiWindow:
+      if (cut.cutValue1>=L1CaloRegionDetId::N_PHI) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; phi value1 " << cutValue1
-	  << " should be less than " << L1CaloRegionDetId::N_PHI << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; phi value1 " << cut.cutValue1
+	  << " should be less than " << L1CaloRegionDetId::N_PHI << std::endl;
       }
-      if (cutValue2>=L1CaloRegionDetId::N_PHI) {
+      if (cut.cutValue2>=L1CaloRegionDetId::N_PHI) {
 	throw cms::Exception("L1GctSetupError")
-	  << "Error in constructing L1GctJetCounterLut; phi value2 " << cutValue2
-	  << " should be less than " << L1CaloRegionDetId::N_PHI << endl;
+	  << "Invalid cut supplied to L1GctJetCounterLut; phi value2 " << cut.cutValue2
+	  << " should be less than " << L1CaloRegionDetId::N_PHI << std::endl;
       }
       return;
 
-    case nullCutType:
+    case L1GctJetCounterSetup::nullCutType:
       return;
 
     default:
     throw cms::Exception("L1GctSetupError")
-      << "Error in constructing L1GctJetCounterLut; unrecognised cut type " << cutType << endl;
+      << "Invalid cut supplied to L1GctJetCounterLut; unrecognised cut type "
+      << cut.cutType << std::endl;
     }
 
 }
   
 bool L1GctJetCounterLut::jetPassesThisCut(const L1GctJetCand jet, const unsigned i) const
 {
-  switch (m_cutType.at(i))
+  switch (m_cutList.at(i).cutType)
     {
-    case minRank:
-      return (jet.rank()>=m_cutValue1.at(i));
+    case L1GctJetCounterSetup::minRank:
+      return (jet.rank()>=m_cutList.at(i).cutValue1);
 
-    case maxRank:
-      return (jet.rank()<=m_cutValue1.at(i));
+    case L1GctJetCounterSetup::maxRank:
+      return (jet.rank()<=m_cutList.at(i).cutValue1);
 
-    case centralEta:
-      return (rctEta(jet)<=m_cutValue1.at(i));
+    case L1GctJetCounterSetup::centralEta:
+      return (rctEta(jet)<=m_cutList.at(i).cutValue1);
 
-    case forwardEta:
-      return (rctEta(jet)>=m_cutValue1.at(i));
+    case L1GctJetCounterSetup::forwardEta:
+      return (rctEta(jet)>=m_cutList.at(i).cutValue1);
 
-    case phiWindow:
-      return (m_cutValue2.at(i)>m_cutValue1.at(i) ?
-	      ((globalPhi(jet) >= m_cutValue1.at(i)) && (globalPhi(jet) <= m_cutValue2.at(i))) :
-	      ((globalPhi(jet) >= m_cutValue1.at(i)) || (globalPhi(jet) <= m_cutValue2.at(i))));
+    case L1GctJetCounterSetup::phiWindow:
+      return (m_cutList.at(i).cutValue2>m_cutList.at(i).cutValue1 ?
+	      ((globalPhi(jet) >= m_cutList.at(i).cutValue1) && (globalPhi(jet) <= m_cutList.at(i).cutValue2)) :
+	      ((globalPhi(jet) >= m_cutList.at(i).cutValue1) || (globalPhi(jet) <= m_cutList.at(i).cutValue2)));
 
-    case nullCutType:
+    case L1GctJetCounterSetup::nullCutType:
       return false;
 
     default:
     throw cms::Exception("L1GctProcessingError")
-      << "Error in L1GctJetCounterLut; encountered unrecognised cut type " << m_cutType.at(i) << endl;
+      << "Error in L1GctJetCounterLut; encountered unrecognised cut type "
+      << m_cutList.at(i).cutType << std::endl;
     }
 
+}
+
+uint16_t L1GctJetCounterLut::value (const uint16_t lutAddress) const
+{
+  return (passesCut(lutAddress) ? 0 : 1) ;
 }
 
 unsigned L1GctJetCounterLut::rctEta(const L1GctJetCand jet) const
@@ -213,3 +235,4 @@ unsigned L1GctJetCounterLut::globalPhi(const L1GctJetCand jet) const
 {
   return jet.phiIndex();
 }
+
