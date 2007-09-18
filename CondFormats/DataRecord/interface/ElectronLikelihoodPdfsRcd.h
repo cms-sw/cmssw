425ae393526fea9064ae8a8da5cb875cbd972e92
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2007-09-18 20:08:37 +0000

    record for electron likelihood PDFs added

---
+++ CondFormats/DataRecord/interface/ElectronLikelihoodPdfsRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_ElectronLikelihoodPdfsRcd_h
+#define DataRecord_ElectronLikelihoodPdfsRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     ElectronLikelihoodPdfsRcd
+// 
+/**\class ElectronLikelihoodPdfsRcd ElectronLikelihoodPdfsRcd.h CondFormats/DataRecord/interface/ElectronLikelihoodPdfsRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Wed Sep  5 16:36:25 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class ElectronLikelihoodPdfsRcd : public edm::eventsetup::EventSetupRecordImplementation<ElectronLikelihoodPdfsRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/ElectronLikelihoodPdfsRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     ElectronLikelihoodPdfsRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Wed Sep  5 16:36:25 CEST 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/ElectronLikelihoodPdfsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(ElectronLikelihoodPdfsRcd);
