9d3679e5403227b3853e9bc874a7fce21973f7e0
Kevin.Klapoetke Klapoetke <sha1-00ae6b1fd57e2673af62ea5af6540d64f8b247c9@cern.ch>
2007-09-19 15:22:21 +0000

    SealModule for the plugins folder, commented out sections are in progress

---
+++ RecoEgamma/EgammaHFProducers/plugins/SealModule.cc
@@ -0,0 +1,12 @@
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+
+#include "HFClusterProducer.h"
+//#include "HFEgammaProducer.h"//coming soon
+
+
+DEFINE_SEAL_MODULE();//may need removal
+
+DEFINE_ANOTHER_FWK_MODULE(HFClusterProducer);
+//DEFINE_ANOTHER_FWK_MODULE(HFEgammaProducer);
