4af67f6b34e05988ab1dcf6c19d06674389c1986
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-09-19 10:51:45 +0000

    new crate mapping object

---
+++ CondFormats/CSCObjects/interface/CSCCrateMap.h
@@ -0,0 +1,41 @@
+#ifndef CSCCrateMap_h
+#define CSCCrateMap_h
+
+#include <vector>
+#include <map>
+#include <string>
+
+class CSCCrateMap{
+ public:
+  CSCCrateMap();
+  ~CSCCrateMap();
+  
+  struct MapItem{
+    std::string chamberLabel;
+    int chamberId;
+    int endcap;
+    int station;
+    int ring;
+    int chamber;
+    int cscIndex;
+    int layerIndex;
+    int stripIndex;
+    int anodeIndex;
+    int strips;
+    int anodes;
+    std::string crateLabel;
+    int crateid;
+    int sector;
+    int trig_sector;
+    int dmb;
+    int cscid;
+    int ddu;
+    int ddu_input;
+    int slink;
+  };
+
+  typedef std::map< int,MapItem > CSCMap;
+  CSCMap crate_map;
+};
+
+#endif
