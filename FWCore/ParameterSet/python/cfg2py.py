66a556cefdae453972fd8b88afeb05632d956d30
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-09-19 04:53:25 +0000

    Use the new dumpCfg method

--- FWCore/ParameterSet/python/cfg2py.py
+++ FWCore/ParameterSet/python/cfg2py.py
@@ -7,7 +7,8 @@ print "import FWCore.ParameterSet.Config as cms"
 fileInPath = argv[1]
 
 if fileInPath.endswith('cfg'):
-    print cmsParse.parseCfgFile(fileInPath).dumpPython()
+    #print cmsParse.parseCfgFile(fileInPath).dumpPython()
+    print cmsParse.dumpCfg(fileInPath)
 else:
     #print cmsParse.parseCffFile(fileInPath).dumpPython()
     print  cmsParse.dump(fileInPath)
