ba500bfd66ad5cd4813048180b917f684d9f4dbe
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-09-21 10:57:35 +0000

    patch to use new CondFormats

--- RecoBTag/XMLCalibration/src/CalibratedHistogramXML.cc
+++ RecoBTag/XMLCalibration/src/CalibratedHistogramXML.cc
@@ -5,9 +5,8 @@
 using namespace std;
 void  CalibratedHistogramXML::read (XERCES_CPP_NAMESPACE::DOMElement * dom)
   {
-    m_normalization=0; 
-    m_binValues.clear();
-    m_binULimits.clear();
+    binValues.clear();
+    binULimits.clear();
     int size=  CalibrationXML::readAttribute<int>(dom,"size");
     
     DOMNode * n1 = dom->getFirstChild();
@@ -18,32 +17,32 @@ void  CalibratedHistogramXML::read (XERCES_CPP_NAMESPACE::DOMElement * dom)
           if (n1)
 	  {
 	      DOMElement * binElement = (DOMElement *) n1;
-              m_binValues.push_back(CalibrationXML::readAttribute<double>(binElement,"value"));
+              binValues.push_back(CalibrationXML::readAttribute<double>(binElement,"value"));
 	
-	      if(bin>0) m_normalization+=m_binValues[bin];
-              m_binULimits.push_back(CalibrationXML::readAttribute<double>(binElement,"uLimit"));      
+              binULimits.push_back(CalibrationXML::readAttribute<double>(binElement,"uLimit"));      
 	       n1 = n1->getNextSibling();
 	  }             
     }
     if(bin>0)
-      m_binValues.push_back(CalibrationXML::readAttribute<int>(dom,"overFlowValue"));
+      binValues.push_back(CalibrationXML::readAttribute<int>(dom,"overFlowValue"));
 
-
+    limits = Range(binULimits.front(), binULimits.back());
+    totalValid = false;
   }
   
 void  CalibratedHistogramXML::write (XERCES_CPP_NAMESPACE::DOMElement * dom) const
   {
 
-    int size=m_binULimits.size();
+    int size=binULimits.size();
     CalibrationXML::writeAttribute(dom,"size",size);
     DOMElement * binElement;
     for(int bin=0; bin < size; bin ++)
     {
        binElement = CalibrationXML::addChild(dom,"Bin");
-       CalibrationXML::writeAttribute(binElement,"value",m_binValues[bin]);
-       CalibrationXML::writeAttribute(binElement,"uLimit",m_binULimits[bin]);
+       CalibrationXML::writeAttribute(binElement,"value",binValues[bin]);
+       CalibrationXML::writeAttribute(binElement,"uLimit",binULimits[bin]);
     }
-    CalibrationXML::writeAttribute(dom,"overFlowValue",m_binValues[size]);
+    CalibrationXML::writeAttribute(dom,"overFlowValue",binValues[size]);
 
   }
 
