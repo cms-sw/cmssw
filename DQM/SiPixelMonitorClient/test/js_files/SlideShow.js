fc18ee07403aac8397efb9fcc65a29cc77f152bd
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2007-09-21 16:15:26 +0000

    added some more features to SlideShow

--- DQM/SiPixelMonitorClient/test/js_files/SlideShow.js
+++ DQM/SiPixelMonitorClient/test/js_files/SlideShow.js
@@ -9,7 +9,9 @@ var BarrelListFiles  = new Array();
 var BarrelListTitles = new Array();
 var EndcapListFiles  = new Array();
 var EndcapListTitles = new Array();
-var slideShowSpeed   = 10000;  // miliseconds
+var ErrorListFiles   = new Array();
+var ErrorListTitles  = new Array();
+var slideShowSpeed   = 3000;  // miliseconds
 var index            = 0;
 var nSlides          = 0;
 var MAX_SLIDES       = 20;
@@ -17,9 +19,20 @@ var timerID;
 //____________________________________________________________________
 //____________________________________________________________________
 
+SlideShow.changeUpdateInterval = function(){
+ try{
+
+  var theRefresh  = document.getElementById("update_slideshow");
+  slideShowSpeed  = theRefresh.options[theRefresh.selectedIndex].value;
+  }catch(e){
+   alert(e.message);
+  }
+}
+//____________________________________________________________________
 SlideShow.StartSlideShow = function() {
  try{
 
+   SlideShow.changeUpdateInterval();
    SlideShow.setSlide(index);
    index = (index+1) % nSlides;
    timerID = setTimeout('SlideShow.StartSlideShow()', slideShowSpeed);
@@ -114,22 +127,29 @@ SlideShow.setSlide = function(index) {
 }
 
 //____________________________________________________________________
-SlideShow.BarrelSummaryOverView = function()
+SlideShow.SummaryOverView = function(type)
 {
   try{
-//    var queryString;
-//    var url = WebLib.getApplicationURL2();
-//    url += "/Request?";
-//    queryString = "RequestID=PlotHistogramFromLayout";
-//    url += queryString;
-//    WebLib.makeRequest(url, WebLib.dummy);
 
-    $('imageCanvas').imageList     = BarrelListFiles;
-    $('imageCanvas').titlesList    = BarrelListTitles;
+    if(type=="Pixel"){
+      $('imageCanvas').imageList  = slideListFiles;
+      $('imageCanvas').titlesList = slideListTitles;
+    }
+    if(type=="Barrel"){
+      $('imageCanvas').imageList  = BarrelListFiles;
+      $('imageCanvas').titlesList = BarrelListTitles;
+    }
+    if(type=="Endcap"){
+      $('imageCanvas').imageList  = EndcapListFiles;
+      $('imageCanvas').titlesList = EndcapListTitles;
+    }
+    if(type=="Error"){
+      $('imageCanvas').imageList  = ErrorListFiles;
+      $('imageCanvas').titlesList = ErrorListTitles;
+    }
     $('imageCanvas').current_start = 0;
   
-    IMGC.IMAGES_PER_ROW      = 3;
-    IMGC.IMAGES_PER_COL      = 3;
+    SlideShow.computeImagesPerCanvas($('imageCanvas').imageList);
     IMGC.IMAGES_PER_PAGE     = IMGC.IMAGES_PER_ROW * IMGC.IMAGES_PER_COL;
     IMGC.ASPECT_RATIO        = 1.5 ;
     IMGC.THUMB_MICROFICATION = 4 ;
@@ -139,39 +159,6 @@ SlideShow.BarrelSummaryOverView = function()
   
     IMGC.computeCanvasSize() ;
  
-    SlideShow.StopSlideShow();
-
-  }catch(e){
-   alert(e.message);
-  }
-}
-
-//____________________________________________________________________
-SlideShow.EndcapSummaryOverView = function()
-{
-  try{
-//    var queryString;
-//    var url = WebLib.getApplicationURL2();
-//    url += "/Request?";
-//    queryString = "RequestID=PlotHistogramFromLayout";
-//    url += queryString;
-//    WebLib.makeRequest(url, WebLib.dummy);
-
-    $('imageCanvas').imageList     = EndcapListFiles;
-    $('imageCanvas').titlesList    = EndcapListTitles;
-    $('imageCanvas').current_start = 0;
-  
-    IMGC.IMAGES_PER_ROW      = 3;
-    IMGC.IMAGES_PER_COL      = 3;
-    IMGC.IMAGES_PER_PAGE     = IMGC.IMAGES_PER_ROW * IMGC.IMAGES_PER_COL;
-    IMGC.ASPECT_RATIO        = 1.5 ;
-    IMGC.THUMB_MICROFICATION = 4 ;
-    IMGC.BASE_IMAGE_WIDTH    = IMGC.DEF_IMAGE_WIDTH ;
-    IMGC.BASE_IMAGE_HEIGHT   = parseInt(IMGC.BASE_IMAGE_WIDTH / IMGC.ASPECT_RATIO);
-    IMGC.PATH_TO_PICTURES    = "images/" ;
-  
-    IMGC.computeCanvasSize() ;
-   
     SlideShow.StopSlideShow();
 
   }catch(e){
@@ -183,19 +170,12 @@ SlideShow.EndcapSummaryOverView = function()
 SlideShow.ErrorOverview = function()
 {
   try{
-//    var queryString;
-//    var url = WebLib.getApplicationURL2();
-//    url += "/Request?";
-//    queryString = "RequestID=PlotErrorOverviewHistogram";
-//    url += queryString;
-//    WebLib.makeRequest(url, WebLib.dummy);
 
     $('imageCanvas').imageList     = ErrorListFiles;
     $('imageCanvas').titlesList    = ErrorListTitles;
     $('imageCanvas').current_start = 0;
   
-    IMGC.IMAGES_PER_ROW      = 2;
-    IMGC.IMAGES_PER_COL      = 1;
+    SlideShow.computeImagesPerCanvas(BarrelListFiles);
     IMGC.IMAGES_PER_PAGE     = IMGC.IMAGES_PER_ROW * IMGC.IMAGES_PER_COL;
     IMGC.ASPECT_RATIO        = 1.5 ;
     IMGC.THUMB_MICROFICATION = 4 ;
@@ -209,5 +189,34 @@ SlideShow.ErrorOverview = function()
 
   }catch(e){
    alert(e.message);
+  }
+}
+//____________________________________________________________________
+SlideShow.computeImagesPerCanvas = function(list)
+{
+  try{
+    var numberOfImages = list.length;
+    if(numberOfImages == 1){
+      IMGC.IMAGES_PER_ROW = 1;
+      IMGC.IMAGES_PER_COL = 1;
+    }
+    if(numberOfImages == 2){
+      IMGC.IMAGES_PER_ROW = 1;
+      IMGC.IMAGES_PER_COL = 2;
+    }
+    if(numberOfImages == 3 || numberOfImages == 4){
+      IMGC.IMAGES_PER_ROW = 2;
+      IMGC.IMAGES_PER_COL = 2;
+    }
+    if(numberOfImages == 5 || numberOfImages == 6){
+      IMGC.IMAGES_PER_ROW = 2;
+      IMGC.IMAGES_PER_COL = 3;
+    }
+    if(numberOfImages >= 7){
+      IMGC.IMAGES_PER_ROW = 3;
+      IMGC.IMAGES_PER_COL = 3;
+    }
+  }catch(e){
+    alert(e.message);
   }
 }
