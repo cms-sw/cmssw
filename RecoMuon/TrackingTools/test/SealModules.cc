e42c0cc31f360f9904f4c0bb25c3292315d06f0b
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-09-21 11:26:00 +0000

    moving MuonErrorMatrix to src, and analyzer to test

---
+++ RecoMuon/TrackingTools/test/SealModules.cc
@@ -0,0 +1,12 @@
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+
+#include "RecoMuon/TrackingTools/test/MuonErrorMatrixAnalyzer.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MuonErrorMatrixAnalyzer);
