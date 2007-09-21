fe2730394dc98a934e2c50b4deeb3ec6a5d38265
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-09-21 03:51:43 +0000

    new syntax

--- FWCore/ParameterSet/python/cfg2py.py
+++ FWCore/ParameterSet/python/cfg2py.py
@@ -2,14 +2,10 @@ import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.parseConfig as cmsParse
 from sys import argv
 
-print "import FWCore.ParameterSet.Config as cms"
-
 fileInPath = argv[1]
 
 if fileInPath.endswith('cfg'):
-    #print cmsParse.parseCfgFile(fileInPath).dumpPython()
-    print cmsParse.dumpCfg(fileInPath)
+    cmsParse.dumpCfg(fileInPath)
 else:
-    #print cmsParse.parseCffFile(fileInPath).dumpPython()
-    print  cmsParse.dump(fileInPath)
+    print cmsParse.dumpCff(fileInPath)
 
