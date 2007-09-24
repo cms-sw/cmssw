b7553e1cc7f5aa214ed4d290a142a8368439f77c
tumanov <sha1-47187222be523f6c8f2edbe22a417c456a25dfe9@cern.ch>
2007-09-24 13:55:01 +0000

    digiToPattern module

--- EventFilter/CSCRawToDigi/plugins/module.cc
+++ EventFilter/CSCRawToDigi/plugins/module.cc
@@ -3,8 +3,9 @@
 #include <EventFilter/CSCRawToDigi/interface/CSCDCCUnpacker.h>
 #include <EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h>
 #include <EventFilter/CSCRawToDigi/src/CSCDigiToRawModule.h>
-
+#include <EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h>
 
 DEFINE_FWK_MODULE(CSCDCCUnpacker);
 DEFINE_ANOTHER_FWK_MODULE(DigiAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiToRawModule);
+DEFINE_ANOTHER_FWK_MODULE(CSCDigiToPattern);
