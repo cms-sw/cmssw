f837beed0385b0cc716d29e802f65830b5cf9aea
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-09-26 17:54:44 +0000

    definition with common MapItem

--- CondFormats/CSCObjects/interface/CSCChamberIndex.h
+++ CondFormats/CSCObjects/interface/CSCChamberIndex.h
@@ -1,39 +1,15 @@
 #ifndef CSCChamberIndex_h
 #define CSCChamberIndex_h
 
+#include "OnlineDB/CSCCondDB/interface/CSCMapItem.h"
 #include <vector>
-#include <string>
 
 class CSCChamberIndex{
  public:
   CSCChamberIndex();
   ~CSCChamberIndex();
-  
-  struct MapItem{
-    std::string chamberLabel;
-    int chamberId;
-    int endcap;
-    int station;
-    int ring;
-    int chamber;
-    int cscIndex;
-    int layerIndex;
-    int stripIndex;
-    int anodeIndex;
-    int strips;
-    int anodes;
-    std::string crateLabel;
-    int crateid;
-    int sector;
-    int trig_sector;
-    int dmb;
-    int cscid;
-    int ddu;
-    int ddu_input;
-    int slink;
-  };
 
-  typedef std::vector< MapItem > CSCVector;
+  typedef std::vector< CSCMapItem::MapItem > CSCVector;
   CSCVector ch_index;
 };
 
--- CondFormats/CSCObjects/interface/CSCChamberMap.h
+++ CondFormats/CSCObjects/interface/CSCChamberMap.h
@@ -1,40 +1,15 @@
 #ifndef CSCChamberMap_h
 #define CSCChamberMap_h
 
-#include <vector>
+#include "OnlineDB/CSCCondDB/interface/CSCMapItem.h"
 #include <map>
-#include <string>
 
 class CSCChamberMap{
  public:
   CSCChamberMap();
   ~CSCChamberMap();
-  
-  struct MapItem{
-    std::string chamberLabel;
-    int chamberId;
-    int endcap;
-    int station;
-    int ring;
-    int chamber;
-    int cscIndex;
-    int layerIndex;
-    int stripIndex;
-    int anodeIndex;
-    int strips;
-    int anodes;
-    std::string crateLabel;
-    int crateid;
-    int sector;
-    int trig_sector;
-    int dmb;
-    int cscid;
-    int ddu;
-    int ddu_input;
-    int slink;
-  };
 
-  typedef std::map< int,MapItem > CSCMap;
+  typedef std::map< int,CSCMapItem::MapItem > CSCMap;
   CSCMap ch_map;
 };
 
