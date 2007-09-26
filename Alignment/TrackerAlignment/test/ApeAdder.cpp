3491ba32ffe7e479f361ca1746a8f7e80c068ffc
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2007-09-26 12:24:03 +0000

    A new simple module to prepare a custom APE for all GeomDets.

---
+++ Alignment/TrackerAlignment/test/ApeAdder.cpp
@@ -0,0 +1,129 @@
+// -*- C++ -*-
+//
+//
+// Description: Module to add a custom APE to the alignment object
+//
+//
+// Original Author:  Frederic Ronga
+//         Created:  December 5, 2006
+//
+
+
+// system include files
+
+// user include files
+//#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+// Conditions database
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
+
+#include "Alignment/TrackerAlignment/interface/AlignableTracker.h"
+#include "CondFormats/Alignment/interface/AlignTransformError.h"
+
+#include "CondFormats/Alignment/interface/Alignments.h"
+#include "CondFormats/Alignment/interface/AlignTransform.h"
+#include "CondFormats/DataRecord/interface/TrackerAlignmentRcd.h"
+#include "CondFormats/Alignment/interface/AlignmentErrors.h"
+#include "CondFormats/Alignment/interface/AlignTransformError.h"
+#include "CondFormats/DataRecord/interface/TrackerAlignmentErrorRcd.h"
+
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+
+
+// Class declaration
+class ApeAdder : public edm::EDAnalyzer {
+public:
+  explicit ApeAdder( const edm::ParameterSet& );
+  ~ApeAdder() {};
+  
+  
+  virtual void analyze( const edm::Event&, const edm::EventSetup& );
+
+private:
+    // methods
+    void addApe( std::vector<Alignable*> alignables );
+    
+private:
+    // members
+  std::string theErrorRecordName;
+    std::vector<double> theApe; // Amount of APE to add (from config.)
+  
+};
+
+ApeAdder::ApeAdder( const edm::ParameterSet& iConfig )  :
+  theErrorRecordName( "TrackerAlignmentErrorRcd" )
+{ 
+
+  // The APE to set to all GeomDets
+  theApe = iConfig.getUntrackedParameter< std::vector<double> >("apeVector");
+  
+}
+
+//__________________________________________________________________________________________________
+void ApeAdder::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup )
+
+{
+
+  // Get geometry from ES
+  edm::ESHandle<GeometricDet> gD;
+  iSetup.get<IdealGeometryRecord>().get( gD );
+  edm::ESHandle<TrackerGeometry> trackerGeometry;
+  iSetup.get<TrackerDigiGeometryRecord>().get( trackerGeometry );
+    
+  // Create the alignable hierarchy
+  AlignableTracker* theAlignableTracker = new AlignableTracker( &(*gD), &(*trackerGeometry) );
+  
+  // Now loop on alignable dets and add alignment error
+  if ( theAlignableTracker->barrelGeomDets().size() ) 
+    this->addApe(theAlignableTracker->barrelGeomDets());
+  if ( theAlignableTracker->pixelHalfBarrelGeomDets().size() ) 
+    this->addApe(theAlignableTracker->pixelHalfBarrelGeomDets());
+  if ( theAlignableTracker->endcapGeomDets().size() ) 
+    this->addApe(theAlignableTracker->endcapGeomDets());
+  if ( theAlignableTracker->TIDGeomDets().size() ) 
+    this->addApe(theAlignableTracker->TIDGeomDets());
+  if ( theAlignableTracker->pixelEndcapGeomDets().size() ) 
+    this->addApe(theAlignableTracker->pixelEndcapGeomDets());
+  
+  // Store to DB
+  AlignmentErrors* alignmentErrors = theAlignableTracker->alignmentErrors();
+
+  // Call service
+  edm::Service<cond::service::PoolDBOutputService> poolDbService;
+  if( !poolDbService.isAvailable() ) // Die if not available
+    throw cms::Exception("NotAvailable") << "PoolDBOutputService not available";
+
+  // Save to DB
+  if ( poolDbService->isNewTagRequest(theErrorRecordName) )
+    poolDbService->createNewIOV<AlignmentErrors>( alignmentErrors,
+                                                  poolDbService->endOfTime(), 
+                                                  theErrorRecordName );
+  else
+    poolDbService->appendSinceTime<AlignmentErrors>( alignmentErrors,
+                                                     poolDbService->currentTime(), 
+                                                     theErrorRecordName );
+  
+}
+
+void ApeAdder::addApe( std::vector<Alignable*> alignables )
+{
+  
+  AlignmentPositionError ape( theApe[0], theApe[1], theApe[2] );
+  for ( std::vector<Alignable*>::iterator iDet = alignables.begin();
+		iDet != alignables.end(); iDet++ )
+    (*iDet)->setAlignmentPositionError( ape );
+    
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ApeAdder);
