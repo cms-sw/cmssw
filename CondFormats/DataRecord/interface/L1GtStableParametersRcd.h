ce9dfd7a7f317197c098d8e59ac2e369af55e222
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-09-27 10:30:01 +0000

    first submission

---
+++ CondFormats/DataRecord/interface/L1GtStableParametersRcd.h
@@ -0,0 +1,36 @@
+#ifndef DataRecord_L1GtStableParametersRcd_h
+#define DataRecord_L1GtStableParametersRcd_h
+
+/**
+ * \class L1GtStableParametersRcd
+ * 
+ * 
+ * Description: record for L1 GT stable parameters.  
+ *
+ * Implementation:
+ *    <TODO: enter implementation details>
+ *   
+ * \author: Vasile Mihai Ghete - HEPHY Vienna
+ * 
+ * $Date$
+ * $Revision$
+ *
+ */
+
+// system include files
+
+// user include files
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+// forward declarations
+
+// class declaration
+class L1GtStableParametersRcd
+            : public edm::eventsetup::EventSetupRecordImplementation<L1GtStableParametersRcd>
+{
+
+    // empty
+
+};
+
+#endif
---
+++ CondFormats/DataRecord/src/L1GtTriggerMenuRcd.cc
@@ -0,0 +1,27 @@
+/**
+ * \class L1GtTriggerMenuRcd
+ * 
+ * 
+ * Description: record for L1 trigger menu.  
+ *
+ * Implementation:
+ *    <TODO: enter implementation details>
+ *   
+ * \author: Vasile Mihai Ghete - HEPHY Vienna
+ * 
+ * $Date$
+ * $Revision$
+ *
+ */
+
+// this class header
+#include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
+
+// system include files
+
+// user include files
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+//
+
+EVENTSETUP_RECORD_REG(L1GtTriggerMenuRcd);
