5a70014e2f485fa4fa358ae557a917399eb2a98e
Filip Moortgat <sha1-236ec69c42ab223e0df5c43733dcda6ad72fa5d0@cern.ch>
2007-09-27 16:24:43 +0000

    add instance names

--- PhysicsTools/HepMCCandAlgos/plugins/GenEventRunInfoProducer.cc
+++ PhysicsTools/HepMCCandAlgos/plugins/GenEventRunInfoProducer.cc
@@ -2,7 +2,9 @@
  *
  * \author Liz,Oliver,Filip
  *
- * Save the cross section info from the RUN section into the EVENT
+ * Save the cross section and filter efficiency info from the RUN section into the EVENT
+ * Workaround for CSA07.
+ *
  */
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
@@ -45,13 +47,19 @@ void GenEventRunInfoProducer::produce( Event& evt, const EventSetup& es ) {
   evt.getRun().getByLabel( src_, gi);
       
   auto_ptr<double> cs1( new double(1) );
+  auto_ptr<double> cs2( new double(1) );
+  auto_ptr<double> eff( new double(1) );
    
-  double cross_section1 = gi->cross_section(); // automatically calculated at the end of the run
-  double cross_section2 = gi->external_cross_section(); // is the one written in the cfg file -- units is pb!!
+  double cross_section1 = gi->cross_section(); // automatically calculated at the end of the Pythia/Herwig run 
+  double cross_section2 = gi->external_cross_section(); // is the precalculated one written in the cfg file -- units is pb!!
   double filter_eff = gi->filter_efficiency();
   
   (*cs1) = cross_section1;
-  evt.put( cs1 );
+  (*cs2) = cross_section2;
+  (*eff) = filter_eff;
+  evt.put( cs1, "AutoCrossSection" );
+  evt.put( cs2, "PreCalculatedCrossSection" );
+  evt.put( eff, "FilterEfficiency" );
   
 }
 
