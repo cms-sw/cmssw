e57608fc6f15d19cde461745d2301e46d61ca58a
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2007-09-27 12:16:17 +0000

    Ecal DB optimization campaign

---
+++ CondFormats/EcalObjects/src/T_EventSetup_EcalCondObjectContainer_Float.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondFormats/EcalObjects/interface/EcalCondObjectContainer.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(EcalCondObjectContainer<float>);
