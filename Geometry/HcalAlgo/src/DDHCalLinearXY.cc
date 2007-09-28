aa258fed2541260bb495d269371b07818f3c4703
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-09-28 17:43:47 +0000

    Provide standalone geomtery for creating shower library

---
+++ Geometry/HcalAlgo/src/DDHCalLinearXY.cc
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////////////////////
+// File: DDHCalLinearXY.cc
+// Description: Position nxXny copies at given intervals along x and y axis
+///////////////////////////////////////////////////////////////////////////////
+
+#include <cmath>
+#include <algorithm>
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DetectorDescription/Core/interface/DDPosPart.h"
+#include "DetectorDescription/Core/interface/DDCurrentNamespace.h"
+#include "DetectorDescription/Core/interface/DDSplit.h"
+#include "Geometry/HcalAlgo/interface/DDHCalLinearXY.h"
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "CLHEP/Units/SystemOfUnits.h"
+
+
+DDHCalLinearXY::DDHCalLinearXY() {
+  LogDebug("HCalGeom") <<"DDHCalLinearXY info: Creating an instance";
+}
+
+DDHCalLinearXY::~DDHCalLinearXY() {}
+
+void DDHCalLinearXY::initialize(const DDNumericArguments & nArgs,
+				   const DDVectorArguments & vArgs,
+				   const DDMapArguments & ,
+				   const DDStringArguments & sArgs,
+				   const DDStringVectorArguments &) {
+
+  numberX   = int(nArgs["NumberX"]);
+  deltaX    = nArgs["DeltaX"];
+  numberY   = int(nArgs["NumberY"]);
+  deltaY    = nArgs["DeltaY"];
+  centre    = vArgs["Center"];
+  
+  idNameSpace = DDCurrentNamespace::ns();
+  childName   = sArgs["ChildName"]; 
+  DDName parentName = parent().name();
+  LogDebug("HCalGeom") << "DDHCalLinearXY debug: Parent " << parentName
+		       << "\tChild " << childName << " NameSpace " 
+		       << idNameSpace << "\tNumber along X/Y " << numberX
+		       << "/" << numberY << "\tDelta along X/Y " << deltaX
+		       << "/" << deltaY << "\tCentre " << centre[0] << ", " 
+		       << centre[1] << ", "	<< centre[2];
+}
+
+void DDHCalLinearXY::execute() {
+
+  DDName mother = parent().name();
+  DDName child(DDSplit(childName).first, DDSplit(childName).second);
+  DDRotation rot;
+  double xoff = centre[0] - (numberX-1)*deltaX/2.;
+  double yoff = centre[1] - (numberY-1)*deltaY/2.;
+  int    copy = 0;
+
+  for (int i=0; i<numberX; i++) {
+    for (int j=0; j<numberY; j++) {
+	
+      DDTranslation tran(xoff+i*deltaX,yoff+j*deltaY,centre[2]);
+      copy++;
+      DDpos (child, mother, copy, tran, rot);
+      LogDebug("HCalGeom") << "DDHCalLinearXY test: " << child 
+			   << " number " << copy << " positioned in "
+			   << mother << " at " << tran << " with " << rot;
+    }
+  }
+}
--- Geometry/HcalAlgo/src/module.cc
+++ Geometry/HcalAlgo/src/module.cc
@@ -1,13 +1,14 @@
 //<<<<<< INCLUDES                                                       >>>>>>
 
+#include "Geometry/HcalAlgo/interface/DDHCalAngular.h"
 #include "Geometry/HcalAlgo/interface/DDHCalBarrelAlgo.h"
 #include "Geometry/HcalAlgo/interface/DDHCalEndcapAlgo.h"
 #include "Geometry/HcalAlgo/interface/DDHCalForwardAlgo.h"
-#include "Geometry/HcalAlgo/interface/DDHCalTestBeamAlgo.h"
-#include "Geometry/HcalAlgo/interface/DDHCalXtalAlgo.h"
+#include "Geometry/HcalAlgo/interface/DDHCalLinearXY.h"
 #include "Geometry/HcalAlgo/interface/DDHCalTBCableAlgo.h"
 #include "Geometry/HcalAlgo/interface/DDHCalTBZposAlgo.h"
-#include "Geometry/HcalAlgo/interface/DDHCalAngular.h"
+#include "Geometry/HcalAlgo/interface/DDHCalTestBeamAlgo.h"
+#include "Geometry/HcalAlgo/interface/DDHCalXtalAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 
@@ -15,6 +16,7 @@ DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalAngular,      "hcal:DDHCalAngular")
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalBarrelAlgo,   "hcal:DDHCalBarrelAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalEndcapAlgo,   "hcal:DDHCalEndcapAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalForwardAlgo,  "hcal:DDHCalForwardAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalLinearXY,     "hcal:DDHCalLinearXY");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalTBCableAlgo,  "hcal:DDHCalTBCableAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalTBZposAlgo,   "hcal:DDHCalTBZposAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHCalTestBeamAlgo, "hcal:DDHCalTestBeamAlgo");
