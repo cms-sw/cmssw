f2a81d9cbeb2ebd80f996877549f98a34d081c79
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-09-28 13:51:27 +0000

    first commit

---
+++ CondCore/L1TPlugins/src/SealModule.cc
@@ -0,0 +1,29 @@
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondTools/L1Trigger/src/WriterProxy.h"
+
+#include "CondFormats/DataRecord/interface/L1CSCTPParametersRcd.h"
+#include "CondFormats/L1TObjects/interface/L1CSCTPParameters.h"
+#include "CondFormats/DataRecord/interface/L1TriggerKeyRcd.h"
+#include "CondFormats/L1TObjects/interface/L1TriggerKey.h"
+
+#include "CondFormats/L1TObjects/interface/L1GctJetEtCalibrationFunction.h"
+#include "CondFormats/DataRecord/interface/L1GctJetCalibFunRcd.h"
+
+#include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
+#include "CondFormats/DataRecord/interface/L1JetEtScaleRcd.h"
+#include "CondFormats/DataRecord/interface/L1EmEtScaleRcd.h"
+
+REGISTER_PLUGIN(L1TriggerKeyRcd, L1TriggerKey);
+REGISTER_L1_WRITER(L1TriggerKeyRcd, L1TriggerKey);
+
+REGISTER_PLUGIN(L1CSCTPParametersRcd, L1CSCTPParameters);
+REGISTER_L1_WRITER(L1CSCTPParametersRcd, L1CSCTPParameters);
+
+REGISTER_PLUGIN(L1GctJetCalibFunRcd, L1GctJetEtCalibrationFunction);
+REGISTER_L1_WRITER(L1GctJetCalibFunRcd, L1GctJetEtCalibrationFunction);
+
+REGISTER_PLUGIN(L1JetEtScaleRcd, L1CaloEtScale);
+REGISTER_L1_WRITER(L1JetEtScaleRcd, L1CaloEtScale);
+
+REGISTER_PLUGIN(L1EmEtScaleRcd, L1CaloEtScale);
+REGISTER_L1_WRITER(L1EmEtScaleRcd, L1CaloEtScale);
