84e1768f341912378dc1ee38f8f3df2035e69bd6
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-09-28 20:50:53 +0000

    Measure the total strip amplitude for 3 strips around a comparator digi

--- Validation/MuonCSCDigis/src/CSCComparatorDigiValidation.cc
+++ Validation/MuonCSCDigis/src/CSCComparatorDigiValidation.cc
@@ -2,21 +2,31 @@
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
 
 
-CSCComparatorDigiValidation::CSCComparatorDigiValidation(DaqMonitorBEInterface* dbe, const edm::InputTag & inputTag)
+CSCComparatorDigiValidation::CSCComparatorDigiValidation(DaqMonitorBEInterface* dbe, 
+    const edm::InputTag & inputTag, const edm::InputTag & stripDigiInputTag)
 : CSCBaseValidation(dbe, inputTag),
+  theStripDigiInputTag(stripDigiInputTag),
   theTimeBinPlots(),
   theNDigisPerLayerPlots(),
+  theStripDigiPlots(),
+  the3StripPlots(),
   theNDigisPerEventPlot( dbe_->book1D("CSCComparatorDigisPerEvent", "CSC Comparator Digis per event", 100, 0, 100) )
 {
   for(int i = 0; i < 10; ++i)
   {
-    char title1[200], title2[200];
+    char title1[200], title2[200], title3[200], title4[200];
     sprintf(title1, "CSCComparatorDigiTimeType%d", i+1);
     sprintf(title2, "CSCComparatorDigisPerLayerType%d", i+1);
+    sprintf(title3, "CSCComparatorStripAmplitudeType%d", i+1);
+    sprintf(title4, "CSCComparator3StripAmplitudeType%d", i+1);
     theTimeBinPlots[i] = dbe_->book1D(title1, title1, 9, 0, 8);
     theNDigisPerLayerPlots[i] = dbe_->book1D(title2, title2, 100, 0, 20);
+    theStripDigiPlots[i] = dbe_->book1D(title3, title3, 100, 0, 1000);
+    the3StripPlots[i] = dbe_->book1D(title4, title4, 100, 0, 1000);
+
   }
 }
 
@@ -30,9 +40,11 @@ CSCComparatorDigiValidation::~CSCComparatorDigiValidation()
 void CSCComparatorDigiValidation::analyze(const edm::Event&e, const edm::EventSetup&)
 {
   edm::Handle<CSCComparatorDigiCollection> comparators;
+  edm::Handle<CSCStripDigiCollection> stripDigis;
 
   try {
     e.getByLabel(theInputTag, comparators);
+    e.getByLabel(theStripDigiInputTag, stripDigis);
   } catch (...) {
     edm::LogError("CSCDigiDump") << "Cannot get comparators by label " << theInputTag.encode();
   }
@@ -47,12 +59,38 @@ void CSCComparatorDigiValidation::analyze(const edm::Event&e, const edm::EventSe
     const CSCLayer * layer = findLayer(detId.rawId());
     int chamberType = layer->chamber()->specs()->chamberType();
 
+    CSCStripDigiCollection::Range stripRange = stripDigis->get(detId);
 
     theNDigisPerLayerPlots[chamberType-1]->Fill(last-digiItr);
 
     for( ; digiItr != last; ++digiItr) {
       ++nDigisPerEvent;
       theTimeBinPlots[chamberType-1]->Fill(digiItr->getTimeBin());
+
+      int strip = digiItr->getStrip();
+      for(std::vector<CSCStripDigi>::const_iterator stripItr = stripRange.first;
+          stripItr != stripRange.second; ++stripItr)
+      {
+        if(stripItr->getStrip() == strip)
+        {
+          std::vector<int> adc = stripItr->getADCCounts();
+          float pedc = 0.5*(adc[0]+adc[1]);
+          float amp = adc[4] - pedc;
+          theStripDigiPlots[chamberType-1]->Fill(amp);
+          // check neighbors
+          if(stripItr != stripRange.first && stripItr != stripRange.second-1)
+          {
+            std::vector<int> adcl = (stripItr-1)->getADCCounts();
+            std::vector<int> adcr = (stripItr+1)->getADCCounts();
+            float pedl = 0.5*(adcl[0]+adcl[1]);
+            float pedr = 0.5*(adcr[0]+adcr[1]);
+            float three = adcl[4]-pedl
+                        + adcr[4]-pedr
+                        + amp;
+            the3StripPlots[chamberType-1]->Fill(three);
+          }
+        }
+      }
     }
 
   }
--- Validation/MuonCSCDigis/src/CSCComparatorDigiValidation.h
+++ Validation/MuonCSCDigis/src/CSCComparatorDigiValidation.h
@@ -6,13 +6,19 @@
 class CSCComparatorDigiValidation : public CSCBaseValidation
 {
 public:
-  CSCComparatorDigiValidation(DaqMonitorBEInterface* dbe, const edm::InputTag & inputTag);
+  CSCComparatorDigiValidation(DaqMonitorBEInterface* dbe, 
+    const edm::InputTag & inputTag, const edm::InputTag & stripDigiInputTag);
   ~CSCComparatorDigiValidation();
   void analyze(const edm::Event&, const edm::EventSetup&);
 
  private:
+  edm::InputTag theStripDigiInputTag;
+
   MonitorElement* theTimeBinPlots[10];
   MonitorElement* theNDigisPerLayerPlots[10];
+  MonitorElement* theStripDigiPlots[10];
+  MonitorElement* the3StripPlots[10];
+
   MonitorElement* theNDigisPerEventPlot;
 
  
