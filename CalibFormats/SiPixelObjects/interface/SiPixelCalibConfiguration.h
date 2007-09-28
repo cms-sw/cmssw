929b5d26827bd02879a671751751373455fab04c
Anders Ryd <sha1-cdf8ae4e5b0e21334a15ab4f05e71c3dd6ac0eb0@cern.ch>
2007-09-28 00:40:39 +0000

    added missing include on stdint.h

--- CalibFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h
+++ CalibFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdint.h>
 #include "CalibFormats/SiPixelObjects/interface/PixelCalibConfiguration.h"
 
 class SiPixelCalibConfiguration
