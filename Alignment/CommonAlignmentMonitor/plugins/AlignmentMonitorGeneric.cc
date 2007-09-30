6a00b3cef3f5fc990d1430db3f2bacd91628e3d8
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-09-30 19:46:10 +0000

    Fix the range of histograms' x-axes.

--- Alignment/CommonAlignmentMonitor/plugins/AlignmentMonitorGeneric.cc
+++ Alignment/CommonAlignmentMonitor/plugins/AlignmentMonitorGeneric.cc
@@ -3,8 +3,6 @@
 
 #include "Alignment/CommonAlignmentMonitor/plugins/AlignmentMonitorGeneric.h"
 
-#include <TString.h>
-
 AlignmentMonitorGeneric::AlignmentMonitorGeneric(const edm::ParameterSet& cfg):
   AlignmentMonitorBase(cfg)
 {
@@ -12,7 +10,7 @@ AlignmentMonitorGeneric::AlignmentMonitorGeneric(const edm::ParameterSet& cfg):
 
 void AlignmentMonitorGeneric::book()
 {
-  TrackerAlignableId idMap;
+  static TrackerAlignableId idMap;
 
   std::vector<std::string> residNames; // names of residual histograms
 
@@ -40,28 +38,28 @@ void AlignmentMonitorGeneric::book()
     {
       const std::string& name = residNames[n];
 
-      TString histName(name.c_str());
-      histName += Form("_%s_%d", idMap.alignableTypeIdToName(id.second).c_str(), id.first);
-      histName.ReplaceAll(" ", "");
+      std::ostringstream histName;
+      std::ostringstream histTitle;
 
-      TString histTitle(name.c_str());
-      histTitle += Form(" for %s with ID %d (subdet %d)",
-			idMap.alignableTypeIdToName(id.second).c_str(),
-			id.first, DetId(id.first).subdetId());
+      histName << name << id.first;
+      histTitle << name << " for " << idMap.alignableTypeIdToName(id.second)
+		<< " with ID " << id.first;
 
-      TH1F* hist = new TH1F(histName, histTitle, nBin_, -5., 5.);
+      TH1F* hist = new TH1F(histName.str().c_str(), histTitle.str().c_str(),
+			    nBin_, -5., 5.);
+
       hists[n] = static_cast<TH1F*>( add("/iterN/" + name + '/', hist) );
     }
   }
 
   m_trkHists.resize(6, 0);
 
-  m_trkHists[0] = new TH1F("pt"  , "track p_{t} (GeV)" , nBin_,   0.0, 10.0);
+  m_trkHists[0] = new TH1F("pt"  , "track p_{t} (GeV)" , nBin_,   0.0,100.0);
   m_trkHists[1] = new TH1F("eta" , "track #eta"        , nBin_, - 3.0,  3.0);
   m_trkHists[2] = new TH1F("phi" , "track #phi"        , nBin_, -M_PI, M_PI);
-  m_trkHists[3] = new TH1F("d0"  , "track d0 (cm)"     , nBin_, - 0.1,  0.1);
-  m_trkHists[4] = new TH1F("dz"  , "track dz (cm)"     , nBin_, -10.0, 10.0);
-  m_trkHists[5] = new TH1F("chi2", "track #chi^{2}/dof", nBin_,   0.0, 10.0);
+  m_trkHists[3] = new TH1F("d0"  , "track d0 (cm)"     , nBin_, -0.02, 0.02);
+  m_trkHists[4] = new TH1F("dz"  , "track dz (cm)"     , nBin_, -20.0, 20.0);
+  m_trkHists[5] = new TH1F("chi2", "track #chi^{2}/dof", nBin_,   0.0, 20.0);
 
   for (unsigned int h = 0; h < m_trkHists.size(); ++h)
     m_trkHists[h] = static_cast<TH1F*>( add("/iterN/", m_trkHists[h]) );
@@ -90,23 +88,24 @@ void AlignmentMonitorGeneric::event(const edm::EventSetup&,
       {
 	const Alignable* ali = pNavigator()->alignableFromDetId( hit.geographicalId() );
 
-	while (ali) {
-	  std::map<const Alignable*, Hist1Ds>::iterator h = m_resHists.find(ali);
-	  if ( h != m_resHists.end() )
-	    {
-	      TrajectoryStateOnSurface tsos = tsoscomb( meas.forwardPredictedState(), meas.backwardPredictedState() );
-	      
-	      align::LocalVector res = tsos.localPosition() - hit.localPosition();
-	      LocalError err1 = tsos.localError().positionError();
-	      LocalError err2 = hit.localPositionError();
-	      
-	      float errX = std::sqrt( err1.xx() + err2.xx() );
-	      float errY = std::sqrt( err1.yy() + err2.yy() );
-	      
-	      h->second[charge > 0 ? 0 : 1]->Fill(res.x() / errX);
-	      h->second[charge > 0 ? 2 : 3]->Fill(res.y() / errY);
-	    }
-	  ali = ali->mother();
+	std::map<const Alignable*, Hist1Ds>::iterator h = m_resHists.find(ali);
+
+	while ( h == m_resHists.end() && ( ali = ali->mother() ) )
+	  h = m_resHists.find(ali);
+
+	if ( h != m_resHists.end() )
+	{
+	  TrajectoryStateOnSurface tsos = tsoscomb( meas.forwardPredictedState(), meas.backwardPredictedState() );
+
+	  align::LocalVector res = tsos.localPosition() - hit.localPosition();
+	  LocalError err1 = tsos.localError().positionError();
+	  LocalError err2 = hit.localPositionError();
+
+	  float errX = std::sqrt( err1.xx() + err2.xx() );
+	  float errY = std::sqrt( err1.yy() + err2.yy() );
+
+	  h->second[charge > 0 ? 0 : 1]->Fill(res.x() / errX);
+	  h->second[charge > 0 ? 2 : 3]->Fill(res.y() / errY);
 	}
       }
     }
@@ -121,5 +120,5 @@ void AlignmentMonitorGeneric::event(const edm::EventSetup&,
 }
 
 #include "Alignment/CommonAlignmentMonitor/interface/AlignmentMonitorPluginFactory.h"
+
 DEFINE_EDM_PLUGIN(AlignmentMonitorPluginFactory, AlignmentMonitorGeneric, "AlignmentMonitorGeneric");
-  
