36abc15d350f978d898de36ec229f85e7859950c
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-10-01 08:21:17 +0000

    added last writer modules

--- OnlineDB/CSCCondDB/src/SealModule.cc
+++ OnlineDB/CSCCondDB/src/SealModule.cc
@@ -12,11 +12,12 @@
 #include "OnlineDB/CSCCondDB/interface/CSCSaturationAnalyzer.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberIndexValues.h"
-#include "OnlineDB/CSCCondDB/interface/WriteChamberMapValuesToDB.h"
-
 #include "OnlineDB/CSCCondDB/interface/CSCCrateMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCDDUMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/WriteChamberMapValuesToDB.h"
+#include "OnlineDB/CSCCondDB/interface/WriteCrateMapValuesToDB.h"
+#include "OnlineDB/CSCCondDB/interface/WriteChamberIndexValuesToDB.h"
+#include "OnlineDB/CSCCondDB/interface/WriteDDUMapValuesToDB.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCAFEBAnalyzer);
@@ -29,8 +30,10 @@ DEFINE_ANOTHER_FWK_MODULE(CSCscaAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(CSCCFEBConnectivityAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(CSCSaturationAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(WriteChamberMapValuesToDB);
+DEFINE_ANOTHER_FWK_MODULE(WriteCrateMapValuesToDB);
+DEFINE_ANOTHER_FWK_MODULE(WriteChamberIndexValuesToDB);
+DEFINE_ANOTHER_FWK_MODULE(WriteDDUMapValuesToDB);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCChamberMapValues);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCChamberIndexValues);
-DEFINE_ANOTHER_FWK_MODULE(WriteChamberMapValuesToDB);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCCrateMapValues);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CSCDDUMapValues);
