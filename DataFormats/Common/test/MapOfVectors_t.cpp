6d718149d5766d7c24d9120cb62f599fbfb173f8
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2007-10-01 15:50:54 +0000

    more tests for mov

--- DataFormats/Common/test/MapOfVectors_t.cpp
+++ DataFormats/Common/test/MapOfVectors_t.cpp
@@ -3,14 +3,15 @@
 
 #define private public
 #include "DataFormats/Common/interface/MapOfVectors.h"
-
-
 #undef private
 
 #include "FWCore/Utilities/interface/EDMException.h"
 
 #include<vector>
 #include<algorithm>
+
+typedef edm::MapOfVectors<int,int>  MII;
+typedef MII::TheMap TheMap;
 
 class TestMapOfVectors: public CppUnit::TestFixture
 {
@@ -35,19 +36,51 @@ public:
   void find();
   void iterator();
   
-
+  TheMap om;
+  unsigned int tot;
 public:
 
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestMapOfVector);
+CPPUNIT_TEST_SUITE_REGISTRATION(TestMapOfVectors);
 
-TestMapOfVectors::TestMapOfVectors()  {}
+TestMapOfVectors::TestMapOfVectors()  {
+  int v[10] = {0,1,2,3,4,5,6,7,8,9};
+  tot=0;
+  for (int i=0;i<10;++i) {
+    tot+=i;
+    om[i].resize(i);
+    std::copy(v,v+i,om[i].begin());
+  }
+
+}
  
 TestMapOfVectors::~TestMapOfVectors() {}
 
   
-void TestMapOfVectors::default_ctor(){}
-void TestMapOfVectors::filling(){}
-void TestMapOfVectors::find(){}
-void TestMapOfVectors::iterator(){}
+void TestMapOfVectors::default_ctor(){
+  MII m;
+  CPPUNIT_ASSERT(m.size()==0);
+  CPPUNIT_ASSERT(m.empty());
+}
+
+void TestMapOfVectors::filling(){
+  MII m(om);
+  CPPUNIT_ASSERT(m.size()==om.size());
+  CPPUNIT_ASSERT(!m.empty());
+  CPPUNIT_ASSERT(m.m_keys.size()==om.size());
+  CPPUNIT_ASSERT(m.m_offsets.size()==om.size()+1);
+  CPPUNIT_ASSERT(m.m_data.size()==tot);
+}
+
+void TestMapOfVectors::find(){
+  MII m(om);
+  CPPUNIT_ASSERT(m.find(-1)==m.emptyRange());
+  for(TheMap::const_iterator p=om.begin(); p!=om.end();++p) {
+    MII::range r = m.find((*p).first); 
+    CPPUNIT_ASSERT(std::equal((*p).second.begin(), (*p).second.end(),r.begin()));
+  }
+}
+
+void TestMapOfVectors::iterator(){
+}
