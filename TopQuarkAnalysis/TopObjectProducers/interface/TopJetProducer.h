d244353d7f02318e7d46f9ee9837851ca690905d
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-10-02 15:59:56 +0000

    Removed obsolete variables + cosmetics.

--- TopQuarkAnalysis/TopObjectProducers/interface/TopJetProducer.h
+++ TopQuarkAnalysis/TopObjectProducers/interface/TopJetProducer.h
@@ -1,5 +1,5 @@
 //
-// $Id: TopJetProducer.h,v 1.20 2007/09/28 17:18:57 lowette Exp $
+// $Id: TopJetProducer.h,v 1.21 2007/10/02 15:34:59 lowette Exp $
 //
 
 #ifndef TopObjectProducers_TopJetProducer_h
@@ -13,7 +13,7 @@
    with possible adding of resolutions and more things to come
 
   \author   Jan Heyninck
-  \version  $Id: TopJetProducer.h,v 1.20 2007/09/28 17:18:57 lowette Exp $
+  \version  $Id: TopJetProducer.h,v 1.21 2007/10/02 15:34:59 lowette Exp $
 */
 
 
@@ -53,48 +53,42 @@ class TopJetProducer : public edm::EDProducer {
     // TEMP End
 
     // configurables
-    edm::InputTag recJetsLabel_;
-    edm::InputTag caliJetsLabel_;
-    edm::InputTag tracksTag_;
+    edm::InputTag            recJetsLabel_;
+    edm::InputTag            caliJetsLabel_;
+    edm::InputTag            tracksTag_;
     // TEMP Jet cleaning from electrons
-    bool          doJetCleaning_;
-    edm::InputTag topElectronsLabel_;
-    edm::InputTag topMuonsLabel_;
-    float LEPJETDR_;
-    float ELEISOCUT_;
-    float MUISOCUT_;
+    bool                     doJetCleaning_;
+    edm::InputTag            topElectronsLabel_;
+    edm::InputTag            topMuonsLabel_;
+    float                    LEPJETDR_;
+    float                    ELEISOCUT_;
+    float                    MUISOCUT_;
     // TEMP End
-    bool          getJetMCFlavour_;
-    edm::InputTag jetPartonMapSource_;
-    bool          addGenPartonMatch_;
-    edm::InputTag genPartonSrc_;
-    bool          addGenJetMatch_;
-    edm::InputTag genJetSrc_;
-    bool          addPartonJetMatch_;
-    edm::InputTag partonJetSrc_;
-    bool          addResolutions_;
-    bool          useNNReso_;
-    std::string   caliJetResoFile_;
-    bool          addBTagInfo_;
-    bool          ignoreTrackCountingFromAOD_;
-    bool          ignoreTrackProbabilityFromAOD_;
-    bool          ignoreSoftMuonFromAOD_;      
-    bool          ignoreSoftElectronFromAOD_; 
-    bool          addDiscriminators_; 
-    bool          addJetTagRefs_;
-    bool          addAssociatedTracks_;
-    edm::ParameterSet trackAssociationPSet_;
-    bool          addJetCharge_;
-    edm::ParameterSet jetChargePSet_;
-
-    std::vector<std::string> tagModuleLabelsToIgnore_;
+    bool                     getJetMCFlavour_;
+    edm::InputTag            jetPartonMapSource_;
+    bool                     addGenPartonMatch_;
+    edm::InputTag            genPartonSrc_;
+    bool                     addGenJetMatch_;
+    edm::InputTag            genJetSrc_;
+    bool                     addPartonJetMatch_;
+    edm::InputTag            partonJetSrc_;
+    bool                     addResolutions_;
+    bool                     useNNReso_;
+    std::string              caliJetResoFile_;
+    bool                     addBTagInfo_;
+    bool                     addDiscriminators_; 
+    bool                     addJetTagRefs_;
     std::vector<std::string> bTaggingTagInfoNames_;
-
+    std::vector<std::string> tagModuleLabelsToIgnore_;
+    bool                     addAssociatedTracks_;
+    edm::ParameterSet        trackAssociationPSet_;
+    bool                     addJetCharge_;
+    edm::ParameterSet        jetChargePSet_;
     // tools
-    TopObjectResolutionCalc *   theResoCalc_;
-    JetCharge *                 jetCharge_;
-    GreaterByEt<TopJet>         eTComparator_;
-    reco::helper::SimpleJetTrackAssociator    simpleJetTrackAssociator_;
+    TopObjectResolutionCalc                * theResoCalc_;
+    reco::helper::SimpleJetTrackAssociator   simpleJetTrackAssociator_;
+    JetCharge                              * jetCharge_;
+    GreaterByEt<TopJet>                      eTComparator_;
 
 };
 
