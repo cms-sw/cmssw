3f8e04c11bbf66a5f50774ff0aa9f01df80a6d96
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-10-04 22:18:13 +0000

    Treatment of suppression of secondaries modified and also cfg files updated

--- SimG4Core/KillSecondaries/src/KillSecondariesTrackAction.cc
+++ SimG4Core/KillSecondaries/src/KillSecondariesTrackAction.cc
@@ -9,45 +9,37 @@
 
 KillSecondariesTrackAction::KillSecondariesTrackAction(edm::ParameterSet const & p) {
 
-  suppress    = p.getUntrackedParameter<bool>("SuppressHeavy", false);
-  pmaxIon     = p.getUntrackedParameter<double>("IonThreshold", 50.0)*MeV;
-  pmaxProton  = p.getUntrackedParameter<double>("ProtonThreshold", 50.0)*MeV;
-  pmaxNeutron = p.getUntrackedParameter<double>("NeutronThreshold", 50.0)*MeV;
+  killHeavy      = p.getParameter<bool>("KillHeavy");
+  kmaxIon        = p.getParameter<double>("IonThreshold")*MeV;
+  kmaxProton     = p.getParameter<double>("ProtonThreshold")*MeV;
+  kmaxNeutron    = p.getParameter<double>("NeutronThreshold")*MeV;
 
-  std::cout << "KillSecondariesTrackAction:: Suppression Flag " << suppress
-	    << " protons below " << pmaxProton << " MeV/c, neutrons below "
-	    << pmaxNeutron << " and ions below " << pmaxIon << " MeV/c\n";
+  edm::LogInfo("KillSecondaries") << "KillSecondariesTrackAction:: Killing"
+				  << " Flag " << killHeavy << " protons below "
+				  << kmaxProton << " MeV, neutrons below "
+				  << kmaxNeutron << " MeV and ions below " 
+				  << kmaxIon << " MeV\n";
 }
 
 KillSecondariesTrackAction::~KillSecondariesTrackAction() {}
  
 void KillSecondariesTrackAction::update(const BeginOfTrack * trk) {
 
-  if (suppress) {
+  if (killHeavy) {
     G4Track* theTrack = (G4Track*)((*trk)());
     TrackInformation * trkInfo = (TrackInformation *)(theTrack->GetUserInformation());
     if (trkInfo) {
       int pdg = theTrack->GetDefinition()->GetPDGEncoding();
       if (!(trkInfo->isPrimary())) { // Only secondary particles
-	double pp = theTrack->GetMomentum().mag()/MeV;
-	if (((pdg/1000000000 == 1 && ((pdg/10000)%100) > 0 &&
-	      ((pdg/10)%100) > 0)) && (pp<pmaxIon)) {
+	double ke = theTrack->GetKineticEnergy()/MeV;
+	if ((((pdg/1000000000 == 1 && ((pdg/10000)%100) > 0 &&
+	      ((pdg/10)%100) > 0)) && (ke<kmaxIon)) ||
+	    ((pdg == 2212) && (ke < kmaxProton)) ||
+	    ((pdg == 2112) && (ke < kmaxNeutron))) {
 	  theTrack->SetTrackStatus(fStopAndKill);
 	  edm::LogInfo("KillSecondaries") << "Kill Track " << theTrack->GetTrackID()
-					  << " Type " << theTrack->GetDefinition()->GetParticleName()
-					  << " Momentum " << pp << " MeV/c";
-	}
-	if ((pdg == 2212) && (pp < pmaxProton)) {
-	  theTrack->SetTrackStatus(fStopAndKill);
-	  edm::LogInfo("KillSecondaries") << "Kill Track " << theTrack->GetTrackID()
-					  << " Type " << theTrack->GetDefinition()->GetParticleName()
-					  << " Momentum " << pp << " MeV/c";
-      	}
-	if ((pdg == 2112) && (pp < pmaxNeutron)) {
-	  theTrack->SetTrackStatus(fStopAndKill);
-	  edm::LogInfo("KillSecondaries") << "Kill Track " << theTrack->GetTrackID()
-					  << " Type " << theTrack->GetDefinition()->GetParticleName()
-					  << " Momentum " << pp << " MeV/c";
+					  << " Type " << theTrack->GetDefinition()->GetParticleName()	
+					  << " Kinetic Energy " << ke <<" MeV";
 	}
       }
     }
