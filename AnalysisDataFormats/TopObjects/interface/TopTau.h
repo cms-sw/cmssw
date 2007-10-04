a1c3d959a0c2719a4985cd3feb67c786fce7117d
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2007-10-04 15:41:54 +0000

    use of the new BaseTau object. On 1_6_0, it requires additional packages backported from 1_7_X:DataFormats/TauReco V00-00-17, RecoTauTag/RecoTau V00-00-33 and RecoTauTag/TauTagTools V00-00-05

--- AnalysisDataFormats/TopObjects/interface/TopTau.h
+++ AnalysisDataFormats/TopObjects/interface/TopTau.h
@@ -1,19 +1,19 @@
 //
-// $Id$
+// $Id: TopTau.h,v 1.1 2007/09/20 18:12:22 lowette Exp $
 //
 
 #ifndef TopObjects_TopTau_h
 #define TopObjects_TopTau_h
 
 
-#include "DataFormats/TauReco/interface/Tau.h"
+#include "DataFormats/TauReco/interface/BaseTau.h"
 #include "DataFormats/BTauReco/interface/IsolatedTauTagInfo.h"
 
 #include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
 
 
 //typedef reco::IsolatedTauTagInfo TopTauType;
-typedef reco::Tau TopTauType;
+typedef reco::BaseTau TopTauType;
 
 
 /// definition of TopTau as a TopLepton of TopTauType
