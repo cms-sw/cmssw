754d6af231fa9cc9fd27411d822fb047a693574b
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2007-10-04 10:13:26 +0000

    Add check for saturation

--- L1Trigger/GlobalCaloTrigger/src/L1GctJetEtCalibrationLut.cc
+++ L1Trigger/GlobalCaloTrigger/src/L1GctJetEtCalibrationLut.cc
@@ -35,14 +35,19 @@ uint16_t L1GctJetEtCalibrationLut::value (const uint16_t lutAddress) const
   static const uint16_t maxEtMask  = static_cast<uint16_t>((1 << JET_ENERGY_BITWIDTH) - 1);
   static const uint16_t etaMask    = static_cast<uint16_t>((1 << nEtaBits) - 1);
   static const uint16_t tauBitMask = static_cast<uint16_t>( 1 << (JET_ENERGY_BITWIDTH + nEtaBits));
+  static const uint16_t ovrFlowOut = 0xffff;
   uint16_t jetEt = lutAddress & maxEtMask;
-  double uncoEt = static_cast<double>(jetEt) * m_outputEtScale->linearLsb();
-  unsigned eta = static_cast<unsigned>((lutAddress >> JET_ENERGY_BITWIDTH) & etaMask);
-  bool tauVeto = ((lutAddress & tauBitMask)==0);
+  // Check for saturation
+  if (jetEt == maxEtMask) {
+    return ovrFlowOut;
+  } else {
+    double uncoEt = static_cast<double>(jetEt) * m_outputEtScale->linearLsb();
+    unsigned eta = static_cast<unsigned>((lutAddress >> JET_ENERGY_BITWIDTH) & etaMask);
+    bool tauVeto = ((lutAddress & tauBitMask)==0);
   
-  double corrEt = m_lutFunction->correctedEt(uncoEt, eta, tauVeto);
-  return m_lutFunction->calibratedEt(corrEt) | (m_outputEtScale->rank(corrEt) << JET_ENERGY_BITWIDTH);
-
+    double corrEt = m_lutFunction->correctedEt(uncoEt, eta, tauVeto);
+    return m_lutFunction->calibratedEt(corrEt) | (m_outputEtScale->rank(corrEt) << JET_ENERGY_BITWIDTH);
+  }
 }
 
 std::ostream& operator << (std::ostream& os, const L1GctJetEtCalibrationLut& lut)
