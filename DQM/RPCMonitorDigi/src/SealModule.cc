9277b0239a8c35ffc3be601744118248c0f36a64
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2007-10-05 09:36:43 +0000

    Bug Fixed

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -1,6 +1,7 @@
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorSync.h"
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorEfficiency.h"
+#include "DQM/RPCMonitorDigi/interface/MuonSegmentEff.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_SEAL_MODULE();
