efd4a548915f1219935cd85d68c5fdfc887f38e8
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2007-10-06 19:31:06 +0000

    faster to have the functions in the objects

--- CondFormats/CSCObjects/src/CSCChamberMap.cc
+++ CondFormats/CSCObjects/src/CSCChamberMap.cc
@@ -1,4 +1,5 @@
-#include "CondFormats/CSCObjects/interface/CSCChamberMap.h"
+#include <CondFormats/CSCObjects/interface/CSCChamberMap.h>
+#include <DataFormats/MuonDetId/interface/CSCDetId.h>
 
 CSCChamberMap::CSCChamberMap(){}
 
@@ -6,4 +7,39 @@ CSCChamberMap::~CSCChamberMap(){}
 
 const CSCMapItem::MapItem& CSCChamberMap::item( int key ) const { 
   return (ch_map.find(key))->second; 
+}
+
+int CSCChamberMap::dbIndex(const CSCDetId& id ) const {
+
+  int ie = id.endcap();
+  int is = id.station();
+  int ir = id.ring();
+  int ic = id.chamber();
+  //  int il = id.layer(); // zero for parent chamber
+
+  return ie*100000 + is*10000 + ir*1000 + ic*10;
+}
+
+int CSCChamberMap::crate( const CSCDetId& id ) const {
+  int igor = dbIndex( id );  
+  CSCMapItem::MapItem mitem = this->item( igor );
+  return mitem.crateid;   
+}
+
+int CSCChamberMap::dmb( const CSCDetId& id ) const {
+  int igor = dbIndex( id );  
+  CSCMapItem::MapItem mitem = this->item( igor );
+  return mitem.dmb;   
+}
+
+int CSCChamberMap::ddu( const CSCDetId& id ) const {
+  int igor = dbIndex( id );  
+  CSCMapItem::MapItem mitem = this->item( igor );
+  return mitem.ddu;   
+}
+
+int CSCChamberMap::slink( const CSCDetId& id ) const {
+  int igor = dbIndex( id );  
+  CSCMapItem::MapItem mitem = this->item( igor );
+  return mitem.slink;   
 }
