7e71a09fd56deceb1568b1f40f7fd4f922cde141
mfhansen <sha1-4c6d66dbcfddc4349cf1514bfba4442abf4433f7@cern.ch>
2007-10-06 15:10:32 +0000

    added TtHadSolutionMaker

--- TopQuarkAnalysis/TopEventProducers/src/SealModule.cc
+++ TopQuarkAnalysis/TopEventProducers/src/SealModule.cc
@@ -1,6 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtSemiEvtSolutionMaker.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtDilepEvtSolutionMaker.h"
+#include "TopQuarkAnalysis/TopEventProducers/interface/TtHadEvtSolutionMaker.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/StEvtSolutionMaker.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/TopDecaySubset.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtGenEventReco.h"
@@ -12,5 +13,6 @@ DEFINE_FWK_MODULE(TtGenEventReco);
 DEFINE_FWK_MODULE(StGenEventReco);
 DEFINE_FWK_MODULE(TtSemiEvtSolutionMaker);
 DEFINE_FWK_MODULE(TtDilepEvtSolutionMaker);
+DEFINE_FWK_MODULE(TtHadEvtSolutionMaker);
 DEFINE_FWK_MODULE(StEvtSolutionMaker);
 DEFINE_FWK_MODULE(TtDecaySelection);
