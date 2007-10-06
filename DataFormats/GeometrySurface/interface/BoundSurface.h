f4e2a2e16a31dbc321033887f7b43c08480fce65
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2007-10-06 16:23:22 +0000

    - some clenup  - add empty rSpan() method

--- DataFormats/GeometrySurface/interface/BoundSurface.h
+++ DataFormats/GeometrySurface/interface/BoundSurface.h
@@ -1,7 +1,6 @@
 #ifndef Geom_BoundSurface_H
 #define Geom_BoundSurface_H
 
-
 /** \class BoundSurface
  *
  *  Adds Bounds to Surface. 
@@ -12,14 +11,10 @@
  *  (e.g. Plane or Cylinder).
  */
 
-
 #include <memory>
 #include "DataFormats/GeometrySurface/interface/Surface.h"
-
 #include "DataFormats/GeometrySurface/interface/Bounds.h"
-
 #include "DataFormats/GeometrySurface/interface/BoundSpan.h"
-
 
 class BoundSurface : public virtual Surface {
 public:
@@ -27,59 +22,81 @@ public:
   BoundSurface( const PositionType& pos, 
 		const RotationType& rot, 
 		const Bounds* bounds) :
-    Surface( pos, rot),  m_phiSpan(0.,0.),m_zSpan(0.,0.),
-    theBounds( bounds->clone()) { computeSpan();}
+    Surface(   pos, rot ),
+    m_phiSpan( 0., 0.),
+    m_zSpan(   0., 0.),
+    theBounds( bounds->clone() )
+  { 
+    computeSpan();
+  }
 
   BoundSurface( const PositionType& pos, 
 		const RotationType& rot, 
 		const Bounds& bounds) :
-    Surface( pos, rot),  m_phiSpan(0.,0.),m_zSpan(0.,0.),
-    theBounds( bounds.clone()) { computeSpan();}
+    Surface(   pos, rot),
+    m_phiSpan( 0., 0.),
+    m_zSpan(   0., 0.),
+    theBounds( bounds.clone()) 
+  { 
+    computeSpan();
+  }
 
   BoundSurface( const PositionType& pos, 
 		const RotationType& rot, 
 		const Bounds* bounds, 
 		MediumProperties* mp) :
-    Surface( pos, rot, mp),  m_phiSpan(0.,0.),m_zSpan(0.,0.),
-    theBounds( bounds->clone()) { computeSpan();}
+    Surface(   pos, rot, mp),  
+    m_phiSpan( 0., 0.),
+    m_zSpan(   0., 0.),
+    theBounds( bounds->clone()) 
+  { 
+    computeSpan();
+  }
 
   BoundSurface( const PositionType& pos, 
 		const RotationType& rot, 
 		const Bounds& bounds, 
 		MediumProperties* mp) :
-    Surface( pos, rot, mp),  m_phiSpan(0.,0.),m_zSpan(0.,0.),
-    theBounds( bounds.clone()) { computeSpan();}
+    Surface(   pos, rot, mp),  
+    m_phiSpan( 0., 0.),
+    m_zSpan(   0., 0.),
+    theBounds( bounds.clone()) 
+  {
+    computeSpan();
+  }
 
   BoundSurface( const BoundSurface& iToCopy) :
-    Surface( iToCopy ), 
-    m_phiSpan(iToCopy.m_phiSpan),
-    theBounds( iToCopy.theBounds->clone() ) {}
+    Surface(   iToCopy ), 
+    m_phiSpan( iToCopy.m_phiSpan ),
+    m_zSpan(   iToCopy.m_zSpan ),
+    theBounds( iToCopy.theBounds->clone() ) 
+  {}
 
-  const BoundSurface& operator=(const BoundSurface& iRHS ) {
-    Surface::operator=(iRHS);
-    m_phiSpan=iRHS.m_phiSpan;
+  const BoundSurface& operator=( const BoundSurface& iRHS ) {
+    Surface::operator=( iRHS );
+    m_phiSpan = iRHS.m_phiSpan;
+    m_zSpan   = iRHS.m_zSpan;
     theBounds.reset( iRHS.theBounds->clone() );
     return *this;
   }
 
-  const Bounds& bounds() const { return *theBounds;}
+  const Bounds& bounds() const { return *theBounds; }
 
-  std::pair<float,float> const & phiSpan() const { return m_phiSpan;}
-  std::pair<float,float> const & zSpan() const { return m_zSpan;}
+  std::pair<float,float> const & phiSpan() const { return m_phiSpan; }
+  std::pair<float,float> const & zSpan()   const { return m_zSpan; }
+  std::pair<float,float> const & rSpan()   const { return std::pair<float,float>(); }
 
 protected:
-  friend  void boundSpan::computeSpan(BoundSurface& plane);
+  friend void boundSpan::computeSpan(BoundSurface& plane);
   void computeSpan();
 
 private:
 
   std::pair<float,float> m_phiSpan;
   std::pair<float,float> m_zSpan;
-
-  //own_ptr<Bounds,OwnerPolicy::Clone> theBounds;
+//std::pair<float,float> z_zSpan;       // FIXME
+  
   std::auto_ptr<Bounds> theBounds;
 };
-
-
 
 #endif // Geom_BoundSurface_H
