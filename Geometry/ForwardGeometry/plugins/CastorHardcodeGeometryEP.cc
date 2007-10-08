6687104c157b8e842d1a184497a9f6de88dfa4a0
Panagiotis Katsas <sha1-163d0c4b196b62c2ba4e39987db97923b896ea8b@cern.ch>
2007-10-08 21:04:00 +0000

    first version for Castor

---
+++ Geometry/ForwardGeometry/plugins/CastorHardcodeGeometryEP.cc
@@ -0,0 +1,44 @@
+#include "Geometry/ForwardGeometry/plugins/CastorHardcodeGeometryEP.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+
+CastorHardcodeGeometryEP::CastorHardcodeGeometryEP(const edm::ParameterSet& iConfig)
+{
+   //the following line is needed to tell the framework what
+   // data is being produced
+   setWhatProduced(this,"CASTOR");
+   loader_=0;
+}
+
+
+CastorHardcodeGeometryEP::~CastorHardcodeGeometryEP()
+{ 
+  if (loader_) delete loader_;
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+CastorHardcodeGeometryEP::ReturnType
+CastorHardcodeGeometryEP::produce(const IdealGeometryRecord& iRecord)
+{
+  //using namespace edm::es;
+  if (loader_==0) {
+    edm::ESHandle<CastorTopology> topo;
+    try {
+      iRecord.get(topo);
+      loader_=new CastorHardcodeGeometryLoader(*topo); 
+    } catch (...) {
+      loader_=new CastorHardcodeGeometryLoader();
+       edm::LogInfo("CASTOR") << "Using default Castor topology";
+         }
+        }
+   std::auto_ptr<CaloSubdetectorGeometry> pCaloSubdetectorGeometry(loader_->load()) ;
+
+   return pCaloSubdetectorGeometry ;
+}
+
+
