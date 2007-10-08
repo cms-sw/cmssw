8a1c61a4f9ae9c5303649520a7a336b2551accd1
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2007-10-08 14:34:24 +0000

    Add the possibility to have several reco geometries (corresponding to several tracker geometries). Used exclusively in Fast Simulation for now, to handle in the same job one misaligned geometry for the simulation and one aligned geometry for the reconstruction

--- RecoTracker/GeometryESProducer/plugins/TrackerRecoGeometryESProducer.cc
+++ RecoTracker/GeometryESProducer/plugins/TrackerRecoGeometryESProducer.cc
@@ -12,20 +12,30 @@
 
 
 #include <memory>
+#include <string>
 
 using namespace edm;
 
 TrackerRecoGeometryESProducer::TrackerRecoGeometryESProducer(const edm::ParameterSet & p) 
 {
     setWhatProduced(this);
+    // 08-Oct-2007 - Patrick Janot
+    // Allow several reco geometries to be created, corresponding to the labelled  
+    // TrackerDigiGeometry's - that must created beforehand. Useful to handle an 
+    // aligned and a misaligned geometry in the same job. 
+    // The default parameter ("") makes this change transparent to the user
+    // See FastSimulation/Configuration/data/ for examples of cfi's.
+    geoLabel = p.getUntrackedParameter<std::string>("trackerGeometryLabel","");
 }
 
 TrackerRecoGeometryESProducer::~TrackerRecoGeometryESProducer() {}
 
 boost::shared_ptr<GeometricSearchTracker> 
 TrackerRecoGeometryESProducer::produce(const TrackerRecoGeometryRecord & iRecord){ 
+
+
   edm::ESHandle<TrackerGeometry> tG;
-  iRecord.getRecord<TrackerDigiGeometryRecord>().get(tG );
+  iRecord.getRecord<TrackerDigiGeometryRecord>().get( geoLabel, tG );
   GeometricSearchTrackerBuilder builder;
   _tracker  = boost::shared_ptr<GeometricSearchTracker>(builder.build( tG->trackerDet(), &(*tG) ));
   return _tracker;
