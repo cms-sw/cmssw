b731cbac9227a473baafeb6065bbaf52055ba5cb
Nicola De Filippis <sha1-50b8e21d12ea3a05a38a84ea0000270cd7663689@cern.ch>
2007-10-10 22:02:05 +0000

    Added ValidationMisalignedTracker

--- Alignment/OfflineValidation/src/SealModule.cc
+++ Alignment/OfflineValidation/src/SealModule.cc
@@ -6,6 +6,10 @@
 
 #include "Alignment/OfflineValidation/interface/TrackerGeometryCompare.h"
 
+#include "Alignment/OfflineValidation/interface/ValidationMisalignedTracker.h"
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MuonAlignmentAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(TrackerGeometryCompare);
+DEFINE_ANOTHER_FWK_MODULE(ValidationMisalignedTracker);
+
