ac78719c5721f999da723732b8957ff1599cf096
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2007-10-10 22:01:01 +0000

    MixedSeed and SmartSeed

--- RecoMuon/TrackerSeedGenerator/plugins/SealModule.cc
+++ RecoMuon/TrackerSeedGenerator/plugins/SealModule.cc
@@ -2,9 +2,11 @@
 
 #include "RecoMuon/TrackerSeedGenerator/interface/TrackerSeedGeneratorFactory.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGFromOrderedHits.h"
+#include "RecoMuon/TrackerSeedGenerator/interface/TSGSmart.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGForRoadSearch.h"
 #include "RecoMuon/TrackerSeedGenerator/interface/TSGFromPropagation.h"
 
+DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGSmart, "TSGSmart");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGFromOrderedHits, "TSGFromOrderedHits");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGForRoadSearch, "TSGForRoadSearch");
 DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, TSGFromPropagation, "TSGFromPropagation");
