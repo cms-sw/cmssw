668d22dbb340ac547925165390aa205224fe6d8c
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-10-11 12:58:16 +0000

    added new printout

--- CalibTracker/SiStripESProducers/test/testSiStripQualityESProducer.cc
+++ CalibTracker/SiStripESProducers/test/testSiStripQualityESProducer.cc
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <sys/time.h>
+#include <sstream>
 
 testSiStripQualityESProducer::testSiStripQualityESProducer( const edm::ParameterSet& iConfig ):
   printdebug_(iConfig.getUntrackedParameter<bool>("printDebug",false)),
@@ -14,6 +15,8 @@ testSiStripQualityESProducer::testSiStripQualityESProducer( const edm::Parameter
 void testSiStripQualityESProducer::analyze( const edm::Event& e, const edm::EventSetup& iSetup){
 
   unsigned long long cacheID = iSetup.get<SiStripQualityRcd>().cacheIdentifier();
+
+  std::stringstream ss;
 
   if (m_cacheID_ == cacheID) 
     return;
@@ -27,19 +30,40 @@ void testSiStripQualityESProducer::analyze( const edm::Event& e, const edm::Even
   std::vector<uint32_t> detid;
   SiStripQuality_->getDetIds(detid);
   
-  if (printdebug_)
+  if (printdebug_){
     for (size_t id=0;id<detid.size();id++)
       {
+	ss.str("");
+	ss<< "Full Info";
 	SiStripQuality::Range range=SiStripQuality_->getRange(detid[id]);
 	
 	for(int it=0;it<range.second-range.first;it++){
 	  unsigned int value=(*(range.first+it));
-	  std::pair<unsigned short, unsigned short> fs=SiStripQuality_->decode(value);
-	  edm::LogInfo("testSiStripQualityESProducer")  << "detid " << detid[id] << " \t"
-						 << " firstBadStrip " <<  fs.first << "\t "
-						 << " NconsecutiveBadStrips " << fs.second  << "\t "
-						 << " packed integer " <<  std::hex << value << std::dec << "\t "
-						 << std::endl;
-	} 
+	  
+	  ss << "\n\tdetid " << detid[id] << " \t"
+	     << " firstBadStrip " <<  SiStripQuality_->decode(value).firstStrip << "\t "
+	     << " NconsecutiveBadStrips " << SiStripQuality_->decode(value).range  << "\t "
+	     << " flag " << SiStripQuality_->decode(value).flag  << "\t "
+	     << " packed integer 0x" <<  std::hex << value << std::dec << "\t ";
+	}
+	
+	ss << "\n\nDetBase Info\n\t  IsModuleBad()="<<SiStripQuality_->IsModuleBad(detid[id])
+	   << "\t IsFiberBad(n)="<< SiStripQuality_->IsFiberBad(detid[id],0) << " " << SiStripQuality_->IsFiberBad(detid[id],1) << " " << SiStripQuality_->IsFiberBad(detid[id],2)
+	   << "\t getBadFibers()=" << SiStripQuality_->getBadFibers(detid[id])
+	   << "\t IsApvBad()="<< SiStripQuality_->IsApvBad(detid[id],0) << " " << SiStripQuality_->IsApvBad(detid[id],1) << " " << SiStripQuality_->IsApvBad(detid[id],2) << " " << SiStripQuality_->IsApvBad(detid[id],3) << " " << SiStripQuality_->IsApvBad(detid[id],4) << " " << SiStripQuality_->IsApvBad(detid[id],5)   
+	   << "\t getBadApvs()=" << SiStripQuality_->getBadApvs(detid[id]);
+	
+	edm::LogInfo("testSiStripQualityESProducer") << ss.str();
       }
+    ss.str("");
+    ss << "Global Info";
+    std::vector<SiStripQuality::BadComponent> BC = SiStripQuality_->getBadComponentList();
+    ss << "\n\t detid \t IsModuleBad \t BadFibers \t BadApvs";
+
+    for (size_t i=0;i<BC.size();++i)
+      ss << "\n\t" << BC[i].detid << "\t " << BC[i].BadModule << "\t "<< BC[i].BadFibers << "\t " << BC[i].BadApvs << "\t ";
+    ss<< std::endl;
+
+    edm::LogInfo("testSiStripQualityESProducer") << ss.str();
+  }
 }
