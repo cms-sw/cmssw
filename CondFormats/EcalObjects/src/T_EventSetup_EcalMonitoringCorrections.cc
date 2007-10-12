3af13128b55e1315b529f52c107fbeecfdca789c
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2007-10-12 14:18:41 +0000

    Not needed

--- CondFormats/EcalObjects/src/T_EventSetup_EcalMonitoringCorrections.cc
+++
@@ -1,5 +0,0 @@
-// user include files
-#include "CondFormats/EcalObjects/interface/EcalMonitoringCorrections.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
-EVENTSETUP_DATA_REG(EcalMonitoringCorrections);
