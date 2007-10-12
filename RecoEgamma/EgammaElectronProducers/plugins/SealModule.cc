ca94188fefa1c9204b8a6badac33fa9c22023c8b
uberthon <sha1-79a1dca24b5010d458ca508db84b2ac655c0b091@cern.ch>
2007-10-12 12:07:42 +0000

    correct SealModule + additional cleanup in cfg files

--- RecoEgamma/EgammaElectronProducers/plugins/SealModule.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/SealModule.cc
@@ -1,4 +1,3 @@
-//#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -7,25 +6,11 @@
 #include "SiStripElectronProducer.h"
 #include "SiStripElectronAssociator.h"
 #include "ElectronPixelSeedProducer.h"
-//#include "PixelMatchElectronProducer.h"
 #include "PixelMatchGsfElectronProducer.h"
-#include "PixelMatchGsfElectronAnalyzer.h"
-//#include "TrackProducerWithSeedAssoc.h"
-//#include "GsfTrackProducerWithSeedAssoc.h"
-//#include "CkfTrackCandidateMakerWithSeedAssoc.h"
-//#include "CkfTrajectoryBuilderWithSeedAssocESProducer.h"
-
-//using cms::CkfTrackCandidateMakerWithSeedAssoc;
 
 DEFINE_SEAL_MODULE();
 
-//DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(CkfTrajectoryBuilderWithSeedAssocESProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAssociator);
 DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedProducer);
-//DEFINE_ANOTHER_FWK_MODULE(PixelMatchElectronProducer);
 DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronProducer);
-DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
-//DEFINE_ANOTHER_FWK_MODULE(TrackProducerWithSeedAssoc);
-//DEFINE_ANOTHER_FWK_MODULE(GsfTrackProducerWithSeedAssoc);
-//DEFINE_ANOTHER_FWK_MODULE(CkfTrackCandidateMakerWithSeedAssoc);
