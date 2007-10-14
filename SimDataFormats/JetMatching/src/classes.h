d92fdf60d917177952f723ff312eb0f64ceb82b0
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2007-10-14 20:08:44 +0000

    *** empty log message ***

--- SimDataFormats/JetMatching/src/classes.h
+++ SimDataFormats/JetMatching/src/classes.h
@@ -2,7 +2,10 @@
 #include <vector>
 
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavour.h"
 #include "SimDataFormats/JetMatching/interface/JetFlavourMatching.h"
+#include "SimDataFormats/JetMatching/interface/MatchedPartons.h"
+#include "SimDataFormats/JetMatching/interface/JetMatchedPartons.h"
 
 namespace {
   namespace {
@@ -13,6 +16,12 @@ namespace {
     reco::JetFlavourMatchingRefProd                  jfmrp;
     reco::JetFlavourMatchingRefVector                jfrv;
     edm::Wrapper<reco::JetFlavourMatchingCollection> wjfmc;
-
+    reco::MatchedPartons                             mp;
+    reco::JetMatchedPartonsCollectionBase           jmpcb;
+    reco::JetMatchedPartonsCollection               jmpc;
+    reco::JetMatchedPartonsRef                      jmpr;
+    reco::JetMatchedPartonsRefProd                  jmprp;
+    reco::JetMatchedPartonsRefVector                jmpv;
+    edm::Wrapper<reco::JetMatchedPartonsCollection> wjmpc;
   }
 }
