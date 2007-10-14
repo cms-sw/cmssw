cb29e727c6dc412ab56abae6cb100560a991b442
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2007-10-14 19:26:15 +0000

    few methods added

--- SimDataFormats/JetMatching/interface/MatchedPartons.h
+++ SimDataFormats/JetMatching/interface/MatchedPartons.h
@@ -2,7 +2,8 @@
 #define MatchedParton_H
 
 #include <vector>
-#include "DataFormats/HepMCCandidate/interface/GenParticleCandidateFwd.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
 
 namespace reco
 {
@@ -10,39 +11,44 @@ namespace reco
 class MatchedPartons
 {
   public:
-    MatchedPartons();
-    MatchedPartons(reco::GenParticleCandidateRefVector partons, std::vector<float> deltaR) : 
-      m_partons(partons), 
-      m_deltaR(deltaR) 
-    { }
-
-   /** 
-      Return the GenParticleRef for the heaviest flavour in the signal cone
-    */ 
-    const reco::GenParticleCandidateRef & heaviest(float signalCone = 0.3) const;
-
-   /**
-      Return the GenParticleRef for the heaviest flavour in the signal cone
-    */ 
-    const reco::GenParticleCandidateRef & physicsDefinitionParton(float signalCone = 0.3) const;
-
-   /**
-      Return the GenParticleRef for the nearest parton
-    */ 
-    const reco::GenParticleCandidateRef & nearest() const;
     
-   /**
-     Return the list of partons associated, ordered by distance
-    */
-    reco::GenParticleCandidateRefVector partons() { return m_partons; }
-   /**
-     Return the list of partons deltaR to the original jet
-    */
-    std::vector<float> deltaR() { return m_deltaR; }
-     
+    MatchedPartons() { }
+    MatchedPartons( 
+                    CandidateRef hv,
+                    CandidateRef n2,
+                    CandidateRef n3,
+                    CandidateRef pd,
+                    CandidateRef ad 
+                  ) : 
+       m_heaviest(hv),
+       m_nearest2(n2),
+       m_nearest3(n3),
+       m_PhysDef(pd),
+       m_AlgoDef(ad) { }
+
+    //Return the ParticleRef for the heaviest flavour in the signal cone
+    const CandidateRef heaviest() const {return m_heaviest;}
+
+    //Return the ParticleRef for the nearest parton (status=2)
+    const CandidateRef & nearest_status2() const {return m_nearest2;}
+
+    //Return the ParticleRef for the nearest parton (status=3)
+    const CandidateRef & nearest_status3() const {return m_nearest3;}
+    
+    //Return the ParticleRef for the Physics Definition parton
+    const CandidateRef & physicsDefinitionParton() const {return m_PhysDef;}
+
+    //Return the ParticleRef for the Algorithmic Definition parton
+    const CandidateRef & algoDefinitionParton() const {return m_AlgoDef;}
+
   private:
-    reco::GenParticleCandidateRefvector m_partons;
-    std::vector<float> m_deltaR;
+
+    CandidateRef m_heaviest;
+    CandidateRef m_nearest2;
+    CandidateRef m_nearest3;
+    CandidateRef m_PhysDef;
+    CandidateRef m_AlgoDef;
+
 };
 
 }
