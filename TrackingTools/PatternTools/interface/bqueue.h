db64eda07f11d73859055e1ff0a4ca21b200ead4
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-10-18 01:49:41 +0000

    TempTrajectory moved from RecoTracker/CkfPattern

---
+++ TrackingTools/PatternTools/interface/bqueue.h
@@ -0,0 +1,105 @@
+#ifndef CMSUTILS_BEUEUE_H
+#define CMSUTILS_BEUEUE_H
+#include <boost/shared_ptr.hpp>
+
+/**  Backwards linked queue with "head sharing"
+
+     Author: Giovanni Petrucciani
+     
+     For use in trajectory building, where we want to "fork" a trajectory candidate in two
+     without copying around all the hits before the fork.
+
+     Supported operations (mimics a std container):
+       - push_back()
+       - fork() which returns a new queue that shares the head part with the old one
+       - copy constructor, which just does fork()
+       - rbegin(), rend(): return iterators which can work only backwards
+            for (cmsutils::bqueue<T>::const_iterator it = queue.rbegin(), end = queue.rend();
+                        it != end; --it) { ... }
+       - front() and back(): returns a reference to the first and last element
+       - size(), empty(): as expected. size() does not count the items
+     
+     Note that boost::shared_ptr is used for items, so they are deleted automatically
+     while avoiding problems if one deletes a queue which shares the head with another one
+
+     Disclaimer: I'm not sure the const_iterator is really const-correct..
+*/
+namespace cmsutils {
+
+template<class T> class _bqueue_itr;
+template<class T> class bqueue;
+
+template <class T> 
+class _bqueue_item {
+        friend class bqueue<T>;
+        friend class _bqueue_itr<T>;
+    private:
+        _bqueue_item() : back(0), value() { }
+        _bqueue_item(boost::shared_ptr< _bqueue_item<T> > tail, const T &val) : back(tail), value(val) { }
+        boost::shared_ptr< _bqueue_item<T> > back;
+        T value;
+};
+template<class T>
+class _bqueue_itr {
+    public:
+        T* operator->() { return &it->value; }
+        T& operator*() { return it->value; }
+        const T* operator->() const { return &it->value; }
+        const T& operator*() const { return it->value; }
+        _bqueue_itr<T> & operator--() { it = it->back; return *this; }
+        const _bqueue_itr<T> & operator--() const { it = it->back.get(); return *this; }
+        bool operator==(const _bqueue_itr<T> &t2) const  { return t2.it == it; }
+        bool operator!=(const _bqueue_itr<T> &t2) const { return t2.it != it; }
+        friend class bqueue<T>;
+    private:
+        _bqueue_itr(_bqueue_item<T> *t) : it(t) { }
+        _bqueue_itr(const _bqueue_item<T> *t) : it(t) { }
+        mutable _bqueue_item<T> *it;
+};
+
+template<class T>
+class bqueue {
+    public:
+        typedef T value_type;
+        typedef unsigned short int size_type;
+        typedef _bqueue_item<value_type>       item;
+        typedef boost::shared_ptr< _bqueue_item<value_type> >  itemptr;
+        typedef _bqueue_itr<value_type>       iterator;
+        typedef const _bqueue_itr<value_type> const_iterator;
+        bqueue() : m_size(0), m_bound(), m_head(m_bound), m_tail(m_bound) { }
+        ~bqueue() { }
+        bqueue(const bqueue<T> &cp) : m_size(cp.m_size), m_bound(cp.m_bound), m_head(cp.m_head), m_tail(cp.m_tail) { }
+        bqueue<T> fork() {
+            return bqueue<T>(m_size,m_bound,m_head,m_tail);
+        }
+        void push_back(const T& val) {
+            m_tail = itemptr(new item(this->m_tail, val)); 
+            if ((++m_size) == 1) { m_head = m_tail; };
+        }
+        T & front() { return m_head->value; }
+        const T & front() const { return m_head->value; }
+        T & back() { return m_tail->value; }
+        const T & back() const { return m_tail->value; }
+        iterator rbegin() { return m_tail.get(); }
+        const_iterator rbegin() const { return m_tail.get(); }
+        const_iterator rend() const { return m_bound.get(); }
+        size_type size() const { return m_size; }
+        bool empty() const { return m_size == 0; }
+        const T & operator[](size_type i) const {
+                int idx = m_size - i - 1;
+                const_iterator it = rbegin();
+                while (idx-- > 0) --it;
+                return *it;
+        }
+    private:
+        bqueue(size_type size, itemptr bound, itemptr head, itemptr tail) :
+            m_size(size), m_bound(bound), m_head(head), m_tail(tail) { }
+        size_type m_size;
+        itemptr m_bound, m_head, m_tail;
+        
+};
+
+
+}
+
+#endif
---
+++ TrackingTools/PatternTools/src/TempTrajectory.cc
@@ -0,0 +1,124 @@
+#include "TrackingTools/PatternTools/interface/TempTrajectory.h"
+#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include <ext/slist>
+
+/*
+void TempTrajectory::pop() { 
+  if (!empty()) {
+    if (theData.back().recHit()->isValid())             theNumberOfFoundHits--;
+    else if(lost(* (theData.back().recHit()) )) theNumberOfLostHits--;
+    theData.pop_back();
+  }
+}
+*/
+
+// bool Trajectory::inactive( const Det& det) 
+// {
+//   typedef Det::DetUnitContainer DUC;
+
+//   // DetUnit case -- straightforward
+//   const DetUnit* detu = dynamic_cast<const DetUnit*>(&det);
+//   if (detu != 0) return detu->inactive();
+//   else {
+//     const DetLayer* detl = dynamic_cast<const DetLayer*>(&det);
+//     if (detl != 0) return false; // DetLayer should have inactive() too,
+// 				 // but for the moment we skip it (see below)
+//     else { // composite case
+//       DUC duc = det.detUnits();
+//       for (DUC::const_iterator i=duc.begin(); i!=duc.end(); i++) {
+// 	if ( !(**i).inactive()) return false;
+//       }
+//       return true;
+//     }
+//   }
+//   // the loop over DetUnits works for all 
+//   // Dets, but it would be too slow for a big DetLayer; it would
+//   // require creatind and copying the vector of all DetUnit* each time
+//   // an invalid RecHit is produced by the layer, so it is penalizing
+//   // even for active layers.
+//   // Therefore the layer is not handled yet, and should eventually have
+//   // it's own inactive() method.
+// }
+  
+void TempTrajectory::push( const TrajectoryMeasurement& tm) {
+  push( tm, tm.estimate());
+}
+
+void TempTrajectory::push( const TrajectoryMeasurement& tm, double chi2Increment)
+{
+  theData.push_back(tm);
+  if ( tm.recHit()->isValid()) {
+    theNumberOfFoundHits++;
+   }
+  //else if (lost( tm.recHit()) && !inactive(tm.recHit().det())) theNumberOfLostHits++;
+  else if (lost( *(tm.recHit()) ) )   theNumberOfLostHits++;
+  
+ 
+  theChiSquared += chi2Increment;
+
+  // in case of a Trajectory constructed without direction, 
+  // determine direction from the radii of the first two measurements
+
+  if ( !theDirectionValidity && theData.size() >= 2) {
+    if (theData.front().updatedState().globalPosition().perp() <
+	theData.back().updatedState().globalPosition().perp())
+      theDirection = alongMomentum;
+    else theDirection = oppositeToMomentum;
+    theDirectionValidity = true;
+  }
+}
+/*
+Trajectory::RecHitContainer Trajectory::recHits() const {
+  RecHitContainer hits;
+  hits.reserve(theData.size());
+
+  for (Trajectory::DataContainer::const_iterator itm
+	 = theData.begin(); itm != theData.end(); itm++) {
+    hits.push_back((*itm).recHit());
+  }
+  return hits;
+}
+
+*/
+
+PropagationDirection TempTrajectory::direction() const {
+  if (theDirectionValidity) return theDirection;
+  else throw cms::Exception("TrackingTools/PatternTools","Trajectory::direction() requested but not set");
+}
+
+void TempTrajectory::check() const {
+  if ( theData.size() == 0) 
+    throw cms::Exception("TrackingTools/PatternTools","Trajectory::check() - information requested from empty Trajectory");
+}
+
+bool TempTrajectory::lost( const TransientTrackingRecHit& hit)
+{
+  if ( hit.isValid()) return false;
+  else {
+  //     // A DetLayer is always inactive in this logic.
+  //     // The DetLayer is the Det of an invalid RecHit only if no DetUnit 
+  //     // is compatible with the predicted state, so we don't really expect
+  //     // a hit in this case.
+  
+    if(hit.geographicalId().rawId() == 0) {return false;}
+    else{
+      return hit.getType() == TrackingRecHit::missing;
+    }
+  }
+}
+
+Trajectory TempTrajectory::toTrajectory() const {
+  Trajectory traj(*theSeed, theDirection);
+
+  traj.reserve(theData.size());
+  __gnu_cxx::slist<const TrajectoryMeasurement*> list;
+  for (TempTrajectory::DataContainer::const_iterator it = theData.rbegin(), ed = theData.rend(); it != ed; --it) {
+        list.push_front(&(*it));
+  }
+  for(__gnu_cxx::slist<const TrajectoryMeasurement*>::const_iterator it = list.begin(), ed = list.end(); it != ed; ++it) {
+        traj.push(**it);
+  }
+  return traj;
+}
