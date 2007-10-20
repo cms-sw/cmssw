c1f29ad5f60df4ec92c3812987fa9b2e4f443b07
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2007-10-20 15:54:07 +0000

    new analyzer to validate GlobalMuon track matching and algorithm

--- Validation/RecoMuon/src/SealModule.cc
+++ Validation/RecoMuon/src/SealModule.cc
@@ -6,9 +6,11 @@
 #include "Validation/RecoMuon/src/MuonTrackResidualAnalyzer.h"
 #include "Validation/RecoMuon/src/RecoMuonValidator.h"
 #include "Validation/RecoMuon/src/MuonSeedTrack.h"
+#include "Validation/RecoMuon/src/GlobalMuonMatchAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_FWK_MODULE(MuonTrackAnalyzer);
 DEFINE_FWK_MODULE(MuonTrackResidualAnalyzer);
 DEFINE_FWK_MODULE(RecoMuonValidator);
 DEFINE_FWK_MODULE(MuonSeedTrack);
+DEFINE_FWK_MODULE(GlobalMuonMatchAnalyzer);
