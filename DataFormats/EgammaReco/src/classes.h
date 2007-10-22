1275b8f77795bec2ae36a5dd7910bb0cd7788413
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-10-22 17:52:35 +0000

    special commit to use latest pi0 rejection with 16X

--- DataFormats/EgammaReco/src/classes.h
+++ DataFormats/EgammaReco/src/classes.h
@@ -1,23 +1,15 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
-#include "Math/Cartesian3D.h" 
-#include "DataFormats/Common/interface/RefProd.h" 
-#include "DataFormats/EgammaReco/interface/BasicClusterFwd.h" 
 #include "DataFormats/EgammaReco/interface/ClusterPi0Discriminator.h"
-#include "DataFormats/EgammaReco/interface/ClusterPi0DiscriminatorFwd.h" 
 #include "DataFormats/EgammaReco/interface/SuperCluster.h"
-#include "Rtypes.h" 
-#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h" 
 #include "DataFormats/EgammaReco/interface/EcalCluster.h"
 #include "DataFormats/EgammaReco/interface/ClusterShape.h"
-#include "DataFormats/EgammaReco/interface/HFEMClusterShape.h"
 #include "DataFormats/EgammaReco/interface/EgammaTrigger.h"
 #include "DataFormats/EgammaReco/interface/ElectronPixelSeed.h"
-#include "DataFormats/EgammaReco/interface/ElectronPixelSeedFwd.h" 
 #include "DataFormats/EgammaReco/interface/PreshowerCluster.h"
-#include "DataFormats/EgammaReco/interface/PreshowerClusterFwd.h" 
+#include "DataFormats/EgammaReco/interface/SeedSuperClusterAssociation.h"
 #include "DataFormats/EgammaReco/interface/BasicClusterShapeAssociation.h"
-#include "DataFormats/EgammaReco/interface/HFEMClusterShapeAssociation.h"
+#include "DataFormats/EgammaReco/interface/PreshowerClusterShape.h"
 
 namespace { 
   namespace {
@@ -49,12 +41,6 @@ namespace {
     edm::RefProd<reco::ClusterShapeCollection> rp5;
     edm::Wrapper<edm::RefVector<reco::ClusterShapeCollection> > wrv5;
 
-    reco::HFEMClusterShapeCollection v8;
-    edm::Wrapper<reco::HFEMClusterShapeCollection> w8;
-    edm::Ref<reco::HFEMClusterShapeCollection> r8;
-    edm::RefProd<reco::HFEMClusterShapeCollection> rp8;
-    edm::Wrapper<edm::RefVector<reco::HFEMClusterShapeCollection> > wrv8;
-
     reco::ClusterPi0DiscriminatorCollection v6;
     edm::Wrapper<reco::ClusterPi0DiscriminatorCollection> w6;
     edm::Ref<reco::ClusterPi0DiscriminatorCollection> r6;
@@ -67,11 +53,24 @@ namespace {
     edm::RefProd<reco::ElectronPixelSeedCollection> rp111;
     edm::Wrapper<edm::RefVector<reco::ElectronPixelSeedCollection> > wrv111;
     
+    reco::SeedSuperClusterAssociationCollection v112;
+    edm::Wrapper<reco::SeedSuperClusterAssociationCollection> w112;
+    reco::SeedSuperClusterAssociation ra112;
+    reco::SeedSuperClusterAssociationRef r112;
+    reco::SeedSuperClusterAssociationRefProd rp112;
+    edm::Wrapper<reco::SeedSuperClusterAssociationRefVector> wrv112;
+    
     reco::PreshowerClusterCollection ps5;
     edm::Wrapper<reco::PreshowerClusterCollection> psw5;
     edm::Ref<reco::PreshowerClusterCollection> psr5;
     edm::RefProd<reco::PreshowerClusterCollection> psrp5;
     edm::Wrapper<edm::RefVector<reco::PreshowerClusterCollection> > wpsrv5;
+
+    reco::PreshowerClusterShapeCollection pss5;
+    edm::Wrapper<reco::PreshowerClusterShapeCollection> pssw5;
+    edm::Ref<reco::PreshowerClusterShapeCollection> pssr5;
+    edm::RefProd<reco::PreshowerClusterShapeCollection> pssrp5;
+    edm::Wrapper<edm::RefVector<reco::PreshowerClusterShapeCollection> > wpssrv5;
 
     reco::BasicClusterShapeAssociationCollection v7;
     edm::Wrapper<reco::BasicClusterShapeAssociationCollection> w7;
@@ -80,12 +79,6 @@ namespace {
     reco::BasicClusterShapeAssociationRefProd vrp7;
     edm::Wrapper<reco::BasicClusterShapeAssociationRefVector> wvrv7;
 
-    reco::HFEMClusterShapeAssociationCollection v9;
-    edm::Wrapper<reco::HFEMClusterShapeAssociationCollection> w9;
-    reco::HFEMClusterShapeAssociation va9;
-    reco::HFEMClusterShapeAssociationRef vr9;
-    reco::HFEMClusterShapeAssociationRefProd vrp9;
-    edm::Wrapper<reco::HFEMClusterShapeAssociationRefVector> wvrv9;
 
   }
 }
