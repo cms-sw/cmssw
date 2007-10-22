ebcf916b8163035aa1c2e5b74630c4cb6e4a897a
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-10-22 18:57:00 +0000

    added ProductDoesNotSupportPtr code

--- FWCore/Utilities/src/EDMException.cc
+++ FWCore/Utilities/src/EDMException.cc
@@ -26,6 +26,7 @@ namespace edm {
       EDM_MAP_ENTRY_NONS(trans_, FileInPathError);
       EDM_MAP_ENTRY_NONS(trans_, FatalRootError);
       EDM_MAP_ENTRY_NONS(trans_, ProductDoesNotSupportViews);
+      EDM_MAP_ENTRY_NONS(trans_, ProductDoesNotSupportPtr);
       EDM_MAP_ENTRY_NONS(trans_, NotFound);
     }
   }
