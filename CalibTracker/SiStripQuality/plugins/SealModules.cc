7cd6941119942f391192640beeaeefc80134630f
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-10-24 08:47:03 +0000

    follow changes in dir

--- CalibTracker/SiStripQuality/plugins/SealModules.cc
+++ CalibTracker/SiStripQuality/plugins/SealModules.cc
@@ -3,9 +3,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripBadModuleByHandBuilder.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripQualityStatistics.h"
+#include "CalibTracker/SiStripQuality/plugins/SiStripQualityHotStripIdentifier.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripBadStripFromASCIIFile.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadModuleByHandBuilder);
 DEFINE_ANOTHER_FWK_MODULE(SiStripQualityStatistics);
+DEFINE_ANOTHER_FWK_MODULE(SiStripQualityHotStripIdentifier);
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadStripFromASCIIFile);
