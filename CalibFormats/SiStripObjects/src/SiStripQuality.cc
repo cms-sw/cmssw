02d0940ec0c38424a794b7fd4eada5dd1b1ff576
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2007-10-24 15:29:15 +0000

    bug fixes

--- CalibFormats/SiStripObjects/src/SiStripQuality.cc
+++ CalibFormats/SiStripObjects/src/SiStripQuality.cc
@@ -1,7 +1,7 @@
  //
 // Author:      Domenico Giordano
 // Created:     Wed Sep 26 17:42:12 CEST 2007
-// $Id: SiStripQuality.cc,v 1.3 2007/10/18 08:42:18 giordano Exp $
+// $Id: SiStripQuality.cc,v 1.4 2007/10/24 08:33:14 giordano Exp $
 //
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 #include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
@@ -93,58 +93,50 @@ void SiStripQuality::add(const SiStripBadStrip* base){
   SiStripBadStrip::Range  range, baserange;
   std::vector<unsigned int> vect, tmp;
   
-  if (indexes.size()==0){
-    //Case A - the Registry is empty
-    indexes.insert(indexes.end(),basebegin,baseend);
-    v_badstrips.insert(v_badstrips.end(),base->getDataVectorBegin(),base->getDataVectorEnd());
-      
-  }else{
-  
-    //Case B - the Registry already contains data
-    //Loop on detids
-    for (SiStripBadStrip::RegistryIterator basep=basebegin; basep != baseend; ++basep) {
-      uint32_t detid=basep->detid;
-      LogTrace("SiStripQuality") << "add detid " <<detid << std::endl;
+  //the Registry already contains data
+  //Loop on detids
+  for (SiStripBadStrip::RegistryIterator basep=basebegin; basep != baseend; ++basep) {
+    uint32_t detid=basep->detid;
+    LogTrace("SiStripQuality") << "add detid " <<detid << std::endl;
 
-      unsigned short Nstrips=reader->getNumberOfApvsAndStripLength(detid).first*128;
+    unsigned short Nstrips=reader->getNumberOfApvsAndStripLength(detid).first*128;
     
-      baserange = SiStripBadStrip::Range( base->getDataVectorBegin()+basep->ibegin , base->getDataVectorBegin()+basep->iend );
+    baserange = SiStripBadStrip::Range( base->getDataVectorBegin()+basep->ibegin , base->getDataVectorBegin()+basep->iend );
   
-      //Is this detid already in the collections owned by this class?
-      range = getRange(detid);
+    //Is this detid already in the collections owned by this class?
+    range = getRange(detid);
     
-      //Append bad strips  
-      tmp.clear();
-      if (range.first==range.second){
-	LogTrace("SiStripQuality") << "new detid" << std::endl;
-	//It's a new detid
-	tmp.insert(tmp.end(),baserange.first,baserange.second);
-  	std::stable_sort(tmp.begin(),tmp.end());
-	LogTrace("SiStripQuality") << "ordered" << std::endl;
-      } else {
-	LogTrace("SiStripQuality") << "already exists" << std::endl;
-	//alredy existing detid
+    //Append bad strips  
+    tmp.clear();
+    if (range.first==range.second){
+      LogTrace("SiStripQuality") << "new detid" << std::endl;
+      //It's a new detid
+      tmp.insert(tmp.end(),baserange.first,baserange.second);
+      std::stable_sort(tmp.begin(),tmp.end());
+      LogTrace("SiStripQuality") << "ordered" << std::endl;
+    } else {
+      LogTrace("SiStripQuality") << "already exists" << std::endl;
+      //alredy existing detid
 	
-	//if full det is bad go to next detid
-	SiStripBadStrip::data data_=decode(*(range.first));
-	if(range.second-range.first==1
-	   && data_.firstStrip==0
-	   && data_.range>=Nstrips){
-	  LogTrace("SiStripQuality") << "full det is bad.. " << range.second-range.first << " " << decode(*(range.first)).firstStrip << " " << decode(*(range.first)).range << " " << decode(*(range.first)).flag <<"\n"<< std::endl;
-	  continue;
-	}
-	 	
- 	tmp.insert(tmp.end(),baserange.first,baserange.second);
-	tmp.insert(tmp.end(),range.first,range.second);
- 	std::stable_sort(tmp.begin(),tmp.end());
-	LogTrace("SiStripQuality") << "ordered" << std::endl;
+      //if full det is bad go to next detid
+      SiStripBadStrip::data data_=decode(*(range.first));
+      if(range.second-range.first==1
+	 && data_.firstStrip==0
+	 && data_.range>=Nstrips){
+	LogTrace("SiStripQuality") << "full det is bad.. " << range.second-range.first << " " << decode(*(range.first)).firstStrip << " " << decode(*(range.first)).range << " " << decode(*(range.first)).flag <<"\n"<< std::endl;
+	continue;
       }
-      //Compact data
-      compact(tmp,vect,Nstrips);
-      SiStripBadStrip::Range newrange(vect.begin(),vect.end());
-      if ( ! put_replace(detid,newrange) )
-	edm::LogError("SiStripQuality")<<"[" << __PRETTY_FUNCTION__ << "] " << std::endl;
+	 	
+      tmp.insert(tmp.end(),baserange.first,baserange.second);
+      tmp.insert(tmp.end(),range.first,range.second);
+      std::stable_sort(tmp.begin(),tmp.end());
+      LogTrace("SiStripQuality") << "ordered" << std::endl;
     }
+    //Compact data
+    compact(tmp,vect,Nstrips);
+    SiStripBadStrip::Range newrange(vect.begin(),vect.end());
+    if ( ! put_replace(detid,newrange) )
+      edm::LogError("SiStripQuality")<<"[" << __PRETTY_FUNCTION__ << "] " << std::endl;
   }
 }
 
@@ -302,17 +294,17 @@ void SiStripQuality::fillBadComponents(){
     unsigned short Nstrips=reader->getNumberOfApvsAndStripLength(basep->detid).first*128;
     
     //BadModules
-    if (basep->iend - basep->ibegin == 1){
-      fs=decode(*(range.first));
-      if ( fs.firstStrip==0 && 
-	   fs.range==Nstrips ){
-	result.detid=basep->detid;
-	result.BadModule=true;
-	result.BadFibers=(1<< (Nstrips/256))-1; 
-	result.BadApvs=(1<< (Nstrips/128))-1; 
+    fs=decode(*(range.first));
+    if (basep->iend - basep->ibegin == 1 &&
+	fs.firstStrip==0 && 
+	fs.range==Nstrips ){
+      result.detid=basep->detid;
+      result.BadModule=true;
+      result.BadFibers=(1<< (Nstrips/256))-1; 
+      result.BadApvs=(1<< (Nstrips/128))-1; 
 	
-	BadComponentVect.push_back(result);
-      }  
+      BadComponentVect.push_back(result);
+      
     } else {
 
       //Bad Fibers and  Apvs
