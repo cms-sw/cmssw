fa1841996782ed321c4ae9723b0d0a3e0ec2888a
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-10-24 13:39:19 +0000

    added missing #include

--- DataFormats/RecoCandidate/interface/TrackCandidateAssociation.h
+++ DataFormats/RecoCandidate/interface/TrackCandidateAssociation.h
@@ -4,6 +4,8 @@
 #include "DataFormats/Common/interface/OneToOne.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
 
 namespace reco {
   typedef edm::AssociationMap<edm::OneToOne<reco::TrackCollection, reco::CandidateCollection> > TrackCandidateAssociation;
