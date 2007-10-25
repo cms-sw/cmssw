254d6ce22bb0a7a03c9e942deb986d4d9c6456f0
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2007-10-25 16:48:14 +0000

    Fix compilation errors for 64-bit build

--- FWCore/Services/src/SimpleProfiler.cc
+++ FWCore/Services/src/SimpleProfiler.cc
@@ -27,6 +27,10 @@
 #error "SimpleProfile requires the definition of __USE_POSIX199309"
 #endif
 
+using namespace std;
+
+
+
 namespace INSTR
 {
   typedef unsigned char byte;
@@ -36,10 +40,10 @@ namespace INSTR
 namespace
 {
 
-  std::string makeFileName()
+  string makeFileName()
   {
     pid_t p = getpid();
-    std::ostringstream ost;
+    ostringstream ost;
     ost << "profdata_" << p;
     return ost.str();
   }
@@ -54,9 +58,9 @@ namespace
 #define getSP(X)  asm ( "movl %%esp,%0" : "=m" (X) )
 
 #if 0
-#define DODEBUG if(1) std::cerr
+#define DODEBUG if(1) cerr
 #else
-#define DODEBUG if(0) std::cerr
+#define DODEBUG if(0) cerr
 #endif
 
 
@@ -90,13 +94,13 @@ namespace
   void write_maps()
   {
     pid_t pid = getpid();
-    std::ostringstream strinput,stroutput;
+    ostringstream strinput,stroutput;
     strinput << "/proc/" << pid << "/maps";
     stroutput << "profdata_" << pid << "_maps";
-    std::ifstream input(strinput.str().c_str());
-    std::ofstream output(stroutput.str().c_str());
-    std::string line;
-    while (std::getline(input, line)) output << line << '\n';
+    ifstream input(strinput.str().c_str());
+    ofstream output(stroutput.str().c_str());
+    string line;
+    while (getline(input, line)) output << line << '\n';
     input.close();
     output.close();
   }
@@ -105,13 +109,13 @@ namespace
 
   void openCondFile()
   {
-    std::string filename(makeFileName());
+    string filename(makeFileName());
     filename += "_condfile";
     frame_cond = fopen(filename.c_str(),"w");
     if(frame_cond==0)
       {
-	std::cerr << "bad open of profdata_condfile\n";
-	throw std::runtime_error("bad open");
+	cerr << "bad open of profdata_condfile\n";
+	throw runtime_error("bad open");
       }
   }
 
@@ -199,7 +203,7 @@ extern "C"
     ++samples_total;
 
 #if 0
-    std::cerr << "siginfo=" << (void*)info
+    cerr << "siginfo=" << (void*)info
 	 << " context=" << context
 	 << " stack=" << (void*)ucp->uc_stack.ss_sp
 	 << "\n";
@@ -220,7 +224,7 @@ extern "C"
 	  {
 	    *cur++ = ((unsigned int)eip);
 	    *cur++ = 0U;
-	    //std::cerr << "early completion for eip = " << (unsigned int)eip << '\n';
+	    //cerr << "early completion for eip = " << (unsigned int)eip << '\n';
 	    fprintf(frame_cond," early completion for eip %8.8x\n",(unsigned int)eip);
 
 	    ++samples_uninterpretable;
@@ -252,7 +256,7 @@ extern "C"
 	if(eip[0]==INSTR::RET)
 	  {
 	    ++samples_between_leave_ret;
-	    //std::cerr << "after the leave and immediately before the ret\n";
+	    //cerr << "after the leave and immediately before the ret\n";
 	    condition += 1;
 	    //*cur++ = ((unsigned int)*esp);
 	    *cur++ = *esp;
@@ -280,14 +284,14 @@ extern "C"
 	  {
 	    ++samples_after_push;
 	    condition += 4;	    
-	    //std::cerr << "after the push, before the mov\n";
+	    //cerr << "after the push, before the mov\n";
 	    ebp = esp;
 	  }
 	else if(eip[0]==0x55 && eip[1]==0x89 && eip[2]==0xe5)
 	  {
 	    ++samples_before_push;
 	    condition += 8;
-	    //std::cerr << "after the call, before the push\n";
+	    //cerr << "after the call, before the push\n";
 	    *cur++ = *esp;
 	  }
 	// if value at stacktop and stacktop-1 are outside stack range
@@ -325,13 +329,13 @@ extern "C"
 		  // already been popped.
 		  ++samples_popped;
 		  condition += 2;
-		  //std::cerr << "after the leave but before the ret\n";
+		  //cerr << "after the leave but before the ret\n";
 		  *cur++ = *esp;
 		}
 	    }
 	  else
 	    {
-	      //std::cerr << "skip stack trace\n";
+	      //cerr << "skip stack trace\n";
 	      ++samples_skipped;
 	      ebp=0;
 	    }
@@ -370,7 +374,7 @@ extern "C"
 
 	    if (++counter > 1000)
 	      {
-		std::cerr << "BAD COUNT!!!!!!\n";
+		cerr << "BAD COUNT!!!!!!\n";
 		break;
 	      }
 	  }
@@ -391,10 +395,10 @@ namespace
   void* setStacktop()
   {
 #if defined(__x86_64__) || defined(__LP64__) || defined(_LP64)
-    throw std::logic_error("setStacktop not callable on 64 bit platform");
+    throw logic_error("setStacktop not callable on 64 bit platform");
     return 0;
 #else
-    const std::string target_name("__libc_start_main");
+    const string target_name("__libc_start_main");
     unsigned int* ebp_reg;
     getBP(ebp_reg);
     unsigned int* ebp = (unsigned int*)(ebp_reg);
@@ -415,12 +419,12 @@ namespace
 	if(dladdr(sta[i],&look)!=0)
 	  {
 #if 0
-	    std::cerr << "sta[" << i << "]=" << sta[i] << ":  " << (look.dli_saddr ? look.dli_sname : "?") << ":" << look.dli_saddr << "\n"; 
+	    cerr << "sta[" << i << "]=" << sta[i] << ":  " << (look.dli_saddr ? look.dli_sname : "?") << ":" << look.dli_saddr << "\n"; 
 #endif
 	    if (look.dli_saddr && target_name==look.dli_sname)
 	      {
 		address_of_target = sta[i];
-		// std::cerr << "found " << target_name << "\n";
+		// cerr << "found " << target_name << "\n";
 	      }
 	  }
 	else
@@ -429,14 +433,14 @@ namespace
 	    // was not found by the dynamic loader. The function might
 	    // be one that is declared 'static', and is thus not
 	    // visible outside of its compilation unit.
-	    std::cerr << "setStacktop: no function information for " 
+	    cerr << "setStacktop: no function information for " 
 		 << sta[i] 
 		 << "\n";
 	  }
       }
 
     if (address_of_target == 0)
-      throw std::runtime_error("no main function found in stack");
+      throw runtime_error("no main function found in stack");
 
     //fprintf(stderr,"depth=%d top=%8.8x\n",depth,top);
 
@@ -455,7 +459,7 @@ namespace
     };
 
     if (depth==0) 
-      throw std::runtime_error("setStacktop: could not find stack bottom");
+      throw runtime_error("setStacktop: could not find stack bottom");
 
     // Now we have to move one frame more, to the frame of the target
     // function. We want the location in memory of this frame (not any
@@ -470,7 +474,7 @@ namespace
   void setupTimer()
   {
 #if USE_SIGALTSTACK
-    static std::vector<char> charbuffer(SIGSTKSZ);
+    static vector<char> charbuffer(SIGSTKSZ);
     //ss_area.ss_sp = new char[SIGSTKSZ];
     ss_area.ss_sp = &charbuffer[0];
     ss_area.ss_flags = 0;
@@ -537,8 +541,8 @@ namespace
 	perror("getrlimit failed");
 	abort();
       }
-    std::cerr << "core size limit (soft): " << limits.rlim_cur << '\n';
-    std::cerr << "core size limit (hard): " << limits.rlim_max << '\n';
+    cerr << "core size limit (soft): " << limits.rlim_cur << '\n';
+    cerr << "core size limit (hard): " << limits.rlim_max << '\n';
 
     struct itimerval newval;
     struct itimerval oldval;
@@ -625,9 +629,9 @@ SimpleProfiler::SimpleProfiler():
   stacktop_(setStacktop())
 {
   if (fd_ < 0) {
-      std::ostringstream ost;
+      ostringstream ost;
       ost << "failed to open profiler output file " << filename_;
-      throw std::runtime_error(ost.str().c_str());
+      throw runtime_error(ost.str().c_str());
   }
   
   openCondFile();
@@ -636,7 +640,7 @@ SimpleProfiler::SimpleProfiler():
 SimpleProfiler::~SimpleProfiler()
 {
   if (running_)
-    std::cerr << "Warning: the profile timer was not stopped,\n"
+    cerr << "Warning: the profile timer was not stopped,\n"
 	 << "profiling data in " << filename_
 	 << " is probably incomplete and will not be processed\n";
 
@@ -646,9 +650,9 @@ SimpleProfiler::~SimpleProfiler()
 void SimpleProfiler::commitFrame(void** first, void** last)
 {
   void** cnt_ptr = curr_; 
-  *cnt_ptr = reinterpret_cast<void*>(std::distance(first,last));
+  *cnt_ptr = reinterpret_cast<void*>(distance(first,last));
   ++curr_;
-  curr_ = std::copy(first,last,curr_);
+  curr_ = copy(first,last,curr_);
   if(curr_ > high_water_) doWrite();
 }
 
@@ -656,12 +660,12 @@ void SimpleProfiler::doWrite()
 {
   void** start = &frame_data_[0];
   if(curr_ == start) return;
-  unsigned int cnt = std::distance(start,curr_) * sizeof(void*);
+  unsigned int cnt = distance(start,curr_) * sizeof(void*);
   unsigned int totwr=0;
 
   while (cnt>0 && (totwr=write(fd_,start,cnt)) != cnt) {
       if(totwr==0) 
-	throw std::runtime_error("SimpleProfiler::doWrite wrote zero bytes");
+	throw runtime_error("SimpleProfiler::doWrite wrote zero bytes");
       start+=(totwr/sizeof(void*));
       cnt-=totwr;
   }
@@ -672,13 +676,13 @@ void SimpleProfiler::doWrite()
 void SimpleProfiler::start()
 {
 #if defined(__x86_64__) || defined(__LP64__) || defined(_LP64)
-  throw std::logic_error("SimpleProfiler not available on 64 bit platform");
+  throw logic_error("SimpleProfiler not available on 64 bit platform");
 #endif
   {
     boost::mutex::scoped_lock sl(lock_);
 
     if (installed_) {
-	std::cerr << "Warning: second thread " << pthread_self()
+	cerr << "Warning: second thread " << pthread_self()
 	     << " requested the profiler timer and another thread\n"
 	     << owner_ << "has already started it.\n"
 	     << "Only one thread can do profiling at a time.\n"
@@ -698,19 +702,19 @@ void SimpleProfiler::stop()
 {
   if(!installed_)
     {
-      std::cerr << "SimpleProfiler::stop - no timer installed to stop\n";
+      cerr << "SimpleProfiler::stop - no timer installed to stop\n";
       return;
     }
 
   if(owner_ != pthread_self())
     {
-      std::cerr << "SimpleProfiler::stop - only owning thread can stop the timer\n";
+      cerr << "SimpleProfiler::stop - only owning thread can stop the timer\n";
       return;
     }
 
   if(!running_)
     {
-      std::cerr << "SimpleProfiler::stop - no timer is running\n";
+      cerr << "SimpleProfiler::stop - no timer is running\n";
       return;
     }
 
@@ -738,7 +742,7 @@ void SimpleProfiler::complete()
 
   if(lseek(fd_,0,SEEK_SET)<0)
     {
-      std::cerr << "SimpleProfiler: could not seek to the start of the profile\n"
+      cerr << "SimpleProfiler: could not seek to the start of the profile\n"
 	   << " data file during completion.  Data will be lost.\n";
       return;
     }
@@ -746,9 +750,9 @@ void SimpleProfiler::complete()
   writeProfileData(fd_,filename_);
   write_maps();
 
-  std::string totsname = makeFileName();
+  string totsname = makeFileName();
   totsname += "_sample_info";
-  std::ofstream ost(totsname.c_str());
+  ofstream ost(totsname.c_str());
   
   ost << "samples_total " << samples_total << "\n"
       << "samples_uninterpretable " << samples_uninterpretable << "\n"
