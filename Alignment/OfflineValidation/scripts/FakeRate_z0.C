61cd5681d7bf8c0b9956a1d26e83e1611551cab7
Nicola De Filippis <sha1-50b8e21d12ea3a05a38a84ea0000270cd7663689@cern.ch>
2007-10-25 21:40:06 +0000

    *** empty log message ***

--- Alignment/OfflineValidation/scripts/FakeRate_z0.C
+++ Alignment/OfflineValidation/scripts/FakeRate_z0.C
@@ -55,13 +55,13 @@ TTree *MyTree5=FakeTracks;
 /// EFFICIENCIES VS ETA ALIGNED
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero = new TH1F("etazero","eta zero",20,0.,500.); 
-TH1F *etauno = new TH1F("etauno","eta uno",20,0.,500.); 
-TH1F *etadue = new TH1F("etadue","eta due",20,0.,500.); 
-TH1F *etatre = new TH1F("etatre","eta tre",20,0.,500.); 
+TH1F *etazero = new TH1F("etazero","eta zero",20,-20.,20.); 
+TH1F *etauno = new TH1F("etauno","eta uno",20,-20.,20.); 
+TH1F *etadue = new TH1F("etadue","eta due",20,-20.,20.); 
+TH1F *etatre = new TH1F("etatre","eta tre",20,-20.,20.); 
 
-MyTree->Project("etazero","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree->Project("etauno","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree->Project("etazero","fakerecz0"," fakerecnhit>=8");
+MyTree->Project("etauno","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue->Add(etazero,etauno,1,-1);
 
@@ -87,7 +87,7 @@ for (int k=1; k<21; k++){
   err=0.;
 }
 
-etadue->SetTitle("Fake rate vs p_{T} for ttbar events");
+etadue->SetTitle("Fake rate vs z_{0} for ttbar events");
 etadue->SetMarkerStyle(20);
 etadue->SetMarkerColor(2);
 etadue->SetMarkerSize(0.9);
@@ -102,13 +102,13 @@ c1->WaitPrimitive();
 // /// EFFICIENCIES VS ETA SCEN 1 
 // //&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero_scen1 = new TH1F("etazero_scen1","eta zero",20,0.,500.); 
-TH1F *etauno_scen1 = new TH1F("etauno_scen1","eta uno",20,0.,500.); 
-TH1F *etadue_scen1 = new TH1F("etadue_scen1","eta due",20,0.,500.); 
-TH1F *etatre_scen1 = new TH1F("etatre_scen1","eta tre",20,0.,500.); 
+TH1F *etazero_scen1 = new TH1F("etazero_scen1","eta zero",20,-20.,20.); 
+TH1F *etauno_scen1 = new TH1F("etauno_scen1","eta uno",20,-20.,20.); 
+TH1F *etadue_scen1 = new TH1F("etadue_scen1","eta due",20,-20.,20.); 
+TH1F *etatre_scen1 = new TH1F("etatre_scen1","eta tre",20,-20.,20.); 
 
-MyTree1->Project("etazero_scen1","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree1->Project("etauno_scen1","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree1->Project("etazero_scen1","fakerecz0"," fakerecnhit>=8");
+MyTree1->Project("etauno_scen1","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue_scen1->Add(etazero_scen1,etauno_scen1,1,-1);
 
@@ -135,7 +135,7 @@ for (int k=1; k<21; k++){
 }
 
 
-etadue_scen1->SetTitle("Fake rate vs p_{T} for ttbar events");
+etadue_scen1->SetTitle("Fake rate vs z_{0} for ttbar events");
 etadue_scen1->SetMarkerStyle(21);
 etadue_scen1->SetMarkerColor(3);
 etadue_scen1->SetMarkerSize(0.9);
@@ -151,13 +151,13 @@ c1->WaitPrimitive();
 // /// EFFICIENCIES VS ETA SCEN 1 
 // //&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero_scen2 = new TH1F("etazero_scen2","eta zero",20,0.,500.); 
-TH1F *etauno_scen2 = new TH1F("etauno_scen2","eta uno",20,0.,500.); 
-TH1F *etadue_scen2 = new TH1F("etadue_scen2","eta due",20,0.,500.); 
-TH1F *etatre_scen2 = new TH1F("etatre_scen2","eta tre",20,0.,500.); 
+TH1F *etazero_scen2 = new TH1F("etazero_scen2","eta zero",20,-20.,20.); 
+TH1F *etauno_scen2 = new TH1F("etauno_scen2","eta uno",20,-20.,20.); 
+TH1F *etadue_scen2 = new TH1F("etadue_scen2","eta due",20,-20.,20.); 
+TH1F *etatre_scen2 = new TH1F("etatre_scen2","eta tre",20,-20.,20.); 
 
-MyTree2->Project("etazero_scen2","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree2->Project("etauno_scen2","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree2->Project("etazero_scen2","fakerecz0"," fakerecnhit>=8");
+MyTree2->Project("etauno_scen2","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue_scen2->Add(etazero_scen2,etauno_scen2,1,-1);
 
@@ -184,7 +184,7 @@ for (int k=1; k<21; k++){
 }
 
 
-etadue_scen2->SetTitle("Fake rate vs p_{T} for ttbar events");
+etadue_scen2->SetTitle("Fake rate vs z_{0} for ttbar events");
 etadue_scen2->SetMarkerStyle(22);
 etadue_scen2->SetMarkerColor(4);
 etadue_scen2->SetMarkerSize(0.9);
@@ -199,13 +199,13 @@ c1->WaitPrimitive();
 // /// EFFICIENCIES VS ETA SCEN 1 
 // //&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero_scen3 = new TH1F("etazero_scen3","eta zero",20,0.,500.); 
-TH1F *etauno_scen3 = new TH1F("etauno_scen3","eta uno",20,0.,500.); 
-TH1F *etadue_scen3 = new TH1F("etadue_scen3","eta due",20,0.,500.); 
-TH1F *etatre_scen3 = new TH1F("etatre_scen3","eta tre",20,0.,500.); 
+TH1F *etazero_scen3 = new TH1F("etazero_scen3","eta zero",20,-20.,20.); 
+TH1F *etauno_scen3 = new TH1F("etauno_scen3","eta uno",20,-20.,20.); 
+TH1F *etadue_scen3 = new TH1F("etadue_scen3","eta due",20,-20.,20.); 
+TH1F *etatre_scen3 = new TH1F("etatre_scen3","eta tre",20,-20.,20.); 
 
-MyTree3->Project("etazero_scen3","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree3->Project("etauno_scen3","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree3->Project("etazero_scen3","fakerecz0"," fakerecnhit>=8");
+MyTree3->Project("etauno_scen3","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue_scen3->Add(etazero_scen3,etauno_scen3,1,-1);
 
@@ -232,7 +232,7 @@ for (int k=1; k<21; k++){
 }
 
 
-etadue_scen3->SetTitle("Fake rate vs p_{T} for ttbar events");
+etadue_scen3->SetTitle("Fake rate vs z_{0} for ttbar events");
 etadue_scen3->SetMarkerStyle(23);
 etadue_scen3->SetMarkerColor(5);
 etadue_scen3->SetMarkerSize(0.9);
@@ -247,13 +247,13 @@ c1->WaitPrimitive();
 // /// EFFICIENCIES VS ETA SCEN 1 
 // //&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero_scen4 = new TH1F("etazero_scen4","eta zero",20,0.,500.); 
-TH1F *etauno_scen4 = new TH1F("etauno_scen4","eta uno",20,0.,500.); 
-TH1F *etadue_scen4 = new TH1F("etadue_scen4","eta due",20,0.,500.); 
-TH1F *etatre_scen4 = new TH1F("etatre_scen4","eta tre",20,0.,500.); 
+TH1F *etazero_scen4 = new TH1F("etazero_scen4","eta zero",20,-20.,20.); 
+TH1F *etauno_scen4 = new TH1F("etauno_scen4","eta uno",20,-20.,20.); 
+TH1F *etadue_scen4 = new TH1F("etadue_scen4","eta due",20,-20.,20.); 
+TH1F *etatre_scen4 = new TH1F("etatre_scen4","eta tre",20,-20.,20.); 
 
-MyTree4->Project("etazero_scen4","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree4->Project("etauno_scen4","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree4->Project("etazero_scen4","fakerecz0"," fakerecnhit>=8");
+MyTree4->Project("etauno_scen4","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue_scen4->Add(etazero_scen4,etauno_scen4,1,-1);
 
@@ -280,7 +280,7 @@ for (int k=1; k<21; k++){
 }
 
 
-etadue_scen4->SetTitle("Fake rate vs p_{T} for ttbar events");
+etadue_scen4->SetTitle("Fake rate vs z_{0} for ttbar events");
 etadue_scen4->SetMarkerStyle(24);
 etadue_scen4->SetMarkerColor(6);
 etadue_scen4->SetMarkerSize(0.9);
@@ -296,13 +296,13 @@ c1->WaitPrimitive();
 /// EFFICIENCIES VS ETA SCEN 1 NO ERR
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-TH1F *etazero_noErr = new TH1F("etazero_noErr","eta zero",20,0.,500.); 
-TH1F *etauno_noErr = new TH1F("etauno_noErr","eta uno",20,0.,500.); 
-TH1F *etadue_noErr = new TH1F("etadue_noErr","eta due",20,0.,500.); 
-TH1F *etatre_noErr = new TH1F("etatre_noErr","eta tre",20,0.,500.); 
+TH1F *etazero_noErr = new TH1F("etazero_noErr","eta zero",20,-20.,20.); 
+TH1F *etauno_noErr = new TH1F("etauno_noErr","eta uno",20,-20.,20.); 
+TH1F *etadue_noErr = new TH1F("etadue_noErr","eta due",20,-20.,20.); 
+TH1F *etatre_noErr = new TH1F("etatre_noErr","eta tre",20,-20.,20.); 
 
-MyTree5->Project("etazero_noErr","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8");
-MyTree5->Project("etauno_noErr","abs(fakerecpt)","fakerecpt<=500 && fakerecnhit>=8 && fake==1");
+MyTree5->Project("etazero_noErr","fakerecz0"," fakerecnhit>=8");
+MyTree5->Project("etauno_noErr","fakerecz0"," fakerecnhit>=8 && fake==1");
 
 etadue_noErr->Add(etazero_noErr,etauno_noErr,1,-1);
 
@@ -328,7 +328,7 @@ for (int k=1; k<21; k++){
   err=0.;
 }
 
-etadue_noErr->SetTitle("Fake rate vs p_{T} noErr");
+etadue_noErr->SetTitle("Fake rate vs z_{0} noErr");
 etadue_noErr->SetMarkerStyle(25);
 etadue_noErr->SetMarkerColor(7);
 etadue_noErr->SetMarkerSize(0.9);
@@ -343,10 +343,10 @@ c1->WaitPrimitive();
 // /// EFFICIENCIES VS ETA STACKED
 // //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-etadue->SetTitle("Fake rate vs p_{T} for ttbar events");
-etadue->SetXTitle("p_{T}(GeV/c)");
+etadue->SetTitle("Fake rate vs z_{0} for ttbar events");
+etadue->SetXTitle("z_{0}(GeV/c)");
 etadue->SetYTitle("Fake Rate");
-etadue->SetAxisRange(-0.005,0.7,"Y");
+etadue->SetAxisRange(0.,0.33,"Y");
 etadue->Draw();
 etadue_noErr->Draw("same");
 etadue_scen1->Draw("same");
@@ -354,10 +354,10 @@ etadue_scen2->Draw("same");
 etadue_scen3->Draw("same");
 etadue_scen4->Draw("same");
 
-TLegend *leg1 = new TLegend(0.1,0.81,0.82,0.925);
+TLegend *leg1 = new TLegend(0.15,0.75,0.55,0.9);
 leg1->SetTextAlign(32);
 leg1->SetTextColor(1);
-leg1->SetTextSize(0.03);
+leg1->SetTextSize(0.02);
 
 leg1->AddEntry(etadue,"perfect alignment", "P");
 leg1->AddEntry(etadue_scen1,"SurveyLAS alignment", "P");
@@ -369,7 +369,7 @@ leg1->AddEntry(etadue_noErr,"10 pb-1 alignment; APE not used", "P");
 leg1->Draw();
 
 c1->Update();
-c1->SaveAs("Fake_pt_ttbar.eps");
+c1->SaveAs("Fake_z0_ttbar.eps");
 c1->WaitPrimitive();
 
 gROOT->Reset();
