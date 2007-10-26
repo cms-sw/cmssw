dc8750561ebad6bec8f0d61bf14ebf03ee563d60
Alfredo Gurrola <sha1-084451a3ed00cf0aa02687697cc833febdbe6441@cern.ch>
2007-10-26 20:10:20 +0000

    added TauMET module

--- JetMETCorrections/Type1MET/src/SealModule.cc
+++ JetMETCorrections/Type1MET/src/SealModule.cc
@@ -5,11 +5,14 @@
 
 #include "Type1MET.h"
 #include "MuonMET.h"
+#include "TauMET.h"
 
 using cms::Type1MET;
 using cms::MuonMET;
+using cms::TauMET;
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(Type1MET);
 DEFINE_ANOTHER_FWK_MODULE(MuonMET);
+DEFINE_ANOTHER_FWK_MODULE(TauMET);
