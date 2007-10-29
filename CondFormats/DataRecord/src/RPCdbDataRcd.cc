fea1bb2780826efb4ded70e87774606d81141993
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2007-10-29 16:11:51 +0000

    first release

---
+++ CondFormats/DataRecord/src/RPCdbDataRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCdbDataRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Thu Sep 27 16:10:49 CEST 2007
+// $Id$
+
+#include "CondFormats/DataRecord/interface/RPCdbDataRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(RPCdbDataRcd);
