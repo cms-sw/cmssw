40b18b51cb37d359f56bb00c352846951efd7458
Stephanie Beauceron <sha1-c117a3d4e0f055ce820584107fcb2695f540f4e1@cern.ch>
2007-10-30 16:07:58 +0000

    Wieland modification

--- Calibration/EcalCalibAlgos/interface/ElectronCalibration.h
+++ Calibration/EcalCalibAlgos/interface/ElectronCalibration.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Lorenzo AGOSTINO, Radek Ofierzynski
 //         Created:  Tue Jul 18 12:17:01 CEST 2006
-// $Id: ElectronCalibration.h,v 1.4 2006/11/20 13:47:56 malgeri Exp $
+// $Id: ElectronCalibration.h,v 1.5.2.1 2007/10/22 16:29:56 beaucero Exp $
 //
 //
 
@@ -64,18 +64,40 @@ class ElectronCalibration : public edm::EDAnalyzer {
       edm::InputTag electronLabel_;
       edm::InputTag trackLabel_;
       std::string calibAlgo_;
+      std::string miscalibfile_;
+      int keventweight_;
+      double ElePt_;
+      double maxeta_;
+      double mineta_;
+      double maxphi_;
+      double minphi_;
+      double cut1_;
+      double cut2_;
+      double cut3_;
+      int elecclass_;
+      double cutEPCalo1_;
+      double cutEPCalo2_;
+      double cutEPin1_;
+      double cutEPin2_;
+      double cutCalo1_;
+      double cutCalo2_;
+      double cutESeed_;
+      int ClusterSize_;
+
       CalibrationCluster calibCluster;
       CalibrationCluster::CalibMap ReducedMap;
       
       int read_events;
       
-      int calibClusterSize;
+      int calibClusterSize,keventweight;
       int etaMin, etaMax, phiMin, phiMax;
       vector<float> EnergyVector;
+      vector<float> WeightVector;
       vector<vector<float> > EventMatrix;
       vector<int> MaxCCeta;
       vector<int> MaxCCphi;
       vector<float> EnergyVectorNoCuts;
+      vector<float> WeightVectorNoCuts;
       vector<vector<float> > EventMatrixNoCuts;
       vector<int> MaxCCetaNoCuts;
       vector<int> MaxCCphiNoCuts;
@@ -85,16 +107,30 @@ class ElectronCalibration : public edm::EDAnalyzer {
       vector<float> newCalibs;
       vector<float> oldCalibs;
       
+      int eventcrystal[171][360];
+      int numevent_;
+
+      TFile * f;
+
       TH1F * e25;
       TH1F * e9;
       TH1F * scE;
       TH1F * trP;
       TH1F * EoP;
+      TH1F * EoP_all;
       TH1F * calibs;
       TH1F * e9Overe25;
       TH1F * e25OverScE;
       TH2F * Map;
       TH1F * E25oP;
+
+      TH1F * PinOverPout;
+      TH1F * eSeedOverPout;
+      TH1F * MisCalibs;
+      TH1F * RatioCalibs;
+      TH1F * DiffCalibs;
+      TH1F * RatioCalibsNoCuts;
+      TH1F * DiffCalibsNoCuts;
 
       TH1F * e25NoCuts;
       TH1F * e9NoCuts;
@@ -106,6 +142,72 @@ class ElectronCalibration : public edm::EDAnalyzer {
       TH1F * e25OverScENoCuts;
       TH2F * MapNoCuts;
       TH1F * E25oPNoCuts;
-      
+      TH2F * Map3Dcalib;
+      TH2F * Map3DcalibNoCuts;
+      TH1F * calibinter;
+      TH1F * calibinterNoCuts;
+      HouseholderDecomposition * MyHH;
+      TH1F * PinOverPoutNoCuts;
+      TH1F * eSeedOverPoutNoCuts;
+
+      TH2F * GeneralMap;
+
+      TH2F * MapCor1;
+      TH2F * MapCor2;
+      TH2F * MapCor3;
+      TH2F * MapCor4;
+      TH2F * MapCor5;
+      TH2F * MapCor6;
+      TH2F * MapCor7;
+      TH2F * MapCor8;
+      TH2F * MapCor9;
+      TH2F * MapCor10;
+      TH2F * MapCor11;
+      TH2F * MapCorCalib;
+
+      TH2F * MapCor1NoCuts;
+      TH2F * MapCor2NoCuts;
+      TH2F * MapCor3NoCuts;
+      TH2F * MapCor4NoCuts;
+      TH2F * MapCor5NoCuts;
+      TH2F * MapCor6NoCuts;
+      TH2F * MapCor7NoCuts;
+      TH2F * MapCor8NoCuts;
+      TH2F * MapCor9NoCuts;
+      TH2F * MapCor10NoCuts;
+      TH2F * MapCor11NoCuts;
+      TH2F * MapCorCalibNoCuts;
+
+      TH2F * MapCor1ESeed;
+      TH2F * MapCor2ESeed;
+      TH2F * MapCor3ESeed;
+      TH2F * MapCor4ESeed;
+      TH2F * MapCor5ESeed;
+      TH2F * MapCor6ESeed;
+      TH2F * MapCor7ESeed;
+      TH2F * MapCor8ESeed;
+      TH2F * MapCor9ESeed;
+      TH2F * MapCor10ESeed;
+      TH2F * MapCor11ESeed;
+
+
+      TH1F * PinMinPout; 
+      TH1F * PinMinPoutNoCuts;
+
+      TH1F * Error1;
+      TH1F * Error2;
+      TH1F * Error3;
+      TH1F * Error1NoCuts;
+      TH1F * Error2NoCuts;
+      TH1F * Error3NoCuts;
+
+      TH1F * eSeedOverPout2;
+      TH1F * eSeedOverPout2NoCuts;
+      TH1F * eSeedOverPout2ESeed;
+
+      TH1F * hadOverEm;
+      TH1F * hadOverEmNoCuts;
+      TH1F * hadOverEmESeed;
+
 };
 #endif
