10711638812ca82a353907209c7b4d106f4ec93b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-11-05 20:14:49 +0000

    Disable automatic adding of histograms to current directory (again)

--- IOPool/Common/src/ClassFiller.cc
+++ IOPool/Common/src/ClassFiller.cc
@@ -1,13 +1,11 @@
 #include "IOPool/Common/interface/ClassFiller.h"
 #include "Cintex/Cintex.h"
-//#include "TH1.h"
+#include "TH1.h"
 
 namespace edm {
   // ---------------------
   void ClassFiller() {
-    // We may want to do this in the future but for now we have to give people
-    // time to adjust their code...
-    //    TH1::AddDirectory(kFALSE);
+    TH1::AddDirectory(kFALSE);
     ROOT::Cintex::Cintex::Enable();
   }
 }
