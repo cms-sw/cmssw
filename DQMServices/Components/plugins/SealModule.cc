197c08e228b2018af0ba87ba8f17dabbcddde810
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-11-05 11:59:33 +0000

    *** empty log message ***

--- DQMServices/Components/plugins/SealModule.cc
+++ DQMServices/Components/plugins/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 // The module providing event information 
-#include "DQMServices/Components/interface/DQMOnlineEnvironment.h"
-DEFINE_FWK_MODULE(DQMOnlineEnvironment);
+#include "DQMServices/Components/src/EventCoordinatesSource.h"
+DEFINE_FWK_MODULE(EventCoordinatesSource);
 #include "DQMServices/Components/interface/QualityTester.h"
 DEFINE_ANOTHER_FWK_MODULE(QualityTester);
 #include "DQMServices/Components/src/DQMFileSaver.h"
