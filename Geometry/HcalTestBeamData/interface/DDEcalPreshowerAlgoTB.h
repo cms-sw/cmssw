069c62c811261c479f4c5f024dacadc6af35e524
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-11-06 00:16:14 +0000

    Try to introduce Preshower detector

---
+++ Geometry/HcalTestBeamData/interface/DDEcalPreshowerAlgoTB.h
@@ -0,0 +1,41 @@
+#ifndef DDEcalPreshowerAlgoTB_h
+#define DDEcalPreshowerAlgoTB_h
+
+#include <vector>
+#include <string>
+
+#include "DetectorDescription/Algorithm/interface/DDAlgorithm.h"
+
+class DDEcalPreshowerAlgoTB : public DDAlgorithm {
+
+public:
+  DDEcalPreshowerAlgoTB();
+  void initialize(const DDNumericArguments & nArgs,
+		  const DDVectorArguments & vArgs,
+		  const DDMapArguments & mArgs,
+		  const DDStringArguments & sArgs,
+		  const DDStringVectorArguments & vsArgs);
+  void execute();
+  std::string getMaterial(unsigned int i)   const {return materials_[i];}
+
+private:
+  void doLayers();
+  void doWedges();    
+  void doSens();
+
+  std::vector<double> quadMin_, quadMax_; 
+  int nmat_; // number of preshower layers
+  double thickness_; // overall thickness of the preshower envelope
+  double zlead1_, zlead2_, zfoam1_, zfoam2_;
+  std::vector<std::string> materials_; // materials of the presh-layers
+  std::vector<double> thickLayers_; 
+  std::vector<double> rminVec; 
+  std::vector<double> rmaxVec;
+  std::vector<double> micromodulesx;
+  std::vector<double> micromodulesy;
+  std::string   idNameSpace; //Namespace of this and ALL sub-parts
+  double waf_intra_col_sep, waf_inter_col_sep, waf_active, wedge_length, wedge_offset, zwedge_ceramic_diff, ywedge_ceramic_diff, absorbx, absorby, trabsorbx, trabsorby; 
+  int go;
+};
+
+#endif // DDEcalPreshowerAlgoTB_h
