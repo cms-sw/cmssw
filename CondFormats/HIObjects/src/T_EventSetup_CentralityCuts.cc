76a94335531ec8fb5a2ff490f13012d3fb05f9b2
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2007-11-06 13:01:37 +0000

    Heavy Ion Record and Heavy Ion Objects added.

---
+++ CondFormats/HIObjects/src/T_EventSetup_CentralityCuts.cc
@@ -0,0 +1,5 @@
+#include "CondFormats/HIObjects/interface/CentralityTable.h" 
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(CentralityTable);
+
---
+++ CondFormats/HIObjects/src/classes.h
@@ -0,0 +1,7 @@
+#include "CondFormats/HIObjects/interface/CentralityTable.h"
+#include <vector>
+
+namespace { namespace {
+std::vector<CentralityTable::Bins> dummy;
+}}
+
