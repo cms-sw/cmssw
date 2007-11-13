8227c9fb6036735988f4b95c0f989f1d97094e0d
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2007-11-13 23:52:39 +0000

    Generic parameters parser EMF based jet corrections

--- CondFormats/JetMETObjects/src/Simple3DMCJetCorrector.cc
+++ CondFormats/JetMETObjects/src/Simple3DMCJetCorrector.cc
@@ -1,48 +1,59 @@
 //
 // Original Author:  Fedor Ratnikov Dec 27, 2006
-// $Id: Simple3DMCJetCorrector.cc,v 1.4 2007/08/15 12:50:38 ratnik Exp $
+// $Id: Simple3DMCJetCorrector.cc,v 1.1 2007/11/01 21:50:30 fedor Exp $
 //
 // MC Jet Corrector
 //
 #include "CondFormats/JetMETObjects/interface/Simple3DMCJetCorrector.h"
 
+#include "CondFormats/JetMETObjects/interface/SimpleJetCorrectorParameters.h"
+
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
+
+#include "FWCore/Utilities/interface/Exception.h"
 
 #include "Math/PtEtaPhiE4D.h"
 #include "Math/LorentzVector.h"
 typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> > PtEtaPhiELorentzVectorD;
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > XYZTLorentzVectorD;
 
-class Simple3DMCJetCorrectorParameters {
-public:
-  Simple3DMCJetCorrectorParameters (const std::string& fDataFile);
-  ~Simple3DMCJetCorrectorParameters () {}
-  double correction (double fPt, double fEta, double fEmFraction) const;
-private:
-  std::vector<double> mPar;
-};
-
-Simple3DMCJetCorrectorParameters::Simple3DMCJetCorrectorParameters (const std::string& fDataFile) 
-{
-  std::ifstream in( fDataFile.c_str() );
-  std::string line;
-  while( std::getline( in, line)){
-    if(!line.size() || line[0]=='#') continue;
-    std::istringstream linestream(line);
-    double value;
-    while (linestream >> value) mPar.push_back (value);
-    break;  // only one parameters line is expected
-  }
-  if (mPar.size() < 20) {
-    std::cerr << "Simple3DMCJetCorrectorParameters-> Initialization error: less than 20 parameters provided" << std::endl;
-  }
+namespace {
+  const unsigned nParameters = 20;
 }
 
+Simple3DMCJetCorrector::Simple3DMCJetCorrector () 
+  : mParameters (0) 
+{}
 
-double Simple3DMCJetCorrectorParameters::correction (double fPt, double fEta, double fEmFraction) const {
+Simple3DMCJetCorrector::Simple3DMCJetCorrector (const std::string& fDataFile) 
+  : mParameters (new SimpleJetCorrectorParameters (fDataFile)) 
+{}
+
+Simple3DMCJetCorrector::~Simple3DMCJetCorrector () {
+  delete mParameters;
+}
+
+double Simple3DMCJetCorrector::correctionXYZTEmfraction (double fPx, double fPy, double fPz, double fE, double fEmFraction) const {
+  XYZTLorentzVectorD p4 (fPx, fPy, fPz, fE);
+  return correctionPtEtaEmfraction (p4.Pt(), p4.Eta(), fEmFraction);
+}
+
+double Simple3DMCJetCorrector::correctionEtEtaPhiPEmfraction (double fEt, double fEta, double fPhi, double fP, double fEmFraction) const {
+  double costhetainv = cosh (fEta);
+  return correctionPtEtaEmfraction (fP/costhetainv, fEta, fEmFraction);
+}
+
+double Simple3DMCJetCorrector::correctionPtEtaEmfraction (double fPt, double fEta, double fEmFraction) const {
+  unsigned band = mParameters->bandIndex (fEta);
+  if (band >= mParameters->size()) {
+    throw cms::Exception ("Simple3DMCJetCorrector") 
+      << "wrong band: " << band << ": only " <<  mParameters->size() << " is available";
+  }
+  const std::vector<float>& p = mParameters->record (band).parameters ();
+  if (p.size() != nParameters) {
+    throw cms::Exception ("Simple3DMCJetCorrector") 
+      << "wrong # of parameters: " << nParameters << " expected, " << p.size() << " got";
+  }
   double aeta = fabs(fEta);
   double aeta2 = aeta*aeta;
   double aeta3 = aeta2*aeta;
@@ -52,43 +63,17 @@ double Simple3DMCJetCorrectorParameters::correction (double fPt, double fEta, do
   double emf3 = emf2*emf;
   double emf4 = emf3*emf;
 
-  double p0 = mPar[0]*emf + 
-    mPar[1]*aeta2 + mPar[2]*emf*aeta + mPar[3]*emf2 + 
-    mPar[4]*emf3;
-  double p1 = mPar[5] + 
-    mPar[6]*aeta + mPar[7]*emf + 
-    mPar[8]*aeta2 + mPar[9]*aeta*emf + mPar[10]*emf2 + 
-    mPar[11]*aeta3 + mPar[12]*aeta2*emf + mPar[13]*aeta*emf2 + mPar[14]*emf3 + 
-    mPar[15]*aeta4 + mPar[16]*aeta3*emf + mPar[17]*aeta2*emf2 + mPar[18]*aeta*emf3 + mPar[19]*emf4;
+  double p0 = p[0]*emf + 
+    p[1]*aeta2 + p[2]*emf*aeta + p[3]*emf2 + 
+    p[4]*emf3;
+  double p1 = p[5] + 
+    p[6]*aeta + p[7]*emf + 
+    p[8]*aeta2 + p[9]*aeta*emf + p[10]*emf2 + 
+    p[11]*aeta3 + p[12]*aeta2*emf + p[13]*aeta*emf2 + p[14]*emf3 + 
+    p[15]*aeta4 + p[16]*aeta3*emf + p[17]*aeta2*emf2 + p[18]*aeta*emf3 + p[19]*emf4;
   double offset = p0 * (pow (fPt, p1) - 1.);
   double result = 1;
   // corrections valid in limited area only;
-  if (fPt > 0 && fPt < 300 && aeta < 2.5) result = 1. + offset/fPt;
-  return result;
+  return (fPt > 0 && fPt < 300) ? result = 1. + offset/fPt : 1.;
 }
 
-Simple3DMCJetCorrector::Simple3DMCJetCorrector () 
-  : mParameters (0) 
-{}
-
-Simple3DMCJetCorrector::Simple3DMCJetCorrector (const std::string& fDataFile) 
-  : mParameters (new Simple3DMCJetCorrectorParameters (fDataFile)) 
-{}
-
-Simple3DMCJetCorrector::~Simple3DMCJetCorrector () {
-  delete mParameters;
-}
-
-double Simple3DMCJetCorrector::correctionXYZTEmfraction (double fPx, double fPy, double fPz, double fE, double fEmFraction) const {
-  XYZTLorentzVectorD p4 (fPx, fPy, fPz, fE);
-  return correctionPtEtaPhiEEmfraction (p4.Pt(), p4.Eta(), p4.Phi(), p4.E(), fEmFraction);
-}
-
-double Simple3DMCJetCorrector::correctionPtEtaPhiEEmfraction (double fPt, double fEta, double fPhi, double fE, double fEmFraction) const {
-  return mParameters->correction (fPt, fEta, fEmFraction);
-}
-
-double Simple3DMCJetCorrector::correctionEtEtaPhiPEmfraction (double fEt, double fEta, double fPhi, double fP, double fEmFraction) const {
-  double costhetainv = cosh (fEta);
-  return correctionPtEtaPhiEEmfraction (fP/costhetainv, fEta, fPhi, fEt*costhetainv, fEmFraction);
-}
