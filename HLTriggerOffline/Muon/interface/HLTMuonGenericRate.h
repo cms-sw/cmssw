60cc6e2a437f4679fef66ee24db3b285c28e4002
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2007-11-14 14:00:07 +0000

    Adding Reference to RECO

--- HLTriggerOffline/Muon/interface/HLTMuonGenericRate.h
+++ HLTriggerOffline/Muon/interface/HLTMuonGenericRate.h
@@ -16,6 +16,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 #include "DataFormats/Common/interface/RefToBase.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
@@ -42,12 +43,12 @@ public:
 
 private:
   // Input from cfg file
-  edm::InputTag theL1CollectionLabel, theGenLabel;
+  edm::InputTag theL1CollectionLabel, theGenLabel, theRecoLabel;
   std::vector<edm::InputTag> theHLTCollectionLabels;
   double theL1ReferenceThreshold,theHLTReferenceThreshold;
   std::vector<double> theNSigmas;
   unsigned int theNumberOfObjects;
-  bool useMuonFromGenerator;
+  bool useMuonFromGenerator,useMuonFromReco;
   double theCrossSection;
   double theLuminosity;
   double thePtMin;
@@ -57,22 +58,34 @@ private:
   // Histograms
   TH1F* hL1eff;
   TH1F* hL1MCeff;
+  TH1F* hL1RECOeff;
   TH1F* hMCptnor;
   TH1F* hMCphinor;
   TH1F* hMCetanor;
+  TH1F* hRECOptnor;
+  TH1F* hRECOphinor;
+  TH1F* hRECOetanor;
   TH1F* hL1rate;
   TH1F* hL1pt;
-  TH1F* hL1eta;
-  TH1F* hL1phi;
+  TH1F* hL1etaMC;
+  TH1F* hL1phiMC;
+  TH1F* hL1etaRECO;
+  TH1F* hL1phiRECO;
+  TH1F* hSteps;
   std::vector <TH1F*> hHLTeff;
   std::vector <TH1F*> hHLTMCeff;
+  std::vector <TH1F*> hHLTRECOeff;
   std::vector <TH1F*> hHLTrate;
   std::vector <TH1F*> hHLTpt;
-  std::vector <TH1F*> hHLTeta;
-  std::vector <TH1F*> hHLTphi;
+  std::vector <TH1F*> hHLTetaMC;
+  std::vector <TH1F*> hHLTphiMC;
+  std::vector <TH1F*> hHLTetaRECO;
+  std::vector <TH1F*> hHLTphiRECO;
   HepMC::GenEvent::particle_const_iterator theAssociatedGenPart;
+  reco::TrackCollection::const_iterator theAssociatedRecoPart;
   const HepMC::GenEvent* evt;
   std::pair<double,double> getGenAngle(edm::RefToBase<reco::Candidate> candref, HepMC::GenEvent evt );
+  std::pair<double,double> getRecoAngle(edm::RefToBase<reco::Candidate> candref,reco::TrackCollection tracks );
   double theNumberOfEvents;
   TDirectory *ratedir;  
   TDirectory *distribdir;
