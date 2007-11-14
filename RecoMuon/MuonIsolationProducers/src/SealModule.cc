1e2b680191ac2262770892a8d35c6dea475d6540
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-11-14 02:12:27 +0000

    remove SumsProducer, replace with IsolatorResult producer

--- RecoMuon/MuonIsolationProducers/src/SealModule.cc
+++ RecoMuon/MuonIsolationProducers/src/SealModule.cc
@@ -5,6 +5,6 @@ DEFINE_SEAL_MODULE();
 #include "MuIsoDepositProducer.h"
 DEFINE_ANOTHER_FWK_MODULE(MuIsoDepositProducer);
 
-#include "MuIsolationSumInConesProducer.h"
-DEFINE_ANOTHER_FWK_MODULE(MuIsolationSumInConesProducer);
+#include "MuIsolatorResultProducer.h"
+DEFINE_ANOTHER_FWK_MODULE(MuIsolatorResultProducer);
 
