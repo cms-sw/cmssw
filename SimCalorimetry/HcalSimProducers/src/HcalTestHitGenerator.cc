4ff40716538464c520dda1e1ec2cd0952b327b71
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-11-14 04:35:56 +0000

    Add a test SimHit in each cell

---
+++ SimCalorimetry/HcalSimProducers/src/HcalTestHitGenerator.cc
@@ -0,0 +1,56 @@
+#include "SimCalorimetry/HcalSimProducers/src/HcalTestHitGenerator.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+#include "DataFormats/HcalDetId/interface/HcalZDCDetId.h"
+
+HcalTestHitGenerator::HcalTestHitGenerator(const edm::ParameterSet & ps)
+:  theBarrelSampling(ps.getParameter<edm::ParameterSet>("hb").getParameter<std::vector<double> >("samplingFactors")),
+   theEndcapSampling(ps.getParameter<edm::ParameterSet>("he").getParameter<std::vector<double> >("samplingFactors"))
+{
+}
+
+
+void HcalTestHitGenerator::getNoiseHits(std::vector<PCaloHit> & noiseHits)
+{
+
+  double e = 10.;
+
+  for(int i = 1; i <= 16; ++i)
+  {
+     HcalDetId detId(HcalBarrel, i, 1, 1);
+     PCaloHit hit(detId.rawId(), e/theBarrelSampling[i-1], 0., 0., 0);
+     noiseHits.push_back(hit);
+  }
+
+  // ring 16 is special
+  HcalDetId detId(HcalEndcap, 16, 1, 3);
+  PCaloHit hit(detId.rawId(), e/theEndcapSampling[0], 0., 0., 0);
+  noiseHits.push_back(hit);
+
+  for(int i = 17; i <= 29; ++i)
+  {
+     HcalDetId detId(HcalEndcap, i, 1, 1);
+     PCaloHit hit(detId.rawId(), e/theEndcapSampling[i-16], 0., 0., 0);
+     noiseHits.push_back(hit);
+  }
+
+  HcalDetId outerDetId(HcalOuter, 1, 1, 4);
+  PCaloHit outerHit(outerDetId.rawId(), 0.45, 0., 0., 0);
+
+  HcalDetId forwardDetId1(HcalForward, 30, 1, 1);
+  PCaloHit forwardHit1(forwardDetId1.rawId(), 35., 0., 0., 0);
+
+  HcalDetId forwardDetId2(HcalForward, 30, 1, 2);
+  PCaloHit forwardHit2(forwardDetId2.rawId(), 48., 0., 0., 0);
+
+  HcalZDCDetId zdcDetId(HcalZDCDetId::Section(2),true,1);
+  PCaloHit zdcHit(zdcDetId.rawId(), 50.0, 0.);
+
+  noiseHits.push_back(outerHit);
+  noiseHits.push_back(forwardHit1);
+  noiseHits.push_back(forwardHit2);
+  //noiseHits.push_back(zdcHit);
+
+}
+
+
