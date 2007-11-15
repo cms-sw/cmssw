d378ecdf45cd2db6ef4dc72ac40ade42c3d176d1
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-11-15 05:06:28 +0000

    Deal with when an ESPrefer name is passed in as 'name@prefer'

--- FWCore/ParameterSet/python/Modules.py
+++ FWCore/ParameterSet/python/Modules.py
@@ -57,8 +57,12 @@ class ESPrefer(_ConfigureComponent,_TypedParameterizable,_Unlabelable,_Labelable
         if name == '':
             name=self.type_()
         proc._placeESPrefer(name,self)
+    def moduleLabel_(self,myname):
+       # the C++ parser can give it a name like "label@prefer".  Get rid of that.
+       return myname.split('@')[0]
     def nameInProcessDesc_(self, myname):
-       return "esprefer_" + self.type_() + "@" + myname
+       # the C++ parser can give it a name like "label@prefer".  Get rid of that.
+       return "esprefer_" + self.type_() + "@" + myname.split('@')[0]
 
 class _Module(_ConfigureComponent,_TypedParameterizable,_Labelable,_Sequenceable):
     """base class for classes which denote framework event based 'modules'"""
