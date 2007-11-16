b3d6c450ffa237e9303bf50b478b6ea0946a989f
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2007-11-16 00:51:20 +0000

    instances of iso result producer to handle MapToTracks (old default) and VectorToCandView (new default) for muons

---
+++ RecoMuon/MuonIsolationProducers/plugins/MuIsoResultFromVectorToCandViewProducer.cc
@@ -0,0 +1,13 @@
+#include "MuIsolatorResultProducer.h"
+
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/MuIsoDeposit.h"
+#include "DataFormats/MuonReco/interface/MuIsoDepositFwd.h"
+
+typedef MuIsolatorResultProducer<reco::MuIsoDepositAssociationVectorToCandidateView> 
+MuIsoResultFromVectorToCandViewProducer;
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(MuIsoResultFromVectorToCandViewProducer);
