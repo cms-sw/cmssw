007f670dc172182f12a66e2744d2b8e016a071ab
William Badgett Jr <sha1-0342ea9cbf4e6781ff6fc4596018aec98179df76@cern.ch>
2007-11-16 02:25:43 +0000

    Testing code

---
+++ DataFormats/Scalers/test/ScalersTest.cpp
@@ -0,0 +1,44 @@
+//
+// File: ScalersTest.cpp
+//
+
+#include "DataFormats/Scalers/interface/L1TriggerScalers.h"
+#include "DataFormats/Scalers/interface/LumiScalers.h"
+#include "DataFormats/Scalers/interface/ScalersRaw.h"
+
+
+#include <iostream>
+#include <math.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+char * fileName = "scalers.dat";
+
+int main(int argc, char** argv)
+{
+  struct ScalersEventRecordRaw_v1 record;
+
+  int fd = open(fileName, O_RDONLY);
+  if ( fd > 0 )
+  {
+    int bytes = read(fd,&record,sizeof(record));
+    if ( bytes <= 0 )
+    {
+      printf("Error %s reading file %s\n", fileName,
+	     strerror(errno));
+    }
+    else
+    {
+      printf("Read %d bytes from %s\n", bytes, fileName);
+    }
+    close(fd);
+  }
+  else
+  {
+    printf("Error %s opening file %s\n", fileName,
+	   strerror(errno));
+  }
+  return 0;
+}
