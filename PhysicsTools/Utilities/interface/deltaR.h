bcd1435f78c13bd5a9e367923d0fe647ba29aab6
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-11-16 09:23:47 +0000

    fixed template implementation to allow use Geom::Phi<T>

--- PhysicsTools/Utilities/interface/deltaR.h
+++ PhysicsTools/Utilities/interface/deltaR.h
@@ -10,26 +10,26 @@
 
 namespace reco {
 
-  template <class T>
-  T deltaR2 (T eta1, T phi1, T eta2, T phi2) {
-    T deta = eta1 - eta2;
-    T dphi = deltaPhi (phi1, phi2);
+  template <class T1, class T2>
+  T1 deltaR2 (T1 eta1, T2 phi1, T1 eta2, T2 phi2) {
+    T1 deta = eta1 - eta2;
+    T2 dphi = deltaPhi(phi1, phi2);
     return deta*deta + dphi*dphi;
   }
   
-  template <class T>
-  T deltaR (T eta1, T phi1, T eta2, T phi2) {
-    return sqrt (deltaR2 (eta1, phi1, eta2, phi2));
+  template <class T1, class T2>
+  T1 deltaR (T1 eta1, T2 phi1, T1 eta2, T2 phi2) {
+    return sqrt(deltaR2 (eta1, phi1, eta2, phi2));
   }
   
   template<typename T1, typename T2>
-  double deltaR2( const T1 & t1, const T2 & t2 ) {
-    return deltaR2( t1.eta(), t1.phi(), t2.eta(), t2.phi() );
+  double deltaR2(const T1 & t1, const T2 & t2) {
+    return deltaR2(t1.eta(), t1.phi(), t2.eta(), t2.phi());
   } 
   
   template<typename T1, typename T2>
-  double deltaR( const T1 & t1, const T2 & t2 ) {
-    return deltaR( t1.eta(), t1.phi(), t2.eta(), t2.phi() );
+  double deltaR(const T1 & t1, const T2 & t2) {
+    return deltaR(t1.eta(), t1.phi(), t2.eta(), t2.phi());
   } 
 
 }
