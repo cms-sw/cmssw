4e13d4c8db9ef5e968e0b574c78e4f15d7479698
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2007-11-19 17:06:25 +0000

    Added Class to hold Reconstructed Centrality info in Heavy Ion Event

---
+++ DataFormats/HeavyIonEvent/src/classes.h
@@ -0,0 +1,11 @@
+#include "DataFormats/HeavyIonEvent/interface/Centrality.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
+namespace { namespace {
+
+reco::Centrality dummy1;
+edm::Wrapper<reco::Centrality> dummy2;
+
+}}
+
+
