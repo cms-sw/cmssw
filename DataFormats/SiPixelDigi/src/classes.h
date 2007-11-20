a79929e243fa09852703c08523a258b892ef4d43
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2007-11-20 11:43:06 +0000

    Update: now including the SiPixelCalibDigi classes in this package. This involved modifications of the classes_def.xml and classes.h files. No modifications to the PixelDigi formats were made.
    The SiPixelCalibDigi classes will only be used for calibration purposes. They contain calibration information for a collection of calibration points. Examples of possible use: Pixel Gain, S-Curve or Alive calibrations.

--- DataFormats/SiPixelDigi/src/classes.h
+++ DataFormats/SiPixelDigi/src/classes.h
@@ -3,12 +3,24 @@
 
 #include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
 #include "DataFormats/SiPixelDigi/interface/PixelDigiCollection.h"
+#include "DataFormats/SiPixelDigi/interface/SiPixelCalibDigi.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include <vector>
 
 namespace {
   namespace {
+    std::pair<short,short> patterncontainer;
+    SiPixelCalibDigi::datacontainer calibdatacontainer;
+    SiPixelCalibDigi calibdigiitself;
+    edm::Wrapper<SiPixelCalibDigi::datacontainer> calibdatacontainer0;
+    edm::Wrapper<std::vector<SiPixelCalibDigi::datacontainer> > calibdatacontainervec0;
+    edm::Wrapper<SiPixelCalibDigi> calibdigi;
+    edm::Wrapper<std::vector<SiPixelCalibDigi> > calibdigivec;
+    edm::Wrapper<edm::DetSet<SiPixelCalibDigi> > calibdigidetvec;
+    edm::Wrapper<std::vector<edm::DetSet<SiPixelCalibDigi> > > calibdigidetset;
+    edm::Wrapper<edm::DetSetVector<SiPixelCalibDigi> > calibdigidetsetvec;
+    
     edm::Wrapper<PixelDigi> zs0;
     edm::Wrapper<PixelDigiCollection> zsc0;
     edm::Wrapper< std::vector<PixelDigi>  > zs1;
