85917ee94e907a3fc3ee9635e1169b0432b055fd
roberfro <sha1-3ef59c6cd087c1ab5075a623fd6cc2bdbd41546d@cern.ch>
2007-11-20 11:07:40 +0000

    Import of NuclearInteraction DataFormat

--- DataFormats/VertexReco/src/classes.h
+++ DataFormats/VertexReco/src/classes.h
@@ -3,6 +3,7 @@
 #include "DataFormats/Common/interface/RefProd.h"
 #include "DataFormats/Common/interface/RefVector.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/NuclearInteraction.h"
 #include <vector>
 #include <utility>
 
@@ -14,5 +15,12 @@ namespace {
     edm::Ref<std::vector<reco::Vertex> > r1;
     edm::RefProd<std::vector<reco::Vertex> > rp1;
     edm::RefVector<std::vector<reco::Vertex> > rvv1;
+
+    reco::NuclearInteraction nrv1;
+    std::vector<reco::NuclearInteraction> nv1;
+    edm::Wrapper<std::vector<reco::NuclearInteraction> > nwc1;
+    edm::Ref<std::vector<reco::NuclearInteraction> > nr1;
+    edm::RefProd<std::vector<reco::NuclearInteraction> > nrp1;
+    edm::RefVector<std::vector<reco::NuclearInteraction> > nrvv1;
   }
 }
