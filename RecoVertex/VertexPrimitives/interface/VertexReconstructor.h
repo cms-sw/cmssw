83e0a85e901b9aa2211cc4f554391f445e67274a
Wolfgang Waltenberger <sha1-9c4ada266a8352c2428af94857b7ab82e25b3b37@cern.ch>
2007-11-20 09:04:10 +0000

    virtual method to include beamspot constraints added

--- RecoVertex/VertexPrimitives/interface/VertexReconstructor.h
+++ RecoVertex/VertexPrimitives/interface/VertexReconstructor.h
@@ -3,9 +3,10 @@
 
 #include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
 #include <vector>
 
-/** Abstract class for vertex reconstructors, 
+/** Abstract class for vertex reconstructors,
  *  i.e. objects reconstructing vertices using a set of TransientTracks
  */
 
@@ -18,9 +19,21 @@ public:
 
   /** Reconstruct vertices
    */
-  virtual 
-  std::vector<TransientVertex> 
-    vertices(const std::vector<reco::TransientTrack> &) const = 0; 
+  virtual
+  std::vector<TransientVertex>
+    vertices(const std::vector<reco::TransientTrack> &) const = 0;
+
+  /** Reconstruct vertices, exploiting the beamspot constraint
+   *  for the primary vertex.
+   *  This trivial implementation is overwritable by the
+   *  concrete reconstructors ...
+   */
+  virtual std::vector<TransientVertex>
+    vertices( const std::vector<reco::TransientTrack> & t, const
+              reco::BeamSpot & ) const
+  {
+     return vertices ( t );
+  }
 
   virtual VertexReconstructor * clone() const = 0;
 
