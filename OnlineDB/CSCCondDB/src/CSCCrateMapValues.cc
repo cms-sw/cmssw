764732e159bcc6cad1aca1237b64005b1a7e4234
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-11-20 17:07:15 +0000

    changed to have 540 chambers

--- OnlineDB/CSCCondDB/src/CSCCrateMapValues.cc
+++ OnlineDB/CSCCondDB/src/CSCCrateMapValues.cc
@@ -19,10 +19,11 @@ void CSCCrateMapValues::fillCrateMap()
   int chamberid;
   int crate_cscid;
 
+  /* This is version for 540 chambers. */
   for(i=1;i<=2;++i){
     for(j=1;j<=4;++j){
       if(j==1) r=3;
-      else if(j==4) r=1;
+      //else if(j==4) r=1;
       else r=2;
       for(k=1;k<=r;++k){
        if(j>1 && k==1) c=18;
