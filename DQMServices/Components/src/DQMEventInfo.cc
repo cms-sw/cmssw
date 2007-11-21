9e83fdc4765ad190d42fa9b45389a8ba1c39d900
Wade Cameron Fisher <sha1-d8bfdd9a9d128ce101927021e65c0a253faeb672@cern.ch>
2007-11-21 00:19:12 +0000

    Expanded the process timestamp into two pieces:
    ==>Process timestamp: UTC time in seconds since epoch of last event
    ==>Process latency: Elapsed time in seconds since last update

--- DQMServices/Components/src/DQMEventInfo.cc
+++ DQMServices/Components/src/DQMEventInfo.cc
@@ -2,9 +2,9 @@
  * \file DQMEventInfo.cc
  * \author M. Zanetti - CERN PH
  * Last Update:
- * $Date: 2007/11/17 13:44:39 $
- * $Revision: 1.6 $
- * $Author: ameyer $
+ * $Date: 2007/11/19 19:24:35 $
+ * $Revision: 1.7 $
+ * $Author: wfisher $
  *
  */
 
@@ -32,9 +32,8 @@ DQMEventInfo::DQMEventInfo(const ParameterSet& ps){
   
   parameters_ = ps;
   pEvent_ = 0;
-  timer_.start();
-  lastUpdateTime_=timer_.realTime();
-
+  gettimeofday(&currentTime_,NULL);
+  
   dbe_ = edm::Service<DaqMonitorBEInterface>().operator->();
   dbe_->setVerbose(1);
 
@@ -55,11 +54,13 @@ DQMEventInfo::DQMEventInfo(const ParameterSet& ps){
 
   //Process specific contents
   processTimeStamp_ = dbe_->bookFloat("processTimeStamp");
-  processTimeStamp_->Fill(timer_.realTime());
+  processTimeStamp_->Fill(getUTCtime(&currentTime_));
+  processLatency_ = dbe_->bookFloat("processLatency");
+  processTimeStamp_->Fill(-1);
   processEvents_ = dbe_->bookInt("processedEvents");
   processEvents_->Fill(pEvent_);
   nUpdates_= dbe_->bookInt("processUpdates");
-  nUpdates_->Fill(0);
+  nUpdates_->Fill(-1);
   
 
   //Static Contents
@@ -86,12 +87,18 @@ void DQMEventInfo::analyze(const Event& e, const EventSetup& c){
   eventId_->Fill(e.id().event());
   eventTimeStamp_->Fill(e.time().value());
 
-
   pEvent_++;
   processEvents_->Fill(pEvent_);
-  
-  processTimeStamp_->Fill(timer_.realTime()-lastUpdateTime_);
-  lastUpdateTime_=timer_.realTime();
- //alternatively can use timer_.cpuTime() for system clock timestamp
 
+  lastUpdateTime_=currentTime_;
+  gettimeofday(&currentTime_,NULL);  
+  processTimeStamp_->Fill(getUTCtime(&currentTime_));
+  processLatency_->Fill(getUTCtime(&lastUpdateTime_,&currentTime_));
+
+}
+
+double DQMEventInfo::getUTCtime(timeval* a, timeval* b){
+  double deltaT=(*a).tv_sec*1000.0+(*a).tv_usec/1000.0;
+  if(b!=NULL) deltaT=(*b).tv_sec*1000.0+(*b).tv_usec/1000.0 - deltaT;
+  return deltaT/1000.0;
 }
--- DQMServices/Components/src/DQMEventInfo.h
+++ DQMServices/Components/src/DQMEventInfo.h
@@ -4,8 +4,8 @@
 /*
  * \file DQMEventInfo.h
  *
- * $Date: 2007/11/15 23:09:41 $
- * $Revision: 1.2 $
+ * $Date: 2007/11/19 19:24:58 $
+ * $Revision: 1.3 $
  * \author M. Zanetti - INFN Padova
  *
 */
@@ -16,7 +16,6 @@
 #include <FWCore/Framework/interface/Event.h>
 #include <FWCore/Framework/interface/MakerMacros.h>
 #include <FWCore/ParameterSet/interface/ParameterSet.h>
-#include <FWCore/Utilities/interface/CPUTimer.h>
 #include <FWCore/ServiceRegistry/interface/Service.h>
 
 #include <DQMServices/Core/interface/DaqMonitorBEInterface.h>
@@ -29,6 +28,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sys/time.h>
 
 class DQMEventInfo: public edm::EDAnalyzer{
 
@@ -48,28 +48,37 @@ protected:
 
 private:
 
+  double getUTCtime(timeval* a, timeval* b = NULL);
+
   DaqMonitorBEInterface* dbe_;
 
   edm::ParameterSet parameters_;
-  edm::CPUTimer timer_;
-  float lastUpdateTime_;
+  timeval currentTime_, lastUpdateTime_;
   int pEvent_;
 
+  //////////////////////////////////////////////////////////////////
+  ///These MEs are filled with the info from the most recent event 
+  ///   by the module
+  //////////////////////////////////////////////////////////////////
   MonitorElement * runId_;
   MonitorElement * eventId_;
   MonitorElement * lumisecId_;
   MonitorElement * eventTimeStamp_;
   
-  MonitorElement * nUpdates_;
-  MonitorElement * processId_;
-  MonitorElement * processTimeStamp_;
-  MonitorElement * processEvents_;
-  MonitorElement * hostName_;
-  MonitorElement * processName_;
-  MonitorElement * workingDir_;
-  MonitorElement * cmsswVer_;
-  MonitorElement * dqmPatch_;
-  MonitorElement * errSummary_;
+  //////////////////////////////////////////////////////////////////
+  ///These MEs are either static or updated upon each analyze() call
+  //////////////////////////////////////////////////////////////////
+  MonitorElement * nUpdates_;  ///Number of collector updates (TBD)
+  MonitorElement * processId_;  ///The PID associated with this job
+  MonitorElement * processTimeStamp_;  ///The UTC time of the last event
+  MonitorElement * processLatency_;  ///Time elapsed since the last event
+  MonitorElement * processEvents_;  ///# of event processed so far
+  MonitorElement * hostName_;  ///Hostname of the local machine
+  MonitorElement * processName_;  ///DQM "name" of the job (eg, Hcal or DT)
+  MonitorElement * workingDir_;  ///Current working directory of the job
+  MonitorElement * cmsswVer_;  ///CMSSW version run for this job
+  MonitorElement * dqmPatch_;  ///DQM patch version for this job
+  MonitorElement * errSummary_;  ///Subdetector-specific error summary (float)
 
 };
 
