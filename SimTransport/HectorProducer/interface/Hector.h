2f9fb5cfa44b6e2c13b9325e006c2ce904d81a32
Alexandr Zhokin <sha1-3934d39e6468375f9412692c75d9c3501c9552f4@cern.ch>
2007-11-21 13:11:37 +0000

    comment some methods

--- SimTransport/HectorProducer/interface/Hector.h
+++ SimTransport/HectorProducer/interface/Hector.h
@@ -12,7 +12,6 @@
 #include "H_BeamLine.h"
 #include "H_RecRPObject.h"
 #include "H_BeamParticle.h"
-
 #include <string>
 
 class Hector {
@@ -37,61 +36,61 @@ public:
   void reconstruct();
 
   /*!Returns the number of the particles in a beamline*/
-  unsigned int getNp() const { return m_beamPart.size(); }
+  // unsigned int getNp() const { return m_beamPart.size(); }
   /*!Return \a true if the particle have interacted with something in a beamline nad \a false if not.
     \param part_n code of a particle (HepMC::GenParticle::barcode())
    */
-  bool isStopped( unsigned int part_n ) const;
+  //bool isStopped( unsigned int part_n ) const;
   /*!Returns the particle \a x coordinate at \a z position in a beamline
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  double getX( double z, unsigned int part_n ) const;
+  //double getX( double z, unsigned int part_n ) const;
   /*!Returns the particle \a y coordinate at \a z position in a beamline
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  double getY( double z, unsigned int part_n ) const;
+  //double getY( double z, unsigned int part_n ) const;
   //
   /*!Returns reconstructed in RP420 x coordinate of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getSimIPX( unsigned int part_n ) const;
+  //float getSimIPX( unsigned int part_n ) const;
   /*!Returns reconstructed in RP420 y coordinate of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getSimIPY( unsigned int part_n ) const;
+  //float getSimIPY( unsigned int part_n ) const;
   /*!Returns reconstructed in RP420 \a tx (\f$\theta_x\f$) of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getSimIPTX( unsigned int part_n ) const;
+  //float getSimIPTX( unsigned int part_n ) const;
   /*!Returns reconstructed in RP420 \a ty (\f$\theta_y\f$)of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getSimIPTY( unsigned int part_n ) const;
+  //float getSimIPTY( unsigned int part_n ) const;
   /*!Returns reconstructed in RP420 dE ( 7000. - E ) of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getSimIPdE( unsigned int part_n ) const;
+  //float getSimIPdE( unsigned int part_n ) const;
   //
   /*!Returns generated x coordinate of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getGenIPX( unsigned int part_n ) const;
+  //loat getGenIPX( unsigned int part_n ) const;
   /*!Returns generated y coordinate of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getGenIPY( unsigned int part_n ) const;
+  //float getGenIPY( unsigned int part_n ) const;
   /*!Returns generated \a tx (\f$\theta_x\f$) of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getGenIPTX( unsigned int part_n ) const;
+  //float getGenIPTX( unsigned int part_n ) const;
   /*!Returns generated \a ty (\f$\theta_y\f$) of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getGenIPTY( unsigned int part_n ) const;
+  //float getGenIPTY( unsigned int part_n ) const;
   /*!Returns generated energy of particle \a part_n at the IP
     \param part_n code of a particle (HepMC::GenParticle::barcode())
   */
-  float getGenIPE( unsigned int part_n ) const;
+  //float getGenIPE( unsigned int part_n ) const;
 
   /*!Returns the direction of fly for the particle \a n, 1 in case of forward, -1 in case of backward
     \param part_n code of a particle (HepMC::GenParticle::barcode())
@@ -128,18 +127,22 @@ private:
   float m_shiftY;
   float m_shiftZ;
 
+  
+  /*
   std::map<unsigned int, float> m_tx0;
   std::map<unsigned int, float> m_ty0;
   std::map<unsigned int, float> m_x0;
   std::map<unsigned int, float> m_y0;
   std::map<unsigned int, float> m_sim_e;
+*/
 
+  /*
   std::map<unsigned int, float> m_gen_x;
   std::map<unsigned int, float> m_gen_y;
   std::map<unsigned int, float> m_gen_tx;
   std::map<unsigned int, float> m_gen_ty;
   std::map<unsigned int, float> m_gen_e;
-
+*/
   double m_IPx;
   double m_IPy;
   double m_IPz;
@@ -149,11 +152,11 @@ private:
   H_BeamLine * m_beamline1;
   H_BeamLine * m_beamline2;
   //
-  H_RecRPObject * m_rp220_f;
-  H_RecRPObject * m_rp220_b;
+//  H_RecRPObject * m_rp220_f;
+//  H_RecRPObject * m_rp220_b;
 
-  H_RecRPObject * m_rp420_f;
-  H_RecRPObject * m_rp420_b;
+    H_RecRPObject * m_rp420_f;
+    H_RecRPObject * m_rp420_b;
 
   std::map<unsigned int, H_BeamParticle*> m_beamPart;
   std::map<unsigned int, int> m_direct;
