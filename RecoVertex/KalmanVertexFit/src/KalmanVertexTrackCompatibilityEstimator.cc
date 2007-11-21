ff2133d1465e5216996fdbdb513d7f6d3c7b21c7
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2007-11-21 09:25:38 +0000

    More efficienct if track is present in vertex

--- RecoVertex/KalmanVertexFit/src/KalmanVertexTrackCompatibilityEstimator.cc
+++ RecoVertex/KalmanVertexFit/src/KalmanVertexTrackCompatibilityEstimator.cc
@@ -85,9 +85,11 @@ KalmanVertexTrackCompatibilityEstimator::estimateFittedTrack
   //remove track from the vertex using the vertex updator
   // Using the update instead of the remove methode, we can specify a weight which
   // is different than then one which the vertex track has been defined with.
-  CachingVertex rVert = updator.remove(v, track);
+  //CachingVertex rVert = updator.remove(v, track);
   RefCountedVertexTrack newSmoothedTrack = trackUpdator.update(v, track);
-  return estimateDifference(v,rVert,newSmoothedTrack);
+//   cout << newSmoothedTrack->smoothedChi2()<<" "<<estimateDifference(v,rVert,newSmoothedTrack)<<endl;
+//   return estimateDifference(v,rVert,newSmoothedTrack);
+  return newSmoothedTrack->smoothedChi2();
 }
 
 // method calculating track<-->vertex compatibility
