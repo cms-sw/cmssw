f640f10b47aac3973858383ac297c9f41c7cf479
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2007-11-21 19:45:24 +0000

    added dependency of trackingComponentsRecord to TrackerGeometry for lowPt tracking

--- TrackingTools/Records/interface/TrackingComponentsRecord.h
+++ TrackingTools/Records/interface/TrackingComponentsRecord.h
@@ -3,15 +3,15 @@
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "FWCore/Framework/interface/DependentRecordImplementation.h"
-//#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/Records/interface/GlobalTrackingGeometryRecord.h"
+//#include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"                
 
 #include "boost/mpl/vector.hpp"
 
 
 class  TrackingComponentsRecord: public edm::eventsetup::DependentRecordImplementation<TrackingComponentsRecord,
-  boost::mpl::vector<IdealMagneticFieldRecord> > {};
+  boost::mpl::vector<IdealMagneticFieldRecord, GlobalTrackingGeometryRecord> > {};
 
 #endif 
 
