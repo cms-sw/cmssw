43c9a690e476718bcb868bcf0fa7cf69d62a6188
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-11-22 00:18:11 +0000

    fix: adding EcalMaxSampleUncalibRecHitProducer module definition and include (from S. Cooper)

--- RecoLocalCalo/EcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/EcalRecProducers/src/SealModule.cc
@@ -3,6 +3,7 @@
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalWeightUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalAnalFitUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalFixedAlphaBetaFitUncalibRecHitProducer.h"
+#include "RecoLocalCalo/EcalRecProducers/interface/EcalMaxSampleUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/ESRecHitProducer.h"
 
@@ -10,5 +11,7 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalWeightUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalAnalFitUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalFixedAlphaBetaFitUncalibRecHitProducer );
+DEFINE_ANOTHER_FWK_MODULE( EcalMaxSampleUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( ESRecHitProducer );
+
