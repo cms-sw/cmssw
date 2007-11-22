a219c8b2ae870c78cb2dfd50c99bedcb67f0b2f5
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2007-11-22 17:19:03 +0000

    Fixing stupid bug in "bad module aware" grouped ckf (1_7_X series)

--- RecoTracker/CkfPattern/src/TrajectorySegmentBuilder.cc
+++ RecoTracker/CkfPattern/src/TrajectorySegmentBuilder.cc
@@ -1,9 +1,11 @@
 
 #include "RecoTracker/CkfPattern/interface/TrajectorySegmentBuilder.h"
 
+//B.M. #include "CommonDet/BasicDet/interface/RecHitEqualByChannels.h"
+//B.M. #include "RecoTracker/CkfPattern/interface/TrajectoryMeasurementEqualByHit.h"
 #include "RecoTracker/CkfPattern/src/RecHitIsInvalid.h"
-#include "TrackingTools/PatternTools/interface/TempTrajectory.h"
-
+#include "RecoTracker/CkfPattern/interface/TempTrajectory.h"
+//B.M. #include "Utilities/Notification/interface/TimingReport.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
 #include "TrackingTools/KalmanUpdators/interface/KFUpdator.h"
 #include "TrackingTools/PatternTools/interface/MeasurementEstimator.h"
@@ -12,6 +14,7 @@
 #include "TrackingTools/MeasurementDet/interface/TrajectoryMeasurementGroup.h"
 #include "TrackingTools/DetLayers/interface/DetGroup.h"
 #include "TrackingTools/DetLayers/interface/DetLayer.h"
+//B.M.#include "CommonDet/PatternPrimitives/interface/PropagationDirection.h"
 #include "RecoTracker/CkfPattern/src/TrajectoryLessByFoundHits.h"
 #include "TrackingTools/PatternTools/interface/TrajectoryStateUpdator.h"
 #include "TrackingTools/DetLayers/interface/GeomDetCompatibilityChecker.h"
@@ -326,7 +329,7 @@ TrajectorySegmentBuilder::updateWithInvalidHit (TempTrajectory& traj,
 	//
 	ConstRecHitPointer hit = im->recHit();
 	//if ( hit->isValid() )  break;                          // gpetrucc: not bad module aware
-	if ( hit->getType() == TrackingRecHit::missing )  break; // let's try this
+	if ( hit->getType() != TrackingRecHit::missing )  break; // let's try this
 
 	//
 	// check, if the extrapolation traverses the Det
