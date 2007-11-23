636f51238a5387f5e8af0fe486d96fc13f998b58
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-11-23 15:06:41 +0000

    removed external dependency

--- DataFormats/RecoCandidate/src/classes.h
+++ DataFormats/RecoCandidate/src/classes.h
@@ -13,6 +13,7 @@
 #include "DataFormats/RecoCandidate/interface/RecoEcalCandidateIsolation.h"
 #include "DataFormats/RecoCandidate/interface/FitResult.h"
 #include "DataFormats/RecoCandidate/interface/CaloRecHitCandidate.h"
+#include "DataFormats/RecoCandidate/interface/TrackAssociation.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/RecoCandidate/interface/TrackCandidateAssociation.h"
 #include "DataFormats/HcalDetId/interface/HcalSubdetector.h" 
@@ -26,7 +27,6 @@
 #include "DataFormats/Common/interface/AssociationMap.h"
 #include "DataFormats/Common/interface/OneToValue.h"
 #include "DataFormats/Common/interface/OneToManyWithQuality.h"
-#include "SimTracker/TrackAssociation/interface/TrackAssociatorBase.h"
 
 namespace {
   namespace {
@@ -59,14 +59,12 @@ namespace {
     edm::Wrapper<reco::FitResultCollection> wfr1;
     edm::Wrapper<reco::TrackCandidateAssociation> tca1;
 
-    reco::SimToRecoCollection ii1;
     edm::Wrapper<reco::SimToRecoCollection > ii2;
     reco::SimToRecoAssociation ii3;
     reco::SimToRecoAssociationRef ii4;
     reco::SimToRecoAssociationRefProd ii5;
     reco::SimToRecoAssociationRefVector ii6;
 
-    reco::RecoToSimCollection jj1;
     edm::Wrapper<reco::RecoToSimCollection > jj2;
     reco::RecoToSimAssociation jj3;
     reco::RecoToSimAssociationRef jj4;
