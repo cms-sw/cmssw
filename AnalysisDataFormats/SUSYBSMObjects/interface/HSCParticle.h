3629cbaaaf0a00afa72093e0fed70d1f33b82ffc
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2007-11-26 08:29:49 +0000

    add HSCP classes

---
+++ AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h
@@ -0,0 +1,64 @@
+#ifndef HSCParticle_H
+#define HSCParticle_H
+#include "DataFormats/Common/interface/AssociationVector.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include <vector>
+
+namespace susybsm {
+ class DriftTubeTOF
+  {
+   public:
+     float invBeta;
+     float invBetaErr;
+     float invBetaFree;
+     float invBetaFreeErr;
+     float vertexTimeFree;
+     float vertexTimeFreeErr;
+     int nStations;
+     int nHits;
+  };
+
+
+ typedef  edm::AssociationVector<reco::MuonRefProd,std::vector<DriftTubeTOF> >  MuonTOFCollection;
+ typedef  MuonTOFCollection::value_type MuonTOF;
+ typedef  edm::Ref<MuonTOFCollection> MuonTOFRef;
+ typedef  edm::RefProd<MuonTOFCollection> MuonTOFRefProd;
+ typedef  edm::RefVector<MuonTOFCollection> MuonTOFRefVector;
+
+class DeDxBeta {
+   public:
+      reco::TrackRef track;
+      float invBeta2;
+      float invBeta2Fit; 
+      int nDeDxHits;
+}; 
+
+
+ class HSCParticle
+  {
+   public:
+      MuonTOF  dt;
+      bool hasDt;
+      DeDxBeta tk;
+      bool hasTk;
+      float massTk()const {return tk.track->p()*sqrt(tk.invBeta2-1);}
+      float massDt()const {return dt.first->track()->p()*sqrt(dt.second.invBeta*dt.second.invBeta-1);}
+      float massDtComb()const {return dt.first->combinedMuon()->p()*sqrt(dt.second.invBeta*dt.second.invBeta-1);}
+      float massDtSta()const {return dt.first->standAloneMuon()->p()*sqrt(dt.second.invBeta*dt.second.invBeta-1);}
+      float massDtAssoTk()const {return tk.track->p()*sqrt(dt.second.invBeta*dt.second.invBeta-1);}
+      float massTkAssoComb() const {return dt.first->combinedMuon()->p()*sqrt(tk.invBeta2-1);}
+      float compatibility() const {return (sqrt(tk.invBeta2)-dt.second.invBeta)/(sqrt(tk.invBeta2/tk.nDeDxHits)*0.1+dt.second.invBeta*0.1);}
+
+  };
+
+typedef  std::vector<HSCParticle> HSCParticleCollection;
+typedef  edm::Ref<HSCParticleCollection> HSCParticleRef;
+typedef  edm::RefProd<HSCParticleCollection> HSCParticleRefProd;
+typedef  edm::RefVector<HSCParticleCollection> HSCParticleRefVector;
+ 
+ 
+}
+
+#endif
---
+++ AnalysisDataFormats/SUSYBSMObjects/src/classes.h
@@ -0,0 +1,24 @@
+#include "AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h"
+namespace {
+ namespace {
+  susybsm::HSCParticle pa;
+  susybsm::DriftTubeTOF dtitof;
+
+  susybsm::MuonTOFCollection mtc; 
+  susybsm::MuonTOF mt;
+  susybsm::MuonTOFRef mtr;
+  susybsm::MuonTOFRefProd mtrp;
+  susybsm::MuonTOFRefVector mtrv;
+  edm::Wrapper<susybsm::MuonTOFCollection> wr;
+  std::vector<std::pair<edm::Ref<std::vector<reco::Muon>,reco::Muon,edm::refhelper::FindUsingAdvance<std::vector<reco::Muon>,reco::Muon> >,susybsm::DriftTubeTOF> > a;
+  std::vector<susybsm::DriftTubeTOF> b;
+
+  susybsm::DeDxBeta dedxbeta;
+  susybsm::HSCParticleCollection hc;
+  susybsm::HSCParticleRef hr;
+  susybsm::HSCParticleRefProd hp;
+  susybsm::HSCParticleRefVector hv;
+  edm::Wrapper<susybsm::HSCParticleCollection> wr1;
+  
+ }
+}
