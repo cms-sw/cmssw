02dfd38122474d1c43f5a2ce2dad273ee71a93de
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2007-11-26 19:43:37 +0000

    add cfi, add Reset() on endRun, cfg add tweaks

--- DQM/TrigXMonitor/src/HLTScalers.cc
+++ DQM/TrigXMonitor/src/HLTScalers.cc
@@ -1,6 +1,9 @@
-// $Id: HLTScalers.cc,v 1.1 2007/11/26 16:37:50 wittich Exp $
+// $Id: HLTScalers.cc,v 1.2 2007/11/26 18:24:21 wittich Exp $
 // 
 // $Log: HLTScalers.cc,v $
+// Revision 1.2  2007/11/26 18:24:21  wittich
+// fix bug in cfg handling
+//
 // Revision 1.1  2007/11/26 16:37:50  wittich
 // Prototype HLT scaler information.
 //
@@ -51,7 +54,7 @@ HLTScalers::HLTScalers(const edm::ParameterSet &ps):
     monitorDaemon_ = true;
   }
   if (dbe_ ) {
-    dbe_->setCurrentFolder("L1TMonitor/HLTScalers");
+    dbe_->setCurrentFolder("L1T/HLTScalers");
   }
 
 }
@@ -68,7 +71,7 @@ void HLTScalers::beginJob(const edm::EventSetup& c)
     if ( verbose_ ) {
       dbe_->setVerbose(1);
     }
-    dbe_->setCurrentFolder("TXMonitor/HLTScalers");
+    dbe_->setCurrentFolder("L1T/HLTScalers");
 
     // need to get these dynamically
     int maxModules = 200;
@@ -104,7 +107,8 @@ void HLTScalers::analyze(const edm::Event &e, const edm::EventSetup &c)
     std::cout << "HLTScalers::analyze: npath = " << npath << std::endl;
   }
 
-  // this is a scaler at the beginning of a run. I hc
+  // this is a scaler at the beginning of a run. I should
+  // also dynamically set up the size of the histogram here.
   if (resetMe_ ) {
     if ( verbose_) {
       std::cout << "HLTScalers: new run: " << std::endl;
@@ -114,6 +118,8 @@ void HLTScalers::analyze(const edm::Event &e, const edm::EventSetup &c)
 	;
       }
     }
+    scalers_->Reset();
+    detailedScalers_->Reset();
     resetMe_ = false;
   }
   int nm = 0;
