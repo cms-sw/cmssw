a645ecd3e8c70e8cbe6710d376140840f159713e
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-11-27 22:53:22 +0000

    import of class to translate     int fed_id <-> string ECAL_slice_name

---
+++ CaloOnlineTools/EcalTools/interface/EcalFedMap.h
@@ -0,0 +1,21 @@
+#include <map>
+#include <string>
+
+class EcalFedMap{
+
+ public: 
+
+  EcalFedMap();
+  ~EcalFedMap();
+  int getFedFromSlice(std::string);
+  std::string getSliceFromFed(int);
+
+ private:
+  // use:
+  // #include <boost/bimap.hpp>
+  // bimap< int, std::string > bm; 
+  // when available
+
+  std::map<int,  std::string> fedToSliceMap_;
+  std::map<std::string, int>  sliceToFedMap_;
+};
