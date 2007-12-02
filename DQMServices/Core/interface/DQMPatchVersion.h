5646ffae10ed51a40eb56dd52fa394e3e8d8173c
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-12-02 16:18:56 +0000

    increment Patch version

--- DQMServices/Core/interface/DQMPatchVersion.h
+++ DQMServices/Core/interface/DQMPatchVersion.h
@@ -4,12 +4,25 @@
 #include <string>
 
 namespace dqm {
-  std::string DQMPatchVersion = "01" ;
+  std::string DQMPatchVersion = "02" ;
 }
 #endif
 
 
-/* 01 (on top of 171)
+/* 171:01 
+ -- List of tags in  DQMPATCH:01 (and submitted for CMSSW_1_7_2)
 cvs co -r V00-05-11 DQMServices/Core
-cvs co -r V00-05-11 DQMServices/CoreROOT
+cvs co -r V00-05-11 DQMSerivces/CoreROOT
+cvs co -r V00-05-11 DQMSerivces/Components
+cvs co -r V00-05-11 DQMSerivces/Examples
+cvs co -r V00-00-10 DQM/Integration
+cvs co -r V00-07-04 DQM/HcalMonitorClient
+cvs co -r V00-07-04 DQM/HcalMonitorTasks
+cvs co -r V00-07-05 DQM/HcalMonitorModule 
+cvs co -r V00-06-00 DQM/RPCMonitorDigi
+# -- List of tags in  DQMPATCH:01 (and NOT submitted for CMSSW_1_7_2)
+cvs co -r V00-16-05 DQM/DTMonitorModule
+cvs co -r V00-03-04 DQM/DTMonitorClient
+cvs co -r V02-00-03 DQM/CSCMonitorModule 
+cvs co -r V01-08-10 EventFilter/CSCRawToDigi 
 */
