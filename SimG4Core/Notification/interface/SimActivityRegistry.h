2d3a1726ca7521d993cb7d2a99a64e1f7efc72df
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-12-02 05:17:47 +0000

    removed use of sigc++

--- SimG4Core/Notification/interface/SimActivityRegistry.h
+++ SimG4Core/Notification/interface/SimActivityRegistry.h
@@ -16,18 +16,16 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Sun Nov 13 11:43:40 EST 2005
-// $Id: SimActivityRegistry.h,v 1.5 2006/06/23 23:55:24 valya Exp $
+// $Id: SimActivityRegistry.h,v 1.6 2007/10/24 17:16:51 chrjones Exp $
 //
 
 // system include files
 #include "boost/bind.hpp"
 #include "boost/mem_fn.hpp"
 
-#include "sigc++/signal.h"
-#include "sigc++/bind.h"
-
 // user include files
-#include "SimG4Core/Notification/interface/SimSlotAdapter.h"
+#include "SimG4Core/Notification/interface/Signaler.h"
+
 
 // forward declarations
 class BeginOfJob;
@@ -41,7 +39,7 @@ class EndOfTrack;
 class DDDWorld;
 class G4Step;
 
-#define SAR_CONNECT_METHOD(signal) template <class TFunc> void connect(SimSlotAdapter<const signal*,TFunc>* iObject) { watch ## signal (boost::bind(boost::mem_fn(&SimSlotAdapter<const signal*,TFunc>::slot), iObject, _1)); }
+#define SAR_CONNECT_METHOD(signal) void connect(Observer<const signal*>* iObject) { watch ## signal (iObject); }
 
 class SimActivityRegistry
 {
@@ -50,63 +48,63 @@ class SimActivityRegistry
       SimActivityRegistry() {}
       //virtual ~SimActivityRegistry();
 
-      typedef sigc::signal< void, const BeginOfJob*> BeginOfJobSignal;
+      typedef sim_act::Signaler<BeginOfJob> BeginOfJobSignal;
       BeginOfJobSignal beginOfJobSignal_;
       void watchBeginOfJob(const BeginOfJobSignal::slot_type& iSlot){
          beginOfJobSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(BeginOfJob)
 
-      typedef sigc::signal< void, const DDDWorld*> DDDWorldSignal;
+      typedef sim_act::Signaler<DDDWorld> DDDWorldSignal;
       DDDWorldSignal dddWorldSignal_;
       void watchDDDWorld(const DDDWorldSignal::slot_type& iSlot){
          dddWorldSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(DDDWorld)
 
-      typedef sigc::signal< void, const BeginOfRun*> BeginOfRunSignal;
+      typedef sim_act::Signaler<BeginOfRun> BeginOfRunSignal;
       BeginOfRunSignal beginOfRunSignal_;
       void watchBeginOfRun(const BeginOfRunSignal::slot_type& iSlot){
          beginOfRunSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(BeginOfRun)
 
-      typedef sigc::signal< void, const BeginOfEvent*> BeginOfEventSignal;
+      typedef sim_act::Signaler<BeginOfEvent> BeginOfEventSignal;
       BeginOfEventSignal beginOfEventSignal_;
       void watchBeginOfEvent(const BeginOfEventSignal::slot_type& iSlot){
          beginOfEventSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(BeginOfEvent)
 
-      typedef sigc::signal< void, const BeginOfTrack*> BeginOfTrackSignal;
+      typedef sim_act::Signaler<BeginOfTrack> BeginOfTrackSignal;
       BeginOfTrackSignal beginOfTrackSignal_;
       void watchBeginOfTrack(const BeginOfTrackSignal::slot_type& iSlot){
          beginOfTrackSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(BeginOfTrack)
       
-      typedef sigc::signal< void, const G4Step*> G4StepSignal;
+      typedef sim_act::Signaler<G4Step> G4StepSignal;
       G4StepSignal g4StepSignal_;
       void watchG4Step(const G4StepSignal::slot_type& iSlot){
          g4StepSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(G4Step)
          
-      typedef sigc::signal< void, const EndOfRun*> EndOfRunSignal;
+      typedef sim_act::Signaler<EndOfRun> EndOfRunSignal;
       EndOfRunSignal endOfRunSignal_;
       void watchEndOfRun(const EndOfRunSignal::slot_type& iSlot){
          endOfRunSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(EndOfRun)
          
-      typedef sigc::signal< void, const EndOfEvent*> EndOfEventSignal;
+      typedef sim_act::Signaler<EndOfEvent> EndOfEventSignal;
       EndOfEventSignal endOfEventSignal_;
       void watchEndOfEvent(const EndOfEventSignal::slot_type& iSlot){
          endOfEventSignal_.connect(iSlot);
       }
       SAR_CONNECT_METHOD(EndOfEvent)
          
-      typedef sigc::signal< void, const EndOfTrack*> EndOfTrackSignal;
+      typedef sim_act::Signaler<EndOfTrack> EndOfTrackSignal;
       EndOfTrackSignal endOfTrackSignal_;
       void watchEndOfTrack(const EndOfTrackSignal::slot_type& iSlot){
          endOfTrackSignal_.connect(iSlot);
