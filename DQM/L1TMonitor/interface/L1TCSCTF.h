53d23c0384ebff9c95c05e4690c0565ce9e14c9d
Jeffrey Berryhill <sha1-962c16289a65616d7050fdaaf5fa06f1f8225240@cern.ch>
2007-12-03 14:24:09 +0000

    update for default gren cfg

--- DQM/L1TMonitor/interface/L1TCSCTF.h
+++ DQM/L1TMonitor/interface/L1TCSCTF.h
@@ -4,8 +4,8 @@
 /*
  * \file L1TCSCTF.h
  *
- * $Date: 2007/07/19 16:48:23 $
- * $Revision: 1.3 $
+ * $Date: 2007/09/28 07:50:40 $
+ * $Revision: 1.4 $
  * \author J. Berryhill
  *
 */
@@ -29,6 +29,9 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "DataFormats/L1GlobalMuonTrigger/interface/L1MuRegionalCand.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTCand.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTExtendedCand.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTReadoutCollection.h"
 
 #include <iostream>
 #include <fstream>
@@ -62,57 +65,20 @@ private:
   // ----------member data ---------------------------
   DaqMonitorBEInterface * dbe;
 
-  MonitorElement* csctfetavalue;
-  MonitorElement* csctfphivalue;
-  MonitorElement* csctfptvalue;
-  MonitorElement* csctfptpacked;
-  MonitorElement* csctfquality;
-  MonitorElement* csctfchargevalue;
+  MonitorElement* csctfetavalue[3];
+  MonitorElement* csctfphivalue[3];
+  MonitorElement* csctfptvalue[3];
+  MonitorElement* csctfchargevalue[3];
+  MonitorElement* csctfquality[3];
   MonitorElement* csctfntrack;
-
-  // Below are two sets of MonitorElement:
-  //  first is modified from what initially was existing in this class
-  //  second is what we also want to see to monitor our system
-
-  // If you monitor CSC TF hardware output data, only "packed" data are available
-  MonitorElement* csctfetapacked[3];
-  MonitorElement* csctfphipacked[3];
-//  MonitorElement* csctfptpacked[3];
-  MonitorElement* csctfchargepacked[3];
-//  MonitorElement* csctfquality[3];
-
   MonitorElement* csctfbx;
-  // We want to monitor our input data too
-  MonitorElement* csctfnlcts;
-  MonitorElement* csctflctbx;
-  MonitorElement* csctflctquality;
-//  MonitorElement* csctflctxy;
 
-
-
-///KK
-  // Type of input data for the DQM
-  bool emulation;
-
-  // geometry may not be properly set in CSC TF hardware (and in data respectively)
-  // make an artificial assignment of each of 12 SPs (slots 6-11 and 16-21) to 12 sectors (1-12, 0-not assigned)
-  std::vector<int> slot2sector;
-
-  // following arrays are indexed by sector # (1-12)
-  //   and have one spare element [0] for unknown SP board ID (in case of corrupted data)
-  MonitorElement* cscsp_fmm_status[13]; // FMM status for each SP
-  MonitorElement* cscsp_errors[13];     // Logical 'OR' of various data errors that SP can detect
-  MonitorElement* csctf_errors;         // Cumulative errors for the whole TF crate
-///
   int nev_; // Number of events processed
   std::string outputFile_; //file name for ROOT ouput
   bool verbose_;
   bool monitorDaemon_;
   ofstream logFile_;
-  edm::InputTag csctfSource_;
-
-
-
+  edm::InputTag csctfSource_ ;
 };
 
 #endif
