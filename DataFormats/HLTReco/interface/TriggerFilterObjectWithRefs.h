c55c5cea7559d17a1514ab4dcfb3b0cddcc3788c
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-12-03 14:21:04 +0000

    Replace types...

--- DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h
+++ DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h
@@ -13,7 +13,7 @@
  *  possible HLT filters. Hence we accept the reasonably small
  *  overhead of empty containers.
  *
- *  $Date: 2007/07/04 07:47:45 $
+ *  $Date: 2007/12/03 13:05:43 $
  *  $Revision: 1.1 $
  *
  *  \author Martin Grunewald
@@ -27,6 +27,7 @@
 #include "DataFormats/EgammaCandidates/interface/ElectronFwd.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/Candidate/interface/CompositeCandidateFwd.h"
 #include "DataFormats/METReco/interface/CaloMETFwd.h"
 #include "DataFormats/METReco/interface/METFwd.h"
 
@@ -46,8 +47,8 @@ namespace trigger
     std::vector<reco::RecoEcalCandidateRef> photons_;
     std::vector<reco::ElectronRef> electrons_;
     std::vector<reco::RecoChargedCandidateRef> muons_;
-    std::vector<reco::CaloJetRef> taus_;
     std::vector<reco::CaloJetRef> jets_;
+    std::vector<reco::CompositeCandidateRef> composites_;
     std::vector<reco::CaloMETRef> mets_;
     std::vector<reco::METRef> hts_;
     
@@ -55,14 +56,14 @@ namespace trigger
   public:
     /// constructors
     TriggerFilterObjectWithRefs():
-      photons_(), electrons_(), muons_(), taus_(), jets_(), mets_(), hts_() { }
+      photons_(), electrons_(), muons_(), jets_(), composites_(), mets_(), hts_() { }
 
     /// setters
     void addPhoton(const reco::RecoEcalCandidateRef& photon) {photons_.push_back(photon);}
     void addElectron(const reco::ElectronRef& electron) {electrons_.push_back(electron);}
     void addMuon(const reco::RecoChargedCandidateRef& muon) {muons_.push_back(muon);}
-    void addTau(const reco::CaloJetRef& tau) {taus_.push_back(tau);}
     void addJet(const reco::CaloJetRef& jet) {jets_.push_back(jet);}
+    void addComposite(const reco::CompositeCandidateRef& composite) {composites_.push_back(composite);}
     void addMET(const reco::CaloMETRef& met) {mets_.push_back(met);}
     void addHT (const reco::METRef& ht) {hts_.push_back(ht);}
 
@@ -70,8 +71,8 @@ namespace trigger
     const std::vector<reco::RecoEcalCandidateRef>& getPhotons() const {return photons_;}
     const std::vector<reco::ElectronRef>& getElectrons() const {return electrons_;}
     const std::vector<reco::RecoChargedCandidateRef>& getMuons() const {return muons_;}
-    const std::vector<reco::CaloJetRef>& getTaus() const {return taus_;}
     const std::vector<reco::CaloJetRef>& getJets() const {return jets_;}
+    const std::vector<reco::CompositeCandidateRef>& getComposites() const {return composites_;}
     const std::vector<reco::CaloMETRef>& getMETs() const {return mets_;}
     const std::vector<reco::METRef>& getHTs() const {return hts_;}
 
