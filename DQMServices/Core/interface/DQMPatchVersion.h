8a9a0052f1864205bd13587f35640164b375b1e5
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-12-04 14:37:40 +0000

    update to patch version 3

--- DQMServices/Core/interface/DQMPatchVersion.h
+++ DQMServices/Core/interface/DQMPatchVersion.h
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace dqm {
-  std::string DQMPatchVersion = "02" ;
+  std::string DQMPatchVersion = "03" ;
 }
 #endif
 
@@ -25,4 +25,28 @@ cvs co -r V00-16-05 DQM/DTMonitorModule
 cvs co -r V00-03-04 DQM/DTMonitorClient
 cvs co -r V02-00-03 DQM/CSCMonitorModule 
 cvs co -r V01-08-10 EventFilter/CSCRawToDigi 
+
+===============
+
+  171:02
+
+# -- List of tags in  DQMPATCH:02 
+cvs co -r V00-05-12 DQMServices/Core
+cvs co -r V00-05-12 DQMServices/CoreROOT
+cvs co -r V00-05-11 DQMServices/Components
+cvs co -r V00-05-11 DQMServices/Examples
+cvs co -r V00-00-11 DQM/Integration
+cvs co -r V00-00-08 DQM/RenderPlugins
+cvs co -r V01-00-01 L1Trigger/HardwareValidation
+cvs co -r V02-03-03-03 DataFormats/L1Trigger
+cvs co -r V00-05-01 EventFilter/EcalRawToDigiDev
+cvs co -r V00-07-05 DQM/HcalMonitorClient
+cvs co -r V00-07-05 DQM/HcalMonitorTasks
+cvs co -r V00-07-07 DQM/HcalMonitorModule
+cvs co -r V00-06-00 DQM/RPCMonitorDigi
+cvs co -r V02-00-03 DQM/CSCMonitorModule
+cvs co -r V01-08-10 EventFilter/CSCRawToDigi
+cvs co -r V00-16-05 DQM/DTMonitorModule
+cvs co -r V00-03-04 DQM/DTMonitorClient
+
 */
