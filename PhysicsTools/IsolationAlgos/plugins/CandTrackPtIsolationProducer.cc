95175b7f9421e1cb42facd624fb0ae1e2edb4667
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-12-04 09:27:00 +0000

    added one more isolation algorithm

---
+++ PhysicsTools/IsolationAlgos/plugins/CandTrackPtIsolationProducer.cc
@@ -0,0 +1,16 @@
+/* \class CandPtIsolationProducerNew
+ *
+ * computes and stores isolation using PtAlgo for Candidates
+ *
+ */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "PhysicsTools/IsolationAlgos/interface/IsolationProducerNew.h"
+#include "PhysicsTools/IsolationAlgos/interface/PtIsolationAlgo.h"
+
+typedef reco::modulesNew::IsolationProducer<reco::CandidateView, reco::TrackCollection,
+					    PtIsolationAlgo<reco::Candidate,reco::TrackCollection>
+                                            > CandTrackPtIsolationProducer;
+
+DEFINE_FWK_MODULE(CandTrackPtIsolationProducer);
