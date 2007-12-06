f90eb98099b6284a5747d0c69cf46f4d34ab7210
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2007-12-06 11:26:43 +0000

    for latency

---
+++ DQM/SiStripCommissioningClients/interface/LatencyHistograms.h
@@ -0,0 +1,37 @@
+#ifndef DQM_SiStripCommissioningClients_LatencyHistograms_H
+#define DQM_SiStripCommissioningClients_LatencyHistograms_H
+
+#include "DQM/SiStripCommissioningClients/interface/CommissioningHistograms.h"
+#include "DQM/SiStripCommissioningSummary/interface/LatencySummaryFactory.h"
+#include "DQM/SiStripCommissioningAnalysis/interface/LatencyAnalysis.h"
+
+class MonitorUserInterface;
+
+class LatencyHistograms : public CommissioningHistograms {
+
+ public:
+  
+  LatencyHistograms( MonitorUserInterface* );
+  virtual ~LatencyHistograms();
+  
+  typedef SummaryHistogramFactory<LatencyAnalysis> Factory;
+  
+  /** */
+  void histoAnalysis( bool debug );
+
+  /** */
+  void createSummaryHisto( const sistrip::Monitorable&,
+                           const sistrip::Presentation&,
+                           const std::string& top_level_dir,
+                           const sistrip::Granularity& );
+  
+ protected: 
+  
+  std::map<uint32_t,LatencyAnalysis> data_;
+  
+  std::auto_ptr<Factory> factory_;
+  
+};
+
+#endif // DQM_SiStripCommissioningClients_LatencyHistograms_H
+
