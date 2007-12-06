06ee86e27ea8a2826b1a2a7e5f74467a3699d067
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2007-12-06 11:00:19 +0000

    added L1Scalers

--- DQM/TrigXMonitor/src/SealModule.cc
+++ DQM/TrigXMonitor/src/SealModule.cc
@@ -1,9 +1,13 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "DataFormats/Scalers/interface/L1TriggerScalers.h"
 
 DEFINE_SEAL_MODULE();
 
 #include "DQM/TrigXMonitor/interface/HLTScalers.h" 
 DEFINE_ANOTHER_FWK_MODULE(HLTScalers);
+
+#include "DQM/TrigXMonitor/interface/L1Scalers.h" 
+DEFINE_ANOTHER_FWK_MODULE(L1Scalers);
 
