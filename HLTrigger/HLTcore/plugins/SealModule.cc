ed77f76df26c2c0be7170d4f301acbbd08fc2430
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-12-06 08:26:01 +0000

    *** empty log message ***

--- HLTrigger/HLTcore/plugins/SealModule.cc
+++ HLTrigger/HLTcore/plugins/SealModule.cc
@@ -2,6 +2,10 @@
 
 #include "HLTrigger/HLTcore/interface/HLTPrescaler.h"
 #include "HLTrigger/HLTcore/interface/HLTMakeSummaryObjects.h"
+#include "HLTrigger/HLTcore/interface/TriggerSummaryProducerAOD.h"
+#include "HLTrigger/HLTcore/interface/TriggerSummaryProducerRAW.h"
 
 DEFINE_FWK_MODULE(HLTPrescaler);
 DEFINE_FWK_MODULE(HLTMakeSummaryObjects);
+DEFINE_FWK_MODULE(TriggerSummaryProducerAOD);
+DEFINE_FWK_MODULE(TriggerSummaryProducerRAW);
