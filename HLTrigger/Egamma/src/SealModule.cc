6a55dea9c5b853d38da7cd979e3f320ddabbdeaa
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2007-12-06 13:26:13 +0000

    updated for the removed files

--- HLTrigger/Egamma/src/SealModule.cc
+++ HLTrigger/Egamma/src/SealModule.cc
@@ -1,8 +1,6 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "HLTrigger/Egamma/interface/HLTEgammaL1MatchFilter.h"
-#include "HLTrigger/Egamma/interface/HLTEgammaL1RelaxedMatchFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaEtFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaDoubleEtFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaEcalIsolFilter.h"
@@ -10,9 +8,7 @@
 #include "HLTrigger/Egamma/interface/HLTEgammaHcalDBCFilter.h"
 #include "HLTrigger/Egamma/interface/HLTEgammaHOEFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPhotonTrackIsolFilter.h"
-#include "HLTrigger/Egamma/interface/HLTElectronTrackIsolFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronPixelMatchFilter.h"
-#include "HLTrigger/Egamma/interface/HLTElectronEoverpFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPMMassFilter.h"
 #include "HLTrigger/Egamma/interface/HLTPMDocaFilter.h"
 
@@ -25,8 +21,6 @@
 #include "HLTrigger/Egamma/interface/HLTElectronOneOEMinusOneOPFilterRegional.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(HLTEgammaL1MatchFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTEgammaL1RelaxedMatchFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaEtFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaDoubleEtFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaEcalIsolFilter);
@@ -34,9 +28,7 @@ DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHcalIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHcalDBCFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaHOEFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTPhotonTrackIsolFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTElectronTrackIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronPixelMatchFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTElectronEoverpFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTPMMassFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTPMDocaFilter);
 
