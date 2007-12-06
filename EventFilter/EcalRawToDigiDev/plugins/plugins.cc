5ce00a92979196fed60b128c13da553aa6067883
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-12-06 16:11:10 +0000

    imported

---
+++ EventFilter/EcalRawToDigiDev/plugins/plugins.cc
@@ -0,0 +1,6 @@
+#include <FWCore/Framework/interface/MakerMacros.h>
+
+DEFINE_SEAL_MODULE();
+
+#include "EventFilter/EcalRawToDigiDev/interface/EcalRawToDigi.h"
+DEFINE_ANOTHER_FWK_MODULE(EcalRawToDigiDev);
