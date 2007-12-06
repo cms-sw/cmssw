a65c920915ae7cb30611cdecbfd0bee019a234d0
mschmitt <sha1-fbc81eaa5bec218f9b8906a4e9e228d017c621b5@cern.ch>
2007-12-06 00:06:33 +0000

    changed PFBenchmarkAnalyzer to reflect latest updates in RecoParticleFlow/Benchmark

--- Validation/RecoParticleFlow/src/PFBenchmarkAnalyzer.cc
+++ Validation/RecoParticleFlow/src/PFBenchmarkAnalyzer.cc
@@ -97,41 +97,7 @@ void PFBenchmarkAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetu
     Handle<PFCandidateCollection> reco_hnd;
     iEvent.getByLabel(inputRecoLabel_, reco_hnd);
     const PFCandidateCollection *pf_candidates = reco_hnd.product();
-    reco_candidates = algo_->newCandidateCollection(pf_candidates);
-
-    /* now implemented in PFBenchmarkAlgo::newCandidateCollection 
-    // Translate PFCandidateCollection into a CandidateCollection
-    static CandidateCollection *copy_candidates = NULL;
-    if (copy_candidates) delete copy_candidates;
-    copy_candidates = new CandidateCollection();
-
-    PFCandidateCollection::const_iterator pfcand;
-    for (pfcand = pf_candidates->begin(); pfcand != pf_candidates->end(); pfcand++) {
-      PFCandidate *c = pfcand->clone();
-      copy_candidates->push_back((PFCandidate* const)c);
-    }
-
-    reco_candidates = reinterpret_cast<const CandidateCollection *>(copy_candidates);
-    */
-
-    /*
-    Handle<View<Candidate> > truth_hnd;
-    iEvent.getByLabel(inputTruthLabel_, truth_hnd);
-    const View<Candidate> *truth_view = truth_hnd.product();
-    static CandidateCollection truth_cc(100);
-    truth_cc.clear(); truth_cc.reserve(truth_hnd->size());
-    copy(truth_view->begin(), truth_view->end(), truth_cc.begin());
-    truth_candidates = &truth_cc;
-
-    // Get Reco Candidates (PFlow, CaloJet, etc.)
-    Handle<View<Candidate> > reco_hnd;
-    iEvent.getByLabel(inputTruthLabel_, reco_hnd);
-    const View<Candidate> *reco_view = reco_hnd.product();
-    static CandidateCollection reco_cc(100);
-    reco_cc.clear(); reco_cc.reserve(reco_hnd->size());
-    copy(reco_view->begin(), reco_view->end(), reco_cc.begin());
-    reco_candidates = &reco_cc;
-    */
+    reco_candidates = algo_->makeCandidateCollection(pf_candidates);
 
   }
 
@@ -148,7 +114,6 @@ void PFBenchmarkAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetu
   // ==========================================================
 
   fill(reco_candidates,truth_candidates,plotAgainstRecoQuantities_);
-  delete reco_candidates;
 
 }
 
