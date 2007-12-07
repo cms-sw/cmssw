02c3f3ed3cb63a58d9359ddbaca6cc618f56c266
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-12-07 16:05:16 +0000

    Add enums for L1 types.

--- DataFormats/HLTReco/interface/TriggerTypeDefs.h
+++ DataFormats/HLTReco/interface/TriggerTypeDefs.h
@@ -5,8 +5,8 @@
  *
  *  Misc. common simple typedefs
  *
- *  $Date: 2007/12/05 14:24:02 $
- *  $Revision: 1.4 $
+ *  $Date: 2007/12/07 13:39:48 $
+ *  $Revision: 1.5 $
  *
  *  \author Martin Grunewald
  *
@@ -23,16 +23,36 @@ namespace trigger
   typedef std::vector<int>       Vids;
 
   enum TriggerObjectType  {
-    TriggerPhoton   = 80,
-    TriggerElectron = 81,
-    TriggerMuon     = 82,
-    TriggerTau      = 83,
-    TriggerJet      = 84,
-    TriggerBJet     = 85,
-    TriggerMET      = 86,
-    TriggerHT       = 87,
-    TriggerTrack    = 88,
-    TriggerCluster  = 89
+
+    /// enum start value shifted to 81 so as to avoid clashes with PDG codes
+
+    /// L1 - using cases as defined in enum L1GtObject, file:
+    /// DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetupFwd.h"
+
+    TriggerL1Mu        = 81,
+    TriggerL1NoIsoEG   = 82,
+    TriggerL1IsoEG     = 83,
+    TriggerL1CenJet    = 84,
+    TriggerL1ForJet    = 85,
+    TriggerL1TauJet    = 86,
+    TriggerL1ETM       = 87,
+    TriggerL1ETT       = 88,
+    TriggerL1HTT       = 89,
+    TriggerL1JetCounts = 90,
+
+    /// HLT
+
+    TriggerPhoton      = 91,
+    TriggerElectron    = 92,
+    TriggerMuon        = 93,
+    TriggerTau         = 94,
+    TriggerJet         = 95,
+    TriggerBJet        = 96,
+    TriggerMET         = 97,
+    TriggerHT          = 98,
+    TriggerTrack       = 99,
+    TriggerCluster     =100
+
   };
 
 }
