9d901798bd4ef0f8b23dbd36eeced7aea8ccb839
mschmitt <sha1-fbc81eaa5bec218f9b8906a4e9e228d017c621b5@cern.ch>
2007-12-07 00:19:25 +0000

    fixed bugs with memory management and improved general structure. Added checks for testing inheritance from Candidates, and added exception handling

--- RecoParticleFlow/Benchmark/interface/PFBenchmarkAlgo.h
+++ RecoParticleFlow/Benchmark/interface/PFBenchmarkAlgo.h
@@ -5,15 +5,7 @@
 
 #include <vector>
 
-class PFBenchmarkAlgo {
-public:
-
-  PFBenchmarkAlgo();
-  virtual ~PFBenchmarkAlgo();
-
-  // garbage management function
-  // IMPORTANT -- run once at start or end of every analysis loop
-  void reset();
+struct PFBenchmarkAlgo {
 
   // calculate base quantities for the given pair of candidates
   static double deltaEt(const reco::Candidate *, const reco::Candidate *);
@@ -22,41 +14,44 @@ public:
   static double deltaR(const reco::Candidate *, const reco::Candidate *);
 
   // simple candidate matching
-  const reco::Candidate *matchByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
-  const reco::Candidate *matchByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
+  static const reco::Candidate *matchByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
+  static const reco::Candidate *matchByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
 
   // find a duplicate of the candidate in the collection and return a pointer to that element
-  const reco::Candidate *recoverCandidate(const reco::Candidate *, const reco::CandidateCollection *);
+  static const reco::Candidate *recoverCandidate(const reco::Candidate *, const reco::CandidateCollection *);
 
   // sorting functions - returns a sorted copy of the input
-  const reco::CandidateCollection *sortByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
-  const reco::CandidateCollection *sortByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
+  static reco::CandidateCollection sortByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
+  static reco::CandidateCollection sortByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
 
-  // multi-match function - returns a sorted, constrained collection
-  const reco::CandidateCollection *findAllInCone(const reco::Candidate *, const reco::CandidateCollection *, double ConeSize);
-  const reco::CandidateCollection *findAllInEtWindow(const reco::Candidate *, const reco::CandidateCollection *, double EtWindow);
+  // multi-match function - returns a sorted, constrained copy of the input
+  static reco::CandidateCollection findAllInCone(const reco::Candidate *, const reco::CandidateCollection *, double ConeSize);
+  static reco::CandidateCollection findAllInEtWindow(const reco::Candidate *, const reco::CandidateCollection *, double EtWindow);
 
-  // make CandidateCollection out of vector<T> (for example, a PFCandidateCollection)
+  // make CandidateCollection out of vector<T> (like a PFCandidateCollection)
   template <typename CandidateDerived>
-  const reco::CandidateCollection *makeCandidateCollection(const std::vector<CandidateDerived> *);
+  static reco::CandidateCollection makeCandidateCollection(const std::vector<CandidateDerived> *);
 
-private:
+  // determine if the given Candidate is valid and of the given type
+  static bool validCandidate(const reco::Candidate *);
+  static bool validPFCandidate(const reco::Candidate *);
+  static bool validPFJet(const reco::Candidate *);
+  static bool validCaloJet(const reco::Candidate *);
 
-  // keep track of what pointers have been allocated
-  std::vector<reco::CandidateCollection *> allocatedMem_;
+  // determine if the given CandidateCollection is valid
+  static bool validCandidateCollection(const reco::CandidateCollection *);
 
 };
 
 // template implementation (required to be in header)
 template <typename CandidateDerived>
-const reco::CandidateCollection *PFBenchmarkAlgo::makeCandidateCollection(const std::vector<CandidateDerived> *InputCollection) {
+reco::CandidateCollection PFBenchmarkAlgo::makeCandidateCollection(const std::vector<CandidateDerived> *InputCollection) {
 
-  reco::CandidateCollection *copy_candidates = new reco::CandidateCollection();
-  allocatedMem_.push_back(copy_candidates);
+  reco::CandidateCollection copy_candidates;
 
   for (unsigned int i = 0; i < InputCollection->size(); i++) {
-    CandidateDerived *c = (*InputCollection)[i].clone();
-    copy_candidates->push_back((CandidateDerived* const)c);
+    const CandidateDerived *c = &(*InputCollection)[i];
+    copy_candidates.push_back((CandidateDerived* const)c->clone());
   }
 
   return copy_candidates;
