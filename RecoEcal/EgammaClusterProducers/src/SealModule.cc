9118bc4bf78c3a63fcb2d00c7e517e5e9818bdda
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-12-07 15:58:26 +0000

    remove BumpProducer

--- RecoEcal/EgammaClusterProducers/src/SealModule.cc
+++ RecoEcal/EgammaClusterProducers/src/SealModule.cc
@@ -1,6 +1,5 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoEcal/EgammaClusterProducers/interface/BumpProducer.h"
 #include "RecoEcal/EgammaClusterProducers/interface/IslandClusterProducer.h"
 #include "RecoEcal/EgammaClusterProducers/interface/HybridClusterProducer.h"
 #include "RecoEcal/EgammaClusterProducers/interface/SuperClusterProducer.h"
@@ -13,7 +12,6 @@
 
 DEFINE_SEAL_MODULE();
 
-DEFINE_ANOTHER_FWK_MODULE(BumpProducer);
 DEFINE_ANOTHER_FWK_MODULE(IslandClusterProducer);
 DEFINE_ANOTHER_FWK_MODULE(HybridClusterProducer);
 DEFINE_ANOTHER_FWK_MODULE(SuperClusterProducer);
