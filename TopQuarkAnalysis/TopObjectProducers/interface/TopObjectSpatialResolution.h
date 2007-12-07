23bd9db74b5a17762541bcb6e5bc0ff52ffa0a9f
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2007-12-07 15:51:18 +0000

    Smearing in eta is available now.

--- TopQuarkAnalysis/TopObjectProducers/interface/TopObjectSpatialResolution.h
+++ TopQuarkAnalysis/TopObjectProducers/interface/TopObjectSpatialResolution.h
@@ -17,12 +17,13 @@
 //
 // Original Author:  Volker Adler
 //         Created:  Mon Oct 15 16:15:05 CEST 2007
-// $Id$
+// $Id: TopObjectSpatialResolution.h,v 1.1 2007/11/09 13:18:39 vadler Exp $
 //
 //
 
 
 #include <memory>
+#include <cmath>
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
@@ -33,6 +34,8 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
 #include "CLHEP/Random/RandGaussQ.h"
+
+#include "DataFormats/Math/interface/normalizedPhi.h"
 
 
 template<class T>
@@ -50,14 +53,13 @@ class TopObjectSpatialResolution : public edm::EDProducer {
     void smearAngles(T& object);
 
     edm::InputTag objects_;
-    double        iniResTheta_,
-                  worsenResTheta_,
-                  finalResTheta_,
+    double        iniResPolar_,
+                  worsenResPolar_,
                   iniResPhi_,
-                  worsenResPhi_,
-                  finalResPhi_;
+                  worsenResPhi_;
     bool          useDefaultIniRes_,
-                  useWorsenResThetaByFactor_,
+                  usePolarTheta_,
+                  useWorsenResPolarByFactor_,
                   useWorsenResPhiByFactor_;
 
     CLHEP::RandGaussQ* gaussian_;
@@ -70,13 +72,13 @@ TopObjectSpatialResolution<T>::TopObjectSpatialResolution(const edm::ParameterSe
 {
   objects_                   = iConfig.getParameter<edm::InputTag>("movedTopObject");
   useDefaultIniRes_          = iConfig.getParameter<bool>         ("useDefaultInitialResolutions");
-  iniResTheta_               = iConfig.getParameter<double>       ("initialResolutionTheta");
-  worsenResTheta_            = iConfig.getParameter<double>       ("worsenResolutionTheta");
-  useWorsenResThetaByFactor_ = iConfig.getParameter<bool>         ("worsenResolutionThetaByFactor");
   iniResPhi_                 = iConfig.getParameter<double>       ("initialResolutionPhi");
   worsenResPhi_              = iConfig.getParameter<double>       ("worsenResolutionPhi");
   useWorsenResPhiByFactor_   = iConfig.getParameter<bool>         ("worsenResolutionPhiByFactor");
-
+  usePolarTheta_             = iConfig.getParameter<bool>         ("usePolarTheta");
+  iniResPolar_               = iConfig.getParameter<double>       ("initialResolutionPolar");
+  worsenResPolar_            = iConfig.getParameter<double>       ("worsenResolutionPolar");
+  useWorsenResPolarByFactor_ = iConfig.getParameter<bool>         ("worsenResolutionPolarByFactor");
 
   edm::Service<edm::RandomNumberGenerator> rng;
   CLHEP::HepRandomEngine& engine = rng->getEngine();
@@ -111,27 +113,61 @@ void TopObjectSpatialResolution<T>::produce(edm::Event& iEvent, const edm::Event
 
 
 /// Sets initial resolution to resolution provided by input TopObject if required,
-/// smears theta and phi and sets the 4-vector accordingl.
+/// smears eta/theta and phi and sets the 4-vector accordingl.
 template<class T>
 void TopObjectSpatialResolution<T>::smearAngles(T& object)
 {
   // overwrite config file parameters 'initialResolution...' if required
   if ( useDefaultIniRes_ ) {
     // get initial resolutions from input TopObject
-    iniResTheta_ = object.getResTheta(); // overwrites config file parameter "initialResolution"
-    iniResPhi_   = object.getResPhi();   // overwrites config file parameter "initialResolution"
+    iniResPhi_   = object.getResPhi();    // overwrites config file parameter "initialResolution"
+    iniResPolar_ = usePolarTheta_      ?
+                   object.getResTheta():
+                   object.getResEta();    // overwrites config file parameter "initialResolution"
   }
-  // Is 'worsenResolution<Angle>' a factor or an summand?
-  finalResTheta_ = ( useWorsenResThetaByFactor_ ) ? (1.+fabs(1.-fabs(worsenResTheta_))) * fabs(iniResTheta_) : fabs(worsenResTheta_) + fabs(iniResTheta_); // conversion as protection from "finalRes_<iniRes_"
-  finalResPhi_   = ( useWorsenResPhiByFactor_   ) ? (1.+fabs(1.-fabs(worsenResPhi_  ))) * fabs(iniResPhi_  ) : fabs(worsenResPhi_  ) + fabs(iniResPhi_  ); // conversion as protection from "finalRes_<iniRes_"
-  // smear angles
-  double smearedTheta = gaussian_->fire(object.theta(), sqrt(pow(finalResTheta_,2)-pow(iniResTheta_,2)));
-  double smearedPhi   = gaussian_->fire(object.phi()  , sqrt(pow(finalResPhi_  ,2)-pow(iniResPhi_  ,2)));
+  // smear phi
+  double finalResPhi = useWorsenResPhiByFactor_                            ?
+                       (1.+fabs(1.-fabs(worsenResPhi_))) * fabs(iniResPhi_):
+                       fabs(worsenResPhi_) + fabs(iniResPhi_);               // conversions as protection from "finalRes_<iniRes_"
+  double smearedPhi = normalizedPhi( gaussian_->fire(object.phi(), sqrt(pow(finalResPhi,2)-pow(iniResPhi_,2))) );
+  double finalResPolar = useWorsenResPolarByFactor_                              ?
+                         (1.+fabs(1.-fabs(worsenResPolar_))) * fabs(iniResPolar_):
+                         fabs(worsenResPolar_) + fabs(iniResPolar_);               // conversions as protection from "finalRes_<iniRes_"
+  // smear theta/eta
+  const double thetaMin = 2.*atan(exp(-ROOT::Math::etaMax<double>())); // to be on the safe side; however, etaMax=22765 ==> thetaMin=0 within double precision
+  double smearedTheta,
+         smearedEta;
+  if ( usePolarTheta_ ) {
+    smearedTheta = gaussian_->fire(object.theta(), sqrt(pow(finalResPolar,2)-pow(iniResPolar_,2)));
+    // 0<theta<Pi needs to be assured to have proper calculation of eta
+    while ( fabs(smearedTheta) > M_PI ) smearedTheta = smearedTheta < 0.     ?
+                                                       smearedTheta + 2.*M_PI:
+                                                       smearedTheta - 2.*M_PI;
+    if ( smearedTheta < 0. ) {
+      smearedTheta = -smearedTheta;
+      smearedPhi   = normalizedPhi(smearedPhi+M_PI);
+    }
+    smearedEta = smearedTheta < thetaMin          ?
+                 ROOT::Math::etaMax<double>()     :
+                 ( smearedTheta > M_PI-thetaMin ?
+                   -ROOT::Math::etaMax<double>():
+                   -log(tan(smearedTheta/2.))    ); // eta not calculable for theta=0,Pi, which could occur
+  } else {
+    smearedEta = gaussian_->fire(object.eta(), sqrt(pow(finalResPolar,2)-pow(iniResPolar_,2)));
+    if ( fabs(smearedEta) > ROOT::Math::etaMax<double>() ) smearedEta = smearedEta < 0.              ?
+                                                                        -ROOT::Math::etaMax<double>():
+                                                                         ROOT::Math::etaMax<double>();
+    smearedTheta = 2. * atan(exp(-smearedEta)); // since exp(x)>0 && atan() returns solution closest to 0, 0<theta<Pi should be assured.
+  }
   // set smeared new 4-vector
-  object.setP4(reco::Particle::LorentzVector(object.p()*sin(smearedTheta)*cos(smearedPhi),
-                                             object.p()*sin(smearedTheta)*sin(smearedPhi),
-                                             object.p()*cos(smearedTheta),
-                                             object.energy()));
+  math::PtEtaPhiELorentzVector newLorentzVector(object.p()*sin(smearedTheta),
+                                                smearedEta,
+                                                smearedPhi,
+                                                object.energy());
+  object.setP4(reco::Particle::LorentzVector(newLorentzVector.Px(),
+                                             newLorentzVector.Py(),
+                                             newLorentzVector.Pz(),
+                                             newLorentzVector.E() ));
 }
 
 
