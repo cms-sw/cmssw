91c0aeeae406354b4b422656d467dd6dc5de66ff
mschmitt <sha1-fbc81eaa5bec218f9b8906a4e9e228d017c621b5@cern.ch>
2007-12-08 03:43:30 +0000

    total revision: prototype template-ized PFBenchmarkAlgo. removed source code (complete implementation in header)

--- RecoParticleFlow/Benchmark/interface/PFBenchmarkAlgo.h
+++ RecoParticleFlow/Benchmark/interface/PFBenchmarkAlgo.h
@@ -1,60 +1,308 @@
 #ifndef RecoParticleFlow_Benchmark_PFBenchmarkAlgo_h
 #define RecoParticleFlow_Benchmark_PFBenchmarkAlgo_h
 
-#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "DataFormats/Common/interface/OwnVector.h"
 
+#include <cmath>
 #include <vector>
 
-struct PFBenchmarkAlgo {
+class PFBenchmarkAlgo {
+public:
 
-  // calculate base quantities for the given pair of candidates
-  static double deltaEt(const reco::Candidate *, const reco::Candidate *);
-  static double deltaEta(const reco::Candidate *, const reco::Candidate *);
-  static double deltaPhi(const reco::Candidate *, const reco::Candidate *);
-  static double deltaR(const reco::Candidate *, const reco::Candidate *);
+  // Calculate Delta-Et for Candidates (T - U)
+  template <typename T, typename U>
+  static double deltaEt(const T *, const U *);
 
-  // simple candidate matching
-  static const reco::Candidate *matchByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
-  static const reco::Candidate *matchByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
+  // Calculate Delta-Eta for Candidates (T - U)
+  template <typename T, typename U>
+  static double deltaEta(const T *, const U *);
 
-  // find a duplicate of the candidate in the collection and return a pointer to that element
-  static const reco::Candidate *recoverCandidate(const reco::Candidate *, const reco::CandidateCollection *);
+  // Calculate Delta-Phi for Candidates (T - U)
+  template <typename T, typename U>
+  static double deltaPhi(const T *, const U *);
 
-  // sorting functions - returns a sorted copy of the input
-  static reco::CandidateCollection sortByDeltaR(const reco::Candidate *, const reco::CandidateCollection *);
-  static reco::CandidateCollection sortByDeltaEt(const reco::Candidate *, const reco::CandidateCollection *);
+  // Calculate Delta-R for Candidates 
+  template <typename T, typename U>
+  static double deltaR(const T *, const U *);
 
-  // multi-match function - returns a sorted, constrained copy of the input
-  static reco::CandidateCollection findAllInCone(const reco::Candidate *, const reco::CandidateCollection *, double ConeSize);
-  static reco::CandidateCollection findAllInEtWindow(const reco::Candidate *, const reco::CandidateCollection *, double EtWindow);
+  // Match Candidate T to a Candidate U in the Collection based on minimum Delta-R
+  template <typename T, typename U, template <typename> class Collection>
+  static const U *matchByDeltaR(const T *, const Collection<U> *);
 
-  // make CandidateCollection out of vector<T> (like a PFCandidateCollection)
-  template <typename CandidateDerived>
-  static reco::CandidateCollection makeCandidateCollection(const std::vector<CandidateDerived> *);
+  // Match Candidate T to a Candidate U in the Collection based on minimum Delta-Et
+  template <typename T, typename U, template <typename> class Collection>
+  static const U *matchByDeltaEt(const T *, const Collection<U> *);
 
-  // determine if the given Candidate is valid and of the given type
-  static bool validCandidate(const reco::Candidate *);
-  static bool validPFCandidate(const reco::Candidate *);
-  static bool validPFJet(const reco::Candidate *);
-  static bool validCaloJet(const reco::Candidate *);
+  // Sort the U Candidates to the T Candidate based on minimum Delta-R
+  template <typename T, typename U, template <typename> class Collection>
+  static void sortByDeltaR(const T *, Collection<U> &);
 
-  // determine if the given CandidateCollection is valid
-  static bool validCandidateCollection(const reco::CandidateCollection *);
+  // Sort the U Candidates to the T Candidate based on minimum Delta-Et
+  template <typename T, typename U, template <typename> class Collection>
+  static void sortByDeltaEt(const T *, Collection<U> &);
+
+  // Constrain the U Candidates to the T Candidate based on Delta-R to T
+  template <typename T, typename U, template <typename> class Collection>
+  static Collection<U> findAllInCone(const T *, const Collection<U> *, double);
+
+  // Constrain the U Candidates to the T Candidate based on Delta-Et to T
+  template <typename T, typename U, template <typename> class Collection>
+  static Collection<U> findAllInEtWindow(const T *, const Collection<U> *, double);
+
+private:
+
+  // std::vector sort helper function
+  template <typename T, typename U, template <typename,typename> class Sorter>
+  static void vector_sort(std::vector<T> &candidates, Sorter<T,U> S) {
+    sort(candidates.begin(),candidates.end(),S);
+  }
+
+  // std::vector push_back helper function
+  template <typename T>
+  static void vector_add(const T *c1, std::vector<T> &candidates) {
+    candidates.push_back(*c1);
+  }
+
+  // edm::OwnVector sort helper functions
+  template <typename T, typename U, template <typename,typename> class Sorter>
+  static void vector_sort(edm::OwnVector<T> &candidates, Sorter<T,U> S) {
+    candidates.sort(S);
+  }
+
+  // edm::OwnVector push_back helper function
+  template <typename T>
+  static void vector_add(const T *c1, edm::OwnVector<T> &candidates) {
+    candidates.push_back((T *const)c1->clone());
+  }
 
 };
 
-// template implementation (required to be in header)
-template <typename CandidateDerived>
-reco::CandidateCollection PFBenchmarkAlgo::makeCandidateCollection(const std::vector<CandidateDerived> *InputCollection) {
+// ========================================================================
+// implementation follows (required to be in header for templates)
+// ========================================================================
 
-  reco::CandidateCollection copy_candidates;
+// Helper class for sorting U Collections by Delta-R to a Candidate T
+template <typename T, typename U> class deltaRSorter {
+public:
 
-  for (unsigned int i = 0; i < InputCollection->size(); i++) {
-    const CandidateDerived *c = &(*InputCollection)[i];
-    copy_candidates.push_back((CandidateDerived* const)c->clone());
+  deltaRSorter(const T *Ref) { cref = Ref; }
+  bool operator()(const U &c1, const U &c2) const {
+    return PFBenchmarkAlgo::deltaR(cref,&c1) < PFBenchmarkAlgo::deltaR(cref,&c2);
   }
 
-  return copy_candidates;
+private:
+
+  const T *cref;
+
+};
+
+// Helper class for sorting U Collections by Delta-Et to a Candidate T
+template <typename T, typename U> class deltaEtSorter {
+public:
+
+  deltaEtSorter(const T *Ref) { cref = Ref; }
+  bool operator()(const U &c1, const U &c2) const {
+    return fabs(PFBenchmarkAlgo::deltaEt(cref,&c1)) < fabs(PFBenchmarkAlgo::deltaEt(cref,&c2));
+  }
+
+private:
+
+  const T *cref;
+
+};
+
+// Calculate Delta-Et for Candidates (T - U)
+template <typename T, typename U>
+double PFBenchmarkAlgo::deltaEt(const T *c1, const U *c2) {
+
+  if (c1 == NULL || c2 == NULL)
+    throw cms::Exception("Invalid Arg") << "attempted to calculate deltaEt for invalid Candidate(s)";
+
+  return c1->et() - c2->et();
+
+}
+
+// Calculate Delta-Eta for Candidates (T - U)
+template <typename T, typename U>
+double PFBenchmarkAlgo::deltaEta(const T *c1, const U *c2) {
+
+  if (c1 == NULL || c2 == NULL)
+    throw cms::Exception("Invalid Arg") << "attempted to calculate deltaEta for invalid Candidate(s)";
+
+  return c1->eta() - c2->eta();
+
+}
+
+// Calculate Delta-Phi for Candidates (T - U)
+template <typename T, typename U>
+double PFBenchmarkAlgo::deltaPhi(const T *c1, const U *c2) {
+
+  if (c1 == NULL || c2 == NULL)
+    throw cms::Exception("Invalid Arg") << "attempted to calculate deltaPhi for invalid Candidate(s)";
+
+  double phi1 = c1->phi();
+  if (phi1 > M_PI) phi1 -= ceil((phi1 - M_PI) / (2 * M_PI)) * 2 * M_PI;
+  if (phi1 <= - M_PI) phi1 += ceil((phi1 - M_PI) / (2 * M_PI)) * 2 * M_PI;
+
+  double phi2 = c2->phi();
+  if (phi2 > M_PI) phi2 -= ceil((phi1 - M_PI) / (2 * M_PI)) * 2 * M_PI;
+  if (phi2 <= - M_PI) phi2 += ceil((phi1 - M_PI) / (2 * M_PI)) * 2 * M_PI;
+
+  // alternative method:
+  // while (phi > M_PI) phi -= 2 * M_PI;
+  // while (phi <= - M_PI) phi += 2 * M_PI;
+
+  return phi1 - phi2;
+
+}
+ 
+// Calculate Delta-R for Candidates 
+template <typename T, typename U>
+double PFBenchmarkAlgo::deltaR(const T *c1, const U *c2) {
+
+  if (c1 == NULL || c2 == NULL)
+    throw cms::Exception("Invalid Arg") << "attempted to calculate deltaR for invalid Candidate(s)";
+
+  return sqrt(pow(deltaPhi(c1,c2),2) + pow(deltaEta(c1,c2),2));
+
+}
+
+// Match Candidate T to a Candidate U in the Collection based on minimum Delta-R
+template <typename T, typename U, template <typename> class Collection>
+const U *PFBenchmarkAlgo::matchByDeltaR(const T *c1, const Collection<U> *candidates) { 
+
+  // Try to verify the validity of the Candidate and Collection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to match invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to match to invalid Collection";
+
+  double minDeltaR = 9999.;
+  const U *match = NULL;
+  
+  // Loop Over the Candidates...
+  for (unsigned int i = 0; i < candidates->size(); i++) {
+
+    const U *c2 = &(*candidates)[i];
+    if (!c2) throw cms::Exception("Invalid Arg") << "attempted to match to invalid Candidate";
+
+    // Find Minimal Delta-R
+    double dR = deltaR(c1,c2);
+    if (dR <= minDeltaR) { match = c2; minDeltaR = dR; }
+  
+  }
+
+  // Return the Candidate with the smallest dR
+  return match;
+
+}
+
+// Match Candidate T to a Candidate U in the Collection based on minimum Delta-Et
+template <typename T, typename U, template <typename> class Collection>
+const U *PFBenchmarkAlgo::matchByDeltaEt(const T *c1, const Collection<U> *candidates) {
+
+  // Try to verify the validity of the Candidate and Collection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to match invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to match to invalid Collection";
+
+  double minDeltaEt = 9999.;
+  const U *match = NULL;
+
+  // Loop Over the Candidates...
+  for (unsigned int i = 0; i < candidates->size(); i++) {
+
+    const T *c2 = &(*candidates)[i];
+    if (!c2) throw cms::Exception("Invalid Arg") << "attempted to match to invalid Candidate";
+
+    // Find Minimal Delta-R
+    double dEt = fabs(deltaEt(c1,c2));
+    if (dEt <= minDeltaEt) { match = c2; minDeltaEt = dEt; }
+
+  }
+
+  // Return the Candidate with the smallest dR
+  return match;
+
+}
+
+// Sort the U Candidates to the Candidate T based on minimum Delta-R
+template <typename T, typename U, template <typename> class Collection>
+void PFBenchmarkAlgo::sortByDeltaR(const T *c1, Collection<U> &candidates) {
+
+  // Try to verify the validity of Candidate and Collection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to sort by invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to sort invalid Candidates";
+
+  // Sort the collection
+  vector_sort(candidates,deltaRSorter<T,U>(c1));
+
+}
+
+// Sort the U Candidates to the Candidate T based on minimum Delta-Et
+template <typename T, typename U, template <typename> class Collection>
+void PFBenchmarkAlgo::sortByDeltaEt(const T *c1, Collection<U> &candidates) {
+
+  // Try to verify the validity of Candidate and Collection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to sort by invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to sort invalid Candidates";
+
+  // Sort the collection
+  vector_sort(candidates,deltaEtSorter<T,U>(c1));
+
+}
+
+// Constrain the U Candidates to the T Candidate based on Delta-R to T
+template <typename T, typename U, template <typename> class Collection>
+Collection<U> PFBenchmarkAlgo::findAllInCone(const T *c1, const Collection<U> *candidates, double ConeSize) {
+
+  // Try to verify the validity of Candidate and the CandidateCollection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to constrain to invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to constrain invalid CandidateCollection";
+  if (ConeSize <= 0) throw cms::Exception("Invalid Arg") << "zero or negative cone size specified";
+
+  Collection<U> constrained;
+
+  for (unsigned int i = 0; i < candidates->size(); i++) {
+
+    const U *c2 = &(*candidates)[i];
+
+    // Add in-cone Candidates to the new Collection
+    double dR = deltaR(c1,c2);
+    if (dR < ConeSize) vector_add(c2,constrained);
+
+  }
+
+  // Sort and return Collection
+  sortByDeltaR(c1,constrained);
+  return constrained;
+
+}
+
+// Constrain the U Candidates to the T Candidate based on Delta-Et to T
+template <typename T, typename U, template <typename> class Collection>
+Collection<U> PFBenchmarkAlgo::findAllInEtWindow(const T *c1, const Collection<U> *candidates, double EtWindow) {
+
+  // Try to verify the validity of Candidate and the CandidateCollection
+  if (!c1) throw cms::Exception("Invalid Arg") << "attempted to constrain to invalid Candidate";
+  if (!candidates) throw cms::Exception("Invalid Arg") << "attempted to constrain invalid CandidateCollection";
+  if (EtWindow <= 0) throw cms::Exception("Invalid Arg") << "zero or negative cone size specified";
+
+  Collection<U> constrained;
+
+  //CandidateCollection::const_iterator candidate;
+  //for (candidate = candidates->begin(); candidate != candidates->end(); candidate++) {
+  for (unsigned int i = 0; i < candidates->size(); i++) {
+
+    const U *c2 = &(*candidates)[i];
+
+    // Add in-cone Candidates to the new Collection
+    double dEt = fabs(deltaEt(c1,c2));
+    if (dEt < EtWindow) vector_add(c2,constrained);
+
+  }
+
+  // Sort and return Collection
+  sortByDeltaEt(c1,constrained);
+  return constrained;
 
 }
 
