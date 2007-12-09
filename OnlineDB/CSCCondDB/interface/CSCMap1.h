f8e06fa672580824fb3899cb3bb797ab7b7f8c86
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-12-09 20:32:12 +0000

    Added methos ruiddu

--- OnlineDB/CSCCondDB/interface/CSCMap1.h
+++ OnlineDB/CSCCondDB/interface/CSCMap1.h
@@ -21,15 +21,20 @@ class cscmap1
   /**
    * Destructor for cscmap1
    */
-
   ~cscmap1 () throw (oracle::occi::SQLException);
+
   /* 'chamberid' is a decimal chamber identifier like 122090 */
   void chamber (int chamberid, CSCMapItem::MapItem *item);
 
   /* 'crate' is either crateid (1-60) or crate logical number,
    corresponding to position of crate: VME+1/11 -> 111
-                                       VME-3/04 -> 234       */
+                                       VME-3/04 -> 234
+     'dmb' : 1-5,7-10                                        */
   void cratedmb (int crate, int dmb, CSCMapItem::MapItem *item);
+
+  /* 'rui' is a rui (ddu) number: 1-36
+     'ddu_input' : 0-14                 */
+  void ruiddu (int rui, int ddu_input, CSCMapItem::MapItem *item);
 
 }; // end of class cscmap1
 #endif
