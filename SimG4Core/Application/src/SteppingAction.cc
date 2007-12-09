6fd8c5762a7cd6328b18a600326d797ddeb23b66
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-12-09 21:14:43 +0000

    Fix g4SimHits.cfi for additional parameter + protect SteppingAction

--- SimG4Core/Application/src/SteppingAction.cc
+++ SimG4Core/Application/src/SteppingAction.cc
@@ -19,15 +19,13 @@ SteppingAction::SteppingAction(EventAction* e,const edm::ParameterSet & p)
 
 SteppingAction::~SteppingAction() {}
 
-void SteppingAction::UserSteppingAction(const G4Step * aStep)
-{
-    m_g4StepSignal(aStep);
-    if (killBeamPipe)
-    {
-        catchLowEnergyInVacuumHere(aStep);
-        catchLowEnergyInVacuumNext(aStep);
-    }
-    
+void SteppingAction::UserSteppingAction(const G4Step * aStep) {
+  m_g4StepSignal(aStep);
+  if (killBeamPipe){
+    catchLowEnergyInVacuumHere(aStep);
+    catchLowEnergyInVacuumNext(aStep);
+  }
+  if (aStep->GetPostStepPoint()->GetPhysicalVolume() != 0) {
     if(!(strcmp(aStep->GetPreStepPoint()->GetPhysicalVolume()->GetLogicalVolume()->GetName().c_str(),"Tracker")&& 
 	 strcmp(aStep->GetPostStepPoint()->GetPhysicalVolume()->GetLogicalVolume()->GetName().c_str(),"CALO"))){
 
@@ -45,51 +43,46 @@ void SteppingAction::UserSteppingAction(const G4Step * aStep)
       std::pair<math::XYZVectorD,math::XYZTLorentzVectorD> p(pos,mom);
       eventAction_->addTkCaloStateInfo(id,p);
     }
+  }
 }
 
-void SteppingAction::catchLowEnergyInVacuumHere(const G4Step * aStep)
-{
-    G4Track * theTrack = aStep->GetTrack();
-    double theKenergy = theTrack->GetKineticEnergy();
-    if (theTrack->GetVolume()!=0)
-    {
-        double density = theTrack->GetVolume()->GetLogicalVolume()->GetMaterial()->GetDensity();
-        if (theKenergy <= theCriticalEnergyForVacuum && theKenergy > 0.0 &&
-            density <= theCriticalDensity && theTrack->GetDefinition()->GetPDGCharge() != 0 &&
-            theTrack->GetTrackStatus() != fStopAndKill)
-        {
-            if (verbose>1)
-	    edm::LogInfo("SimG4CoreApplication") 
-	         <<   " SteppingAction: LoopCatchSteppingAction:catchLowEnergyInVacuumHere: "
-                 << " Track from " << theTrack->GetDefinition()->GetParticleName()
-                 << " of kinetic energy " << theKenergy/MeV << " MeV "
-                 << " killed in " << theTrack->GetVolume()->GetLogicalVolume()->GetName()
-                 << " of density " << density/(g/cm3) << " g/cm3" ;
-            theTrack->SetTrackStatus(fStopAndKill);
-        }
+void SteppingAction::catchLowEnergyInVacuumHere(const G4Step * aStep) {
+  G4Track * theTrack = aStep->GetTrack();
+  double theKenergy = theTrack->GetKineticEnergy();
+  if (theTrack->GetVolume()!=0) {
+    double density = theTrack->GetVolume()->GetLogicalVolume()->GetMaterial()->GetDensity();
+    if (theKenergy <= theCriticalEnergyForVacuum && theKenergy > 0.0 &&
+	density <= theCriticalDensity && theTrack->GetDefinition()->GetPDGCharge() != 0 &&
+	theTrack->GetTrackStatus() != fStopAndKill) {
+      if (verbose>1)
+	edm::LogInfo("SimG4CoreApplication") 
+	  <<   " SteppingAction: LoopCatchSteppingAction:catchLowEnergyInVacuumHere: "
+	  << " Track from " << theTrack->GetDefinition()->GetParticleName()
+	  << " of kinetic energy " << theKenergy/MeV << " MeV "
+	  << " killed in " << theTrack->GetVolume()->GetLogicalVolume()->GetName()
+	  << " of density " << density/(g/cm3) << " g/cm3" ;
+      theTrack->SetTrackStatus(fStopAndKill);
     }
+  }
 }
 
-void SteppingAction::catchLowEnergyInVacuumNext(const G4Step * aStep)
-{
-    G4Track * theTrack = aStep->GetTrack();
-    double theKenergy = theTrack->GetKineticEnergy();
-    if (theTrack->GetNextVolume())
-    {
-        double density = theTrack->GetNextVolume()->GetLogicalVolume()->GetMaterial()->GetDensity();
-        if (theKenergy <=  theCriticalEnergyForVacuum && theKenergy > 0.0 &&
-            density <= theCriticalDensity && theTrack->GetDefinition()->GetPDGCharge() != 0 &&
-            theTrack->GetTrackStatus() != fStopAndKill)
-        {
-            if (verbose>1)
-	    edm::LogInfo("SimG4CoreApplication") 
-	         << " SteppingAction: LoopCatchSteppingAction::catchLowEnergyInVacuumNext: "
-                 << " Track from " << theTrack->GetDefinition()->GetParticleName()
-                 << " of kinetic energy " << theKenergy/MeV << " MeV "
-                 << " stopped in " << theTrack->GetVolume()->GetLogicalVolume()->GetName()
-                 << " before going into "<< theTrack->GetNextVolume()->GetLogicalVolume()->GetName()
-                 << " of density " << density/(g/cm3) << " g/cm3" ;
-            theTrack->SetTrackStatus(fStopButAlive);
-        }
+void SteppingAction::catchLowEnergyInVacuumNext(const G4Step * aStep) {
+  G4Track * theTrack = aStep->GetTrack();
+  double theKenergy = theTrack->GetKineticEnergy();
+  if (theTrack->GetNextVolume()) {
+    double density = theTrack->GetNextVolume()->GetLogicalVolume()->GetMaterial()->GetDensity();
+    if (theKenergy <=  theCriticalEnergyForVacuum && theKenergy > 0.0 &&
+	density <= theCriticalDensity && theTrack->GetDefinition()->GetPDGCharge() != 0 &&
+	theTrack->GetTrackStatus() != fStopAndKill) {
+      if (verbose>1)
+	edm::LogInfo("SimG4CoreApplication") 
+	  << " SteppingAction: LoopCatchSteppingAction::catchLowEnergyInVacuumNext: "
+	  << " Track from " << theTrack->GetDefinition()->GetParticleName()
+	  << " of kinetic energy " << theKenergy/MeV << " MeV "
+	  << " stopped in " << theTrack->GetVolume()->GetLogicalVolume()->GetName()
+	  << " before going into "<< theTrack->GetNextVolume()->GetLogicalVolume()->GetName()
+	  << " of density " << density/(g/cm3) << " g/cm3" ;
+      theTrack->SetTrackStatus(fStopButAlive);
     }
+  }
 }
