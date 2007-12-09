12e29eb4a8c51bdaf6a19696805116551409bcd4
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2007-12-09 20:33:59 +0000

    Added method ruiddu

--- OnlineDB/CSCCondDB/src/CSCMap1.cc
+++ OnlineDB/CSCCondDB/src/CSCMap1.cc
@@ -12,7 +12,7 @@
     char* c_pass = ::getenv("CSCMAP_AUTH_PASSWORD");
     db_user=std::string(c_user);
     db_pass=std::string(c_pass);
-    con = env->createConnection (db_user,db_pass,"omds");
+    con = env->createConnection (db_user,db_pass,"cmsr");
      std::cout << "Connection to mapping DB is done." << std::endl;
   }// end of constructor cscmap1 ()
 
@@ -28,7 +28,8 @@
   void cscmap1::chamber (int chamberid, CSCMapItem::MapItem *item)
   {
     oracle::occi::Statement *stmt = con->createStatement();
-    stmt->setSQL("begin cscmap.chamberid_crate(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20); end;");
+
+    stmt->setSQL("begin cms_emu_cern.cscmap.chamberid_crate(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20); end;");
 
     stmt->setInt (1, chamberid);
     stmt->registerOutParam (2, oracle::occi::OCCISTRING, 10);
@@ -78,7 +79,7 @@
     item->dmb = stmt->getInt(5);
     item->cscid = stmt->getInt(7);
 
-    stmt->setSQL("begin ddumap.chamberid_ddu(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19); end;");
+    stmt->setSQL("begin cms_emu_cern.ddumap.chamberid_ddu(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19); end;");
 
     stmt->setInt (1, chamberid);
     stmt->registerOutParam (2, oracle::occi::OCCIINT);
@@ -119,7 +120,8 @@
   void cscmap1::cratedmb (int crate, int dmb, CSCMapItem::MapItem *item)
   {
     oracle::occi::Statement *stmt = con->createStatement();
-    stmt->setSQL("begin cscmap.crateid_chamber(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17); end;");
+
+    stmt->setSQL("begin cms_emu_cern.cscmap.crateid_chamber(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17); end;");
 
     stmt->setInt (1, crate);
     stmt->setInt (2, dmb);
@@ -166,7 +168,7 @@
     item->trig_sector = stmt->getInt(8);
     item->cscid = stmt->getInt(5);
 
-    stmt->setSQL("begin ddumap.chamberid_ddu(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19); end;");
+    stmt->setSQL("begin cms_emu_cern.ddumap.chamberid_ddu(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19); end;");
 
     stmt->setInt (1, chamberid);
     stmt->registerOutParam (2, oracle::occi::OCCIINT);
@@ -203,3 +205,98 @@
 
     con->terminateStatement (stmt);
   } //end of cratedmb
+
+  void cscmap1::ruiddu (int rui, int ddu_input, CSCMapItem::MapItem *item)
+  {
+    oracle::occi::Statement *stmt = con->createStatement();
+
+    stmt->setSQL("begin cms_emu_cern.ddumap.ddu_chamber(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19); end;");
+
+    stmt->setInt (1, rui);
+    stmt->setInt (2, ddu_input);
+    stmt->registerOutParam (3, oracle::occi::OCCIINT);
+    stmt->registerOutParam (4, oracle::occi::OCCIINT);
+    stmt->registerOutParam (5, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (6, oracle::occi::OCCIINT);
+    stmt->registerOutParam (7, oracle::occi::OCCIINT);
+    stmt->registerOutParam (8, oracle::occi::OCCIINT);
+    stmt->registerOutParam (9, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (10, oracle::occi::OCCIINT);
+    stmt->registerOutParam (11, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (12, oracle::occi::OCCIINT);
+    stmt->registerOutParam (13, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (14, oracle::occi::OCCIINT);
+    stmt->registerOutParam (15, oracle::occi::OCCIINT);
+    stmt->registerOutParam (16, oracle::occi::OCCIINT);
+    stmt->registerOutParam (17, oracle::occi::OCCIINT);
+    stmt->registerOutParam (18, oracle::occi::OCCIINT);
+    stmt->registerOutParam (19, oracle::occi::OCCIINT);
+
+    stmt->execute(); //execute procedure
+
+    item->ddu = rui;
+    item->ddu_input = ddu_input;
+    item->slink = stmt->getInt(6);
+
+    item->fed_crate = stmt->getInt(3);
+    item->ddu_slot = stmt->getInt(4);
+    item->dcc_fifo = stmt->getString(5);
+    item->fiber_crate = stmt->getInt(7);
+    item->fiber_pos = stmt->getInt(7);
+    item->fiber_socket = stmt->getString(9);
+    int chamberid = stmt->getInt(14);
+
+    stmt->setSQL("begin cms_emu_cern.cscmap.chamberid_crate(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20); end;");
+
+    stmt->setInt (1, chamberid);
+    stmt->registerOutParam (2, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (3, oracle::occi::OCCIINT);
+    stmt->registerOutParam (4, oracle::occi::OCCIINT);
+    stmt->registerOutParam (5, oracle::occi::OCCIINT);
+    stmt->registerOutParam (6, oracle::occi::OCCIINT);
+    stmt->registerOutParam (7, oracle::occi::OCCIINT);
+    stmt->registerOutParam (8, oracle::occi::OCCIINT);
+    stmt->registerOutParam (9, oracle::occi::OCCIINT);
+    stmt->registerOutParam (10, oracle::occi::OCCISTRING, 10);
+    stmt->registerOutParam (11, oracle::occi::OCCIINT);
+    stmt->registerOutParam (12, oracle::occi::OCCIINT);
+    stmt->registerOutParam (13, oracle::occi::OCCIINT);
+    stmt->registerOutParam (14, oracle::occi::OCCIINT);
+    stmt->registerOutParam (15, oracle::occi::OCCIINT);
+    stmt->registerOutParam (16, oracle::occi::OCCIINT);
+    stmt->registerOutParam (17, oracle::occi::OCCIINT);
+    stmt->registerOutParam (18, oracle::occi::OCCIINT);
+    stmt->registerOutParam (19, oracle::occi::OCCIINT);
+    stmt->registerOutParam (20, oracle::occi::OCCIINT);
+
+    stmt->execute(); //execute procedure
+
+    item->chamberLabel = stmt->getString(10);
+    item->chamberId = chamberid;
+     int chamber = chamberid/10%100;
+     int rest = (chamberid - chamber*10)/1000;
+     int ring = rest%10;
+     rest = (rest - ring)/10;
+     int station = rest%10;
+     int endcap = (rest - station)/10;
+    item->endcap = endcap;
+    item->station = station;
+    item->ring = ring;
+    item->chamber = chamber;
+    item->cscIndex = stmt->getInt(13);
+    item->layerIndex = stmt->getInt(14);
+    item->stripIndex = stmt->getInt(15);
+    item->anodeIndex = stmt->getInt(16);
+    item->strips = stmt->getInt(11);
+    item->anodes = stmt->getInt(12);
+    item->crateLabel = stmt->getString(2);
+    item->crateid  = stmt->getInt(3);
+    item->sector = stmt->getInt(8);
+    item->trig_sector = stmt->getInt(9);
+    item->dmb = stmt->getInt(5);
+    item->cscid = stmt->getInt(7);
+
+    stmt->execute(); //execute procedure
+
+    con->terminateStatement (stmt);
+  } //end of ruiddu
