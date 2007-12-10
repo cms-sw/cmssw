17bda1e7849070f7fd022928cd66573740b61349
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2007-12-10 20:28:02 +0000

    added the module to monitor SiStripQuality from CondDB

---
+++ DQM/SiStripMonitorPedestals/src/SiStripMonitorQuality.cc
@@ -0,0 +1,193 @@
+// -*- C++ -*-
+//
+// Package:    SiStripMonitorQuality
+// Class:      SiStripMonitorQuality
+// 
+/**\class SiStripMonitorDigi SiStripMonitorDigi.cc DQM/SiStripMonitorDigi/src/SiStripMonitorDigi.cc
+
+ Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Suchandra Dutta
+//         Created:  Fri Dec  7 20:50 CET 2007
+// $Id: SiStripMonitorQuality.cc,v 1.4 2007/12/7 20:50:33 dutta Exp $
+//
+//
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+#include "CalibTracker/Records/interface/SiStripDetCablingRcd.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripDetCabling.h"
+#include "CalibTracker/Records/interface/SiStripQualityRcd.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
+#include "DQM/SiStripCommon/interface/SiStripFolderOrganizer.h"
+#include "DQM/SiStripCommon/interface/SiStripHistoId.h"
+#include "DQM/SiStripCommon/interface/ExtractTObject.h"
+
+#include "DataFormats/SiStripDetId/interface/SiStripSubStructure.h"
+
+#include <FWCore/Framework/interface/Event.h>
+#include <FWCore/Framework/interface/EventSetup.h>
+#include <FWCore/ParameterSet/interface/ParameterSet.h>
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DQM/SiStripMonitorPedestals/interface/SiStripMonitorQuality.h"
+
+#include "TH1F.h"
+// std
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+
+SiStripMonitorQuality::SiStripMonitorQuality(edm::ParameterSet const& iConfig):
+  dbe_(edm::Service<DaqMonitorBEInterface>().operator->()),
+  conf_(iConfig),
+  m_cacheID_(0)
+
+
+{
+  edm::LogInfo("SiStripMonitorQuality") <<"SiStripMonitorQuality  " 
+					  << " Constructing....... ";     
+}
+
+
+SiStripMonitorQuality::~SiStripMonitorQuality()
+{
+  edm::LogInfo("SiStripMonitorQuality") <<"SiStripMonitorQuality  " 
+					  << " Destructing....... ";     
+}
+//
+// -- Begin Job
+//
+void SiStripMonitorQuality::beginJob(edm::EventSetup const&) {
+}
+//
+// -- BeginRun
+//
+void SiStripMonitorQuality::beginRun(edm::Run const& run, edm::EventSetup const& eSetup){
+}
+
+
+// ------------ method called to produce the data  ------------
+void SiStripMonitorQuality::analyze(edm::Event const& iEvent, edm::EventSetup const& eSetup)
+{
+  unsigned long long cacheID = eSetup.get<SiStripQualityRcd>().cacheIdentifier();  
+  if (m_cacheID_ == cacheID) return;
+  
+  m_cacheID_ = cacheID;       
+
+  std::string data_lebel = conf_.getParameter<std::string>("dataLabel");
+  eSetup.get<SiStripQualityRcd>().get(data_lebel,stripQuality_);
+  eSetup.get<SiStripDetCablingRcd>().get( detCabling_ );
+  
+  edm::LogInfo("SiStripMonitorQuality") << "SiStripMonitorQuality::analyze: "<<
+    " Reading SiStripQuality "<< std::endl;
+
+  SiStripBadStrip::RegistryIterator rbegin = stripQuality_->getRegistryVectorBegin();
+  SiStripBadStrip::RegistryIterator rend   = stripQuality_->getRegistryVectorEnd();
+  uint32_t detid;
+    
+  if (rbegin==rend) return;
+
+  for (SiStripBadStrip::RegistryIterator rp=rbegin; rp != rend; ++rp) {
+    
+    detid = rp->detid;
+    // Check consistency in DetId
+    if (detid == 0 || detid == 0xFFFFFFFF){
+      edm::LogError("SiStripMonitorQuality") <<"SiStripMonitorQuality::analyze : " 
+					       << "Wrong DetId !!!!!! " <<  detid << " Neglecting !!!!!! ";
+      continue;
+    }
+    // check if the detid is connected in cabling
+    if (!detCabling_->IsConnected(detid)) {
+      edm::LogError("SiStripMonitorQuality") <<"SiStripMonitorQuality::analyze : " 
+					   << " DetId " <<  detid << " not connected,  Neglecting !!!!!! ";
+      continue;
+    }
+    MonitorElement* me = getQualityME(detid);
+    SiStripBadStrip::Range range = SiStripBadStrip::Range( stripQuality_->getDataVectorBegin()+rp->ibegin , 
+							   stripQuality_->getDataVectorBegin()+rp->iend );
+    SiStripBadStrip::ContainerIterator it=range.first;
+    for(;it!=range.second;++it){
+      unsigned int value=(*it);
+      short str_start = stripQuality_->decode(value).firstStrip;
+      short str_end   = str_start + stripQuality_->decode(value).range;
+      for ( short isr = str_start; isr < str_end + 1; isr++) {        
+        if (isr <= (me->getNbinsX()-1)) me->Fill(isr+1, 1.0);
+      }
+    }
+  }
+}
+//
+// -- End Run
+//    
+void SiStripMonitorQuality::endRun(edm::Run const& run, edm::EventSetup const& eSetup) {
+  bool outputMEsInRootFile = conf_.getParameter<bool>("OutputMEsInRootFile");
+  std::string outputFileName = conf_.getParameter<std::string>("OutputFileName");
+  if (outputMEsInRootFile) {    
+    dbe_->showDirStructure();
+    dbe_->save(outputFileName);
+  }
+}
+//
+// -- End Job
+//
+void SiStripMonitorQuality::endJob(void){
+  edm::LogInfo("SiStripMonitorQuality") <<"SiStripMonitorQuality::EndJob: " 
+					  << " Finishing!! ";        
+}
+//
+// -- End Job
+//
+MonitorElement* SiStripMonitorQuality::getQualityME(uint32_t idet){
+
+  std::map<uint32_t, MonitorElement* >::iterator pos = QualityMEs.find(idet);
+
+  MonitorElement* det_me;
+  if (pos != QualityMEs.end()) {
+    det_me = pos->second;
+    resetME(det_me);
+  } else {
+    int nStrip =  detCabling_->nApvPairs(idet) * 256;
+
+    // use SistripHistoId for producing histogram id (and title)
+    SiStripHistoId hidmanager;
+    // create SiStripFolderOrganizer
+    SiStripFolderOrganizer folder_organizer;
+    // set appropriate folder using SiStripFolderOrganizer
+    folder_organizer.setDetectorFolder(idet); // pass the detid to this method
+
+    std::string hid;
+    hid = hidmanager.createHistoId("StripQualityFromCondDB","det", idet);
+    
+    std::map<uint32_t, MonitorElement* >::iterator pos = QualityMEs.find(idet);
+
+    det_me = dbe_->book1D(hid, hid, nStrip,0.5,nStrip+0.5);
+    dbe_->tag(det_me, idet);
+    det_me->setAxisTitle("Strip Number",1);
+    det_me->setAxisTitle("Quality Flag from CondDB ",2);
+    QualityMEs.insert( std::make_pair(idet, det_me));
+  }
+  return det_me;
+}
+//
+// -- Reset individual Monitor Elements
+//    
+void SiStripMonitorQuality::resetME(MonitorElement* me){
+  TH1F* hist1 = ExtractTObject<TH1F>().extract(me);
+  if (hist1) hist1->Reset();
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(SiStripMonitorQuality);
+
