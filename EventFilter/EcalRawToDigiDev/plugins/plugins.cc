e71f8535494b16e696421c892cc59741f26ffed4
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2007-12-10 18:53:20 +0000

    seal unnecessary

--- EventFilter/EcalRawToDigiDev/plugins/plugins.cc
+++ EventFilter/EcalRawToDigiDev/plugins/plugins.cc
@@ -1,6 +1,4 @@
 #include <FWCore/Framework/interface/MakerMacros.h>
 
-DEFINE_SEAL_MODULE();
-
 #include "EventFilter/EcalRawToDigiDev/interface/EcalRawToDigi.h"
-DEFINE_ANOTHER_FWK_MODULE(EcalRawToDigiDev);
+DEFINE_FWK_MODULE(EcalRawToDigiDev);
