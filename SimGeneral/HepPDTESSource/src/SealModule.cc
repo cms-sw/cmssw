872602fe875308f45a80e9742a3a54a947da3c93
Todd Adams <sha1-701f8de8162420686577eb61e6718f6876a7f91f@cern.ch>
2007-12-10 21:27:26 +0000

    remove include of PythiaPDTESSource.h

--- SimGeneral/HepPDTESSource/src/SealModule.cc
+++ SimGeneral/HepPDTESSource/src/SealModule.cc
@@ -2,7 +2,6 @@
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimGeneral/HepPDTESSource/interface/HepPDTESSource.h"
-#include "SimGeneral/HepPDTESSource/interface/PythiaPDTESSource.h"
 
 
   DEFINE_SEAL_MODULE();
