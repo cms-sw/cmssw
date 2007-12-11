fe7f0519f46e12866ea87c6690449fdb8a5796c2
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2007-12-11 18:36:32 +0000

    bug fix

--- DQM/L1TMonitorClient/src/SealModule.cc
+++ DQM/L1TMonitorClient/src/SealModule.cc
@@ -6,14 +6,3 @@ DEFINE_SEAL_MODULE();
 #include <DQM/L1TMonitorClient/interface/L1THcalClient.h>
 DEFINE_ANOTHER_FWK_MODULE(L1THcalClient);
 
-#include <DQM/L1TMonitorClient/interface/L1TCaloClient.h>
-DEFINE_ANOTHER_FWK_MODULE(L1TCaloClient);
-
-#include <DQM/L1TMonitorClient/interface/L1TMuonClient.h>
-DEFINE_ANOTHER_FWK_MODULE(L1TMuonClient);
-
-#include <DQM/L1TMonitorClient/interface/L1TDTTPGClient.h>
-DEFINE_ANOTHER_FWK_MODULE(L1TDTTPGClient);
-
-#include <DQM/L1TMonitorClient/interface/L1TGTClient.h>
-DEFINE_ANOTHER_FWK_MODULE(L1TGTClient);
