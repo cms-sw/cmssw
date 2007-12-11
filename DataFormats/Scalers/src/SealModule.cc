9ead8491d24b45cb376100b4c52a70cbbcbd0b54
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2007-12-11 13:59:22 +0000

    V0.1

--- DataFormats/Scalers/src/SealModule.cc
+++ DataFormats/Scalers/src/SealModule.cc
@@ -1,9 +1,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "DataFormats/Scalers/interface/L1TriggerScalers.h"
+//#include "DataFormats/Scalers/interface/L1TriggerScalers.h"
 
 DEFINE_SEAL_MODULE();
 
 #include "DataFormats/Scalers/interface/ScalersProducer.h" 
 DEFINE_ANOTHER_FWK_MODULE(ScalersProducer);
+
+#include "DataFormats/Scalers/interface/DummyDataReader.h" 
+DEFINE_ANOTHER_FWK_MODULE(DummyDataReader);
+
+
