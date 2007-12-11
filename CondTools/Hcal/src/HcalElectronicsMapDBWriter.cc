84b2c3e0994ca88c140a8c003bf2eadc25f3a72a
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2007-12-11 16:00:32 +0000

    re-checkin

---
+++ CondTools/Hcal/src/HcalElectronicsMapDBWriter.cc
@@ -0,0 +1,21 @@
+#include "CondTools/Hcal/interface/HcalElectronicsMapDBWriter.h"
+
+using namespace popcon;
+
+HcalElectronicsMapDBWriter::HcalElectronicsMapDBWriter(const edm::ParameterSet& ps) : PopConAnalyzer<HcalElectronicsMap>(ps,"HcalElectronicsMap")
+{
+	m_pop_connection = ps.getParameter<std::string> ("popConDBSchema");
+	unsigned int defBeginTime = edm::IOVSyncValue::beginOfTime().eventID().run();
+	unsigned int defEndTime = edm::IOVSyncValue::endOfTime().eventID().run();
+	sinceTime = ps.getUntrackedParameter<unsigned>("startRun",defBeginTime);
+	tillTime = ps.getUntrackedParameter<unsigned>("endRun",defEndTime);
+} 	
+
+void HcalElectronicsMapDBWriter::HcalElectronicsMapDBWriter::initSource(const edm::Event& evt, const edm::EventSetup& est)
+{
+  this->m_handler_object = new HcalElectronicsMapSourceHandler("HcalElectronicsMap", m_offline_connection, m_catalog, evt, est, m_pop_connection, sinceTime, tillTime);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HcalElectronicsMapDBWriter);
+
