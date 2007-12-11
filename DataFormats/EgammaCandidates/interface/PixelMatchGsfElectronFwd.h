4e73acec120782c7b208713b68dbd6b01681a00f
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2007-12-11 10:05:28 +0000

    replace by dummy with include ot GsfElectronFwd.h

--- DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h
+++ DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h
@@ -1,33 +1,6 @@
-#ifndef EgammaReco_PixelMatchGsfElectronFwd_h
-#define EgammaReco_PixelMatchGsfElectronFwd_h
-#include <vector>
-#include "DataFormats/Common/interface/Ref.h"
-#include "DataFormats/Common/interface/RefProd.h"
-#include "DataFormats/Common/interface/RefVector.h"
-#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+#ifndef PixelMatchGsfElectronFwd_h
+#define PixelMatchGsfElectronFwd_h
 
-namespace reco {
-  class PixelMatchGsfElectron;
-
-  /// collection of PixelMatchGsfElectron objects
-  typedef std::vector<PixelMatchGsfElectron> PixelMatchGsfElectronCollection;
-  typedef PixelMatchGsfElectronCollection GsfElectronCollection;
-
-  /// reference to an object in a collection of PixelMatchGsfElectron objects
-  typedef edm::Ref<PixelMatchGsfElectronCollection> PixelMatchGsfElectronRef;
-  typedef PixelMatchGsfElectronRef GsfElectronRef;
-
-  /// reference to a collection of PixelMatchGsfElectron objects
-  typedef edm::RefProd<PixelMatchGsfElectronCollection> PixelMatchGsfElectronRefProd;
-  typedef PixelMatchGsfElectronRefProd GsfElectronRefProd;
-
-  /// vector of objects in the same collection of PixelMatchGsfElectron objects
-  typedef edm::RefVector<PixelMatchGsfElectronCollection> PixelMatchGsfElectronRefVector;
-  typedef PixelMatchGsfElectronRefVector GsfElectronRefVector;
-
-  /// iterator over a vector of reference to PixelMatchGsfElectron objects
-  typedef PixelMatchGsfElectronRefVector::iterator pixelMatchGsfElectron_iterator;
-  typedef pixelMatchGsfElectron_iterator GsfElectron_iterator;
-}
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 
 #endif
