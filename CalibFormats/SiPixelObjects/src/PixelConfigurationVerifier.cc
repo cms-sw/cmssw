7843cccc523b43b3edb2966588308dbf9570c682
Anders Ryd <sha1-cdf8ae4e5b0e21334a15ab4f05e71c3dd6ac0eb0@cern.ch>
2007-12-12 00:55:46 +0000

    Fixed so that fed channel 36 is checked and also fixed printout

--- CalibFormats/SiPixelObjects/src/PixelConfigurationVerifier.cc
+++ CalibFormats/SiPixelObjects/src/PixelConfigurationVerifier.cc
@@ -32,6 +32,7 @@ void PixelConfigurationVerifier::checkChannelEnable(PixelFEDCard *theFEDCard,
 
   set<PixelChannel>::const_iterator iChannel=channels.begin();
 
+
   for(;iChannel!=channels.end();++iChannel){
     PixelHdwAddress hdw=theNameTranslation->getHdwAddress(*iChannel);
     if (fedid==hdw.fednumber()){
@@ -43,16 +44,20 @@ void PixelConfigurationVerifier::checkChannelEnable(PixelFEDCard *theFEDCard,
 
   //Now check the channels
   
-  for(unsigned int iChannel=1;iChannel<36;iChannel++){
+  for(unsigned int iChannel=1;iChannel<37;iChannel++){
     bool used=theFEDCard->useChannel(iChannel);
+    //if (used) cout << "Channel="<<iChannel<<" is used"<<endl;
+    //if (!used) cout << "Channel="<<iChannel<<" is not used"<<endl;
     if (used!=usedChannel[iChannel]) {
-      cout << "WARNING for fedid="<<fedid<<" and channe="<<iChannel;
-      cout << " found that fedcard has channel as ";
-      if (used) cout << " used while configuration not using this channel"
+      cout << "*******************************************************"<<endl;
+      cout << "WARNING for fedid="<<fedid<<" and channel="<<iChannel;
+      cout << " found that fedcard has channel as"<<endl;
+      if (used) cout << "used while configuration not using this channel"
 		     << endl;
-      if (!used)cout << " not used while configuration uses this channel"
+      if (!used)cout << "not used while configuration uses this channel"
 		     << endl;
       cout << "The fedcard will be modifed to agree with configuration"<<endl;
+      cout << "*******************************************************"<<endl;
       theFEDCard->setChannel(iChannel,usedChannel[iChannel]);
     }
   }
