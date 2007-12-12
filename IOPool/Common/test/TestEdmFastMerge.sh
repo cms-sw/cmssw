b657b569ae3f10e49607055d24ef70cbf43c18d6
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-12-12 22:07:31 +0000

    Remove edmFastMerge, which is now obsolete.  Revise fast merge test to use cmsRun

--- IOPool/Common/test/TestEdmFastMerge.sh
+++ IOPool/Common/test/TestEdmFastMerge.sh
@@ -2,20 +2,20 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-INPUT_1=${LOCAL_TMP_DIR}/EdmFastMergeTest_1.root
-INPUT_2=${LOCAL_TMP_DIR}/EdmFastMergeTest_2.root
+INPUT_1=${LOCAL_TMP_DIR}/FastMergeTest_1.root
+INPUT_2=${LOCAL_TMP_DIR}/FastMergeTest_2.root
 LOGICAL_INPUT_1=PoolTest_1.root
 LOGICAL_INPUT_2=PoolTest_2.root
 
 rm -f ${INPUT_1} ${INPUT_2}
-rm -f ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_1.cfg          ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_2.cfg
+rm -f ${LOCAL_TMP_DIR}/PreFastMergeTest_1.cfg          ${LOCAL_TMP_DIR}/PreFastMergeTest_2.cfg
 
 #---------------------------
 # Create first input file
 #---------------------------
 
-cat > ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_1.cfg << !
-# Configuration file for PreEdmFastMergeTest_1
+cat > ${LOCAL_TMP_DIR}/PreFastMergeTest_1.cfg << !
+# Configuration file for PreFastMergeTest_1
 process TESTPROD = {
 	untracked PSet maxEvents = {untracked int32 input = 10}
         include "FWCore/Framework/test/cmsExceptionsFatal.cff"
@@ -33,14 +33,14 @@ process TESTPROD = {
         endpath ep = {output}
 }
 !
-cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_1.cfg || die 'Failure using PreEdmFastMergeTest_1.cfg' $?
+cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreFastMergeTest_1.cfg || die 'Failure using PreFastMergeTest_1.cfg' $?
 
 #---------------------------
 # Create second input file
 #---------------------------
 
-cat > ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_2.cfg << !
-# Configuration file for PreEdmFastMergeTest_2
+cat > ${LOCAL_TMP_DIR}/PreFastMergeTest_2.cfg << !
+# Configuration file for PreFastMergeTest_2
 process TESTPROD = {
 	untracked PSet maxEvents = {untracked int32 input = 15}
         include "FWCore/Framework/test/cmsExceptionsFatal.cff"
@@ -57,16 +57,34 @@ process TESTPROD = {
         endpath ep = {output}
 }
 !
-cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreEdmFastMergeTest_2.cfg || die 'Failure using PreEdmFastMergeTest_2.cfg' $?
+cmsRun --parameter-set ${LOCAL_TMP_DIR}/PreFastMergeTest_2.cfg || die 'Failure using PreFastMergeTest_2.cfg' $?
 
 
 #---------------------------
 # Merge files
 #---------------------------
 
-edmFastMerge -i file:${INPUT_1} file:${INPUT_2} -j ${LOCAL_TMP_DIR}/TestFastMergeFJR.xml -o ${LOCAL_TMP_DIR}/EdmFastMerge_out.root || die 'Failure using edmFastMerge' $?
+cat > ${LOCAL_TMP_DIR}/FastMergeTest.cfg << !
+# Configuration file for FastMergeTest
+process TESTMERGE = {
+	service = AdaptorConfig{untracked bool Stats = false}
+	untracked PSet maxEvents = {untracked int32 input = -1}
+        include "FWCore/Framework/test/cmsExceptionsFatal.cff"
+	module output = PoolOutputModule {
+		untracked string fileName = '${LOCAL_TMP_DIR}/FastMerge_out.root'
+	}
+	source = PoolSource {
+		untracked vstring fileNames = {
+			'file:${INPUT_1}',
+			'file:${INPUT_2}'
+		}
+        }
+        endpath ep = {output}
+}
+!
+cmsRun -j ${LOCAL_TMP_DIR}/TestFastMergeFJR.xml --parameter-set ${LOCAL_TMP_DIR}/FastMergeTest.cfg || die 'Failure using FastMergeTest.cfg' $?
 #need to filter items in job report which always change
-grep -v "<GUID>" $LOCAL_TEST_DIR/proper_fjr_output | grep -v "<PFN>" > $LOCAL_TMP_DIR/proper_fjr_output
-grep -v "<GUID>" $LOCAL_TMP_DIR/TestFastMergeFJR.xml | grep -v "<PFN>" > $LOCAL_TMP_DIR/TestFastMergeFJR_filtered.xml
+egrep -v "<GUID>|<PFN>" $LOCAL_TEST_DIR/proper_fjr_output > $LOCAL_TMP_DIR/proper_fjr_output
+egrep -v "<GUID>|<PFN>" $LOCAL_TMP_DIR/TestFastMergeFJR.xml  > $LOCAL_TMP_DIR/TestFastMergeFJR_filtered.xml
 diff $LOCAL_TMP_DIR/proper_fjr_output $LOCAL_TMP_DIR/TestFastMergeFJR_filtered.xml || die 'output framework job report is wrong' $?
 
