b41b032c4e2fa355f814ee56c6470db230325396
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-12-13 14:08:43 +0000

    Include the new PhotonsWithConversionsAnalyzer

--- RecoEgamma/Examples/plugins/SealModule.cc
+++ RecoEgamma/Examples/plugins/SealModule.cc
@@ -9,10 +9,11 @@
 #include "MCElectronAnalyzer.h"
 #include "MCPhotonAnalyzer.h"
 #include "MCPizeroAnalyzer.h"
-#include "GsfElectronAnalyzer.h"
+#include "PixelMatchGsfElectronAnalyzer.h"
 #include "SimpleConvertedPhotonAnalyzer.h"
 #include "SimplePhotonAnalyzer.h"
 #include "SiStripElectronAnalyzer.h"
+#include "PhotonsWithConversionsAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 
@@ -22,7 +23,8 @@ DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCPhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCPizeroAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(GsfElectronAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimpleConvertedPhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimplePhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(PhotonsWithConversionsAnalyzer);
