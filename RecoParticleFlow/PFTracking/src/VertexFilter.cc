b73429fb1c157cd0c3766ac7c7cb3376b0855416
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2007-12-13 17:21:50 +0000

    speed-up the leading track search

--- RecoParticleFlow/PFTracking/src/VertexFilter.cc
+++ RecoParticleFlow/PFTracking/src/VertexFilter.cc
@@ -1,6 +1,7 @@
 #include "RecoParticleFlow/PFTracking/interface/VertexFilter.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
+#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
@@ -12,18 +13,7 @@
 //
 
 
-class LeadTrack{
- public:
-  LeadTrack(){};
-  bool operator()( reco::Track tk1,
-		   reco::Track tk2){
-    if((tk1.pt()>3)&&(tk2.pt()<3.)) return true;
-    if((tk1.pt()<3)&&(tk2.pt()>3.)) return false;
-    if((tk1.found()>6)&&(tk2.found()<6)) return true;
-    if((tk1.found()<6)&&(tk2.found()>6)) return false;
-    return tk1.normalizedChi2()<tk2.normalizedChi2();
-  };
-};
+
 
 VertexFilter::VertexFilter(const edm::ParameterSet& iConfig):
   conf_(iConfig)
@@ -64,8 +54,6 @@ VertexFilter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   auto_ptr<vector<Trajectory> > outputTJ(new vector<Trajectory> );
   auto_ptr<TrajTrackAssociationCollection> trajTrackMap( new TrajTrackAssociationCollection() );
   
-  Ref<TrackExtraCollection>::key_type idx = 0;
-  Ref<TrackCollection>::key_type iTkRef = 0;
   
   TrackExtraRefProd rTrackExtras = iEvent.getRefBeforePut<TrackExtraCollection>();
   TrackingRecHitRefProd rHits = iEvent.getRefBeforePut<TrackingRecHitCollection>();
@@ -89,11 +77,11 @@ VertexFilter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByLabel( tkTag, tkCollection);
   TrackCollection  tC = *(tkCollection.product());
   TrackCollection::const_iterator itxc;
-  stable_sort(tC.begin(),tC.end(),LeadTrack());
+  
 
 
-
-
+  
+  
   iEvent.getByLabel(tkTag,TrajectoryCollection);
   iEvent.getByLabel(tkTag,assoMap);
 
@@ -103,7 +91,32 @@ VertexFilter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByLabel( vtxTag            , vtxCollection);
   const VertexCollection  vxC = *(vtxCollection.product()); 
 
-
+  float z_lead=0;
+  if(vxC.size()==0){
+    float pt_lead=0;
+    float chi_lead=1000000;
+    int nhit_lead=0;
+    for(itxc=tC.begin();itxc!=tC.end();++itxc){
+      if ((nhit_lead>6) && ((*itxc).found()<6))continue;
+      
+      if ((nhit_lead<6) && ((*itxc).found()>6)){
+	z_lead=(*itxc).vertex().z();pt_lead=(*itxc).pt();
+	chi_lead=(*itxc).normalizedChi2();nhit_lead=(*itxc).found();
+	continue;
+      }  
+      if ((chi_lead<10) && ((*itxc).normalizedChi2()>10))	continue;
+      if ((chi_lead>10) && ((*itxc).normalizedChi2()<10)){
+	z_lead=(*itxc).vertex().z();pt_lead=(*itxc).pt();
+	chi_lead=(*itxc).normalizedChi2();nhit_lead=(*itxc).found();
+	continue;
+      }
+      
+      if(pt_lead<(*itxc).pt()){
+	z_lead=(*itxc).vertex().z();pt_lead=(*itxc).pt();
+	chi_lead=(*itxc).normalizedChi2();nhit_lead=(*itxc).found(); 
+      }
+    }
+  }
   
   for(TrajTrackAssociationCollection::const_iterator it = assoMap->begin();
       it != assoMap->end(); ++it){
@@ -125,10 +138,9 @@ VertexFilter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 
     if((vxC.size()==0)&&(tC.size()>0)){
-      math::XYZPoint  gp2=(*tC.begin()).vertex();
-      math::XYZVector dgp=gp2-gp1;
+      
       if ((gp1.Rho()<distrho) &&	
-	  (fabs(dgp.Z())<distz)     &&
+	  (fabs(gp1.Z()-z_lead)<distz)     &&
 	  ((*itc).found()>=minhits) &&
 	  ((*itc).chi2()<chi_cut)) hasAVertex=true;
       
@@ -136,36 +148,56 @@ VertexFilter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     
     if (hasAVertex){
       Track track =(*itc);
-
-      
-      
-      iTkRef++;
-      TrackExtraRef teref=TrackExtraRef ( rTrackExtras, idx ++ );
-      track.setExtra( teref );
-      TrackExtra & tx =const_cast<TrackExtra &>((*(*itc).extra()));
-      
-      
- 
-    trackingRecHit_iterator irhit=(*itc).recHitsBegin();
-    trackingRecHit_iterator irhit_last=(*itc).recHitsEnd();
-    for (; irhit!=irhit_last; ++irhit){
-	  TrackingRecHit *hit=(*(*irhit)).clone();
-
-	  selHits->push_back(hit );
-     }
-
-      selTrackExtras->push_back(tx);
-      outputTJ->push_back(*traj);	
+      //tracks and trajectories
       selTracks->push_back( track );
+      outputTJ->push_back( *traj );
+      //TRACKING HITS
+      trackingRecHit_iterator irhit   =(*itc).recHitsBegin();
+      trackingRecHit_iterator lasthit =(*itc).recHitsEnd();
+      for (; irhit!=lasthit; ++irhit)
+	selHits->push_back((*irhit)->clone() );
+      
+    
+ 
+ 
     }
+
   }
+
+  //PUT TRACKING HITS IN THE EVENT
+  OrphanHandle<TrackingRecHitCollection> theRecoHits = iEvent.put(selHits );
   
-  
-  
-  iEvent.put(selTracks);
-  iEvent.put(selTrackExtras);    
-  iEvent.put(selHits);
-  iEvent.put(outputTJ);
+  //PUT TRACK EXTRA IN THE EVENT
+  for ( unsigned index = 0; index < selTracks->size(); ++index ) { 
+
+    unsigned hits=0;
+    reco::TrackExtra aTrackExtra;
+    unsigned nHits = selTracks->at(index).numberOfValidHits();
+    for ( unsigned int ih=0; ih<nHits; ++ih) {
+      aTrackExtra.add(TrackingRecHitRef(theRecoHits,hits++));
+    }
+    selTrackExtras->push_back(aTrackExtra);
+  }
+
+  //CORRECT REF TO TRACK
+  OrphanHandle<TrackExtraCollection> theRecoTrackExtras = iEvent.put(selTrackExtras); 
+  unsigned nTracks = selTracks->size();
+  for ( unsigned index = 0; index<nTracks; ++index ) { 
+    const reco::TrackExtraRef theTrackExtraRef(theRecoTrackExtras,index);
+    (selTracks->at(index)).setExtra(theTrackExtraRef);
+  }
+
+  //TRACKS AND TRAJECTORIES
+  OrphanHandle<TrackCollection> theRecoTracks = iEvent.put(selTracks);
+  OrphanHandle<vector<Trajectory> > theRecoTrajectories = iEvent.put(outputTJ);
+
+  //TRACKS<->TRAJECTORIES MAP 
+  for ( unsigned index = 0; index < theRecoTracks->size(); ++index ) { 
+    Ref<vector<Trajectory> > trajRef( theRecoTrajectories, index );
+    Ref<TrackCollection>    tkRef( theRecoTracks, index );
+    trajTrackMap->insert(trajRef,tkRef);
+  }
+  //MAP IN THE EVENT
   iEvent.put( trajTrackMap );
 }
 
