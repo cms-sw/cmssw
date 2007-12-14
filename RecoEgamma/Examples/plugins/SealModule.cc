215c63d869bc42beddceeb48247d645708b415c7
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2007-12-14 08:10:09 +0000

    By mistake the version with the old PixelMatchGsfElectron had gone in at the last commit. Fixed

--- RecoEgamma/Examples/plugins/SealModule.cc
+++ RecoEgamma/Examples/plugins/SealModule.cc
@@ -9,11 +9,11 @@
 #include "MCElectronAnalyzer.h"
 #include "MCPhotonAnalyzer.h"
 #include "MCPizeroAnalyzer.h"
-#include "PixelMatchGsfElectronAnalyzer.h"
 #include "SimpleConvertedPhotonAnalyzer.h"
 #include "SimplePhotonAnalyzer.h"
 #include "SiStripElectronAnalyzer.h"
 #include "PhotonsWithConversionsAnalyzer.h"
+#include "GsfElectronAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 
@@ -23,7 +23,7 @@ DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCPhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MCPizeroAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(PixelMatchGsfElectronAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(GsfElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimpleConvertedPhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimplePhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAnalyzer);
