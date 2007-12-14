fc49edb197af7cb8ae64845ab3d7ce4e606a60dc
roberfro <sha1-3ef59c6cd087c1ab5075a623fd6cc2bdbd41546d@cern.ch>
2007-12-14 16:03:39 +0000

    In case AdaptiveVertexFitter does not work calculate the crossing point

--- RecoVertex/NuclearInteractionProducer/src/NuclearVertexBuilder.cc
+++ RecoVertex/NuclearInteractionProducer/src/NuclearVertexBuilder.cc
@@ -18,8 +18,39 @@ void NuclearVertexBuilder::build( const reco::TrackRef& primTrack, const reco::T
                     transientTracks.push_back( theTransientTrackBuilder->build(*tk));
          }
          AdaptiveVertexFitter AVF;
-         TransientVertex tv = AVF.vertex(transientTracks); 
-         the_vertex = reco::Vertex(tv);
+         try {
+            TransientVertex tv = AVF.vertex(transientTracks); 
+            the_vertex = reco::Vertex(tv);
+         }
+         catch(cms::Exception& exception){
+            // AdaptivevertexFitter does not work 
+            // Try here a simple intersection between the primary track
+            // and the longer secondary track
+            FreeTrajectoryState primTraj = getTrajectory(primTrack);
+
+            // get the secondary track with the max number of hits
+            unsigned short maxHits = 0; int indice=0;
+            for( unsigned short i=0; i < secTracks.size(); i++) { 
+                   // Add references to daughters
+                   unsigned short nhits = secTracks[i]->numberOfValidHits();
+                   if( nhits > maxHits ) { maxHits = nhits; indice=i; }
+            }
+            FreeTrajectoryState secTraj = getTrajectory(secTracks[indice]);
+
+            // Closest points
+            ClosestApproachInRPhi *theApproach = new ClosestApproachInRPhi();
+            GlobalPoint crossing = theApproach->crossingPoint(primTraj,secTraj);
+            delete theApproach;
+
+            // Create vertex (creation point)
+            the_vertex = reco::Vertex(reco::Vertex::Point(crossing.x(),
+                                                          crossing.y(),
+                                                          crossing.z()),
+                                      reco::Vertex::Error(), 0.,0.,0);
+
+            the_vertex.add(reco::TrackBaseRef( secTracks[indice] ));
+            the_vertex.add(reco::TrackBaseRef( primTrack ));
+        }
      }
      else {
        // if no secondary tracks : vertex position = position of last rechit of the primary track 
