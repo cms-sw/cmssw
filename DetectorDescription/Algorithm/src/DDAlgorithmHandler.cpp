0beb7b5a135c1fbae1a079f96427d5c4af883f14
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2007-12-17 03:20:41 +0000

    remove a bunch of try-catch.  depend on fw to throw appropriate error.

--- DetectorDescription/Algorithm/src/DDAlgorithmHandler.cpp
+++ DetectorDescription/Algorithm/src/DDAlgorithmHandler.cpp
@@ -32,57 +32,13 @@ void DDAlgorithmHandler::initialize(const std::string & algoName,
 {
   std::pair<std::string,std::string> algoNmNs = DDSplit(algoName);
   algoname_ = algoName;
-  //  try {
-  //  edmplugin::PluginManager::configure(edmplugin::standard::config());
-  //}
-  //catch(...) {
-  //  std::cout << "FATAL!!! Could not initialize the PluginManager!!!" << std::endl;
-  //}
-
-  try {
-    //try the old name
-    algo_ = DDAlgorithmFactory::get()->create(algoNmNs.first);
-  } catch(const cms::Exception& ) { }
   DCOUT ('T',"ALGO: name=" + algoNmNs.first + " algo=" + algoName);
-  if (!algo_) {
-    try {
-      algo_ = DDAlgorithmFactory::get()->create(algoname_);
-    }catch(const cms::Exception& e) {
-      std::string message("no algorithm with name=\"" + algoName + "\" found. Becauses\n");
-      message += e.what();
-      throw DDException(message);
-    }
-  }
-  if (algo_) {
-    try {
-      algo_->setParent(parent);
-      algo_->initialize(nArgs,vArgs,mArgs,sArgs, svArgs);
-    }
-    catch(DDException e) {
-      throw;
-    }
-    catch(...) {
-      std::string message("initialization of algorithm name=\"" + algoName + "\" failed");
-      throw DDException(message);
-    }
-  }
-  else {
-      //should never get here
-      std::string message("no algorithm with name=\"" + algoName + "\" found.");
-      throw DDException(message);
-  }
+  algo_ = DDAlgorithmFactory::get()->create(algoname_);
+  algo_->setParent(parent);
+  algo_->initialize(nArgs,vArgs,mArgs,sArgs, svArgs);
 }
 
 void DDAlgorithmHandler::execute() throw (DDException)
 {
-  try {
-    algo_->execute();
-  }
-  catch(const DDException & e) {
-    throw;
-  }
-  catch(...) {
-    std::string message("execution of algorithm name=\"" + algoname_ + "\" failed!");
-    throw DDException(message);
-  }
+  algo_->execute();
 }
