8355114c52adfee8ec6e6fc4c64d0c0ae20dbf4d
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-12-18 22:54:35 +0000

    Modify Stepping action to take out string comparisons

--- SimG4Core/Application/interface/SteppingAction.h
+++ SimG4Core/Application/interface/SteppingAction.h
@@ -5,8 +5,10 @@
 #include "SimG4Core/Notification/interface/SimActivityRegistry.h"
 
 #include "G4UserSteppingAction.hh"
+#include "G4VPhysicalVolume.hh"
 
 class EventAction;
+class G4VTouchable;
 
 class SteppingAction: public G4UserSteppingAction
 {
@@ -19,12 +21,16 @@ public:
 private:
   void catchLowEnergyInVacuumHere(const G4Step * aStep);
   void catchLowEnergyInVacuumNext(const G4Step * aStep);
+  bool initPointer();
+  bool isThisVolume(const G4VTouchable* touch, G4VPhysicalVolume* pv);
 private:
   EventAction * eventAction_;
   bool   killBeamPipe;
   double theCriticalEnergyForVacuum;
   double theCriticalDensity;
   int    verbose;
+  bool   initialized;
+  G4VPhysicalVolume *tracker, *calo;
 };
 
 #endif
--- SimG4Core/Application/src/SteppingAction.cc
+++ SimG4Core/Application/src/SteppingAction.cc
@@ -2,6 +2,7 @@
 #include "SimG4Core/Application/interface/EventAction.h"
 
 #include "G4Track.hh"
+#include "G4PhysicalVolumeStore.hh"
 #include "G4UnitsTable.hh"
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -14,20 +15,22 @@ SteppingAction::SteppingAction(EventAction* e,const edm::ParameterSet & p)
     killBeamPipe(p.getParameter<bool>("KillBeamPipe")),
     theCriticalEnergyForVacuum(p.getParameter<double>("CriticalEnergyForVacuum")*MeV),
     theCriticalDensity(p.getParameter<double>("CriticalDensity")*g/cm3),
-    verbose(p.getUntrackedParameter<int>("Verbosity",0))
-{}
+    verbose(p.getUntrackedParameter<int>("Verbosity",0)), 
+    initialized(false), tracker(0), calo(0) {}
 
 SteppingAction::~SteppingAction() {}
 
 void SteppingAction::UserSteppingAction(const G4Step * aStep) {
+  if (!initialized) initialized = initPointer();
   m_g4StepSignal(aStep);
   if (killBeamPipe){
     catchLowEnergyInVacuumHere(aStep);
     catchLowEnergyInVacuumNext(aStep);
   }
   if (aStep->GetPostStepPoint()->GetPhysicalVolume() != 0) {
-    if(!(strcmp(aStep->GetPreStepPoint()->GetPhysicalVolume()->GetLogicalVolume()->GetName().c_str(),"Tracker")&& 
-	 strcmp(aStep->GetPostStepPoint()->GetPhysicalVolume()->GetLogicalVolume()->GetName().c_str(),"CALO"))){
+    bool ok = (isThisVolume(aStep->GetPreStepPoint()->GetTouchable(),tracker)&&
+	       isThisVolume(aStep->GetPostStepPoint()->GetTouchable(),calo));
+    if (ok) {
 
       math::XYZVectorD pos((aStep->GetPreStepPoint()->GetPosition()).x(),
 			   (aStep->GetPreStepPoint()->GetPosition()).y(),
@@ -85,4 +88,36 @@ void SteppingAction::catchLowEnergyInVacuumNext(const G4Step * aStep) {
       theTrack->SetTrackStatus(fStopButAlive);
     }
   }
+}
+
+bool SteppingAction::initPointer() {
+
+  const G4PhysicalVolumeStore * pvs = G4PhysicalVolumeStore::GetInstance();
+  if (pvs) {
+    std::vector<G4VPhysicalVolume*>::const_iterator pvcite;
+    for (pvcite = pvs->begin(); pvcite != pvs->end(); pvcite++) {
+      if ((*pvcite)->GetName() == "Tracker") tracker = (*pvcite);
+      if ((*pvcite)->GetName() == "CALO")    calo    = (*pvcite);
+      if (tracker && calo) break;
+    }
+    edm::LogInfo("SimG4CoreApplication") << "Pointer for Tracker " << tracker
+					 << " and for Calo " << calo;
+    if (tracker) LogDebug("SimG4CoreApplication") << "Tracker vol name "
+						  << tracker->GetName();
+    if (calo)    LogDebug("SimG4CoreApplication") << "Calorimeter vol name "
+						  << calo->GetName();
+    return true;
+  }
+  return false;
+}
+
+bool SteppingAction::isThisVolume(const G4VTouchable* touch, 
+				  G4VPhysicalVolume* pv) {
+
+  int level = ((touch->GetHistoryDepth())+1);
+  if (level > 0 && level >= 3) {
+    int ii = level - 3;
+    return (touch->GetVolume(ii) == pv);
+  }
+  return false;
 }
