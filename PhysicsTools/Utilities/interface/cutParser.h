695f960e22e81481b6cfaa350f789a91af1ec810
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-12-19 10:18:14 +0000

    not exposing MethodMap in cutParser

--- PhysicsTools/Utilities/interface/cutParser.h
+++ PhysicsTools/Utilities/interface/cutParser.h
@@ -2,11 +2,17 @@
 #define PhysicsTools_Utilities_cutParset_h
 #include "PhysicsTools/Utilities/interface/MethodMap.h"
 #include "PhysicsTools/Utilities/src/SelectorPtr.h"
+#include "PhysicsTools/Utilities/src/Grammar.h"
 #include <string>
 
 namespace reco {
   namespace parser {
-    bool cutParser( const std::string &, const MethodMap &, SelectorPtr & );
+    template<typename T>
+    bool cutParser(const std::string & value, SelectorPtr & sel) {
+      using namespace boost::spirit;
+      Grammar grammar(reco::MethodMap::methods<T>(), sel);
+      return parse(value.c_str(), grammar.use_parser<0>(), space_p).full;
+    } 
   }
 }
 
