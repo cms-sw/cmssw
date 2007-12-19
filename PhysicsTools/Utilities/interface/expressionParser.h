30203056a60749e97ec3c63e6461f5cf9223c05d
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-12-19 10:26:38 +0000

    removed method map exposition from expressionParser

--- PhysicsTools/Utilities/interface/expressionParser.h
+++ PhysicsTools/Utilities/interface/expressionParser.h
@@ -2,11 +2,17 @@
 #define PhysicsTools_Utilities_expressionParset_h
 #include "PhysicsTools/Utilities/interface/MethodMap.h"
 #include "PhysicsTools/Utilities/src/ExpressionPtr.h"
+#include "PhysicsTools/Utilities/src/Grammar.h"
 #include <string>
 
 namespace reco {
   namespace parser {
-    bool expressionParser( const std::string &, const MethodMap &, ExpressionPtr & );
+    template<typename T>
+    bool expressionParser( const std::string & value, ExpressionPtr & expr) {
+      using namespace boost::spirit;
+      Grammar grammar(reco::MethodMap::methods<T>(), expr);
+      return parse(value.c_str(), grammar.use_parser<1>(), space_p).full;
+    }
   }
 }
 
