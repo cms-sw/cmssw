c98d708e7758aafb3ca012469266bb080608bdef
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2007-12-21 15:51:29 +0000

    Added TrackMultiSelector

--- RecoTracker/FinalTrackSelectors/src/SealModules.cc
+++ RecoTracker/FinalTrackSelectors/src/SealModules.cc
@@ -4,7 +4,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoTracker/FinalTrackSelectors/interface/SimpleTrackListMerger.h"
+#include "RecoTracker/FinalTrackSelectors/interface/TrackMultiSelector.h"
 
 using cms::SimpleTrackListMerger;
+using reco::modules::TrackMultiSelector;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SimpleTrackListMerger);
+DEFINE_ANOTHER_FWK_MODULE(TrackMultiSelector);
