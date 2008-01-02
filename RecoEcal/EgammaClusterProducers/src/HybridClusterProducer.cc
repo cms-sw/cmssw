8bc7e9422cdf4f5cdd1926dd8fd47a2a2a976c6e
Dave Evans <sha1-fd5bf8c7bc363fe103f075cd5169aefdf85f5f6f@cern.ch>
2008-01-02 16:55:10 +0000

    allow choice of fixed or dynamic phi road

--- RecoEcal/EgammaClusterProducers/src/HybridClusterProducer.cc
+++ RecoEcal/EgammaClusterProducers/src/HybridClusterProducer.cc
@@ -63,6 +63,7 @@ HybridClusterProducer::HybridClusterProducer(const edm::ParameterSet& ps)
 
   hybrid_p = new HybridClusterAlgo(ps.getParameter<double>("HybridBarrelSeedThr"), 
                                    ps.getParameter<int>("step"),
+				   ps.getParameter<bool>("dynamicPhiRoad"),
                                    ps.getParameter<double>("ethresh"),
                                    ps.getParameter<double>("eseed"),
                                    ps.getParameter<double>("ewing"),
