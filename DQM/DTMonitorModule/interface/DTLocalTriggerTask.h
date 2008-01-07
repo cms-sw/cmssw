1a33ebe8a5f25cb5d0f3f72b610f3d098228262e
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2008-01-07 14:33:40 +0000

    Added comparison plot, fixed best track selection, separated BXvsQual plot for 1st/2nd tracks

--- DQM/DTMonitorModule/interface/DTLocalTriggerTask.h
+++ DQM/DTMonitorModule/interface/DTLocalTriggerTask.h
@@ -4,8 +4,8 @@
 /*
  * \file DTLocalTriggerTask.h
  *
- * $Date: 2007/11/12 18:00:30 $
- * $Revision: 1.13 $
+ * $Date: 2007/11/19 14:32:02 $
+ * $Revision: 1.14 $
  * \author M. Zanetti - INFN Padova
  *
 */
@@ -21,8 +21,6 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "DataFormats/LTCDigi/interface/LTCDigi.h"
-
 #include "DQMServices/Core/interface/DaqMonitorBEInterface.h"
 #include "DQMServices/Core/interface/MonitorElementBaseT.h"
 #include "DQMServices/Daemon/interface/MonitorDaemon.h"
@@ -35,6 +33,7 @@
 class DTGeometry;
 class DTChamberId;
 class DTRecSegment4D;
+class DTLocalTrigger;
 class L1MuDTChambPhDigi;
 class L1MuDTChambThDigi;
 
@@ -63,7 +62,7 @@ class DTLocalTriggerTask: public edm::EDAnalyzer{
   std::pair<float,float> phiRange(const DTChamberId& id);
 
   /// Compute track coordinates using trigger SC sectors
-  void computeCoordinates(const DTRecSegment4D& track, int& scsector, float& phpos, float& phdir, float& zpos, float& zdir);
+  void computeCoordinates(const DTRecSegment4D* track, int& scsector, float& phpos, float& phdir, float& zpos, float& zdir);
 
   /// Convert phi to local x coordinate
   float phi2Pos(const DTChamberId & id, int phi);
@@ -80,6 +79,9 @@ class DTLocalTriggerTask: public edm::EDAnalyzer{
   /// Run analysis using DT 4D segments
   void runSegmentAnalysis(const edm::Event& e, std::string& trigsrc);
 
+  /// Run analysis on ROS data
+  void runDDUvsDCCAnalysis(std::string& trigsrc);
+   
   /// Load DTTF map correction
   void loadDTTFMap();
 
@@ -107,14 +109,14 @@ class DTLocalTriggerTask: public edm::EDAnalyzer{
   std::string outputFile;  
   int nevents;
  
-  int phcode_best[6][5][13];  
+  int phcode_best[6][5][13];
   int dduphcode_best[6][5][13];
   int thcode_best[6][5][13];  
   int dduthcode_best[6][5][13];
   int mapDTTF[6][13][2];
   const L1MuDTChambPhDigi* iphbest[6][5][13];
+  const DTLocalTrigger*    iphbestddu[6][5][13];
   const L1MuDTChambThDigi* ithbest[6][5][13];
-  bool track_flag[6][5][15];
   bool track_ok[6][5][15];
 
   DaqMonitorBEInterface* dbe;
