65f44f12c8b2c82bae06cf8baf483ca99d3ce3ab
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-01-09 13:52:33 +0000

    Removing classes for old HLT data model.

--- DataFormats/HLTReco/src/classes.h
+++ DataFormats/HLTReco/src/classes.h
@@ -1,7 +1,4 @@
 #include "DataFormats/HLTReco/interface/HLTResult.h"
-#include "DataFormats/HLTReco/interface/HLTFilterObject.h"
-#include "DataFormats/HLTReco/interface/HLTPathObject.h"
-#include "DataFormats/HLTReco/interface/HLTGlobalObject.h"
 #include "DataFormats/HLTReco/interface/ModuleTiming.h"
 #include "DataFormats/HLTReco/interface/HLTPerformanceInfo.h"
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
@@ -31,26 +28,6 @@ namespace {
     edm::Wrapper<reco::HLTResult< 8> > w1;
     edm::Wrapper<reco::HLTResult<16> > w2;
     edm::Wrapper<reco::HLTResult<24> > w3;
-
-    reco::HLTFilterObjectBase                     hlt1;
-    reco::HLTFilterObject                         hlt2;
-    reco::HLTFilterObjectWithRefs                 hlt3;
-    reco::HLTPathObject                           hlt4;
-    reco::HLTGlobalObject                         hlt6;
-
-    edm::RefProd<reco::HLTFilterObjectBase>       r0;
-    edm::RefProd<reco::HLTFilterObject>           r1;
-    edm::RefProd<reco::HLTFilterObjectWithRefs>   r2;
-
-    edm::reftobase::Holder<reco::HLTFilterObjectBase, edm::RefProd<reco::HLTFilterObjectBase> > rb0;
-    edm::reftobase::Holder<reco::HLTFilterObjectBase, edm::RefProd<reco::HLTFilterObject    > > rb1;
-    edm::reftobase::Holder<reco::HLTFilterObjectBase, edm::RefProd<reco::HLTFilterObjectWithRefs> > rb2;
-
-    edm::Wrapper<reco::HLTFilterObjectBase>      whlt1;
-    edm::Wrapper<reco::HLTFilterObject>          whlt2;
-    edm::Wrapper<reco::HLTFilterObjectWithRefs>  whlt3;
-    edm::Wrapper<reco::HLTPathObject>            whlt4;
-    edm::Wrapper<reco::HLTGlobalObject>          whlt6;
 
     edm::EventTime                                et0;
 
