575527af9fe8f76b19fc281802140e309038ce3d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-01-10 16:19:28 +0000

    Add Pi0 filter module as plugin.

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -5,9 +5,11 @@
 #include "HLTrigger/special/interface/HLTPixelIsolTrackFilter.h"
 #include "HLTrigger/special/interface/HLTEcalIsolationFilter.h"
 #include "HLTrigger/special/interface/HLTEcalPhiSymFilter.h"
+#include "HLTrigger/special/interface/HLTPi0RecHitsFilter.h"
 
 DEFINE_FWK_MODULE(HLTPixlMBFilt);
 DEFINE_FWK_MODULE(HLTPixlMBForAlignmentFilter);
 DEFINE_FWK_MODULE(HLTPixelIsolTrackFilter);
 DEFINE_FWK_MODULE(HLTEcalIsolationFilter);
 DEFINE_FWK_MODULE(HLTEcalPhiSymFilter);
+DEFINE_FWK_MODULE(HLTPi0RecHitsFilter);
