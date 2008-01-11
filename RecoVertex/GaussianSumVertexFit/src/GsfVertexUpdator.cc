eaecdf7c454d3c966b9e9a1b81f9a4f3566bb441
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2008-01-11 13:34:46 +0000

    Remove exceptions thrown during fit and improve problem reporting

--- RecoVertex/GaussianSumVertexFit/src/GsfVertexUpdator.cc
+++ RecoVertex/GaussianSumVertexFit/src/GsfVertexUpdator.cc
@@ -1,5 +1,6 @@
 #include "RecoVertex/GaussianSumVertexFit/interface/GsfVertexUpdator.h"
 #include "RecoVertex/GaussianSumVertexFit/interface/BasicMultiVertexState.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include <cfloat>
 
 GsfVertexUpdator::GsfVertexUpdator(bool limit, const GsfVertexMerger * merger) :
@@ -49,6 +50,7 @@ CachingVertex<5> GsfVertexUpdator::add(const CachingVertex<5> & oldVertex,
   	trackCompIter != ltComponents.end(); trackCompIter++ ) {
       newVertexComponents.push_back(
         createNewComponent(*vertexCompIter, *trackCompIter, trackWeight, +1));
+      if (!newVertexComponents.back().first.isValid()) return CachingVertex<5>(); // return invalid vertex
     }
   }
 //   cout << "updator components: "<<newVertexComponents.size()<<endl;
@@ -111,15 +113,17 @@ GsfVertexUpdator::createNewComponent(const VertexState & oldVertex,
 
   // Weight of the component in the mixture (non-normalized)
   double weightInMixture = theWeightCalculator.calculate(oldVertex, linTrack, 1.E9);
+  if (weightInMixture < 0.) return VertexComponent(VertexState(), WeightChi2Pair(0.,0.));
 
   // position estimate of the component
   VertexState newVertex = kalmanVertexUpdator.positionUpdate(oldVertex, 
   				linTrack, weight, sign);
-// cout << "c"<<endl;
+  if (!newVertex.isValid()) return VertexComponent(newVertex, WeightChi2Pair(0.,0.));
 
   //Chi**2 contribution of the component
   float chi2 = kalmanVertexUpdator.chi2Increment(oldVertex, newVertex, 
   				linTrack, weight);
+  if (chi2 < 0.) return VertexComponent(VertexState(), WeightChi2Pair(0.,0.));
 //         cout << "Update: "<<oldVertex.position()<<" "<<newVertex.position()<<" "<<chi2
 // 	     <<" "<<linTrack->weightInMixture()<<" "<<weightInMixture<<endl;
 
@@ -144,8 +148,9 @@ GsfVertexUpdator::VertexChi2Pair GsfVertexUpdator::assembleVertexComponents(
   }
 // cout << "totalWeight "<<totalWeight<<endl;
   if (totalWeight<DBL_MIN) {
-    throw VertexException
-      ("GsfVertexUpdator:: Updated Vertex has total weight of 0.");
+    edm::LogWarning("GsfVertexUpdator") << "Updated Vertex has total weight of 0. "
+    <<"The track is probably very far away.";
+    return VertexChi2Pair( VertexState(), 0.);
   }
 
   for (vector<VertexComponent>::const_iterator iter = newVertexComponents.begin();
@@ -161,8 +166,8 @@ GsfVertexUpdator::VertexChi2Pair GsfVertexUpdator::assembleVertexComponents(
 // cout << "vertexComponents "<<vertexComponents.size()<<endl;
 
   if (vertexComponents.empty()){
-    throw VertexException
-      ("GsfVertexUpdator:: No Vertex State left after reweighting.");
+    edm::LogWarning("GsfVertexUpdator") << "No Vertex State left after reweighting.";
+    return VertexChi2Pair( VertexState(), 0.);
   }
 
   return VertexChi2Pair( VertexState( new BasicMultiVertexState( vertexComponents)),
