b09eb66bbb8e097b9c2f79f692bd78dfecb236e1
ewidl <sha1-3c65bb8ad5ab20d6db077c9cd74c65f6de0120f6@cern.ch>
2008-01-11 15:18:58 +0000

    added CombinedTrajectoryFactory

---
+++ Alignment/ReferenceTrajectories/plugins/CombinedTrajectoryFactory.cc
@@ -0,0 +1,60 @@
+// Do not include .h from plugin directory, but locally:
+#include "CombinedTrajectoryFactory.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "Alignment/ReferenceTrajectories/interface/TrajectoryFactoryPlugin.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+CombinedTrajectoryFactory::CombinedTrajectoryFactory( const edm::ParameterSet & config ) :
+  TrajectoryFactoryBase( config )
+{
+  vector<string> factoryNames = config.getParameter< vector<string> >( "TrajectoryFactoryNames" );
+  vector<string>::iterator itFactoryName;
+  for ( itFactoryName = factoryNames.begin(); itFactoryName != factoryNames.end(); ++itFactoryName )
+  {
+cout << "add factory: " << *itFactoryName << endl;
+    theFactories.push_back( TrajectoryFactoryPlugin::get()->create( *itFactoryName, config ) );
+  }
+}
+
+ 
+CombinedTrajectoryFactory::~CombinedTrajectoryFactory( void ) {}
+
+
+const CombinedTrajectoryFactory::ReferenceTrajectoryCollection
+CombinedTrajectoryFactory::trajectories( const edm::EventSetup & setup,
+					  const ConstTrajTrackPairCollection & tracks ) const
+{
+  ReferenceTrajectoryCollection trajectories;
+
+  vector< TrajectoryFactoryBase* >::const_iterator itFactory;
+  for ( itFactory = theFactories.begin(); trajectories.empty() && ( itFactory != theFactories.end() ); ++itFactory )
+  {
+    trajectories = ( *itFactory )->trajectories( setup, tracks );
+  }
+
+  return trajectories;
+}
+
+const CombinedTrajectoryFactory::ReferenceTrajectoryCollection
+CombinedTrajectoryFactory::trajectories( const edm::EventSetup & setup,
+					  const ConstTrajTrackPairCollection& tracks,
+					  const ExternalPredictionCollection& external ) const
+{
+  ReferenceTrajectoryCollection trajectories;
+
+  vector< TrajectoryFactoryBase* >::const_iterator itFactory;
+  for ( itFactory = theFactories.begin(); trajectories.empty() && ( itFactory != theFactories.end() ); ++itFactory )
+  {
+    trajectories = ( *itFactory )->trajectories( setup, tracks, external );
+  }
+
+  return trajectories;
+}
+
+
+DEFINE_EDM_PLUGIN( TrajectoryFactoryPlugin, CombinedTrajectoryFactory, "CombinedTrajectoryFactory" );
