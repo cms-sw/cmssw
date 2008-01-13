e8381b4aafc131c0c8567f2d6c059a470b5f259d
Paolo Azzurri <sha1-685b616caa4a1ff5ce8554bbb7f424196046512a@cern.ch>
2008-01-13 21:40:14 +0000

    Added AnalyticalTrackSelector

--- RecoTracker/FinalTrackSelectors/src/SealModules.cc
+++ RecoTracker/FinalTrackSelectors/src/SealModules.cc
@@ -5,9 +5,12 @@
 
 #include "RecoTracker/FinalTrackSelectors/interface/SimpleTrackListMerger.h"
 #include "RecoTracker/FinalTrackSelectors/interface/TrackMultiSelector.h"
+#include "RecoTracker/FinalTrackSelectors/interface/AnalyticalTrackSelector.h"
 
 using cms::SimpleTrackListMerger;
 using reco::modules::TrackMultiSelector;
+using reco::modules::AnalyticalTrackSelector;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SimpleTrackListMerger);
 DEFINE_ANOTHER_FWK_MODULE(TrackMultiSelector);
+DEFINE_ANOTHER_FWK_MODULE(AnalyticalTrackSelector);
