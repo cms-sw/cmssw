25fd27257bd2798c4651c73fde57ca6eddc1ae9c
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-01-14 14:08:18 +0000

    supressing volunteer tracks in muon trigger

--- HLTrigger/Muon/src/SealModule.cc
+++ HLTrigger/Muon/src/SealModule.cc
@@ -3,11 +3,19 @@
 
 #include "HLTrigger/Muon/interface/HLTMuonL1Filter.h"
 #include "HLTrigger/Muon/interface/HLTMuonPreFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonL2PreFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonL3PreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonIsoFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonDimuonFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonDimuonL2Filter.h"
+#include "HLTrigger/Muon/interface/HLTMuonDimuonL3Filter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonL1Filter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonL2PreFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonL3PreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonPreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonIsoFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonL2Filter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonL3Filter);
