3c5ef2edd72c90d657b25aa34ea45e59e260a00e
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2008-01-14 16:48:18 +0000

    noise calibration added to the SealModule

--- CalibMuon/DTCalibration/plugins/SealModule.cc
+++ CalibMuon/DTCalibration/plugins/SealModule.cc
@@ -9,6 +9,8 @@
 #include "CalibMuon/DTCalibration/plugins/DTT0Calibration.h"
 #include "CalibMuon/DTCalibration/plugins/DTVDriftCalibration.h"
 #include "CalibMuon/DTCalibration/plugins/DTVDriftWriter.h"
+#include "CalibMuon/DTCalibration/plugins/DTNoiseComputation.h"
+#include "CalibMuon/DTCalibration/plugins/DTNoiseCalibration.h"
 #include "CalibMuon/DTCalibration/plugins/DTFakeTTrigESProducer.h"
 #include "CalibMuon/DTCalibration/plugins/DTFakeT0ESProducer.h"
 
@@ -20,5 +22,7 @@ DEFINE_ANOTHER_FWK_MODULE(DTTTrigWriter);
 DEFINE_ANOTHER_FWK_MODULE(DTT0Calibration);
 DEFINE_ANOTHER_FWK_MODULE(DTVDriftCalibration);
 DEFINE_ANOTHER_FWK_MODULE(DTVDriftWriter);
+DEFINE_ANOTHER_FWK_MODULE(DTNoiseComputation);
+DEFINE_ANOTHER_FWK_MODULE(DTNoiseCalibration);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(DTFakeTTrigESProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(DTFakeT0ESProducer);
