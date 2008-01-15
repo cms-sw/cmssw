9ba2c3cd13c555247039041d547fa6a380ed56ba
roberfro <sha1-3ef59c6cd087c1ab5075a623fd6cc2bdbd41546d@cern.ch>
2008-01-15 11:43:56 +0000

    Add of PFNuclearproducer as new module

--- RecoParticleFlow/PFTracking/src/SealModule.cc
+++ RecoParticleFlow/PFTracking/src/SealModule.cc
@@ -4,8 +4,10 @@
 #include "RecoParticleFlow/PFTracking/interface/PFElecTkProducer.h"
 #include "RecoParticleFlow/PFTracking/interface/VertexFilter.h"
 #include "RecoParticleFlow/PFTracking/interface/LightPFTrackProducer.h"
+#include "RecoParticleFlow/PFTracking/interface/PFNuclearProducer.h"
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(GoodSeedProducer);
 DEFINE_ANOTHER_FWK_MODULE(PFElecTkProducer);
 DEFINE_ANOTHER_FWK_MODULE(VertexFilter);
 DEFINE_ANOTHER_FWK_MODULE(LightPFTrackProducer);
+DEFINE_ANOTHER_FWK_MODULE(PFNuclearProducer);
