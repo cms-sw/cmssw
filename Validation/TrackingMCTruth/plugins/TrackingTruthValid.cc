ebd225eaed1f5be11d1ddf5684adfeedbc0088e7
riccardo <sha1-a8fe582e28bc10906e7eb2fd4c1098a4a0c0276f@cern.ch>
2008-01-16 15:18:24 +0000

    Renaming of axis labels for the "process" histogram

--- Validation/TrackingMCTruth/plugins/TrackingTruthValid.cc
+++ Validation/TrackingMCTruth/plugins/TrackingTruthValid.cc
@@ -47,7 +47,7 @@ TrackingTruthValid::TrackingTruthValid(const edm::ParameterSet& conf) {
   meTPMass = dbe_->book1D("TPMass","Tracking Particle Mass",100, -1,+5.);
   meTPCharge = dbe_->book1D("TPCharge","Tracking Particle Charge",10, -5, 5);
   meTPId = dbe_->book1D("TPId","Tracking Particle Id",500, -5000, 5000);
-  meTPProc = dbe_->book1D("TPProc","Tracking Particle Proc",20, 0, 20);
+  meTPProc = dbe_->book1D("TPProc","Tracking Particle Proc",20, -0.5, 19.5);
   meTPAllHits = dbe_->book1D("TPAllHits", "Tracking Particle All Hits", 200, -0.5, 199.5);
   meTPMatchedHits = dbe_->book1D("TPMatchedHits", "Tracking Particle Matched Hits", 100, -0.5, 99.5);
   meTPPt = dbe_->book1D("TPPt", "Tracking Particle Pt",100, 0, 100.);
@@ -58,6 +58,29 @@ TrackingTruthValid::TrackingTruthValid(const edm::ParameterSet& conf) {
   meTPVtxZ = dbe_->book1D("TPVtxZ", "Tracking Particle VtxZ",100, -100, 100.);
   meTPtip = dbe_->book1D("TPtip", "Tracking Particle tip",100, 0, 1000.);
   meTPlip = dbe_->book1D("TPlip", "Tracking Particle lip",100, 0, 100.);
+  
+  
+  // Prepare Axes Labels for Processes
+  meTPProc->setBinLabel( 1,"Undefined");            // value =  0
+  meTPProc->setBinLabel( 2,"Unknown");              // value =  1
+  meTPProc->setBinLabel( 3,"Primary");              // value =  2
+  meTPProc->setBinLabel( 4,"Hadronic");             // value =  3   
+  meTPProc->setBinLabel( 5,"Decay");                // value =  4 
+  meTPProc->setBinLabel( 6,"Compton");              // value =  5
+  meTPProc->setBinLabel( 7,"Annihilation");         // value =  6
+  meTPProc->setBinLabel( 8,"EIoni");                // value =  7
+  meTPProc->setBinLabel( 9,"HIoni");                // value =  8
+  meTPProc->setBinLabel(10,"MuIoni");               // value =  9
+  meTPProc->setBinLabel(11,"Photon");               // value = 10
+  meTPProc->setBinLabel(12,"MuPairProd");           // value = 11
+  meTPProc->setBinLabel(13,"Conversions");          // value = 12
+  meTPProc->setBinLabel(14,"EBrem");                // value = 13
+  meTPProc->setBinLabel(15,"SynchrotronRadiation"); // value = 14
+  meTPProc->setBinLabel(16,"MuBrem");               // value = 15
+  meTPProc->setBinLabel(17,"MuNucl");               // value = 16
+  meTPProc->setBinLabel(18,"");
+  meTPProc->setBinLabel(19,"");
+  meTPProc->setBinLabel(20,"");
 }
 
 void TrackingTruthValid::analyze(const edm::Event& event, const edm::EventSetup& c){
