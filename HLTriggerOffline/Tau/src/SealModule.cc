e20cab0b86ce81ebeb3ae0419f0554bfc8b86257
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-01-16 12:21:44 +0000

    Removed TauJetFilter

--- HLTriggerOffline/Tau/src/SealModule.cc
+++ HLTriggerOffline/Tau/src/SealModule.cc
@@ -3,11 +3,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "HLTriggerOffline/Tau/interface/HLTTauMcInfo.h"
 #include "HLTriggerOffline/Tau/interface/HLTTauAnalyzer.h"
-#include "HLTriggerOffline/Tau/interface/TauJetMCFilter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(TauJetMCFilter);
 
 
