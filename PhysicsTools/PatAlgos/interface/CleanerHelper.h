cf6710795e69666aa53abf38acc64a92acba8352
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2008-01-16 01:19:25 +0000

    1) Fixed serious bugs 2) Changed the interface so that now both the source edm::View and the    std::vector of selected items can be accessed 3) Allowed for "marking" the selected items, and eventually save in the    event only those with mark == 0)    (much easier that to allow user to unselect a selected item)

--- PhysicsTools/PatAlgos/interface/CleanerHelper.h
+++ PhysicsTools/PatAlgos/interface/CleanerHelper.h
@@ -17,34 +17,56 @@ namespace pat { namespace helper {
 template<typename T, typename T2=T, typename Collection=std::vector<T2> >
 class CleanerHelper {
     public:
+        CleanerHelper() { } // needed for EDM Modules
         CleanerHelper(const edm::InputTag &src, const std::string &instanceName="") ;
         ~CleanerHelper() { }
 
         /// read data from the event and setup internal containers
         void newEvent(edm::Event &iEvent);
 
-        /// put data in the event
+        /// Put data in the event and clear transient containers.
+        /// Only items with mark == 0 (which is the default) will be saved.
         void done();
 
         /// a reference to a view of the the source collection
-        const edm::View<T> & view() const { return *sourceView_; }
+        const edm::View<T> & source() const { return *sourceView_; }
 
         /// size of the source collection
-        size_t size() const { return sourceView_->size(); }
+        size_t srcSize() const { return sourceView_->size(); }
 
-        /// a reference to the view 
-        const T & operator[](size_t i) const { return (*sourceView_)[i]; }
+        /// a reference to an item in the source collection
+        const T & srcAt(size_t i) const { return (*sourceView_)[i]; }
+        /// a reference to an item in the source collection
+        edm::RefToBase<T> srcRefAt(size_t i) const { return sourceView_->refAt(i); }
 
-        /// says that item idx is selected and possibly specifies a new value for it
-        void addItem(size_t idx, const T2 &value) ;
-        //void addItem(size_t idx, 
-        //        const T2 &value = dynamic_cast<const T2 &>((*sourceView_)[idx])) ;
+        /// A const reference to the selected items. 
+        /// Must be const, to avoid people doing push_back or remove by hand and breaking the refs.
+        const Collection & selected() const { return selected_; }
+        /// Size of the collection of selected items
+        size_t size() const { return selected_.size(); }
+        /// A reference to the i-th selected item (const)
+        const T2 & operator[](size_t i) const { return selected_[i]; }
+        /// A reference to the i-th selected item. Non const, you are allowed to modify the item.
+        T2 & operator[](size_t i) { return selected_[i]; }
+
+        /// Mark an item (given its index in the selected collection).
+        /// At the end only items with mark == 0 will be saved
+        void setMark(size_t selIdx, uint32_t mark) { marks_[selIdx] = mark; }
+        /// Get the mark of an item (given its index in the selected collection)
+        /// At the end only items with mark == 0 will be saved
+        uint32_t mark(size_t selIdx) { return marks_[selIdx]; }
+
+        /// Inform the helper that item with index sourceIdx in the source collection is selected,
+        /// and it's value in the new collection is "value" (that can just be original object if you only select)
+        /// It also allows to set a transient marks on the items (in the end only items with mark == 0 will be saved)
+        /// Returns the index in the collection of selected items.
+        size_t addItem(size_t sourceIdx, const T2 &value, uint32_t mark=0) ;
 
         /// Pt sort
         typedef size_t first_argument_type;
         typedef size_t second_argument_type;
         bool operator()( const size_t & t1, const size_t & t2 ) const {
-            return (*sourceView_)[t1].pt() > (*sourceView_)[t2].pt();
+            return selected_[t1].pt() > selected_[t2].pt();
         }
 
     private:
@@ -60,6 +82,7 @@ class CleanerHelper {
         edm::RefProd<Collection> outRefProd_;
         std::vector<reco::CandidateBaseRef> originalRefs_;
         Collection selected_;
+        std::vector<uint32_t> marks_;
         // temporary but we keep them to avoid re allocating  memory
         std::vector<size_t> indices_;
         std::vector<reco::CandidateBaseRef> sortedRefs_; 
@@ -88,10 +111,13 @@ void CleanerHelper<T,T2,Collection>::newEvent(edm::Event &iEvent)
 }
 
 template<typename T, typename T2, typename Collection>
-void CleanerHelper<T,T2,Collection>::addItem(size_t idx, const T2 &value) 
+size_t CleanerHelper<T,T2,Collection>::addItem(size_t idx, const T2 &value, const uint32_t mark) 
 {
-    edm::RefToBase<T> backRef(sourceView_, idx);
     selected_.push_back(value);
+    marks_.push_back(mark);
+    edm::RefToBase<T> backRef(sourceView_, idx); // I need to pass from RefToBase<T> to reach RefToBase<Candidate>
+    originalRefs_.push_back(reco::CandidateBaseRef(backRef));
+    return selected_.size() - 1;
 }
 
 template<typename T, typename T2, typename Collection>
@@ -111,11 +137,13 @@ void CleanerHelper<T,T2,Collection>::done() {
     std::sort(indices_.begin(), indices_.end(), *this);
 
     // step 3: use sorted indices
-    std::auto_ptr<Collection> sorted(new Collection(nselected));
-    sortedRefs_.resize(nselected);
+    std::auto_ptr<Collection> sorted(new Collection()); sorted->reserve(nselected);
+    sortedRefs_.reserve(nselected);
     for (size_t i = 0; i < nselected; ++i) {
-        (*sorted)[i]     = selected_[indices_[i]];
-        sortedRefs_[i]   = originalRefs_[indices_[i]];
+        size_t idx = indices_[i];
+        if (marks_[idx] != 0) continue; // skip marked items
+        sorted->push_back( selected_[idx] );
+        sortedRefs_.push_back( originalRefs_[idx] );
     }
 
     // fill in backrefs
@@ -132,6 +160,7 @@ void CleanerHelper<T,T2,Collection>::done() {
 template<typename T, typename T2, typename Collection>
 void CleanerHelper<T,T2,Collection>::cleanup() {
     selected_.clear();
+    marks_.clear();
     originalRefs_.clear();
     sortedRefs_.clear();
 
