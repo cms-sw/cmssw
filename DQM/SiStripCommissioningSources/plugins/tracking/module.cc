b29288218348c6bb12acfb6e92a95531db807fba
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2008-01-17 16:51:42 +0000

    added code to preselect digis for latency and fine delay, using tracking.

---
+++ DQM/SiStripCommissioningSources/plugins/tracking/module.cc
@@ -0,0 +1,5 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+
+#include "DQM/SiStripCommissioningSources/plugins/tracking/SiStripFineDelayHit.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripFineDelayHit);
