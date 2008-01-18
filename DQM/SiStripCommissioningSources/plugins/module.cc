7dbd7174e1e48b0ef5318f21a2589e1d7e019a7e
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2008-01-18 14:07:37 +0000

    to have different paths depending on the run type

--- DQM/SiStripCommissioningSources/plugins/module.cc
+++ DQM/SiStripCommissioningSources/plugins/module.cc
@@ -3,4 +3,7 @@ DEFINE_SEAL_MODULE();
 
 #include "DQM/SiStripCommissioningSources/interface/SiStripCommissioningSource.h"
 DEFINE_ANOTHER_FWK_MODULE(SiStripCommissioningSource);
-  
+
+#include "DQM/SiStripCommissioningSources/interface/SiStripCommissioningRunTypeFilter.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripCommissioningRunTypeFilter);
+
