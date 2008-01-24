83bfc70531b1fcd39a16e1f6808a1035a8253a4a
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-01-24 15:27:19 +0000

    removing files moved to plugins

--- DQM/HLTEvF/src/SealModule.cc
+++
@@ -1,6 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(HLTMuonDQMSource);
