2025ef97802ae003531e20a12f53ddf778e6c026
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-01-24 22:52:10 +0000

    Temporarily comment out two bits of code that do not work with POOL_2_7_0, replacing them with assert(0). One of these functions is not actually used and the other is used only in the PoolDBOutputService. A tag with this revision will be used only in 19X testing.

--- CondCore/DBCommon/src/ObjectRelationalMappingUtility.cc
+++ CondCore/DBCommon/src/ObjectRelationalMappingUtility.cc
@@ -1,5 +1,6 @@
 #include "CondCore/DBCommon/interface/ObjectRelationalMappingUtility.h"
 #include "ObjectRelationalAccess/ObjectRelationalMappingUtilities.h"
+#include <cassert>
 cond::ObjectRelationalMappingUtility::ObjectRelationalMappingUtility( coral::ISessionProxy* coralsessionHandle ){
   m_mappingutil=new pool::ObjectRelationalMappingUtilities( coralsessionHandle );
 }
@@ -7,10 +8,16 @@ cond::ObjectRelationalMappingUtility::~ObjectRelationalMappingUtility(){
   delete m_mappingutil;
 }
 void cond::ObjectRelationalMappingUtility::buildAndStoreMappingFromBuffer( const std::string& buffer ){
-  m_mappingutil->buildAndStoreMappingFromBuffer( buffer.c_str() );
+  // The following is temporarily commented out (and replaced with the
+  // assert) pending migration to POOL_2_7_0
+  //m_mappingutil->buildAndStoreMappingFromBuffer( buffer.c_str() );
+  assert(0); 
 }
 void cond::ObjectRelationalMappingUtility::listMappings( std::vector<std::string>& mappinglist ){
-  mappinglist=m_mappingutil->listMappings();
+  // The following is temporarily commented out (and replaced with the
+  // assert) pending migration to POOL_2_7_0
+  //mappinglist=m_mappingutil->listMappings();
+  assert(0); 
 }
 bool cond::ObjectRelationalMappingUtility::existsMapping(const std::string& version){
   return m_mappingutil->existsMapping(version);
