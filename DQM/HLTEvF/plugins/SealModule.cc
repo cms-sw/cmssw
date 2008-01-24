6c3b849cd14d09b193a0da2b55d7fbc48851b7eb
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-01-24 15:25:37 +0000

    qdding this to plugins rather than src

--- DQM/HLTEvF/plugins/SealModule.cc
+++ DQM/HLTEvF/plugins/SealModule.cc
@@ -1,9 +1,10 @@
-// $Id: SealModule.cc,v 1.3 2007/04/10 23:07:34 wmtan Exp $
+// $Id: SealModule.cc,v 1.1 2007/04/23 22:58:54 bdahmes Exp $
 // 
 
 #include "DQM/HLTEvF/interface/HltAnalyzer.h"
 #include "DQM/HLTEvF/interface/PathTimerService.h"
 #include "DQM/HLTEvF/interface/PathTimerInserter.h"
+#include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
@@ -15,5 +16,7 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HltAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PathTimerInserter);
 DEFINE_ANOTHER_FWK_SERVICE(PathTimerService);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonDQMSource);
+
 //DEFINE_ANOTHER_FWK_SERVICE_MAKER(PathTimerService,PathTimerServiceMaker);
  
