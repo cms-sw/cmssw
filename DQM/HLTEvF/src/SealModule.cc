d8acf5c8c7e2d26d69a9ac88da8260fae05eaaee
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-01-24 15:07:17 +0000

    first commit

---
+++ DQM/HLTEvF/src/SealModule.cc
@@ -0,0 +1,6 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonDQMSource);
