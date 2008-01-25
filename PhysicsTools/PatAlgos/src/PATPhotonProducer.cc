2f53caf1029004971688853c68c9caa5f08229b8
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2008-01-25 13:53:15 +0000

    First version of a PAT Photon producer.

---
+++ PhysicsTools/PatAlgos/src/PATPhotonProducer.cc
@@ -0,0 +1,56 @@
+//
+// $Id: PATPhotonProducer.cc,v 1.3 2008/01/22 21:58:16 lowette Exp $
+//
+
+#include "PhysicsTools/PatAlgos/interface/PATPhotonProducer.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/Common/interface/View.h"
+
+#include <memory>
+
+
+using namespace pat;
+
+
+PATPhotonProducer::PATPhotonProducer(const edm::ParameterSet & iConfig) {
+  // initialize the configurables
+  photonSrc_         = iConfig.getParameter<edm::InputTag>("photonSource");
+  
+  // produces vector of photons
+  produces<std::vector<Photon> >();
+}
+
+
+PATPhotonProducer::~PATPhotonProducer() {
+}
+
+
+void PATPhotonProducer::produce(edm::Event & iEvent, const edm::EventSetup & iSetup) {
+ 
+  // Get the vector of Photon's from the event
+  edm::Handle<edm::View<PhotonType> > photons;
+  iEvent.getByLabel(photonSrc_, photons);
+
+  // loop over photons
+  std::vector<Photon> * PATPhotons = new std::vector<Photon>(); 
+  for (edm::View<PhotonType>::const_iterator itPhoton = photons->begin(); itPhoton != photons->end(); itPhoton++) {
+    // construct the Photon from the ref -> save ref to original object
+    unsigned int idx = itPhoton - photons->begin();
+    edm::Ref<std::vector<PhotonType> > photonRef = photons->refAt(idx).castTo<edm::Ref<std::vector<PhotonType> > >();
+    Photon aPhoton(photonRef);
+
+    // here comes the extra functionality
+
+    // add the Photon to the vector of Photons
+    PATPhotons->push_back(aPhoton);
+  }
+
+  // sort Photons in ET
+  std::sort(PATPhotons->begin(), PATPhotons->end(), eTComparator_);
+
+  // put genEvt object in Event
+  std::auto_ptr<std::vector<Photon> > myPhotons(PATPhotons);
+  iEvent.put(myPhotons);
+
+}
