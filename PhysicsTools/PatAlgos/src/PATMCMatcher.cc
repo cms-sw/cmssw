a664849a377856a3a2240c40d4ba5b9e5d96acb7
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2008-01-25 17:37:40 +0000

    new preselector for matches (including MC status code)

--- PhysicsTools/PatAlgos/src/PATMCMatcher.cc
+++ PhysicsTools/PatAlgos/src/PATMCMatcher.cc
@@ -1,20 +1,29 @@
 #include "PhysicsTools/PatAlgos/interface/PATCandMatcher.h"
-#include "PhysicsTools/HepMCCandAlgos/interface/MCTruthPairSelector.h"
-// #include "PhysicsTools/PatUtils/interface/PATMatchByDR.h"
+#include "PhysicsTools/PatAlgos/interface/PATMatchSelector.h"
 #include "PhysicsTools/PatUtils/interface/PATMatchByDRDPt.h"
 #include "PhysicsTools/PatUtils/interface/PATMatchLessByDPt.h"
 
+// Match by deltaR and deltaPt, ranking by deltaR (default)
 typedef pat::PATCandMatcher<
   reco::CandidateView,
   reco::CandidateCollection,
-  helpers::MCTruthPairSelector<reco::Candidate>,
+  pat::PATMatchSelector<reco::Candidate>,
   pat::PATMatchByDRDPt<reco::CandidateView::value_type,
-		    reco::CandidateCollection::value_type>,
-  pat::PATMatchLessByDPt<reco::CandidateView,
-			 reco::CandidateCollection>
+		    reco::CandidateCollection::value_type>
 > PATMCMatcher;
 
-// Alternative with pure deltaR matching (explicit) & sorting (default)
+// Alternative: match by deltaR and deltaPt, ranking by deltaPt
+// typedef pat::PATCandMatcher<
+//   reco::CandidateView,
+//   reco::CandidateCollection,
+//   pat::PATMatchSelector<reco::Candidate>,
+//   pat::PATMatchByDRDPt<reco::CandidateView::value_type,
+// 		    reco::CandidateCollection::value_type>,
+//   pat::PATMatchLessByDPt<reco::CandidateView,
+// 			 reco::CandidateCollection>
+// > PATMCMatcher;
+
+// Alternative: pure deltaR matching (explicit) & sorting (default)
 // typedef pat::PATCandMatcher<
 //   reco::CandidateView,
 //   reco::CandidateCollection,
