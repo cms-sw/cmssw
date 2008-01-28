4b590056e3cd8e58b912953c4bbf05f34cf7179a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-01-28 09:23:53 +0000

    restore head

--- CondCore/DBCommon/src/ObjectRelationalMappingUtility.cc
+++ CondCore/DBCommon/src/ObjectRelationalMappingUtility.cc
@@ -1,6 +1,5 @@
 #include "CondCore/DBCommon/interface/ObjectRelationalMappingUtility.h"
 #include "ObjectRelationalAccess/ObjectRelationalMappingUtilities.h"
-#include <cassert>
 cond::ObjectRelationalMappingUtility::ObjectRelationalMappingUtility( coral::ISessionProxy* coralsessionHandle ){
   m_mappingutil=new pool::ObjectRelationalMappingUtilities( coralsessionHandle );
 }
@@ -8,16 +7,10 @@ cond::ObjectRelationalMappingUtility::~ObjectRelationalMappingUtility(){
   delete m_mappingutil;
 }
 void cond::ObjectRelationalMappingUtility::buildAndStoreMappingFromBuffer( const std::string& buffer ){
-  // The following is temporarily commented out (and replaced with the
-  // assert) pending migration to POOL_2_7_0
-  //m_mappingutil->buildAndStoreMappingFromBuffer( buffer.c_str() );
-  assert(0); 
+  m_mappingutil->buildAndStoreMappingFromBuffer( buffer.c_str() );
 }
 void cond::ObjectRelationalMappingUtility::listMappings( std::vector<std::string>& mappinglist ){
-  // The following is temporarily commented out (and replaced with the
-  // assert) pending migration to POOL_2_7_0
-  //mappinglist=m_mappingutil->listMappings();
-  assert(0); 
+  mappinglist=m_mappingutil->listMappings();
 }
 bool cond::ObjectRelationalMappingUtility::existsMapping(const std::string& version){
   return m_mappingutil->existsMapping(version);
