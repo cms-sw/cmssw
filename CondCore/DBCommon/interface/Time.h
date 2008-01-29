d4a435621a92e3e92d1f4f36090b7bcf440260ae
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-01-29 11:39:53 +0000

    added typedef for validity interval

--- CondCore/DBCommon/interface/Time.h
+++ CondCore/DBCommon/interface/Time.h
@@ -1,8 +1,10 @@
 #ifndef DBCommon_Time_h
 #define DBCommon_Time_h 
+#include<utility>
 namespace cond{  
   //typedef unsigned int Time_t;
   typedef unsigned long long Time_t;
+  typedef std::pair<Time_t, Time_t> ValidityInterval;
   typedef enum { runnumber,timestamp } TimeType;
 }
 #endif
