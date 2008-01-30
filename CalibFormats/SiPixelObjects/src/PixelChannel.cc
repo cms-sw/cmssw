357ffbbe125f552328e081fac1c045da7d401f25
Marco Rovere <sha1-46e684da570041e629f20e4154a1c32d32598aaf@cern.ch>
2008-01-30 15:01:44 +0000

    *** empty log message ***

--- CalibFormats/SiPixelObjects/src/PixelChannel.cc
+++ CalibFormats/SiPixelObjects/src/PixelChannel.cc
@@ -3,15 +3,15 @@
 using namespace pos;
 
 PixelChannel::PixelChannel(PixelModuleName module, std::string TBMChannel):
-	module_(module), TBMChannel_(TBMChannel)
+  module_(module), TBMChannel_(TBMChannel)
 {}
 
 PixelChannel::PixelChannel(PixelModuleName module, PixelTBMChannel TBMChannel):
-	module_(module), TBMChannel_(TBMChannel)
+  module_(module), TBMChannel_(TBMChannel)
 {}
 
 std::ostream& pos::operator<<(std::ostream& s, const PixelChannel& channel)
 {
-	s << channel.modulename() << "_" << channel.TBMChannelString();
-	return s;
+  s << channel.modulename() << "_" << channel.TBMChannelString();
+  return s;
 }
