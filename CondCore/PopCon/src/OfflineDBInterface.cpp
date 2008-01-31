8fffc98c112b65a49b5cab89936ca51c2a8faa1e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-01-31 14:00:53 +0000

    use reverse iterator

--- CondCore/PopCon/src/OfflineDBInterface.cpp
+++ CondCore/PopCon/src/OfflineDBInterface.cpp
@@ -13,9 +13,8 @@
 #include "CondCore/MetaDataService/interface/MetaData.h"
 //static cond::ConnectionHandler& conHandler=cond::ConnectionHandler::Instance();
 popcon::OfflineDBInterface::OfflineDBInterface (const std::string& connect ) : m_connect(connect) {
-  session=new cond::DBSession;
-  session->configuration().setAuthenticationMethod( cond::XML );
-  session->configuration().setMessageLevel( cond::Error );
+  session.configuration().setAuthenticationMethod( cond::XML );
+  session.configuration().setMessageLevel( cond::Error );
 }
 
 popcon::OfflineDBInterface::~OfflineDBInterface ()
@@ -36,7 +35,7 @@ std::map<std::string, popcon::PayloadIOV> popcon::OfflineDBInterface::getStatusM
 
 popcon::PayloadIOV popcon::OfflineDBInterface::getSpecificTagInfo(const std::string& tag)
 {
-  PayloadIOV dummy = {0,0,0xffffffff,"noTag"};
+  static PayloadIOV dummy = {0,0,0xffffffff,"noTag"};
   getAllTagsInfo();
   if(m_status_map.find(tag) == m_status_map.end())	
     return dummy;
@@ -53,10 +52,7 @@ void  popcon::OfflineDBInterface::getAllTagsInfo()
   //m_status_map.clear();
   if ( !m_status_map.empty() )  return;
   popcon::PayloadIOV piov;
-  session=new cond::DBSession;
-  session->configuration().setAuthenticationMethod( cond::XML );
-  session->configuration().setMessageLevel( cond::Error );
-  session->open();
+  session.open();
   cond::Connection con(m_connect,-1);
   con.connect(session);
   cond::CoralTransaction& coraldb=con.coralTransaction();
@@ -66,7 +62,7 @@ void  popcon::OfflineDBInterface::getAllTagsInfo()
   coraldb.start(false);
   metadata_svc.listAllTags(alltags);
   //get the pool tokens
-  for(std::vector<std::string>::iterator it = alltags.begin(); it != alltags.end(); it++){
+  for(std::vector<std::string>::const_iterator it = alltags.begin(); it != alltags.end(); it++){
     alltokens.push_back( metadata_svc.getToken(*it));
   }
   coraldb.commit();	
@@ -78,30 +74,16 @@ void  popcon::OfflineDBInterface::getAllTagsInfo()
   cond::PoolTransaction& pooldb=con.poolTransaction();
   cond::IOVService iovservice(pooldb);
   pooldb.start(true);
-  cond::IOVIterator* ioviterator;
-  std::string payloadContainer;
-  unsigned int counter=0;
-  unsigned int itpos =0;
   for(std::vector<std::string>::iterator tok_it = alltokens.begin(); tok_it != alltokens.end(); tok_it++, itpos++){
-    ioviterator=iovservice.newIOVIterator(*tok_it);
-    counter = 0;
-    payloadContainer=iovservice.payloadContainerName(*tok_it);
-    //std::cerr<<"Tag "<< alltags[itpos]  <<"\n";
-    //std::cerr<<"PayloadContainerName "<<payloadContainer<<"\n";
-    //std::cerr<<"since \t till \t payloadToken"<<std::endl;
-    while( ioviterator->next() ){
-      //	std::cout<<ioviterator->validity().first<<" \t "<<ioviterator->validity().second<<" \t "<<ioviterator->payloadToken()<<std::endl;	
-      ++counter;
-    }
-    piov.number_of_objects = counter;		
+    std::auto_ptr<cond::IOVIterator> ioviterator(iovservice.newIOVIterator(*tok_it,cond::IOVService::backwardIter));
+    ioviterator->next();
+    piov.number_of_objects = ioviterator.size();		
     piov.last_since = ioviterator->validity().first;
     piov.last_till = ioviterator->validity().second;
-    piov.container_name  = payloadContainer;
+    piov.container_name  = iovservice.payloadContainerName(*tok_it);
     m_status_map[alltags[itpos]] = piov;
-    delete ioviterator;
   }
   pooldb.commit();
   con.disconnect();
-  delete session;
 }
 
--- CondCore/PopCon/src/PopConAnalyzer.cpp
+++ CondCore/PopCon/src/PopConAnalyzer.cpp
@@ -1,1 +1,139 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
+#include "CondCore/PopCon/interface/PopConAnalyzerBase.h"
+
+
+namespace popcon {
+
+  PopConAnalyzerBase::PopConAnalyzerBase(const edm::ParameterSet& pset, 
+					 const std::string& object_name):
+    tryToValidate(false), corrupted(false), greenLight (true), fixed(true), 
+    m_payload_name(object_name) {
+    
+    //TODO set the policy (cfg or global configuration?)
+    //Policy if corrupted data found
+    m_debug = pset.getParameter< bool > ("debug");
+    
+    //MANDATORY 
+    m_offline_connection = pset.getParameter<std::string> ("connect");
+    sinceAppend = pset.getParameter<bool> ("SinceAppendMode");
+  }
+  
+  PopConAnalyzerBase::~PopConAnalyzerBase(){}
+  
+  void PopConAnalyzerBase::beginJob(const edm::EventSetup& es)
+  {	
+    if(m_debug) std::cerr << "Begin Job\n"; 
+    try{
+      std::cout<<"offline_connection "<<m_offline_connection<<std::endl;
+      std::cout<<"payload name "<<m_payload_name<<std::endl;
+      
+      /*     
+	
+      //checks the exceptions, validates new data if necessary
+      if (stc->previousExceptions(fixed)){
+	std::cerr << "There's been a problem with a previous run" << std::endl;
+	if (!fixed){	
+	  //TODO - set the flag
+	  logMsg="Running with unfixed state, EXITING";
+	  return;
+	}else{
+	  std::cerr << "Handled exception, attempting to validate" << std::endl;
+	  //TODO - implement ?
+	}
+      }
+      
+      if (stc->checkAndCompareState()){
+	//std::cerr << "State OK" << std::endl;
+	greenLight = true;
+	logMsg="OK";
+	corrupted = false;
+	//	safe to run;
+      }else{
+	//	data is corrupted;
+	if (tryToValidate){
+	  std::cerr << "attempting to validate" << std::endl;
+	  corrupted = false;
+	}else {
+	  //Report an error 	
+	  std::cerr << "State Corruption, EXITING!!!\n";
+	  logMsg="State corruption";
+	  return;
+	}
+      }
+    }catch(popcon::Exception & er){
+      std::cerr << "Begin Job PopCon exception\n";
+      logMsg = "Begin Job exception";
+      greenLight = false;
+    }catch(cond::Exception& er){
+      std::cerr<< "Begin Job cond exception " << er.what()<<std::endl;
+      logMsg = "Begin Job exception";
+    }catch(std::exception& e){
+      greenLight = false;
+      std::cerr << "Begin Job std-based exception\n";
+      logMsg = "Begin Job exception";
+    }
+
+  */  
+    
+  }
+     
+    
+    //this method handles the transformation algorithm, 
+    //Subdetector responsibility ends with returning the payload vector.
+    //Therefore this code is stripped of DBOutput service, state management etc.     
+      
+    virtual void PopConAnalyzerBase::analyze(const edm::Event& evt, const edm::EventSetup& est){
+      if(m_debug) std::cerr << "Analyze Begins\n"; 
+      try{
+	if(greenLight){
+	  //get New objects 	  
+          takeTheData();
+	  if(m_debug)
+	    displayHelper();	  
+	}
+	/// write in DB
+
+	if(m_debug)  std::cerr << "Analyze Ends\n"; 
+      }catch(std::exception& e){
+	std::cerr << "Analyzer Exception\n";
+	std::cerr << e.what() << std::endl; 
+      }
+    }
+      
+    virtual void endJob(){
+      if(m_debug) std::cerr << "Destructor begins\n";	
+    
+      /*
+  
+      try{
+	if (!fixed || corrupted){	
+	  if(m_debug)
+	    std::cerr << "Corrupted | unfixed state | problem with PopCon DB\n";
+	  lgr->finalizeExecution(logMsg);
+	}else{ //ok 
+	  if(m_debug)
+	    std::cerr << "OK, finalizing the log\n";
+	  lgr->finalizeExecution(logMsg);
+	  stc->generateStatusData();
+	  stc->storeStatusData();
+	  if(m_debug)
+	    std::cerr << "Deleting stc\n";	
+	  delete stc;
+	}	  
+	lgr->unlock();
+	
+      }catch(std::exception& e){
+	std::cerr << "Exception caught in destructor: "<< e.what();
+      }
+      
+      if(m_debug)
+	std::cerr << "Deleting lgr\n";	
+      delete lgr;
+      
+      if(m_debug)
+	std::cerr << "Destructor ends\n";	
+
+      */
+
+    }
+    
+}
