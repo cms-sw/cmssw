3b72f16bbbf128217f1a856ee8a2a12c32d53c0c
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-01-31 16:01:47 +0000

    code cleanup

--- RecoTauTag/HLTProducers/src/TauRegionalPixelSeedGenerator.h
+++ RecoTauTag/HLTProducers/src/TauRegionalPixelSeedGenerator.h
@@ -7,18 +7,18 @@
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/EDProduct.h"
-#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducer.h"
-#include "RecoTracker/TkTrackingRegions/interface/GlobalTrackingRegion.h"
-#include "RecoTracker/TkTrackingRegions/interface/RectangularEtaPhiTrackingRegion.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/Math/interface/Vector3D.h"
+#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducer.h"
+#include "RecoTracker/TkTrackingRegions/interface/GlobalTrackingRegion.h"
+#include "RecoTracker/TkTrackingRegions/interface/RectangularEtaPhiTrackingRegion.h"
 // Math
 #include "Math/GenVector/VectorUtil.h"
 #include "Math/GenVector/PxPyPzE4D.h"
@@ -37,13 +37,13 @@ class TauRegionalPixelSeedGenerator : public TrackingRegionProducer {
 
       edm::ParameterSet regionPSet = conf_.getParameter<edm::ParameterSet>("RegionPSet");
 
-      ptmin        = regionPSet.getParameter<double>("ptMin");
-      originradius = regionPSet.getParameter<double>("originRadius");
-      halflength   = regionPSet.getParameter<double>("originHalfLength");
-      deltaEta     = regionPSet.getParameter<double>("deltaEtaRegion");
-      deltaPhi     = regionPSet.getParameter<double>("deltaPhiRegion");
-      jetSrc       = regionPSet.getParameter<edm::InputTag>("JetSrc");
-      vertexSrc    = regionPSet.getParameter<edm::InputTag>("vertexSrc");
+      m_ptMin        = regionPSet.getParameter<double>("ptMin");
+      m_originRadius = regionPSet.getParameter<double>("originRadius");
+      m_halfLength   = regionPSet.getParameter<double>("originHalfLength");
+      m_deltaEta     = regionPSet.getParameter<double>("deltaEtaRegion");
+      m_deltaPhi     = regionPSet.getParameter<double>("deltaPhiRegion");
+      m_jetSrc       = regionPSet.getParameter<edm::InputTag>("JetSrc");
+      m_vertexSrc    = regionPSet.getParameter<edm::InputTag>("vertexSrc");
     }
   
     virtual ~TauRegionalPixelSeedGenerator() {}
@@ -51,60 +51,54 @@ class TauRegionalPixelSeedGenerator : public TrackingRegionProducer {
 
     virtual std::vector<TrackingRegion* > regions(const edm::Event& e, const edm::EventSetup& es) const {
       std::vector<TrackingRegion* > result;
-      float originz =0.;
-	
-      double deltaZVertex =  halflength;
-      // get Inputs
-      edm::Handle<reco::VertexCollection> vertices;
-      e.getByLabel(vertexSrc,vertices);
-      const reco::VertexCollection vertCollection = *(vertices.product());
-      reco::VertexCollection::const_iterator ci = vertCollection.begin();
-      if(vertCollection.size() > 0) {
-	originz = ci->z();
-      }else{
-	deltaZVertex = 15.;
+
+      double originZ;
+      double deltaZVertex;
+      
+      // get the primary vertex
+      edm::Handle<reco::VertexCollection> h_vertices;
+      e.getByLabel(m_vertexSrc, h_vertices);
+      const reco::VertexCollection & vertices = * h_vertices;
+      if (not vertices.empty()) {
+        originZ      = vertices.front().z();
+        deltaZVertex = m_halfLength;
+      } else {
+        originZ      =  0.;
+        deltaZVertex = 15.;
       }
       
-      //Get the jet direction
-      edm::Handle<CaloJetCollection> jets;
-      e.getByLabel(jetSrc, jets);
+      // get the jet direction
+      edm::Handle<reco::CaloJetCollection> h_jets;
+      e.getByLabel(m_jetSrc, h_jets);
+      const reco::CaloJetCollection & jets = * h_jets;
       
-      GlobalVector globalVector(0,0,1);
-      if(jets->size() == 0) return result;
-      
-      CaloJetCollection::const_iterator iJet = jets->begin();
-      for(;iJet != jets->end();iJet++)
-	{
-	
-	  GlobalVector jetVector((iJet)->p4().x(),(iJet)->p4().y(),(iJet)->p4().z());
-	  globalVector = jetVector;
-	  
-	
-	  RectangularEtaPhiTrackingRegion* etaphiRegion = new  RectangularEtaPhiTrackingRegion(globalVector,
-											       GlobalPoint(0,0,originz), 
-											       ptmin,
-											       originradius,
-											       deltaZVertex,
-											       deltaEta,
-											       deltaPhi);
-	  result.push_back(etaphiRegion);
-	  
-	}
-      
+      for (reco::CaloJetCollection::const_iterator iJet = jets.begin(); iJet != jets.end(); ++iJet)
+      {
+          GlobalVector jetVector(iJet->p4().x(), iJet->p4().y(), iJet->p4().z());
+          GlobalPoint  vertex(0, 0, originZ);
+          RectangularEtaPhiTrackingRegion* etaphiRegion = new RectangularEtaPhiTrackingRegion( jetVector,
+                                                                                               vertex,
+                                                                                               m_ptMin,
+                                                                                               m_originRadius,
+                                                                                               deltaZVertex,
+                                                                                               m_deltaEta,
+                                                                                               m_deltaPhi );
+          result.push_back(etaphiRegion);
+      }
+
       return result;
     }
-
   
  private:
   edm::ParameterSet conf_;
 
-  float ptmin;
-  float originradius;
-  float halflength;
-  float deltaEta;
-  float deltaPhi;
-  edm::InputTag jetSrc;
-  edm::InputTag vertexSrc;
+  float m_ptMin;
+  float m_originRadius;
+  float m_halfLength;
+  float m_deltaEta;
+  float m_deltaPhi;
+  edm::InputTag m_jetSrc;
+  edm::InputTag m_vertexSrc;
 };
 
 #endif
