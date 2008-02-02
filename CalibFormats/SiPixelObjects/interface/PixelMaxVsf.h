20888aaea0ff292664a3fa55245a6a8e0d7d8614
Anders Ryd <sha1-cdf8ae4e5b0e21334a15ab4f05e71c3dd6ac0eb0@cern.ch>
2008-02-02 20:36:17 +0000

    Added first version of code to hold the maximum Vsf value

---
+++ CalibFormats/SiPixelObjects/interface/PixelMaxVsf.h
@@ -0,0 +1,36 @@
+#ifndef PixelMaxVsf_h
+#define PixelMaxVsf_h
+//
+// This class specifies the maximum
+// Vsf setting that should be used
+// for each ROC.
+//
+//
+
+#include <map>
+#include <string>
+#include <vector>
+#include "CalibFormats/SiPixelObjects/interface/PixelConfigBase.h"
+#include "CalibFormats/SiPixelObjects/interface/PixelROCName.h"
+
+namespace pos{
+
+  class PixelMaxVsf: public PixelConfigBase {
+
+  public:
+
+    PixelMaxVsf(std::vector< std::vector < std::string> > &tableMat);
+    PixelMaxVsf(std::string filename);
+
+    void writeASCII(std::string dir="") const;
+
+    bool getVsf(PixelROCName roc, double& Vsf) const;
+
+  private:
+
+    std::map<PixelROCName, unsigned int> rocs_;
+ 
+  };
+
+}
+#endif
---
+++ CalibFormats/SiPixelObjects/src/PixelMaxVsf.cc
@@ -0,0 +1,110 @@
+//
+// Implementation of the max Vsf
+//
+//
+//
+//
+
+#include "CalibFormats/SiPixelObjects/interface/PixelMaxVsf.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <ios>
+#include <assert.h>
+#include <stdio.h>
+
+using namespace std;
+using namespace pos;
+
+
+PixelMaxVsf::PixelMaxVsf(std::vector< std::vector< std::string > > &tableMat):PixelConfigBase("","",""){
+
+  //Needs to be implemented when we have this in the database.
+  assert(0);
+
+}//end constructor
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+PixelMaxVsf::PixelMaxVsf(std::string filename):
+  PixelConfigBase("","",""){
+
+
+  if (filename[filename.size()-1]=='t'){
+
+    std::ifstream in(filename.c_str());
+
+    if (!in.good()){
+      std::cout << "[PixelMaxVsf::PixelMaxVsf()]\t\tCould not open:"<<filename<<std::endl;
+      assert(0);
+    }
+    else {
+      std::cout << "[PixelMaxVsf::PixelMaxVsf()]\t\tOpened:"<<filename<<std::endl;
+    }
+	
+    if (in.eof()){
+      std::cout << "[PixelMaxVsf::PixelMaxVsf()]\t\teof before reading anything!"<<std::endl;
+      ::abort();
+    }
+
+	
+    rocs_.clear();
+	
+    std::string rocname;
+	
+    in >> rocname;
+    while (!in.eof()){
+      //cout << "Read rocname:"<<rocname<<endl;
+      PixelROCName roc(rocname);
+      unsigned int vsf;
+      in >> vsf;
+      rocs_[roc]=vsf;
+      in >> rocname;
+    }
+    return;
+  }
+  else{
+    assert(0);
+  }
+
+}
+ 
+bool PixelMaxVsf::getVsf(PixelROCName roc, double& Vsf) const{
+
+  std::map<PixelROCName,unsigned int>::const_iterator itr = rocs_.find(roc);
+
+  if (itr==rocs_.end()) {
+    return false;
+  }
+
+  Vsf=itr->second;
+
+  return true;
+
+}
+
+
+
+void PixelMaxVsf::writeASCII(std::string dir) const {
+
+  if (dir!="") dir+="/";
+  std::string filename=dir+"maxvsf.dat";
+
+  std::ofstream out(filename.c_str(), std::ios_base::out) ;
+  if(!out) {
+    std::cout << "[PixelMaxVsf::writeASCII()] Could not open file " << filename << " for write" << std::endl ;
+    exit(1);
+  }
+
+
+  std::map<PixelROCName, unsigned int>::const_iterator irocs = rocs_.begin();
+  for(; irocs != rocs_.end() ; irocs++){
+    out << (irocs->first).rocname() << " " << irocs->second << endl ;
+  }
+  
+  out.close();
+
+}
+
+
+
