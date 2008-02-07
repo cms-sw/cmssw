e477efb5b449d85ed3a8d90e4585225177151897
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2008-02-07 14:27:21 +0000

    Adding collections for Et sums and Jet Counts

--- DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
@@ -9,10 +9,17 @@
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctJetCand.h"
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctFibreWord.h"
 
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctEtSums.h"
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctJetCounts.h"
+
 typedef std::vector<L1GctInternEmCand> L1GctInternEmCandCollection;
 typedef std::vector<L1GctEmCand> L1GctEmCandCollection;
 typedef std::vector<L1GctJetCand> L1GctJetCandCollection;
 typedef std::vector<L1GctFibreWord> L1GctFibreCollection;
 
+typedef std::vector<L1GctEtHad> L1GctEtHadCollection;
+typedef std::vector<L1GctEtMiss> L1GctEtMissCollection;
+typedef std::vector<L1GctEtTotal> L1GctEtTotalCollection;
+typedef std::vector<L1GctJetCounts> L1GctJetCountsCollection;
 
 #endif
