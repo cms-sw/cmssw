21c5b2c21f9a38caa4fac6e9d2ad27318e2b8e1f
Michel Della Negra <sha1-71960d34083959c446a60b6ac93c87797388a3c5@cern.ch>
2008-02-11 14:03:37 +0000

    restore PFJetfwd.h instead of PFJetCollection.h

--- RecoParticleFlow/Benchmark/interface/PFJetBenchmark.h
+++ RecoParticleFlow/Benchmark/interface/PFJetBenchmark.h
@@ -3,9 +3,9 @@
 
 #include "RecoParticleFlow/Benchmark/interface/PFBenchmarkAlgo.h"
 
-#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/JetReco/interface/PFJetfwd.h"
 #include "DataFormats/JetReco/interface/PFJet.h"
-#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/JetReco/interface/GenJetfwd.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/JetReco/interface/BasicJetCollection.h"
