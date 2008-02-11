165f74b4c0478a4fdd301baf84f2fa247688a5c4
Gabriele Benelli <sha1-9f689a57ab1cff5a2566702b3cac6c22c95334fa@cern.ch>
2008-02-11 09:55:56 +0000

    Added a local directory option for the simulation performance suite publication script. Added also a link to be able to manually browse the results directory from the index page.  Modified Files:  	doc/index.html scripts/cmsSimPerfPublish.pl  ----------------------------------------------------------------------

--- Validation/Performance/scripts/cmsSimPerfPublish.pl
+++ Validation/Performance/scripts/cmsSimPerfPublish.pl
@@ -7,6 +7,7 @@
 $TimeSizeNumOfEvents=100;
 $IgProfNumOfEvents=5;
 $ValgrindNumOfEvents=1;
+$LocalWebArea=$ARGV[0]; 
 
 #Some nomenclature
 @Candle=(
@@ -100,223 +101,232 @@ else
 }
 
 #Define the web publishing area
-$WebArea="/afs/cern.ch/cms/sdt/web/performance/simulation/"."$CMSSW_VERSION";
-#Dump some info in a file
-if (-e! $WebArea)
-{
-    print "The area $WebArea does not exist!\n";
-    print "Run the appropriate script to request AFS space first, or wait for it to create it!\n";
-    exit;
+if ($LocalWebArea eq "local")
+{ 
+    $WebArea="/tmp/gbenelli/"."$CMSSW_VERSION";
+    print "Creating local directory $WebArea\n";
+    system("mkdir $WebArea");
 }
 else
 {
-    $CheckDir=`ls $WebArea`;
-    if ($CheckDir eq "")
+    $WebArea="/afs/cern.ch/cms/sdt/web/performance/simulation/"."$CMSSW_VERSION";
+}
+#Dump some info in a file   
+opendir(WEBDIR,$WebArea)||die "The area $WebArea does not exist!\nRun the appropriate script to request AFS space first, or wait for it to create it!\n";
+@Contents=readdir(WEBDIR);
+#    $CheckDir=`ls $WebArea`;
+#    if ($CheckDir eq "")
+if ($#Contents==1)#@Contents will have only 2 entries . and .. if the dir is empty.
+{
+    print "The area $WebArea is ready to be populated!\n";
+}
+else
+{
+    print "The area $WebArea already exists!\n";
+    exit;
+}
+$date=`date`;
+@LogFiles=`ls cms*.log`;
+print "Found the following log files:\n";
+print @LogFiles;
+$ExecutionDateSec=0;
+foreach (@LogFiles)
+{
+    chomp($_);
+    if ($_=~/^cmsCreateSimPerfTest/)
     {
-	print "The area $WebArea is ready to be populated!\n";
-    }
-    else
-    {
-	print "The area $WebArea already exists!\n";
-	exit;
-    }
-    $date=`date`;
-    @LogFiles=`ls cms*.log`;
-    print "Found the following log files:\n";
-    print @LogFiles;
-    $ExecutionDateSec=0;
-    foreach (@LogFiles)
-    {
-	chomp($_);
-	if ($_=~/^cmsCreateSimPerfTest/)
+	$ExecutionDateLastSec=`stat --format=\%Z $_`;
+	$ExecutionDateLast=`stat --format=\%y $_`;
+	print "Execution date for $_ was: $ExecutionDateLast";
+	if ($ExecutionDateLastSec>$ExecutionDateSec)
 	{
-	    $ExecutionDateLastSec=`stat --format=\%Z $_`;
-	    $ExecutionDateLast=`stat --format=\%y $_`;
-	    print "Execution date for $_ was: $ExecutionDateLast";
-	    if ($ExecutionDateLastSec>$ExecutionDateSec)
-	    {
-		$ExecutionDateSec=$ExecutionDateLastSec;
-		$ExecutionDate=$ExecutionDateLast;
-	    }	
-	}
+	    $ExecutionDateSec=$ExecutionDateLastSec;
+	    $ExecutionDate=$ExecutionDateLast;
+	}	
     }
-    print "Copying the logfiles to $WebArea/.\n";
-    system("cp -pR cms*.log $WebArea/.");
+}
+print "Copying the logfiles to $WebArea/.\n";
+system("cp -pR cms*.log $WebArea/.");
 #Copy the perf_style.css file from Validation/Performance/doc
-    print "Copying perf_style.css style file to $WebArea/.\n";
-    system("cp -pR $BASE_PERFORMANCE/doc/perf_style.css $WebArea/.");
-    @Dir=`ls`;
-    chomp(@Dir);
+print "Copying perf_style.css style file to $WebArea/.\n";
+system("cp -pR $BASE_PERFORMANCE/doc/perf_style.css $WebArea/.");
+@Dir=`ls`;
+chomp(@Dir);
 #Produce a small logfile with basic info on the Production area
-    $LogFile="$WebArea/"."ProductionLog.txt";
-    open(LOG,">$LogFile")||die "CAnnot open file $_!\n$!\n";
-    print "Writing Production Host, Location, Release and Tags information in $LogFile\n"; 
-    print LOG "These performance tests were executed on host $HOST and published on $date\n";
-    print LOG "They were run in $LocalPath\n";
-    print LOG "Results of showtags -r in the local release:\n$ShowTagsResult\n";
-    close(LOG);
+$LogFile="$WebArea/"."ProductionLog.txt";
+open(LOG,">$LogFile")||die "CAnnot open file $_!\n$!\n";
+print "Writing Production Host, Location, Release and Tags information in $LogFile\n"; 
+print LOG "These performance tests were executed on host $HOST and published on $date\n";
+print LOG "They were run in $LocalPath\n";
+print LOG "Results of showtags -r in the local release:\n$ShowTagsResult\n";
+close(LOG);
 #Produce a small index.html file to navigate the html reports/logs etc
-    $IndexFile="$WebArea"."/index.html";
-    open(INDEX,">$IndexFile")||die "Cannot open file $IndexFile!\n$!\n";
-    print "Writing an index.html file with links to the profiles report information for easier navigation\n"; 
-    $TemplateHtml="$BASE_PERFORMANCE"."/doc/index.html";
-    open(TEMPLATE,"<$TemplateHtml")||die "Couldn't open file $TemplateHtml - $!\n";
-    #Loop line by line to build our index.html based on the template one
-    while (<TEMPLATE>)
+$IndexFile="$WebArea"."/index.html";
+open(INDEX,">$IndexFile")||die "Cannot open file $IndexFile!\n$!\n";
+print "Writing an index.html file with links to the profiles report information for easier navigation\n"; 
+$TemplateHtml="$BASE_PERFORMANCE"."/doc/index.html";
+open(TEMPLATE,"<$TemplateHtml")||die "Couldn't open file $TemplateHtml - $!\n";
+#Loop line by line to build our index.html based on the template one
+while (<TEMPLATE>)
+{
+    $NewFileLine=$_;
+    if ($_=~/\CMSSW_VERSION/)
     {
-	$NewFileLine=$_;
-	if ($_=~/\CMSSW_VERSION/)
-	{
-	    print INDEX $CMSSW_VERSION;
-	    next;
+	print INDEX $CMSSW_VERSION;
+	next;
 	}
-	if ($_=~/\HOST/)
-	{
-	    print INDEX $HOST;
-	    next;
-	}
-	if ($_=~/LocalPath/)
+    if ($_=~/\HOST/)
+    {
+	print INDEX $HOST;
+	next;
+    }
+    if ($_=~/LocalPath/)
 	{
 	    print INDEX $LocalPath;
 	    next;
 	}
-	if ($_=~/ProductionDate/)
+    if ($_=~/ProductionDate/)
+    {
+	print INDEX $ExecutionDate;
+	next;
+    }
+    if ($_=~/LogfileLinks/)
+    {
+	print INDEX "<br><br>";
+	foreach (@LogFiles)
 	{
-	    print INDEX $ExecutionDate;
-	    next;
-	}
-	if ($_=~/LogfileLinks/)
-	{
+	    chomp($_);
+	    #$LogFileLink="$WebArea/"."$_";
+	    print INDEX "<a href="."$_"."> $_ <\/a>";
 	    print INDEX "<br><br>";
-	    foreach (@LogFiles)
+	}
+	next;
+    }
+    if ($_=~/DirectoryBrowsing/)
+    {
+	print INDEX "Click <a href=\.\/>here<\/a> to browse the directory containing all results (but the root files)\n";
+	next;
+    }
+    if ($_=~/PublicationDate/)
+    {
+	print INDEX $date;
+	next;
+    }
+    if ($_=~/\CandlesHere/)
+    {
+	foreach (@Candle)
+	{
+	    print INDEX "<table cellpadding=20px border=1><td width=25% bgcolor=#FFFFFE >";
+	    print INDEX "<h2>";
+	    print INDEX $_;
+	    $CurrentCandle=$_;
+	    print INDEX "<\/h2>";
+	    print INDEX "<dir style=\"font-size: 13\"> \n <p>";
+	    foreach (@DirName)
 	    {
 		chomp($_);
-		#$LogFileLink="$WebArea/"."$_";
-		print INDEX "<a href="."$_"."> $_ <\/a>";;
-		print INDEX "<br><br>";
-	    }
-	    next;
-	}
-	if ($_=~/PublicationDate/)
-	{
-	    print INDEX $date;
-	    next;
-	}
-	if ($_=~/\CandlesHere/)
-	{
-	    foreach (@Candle)
-	    {
-		print INDEX "<table cellpadding=20px border=1><td width=25% bgcolor=#FFFFFE >";
-		print INDEX "<h2>";
-		print INDEX $_;
-		$CurrentCandle=$_;
-		print INDEX "<\/h2>";
-		print INDEX "<dir style=\"font-size: 13\"> \n <p>";
-		foreach (@DirName)
+		$CurDir=$_;
+		$LocalPath="$CurrentCandle"."_$CurDir";
+		@CandleLogFiles=`find $LocalPath \-name \"\*.log\"`;
+		if (@CandleLogFiles)
 		{
-		    chomp($_);
-		    $CurDir=$_;
-		    $LocalPath="$CurrentCandle"."_$CurDir";
-		    @CandleLogFiles=`find $LocalPath \-name \"\*.log\"`;
-		    if (@CandleLogFiles)
+		    print INDEX "<br><b>Logfiles for $CurDir<\/b><br>";
+		    foreach (@CandleLogFiles)
 		    {
-			print INDEX "<br><b>Logfiles for $CurDir<\/b><br>";
-			foreach (@CandleLogFiles)
-			{
-			    chomp($_);
-			    print "Found $_ in $LocalPath\n";
-			    system("cp -pR $_ $WebArea/.");
-			    print INDEX "<a href="."$_".">$_ <\/a>";
-			}
+			chomp($_);
+			print "Found $_ in $LocalPath\n";
+			system("cp -pR $_ $WebArea/.");
+			print INDEX "<a href="."$_".">$_ <\/a>";
 		    }
-		    foreach (@Profile)
-		    {
-			$CurrentProfile=$_;
-			foreach (@Step)
-			{
-			    $ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$OutputHtml{$CurrentProfile}";
-			    $ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$OutputHtml{$CurrentProfile}";
-			    $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
-			    if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
-			    {
-				$ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
-			    }
-			    if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
-			    {
-				if ($PrintedOnce==0)
-				{
-				    print INDEX "<br>";
-				    print INDEX "<b>$CurDir</b>";
-				    $PrintedOnce=1;
-				}
-				print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
-				if ($CurrentProfile eq $Profile[7])
-				{
-				   for ($i=1;$i<3;$i++)
-				   {
-				       $ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$IgProfMemAnalyseOut[$i]";
-				       $ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$IgProfMemAnalyseOut[$i]";
-				       $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
-				       if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
-				       {
-					   $ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
-				       }
-				       if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
-				       {
-					   print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $IgProfMemAnalyseOut[$i] $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
-				       }
-				   }
-				}
-				if ($CurrentProfile eq $Profile[9])
-				{
-				   for ($i=1;$i<3;$i++)
-				   {
-				       $ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$memcheck_valgrindOut[$i]";
-			    $ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$memcheck_valgrindOut[$i]";
-				       $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
-				       if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
-				       {
-					   $ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
-				       }
-				       if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
-				       {
-					   print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $memcheck_valgrindOut[$i] $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
-				       }
-				   }
-				}
-			    }
-			}
-		    }
-		    $PrintedOnce=0;
 		}
-		print INDEX "<\/p>";
-		print INDEX "</dir>";
-		print INDEX "<hr>";
-		print INDEX "<br>";
-		print INDEX "<\/td><\/table>";
-	    }	    
-	    next;
-	}
-	print INDEX $NewFileLine;
-    }#End of while loop on template html file
-	
-    foreach (@Dir)
-    {
-	$CurrentDir=$_;
-	foreach (@DirName)
-	{
-	    if ($CurrentDir=~/$_/)#To get rid of possible spurious dirs
-	    {
-		print "Copying $CurrentDir to $WebArea\n";
-		$CopyDir=`cp -pR $CurrentDir $WebArea/.`;
-		$RemoteDirRootFiles="$WebArea/"."$CurrentDir/*.root";
-		$RemoveRootFiles=`rm -Rf $RemoteDirRootFiles`; 
+		foreach (@Profile)
+		{
+		    $CurrentProfile=$_;
+		    foreach (@Step)
+		    {
+			$ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$OutputHtml{$CurrentProfile}";
+			$ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$OutputHtml{$CurrentProfile}";
+			    $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
+			if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
+			{
+			    $ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
+			}
+			if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
+			{
+			    if ($PrintedOnce==0)
+			    {
+				print INDEX "<br>";
+				print INDEX "<b>$CurDir</b>";
+				$PrintedOnce=1;
+			    }
+			    print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
+			    if ($CurrentProfile eq $Profile[7])
+			    {
+				for ($i=1;$i<3;$i++)
+				{
+				    $ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$IgProfMemAnalyseOut[$i]";
+				    $ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$IgProfMemAnalyseOut[$i]";
+				    $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
+				    if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
+				    {
+					$ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
+				    }
+				    if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
+				    {
+					print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $IgProfMemAnalyseOut[$i] $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
+				    }
+				}
+			    }
+			    if ($CurrentProfile eq $Profile[9])
+			    {
+				for ($i=1;$i<3;$i++)
+				{
+				    $ProfileTemplate="$CurrentCandle"."_"."$CurDir"."/"."*_"."$_"."_"."$CurrentProfile"."*/"."$memcheck_valgrindOut[$i]";
+				    $ProfileTemplateLowCaps="$CurrentCandle"."_"."$CurDir"."/"."*_"."$StepLowCaps{$_}"."_"."$CurrentProfile"."*/"."$memcheck_valgrindOut[$i]";
+				    $ProfileReportLink=`ls $ProfileTemplate 2>/dev/null`;
+				    if ( $ProfileReportLink !~ /^$CurrentCandle/)#no match with caps try low caps
+				    {
+					$ProfileReportLink=`ls $ProfileTemplateLowCaps 2>/dev/null`;
+				    }
+				    if ($ProfileReportLink=~/$CurrentProfile/)#It could also not be there
+				    {
+					print INDEX "<li><a href="."$ProfileReportLink".">$CurrentProfile $memcheck_valgrindOut[$i] $_ ("."$NumOfEvents{$CurDir}"." events)<\/a>";
+				    }
+				}
+			    }
+			}
+		    }
+		}
+		$PrintedOnce=0;
 	    }
+	    print INDEX "<\/p>";
+	    print INDEX "</dir>";
+	    print INDEX "<hr>";
+	    print INDEX "<br>";
+	    print INDEX "<\/td><\/table>";
+	}	    
+	next;
+    }
+    print INDEX $NewFileLine;
+} #End of while loop on template html file
+
+foreach (@Dir)
+{
+    $CurrentDir=$_;
+    foreach (@DirName)
+    {
+	if ($CurrentDir=~/$_/)#To get rid of possible spurious dirs
+	{
+	    print "Copying $CurrentDir to $WebArea\n";
+	    $CopyDir=`cp -pR $CurrentDir $WebArea/.`;
+	    $RemoteDirRootFiles="$WebArea/"."$CurrentDir/*.root";
+	    $RemoveRootFiles=`rm -Rf $RemoteDirRootFiles`; 
 	}
     }
-    print INDEX "\<\/body\>\n";
-    print INDEX "\<\/html\>\n";
-    close(INDEX);
 }
+print INDEX "\<\/body\>\n";
+print INDEX "\<\/html\>\n";
+close(INDEX);
+
 exit;
 
