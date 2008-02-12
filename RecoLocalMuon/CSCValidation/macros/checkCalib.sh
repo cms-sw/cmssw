bb98e3b98b7ff79fdc68f39375b306070b81f2ba
Andrew Michael Kubik <sha1-11462ee980c99915eaf4bc9c9deba6cf31f9cb9d@cern.ch>
2008-02-12 20:46:43 +0000

    add scripts to check calibration constants and residuals

---
+++ RecoLocalMuon/CSCValidation/macros/checkCalib.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# to run this script, do
+# ./checkCalib.sh <filepath>
+# where <filepath> is the path to the output root file from CSCValiation
+
+# example:  ./checkCalib.sh ~/work/DEV/CMSSW_1_8_0_pre8/src/RecoLocalMuon/CSCValidation/test/validationHists_muongun.root
+
+ARG1=$1
+
+MACRO=checkCalib_temp.C
+cat > ${MACRO}<<EOF
+
+{
+  std::string newReleaseFile = "${ARG1}";
+  std::string refReleaseFile = "~akub19/public/fakeCalibReference.root";
+
+  ofstream out;
+  out.open("calib_compare_results.txt");
+
+  TFile *fn = new TFile(newReleaseFile.c_str(),"READ");
+  TFile *fo = new TFile(refReleaseFile.c_str(),"READ");
+
+  vector<TH1F*> hCo;
+  vector<TH1F*> hCn;
+
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibGainsS"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibGainsI"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibGainsChi2"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkSL"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkSR"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkIL"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkIR"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkChi2L"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibXtalkChi2R"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibPedsP"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibPedsR"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise33"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise34"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise35"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise44"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise45"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise46"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise55"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise56"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise57"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise66"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise67"));
+  hCn.push_back((TH1F*)fn->Get("Calib/hCalibNoise77"));
+
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibGainsS"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibGainsI"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibGainsChi2"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkSL"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkSR"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkIL"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkIR"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkChi2L"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibXtalkChi2R"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibPedsP"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibPedsR"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise33"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise34"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise35"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise44"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise45"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise46"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise55"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise56"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise57"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise66"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise67"));
+  hCo.push_back((TH1F*)fo->Get("Calib/hCalibNoise77"));
+
+  float diff = 0;
+  vector<int> ndiff;
+  vector<float> vdiff;
+
+  for (int k = 0; k < 23; k++){
+    ndiff.push_back(0);
+    for (int i = 0; i < 217728; i++){
+      float vo = hCo[k]->GetBinContent(i+1);
+      float vn = hCn[k]->GetBinContent(i+1);
+      diff = (vn - vo);
+      if (diff > 0.01){ ndiff[k] = ndiff[k] + 1; vdiff.push_back(diff); }
+    }
+  }
+
+  out << "Results: " << endl;
+  out << "Number of channels with diff > 0.01 for... " << endl;
+  out << "Gain Slopes: " << ndiff[0] << endl;
+  out << "Gain Intercepts: " << ndiff[1] << endl;
+  out << "Gain Chi2: " << ndiff[2] << endl;
+  out << "Xtalk Slopes Left: " << ndiff[3] << endl;
+  out << "Xtalk Slopes Right: " << ndiff[4] << endl;
+  out << "Xtalk Intercepts Left: " << ndiff[5] << endl;
+  out << "Xtalk Intercepts Right: " << ndiff[6] << endl;
+  out << "Xtalk Chi2 Left: " << ndiff[7] << endl;
+  out << "Xtalk Chi2 Right: " << ndiff[8] << endl;
+  out << "Peds: " << ndiff[9] << endl;
+  out << "Ped RMS: " << ndiff[10] << endl;
+  out << "Noise Matrix 33: " << ndiff[11] << endl;
+  out << "Noise Matrix 34: " << ndiff[12] << endl;
+  out << "Noise Matrix 35: " << ndiff[13] << endl;
+  out << "Noise Matrix 44: " << ndiff[14] << endl;
+  out << "Noise Matrix 45: " << ndiff[15] << endl;
+  out << "Noise Matrix 46: " << ndiff[16] << endl;
+  out << "Noise Matrix 55: " << ndiff[17] << endl;
+  out << "Noise Matrix 56: " << ndiff[18] << endl;
+  out << "Noise Matrix 57: " << ndiff[19] << endl;
+  out << "Noise Matrix 66: " << ndiff[20] << endl;
+  out << "Noise Matrix 67: " << ndiff[21] << endl;
+  out << "Noise Matrix 77: " << ndiff[22] << endl;
+
+  out.close();
+}
+
+EOF
+
+root -l -q ${MACRO}
+
+rm checkCalib_temp.C
+
