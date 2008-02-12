a153012cb58baa334cfebaf92e721b78ca75f4ff
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2008-02-12 19:22:51 +0000

    Add additional debugging information to the Unpacker Report

--- DataFormats/HcalDigi/interface/HcalUnpackerReport.h
+++ DataFormats/HcalDigi/interface/HcalUnpackerReport.h
@@ -2,11 +2,13 @@
 #define DATAFORMATS_HCALDIGI_HCALUNPACKERREPORT_H 1
 
 #include <vector>
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/HcalDetId/interface/HcalElectronicsId.h"
 
 /** \class HcalUnpackerReport
   *  
-  * $Date: $
-  * $Revision: $
+  * $Date: 2006/10/09 14:43:25 $
+  * $Revision: 1.1 $
   * \author J. Mans - Minnesota
   */
 class HcalUnpackerReport {
@@ -20,19 +22,39 @@ public:
   int unmappedTPDigis() const { return unmappedTPDigis_; }
   int spigotFormatErrors() const { return spigotFormatErrors_; }
   int badQualityDigis() const { return badqualityDigis_; }
+  int totalDigis() const { return totalDigis_; }
+  int totalTPDigis() const { return totalTPDigis_; }
+  int totalHOTPDigis() const { return totalHOTPDigis_; }
+
+
+  typedef std::vector<DetId> DetIdVector;
+  typedef std::vector<HcalElectronicsId> ElectronicsIdVector;
+
+  DetIdVector::const_iterator bad_quality_begin() const { return badqualityIds_.begin(); }
+  DetIdVector::const_iterator bad_quality_end() const { return badqualityIds_.end(); }
+  ElectronicsIdVector::const_iterator unmapped_begin() const { return unmappedIds_.begin(); }
+  ElectronicsIdVector::const_iterator unmapped_end() const { return unmappedIds_.end(); }
 
   // setters
   void addUnpacked(int fed);
   void addError(int fed);
+  void countDigi();
+  void countTPDigi(bool ho=false);
   void countUnmappedDigi();
   void countUnmappedTPDigi();
   void countSpigotFormatError();
   void countBadQualityDigi();
+  void countUnmappedDigi(const HcalElectronicsId& eid);
+  void countUnmappedTPDigi(const HcalElectronicsId& eid);
+  void countBadQualityDigi(const DetId& did);
 private:
   std::vector<int> FEDsUnpacked_;
   std::vector<int> FEDsError_;
   int unmappedDigis_, unmappedTPDigis_;
   int spigotFormatErrors_, badqualityDigis_;
+  int totalDigis_, totalTPDigis_, totalHOTPDigis_;
+  DetIdVector badqualityIds_;
+  ElectronicsIdVector unmappedIds_;
 };
 
 #endif
--- DataFormats/HcalDigi/src/HcalUnpackerReport.cc
+++ DataFormats/HcalDigi/src/HcalUnpackerReport.cc
@@ -2,7 +2,8 @@
 
 HcalUnpackerReport::HcalUnpackerReport() :
   unmappedDigis_(0), unmappedTPDigis_(0),
-  spigotFormatErrors_(0), badqualityDigis_(0)
+  spigotFormatErrors_(0), badqualityDigis_(0),
+  totalDigis_(0),totalTPDigis_(0),totalHOTPDigis_(0)
 {
 }
 
@@ -21,6 +22,15 @@ void HcalUnpackerReport::addUnpacked(int fed) {
 void HcalUnpackerReport::addError(int fed) {
   FEDsError_.push_back(fed);
 }
+
+void HcalUnpackerReport::countDigi() {
+  totalDigis_++;
+}
+void HcalUnpackerReport::countTPDigi(bool ho) {
+  if (ho) totalHOTPDigis_++;
+  else totalTPDigis_++;
+}
+
 void HcalUnpackerReport::countUnmappedDigi() {
   unmappedDigis_++;
 }
@@ -32,4 +42,16 @@ void HcalUnpackerReport::countSpigotFormatError() {
 }
 void HcalUnpackerReport::countBadQualityDigi() {
   badqualityDigis_++;
+}
+void HcalUnpackerReport::countBadQualityDigi(const DetId& id) {
+  badqualityDigis_++;
+  badqualityIds_.push_back(id);
+}
+void HcalUnpackerReport::countUnmappedDigi(const HcalElectronicsId& eid) {
+  unmappedDigis_++;
+  unmappedIds_.push_back(eid);
+}
+void HcalUnpackerReport::countUnmappedTPDigi(const HcalElectronicsId& eid) {
+  unmappedTPDigis_++;
+  unmappedIds_.push_back(eid);
 }
