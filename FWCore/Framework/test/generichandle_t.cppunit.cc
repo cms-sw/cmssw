bb454e0d80995955e74bc4f8221596cef5f71ee3
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-02-12 21:49:14 +0000

    Added test for generic edm::Event 'put' using Reflex.

--- FWCore/Framework/test/generichandle_t.cppunit.cc
+++ FWCore/Framework/test/generichandle_t.cppunit.cc
@@ -2,7 +2,7 @@
 
 Test of the EventPrincipal class.
 
-$Id: generichandle_t.cppunit.cc,v 1.29 2008/01/30 00:32:02 wmtan Exp $
+$Id: generichandle_t.cppunit.cc,v 1.30 2008/01/31 04:56:34 wmtan Exp $
 
 ----------------------------------------------------------------------*/  
 #include <string>
@@ -27,13 +27,26 @@ $Id: generichandle_t.cppunit.cc,v 1.29 2008/01/30 00:32:02 wmtan Exp $
 #include "FWCore/Framework/interface/RunPrincipal.h"
 
 #include "FWCore/Framework/interface/GenericHandle.h"
+#include "FWCore/Framework/interface/GenericObjectOwner.h"
 #include <cppunit/extensions/HelperMacros.h>
+
+// This is a gross hack, to allow us to test the event
+namespace edm
+{
+   class EDProducer
+      {
+      public:
+         static void commitEvent(Event& e) { e.commit_(); }
+         
+      };
+}
 
 class testGenericHandle: public CppUnit::TestFixture {
 CPPUNIT_TEST_SUITE(testGenericHandle);
 CPPUNIT_TEST(failgetbyLabelTest);
 CPPUNIT_TEST(getbyLabelTest);
 CPPUNIT_TEST(failWrongType);
+CPPUNIT_TEST(putTest);
 CPPUNIT_TEST_SUITE_END();
 public:
   void setUp(){}
@@ -41,6 +54,7 @@ public:
   void failgetbyLabelTest();
   void failWrongType();
   void getbyLabelTest();
+  void putTest();
 };
 
 ///registration of the test so that the runner can find it
@@ -176,5 +190,94 @@ void testGenericHandle::getbyLabelTest() {
   }
   CPPUNIT_ASSERT(h.isValid());
   CPPUNIT_ASSERT(h.provenance()->moduleLabel() == label);
+}
+
+void testGenericHandle::putTest() {
+   std::string processName = "PROD";
+   
+   typedef edmtest::DummyProduct DP;
+   typedef edm::Wrapper<DP> WDP;
+   std::auto_ptr<DP> pr(new DP);
+   //std::auto_ptr<edm::EDProduct> pprod(new WDP(pr));
+   std::string label("fred");
+   std::string productInstanceName("Rick");
+   
+   edmtest::DummyProduct dp;
+   edm::TypeID dummytype(dp);
+   std::string className = dummytype.friendlyClassName();
+   
+   edm::BranchDescription product;
+   
+   product.fullClassName_ = dummytype.userClassName();
+   product.friendlyClassName_ = className;
+   
+   edm::ModuleDescription modDesc;
+   modDesc.moduleName_ = "Blah";
+   
+   product.moduleLabel_ = label;
+   product.productInstanceName_ = productInstanceName;
+   product.processName_ = processName;
+   product.moduleDescriptionID_ = modDesc.id();
+   product.init();
+   
+   edm::ProductRegistry *preg = new edm::ProductRegistry;
+   preg->addProduct(product);
+   preg->setProductIDs();
+   
+   edm::ProductRegistry::ProductList const& pl = preg->productList();
+   edm::BranchKey const bk(product);
+   edm::ProductRegistry::ProductList::const_iterator it = pl.find(bk);
+   product.productID_ = it->second.productID_;
+   
+   edm::EventID col(1L, 1L);
+   edm::Timestamp fakeTime;
+   std::string uuid = edm::createGlobalIdentifier();
+   edm::ProcessConfiguration pc("PROD", edm::ParameterSetID(), edm::getReleaseVersion(), edm::getPassID());
+   boost::shared_ptr<edm::ProductRegistry const> pregc(preg);
+   edm::RunAuxiliary runAux(col.run(), fakeTime, fakeTime);
+   boost::shared_ptr<edm::RunPrincipal> rp(new edm::RunPrincipal(runAux, pregc, pc));
+   edm::LuminosityBlockAuxiliary lumiAux(rp->run(), 1, fakeTime, fakeTime);
+   boost::shared_ptr<edm::LuminosityBlockPrincipal>lbp(new edm::LuminosityBlockPrincipal(lumiAux, pregc, rp, pc));
+   edm::EventAuxiliary eventAux(col, uuid, fakeTime, lbp->luminosityBlock(), true);
+   edm::EventPrincipal ep(eventAux, pregc, lbp, pc);
+   
+   {
+      edm::ModuleDescription modDesc;
+      modDesc.moduleName_="Fred";
+      modDesc.moduleLabel_=label;
+      modDesc.processConfiguration_ = pc;
+      edm::Event ev(ep,modDesc);
+      
+      ROOT::Reflex::Object obj( ROOT::Reflex::Type::ByTypeInfo(typeid(edmtest::DummyProduct)), pr.release());
+      std::auto_ptr<edm::GenericObjectOwner> goo(new edm::GenericObjectOwner(obj));
+      edm::OrphanHandle<edm::GenericObjectOwner> oh(ev.put(goo,productInstanceName));
+      CPPUNIT_ASSERT(0!= oh->object().Address());
+      
+      edm::EDProducer::commitEvent(ev);
+   }
+   
+   edm::GenericHandle h("edmtest::DummyProduct");
+   try {
+      edm::ModuleDescription modDesc;
+      modDesc.moduleName_="Blah";
+      modDesc.moduleLabel_="blahs"; 
+      edm::Event event(ep, modDesc);
+      
+      event.getByLabel(label, productInstanceName,h);
+   }
+   catch (cms::Exception& x) {
+      std::cerr << x.explainSelf()<< std::endl;
+      CPPUNIT_ASSERT("Threw cms::Exception unexpectedly" == 0);
+   }
+   catch(std::exception& x){
+      std::cerr <<x.what()<<std::endl;
+      CPPUNIT_ASSERT("threw std::exception"==0);
+   }
+   catch (...) {
+      std::cerr << "Unknown exception type\n";
+      CPPUNIT_ASSERT("Threw exception unexpectedly" == 0);
+   }
+   CPPUNIT_ASSERT(h.isValid());
+   CPPUNIT_ASSERT(h.provenance()->moduleLabel() == label);
 }
 
