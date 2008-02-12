c18815276e0fabf7a3c99a4205ef7c68d024f8c5
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-02-12 14:48:15 +0000

    Take putInEvt from the latest version of KfTrackProducerBase.cc

--- RecoEgamma/EgammaPhotonProducers/src/TrackProducerWithSCAssociation.cc
+++ RecoEgamma/EgammaPhotonProducers/src/TrackProducerWithSCAssociation.cc
@@ -80,10 +80,8 @@ void TrackProducerWithSCAssociation::produce(edm::Event& theEvent, const edm::Ev
   edm::Handle<TrackCandidateCollection> theTCCollection;
   //// Get the association map between candidate out in tracks and the SC where they originated
   edm::Handle<reco::TrackCandidateSuperClusterAssociationCollection> trkCandidateSCAssocHandle;
+
   theEvent.getByLabel(conversionTrackCandidateProducer_, trackCSuperClusterAssociationCollection_ , trkCandidateSCAssocHandle);
-  if (!theTCCollection.isValid()) {
-    edm::LogError("TrackProducerWithSCAssociation") << "Error! Can't get the product "<< trackCSuperClusterAssociationCollection_.c_str();
-  }
   reco::TrackCandidateSuperClusterAssociationCollection scTrkCandAssCollection = *trkCandidateSCAssocHandle;  
   LogDebug("TrackProducerWithSCAssociation")  << "TrackProducerWithSCAssociation  TrkCandidateSCAssoc collection size " << (*trkCandidateSCAssocHandle).size() <<"\n";
 
@@ -235,38 +233,55 @@ std::vector<reco::TransientTrack> TrackProducerWithSCAssociation::getTransient(e
 				 AlgoProductCollection& algoResults)
 {
 
-  TrackingRecHitRefProd rHits = evt.getRefBeforePut<TrackingRecHitCollection>();
+TrackingRecHitRefProd rHits = evt.getRefBeforePut<TrackingRecHitCollection>();
   reco::TrackExtraRefProd rTrackExtras = evt.getRefBeforePut<reco::TrackExtraCollection>();
-  //doesnt work!!  reco::TrackRefProd rTracks = evt.getRefBeforePut<reco::TrackCollection>();
 
   edm::Ref<reco::TrackExtraCollection>::key_type idx = 0;
   edm::Ref<reco::TrackExtraCollection>::key_type hidx = 0;
+  edm::Ref<reco::TrackCollection>::key_type iTkRef = 0;
+  edm::Ref< std::vector<Trajectory> >::key_type iTjRef = 0;
+  std::map<unsigned int, unsigned int> tjTkMap;
+
   for(AlgoProductCollection::iterator i=algoResults.begin(); i!=algoResults.end();i++){
     Trajectory * theTraj = (*i).first;
-    if(myTrajectoryInEvent_) selTrajectories->push_back(*theTraj);
+    if(myTrajectoryInEvent_) {
+      selTrajectories->push_back(*theTraj);
+      iTjRef++;
+    }
     const TrajectoryFitter::RecHitContainer& transHits = theTraj->recHits();
-
-    reco::Track * theTrack = ((*i).second).first;
     
-    //     if( ) {
+    reco::Track * theTrack = (*i).second.first;
+    PropagationDirection seedDir = (*i).second.second;
+    
+    LogDebug("TrackProducer") << "In KfTrackProducerBase::putInEvt - seedDir=" << seedDir;
+    
     reco::Track t = * theTrack;
     selTracks->push_back( t );
+    iTkRef++;
+    
+    // Store indices in local map (starts at 0)
+    if(trajectoryInEvent_) tjTkMap[iTjRef-1] = iTkRef-1;
     
     //sets the outermost and innermost TSOSs
-  TrajectoryStateOnSurface outertsos;
+    
+    TrajectoryStateOnSurface outertsos;
     TrajectoryStateOnSurface innertsos;
     unsigned int innerId, outerId;
+
+    // ---  NOTA BENE: the convention is to sort hits and measurements "along the momentum".
+    // This is consistent with innermost and outermost labels only for tracks from LHC collision
     if (theTraj->direction() == alongMomentum) {
       outertsos = theTraj->lastMeasurement().updatedState();
       innertsos = theTraj->firstMeasurement().updatedState();
       outerId = theTraj->lastMeasurement().recHit()->geographicalId().rawId();
       innerId = theTraj->firstMeasurement().recHit()->geographicalId().rawId();
-    } else { 
+    } else {
       outertsos = theTraj->firstMeasurement().updatedState();
       innertsos = theTraj->lastMeasurement().updatedState();
       outerId = theTraj->firstMeasurement().recHit()->geographicalId().rawId();
       innerId = theTraj->lastMeasurement().recHit()->geographicalId().rawId();
-   }
+    }
+    // ---
     //build the TrackExtra
     GlobalPoint v = outertsos.globalParameters().position();
     GlobalVector p = outertsos.globalParameters().momentum();
@@ -280,27 +295,81 @@ std::vector<reco::TransientTrack> TrackProducerWithSCAssociation::getTransient(e
     reco::TrackExtraRef teref= reco::TrackExtraRef ( rTrackExtras, idx ++ );
     reco::Track & track = selTracks->back();
     track.setExtra( teref );
-    PropagationDirection dir = theTraj->direction();
+
+
     selTrackExtras->push_back( reco::TrackExtra (outpos, outmom, true, inpos, inmom, true,
-						 outertsos.curvilinearError(), outerId,
-						 innertsos.curvilinearError(), innerId, dir));
+                                                 outertsos.curvilinearError(), outerId,
+                                                 innertsos.curvilinearError(), innerId,
+                                                 seedDir,theTraj->seedRef()));
+
 
     reco::TrackExtra & tx = selTrackExtras->back();
-    size_t k = 0;
-    for( TrajectoryFitter::RecHitContainer::const_iterator j = transHits.begin();
-	 j != transHits.end(); j ++ ) {
-      TrackingRecHit * hit = (**j).hit()->clone();
-      track.setHitPattern( * hit, k ++ );
-      selHits->push_back( hit );
-      tx.add( TrackingRecHitRef( rHits, hidx ++ ) );
+   // ---  NOTA BENE: the convention is to sort hits and measurements "along the momentum".
+    // This is consistent with innermost and outermost labels only for tracks from LHC collisions
+    size_t i = 0;
+    if (theTraj->direction() == alongMomentum) {
+      for( TrajectoryFitter::RecHitContainer::const_iterator j = transHits.begin();
+           j != transHits.end(); j ++ ) {
+        if ((**j).hit()!=0){
+          TrackingRecHit * hit = (**j).hit()->clone();
+          track.setHitPattern( * hit, i ++ );
+          selHits->push_back( hit );
+          tx.add( TrackingRecHitRef( rHits, hidx ++ ) );
+        }
+      }
+    }else{
+      for( TrajectoryFitter::RecHitContainer::const_iterator j = transHits.end()-1;
+           j != transHits.begin()-1; --j ) {
+        if ((**j).hit()!=0){
+          TrackingRecHit * hit = (**j).hit()->clone();
+          track.setHitPattern( * hit, i ++ );
+          selHits->push_back( hit );
+        tx.add( TrackingRecHitRef( rHits, hidx ++ ) );
+        }
+      }
     }
+    // ----
+
     delete theTrack;
     delete theTraj;
   }
 
-  rTracks_ = evt.put( selTracks ); //this is changed
+  LogTrace("TrackingRegressionTest") << "========== TrackProducer Info ===================";
+  LogTrace("TrackingRegressionTest") << "number of finalTracks: " << selTracks->size();
+  for (reco::TrackCollection::const_iterator it = selTracks->begin(); it != selTracks->end(); it++) {
+    LogTrace("TrackingRegressionTest") << "track's n valid and invalid hit, chi2, pt : "
+                                       << it->found() << " , "
+                                       << it->lost()  <<" , "
+                                       << it->normalizedChi2() << " , "
+                                       << it->pt();
+  }
+  LogTrace("TrackingRegressionTest") << "=================================================";
+
+
+  rTracks_ = evt.put( selTracks );
+
+
   evt.put( selTrackExtras );
   evt.put( selHits );
-  if(myTrajectoryInEvent_) evt.put(selTrajectories);
+  
+  if(myTrajectoryInEvent_) {
+    edm::OrphanHandle<std::vector<Trajectory> > rTrajs = evt.put(selTrajectories);
+    
+    // Now Create traj<->tracks association map
+    std::auto_ptr<TrajTrackAssociationCollection> trajTrackMap( new TrajTrackAssociationCollection() );
+    for ( std::map<unsigned int, unsigned int>::iterator i = tjTkMap.begin();
+          i != tjTkMap.end(); i++ ) {
+      edm::Ref<std::vector<Trajectory> > trajRef( rTrajs, (*i).first );
+      edm::Ref<reco::TrackCollection>    tkRef( rTracks_, (*i).second );
+      trajTrackMap->insert( edm::Ref<std::vector<Trajectory> >( rTrajs, (*i).first ),
+                            edm::Ref<reco::TrackCollection>( rTracks_, (*i).second ) );
+    }
+    evt.put( trajTrackMap );
+  }
+
+
+
+
+
 
 }
