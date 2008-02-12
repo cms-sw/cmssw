a592e1e92d9dcfedf3f51976a9ed9f55c30313e4
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-02-12 21:48:33 +0000

    Used for generic edm::Event 'put' using Reflex.

---
+++ FWCore/Framework/src/GenericObjectOwner.cc
@@ -0,0 +1,82 @@
+// -*- C++ -*-
+//
+// Package:     Framework
+// Class  :     GenericObjectOwner
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Thu Feb  7 17:21:22 EST 2008
+// $Id$
+//
+
+// system include files
+
+// user include files
+#include "FWCore/Framework/interface/GenericObjectOwner.h"
+
+namespace edm {
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+//GenericObjectOwner::GenericObjectOwner()
+//{
+//}
+
+// GenericObjectOwner::GenericObjectOwner(const GenericObjectOwner& rhs)
+// {
+//    // do actual copying here;
+// }
+
+GenericObjectOwner::~GenericObjectOwner()
+{
+   if(m_ownData) {
+      m_object.Destruct();
+   }
+}
+
+//
+// assignment operators
+//
+// const GenericObjectOwner& GenericObjectOwner::operator=(const GenericObjectOwner& rhs)
+// {
+//   //An exception safe implementation is
+//   GenericObjectOwner temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+void 
+GenericObjectOwner::swap(GenericObjectOwner& iOther)
+{
+   ROOT::Reflex::Object old(m_object);
+   m_object = iOther.m_object;
+   iOther.m_object = m_object;
+}
+
+//
+// const member functions
+//
+ROOT::Reflex::Object 
+GenericObjectOwner::object() const
+{
+   return m_object;
+}
+
+//
+// static member functions
+//
+}
