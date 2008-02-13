c3147921b9a7121ef74216405a18f53eded5358b
Shan-Huei Chuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-02-13 14:18:11 +0000

    addition of SiPixelPerformanceSummary test items.

--- CondTools/SiPixel/test/SealModules.cc
+++ CondTools/SiPixel/test/SealModules.cc
@@ -1,11 +1,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 #include "CondTools/SiPixel/test/SiPixelCondObjBuilder.h"
 #include "CondTools/SiPixel/test/SiPixelCondObjReader.h"
 #include "CondTools/SiPixel/test/SiPixelCondObjForHLTBuilder.h"
 #include "CondTools/SiPixel/test/SiPixelCondObjForHLTReader.h"
 #include "CondTools/SiPixel/test/SiPixelCondObjOfflineBuilder.h"
 #include "CondTools/SiPixel/test/SiPixelCondObjOfflineReader.h"
+#include "CondTools/SiPixel/test/SiPixelPerformanceSummaryBuilder.h"
+#include "CondTools/SiPixel/test/SiPixelPerformanceSummaryReader.h"
 
 
 using cms::SiPixelCondObjBuilder;
@@ -14,12 +17,17 @@ using cms::SiPixelCondObjForHLTBuilder;
 using cms::SiPixelCondObjForHLTReader;
 using cms::SiPixelCondObjOfflineBuilder;
 using cms::SiPixelCondObjOfflineReader;
+using cms::SiPixelPerformanceSummaryBuilder;
+using cms::SiPixelPerformanceSummaryReader;
 
 
 DEFINE_SEAL_MODULE();
+
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjBuilder);
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjReader);
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjForHLTBuilder);
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjForHLTReader);
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjOfflineBuilder);
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCondObjOfflineReader);
+DEFINE_ANOTHER_FWK_MODULE(SiPixelPerformanceSummaryBuilder);
+DEFINE_ANOTHER_FWK_MODULE(SiPixelPerformanceSummaryReader);
