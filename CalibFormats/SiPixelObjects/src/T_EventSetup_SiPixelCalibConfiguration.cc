8399213f18bc8564b797708f2261f97b28f4dafd
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2008-02-14 11:11:26 +0000

    moved objects to CondFormats/SiPixelObjects (which will now link to this package CalibFormats/SiPixelObjects...)

--- CalibFormats/SiPixelObjects/src/T_EventSetup_SiPixelCalibConfiguration.cc
+++
@@ -1,4 +0,0 @@
-#include "CalibFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
-EVENTSETUP_DATA_REG(SiPixelCalibConfiguration);
--- CalibFormats/SiPixelObjects/src/classes.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <string>
-#include <iostream>
-#include <utility>
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "CalibFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h"
-
