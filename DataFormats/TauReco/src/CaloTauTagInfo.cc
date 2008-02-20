b6ed15d59b7cc1b48270e76f43f7f8fe975e5366
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2008-02-20 16:09:25 +0000

    replaced BasicClusterRefVector& neutralECALBasicClusters() by vector<BasicClusterRef> neutralECALBasicClusters()

--- DataFormats/TauReco/src/CaloTauTagInfo.cc
+++ DataFormats/TauReco/src/CaloTauTagInfo.cc
@@ -8,6 +8,6 @@ void CaloTauTagInfo::setcalojetRef(const CaloJetRef x){CaloJetRef_=x;}
 const vector<pair<math::XYZPoint,float> > CaloTauTagInfo::positionAndEnergyECALRecHits()const{return positionAndEnergyECALRecHits_;}
 void CaloTauTagInfo::setpositionAndEnergyECALRecHits(vector<pair<math::XYZPoint,float> > x){positionAndEnergyECALRecHits_=x;}
   
-const BasicClusterRefVector& CaloTauTagInfo::neutralECALBasicClusters()const{return neutralECALBasicClusters_;}
-void CaloTauTagInfo::setneutralECALBasicClusters(BasicClusterRefVector x){neutralECALBasicClusters_=x;}
+const vector<BasicClusterRef> CaloTauTagInfo::neutralECALBasicClusters()const{return neutralECALBasicClusters_;}
+void CaloTauTagInfo::setneutralECALBasicClusters(vector<BasicClusterRef> x){neutralECALBasicClusters_=x;}
   
