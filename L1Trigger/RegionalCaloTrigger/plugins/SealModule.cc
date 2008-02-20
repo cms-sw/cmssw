3c739ae540c469db3561712dddae7b58d7ab45cc
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2008-02-20 17:54:07 +0000

    Adding plots for release validation

--- L1Trigger/RegionalCaloTrigger/plugins/SealModule.cc
+++ L1Trigger/RegionalCaloTrigger/plugins/SealModule.cc
@@ -3,9 +3,11 @@
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTInputProducer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTTestAnalyzer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTSaveInput.h"
+#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTRelValAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L1RCTProducer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTInputProducer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTTestAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTSaveInput);
+DEFINE_ANOTHER_FWK_MODULE(L1RCTRelValAnalyzer);
