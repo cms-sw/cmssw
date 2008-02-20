2bb75a71267070c5a98b258af67eb411c6cf924f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-02-20 12:13:04 +0000

    added L1 CSCTF configuration Record

---
+++ CondFormats/DataRecord/interface/L1MuCSCTFConfigurationRcd.h
@@ -0,0 +1,16 @@
+#ifndef CondFormatsDataRecord_L1MuCSCTFConfigurationRcd_h
+
+#define CondFormatsDataRecord_L1MuCSCTFConfigurationRcd_h
+
+
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+
+
+class L1MuCSCTFConfigurationRcd : public edm::eventsetup::EventSetupRecordImplementation<L1MuCSCTFConfigurationRcd> {};
+
+
+
+#endif
+
---
+++ CondFormats/DataRecord/src/L1MuCSCTFConfigurationRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     L1MuCSCTFConfigurationRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:  K. Khotov
+// Created: 20 Feb 2008
+// $Id$
+
+#include "CondFormats/DataRecord/interface/L1MuCSCTFConfigurationRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(L1MuCSCTFConfigurationRcd);
