f4b4909dc4518ec147322079377c7dab8dee0a26
houchu <sha1-fb47cddc526e1145f4232d985e69b67296a1a214@cern.ch>
2008-02-20 16:03:27 +0000

    added member functions for hadr. tau-jet / electron discrimination

--- DataFormats/TauReco/interface/CaloTau.h
+++ DataFormats/TauReco/interface/CaloTau.h
@@ -6,7 +6,7 @@
  *                          is a hadronic tau-jet candidate -built from a calo. jet- that analysts manipulate;
  * authors: Simone Gennai (simone.gennai@cern.ch), Ludovic Houchu (Ludovic.Houchu@cern.ch)
  * created: Jun 21 2007,
- * revised: Sep 28 2007
+ * revised: Feb 20 2007
  */
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "DataFormats/TauReco/interface/BaseTau.h"
@@ -31,6 +31,14 @@ namespace reco {
     // signed transverse impact parameter significance of leading Track ; NaN if no leading Track
     float leadTracksignedSipt()const;
     void setleadTracksignedSipt(const float&);
+
+    // sum of Et of HCAL hits inside a 3x3 calo. tower matrix centered on direction of propag. leading Track - ECAL inner surf. contact point ; NaN if no leading Track or if invalid propag. leading Track - ECAL inner surf. contact point
+    float leadTrackHCAL3x3hitsEtSum()const;
+    void setleadTrackHCAL3x3hitsEtSum(const float&);
+
+    // |DEta| between direction of propag. leading Track - ECAL inner surf. contact point and direction of highest Et hit among HCAL hits inside a 3x3 calo. tower matrix centered on direction of propag. leading Track - ECAL inner surf. contact point ; NaN if no leading Track or if invalid propag. leading Track - ECAL inner surf. contact point
+    float leadTrackHCAL3x3hottesthitDEta()const;
+    void setleadTrackHCAL3x3hottesthitDEta(const float&);
     
     // invariant mass of the system of Tracks inside a signal cone around leading Track ; NaN if no leading Track
     float signalTracksInvariantMass()const;
@@ -48,7 +56,7 @@ namespace reco {
     float isolationECALhitsEtSum()const;
     void setisolationECALhitsEtSum(const float&);
   
-    // Et of the highest Et HCAL CaloTower
+    // Et of the highest Et HCAL hit
     float maximumHCALhitEt()const;
     void setmaximumHCALhitEt(const float&);    
   private:
@@ -56,6 +64,8 @@ namespace reco {
     virtual bool overlap(const Candidate&d)const;
     CaloTauTagInfoRef CaloTauTagInfoRef_;  
     float leadTracksignedSipt_;
+    float leadTrackHCAL3x3hitsEtSum_;
+    float leadTrackHCAL3x3hottesthitDEta_;
     float signalTracksInvariantMass_;
     float TracksInvariantMass_; 
     float isolationTracksPtSum_;
