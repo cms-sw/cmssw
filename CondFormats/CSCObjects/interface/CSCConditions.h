4a0cf184722f0d483430a155b5ada09b7a4cf644
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2008-02-21 20:21:42 +0000

    extending and updating

--- CondFormats/CSCObjects/interface/CSCConditions.h
+++ CondFormats/CSCObjects/interface/CSCConditions.h
@@ -4,12 +4,17 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/MuonDetId/interface/CSCDetId.h"
 #include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
+#include <vector>
+
 class CSCDBGains;
 class CSCDBPedestals;
 class CSCDBCrosstalk;
 
-/*  Encapsulates a user interface into the CSC conditions
-*/
+/**  Encapsulates a user interface into the CSC conditions
+ *
+ * \author Rick Wilkinson
+ * \author Tim Cox
+ */
 
 class CSCConditions
 {
@@ -32,10 +37,19 @@ public:
   float crosstalkSlope(const CSCDetId&detId, int channel, bool leftRight) const;
   float crosstalkIntercept(const CSCDetId&detId, int channel, bool leftRight) const;
 
-  const CSCDBNoiseMatrix::Item &
-  noiseMatrix(const CSCDetId&detId, int channel) const;
+  /// return raw noise matrix (unscaled short int elements)
+  const CSCDBNoiseMatrix::Item & noiseMatrix(const CSCDetId&detId, int channel) const;
+
+  /// fill vector (dim 12, must be allocated by caller) with noise matrix elements (scaled to float)
+  void noiseMatrixElements( const CSCDetId& id, int channel, std::vector<float>& me ) const;
+
+  /// fill vector (dim 4, must be allocated by caller) with crosstalk sl, il, sr, ir
+  void crossTalk( const CSCDetId& id, int channel, std::vector<float>& ct ) const;
 
   void print() const;
+
+  /// average gain over entire CSC system (logically const although must be cached here).
+  float averageGain() const;
 
 private:
 
@@ -44,6 +58,7 @@ private:
   const CSCDBPedestals * thePedestals;
   const CSCDBCrosstalk * theCrosstalk;
 
+  mutable float theAverageGain; // average over entire system, subject to some constraints!
 };
 
 #endif
