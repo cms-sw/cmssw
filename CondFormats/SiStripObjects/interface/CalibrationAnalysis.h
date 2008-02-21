9ecdad98903e816ee5b03ed39d4f668543802438
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2008-02-21 09:14:24 +0000

    first working implementation of the calibration task

--- CondFormats/SiStripObjects/interface/CalibrationAnalysis.h
+++ CondFormats/SiStripObjects/interface/CalibrationAnalysis.h
@@ -20,18 +20,47 @@ class CalibrationAnalysis : public CommissioningAnalysis {
   
  public:
   
-  CalibrationAnalysis( const uint32_t& key, const bool& deconv );
-  CalibrationAnalysis(const bool& deconv = false);
+  CalibrationAnalysis( const uint32_t& key, const bool& deconv, int calchan );
+  CalibrationAnalysis(const bool& , int  );
   virtual ~CalibrationAnalysis() {;}
   
-  inline const float& amplitude() const { return amplitude_; }
-  inline const float& tail() const { return tail_; }
-  inline const float& riseTime() const { return riseTime_; }
-  inline const float& timeConstant() const { return timeConstant_; }
-  inline const float& smearing() const { return smearing_; }
-  inline const float& chi2() const { return chi2_; }
+  // values per strip and per APV
+  inline const VVFloat& amplitude() const { return amplitude_; }
+  inline const VVFloat& tail() const { return tail_; }
+  inline const VVFloat& riseTime() const { return riseTime_; }
+  inline const VVFloat& timeConstant() const { return timeConstant_; }
+  inline const VVFloat& smearing() const { return smearing_; }
+  inline const VVFloat& chi2() const { return chi2_; }
+
+  // mean values per APV
+  inline const VFloat& amplitudeMean() const { return mean_amplitude_; }
+  inline const VFloat& tailMean() const { return mean_tail_; }
+  inline const VFloat& riseTimeMean() const { return mean_riseTime_; }
+  inline const VFloat& timeConstantMean() const { return mean_timeConstant_; }
+  inline const VFloat& smearingMean() const { return mean_smearing_; }
+  inline const VFloat& chi2Mean() const { return mean_chi2_; }
+
+  // spread, min and max
+  inline const VFloat& amplitudeSpread() const { return spread_amplitude_; }
+  inline const VFloat& amplitudeMin() const { return min_amplitude_; }
+  inline const VFloat& amplitudeMax() const { return max_amplitude_; }
+  inline const VFloat& tailSpread() const { return spread_tail_; }
+  inline const VFloat& tailMin() const { return min_tail_; }
+  inline const VFloat& tailMax() const { return max_tail_; }
+  inline const VFloat& riseTimeSpread() const { return spread_riseTime_; }
+  inline const VFloat& riseTimeMin() const { return min_riseTime_; }
+  inline const VFloat& riseTimeMax() const { return max_riseTime_; }
+  inline const VFloat& timeConstantSpread() const { return spread_timeConstant_; }
+  inline const VFloat& timeConstantMin() const { return min_timeConstant_; }
+  inline const VFloat& timeConstantMax() const { return max_timeConstant_; }
+  inline const VFloat& smearingSpread() const { return spread_smearing_; }
+  inline const VFloat& smearingMin() const { return min_smearing_; }
+  inline const VFloat& smearingMax() const { return max_smearing_; }
+  inline const VFloat& chi2Spread() const { return spread_chi2_; }
+  inline const VFloat& chi2Min() const { return min_chi2_; }
+  inline const VFloat& chi2Max() const { return max_chi2_; }
   
-  inline const Histo& histo() const;
+  inline const Histo& histo(int i) const { return histo_[i]; }
   void print( std::stringstream&, uint32_t not_used = 0 );
   
  private:
@@ -45,15 +74,21 @@ class CalibrationAnalysis : public CommissioningAnalysis {
  private:
   
   /** Parameters extracted from the fit of pulse shape */
-  float amplitude_,tail_,riseTime_,timeConstant_,smearing_,chi2_;
+  VVFloat amplitude_, tail_, riseTime_, timeConstant_, smearing_, chi2_;
+  VFloat  mean_amplitude_,mean_tail_,mean_riseTime_,mean_timeConstant_,mean_smearing_,mean_chi2_;
+  VFloat  min_amplitude_,min_tail_,min_riseTime_,min_timeConstant_,min_smearing_,min_chi2_;
+  VFloat  max_amplitude_,max_tail_,max_riseTime_,max_timeConstant_,max_smearing_,max_chi2_;
+  VFloat  spread_amplitude_,spread_tail_,spread_riseTime_,spread_timeConstant_,spread_smearing_,spread_chi2_;
   /** pulse shape*/
-  Histo histo_;
+  Histo histo_[32];
   /** Fitter in deconvolution mode */
   TF1* deconv_fitter_;
   /** Fitter in peak mode */
   TF1* peak_fitter_;
   /** fit mode: deconv or not ? */
   bool deconv_;
+  /** calchan value used in that dataset */
+  int calchan_;
   
 };
 
--- CondFormats/SiStripObjects/src/CalibrationAnalysis.cc
+++ CondFormats/SiStripObjects/src/CalibrationAnalysis.cc
@@ -16,96 +16,172 @@ using namespace sistrip;
 
 // ----------------------------------------------------------------------------
 // 
-CalibrationAnalysis::CalibrationAnalysis( const uint32_t& key, const bool& deconv ) 
+CalibrationAnalysis::CalibrationAnalysis( const uint32_t& key, const bool& deconv, int calchan ) 
   : CommissioningAnalysis(key,"CalibrationAnalysis"),
-    amplitude_(sistrip::invalid_),
-    tail_(sistrip::invalid_),
-    riseTime_(sistrip::invalid_),
-    timeConstant_(sistrip::invalid_),
-    smearing_(sistrip::invalid_),
-    chi2_(sistrip::invalid_),
-    histo_(0,""),
-    deconv_(deconv)
+    amplitude_(2,VFloat(128,sistrip::invalid_)),
+    tail_(2,VFloat(128,sistrip::invalid_)),
+    riseTime_(2,VFloat(128,sistrip::invalid_)),
+    timeConstant_(2,VFloat(128,sistrip::invalid_)),
+    smearing_(2,VFloat(128,sistrip::invalid_)),
+    chi2_(2,VFloat(128,sistrip::invalid_)),
+    mean_amplitude_(2,sistrip::invalid_),
+    mean_tail_(2,sistrip::invalid_),
+    mean_riseTime_(2,sistrip::invalid_),
+    mean_timeConstant_(2,sistrip::invalid_),
+    mean_smearing_(2,sistrip::invalid_),
+    mean_chi2_(2,sistrip::invalid_),
+    min_amplitude_(2,sistrip::invalid_),
+    min_tail_(2,sistrip::invalid_),
+    min_riseTime_(2,sistrip::invalid_),
+    min_timeConstant_(2,sistrip::invalid_),
+    min_smearing_(2,sistrip::invalid_),
+    min_chi2_(2,sistrip::invalid_),
+    max_amplitude_(2,sistrip::invalid_),
+    max_tail_(2,sistrip::invalid_),
+    max_riseTime_(2,sistrip::invalid_),
+    max_timeConstant_(2,sistrip::invalid_),
+    max_smearing_(2,sistrip::invalid_),
+    max_chi2_(2,sistrip::invalid_),
+    spread_amplitude_(2,sistrip::invalid_),
+    spread_tail_(2,sistrip::invalid_),
+    spread_riseTime_(2,sistrip::invalid_),
+    spread_timeConstant_(2,sistrip::invalid_),
+    spread_smearing_(2,sistrip::invalid_),
+    spread_chi2_(2,sistrip::invalid_),
+    deconv_(deconv),
+    calchan_(calchan)
 {
   deconv_fitter_ = new TF1("deconv_fitter",fdeconv_convoluted,-50,50,5);
   deconv_fitter_->FixParameter(0,0);
-  deconv_fitter_->SetParLimits(1,-10,10);
+  deconv_fitter_->SetParLimits(1,-100,0);
   deconv_fitter_->SetParLimits(2,0,200);
   deconv_fitter_->SetParLimits(3,5,100);
   deconv_fitter_->FixParameter(3,50);
   deconv_fitter_->SetParLimits(4,0,50);
-  deconv_fitter_->SetParameters(0.,-2.82,0.96,50,20);
+  deconv_fitter_->SetParameters(0.,-10,0.96,50,20);
   peak_fitter_ = new TF1("peak_fitter",fpeak_convoluted,-50,50,5);
   peak_fitter_->FixParameter(0,0);
-  peak_fitter_->SetParLimits(1,-10,10);
+  peak_fitter_->SetParLimits(1,-100,0);
   peak_fitter_->SetParLimits(2,0,200);
   peak_fitter_->SetParLimits(3,5,100);
   peak_fitter_->FixParameter(3,50);
   peak_fitter_->SetParLimits(4,0,50);
-  peak_fitter_->SetParameters(0.,-2.82,0.96,50,20);
+  peak_fitter_->SetParameters(0.,-10,0.96,50,20);
 }
 // ----------------------------------------------------------------------------
 // 
-CalibrationAnalysis::CalibrationAnalysis(const bool& deconv) 
+CalibrationAnalysis::CalibrationAnalysis(const bool& deconv, int calchan) 
   : CommissioningAnalysis("CalibrationAnalysis"),
-    amplitude_(sistrip::invalid_),
-    tail_(sistrip::invalid_),
-    riseTime_(sistrip::invalid_),
-    timeConstant_(sistrip::invalid_),
-    smearing_(sistrip::invalid_),
-    chi2_(sistrip::invalid_),
-    histo_(0,""),
-    deconv_(deconv)
+    amplitude_(2,VFloat(128,sistrip::invalid_)),
+    tail_(2,VFloat(128,sistrip::invalid_)),
+    riseTime_(2,VFloat(128,sistrip::invalid_)),
+    timeConstant_(2,VFloat(128,sistrip::invalid_)),
+    smearing_(2,VFloat(128,sistrip::invalid_)),
+    chi2_(2,VFloat(128,sistrip::invalid_)),
+    mean_amplitude_(2,sistrip::invalid_),
+    mean_tail_(2,sistrip::invalid_),
+    mean_riseTime_(2,sistrip::invalid_),
+    mean_timeConstant_(2,sistrip::invalid_),
+    mean_smearing_(2,sistrip::invalid_),
+    mean_chi2_(2,sistrip::invalid_),
+    min_amplitude_(2,sistrip::invalid_),
+    min_tail_(2,sistrip::invalid_),
+    min_riseTime_(2,sistrip::invalid_),
+    min_timeConstant_(2,sistrip::invalid_),
+    min_smearing_(2,sistrip::invalid_),
+    min_chi2_(2,sistrip::invalid_),
+    max_amplitude_(2,sistrip::invalid_),
+    max_tail_(2,sistrip::invalid_),
+    max_riseTime_(2,sistrip::invalid_),
+    max_timeConstant_(2,sistrip::invalid_),
+    max_smearing_(2,sistrip::invalid_),
+    max_chi2_(2,sistrip::invalid_),
+    spread_amplitude_(2,sistrip::invalid_),
+    spread_tail_(2,sistrip::invalid_),
+    spread_riseTime_(2,sistrip::invalid_),
+    spread_timeConstant_(2,sistrip::invalid_),
+    spread_smearing_(2,sistrip::invalid_),
+    spread_chi2_(2,sistrip::invalid_),
+    deconv_(deconv),
+    calchan_(calchan)
 {
   deconv_fitter_ = new TF1("deconv_fitter",fdeconv_convoluted,-50,50,5);
   deconv_fitter_->FixParameter(0,0);
-  deconv_fitter_->SetParLimits(1,-10,10);
+  deconv_fitter_->SetParLimits(1,-100,0);
   deconv_fitter_->SetParLimits(2,0,200);
   deconv_fitter_->SetParLimits(3,5,100);
   deconv_fitter_->FixParameter(3,50);
   deconv_fitter_->SetParLimits(4,0,50);
-  deconv_fitter_->SetParameters(0.,-2.82,0.96,50,20);
+  deconv_fitter_->SetParameters(0.,-10,0.96,50,20);
   peak_fitter_ = new TF1("peak_fitter",fpeak_convoluted,-50,50,5);
   peak_fitter_->FixParameter(0,0);
-  peak_fitter_->SetParLimits(1,-10,10);
+  peak_fitter_->SetParLimits(1,-100,0);
   peak_fitter_->SetParLimits(2,0,200);
   peak_fitter_->SetParLimits(3,5,100);
   peak_fitter_->FixParameter(3,50);
   peak_fitter_->SetParLimits(4,0,50);
-  peak_fitter_->SetParameters(0.,-2.82,0.96,50,20);
+  peak_fitter_->SetParameters(0.,-10,0.96,50,20);
 }
 
 // ----------------------------------------------------------------------------
 // 
-void CalibrationAnalysis::print( std::stringstream& ss, uint32_t not_used ) { 
+void CalibrationAnalysis::print( std::stringstream& ss, uint32_t iapv ) { 
   header( ss );
-  ss << " Amplitude of the pulse : " << amplitude_ << std::endl
-     << " Tail amplitude after 150ns : " << tail_ << std::endl
-     << " Rise time : " << riseTime_ << std::endl
-     << " Time constant : " << timeConstant_ << std::endl
-     << " Smearing parameter : " << smearing_ << std::endl
-     << " Chi2 of the fit : " << chi2_ << std::endl;
+  ss << " Monitorables for APV number     : " << iapv;
+  if ( iapv == 0 ) { ss << " (first of pair)"; }
+  else if ( iapv == 1 ) { ss << " (second of pair)"; }
+  ss << std::endl;
+  ss << " Mean values:" << std::endl;
+  ss << " Amplitude of the pulse : " << mean_amplitude_[iapv] << std::endl
+     << " Tail amplitude after 150ns : " << mean_tail_[iapv] << std::endl
+     << " Rise time : " << mean_riseTime_[iapv] << std::endl
+     << " Time constant : " << mean_timeConstant_[iapv] << std::endl
+     << " Smearing parameter : " << mean_smearing_[iapv] << std::endl
+     << " Chi2 of the fit : " << mean_chi2_[iapv] << std::endl;
 }
 
 // ----------------------------------------------------------------------------
 // 
 void CalibrationAnalysis::reset() {
-  amplitude_ = sistrip::invalid_;
-  tail_ = sistrip::invalid_;
-  riseTime_ = sistrip::invalid_;
-  timeConstant_ = sistrip::invalid_;
-  smearing_ = sistrip::invalid_;
-  chi2_ = sistrip::invalid_;
-  histo_ = Histo(0,"");
+  amplitude_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  tail_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  riseTime_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  timeConstant_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  smearing_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  chi2_ = VVFloat(2,VFloat(128,sistrip::invalid_));
+  mean_amplitude_ = VFloat(2,sistrip::invalid_);
+  mean_tail_ = VFloat(2,sistrip::invalid_);
+  mean_riseTime_ = VFloat(2,sistrip::invalid_);
+  mean_timeConstant_ = VFloat(2,sistrip::invalid_);
+  mean_smearing_ = VFloat(2,sistrip::invalid_);
+  mean_chi2_ = VFloat(2,sistrip::invalid_);
+  min_amplitude_ = VFloat(2,sistrip::invalid_);
+  min_tail_ = VFloat(2,sistrip::invalid_);
+  min_riseTime_ = VFloat(2,sistrip::invalid_);
+  min_timeConstant_ = VFloat(2,sistrip::invalid_);
+  min_smearing_ = VFloat(2,sistrip::invalid_);
+  min_chi2_ = VFloat(2,sistrip::invalid_);
+  max_amplitude_ = VFloat(2,sistrip::invalid_);
+  max_tail_ = VFloat(2,sistrip::invalid_);
+  max_riseTime_ = VFloat(2,sistrip::invalid_);
+  max_timeConstant_ = VFloat(2,sistrip::invalid_);
+  max_smearing_ = VFloat(2,sistrip::invalid_);
+  max_chi2_ = VFloat(2,sistrip::invalid_);
+  spread_amplitude_ = VFloat(2,sistrip::invalid_);
+  spread_tail_ = VFloat(2,sistrip::invalid_);
+  spread_riseTime_ = VFloat(2,sistrip::invalid_);
+  spread_timeConstant_ = VFloat(2,sistrip::invalid_);
+  spread_smearing_ = VFloat(2,sistrip::invalid_);
+  spread_chi2_ = VFloat(2,sistrip::invalid_);
   deconv_fitter_->FixParameter(0,0);
-  deconv_fitter_->SetParLimits(1,-10,10);
+  deconv_fitter_->SetParLimits(1,-100,0);
   deconv_fitter_->SetParLimits(2,0,200);
   deconv_fitter_->SetParLimits(3,5,100);
   deconv_fitter_->FixParameter(3,50);
   deconv_fitter_->SetParLimits(4,0,50);
   deconv_fitter_->SetParameters(0.,-2.82,0.96,50,20);
   peak_fitter_->FixParameter(0,0);
-  peak_fitter_->SetParLimits(1,-10,10);
+  peak_fitter_->SetParLimits(1,-100,0);
   peak_fitter_->SetParLimits(2,0,200);
   peak_fitter_->SetParLimits(3,5,100);
   peak_fitter_->FixParameter(3,50);
@@ -118,13 +194,14 @@ void CalibrationAnalysis::reset() {
 void CalibrationAnalysis::extract( const std::vector<TH1*>& histos) {
   
   // Check
-  if ( histos.size() != 1 ) {
+  if ( histos.size() != 32 ) {
     edm::LogWarning(mlCommissioning_) << " Unexpected number of histograms: " << histos.size();
   }
   
   // Extract
   std::vector<TH1*>::const_iterator ihis = histos.begin();
-  for ( ; ihis != histos.end(); ihis++ ) {
+  unsigned int cnt = 0;
+  for ( ; ihis != histos.end(); ihis++,cnt++ ) {
     
     // Check pointer
     if ( !(*ihis) ) {
@@ -134,7 +211,7 @@ void CalibrationAnalysis::extract( const std::vector<TH1*>& histos) {
     
     // Check name
     SiStripHistoTitle title( (*ihis)->GetName() );
-    if ( title.runType() != sistrip::FINE_DELAY ) {
+    if ( title.runType() != sistrip::CALIBRATION && title.runType() != sistrip::CALIBRATION_DECO ) {
       edm::LogWarning(mlCommissioning_) 
 	<< " Unexpected commissioning task: "
 	<< SiStripEnumsAndStrings::runType(title.runType());
@@ -142,8 +219,8 @@ void CalibrationAnalysis::extract( const std::vector<TH1*>& histos) {
     }
     
     // Extract calibration histo
-    histo_.first = *ihis;
-    histo_.second = (*ihis)->GetName();
+    histo_[cnt].first = *ihis;
+    histo_[cnt].second = (*ihis)->GetTitle(); //CD was GetName
     
   }
   
@@ -152,56 +229,160 @@ void CalibrationAnalysis::extract( const std::vector<TH1*>& histos) {
 // ----------------------------------------------------------------------------
 // 
 void CalibrationAnalysis::analyse() { 
-  if ( !histo_.first ) {
-    edm::LogWarning(mlCommissioning_) << " NULL pointer to histogram!" ;
-    return;
-  }
+
+  float Amean[2]   = {0.,0.};
+  float Amin[2]    = {2000.,2000.};
+  float Amax[2]    = {0.,0.};
+  float Aspread[2] = {0.,0.};
+  float Tmean[2]   = {0.,0.};
+  float Tmin[2]    = {2000.,2000.};
+  float Tmax[2]    = {0.,0.};
+  float Tspread[2] = {0.,0.};
+  float Rmean[2]   = {0.,0.};
+  float Rmin[2]    = {2000.,2000.};
+  float Rmax[2]    = {0.,0.};
+  float Rspread[2] = {0.,0.};
+  float Cmean[2]   = {0.,0.};
+  float Cmin[2]    = {2000.,2000.};
+  float Cmax[2]    = {0.,0.};
+  float Cspread[2] = {0.,0.};
+  float Smean[2]   = {0.,0.};
+  float Smin[2]    = {2000.,2000.};
+  float Smax[2]    = {0.,0.};
+  float Sspread[2] = {0.,0.};
+  float Kmean[2]   = {0.,0.};
+  float Kmin[2]    = {2000000.,2000000.};
+  float Kmax[2]    = {0.,0.};
+  float Kspread[2] = {0.,0.};
   
-  // rescale the plot
-  correctDistribution(histo_.first);
-  
-  // amplitude
-  amplitude_ = histo_.first->GetMaximum();
-  
-  // tail 
-  int lastBin = deconv_ ? 80 : 65;
-  tail_ = 100*histo_.first->GetBinContent(lastBin)/histo_.first->GetMaximum();
-  
-  // rise time
-  int bin_a=0, bin_b=0, bin_c=0;
-  for(int bin = 1; bin<= histo_.first->GetNbinsX() && bin_b == 0; ++bin) {
-      if(histo_.first->GetBinContent(bin)<0.1*amplitude_) bin_a = bin;
-      if(histo_.first->GetBinContent(bin)<0.6*amplitude_) bin_c = bin;
-      if(histo_.first->GetBinContent(bin)>0.99*amplitude_) bin_b = bin;
+  for(unsigned int i=0;i<32;++i) {
+    if ( !histo_[i].first ) {
+      edm::LogWarning(mlCommissioning_) << " NULL pointer to histogram!" ;
+      return;
     }
-  histo_.first->Fit("gaus","0Q","",histo_.first->GetBinCenter(bin_b)-25,histo_.first->GetBinCenter(bin_b)+25);
-  float time_max = ((TF1*)(TVirtualFitter::GetFitter()->GetUserFunc()))->GetMaximumX();
-  histo_.first->Fit("gaus","0Q","",0,histo_.first->GetBinCenter(bin_c));
-  TF1* tmp_f = (TF1*)(TVirtualFitter::GetFitter()->GetUserFunc());
-  float time_start = tmp_f->GetParameter(1)-3*tmp_f->GetParameter(2);
-  riseTime_ = time_max - time_start;
-
-  // perform the fit for the next quantities
-  TF1* fit = fitPulse(histo_.first);
+     
+    // determine which APV and strip is being looked at.
+    std::string title = histo_[i].first->GetName();
+    int apv = 0;
+    int strip = 0;
+    if(title.find("STRIP")!=std::string::npos && title.find("Apv")!=std::string::npos) {
+      strip = atoi(title.c_str()+title.find("STRIP")+6);
+      apv = (atoi(title.c_str()+title.find("Apv")+3))%2;
+    } else {
+      strip = (atoi(title.c_str()+title.find_last_of("_")+1))%16;
+      apv = (atoi(title.c_str()+title.find_last_of("_")+1))/16;
+      if(title.find("Apv")!=std::string::npos) {
+        apv = (atoi(title.c_str()+title.find("Apv")+3))%2;
+	strip = strip*8 + calchan_;
+      } else {
+        edm::LogWarning(mlCommissioning_) << " Malformed histogram title! Strip/APV not retreived: " 
+                                          << title;
+      }
+    }
+    
+    LogDebug(mlCommissioning_) << "start the calibration analysis for APV " << apv << " strip " << strip;
+    // rescale the plot
+    correctDistribution(histo_[i].first);
+    
+    // amplitude
+    amplitude_[apv][strip] = histo_[i].first->GetMaximum();
+    
+    // tail 
+    int lastBin = deconv_ ? 80 : 64;
+    tail_[apv][strip] = 100*histo_[i].first->GetBinContent(lastBin)/histo_[i].first->GetMaximum();
+    
+    // rise time
+    int bin_a=0, bin_b=0, bin_c=0;
+    for(int bin = 1; bin<= histo_[i].first->GetNbinsX() && bin_b == 0; ++bin) {
+      if(histo_[i].first->GetBinContent(bin)<0.1*amplitude_[apv][strip]) bin_a = bin;
+      if(histo_[i].first->GetBinContent(bin)<0.6*amplitude_[apv][strip]) bin_c = bin;
+      if(histo_[i].first->GetBinContent(bin)>0.99*amplitude_[apv][strip]) bin_b = bin;
+    }
+    histo_[i].first->Fit("gaus","0Q","",histo_[i].first->GetBinCenter(bin_b)-25,histo_[i].first->GetBinCenter(bin_b)+25);
+    float time_max = ((TF1*)(TVirtualFitter::GetFitter()->GetUserFunc()))->GetMaximumX();
+    histo_[i].first->Fit("gaus","0Q","",0,histo_[i].first->GetBinCenter(bin_c));
+    TF1* tmp_f = (TF1*)(TVirtualFitter::GetFitter()->GetUserFunc());
+    float time_start = tmp_f->GetParameter(1)-3*tmp_f->GetParameter(2);
+    riseTime_[apv][strip] = time_max - time_start;
   
-  // time constant
-  timeConstant_ = fit->GetParameter(3);
-
-  // smearing
-  smearing_ = fit->GetParameter(4);
-
-  // chi2
-  chi2_ = fit->GetChisquare();
+    // perform the fit for the next quantities
+    TF1* fit = fitPulse(histo_[i].first);
+    
+    // time constant
+    timeConstant_[apv][strip] = fit->GetParameter(3);
   
+    // smearing
+    smearing_[apv][strip] = fit->GetParameter(4);
+  
+    // chi2
+    chi2_[apv][strip] = fit->GetChisquare();
+    
+    LogDebug(mlCommissioning_) << "Results: " << chi2_[apv][strip] << " " << smearing_[apv][strip] << " " 
+                               << timeConstant_[apv][strip] << " " << riseTime_[apv][strip] << " " 
+  			       << tail_[apv][strip] << " " << amplitude_[apv][strip];
+			       
+    //compute mean, max, min, spread
+    Amean[apv] += amplitude_[apv][strip]/16.;
+    Amin[apv] = Amin[apv]<amplitude_[apv][strip] ? Amin[apv] : amplitude_[apv][strip];
+    Amax[apv] = Amax[apv]>amplitude_[apv][strip] ? Amax[apv] : amplitude_[apv][strip];
+    Aspread[apv] += amplitude_[apv][strip]*amplitude_[apv][strip]/16.;
+    Tmean[apv] += tail_[apv][strip]/16.;
+    Tmin[apv] = Tmin[apv]<tail_[apv][strip] ? Tmin[apv] : tail_[apv][strip];
+    Tmax[apv] = Tmax[apv]>tail_[apv][strip] ? Tmax[apv] : tail_[apv][strip];
+    Tspread[apv] += tail_[apv][strip]*tail_[apv][strip]/16.;
+    Rmean[apv] += riseTime_[apv][strip]/16.;
+    Rmin[apv] = Rmin[apv]<riseTime_[apv][strip] ? Rmin[apv] : riseTime_[apv][strip];
+    Rmax[apv] = Rmax[apv]>riseTime_[apv][strip] ? Rmax[apv] : riseTime_[apv][strip];
+    Rspread[apv] += riseTime_[apv][strip]*riseTime_[apv][strip]/16.;
+    Cmean[apv] += timeConstant_[apv][strip]/16.;
+    Cmin[apv] = Cmin[apv]<timeConstant_[apv][strip] ? Cmin[apv] : timeConstant_[apv][strip];
+    Cmax[apv] = Cmax[apv]>timeConstant_[apv][strip] ? Cmax[apv] : timeConstant_[apv][strip];
+    Cspread[apv] += timeConstant_[apv][strip]*timeConstant_[apv][strip]/16.;
+    Smean[apv] += smearing_[apv][strip]/16.;
+    Smin[apv] = Smin[apv]<smearing_[apv][strip] ? Smin[apv] : smearing_[apv][strip];
+    Smax[apv] = Smax[apv]>smearing_[apv][strip] ? Smax[apv] : smearing_[apv][strip];
+    Sspread[apv] += smearing_[apv][strip]*smearing_[apv][strip]/16.;
+    Kmean[apv] += chi2_[apv][strip]/16.;
+    Kmin[apv] = Kmin[apv]<chi2_[apv][strip] ? Kmin[apv] : chi2_[apv][strip];
+    Kmax[apv] = Kmax[apv]>chi2_[apv][strip] ? Kmax[apv] : chi2_[apv][strip];
+    Kspread[apv] += chi2_[apv][strip]*chi2_[apv][strip]/16.;
+  }
+				
+  // fill the mean, max, min, spread, ... histograms.
+  for(int i=0;i<2;++i) {
+    mean_amplitude_[i] = Amean[i];
+    mean_tail_[i] = Tmean[i];
+    mean_riseTime_[i] = Rmean[i];
+    mean_timeConstant_[i] = Cmean[i];
+    mean_smearing_[i] = Smean[i];
+    mean_chi2_[i] = Kmean[i];
+    min_amplitude_[i] = Amin[i];
+    min_tail_[i] = Tmin[i];
+    min_riseTime_[i] = Rmin[i];
+    min_timeConstant_[i] = Cmin[i];
+    min_smearing_[i] = Smin[i];
+    min_chi2_[i] = Kmin[i];
+    max_amplitude_[i] = Amax[i];
+    max_tail_[i] = Tmax[i];
+    max_riseTime_[i] = Rmax[i];
+    max_timeConstant_[i] = Cmax[i];
+    max_smearing_[i] = Smax[i];
+    max_chi2_[i] = Kmax[i];
+    spread_amplitude_[i] = sqrt(fabs(Aspread[i]-Amean[i]*Amean[i]));
+    spread_tail_[i] = sqrt(fabs(Tspread[i]-Tmean[i]*Tmean[i]));
+    spread_riseTime_[i] = sqrt(fabs(Rspread[i]-Rmean[i]*Rmean[i]));
+    spread_timeConstant_[i] = sqrt(fabs(Cspread[i]-Cmean[i]*Cmean[i]));
+    spread_smearing_[i] = sqrt(fabs(Sspread[i]-Smean[i]*Smean[i]));
+    spread_chi2_[i] = sqrt(fabs(Kspread[i]-Kmean[i]*Kmean[i]));
+  }
 }
 
 // ----------------------------------------------------------------------------
 //
 void CalibrationAnalysis::correctDistribution(TH1* histo) const
 {
+  // return the curve
   histo->Scale(-1);
-  histo->GetXaxis()->SetLimits(histo->GetXaxis()->GetXmin()*3.125,
-                               histo->GetXaxis()->GetXmax()*3.125);		   
 }
 
 // ----------------------------------------------------------------------------
@@ -210,10 +391,10 @@ TF1* CalibrationAnalysis::fitPulse(TH1* histo)
 {
   if(!histo) return 0;
   if (deconv_) {
-    histo->Fit(deconv_fitter_,"0Q");
+    histo->Fit(deconv_fitter_,"Q");
     return deconv_fitter_; 
   } else {
-    histo->Fit(peak_fitter_,"0Q");
+    histo->Fit(peak_fitter_,"Q");
     return peak_fitter_; 
   } 
 }
