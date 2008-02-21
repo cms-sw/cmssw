356fc1d205c70c443fe93a677a5ff43bf9472598
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2008-02-21 16:11:57 +0000

    Cleanup for 2_0_0

--- Validation/CaloTowers/src/CaloTowersValidation.cc
+++ Validation/CaloTowers/src/CaloTowersValidation.cc
@@ -98,13 +98,8 @@ CaloTowersValidation::~CaloTowersValidation() {
   
 }
 
-void CaloTowersValidation::endJob() {
-  
-  std::cout << " outputFile_.size() =  " << outputFile_.size() << std::endl;
-  std::cout << " dbe_ = " << dbe_ << std::endl; 
-  if ( outputFile_.size() != 0 && dbe_ ) dbe_->save(outputFile_);
-  
-}
+void CaloTowersValidation::endJob() { }
+
 void CaloTowersValidation::beginJob(const edm::EventSetup& c){
 
 }
