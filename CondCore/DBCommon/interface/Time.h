d008295e7d76f78f852f07375f394b5e384dec47
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-02-24 14:36:15 +0000

    add timetypelist

--- CondCore/DBCommon/interface/Time.h
+++ CondCore/DBCommon/interface/Time.h
@@ -5,6 +5,8 @@ namespace cond{
   //typedef unsigned int Time_t;
   typedef unsigned long long Time_t;
   typedef std::pair<Time_t, Time_t> ValidityInterval;
-  typedef enum { runnumber,timestamp } TimeType;
+  enum TimeType{ runnumber=0,timestamp,lumiid };
+  const unsigned int TIMETYPE_LIST_MAX=3;
+  cond::TimeType TimeTypeList[TIMETYPE_LIST_MAX]={runnumber,timestamp,lumiid};
 }
 #endif
