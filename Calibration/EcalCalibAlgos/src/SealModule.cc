24ca4784d90654eb70c639075c58c17520645c79
Martina Malberti <sha1-2b004fb57608a448d74be744ecf92d42b9b3ffaa@cern.ch>
2008-02-25 18:08:37 +0000

    first commit

--- Calibration/EcalCalibAlgos/src/SealModule.cc
+++ Calibration/EcalCalibAlgos/src/SealModule.cc
@@ -1,17 +1,21 @@
 // #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "Calibration/EcalCalibAlgos/interface/EcalEleCalibLooper.h"
+#include "Calibration/EcalCalibAlgos/interface/InvRingCalib.h"
 #include "Calibration/EcalCalibAlgos/interface/miscalibExample.h"
 #include "Calibration/EcalCalibAlgos/interface/ElectronCalibration.h"
 #include "Calibration/EcalCalibAlgos/interface/ZeeCalibration.h"
-#include "Calibration/EcalCalibAlgos/interface/ElectronRecalibSuperClusterAssociator.h"
+//#include "Calibration/EcalCalibAlgos/interface/ElectronRecalibSuperClusterAssociator.h"
 #include "Calibration/EcalCalibAlgos/interface/PhiSymmetryCalibration.h"
 #include "Calibration/EcalCalibAlgos/interface/Pi0FixedMassWindowCalibration.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(miscalibExample);
-DEFINE_ANOTHER_FWK_MODULE(ElectronRecalibSuperClusterAssociator);
+//DEFINE_ANOTHER_FWK_MODULE(ElectronRecalibSuperClusterAssociator);
 DEFINE_ANOTHER_FWK_MODULE(ElectronCalibration);
 DEFINE_ANOTHER_FWK_MODULE(PhiSymmetryCalibration);
 DEFINE_ANOTHER_FWK_LOOPER(Pi0FixedMassWindowCalibration);
 DEFINE_ANOTHER_FWK_LOOPER(ZeeCalibration);
+DEFINE_ANOTHER_FWK_LOOPER(EcalEleCalibLooper);	
+DEFINE_ANOTHER_FWK_LOOPER(InvRingCalib);
 
