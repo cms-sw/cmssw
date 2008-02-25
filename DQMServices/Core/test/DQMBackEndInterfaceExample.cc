a69b58e924c33d69308760a3b054fa8e24d4d359
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-02-25 16:44:31 +0000

    Use new pattern matching when wildcards are used. Fix a likely typo in the last check ('*sto?' should not match 'histo'). Fix the array order in which monitor elements are expected; this has changed from the earlier versions.

--- DQMServices/Core/test/DQMBackEndInterfaceExample.cc
+++ DQMServices/Core/test/DQMBackEndInterfaceExample.cc
@@ -185,33 +185,34 @@ void DQMBackEndInterfaceExample::integrityChecks()
 
   // contents of "C1/C2" (including subdirectories)
   contents.clear();
-  contents.push_back(f1); contents.push_back(h5);
-  contents.push_back(h6); contents.push_back(i1);
   contents.push_back(h1); contents.push_back(h2);
   contents.push_back(h7); contents.push_back(s1);
   contents.push_back(h64); contents.push_back(h8);
+  contents.push_back(f1); contents.push_back(h5);
+  contents.push_back(h6); contents.push_back(i1);
   dbe_ret = dbe->getAllContents("C1/C2");
   assert(dbe_ret == contents); 
   dbe_ret = dbe->getAllContents("C1/C2/"); // w/ or w/o a "slash" at the end
   assert(dbe_ret == contents);
 
-
   // Third set of checks: make sure we can use simple pathnames with wildcards
 
   // contents of "C1/C2" (including subdirectories)
-  dbe_ret = dbe->getAllContents("C1/C2/*");
+  dbe_ret = dbe->getMatchingContents("C1/C2/*");
   assert(dbe_ret == contents);
 
   // contents of "C1/C2": only histograms (including subdirectories)
   contents.clear();
-  contents.push_back(h5);
-  contents.push_back(h6);
   contents.push_back(h1); contents.push_back(h2);
   contents.push_back(h7);
   contents.push_back(h64); contents.push_back(h8);
-  dbe_ret = dbe->getAllContents("C1/C2/*histo*");
+  contents.push_back(h5);
+  contents.push_back(h6);
+  dbe_ret = dbe->getMatchingContents("C1/C2/*histo*");
   assert(dbe_ret == contents);
-  dbe_ret = dbe->getAllContents("C1/C2/*sto?");
+
+  contents.erase(contents.begin());
+  dbe_ret = dbe->getMatchingContents("C1/C2/*sto?");
   assert(dbe_ret == contents);
 }
 
