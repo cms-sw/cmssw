bcf50d563394c2c4b148f81439e803b4ef87ed68
Jorge Fernandez De Troconiz <sha1-75dae295003aa1cfa4337639ae3380ba6ad18aa2@cern.ch>
2008-02-25 15:37:30 +0000

    DTTF upgrade

--- L1TriggerConfig/DTTrackFinder/src/DTTrackFinderConfig.cc
+++ L1TriggerConfig/DTTrackFinder/src/DTTrackFinderConfig.cc
@@ -5,8 +5,8 @@
 //   L1 DT Track Finder ESProducer
 //
 //
-//   $Date: 2007/02/27 11:44:00 $
-//   $Revision: 1.5 $
+//   $Date: 2007/03/30 08:03:00 $
+//   $Revision: 1.1 $
 //
 //   Author :
 //   J. Troconiz              UAM Madrid
@@ -15,12 +15,12 @@
 
 #include "L1TriggerConfig/DTTrackFinder/interface/DTTrackFinderConfig.h"
 
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 DTTrackFinderConfig::DTTrackFinderConfig(const edm::ParameterSet& pset) {
-
-  string lutdir_ = pset.getUntrackedParameter<string>("lutdir","L1TriggerConfig/DTTrackFinder/parameters/");
-  setenv("DTTF_DATA_PATH",lutdir_.c_str(),1); 
 
   setWhatProduced(this, &DTTrackFinderConfig::produceL1MuDTExtLut);
   setWhatProduced(this, &DTTrackFinderConfig::produceL1MuDTPhiLut);
@@ -38,12 +38,20 @@ auto_ptr<L1MuDTExtLut> DTTrackFinderConfig::produceL1MuDTExtLut(const L1MuDTExtL
 
    auto_ptr<L1MuDTExtLut> extlut = auto_ptr<L1MuDTExtLut>( new L1MuDTExtLut() );
 
+   if ( extlut->load() != 0 ) {
+     cout << "Can not open files to load  extrapolation look-up tables for DTTrackFinder!" << endl;
+   }
+
    return extlut;
 }
 
 auto_ptr<L1MuDTPhiLut> DTTrackFinderConfig::produceL1MuDTPhiLut(const L1MuDTPhiLutRcd& iRecord) {
 
    auto_ptr<L1MuDTPhiLut> philut = auto_ptr<L1MuDTPhiLut>( new L1MuDTPhiLut() );
+
+   if ( philut->load() != 0 ) {
+     cout << "Can not open files to load phi-assignment look-up tables for DTTrackFinder!" << endl;
+   }
 
    return philut;
 }
@@ -52,6 +60,10 @@ auto_ptr<L1MuDTPtaLut> DTTrackFinderConfig::produceL1MuDTPtaLut(const L1MuDTPtaL
 
    auto_ptr<L1MuDTPtaLut> ptalut = auto_ptr<L1MuDTPtaLut>( new L1MuDTPtaLut() );
 
+   if ( ptalut->load() != 0 ) {
+     cout << "Can not open files to load pt-assignment look-up tables for DTTrackFinder!" << endl;
+   }
+
    return ptalut;
 }
 
@@ -59,12 +71,20 @@ auto_ptr<L1MuDTEtaPatternLut> DTTrackFinderConfig::produceL1MuDTEtaPatternLut(co
 
    auto_ptr<L1MuDTEtaPatternLut> etalut = auto_ptr<L1MuDTEtaPatternLut>( new L1MuDTEtaPatternLut() );
 
+   if ( etalut->load() != 0 ) {
+     cout << "Can not open files to load eta track finder look-up tables for DTTrackFinder!" << endl;
+   }
+
    return etalut;
 }
 
 auto_ptr<L1MuDTQualPatternLut> DTTrackFinderConfig::produceL1MuDTQualPatternLut(const L1MuDTQualPatternLutRcd& iRecord) {
 
    auto_ptr<L1MuDTQualPatternLut> qualut = auto_ptr<L1MuDTQualPatternLut>( new L1MuDTQualPatternLut() );
+
+   if ( qualut->load() != 0 ) {
+     cout << "Can not open files to load eta matching look-up tables for DTTrackFinder!" << endl;
+   }
 
    return qualut;
 }
