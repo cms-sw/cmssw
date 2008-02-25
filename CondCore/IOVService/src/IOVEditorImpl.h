900a2eb511f68d1d56394c5670dd8252acacfb07
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-02-25 15:33:01 +0000

    new IOV

--- CondCore/IOVService/src/IOVEditorImpl.h
+++ CondCore/IOVService/src/IOVEditorImpl.h
@@ -7,10 +7,12 @@ namespace cond{
   class IOV;
   class IOVEditorImpl : virtual public cond::IOVEditor{
   public:
+    // constructor from existing iov
     IOVEditorImpl( cond::PoolTransaction& pooldb,
 		   const std::string& token,
 		   cond::Time_t globalSince, 
-		   cond::Time_t globalTime);
+		   cond::Time_t globalTill);
+
     /// Destructor
     virtual ~IOVEditorImpl();
 
@@ -22,24 +24,34 @@ namespace cond{
     virtual unsigned int insert( cond::Time_t tillTime,
 				 const std::string& payloadToken
 				 );
+
     void bulkInsert( std::vector< std::pair<cond::Time_t,std::string> >& values );
+
     virtual void updateClosure( cond::Time_t newtillTime );
+
     /// Append a payload with known since time. The previous last payload's till time will be adjusted to the new payload since time. Returns the payload index in the iov sequence
     virtual unsigned int append(  cond::Time_t sinceTime,
 				  const std::string& payloadToken
 				  );
+
     virtual void deleteEntries( bool withPayload=false );
+
     virtual void import( const std::string& sourceIOVtoken );
+
     std::string token() const {
       return m_token;
     }
+
+    cond::Time_t firstSince() const;
+    
+    cond::TimeType timetype() const;
+
   private:
     void init();
     cond::PoolTransaction* m_pooldb;
     std::string m_token;
     cond::Time_t m_globalSince;
     cond::Time_t m_globalTill;
-    cond::TimeType m_timetype;
     bool m_isActive;
     cond::TypedRef<cond::IOV> m_iov;
   };
