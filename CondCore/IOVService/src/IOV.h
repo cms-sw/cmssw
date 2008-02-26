f0f5baf6969d18b031d0963cee35e5b4d05b9d46
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-02-26 15:51:44 +0000

    use vector

--- CondCore/IOVService/src/IOV.h
+++ CondCore/IOVService/src/IOV.h
@@ -1,18 +1,45 @@
 #ifndef IOVService_IOV_h
 #define IOVService_IOV_h
 #include "CondCore/DBCommon/interface/Time.h"
-#include <map>
+#include <vector>
 #include <string>
+#include <algoritm>
+#include <boost/bind.hpp>
+
 namespace cond{
   class IOV {
   public:
-    typedef IOV::Container Container;
+    typedef std::pair<cond::Time_t, std::string> Item;
+    typedef std::vector<Item> Container;
     typedef Container::iterator iterator;
     typedef Container::const_iterator const_iterator;
+
     IOV(){}
+    IOV(int type, cond::Time_t since) :
+      timetype(type), firstsince(since){}
+    
     virtual ~IOV(){}
+
+    iterator find(cond::Time_t time) {
+      return std::lower_bound(iov.begin(),iov.end(),
+			      boost::bind(std::less<float>(),
+					  boost::bind(&Item::first,_1),
+					  boost::bind(&Item::first,_2)
+					  )
+			      );
+    }
+
+    const_iterator find(cond::Time_t time) const {
+      return std::lower_bound(iov.begin(),iov.end(),
+			      boost::bind(std::less<float>(),
+					  boost::bind(&Item::first,_1),
+					  boost::bind(&Item::first,_2)
+					  )
+			      );
+    }
+
     //std::map<unsigned long long,std::string> iov;
-    IOV::Container iov;
+    Container iov;
     int timetype;
     cond::Time_t firstsince;
   };
