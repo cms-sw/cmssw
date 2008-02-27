21fc1952d22912ac78e720439b538f93a7075a3e
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2008-02-27 21:49:23 +0000

    modify to DQMStore

--- Validation/GlobalHits/python/globalhits_analyze_cff.py
+++ Validation/GlobalHits/python/globalhits_analyze_cff.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from Validation.GlobalHits.globalhits_analyze_cfi import *
 
-DaqMonitorROOTBackEnd = cms.Service("DaqMonitorROOTBackEnd")
+DQMStore = cms.Service("DQMStore")
