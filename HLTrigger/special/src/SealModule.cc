a8964d53ab317543899e02788a3b455f9c06daff
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2008-02-27 14:53:30 +0000

    added HLTCSCRing2or3Filter to select large-radius beam-halo, regardless of overlap hits

--- HLTrigger/special/src/SealModule.cc
+++ HLTrigger/special/src/SealModule.cc
@@ -8,6 +8,7 @@
 #include "HLTrigger/special/interface/HLTHcalPhiSymFilter.h"
 #include "HLTrigger/special/interface/HLTPi0RecHitsFilter.h"
 #include "HLTrigger/special/interface/HLTCSCOverlapFilter.h"
+#include "HLTrigger/special/interface/HLTCSCRing2or3Filter.h"
 
 DEFINE_FWK_MODULE(HLTPixlMBFilt);
 DEFINE_FWK_MODULE(HLTPixlMBForAlignmentFilter);
@@ -17,3 +18,4 @@ DEFINE_FWK_MODULE(HLTEcalPhiSymFilter);
 DEFINE_FWK_MODULE(HLTHcalPhiSymFilter);
 DEFINE_FWK_MODULE(HLTPi0RecHitsFilter);
 DEFINE_FWK_MODULE(HLTCSCOverlapFilter);
+DEFINE_FWK_MODULE(HLTCSCRing2or3Filter);
