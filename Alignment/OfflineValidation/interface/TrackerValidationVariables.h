c2cb0076aa0c2bec475c5e440a153e26e7e76e40
Erik Butz <sha1-53bc992219043be71927609d789fbc8f1ac8e7d6@cern.ch>
2008-02-27 17:48:50 +0000

    TrackerValidationVariables

---
+++ Alignment/OfflineValidation/interface/TrackerValidationVariables.h
@@ -0,0 +1,78 @@
+#ifndef TrackerTrackerValidationVariables_h
+#define TrackerTrackerValidationVariables_h
+// system include files
+#include <memory>
+#include <vector>
+//#include <TROOT.h>
+// user include files
+#include "CLHEP/Vector/RotationInterfaces.h" 
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/GeometryCommonDetAlgo/interface/MeasurementVector.h"
+#include "AnalysisDataFormats/TrackInfo/interface/TrackInfo.h"
+#include "AnalysisDataFormats/TrackInfo/interface/TrackInfoTrackAssociation.h"
+#include "RecoTracker/TrackProducer/interface/TrackingRecHitLessFromGlobalPosition.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateTransform.h"
+#include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
+#include "TrackingTools/PatternTools/interface/Trajectory.h"
+#include "TrackingTools/PatternTools/interface/TrajectoryMeasurement.h"
+#include "TrackingTools/TrackFitters/interface/TrajectoryStateCombiner.h"
+#include "TrackingTools/TransientTrackingRecHit/interface/TransientTrackingRecHitBuilder.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/Records/interface/TransientRecHitRecord.h"
+#include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"  
+#include "CalibFormats/SiStripObjects/interface/SiStripDetCabling.h"
+#include "CalibTracker/Records/interface/SiStripDetCablingRcd.h"
+#include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentParameterStore.h"
+//using namespace edm;
+//using namespace std;
+const double fBfield=4.06;
+
+class TrackerValidationVariables  {
+ public:  
+  struct AVHitStruct{
+    AVHitStruct() : resX(-999.), resY(-999.), resErrX(-999.), resErrY(-999.), phi(-999.), eta(-999.), rawDetId(0) {}
+    float resX;
+    float resY;
+    float resErrX;
+    float resErrY;
+    float phi;
+    float eta;
+    uint32_t rawDetId;
+  };
+  struct AVTrackStruct{
+    AVTrackStruct() : pt(0.), px(0.), py(0.), pz(0.), eta(0.), phi(0.), kappa(0.),
+	 chi2(0.), normchi2(0), d0(-999.), dz(-999.), charge(-999) {};
+    float pt;
+    float px;
+    float py;
+    float pz;
+    float eta;
+    float phi;
+    float kappa;
+    float chi2;
+    float normchi2;
+    float d0;
+    float dz;
+    int charge;
+  };
+  TrackerValidationVariables();
+  TrackerValidationVariables(const edm::EventSetup&, const edm::ParameterSet&);
+  ~TrackerValidationVariables();
+  void fillHitQuantities(const edm::Event&, std::vector<AVHitStruct> & v_avhitout );
+  void fillTrackQuantities(const edm::Event&, std::vector<AVTrackStruct> & v_avtrackout );
+ private:  
+  const edm::ParameterSet conf_;
+  edm::ESHandle<TrackerGeometry> tkgeom;
+  //edm::ESHandle<SiStripDetCabling> SiStripDetCabling_;
+};
+#endif
