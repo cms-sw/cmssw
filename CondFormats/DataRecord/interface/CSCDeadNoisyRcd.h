15fe466297ac2f0352f398aaaa425a4a8f2b9465
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2008-02-28 12:24:43 +0000

    added record for dead/noisy CSC object

---
+++ CondFormats/DataRecord/interface/CSCDeadNoisyRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCDEADNOISYRCD_H
+#define CSCDEADNOISYRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCDeadNoisyRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCDeadNoisyRcd> {};
+#endif
