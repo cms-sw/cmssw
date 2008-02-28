05fcfa627a9246f023a3181e95b6b9bbad89c8be
Alexandr Zhokin <sha1-3934d39e6468375f9412692c75d9c3501c9552f4@cern.ch>
2008-02-28 10:47:03 +0000

    some changes

--- SimTransport/HectorProducer/interface/Hector.h
+++ SimTransport/HectorProducer/interface/Hector.h
@@ -1,9 +1,26 @@
 #ifndef SimTransport_Hector_h
 #define SimTransport_Hector_h
 
+// user include files
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+/*
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+*/
 // HepMC headers
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+
 #include "HepMC/GenEvent.h"
+#include "HepMC/GenVertex.h"
 #include "HepMC/GenParticle.h"
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 
 // SimpleConfigurable replacement
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -15,13 +32,16 @@
 #include <string>
 
 class Hector {
-
-public:
-  Hector(const edm::ParameterSet & ps);
+  
+ public:
+  //  Hector(const edm::ParameterSet & ps);
+  Hector(const edm::ParameterSet & ps, bool verbosity, bool FP420Transport,bool ZDCTransport);
   //  Hector();
   virtual ~Hector();
-
-  /*!Clears all data, prepares Hector for a next event*/
+  
+  /*!Clears ApertureFlags, prepares Hector for a next event*/
+  void clearApertureFlags();
+  /*!Clears BeamParticle, prepares Hector for a next Aperture check or/and a next event*/
   void clear();
   /*!Adds the stable protons from the event \a ev to a beamline*/
   void add( const HepMC::GenEvent * ev );
@@ -29,71 +49,77 @@ public:
   void filterFP420();
   /*!propagate the particles through a beamline to ZDC*/
   void filterZDC();
-
+  /*!propagate the particles through a beamline to ZDC*/
+  void filterD1();
+  
   int getDirect( unsigned int part_n ) const;
 
   /*!Prints properties of all particles in a beamline*/
     void print() const;
   /*!Return vector of the particle lines (HepMC::GenParticle::barcode()) in a beamline*/
   // std::vector<unsigned int> part_list() const;
-
-  HepMC::GenEvent * addPartToHepMC( HepMC::GenEvent * event );
-
-  /*  
-private:
-  //  edm::ParameterSet m_pBeamLine;
-  */
-private:
-  bool m_verbosity;
-
-
-  // Defaults
-  double lengthfp420 ;
-  double lengthzdc ;
-  double lengthd1 ;
-
-  float etacut;
-  bool m_smearAng;
-
-  float m_rpp420_f;
-  float m_rpp420_b;
-  float m_rppzdc;
-  float m_rppd1;
-  
-  double m_IPx;
-  double m_IPy;
-  double m_IPz;
-  double m_IPt;
-
-  double Rpipe;// inner radius of beam pipe mm
-  double x0_zdc;// X0-coord. center of beam pipe at z=140m 
-  double y0_zdc;// Y0-coord. center of beam pipe at z=140m 
-
-  // Hector
-  H_BeamLine * m_beamlineFP4201;
-  H_BeamLine * m_beamlineFP4202;
-  //
-
+    
+    //    bool isCharged(const HepMC::GenParticle * p);
+    
+    HepMC::GenEvent * addPartToHepMC( HepMC::GenEvent * event );
+    
+    /*  
+	private:
+	//  edm::ParameterSet m_pBeamLine;
+	*/
+ private:
+    
+    
+    // Defaults
+    double lengthfp420 ;
+    double lengthzdc ;
+    double lengthd1 ;
+    
+    float etacut;
+    bool m_smearAng;
+    
+    float m_rpp420_f;
+    float m_rpp420_b;
+    float m_rppzdc;
+    float m_rppd1;
+    
+    
+    edm::ESHandle < ParticleDataTable > pdt;
+    
+    // Hector
+    H_BeamLine * m_beamlineFP4201;
+    H_BeamLine * m_beamlineFP4202;
+    H_BeamLine * m_beamlineZDC1;
+    H_BeamLine * m_beamlineZDC2;
+    H_BeamLine * m_beamlineD11;
+    H_BeamLine * m_beamlineD12;
+    //
+    
     H_RecRPObject * m_rp420_f;
     H_RecRPObject * m_rp420_b;
-
-  std::map<unsigned int, H_BeamParticle*> m_beamPart;
-  std::map<unsigned int, int> m_direct;
-  std::map<unsigned int, bool> m_isStoppedfp420;
-  std::map<unsigned int, bool> m_isStoppedzdc;
-  std::map<unsigned int, bool> m_isStoppedd1;
-  std::map<unsigned int, double> m_xAtTrPoint;
-  std::map<unsigned int, double> m_yAtTrPoint;
-  std::map<unsigned int, double> m_TxAtTrPoint;
-  std::map<unsigned int, double> m_TyAtTrPoint;
-  std::map<unsigned int, double> m_eAtTrPoint;
-
-  //std::map<unsigned int, double> m_eta;
-  //std::map<unsigned int, int> m_pdg;
-  //std::map<unsigned int, double> m_pz;
-
-
-  string beam1filename;
-  string beam2filename;
+    
+    std::map<unsigned int, H_BeamParticle*> m_beamPart;
+    std::map<unsigned int, int> m_direct;
+    std::map<unsigned int, bool> m_isStoppedfp420;
+    std::map<unsigned int, bool> m_isStoppedzdc;
+    std::map<unsigned int, bool> m_isStoppedd1;
+    std::map<unsigned int, double> m_xAtTrPoint;
+    std::map<unsigned int, double> m_yAtTrPoint;
+    std::map<unsigned int, double> m_TxAtTrPoint;
+    std::map<unsigned int, double> m_TyAtTrPoint;
+    std::map<unsigned int, double> m_eAtTrPoint;
+    
+    std::map<unsigned int, double> m_eta;
+    std::map<unsigned int, int> m_pdg;
+    std::map<unsigned int, double> m_pz;
+    std::map<unsigned int, bool> m_isCharged;
+    
+    string beam1filename;
+    string beam2filename;
+    
+    bool m_verbosity;
+    bool m_FP420Transport;
+    bool m_ZDCTransport;
+    
 };
 #endif
