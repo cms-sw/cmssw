225748b0c0fadc0fe8496daf01a66cfd5a9ce6b4
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-03-02 13:13:55 +0000

    Add 2 additional physics lists QGSP_BIC and QBBC

---
+++ SimG4Core/PhysicsLists/plugins/QBBCCMS.cc
@@ -0,0 +1,69 @@
+#include "QBBCCMS.hh"
+#include "SimG4Core/PhysicsLists/interface/CMSEmStandardPhysics.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "G4DecayPhysics.hh"
+#include "G4EmExtraPhysics.hh"
+#include "G4QStoppingPhysics.hh"
+#include "G4LHEPStoppingPhysics.hh" 
+
+#include "G4DataQuestionaire.hh"
+#include "G4HadronInelasticQBBC.hh"
+#include "G4HadronInelasticQLHEP.hh"
+#include "G4HadronElasticPhysics.hh"
+#include "G4HadronDElasticPhysics.hh"
+#include "G4HadronHElasticPhysics.hh"
+#include "G4IonBinaryCascadePhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4NeutronTrackingCut.hh"
+
+QBBCCMS::QBBCCMS(G4LogicalVolumeToDDLogicalPartMap& map, 
+		 const edm::ParameterSet & p) : PhysicsList(map, p) {
+
+  G4DataQuestionaire it(photon);
+  
+  int  ver     = p.getUntrackedParameter<int>("Verbosity",0);
+  bool emPhys  = p.getUntrackedParameter<bool>("EMPhysics",true);
+  bool hadPhys = p.getUntrackedParameter<bool>("HadPhysics",true);
+  bool ftf     = p.getUntrackedParameter<bool>("FlagFTF",false);
+  bool bert    = p.getUntrackedParameter<bool>("FlagBERT",false);
+  bool chips   = p.getUntrackedParameter<bool>("FlagCHIPS",false);
+  bool hp      = p.getUntrackedParameter<bool>("FlagHP",false);
+  bool glauber = p.getUntrackedParameter<bool>("FlagGlauber",false);
+  edm::LogInfo("PhysicsList") << "You are using the simulation engine: "
+			      << "QBBC 3.1 with Flags for EM Physics "
+			      << emPhys << " and for Hadronic Physics "
+			      << hadPhys << " Flags for FTF " << ftf
+			      << " BERT " << bert << " CHIPS " << chips
+			      << " HP " << hp << " Glauber " << glauber <<"\n";
+
+  if (emPhys) {
+    // EM Physics
+    RegisterPhysics( new CMSEmStandardPhysics("standard EM",ver));
+
+    // Synchroton Radiation & GN Physics
+    RegisterPhysics(new G4EmExtraPhysics("extra EM"));
+  }
+
+  // Decays
+  RegisterPhysics(new G4DecayPhysics("decay",ver));
+
+  if (hadPhys) {
+    // Hadron Elastic scattering
+    RegisterPhysics(new G4HadronElasticPhysics("hElastic",ver,false,true));
+
+    // Hadron Physics
+    RegisterPhysics( new G4HadronInelasticQBBC("inelastic", ver, ftf,
+					       bert, chips, hp, glauber));
+
+    // Stopping Physics
+    RegisterPhysics(new G4QStoppingPhysics("stopping",ver));
+
+    // Ion Physics
+    RegisterPhysics(new G4IonBinaryCascadePhysics("ionBIC"));
+
+    // Neutron tracking cut
+    RegisterPhysics( new G4NeutronTrackingCut("Neutron tracking cut", ver));
+  }
+}
+
---
+++ SimG4Core/PhysicsLists/plugins/QGSPCMS_BIC.hh
@@ -0,0 +1,16 @@
+#ifndef SimG4Core_PhysicsLists_QGSPCMS_BIC_H
+#define SimG4Core_PhysicsLists_QGSPCMS_BIC_H
+ 
+#include "SimG4Core/Physics/interface/PhysicsList.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+ 
+class QGSPCMS_BIC: public PhysicsList {
+
+public:
+  QGSPCMS_BIC(G4LogicalVolumeToDDLogicalPartMap& map, 
+	      const edm::ParameterSet & p);
+};
+
+#endif
+
+
