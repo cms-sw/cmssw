13ba552608bb808c12ae02491737caac38365a38
Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch>
2008-03-03 00:36:08 +0000

    Fix include path.

--- HLTriggerOffline/Tau/src/L1TauAnalyzer.cc
+++ HLTriggerOffline/Tau/src/L1TauAnalyzer.cc
@@ -13,11 +13,11 @@
 //
 // Original Author:  Chi Nhan Nguyen
 //         Created:  Fri Feb 22 09:20:55 CST 2008
-// $Id: L1TauAnalyzer.cc,v 1.6 2008/02/25 01:22:39 chinhan Exp $
+// $Id: L1TauAnalyzer.cc,v 1.1 2008/02/25 22:01:05 chinhan Exp $
 //
 //
 
-#include "RecoTauTag/HLTAnalyzers/interface/L1TauAnalyzer.h"
+#include "HLTriggerOffline/Tau/interface/L1TauAnalyzer.h"
 
 
 //
