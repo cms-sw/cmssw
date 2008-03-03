d58bcb43342006419c59b2305f4694478dc66bcd
Martina Malberti <sha1-2b004fb57608a448d74be744ecf92d42b9b3ffaa@cern.ch>
2008-03-03 18:18:35 +0000

    bug fix

--- Calibration/Tools/plugins/SealModule.cc
+++ Calibration/Tools/plugins/SealModule.cc
@@ -3,15 +3,15 @@
 
 #include "Calibration/Tools/interface/PhiRangeSelector.h"
 #include "Calibration/Tools/interface/IMASelector.h"
-#include "Calibration/Tools/interface/SingleEleCalibSelector.h"
+#include "Calibration/Tools/plugins/SingleEleCalibSelector.h"
 
 #include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
 #include "PhysicsTools/UtilAlgos/interface/SingleObjectSelector.h"
 #include "PhysicsTools/UtilAlgos/interface/SingleElementCollectionSelector.h"
 
-#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
-#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
-#include "PhysicsTools/RecoAlgos/interface/PixelMatchGsfElectronSelector.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "PhysicsTools/RecoAlgos/interface/GsfElectronSelector.h"
 
 #include "Calibration/Tools/plugins/SelectorWithEventSetup.h"
 
@@ -20,11 +20,11 @@ DEFINE_SEAL_MODULE();
 
 namespace reco {
   namespace modules {
-    typedef SingleObjectSelector<reco::PixelMatchGsfElectronCollection,
+    typedef SingleObjectSelector<reco::GsfElectronCollection,
                                  ::PhiRangeSelector> PhiRangeSelector; 
     DEFINE_ANOTHER_FWK_MODULE(PhiRangeSelector);
 
-    typedef SingleObjectSelector<reco::PixelMatchGsfElectronCollection,
+    typedef SingleObjectSelector<reco::GsfElectronCollection,
                                  ::IMASelector> IMASelector; 
     DEFINE_ANOTHER_FWK_MODULE(IMASelector);
 
@@ -32,5 +32,5 @@ namespace reco {
 }
 
 
-typedef SelectorWithEventSetup<SingleEleCalibSelector> SingleElectronCalibrationSelector;
-DEFINE_ANOTHER_FWK_MODULE( SingleElectronCalibrationSelector );
+// typedef SelectorWithEventSetup<SingleEleCalibSelector> SingleElectronCalibrationSelector;
+// DEFINE_ANOTHER_FWK_MODULE( SingleElectronCalibrationSelector );
