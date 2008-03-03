55befd505a6e249be33a00bb71b8aeca23f0a09b
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2008-03-03 21:54:41 +0000

    New records for prescale factors, trigger masks, veto masks. New default values, re-organized menu-related cff. Added technical triggers.

--- L1TriggerConfig/L1GtConfigProducers/src/SealModule.cc
+++ L1TriggerConfig/L1GtConfigProducers/src/SealModule.cc
@@ -3,13 +3,28 @@
 
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtStableParametersTrivialProducer.h"
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtParametersTrivialProducer.h"
+
+//
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtFactorsTrivialProducer.h"
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtPrescaleFactorsAlgoTrigTrivialProducer.h"
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtPrescaleFactorsTechTrigTrivialProducer.h"
+
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMaskAlgoTrigTrivialProducer.h"
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMaskTechTrigTrivialProducer.h"
+
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMaskVetoAlgoTrigTrivialProducer.h"
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMaskVetoTechTrigTrivialProducer.h"
+
+//
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtBoardMapsTrivialProducer.h"
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMenuXmlProducer.h"
 
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtStableParametersTester.h"
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtParametersTester.h"
+
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtFactorsTester.h"
+#include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtPrescaleFactorsAndMasksTester.h"
+
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtBoardMapsTester.h"
 #include "L1TriggerConfig/L1GtConfigProducers/interface/L1GtTriggerMenuTester.h"
 
@@ -17,13 +32,29 @@
 
 DEFINE_FWK_EVENTSETUP_MODULE(L1GtStableParametersTrivialProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtParametersTrivialProducer);
+
+//
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtFactorsTrivialProducer);
+
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtPrescaleFactorsAlgoTrigTrivialProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtPrescaleFactorsTechTrigTrivialProducer);
+
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtTriggerMaskAlgoTrigTrivialProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtTriggerMaskTechTrigTrivialProducer);
+
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtTriggerMaskVetoAlgoTrigTrivialProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtTriggerMaskVetoTechTrigTrivialProducer);
+
+//
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtBoardMapsTrivialProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1GtTriggerMenuXmlProducer);
 
 DEFINE_ANOTHER_FWK_MODULE(L1GtStableParametersTester);
 DEFINE_ANOTHER_FWK_MODULE(L1GtParametersTester);
+
 DEFINE_ANOTHER_FWK_MODULE(L1GtFactorsTester);
+DEFINE_ANOTHER_FWK_MODULE(L1GtPrescaleFactorsAndMasksTester);
+
 DEFINE_ANOTHER_FWK_MODULE(L1GtBoardMapsTester);
 DEFINE_ANOTHER_FWK_MODULE(L1GtTriggerMenuTester);
 DEFINE_ANOTHER_FWK_MODULE(L1GtVhdlWriter);
