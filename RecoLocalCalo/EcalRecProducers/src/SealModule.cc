40f4aad00731c0d73a244352110c2c2225bdb300
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-03-03 17:37:27 +0000

    added ecal re-calibrated recHit producer

--- RecoLocalCalo/EcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/EcalRecProducers/src/SealModule.cc
@@ -6,6 +6,7 @@
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalMaxSampleUncalibRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/EcalRecHitProducer.h"
 #include "RecoLocalCalo/EcalRecProducers/interface/ESRecHitProducer.h"
+#include "RecoLocalCalo/EcalRecProducers/interface/EcalRecalibRecHitProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( EcalWeightUncalibRecHitProducer );
@@ -14,4 +15,4 @@ DEFINE_ANOTHER_FWK_MODULE( EcalFixedAlphaBetaFitUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalMaxSampleUncalibRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( EcalRecHitProducer );
 DEFINE_ANOTHER_FWK_MODULE( ESRecHitProducer );
-
+DEFINE_ANOTHER_FWK_MODULE( EcalRecalibRecHitProducer );
