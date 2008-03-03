05ab7748d4fa659a7326bc8c6dde8f70e9ad2f92
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2008-03-03 16:58:17 +0000

    additional cond objects

---
+++ CondFormats/DataRecord/src/HcalCalibrationQIEDataRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     HcalCalibrationQIEDataRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Sat Mar  1 15:49:07 CET 2008
+// $Id$
+
+#include "CondFormats/DataRecord/interface/HcalCalibrationQIEDataRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(HcalCalibrationQIEDataRcd);
