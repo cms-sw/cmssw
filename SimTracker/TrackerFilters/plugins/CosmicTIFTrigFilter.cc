dad8d8d140f1faea8e45bffb84a800af02ad0d4b
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2008-03-03 16:16:02 +0000

    Scintillator position updated and configurable via cfg

--- SimTracker/TrackerFilters/plugins/CosmicTIFTrigFilter.cc
+++ SimTracker/TrackerFilters/plugins/CosmicTIFTrigFilter.cc
@@ -21,6 +21,18 @@ namespace cms
   CosmicTIFTrigFilter::CosmicTIFTrigFilter(const edm::ParameterSet& conf):    conf_(conf)
   {
     trigconf  = conf_.getParameter<int>("trig_conf");
+    trigS1  = conf_.getParameter<std::vector<double> >("PosScint1");
+    trigS2  = conf_.getParameter<std::vector<double> >("PosScint2");
+    trigS3  = conf_.getParameter<std::vector<double> >("PosScint3");
+    trigS4  = conf_.getParameter<std::vector<double> >("PosScint4");
+
+    /*
+    std::cout << "S1 = " << trigS1[0] << ", " <<  trigS1[1] << ", " <<trigS1[2]
+	      << "\nS2 = " << trigS2[0] << ", " <<  trigS2[1] << ", " <<trigS2[2]
+	      << "\nS3 = " << trigS3[0] << ", " <<  trigS3[1] << ", " <<trigS3[2]  
+	      << "\nS4 = " << trigS4[0] << ", " <<  trigS4[1] << ", " <<trigS4[2]  
+	      << std::endl;  
+    */
   }
   
   bool CosmicTIFTrigFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
@@ -54,11 +66,21 @@ namespace cms
 	    
 	    if(trigconf==1){
 
+/*
 	      HepMC::FourVector S1(350.,1600.,500.,0.);
 	      HepMC::FourVector S2(350.,-1600.,400.,0.);
 	      HepMC::FourVector S3(350.,1600.,1600.,0.);
-
-
+*/
+/*
+//numbers taken from real data
+	     HepMC::FourVector S1(150.,1600.,550.,0.);
+ 	     HepMC::FourVector S2(400.,-1600.,50.,0.);
+	     HepMC::FourVector S3(0.,1600.,2400.,0.);
+*/
+	      HepMC::FourVector S1(trigS1[0],trigS1[1],trigS1[2],0.);
+	      HepMC::FourVector S2(trigS2[0],trigS2[1],trigS2[2],0.);
+	      HepMC::FourVector S3(trigS3[0],trigS3[1],trigS3[2],0.);
+	      
 	      hit1=Sci_trig(vertex, momentum, S1);
 	      hit2=Sci_trig(vertex, momentum, S2);
 	      hit3=Sci_trig(vertex, momentum, S3);
@@ -78,9 +100,16 @@ namespace cms
 		}
 	    }else if(trigconf ==2) {
 
+	      /*
 	      HepMC::FourVector S1(350.,1600.,850.,0.);
 	      HepMC::FourVector S2(0.,-1550.,-1650.,0.);
 	      HepMC::FourVector S3(350.,1600.,2300.,0.);
+	      */
+
+	      HepMC::FourVector S1(trigS1[0],trigS1[1],trigS1[2],0.);
+	      HepMC::FourVector S2(trigS2[0],trigS2[1],trigS2[2],0.);
+	      HepMC::FourVector S3(trigS3[0],trigS3[1],trigS3[2],0.);
+
 	      
 	      hit1=Sci_trig(vertex, momentum, S1);
 	      hit2=Sci_trig(vertex, momentum, S2);
@@ -101,12 +130,33 @@ namespace cms
 		}
 
 	    }else if(trigconf ==3) {
-
+/*
 	      HepMC::FourVector S1(350.,1600.,850.,0.);
 	      HepMC::FourVector S3(350.,1600.,2300.,0.);
 	      HepMC::FourVector S2(350.,-1600.,400.,0.);
 	      HepMC::FourVector S4(0.,-1600.,-2000.,0.);
-	      
+*/
+/*
+//new configuration from data
+	      HepMC::FourVector S1(150.,1600.,900.,0.);
+	      HepMC::FourVector S3(50.,1600.,2450.,0.);
+	      HepMC::FourVector S2(400.,-1600.,100.,0.);
+  	      HepMC::FourVector S4(-250.,-1600.,-2050.,0.);
+*/	      
+
+	      HepMC::FourVector S1(trigS1[0],trigS1[1],trigS1[2],0.);
+	      HepMC::FourVector S2(trigS2[0],trigS2[1],trigS2[2],0.);
+	      HepMC::FourVector S3(trigS3[0],trigS3[1],trigS3[2],0.);
+	      HepMC::FourVector S4(trigS4[0],trigS4[1],trigS4[2],0.);
+
+	      /*
+	      std::cout << "S1 = " << S1.x() 
+			<< "\tS2 = " << S2.x()  
+			<< "\tS3 = " << S3.x()  
+			<< "\tS4 = " << S4.x()
+			<< std::endl;  
+	      */
+
 	      hit1=Sci_trig(vertex, momentum, S1);
 	      hit2=Sci_trig(vertex, momentum, S2);
 	      hit3=Sci_trig(vertex, momentum, S3);
@@ -115,11 +165,13 @@ namespace cms
 	      // trigger conditions
 	      if((hit1&&hit2) || (hit3&&hit2) || (hit1&&hit4) || (hit3&&hit4))
 		{
+		  /*
 		  cout << "\tGot a trigger in configuration C " << endl; 
 		  if(hit1)cout << "hit1 " << endl;
 		  if(hit2)cout << "hit2 " << endl;
 		  if(hit3)cout << "hit3 " << endl;
 		  if(hit4)cout << "hit4 " << endl;
+		  */
 		  trig3++;
 		  return true;
 		}
