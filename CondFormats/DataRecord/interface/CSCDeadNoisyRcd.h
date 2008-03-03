d7f1ef84a1880f70bc25ecdd3994edb142b0c7c3
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2008-03-03 12:55:13 +0000

    removed

--- CondFormats/DataRecord/interface/CSCDeadNoisyRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef CSCDEADNOISYRCD_H
-#define CSCDEADNOISYRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class CSCDeadNoisyRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCDeadNoisyRcd> {};
-#endif
