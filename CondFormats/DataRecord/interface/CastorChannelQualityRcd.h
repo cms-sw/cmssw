7bdc34399fe825680286fd8287dc704abc78524f
Panagiotis Katsas <sha1-163d0c4b196b62c2ba4e39987db97923b896ea8b@cern.ch>
2008-03-04 10:38:54 +0000

    records for CASTOR

---
+++ CondFormats/DataRecord/interface/CastorChannelQualityRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_CastorChannelQualityRcd_h
+#define DataRecord_CastorChannelQualityRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     CastorChannelQualityRcd
+// 
+/**\class CastorChannelQualityRcd CastorChannelQualityRcd.h CondFormats/DataRecord/interface/CastorChannelQualityRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Mon Feb 11 12:15:32 CET 2008
+// $Id: CastorChannelQualityRcd.h,v 1.1 2008/02/15 15:53:03 mccauley Exp $
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class CastorChannelQualityRcd : public edm::eventsetup::EventSetupRecordImplementation<CastorChannelQualityRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/CastorPedestalWidthsRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     CastorPedestalWidthsRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Mon Feb 11 12:18:02 CET 2008
+// $Id: CastorPedestalWidthsRcd.cc,v 1.1 2008/02/15 15:53:56 mccauley Exp $
+
+#include "CondFormats/DataRecord/interface/CastorPedestalWidthsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(CastorPedestalWidthsRcd);
