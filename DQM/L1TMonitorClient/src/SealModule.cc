b08c964a42172b3e071b9b500065b833208e66d3
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-03-04 14:51:47 +0000

    added L1TdeEcal

--- DQM/L1TMonitorClient/src/SealModule.cc
+++ DQM/L1TMonitorClient/src/SealModule.cc
@@ -7,4 +7,6 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L1THcalClient);
 #include <DQM/L1TMonitorClient/interface/L1TDTTPGClient.h>
 DEFINE_ANOTHER_FWK_MODULE(L1TDTTPGClient);
+#include <DQM/L1TMonitorClient/interface/L1TdeECALClient.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TdeECALClient);
 
