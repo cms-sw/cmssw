9e9e04078e91a210853501f971a0e3408f09f179
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-03-06 09:26:15 +0000

    removed files

--- PhysicsTools/PatAlgos/src/PATPhotonCleaner.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "PhysicsTools/PatAlgos/interface/PATPhotonCleaner.h"
-
-using namespace pat;
-DEFINE_FWK_MODULE(PATBasePhotonCleaner);
-//DEFINE_FWK_MODULE(PATConvertedPhotonCleaner);
