c47546ee218fa2d332a67a318ff45d343a1c2ea8
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-03-07 00:13:39 +0000

    fix conflict

--- AnalysisExamples/SiStripDetectorPerformance/plugins/SealModules.cc
+++ AnalysisExamples/SiStripDetectorPerformance/plugins/SealModules.cc
@@ -2,22 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/plugins/ClusterAnalysis.h"
 #include "AnalysisExamples/SiStripDetectorPerformance/plugins/ClusterAnalysisFilter.h"
-<<<<<<< SealModules.cc
 //#include "AnalysisExamples/SiStripDetectorPerformance/plugins/ClusterThr.h"
-=======
->>>>>>> 1.4
 
 using cms::ClusterAnalysis;
 using cms::ClusterAnalysisFilter;
-<<<<<<< SealModules.cc
 //using cms::ClusterThr;
-=======
->>>>>>> 1.4
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(ClusterAnalysisFilter);
-<<<<<<< SealModules.cc
 //DEFINE_ANOTHER_FWK_MODULE(ClusterThr);
-=======
->>>>>>> 1.4
