4932bf9802c68c178df422ebeef771ccc492eb61
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2008-03-07 18:35:53 +0000

    Adding testing for event history

---
+++ FWCore/Framework/test/stubs/HistoryAnalyzer.cc
@@ -0,0 +1,75 @@
+
+/*----------------------------------------------------------------------
+
+ EDAnalyzer for testing History class and history tracking mechanism.
+
+----------------------------------------------------------------------*/
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "DataFormats/Provenance/interface/History.h"
+#include "DataFormats/Provenance/interface/ParameterSetID.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+using namespace edm;
+
+namespace edmtest {
+
+  class HistoryAnalyzer : public EDAnalyzer {
+  public:
+
+    explicit HistoryAnalyzer(const ParameterSet& params);
+    void analyze(const Event& event, EventSetup const&);
+    void endJob();
+
+  private:
+    int pass_;
+    int eventCount_; 
+    int expectedCount_;
+    ParameterSetID emptyID_;
+    ParameterSetID outputConfigID_;
+  };
+
+  HistoryAnalyzer::HistoryAnalyzer(const ParameterSet& params) :
+    pass_(params.getParameter<int>("pass")),
+    eventCount_(0),
+    expectedCount_(params.getParameter<int>("expectedCount")),
+    emptyID_(ParameterSet().id()),
+    outputConfigID_()
+  {
+    ParameterSet temp;
+    typedef std::vector<std::string> vstring;
+    vstring wanted_paths(1, "f55");
+    temp.addParameter<std::vector<std::string> >("SelectEvents", wanted_paths);
+    outputConfigID_ = temp.id();
+  }
+
+  void
+  HistoryAnalyzer::analyze(const Event& event, EventSetup const&) 
+  {
+    History const& h = event.history();
+    assert(h.size() == static_cast<size_t>(pass_ - 1));
+
+    assert(h.getEventSelectionID(0) == emptyID_);
+    assert(h.getEventSelectionID(1) == outputConfigID_);
+    assert(h.getEventSelectionID(2) == emptyID_);
+    ++eventCount_;    
+  }
+
+  void
+  HistoryAnalyzer::endJob()
+  {
+    std::cout << "Expected count is: " << expectedCount_ << std::endl;
+    std::cout << "Event count is:    " << eventCount_ << std::endl;
+    assert(eventCount_ == expectedCount_);
+  }
+}
+
+using edmtest::HistoryAnalyzer;
+DEFINE_FWK_MODULE(HistoryAnalyzer);
--- FWCore/Integration/test/eventHistoryTest.sh
+++ FWCore/Integration/test/eventHistoryTest.sh
@@ -9,17 +9,25 @@ echo "*************************************************"
 echo "**************** Finished pass 1 ****************"
 echo "*************************************************"
 
-# Read the first file, make the SECOND process
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/EventHistory_2.cfg || die 'Failed in EventHistory_1' $?
+# Read the first file, write the second.
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/EventHistory_2.cfg || die 'Failed in EventHistory_2' $?
 echo "*************************************************"
 echo "**************** Finished pass 2 ****************"
 echo "*************************************************"
 
-# Read the second file, make sure the event data have the right history
-cmsRun --parameter-set ${LOCAL_TEST_DIR}/EventHistory_3.cfg || die 'Failed in EventHistory_1' $?
+# Read the second file, write the third.
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/EventHistory_3.cfg || die 'Failed in EventHistory_3' $?
 echo "*************************************************"
 echo "**************** Finished pass 3 ****************"
 echo "*************************************************"
+
+# Read the third file, make sure the event data have the right history
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/EventHistory_4.cfg || die 'Failed in EventHistory_4' $?
+echo "*************************************************"
+echo "**************** Finished pass 4 ****************"
+echo "*************************************************"
+
+
 
 
 
