2a81ebba3849c422bfa86087cbb160946e24e5f5
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-03-08 00:39:48 +0000

    Adding CaloTauTag to the package

--- Validation/RecoTau/src/SealModule.cc
+++ Validation/RecoTau/src/SealModule.cc
@@ -2,8 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoTau/interface/TauTagVal.h"
 #include "Validation/RecoTau/interface/PFTauTagVal.h"
+#include "Validation/RecoTau/interface/CaloTauTagVal.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( TauTagVal );
 DEFINE_ANOTHER_FWK_MODULE( PFTauTagVal );
-
+DEFINE_ANOTHER_FWK_MODULE( CaloTauTagVal );
