f677ba9d8232c0e187d37c810d61d627730116ff
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-03-10 10:34:25 +0000

    EcalMappingElectronics added

---
+++ CondFormats/DataRecord/interface/EcalMappingElectronicsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalMappingElectronicsRcd_H
+#define CondFormats_DataRecord_EcalMappingElectronicsRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalMappingElectronicsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalMappingElectronicsRcd> {};
+#endif
