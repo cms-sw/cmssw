446b9ef3fa6f0142b4688ac2e35f4ec1d432696d
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2008-03-10 18:43:06 +0000

    This shouldn't be needed anymore in the Layer-1.

--- PhysicsTools/PatAlgos/plugins/PATJetProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATJetProducer.cc
@@ -1,5 +1,5 @@
 //
-// $Id$
+// $Id: PATJetProducer.cc,v 1.1.2.3 2008/03/10 18:28:00 lowette Exp $
 //
 
 #include "PhysicsTools/PatAlgos/plugins/PATJetProducer.h"
@@ -11,7 +11,7 @@
 #include "DataFormats/Common/interface/Association.h"
 #include "DataFormats/Candidate/interface/CandAssociation.h"
 
-#include "JetMETCorrections/Objects/interface/JetCorrector.h"
+#include "DataFormats/BTauReco/interface/JetTagFwd.h"
 #include "DataFormats/BTauReco/interface/JetTag.h"
 #include "DataFormats/BTauReco/interface/TrackProbabilityTagInfo.h"
 //#include "DataFormats/BTauReco/interface/TrackProbabilityTagInfoFwd.h"
