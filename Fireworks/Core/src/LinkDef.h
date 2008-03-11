26c5812dd67808c302bb112e57376d4b94c46fdf
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-03-11 02:51:41 +0000

    added FWListViewObject and its editor

--- Fireworks/Core/src/LinkDef.h
+++ Fireworks/Core/src/LinkDef.h
@@ -2,6 +2,8 @@
 #include "Fireworks/Core/src/FWListEventItemEditor.h"
 #include "Fireworks/Core/src/FWListModel.h"
 #include "Fireworks/Core/src/FWListModelEditor.h"
+#include "Fireworks/Core/src/FWListViewObject.h"
+#include "Fireworks/Core/src/FWListViewObjectEditor.h"
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
@@ -10,4 +12,6 @@
 #pragma link C++ class FWListEventItemEditor;
 #pragma link C++ class FWListModel;
 #pragma link C++ class FWListModelEditor;
+#pragma link C++ class FWListViewObject;
+#pragma link C++ class FWListViewObjectEditor;
 #endif
