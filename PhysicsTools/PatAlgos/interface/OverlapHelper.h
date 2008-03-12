41bb601491b7aaf6a83e3b7a45575e006adf6614
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2008-03-12 13:18:40 +0000

    Better OverlapHelper: - uses flags defined in pat::Flags - allows optional string cut on items

--- PhysicsTools/PatAlgos/interface/OverlapHelper.h
+++ PhysicsTools/PatAlgos/interface/OverlapHelper.h
@@ -9,6 +9,7 @@
 
 #include "DataFormats/Candidate/interface/CandidateFwd.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "PhysicsTools/Utilities/interface/StringCutObjectSelector.h"
 
 //#include "PhysicsTools/PatUtils/interface/SimpleOverlapFinder.h"
 #include "PhysicsTools/PatUtils/interface/GenericOverlapFinder.h"
@@ -18,15 +19,39 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 
+#include <boost/shared_ptr.hpp>
 
 namespace pat { 
     namespace helper {
 
-
+    struct OverlapWithPointerByDeltaR {
+        public:
+            OverlapWithPointerByDeltaR(double deltaR) :  scale_(1.0/(deltaR*deltaR)) {}
+            template<typename T1, typename T2>
+            double operator()(const T1 &t1, const T2 &t2) const {
+                return deltaR2(t1,*t2) * scale_;
+            }
+        private:
+            double scale_;
+    };
 class OverlapHelper {
     public:
-        typedef pat::GenericOverlapFinder<pat::OverlapByDeltaR> SimpleOverlapFinder;
+        typedef pat::GenericOverlapFinder<pat::helper::OverlapWithPointerByDeltaR> SimpleOverlapFinder;
 
+        // == Generic worker class ==
+        class Worker {
+            public:
+                Worker(const edm::ParameterSet &pset) ; 
+                template<typename Collection>
+                std::auto_ptr<pat::OverlapList> 
+                    run( const edm::Event &iEvent, const Collection &items) const ;
+            protected:
+                edm::InputTag tag_;
+                SimpleOverlapFinder finder_;
+                boost::shared_ptr<StringCutObjectSelector<reco::Candidate> > cut_;
+                mutable std::vector<const reco::Candidate *> tmp_;
+        }; // worker
+ 
         /// A vector of the results of overlap testing for element in the collection to isolate, encoded in bits. 
         /// 0 = no overlaps.
         /// Each bit is set to 1 if there is an overlap with the corresponding collection.
@@ -34,6 +59,10 @@ class OverlapHelper {
 
         OverlapHelper() { }
         OverlapHelper(const std::vector<edm::ParameterSet> &psets) ;
+        OverlapHelper(const edm::ParameterSet &pset) ;
+
+        void addWorker(const edm::ParameterSet &pset, uint32_t mask=0) ;
+        void addWorker(Worker w, uint32_t mask=0) ;
 
         /// True if it has a non null configuration
         bool enabled() const  { return !workers_.empty(); }
@@ -43,21 +72,9 @@ class OverlapHelper {
         template<typename Collection>
         std::auto_ptr<Result> test( const edm::Event &iEvent, const Collection &items ) const ;
 
-        // == Generic worker class ==
-        class Worker {
-            public:
-                Worker(const edm::ParameterSet &pset) : 
-                    tag_(pset.getParameter<edm::InputTag>("collection")),
-                    finder_(pset.getParameter<double>("deltaR")) { }
-                template<typename Collection>
-                std::auto_ptr<pat::OverlapList> 
-                    run( const edm::Event &iEvent, const Collection &items) const ;
-            protected:
-                edm::InputTag tag_;
-                SimpleOverlapFinder finder_;
-        }; // worker
-    protected:
-        std::vector<Worker> workers_;
+   protected:
+        std::vector<Worker>   workers_;
+        std::vector<uint32_t> masks_;
 } ; // class
 
 
@@ -69,7 +86,14 @@ pat::helper::OverlapHelper::Worker::run(const edm::Event &iEvent, const Collecti
         using namespace edm;
         Handle< View<reco::Candidate> > handle;
         iEvent.getByLabel(tag_, handle);
-        return finder_.find(items, *handle);
+        tmp_.clear(); // just in case
+        for (View<reco::Candidate>::const_iterator it = handle->begin(), ed = handle->end(); it != ed; ++it) {
+            if (cut_.get() && ! (*cut_)(*it) ) continue;
+            tmp_.push_back( & *it );
+        }
+        std::auto_ptr<pat::OverlapList> ret = finder_.find(items, tmp_);
+        tmp_.clear(); 
+        return ret;
 }
 
 template<typename Collection>
@@ -81,13 +105,13 @@ pat::helper::OverlapHelper::test( const edm::Event &iEvent, const Collection &in
     auto_ptr<Result> ret(new Result(size, 0));
     Result &result = *ret; // pointers to vectors are more ugly work with than refs to vectors
 
-    int testBit = 1;
+    vector<uint32_t>::const_iterator itm = masks_.begin();
     for (vector<Worker>::const_iterator itw = workers_.begin(), edw = workers_.end(); 
-                itw != edw; ++itw, testBit <<= 1) {
+                itw != edw; ++itw, ++itm) {
         auto_ptr<pat::OverlapList> overlaps = itw->run(iEvent, in);
         for (pat::OverlapList::const_iterator ito = overlaps->begin(), edo = overlaps->end(); 
                     ito != edo; ++ito) {
-            result[ito->first] += testBit;
+            result[ito->first] |= *itm;
         }
     }
 
