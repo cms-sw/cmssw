b48293e52e2d3cdd9b144cf9da9a4e5b4016518b
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-03-12 14:55:45 +0000

    new minuit interface and command file parser added

--- PhysicsTools/Utilities/interface/RootMinuit.h
+++ PhysicsTools/Utilities/interface/RootMinuit.h
@@ -3,87 +3,133 @@
 /** \class fit::RootMinuit
  *
  */
+#include "PhysicsTools/Utilities/interface/Parameter.h"
+#include "PhysicsTools/Utilities/interface/ParameterMap.h"
+#include "FWCore/Utilities/interface/EDMException.h"
 #include "TMinuit.h"
+#include <boost/shared_ptr.hpp>
 #include <iostream>
 #include <vector>
 #include <string>
-#include <boost/shared_ptr.hpp>
-#include "PhysicsTools/Utilities/interface/Parameter.h"
+#include <memory>
 
 namespace fit {
   
   template<class Function>
   class RootMinuit {
   public:
-    RootMinuit(int pars, Function f, bool verbose = false) : 
-      minuit_(pars), pars_(pars), verbose_(verbose) { 
+    RootMinuit(Function f, bool verbose = false) : 
+      initialized_(false), verbose_(verbose) { 
       f_ = f;
+    }
+    void init() { 
+      minuit_.reset(new TMinuit(parMap_.size()));
       double arglist[10];
       int ierflg = 0;      
       if (! verbose_) {
 	arglist[0] = -1;
-	minuit_.mnexcm("SET PRINT", arglist, 1, ierflg); 
-	if ( ierflg != 0 ) std::cerr << "ERROR in set print!" << std::endl;
+	minuit_->mnexcm("SET PRINT", arglist, 1, ierflg); 
+	if (ierflg != 0) 
+	  throw edm::Exception(edm::errors::Configuration)
+	    << "RootMinuit: error in calling SET PRINT\n";
       }     
       arglist[0] = 1;
-      minuit_.mnexcm("SET ERR", arglist, 1, ierflg);
-      if ( ierflg != 0 ) std::cerr << "ERROR in set err!" << std::endl;
+      minuit_->mnexcm("SET ERR", arglist, 1, ierflg);
+      if (ierflg != 0) 
+	throw edm::Exception(edm::errors::Configuration)
+	  << "RootMinuit: error in calling SET ERR\n";
+
+      size_t i = 0;
+      typename parameterVector_t::const_iterator p = parMap_.begin(), end = parMap_.end();
+      for(; p != end; ++p, ++i) {
+	const std::string & name = p->first;
+	const parameter_t & par = p->second;
+	minuit_->mnparm(i, name, par.val, par.err, par.min, par.max, ierflg);
+	if(ierflg != 0)
+	  throw edm::Exception(edm::errors::Configuration)
+	    << "RootMinuit: error in setting parameter " << i 
+	    << " value = " << par.val << " error = " << par.err
+	    << " range = [" << par.min << ", " << par.max << "]\n";
+	if(par.fixed)
+	  minuit_->FixParameter(i);
+      }
+      initialized_ = true;
     }
-    bool setParameter(int i, const std::string & name, boost::shared_ptr<double> val, double err, double min, double max) {
-      pars_[i] = val;
-      int ierflag;
-      minuit_.mnparm(i, name.c_str(), *val, err, min, max, ierflag);
-      return ierflag == 0;
+    void addParameter(const std::string & name, boost::shared_ptr<double> val, double err, double min, double max) {
+      pars_.push_back(val);
+      parameter_t par;
+      par.val = *val;
+      par.err = err;
+      par.min = min;
+      par.max = max;
+      par.fixed = false;
+      parMap_.push_back(std::make_pair(name, par));
+      size_t s = parIndices_.size();
+      parIndices_[name] = s;
     }
-    bool setParameter(int i, const function::Parameter & par, double err, double min, double max) {
-      return setParameter(i, par.name(), par, err, min, max);
+    void addParameter(const function::Parameter & par, double err, double min, double max) {
+      return addParameter(par.name(), par, err, min, max);
     }
-    double getParameter(int i, double & err) const {
+    double getParameter(const std::string & name, double & err) {
       double val;
-      minuit_.GetParameter(i, val, err);
+      if(!initialized_) init();
+      minuit_->GetParameter(parameterIndex(name), val, err);
       return val;
     }
-    double getParameter(int i) const {
+    double getParameter(const std::string & name) {
       double val, err;
-      minuit_.GetParameter(i, val, err);
+      if(!initialized_) init();
+      minuit_->GetParameter(parameterIndex(name), val, err);
       return val;
     }
-    double getParameterError(int i, double & val) const {
+    double getParameterError(const std::string & name, double & val) {
       double err;
-      minuit_.GetParameter(i, val, err);
+      if(!initialized_) init();
+      minuit_->GetParameter(parameterIndex(name), val, err);
       return err;
     }
-    double getParameterError(int i) const {
+    double getParameterError(const std::string & name) {
       double val, err;
-      minuit_.GetParameter(i, val, err);
+      if(!initialized_) init();
+      minuit_->GetParameter(parameterIndex(name), val, err);
       return err;
     }
-    void fixParameter(int i) {
-      minuit_.FixParameter(i);
+    void fixParameter(const std::string & name) {
+      size_t i = parameterIndex(name);
+      if(initialized_) {
+	minuit_->FixParameter(i);
+      }
+      parMap_[i].second.fixed = true;
     }
-    int getNumberOfFreeParameters() const { 
-      return minuit_.GetNumFreePars();
+    int getNumberOfFreeParameters() { 
+      if(!initialized_) init();
+      return minuit_->GetNumFreePars();
     }
     double minimize() {
+      if(!initialized_) init();
       std::cout << ">>> running fit" << std::endl;
       double arglist[10];
       arglist[0] = 5000;
       arglist[1] = 0.1;
       int ierflag;
       fPars_= & pars_; 
-      minuit_.SetFCN(fcn_);
-      minuit_.mnexcm("MIGRAD", arglist, 2, ierflag);
+      minuit_->SetFCN(fcn_);
+      minuit_->mnexcm("MIGRAD", arglist, 2, ierflag);
       if ( ierflag != 0 ) std::cerr << "ERROR in migrad!!" << std::endl;
-      if(verbose_) minuit_.mnmatu(1); //Prints the covariance matrix
+      if(verbose_) minuit_->mnmatu(1); //Prints the covariance matrix
       std::cout << ">>> fit completed" << std::endl;
       double amin, edm, errdef;
       int nvpar, nparx;
-      minuit_.mnstat(amin, edm, errdef, nvpar, nparx, ierflag);
-      if(verbose_) minuit_.mnprin(3, amin);
+      minuit_->mnstat(amin, edm, errdef, nvpar, nparx, ierflag);
+      if(verbose_) minuit_->mnprin(3, amin);
       return amin;
     }
+
   private:
-    TMinuit minuit_;
+    parameterVector_t parMap_;
+    std::map<std::string, size_t> parIndices_;
+    bool initialized_;
+    std::auto_ptr<TMinuit> minuit_;
     std::vector<boost::shared_ptr<double> > pars_;
     static std::vector<boost::shared_ptr<double> > *fPars_;
     bool verbose_;
@@ -93,6 +139,13 @@ namespace fit {
       for(size_t i = 0; i < size; ++i) 
 	*((*fPars_)[i]) = par[i];
       f = f_();
+    }
+    size_t parameterIndex(const std::string &name) const {
+      typename std::map<std::string, size_t>::const_iterator p = parIndices_.find(name);
+      if(p == parIndices_.end())
+	throw edm::Exception(edm::errors::Configuration)
+	  << "RootMinuit: can't find parameter " << name << "\n";
+      return p->second;
     }
   };
   
---
+++ PhysicsTools/Utilities/interface/RootMinuitCommands.h
@@ -0,0 +1,143 @@
+#ifndef PhysicsTools_Utilities_RootMinuitCommands_h
+#define PhysicsTools_Utilities_RootMinuitCommands_h
+#include "PhysicsTools/Utilities/interface/RootMinuit.h"
+#include "PhysicsTools/Utilities/interface/ParameterMap.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "Utilities/General/interface/FileInPath.h"
+#include "PhysicsTools/Utilities/interface/Parameter.h"
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include<boost/tokenizer.hpp>
+
+const char * kParameter = "par";
+
+namespace fit {
+  template<class Function>
+  class RootMinuitCommands {
+  public:
+    typedef RootMinuit<Function> minuit;
+    RootMinuitCommands(bool verbose = true) :
+      verbose_(verbose) {
+    }
+    RootMinuitCommands(const char * fileName, bool verbose = true) :
+      verbose_(verbose) {
+      init(fileName);
+    }
+    void init(const char * fileName) {
+      using namespace std;
+      string cmssw_release_base = getenv("CMSSW_RELEASE_BASE");
+      string cmssw_base = getenv("CMSSW_BASE");
+      string path = "."; 
+      if(!cmssw_release_base.empty()) {
+	path += ':';
+	path += (cmssw_release_base + "/src");
+      }
+      if(!cmssw_base.empty()) {
+	path += ':';
+	path += (cmssw_base + "/src");
+      }
+      FileInPath fileInPath(path, fileName);
+      ifstream * file = fileInPath();
+      if(file==0 || !file->is_open())
+	throw edm::Exception(edm::errors::Configuration)
+	  << "RootMinuitCommands: can't open file: " << fileName 
+	  << " in path: " << path << "\n";
+      if (verbose_) 
+	cout << ">>> configuration file: " << fileName << endl;
+      string line;
+      while(getline(*file, line)) {
+	line.erase(line.end()-1);
+	using namespace boost;
+	typedef tokenizer<char_separator<char> > tokenizer;
+	char_separator<char> sep(" ");
+	tokenizer tokens(line, sep);
+	tokenizer::iterator i = tokens.begin(), e = tokens.end();
+	if(tokens.begin()==tokens.end()) continue;
+	if(*(i->begin()) != '#') {
+	  if(*i == kParameter) {
+	    string name = *(++i);
+	    parameter_t par;
+	    par.val = string2double(*(++i));
+	    par.err = string2double(*(++i));
+	    par.min = string2double(*(++i));
+	    par.max = string2double(*(++i));
+	    string fixed = *(++i);
+	    if(fixed == "fixed") 
+	      par.fixed = true;
+	    else if(fixed == "free")
+	      par.fixed = false;
+	    else
+	      throw edm::Exception(edm::errors::Configuration)
+		<< "RootMinuitCommands: fix parameter option unknown: " << *i << "\n"
+		<< "valid options are: fixed, free.\n";
+	    pars_.push_back(std::make_pair(name, par));
+	    size_t s = parIndices_.size();
+	    parIndices_[name] = s;
+	    if(verbose_)
+	      cout << ">>> " << kParameter << " " << name 
+		   << " " << par.val 
+		   << " [" << par.min << ", " << par.max << "],"
+		   << " err: " << par.err
+		   << endl;
+	  } else {
+	    throw edm::Exception(edm::errors::Configuration)
+	      << "RootMinuitCommands: unkonwn command:: " << *i
+	      << "\n";
+	    
+	  }
+	}
+      }
+      if (verbose_) 
+	cout << ">>> end configuration" << endl;
+    }
+    double par(const std::string& name) {
+      return parameter(name).val;
+    }
+    double err(const std::string& name) {
+      return parameter(name).err;
+    }  
+    double min(const std::string& name) {
+      return parameter(name).min;
+    }
+    double max(const std::string& name) {
+      return parameter(name).max;
+    }  
+    bool fixed(const std::string& name) {
+      return parameter(name).fixed;
+    }   
+    void add(RootMinuit<Function>& minuit, function::Parameter& p) const {
+      const std::string & name = p.name();
+      const parameter_t & par = parameter(name);
+      minuit.addParameter(p, par.err, par.min, par.max);
+      if(par.fixed) minuit.fixParameter(name);
+    }
+  private:
+    bool verbose_;
+    parameterVector_t pars_;
+    std::map<std::string, size_t> parIndices_;
+    double string2double(const std::string & str) const {
+      const char * begin = str.c_str();
+      char * end;
+      double val = strtod(begin, &end);
+      size_t s = end - begin;
+      if(s < str.size()) {
+	throw edm::Exception(edm::errors::Configuration)
+	  << "RootMinuitCommands: invalid double value: " 
+	  << str  << "\n";
+      }
+      return val;
+    }
+    const parameter_t & parameter(const std::string& name) const {
+      typename std::map<std::string, size_t>::const_iterator p = parIndices_.find(name);
+      if(p == parIndices_.end())
+	throw edm::Exception(edm::errors::Configuration)
+	  << "RootMinuit: can't find parameter " << name << "\n";
+      return pars_[p->second].second;
+    }
+  };
+}
+
+#endif
