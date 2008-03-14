1cb0158b083195c4d779e8b6f2628a5c19cc6e8d
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-03-14 11:11:13 +0000

    - optimize strings usage

--- DQM/EcalCommon/src/LogicID.cc
+++ DQM/EcalCommon/src/LogicID.cc
@@ -1,12 +1,12 @@
-// $Id: LogicID.cc,v 1.9 2008/02/09 19:50:10 dellaric Exp $
+// $Id: LogicID.cc,v 1.10 2008/02/09 22:00:49 dellaric Exp $
 
 /*!
   \file LogicID.cc
   \brief Construct EcalLogicIDs
   \author G. Della Ricca
   \author B. Gobbo
-  \version $Revision: 1.9 $
-  \date $Date: 2008/02/09 19:50:10 $
+  \version $Revision: 1.10 $
+  \date $Date: 2008/02/09 22:00:49 $
 */
 
 #include "DQM/EcalCommon/interface/LogicID.h"
@@ -26,35 +26,35 @@ EcalLogicID LogicID::getEcalLogicID( std::string name,
 
   // EcalBarrel
 
-  if( name == "EB" ) {
+  if( strcmp(name.c_str(), "EB") == 0 ) {
     return( EcalLogicID( "EB",
                          1000000000 ) );
   }
-  if( name == "EB_crystal_number" ) {
+  if( strcmp(name.c_str(), "EB_crystal_number") == 0 ) {
     return( EcalLogicID( "EB_crystal_number",
                          1011000000+10000*id1+id2,
                          id1,
                          id2 ) );
   }
-  if( name == "EB_trigger_tower" ) {
+  if( strcmp(name.c_str(), "EB_trigger_tower") == 0 ) {
     return( EcalLogicID( "EB_trigger_tower",
                          1021000000+10000*id1+id2,
                          id1,
                          id2 ) );
   }
-  if( name == "EB_mem_channel" ) {
+  if( strcmp(name.c_str(), "EB_mem_channel") == 0 ) {
     return( EcalLogicID( "EB_mem_channel",
                          1191000000+10000*id1+id2,
                          id1,
                          id2 ) );
   }
-  if( name == "EB_mem_TT" ) {
+  if( strcmp(name.c_str(), "EB_mem_TT") == 0 ) {
     return( EcalLogicID( "EB_mem_TT",
                          1181000000+10000*id1+id2,
                          id1,
                          id2 ) );
   }
-  if( name == "EB_LM_PN" ) {
+  if( strcmp(name.c_str(), "EB_LM_PN") == 0 ) {
     return( EcalLogicID( "EB_LM_PN",
                          1131000000+10000*id1+id2,
                          id1,
@@ -63,30 +63,30 @@ EcalLogicID LogicID::getEcalLogicID( std::string name,
 
   // EcalEndcap
 
-  if( name == "EE" ) {
+  if( strcmp(name.c_str(), "EE") == 0 ) {
     return( EcalLogicID( "EE",
                          2000000000 ) );
   }
-  if( name == "EE_crystal_number" ) {
+  if( strcmp(name.c_str(), "EE_crystal_number") == 0 ) {
     return( EcalLogicID( "EE_crystal_number",
                          2010000000+1000000*((id1>=1&&id1<=9)?2:0)+1000*int(id2/1000)+int(id2%1000),
                          (id1>=1&&id1<=9)?+1:-1,
                          int(id2/1000),
                          int(id2%1000) ) );
   }
-  if( name == "EE_readout_tower" ) {
+  if( strcmp(name.c_str(), "EE_readout_tower") == 0 ) {
     return( EcalLogicID( "EE_readout_tower",
                          2000000000+100*((id1>=1&&id1<=9)?(646+(id1-1)):(601+(id1-9)))+id2,
                          ((id1>=1&&id1<=9)?(646+(id1-1)):(601+(id1-9))),
                          id2 ) );
   }
-  if( name == "EE_mem_channel" ) {
+  if( strcmp(name.c_str(), "EE_mem_channel") == 0 ) {
     return( EcalLogicID( "EE_mem_channel", EcalLogicID::NULLID ) );
   }
-  if( name == "EE_mem_TT" ) {
+  if( strcmp(name.c_str(), "EE_mem_TT") == 0 ) {
     return( EcalLogicID( "EE_mem_TT", EcalLogicID::NULLID ) );
   }
-  if( name == "EE_LM_PN" ) {
+  if( strcmp(name.c_str(), "EE_LM_PN") == 0 ) {
     return( EcalLogicID( "EE_mem_TT", EcalLogicID::NULLID ) );
   }
 
