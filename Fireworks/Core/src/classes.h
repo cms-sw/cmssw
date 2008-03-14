1e1aa653832e31e7a1cdb469a8fbd30f09cdfb44
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-03-14 03:21:32 +0000

    added KeyValues dictionary

--- Fireworks/Core/src/classes.h
+++ Fireworks/Core/src/classes.h
@@ -42,5 +42,6 @@ namespace {
    struct Fireworks_Core {
       //add 'dummy' Wrapper variable for each class type you put into the Event
      //FWDisplayEvent de;
+      FWConfiguration::KeyValues kv;
    };
 }
