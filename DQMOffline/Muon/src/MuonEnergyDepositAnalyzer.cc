99f8e3447d35fe028a6654bed3a35333910775e7
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-03-18 11:59:59 +0000

    DQM monitoring source for muon energy deposits - cout to fix

---
+++ DQMOffline/Muon/src/MuonEnergyDepositAnalyzer.cc
@@ -0,0 +1,117 @@
+
+/*
+ *  See header file for a description of this class.
+ *
+ *  $Date: 2008/03/13 11:11:12 $
+ *  $Revision: 1.1 $
+ *  \author G. Mila - INFN Torino
+ */
+
+#include "DQMOffline/Muon/src/MuonEnergyDepositAnalyzer.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h" 
+#include "DataFormats/MuonReco/interface/MuonEnergy.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <string>
+using namespace std;
+using namespace edm;
+
+
+
+MuonEnergyDepositAnalyzer::MuonEnergyDepositAnalyzer(const edm::ParameterSet& pSet) {
+
+  cout<<"[MuonEnergyDepositAnalyzer] Constructor called!"<<endl;
+  parameters = pSet;
+  // Set the verbosity
+  debug = parameters.getParameter<bool>("debug");
+}
+
+
+MuonEnergyDepositAnalyzer::~MuonEnergyDepositAnalyzer() { }
+
+
+void MuonEnergyDepositAnalyzer::beginJob(edm::EventSetup const& iSetup, DaqMonitorBEInterface * dbe) {
+
+  cout<<"[MuonEnergyDepositAnalyzer] Parameters initialization"<<endl;
+  metname = "muEnergyDeposit";
+  dbe->setCurrentFolder("Muons/MuonEnergyDepositAnalyzer");
+  std::string AlgoName = parameters.getParameter<std::string>("AlgoName");
+
+  emNoBin = parameters.getParameter<int>("emSizeBin");
+  emNoMin = parameters.getParameter<double>("emSizeMin");
+  emNoMax = parameters.getParameter<double>("emSizeMax");
+  std::string histname = "ecalDepositedEnergy_";
+  ecalDepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, emNoBin, emNoMin, emNoMax);
+
+  emS9NoBin = parameters.getParameter<int>("emS9SizeBin");
+  emS9NoMin = parameters.getParameter<double>("emS9SizeMin");
+  emS9NoMax = parameters.getParameter<double>("emS9SizeMax");
+  histname = "ecalS9DepositedEnergy_";
+  ecalS9DepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, emS9NoBin, emS9NoMin, emS9NoMax);
+  
+  hadNoBin = parameters.getParameter<int>("hadSizeBin");
+  hadNoMin = parameters.getParameter<double>("hadSizeMin");
+  hadNoMax = parameters.getParameter<double>("hadSizeMax");
+  histname = "hadDepositedEnergy_";
+  hcalDepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, hadNoBin, hadNoMin, hadNoMax);
+
+  hadS9NoBin = parameters.getParameter<int>("hadS9SizeBin");
+  hadS9NoMin = parameters.getParameter<double>("hadS9SizeMin");
+  hadS9NoMax = parameters.getParameter<double>("hadS9SizeMax");
+  histname = "hadS9DepositedEnergy_";
+  hcalS9DepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, hadS9NoBin, hadS9NoMin, hadS9NoMax);
+
+  hoNoBin = parameters.getParameter<int>("hoSizeBin");
+  hoNoMin = parameters.getParameter<double>("hoSizeMin");
+  hoNoMax = parameters.getParameter<double>("hoSizeMax");
+  histname = "hoDepositedEnergy_";
+  hoDepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, hoNoBin, hoNoMin, hoNoMax);
+
+  hoS9NoBin = parameters.getParameter<int>("hoS9SizeBin");
+  hoS9NoMin = parameters.getParameter<double>("hoS9SizeMin");
+  hoS9NoMax = parameters.getParameter<double>("hoS9SizeMax");
+  histname = "hoS9DepositedEnergy_";
+  hoS9DepEnergy = dbe->book1D(histname+AlgoName, histname+AlgoName, hoS9NoBin, hoS9NoMin, hoS9NoMax);
+
+}
+
+
+
+void MuonEnergyDepositAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup, const reco::Muon& recoMu) {
+
+  cout<<"[MuonEnergyDepositAnalyzer] Filling the histos"<<endl;
+
+  // get all the mu energy deposits
+  reco::MuonEnergy muEnergy = recoMu.getCalEnergy();
+  
+  // energy deposited in ECAL
+  cout << "Energy deposited in ECAL: "<<muEnergy.em;
+  ecalDepEnergy->Fill(muEnergy.em);
+  
+  // energy deposited in HCAL
+  cout << "Energy deposited in ECAL: "<<muEnergy.had;
+  hcalDepEnergy->Fill(muEnergy.had);
+  
+  // energy deposited in HO
+  cout << "Energy deposited in ECAL: "<<muEnergy.ho;
+  hoDepEnergy->Fill(muEnergy.ho);
+  
+  // energy deposited in ECAL in 3*3 towers
+  cout << "Energy deposited in ECAL: "<<muEnergy.emS9;
+  ecalS9DepEnergy->Fill(muEnergy.emS9);
+     
+  // energy deposited in HCAL in 3*3 crystals
+  cout << "Energy deposited in ECAL: "<<muEnergy.hadS9;
+  hcalS9DepEnergy->Fill(muEnergy.hadS9);
+  
+  // energy deposited in HO in 3*3 crystals
+  cout << "Energy deposited in ECAL: "<<muEnergy.hoS9;
+  hoS9DepEnergy->Fill(muEnergy.hoS9);
+  
+}
+
+
