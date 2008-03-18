ee89a80b20836a7b557bc1c1ad04bd0f1bb893f4
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-03-18 12:03:55 +0000

    new class hierarchy

--- DQMOffline/Muon/src/SealModule.cc
+++ DQMOffline/Muon/src/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "DQMOffline/Muon/src/MuDepositEnergyMonitoring.h"
+#include "DQMOffline/Muon/src/MuonAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(MuDepositEnergyMonitoring);
+DEFINE_ANOTHER_FWK_MODULE(MuonAnalyzer);
