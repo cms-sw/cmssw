60ce5f0b74e4317cc9ab4be9e6c0465b6dc25c00
Salvatore Di Guida <sha1-744703a2e531bf35de4c8c886b1940546bb70d35@cern.ch>
2008-03-19 13:23:43 +0000

    new function

---
+++ PhysicsTools/Utilities/interface/Number.h
@@ -0,0 +1,14 @@
+#ifndef PhysicsTools_Utilities_Number_h
+#define PhysicsTools_Utilities_Number_h
+
+namespace function {
+  struct Number {
+    static const unsigned int arguments = 1;
+    Number(double value) : value_(value) { }
+    double operator()(double x) const { return value_; }
+  private:
+    double value_;
+  };
+}
+
+#endif
