8439f18ee0836c450969230fbef8d56417391833
Raffaello Trentadue <sha1-9504fe7d3bbc4a07e3333b20772b63251c823b73@cern.ch>
2008-03-19 10:39:17 +0000

    data

--- Validation/MuonRPCDigis/src/SealModule.cc
+++ Validation/MuonRPCDigis/src/SealModule.cc
@@ -1,8 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "Validation/MuonRPCDigis/src/RPCDigiQuality.h"
-
+#include "Validation/MuonRPCDigis/interface/RPCDigiValid.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(RPCDigiQuality);
+DEFINE_ANOTHER_FWK_MODULE(RPCDigiValid);
