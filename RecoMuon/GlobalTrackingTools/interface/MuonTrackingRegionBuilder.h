c6c7ee525709a70e46614975469095ab9ccf0e36
Norbert Neumeister <sha1-7abc839d53ede37ea0c445f89ed7dc2d61338f2b@cern.ch>
2008-03-20 18:18:55 +0000

    use InputTag for vertex collection

--- RecoMuon/GlobalTrackingTools/interface/MuonTrackingRegionBuilder.h
+++ RecoMuon/GlobalTrackingTools/interface/MuonTrackingRegionBuilder.h
@@ -2,75 +2,76 @@
 #define RecoMuon_TrackingTools_MuonTrackingRegionBuilder_H
 
 /** \class MuonTrackingRegionBuilder
- *  Base class for the Muon reco TrackingRegion Builder
  *
- *  $Date: 2008/02/29 22:09:26 $
- *  $Revision: 1.5 $
+ *  Build a TrackingRegion around a standalone muon
+ *
+ *
+ *  $Date: 2008/03/05 21:12:54 $
+ *  $Revision: 1.6 $
+ *
  *  \author A. Everett - Purdue University
  *  \author A. Grelli -  Purdue University, Pavia University 
  */
 
-#include "DataFormats/Common/interface/Handle.h"
-
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
-#include "DataFormats/GeometryCommonDetAlgo/interface/GlobalError.h"
-
 #include "DataFormats/BeamSpot/interface/BeamSpot.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
-
-namespace edm {class Event;}
 
 class MuonServiceProxy;
 class RectangularEtaPhiTrackingRegion;
 
-//namespace reco {class Track; class TrackRef;}
+namespace edm {class ParameterSet; class Event;}
+
+//              ---------------------
+//              -- Class Interface --
+//              ---------------------
 
 class MuonTrackingRegionBuilder {
   
- public:
+  public:
+ 
+    /// constructor
+    MuonTrackingRegionBuilder(const edm::ParameterSet&, 
+                              const MuonServiceProxy*);
   
-  /// constructor
-  MuonTrackingRegionBuilder(const edm::ParameterSet&, const MuonServiceProxy*);
+    /// destructor
+    virtual ~MuonTrackingRegionBuilder() {}
   
-  /// destructor
-  virtual ~MuonTrackingRegionBuilder() {}
+    /// define tracking region
+    RectangularEtaPhiTrackingRegion* region(const reco::TrackRef&) const;
+
+    /// define tracking region
+    RectangularEtaPhiTrackingRegion* region(const reco::Track&) const;
+
+    /// pass the Event to the algo at each event
+    virtual void setEvent(const edm::Event&);
+
+  private:
+
+    edm::InputTag theBeamSpotTag;   // beam spot
+    edm::InputTag theVertexCollTag; // vertex collection
+
+    const edm::Event* theEvent;
+    const MuonServiceProxy* theService;
+
+    bool useFixedRegion;
+    bool useVertex;
+
+    double theTkEscapePt;
+    double theNsigmaEta,theNsigmaDz,theNsigmaPhi;
   
-  RectangularEtaPhiTrackingRegion* region(const reco::TrackRef&) const;
+    double theEtaRegionPar1; 
+    double theEtaRegionPar2;
+    double thePhiRegionPar1;
+    double thePhiRegionPar2;
 
-  RectangularEtaPhiTrackingRegion* region(const reco::Track&) const;
+    double thePhiMin;
+    double theEtaMin;
+    double theDeltaR,theHalfZ;
+    double thePhiFixed,theEtaFixed;
 
-  /// pass the Event to the algo at each event
-  virtual void setEvent(const edm::Event&);
+    GlobalPoint theVertexPos;
 
-
- private:
-
-  edm::InputTag theBeamSpotTag; //beam spot
-  std::string vertexCollName;   // Vertexing
-
-  const edm::Event* theEvent;
-  const MuonServiceProxy * theService;
-
-  bool    theFixedFlag,EnableBeamSpot,usePixelVertex;
-  double  TkEscapePt;
-  double  Nsigma_eta,Nsigma_Dz,Nsigma_phi ;
-  
-  double  Eta_Region_parameter1; 
-  double  Eta_Region_parameter2;
-  double  Phi_Region_parameter1;
-  double  Phi_Region_parameter2;
-
-  double  Phi_minimum,Eta_minimum;
-  double  Delta_R_Region,HalfZRegion_size;
-  double  Phi_fixed,Eta_fixed;
-
-  GlobalPoint theVertexPos;
-  GlobalPoint vertexPosiBS;
 };
 #endif
--- RecoMuon/GlobalTrackingTools/src/MuonTrackingRegionBuilder.cc
+++ RecoMuon/GlobalTrackingTools/src/MuonTrackingRegionBuilder.cc
@@ -1,199 +1,216 @@
-/** \class MuonTrackingRegionBuilder
- *  Base class for the Muon reco TrackingRegion Builder
+/** 
+ *  Class: MuonTrackingRegionBuilder
  *
- *  $Date: 2008/02/29 22:09:30 $
- *  $Revision: 1.6 $
+ *  Build a TrackingRegion around a standalone muon 
+ *
+ *  $Date: 2008/03/05 21:12:55 $
+ *  $Revision: 1.7 $
+ *
  *  \author A. Everett - Purdue University
-    \author A. Grelli -  Purdue University, Pavia University
+ *  \author A. Grelli -  Purdue University, Pavia University
  */
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "RecoMuon/GlobalTrackingTools/interface/MuonTrackingRegionBuilder.h"
 
-#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
-#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
+//---------------
+// C++ Headers --
+//---------------
 
-#include "RecoTracker/TkTrackingRegions/interface/GlobalTrackingRegion.h"
+
+//-------------------------------
+// Collaborating Class Headers --
+//-------------------------------
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
 #include "RecoTracker/TkTrackingRegions/interface/RectangularEtaPhiTrackingRegion.h"
 #include "TrackingTools/TrajectoryState/interface/TrajectoryStateTransform.h"
 #include "TrackingTools/PatternTools/interface/TSCPBuilderNoMaterial.h"
-
-//Beam Spot option & vertexing
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 
 using namespace std;
 
-/// constructor
-MuonTrackingRegionBuilder::MuonTrackingRegionBuilder(const edm::ParameterSet& par, const MuonServiceProxy* service)
-  : theService(service)
-  {
+//
+// constructor
+//
+MuonTrackingRegionBuilder::MuonTrackingRegionBuilder(const edm::ParameterSet& par, 
+                                                     const MuonServiceProxy* service) :
+ theService(service) {
 
-  // Vertex Collection and Beam Spot
+  // vertex Collection and Beam Spot
   theBeamSpotTag = par.getParameter<edm::InputTag>("beamSpot");
+  theVertexCollTag = par.getParameter<edm::InputTag>("vertexCollection");
 
-  // Parmeters
-  Nsigma_eta     = par.getParameter<double>("Rescale_eta");
-  Nsigma_phi     = par.getParameter<double>("Rescale_phi");
-  Nsigma_Dz      = par.getParameter<double>("Rescale_Dz");
+  // parmeters
+  theNsigmaEta  = par.getParameter<double>("Rescale_eta");
+  theNsigmaPhi  = par.getParameter<double>("Rescale_phi");
+  theNsigmaDz   = par.getParameter<double>("Rescale_Dz");
+  theTkEscapePt = par.getParameter<double>("EscapePt");
 
-  TkEscapePt     = par.getParameter<double>("EscapePt");
+  // upper limits parameters   
+  theEtaRegionPar1 = par.getParameter<double>("EtaR_UpperLimit_Par1"); 
+  theEtaRegionPar2 = par.getParameter<double>("EtaR_UpperLimit_Par2");
+  thePhiRegionPar1 = par.getParameter<double>("PhiR_UpperLimit_Par1");
+  thePhiRegionPar2 = par.getParameter<double>("PhiR_UpperLimit_Par2");
 
-  //Upper Limits Parameters   
-  Eta_Region_parameter1 = par.getParameter<double>("EtaR_UpperLimit_Par1"); 
-  Eta_Region_parameter2 = par.getParameter<double>("EtaR_UpperLimit_Par2");
-  Phi_Region_parameter1 = par.getParameter<double>("PhiR_UpperLimit_Par1");
-  Phi_Region_parameter2 = par.getParameter<double>("PhiR_UpperLimit_Par2");
+  useVertex = par.getParameter<bool>("UseVertex");
+  useFixedRegion = par.getParameter<bool>("UseFixedRegion");
 
-  usePixelVertex = par.getParameter<bool>("UseVertex");
-  //Fixed limits
-  theFixedFlag     = par.getParameter<bool>("UseFixedRegion");
-  Phi_minimum      = par.getParameter<double>("Phi_min");
-  Eta_minimum      = par.getParameter<double>("Eta_min");
-  HalfZRegion_size = par.getParameter<double>("DeltaZ_Region");
-  Delta_R_Region   = par.getParameter<double>("DeltaR");
+  // fixed limits
+  thePhiFixed = par.getParameter<double>("Phi_fixed");
+  theEtaFixed = par.getParameter<double>("Eta_fixed");
 
-  //Perigee Reference point
+  thePhiMin = par.getParameter<double>("Phi_min");
+  theEtaMin = par.getParameter<double>("Eta_min");
+  theHalfZ  = par.getParameter<double>("DeltaZ_Region");
+  theDeltaR = par.getParameter<double>("DeltaR");
+
+  // perigee reference point
   theVertexPos = GlobalPoint(0.0,0.0,0.0);
+
 }
 
-RectangularEtaPhiTrackingRegion* MuonTrackingRegionBuilder::region(const reco::TrackRef& track) const//, const reco::BeamSpot& beamSpot) const
-{
+
+//
+//
+//
+RectangularEtaPhiTrackingRegion* 
+MuonTrackingRegionBuilder::region(const reco::TrackRef& track) const {
+
   return region(*track);
+
 }
 
+
+//
+//
+//
 void MuonTrackingRegionBuilder::setEvent(const edm::Event& event) {
   
   theEvent = &event;
+
 }
 
 
-RectangularEtaPhiTrackingRegion* MuonTrackingRegionBuilder::region(const reco::Track& staTrack) const//, const reco::BeamSpot& beamSpot) const
-{
+//
+//
+//
+RectangularEtaPhiTrackingRegion* 
+MuonTrackingRegionBuilder::region(const reco::Track& staTrack) const {
+
+  // get the free trajectory state of the muon updated at vertex
   TSCPBuilderNoMaterial tscpBuilder; 
-  //Get muon free state updated at vertex
   TrajectoryStateTransform tsTransform;
   FreeTrajectoryState muFTS = tsTransform.initialFreeState(staTrack,&*theService->magneticField());
    
-  //Get track direction at vertex
+  // get track direction at vertex
   GlobalVector dirVector(muFTS.momentum());
 
-  //Get track momentum
+  // get track momentum
   const math::XYZVector& mo = staTrack.innerMomentum();
   GlobalVector mom(mo.x(),mo.y(),mo.z());
   if ( staTrack.p() > 1.5 ) {
     mom = dirVector; 
   }
 
-  // Inizial vertex position (in the following replaced with beam spot/vertexing)
+  // initial vertex position -  in the following it is replaced with beam spot/vertexing
   GlobalPoint vertexPos(0.0,0.0,0.0);
+  double deltaZatVTX = 0.0;
 
-  double TheDeltaZatVTX = 0;
-
-  //retrieve beam spot information
+  // retrieve beam spot information
   edm::Handle<reco::BeamSpot> bsHandle;
   bool bsHandleFlag = theEvent->getByLabel(theBeamSpotTag, bsHandle);
-  //cechk the validity otherwise vertexing
-  if(bsHandleFlag && !usePixelVertex) {
-    const reco::BeamSpot & bs = *bsHandle;
-    GlobalPoint vertexPosBS(bs.x0(), bs.y0(), bs.z0());
-    vertexPos = vertexPosBS;
-  }else{
-  // Get originZPos from list of vertices (first or all)
+  // check the validity, otherwise vertexing
+  if ( bsHandleFlag && !useVertex ) {
+    const reco::BeamSpot& bs = *bsHandle;
+    vertexPos = GlobalPoint(bs.x0(), bs.y0(), bs.z0());
+  } else {
+    // get originZPos from list of reconstructed vertices (first or all)
     edm::Handle<reco::VertexCollection> vertexCollection;
-    bool vtxHandleFlag = theEvent->getByLabel(vertexCollName,vertexCollection);
-  // ceck if exsist a non empty vertex collection
-    if(vtxHandleFlag && vertexCollection->size() > 0) {
-     reco::VertexCollection::const_iterator Vtx=vertexCollection->begin();// ! only the first low lomi option
-     // const Vtx = vertexCollection->front();
-      double TheZPosition = Vtx->z();
-
-      GlobalPoint vertexPosVT(0.0,0.0,TheZPosition);
-      vertexPos = vertexPosVT;
-
+    bool vtxHandleFlag = theEvent->getByLabel(theVertexCollTag,vertexCollection);
+    // check if there exists at least one reconstructed vertex
+    if ( vtxHandleFlag && !vertexCollection->empty() ) {
+      // use the first vertex in the collection and assume it is the primary event vertex 
+      reco::VertexCollection::const_iterator vtx = vertexCollection->begin();
+      vertexPos = GlobalPoint(0.0,0.0,vtx->z());
       // delta Z from vertex error
-      TheDeltaZatVTX = (Vtx->zError())*Nsigma_Dz;
-
-     }
-   }
-
+      deltaZatVTX = vtx->zError() * theNsigmaDz;
+    }
+  }
 
   TrajectoryStateClosestToPoint tscp = tscpBuilder(muFTS,theVertexPos);
+  const PerigeeTrajectoryError& covar = tscp.perigeeError();
+  const PerigeeTrajectoryParameters& param = tscp.perigeeParameters();
 
-  const PerigeeTrajectoryError & covar = tscp.perigeeError();
-  const PerigeeTrajectoryParameters & param = tscp.perigeeParameters();
-
-  //calculate deltaEta from deltaTheta
+  // calculate deltaEta from deltaTheta
   double deltaTheta = covar.thetaError();
   double theta      = param.theta();
   double sin_theta  = sin(theta);
 
-  //Get dEta and dPhi
-  double deta = Nsigma_eta*(1/fabs(sin_theta))*deltaTheta;
-  double dphi = Nsigma_phi*(covar.phiError());
-
+  // get dEta and dPhi
+  double deta = theNsigmaEta*(1/fabs(sin_theta))*deltaTheta;
+  double dphi = theNsigmaPhi*(covar.phiError());
 
   /* Region_Parametrizations to take into account possible 
-     L2 matrix inconsistencies. Detailed Explanation in TWIKI
+     L2 error matrix inconsistencies. Detailed Explanation in TWIKI
      page.
   */
   double region_dEta = 0;
   double region_dPhi = 0;
+  double eta,phi;
 
-  double region_dEta1,region_dPhi1,Par_eta,Par_phi;
-  float  acoeff1_Phi,acoeff1_Eta,acoeff3_Phi,acoeff3_Eta;
+  // eta, pt parametrization from MC study
+  float pt = abs(mom.perp());
+  if ( pt <= 10. ) {
+     // angular coefficients
+     float acoeff_Phi = (thePhiRegionPar2 - thePhiRegionPar1)/5;
+     float acoeff_Eta = (theEtaRegionPar2 - theEtaRegionPar1)/5;
 
-  // Eta , ptparametrization as in MC study
-   if(abs(mom.perp())<=10.){
-     // angolar coefficients
-     acoeff1_Phi = (Phi_Region_parameter2-Phi_Region_parameter1)/5;
-     acoeff1_Eta = (Eta_Region_parameter2-Eta_Region_parameter1)/5;
+     eta = theEtaRegionPar1 + (acoeff_Eta)*(mom.perp()-5.);
+     phi = thePhiRegionPar1 + (acoeff_Phi)*(mom.perp()-5.) ;
+   }
+   // parametrization 2nd bin in pt from MC study  
+   if ( pt > 10. && pt < 100. ) {
+     eta = theEtaRegionPar2;
+     phi = thePhiRegionPar2;
+   }
+   // parametrization 3rd bin in pt from MC study
+   if ( pt >= 100. ) {
+     // angular coefficients
+     float acoeff_Phi = (thePhiRegionPar1 - thePhiRegionPar2)/900;
+     float acoeff_Eta = (theEtaRegionPar1 - theEtaRegionPar2)/900;
 
-     Par_eta  = Eta_Region_parameter1 + (acoeff1_Eta)*(mom.perp()-5.);
-     Par_phi  = Phi_Region_parameter1 + (acoeff1_Phi)*(mom.perp()-5.) ;
+     eta = theEtaRegionPar2 + (acoeff_Eta)*(mom.perp()-100.);
+     phi = thePhiRegionPar2 + (acoeff_Phi)*(mom.perp()-100.);
    }
 
-   //Parametrization 2nd bin in pt from MC study  
-   if(abs(mom.perp())>10. && abs(mom.perp())<100.){
-
-     Par_eta = Eta_Region_parameter2;
-     Par_phi = Phi_Region_parameter2;
-   }
-   //Parametrization 3rd bin in pt from MC study
-   if(abs(mom.perp())>=100.){
-     // angolar coefficients
-     acoeff3_Phi = (Phi_Region_parameter1-Phi_Region_parameter2)/900;
-     acoeff3_Eta = (Eta_Region_parameter1-Eta_Region_parameter2)/900;
-
-     Par_eta = Eta_Region_parameter2 + (acoeff3_Eta)*(mom.perp()-100.);
-     Par_phi = Phi_Region_parameter2 + (acoeff3_Phi)*(mom.perp()-100.);
-   }
-
-  // here decide to use parametrization or dinamical region
-  region_dPhi1 = min(Par_phi,dphi);
-  region_dEta1 = min(Par_eta,deta);
+  // decide to use either a parametrization or a dynamical region
+  double region_dPhi1 = min(phi,dphi);
+  double region_dEta1 = min(eta,deta);
 
   // minimum size
-  region_dPhi = max(Phi_minimum,region_dPhi1);
-  region_dEta = max(Eta_minimum,region_dEta1);
+  region_dPhi = max(thePhiMin,region_dPhi1);
+  region_dEta = max(theEtaMin,region_dEta1);
 
-  float deltaZ=0;
-  //standard 15.9 is useVertex than region from vertexing
-  if(usePixelVertex){
-    deltaZ  = TheDeltaZatVTX;
-  } else { deltaZ = HalfZRegion_size; }
+  float deltaZ = 0.0;
+  // standard 15.9 is useVertex than region from vertexing
+  if ( useVertex ) {
+    deltaZ = deltaZatVTX;
+  } else { 
+    deltaZ = theHalfZ;
+  }
 
-  float deltaR  = Delta_R_Region;
-  double minPt   = max(TkEscapePt,mom.perp()*0.6);
+  float deltaR = theDeltaR;
+  double minPt = max(theTkEscapePt,mom.perp()*0.6);
 
-  RectangularEtaPhiTrackingRegion * region = 0;  
+  RectangularEtaPhiTrackingRegion* region = 0;  
 
-
-  if(theFixedFlag) {
-     region_dEta = Eta_fixed;
-     region_dPhi = Phi_fixed;
+  if (useFixedRegion) {
+     region_dEta = theEtaFixed;
+     region_dPhi = thePhiFixed;
   }
 
   region = new RectangularEtaPhiTrackingRegion(dirVector, vertexPos,
@@ -202,7 +219,4 @@ RectangularEtaPhiTrackingRegion* MuonTrackingRegionBuilder::region(const reco::T
   
   return region;
   
-
-
 }
-
