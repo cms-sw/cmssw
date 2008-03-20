04c0045673860c84a9d841fb272199aa8f66b43f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-03-20 16:38:10 +0000

    revert head to correct revisions

--- EventFilter/GctRawToDigi/plugins/GctRawToDigi.h
+++ EventFilter/GctRawToDigi/plugins/GctRawToDigi.h
@@ -16,10 +16,9 @@
 //
 // Original Author:  Jim Brooke
 //         Created:  Wed Nov  1 11:57:10 CET 2006
-// $Id: GctRawToDigi.h,v 1.10.2.1 2008/01/16 22:01:04 frazier Exp $
+// $Id: GctRawToDigi.h,v 1.18 2008/03/19 20:27:28 frazier Exp $
 //
 //
-
 
 // system include files
 #include <memory>
@@ -33,43 +32,55 @@
 
 #include "DataFormats/FEDRawData/interface/FEDRawData.h"
 
-#include "EventFilter/GctRawToDigi/src/GctBlockUnpacker.h"
+#include "EventFilter/GctRawToDigi/src/GctBlockUnpackerBase.h"
 
-//
-// class decleration
-//
 
-class GctRawToDigi : public edm::EDProducer {
- public:
+// *******************************************************************
+// ***  THE UNPACK PROCESS MUST NEVER THROW ANY KIND OF EXCEPTION! *** 
+// *******************************************************************
+
+class GctRawToDigi : public edm::EDProducer
+{
+public:
+
   explicit GctRawToDigi(const edm::ParameterSet&);
   ~GctRawToDigi();
+
   
- private: // methods
+private: // methods
+
   virtual void beginJob(const edm::EventSetup&) ;
   virtual void produce(edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
   
-  void unpack(const FEDRawData& d, edm::Event& e, const bool invalidDataFlag);
+  /// Unpacks the raw data
+  /*! \param invalidDataFlag - if true, then won't attempt unpack but just output empty collecions. */
+  void unpack(const FEDRawData& d, edm::Event& e, const bool invalidDataFlag=false);
 
- private: // members
 
-  static const unsigned MAX_BLOCKS;
+private: // members
+
+  /// The maximum number of blocks we will try to unpack before thinking something is wrong
+  static const unsigned MAX_BLOCKS = 256;
 
   edm::InputTag inputLabel_;  // FED collection label.
   int fedId_;                 // GCT FED ID.
-  bool verbose_;              // If true, then debug print out for each event.
+  const bool verbose_;        // If true, then debug print out for each event.
 
   // unpacking options
-  bool doEm_;
-  bool doJets_;
-  bool doEtSums_;
-  bool doInternEm_;
-  bool doRct_;
-  bool doFibres_;
+  const bool hltMode_;  // If true, only outputs the GT output data, and only BX = 0.
+  const bool grenCompatibilityMode_;  // If true, use old-style (GREN07 era) block headers & pipe format.
+  const bool doEm_;
+  const bool doJets_;
+  const bool doEtSums_;
+  const bool doInternEm_;
+  const bool doRct_;
+  const bool doFibres_;
 
   // Block to Digi converter
-  GctBlockUnpacker blockUnpacker_;
+  GctBlockUnpackerBase * blockUnpacker_;
 
+  unsigned unpackFailures_;
 };
 
 #endif
