a19667324a3882f535a52110c6f2179be32f1abf
Antonio Pierro <sha1-517165799901f315cb4eddd2c0b58182648a0767@cern.ch>
2008-03-25 20:55:44 +0000

    add method to iterate collection of vector

--- CondFormats/SiStripObjects/interface/SiStripPedestals.h
+++ CondFormats/SiStripObjects/interface/SiStripPedestals.h
@@ -45,6 +45,48 @@ class SiStripPedestals {
 		RegistryIterator getRegistryVectorBegin() const {return indexes.begin();}
 		RegistryIterator getRegistryVectorEnd()   const{return indexes.end();}
 
+		void  setData(float ped, float lth, float hth, std::vector<char>& vped);
+		float getPed   (const uint16_t& strip, const Range& range) const;
+		float getLowTh (const uint16_t& strip, const Range& range) const;
+		float getHighTh(const uint16_t& strip, const Range& range) const;
+
+	private:
+		std::vector<char> v_pedestals; //@@@ blob streaming doesn't work with uint16_t and with SiStripData::Data
+		std::vector<DetRegistry> indexes;
+		struct DecodingStructure{  
+			unsigned int lth :6;
+			unsigned int hth :6;
+			unsigned int ped :10;
+		};
+
+		struct DetRegistry{
+			uint32_t detid;
+			uint32_t ibegin;
+			uint32_t iend;
+		};
+
+		class StrictWeakOrdering{
+			public:
+				bool operator() (const DetRegistry& p,const uint32_t& i) const {return p.detid < i;}
+		};
+
+		typedef std::vector<char>::const_iterator                ContainerIterator;  
+		typedef std::pair<ContainerIterator, ContainerIterator>  Range;      
+		typedef std::vector<DetRegistry>                         Registry;
+		typedef Registry::const_iterator                         RegistryIterator;
+
+		SiStripPedestals(){};
+		~SiStripPedestals(){};
+
+		bool  put(const uint32_t& detID,Range input);
+		const Range getRange(const uint32_t& detID) const;
+		void  getDetIds(std::vector<uint32_t>& DetIds_) const;
+
+		ContainerIterator getDataVectorBegin()    const {return v_pedestals.begin();}
+		ContainerIterator getDataVectorEnd()      const {return v_pedestals.end();}
+		RegistryIterator getRegistryVectorBegin() const {return indexes.begin();}
+		RegistryIterator getRegistryVectorEnd()   const{return indexes.end();}
+
 
 		void  setData(float ped, float lth, float hth, std::vector<char>& vped);
 		float getPed   (const uint16_t& strip, const Range& range) const;
