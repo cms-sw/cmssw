e2a786aa12aa2185d9275593bd16d8a91d87cf57
Antonio Pierro <sha1-517165799901f315cb4eddd2c0b58182648a0767@cern.ch>
2008-03-26 10:55:28 +0000

    delete method to iterate collection of vector

--- CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h
+++ CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h
@@ -9,33 +9,20 @@
 
 class SiStripLorentzAngle {
 
- public:
- 
-  struct DetRegistry{
-    uint32_t detid;
-    uint32_t ibegin;
-    uint32_t iend;
-  };
+	public:
 
-  SiStripLorentzAngle(){};
-  ~SiStripLorentzAngle(){};
+		SiStripLorentzAngle(){};
+		~SiStripLorentzAngle(){};
 
-  inline void putLorentsAngles(std::map<unsigned int,float>& LA){m_LA=LA;}   
-  inline const std::map<unsigned int,float>&  getLorentzAngles () const {return m_LA;}
+		inline void putLorentsAngles(std::map<unsigned int,float>& LA){m_LA=LA;}   
+		inline const std::map<unsigned int,float>&  getLorentzAngles () const {return m_LA;}
 
-  bool   putLorentzAngle(const uint32_t&, float&);
-  const float&  getLorentzAngle (const uint32_t&) const;
-
-  ContainerIterator getDataVectorBegin()    const {return v_LA.begin();}
-  ContainerIterator getDataVectorEnd()      const {return v_LA.end();}
-  RegistryIterator getRegistryVectorBegin() const {return indexes.begin();}
-  RegistryIterator getRegistryVectorEnd()   const{return indexes.end();}
+		bool   putLorentzAngle(const uint32_t&, float&);
+		const float&  getLorentzAngle (const uint32_t&) const;
 
 
- private:
-  std::map<unsigned int,float> m_LA; 
-  std::vector<float> v_LA; 
-  std::vector<DetRegistry> indexes;
+	private:
+		std::map<unsigned int,float> m_LA; 
 };
 
 #endif
