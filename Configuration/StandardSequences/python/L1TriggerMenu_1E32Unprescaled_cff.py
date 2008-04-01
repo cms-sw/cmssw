a9ed914b5721ad508b34eb5cfb67887071908e60
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-04-01 12:48:49 +0000

    added python config for recent L1 additions

---
+++ Configuration/StandardSequences/python/L1TriggerMenu_1E32Unprescaled_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1x1032.L1Menu2007_Unprescaled_cff import *
+
+
---
+++ Configuration/StandardSequences/python/L1Unprescale_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1TriggerConfig.L1GtConfigProducers.L1GtFactorsUnprescale_cff import *
+
+
