c0fab5a96f6d10e19e1622317891a696f8bd5703
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-01 08:53:00 +0000

    Updated for new DB, improved logging.

--- EventFilter/StorageManager/scripts/perl/insertFile.pl
+++ EventFilter/StorageManager/scripts/perl/insertFile.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-# Created by Markus Klute on 2007 Jan 24.
 # $Id:$
 ################################################################################
 
@@ -7,10 +6,7 @@ use strict;
 use Getopt::Long;
 use DBI;
 
-################################################################################
-
 sub show_help {
-
   my $exit_status = shift@_;
   print " 
   ############################################################################## 
@@ -21,24 +17,42 @@ sub show_help {
 
   Syntax:
   ======= 
-  ./insertFile.pl  --RUNNUMBER 999999  --LUMISECTION 0  --INSTANCE 0  --COUNT 0  
-                   --START_TIME  1181833642  --STOP_TIME  1181834642 
-                   --FILENAME test.00999999.0000.A.test.0.0000.dat
-                   --PATHNAME /data1/ --HOSTNAME cmsdisk1
-                   --DATASET test --STREAM A  --STATUS closed  --TYPE streamer               
+  ./insertFile.pl  --RUNNUMBER 999999  --LUMISECTION 0  --INSTANCE 0  --COUNT 0 \
+                   --START_TIME  1181833642  --STOP_TIME  1181834642 \
+                   --FILENAME test.00999999.0000.A.test.0.0000.dat \
+                   --PATHNAME /data1/ --HOSTNAME cmsdisk1 \
+                   --DATASET test --STREAM A  --STATUS closed  --TYPE streamer \
                    --SAFETY 0  --NEVENTS 999  --FILESIZE 1024  --CHECKSUM 0 
-  Example:
-  ========
   
   ##############################################################################   
   \n";
   exit $exit_status;
 }
+
+sub getdatestr()
+{
+    my @ltime = localtime(time);
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = @ltime;
+    $year += 1900;
+    $mon++;
+
+    my $datestr=$year;
+    if ($mon < 10) {
+	$datestr=$datestr . "0";
+    }
+
+    $datestr=$datestr . $mon;
+    if ($mday < 10) {
+	$datestr=$datestr . "0";
+    }
+    $datestr=$datestr . $mday;
+    return $datestr;
+}
+
 ################################################################################
 
-open LOG, ">> /nfshome0/klute/globalRun-09-2007/log/insert.log";
-print LOG scalar localtime(time),' ',join(' ',@ARGV),"\n";
-close LOG;
+my $dolog = 1;
+my @MYARGV = @ARGV;
 
 my ($runnumber,$lumisection,$instance,$count,$stoptime,$filename,$pathname);
 my ($hostname,$dataset,$stream,$status,$type,$safety,$nevents,$filesize);
@@ -46,9 +60,6 @@ my ($starttime,$checksum);
 
 my ($CRC)      = ('0');
 my ($PRODUCER) = ('StorageManager');
-
-my $sender;
-
 
 # get options
 GetOptions(
@@ -71,17 +82,40 @@ GetOptions(
            "CHECKSUM=s"    => \$checksum
           );
 
+################################################################################
+
+#make sure domain is stripped
+my @harray = split(/\./,$hostname);
+$hostname = $harray[0];
+
+#overwrite TNS to be sure it points to new DB
+$ENV{'TNS_ADMIN'} = '/etc/tnsnames.ora';
+
+if ( $dolog == 1 )
+{
+    my $dstr = getdatestr();
+    my $ltime = localtime(time);
+    my $outfile = ">> /nfshome0/smdev/logs/" . $dstr . "-" . $hostname . ".log";
+    open LOG, $outfile;
+    print LOG "#--------------------\n";
+    print LOG scalar localtime(time),": insertFile.pl ",join(' ',@MYARGV),"\n";
+    close LOG;
+}
+
+################################################################################
+#exit 0;
 
 # connect to DB
-my $dbi    = "DBI:Oracle:omds";
-my $reader = "cms_sto_mgr";
+my $dbi    = "DBI:Oracle:cms_rcms";
+my $reader = "CMS_STOMGR_W";
 my $dbh    = DBI->connect($dbi,$reader,"qwerty");
  
 # do the update
-my $SQL = "INSERT INTO CMS_STO_MGR_ADMIN.TIER0_INJECTION (RUNNUMBER,LUMISECTION,INSTANCE,PRODUCER,PATHNAME,FILENAME,HOSTNAME,STREAM,DATASET,STATUS,NEVENTS,FILESIZE,START_TIME,STOP_TIME,CHECKSUM,CRC,SAFETY,COUNT,TYPE) VALUES ($runnumber,$lumisection,$instance,'$PRODUCER','$pathname','$filename','$hostname','$stream','$dataset','$status',$nevents,$filesize,'$starttime','$stoptime',$checksum,$CRC,$safety,$count,'$type')";
-my $sth = $dbh->do($SQL);
+my $SQL = "INSERT INTO CMS_STOMGR.TIER0_INJECTION (RUNNUMBER,LUMISECTION,INSTANCE,PRODUCER,PATHNAME,FILENAME,HOSTNAME,STREAM,DATASET,STATUS,NEVENTS,FILESIZE,START_TIME,STOP_TIME,CHECKSUM,CRC,SAFETY,COUNT,TYPE) VALUES ($runnumber,$lumisection,$instance,'$PRODUCER','$pathname','$filename','$hostname','$stream','$dataset','$status',$nevents,$filesize,'$starttime','$stoptime',$checksum,$CRC,$safety,$count,'$type')";
 
+my $sth = $dbh->do($SQL);
 # print $SQL;
+
 # Disconnect from DB
 $dbh->disconnect;
 
