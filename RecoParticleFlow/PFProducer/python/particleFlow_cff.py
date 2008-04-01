11afc823d6e48ceaf6a6a125776daff1d96bd2f9
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-04-01 08:33:27 +0000

    removed jet stuff

--- RecoParticleFlow/PFProducer/python/particleFlow_cff.py
+++ RecoParticleFlow/PFProducer/python/particleFlow_cff.py
@@ -7,28 +7,5 @@ from Geometry.CaloEventSetup.CaloGeometry_cfi import *
 # are not stored in the event!
 from RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cff import *
 from RecoParticleFlow.PFProducer.particleFlow_cfi import *
-# jet reconstruction --------------------------------------------------
-from RecoJets.Configuration.RecoJets_cff import *
-from RecoParticleFlow.PFProducer.particleFlowJetCandidates_cfi import *
-iterativeCone5PFJets = cms.EDProducer("IterativeConeJetProducer",
-    IconeJetParameters,
-    src = cms.InputTag("particleFlowJetCandidates"),
-    inputEtMin = cms.double(0.0),
-    coneRadius = cms.double(0.5),
-    jetType = cms.untracked.string('PFJet'),
-    inputEMin = cms.double(0.0),
-    towerThreshold = cms.double(0.5)
-)
-
-Fastjet10PFJets = cms.EDProducer("FastJetProducer",
-    FastjetParameters,
-    src = cms.InputTag("particleFlowJetCandidates"),
-    inputEtMin = cms.double(0.0),
-    inputEMin = cms.double(0.0),
-    FJ_ktRParam = cms.double(1.0),
-    jetType = cms.untracked.string('PFJet'),
-    towerThreshold = cms.double(0.5),
-    PtMin = cms.double(1.0)
-)
 
 
