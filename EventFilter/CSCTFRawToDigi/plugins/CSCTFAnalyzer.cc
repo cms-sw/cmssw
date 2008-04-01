1558d9be4ac2e47d269cb5fabd5c0396348faa88
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2008-04-01 10:02:20 +0000

    more nulls added

--- EventFilter/CSCTFRawToDigi/plugins/CSCTFAnalyzer.cc
+++ EventFilter/CSCTFRawToDigi/plugins/CSCTFAnalyzer.cc
@@ -43,41 +43,44 @@ void CSCTFAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& c){
 		}
 	}
 
-	edm::Handle<CSCCorrelatedLCTDigiCollection> corrlcts;
-	e.getByLabel(lctProducer.label(),lctProducer.instance(),corrlcts);
+	if( lctProducer.label() != "null" ){
+		edm::Handle<CSCCorrelatedLCTDigiCollection> corrlcts;
+		e.getByLabel(lctProducer.label(),lctProducer.instance(),corrlcts);
 
-	for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=corrlcts.product()->begin(); csc!=corrlcts.product()->end(); csc++){
-		int lctId=0;
+		for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=corrlcts.product()->begin(); csc!=corrlcts.product()->end(); csc++){
+			int lctId=0;
 
-		CSCCorrelatedLCTDigiCollection::Range range1 = corrlcts.product()->get((*csc).first);
-		for(CSCCorrelatedLCTDigiCollection::const_iterator lct=range1.first; lct!=range1.second; lct++,lctId++){
-			int station = (*csc).first.station()-1;
-			int cscId   = (*csc).first.triggerCscId()-1;
-			int sector  = (*csc).first.triggerSector()-1;// + ( (*csc).first.endcap()==1 ? 0 : 6 );
-			int subSector = CSCTriggerNumbering::triggerSubSectorFromLabels((*csc).first);
-			int tbin    = lct->getBX();
-			int fpga    = ( subSector ? subSector-1 : station+1 );
+			CSCCorrelatedLCTDigiCollection::Range range1 = corrlcts.product()->get((*csc).first);
+			for(CSCCorrelatedLCTDigiCollection::const_iterator lct=range1.first; lct!=range1.second; lct++,lctId++){
+				int station = (*csc).first.station()-1;
+				int cscId   = (*csc).first.triggerCscId()-1;
+				int sector  = (*csc).first.triggerSector()-1;// + ( (*csc).first.endcap()==1 ? 0 : 6 );
+				int subSector = CSCTriggerNumbering::triggerSubSectorFromLabels((*csc).first);
+				int tbin    = lct->getBX();
+				int fpga    = ( subSector ? subSector-1 : station+1 );
 
-			// If Det Id is within range
-			if( sector<0 || sector>11 || station<0 || station>3 || cscId<0 || cscId>8 || lctId<0 || lctId>1){
-				edm::LogInfo("CSCTFAnalyzer: CSC digi are out of range: ");
-				continue;
+				// If Det Id is within range
+				if( sector<0 || sector>11 || station<0 || station>3 || cscId<0 || cscId>8 || lctId<0 || lctId>1){
+					edm::LogInfo("CSCTFAnalyzer: CSC digi are out of range: ");
+					continue;
+				}
+
+				edm::LogInfo("CSCTFAnalyzer|print") << "  Front data   endcap: "<<(*csc).first.endcap()<<"  station: "<<(station+1)<<"  sector: "<<(sector+1)<<"  subSector: "<<subSector<<"  tbin: "<<tbin<<"  cscId: "<<(cscId+1)<<"  fpga: "<<(fpga+1)<<" "<<
+					"LCT(vp="<<lct->isValid()<<",qual="<<lct->getQuality()<<",wg="<<lct->getKeyWG()<<",strip="<<lct->getStrip()<<")";
 			}
-
-			edm::LogInfo("CSCTFAnalyzer|print") << "  Front data   endcap: "<<(*csc).first.endcap()<<"  station: "<<(station+1)<<"  sector: "<<(sector+1)<<"  subSector: "<<subSector<<"  tbin: "<<tbin<<"  cscId: "<<(cscId+1)<<"  fpga: "<<(fpga+1)<<" "<<
-				"LCT(vp="<<lct->isValid()<<",qual="<<lct->getQuality()<<",wg="<<lct->getKeyWG()<<",strip="<<lct->getStrip()<<")";
 		}
 	}
 
+	if( trackProducer.label() != "null" ){
+		edm::Handle<L1CSCTrackCollection> tracks;
+		e.getByLabel(trackProducer.label(),trackProducer.instance(),tracks);
 
-	edm::Handle<L1CSCTrackCollection> tracks;
-	e.getByLabel(trackProducer.label(),trackProducer.instance(),tracks);
-
-	int nTrk=0;
-	for(L1CSCTrackCollection::const_iterator trk=tracks->begin(); trk<tracks->end(); trk++,nTrk++){
-		int sector = 6*(trk->first.endcap()-1)+trk->first.sector()-1;
-		int tbin   = trk->first.BX();
-		edm::LogInfo("CSCTFAnalyzer|print") << "   Track sector: "<<(sector+1)<<"  tbin: "<<tbin<<" "<<
-			"TRK(mode="<<((trk->first.ptLUTAddress()>>16)&0xF)<<",eta="<<trk->first.eta_packed()<<",phi="<<trk->first.phi_packed()<<")";
+		int nTrk=0;
+		for(L1CSCTrackCollection::const_iterator trk=tracks->begin(); trk<tracks->end(); trk++,nTrk++){
+			int sector = 6*(trk->first.endcap()-1)+trk->first.sector()-1;
+			int tbin   = trk->first.BX();
+			edm::LogInfo("CSCTFAnalyzer|print") << "   Track sector: "<<(sector+1)<<"  tbin: "<<tbin<<" "<<
+				"TRK(mode="<<((trk->first.ptLUTAddress()>>16)&0xF)<<",eta="<<trk->first.eta_packed()<<",phi="<<trk->first.phi_packed()<<")";
+		}
 	}
 }
