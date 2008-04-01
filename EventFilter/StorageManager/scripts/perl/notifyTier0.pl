8d7b3f8e0d8323ed0d13b31cd388d466321becfe
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-01 09:02:42 +0000

    New db plus logging.

--- EventFilter/StorageManager/scripts/perl/notifyTier0.pl
+++ EventFilter/StorageManager/scripts/perl/notifyTier0.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 # $Id: notifyTier0.pl,v 1.1 2007/02/01 08:19:29 klute Exp $
 ################################################################################
 
@@ -6,68 +6,101 @@ use strict;
 use Getopt::Long;
 use DBI;
 
-# log the 
-open LOG, ">> /nfshome0/klute/globalRun-09-2007/log/notifyTier0.log";
-print LOG scalar localtime(time),' ',join(' ',@ARGV),"\n";
-close LOG;
+sub getdatestr()
+{
+    my @ltime = localtime(time);
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = @ltime;
+    $year += 1900;
+    $mon++;
 
-#my ($runnumber,$lumisection,$instance,$count,$stoptime,$filename,$pathname);
-#my ($hostname,$dataset,$stream,$status,$type,$safety,$nevents,$filesize);
-#my ($starttime,$checksum);
+    my $datestr=$year;
+    if ($mon < 10) {
+	$datestr=$datestr . "0";
+    }
 
-#my ($CRC)        = ('0');
-#my ($PRODUCER)   = ('StorageManager');
-#my ($MYCHECKSUM) = ('0');
-#my ($MYFILESIZE) = ('0');
+    $datestr=$datestr . $mon;
+    if ($mday < 10) {
+	$datestr=$datestr . "0";
+    }
+    $datestr=$datestr . $mday;
+    return $datestr;
+}
 
-#my $sender;
+################################################################################
+
+my $dolog = 1;
+my @MYARGV = @ARGV;
+
+my ($runnumber,$lumisection,$instance,$count,$stoptime,$filename,$pathname);
+my ($hostname,$dataset,$stream,$status,$type,$safety,$nevents,$filesize);
+my ($starttime,$checksum);
+
+my $cmsver = $ENV{'CMSSW_VERSION'};
+if(length $cmsver <= 0) {
+    $cmsver = "CMSSW_1_7_1";
+}
 
 # get options
-#GetOptions(
-#           "RUNNUMBER=i"   => \$runnumber,
-#           "LUMISECTION=i" => \$lumisection,
-#           "INSTANCE=i"    => \$instance,
-#           "COUNT=i"       => \$count,
-#           "START_TIME=i"  => \$starttime,
-#           "STOP_TIME=i"   => \$stoptime,
-#           "FILENAME=s"    => \$filename,
-#           "PATHNAME=s"    => \$pathname,
-#           "HOSTNAME=s"    => \$hostname,
-#           "DATASET=s"     => \$dataset,
-#           "STREAM=s"      => \$stream,
-#           "STATUS=s"      => \$status,
-#           "TYPE=s"        => \$type,
-#           "SAFETY=i"      => \$safety,
-#           "NEVENTS=i"     => \$nevents,
-#           "FILESIZE=i"    => \$filesize,
-#           "CHECKSUM=s"    => \$checksum
-#         );
+GetOptions(
+           "RUNNUMBER=i"   => \$runnumber,
+           "LUMISECTION=i" => \$lumisection,
+           "INSTANCE=i"    => \$instance,
+           "COUNT=i"       => \$count,
+           "START_TIME=i"  => \$starttime,
+           "STOP_TIME=i"   => \$stoptime,
+           "FILENAME=s"    => \$filename,
+           "PATHNAME=s"    => \$pathname,
+           "HOSTNAME=s"    => \$hostname,
+           "DATASET=s"     => \$dataset,
+           "STREAM=s"      => \$stream,
+           "STATUS=s"      => \$status,
+           "TYPE=s"        => \$type,
+           "SAFETY=i"      => \$safety,
+           "NEVENTS=i"     => \$nevents,
+           "FILESIZE=i"    => \$filesize,
+           "CHECKSUM=s"    => \$checksum
+          );
 
+################################################################################
 
-# checksum
-#my $FULLFILENAME = "$pathname$filename";
-#($MYCHECKSUM, $MYFILESIZE, $FULLFILENAME) = split(" ",qx(cksum $FULLFILENAME));
+#make sure domain is stripped
+my @harray = split(/\./,$hostname);
+$hostname = $harray[0];
 
-#print LOG scalar localtime(time),"  CHECKSUM=",$MYCHECKSUM, " FILESIZE=", $MYFILESIZE, "\n";
-#close LOG;
+#overwrite TNS to be sure it points to new DB
+$ENV{'TNS_ADMIN'} = '/etc/tnsnames.ora';
 
-# connect to DB
-#my $dbi    = "DBI:Oracle:omds";
-#my $reader = "cms_sto_mgr";
-#my $dbh    = DBI->connect($dbi,$reader,"qwerty");
+if ( $dolog == 1 )
+{
+    my $dstr = getdatestr();
+    my $ltime = localtime(time);
+    my $outfile = ">> /nfshome0/smdev/logs/" . $dstr . "-" . $hostname . ".log";
+    open LOG, $outfile;
+    print LOG scalar localtime(time),": notifyTier0.pl CMSSW_VERSION=",$cmsver," ",join(' ',@MYARGV),"\n";
+    close LOG;
+}
 
-# do the update 
-#my $SQL = "UPDATE CMS_STO_MGR_ADMIN.RUN_FILES SET FILESIZE=$filesize, STATUS='$status', STOP_TIME=$stoptime, NEVENTS=$nevents, CHECKSUM=$MYCHECKSUM, PATHNAME='$pathname' WHERE FILENAME = '$filename'";
-#my $sth = $dbh->do($SQL);
-
-# disconnect from DB
-#$dbh->disconnect;
+################################################################################
+#exit 0;
 
 # forward the argument to the next script
-#my $TIERZERO = "/nfshome0/cmsprod/TransferTest/injection/sendNotification.sh --RUNNUMBER $runnumber --LUMISECTION $lumisection --INSTANCE $instance --COUNT $count --START_TIME $starttime --STOP_TIME $stoptime --FILENAME $filename --PATHNAME $pathname --HOSTNAME $hostname --DATASET $dataset --STREAM $stream --STATUS $status --TYPE $type --SAFETY $safety --NEVENTS $nevents --FILESIZE $filesize --CHECKSUM $MYCHECKSUM";
-my $TIERZERO = "/nfshome0/cmsprod/TransferTest/injection/sendNotification.sh --APP_NAME=StorageManager --APP_VERSION=CMSSW_1_6_0_DAQ3 @ARGV";
+my $TIERZERO = "/nfshome0/cmsprod/TransferTest/injection/sendNotification.sh --APP_NAME=StorageManager --APP_VERSION=$cmsver --RUNNUMBER $runnumber --LUMISECTION $lumisection --INSTANCE $instance --COUNT $count --START_TIME $starttime --STOP_TIME $stoptime --FILENAME $filename --PATHNAME $pathname --HOSTNAME $hostname --DATASET $dataset --STREAM $stream --STATUS $status --TYPE $type --SAFETY $safety --NEVENTS $nevents --FILESIZE $filesize --CHECKSUM $checksum";
 
-#print $TIERZERO;
 system($TIERZERO);
+#print $TIERZERO;
+
+# connect to DB
+my $dbi    = "DBI:Oracle:cms_rcms";
+my $reader = "CMS_STOMGR_W";
+my $dbh    = DBI->connect($dbi,$reader,"qwerty");
+
+# do the update 
+my $SQL = "UPDATE CMS_STOMGR.TIER0_INJECTION SET SAFETY=1 WHERE FILENAME = '$filename'";
+
+my $sth = $dbh->do($SQL);
+#print $SQL;
+
+# disconnect from DB
+$dbh->disconnect;
 
 exit 0;
