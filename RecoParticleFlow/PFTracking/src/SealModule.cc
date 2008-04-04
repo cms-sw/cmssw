864ec949aa1fc8afe707147a4bc92fc84294a777
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-04-04 20:07:02 +0000

    Fix the previous commit. By mistake was not done on top of the head and the PFNuclearProducer was left out

--- RecoParticleFlow/PFTracking/src/SealModule.cc
+++ RecoParticleFlow/PFTracking/src/SealModule.cc
@@ -4,6 +4,7 @@
 #include "RecoParticleFlow/PFTracking/interface/PFElecTkProducer.h"
 #include "RecoParticleFlow/PFTracking/interface/VertexFilter.h"
 #include "RecoParticleFlow/PFTracking/interface/LightPFTrackProducer.h"
+#include "RecoParticleFlow/PFTracking/interface/PFNuclearProducer.h"
 #include "RecoParticleFlow/PFTracking/interface/PFConversionsProducer.h"
 
 DEFINE_SEAL_MODULE();
@@ -11,4 +12,5 @@ DEFINE_ANOTHER_FWK_MODULE(GoodSeedProducer);
 DEFINE_ANOTHER_FWK_MODULE(PFElecTkProducer);
 DEFINE_ANOTHER_FWK_MODULE(VertexFilter);
 DEFINE_ANOTHER_FWK_MODULE(LightPFTrackProducer);
+DEFINE_ANOTHER_FWK_MODULE(PFNuclearProducer);
 DEFINE_ANOTHER_FWK_MODULE(PFConversionsProducer);
