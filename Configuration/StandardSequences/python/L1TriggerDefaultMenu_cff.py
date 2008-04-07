e6f5afb8725725826944468c47f7e3708e6ba883
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-04-07 17:40:39 +0000

    update for L1 trigger sequences

---
+++ Configuration/StandardSequences/python/L1TriggerDefaultMenu_cff.py
@@ -0,0 +1,6 @@
+Making L1TriggerConfig/L1GtConfigProducers/python/Luminosity/lumi1030
+import FWCore.ParameterSet.Config as cms
+
+from L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu2008_2E30_Unprescaled_cff import *
+
+
--- Configuration/StandardSequences/python/L1Unprescale_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from L1TriggerConfig.L1GtConfigProducers.L1GtFactorsUnprescale_cff import *
-
-
