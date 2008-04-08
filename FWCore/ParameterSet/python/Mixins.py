6413324196044e7420857a071542f6f1cd026ded
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-08 18:16:42 +0000

    parameters_() method does a copy, so when we just need the length, better to use len(self.parameterNames_())

--- FWCore/ParameterSet/python/Mixins.py
+++ FWCore/ParameterSet/python/Mixins.py
@@ -271,7 +271,7 @@ class _TypedParameterizable(_Parameterizable):
 
     def dumpPython(self, options=PrintOptions()):
         result = "cms."+str(type(self).__name__)+"(\""+self.type_()+"\""
-        nparam = len(self.parameters_())
+        nparam = len(self.parameterNames_())
         if nparam == 0:
             result += ")\n"
         elif nparam < 256:
