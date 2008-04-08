80423a9ecc2d3768ad34695af883b80a45dbc6a4
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-04-08 13:09:07 +0000

    exclusive jet parton match producer module added

---
+++ TopQuarkAnalysis/TopEventProducers/interface/TtJetPartonMatch.h
@@ -0,0 +1,123 @@
+#ifndef TtJetPartonMatch_h
+#define TtJetPartonMatch_h
+
+#include <memory>
+#include <vector>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopJet.h"
+#include "AnalysisDataFormats/TopObjects/interface/TtGenEvent.h"
+#include "TopQuarkAnalysis/TopTools/interface/JetPartonMatching.h"
+
+class TtSemiEvtPartons {
+
+ public:
+
+  TtSemiEvtPartons(){};
+  ~TtSemiEvtPartons(){};
+  std::vector<const reco::Candidate*> vec(const TtGenEvent& genEvt)
+  {
+    std::vector<const reco::Candidate*> partons;
+    partons.push_back(genEvt.hadronicDecayQuark());
+    partons.push_back(genEvt.hadronicDecayQuarkBar());
+    partons.push_back(genEvt.hadronicDecayB());
+    partons.push_back(genEvt.leptonicDecayB());
+    return partons;
+  }
+};
+
+class TtHadEvtPartons {
+
+ public:
+
+  TtHadEvtPartons(){};
+  ~TtHadEvtPartons(){};
+  std::vector<const reco::Candidate*> vec(const TtGenEvent& genEvt)
+  {
+    std::vector<const reco::Candidate*> partons;
+    partons.push_back(genEvt.quarkFromTop());
+    partons.push_back(genEvt.quarkFromTopBar());
+    partons.push_back(genEvt.b());
+    partons.push_back(genEvt.quarkFromAntiTop());
+    partons.push_back(genEvt.quarkFromAntiTopBar());
+    partons.push_back(genEvt.bBar());
+    return partons;
+  }
+};
+
+template <typename C>
+class TtJetPartonMatch : public edm::EDProducer {
+  
+ public:
+  
+  explicit TtJetPartonMatch(const edm::ParameterSet&);
+  ~TtJetPartonMatch();
+  
+ private:
+  
+  typedef std::vector<TopJet> TopJetCollection;
+  
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+
+  edm::InputTag jets_;
+
+  unsigned int nJets_;
+  int algorithm_;
+  bool useDeltaR_;
+  bool useMaxDist_;
+  double maxDist_;
+};
+
+template<typename C>
+TtJetPartonMatch<C>::TtJetPartonMatch(const edm::ParameterSet& cfg):
+  jets_(cfg.getParameter<edm::InputTag>("jets")),
+  nJets_(cfg.getParameter<unsigned int>("nJets")),
+  algorithm_(cfg.getParameter<int>("algorithm")),
+  useDeltaR_(cfg.getParameter<bool>("useDeltaR")),
+  useMaxDist_(cfg.getParameter<bool>("useMaxDist")),
+  maxDist_(cfg.getParameter<double>("maxDist"))
+{
+  produces< std::vector<int> >();
+}
+
+template<typename C>
+TtJetPartonMatch<C>::~TtJetPartonMatch()
+{
+}
+
+template<typename C>
+void
+TtJetPartonMatch<C>::produce(edm::Event& evt, const edm::EventSetup& setup)
+{
+  edm::Handle<TtGenEvent> genEvt;
+  evt.getByLabel("genEvt", genEvt);
+  
+  edm::Handle<TopJetCollection> topJets;
+  evt.getByLabel(jets_, topJets);
+  
+  C parts;
+  std::vector<const reco::Candidate*> partons = parts.vec(*genEvt);
+
+  //prepare vector of jets
+  std::vector<reco::CaloJet> jets;
+  for(unsigned int ij = 0; ij < topJets->size(); ij++) {
+    if(nJets_ >= partons.size()){ if(ij == nJets_) break; }
+    else{ if(ij == partons.size()) break; }
+    const reco::CaloJet jet = (*topJets)[ij].getRecJet();
+    jets.push_back( jet );
+  }
+
+  //do the matching with specified parameters
+  JetPartonMatching jetPartonMatch(partons, jets, algorithm_, useMaxDist_, useDeltaR_, maxDist_);
+
+  std::auto_ptr< std::vector<int> > pOut(new std::vector<int>);
+  for(unsigned int i=0; i<partons.size(); ++i){
+    pOut->push_back( jetPartonMatch.getMatchForParton(i) );
+  }
+  evt.put(pOut);
+}
+
+#endif
--- TopQuarkAnalysis/TopEventProducers/src/SealModule.cc
+++ TopQuarkAnalysis/TopEventProducers/src/SealModule.cc
@@ -8,7 +8,13 @@
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtGenEventReco.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/StGenEventReco.h"
 #include "TopQuarkAnalysis/TopEventProducers/interface/TtDecaySelection.h"
+#include "TopQuarkAnalysis/TopEventProducers/interface/TtJetPartonMatch.h"
 
+//expand templates 
+typedef TtJetPartonMatch< TtHadEvtPartons  > TtHadEvtJetPartonMatch;
+typedef TtJetPartonMatch< TtSemiEvtPartons > TtSemiEvtJetPartonMatch;
+
+//define modules
 DEFINE_FWK_MODULE(TopInitSubset);
 DEFINE_FWK_MODULE(TopDecaySubset);
 DEFINE_FWK_MODULE(TtGenEventReco);
@@ -18,3 +24,6 @@ DEFINE_FWK_MODULE(TtDilepEvtSolutionMaker);
 DEFINE_FWK_MODULE(TtHadEvtSolutionMaker);
 DEFINE_FWK_MODULE(StEvtSolutionMaker);
 DEFINE_FWK_MODULE(TtDecaySelection);
+DEFINE_FWK_MODULE(TtHadEvtJetPartonMatch);
+DEFINE_FWK_MODULE(TtSemiEvtJetPartonMatch);
+
