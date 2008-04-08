9c8ba2cc357f32c6dcb9a5e8e7d25a5cd90016f4
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2008-04-08 16:24:46 +0000

    *** empty log message ***

--- Calibration/HcalCalibAlgos/src/HcalConstantsASCIIWriter.cc
+++ Calibration/HcalCalibAlgos/src/HcalConstantsASCIIWriter.cc
@@ -12,6 +12,8 @@
 #include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
 
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+
 #include "CondFormats/HcalObjects/interface/HcalRespCorrs.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "HepMC/GenParticle.h"
@@ -20,6 +22,8 @@
 #include "CondFormats/DataRecord/interface/HcalRespCorrsRcd.h"
 #include <fstream>
 #include <sstream>
+#include <map>
+#include <vector>
 
 using namespace std;
 using namespace reco;
@@ -30,7 +34,7 @@ namespace cms{
 HcalConstantsASCIIWriter::HcalConstantsASCIIWriter(const edm::ParameterSet& iConfig)
 {
   // get name of output file with histogramms
-  file_input="data/"+iConfig.getParameter <std::string> ("tagName1")+".txt";
+  file_input="/Calibration/HcalCalibAlgos/data/"+iConfig.getParameter <std::string> ("tagName1")+".txt";
   
 }
 
@@ -72,26 +76,29 @@ HcalConstantsASCIIWriter::analyze(const edm::Event& iEvent, const edm::EventSetu
     iSetup.get<HcalRespCorrsRcd>().get(r);
     HcalRespCorrs* oldRespCorrs = new HcalRespCorrs(*r.product());
     std::vector<DetId> dd = oldRespCorrs->getAllChannels();
+   
+    map<HcalDetId,float> corrold;
+    map<HcalDetId,float> corrnew; 
     
-    
-    float corrold[4][4][4][42][72];
-    float corrnew[4][4][4][42][72];
+//    float corrold[4][4][2][4][42][72];
+//    float corrnew[4][4][2][4][42][72];
     
     int mydet,mysubd,depth,ieta,iphi;
     float coradd;
      
-     
-     
+    vector<HcalDetId> theVector; 
     for(std::vector<DetId>::iterator i = dd.begin(); i != dd.end(); i++)
     {
     
       HcalDetId hid = HcalDetId(*i);
+      theVector.push_back(hid);
       mydet = ((hid).rawId()>>28)&0xF;
       mysubd = ((hid).rawId()>>25)&0x7;
       depth =(hid).depth();
       ieta = (hid).ieta();
       iphi = (hid).iphi();
-      corrold[mydet][mysubd][depth][ieta][iphi] = (oldRespCorrs->getValues(*i))->getValue();
+//      corrold[mydet][mysubd][depth][ieta][iphi] = (oldRespCorrs->getValues(*i))->getValue();
+      corrold[hid] = (oldRespCorrs->getValues(*i))->getValue();
       
     }
 
@@ -111,12 +118,25 @@ HcalConstantsASCIIWriter::analyze(const edm::Event& iEvent, const edm::EventSetu
       double par;
       int type;
       linestream>>mydet>>mysubd>>depth>>ieta>>iphi>>coradd;
-      corrnew[mydet][mysubd][depth][ieta][iphi] = coradd;
-
+//      corrnew[mydet][mysubd][depth][ieta][iphi] = coradd;
+        HcalDetId  hid(HcalSubdetector(mysubd),ieta,iphi,depth);
+        corrnew[hid] =  coradd; 
     } 
     
     HcalRespCorrs* mycorrections = new HcalRespCorrs(); 
-         
+    
+   for(vector<HcalDetId>::iterator it = theVector.begin(); it != theVector.end(); it++)
+   {
+     float cc1 = (*corrold.find(*it)).second;
+     float cc2 = (*corrnew.find(*it)).second;
+     float cc = cc1*cc2;
+//      float cc = corrnew[i][j][k][l][m]*corrold[i][j][k][l][m];
+  // now make the basic object for one cell with HcalDetId myDetId containing the value myValue
+      HcalRespCorr item ((*it).rawId(), cc);
+      bool rr = mycorrections->addValues(item);
+   }   
+
+/*     
     for (int i = 0 ; i < 4 ; i++)
     {
       for (int j = 0 ; j < 4 ; j++)
@@ -136,6 +156,7 @@ HcalConstantsASCIIWriter::analyze(const edm::Event& iEvent, const edm::EventSetu
        }
      }
     }
+*/
     HcalRespCorrs mycc = *mycorrections;
     HcalDbASCIIIO::dumpObject (*myout_hcal, mycc);
 
