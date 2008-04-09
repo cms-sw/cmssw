b4de33369d9e2d66340589f41513d06e5b92b559
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-09 19:45:29 +0000

    dox; debug info

--- MagneticField/Interpolation/src/LinearGridInterpolator3D.h
+++ MagneticField/Interpolation/src/LinearGridInterpolator3D.h
@@ -1,12 +1,21 @@
-// #define DEBUG_LinearGridInterpolator3D
+#ifndef Interpolation_LinearGridInterpolator3D_h
+#define Interpolation_LinearGridInterpolator3D_h
 
-#ifndef LinearGridInterpolator3D_H
-#define LinearGridInterpolator3D_H
+/** \class LinearGridInterpolator3D
+ *
+ *  Linear interpolation in a regular 3D grid.
+ *
+ *  $Date: $
+ *  $Revision: $
+ *  \author T. Todorov 
+ */
 
 #include "MagneticField/VolumeGeometry/interface/MagExceptions.h"
 
 #ifdef DEBUG_LinearGridInterpolator3D
 #include <iostream>
+using namespace std;
+#include "MagneticField/Interpolation/src/InterpolationDebug.h"
 #endif
 
 template <class Value, class T>
@@ -34,17 +43,30 @@ public:
     Scalar u = (c - gridc.node(k)) / gridc.step();
 
 #ifdef DEBUG_LinearGridInterpolator3D
-    cout << "LinearGridInterpolator3D called with a,b,c " << a << "," << b << "," << c << endl;
-    cout <<" i,j,k = " << i << "," << j << "," << k 
-	 << " s,t,u = " << s << "," << t << "," << u << endl;
-    cout << "Node positions for " << i << "," << j << "," << k << " : "
-	 << grida.node(i) << "," << gridb.node(j) << "," << gridc.node(k) << endl;
-    cout << "Node positions for " << i+1 << "," << j+1 << "," << k+1 << " : "
-	 << grida.node(i+1) << "," << gridb.node(j+1) << "," << gridc.node(k+1) << endl;
-    cout << "Grid(" << i << "," << j << "," << k << ") = " << grid(i,  j,  k) << endl;
-    cout << "Grid(" << i+1 << "," << j+1 << "," << k+1 << ") = " << grid(i+1,j+1,k+1) << endl;
-    cout << "Grid(" << i << "," << j+1 << "," << k << ") = " << grid(i,j+1,k) << endl;
-    cout << "number of nodes: " << grida.nodes() << "," << gridb.nodes() << "," << gridc.nodes() << endl;
+    if (InterpolationDebug::debug) {
+      cout << "LinearGridInterpolator3D called with a,b,c " << a << "," << b << "," << c << endl;
+      cout <<" i,j,k = " << i << "," << j << "," << k 
+	   << " s,t,u = " << s << "," << t << "," << u << endl;
+      cout << "Node positions for " << i << "," << j << "," << k << " : "
+	   << grida.node(i) << "," << gridb.node(j) << "," << gridc.node(k) << endl;
+      cout << "Node positions for " << i+1 << "," << j+1 << "," << k+1 << " : "
+	   << grida.node(i+1) << "," << gridb.node(j+1) << "," << gridc.node(k+1) << endl;
+      cout << "Grid(" << i << "," << j << "," << k << ") = " << grid(i,  j,  k) << " ";
+      cout << "Grid(" << i << "," << j << "," << k+1 << ") = " << grid(i,j,k+1) << endl;
+      cout << "Grid(" << i << "," << j+1 << "," << k << ") = " << grid(i,j+1,k) << " ";
+      cout << "Grid(" << i << "," << j+1 << "," << k+1 << ") = " << grid(i,j+1,k+1) << endl;
+      cout << "Grid(" << i+1 << "," << j << "," << k << ") = " << grid(i+1,j,k) << " ";
+      cout << "Grid(" << i+1 << "," << j << "," << k+1 << ") = " << grid(i+1,j,k+1) << endl;
+      cout << "Grid(" << i+1 << "," << j+1 << "," << k << ") = " << grid(i+1,j+1,k) << " ";
+      cout << "Grid(" << i+1 << "," << j+1 << "," << k+1 << ") = " << grid(i+1,j+1,k+1) << endl;
+      cout << "number of nodes: " << grida.nodes() << "," << gridb.nodes() << "," << gridc.nodes() << endl;
+
+//     cout << (1-s)*(1-t)*(1-u)*grid(i,  j,  k) << " " << (1-s)*(1-t)*u*grid(i,  j,  k+1) << endl 
+// 	 << (1-s)*   t *(1-u)*grid(i,  j+1,k) << " " << (1-s)*   t *u*grid(i,  j+1,k+1) << endl 
+// 	 << s    *(1-t)*(1-u)*grid(i+1,j,  k) << " " <<  s    *(1-t)*u*grid(i+1,j,  k+1) << endl 
+// 	 << s    *   t *(1-u)*grid(i+1,j+1,k) << " " << s    *   t *u*grid(i+1,j+1,k+1) << endl;
+    }
+
 #endif
 
     Value result = 
@@ -52,13 +74,13 @@ public:
       (1-s)*   t *(1-u)*grid(i,  j+1,k) + (1-s)*   t *u*grid(i,  j+1,k+1) +
       s    *(1-t)*(1-u)*grid(i+1,j,  k) + s    *(1-t)*u*grid(i+1,j,  k+1) + 
       s    *   t *(1-u)*grid(i+1,j+1,k) + s    *   t *u*grid(i+1,j+1,k+1);
+
     return result;
   }
 
-  Value operator()( Scalar a, Scalar b, Scalar c) {return interpolate(a,b,c);}
+  //  Value operator()( Scalar a, Scalar b, Scalar c) {return interpolate(a,b,c);}
 
 private:
-
   const Grid3D<Value,T>& grid;
   const Grid1D<T>& grida;
   const Grid1D<T>& gridb;
