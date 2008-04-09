27d98a613115a125b665ade8e247e7dc960f7a13
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-09 19:41:48 +0000

    hadd debug handles

---
+++ MagneticField/Interpolation/src/InterpolationDebug.cc
@@ -0,0 +1,5 @@
+#include "MagneticField/Interpolation/src/InterpolationDebug.h"
+
+#ifdef DEBUG_LinearGridInterpolator3D
+bool InterpolationDebug::debug=false;
+#endif
---
+++ MagneticField/Interpolation/src/InterpolationDebug.h
@@ -0,0 +1,17 @@
+#ifndef InterpolationDebug_h
+#define InterpolationDebug_h
+
+/** \class InterpolationDebug
+ *
+ *  No description available.
+ *
+ *  $Date: $
+ *  $Revision: $
+ *  \author N. Amapane - CERN
+ */
+
+struct InterpolationDebug {
+  static bool debug;
+};
+
+#endif
