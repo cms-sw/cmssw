b2db8cca818c45bf5d8132ffcfe90ccafe06b261
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-09 16:46:11 +0000

    validate TOSCA tables

--- MagneticField/Engine/test/testMagneticField.cc
+++ MagneticField/Engine/test/testMagneticField.cc
@@ -1,8 +1,8 @@
 /** \file
  *  A simple example of ho to access the magnetic field.
  *
- *  $Date: 2008/03/28 16:52:39 $
- *  $Revision: 1.11 $
+ *  $Date: 2008/03/29 10:12:13 $
+ *  $Revision: 1.12 $
  *  \author N. Amapane - CERN
  */
 
@@ -25,6 +25,8 @@
 #include "DataFormats/GeometryVector/interface/Pi.h"
 #include "DataFormats/GeometryVector/interface/CoordinateSets.h"
 #include "MagneticField/GeomBuilder/test/stubs/GlobalPointProvider.h"
+#include "MagneticField/VolumeBasedEngine/interface/MagGeometry.h"
+#include "MagneticField/VolumeGeometry/interface/MagVolume6Faces.h"
 
 
 #include <iostream>
@@ -32,6 +34,7 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace edm;
 using namespace Geom;
@@ -53,6 +56,8 @@ class testMagneticField : public edm::EDAnalyzer {
     //    number of random points to try
     numberOfPoints = pset.getUntrackedParameter<int>("numberOfPoints", 10000);
     //    outer radius of test cylinder
+    InnerRadius = pset.getUntrackedParameter<double>("InnerRadius",0.);
+    //    half length of test cylinder
     OuterRadius = pset.getUntrackedParameter<double>("OuterRadius",900);
     //    half length of test cylinder
     HalfLength = pset.getUntrackedParameter<double>("HalfLength",1600);
@@ -78,7 +83,9 @@ class testMagneticField : public edm::EDAnalyzer {
      writeValidationTable(numberOfPoints,outputFile);
    }
    
-   if (inputFile!="") {
+   if (inputFileType == "TOSCA") {
+     validateVsTOSCATable(inputFile);
+       } else if (inputFile!="") {
      validate (inputFile, inputFileType);
    }
 
@@ -90,7 +97,10 @@ class testMagneticField : public edm::EDAnalyzer {
   
   void writeValidationTable(int npoints, string filename);
   void validate(string filename, string type="xyz");
-   
+  void validateVsTOSCATable(string filename);
+
+  const MagVolume6Faces* findMasterVolume(string volume);
+
  private:
   const MagneticField* field;
   string inputFile;
@@ -99,12 +109,13 @@ class testMagneticField : public edm::EDAnalyzer {
   double reso;
   int numberOfPoints;
   double OuterRadius;
+  double InnerRadius;
   double HalfLength;
 };
 
 
 void testMagneticField::writeValidationTable(int npoints, string filename) {
-  GlobalPointProvider p(0, OuterRadius, -Geom::pi(), Geom::pi(), -HalfLength, HalfLength);
+  GlobalPointProvider p(InnerRadius, OuterRadius, -Geom::pi(), Geom::pi(), -HalfLength, HalfLength);
   ofstream file(filename.c_str());
 
   for (int i = 0; i<npoints; ++i) {
@@ -144,7 +155,7 @@ void testMagneticField::validate(string filename, string type) {
       gp = GlobalPoint(px, py, pz);      
     }
 
-    if (gp.perp() > OuterRadius || fabs(gp.z()) > HalfLength) continue;
+    if (gp.perp() < InnerRadius || gp.perp() > OuterRadius || fabs(gp.z()) > HalfLength) continue;
     
     GlobalVector oldB(bx, by, bz);
     GlobalVector newB = field->inTesla(gp);
@@ -162,6 +173,106 @@ void testMagneticField::validate(string filename, string type) {
   
 }
 
+void testMagneticField::validateVsTOSCATable(string filename) {
+  // The magic here is that we want to check against the result of the master volume only 
+  // as grid points on the border of volumes can end up in the neighbor volume.
+
+  string::size_type ibeg, iend;
+  ibeg = filename.find('-');  // first occurence of "-"
+  iend = filename.rfind('-'); // last  occurence of "-"
+  string type = filename.substr(ibeg+1, iend-ibeg-1);
+
+  string volume;
+  int volnumber=0;
+  stringstream sstr;
+  sstr << filename.substr(iend+1, 3);
+  sstr >> volnumber;
+  stringstream sstr2;
+  sstr2 << "V_" << volnumber;
+  sstr2 >> volume;
+  const MagVolume6Faces* vol = findMasterVolume(volume);
+  if (vol==0) { // rollback to old naming conventions...
+    stringstream sstr3;
+    sstr3 << "V_ZN_" << volnumber;
+    sstr3 >> volume;
+    vol = findMasterVolume(volume);
+  }
+  
+  cout << "Validate interpolation vs TOSCATable: " << filename << " volume " << volume << " type " << type << endl;
+
+  cout << vol->name << " " << int(vol->copyno) << endl;
+
+  ifstream file(filename.c_str());
+  string line;
+
+  int fail = 0;
+  int count = 0;
+  
+  float maxdelta=0.;
+
+  while (getline(file,line) && count < numberOfPoints) {
+    if( line == "" || line[0] == '#' ) continue;
+    stringstream linestr;
+    linestr << line;
+    double px, py, pz;
+    double bx, by, bz;
+    linestr  >> px >> py >> pz >> bx >> by >> bz;
+    GlobalPoint gp;
+    if (type=="rpz") { // rpz file with units in m.
+      gp = GlobalPoint(GlobalPoint::Cylindrical(px*100.,py,pz*100.));
+    } else if (type=="xyz") { // rpz file with units in m.
+      gp = GlobalPoint(px*100., py*100., pz*100.);
+    } else {
+      cout << "validateVsTOSCATable: type " << type << " unknown " << endl;
+      return;
+    }
+
+    //    if (gp.perp() < InnerRadius || gp.perp() > OuterRadius || fabs(gp.z()) > HalfLength) continue;
+    
+    GlobalVector oldB(bx, by, bz);
+    GlobalVector newB = vol->inTesla(gp);
+    if ((newB-oldB).mag() > reso) {
+      ++fail;
+      float delta = (newB-oldB).mag();
+      if (delta > maxdelta) maxdelta = delta;
+      cout << " Discrepancy at: # " << count+1 << " " << gp << " delta : " << newB-oldB << " " << delta <<  endl;
+      cout << " Old: " << oldB << " New: " << newB << endl;
+    }
+    count++;
+  }
+  cout << endl << " testMagneticField::validateVsTOSCATable: tested " << count
+       << " points " << fail << " failures; max delta = " << maxdelta
+       << endl << endl;
+  
+}
+
+
+#define private public
+#include "MagneticField/VolumeBasedEngine/interface/VolumeBasedMagneticField.h"
+
+const MagVolume6Faces* testMagneticField::findMasterVolume(string volume) {
+  const MagGeometry* vbffield = (dynamic_cast<const VolumeBasedMagneticField*>(field))->field;;
+
+  const vector<MagVolume6Faces*>& bvol = vbffield->barrelVolumes();
+  for (vector<MagVolume6Faces*>::const_iterator i=bvol.begin();
+       i!=bvol.end(); i++) {
+    if ((*i)->copyno == 1 && (*i)->name==volume) {
+      return (*i);
+    }
+  }
+  
+  const vector<MagVolume6Faces*>& evol = vbffield->endcapVolumes();
+  for (vector<MagVolume6Faces*>::const_iterator i=evol.begin();
+       i!=evol.end(); i++) {
+    if ((*i)->copyno == 1 && (*i)->name==volume) {
+      return (*i);
+    }
+  }
+
+  return 0;
+}
+
+ 
 
 DEFINE_FWK_MODULE(testMagneticField);
 
