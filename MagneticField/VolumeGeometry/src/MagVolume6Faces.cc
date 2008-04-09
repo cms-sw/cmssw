6c27157a67b6c5148aa262a6f4a0ca87d9a9e17e
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-09 16:18:23 +0000

    Add copyno; cleanup

--- MagneticField/VolumeGeometry/src/MagVolume6Faces.cc
+++ MagneticField/VolumeGeometry/src/MagVolume6Faces.cc
@@ -1,35 +1,15 @@
-// #include "Utilities/Configuration/interface/Architecture.h"
-
 #include "MagneticField/VolumeGeometry/interface/MagVolume6Faces.h"
-
-#ifndef NO_DETAILED_TIMING
-// TOFIX
-// #include "Utilities/Notification/interface/TimingReport.h"
-//#include "Utilities/UI/interface/SimpleConfigurable.h"
-#endif
 
 MagVolume6Faces::MagVolume6Faces( const PositionType& pos,
 				  const RotationType& rot, 
 				  DDSolidShape shape,
 				  const std::vector<VolumeSide>& faces,
 				  const MagneticFieldProvider<float> * mfp)
-  : MagVolume(pos,rot,shape,mfp), theFaces(faces) 
-{
-#ifndef NO_DETAILED_TIMING
-// TOFIX
-//   static SimpleConfigurable<bool> timerOn(false,"MagVolume6Faces:timing");
-//   bool timerOn = false;
-//   (*TimingReport::current()).switchOn("MagVolume6Faces::inside",timerOn);
-#endif
-}
+  : MagVolume(pos,rot,shape,mfp), theFaces(faces), copyno(0) 
+{}
 
 bool MagVolume6Faces::inside( const GlobalPoint& gp, double tolerance) const 
 {
-#ifndef NO_DETAILED_TIMING
-// TOFIX
-//   static TimingReport::Item & timer = (*TimingReport::current())["MagVolume6Faces::inside"];
-//   TimeMe t(timer,false);
-#endif
 
   // check if the point is on the correct side of all delimiting surfaces
   for (std::vector<VolumeSide>::const_iterator i=theFaces.begin(); i!=theFaces.end(); ++i) {
