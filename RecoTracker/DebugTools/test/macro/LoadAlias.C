4c5087fd67ac7eb8dd0744a5578921462647a485
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-04-11 11:30:12 +0000

    use second step track collection

--- RecoTracker/DebugTools/test/macro/LoadAlias.C
+++ RecoTracker/DebugTools/test/macro/LoadAlias.C
@@ -8,4 +8,7 @@ void LoadAlias()
     Events->SetAlias("T",  "TPtoRecoTracks_trackAlgoCompare_TP_TrackAlgoCompare.obj");
     Events->SetAlias("TA", "TPtoRecoTracks_trackAlgoCompare_AlgoA_TrackAlgoCompare.obj");
     Events->SetAlias("TB", "TPtoRecoTracks_trackAlgoCompare_AlgoB_TrackAlgoCompare.obj");
+
+    TCut trackablePrimaryTP = "abs(T.TP().eta())<2.4 && T.TP().vertex().rho()<3.5 && abs(T.TP().vertex().z())< 30 && T.TP().charge()!=0 && T.TP().pt()>0.9";
+
 }
