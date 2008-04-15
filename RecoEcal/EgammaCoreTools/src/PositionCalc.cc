82b81816fff1440cbfdb2fc9be5f2e97c3174a99
Dave Evans <sha1-fd5bf8c7bc363fe103f075cd5169aefdf85f5f6f@cern.ch>
2008-04-15 18:50:28 +0000

    PositionCalc logs warning not error if a cluster with negative energy is found.

--- RecoEcal/EgammaCoreTools/src/PositionCalc.cc
+++ RecoEcal/EgammaCoreTools/src/PositionCalc.cc
@@ -102,7 +102,7 @@ math::XYZPoint PositionCalc::Calculate_Location(std::vector<DetId> passedDetIds,
   // Calculate shower depth
   float depth = 0.;
   if(eTot<=0.) {
-    edm::LogError("NegativeClusterEnergy") << "cluster with negative energy: " << eTot
+    edm::LogWarning("NegativeClusterEnergy") << "cluster with negative energy: " << eTot
                                       << " setting depth to 0.";
   } else {
     depth = param_X0_ * (T0 + log(eTot));
