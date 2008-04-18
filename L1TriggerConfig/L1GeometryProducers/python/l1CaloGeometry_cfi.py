2a7fa01e2e097a5df233f5de915dc1a4fa8b32c7
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-04-18 00:06:32 +0000

    Updated to new L1CaloGeometry in CondFormats/L1TObjects.

--- L1TriggerConfig/L1GeometryProducers/python/l1CaloGeometry_cfi.py
+++ L1TriggerConfig/L1GeometryProducers/python/l1CaloGeometry_cfi.py
@@ -1,5 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-l1CaloGeometry = cms.ESProducer("L1CaloGeometryProd")
+l1CaloGeometry = cms.ESProducer("L1CaloGeometryProd",
+    gctEmJetPhiBinOffset = cms.double(-0.5),
+    numberGctEmJetPhiBins = cms.uint32(18),
+    numberGctEtSumPhiBins = cms.uint32(72),
+    gctEtaBinBoundaries = cms.vdouble(0.0, 0.348, 0.695, 1.044, 1.392, 
+        1.74, 2.172, 3.0, 3.5, 4.0, 
+        4.5, 5.0),
+    numberGctCentralEtaBinsPerHalf = cms.uint32(7),
+    gctEtSumPhiBinOffset = cms.double(0.0),
+    numberGctForwardEtaBinsPerHalf = cms.uint32(4),
+    etaSignBitOffset = cms.uint32(8)
+)
+
 
 
