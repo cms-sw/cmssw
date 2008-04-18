bacb062babd4baf14b752fc5501da3a5b79acb6a
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-04-18 06:04:40 +0000

    corrected bug

--- CondTools/SiStrip/scripts/CreatingTables.sh
+++ CondTools/SiStrip/scripts/CreatingTables.sh
@@ -8,7 +8,7 @@ eval `scramv1 runtime -sh`
 workdir=`pwd`
 
 path=$CMSSW_BASE/src/CondFormats/SiStripObjects/xml/
-if [ ! -e $path] ;then
+if [ ! -e $path ] ;then
 path=$CMSSW_RELEASE_BASE/src/CondFormats/SiStripObjects/xml/
 if [ ! -e $path ]; then
 echo -e "Error: CondFormats/SiStripObjects/xml doesn't exist\nplease install that package\nexit"
@@ -21,7 +21,7 @@ echo -e "\n-----------\nCreating tables for db ${connectstring} \n-----------\n"
 
 #cmscond_bootstrap_detector.pl --offline_connect ${connectstring} --auth /afs/cern.ch/cms/DB/conddb/authentication.xml STRIP
 for obj in `ls $path/*xml`
-		      do
-		      echo -e  "\npool_build_object_relational_mapping -f $obj   -d CondFormatsSiStripObjects -c ${connectstring}\n"
-		      pool_build_object_relational_mapping -f $obj   -d CondFormatsSiStripObjects -c ${connectstring} -u $USER -p $PASSWD
-		      done
+  do
+  echo -e  "\npool_build_object_relational_mapping -f $obj   -d CondFormatsSiStripObjects -c ${connectstring}\n"
+  pool_build_object_relational_mapping -f $obj   -d CondFormatsSiStripObjects -c ${connectstring} -u $USER -p $PASSWD
+done
