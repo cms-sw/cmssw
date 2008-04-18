3ef4fd15e02f712626dd215f57f087454400cd00
Erik Butz <sha1-53bc992219043be71927609d789fbc8f1ac8e7d6@cern.ch>
2008-04-18 16:58:05 +0000

    included local residual and first try for overlap residuals

--- Alignment/OfflineValidation/interface/TrackerValidationVariables.h
+++ Alignment/OfflineValidation/interface/TrackerValidationVariables.h
@@ -14,14 +14,17 @@
 class TrackerValidationVariables  {
  public:  
   struct AVHitStruct{
-    AVHitStruct() : resX(-999.), resY(-999.), resErrX(-999.), resErrY(-999.), phi(-999.), eta(-999.), rawDetId(0) {}
+    AVHitStruct() : resX(-999.), resY(-999.), resErrX(-999.), resErrY(-999.), resXprime(-999.), 
+	 phi(-999.), eta(-999.), rawDetId(0), overlapres(std::make_pair(0,-999.)) {}
     float resX;
     float resY;
     float resErrX;
     float resErrY;
+    float resXprime;
     float phi;
     float eta;
     uint32_t rawDetId;
+    std::pair<uint,float> overlapres;
   };
   struct AVTrackStruct{
     AVTrackStruct() : pt(0.), px(0.), py(0.), pz(0.), eta(0.), phi(0.), kappa(0.),
--- Alignment/OfflineValidation/src/TrackerValidationVariables.cc
+++ Alignment/OfflineValidation/src/TrackerValidationVariables.cc
@@ -19,8 +19,9 @@
 #include "RecoTracker/TrackProducer/interface/TrackingRecHitLessFromGlobalPosition.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/MeasurementVector.h"
-
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
 #include "TrackingTools/PatternTools/interface/Trajectory.h"
 #include "TrackingTools/PatternTools/interface/TrajectoryMeasurement.h"
 #include "TrackingTools/TrackFitters/interface/TrajectoryStateCombiner.h"
@@ -32,11 +33,15 @@
 
 #include "CalibFormats/SiStripObjects/interface/SiStripDetCabling.h"
 #include "CalibTracker/Records/interface/SiStripDetCablingRcd.h"
-
+#include "Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h"
+#include "Geometry/CommonTopologies/interface/RadialStripTopology.h"
+#include "Geometry/CommonTopologies/interface/RectangularStripTopology.h"
+#include "Geometry/CommonTopologies/interface/StripTopology.h"
+#include "Alignment/TrackerAlignment/interface/TrackerAlignableId.h"
 #include "Alignment/OfflineValidation/interface/TrackerValidationVariables.h"
 
 
-TrackerValidationVariables::TrackerValidationVariables() {}
+TrackerValidationVariables::TrackerValidationVariables(){}
 
 
 TrackerValidationVariables::TrackerValidationVariables(const edm::EventSetup& es, const edm::ParameterSet& iSetup) 
@@ -66,43 +71,112 @@ TrackerValidationVariables::fillHitQuantities(const edm::Event& iEvent,
 
       if(! itTraj->updatedState().isValid()) continue;
       
-      TrajectoryStateOnSurface theCombinedPredictedState = tsoscomb.combine(itTraj->backwardPredictedState(), 
-									    itTraj->forwardPredictedState() );      
+      
+      TrajectoryStateOnSurface tsos = tsoscomb( itTraj->forwardPredictedState(), itTraj->backwardPredictedState() );
       TransientTrackingRecHit::ConstRecHitPointer hit = itTraj->recHit();
-      if(! hit->isValid()) {
+      if(! hit->isValid() || hit->geographicalId().det() != DetId::Tracker ) {
 	continue; 
       } else {
 	AVHitStruct hitStruct;
 	const DetId & hit_detId = hit->geographicalId();
 	uint IntRawDetID = (hit_detId.rawId());	
-	//
-	TrajectoryStateOnSurface tsos = tsoscomb( itTraj->forwardPredictedState(), itTraj->backwardPredictedState() );
+	uint IntSubDetID = (hit_detId.subdetId());
+	
+	if(IntSubDetID == 0 )
+	  continue;
 	
 	align::LocalVector res = tsos.localPosition() - hit->localPosition();
+
 	LocalError err1 = tsos.localError().positionError();
 	LocalError err2 = hit->localPositionError();
 	
 	float errX = std::sqrt( err1.xx() + err2.xx() );
 	float errY = std::sqrt( err1.yy() + err2.yy() );
 	
-	LogDebug("TrackerValidationVariables") << "@SUB=TrackerValidationVariables::fillHitQuantities" 
-					       << "Residual x/y " << res.x() << '/' << res.y() 
-					       << ", Error x/y " << errX << '/' << errY;
-		
+	LogDebug("TrackerValidationVariables") << "Residual x/y " << res.x() << '/' << res.y() 
+					       << ", Error x/y " << errX << '/' << errY;		
+
+	// begin partly copied from Tifanalyser 
+
+	const GeomDetUnit* detUnit = hit->detUnit();
+	double dPhi = -999, dR = -999, dZ = -999, phiorientation = -999;
+	double R = 0.;
+	double origintointersect = 0.;	
+
 	hitStruct.resX = res.x();
-	hitStruct.resY = res.y();
 	hitStruct.resErrX = errX;
 	hitStruct.resErrY = errY;
+
+	if(detUnit) {
+	  const Surface& surface = hit->detUnit()->surface();
+	  LocalPoint lPModule(0.,0.,0.), lPhiDirection(1.,0.,0.), lROrZDirection(0.,1.,0.);
+	  GlobalPoint gPModule       = surface.toGlobal(lPModule),
+	    gPhiDirection  = surface.toGlobal(lPhiDirection),
+	    gROrZDirection = surface.toGlobal(lROrZDirection);
+	  phiorientation = deltaPhi(gPhiDirection.phi(),gPModule.phi()) >= 0 ? +1. : -1.;
+
+	  dPhi = tsos.globalPosition().phi() - hit->globalPosition().phi();
+	  
+	  if(IntSubDetID == PixelSubdetector::PixelBarrel || IntSubDetID == PixelSubdetector::PixelEndcap || 
+	     IntSubDetID == StripSubdetector::TIB || 
+	     IntSubDetID == StripSubdetector::TOB) {
+	    hitStruct.resXprime = (res.x())*(phiorientation );
+	    dZ = gROrZDirection.z() - gPModule.z();
+	  } else if (IntSubDetID == StripSubdetector::TID || IntSubDetID == StripSubdetector::TEC) {
+	    const RadialStripTopology* theTopol = dynamic_cast<const RadialStripTopology*>(&(detUnit->topology()));
+	    origintointersect =  static_cast<float>(theTopol->originToIntersection());
+	    
+	    MeasurementPoint theMeasHitPos = theTopol->measurementPosition(hit->localPosition());
+	    MeasurementPoint theMeasStatePos = theTopol->measurementPosition(tsos.localPosition());
+	    Measurement2DVector residual =  theMeasStatePos - theMeasHitPos;
+	    
+	    MeasurementError theMeasHitErr = theTopol->measurementError(hit->localPosition(),err2);
+	    MeasurementError theMeasStateErr = theTopol->measurementError(tsos.localPosition(),err1);
+
+	    double localPitch = theTopol->localPitch(hit->localPosition());
+
+	    float measErr = std::sqrt( theMeasHitErr.uu()*localPitch*localPitch + theMeasStateErr.uu() );
+	    R = origintointersect;
+	    dR = theTopol->yDistanceToIntersection( tsos.localPosition().y()) - 
+	      theTopol->yDistanceToIntersection( hit->localPosition().y());
+	    
+	    hitStruct.resXprime = residual.x()*localPitch ;
+	    
+	  } else {
+	    edm::LogWarning("TrackerValidationVariables") << "@SUB=TrackerValidationVariables::fillHitQuantities" 
+							  << "No valid tracker subdetector " << IntSubDetID;
+	    hitStruct.resXprime = -999;
+	  }	 
+	  
+	}
+	
+	
+	if(dR != -999) hitStruct.resY = dR;
+	else if(dZ != -999) hitStruct.resY = res.y() * (dZ >=0.? +1 : -1) ;
+	else hitStruct.resY = res.y();
+	
 	hitStruct.rawDetId = IntRawDetID;
 	hitStruct.phi = tsos.globalDirection().phi();
 
-	
+	// first try for overlapp residuals
+	if(itTraj+1 != itTrajEnd) {
+	  TransientTrackingRecHit::ConstRecHitPointer hit2 = (itTraj+1)->recHit();
+	  TrackerAlignableId ali1, ali2;
+	  if(hit2->isValid() && 
+	     ali1.typeAndLayerFromDetId(hit->geographicalId()) == ali2.typeAndLayerFromDetId(hit2->geographicalId()) ) {
+	    TrajectoryStateOnSurface tsos2 = tsoscomb( (itTraj+1)->forwardPredictedState(), (itTraj+1)->backwardPredictedState() );
+	    align::LocalVector res2 = tsos2.localPosition() - hit2->localPosition();
+	    float overlapresidual = res2.x() - res.x();
+	    hitStruct.overlapres = std::make_pair(hit2->geographicalId().rawId(),overlapresidual);
+	  }
+	}
+
 	v_avhitout.push_back(hitStruct);
-      } 
+      }
     } 
   }  
 
-} 
+}
 
 void 
 TrackerValidationVariables::fillTrackQuantities(const edm::Event& iEvent,
