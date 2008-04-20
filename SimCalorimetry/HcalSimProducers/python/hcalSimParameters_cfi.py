d3546b0c6b6bca924fdc1b0dc406983bddc73fa5
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-20 23:47:48 +0000

    second run of translating configs

--- SimCalorimetry/HcalSimProducers/python/hcalSimParameters_cfi.py
+++ SimCalorimetry/HcalSimProducers/python/hcalSimParameters_cfi.py
@@ -1,6 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 hcalSimParameters = cms.PSet(
+    #  In HF, the SimHits energy is actually
+    # the number of photoelectrons from the shower
+    # library.  However, we need a lot more
+    # smearing, because the first level of the PMT
+    # only gives ~6 electrons per pe, which
+    # comes out to a 40% smearing of the single pe peak!
+    # 
     hf1 = cms.PSet(
         readoutFrameSize = cms.int32(6),
         binOfMaximum = cms.int32(4),
@@ -28,7 +35,10 @@ hcalSimParameters = cms.PSet(
         doPhotoStatistics = cms.bool(True),
         photoelectronsToAnalog = cms.double(0.3065),
         simHitToPhotoelectrons = cms.double(4000.0),
-        samplingFactors = cms.vdouble(217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0, 217.0),
+        samplingFactors = cms.vdouble(217.0, 217.0, 217.0, 217.0, 217.0, 
+            217.0, 217.0, 217.0, 217.0, 217.0, 
+            217.0, 217.0, 217.0, 217.0, 217.0, 
+            217.0),
         syncPhase = cms.bool(True),
         timePhase = cms.double(5.0)
     ),
@@ -39,7 +49,10 @@ hcalSimParameters = cms.PSet(
         doPhotoStatistics = cms.bool(True),
         photoelectronsToAnalog = cms.double(0.3305),
         simHitToPhotoelectrons = cms.double(2000.0),
-        samplingFactors = cms.vdouble(120.58, 120.58, 119.9, 120.14, 120.63, 120.21, 119.69, 119.55, 119.62, 119.6, 118.65, 119.38, 118.89, 120.34, 119.01, 132.37),
+        samplingFactors = cms.vdouble(120.58, 120.58, 119.9, 120.14, 120.63, 
+            120.21, 119.69, 119.55, 119.62, 119.6, 
+            118.65, 119.38, 118.89, 120.34, 119.01, 
+            132.37),
         syncPhase = cms.bool(True),
         timePhase = cms.double(5.0)
     ),
@@ -60,7 +73,9 @@ hcalSimParameters = cms.PSet(
         doPhotoStatistics = cms.bool(True),
         photoelectronsToAnalog = cms.double(0.3305),
         simHitToPhotoelectrons = cms.double(2000.0),
-        samplingFactors = cms.vdouble(196.83, 188.3, 175.24, 177.2, 178.45, 177.39, 178.18, 177.6, 178.26, 179.63, 179.98, 180.52, 174.63, 174.63),
+        samplingFactors = cms.vdouble(196.83, 188.3, 175.24, 177.2, 178.45, 
+            177.39, 178.18, 177.6, 178.26, 179.63, 
+            179.98, 180.52, 174.63, 174.63),
         syncPhase = cms.bool(True),
         timePhase = cms.double(5.0)
     )
