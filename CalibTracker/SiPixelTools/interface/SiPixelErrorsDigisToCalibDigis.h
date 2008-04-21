7542ec8007eb8128b903e540dc39faf67ece26fc
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-04-21 19:00:15 +0000

    Monitoring and booking the Errors produced during Digis to CalibDigis conversion

---
+++ CalibTracker/SiPixelTools/interface/SiPixelErrorsDigisToCalibDigis.h
@@ -0,0 +1,65 @@
+#ifndef SiPixelErrorsDigisToCalibDigis_h
+#define SiPixelErrorsDigisToCalibDigis_h
+
+/**  class SiPixelErrorsDigisToCalibDigis
+
+Description: Create monitorElements for the Errors in created in the reduction of digis to calibDigis
+
+**/
+
+// Original Author: Ricardo Vasquez Sierra on April 9, 2008
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/DetId/interface/DetId.h"
+
+
+#include "DataFormats/SiPixelDigi/interface/SiPixelCalibDigi.h"
+#include "DataFormats/SiPixelDigi/interface/SiPixelCalibDigiError.h"
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+//
+// class declaration
+//
+
+class SiPixelErrorsDigisToCalibDigis : public edm::EDAnalyzer {
+   public:
+      explicit SiPixelErrorsDigisToCalibDigis(const edm::ParameterSet&);
+      ~SiPixelErrorsDigisToCalibDigis();
+
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+
+      // store the number of error per detector id encountered
+      // store the location of the mismatching error in the detector id
+
+      edm::InputTag siPixelProducerLabel_;
+      DQMStore* daqBE_;
+      std::string outputFilename_;
+      bool createOutputFile_;
+
+      
+      std::map<uint32_t, MonitorElement*> SiPixelErrorsDigisToCalibDigis_2DErrorInformation_; 
+      
+};
+
+#endif
--- CalibTracker/SiPixelTools/src/SealModule.cc
+++ CalibTracker/SiPixelTools/src/SealModule.cc
@@ -4,5 +4,6 @@
 DEFINE_SEAL_MODULE();
 
 #include "CalibTracker/SiPixelTools/interface/SiPixelOfflineCalibAnalysisBase.h"
+#include "CalibTracker/SiPixelTools/interface/SiPixelErrorsDigisToCalibDigis.h"
 DEFINE_ANOTHER_FWK_MODULE(SiPixelOfflineCalibAnalysisBase);
-
+DEFINE_ANOTHER_FWK_MODULE(SiPixelErrorsDigisToCalibDigis);
