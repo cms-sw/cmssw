07c1262d6cc2095f10228f5280593b36c5bf39d9
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2008-04-21 17:24:45 +0000

    Fixing gren configuration files

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_ECALGREN_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_ECALGREN_cff.py
@@ -1,9 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_ECALGREN_cfi import *
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
     recordName = cms.string('L1RCTParametersRcd'),
     iovIsRunNotTime = cms.bool(True),
     firstValid = cms.vuint32(1)
 )
+
 
 
