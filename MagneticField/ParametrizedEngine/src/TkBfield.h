64a40d1a100a4168290a0a3a175c4e3e605b0d69
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-21 16:49:05 +0000

    New version optimized for r<1.15m and |z|<2.80m

--- MagneticField/ParametrizedEngine/src/TkBfield.h
+++ MagneticField/ParametrizedEngine/src/TkBfield.h
@@ -11,11 +11,11 @@
  *    Out:   B[3]: Bx,By,Bz    (T)    (getBxyz)
  *    Out:   B[3]: Br,Bf,Bz    (T)    (getBrfz)
  *
- *    Valid for r<1.1 and |z|<3.0
+ *    Valid for r<1.15m and |z|<2.80m
  *
- *  $Date: 2008/03/28 16:49:25 $
- *  $Revision: 1.1 $
- *  \author V.Karimaki 080228
+ *  $Date: 2008/04/01 08:32:41 $
+ *  $Revision: 1.2 $
+ *  \author V.Karimaki 080228, 080407
  */
 
 
@@ -34,6 +34,9 @@ namespace magfieldparam {
     void getBxyz(const double* x, double* B); 
     /// B out in cylindrical
     void getBrfz(const double* x, double* B);    
+
+    /// Define validity region (r, z in m)
+    bool isDefined(double r, double z);
 
   private:
     double prm[9];
