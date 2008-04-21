81580da3cc4467ece772dd87bf92a5fb486f59cc
meads <sha1-7052bbc2d9967a3e6c07145a863ac9557feddf7e@cern.ch>
2008-04-21 13:36:11 +0000

    Base classes for the source handler for the pixel popcon applications

---
+++ CondTools/SiPixel/interface/PixelPopConSourceHandler.h
@@ -0,0 +1,34 @@
+#ifndef PIXELPOPCONSOURCEHANDLER_H
+#define PIXELPOPCONSOURCEHANDLER_H
+
+// Package: CondTools/SiPixel
+// Class: PixelPopConSourceHandler
+/** \class PixelPopConSourceHandler PixelPopConSourceHandler.cc CondTools/SiPixel/src/PixelPopConSourceHandler.cc
+
+ Description: PopCon source handler base class for all pixel popcon applications
+
+ Implementation: 
+   <Notes on implementation>
+*/
+//
+// Original Author:  Michael Eads
+//         Created:  19 Apr 2008
+//
+//
+
+#include "CondCore/PopCon/interface/PopConSourceHandler.h"
+
+template <class T> 
+class PixelPopConSourceHandler : public popcon::PopConSourceHandler<T> {
+
+
+};
+
+
+
+
+
+
+
+
+#endif
