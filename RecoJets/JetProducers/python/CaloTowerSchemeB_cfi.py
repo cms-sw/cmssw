5e57a3acda708ea4e8b16d3318d5c49811cd6145
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-21 21:41:37 +0000

    second pass at translation

--- RecoJets/JetProducers/python/CaloTowerSchemeB_cfi.py
+++ RecoJets/JetProducers/python/CaloTowerSchemeB_cfi.py
@@ -1,15 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
 towerMaker = cms.EDFilter("CaloTowersCreator",
-    EBSumThreshold = cms.double(0.2),
+    EBSumThreshold = cms.double(0.2), ## GeV, Scheme B
+
     EBWeight = cms.double(1.0),
     hfInput = cms.InputTag("hfreco"),
     AllowMissingInputs = cms.untracked.bool(False),
-    EESumThreshold = cms.double(0.45),
-    HOThreshold = cms.double(1.1),
-    HBThreshold = cms.double(0.9),
-    EBThreshold = cms.double(0.09),
-    HcalThreshold = cms.double(-1000.0),
+    EESumThreshold = cms.double(0.45), ## GeV, Scheme B
+
+    HOThreshold = cms.double(1.1), ## GeV, Scheme B
+
+    HBThreshold = cms.double(0.9), ## GeV, Scheme B
+
+    EBThreshold = cms.double(0.09), ## GeV, ORCA value w/o selective readout
+
+    HcalThreshold = cms.double(-1000.0), ## GeV, -1000 means cut not used 
+
     HEDWeight = cms.double(1.0),
     EEWeight = cms.double(1.0),
     UseHO = cms.bool(True),
@@ -18,13 +24,19 @@ towerMaker = cms.EDFilter("CaloTowersCreator",
     HESWeight = cms.double(1.0),
     hbheInput = cms.InputTag("hbhereco"),
     HF2Weight = cms.double(1.0),
-    HF2Threshold = cms.double(1.8),
-    EEThreshold = cms.double(0.45),
-    HESThreshold = cms.double(1.4),
+    HF2Threshold = cms.double(1.8), ## GeV, Oprimized on 10% occupancy
+
+    EEThreshold = cms.double(0.45), ## GeV, ORCA value w/o selective readout
+
+    HESThreshold = cms.double(1.4), ## GeV, Scheme B
+
     hoInput = cms.InputTag("horeco"),
-    HF1Threshold = cms.double(1.2),
-    HEDThreshold = cms.double(1.4),
-    EcutTower = cms.double(-1000.0),
+    HF1Threshold = cms.double(1.2), ## GeV, Oprimized on 10% occupancy
+
+    HEDThreshold = cms.double(1.4), ## GeV, Scheme B
+
+    EcutTower = cms.double(-1000.0), ## GeV, -1000 means cut not used
+
     ecalInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE")),
     HBWeight = cms.double(1.0)
 )
