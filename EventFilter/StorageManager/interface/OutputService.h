92176544cc8e23ac73fe50096ffadae8a80c8438
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-21 12:14:13 +0000

    Added constness.

--- EventFilter/StorageManager/interface/OutputService.h
+++ EventFilter/StorageManager/interface/OutputService.h
@@ -1,7 +1,7 @@
 #ifndef OUTPUTSERVICE_H
 #define OUTPUTSERVICE_H
 
-// $Id:$
+// $Id: OutputService.h,v 1.1 2007/02/05 11:19:56 klute Exp $
 
 #include "IOPool/Streamer/interface/EventMessage.h"
 #include "IOPool/Streamer/interface/InitMessage.h"
@@ -27,14 +27,13 @@ namespace edm {
       double lastEntry() { return file_->lastEntry(); }
       void   report(std::ostream &os, int indentation) const;
 
-
     private:
       void   writeHeader(InitMsgView const&);
       void   closeFile();
-      double getTimeStamp();
+      double getTimeStamp() const;
 
       boost::shared_ptr<StreamerFileWriter> writer_; // writes streamer and index file
-      boost::shared_ptr<FileRecord>           file_;   // writes streamer and index file
+      boost::shared_ptr<FileRecord>           file_; // writes streamer and index file
  };
 
 } // edm namespace
