ad8fb20875b39c74837d64b1b205cd89235199c8
meads <sha1-7052bbc2d9967a3e6c07145a863ac9557feddf7e@cern.ch>
2008-04-21 14:37:54 +0000

    Define analyzer and source handler classes for pixel dcs cabling map popcon application. Just definitions - no real functionality.

---
+++ CondTools/SiPixel/src/PixelPopConDCSCablingSourceHandler.cc
@@ -0,0 +1,9 @@
+// Package: CondTools/SiPixel
+// Class:   PixelPopConDCSCablingSourceHandler
+//
+// Description: Source handler class for pixel DCS cabling map
+//              popcon application
+//
+// Created: M. Eads, Apr 2008
+
+#include "CondTools/SiPixel/interface/PixelPopConDCSCablingSourceHandler.h"
