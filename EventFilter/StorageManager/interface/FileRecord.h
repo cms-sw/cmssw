9d3c55dcff39c9b09bba995d2f07196c0ea0f443
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-21 12:13:35 +0000

    Removed script handling. Scripts hooks should be removed in a later version

--- EventFilter/StorageManager/interface/FileRecord.h
+++ EventFilter/StorageManager/interface/FileRecord.h
@@ -1,7 +1,7 @@
 #ifndef FILERECORD_H
 #define FILERECORD_H
 
-// $Id: FileRecord.h,v 1.3.2.1 2008/03/07 20:41:29 biery Exp $
+// $Id: FileRecord.h,v 1.4 2008/03/10 14:50:07 biery Exp $
 #include <EventFilter/StorageManager/interface/Parameter.h>
 #include <boost/shared_ptr.hpp>
 #include <string>
@@ -12,11 +12,11 @@ namespace edm {
   class FileRecord
     {
     public:
-      FileRecord(int, std::string, std::string);
+      FileRecord(int lumi, const std::string &file, const std::string &path);
       ~FileRecord() {}
 
       void   report(std::ostream &os, int indentation) const;
-      void   setFileCounter(int i)  { fileCounter_ = i; }
+      void   setFileCounter(int i)         { fileCounter_ = i; }
       void   fileSystem(int);
       void   writeToSummaryCatalog();
       void   writeToMailBox();
@@ -24,60 +24,59 @@ namespace edm {
       void   updateDatabase();
       void   insertFileInDatabase();
       void   moveFileToClosed();
-      void   firstEntry(double d)   { firstEntry_ = d; }
-      void   lastEntry(double d)    { lastEntry_  = d; }
-      void   increaseFileSize(int i){ fileSize_   += (long long) i; }
-      void   increaseEventCount()   { events_++; }
-      void   checkDirectories();
-      void   setCatalog(std::string s)     { statFileName_ = s; }
-      void   setRunNumber(int i)           { runNumber_ = i; }
-      void   setStreamLabel(std::string s) { streamLabel_ = s;}
-      void   setSetupLabel(std::string s)  { setupLabel_ = s;}
+      void   firstEntry(double d)          { firstEntry_ = d; }
+      void   lastEntry(double d)           { lastEntry_  = d; }
+      void   increaseFileSize(int i)       { fileSize_   += (long long) i; }
+      void   increaseEventCount()          { events_++; }
+      void   checkDirectories()      const;
+      void   setRunNumber(int i)                     { runNumber_ = i; }
+      void   setStreamLabel(const std::string &s)    { streamLabel_ = s;}
+      void   setSetupLabel(const std::string &s)     { setupLabel_ = s;}
+      void   setNotifyFile(const std::string &f)     { notifyFileName_ = f; }
 
-      std::string fileName()             { return fileName_; }
-      std::string basePath()             { return basePath_; }
-      std::string fileSystem()           { return basePath_ + fileSystem_; }
-      std::string workingDir()           { return basePath_ + fileSystem_ + workingDir_; }
-
-      std::string fileCounterStr();
-      std::string filePath();
-      std::string completeFileName();
-      std::string timeStamp(double);
+      const std::string& fileName()  const { return fileName_; }
+      const std::string& basePath()  const { return basePath_; }
+      std::string fileSystem()       const { return basePath_ + fileSystem_; }
+      std::string workingDir()       const { return basePath_ + fileSystem_ + workingDir_; }
+      std::string fileCounterStr()   const;
+      std::string filePath()         const;
+      std::string completeFileName() const;
+      std::string timeStamp(double)  const;
+      std::string logFile()          const;
+      std::string notFile()          const;
      
-      int    lumiSection()          { return lumiSection_; }
-      int    fileCounter()          { return fileCounter_; }
-      long long fileSize()          { return fileSize_; }
-      int    events()               { return events_; }
-
-      double lastEntry()            { return lastEntry_; }
-      double firstEntry()           { return firstEntry_; }
+      int         lumiSection()      const { return lumiSection_; }
+      int         fileCounter()      const { return fileCounter_; }
+      long long   fileSize()         const { return fileSize_; }
+      int         events()           const { return events_; } 
+      double      lastEntry()        const { return lastEntry_; }
+      double      firstEntry()       const { return firstEntry_; }
 
     private:
-      std::string fileName_;                              // file name ( w/o ending )
-      std::string basePath_;                              // base path name
-      std::string fileSystem_;                            // file system directory
-      std::string workingDir_;                            // current working directory
-
-      std::string statFileName_;                          // catalog file name
-      std::string mailBoxPath_;                           // mail box path
-
-      std::string setupLabel_;
-      std::string streamLabel_;
+      std::string fileName_;                         // file name (w/o ending)
+      std::string basePath_;                         // base path name
+      std::string fileSystem_;                       // file system directory
+      std::string workingDir_;                       // current working directory
+      std::string mailBoxPath_;                      // mail box path
+      std::string logPath_;                          // log path
+      std::string logFile_;                          // log file including path
+      std::string setupLabel_;                       // setup label
+      std::string streamLabel_;                      // datastream label
+      std::string notifyFileName_;                   // notify file name
       
-      int    lumiSection_;                           // luminosity section  
-      int    runNumber_;                             // runNumber
-      int    fileCounter_;                           // number of files with fileName_ as name
-      long long fileSize_;                           // current file size
-      int    events_;                                // total number of events
-      double firstEntry_;                            // time when last event was writen
-      double lastEntry_;                             // time when last event was writen
- 
-      boost::shared_ptr<stor::Parameter> smParameter_;     // 
+      int         lumiSection_;                      // luminosity section  
+      int         runNumber_;                        // runNumber
+      int         fileCounter_;                      // number of files with fileName_ as name
+      long long   fileSize_;                         // current file size
+      int         events_;                           // total number of events
+      double      firstEntry_;                       // time when last event was writen
+      double      lastEntry_;                        // time when last event was writen
+
+      boost::shared_ptr<stor::Parameter> smParameter_; // 
       
-      void   checkDirectory(std::string);
-      double calcPctDiff(long long, long long);
+      void   checkDirectory(const std::string &) const;
+      double calcPctDiff(long long, long long) const;
    };
-
  
 } // edm namespace
 #endif
