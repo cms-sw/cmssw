d160213103c9c0806c0e00a64340ff27d8ddab19
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-21 18:47:48 +0000

    Use the new name for the muonCSCDigis file, and not just the wrapper

--- SimMuon/Configuration/python/SimMuon_cff.py
+++ SimMuon/Configuration/python/SimMuon_cff.py
@@ -1,14 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-# Muon Digitization (CSC, DT, RPC electronics responce)
-# CSC digitizer
-#
-from SimMuon.CSCDigitizer.muoncscdigi_cfi import *
-# DT digitizer
-#
+from SimMuon.CSCDigitizer.muonCSCDigis_cfi import *
 from SimMuon.DTDigitizer.muondtdigi_cfi import *
-# RPC digitizer
-# 
 from SimMuon.RPCDigitizer.muonrpcdigi_cfi import *
 muonDigi = cms.Sequence(muonCSCDigis+muonDTDigis+muonRPCDigis)
 
