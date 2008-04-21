b6e2267f96382c19a2812eed037c7a68ff57d67c
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-04-21 17:24:50 +0000

    Fixed L1CaloGeometryRecord include file.

--- CondFormats/DataRecord/src/L1CaloGeometryRecord.cc
+++ CondFormats/DataRecord/src/L1CaloGeometryRecord.cc
@@ -8,10 +8,10 @@
 //
 // Original Author:  Werner Sun
 //         Created:  Mon Oct 23 23:10:22 EDT 2006
-// $Id: L1CaloGeometryRecord.cc,v 1.1 2006/12/21 01:55:35 wsun Exp $
+// $Id: L1CaloGeometryRecord.cc,v 1.1 2008/04/16 23:06:56 wsun Exp $
 //
 
-#include "L1TriggerConfig/L1Geometry/interface/L1CaloGeometryRecord.h"
+#include "CondFormats/DataRecord/interface/L1CaloGeometryRecord.h"
 #include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
 
 EVENTSETUP_RECORD_REG(L1CaloGeometryRecord);
