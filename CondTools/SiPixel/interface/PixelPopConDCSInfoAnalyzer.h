378fee3cb32b7e0c963e1a6baec2e89402aaad18
meads <sha1-7052bbc2d9967a3e6c07145a863ac9557feddf7e@cern.ch>
2008-04-21 14:43:44 +0000

    Define analyzer and source handler classes for dcs info pixel popcon application. Definitions only - no real functionality.

---
+++ CondTools/SiPixel/interface/PixelPopConDCSInfoAnalyzer.h
@@ -0,0 +1,15 @@
+#ifndef PIXELPOPCONDCSINFOANALYZER_H
+#define PIXELPOPCONDCSINFOANALYZER_H
+
+// Package: CondTools/SiPixel
+// Class:   PixelPopConDCSInfoAnalyzer
+//
+// Description: Analyzer class for pixel dcs info
+//              popcon application
+//
+// Created: M. Eads, Apr 2008
+
+
+
+
+#endif
