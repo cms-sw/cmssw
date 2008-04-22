c9399cdad3173455299f7b102d7d1bd7ed10d76c
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2008-04-22 12:19:51 +0000

    config files update

--- L1Trigger/DTTrigger/python/dtTriggerPrimitiveDigis_cfi.py
+++ L1Trigger/DTTrigger/python/dtTriggerPrimitiveDigis_cfi.py
@@ -3,23 +3,20 @@ import FWCore.ParameterSet.Config as cms
 dtTriggerPrimitiveDigis = cms.EDProducer("DTTrigProd",
     debug = cms.untracked.bool(False),
     tTrigModeConfig = cms.PSet(
-        vPropWire = cms.double(24.4),
-        doTOFCorrection = cms.bool(False),
-        tofCorrType = cms.int32(1),
+        debug = cms.untracked.bool(False),
         kFactor = cms.double(-2.0), ##retuned in CMSSW15X
 
-        wirePropCorrType = cms.int32(1),
-        doWirePropCorrection = cms.bool(False),
-        doT0Correction = cms.bool(True), ##FIXME: remove, not anymore needed from CMSSW180pre1
-
-        debug = cms.untracked.bool(False)
+        vPropWire = cms.double(24.4),
+        tofCorrType = cms.int32(1),
+        tTrig = cms.double(500.0)
     ),
     # Synchronizer related stuff
-    tTrigMode = cms.string('DTTTrigSyncFromDB'),
+    tTrigMode = cms.string('DTTTrigSyncTOFCorr'),
     # Convert output into DTTF sector numbering: 
     # false means [1-12] (useful for debug)
     # true is [0-11] useful as input for the DTTF emulator
     DTTFSectorNumbering = cms.bool(True)
 )
+
 
 
--- L1Trigger/DTTrigger/python/dttriganalyzer_cfi.py
+++ L1Trigger/DTTrigger/python/dttriganalyzer_cfi.py
@@ -1,26 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
-from L1Trigger.DTTrigger.dttpg_conf_cff import *
 dttriganalyzer = cms.EDFilter("DTTrigTest",
-    # Incude configuration parametersets
-    DTTPGParametersBlock,
     debug = cms.untracked.bool(False),
     tTrigModeConfig = cms.PSet(
-        vPropWire = cms.double(24.4),
-        doTOFCorrection = cms.bool(False),
-        tofCorrType = cms.int32(1),
+        debug = cms.untracked.bool(False),
         kFactor = cms.double(-2.0), ##retuned in CMSSW15X
 
-        wirePropCorrType = cms.int32(1),
-        doWirePropCorrection = cms.bool(False),
-        doT0Correction = cms.bool(True), ##FIXME: remove, not anymore needed from CMSSW180pre1
-
-        debug = cms.untracked.bool(False)
+        vPropWire = cms.double(24.4),
+        tofCorrType = cms.int32(1),
+        tTrig = cms.double(500.0)
     ),
     # Synchronizer related stuff
-    tTrigMode = cms.string('DTTTrigSyncFromDB'),
+    tTrigMode = cms.string('DTTTrigSyncTOFCorr'),
     # Output filename
     outputFileName = cms.untracked.string('DTTPG_test.root')
 )
+
 
 
