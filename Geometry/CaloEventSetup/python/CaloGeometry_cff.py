60c9e16e646295c0f62a9b38006d7ba46499ae51
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-22 21:25:32 +0000

    python needs to use 'False' with a capital F

--- Geometry/CaloEventSetup/python/CaloGeometry_cff.py
+++ Geometry/CaloEventSetup/python/CaloGeometry_cff.py
@@ -4,19 +4,19 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the ECAL + HCAL geometry model
 #
 EcalBarrelGeometryEP = cms.ESProducer("EcalBarrelGeometryEP",
-                                      applyAlignment = cms.untracked.bool(false) )
+                                      applyAlignment = cms.untracked.bool(False) )
 
 EcalEndcapGeometryEP = cms.ESProducer("EcalEndcapGeometryEP",
-                                      applyAlignment = cms.untracked.bool(false))
+                                      applyAlignment = cms.untracked.bool(False))
 
 EcalPreshowerGeometryEP = cms.ESProducer("EcalPreshowerGeometryEP",
-                                      applyAlignment = cms.untracked.bool(false))
+                                      applyAlignment = cms.untracked.bool(False))
 
 HcalHardcodeGeometryEP = cms.ESProducer("HcalHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(false))
+                                      applyAlignment = cms.untracked.bool(False))
 
 ZdcHardcodeGeometryEP = cms.ESProducer("ZdcHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(false))
+                                      applyAlignment = cms.untracked.bool(False))
 
 CaloTowerHardcodeGeometryEP = cms.ESProducer("CaloTowerHardcodeGeometryEP")
 
