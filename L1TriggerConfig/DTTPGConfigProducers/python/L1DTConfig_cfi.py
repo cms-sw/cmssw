2b216bb6c43629d4ef161dd98f497107a3e18260
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2008-04-22 12:20:56 +0000

    config files update (moved .cff from L1Trigger/DT*)

--- L1TriggerConfig/DTTPGConfigProducers/python/L1DTConfig_cfi.py
+++ L1TriggerConfig/DTTPGConfigProducers/python/L1DTConfig_cfi.py
@@ -1,11 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 #Include configuration ParameterSets
-from L1Trigger.DTTrigger.dttpg_conf_cff import *
+from L1TriggerConfig.DTTPGConfigProducers.L1DTConfigParams_cff import *
 from L1TriggerConfig.DTTPGConfigProducers.L1DTConfigMap_cff import *
 L1DTConfig = cms.ESProducer("DTConfigTrivialProducer",
     DTTPGMapBlock,
     DTTPGParametersBlock
 )
+
 
 
