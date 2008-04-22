2db9c0108ed277f5aebb0beeccd0fabf8472ecf6
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-22 17:13:10 +0000

    Accidentally committed the include where I factored this out, so might as well

---
+++ FWCore/ParameterSet/src/PythonWrapper.cc
@@ -0,0 +1,45 @@
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/ParameterSet/src/PythonWrapper.h"
+#include <iostream>
+namespace edm {
+
+void pythonToCppException(const std::string& iType)
+ {
+  using namespace boost::python;
+  PyObject *exc=NULL, *val=NULL, *trace=NULL;
+  PyErr_Fetch(&exc,&val,&trace);
+  PyErr_NormalizeException(&exc,&val,&trace);
+  handle<> hExc(allow_null(exc));
+  handle<> hVal(allow_null(val));
+  handle<> hTrace(allow_null(trace));
+  std::cout << "HTRACE ? " << hTrace << std::endl;
+  if(hTrace) {
+    object oTrace(hTrace);
+    handle<> hStringTr(PyObject_Str(oTrace.ptr()));
+    object stringTr(hStringTr);
+//std::cout << "PR TR " << stringTr <<  " DONE "<<  std::endl;
+PyErr_Print();
+  }
+
+  if(hVal && hExc) {
+    object oExc(hExc);
+    object oVal(hVal);
+    handle<> hStringVal(PyObject_Str(oVal.ptr()));
+    object stringVal( hStringVal );
+
+    handle<> hStringExc(PyObject_Str(oExc.ptr()));
+    object stringExc( hStringExc);
+
+    //PyErr_Print();
+    throw cms::Exception(iType) <<"python encountered the error: "
+                                << PyString_AsString(stringExc.ptr())<<" "
+                                << " VAL " 
+                                << PyString_AsString(stringVal.ptr())<<"\n";
+  } else {
+    throw cms::Exception(iType)<<" unknown python problem occurred.\n";
+  }
+}
+
+}
+
+
