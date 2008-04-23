e2307f2059364119e6b656231d05f315d552a18c
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-23 22:20:48 +0000

    New translation: picks up CondDBSetUp

--- FastSimulation/Configuration/python/GlobalTag_cfi.py
+++ FastSimulation/Configuration/python/GlobalTag_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
 GlobalTag = cms.ESSource("PoolDBESSource",
     CondDBSetup,
     connect = cms.string('frontier://FrontierProd/CMS_COND_20X_GLOBALTAG'), ##FrontierProd/CMS_COND_20X_GLOBALTAG"
