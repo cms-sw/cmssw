940ad3cedba098bd55c4b72a74b2ad1c2e3a68ef
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2008-04-23 09:23:24 +0000

    Update L1RCTConfig-GREN.cff and .py files

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_GREN_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_GREN_cff.py
@@ -13,27 +13,36 @@ RCTConfigProducers = cms.ESProducer("RCTConfigProducers",
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0),
-    eMaxForFGCut = cms.double(-999.0),
+    eMaxForFGCut = cms.double(-999.0), ## to disable it
+
     noiseVetoHB = cms.bool(False),
-    eMaxForHoECut = cms.double(-999.0),
-    hOeCut = cms.double(999.0),
+    eMaxForHoECut = cms.double(-999.0), ## disabled here
+
+    hOeCut = cms.double(999.0), ## H/E not used
+
     eGammaECalScaleFactors = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0, 1.0, 1.0, 
         1.0, 1.0, 1.0),
-    eMinForHoECut = cms.double(999.0),
+    eMinForHoECut = cms.double(999.0), ## H/E cut not used, 
+
+    hActivityCut = cms.double(999.0), ## not used in GR
+
+    eActivityCut = cms.double(999.0), ## Activity bits for tau calc
+
     jetMETHCalScaleFactors = cms.vdouble(0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0),
-    eActivityCut = cms.double(999.0),
-    jetMETLSB = cms.double(1.0),
     noiseVetoHEplus = cms.bool(False),
-    hActivityCut = cms.double(999.0),
+    eicIsolationThreshold = cms.double(0.0), ## force non-isolation
+
+    jetMETLSB = cms.double(1.0),
+    # jetMET path not used
     jetMETECalScaleFactors = cms.vdouble(0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0, 0.0, 0.0, 
@@ -41,9 +50,12 @@ RCTConfigProducers = cms.ESProducer("RCTConfigProducers",
         0.0, 0.0, 0.0, 0.0, 0.0, 
         0.0, 0.0, 0.0),
     eGammaLSB = cms.double(1.0),
-    eMinForFGCut = cms.double(999.0),
-    hMinForHoECut = cms.double(999.0),
+    eMinForFGCut = cms.double(999.0), ## FG cut not used, this serves
+
+    hMinForHoECut = cms.double(999.0), ##
+
     noiseVetoHEminus = cms.bool(False)
 )
+
 
 
