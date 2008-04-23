061b02c9ad625f15d17c6e292a727e28a3831721
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-23 14:33:13 +0000

    impr. readability

--- MagneticField/Engine/python/volumeBasedMagneticField_1103l_cfi.py
+++ MagneticField/Engine/python/volumeBasedMagneticField_1103l_cfi.py
@@ -1,13 +1,14 @@
-# The following comments couldn't be translated into the new config version:
-
-# avoid interference with EmptyESSource in uniformMagneticField.cfi
-
 import FWCore.ParameterSet.Config as cms
 
 # This cfi contains everything needed to use the VolumeBased magnetic
 # field engine version 1103l
+
 from MagneticField.GeomBuilder.cmsMagneticFieldXML_1103l_cfi import *
+
+# avoid interference with EmptyESSource in uniformMagneticField.cfi
 es_prefer_magfield = cms.ESPrefer("XMLIdealGeometryESSource","magfield")
+
+
 ParametrizedMagneticFieldProducer = cms.ESProducer("ParametrizedMagneticFieldProducer",
     version = cms.string('OAE_1103l_071212'),
     parameters = cms.PSet(
@@ -15,6 +16,7 @@ ParametrizedMagneticFieldProducer = cms.ESProducer("ParametrizedMagneticFieldPro
     ),
     label = cms.untracked.string('parametrizedField')
 )
+
 
 VolumeBasedMagneticFieldESProducer = cms.ESProducer("VolumeBasedMagneticFieldESProducer",
     timerOn = cms.untracked.bool(False),
--- MagneticField/Engine/python/volumeBasedMagneticField_85l_cfi.py
+++ MagneticField/Engine/python/volumeBasedMagneticField_85l_cfi.py
@@ -1,13 +1,14 @@
-# The following comments couldn't be translated into the new config version:
-
-# avoid interference with EmptyESSource in uniformMagneticField.cfi
-
 import FWCore.ParameterSet.Config as cms
 
 # This cfi contains everything needed to use the VolumeBased magnetic
 # field engine version 85l
+
 from MagneticField.GeomBuilder.cmsMagneticFieldXML_85l_cfi import *
+
+# avoid interference with EmptyESSource in uniformMagneticField.cfi
 es_prefer_magfield = cms.ESPrefer("XMLIdealGeometryESSource","magfield")
+
+
 ParametrizedMagneticFieldProducer = cms.ESProducer("ParametrizedMagneticFieldProducer",
     version = cms.string('OAE_85l_030919'),
     parameters = cms.PSet(
@@ -17,6 +18,7 @@ ParametrizedMagneticFieldProducer = cms.ESProducer("ParametrizedMagneticFieldPro
     ),
     label = cms.untracked.string('parametrizedField')
 )
+
 
 VolumeBasedMagneticFieldESProducer = cms.ESProducer("VolumeBasedMagneticFieldESProducer",
     timerOn = cms.untracked.bool(False),
