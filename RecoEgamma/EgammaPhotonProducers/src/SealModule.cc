5b8721b2e23f2e1b0467a7cbedd74339dd389af0
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-04-23 20:05:12 +0000

    Remove the PhotonCorrectionProducer

--- RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
+++ RecoEgamma/EgammaPhotonProducers/src/SealModule.cc
@@ -2,7 +2,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/PhotonProducer.h"
-#include "RecoEgamma/EgammaPhotonProducers/interface/PhotonCorrectionProducer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/ConvertedPhotonProducer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/ConversionTrackCandidateProducer.h"
 #include "RecoEgamma/EgammaPhotonProducers/interface/TrackProducerWithSCAssociation.h"
@@ -14,7 +13,6 @@ DEFINE_SEAL_MODULE();
 
 
 DEFINE_ANOTHER_FWK_MODULE(PhotonProducer);
-DEFINE_ANOTHER_FWK_MODULE(PhotonCorrectionProducer);
 DEFINE_ANOTHER_FWK_MODULE(ConvertedPhotonProducer);
 DEFINE_ANOTHER_FWK_MODULE(ConversionTrackCandidateProducer);
 DEFINE_ANOTHER_FWK_MODULE(TrackProducerWithSCAssociation);
