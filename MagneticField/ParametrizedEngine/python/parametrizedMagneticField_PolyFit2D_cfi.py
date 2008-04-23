d09ea263a96ca1d5948d2876304ebde21f4cf56f
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-04-23 14:50:47 +0000

    Vassili's parametrization

---
+++ MagneticField/ParametrizedEngine/python/parametrizedMagneticField_PolyFit2D_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+idealMagneticFieldRecordSource = cms.ESSource("EmptyESSource",
+    recordName = cms.string('IdealMagneticFieldRecord'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
+
+ParametrizedMagneticFieldProducer = cms.ESProducer("ParametrizedMagneticFieldProducer",
+    version = cms.string('PolyFit2D'),
+    parameters = cms.PSet(
+        BValue = cms.double(4.01242188708911)
+    ),
+    label = cms.untracked.string('')
+)
+
+
