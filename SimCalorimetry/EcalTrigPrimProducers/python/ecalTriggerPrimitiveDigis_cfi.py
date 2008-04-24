614362a8e9a4a1bb725b0acd5adf66eb96693919
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-04-24 09:40:10 +0000

    migration to the new naming of digi collections

--- SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_cfi.py
+++ SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_cfi.py
@@ -3,16 +3,15 @@ import FWCore.ParameterSet.Config as cms
 #
 # attention: default is changed to work on unsuppressed digis!! ##############
 #
-ecalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
+simEcalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
     BarrelOnly = cms.bool(False),
     InstanceEB = cms.string(''),
     InstanceEE = cms.string(''),
     binOfMaximum = cms.int32(6), ## optional from release 200 on, from 1-10
-
     Famos = cms.bool(False),
     TcpOutput = cms.bool(False),
     Debug = cms.bool(False),
-    Label = cms.string('ecalUnsuppressedDigis')
+    Label = cms.string('simEcalUnsuppressedDigis')
 )
 
 
--- SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_with_suppressed_cfi.py
+++ SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_with_suppressed_cfi.py
@@ -3,16 +3,15 @@ import FWCore.ParameterSet.Config as cms
 #
 # attention: default is changed to work on unsuppressed digis!! ##############
 #
-ecalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
+simEcalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
     BarrelOnly = cms.bool(False),
     InstanceEB = cms.string('ebDigis'),
     InstanceEE = cms.string('eeDigis'),
-    binOfMaximum = cms.int32(6), ## optional from release 200 on, from 1-10
-
+    binOfMaximum = cms.int32(6), ## optional from release 200 on, from 1-10xsxsxd
     Famos = cms.bool(False),
     TcpOutput = cms.bool(False),
     Debug = cms.bool(False),
-    Label = cms.string('ecalDigis')
+    Label = cms.string('simEcalDigis')
 )
 
 
