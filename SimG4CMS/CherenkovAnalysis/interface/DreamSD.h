b79fc2144f959f58691f2497554e183500d38a6b
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2008-04-24 09:42:12 +0000

    First steps towards implementation of Cherenkov light contribution.

--- SimG4CMS/CherenkovAnalysis/interface/DreamSD.h
+++ SimG4CMS/CherenkovAnalysis/interface/DreamSD.h
@@ -6,6 +6,8 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "G4String.hh"
+#include "G4PhysicsOrderedFreeVector.hh"
+
 #include <map>
 
 class G4LogicalVolume;
@@ -16,19 +18,35 @@ public:
 
   DreamSD(G4String, const DDCompactView &, SensitiveDetectorCatalog &, 
 	  edm::ParameterSet const &, const SimTrackManager*);
-  virtual ~DreamSD();
+  virtual ~DreamSD() {}
   virtual double                    getEnergyDeposit(G4Step*);
   virtual uint32_t                  setDetUnitId(G4Step*);
 
 private:    
 
-  void                              initMap(G4String, const DDCompactView &);
-  double                            curve_LY(G4Step*); 
-  double                            crystalLength(G4LogicalVolume*);
-  bool                              useBirk;
+  void   initMap(G4String, const DDCompactView &);
+  double curve_LY(G4Step*); 
+  double crystalLength(G4LogicalVolume*);
+  /// Returns the total energy due to Cherenkov radiation
+  double cherenkovDeposit_( G4Step* aStep );
+  /// Returns average number of photons created by track
+  double getAverageNumberOfPhotons_( const double charge,
+                                     const double beta,
+                                     const G4Material* aMaterial,
+                                     const G4MaterialPropertyVector* rIndex ) const;
+  /// Returns energy deposit for a given photon
+  double getPhotonEnergyDeposit_( );
+  /// Sets material properties at run-time...
+  bool setPbWO2MaterialProperties_( G4Material* aMaterial );
+
+  bool                              useBirk,doCherenkov_;
   double                            birk1, birk2;
   double                            slopeLY;
   std::map<G4LogicalVolume*,double> xtalLMap;
+
+  /// Table of Cherenkov angle integrals vs photon momentum
+  std::auto_ptr<G4PhysicsOrderedFreeVector> chAngleIntegrals_;
+
 };
 
 #endif // DreamSD_h
--- SimG4CMS/CherenkovAnalysis/src/DreamSD.cc
+++ SimG4CMS/CherenkovAnalysis/src/DreamSD.cc
@@ -11,7 +11,9 @@
 #include "G4Step.hh"
 #include "G4Track.hh"
 #include "G4VProcess.hh"
+#include "G4Poisson.hh"
 
+//________________________________________________________________________________________
 DreamSD::DreamSD(G4String name, const DDCompactView & cpv,
 	       SensitiveDetectorCatalog & clg, 
 	       edm::ParameterSet const & p, const SimTrackManager* manager) : 
@@ -19,29 +21,30 @@ DreamSD::DreamSD(G4String name, const DDCompactView & cpv,
 
   edm::ParameterSet m_EC = p.getParameter<edm::ParameterSet>("ECalSD");
   useBirk= m_EC.getParameter<bool>("UseBirkLaw");
+  doCherenkov_ = m_EC.getParameter<bool>("doCherenkov");
   birk1  = m_EC.getParameter<double>("BirkC1")*(g/(MeV*cm2));
   birk2  = m_EC.getParameter<double>("BirkC2")*(g/(MeV*cm2))*(g/(MeV*cm2));
   slopeLY= m_EC.getParameter<double>("SlopeLightYield");
   
-  edm::LogInfo("EcalSim")  << "Constructing a DreamSD  with name " 
-			   << GetName() << "\n"
+  edm::LogInfo("EcalSim")  << "Constructing a DreamSD  with name " << GetName() << "\n"
 			   << "DreamSD:: Use of Birks law is set to      " 
 			   << useBirk << "        with the two constants C1 = "
 			   << birk1 << ", C2 = " << birk2 << "\n"
 			   << "         Slope for Light yield is set to "
-			   << slopeLY;
+			   << slopeLY << "\n"
+                           << "         Parameterization of Cherenkov is set to " 
+                           << doCherenkov_;
 
   initMap(name,cpv);
 
 }
 
-DreamSD::~DreamSD() {}
-
+//________________________________________________________________________________________
 double DreamSD::getEnergyDeposit(G4Step * aStep) {
-  
-  if (aStep == NULL) {
-    return 0;
-  } else {
+
+  double edep = 0;
+
+  if ( aStep ) {
     preStepPoint        = aStep->GetPreStepPoint();
     G4String nameVolume = preStepPoint->GetPhysicalVolume()->GetName();
 
@@ -49,20 +52,32 @@ double DreamSD::getEnergyDeposit(G4Step * aStep) {
     double weight = 1.;
     weight *= curve_LY(aStep);
     if (useBirk)   weight *= getAttenuation(aStep, birk1, birk2);
-    double edep   = aStep->GetTotalEnergyDeposit() * weight;
+    edep    = aStep->GetTotalEnergyDeposit() * weight;
     LogDebug("EcalSim") << "DreamSD:: " << nameVolume
 			<<" Light Collection Efficiency " << weight 
 			<< " Weighted Energy Deposit " << edep/MeV << " MeV";
-    return edep;
+
+    // Get cherenkov contribution
+    if ( doCherenkov_ ) {
+      edep += cherenkovDeposit_( aStep );
+    }
+
   } 
+
+  return edep;
+
 }
 
+
+//________________________________________________________________________________________
 uint32_t DreamSD::setDetUnitId(G4Step * aStep) { 
 
   const G4VTouchable* touch = aStep->GetPostStepPoint()->GetTouchable();
   return touch->GetReplicaNumber(0);
 }
 
+
+//________________________________________________________________________________________
 void DreamSD::initMap(G4String sd, const DDCompactView & cpv) {
 
   G4String attribute = "ReadOutName";
@@ -110,6 +125,7 @@ void DreamSD::initMap(G4String sd, const DDCompactView & cpv) {
   }
 }
 
+//________________________________________________________________________________________
 double DreamSD::curve_LY(G4Step* aStep) {
 
   G4StepPoint*     stepPoint = aStep->GetPreStepPoint();
@@ -120,7 +136,7 @@ double DreamSD::curve_LY(G4Step* aStep) {
   G4ThreeVector  localPoint = setToLocal(stepPoint->GetPosition(),
 					 stepPoint->GetTouchable());
   double crlength = crystalLength(lv);
-  double dapd = 0.5 * crlength - localPoint.z();
+  double dapd = 0.5 * crlength - localPoint.z(); // Distance from closest APD
   if (dapd >= -0.1 || dapd <= crlength+0.1) {
     if (dapd <= 100.)
       weight = 1.0 + slopeLY - dapd * 0.01 * slopeLY;
@@ -139,10 +155,219 @@ double DreamSD::curve_LY(G4Step* aStep) {
   return weight;
 }
 
+//________________________________________________________________________________________
 double DreamSD::crystalLength(G4LogicalVolume* lv) {
 
   double length= 230.;
   std::map<G4LogicalVolume*,double>::const_iterator ite = xtalLMap.find(lv);
   if (ite != xtalLMap.end()) length = ite->second;
   return length;
+}
+
+
+//________________________________________________________________________________________
+// Calculate total cherenkov deposit
+// Inspired by Geant4's Cherenkov implementation
+double DreamSD::cherenkovDeposit_( G4Step* aStep ) {
+
+  double cherenkovEnergy = 0;
+
+  // Get the material and set properties if needed
+  G4Material* material = aStep->GetTrack()->GetMaterial();
+  G4MaterialPropertiesTable* materialPropertiesTable = material->GetMaterialPropertiesTable();
+  if ( !materialPropertiesTable ) {
+    if ( !setPbWO2MaterialProperties_( material ) ) {
+      edm::LogWarning("EcalSim") << "Couldn't retrieve material properties table\n"
+                                 << " Material = " << material->GetName();
+      return cherenkovEnergy;
+    }
+    materialPropertiesTable = material->GetMaterialPropertiesTable();
+  }
+
+  // Retrieve refractive index
+  const G4MaterialPropertyVector* Rindex = materialPropertiesTable->GetProperty("RINDEX"); 
+  if ( Rindex == NULL ) {
+    edm::LogWarning("EcalSim") << "Couldn't retrieve refractive index";
+    return cherenkovEnergy;
+  }
+
+  LogDebug("EcalSim") << "Material properties: " << "\n"
+                      << "  Pmin = " << Rindex->GetMinPhotonMomentum()
+                      << "  Pmax = " << Rindex->GetMaxPhotonMomentum();
+  
+  // Get particle properties
+  G4StepPoint* pPreStepPoint  = aStep->GetPreStepPoint();
+  G4StepPoint* pPostStepPoint = aStep->GetPostStepPoint();
+  const G4DynamicParticle* aParticle = aStep->GetTrack()->GetDynamicParticle();
+  const double charge = aParticle->GetDefinition()->GetPDGCharge();
+  // beta is averaged over step
+  const double beta = 0.5*( pPreStepPoint->GetBeta() + pPostStepPoint->GetBeta() );
+
+  LogDebug("EcalSim") << "Particle properties: " << "\n"
+                      << "  charge = " << charge
+                      << "  beta   = " << beta;
+
+  // Now get number of photons generated in this step
+  double meanNumberOfPhotons = getAverageNumberOfPhotons_( charge, beta, material, Rindex );
+  if ( meanNumberOfPhotons <= 0.0 ) { // Don't do anything
+    LogDebug("EcalSim") << "Mean number of photons is zero: " << meanNumberOfPhotons
+                        << ", stopping here";
+    return cherenkovEnergy;
+  }
+
+  // number of photons is in unit of Geant4...
+  meanNumberOfPhotons *= aStep->GetStepLength();
+
+  // Now get a poisson distribution
+  int numPhotons = static_cast<int>( G4Poisson(meanNumberOfPhotons) );
+  edm::LogVerbatim("EcalSim") << "Number of photons = " << numPhotons;
+  if ( numPhotons <= 0 ) {
+    LogDebug("EcalSim") << "Poission number of photons is zero: " << numPhotons
+                      << ", stopping here";
+    return cherenkovEnergy;
+  }
+
+//   // Finally: get contribution of each photon
+//   for ( int iPhoton = 0; iPhoton<numPhotons; ++iPhoton ) {
+//     // Sample momentum
+//     double momentum = 0., 
+//     cherenkovEnergy += getPhotonEnergyDeposit_( momentum );
+//   }
+  
+
+  return cherenkovEnergy;
+
+}
+
+
+//________________________________________________________________________________________
+// Returns number of photons produced per GEANT-unit (millimeter) in the current medium. 
+// From G4Cerenkov.cc
+double DreamSD::getAverageNumberOfPhotons_( const double charge,
+                                            const double beta,
+                                            const G4Material* aMaterial,
+                                            const G4MaterialPropertyVector* Rindex ) const
+{
+  const G4double rFact = 369.81/(eV * cm);
+
+  if( beta <= 0.0 ) return 0.0;
+
+  double BetaInverse = 1./beta;
+
+  // Vectors used in computation of Cerenkov Angle Integral:
+  // 	- Refraction Indices for the current material
+  //	- new G4PhysicsOrderedFreeVector allocated to hold CAI's
+ 
+  // Min and Max photon momenta  
+  double Pmin = Rindex->GetMinPhotonMomentum();
+  double Pmax = Rindex->GetMaxPhotonMomentum();
+
+  // Min and Max Refraction Indices 
+  double nMin = Rindex->GetMinProperty();	
+  double nMax = Rindex->GetMaxProperty();
+
+  // Max Cerenkov Angle Integral 
+  double CAImax = chAngleIntegrals_->GetMaxValue();
+
+  double dp = 0., ge = 0., CAImin = 0.;
+
+  // If n(Pmax) < 1/Beta -- no photons generated 
+  if ( nMax < BetaInverse) { } 
+
+  // otherwise if n(Pmin) >= 1/Beta -- photons generated  
+  else if (nMin > BetaInverse) {
+    dp = Pmax - Pmin;	
+    ge = CAImax; 
+  } 
+  // If n(Pmin) < 1/Beta, and n(Pmax) >= 1/Beta, then
+  // we need to find a P such that the value of n(P) == 1/Beta.
+  // Interpolation is performed by the GetPhotonMomentum() and
+  // GetProperty() methods of the G4MaterialPropertiesTable and
+  // the GetValue() method of G4PhysicsVector.  
+  else {
+    Pmin = Rindex->GetPhotonMomentum(BetaInverse);
+    dp = Pmax - Pmin;
+    // need boolean for current implementation of G4PhysicsVector
+    // ==> being phased out
+    bool isOutRange;
+    double CAImin = chAngleIntegrals_->GetValue(Pmin, isOutRange);
+    ge = CAImax - CAImin;
+    
+  }
+
+  // Calculate number of photons 
+  double numPhotons = rFact * charge/eplus * charge/eplus *
+    (dp - ge * BetaInverse*BetaInverse);
+
+  LogDebug("EcalSim") << "@SUB=getAverageNumberOfPhotons" 
+                      << "CAImin = " << CAImin << "\n"
+                      << "CAImax = " << CAImax << "\n"
+                      << "dp = " << dp << ", ge = " << ge << "\n"
+                      << "numPhotons = " << numPhotons;
+
+
+  
+  return numPhotons;
+
+}
+
+
+//________________________________________________________________________________________
+// Set lead tungstate material properties on the fly.
+// Values from Ts42 detector construction
+bool DreamSD::setPbWO2MaterialProperties_( G4Material* aMaterial ) {
+
+  std::string pbWO2Name("E_PbWO4");
+  if ( pbWO2Name != aMaterial->GetName() ) { // Wrong material!
+    edm::LogWarning("EcalSim") << "This is not the right material: "
+                               << "expecting " << pbWO2Name 
+                               << ", got " << aMaterial->GetName();
+    return false;
+  }
+
+  G4MaterialPropertiesTable* table = new G4MaterialPropertiesTable();
+
+  // Refractive index as a function of photon momentum
+  // FIXME: Should somehow put that in the configuration
+  const int nEntries = 14;
+  double PhotonEnergy[nEntries] = { 1.7712*eV,  1.8368*eV,  1.90745*eV, 1.98375*eV, 2.0664*eV, 
+                                    2.15625*eV, 2.25426*eV, 2.3616*eV,  2.47968*eV, 2.61019*eV, 
+                                    2.75521*eV, 2.91728*eV, 3.09961*eV, 3.30625*eV };
+  double RefractiveIndex[nEntries] = { 2.17728, 2.18025, 2.18357, 2.18753, 2.19285, 
+                                       2.19813, 2.20441, 2.21337, 2.22328, 2.23619, 
+                                       2.25203, 2.27381, 2.30282, 2.34666 };
+  
+  table->AddProperty( "RINDEX", PhotonEnergy, RefractiveIndex, nEntries );
+  aMaterial->SetMaterialPropertiesTable(table); // FIXME: could this leak? What does G4 do?
+
+  // Calculate Cherenkov angle integrals: 
+  // This is an ad-hoc solution (we hold it in the class, not in the material)
+  chAngleIntegrals_ = 
+    std::auto_ptr<G4PhysicsOrderedFreeVector>( new G4PhysicsOrderedFreeVector() );
+
+  int index = 0;
+  double currentRI = RefractiveIndex[index];
+  double currentPM = PhotonEnergy[index];
+  double currentCAI = 0.0;
+  chAngleIntegrals_->InsertValues(currentPM, currentCAI);
+  double prevPM  = currentPM;
+  double prevCAI = currentCAI;
+  double prevRI  = currentRI;
+  while ( ++index < nEntries ) {
+    currentRI = RefractiveIndex[index];
+    currentPM = PhotonEnergy[index];
+    currentCAI = 0.5*(1.0/(prevRI*prevRI) + 1.0/(currentRI*currentRI));
+    currentCAI = prevCAI + (currentPM - prevPM) * currentCAI;
+
+    chAngleIntegrals_->InsertValues(currentPM, currentCAI);
+
+    prevPM  = currentPM;
+    prevCAI = currentCAI;
+    prevRI  = currentRI;
+  }
+
+  LogDebug("EcalSim") << "Material properties set for " << aMaterial->GetName();
+
+  return true;
+
 }
