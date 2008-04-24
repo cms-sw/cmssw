fbd54a7bb4dee942a4e55b550145df7c32574e8f
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-04-24 09:03:14 +0000

    migration to the new naming of digi collections

--- Validation/GlobalHits/python/Random_cfi.py
+++ Validation/GlobalHits/python/Random_cfi.py
@@ -3,15 +3,15 @@ import FWCore.ParameterSet.Config as cms
 RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
     moduleSeeds = cms.PSet(
         g4SimHits = cms.untracked.uint32(11),
-        ecalUnsuppressedDigis = cms.untracked.uint32(1234567),
-        muonCSCDigis = cms.untracked.uint32(11223344),
+        simEcalUnsuppressedDigis = cms.untracked.uint32(1234567),
+        simMuonCSCDigis = cms.untracked.uint32(11223344),
         mix = cms.untracked.uint32(12345),
-        muonDTDigis = cms.untracked.uint32(1234567),
+        simMuonDTDigis = cms.untracked.uint32(1234567),
         VtxSmeared = cms.untracked.uint32(98765432),
-        siPixelDigis = cms.untracked.uint32(1234567),
-        siStripDigis = cms.untracked.uint32(1234567),
-        hcalDigis = cms.untracked.uint32(11223344),
-        muonRPCDigis = cms.untracked.uint32(1234567)
+        simSiPixelDigis = cms.untracked.uint32(1234567),
+        simSiStripDigis = cms.untracked.uint32(1234567),
+        simHcalDigis = cms.untracked.uint32(11223344),
+        simMuonRPCDigis = cms.untracked.uint32(1234567)
     ),
     sourceSeed = cms.untracked.uint32(123456789)
 )
