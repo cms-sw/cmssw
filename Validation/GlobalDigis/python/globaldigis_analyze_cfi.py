7c20f9bffa57fd29968219364d5091cc9ad45b2f
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-04-24 09:12:00 +0000

    migration to the new naming of digi collections

--- Validation/GlobalDigis/python/globaldigis_analyze_cfi.py
+++ Validation/GlobalDigis/python/globaldigis_analyze_cfi.py
@@ -1,32 +1,32 @@
 import FWCore.ParameterSet.Config as cms
 
 globaldigisanalyze = cms.EDAnalyzer("GlobalDigisAnalyzer",
-    MuCSCStripSrc = cms.InputTag("muonCSCDigis","MuonCSCStripDigi"),
-    MuDTSrc = cms.InputTag("muonDTDigis"),
+    MuCSCStripSrc = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
+    MuDTSrc = cms.InputTag("simMuonDTDigis"),
     Name = cms.untracked.string('GlobalDigisAnalyzer'),
-    MuCSCWireSrc = cms.InputTag("muonCSCDigis","MuonCSCWireDigi"),
+    MuCSCWireSrc = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     Verbosity = cms.untracked.int32(0), ## 0 provides no output
 
-    ECalEESrc = cms.InputTag("ecalDigis","eeDigis"),
-    SiStripSrc = cms.InputTag("siStripDigis","ZeroSuppressed"),
+    ECalEESrc = cms.InputTag("simEcalDigis","eeDigis"),
+    SiStripSrc = cms.InputTag("simSiStripDigis","ZeroSuppressed"),
     # 1 assumes cm in SimVertex
     ProvenanceLookup = cms.PSet(
         PrintProvenanceInfo = cms.untracked.bool(False),
         GetAllProvenances = cms.untracked.bool(False)
     ),
     HCalSrc = cms.InputTag("g4SimHits","HcalHits"),
-    SiPxlSrc = cms.InputTag("siPixelDigis"),
+    SiPxlSrc = cms.InputTag("simSiPixelDigis"),
     # 1 provides basic output
     # 2 provides output of the fill step + 1
     # 3 provides output of the store step + 2
     Frequency = cms.untracked.int32(50),
-    MuRPCSrc = cms.InputTag("muonRPCDigis"),
-    ECalEBSrc = cms.InputTag("ecalDigis","ebDigis"),
-    ECalESSrc = cms.InputTag("ecalPreshowerDigis"),
+    MuRPCSrc = cms.InputTag("simMuonRPCDigis"),
+    ECalEBSrc = cms.InputTag("simEcalDigis","ebDigis"),
+    ECalESSrc = cms.InputTag("simEcalPreshowerDigis"),
     # as of 110p2, needs to be 1. Anything ealier should be 0.
     VtxUnit = cms.untracked.int32(1),
     #InputTag HCalDigi  = hcalUnsuppressedDigis
-    HCalDigi = cms.InputTag("hcalDigis")
+    HCalDigi = cms.InputTag("simHcalDigis")
 )
 
 
