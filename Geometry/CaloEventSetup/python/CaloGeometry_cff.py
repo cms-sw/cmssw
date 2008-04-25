254ef3e6a331049acb11d515761ed19299de7169
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-25 21:35:09 +0000

    From Sunanda's cfi update

--- Geometry/CaloEventSetup/python/CaloGeometry_cff.py
+++ Geometry/CaloEventSetup/python/CaloGeometry_cff.py
@@ -1,32 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# This cfi should be included to build the ECAL + HCAL geometry model
-#
-EcalBarrelGeometryEP = cms.ESProducer("EcalBarrelGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False) )
-
-EcalEndcapGeometryEP = cms.ESProducer("EcalEndcapGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
-
-EcalPreshowerGeometryEP = cms.ESProducer("EcalPreshowerGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
-
-HcalHardcodeGeometryEP = cms.ESProducer("HcalHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
-
-ZdcHardcodeGeometryEP = cms.ESProducer("ZdcHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
-
-CaloTowerHardcodeGeometryEP = cms.ESProducer("CaloTowerHardcodeGeometryEP")
-
-CaloGeometryBuilder = cms.ESProducer("CaloGeometryBuilder",
-    SelectedCalos = cms.vstring('HCAL',
-                                'ZDC',
-                                'EcalBarrel',
-                                'EcalEndcap',
-                                'EcalPreshower',
-                                'TOWER')
-)
-
+from Geometry.CaloEventSetup.CaloGeometryBuilder_cfi import *
+from Geometry.EcalAlgo.EcalGeometry_cff import *
+from Geometry.HcalEventSetup.HcalGeometry_cff import *
+from Geometry.ForwardGeometry.ZdcHardcodeGeometry_cfi import *
 
