97ac1317bab56761dc23f28769d4740e3e8996a6
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2008-04-25 13:48:18 +0000

    Support for overrideCatalog added. * It is now possible to specify an `overrideCatalog` untracked parameter in   the source where to specify the contact string of an additional catalog,   which will have precedence over the one specified in the SiteLocalConfig.   If the overrideCatalog is not able to perform the requested lfn-to-pfn or   pfn-to-lfn lookup(i.e. if any of the rules match, in the case of the TFC),   it will fallback to use the catalog specified in the SiteLocalConfig.

--- FWCore/Catalog/src/InputFileCatalog.cc
+++ FWCore/Catalog/src/InputFileCatalog.cc
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 //
-// $Id: InputFileCatalog.cc,v 1.4 2008/03/14 03:46:02 wmtan Exp $
+// $Id: InputFileCatalog.cc,v 1.5 2008/04/16 16:20:33 wmtan Exp $
 //
 // Original Author: Luca Lista
 // Current Author: Bill Tanenbaum
@@ -56,13 +56,21 @@ namespace edm {
 	  // There is only one catalog, so there is no need
 	  // to support multiple different catalogs.
           if (catalog().nReadCatalogs() == 0) {
+	    // Add the override catalog, if specified in the pset.
+	    std::string overriderUrl = pset.getUntrackedParameter<std::string>("overrideCatalog", std::string());
+           if (!overriderUrl.empty ())
+	    {  
+              pool::URIParser overriderParser(overriderUrl); 
+              overriderParser.parse();
+              catalog().addReadCatalog(overriderParser.contactstring());
+	    }
 	    // For reading use the catalog specified in the site-local config file
 	    url() = Service<edm::SiteLocalConfig>()->dataCatalog();
 	    pool::URIParser parser(url());
 	    parser.parse();
 
-	    catalog().addReadCatalog(parser.contactstring());
-	    catalog().connect();
+           catalog().addReadCatalog(parser.contactstring());
+           catalog().connect();
 
             catalog().start();
 	    setActive();
