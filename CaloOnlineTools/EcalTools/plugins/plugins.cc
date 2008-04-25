68a7c737186b948e529ddbce3776898c67b05e1d
Seth Cooper <sha1-32dd4c14096c6277ce1889fd6c006e25f1f45009@cern.ch>
2008-04-25 17:13:21 +0000

    Importing hexadecimal display from EcalTBRawToDigi package.

--- CaloOnlineTools/EcalTools/plugins/plugins.cc
+++ CaloOnlineTools/EcalTools/plugins/plugins.cc
@@ -5,6 +5,7 @@
 #include "CaloOnlineTools/EcalTools/plugins/EcalMipGraphs.h"
 #include "CaloOnlineTools/EcalTools/plugins/EcalPedHists.h"
 #include "CaloOnlineTools/EcalTools/plugins/EcalURecHitHists.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalHexDisplay.h"
 
 
 DEFINE_FWK_MODULE(EcalDigiDisplay);
@@ -13,4 +14,5 @@ DEFINE_FWK_MODULE(EcalDCCHeaderDisplay);
 DEFINE_FWK_MODULE(EcalMipGraphs);
 DEFINE_FWK_MODULE(EcalPedHists);
 DEFINE_FWK_MODULE(EcalURecHitHists);
+DEFINE_FWK_MODULE(EcalHexDisplay);
 
