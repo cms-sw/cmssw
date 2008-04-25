21c55a51f0812035360d5cf1bee59ed322b4573f
jvaughan <sha1-99aa072b68cff2824e2d8e96eccd1b97baf23061@cern.ch>
2008-04-25 17:56:09 +0000

    New calib object to accomodate changes in delay25 calibration routine

--- CalibFormats/SiPixelObjects/interface/PixelDelay25Calib.h
+++ CalibFormats/SiPixelObjects/interface/PixelDelay25Calib.h
@@ -33,38 +33,26 @@ namespace pos{
 
     virtual std::string mode() {return mode_;}
     std::set<std::string>& portcardList() {return portcardNames_;}
+    bool allPortcards() {return allPortcards_;}
     bool allModules() {return allModules_;}
     int getGridSize() {return gridSize_;}
     int getGridSteps() {return gridSteps_;}
     int getNumberTests() {return numTests_;}
-    int getNextOrigSDa(int n);
-    int getNextOrigRDa(int n);
+    int getRange() {return range_;}
+    int getOrigSDa() {return origSDa_;}
+    int getOrigRDa() {return origRDa_;}
     void openFiles(std::string portcardName, std::string moduleName, 
 		   std::string path="");
     void writeSettings(std::string portcardName, std::string moduleName);
     void writeFiles(std::string tmp);
     void writeFiles(int currentSDa, int currentRDa, int number);
     void closeFiles();
-    void getCandidatePoints();
-    int getNumCandidatePoints() {return numCandidatePoints_;}
-    int getNextCandidateSDa(int n);
-    int getNextCandidateRDa(int n);
-    int getStableRange() {return stableRange_;}
-    void makeNeighbors(int SDa, int RDa);
-    int getNumNeighbors() {return numNeighbors_;}
-    int getNextNeighborSDa(int n);
-    int getNextNeighborRDa(int n);
 
   private:
     std::string mode_;
     std::set<std::string> portcardNames_;
-    bool allModules_;
-    int origSDa_, origRDa_, range_, gridSize_;
-    int numTests_, stableRange_, stableShape_;
-    int gridSteps_, numShifts_, numCandidatePoints_, numNeighbors_;
-    std::vector<int> vecOrigSDa_, vecOrigRDa_, vecShifts_;
-    std::vector<int> vecCandidateSDa_, vecCandidateRDa_;
-    std::vector<int> vecNeighborSDa_, vecNeighborRDa_;
+    bool allPortcards_, allModules_;
+    int origSDa_, origRDa_, range_, gridSize_, gridSteps_, numTests_;
     std::ofstream graphout_;
     std::ofstream goodout_;
     std::string graph_, good_;
--- CalibFormats/SiPixelObjects/src/PixelDelay25Calib.cc
+++ CalibFormats/SiPixelObjects/src/PixelDelay25Calib.cc
@@ -44,6 +44,12 @@ PixelDelay25Calib::PixelDelay25Calib(std::string filename) :
 
   assert(tmp=="Portcards:");
   in >> tmp;
+  if(tmp=="All")
+    {
+      allPortcards_ = true;
+    } else {
+      allPortcards_ = false;
+    }
   while (tmp!="AllModules:")
     {
       portcardNames_.insert(tmp);
@@ -77,65 +83,14 @@ PixelDelay25Calib::PixelDelay25Calib(std::string filename) :
   assert(tmp=="Tests:");
   in >> numTests_;
 
-  in >> tmp;
-
-  assert(tmp=="StableRange:");
-  in >> stableRange_;
-
-  in >> tmp;
-
-  assert(tmp=="StableShape:");
-  in >> stableShape_;
-
   in.close();
 
   //Number of steps in the grid
   gridSteps_ = range_/gridSize_;
 
-  //Prepare to shift the origin of the grid
-  int testBinary = gridSize_-1;
-  int testShift = gridSize_ >> 1;
-  numShifts_ = 0;
-  //Gather a vector of the shift sizes
-  while( testShift & testBinary )
-    {
-      vecShifts_.push_back(testShift);
-      numShifts_++;
-      testShift >>= 1;
-    }
-
-  int nextSDa = origSDa_;
-  int nextRDa = origRDa_;
-  //Gather vectors of SDa and RDa origin values
-  for(int overi=0; overi<gridSize_*gridSize_; overi++)
-    {
-      int parseoveri = overi;
-      for(int n=0; n<numShifts_; n++)
-	{
-	  nextSDa = nextSDa + (parseoveri & 1)*(vecShifts_.at(n));
-	  parseoveri >>= 1;
-	  nextRDa = nextRDa + (parseoveri & 1)*(vecShifts_.at(n));
-	  parseoveri >>= 1;
-	}
-      vecOrigSDa_.push_back(nextSDa);
-      vecOrigRDa_.push_back(nextRDa);
-      nextSDa = origSDa_;
-      nextRDa = origRDa_;
-    }
-  //Preparation for stability test of candidate points
-  numCandidatePoints_=0;
-  numNeighbors_=0;
 }
 
 PixelDelay25Calib::~PixelDelay25Calib() {
-}
-
-int PixelDelay25Calib::getNextOrigSDa(int n) {
-    return vecOrigSDa_.at(n);
-}
-
-int PixelDelay25Calib::getNextOrigRDa(int n) {
-  return vecOrigRDa_.at(n);
 }
 
 void PixelDelay25Calib::openFiles(std::string portcardName, std::string moduleName, std::string path) {
@@ -175,182 +130,6 @@ void PixelDelay25Calib::closeFiles() {
   graphout_.close();
   goodout_.close();
   return;
-}
-
-void PixelDelay25Calib::getCandidatePoints() {
-
-  std::ifstream goodin(good_.c_str());
-  
-  if(!goodin.good()){
-    std::cout<<"Could not open: "<<good_<<std::endl;
-    assert(0);
-  }
-  else {
-    //std::cout<<"Opened: "<<good_<<std::endl;
-  }
-  int goodSDa=0;
-  int goodRDa=0;
-  int nearSDa=0;
-  int nearRDa=0;
-  int distance=gridSize_/2;
-  goodin >> goodSDa >> goodRDa;
-  while(!goodin.eof()){
-    //store the good point as a candidate
-    vecCandidateSDa_.push_back(goodSDa);
-    vecCandidateRDa_.push_back(goodRDa);
-    //also store its three nearest neighbors in the positive
-    //SDa and RDa directions as candidates
-    //loop around if any of these points is off the scale
-    nearSDa=goodSDa+distance;
-    nearRDa=goodRDa+distance;
-    if(nearSDa>127)
-      nearSDa = nearSDa-64;
-    if(nearRDa>127)
-      nearRDa=nearRDa-64;
-    //neighbor #1
-    vecCandidateSDa_.push_back(nearSDa);
-    vecCandidateRDa_.push_back(goodRDa);
-    //neighbor #2
-    vecCandidateSDa_.push_back(goodSDa);
-    vecCandidateRDa_.push_back(nearRDa);
-    //neighbor #3
-    vecCandidateSDa_.push_back(nearSDa);
-    vecCandidateRDa_.push_back(nearRDa);
-    goodin >> goodSDa >> goodRDa;
-  }
-  goodin.close();
-  numCandidatePoints_=vecCandidateSDa_.size();
-
-  return;
-}
-
-int PixelDelay25Calib::getNextCandidateSDa(int n) {
-  return vecCandidateSDa_.at(n);
-}
-
-int PixelDelay25Calib::getNextCandidateRDa(int n) {
-  return vecCandidateRDa_.at(n);
-}
-
-void PixelDelay25Calib::makeNeighbors(int SDa, int RDa) {
-  vecNeighborSDa_.clear();
-  vecNeighborRDa_.clear();
-  int leftSDa = SDa-stableRange_;
-  if (leftSDa < 64)
-    leftSDa = leftSDa + 64;
-  int rightSDa = SDa+stableRange_;
-  if (rightSDa > 127)
-    rightSDa = rightSDa - 64;
-  int downRDa = RDa-stableRange_;
-  if(downRDa < 64)
-    downRDa = downRDa + 64;
-  int upRDa = RDa+stableRange_;
-  if(upRDa > 127)
-    upRDa = upRDa - 64;
-
-  //The definition of a neighbor depends on stableShape_
-  if(stableShape_==1)
-    {
-      //This time the point has four neighbors
-      //Neighbor #1
-      vecNeighborSDa_.push_back(leftSDa);
-      vecNeighborRDa_.push_back(RDa);
-      //Neighbor #2
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(downRDa);
-      //Neighbor #3
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(upRDa);
-      //Neighbor #4
-      vecNeighborSDa_.push_back(rightSDa);
-      vecNeighborRDa_.push_back(RDa);
-    }
-  else if(stableShape_==2)
-    {
-      //This time the point has eight neighbors
-      int smallRange = stableRange_/2;
-      int smallLeftSDa = SDa-smallRange;
-      if (smallLeftSDa < 64)
-	smallLeftSDa = smallLeftSDa + 64;
-      int smallRightSDa = SDa+smallRange;
-      if (smallRightSDa > 127)
-	smallRightSDa = smallRightSDa - 64;
-      int smallDownRDa = RDa-smallRange;
-      if(smallDownRDa < 64)
-	smallDownRDa = smallDownRDa + 64;
-      int smallUpRDa = RDa+smallRange;
-      if(smallUpRDa > 127)
-	smallUpRDa = smallUpRDa - 64;
-      //Neighbor #1
-      vecNeighborSDa_.push_back(leftSDa);
-      vecNeighborRDa_.push_back(RDa);
-      //Neighbor #2
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(upRDa);
-      //Neighbor #3
-      vecNeighborSDa_.push_back(rightSDa);
-      vecNeighborRDa_.push_back(RDa);
-      //Neighbor #4
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(downRDa);
-      //Neighbor #5
-      vecNeighborSDa_.push_back(smallLeftSDa);
-      vecNeighborRDa_.push_back(smallDownRDa);
-      //Neighbor #6
-      vecNeighborSDa_.push_back(smallLeftSDa);
-      vecNeighborRDa_.push_back(smallUpRDa);
-      //Neighbor #7
-      vecNeighborSDa_.push_back(smallRightSDa);
-      vecNeighborRDa_.push_back(smallUpRDa);
-      //Neighbor #8
-      vecNeighborSDa_.push_back(smallRightSDa);
-      vecNeighborRDa_.push_back(smallDownRDa);
-    }
-  else if(stableShape_==3)
-    {
-      //This time the point has eight neighbors,
-      //spaced more widely than in shape 2
-      //Neighbor #1
-      vecNeighborSDa_.push_back(leftSDa);
-      vecNeighborRDa_.push_back(RDa);
-      //Neighbor #2
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(upRDa);
-      //Neighbor #3
-      vecNeighborSDa_.push_back(rightSDa);
-      vecNeighborRDa_.push_back(RDa);
-      //Neighbor #4
-      vecNeighborSDa_.push_back(SDa);
-      vecNeighborRDa_.push_back(downRDa);
-      //Neighbor #5
-      vecNeighborSDa_.push_back(leftSDa);
-      vecNeighborRDa_.push_back(downRDa);
-      //Neighbor #6
-      vecNeighborSDa_.push_back(leftSDa);
-      vecNeighborRDa_.push_back(upRDa);
-      //Neighbor #7
-      vecNeighborSDa_.push_back(rightSDa);
-      vecNeighborRDa_.push_back(upRDa);
-      //Neighbor #8
-      vecNeighborSDa_.push_back(rightSDa);
-      vecNeighborRDa_.push_back(downRDa);
-    }
-  else
-    {
-      cout << "I don't recognize the value of StableShape." << endl;
-      cout << "Choices are 1, 2, or 3." << endl;
-      assert(0);
-    }
-  numNeighbors_ = vecNeighborSDa_.size();
-  return;
-}
-
-int PixelDelay25Calib::getNextNeighborSDa(int n) {
-  return vecNeighborSDa_.at(n);
-}
-
-int PixelDelay25Calib::getNextNeighborRDa(int n) {
-  return vecNeighborRDa_.at(n);
 }
 
 void PixelDelay25Calib::writeASCII(std::string dir) const {
@@ -393,12 +172,6 @@ void PixelDelay25Calib::writeASCII(std::string dir) const {
   
   out << "Tests:"<<endl;
   out << numTests_<<endl;
-  
-  out << "StableRange:"<<endl;
-  out << stableRange_<<endl;
-
-  out << "StableShape:"<<endl;
-  out << stableShape_<<endl;
   
   out.close();
 }
