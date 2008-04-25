abc8ddb4b0d29b93314d7f24a1adfd9924a2b431
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-25 21:34:28 +0000

    *** empty log message ***

---
+++ Geometry/CaloEventSetup/python/CaloGeometryBuilder_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+CaloGeometryBuilder = cms.ESProducer("CaloGeometryBuilder",
+    SelectedCalos = cms.vstring('HCAL', 
+        'ZDC', 
+        'EcalBarrel', 
+        'EcalEndcap', 
+        'EcalPreshower', 
+        'TOWER')
+)
+
+
