61d6473dae241020bb1ad663e73ba07e1fdce385
ballin <sha1-4e4bbef69ed66b5ba60dd13d54b406ba31eb806d@cern.ch>
2008-04-25 16:06:32 +0000

    Updated with Minimiser package classes. Subject to major changes soon.

---
+++ RecoParticleFlow/PFClusterTools/src/classes.h
@@ -0,0 +1,20 @@
+#include "RecoParticleFlow/PFClusterTools/interface/TreeUtility.hh"
+#include "RecoParticleFlow/PFClusterTools/interface/Calibrator.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/DetectorElement.hh"      
+#include "RecoParticleFlow/PFClusterTools/interface/LinearCalibrator.hh"    
+#include "RecoParticleFlow/PFClusterTools/interface/Operators.h"         
+#include "RecoParticleFlow/PFClusterTools/interface/SpaceManager.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/ToString.h"
+#include "RecoParticleFlow/PFClusterTools/interface/Deposition.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/DetectorElementType.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/MinimiserException.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/ParticleDeposit.hh"  
+#include "RecoParticleFlow/PFClusterTools/interface/SpaceVoxel.hh"    
+#include "RecoParticleFlow/PFClusterTools/interface/TreeUtility.hh"
+#include "RecoParticleFlow/PFClusterTools/interface/SingleParticleWrapper.hh"
+namespace { 
+  namespace {
+
+
+  }
+}
