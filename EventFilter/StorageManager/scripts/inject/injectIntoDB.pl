812b577065a4627f1aad63ba65e3bdb9f4c64546
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-25 15:40:05 +0000

    Cosmetics

--- EventFilter/StorageManager/scripts/inject/injectIntoDB.pl
+++ EventFilter/StorageManager/scripts/inject/injectIntoDB.pl
@@ -1,35 +1,35 @@
 #!/usr/bin/perl -w
-# $Id: injectIntoDB.pl,v 1.3 2008/04/25 13:56:44 loizides Exp $
+# $Id: injectIntoDB.pl,v 1.4 2008/04/25 14:49:05 loizides Exp $
 
 use strict;
 use DBI;
 use Getopt::Long;
 
 # injection subroutine
-sub inject()
+sub inject($)
 {
-    use vars qw($dbh);
+    my $dbh = shift;
 
-    my $filename=$ENV{'SM_FILENAME'};
-    my $count=$ENV{'SM_FILECOUNTER'};
-    my $nevents=$ENV{'SM_NEVENTS'};;
-    my $filesize=$ENV{'SM_FILESIZE'};
-    my $starttime=$ENV{'SM_STARTTIME'};
-    my $stoptime=$ENV{'SM_STOPTIME'};
-    my $status=$ENV{'SM_STATUS'};
-    my $runnumber=$ENV{'SM_RUNNUMBER'};
-    my $lumisection=$ENV{'SM_LUMISECTION'};
-    my $pathname=$ENV{'SM_PATHNAME'};
-    my $hostname=$ENV{'SM_HOSTNAME'};
-    my $dataset=$ENV{'SM_DATASET'};
-    my $stream=$ENV{'SM_STREAM'};
-    my $instance=$ENV{'SM_INSTANCE'};
-    my $safety=$ENV{'SM_SAFETY'};
-    my $appversion=$ENV{'SM_APPVERSION'};
-    my $appname=$ENV{'SM_APPNAME'};
-    my $type=$ENV{'SM_TYPE'};
-    my $checksum=$ENV{'SM_CHECKSUM'};
-    my $producer='StorageManager';
+    my $filename   =  $ENV{'SM_FILENAME'};
+    my $count      =  $ENV{'SM_FILECOUNTER'};
+    my $nevents    =  $ENV{'SM_NEVENTS'};;
+    my $filesize   =  $ENV{'SM_FILESIZE'};
+    my $starttime  =  $ENV{'SM_STARTTIME'};
+    my $stoptime   =  $ENV{'SM_STOPTIME'};
+    my $status      = $ENV{'SM_STATUS'};
+    my $runnumber   = $ENV{'SM_RUNNUMBER'};
+    my $lumisection = $ENV{'SM_LUMISECTION'};
+    my $pathname    = $ENV{'SM_PATHNAME'};
+    my $hostname    = $ENV{'SM_HOSTNAME'};
+    my $dataset     = $ENV{'SM_DATASET'};
+    my $stream      = $ENV{'SM_STREAM'};
+    my $instance    = $ENV{'SM_INSTANCE'};
+    my $safety      = $ENV{'SM_SAFETY'};
+    my $appversion  = $ENV{'SM_APPVERSION'};
+    my $appname     = $ENV{'SM_APPNAME'};
+    my $type        = $ENV{'SM_TYPE'};
+    my $checksum    = $ENV{'SM_CHECKSUM'};
+    my $producer    = 'StorageManager';
 
     my $SQL = "INSERT INTO CMS_STOMGR.TIER0_INJECTION (" .
         "RUNNUMBER,LUMISECTION,INSTANCE,COUNT,START_TIME,STOP_TIME,FILENAME,PATHNAME," .
@@ -38,7 +38,8 @@ sub inject()
         "'$filename','$pathname','$hostname','$dataset','$producer','$stream','$status'," .
         "$type,$safety,$nevents,$filesize,$checksum)";
 
-    if (defined $ddh) { 
+    if (defined $dbh) { 
+        my $rows = 1;
         my $rows = $dbh->do($SQL) or 
             die $dbh->errstr;
         return $rows-1;
@@ -48,7 +49,13 @@ sub inject()
     return 0;
 }
 
+if (!defined $ARGV[0]) {
+    die "Syntax: ./injectIntoDB.pl infile outfile";
+}
 my $infile="$ARGV[0]";
+if (!defined $ARGV[1]) {
+    die "Syntax: ./injectIntoDB.pl infile outfile";
+}
 my $outfile=">$ARGV[1]";
 
 #overwrite TNS to be sure it points to new DB
@@ -59,7 +66,7 @@ my $dbh; #my DB handle
 if (!defined $ENV{'SM_DONTACCESSDB'}) { 
     my $dbi    = "DBI:Oracle:cms_rcms";
     my $reader = "CMS_STOMGR_W";
-    $dbh    = DBI->connect($dbi,$reader,"qwerty") or 
+    $dbh = DBI->connect($dbi,$reader,"qwerty") or 
         die "Error: Connection to Oracle failed: $DBI::errstr\n";
     print "dfadfadfa\n";
 }
@@ -84,7 +91,7 @@ while( $line = <INDATA> ){
         }
     }
 
-    my $ret=inject();
+    my $ret=inject($dbh);
     if ($ret == 0) {
         print OUTDATA "$line\n";
         my $cmd=$ENV{'SM_HOOKSCRIPT'};
@@ -99,7 +106,7 @@ close INDATA;
 close OUTDATA;
 
 # Disconnect from DB
-if (defined $ddh) { 
+if (defined $dbh) { 
     $dbh->disconnect or 
         warn "Warning: Disconnection from Oracle failed: $DBI::errstr\n";
 }
