b716246ef24b80c785b74c691e0013dd9dad1e25
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-04-25 15:14:25 +0000

    Somthing might be redundant something might be missing. Work in progress

--- DataFormats/CaloRecHit/src/classes.h
+++ DataFormats/CaloRecHit/src/classes.h
@@ -1,3 +1,16 @@
+#include "DataFormats/Common/interface/RefProd.h" 
+#include "DataFormats/Common/interface/RefToBase.h"
 #include "DataFormats/CaloRecHit/interface/CaloRecHit.h"
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
+#include "DataFormats/CaloRecHit/interface/CaloClusterFwd.h"
+
 /* #include <boost/cstdint.hpp>  */
+
+    std::vector<reco::CaloCluster> v11;
+    reco::CaloClusterCollection v1;
+    edm::Wrapper<reco::CaloClusterCollection> w1;
+    edm::Ref<reco::CaloClusterCollection> r1;
+    edm::RefProd<reco::CaloClusterCollection> rp1;
+    edm::Wrapper<edm::RefVector<reco::CaloClusterCollection> > wrv1;
+    edm::Ptr<reco::CaloClusterCollection> p1;
+    edm::PtrVector<reco::CaloClusterCollection> pv1;
