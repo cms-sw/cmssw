2a04f80eed3c3a926cd9b2bed1dc58ac457742ff
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2008-04-26 01:53:56 +0000

    new interface to eliminate 1_8+ bugs and transition to DQM

--- Alignment/CommonAlignmentMonitor/python/AlignmentMonitorMuonPositionsDefaults_cff.py
+++ Alignment/CommonAlignmentMonitor/python/AlignmentMonitorMuonPositionsDefaults_cff.py
@@ -1,9 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-AlignmentMonitorMuonPositionsDefaults = cms.PSet(
-    collectorActive = cms.bool(False),
-    collectorPath = cms.string('./'),
-    outpath = cms.string('./'),
-    collectorNJobs = cms.int32(0)
-)
+AlignmentMonitorMuonPositionsDefaults = cms.PSet()
 
