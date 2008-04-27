ec20758f078a584097a56a166265bf88ae10a97c
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2008-04-27 00:31:27 +0000

    Hmmm, matrix flipped..

--- PhysicsTools/MVATrainer/src/ProcMatrix.cc
+++ PhysicsTools/MVATrainer/src/ProcMatrix.cc
@@ -121,7 +121,7 @@ Calibration::VarProcessor *ProcMatrix::getCalibration() const
 
 	for(unsigned int i = 0; i < n; i++)
 		for(unsigned int j = 0; j < n; j++)
-			calib->matrix.elements.push_back(rotation(i, j));
+			calib->matrix.elements.push_back(rotation(j, i));
 
 	return calib;
 }
