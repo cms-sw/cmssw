1c6638ed31ad8791f1bbfb6a0ac2e07a3249f782
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2008-04-28 19:28:24 +0000

    Using fixedMatrix

--- RecoEcal/EgammaCoreTools/test/testEcalClusterTools.py
+++ RecoEcal/EgammaCoreTools/test/testEcalClusterTools.py
@@ -8,7 +8,7 @@ process.extend(include("RecoEcal/EgammaClusterProducers/data/geometryForClusteri
 
 input_files = vstring()
 #input_files.append( "/store/relval/2008/4/17/RelVal-RelValTTbar-1208465820/0000/0ABDA540-EE0C-DD11-BA9F-000423D94990.root" )
-input_files.append( "file:/tmp/ferriff/0ABDA540-EE0C-DD11-BA9F-000423D94990.root" )
+input_files.append( "file:/tmp/meridian/egamma_clusters.root" )
 
 process.source = Source("PoolSource",
     fileNames = untracked( input_files )
@@ -18,10 +18,10 @@ process.maxEvents = untracked.PSet( input = untracked.int32( 10 ) )
 
 
 process.testEcalClusterTools = EDAnalyzer("testEcalClusterTools",
-    reducedBarrelRecHitCollection = InputTag("ecalRecHit:EcalRecHitsEB"),
-    reducedEndcapRecHitCollection = InputTag("ecalRecHit:EcalRecHitsEE"),
+    reducedBarrelRecHitCollection = InputTag("reducedEcalRecHitsEB"),
+    reducedEndcapRecHitCollection = InputTag("reducedEcalRecHitsEE"),
     barrelClusterCollection = InputTag("hybridSuperClusters"),
-    endcapClusterCollection = InputTag("islandBasicClusters:islandEndcapBasicClusters")
+    endcapClusterCollection = InputTag("fixedMatrixBasicClusters:fixedMatrixEndcapBasicClusters")
 )
 
 process.p1 = Path( process.testEcalClusterTools )
