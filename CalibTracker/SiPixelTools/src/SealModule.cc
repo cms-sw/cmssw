45223ec1ed1510d8b0cfcc4c36bbbec24b9125ca
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-04-28 21:57:16 +0000

    Fixing bug

--- CalibTracker/SiPixelTools/src/SealModule.cc
+++ CalibTracker/SiPixelTools/src/SealModule.cc
@@ -1,9 +1,6 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "CalibTracker/SiPixelTools/interface/SiPixelOfflineCalibAnalysisBase.h"
 
 DEFINE_SEAL_MODULE();
-
-#include "CalibTracker/SiPixelTools/interface/SiPixelOfflineCalibAnalysisBase.h"
-#include "CalibTracker/SiPixelTools/interface/SiPixelErrorsDigisToCalibDigis.h"
 DEFINE_ANOTHER_FWK_MODULE(SiPixelOfflineCalibAnalysisBase);
-DEFINE_ANOTHER_FWK_MODULE(SiPixelErrorsDigisToCalibDigis);
