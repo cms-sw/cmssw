173f124f946ffa27b5e40c68cb3981d0fa73a95a
Giovanni Abbiendi <sha1-03ca615cb6d62d8479399a2c21824cf89dd4fac5@cern.ch>
2008-04-28 18:50:55 +0000

    format change

--- SimDataFormats/DigiSimLinks/interface/DTDigiSimLink.h
+++ SimDataFormats/DigiSimLinks/interface/DTDigiSimLink.h
@@ -10,8 +10,13 @@ class DTDigiSimLink {
   typedef uint32_t ChannelType;
 
   // Construct from the wire number and the digi number (this identifies 
-  // uniquely multiple digis on the same wire), the SimTrack Id and the EncodedEvent Id.
-  explicit DTDigiSimLink(int wireNr, int digiNr, unsigned int trackId, EncodedEventId evId);
+  // uniquely multiple digis on the same wire), the TDC counts, the SimTrack Id and the EncodedEvent Id.
+  explicit DTDigiSimLink(int wireNr, int digiNr, int nTDC, unsigned int trackId, EncodedEventId evId);
+
+  // Construct from the wire number and the digi number (this identifies 
+  // uniquely multiple digis on the same wire), the time (ns), the SimTrack Id and the EncodedEvent Id.
+  // time is converted in TDC counts (1 TDC = 25./32. ns)
+  explicit DTDigiSimLink(int wireNr, int digiNr, double tdrift, unsigned int trackId, EncodedEventId evId);
 
   // Default constructor.
   DTDigiSimLink();
@@ -25,6 +30,12 @@ class DTDigiSimLink {
   // Identifies different digis within the same cell
   int number() const;
 
+  // Get raw TDC count
+  uint32_t countsTDC() const;
+
+  // Get time in ns
+  double time() const;
+
   // Return the SimTrack Id
   unsigned int SimTrackId()  const;
 
@@ -32,6 +43,9 @@ class DTDigiSimLink {
   EncodedEventId eventId()  const;
 
 private:
+  // The value of one TDC count in ns
+  static const double reso;
+
   // Used to repack the channel number to an int
   struct ChannelPacking {
     uint16_t wi;
@@ -39,8 +53,9 @@ private:
   };
   
  private:
-  uint16_t theWire;   // wire number
+  uint16_t theWire;       // wire number
   uint16_t theDigiNumber; // digi number on the wire
+  uint32_t theCounts;     // TDC count, up to 20 bits actually used
   uint32_t theSimTrackId; // identifier of the SimTrack that produced the digi
   EncodedEventId theEventId;
 };
@@ -49,6 +64,7 @@ private:
 inline std::ostream & operator<<(std::ostream & o, const DTDigiSimLink& digisimlink) {
   return o << "wire:"<<digisimlink.wire()
 	   << " digi:" << digisimlink.number()
+	   << " time:" << digisimlink.time()
 	   << " SimTrack:" << digisimlink.SimTrackId()
 	   << " eventId:" << digisimlink.eventId().rawId();
 }
