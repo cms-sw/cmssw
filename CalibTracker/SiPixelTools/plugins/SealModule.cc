46e31079e3fc29fedb4f300525d255ddcefc2e0d
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-04-28 21:55:19 +0000

    Adding SiPixelErrosDigisToCalibDigis

--- CalibTracker/SiPixelTools/plugins/SealModule.cc
+++ CalibTracker/SiPixelTools/plugins/SealModule.cc
@@ -1,7 +1,11 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+#include "CalibTracker/SiPixelTools/interface/SiPixelCalibDigiFilter.h"
+#include "CalibTracker/SiPixelTools/interface/SiPixelErrorsDigisToCalibDigis.h"
+
 DEFINE_SEAL_MODULE();
 
-#include "CalibTracker/SiPixelTools/interface/SiPixelCalibDigiFilter.h"
 DEFINE_ANOTHER_FWK_MODULE(SiPixelCalibDigiFilter);
+DEFINE_ANOTHER_FWK_MODULE(SiPixelErrorsDigisToCalibDigis);
+
