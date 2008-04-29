b71680f1ffd5ed3a0403e615c95d6d4984f93906
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-04-29 23:08:35 +0000

    explained clearly what is what in the energy block and added total tower energy as well (useful for MET corrections to avoid mixing rec hits and towers)

--- DataFormats/MuonReco/interface/MuonEnergy.h
+++ DataFormats/MuonReco/interface/MuonEnergy.h
@@ -4,14 +4,34 @@
 
 namespace reco {
     struct MuonEnergy {
-       float em;        // energy deposited in ECAL
-       float had;       // energy deposited in HCAL
-       float ho;        // energy deposited in HO
-       float emS9;     // energy deposited in ECAL in 3x3 towers
-       float hadS9;    // energy deposited in HCAL in 3x3 crystals
-       float hoS9;     // energy deposited in HO in 3x3 towers
+       /// CaloTower based energy
+       /// total energy
+       float tower; 
+       /// total energy in 3x3 tower shape
+       float towerS9; 
+       
+       /// RecHit based energy (eta-phi size don't match!)
+       /// energy deposited in crossed ECAL crystals
+       float em; 
+       /// energy deposited in 3x3 ECAL crystal shape around 
+       /// crossed crystal
+       float emS9;
+       /// energy deposited in crossed HCAL tower (RecHits)
+       float had;
+       /// energy deposited in 3x3 HCAL tower shape around 
+       /// crossed tower (RecHits)
+       float hadS9;
+       /// energy deposited in crossed HO tower (RecHits)
+       float ho;
+       /// energy deposited in 3x3 HO tower shape around 
+       /// crossed tower (RecHits)
+       float hoS9;
+       
        MuonEnergy():
-       em(0),had(0),ho(0),emS9(0),hadS9(0),hoS9(0){ }
+       tower(0), towerS9(0),
+       em(0), emS9(0),
+       had(0), hadS9(0),
+       ho(0), hoS9(0){ }
        
     };
 }
