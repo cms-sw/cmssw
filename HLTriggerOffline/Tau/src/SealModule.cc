dcdc0f5b7f42c01a4f9bf3790c9ea0c2594a001f
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-04-29 16:42:04 +0000

    Restored L25 analyzers

--- HLTriggerOffline/Tau/src/SealModule.cc
+++ HLTriggerOffline/Tau/src/SealModule.cc
@@ -4,6 +4,7 @@
 #include "HLTriggerOffline/Tau/interface/HLTTauMcInfo.h"
 
 #include "HLTriggerOffline/Tau/interface/HLTTauAnalyzer.h"
+#include "HLTriggerOffline/Tau/interface/MCTauCand.h"
 
 #include "HLTriggerOffline/Tau/interface/HLTTauL25Validation.h"
 #include "HLTriggerOffline/Tau/interface/L25TauAnalyzer.h"
@@ -16,9 +17,10 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauL25Validation);
-//DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L2TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
+
 
 
 
