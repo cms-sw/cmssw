ef2fcf25a11c90023d11cc8d04fad1f26c9b56fe
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-04-29 17:46:46 +0000

    fix.

--- EventFilter/StorageManager/scripts/inject/sm_hookscript.pl
+++ EventFilter/StorageManager/scripts/inject/sm_hookscript.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: closeFile.pl,v 1.7 2008/04/01 10:59:53 loizides Exp $
+# $Id: sm_hookscript.pl,v 1.1 2008/04/28 12:57:31 loizides Exp $
 ################################################################################
 
 use strict;
@@ -26,11 +26,11 @@ my $checksum    = $ENV{'SM_CHECKSUM'};
 my $producer    = 'StorageManager';
 
 # copy first file per lumi section to look area 
-my $dola = $ENV{'SM_LA_NFS'}
+my $dola = $ENV{'SM_LA_NFS'};
 if (defined $dola) {
     if ($lumisection == 1 && $count < 1)
     {
-        my $COPYCOMMAND = "if test -n \"`mount | grep $SM_LA_NFS`\"; then cp $pathname/$filename $SM_LOOKAREA && chmod a+r $SM_LOOKAREA$filename; fi &"; 
+        my $COPYCOMMAND = "if test -n \"`mount | grep $SM_LA_NFS`\"; then cp $pathname/$filename $SM_LOOKAREA && chmod a+r $SM_LOOKAREA/$filename; fi &"; 
         system($COPYCOMMAND);
     }
 }
