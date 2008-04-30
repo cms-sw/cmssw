563cdf4d44caec3e5887e0bb084959f26ddf9023
Giovanni Abbiendi <sha1-03ca615cb6d62d8479399a2c21824cf89dd4fac5@cern.ch>
2008-04-30 18:22:38 +0000

    add DT and RPC DigiSimLinks to FEVTSIM

--- SimMuon/Configuration/python/SimMuon_EventContent_cff.py
+++ SimMuon/Configuration/python/SimMuon_EventContent_cff.py
@@ -9,7 +9,9 @@ import FWCore.ParameterSet.Config as cms
 #Full Event content 
 SimMuonFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep StripDigiSimLinkedmDetSetVector_simMuonCSCDigis_*_*', 
-        'keep CSCDetIdCSCComparatorDigiMuonDigiCollection_simMuonCSCDigis_*_*')
+        'keep CSCDetIdCSCComparatorDigiMuonDigiCollection_simMuonCSCDigis_*_*', 
+        'keep DTLayerIdDTDigiSimLinkMuonDigiCollection_simMuonDTDigis_*_*', 
+        'keep RPCDigiSimLinkedmDetSetVector_simMuonRPCDigis_*_*')
 )
 #Full Event content with DIGI
 SimMuonFEVTDIGI = cms.PSet(
