34c159cdfab550528220e0c74a72487593cdd783
Tulika Bose <sha1-8b8c25cc70f9a5790e3c425b034aa99249523492@cern.ch>
2008-05-01 22:16:10 +0000

    Updating python config

--- CalibCalorimetry/CaloTPG/python/CaloTPGTranscoder_cfi.py
+++ CalibCalorimetry/CaloTPG/python/CaloTPGTranscoder_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 CaloTPGTranscoder = cms.ESSource("CaloTPGTranscoderULUTs",
-    hcalLUT1 = cms.FileInPath('CalibCalorimetry/CaloTPG/data/outputLUTtranscoder.dat'),
+    hcalLUT1 = cms.FileInPath('CalibCalorimetry/CaloTPG/data/outputLUTtranscoder_physics.dat'),
     hcalLUT2 = cms.FileInPath('CalibCalorimetry/CaloTPG/data/TPGcalcDecompress2.txt')
 )
 
