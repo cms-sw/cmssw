733a8d02a6b175e8b53d32defcaf8fcf88d72e75
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-01 21:36:55 +0000

    Add CSCDigiSuppressor

--- L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
+++ L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
@@ -1,5 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <L1Trigger/CSCTriggerPrimitives/plugins/CSCTriggerPrimitivesProducer.h>
+#include "L1Trigger/CSCTriggerPrimitives/src/CSCDigiSuppressor.h"
 
 DEFINE_FWK_MODULE(CSCTriggerPrimitivesProducer);
+DEFINE_ANOTHER_FWK_MODULE(CSCDigiSuppressor);
