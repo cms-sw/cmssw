1ff0c3f1c30c483838792160f8875fb2d4a653a4
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-05-02 13:20:18 +0000

    Include cmsdisk

--- EventFilter/StorageManager/scripts/operations/setup_sm.sh
+++ EventFilter/StorageManager/scripts/operations/setup_sm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: setup_sm.sh,v 1.4 2008/04/28 13:34:22 loizides Exp $
+# $Id: setup_sm.sh,v 1.5 2008/05/02 11:10:17 loizides Exp $
 
 if test -e "/etc/profile.d/sm_env.sh"; then 
     source /etc/profile.d/sm_env.sh;
@@ -15,10 +15,15 @@ if test -n "$SM_LOOKAREA"; then
 fi
 
 hname=`hostname | cut -d. -f1`;
+nname="node"`echo $hname | cut -d- -f3` 
+
 case $hname in
     cmsdisk0)
+        echo "cmsdisk0 needs manual treatment"
+        exit 0;
         ;;
     cmsdisk1)
+        nname=node_cmsdisk1
         ;;
     srv-*)
 	for i in $store/sata*a*v*; do 
@@ -27,29 +32,31 @@ case $hname in
                 mount -L $sn $i
             fi
 	done
-        if test -n "$SM_LA_NFS"; then
-            if test -z "`mount | grep $lookarea`"; then
-                mkdir -p $lookarea
-                chmod 000 $lookarea
-                echo "Attempting to mount $lookarea"
-                mount -t nfs -o rsize=32768,wsize=32768,timeo=14,intr $SM_LA_NFS $lookarea
-            fi
-        fi
-        if test -n "$SM_CALIB_NFS" -a -n "$SM_CALIBAREA"; then
-            if test -z "`mount | grep $SM_CALIBAREA`"; then
-                mkdir -p $SM_CALIBAREA
-                chmod 000 $SM_CALIBAREA
-                echo "Attempting to mount $SM_CALIBAREA"
-                mount -t nfs -o rsize=32768,wsize=32768,timeo=14,intr $SM_CALIB_NFS $SM_CALIBAREA
-            fi
-        fi
-	nname="node"`echo $hname | cut -d- -f3` 
-	su - cmsprod -c "~cmsprod/$nname/t0_control.sh stop" >/dev/null 2>&1
-	su - cmsprod -c "~cmsprod/$nname/t0_control.sh start"
-	su - smpro -c "~smpro/scripts/t0inject.sh stop" >/dev/null 2>&1
-	su - smpro -c "~smpro/scripts/t0inject.sh start"
-	;;
+        ;;
     *)
         echo "Unknown host: $hname"
         ;;
 esac
+
+if test -n "$SM_LA_NFS"; then
+    if test -z "`mount | grep $lookarea`"; then
+        mkdir -p $lookarea
+        chmod 000 $lookarea
+        echo "Attempting to mount $lookarea"
+        mount -t nfs -o rsize=32768,wsize=32768,timeo=14,intr $SM_LA_NFS $lookarea
+    fi
+fi
+
+if test -n "$SM_CALIB_NFS" -a -n "$SM_CALIBAREA"; then
+    if test -z "`mount | grep $SM_CALIBAREA`"; then
+        mkdir -p $SM_CALIBAREA
+        chmod 000 $SM_CALIBAREA
+        echo "Attempting to mount $SM_CALIBAREA"
+        mount -t nfs -o rsize=32768,wsize=32768,timeo=14,intr $SM_CALIB_NFS $SM_CALIBAREA
+    fi
+fi
+
+su - cmsprod -c "~cmsprod/$nname/t0_control.sh stop" >/dev/null 2>&1
+su - cmsprod -c "~cmsprod/$nname/t0_control.sh start"
+su - smpro -c "~smpro/scripts/t0inject.sh stop" >/dev/null 2>&1
+su - smpro -c "~smpro/scripts/t0inject.sh start"
