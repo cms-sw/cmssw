d5dbb5dec7f8d91d432e025ba6ada86441da13f5
Anton Anastassov <sha1-3db3eeb8464f657978ce5211c2d36ab42dec8d78@cern.ch>
2008-05-02 05:23:57 +0000

    Changes to enable Jet/Met to store references/pointers to calotowers.

--- DataFormats/CaloTowers/src/classes.h
+++ DataFormats/CaloTowers/src/classes.h
@@ -4,6 +4,13 @@
 #include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
+#include "DataFormats/Common/interface/RefHolder.h"
+#include "DataFormats/Common/interface/Holder.h"
+#include "DataFormats/Common/interface/RefVectorHolder.h"
+#include "DataFormats/Common/interface/VectorHolder.h"
+#include "DataFormats/Common/interface/PtrVector.h"
+
+
 namespace {
   namespace {
     std::vector<CaloTower> v1;
@@ -12,5 +19,12 @@ namespace {
     CaloTowersRef rr1;
     CaloTowerRefs rrr1;
     edm::Wrapper<CaloTowerCollection> w1;
+
+    edm::reftobase::Holder<reco::Candidate, CaloTowerRef> rbh;
+    edm::reftobase::RefHolder<CaloTowerRef> rbrh;
+    edm::reftobase::VectorHolder<reco::Candidate, CaloTowerRefs> rbhs;
+    edm::reftobase::RefVectorHolder<CaloTowerRefs> rbrhs;
+   edm::Ptr<CaloTower> ptr;
+   edm::PtrVector<CaloTower> ptrv;
   }
 }
