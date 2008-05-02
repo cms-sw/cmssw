f54ee6aa3cc238034a2870a5f3e44a3f93692246
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-05-02 09:56:29 +0000

    removed files that are now migrated to RecoEgamma/electronIdentification

--- EgammaAnalysis/ElectronIDESSources/src/ESSources.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-#include "RecoEgamma/ElectronIdentification/interface/ElectronLikelihood.h"
-
-EVENTSETUP_DATA_REG( ElectronLikelihood );
