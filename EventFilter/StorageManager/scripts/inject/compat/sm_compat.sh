5f8ebc41c744ddca010006db65ef47ce7eed3770
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-05-02 12:36:15 +0000

    Delete _transfd

--- EventFilter/StorageManager/scripts/inject/compat/sm_compat.sh
+++ EventFilter/StorageManager/scripts/inject/compat/sm_compat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id:$
+# $Id: sm_compat.sh,v 1.2 2008/04/28 13:33:50 loizides Exp $
 
 if test -e "/etc/profile.d/sm_env.sh"; then 
     source /etc/profile.d/sm_env.sh;
@@ -10,8 +10,10 @@ if test -n "$SM_STORE"; then
     store=$SM_STORE
 fi
 
+echo -n "$0 run at " >> /tmp/sm_compat.tmp
 date >> /tmp/sm_compat.tmp
 
+# notify
 for i in `find $store/global/mbox/ -iname "*.work_in_progress" -cmin +10`; do
     mv $i `echo $i | cut -d. -f1`.notify
 done
@@ -20,6 +22,7 @@ for i in `find $store/emulator/mbox/ -iname "*.work_in_progress" -cmin +10`; do
     mv $i `echo $i | cut -d. -f1`.notify
 done
 
+# cleanup
 for i in `find $store/global/mbox/ -iname "*.smry*"`; do 
     rm -f $i; 
 done
@@ -27,3 +30,9 @@ done
 for i in `find $store/emulator/mbox/ -iname "*.smry*"`; do 
     rm -f $i; 
 done
+
+if test -e $store/global/00; then
+    for i in `find $store/global/*/closed -iname "*.*_transfd"`; do 
+        rm -f $i; 
+    done
+fi
