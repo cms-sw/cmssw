34fa58a289451878764b01f2744db26c5d42f6ef
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-05-06 12:15:22 +0000

    remove obsolete version

--- MagneticField/ParametrizedEngine/src/ParametrizedMagneticFieldProducer.cc
+++ MagneticField/ParametrizedEngine/src/ParametrizedMagneticFieldProducer.cc
@@ -1,12 +1,11 @@
 /** \file
  *
- *  $Date: 2008/04/23 14:41:52 $
- *  $Revision: 1.3 $
+ *  $Date: 2008/04/29 11:39:11 $
+ *  $Revision: 1.4 $
  *  \author Massimiliano Chiorboli, updated NA 03/08
  */
 
-#include "MagneticField/ParametrizedEngine/interface/ParametrizedMagneticFieldProducer.h"
-#include "MagneticField/ParametrizedEngine/interface/ParametrizedMagneticField.h"
+#include "MagneticField/ParametrizedEngine/src/ParametrizedMagneticFieldProducer.h"
 #include "MagneticField/ParametrizedEngine/src/OAEParametrizedMagneticField.h"
 #include "MagneticField/ParametrizedEngine/src/OAE85lParametrizedMagneticField.h"
 #include "MagneticField/ParametrizedEngine/src/PolyFit2DParametrizedMagneticField.h"
@@ -41,22 +40,18 @@ ParametrizedMagneticFieldProducer::produce(const IdealMagneticFieldRecord& iReco
   ParameterSet parameters = pset.getParameter<ParameterSet>("parameters");
 
   if (version=="OAE_85l_030919") {
+    // V. Karimaki's off-axis expansion fitted to v85l TOSCA computation
     std::auto_ptr<MagneticField> result(new OAE85lParametrizedMagneticField(parameters));
     return result;
   } else if (version=="OAE_1103l_071212") {
+    // V. Karimaki's off-axis expansion fitted to v1103l TOSCA computation
     std::auto_ptr<MagneticField> result( new OAEParametrizedMagneticField(parameters));
     return result;
   } else if (version=="PolyFit2D") {
-  // V. Maroussov polynomial fit to mapping data
+    // V. Maroussov polynomial fit to mapping data
     std::auto_ptr<MagneticField> result( new PolyFit2DParametrizedMagneticField(parameters));
     return result;
-  } else if (version=="OAE_85l_030919_t") {
-    // Use old field - this should be replaced by OAE_85l_030919!!!
-    float a = parameters.getParameter<double>("paramFieldRadius");
-    float l = parameters.getParameter<double>("paramFieldLength");
-    std::auto_ptr<MagneticField> result(new ParametrizedMagneticField(a,l));
-    return result;
-  } else {
+  }  else {
     throw cms::Exception("InvalidParameter")<<"Invalid parametrization version " << version;
   }
   return std::auto_ptr<MagneticField>(0); //make compiler happy
