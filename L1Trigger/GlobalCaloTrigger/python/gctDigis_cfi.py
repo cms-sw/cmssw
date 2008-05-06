2093e1c03cee5234163004db96d9061af9fecb96
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-05-06 17:26:50 +0000

    fxi for GT seg fault

--- L1Trigger/GlobalCaloTrigger/python/gctDigis_cfi.py
+++ L1Trigger/GlobalCaloTrigger/python/gctDigis_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 gctDigis = cms.EDFilter("L1GctEmulator",
     jetFinderType = cms.string('hardwareJetFinder'),
     inputLabel = cms.InputTag("rctDigis"),
-    preSamples = cms.uint32(0),
-    postSamples = cms.uint32(0)
+    preSamples = cms.uint32(1),
+    postSamples = cms.uint32(1)
 )
 
 
