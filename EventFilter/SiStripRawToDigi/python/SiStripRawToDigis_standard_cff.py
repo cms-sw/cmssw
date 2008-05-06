b0aa4e6ae7a4f3294700808dc67b0f7b1217766c
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-05-06 19:39:08 +0000

    Follow SiStripDigis->siStripDigis naming convention change

--- EventFilter/SiStripRawToDigi/python/SiStripRawToDigis_standard_cff.py
+++ EventFilter/SiStripRawToDigi/python/SiStripRawToDigis_standard_cff.py
@@ -4,7 +4,7 @@ import copy
 from EventFilter.SiStripRawToDigi.SiStripDigis_cfi import *
 # raw-to-digi module
 #include "EventFilter/SiStripRawToDigi/data/SiStripDigis.cfi"
-siStripDigis = copy.deepcopy(SiStripDigis)
+siStripDigis = copy.deepcopy(siStripDigis)
 # zero-suppressor module for raw modes
 #include "RecoLocalTracker/SiStripZeroSuppression/data/SiStripZeroSuppression.cfi"
 SiStripRawToDigis = cms.Sequence(siStripDigis)
