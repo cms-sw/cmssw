1af478e416985dd0a0a2f80983f0d8067444d944
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2008-05-07 09:28:40 +0000

    update for CRUZET

--- EventFilter/CSCRawToDigi/python/cscUnpacker_cfi.py
+++ EventFilter/CSCRawToDigi/python/cscUnpacker_cfi.py
@@ -1,18 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 # This is a generic cfi file for CSC unpacking
-# tumanov@rice.edu 3/16/07
+
 muonCSCDigis = cms.EDFilter("CSCDCCUnpacker",
     PrintEventNumber = cms.untracked.bool(False),
     # Use CSC examiner for corrupt or semi-corrupt data to avoid unpacker crashes
     UseExaminer = cms.untracked.bool(True),
     # This mask simply reduces error reporting
-    ErrorMask = cms.untracked.uint32(3754946559),
+    ErrorMask = cms.untracked.uint32(0x0),
     # Define input to the unpacker
     #InputTag InputObjects = cscpacker:CSCRawData
     InputObjects = cms.InputTag("source"),
     # This mask is needed by the examiner if it's used
-    ExaminerMask = cms.untracked.uint32(374076406),
+    ExaminerMask = cms.untracked.uint32(0x1FEBF3F6),
     #this flag disables unpacking of the Status Digis
     UnpackStatusDigis = cms.untracked.bool(False),
     #set this to true if unpacking MTCC data from summer-fall MTCC2006 
