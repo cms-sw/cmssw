da8fc3f391aaa95b2296d0f4f066719f5700a24b
John Marraffino <sha1-3565b4bb138dc66a8251e997feee5430e8526679@cern.ch>
2008-05-07 18:38:21 +0000

    Expand RandomNumberGeneratorService test to include both old style cfg's and new style cfg's.

--- IOMC/RandomEngine/test/testRandomService.sh
+++ IOMC/RandomEngine/test/testRandomService.sh
@@ -1,41 +1,67 @@
 #!/bin/bash
 
-test=testRandomService
-
 function die { echo Failure $1: status $2 ; exit $2 ; }
+
+newtest=testRandomService
+oldtest=oldStyle
 
 pushd ${LOCAL_TMP_DIR}
 
   echo " "
-  echo "RandomNumberGeneratorService test 1"
-  echo "==================================="
-  cmsRun -p ${LOCAL_TEST_DIR}/${test}1.cfg || die "cmsRun ${test}1.cfg" $?
-  mv ${test}.txt ${test}1.txt 
-  diff ${LOCAL_TEST_DIR}/unit_test_outputs/${test}1.txt ${LOCAL_TMP_DIR}/${test}1.txt || die "comparing ${test}1.txt" $?
+  echo "RandomNumberGeneratorService test new format 1"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${newtest}1.cfg 
+  mv ${newtest}.txt ${newtest}1.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}1.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}1.txt || die "comparing ${newtest}1.txt" $?
 
   echo " "
-  echo "RandomNumberGeneratorService test 2"
-  echo "==================================="
-  cmsRun -p ${LOCAL_TEST_DIR}/${test}2.cfg || die "cmsRun ${test}2.cfg" $?
-  mv ${test}.txt ${test}2.txt 
-  diff ${LOCAL_TEST_DIR}/unit_test_outputs/${test}2.txt ${LOCAL_TMP_DIR}/${test}2.txt || die "comparing ${test}2.txt" $?
+  echo "RandomNumberGeneratorService test new format 2"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${newtest}2.cfg 
+  mv ${newtest}.txt ${newtest}2.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}2.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}2.txt || die "comparing ${newtest}2.txt" $?
 
   echo " "
-  echo "RandomNumberGeneratorService test 3"
-  echo "==================================="
-  cmsRun -p ${LOCAL_TEST_DIR}/${test}3.cfg || die "cmsRun ${test}3.cfg" $?
-  mv ${test}.txt ${test}3.txt 
-  diff ${LOCAL_TEST_DIR}/unit_test_outputs/${test}3.txt ${LOCAL_TMP_DIR}/${test}3.txt || die "comparing ${test}3.txt" $?
+  echo "RandomNumberGeneratorService test new format 3"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${newtest}3.cfg 
+  mv ${newtest}.txt ${newtest}3.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}3.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}3.txt || die "comparing ${newtest}3.txt" $?
 
   echo " "
-  echo "RandomNumberGeneratorService test 4"
-  echo "==================================="
-  cmsRun -p ${LOCAL_TEST_DIR}/${test}4.cfg -j testRandomService4.xml || die "cmsRun ${test}4.cfg" $?
-  mv ${test}.txt ${test}4.txt 
-  diff ${LOCAL_TEST_DIR}/unit_test_outputs/${test}4.txt ${LOCAL_TMP_DIR}/${test}4.txt || die "comparing ${test}4.txt" $?
-#
-# Can't do this comparison since the job report has dates in it.
-# diff ${LOCAL_TEST_DIR}/unit_test_outputs/${test}4.xml ${LOCAL_TMP_DIR}/${test}4.xml || die "comparing ${test}4.xml" $?
+  echo "RandomNumberGeneratorService test new format 4"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${newtest}4.cfg 
+  mv ${newtest}.txt ${newtest}4.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}4.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}4.txt || die "comparing ${newtest}4.txt" $?
+
+  echo " "
+  echo "RandomNumberGeneratorService test old format 1"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${oldtest}1.cfg
+  mv ${newtest}.txt ${newtest}1.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}1.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}1.txt || die "comparing ${newtest}1.txt" $?
+
+  echo " "
+  echo "RandomNumberGeneratorService test old format 2"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${oldtest}2.cfg 
+  mv ${newtest}.txt ${newtest}2.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}2.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}2.txt || die "comparing ${newtest}2.txt" $?
+
+  echo " "
+  echo "RandomNumberGeneratorService test old format 3"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${oldtest}3.cfg 
+  mv ${newtest}.txt ${newtest}3.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}3.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}3.txt || die "comparing ${newtest}3.txt" $?
+
+  echo " "
+  echo "RandomNumberGeneratorService test old format 4"
+  echo "=============================================="
+  cmsRun ${LOCAL_TEST_DIR}/${oldtest}4.cfg 
+  mv ${newtest}.txt ${newtest}4.txt
+  diff ${LOCAL_TMP_DIR}/${newtest}4.txt ${LOCAL_TEST_DIR}/unit_test_outputs/${newtest}4.txt || die "comparing ${newtest}4.txt" $?
 
 popd
 
