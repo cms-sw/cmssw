d2fbab3cfc0a55f7e9e6bbcf68b97d12e77ca0ab
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-07 14:30:41 +0000

    syncing with old style cfg

--- RecoBTag/SoftLepton/python/softMuonNoIPBJetTags_cfi.py
+++ RecoBTag/SoftLepton/python/softMuonNoIPBJetTags_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 softMuonNoIPBJetTags = cms.EDProducer("JetTagProducer",
-    tagInfo = cms.InputTag("softMuonTagInfos"),
+    tagInfos = cms.VInputTag( cms.InputTag("softMuonTagInfos") ),
     jetTagComputer = cms.string('softMuonNoIP')
 )
 
