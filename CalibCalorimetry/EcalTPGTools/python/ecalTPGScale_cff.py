f82a8a75ba7e80155852457c7073c22809d43eac
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-07 21:13:05 +0000

    fixed typo

--- CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
+++ CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 tpparams12 = ESSource("EmptyESSource", 
                       recordName = cms.string("EcalTPGPhysicsConstRcd"),
                       firstValid = cms.vuint32( ),
-                      bool iovIsRunNotTime = cms.bool(True)
+                      iovIsRunNotTime = cms.bool(True)
                      )
