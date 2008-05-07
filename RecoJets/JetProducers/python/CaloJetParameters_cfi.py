4fb4a26cca3d1625f1e5f71ba60e6cf874ff4216
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-07 23:13:39 +0000

    updating python after change in data

--- RecoJets/JetProducers/python/CaloJetParameters_cfi.py
+++ RecoJets/JetProducers/python/CaloJetParameters_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 # Standard CalooJets parameters
-# $Id: CaloJetParameters.cfi,v 1.2 2008/03/11 21:34:33 fedor Exp $
+# $Id: CaloJetParameters_cfi.py,v 1.2 2008/04/21 03:28:08 rpw Exp $
 CaloJetParameters = cms.PSet(
-    src = cms.InputTag("caloTowers"),
+    src = cms.InputTag("towerMaker"),
     verbose = cms.untracked.bool(False),
     jetPtMin = cms.double(0.0),
     inputEtMin = cms.double(0.5),
