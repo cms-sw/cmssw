911c210e18f754a8f03ef3fba6e971e18eacae36
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2008-05-08 04:17:00 +0000

    Moving defaults to L1RCTConfig.cfx

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
-
+ 
 from CalibCalorimetry.EcalTPGTools.ecalTPGScale_cff import *
 
-from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
+from L1TriggerConfig.RCTConfigProducers.L1RCTORCAConfig_cfi import *
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
     recordName = cms.string('L1RCTParametersRcd'),
     iovIsRunNotTime = cms.bool(True),
