01aa48490b2bd9116a69f8a606b4142ef454d890
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-08 21:29:19 +0000

    fixing namespaces

--- CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
+++ CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-tpparams12 = ESSource("EmptyESSource", 
+tpparams12 = cms.ESSource("EmptyESSource", 
                       recordName = cms.string("EcalTPGPhysicsConstRcd"),
                       firstValid = cms.vuint32(1),
                       iovIsRunNotTime = cms.bool(True)
