6e05d59da593d6b4ca8b47c32cececf6db16217f
Sho Maruyama <sha1-b47564b910de3cc3cd657a4905f52d93b10eea83@cern.ch>
2008-05-08 04:27:22 +0000

    minor fix

--- RecoTauTag/RecoTau/src/PFRecoTauDiscriminationAgainstMuon.cc
+++ RecoTauTag/RecoTau/src/PFRecoTauDiscriminationAgainstMuon.cc
@@ -7,21 +7,19 @@ void PFRecoTauDiscriminationAgainstMuon::produce(Event& iEvent,const EventSetup&
   // fill the AssociationVector object
   auto_ptr<PFTauDiscriminator> thePFTauDiscriminatorAgainstMuon(new PFTauDiscriminator(PFTauRefProd(thePFTauCollection)));
 
-if(discriminatorOption_.c_str() != "noSegMatch" && discriminatorOption_.c_str() != "twoDCut" && discriminatorOption_.c_str() != "merePresence" && discriminatorOption_.c_str() != "combined") {
-throw edm::Exception(edm::errors::UnimplementedFeature) << " Invalid Discriminator Option! Please check cfi file \n";
-}
-
   for(size_t iPFTau=0;iPFTau<thePFTauCollection->size();++iPFTau) {
     PFTauRef thePFTauRef(thePFTauCollection,iPFTau);
     bool decision = true;
+
 if((*thePFTauRef).hasMuonReference() ){
+
 MuonRef muonref = (*thePFTauRef).leadPFChargedHadrCand()->muonRef();
-    if (discriminatorOption_.c_str() == "noSegMatch") {
+    if (discriminatorOption_ == "noSegMatch") {
       if ( muonref ->numberOfMatches() > 0 ) {
 	decision = false;
       }
     }
-    if (discriminatorOption_.c_str() == "twoDCut") {
+    else if (discriminatorOption_ == "twoDCut") {
 double seg = muonid::getSegmentCompatibility(*muonref);
 double calo= muonref->caloCompatibility(); 
 double border = calo * a + seg * b;
@@ -29,8 +27,8 @@ double border = calo * a + seg * b;
     decision = false; 
       } 
     }
-    if (discriminatorOption_.c_str() == "merePresence") decision = false;
-    if (discriminatorOption_.c_str() == "combined") { // testing purpose only
+    else if (discriminatorOption_ == "merePresence") decision = false;
+    else if (discriminatorOption_ == "combined") { // testing purpose only
 unsigned int muType = 0;
 if(muonref->isGlobalMuon()) muType = 1;
 else if(muonref->isCaloMuon()) muType = 2;
@@ -42,6 +40,9 @@ if(fabs(muonref->eta()) > 2.3 || (fabs(muonref->eta()) > 1.4 && fabs(muonref->et
 if(muonref->phi() < 0.1 && muonref->phi() > -0.1) phi_veto = true;
 if( muType != 1 || muonref ->numberOfMatches() > 0 || eta_veto || phi_veto || muonEnergyFraction > 0.9 ) decision = false; // as place holder
 }
+else{
+//cout << discriminatorOption_ << endl;
+throw edm::Exception(edm::errors::UnimplementedFeature) << " Invalid Discriminator Option! Please check cfi file \n";
 }
 
     if (decision) {
@@ -49,7 +50,8 @@ if( muType != 1 || muonref ->numberOfMatches() > 0 || eta_veto || phi_veto || mu
     } else {
       thePFTauDiscriminatorAgainstMuon->setValue(iPFTau,0);
     }
-}
+} // valid muon ref
+} // pf tau loop
   iEvent.put(thePFTauDiscriminatorAgainstMuon);
-}
+} // class itself
 
