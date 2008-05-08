9cc92360b2e957b75659fc72894d83ee4109a578
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-08 21:27:11 +0000

    fixing syntax error

--- CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
+++ CalibCalorimetry/EcalTPGTools/python/ecalTPGScale_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 tpparams12 = ESSource("EmptyESSource", 
                       recordName = cms.string("EcalTPGPhysicsConstRcd"),
-                      firstValid = cms.vuint32( ),
+                      firstValid = cms.vuint32(1),
                       iovIsRunNotTime = cms.bool(True)
                      )
