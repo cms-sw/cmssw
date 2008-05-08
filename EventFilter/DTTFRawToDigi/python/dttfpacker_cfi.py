ae1b9df5a747b9bb7377aecc95ba039df10bfe6e
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-05-08 18:42:45 +0000

    fix following sim digi label change

--- EventFilter/DTTFRawToDigi/python/dttfpacker_cfi.py
+++ EventFilter/DTTFRawToDigi/python/dttfpacker_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 dttfpacker = cms.EDFilter("DTTFFEDSim",
-    DTTracks_Source = cms.InputTag("dttfDigis","DTTF"),
-    DTDigi_Source = cms.InputTag("dtTriggerPrimitiveDigis")
+    DTTracks_Source = cms.InputTag("simDttfDigis","DTTF"),
+    DTDigi_Source = cms.InputTag("simDtTriggerPrimitiveDigis")
 )
 
 
