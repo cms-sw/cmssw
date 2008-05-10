9764c4fa2285e2b20b57891ced80863a99a54be5
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-10 09:25:10 +0000

    add missing file

---
+++ SimGeneral/MixingModule/python/mixObjects_cfi.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+
+mixSimHits = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits","BSCHits"), cms.InputTag("g4SimHits","FP420SI"), cms.InputTag("g4SimHits","MuonCSCHits"), cms.InputTag("g4SimHits","MuonDTHits"), cms.InputTag("g4SimHits","MuonRPCHits"), 
+        cms.InputTag("g4SimHits","TotemHitsRP"), cms.InputTag("g4SimHits","TotemHitsT1"), cms.InputTag("g4SimHits","TotemHitsT2Gem"), cms.InputTag("g4SimHits","TrackerHitsPixelBarrelHighTof"), cms.InputTag("g4SimHits","TrackerHitsPixelBarrelLowTof"), 
+        cms.InputTag("g4SimHits","TrackerHitsPixelEndcapHighTof"), cms.InputTag("g4SimHits","TrackerHitsPixelEndcapLowTof"), cms.InputTag("g4SimHits","TrackerHitsTECHighTof"), cms.InputTag("g4SimHits","TrackerHitsTECLowTof"), cms.InputTag("g4SimHits","TrackerHitsTIBHighTof"), 
+        cms.InputTag("g4SimHits","TrackerHitsTIBLowTof"), cms.InputTag("g4SimHits","TrackerHitsTIDHighTof"), cms.InputTag("g4SimHits","TrackerHitsTIDLowTof"), cms.InputTag("g4SimHits","TrackerHitsTOBHighTof"), cms.InputTag("g4SimHits","TrackerHitsTOBLowTof")),
+    type = cms.string('PSimHit'),
+    subdets = cms.vstring('BSCHits', 
+        'FP420SI', 
+        'MuonCSCHits', 
+        'MuonDTHits', 
+        'MuonRPCHits', 
+        'TotemHitsRP', 
+        'TotemHitsT1', 
+        'TotemHitsT2Gem', 
+        'TrackerHitsPixelBarrelHighTof', 
+        'TrackerHitsPixelBarrelLowTof', 
+        'TrackerHitsPixelEndcapHighTof', 
+        'TrackerHitsPixelEndcapLowTof', 
+        'TrackerHitsTECHighTof', 
+        'TrackerHitsTECLowTof', 
+        'TrackerHitsTIBHighTof', 
+        'TrackerHitsTIBLowTof', 
+        'TrackerHitsTIDHighTof', 
+        'TrackerHitsTIDLowTof', 
+        'TrackerHitsTOBHighTof', 
+        'TrackerHitsTOBLowTof')
+)
+mixCaloHits = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits","CaloHitsTk"), cms.InputTag("g4SimHits","CastorBU"), cms.InputTag("g4SimHits","CastorFI"), cms.InputTag("g4SimHits","CastorPL"), cms.InputTag("g4SimHits","CastorTU"), 
+        cms.InputTag("g4SimHits","EcalHitsEB"), cms.InputTag("g4SimHits","EcalHitsEE"), cms.InputTag("g4SimHits","EcalHitsES"), cms.InputTag("g4SimHits","EcalTBH4BeamHits"), cms.InputTag("g4SimHits","HcalHits"), 
+        cms.InputTag("g4SimHits","HcalTB06BeamHits"), cms.InputTag("g4SimHits","ZDCHITS")),
+    type = cms.string('PCaloHit'),
+    subdets = cms.vstring('CaloHitsTk', 
+        'CastorBU', 
+        'CastorFI', 
+        'CastorPL', 
+        'CastorTU', 
+        'EcalHitsEB', 
+        'EcalHitsEE', 
+        'EcalHitsES', 
+        'EcalTBH4BeamHits', 
+        'HcalHits', 
+        'HcalTB06BeamHits', 
+        'ZDCHITS')
+)
+mixSimTracks = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits")),
+    type = cms.string('SimTrack')
+)
+mixSimVertices = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits")),
+    type = cms.string('SimVertex')
+)
+mixHepMCProducts = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits")),
+    type = cms.string('HepMCProduct')
+)
+
