a5bfb2b7c02b1591af971d0c52c4cfdf889c48b0
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2008-05-12 10:43:24 +0000

    removed the EgammaHLTFixedMatrixClusterProducer (.h and .cc) and renamed it to EgammaHLTMulti5x5ClusterProducer (.h and .cc), according to the name changing in RecoEcal/EgammaClusterAlgos

--- RecoEgamma/EgammaHLTProducers/src/SealModules.cc
+++ RecoEgamma/EgammaHLTProducers/src/SealModules.cc
@@ -9,7 +9,7 @@
 
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTHybridClusterProducer.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTIslandClusterProducer.h"
-#include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTFixedMatrixClusterProducer.h"
+#include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTMulti5x5ClusterProducer.h"
 
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTHcalIsolationProducersRegional.h"
 #include "RecoEgamma/EgammaHLTProducers/interface/EgammaHLTEcalIsolationProducersRegional.h"
@@ -30,6 +30,6 @@ DEFINE_ANOTHER_FWK_MODULE(EgammaHLTHcalIsolationDoubleConeProducers);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTPhotonTrackIsolationProducersRegional);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTHybridClusterProducer);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTIslandClusterProducer);
-DEFINE_ANOTHER_FWK_MODULE(EgammaHLTFixedMatrixClusterProducer);
+DEFINE_ANOTHER_FWK_MODULE(EgammaHLTMulti5x5ClusterProducer);
 DEFINE_ANOTHER_FWK_MODULE(EcalListOfFEDSProducer);
 DEFINE_ANOTHER_FWK_MODULE(EcalRecHitsMerger);
