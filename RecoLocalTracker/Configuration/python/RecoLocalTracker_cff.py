94bb0a04578106f1978e2be3d311435c17ce3620
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-05-12 09:09:25 +0000

    adapt to the new workflow now the default is run over teh output of raw2digi

--- RecoLocalTracker/Configuration/python/RecoLocalTracker_cff.py
+++ RecoLocalTracker/Configuration/python/RecoLocalTracker_cff.py
@@ -11,11 +11,12 @@ from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
-from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_SimData_cfi import *
+from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi import *
 from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi import *
 from RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi import *
 from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
 pixeltrackerlocalreco = cms.Sequence(siPixelClusters*siPixelRecHits)
 striptrackerlocalreco = cms.Sequence(siStripZeroSuppression*siStripClusters*siStripMatchedRecHits)
 trackerlocalreco = cms.Sequence(pixeltrackerlocalreco*striptrackerlocalreco)
+
 
