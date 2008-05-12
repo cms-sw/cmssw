d50444d9abc95b831f9a1b7feb5aef68881d6267
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-05-12 20:58:19 +0000

    fixed interface change

--- Fireworks/Muons/src/MuonsProxy3DBuilder.cc
+++ Fireworks/Muons/src/MuonsProxy3DBuilder.cc
@@ -123,7 +123,7 @@ void MuonsProxy3DBuilder::build(const FWEventItem* iItem,
            
            
 	// add muon segments
-	const std::vector<reco::MuonChamberMatch>& matches = muon->getMatches();
+	const std::vector<reco::MuonChamberMatch>& matches = muon->matches();
 	Double_t localTrajectoryPoint[3];
 	Double_t globalTrajectoryPoint[3];
 	//need to use auto_ptr since the segmentSet may not be passed to muonList
