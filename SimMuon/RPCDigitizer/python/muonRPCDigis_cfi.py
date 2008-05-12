7135cd9c17de4fce55df94e45fbefee4fed7b962
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-05-12 07:54:38 +0000

    correct an error

--- SimMuon/RPCDigitizer/python/muonRPCDigis_cfi.py
+++ SimMuon/RPCDigitizer/python/muonRPCDigis_cfi.py
@@ -1,7 +1,32 @@
-muonRPCDigis.cfi
-Making 
-ERROR in muonRPCDigis.cfi
-Traceback (most recent call last):
-  File "/afs/cern.ch/cms/sw/ReleaseCandidates/slc4_ia32_gcc345/sun/2.1-sun-09/CMSSW_2_1_X_2008-05-11-0900/src/FWCore/ParameterSet/python/translate.py", line 70, in ?
-    os.remove(includeNode.pythonFileName())
-OSError: [Errno 2] No such file or directory: 'muonRPCDigis_cfi.py'
+import FWCore.ParameterSet.Config as cms
+
+# Module to create simulated RPC digis.
+simMuonRPCDigis = cms.EDProducer("RPCDigiProducer",
+    Noise = cms.bool(True),
+    digiModelConfig = cms.PSet(
+        signalPropagationSpeed = cms.double(0.66),
+        timingRPCOffset = cms.double(50.0),
+        Frate = cms.double(1.0),
+        printOutDigitizer = cms.bool(False),
+        cosmics = cms.bool(False),
+        deltatimeAdjacentStrip = cms.double(3.0),
+        linkGateWidth = cms.double(20.0),
+        Rate = cms.double(0.0),
+        timeResolution = cms.double(2.5),
+        averageClusterSize = cms.double(1.5),
+        Gate = cms.double(25.0),
+        averageEfficiency = cms.double(0.95),
+        Nbxing = cms.int32(9),
+        timeJitter = cms.double(1.0)
+    ),
+    effmapfile = cms.FileInPath('SimMuon/RPCDigitizer/data/RPCDetId_Eff.dat'),
+    Signal = cms.bool(True),
+    clsmapfile = cms.FileInPath('SimMuon/RPCDigitizer/data/ClSizeTot.dat'),
+    noisemapfile = cms.FileInPath('SimMuon/RPCDigitizer/data/RPCDetId_Noise.dat'),
+    timingMap = cms.FileInPath('SimMuon/RPCDigitizer/data/RPCTiming.dat'),
+    InputCollection = cms.string('g4SimHitsMuonRPCHits'),
+    digiModel = cms.string('RPCSimAverageNoiseEff')
+)
+
+
+
