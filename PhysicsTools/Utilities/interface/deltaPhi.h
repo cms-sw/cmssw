78e0c0bbc0039d901f42946cb5dae715e8da53b6
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-05-13 09:35:27 +0000

    added new functions to resolve the ambiguity

--- PhysicsTools/Utilities/interface/deltaPhi.h
+++ PhysicsTools/Utilities/interface/deltaPhi.h
@@ -16,8 +16,21 @@ namespace reco {
     return result;
   }
 
+  inline double deltaPhi(float phi1, double phi2) {
+    return deltaPhi(static_cast<double>(phi1), phi2);
+  }
+  
+  inline double deltaPhi(double phi1, float phi2) {
+    return deltaPhi(phi1, static_cast<double>(phi2));
+  }
+  
+  inline double deltaPhi(float phi1, float phi2) {
+    return deltaPhi(static_cast<double>(phi1),
+		    static_cast<double>(phi2));
+ } 
+
   template<typename T1, typename T2>
-  inline double deltaPhi(const T1& t1, const T2 & t2) {
+    inline double deltaPhi(const T1& t1, const T2 & t2) {
     return deltaPhi(t1.phi(), t2.phi());
   }      
 
