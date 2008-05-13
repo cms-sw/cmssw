e414391784658e53f6cf41e29a95f69fd5f239d7
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-05-13 09:20:59 +0000

    fixed parameter setting

--- PhysicsTools/CandAlgos/interface/CompositeCandSelector.h
+++ PhysicsTools/CandAlgos/interface/CompositeCandSelector.h
@@ -2,6 +2,7 @@
 #define PhysicsTools_CandAlgos_CompositeCandSelector_h
 #include "PhysicsTools/UtilAlgos/interface/ParameterAdapter.h"
 #include "PhysicsTools/CandUtils/interface/CompositeCandSelector.h"
+#include "PhysicsTools/UtilAlgos/interface/StringCutObjectSelector.h"
 
 namespace reco {
   namespace modules {
@@ -11,6 +12,19 @@ namespace reco {
 	static CompositeCandSelector<Selector, T1, T2, nDau> make(const edm::ParameterSet & cfg) {
 	  return CompositeCandSelector<Selector, T1, T2, nDau>(modules::make<Selector>(cfg));
 	}
+      };
+
+    template<template<typename, typename> class Combiner, typename T1, typename T2, unsigned int nDau>
+      struct ParameterAdapter<CompositeCandSelector<Combiner<StringCutObjectSelector<T1>, 
+                                                             StringCutObjectSelector<T2> >, T1, T2, nDau> > {
+	typedef CompositeCandSelector<Combiner<StringCutObjectSelector<T1>,
+					       StringCutObjectSelector<T2> >, T1, T2, nDau> Selector;
+	  static Selector make(const edm::ParameterSet & cfg) {
+	    StringCutObjectSelector<T1> s1(cfg.getParameter<std::string>("cut1"));
+	    StringCutObjectSelector<T2> s2(cfg.getParameter<std::string>("cut2"));
+	    Combiner<StringCutObjectSelector<T1>, StringCutObjectSelector<T2> > c(s1, s2);					   
+	    return Selector(c);
+	  }
       };
 
   }
