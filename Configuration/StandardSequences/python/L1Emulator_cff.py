ccac4f570322f284a662f3b185c30a9653ab6457
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-13 23:09:31 +0000

    adding new py configs

--- Configuration/StandardSequences/python/L1Emulator_cff.py
+++ Configuration/StandardSequences/python/L1Emulator_cff.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from L1Trigger.Configuration.L1Emulator_cff import *
+from Configuration.StandardSequences.SimL1Emulator_cff import *
+L1Emulator = cms.Sequence(SimL1Emulator)
 
