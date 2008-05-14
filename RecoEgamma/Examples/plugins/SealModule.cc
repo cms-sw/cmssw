a39c2f5edfbd069ef702749b4e13041f673d973d
Claude Charlot <sha1-5503bd15f2dc44299f7f663420dce7b3ea3927c4@cern.ch>
2008-05-14 12:58:37 +0000

    updated for SuperClusterMerger

--- RecoEgamma/Examples/plugins/SealModule.cc
+++ RecoEgamma/Examples/plugins/SealModule.cc
@@ -11,7 +11,7 @@
 #include "MCPizeroAnalyzer.h"
 #include "SimpleConvertedPhotonAnalyzer.h"
 #include "SimplePhotonAnalyzer.h"
-#include "SiStripElectronAnalyzer.h"
+//#include "SiStripElectronAnalyzer.h"
 #include "PhotonsWithConversionsAnalyzer.h"
 #include "GsfElectronMCAnalyzer.h"
 #include "GsfElectronDataAnalyzer.h"
@@ -19,6 +19,11 @@
 
 DEFINE_SEAL_MODULE();
 
+#include "PhysicsTools/UtilAlgos/interface/Merger.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+typedef Merger<reco::SuperClusterCollection> SuperClusterMerger;
+DEFINE_FWK_MODULE( SuperClusterMerger );
 DEFINE_FWK_MODULE(ElectronIDAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronPixelSeedAnalyzer);
@@ -30,5 +35,5 @@ DEFINE_ANOTHER_FWK_MODULE(GsfElectronDataAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(GsfElectronFakeAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimpleConvertedPhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(SimplePhotonAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAnalyzer);
+//DEFINE_ANOTHER_FWK_MODULE(SiStripElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PhotonsWithConversionsAnalyzer);
