5b56c1089a8c042595aa3f0d2755412dfc891de5
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-05-14 09:31:40 +0000

    threshold object for ZeroSuppression

---
+++ CondFormats/DataRecord/interface/SiStripThresholdRcd.h
@@ -0,0 +1,26 @@
+#ifndef CondFormats_SiStripThresholdRcd_h
+#define CondFormats_SiStripThresholdRcd_h
+// -*- C++ -*-
+//
+// Package:     CondFormats
+// Class  :     SiStripThresholdRcd
+// 
+/**\class SiStripThresholdRcd SiStripThresholdRcd.h src/CondFormats/interface/SiStripThresholdRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Tue Oct 25 09:42:05 CEST 2005
+// $Id: SiStripThresholdRcd.h,v 1.1 2005/11/29 11:03:25 gbruno Exp $
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripThresholdRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripThresholdRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/SiStripThresholdRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats
+// Class  :     SiStripThresholdRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// $Id: SiStripThresholdRcd.cc,
+
+#include "CondFormats/DataRecord/interface/SiStripThresholdRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiStripThresholdRcd);
