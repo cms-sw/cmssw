7b0b9d2c066daabbdf6b62ca1e26d3226f9e8b47
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-05-14 10:01:01 +0000

    moved here essources for pedestal/noise. added essources for thresholds

--- CalibTracker/SiStripESProducers/plugins/fake/modules.cc
+++ CalibTracker/SiStripESProducers/plugins/fake/modules.cc
@@ -16,3 +16,15 @@ DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripFedCablingFakeESSource);
 #include "CalibTracker/SiStripESProducers/plugins/fake/SiStripHashedDetIdFakeESSource.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripHashedDetIdFakeESSource);
 
+#include "CalibTracker/SiStripESProducers/plugins/fake/SiStripNoiseFakeESSource.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripNoiseFakeESSource);
+
+#include "CalibTracker/SiStripESProducers/plugins/fake/SiStripPedestalsFakeESSource.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripPedestalsFakeESSource);
+
+#include "CalibTracker/SiStripESProducers/plugins/fake/SiStripThresholdFakeESSource.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(SiStripThresholdFakeESSource);
+
+#include "CalibTracker/SiStripESProducers/plugins/fake/SiStripThresholdFakeOnDB.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripThresholdFakeOnDB);
+
--- CalibTracker/SiStripESProducers/plugins/geom/modules.cc
+++ CalibTracker/SiStripESProducers/plugins/geom/modules.cc
@@ -7,3 +7,5 @@ DEFINE_SEAL_MODULE();
 #include "CalibTracker/SiStripESProducers/plugins/geom/SiStripHashedDetIdESModule.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(SiStripHashedDetIdESModule);
 
+#include "CalibTracker/SiStripESProducers/plugins/geom/SiStripNoiseDummyCalculator.h"
+DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDummyCalculator);
