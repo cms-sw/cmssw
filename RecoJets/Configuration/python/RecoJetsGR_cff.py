100f957fdae8c0a4747d36aed7ed0f144df4bd83
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-15 18:14:13 +0000

    adding new py configs

--- RecoJets/Configuration/python/RecoJetsGR_cff.py
+++ RecoJets/Configuration/python/RecoJetsGR_cff.py
@@ -1,23 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoJetsGR.cff,v 1.1 2008/04/08 20:22:09 fedor Exp $
+# Standard set:
+from RecoJets.JetProducers.kt4CaloJets_cff import *
+from RecoJets.JetProducers.kt6CaloJets_cff import *
+from RecoJets.JetProducers.iterativeCone5CaloJets_cff import *
+from RecoJets.JetProducers.sisCone5CaloJets_cff import *
+from RecoJets.JetProducers.sisCone7CaloJets_cff import *
+# $Id: RecoJetsGR.cff,v 1.3 2008/04/30 14:23:31 arizzi Exp $
 #
-from RecoJets.JetProducers.CaloJetParameters_cfi import *
-from RecoJets.JetProducers.IconeJetParameters_cfi import *
-iterativeCone5CaloJets = cms.EDProducer("IterativeConeJetProducer",
-    CaloJetParameters,
-    IconeJetParameters,
-    alias = cms.untracked.string('IC5CaloJet'),
-    coneRadius = cms.double(0.5)
-)
-
+# special R=0.15 IC jets:
 iterativeCone15CaloJets = cms.EDProducer("IterativeConeJetProducer",
+    #       using IconeJetParameters
     CaloJetParameters,
-    IconeJetParameters,
     alias = cms.untracked.string('IC15CaloJet'),
+    coneRadius = cms.double(0.15),
     seedThreshold = cms.double(0.5),
-    coneRadius = cms.double(0.15)
+    debugLevel = cms.untracked.int32(0)
 )
 
-recoJetsGR = cms.Sequence(iterativeCone5CaloJets+iterativeCone15CaloJets)
+recoJetsGR = cms.Sequence(iterativeCone15CaloJets+kt4CaloJets+kt6CaloJets+iterativeCone5CaloJets+sisCone5CaloJets+sisCone7CaloJets)
 
