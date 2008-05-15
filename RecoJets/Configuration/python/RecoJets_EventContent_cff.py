b12061c776ccda8b65d461b5ffffc76baa9d85b9
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2008-05-15 21:02:06 +0000

    Explicitly specify FullEvent objects

--- RecoJets/Configuration/python/RecoJets_EventContent_cff.py
+++ RecoJets/Configuration/python/RecoJets_EventContent_cff.py
@@ -2,9 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 #Full Event content 
 RecoJetsFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_*_*_*', 
-        'keep recoPFJets_*_*_*', 
-        'keep *_caloTowers_*_*', 
+    outputCommands = cms.untracked.vstring(
+        'keep *_kt4CaloJets_*_*', 
+        'keep *_kt6CaloJets_*_*', 
+        'keep *_iterativeCone5CaloJets_*_*', 
+        'keep *_iterativeCone15CaloJets_*_*', 
+        'keep *_sisCone5CaloJets_*_*', 
+        'keep *_sisCone7CaloJets_*_*', 
+        'keep *_kt4PFJets_*_*', 
+        'keep *_kt6PFJets_*_*', 
+        'keep *_iterativeCone5PFJets_*_*', 
+        'keep *_sisCone5PFJets_*_*', 
+        'keep *_sisCone7PFJets_*_*', 
         'keep *_towerMaker_*_*', 
         'keep *_ic5JetTracksAssociatorAtVertex_*_*', 
         'keep *_iterativeCone5JetTracksAssociatorAtVertex_*_*', 
@@ -18,12 +27,18 @@ RecoJetsFEVT = cms.PSet(
         'keep *_kt4JetExtender_*_*')
 )
 RecoGenJetsFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoGenJets_*_*_*', 
+    outputCommands = cms.untracked.vstring(
+        'keep *_kt4GenJets_*_*', 
+        'keep *_kt6GenJets_*_*', 
+        'keep *_iterativeCone5GenJets_*_*', 
+        'keep *_sisCone5GenJets_*_*', 
+        'keep *_sisCone7GenJets_*_*', 
         'keep *_genParticle_*_*')
 )
 #RECO content
 RecoJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_kt4CaloJets_*_*', 
+    outputCommands = cms.untracked.vstring(
+        'keep *_kt4CaloJets_*_*', 
         'keep *_kt6CaloJets_*_*', 
         'keep *_iterativeCone5CaloJets_*_*', 
         'keep *_iterativeCone15CaloJets_*_*', 
@@ -34,7 +49,6 @@ RecoJetsRECO = cms.PSet(
         'keep *_iterativeCone5PFJets_*_*', 
         'keep *_sisCone5PFJets_*_*', 
         'keep *_sisCone7PFJets_*_*', 
-        'keep *_caloTowers_*_*', 
         'keep *_towerMaker_*_*', 
         'keep *_ic5JetTracksAssociatorAtVertex_*_*', 
         'keep *_iterativeCone5JetTracksAssociatorAtVertex_*_*', 
@@ -48,7 +62,8 @@ RecoJetsRECO = cms.PSet(
         'keep *_kt4JetExtender_*_*')
 )
 RecoGenJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_kt4GenJets_*_*', 
+    outputCommands = cms.untracked.vstring(
+        'keep *_kt4GenJets_*_*', 
         'keep *_kt6GenJets_*_*', 
         'keep *_iterativeCone5GenJets_*_*', 
         'keep *_sisCone5GenJets_*_*', 
@@ -63,7 +78,6 @@ RecoJetsAOD = cms.PSet(
         'keep *_iterativeCone15CaloJets_*_*', 
         'keep *_sisCone5CaloJets_*_*', 
         'keep *_sisCone7CaloJets_*_*', 
-        'keep *_caloTowers_*_*', 
         'keep *_towerMaker_*_*', 
         'keep *_ic5JetTracksAssociatorAtVertex_*_*', 
         'keep *_iterativeCone5JetExtender_*_*', 
