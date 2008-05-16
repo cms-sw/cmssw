8e2d7e658965bea5bf455e25b2d4d3c51b914b5a
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-05-16 15:01:45 +0000

    added new producer for RCT input scales

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
- 
+
 from CalibCalorimetry.EcalTPGTools.ecalTPGScale_cff import *
 
 from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
+
+from L1TriggerConfig.RCTConfigProducers.L1RctInputScalesProducer_cfi import *
+
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
-    recordName = cms.string('L1RCTParametersRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
-
-
+                                  recordName = cms.string('L1RCTParametersRcd'),
+                                  iovIsRunNotTime = cms.bool(True),
+                                  firstValid = cms.vuint32(1)
+                                  )
---
+++ L1TriggerConfig/RCTConfigProducers/python/L1RctInputScalesProducer_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+L1RctInputScalesProducer = cms.ESProducer("L1RctInputScalesProducer")
+
+
+
