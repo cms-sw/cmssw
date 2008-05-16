69462ebefba6d8e3d5de2adc2c69078047fe8b01
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-16 01:10:37 +0000

    update py configs

---
+++ Geometry/CaloEventSetup/python/AlignedCaloGeometry_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# This cfi should be included to build the ALIGNED ECAL + HCAL geometry model
+#
+EcalBarrelGeometryEP.applyAlignment = True
+EcalEndcapGeometryEP.applyAlignment = True
+EcalPreshowerGeometryEP.applyAlignment = True
+
