8220d8425069b694e6b794bb760a0297e4c5a333
Juan Alcaraz Maestre <sha1-00c52069b91662a716b700699899914afb62bf4e@cern.ch>
2008-05-18 15:10:24 +0000

    Update producer of X1/X2 fractions for 16X

--- ElectroWeakAnalysis/WReco/plugins/X1X2PythiaProducer.cc
+++ ElectroWeakAnalysis/WReco/plugins/X1X2PythiaProducer.cc
@@ -20,6 +20,8 @@
 #include "PhysicsTools/CandUtils/interface/Booster.h"
 #include <Math/VectorUtil.h>
 
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
 //
 // class declaration
 //
@@ -32,9 +34,9 @@ class X1X2PythiaProducer : public edm::EDProducer {
       virtual void beginJob(const edm::EventSetup&) ;
       virtual void produce(edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
-      bool FindX1X2(const reco::Candidate* gen1, const reco::Candidate* gen2);
-      double Mass12(const reco::Candidate* ge1, const reco::Candidate* ge2);
-      double pt2Evol(const reco::Candidate* ge1, const reco::Candidate* ge2, int choice);
+      bool FindX1X2(edm::Handle<reco::GenParticleCollection>, const reco::GenParticle*, const reco::GenParticle*);
+      double Mass12(const reco::GenParticle*, const reco::GenParticle*);
+      double Q2InBranch(edm::Handle<reco::GenParticleCollection>, const reco::GenParticle*);
       double _x1;
       double _x2;
 };
@@ -48,62 +50,46 @@ X1X2PythiaProducer::X1X2PythiaProducer(const edm::ParameterSet& iConfig) {
    //register your products
    produces<std::vector<double> >();
    produces<std::vector<int> >();
+   //produces<reco::GenParticleCollection>();
 }
 X1X2PythiaProducer::~X1X2PythiaProducer(){}
 
 //
 // member functions
+
 //
 // ------------ method called to produce the data  ------------
 void
 X1X2PythiaProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-      // New method (CMSSW versions > 167)
-      bool newgen = true;
-      edm::Handle<reco::GenParticleCollection> genNewParticles;
-      // Old method (CMSSW versions <= 167)
-      edm::Handle<reco::CandidateCollection> genParticles;
+      edm::Handle<reco::GenParticleCollection> genParticles;
+      iEvent.getByLabel("genParticles", genParticles);
+      unsigned int gensize = genParticles->size();
 
-      try {
-            iEvent.getByLabel("genParticles", genNewParticles);
-      } catch (...) {
-            // Try old method if new one did not work
-            newgen = false;
-            iEvent.getByLabel("genParticleCandidates", genParticles);
-      }
+      const reco::GenParticle* m1 = 0;
+      const reco::GenParticle* m2 = 0;
 
-      //DumpEvent(iEvent);
-
-      const reco::Candidate* m1 = 0;
-      const reco::Candidate* m2 = 0;
-
-      unsigned int gensize = 0;
-      if (newgen) {
-            gensize = genNewParticles->size();
-            
-      } else {
-            gensize = genParticles->size();
-      }
       for(unsigned int i = 0; i<gensize; ++i) {
-            const reco::Candidate* part;
-            if (newgen) {
-                  part = &((*genNewParticles)[i]);
-            } else {
-                  part = &((*genParticles)[i]);
-            }
-            int status = part->status();
+            const reco::GenParticle& part = (*genParticles)[i];
+            //printf("status %d id %d\n", part.status(), part.pdgId());
+            int status = part.status();
             if (status!=3) continue;
-            int id = part->pdgId();
+            int id = part.pdgId();
             if (id != 23 && abs(id) != 24 ) continue;
-            int nmothers = part->numberOfMothers();
+            int nmothers = part.numberOfMothers();
+            //printf("Boson Id %d numberOfMothers %d\n", id, nmothers);
             if (nmothers!=2) continue;
-            m1 = part->mother(0);
-            m2 = part->mother(1);
+            size_t key1 = part.motherRef(0).key();
+            m1 = &((*genParticles)[key1]);
+            size_t key2 = part.motherRef(1).key();
+            m2 = &((*genParticles)[key2]);
+            //printf("m1 %d m2 %d\n", m1->pdgId(), m2->pdgId());
             break;
       }
 
-      if (m1 && m2 && FindX1X2(m1, m2) ) {
+      if (m1 && m2 && FindX1X2(genParticles, m1, m2) ) {
             std::auto_ptr<std::vector<double> > xfrac (new std::vector<double>);
+
             xfrac->push_back(_x1);
             xfrac->push_back(_x2);
             iEvent.put(xfrac);
@@ -111,17 +97,29 @@ X1X2PythiaProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
             iflav->push_back(m1->pdgId());
             iflav->push_back(m2->pdgId());
             iEvent.put(iflav);
-            if (newgen) {
-                  LogTrace("") << ">>> Parton 1: flavor = " << m1->pdgId() << ", x1 = " << _x1 << ", naive x1= " << fabs(m1->pz()/((*genNewParticles)[0]).pz());
-                  LogTrace("") << ">>> Parton 2: flavor = " << m2->pdgId() << ", x2 = " << _x2 << ", naive x2= " << fabs(m2->pz()/((*genNewParticles)[0]).pz());
-            } else {
-                  LogTrace("") << ">>> Parton 1: flavor = " << m1->pdgId() << ", x1 = " << _x1 << ", naive x1= " << fabs(m1->pz()/((*genParticles)[0]).pz());
-                  LogTrace("") << ">>> Parton 2: flavor = " << m2->pdgId() << ", x2 = " << _x2 << ", naive x2= " << fabs(m2->pz()/((*genParticles)[0]).pz());
+            LogTrace("") << "Naive:      flv1 " << m1->pdgId() << " x1 " << (m1->energy()+fabs(m1->pz()))/2/(*genParticles)[0].pz()<< " flv2 " << m2->pdgId() << " x2 " << (m2->energy()+fabs(m2->pz()))/2/(*genParticles)[0].pz();
+            LogTrace("") << "X1X2PyProd: flv1 " << m1->pdgId() << " x1 " << _x1 << " flv2 " << m2->pdgId() << " x2 " << _x2;
+
+            if (edm::MessageDrop::instance()->debugEnabled) {
+              try {
+                  edm::Handle<edm::HepMCProduct> mc;
+                  iEvent.getByLabel("source",mc);
+                  const HepMC::GenEvent* genev = mc->GetEvent();
+                  HepMC::PdfInfo* pdfstuff = genev->pdf_info();
+                  if (pdfstuff!=0) {
+                        LogTrace("") << "PDFInfo:    flv1 " << pdfstuff->id1() << " x1 " << pdfstuff->x1() << " flv2 " << pdfstuff->id2() << " x2 " << pdfstuff->x2();
+                        LogTrace("") << "PDFInfo:   Scale " << pdfstuff->scalePDF();
+                        //genev->print();
+                  }
+              } catch (...) {
+              }
             }
+
       }
+
 }
       
-bool X1X2PythiaProducer::FindX1X2(const reco::Candidate* gen1, const reco::Candidate* gen2) {
+bool X1X2PythiaProducer::FindX1X2(edm::Handle<reco::GenParticleCollection> genParticles, const reco::GenParticle* gen1, const reco::GenParticle* gen2) {
       _x1 = 1.;
       _x2 = 1.;
 
@@ -130,43 +128,74 @@ bool X1X2PythiaProducer::FindX1X2(const reco::Candidate* gen1, const reco::Candi
       if (!gen1) return false;
       if (!gen2) return false;
 
-      const reco::Candidate* g1 = gen1;
-      const reco::Candidate* g2 = gen2;
-      const reco::Candidate* gprev1 = NULL;
-      const reco::Candidate* gprev2 = NULL;
+      const reco::GenParticle* g1 = gen1;
+      const reco::GenParticle* g2 = gen2;
+      const reco::GenParticle* gprev1 = NULL;
+      const reco::GenParticle* gprev2 = NULL;
       while (1) {
             if (g1==gprev1 && g2==gprev2) break;
             gprev1 = g1;
             gprev2 = g2;
             double sold = Mass12(g1,g2);
+            const reco::GenParticle* m1 = 0;
+            const reco::GenParticle* m2 = 0;
 
             bool choose_x1 = false;
             if (g1->numberOfMothers()>0 && g2->numberOfMothers()>0) {
-                  double pt12 = pt2Evol(g1, g2, 1);
-                  double pt22 = pt2Evol(g1, g2, 2);
+                  double Q21 = Q2InBranch(genParticles, g1);
+                  double Q22 = Q2InBranch(genParticles, g2);
+                  size_t key1 = g1->motherRef(0).key();
+                  size_t key2 = g2->motherRef(0).key();
+                  m1 = &((*genParticles)[key1]);
+                  m2 = &((*genParticles)[key2]);
+                  double mass12 = Mass12(m1,g2);
+                  double mass22 = Mass12(g1,m2);
+                  double z1 = sold/mass12;
+                  double z2 = sold/mass22;
+                  double pt21 = (1-z1)*Q21;
+                  double pt22 = (1-z2)*Q22;
 
-                  if (pt12 > pt22) choose_x1 = true; 
+                  if (pt21 > pt22) choose_x1 = true; 
                   else choose_x1 = false;
 
-                  //double mass12 = Mass12(g1->mother(),g2);
-                  //double mass22 = Mass12(g1,g2->mother());
-                  //printf("sold %f snew1 %f snew2 %f\n", sold, mass12, mass22);
-                  //printf("z1 %f z2 %f pt12 %f pt22 %f\n", sold/mass12, sold/mass22, pt12, pt22);
             } else if (g1->numberOfMothers()>0) {
                   choose_x1 = true;
+                  size_t key1 = g1->motherRef(0).key();
+                  m1 = &((*genParticles)[key1]);
             } else if (g2->numberOfMothers()>0) {
                   choose_x1 = false;
+                  size_t key2 = g2->motherRef(0).key();
+                  m2 = &((*genParticles)[key2]);
             } else {
                   continue;
             }
+
             if (choose_x1) {
-                  g1 = g1->mother();
-                  _x1 *= sold/Mass12(g1,g2);
-                  //printf("x1_step %.3f\n", sold/Mass12(g1,g2));
+                  double mass12 = Mass12(m1,g2);
+                  _x1 *= sold/mass12;
+                  if (edm::MessageDrop::instance()->debugEnabled) {
+                        double q21 = Q2InBranch(genParticles, g1);
+                        LogTrace("") << "x1_step " << sold/mass12 << " Q21 " <<  q21 << " pt21 " << (1-sold/mass12)*q21 << " sold " << sold << " mass12 " << mass12;
+                        if (g2->numberOfMothers()>0) {
+                              double mass22 = Mass12(g1,m2);
+                              double q22 = Q2InBranch(genParticles, g2);
+                              LogTrace("") << "... NOT SELECTED: x2_step " << sold/mass22 << " Q22 " <<  q22 << " pt22 " << (1-sold/mass22)*q22 << " sold " << sold << " mass22 " << mass22;
+                        }
+                  }
+                  g1 = m1;
             } else {
-                  g2 = g2->mother();
-                  _x2 *= sold/Mass12(g1,g2);
-                  //printf("x2_step %.3f\n", sold/Mass12(g1,g2));
+                  double mass22 = Mass12(g1,m2);
+                  _x2 *= sold/mass22;
+                  if (edm::MessageDrop::instance()->debugEnabled) {
+                        double q22 = Q2InBranch(genParticles, g2);
+                        LogTrace("") << "x2_step " << sold/mass22 << " Q22 " <<  q22 << " pt22 " << (1-sold/mass22)*q22 << " sold " << sold << " mass22 " << mass22;
+                        if (g1->numberOfMothers()>0) {
+                              double mass12 = Mass12(m1,g2);
+                              double q21 = Q2InBranch(genParticles, g1);
+                              LogTrace("") << "... NOT SELECTED: x1_step " << sold/mass12 << " Q21 " <<  q21 << " pt21 " << (1-sold/mass12)*q21 << " sold " << sold << " mass12 " << mass12;
+                        }
+                  }
+                  g2 = m2;
             }
       }
 
@@ -185,7 +214,7 @@ X1X2PythiaProducer::endJob() {
 }
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-double X1X2PythiaProducer::Mass12(const reco::Candidate* ge1, const reco::Candidate* ge2){
+double X1X2PythiaProducer::Mass12(const reco::GenParticle* ge1, const reco::GenParticle* ge2){
       double en = ge1->energy() + ge2->energy();
       double px = ge1->px() + ge2->px();
       double py = ge1->py() + ge2->py();
@@ -194,76 +223,37 @@ double X1X2PythiaProducer::Mass12(const reco::Candidate* ge1, const reco::Candid
 }
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-double X1X2PythiaProducer::pt2Evol(const reco::Candidate* ge1, const reco::Candidate* ge2, int choice){
+double X1X2PythiaProducer::Q2InBranch(edm::Handle<reco::GenParticleCollection> genParticles, const reco::GenParticle* gp){
+      // Initial value set by PYTHIA
+      double q2 = gp->p4().M2();
 
-      const reco::Candidate* gea = ge1->mother();
-      const reco::Candidate* geb = ge1;
-      const reco::Candidate* ger = ge2;
-      if (choice!=1) {
-            gea = ge2->mother();
-            geb = ge2;
-            ger = ge1;
+      // Do nothing in some cases (just a protection)
+      if (gp->status()!=3 || gp->numberOfMothers()!=1) return q2;
+
+      size_t key = gp->motherRef(0).key();
+      const reco::GenParticle* moth = &((*genParticles)[key]);
+      //printf("Mother pdgId %d\n", moth->pdgId());
+      double px = moth->px();
+      double py = moth->py();
+      double pz = moth->pz();
+      double en = moth->energy();
+      //printf("Mother px %f py %f pz %f en %f\n", moth->px(), moth->py(), moth->pz(), moth->energy());
+      //printf("Number of daughters %d\n", moth->numberOfDaughters());
+      for (unsigned int j=0; j<moth->numberOfDaughters(); ++j) {
+            size_t key = moth->daughterRef(j).key();
+            const reco::GenParticle* daug = &((*genParticles)[key]);
+            if (daug->status()==3) continue; // this is the entry that must be modified...
+            px -= daug->px();
+            py -= daug->py();
+            pz -= daug->pz();
+            en -= daug->energy();
+            //printf("Daughter px %f py %f pz %f en %f\n", daug->px(), daug->py(), daug->pz(), daug->energy());
       }
+      //printf("Q2 BEFORE: %f\n", q2);
+      q2 = - en*en + px*px + py*py + pz*pz;
+      //printf("Q2 AFTER:  %f\n", q2);
 
-      reco::CompositeCandidate geBR;
-      geBR.addDaughter(*geb);
-      geBR.addDaughter(*ger);
-      AddFourMomenta addP4;
-      addP4.set(geBR);
-
-      reco::Candidate* cb = geb->clone();
-      math::XYZVector boost(geBR.boostToCM());
-      cb->setP4(ROOT::Math::VectorUtil::boost(cb->p4(),boost));
-      //boost.set(*cb);
-
-      double mBR = geBR.mass();
-      double px = cb->px()/cb->p()/2/mBR;
-      double py = cb->py()/cb->p()/2/mBR;
-      double pz = cb->pz()/cb->p()/2/mBR;
-      double en = -1./2/mBR;
-      math::XYZTLorentzVectorD p4aux(px,py,pz,en);
-      //reco::GenParticle caux(cb->charge(), p4aux, cb->pdgId(), cb->status(), cb->charge(), false);
-      //boost.Invert();
-      //caux.setP4(ROOT::Math::VectorUtil::boost(caux.p4(),boost);
-      cb->setP4(p4aux);
-      cb->setP4(ROOT::Math::VectorUtil::boost(cb->p4(),-boost));
-
-      delete cb;
-
-      double eA = gea->energy();
-      double pxA = gea->px();
-      double pyA = gea->py();
-      double pzA = gea->pz();
-
-      double eB = geb->energy();
-      double pxB = geb->px();
-      double pyB = geb->py();
-      double pzB = geb->pz();
-
-      double eAUX = cb->energy();
-      double pxAUX = cb->px();
-      double pyAUX = cb->py();
-      double pzAUX = cb->pz();
-
-      double papb = eA*eB - pxA*pxB - pyA*pyB - pzA*pzB;
-      double papAUX = eA*eAUX - pxA*pxAUX - pyA*pyAUX - pzA*pzAUX;
-      //printf("choice %d papb %f papAUX %f\n", choice, papb, papAUX);
-
-      double Q2 = -papb / (papAUX + 0.5);
-      double z = mBR/Mass12(gea,ger);
-      if (papAUX>=-0.50) {
-            // This should not happen
-            LogTrace("") << ">>> WARNING: unstable calculation; papAUX = " << papAUX << "; ad-hoc fix by setting papAUX=-0,501";
-            papAUX = -0.501;
-      }
-      double pt2 = (1-z)*Q2;
-      if (pt2<-0.1 && z>0.02) {
-            // pt2<0 can happen due to accuracy problem when:
-            //    * z is too small: z<0.01 or so
-            // Otherwise one should worry about, so dump it out...
-            LogTrace("") << ">>> WARNING: unstable calculation; pt2Evol = " << pt2 << ", z = " << z << ", papb = " << papb << "papAUX = " << papAUX;
-      }
-      return pt2;
+      return q2;
 }
 
 //define this as a plug-in
