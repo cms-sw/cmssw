1eb7641eb57f4a5983d573ef02a9219e726e496d
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-19 22:06:51 +0000

    Some stray text in here.

--- L1TriggerConfig/L1GtConfigProducers/python/l1GtTriggerMaskVetoTechTrig_cfi.py
+++ L1TriggerConfig/L1GtConfigProducers/python/l1GtTriggerMaskVetoTechTrig_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 # trigger mask: disable L1A result if the corresponding technical trigger
 # has bit value equal to 1 for the respective DAQ partition
-Configuration/Examples/data/RecoExample_GlobalTag.cfg
 l1GtTriggerMaskVetoTechTrig = cms.ESProducer("L1GtTriggerMaskVetoTechTrigTrivialProducer",
     TriggerMask = cms.vuint32(0, 0, 0, 0, 0, 
         0, 0, 0, 0, 0, 
