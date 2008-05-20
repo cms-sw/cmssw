968932cdc42031c9eb06fe1e1c666072006283c5
strang <sha1-d1826936ea83bdf7c4e239e43ef6698e2223180c@cern.ch>
2008-05-20 21:33:42 +0000

    remove old and unused cfg, cfi and cff

--- Validation/GlobalDigis/python/PoolSource_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# actual location is /pnfs/cms/WAX/11/
-source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:MinBias.root')
-)
-
-
