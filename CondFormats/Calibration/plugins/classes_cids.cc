29997e9e6cb8d216318954f5436b21b79ce79951
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-20 14:34:03 +0000

    register ids as plugins

---
+++ CondFormats/Calibration/plugins/classes_cids.cc
@@ -0,0 +1,15 @@
+#include "CondFormats/Common/interface/ClassIDCapability.h"
+#include "../src/classes.h"
+
+/*
+namespace {
+  cond::ClassID<Pedestals> aid;
+  cond::ClassID<BlobPedestals> bid;
+  cond::ClassID<BlobNoises> cid;
+}
+*/
+
+CLASS_ID(Pedestals, aid);
+CLASS_ID(BlobPedestals, bid);
+CLASS_ID(BlobNoises, cid);
+
