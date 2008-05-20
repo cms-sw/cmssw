56c5f4ebc0d1d354af03e56e7436161b18926dde
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-20 14:18:48 +0000

    compils

--- CondFormats/Common/interface/ClassIDCapability.h
+++ CondFormats/Common/interface/ClassIDCapability.h
@@ -24,6 +24,6 @@ extern "C" void NOT_SEAL_CAPABILITIES (const char**& names, int& n )
 #define CLASS_ID(type_, name_)  \
 typedef cond::ClassID<type_> name_; \
 namespace{ name_ EDM_PLUGIN_SYM(instance_cld, __LINE__); } \
-int bha##name_
-//DEFINE_EDM_PLUGIN(cond::ClassIdFactory, name_ , packageClassIDRegistry.csids.back())
+int bha##name_; \
+DEFINE_EDM_PLUGIN(cond::ClassIdFactory, name_ , packageClassIDRegistry.csids.back())
   
