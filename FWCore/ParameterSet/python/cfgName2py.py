8a6e4bf87f10258ad62cc753eda091b7ee3a253f
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-20 17:44:55 +0000

    CFGs in test areas have their python also in test area

--- FWCore/ParameterSet/python/cfgName2py.py
+++ FWCore/ParameterSet/python/cfgName2py.py
@@ -2,7 +2,9 @@
 #
 from sys import argv
 def cfgName2py(cfgName):
-    return  cfgName.replace("/data/","/python/").replace("/test/","/python/").replace(".","_") + ".py"
+    return  cfgName.replace("/data/","/python/").replace(".","_") + ".py"
 
 # for shell-scripters who want a value printed
-print cfgName2py(argv[1])
+if __name__=="__main__":
+   if len(argv) == 2:
+       print cfgName2py(argv[1])
