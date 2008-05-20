a7b9340a7230c5f86c09ac5cf0381dbb02d36e7c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-20 16:42:54 +0000

    change id

--- CondFormats/Common/interface/ClassIDRegistry.h
+++ CondFormats/Common/interface/ClassIDRegistry.h
@@ -16,34 +16,43 @@ namespace cond {
     std::vector<std::string> sids;
     std::vector<const char*> csids;
     ClassIDRegistry(std::string const & pfix);
-    struct Elem {
+    class Elem {
+    public:
+      inline Elem(const std::type_info& t) : tinfo(t) {}
+      inline Elem(const std::type_info& t, int);
+      inline const std::type_info& type() const { return tinfo;}
+    private:
       ClassIDRegistry * registry;
-      inline Elem(){}
-      inline Elem(const std::type_info& t);
       const char * registerMe(const std::type_info& t);
+
     };
     const char * registerMe(const std::type_info& t);
+
   private:
     std::string prefix;
+    const std::type_info& tinfo;
 
   };
 
   template<typename T>
   struct ClassID : public  ClassIDRegistry::Elem {
-    ClassID() {}
-    ClassID(int) : Elem(typeid(T)) {}
+    ClassID() : Elem(typeid(T) {}
+    ClassID(int i) : Elem(typeid(T),i) {}
   };
+
 
 }
 
 // magic: works only if a file local registry exists in the file
 #define ELEM_CONSTR(xx_)  \
-cond::ClassIDRegistry::Elem::Elem(const std::type_info& t){registry = &xx_;registerMe(t);} 
+  cond::ClassIDRegistry::Elem::Elem(const std::type_info& t,int) : tinfo(t) {registry = &xx_;registerMe(t);} 
 
 
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 namespace cond{
   typedef edmplugin::PluginFactory<ClassIDRegistry::Elem*() > ClassIdFactory;
+  typedef ClassIDRegistry::Elem ClassInfo;
+
 }
 
 #endif
