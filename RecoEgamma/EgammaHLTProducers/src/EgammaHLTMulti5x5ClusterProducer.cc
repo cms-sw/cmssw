b4af608a2b9cab33ccae2987275c3f7dd6139d46
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2008-05-21 12:24:38 +0000

    moved the EgammaHLTMulti5x5ClusterProducer from IdealGeometryRecord to CaloGeometryRecord following the changes by B. Heltsley

--- RecoEgamma/EgammaHLTProducers/src/EgammaHLTMulti5x5ClusterProducer.cc
+++ RecoEgamma/EgammaHLTProducers/src/EgammaHLTMulti5x5ClusterProducer.cc
@@ -19,7 +19,7 @@
 #include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
 
 // Geometry
-#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
 #include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
@@ -288,7 +288,7 @@ void EgammaHLTMulti5x5ClusterProducer::clusterizeECALPart(edm::Event &evt, const
 
   // get the geometry and topology from the event setup:
   edm::ESHandle<CaloGeometry> geoHandle;
-  es.get<IdealGeometryRecord>().get(geoHandle);
+  es.get<CaloGeometryRecord>().get(geoHandle);
 
   const CaloSubdetectorGeometry *geometry_p;
   CaloSubdetectorTopology *topology_p;
