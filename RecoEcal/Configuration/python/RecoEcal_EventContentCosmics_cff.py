c67be06d3efd335901db84fe7ecb7fe3e2327059
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-22 11:24:00 +0000

    update to python config files

--- RecoEcal/Configuration/python/RecoEcal_EventContentCosmics_cff.py
+++ RecoEcal/Configuration/python/RecoEcal_EventContentCosmics_cff.py
@@ -3,15 +3,14 @@ import FWCore.ParameterSet.Config as cms
 # Full Event content 
 RecoEcalFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_ecalRecHit_*_*', 
-        'keep *_reducedEcalRecHits*_*_*', 
-        'keep *_interestingEcalDetId*_*_*', 
         'keep *_ecalWeightUncalibRecHit_*_*', 
         'keep *_ecalPreshowerRecHit_*_*', 
         'keep *_islandBasicClusters_*_*', 
         'keep *_islandSuperClusters_*_*', 
         'keep *_hybridSuperClusters_*_*', 
-        'keep *_correctedMulti5x5*_*_*', 
-        'keep *_multi5x5*_*_*', 
+        'keep *_correctedFixedMatrix*_*_*', 
+        'keep *_cosmicBasicClusters_*_*', 
+        'keep *_fixedMatrix*_*_*', 
         'keep *_correctedIslandBarrelSuperClusters_*_*', 
         'keep *_correctedIslandEndcapSuperClusters_*_*', 
         'keep *_correctedHybridSuperClusters_*_*', 
@@ -21,35 +20,31 @@ RecoEcalFEVT = cms.PSet(
 # RECO content
 RecoEcalRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_islandBasicClusters_*_*', 
-        'keep *_multi5x5BasicClusters_*_*', 
+        'keep *_fixedMatrixBasicClusters_*_*', 
         'keep *_hybridSuperClusters_*_*', 
-        'keep EcalRecHitsSorted_reducedEcalRecHits*_*_*', 
+        'keep *_cosmicBasicClusters_*_*', 
         'drop recoSuperClusters_hybridSuperClusters_*_*', 
-        'drop recoClusterShapes_*_*_*', 
-        'drop recoBasicClustersToOnerecoClusterShapesAssociation_*_*_*', 
         'keep recoSuperClusters_islandSuperClusters_islandBarrelSuperClusters_*', 
         'keep recoSuperClusters_correctedHybridSuperClusters_*_*', 
-        'keep *_correctedMulti5x5SuperClustersWithPreshower_*_*', 
-        'keep recoPreshowerClusters_multi5x5SuperClustersWithPreshower_*_*', 
+        'keep *_correctedFixedMatrixSuperClustersWithPreshower_*_*', 
+        'keep recoPreshowerClusters_fixedMatrixSuperClustersWithPreshower_*_*', 
         'keep *_correctedEndcapSuperClustersWithPreshower_*_*', 
         'keep recoPreshowerClusterShapes_preshowerClusterShape_*_*', 
-        'keep recoPreshowerClusterShapes_multi5x5PreshowerClusterShape_*_*')
+        'keep recoPreshowerClusterShapes_fixedMatrixPreshowerClusterShape_*_*')
 )
 # AOD content
 RecoEcalAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_islandBasicClusters_*_*', 
-        'keep *_multi5x5BasicClusters_*_*', 
+        'keep *_fixedMatrixBasicClusters_*_*', 
         'keep *_hybridSuperClusters_*_*', 
-        'keep EcalRecHitsSorted_reducedEcalRecHits*_*_*', 
+        'keep *_cosmicBasicClusters_*_*', 
         'drop recoSuperClusters_hybridSuperClusters_*_*', 
-        'drop recoClusterShapes_*_*_*', 
-        'drop recoBasicClustersToOnerecoClusterShapesAssociation_*_*_*', 
         'keep recoSuperClusters_islandSuperClusters_islandBarrelSuperClusters_*', 
         'keep recoSuperClusters_correctedHybridSuperClusters_*_*', 
         'keep recoSuperClusters_correctedEndcapSuperClustersWithPreshower_*_*', 
-        'keep recoPreshowerClusters_multi5x5SuperClustersWithPreshower_*_*', 
-        'keep recoSuperClusters_correctedMulti5x5SuperClustersWithPreshower_*_*', 
+        'keep recoPreshowerClusters_fixedMatrixSuperClustersWithPreshower_*_*', 
+        'keep recoSuperClusters_correctedFixedMatrixSuperClustersWithPreshower_*_*', 
         'keep recoPreshowerClusterShapes_preshowerClusterShape_*_*', 
-        'keep recoPreshowerClusterShapes_multi5x5PreshowerClusterShape_*_*')
+        'keep recoPreshowerClusterShapes_fixedMatrixPreshowerClusterShape_*_*')
 )
 
--- RecoEcal/Configuration/python/RecoEcal_cff.py
+++ RecoEcal/Configuration/python/RecoEcal_cff.py
@@ -10,12 +10,14 @@ from RecoEcal.EgammaClusterProducers.hybridClusteringSequence_cff import *
 # clusters in preshower
 from RecoEcal.EgammaClusterProducers.preshowerClusteringSequence_cff import *
 # dynamic hybrid sequence
-from RecoEcal.EgammaClusterProducers.dynamicHybridClusteringSequence_cff import *
-# fixed matrix clusters
-from RecoEcal.EgammaClusterProducers.fixedMatrixClusteringSequence_cff import *
-# preshower sequence for fixed matrix clusters
-from RecoEcal.EgammaClusterProducers.fixedMatrixPreshowerClusteringSequence_cff import *
+#include "RecoEcal/EgammaClusterProducers/data/dynamicHybridClusteringSequence.cff"
+# multi5x5 clusters
+from RecoEcal.EgammaClusterProducers.multi5x5ClusteringSequence_cff import *
+# preshower sequence for multi5x5 clusters
+from RecoEcal.EgammaClusterProducers.multi5x5PreshowerClusteringSequence_cff import *
+#reduced recHit collection producer
+from RecoEcal.EgammaClusterProducers.reducedRecHitsSequence_cff import *
 # create path with all clustering algos
 # NB: preshower MUST be run after island clustering in the endcap
-ecalClusters = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence*dynamicHybridClusteringSequence*fixedMatrixClusteringSequence*fixedMatrixPreshowerClusteringSequence)
+ecalClusters = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence*reducedRecHitsSequence)
 
