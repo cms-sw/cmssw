6ff25fec36a99ac54ddc7aa8301e020d81de96e0
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-05-22 10:49:43 +0000

    - change float with double

--- DQM/EcalBarrelMonitorDbModule/src/MonitorElementsDb.cc
+++ DQM/EcalBarrelMonitorDbModule/src/MonitorElementsDb.cc
@@ -1,11 +1,11 @@
-// $Id: MonitorElementsDb.cc,v 1.17 2008/04/08 15:06:22 dellaric Exp $
+// $Id: MonitorElementsDb.cc,v 1.18 2008/04/08 18:32:10 dellaric Exp $
 
 /*!
   \file MonitorElementsDb.cc
   \brief Generate a Monitor Element from DB data
   \author B. Gobbo
-  \version $Revision: 1.17 $
-  \date $Date: 2008/04/08 15:06:22 $
+  \version $Revision: 1.18 $
+  \date $Date: 2008/04/08 18:32:10 $
 */
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
@@ -163,7 +163,7 @@ void MonitorElementsDb::analyze( const edm::Event& e, const edm::EventSetup& c, 
             vvars.clear();
             for( unsigned int l=0; l<vars.size(); l++ ) {
               if( !vars[l].empty() ) {
-        	vvars.push_back( row[vars[l].c_str()].data<float>() );
+        	vvars.push_back( row[vars[l].c_str()].data<double>() );
               }
             }
             if( vvars.size() == 2 ) {
