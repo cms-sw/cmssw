685a931213d356b9e453578074d1f7cca19165de
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-22 17:58:42 +0000

    replaced copy by clone

--- TrackingTools/TrajectoryFiltering/python/TrajectoryFilterESProducer_cff.py
+++ TrackingTools/TrajectoryFiltering/python/TrajectoryFilterESProducer_cff.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from TrackingTools.TrajectoryFiltering.TrajectoryFilter_cff import *
 from TrackingTools.TrajectoryFiltering.CompositeTrajectoryFilterESProducer_cfi import *
-import copy
-from TrackingTools.TrajectoryFiltering.TrajectoryFilterESProducer_cfi import *
-ckfBaseTrajectoryFilter = copy.deepcopy(trajectoryFilterESProducer)
+import TrackingTools.TrajectoryFiltering.TrajectoryFilterESProducer_cfi
+ckfBaseTrajectoryFilter = TrackingTools.TrajectoryFiltering.TrajectoryFilterESProducer_cfi.trajectoryFilterESProducer.clone()
 ckfBaseTrajectoryFilter.ComponentName = 'ckfBaseTrajectoryFilter' ## actually not needed
 
 
