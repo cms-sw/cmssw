88c5b819765cb7faa89fa958fa3e8a4f0ba5cf2a
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-23 09:22:52 +0000

    test work

--- CondCore/PopCon/test/inspect.py
+++ CondCore/PopCon/test/inspect.py
@@ -2,7 +2,6 @@ import DLFCN
 sys.setdlopenflags(DLFCN.RTLD_GLOBAL+DLFCN.RTLD_LAZY)
 
 from pluginCondDBPyInterface import *
-import pluginPedestalPyInterface as Pedestal
 a = FWIncantation()
 os.putenv("CORAL_AUTH_PATH","/afs/cern.ch/cms/DB/conddb")
 rdbms = RDBMS()
@@ -10,10 +9,11 @@ db = rdbms.getDB("sqlite_file:pop_test.db")
 tags = db.allTags()
 for tag in tags.split() :
     try :
+        exec('import '+db.moduleName(tag)+' as Plug')   
         iov = db.iov(tag)
         print tag, iov.size()
         for elem in iov.elements :
-            p = Pedestal.Object(elem)
+            p = Plug.Object(elem)
             print elem.since(), elem.till(),p.summary()
     except RuntimeError :
         print " no iov?"
