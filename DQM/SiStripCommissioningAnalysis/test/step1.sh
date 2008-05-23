577f988d16584d2d7b63bc545c1c0313403b89e7
Christophe Delaere <sha1-5ef908e2c391e2be8df6882d72b2c03008d08c74@cern.ch>
2008-05-23 12:34:37 +0000

    standardisation of the partition

--- DQM/SiStripCommissioningAnalysis/test/step1.sh
+++ DQM/SiStripCommissioningAnalysis/test/step1.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-#paramters: runnumber ishalow ishahigh ishastep vfslow vfshigh vfsstep
+#paramters: runnumber partition ishalow ishahigh ishastep vfslow vfshigh vfsstep
 
 eval `scramv1 runtime -sh`
 source /exports/slc4/development/FecSoftwareV3_0/config/oracle.env.bash afs
 export ENV_TRACKER_DAQ=/exports/slc4/development/opt/trackerDAQ
 export SCRATCH=`pwd`
+cp summaryScan.xml ../../..
+mkdir input
 
-for isha in `seq $2 $4 $3`; do
-for vfs  in `seq $5 $7 $6`; do
+for isha in `seq $3 $5 $4`; do
+for vfs  in `seq $6 $8 $7`; do
 
 #put the right files in /input
 rm -f input/*
-cp *${1}_*_ISHA${isha}_VFS${vfs}_*.root input/
+cp /tmp/*${1}_*_ISHA${isha}_VFS${vfs}_*.root input/
 
 #prepare input
 cp OfflineDbClient.template OfflineClientScan.cfg
 ex OfflineClientScan.cfg +":%s/RUNNUMBER/$1" +wq
+ex OfflineClientScan.cfg +":%s/PARTITION/$2" +wq
 
 #run
 cmsRun OfflineClientScan.cfg
