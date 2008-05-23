2646d00267f537e9ffa227887125e76bede1fc8b
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2008-05-23 13:46:25 +0000

    Putting back rct input scales into .cff

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
@@ -6,6 +6,12 @@ from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
 
 from L1TriggerConfig.RCTConfigProducers.L1RctInputScalesProducer_cfi import *
 
+l1RctMaskRcds = cms.ESSource("EmptyESSource",
+                             recordname = cms.string('L1RCTChannelMaskRcd'),
+                             iovIsRunNotTime = cms.bool(True),
+                             firstValid = cms.vuint32(1)
+                             )
+
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
                                   recordName = cms.string('L1RCTParametersRcd'),
                                   iovIsRunNotTime = cms.bool(True),
