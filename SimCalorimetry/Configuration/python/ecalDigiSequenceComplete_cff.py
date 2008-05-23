45af35a26f36a01caf33547d5ba8bc5c3fb57f65
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-05-23 08:07:50 +0000

    Remove geometry definitions, erroneously still present

--- SimCalorimetry/Configuration/python/ecalDigiSequenceComplete_cff.py
+++ SimCalorimetry/Configuration/python/ecalDigiSequenceComplete_cff.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-from Geometry.CaloEventSetup.CaloGeometry_cff import *
-from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
+# unsuppressed digis simulation - full ES digitization
 from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
-from Geometry.EcalMapping.EcalMapping_cfi import *
+# ECAL Trigger Primitives (needed by SRP)
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
+# Selective Readout Processor producer
 from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
+# Preshower Zero suppression producer
 from SimCalorimetry.EcalZeroSuppressionProducers.ecalPreshowerDigis_cfi import *
 ecalDigiSequenceComplete = cms.Sequence(simEcalUnsuppressedDigis*simEcalTriggerPrimitiveDigis*simEcalDigis*simEcalPreshowerDigis)
+
 
--- SimCalorimetry/Configuration/python/ecalDigiSequence_cff.py
+++ SimCalorimetry/Configuration/python/ecalDigiSequence_cff.py
@@ -1,13 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-from Geometry.CaloEventSetup.CaloGeometry_cff import *
-from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
+# unsuppressed digis simulation - fast preshower
 from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
-from Geometry.EcalMapping.EcalMapping_cfi import *
+# ECAL Trigger Primitives (needed by SRP)
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
+# Selective Readout Processor producer
 from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
+# Preshower Zero suppression producer
 from SimCalorimetry.EcalZeroSuppressionProducers.ecalPreshowerDigis_cfi import *
 ecalDigiSequence = cms.Sequence(simEcalUnsuppressedDigis*simEcalTriggerPrimitiveDigis*simEcalDigis*simEcalPreshowerDigis)
 simEcalUnsuppressedDigis.doFast = True
 simEcalPreshowerDigis.ESNoiseSigma = 2.98595
+
 
