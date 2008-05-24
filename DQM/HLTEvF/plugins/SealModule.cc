a7d8e6a83e96b69831db7cf374cda8fbd82e25a6
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2008-05-24 14:43:54 +0000

    add FourVectorHLT

--- DQM/HLTEvF/plugins/SealModule.cc
+++ DQM/HLTEvF/plugins/SealModule.cc
@@ -1,10 +1,11 @@
-// $Id: SealModule.cc,v 1.3 2008/02/06 13:11:33 lorenzo Exp $
+// $Id: SealModule.cc,v 1.4 2008/05/13 13:51:39 fwyzard Exp $
 
 #include "DQM/HLTEvF/interface/HltAnalyzer.h"
 #include "DQM/HLTEvF/interface/PathTimerService.h"
 #include "DQM/HLTEvF/interface/PathTimerInserter.h"
 #include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
 #include "DQM/HLTEvF/interface/HLTMonElectron.h"
+#include "DQM/HLTEvF/interface/FourVectorHLT.h"
 
 using edm::service::PathTimerService;
 
@@ -17,4 +18,5 @@ DEFINE_FWK_MODULE(PathTimerInserter);
 DEFINE_FWK_MODULE(HltAnalyzer);
 DEFINE_FWK_MODULE(HLTMuonDQMSource);
 DEFINE_FWK_MODULE(HLTMonElectron);
+DEFINE_FWK_MODULE(FourVectorHLT);
 
