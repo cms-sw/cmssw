fd58d6742788bb302da6e162d696ce7e05a65c89
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-05-24 00:30:20 +0000

    Added sources for L1RctInputScale.

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
@@ -1,19 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 
 from CalibCalorimetry.EcalTPGTools.ecalTPGScale_cff import *
-
-from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
-
 from L1TriggerConfig.RCTConfigProducers.L1RctInputScalesProducer_cfi import *
+from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
+l1RctEcalScaleRecord = cms.ESSource("EmptyESSource",
+    recordName = cms.string('L1RctEcalScaleRcd'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
 
-l1RctMaskRcds = cms.ESSource("EmptyESSource",
-                             recordname = cms.string('L1RCTChannelMaskRcd'),
-                             iovIsRunNotTime = cms.bool(True),
-                             firstValid = cms.vuint32(1)
-                             )
+l1RctHcalScaleRecord = cms.ESSource("EmptyESSource",
+    recordName = cms.string('L1RctHcalScaleRcd'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
 
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
-                                  recordName = cms.string('L1RCTParametersRcd'),
-                                  iovIsRunNotTime = cms.bool(True),
-                                  firstValid = cms.vuint32(1)
-                                  )
+    recordName = cms.string('L1RCTParametersRcd'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
+
+l1RctMaskRcds = cms.ESSource("EmptyESSource",
+    recordName = cms.string('L1RCTChannelMaskRcd'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
+
+
