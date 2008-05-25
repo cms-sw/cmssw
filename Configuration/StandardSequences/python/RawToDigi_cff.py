1c4966a8329735bd8aa24da327dc1c16fa5df72f
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-25 13:29:05 +0000

    removing dependency on HLT

--- Configuration/StandardSequences/python/RawToDigi_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_cff.py
@@ -11,7 +11,7 @@ from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
 from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
-from HLTrigger.Configuration.rawToDigi.EcalGeometrySetup_cff import *
+#from HLTrigger.Configuration.rawToDigi.EcalGeometrySetup_cff import *
 from Geometry.DTGeometry.dtGeometry_cfi import *
 from Geometry.RPCGeometry.rpcGeometry_cfi import *
 #-------------------------#
