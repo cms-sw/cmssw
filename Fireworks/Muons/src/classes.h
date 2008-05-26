feb474e080974a34af86502231bbd1493b01aa9b
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-05-26 14:48:52 +0000

    updated dictionaries

--- Fireworks/Muons/src/classes.h
+++ Fireworks/Muons/src/classes.h
@@ -1,3 +1,4 @@
 //Add includes for your classes here
 #include "Fireworks/Muons/interface/MuonsProxy3DBuilder.h"
 #include "Fireworks/Muons/interface/MuonsProxyRhoPhiZ2DBuilder.h"
+#include "Fireworks/Muons/interface/DTSegmentsProxyRhoPhiZ2DBuilder.h"
