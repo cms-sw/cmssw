b85af73b54da35cae646543618bbd799bc43c3b3
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2008-05-26 09:50:29 +0000

    Downgraded "sum of weights < DBL_MIN" from error to warning Return empty vector for all cases of serious errors

--- TrackingTools/GsfTracking/src/PosteriorWeightsCalculator.cc
+++ TrackingTools/GsfTracking/src/PosteriorWeightsCalculator.cc
@@ -46,20 +46,27 @@ std::vector<double> PosteriorWeightsCalculator::weights(const TransientTrackingR
     SMatDD V = asSMatrix<D>(recHit.parametersError());
     SMatDD R = V + me.measuredError<D>(recHit);
     double detR;
-    if (! R.Det2(detR) ) edm::LogError("PosteriorWeightsCalculator") << "PosteriorWeightsCalculator: determinant failed";
+    if (! R.Det2(detR) ) {
+      edm::LogError("PosteriorWeightsCalculator") << "PosteriorWeightsCalculator: determinant failed";
+      return std::vector<double>();
+    }
     detRs.push_back(detR);
 
     int ierr = ! R.Invert(); // if (ierr != 0) throw exception;
-    if ( ierr!=0 )  
+    if ( ierr!=0 ) {
       edm::LogError("PosteriorWeightsCalculator") 
 	<< "PosteriorWeightsCalculator: inversion failed, ierr = " << ierr;
+      return std::vector<double>();
+    }
     double chi2 = ROOT::Math::Similarity(r,R); 
     chi2s.push_back(chi2);
     if ( chi2<chi2Min )  chi2Min = chi2;
   }
   if ( detRs.size()!=predictedComponents.size() ||
-       chi2s.size()!=predictedComponents.size() )  
+       chi2s.size()!=predictedComponents.size() ) {
     edm::LogError("PosteriorWeightsCalculator") << "Problem in vector sizes";
+    return std::vector<double>();
+  }
   //
   // calculate weights (extracting a common factor
   //   exp(-0.5*chi2Min) to avoid numerical problems
@@ -86,12 +93,14 @@ std::vector<double> PosteriorWeightsCalculator::weights(const TransientTrackingR
     sumWeights += tempWeight;
   }
   if ( sumWeights<DBL_MIN ) {
-    edm::LogError("PosteriorWeightsCalculator") << "PosteriorWeightsCalculator: sumWeight < DBL_MIN";
+    edm::LogInfo("PosteriorWeightsCalculator") << "PosteriorWeightsCalculator: sumWeight < DBL_MIN";
     return std::vector<double>();
   }
 
-  if ( weights.size()!=predictedComponents.size() )  
+  if ( weights.size()!=predictedComponents.size() ) {
     edm::LogError("PosteriorWeightsCalculator") << "Problem in vector sizes (2)";
+    return std::vector<double>();
+  }
   for (std::vector<double>::iterator iter = weights.begin();
        iter != weights.end(); iter++) {
     (*iter) /= sumWeights;
