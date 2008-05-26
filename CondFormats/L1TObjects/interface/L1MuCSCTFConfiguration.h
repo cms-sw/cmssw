c895d2652b4cdedc9ab650d9587f856247ed8a31
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2008-05-26 11:00:17 +0000

    *** empty log message ***

--- CondFormats/L1TObjects/interface/L1MuCSCTFConfiguration.h
+++ CondFormats/L1TObjects/interface/L1MuCSCTFConfiguration.h
@@ -2,13 +2,7 @@
 #define L1TObjects_L1MuCSCTFConfiguration_h
 
 #include <string>
-<<<<<<< L1MuCSCTFConfiguration.h
 #include <FWCore/ParameterSet/interface/ParameterSet.h>
-=======
-#include <FWCore/ParameterSet/interface/ParameterSet.h>
-
-class CSCTFConfigProducer;
->>>>>>> 1.3
 
 class L1MuCSCTFConfiguration {
 private:
@@ -19,13 +13,8 @@ public:
 		return registers;
 	}
 
-<<<<<<< L1MuCSCTFConfiguration.h
 	edm::ParameterSet parameters(int sp) const ;
 
-=======
-	edm::ParameterSet parse(void) const ;
-
->>>>>>> 1.3
 	L1MuCSCTFConfiguration& operator=(const L1MuCSCTFConfiguration& conf){
 		for(int sp=0;sp<12;sp++) registers[sp] = conf.registers[sp];
 		return *this;
