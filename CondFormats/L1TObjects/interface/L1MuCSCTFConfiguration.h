31e8b6a597e53e73cbbc3064591b1ac02e1220f8
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2008-05-26 09:31:57 +0000

    Removed CSCTF Front LUTs, added alignment coefficients for new meta LUTs

--- CondFormats/L1TObjects/interface/L1MuCSCTFConfiguration.h
+++ CondFormats/L1TObjects/interface/L1MuCSCTFConfiguration.h
@@ -2,30 +2,39 @@
 #define L1TObjects_L1MuCSCTFConfiguration_h
 
 #include <string>
+<<<<<<< L1MuCSCTFConfiguration.h
+#include <FWCore/ParameterSet/interface/ParameterSet.h>
+=======
 #include <FWCore/ParameterSet/interface/ParameterSet.h>
 
 class CSCTFConfigProducer;
+>>>>>>> 1.3
 
 class L1MuCSCTFConfiguration {
 private:
-	std::string parametersAsText;
-	friend class CSCTFConfigProducer;
+	std::string registers[12];
 
 public:
-	const std::string& parameters(void) const throw() {
-		return parametersAsText;
+	const std::string* configAsText(void) const throw() {
+		return registers;
 	}
 
+<<<<<<< L1MuCSCTFConfiguration.h
+	edm::ParameterSet parameters(int sp) const ;
+
+=======
 	edm::ParameterSet parse(void) const ;
 
+>>>>>>> 1.3
 	L1MuCSCTFConfiguration& operator=(const L1MuCSCTFConfiguration& conf){
-		parametersAsText = conf.parametersAsText;
+		for(int sp=0;sp<12;sp++) registers[sp] = conf.registers[sp];
 		return *this;
 	}
 
 	L1MuCSCTFConfiguration(void){}
+	L1MuCSCTFConfiguration(std::string regs[12]){ for(int sp=0;sp<12;sp++) registers[sp]=regs[sp]; }
 	L1MuCSCTFConfiguration(const L1MuCSCTFConfiguration& conf){
-		parametersAsText = conf.parametersAsText;
+		for(int sp=0;sp<12;sp++) registers[sp] = conf.registers[sp];
 	}
 	~L1MuCSCTFConfiguration(void){}
 };
