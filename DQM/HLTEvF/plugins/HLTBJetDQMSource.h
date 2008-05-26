6e0bd52417d1e8709f7468e1962cdf84a171bd45
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-05-26 16:15:49 +0000

    first complete implementation

--- DQM/HLTEvF/plugins/HLTBJetDQMSource.h
+++ DQM/HLTEvF/plugins/HLTBJetDQMSource.h
@@ -5,13 +5,14 @@
  * *
  *  DQM source for BJet HLT paths
  *
- *  $Date: 2008/05/13 13:57:48 $
- *  $Revision: 1.2 $
+ *  $Date: 2008/05/13 16:38:49 $
+ *  $Revision: 1.3 $
  *  \author Andrea Bocci, Pisa
  *
  */
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 class DQMStore;
@@ -41,9 +42,15 @@ protected:
   void beginLuminosityBlock(const edm::LuminosityBlock & lumi, const edm::EventSetup & setup);
   void endLuminosityBlock(const edm::LuminosityBlock & lumi, const edm::EventSetup & setup);
 
-  void analyze(const edm::Event & event, const edm::EventSetup & setup) ;
+  void analyze(const edm::Event & event, const edm::EventSetup & setup);
+  void analyzeLifetime(const edm::Event & event, const edm::EventSetup & setup);
+  void analyzeSoftmuon(const edm::Event & event, const edm::EventSetup & setup);
+  void analyzePerformance(const edm::Event & event, const edm::EventSetup & setup);
 
 private:
+
+  MonitorElement * book(const char * name, const char * title , int x_bins, double x_min, double x_max, const char * x_axis = 0);
+  MonitorElement * book(const char * name, const char * title , int x_bins, double x_min, double x_max, int y_bins, double y_min, double y_max, const char * x_axis = 0, const char * y_axis = 0);
 
   edm::InputTag m_lifetimeL2Jets;
   edm::InputTag m_lifetimeL25TagInfo;
@@ -63,10 +70,8 @@ private:
 
   std::string m_monitorName;
   std::string m_outputFile;
-  bool m_storeDQM;
   bool m_storeROOT;
-
-  DQMStore * m_dbe;
+  edm::Service<DQMStore> m_dbe;
 
   MonitorElement * m_lifetimeL2JetsEnergy;
   MonitorElement * m_lifetimeL2JetsET;
@@ -117,12 +122,9 @@ private:
   MonitorElement * m_softmuonL25MuonHits;
   MonitorElement * m_softmuonL25MuonChi2;
   MonitorElement * m_softmuonL25MuonDeltaR;
-  MonitorElement * m_softmuonL25MuonIP2d;
   MonitorElement * m_softmuonL25MuonIP2dSig;
-  MonitorElement * m_softmuonL25MuonIP3d;
   MonitorElement * m_softmuonL25MuonIP3dSig;
   MonitorElement * m_softmuonL25MuonPtrel;
-  MonitorElement * m_softmuonL25MuonPtrelSig;
   MonitorElement * m_softmuonL25Discriminator;
   MonitorElement * m_softmuonL3JetsEnergy;
   MonitorElement * m_softmuonL3JetsET;
@@ -132,12 +134,9 @@ private:
   MonitorElement * m_softmuonL3MuonHits;
   MonitorElement * m_softmuonL3MuonChi2;
   MonitorElement * m_softmuonL3MuonDeltaR;
-  MonitorElement * m_softmuonL3MuonIP2d;
   MonitorElement * m_softmuonL3MuonIP2dSig;
-  MonitorElement * m_softmuonL3MuonIP3d;
   MonitorElement * m_softmuonL3MuonIP3dSig;
   MonitorElement * m_softmuonL3MuonPtrel;
-  MonitorElement * m_softmuonL3MuonPtrelSig;
   MonitorElement * m_softmuonL3Discriminator;
 
   MonitorElement * m_performanceL2JetsEnergy;
@@ -152,12 +151,9 @@ private:
   MonitorElement * m_performanceL25MuonHits;
   MonitorElement * m_performanceL25MuonChi2;
   MonitorElement * m_performanceL25MuonDeltaR;
-  MonitorElement * m_performanceL25MuonIP2d;
   MonitorElement * m_performanceL25MuonIP2dSig;
-  MonitorElement * m_performanceL25MuonIP3d;
   MonitorElement * m_performanceL25MuonIP3dSig;
   MonitorElement * m_performanceL25MuonPtrel;
-  MonitorElement * m_performanceL25MuonPtrelSig;
   MonitorElement * m_performanceL25Discriminator;
   MonitorElement * m_performanceL3JetsEnergy;
   MonitorElement * m_performanceL3JetsET;
@@ -167,12 +163,9 @@ private:
   MonitorElement * m_performanceL3MuonHits;
   MonitorElement * m_performanceL3MuonChi2;
   MonitorElement * m_performanceL3MuonDeltaR;
-  MonitorElement * m_performanceL3MuonIP2d;
   MonitorElement * m_performanceL3MuonIP2dSig;
-  MonitorElement * m_performanceL3MuonIP3d;
   MonitorElement * m_performanceL3MuonIP3dSig;
   MonitorElement * m_performanceL3MuonPtrel;
-  MonitorElement * m_performanceL3MuonPtrelSig;
   MonitorElement * m_performanceL3Discriminator;
 
   unsigned int m_counterEvent;
