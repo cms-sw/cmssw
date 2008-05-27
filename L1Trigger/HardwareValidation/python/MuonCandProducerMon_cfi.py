ae25294455fc3978e3345b13a279e52743f7a50f
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-05-27 15:53:53 +0000

    revert head to 20X compatibility

--- L1Trigger/HardwareValidation/python/MuonCandProducerMon_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-muonCandMon = cms.EDFilter("MuonCandProducerMon",
-    DTinput = cms.untracked.InputTag("dttfdigis"),
-    VerboseFlag = cms.untracked.int32(0),
-    CSCinput = cms.untracked.InputTag("csctfdigis")
-)
-
-
