621bc4a84b20a427bb36d0ae1fc009200f352995
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2008-05-27 17:35:49 +0000

    adding L1 to L3 filter

--- HLTrigger/Muon/src/SealModule.cc
+++ HLTrigger/Muon/src/SealModule.cc
@@ -5,6 +5,7 @@
 #include "HLTrigger/Muon/interface/HLTMuonL2PreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL3PreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonL3TkPreFilter.h"
+#include "HLTrigger/Muon/interface/HLTMuonL1toL3TkPreFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonIsoFilter.h"
 #include "HLTrigger/Muon/interface/HLTMuonDimuonL2Filter.h"
 #include "HLTrigger/Muon/interface/HLTMuonDimuonL3Filter.h"
@@ -14,6 +15,7 @@ DEFINE_ANOTHER_FWK_MODULE(HLTMuonL1Filter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonL2PreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonL3PreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonL3TkPreFilter);
+DEFINE_ANOTHER_FWK_MODULE(HLTMuonL1toL3TkPreFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonIsoFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonL2Filter);
 DEFINE_ANOTHER_FWK_MODULE(HLTMuonDimuonL3Filter);
