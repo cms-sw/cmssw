7126b2a658832295d2010e02f0a913a1ad23f4c0
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-05-27 21:20:52 +0000

    syncing with cfg

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
@@ -4,8 +4,8 @@ from CondCore.DBCommon.CondDBSetup_cfi import *
 
 GlobalTag = cms.ESSource("PoolDBESSource",
     CondDBSetup,
-    connect = cms.string('frontier://FrontierDev/CMS_COND_GLOBALTAG'), ##FrontierProd/CMS_COND_21X_GLOBALTAG"
+    connect = cms.string('frontier://FrontierInt/CMS_COND_21X_GLOBALTAG'), ##FrontierProd/CMS_COND_21X_GLOBALTAG"
 
-    globaltag = cms.untracked.string('IDEAL::All'),
+    globaltag = cms.untracked.string('IDEAL_V1::All'),
     BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService')
 )
