f30a0ba2534d6624dea65663af01b7f9789ae9b8
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-05-28 08:26:31 +0000

    - cosmetics

--- DQM/EcalCommon/interface/EcalMonitorPrescaler.h
+++ DQM/EcalCommon/interface/EcalMonitorPrescaler.h
@@ -1,11 +1,11 @@
-// $Id: EcalMonitorPrescaler.h,v 1.1 2007/04/27 16:53:27 dellaric Exp $
+// $Id: EcalMonitorPrescaler.h,v 1.2 2008/05/17 14:23:13 dellaric Exp $
 
 /*!
   \file EcalMonitorPrescaler.h
   \brief Ecal specific Prescaler 
   \author G. Della Ricca
-  \version $Revision: 1.1 $
-  \date $Date: 2007/04/27 16:53:27 $
+  \version $Revision: 1.2 $
+  \date $Date: 2008/05/17 14:23:13 $
 */
 
 #ifndef EcalMonitorPrescaler_H
@@ -24,7 +24,7 @@ explicit EcalMonitorPrescaler(edm::ParameterSet const&);
 virtual ~EcalMonitorPrescaler();
 
 virtual bool filter(edm::Event& e, edm::EventSetup const& c);
-void endJob();
+void endJob(void);
 
 private:
 
