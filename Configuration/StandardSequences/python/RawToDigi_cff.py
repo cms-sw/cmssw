a9221c846e63da5e49c2657562c1a0f3e3be1c3f
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-29 06:34:01 +0000

    Change from deepcopy() to clone() syntax

--- Configuration/StandardSequences/python/RawToDigi_cff.py
+++ Configuration/StandardSequences/python/RawToDigi_cff.py
@@ -1,12 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# David Lange, Bryan Dahmes LLNL
-# 30 April, 2007
-#
-#--- Allow for multiple calls to the database ---#
 from CondCore.DBCommon.CondDBSetup_cfi import *
-#--- Geometry Setup ---#
 from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
 from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
@@ -14,61 +8,35 @@ from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 #from HLTrigger.Configuration.rawToDigi.EcalGeometrySetup_cff import *
 from Geometry.DTGeometry.dtGeometry_cfi import *
 from Geometry.RPCGeometry.rpcGeometry_cfi import *
-#-------------------------#
-#--- RawToDigi modules ---#
-#-------------------------#
-#--- L1 Setup ---#
 from L1TriggerConfig.L1ScalesProducers.L1MuTriggerScalesConfig_cff import *
 from L1TriggerConfig.L1ScalesProducers.L1MuTriggerPtScaleConfig_cff import *
 from L1TriggerConfig.L1ScalesProducers.L1MuGMTScalesConfig_cff import *
 from L1TriggerConfig.GctConfigProducers.L1GctConfig_cff import *
 from L1TriggerConfig.L1GtConfigProducers.L1GtConfig_cff import *
 from L1TriggerConfig.GMTConfigProducers.L1MuGMTParametersConfig_cff import *
-import copy
-from EventFilter.CSCTFRawToDigi.csctfunpacker_cfi import *
-#--- CSC TF ---#
-csctfDigis = copy.deepcopy(csctfunpacker)
-import copy
-from EventFilter.DTTFRawToDigi.dttfunpacker_cfi import *
-#--- DT TF ---#
-dttfDigis = copy.deepcopy(dttfunpacker)
-import copy
-from EventFilter.GctRawToDigi.l1GctHwDigis_cfi import *
-#--- GCT ---#
-gctDigis = copy.deepcopy(l1GctHwDigis)
-import copy
-from EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi import *
-#--- GT ---#
-gtDigis = copy.deepcopy(l1GtUnpack)
-#--- SiPixel ---#
+import EventFilter.CSCTFRawToDigi.csctfunpacker_cfi
+csctfDigis = EventFilter.CSCTFRawToDigi.csctfunpacker_cfi.csctfunpacker.clone()
+import EventFilter.DTTFRawToDigi.dttfunpacker_cfi
+dttfDigis = EventFilter.DTTFRawToDigi.dttfunpacker_cfi.dttfunpacker.clone()
+import EventFilter.GctRawToDigi.l1GctHwDigis_cfi
+gctDigis = EventFilter.GctRawToDigi.l1GctHwDigis_cfi.l1GctHwDigis.clone()
+import EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi
+gtDigis = EventFilter.L1GlobalTriggerRawToDigi.l1GtUnpack_cfi.l1GtUnpack.clone()
 from EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi import *
-#--- SiStrip ---#
 from EventFilter.SiStripRawToDigi.SiStripRawToDigis_standard_cff import *
-#--- Ecal ---#
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
-import copy
-from EventFilter.EcalRawToDigiDev.EcalUnpackerData_cfi import *
-ecalDigis = copy.deepcopy(ecalEBunpacker)
-import copy
-from EventFilter.ESRawToDigi.esRawToDigi_cfi import *
-#--- Ecal Preshower ---#
-ecalPreshowerDigis = copy.deepcopy(esRawToDigi)
-import copy
-from EventFilter.HcalRawToDigi.HcalRawToDigi_cfi import *
-#--- Hcal ---#
-hcalDigis = copy.deepcopy(hcalDigis)
-import copy
-from EventFilter.CSCRawToDigi.cscUnpacker_cfi import *
-#--- CSC ---#
-muonCSCDigis = copy.deepcopy(muonCSCDigis)
-import copy
-from EventFilter.DTRawToDigi.dtunpacker_cfi import *
-#--- DT ---#
-muonDTDigis = copy.deepcopy(muonDTDigis)
-import copy
-from EventFilter.RPCRawToDigi.rpcUnpacker_cfi import *
-#--- RPC ---#
-muonRPCDigis = copy.deepcopy(rpcunpacker)
+import EventFilter.EcalRawToDigiDev.EcalUnpackerData_cfi
+ecalDigis = EventFilter.EcalRawToDigiDev.EcalUnpackerData_cfi.ecalEBunpacker.clone()
+import EventFilter.ESRawToDigi.esRawToDigi_cfi
+ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
+import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
+hcalDigis = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
+import EventFilter.CSCRawToDigi.cscUnpacker_cfi
+muonCSCDigis = EventFilter.CSCRawToDigi.cscUnpacker_cfi.muonCSCDigis.clone()
+import EventFilter.DTRawToDigi.dtunpacker_cfi
+muonDTDigis = EventFilter.DTRawToDigi.dtunpacker_cfi.muonDTDigis.clone()
+import EventFilter.RPCRawToDigi.rpcUnpacker_cfi
+muonRPCDigis = EventFilter.RPCRawToDigi.rpcUnpacker_cfi.rpcunpacker.clone()
 RawToDigi = cms.Sequence(csctfDigis+dttfDigis+gctDigis+gtDigis+siPixelDigis+SiStripRawToDigis+ecalDigis+ecalPreshowerDigis+hcalDigis+muonCSCDigis+muonDTDigis+muonRPCDigis)
 csctfDigis.producer = 'rawDataCollector'
 dttfDigis.DTTF_FED_Source = 'rawDataCollector'
