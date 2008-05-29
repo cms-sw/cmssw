157f4ef6e85bbdafa4d894ab7540ad0268ab8a94
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-05-29 13:40:21 +0000

    fix the name of simulated digi collection

--- Validation/HcalDigis/python/HcalDigiParam_cfi.py
+++ Validation/HcalDigis/python/HcalDigiParam_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hcalDigiAnalyzer = cms.EDFilter("HcalDigiTester",
-    digiLabel = cms.InputTag("hcalUnsuppressedDigis"),
+    digiLabel = cms.InputTag("simHcalUnsuppressedDigis"),
     zside = cms.untracked.string('*'),
     outputFile = cms.untracked.string('HcalDigisValidationHB.root'),
     hcalselector = cms.untracked.string('HB')
