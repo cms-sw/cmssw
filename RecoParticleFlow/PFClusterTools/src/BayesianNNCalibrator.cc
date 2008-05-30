1e52ec254b8deb45fd100b75668e4389e47759cb
ballin <sha1-4e4bbef69ed66b5ba60dd13d54b406ba31eb806d@cern.ch>
2008-05-30 16:25:31 +0000

    First commit

---
+++ RecoParticleFlow/PFClusterTools/src/BayesianNNCalibrator.cc
@@ -0,0 +1,11 @@
+#include "RecoParticleFlow/PFClusterTools/interface/BayesianNNCalibrator.h"
+
+using namespace pftools;
+
+BayesianNNCalibrator::BayesianNNCalibrator()
+{
+}
+
+BayesianNNCalibrator::~BayesianNNCalibrator()
+{
+}
