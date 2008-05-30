41f0326aacb283d1638d2255710c8f1f0e8a48c0
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-05-30 16:30:43 +0000

    change some deepcopies to clones

--- RecoEgamma/EgammaPhotonProducers/python/chi2EstimatorForInOutFit_cfi.py
+++ RecoEgamma/EgammaPhotonProducers/python/chi2EstimatorForInOutFit_cfi.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-import copy
-from TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi import *
-Chi2MeasurementEstimatorForInOut = copy.deepcopy(Chi2MeasurementEstimator)
+import TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi
+Chi2MeasurementEstimatorForInOut = TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi.Chi2MeasurementEstimator.clone()
 Chi2MeasurementEstimatorForInOut.ComponentName = 'Chi2ForInOut'
 Chi2MeasurementEstimatorForInOut.MaxChi2 = 100.
 Chi2MeasurementEstimatorForInOut.nSigma = 3.
