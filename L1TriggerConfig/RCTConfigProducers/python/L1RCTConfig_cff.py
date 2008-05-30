159d52e0c5ae894c338d497202923cc67f1f22e0
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-05-30 19:17:19 +0000

    Removed L1RctInputScale and its records.

--- L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
+++ L1TriggerConfig/RCTConfigProducers/python/L1RCTConfig_cff.py
@@ -1,20 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from CalibCalorimetry.EcalTPGTools.ecalTPGScale_cff import *
-from L1TriggerConfig.RCTConfigProducers.L1RctInputScalesProducer_cfi import *
 from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
-l1RctEcalScaleRecord = cms.ESSource("EmptyESSource",
-    recordName = cms.string('L1RctEcalScaleRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
-l1RctHcalScaleRecord = cms.ESSource("EmptyESSource",
-    recordName = cms.string('L1RctHcalScaleRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
     recordName = cms.string('L1RCTParametersRcd'),
     iovIsRunNotTime = cms.bool(True),
