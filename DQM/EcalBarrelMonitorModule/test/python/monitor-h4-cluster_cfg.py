ff161f6aaf62aacc5dc46a36c9693de55b49385b
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-06-03 09:47:02 +0000

    - add new py files

---
+++ DQM/EcalBarrelMonitorModule/test/python/monitor-h4-cluster_cfg.py
@@ -0,0 +1,226 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ECALDQM")
+
+process.load("EventFilter.EcalTBRawToDigi.EcalUnpackerData_cfi")
+
+import RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi
+process.ecalUncalibHit = RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi.ecalFixedAlphaBetaFitUncalibRecHit.clone()
+
+#import RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi
+#process.ecalUncalibHit = RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi.ecalWeightUncalibRecHit.clone()
+
+process.load("RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi")
+
+process.load("DQM.EcalBarrelMonitorModule.EcalBarrelMonitorModule_cfi")
+
+process.load("DQM.EcalBarrelMonitorTasks.EcalBarrelMonitorTasks_cfi")
+
+process.load("DQM.EcalBarrelMonitorClient.EcalBarrelMonitorClient_cfi")
+
+process.load("RecoEcal.EgammaClusterProducers.ecalClusteringSequence_cff")
+
+process.load("CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi")
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.preScaler = cms.EDFilter("Prescaler",
+    prescaleFactor = cms.int32(1)
+)
+
+process.dqmInfoEB = cms.EDFilter("DQMEventInfo",
+    subSystemFolder = cms.untracked.string('EcalBarrel')
+)
+
+process.dqmQTestEB = cms.EDFilter("QualityTester",
+    reportThreshold = cms.untracked.string('red'),
+    prescaleFactor = cms.untracked.int32(1),
+    qtList = cms.untracked.FileInPath('DQM/EcalBarrelMonitorModule/test/data/EcalBarrelQualityTests.xml'),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
+
+process.dqmSaverEB = cms.EDFilter("DQMFileSaver",
+# special fix for H4 cosmic runs
+    saveByRun = cms.untracked.int32(2),
+    dirName = cms.untracked.string('.'),
+    fileName = cms.untracked.string('EcalBarrel'),
+    convention = cms.untracked.string('Online')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(150)
+)
+process.source = cms.Source("DAQEcalTBInputService",
+#---
+    fileNames = cms.untracked.vstring('/store/users/dellaric/data/H4-000017188-SM5-COSMIC-STD'),
+    isBinary = cms.untracked.bool(True)
+#---
+)
+
+process.EcalTrivialConditionRetriever = cms.ESSource("EcalTrivialConditionRetriever",
+    adcToGeVEBConstant = cms.untracked.double(0.00875), ## 0.035
+    pedWeights = cms.untracked.vdouble(0.333, 0.333, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+    amplWeights = cms.untracked.vdouble(-0.333, -0.333, -0.333, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0)
+    jittWeights = cms.untracked.vdouble(0.041, 0.041, 0.041, 0.0, 1.325, -0.05, -0.504, -0.502, -0.390, 0.0)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        EcalTBRawToDigiDccBlockSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiNumTowerBlocks = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        threshold = cms.untracked.string('WARNING'),
+        noLineBreaks = cms.untracked.bool(True),
+        EcalRawToDigiDevSRP = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTpg = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDev = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemGain = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiGainSwitch = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemBlock = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevDccBlockSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTriggerType = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTpg = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevNumTowerBlocks = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalBarrelMonitorModule = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTowerSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevGainSwitch = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBInputService = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        EcalTBRawToDigiGainZero = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTowerSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        noTimeStamps = cms.untracked.bool(True),
+        EcalRawToDigiDevChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevGainZero = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTriggerType = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        EcalRawToDigiDevTCC = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalDCCHeaderRuntypeDecoder = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigi = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        )
+    ),
+    categories = cms.untracked.vstring('EcalTBInputService', 
+        'EcalTBRawToDigi', 
+        'EcalTBRawToDigiTriggerType', 
+        'EcalTBRawToDigiTpg', 
+        'EcalTBRawToDigiNumTowerBlocks', 
+        'EcalTBRawToDigiTowerId', 
+        'EcalTBRawToDigiTowerSize', 
+        'EcalTBRawToDigiChId', 
+        'EcalTBRawToDigiGainZero', 
+        'EcalTBRawToDigiGainSwitch', 
+        'EcalTBRawToDigiDccBlockSize', 
+        'EcalRawToDigiDev', 
+        'EcalRawToDigiDevTriggerType', 
+        'EcalRawToDigiDevTpg', 
+        'EcalRawToDigiDevNumTowerBlocks', 
+        'EcalRawToDigiDevTowerId', 
+        'EcalRawToDigiDevTowerSize', 
+        'EcalRawToDigiDevChId', 
+        'EcalRawToDigiDevGainZero', 
+        'EcalRawToDigiDevGainSwitch', 
+        'EcalRawToDigiDevDccBlockSize', 
+        'EcalRawToDigiDevMemBlock', 
+        'EcalRawToDigiDevMemTowerId', 
+        'EcalRawToDigiDevMemChId', 
+        'EcalRawToDigiDevMemGain', 
+        'EcalRawToDigiDevTCC', 
+        'EcalRawToDigiDevSRP', 
+        'EcalDCCHeaderRuntypeDecoder', 
+        'EcalBarrelMonitorModule'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.ecalBarrelDataSequence = cms.Sequence(process.preScaler*process.ecalEBunpacker*process.ecalUncalibHit*process.ecalRecHit*process.islandBasicClusters*process.islandSuperClusters*process.hybridSuperClusters)
+process.ecalBarrelMonitorSequence = cms.Sequence(process.ecalBarrelMonitorModule*process.dqmInfoEB*process.ecalBarrelMonitorClient*process.dqmQTestEB*process.dqmSaverEB)
+
+process.p = cms.Path(process.ecalBarrelDataSequence*process.ecalBarrelMonitorSequence)
+process.q = cms.EndPath(process.ecalBarrelCosmicTasksSequence*process.ecalBarrelClusterTask)
+
+process.ecalUncalibHit.MinAmplBarrel = 12.
+process.ecalUncalibHit.MinAmplEndcap = 16.
+
+process.ecalUncalibHit.EBdigiCollection = cms.InputTag("ecalEBunpacker","ebDigis")
+process.ecalUncalibHit.EEdigiCollection = cms.InputTag("ecalEBunpacker","eeDigis")
+
+process.ecalRecHit.EBuncalibRecHitCollection = cms.InputTag("ecalUncalibHit","EcalUncalibRecHitsEB")
+process.ecalRecHit.EEuncalibRecHitCollection = cms.InputTag("ecalUncalibHit","EcalUncalibRecHitsEE")
+
+process.ecalBarrelMonitorClient.maskFile = ''
+process.ecalBarrelMonitorClient.location = 'H4'
+process.ecalBarrelMonitorClient.baseHtmlDir = '.'
+process.ecalBarrelMonitorClient.enabledClients = ['Integrity', 'Occupancy', 'StatusFlags', 'PedestalOnline', 'Cosmic', 'Timing', 'Cluster', 'Summary']
+process.ecalBarrelMonitorClient.superModules = [19]
+
+process.islandBasicClusters.IslandBarrelSeedThr = 0.150
+process.islandBasicClusters.IslandEndcapSeedThr = 0.150
+
+process.hybridSuperClusters.HybridBarrelSeedThr = 0.150
+process.hybridSuperClusters.step = 1
+process.hybridSuperClusters.eseed = 0.150
+
+process.islandSuperClusters.seedTransverseEnergyThreshold = 0.150
+
+process.DQM.collectorHost = ''
+
---
+++ DQM/EcalBarrelMonitorModule/test/python/monitor-h4b-cluster_cfg.py
@@ -0,0 +1,206 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ECALDQM")
+
+process.load("EventFilter.EcalTBRawToDigi.EcalUnpackerData_cfi")
+
+process.load("RecoTBCalo.EcalTBHodoscopeReconstructor.ecal2006TBHodoscopeReconstructor_cfi")
+
+process.load("RecoTBCalo.EcalTBTDCReconstructor.ecal2006TBTDCReconstructor_cfi")
+
+import RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi
+process.ecalUncalibHit = RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi.ecalFixedAlphaBetaFitUncalibRecHit.clone()
+
+#import RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi
+#process.ecalUncalibHit = RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi.ecalWeightUncalibRecHit.clone()
+
+process.load("RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi")
+
+process.load("DQM.EcalBarrelMonitorModule.EcalBarrelMonitorModule_cfi")
+
+process.load("DQM.EcalBarrelMonitorTasks.EcalBarrelMonitorTasks_cfi")
+
+process.load("DQM.EcalBarrelMonitorClient.EcalBarrelMonitorClient_cfi")
+
+process.load("RecoEcal.EgammaClusterProducers.ecalClusteringSequence_cff")
+
+process.load("CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi")
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.preScaler = cms.EDFilter("Prescaler",
+    prescaleFactor = cms.int32(1)
+)
+
+process.dqmInfoEB = cms.EDFilter("DQMEventInfo",
+    subSystemFolder = cms.untracked.string('EcalBarrel')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(150)
+)
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/users/dellaric/data/h4b.00014790.A.0.0.root')
+)
+
+process.src1 = cms.ESSource("EcalTrivialConditionRetriever",
+    pedWeights = cms.untracked.vdouble(0.333, 0.333, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+    amplWeights = cms.untracked.vdouble(-0.333, -0.333, -0.333, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0)
+    jittWeights = cms.untracked.vdouble(0.041, 0.041, 0.041, 0.0, 1.325, -0.05, -0.504, -0.502, -0.390, 0.0)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        EcalTBRawToDigiDccBlockSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiNumTowerBlocks = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        threshold = cms.untracked.string('WARNING'),
+        noLineBreaks = cms.untracked.bool(True),
+        EcalRawToDigiDevSRP = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTpg = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDev = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemGain = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiGainSwitch = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemBlock = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevDccBlockSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTriggerType = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTpg = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevNumTowerBlocks = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalBarrelMonitorModule = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTowerSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevGainSwitch = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBInputService = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        EcalTBRawToDigiGainZero = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevTowerSize = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        noTimeStamps = cms.untracked.bool(True),
+        EcalRawToDigiDevChId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevGainZero = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigiTriggerType = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        EcalRawToDigiDevTCC = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalDCCHeaderRuntypeDecoder = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalRawToDigiDevMemTowerId = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        ),
+        EcalTBRawToDigi = cms.untracked.PSet(
+            limit = cms.untracked.int32(1000)
+        )
+    ),
+    categories = cms.untracked.vstring('EcalTBInputService', 
+        'EcalTBRawToDigi', 
+        'EcalTBRawToDigiTriggerType', 
+        'EcalTBRawToDigiTpg', 
+        'EcalTBRawToDigiNumTowerBlocks', 
+        'EcalTBRawToDigiTowerId', 
+        'EcalTBRawToDigiTowerSize', 
+        'EcalTBRawToDigiChId', 
+        'EcalTBRawToDigiGainZero', 
+        'EcalTBRawToDigiGainSwitch', 
+        'EcalTBRawToDigiDccBlockSize', 
+        'EcalRawToDigiDev', 
+        'EcalRawToDigiDevTriggerType', 
+        'EcalRawToDigiDevTpg', 
+        'EcalRawToDigiDevNumTowerBlocks', 
+        'EcalRawToDigiDevTowerId', 
+        'EcalRawToDigiDevTowerSize', 
+        'EcalRawToDigiDevChId', 
+        'EcalRawToDigiDevGainZero', 
+        'EcalRawToDigiDevGainSwitch', 
+        'EcalRawToDigiDevDccBlockSize', 
+        'EcalRawToDigiDevMemBlock', 
+        'EcalRawToDigiDevMemTowerId', 
+        'EcalRawToDigiDevMemChId', 
+        'EcalRawToDigiDevMemGain', 
+        'EcalRawToDigiDevTCC', 
+        'EcalRawToDigiDevSRP', 
+        'EcalDCCHeaderRuntypeDecoder', 
+        'EcalBarrelMonitorModule'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.ecalBarrelDataSequence = cms.Sequence(process.preScaler*process.ecalEBunpacker*process.ecal2006TBHodoscopeReconstructor*process.ecal2006TBTDCReconstructor*process.ecalUncalibHit*process.ecalRecHit*process.islandBasicClusters*process.islandSuperClusters*process.hybridSuperClusters)
+process.ecalBarrelMonitorSequence = cms.Sequence(process.ecalBarrelMonitorModule*process.dqmInfoEB*process.ecalBarrelMonitorClient)
+
+process.p = cms.Path(process.ecalBarrelDataSequence*process.ecalBarrelMonitorSequence)
+process.q = cms.EndPath(process.ecalBarrelTestBeamTasksSequence*process.ecalBarrelClusterTask)
+
+process.ecal2006TBHodoscopeReconstructor.rawInfoProducer = 'ecalEBunpacker'
+process.ecal2006TBTDCReconstructor.rawInfoProducer = 'ecalEBunpacker'
+process.ecal2006TBTDCReconstructor.eventHeaderProducer = 'ecalEBunpacker'
+
+process.ecalUncalibHit.MinAmplBarrel = 12.
+process.ecalUncalibHit.MinAmplEndcap = 16.
+
+process.ecalUncalibHit.EBdigiCollection = cms.InputTag("ecalEBunpacker","ebDigis")
+process.ecalUncalibHit.EEdigiCollection = cms.InputTag("ecalEBunpacker","eeDigis")
+
+process.ecalRecHit.EBuncalibRecHitCollection = cms.InputTag("ecalUncalibHit","EcalUncalibRecHitsEB")
+process.ecalRecHit.EEuncalibRecHitCollection = cms.InputTag("ecalUncalibHit","EcalUncalibRecHitsEE")
+
+process.ecalBarrelMonitorClient.maskFile = ''
+process.ecalBarrelMonitorClient.location = 'H4B'
+process.ecalBarrelMonitorClient.baseHtmlDir = '.'
+process.ecalBarrelMonitorClient.enabledClients = ['Integrity', 'Occupancy', 'StatusFlags', 'PedestalOnline', 'Pedestal', 'TestPulse', 'Laser', 'Timing', 'BeamCalo', 'BeamHodo', 'Cluster', 'Summary']
+process.ecalBarrelMonitorClient.superModules = [19]
+
+process.DQM.collectorHost = ''
+
