621fbeb5e92867e8875ff3fe8cc5f8439ba7512a
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-06-03 08:45:16 +0000

    - fix error in py

--- DQM/EcalBarrelMonitorTasks/python/mergeRuns_cff.py
+++ DQM/EcalBarrelMonitorTasks/python/mergeRuns_cff.py
@@ -9,7 +9,7 @@ from DQM.EcalBarrelMonitorTasks.EBLaserTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBOccupancyTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBPedestalOnlineTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBPedestalTask_cfi import *
-from DQM.EcalBarrelMonitorTasks.EBStatusFlagsTask import *
+from DQM.EcalBarrelMonitorTasks.EBStatusFlagsTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBTestPulseTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBTimingTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBTriggerTowerTask_cfi import *
