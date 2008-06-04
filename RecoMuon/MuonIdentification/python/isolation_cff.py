c65fbabebfe342910fc8723f271578cbae847b80
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-04 21:42:37 +0000

    Update from config

--- RecoMuon/MuonIdentification/python/isolation_cff.py
+++ RecoMuon/MuonIdentification/python/isolation_cff.py
@@ -3,13 +3,16 @@ import FWCore.ParameterSet.Config as cms
 # -*-SH-*-
 from RecoMuon.MuonIsolationProducers.caloExtractorByAssociatorBlocks_cff import *
 from RecoMuon.MuonIsolationProducers.trackExtractorBlocks_cff import *
+from RecoMuon.MuonIsolationProducers.jetExtractorBlock_cff import *
 MIdIsoExtractorPSetBlock = cms.PSet(
     CaloExtractorPSet = cms.PSet(
         MIsoCaloExtractorByAssociatorTowersBlock
     ),
     TrackExtractorPSet = cms.PSet(
         MIsoTrackExtractorBlock
+    ),
+    JetExtractorPSet = cms.PSet(
+        MIsoJetExtractorBlock
     )
 )
-
 
