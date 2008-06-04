c78dc2b9e48682a00cdeaa7f0186d4c48029d0ea
Seth Cooper <sha1-32dd4c14096c6277ce1889fd6c006e25f1f45009@cern.ch>
2008-06-04 19:39:14 +0000

    Updating for new packages.

--- CaloOnlineTools/EcalTools/plugins/plugins.cc
+++ CaloOnlineTools/EcalTools/plugins/plugins.cc
@@ -6,6 +6,13 @@
 #include "CaloOnlineTools/EcalTools/plugins/EcalPedHists.h"
 #include "CaloOnlineTools/EcalTools/plugins/EcalURecHitHists.h"
 #include "CaloOnlineTools/EcalTools/plugins/EcalHexDisplay.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalCosmicsHists.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalExclusiveTrigFilter.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalHighEnCosmicFilter.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalTPGAnalyzer.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalPulseShapeGrapher.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalChannelCheckerTreeHists.h"
+#include "CaloOnlineTools/EcalTools/plugins/EcalChannelChecker.h"
 
 
 DEFINE_FWK_MODULE(EcalDigiDisplay);
@@ -15,4 +22,11 @@ DEFINE_FWK_MODULE(EcalMipGraphs);
 DEFINE_FWK_MODULE(EcalPedHists);
 DEFINE_FWK_MODULE(EcalURecHitHists);
 DEFINE_FWK_MODULE(EcalHexDisplay);
+DEFINE_FWK_MODULE(EcalCosmicsHists);
+DEFINE_FWK_MODULE(EcalExclusiveTrigFilter);
+DEFINE_FWK_MODULE(EcalHighEnCosmicFilter);
+DEFINE_FWK_MODULE(EcalTPGAnalyzer);
+DEFINE_FWK_MODULE(EcalPulseShapeGrapher);
+DEFINE_FWK_MODULE(EcalChannelCheckerTreeHists);
+DEFINE_FWK_MODULE(EcalChannelChecker);
 
