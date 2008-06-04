a3ec439cb18cd77b223b424d1ae5daab68608415
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-04 21:25:19 +0000

    Update debug to false

--- RecoLocalMuon/DTRecHit/python/dt1DRecHits_NoDrift_CosmicData_cfi.py
+++ RecoLocalMuon/DTRecHit/python/dt1DRecHits_NoDrift_CosmicData_cfi.py
@@ -7,7 +7,7 @@ from RecoLocalMuon.DTRecHit.DTNoDriftAlgo_CosmicData_cfi import *
 dt1DRecHits = cms.EDProducer("DTRecHitProducer",
     # The reconstruction algo and its parameter set
     DTNoDriftAlgo_CosmicData,
-    debug = cms.untracked.bool(True),
+    debug = cms.untracked.bool(False),
     # The label to retrieve digis from the event
     dtDigiLabel = cms.InputTag("dtunpacker")
 )
