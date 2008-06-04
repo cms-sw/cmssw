5681b84387cabbcb698fdca7d44619996bf3e641
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-04 21:49:47 +0000

    update py configs

---
+++ RecoLocalCalo/EcalRecProducers/python/ecal2004TBRecHit_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+# rechit producer
+ecal2004TBRecHit = cms.EDProducer("EcalRecHitProducer",
+    EErechitCollection = cms.string(''),
+    EEuncalibRecHitCollection = cms.InputTag(""),
+    EBuncalibRecHitCollection = cms.InputTag("ecal2004TBWeightUncalibRecHit","EcalUncalibRecHitsEB"),
+    EBrechitCollection = cms.string('EcalRecHitsEB')
+)
+
+
