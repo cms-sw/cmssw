4271f94a37ca2c0c262b5dbbc6821160631ea995
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-04 19:40:01 +0000

    update py configs

--- RecoEgamma/Examples/python/simplePhotonAnalyzer_cfi.py
+++ RecoEgamma/Examples/python/simplePhotonAnalyzer_cfi.py
@@ -2,13 +2,17 @@ import FWCore.ParameterSet.Config as cms
 
 #
 #  Author: N. Marinelli, U. of Notre Dame, US
-#  $Id: simplePhotonAnalyzer.cfi,v 1.6 2008/03/16 23:13:04 nancy Exp $ 
+#  $Id: simplePhotonAnalyzer.cfi,v 1.8 2008/06/03 13:53:55 nancy Exp $ 
 #
 simplePhotonAnalyzer = cms.EDAnalyzer("SimplePhotonAnalyzer",
-    mcProducer = cms.string('source'),
-    photonCollection = cms.string(''),
     phoProducer = cms.string('photons'),
-    primaryVertexProducer = cms.string('offlinePrimaryVerticesFromCTFTracks')
+    mcProducer = cms.string('source'),
+    endcapEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+    # 1=pt10,2=pt35,3=E10,4=E1000
+    sample = cms.int32(2),
+    photonCollection = cms.string(''),
+    primaryVertexProducer = cms.string('offlinePrimaryVerticesWithBS'),
+    barrelEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEB")
 )
 
 
