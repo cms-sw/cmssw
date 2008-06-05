4921c5732d09e71b728902450ee85f4363839955
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2008-06-05 14:47:34 +0000

    correct and move the algorithm part to RPCDetId

--- CondFormats/RPCObjects/src/DBSpecToDetUnit.cc
+++ CondFormats/RPCObjects/src/DBSpecToDetUnit.cc
@@ -12,11 +12,6 @@ using namespace edm;
 uint32_t DBSpecToDetUnit::operator()(const ChamberLocationSpec & ch, 
    const FebLocationSpec & feb)
 {
-  //
-  //FIXME !!!  semi-dummy and buggy method, check carefully !!!!
-  //
-  
-
   // REGION
   int region = -2;
   bool barrel = (ch.barrelOrEndcap=="Barrel");
@@ -24,128 +19,20 @@ uint32_t DBSpecToDetUnit::operator()(const ChamberLocationSpec & ch,
   else if (ch.diskOrWheel<0) region = -1;
   else if (ch.diskOrWheel>0) region = 1;
 
-  // RING - what to hell is it.......
-  int ring = ch.diskOrWheel;
+  //ROLL
+  string nroll = feb.localEtaPartition;
 
-  //STATION
-  int station = -1;
-  if (barrel) {
-    if (ch.layer==1 || ch.layer==2) station = 1;
-    else if (ch.layer==3 || ch.layer==4) station = 2;
-    else station = ch.layer-2;
-  } else {   
-   station = abs(ring); 
-  }
-
-  //LAYER
-  int layer = 1;
-  if (barrel && station==1) layer = ch.layer;
-  if (barrel && station==2) layer = ch.layer-2; 
-
-  //SECTOR
-  int sector = ch.sector;
- 
-  //SUBSECTOR
-  int subsector = 1;
-
-  if (barrel) {
-    if (station==3 && ch.subsector=="+") subsector = 2;
-    if (station==4 && 
-         (   sector==1 || sector==2 || sector==3 
-                       || sector==5 || sector==6   
-          || sector==7 || sector==8 
-          || sector==10             || sector==12)
-          && (ch.subsector=="+")) {
-         subsector = 2;
-    }
-
-    if (station==4 && sector==4) {
-      if (ch.subsector=="--") subsector=1;
-      if (ch.subsector=="-")  subsector=2;
-      if (ch.subsector=="+")  subsector=3;
-      if (ch.subsector=="++") subsector=4;
-    } 
-  }
-
-   // ROLL
-  string roll = feb.cmsEtaPartition;
-  int iroll=0;
-
-  if      (roll=="1" || roll=="A") iroll = 1;
-  else if (roll=="2" || roll=="B") iroll = 2;
-  else if (roll=="3" || roll=="C") iroll = 3;
-  else if (roll=="D") iroll = 4;
-  else {
-    cout << "** RPC: DBSpecToDetUnit, how to assigne roll to: "
-         <<roll<<" ???" << endl;
-  }
-
-  if(region==0 && ring<0){
-    if      (roll=="1" || roll=="A") iroll = 3;
-    else if (roll=="2" || roll=="B") iroll = 2;
-    else if (roll=="3" || roll=="C") iroll = 1;
-  }
-
-  /*
-  // ROLL
-  string roll = feb.cmsEtaPartition;
-  int iroll=0;
-  //MB A (temporary?) change to fix an invalid FEB z-rotation
-  string localRoll = feb.localEtaPartition;
-  //if      (roll=="1" || roll=="A") iroll = 1;
-  if      ((roll=="1"&& localRoll=="Backward") || (roll=="3"&& localRoll=="Forward") || roll=="A") iroll = 1;  
-  else if (roll=="2" || roll=="B") iroll = 2;
-  //else if (roll=="3" || roll=="C") iroll = 3;
-  else if ((roll=="3"&& localRoll=="Backward") || (roll=="1"&& localRoll=="Forward") || roll=="C") iroll = 3;
-  else if (roll=="D") iroll = 4;
-  else {
-    cout << "** RPC: DBSpecToDetUnit, how to assigne roll to: "
-               <<roll<<" ???" << endl;
-  }
-  */
-  int trIndex = 0;
-  if(barrel){   
-    //cout <<" BARREL: " << endl; 
-    int eta_id = 6+ch.diskOrWheel;
-    int plane_id = station;
-    if(ch.layer==2) plane_id=5;
-    if(ch.layer==4) plane_id=6;
-    int sector_id = ch.sector*3;
-    int copy_id = subsector;
-    int roll_id = iroll;
-    trIndex=(eta_id*10000+plane_id*1000+sector_id*10+copy_id)*10+roll_id;
-  } 
-  else { 
-    //    cout << "ENDCAP : " << endl;
-    int eta_id = ch.layer;
-    if(ch.diskOrWheel>0) eta_id = 12-ch.layer;
-    int plane_id = abs(ch.diskOrWheel);
-    int sector_id = ch.sector;
-    // patch to fix phi rotation
-    sector_id--;
-    if (sector_id==0) sector_id=36;
-    //
-    int copy_id = 1;
-    int roll_id = iroll;
-    trIndex=(eta_id*10000+plane_id*1000+sector_id*10+copy_id)*10+ roll_id;
-  }
-
-
-  //
   // build RPCdetId
-  //
-  //cout<<"DBSpec: "<<trIndex;
   try {
-    RPCDetId du;
-    //du = RPCDetId(region, ring, station, sector, layer, subsector, iroll);
-    du.buildfromTrIndex(trIndex);
-    return du.rawId();
+    RPCDetId dn;
+    dn.buildfromDB(region, ch.diskOrWheel, ch.layer, ch.sector, 
+		   ch.subsector, nroll, ch.chamberLocationName);
+    return dn.rawId();
   } 
   catch(...) {
-    cout <<" Problem with RPCDetId, got exception!! " <<endl;
-    cout<<"TRindex from DBSpec: "<<trIndex;
+    edm::LogWarning("CondFormas/DBSpecToDetInit") 
+      <<" Problem with RPCDetId, got exception!! " 
+      <<"DB Chamber "<<ch.chamberLocationName<<" roll "<<nroll;
     return 0;
   }
-
-
 }
