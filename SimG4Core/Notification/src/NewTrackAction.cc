e921051e7fc53ddcc9e1210fd54c68564de114aa
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-06-05 06:50:45 +0000

    Take care of savings of decays/conversions

--- SimG4Core/Notification/src/NewTrackAction.cc
+++ SimG4Core/Notification/src/NewTrackAction.cc
@@ -3,71 +3,64 @@
 #include "SimG4Core/Notification/interface/SimG4Exception.h"
 #include "SimG4Core/Notification/interface/TrackInformationExtractor.h"
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "G4Track.hh"
-#include "G4VProcess.hh"
 
-NewTrackAction::NewTrackAction(bool saveDPandConv) 
-: savePrimaryDecayProductsAndConversions(saveDPandConv) {}
+NewTrackAction::NewTrackAction() {}
 
-void NewTrackAction::primary(const G4Track * aTrack) const
-{ primary(const_cast<G4Track *>(aTrack)); }
-
-void NewTrackAction::primary(G4Track * aTrack) const
-{ addUserInfoToPrimary(aTrack); }
-
-void NewTrackAction::secondary(const G4Track * aSecondary,const G4Track & mother) const
-{ secondary(const_cast<G4Track *>(aSecondary),mother); }
-
-void NewTrackAction::secondary(G4Track * aSecondary,const G4Track & mother) const
-{
-    if (aSecondary->GetParentID() != mother.GetTrackID()) 
-	throw SimG4Exception("NewTrackAction: secondary parent ID does not match mother id");
-    TrackInformationExtractor extractor;
-    const TrackInformation & motherInfo(extractor(mother));
-    addUserInfoToSecondary(aSecondary,motherInfo);
+void NewTrackAction::primary(const G4Track * aTrack) const {
+  primary(const_cast<G4Track *>(aTrack)); 
 }
 
-void NewTrackAction::addUserInfoToPrimary(G4Track * aTrack) const
-{
-    TrackInformation * trkInfo = new TrackInformation();
+void NewTrackAction::primary(G4Track * aTrack) const {
+  addUserInfoToPrimary(aTrack);
+}
+
+void NewTrackAction::secondary(const G4Track * aSecondary,const G4Track & mother, int flag) const {
+  secondary(const_cast<G4Track *>(aSecondary),mother,flag); 
+}
+
+void NewTrackAction::secondary(G4Track * aSecondary,const G4Track & mother, int flag) const {
+  if (aSecondary->GetParentID() != mother.GetTrackID()) 
+    throw SimG4Exception("NewTrackAction: secondary parent ID does not match mother id");
+  TrackInformationExtractor extractor;
+  const TrackInformation & motherInfo(extractor(mother));
+  addUserInfoToSecondary(aSecondary,motherInfo,flag);
+}
+
+void NewTrackAction::addUserInfoToPrimary(G4Track * aTrack) const {
+  TrackInformation * trkInfo = new TrackInformation();
+  trkInfo->isPrimary(true);
+  trkInfo->storeTrack(true);
+  trkInfo->putInHistory();
+  aTrack->SetUserInformation(trkInfo);  
+}
+
+void NewTrackAction::addUserInfoToSecondary(G4Track * aTrack,const TrackInformation & motherInfo, int flag) const {
+
+  TrackInformation * trkInfo = new TrackInformation();
+  LogDebug("SimG4CoreApplication") << "NewTrackAction called for "
+				   << aTrack->GetTrackID()
+				   << " mother " << motherInfo.isPrimary()
+				   << " flag " << flag;
+  
+  // Take care of cascade decays
+  if (flag == 1)
     trkInfo->isPrimary(true);
+
+  // Store if decay or conversion
+  if (flag > 0) {
     trkInfo->storeTrack(true);
     trkInfo->putInHistory();
-    aTrack->SetUserInformation(trkInfo);  
-}
-
-void NewTrackAction::addUserInfoToSecondary(G4Track * aTrack,const TrackInformation & motherInfo) const
-{
-    TrackInformation * trkInfo = new TrackInformation();
+    trkInfo->setIDonCaloSurface(aTrack->GetTrackID(),
+				motherInfo.getIDCaloVolume(),
+				motherInfo.getIDLastVolume());
+  } else {
     // transfer calo ID from mother (to be checked in TrackingAction)
     trkInfo->setIDonCaloSurface(motherInfo.getIDonCaloSurface(),
 				motherInfo.getIDCaloVolume(),
 				motherInfo.getIDLastVolume());
-
-    if (savePrimaryDecayProductsAndConversions)
-    {
-       // Check whether mother is a primary
-       if (motherInfo.isPrimary())
-       {
-          bool isDecayProduct =
-          (aTrack->GetCreatorProcess()->GetProcessType() == fDecay &&
-          aTrack->GetCreatorProcess()->GetProcessName() == "Decay");
-
-          bool isConversionProduct =
-          (aTrack->GetCreatorProcess()->GetProcessType() == fElectromagnetic &&
-          aTrack->GetCreatorProcess()->GetProcessName() == "conv");
-
-          // Take care of cascade decays
-          if (isDecayProduct)
-             trkInfo->isPrimary(true);
-
-          // Store if decay or conversion
-          if (isDecayProduct || isConversionProduct) 
-          {
-             trkInfo->storeTrack(true);
-             trkInfo->putInHistory();
-          }
-       }
-    }
-    aTrack->SetUserInformation(trkInfo);  
+  }
+  aTrack->SetUserInformation(trkInfo);  
 }
