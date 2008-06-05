00798a4641586a843f094d5f06367120cc1b2892
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-06-05 10:14:05 +0000

    Change due to interface change for Notification

--- SimG4Core/KillSecondaries/src/KillSecondariesStackingAction.cc
+++ SimG4Core/KillSecondaries/src/KillSecondariesStackingAction.cc
@@ -4,20 +4,17 @@
 
 #include "G4Track.hh"
 
-G4ClassificationOfNewTrack KillSecondariesStackingAction::ClassifyNewTrack(const G4Track * aTrack) 
-{
-    NewTrackAction newTA(true);
-    if (aTrack->GetCreatorProcess()==0 || aTrack->GetParentID()==0)     
-    {
-	newTA.primary(aTrack);
-	return fUrgent;
-    }
-    else
-    {
-        const G4Track * mother = CurrentG4Track::track();
-        newTA.secondary(aTrack, *mother);
-	return fKill;
-    }
+G4ClassificationOfNewTrack KillSecondariesStackingAction::ClassifyNewTrack(const G4Track * aTrack) {
+
+  NewTrackAction newTA;
+  if (aTrack->GetCreatorProcess()==0 || aTrack->GetParentID()==0) {
+    newTA.primary(aTrack);
+    return fUrgent;
+  } else {
+    const G4Track * mother = CurrentG4Track::track();
+    newTA.secondary(aTrack, *mother, 0);
+    return fKill;
+  }
 
 }
 
