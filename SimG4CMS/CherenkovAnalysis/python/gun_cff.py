7319537ee87435a26ff32f4565c62a89955f09d8
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-05 06:20:54 +0000

    update py configs

---
+++ SimG4CMS/CherenkovAnalysis/python/gun_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("FlatRandomEGunSource",
+    PGunParameters = cms.untracked.PSet(
+        PartID = cms.untracked.vint32(11),
+        MaxEta = cms.untracked.double(0.0),
+        MaxPhi = cms.untracked.double(1.57079632679),
+        MinEta = cms.untracked.double(0.0),
+        MinE = cms.untracked.double(10.0),
+        MinPhi = cms.untracked.double(1.57079632679),
+        MaxE = cms.untracked.double(10.0)
+    ),
+    Verbosity = cms.untracked.int32(0)
+)
+
+# Don't smear our vertex!
+VtxSmeared = cms.EDFilter("GaussEvtVtxGenerator",
+    MeanX = cms.double(0.0),
+    MeanY = cms.double(-2.0),
+    MeanZ = cms.double(0.0),
+    SigmaY = cms.double(0.0),
+    SigmaX = cms.double(0.0),
+    SigmaZ = cms.double(0.0)
+)
+
+
