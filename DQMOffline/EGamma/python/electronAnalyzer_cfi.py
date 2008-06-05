02588e51955eb2e974635bbbe3ca9a040f844792
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-05 23:25:37 +0000

    update py configs

---
+++ DQMOffline/EGamma/python/electronAnalyzer_cfi.py
@@ -0,0 +1,60 @@
+# The following comments couldn't be translated into the new config version:
+
+# histos limits and binning
+
+import FWCore.ParameterSet.Config as cms
+
+gsfElectronAnalysis = cms.EDAnalyzer("ElectronAnalyzer",
+    electronCollection = cms.InputTag("pixelMatchGsfElectrons"),
+    Nbinxyz = cms.int32(50),
+    Nbineop2D = cms.int32(30),
+    Nbinp = cms.int32(50),
+    Lhitsmax = cms.double(10.0),
+    Etamin = cms.double(-2.5),
+    Nbinfhits = cms.int32(20),
+    Dphimin = cms.double(-0.01),
+    Pmax = cms.double(300.0),
+    Phimax = cms.double(3.2),
+    Phimin = cms.double(-3.2),
+    Eopmax = cms.double(5.0),
+    # efficiency cuts
+    MaxPt = cms.double(100.0),
+    Nbinlhits = cms.int32(5),
+    Nbinpteff = cms.int32(19),
+    Nbinphi2D = cms.int32(32),
+    Nbindetamatch2D = cms.int32(50),
+    Nbineta = cms.int32(50),
+    DeltaR = cms.double(0.05),
+    # 1 provides basic output
+    # 2 provides output of the fill step + 1
+    # 3 provides output of the store step + 2
+    outputFile = cms.string('gsfElectronHistos.root'),
+    Nbinp2D = cms.int32(50),
+    Nbindeta = cms.int32(100),
+    # DBE verbosity
+    Verbosity = cms.untracked.int32(2),
+    Nbinpt2D = cms.int32(50),
+    Nbindetamatch = cms.int32(100),
+    Fhitsmax = cms.double(20.0),
+    matchingObjectCollection = cms.InputTag("mergedSuperClusters"),
+    Nbinphi = cms.int32(64),
+    Nbineta2D = cms.int32(50),
+    Eopmaxsht = cms.double(3.0),
+    MaxAbsEta = cms.double(2.5),
+    Nbindphimatch = cms.int32(100),
+    Detamax = cms.double(0.005),
+    Nbinpt = cms.int32(50),
+    Nbindphimatch2D = cms.int32(50),
+    Etamax = cms.double(2.5),
+    Dphimax = cms.double(0.01),
+    Dphimatchmax = cms.double(0.2),
+    Detamatchmax = cms.double(0.05),
+    Nbindphi = cms.int32(100),
+    Detamatchmin = cms.double(-0.05),
+    Ptmax = cms.double(100.0),
+    Nbineop = cms.int32(50),
+    Dphimatchmin = cms.double(-0.2),
+    Detamin = cms.double(-0.005)
+)
+
+
---
+++ DQMOffline/EGamma/python/photonAnalyzer_cfi.py
@@ -0,0 +1,63 @@
+import FWCore.ParameterSet.Config as cms
+
+photonAnalysis = cms.EDAnalyzer("PhotonAnalyzer",
+    OutputMEsInRootFile = cms.bool(True),
+    dEtaTracksMin = cms.double(-0.2),
+    eMin = cms.double(0.0),
+    hcalIsolExtR = cms.double(0.3),
+    dEtaTracksMax = cms.double(0.2),
+    hcalIsolInnR = cms.double(0.0),
+    scEndcapProducer = cms.string('correctedMulti5x5SuperClustersWithPreshower'),
+    minTrackPtCut = cms.double(1.5),
+    minBcEtCut = cms.double(0.0),
+    bcBarrelCollection = cms.InputTag("hybridSuperClusters","hybridBarrelBasicClusters"),
+    etBin = cms.int32(100),
+    etaMax = cms.double(2.5),
+    phiMax = cms.double(3.14),
+    r9Max = cms.double(1.1),
+    phiMin = cms.double(-3.14),
+    lipCut = cms.double(2.0),
+    eMax = cms.double(100.0),
+    bcEndcapCollection = cms.InputTag("multi5x5BasicClusters","multi5x5EndcapBasicClusters"),
+    OutputFileName = cms.string('DQMPhotonHisto.root'),
+    photonCollection = cms.string(''),
+    etaBin = cms.int32(100),
+    dEtaTracksBin = cms.int32(100),
+    hbheInstance = cms.string(''),
+    endcapEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+    barrelEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+    dPhiTracksMax = cms.double(0.5),
+    # string  bcProducer  =        "islandBasicClusters"
+    # string  bcBarrelCollection = "islandBarrelBasicClusters"
+    # string  bcEndcapCollection = "islandEndcapBasicClusters"
+    hbheModule = cms.string('hbhereco'),
+    trackProducer = cms.InputTag("generalTracks"),
+    # DBE verbosity
+    Verbosity = cms.untracked.int32(2),
+    trkIsolInnR = cms.double(0.03),
+    r9Min = cms.double(0.0),
+    ecalIsolR = cms.double(0.35),
+    dPhiTracksMin = cms.double(-0.5),
+    phiBin = cms.int32(100),
+    r9Bin = cms.int32(100),
+    etMin = cms.double(0.0),
+    trkIsolExtR = cms.double(0.3),
+    eBin = cms.int32(100),
+    maxNumOfTracksInCone = cms.int32(3),
+    etMax = cms.double(100.0),
+    hcalEtSumCut = cms.double(6.0),
+    # 1 provides basic output
+    # 2 provides output of the fill step + 1
+    # 3 provides output of the store step + 2
+    phoProducer = cms.string('photons'),
+    minHcalHitEtCut = cms.double(0.0),
+    etaMin = cms.double(-2.5),
+    minPhoEtCut = cms.double(5.0),
+    dPhiTracksBin = cms.int32(100),
+    scBarrelProducer = cms.string('correctedHybridSuperClusters'),
+    Name = cms.untracked.string('photonAnalysis'),
+    trkPtSumCut = cms.double(9999.0),
+    ecalEtSumCut = cms.double(5.0)
+)
+
+
