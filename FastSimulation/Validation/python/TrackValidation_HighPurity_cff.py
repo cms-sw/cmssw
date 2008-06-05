b57b36666c883de6643cea75476bfe96d75f5b57
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-06-05 02:42:51 +0000

    sync with latest config changes

---
+++ FastSimulation/Validation/python/TrackValidation_HighPurity_cff.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# Define Validation sequence over "highPurity" tracks. 
+# For proper comparison the same requirement should be applied on FullSim as well.
+#
+# Tracking particle module
+from SimGeneral.TrackingAnalysis.trackingParticles_cfi import *
+# Track Associators
+from SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi import *
+from SimTracker.TrackAssociation.TrackAssociatorByHits_cfi import *
+#new postreco sequence
+from SimTracker.TrackAssociation.trackMCMatch_cfi import *
+from SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cff import *
+# Track Validator    
+from Validation.RecoTrack.cuts_cff import *
+from Validation.RecoTrack.cutsTPEffic_cfi import *
+from Validation.RecoTrack.cutsTPFake_cfi import *
+from Validation.RecoTrack.MultiTrackValidator_cff import *
+valid = cms.Sequence(cms.SequencePlaceholder("genParticles")*trackingParticles*cutsTPEffic*cutsTPFake*multiTrackValidator)
+trackingtruthprod.TrackerHitLabels = ['TrackerHits']
+TrackAssociatorByHitsESProducer.associateStrip = False
+TrackAssociatorByHitsESProducer.associatePixel = False
+TrackAssociatorByHitsESProducer.ROUList = ['TrackerHits']
+# pass only high purity tracks
+multiTrackValidator.label = ['firstfilter']
+multiTrackValidator.associators = ['TrackAssociatorByHits']
+multiTrackValidator.UseAssociators = True
+
