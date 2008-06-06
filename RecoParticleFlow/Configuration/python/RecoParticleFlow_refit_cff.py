4f62124ccc3bfd67aab3cbe5cab59e4113229bfa
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-06 22:43:38 +0000

    update py configs

---
+++ RecoParticleFlow/Configuration/python/RecoParticleFlow_refit_cff.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoParticleFlow.PFClusterProducer.towerMakerPF_cff import *
+from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import *
+from RecoParticleFlow.PFTracking.particleFlowTrack_cff import *
+from RecoParticleFlow.PFBlockProducer.particleFlowSimParticle_cff import *
+from RecoParticleFlow.PFBlockProducer.particleFlowBlock_cff import *
+from RecoParticleFlow.PFProducer.particleFlow_cff import *
+from RecoTracker.Configuration.RecoTracker_cff import *
+CaloTowerConstituentsMapBuilder = cms.ESProducer("CaloTowerConstituentsMapBuilder",
+    MapFile = cms.untracked.string('Geometry/CaloTopology/data/CaloTowerEEGeometric.map.gz')
+)
+
+particleFlowReco = cms.Sequence(ckftracks*caloTowersPFRec*particleFlowCluster*elecPreId*particleFlowBlock*particleFlow)
+
