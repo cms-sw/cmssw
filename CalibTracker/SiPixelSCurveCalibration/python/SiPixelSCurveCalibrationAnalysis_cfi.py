7b410881f3d63a9e0937bc68e55602f96e46210f
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-06 18:14:28 +0000

    Update from config

--- CalibTracker/SiPixelSCurveCalibration/python/SiPixelSCurveCalibrationAnalysis_cfi.py
+++ CalibTracker/SiPixelSCurveCalibration/python/SiPixelSCurveCalibrationAnalysis_cfi.py
@@ -8,22 +8,30 @@ import FWCore.ParameterSet.Config as cms
 siPixelSCurveAnalysis = cms.EDFilter("SiPixelSCurveCalibrationAnalysis",
     #setting this value to false will put all folders in one 
     useDetectorHierarchyFolders = cms.untracked.bool(True),
-    plaquettesToSave = cms.untracked.vstring(''),
-    #parameters common to SiPixelOfflineCalibAnalysisBase 
-    DetSetVectorSiPixelCalibDigiTag = cms.InputTag("siPixelCalibDigis"),
-    minimumThreshold = cms.untracked.double(0.0),
-    #these values are all very large (or small) - user should edit them for what they want to look for
-    minimumChi2prob = cms.untracked.double(0.4),
     saveFile = cms.untracked.bool(True),
+    # entire DetIDs
+    # Watch out, setting this too high on corrupted data will cause HUGE memory consumption!
+    detIDsToSave = cms.untracked.vuint32(),
+    minimumThreshold = cms.untracked.double(0.0),
+    # example  { 352394505, 352394505 }  AGAIN, this will eat A LOT of memory
+    #these values are all very large (or small) - user should edit them for what they want to look for
+    minimumChi2prob = cms.untracked.double(0.8),
     minimumSigma = cms.untracked.double(0.0),
     write2dFitResult = cms.untracked.bool(True),
-    maximumEffAsymptote = cms.untracked.double(100.0),
+    maxCurvesToSave = cms.untracked.uint32(1000), ## limit the maximum number of bad curves to save. This limit applies to both saving bad-flagged error histograms and
+
+    maximumEffAsymptote = cms.untracked.double(1.01), ##this is a pretty silly parameter but it could be helpful for debugging in the future
+
     write2dHistograms = cms.untracked.bool(True),
-    maximumThreshold = cms.untracked.double(260.0),
+    maximumSigma = cms.untracked.double(10.0),
     minimumEffAsymptote = cms.untracked.double(0.0),
     outputFileName = cms.string('Pixel_DQM_Calibration.root'),
+    #parameters common to SiPixelOfflineCalibAnalysisBase 
+    DetSetVectorSiPixelCalibDigiTag = cms.InputTag("siPixelCalibDigis"),
+    maximumThreshold = cms.untracked.double(255.0),
     saveCurvesThatFlaggedBad = cms.untracked.bool(False),
-    maximumSigma = cms.untracked.double(20.0)
+    maximumSigmaBin = cms.untracked.double(10.0),
+    maximumThresholdBin = cms.untracked.double(255.0)
 )
 
 
