34ec51887e26d9c05d00666a1158efcc9d1262e5
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2008-06-06 09:42:59 +0000

    DQM Core Migration for all Sub paths

--- HLTriggerOffline/Tau/src/SealModule.cc
+++ HLTriggerOffline/Tau/src/SealModule.cc
@@ -1,25 +1,17 @@
-
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
-
 #include "HLTriggerOffline/Tau/interface/HLTTauMcInfo.h"
 #include "HLTriggerOffline/Tau/interface/HLTTauRefInfo.h"
 #include "HLTriggerOffline/Tau/interface/HLTTauRefCombiner.h"
-
 #include "HLTriggerOffline/Tau/interface/HLTTauAnalyzer.h"
 #include "HLTriggerOffline/Tau/interface/MCTauCand.h"
-
 #include "HLTriggerOffline/Tau/interface/HLTTauL25Validation.h"
 #include "HLTriggerOffline/Tau/interface/L25TauAnalyzer.h"
 #include "HLTriggerOffline/Tau/interface/L2TauValidation.h"
 #include "HLTriggerOffline/Tau/interface/L2TauAnalyzer.h"
 #include "HLTriggerOffline/Tau/interface/L1TauAnalyzer.h"
-<<<<<<< SealModule.cc
 #include "HLTriggerOffline/Tau/interface/L25TauValidation.h"
-
-=======
 #include "HLTriggerOffline/Tau/interface/HLTTauValidation.h"
->>>>>>> 1.15
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTTauMcInfo);
@@ -27,17 +19,13 @@ DEFINE_ANOTHER_FWK_MODULE(HLTTauRefInfo);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauRefCombiner);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauL25Validation);
-DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L2TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L2TauValidation);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
-<<<<<<< SealModule.cc
+DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L25TauValidation);
-=======
 DEFINE_ANOTHER_FWK_MODULE(HLTTauValidation);
 
 
 
-
->>>>>>> 1.15
 
