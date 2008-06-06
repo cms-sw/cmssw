4c3d353c6d234987f6a4b2c59d526face1d24908
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-06 22:54:55 +0000

    Update from configs

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHCAL_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHCAL_cfi.py
@@ -1,14 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 particleFlowRecHitHCAL = cms.EDFilter("PFRecHitProducerHCAL",
+    # verbosity 
+    verbose = cms.untracked.bool(False),
+    caloTowers = cms.InputTag("towerMakerPF"),
     hcalRecHitsHBHE = cms.InputTag(""),
     # cell threshold in barrel 
     thresh_Barrel = cms.double(0.8),
-    # verbosity 
-    verbose = cms.untracked.bool(False),
+    # cell threshold in HF
+    thresh_HF = cms.double(0.0),
     # cell threshold in endcap 
-    thresh_Endcap = cms.double(0.8),
-    caloTowers = cms.InputTag("towerMakerPF")
+    thresh_Endcap = cms.double(0.8)
 )
 
 
--- RecoParticleFlow/PFClusterProducer/python/towerMakerPF_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/towerMakerPF_cfi.py
@@ -1,25 +1,19 @@
-# The following comments couldn't be translated into the new config version:
-
-# GeV, -1000 means cut not used 
-# GeV, -1000 means cut not used 
 import FWCore.ParameterSet.Config as cms
 
 import RecoJets.JetProducers.CaloTowerSchemeB_cfi
 towerMakerPF = RecoJets.JetProducers.CaloTowerSchemeB_cfi.towerMaker.clone()
-towerMakerPF.HBThreshold = 0.
+# replace towerMakerPF.HBThreshold = 0.
 towerMakerPF.HOThreshold = 999999
 towerMakerPF.HESThreshold = 0.
 towerMakerPF.HEDThreshold = 0.
-towerMakerPF.HF1Threshold = 999999
-towerMakerPF.HF2Threshold = 999999
+#replace towerMakerPF.HF1Threshold = 0
+#replace towerMakerPF.HF2Threshold = 0
 towerMakerPF.EBThreshold = 999999
 towerMakerPF.EEThreshold = 999999
-towerMakerPF.EBSumThreshold = -1000
-towerMakerPF.EESumThreshold = -1000
-towerMakerPF.HcalThreshold = -1000 ## GeV, -1000 means cut not used 
-
-towerMakerPF.EcutTower = -1000 ## GeV, -1000 means cut not used
-
+#replace towerMakerPF.EBSumThreshold =-1000 // GeV, -1000 means cut not used 
+#replace towerMakerPF.EESumThreshold =-1000 // GeV, -1000 means cut not used 
+#replace towerMakerPF.HcalThreshold = -1000 // GeV, -1000 means cut not used 
+# replace towerMakerPF.EcutTower = -1000     // GeV, -1000 means cut not used
 towerMakerPF.UseHO = False
 towerMakerPF.AllowMissingInputs = True
 
