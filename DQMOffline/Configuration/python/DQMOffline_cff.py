29b365dd1df6ab1d172e55f6650ce07c663a502a
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-06-07 14:16:13 +0000

    - rename ecal sequences

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -5,6 +5,6 @@ from DQMOffline.Ecal.ecal_dqm_source_offline_cff import *
 from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
-DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source-offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
-DQMOffline_woTracker = cms.Sequence(ecal_dqm_source-offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
+DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
+DQMOffline_woTracker = cms.Sequence(ecal_dqm_source_offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
 
