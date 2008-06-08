771530604b545833153adf6e1dfdb8a14f7a7a4d
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2008-06-08 16:57:40 +0000

    vertexFilter_cfi.py

--- RecoParticleFlow/PFTracking/python/vertexFilter_cfi.py
+++ RecoParticleFlow/PFTracking/python/vertexFilter_cfi.py
@@ -4,13 +4,16 @@ vertFilter = cms.EDFilter("VertexFilter",
     TrackAlgorithm = cms.string('iter2'),
     recVertices = cms.InputTag("pixelVertices"),
     MinHits = cms.int32(3),
-    DistRhoFromVertex = cms.double(0.1),
-    DistZFromVertex = cms.double(0.4),
+    TrackQualities = cms.vstring('loose', 
+        'tight', 
+        'highPurity'),
     recTracks = cms.InputTag("secWithMaterialTracks"),
+    DistZFromVertex = cms.double(0.4),
+    DistRhoFromVertex = cms.double(0.1),
     UseQuality = cms.bool(True),
     ChiCut = cms.double(130.0),
-    TrackQuality = cms.string('highPurity'),
     VertexCut = cms.bool(True)
 )
+
 
 
