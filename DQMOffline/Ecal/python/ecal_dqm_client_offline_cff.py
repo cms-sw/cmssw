14d682aa983f45ff58678703baa2d67af0bb46ca
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-06-08 08:47:41 +0000

    - run QualityTest at endRun, too

--- DQMOffline/Ecal/python/ecal_dqm_client_offline_cff.py
+++ DQMOffline/Ecal/python/ecal_dqm_client_offline_cff.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.EcalBarrelMonitorClient.EcalBarrelMonitorClient_cfi import *
+
 dqmQTestEB = cms.EDFilter("QualityTester",
     reportThreshold = cms.untracked.string('red'),
     prescaleFactor = cms.untracked.int32(1),
     qtList = cms.untracked.FileInPath('DQM/EcalBarrelMonitorModule/test/data/EcalBarrelQualityTests.xml'),
-    getQualityTestsFromFile = cms.untracked.bool(True)
+    getQualityTestsFromFile = cms.untracked.bool(True),
+    qtestOnEndRun = cms.untracked.bool(True)
 )
 
 ecal_dqm_client_offline = cms.Sequence(ecalBarrelMonitorClient*dqmQTestEB)
@@ -14,5 +16,4 @@ ecalBarrelMonitorClient.maskFile = ''
 ecalBarrelMonitorClient.location = 'P5'
 ecalBarrelMonitorClient.verbose = False
 ecalBarrelMonitorClient.enabledClients = ['Integrity', 'StatusFlags', 'Occupancy', 'PedestalOnline', 'Cosmic', 'Cluster', 'TriggerTower', 'Summary']
-
 
