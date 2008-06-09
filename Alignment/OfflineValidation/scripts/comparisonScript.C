32f01b9559dbac27f5a8de35b93c6086b59eaa75
Matthias Edelhoff <sha1-b4e200e941da071480460e77b2afa35e3ad47d9a@cern.ch>
2008-06-09 10:31:59 +0000

    * added inputfile and output directory selection and 2D module only histograms

--- Alignment/OfflineValidation/scripts/comparisonScript.C
+++ Alignment/OfflineValidation/scripts/comparisonScript.C
@@ -10,15 +10,14 @@
 #include "TStyle.h"
 #include "TCut.h"
 
-void comparisonScript(){
-
-	
-	gStyle->SetOptStat("emr");
-	gSystem->Load("comparisonPlots_cc.so");
-	
+void comparisonScript(string inFile="../test/comparisonSurvey.root",string outDir="outputDir/")
+{
+       	gStyle->SetOptStat("emr");
+	gROOT->ProcessLine(".L comparisonPlots.cc+");
+		
 	// create plots object for given input
 	// arg1 = input ROOT comparison, arg2 = output directory, arg3 = name of output ROOT file
-	comparisonPlots c1("../test/comparisonSurvey.root","outputDir/");
+	comparisonPlots c1(inFile.c_str(),outDir.c_str());
 	
 	// ------------ COMMON CUTS -----------
 	// LEVEL CUT - which hierarchy to plot
@@ -31,6 +30,10 @@ void comparisonScript(){
 	TCut TIDCut = "(sublevel == 4)"; // TID
 	TCut TOBCut = "(sublevel == 5)"; // TOB
 	TCut TECCut = "(sublevel == 6)"; // TEC
+
+	TCut Det1dCut = "(detDim==1)";
+        TCut Det2dCut = "(detDim==2)";  
+
 		
 	// for plot3x5:
 	// arg1 = cuts, arg2 = bool to save the 3x5 plot, arg3 = name of saved plot
@@ -39,24 +42,35 @@ void comparisonScript(){
 	
 	// plots the normal 3x3 plus dx/dy vs. r/z/phi
 	// stores each histogram to output file (including dr/dz/r*dphi 1D plots)
-	c1.plot3x5( levelCut, true );
-	//c1.plot3x5( levelCut+PXBCut );
-	//c1.plot3x5( levelCut+PXFCut );
-	//c1.plot3x5( levelCut+TIBCut );
-	//c1.plot3x5( levelCut+TIDCut );
-	//c1.plot3x5( levelCut+TOBCut );
-	//c1.plot3x5( levelCut+TECCut );
-	
+	c1.plot3x5( levelCut, "Tracker",true,"Tracker.eps" );
+	c1.plot3x5( levelCut+PXBCut, "PXB", true,  "PXB.eps" );
+	c1.plot3x5( levelCut+PXFCut, "PXF", true,  "PXF.eps" );
+	c1.plot3x5( levelCut+TIBCut, "TIB", true,  "TIB.eps" );
+	c1.plot3x5( levelCut+TIDCut, "TID", true,  "TID.eps" );
+	c1.plot3x5( levelCut+TOBCut, "TOB", true,  "TOB.eps" );
+	c1.plot3x5( levelCut+TECCut, "TEC", true,  "TEC.eps" );
+
+	//again this time only for 2D modules
+
+	c1.plot3x5( levelCut+Det2dCut, "Tracker2D",true,"Tracker2D.eps" );
+	c1.plot3x5( levelCut+PXBCut+Det2dCut, "PXB2D", true,  "PXB2D.eps" );
+	c1.plot3x5( levelCut+PXFCut+Det2dCut, "PXF2D", true,  "PXF2D.eps" );
+	c1.plot3x5( levelCut+TIBCut+Det2dCut, "TIB2D", true,  "TIB2D.eps" );
+	c1.plot3x5( levelCut+TIDCut+Det2dCut, "TID2D", true,  "TID2D.eps" );
+	c1.plot3x5( levelCut+TOBCut+Det2dCut, "TOB2D", true,  "TOB2D.eps" );
+	c1.plot3x5( levelCut+TECCut+Det2dCut, "TEC2D", true,  "TEC2D.eps" );
+
+
 	// plots the normal 3x3 plus dx/dy vs. r/z/phi 2D profile plots
 	// second argument is the nBinsX for profile plot
 	// all arguments are stored to output file
-	c1.plot3x5Profile( levelCut, 30 );
-	//c1.plot3x5Profile( levelCut+PXBCut, 30 );
-	//c1.plot3x5Profile( levelCut+PXFCut, 30 );
-	//c1.plot3x5Profile( levelCut+TIBCut, 30 );
-	//c1.plot3x5Profile( levelCut+TIDCut, 30) ;
-	//c1.plot3x5Profile( levelCut+TOBCut, 30 );
-	//c1.plot3x5Profile( levelCut+TECCut, 30 );
+	c1.plot3x5Profile( levelCut, "Tracker", 30 );
+	c1.plot3x5Profile( levelCut+PXBCut,"PXB", 30 );
+	c1.plot3x5Profile( levelCut+PXFCut,"PXF", 30 );
+	c1.plot3x5Profile( levelCut+TIBCut,"TIB", 30 );
+	c1.plot3x5Profile( levelCut+TIDCut,"TID", 30) ;
+	c1.plot3x5Profile( levelCut+TOBCut,"TOB", 30 );
+	c1.plot3x5Profile( levelCut+TECCut,"TEC", 30 );
 	
 	
 }
