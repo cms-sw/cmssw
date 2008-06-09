4207cae9e00e81da5b698519e15fe16973e4c0ec
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-06-09 13:35:09 +0000

    Re-organization of the simulation standard sequences Sim.cff -> psim Digi.cff -> pdigi Simulation.cff keeps unchanged functionality

--- Configuration/StandardSequences/python/Services_cff.py
+++ Configuration/StandardSequences/python/Services_cff.py
@@ -4,16 +4,10 @@
 
 import FWCore.ParameterSet.Config as cms
 
-#
-#service = Timing { } 
-#service = SimpleMemoryCheck {
-#    untracked int32 ignoreTotal = 1 # default is one
-#} 
-#service = Tracer {} 
-#
-# not for the moment
-#include "FWCore/Services/data/EnableFloatingPointExceptions.cfi"
+from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
+# Random numbers initialization service
 from Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff import *
 DQMStore = cms.Service("DQMStore")
+
 
 
--- Configuration/StandardSequences/python/Simulation_cff.py
+++ Configuration/StandardSequences/python/Simulation_cff.py
@@ -2,18 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 # REMINDER : det.simulation need a startup seed;
 # in your cfg, do NOT forget to give seeds via RandomNumberGeneratorService !!!
+# Include Configuration/StandardSequences/data/SimulationRandomNumberGeneratorSeeds.cff
 # 
-# Example configuration of the RandomNumberGeneratorService to appear in cfg:
-# service = RandomNumberGeneratorService
-# {
-#   untracked uint32 sourceSeed = 123456789
-#   PSet moduleSeeds =
-#   {
-#      untracked uint32 VtxSmeared = 98765432
-#      untracked uint32 g4SimHits  = 11
-#      untracked uint32 mix        = 12345
-#   }
-# }
 # Vertex smearing is exectuted in the pgen sequence
 # Geant4-based detector simulation
 # (necessary geometry and mag.field records included)
@@ -26,7 +16,7 @@ import FWCore.ParameterSet.Config as cms
 # the parameter's name is HepMCProductLabel, it belongs to
 # the PSet Generator, and the default = "VtxSmeared"
 #
-from SimG4Core.Configuration.SimG4Core_cff import *
+from Configuration.StandardSequences.Sim_cff import *
 #
 # if you want to skip vertex smearing, you need to reset it:
 # replace g4SimHits.Generator.HepMCProductLabel = "source"
@@ -38,7 +28,6 @@ from SimG4Core.Configuration.SimG4Core_cff import *
 # NOTA BENE: it MUST be run here at the moment, since it depends 
 # of the availability of the CrossingFrame in the Event
 #
-from SimGeneral.Configuration.SimGeneral_cff import *
 # Digitization (electronics response modeling)
 # (all necessary geometry and other records included in the cff's)
 #
@@ -46,8 +35,6 @@ from SimGeneral.Configuration.SimGeneral_cff import *
 #
 from Configuration.StandardSequences.Digi_cff import *
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
-psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*g4SimHits)
-pdigi = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*cms.SequencePlaceholder("mix")*doAllDigi*trackingParticles)
 simulation = cms.Sequence(psim*pdigi)
 
 
