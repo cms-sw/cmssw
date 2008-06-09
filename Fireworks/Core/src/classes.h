0835d06c25def8ecef9c13f79ce3d6202b21b230
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-06-09 20:21:18 +0000

    removed classes which ROOT no longer needs to know about

--- Fireworks/Core/src/classes.h
+++ Fireworks/Core/src/classes.h
@@ -1,17 +1,11 @@
 
 //Add includes for your classes here
 #include "Fireworks/Core/interface/FWDisplayEvent.h"
-#include "Fireworks/Core/interface/FWDataProxyBuilder.h"
-#include "Fireworks/Core/interface/FWRPZDataProxyBuilder.h"
-#include "Fireworks/Core/interface/FWRPZ2DDataProxyBuilder.h"
 #include "Fireworks/Core/interface/FWPhysicsObjectDesc.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/FWDisplayProperties.h"
-#include "Fireworks/Core/interface/FW3DLegoDataProxyBuilder.h"
 #include "Fireworks/Core/interface/FW3DLegoViewManager.h"
 #include "Fireworks/Core/interface/DetIdToMatrix.h"
-#include "Fireworks/Core/interface/TracksProxy3DBuilder.h"
-#include "Fireworks/Core/interface/TracksRecHitsProxy3DBuilder.h"
 #include "Fireworks/Core/interface/TEveElementIter.h"
 #include "Fireworks/Core/interface/FWGUIManager.h"
 #include "Fireworks/Core/interface/FWGUISubviewArea.h"
@@ -37,7 +31,6 @@
 #include "Fireworks/Core/interface/FWDoubleParameter.h"
 #include "Fireworks/Core/src/FWLongParameterSetter.h"
 #include "Fireworks/Core/interface/FWLongParameter.h"
-#include "Fireworks/Core/interface/GenParticleProxy3DBuilder.h"
 #include "Fireworks/Core/interface/GenParticleDetailView.h"
 
 
