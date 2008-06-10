c8c19ab5ecfeda250cafcd569db7a5043af36909
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2008-06-10 12:02:31 +0000

    pixelless removed

--- RecoTracker/IterativeTracking/python/SecStep_cff.py
+++ RecoTracker/IterativeTracking/python/SecStep_cff.py
@@ -1,42 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 import RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi
-#TRACKER HITS
 secPixelRecHits = RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi.siPixelRecHits.clone()
 import RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi
 secStripRecHits = RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi.siStripMatchedRecHits.clone()
 import RecoTracker.TkSeedingLayers.PixelLayerTriplets_cfi
-#SEEDING LAYERS
-#PIXEL
 seclayertriplets = RecoTracker.TkSeedingLayers.PixelLayerTriplets_cfi.pixellayertriplets.clone()
 import RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cfi
-#SEEDS
-#TRIPLETS
 secTriplets = RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cfi.globalSeedsFromTripletsWithVertices.clone()
-import RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cfi
-#TRIPLETS
-secPlTriplets = RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cfi.globalSeedsFromTripletsWithVertices.clone()
-import RecoTracker.TkSeedGenerator.GlobalCombinedSeeds_cfi
-#COMBINED 
-secCombSeeds = RecoTracker.TkSeedGenerator.GlobalCombinedSeeds_cfi.globalCombinedSeeds.clone()
 import RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cfi
-#TRAJECTORY MEASUREMENT
 secMeasurementTracker = RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cfi.MeasurementTracker.clone()
 import TrackingTools.TrajectoryFiltering.TrajectoryFilterESProducer_cfi
-#TRAJECTORY FILTER
 secCkfTrajectoryFilter = TrackingTools.TrajectoryFiltering.TrajectoryFilterESProducer_cfi.trajectoryFilterESProducer.clone()
 import RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilderESProducer_cfi
-#TRAJECTORY BUILDER
 secCkfTrajectoryBuilder = RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilderESProducer_cfi.GroupedCkfTrajectoryBuilder.clone()
 import RecoTracker.CkfPattern.CkfTrackCandidates_cfi
-#TRACK CANDIDATES
 secTrackCandidates = RecoTracker.CkfPattern.CkfTrackCandidates_cfi.ckfTrackCandidates.clone()
 import RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cfi
-#TRACKS
 secWithMaterialTracks = RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cfi.ctfWithMaterialTracks.clone()
-#FINAL TRACKS
 from RecoTracker.IterativeTracking.SecVxFilter_cff import *
-#HIT REMOVAL
 secClusters = cms.EDFilter("TrackClusterRemover",
     trajectories = cms.InputTag("firstfilter"),
     pixelClusters = cms.InputTag("siPixelClusters"),
@@ -46,19 +28,7 @@ secClusters = cms.EDFilter("TrackClusterRemover",
     stripClusters = cms.InputTag("siStripClusters")
 )
 
-#TIB LAYERS
-secPLlayertriplets = cms.ESProducer("MixedLayerTripletsESProducer",
-    ComponentName = cms.string('SecPlLayerTriplets'),
-    layerList = cms.vstring('TIB1+TIB2+TIB3'),
-    TIB = cms.PSet(
-        TTRHBuilder = cms.string('WithTrackAngle'),
-        matchedRecHits = cms.InputTag("secStripRecHits","matchedRecHit"),
-        useSimpleRphiHitsCleaner = cms.untracked.bool(False),
-        rphiRecHits = cms.InputTag("secStripRecHits","rphiRecHit")
-    )
-)
-
-secondStep = cms.Sequence(secClusters*secPixelRecHits*secStripRecHits*secTriplets*secPlTriplets*secCombSeeds*secTrackCandidates*secWithMaterialTracks*secStep)
+secondStep = cms.Sequence(secClusters*secPixelRecHits*secStripRecHits*secTriplets*secTrackCandidates*secWithMaterialTracks*secStep)
 secPixelRecHits.src = cms.InputTag("secClusters")
 secStripRecHits.ClusterProducer = 'secClusters'
 seclayertriplets.ComponentName = 'SecLayerTriplets'
@@ -67,12 +37,6 @@ seclayertriplets.FPix.HitProducer = 'secPixelRecHits'
 secTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 22.7
 secTriplets.OrderedHitsFactoryPSet.SeedingLayers = 'SecLayerTriplets'
 secTriplets.RegionFactoryPSet.RegionPSet.ptMin = 0.3
-secPlTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 22.7
-secPlTriplets.OrderedHitsFactoryPSet.SeedingLayers = 'SecPlLayerTriplets'
-secPlTriplets.RegionFactoryPSet.RegionPSet.ptMin = 0.3
-secPlTriplets.OrderedHitsFactoryPSet.GeneratorPSet.extraHitRZtolerance = 12.0
-secCombSeeds.PairCollection = 'secTriplets'
-secCombSeeds.TripletCollection = 'secPlTriplets'
 secMeasurementTracker.ComponentName = 'secMeasurementTracker'
 secMeasurementTracker.pixelClusterProducer = 'secClusters'
 secMeasurementTracker.stripClusterProducer = 'secClusters'
@@ -83,11 +47,11 @@ secCkfTrajectoryFilter.filterPset.minPt = 0.3
 secCkfTrajectoryBuilder.ComponentName = 'secCkfTrajectoryBuilder'
 secCkfTrajectoryBuilder.MeasurementTrackerName = 'secMeasurementTracker'
 secCkfTrajectoryBuilder.trajectoryFilterName = 'secCkfTrajectoryFilter'
-secTrackCandidates.SeedProducer = 'secCombSeeds'
-#replace secTrackCandidates.SeedProducer= "secPlTriplets"
+secTrackCandidates.SeedProducer = 'secTriplets'
 secTrackCandidates.TrajectoryBuilder = 'secCkfTrajectoryBuilder'
 secTrackCandidates.doSeedingRegionRebuilding = True
 secTrackCandidates.useHitsSplitting = True
 secWithMaterialTracks.src = 'secTrackCandidates'
 secWithMaterialTracks.clusterRemovalInfo = 'secClusters'
+
 
