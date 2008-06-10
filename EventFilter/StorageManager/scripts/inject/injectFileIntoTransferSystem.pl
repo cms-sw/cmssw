5adc1f72cff12314f1995fedd55a6ef055b59c69
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-06-10 14:00:53 +0000

    Strip of quotes.

--- EventFilter/StorageManager/scripts/inject/injectFileIntoTransferSystem.pl
+++ EventFilter/StorageManager/scripts/inject/injectFileIntoTransferSystem.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
+# $Id:$
+#
 # Written by Matt Rudolph June 2008
+#
 
 use strict;
 use DBI;
@@ -7,6 +10,17 @@ use Getopt::Long;
 use File::Basename;
 use Sys::Hostname;
 
+
+sub usageShort
+{
+  print "
+  ############################################################################################
+  For more information please see help:
+  $0 --help
+  ############################################################################################
+  \n";
+  exit;
+}
 
 sub usage
 {
@@ -16,21 +30,22 @@ sub usage
   $0 --help  : show this message
   --------------------------------------------------------------------------------------------
   Required parameters:
-  $0 --filename='file' --path='path' --filesize=size --type='type' [--destination]
+  $0 --filename='file' --path='path' --filesize=size 
+                                    --type='type' [--destination]
  
   Filename, path, and filesize are self explanatory
 
-  Type is the type of file, which can require extra parameters to be specified
-  Current enforced types: streamer, lumi, edm
+  Type is the type of file, which requires extra parameters to be specified
+  Current supported types: streamer, lumi, edm
 
   Destination determines where file ends up on Tier0. This corresponds to the 'dataset' 
   parameter of the previous system. It will be set to default if not set by user. 
-  If you are not sure about what you are doing please send an inquery to hn-tier0-ops\@cern.ch.
+  If you are not sure about what you are doing please send an inquiry to hn-tier0-ops\@cern.ch.
 
   -------------------------------------------------------------------------------------------- 
   Other parameters:
   --debug           : Print out extra messages
-  --hostname='host' : Specify a host different than where script run
+  --hostname        : Specify a host different than where script run
   --producer        : Producer of file
   --appname         : Application name for file (e.g. CMSSW)
   --appversion      : Application version
@@ -76,6 +91,26 @@ sub gettimestamp($)
     return $timestr;
 }
 
+#Strip quotes and otherwise check that a parameter is in good format
+sub checkOption($) {
+
+    my $theOpt=shift;
+
+    # strip off any double or single quotes 
+    # they won't play nice with sql query or notify script call
+    $theOpt =~ s/\'//g;
+    $theOpt =~ s/\"//g;
+
+    # check string for spaces
+    # return an error if found
+    if($theOpt=~ / /) {
+	print "Option specified as '$theOpt' has spaces in it.  Please use without spaces.  Exit\n";
+	exit;
+    }
+
+    return $theOpt;
+}
+
 my ($help, $debug, $hostname, $filename, $pathname, $index, $filesize);
 my ($producer, $stream, $type, $runnumber, $lumisection, $count,$instance);
 my ($createtime, $injecttime, $ctime, $itime, $comment, $destination);
@@ -109,41 +144,30 @@ $destination = 'default';
 $index ='';
 $comment = '';
 
-
-
 GetOptions(
-           "help"          => \$help,
-           "debug"         => \$debug,
-           "hostname=s"    => \$hostname,
-	   "filename=s"    => \$filename,
-	   "file=s"        => \$filename,
-	   "path=s"        => \$pathname,
-	   "pathname=s"    => \$pathname,
-	   "destination=s" => \$destination,
-	   "setuplabel=s"  => \$setuplabel,
-	   "dataset=s"	   => \$setuplabel,
-	   "filesize=i"    => \$filesize,
-	   "producer=s"    => \$producer,
-           "run=i"         => \$runnumber,
-           "runnumber=i"   => \$runnumber,
-	   "lumisection=i" => \$lumisection,
-	   "count=i"       => \$count,
-	   "instance=i"    => \$count,
-           "stream=s"      => \$stream,
-	   "type=s"        => \$type,
-	   "index=s"       => \$index,
-	   "nevents=i"     => \$nevents,
-	   "numevents=i"   => \$nevents,
-	   "appname=s"     => \$appname,
-	   "app_name=s"    => \$appname,
-	   "appversion=s"  => \$appversion,
-	   "app_version=s" => \$appversion,
-	   "checksum=s"    => \$checksum,
-	   "ctime=i"       => \$ctime,
-	   "start_time=i"  => \$ctime,
-	   "itime=i"       => \$itime,
-	   "stop_time=i" => \$itime,
-	   "comment=s"     => \$comment
+           "h|help"                   => \$help,
+           "debug"                    => \$debug,
+           "hostname=s"               => \$hostname,
+	   "file|filename=s"          => \$filename,
+	   "path|pathname=s"          => \$pathname,
+	   "destination=s"            => \$destination,
+	   "setuplabel|dataset=s"     => \$setuplabel,
+	   "filesize=i"               => \$filesize,
+	   "producer=s"               => \$producer,
+           "run|runnumber=i"          => \$runnumber,
+	   "lumisection=i"            => \$lumisection,
+	   "count=i"                  => \$count,
+	   "instance=i"               => \$instance,
+           "stream=s"                 => \$stream,
+	   "type=s"                   => \$type,
+	   "index=s"                  => \$index,
+	   "nevents|numevents=i"      => \$nevents,
+	   "appname|app_name=s"       => \$appname,
+	   "appversion|app_version=s" => \$appversion,
+	   "checksum=s"               => \$checksum,
+	   "ctime|start_time=i"       => \$ctime,
+	   "itime|stop_time=i"        => \$itime,
+	   "comment=s"                => \$comment
 	  ) or usage;
 
 $help && usage;
@@ -153,34 +177,46 @@ $help && usage;
 #Main starts here
 ############################################
 
+# first check formatting of options
+$filename = checkOption($filename);
+$pathname = checkOption($pathname);
+$hostname = checkOption($hostname);
+$destination = checkOption($destination);
+$setuplabel = checkOption($setuplabel);
+$producer = checkOption($producer);
+$stream = checkOption($stream);
+$type = checkOption($type);
+$index = checkOption($index);
+$appname = checkOption($appname);
+$appversion = checkOption($appversion);
+$checksum = checkOption($checksum);
+# explicit way to do comment since it can do ok with spaces
+$comment =~ s/\'//g;
+$comment =~ s/\"//g;
 
 #Filename, path, host and filesize must be correct or transfer will never work
 #First check to make sure all of these are set and exit if not
 unless($filename) {
-    print "No filename supplied, exiting \n";
-    usage();
-    exit;
+    print "Error: No filename supplied, exiting \n";
+    usageShort();
 }
 
 unless($pathname) {
-    print "No path supplied, exiting \n";
-    usage();
-    exit;
+    print "Error: No path supplied, exiting \n";
+    usageShort();
 }
 
 if(hostname() eq $hostname && !(-e "$pathname/$filename")) {
-    print "Hostname = this machine, but file does not exist, exiting \n";
-    usage();
-    exit;
+    print "Error: Hostname = this machine, but file does not exist, exiting \n";
+    usageShort();
 } 
 
 #If we are running this on same host as the file can find out filesize as a fallback
 unless($filesize) {
-    print "No filesize supplied or filesize=0, ";
+    print "Warning: No filesize supplied or filesize=0, ";
     if(hostname() ne $hostname) {
 	print "exiting \n";
-        usage();
-	exit;
+        usageShort();
     } else {
 	print "but hostname = this machine: using size of file on disk \n"; 
 	$filesize = -s "$pathname/$filename";
@@ -188,33 +224,31 @@ unless($filesize) {
 }
 
 unless($type) {
-    print "No type specified, exiting \n";
-    usage();
-    exit;
+    print "Error: No type specified, exiting \n";
+    usageShort();
 }
 
 
 #Depending on type check for different required parameters
 if($type eq "streamer") {
     unless( $runnumber && $lumisection != -1 && $nevents && $appname && $appversion && $stream && $setuplabel ne 'default') {
-	print "For streamer files need runnumber, lumisection, num events, app name, app version, stream, setup label, and (optionally) index specified\n";
-        usage();
-	exit;
+	print "Error: For streamer files need runnumber, lumisection, num events, app name, app version, stream, setup label, and index specified\n";
+        usageShort();
     }
 } elsif($type eq "edm") {
     unless($runnumber && $lumisection != -1 && $nevents && $appname && $appversion && $stream && $setuplabel ne 'default') {
-	print "For edm files need runnumber, lumisection, num events, app name, app version, setup label, and stream specified\n";
-        usage();
-	exit;
+	print "Error: For edm files need runnumber, lumisection, num events, app name, app version, setup label, and stream specified\n";
+        usageShort();
     }
 } elsif($type eq "lumi") {
     unless( $runnumber && $lumisection != -1 && $appname && $appversion) {
-	print "For lumi files need runnumber, lumisection, app name, and app version specified.\n";
-        usage();
-	exit;
+	print "Error: For lumi files need runnumber, lumisection, app name, and app version specified.\n";
+        usageShort();
     }
 } else {
-    print "Warning: File type not a recognized type; special set of parameters is not enforced. Good luck.\n";
+    print "Error: File type not a recognized type.\n" .
+          "Recognized types are streamer, edm, and lumi";
+    usageShort();
 }
 
 #Setuplabel used to be called DATASET in transfer system but name was misleading
@@ -295,10 +329,25 @@ $debug && print "DB connection set up succesfully \n";
 $debug && print "Preparing DB inserts\n";
 my $createHan = $dbh->prepare($SQLcreate) or die("Error: Prepare failed, $dbh->errstr \n");
 my $injectHan = $dbh->prepare($SQLinject) or die("Error: Prepare failed, $dbh->errstr \n");
+
+my $dbErr;
 $debug && print "Inserting into FILES_CREATED \n";
-my $rowsCreate = $createHan->execute() or die("Error: Insert into FILES_CREATED failed: returned $createHan->errstr \n");
+my $rowsCreate = $createHan->execute() or $dbErr=$createHan->errstr;
+if(defined($dbErr)) {
+    print "Error: Insert into FILES_CREATED failed.\n";
+    if($dbErr =~ /unique constraint/ ) { print "File was already in the database.\n";}
+    print "Error string from db was $dbErr\n";
+    exit;
+}
+
 $debug && print "Inserting into FILES_INJECTED \n";
-my $rowsInject = $injectHan->execute() or die("Error: Insert into FILES_INJECTED failed: returned $injectHan->errstr \n");
+my $rowsInject = $injectHan->execute() or $dbErr=$injectHan->errstr;
+if(defined($dbErr)) {
+    print "Error: Insert into FILES_INJECTED failed.\n";
+    if($dbErr =~ /unique constraint/ ) { print "File was already in the database.\n";}
+    print "Error string from db was $dbErr\n";
+    exit;
+}
 
 #Check return values before calling notification script.  Want to make sure no DB errors
 if($rowsCreate==1 && $rowsInject==1) {
