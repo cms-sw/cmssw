41306dcc18f0c4591e8a82948ddbd4363ea7d0f5
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-06-10 14:22:01 +0000

    Formatted

--- EventFilter/StorageManager/scripts/operations/mk_satamap.pl
+++ EventFilter/StorageManager/scripts/operations/mk_satamap.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl -w
+# $Id:$
+#
+# Make sata mapping of volume serial numbers, suitable for intput to "makeall" script
+#
+
 use strict;
-# make sata mapping of volume serial numbers, suitable for intput to "makeall" script
 
 #define list of Satabeasts (order implicitly matched to PC's in @nodeAssign):
 my @satabeasts = (
@@ -17,27 +21,27 @@ my @nodeAssign = (
 		  "srv-C2C07-15",
 		  "srv-C2C07-16");
 
-
 my $nbeast=0;
 my $VolPerSata;
 my @VolumeID;
 my @VolumeSerial;
+
 foreach my $ibeast ( @satabeasts ) {
     
-# Get Vol Name:
+    # get volume name
     my $command = " lynx -connect_timeout=2 -dump -auth=USER:mickey2mouse 'http://$ibeast/hluninf.asp?detail&rpath=/vwlunmap.asp' | grep 'Volume name'";
     my @volIDs = `$command`;
     
     $VolPerSata=0;
-foreach my $volline ( @volIDs) {    
-    chomp($volline);    
-    my @split = split(m/name/, $volline);
-    $VolumeID[$VolPerSata] = $split[1];
-    $VolPerSata++;
-}
+    foreach my $volline ( @volIDs) {    
+        chomp($volline);    
+        my @split = split(m/name/, $volline);
+        $VolumeID[$VolPerSata] = $split[1];
+        $VolPerSata++;
+    }
     
-#Get Vol Serial Number:
- $command = " lynx -connect_timeout=2 -dump -auth=USER:mickey2mouse 'http://$ibeast/hluninf.asp?detail&rpath=/vwlunmap.asp'  | grep 'Volume serial number'";
+    #get vol serial number
+    $command = " lynx -connect_timeout=2 -dump -auth=USER:mickey2mouse 'http://$ibeast/hluninf.asp?detail&rpath=/vwlunmap.asp'  | grep 'Volume serial number'";
     my @volSerl = `$command`;
     
     my $SerialPerSata=0;
@@ -49,33 +53,27 @@ foreach my $volline ( @volIDs) {
 	$SerialPerSata++;
     }
     
-# if there is a mismatch, go into error:
+    # if there is a mismatch, go into error:
     if($VolPerSata != $SerialPerSata){
 	print "\n !!!!! Something is VERY WRONG: Number of Vol Names NOT Equal to Number of ID's !!!!!! \n";
 	print "\n !!!!!          $VolPerSata Vols  vs  $SerialPerSata Serial No.        !!!!!! \n\n";
 	exit;
     }
     
-    
-    
     $nbeast++;
     my $nvol = $VolPerSata;
-    
-    
-#------------dump everything together:
+
+    #------------dump everything together:
     for( my $i=0; $i<$VolPerSata; $i++){
 	
-# compute index to point to right PC as owner of Satabeast
-# ASSUME half of all volumes go to each PC
+        # compute index to point to right PC as owner of Satabeast
+        # ASSUME half of all volumes go to each PC
 	my $mod;   {use integer;   $mod = (2*$i)/$VolPerSata; }
 	my $pcIndx = 2*($nbeast-1)+$mod;
-#Switch to lower case:    
+        # switch to lower case:    
 	$VolumeID[$i] =~ s/([a-zA-Z]*)([0-9][0-9])([a-zA-Z]+)([0-9]+)(.*)/$1$2 $3$4 $5/;
 	print "$VolumeID[$i]  $VolumeSerial[$i]  $nodeAssign[$pcIndx] \n"; 
     }
-    
-
 }
 
 exit;
-
