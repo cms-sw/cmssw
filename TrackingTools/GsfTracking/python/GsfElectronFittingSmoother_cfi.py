ec1f64def45205981796063f19b9fe413c1fbc96
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-06-10 05:47:58 +0000

    placing GsfElectronFittingSmoother_cfi.py in the right directory

--- TrackingTools/GsfTracking/python/GsfElectronFittingSmoother_cfi.py
+++ TrackingTools/GsfTracking/python/GsfElectronFittingSmoother_cfi.py
@@ -1,12 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-GsfElectronFittingSmoother = cms.ESProducer("KFFittingSmootherESProducer",
-    EstimateCut = cms.double(-1.0),
-    Fitter = cms.string('GsfTrajectoryFitter'),
-    MinNumberOfHits = cms.int32(5),
-    Smoother = cms.string('GsfTrajectorySmoother'),
-    ComponentName = cms.string('GsfElectronFittingSmoother'),
-    RejectTracks = cms.bool(True)
-)
+import TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi
+GsfElectronFittingSmoother = TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi.KFFittingSmoother.clone()
+GsfElectronFittingSmoother.ComponentName = 'GsfElectronFittingSmoother'
+GsfElectronFittingSmoother.Fitter = 'GsfTrajectoryFitter'
+GsfElectronFittingSmoother.Smoother = 'GsfTrajectorySmoother'
 
 
