df8b9745f3e37fb42821d715175153c9ed54e070
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2008-06-10 09:25:34 +0000

    include the ValueMap module

--- RecoEgamma/ElectronIdentification/plugins/SealModule.cc
+++ RecoEgamma/ElectronIdentification/plugins/SealModule.cc
@@ -9,16 +9,13 @@
 #include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
 #include "PhysicsTools/UtilAlgos/interface/EventSetupInitTrait.h"
 
-//#include "RecoEgamma/ElectronIdentification/interface/ElectronIDProducer.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronIDSelector.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronIDSelectorCutBased.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronIDSelectorNeuralNet.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronIDSelectorLikelihood.h"
-
 typedef ElectronIDSelector<ElectronIDSelectorCutBased>   EleIdCutBasedSel;
 typedef ElectronIDSelector<ElectronIDSelectorNeuralNet>  EleIdNeuralNetSel;
 typedef ElectronIDSelector<ElectronIDSelectorLikelihood> EleIdLikelihoodSel;
-//typedef ObjectSelector<EleIdCutBasedSel> EleIdCutBased ;
 typedef ObjectSelector<
           EleIdCutBasedSel, 
           edm::RefVector<reco::PixelMatchGsfElectronCollection> 
@@ -31,17 +28,17 @@ typedef ObjectSelector<
           EleIdLikelihoodSel, 
           edm::RefVector<reco::PixelMatchGsfElectronCollection> 
          > EleIdLikelihoodRef ;
-
-
-//DEFINE_SEAL_MODULE();
-
-//DEFINE_ANOTHER_FWK_MODULE(ElectronIDProducer);
-
-//DEFINE_ANOTHER_FWK_MODULE(EleIdCutBased);
 DEFINE_FWK_MODULE(EleIdCutBasedRef);
 DEFINE_ANOTHER_FWK_MODULE(EleIdNeuralNetRef);
 DEFINE_ANOTHER_FWK_MODULE(EleIdLikelihoodRef);
 
+#include "RecoEgamma/ElectronIdentification/plugins/ElectronIDExternalProducer.h"
+typedef ElectronIDExternalProducer<ElectronIDSelectorCutBased>   EleIdCutBasedExtProducer;
+typedef ElectronIDExternalProducer<ElectronIDSelectorNeuralNet>  EleIdNeuralNetExtProducer;
+typedef ElectronIDExternalProducer<ElectronIDSelectorLikelihood> EleIdLikelihoodExtProducer;
+DEFINE_ANOTHER_FWK_MODULE(EleIdCutBasedExtProducer);
+DEFINE_ANOTHER_FWK_MODULE(EleIdNeuralNetExtProducer);
+DEFINE_ANOTHER_FWK_MODULE(EleIdLikelihoodExtProducer);
 
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronLikelihoodESSource.h"
