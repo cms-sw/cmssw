95197c99e5464e43d1e16c240eab641f4cbeb0fe
riccardo <sha1-a8fe582e28bc10906e7eb2fd4c1098a4a0c0276f@cern.ch>
2008-06-11 15:46:45 +0000

    Update of the TrackingParticle class to handle both Tracker and Muon PSimHit's and keep the backward compatibility

--- SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h
+++ SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h
@@ -12,6 +12,7 @@
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
 #include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 #include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "DataFormats/DetId/interface/DetId.h"
 
 namespace HepMC {
   class GenParticle;
@@ -19,86 +20,110 @@ namespace HepMC {
 class TrackingVertex;
 
 class TrackingParticle : public reco::Particle {
-
+  
   friend std::ostream& operator<< (std::ostream& s, const TrackingParticle & tp);
-
- public:
+  
+public:
   /// reference to HepMC::GenParticle
   typedef edm::RefVector<edm::HepMCProduct, HepMC::GenParticle > GenParticleRefVector;
   typedef edm::Ref<edm::HepMCProduct, HepMC::GenParticle >       GenParticleRef;
   typedef GenParticleRefVector::iterator                         genp_iterator;
-  typedef       std::vector<SimTrack>::const_iterator             g4t_iterator;
-
+  typedef std::vector<SimTrack>::const_iterator                  g4t_iterator;
+  
   typedef std::vector<TrackingVertex>                TrackingVertexCollection;
   typedef edm::Ref<TrackingVertexCollection>         TrackingVertexRef;
   typedef edm::RefVector<TrackingVertexCollection>   TrackingVertexRefVector;
   typedef TrackingVertexRefVector::iterator   tv_iterator;
-
+  
   /// default constructor
-  TrackingParticle() { }
+  TrackingParticle() { subdetVectorFill_ = false; }
   // destructor
   ~TrackingParticle();
   /// constructor from pointer to generator particle
   TrackingParticle( char q, const LorentzVector & p4, const Point & vtx,
                     double t, const int pdgId,  const int status, const EncodedEventId eventId);
-
+  
   /// PDG id, signal source, crossing number
   int pdgId() const { return pdgId_; }
   EncodedEventId eventId() const { return eventId_; }
-
+  
   ///iterators
   genp_iterator genParticle_begin() const;
   genp_iterator genParticle_end()   const;
   g4t_iterator  g4Track_begin()     const;
   g4t_iterator  g4Track_end()       const;
-
+  
+  /** Returns the begin of the vector of ALL the PSimHits of the TrackingParticle */
   const std::vector<PSimHit>::const_iterator  pSimHit_begin() const;
+  /** Returns the end of the vector of ALL the PSimHits of the TrackingParticle */
   const std::vector<PSimHit>::const_iterator  pSimHit_end()   const;
-
-// Setters for G4 and HepMC
+  
+  /** Returns the begin of the vector the Tracker PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>::const_iterator  trackerPSimHit_begin();
+  /** Returns the end of the vector the Tracker PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>::const_iterator  trackerPSimHit_end();
+  
+  /** Returns the begin of the vector the Muon PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>::const_iterator  muonPSimHit_begin();
+  /** Returns the end of the vector the Muon PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>::const_iterator  muonPSimHit_end();
+  
+  // Setters for G4 and HepMC
   void addG4Track(    const SimTrack&);
   void addGenParticle(const GenParticleRef&);
-
+  
   void addPSimHit(const PSimHit&);
   void setParentVertex(const TrackingVertexRef&);
-  void  addDecayVertex(const TrackingVertexRef&);
+  void addDecayVertex(const TrackingVertexRef&);
   void clearParentVertex();
   void clearDecayVertices();
   void setMatchedHit(const int&);
   void setVertex(const Point & vtx, double t);
+  void fillSubDetHitVectors();
 
-// Getters for Embd and Sim Tracks
-  const GenParticleRefVector&   genParticle() const { return genParticles_; }
-  const std::vector<SimTrack>&     g4Tracks() const { return g4Tracks_ ;    }
-  const std::vector<PSimHit>&  trackPSimHit() const { return trackPSimHit_; }
-  const TrackingVertexRef&     parentVertex() const { return parentVertex_; }
+  // Getters for Embd and Sim Tracks
+  const GenParticleRefVector&     genParticle() const { return genParticles_; }
+  const std::vector<SimTrack>&       g4Tracks() const { return g4Tracks_ ;    }
+  const TrackingVertexRef&       parentVertex() const { return parentVertex_; }
+  /** The vector of ALL the PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>&    trackPSimHit() const { return trackPSimHit_; }
 
-// Accessors for vector of decay vertices
+  // Sub-detectors details
+  /** The vector of the Tracker PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>& trackerPSimHit();
+  /** The vector of the Muon PSimHits of the TrackingParticle */
+  const std::vector<PSimHit>&    muonPSimHit();
+  
+  // Accessors for vector of decay vertices
   const TrackingVertexRefVector& decayVertices() const { return decayVertices_; }
   tv_iterator decayVertices_begin()       const { return decayVertices_.begin(); }
   tv_iterator decayVertices_end()         const { return decayVertices_.end(); }
   int matchedHit() const {return matchedHit_;}
-
- private:
+  
+private:
   /// production time
   double t_;
   /// PDG identifier, signal source, crossing number
   int pdgId_;
   EncodedEventId eventId_;
-
-  /// Number of Hit to be used for tracking Study
+  
+  /// Total Number of Hits belonging to the TrackingParticle
   int matchedHit_;
-
+  
   /// references to G4 and HepMC tracks
   std::vector<SimTrack> g4Tracks_;
   GenParticleRefVector  genParticles_;
-
-//  TrackPSimHitRefVector trackPSimHit_;
+  
+  //  TrackPSimHitRefVector trackPSimHit_;
   std::vector<PSimHit> trackPSimHit_;
-
-// Source and decay vertices
+  // TrackPSimHitRefVector for Tracker and Muon (not persistent)
+  std::vector<PSimHit> trackerPSimHit_;
+  std::vector<PSimHit> muonPSimHit_;
+  bool subdetVectorFill_;
+  
+  // Source and decay vertices
   TrackingVertexRef       parentVertex_;
-  TrackingVertexRefVector  decayVertices_;
+  TrackingVertexRefVector decayVertices_;
 };
 
 #endif // SimDataFormats_TrackingParticle_H
--- SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
+++ SimDataFormats/TrackingAnalysis/src/TrackingParticle.cc
@@ -7,7 +7,7 @@ typedef TrackingVertexRefVector::iterator          tv_iterator;
 
 TrackingParticle::TrackingParticle( char q, const LorentzVector & p4, const Point & vtx,
                                     double t, const int pdgId, const int status, const EncodedEventId eventId) :
-  reco::Particle( q, p4, vtx,pdgId,status ), t_( t ), pdgId_( pdgId ), eventId_( eventId ){
+  reco::Particle( q, p4, vtx,pdgId,status ), t_( t ), pdgId_( pdgId ), eventId_( eventId ), subdetVectorFill_( false ) {
 }
 
 TrackingParticle::~TrackingParticle() {
@@ -26,35 +26,123 @@ void TrackingParticle::addPSimHit( const PSimHit& hit){
 }
 
 TrackingParticle::genp_iterator TrackingParticle::genParticle_begin() const {
-   return genParticles_.begin();
+  return genParticles_.begin();
 }
 
 TrackingParticle::genp_iterator TrackingParticle::genParticle_end() const {
-   return genParticles_.end();
+  return genParticles_.end();
 }
 
 TrackingParticle::g4t_iterator TrackingParticle::g4Track_begin() const {
-    return g4Tracks_.begin();
+  return g4Tracks_.begin();
 }
 
 TrackingParticle::g4t_iterator TrackingParticle::g4Track_end() const {
-    return g4Tracks_.end();
+  return g4Tracks_.end();
 }
 
 const std::vector<PSimHit>::const_iterator TrackingParticle::pSimHit_begin() const {
-    return trackPSimHit_.begin();
+  return trackPSimHit_.begin();
 }
 
 const std::vector<PSimHit>::const_iterator TrackingParticle::pSimHit_end() const {
-    return trackPSimHit_.end();
+  return trackPSimHit_.end();
 }
+
+const std::vector<PSimHit>::const_iterator TrackingParticle::trackerPSimHit_begin() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return trackerPSimHit_.begin();
+}
+
+const std::vector<PSimHit>::const_iterator TrackingParticle::trackerPSimHit_end() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return trackerPSimHit_.end();
+}
+
+const std::vector<PSimHit>::const_iterator TrackingParticle::muonPSimHit_begin() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return muonPSimHit_.begin();
+}
+
+const std::vector<PSimHit>::const_iterator TrackingParticle::muonPSimHit_end() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return muonPSimHit_.end();
+}
+
+const std::vector<PSimHit>& TrackingParticle::trackerPSimHit() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return trackerPSimHit_;
+}
+
+const std::vector<PSimHit>& TrackingParticle::muonPSimHit() {
+  /* do it because PSimHit vectors
+     trackerPSimHit_ and muonPSimHit_
+     are transient */
+  if(!subdetVectorFill_)
+    fillSubDetHitVectors();
+  
+  return muonPSimHit_;
+}
+
+void TrackingParticle::fillSubDetHitVectors() {
+  trackerPSimHit_.clear();
+  muonPSimHit_.clear();
+  subdetVectorFill_ = true;
+  //
+  for(std::vector<PSimHit>::const_iterator aHit = trackPSimHit_.begin(); aHit != trackPSimHit_.end(); ++aHit) {
+    unsigned int subDet_enum = DetId( (uint32_t)((*aHit).detUnitId()) ).det();
+    switch (subDet_enum) {
+    case DetId::Tracker:
+      {
+	trackerPSimHit_.push_back((*aHit));
+	break;
+      }
+    case DetId::Muon:
+      {
+	muonPSimHit_.push_back((*aHit));
+	break;
+      }
+    default: 
+      {
+      std::cout << "TrackingParticle WARNING - Not Tracker or Muon Subdetector " << subDet_enum << std::endl;
+      break;
+      }
+    } // switch
+  }
+}
+
 
 void TrackingParticle::setParentVertex(const TrackingVertexRef &ref) {
   parentVertex_ = ref;
 }
 
 void TrackingParticle::addDecayVertex(const TrackingVertexRef &ref){
-    decayVertices_.push_back(ref);
+  decayVertices_.push_back(ref);
 }
 
 void TrackingParticle::clearParentVertex() {
@@ -75,17 +163,19 @@ void TrackingParticle::setVertex(const Point & vtx, double t){
 }
 
 std::ostream& operator<< (std::ostream& s, const TrackingParticle & tp) {
-
+  
   // Compare momenta from sources
   s << "TP momentum, q, ID, & Event #: "
     << tp.p4()                      << " " << tp.charge() << " "   << tp.pdgId() << " "
     << tp.eventId().bunchCrossing() << "." << tp.eventId().event() << std::endl;
   s << " Hits for this track: " << tp.trackPSimHit().size() << std::endl;
-
+  s << "\t Tracker: " << tp.trackerPSimHit_.size() << std::endl;
+  s << "\t Muon: "    << tp.muonPSimHit_.size()    << std::endl;
+  
   for (TrackingParticle::genp_iterator hepT = tp.genParticle_begin(); hepT !=  tp.genParticle_end(); ++hepT) {
     s << " HepMC Track Momentum " << (*hepT)->momentum().mag() << std::endl;
   }
-
+  
   for (TrackingParticle::g4t_iterator g4T = tp.g4Track_begin(); g4T !=  tp.g4Track_end(); ++g4T) {
     s << " Geant Track Momentum  " << g4T->momentum() << std::endl;
     s << " Geant Track ID & type " << g4T->trackId() << " " << g4T->type() << std::endl;
@@ -100,6 +190,6 @@ std::ostream& operator<< (std::ostream& s, const TrackingParticle & tp) {
   for (tv_iterator iTV = tp.decayVertices_begin(); iTV != tp.decayVertices_end(); ++iTV) {
     s << " Decay vertices:      " << (**iTV).position() << std::endl;
   }
-
+  
   return s;
 }
