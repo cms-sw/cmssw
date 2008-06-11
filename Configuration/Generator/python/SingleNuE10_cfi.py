6e9fd1826ffa442ccec06e11814892ae22422868
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-06-11 21:38:27 +0000

    add neutrino gun

---
+++ Configuration/Generator/python/SingleNuE10_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("FlatRandomEGunSource",
+    PGunParameters = cms.untracked.PSet(
+        PartID = cms.untracked.vint32(12),
+        MaxEta = cms.untracked.double(2.5),
+        MaxPhi = cms.untracked.double(3.14159265359),
+        MinEta = cms.untracked.double(-2.5),
+        MinE = cms.untracked.double(9.99),
+        MinPhi = cms.untracked.double(-3.14159265359), ## in radians
+
+        MaxE = cms.untracked.double(10.01)
+    ),
+    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+
+    psethack = cms.string('single Nu E 10'),
+    firstRun = cms.untracked.uint32(1)
+)
+
+
