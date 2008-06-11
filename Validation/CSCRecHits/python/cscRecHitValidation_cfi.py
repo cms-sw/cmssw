7187afd951ac8873d9616378734122383241ce9b
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-11 18:41:57 +0000

    Correct InputTag is mix:g4SimHitsMuonCSCHits

--- Validation/CSCRecHits/python/cscRecHitValidation_cfi.py
+++ Validation/CSCRecHits/python/cscRecHitValidation_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-cscRecHitValidation = cms.EDFilter("CSCRecHitValidation",
-    simHitsTag = cms.InputTag("g4SimHitsMuonCSCHits"),
+cscRecHitValidation = cms.EDAnalyzer("CSCRecHitValidation",
+    simHitsTag = cms.InputTag("mix","g4SimHitsMuonCSCHits"),
     outputFile = cms.string('CSCRecHitValidation.root'),
     recHitLabel = cms.InputTag("csc2DRecHits"),
     segmentLabel = cms.InputTag("cscSegments")
--- Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
+++ Validation/MuonCSCDigis/python/cscDigiValidation_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 cscDigiValidation = cms.EDAnalyzer("CSCDigiValidation",
-    simHitsTag = cms.InputTag("g4SimHitsMuonCSCHits"),
+    simHitsTag = cms.InputTag("mix", "g4SimHitsMuonCSCHits"),
     wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     outputFile = cms.string('CSCDigiValidation.root'),
     stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
