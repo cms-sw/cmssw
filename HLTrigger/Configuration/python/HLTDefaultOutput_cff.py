dc2cd14d8ec1ed6875aa0e625a75753d2eb9b25b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-06-11 10:35:03 +0000

    Updates to also take EventContent from ConfDB.

---
+++ HLTrigger/Configuration/python/HLTDefaultOutput_cff.py
@@ -0,0 +1,10 @@
+# /dev/CMSSW_2_1_0_pre5/HLT/V31 (CMSSW_2_1_X_2008-06-10-0200_HLT1)
+
+import FWCore.ParameterSet.Config as cms
+
+block_hltDefaultOutput = cms.PSet(
+outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*', 'keep edmTriggerResults_*_*_*', 'keep triggerTriggerEvent_*_*_*', 'keep *_hltGtDigis_*_*', 'keep *_hltGctDigis_*_*', 'keep *_hltL1GtObjectMap_*_*', 'keep *_hltL1extraParticles_*_*', 'keep *_hltOfflineBeamSpot_*_*' )
+)
+
+
+
