d555c1049cf5685cdf68c98262552862f7e012ac
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-06-11 21:02:08 +0000

    Add new sequences containing isolation and photonId

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -3,6 +3,13 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaElectronProducers.electronSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.conversionSequence_cff import *
-egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
-egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
+from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
+from RecoEgamma.PhotonIdentification.photonId_cff import *
 
+egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
+egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDsequence)
+
+egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
+egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
+egammareco_withIsolation_woConvPhotons = cms.Sequence(egammareco_woConvPhotons*egammaIsolationSequence)
+egammareco_withPhotonID = cms.Sequence(egammareco*photonIDSequence)
