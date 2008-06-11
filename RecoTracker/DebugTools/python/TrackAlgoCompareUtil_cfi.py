dbef6ef2204ff665c03111b88f7372b858f64e99
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-06-11 12:30:29 +0000

    syncronize with cfi file and correct string error

--- RecoTracker/DebugTools/python/TrackAlgoCompareUtil_cfi.py
+++ RecoTracker/DebugTools/python/TrackAlgoCompareUtil_cfi.py
@@ -1,21 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 trackAlgoCompare = cms.EDFilter("TrackAlgoCompareUtil",
-
-    trackLabel_algoA = cms.InputTag("generalTracks"),
-    trackLabel_algoB = cms.InputTag("rsWithMaterialTracks"),
-    vertexLabel_algoA = cms.InputTag("offlinePrimaryVerticesFromCTFTracks"),
-    vertexLabel_algoB = cms.InputTag("offlinePrimaryVerticesFromCTFTracks"),
-    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
-    
-    trackingVertexLabel = cms.InputTag("mergedtruth","MergedTrackTruth"),
-    trackingParticleLabel_effic = cms.InputTag("mergedtruth","MergedTrackTruth"),
-    trackingParticleLabel_fakes = cms.InputTag("mergedtruth","MergedTrackTruth"),
-
-    assocLabel = cms.untracked.string('TrackAssociatorByHits'),
-    UseAssociators = cms.bool("true"),
     associatormap_algoA = cms.InputTag("trackingParticleRecoTrackAsssociation"),
-    associatormap_algoB = cms.InputTag("assoc2secStepTk")
+    vertexLabel_algoB = cms.InputTag("offlinePrimaryVerticesFromCTFTracks"),
+    vertexLabel_algoA = cms.InputTag("offlinePrimaryVerticesFromCTFTracks"),
+    associatormap_algoB = cms.InputTag("assoc2secStepTk"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    UseAssociators = cms.bool(False),
+    trackLabel_algoB = cms.InputTag("secStep"),
+    trackLabel_algoA = cms.InputTag("generalTracks"),
+    assocLabel = cms.untracked.string('TrackAssociatorByHits'),
+    trackingParticleLabel_effic = cms.InputTag("mergedtruth","MergedTrackTruth"),
+    trackingParticleLabel_fakes = cms.InputTag("mergedtruth","MergedTrackTruth")
 )
+
 
 
