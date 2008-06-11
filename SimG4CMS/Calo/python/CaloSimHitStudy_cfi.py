48504d25bef50c85bcc2cc8b90f11404bba0b59b
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-11 20:18:16 +0000

    update py configs

--- SimG4CMS/Calo/python/CaloSimHitStudy_cfi.py
+++ SimG4CMS/Calo/python/CaloSimHitStudy_cfi.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 caloSimHitStudy = cms.EDFilter("CaloSimHitStudy",
-    ModuleLabel  = cms.untracked.string('g4SimHits'),
-    EBCollection = cms.untracked.string('EcalHitsEB'),
-    EECollection = cms.untracked.string('EcalHitsEE'),
-    ESCollection = cms.untracked.string('EcalHitsES'),
     HCCollection = cms.untracked.string('HcalHits'),
-    MaxEnergy    = cms.untracked.double(50.0)
+    ModuleLabel = cms.untracked.string('g4SimHits'),
+    MaxEnergy = cms.untracked.double(50.0),
+    EECollection = cms.untracked.string('EcalHitsEE'),
+    EBCollection = cms.untracked.string('EcalHitsEB'),
+    ESCollection = cms.untracked.string('EcalHitsES')
 )
+
+
