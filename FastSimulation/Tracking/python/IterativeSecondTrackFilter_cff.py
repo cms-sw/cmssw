e15be27164ac86bed5346de44b92542212a1f1d3
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2008-06-11 15:09:10 +0000

    vertex filter is now cloned

--- FastSimulation/Tracking/python/IterativeSecondTrackFilter_cff.py
+++ FastSimulation/Tracking/python/IterativeSecondTrackFilter_cff.py
@@ -1,17 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-secStep = cms.EDFilter("VertexFilter",
-    TrackAlgorithm = cms.string('iter2'),
-    recVertices = cms.InputTag("pixelVertices"),
-    MinHits = cms.int32(3),
-    DistRhoFromVertex = cms.double(0.1),
-    DistZFromVertex = cms.double(0.4),
-    recTracks = cms.InputTag("iterativeSecondTrackMerging"),
-    UseQuality = cms.bool(True),
-    ChiCut = cms.double(130.0),
-    TrackQuality = cms.string('highPurity'),
-    VertexCut = cms.bool(True)
-)
-
+import RecoParticleFlow.PFTracking.vertexFilter_cfi
+secStep = RecoParticleFlow.PFTracking.vertexFilter_cfi.vertFilter.clone()
 iterativeSecondTrackFiltering = cms.Sequence(secStep)
+secStep.recTracks = cms.InputTag("iterativeSecondTrackMerging")
+secStep.TrackAlgorithm = 'iter2'
+
 
--- FastSimulation/Tracking/python/IterativeThirdTrackFilter_cff.py
+++ FastSimulation/Tracking/python/IterativeThirdTrackFilter_cff.py
@@ -1,17 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-thStep = cms.EDFilter("VertexFilter",
-    TrackAlgorithm = cms.string('iter3'),
-    recVertices = cms.InputTag("pixelVertices"),
-    MinHits = cms.int32(3),
-    DistRhoFromVertex = cms.double(0.1),
-    DistZFromVertex = cms.double(0.1),
-    recTracks = cms.InputTag("iterativeThirdTrackMerging"),
-    UseQuality = cms.bool(True),
-    ChiCut = cms.double(250000.0),
-    TrackQuality = cms.string('highPurity'),
-    VertexCut = cms.bool(True)
-)
-
+import RecoParticleFlow.PFTracking.vertexFilter_cfi
+thStep = RecoParticleFlow.PFTracking.vertexFilter_cfi.vertFilter.clone()
 iterativeThirdTrackFiltering = cms.Sequence(thStep)
+thStep.recTracks = cms.InputTag("iterativeThirdTrackMerging")
+thStep.TrackAlgorithm = 'iter3'
+thStep.DistZFromVertex = 0.1
+
 
