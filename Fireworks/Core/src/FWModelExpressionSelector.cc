7d69f2cd8fcc847d9327b4e02169b29f86aae770
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-06-12 20:29:59 +0000

    use new Cint helper functions to make code safer and faster

--- Fireworks/Core/src/FWModelExpressionSelector.cc
+++ Fireworks/Core/src/FWModelExpressionSelector.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Wed Jan 23 10:37:22 EST 2008
-// $Id$
+// $Id: FWModelExpressionSelector.cc,v 1.1 2008/01/24 00:30:30 chrjones Exp $
 //
 
 // system include files
@@ -23,6 +23,7 @@
 #include "Fireworks/Core/interface/FWModelExpressionSelector.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/FWModelChangeManager.h"
+#include "Fireworks/Core/src/fwCintInterfaces.h"
 
 
 //
@@ -72,29 +73,47 @@ FWModelExpressionSelector::FWModelExpressionSelector()
 bool 
 FWModelExpressionSelector::select(FWEventItem* iItem, const std::string& iExpression) const
 {
+   const std::string variable(std::string("(*((const ")+iItem->modelType()->GetName()+"*)(fwGetObjectPtr())))");
+   static boost::regex const reVarName("\\$");
+   
+   std::string fullExpression(std::string("fwSetInCint((long)(")+iExpression+"))");
+   
+   fullExpression = boost::regex_replace(fullExpression,reVarName,variable);
+   
+   
+   /*
    const std::string modelPrefix= std::string("(*((const ")+iItem->modelType()->GetName()+"*)(";
    const std::string modelPostfix(")))");
    
    const std::string expression(std::string("(Long_t)(")+iExpression+")");
    
    static boost::regex const reVarName("\\$");
+    */
    bool returnValue = true;
 
    //we are probably making many changes
    FWChangeSentry sentry(*(iItem->changeManager()));
    for( unsigned int index = 0; index < iItem->size(); ++index ) {
+      fwSetObjectPtr(iItem->modelData(index));
+      fwCintReturnType()=kFWCintReturnNoReturn;
+      Int_t error = 0;
+      gROOT->ProcessLineFast(fullExpression.c_str(),
+                             &error);
+      
+      /*
       std::stringstream fullVariable;
       fullVariable <<modelPrefix<<iItem->modelData(index)<<modelPostfix;
       Int_t error = 0;
       Long_t value = gROOT->ProcessLineFast(boost::regex_replace(expression,reVarName,fullVariable.str()).c_str(),
                                             &error);
-      if(TInterpreter::kNoError != error) {
+       */
+      if(TInterpreter::kNoError != error || fwCintReturnType() == kFWCintReturnNoReturn) {
          if(index==0) {
             return false;
          }
          returnValue = false;
       } else {
-         if(value) {
+         if(fwGetFromCintLong()) {
             iItem->select(index);
          }
       }
