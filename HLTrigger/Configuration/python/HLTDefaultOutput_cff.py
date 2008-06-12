b98ec1b87d0cd31f459bcd52b0497e5e78f0977b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-06-12 12:23:43 +0000

    Add FEDRawDataCollection to EventContent.

--- HLTrigger/Configuration/python/HLTDefaultOutput_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutput_cff.py
@@ -1,9 +1,9 @@
-# /dev/CMSSW_2_1_0_pre5/HLT/V40 (CMSSW_2_1_X_2008-06-10-0200_HLT1)
+# /dev/CMSSW_2_1_0_pre5/HLT/V41 (CMSSW_2_1_X_2008-06-10-0200_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 block_hltDefaultOutput = cms.PSet(
-outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*', 'keep edmTriggerResults_*_*_*', 'keep triggerTriggerEvent_*_*_*', 'keep *_hltGtDigis_*_*', 'keep *_hltGctDigis_*_*', 'keep *_hltL1GtObjectMap_*_*', 'keep *_hltL1extraParticles_*_*', 'keep *_hltOfflineBeamSpot_*_*' )
+outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*', 'keep FEDRawDataCollection_*_*_*', 'keep edmTriggerResults_*_*_*', 'keep triggerTriggerEvent_*_*_*', 'keep *_hltGtDigis_*_*', 'keep *_hltGctDigis_*_*', 'keep *_hltL1GtObjectMap_*_*', 'keep *_hltL1extraParticles_*_*', 'keep *_hltOfflineBeamSpot_*_*' )
 )
 
 
