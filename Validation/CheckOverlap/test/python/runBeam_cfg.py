fe5000c5afdc8532148bc55fef45d8a4afe6d21a
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-06-12 10:55:40 +0000

    Add python files

---
+++ Validation/CheckOverlap/test/python/runBeam_cfg.py
@@ -0,0 +1,65 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+process.load("SimGeneral.HepPDTESSource.pdt_cfi")
+
+process.load("Geometry.CMSCommonData.cmsAllGeometryXML_cfi")
+
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.load("SimG4Core.Application.g4SimHits_cfi")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        G4cout = cms.untracked.PSet(
+            limit = cms.untracked.int32(-1)
+        ),
+        G4cerr = cms.untracked.PSet(
+            limit = cms.untracked.int32(-1)
+        )
+    ),
+    categories = cms.untracked.vstring('G4cout', 
+        'G4cerr'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    moduleSeeds = cms.PSet(
+        g4SimHits = cms.untracked.uint32(9876),
+        VtxSmeared = cms.untracked.uint32(12345)
+    ),
+    sourceSeed = cms.untracked.uint32(98765)
+)
+
+process.source = cms.Source("FlatRandomEGunSource",
+    PGunParameters = cms.untracked.PSet(
+        PartID = cms.untracked.vint32(211),
+        MaxEta = cms.untracked.double(3.5),
+        MaxPhi = cms.untracked.double(3.14159265359),
+        MinEta = cms.untracked.double(-3.5),
+        MinE = cms.untracked.double(9.99),
+        MinPhi = cms.untracked.double(-3.14159265359),
+        MaxE = cms.untracked.double(10.01)
+    ),
+    Verbosity = cms.untracked.int32(0)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.p1 = cms.Path(process.g4SimHits)
+process.g4SimHits.UseMagneticField = False
+process.g4SimHits.Physics.type = 'SimG4Core/Physics/DummyPhysics'
+process.g4SimHits.Physics.DummyEMPhysics = True
+process.g4SimHits.Generator.HepMCProductLabel = 'source'
+process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
+    Resolution = cms.untracked.int32(1000),
+    type = cms.string('CheckOverlap'),
+    NodeName = cms.untracked.string('BEAM')
+))
+
