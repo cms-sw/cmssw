7516245fbb3a1104e0daf87a26ae180350eff0b0
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-06-12 07:45:06 +0000

    removing useless config

--- HLTrigger/Muon/python/CommonModules_2x1033_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HLTrigger.Muon.CommonModules_1032_cff import *
-
--- HLTrigger/Muon/python/CommonModules_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# RecoMuon flux ##########################################################
-from HLTrigger.Muon.CommonModules_1032_cff import *
-
