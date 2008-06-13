ec79a4dfe0c0082ef82a32d2996d3afdb4dbe081
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-06-13 18:31:44 +0000

    adjust the l1 name

--- Validation/Configuration/python/SingleMuPt10.py
+++ Validation/Configuration/python/SingleMuPt10.py
@@ -8,7 +8,7 @@ def customise(process):
     process.schedule.append(process.generation_step)
     process.schedule.append(process.simulation_step)
     process.schedule.append(process.digitisation_step)
-    process.schedule.append(process.L1_Emulation)
+    process.schedule.append(process.L1simulation_step)
     process.schedule.append(process.digi2raw_step)
     process.schedule.append(process.raw2digi_step)
     process.schedule.append(process.reconstruction_step)
