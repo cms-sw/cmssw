203531f43a67a1ef53f08940877d4db7fbf40144
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2008-06-13 10:10:38 +0000

    fixed conf file and sync python configuration file

--- Validation/RecoTrack/python/SiPixelTrackingRecHitsValid_cfi.py
+++ Validation/RecoTrack/python/SiPixelTrackingRecHitsValid_cfi.py
@@ -1,16 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
 PixelTrackingRecHitsValid = cms.EDFilter("SiPixelTrackingRecHitsValid",
-    src = cms.untracked.string('generalTracks'),
-    outputFile = cms.untracked.string('pixeltrackingrechitshist.root'),
-    Fitter = cms.string('KFFittingSmoother'),
-    # do we check that the simHit associated with recHit is of the expected particle type ?
-    checkType = cms.bool(True),
-    MTCCtrack = cms.bool(False),
-    TTRHBuilder = cms.string('WithTrackAngle'),
-    # the type of particle that the simHit associated with recHits should be
-    genType = cms.int32(13),
-    Propagator = cms.string('PropagatorWithMaterial')
-)
+                                         src = cms.untracked.string('TrackRefitter'),
+                                         outputFile = cms.untracked.string('pixeltrackingrechitshist.root'),
+                                         Fitter = cms.string('KFFittingSmoother'),
+                                         # do we check that the simHit associated with recHit is of the expected particle type ?
+                                         checkType = cms.bool(True),
+                                         MTCCtrack = cms.bool(False),
+                                         TTRHBuilder = cms.string('WithAngleAndTemplate'),
+                                         # the type of particle that the simHit associated with recHits should be
+                                         genType = cms.int32(13),
+                                         associatePixel = cms.bool(True),
+                                         associateRecoTracks = cms.bool(False),
+                                         associateStrip = cms.bool(False),
+                                         ROUList = cms.vstring('g4SimHitsTrackerHitsPixelBarrelLowTof', 
+                                                               'g4SimHitsTrackerHitsPixelBarrelHighTof', 
+                                                               'g4SimHitsTrackerHitsPixelEndcapLowTof', 
+                                                               'g4SimHitsTrackerHitsPixelEndcapHighTof'),
+                                         Propagator = cms.string('PropagatorWithMaterial')
+                                         )
 
 
