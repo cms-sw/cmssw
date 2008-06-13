56ce7acfacec2fd5ac089c599cfa094b39664d11
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-06-13 23:28:47 +0000

    ProxyBuilders no longer need dictionaries (in fact, they never did)

--- Fireworks/Calo/src/classes.h
+++ Fireworks/Calo/src/classes.h
@@ -1,13 +1,1 @@
 //Add includes for your classes here
-#include "Fireworks/Calo/interface/CaloProxyLegoBuilder.h"
-#include "Fireworks/Calo/interface/ECalCaloTowerProxy3DLegoBuilder.h"
-#include "Fireworks/Calo/interface/HCalCaloTowerProxy3DLegoBuilder.h"
-#include "Fireworks/Calo/interface/CaloJetProxyTH2LegoBuilder.h"
-#include "Fireworks/Calo/interface/CaloJetSelectedProxyTH2LegoBuilder.h"
-#include "Fireworks/Calo/interface/CaloJetProxyEveLegoBuilder.h"
-#include "Fireworks/Calo/interface/CaloJetProxyRhoPhiZ2DBuilder.h"
-#include "Fireworks/Calo/interface/L1TrigProxyEveLegoBuilder.h"
-#include "Fireworks/Calo/interface/L1TrigProxyRhoPhiZ2DBuilder.h"
-#include "Fireworks/Calo/interface/ECalCaloTowerProxyRhoPhiZ2DBuilder.h"
-#include "Fireworks/Calo/interface/HCalCaloTowerProxyRhoPhiZ2DBuilder.h"
-#include "Fireworks/Calo/interface/BaseCaloTowerProxyRhoPhiZ2DBuilder.h"
