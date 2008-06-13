9536dcd5949973e6233cd7df87e2ae5c41065b8f
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-06-13 09:21:23 +0000

    Add the last integration test with HLT and Frontier

--- FastSimulation/Configuration/test/IntegrationTestWithHLT_cfg.py
+++ FastSimulation/Configuration/test/IntegrationTestWithHLT_cfg.py
@@ -23,17 +23,19 @@ process.load("FastSimulation.HighLevelTrigger.HLTSetup_cff")
 # L1 Menu and prescale factors : useful for testing all L1 paths
 process.load("Configuration.StandardSequences.L1TriggerDefaultMenu_cff")
 
-# Reconstruction of all events, including those rejected at L1/HLT
-# Uncomment this line (and the path reconstruction)
-# sequence HLTEndSequence = { dummyModule }
 # HLT paths - defined by configDB
-# This one is created on the fly by FastSimulation/Configuration/test/IntegrationTestWithHLT.csh
-process.load("FastSimulation.Configuration.test.HLT_cff")
+# This one is created on the fly by FastSimulation/Configuration/test/IntegrationTestWithHLT_py.csh
+process.load("FastSimulation.Configuration.HLT_cff")
 
 # All event accepted by HLT are reconstructed
 process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+
 # No reconstruction - only HLT
 # process.HLTEndSequence = cms.Sequence(process.dummyModule)
+
+# All events are reconstructed, including those rejected at L1/HLT
+# process.HLTEndSequence = cms.Sequence(process.dummyModule)
+# reconstruction = cms.Path( reconstructionWithFamos )
 
 # Simulation sequence
 process.simulation = cms.Sequence(process.simulationWithFamos)
@@ -45,13 +47,24 @@ process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 # Number of pileup events per crossing
 process.famosPileUp.PileUpSimulator.averageNumber = 0.0
 
-# Note : if your process is not called HLT, you have to change that! 
-#replace hltTrigReport.HLTriggerResults = TriggerResults::PROD
-#replace hltHighLevel.TriggerResultsTag = TriggerResults::PROD 
-# Reconstruction of all events, including those rejected at L1/HLT
-# Uncomment this path
-# path reconstruction = { reconstructionWithFamos } 
+# Get frontier conditions
+# Values for globaltag are "STARTUP_V1::All", "1PB::All", "10PB::All", "IDEAL_V2::All"
+process.GlobalTag.globaltag = "IDEAL_V2::All"
 
+# Apply ECAL miscalibration
+process.caloRecHits.RecHitsFactory.doMiscalib = True
+
+# Apply Tracker misalignment (ideal alignment though)
+process.famosSimHits.ApplyAlignment = True
+process.misalignedTrackerGeometry.applyAlignment = True
+
+# Apply HCAL miscalibration (not ideal in that case)
+process.caloRecHits.RecHitsFactory.HCAL.Refactor = 1.0
+process.caloRecHits.RecHitsFactory.HCAL.Refactor_mean = 1.0
+
+# Note : if your process is not called HLT, you have to change that! 
+# process.hltTrigReport.HLTriggerResults = TriggerResults::PROD
+# process.hltHighLevel.TriggerResultsTag = TriggerResults::PROD 
 
 # To write out events 
 process.load("FastSimulation.Configuration.EventContent_cff")
@@ -61,11 +74,5 @@ process.o1 = cms.OutputModule("PoolOutputModule",
 )
 process.outpath = cms.EndPath(process.o1)
 
-process.GlobalTag.globaltag = cms.InputTag("IDEAL_V2","","All")
-process.caloRecHits.RecHitsFactory.doMiscalib = True
-process.famosSimHits.ApplyAlignment = True
-process.misalignedTrackerGeometry.applyAlignment = True
-process.caloRecHits.RecHitsFactory.HCAL.Refactor = 1.0
-process.caloRecHits.RecHitsFactory.HCAL.Refactor_mean = 1.00
 
 
