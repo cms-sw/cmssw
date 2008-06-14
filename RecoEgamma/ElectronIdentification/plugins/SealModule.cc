3c6824e943d3ec6468520e800dd7cf2725f762d8
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2008-06-14 19:19:49 +0000

    updated for the ESSource registration

--- RecoEgamma/ElectronIdentification/plugins/SealModule.cc
+++ RecoEgamma/ElectronIdentification/plugins/SealModule.cc
@@ -43,7 +43,3 @@ DEFINE_ANOTHER_FWK_MODULE(EleIdLikelihoodExtProducer);
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "RecoEgamma/ElectronIdentification/plugins/ElectronLikelihoodESSource.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE( ElectronLikelihoodESSource );
-
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-#include "RecoEgamma/ElectronIdentification/interface/ElectronLikelihood.h"
-EVENTSETUP_DATA_REG( ElectronLikelihood );
