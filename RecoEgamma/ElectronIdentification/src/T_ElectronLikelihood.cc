df6c309190e34a7d80fff8aa7460bf6ee93922d2
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2008-06-14 19:20:21 +0000

    new file for the ESSource registration

---
+++ RecoEgamma/ElectronIdentification/src/T_ElectronLikelihood.cc
@@ -0,0 +1,4 @@
+
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "RecoEgamma/ElectronIdentification/interface/ElectronLikelihood.h"
+EVENTSETUP_DATA_REG( ElectronLikelihood );
