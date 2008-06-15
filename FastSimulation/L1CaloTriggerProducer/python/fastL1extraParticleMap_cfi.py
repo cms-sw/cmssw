0f49a26f10855bd6b8e9f8ce4963e26ea735910c
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-06-15 04:30:04 +0000

    replace copy by clone statement (prevents pollution of config with to be cloned object)

--- FastSimulation/L1CaloTriggerProducer/python/fastL1extraParticleMap_cfi.py
+++ FastSimulation/L1CaloTriggerProducer/python/fastL1extraParticleMap_cfi.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-import copy
-from L1Trigger.L1ExtraFromDigis.l1extraParticleMap_cfi import *
-fastL1extraParticleMap = copy.deepcopy(l1extraParticleMap)
+import  L1Trigger.L1ExtraFromDigis.l1extraParticleMap_cfi 
+fastL1extraParticleMap = L1Trigger.L1ExtraFromDigis.l1extraParticleMap_cfi.l1extraParticleMap.clone()
 fastL1extraParticleMap.isolatedEmSource = cms.InputTag("fastL1CaloSim","Isolated")
 fastL1extraParticleMap.nonIsolatedEmSource = cms.InputTag("fastL1CaloSim","NonIsolated")
 fastL1extraParticleMap.centralJetSource = cms.InputTag("fastL1CaloSim","Central")
