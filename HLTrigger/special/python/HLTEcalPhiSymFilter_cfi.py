84a801ea2cc67b6818d6e76c44eee4a5cc22165d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-06-16 14:11:11 +0000

    Update outdated py files.

--- HLTrigger/special/python/HLTEcalPhiSymFilter_cfi.py
+++ HLTrigger/special/python/HLTEcalPhiSymFilter_cfi.py
@@ -5,11 +5,11 @@ import FWCore.ParameterSet.Config as cms
 # Ecal PhiSymmetry
 #
 # Author: Stefano Argiro
-# $Id: HLTEcalPhiSymFilter.cfi,v 1.3 2008/03/04 16:38:12 argiro Exp $
+# $Id: HLTEcalPhiSymFilter.cfi,v 1.4 2008/06/12 12:46:04 argiro Exp $
 alCaPhiSymStream = cms.EDFilter("HLTEcalPhiSymFilter",
     endcapHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
     eCut_barrel = cms.double(0.15),
-    eCut_endcap = cms.double(0.75),
+    eCut_endcap = cms.double(0.65),
     phiSymBarrelHitCollection = cms.string('phiSymEcalRecHitsEB'),
     barrelHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
     phiSymEndcapHitCollection = cms.string('phiSymEcalRecHitsEE')
--- HLTrigger/special/python/alCaPi0HLTRegRecHits_cfi.py
+++ HLTrigger/special/python/alCaPi0HLTRegRecHits_cfi.py
@@ -9,10 +9,12 @@ alCaPi0RegRecHits = cms.EDFilter("HLTPi0RecHitsFilter",
     ParameterX0 = cms.double(0.89),
     seleXtalMinEnergy = cms.double(0.0),
     selePtPi0 = cms.double(2.5),
+    detaL1 = cms.double(0.2),
     clusSeedThr = cms.double(0.5),
     clusPhiSize = cms.int32(3),
     selePi0BeltDR = cms.double(0.2),
     clusEtaSize = cms.int32(3),
+    UseMatchedL1Seed = cms.bool(False),
     selePi0Iso = cms.double(0.5),
     ParameterW0 = cms.double(4.2),
     seleNRHMax = cms.int32(1000),
@@ -20,13 +22,17 @@ alCaPi0RegRecHits = cms.EDFilter("HLTPi0RecHitsFilter",
     #    string ecalRecHitsProducer = "ecalRegionalEgammaRecHit"
     #    string barrelHitCollection = "EcalRecHitsEB"
     #   replace the 2 strings with 1 InputTag of form label:instance
-    barrelHits = cms.InputTag("ecalRegionalEgammaPi0RecHit","EcalRecHitsPi0EB"),
+    barrelHits = cms.InputTag("hltEcalRegionalEgammaRecHit","EcalRecHitsEB"),
+    l1SeedFilterTag = cms.InputTag("hltL1sEcalPi0"),
     ParameterLogWeighted = cms.bool(True),
     seleS4S9GammaTwo = cms.double(0.85),
     pi0BarrelHitCollection = cms.string('pi0EcalRecHitsEB'),
+    dphiL1 = cms.double(0.2),
+    l1IsolatedTag = cms.InputTag("hltL1extraParticles","Isolated"),
     seleMinvMinPi0 = cms.double(0.06),
     gammaCandEtaSize = cms.int32(9),
     selePtGammaTwo = cms.double(0.9),
+    l1NonIsolatedTag = cms.InputTag("hltL1extraParticles","NonIsolated"),
     ParameterT0_barl = cms.double(5.7)
 )
 
