56720fa8a6d73b33f026683c2eef60dd3a6bd719
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-06-16 21:48:04 +0000

    volumeBasedMagneticField.cfi is now dummy and replaced by high level standard cff files in StandardSequences

--- MagneticField/Engine/python/volumeBasedMagneticField_cfi.py
+++ MagneticField/Engine/python/volumeBasedMagneticField_cfi.py
@@ -1,7 +1,12 @@
-import FWCore.ParameterSet.Config as cms
+#import FWCore.ParameterSet.Config as cms
 
 # This cfi contains everything needed to use the VolumeBased magnetic
 # field engine.
 #Default is version 85l
-from MagneticField.Engine.volumeBasedMagneticField_85l_cfi import *
+#from MagneticField.Engine.volumeBasedMagneticField_85l_cfi import *
 
+import sys
+print "This file is obsolete and will soon be removed.";
+print "please use Configuration/StandardSequences/python/MagneticField_cff.py or Configuration/StandardSequences/data/MagneticField.cff";
+print "if still using old configuration files. Now exiting with -1 error. please remove inclusion of this file and try again.";
+sys.exit(-1);
