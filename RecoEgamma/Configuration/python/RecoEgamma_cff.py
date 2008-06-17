8a1c94a9ac4f9ed1f6a2abe273f6c03d1da2b721
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-06-17 14:19:29 +0000

    Sync python with the latest updates from ElectornID

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -5,10 +5,12 @@ from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.conversionSequence_cff import *
 from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
 from RecoEgamma.PhotonIdentification.photonId_cff import *
+from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
-egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
+egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence*eIdSequence)
 egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDSequence)
 egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
 egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
 egammareco_withIsolation_woConvPhotons = cms.Sequence(egammareco_woConvPhotons*egammaIsolationSequence)
 egammareco_withPhotonID = cms.Sequence(egammareco*photonIDSequence)
+egammareco_withElectronID = cms.Sequence(egammareco*eIdSequence)
