f7f1b7b50831aa9786e237aa21074d7797a9bcad
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-06-17 16:00:27 +0000

    Fix python version of RecoEgamma_cff. There was a mismatch with the old config version

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -7,8 +7,8 @@ from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
 from RecoEgamma.PhotonIdentification.photonId_cff import *
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
-egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence*eIdSequence)
-egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDSequence)
+egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
+egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDSequence*eIdSequence)
 egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
 egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
 egammareco_withIsolation_woConvPhotons = cms.Sequence(egammareco_woConvPhotons*egammaIsolationSequence)
