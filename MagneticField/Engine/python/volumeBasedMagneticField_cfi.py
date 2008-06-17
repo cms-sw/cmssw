e13d539086dc8e4cc8ed1de67fd8258e80ff0753
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-06-17 07:55:37 +0000

    made the comment more expliti by adding the name of the now obsolete files

--- MagneticField/Engine/python/volumeBasedMagneticField_cfi.py
+++ MagneticField/Engine/python/volumeBasedMagneticField_cfi.py
@@ -6,7 +6,7 @@
 #from MagneticField.Engine.volumeBasedMagneticField_85l_cfi import *
 
 import sys
-print "This file is obsolete and will soon be removed.";
-print "please use Configuration/StandardSequences/python/MagneticField_cff.py or Configuration/StandardSequences/data/MagneticField.cff";
-print "if still using old configuration files. Now exiting with -1 error. please remove inclusion of this file and try again.";
+print "MagneticField/Engine/data/volumeBasedMagneticField.cfi and \nMagneticField/Engine/python/volumeBasedMagneticField_cfi.py are obsolete and will soon be removed.";
+print "please use Configuration/StandardSequences/python/MagneticField_cff.py \nor Configuration/StandardSequences/data/MagneticField.cff";
+print "if still using old configuration files. Now exiting with -1 error. \nplease remove inclusion of this file and try again.";
 sys.exit(-1);
