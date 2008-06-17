8e77ee2a46d1d4beda915c654d4ad5d8a51c4f28
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-17 23:55:45 +0000

    add PixelTrackExtractor

--- RecoMuon/MuonIsolation/plugins/module.cc
+++ RecoMuon/MuonIsolation/plugins/module.cc
@@ -5,11 +5,13 @@ DEFINE_SEAL_MODULE();
 #include "PhysicsTools/IsolationAlgos/interface/IsoDepositExtractor.h"
 #include "PhysicsTools/IsolationAlgos/interface/IsoDepositExtractorFactory.h"
 #include "TrackExtractor.h"
+#include "PixelTrackExtractor.h"
 #include "CaloExtractor.h"
 #include "CaloExtractorByAssociator.h"
 #include "JetExtractor.h"
 #include "ExtractorFromDeposits.h"
 DEFINE_EDM_PLUGIN(IsoDepositExtractorFactory, muonisolation::TrackExtractor, "TrackExtractor");
+DEFINE_EDM_PLUGIN(IsoDepositExtractorFactory, muonisolation::PixelTrackExtractor, "PixelTrackExtractor");
 DEFINE_EDM_PLUGIN(IsoDepositExtractorFactory, muonisolation::CaloExtractor, "CaloExtractor");
 DEFINE_EDM_PLUGIN(IsoDepositExtractorFactory, muonisolation::CaloExtractorByAssociator, "CaloExtractorByAssociator");
 DEFINE_EDM_PLUGIN(IsoDepositExtractorFactory, muonisolation::JetExtractor, "JetExtractor");
