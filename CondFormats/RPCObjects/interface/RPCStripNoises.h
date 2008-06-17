d5286593e7b87e77041b813f455aa1c18780f719
Raffaello Trentadue <sha1-9504fe7d3bbc4a07e3333b20772b63251c823b73@cern.ch>
2008-06-17 14:45:58 +0000

    Modified structure RPCStripNoises

--- CondFormats/RPCObjects/interface/RPCStripNoises.h
+++ CondFormats/RPCObjects/interface/RPCStripNoises.h
@@ -6,15 +6,14 @@
 #include<iostream>
 #include<boost/cstdint.hpp>
 
-
 class RPCStripNoises {
 
  public:
 
   struct NoiseItem {
     int dpid;
-    float noise[96];
-    float eff[96];
+    float noise;
+    float eff;
     float time;
   };
   
@@ -23,10 +22,10 @@ class RPCStripNoises {
   ~RPCStripNoises(){};
   
   std::vector<NoiseItem>  getVNoise() const {return v_noises;}
-  std::vector<double>  getCls() const {return v_cls;}
+  std::vector<float>  getCls() const {return v_cls;}
 
   std::vector<NoiseItem>  v_noises; 
-  std::vector<double>  v_cls; 
+  std::vector<float>  v_cls; 
 };
 
 #endif
