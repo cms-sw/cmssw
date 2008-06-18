b817792620a4bc181463d4987e872073ce4c5fb5
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-06-18 11:00:13 +0000

    Increase the Et cut from 5 to 10, plus siwtch off (again) the H/E cut (i nsome commit I must have started from an old version where the cut was still on

--- RecoEgamma/EgammaPhotonProducers/python/photons_cfi.py
+++ RecoEgamma/EgammaPhotonProducers/python/photons_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 #
 # producer for photons
-# $Id: photons.cfi,v 1.27 2008/06/02 21:33:28 nancy Exp $
+# $Id: photons_cfi.py,v 1.8 2008/06/04 19:25:32 rpw Exp $
 #
 photons = cms.EDProducer("PhotonProducer",
     scHybridBarrelProducer = cms.InputTag("correctedHybridSuperClusters"),
@@ -26,8 +26,8 @@ photons = cms.EDProducer("PhotonProducer",
     barrelEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
     hbheModule = cms.string('hbhereco'),
     endcapEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-    minSCEt = cms.double(5.0),
-    maxHOverE = cms.double(0.2),
+    minSCEt = cms.double(10.0),
+    maxHOverE = cms.double(999.),
     hOverEConeSize = cms.double(0.1),
     posCalc_x0 = cms.double(0.89),
     MVA_weights_location = cms.string('RecoEgamma/EgammaTools/data/TMVAnalysis_Likelihood.weights.txt'),
