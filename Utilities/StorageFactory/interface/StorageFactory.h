6a2f8316136371dd7070a58b911aa2535c22ab85
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-06-18 09:39:56 +0000

    Add hinting for caching and read behaviour.

--- Utilities/StorageFactory/interface/StorageFactory.h
+++ Utilities/StorageFactory/interface/StorageFactory.h
@@ -11,13 +11,36 @@ class Storage;
 class StorageFactory 
 {
 public:
+  enum CacheHint
+  {
+    CACHE_HINT_APPLICATION,
+    CACHE_HINT_STORAGE,
+    CACHE_HINT_LAZY_DOWNLOAD,
+    CACHE_HINT_AUTO_DETECT
+  };
+
+  enum ReadHint
+  {
+    READ_HINT_UNBUFFERED,
+    READ_HINT_READAHEAD,
+    READ_HINT_AUTO
+  };
+
   static StorageFactory *get (void);
   ~StorageFactory (void);
 
   // implicit copy constructor
   // implicit assignment operator
 
+  void		setCacheHint(CacheHint value);
+  CacheHint	cacheHint(void) const;
+
+  void		setReadHint(ReadHint value);
+  ReadHint	readHint(void) const;
+
   bool		enableAccounting (bool enabled);
+  bool		accounting (void) const;
+
   Storage *	open (const std::string &url,
 	    	      int mode = IOFlags::OpenRead,
 	    	      const std::string &tmpdir = "");
@@ -34,6 +57,8 @@ protected:
 			  std::string &rest);
   
   MakerTable	m_makers;
+  CacheHint	m_cacheHint;
+  ReadHint	m_readHint;
   bool		m_accounting;
   static StorageFactory s_instance;
 };
--- Utilities/StorageFactory/src/StorageFactory.cc
+++ Utilities/StorageFactory/src/StorageFactory.cc
@@ -2,6 +2,7 @@
 #include "Utilities/StorageFactory/interface/StorageMakerFactory.h"
 #include "Utilities/StorageFactory/interface/StorageAccount.h"
 #include "Utilities/StorageFactory/interface/StorageAccountProxy.h"
+#include "Utilities/StorageFactory/interface/LocalCacheFile.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/PluginManager/interface/PluginManager.h"
 #include "FWCore/PluginManager/interface/standard.h"
@@ -11,7 +12,9 @@
 StorageFactory StorageFactory::s_instance;
 
 StorageFactory::StorageFactory (void)
-  : m_accounting (false)
+  : m_cacheHint(CACHE_HINT_AUTO_DETECT),
+    m_readHint(READ_HINT_AUTO),
+    m_accounting (false)
 {}
 
 StorageFactory::~StorageFactory (void)
@@ -22,9 +25,7 @@ StorageFactory::~StorageFactory (void)
 
 StorageFactory *
 StorageFactory::get (void)
-{
-  return &s_instance;
-}
+{ return &s_instance; }
 
 bool
 StorageFactory::enableAccounting (bool enabled)
@@ -33,6 +34,26 @@ StorageFactory::enableAccounting (bool enabled)
   m_accounting = enabled;
   return old;
 }
+
+bool
+StorageFactory::accounting(void) const
+{ return m_accounting; }
+
+void
+StorageFactory::setCacheHint(CacheHint value)
+{ m_cacheHint = value; }
+
+StorageFactory::CacheHint
+StorageFactory::cacheHint(void) const
+{ return m_cacheHint; }
+
+void
+StorageFactory::setReadHint(ReadHint value)
+{ m_readHint = value; }
+
+StorageFactory::ReadHint
+StorageFactory::readHint(void) const
+{ return m_readHint; }
 
 StorageMaker *
 StorageFactory::getMaker (const std::string &proto)
@@ -80,8 +101,18 @@ StorageFactory::open (const std::string &url, int mode, const std::string &tmpdi
     try
     {
       if (Storage *storage = maker->open (protocol, rest, mode, tmpdir))
-        ret = m_accounting ? new StorageAccountProxy (protocol, storage) : storage;
-      if (stats) stats->tick();
+      {
+	if (dynamic_cast<LocalCacheFile *>(storage))
+	  protocol = "local-cache";
+
+	if (m_accounting)
+	  ret = new StorageAccountProxy(protocol, storage);
+	else
+	  ret = storage;
+
+        if (stats)
+	  stats->tick();
+      }
     }
     catch (cms::Exception &err)
     {
