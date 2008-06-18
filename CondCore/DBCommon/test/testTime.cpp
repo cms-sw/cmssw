64d25cc871b1c8ca643e909f0e19e0bf38a56d82
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-06-18 17:00:14 +0000

    add timestamp test

--- CondCore/DBCommon/test/testTime.cpp
+++ CondCore/DBCommon/test/testTime.cpp
@@ -1,8 +1,24 @@
 #include "CondCore/DBCommon/interface/Time.h"
 
 
-#include<iostream>
+#include <iostream>
+#include <sys/time.h>
+#include "DataFormats/Provenance/interface/Timestamp.h"
 int main() {
+  ::timeval tv;
+  gettimeofday(&tv,0);
+  std::cout<<"sec "<<tv.tv_sec<<std::endl;
+  std::cout<<"micro sec "<<tv.tv_usec<<std::endl;
+  edm::Timestamp tmstamp((unsigned long long)tv.tv_sec*1000000+(unsigned long long)tv.tv_usec);
+  std::cout<<"timestamp of the day since 1970 in microsecond "<<tmstamp.value()<<std::endl;
+  edm::Timestamp tstamp((unsigned long long)tv.tv_usec);
+  std::cout<<"timestamp of the day since 1970 in second "<<tstamp.value()<<std::endl;
+  edm::TimeValue_t hlttime=0LL;;
+  gettimeofday((timeval*)(&hlttime),0);
+  edm::Timestamp hltstamp(hlttime);
+  std::cout<<"timestamp of the day since 1970 in HLT "<<hltstamp.value()<<std::endl;
+  //  edm::TimeValue_t bizzartime=4294967295LL;
+  //usec=bizzartime
   using namespace cond;
   for (size_t i=0; i<TIMETYPE_LIST_MAX; i++) 
     std::cout << "Time Specs:" 
