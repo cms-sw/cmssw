7387684b919e37baf867c10b527098580adaf720
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-06-19 12:17:35 +0000

    210 cosmics and python-cosmics

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -1,10 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Muon.muonMonitors_cff import *
+from DQMOffline.Muon.muonCosmicMonitors_cff import *
 from DQMOffline.Ecal.ecal_dqm_source_offline_cff import *
 from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
-DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
-DQMOffline_woTracker = cms.Sequence(ecal_dqm_source_offline7*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
+#from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
 
+
+#DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*siPixelOfflineDQM_source)
+#DQMOffline_woHcal = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*siPixelOfflineDQM_source)
+DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
+DQMOffline_woHcal = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer)
