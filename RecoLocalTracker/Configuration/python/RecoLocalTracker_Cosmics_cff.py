0b05bc015448b25e540771084fd36bf7e8aac707
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-06-19 14:55:02 +0000

    update python dir

--- RecoLocalTracker/Configuration/python/RecoLocalTracker_Cosmics_cff.py
+++ RecoLocalTracker/Configuration/python/RecoLocalTracker_Cosmics_cff.py
@@ -1,18 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# Tracker Local Reco
 from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
-from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_SimData_cfi import * ##SimData.cfi compatible with RealData reco in 20x
-
-from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_SimData2_cfi import * ##SimData2.cfi compatible with RealData reco in 20x
-
+from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi import *
+from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi import *
 from RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi import *
 from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
+from RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff import *
 pixeltrackerlocalreco = cms.Sequence(siPixelClusters*siPixelRecHits)
 striptrackerlocalreco = cms.Sequence(siStripZeroSuppression*siStripClusters*siStripMatchedRecHits)
 trackerlocalreco = cms.Sequence(pixeltrackerlocalreco*striptrackerlocalreco)
