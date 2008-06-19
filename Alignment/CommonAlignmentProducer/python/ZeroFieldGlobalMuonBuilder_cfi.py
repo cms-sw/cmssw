4d03511f65a7acf0302a4f192033b599e952245b
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-06-19 18:24:48 +0000

    make it parse

--- Alignment/CommonAlignmentProducer/python/ZeroFieldGlobalMuonBuilder_cfi.py
+++ Alignment/CommonAlignmentProducer/python/ZeroFieldGlobalMuonBuilder_cfi.py
@@ -1,9 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
 ZeroFieldGlobalMuonBuilder = cms.EDFilter("ZeroFieldGlobalMuonBuilder",
-                                          inputTracker = cms.InputTag("cosmictrackfinderP5"),
-                                          inputMuon = cms.InputTag("cosmicMuons"),
-                                          minTrackerHits = cms.int32(0),
-                                          minMuonHits = cms.int32(0),
-                                          minPdot = cms.double(0.99),
-                                          minDdotP = cms.double(0.99),
-                                          debuggingHistograms = cms.untracked.bool(False),
-                                          )
+    minTrackerHits = cms.int32(0),
+    minPdot = cms.double(0.99),
+    minMuonHits = cms.int32(0),
+    minDdotP = cms.double(0.99),
+    inputMuon = cms.InputTag("cosmicMuons"),
+    inputTracker = cms.InputTag("cosmictrackfinderP5"),
+    debuggingHistograms = cms.untracked.bool(False)
+)
+
+
