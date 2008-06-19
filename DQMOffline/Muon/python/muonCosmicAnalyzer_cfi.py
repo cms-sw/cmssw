030a101f00213fa065344b8e1b6c3ecfced8d615
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-06-19 12:18:26 +0000

    python fix

--- DQMOffline/Muon/python/muonCosmicAnalyzer_cfi.py
+++ DQMOffline/Muon/python/muonCosmicAnalyzer_cfi.py
@@ -5,6 +5,6 @@ import DQMOffline.Muon.muonAnalyzer_cfi
 muonCosmicAnalyzer = DQMOffline.Muon.muonAnalyzer_cfi.muonAnalyzer.clone()
 muonCosmicAnalyzer.MuonCollection = 'muons'
 muonCosmicAnalyzer.SeedCollection = 'CosmicMuonSeed'
-muonCosmicAnalyzer.TrackCollection = 'cosmicMuons'
-
+muonCosmicAnalyzer.GlobalMuTrackCollection = 'globalCosmicMuons'
+muonCosmicAnalyzer.STAMuTrackCollection = 'cosmicMuons'
 
