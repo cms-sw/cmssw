2cb17d8b73512bd71b90829bca193ff6dd68fc3f
Javier Fernandez Menendez <sha1-96a07b193caeb9d667466687d23e726533fc5d4f@cern.ch>
2008-06-19 08:07:30 +0000

    *** empty log message ***

--- Alignment/OfflineValidation/scripts/drawDT2D.C
+++ Alignment/OfflineValidation/scripts/drawDT2D.C
@@ -13,48 +13,48 @@ mycanvas->cd(1);
 nameOfHisto= "hprofLocalPositionDT";
 TH2F * h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
 
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(2);
 
 nameOfHisto= "hprofGlobalPositionDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(3);
 
 nameOfHisto= "hprofLocalPositionRmsDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(4);
 
 nameOfHisto= "hprofGlobalPositionRmsDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(5);
 
 nameOfHisto= "hprofLocalAngleDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(6);
 
 nameOfHisto= "hprofGlobalAngleDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(7);
 
 nameOfHisto= "hprofLocalAngleRmsDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 mycanvas->cd(8);
 
 nameOfHisto= "hprofGlobalAngleRmsDT";
 h2 = (TH2F*) gROOT->FindObject(nameOfHisto);
-h->Draw("COLZ");
+h2->Draw("COLZ");
 
 }
