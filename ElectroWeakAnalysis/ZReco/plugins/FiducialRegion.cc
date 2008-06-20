3eb67ab24be3c20f149757eb8851dc2316662278
Alexandre Zabi <sha1-317a4bf0815cf2e6e714c0614a0f72cd31dbdecf@cern.ch>
2008-06-20 13:36:54 +0000

    Cutting on detector eta (0.0.0) not electron eta

--- ElectroWeakAnalysis/ZReco/plugins/FiducialRegion.cc
+++ ElectroWeakAnalysis/ZReco/plugins/FiducialRegion.cc
@@ -25,7 +25,13 @@ void FiducialRegion::select(edm::Handle<reco::PixelMatchGsfElectronCollection> c
   selected_.clear();
 
   for( reco::PixelMatchGsfElectronCollection::const_iterator el = c->begin();  el != c->end(); el++ ) {
-    if (fabs(el->eta()) < 1.4442 || 1.560 < fabs(el->eta()) ) {
+    
+    //cutting on detector eta
+    const reco::SuperCluster& sc = *(el->superCluster()) ;
+    //cout << "eta=" << sc.eta() << " " << el->eta() << endl;
+
+    //if (fabs(el->eta()) < 1.4442 || 1.560 < fabs(el->eta()) ) {
+    if (fabs(sc.eta()) < 1.4442 || 1.560 < fabs(sc.eta()) ) {
       selected_.push_back( &(*el) );
     }
   }  
