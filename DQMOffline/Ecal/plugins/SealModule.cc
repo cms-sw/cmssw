ceb1daab86de10533e5c976703c018003bde77a3
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-06-23 07:22:50 +0000

    removed client from module def

--- DQMOffline/Ecal/plugins/SealModule.cc
+++ DQMOffline/Ecal/plugins/SealModule.cc
@@ -2,10 +2,6 @@
 
 // The PhiSym source module
 #include "DQMOffline/Ecal/src/DQMSourcePhiSym.h"
-// The PhiSym client module
-#include "DQMOffline/Ecal/src/DQMClientPhiSym.h"
 
-
-DEFINE_ANOTHER_FWK_MODULE(DQMClientPhiSym);
 DEFINE_ANOTHER_FWK_MODULE(DQMSourcePhiSym);
 
