0b6c6e8a7901968102617a0f1913b62a2f5ac13a
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-06-23 06:24:20 +0000

    added proper state intialization

--- Fireworks/Core/src/FWBoolParameterSetter.cc
+++ Fireworks/Core/src/FWBoolParameterSetter.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Mar 10 11:22:32 CDT 2008
-// $Id: FWBoolParameterSetter.cc,v 1.2 2008/05/18 09:42:48 jmuelmen Exp $
+// $Id: FWBoolParameterSetter.cc,v 1.1 2008/06/20 05:57:06 dmytro Exp $
 //
 
 // system include files
@@ -16,6 +16,7 @@
 #include "TGButton.h"
 #include <assert.h>
 #include <iostream>
+#include "TGedFrame.h"
 
 // user include files
 #include "Fireworks/Core/src/FWBoolParameterSetter.h"
@@ -77,6 +78,7 @@ FWBoolParameterSetter::build(TGFrame* iParent)
    TGCompositeFrame* frame = new TGHorizontalFrame(iParent);
    
    m_widget = new TGCheckButton(frame, m_param->name().c_str(), 0);
+   m_widget->SetState( m_param->value() ? kButtonDown : kButtonUp );
    m_widget->Connect("Clicked()", "FWBoolParameterSetter", this, "doUpdate()");
    frame->AddFrame(m_widget, new TGLayoutHints(kLHintsLeft|kLHintsCenterY,2,0,1,1));
    return frame;
