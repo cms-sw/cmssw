c68299ab7983520f4b7dc77e3d13faf4c5a36ba2
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-06-23 19:57:29 +0000

    removed genMatch hypothesis from TopTools

--- TopQuarkAnalysis/TopTools/plugins/SealModule.cc
+++ TopQuarkAnalysis/TopTools/plugins/SealModule.cc
@@ -7,8 +7,3 @@ typedef TtJetPartonMatch< TtSemiEvtPartons > TtSemiEvtJetPartonMatch;
 
 DEFINE_FWK_MODULE(TtHadEvtJetPartonMatch);
 DEFINE_FWK_MODULE(TtSemiEvtJetPartonMatch);
-
-#include "TopQuarkAnalysis/TopTools/plugins/TtSemiGenMatchHypothesis.h"
-
-DEFINE_FWK_MODULE(TtSemiGenMatchHypothesis);
-
