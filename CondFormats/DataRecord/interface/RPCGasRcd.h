0bdb7a2e61bb32872e23f62cbe92ea6c36a197b0
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-06-24 09:31:41 +0000

    first release

---
+++ CondFormats/DataRecord/interface/RPCGasRcd.h
@@ -0,0 +1,13 @@
+#ifndef DataRecord_RPCGasRcd_h
+#define DataRecord_RPCGasRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCdbDataRcd
+// 
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class RPCGasRcd : public edm::eventsetup::EventSetupRecordImplementation<RPCGasRcd> {};
+
+#endif
