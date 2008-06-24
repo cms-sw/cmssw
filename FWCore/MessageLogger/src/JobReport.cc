cb4b3b23ad91d4dedc64f812095934d3fd990882
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2008-06-24 21:12:18 +0000

    CDATA fix for FrameworkError targeted for 2.1.0

--- FWCore/MessageLogger/src/JobReport.cc
+++ FWCore/MessageLogger/src/JobReport.cc
@@ -10,12 +10,10 @@
 //		to direct use of LogInfo.
 //
 // 4/8/08   mf	Encase the logdesc for in <CDATA> ... </CDATA>
-//
-// 24 June 2008   ewv  Fixed <CDATA> ... </CDATA> for second instance of reportError
 
 //
 // Original Author:  Marc Paterno
-// $Id: JobReport.cc,v 1.33 2008/04/07 20:32:41 fischler Exp $
+// $Id: JobReport.cc,v 1.34 2008/04/24 22:28:27 fischler Exp $
 //
 
 
@@ -668,8 +666,6 @@ namespace edm
     if(impl_->ost_) {
       std::ostream& msg=*(impl_->ost_);
       msg << "<TimingService>\n";
-
-
       std::map<std::string, double>::const_iterator pos;
       for (pos = timingData.begin(); pos != timingData.end(); ++pos){
         msg <<  "  <" << pos->first
@@ -678,6 +674,39 @@ namespace edm
       }
       msg << "</TimingService>\n";
       //LogInfo("FwkJob") << msg.str();
+      msg << std::flush;
+    }
+  }
+
+  void
+  JobReport::reportMemoryInfo(std::map<std::string, double> const& memoryData){
+
+    if(impl_->ost_) {
+      std::ostream& msg=*(impl_->ost_);
+      msg << "<MemoryService>\n";
+      std::map<std::string, double>::const_iterator pos;
+      for (pos = memoryData.begin(); pos != memoryData.end(); ++pos){
+        msg <<  "  <" << pos->first
+        <<  "  Value=\"" << pos->second  << "\" />"
+        <<  "\n";
+      }
+      msg << "</MemoryService>\n";
+      msg << std::flush;
+    }
+  }
+
+  void
+  JobReport::reportMemoryInfo(std::vector<std::string> const& memoryData){
+
+    if(impl_->ost_) {
+      std::ostream& msg=*(impl_->ost_);
+      msg << "<MemoryService>\n";
+
+      std::vector<std::string>::const_iterator pos;
+      for (pos = memoryData.begin(); pos != memoryData.end(); ++pos){
+        msg << *pos << "\n";
+      }
+      msg << "</MemoryService>\n";
       msg << std::flush;
     }
   }
