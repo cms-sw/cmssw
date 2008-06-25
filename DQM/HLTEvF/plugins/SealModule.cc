631a25f227b62d1fe7c11647c2084f9c97ab0f70
Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch>
2008-06-25 10:59:13 +0000

    Adding new split muon modules

--- DQM/HLTEvF/plugins/SealModule.cc
+++ DQM/HLTEvF/plugins/SealModule.cc
@@ -1,4 +1,5 @@
-// $Id: SealModule.cc,v 1.7 2008/05/30 09:25:48 bachtis Exp $
+// $Id: SealModule.cc,v 1.8 2008/06/11 10:03:57 lorenzo Exp $
+
 
 #include "DQM/HLTEvF/interface/HltAnalyzer.h"
 #include "DQM/HLTEvF/interface/PathTimerService.h"
@@ -8,12 +9,16 @@
 #include "DQM/HLTEvF/interface/HLTMonElectron.h"
 #include "DQM/HLTEvF/interface/FourVectorHLT.h"
 #include "DQM/HLTEvF/interface/HLTEventInfoClient.h"
+#include "DQM/HLTEvF/interface/HLTMuonL1DQMSource.h"
+#include "DQM/HLTEvF/interface/HLTMuonRecoDQMSource.h"
+#include "DQM/HLTEvF/interface/HLTMuonIsoDQMSource.h"
+
 
 using edm::service::PathTimerService;
 
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 DEFINE_FWK_SERVICE(PathTimerService);
-//DEFINE_FWK_SERVICE_MAKER(PathTimerService,PathTimerServiceMaker);
+DEFINE_FWK_SERVICE_MAKER(PathTimerService,PathTimerServiceMaker);
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(PathTimerInserter);
@@ -23,4 +28,8 @@ DEFINE_FWK_MODULE(HLTTauDQMSource);
 DEFINE_FWK_MODULE(HLTMonElectron);
 DEFINE_FWK_MODULE(FourVectorHLT);
 DEFINE_FWK_MODULE(HLTEventInfoClient);
+DEFINE_FWK_MODULE(HLTMuonL1DQMSource);
+DEFINE_FWK_MODULE(HLTMuonRecoDQMSource);
+DEFINE_FWK_MODULE(HLTMuonIsoDQMSource);
+
 
