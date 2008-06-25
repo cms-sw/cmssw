3d17c44c13cfa3dbd42ee2e37463d850050b1670
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2008-06-25 07:10:19 +0000

    fallback to older tag

--- EventFilter/ESRawToDigi/src/SealModule.cc
+++ EventFilter/ESRawToDigi/src/SealModule.cc
@@ -4,14 +4,11 @@
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigi.h"
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigiTB.h"
 #include "EventFilter/ESRawToDigi/interface/ESRawToDigiCT.h"
-#include "EventFilter/ESRawToDigi/interface/ESRecHitProducerCT.h"
-#include "EventFilter/ESRawToDigi/interface/ESRecHitProducerTB.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiTB);
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigiCT);
-DEFINE_ANOTHER_FWK_MODULE(ESRecHitProducerCT);
-DEFINE_ANOTHER_FWK_MODULE(ESRecHitProducerTB);
+
 
 
