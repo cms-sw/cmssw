96d205fa6900a39c8f59aabca0189a7b02ca5757
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-06-26 20:19:24 +0000

    Add the module PhotonValidator

--- Validation/RecoEgamma/src/SealModule.cc
+++ Validation/RecoEgamma/src/SealModule.cc
@@ -1,7 +1,10 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoEgamma/interface/EgammaObjects.h"
+#include "Validation/RecoEgamma/interface/PhotonValidator.h"
+
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(EgammaObjects);
+DEFINE_ANOTHER_FWK_MODULE(PhotonValidator);
