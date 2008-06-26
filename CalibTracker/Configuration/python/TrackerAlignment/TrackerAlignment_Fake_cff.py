f65e002f090116060ea3857ae35740cdc47e7bdb
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-06-26 11:39:13 +0000

    we now have a FakeAlignmentSource, not only a producer as before

--- CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Fake_cff.py
+++ CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Fake_cff.py
@@ -1,23 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# Producer module
-# ===============
-# Dummy alignment producer (creating empty Aligments and AlignmentErrors)
-from Alignment.CommonAlignmentProducer.fakeAlignmentProducer_cfi import *
-# Sources that actually put the records into ESetup
-# (Here with unlimited IOV, taken from producer above.)
-# =====================================================
-from Alignment.CommonAlignmentProducer.GlobalPosition_Fake_cff import *
-fakeTrackerAlignmentSource = cms.ESSource("EmptyESSource",
-    recordName = cms.string('TrackerAlignmentRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
-fakeTrackerAlignmentErrorSource = cms.ESSource("EmptyESSource",
-    recordName = cms.string('TrackerAlignmentErrorRcd'),
-    iovIsRunNotTime = cms.bool(True),
-    firstValid = cms.vuint32(1)
-)
-
+# Dummy es_source (creating empty Alignments and AlignmentErrors)
+# ===============================================================
+from Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi import *
 
