9e407b6b2fe00af12c25fe2757b0def1dc3e4c7a
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-06-26 11:57:16 +0000

    moved real cfi to directory where the module is defined (CSCGeometryBuilder) but keep this for backward compatibility

--- Geometry/CSCGeometry/python/cscGeometry_cfi.py
+++ Geometry/CSCGeometry/python/cscGeometry_cfi.py
@@ -1,16 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# This cfi should be included to build the CSC geometry model.
-#
-# modelling flags (for completeness - internal defaults are already sane)
-CSCGeometryESModule = cms.ESProducer("CSCGeometryESModule",
-    debugV = cms.untracked.bool(False),
-    useGangedStripsInME1a = cms.bool(True),
-    useOnlyWiresInME1a = cms.bool(False),
-    useRealWireGeometry = cms.bool(True),
-    useCentreTIOffsets = cms.bool(False),
-    applyAlignment = cms.untracked.bool(False)
-)
-
+# kept for backward compatibility
+from Geometry.CSCGeometryBuilder.cscGeometry_cfi import *
 
