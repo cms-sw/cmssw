a86fce602bdb110bb5538071bb2afbd9acea08a8
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-26 00:04:42 +0000

    port of accessors to uncorrected MET from 1_6_12

--- DataFormats/PatCandidates/interface/MET.h
+++ DataFormats/PatCandidates/interface/MET.h
@@ -1,5 +1,5 @@
 //
-// $Id: MET.h,v 1.11 2008/06/03 22:28:07 gpetrucc Exp $
+// $Id: MET.h,v 1.9.2.1 2008/04/04 20:26:33 slava77 Exp $
 //
 
 #ifndef DataFormats_PatCandidates_MET_h
@@ -13,7 +13,7 @@
    within the 'pat' namespace.
 
   \author   Steven Lowette
-  \version  $Id: MET.h,v 1.11 2008/06/03 22:28:07 gpetrucc Exp $
+  \version  $Id: MET.h,v 1.9.2.1 2008/04/04 20:26:33 slava77 Exp $
 */
 
 
@@ -92,12 +92,48 @@ namespace pat {
       }
 
 
+      //! uses internal info from mEtCorr
+      //! except for full uncorrection, how do you know which is which?
+      //! you don't, 
+      //! present ordering: 
+      //! 1: jet escale Type1 correction
+      //! 2: muon Type1 (?) correction
+      uint nCorrections() const;
+      enum UncorectionType {
+	uncorrALL = 0, //! uncorrect to bare bones
+	uncorrJES,     //! uncorrect for JES only
+	uncorrMUON,    //! uncorrect for MUON only
+	uncorrMAXN
+      };
+      double corEx(UncorectionType ix = uncorrALL) const;
+      double corEy(UncorectionType ix = uncorrALL) const;
+      double corSumEt(UncorectionType ix = uncorrALL) const;
+
+      double uncorrectedPt(UncorectionType ix = uncorrALL) const;
+      double uncorrectedPhi(UncorectionType ix = uncorrALL) const;
+
     protected:
+      struct UncorInfo {
+	UncorInfo(): corEx(0), corEy(0), corSumEt(0), pt(0), phi(0) {}
+	double corEx;
+	double corEy;
+	double corSumEt;
+	double pt;
+	double phi;
+      };
 
       std::vector<reco::GenMET> genMET_;
-
       std::vector<SpecificCaloMETData> caloMET_;
 
+      //! uncorrection transients
+      mutable std::vector<UncorInfo> uncorInfo_;
+      mutable uint nCorrections_;
+      mutable double oldPt_;
+
+      void checkUncor_() const;
+
+      void setPtPhi_(UncorInfo& uci) const;
+      
   };
 
 
--- DataFormats/PatCandidates/src/MET.cc
+++ DataFormats/PatCandidates/src/MET.cc
@@ -1,5 +1,5 @@
 //
-// $Id: MET.cc,v 1.7 2008/06/03 22:28:07 gpetrucc Exp $
+// $Id: MET.cc,v 1.6.2.1 2008/04/04 20:26:33 slava77 Exp $
 //
 
 #include "DataFormats/PatCandidates/interface/MET.h"
@@ -9,25 +9,25 @@ using namespace pat;
 
 
 /// default constructor
-MET::MET(){
+MET::MET(): uncorInfo_(0) {
 }
 
 
 /// constructor from METType
-MET::MET(const METType & aMET) : PATObject<METType>(aMET) {
+MET::MET(const METType & aMET) : PATObject<METType>(aMET), uncorInfo_(0) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(&aMET);
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
 }
 
 
 /// constructor from ref to METType
-MET::MET(const edm::RefToBase<METType> & aMETRef) : PATObject<METType>(aMETRef) {
+MET::MET(const edm::RefToBase<METType> & aMETRef) : PATObject<METType>(aMETRef), uncorInfo_(0) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(aMETRef.get());
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
 }
 
 /// constructor from ref to METType
-MET::MET(const edm::Ptr<METType> & aMETRef) : PATObject<METType>(aMETRef) {
+MET::MET(const edm::Ptr<METType> & aMETRef) : PATObject<METType>(aMETRef), uncorInfo_(0) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(aMETRef.get());
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
 }
@@ -43,10 +43,71 @@ const reco::GenMET * MET::genMET() const {
   return (genMET_.size() > 0 ? &genMET_.front() : 0 );
 }
 
-
 /// method to set the generated MET
 void MET::setGenMET(const reco::GenMET & gm) {
   genMET_.clear();
   genMET_.push_back(gm);
 }
 
+//! return uncorrrection related stuff
+uint MET::nCorrections() const { checkUncor_(); return nCorrections_; }
+
+double MET::corEx(UncorectionType ix) const { checkUncor_(); return uncorInfo_[ix].corEx; }
+double MET::corEy(UncorectionType ix) const { checkUncor_(); return uncorInfo_[ix].corEy; }
+double MET::corSumEt(UncorectionType ix) const { checkUncor_(); return uncorInfo_[ix].corSumEt; }
+double MET::uncorrectedPt(UncorectionType ix) const { checkUncor_(); return uncorInfo_[ix].pt; }
+double MET::uncorrectedPhi(UncorectionType ix) const { checkUncor_(); return uncorInfo_[ix].phi; }
+
+
+//! check and set transients
+void MET::checkUncor_() const {
+  if (uncorInfo_.size() == uncorrMAXN && oldPt_ == pt() ) return;
+
+  oldPt_ = pt();
+  std::vector<CorrMETData> corrs(mEtCorr());
+  nCorrections_ = corrs.size();
+
+  uncorInfo_.resize(uncorrMAXN);
+  UncorectionType ix;
+
+  //! ugly
+  //! ALL
+  ix = uncorrALL;
+  uncorInfo_[ix] = UncorInfo();
+  for (uint iC=0; iC < nCorrections_; ++iC){
+    uncorInfo_[ix].corEx +=    corrs[iC].mex;
+    uncorInfo_[ix].corEy +=    corrs[iC].mey;
+    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+  }
+  setPtPhi_(uncorInfo_[ix]);
+
+  //! JES
+  ix = uncorrJES;
+  uncorInfo_[ix] = UncorInfo();
+  if (nCorrections_ >=1 ){
+    uint iC = 0;
+    uncorInfo_[ix].corEx +=    corrs[iC].mex;
+    uncorInfo_[ix].corEy +=    corrs[iC].mey;
+    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+  }
+  setPtPhi_(uncorInfo_[ix]);
+
+  //! MUON
+  ix = uncorrMUON;
+  uncorInfo_[ix] = UncorInfo();
+  if (nCorrections_ >=2 ){
+    uint iC = 1;
+    uncorInfo_[ix].corEx +=    corrs[iC].mex;
+    uncorInfo_[ix].corEy +=    corrs[iC].mey;
+    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+  }
+  setPtPhi_(uncorInfo_[ix]);
+
+}
+
+void MET::setPtPhi_(UncorInfo& uci) const {
+  double lpx = px() - uci.corEx;
+  double lpy = py() - uci.corEy;
+  uci.pt = sqrt(lpx*lpx + lpy*lpy);
+  uci.phi = atan2(lpy, lpx);  
+}
