10ecff7672afe1b63621944b9a461a6e0aab7010
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-26 18:42:09 +0000

    For packing LCTs

--- EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
+++ EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
@@ -2,7 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 cscpacker = cms.EDFilter("CSCDigiToRawModule",
     wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
-    stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi")
+    stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
+    comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
+    alctDigiTag = cms.InputTag("cscTriggerPrimitiveDigis"),
+    clctDigiTag = cms.InputTag("cscTriggerPrimitiveDigis"),
+    correlatedLCTDigiTag = cms.InputTag("cscTriggerPrimitiveDigis", "MPCSORTED")
 )
 
 
