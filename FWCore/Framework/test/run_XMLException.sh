33ade1c8731c819ca8a6d98f7b821cc1ece1086e
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2008-06-26 15:03:02 +0000

    Python version of XMLSafeException

--- FWCore/Framework/test/run_XMLException.sh
+++ FWCore/Framework/test/run_XMLException.sh
@@ -6,8 +6,8 @@ function die { echo Failure $1: status $2 ; exit $2 ; }
 
 pushd ${LOCAL_TMP_DIR}
   echo ${LOCAL_TMP_DIR}
-  cmsRun -j badXMLException.xml -p ${LOCAL_TEST_DIR}/testBadXMLJobReport.cfg
-  xmllint badXMLException.xml || die "cmsRun testBadXMLJobReport.cfg produced invalid XML job report" $?
+  cmsRun -j testXMLSafeException.xml -p ${LOCAL_TEST_DIR}/testXMLSafeException_cfg.py
+  xmllint testXMLSafeException.xml || die "cmsRun testBadXMLJobReport.cfg produced invalid XML job report" $?
 
 popd
 
