602530ca599dc841f520b33d0ba21baa60ad5a7f
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-26 21:43:52 +0000

    correct met for muons using jet-met pog tools

--- PhysicsTools/PatAlgos/python/recoLayer0/jetMETCorrections_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/jetMETCorrections_cff.py
@@ -9,6 +9,21 @@ from JetMETCorrections.Configuration.L5FlavorCorrections_cff import *
 # produce associated jet correction factors in a valuemap
 from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
 
+import JetMETCorrections.Type1MET.corMetMuons_cfi
+# muon MET correction maker 
+corMetType1Icone5Muons = JetMETCorrections.Type1MET.corMetMuons_cfi.corMetMuons.clone()
+# JetMET corrections for muons: input jet-corrected MET
+corMetType1Icone5Muons.inputUncorMetLabel = cms.InputTag('corMetType1Icone5')
+
+# It would be better to get this config to JetMETCorrections/Type1MET/data/ at some point
+corMetType1Icone5Muons.TrackAssociatorParameters.useEcal = False ## RecoHits
+corMetType1Icone5Muons.TrackAssociatorParameters.useHcal = False ## RecoHits
+corMetType1Icone5Muons.TrackAssociatorParameters.useHO = False ## RecoHits
+corMetType1Icone5Muons.TrackAssociatorParameters.useCalo = True ## CaloTowers
+corMetType1Icone5Muons.TrackAssociatorParameters.useMuon = False ## RecoHits
+corMetType1Icone5Muons.TrackAssociatorParameters.truthMatch = False
+
+
 # re-key jet energy corrections to layer 0 output
 layer0JetCorrFactors = cms.EDFilter("JetCorrFactorsValueMapSkimmer",
     collection  = cms.InputTag("allLayer0Jets"),
@@ -20,7 +35,7 @@ layer0JetCorrFactors = cms.EDFilter("JetCorrFactorsValueMapSkimmer",
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
 
 # default PAT sequence for JetMET corrections before cleaners
-patAODJetMETCorrections = cms.Sequence(jetCorrFactors * corMetType1Icone5)
+patAODJetMETCorrections = cms.Sequence(jetCorrFactors * corMetType1Icone5 * corMetType1Icone5Muons)
 
 # default PAT sequence for JetMET corrections after cleaners
 patLayer0JetMETCorrections = cms.Sequence(layer0JetCorrFactors)
