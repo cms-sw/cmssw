d4eab3db9b24239d30b28b83c54303aeaf90106e
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-06-26 14:53:14 +0000

    cleaning

--- RecoParticleFlow/PFBlockProducer/python/particleFlowBlock_cfi.py
+++ RecoParticleFlow/PFBlockProducer/python/particleFlowBlock_cfi.py
@@ -1,42 +1,51 @@
 import FWCore.ParameterSet.Config as cms
 
 particleFlowBlock = cms.EDProducer("PFBlockProducer",
+
+    # resolution maps
     pf_resolution_map_HCAL_eta = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_HCAL_eta.dat'),
-    pf_chi2_PS_Track = cms.double(100.0),
-    # verbosity 
-    verbose = cms.untracked.bool(False),
-    RecMuons = cms.InputTag("muons"),
-    PFConversions = cms.InputTag("pfConversions"),
-    useConversions = cms.bool(False),
     pf_resolution_map_HCAL_phi = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_HCAL_phi.dat'),
-    # module label to find input PFClusters
-    PFClustersECAL = cms.InputTag("particleFlowClusterECAL"),
-    # if true, using special algorithm to process
-    # multiple track associations to the same hcal cluster    
-    pf_multilink = cms.bool(True),
+    pf_resolution_map_ECAL_eta = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_ECAL_eta.dat'),
+    pf_resolution_map_ECAL_phi = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_ECAL_phi.dat'),
+                                   
     # max chi2 for element associations in PFBlocks
-    pf_chi2_ECAL_HCAL = cms.double(10.0),
-    PFClustersHCAL = cms.InputTag("particleFlowClusterHCAL"),
-    PFClustersPS = cms.InputTag("particleFlowClusterPS"),
-    pf_chi2_HCAL_Track = cms.double(100.0),
-    useNuclear = cms.bool(False),
-    # double  pf_chi2_HCAL_PS    = 0
     pf_chi2_ECAL_Track = cms.double(100.0),
+    pf_chi2_HCAL_Track = cms.double(100.0),
+    pf_chi2_PS_Track = cms.double(100.0),
+    pf_chi2_ECAL_HCAL = cms.double(10.0),
     pf_chi2_PSH_PSV = cms.double(5.0),
     pf_chi2_ECAL_PS = cms.double(100.0),
+
+    pf_multilink = cms.bool(True),
+
+    # verbosity 
+    verbose = cms.untracked.bool(False),
+                                   
+
+    # input clusters
+    PFClustersECAL = cms.InputTag("particleFlowClusterECAL"),
+    PFClustersHCAL = cms.InputTag("particleFlowClusterHCAL"),
+    PFClustersPS = cms.InputTag("particleFlowClusterPS"),
+
+    # input tracks
+    GsfRecTracks = cms.InputTag("pfTrackElec"),
+    RecTracks = cms.InputTag("elecpreid"),
+
+    # input nuclear interactions 
+    PFNuclear = cms.InputTag("pfNuclear"),
+    useNuclear = cms.bool(False),
+
+    # input muons
+    RecMuons = cms.InputTag("muons"),
+
+    # input conversions
+    PFConversions = cms.InputTag("pfConversions"),
+    useConversions = cms.bool(False),
+
     # Track Quality Cut: Tracks are kept if DPt/Pt < Cut
     pf_DPtoverPt_Cut = cms.double(999.9),
-    GsfRecTracks = cms.InputTag("pfTrackElec"),
-    # input collections ----------------------------------------
-    # module label to find input rec tracks
-    RecTracks = cms.InputTag("elecpreid"),
-    # particle flow parameters ---------------------------------
-    # reconstruction method, see PFAlgo/src/PFBlock.cc 
-    # int32   pf_recon_method = 3
-    # resolution maps
-    pf_resolution_map_ECAL_eta = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_ECAL_eta.dat'),
-    PFNuclear = cms.InputTag("pfNuclear"),
-    pf_resolution_map_ECAL_phi = cms.string('RecoParticleFlow/PFBlockProducer/data/resmap_ECAL_phi.dat'),
+                                   
+
     debug = cms.untracked.bool(False)
 )
 
