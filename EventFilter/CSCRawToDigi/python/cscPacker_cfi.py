b2050fd6c1e7567ecf381cf9058541234625e98d
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-27 01:22:32 +0000

    Get LCTs from simCscTriggerPrimitiveDigis

--- EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
+++ EventFilter/CSCRawToDigi/python/cscPacker_cfi.py
@@ -4,9 +4,9 @@ cscpacker = cms.EDFilter("CSCDigiToRawModule",
     wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
     comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
-    alctDigiTag = cms.InputTag("cscTriggerPrimitiveDigis"),
-    clctDigiTag = cms.InputTag("cscTriggerPrimitiveDigis"),
-    correlatedLCTDigiTag = cms.InputTag("cscTriggerPrimitiveDigis", "MPCSORTED")
+    alctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
+    clctDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis"),
+    correlatedLCTDigiTag = cms.InputTag("simCscTriggerPrimitiveDigis", "MPCSORTED")
 )
 
 
