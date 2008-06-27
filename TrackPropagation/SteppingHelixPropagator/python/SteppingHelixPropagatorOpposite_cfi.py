fd3e4399457b3824e17ac644824c22b81d831d47
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-27 15:54:30 +0000

    update python cfi/cff: include an option to shift endcaps in Z

--- TrackPropagation/SteppingHelixPropagator/python/SteppingHelixPropagatorOpposite_cfi.py
+++ TrackPropagation/SteppingHelixPropagator/python/SteppingHelixPropagatorOpposite_cfi.py
@@ -1,22 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
 SteppingHelixPropagatorOpposite = cms.ESProducer("SteppingHelixPropagatorESProducer",
+    ComponentName = cms.string('SteppingHelixPropagatorOpposite'),
+    NoErrorPropagation = cms.bool(False),
     PropagationDirection = cms.string('oppositeToMomentum'),
     useTuningForL2Speed = cms.bool(False),
     useIsYokeFlag = cms.bool(True),
-    NoErrorPropagation = cms.bool(False),
+    endcapShiftInZNeg = cms.double(0.0),
     SetVBFPointer = cms.bool(False),
     AssumeNoMaterial = cms.bool(False),
-    returnTangentPlane = cms.bool(True),
+    endcapShiftInZPos = cms.double(0.0),
     useInTeslaFromMagField = cms.bool(False),
     VBFName = cms.string('VolumeBasedMagneticField'),
+    useEndcapShiftsInZ = cms.bool(False),
     sendLogWarning = cms.bool(False),
     useMatVolumes = cms.bool(True),
     debug = cms.bool(False),
     #This sort of works but assumes a measurement at propagation origin  
     ApplyRadX0Correction = cms.bool(True),
     useMagVolumes = cms.bool(True),
-    ComponentName = cms.string('SteppingHelixPropagatorOpposite')
+    returnTangentPlane = cms.bool(True)
 )
 
 
