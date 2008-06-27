0976c5046833d35fd50aeba65ae3ebe028ae67e6
biallass <sha1-7a1bc73326736ef8230c83e0a5d14ba2577a78fe@cern.ch>
2008-06-27 16:12:04 +0000

    translation of cfg-files to new python language

---
+++ GeneratorInterface/CosmicMuonGenerator/test/CosmicMuonsSurface_cfg.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("runCosMuoGen")
+process.load("GeneratorInterface.CosmicMuonGenerator.CMSCGENsource_cfi")
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    sourceSeed = cms.untracked.uint32(1359968)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(500)
+)
+process.CMSCGEN_out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('cosmic.root')
+)
+
+process.outpath = cms.EndPath(process.CMSCGEN_out)
+process.CosMuoGenSource.MaxTheta = 84.
+process.CosMuoGenSource.ElossScaleFactor = 0.0
+
