617ba31c78fe8ca0f4f8c7e8990e665027e8b86d
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-06-27 20:39:55 +0000

    Hack for cmsdisk1.

--- EventFilter/StorageManager/scripts/perl/smCleanupFiles.pl
+++ EventFilter/StorageManager/scripts/perl/smCleanupFiles.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: smCleanupFiles.pl,v 1.1 2008/06/10 12:10:51 loizides Exp $
+# $Id: smCleanupFiles.pl,v 1.2 2008/06/11 13:59:47 loizides Exp $
 
 use strict;
 use DBI;
@@ -66,6 +66,11 @@ $force      = 0;
 
 $hostname   = `hostname -s`;
 chomp($hostname);
+
+# have to redirect for minidaq/localdaq runs
+if($hostname eq 'srv-s2c17-01' || $hostname eq 'srv-C2D05-02') {
+    $hostname = 'cmsdisk1';
+}
 
 GetOptions(
            "help"          => \$help,
