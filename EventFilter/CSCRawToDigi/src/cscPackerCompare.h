3efa1d75b251603f7706ef0fee066f16e059f144
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-27 03:25:58 +0000

    Add something to test pack->unpack->pack

--- EventFilter/CSCRawToDigi/src/cscPackerCompare.h
+++ EventFilter/CSCRawToDigi/src/cscPackerCompare.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "EventFilter/CSCRawToDigi/src/bitset_append.h"
+
 
 /** Compares two objects, and prints them out if they differ
  */
@@ -14,3 +16,23 @@ bool cscPackerCompare(const T & t1, const T & t2)
   return result;
 }
 
+
+// packs a class, then unpacks, packs again, and compares
+template <class T>
+bool cscClassPackerCompare(T & t)
+{
+  boost::dynamic_bitset<> firstPack = t.pack();
+  unsigned char data[1000];
+  bitset_utilities::bitsetToChar(firstPack, data);
+  T newObject((unsigned short int *)data);
+  boost::dynamic_bitset<> secondPack = newObject.pack();
+  if(firstPack != secondPack)
+  {
+    std::cerr << "Mismatch in " << typeid(t).name() << "\n";
+    bitset_utilities::printWords(firstPack);
+    bitset_utilities::printWords(secondPack);
+    return false;
+  }
+  return true;
+}
+     
