a92b10d73bc68907c3b98c86c10ed7e29ab53802
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-06-28 07:01:19 +0000

    new versions of edmPythonSearch and edmPythonTree (using python introspection instead of custom parsing rountines)

--- FWCore/ParameterSet/python/TreeCrawler.py
+++ FWCore/ParameterSet/python/TreeCrawler.py
@@ -1,19 +1,37 @@
-#REMOVE-THIS-SAFEGUARD## #!/usr/bin/env python
+# This CMS code is based on previous work done by Toby Dickenson, as indiciated below
+#
+# for questions: Benedikt.Hegner@cern.ch
 
-import distutils
-import distutils.fancy_getopt
-import os
-import sys
-import string
-import re
-import glob
-import commands
+# Copyright 2004 Toby Dickenson
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import sys, os, inspect
+import modulefinder
 
-#---Settings----#000000#FFFFFF--------------------------------------------------
+def packageNameFromFilename(name):
+    return ".".join(name.replace("python/","").replace(".py","").split("/")[-3:])
+
 
 class Color:
-  "ANSI escape display sequences"
+  """ANSI escape display sequences"""
   info          = "\033[1;34m"
   hilight       = "\033[31m"
   alternate     = "\033[32m"
@@ -24,188 +42,145 @@ class Color:
   deemphasis    = "\033[1;30m"
   none          = "\033[0m"
 
-class Error:
-  "Exit codes"
-  none        = 0
-  usage       = 1
-  argument    = 2
-  execution   = 3
 
-class Settings:
-  "Settings for execution etc."
-
-  blurb         = """
-Usage: %program% [options] <config-file>
-
-Dumps all the included configuration files (and fragments) using a deep search
-starting from the given top-level file.
-
-Command-line options can be:
-"""
-  variables     = distutils.fancy_getopt.FancyGetopt([
-                    ("help",                  "h",  "Print this informational message."),
-                    ("search-paths",          "s",  os.path.pathsep + " separated list of paths to try and render all those relative include paths from -- they will be tried in the given order until a match is found. Default %search-paths% (i.e. current directory plus your CMSSW_SEARCH_PATH if defined)."),
-                    ("dump-contents",         "c",  "Dump also the contents of each file, not just the list of include files. Default %dump-contents%."),
-                    ("search-mask",           "s",  "Semi-colon separated list of glob expressions for include files to follow. This controls the search and therefore also the display (if you don't find it, you can't show it). Default %searched-extensions%."),
-                    ("dump-mask",             "d",  "Semi-colon separated list of glob expressions for files to display. This only controls display and not the search. Default %displayed-extensions%."),
-                    ("veto-dump-mask",        "v",  "Semi-colon separated list of glob expressions for files to NOT dump the contents of, if relevant (if you're not dumping anything...). Note that these are just the relative to CMSSW/src paths, as they appear in the config files. Default %veto-dump-mask%."),
-                    ("min-dump-depth",        "M",  "Minimum depth of tree to dump contents of, if relevant (they will still be traversed in order to get to the lower echelons, of course). Default %min-dump-depth%."),
-                    ("max-depth",             "m",  "Maximum depth of tree to traverse. Default %max-depth%, use negative to get 'em all."),
-                    ("no-color",              "n",  "Print without color, but why would you want to do that? Default %no-color%. WARNING: not yet implemented, so there."),
-                  ])
-  options       = { "help"                    : False,
-                    "search-paths"            : "." + os.path.pathsep + os.environ.get("CMSSW_SEARCH_PATH", ""),
-                    "dump-contents"           : False,
-                    "search-mask"             : "*_cfi.py;*_cff.py;*_cfg.py",
-                    "dump-mask"               : "*",
-                    "veto-dump-mask"          : "",
-                    "min-dump-depth"          : 0,
-                    "max-depth"               : -1,
-                    "no-color"                : False,
-                  }
-  usage         = None
-  
-#  
-  matchIncludes = [ re.compile("""^\s*include\s+(['"])(.+)\\1""", re.IGNORECASE | re.MULTILINE)
-#                  , re.compile("""^\s*module\s+[^=]+=.*\s+from\s*(['"])(.+)\\1""", re.IGNORECASE | re.MULTILINE)
-                   , re.compile("""process\.load\(['"](.+)\\1""", re.IGNORECASE | re.MULTILINE)
-                  ]
-  matchImports =  [ re.compile("""^\s*import\s+(.+)""", re.IGNORECASE | re.MULTILINE)
-                   , re.compile("""^\s*from\s+(.+)+\s+import\s""", re.IGNORECASE | re.MULTILINE)
-                   , re.compile("""process\.load\(['"](.+)['"]\)""", re.IGNORECASE | re.MULTILINE)
-                  ]
+class SearchHit:
+    pass 
 
 
+class Package(object):
+    def __init__(self,name,top=False):
+        self.name = name
+        self.dependencies = []
+        self.searched = False
+        if top:
+            self.module = None
+        else:    
+            self.module = __import__(name,[],[],"*")
+    def dump(self,level):
+        indent = "  " * level
+        print indent, "+", Color.info, self.name, Color.none
+        # sort dependencies alphabetically
+        self.dependencies.sort(key = lambda x: x.name)
+        for package in self.dependencies:
+            package.dump(level+1)
+    def search(self,pattern,result):
+        """ recursive search for pattern in source files"""
+        # first start searching in the package itself / do this only once
+        if self.module and not self.searched:
+            for number, line in enumerate(inspect.getsource(self.module).splitlines()):
+                if pattern in line:
+                     filename = packageNameFromFilename(inspect.getsourcefile(self.module))
+                     hit = SearchHit()
+                     hit.number = number
+                     hit.filename = filename
+                     hit.line = line
+                     result.append(hit)
+        # then go on with dependencies
+        for package in self.dependencies:
+            package.search(pattern,result)
+        self.searched = True    
 
 
-#---Functions---#000000#FFFFFF--------------------------------------------------
-
-def convertSetting(setting, convertor, errorMessage):
-  """
-  Convert the given setting using the form produced by convertor. ValueError's
-  are caught and errorMessage is printed, then the application exits. errorMessage
-  is a format string taking one string argument, the problematic value.
-  """
-
-  try:
-    Settings.options[setting] = convertor(Settings.options[setting])
-  except ValueError:
-    print Color.hilight + (errorMessage % str(Settings.options[setting])) + Color.none
-    sys.exit(Error.argument)
-
-  return
-
-def toList(streeng, delimiter = ";"):
-  streeng = streeng.strip()
-  if streeng == "":   return []
-  return string.split(streeng, delimiter)
-
-def fileName(pyModule):
-  result = ''
-  l = pyModule.split('.')
-  if len(l) == 3:
-    result = l[0]+'/'+l[1]+'/python/'+l[2]+'.py'
-  return result
-
-def isRelevant(filePath, patterns):
-  """
-  Yes if matched and not vetoed, no otherwise.
-  """
-  
-  for match in patterns:
-    if glob.fnmatch.fnmatch(filePath, match):
-      return True
-  return False
-
-def goGetIt(relativePath):
-  """
-  Find the first instance of relativePath in the search-paths list.
-  If not found, returns None.
-  """
-  if os.path.isabs(relativePath): return relativePath
-  for basePath in Settings.options["search-paths"]:
-    fullPath    = os.path.join(basePath, relativePath)
-    if os.path.isfile(fullPath):
-      return fullPath
-  return None
+class mymf(modulefinder.ModuleFinder):
+    def __init__(self,*args,**kwargs):
+        self._depgraph = {}
+        self._types = {}
+        self._last_caller = None
+        modulefinder.ModuleFinder.__init__(self,*args,**kwargs)
+    def import_hook(self, name, caller=None, fromlist=None):
+        old_last_caller = self._last_caller
+        try:
+            self._last_caller = caller
+            return modulefinder.ModuleFinder.import_hook(self,name,caller,fromlist)
+        finally:
+            self._last_caller = old_last_caller
+    def import_module(self,partnam,fqname,parent):
+        if partnam in ("FWCore","os"):
+            r = None
+        else:
+            r = modulefinder.ModuleFinder.import_module(self,partnam,fqname,parent)
+        if r is not None:
+            self._depgraph.setdefault(self._last_caller.__name__,{})[r.__name__] = 1
+        return r
+    def load_module(self, fqname, fp, pathname, (suffix, mode, type)):
+        r = modulefinder.ModuleFinder.load_module(self, fqname, fp, pathname, (suffix, mode, type))
+        if r is not None:
+            self._types[r.__name__] = type
+        return r
 
 
+def transformIntoGraph(depgraph,toplevel):
+    packageDict = {}
+    # create the top level config
+    packageDict[toplevel] = Package(toplevel, top = True) 
 
-#---Main Execution Point---#D50000#FFFF80---------------------------------------
-class TreeCrawler:
-  def __init__(self, doer):
-    self.__levels = 0
-    self.__alreadyDone = []
-    self.__doer = doer
-  def parseArgs(self,args):
-    "Parses command-line arguments into options and variables"
+    # create package objects
+    for key, value in depgraph.iteritems():
+        if key.count(".") == 2: packageDict[key] = Package(key)
+        for name in value.keys():
+            if name.count(".") == 2: packageDict[name] = Package(name)
+    # now create dependencies
+    for key, value in depgraph.iteritems():
+        if key.count(".") == 2 or key == toplevel:
+            package = packageDict[key]
+            package.dependencies = [packageDict[name] for name in value.keys() if name.count(".") == 2]
 
-    Settings.scriptDir  = os.path.dirname(os.path.abspath(args[0]))
-    Settings.blurb      = Settings.blurb.replace("%program%", os.path.basename(args[0]))
-    Settings.usage      = string.join(Settings.variables.generate_help(Settings.blurb), "\n")
-    for (variable, default) in Settings.options.iteritems():
-      if type(default) == type(""):
-        Settings.options[variable]  = default.replace("%script-dir%", Settings.scriptDir)
-    for (variable, default) in Settings.options.iteritems():
-      Settings.usage    = re.compile("%" + variable.replace("-", "-\s*") + "%") \
-                            .sub(str(default), Settings.usage)
+    # find and return the top level config
+    return packageDict[toplevel]
 
-    try:
-      (arguments, options)  = Settings.variables.getopt(args[1:])
-    except distutils.errors.DistutilsArgError, details:
-      print "Error in command-line:", details
-      print Settings.usage
-      sys.exit(Error.usage)
 
-    # Get the dictionary of option -> value
-    Settings.options.update(dict(Settings.variables.get_option_order()))
-
-    # Special options
-    convertSetting("search-mask",   toList,   "%s must be a semi-colon separated list.")
-    convertSetting("dump-mask",   toList,   "%s must be a semi-colon separated list.")
-    convertSetting("veto-dump-mask",toList,   "%s must be a semi-colon separated list.")
-    convertSetting("search-paths",  lambda x: toList(x, os.path.pathsep), "%s must be a " + os.path.pathsep + " separated list.")
-
-    #if Settings.options["help"] or len(arguments) != 1:
-    #  print Settings.usage
-    #  print
-    #  sys.exit(Error.none)
- 
-    return arguments
-
-  def crawlInto(self, filePath):
-    maxDepth      = Settings.options["max-depth"]
-
-    if (maxDepth >= 0 and self.__levels > maxDepth):  return
-    fullPath  = goGetIt(filePath)
-    self.__doer.do(filePath, self.__levels)
+def getDependenciesFromConfig(filename,toplevelname,path):
+    imports = [filename]
+    config = open(filename)
     
-    if fullPath in self.__alreadyDone: return
-    aFile     = open(fullPath)
-    content   = aFile.read()
-    aFile.close()
+    for line in config.readlines():
+        # look for all load statements
+        if "process.load" in line:
+            moduleName = line.split("'")[1]
+            module = __import__(moduleName,[],[],"*")
+            imports.append(inspect.getsourcefile(module))
+    config.close()
 
-    # Content 
-    if self.__levels >= Settings.options["min-dump-depth"]                \
-      and isRelevant(filePath, Settings.options["dump-mask"])         \
-      and not isRelevant(filePath, Settings.options["veto-dump-mask"]):
-      self.__doer.doContent(content)
+    # sort our loaded files alphabetically in Subsytem/Package pattern
+    imports.sort(key= lambda x: x.split("python/")[-1])
 
-    self.__alreadyDone.append(fullPath)
+    # hold a dict of the kind: {package : {dependencies}}
+    globalDependencyDict = {}
 
-    # Search for includes
-    includes  = []
-    for matchImports in Settings.matchImports:
-      includes.extend(matchImports.findall(content))
+    # create the flat include dictionaries for each load and merge them
+    for item in imports:
+        modulefinder = mymf(path)
+        modulefinder.run_script(item)
 
-    for include in includes:
-      anotherFile = fileName(include)
-      if isRelevant(anotherFile, Settings.options["search-mask"]):
-        self.__levels += 1
-        self.crawlInto(anotherFile)
-        self.__levels -= 1
+        for key, value in modulefinder._depgraph.iteritems():
+            if key == "__main__": key = packageNameFromFilename(item)
+            globalDependencyDict[key] = value
+
+    # now set the dependencies of our top level config with the load statements
+    globalDependencyDict[toplevelname] = {}
+    for item in imports:
+        name = packageNameFromFilename(item)
+        if name != toplevelname:
+            globalDependencyDict[toplevelname][name] = 1
+    return globalDependencyDict                                                
 
 
-  
-if __name__ == '__main__':
-  pass
+def getDependenciesFromPythonFile(filename,toplevelname,path):
+    modulefinder = mymf(path)
+    modulefinder.run_script(filename)
+    globalDependencyDict = modulefinder._depgraph
+    globalDependencyDict[toplevelname] = globalDependencyDict["__main__"] 
+    return globalDependencyDict
+
+
+def getImportTree(filename,path):
+    config = __import__(filename.rstrip(".py"))
+    toplevelname = packageNameFromFilename(filename)
+    # get dependencies from given file
+    if hasattr(config,"process"):
+        globalDependencyDict = getDependenciesFromConfig(filename,toplevelname,path)
+    else:
+        globalDependencyDict = getDependenciesFromPythonFile(filename,toplevelname,path)
+    # transform this flat structure in a dependency tree
+    dependencyGraph = transformIntoGraph(globalDependencyDict,toplevelname)
+    return dependencyGraph                                               
