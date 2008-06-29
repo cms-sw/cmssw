4166f056f1dfe133ec750849bfc61974f7fc9390
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-06-29 17:44:15 +0000

    Remove obsolete #include "PluginManager/ModuleDef.h"

--- OnlineDB/CSCCondDB/src/SealModule.cc
+++ OnlineDB/CSCCondDB/src/SealModule.cc
@@ -1,4 +1,3 @@
-#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "OnlineDB/CSCCondDB/interface/CSCAFEBAnalyzer.h"
