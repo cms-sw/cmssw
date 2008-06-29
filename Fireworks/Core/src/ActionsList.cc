8b1eb2d170882b930a9f912cbace4cf207e6efaf
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-06-29 13:11:25 +0000

    first submission

---
+++ Fireworks/Core/src/ActionsList.cc
@@ -0,0 +1,27 @@
+#include "Fireworks/Core/interface/ActionsList.h"
+
+const std::string cmsshow::sHome = "Home";
+const std::string cmsshow::sAddRhoPhi = "Rho Phi";
+const std::string cmsshow::sAddRhoZ = "Rho Z";
+const std::string cmsshow::sAddLego = "Lego";
+const std::string cmsshow::sOpenData = "Open Data File";
+const std::string cmsshow::sLoadConfig = "Load Configuration";
+const std::string cmsshow::sSaveConfig = "Save Configuration";
+const std::string cmsshow::sSaveConfigAs = "Save Configuration";
+const std::string cmsshow::sExportImage = "Export Main Viewer Image";
+const std::string cmsshow::sQuit = "Quit";
+const std::string cmsshow::sUndo = "Undo";
+const std::string cmsshow::sRedo = "Redo";
+const std::string cmsshow::sCut = "Cut";
+const std::string cmsshow::sCopy = "Copy";
+const std::string cmsshow::sPaste = "Paste";
+const std::string cmsshow::sNextEvent = "Goto Next Event";
+const std::string cmsshow::sPreviousEvent = "Goto Previous Event";
+const std::string cmsshow::sPlayEvents = "Play Events";
+const std::string cmsshow::sPlayEventsBack = "Play Events Backwards";
+const std::string cmsshow::sPause = "Pause";
+const std::string cmsshow::sShowObjInsp = "Show Object Inspector";
+const std::string cmsshow::sShowEventDisplayInsp = "Show Event Display Inspector";
+const std::string cmsshow::sShowMainViewCtl = "Show Main View Controller";
+const std::string cmsshow::sHelp = "cmsShow Help...";
+const std::string cmsshow::sKeyboardShort = "Keyboard Shortcuts...";
