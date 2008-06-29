ff84d6e89b46b0799639e5a2b58adf09c96b2d20
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-06-29 13:06:10 +0000

    constants for all names

--- Fireworks/Core/interface/ActionsList.h
+++ Fireworks/Core/interface/ActionsList.h
@@ -1,4 +1,29 @@
-const std::string sNextEvent = "Goto Next Event";
-const std::string sPreviousEvent = "Goto Previous Event";
-const std::string sOpenData = "Open Data File...";
-const std::string sQuit = "Quit";
+#include <string>
+
+namespace cmsshow {
+  extern const std::string sHome;
+  extern const std::string sAddRhoPhi;
+  extern const std::string sAddRhoZ;
+  extern const std::string sAddLego;
+  extern const std::string sOpenData;
+  extern const std::string sLoadConfig;
+  extern const std::string sSaveConfig;
+  extern const std::string sSaveConfigAs;
+  extern const std::string sExportImage;
+  extern const std::string sQuit;
+  extern const std::string sUndo;
+  extern const std::string sRedo;
+  extern const std::string sCut;
+  extern const std::string sCopy;
+  extern const std::string sPaste;
+  extern const std::string sNextEvent;
+  extern const std::string sPreviousEvent;
+  extern const std::string sPlayEvents;
+  extern const std::string sPlayEventsBack;
+  extern const std::string sPause;
+  extern const std::string sShowObjInsp;
+  extern const std::string sShowEventDisplayInsp;
+  extern const std::string sShowMainViewCtl;
+  extern const std::string sHelp;
+  extern const std::string sKeyboardShort;
+}
