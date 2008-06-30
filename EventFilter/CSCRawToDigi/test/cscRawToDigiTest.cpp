5719b08a29ed3a916f0090a9d91ceb9fa40c409d
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-06-30 22:35:42 +0000

    Include a self-test for CSCEventData packing

--- EventFilter/CSCRawToDigi/test/cscRawToDigiTest.cpp
+++ EventFilter/CSCRawToDigi/test/cscRawToDigiTest.cpp
@@ -1,11 +1,13 @@
 #include "EventFilter/CSCRawToDigi/interface/CSCTMBHeader.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCTMBData.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCALCTHeader.h"
+#include "EventFilter/CSCRawToDigi/interface/CSCEventData.h"
 
 int main()
 {
   CSCTMBHeader::selfTest();
   CSCALCTHeader::selfTest();
   CSCTMBData::selfTest();
+  CSCEventData::selfTest();
 }
 
