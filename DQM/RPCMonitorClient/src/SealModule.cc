45613e6d2eeddd9f42c1dc814cf12ae49afd7fcf
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2008-06-30 14:03:44 +0000

    new report summary

--- DQM/RPCMonitorClient/src/SealModule.cc
+++ DQM/RPCMonitorClient/src/SealModule.cc
@@ -3,10 +3,10 @@
 
 #include <DQM/RPCMonitorClient/interface/RPCQualityTests.h>
 #include <DQM/RPCMonitorClient/interface/RPCEventSummary.h>
-#include <DQM/RPCMonitorClient/interface/RPCTriggerFilter.h>
+//#include <DQM/RPCMonitorClient/interface/RPCTriggerFilter.h>
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RPCQualityTests);
 DEFINE_ANOTHER_FWK_MODULE(RPCEventSummary);
-DEFINE_ANOTHER_FWK_MODULE(RPCTriggerFilter);
+//DEFINE_ANOTHER_FWK_MODULE(RPCTriggerFilter);
 
