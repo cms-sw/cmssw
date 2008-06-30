ee754a9fde78ac814ef7048f42f65ab9a01e9537
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-06-30 16:10:04 +0000

    start and stop time added

--- CondFormats/RunInfo/interface/RunNumber.h
+++ CondFormats/RunInfo/interface/RunNumber.h
@@ -1,25 +1,34 @@
 #ifndef RunNumber_h
 #define RunNumber_h
-*
+
+#include <iostream>
+#include<vector>
+
+/*
  *  \class RunNumber
  *  
- *  hold runnumber info from Run Control
- *  
+ *  hosting runinfo information, above all the runnumber 
+ *
  *  \author Michele de Gruttola (degrutto) - INFN Naples / CERN (June-12-2008)
  *
 */
 
-#include <iostream>
-#include<vector>
 
 class RunNumber {
 public:
   struct Item {
     Item(){}
     ~Item(){}
-    int m_index;
+    int m_run;
+    long long  m_id_start;
+    long long  m_id_stop;
     std::string m_number;
     std::string m_name;
+    signed long long m_start_time_sll;
+    std::string m_start_time_str;
+    signed long long m_stop_time_sll;
+    std::string m_stop_time_str;
+    int  m_lumisections;
   };
   RunNumber();
   virtual ~RunNumber(){}
