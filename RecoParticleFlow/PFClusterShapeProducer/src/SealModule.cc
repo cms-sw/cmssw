799fbd8ac19570bec63d828a0f0d8a307f84e56f
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-06-30 12:28:50 +0000

    Remove obsolete #include "PluginManager/ModuleDef.h" Remove Foundation/PluginManager dependence in BuildFile Remove obsolete DEFINE_SEAL_MODULE macro Change DEFINE_ANOTHER_FWK_MODULE to standard DEFINE_FWK_MODULE

--- RecoParticleFlow/PFClusterShapeProducer/src/SealModule.cc
+++ RecoParticleFlow/PFClusterShapeProducer/src/SealModule.cc
@@ -1,6 +1,3 @@
-#include "PluginManager/ModuleDef.h"
 #include "RecoParticleFlow/PFClusterShapeProducer/interface/PFClusterShapeProducer.h"
 
-DEFINE_SEAL_MODULE();
-
-DEFINE_ANOTHER_FWK_MODULE(PFClusterShapeProducer);
+DEFINE_FWK_MODULE(PFClusterShapeProducer);
