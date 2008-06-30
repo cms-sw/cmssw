d1bfa3fc0564fcc0c26c2d6d97322a945c1e2fae
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2008-06-30 14:17:39 +0000

    pythia parameters updated to python

--- GeneratorInterface/PyquenInterface/python/pyquenPythiaDefault_cfi.py
+++ GeneratorInterface/PyquenInterface/python/pyquenPythiaDefault_cfi.py
@@ -1,51 +1,46 @@
-# The following comments couldn't be translated into the new config version:
+#Default Pythia Paramters for Hydjet & Pyquen
 
-# ! hard QCD on',
-# ! to avoid stopping run',
-# ! tolerance parameter to adjust fragmentation',
-# ! pp multiple scattering off',
-# ! b quark mass',
-# ! t quark mass'
-# ! ptMin
-# q+q->q+q
-# q+qbar->q+qbar
-# q+qbar->g+g
-# q+g->q+g
-# g+g->q+qbar
-# g+g->g+g
-# q+qbar->g+gamma
-# q+qbar->gamma+gamma
-# q+g->q+gamma
-# g+g->gamma+gamma
-# g+g->g+gamma
-# ! Only user defined processes,
 import FWCore.ParameterSet.Config as cms
 
-pythiaDefault = cms.vstring('MSEL=1', 
-    'MSTU(21)=1', 
-    'PARU(14)=1.', 
-    'MSTP(81)=0', 
-    'PMAS(5,1)=4.8', 
-    'PMAS(6,1)=175.0', 
-    'CKIN(3)=7.')
-pythiaJets = cms.vstring('MSUB(11)=1', 
-    'MSUB(12)=1', 
-    'MSUB(13)=1', 
-    'MSUB(28)=1', 
-    'MSUB(53)=1', 
-    'MSUB(68)=1')
-pythiaPromptPhotons = cms.vstring('MSUB(14)=1', 
-    'MSUB(18)=1', 
-    'MSUB(29)=1', 
-    'MSUB(114)=1', 
-    'MSUB(115)=1')
-orcaSettings = cms.vstring('MSEL=0', 
-    'PARP(67)=1.', 
-    'PARP(82)=1.9', 
-    'PARP(85)=0.33', 
-    'PARP(86)=0.66', 
-    'PARP(89)=1000.', 
-    'PARP(91)=1.0', 
-    'MSTJ(11)=3', 
-    'MSTJ(22)=2')
+pyquenPythiaDefaultBlock = cms.PSet(
+
+    pythiaDefault = cms.vstring('MSEL=1', # ! hard QCD on',
+                                    'MSTU(21)=1', # ! to avoid stopping run',
+                                    'PARU(14)=1.', # ! tolerance parameter to adjust fragmentation',
+                                    'MSTP(81)=0', # ! pp multiple scattering off',
+                                    'PMAS(5,1)=4.8', # ! b quark mass',
+                                    'PMAS(6,1)=175.0', # ! t quark mass'
+                                    'CKIN(3)=7.',# ! ptMin
+                                    'MSTJ(22)=2',
+                                    'PARJ(71)=10.' # Decays only if life time < 10mm
+                                ),
+    pythiaJets = cms.vstring('MSUB(11)=1', # q+q->q+q
+                                 'MSUB(12)=1', # q+qbar->q+qbar
+                                 'MSUB(13)=1', # q+qbar->g+g
+                                 'MSUB(28)=1', # q+g->q+g
+                                 'MSUB(53)=1', # g+g->q+qbar
+                                 'MSUB(68)=1' # g+g->g+g
+                             ),
+    pythiaPromptPhotons = cms.vstring('MSUB(14)=1', # q+qbar->g+gamma
+                                          'MSUB(18)=1', # q+qbar->gamma+gamma
+                                          'MSUB(29)=1', # q+g->q+gamma
+                                          'MSUB(114)=1', # g+g->gamma+gamma
+                                          'MSUB(115)=1' # g+g->g+gamma
+                                      ),
+    csa08Settings = cms.vstring('MSEL=0', # ! Only user defined processes,
+                                    'PARP(67)=1.',
+                                    'PARP(82)=1.9',
+                                    'PARP(85)=0.33',
+                                    'PARP(86)=0.66',
+                                    'PARP(89)=1000.',
+                                    'PARP(91)=1.0',
+                                    'MSTJ(11)=3',
+                                    'MSTJ(22)=2'
+                                ),
+    pythiaMuonCandidates = cms.vstring(
+    'CKIN(3)=20',
+    'MSTJ(22)=2',
+    'PARJ(71)=40.'
+    ),
+    )
 
