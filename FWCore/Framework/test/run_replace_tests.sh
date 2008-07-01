f9f8009dda4ddf126c5010721cbc7e272b3b748d
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2008-07-01 15:16:21 +0000

    Remove old style configuration files

--- FWCore/Framework/test/run_replace_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Pass in name and status
-function die { echo $1: status $2 ;  exit $2; }
-
-(cmsRun ${LOCAL_TEST_DIR}/test_replace_with_unnamed_esproducer.cfg ) || die 'Failure using test_replace_with_unnamed_esproducer.cfg' $?
-(cmsRun ${LOCAL_TEST_DIR}/test_replace_with_unnamed_essource.cfg ) || die 'Failure using test_replace_with_unnamed_essource.cfg' $?
