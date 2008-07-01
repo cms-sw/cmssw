3b9885df0ecbffd7d6d20d306eab085f1f8aab0f
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-07-01 14:55:34 +0000

    Add another sequence which only runs electron isolation

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -6,9 +6,14 @@ from RecoEgamma.EgammaPhotonProducers.conversionSequence_cff import *
 from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
 from RecoEgamma.PhotonIdentification.photonId_cff import *
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
+from RecoEgamma.EgammaIsolationAlgos.gamIsolationSequence_cff import *
+from RecoEgamma.EgammaIsolationAlgos.eleIsolationSequence_cff import *
+
 
 egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
 egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDSequence*eIdSequence)
+egammarecoFullSmall = cms.Sequence(egammareco*eleIsolationSequence*photonIDSequence*eIdSequence)
+
 egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
 egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
 egammareco_withIsolation_woConvPhotons = cms.Sequence(egammareco_woConvPhotons*egammaIsolationSequence)
