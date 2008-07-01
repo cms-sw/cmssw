bf6c73d857fa9260cd3f455ceec7a7167241cc08
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2008-07-01 18:44:38 +0000

    update example

--- FastSimulation/Configuration/test/Example_cfg.py
+++ FastSimulation/Configuration/test/Example_cfg.py
@@ -18,17 +18,17 @@ process.load("FastSimulation/Configuration/RandomServiceInitialization_cff")
 # Generate H -> ZZ -> l+l- l'+l'- (l,l'=e or mu), with mH=180GeV/c2
 process.load("FastSimulation/Configuration/HZZllll_cfi")
 # Generate ttbar events
-#  process.load("FastSimulation/Configuration/data/ttbar.cfi")
+#  process.load("FastSimulation/Configuration/ttbar_cfi")
 # Generate multijet events with different ptHAT bins
-#  process.load("FastSimulation/Configuration/data/QCDpt80-120.cfi")
-#  process.load("FastSimulation/Configuration/data/QCDpt600-800.cfi")
+#  process.load("FastSimulation/Configuration/QCDpt80-120_cfi")
+#  process.load("FastSimulation/Configuration/QCDpt600-800_cfi")
 # Generate Minimum Bias Events
-#  process.load("FastSimulation/Configuration/data/MinBiasEvents.cfi")
+#  process.load("FastSimulation/Configuration/MinBiasEvents_cfi")
 # Generate muons with a flat pT particle gun, and with pT=10.
-# process.load("FastSimulation/Configuration/data/FlatPtMuonGun.cfi")
+# process.load("FastSimulation/Configuration/FlatPtMuonGun_cfi")
 # replace FlatRandomPtGunSource.PGunParameters.PartID={130}
 # Generate di-electrons with pT=35 GeV
-# process.load("FastSimulation/Configuration/data/DiElectrons.cfi")
+# process.load("FastSimulation/Configuration/DiElectrons_.cfi")
 
 # Famos sequences (Frontier conditions)
 process.load("FastSimulation/Configuration/CommonInputsFake_cff")
