4d9cec9bf205caed5d45e129718568cbd41cfb62
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2008-07-01 16:08:18 +0000

    Remove old style configuration files

--- FWCore/FWLite/test/RefTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -f ${LOCAL_TEST_DIR}/good.root
-rm -f ${LOCAL_TEST_DIR}/good2.root
-rm -f ${LOCAL_TEST_DIR}/good_delta5.root
-rm -f good.root
-rm -f good2.root
-rm -f good_delta5.root
-cmsRun ${LOCAL_TEST_DIR}/RefTest.cfg
-cmsRun ${LOCAL_TEST_DIR}/RefTest2.cfg
