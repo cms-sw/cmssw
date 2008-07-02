d4e3d9530afb8215b32654125e9574ce8d01601a
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2008-07-02 17:05:35 +0000

    added to the py cfi the new parameters for HcalIsolFilter and PhotonEcalIsolFilter

--- HLTrigger/Egamma/python/hltEgammaHcalIsolFilter_cfi.py
+++ HLTrigger/Egamma/python/hltEgammaHcalIsolFilter_cfi.py
@@ -1,11 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 hltEgammaHcalIsolFilter = cms.EDFilter("HLTEgammaHcalIsolFilter",
-    doIsolated = cms.bool(True),
+    HoverEt2cut = cms.double(0.0),
     nonIsoTag = cms.InputTag("hltSingleEgammaHcalNonIsol"),
     hcalisolbarrelcut = cms.double(3.0),
     HoverEcut = cms.double(0.05),
     hcalisolendcapcut = cms.double(3.0),
+    doIsolated = cms.bool(True),
     ncandcut = cms.int32(1),
     isoTag = cms.InputTag("hltSingleEgammaHcalIsol"),
     candTag = cms.InputTag("hltSingleEgammaEtFilter")
--- HLTrigger/Egamma/python/hltPhotonEcalIsolFilter_cfi.py
+++ HLTrigger/Egamma/python/hltPhotonEcalIsolFilter_cfi.py
@@ -1,15 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# producer for hltPhotonEcalIsolFilter
-#
 hltPhotonEcalIsolFilter = cms.EDFilter("HLTEgammaEcalIsolFilter",
     doIsolated = cms.bool(True),
     nonIsoTag = cms.InputTag("hltPhotonEcalNonIsol"),
     ncandcut = cms.int32(1),
+    ecalIsoOverEt2Cut = cms.double(0.0),
     ecalisolcut = cms.double(1.5),
     isoTag = cms.InputTag("hltPhotonEcalIsol"),
-    candTag = cms.InputTag("hltPhotonEtFilter")
+    candTag = cms.InputTag("hltPhotonEtFilter"),
+    ecalIsoOverEtCut = cms.double(0.05)
 )
 
 
