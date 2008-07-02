61c4bc7c66a7b3170d5dfc1878c5e279788e1573
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2008-07-02 16:10:57 +0000

    removed the *cff_py files (taken from confDB) and the unused pixelSeedConfigurationsForHLT_cfi.py

--- RecoEgamma/EgammaHLTProducers/python/l1NonIsoRecoEcalCandidate_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoEgamma.EgammaHLTProducers.hltRecoEcalCandidate_cfi import *
-import copy
-from RecoEgamma.EgammaHLTProducers.hltRecoEcalCandidate_cfi import *
-l1NonIsoRecoEcalCandidate = copy.deepcopy(hltRecoEcalCandidate)
-l1NonIsoRecoEcalCandidate.scHybridBarrelProducer = 'correctedHybridSuperClustersL1NonIsolated'
-l1NonIsoRecoEcalCandidate.scIslandEndcapProducer = 'correctedEndcapSuperClustersWithPreshowerL1NonIsolated'
-
--- RecoEgamma/EgammaHLTProducers/python/pixelMatchElectronTrackingSequenceForHLT_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# electron tracking sequence after pixel seeding
-# $Id: pixelMatchElectronTrackingSequenceForHLT.cff,v 1.3 2007/02/23 11:36:17 monicava Exp $
-pixelMatchElectronTrackingSequenceForHLT = cms.Sequence(cms.SequencePlaceholder("ckfTrackCandidatesBarrel")+cms.SequencePlaceholder("ckfTrackCandidatesEndcap")+cms.SequencePlaceholder("ctfWithMaterialTracksBarrel")+cms.SequencePlaceholder("ctfWithMaterialTracksEndcap")+cms.SequencePlaceholder("pixelMatchElectronsForHLT"))
-
