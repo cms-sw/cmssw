38ff6e91a74e2f934c138e25995e9f3f928525d9
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-07-02 08:53:43 +0000

    update to mirror the cfi

--- Validation/EcalClusters/python/egammaSCAnalyzer_cfi.py
+++ Validation/EcalClusters/python/egammaSCAnalyzer_cfi.py
@@ -2,24 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 #
 #  Author: Michael A. Balazs, University of Virginia
-#  $Id: egammaSCAnalyzer_cfi.py,v 1.3 2008/05/28 20:07:58 fabiocos Exp $
+#  $Id: egammaSCAnalyzer.cfi,v 1.10 2008/06/17 12:27:23 ferriff Exp $
 #
 from Validation.EcalClusters.VerificationCommonParameters_cfi import *
 egammaSuperClusterAnalyzer = cms.EDFilter("EgammaSuperClusters",
     VerificationCommonParameters,
     hist_max_Size = cms.double(5.0),
-    barrelSuperClusterCollection = cms.InputTag("correctedHybridSuperClusters"),
     hist_min_EToverTruth = cms.double(0.5),
+    barrelRecHitCollection = cms.InputTag("reducedEcalRecHitsEB"),
     hist_max_deltaEta = cms.double(0.025),
     hist_max_EToverTruth = cms.double(1.5),
     hist_min_Eta = cms.double(-2.5),
     hist_bins_S25toE = cms.int32(50),
     hist_min_deltaEta = cms.double(-0.025),
-    endcapSuperClusterCollection = cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"),
     hist_bins_Eta = cms.int32(91),
     hist_max_S25toE = cms.double(1.0),
     hist_bins_Phi = cms.int32(181),
     hist_bins_EToverTruth = cms.int32(100),
+    barrelSuperClusterCollection = cms.InputTag("correctedHybridSuperClusters"),
+    endcapSuperClusterCollection = cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"),
     hist_bins_S1toS9 = cms.int32(50),
     hist_min_ET = cms.double(0.0),
     hist_bins_deltaEta = cms.int32(51),
@@ -32,11 +33,11 @@ egammaSuperClusterAnalyzer = cms.EDFilter("EgammaSuperClusters",
     hist_min_NumBC = cms.double(0.0),
     hist_bins_Size = cms.int32(5),
     hist_min_S25toE = cms.double(0.0),
+    endcapRecHitCollection = cms.InputTag("reducedEcalRecHitsEE"),
     hist_min_Phi = cms.double(-3.14159),
     hist_max_NumBC = cms.double(10.0),
     hist_bins_ET = cms.int32(50),
     hist_bins_NumBC = cms.int32(10)
 )
-
 
 
