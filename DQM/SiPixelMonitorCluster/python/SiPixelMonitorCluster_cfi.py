4f68cbda683eeda4c8f9aa1566e87e26a4ad0485
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-07-02 08:19:51 +0000

    update config parameters for offline DQM running at Tier-0

--- DQM/SiPixelMonitorCluster/python/SiPixelMonitorCluster_cfi.py
+++ DQM/SiPixelMonitorCluster/python/SiPixelMonitorCluster_cfi.py
@@ -4,6 +4,15 @@ SiPixelClusterSource = cms.EDFilter("SiPixelClusterSource",
     src = cms.InputTag("siPixelClusters"),
     outputFile = cms.string('Pixel_DQM_Cluster.root'),
     saveFile = cms.untracked.bool(False)
+    isPIB = cms.untracked.bool(False),
+    slowDown = cms.untracked.bool(False),
+    modOn = cms.untracked.bool(True),
+    ladOn = cms.untracked.bool(False),
+    layOn = cms.untracked.bool(False),
+    phiOn = cms.untracked.bool(False),
+    ringOn = cms.untracked.bool(False),
+    bladeOn = cms.untracked.bool(False),
+    diskOn = cms.untracked.bool(False)
 )
 
 
