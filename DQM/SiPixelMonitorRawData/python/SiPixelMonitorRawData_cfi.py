a20149e99b87a1428013b30ce362974d80cd06be
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-07-02 08:50:27 +0000

    bugfix: typo: missing commas

--- DQM/SiPixelMonitorRawData/python/SiPixelMonitorRawData_cfi.py
+++ DQM/SiPixelMonitorRawData/python/SiPixelMonitorRawData_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 SiPixelRawDataErrorSource = cms.EDFilter("SiPixelRawDataErrorSource",
     src = cms.InputTag("siPixelDigis"),
     outputFile = cms.string('Pixel_DQM_Error.root'),
-    saveFile = cms.untracked.bool(False)
-    isPIB = cms.untracked.bool(False)
+    saveFile = cms.untracked.bool(False),
+    isPIB = cms.untracked.bool(False),
     slowDown = cms.untracked.bool(False)
 )
 
