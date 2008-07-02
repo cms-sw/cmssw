96e9f3b2469a0abf224205a6ae9e19a595805cef
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2008-07-02 15:25:54 +0000

    new parameters

--- Validation/EcalDigis/python/ecalSelectiveReadoutValidation_cfi.py
+++ Validation/EcalDigis/python/ecalSelectiveReadoutValidation_cfi.py
@@ -1,23 +1,45 @@
 import FWCore.ParameterSet.Config as cms
 
 ecalSelectiveReadoutValidation = cms.EDFilter("EcalSelectiveReadoutValidation",
-    TrigPrimCollection = cms.InputTag("simEcalTriggerPrimitiveDigis"),
-    outputFile = cms.untracked.string('EcalSelectiveReadoutValidationHistos.root'),
-    verbose = cms.untracked.bool(True),
-    EeDigiCollection = cms.InputTag("simEcalDigis","eeDigis"),
+    #Input collection names:
     EbDigiCollection = cms.InputTag("simEcalDigis","ebDigis"),
-    EeRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-    EeUnsuppressedDigiCollection = cms.InputTag("simEcalUnsuppressedDigis"),
-    EbRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
-    EbSimHitCollection = cms.InputTag("g4SimHits","EcalHitsEB"),
+    EeDigiCollection = cms.InputTag("simEcalDigis","eeDigis"),
     EbUnsuppressedDigiCollection = cms.InputTag("simEcalUnsuppressedDigis"),
-    weights = cms.vdouble(-0.295252, -0.295252, -0.295252, -0.286034, 0.240376,  ##my computation
-
-        0.402839, 0.322126, 0.172504, 0.0339461, 0.0),
-    EeSrFlagCollection = cms.InputTag("simEcalDigis","eeSrFlags"),
+    EeUnsuppressedDigiCollection = cms.InputTag("simEcalUnsuppressedDigis"),
     EbSrFlagCollection = cms.InputTag("simEcalDigis","ebSrFlags"),
+    EeSrFlagCollection = cms.InputTag("simEcalDigis","eeSrFlags"),
+    EbSimHitCollection = cms.InputTag("g4SimHits","EcalHitsEB"),
     EeSimHitCollection = cms.InputTag("g4SimHits","EcalHitsEE"),
-    LocalReco = cms.bool(True)
+    TrigPrimCollection = cms.InputTag("simEcalTriggerPrimitiveDigis"),
+    EbRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+    EeRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+    FEDRawCollection = cms.InputTag("source"),
+
+    #Versbose mode switch:
+    verbose = cms.untracked.bool(True),
+
+    #Name of the output histrogram root file:
+    outputFile = cms.untracked.string('EcalSelectiveReadoutValidationHistos.root'),
+
+    #Switch to enable locale amplitude reconstruction from digis instead 
+    #of RecHit's:
+    LocalReco = cms.bool(True),
+
+    #Weights used for the local reconstruction of the signal amplitude:
+    weights = cms.vdouble(-0.295252, -0.295252, -0.295252, -0.286034, 0.240376, 
+        0.402839, 0.322126, 0.172504, 0.0339461, 0.0),
+
+    #Switch to express TP in GeV for the histograms:
+    tpInGeV = cms.bool(True),
+
+    #ROOT/DQM directory where to store the histograms
+    histDir = cms.string('EcalDigisV/EcalDigiTask'),
+
+    #List of histograms to produce. Run the module once with LogInfo enabled
+    #in order to get the list of available histograms. If the list contains
+    #the keyword "all", every available histogram is produced.
+    histograms = cms.untracked.vstring('all')
 )
+
 
 
