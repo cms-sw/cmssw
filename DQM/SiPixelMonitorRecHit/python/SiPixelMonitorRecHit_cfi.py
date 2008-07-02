c6a6b714b21ab6b2ef79568d885d72758123a5ce
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-07-02 08:49:18 +0000

    updated config parameters for offline DQM running at Tier-0

--- DQM/SiPixelMonitorRecHit/python/SiPixelMonitorRecHit_cfi.py
+++ DQM/SiPixelMonitorRecHit/python/SiPixelMonitorRecHit_cfi.py
@@ -3,7 +3,16 @@ import FWCore.ParameterSet.Config as cms
 SiPixelRecHitSource = cms.EDFilter("SiPixelRecHitSource",
     src = cms.InputTag("siPixelRecHits"),
     outputFile = cms.string('Pixel_DQM_RecHits.root'),
-    saveFile = cms.untracked.bool(False)
+    saveFile = cms.untracked.bool(False),
+    isPIB = cms.untracked.bool(False),
+    slowDown = cms.untracked.bool(False),
+    modOn = cms.untracked.bool(True),
+    ladOn = cms.untracked.bool(False),
+    layOn = cms.untracked.bool(False),
+    phiOn = cms.untracked.bool(False),
+    ringOn = cms.untracked.bool(False),
+    bladeOn = cms.untracked.bool(False),
+    diskOn = cms.untracked.bool(False)
 )
 
 
