025a09c0e494367eafec3ed15cfd96dfcea65a59
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-07-02 15:47:51 +0000

    Whoops. Don't put python here

--- FWCore/Framework/python/cmsExceptionsFatal_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import FWCore.Framework.cmsExceptionsFatalOption_cff
-options = cms.untracked.PSet(
-  Rethrow = FWCore.Framework.cmsExceptionsFatalOption_cff.Rethrow
-)
