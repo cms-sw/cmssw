1c18477e9bde1573943a642e0232851af6a4cfc7
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-07-03 10:46:58 +0000

    Cleanup

--- EventFilter/StorageManager/scripts/operations/check_left_files_opensize.sh
+++ EventFilter/StorageManager/scripts/operations/check_left_files_opensize.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+# $Id:$
+
 # This script iterates over all files in the list of hosts provided and calls
 # injection script with --check option to show a status report.
 
 # Have a look at the configuration file
 source check_left_files.cfg
 
-
 if [ ! -r $HOSTLIST ]
 then
-	echo "ERROR: File $HOSTLIST missing!"
-	exit 1
+    echo "ERROR: File $HOSTLIST missing!"
+    exit 1
 fi
 echo "File $HOSTLIST is 0K"
 echo;echo;echo;
 
 for host in $( cat $HOSTLIST )
 do
-	echo Processing $host:
+  echo Processing $host:
 
-	echo;echo;echo OPEN FILES
-	ssh $host "du -h $CHECK_PATH_OPEN"
+  echo;echo;echo OPEN FILES
+  ssh $host "du -h $CHECK_PATH_OPEN"
 done
 exit 0
