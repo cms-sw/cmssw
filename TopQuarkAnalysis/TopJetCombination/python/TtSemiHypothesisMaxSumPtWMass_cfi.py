918c302231459fdf60cfcc10f88d7f67334870f9
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-03 10:01:45 +0000

    fixes

--- TopQuarkAnalysis/TopJetCombination/python/TtSemiHypothesisMaxSumPtWMass_cfi.py
+++ TopQuarkAnalysis/TopJetCombination/python/TtSemiHypothesisMaxSumPtWMass_cfi.py
@@ -4,11 +4,11 @@ import FWCore.ParameterSet.Config as cms
 # module to make the maxSumPtWMAss hypothesis
 #
 ttSemiHypothesisMaxSumPtWMass = cms.EDProducer("TtSemiHypothesisMaxSumPtWMass",
-    leps  = cms.InputTag("selectedLayer1Muons")
+    leps  = cms.InputTag("selectedLayer1Muons"),
     mets  = cms.InputTag("selectedLayer1METs"),
     jets  = cms.InputTag("selectedLayer1Jets"),
     match = cms.InputTag( ),
-    nJetsMax = cms.uint32(4),
+    nJetsMax = cms.uint32(4)
 )
 
 
