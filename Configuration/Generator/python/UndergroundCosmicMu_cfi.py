2a25243b77e8b2b72d6b42bf94bca8b5870565fd
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-07-03 16:22:09 +0000

    Add configuration for beam halo and underground cosmics data

---
+++ Configuration/Generator/python/UndergroundCosmicMu_cfi.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("CosMuoGenSource",
+    MinP = cms.double(10.0),
+    MinP_CMS = cms.double(-1.0), ##negative means MinP_CMS = MinP. Only change this if you know what you are doing!
+    MaxP = cms.double(3000.0),
+    MinTheta = cms.double(0.0),
+    MaxTheta = cms.double(75.),
+    MinPhi = cms.double(0.0),
+    MaxPhi = cms.double(360.0),
+    MinT0 = cms.double(-12.5),
+    MaxT0 = cms.double(12.5),
+    ElossScaleFactor = cms.double(1.0),
+    RadiusOfTarget = cms.double(8000.0),
+    ZDistOfTarget = cms.double(15000.0),
+    TrackerOnly = cms.bool(False),
+    TIFOnly_constant = cms.bool(False),
+    TIFOnly_linear = cms.bool(False),
+    MTCCHalf = cms.bool(False),
+    Verbosity = cms.bool(False)
+)
+
+
