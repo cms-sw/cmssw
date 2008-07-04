e822f20ce3af098d460d1915095a87914a605ef7
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-07-04 15:18:10 +0000

    added internal et sum class

--- DataFormats/L1GlobalCaloTrigger/src/classes.h
+++ DataFormats/L1GlobalCaloTrigger/src/classes.h
@@ -8,9 +8,14 @@
 
 namespace {
   namespace {
-    L1GctInternEmCandCollection internEmCand;
+    // internal collections
+    L1GctInternEmCandCollection internEmColl;
+    L1GctInternJetDataCollection internJetDataColl;
+    L1GctInternEtSumCollection internEtSumColl;
+    L1GctFibreCollection fibreWordColl;
+
+    // output collections
     L1GctEmCandCollection emCand;
-    L1GctInternJetDataCollection jetData;
     L1GctJetCandCollection jetCand;
     L1GctEtTotal etTot;
     L1GctEtHad etHad;
@@ -20,13 +25,16 @@ namespace {
     L1GctEtTotalCollection etTotColl;
     L1GctEtHadCollection etHadColl;
     L1GctJetCountsCollection jetCountsColl;
-    L1GctFibreWord fibreWord;
 
-    edm::Wrapper<L1GctInternEmCandCollection> w_internEmCand;
+    // wrapped internal collections
+    edm::Wrapper<L1GctInternEmCandCollection> w_internEmCandColl;
+    edm::Wrapper<L1GctInternJetDataCollection> w_internJetDataColl;
+    edm::Wrapper<L1GctInternEtSumCollection> w_internEtSumColl;
+    edm::Wrapper<L1GctFibreCollection> w_fibreWordColl;
+
+    // wrapped output collections
     edm::Wrapper<L1GctEmCandCollection> w_emCand;
-    edm::Wrapper<L1GctInternJetDataCollection> w_jetData;
     edm::Wrapper<L1GctJetCandCollection> w_jetCand;
-    edm::Wrapper<L1GctFibreCollection> w_fibreWord;
     edm::Wrapper<L1GctEtTotal> w_etTot;
     edm::Wrapper<L1GctEtHad> w_etHad;
     edm::Wrapper<L1GctEtMiss> w_etMiss;
@@ -36,11 +44,9 @@ namespace {
     edm::Wrapper<L1GctEtMissCollection> w_etMissColl;
     edm::Wrapper<L1GctJetCountsCollection> w_jetCountsColl;
 
-    edm::Ref<L1GctInternEmCandCollection> internEmRef ;
+    // references, used by L1Extra
     edm::Ref<L1GctEmCandCollection> emRef ;
-    edm::Ref<L1GctInternJetDataCollection> jetDataRef ;
     edm::Ref<L1GctJetCandCollection> jetRef ;
-    edm::Ref<L1GctFibreCollection> fibreRef ;
     edm::RefProd<L1GctEtTotal> etTotRef ;
     edm::RefProd<L1GctEtHad> etHadRef ;
     edm::RefProd<L1GctEtMiss> etMissRef ;
