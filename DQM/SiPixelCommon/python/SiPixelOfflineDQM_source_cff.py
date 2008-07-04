a3ee048e104a53675530d6abf234ba8c16afa0f1
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-07-04 10:43:43 +0000

    updated parameters fro offline DQM at Tier-0

--- DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
+++ DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
@@ -1,17 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
-# Pixel Digi Monitoring
 from DQM.SiPixelMonitorDigi.SiPixelMonitorDigi_cfi import *
-siPixelOfflineDQM_source = cms.Sequence(SiPixelDigiSource)
+from DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi import *
+from DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi import *
+siPixelOfflineDQM_source = cms.Sequence(SiPixelDigiSource*SiPixelClusterSource*SiPixelRecHitSource)
 SiPixelDigiSource.saveFile = False
-SiPixelDigiSource.isPIB = True
-SiPixelDigiSource.slowDown = False
-#online/offline
-SiPixelDigiSource.modOn = True
+SiPixelClusterSource.saveFile = False
+SiPixelRecHitSource.saveFile = False
+SiPixelDigiSource.modOn = False
 SiPixelDigiSource.ladOn = True
 SiPixelDigiSource.layOn = True
 SiPixelDigiSource.phiOn = True
 SiPixelDigiSource.bladeOn = True
 SiPixelDigiSource.diskOn = True
 SiPixelDigiSource.ringOn = True
+SiPixelClusterSource.modOn = False
+SiPixelClusterSource.ladOn = True
+SiPixelClusterSource.layOn = True
+SiPixelClusterSource.phiOn = True
+SiPixelClusterSource.bladeOn = True
+SiPixelClusterSource.diskOn = True
+SiPixelClusterSource.ringOn = True
+SiPixelRecHitSource.modOn = False
+SiPixelRecHitSource.ladOn = True
+SiPixelRecHitSource.layOn = True
+SiPixelRecHitSource.phiOn = True
+SiPixelRecHitSource.bladeOn = True
+SiPixelRecHitSource.diskOn = True
+SiPixelRecHitSource.ringOn = True
 
