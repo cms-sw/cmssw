24e177755313303ebd20ca2e5b3d7ff3f0500f5d
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-07-04 01:41:49 +0000

    added MET for LEGO view

---
+++ Fireworks/Calo/interface/MetProxyEveLegoBuilder.h
@@ -0,0 +1,51 @@
+#ifndef Fireworks_Calo_MetProxyEveLegoBuilder_h
+#define Fireworks_Calo_MetProxyEveLegoBuilder_h
+// -*- C++ -*-
+//
+// Package:     Calo
+// Class  :     MetProxyEveLegoBuilder
+// 
+/**\class MetProxyEveLegoBuilder MetProxyEveLegoBuilder.h Fireworks/Calo/interface/MetProxyEveLegoBuilder.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  
+//         Created:  Sun Jan  6 23:42:33 EST 2008
+// $Id: MetProxyEveLegoBuilder.h,v 1.2 2008/06/09 19:54:03 chrjones Exp $
+//
+
+// system include files
+
+// user include files
+#include "Fireworks/Core/interface/FW3DLegoDataProxyBuilder.h"
+
+// forward declarations
+class MetProxyEveLegoBuilder : public FW3DLegoDataProxyBuilder
+{
+
+   public:
+      MetProxyEveLegoBuilder();
+      virtual ~MetProxyEveLegoBuilder();
+
+      // ---------- const member functions ---------------------
+      REGISTER_PROXYBUILDER_METHODS();
+   
+      // ---------- static member functions --------------------
+   private:
+      virtual void build(const FWEventItem* iItem, 
+			 TEveElementList** product);
+
+      MetProxyEveLegoBuilder(const MetProxyEveLegoBuilder&); // stop default
+
+      const MetProxyEveLegoBuilder& operator=(const MetProxyEveLegoBuilder&); // stop default
+
+      // ---------- member data --------------------------------
+};
+
+
+#endif
