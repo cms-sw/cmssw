9c9d1db37518cd6ec3ef5b9681e6943b58771077
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-07-04 15:51:23 +0000

    changed segments to qtresults

--- DQM/L1TMonitorClient/src/L1TEMUEventInfoClient.cc
+++ DQM/L1TMonitorClient/src/L1TEMUEventInfoClient.cc
@@ -86,27 +86,28 @@ void L1TEMUEventInfoClient::beginJob(const EventSetup& context){
   
   for (int i = 0; i < nSubsystems; i++) {    
 
-  if(i==0)  sprintf(histo,"L1TEMU_GT");
-  if(i==1)  sprintf(histo,"L1TEMU_GMT");
-  if(i==2)  sprintf(histo,"L1TEMU_GCT");
-  if(i==3)  sprintf(histo,"L1TEMU_RCT");
-  if(i==4)  sprintf(histo,"L1TEMU_DTTF");
-  if(i==5)  sprintf(histo,"L1TEMU_RPCTF");
-  if(i==6)  sprintf(histo,"L1TEMU_CSCTF");
-  if(i==7)  sprintf(histo,"L1TEMU_DTTPG");
-  if(i==8)  sprintf(histo,"L1TEMU_RPCTPG");
-  if(i==9)  sprintf(histo,"L1TEMU_CSCTPG");
-  if(i==10) sprintf(histo,"L1TEMU_ECAL");
-  if(i==11) sprintf(histo,"L1TEMU_HCAL");
-  if(i==12) sprintf(histo,"L1TEMU_EMUL");
-  if(i==13) sprintf(histo,"L1TEMU_Test1");
-  if(i==14) sprintf(histo,"L1TEMU_Test2");
-  if(i==15) sprintf(histo,"L1TEMU_Test3");
-  if(i==16) sprintf(histo,"L1TEMU_Test4");
-  if(i==17) sprintf(histo,"L1TEMU_Test5");
-  if(i==18) sprintf(histo,"L1TEMU_Test6");
-  if(i==19) sprintf(histo,"L1TEMU_Test7");
-  
+    switch(i){
+    case 0 :   sprintf(histo,"L1TEMU_ECAL");    break;
+    case 1 :   sprintf(histo,"L1TEMU_HCAL");    break;
+    case 2 :   sprintf(histo,"L1TEMU_RCT");     break;
+    case 3 :   sprintf(histo,"L1TEMU_GCT");     break;
+    case 4 :   sprintf(histo,"L1TEMU_DTTPG");   break;
+    case 5 :   sprintf(histo,"L1TEMU_DTTF");    break;
+    case 6 :   sprintf(histo,"L1TEMU_CSCTPG");  break;
+    case 7 :   sprintf(histo,"L1TEMU_CSCTF");   break;
+    case 8 :   sprintf(histo,"L1TEMU_RPC");     break;
+    case 9 :   sprintf(histo,"L1TEMU_GMT");     break;
+    case 10 :  sprintf(histo,"L1TEMU_GT");      break;
+    case 11 :  sprintf(histo,"L1TEMU_RPCTG");   break;
+    case 12 :  sprintf(histo,"L1TEMU_EMUL");    break;
+    case 13 :  sprintf(histo,"L1TEMU_Test1");   break;
+    case 14 :  sprintf(histo,"L1TEMU_Test2");   break;
+    case 15 :  sprintf(histo,"L1TEMU_Test3");   break;
+    case 16 :  sprintf(histo,"L1TEMU_Test4");   break;
+    case 17 :  sprintf(histo,"L1TEMU_Test5");   break;
+    case 18 :  sprintf(histo,"L1TEMU_Test6");   break;
+    case 19 :  sprintf(histo,"L1TEMU_Test7");   break;
+    }  
 //  if( reportSummaryContent_[i] = dbe_->get("L1T/EventInfo/reportSummaryContents/" + histo) ) 
 //  {
 //       dbe_->removeElement(reportSummaryContent_[i]->getName());
@@ -123,8 +124,8 @@ void L1TEMUEventInfoClient::beginJob(const EventSetup& context){
   }
 
   reportSummaryMap_ = dbe_->book2D("reportSummaryMap", "reportSummaryMap", 5, 0.,5., 4, 0., 4.);
-			    reportSummaryMap_->setAxisTitle("XXXX", 1);
-			      reportSummaryMap_->setAxisTitle("YYYY", 2);
+  reportSummaryMap_->setAxisTitle("Subsystem Index", 1);
+  reportSummaryMap_->setAxisTitle("Subsystem Index", 2);
 
 }
 
@@ -139,6 +140,137 @@ void L1TEMUEventInfoClient::beginLuminosityBlock(const LuminosityBlock& lumiSeg,
 
 void L1TEMUEventInfoClient::endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
                           const edm::EventSetup& c){
+
+  int nSubsystems = 20; 
+  for (int i = 0; i < nSubsystems; i++) {
+    summaryContent[i] = 1;    
+    reportSummaryContent_[i]->Fill(1.);
+  }
+  summarySum = 0;
+  
+
+  MonitorElement *ECAL_QHist = dbe_->get("L1TEMU/ECAL/ETPErrorFlag");
+  MonitorElement *HCAL_QHist = dbe_->get("L1TEMU/HCAL/HTPErrorFlag");
+  MonitorElement *RCT_QHist = dbe_->get("L1TEMU/RCT/RCTErrorFlag");
+  MonitorElement *GCT_QHist = dbe_->get("L1TEMU/GCT/GCTErrorFlag");
+  MonitorElement *DTTPG_QHist = dbe_->get("L1TEMU/DTTPG/DTPErrorFlag");
+  MonitorElement *DTTF_QHist = dbe_->get("L1TEMU/DTTF/DTFErrorFlag");
+  MonitorElement *CSCTPG_QHist = dbe_->get("L1TEMU/CSCTPG/CTPErrorFlag");
+  MonitorElement *CSCTF_QHist = dbe_->get("L1TEMU/CSCTF/CTFErrorFlag");
+  MonitorElement *RPC_QHist = dbe_->get("L1TEMU/RPC/RPCErrorFlag");
+  MonitorElement *GMT_QHist = dbe_->get("L1TEMU/GMT/GMTErrorFlag");
+  MonitorElement *GT_QHist = dbe_->get("L1TEMU/GT/GLTErrorFlag");
+
+  if(ECAL_QHist){
+    if(ECAL_QHist->getEntries())
+      summaryContent[0] = (ECAL_QHist->getBinContent(1)) / (ECAL_QHist->getEntries());
+    else summaryContent[0] = 1;
+    reportSummaryContent_[0]->Fill( summaryContent[0] );
+  }
+ 
+ //double ECAL_nEnt = ECAL_QHist->;
+//  if (ECAL_QHist){
+//    const QReport *ECAL_QReport = ECAL_QHist->getQReport("deDiffInXRange_ErrorFlag");
+//    int ECAL_nBadCh = ECAL_QReport->getBadChannels().size();
+//    cout << "RPC_nBadCh = " << ECAL_nBadCh << endl;
+//  }
+
+ 
+  if(HCAL_QHist){
+    if(HCAL_QHist->getEntries())
+       summaryContent[1] = (HCAL_QHist->getBinContent(1)) / (HCAL_QHist->getEntries());
+    else summaryContent[1] = 1;
+       reportSummaryContent_[1]->Fill( summaryContent[1] );
+  }
+  
+  if(RCT_QHist){
+    if(RCT_QHist->getEntries())
+      summaryContent[2] = (RCT_QHist->getBinContent(1)) / (RCT_QHist->getEntries());
+    else summaryContent[2] = 1;
+    reportSummaryContent_[2]->Fill( summaryContent[2] );
+  }
+  
+  if(GCT_QHist){
+    if(GCT_QHist->getEntries())
+      summaryContent[3] = (GCT_QHist->getBinContent(1)) / (GCT_QHist->getEntries());
+    else summaryContent[3] = 1;
+    reportSummaryContent_[3]->Fill( summaryContent[3] );
+  }
+
+  if(DTTPG_QHist){
+    if(DTTPG_QHist->getEntries())
+      summaryContent[4] = (DTTPG_QHist->getBinContent(1)) / (DTTPG_QHist->getEntries());
+    else summaryContent[4] = 1;
+    reportSummaryContent_[4]->Fill( summaryContent[4] );
+  }
+  
+  if(DTTF_QHist){
+    if(DTTF_QHist->getEntries())
+      summaryContent[5] = (DTTF_QHist->getBinContent(1)) / (DTTF_QHist->getEntries());
+    else summaryContent[5] = 1;
+    reportSummaryContent_[5]->Fill( summaryContent[5] );
+  }
+  
+  if(CSCTPG_QHist){
+    if(CSCTPG_QHist->getEntries())
+      summaryContent[6] = (CSCTPG_QHist->getBinContent(1)) / (CSCTPG_QHist->getEntries());
+    else summaryContent[6] = 1;
+    reportSummaryContent_[6]->Fill( summaryContent[6] );
+  }
+
+  if(CSCTF_QHist){
+    if(CSCTF_QHist->getEntries())
+      summaryContent[7] = (CSCTF_QHist->getBinContent(1)) / (CSCTF_QHist->getEntries());
+    else summaryContent[7] = 1;
+    reportSummaryContent_[7]->Fill( summaryContent[7] );
+  }
+  
+  if(RPC_QHist){
+    if(RPC_QHist->getEntries())
+      summaryContent[8] = (RPC_QHist->getBinContent(1)) / (RPC_QHist->getEntries());
+    else summaryContent[8] = 1;
+    reportSummaryContent_[8]->Fill( summaryContent[8] );
+  }
+  
+  if(GMT_QHist){
+    if(GMT_QHist->getEntries())
+      summaryContent[9] = (GMT_QHist->getBinContent(1)) / (GMT_QHist->getEntries());
+    else summaryContent[9] = 1;
+    reportSummaryContent_[9]->Fill( summaryContent[9] );
+  }
+  
+  if(GT_QHist){
+    if(GT_QHist->getEntries())
+      summaryContent[10] = (GT_QHist->getBinContent(1)) / (GT_QHist->getEntries());
+    else summaryContent[10] = 1;
+    reportSummaryContent_[10]->Fill( summaryContent[10] );
+  }
+  
+
+  for (int i = 0; i < nSubsystems; i++) {    
+    summarySum += summaryContent[i];
+  }
+  
+  reportSummary = summarySum / nSubsystems;
+  cout << "reportSummary " << reportSummary << endl;
+  if (reportSummary_) reportSummary_->Fill(reportSummary);
+
+  int jcount = 0;
+
+  //fill the known systems
+  for (int i = 0; i < nSubsystems; i++) {
+    cout << "summaryContent[" << i << "]" << summaryContent[i] << endl;
+    if((i%5)==0)jcount++;
+    reportSummaryMap_->setBinContent(i%5+1,jcount, summaryContent[i]);
+  }
+
+//   //fill the rest
+//   for (int i = 0; i < 5; i++) {    
+//     for (int j = 0; j < 4; j++) {    
+
+//      reportSummaryMap_->setBinContent( i, j, 1. );
+//     }
+//   }
 
 
 }
@@ -166,22 +298,6 @@ void L1TEMUEventInfoClient::analyze(const Event& e, const EventSetup& context){
     } 
   }   
 */ 
-  reportSummary = 1.;
-  if (reportSummary_) reportSummary_->Fill(reportSummary);
-
-
-  int nSubsystems = 20;
-  for (int i = 0; i < nSubsystems; i++) {    
-
-     reportSummaryContent_[i]->Fill(1.);
-  }
-
-  for (int i = 0; i < 5; i++) {    
-    for (int j = 0; j < 4; j++) {    
-
-     reportSummaryMap_->setBinContent( i, j, 1. );
-    }
-  }
 
 }
 
--- DQM/L1TMonitorClient/src/L1TEventInfoClient.cc
+++ DQM/L1TMonitorClient/src/L1TEventInfoClient.cc
@@ -84,39 +84,41 @@ void L1TEventInfoClient::beginJob(const EventSetup& context){
   
   char histo[100];
   
-  for (int i = 0; i < nSubsystems; i++) {    
+  for (int n = 0; n < nSubsystems; n++) {    
 
-    // ugly hack for cruzet3
-    if(i==0)  sprintf(histo,"L1T_GT");
-    if(i==1)  sprintf(histo,"L1T_GMT");
-    if(i==2)  sprintf(histo,"L1T_GCT");
-    if(i==3)  sprintf(histo,"L1T_RCT");
-    if(i==4)  sprintf(histo,"L1T_DTTF");
-    if(i==5)  sprintf(histo,"L1T_RPCTF");
-    if(i==6)  sprintf(histo,"L1T_CSCTF");
-    if(i==7)  sprintf(histo,"L1T_DTTPG");
-    if(i==8)  sprintf(histo,"L1T_RPCTPG");
-    if(i==9)  sprintf(histo,"L1T_CSCTPG");
-    if(i==10) sprintf(histo,"L1T_ECAL");
-    if(i==11) sprintf(histo,"L1T_HCAL");
-    if(i==12) sprintf(histo,"L1T_EMUL");
-    if(i==13) sprintf(histo,"L1T_Timing");
-    if(i==14) sprintf(histo,"L1T_Test1");
-    if(i==15) sprintf(histo,"L1T_Test2");
-    if(i==16) sprintf(histo,"L1T_Test3");
-    if(i==17) sprintf(histo,"L1T_Test4");
-    if(i==18) sprintf(histo,"L1T_Test5");
-    if(i==19) sprintf(histo,"L1T_Test6");
-
+    
+    switch(n){
+    case 0 :   sprintf(histo,"L1T_ECAL");    break;
+    case 1 :   sprintf(histo,"L1T_HCAL");    break;
+    case 2 :   sprintf(histo,"L1T_RCT");     break;
+    case 3 :   sprintf(histo,"L1T_GCT");     break;
+    case 4 :   sprintf(histo,"L1T_DTTPG");   break;
+    case 5 :   sprintf(histo,"L1T_DTTF");    break;
+    case 6 :   sprintf(histo,"L1T_CSCTPG");  break;
+    case 7 :   sprintf(histo,"L1T_CSCTF");   break;
+    case 8 :   sprintf(histo,"L1T_RPC");     break;
+    case 9 :   sprintf(histo,"L1T_GMT");     break;
+    case 10 :  sprintf(histo,"L1T_GT");      break;
+    case 11 :  sprintf(histo,"L1T_RPCTG");   break;
+    case 12 :  sprintf(histo,"L1T_EMUL");    break;
+    case 13 :  sprintf(histo,"L1T_Timing");  break;
+    case 14 :  sprintf(histo,"L1T_Test1");   break;
+    case 15 :  sprintf(histo,"L1T_Test2");   break;
+    case 16 :  sprintf(histo,"L1T_Test3");   break;
+    case 17 :  sprintf(histo,"L1T_Test4");   break;
+    case 18 :  sprintf(histo,"L1T_Test5");   break;
+    case 19 :  sprintf(histo,"L1T_Test6");   break;
+    }  
+    
   
-//  if( reportSummaryContent_[i] = dbe_->get("L1T/EventInfo/reportSummaryContents/" + histo) ) 
-//  {
-//       dbe_->removeElement(reportSummaryContent_[i]->getName());
-//   }
-  
-   reportSummaryContent_[i] = dbe_->bookFloat(histo);
+    //    if( reportSummaryContent_[i] = dbe_->get("L1T/EventInfo/reportSummaryContents/" + histo) ) 
+    //  {
+    //	dbe_->removeElement(reportSummaryContent_[i]->getName());
+    //  }
+    
+    reportSummaryContent_[n] = dbe_->bookFloat(histo);
   }
-
+  
 
   dbe_->setCurrentFolder("L1T/EventInfo");
 
@@ -125,8 +127,8 @@ void L1TEventInfoClient::beginJob(const EventSetup& context){
   }
 
   reportSummaryMap_ = dbe_->book2D("reportSummaryMap", "reportSummaryMap", 5, 0., 5., 4, 0., 4);
-			    reportSummaryMap_->setAxisTitle("XXXX", 1);
-			      reportSummaryMap_->setAxisTitle("YYYY", 2);
+  reportSummaryMap_->setAxisTitle("Subsystem Index", 1);
+  reportSummaryMap_->setAxisTitle("Subsystem Index", 2);
 
 }
 
@@ -140,53 +142,172 @@ void L1TEventInfoClient::beginLuminosityBlock(const LuminosityBlock& lumiSeg, co
 }
 
 void L1TEventInfoClient::endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
-                          const edm::EventSetup& c){
+                          const edm::EventSetup& c){   
 
 
-}
+  MonitorElement *GCT_QHist = dbe_->get("L1T/L1TGCT/NonIsoEmOccEtaPhi");
+  MonitorElement *RCT_QHist = dbe_->get("L1T/L1TRCT/RctNonIsoEmOccEtaPhi");
+  MonitorElement *GMT_QHist = dbe_->get("L1T/L1TGMT/GMT_etaphi");
+  MonitorElement *CSCTF_QHist = dbe_->get("L1T/L1TCSCTF/CSCTF_occupancies");
+  MonitorElement *DTTF_QHist = dbe_->get("L1T/L1TDTTF/DTTF_TRACKS/INTEG/dttf_p_phi_eta");
+  
+  int nSubsystems = 20;
+  for (int k = 0; k < nSubsystems; k++) {
+    summaryContent[k] = 1;
+    reportSummaryContent_[k]->Fill(1.);
+  }
+  summarySum = 0;
+
+  int GCT_nXCh = GCT_QHist->getNbinsX(); int GCT_nYCh = GCT_QHist->getNbinsY(); 
+  int RCT_nXCh = RCT_QHist->getNbinsX(); int RCT_nYCh = RCT_QHist->getNbinsY();
+  int GMT_nXCh = GMT_QHist->getNbinsX(); int GMT_nYCh = GMT_QHist->getNbinsY();
+  int CSCTF_nXCh = CSCTF_QHist->getNbinsX(); int CSCTF_nYCh = CSCTF_QHist->getNbinsY();
+  int DTTF_nXCh = DTTF_QHist->getNbinsX();  int DTTF_nYCh = DTTF_QHist->getNbinsY();
+
+  int GCT_nCh=0,RCT_nCh=0,GMT_nCh=0,CSCTF_nCh=0,DTTF_nCh=0;
+  
+  //cout << "CSCTF_nXCh = " << CSCTF_nXCh << ", CSCTF_nYCh = " << CSCTF_nYCh << endl;
+  if(GCT_nYCh) 
+    GCT_nCh = GCT_nXCh*GCT_nYCh;
+  if(RCT_nYCh) 
+    RCT_nCh = RCT_nXCh*RCT_nYCh;
+  if(GMT_nYCh) 
+    GMT_nCh = GMT_nXCh*GMT_nYCh;
+  if(CSCTF_nYCh) 
+    CSCTF_nCh = CSCTF_nXCh*CSCTF_nYCh;
+  if(DTTF_nYCh)
+    DTTF_nCh = DTTF_nXCh*DTTF_nYCh;
+  
+
+  if (GCT_QHist){
+    const QReport *GCT_QReport = GCT_QHist->getQReport("HotChannels");
+    if (GCT_QReport) {
+      int GCT_nBadCh = GCT_QReport->getBadChannels().size();
+      //cout << "nBadCh(GCT): "  << GCT_nBadCh << endl;
+      summaryContent[3] =  1 - GCT_nBadCh/GCT_nCh;
+      //cout << "summaryContent[0]-GCT=" << summaryContent[0] << endl;
+      reportSummaryContent_[3]->Fill( summaryContent[3] );
+    } 
+   
+    // //get list of quality tests
+   //     std::vector<QReport *> Qtest_map = NonIsoEmDeadEtaPhiChannels->getQReports();
+   //     cout << "Qtest_map.size() = " << Qtest_map.size() << endl;
+   //     if(Qtest_map.size() > 0) {
+   //       for (std::vector<QReport *>::const_iterator it=Qtest_map.begin(); it!=Qtest_map.end(); it++)
+   // 	{
+   // 	  //cout << endl;
+   // 	  string qt_name = (*it)->getQRName();
+   // 	  int qt_status = (*it)->getStatus();
+   
+   // 	  cout << "qt_name = " << qt_name << endl;
+   // 	  cout << "qt_status = " << qt_status << endl;
+   //	  
+   //	}
+ }
+
+  
+  if (RCT_QHist){
+    const QReport *RCT_QReport = RCT_QHist->getQReport("HotChannels");
+    if (RCT_QReport) {
+      int RCT_nBadCh = RCT_QReport->getBadChannels().size();
+      //cout << "nBadCh(RCT): "  << RCT_nBadCh << endl;
+      summaryContent[2]=1-RCT_nBadCh/RCT_nCh;
+      //cout << "summaryContent[1]-RCT=" << summaryContent[1] << endl;
+      reportSummaryContent_[2]->Fill( summaryContent[2] );
+    } 
+  }
+
+  if (GMT_QHist){
+    const QReport *GMT_QReport = GMT_QHist->getQReport("HotChannels");
+    //cout << "GMT_QReport: " << GMT_QReport << endl;
+    if (GMT_QReport) {
+      int GMT_nBadCh = GMT_QReport->getBadChannels().size();
+      //cout << "nBadCh(GMT): "  << GMT_nBadCh << endl;
+      //cout << "GMT_nCh: " << GMT_nCh << endl;
+      summaryContent[9] = 1 - GMT_nBadCh/GMT_nCh;
+      //cout << "summaryContent[2]-GMT=" << summaryContent[2] << endl;
+      reportSummaryContent_[9]->Fill( summaryContent[9] );
+    } 
+  }
+
+  if (CSCTF_QHist){
+    const QReport *CSCTF_QReport = CSCTF_QHist->getQReport("HotChannels");
+    //cout << "CSCTF_QReport: " << CSCTF_QReport << endl;
+    if (CSCTF_QReport) {
+      int CSCTF_nBadCh = CSCTF_QReport->getBadChannels().size();
+      //cout << "nBadCh(CSCTF): "  << CSCTF_nBadCh << endl;
+      summaryContent[7] = 1 - CSCTF_nBadCh/CSCTF_nCh;
+      //cout << "summaryContent[3]-CSCTF=" << summaryContent[3] << endl;
+      reportSummaryContent_[7]->Fill( summaryContent[7]);
+    } 
+  }
+
+  if (DTTF_QHist){
+    const QReport *DTTF_QReport = DTTF_QHist->getQReport("HotChannels");
+    //cout << "DTTF_QReport: " << DTTF_QReport << endl;
+    if (DTTF_QReport) {
+      int DTTF_nBadCh = DTTF_QReport->getBadChannels().size();
+      //cout << "nBadCh(DTTF): "  << DTTF_nBadCh << endl;
+      summaryContent[5] = 1 - DTTF_nBadCh/DTTF_nCh;
+      //cout << "summaryContent[4]-DTTF=" << summaryContent[4] << endl;
+      reportSummaryContent_[5]->Fill( summaryContent[5] );
+    } 
+  }
+
+  
+  
+  for (int m = 0; m < nSubsystems; m++) {    
+    summarySum += summaryContent[m];
+  }
+  
+  reportSummary = summarySum / nSubsystems;
+  cout << "reportSummary " << reportSummary << endl;
+  if (reportSummary_) reportSummary_->Fill(reportSummary);
+  
+
+ int jcount=0;
+   //fill the known systems
+  for (int i = 0; i < nSubsystems; i++) {
+    cout << "summaryContent[" << i << "]" << summaryContent[i] << endl;
+    if(!(i%5))jcount++;
+    reportSummaryMap_->setBinContent(i%5+1,jcount, summaryContent[i]);
+  }
+
+//   //fill for known systems
+//   for(int i = 1; i < 6; i++){
+    
+//     cout << "summaryContent[" << i-1 << "] = " << summaryContent[i-1] << endl;
+//     reportSummaryMap_->setBinContent( i, 1, summaryContent[i-1] );
+//   }
+
+//   //fill the rest
+//   for (int i = 1; i < 6; i++) {    
+//     for (int j = 2; j < 5; j++) {    
+      
+//       reportSummaryMap_->setBinContent( i, j, 1. );
+//     }
+//   }
+
+
+} 
+
+
 
 //--------------------------------------------------------
 void L1TEventInfoClient::analyze(const Event& e, const EventSetup& context){
    
-   counterEvt_++;
-   if (prescaleEvt_<1) return;
-   if (prescaleEvt_>0 && counterEvt_%prescaleEvt_ != 0) return;
-
-   if(verbose_) cout << "L1TEventInfoClient::analyze" << endl;
-/*
-// check GCT
-  MonitorElement *NonIsoEmDeadEtaChannels = dbe_->get("L1T/L1TGCT/NonIsoEmOccEta");
-  if(!NonIsoEmDeadEtaChannels) gctFloat = 0.;
+  counterEvt_++;
+  if (prescaleEvt_<1) return;
+  if (prescaleEvt_>0 && counterEvt_%prescaleEvt_ != 0) return;
   
-  int nXChannels = NonIsoEmDeadEtaChannels->getNbinsX();
-  int nYChannels = NonIsoEmDeadEtaChannels->getNbinsY();
-  if(nYChannels) nChannels = nXChannels*nYChannels;
+  if(verbose_) cout << "L1TEventInfoClient::analyze" << endl;
+
   
-  if (NonIsoEmDeadEtaChannels){
-    const QReport *NonIsoEmDeadEtaQReport = NonIsoEmDeadEtaChannels->getQReport("DeadChannels");
-    if (NonIsoEmDeadEtaQReport) {
-      int nBadChannels = NonIsoEmDeadEtaQReport->getBadChannels().size();
-      reportSummary = nBadChannels/nChannels;
-    } 
-  }   
-*/
-  reportSummary = 1.;
-  if (reportSummary_) reportSummary_->Fill(reportSummary);
 
 
-  int nSubsystems = 20;
-  for (int i = 0; i < nSubsystems; i++) {    
-
-     reportSummaryContent_[i]->Fill(1.);
-  }
-
-  for (int i = 0; i < 5; i++) {    
-    for (int j = 0; j < 4; j++) {    
-
-     reportSummaryMap_->setBinContent( i, j, 1. );
-    }
-  }
-
+  //reportSummary = average of report summaries of each system
+  
+ 
 }
 
 //--------------------------------------------------------
