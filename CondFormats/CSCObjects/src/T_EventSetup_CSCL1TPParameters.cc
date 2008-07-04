ed3100a8108710c4ed517d9b515138a851b292df
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2008-07-04 13:43:22 +0000

    add EventSetup for CSCL1TPParameters

---
+++ CondFormats/CSCObjects/src/T_EventSetup_CSCL1TPParameters.cc
@@ -0,0 +1,5 @@
+
+#include "CondFormats/CSCObjects/interface/CSCL1TPParameters.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(CSCL1TPParameters);
