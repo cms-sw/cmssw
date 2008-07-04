de6fcc7bbb1500fad21cd37efebc614ce94b08b8
Steven Won <sha1-1d14a4a31865688509776ba3bfa7911d21c168a7@cern.ch>
2008-07-04 16:46:46 +0000

    *** empty log message ***

---
+++ CondTools/Hcal/plugins/HcalAutoPedestalValidator.cc
@@ -0,0 +1,63 @@
+#include "CondTools/Hcal/interface/HcalAutoPedestalValidator.h"
+
+HcalAutoPedestalValidator::HcalAutoPedestalValidator(edm::ParameterSet const& ps)
+{
+  outfile = ps.getUntrackedParameter<std::string>("outFile","changed.bool");
+  epsilon = ps.getUntrackedParameter<double>("deltaP",.25);
+}
+
+HcalAutoPedestalValidator::~HcalAutoPedestalValidator()
+{
+}
+
+void HcalAutoPedestalValidator::analyze(const edm::Event& ev, const edm::EventSetup& es)
+{
+  using namespace edm::eventsetup;
+  std::cout << "\n";
+  // get fake pedestals from file ("new pedestals")
+  edm::ESHandle<HcalPedestals> newPeds;
+  es.get<HcalPedestalsRcd>().get("update",newPeds);
+  const HcalPedestals* myNewPeds = newPeds.product();
+
+  // get DB pedestals from Frontier/OrcoX ("reference")
+  edm::ESHandle<HcalPedestals> refPeds;
+  es.get<HcalPedestalsRcd>().get("reference",refPeds);
+  const HcalPedestals* myRefPeds = refPeds.product();
+
+  // get e-map from reference
+//  edm::ESHandle<HcalElectronicsMap> refEMap;
+//  es.get<HcalElectronicsMapRcd>().get("reference",refEMap);
+//  const HcalElectronicsMap* myRefEMap = refEMap.product();
+
+  std::vector<DetId> listNewChan = myNewPeds->getAllChannels();
+  std::vector<DetId> listRefChan = myRefPeds->getAllChannels();
+  std::vector<DetId>::iterator cell;
+  bool failflag = false;
+  for (std::vector<DetId>::iterator it = listRefChan.begin(); it != listRefChan.end(); it++)
+  {
+     DetId mydetid = *it;
+     cell = std::find(listNewChan.begin(), listNewChan.end(), mydetid);
+     if (cell == listNewChan.end()) {continue;}
+     else
+     {
+        const float* values = (myNewPeds->getValues( mydetid ))->getValues();
+        const float* oldvalue = (myRefPeds->getValues( mydetid ))->getValues();
+        if( (fabs(*oldvalue-*values)>epsilon) || (fabs(*(oldvalue+1)-*(values+1))>epsilon) || (fabs(*(oldvalue+2)-*(values+2))>epsilon) || (fabs(*(oldvalue+3)-*(values+3))>epsilon) )
+        {
+           std::cout << "Channel " << mydetid.rawId() <<  " Values differ by " << (*oldvalue-*values) << "  " << (*(oldvalue+1)-*(values+1)) << "  " << (*(oldvalue+2)-*(values+2)) << "  " << (*(oldvalue+3)-*(values+3)) << std::endl;
+           failflag = true;
+        }
+     listNewChan.erase(cell);  // fix 25.02.08
+     }
+  }
+
+  if(!failflag) std::cout << "These are identical to within deltaP" << std::endl;
+  if(failflag)
+  {
+    // dump the resulting list of pedestals into a file
+    std::ofstream outStream3(outfile.c_str());
+    std::cout << "--- Pedestals changed! ---" << std::endl;
+  }
+}
+
+DEFINE_FWK_MODULE(HcalAutoPedestalValidator);
