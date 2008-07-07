a41d65f8d8b22371e8f4daaee15dc7018c35d2f1
Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch>
2008-07-07 13:20:26 +0000

    Added DQM style L1Validation.

--- HLTriggerOffline/Tau/src/SealModule.cc
+++ HLTriggerOffline/Tau/src/SealModule.cc
@@ -10,6 +10,7 @@
 #include "HLTriggerOffline/Tau/interface/L2TauValidation.h"
 #include "HLTriggerOffline/Tau/interface/L2TauAnalyzer.h"
 #include "HLTriggerOffline/Tau/interface/L1TauAnalyzer.h"
+#include "HLTriggerOffline/Tau/interface/L1TauValidation.h"
 #include "HLTriggerOffline/Tau/interface/L25TauValidation.h"
 #include "HLTriggerOffline/Tau/interface/HLTTauValidation.h"
 
@@ -22,6 +23,7 @@ DEFINE_ANOTHER_FWK_MODULE(HLTTauL25Validation);
 DEFINE_ANOTHER_FWK_MODULE(L2TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L2TauValidation);
 DEFINE_ANOTHER_FWK_MODULE(L1TauAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(L1TauValidation);
 DEFINE_ANOTHER_FWK_MODULE(L25TauAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L25TauValidation);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauValidation);
