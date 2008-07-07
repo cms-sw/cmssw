ae3c22cb40bfd2aa70391dba88c3a17c5e8bd3ad
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2008-07-07 17:54:51 +0000

    implementation of zdc shower libraries

--- SimG4CMS/Forward/src/module.cc
+++ SimG4CMS/Forward/src/module.cc
@@ -9,6 +9,7 @@
 #include "SimG4Core/SensitiveDetector/interface/SensitiveDetectorPluginFactory.h"
 #include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "SimG4CMS/Forward/interface/ZdcShowerLibrary.h"
   
 DEFINE_SEAL_MODULE ();
 typedef CastorSD CastorSensitiveDetector;
