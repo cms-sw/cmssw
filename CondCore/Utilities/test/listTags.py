15709f043be2caa301d2b1acb878940b8afc4c16
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-07-07 10:39:04 +0000

    works with global tag

--- CondCore/Utilities/test/listTags.py
+++ CondCore/Utilities/test/listTags.py
@@ -4,6 +4,41 @@ sys.setdlopenflags(DLFCN.RTLD_GLOBAL+DLFCN.RTLD_LAZY)
 from pluginCondDBPyInterface import *
 a = FWIncantation()
 os.putenv("CORAL_AUTH_PATH","/afs/cern.ch/cms/DB/conddb")
+
+import coral
+from CondCore.TagCollection import Node,tagInventory,TagTree
+context = coral.Context()
+context.setVerbosityLevel( 'ERROR' )
+svc = coral.ConnectionService( context )
+session = svc.connect("oracle://cms_orcoff_prod/CMS_COND_20X_GLOBALTAG",accessMode = coral.access_ReadOnly )
+inv=tagInventory.tagInventory(session)
+mytree=TagTree.tagTree(session,"CRUZET_V3")
+result=mytree.getAllLeaves()
+tags=[]
+for r in result:
+    if r.tagid != 0:
+        tagcontent=inv.getEntryById(r.tagid)
+        tags.append((tagcontent.tagname,tagcontent.pfn))
+
+
+mytree=0
+inv=0
+del session
+del svc
+rdbms = RDBMS()
+for tag in tags:
+    db = rdbms.getDB(tag[1].replace('frontier://FrontierProd',"oracle://cms_orcoff_prod"))
+    try :
+        iov = db.iov(tag[0])
+        print tag[0], iov.size()
+        for elem in iov.elements :
+            print elem.since(), elem.till(), elem.payloadToken()
+        iov=0
+    except RuntimeError :
+        print tag," no iov?"
+
+
+
 rdbms = RDBMS()
 dba = rdbms.getDB("oracle://cms_orcoff_prod/CMS_COND_20X_ALIGNMENT")
 dbe = rdbms.getDB("oracle://cms_orcoff_prod/CMS_COND_20X_ECAL")
