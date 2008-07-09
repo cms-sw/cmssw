1faebe2ef7f1b16cf0c4ba3163fbb2ac082a9853
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2008-07-09 15:16:20 +0000

    upgrade of DEDx software

--- DataFormats/TrackReco/interface/TrackDeDxEstimate.h
+++ DataFormats/TrackReco/interface/TrackDeDxEstimate.h
@@ -1,18 +1,37 @@
 #ifndef TrackReco_TrackDeDxEstimate_h
 #define TrackReco_TrackDeDxEstimate_h
-
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/Common/interface/AssociationVector.h"
-#include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
-#include <vector>
+//#include "DataFormats/TrackReco/interface/TrackFwd.h"
+//#include "DataFormats/Common/interface/AssociationVector.h"
+//#include "DataFormats/GeometryCommonDetAlgo/interface/Measurement1D.h"
+//#include <vector>
 
 
 namespace reco {
-//Association Track -> float estimator
-typedef  edm::AssociationVector<reco::TrackRefProd,std::vector<Measurement1D> >  TrackDeDxEstimateCollection;
-typedef  TrackDeDxEstimateCollection::value_type TrackDeDxEstimate;
-typedef  edm::Ref<TrackDeDxEstimateCollection> TrackDeDxEstimateRef;
-typedef  edm::RefProd<TrackDeDxEstimateCollection> TrackDeDxEstimateRefProd;
-typedef  edm::RefVector<TrackDeDxEstimateCollection> TrackDeDxEstimateRefVector;
+
+  class TrackDeDxEstimate{
+
+    TrackDeDxEstimate();
+    TrackDeDxEstimate(float val, float er, unsigned int num );
+    ~TrackDeDxEstimate();
+
+    float dEdx() const;
+    float dEdxError() const;
+    unsigned int numberOfMeasurements() const;
+
+
+  private:
+
+    float value_;
+    float error_;
+    unsigned int numberOfMeasurements_;
+
+  }
+
+// //Association Track -> float estimator
+// typedef  edm::AssociationVector<reco::TrackRefProd,std::vector<Measurement1D> >  TrackDeDxEstimateCollection;
+// typedef  TrackDeDxEstimateCollection::value_type TrackDeDxEstimate;
+// typedef  edm::Ref<TrackDeDxEstimateCollection> TrackDeDxEstimateRef;
+// typedef  edm::RefProd<TrackDeDxEstimateCollection> TrackDeDxEstimateRefProd;
+// typedef  edm::RefVector<TrackDeDxEstimateCollection> TrackDeDxEstimateRefVector;
 }
 #endif
