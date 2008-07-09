87f412fa27c535c389dcd0f6a09f6028c6cce3b6
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-07-09 18:57:07 +0000

    Convert tests to python

---
+++ IOPool/SecondaryInput/test/PreSecondaryInputTest_cfg.py
@@ -0,0 +1,30 @@
+# The following comments couldn't be translated into the new config version:
+
+# Configuration file for PreSecondaryInputTest 
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(50)
+)
+process.Thing = cms.EDProducer("ThingProducer",
+    debugLevel = cms.untracked.int32(1)
+)
+
+process.OtherThing = cms.EDProducer("OtherThingProducer",
+    debugLevel = cms.untracked.int32(1)
+)
+
+process.output = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('SecondaryInputTest.root')
+)
+
+process.source = cms.Source("EmptySource")
+
+process.p = cms.Path(process.Thing*process.OtherThing)
+process.ep = cms.EndPath(process.output)
+
+
---
+++ IOPool/SecondaryInput/test/SecondaryInputTest_cfg.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(42)
+)
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    moduleSeeds = cms.PSet(
+        Thing = cms.untracked.uint32(12345)
+    ),
+    sourceSeed = cms.untracked.uint32(98765)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:SecondaryInputTest.root')
+)
+
+process.Thing = cms.EDProducer("SecondaryProducer",
+    input = cms.SecSource("PoolSource",
+        fileNames = cms.untracked.vstring('file:SecondaryInputTest2.root')
+    )
+)
+
+process.Analysis = cms.EDAnalyzer("EventContentAnalyzer",
+    verbose = cms.untracked.bool(True)
+)
+
+process.p = cms.Path(process.Thing*process.Analysis)
+
+
