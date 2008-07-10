6334b6f6f05a61e8dd1c794334b6ffdaf1da0ae6
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-07-10 09:20:50 +0000

    Modified python file. Changing it from EDFilter to EDAnalyzer

--- CalibTracker/SiPixelTools/python/SiPixelErrorsCalibDigis_cfi.py
+++ CalibTracker/SiPixelTools/python/SiPixelErrorsCalibDigis_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-siPixelErrorsDigisToCalibDigis = cms.EDFilter("SiPixelErrorsDigisToCalibDigis",
+siPixelErrorsDigisToCalibDigis = cms.EDAnalyzer("SiPixelErrorsDigisToCalibDigis",
     saveFile = cms.untracked.bool(True),
     outputFilename = cms.string('myResults.root'),
     SiPixelProducerLabelTag = cms.InputTag("siPixelCalibDigis")
