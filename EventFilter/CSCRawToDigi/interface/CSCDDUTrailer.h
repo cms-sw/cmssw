db29a4e29f24552cd764238a009e7354ea675078
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-07-10 00:42:35 +0000

    Interface to add the wordcount

--- EventFilter/CSCRawToDigi/interface/CSCDDUTrailer.h
+++ EventFilter/CSCRawToDigi/interface/CSCDDUTrailer.h
@@ -40,6 +40,7 @@ class CSCDDUTrailer {
   
   unsigned errorstat() const { return errorstat_; }
   unsigned wordcount() const { return word_count_; }  
+  void setWordCount(unsigned wordcount) {word_count_ = wordcount;}
   unsigned dmb_warn() const { return dmb_warn_; }  
   unsigned dmb_full() const { return dmb_full_; }
   unsigned reserved() const { return whatever; } 
