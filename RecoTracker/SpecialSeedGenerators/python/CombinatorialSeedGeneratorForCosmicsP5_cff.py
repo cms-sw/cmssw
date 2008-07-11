0d9852754aaa7df31c5d96ab95bbb3b1c29856b5
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-07-11 15:57:25 +0000

    geometry cleanup for HEAD

--- RecoTracker/SpecialSeedGenerators/python/CombinatorialSeedGeneratorForCosmicsP5_cff.py
+++ RecoTracker/SpecialSeedGenerators/python/CombinatorialSeedGeneratorForCosmicsP5_cff.py
@@ -11,7 +11,6 @@ import FWCore.ParameterSet.Config as cms
 
 # magnetic field
 #include "Geometry/CMSCommonData/data/cmsMagneticFieldXML.cfi"
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 # cms geometry
 # tracker geometry
 # tracker numbering
--- RecoTracker/SpecialSeedGenerators/python/CosmicSeedP5Pairs_cff.py
+++ RecoTracker/SpecialSeedGenerators/python/CosmicSeedP5Pairs_cff.py
@@ -8,7 +8,8 @@ from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 #pixelCPE
 from RecoLocalTracker.SiPixelRecHits.PixelCPEParmError_cfi import *
 #TransientTrackingBuilder
-from RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi import *
+#from RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi import *
+from RecoTracker.TransientTrackingRecHit.TTRHBuilderWithTemplate_cfi import *
 import copy
 from RecoTracker.SpecialSeedGenerators.CosmicSeed_cfi import *
 # generate Cosmic seeds #####################
