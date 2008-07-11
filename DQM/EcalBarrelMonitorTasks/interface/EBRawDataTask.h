17a50a86438c9dd310d8b88508e301d7679879c1
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2008-07-11 18:07:44 +0000

    raw data task first commit. Notice: it does not work for the local runs for now.

---
+++ DQM/EcalBarrelMonitorTasks/interface/EBRawDataTask.h
@@ -0,0 +1,74 @@
+#ifndef EBRawDataTask_H
+#define EBRawDataTask_H
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+class MonitorElement;
+class DQMStore;
+
+class EBRawDataTask : public edm::EDAnalyzer {
+
+ public:
+
+/// Constructor
+EBRawDataTask(const edm::ParameterSet& ps);
+
+/// Destructor
+virtual ~EBRawDataTask();
+
+protected:
+
+/// Analyze
+void analyze(const edm::Event& e, const edm::EventSetup& c);
+
+/// BeginJob
+void beginJob(const edm::EventSetup& c);
+
+/// EndJob
+void endJob(void);
+
+/// BeginRun
+void beginRun(const edm::Run & r, const edm::EventSetup & c);
+
+/// EndRun
+void endRun(const edm::Run & r, const edm::EventSetup & c);
+
+/// Reset
+void reset(void);
+
+/// Setup
+void setup(void);
+
+/// Cleanup
+void cleanup(void);
+
+private:
+
+int ievt_;
+
+DQMStore* dqmStore_;
+
+std::string prefixME_;
+
+bool enableCleanup_;
+
+bool mergeRuns_;
+
+edm::InputTag FEDRawDataCollection_;
+edm::InputTag EcalRawDataCollection_;
+
+MonitorElement* meEBCRCErrors_;
+
+MonitorElement* meEBRunNumberErrors_;
+MonitorElement* meEBL1AErrors_;
+MonitorElement* meEBOrbitNumberErrors_;
+MonitorElement* meEBBunchCrossingErrors_;
+MonitorElement* meEBTriggerTypeErrors_;
+
+bool init_;
+
+};
+
+#endif
---
+++ DQM/EcalBarrelMonitorTasks/python/EBRawDataTask_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+ecalBarrelRawDataTask = cms.EDFilter("EBRawDataTask",
+    prefixME = cms.untracked.string('EcalBarrel'),
+    enableCleanup = cms.untracked.bool(False),
+    mergeRuns = cms.untracked.bool(False),
+    FEDRawDataCollection = cms.InputTag("source"),
+    EcalRawDataCollection = cms.InputTag("ecalEBunpacker")
+)
