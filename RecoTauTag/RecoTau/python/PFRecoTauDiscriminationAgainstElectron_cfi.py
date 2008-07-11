b9e5ea648f4cea3f171ee548dc8d8bb35631f005
Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch>
2008-07-11 09:57:42 +0000

    Reset old defaults.

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectron_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectron_cfi.py
@@ -1,31 +1,39 @@
 import FWCore.ParameterSet.Config as cms
 
 pfRecoTauDiscriminationAgainstElectron = cms.EDFilter("PFRecoTauDiscriminationAgainstElectron",
-    EcalStripSumEOverPLead_maxValue = cms.double(1.8), ## Upper cut away window threshold#
 
-    EcalStripSumEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
+    PFTauProducer = cms.string('pfRecoTauProducer'),
 
-    ElecPreID0_SumEOverPLead_maxValue = cms.double(0.95),
     ApplyCut_EmFraction = cms.bool(False),
     EmFraction_maxValue = cms.double(0.9),
-    PFTauProducer = cms.string('pfRecoTauProducer'),
+
+    ApplyCut_HcalTotOverPLead = cms.bool(False),
     HcalTotOverPLead_minValue = cms.double(0.1),
-    ApplyCut_ElectronPreID = cms.bool(True),
+
+    ApplyCut_Hcal3x3OverPLead = cms.bool(False),
+    Hcal3x3OverPLead_minValue = cms.double(0.1),
+
+    ApplyCut_HcalMaxOverPLead = cms.bool(False),
+    HcalMaxOverPLead_minValue = cms.double(0.1)
+
+    ApplyCut_EcalStripSumEOverPLead = cms.bool(False),
+    EcalStripSumEOverPLead_maxValue = cms.double(1.8), ## Upper cut away window threshold#
+    EcalStripSumEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
+
+    ApplyCut_BremsRecoveryEOverPLead = cms.bool(False),
+    BremsRecoveryEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
     BremsRecoveryEOverPLead_maxValue = cms.double(1.8),  ##Upper cut away window threshold
 
-    ElecPreID0_Hcal3x3_minValue = cms.double(0.05),
-    ApplyCut_BremsRecoveryEOverPLead = cms.bool(False),
-    ApplyCut_HcalTotOverPLead = cms.bool(False),
-    ApplyCut_HcalMaxOverPLead = cms.bool(False),
-    ApplyCut_Hcal3x3OverPLead = cms.bool(False),
-    ElecPreID1_Hcal3x3_minValue = cms.double(0.15),
-    ElecPreID1_SumEOverPLead_maxValue = cms.double(0.8),
-    ApplyCut_EcalCrackCut = cms.bool(True),
-    ApplyCut_EcalStripSumEOverPLead = cms.bool(False),
-    BremsRecoveryEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
+    ApplyCut_ElectronPreID = cms.bool(False), # Electron PreID only
 
-    Hcal3x3OverPLead_minValue = cms.double(0.1),
-    HcalMaxOverPLead_minValue = cms.double(0.1)
+    ApplyCut_ElectronPreID_2D = cms.bool(True),
+    ElecPreID0_SumEOverPLead_maxValue = cms.double(0.95),
+    ElecPreID0_Hcal3x3_minValue = cms.double(0.05),
+    ElecPreID1_SumEOverPLead_maxValue = cms.double(0.8),
+   ElecPreID1_Hcal3x3_minValue = cms.double(0.15),
+ 
+    ApplyCut_EcalCrackCut = cms.bool(True),
+
 )
 
 
