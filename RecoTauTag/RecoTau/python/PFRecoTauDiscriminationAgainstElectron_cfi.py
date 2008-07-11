f805bae2ef576fb92d58a3d61d031850bb7d163f
Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch>
2008-07-11 13:00:32 +0000

    Typos.

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectron_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationAgainstElectron_cfi.py
@@ -14,11 +14,11 @@ pfRecoTauDiscriminationAgainstElectron = cms.EDFilter("PFRecoTauDiscriminationAg
     Hcal3x3OverPLead_minValue = cms.double(0.1),
 
     ApplyCut_HcalMaxOverPLead = cms.bool(False),
-    HcalMaxOverPLead_minValue = cms.double(0.1)
+    HcalMaxOverPLead_minValue = cms.double(0.1),
 
-    ApplyCut_EcalStripSumEOverPLead = cms.bool(False),
-    EcalStripSumEOverPLead_maxValue = cms.double(1.8), ## Upper cut away window threshold#
-    EcalStripSumEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
+    ApplyCut_EOverPLead = cms.bool(False),
+    EOverPLead_maxValue = cms.double(1.8), ## Upper cut away window threshold#
+    EOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
 
     ApplyCut_BremsRecoveryEOverPLead = cms.bool(False),
     BremsRecoveryEOverPLead_minValue = cms.double(0.8), ## Lower cut away window threshold
@@ -27,12 +27,12 @@ pfRecoTauDiscriminationAgainstElectron = cms.EDFilter("PFRecoTauDiscriminationAg
     ApplyCut_ElectronPreID = cms.bool(False), # Electron PreID only
 
     ApplyCut_ElectronPreID_2D = cms.bool(True),
-    ElecPreID0_SumEOverPLead_maxValue = cms.double(0.95),
-    ElecPreID0_Hcal3x3_minValue = cms.double(0.05),
-    ElecPreID1_SumEOverPLead_maxValue = cms.double(0.8),
-   ElecPreID1_Hcal3x3_minValue = cms.double(0.15),
+    ElecPreID0_EOverPLead_maxValue = cms.double(0.95),
+    ElecPreID0_HOverPLead_minValue = cms.double(0.05),
+    ElecPreID1_EOverPLead_maxValue = cms.double(0.8),
+    ElecPreID1_HOverPLead_minValue = cms.double(0.15),
  
-    ApplyCut_EcalCrackCut = cms.bool(True),
+    ApplyCut_EcalCrackCut = cms.bool(True)
 
 )
 
