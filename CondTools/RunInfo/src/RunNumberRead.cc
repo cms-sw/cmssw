83bf1f74e44cc6614eeae61f4dce06eb1d2b5386
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-07-11 16:03:30 +0000

    new controls added

--- CondTools/RunInfo/src/RunNumberRead.cc
+++ CondTools/RunInfo/src/RunNumberRead.cc
@@ -119,7 +119,7 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
 
  //  queryI->addToTableList( m_tableToRead );
   std::cout<< "table handling " << std::endl;
-// implemating the query here....... 
+  // implemating the query here....... 
   queryI->addToOutputList( m_tableToRead + "." +  m_columnToRead, m_columnToRead  );
   //  condition 
   std::string  condition = m_tableToRead + ".runnumber=:n_run AND " +  m_tableToRead +  ".name='CMS.LVL0:SEQ_NUMBER'";
@@ -129,16 +129,44 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
   conditionData[0].data<int>() = r_number;
   coral::ICursor& cursorI = queryI->execute();
  
+  /*  class RunNumber {
+public:
+struct Item {
+Item(){}
+~Item(){}
+int m_run;
+long long  m_id_start;
+long long  m_id_stop;
+std::string m_number;
+std::string m_name;
+signed long long m_start_time_sll;
+std::string m_start_time_str;
+signed long long m_stop_time_sll;
+std::string m_stop_time_str;
+int  m_lumisections;
+.....
+
+  */
+
+
   std::vector<RunNumber::Item> rnarray;
   RunNumber::Item Itemp;
-  std::cout<< " before entering the query == " << std::endl;
-  while ( cursorI.next() ) {
+  
+
+  // if cursor is null  setting null values  
+  Itemp.m_run = r_number;
+  
+  if ( cursorI.next()!=0 ) {
     const coral::AttributeList& row = cursorI.currentRow();
     std::cout<< " entering the query == " << std::endl;
     Itemp.m_number = row[m_columnToRead].data<std::string>();
-    Itemp.m_run = r_number ;
     std::cout<< " number extracted == " << Itemp.m_number  << std::endl;
-    }
+  }
+  else {
+    Itemp.m_number="null";
+    std::cout<< " null number extracted == " << Itemp.m_number  << std::endl;
+  }
+  
   delete queryI;
   
   // new query to obtain the string value
@@ -150,31 +178,41 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
   //  condition 
   std::string condition2 = m_tableToRead + ".runnumber=:n_run AND " +  m_tableToRead +  ".name='CMS.LVL0:SEQ_NAME'";
   queryII->setCondition( condition2, conditionData );
- coral::ICursor& cursorII = queryII->execute();
- 
- while ( cursorII.next()  ) {
+  coral::ICursor& cursorII = queryII->execute();
+  
+  if ( cursorII.next()!=0  ) {
     const coral::AttributeList& row = cursorII.currentRow();
     Itemp.m_name = row[m_columnToRead].data<std::string>();
     std::cout<< " name extracted == " <<Itemp.m_name   << std::endl;
- }
+  }
+  else{
+    Itemp.m_name = "null";
+    std::cout<< " name extracted == " <<Itemp.m_name   << std::endl;
+  }
    delete queryII;
+   
    
    std::string m_columnToRead_id = "ID";
    // new query to obtain the start_time, fist obtaining the id
    coral::IQuery* queryIII = schema.tableHandle( m_tableToRead).newQuery();  
- //queryIII->addToTableList( m_tableToRead );
+   //queryIII->addToTableList( m_tableToRead );
    // implemating the query here....... 
    queryIII->addToOutputList( m_tableToRead + "." +  m_columnToRead_id, m_columnToRead_id    );
    //  condition 
    std::string condition3 = m_tableToRead + ".runnumber=:n_run AND " +  m_tableToRead +  ".name='CMS.LVL0:START_TIME_T'";
    queryIII->setCondition( condition3, conditionData );
- coral::ICursor& cursorIII = queryIII->execute();
- 
- while ( cursorIII.next() ) {
+   coral::ICursor& cursorIII = queryIII->execute();
+   
+ if ( cursorIII.next()!=0 ) {
    const coral::AttributeList& row = cursorIII.currentRow();
    Itemp.m_id_start= row[m_columnToRead_id].data<long long>();
    std::cout<< " id for  start time time extracted == " <<Itemp.m_id_start   << std::endl;
  }
+ else{
+   Itemp.m_id_start=-1;
+   std::cout<< " id for  start time time extracted == " <<Itemp.m_id_start   << std::endl;
+ }
+ 
  delete queryIII;
  
  // now exctracting the start time
@@ -192,8 +230,8 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
  std::string condition3bis = m_tableToRead_date + ".runsession_parameter_id=:n_id";
  queryIIIbis->setCondition( condition3bis, conditionData3bis );
  coral::ICursor& cursorIIIbis = queryIIIbis->execute();
-
- while ( cursorIIIbis.next() ) {
+ 
+ if ( cursorIIIbis.next()!=0 ) {
    const coral::AttributeList& row = cursorIIIbis.currentRow();
    coral::TimeStamp ts =  row[m_columnToRead_val].data<coral::TimeStamp>();    
    int  year= ts.year();
@@ -203,10 +241,10 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
    int  minute= ts.minute();
    int  second = ts.second();
    long nanosecond =  ts.nanosecond();
-//const std::string toString= ts.toString() ;    
-/// The number of nanoseconds from epoch 01/01/1970 UTC, normally should fit into 64bit signed integer, depends on the BOOST installation
-//const signed long long int  total_nanoseconds=ts.total_nanoseconds() ;
- 
+   //const std::string toString= ts.toString() ;    
+   /// The number of nanoseconds from epoch 01/01/1970 UTC, normally should fit into 64bit signed integer, depends on the BOOST installation
+   //const signed long long int  total_nanoseconds=ts.total_nanoseconds() ;
+   
    
    std::cout<< "  start time time extracted == " << "-->year " << year
 	    << "-- month " << month
@@ -216,29 +254,34 @@ select MAX(lsnumber)   FROM cms_runinfo.hlt_supervisor_lumisections where cms_ru
 	    << "-- second " << second
 	    << "-- nanosecond " << nanosecond<<std::endl;
    boost::gregorian::date dt(year,month,day);
- // td in microsecond
+   // td in microsecond
    boost::posix_time::time_duration td(hour,minute,second,nanosecond/1000);  
    
    boost::posix_time::ptime pt( dt, td); 
-//boost::gregorian::date(year,month,day),
-//boost::posix_time::hours(hour)+boost::posix_time::minutes(minute)+ 
-//boost::posix_time::seconds(second)+ 
-//nanosec(nanosecond));
-// boost::posix_time::ptime pt(ts);
-    std::cout<<"ptime == "<< pt <<std::endl;          
-    
-    Itemp.m_start_time_str = boost::posix_time::to_iso_extended_string(pt);
-std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::endl;   
- boost::posix_time::ptime time_at_epoch( boost::gregorian::date( 1970, 1, 1 ) ) ;
- // Subtract time_at_epoch from current time to get the required value.
- boost::posix_time::time_duration time_diff = ( pt - time_at_epoch ) ;
- Itemp.m_start_time_sll = time_diff.total_microseconds();
- std::cout << "microsecond since Epoch (UTC) : " <<Itemp.m_start_time_sll  <<std::endl;    
+   //boost::gregorian::date(year,month,day),
+   //boost::posix_time::hours(hour)+boost::posix_time::minutes(minute)+ 
+   //boost::posix_time::seconds(second)+ 
+   //nanosec(nanosecond));
+   // boost::posix_time::ptime pt(ts);
+   std::cout<<"ptime == "<< pt <<std::endl;          
+   
+   Itemp.m_start_time_str = boost::posix_time::to_iso_extended_string(pt);
+   std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::endl;   
+   boost::posix_time::ptime time_at_epoch( boost::gregorian::date( 1970, 1, 1 ) ) ;
+   // Subtract time_at_epoch from current time to get the required value.
+   boost::posix_time::time_duration time_diff = ( pt - time_at_epoch ) ;
+   Itemp.m_start_time_sll = time_diff.total_microseconds();
+   std::cout << "microsecond since Epoch (UTC) : " <<Itemp.m_start_time_sll  <<std::endl;    
  }
+ else 
+   {
+     Itemp.m_start_time_str = "null";
+     Itemp.m_start_time_sll = -1;
+   }
  delete queryIIIbis;
  
  
-// new query to obtain the stop_time, fist obtaining the id
+ // new query to obtain the stop_time, fist obtaining the id
  coral::IQuery* queryIV = schema.tableHandle( m_tableToRead).newQuery();  
  //queryIII->addToTableList( m_tableToRead );
  // implemating the query here....... 
@@ -252,11 +295,14 @@ std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::
  
  
  
- while ( cursorIV.next() ) {
+ if ( cursorIV.next()!=0 ) {
    const coral::AttributeList& row = cursorIV.currentRow();
         
    Itemp.m_id_stop= row[m_columnToRead_id].data<long long>();
    std::cout<< " id for  stop time time extracted == " <<Itemp.m_id_stop   << std::endl;
+ }
+ else{
+   Itemp.m_id_stop=-1;
  }
  delete queryIV;
  
@@ -273,7 +319,7 @@ std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::
  std::string condition4bis = m_tableToRead_date + ".runsession_parameter_id=:n_id";
  queryIVbis->setCondition( condition4bis, conditionData4bis );
  coral::ICursor& cursorIVbis = queryIVbis->execute();
- while ( cursorIVbis.next() ) {
+ if ( cursorIVbis.next()!=0 ) {
    const coral::AttributeList& row = cursorIVbis.currentRow();
    coral::TimeStamp ts =  row[m_columnToRead_val].data<coral::TimeStamp>(); 
    int  year= ts.year();
@@ -302,6 +348,10 @@ std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::
    Itemp.m_stop_time_sll = time_diff.total_microseconds();
    std::cout << "microsecond since Epoch (UTC) : " <<Itemp.m_stop_time_sll  <<std::endl;    
  }
+ else{
+   Itemp.m_stop_time_str="null";
+   Itemp.m_stop_time_sll=-1;
+ }
  delete queryIVbis;
  
  // new query to obtain the lumisections number
@@ -317,17 +367,22 @@ std::cout<<"start time string  extracted  == "<<Itemp.m_start_time_str   <<std::
  // queryV->setCondition(std::string("max_lsnumber"),coral::AttributeList() );
  // queryV->defineOutputType( m_columnToRead_ls, "double" );
  coral::ICursor& cursorV = queryV->execute();
- while ( cursorV.next()  ) {
+ if ( cursorV.next()!=0  ) {
    const coral::AttributeList& row = cursorV.currentRow();
    double lumisections =  row[m_columnToRead_ls].data<double>();
    Itemp.m_lumisections = static_cast<int>(lumisections);
    std::cout<<" lumisections number extracted == "<<Itemp.m_lumisections  << std::endl;  
+ }
+ else{
+   Itemp.m_lumisections=-1; 
  }
  std::cout<<" leaving the query  "<< std::endl;  
  delete queryV;
  
  session->transaction().commit();
  delete session;
+ 
+ 
  rnarray.push_back(Itemp);
  return rnarray;
 }
