6943f52842e32dfc4db18545d76b1a8f8c1ebb63
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-07-11 15:22:00 +0000

    geometry cleanup for HEAD

--- RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cff.py
+++ RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cff.py
@@ -9,6 +9,9 @@ from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 #include "RecoMuon/MuonSeedGenerator/data/standAloneMuonSeeds.cfi"
 # New standalone muon producer to be used in 2-X-X
 from RecoMuon.MuonSeedGenerator.standAloneMuonSeedProducer_cfi import *
-from RecoMuon.MuonSeedGenerator.ptSeedParameterization_cfi import *
+# Old field map
+#include "RecoMuon/MuonSeedGenerator/data/ptSeedParameterization_40T_851.cfi"
+# New map at 3.8 T
+from RecoMuon.MuonSeedGenerator.ptSeedParameterization_38T_cfi import *
 
 
