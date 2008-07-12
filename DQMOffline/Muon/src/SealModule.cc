d6722f72be7f79878667d10be2e3bc0cf102bcb3
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-07-12 10:55:46 +0000

    added muonRecoTest module

--- DQMOffline/Muon/src/SealModule.cc
+++ DQMOffline/Muon/src/SealModule.cc
@@ -7,10 +7,12 @@
 
 // the clients
 #include "DQMOffline/Muon/src/MuonTrackResidualsTest.h"
+#include "DQMOffline/Muon/src/MuonRecoTest.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MuonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(MuonTrackResidualsTest);
+DEFINE_ANOTHER_FWK_MODULE(MuonRecoTest);
 DEFINE_ANOTHER_FWK_MODULE(RPCEfficiency);
 DEFINE_ANOTHER_FWK_MODULE(RPCEfficiencySecond);
 
