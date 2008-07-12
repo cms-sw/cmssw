43a1039b92738dda6464c6ca52b2b6711e3a1e25
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-07-12 01:13:07 +0000

    Minor vector::reserve() fix to remove memory allocations

--- TrackingTools/TrajectoryCleaning/src/TrajectoryCleaner.cc
+++ TrackingTools/TrajectoryCleaning/src/TrajectoryCleaner.cc
@@ -5,6 +5,7 @@
 void TrajectoryCleaner::clean( TrajectoryContainer& tc) const
 {
   TrajectoryPointerContainer thePointerContainer;
+  thePointerContainer.reserve(tc.size());
   for (TrajectoryCleaner::TrajectoryIterator it = tc.begin(); it != tc.end(); it++) {
     thePointerContainer.push_back( &(*it) );
   }
