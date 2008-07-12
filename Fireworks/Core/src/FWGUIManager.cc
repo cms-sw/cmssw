dc43f330cdab3b34aaec2d982457b84fca89c25e
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-07-12 13:50:36 +0000

    moved connecting GUIManager actions with itself to insided GUIManager added missing connection to open Object Display Controller

--- Fireworks/Core/src/FWGUIManager.cc
+++ Fireworks/Core/src/FWGUIManager.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Feb 11 11:06:40 EST 2008
-// $Id: FWGUIManager.cc,v 1.51 2008/07/11 01:15:23 dmytro Exp $
+// $Id: FWGUIManager.cc,v 1.52 2008/07/12 01:29:56 dmytro Exp $
 //
 
 // system include files
@@ -142,6 +142,10 @@ m_viewPopup(0)
      getAction(cmsshow::sExportImage)->activated.connect(sigc::mem_fun(*this, &FWGUIManager::exportImageOfMainView));
      getAction(cmsshow::sSaveConfig)->activated.connect(writeToPresentConfigurationFile_);
      getAction(cmsshow::sSaveConfigAs)->activated.connect(sigc::mem_fun(*this,&FWGUIManager::promptForConfigurationFile));
+     getAction(cmsshow::sShowEventDisplayInsp)->activated.connect(sigc::mem_fun(*m_guiManager, &FWGUIManager::createEDIFrame));
+     getAction(cmsshow::sShowMainViewCtl)->activated.connect(sigc::mem_fun(*m_guiManager, &FWGUIManager::createViewPopup));
+      getAction(cmsshow::sShowObjInsp)->activated.connect(sigc::mem_fun(*m_guiManager, &FWGUIManager::createModelPopup));
+      
    }
    {
      // createEDIFrame();
