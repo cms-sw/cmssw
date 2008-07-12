d1c0ac8f079f3092fb26cb5c61ab25aec71d9e21
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-07-12 12:21:16 +0000

    bha, required

---
+++ CondFormats/DataRecord/interface/ExEfficiency.h
@@ -0,0 +1,6 @@
+#ifndef ExEfficiencyRCD_H
+#define ExEfficiencyRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class ExEfficiencyRcd : public edm::eventsetup::EventSetupRecordImplementation<ExEfficiencyRcd> {};
+#endif
