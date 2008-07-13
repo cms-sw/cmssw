b07248aedfd38e67329b98796d1163cf6424963c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-07-13 16:50:11 +0000

    extractor

--- CondCore/Utilities/interface/InspectorPythonWrapper.h
+++ CondCore/Utilities/interface/InspectorPythonWrapper.h
@@ -5,16 +5,29 @@
 
 using namespace boost::python;
 
+namespace {
+  template<typename Wrapper>
+  void define() {
+    typedef typename Wrapper::Extractor Extractor;
+    class_<Extractor>("Extractor", init<>())
+      .def(init<std::string, std::vector<int> )
+      .def("values",&Extractor::values, return_value_policy<copy_const_reference>())
+      ;
+
+    class_<Wrapper>("Object",init<>()) 
+      .def(init<cond::IOVElement>())
+      .def("print",&PythonWrapper::print)
+      .def("summary",&PythonWrapper::summary) 
+      .def("extract",&PythonWrapper::extractor)
+      ; 
+  }
+
+}
 
 
 #define PYTHON_WRAPPER(_class,_name) \
 namespace { typedef cond::PayLoadInspector< _class > PythonWrapper;} \
-BOOST_PYTHON_MODULE(plugin ## _name ## PyInterface) { \
-  class_<PythonWrapper>("Object",init<>()) \
-    .def(init<cond::IOVElement>()) \
-    .def("print",&PythonWrapper::print) \
-    .def("summary",&PythonWrapper::summary); \
-} \
+ BOOST_PYTHON_MODULE(plugin ## _name ## PyInterface) { define<PythonWrapper>(); } 
 namespace { const char * pluginName_="plugin"  #_name "PyInterface"; }\
 PYTHON_ID(PythonWrapper::Class, pluginName_)
 
