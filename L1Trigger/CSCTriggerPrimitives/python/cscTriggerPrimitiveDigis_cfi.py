c679204fb7aa4c45a7849d09aa131b22cd9c1623
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2008-07-14 14:31:21 +0000

    Add alct_acc_thresh and alct_acc_pattern, and restructure.

--- L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigis_cfi.py
+++ L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigis_cfi.py
@@ -5,86 +5,101 @@ from L1Trigger.CSCCommonTrigger.CSCCommonTrigger_cfi import *
 # =====================================================
 cscTriggerPrimitiveDigis = cms.EDProducer("CSCTriggerPrimitivesProducer",
     CSCCommonTrigger,
-    # Parameters for ALCT processors: default
-    alctParamDef = cms.PSet(
-        alctMode = cms.uint32(1),
-        alctTrigMode = cms.uint32(3),
-        verbosity = cms.untracked.int32(0),
-        alctNphPattern = cms.uint32(4),
-        alctBxWidth = cms.uint32(6),
-        alctL1aWindow = cms.uint32(5),
-        alctDriftDelay = cms.uint32(3),
-        alctNphThresh = cms.uint32(2),
-        alctFifoTbins = cms.uint32(16),
-        alctFifoPretrig = cms.uint32(10)
-    ),
-    # Parameters for CLCT processors: default
-    clctParamDef = cms.PSet(
-        clctDriftDelay = cms.uint32(2),
-        clctFifoPretrig = cms.uint32(7),
-        clctFifoTbins = cms.uint32(12),
-        clctNphPattern = cms.uint32(4),
-        clctBxWidth = cms.uint32(6),
-        verbosity = cms.untracked.int32(0),
-        clctHsThresh = cms.uint32(2),
-        clctSepSrc = cms.uint32(1),
-        clctSepVme = cms.uint32(10),
-        clctDsThresh = cms.uint32(2),
-        clctPidThresh = cms.uint32(2)
-    ),
+
     # Name of digi producer module(s)
     CSCComparatorDigiProducer = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
-    # Parameters for CLCT processors: MTCC-II
-    clctParamMTCC2 = cms.PSet(
-        clctDriftDelay = cms.uint32(2),
-        clctFifoPretrig = cms.uint32(7),
-        clctFifoTbins = cms.uint32(12),
-        clctNphPattern = cms.uint32(1),
-        clctBxWidth = cms.uint32(6),
-        verbosity = cms.untracked.int32(0),
-        clctHsThresh = cms.uint32(4),
-        # TMB07 parameters
-        clctHitThresh = cms.uint32(2),
-        clctSepSrc = cms.uint32(1),
-        clctSepVme = cms.uint32(10),
-        clctDsThresh = cms.uint32(4),
-        clctPidThresh = cms.uint32(2)
-    ),
-    tmbParam = cms.PSet(
-        verbosity = cms.untracked.int32(0)
-    ),
+    CSCWireDigiProducer = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
+
     # Parameters common for all boards
     commonParam = cms.PSet(
         isTMB07 = cms.bool(True),
         isMTCC = cms.bool(True)
     ),
-    CSCWireDigiProducer = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
-    # Parameters for ALCT processors: MTCC-II
-    alctParamMTCC2 = cms.PSet(
-        alctMode = cms.uint32(0),
-        alctTrigMode = cms.uint32(2),
-        verbosity = cms.untracked.int32(0),
-        alctNphPattern = cms.uint32(4),
-        alctBxWidth = cms.uint32(6),
-        alctL1aWindow = cms.uint32(3),
-        alctDriftDelay = cms.uint32(3),
-        alctNphThresh = cms.uint32(2),
-        alctFifoTbins = cms.uint32(16),
-        alctFifoPretrig = cms.uint32(10)
+
+    # Parameters for ALCT processors: default
+    alctParamDef = cms.PSet(
+        alctFifoTbins   = cms.uint32(16),
+        alctFifoPretrig = cms.uint32(10),
+        alctBxWidth     = cms.uint32(6),
+        alctDriftDelay  = cms.uint32(3),
+        alctNphThresh   = cms.uint32(2),
+        alctNphPattern  = cms.uint32(4),
+        alctAccThresh   = cms.uint32(2),
+        alctAccPattern  = cms.uint32(4),
+        alctTrigMode    = cms.uint32(3),
+        alctMode        = cms.uint32(1),
+        alctL1aWindow   = cms.uint32(5),
+        verbosity = cms.untracked.int32(0)
     ),
+
     # Parameters for ALCT processors: MTCC-I
     alctParamMTCC1 = cms.PSet(
-        alctMode = cms.uint32(2),
-        alctTrigMode = cms.uint32(0),
-        verbosity = cms.untracked.int32(0),
-        alctNphPattern = cms.uint32(4),
-        alctBxWidth = cms.uint32(6),
-        alctL1aWindow = cms.uint32(3),
-        alctDriftDelay = cms.uint32(3),
-        alctNphThresh = cms.uint32(4),
-        alctFifoTbins = cms.uint32(10),
-        alctFifoPretrig = cms.uint32(12)
+        alctFifoTbins   = cms.uint32(10),
+        alctFifoPretrig = cms.uint32(12),
+        alctBxWidth     = cms.uint32(6),
+        alctDriftDelay  = cms.uint32(3),
+        alctNphThresh   = cms.uint32(4),
+        alctNphPattern  = cms.uint32(4),
+        alctAccThresh   = cms.uint32(4),
+        alctAccPattern  = cms.uint32(4),
+        alctTrigMode    = cms.uint32(0),
+        alctMode        = cms.uint32(2),
+        alctL1aWindow   = cms.uint32(3),
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    # Parameters for ALCT processors: MTCC-II
+    alctParamMTCC2 = cms.PSet(
+        alctFifoTbins   = cms.uint32(16),
+        alctFifoPretrig = cms.uint32(10),
+        alctBxWidth     = cms.uint32(6),
+        alctDriftDelay  = cms.uint32(3),
+        alctNphThresh   = cms.uint32(2),
+        alctNphPattern  = cms.uint32(4),
+        alctAccThresh   = cms.uint32(2),
+        alctAccPattern  = cms.uint32(4),
+        alctTrigMode    = cms.uint32(2),
+        alctMode        = cms.uint32(0),
+        alctL1aWindow   = cms.uint32(3),
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    # Parameters for CLCT processors: default
+    clctParamDef = cms.PSet(
+        clctBxWidth     = cms.uint32(6),
+        clctDriftDelay  = cms.uint32(2),
+        clctHsThresh    = cms.uint32(2),
+        clctDsThresh    = cms.uint32(2),
+        clctNphPattern  = cms.uint32(4),
+        clctFifoTbins   = cms.uint32(12),
+        clctFifoPretrig = cms.uint32(7),
+        # TMB07 parameters
+        clctPidThresh   = cms.uint32(2)
+        clctSepSrc      = cms.uint32(1),
+        clctSepVme      = cms.uint32(10),
+        # Debug
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    # Parameters for CLCT processors: MTCC-II
+    clctParamMTCC2 = cms.PSet(
+        clctBxWidth     = cms.uint32(6),
+        clctDriftDelay  = cms.uint32(2),
+        clctHsThresh    = cms.uint32(4),
+        clctDsThresh    = cms.uint32(4),
+        clctNphPattern  = cms.uint32(1),
+        clctFifoTbins   = cms.uint32(12),
+        clctFifoPretrig = cms.uint32(7),
+        # TMB07 parameters
+        clctHitThresh   = cms.uint32(2),
+        clctPidThresh   = cms.uint32(2),
+        clctSepSrc      = cms.uint32(1),
+        clctSepVme      = cms.uint32(10),
+        # Debug
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    tmbParam = cms.PSet(
+        verbosity = cms.untracked.int32(0)
     )
 )
-
-
