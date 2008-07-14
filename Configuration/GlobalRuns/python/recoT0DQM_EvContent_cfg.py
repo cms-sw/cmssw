afc50bc73b056bbface42b2445f0547839f24b80
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-07-14 19:22:40 +0000

    fix python version to work with CRUZET2 data. this file is still far away frombeing in sync with the cfg version used for CRUZET V3 processing. will clean this mess up this week to have this file work also with CRUZET3

--- Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py
+++ Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py
@@ -6,58 +6,17 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-# output module
-#
-process.load("Configuration.EventContent.EventContentCosmics_cff")
-
-# Conditions (Global Tag is used here):
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-
-# Magnetic fiuld: force mag field to be 0.0 tesla
-process.load("Configuration.StandardSequences.MagneticField_cff")
-
-
-
-
-#Geometry
-process.load("Configuration.StandardSequences.Geometry_cff")
-
-# reconstruction sequence for Cosmics
-process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
-
-process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
-
-
-process.SteppingHelixPropagatorAny.useInTeslaFromMagField = True
-process.SteppingHelixPropagatorAlong.useInTeslaFromMagField = True
-process.SteppingHelixPropagatorOpposite.useInTeslaFromMagField = True
-process.SteppingHelixPropagatorAny.SetVBFPointer = True
-process.SteppingHelixPropagatorAlong.SetVBFPointer = True
-process.SteppingHelixPropagatorOpposite.SetVBFPointer = True
-process.VolumeBasedMagneticFieldESProducer.label = 'VolumeBasedMagneticField'
-
-
-# Real data raw to digi
-process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
-
-# offline DQM
-process.load("DQMOffline.Configuration.DQMOffline_cff")
-process.load("DQMServices.Components.MEtoEDMConverter_cff")
-
-#L1 trigger validation
-#process.load("L1Trigger.HardwareValidation.L1HardwareValidation_cff")
-#process.load("L1Trigger.Configuration.L1Config_cff")
-#process.load("L1TriggerConfig.CSCTFConfigProducers.CSCTFConfigProducer_cfi")
-#process.load("L1TriggerConfig.CSCTFConfigProducers.L1MuCSCTFConfigurationRcdSrc_cfi")
 
 process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
-    firstFreeID = cms.untracked.uint32(131072),
+    #firstFreeID = cms.untracked.uint32(131072),
     fileNames = cms.untracked.vstring(
-	'/store/data/CRUZET2/Cosmics/RAW/v1/000/046/549/28581B1A-B137-DD11-B2B7-000423D9870C.root', 
-        '/store/data/CRUZET2/Cosmics/RAW/v1/000/046/549/409C7C75-B137-DD11-ABA6-000423D9880C.root', 
         '/store/data/CRUZET2/Cosmics/RAW/v1/000/046/549/8CB01268-B437-DD11-8FF6-000423D6B358.root')
 )
+
+# output module
+#
+process.load("Configuration.EventContent.EventContentCosmics_cff")
 
 process.FEVT = cms.OutputModule("PoolOutputModule",
     process.FEVTEventContent,
@@ -65,21 +24,6 @@ process.FEVT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('reco.root')
 )
 
-process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py,v $'),
-    annotation = cms.untracked.string('CRUZET Prompt Reco with DQM')
-)
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) ) ## default is false
-
-process.GlobalTag.connect = 'sqlite_file:/afs/cern.ch/user/m/malgeri/public/globtag/CRUZET.db'
-process.GlobalTag.globaltag = 'CRUZET2_AR::All'
-process.prefer("GlobalTag")
-
-process.allPath = cms.Path(process.RawToDigi*process.reconstructionCosmics*process.DQMOffline*process.MEtoEDMConverter)
-process.outpath = cms.EndPath(process.FEVT)
-process.MessageLogger.cerr.default.limit = 10
-process.FEVT.outputCommands.append('keep FEDRawDataCollection_source__*')
 process.FEVT.outputCommands.append('keep CaloTowersSorted_calotoweroptmaker_*_*')
 process.FEVT.outputCommands.append('keep CSCDetIdCSCALCTDigiMuonDigiCollection_muonCSCDigis_MuonCSCALCTDigi_*')
 process.FEVT.outputCommands.append('keep CSCDetIdCSCCLCTDigiMuonDigiCollection_muonCSCDigis_MuonCSCCLCTDigi_*')
@@ -100,3 +44,73 @@ process.FEVT.outputCommands.append('keep PixelDigiedmDetSetVector_siPixelDigis_*
 process.FEVT.outputCommands.append('keep recoCandidatesOwned_caloTowersOpt_*_*')
 process.FEVT.outputCommands.append('keep RPCDetIdRPCDigiMuonDigiCollection_muonRPCDigis_*_*')
 
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.2 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py,v $'),
+    annotation = cms.untracked.string('CRUZET Prompt Reco with DQM')
+)
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) ) ## default is false
+
+
+# Conditions (Global Tag is used here):
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.connect = 'sqlite_file:/afs/cern.ch/user/m/malgeri/public/globtag/CRUZET.db'
+process.GlobalTag.globaltag = 'CRZT210_DRV::All'
+process.prefer("GlobalTag")
+
+# Magnetic fiuld: force mag field to be 0.0 tesla
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
+#Geometry
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+# Real data raw to digi
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+
+# reconstruction sequence for Cosmics
+process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
+
+# patch needed for CRUZET2 but not used in CRUZET3 cfg
+process.SteppingHelixPropagatorAny.useInTeslaFromMagField = True
+process.SteppingHelixPropagatorAlong.useInTeslaFromMagField = True
+process.SteppingHelixPropagatorOpposite.useInTeslaFromMagField = True
+process.SteppingHelixPropagatorAny.SetVBFPointer = True
+process.SteppingHelixPropagatorAlong.SetVBFPointer = True
+process.SteppingHelixPropagatorOpposite.SetVBFPointer = True
+process.VolumeBasedMagneticFieldESProducer.label = 'VolumeBasedMagneticField'
+
+
+# offline DQM
+process.load("DQMOffline.Configuration.DQMOffline_cff")
+process.load("DQMServices.Components.MEtoEDMConverter_cff")
+
+#L1 trigger validation
+#process.load("L1Trigger.HardwareValidation.L1HardwareValidation_cff")
+process.load("L1Trigger.Configuration.L1Config_cff")
+process.load("L1TriggerConfig.CSCTFConfigProducers.CSCTFConfigProducer_cfi")
+process.load("L1TriggerConfig.CSCTFConfigProducers.L1MuCSCTFConfigurationRcdSrc_cfi")
+
+#
+## add quality info
+#
+#process.load("CalibTracker/SiStripESProducers/data/SiStripQualityESProducer.cfi")
+#replace siStripQualityESProducer.ListOfRecordToMerge = {
+#     { string record = "SiStripFedCablingRcd" string tag = "" },
+#     { string record = "SiStripBadChannelRcd" string tag = "" }
+# }
+#process.prefer("siStripQualityESProducer")
+
+# ad hoc changes for V3 reco to lower seed threshold to 5 GeV
+process.cosmicseedfinderP5.SeedPt = 5
+process.combinatorialcosmicseedfinderP5.SeedMomentum = 5
+
+process.load("DQMOffline.Muon.muonCosmicMonitors_cff")
+process.muonTrackAnalyzers = cms.Sequence(process.muonTrackCosmicAnalyzers)
+#process.DQMOffline_fixed   = cms.Sequence(process.SiStripDQMTier0*process.ecal_dqm_source_offline7*process.muonCosmicMonitors*process.jetMETAnalyzer*process.hcalOfflineDQMSource*process.siPixelOfflineDQM_source*process.l1tmonitor )
+#process.DQMOffline_fixed   = cms.Sequence(process.SiStripDQMTier0*process.ecal_dqm_source_offline7*process.muonCosmicMonitors*process.jetMETAnalyzer*process.hcalOfflineDQMSource*process.l1tmonitor )
+
+#process.allPath = cms.Path( process.RawToDigi * process.reconstructionCosmics *  process.DQMOffline_fixed * process.MEtoEDMConverter)
+process.allPath = cms.Path( process.RawToDigi * process.reconstructionCosmics )
+
+process.outpath = cms.EndPath(process.FEVT)
