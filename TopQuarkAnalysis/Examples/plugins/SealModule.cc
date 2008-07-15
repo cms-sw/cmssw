fbbd2025d4b027aeb7d78aeb817ffdb402e8ef0c
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-15 22:55:47 +0000

    intermediate state

---
+++ TopQuarkAnalysis/Examples/plugins/SealModule.cc
@@ -0,0 +1,7 @@
+#include "TopQuarkAnalysis/Examples/interface/TtSemiEvtKit.h"
+#include "TopQuarkAnalysis/Examples/plugins/SemiLepHypothesesAnalyzer.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(TtSemiEvtKit);
+DEFINE_FWK_MODULE(SemiLepHypothesesAnalyzer);
+
