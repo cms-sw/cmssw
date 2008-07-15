c10cef2081825689d1bea4afe4d018772aa96c42
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-07-15 14:53:35 +0000

    add missing import for Geometry and Magnetic Field

--- SimTracker/TrackerMaterialAnalysis/python/trackingMaterialAnalyser_cff.py
+++ SimTracker/TrackerMaterialAnalysis/python/trackingMaterialAnalyser_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Geometry and Magnetic Field
+from Configuration.StandardSequences.Geometry_cff import *
+from Configuration.StandardSequences.MagneticField_40T_cff import *
 from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
 from SimTracker.TrackerMaterialAnalysis.trackingMaterialAnalyser_cfi import *
-
