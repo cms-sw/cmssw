c4f33ff39de74178358165e6d4c70b815c956653
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-07-15 15:50:17 +0000

    clean up

--- SimTracker/TrackerMaterialAnalysis/python/double10GeVPions_cfi.py
+++ SimTracker/TrackerMaterialAnalysis/python/double10GeVPions_cfi.py
@@ -1,20 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
-# generate single-particle events
+# generate pi0/pi0* events
 source = cms.Source("FlatRandomPtGunSource",
     PGunParameters = cms.untracked.PSet(
-        MaxPt = cms.untracked.double(10.01),
-        MinPt = cms.untracked.double(9.99),
         # you can request more than 1 particle
         PartID = cms.untracked.vint32(211),
-        MaxEta = cms.untracked.double(4.0),
-        MaxPhi = cms.untracked.double(3.14159265359),
         MinEta = cms.untracked.double(-4.0),
+        MaxEta = cms.untracked.double( 4.0),
         MinPhi = cms.untracked.double(-3.14159265359)
+        MaxPhi = cms.untracked.double( 3.14159265359),
+        MinPt  = cms.untracked.double( 9.99),
+        MaxPt  = cms.untracked.double(10.01),
     ),
-    Verbosity = cms.untracked.int32(0),
     AddAntiParticle = cms.untracked.bool(True),
-    firstRun = cms.untracked.uint32(1)
+    Verbosity       = cms.untracked.int32(0),
+    firstRun        = cms.untracked.uint32(1)
 )
-
-
