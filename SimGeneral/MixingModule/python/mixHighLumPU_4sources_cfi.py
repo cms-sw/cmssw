2c00c5920373ee782bcb2064eb66d1bc9a8ebbb9
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-07-15 16:26:51 +0000

    Merge changes in V03-01-00 and updates in the older version

--- SimGeneral/MixingModule/python/mixHighLumPU_4sources_cfi.py
+++ SimGeneral/MixingModule/python/mixHighLumPU_4sources_cfi.py
@@ -19,6 +19,7 @@ mix = cms.EDFilter("MixingModule",
     bunchspace = cms.int32(25), ## nsec
 
     playback = cms.untracked.bool(False),
+    useCurrentProcessOnly = cms.bool(False),
     input = cms.SecSource("PoolSource",
         nbPileupEvents = cms.PSet(
             sigmaInel = cms.double(80.0),
