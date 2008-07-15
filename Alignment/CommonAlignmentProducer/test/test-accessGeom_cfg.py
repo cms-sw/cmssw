ff4a4b9a86ba923d7b5719a9b1ede10c565f50ad
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-07-15 21:07:15 +0000

    applyAlignment now true by default, add tracker/muon only frontier

--- Alignment/CommonAlignmentProducer/test/test-accessGeom_cfg.py
+++ Alignment/CommonAlignmentProducer/test/test-accessGeom_cfg.py
@@ -5,34 +5,32 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 #    service = Tracer {}
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
-
 process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
-
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
 process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
-
 process.load("Geometry.DTGeometryBuilder.dtGeometry_cfi")
-
 process.load("Geometry.CSCGeometryBuilder.cscGeometry_cfi")
 
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+# global tag...
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#... or fake...
+#process.load("Configuration.StandardSequences.FakeConditions_cff")
+#... or specific tracker/muon conditions
+process.load("CalibTracker.Configuration.TrackerAlignment.TrackerAlignment_Frontier_cff")
+process.load("CalibMuon.Configuration.Muon_FrontierAlignment_cff")
+#process.load("CalibTracker.Configuration.TrackerAlignment.TrackerAlignment_Frontier_IntDB_cff")
+#process.load("CalibMuon.Configuration.Muon_FrontierAlignment_IntDB_cff")
 
-#    include "Configuration/StandardSequences/data/FakeConditions.cff"
-#    replace TrackerDigiGeometryESModule.applyAlignment = true
-#    replace DTGeometryESModule.applyAlignment = true
-#    replace CSCGeometryESModule.applyAlignment = true
 process.load("Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff")
-
 process.load("Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff")
-
 process.load("Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff")
 
 process.source = cms.Source("EmptySource")
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
+
 process.demo = cms.EDFilter("TestAccessGeom",
     # spaces are removed internally - but are needed since framework seems to ignore empty strings...
     TrackerGeomLabels = cms.vstring('idealForDigi', 
