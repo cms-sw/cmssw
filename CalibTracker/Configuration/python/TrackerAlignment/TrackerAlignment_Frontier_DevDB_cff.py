f37be954985bc39ad217504eed13b0090a4439ec
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-07-15 20:42:12 +0000

    - adjust tags and DB accounts for 21X - applyAlignment now is true by default - remove any es_prefer

--- CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Frontier_DevDB_cff.py
+++ CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Frontier_DevDB_cff.py
@@ -1,23 +1,16 @@
-# The following comments couldn't be translated into the new config version:
-
-#FrontierDev/CMS_COND_ALIGNMENT"
-# FIXME: Should we use that?
 import FWCore.ParameterSet.Config as cms
 
 from Alignment.CommonAlignmentProducer.GlobalPosition_Frontier_DevDB_cff import *
+
 import CalibTracker.Configuration.Common.PoolDBESSource_cfi
 trackerAlignment = CalibTracker.Configuration.Common.PoolDBESSource_cfi.poolDBESSource.clone()
-from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
-es_prefer_trackerAlignment = cms.ESPrefer("PoolDBESSource","trackerAlignment")
-trackerAlignment.connect = 'frontier://FrontierDev/CMS_COND_ALIGNMENT'
+trackerAlignment.connect = 'frontier://FrontierPrep/CMS_COND_ALIGNMENT'
 trackerAlignment.toGet = cms.VPSet(cms.PSet(
     record = cms.string('TrackerAlignmentRcd'),
-    tag = cms.string('TrackerIdealGeometry200')
+    tag = cms.string('TrackerIdealGeometry210_mc')
 ), 
     cms.PSet(
         record = cms.string('TrackerAlignmentErrorRcd'),
-        tag = cms.string('TrackerIdealGeometryErrors200')
+        tag = cms.string('TrackerIdealGeometryErrors210_mc')
     ))
-# to apply misalignments
-TrackerDigiGeometryESModule.applyAlignment = True
 
--- CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Frontier_cff.py
+++ CalibTracker/Configuration/python/TrackerAlignment/TrackerAlignment_Frontier_cff.py
@@ -1,24 +1,22 @@
 # The following comments couldn't be translated into the new config version:
 
-#FrontierProd/CMS_COND_20X_ALIGNMENT"
-# FIXME: Should we use that?
 import FWCore.ParameterSet.Config as cms
 
 from Alignment.CommonAlignmentProducer.GlobalPosition_Frontier_cff import *
 import CalibTracker.Configuration.Common.PoolDBESSource_cfi
 trackerAlignment = CalibTracker.Configuration.Common.PoolDBESSource_cfi.poolDBESSource.clone()
-from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
-es_prefer_trackerAlignment = cms.ESPrefer("PoolDBESSource","trackerAlignment")
-trackerAlignment.connect = 'frontier://FrontierProd/CMS_COND_20X_ALIGNMENT'
-# replace trackerAlignment.connect = "oracle://cms_orcoff_int2r/CMS_COND_ALIGNMENT" # oracle dev. DB
+trackerAlignment.connect = 'frontier://FrontierProd/CMS_COND_21X_ALIGNMENT'
+#trackerAlignment.connect = 'oracle://cms_orcoff_prep/CMS_COND_ALIGNMENT' # oracle dev. DB
 trackerAlignment.toGet = cms.VPSet(cms.PSet(
     record = cms.string('TrackerAlignmentRcd'),
-    tag = cms.string('TrackerIdealGeometry200')
+    tag = cms.string('TrackerIdealGeometry210_mc')
 ), 
     cms.PSet(
         record = cms.string('TrackerAlignmentErrorRcd'),
-        tag = cms.string('TrackerIdealGeometryErrors200')
+        tag = cms.string('TrackerIdealGeometryErrors210_mc')
     ))
+#from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
+#es_prefer_trackerAlignment = cms.ESPrefer("PoolDBESSource","trackerAlignment")
 # to apply misalignments
-TrackerDigiGeometryESModule.applyAlignment = True
+#TrackerDigiGeometryESModule.applyAlignment = True
 
