b42883f69ab79a14176a8caf89372700db47a25a
Sam James Harper <sha1-46bf45603b908af75fae6d2e55b9a2f9bfca0d8e@cern.ch>
2008-07-15 14:20:55 +0000

    one file test before I add the rest of Egamma HLT offline DQM package

--- DQMOffline/Trigger/src/SealModule.cc
+++ DQMOffline/Trigger/src/SealModule.cc
@@ -1,6 +1,9 @@
-// $Id: SealModule.cc,v 1.5 2008/05/24 14:43:54 wittich Exp $
+// $Id: SealModule.cc,v 1.1 2008/06/12 21:58:49 berryhil Exp $
 
 #include "DQMOffline/Trigger/interface/FourVectorHLTOffline.h"
+#include "DQMOffline/Trigger/interface/EgammaHLTOffline.h"
+#include "DQMOffline/Trigger/interface/EgHLTOfflineClient.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(FourVectorHLTOffline);
-
+DEFINE_ANOTHER_FWK_MODULE(EgammaHLTOffline);
+DEFINE_ANOTHER_FWK_MODULE(EgHLTOfflineClient);
