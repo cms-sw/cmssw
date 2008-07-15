b9bcac7449ee698d03c936ce9b2737678565d977
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-07-15 16:03:22 +0000

    fix typo

--- SimTracker/TrackerMaterialAnalysis/python/double10GeVMuons_cfi.py
+++ SimTracker/TrackerMaterialAnalysis/python/double10GeVMuons_cfi.py
@@ -7,10 +7,10 @@ source = cms.Source("FlatRandomPtGunSource",
         PartID = cms.untracked.vint32(13),
         MinEta = cms.untracked.double(-4.0),
         MaxEta = cms.untracked.double( 4.0),
-        MinPhi = cms.untracked.double(-3.14159265359)
+        MinPhi = cms.untracked.double(-3.14159265359),
         MaxPhi = cms.untracked.double( 3.14159265359),
         MinPt  = cms.untracked.double( 9.99),
-        MaxPt  = cms.untracked.double(10.01),
+        MaxPt  = cms.untracked.double(10.01)
     ),
     AddAntiParticle = cms.untracked.bool(True),
     Verbosity       = cms.untracked.int32(0),
