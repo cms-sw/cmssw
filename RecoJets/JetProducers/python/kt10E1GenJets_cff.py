114271e8884d207ff9cf9b105b010b066fced9d3
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2008-07-16 03:37:08 +0000

    correct configuration

--- RecoJets/JetProducers/python/kt10E1GenJets_cff.py
+++ RecoJets/JetProducers/python/kt10E1GenJets_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: kt10E1GenJets.cff,v 1.1 2007/08/02 21:58:22 fedor Exp $
+# $Id: kt10E1GenJets_cff.py,v 1.2 2008/04/21 03:28:40 rpw Exp $
 from RecoJets.JetProducers.GenJetParameters_cfi import *
+from RecoJets.JetProducers.FastjetParameters_cfi import *
 from RecoJets.JetProducers.KtJetParameters_cfi import *
 kt10E1GenJets = cms.EDProducer("KtJetProducer",
     FastjetNoPU,
     KtJetParameters,
-    CaloJetParameters,
+    GenJetParameters,
     JetPtMin = cms.double(1.0),
     alias = cms.untracked.string('KT10E1GenJet'),
     FJ_ktRParam = cms.double(1.0)
