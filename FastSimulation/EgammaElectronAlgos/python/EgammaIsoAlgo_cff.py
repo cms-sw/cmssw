dbcb655875c88873689de2ec2984f0a3b2a096f5
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2008-07-16 15:44:50 +0000

    removed a file which was not needed (and was making python complaining) and deactivate the hit splitting of the GSF tracks

--- FastSimulation/EgammaElectronAlgos/python/EgammaIsoAlgo_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoEgamma.EgammaIsolationAlgos.egammaCalExtractorBlocks_cff import *
-
-EgammaIsoEcalFromHitsExtractorBlock.barrelRecHits = 'caloRecHits:EcalRecHitsEB'
-EgammaIsoEcalFromHitsExtractorBlock.endcapRecHits = 'caloRecHits:EcalRecHitsEE'
-EgammaIsoHcalFromHitsExtractorBlock.hcalRecHits = 'caloRecHits'
-
-from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
--- FastSimulation/EgammaElectronAlgos/python/electronGSGsfTrackCandidates_cff.py
+++ FastSimulation/EgammaElectronAlgos/python/electronGSGsfTrackCandidates_cff.py
@@ -7,4 +7,5 @@ electronGSGsfTrackCandidates.SeedProducer = cms.InputTag("electronGSPixelSeeds")
 electronGSGsfTrackCandidates.TrackProducers = []
 electronGSGsfTrackCandidates.SeedCleaning = True
 electronGSGsfTrackCandidates.MinNumberOfCrossedLayers = 5
+electronGSGsfTrackCandidates.SplitHits = False
 
