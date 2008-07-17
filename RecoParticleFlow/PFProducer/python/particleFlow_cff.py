fd45a96240bcf01f33b80f5b000078dbe2443554
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-07-17 14:28:36 +0000

    removed inclusion of CaloGeometry_cfi that should happen only via Geometry_cff and ONLY in the high level cfg file

--- RecoParticleFlow/PFProducer/python/particleFlow_cff.py
+++ RecoParticleFlow/PFProducer/python/particleFlow_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 #Geometry
-from Geometry.CaloEventSetup.CaloGeometry_cfi import *
 # include used for track reconstruction 
 # note that tracking is redone since we need updated hits and they 
 # are not stored in the event!
