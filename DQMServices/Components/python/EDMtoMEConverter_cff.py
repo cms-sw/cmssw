9134d22de418a9c9cc556ae1b0b5bd95d7d3dfad
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-07-18 16:47:16 +0000

    - add parameter

--- DQMServices/Components/python/EDMtoMEConverter_cff.py
+++ DQMServices/Components/python/EDMtoMEConverter_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 # needed backend
 DQMStore = cms.Service("DQMStore",
     referenceFileName = cms.untracked.string(''),
-    verbose = cms.untracked.int32(0)
+    verbose = cms.untracked.int32(0),
+    collateHistograms = cms.bool(False)
 )
 
 # needed output
