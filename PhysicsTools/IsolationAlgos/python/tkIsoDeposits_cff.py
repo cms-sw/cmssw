cb01ca0f8ea186e3fda71d3f3361d4f3eca474e7
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2008-07-18 10:13:18 +0000

    - removed dependencies from cfg in /test

--- PhysicsTools/IsolationAlgos/python/tkIsoDeposits_cff.py
+++ PhysicsTools/IsolationAlgos/python/tkIsoDeposits_cff.py
@@ -1,63 +1,75 @@
 import FWCore.ParameterSet.Config as cms
 
+#pieces needed to run the associator-related stuff
 from Geometry.CMSCommonData.cmsIdealGeometryXML_cff import *
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
 from Geometry.CaloEventSetup.CaloGeometry_cfi import *
 from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi import *
 from TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff import *
+from PhysicsTools.RecoAlgos.highPtTracks_cfi import *
+from RecoMuon.MuonIsolationProducers.isoDepositProducerIOBlocks_cff import *
 from RecoMuon.MuonIsolationProducers.trackExtractorBlocks_cff import *
 from RecoMuon.MuonIsolationProducers.caloExtractorByAssociatorBlocks_cff import *
 from RecoMuon.MuonIsolationProducers.jetExtractorBlock_cff import *
 from RecoMuon.MuonIsolationProducers.caloExtractorBlocks_cff import *
-from PhysicsTools.RecoAlgos.highPtTracks_cfi import *
-
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCtfTk_cfi
-tkIsoDepositTk = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCtfTk_cfi.candIsoDepositCtfTk.clone()
-
-tkIsoDepositTk.src = 'highPtTracks'
-tkIsoDepositTk.trackType = 'best'
-tkIsoDepositTk.ExtractorPSet = cms.PSet(
-    MIsoTrackExtractorBlock
+tkIsoDepositTk = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(False),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoTrackExtractorBlock
+    )
 )
 
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCalByAssociatorTowers_cfi
-tkIsoDepositCalByAssociatorTowers = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCalByAssociatorTowers_cfi.candIsoDepositCalByAssociatorTowers.clone()
-
-tkIsoDepositCalByAssociatorTowers.src = 'highPtTracks'
-tkIsoDepositCalByAssociatorTowers.trackType = 'best'
-
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCalByAssociatorHits_cfi
-tkIsoDepositCalByAssociatorHits = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCalByAssociatorHits_cfi.candIsoDepositCalByAssociatorHits.clone()
-
-tkIsoDepositCalByAssociatorHits.src = 'highPtTracks'
-tkIsoDepositCalByAssociatorHits.trackType = 'best'
-
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositJets_cfi
-tkIsoDepositJets = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositJets_cfi.candIsoDepositJets.clone()
-
-tkIsoDepositJets.src = 'highPtTracks'
-tkIsoDepositJets.trackType = 'best'
-
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCal_cfi
-tkIsoDepositCalEcal = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCal_cfi.candIsoDepositCal.clone()
-
-tkIsoDepositCalEcal.src = 'highPtTracks'
-tkIsoDepositCalEcal.trackType = 'best'
-tkIsoDepositCalEcal.ExtractorPSet = cms.PSet(
-    MIsoCaloExtractorEcalBlock
+tkIsoDepositCalByAssociatorTowers = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(True),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoCaloExtractorByAssociatorTowersBlock
+    )
 )
 
-import PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCal_cfi
-tkIsoDepositCalHcal = PhysicsTools.IsolationAlgos.test.mu.candIsoDepositCal_cfi.candIsoDepositCal.clone()
-
-tkIsoDepositCalHcal.src = 'highPtTracks'
-tkIsoDepositCalHcal.trackType = 'best'
-tkIsoDepositCalHcal.ExtractorPSet = cms.PSet(
-    MIsoCaloExtractorHcalBlock
+tkIsoDepositCalByAssociatorHits = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(True),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoCaloExtractorByAssociatorHitsBlock
+    )
 )
 
+tkIsoDepositJets = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(False),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoJetExtractorBlock
+    )
+)
+
+tkIsoDepositCalEcal = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(False),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoCaloExtractorEcalBlock
+    )
+)
+
+tkIsoDepositCalHcal = cms.EDProducer("CandIsoDepositProducer",
+    src = cms.InputTag("highPtTracks"),
+    MultipleDepositsFlag = cms.bool(False),
+    trackType = cms.string('best'),
+    ExtractorPSet = cms.PSet(
+        MIsoCaloExtractorHcalBlock
+    )
+)
+
+#
+# and now sequences of the above
+#
+# "standard sequence"
 tkIsoDeposits = cms.Sequence(highPtTracks+tkIsoDepositTk+tkIsoDepositCalByAssociatorTowers+tkIsoDepositJets)
-
 
 
