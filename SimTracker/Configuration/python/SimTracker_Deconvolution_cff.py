ef458a5292a2c80ce1078f7ba2f1a2efeb97d853
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-07-19 07:46:53 +0000

    adjust the cfg for PU production

--- SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py
+++ SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py
@@ -1,11 +1,14 @@
 # Auto generated configuration file
 # using: 
-# $Revision: 1.45 $
-# $Source: /cvs/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v $
+# $Revision: 1.1 $
+# $Source: /cvs_server/repositories/CMSSW/CMSSW/SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py,v $
 
 import FWCore.ParameterSet.Config as cms
 def customise(process):
-    
+    # Load and Prefer the FakeConditions for SiStripNoise
+    process.load("CalibTracker.SiStripESProducers.SiStripNoiseFakeSource_cfi")
+    process.prefer("SiStripNoiseFakeESSource")
+                                                                                          
     # Signal in Deconvolution Mode
     process.simSiStripDigis.APVpeakmode = cms.bool(False)
 
