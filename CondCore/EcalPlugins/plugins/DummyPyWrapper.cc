979b8437e7aec323a67556bc31aea6f945e4f0ad
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-07-20 15:04:02 +0000

    try ths

--- CondCore/EcalPlugins/plugins/DummyPyWrapper.cc
+++ CondCore/EcalPlugins/plugins/DummyPyWrapper.cc
@@ -30,17 +30,17 @@
 namespace { const char * pluginName_ = "pluginEcalDummiesPyInterface"; }
 
 BOOST_PYTHON_MODULE(pluginEcalDummiesPyInterface) {
-    define<cond::PayLoadInspector<EcalWeightXtalGroups>();
-    define<cond::PayLoadInspector<EcalTBWeights>();
-    define<cond::PayLoadInspector<EcalGainRatios>();
-    define<cond::PayLoadInspector<EcalFloatCondObjectContainer>();
-    define<cond::PayLoadInspector<EcalFloatCondObjectContainer>();
-    define<cond::PayLoadInspector<EcalADCToGeVConstant>();
-    define<cond::PayLoadInspector<EcalFloatCondObjectContainer>();
-    define<cond::PayLoadInspector<EcalLaserAPDPNRatios>();
-    define<cond::PayLoadInspector<EcalFloatCondObjectContainer>();
-    define<cond::PayLoadInspector<EcalChannelStatus>();
-    define<cond::PayLoadInspector<EcalMappingElectronics>();
+  define<cond::PayLoadInspector<EcalWeightXtalGroups> >();
+  define<cond::PayLoadInspector<EcalTBWeights> >();
+  define<cond::PayLoadInspector<EcalGainRatios> >();
+  define<cond::PayLoadInspector<EcalFloatCondObjectContainer> >();
+  define<cond::PayLoadInspector<EcalFloatCondObjectContainer> >();
+  define<cond::PayLoadInspector<EcalADCToGeVConstant> >();
+  define<cond::PayLoadInspector<EcalFloatCondObjectContainer> >();
+  define<cond::PayLoadInspector<EcalLaserAPDPNRatios> >();
+  define<cond::PayLoadInspector<EcalFloatCondObjectContainer> >();
+  define<cond::PayLoadInspector<EcalChannelStatus> >();
+  define<cond::PayLoadInspector<EcalMappingElectronics> >();
 }
 
 PYTHON_ID(EcalWeightXtalGroups,pluginName_);
