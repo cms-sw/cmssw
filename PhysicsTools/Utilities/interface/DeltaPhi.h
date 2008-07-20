9587c19fe1f5ec1a977afe7592b295b6bd047c79
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2008-07-20 17:49:59 +0000

    removed duplicated headers

--- PhysicsTools/Utilities/interface/DeltaPhi.h
+++
@@ -1,9 +0,0 @@
-#ifndef Utilities_DeltaPhi_h
-#define Utilities_DeltaPhi_h
-
-#warning This header file has moved. Please use, #include PhysicsTools/Utilities/interface/deltaPhi.h
-
-#include "PhysicsTools/Utilities/interface/deltaPhi.h"
-
-
-#endif
--- PhysicsTools/Utilities/interface/DeltaR.h
+++
@@ -1,8 +0,0 @@
-#ifndef PhysicsTools_Utilities_DeltaR_h
-#define PhysicsTools_Utilities_DeltaR_h
-
-#warning This header file has moved. Please use, #include PhysicsTools/Utilities/interface/deltaR.h
-
-#include "PhysicsTools/Utilities/interface/deltaR.h"
-
-#endif
