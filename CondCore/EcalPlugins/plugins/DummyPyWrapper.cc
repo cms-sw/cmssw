07f5b05af4d646702d1f40624809bec2f58d691c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-07-20 14:20:18 +0000

    try to add dummies

---
+++ CondCore/EcalPlugins/plugins/DummyPyWrapper.cc
@@ -0,0 +1,42 @@
+#include "CondCore/Utilities/interface/PayLoadInspector.h"
+#include "CondCore/Utilities/interface/InspectorPythonWrapper.h"
+
+
+#include "CondFormats/EcalObjects/interface/EcalWeightXtalGroups.h"
+
+#include "CondFormats/EcalObjects/interface/EcalTBWeights.h"
+
+#include "CondFormats/EcalObjects/interface/EcalGainRatios.h"
+
+#include "CondFormats/EcalObjects/interface/EcalCondObjectContainer.h"
+
+#include "CondFormats/EcalObjects/interface/EcalADCToGeVConstant.h"
+
+
+
+
+#include "CondFormats/EcalObjects/interface/EcalLaserAPDPNRatios.h"
+
+
+#include "CondFormats/EcalObjects/interface/EcalChannelStatus.h"
+
+
+#include "CondFormats/EcalObjects/interface/EcalMappingElectronics.h"
+
+
+
+
+
+
+
+PYTHON_WRAPPER(EcalWeightXtalGroups,EcalDummies);
+PYTHON_WRAPPER(EcalTBWeights,EcalDummies);
+PYTHON_WRAPPER(EcalGainRatios,EcalDummies);
+PYTHON_WRAPPER(EcalFloatCondObjectContainer,EcalDummies);
+PYTHON_WRAPPER(EcalFloatCondObjectContainer,EcalDummies);
+PYTHON_WRAPPER(EcalADCToGeVConstant,EcalDummies);
+PYTHON_WRAPPER(EcalFloatCondObjectContainer,EcalDummies);
+PYTHON_WRAPPER(EcalLaserAPDPNRatios,EcalDummies);
+PYTHON_WRAPPER(EcalFloatCondObjectContainer,EcalDummies);
+PYTHON_WRAPPER(EcalChannelStatus,EcalDummies);
+PYTHON_WRAPPER(EcalMappingElectronics,EcalDummies);
