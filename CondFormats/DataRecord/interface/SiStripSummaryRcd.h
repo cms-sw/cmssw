d593b4345fe3b85a789cb0a8835b2f2f33ed0d54
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-07-21 23:16:27 +0000

    added SiStripSummaryRcd for Historic DQM

---
+++ CondFormats/DataRecord/interface/SiStripSummaryRcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormats_SiStripSummaryRcd_h
+#define CondFormats_SiStripSummaryRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripSummaryRcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripSummaryRcd> {};
+
+#endif
