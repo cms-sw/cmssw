819f6356bf9a3a38b0fc98d9e4590e37464634da
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-21 18:40:08 +0000

    re-arrangement of cff files

--- TopQuarkAnalysis/TopObjectProducers/python/tqafLayer1_fast_cff.py
+++ TopQuarkAnalysis/TopObjectProducers/python/tqafLayer1_fast_cff.py
@@ -7,20 +7,14 @@ import FWCore.ParameterSet.Config as cms
 ## extra includes for fastsim
 from PhysicsTools.PatAlgos.famos.famosSequences_cff import *
 
-## extra replacements for fastsim
-#allLayer1Photons.addTrigMatch = False
-
 #-----------------------------------------------------------------
 # build tqafLayer0 objects (jets, muons, electrons, mets, taus)
 #-----------------------------------------------------------------
 from PhysicsTools.PatAlgos.patLayer0_cff import *
 
 ## define the tqafLayer0 input
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_Jets_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_Muons_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_Elecs_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_Taus_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_METs_cff import *
+from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_objectCleaning_cff import *
+from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_mcMatching_cff import *
 
 #-----------------------------------------------------------------
 # build tqafLayer1 objects (jets, muons, electrons, mets, taus)
@@ -28,11 +22,8 @@ from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer0_METs_cff import *
 from PhysicsTools.PatAlgos.patLayer1_cff import *
 
 ## define the tqafLayer1 input
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_Jets_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_Muons_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_Elecs_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_Taus_cff import *
-from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_METs_cff import *
+from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_objectProduction_cff import *
+from TopQuarkAnalysis.TopObjectProducers.fast.tqafLayer1_objectSelection_cff import *
 
 ## std sequence for tqafLayer1 production (w/o trigger for fastsim)
 tqafLayer1_withoutTrigMatch = cms.Sequence(patLayer0_withoutTrigMatch*patLayer1)
--- TopQuarkAnalysis/TopObjectProducers/python/tqafLayer1_full_cff.py
+++ TopQuarkAnalysis/TopObjectProducers/python/tqafLayer1_full_cff.py
@@ -10,11 +10,9 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.patLayer0_cff import *
 
 ## define the tqafLayer0 input
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_Jets_cff  import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_Muons_cff import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_Elecs_cff import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_Taus_cff  import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_METs_cff  import *
+from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_objectCleaning_cff import *
+from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_mcMatching_cff import *
+
 
 #-----------------------------------------------------------------
 # build tqafLayer1 objects (jets, muons, electrons, mets, taus)
@@ -22,15 +20,12 @@ from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer0_METs_cff  import *
 from PhysicsTools.PatAlgos.patLayer1_cff import *
 
 ## define the tqafLayer1 input
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_Jets_cff  import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_Muons_cff import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_Elecs_cff import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_Taus_cff  import *
-from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_METs_cff  import *
+from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_objectProduction_cff import *
+from TopQuarkAnalysis.TopObjectProducers.full.tqafLayer1_objectSelection_cff import *
 
 ## std sequence for tqafLayer1 production
 tqafLayer1 = cms.Sequence(patLayer0*patLayer1)
 
 ## std sequence for tqafLayer1 production (w/o trigger)
-tqafLayer1_withoutTrigMatch = cms.Sequence(patLayer0_withoutTrigMatch*patLayer1)
+#tqafLayer1_withoutTrigMatch = cms.Sequence(patLayer0_withoutTrigMatch*patLayer1)
 
