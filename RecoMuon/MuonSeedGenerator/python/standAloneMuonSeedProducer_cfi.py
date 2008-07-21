00e3f3f4c8573f8d23e91b8628ef9418ba44f2d0
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-07-21 16:14:45 +0000

    update the config files with 3.8T param

--- RecoMuon/MuonSeedGenerator/python/standAloneMuonSeedProducer_cfi.py
+++ RecoMuon/MuonSeedGenerator/python/standAloneMuonSeedProducer_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 # The new seed generator to be used in 2-X-X is MuonSeedProducer
 # to Uncomment for using the new seed generator
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
-from RecoMuon.MuonSeedGenerator.ptSeedParameterization_cfi import *
+from RecoMuon.MuonSeedGenerator.ptSeedParameterization_38T_cfi import *
 MuonSeed = cms.EDProducer("MuonSeedProducer",
     ptSeedParameterization,
     MuonServiceProxy,
