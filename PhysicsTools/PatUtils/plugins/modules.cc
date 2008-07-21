7851b2d7806ec06871360a8267d0cab700fa4ea9
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2008-07-21 13:22:57 +0000

    adding event selectors

--- PhysicsTools/PatUtils/plugins/modules.cc
+++ PhysicsTools/PatUtils/plugins/modules.cc
@@ -5,25 +5,36 @@
 
 DEFINE_SEAL_MODULE();
 
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+
+
 #include "PhysicsTools/UtilAlgos/interface/EventSelector.h"
+#include "PhysicsTools/UtilAlgos/interface/StringCutEventSelector.h"
 
-/*
-#include "Workspace/ConfigurableAnalysis/plugins/JetMetDphiEventSelector.h"
-#include "Workspace/ConfigurableAnalysis/plugins/JetJetDphiEventSelector.h"
-#include "Workspace/ConfigurableAnalysis/plugins/CandidateEventSelector.h"
-#include "Workspace/ConfigurableAnalysis/plugins/JetEventSelector.h"
-#include "Workspace/ConfigurableAnalysis/plugins/METEventSelector.h"
-#include "Workspace/ConfigurableAnalysis/plugins/MuonEventSelector.h"
+//#include "Workspace/ConfigurableAnalysis/plugins/JetMetDphiEventSelector.h"
+//#include "Workspace/ConfigurableAnalysis/plugins/JetJetDphiEventSelector.h"
+//#include "Workspace/ConfigurableAnalysis/plugins/CandidateEventSelector.h"
+typedef StringCutEventSelector<pat::Jet> JetEventSelector;
+typedef StringCutsEventSelector<pat::Jet> JetSEventSelector;
+typedef StringCutsEventSelector<pat::Jet,false> JetSEventVetoSelector;
+typedef StringCutEventSelector<pat::Muon> MuonEventSelector;
+typedef StringCutsEventSelector<pat::Muon> MuonSEventSelector;
+typedef StringCutEventSelector<pat::MET> METEventSelector;
+typedef StringCutsEventSelector<pat::MET> METSEventSelector;
 
-DEFINE_EDM_PLUGIN(EventSelectorFactory, JetMetDphiEventSelector, "JetMetDphiEventSelector");
-DEFINE_EDM_PLUGIN(EventSelectorFactory, JetJetDphiEventSelector, "JetJetDphiEventSelector");
+//DEFINE_EDM_PLUGIN(EventSelectorFactory, JetMetDphiEventSelector, "JetMetDphiEventSelector");
+//DEFINE_EDM_PLUGIN(EventSelectorFactory, JetJetDphiEventSelector, "JetJetDphiEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, JetEventSelector, "JetEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, JetSEventSelector, "JetSEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, METEventSelector, "METEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, METSEventSelector, "METSEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, MuonEventSelector, "MuonEventSelector");
 DEFINE_EDM_PLUGIN(EventSelectorFactory, MuonSEventSelector, "MuonSEventSelector");
-*/
 
 #include "PhysicsTools/UtilAlgos/interface/CachingVariable.h"
 
@@ -35,12 +46,6 @@ namespace configurableAnalysis{
   char Tau[]="pat::Tau";
   char Photon[]="pat::Photon";
 }
-#include "DataFormats/PatCandidates/interface/Jet.h"
-#include "DataFormats/PatCandidates/interface/MET.h"
-#include "DataFormats/PatCandidates/interface/Muon.h"
-#include "DataFormats/PatCandidates/interface/Tau.h"
-#include "DataFormats/PatCandidates/interface/Photon.h"
-#include "DataFormats/PatCandidates/interface/Electron.h"
 
 typedef ExpressionVariable<pat::Jet,configurableAnalysis::Jet> JetExpressionVariable;
 typedef ExpressionVariable<pat::MET,configurableAnalysis::MET> METExpressionVariable;
