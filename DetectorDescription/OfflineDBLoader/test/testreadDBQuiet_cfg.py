2ce6b02f847f8c8b7f7265e20535192235a4efd3
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2008-07-22 21:11:35 +0000

    clean up and include more options/parameters from examples of PoolDBESSource.

--- DetectorDescription/OfflineDBLoader/test/testreadDBQuiet_cfg.py
+++ DetectorDescription/OfflineDBLoader/test/testreadDBQuiet_cfg.py
@@ -1,24 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("DBGeometryTest")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
         )
 process.source = cms.Source("EmptySource")
 
 process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-                                          loadAll = cms.bool(True),
-                                          toGet = cms.VPSet(cms.PSet(
-            record = cms.string('IdealGeometryRecord'),
-                    tag = cms.string('IdealGeometry01')
-                )),
-                                          DBParameters = cms.PSet(
-            messageLevel = cms.untracked.int32(0),
-#                    authenticationPath = cms.untracked.string('.')
-                ),
-#                                          catalog = cms.untracked.string('file:PoolFileCatalog.xml'),
-                                          timetype = cms.string('runnumber'),
-                                          connect = cms.string('sqlite_file:testIdeal.db')
+                                         process.CondDBSetup,
+                                         loadAll = cms.bool(True),
+                                         toGet = cms.VPSet(cms.PSet(
+                                            record = cms.string('IdealGeometryRecord'),
+                                            tag = cms.string('IdealGeometry01')
+                                         )),
+                                         DBParameters = cms.PSet(
+                                            messageLevel = cms.untracked.int32(9),
+                                            authenticationPath = cms.untracked.string('.')
+                                         ),
+                                         catalog = cms.untracked.string('file:PoolFileCatalog.xml'),
+                                         timetype = cms.string('runnumber'),
+                                         connect = cms.string('sqlite_file:testIdeal.db')
                                       )
 
 process.myprint = cms.OutputModule("AsciiOutputModule")
