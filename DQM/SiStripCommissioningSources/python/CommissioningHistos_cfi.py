685d1849d6b4125f107faba86fa47545b5bbc3fa
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-07-22 16:22:28 +0000

    *** empty log message ***

--- DQM/SiStripCommissioningSources/python/CommissioningHistos_cfi.py
+++ DQM/SiStripCommissioningSources/python/CommissioningHistos_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 CommissioningHistos = cms.EDAnalyzer("SiStripCommissioningSource",
     InputModuleLabel        = cms.string('FedChannelDigis'),
     SummaryInputModuleLabel = cms.string('FedChannelDigis'),
-    HistoUpdateFreq         = cms.untracked.int32(10)
+    HistoUpdateFreq         = cms.untracked.int32(10),
     RootFileName            = cms.untracked.string('SiStripCommissioningSource'),
-    CommissioningTask       = cms.untracked.string('UNDEFINED'),
+    CommissioningTask       = cms.untracked.string('UNDEFINED')
 )
 
 
