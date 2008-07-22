4c0a4244807ca253030667a309e9c96b70c1e3af
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-07-22 14:19:55 +0000

    include Configuration.StandardSequences.Geometry_cff

--- Validation/RecoEgamma/test/PhotonValidator_cfg.py
+++ Validation/RecoEgamma/test/PhotonValidator_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("TestPhotonValidator")
 
 process.load("RecoEcal.EgammaClusterProducers.geometryForClustering_cff")
 process.load("Configuration.StandardSequences.MagneticField_40T_cff")
-process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
+process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
 process.load("RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi")
 process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
@@ -30,38 +30,25 @@ process.maxEvents = cms.untracked.PSet(
 
 
 from Validation.RecoEgamma.photonValidator_cfi import *
-photonValidation.OutputFileName = 'PhotonValidationRelVal210_pre6_3.8T.root'
+photonValidation.OutputFileName = 'PhotonValidationRelVal210_pre9_SingleGammaPt35.root'
 
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    #'file:/data/test/testIso/CMSSW_2_1_0_pre6/src/Configuration/Examples/data/reRecoSingleGammaPt35WithIsol.root')
+ 
+## Official RelVal 210pre9 Single Photon pt=10
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt10-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt10/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/4862131C-6D57-DD11-847F-000423D996C8.root',
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt10-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt10/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/E421F150-6D57-DD11-ABEC-000423DD2F34.root')
 
+## Official RelVal 210pre9 Single Photon pt=35
+'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt35-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt35/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/60AF541B-6D57-DD11-9EB6-001617C3B77C.root',
+'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt35-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt35/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/BE093BF5-6C57-DD11-9258-001617C3B5F4.root')
 
-
- ## Official RelVal 210pre6 Single Photon pt=35 GeV 4T   
-  #                         'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt35-1213920853-IDEAL_V2-2nd/0000/28C3DDA9-B03E-DD11-AAA9-000423D9A212.root',
-  #                          'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt35-1213920853-IDEAL_V2-2nd/0000/34A839BD-AF3E-DD11-ADFC-000423D99E46.root',
-  #                          'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt35-1213920853-IDEAL_V2-2nd/0000/4ECD9899-B23E-DD11-90A9-001617C3B6E2.root',
-  #                          'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt35-1213920853-IDEAL_V2-2nd/0000/CC46175D-B03E-DD11-8EE1-000423D9890C.root')
-
-## Official RelVal 210pre6 Single Photon pt=35 GeV 3.8 T
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0004/6AF10932-F340-DD11-9BB7-000423D94700.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0004/AAFFE6D9-0141-DD11-9019-001617DBCF6A.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0005/245E3E90-0741-DD11-BB73-000423D99660.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0005/2A7A33B3-1841-DD11-8002-000423D6B42C.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0005/4CD97038-0741-DD11-8FD7-001D09F23174.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0005/9058D215-0541-DD11-BA20-000423D986C4.root',
-'rfio:/castor/cern.ch/cms/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213987236-IDEAL_V2-2nd/0005/E2B6920E-0541-DD11-9144-000423D99A8E.root' )
-
-
-## Official RelVal 210pre6 Single Photon pt=1000 GeV 4T   
- #'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt1000-1213920853-IDEAL_V2-2nd/0000/10FE12B7-AF3E-DD11-9B4C-000423D6CA6E.root',
- #'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt1000-1213920853-IDEAL_V2-2nd/0000/4A9C5FF7-AE3E-DD11-9E71-000423D992A4.root',
- #'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt1000-1213920853-IDEAL_V2-2nd/0000/5294E693-AE3E-DD11-9E19-001617C3B710.root',
- #'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt1000-1213920853-IDEAL_V2-2nd/0000/9EFA96BC-AF3E-DD11-88B1-001617E30D06.root',
- #'rfio:/castor/cern.ch/cms/store/relval/2008/6/20/RelVal-RelValSingleGammaPt1000-1213920853-IDEAL_V2-2nd/0000/A6FE0D26-AF3E-DD11-A4DE-001617C3B710.root')
-
+## Official RelVal 210pre9 Single Photon pt=1000
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt1000-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt1000/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/140FD6C7-6C57-DD11-8F00-000423D94534.root',
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt1000-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt1000/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/3AC9638C-6C57-DD11-AED4-001617C3B70E.root',
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt1000-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt1000/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/7C2E07C4-6C57-DD11-AB1F-000423D98B5C.root',
+#'rfio:/castor/cern.ch/cms/store/relval/2008/7/21/RelVal-RelValSingleGammaPt1000-1216579481-IDEAL_V5-2nd/RelValSingleGammaPt1000/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre9-RelVal-1216579481-IDEAL_V5-2nd-unmerged/0000/EE1DBDFC-6C57-DD11-8FF0-000423D6B358.root')
 
 
 )
