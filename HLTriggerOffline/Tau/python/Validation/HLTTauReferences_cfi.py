2115e978a2ac4a1aaeb633ac57fcd8da5c40a6f5
Chi Nhan Nguyen <sha1-d7c3810b3b8595346f468e2d6413da596572dbe0@cern.ch>
2008-07-23 15:13:43 +0000

    Typo fix.

--- HLTriggerOffline/Tau/python/Validation/HLTTauReferences_cfi.py
+++ HLTriggerOffline/Tau/python/Validation/HLTTauReferences_cfi.py
@@ -3,10 +3,10 @@ import FWCore.ParameterSet.Config as cms
 TauMCProducer  = cms.EDProducer("HLTTauMCProducer",
                               GenParticles  = cms.untracked.InputTag("source"),
                               ptMinTau      = cms.untracked.double(15),
-                              ptMinMuon     = cms.untracked.double(14),
-                              ptMinElectron = cms.untracked.double(12),
+                              ptMinMuon     = cms.untracked.double(2),
+                              ptMinElectron = cms.untracked.double(5),
                               BosonID       = cms.untracked.int32(23),
-                              EtMax         = cms.untracked.double(2.5)
+                              EtaMax        = cms.untracked.double(2.5)
 )
 
 HLTTauRef = cms.Sequence(TauMCProducer)
