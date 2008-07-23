36855a6aff9fa79458d3b699302695af3943c177
Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch>
2008-07-23 18:17:34 +0000

    choose from a list which for which of the ECAL feds raw data are produced

--- EventFilter/EcalDigiToRaw/python/ecalDigiToRaw_cfi.py
+++ EventFilter/EcalDigiToRaw/python/ecalDigiToRaw_cfi.py
@@ -1,3 +1,8 @@
+# The following comments couldn't be translated into the new config version:
+
+#EB-
+#EB+
+#EE+
 import FWCore.ParameterSet.Config as cms
 
 ecaldigitorawzerosup = cms.EDFilter("EcalDigiToRaw",
@@ -11,6 +16,21 @@ ecaldigitorawzerosup = cms.EDFilter("EcalDigiToRaw",
     WriteSRFlags = cms.untracked.bool(True),
     WriteTowerBlock = cms.untracked.bool(True),
     labelEBSRFlags = cms.InputTag("simEcalDigis","ebSrFlags"),
+    listDCCId = cms.untracked.vint32(1, 2, 3, 4, 5,
+        6, 7, 8, 9,
+
+        10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19,
+        20, 21, 22, 23, 24,
+        25, 26, 27,
+
+        28, 29, 30, 
+        31, 32, 33, 34, 35, 
+        36, 37, 38, 39, 40, 
+        41, 42, 43, 44, 45, 
+
+        46, 47, 48, 49, 50, 
+        51, 52, 53, 54),
     WriteTCCBlock = cms.untracked.bool(True),
     DoBarrel = cms.untracked.bool(True)
 )
