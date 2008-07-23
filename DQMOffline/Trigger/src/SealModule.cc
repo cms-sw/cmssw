9229fb2916f1dca220b159022efdd65fb9fdeb43
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2008-07-23 19:43:43 +0000

    Integrated L1 Module

--- DQMOffline/Trigger/src/SealModule.cc
+++ DQMOffline/Trigger/src/SealModule.cc
@@ -1,4 +1,4 @@
-// $Id: SealModule.cc,v 1.3 2008/07/17 18:38:51 bachtis Exp $
+// $Id: SealModule.cc,v 1.4 2008/07/17 20:27:06 bachtis Exp $
 
 #include "DQMOffline/Trigger/interface/FourVectorHLTOffline.h"
 #include "DQMOffline/Trigger/interface/EgammaHLTOffline.h"
@@ -7,7 +7,9 @@
 #include "DQMOffline/Trigger/interface/HLTTauCaloDQMOfflineSource.h"
 #include "DQMOffline/Trigger/interface/HLTTauTrkDQMOfflineSource.h"
 #include "DQMOffline/Trigger/interface/HLTTauElDQMOfflineSource.h"
+#include "DQMOffline/Trigger/interface/HLTTauL1DQMOfflineSource.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 DEFINE_FWK_MODULE(FourVectorHLTOffline);
 DEFINE_ANOTHER_FWK_MODULE(EgammaHLTOffline);
 DEFINE_ANOTHER_FWK_MODULE(EgHLTOfflineClient);
@@ -15,6 +17,8 @@ DEFINE_ANOTHER_FWK_MODULE(HLTTauRefProducer);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauCaloDQMOfflineSource);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauTrkDQMOfflineSource);
 DEFINE_ANOTHER_FWK_MODULE(HLTTauElDQMOfflineSource);
+DEFINE_ANOTHER_FWK_MODULE(HLTTauL1DQMOfflineSource);
+
 
 
 
