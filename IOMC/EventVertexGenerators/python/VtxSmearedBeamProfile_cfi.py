75a10e1d16c47d3c90eccd670e5f5915447f9e14
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-07-23 15:42:26 +0000

    Fix the beam profile generator configuration common_beam_profile_parameters was not defined in the fragment

--- IOMC/EventVertexGenerators/python/VtxSmearedBeamProfile_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedBeamProfile_cfi.py
@@ -1,9 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
+
+# default definition of common parameters
+
+common_beam_direction_parameters = cms.PSet(
+    BeamMeanY = cms.untracked.double(0.0),
+    BeamMeanX = cms.untracked.double(0.0),
+    BeamPosition = cms.untracked.double(0.),
+    MinEta = cms.untracked.double(0.),
+    MaxEta = cms.untracked.double(1.5),
+    MinPhi = cms.untracked.double(-3.14159265358979323846),
+    MaxPhi = cms.untracked.double(3.14159265358979323846)
+)
+
 #
-# this module takes inpuut in the units of *cm* and *radian*!!!
+# this module takes input in the units of *cm* and *radian*!!!
 #
+
 VtxSmeared = cms.EDFilter("BeamProfileVtxGenerator",
     common_beam_direction_parameters,
     VtxSmearedCommon,
@@ -15,7 +29,8 @@ VtxSmeared = cms.EDFilter("BeamProfileVtxGenerator",
     BinY = cms.untracked.int32(50),
     BinX = cms.untracked.int32(50),
     File = cms.untracked.string('beam.profile'),
-    UseFile = cms.untracked.bool(False)
+    UseFile = cms.untracked.bool(False),
+    TimeOffset = cms.double(0.)                      
 )
 
 
