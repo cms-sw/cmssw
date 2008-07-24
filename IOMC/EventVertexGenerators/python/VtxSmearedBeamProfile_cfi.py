4bff60eacfda248f263d88d88be4b655bc881c6e
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-07-24 15:10:10 +0000

    delete doubly defined parameters

--- IOMC/EventVertexGenerators/python/VtxSmearedBeamProfile_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedBeamProfile_cfi.py
@@ -5,8 +5,6 @@ from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 # default definition of common parameters
 
 common_beam_direction_parameters = cms.PSet(
-    BeamMeanY = cms.untracked.double(0.0),
-    BeamMeanX = cms.untracked.double(0.0),
     BeamPosition = cms.untracked.double(0.),
     MinEta = cms.untracked.double(0.),
     MaxEta = cms.untracked.double(1.5),
@@ -21,13 +19,13 @@ common_beam_direction_parameters = cms.PSet(
 VtxSmeared = cms.EDFilter("BeamProfileVtxGenerator",
     common_beam_direction_parameters,
     VtxSmearedCommon,
+    BeamMeanX = cms.untracked.double(0.0),
+    BeamMeanY = cms.untracked.double(0.0),
     BeamSigmaX = cms.untracked.double(0.0001),
     BeamSigmaY = cms.untracked.double(0.0001),
-    BeamMeanY = cms.untracked.double(0.0),
-    BeamMeanX = cms.untracked.double(0.0),
     GaussianProfile = cms.untracked.bool(True),
-    BinY = cms.untracked.int32(50),
     BinX = cms.untracked.int32(50),
+    BinY = cms.untracked.int32(50),
     File = cms.untracked.string('beam.profile'),
     UseFile = cms.untracked.bool(False),
     TimeOffset = cms.double(0.)                      
