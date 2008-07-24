66d2406cfb3980838ac947a2934ee3c915e6085a
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-07-24 09:40:04 +0000

    change the default field from 4.0T with old mapping to 3.8T and new mapping

--- Configuration/StandardSequences/python/MagneticField_cff.py
+++ Configuration/StandardSequences/python/MagneticField_cff.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 #
 # Master configuration for the magnetic field
 # old mapping with 4T
-from Configuration.StandardSequences.MagneticField_40T_851_cff import *
+from Configuration.StandardSequences.MagneticField_38T_cff import *
