3a8a5bc5686259397bc6ab3c651c5f75bb75d9d3
Shan-Huei Chuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-07-25 20:57:09 +0000

    removing past python stuff

---
+++ DQM/SiPixelMonitorTrack/python/SiPixelMonitorTrack_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+SiPixelMonitorTrackResiduals = cms.EDFilter("SiPixelMonitorTrackResiduals",
+    src = cms.InputTag("siPixelTrackResiduals"),
+    debug = cms.bool(True),
+    saveFile = cms.bool(True),
+    outputFilename = cms.string('Pixel_DQM_TrackResidual.root'),
+    TrackCandidateProducer = cms.string('newTrackCandidateMaker'),
+    TrackCandidateLabel = cms.string(''),
+    TTRHBuilder = cms.string('WithTrackAngle')
+    Fitter = cms.string('FittingSmootherWithOutlierRejection'),
+)
