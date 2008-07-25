e911a698206e0eb76e2fd57fecd557208c08e3ff
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-07-25 15:23:16 +0000

    revert sequence name to seqALCARECOHcalCalIsoTrkNoHLT

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalIsoTrkNoHLT_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalIsoTrkNoHLT_cff.py
@@ -7,6 +7,6 @@ import FWCore.ParameterSet.Config as cms
 from Calibration.HcalAlCaRecoProducers.alcaisotrk_cfi import *
 IsoProd.CheckHLTMatch = False
 
-seqALCARECOHcalCalIsoTrk = cms.Sequence(IsoProd)
+seqALCARECOHcalCalIsoTrkNoHLT = cms.Sequence(IsoProd)
 
 
