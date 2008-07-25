3ba6f6b9d63eeeb2cd7bc7eefd9c9eb76a5f9fdd
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2008-07-25 13:18:38 +0000

    Python tools for working with jets. Need at least FWCore/ParameterSet V08-09-10 to run, and something more to get the full set of features.

---
+++ PhysicsTools/PatAlgos/python/tools/circuitry.py
@@ -0,0 +1,92 @@
+def plotSequences(seq,filename):
+    from sys import stderr, argv
+    from os import popen
+    from os.path import basename
+    from re import sub;
+    import FWCore.ParameterSet.Config as cms
+    stderr.write("Writing plot to %s\n" % (filename,))
+    dot = popen("dot -Tpng > %s" % (filename,), "w")
+    dot.write("digraph G { \n rankdir=\"LR\" \n")
+    class visitor(object):
+        def __init__(self,seq,dot):
+            self._dot = dot
+            self._stack = []
+            self._seq = seq.label()
+            self._dot.write( "%s [  shape=rect style=filled fillcolor=%s label=\"%s\" ]" % (self._seq,'orange',self._seq) + "\n" )
+        def seq(self, seq):
+            self._stack.append(self._seq)
+            self._seq = seq.label()
+        def enter(self,v):
+            if isinstance(v, cms.Sequence):
+                self._dot.write( "%s [  shape=rect style=filled fillcolor=%s label=\"%s\" ]" % (v.label(),'orange',v.label()) + "\n" )
+                self.dep(v)
+                self.seq(v)
+            if isinstance(v, (cms.EDProducer, cms.EDFilter, cms.EDAnalyzer)):
+                self._dot.write( "%s [  shape=rect style=filled fillcolor=%s label=\"%s\" ]" % (v.label(),'green',v.label()) + "\n" ) 
+                self.dep(v)
+        def leave(self,v):
+            if isinstance(v, cms.Sequence):
+                self._seq = self._stack.pop()
+        def dep(self,v):
+            self._dot.write("%s -> %s" %(v.label(), self._seq) +"\n")
+    seq.visit(visitor(seq,dot))
+    dot.write("}\n")
+    dot.close()
+
+def plotModuleInputs(seq,filename,printOuter=True,printLinkNames=True):
+    from sys import stderr, argv
+    from os import popen
+    from os.path import basename
+    from re import sub;
+    import FWCore.ParameterSet.Config as cms
+    stderr.write("Writing plot to %s\n" % (filename,))
+    dot = popen("dot -Tpng > %s" % (filename,), "w")
+    #dot = open("%s.dot" % (filename,), "w")
+    dot.write("digraph G { \n rankdir=\"LR\" \n")
+    deps = {}; alls = {}
+    modules = []
+    class visitor(object):
+        def enter(self,v):
+            if isinstance(v, (cms.EDProducer, cms.EDFilter, cms.EDAnalyzer)):
+                modules.append(v)
+        def leave(self,v):
+            pass
+    def greptags(ps,basename=""):
+        ret = []
+        for pn, pv in ps.parameters_().items():
+            type = pv.configTypeName()
+            if type == 'InputTag'    : ret.append( (basename+pn, pv.configValue()) )
+            elif type == 'VInputTag' : ret += [ ("%s%s[%d]"%(basename,pn,i+1),v.configValue()) for i,v in enumerate(pv.value()) ]
+            elif type == 'PSet'      : ret += greptags(pv, basename+pn+'.')
+            elif type == 'VPset'     : 
+                for r1 in [greptags(pvi, basename+pn+'.') for pvi in pv.value()]: ret += r1
+        return ret
+    def escapeParValue(name): return sub(r":.*","", name)
+    seq.visit(visitor())
+    for m in modules:
+        dot.write( "%s [  shape=rect style=filled fillcolor=%s label=\"%s\" ]" % (m.label(),'green',m.label()) + "\n")
+        tags = greptags(m)
+        #stderr.write("Tags for %s: %s\n" % (m.label(), tags))
+        deps[m.label()] = tags;
+        if not alls.has_key(m.label()): alls[m.label()]=True
+        for (tn,tv) in tags:
+            tve = escapeParValue(tv)
+            if not alls.has_key(tve): alls[tve]=True
+    names = deps.keys();
+    if printOuter: names = alls.keys()
+    done = {}
+    for n in names:
+        ne = escapeParValue(n)
+        if not deps.has_key(ne):
+            dot.write( "%s [  shape=rect style=filled fillcolor=%s label=\"%s\" ]" % (ne,'yellow',ne) + "\n")
+        else:
+            for tn,tv in deps[ne]:
+                tve = escapeParValue(tv)
+                if printOuter or deps.has_key(tve):
+                    style = ""
+                    if printLinkNames: style = " [label=\"%s\" ]" %(tn,)
+                    dot.write(  "%s -> %s%s\n"%(tve,ne,style))
+    dot.write("}\n")
+    dot.close()
+
+
