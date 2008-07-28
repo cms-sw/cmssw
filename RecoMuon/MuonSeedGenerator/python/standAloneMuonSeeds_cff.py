463d73e3dc008ccdf47e412d6ee322c61764bfe8
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-07-28 15:27:15 +0000

    revert to old seed, as global muon efficiency is still better (especially at high pt)

--- RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cff.py
+++ RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cff.py
@@ -1,13 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-# Magnetic Field
 # Geometries
 from Geometry.CMSCommonData.cmsIdealGeometryXML_cff import *
 from Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
+
 # Old stand alone muon seed producer used priod to 2-X-X
-#include "RecoMuon/MuonSeedGenerator/data/standAloneMuonSeeds.cfi"
+from RecoMuon.MuonSeedGenerator.standAloneMuonSeeds_cfi import *
+
 # New standalone muon producer to be used in 2-X-X
-from RecoMuon.MuonSeedGenerator.standAloneMuonSeedProducer_cfi import *
+#from RecoMuon.MuonSeedGenerator.standAloneMuonSeedProducer_cfi import *
 
 
