9afa1d727161008946f5572b1c87bddd3ff69408
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2008-07-28 22:47:47 +0000

    Modified to include the jet corrections examples.

--- JetMETCorrections/MCJet/plugins/SealModule.cc
+++ JetMETCorrections/MCJet/plugins/SealModule.cc
@@ -2,8 +2,14 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "MCTruthTreeProducer.h"
+#include "PFJetsCorExample.h"
+#include "CaloJetsCorExample.h"
 
 using cms::MCTruthTreeProducer;
+using cms::PFJetsCorExample;
+using cms::CaloJetsCorExample;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MCTruthTreeProducer);
+DEFINE_ANOTHER_FWK_MODULE(PFJetsCorExample);
+DEFINE_ANOTHER_FWK_MODULE(CaloJetsCorExample);
