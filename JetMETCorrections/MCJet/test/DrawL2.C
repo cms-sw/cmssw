91d6b1e850c4f9c1f55c9182f45fdae2050d7a9c
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2008-07-28 17:36:55 +0000

    Removing obsolete files

--- JetMETCorrections/MCJet/test/DrawL2.C
+++ JetMETCorrections/MCJet/test/DrawL2.C
@@ -1,61 +1,50 @@
-void DrawL2(const char algorithm[100],int etabin)
+#include "Settings.h"
+void DrawL2(int etabin)
 {
-  gROOT->Reset();
   gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0000);
   gStyle->SetOptFit(000); 
   gStyle->SetPalette(1);
-  const int NETA = 82;
-  const double eta_boundaries[NETA+1]={-5.191,-4.889,-4.716,-4.538,-4.363,-4.191,-4.013,-3.839,-3.664, 
-			             -3.489,-3.314,-3.139,-2.964,-2.853,-2.650,-2.500,-2.322,-2.172,
-                                     -2.043,-1.930,-1.830,-1.740,-1.653,-1.566,-1.479,-1.392,-1.305,
-			             -1.218,-1.131,-1.044,-0.957,-0.879,-0.783,-0.696,-0.609,-0.522,
-			             -0.435,-0.348,-0.261,-0.174,-0.087,0.000,0.087,0.174,0.261,0.348,
-                                      0.435,0.522,0.609,0.696,0.783,0.879,0.957,1.044,1.131,1.218,1.305,
-                                      1.392,1.479,1.566,1.653,1.740,1.830,1.930,2.043,2.172,2.322,2.500,
-                                      2.650,2.853,2.964,3.139,3.314,3.489,3.664,3.839,4.013,4.191,4.363,4.538,
-                                      4.716,4.889,5.191};
   if (etabin<0 || etabin>=NETA)
     {
       cout<<"Eta bin must be >=0 and <"<<NETA<<endl;
       break;
     }
-  TGraphErrors *g_EtaResponse;
+  TPaveText *pave = new TPaveText(0.3,0.7,0.5,0.85,"NDC");  
+  pave->AddText(Version);
+  pave->AddText(Algorithm);
+  pave->SetLineColor(0);
+  pave->SetBorderSize(0);
+  pave->SetFillColor(0);
+  pave->SetBorderSize(0);
+  TGraphErrors *g_EtaCorrection;
   TGraph *g_L2Correction;
   TCanvas *c_Resp;
   TCanvas *c_L2Cor;
   TF1 *L2Fit;
-  TF1 *RespFit;
-  
+  TF1 *CorFit;
+  TFile *rel_f;
   char filename[100],name[100];
-  sprintf(name,"L2Graphs_%s.root",algorithm);
-  TFile *rel_f = new TFile(name,"r");
+  rel_f = new TFile(L2OutputROOTFilename,"r");
   if (!rel_f->IsOpen()) break;
-  /////////////////////////////// Response /////////////////////////
-   
-  sprintf(name,"Response");
-  c_Resp = new TCanvas(name,name,900,700);
-  sprintf(name,"Response_EtaBin%d",etabin);
-  g_EtaResponse = (TGraphErrors*)rel_f->Get(name);
-  sprintf(name,"Response%d",etabin);      
-  RespFit = (TF1*)g_EtaResponse->GetFunction(name);
-  if (RespFit->GetXmax()>200) 
+  /////////////////////////////// Correction /////////////////////////
+  sprintf(name,"EtaCorrection");
+  c_Cor = new TCanvas(name,name,900,700);
+  sprintf(name,"Correction_EtaBin%d",etabin);
+  g_EtaCorrection = (TGraphErrors*)rel_f->Get(name);
+  sprintf(name,"Correction%d",etabin);      
+  CorFit = (TF1*)g_EtaCorrection->GetFunction(name); 
+  if (CorFit->GetXmax()>200) 
     gPad->SetLogx();
-  g_EtaResponse->SetMarkerStyle(21);
-  g_EtaResponse->SetMarkerColor(1);
-  g_EtaResponse->SetLineColor(1);
-  g_EtaResponse->SetMinimum(0.);
-  g_EtaResponse->SetMaximum(1.2);
-  g_EtaResponse->GetXaxis()->SetTitle("p_{T}^{gen} (GeV)");
-  g_EtaResponse->GetYaxis()->SetTitle("Response");  
-  RespFit->SetLineColor(2);  
-  g_EtaResponse->Draw("AP");
+  CorFit->SetLineColor(2);
+  g_EtaCorrection->GetXaxis()->SetTitle("Uncorrected jet p_{T} (GeV)");
+  g_EtaCorrection->GetYaxis()->SetTitle("Absolute Correction"); 
   sprintf(name,"%1.3f<#eta<%1.3f",eta_boundaries[etabin],eta_boundaries[etabin+1]);
-  g_EtaResponse->SetTitle(name);
-  TPaveLabel *pave = new TPaveLabel(0.15,0.15,0.35,0.25,algorithm,"NDC");
-  pave->SetFillColor(0);
+  g_EtaCorrection->SetTitle(name); 
+  g_EtaCorrection->SetMarkerStyle(20);
+  g_EtaCorrection->Draw("AP"); 
   pave->Draw();
-    
+  
   /////////////////////////////// L2 correction ///////////////////////// 
   sprintf(name,"L2Correction");
   c_L2Cor = new TCanvas(name,name,900,700);
@@ -65,19 +54,15 @@ void DrawL2(const char algorithm[100],int etabin)
   L2Fit = (TF1*)g_L2Correction->GetFunction(name);
   if (L2Fit->GetXmax()>200) 
     gPad->SetLogx(); 
-  g_L2Correction->SetMarkerStyle(21);
-  g_L2Correction->SetMarkerColor(1);
-  g_L2Correction->SetLineColor(1);
   g_L2Correction->SetMinimum(0.3);
-  g_L2Correction->SetMaximum(1.1);
-  g_L2Correction->GetXaxis()->SetTitle("p_{T} (GeV)");
-  g_L2Correction->GetYaxis()->SetTitle("L2 Correction"); 
-  L2Fit->SetLineColor(2); 
-  g_L2Correction->Draw("AP");
+  g_L2Correction->SetMaximum(1.4);
+  g_L2Correction->GetXaxis()->SetTitle("Uncorrected jet p_{T} (GeV)");
+  g_L2Correction->GetYaxis()->SetTitle("Relative Correction");  
   sprintf(name,"%1.3f<#eta<%1.3f",eta_boundaries[etabin],eta_boundaries[etabin+1]);
-  g_L2Correction->SetTitle(name);
-  TPaveLabel *pave = new TPaveLabel(0.15,0.15,0.35,0.25,algorithm,"NDC");
-  pave->SetFillColor(0);
+  g_L2Correction->SetTitle(name); 
+  g_L2Correction->Draw("AP");
+  g_L2Correction->SetMarkerStyle(20);
+  L2Fit->SetLineColor(2);
   pave->Draw();
 }
 
--- JetMETCorrections/MCJet/test/DrawL3Comparison.C
+++ JetMETCorrections/MCJet/test/DrawL3Comparison.C
@@ -1,105 +1,144 @@
 void DrawL3Comparison()
+{  
+  PrintMessage();
+}
+void DrawL3Comparison(char s1[1024])
+{  
+  char filename[1][1024];
+  sprintf(filename[0],"%s",s1);
+  MainProgram(1,filename);
+}
+void DrawL3Comparison(char s1[1024],char s2[1024])
+{ 
+  char filename[2][1024];
+  sprintf(filename[0],"%s",s1);
+  sprintf(filename[1],"%s",s2);
+  MainProgram(2,filename);
+}
+void DrawL3Comparison(char s1[1024],char s2[1024],char s3[1024])
+{ 
+  char filename[3][1024];
+  sprintf(filename[0],"%s",s1);
+  sprintf(filename[1],"%s",s2);
+  sprintf(filename[2],"%s",s3);
+  MainProgram(3,filename);
+}
+void DrawL3Comparison(char s1[1024],char s2[1024],char s3[1024],char s4[1024])
+{ 
+  char filename[4][1024];
+  sprintf(filename[0],"%s",s1);
+  sprintf(filename[1],"%s",s2);
+  sprintf(filename[2],"%s",s3);
+  sprintf(filename[3],"%s",s4);
+  MainProgram(4,filename);
+}
+void DrawL3Comparison(char s1[1024],char s2[1024],char s3[1024],char s4[1024],char s5[1024])
+{ 
+  char filename[5][1024];
+  sprintf(filename[0],"%s",s1);
+  sprintf(filename[1],"%s",s2);
+  sprintf(filename[2],"%s",s3);
+  sprintf(filename[3],"%s",s4);
+  sprintf(filename[4],"%s",s5);
+  MainProgram(5,filename);
+}
+void MainProgram(const int NAlg,char filename[][1024])
 {
-  gROOT->Reset();
   gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0000);
   gStyle->SetOptFit(000); 
-  //gStyle->SetPadGridX(1);
-  //gStyle->SetPadGridY(1);
   gStyle->SetPalette(1);
  
-  char name[100];
-  const int NAlg = 4;
-  const char algorithm[NAlg][100] = {"iterativeCone5","midPointCone5","sisCone5","fastjet4"};
-  //const char algorithm[NAlg][100] = {"iterativeCone7","midPointCone7","sisCone7","fastjet6"};
-  int alg;
+  char name[1024];
+  int alg,color;
   TFile *inf[NAlg];
   
-  TGraphErrors *g_Resp[NAlg];
-  TGraph *g_Cor[NAlg];
-  TF1 *CorFit[NAlg];
-  TF1 *RespFit[NAlg];
+  TGraphErrors *g_Cor[NAlg],*g_Resp[NAlg];
+  TF1 *CorFit[NAlg],*RespFit[NAlg];
   ///////////////////////////////////////////////////////////////
   for(alg=0;alg<NAlg;alg++)
     {
-      sprintf(name,"L3Graphs_%s.root",algorithm[alg]); 
-      inf[alg] = new TFile(name,"r");
-      sprintf(name,"Response_vs_GenPt");      
-      g_Resp[alg] = (TGraphErrors*)inf[alg]->Get(name);
-      sprintf(name,"Response_%s",algorithm[alg]);
-      g_Resp[alg]->SetName(name);
-      sprintf(name,"Correction_vs_CaloPt");      
-      g_Cor[alg] = (TGraph*)inf[alg]->Get(name);
-      sprintf(name,"Correction_%s",algorithm[alg]);
+      inf[alg] = new TFile(filename[alg],"r");
+      g_Cor[alg] = (TGraphErrors*)inf[alg]->Get("Correction_vs_CaloPt");
+      sprintf(name,"Correction_%s",filename[alg]);
       g_Cor[alg]->SetName(name);
-      sprintf(name,"CorFit");
-      CorFit[alg] = (TF1*)g_Cor[alg]->GetFunction(name);
-      sprintf(name,"CorFit_%s",algorithm[alg]);
+      CorFit[alg] = (TF1*)g_Cor[alg]->GetFunction("CorFit");
+      sprintf(name,"CorFit_%s",filename[alg]); 
       CorFit[alg]->SetName(name);
-      sprintf(name,"RespFit");
-      RespFit[alg] = (TF1*)g_Resp[alg]->GetFunction(name);
-      sprintf(name,"RespFit_%s",algorithm[alg]);
+      CorFit[alg]->SetRange(5,5000);
+      g_Resp[alg] = (TGraphErrors*)inf[alg]->Get("Response_vs_RefPt");
+      sprintf(name,"Response_%s",filename[alg]);
+      g_Resp[alg]->SetName(name);
+      RespFit[alg] = (TF1*)g_Resp[alg]->GetFunction("RespFit");
+      sprintf(name,"RespFit_%s",filename[alg]); 
       RespFit[alg]->SetName(name);
+      RespFit[alg]->SetRange(5,5000);
     }
-  ////////////////////// Response ///////////////////////////////////////
-  sprintf(name,"Response");
-  TCanvas *c_Response = new TCanvas(name,name,900,600);
-  c_Response->cd();
-  gPad->SetLogx(); 
-  g_Resp[0]->SetTitle("Jet Response, |#eta|<1.3");
-  g_Resp[0]->GetXaxis()->SetTitle("<p_{T}^{gen}> (GeV)");
-  g_Resp[0]->GetYaxis()->SetTitle("1+#frac{<<#Delta p_{T}>>}{<p_{T}^{gen}>}");
-  g_Resp[0]->SetMaximum(1.);
-  g_Resp[0]->SetMinimum(0.);
-  g_Resp[0]->Draw("AP");
-  TLegend *leg = new TLegend(0.6,0.15,0.9,0.45);
-  for(alg=0;alg<NAlg;alg++)
-    {
-      g_Resp[alg]->SetMarkerStyle(20+alg);
-      g_Resp[alg]->SetMarkerColor(alg+1);
-      g_Resp[alg]->SetLineColor(alg+1);
-      g_Resp[alg]->SetLineStyle(1);
-      RespFit[alg]->SetLineColor(alg+1);
-      RespFit[alg]->SetLineStyle(alg+1);
-      RespFit[alg]->SetLineWidth(2);
-      g_Resp[alg]->Draw("sameP");
-      sprintf(name,"%s measurement",algorithm[alg]);
-      leg->AddEntry(g_Resp[alg],name,"p");
-      sprintf(name,"%s fit",algorithm[alg]);
-      leg->AddEntry(RespFit[alg],name,"l");
-    }
-  leg->SetFillColor(0);
-  leg->Draw();
   ////////////////////// Correction ///////////////////////////////////////
-  sprintf(name,"Correction");
+  sprintf(name,"L3CorrectionComparison");
   TCanvas *c_Correction = new TCanvas(name,name,900,600);
   c_Correction->cd();
   gPad->SetLogx(); 
-  g_Cor[0]->SetTitle("L3 Correction");
-  g_Cor[0]->GetXaxis()->SetTitle("p_{T} (GeV)");
-  g_Cor[0]->GetYaxis()->SetTitle("#frac{p_{T}^{gen}}{p_{T}}");
-  g_Cor[0]->SetMaximum(4.);
-  g_Cor[0]->SetMinimum(1.);
+  g_Cor[0]->SetTitle("");
+  g_Cor[0]->GetXaxis()->SetTitle("Uncorrected jet p_{T} (GeV)");
+  g_Cor[0]->GetYaxis()->SetTitle("Correction Factor");
+  g_Cor[0]->SetMaximum(3.5);
+  g_Cor[0]->SetMinimum(0.8);
   g_Cor[0]->Draw("AP");
-  TLegend *leg = new TLegend(0.6,0.6,0.9,0.9);
+  TLegend *leg = new TLegend(0.5,0.6,0.85,0.85);
   for(alg=0;alg<NAlg;alg++)
     {
+      color = alg+1;
+      if (color==5)
+        color = 7;
       g_Cor[alg]->SetMarkerStyle(20+alg);
-      g_Cor[alg]->SetMarkerColor(1+alg);
-      g_Cor[alg]->SetLineColor(1+alg);
+      g_Cor[alg]->SetMarkerColor(color);
+      g_Cor[alg]->SetLineColor(color);
       g_Cor[alg]->SetLineStyle(1);
-      CorFit[alg]->SetLineColor(1+alg);
-      CorFit[alg]->SetLineStyle(alg+1);
+      CorFit[alg]->SetLineColor(color);
+      CorFit[alg]->SetLineStyle(1);
       CorFit[alg]->SetLineWidth(2); 
       g_Cor[alg]->Draw("sameP");
-      sprintf(name,"%s_measurement",algorithm[alg]);
-      leg->AddEntry(g_Cor[alg],name,"p");
-      sprintf(name,"%s_fit",algorithm[alg]);
-      leg->AddEntry(CorFit[alg],name,"l");
+      leg->AddEntry(g_Cor[alg],filename[alg],"LP");
     }
   leg->SetFillColor(0);
+  leg->SetLineColor(0);
+  leg->Draw();
+  ////////////////////// Response ///////////////////////////////////////
+  sprintf(name,"ResponseComparison");
+  TCanvas *c_Correction = new TCanvas(name,name,900,600);
+  c_Correction->cd();
+  gPad->SetLogx(); 
+  g_Resp[0]->SetTitle("");
+  g_Resp[0]->GetXaxis()->SetTitle("p_{T}^{gen} (GeV)");
+  g_Resp[0]->GetYaxis()->SetTitle("Response");
+  g_Resp[0]->SetMaximum(1);
+  g_Resp[0]->SetMinimum(0);
+  g_Resp[0]->Draw("AP");
+  TLegend *leg = new TLegend(0.5,0.15,0.85,0.4);
+  for(alg=0;alg<NAlg;alg++)
+    {
+      color = alg+1;
+      if (color==5)
+        color = 7;
+      g_Resp[alg]->SetMarkerStyle(20+alg);
+      g_Resp[alg]->SetMarkerColor(color);
+      g_Resp[alg]->SetLineColor(color);
+      g_Resp[alg]->SetLineStyle(1);
+      RespFit[alg]->SetLineColor(color);
+      RespFit[alg]->SetLineStyle(1);
+      RespFit[alg]->SetLineWidth(2); 
+      g_Resp[alg]->Draw("sameP");
+      leg->AddEntry(g_Resp[alg],filename[alg],"LP");
+    }
+  leg->SetFillColor(0);
+  leg->SetLineColor(0);
   leg->Draw();
 }
 
-
+void PrintMessage()
+{
+  cout<<"This ROOT macro can compare up to 5 cases."<<endl;
+  cout<<"Usage: .X DrawL3Comparison.C(\"filename1\",...,\"filename5\")"<<endl;
+}
 
