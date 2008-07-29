9232374536230a9cf4b41a1bba5ed8896f1b45b7
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-07-29 14:19:51 +0000

    added more MEs

--- DQM/L1TMonitor/interface/L1TDTTF.h
+++ DQM/L1TMonitor/interface/L1TDTTF.h
@@ -90,17 +90,51 @@ class L1TDTTF : public edm::EDAnalyzer {
   MonitorElement* dttf_p_eta[3][6][12];
   MonitorElement* dttf_p_qual[3][6][12];
   MonitorElement* dttf_p_q[3][6][12];
-  MonitorElement* dttf_p_pt[3][6][12];  
+  MonitorElement* dttf_p_pt[3][6][12];
+  MonitorElement* dttf_bx[6][12];
+  MonitorElement* dttf_bx_2ndTrack[6][12];
+  MonitorElement* dttf_bx_wheel[6];
+  //MonitorElement* dttf_bx_wheel_2ndTrack[6];
+  MonitorElement* dttf_nTracks_wheel[6];
+  //MonitorElement* dttf_nTracks_wheel_2ndTrack[6];
+  MonitorElement* dttf_nTrksPerEv[3][6][12];
+  MonitorElement* dttf_nTracksPerEvent_wheel[6];
+  //MonitorElement* dttf_nTracksPerEvent_wheel_2ndTrack[6];
+
+  MonitorElement* dttf_n2ndTracks_wheel[6];
+
+  MonitorElement* dttf_nTracksPerEvent_integ;
   MonitorElement* dttf_p_phi_integ;
   MonitorElement* dttf_p_eta_integ;
   MonitorElement* dttf_p_pt_integ;
   MonitorElement* dttf_p_qual_integ;
-  MonitorElement* dttf_phys_phi_integ;
-  MonitorElement* dttf_phys_eta_integ;
-  MonitorElement* dttf_phys_pt_integ;
+  MonitorElement* dttf_p_q_integ;
+  MonitorElement* dttf_bx_integ;
+  //MonitorElement* dttf_nTracks_integ;
+  //MonitorElement* dttf_phys_phi_integ;
+  //MonitorElement* dttf_phys_eta_integ;
+  //MonitorElement* dttf_phys_pt_integ;
   MonitorElement* dttf_p_phi_eta_integ;
-  MonitorElement* dttf_p_phi_eta[6];
+
+  MonitorElement* dttf_p_phi_eta_wheel[6];
+  MonitorElement* dttf_bx_Summary;
   MonitorElement* dttf_occupancySummary;
+  MonitorElement* dttf_occupancySummary_r;
+  MonitorElement* dttf_highQual_Summary;
+  MonitorElement* dttf_2ndTrack_Summary;
+
+  MonitorElement* dttf_p_phi_integ_2ndTrack;
+  MonitorElement* dttf_p_eta_integ_2ndTrack;
+  MonitorElement* dttf_p_pt_integ_2ndTrack;
+  MonitorElement* dttf_p_qual_integ_2ndTrack;
+  MonitorElement* dttf_p_q_integ_2ndTrack;
+  MonitorElement* dttf_bx_integ_2ndTrack;
+  MonitorElement* dttf_nTracks_integ_2ndTrack;
+  //MonitorElement* dttf_nTracksPerEvent_integ_2ndTrack;
+  MonitorElement* dttf_p_phi_eta_integ_2ndTrack;
+  MonitorElement* dttf_bx_Summary_2ndTrack;
+  MonitorElement* dttf_occupancySummary_2ndTrack;
+  MonitorElement* dttf_highQual_Summary_2ndTrack;
 
   MonitorElement* dttpgphmap;
   MonitorElement* dttpgphmap2nd;
@@ -113,8 +147,23 @@ class L1TDTTF : public edm::EDAnalyzer {
   MonitorElement* dttpgthbestmap;
   MonitorElement* dttpgthbestmaph;
 
-  
+
+  //int bx0,bxp1,bxn1;
   int nev_; // Number of events processed
+  bool dttf_track;
+  bool dttf_track_2;
+  int nev_dttf; //Number of events with at least one DTTF track
+  int nev_dttf_track2; //Number of events with at least one DTTF 2nd track
+  int nBx[6][3]; //Number of beam crossings for each wheel
+  int nBx_2ndTrack[6][3];//Number of beam crossings for each wheel (2nd track only)
+  int nOccupancy_integ[6][12];
+  int nOccupancy_integ_2ndTrack[6][12];
+  int nOccupancy_integ_phi_eta[64][144];
+  int nOccupancy_integ_phi_eta_2ndTrack[64][144];
+  int nOccupancy_wheel_phi_eta[64][144][6];
+  int nHighQual[6][12]; //Number of high quality tracks
+  int nHighQual_2ndTrack[6][12];//Number of high quality 2nd tracks
+  int n2ndTrack[6][12]; //Number of 2nd tracks
   std::string outputFile_; //file name for ROOT ouput
   bool verbose_;
   bool monitorDaemon_;
