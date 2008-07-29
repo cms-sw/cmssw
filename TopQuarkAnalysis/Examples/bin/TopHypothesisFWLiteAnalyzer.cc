a209eead0f6249ed526450e0f66e18c9c98e4a86
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-29 00:03:52 +0000

    add FWLite examples

---
+++ TopQuarkAnalysis/Examples/bin/TopHypothesisFWLiteAnalyzer.cc
@@ -0,0 +1,196 @@
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "AnalysisDataFormats/TopObjects/interface/TtSemiEvent.h"
+
+#include "TopQuarkAnalysis/Examples/bin/NiceStyle.cc"
+#include "TopQuarkAnalysis/Examples/interface/RootSystem.h"
+#include "TopQuarkAnalysis/Examples/interface/RootHistograms.h"
+#include "TopQuarkAnalysis/Examples/interface/RootPostScript.h"
+
+
+int main(int argc, char* argv[]) 
+{
+  if( argc<4 ){
+    // -------------------------------------------------  
+    std::cerr << "ERROR:: " 
+	      << "Wrong number of arguments!" << std::endl 
+	      << "        Please specify:" << std::endl
+	      << "        * filepath" << std::endl
+	      << "        * process name" << std::endl
+	      << "        * HypoKey" << std::endl;
+    // -------------------------------------------------  
+    return -1;
+  }
+
+  // parse HypoKey
+  TtSemiEvent::HypoKey hypoKey;
+  if(!strcmp(argv[3], "kWMassMaxSumPt")) hypoKey = TtSemiEvent::kWMassMaxSumPt; else if
+    (!strcmp(argv[3], "kMaxSumPtWMass")) hypoKey = TtSemiEvent::kMaxSumPtWMass; else if
+    (!strcmp(argv[3], "kKinFit"       )) hypoKey = TtSemiEvent::kKinFit;        else if
+    (!strcmp(argv[3], "kGenMatch"     )) hypoKey = TtSemiEvent::kGenMatch;      else if
+    (!strcmp(argv[3], "kMVADisc"      )) hypoKey = TtSemiEvent::kMVADisc; 
+  else{
+    // -------------------------------------------------  
+    std::cerr << "ERROR:: " 
+	      << "Unknown HypoKey!" << std::endl
+	      << "        Please specify one out of the following keys:" << std::endl
+	      << "        * kWMassMaxSumPt" << std::endl
+	      << "        * kMaxSumPtWMass" << std::endl
+	      << "        * kKinFit" << std::endl
+	      << "        * kGenMatch" << std::endl
+	      << "        * kMVADisc" << std::endl;
+    // -------------------------------------------------  
+    return -1;
+  }
+
+  // load framework libraries
+  gSystem->Load( "libFWCoreFWLite" );
+  AutoLibraryLoader::enable();
+  
+  // set nice style for histograms
+  setNiceStyle();
+
+  // define some histograms 
+  TH1F* hadWPt_    = new TH1F("hadWPt",     "p_{t} (W_{had}) [GeV]", 100,  0., 500.);
+  TH1F* hadWMass_  = new TH1F("hadWMass",   "M (W_{had}) [GeV]",      50,  0., 150.);
+  TH1F* hadTopPt_  = new TH1F("hadTopPt",   "p_{t} (t_{had}) [GeV]", 100,  0., 500.);
+  TH1F* hadTopMass_= new TH1F("hadTopMass", "M (t_{had}) [GeV]",      50, 50., 250.);
+
+  TH1F* lepWPt_    = new TH1F("lepWPt",     "p_{t} (W_{lep}) [GeV]", 100,  0., 500.);
+  TH1F* lepWMass_  = new TH1F("lepWMass",   "M (W_{lep}) [GeV]",      50,  0., 150.);
+  TH1F* lepTopPt_  = new TH1F("lepTopPt",   "p_{t} (t_{lep}) [GeV]", 100,  0., 500.);
+  TH1F* lepTopMass_= new TH1F("lepTopMass", "M (t_{lep}) [GeV]",      50, 50., 250.);
+
+
+  // -------------------------------------------------  
+  std::cout << "open  file: " << argv[1] << std::endl;
+  // -------------------------------------------------
+  TFile* inFile = TFile::Open(argv[1]);
+  TTree* events_= 0;
+  if( inFile ) inFile->GetObject("Events", events_); 
+  if( events_==0 ){
+    // -------------------------------------------------  
+    std::cerr << "ERROR:: " 
+	      << "Unable to retrieve TTree Events!" << std::endl
+	      << "        Eighter wrong file name or the the tree doesn't exists" << std::endl;
+    // -------------------------------------------------  
+    return -1;
+  }
+
+  // acess branch of ttSemiEvent
+  char decayName[50];
+  sprintf(decayName, "recoGenParticles_decaySubset__%s.obj", argv[2]);
+  TBranch* decay_   = events_->GetBranch( decayName ); // referred to from within TtGenEvent class
+  assert( decay_ != 0 ); 
+  char genEvtName[50];
+  sprintf(genEvtName, "TtGenEvent_genEvt__%s.obj", argv[2]);
+  TBranch* genEvt_  = events_->GetBranch( genEvtName ); // referred to from within TtSemiEvent class
+  assert( genEvt_ != 0 ); 
+  char semiEvtName[50];
+  sprintf(semiEvtName, "TtSemiEvent_ttSemiEvent__%s.obj", argv[2]);
+  TBranch* semiEvt_ = events_->GetBranch( semiEvtName ); 
+  assert( semiEvt_ != 0 );
+  
+  // loop over events and fill histograms  
+  int nevt = events_->GetEntries();
+  TtSemiEvent semiEvt;
+  // -------------------------------------------------  
+  std::cout << "start looping " << nevt << " events..." << std::endl;
+  // -------------------------------------------------
+  for(int evt=0; evt<nevt; ++evt){
+    // set branch address
+    semiEvt_-> SetAddress( &semiEvt );
+    // get event
+    decay_  ->GetEntry( evt );
+    genEvt_ ->GetEntry( evt );
+    semiEvt_->GetEntry( evt );
+    events_ ->GetEntry( evt, 0 );
+
+    // -------------------------------------------------  
+    /* if(evt>0 && !evt%100) */ std::cout << "  processing event: " << evt << std::endl;
+    // -------------------------------------------------  
+
+    // fill histograms
+    if( !semiEvt.isHypoAvailable(hypoKey) ){
+      std::cerr << "NonValidHyp:: " << "Hypothesis not available for this event" << std::endl;
+      continue;
+    }
+    if( !semiEvt.isHypoValid(hypoKey) ){
+      std::cerr << "NonValidHyp::" << "Hypothesis not valid for this event" << std::endl;
+      continue;
+    }
+    
+    const reco::Candidate* hadTop = semiEvt.hadronicTop(hypoKey);
+    const reco::Candidate* hadW   = semiEvt.hadronicW  (hypoKey);
+    const reco::Candidate* lepTop = semiEvt.leptonicTop(hypoKey);
+    const reco::Candidate* lepW   = semiEvt.leptonicW  (hypoKey);
+    
+    if(hadTop && hadW && lepTop && lepW){
+      hadWPt_    ->Fill( hadW->pt()    );
+      hadWMass_  ->Fill( hadW->mass()  );
+      hadTopPt_  ->Fill( hadTop->pt()  );
+      hadTopMass_->Fill( hadTop->mass());
+      
+      lepWPt_    ->Fill( lepW->pt()    );
+      lepWMass_  ->Fill( lepW->mass()  );
+      lepTopPt_  ->Fill( lepTop->pt()  );
+      lepTopMass_->Fill( lepTop->mass());
+    }
+  }
+  // -------------------------------------------------  
+  std::cout << "close file" << std::endl;
+  // -------------------------------------------------
+  inFile->Close();
+  
+  // save histograms to file
+  TFile outFile( "analyzeHypothesis.root", "recreate" );
+  switch( hypoKey ){
+  case TtSemiEvent::kWMassMaxSumPt : 
+    outFile.mkdir("analyzeMaxSumPtWMass");
+    outFile.cd("analyzeMaxSumPtWMass");
+    break;
+  case TtSemiEvent::kMaxSumPtWMass : 
+    outFile.mkdir("analyzeMaxSumPtWMass");
+    outFile.cd("analyzeMaxSumPtWMass");
+    break;
+  case TtSemiEvent::kKinFit : 
+    outFile.mkdir("analyzeKinFit");
+    outFile.cd("analyzeKinFit");
+    break;
+  case TtSemiEvent::kGenMatch : 
+    outFile.mkdir("analyzeGenMatch");
+    outFile.cd("analyzeGenMatch");
+    break;
+  case TtSemiEvent::kMVADisc : 
+    outFile.mkdir("analyzeMVADisc");
+    outFile.cd("analyzeMVADisc");
+    break;
+  }
+  hadWPt_    ->Write( );
+  hadWMass_  ->Write( );
+  hadTopPt_  ->Write( );
+  hadTopMass_->Write( );
+  lepWPt_    ->Write( );
+  lepWMass_  ->Write( );
+  lepTopPt_  ->Write( );
+  lepTopMass_->Write( );
+  outFile.Close();
+  
+  // free allocated space
+  delete hadWPt_;
+  delete hadWMass_;
+  delete hadTopPt_;
+  delete hadTopMass_;
+  delete lepWPt_;
+  delete lepWMass_;
+  delete lepTopPt_;
+  delete lepTopMass_;
+
+  return 0;
+}
