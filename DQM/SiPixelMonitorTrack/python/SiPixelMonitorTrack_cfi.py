cdf48a5ba379125b0b05c7ce2e2bf128af8a6666
Shan-Huei Chuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-07-30 21:17:23 +0000

    python cfi fitter-concurrent

--- DQM/SiPixelMonitorTrack/python/SiPixelMonitorTrack_cfi.py
+++ DQM/SiPixelMonitorTrack/python/SiPixelMonitorTrack_cfi.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-SiPixelMonitorTrackResiduals = cms.EDFilter("SiPixelMonitorTrackResiduals",
+SiPixelTrackResidualSource = cms.EDFilter("SiPixelTrackResidualSource",
     src = cms.InputTag("siPixelTrackResiduals"),
-    debug = cms.bool(False),
-    saveFile = cms.bool(True),
+    debug = cms.untracked.bool(False),
+    saveFile = cms.untracked.bool(True),
     outputFile = cms.string('Pixel_DQM_TrackResidual.root'),
     TrackCandidateProducer = cms.string('newTrackCandidateMaker'),
     TrackCandidateLabel = cms.string(''),
     TTRHBuilder = cms.string('WithTrackAngle'),
-    Fitter = cms.string('FittingSmootherWithOutlierRejection')
+    Fitter = cms.string('KFFittingSmootherWithOutliersRejectionAndRK')
 )
