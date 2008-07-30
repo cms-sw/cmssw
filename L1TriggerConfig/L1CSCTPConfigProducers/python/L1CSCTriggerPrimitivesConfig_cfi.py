d98274b485be8b466af1bfbe2ccda646467e1250
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2008-07-30 08:30:34 +0000

    Agree on common parameter names for online, offline, and documentation.

--- L1TriggerConfig/L1CSCTPConfigProducers/python/L1CSCTriggerPrimitivesConfig_cfi.py
+++ L1TriggerConfig/L1CSCTPConfigProducers/python/L1CSCTriggerPrimitivesConfig_cfi.py
@@ -11,63 +11,53 @@ l1csctpconf = cms.ESProducer("L1CSCTriggerPrimitivesConfigProducer",
 
     # Parameters for ALCT processors: default
     alctParam = cms.PSet(
-        alctFifoTbins   = cms.uint32(16),
-        alctFifoPretrig = cms.uint32(10),
-        alctBxWidth     = cms.uint32(6),
-        alctDriftDelay  = cms.uint32(3),
-        alctNphThresh   = cms.uint32(2),
-        alctNphPattern  = cms.uint32(4),
-        alctAccThresh   = cms.uint32(2),
-        alctAccPattern  = cms.uint32(4),
-        alctTrigMode    = cms.uint32(3),
-        alctAlctAmode   = cms.uint32(1),
-        alctL1aWindow   = cms.uint32(5)
+        alctFifoTbins              = cms.uint32(16),
+        alctFifoPretrig            = cms.uint32(10),
+        alctDriftDelay             = cms.uint32(3),
+        alctNplanesHitPretrig      = cms.uint32(2),
+        alctNplanesHitPattern      = cms.uint32(4),
+        alctNplanesHitAccelPretrig = cms.uint32(2),
+        alctNplanesHitAccelPattern = cms.uint32(4),
+        alctTrigMode               = cms.uint32(3),
+        alctAccelMode              = cms.uint32(1),
+        alctL1aWindowWidth         = cms.uint32(5)
     ),
 
     # Parameters for ALCT processors: MTCC-II and 2007 tests of new firmware
     alctParamMTCC2 = cms.PSet(
-        alctFifoTbins   = cms.uint32(16),
-        alctFifoPretrig = cms.uint32(10),
-        alctBxWidth     = cms.uint32(6),
-        alctDriftDelay  = cms.uint32(2),
-        alctNphThresh   = cms.uint32(2),
-        alctNphPattern  = cms.uint32(4),
-        alctAccThresh   = cms.uint32(2),
-        alctAccPattern  = cms.uint32(4),
-        alctTrigMode    = cms.uint32(2),
-        alctAlctAmode   = cms.uint32(0),
-        alctL1aWindow   = cms.uint32(7)
+        alctFifoTbins              = cms.uint32(16),
+        alctFifoPretrig            = cms.uint32(10),
+        alctDriftDelay             = cms.uint32(2),
+        alctNplanesHitPretrig      = cms.uint32(2),
+        alctNplanesHitPattern      = cms.uint32(4),
+        alctNplanesHitAccelPretrig = cms.uint32(2),
+        alctNplanesHitAccelPattern = cms.uint32(4),
+        alctTrigMode               = cms.uint32(2),
+        alctAccelMode              = cms.uint32(0),
+        alctL1aWindowWidth         = cms.uint32(7)
     ),
 
     # Parameters for CLCT processors: default and 2007 tests of new firmware
     clctParam = cms.PSet(
         clctFifoTbins   = cms.uint32(12),
         clctFifoPretrig = cms.uint32(7),
-        clctBxWidth     = cms.uint32(6),
+        clctHitPersist  = cms.uint32(6),
         clctDriftDelay  = cms.uint32(2),
-        clctHsThresh    = cms.uint32(2),
-        clctDsThresh    = cms.uint32(2),
-        clctNphPattern  = cms.uint32(4),
-        # TMB07 parameters
-        clctHitThresh   = cms.uint32(2),
-        clctPidThresh   = cms.uint32(2),
-        clctSepSrc      = cms.uint32(1),
-        clctSepVme      = cms.uint32(10)
+        clctNplanesHitPretrig = cms.uint32(2),
+        clctNplanesHitPattern = cms.uint32(4),
+        clctPidThreshPretrig  = cms.uint32(2),
+        clctMinSeparation     = cms.uint32(10)
     ),
 
     # Parameters for CLCT processors: MTCC-II
     clctParamMTCC2 = cms.PSet(
         clctFifoTbins   = cms.uint32(12),
         clctFifoPretrig = cms.uint32(7),
-        clctBxWidth     = cms.uint32(6),
+        clctHitPersist  = cms.uint32(6),
         clctDriftDelay  = cms.uint32(2),
-        clctHsThresh    = cms.uint32(4),
-        clctDsThresh    = cms.uint32(4),
-        clctNphPattern  = cms.uint32(1),
-        # TMB07 parameters
-        clctHitThresh   = cms.uint32(2),
-        clctPidThresh   = cms.uint32(2),
-        clctSepSrc      = cms.uint32(1),
-        clctSepVme      = cms.uint32(10)
+        clctNplanesHitPretrig = cms.uint32(4),
+        clctNplanesHitPattern = cms.uint32(1),
+        clctPidThreshPretrig  = cms.uint32(2),
+        clctMinSeparation     = cms.uint32(10)
     )
 )
