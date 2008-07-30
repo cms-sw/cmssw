a8ef87a172cbc7bf3ab1cf97b74d0acc5c133aea
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2008-07-30 17:14:00 +0000

    Modify the analysis, add MEM_TOTAL and change the output name for the MEM_LIVE with --value_peak

--- Configuration/PyReleaseValidation/scripts/cmsIgProf_Analysis.py
+++ Configuration/PyReleaseValidation/scripts/cmsIgProf_Analysis.py
@@ -16,24 +16,28 @@ def execute(command):
 def analyse_prof_sim(profile_name,outdir):
     
     # A way of formatting strings similar to the c++ one                 
-    outfile1='%s/mem_live.res' %outdir
+    outfile1='%s/mem_live_peak.res' %outdir
     outfile2='%s/doEvent_output.txt' %outdir
     outfile3='%s/doBeginJob_output.txt' %outdir    
-    dummyfile='%s/IgProf-Analyse_output.txt' %outdir
+    outfile4='%s/mem_live.res' %outdir
+    outfile5='%s/mem_total.res' %outdir    
     
     #A first manipulation of the profile
     command='igprof-analyse -d -v -g --value peak -r MEM_LIVE %s > %s'\
-                                    %(profile_name,dummyfile)
+                                    %(profile_name,outfile1)
     execute(command) #we use the method system of the object os to run a command
     
     # now let's execute the segment and analyse commands                                                                
     commands_list=(\
-    'igprof-segment edm::EDProducer::doEvent < %s |tee  -a %s' %(dummyfile,outfile2),
+    'igprof-segment edm::EDProducer::doEvent < %s |tee  -a %s' %(outfile1,outfile2),
     
-    'igprof-segment edm::EDProducer::doBeginJob < %s |tee -a %s' %(dummyfile,outfile3),
+    'igprof-segment edm::EDProducer::doBeginJob < %s |tee -a %s' %(outfile1,outfile3),
     
     'igprof-analyse -d -v -g -r MEM_LIVE %s |tee -a \%s'\
-                                    %(profile_name,outfile1)
+                                    %(profile_name,outfile4),
+    
+    'igprof-analyse -d -v -g -r MEM_TOTAL %s |tee -a \%s'\
+                                    %(profile_name,outfile5)
                   )
     
     for command in commands_list: #no iterator can be clearer than this one
@@ -44,7 +48,7 @@ def analyse_prof_sim(profile_name,outdir):
     titlestring='<b>Report executed with release %s on %s.</b>\n<br>\n<hr>\n'\
                                    %(os.environ['CMSSW_VERSION'],time.asctime())
     
-    for command,filename in map(None,commands_list,[outfile2,outfile3,outfile1]):
+    for command,filename in map(None,commands_list,[outfile2,outfile3,outfile4,outfile5]):
         command_info='Command executed: <em>%s</em><br><br>\n' %command 
         
         # we open and read the txt ascii file
