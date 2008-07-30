e407eac27945e59aaa5c1c81102d2e2f134904c7
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2008-07-30 14:57:03 +0000

    Fixed flavor history filter and producer

--- PhysicsTools/HepMCCandAlgos/python/flavorHistoryFilter_cfi.py
+++ PhysicsTools/HepMCCandAlgos/python/flavorHistoryFilter_cfi.py
@@ -1,11 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
-flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
-                                   src = cms.InputTag("flavorHistoryProducer", "bpartonFlavorHistory"),
-                                   jets = cms.InputTag("sisCone5GenJets"),
-                                   type = cms.int32(3), # only look at matrix element products
-                                   minPt = cms.double(10.0),
-                                   minDR = cms.double(0.5),
-                                   scheme = cms.string("deltaR"),
-                                   verbose = cms.bool(False)
-                                   )
+wbb_me_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("bFlavorHistoryProducer", "bPartonFlavorHistory"),
+                                        jets = cms.InputTag("iterativeCone5GenJets"),
+                                        type = cms.int32(3), # only look at matrix element products
+                                        minPt = cms.double(20.0),
+                                        minDR = cms.double(0.5),
+                                        scheme = cms.string("deltaR"),
+                                        verbose = cms.bool(False)
+                                        )
+
+wcc_me_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        jets = cms.InputTag("iterativeCone5GenJets"),
+                                        type = cms.int32(3), # only look at matrix element products
+                                        minPt = cms.double(20.0),
+                                        minDR = cms.double(0.5),
+                                        scheme = cms.string("deltaR"),
+                                        verbose = cms.bool(False)
+                                        )
+
+wc_fe_flavorHistoryFilter = cms.EDFilter("FlavorHistoryFilter",
+                                        src = cms.InputTag("cFlavorHistoryProducer", "cPartonFlavorHistory"),
+                                        jets = cms.InputTag("iterativeCone5GenJets"),
+                                        type = cms.int32(2), # only look at flavor excitation products
+                                        minPt = cms.double(20.0),
+                                        minDR = cms.double(0.5),
+                                        scheme = cms.string("deltaR"),
+                                        verbose = cms.bool(False)
+                                        )
--- PhysicsTools/HepMCCandAlgos/python/flavorHistoryProducer_cfi.py
+++ PhysicsTools/HepMCCandAlgos/python/flavorHistoryProducer_cfi.py
@@ -1,12 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-flavorHistoryProducer = cms.EDProducer("FlavorHistoryProducer",
+cFlavorHistoryProducer = cms.EDProducer("FlavorHistoryProducer",
+                                       src = cms.InputTag("genParticles"),
+                                       pdgIdToSelect = cms.int32(4),
+                                       ptMinParticle = cms.double(0.0),
+                                       ptMinShower = cms.double(0.0),
+                                       etaMaxParticle = cms.double(4.0),
+                                       etaMaxShower = cms.double(4.0),
+                                       flavorHistoryName = cms.string("cPartonFlavorHistory"),
+                                       verbose = cms.untracked.bool(False)
+                                       )
+
+bFlavorHistoryProducer = cms.EDProducer("FlavorHistoryProducer",
                                        src = cms.InputTag("genParticles"),
                                        pdgIdToSelect = cms.int32(5),
                                        ptMinParticle = cms.double(0.0),
                                        ptMinShower = cms.double(0.0),
                                        etaMaxParticle = cms.double(4.0),
                                        etaMaxShower = cms.double(4.0),
-                                       flavorHistoryName = cms.string("bpartonFlavorHistory"),
+                                       flavorHistoryName = cms.string("bPartonFlavorHistory"),
                                        verbose = cms.untracked.bool(False)
                                        )
