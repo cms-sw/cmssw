7b21d4b9569278a52b7c54d5ec05b078efde25cc
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2008-07-30 14:57:21 +0000

    (python) configuration to run the missing ET signficance

---
+++ RecoMET/METProducers/python/CaloMETSignif_cfi.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+# File: CaloMETSignif_cfi.py
+# Author: F.Blekman
+# Date: 07.30.2008
+#
+# Run the missing ET significance algorithm.
+
+# product
+met = cms.EDProducer("METProducer",
+    src = cms.InputTag("towerMaker"),
+    METType = cms.string('CaloMET'),
+    alias = cms.string('RawCaloMET'),
+    noHF = cms.bool(False),
+    globalThreshold = cms.double(0.5),
+    InputType = cms.string('CandidateCollection'),
+      
+    #missing ET significance resolution parameters: 
+    # ET resolution has three parameters, defined as parameters in 'standard' resolution function res= et*sqrt((par[2]*par[2])+(par[1]*par[1]/et)+(par[0]*par[0]/(et*et)));
+    # PHI resolution has one parameter,defined as res = par[0]*et;
+
+    #ECAL:
+    EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EB_PhiResPar = cms.vdouble(0.0174),
+    EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EE_PhiResPar = cms.vdouble(0.087),
+    #HCAL:
+    HB_EtResPar = cms.vdouble(0.,1.22,0.05),
+    HB_PhiResPar = cms.vdouble(0.087),
+    HE_EtResPar = cms.vdouble(0.,1.3,0.05),
+    HE_PhiResPar = cms.vdouble(0.087),
+    HO_EtResPar = cms.vdouble(0.,1.3,0.005),
+    HO_PhiResPar = cms.vdouble(0.087),
+    HF_EtResPar = cms.vdouble(0.,1.82,0.09),
+    HF_PhiResPar = cms.vdouble(0.174)               
+)
+
+
