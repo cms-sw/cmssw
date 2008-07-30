f7c1548383cb97ad1bc94d053f3d95d8a66bd199
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-07-30 19:30:01 +0000

    - fix DCC event size name

--- DQM/EcalEndcapMonitorTasks/src/EESelectiveReadoutTask.cc
+++ DQM/EcalEndcapMonitorTasks/src/EESelectiveReadoutTask.cc
@@ -1,8 +1,8 @@
 /*
  * \file EESelectiveReadoutTask.cc
  *
- * $Date: 2008/07/30 16:22:22 $
- * $Revision: 1.9 $
+ * $Date: 2008/07/30 16:24:44 $
+ * $Revision: 1.10 $
  * \author P. Gras
  * \author E. Di Marco
  *
@@ -97,7 +97,7 @@ void EESelectiveReadoutTask::setup(void) {
   if ( dqmStore_ ) {
     dqmStore_->setCurrentFolder(prefixME_ + "/EESelectiveReadoutTask");
 
-    sprintf(histo, "EESRT DCC event size EE");
+    sprintf(histo, "EESRT DCC event size");
     EEDccEventSize_ = dqmStore_->bookProfile(histo, histo, 18, 1, 19, 100, 0., 200., "s");
     for (int i = 0; i < 18; i++) {
       EEDccEventSize_->setBinLabel(i+1, Numbers::sEE(i+1).c_str(), 1);
