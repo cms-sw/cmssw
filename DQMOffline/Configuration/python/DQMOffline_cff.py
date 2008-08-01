ba92833d4c5c3d3fa04ecafe67395be2d5c38ea1
Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch>
2008-08-01 12:01:08 +0000

    define seq w/o hlt mon until the variable bin number problem is sorted out

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -6,9 +6,13 @@ from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
 #from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
+#from DQMOfflin.Trigger.FourVectorHLTOffline_cfi import *
+#from DQMOffline.Trigger.L1TMonitor_dqmoffline_cff import *
 
 
 #DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*siPixelOfflineDQM_source)
 #DQMOffline_woHcal = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*siPixelOfflineDQM_source)
 DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
 DQMOffline_woHcal = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer)
+
+DQMOffline_noHLT = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource)
