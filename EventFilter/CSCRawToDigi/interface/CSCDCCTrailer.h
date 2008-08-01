dcf2c7bec27d5ae62e4eee118ca3b04fcce8a371
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-08-01 17:51:54 +0000

    Reverse the order of everything

--- EventFilter/CSCRawToDigi/interface/CSCDCCTrailer.h
+++ EventFilter/CSCRawToDigi/interface/CSCDCCTrailer.h
@@ -26,23 +26,23 @@ struct CSCDCCTrailer {
   }
   /// for reference www.physics.ohio-state.edu/%7Ecms/dcc/outdatafmt.html
   /// dcc_trail1 should be EF
-  unsigned dcc_trail1       : 8;
-  unsigned readout_time     : 8;
-  unsigned ddu_data_status1 : 16;
-  unsigned ddu_data_status2 : 24;
   unsigned fifo_status      : 8;
-  
+  unsigned ddu_data_status2 : 24;
+  unsigned ddu_data_status1 : 16;
+  unsigned readout_time     : 8;
+  unsigned dcc_trail1       : 8;
+
   /// this line defined by CMS CDF 
   /// http://cmsdoc.cern.ch/cms/TRIDAS/horizontal/
-  unsigned EOE_1        : 4;
-  unsigned XXXX_1       : 4;
-  unsigned Evt_lgth     : 24;
-  unsigned CRC          : 16;
-  unsigned XXXX_2       : 4;
-  unsigned Evt_stat     : 4;
-  unsigned TTS          : 4;
-  unsigned Tx           : 3;
   unsigned dollardollar : 1;
+  unsigned Tx           : 3;
+  unsigned TTS          : 4;
+  unsigned Evt_stat     : 4;
+  unsigned XXXX_2       : 4;
+  unsigned CRC          : 16;
+  unsigned Evt_lgth     : 24;
+  unsigned XXXX_1       : 4;
+  unsigned EOE_1        : 4;
 
   static unsigned sizeInWords() {return 8;}
   bool check() const {return (dcc_trail1 == 0xEF) && (EOE_1 == 0XA) && (XXXX_1 == 0XF);}
