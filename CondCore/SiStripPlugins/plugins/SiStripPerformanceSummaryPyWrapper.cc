998b168d9d2121707969402b998696bd11f94f1e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-08-05 12:35:11 +0000

    empy files

---
+++ CondCore/SiStripPlugins/plugins/SiStripPerformanceSummaryPyWrapper.cc
@@ -0,0 +1,57 @@
+
+#include "CondFormats//SiStripObjects/interface/SiStripPerformanceSummary.h"
+
+#include "CondCore/Utilities/interface/PayLoadInspector.h"
+#include "CondCore/Utilities/interface/InspectorPythonWrapper.h"
+
+namespace cond {
+
+  template<>
+  class ValueExtractor<SiStripPerformanceSummary>: public  BaseValueExtractor<SiStripPerformanceSummary> {
+  public:
+
+    typedef SiStripPerformanceSummary Class;
+    typedef ExtractWhat<Class> What;
+    static What what() { return What();}
+
+    ValueExtractor(){}
+    ValueExtractor(What const & what, std::vector<int> const& which)
+    {
+      // here one can make stuff really complicated...
+    }
+    void compute(Class const & it){
+    }
+  private:
+  
+  };
+
+
+  template<>
+  std::string
+  PayLoadInspector<SiStripPerformanceSummary>::dump() const {
+    std::stringstream ss;
+    return ss.str();
+    
+  }
+  
+  template<>
+  std::string PayLoadInspector<SiStripPerformanceSummary>::summary() const {
+    std::stringstream ss;
+    return ss.str();
+  }
+  
+
+  template<>
+  std::string PayLoadInspector<SiStripPerformanceSummary>::plot(std::string const & filename,
+						   std::string const &, 
+						   std::vector<int> const&, 
+						   std::vector<float> const& ) const {
+    std::string fname = filename + ".png";
+    std::ofstream f(fname.c_str());
+    return fname;
+  }
+
+
+}
+
+PYTHON_WRAPPER(SiStripPerformanceSummary,SiStripPerformanceSummary);
