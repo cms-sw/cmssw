333b62f011b0b167d6ea2127fb47712bd926598e
Dongwook Jang <sha1-238fd1bb7a3396d331821fd7ca6808ad9eef1aff@cern.ch>
2008-08-05 14:53:10 +0000

    use default cone definition as PFTau

--- RecoTauTag/Pi0Tau/test/TauAna.h
+++ RecoTauTag/Pi0Tau/test/TauAna.h
@@ -15,7 +15,7 @@ Implementation:
 //
 // Original Author:  Dongwook Jang
 //         Created:  Wed Oct 11 11:08:40 CDT 2006
-// $Id: TauAna.h,v 1.1 2007/03/27 21:32:03 dwjang Exp $
+// $Id: TauAna.h,v 1.1 2007/06/04 23:11:24 dwjang Exp $
 //
 
 // system include files
@@ -30,6 +30,7 @@ Implementation:
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include <TTree.h>
+#include <TVector3.h>
 #include <TLorentzVector.h>
 
 //
@@ -37,7 +38,7 @@ Implementation:
 //
 
 class TauAna : public edm::EDAnalyzer {
-//class TauAna : public edm::EDProducer {
+
  public:
   explicit TauAna(const edm::ParameterSet&);
   ~TauAna();
@@ -61,6 +62,7 @@ class TauAna : public edm::EDAnalyzer {
   TLorentzVector *t_tracksMomentum;
   TLorentzVector *t_pi0sMomentum;
   TLorentzVector *t_momentum;
+  TVector3 *t_seedTrackVertex;
 
 };
 
--- RecoTauTag/Pi0Tau/test/tauAna.py
+++ RecoTauTag/Pi0Tau/test/tauAna.py
@@ -6,13 +6,14 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("RecoTauTag.Pi0Tau.tauAna_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/uscmst1b_scratch/lpc1/lpctau/dwjang/single_particles/210pre10/stau/pdg15_pt10.0-100.0_events1000_1.root')
+#    fileNames = cms.untracked.vstring('file:/uscmst1b_scratch/lpc1/lpctau/dwjang/single_particles/210pre10/stau/pdg15_pt10.0-100.0_events1000_1.root')
+    fileNames = cms.untracked.vstring('/store/RelVal/CMSSW_2_1_0/RelValZTT/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V5_v1/0001/0C4FEB17-9E60-DD11-997C-001617E30D00.root')
 )
 
-process.tauAna.histFileName = 'hist_stau.root'
+process.tauAna.histFileName = 'hist_ztau.root'
 
 process.p = cms.Path(process.tauAna)
 
