5461ae919d94021446eded5a1ceb8b53eb827ddc
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2008-08-05 13:03:57 +0000

    correcting the METSig input resolutions in phi by a factor /sqrt(12).

--- RecoMET/METProducers/python/CaloMETSignif_cfi.py
+++ RecoMET/METProducers/python/CaloMETSignif_cfi.py
@@ -20,18 +20,18 @@ metsig = cms.EDProducer("METProducer",
 
     #ECAL:
     EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
-    EB_PhiResPar = cms.vdouble(0.0174),
+    EB_PhiResPar = cms.vdouble(0.00502),    # 0.0174/sqrt(12)
     EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
-    EE_PhiResPar = cms.vdouble(0.087),
+    EE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
     #HCAL:
     HB_EtResPar = cms.vdouble(0.,1.22,0.05),
-    HB_PhiResPar = cms.vdouble(0.087),
+    HB_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
     HE_EtResPar = cms.vdouble(0.,1.3,0.05),
-    HE_PhiResPar = cms.vdouble(0.087),
+    HE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
     HO_EtResPar = cms.vdouble(0.,1.3,0.005),
-    HO_PhiResPar = cms.vdouble(0.087),
+    HO_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
     HF_EtResPar = cms.vdouble(0.,1.82,0.09),
-    HF_PhiResPar = cms.vdouble(0.174)               
+    HF_PhiResPar = cms.vdouble(0.05022)     # 0.174/sqrt(12)          
 )
 
 
