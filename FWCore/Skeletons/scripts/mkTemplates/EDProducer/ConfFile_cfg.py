63b8cfc564729d4cbfc08d35d15ef010d743d532
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-08-06 10:46:34 +0000

    add maxevents to conf template

--- FWCore/Skeletons/scripts/mkTemplates/EDProducer/ConfFile_cfg.py
+++ FWCore/Skeletons/scripts/mkTemplates/EDProducer/ConfFile_cfg.py
@@ -4,6 +4,8 @@ process = cms.Process("OWNPARTICLES")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
