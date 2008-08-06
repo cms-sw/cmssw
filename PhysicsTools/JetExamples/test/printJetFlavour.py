c2dc06046e7b8d17edf71a78c95894343636e83a
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2008-08-06 03:37:01 +0000

    ParticleListDrawer example added

--- PhysicsTools/JetExamples/test/printJetFlavour.py
+++ PhysicsTools/JetExamples/test/printJetFlavour.py
@@ -12,6 +12,11 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/relval/2008/5/20/RelVal-RelValTTbar-1211209682-FakeConditions-2nd/0000/08765709-5826-DD11-9CE8-000423D94700.root')
 )
 
+process.printTree = cms.EDFilter("ParticleListDrawer",
+    src = cms.InputTag("genParticles"),
+    maxEventsToPrint  = cms.untracked.int32(1)
+)
+
 process.myPartons = cms.EDFilter("PartonSelector",
     withLeptons = cms.bool(False)
 )
@@ -35,11 +40,11 @@ process.printEvent = cms.EDFilter("printJetFlavour",
 
 process.printEventNumber = cms.OutputModule("AsciiOutputModule")
 
-process.p = cms.Path(process.myPartons*process.flavourByRef*process.flavourByVal*process.printEvent)
+process.p = cms.Path(process.printTree*process.myPartons*process.flavourByRef*process.flavourByVal*process.printEvent)
 process.outpath = cms.EndPath(process.printEventNumber)
-process.MessageLogger.destinations = ['cout']
-process.MessageLogger.cout = cms.PSet(
-    threshold = cms.untracked.string('ERROR')
-)
+process.MessageLogger.destinations = cms.untracked.vstring('cout','cerr')
+#process.MessageLogger.cout = cms.PSet(
+#    threshold = cms.untracked.string('ERROR')
+#)
 
 
