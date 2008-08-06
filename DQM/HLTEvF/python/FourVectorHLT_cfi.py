9c55f190f202edaad238fcd70e2b41a8241d67cf
bjbloom <sha1-b16c2c250931f43e06dcda266c05bdc208573998@cern.ch>
2008-08-06 15:14:32 +0000

    process=FU

--- DQM/HLTEvF/python/FourVectorHLT_cfi.py
+++ DQM/HLTEvF/python/FourVectorHLT_cfi.py
@@ -1,4 +1,4 @@
-# $Id$
+# $Id: FourVectorHLT_cfi.py,v 1.2 2008/08/05 21:00:43 wittich Exp $
 import FWCore.ParameterSet.Config as cms
 
 hltResults = cms.EDAnalyzer("FourVectorHLT",
@@ -7,9 +7,9 @@ hltResults = cms.EDAnalyzer("FourVectorHLT",
     ptMin = cms.untracked.double(0.0),
     filters = cms.VPSet(),
     # data best guess
-    # triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","FU")
+    triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","FU")
     # MC
-    triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT")
+    #triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT")
 )
 
 
