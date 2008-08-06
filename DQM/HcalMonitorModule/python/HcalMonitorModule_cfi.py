05f740f9f72f07d66e44bfeca20ba359848f013f
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-08-06 23:27:37 +0000

    updated python versions of .cfg files

--- DQM/HcalMonitorModule/python/HcalMonitorModule_cfi.py
+++ DQM/HcalMonitorModule/python/HcalMonitorModule_cfi.py
@@ -1,103 +1,126 @@
 # The following comments couldn't be translated into the new config version:
 
+# of sigma above threshold for a cell to be considered hot
 # orig vale was 0.02
+# HotCell Tags for individual subdetectors
+# If these aren't specified, then the global 
+# values are used
+#untracked bool HBdebug = false
+#untracked bool HEdebug = false
+#untracked bool HOdebug = false
+#untracked bool HFdebug = false
+# Re-tuned threshold based on CRUZET1 run 43636 
+# First threshold is used when looking for hot cells 
+
 import FWCore.ParameterSet.Config as cms
 
 hcalMonitor = cms.EDFilter("HcalMonitorModule",
     PedestalsPerChannel = cms.untracked.bool(True),
-    NADA_NegCand_cut = cms.untracked.double(-1.5),
-    RecHitOccThresh = cms.untracked.double(2.0),
-    # NADA params updated for Jan DQM challenge using GREN data
-    NADA_Ecand_cut0 = cms.untracked.double(1.5),
-    NADA_Ecand_cut1 = cms.untracked.double(2.5), ## orig value was 2.5
-
-    NADA_Ecand_cut2 = cms.untracked.double(500.0),
-    checkHF = cms.untracked.bool(True),
-    thresholds = cms.untracked.vdouble(1.0, 1.5, 2.0, 5.0, 15.0),
-    #Determines connection to back-end daemon
-    MonitorDaemon = cms.untracked.bool(True),
-    NADA_maxeta = cms.untracked.int32(1),
-    NADA_maxdepth = cms.untracked.int32(0),
-    gtLabel = cms.InputTag("l1GtUnpack"),
-    hoRecHitLabel = cms.InputTag("horeco"),
-    caloTowerLabel = cms.InputTag("towerMaker"),
-    hbheRecHitLabel = cms.InputTag("hbhereco"),
-    #Flags for pedestal monitor
-    PedestalMonitor = cms.untracked.bool(True),
-    #Flags for RecHitMonitor.  Threshold is in GeV
-    RecHitMonitor = cms.untracked.bool(True),
-    #Flags for DigiMonitor.  Threshold is in ADC counts
-    DigiMonitor = cms.untracked.bool(True),
-    MinPhi = cms.untracked.double(-0.5),
-    DumpEtaHigh = cms.untracked.int32(10),
-    LEDPerChannel = cms.untracked.bool(True),
-    DumpEtaLow = cms.untracked.int32(0),
-    DumpPhiHigh = cms.untracked.int32(10),
     DumpThreshold = cms.untracked.double(500.0),
-    NADA_Ecell_frac = cms.untracked.double(0.02),
-    #Flags for DataFormatMonitor
-    DataFormatMonitor = cms.untracked.bool(True),
-    #Flags for HotCellMonitor.  Thresholds are in GeV
-    HotCellMonitor = cms.untracked.bool(True),
-    deadcellmindiff = cms.untracked.double(1.0), ## cell must be at least this much below neighbors to be considered a cool cell candidate 
-
-    # Labels for input products
+    thresholds = cms.untracked.vdouble(15.0, 5.0, 2.0, 1.5, 1.0),
+    coolcellfrac = cms.untracked.double(0.5),
+    NADA_maxeta = cms.untracked.int32(1),
+    hoRecHitLabel = cms.InputTag("horeco"),
+    PedestalMonitor = cms.untracked.bool(True),
+    MakeHotCellDiagnosticPlots = cms.untracked.bool(False),
+    DigiMonitor = cms.untracked.bool(True),
+    HotCell_checkNADA = cms.untracked.bool(True),
     digiLabel = cms.InputTag("hcalDigis"),
-    checkNevents = cms.untracked.int32(25),
-    minADCcount = cms.untracked.double(0.0),
-    #Flags for MTCCMonitor.  Thresholds are in fC
-    #Dump threshold in fC, ranges in iEta/iPhi.  Dumps to stdout
-    MTCCMonitor = cms.untracked.bool(False),
-    PedestalsInFC = cms.untracked.bool(False),
-    # Operate every N updates
-    diagnosticPrescaleUpdate = cms.untracked.int32(-1),
-    #Flags for the hcal template analysis
-    HcalAnalysis = cms.untracked.bool(False),
-    NADA_maxphi = cms.untracked.int32(1),
-    DumpPhiLow = cms.untracked.int32(10),
-    checkHE = cms.untracked.bool(True),
-    coolcellfrac = cms.untracked.double(0.5), ## fraction of cell's neighbors that must be significantly hotter than the cell for the cell to be considered "cool"
-
-    DigiOccThresh = cms.untracked.int32(0),
     hfRecHitLabel = cms.InputTag("hfreco"),
+    DumpPhiLow = cms.untracked.int32(10),
+    DigiOccThresh = cms.untracked.int32(0),
     RecHitsPerChannel = cms.untracked.bool(False),
-    checkHO = cms.untracked.bool(True),
-    MaxPhi = cms.untracked.double(73.5),
-    checkHB = cms.untracked.bool(True),
-    DigisPerChannel = cms.untracked.bool(False), ## set DigisPerChannel off by default -- JT, 08 May 2008
-
-    LED_ADC_Thresh = cms.untracked.double(-1000.0),
-    # Operate every N lumi sections
+    HE_NADA_Ecell_cut = cms.untracked.double(0.0),
+    HF_NADA_Ecube_frac = cms.untracked.double(0.5714),
+    HB_NADA_Ecell_frac = cms.untracked.double(0.0),
+    HF_NADA_Ecand_cut2 = cms.untracked.double(20.0),
+    HF_NADA_Ecand_cut0 = cms.untracked.double(1.5),
+    HF_NADA_Ecand_cut1 = cms.untracked.double(1.5),
     diagnosticPrescaleLS = cms.untracked.int32(-1),
-    MTCCOccThresh = cms.untracked.double(10.0),
-    ped_Nsigma = cms.untracked.double(0.0),
-    #Flags for CaloTower Monitor
+    MakeDigiDiagnosticPlots = cms.untracked.bool(False),
     CaloTowerMonitor = cms.untracked.bool(False),
-    NADA_Ecube_cut = cms.untracked.double(0.5), ## orig value was 0.1
-
-    #List of known hot cells to veto using DetId
+    NADA_Ecube_frac = cms.untracked.double(0.3),
+    NADA_maxphi = cms.untracked.int32(1),
+    debug = cms.untracked.bool(False),
+    RecHitOccThresh = cms.untracked.double(2.0),
+    NADA_Ecand_cut0 = cms.untracked.double(1.5),
+    NADA_Ecand_cut1 = cms.untracked.double(2.5),
+    NADA_Ecand_cut2 = cms.untracked.double(500.0),
+    MonitorDaemon = cms.untracked.bool(True),
+    HE_NADA_Ecand_cut1 = cms.untracked.double(2.5),
+    RecHitMonitor = cms.untracked.bool(True),
+    caloTowerLabel = cms.InputTag("towerMaker"),
+    HE_NADA_Ecand_cut2 = cms.untracked.double(10.0),
+    HF_NADA_Ecell_cut = cms.untracked.double(0.0),
+    MakeDiagnosticPlots = cms.untracked.bool(True),
+    NADA_Ecell_frac = cms.untracked.double(0.02),
+    HO_NADA_Ecube_cut = cms.untracked.double(0.1),
+    HF_NADA_Ecell_frac = cms.untracked.double(0.0),
+    HB_NADA_Ecell_cut = cms.untracked.double(0.0),
+    MTCCMonitor = cms.untracked.bool(False),
+    HcalAnalysis = cms.untracked.bool(False),
+    DumpEtaHigh = cms.untracked.int32(10),
+    MaxPhi = cms.untracked.double(73.5),
+    DigisPerChannel = cms.untracked.bool(False),
+    HEthresholds = cms.untracked.vdouble(3.0, 2.0, 1.0, 5.0, 10.0),
+    LED_ADC_Thresh = cms.untracked.double(-1000.0),
+    diagnosticPrescaleTime = cms.untracked.int32(-1),
+    LEDPerChannel = cms.untracked.bool(True),
+    MaxEta = cms.untracked.double(42.5),
+    NADA_NegCand_cut = cms.untracked.double(-1.5),
+    HE_NADA_Ecand_cut0 = cms.untracked.double(1.5),
+    NADA_maxdepth = cms.untracked.int32(0),
+    hbheRecHitLabel = cms.InputTag("hbhereco"),
+    HotCellDigiSigma = cms.untracked.double(5.0),
+    DataFormatMonitor = cms.untracked.bool(True),
+    HFthresholds = cms.untracked.vdouble(9.0, 2.0, 4.0, 6.0, 10.0),
+    HotCell_checkThreshold = cms.untracked.bool(True),
+    minADCcount = cms.untracked.double(0.0),
+    PedestalsInFC = cms.untracked.bool(False),
+    HO_NADA_Ecell_frac = cms.untracked.double(0.0),
+    HE_NADA_Ecell_frac = cms.untracked.double(0.0),
+    showTiming = cms.untracked.bool(False),
+    HB_NADA_Ecube_frac = cms.untracked.double(0.333),
+    HB_NADA_Ecand_cut2 = cms.untracked.double(10.0),
+    HB_NADA_Ecand_cut0 = cms.untracked.double(1.5),
+    HB_NADA_Ecand_cut1 = cms.untracked.double(2.25),
+    HF_NADA_Ecube_cut = cms.untracked.double(0.1),
+    HOthresholds = cms.untracked.vdouble(2.0, 3.0, 1.0, 5.0, 10.0),
+    ped_Nsigma = cms.untracked.double(0.0),
     HotCells = cms.untracked.vstring(),
-    #Flags for DeadCellMonitor
     DeadCellMonitor = cms.untracked.bool(True),
-    #Flags for LED monitor
+    HO_NADA_Ecand_cut2 = cms.untracked.double(10.0),
+    HO_NADA_Ecand_cut1 = cms.untracked.double(2.5),
+    HO_NADA_Ecand_cut0 = cms.untracked.double(1.5),
+    NADA_Ecell_cut = cms.untracked.double(0.1),
+    diagnosticPrescaleEvt = cms.untracked.int32(-1),
+    HE_NADA_Ecube_frac = cms.untracked.double(0.222),
+    DumpPhiHigh = cms.untracked.int32(10),
+    HB_NADA_Ecube_cut = cms.untracked.double(0.1),
+    DeadCell_checkAbovePed = cms.untracked.bool(True),
+    gtLabel = cms.InputTag("l1GtUnpack"),
+    HO_NADA_Ecell_cut = cms.untracked.double(0.0),
+    DumpEtaLow = cms.untracked.int32(0),
+    HotCell_checkAbovePed = cms.untracked.bool(True),
+    TrigPrimMonitor = cms.untracked.bool(True),
+    MinPhi = cms.untracked.double(-0.5),
+    MakeDeadCellDiagnosticPlots = cms.untracked.bool(False),
+    HotCellMonitor = cms.untracked.bool(True),
+    deadcellmindiff = cms.untracked.double(1.0),
+    checkNevents = cms.untracked.int32(25),
+    diagnosticPrescaleUpdate = cms.untracked.int32(-1),
+    HE_NADA_Ecube_cut = cms.untracked.double(0.1),
+    HO_NADA_Ecube_frac = cms.untracked.double(0.2),
+    checkHF = cms.untracked.bool(True),
+    checkHE = cms.untracked.bool(True),
+    checkHB = cms.untracked.bool(True),
+    checkHO = cms.untracked.bool(True),
+    MTCCOccThresh = cms.untracked.double(10.0),
+    NADA_Ecube_cut = cms.untracked.double(0.5),
+    HBthresholds = cms.untracked.vdouble(3.0, 2.0, 1.0, 5.0, 10.0),
     LEDMonitor = cms.untracked.bool(True),
     MinEta = cms.untracked.double(-42.5),
-    NADA_Ecell_cut = cms.untracked.double(0.1),
-    #Flags for TPG monitor
-    TrigPrimMonitor = cms.untracked.bool(True),
-    # Choices for prescaling your module (-1 mean no prescale)
-    # Operate every N events
-    diagnosticPrescaleEvt = cms.untracked.int32(-1),
-    #Global HCAL DQM parameters.  
-    #Sets range of iEta/iPhi plots
-    MaxEta = cms.untracked.double(42.5),
-    NADA_Ecube_frac = cms.untracked.double(0.3),
-    deadcellfloor = cms.untracked.double(-10.0), ## cells below this energy are automatically set as "cool"
-
-    # Operate every N minutes
-    diagnosticPrescaleTime = cms.untracked.int32(-1),
-    # Verbosity Switch
-    debug = cms.untracked.bool(False),
+    deadcellfloor = cms.untracked.double(-10.0),
     dfPrtLvl = cms.untracked.int32(0)
 )
 
--- DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-file_cfg.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-file_cfg.py
@@ -1,40 +1,22 @@
-# The following comments couldn't be translated into the new config version:
-
-#        ecalRecHit, towerMaker,  
-
-# To run on .dat streamer files (you need to change the path to the datafile):
-# source = NewEventStreamFileReader {
-# 	untracked vstring fileNames = {
-# 	    "file:/data1/lookarea/GlobalAug07.00017220.0001.A.storageManager.0.0000.dat",
-# 	    "file:/data1/lookarea/GlobalAug07.00017220.0002.A.storageManager.0.0000.dat",
-# 	    "file:/data1/lookarea/GlobalAug07.00017220.0003.A.storageManager.0.0000.dat"
-# 	}
-# }
-#To run on HCAL local runs:  (you need to have acces to /bigspool or modify the path to the datafile)	
-# source = HcalTBSource {
-#        untracked vstring fileNames = {'file:/bigspool/usc/USC_035349.root'}
-#        untracked vstring streams = { 
-# 		//HBHE a, b, c:
-# 		'HCAL_DCC700','HCAL_DCC701','HCAL_DCC702','HCAL_DCC703','HCAL_DCC704','HCAL_DCC705',
-# 		'HCAL_DCC706','HCAL_DCC707','HCAL_DCC708','HCAL_DCC709','HCAL_DCC710','HCAL_DCC711',
-# 		'HCAL_DCC712','HCAL_DCC713','HCAL_DCC714','HCAL_DCC715','HCAL_DCC716','HCAL_DCC717',
-# 		//HF:
-# 		'HCAL_DCC718','HCAL_DCC719','HCAL_DCC720','HCAL_DCC721','HCAL_DCC722','HCAL_DCC723',
-# 		//HO:
-# 		'HCAL_DCC724','HCAL_DCC725','HCAL_DCC726','HCAL_DCC727','HCAL_DCC728','HCAL_DCC729',
-# 		'HCAL_DCC730','HCAL_DCC731',
-# 		'HCAL_Trigger','HCAL_SlowData'
-# 	}
-# }	
-
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("HCALDQM")
+#
+# BEGIN DQM Online Environment ###########################
+#
+process.load("DQMServices.Core.DQM_cfg")
+
+# use include file for dqmEnv dqmSaver
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+#
+# END ################################################
+#
 process.load("DQM.HcalMonitorModule.HcalMonitorModule_cfi")
 
 process.load("DQM.HcalMonitorClient.HcalMonitorClient_cfi")
 
-process.load("DQM.HcalMonitorModule.Hcal_FrontierConditions_GREN_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
@@ -46,55 +28,87 @@ process.load("RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi")
 
 process.load("RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi")
 
-#
-# BEGIN DQM Online Environment #######################
-#
-# use include file for dqmEnv dqmSaver
-process.load("DQMServices.Components.test.dqm_onlineEnv_cfi")
+# will add switch to select histograms to be saved soon
+# Quality Tester #### 
+process.qTester = cms.EDFilter("QualityTester",
+    prescaleFactor = cms.untracked.int32(1),
+    qtList = cms.untracked.FileInPath('DQM/HcalMonitorClient/data/hcal_qualitytest_config.xml'),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
 
+process.prefer("GlobalTag")
+process.Timing = cms.Service("Timing")
+
+#path p = {hcalDigis, horeco, hfreco, hbhereco, hcalMonitor}
 process.options = cms.untracked.PSet(
-    Rethrow = cms.untracked.vstring('ProductNotFound', 'TooManyProducts', 'TooFewProducts')
+    Rethrow = cms.untracked.vstring('ProductNotFound', 
+        'TooManyProducts', 
+        'TooFewProducts')
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10)
 )
+#To run on HCAL local runs:  (you need to have access to /bigspool or modify the path to the datafile)	
+#source = HcalTBSource {
+#	untracked vstring fileNames = {'file:/afs/cern.ch/user/s/stjohn/scratch0/USC_044790.root'}
+#untracked vstring streams = { 
+#HBHE a, b, c:
+#'HCAL_DCC700','HCAL_DCC701','HCAL_DCC702','HCAL_DCC703','HCAL_DCC704','HCAL_DCC705',
+#'HCAL_DCC706','HCAL_DCC707','HCAL_DCC708','HCAL_DCC709','HCAL_DCC710','HCAL_DCC711',
+#'HCAL_DCC712','HCAL_DCC713','HCAL_DCC714','HCAL_DCC715','HCAL_DCC716','HCAL_DCC717',
+#HF:
+#'HCAL_DCC718','HCAL_DCC719','HCAL_DCC720','HCAL_DCC721','HCAL_DCC722','HCAL_DCC723',
+#HO:
+#'HCAL_DCC724','HCAL_DCC725','HCAL_DCC726','HCAL_DCC727','HCAL_DCC728','HCAL_DCC729',
+#'HCAL_DCC730','HCAL_DCC731',
+#'HCAL_Trigger','HCAL_SlowData'
+#}
+#}	
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/121F13FE-E9A4-DC11-994D-001617DBD272.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/324F1DA3-E5A4-DC11-A732-000423D65A7E.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/34F36443-E9A4-DC11-8C8A-000423D944D4.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/360B4662-E8A4-DC11-B423-001617DBCEF6.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/421BAA91-E8A4-DC11-87A9-001617DBD36C.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/4CE78852-E6A4-DC11-B47A-000423D665B2.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/7637B20C-E8A4-DC11-950A-001617C3B652.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/7832F4A6-E6A4-DC11-AE02-00304856284E.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/943B3531-E7A4-DC11-B67C-001617DBD49A.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/A4C5514F-E5A4-DC11-9BDF-000423D98708.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/D0D20FF6-E7A4-DC11-BBDA-001617DBD2E2.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/D4486B09-E6A4-DC11-AA00-000E0C3F08B7.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/E0481BB5-E7A4-DC11-8BB2-003048561116.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/F016E2D2-E8A4-DC11-AD13-001617DBCFA6.root', '/store/data/GlobalNov07/DTHcal/000/030/625/RAW/0000/F2A2C7F7-E6A4-DC11-BF27-000423D59C46.root')
-)
-
-process.DaqMonitorROOTBackEnd = cms.Service("DaqMonitorROOTBackEnd")
-
-process.MonitorDaemon = cms.Service("MonitorDaemon",
-    #	 if true, will automatically start DQM thread in background
-    #  (default: false)
-    AutoInstantiate = cms.untracked.bool(False),
-    # collector hostname
-    # (default: localhost)
-    DestinationAddress = cms.untracked.string('localhost'),
-    # maximum     # of reconnection attempts upon connection problems (default: 10)
-    maxAttempts2Reconnect = cms.untracked.int32(0),
-    # port for communicating with collector
-    # (default: 9090)
-    SendPort = cms.untracked.int32(9090),
-    # name of DQM source
-    # (default: DQMSource)
-    NameAsSource = cms.untracked.string('Hcal'),
-    # monitoring period in ms (i.e. how often monitoring elements 
-    # are shipped to the collector
-    # (default: 1000)
-    UpdateDelay = cms.untracked.int32(10),
-    # if >=0, upon a connection problem, the source will automatically 
-    # attempt to reconnect with a time delay (secs) specified here 
-    # (default: 5)
-    reconnect_delay = cms.untracked.int32(5)
-)
-
-process.DQMShipMonitoring = cms.Service("DQMShipMonitoring",
-    # event-period for shipping monitoring to collector (default: 25)
-    period = cms.untracked.uint32(100)
+    fileNames = cms.untracked.vstring('/store/data/CRUZET3/Cosmics/RAW/v1/000/051/199/EA1F908F-AD4E-DD11-8235-000423D6A6F4.root')
 )
 
 process.p = cms.Path(process.hcalDigis*process.horeco*process.hfreco*process.hbhereco*process.hcalMonitor*process.hcalClient*process.dqmEnv*process.dqmSaver)
-process.dqmSaver.fileName = 'Hcal'
+process.DQM.collectorHost = 'myhost'
+process.DQM.collectorPort = 9092
+process.dqmSaver.convention = 'Online'
+#replace dqmSaver.dirName          = "."
+process.dqmSaver.producer = 'DQM'
 process.dqmEnv.subSystemFolder = 'Hcal'
+# optionally change fileSaving  conditions
+# replace dqmSaver.prescaleLS =   -1
+# replace dqmSaver.prescaleTime = -1 # in minutes
+# replace dqmSaver.prescaleEvt =  -1
+#> For Hcal local run files, replace dqmSaver.saveByRun = 2 
+process.dqmSaver.saveByRun = 1
+process.hcalMonitor.DigiOccThresh = -999999999 ##Temporary measure while DigiOcc is reworked.
+
+process.hcalMonitor.debug = False
+process.hcalMonitor.PedestalsPerChannel = False
+process.hcalMonitor.DataFormatMonitor = True
+process.hcalMonitor.DigiMonitor = False
+process.hcalMonitor.PedestalMonitor = False
+process.hcalMonitor.LEDMonitor = False
+process.hcalMonitor.TrigPrimMonitor = True
+process.hcalMonitor.HotCellMonitor = False
+process.hcalMonitor.DeadCellMonitor = False
+process.hcalMonitor.RecHitMonitor = False
+process.hcalMonitor.MTCCMonitor = False
+process.hcalMonitor.CaloTowerMonitor = False
+process.hcalMonitor.HcalAnalysis = False
+process.hcalClient.plotPedRAW = True
+process.hcalClient.DoPerChanTests = False
+process.hcalClient.SummaryClient = True
+process.hcalClient.DataFormatClient = True
+process.hcalClient.DigiClient = False
+process.hcalClient.LEDClient = False
+process.hcalClient.PedestalClient = False
+process.hcalClient.TrigPrimClient = True
+process.hcalClient.RecHitClient = False
+process.hcalClient.HotCellClient = False
+process.hcalClient.DeadCellClient = False
+process.hcalClient.CaloTowerClient = False
+process.GlobalTag.globaltag = 'CRUZET3_V2P::All'
+process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_20X_GLOBALTAG' ##Frontier/CMS_COND_20X_GLOBALTAG"
+
 
