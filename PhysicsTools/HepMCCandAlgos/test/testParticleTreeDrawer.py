7399a21f9f20aee2eb010932b318db98cadd84bd
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2008-08-06 04:10:07 +0000

    new python version

--- PhysicsTools/HepMCCandAlgos/test/testParticleTreeDrawer.py
+++ PhysicsTools/HepMCCandAlgos/test/testParticleTreeDrawer.py
@@ -5,7 +5,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(5)
+    input = cms.untracked.int32(1)
 )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/relval/2008/5/20/RelVal-RelValTTbar-1211209682-FakeConditions-2nd/0000/08765709-5826-DD11-9CE8-000423D94700.root')
@@ -18,7 +18,7 @@ process.printTree1 = cms.EDAnalyzer("ParticleListDrawer",
 
 process.printTree2 = cms.EDAnalyzer("ParticleTreeDrawer",
     src = cms.InputTag("genParticles"),
-    printP4 = cms.untracked.bool(True),
+    printP4 = cms.untracked.bool(False),
     printPtEtaPhi = cms.untracked.bool(False),
     printVertex = cms.untracked.bool(False),
     printStatus = cms.untracked.bool(False),
@@ -27,8 +27,7 @@ process.printTree2 = cms.EDAnalyzer("ParticleTreeDrawer",
 
 process.printEventNumber = cms.OutputModule("AsciiOutputModule")
 
-process.p = cms.Path(process.printTree1)
-#process.p = cms.Path(process.printTree1*process.printTree2)
+process.p = cms.Path(process.printTree1*process.printTree2)
 process.outpath = cms.EndPath(process.printEventNumber)
 process.MessageLogger.destinations = cms.untracked.vstring('cout','cerr')
 
