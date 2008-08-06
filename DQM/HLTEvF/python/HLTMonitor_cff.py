717cd0bbbfb9fd59a22abf526b98bd6f4cdd60b6
bjbloom <sha1-b16c2c250931f43e06dcda266c05bdc208573998@cern.ch>
2008-08-06 12:34:21 +0000

    ready for Cruzet 4

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -2,4 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.HLTEvF.HLTMonElectron_cff import *
 from DQM.HLTEvF.HLTMonMuon_cff import *
+#from DQM.HLTEvF.HLTMonTau_cff import *
+from DQM.HLTEvF.FourVectorHLT_cff import *
 
