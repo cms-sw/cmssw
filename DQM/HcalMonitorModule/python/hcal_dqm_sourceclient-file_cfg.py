07dd813e67f9a3cca30d0a14a49b7cc05f238c5c
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-08-07 16:24:17 +0000

    Updated global tags to COND_21X, STARTUP_V4, and made booleans the same for monitors and clients

--- DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-file_cfg.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-file_cfg.py
@@ -39,7 +39,6 @@ process.qTester = cms.EDFilter("QualityTester",
 process.prefer("GlobalTag")
 process.Timing = cms.Service("Timing")
 
-#path p = {hcalDigis, horeco, hfreco, hbhereco, hcalMonitor}
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound', 
         'TooManyProducts', 
@@ -49,21 +48,22 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10)
 )
 #To run on HCAL local runs:  (you need to have access to /bigspool or modify the path to the datafile)	
-#source = HcalTBSource {
-#	untracked vstring fileNames = {'file:/afs/cern.ch/user/s/stjohn/scratch0/USC_044790.root'}
-#untracked vstring streams = { 
-#HBHE a, b, c:
-#'HCAL_DCC700','HCAL_DCC701','HCAL_DCC702','HCAL_DCC703','HCAL_DCC704','HCAL_DCC705',
-#'HCAL_DCC706','HCAL_DCC707','HCAL_DCC708','HCAL_DCC709','HCAL_DCC710','HCAL_DCC711',
-#'HCAL_DCC712','HCAL_DCC713','HCAL_DCC714','HCAL_DCC715','HCAL_DCC716','HCAL_DCC717',
-#HF:
-#'HCAL_DCC718','HCAL_DCC719','HCAL_DCC720','HCAL_DCC721','HCAL_DCC722','HCAL_DCC723',
-#HO:
-#'HCAL_DCC724','HCAL_DCC725','HCAL_DCC726','HCAL_DCC727','HCAL_DCC728','HCAL_DCC729',
-#'HCAL_DCC730','HCAL_DCC731',
-#'HCAL_Trigger','HCAL_SlowData'
-#}
-#}	
+# (This should work in python, but has not yet been tested as of 7 August 2008)
+###process.source = cms.Source("HcalTBSource",
+###                            fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/stjohn/scratch0/USC_044790.root'),
+###                            streams   = cms.untracked.vstring(#HBHEa,b,c:
+###                                                              'HCAL_DCC700','HCAL_DCC701','HCAL_DCC702','HCAL_DCC703','HCAL_DCC704','HCAL_DCC705',
+###                                                              'HCAL_DCC706','HCAL_DCC707','HCAL_DCC708','HCAL_DCC709','HCAL_DCC710','HCAL_DCC711',
+###                                                              'HCAL_DCC712','HCAL_DCC713','HCAL_DCC714','HCAL_DCC715','HCAL_DCC716','HCAL_DCC717',
+###                                                              #HF:
+###                                                              'HCAL_DCC718','HCAL_DCC719','HCAL_DCC720','HCAL_DCC721','HCAL_DCC722','HCAL_DCC723',
+###                                                              #HO:
+###                                                              #'HCAL_DCC724','HCAL_DCC725','HCAL_DCC726','HCAL_DCC727','HCAL_DCC728','HCAL_DCC729',
+###                                                              #'HCAL_DCC730','HCAL_DCC731',
+###                                                              #'HCAL_Trigger','HCAL_SlowData'
+###                                                              )
+###                            )
+
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/data/CRUZET3/Cosmics/RAW/v1/000/051/199/EA1F908F-AD4E-DD11-8235-000423D6A6F4.root')
 )
@@ -79,12 +79,14 @@ process.dqmEnv.subSystemFolder = 'Hcal'
 # replace dqmSaver.prescaleLS =   -1
 # replace dqmSaver.prescaleTime = -1 # in minutes
 # replace dqmSaver.prescaleEvt =  -1
-#> For Hcal local run files, replace dqmSaver.saveByRun = 2 
+# For Hcal local run files, replace dqmSaver.saveByRun = 2 
 process.dqmSaver.saveByRun = 1
-process.hcalMonitor.DigiOccThresh = -999999999 ##Temporary measure while DigiOcc is reworked.
 
+# hcalMonitor configurable values
 process.hcalMonitor.debug = False
+process.hcalMonitor.DigiOccThresh = -999999999 ##Temporary measure while DigiOcc is reworked.
 process.hcalMonitor.PedestalsPerChannel = False
+# Turn on/off individual hcalMonitor modules
 process.hcalMonitor.DataFormatMonitor = True
 process.hcalMonitor.DigiMonitor = False
 process.hcalMonitor.PedestalMonitor = False
@@ -96,19 +98,31 @@ process.hcalMonitor.RecHitMonitor = False
 process.hcalMonitor.MTCCMonitor = False
 process.hcalMonitor.CaloTowerMonitor = False
 process.hcalMonitor.HcalAnalysis = False
+
+# hcalClient configurable values
 process.hcalClient.plotPedRAW = True
 process.hcalClient.DoPerChanTests = False
-process.hcalClient.SummaryClient = True
-process.hcalClient.DataFormatClient = True
-process.hcalClient.DigiClient = False
-process.hcalClient.LEDClient = False
-process.hcalClient.PedestalClient = False
-process.hcalClient.TrigPrimClient = True
-process.hcalClient.RecHitClient = False
-process.hcalClient.HotCellClient = False
-process.hcalClient.DeadCellClient = False
-process.hcalClient.CaloTowerClient = False
-process.GlobalTag.globaltag = 'CRUZET3_V2P::All'
-process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_20X_GLOBALTAG' ##Frontier/CMS_COND_20X_GLOBALTAG"
+# Turn on/off individual hcalClient modules -- by default, set them equal to the hcalMonitor values
+# (clients can't run without monitors being present -- if you want to turn off the client but keep the monitor,
+# just set the appropriate client value to False)
+process.hcalClient.SummaryClient = True # no corresponding hcalMonitor process for SummaryClient
+process.hcalClient.DataFormatClient = process.hcalMonitor.DataFormatMonitor 
+process.hcalClient.DigiClient = process.hcalMonitor.DigiMonitor 
+process.hcalClient.LEDClient = process.hcalMonitor.LEDMonitor 
+process.hcalClient.PedestalClient = process.hcalMonitor.PedestalMonitor 
+process.hcalClient.TrigPrimClient = process.hcalMonitor.TrigPrimMonitor 
+process.hcalClient.RecHitClient = process.hcalMonitor.RecHitMonitor 
+process.hcalClient.HotCellClient = process.hcalMonitor.HotCellMonitor 
+process.hcalClient.DeadCellClient = process.hcalMonitor.DeadCellMonitor 
+process.hcalClient.CaloTowerClient = process.hcalMonitor.CaloTowerMonitor 
+
+# Get conditions, global tag
+
+#This works
+process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_21X_GLOBALTAG'
+process.GlobalTag.globaltag = 'STARTUP_V4::All'
+
+#process.GlobalTag.globaltag = 'CRUZET3_V2P::All' # CRUZET3_V2P::All doesn't seem to exist?
+#process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_20X_GLOBALTAG' ##Frontier/CMS_COND_20X_GLOBALTAG"
 
 
