f4e1001180b00dd7bbe29faaa9d952c527ea7401
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-08-07 10:19:06 +0000

    SiStripQualityHotStripIdentifierRoot moved in DQMOffline/CalibTracker to remove depencency from DQM from this package

--- CalibTracker/SiStripQuality/plugins/SealModules.cc
+++ CalibTracker/SiStripQuality/plugins/SealModules.cc
@@ -3,12 +3,10 @@
 #include "CalibTracker/SiStripQuality/plugins/SiStripBadModuleByHandBuilder.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripQualityStatistics.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripQualityHotStripIdentifier.h"
-#include "CalibTracker/SiStripQuality/plugins/SiStripQualityHotStripIdentifierRoot.h"
 #include "CalibTracker/SiStripQuality/plugins/SiStripBadStripFromASCIIFile.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadModuleByHandBuilder);
 DEFINE_ANOTHER_FWK_MODULE(SiStripQualityStatistics);
 DEFINE_ANOTHER_FWK_MODULE(SiStripQualityHotStripIdentifier);
-DEFINE_ANOTHER_FWK_MODULE(SiStripQualityHotStripIdentifierRoot);
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadStripFromASCIIFile);
