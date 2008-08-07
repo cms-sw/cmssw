4dc07401150ed3fb02483a160ecd563072036715
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-08-07 22:52:51 +0000

    add intern jets parameter

--- EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
+++ EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
@@ -1,17 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 l1GctHwDigis = cms.EDProducer("GctRawToDigi",
-    verbose = cms.untracked.bool(False),
+    inputLabel = cms.InputTag("source"),
     gctFedId = cms.int32(745),
     hltMode = cms.bool(False),
     grenCompatibilityMode = cms.bool(False),
-    inputLabel = cms.InputTag("source"),
     unpackRct = cms.untracked.bool(True),
+    unpackInternEm = cms.untracked.bool(False),
+    unpackInternJets = cms.untracked.bool(False),
+    unpackFibres = cms.untracked.bool(False),
     unpackEm = cms.untracked.bool(True),
     unpackJets = cms.untracked.bool(True),
     unpackEtSums = cms.untracked.bool(True),
-    unpackInternEm = cms.untracked.bool(False),
-    unpackFibres = cms.untracked.bool(False)
+    verbose = cms.untracked.bool(False)
 )
 
 
