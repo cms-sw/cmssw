f2dcff8fef695b600c0e08b661a31c6bec93c06a
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-08-07 19:26:34 +0000

    Updated live, playback files based on most recent file in DQM/Integration/test

--- DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-live_cfg.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_sourceclient-live_cfg.py
@@ -4,13 +4,30 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("HCALDQM")
+#----------------------------
+# Event Source
+#-----------------------------
+process.load("DQM.Integration.test.inputsource_cfi")
+
+#----------------------------
+# DQM Environment
+#-----------------------------
 process.load("DQMServices.Core.DQM_cfg")
 
+#replace DQMStore.referenceFileName = "Hcal_reference.root"
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+#----------------------------
+# DQM Live Environment
+#-----------------------------
+process.load("DQM.Integration.test.environment_cfi")
+
+#-----------------------------
 process.load("DQM.HcalMonitorModule.HcalMonitorModule_cfi")
 
 process.load("DQM.HcalMonitorClient.HcalMonitorClient_cfi")
 
-process.load("DQM.HcalMonitorModule.Hcal_FrontierConditions_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
@@ -22,72 +39,41 @@ process.load("RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi")
 
 process.load("RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi")
 
-#
-# BEGIN DQM Online Environment #######################
-#
-# use include file for dqmEnv dqmSaver
-process.load("DQMServices.Components.DQMEnvironment_cfi")
-
+process.prefer("GlobalTag")
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound', 
         'TooManyProducts', 
         'TooFewProducts')
 )
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
-process.source = cms.Source("EventStreamHttpReader",
-    sourceURL = cms.string('http://srv-c2c07-19.cms:11100/urn:xdaq-application:service=storagemanager'),
-    consumerPriority = cms.untracked.string('normal'),
-    max_event_size = cms.int32(7000000),
-    consumerName = cms.untracked.string('HCAL DQM Consumer'),
-    max_queue_depth = cms.int32(5),
-    maxEventRequestRate = cms.untracked.double(10.0),
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('PhysicsPath')
-    ),
-    headerRetryInterval = cms.untracked.int32(3)
-)
-
 process.p = cms.Path(process.hcalDigis*process.horeco*process.hfreco*process.hbhereco*process.hcalMonitor*process.hcalClient*process.dqmEnv*process.dqmSaver)
-process.DQM.collectorHost = 'myhost'
-process.DQM.collectorPort = 9092
-
-# hcalMonitor configurable values -- toggle individual modules on/off
+process.EventStreamHttpReader.consumerName = 'Hcal DQM Consumer'
+process.dqmEnv.subSystemFolder = 'Hcal'
+process.hcalMonitor.PedestalsPerChannel = False
+process.hcalMonitor.PedestalsInFC = True
 process.hcalMonitor.DataFormatMonitor = True
 process.hcalMonitor.DigiMonitor = True
-process.hcalMonitor.HotCellMonitor = True
-process.hcalMonitor.DeadCellMonitor = True
 process.hcalMonitor.RecHitMonitor = True
 process.hcalMonitor.TrigPrimMonitor = True
+process.hcalMonitor.PedestalMonitor = True
+process.hcalMonitor.DeadCellMonitor = True
+process.hcalMonitor.HotCellMonitor = True
 process.hcalMonitor.MTCCMonitor = False
-process.hcalMonitor.PedestalMonitor = False
 process.hcalMonitor.LEDMonitor = False
 process.hcalMonitor.CaloTowerMonitor = False
 process.hcalMonitor.HcalAnalysis = False
-
-# hcalClient configurable values -- toggle individual modules on/off
+process.hcalClient.plotPedRAW = True
+process.hcalClient.baseHtmlDir = ''
+process.hcalClient.SummaryClient = True
 process.hcalClient.DataFormatClient = True
 process.hcalClient.DigiClient = True
 process.hcalClient.RecHitClient = True
-process.hcalClient.HotCellClient = True
 process.hcalClient.DeadCellClient = True
 process.hcalClient.TrigPrimClient = True
+process.hcalClient.HotCellClient = True
 process.hcalClient.CaloTowerClient = False
 process.hcalClient.LEDClient = False
 process.hcalClient.PedestalClient = False
-process.hcalClient.baseHtmlDir = ''
+process.GlobalTag.globaltag = 'CRUZET3_V6::All'
+process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG' ##(proxyurl=http:
 
-
-# Old tags don't seem to work; replace with STARTUP_V4 until given updated tags
-process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_21X_GLOBALTAG'
-process.GlobalTag.globaltag = 'STARTUP_V4::All'
-
-#process.GlobalTag.globaltag = 'CRUZET2_V2::All'
-#process.GlobalTag.connect = 'frontier://Frontier/CMS_COND_20X_GLOBALTAG' ##Frontier/CMS_COND_20X_GLOBALTAG" 
-
-process.dqmSaver.convention = 'Online'
-#replace dqmSaver.dirName          = "."
-process.dqmSaver.producer = 'DQM'
-process.dqmEnv.subSystemFolder = 'Hcal'
 
