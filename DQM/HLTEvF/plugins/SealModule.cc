0d45f473a14e75152fb76308cc57c971cf204a32
bjbloom <sha1-b16c2c250931f43e06dcda266c05bdc208573998@cern.ch>
2008-08-07 12:25:54 +0000

    readded HLTEventInfoClient

--- DQM/HLTEvF/plugins/SealModule.cc
+++ DQM/HLTEvF/plugins/SealModule.cc
@@ -1,4 +1,4 @@
-// $Id: SealModule.cc,v 1.11 2008/07/24 16:04:59 wittich Exp $
+// $Id: SealModule.cc,v 1.12 2008/08/06 12:00:31 bjbloom Exp $
 
 #include "DQM/HLTEvF/interface/PathTimerService.h"
 #include "DQM/HLTEvF/interface/PathTimerInserter.h"
@@ -7,6 +7,7 @@
 #include "DQM/HLTEvF/interface/HLTMonElectronConsumer.h"
 #include "DQM/HLTEvF/interface/FourVectorHLT.h"
 #include "DQM/HLTEvF/interface/HLTMon.h"
+#include "DQM/HLTEvF/interface/HLTEventInfoClient.h"
 
 using edm::service::PathTimerService;
 
@@ -19,6 +20,7 @@ DEFINE_FWK_MODULE(PathTimerInserter);
 DEFINE_FWK_MODULE(HLTTauDQMSource);
 DEFINE_FWK_MODULE(HLTMonElectron);
 DEFINE_FWK_MODULE(HLTMonElectronConsumer);
+DEFINE_FWK_MODULE(HLTEventInfoClient);
 DEFINE_FWK_MODULE(FourVectorHLT);
 DEFINE_FWK_MODULE(HLTMon);
 
--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -4,4 +4,4 @@ from DQM.HLTEvF.HLTMonElectron_cff import *
 from DQM.HLTEvF.HLTMonMuon_cff import *
 #from DQM.HLTEvF.HLTMonTau_cff import *
 from DQM.HLTEvF.FourVectorHLT_cff import *
-
+from DQM.HLTEvF.HLTEventInfoClient_cff import *
