96ccda77a4ded9344ffb5f9bce12621877833daa
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2008-08-08 08:47:00 +0000

    more edanalyzers in one pacakge

---
+++ CalibTracker/SiPixelGainCalibration/test/SealModules.cc
@@ -0,0 +1,10 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "CalibTracker/SiPixelGainCalibration/test/SimpleTestPrintOutPixelCalibAnalyzer.h"
+#include "CalibTracker/SiPixelGainCalibration/test/SiPixelGainCalibrationReadDQMFile.h"
+
+DEFINE_SEAL_MODULE();
+
+DEFINE_ANOTHER_FWK_MODULE(SimpleTestPrintOutPixelCalibAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(SiPixelGainCalibrationReadDQMFile);
