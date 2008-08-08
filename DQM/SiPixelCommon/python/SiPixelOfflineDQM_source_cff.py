2f3c7d08025801810eea2baa26eb419b91a900ed
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-08-08 15:28:19 +0000

    new offline DQM script with major reduction of bins

--- DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
+++ DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
@@ -1,31 +1,48 @@
 import FWCore.ParameterSet.Config as cms
 
+# Pixel Digi Monitoring
 from DQM.SiPixelMonitorDigi.SiPixelMonitorDigi_cfi import *
-from DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi import *
-from DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi import *
-siPixelOfflineDQM_source = cms.Sequence(SiPixelDigiSource*SiPixelClusterSource*SiPixelRecHitSource)
 SiPixelDigiSource.saveFile = False
+SiPixelDigiSource.isPIB = False
+SiPixelDigiSource.slowDown = False
+
+# Pixel Cluster Monitoring
+from DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi import *
 SiPixelClusterSource.saveFile = False
+
+# Pixel RecHit Monitoring
+from DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi import *
 SiPixelRecHitSource.saveFile = False
+
+        
+##online/offline
+#Digi
 SiPixelDigiSource.modOn = False
+SiPixelDigiSource.twoDimOn = False
 SiPixelDigiSource.ladOn = True
 SiPixelDigiSource.layOn = True
 SiPixelDigiSource.phiOn = True
 SiPixelDigiSource.bladeOn = True
 SiPixelDigiSource.diskOn = True
 SiPixelDigiSource.ringOn = True
+#Cluster
 SiPixelClusterSource.modOn = False
+SiPixelClusterSource.twoDimOn = False
 SiPixelClusterSource.ladOn = True
 SiPixelClusterSource.layOn = True
 SiPixelClusterSource.phiOn = True
 SiPixelClusterSource.bladeOn = True
 SiPixelClusterSource.diskOn = True
 SiPixelClusterSource.ringOn = True
+#RecHit
 SiPixelRecHitSource.modOn = False
+SiPixelRecHitSource.twoDimOn = False
 SiPixelRecHitSource.ladOn = True
 SiPixelRecHitSource.layOn = True
-SiPixelRecHitSource.phiOn = True
+SiPixelRecHitSource.phiOn = True	
 SiPixelRecHitSource.bladeOn = True
 SiPixelRecHitSource.diskOn = True
 SiPixelRecHitSource.ringOn = True
 
+
+siPixelOfflineDQM_source = cms.Sequence(SiPixelDigiSource + SiPixelRecHitSource + SiPixelClusterSource)
