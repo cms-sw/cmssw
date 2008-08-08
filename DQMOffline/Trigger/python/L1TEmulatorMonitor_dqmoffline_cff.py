adbc0479bf552e921a82fe422d220e79c56c2515
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-08-08 08:37:22 +0000

    emulator dqm

---
+++ DQMOffline/Trigger/python/L1TEmulatorMonitor_dqmoffline_cff.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+#global config
+#GlobalTag.globaltag = 'CRUZET3_V6::All'
+#GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
+#es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
+#unpacking
+#import Configuration.StandardSequences.RawToDigi_Data_cff
+
+#emulator/comparator
+from L1Trigger.Configuration.L1Config_cff import *
+from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
+l1compare.COMPARE_COLLS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1]
+
+#dqm
+from DQM.L1TMonitor.L1TDEMON_cfi import *
+from DQM.L1TMonitor.L1TdeECAL_cfi import *
+from DQM.L1TMonitor.L1TdeRCT_cfi import *
+l1tderct.rctSourceData = 'gctDigis'
+l1tderct.rctSourceEmul = 'valRctDigis'
+
+p = cms.Path(
+    #cms.SequencePlaceholder("RawToDigi")
+    cms.SequencePlaceholder("L1HardwareValidation")
+    *cms.SequencePlaceholder("l1demon")
+    *cms.SequencePlaceholder("l1demonecal")
+    *cms.SequencePlaceholder("l1tderct")
+    )
+
