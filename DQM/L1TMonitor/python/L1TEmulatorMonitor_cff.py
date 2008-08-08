d79a981a4cea59e90b443981f5c099db7b638dcf
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-08-08 09:30:14 +0000

    py updates

--- DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
+++ DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
@@ -1,80 +1,52 @@
 import FWCore.ParameterSet.Config as cms
-
-#  description:
+# description:
 # workflow for L1 Trigger Emulator DQM
-# used by DQM GUI: DQM/Integration/l1temulator*.cfg
+# used by DQM GUI: DQM/Integration/l1temulator*
 # nuno.leonardo@cern.ch 08.02
-# 
-#systems configuration
-from L1Trigger.HardwareValidation.hwtest.globrun.deGmt_cff import *
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deRpc.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deDt.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deCsc.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deGct.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deRct.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deEcal.cff"
-#include "L1Trigger/HardwareValidation/hwtest/globrun/deHcal.cff"
-#data|emulator comparator
-from L1Trigger.HardwareValidation.L1Comparator_cfi import *
-#sequence {ETP,HTP,RCT,GCT, DTP,DTF,CTP,CTF,RPC, LTC,GMT,GT}
-#dqm sources    
-from DQM.L1TMonitor.L1TDEMON_cfi import *
-#calo unpack
-#path unpketp = {ecalEBunpacker}
-#path unpkhtp = {hcalDigis}
-#path unpkgct = {gctUnpacker}
-#calo emulator
-#path emuletp = {ecalTriggerPrimitiveDigis}
-#path emulhtp = {hcalTriggerPrimitiveDigis}
-#path emulrct = {maskedRctInputDigis, l1RctEmulDigis }
-#path emulgct = {l1GctEmulDigis}
-#muon unpack
-#path unpkctp = {muonCSCDigis}
-#path unpkctf = {csctfunpacker}
-#path unpkdtp = {muonDTDigis}
-#path unpkdtf = {muonDTTFDigis}
-#path unpkrpc = {muonRPCDigis}
-unpkglt = cms.Path(gtUnpack)
-#muon emulator
-#path emulctp = {l1CscTpgEmulDigis}
-#path emulctf = {l1CscTfTrackEmulDigis,l1CscTfEmulDigis}
-#path emuldtp = {l1DTTPGEmulDigis}
-#path emuldtf = {l1DttfEmulDigis}
-#path emulrpc = {l1RpcEmulDigis}
-emulgmt = cms.Path(cms.SequencePlaceholder("l1GmtEmulDigis"))
-#comparator, dqm
-anacomp = cms.Path(l1compare*l1demon)
-#ecal
-l1compare.ETPsourceData = cms.InputTag("ecalEBunpacker","EBTT")
-l1compare.ETPsourceEmul = 'ecalTriggerPrimitiveDigis'
-#hcal
-l1compare.HTPsourceData = 'hcalDigis'
-l1compare.HTPsourceEmul = 'hcalTriggerPrimitiveDigis'
-#rct
-l1compare.RCTsourceData = 'gctUnpacker'
-l1compare.RCTsourceEmul = 'l1RctEmulDigis'
-#gct
-l1compare.GCTsourceData = 'gctUnpacker'
-l1compare.GCTsourceEmul = 'l1GctEmulDigis'
-#dt
-l1compare.DTPsourceData = 'muonDTTFDigis'
-l1compare.DTPsourceEmul = '1DTTPGEmulDigis'
-l1compare.DTFsourceData = 'muonDTTFDigis'
-l1compare.DTFsourceEmul = 'l1DttfEmulDigis'
-#csc
-l1compare.CTPsourceData = 'csctfunpacker'
-l1compare.CTPsourceEmul = 'l1CscTpgEmulDigis'
-l1compare.CTFsourceData = 'csctfunpacker'
-l1compare.CTFsourceEmul = 'l1CscTfEmulDigis'
-l1compare.CTTsourceData = 'csctfunpacker'
-l1compare.CTTsourceEmul = 'l1CscTfTrackEmulDigis'
-#rpc
-l1compare.RPCsourceData = 'muonRPCDigis'
-l1compare.RPCsourceEmul = 'l1RpcEmulDigis'
-#gmt
-l1compare.GMTsourceData = 'gtUnpack'
-l1compare.GMTsourceEmul = 'l1GmtEmulDigis'
-l1compare.COMPARE_COLLS = [0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 
-    1, 0]
+#
+#global configuration
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
+es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
+#off-line
+GlobalTag.globaltag = 'IDEAL_V5::All'
+GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
+#on-line
+#GlobalTag.globaltag = 'CRUZET3_V6H2::All'
+#GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG'
+#
+#unpacking
+from Configuration.StandardSequences.RawToDigi_Data_cff import *
+#
+#emulator/comparator
+from L1Trigger.HardwareValidation.L1HardwareValidationGR_cff import *
+from L1Trigger.Configuration.L1Config_cff import *
+l1compare.COMPARE_COLLS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1]
+#l1compare.DumpMode = -1
+#l1compare.VerboseFlag = 1
 
+#dqm
+from DQM.L1TMonitor.L1TDEMON_cfi import *
+#l1demon.VerboseFlag = -1
+#l1demon.HistFile = 'l1demon.root'
+from DQM.L1TMonitor.L1TdeECAL_cfi import *
+from DQM.L1TMonitor.L1TdeRCT_cfi import *
+l1tderct.rctSourceData = 'gctDigis'
+l1tderct.rctSourceEmul = 'valRctDigis'
+#
+p = cms.Path(
+    cms.SequencePlaceholder("RawToDigi")
+    *cms.SequencePlaceholder("L1HardwareValidation")
+    *cms.SequencePlaceholder("l1demon")
+    *cms.SequencePlaceholder("l1demonecal")
+    *cms.SequencePlaceholder("l1tderct")
+    )
+
+#from L1Trigger.HardwareValidation.hwtest.globrun.deEcal_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deHcal_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deRct_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deGct_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deDt_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deCsc_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deRpc_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deGmt_cff import *
+#from L1Trigger.HardwareValidation.hwtest.globrun.deGt_cff import *
