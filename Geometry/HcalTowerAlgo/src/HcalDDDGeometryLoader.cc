236008733b97264da931f841d1fbd67b18470994
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2008-08-10 14:55:56 +0000

    Fix HcalDDDGeometryLoader for HF

--- Geometry/HcalTowerAlgo/src/HcalDDDGeometryLoader.cc
+++ Geometry/HcalTowerAlgo/src/HcalDDDGeometryLoader.cc
@@ -8,27 +8,24 @@
 
 #include<string>
 
-HcalDDDGeometryLoader::HcalDDDGeometryLoader(const DDCompactView & cpv) 
-{
+HcalDDDGeometryLoader::HcalDDDGeometryLoader(const DDCompactView & cpv) {
   std::string name = "HcalHits";
   numberingFromDDD = new HcalNumberingFromDDD(name, cpv);
 }
 
-HcalDDDGeometryLoader::~HcalDDDGeometryLoader() 
-{
-   delete numberingFromDDD;
+HcalDDDGeometryLoader::~HcalDDDGeometryLoader() {
+  delete numberingFromDDD;
 }
 
 
 HcalDDDGeometryLoader::ReturnType 
-HcalDDDGeometryLoader::load(DetId::Detector det, int subdet) 
-{
+HcalDDDGeometryLoader::load(DetId::Detector det, int subdet) {
+
   HcalSubdetector  hsub        = static_cast<HcalSubdetector>(subdet);
   HcalDDDGeometry* gDDD ( new HcalDDDGeometry );
   ReturnType geom ( gDDD );
 
-/*  if( geom->cornersMgr() == 0 ) 
-  {
+  if ( geom->cornersMgr() == 0 ) {
      const unsigned int count (
 	numberingFromDDD->HcalCellTypes( HcalBarrel  ).size() +
 	numberingFromDDD->HcalCellTypes( HcalEndcap  ).size() +
@@ -36,25 +33,22 @@ HcalDDDGeometryLoader::load(DetId::Detector det, int subdet)
 	numberingFromDDD->HcalCellTypes( HcalOuter   ).size()   ) ;
      geom->allocateCorners( count ) ;
   }
-*/
-  if( geom->cornersMgr() == 0 ) 
-  {
-     geom->allocateCorners( HcalDetId::kSizeForDenseIndexing ) ;
-  }
-  if( geom->parMgr()     == 0 ) geom->allocatePar( 75, 3 ) ;
+
+  //  if( geom->cornersMgr() == 0 )  geom->allocateCorners( 2592 ) ;
+
+  if ( geom->parMgr()     == 0 ) geom->allocatePar( 500, 3 ) ;
 
   fill (hsub, gDDD, geom );
   return geom ;
 }
 
 HcalDDDGeometryLoader::ReturnType 
-HcalDDDGeometryLoader::load() 
-{
+HcalDDDGeometryLoader::load() {
+
    HcalDDDGeometry* gDDD ( new HcalDDDGeometry );
    ReturnType geom ( gDDD );
 
-   if( geom->cornersMgr() == 0 ) 
-   {
+   if( geom->cornersMgr() == 0 ) {
       const unsigned int count (
 	 numberingFromDDD->HcalCellTypes( HcalBarrel  ).size() +
 	 numberingFromDDD->HcalCellTypes( HcalEndcap  ).size() +
@@ -71,11 +65,10 @@ HcalDDDGeometryLoader::load()
    return geom ;
 }
 
-void 
-HcalDDDGeometryLoader::fill( HcalSubdetector          subdet, 
-			     HcalDDDGeometry*         geometryDDD,
-			     CaloSubdetectorGeometry* geom           ) 
-{
+void HcalDDDGeometryLoader::fill( HcalSubdetector          subdet, 
+				  HcalDDDGeometry*         geometryDDD,
+				  CaloSubdetectorGeometry* geom           ) {
+
   // start by making the new HcalDetIds
   std::vector<HcalCellType::HcalCellType> hcalCells = numberingFromDDD->HcalCellTypes(subdet);
   geometryDDD->insertCell(hcalCells);
@@ -126,13 +119,12 @@ HcalDDDGeometryLoader::fill( HcalSubdetector          subdet,
 			   << " is " << hcalIds.size();
 }
 
-const CaloCellGeometry* 
-HcalDDDGeometryLoader::makeCell(const HcalDetId& detId,
-				HcalCellType::HcalCellType hcalCell,
-				double phi, 
-				double dphi,
-				CaloSubdetectorGeometry* geom) const 
-{
+const CaloCellGeometry* HcalDDDGeometryLoader::makeCell(const HcalDetId& detId,
+							HcalCellType::HcalCellType hcalCell,
+							double phi, 
+							double dphi,
+							CaloSubdetectorGeometry* geom) const {
+
   // the two eta boundaries of the cell
   double          eta1   = hcalCell.etaMin();
   double          eta2   = hcalCell.etaMax();
@@ -142,18 +134,23 @@ HcalDDDGeometryLoader::makeCell(const HcalDetId& detId,
   double          theta  = 2.0*atan(exp(-eta));
 
   // barrel vs forward
+  bool rzType   = hcalCell.depthType();
   bool isBarrel = (subdet == HcalBarrel || subdet == HcalOuter);
 
   double          z, r, thickness;
 
-  if (isBarrel) {
+  if (rzType) {
     r          = hcalCell.depthMin();
-    thickness  = hcalCell.depthMax() - r;
+    if (isBarrel) {
+      z         = r * sinh(eta); // sinh(eta) == 1/tan(theta)
+      thickness = (hcalCell.depthMax() - r) * cosh(eta); // cosh(eta) == 1/sin(theta)
+    } else {
+      z         = r * sinh(eta2);
+      thickness = 2. * hcalCell.halfSize();
+    }
     LogDebug("HCalGeom") << "HcalDDDGeometryLoader::makeCell SubDet " << subdet
 			 << " eta = " << eta << " theta = " << theta
 			 << " r = " << r << " thickness = " << thickness;
-    z          = r * sinh(eta); // sinh(eta) == 1/tan(theta)
-    thickness *= cosh(eta);     // cosh(eta) == 1/sin(theta)
   } else {
     z          = hcalCell.depthMin();
     thickness  = hcalCell.depthMax() - z;
@@ -171,36 +168,31 @@ HcalDDDGeometryLoader::makeCell(const HcalDetId& detId,
   LogDebug("HCalGeom") << "HcalDDDGeometryLoader::makeCell for " << detId
 		       << " Point " << point << " deta = " << deta 
 		       << " dphi = " << dphi << " thickness = " << thickness
-		       << " isBarrel = " << isBarrel;
+		       << " isBarrel = " << isBarrel << " " << rzType;
 
   
-  if (subdet==HcalForward) 
-  {
-     std::vector<float> hf ;
-     hf.reserve(3) ;
-     hf.push_back( deta/2. ) ;
-     hf.push_back( dphi/2. ) ;
-     hf.push_back( thickness/2. ) ;
-     return new calogeom::IdealZPrism( 
-	point, 
-	geom->cornersMgr(),
-	CaloCellGeometry::getParmPtr( hf, 
-				      geom->parMgr(), 
-				      geom->parVecVec() ) );
-  } 
-  else 
-  { 
-     const double sign ( isBarrel ? 1 : -1 ) ;
-     std::vector<float> hh ;
-     hh.reserve(3) ;
-     hh.push_back( deta/2. ) ;
-     hh.push_back( dphi/2. ) ;
-     hh.push_back( sign*thickness/2. ) ;
-     return new calogeom::IdealObliquePrism(
-	point,
-	geom->cornersMgr(),
-	CaloCellGeometry::getParmPtr( hh, 
-				      geom->parMgr(), 
-				      geom->parVecVec() ) );
- }
+  if (subdet==HcalForward) {
+    std::vector<float> hf ;
+    hf.reserve(3) ;
+    hf.push_back( deta/2. ) ;
+    hf.push_back( dphi/2. ) ;
+    hf.push_back( thickness/2. ) ;
+    return new calogeom::IdealZPrism(point, 
+				     geom->cornersMgr(),
+				     CaloCellGeometry::getParmPtr(hf, 
+								  geom->parMgr(), 
+								  geom->parVecVec() ) );
+  } else  { 
+    const double sign ( isBarrel ? 1 : -1 ) ;
+    std::vector<float> hh ;
+    hh.reserve(3) ;
+    hh.push_back( deta/2. ) ;
+    hh.push_back( dphi/2. ) ;
+    hh.push_back( sign*thickness/2. ) ;
+    return new calogeom::IdealObliquePrism(point,
+					   geom->cornersMgr(),
+					   CaloCellGeometry::getParmPtr( hh, 
+									 geom->parMgr(), 
+									 geom->parVecVec() ) );
+  }
 }
