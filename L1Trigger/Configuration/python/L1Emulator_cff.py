d54374ae49854eccbd14cf0b99d7bb79e3c1d3f9
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-08-11 12:09:37 +0000

    add comment remove GT AOD record

--- L1Trigger/Configuration/python/L1Emulator_cff.py
+++ L1Trigger/Configuration/python/L1Emulator_cff.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-# Emulator configuration
-# select one of the two lines below
-# By default (17X) take configuration from dummy producers
+# this file is still used by FastSim
+# no-one else should use it!!!
+
+# Configuration (use Fake/Frontier in future)
 from L1Trigger.Configuration.L1Config_cff import *
 # Emulator modules
 from L1Trigger.Configuration.L1MuonEmulator_cff import *
 from L1Trigger.Configuration.L1CaloEmulator_cff import *
 from L1Trigger.GlobalTrigger.gtDigis_cfi import *
-from EventFilter.L1GlobalTriggerRawToDigi.l1GtRecord_cfi import *
 # Emulator sequence
-L1Emulator = cms.Sequence(L1CaloEmulator*L1MuonEmulator*gtDigis*l1GtRecord)
+L1Emulator = cms.Sequence(L1CaloEmulator*L1MuonEmulator*gtDigis)
 
