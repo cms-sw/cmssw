3f8c3e360cdd3847cabb55c78828d59a287b21bc
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-08-11 15:10:40 +0000

    analyzer, not filter

--- RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cfi.py
+++ RecoMuon/MuonSeedGenerator/python/standAloneMuonSeeds_cfi.py
@@ -5,15 +5,17 @@ import FWCore.ParameterSet.Config as cms
 #include "RecoMuon/MuonSeedGenerator/data/ptSeedParameterization_40T_851.cfi"
 # New map at 3.8 T
 from RecoMuon.MuonSeedGenerator.ptSeedParameterization_38T_cfi import *
+
 # New map at 4.0 T
 #include "RecoMuon/MuonSeedGenerator/data/ptSeedParameterization_40T.cfi"
+
 # module standAloneMuonSeeds = MuonSeedGenerator {
-MuonSeed = cms.EDFilter("MuonSeedGenerator",
-    CSCRecSegmentLabel = cms.InputTag("cscSegments"),
-    DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
-    EnableDTMeasurement = cms.bool(True),
-    EnableCSCMeasurement = cms.bool(True)
-)
+MuonSeed = cms.EDAnalyzer("MuonSeedGenerator",
+                          CSCRecSegmentLabel = cms.InputTag("cscSegments"),
+                          DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
+                          EnableDTMeasurement = cms.bool(True),
+                          EnableCSCMeasurement = cms.bool(True)
+                          )
 
 
 
