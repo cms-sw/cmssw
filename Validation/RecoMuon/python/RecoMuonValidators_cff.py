a664042b64a93fa4001b534c806b2c68e7400aac
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2008-08-11 11:27:56 +0000

    deprecated

--- Validation/RecoMuon/python/RecoMuonValidators_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
-from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-from Validation.RecoMuon.RecoMuonValidators_cfi import *
-
-
