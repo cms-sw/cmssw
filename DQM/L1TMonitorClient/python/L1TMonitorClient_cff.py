953ae029af272eddd007d497beb429b461f3ae2d
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-08-11 14:40:01 +0000

    updated path to include L1TEventInfoClient and DTTF

--- DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
@@ -1,12 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-#include "DQM/L1TMonitorClient/data/L1THcalClient.cff"
-#include "DQM/L1TMonitorClient/data/L1TDTTFClient.cff"
 #include "DQM/L1TMonitorClient/data/L1TDTTPGClient.cff"
-#include "DQM/L1TMonitorClient/data/L1TdeECALClient.cff"
+from DQM.L1TMonitorClient.L1TEventInfoClient_cff import *
 from DQM.L1TMonitorClient.L1TGMTClient_cff import *
 from DQM.L1TMonitorClient.L1TGCTClient_cff import *
-#include "DQM/L1TMonitorClient/data/L1TdeECALClient.cff"
+from DQM.L1TMonitorClient.L1TDTTFClient_cff import *
 from DQM.L1TMonitorClient.L1TCSCTFClient_cff import *
 from DQM.L1TMonitorClient.L1TDEMONClient_cff import *
 from DQM.L1TMonitorClient.L1TRPCTFClient_cff import *
@@ -16,5 +14,5 @@ from DQMServices.Components.DQMEnvironment_cfi import *
 #
 # END ################################################
 #
-l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tcsctfseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*dqmEnv*dqmSaver)
+l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tcsctfseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
 
