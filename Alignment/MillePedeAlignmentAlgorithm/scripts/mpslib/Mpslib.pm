fe8220335af9d00e5ad8500d52823d3a97c0fc24
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-08-12 18:16:22 +0000

    create backup database before writing in case of interupt

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mpslib/Mpslib.pm
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mpslib/Mpslib.pm
@@ -38,15 +38,12 @@ package Mpslib;  # assumes Some/Module.pm
 #  @JOBSP2 - spare
 #  @JOBSP3 - spare
 
-#use strict;
-# use lib '/data/zenith217a/mankel/mgun';
-# use JobidMap;
   
   use Exporter   ();
   use vars       qw( $VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 #  use DBI;
   # set the version for version checking
-  $VERSION     = 1.00;
+  $VERSION     = 1.01;
 
   @ISA         = qw(Exporter);
   @EXPORT      = qw(
@@ -77,6 +74,8 @@ sub write_db() {
   $spare4 = "-- unused --";
   $spare5 = "-- unused --";
 
+  system "cp -p mps.db mps.db_~"; # GF: backup in case of interupt during write
+#  system "cp -p mps.db mps.db_$updateTime~"; # safest, but too many files...
   open DBFILE,">mps.db";
   printf DBFILE "%s\n",$header;
   printf DBFILE "%s\n",$batchScript;
