1332507d51b162af8b5c8b6ba70046ea6c31cb4a
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-08-12 11:09:17 +0000

    import geometry config

--- DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
+++ DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
@@ -1,22 +1,26 @@
 import FWCore.ParameterSet.Config as cms
+
 # description:
 # workflow for L1 Trigger Emulator DQM
 # used by DQM GUI: DQM/Integration/l1temulator*
 # nuno.leonardo@cern.ch 08.02
-#
+
 #global configuration
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
 #off-line
-GlobalTag.globaltag = 'IDEAL_V5::All'
+GlobalTag.globaltag = 'CRZT210_V1::All'
 GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
 #on-line
-#GlobalTag.globaltag = 'CRUZET3_V6H2::All'
-#GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG'
-#
+#GlobalTag.globaltag = 'CRZT210_V1H::All'
+#GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
+
+#add'n
+from Configuration.StandardSequences.Geometry_cff import *
+
 #unpacking
 from Configuration.StandardSequences.RawToDigi_Data_cff import *
-#
+
 #emulator/comparator
 from L1Trigger.HardwareValidation.L1HardwareValidationGR_cff import *
 from L1Trigger.Configuration.L1Config_cff import *
@@ -32,7 +36,7 @@ from DQM.L1TMonitor.L1TdeECAL_cfi import *
 from DQM.L1TMonitor.L1TdeRCT_cfi import *
 l1tderct.rctSourceData = 'gctDigis'
 l1tderct.rctSourceEmul = 'valRctDigis'
-#
+
 p = cms.Path(
     cms.SequencePlaceholder("RawToDigi")
     *cms.SequencePlaceholder("L1HardwareValidation")
