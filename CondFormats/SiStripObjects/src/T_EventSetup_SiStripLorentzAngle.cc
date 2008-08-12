ab16ac72d72374e818ac60b6a25342d57a87128a
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-08-12 10:12:09 +0000

    moved T_EventSetup_SiStripXXX in a single file T_EventSetup_SiStrip.cc

--- CondFormats/SiStripObjects/src/T_EventSetup_SiStripLorentzAngle.cc
+++
@@ -1,7 +0,0 @@
-// system include files
-
-// user include files
-#include "CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
-EVENTSETUP_DATA_REG(SiStripLorentzAngle);
--- CondFormats/SiStripObjects/src/T_EventSetup_SiStripPerformanceSummary.cc
+++
@@ -1,3 +0,0 @@
-#include "CondFormats/SiStripObjects/interface/SiStripPerformanceSummary.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-EVENTSETUP_DATA_REG(SiStripPerformanceSummary);
