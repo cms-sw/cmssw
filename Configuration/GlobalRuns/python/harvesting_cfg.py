28766cf031217d626b583809bafd860c29301001
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-08-12 19:22:32 +0000

    - fix harvesting for 2_1_2

--- Configuration/GlobalRuns/python/harvesting_cfg.py
+++ Configuration/GlobalRuns/python/harvesting_cfg.py
@@ -7,6 +7,12 @@ process = cms.Process("EDMtoMEConvert")
 process.load("DQMServices.Components.EDMtoMEConverter_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.connect = "frontier://PromptProd/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRZT210_V1P::All"
+process.prefer("GlobalTag")
+
+process.load("Geometry.EcalMapping.EcalMapping_cfi")
+process.load("Geometry.EcalMapping.EcalMappingRecord_cfi")
 
 # execute path
 #
@@ -16,17 +22,23 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 process.source = cms.Source("PoolSource",
-    dropMetaData = cms.untracked.bool(True),
+#    dropMetaData = cms.untracked.bool(True),
+    processingMode = cms.untracked.string("RunsLumisAndEvents"),
     fileNames = cms.untracked.vstring('file:reco2.root')
 )
 
-process.p1 = cms.Path(process.EDMtoMEConverter*process.DQMOffline_SecondStep_woHcal*process.dqmSaver)
 process.maxEvents.input = -1
+
+#process.source.processingMode = "Runs"
+process.source.processingMode = "RunsAndLumis"
+
 process.DQMStore.referenceFileName = ''
 process.dqmSaver.convention = 'Offline'
-process.dqmSaver.workflow = '/GlobalCruzet1-A/CMSSW_2_0_8-Testing/RECO'
+process.dqmSaver.workflow = '/GlobalCruzet3-A/CMSSW_2_1_0-Testing/RECO'
 
-process.GlobalTag.globaltag = 'CRZT210_DRV::All'
-process.prefer("GlobalTag")
+#process.DQMStore.collateHistograms = False
+process.EDMtoMEConverter.convertOnEndLumi = True
+process.EDMtoMEConverter.convertOnEndRun = False
 
+process.p1 = cms.Path(process.EDMtoMEConverter*process.DQMOffline_SecondStep*process.dqmSaver)
 
