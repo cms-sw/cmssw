6d3e478c3ded9e942e23458d47f126a86461e6e7
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-08-13 15:53:47 +0000

    for standalone testing on cosmics

---
+++ DQMOffline/EGamma/test/cosmicPhotonAnalyzer_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("TestPhotonValidator")
+
+process.load("RecoEcal.EgammaClusterProducers.geometryForClustering_cff")
+process.load("DQMOffline.EGamma.cosmicPhotonAnalyzer_cfi")
+process.load("DQMServices.Components.MEtoEDMConverter_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+DQMStore = cms.Service("DQMStore")
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5000)
+)
+
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+       "file:reco2.root"
+    )
+)
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring("keep *_MEtoEDMConverter_*_*"),
+    fileName = cms.untracked.string('photonsMEtoEDMConverter.root')
+)
+
+process.p1 = cms.Path(process.cosmicPhotonAnalysis*process.MEtoEDMConverter)
+process.outPath = cms.EndPath(process.FEVT)
