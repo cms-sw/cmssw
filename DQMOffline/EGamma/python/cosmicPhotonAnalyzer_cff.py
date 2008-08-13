6a447c041b8785bfd81b4d2709e932aa16526d0b
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-08-13 17:20:13 +0000

    dqm verbosity off

--- DQMOffline/EGamma/python/cosmicPhotonAnalyzer_cff.py
+++ DQMOffline/EGamma/python/cosmicPhotonAnalyzer_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.EGamma.cosmicPhotonAnalyzer_cfi import *
 cosmicPhotonAnalysis.OutputMEsInRootFile = cms.bool(False)
-cosmicPhotonAnalysis.Verbosity = cms.untracked.int32(1)
+cosmicPhotonAnalysis.Verbosity = cms.untracked.int32(0)
 
 egammaCosmicPhotonMonitors = cms.Sequence(cosmicPhotonAnalysis)
