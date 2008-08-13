7cad4f755db3c415d94e1ecdc05a0a1a36cfddaf
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-08-13 09:20:28 +0000

    Moved from DQMOffline/Ecal. Prametrized histogram binning

---
+++ DQMOffline/CalibCalo/plugins/SealModule.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+// The PhiSym source module
+#include "DQMOffline/CalibCalo/src/DQMSourcePhiSym.h"
+
+DEFINE_ANOTHER_FWK_MODULE(DQMSourcePhiSym);
+
