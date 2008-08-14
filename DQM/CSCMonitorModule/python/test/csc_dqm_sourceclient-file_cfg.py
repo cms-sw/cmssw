4ceb3304a907147b4b73446633814b5c4e9f19b1
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-08-14 12:36:03 +0000

    Python cfg files updated

--- DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
+++ DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
@@ -1,27 +1,77 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("DQM")
+process = cms.Process("CSCDQM")
+
+#-------------------------------------------------
+# DQM Module Configuration
+#-------------------------------------------------
+
 process.load("DQM.CSCMonitorModule.test.csc_dqm_sourceclient_cfi")
 
-process.load("DQMServices.Components.test.dqm_onlineEnv_cfi")
+#----------------------------
+# Event Source
+#-----------------------------
+
+process.source = cms.Source("PoolSource",
+     fileNames = cms.untracked.vstring('file:/home/dqmdevlocal/input/02D59D05-4151-DD11-9E79-001617DBD5AC.root')
+)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(50))
+
+#----------------------------
+# DQM Environment
+#-----------------------------
 
 process.load("DQMServices.Core.DQM_cfg")
-
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
-process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring('file:/tmp/valdo/GlobalCruzet1.00043553.0227.A.storageManager.3.0000.dat')
-)
+#process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
+#process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
+process.DQMStore.referenceFileName = '/nfshome0/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
+
+#----------------------------
+# DQM Playback Environment
+#-----------------------------
+
+process.load("DQM.Integration.test.environment_playback_cfi")
+process.dqmEnv.subSystemFolder    = "CSC"
+
+#process.DQM.collectorHost = 'pccmsdqm02.cern.ch'
+process.DQM.collectorHost = 'localhost'
+process.dqmSaver.dirName = '.'
+
+#-----------------------------
+# Magnetic Field
+#-----------------------------
+
+process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
+#-------------------------------------------------
+# GEOMETRY
+#-------------------------------------------------
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+#-------------------------------------------------
+# Global Tag
+#-------------------------------------------------
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.connect = "sqlite_file:/nfshome0/malgeri/public/globtag/CRZT210_V1H.db"
+#process.GlobalTag.connect = "frontier://FrontierDev/CMS_COND_CSC"
+process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRZT210_V1H::All"
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+#--------------------------
+# Web Service
+#--------------------------
+
+process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
+process.AdaptorConfig = cms.Service("AdaptorConfig")
+
+#--------------------------
+# Sequences
+#--------------------------
 
 process.p = cms.Path(process.dqmClient+process.dqmEnv+process.dqmSaver)
-process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
-process.DQM.collectorHost = 'srv-c2d05-16'
-process.DQM.collectorPort = 9090
-process.dqmSaver.dirName = '.'
-process.dqmEnv.subSystemFolder = 'CSC'
-process.dqmSaver.convention = 'Online'
-process.dqmSaver.producer = 'Playback'
-process.dqmSaver.saveByRun = 1
-process.dqmSaver.saveAtJobEnd = True
 
 
--- DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-playback_cfg.py
+++ DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-playback_cfg.py
@@ -2,38 +2,76 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("CSCDQM")
 
-process.load("DQM.Integration.test.inputsource_playback_cfi")
+#-------------------------------------------------
+# DQM Module Configuration
+#-------------------------------------------------
+
 process.load("DQM.CSCMonitorModule.test.csc_dqm_sourceclient_cfi")
-process.load("DQM.Integration.test.environment_playback_cfi")
+
+#----------------------------
+# Event Source
+#-----------------------------
+
+process.load("DQM.Integration.test.inputsource_playback_cfi")
+
+process.EventStreamHttpReader.consumerName = 'CSC DQM Consumer'
+
+#----------------------------
+# DQM Environment
+#-----------------------------
+
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
-#process.source = cms.Source("EventStreamHttpReader",
-#    sourceURL = cms.string('http://localhost:50082/urn:xdaq-application:lid=29'),
-#    consumerPriority = cms.untracked.string('normal'),
-#    max_event_size = cms.int32(7000000),
-#    consumerName = cms.untracked.string('CSC DQM Source'),
-#    max_queue_depth = cms.int32(5),
-#    maxEventRequestRate = cms.untracked.double(10.0),
-#    SelectEvents = cms.untracked.PSet(
-#        SelectEvents = cms.vstring('*')
-#    ),
-#    headerRetryInterval = cms.untracked.int32(3)
-#)
+#process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
+#process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
+process.DQMStore.referenceFileName = '/nfshome0/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
+
+#----------------------------
+# DQM Playback Environment
+#-----------------------------
+
+process.load("DQM.Integration.test.environment_playback_cfi")
+process.dqmEnv.subSystemFolder    = "CSC"
+
+#process.DQM.collectorHost = 'pccmsdqm02.cern.ch'
+process.DQM.collectorHost = 'localhost'
+process.dqmSaver.dirName = '.'
+
+#-----------------------------
+# Magnetic Field
+#-----------------------------
+
+process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
+#-------------------------------------------------
+# GEOMETRY
+#-------------------------------------------------
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+#-------------------------------------------------
+# Global Tag
+#-------------------------------------------------
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.connect = "sqlite_file:/nfshome0/malgeri/public/globtag/CRZT210_V1H.db"
+#process.GlobalTag.connect = "frontier://FrontierDev/CMS_COND_CSC"
+process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRZT210_V1H::All"
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+#--------------------------
+# Web Service
+#--------------------------
+
+process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
+process.AdaptorConfig = cms.Service("AdaptorConfig")
+
+#--------------------------
+# Sequences
+#--------------------------
 
 process.p = cms.Path(process.dqmClient+process.dqmEnv+process.dqmSaver)
 
-#process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
-process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
 
-process.DQM.collectorHost = 'pccmsdqm02.cern.ch'
-process.DQM.collectorPort = 9090
-
-process.dqmSaver.convention = 'Online'
-process.dqmSaver.dirName = '.'
-process.dqmSaver.producer = 'Playback'
-
-process.dqmSaver.saveByRun = 1
-process.dqmSaver.saveAtJobEnd = True
-
-process.dqmEnv.subSystemFolder = 'CSC'
