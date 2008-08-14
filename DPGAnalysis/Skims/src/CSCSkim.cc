a0764431220469e8fcf82d978502ccc8a2893a68
Michael Henry Schmitt <sha1-3781f210b057607fc6ee487d8b6ee9ae3e98f9eb@cern.ch>
2008-08-14 08:41:21 +0000

    additional skim methods for overlaps and messy events

--- DPGAnalysis/Skims/src/CSCSkim.cc
+++ DPGAnalysis/Skims/src/CSCSkim.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Michael Schmitt
 //         Created:  Sat Jul 12 17:43:33 CEST 2008
-// $Id: CSCSkim.cc,v 1.1 2008/07/30 15:32:49 schmittm Exp $
+// $Id: CSCSkim.cc,v 1.2 2008/07/30 15:57:13 rahatlou Exp $
 //
 //
 //======================================================================
@@ -55,6 +55,7 @@ CSCSkim::CSCSkim(const edm::ParameterSet& pset)
   minimumSegments         = pset.getUntrackedParameter<int>("minimumSegments",3);
   demandChambersBothSides = pset.getUntrackedParameter<bool>("demandChambersBothSides",false);
   makeHistograms          = pset.getUntrackedParameter<bool>("makeHistograms",false);
+  makeHistogramsForMessyEvents = pset.getUntrackedParameter<bool>("makeHistogramsForMessyEvebts",false);
 
   LogInfo("[CSCSkim] Setup")
     << "\n\t===== CSCSkim =====\n"
@@ -64,6 +65,7 @@ CSCSkim::CSCSkim(const edm::ParameterSet& pset)
     << "\n\t\tminimum number of segments ...............\t" << minimumSegments
     << "\n\t\tdemand chambers on both sides.............\t" << demandChambersBothSides
     << "\n\t\tmake histograms...........................\t" << makeHistograms
+    << "\n\t\t..for messy events........................\t" << makeHistogramsForMessyEvents
     << "\n\t===================\n\n";
 
 }
@@ -87,20 +89,32 @@ CSCSkim::beginJob(const edm::EventSetup&)
   nEventsSelected = 0;
   nEventsChambersBothSides = 0;
   nEventsOverlappingChambers = 0;
+  nEventsMessy = 0;
   iRun = 0;
   iEvent = 0;
 
-  if (makeHistograms) {
-  
+  if (makeHistograms || makeHistogramsForMessyEvents) {
+
     // Create the root file for the histograms
     theHistogramFile = new TFile(histogramFileName.c_str(), "RECREATE");
     theHistogramFile->cd();
-    
-    // book histograms for the skimming module
-    hxnRecHits     = new TH1F("hxnRecHits","n RecHits",61,-0.5,60.5);
-    hxnSegments    = new TH1F("hxnSegments","n Segments",11,-0.5,10.5);
-    hxnHitChambers = new TH1F("hxnHitsChambers","n chambers with hits",11,-0.5,10.5);
-    hxnRecHitsSel  = new TH1F("hxnRecHitsSel","n RecHits selected",61,-0.5,60.5);
+
+    if (makeHistograms) {
+      // book histograms for the skimming module
+      hxnRecHits     = new TH1F("hxnRecHits","n RecHits",61,-0.5,60.5);
+      hxnSegments    = new TH1F("hxnSegments","n Segments",11,-0.5,10.5);
+      hxnHitChambers = new TH1F("hxnHitsChambers","n chambers with hits",11,-0.5,10.5);
+      hxnRecHitsSel  = new TH1F("hxnRecHitsSel","n RecHits selected",61,-0.5,60.5);
+    }
+    if (makeHistogramsForMessyEvents) {
+      // book histograms for the messy event skimming module
+      mevnRecHits0     = new TH1F("mevnRecHits0","n RecHits",121,-0.5,120.5);
+      mevnChambers0    = new TH1F("mevnChambers0","n chambers with hits",21,-0.5,20.5);
+      mevnSegments0    = new TH1F("mevnSegments0","n Segments",21,-0.5,20.5);
+      mevnRecHits1     = new TH1F("mevnRecHits1","n RecHits",100,0.,300.);
+      mevnChambers1    = new TH1F("mevnChambers1","n chambers with hits",50,0.,50.);
+      mevnSegments1    = new TH1F("mevnSegments1","n Segments",30,0.,30.);
+    }
 
   }
 
@@ -124,22 +138,30 @@ CSCSkim::endJob() {
     << "\t\tevents selected ..............\t" << nEventsSelected << "\tfraction= " << fraction << endl
     << "\t\tevents chambers both sides ...\t" << nEventsChambersBothSides << "\n"
     << "\t\tevents w/ overlaps .......... \t" << nEventsOverlappingChambers << "\n"
+    << "\t\tevents lots of hit chambers . \t" << nEventsMessy << "\n"
     <<     "\t=========================================================================\n\n";
 
 
-  if (makeHistograms) {
-
+  if (makeHistograms || makeHistogramsForMessyEvents) {
     // Write the histos to file
     LogDebug("[CSCSkim]") << "======= write out my histograms ====\n" ;
     theHistogramFile->cd();
-    hxnRecHits->Write();
-    hxnSegments->Write();
-    hxnHitChambers->Write();
-    hxnRecHitsSel->Write();
+    if (makeHistograms) {
+      hxnRecHits->Write();
+      hxnSegments->Write();
+      hxnHitChambers->Write();
+      hxnRecHitsSel->Write();
+    }
+    if (makeHistogramsForMessyEvents) {
+      mevnRecHits0->Write();
+      mevnChambers0->Write();
+      mevnSegments0->Write();
+      mevnRecHits1->Write();
+      mevnChambers1->Write();
+      mevnSegments1->Write();
+    }
     theHistogramFile->Close();
-
   }
-
 }
 
 
@@ -178,10 +200,18 @@ CSCSkim::filter(edm::Event& event, const edm::EventSetup& eventSetup)
     if (goodOverlapEvent) {nEventsOverlappingChambers++;}
   }
 
+  // messy events skim
+  bool messyEvent = false;
+  if (typeOfSkim == 4) {
+    messyEvent = doMessyEventSkimming(cscRecHits,cscSegments);
+    if (messyEvent) {nEventsMessy++;}
+  }
+
   // set filter flag
   bool selectThisEvent = false;
   if (typeOfSkim == 1 || typeOfSkim == 2) {selectThisEvent = basicEvent;}
   if (typeOfSkim == 3) {selectThisEvent = goodOverlapEvent;}
+  if (typeOfSkim == 4) {selectThisEvent = messyEvent;}
 
   if (selectThisEvent) {nEventsSelected++;}
 
@@ -330,7 +360,7 @@ bool CSCSkim::doOverlapSkimming(edm::Handle<CSCSegmentCollection> cscSegments){
   const int nAllMaximum  = 3;
 
   // how many Segments?
-  int nSegments = cscSegments->size();
+  //  int nSegments = cscSegments->size();
 
   // zero arrays
   int nAll[600];
@@ -397,6 +427,138 @@ bool CSCSkim::doOverlapSkimming(edm::Handle<CSCSegmentCollection> cscSegments){
 
   return selectThisEvent;
 
+}
+
+//--------------------------------------------------
+//
+// This module selects events with a large numbere
+// of recHits and larger number of chambers with hits.
+//
+//--------------------------------------------------
+bool CSCSkim::doMessyEventSkimming(edm::Handle<CSCRecHit2DCollection> cscRecHits, edm::Handle<CSCSegmentCollection> cscSegments){
+
+  // how many RecHits in the collection?
+  int nRecHits = cscRecHits->size();
+
+  // zero the recHit counter
+  int cntRecHit[600];
+  for (int i = 0; i < 600; i++) {
+    cntRecHit[i] = 0;
+  }
+
+  // ---------------------
+  // Loop over rechits 
+  // ---------------------
+
+  CSCRecHit2DCollection::const_iterator recIt;
+  for (recIt = cscRecHits->begin(); recIt != cscRecHits->end(); recIt++) {
+
+    // which chamber is it?
+    CSCDetId idrec = (CSCDetId)(*recIt).cscDetId();
+    int kEndcap  = idrec.endcap();
+    int kRing    = idrec.ring();
+    int kStation = idrec.station();
+    int kChamber = idrec.chamber();
+    int kLayer   = idrec.layer();
+
+    // compute chamber serial number
+    int kSerial = chamberSerial( kEndcap, kStation, kRing, kChamber ) ;
+
+    // increment recHit counter
+    //     (each layer is represented by a different power of 10)
+    int kDigit = (int) pow((float)10.,(float)(kLayer-1));
+    cntRecHit[kSerial] += kDigit;
+
+  } //end rechit loop
+
+
+  // ------------------------------------------------------
+  // Are there chambers with the minimum number of hits?
+  // ------------------------------------------------------
+
+  int nChambersWithMinimalHits = 0;
+  int nChambersWithMinimalHitsPOS = 0;
+  int nChambersWithMinimalHitsNEG = 0;
+  if (nRecHits > 0) {
+    for (int i = 0; i < 600; i++) {
+      if (cntRecHit[i] > 0) {
+	int nLayersWithHits = 0;
+	float dummy = (float) cntRecHit[i];
+	for (int j = 5; j > -1; j--) {
+	  float digit = dummy / pow( (float)10., (float)j );
+	  int kCount = (int) digit;
+	  if (kCount > 0) nLayersWithHits++;
+	  dummy = dummy - ( (float) kCount) * pow( (float)10., (float)j );
+	}
+	if (nLayersWithHits > nLayersWithHitsMinimum) {
+	  if (i < 300) {nChambersWithMinimalHitsPOS++;}
+	  else         {nChambersWithMinimalHitsNEG++;}
+	}
+      }
+    }
+    nChambersWithMinimalHits = nChambersWithMinimalHitsPOS + nChambersWithMinimalHitsNEG;
+  }
+
+  // how many Segments?
+  int nSegments = cscSegments->size();
+
+  // ----------------------
+  // fill histograms
+  // ----------------------
+
+  if (makeHistogramsForMessyEvents) {
+    if (nRecHits > 8) {
+      mevnRecHits0->Fill(nRecHits);
+      mevnChambers0->Fill(nChambersWithMinimalHits);
+      mevnSegments0->Fill(nSegments);
+    }
+    if (nRecHits > 54) {
+      double dummy = (double) nRecHits;
+      if (dummy > 299.9) dummy = 299.9;
+      mevnRecHits1->Fill(dummy);
+      dummy = (double) nChambersWithMinimalHits;
+      if (dummy > 49.9) dummy = 49.9;
+      mevnChambers1->Fill(dummy);
+      dummy = (double) nSegments;
+      if (dummy > 29.9) dummy = 29.9;
+      mevnSegments1->Fill(dummy);
+    }
+  }
+
+  // ----------------------
+  // set the filter flag
+  // ----------------------
+
+  bool selectEvent = false;
+  if ( (nRecHits > 54) && (nChambersWithMinimalHits > 5) ) {selectEvent = true;}
+
+  // debug
+  LogDebug("[CSCSkim]") << "----- nRecHits = " << nRecHits
+		       << "\tnChambersWithMinimalHits = " << nChambersWithMinimalHits
+		       << "\tnSegments = " << nSegments 
+		       << "\tselect? " << selectEvent << endl;
+
+  /*
+  if (selectEvent) {
+    cout << "\n==========================================================================\n"
+	 << "\tmessy event!\n"
+	 << "\t  " <<  nEventsAnalyzed
+	 << "\trun " << iRun << "\tevent " << iEvent << endl;
+    cout << "----- nRecHits = " << nRecHits
+	 << "\tnChambersWithMinimalHits = " << nChambersWithMinimalHits
+	 << "\tnSegments = " << nSegments 
+	 << "\tselect? " << selectEvent << endl;
+    for (int i = 0; i < 600; i++) {
+      if (cntRecHit[i] > 0) {
+	cout << "\t\t" << i << "\tcntRecHit= " << cntRecHit[i] << endl;
+      }
+    }
+    cout << "==========================================================================\n\n" ;
+  }
+  */
+
+
+  return selectEvent;
 }
 
 
