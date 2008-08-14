87d7c69b98295bbb4fa70069948811b62426553a
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-08-14 09:24:51 +0000

    added pi0 calib

--- DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
+++ DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 import DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi
+import DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi
+
 ALCARECOEcalCalPhisymDQM = DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi.EcalPhiSymMon.clone()
 
-
+ALCARECOEcalCalPi0CalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0Mon.clone()
