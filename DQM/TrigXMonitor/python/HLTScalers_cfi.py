31e772e679c944c22a315d8d0273cce97d1e6ab1
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-08-15 15:44:01 +0000

    translated new config

--- DQM/TrigXMonitor/python/HLTScalers_cfi.py
+++ DQM/TrigXMonitor/python/HLTScalers_cfi.py
@@ -1,8 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 # HLT scalers. wittich 11/07
-# $Id: HLTScalers.cfi,v 1.3 2007/12/04 20:24:32 wittich Exp $
+# $Id: HLTScalers.cfi,v 1.4 2008/08/01 14:38:11 bjbloom Exp $
 # $Log: HLTScalers.cfi,v $
+# Revision 1.4  2008/08/01 14:38:11  bjbloom
+# Example of specifying cross-correlated paths
+#
 # Revision 1.3  2007/12/04 20:24:32  wittich
 # - make hlt histograms variable width depending on path
 # - add strings for path names
@@ -16,9 +19,13 @@ import FWCore.ParameterSet.Config as cms
 # - remove PathTimerService from cfg file in test subdir
 #
 hlts = cms.EDFilter("HLTScalers",
-    triggerResults = cms.InputTag("TriggerResults","","HLT"),
+    #    untracked bool specifyPaths = true
+    #    untracked vstring pathNames = {'HLT1MuonIso',
+    #  				'HLT1MuonNonIso',
+    #  				'HLT2MuonIso',
+    #  				'HLT2MuonNonIso'}
+    triggerResults = cms.InputTag("TriggerResults","","FU"),
     l1GtData = cms.InputTag("l1GtUnpack","","HLT"),
     verbose = cms.untracked.bool(False)
 )
-
 
