35326bf9d19fe254987a9f97de1a2007c28e2dbf
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-08-15 22:02:38 +0000

    Add L1GtTriggerMaskVeto*Trig.

--- CondTools/L1Trigger/plugins/SealModule.cc
+++ CondTools/L1Trigger/plugins/SealModule.cc
@@ -227,11 +227,18 @@ REGISTER_L1_WRITER(L1GtStableParametersRcd, L1GtStableParameters);
 #include "CondFormats/L1TObjects/interface/L1GtTriggerMask.h"
 #include "CondFormats/DataRecord/interface/L1GtTriggerMaskAlgoTrigRcd.h"
 #include "CondFormats/DataRecord/interface/L1GtTriggerMaskTechTrigRcd.h"
+#include "CondFormats/DataRecord/interface/L1GtTriggerMaskVetoAlgoTrigRcd.h"
+#include "CondFormats/DataRecord/interface/L1GtTriggerMaskVetoTechTrigRcd.h"
 
 REGISTER_PLUGIN(L1GtTriggerMaskAlgoTrigRcd, L1GtTriggerMask);
 REGISTER_L1_WRITER(L1GtTriggerMaskAlgoTrigRcd, L1GtTriggerMask);
 REGISTER_PLUGIN(L1GtTriggerMaskTechTrigRcd, L1GtTriggerMask);
 REGISTER_L1_WRITER(L1GtTriggerMaskTechTrigRcd, L1GtTriggerMask);
+
+REGISTER_PLUGIN(L1GtTriggerMaskVetoAlgoTrigRcd, L1GtTriggerMask);
+REGISTER_L1_WRITER(L1GtTriggerMaskVetoAlgoTrigRcd, L1GtTriggerMask);
+REGISTER_PLUGIN(L1GtTriggerMaskVetoTechTrigRcd, L1GtTriggerMask);
+REGISTER_L1_WRITER(L1GtTriggerMaskVetoTechTrigRcd, L1GtTriggerMask);
 
 #include "CondFormats/L1TObjects/interface/L1GtTriggerMenu.h"
 #include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
