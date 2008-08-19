d0975e8bdae63163531e82bfe241f2f635fed016
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-08-19 17:32:13 +0000

    Use Taylan's HPDNoiseLibraryReader

--- SimCalorimetry/HcalSimAlgos/interface/HPDNoiseGenerator.h
+++ SimCalorimetry/HcalSimAlgos/interface/HPDNoiseGenerator.h
@@ -2,19 +2,22 @@
 #define HcalSimAlgos_HPDNoiseGenerator_h
 
 #include "SimCalorimetry/CaloSimAlgos/interface/CaloVNoiseSignalGenerator.h"
+#include "SimCalorimetry/HcalSimAlgos/interface/HPDNoiseLibraryReader.h"
+#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
 #include <vector>
 class HcalSimParameterMap;
 
 class HPDNoiseGenerator : public CaloVNoiseSignalGenerator
 {
 public:
-  HPDNoiseGenerator(const HcalSimParameterMap * parameterMap);
+  HPDNoiseGenerator(const edm::ParameterSet & pset, const HcalSimParameterMap * parameterMap);
+  virtual ~HPDNoiseGenerator() {}
 
   void getNoiseSignals(std::vector<CaloSamples> & result);
 
 private:
   void fC2pe(CaloSamples & samples) const;
-
+  HPDNoiseLibraryReader theLibraryReader;
   const HcalSimParameterMap * theParameterMap;
 };
 
--- SimCalorimetry/HcalSimAlgos/src/HPDNoiseGenerator.cc
+++ SimCalorimetry/HcalSimAlgos/src/HPDNoiseGenerator.cc
@@ -1,15 +1,28 @@
 #include "SimCalorimetry/HcalSimAlgos/interface/HPDNoiseGenerator.h"
 #include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h"
 
-HPDNoiseGenerator::HPDNoiseGenerator(const HcalSimParameterMap * parameterMap)
-: theParameterMap(parameterMap)
+HPDNoiseGenerator::HPDNoiseGenerator(const edm::ParameterSet & pset, const HcalSimParameterMap * parameterMap)
+: theLibraryReader(pset),
+  theParameterMap(parameterMap)
 {
 }
 
 
 void HPDNoiseGenerator::getNoiseSignals(std::vector<CaloSamples> & result)
 {
-  // result should come back in units of photoelectrons
+  std::vector<std::pair <HcalDetId, const float* > > noise = theLibraryReader.getNoisyHcalDetIds();
+  for(std::vector<std::pair <HcalDetId, const float* > >::const_iterator noiseItr = noise.begin();
+      noiseItr != noise.end(); ++noiseItr)
+  {
+    CaloSamples newSamples(noiseItr->first, 10);
+    for(unsigned i = 0; i < 10; ++i)
+    {
+      newSamples[i] = (noiseItr->second)[i];
+    }
+    // result should come back in units of photoelectrons
+    fC2pe(newSamples);
+    result.push_back(newSamples);
+  }
 }
 
 void HPDNoiseGenerator::fC2pe(CaloSamples & samples) const
