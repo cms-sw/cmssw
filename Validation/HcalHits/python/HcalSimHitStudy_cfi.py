a0c5c9823fe75db5446e070432f8dde668b5eb45
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2008-08-19 15:48:46 +0000

    more cleaning:  OutputFile to outputFile

--- Validation/HcalHits/python/HcalSimHitStudy_cfi.py
+++ Validation/HcalHits/python/HcalSimHitStudy_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 hcalSimHitStudy = cms.EDFilter("HcalSimHitStudy",
     ModuleLabel = cms.untracked.string('g4SimHits'),
-    OutputFile = cms.untracked.string('valid_HB.root'),
+    outputFile = cms.untracked.string('valid_HB.root'),
     Verbose = cms.untracked.bool(True),
     HitCollection = cms.untracked.string('HcalHits')
 )
