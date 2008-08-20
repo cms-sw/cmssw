1715d270b4bdc2768fb5f365023beb5a8f3d0231
Andreas Oehler <sha1-bf17a625e96eed2dfa3f3b53d79864d479019360@cern.ch>
2008-08-20 16:10:09 +0000

    switch to common jetPtMin usage - cut on final jet pt.

--- RecoJets/JetProducers/python/GenJetParameters_cfi.py
+++ RecoJets/JetProducers/python/GenJetParameters_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 # Standard CalooJets parameters
-# $Id: GenJetParameters_cfi.py,v 1.2 2008/04/21 03:28:21 rpw Exp $
+# $Id: GenJetParameters_cfi.py,v 1.3 2008/08/20 15:51:08 oehler Exp $
 GenJetParameters = cms.PSet(
     src = cms.InputTag("genParticlesForJets"),
     verbose = cms.untracked.bool(False),
-    jetPtMin = cms.double(15.0),
+    jetPtMin = cms.double(5.0),
     inputEtMin = cms.double(0.0),
     jetType = cms.untracked.string('GenJet'),
     inputEMin = cms.double(0.0)
