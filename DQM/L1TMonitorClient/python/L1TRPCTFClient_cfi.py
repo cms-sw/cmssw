ea2a354379b34fcca9ee89144a4c50d0ab5e0409
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2008-08-21 07:27:41 +0000

    Fix wrong output directory

--- DQM/L1TMonitorClient/python/L1TRPCTFClient_cfi.py
+++ DQM/L1TMonitorClient/python/L1TRPCTFClient_cfi.py
@@ -4,7 +4,7 @@ l1trpctfClient = cms.EDFilter("L1TRPCTFClient",
     input_dir = cms.untracked.string('L1T/L1TRPCTF'),
     prescaleEvt = cms.untracked.int32(1),
     verbose = cms.untracked.bool(False),
-    output_dir = cms.untracked.string('L1T/L1TRPCTFClient')
+    output_dir = cms.untracked.string('L1T/L1TRPCTF/Client')
 )
 
 
