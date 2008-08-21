89b4583534425e8a79515cfb749de7af57e240ef
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2008-08-21 15:50:43 +0000

    bug fix

--- DQM/RPCMonitorClient/python/RPCMon_SS_Dbx_Global_cfi.py
+++ DQM/RPCMonitorClient/python/RPCMon_SS_Dbx_Global_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 rpcAfterPulse = cms.EDAnalyzer("RPCMon_SS_Dbx_Global",
    GlobalHistogramsFolder = cms.untracked.string('RPC/RecHits/SummaryHistogram'),
    RootFileName =  cms.untracked.string('out.root'),
-   SaveRootFile = cms.untracked.bool(false)
+   SaveRootFile = cms.untracked.bool(False)
 )
