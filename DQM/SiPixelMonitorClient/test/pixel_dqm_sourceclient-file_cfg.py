f1946fababe8fd898616c54f15809a3f340fd397
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-08-21 14:56:36 +0000

    took RecHit reco and source back out due to config problems. This only affects the pixel internal shifter script.

--- DQM/SiPixelMonitorClient/test/pixel_dqm_sourceclient-file_cfg.py
+++ DQM/SiPixelMonitorClient/test/pixel_dqm_sourceclient-file_cfg.py
@@ -9,13 +9,15 @@ process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
 
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
+##process.load("Configuration.StandardSequences.Reconstruction_cff")
+
 process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
 process.siPixelDigis.InputLabel = 'source'
 process.siPixelDigis.IncludeErrors = True
 
 process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
 
-process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
+#process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
 
 process.load("DQM.SiPixelMonitorRawData.SiPixelMonitorRawData_cfi")
 process.SiPixelRawDataErrorSource.saveFile = False
@@ -71,27 +73,11 @@ process.source = cms.Source("PoolSource",
     debugVebosity = cms.untracked.uint32(1),
     #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/c/chiochia/cmssw/Muon_FullValidation_150pre3.root')
     #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/2008/6/6/RelVal-RelValTTbar-1212531852-IDEAL_V1-2nd-02/0000/081018D5-EC33-DD11-A623-000423D6CA42.root')
-    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/2CB57036-4C6A-DD11-9A04-000423D94990.root',
-    #                                  'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/7EB80A4C-4C6A-DD11-A589-000423D987E0.root',
-    #                                  'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B0319435-4C6A-DD11-9B18-000423D952C0.root',
-    #                                  'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B428FF51-4C6A-DD11-87D8-000423D986C4.root',
-    #                                  'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/DEAB1B33-4C6A-DD11-B83D-000423D99BF2.root'
-#				      )
-#    fileNames = cms.untracked.vstring(
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/2267B14D-4C6A-DD11-BEFE-001617C3B70E.root',
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/2A16B54C-4C6A-DD11-9612-001617C3B6E2.root',
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/32B8FC50-4C6A-DD11-BCDA-000423D944DC.root'
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/3EC4E22A-4C6A-DD11-B533-000423D94C68.root',
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/48232F48-4C6A-DD11-84AE-000423D996B4.root',
-                                      #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/741/6016DF32-4C6A-DD11-AE83-000423D94A04.root'
-	#			      )
-    fileNames = cms.untracked.vstring(
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root',
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root',
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root',
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root',
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root',
-                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/774/2260AF5F-356E-DD11-99F3-000423D6C8E6.root'
+    fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/2CB57036-4C6A-DD11-9A04-000423D94990.root',
+                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/7EB80A4C-4C6A-DD11-A589-000423D987E0.root',
+                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B0319435-4C6A-DD11-9B18-000423D952C0.root',
+                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B428FF51-4C6A-DD11-87D8-000423D986C4.root',
+                                      'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/DEAB1B33-4C6A-DD11-B83D-000423D99BF2.root'
 				      )
 )
 
@@ -130,14 +116,14 @@ process.LockService = cms.Service("LockService",
     labels = cms.untracked.vstring('source')
 )
 
-process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters*siPixelRecHits)
+process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters)
 process.RAWmonitor = cms.Sequence(process.SiPixelRawDataErrorSource)
 process.DIGImonitor = cms.Sequence(process.SiPixelDigiSource)
 process.CLUmonitor = cms.Sequence(process.SiPixelClusterSource)
 process.HITmonitor = cms.Sequence(process.SiPixelRecHitSource)
 #process.DQMmodules = cms.Sequence(process.qTester*process.dqmEnv*process.dqmSaver)
 process.DQMmodules = cms.Sequence(process.dqmEnv*process.dqmSaver)
-process.p = cms.Path(process.Reco*process.DQMmodules*process.RAWmonitor*process.DIGImonitor*process.CLUmonitor*process.HITmonitor*process.sipixelEDAClient)
+process.p = cms.Path(process.Reco*process.DQMmodules*process.RAWmonitor*process.DIGImonitor*process.CLUmonitor*process.sipixelEDAClient)
 #process.p = cms.Path(process.DQMmodules*process.DIGImonitor*process.sipixelEDAClient)
 process.DQM.collectorHost = ''
 process.dqmSaver.convention = 'Online'
