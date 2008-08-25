b62527f4dc8abc139d65753c013bd9c1058b373b
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-08-25 11:36:00 +0000

    Move AlCaReco stream definitions for AlCaRaws into separate files (from AlCaRecoStreams_cff)

---
+++ Configuration/StandardSequences/python/AlCaRecoStream_EcalCalPi0Calib_cff.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+# last update: $Date$ by $Author$
+
+# ECAL calibration with pi0
+from Calibration.EcalAlCaRecoProducers.ALCARECOEcalCalPi0Calib_cff import *
+
+from DQMOffline.Configuration.AlCaRecoDQM_cff import *
+
+pathALCARECOEcalCalPi0Calib = cms.Path(seqALCARECOEcalCalPi0Calib*ALCARECOEcalCalPi0CalibDQM)
+
+from Configuration.EventContent.AlCaRecoOutput_cff import *
+
+ALCARECOStreamEcalCalPi0Calib = cms.FilteredStream(
+	responsible = 'Vladimir Litvine',
+	name = 'ALCARECOEcalCalPi0Calib',
+	paths  = (pathALCARECOEcalCalPi0Calib),
+	content = OutALCARECOEcalCalPi0Calib.outputCommands,
+	selectEvents = OutALCARECOEcalCalPi0Calib.SelectEvents,
+	dataTier = cms.untracked.string('ALCARECO')
+	)
