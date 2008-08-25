6c732f4dcb73561d796fb340587caf17c37122a0
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-08-25 15:57:39 +0000

    - remove EGamma, use new EB+EE sequence

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -8,6 +8,9 @@ from DQMOffline.JetMET.jetMETAnalyzer_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
 #from DQMOffline.Trigger.FourVectorHLTOffline_cfi import *
 from DQMOffline.Trigger.L1TMonitor_dqmoffline_cff import *
+#from DQMOffline.EGamma.cosmicPhotonAnalyzer_cff import *
 
-DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline7*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*l1tmonitor*siPixelOfflineDQM_source)
+DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*l1tmonitor*siPixelOfflineDQM_source)
+
+#DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*l1tmonitor*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
 
