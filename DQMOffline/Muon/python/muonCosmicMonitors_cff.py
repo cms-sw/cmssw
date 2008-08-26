d72eea390ced79f8cba4c0af6b75c0ea95770cae
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-08-26 11:02:17 +0000

    added RPC monitoring

--- DQMOffline/Muon/python/muonCosmicMonitors_cff.py
+++ DQMOffline/Muon/python/muonCosmicMonitors_cff.py
@@ -6,20 +6,22 @@ from DQM.TrackingMonitor.MonitorTrackSTACosmicMuonsHLT_cff import *
 from DQM.TrackingMonitor.MonitorTrackGLBCosmicMuons_cfi import *
 from DQM.TrackingMonitor.MonitorTrackTKCosmicMuons_cfi import *
 from DQM.DTMonitorModule.dtSegmTask_cfi import *
+from DQMOffline.Muon.rpcSource_cfi import *
 #dedicated analyzers for offline dqm
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
 from DQMOffline.Muon.muonCosmicAnalyzer_cfi import *
 from DQMOffline.Muon.CSCMonitor_cfi import *
 import DQMOffline.Muon.muonAnalyzer_cfi
 muonStandAloneCosmicAnalyzer = DQMOffline.Muon.muonAnalyzer_cfi.muonAnalyzer.clone()
-#dedicated clients for offline dqm 
+#dedicated clients for offline dqm
+from DQMOffline.Muon.rpcClient_cfi import *
 from DQMOffline.Muon.muonQualityTests_cff import *
 muonTrackCosmicAnalyzers = cms.Sequence(standAloneCosmicMuonsMonitors*MonitorTrackTKCosmicMuons*MonitorTrackGLBCosmicMuons)
 muonTrackCosmicAnalyzersHLT = cms.Sequence(MonitorTrackSTACosmicMuonsHLTDT*MonitorTrackSTACosmicMuonsHLTCSC)
-muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*cscMonitor*muonCosmicAnalyzer)
-muonCosmicMonitors_woCSC = cms.Sequence(cms.SequencePlaceholder("muonTrackAnalyzers")*dtSegmentsMonitor*muonCosmicMonitors)
-muonStandAloneCosmicMonitors = cms.Sequence(MonitorTrackSTACosmicMuons*dtSegmentsMonitor*cscMonitor*muonStandAloneCosmicAnalyzer)
-muonCosmicMonitorsAndQualityTests = cms.Sequence(muonCosmicMonitors*muonQualityTests)
+muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*rpcSource*cscMonitor*muonCosmicAnalyzer)
+muonCosmicMonitors_woCSC = cms.Sequence(cms.SequencePlaceholder("muonTrackAnalyzers")*dtSegmentsMonitor*rpcSource*muonCosmicMonitors)
+muonStandAloneCosmicMonitors = cms.Sequence(MonitorTrackSTACosmicMuons*dtSegmentsMonitor*cscMonitor*rpcSource*muonStandAloneCosmicAnalyzer)
+muonCosmicMonitorsAndQualityTests = cms.Sequence(muonCosmicMonitors*rpcClient*muonQualityTests)
 muonStandAloneCosmicAnalyzer.DoMuonRecoAnalysis = False
 
 
