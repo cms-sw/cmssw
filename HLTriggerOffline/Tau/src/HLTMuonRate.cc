43dd279918b552ad0c58488051b39947956d61f9
Sho Maruyama <sha1-b47564b910de3cc3cd657a4905f52d93b10eea83@cern.ch>
2008-08-27 21:13:47 +0000

    header clean-up

--- HLTriggerOffline/Tau/src/HLTMuonRate.cc
+++ HLTriggerOffline/Tau/src/HLTMuonRate.cc
@@ -10,7 +10,6 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/L1Trigger/interface/L1MuonParticleFwd.h"
 #include "DataFormats/L1Trigger/interface/L1MuonParticle.h"
