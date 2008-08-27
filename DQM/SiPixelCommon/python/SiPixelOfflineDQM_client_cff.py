4ebdec653a13b3eac95cccf88191ddaab726ae29
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-08-27 08:06:23 +0000

    update for Tier-0 processing

--- DQM/SiPixelCommon/python/SiPixelOfflineDQM_client_cff.py
+++ DQM/SiPixelCommon/python/SiPixelOfflineDQM_client_cff.py
@@ -1,19 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-# DQM Services:
-from DQMServices.Components.DQMEnvironment_cfi import *
-dqmSaver.convention = 'Offline'
-dqmSaver.dirName = '.'
-dqmSaver.producer = 'DQM'
-dqmEnv.subSystemFolder = 'Pixel'
-dqmSaver.saveByLumiSection = -1
-dqmSaver.saveByRun = 1
-dqmSaver.saveAtJobEnd = True
-
 sipixelEDAClient = cms.EDFilter("SiPixelEDAClient",
     EventOffsetForInit = cms.untracked.int32(10),
     ActionOnLumiSection = cms.untracked.bool(False),
-    ActionOnRunEnd = cms.untracked.bool(True)
+    ActionOnRunEnd = cms.untracked.bool(True),
+    SummaryXMLFileName = cms.untracked.string('DQM/SiPixelMonitorClient/test/sipixel_offline_config.xml')
 )
 
-PixelOfflineDQMClient = cms.Sequence(sipixelEDAClient*dqmEnv*dqmSaver)
+PixelOfflineDQMClient = cms.Sequence(sipixelEDAClient)
