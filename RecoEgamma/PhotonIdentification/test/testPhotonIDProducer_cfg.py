98d8cb40ee326391bf0d4ad77d5c7f46962ff860
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2008-08-28 18:41:00 +0000

    Added some simple variables passed to the analyzer

--- RecoEgamma/PhotonIdentification/test/testPhotonIDProducer_cfg.py
+++ RecoEgamma/PhotonIdentification/test/testPhotonIDProducer_cfg.py
@@ -11,7 +11,8 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213986417-IDEAL_V2-2nd/0004/443BCAED-CB40-DD11-AB37-000423D6B48C.root')
+    #fileNames = cms.untracked.vstring('/store/relval/2008/6/22/RelVal-RelValSingleGammaPt35-1213986417-IDEAL_V2-2nd/0004/443BCAED-CB40-DD11-AB37-000423D6B48C.root')
+    fileNames = cms.untracked.vstring('dcap://cmsdcap.hep.wisc.edu:22125/pnfs/hep.wisc.edu/store/user/mbanderson/PhotonJet20-200/PhotonJet20-200-0000.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
@@ -30,7 +31,16 @@ process.Out = cms.OutputModule("PoolOutputModule",
 )
 
 process.photonIDAna = cms.EDAnalyzer("PhotonIDSimpleAnalyzer",
-    outputFile = cms.string('PhoIDHists.root')
+    outputFile  = cms.string('PhoIDHists.root'),
+
+    # Variables that must be passed before a photon candidate (a SuperCluster)
+    #  gets placed into the histograms.  Basic, simple cuts.
+    # Minimum Et
+    minPhotonEt     = cms.double(10.0),
+    # Minimum R9 = E(3x3) / E(SuperCluster)
+    minPhotonR9     = cms.double(0.6),
+    # Maximum HCAL / ECAL Energy
+    maxPhotonHoverE = cms.double(0.2)
 )
 
 process.p = cms.Path(process.photonIDSequence*process.photonIDAna)
