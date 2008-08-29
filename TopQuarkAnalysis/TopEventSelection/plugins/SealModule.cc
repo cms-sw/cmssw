6fe2ee170fb28eddba33116aadbdf4bc58b871fd
mrenz <sha1-ef64296b7d9a5f151fc02aa53111261c1c9f6b77@cern.ch>
2008-08-29 14:36:02 +0000

    MVA Trainer and Computer

---
+++ TopQuarkAnalysis/TopEventSelection/plugins/SealModule.cc
@@ -0,0 +1,9 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "TopQuarkAnalysis/TopEventSelection/plugins/TtSemiLepSignalSelMVAComputer.h"
+#include "TopQuarkAnalysis/TopEventSelection/plugins/TtSemiLepSignalSelMVATrainer.h"
+
+// define mva modules
+DEFINE_FWK_MODULE(TtSemiLepSignalSelMVAComputer);
+DEFINE_FWK_MODULE(TtSemiLepSignalSelMVATrainer);
+
