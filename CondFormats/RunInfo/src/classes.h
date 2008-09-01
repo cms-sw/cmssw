2f909e227732407ce558fdda76f6a5254280a670
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-09-01 10:38:10 +0000

    added l1triggerscaler

--- CondFormats/RunInfo/src/classes.h
+++ CondFormats/RunInfo/src/classes.h
@@ -1,4 +1,6 @@
 #include "CondFormats/RunInfo/interface/RunNumber.h"
+#include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
 namespace {
-  std::vector<RunNumber::Item>::iterator tmp0;
+  std::vector<runinfo_test::RunNumber::Item>::iterator tmp0;
+  std::vector<L1TriggerScaler::Lumi>::iterator tmp1;
 }
