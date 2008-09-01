b89eb4395132eea388e882217f6a008be3ac2c38
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2008-09-01 21:19:22 +0000

    Un-mangle beam-halo AlCaRecos after Pythonization

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlBeamHaloOverlaps_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlBeamHaloOverlaps_cff.py
@@ -1,21 +1,36 @@
+# AlCaReco for muon based alignment using beam-halo muons in the CSC overlap regions
+
 import FWCore.ParameterSet.Config as cms
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
-# AlCaReco for muon based alignment using beam-halo muons in the CSC overlap regions
 ALCARECOMuAlBeamHaloOverlapsHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+ALCARECOMuAlBeamHaloOverlapsHLT.HLTPaths = ['HLT_CSCBeamHaloOverlapRing1', 'HLT_CSCBeamHaloOverlapRing2']
+
 from Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
+
 import RecoLocalMuon.CSCSegment.cscSegments_cfi
 cscSegmentsALCARECOBHO = RecoLocalMuon.CSCSegment.cscSegments_cfi.cscSegments.clone()
+cscSegmentsALCARECOBHO.algo_type = 4 # Choice of the building algo: 1 SK, 2 TC, 3 DF, 4 ST, ...
+cscSegmentsALCARECOBHO.inputObjects = 'hltCsc2DRecHits'
+
 import RecoMuon.MuonSeedGenerator.CosmicMuonSeedProducer_cfi
 CosmicMuonSeedALCARECOBHO = RecoMuon.MuonSeedGenerator.CosmicMuonSeedProducer_cfi.CosmicMuonSeed.clone()
+CosmicMuonSeedALCARECOBHO.EnableDTMeasurement = False
+CosmicMuonSeedALCARECOBHO.CSCRecSegmentLabel = 'cscSegmentsALCARECOBHO'
+
 import RecoMuon.CosmicMuonProducer.cosmicMuons_cfi
 cosmicMuonsALCARECOBHO = RecoMuon.CosmicMuonProducer.cosmicMuons_cfi.cosmicMuons.clone()
+cosmicMuonsALCARECOBHO.MuonSeedCollectionLabel = 'CosmicMuonSeedALCARECOBHO'
+cosmicMuonsALCARECOBHO.TrajectoryBuilderParameters.EnableDTMeasurement = False
+cosmicMuonsALCARECOBHO.TrajectoryBuilderParameters.CSCRecSegmentLabel = 'cscSegmentsALCARECOBHO'
+
+reconstructAsCosmicMuonsALCARECOBHO = cms.Sequence(cscSegmentsALCARECOBHO * CosmicMuonSeedALCARECOBHO * cosmicMuonsALCARECOBHO)
+
 ALCARECOMuAlBeamHaloOverlapsEnergyCut = cms.EDFilter("AlignmentCSCBeamHaloSelectorModule",
     filter = cms.bool(True),
     src = cms.InputTag("cosmicMuonsALCARECOBHO"),
-    minStations = cms.uint32(0), ## no "energy cut" yet
-
+    minStations = cms.uint32(0), # no "energy cut" yet
     minHitsPerStation = cms.uint32(4)
 )
 
@@ -23,19 +38,8 @@ ALCARECOMuAlBeamHaloOverlaps = cms.EDFilter("AlignmentCSCOverlapSelectorModule",
     filter = cms.bool(True),
     src = cms.InputTag("ALCARECOMuAlBeamHaloOverlapsEnergyCut"),
     minHitsPerChamber = cms.uint32(4),
-    station = cms.int32(0) ## all stations: I'll need to split it by station (8 subsamples) offline
-
+    station = cms.int32(0) # all stations: I'll need to split it by station (8 subsamples) offline
 )
 
-reconstructAsCosmicMuonsALCARECOBHO = cms.Sequence(cscSegmentsALCARECOBHO*CosmicMuonSeedALCARECOBHO*cosmicMuonsALCARECOBHO)
-seqALCARECOMuAlBeamHaloOverlaps = cms.Sequence(ALCARECOMuAlBeamHaloOverlapsHLT+reconstructAsCosmicMuonsALCARECOBHO*ALCARECOMuAlBeamHaloOverlapsEnergyCut*ALCARECOMuAlBeamHaloOverlaps)
-ALCARECOMuAlBeamHaloOverlapsHLT.HLTPaths = ['HLT_CSCBeamHaloOverlapRing1', 'HLT_CSCBeamHaloOverlapRing2']
-# Choice of the building algo: 1 SK, 2 TC, 3 DF, 4 ST, ...
-cscSegmentsALCARECOBHO.algo_type = 4
-cscSegmentsALCARECOBHO.inputObjects = 'hltCsc2DRecHits'
-CosmicMuonSeedALCARECOBHO.EnableDTMeasurement = False
-CosmicMuonSeedALCARECOBHO.CSCRecSegmentLabel = 'cscSegmentsALCARECOBHO'
-cosmicMuonsALCARECOBHO.MuonSeedCollectionLabel = 'CosmicMuonSeedALCARECOBHO'
-cosmicMuonsALCARECOBHO.TrajectoryBuilderParameters.EnableDTMeasurement = False
-cosmicMuonsALCARECOBHO.TrajectoryBuilderParameters.CSCRecSegmentLabel = 'cscSegmentsALCARECOBHO'
+seqALCARECOMuAlBeamHaloOverlaps = cms.Sequence(ALCARECOMuAlBeamHaloOverlapsHLT + reconstructAsCosmicMuonsALCARECOBHO * ALCARECOMuAlBeamHaloOverlapsEnergyCut * ALCARECOMuAlBeamHaloOverlaps)
 
