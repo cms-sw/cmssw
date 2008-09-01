7ac1713ad8867681ef6613fa22d8281b4bd6a2fe
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-09-01 10:47:53 +0000

    - add cosmic-specific ecal sequence

--- DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Muon.muonQualityTests_cff import *
-from DQMOffline.Ecal.ecal_dqm_client_offline_cff import *
+from DQMOffline.Ecal.ecal_dqm_client_offline_cosmic_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_client_fileT0_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_client_cff import *
 from DQMOffline.Trigger.DQMOffline_Trigger_Client_cff import *
--- DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Muon.muonCosmicMonitors_cff import *
-from DQMOffline.Ecal.ecal_dqm_source_offline_cff import *
+from DQMOffline.Ecal.ecal_dqm_source_offline_cosmic_cff import *
 from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
@@ -11,5 +11,7 @@ from DQMOffline.Trigger.DQMOffline_Trigger_cff import *
 
 DQMOfflineCosmics_wHLT = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
 
-DQMOfflineCosmics = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*l1tmonitor*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
+triggerOfflineDQMSource_noEmu = cms.Sequence(l1tmonitor*hltResults*egammaHLTDQM*HLTTauDQMOffline)
+
+DQMOfflineCosmics = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource_noEmu*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
 
