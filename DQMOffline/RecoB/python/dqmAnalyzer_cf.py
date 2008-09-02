8d1ae59d032486ccab8955549f83432cf9863b57
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2008-09-02 06:59:35 +0000

    addded by mistake

--- DQMOffline/RecoB/python/dqmAnalyzer_cf.py
+++
@@ -1,8 +0,0 @@
-Traceback (most recent call last):
-  File "/afs/cern.ch/cms/sw/slc4_ia32_gcc345/cms/cmssw/CMSSW_2_1_0_pre4/src/FWCore/ParameterSet/python/cfg2py.py", line 1, in ?
-    import FWCore.ParameterSet.Config as cms
-  File "/afs/cern.ch/cms/sw/slc4_ia32_gcc345/cms/cmssw/CMSSW_2_1_0_pre4/python/FWCore/ParameterSet/Config.py", line 5, in ?
-    options = Options()
-  File "/afs/cern.ch/cms/sw/slc4_ia32_gcc345/cms/cmssw/CMSSW_2_1_0_pre4/python/FWCore/ParameterSet/Options.py", line 5, in __init__
-    self.readKeys = set()
-NameError: global name 'set' is not defined
