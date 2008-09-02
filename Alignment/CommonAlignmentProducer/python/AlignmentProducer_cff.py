172e3c98bfa57d89e472e80ab21ce7623f8731a4
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-09-02 17:36:19 +0000

    adjust configuration to now configurable type of AlignmentParameters

--- Alignment/CommonAlignmentProducer/python/AlignmentProducer_cff.py
+++ Alignment/CommonAlignmentProducer/python/AlignmentProducer_cff.py
@@ -37,6 +37,7 @@ looper = cms.Looper("AlignmentProducer",
         HIPAlignmentAlgorithm
     ),
     ParameterBuilder = cms.PSet(
+	parameterTypes = cms.vstring('Selector,RigidBody'),
         Selector = cms.PSet(
             # selection of alignables and their parameters:
             # comma separated pairs of detector parts/levels as defined in AlinmentParameterSelector
