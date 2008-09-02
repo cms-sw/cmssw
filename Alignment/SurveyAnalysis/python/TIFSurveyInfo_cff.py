9122f48c29a5b3521735ef6f2353ba3e897ad55a
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-09-02 17:55:53 +0000

    make it parse, 'TrackerDigiGeometryESModule.applyAlignment = True' now redundant

--- Alignment/SurveyAnalysis/python/TIFSurveyInfo_cff.py
+++ Alignment/SurveyAnalysis/python/TIFSurveyInfo_cff.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 # Reading from DB
-from CondCore.DBCommon.CondDBCommon_cfi import *
+from CondCore.DBCommon.CondDBSetup_cfi import *
 PoolDBESSource = cms.ESSource("PoolDBESSource",
-    CondDBCommon,
+    CondDBSetup,
+    connect = cms.string('frontier://FrontierProd/CMS_COND_21X_ALIGNMENT'),
     toGet = cms.VPSet(cms.PSet(
         record = cms.string('TrackerAlignmentRcd'),
         tag = cms.string('TibTidTecAllSurvey_v2')
@@ -13,13 +14,4 @@ PoolDBESSource = cms.ESSource("PoolDBESSource",
             tag = cms.string('TibTidTecAllSurveyAPE_v2')
         ))
 )
-
-#
-# Survey info corrections for the TIF tracker setup
-# It assumes the standard geometry builder is used:
-# include "Geometry/TrackerGeometryBuilder/data/trackerGeometry.cfi"
-TrackerDigiGeometryESModule.applyAlignment = True
-CondDBCommon.connect = 'oracle://cms_orcoff_int2r/CMS_COND_ALIGNMENT' ##cms_orcoff_int2r/CMS_COND_ALIGNMENT"
-
-CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
 
