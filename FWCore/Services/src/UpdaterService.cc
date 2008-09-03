be4b07e2f7a3980d74ec5ed22e12b3ca9f0b1e8d
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-09-03 14:21:50 +0000

    move UpdaterService to this central place

---
+++ FWCore/Services/src/UpdaterService.cc
@@ -0,0 +1,48 @@
+#include "PhysicsTools/UtilAlgos/interface/UpdaterService.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/Provenance/interface/EventID.h"
+
+#include <iostream>
+#include "DataFormats/Common/interface/Trie.h"
+
+UpdaterService::UpdaterService(const edm::ParameterSet & cfg, edm::ActivityRegistry & r ) :
+  theEventId(0) {
+  r.watchPreProcessEvent( this, & UpdaterService::init );
+  theInit();
+}
+
+UpdaterService::~UpdaterService(){
+}
+
+void UpdaterService::init(const edm::EventID& eId, const edm::Timestamp&){
+  theInit();
+  theEventId = &eId;
+}
+
+void UpdaterService::theInit(){
+  theCounts.clear();  
+}
+
+bool UpdaterService::checkOnce(std::string tag){
+  bool answer=true;
+
+  std::map<std::string, uint>::iterator i=theCounts.find(tag);
+  if (i!=theCounts.end()){
+    i->second++;
+    answer=false;
+  }
+  else{
+    theCounts[tag]=1;
+    answer=true;
+  }
+
+  if (theEventId){ LogDebug("UpdaterService")<<"checking ONCE on tag: "<<tag
+					     <<"on run: "<<theEventId->run()<<" event: "<<theEventId->event()
+					     <<((answer)?" -> true":" -> false");
+  }
+  return answer;
+}
+
+bool UpdaterService::check(std::string tag, std::string label){
+  return true;
+}
