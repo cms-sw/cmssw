5efa9dd64dd7839508c85bcb1a96a8f99b863193
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-09-03 12:25:28 +0000

    make correct python

--- Alignment/MuonAlignment/test/test-misalign_cfg.py
+++ Alignment/MuonAlignment/test/test-misalign_cfg.py
@@ -7,22 +7,29 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # Ideal DT & CSC geometry 
 process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
-
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
-# Database output service
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
-
 # Misalignment example scenario producer
-process.load("Alignment.MuonAlignment.MisalignedMuon_cfi")
+from Alignment.MuonAlignment.Scenarios_cff import *
+process.MisalignedMuon = cms.ESProducer("MisalignedMuonESProducer",
+    Muon100InversepbScenario #MuonNoMovementsScenario #  from Scenarios_cff
+)
+
+# or standard stuff 
+# Reco geometry producer
+#process.load("Geometry.DTGeometry.dtGeometry_cfi")
+#process.load("Geometry.CSCGeometry.cscGeometry_cfi")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 process.source = cms.Source("EmptySource")
 
+# Database output service if you want to store soemthing in MisalignedMuon
+process.MisalignedMuon.saveToDbase = True
+from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
-    process.CondDBSetup,
+    CondDBSetup,
     toPut = cms.VPSet(cms.PSet(
         record = cms.string('DTAlignmentRcd'),
         tag = cms.string('DT100InversepbScenario')
@@ -42,17 +49,16 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     connect = cms.string('sqlite_file:Alignments.db')
 )
 
-process.prod = cms.EDFilter("TestMisalign",
+process.prod = cms.EDAnalyzer("TestMisalign",
     fileName = cms.untracked.string('misaligment.root')
 )
 
 process.p1 = cms.Path(process.prod)
-process.MessageLogger.cout = cms.PSet(
+process.MessageLogger.cout = cms.untracked.PSet(
     threshold = cms.untracked.string('INFO'),
     default = cms.untracked.PSet(
         limit = cms.untracked.int32(10000000)
     )
 )
-process.MisalignedMuon.saveToDbase = True
 
 
