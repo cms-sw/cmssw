beee27a9966d2c1806038118421f58ac4888dfc9
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-09-03 10:35:27 +0000

    changed dir path

--- DQM/TrigXMonitor/python/L1Scalers_cfi.py
+++ DQM/TrigXMonitor/python/L1Scalers_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: L1Scalers_cfi.py,v 1.1 2008/09/02 02:35:32 wittich Exp $
+# $Id: L1Scalers_cfi.py,v 1.2 2008/09/03 02:13:47 wittich Exp $
 
 l1s = cms.EDFilter("L1Scalers",
                    l1GtData = cms.InputTag("l1GtUnpack","","HLT"),
                    scalersResults = cms.InputTag("l1scalers","","HLT"),
-                   dqmFolder = cms.untracked.string("L1Scalers_EvF"),
+                   dqmFolder = cms.untracked.string("L1T/L1Scalers_EvF"),
                    verbose = cms.untracked.bool(False)
                    )
