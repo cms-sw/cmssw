e922fd1a7f0057f28c239a82ae9d298292522659
Erik Butz <sha1-53bc992219043be71927609d789fbc8f1ac8e7d6@cern.ch>
2008-09-03 12:13:27 +0000

    included steering for Y coordinates in tracker, included steering for several histogramm groups

--- Alignment/OfflineValidation/python/TrackerOfflineValidation_cfi.py
+++ Alignment/OfflineValidation/python/TrackerOfflineValidation_cfi.py
@@ -10,17 +10,79 @@ TrackerOfflineValidation = cms.EDFilter("TrackerOfflineValidation",
     trajectoryInput           = cms.string('TrackRefitter'),
     localCoorHistosOn         = cms.bool(False),
     moduleLevelHistsTransient = cms.bool(False),
+    stripYResiduals           = cms.bool(False),                                        
     overlappOn                = cms.bool(False),                                      
-        TH1NormResModules = cms.PSet(
+
+    # Normalized X Residuals, normal local coordinates (Strip)
+    TH1NormXResStripModules = cms.PSet(
         xmin = cms.double(-3.0),
-        Nbinx = cms.int32(300),
+        Nbinx = cms.int32(100),
         xmax = cms.double(3.0)
     ),
 
-    TH1ResModules = cms.PSet(
+    # X Residuals, normal local coordinates (Strip)                      
+    TH1XResStripModules = cms.PSet(
         xmin = cms.double(-3.0),
-        Nbinx = cms.int32(300),
+        Nbinx = cms.int32(100),
         xmax = cms.double(3.0)
+    ),
+
+    # Normalized X Residuals, native coordinates (Strip)
+    TH1NormXprimeResStripModules = cms.PSet(
+        Nbinx = cms.int32(100),
+        xmin = cms.double(-3.0),
+        xmax = cms.double(3.0)
+    ),
+
+    # X Residuals, native coordinates (Strip)
+    TH1XprimeResStripModules = cms.PSet(
+        Nbinx = cms.int32(100), xmin = cms.double(-3.0), xmax = cms.double(3.0)
+    ),
+
+    # Normalized Y Residuals, native coordinates (Strip -> hardly defined)
+    TH1NormYResStripModules = cms.PSet(
+        Nbinx = cms.int32(100), xmin = cms.double(-3.0), xmax = cms.double(3.0)
+    ),
+    # -> very broad distributions expected                                         
+    TH1YResStripModules = cms.PSet(
+        Nbinx = cms.int32(100), xmin = cms.double(-10.0), xmax = cms.double(10.0)
+    ),
+
+    TH1NormXResPixelModules = cms.PSet(
+        xmin = cms.double(-3.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(3.0)
+    ),
+
+    TH1XResPixelModules = cms.PSet(
+        xmin = cms.double(-3.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(3.0)
+    ),
+
+    TH1NormXprimeResPixelModules = cms.PSet(
+        xmin = cms.double(-3.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(3.0)
+    ),
+
+    TH1XprimeResPixelModules = cms.PSet(
+        xmin = cms.double(-3.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(3.0)
+    ),                                        
+                                        
+
+    TH1NormYResPixelModules = cms.PSet(
+        xmin = cms.double(-3.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(3.0)
+    ),
+
+    TH1YResPixelModules = cms.PSet(
+        xmin = cms.double(-0.5),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(0.5)
     )
 )
 
