98b2f4ecbd2e0e0ebde353632617e7e118ea7bd9
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-09-03 22:19:43 +0000

    minor fix

--- RecoTracker/TkDetLayers/src/PixelBarrelLayer.cc
+++ RecoTracker/TkDetLayers/src/PixelBarrelLayer.cc
@@ -46,11 +46,11 @@ PixelBarrelLayer::PixelBarrelLayer(vector<const PixelRod*>& innerRods,
 
   //--------- DEBUG INFO --------------
   LogDebug("TkDetLayers") << "==== DEBUG PixelBarrelLayer =====" ; 
-  LogDebug("TkDetLayers") << "PixelBarrelLayer innerCyl r,lenght"
+  LogDebug("TkDetLayers") << "PixelBarrelLayer innerCyl r,lenght: "
                           << theInnerCylinder->radius() << " , "
                           << theInnerCylinder->bounds().length();
 
-  LogDebug("TkDetLayers") << "PixelBarrelLayer outerCyl r,lenght" 
+  LogDebug("TkDetLayers") << "PixelBarrelLayer outerCyl r,lenght: " 
 			  << theOuterCylinder->radius() << " , "
 			  << theOuterCylinder->bounds().length();
 
