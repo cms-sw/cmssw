c17a27268b1e209e509d8b8859f68b2ac677c0c5
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2008-09-04 17:37:49 +0000

    Change the source ROOT file

--- RecoJets/JetAnalyzers/test/JetPlotsExample.py
+++ RecoJets/JetAnalyzers/test/JetPlotsExample.py
@@ -13,7 +13,7 @@ process.maxEvents = cms.untracked.PSet(
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-'/store/relval/CMSSW_2_1_2/RelValZPrime5000JJ/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V5_10TeV_v3/0001/0436C4C1-8A71-DD11-B8E4-001A92971BD8.root')
+'/store/relval/CMSSW_2_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V6_10TeV_v7/0000/044A7A03-BE6E-DD11-B3C0-000423D98B08.root')
 )
 #############   Calo Jets  ###########################
 process.calo = cms.EDAnalyzer("CaloJetPlotsExample",
