e08e20646ee9991d301d6ee29407d170278cf984
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-09-04 10:58:58 +0000

    added L1Scalers

--- DQM/TrigXMonitorClient/src/SealModule.cc
+++ DQM/TrigXMonitorClient/src/SealModule.cc
@@ -6,5 +6,7 @@ DEFINE_SEAL_MODULE();
 
 #include "DQM/TrigXMonitorClient/interface/HLTScalersClient.h" 
 DEFINE_ANOTHER_FWK_MODULE(HLTScalersClient);
+#include "DQM/TrigXMonitorClient/interface/L1ScalersClient.h" 
+DEFINE_ANOTHER_FWK_MODULE(L1ScalersClient);
 
 
