9ec7bd6c5469ac6e7c8bf88096f42d5a4d69d810
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2008-09-04 19:44:01 +0000

    removed unused parameter

--- DQM/TrigXMonitor/python/L1Scalers_cfi.py
+++ DQM/TrigXMonitor/python/L1Scalers_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: L1Scalers_cfi.py,v 1.2 2008/09/03 02:13:47 wittich Exp $
+# $Id: L1Scalers_cfi.py,v 1.3 2008/09/03 10:35:27 lorenzo Exp $
 
 l1s = cms.EDFilter("L1Scalers",
                    l1GtData = cms.InputTag("l1GtUnpack","","HLT"),
-                   scalersResults = cms.InputTag("l1scalers","","HLT"),
                    dqmFolder = cms.untracked.string("L1T/L1Scalers_EvF"),
                    verbose = cms.untracked.bool(False)
                    )
