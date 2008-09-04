3cec8a49d62d2e1dd18f73e67e1f6c6c7f5e89ee
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-09-04 15:51:45 +0000

    check for presence of valid comparison record

--- DQM/L1TMonitor/src/L1TdeECAL.cc
+++ DQM/L1TMonitor/src/L1TdeECAL.cc
@@ -29,6 +29,13 @@ L1TdeECAL::L1TdeECAL(const edm::ParameterSet& iConfig) {
   if(iConfig.getUntrackedParameter<bool> ("disableROOToutput", true))
     histFile_ = "";
 
+  if (histFile_.size()!=0) {
+    edm::LogInfo("OutputRootFile") 
+      << "L1TEmulator ECAL specific histograms will be saved to " 
+      << histFile_.c_str() 
+      << std::endl;
+  }
+
   if(dbe!=NULL)
     dbe->setCurrentFolder(histFolder_);
   
@@ -121,7 +128,16 @@ void
   /// get the comparison results
   edm::Handle<L1DataEmulRecord> deRecord;
   iEvent.getByLabel(DEsource_, deRecord);
-  
+
+  if (!deRecord.isValid()) {
+    edm::LogInfo("DataNotFound") 
+      << "Cannot find L1DataEmulRecord with label "
+      << DEsource_.label() 
+      << " Please verrify that comparator was successfully executed."
+      << std::endl;
+    return;
+  }
+
   bool isComp = deRecord->get_isComp(ETP);
   if(!isComp) {
     if(verbose()) 
