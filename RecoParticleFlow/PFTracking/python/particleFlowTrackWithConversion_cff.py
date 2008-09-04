2d6a061c57e5da1cf64dadeb8390b62e44b9e685
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2008-09-04 13:02:58 +0000

    Configuration for conversion

---
+++ RecoParticleFlow/PFTracking/python/particleFlowTrackWithConversion_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoParticleFlow.PFTracking.elecPreId_cff import *
+from RecoParticleFlow.PFTracking.gsfSeedClean_cfi import *
+from TrackingTools.GsfTracking.CkfElectronCandidates_cff import *
+from TrackingTools.GsfTracking.GsfElectrons_cff import *
+from RecoParticleFlow.PFTracking.pfNuclear_cfi import *
+from RecoEgamma.EgammaElectronProducers.gsfElectronCkfTrackCandidateMaker_cff import *
+import RecoTracker.CkfPattern.CkfTrackCandidates_cfi
+gsfElCandidates = RecoTracker.CkfPattern.CkfTrackCandidates_cfi.ckfTrackCandidates.clone()
+import TrackingTools.GsfTracking.GsfElectronFit_cfi
+gsfPFtracks = TrackingTools.GsfTracking.GsfElectronFit_cfi.GsfGlobalElectronTest.clone()
+from RecoParticleFlow.PFTracking.pfTrackElec_cfi import *
+from RecoEgamma.EgammaPhotonProducers.softConversionSequence_cff import *
+
+particleFlowTrackWithConversion =cms.Sequence(
+    elecPreId*
+    gsfSeedclean*
+    gsfElCandidates*
+    gsfPFtracks*
+    pfTrackElec*
+    softConversionSequence)
+gsfElCandidates.TrajectoryBuilder = 'TrajectoryBuilderForPixelMatchGsfElectrons'
+gsfElCandidates.SeedProducer = 'gsfSeedclean'
+gsfElCandidates.SeedLabel = ''
+gsfPFtracks.Fitter = 'GsfElectronFittingSmoother'
+gsfPFtracks.Propagator = 'fwdElectronPropagator'
+gsfPFtracks.src = 'gsfElCandidates'
+gsfPFtracks.TTRHBuilder = 'WithTrackAngle'
+gsfPFtracks.TrajectoryInEvent = True
+
+
