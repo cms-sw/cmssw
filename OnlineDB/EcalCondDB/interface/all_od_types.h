78db4bfe288aaaf6ce11bfcb63c9e7a49fd9e2dd
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2008-09-04 12:36:34 +0000

    added handling of bad channels

--- OnlineDB/EcalCondDB/interface/all_od_types.h
+++ OnlineDB/EcalCondDB/interface/all_od_types.h
@@ -9,6 +9,7 @@
 #include "OnlineDB/EcalCondDB/interface/ODRunConfigSeqInfo.h"
 #include "OnlineDB/EcalCondDB/interface/RunConfigDat.h"
 #include "OnlineDB/EcalCondDB/interface/RunFEConfigDat.h"
+#include "OnlineDB/EcalCondDB/interface/RunTPGConfigDat.h"
 #include "OnlineDB/EcalCondDB/interface/RunModeDef.h"
 #include "OnlineDB/EcalCondDB/interface/RunSeqDef.h"
 #include "OnlineDB/EcalCondDB/interface/RunTag.h"
@@ -44,6 +45,10 @@
 #include "OnlineDB/EcalCondDB/interface/ODFEDelaysInfo.h"
 #include "OnlineDB/EcalCondDB/interface/ODFEWeightsInfo.h"
 #include "OnlineDB/EcalCondDB/interface/ODDelaysDat.h"
+#include "OnlineDB/EcalCondDB/interface/ODBadTTInfo.h"
+#include "OnlineDB/EcalCondDB/interface/ODBadTTDat.h"
+#include "OnlineDB/EcalCondDB/interface/ODBadXTInfo.h"
+#include "OnlineDB/EcalCondDB/interface/ODBadXTDat.h"
 #include "OnlineDB/EcalCondDB/interface/ODCond2ConfInfo.h"
 
 
