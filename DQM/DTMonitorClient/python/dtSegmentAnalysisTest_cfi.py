104bf1a9a820add468f9fa9c16b322dbe75efc8b
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-09-07 18:32:59 +0000

    Fix the configuration

--- DQM/DTMonitorClient/python/dtSegmentAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtSegmentAnalysisTest_cfi.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 segmentTest = cms.EDAnalyzer("DTSegmentAnalysisTest",
-                             folderRoot = cms.untracked.string('')
-                             detailedAnalysis = cms.untracked.bool(False)
+                             folderRoot = cms.untracked.string(''),
+                             detailedAnalysis = cms.untracked.bool(False),
                              #Permetted percentual of CH with bad chi2 segments 
                              badCHpercentual = cms.untracked.int32(10),
                              #Permetted value of chi2 segment quality
                              chi2Threshold = cms.untracked.double(5.0),
                              #Permetted percentual of segments with bad chi2
-                             badSegmPercentual = cms.untracked.int32(30),
+                             badSegmPercentual = cms.untracked.int32(30)
                              )
 
 
