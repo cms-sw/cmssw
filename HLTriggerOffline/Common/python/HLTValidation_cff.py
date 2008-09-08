2870953d3e6d7306d21a7d69aa55bd4c57d59a29
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-09-08 13:29:26 +0000

    add Egamma to validation sequence

--- HLTriggerOffline/Common/python/HLTValidation_cff.py
+++ HLTriggerOffline/Common/python/HLTValidation_cff.py
@@ -1,8 +1,11 @@
 from HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi import *
 from HLTriggerOffline.Tau.Validation.HLTTauValidation_cff import *
 from HLTriggerOffline.special.MonitorAlCaEcalPi0_cfi import *
+from HLTriggerOffline.Egamma.EgammaValidation_cff import *
 hltvalidation = cms.Sequence(
     muonTriggerRateTimeAnalyzer
-    *HLTTauVal
-    *EcalPi0Mon
+    +HLTTauVal
+    +EcalPi0Mon
+    +egammavalZee
+    +egammavalWenu
     )
