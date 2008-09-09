b43e384454aba2f610a1d8f21241c323dffebd8d
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2008-09-09 15:52:50 +0000

    ElectronIDValidator included

--- Validation/RecoEgamma/src/SealModule.cc
+++ Validation/RecoEgamma/src/SealModule.cc
@@ -1,10 +1,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
 #include "Validation/RecoEgamma/interface/EgammaObjects.h"
 #include "Validation/RecoEgamma/interface/PhotonValidator.h"
-
+#include "Validation/RecoEgamma/interface/ElectronIDValidator.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(EgammaObjects);
 DEFINE_ANOTHER_FWK_MODULE(PhotonValidator);
+DEFINE_ANOTHER_FWK_MODULE(ElectronIDValidator);
+
