46479db811b1a9ce79b1f395a4c80500f290d383
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2008-09-09 12:19:43 +0000

    slight cleanup

--- RecoLocalMuon/CSCRecHitD/src/CSCHitFromStripOnly.h
+++ RecoLocalMuon/CSCRecHitD/src/CSCHitFromStripOnly.h
@@ -10,30 +10,25 @@
  *
  * Be careful with the ME_1/a chambers: the 48 strips are ganged into 16 channels,
  * Each channel contains signals from three strips, each separated by 16 strips from the next.
- * This is the same for real data and for the MC. (This ME1a info is from Tim Cox.)
- *
- * \author Dominique Fortin - UCR
  *
  */
-//---- Possible changes from Stoyan Stoynev - NU
-#include <RecoLocalMuon/CSCRecHitD/src/CSCStripData.h>
-#include <RecoLocalMuon/CSCRecHitD/src/CSCStripHitData.h>
-#include <RecoLocalMuon/CSCRecHitD/src/CSCStripHit.h>
-#include <RecoLocalMuon/CSCRecHitD/src/CSCRecoConditions.h>
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
 
-#include <DataFormats/CSCDigi/interface/CSCStripDigiCollection.h>
+#include "RecoLocalMuon/CSCRecHitD/src/CSCStripData.h"
+#include "RecoLocalMuon/CSCRecHitD/src/CSCStripHitData.h"
+#include "RecoLocalMuon/CSCRecHitD/src/CSCStripHit.h"
+#include "RecoLocalMuon/CSCRecHitD/src/CSCRecoConditions.h"
 
-#include <FWCore/Framework/interface/Frameworkfwd.h>
-#include <FWCore/ParameterSet/interface/ParameterSet.h>
+#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include <vector>
 
 class CSCLayer;
-class CSCChamberSpecs;
-class CSCLayerGeometry;
 class CSCStripDigi;
-class CSCPeakBinOfStripPulse;
-
+class CSCPedestalChoice;
 
 class CSCHitFromStripOnly 
 {
@@ -50,11 +45,12 @@ class CSCHitFromStripOnly
 
   void setConditions( const CSCRecoConditions* reco ) {
     recoConditions_ = reco;
-  }
-
- protected:
+  } 
+ 
   
-  /// Go through strip in layers and build a table with 
+ private:
+	
+  /// Store SCA pulseheight information from strips in digis of one layer
   void fillPulseHeights( const CSCStripDigiCollection::Range& rstripd );  
 
   /// Find local maxima
@@ -63,51 +59,51 @@ class CSCHitFromStripOnly
   /// Make clusters using local maxima
   float makeCluster( int centerStrip );
 
-  std::vector<int> theMaxima;
-  std::vector<int>  theConsecutiveStrips;//... with charge for a given maximum
-  std::vector<int> theClosestMaximum; // this is number of strips to the closest other maximum
+  /// 
+  CSCStripHitData makeStripData( int centerStrip, int offset );
 
-  PulseHeightMap thePulseHeightMap;
-  
+  /// Is either neighbour 'bad'?
+  bool isNearDeadStrip(const CSCDetId& id, int centralStrip); 
+
   /// Find position of hit in strip cluster in terms of strip #
   float findHitOnStripPosition( const std::vector<CSCStripHitData>& data, const int& centerStrip );
   
+
+// MEMBER DATA
+
+  // Hold pointers to current layer, conditions data
   CSCDetId id_;    
   const CSCLayer * layer_;
-  const CSCLayerGeometry * layergeom_;
-  const CSCChamberSpecs * specs_;
-  
- private:
-  
-  CSCStripHitData makeStripData( int centerStrip, int offset );
-  bool isNearDeadStrip(const CSCDetId& id, int centralStrip); 
+  const CSCRecoConditions* recoConditions_;
+  // Number of strips in layer
+  unsigned nstrips_;
+  // gain correction weights and crosstalks read in from conditions database.
+  float gainWeight[80];
+
+  // The specific pedestal calculator
+	CSCPedestalChoice* calcped_;
+
+  // The cuts for forming the strip hits are described in the config file
+  bool useCalib;
+  static const int theClusterSize = 3;
+  float theThresholdForAPeak;
+  float theThresholdForCluster;
+
+
+  // working buffer for sca pulseheights
+  PulseHeightMap thePulseHeightMap;
+
+  std::vector<int> theMaxima;
+  std::vector<int> theConsecutiveStrips;//... with charge for a given maximum
+  std::vector<int> theClosestMaximum; // this is number of strips to the closest other maximum
 
   // Variables entering the CSCStripHit construction:
-  int tmax_cluster;
+  int tmax_cluster; // Peaking time for strip hit, in time bin units
   int clusterSize;
   std::vector<float> strips_adc;
   std::vector<float> strips_adcRaw;
   std::vector<int> theStrips;
   
-  // The cuts for forming the strip hits are described in the data/.cfi file
-  bool useCalib;
-  static const int theClusterSize = 3;
-  float theThresholdForAPeak;
-  float theThresholdForCluster;
-  //bool isData;
-
-  /// These are the gain correction weights and X-talks read in from database.
-  float gainWeight[80];
-
-  // Peaking time for strip hit
-  int TmaxOfCluster;            // in time bins;
-  // Number of strips in layer
-  unsigned Nstrips;
-
-  /// Hold pointer to current conditions data
-  const CSCRecoConditions* recoConditions_;
-
-  CSCPeakBinOfStripPulse* pulseheightOnStripFinder_;
 };
 
 #endif
