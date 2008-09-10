b256d7ac65efdf87e386eb1ac64e3e072ed8ed04
Keith Jarid Rose <sha1-a42f30ab844a85e96027297edca4d282e314c122@cern.ch>
2008-09-10 12:58:07 +0000

    new default option for calibrations

--- DQM/SiPixelCommon/test/client_template_cfg.py
+++ DQM/SiPixelCommon/test/client_template_cfg.py
@@ -1,94 +1,86 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("SIPIXELDQM")
+
+# load all appropriate modules:
+# get alignment conditions needed for geometry:
+process.load("Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff")
 process.load("Geometry.TrackerSimData.trackerSimGeometryXML_cfi")
-
 process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
-
 process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
-
 process.load("Configuration.StandardSequences.MagneticField_cff")
-
 process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
-
 process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff")
-
 process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
-
 process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
-
 process.load("DQM.SiPixelMonitorRawData.SiPixelMonitorRawData_cfi")
-
 process.load("DQM.SiPixelMonitorDigi.SiPixelMonitorDigi_cfi")
-
 process.load("DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi")
-
 process.load("DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi")
-
 process.load("DQMServices.Core.DQM_cfg")
-
 process.load("DQMServices.Components.DQMEnvironment_cfi")
-
+process.load("CalibTracker.SiPixelTools.SiPixelErrorsCalibDigis_cfi")
 process.load("CalibTracker.SiPixelGainCalibration.SiPixelCalibDigiProducer_cfi")
-
 process.load("CalibTracker.SiPixelSCurveCalibration.SiPixelSCurveCalibrationAnalysis_cfi")
-
 process.load("CalibTracker.SiPixelIsAliveCalibration.SiPixelIsAliveCalibration_cfi")
-
 process.load("CalibTracker.SiPixelGainCalibration.SiPixelGainCalibrationAnalysis_cfi")
 
-CALIBfrom CalibTracker.Configuration.SiPixelGain.SiPixelGain_Fake_cff import *
+# get the global tag with all cabling maps, alignment info, etc.
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_noesprefer_cff")
 
-CALIBfrom CalibTracker.Configuration.SiPixelLorentzAngle.SiPixelLorentzAngle_Fake_cff import *
+process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "GTAG"
 
+
+# and access the calibration information:
 CALIBfrom CondCore.DBCommon.CondDBCommon_cfi import *
-CALIBCondDBCommon.connect = "oracle://cms_orcoff_prep/CMS_COND_PIXEL_COMM_21X"
-CALIBCondDBCommon.DBParameters.authenticationPath = "/afs/cern.ch/cms/DB/conddb"
+#                                              
+CALIBprocess.siPixelCalibGlobalTag =  cms.ESSource("PoolDBESSource",
+CALIB                                              CondDBCommon,
+CALIB                                              connect = cms.string("oracle://cms_orcoff_prep/CMS_COND_PIXEL_COMM_21X"),
+CALIB                                              globaltag = cms.string("PIXELCALIB_01::TypeGLOBALCALIB"),
+CALIB                                              BlobStreamerName = cms.untracked.string("TBufferBlobStreamingService")
+CALIB                                              )
+CALIBprocess.siPixelCalibGlobalTag.DBParameters.authenticationPath = "/afs/cern.ch/cms/DB/conddb"
 
-CALIBprocess.db_pixel_source = cms.ESSource("PoolDBESSource",
-CALIB                                        CondDBCommon,
-CALIB                                        globaltag = cms.string("PIXELCALIB_01::TypeGLOBALCALIB"),
-CALIB                                        BlobStreamerName = cms.untracked.string("TBufferBlobStreamingService")
-CALIB)
+process.esprefer_dbcalib = cms.ESPrefer("PoolDBESSource","GlobalTag")
 
-CALIBprocess.load("CondTools.SiPixel.SiPixelGainCalibrationService_cfi")
+# this is needed by the gain calibration analyzer 
+process.load("CondTools.SiPixel.SiPixelGainCalibrationService_cfi")
 
-PHYSprocess.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-
-PHYSprocess.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
-PHYSprocess.GlobalTag.globaltag = "CRZT210_V1P::All"
-PHYSprocess.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
-
-
-process.source = cms.Source("SOURCETYPE",
+# the input file source
+process.source = cms.Source("PoolSource",
     debugFlag = cms.untracked.bool(True),
     debugVebosity = cms.untracked.uint32(1),
-    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/c/chiochia/cmssw/Muon_FullValidation_150pre3.root')
-    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/2008/6/6/RelVal-RelValTTbar-1212531852-IDEAL_V1-2nd-02/0000/081018D5-EC33-DD11-A623-000423D6CA42.root')
     ONEPARAM
-    TWOPARAM                       
-    fileNames = cms.untracked.vstring('FILENAME'
-				      )
-)
+    TWOPARAM
+    fileNames = cms.untracked.vstring('FILENAME')
+                            )
+
 
 process.maxEvents = cms.untracked.PSet(
+#    input = cms.untracked.int32(-1)
     input = cms.untracked.int32(-1)
 )
+
+# message logger
 process.MessageLogger = cms.Service("MessageLogger",
-    debugModules = cms.untracked.vstring('siPixelDigis', 
-        'SiPixelRawDataErrorSource', 
-        'SiPixelDigiSource', 
-        'SiPixelClusterSource', 
-        'SiPixelRecHitSource', 
-        'sipixelEDAClient'),
-    TEXTFILE = cms.untracked.PSet(
-        threshold = cms.untracked.string('ERROR')
-    ),
-    destinations = cms.untracked.vstring('TEXTFILE')
-)
+                                    debugModules = cms.untracked.vstring('siPixelDigis', 
+                                                                         'SiPixelRawDataErrorSource',
+                                                                         'SiPixelCalibProducer',
+                                                                         'SiPixelDigiSource', 
+                                                                         'SiPixelClusterSource', 
+                                                                         'SiPixelRecHitSource', 
+                                                                         'sipixelEDAClient'),
+                                    TEXTFILE = cms.untracked.PSet(threshold = cms.untracked.string('ERROR')
+                                                                           ),
+                                    
+#                                   destinations = cms.untracked.vstring('TEXTFILE')
+                                    )
 
 process.AdaptorConfig = cms.Service("AdaptorConfig")
 
+# DQM modules:
 process.sipixelEDAClient = cms.EDFilter("SiPixelEDAClient",
     FileSaveFrequency = cms.untracked.int32(50),
     StaticUpdateFrequency = cms.untracked.int32(10)
@@ -106,14 +98,23 @@ process.LockService = cms.Service("LockService",
     labels = cms.untracked.vstring('source')
 )
 
-process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters)
+# define all paths and sequences:
+process.Digis = cms.Sequence(process.siPixelDigis)
+process.Clusters = cms.Sequence(process.siPixelClusters)
+process.Calibration = cms.Sequence(process.siPixelCalibDigis*process.siPixelErrorsDigisToCalibDigis*process.siPixelGainCalibrationAnalysis*process.siPixelIsAliveCalibration*process.siPixelSCurveAnalysis)
 process.RAWmonitor = cms.Sequence(process.SiPixelRawDataErrorSource)
 process.DIGImonitor = cms.Sequence(process.SiPixelDigiSource)
 process.CLUmonitor = cms.Sequence(process.SiPixelClusterSource)
 process.HITmonitor = cms.Sequence(process.SiPixelRecHitSource)
 process.DQMmodules = cms.Sequence(process.qTester*process.dqmEnv*process.dqmSaver)
-process.p = cms.Path(process.DQMmodules*CLUSPOTRECSPOTCDSPOTSCURVESPOTGAINSPOTPIXELSPOTRAWMONSPOTDIGMONSPOTCLUMONSPOTRECMONSPOTprocess.sipixelEDAClient)
+process.p = cms.Path(process.DQMmodules*DIGISPOTCLUSPOTRECSPOTCDSPOTSCURVESPOTGAINSPOTPIXELSPOTRAWMONSPOTDIGMONSPOTCLUMONSPOTRECMONSPOTprocess.sipixelEDAClient)
+# choose one of these two:
+# online-style DQM (runs RECO)
+
+# offline-style DQM (reco in input file)
 #process.p = cms.Path(process.DQMmodules*process.DIGImonitor*process.sipixelEDAClient)
+
+# list of replace statements
 process.siPixelDigis.InputLabel = 'source'
 process.siPixelDigis.IncludeErrors = True
 process.SiPixelRawDataErrorSource.saveFile = False
@@ -156,6 +157,8 @@ process.dqmSaver.saveAtJobEnd = True
 process.siPixelIsAliveCalibration.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
 process.siPixelSCurveAnalysis.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
 process.siPixelGainCalibrationAnalysis.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
+process.siPixelErrorsDigisToCalibDigis.SiPixelProducerLabelTag = 'siPixelCalibDigis'
 process.siPixelIsAliveCalibration.saveFile = False
 process.siPixelGainCalibrationAnalysis.saveFile = False
 process.siPixelSCurveAnalysis.saveFile = False
+process.siPixelErrorsDigisToCalibDigis.saveFile=False
