3f5ee5f8e791008d7b6b330f993918db4c6d75ae
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2008-09-10 16:47:46 +0000

    Merging into associators_cff.py

--- Validation/RecoMuon/python/tpToGlbTrackAssociation_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-tpToGlbTrackAssociation = cms.EDProducer("TrackAssociatorEDProducer",
-    associator = cms.string('TrackAssociatorByDeltaR1'),
-    label_tp = cms.InputTag("mergedtruth","MergedTrackTruth"),
-    label_tr = cms.InputTag("globalMuons")
-)
-
-
--- Validation/RecoMuon/python/tpToL3TrackAssociation_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-tpToL3TrackAssociation = cms.EDProducer("TrackAssociatorEDProducer",
-    associator = cms.string('TrackAssociatorByDeltaR1'),
-    label_tp = cms.InputTag("mergedtruth","MergedTrackTruth"),
-    label_tr = cms.InputTag("hltL3Muons")
-)
-
-
