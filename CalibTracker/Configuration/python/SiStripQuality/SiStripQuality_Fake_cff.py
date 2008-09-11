9d993787fe381aeca0ca4913acaa2aec91799d7c
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-09-11 16:42:31 +0000

    correct bug in name

--- CalibTracker/Configuration/python/SiStripQuality/SiStripQuality_Fake_cff.py
+++ CalibTracker/Configuration/python/SiStripQuality/SiStripQuality_Fake_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from CalibTracker.SiStripESProducers.SiStripQualityFakeESSource_cfi import *
 
 from CalibTracker.SiStripESProducers.SiStripBadStripFakeESSource_cfi import *
-from CalibTracker.SiStripESProducers.SiStripBadModuleESSource_cfi import *
-from CalibTracker.SiStripESProducers.SiStripBadFibereESSource_cfi import *
-from CalibTracker.SiStripESProducers.SiStripBadChannelESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadModuleFakeESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadFiberFakeESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadChannelFakeESSource_cfi import *
