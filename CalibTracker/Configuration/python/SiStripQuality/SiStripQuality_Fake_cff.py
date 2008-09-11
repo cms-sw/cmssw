9731412c00ab23af728b67edb165aa9eb1d3fd19
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-09-11 16:33:26 +0000

    created FakeConditions for all the BadXXXRcd

--- CalibTracker/Configuration/python/SiStripQuality/SiStripQuality_Fake_cff.py
+++ CalibTracker/Configuration/python/SiStripQuality/SiStripQuality_Fake_cff.py
@@ -2,3 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from CalibTracker.SiStripESProducers.SiStripQualityFakeESSource_cfi import *
 
+from CalibTracker.SiStripESProducers.SiStripBadStripFakeESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadModuleESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadFibereESSource_cfi import *
+from CalibTracker.SiStripESProducers.SiStripBadChannelESSource_cfi import *
