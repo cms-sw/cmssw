b6b62aa40cdae080fb81520664b85d0d93837ab4
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-09-12 04:55:54 +0000

    Added writeL1TriggerKey and writeConfigData.

--- CondTools/L1Trigger/python/L1CondDBPayloadWriter_cfi.py
+++ CondTools/L1Trigger/python/L1CondDBPayloadWriter_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 L1CondDBPayloadWriter = cms.EDFilter("L1CondDBPayloadWriter",
     offlineDB = cms.string('sqlite_file:l1config.db'),
     L1TriggerKeyListTag = cms.string('L1TriggerKeyList_IDEAL'),
-    offlineAuthentication = cms.string('')
+    offlineAuthentication = cms.string(''),
+    writeL1TriggerKey = cms.bool(True),
+    writeConfigData = cms.bool(True)
 )
 
 
