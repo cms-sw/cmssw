98a0a6d9fe10c5266260171ee0601289fd0bfa2d
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-09-12 23:10:00 +0000

    Factor almost everything out into relaceable components

--- RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.h
+++ RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.h
@@ -4,18 +4,18 @@
 /** \class MuonSeedGenerator
  *  No description available.
  *
- *  $Date: 2007/10/16 17:50:40 $
- *  $Revision: 1.10 $
+ *  $Date: 2008/08/25 22:04:31 $
+ *  $Revision: 1.11 $
  *  \author R. Bellan - INFN Torino
  */
 
 #include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
-#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
-#include "RecoMuon/MuonSeedGenerator/src/MuonSeedFinder.h"
 #include <vector>
 
+class MuonSeedVFinder;
+class MuonSeedVPatternRecognition;
+class MuonSeedVCleaner;
 
 class MuonSeedGenerator: public edm::EDProducer {
  public:
@@ -33,29 +33,10 @@ class MuonSeedGenerator: public edm::EDProducer {
 
  protected:
 
- private:
-  void complete(MuonSeedFinder& seed, MuonTransientTrackingRecHit::MuonRecHitContainer &recHits, bool* used=0) const;
-  void checkAndFill(MuonSeedFinder& seedFinder, const edm::EventSetup& eSetup);
-  void fill(MuonSeedFinder& seedFinder, const edm::EventSetup& eSetup);
-  //returns a zeroed array of bools of length listSize
-  static bool * zero(unsigned listSize);
+  MuonSeedVPatternRecognition * thePatternRecognition;
+  MuonSeedVFinder * theSeedFinder;
+  MuonSeedVCleaner * theSeedCleaner;
 
-  MuonSeedFinder theSeedFinder;
-
-  // FIXME: change in OwnVector?
-  std::vector<TrajectorySeed> theSeeds;
-
-  /// the name of the DT rec hits collection
-  edm::InputTag theDTRecSegmentLabel;
-
-  /// the name of the CSC rec hits collection
-  edm::InputTag theCSCRecSegmentLabel;
-
-  ///Enable the DT measurement
-  bool enableDTMeasurement;
-
-  ///Enable the CSC measurement
-  bool enableCSCMeasurement;
 };
 #endif
 
