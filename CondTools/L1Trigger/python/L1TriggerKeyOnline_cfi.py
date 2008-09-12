aa0717ee4d765089aa0c015d44f2476b34f9594b
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-09-12 04:56:13 +0000

    Removed forceGeneration, added recordsToInclude.

--- CondTools/L1Trigger/python/L1TriggerKeyOnline_cfi.py
+++ CondTools/L1Trigger/python/L1TriggerKeyOnline_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 L1TriggerKeyOnline = cms.ESProducer("L1TriggerKeyOnlineProd",
     onlineAuthentication = cms.string('.'),
-    forceGeneration = cms.bool(False),
     tscKey = cms.string('dummy'),
-    onlineDB = cms.string('oracle://omds/GLOBALCALTRIGGER')
+    onlineDB = cms.string('oracle://CMS_OMDS_LB/CMS_TRG_R'),
+    recordsToInclude = cms.vstring()
 )
 
 
