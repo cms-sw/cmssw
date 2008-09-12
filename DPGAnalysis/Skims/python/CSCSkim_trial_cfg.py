920a44581698dbcd037bfa00e73b444171f40e80
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-09-12 17:27:59 +0000

    from RECO

--- DPGAnalysis/Skims/python/CSCSkim_trial_cfg.py
+++ DPGAnalysis/Skims/python/CSCSkim_trial_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("USER")
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.2 $'),
+    version = cms.untracked.string('$Revision: 1.3 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/CSCSkim_trial_cfg.py,v $'),
     annotation = cms.untracked.string('CRUZET4 CSCSkim skim')
 )
@@ -15,18 +15,7 @@ process.configurationMetadata = cms.untracked.PSet(
 #
 process.source = cms.Source("PoolSource",
        fileNames = cms.untracked.vstring (
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/056/591/8E478B5A-3C6F-DD11-AC01-000423D985B0.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/289/1E1407F1-106D-DD11-97A7-000423D985E4.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/313/3C2E24C9-106D-DD11-9503-000423D6B358.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/381/16B04CC1-2A6D-DD11-8580-000423D6BA18.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/381/C2E66C73-2A6D-DD11-AA12-000423D6AF24.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/394/14AE2A35-2D6D-DD11-8EBB-000423D9853C.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/400/18DA955F-3F6D-DD11-B38C-0016177CA7A0.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/404/0074DB01-3F6D-DD11-BDC4-001617C3B6C6.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/404/1C48FCC8-3E6D-DD11-BBB6-000423D6AF24.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/404/28DBA2A1-3E6D-DD11-986E-000423D6CA02.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/404/2ACCFB81-3F6D-DD11-BDAF-001617DBD288.root',
-       '/store/data/Commissioning08/Cosmics/RAW/CRUZET4_v1/000/057/404/32E3B105-416D-DD11-80B6-000423D9880C.root'
+      '/store/data/BeamCommissioning08/BeamHalo/RECO/v1/000/062/232/04FF0C7D-7280-DD11-8FCD-000423D98F98.root'
        )
 )
 process.maxEvents = cms.untracked.PSet(
@@ -53,13 +42,13 @@ process.load("DPGAnalysis/Skims/CSCSkim_cfi")
 
 #### the path
 
-process.mySkim = cms.Path(process.muonCSCDigis*process.csc2DRecHits*process.cscSegments*process.cscSkim)
+process.mySkim = cms.Path(process.cscSkim)
 
 
 #### output 
 process.outputSkim = cms.OutputModule(
     "PoolOutputModule",
-    fileName = cms.untracked.string("/tmp/schmittm/selectedEvents.root"),
+    fileName = cms.untracked.string("/tmp/arizzi/selectedEvents.root"),
     dataset = cms.untracked.PSet(
       dataTier = cms.untracked.string('RECO'),
       filterName = cms.untracked.string('CSCSkim_trial')
