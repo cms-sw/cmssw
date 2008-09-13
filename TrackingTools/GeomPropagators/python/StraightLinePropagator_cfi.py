817a40b46db13151603f9e45f11262517225c5ed
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2008-09-13 20:17:03 +0000

    add

---
+++ TrackingTools/GeomPropagators/python/StraightLinePropagator_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+StraightLinePropagator = cms.ESProducer("StraightLinePropagatorESProducer",
+    ComponentName = cms.string('StraightLinePropagator'),
+    PropagationDirection = cms.string('alongMomentum')
+)
+
+
