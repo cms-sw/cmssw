9b8dc9b25074f035e84e3ad3fe9962874cefc148
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-09-13 01:19:07 +0000

    fix for upper hostname.

--- EventFilter/StorageManager/scripts/operations/getwwpn.sh
+++ EventFilter/StorageManager/scripts/operations/getwwpn.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
-# $Id: getwwpn.sh,v 1.1 2008/09/13 01:01:54 loizides Exp $
+# $Id: getwwpn.sh,v 1.2 2008/09/13 01:06:45 loizides Exp $
 #
 # run this script to get a mapping between hostname and wwpn
 # the mapping has the syntax of a perl hash and can be put
 # into the mk_satamap.pl script.
 #
 
-for i in 13 14 15 16 17 18 19 20; do
-    ping -c1 -q -W3 srv-c2c07-$i > /dev/null
+nodes=$1
+if test -z $1; then 
+    if test -e host_list.cfg; then
+        nodes=`cat host_list.cfg`
+    else
+        nodes="";
+        for i in `seq 13 20`; do
+            nodes="$nodes srv-C2C07-$i"
+        done
+    fi
+else
+    nodes=`echo $nodes | tr c C`;
+fi
+
+for i in $nodes; do
+    ping -c1 -q -W3 $i > /dev/null
     if test "$?" = "0"; then
-        res=`ssh srv-c2c07-$i grep scsi-qla0-adapter-port /proc/scsi/qla2xxx/* 2>/dev/null | cut -d= -f2 | cut -d\; -f1`;
+        res=`ssh $i grep scsi-qla0-adapter-port /proc/scsi/qla2xxx/* 2>/dev/null | cut -d= -f2 | cut -d\; -f1`;
         re2=`echo $res | tr 'a-z' 'A-Z'`;
         re3=`echo $re2 | cut -b1-2`-`echo $re2 | cut -b3-4`-`echo $re2 | cut -b5-6`-`echo $re2 | cut -b7-8`-`echo $re2 | cut -b9-10`-`echo $re2 | cut -b11-12`-`echo $re2 | cut -b13-14`-`echo $re2 | cut -b15-16`
-        echo -e \"srv-c2c07-$i\" =\> \"$re3\",
+        echo -e \"$i\" =\> \"$re3\",
     else
-        echo -e \"srv-c2c07-$i\" =\> \"unknown\",
+        echo -e \"$i\" =\> \"unknown\",
     fi
 done
