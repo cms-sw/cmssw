dd331fc6d455e52b6e5c755f88ec4975bce6e2fa
Brian Drell <sha1-c7cbb7ddebb3a56d09a58e9398ceff52182b647f@cern.ch>
2008-09-15 20:52:36 +0000

    Added new cut value to generalTracks python config

--- RecoVertex/V0Producer/python/generalV0Candidates_cfi.py
+++ RecoVertex/V0Producer/python/generalV0Candidates_cfi.py
@@ -1,34 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
 generalV0Candidates = cms.EDProducer("V0Producer",
-    collinearityCut = cms.double(0.02),
-    #  Setting this one to zero; significance cut is sufficient
-    rVtxCut = cms.double(0.0),
-    doPostFitCuts = cms.bool(True),
     # string that tells which TrackCollection to use for vertexing
     trackRecoAlgorithm = cms.untracked.string('generalTracks'),
-    doTrackQualityCuts = cms.bool(True),
-    # set to 1, uses tracks refit by the KVF for V0Candidate kinematics
+
+    # set to true, uses tracks refit by the KVF for V0Candidate kinematics
     useSmoothing = cms.bool(True),
-    selectLambdas = cms.bool(True),
-    # These parameters decide whether or not to reconstruct
-    #  specific V0 particles
-    selectKshorts = cms.bool(True),
-    lambdaMassCut = cms.double(0.25),
-    kShortMassCut = cms.double(0.07),
-    vtxSignificanceCut = cms.double(22.0),
+
+    # set to true, stores tracks refit by KVF in reco::Vertex object
+    #  that is contained in the produced reco::V0Candidate 
+    storeSmoothedTracksInRecoVertex = cms.bool(True),
+
+    doPostFitCuts = cms.bool(True),
+    doTrackQualityCuts = cms.bool(True),
+
     # The next parameters are cut values
+
     # Track quality cuts
     #   Normalized track Chi2:
     tkChi2Cut = cms.double(5.0),
-    # Vertex cuts
-    vtxChi2Cut = cms.double(7.0),
-    # set to 1, stores tracks refit by KVF in reco::Vertex object
-    #  that is contained in the produced reco::V0Candidate 
-    storeSmoothedTracksInRecoVertex = cms.bool(True),
-    vertexFitter = cms.untracked.string('kvf'),
     #   Number of valid hits on track:
     tkNhitsCut = cms.int32(6)
+
+    # Vertex cuts
+    vtxChi2Cut = cms.double(7.0),
+    collinearityCut = cms.double(0.02),
+    #  Setting this one to zero; significance cut is sufficient
+    rVtxCut = cms.double(0.0),
+    vtxSignificanceCut = cms.double(22.0),
+    kShortMassCut = cms.double(0.07),
+    lambdaMassCut = cms.double(0.25),
+    impactParameterCut = cms.double(0.05),
+
+    # These parameters decide whether or not to reconstruct
+    #  specific V0 particles
+    selectKshorts = cms.bool(True),
+    selectLambdas = cms.bool(True),
+
+    vertexFitter = cms.untracked.string('kvf'),
+
 )
 
 
