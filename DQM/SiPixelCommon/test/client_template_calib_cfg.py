db1fd05fc8917f09a206309091a1131b1dd2a5f5
Keith Jarid Rose <sha1-a42f30ab844a85e96027297edca4d282e314c122@cern.ch>
2008-09-15 16:17:54 +0000

    splitting script into physics and calibration options

---
+++ DQM/SiPixelCommon/test/client_template_calib_cfg.py
@@ -0,0 +1,211 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SIPIXELDQM")
+process.load("Geometry.TrackerSimData.trackerSimGeometryXML_cfi")
+process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+#process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
+##process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+process.siPixelDigis.InputLabel = 'source'
+process.siPixelDigis.IncludeErrors = True
+
+process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
+
+process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
+process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff")
+
+#process.load("EventFilter.SiStripRawToDigi.SiStripRawToDigis_standard_cff")
+#process.siStripDigis.ProductLabel = 'source'
+
+#process.load("RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi")
+#process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi")
+
+#process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+#process.load("RecoPixelVertexing.Configuration.RecoPixelVertexing_cff")
+#process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
+  
+
+
+
+
+
+
+
+process.load("DQM.SiPixelMonitorRawData.SiPixelMonitorRawData_cfi")
+process.SiPixelRawDataErrorSource.saveFile = False
+process.SiPixelRawDataErrorSource.isPIB = False
+process.SiPixelRawDataErrorSource.slowDown = False
+
+process.load("DQM.SiPixelMonitorDigi.SiPixelMonitorDigi_cfi")
+process.SiPixelDigiSource.saveFile = False
+process.SiPixelDigiSource.isPIB = False
+process.SiPixelDigiSource.slowDown = False
+process.SiPixelDigiSource.modOn = True
+process.SiPixelDigiSource.twoDimOn = True
+process.SiPixelDigiSource.hiRes = True
+process.SiPixelDigiSource.ladOn = False
+process.SiPixelDigiSource.layOn = False
+process.SiPixelDigiSource.phiOn = False
+process.SiPixelDigiSource.bladeOn = False
+process.SiPixelDigiSource.diskOn = False
+process.SiPixelDigiSource.ringOn = False
+
+process.load("DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi")
+process.SiPixelClusterSource.saveFile = False
+process.SiPixelClusterSource.isPIB = False
+process.SiPixelClusterSource.modOn = True
+process.SiPixelClusterSource.twoDimOn = True
+process.SiPixelClusterSource.reducedSet = False
+process.SiPixelClusterSource.ladOn = False
+process.SiPixelClusterSource.layOn = False
+process.SiPixelClusterSource.phiOn = False
+process.SiPixelClusterSource.bladeOn = False
+process.SiPixelClusterSource.diskOn = False
+process.SiPixelClusterSource.ringOn = False
+
+process.load("DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi")
+process.SiPixelRecHitSource.saveFile = False
+process.SiPixelRecHitSource.isPIB = False
+process.SiPixelRecHitSource.modOn = True
+process.SiPixelRecHitSource.twoDimOn = True
+process.SiPixelRecHitSource.ladOn = False
+process.SiPixelRecHitSource.layOn = False
+process.SiPixelRecHitSource.phiOn = False
+process.SiPixelRecHitSource.bladeOn = False
+process.SiPixelRecHitSource.ringOn = False
+process.SiPixelRecHitSource.diskOn = False
+
+process.load("CalibTracker.SiPixelTools.SiPixelErrorsCalibDigis_cfi")
+process.siPixelErrorsDigisToCalibDigis.saveFile=False
+process.siPixelErrorsDigisToCalibDigis.SiPixelProducerLabelTag = 'siPixelCalibDigis'
+process.load("CalibTracker.SiPixelGainCalibration.SiPixelCalibDigiProducer_cfi")
+process.load("CalibTracker.SiPixelSCurveCalibration.SiPixelSCurveCalibrationAnalysis_cfi")
+process.siPixelSCurveAnalysis.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
+process.siPixelSCurveAnalysis.saveFile = False
+process.load("CalibTracker.SiPixelIsAliveCalibration.SiPixelIsAliveCalibration_cfi")
+process.siPixelIsAliveCalibration.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
+process.siPixelIsAliveCalibration.saveFile = False
+process.load("CalibTracker.SiPixelGainCalibration.SiPixelGainCalibrationAnalysis_cfi")
+process.siPixelGainCalibrationAnalysis.DetSetVectorSiPixelCalibDigiTag = 'siPixelCalibDigis'
+process.siPixelGainCalibrationAnalysis.saveFile = False
+process.siPixelGainCalibrationAnalysis.savePixelLevelHists = False
+
+# process.load("DQM.SiPixelMonitorTrack.SiPixelMonitorTrack_cfi")
+# process.SiPixelTrackResidualSource.TrackCandidateProducer = 'ckfTrackCandidatesP5'
+# process.SiPixelTrackResidualSource.TrackProducer = 'ALCARECOTkAlCosmicsCosmicTF0T'
+# process.SiPixelTrackResidualSource.debug = True
+# process.SiPixelTrackResidualSource.outputFile = '/tmp/merkelp/Pixel_DQM_TrackResidual_cosmictrackfinderP5.root'
+
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.connect ="sqlite_file:/afs/cern.ch/user/m/malgeri/public/globtag/CRZT210_V1.db"
+process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRUZET4_V5P::All"
+
+from CondCore.DBCommon.CondDBCommon_cfi import *
+process.siPixelCalibGlobalTag =  cms.ESSource("PoolDBESSource",
+                                              CondDBCommon,
+                                              connect = cms.string("oracle://cms_orcoff_prep/CMS_COND_PIXEL_COMM_21X"),
+                                              globaltag = cms.string("PIXELCALIB_01::TypeGLOBALCALIB"),
+                                              BlobStreamerName = cms.untracked.string("TBufferBlobStreamingService")
+                                              )
+process.siPixelCalibGlobalTag.DBParameters.authenticationPath = "/afs/cern.ch/cms/DB/conddb"
+process.es_prefer_dbcalib = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+process.source = cms.Source("PoolSource",
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(1),
+                            ONEPARAM
+                            TWOPARAM
+    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/c/chiochia/cmssw/Muon_FullValidation_150pre3.root')
+    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/2008/6/6/RelVal-RelValTTbar-1212531852-IDEAL_V1-2nd-02/0000/081018D5-EC33-DD11-A623-000423D6CA42.root')
+    fileNames = cms.untracked.vstring(
+    #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/2CB57036-4C6A-DD11-9A04-000423D94990.root',
+    #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/7EB80A4C-4C6A-DD11-A589-000423D987E0.root',
+    #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B0319435-4C6A-DD11-9B18-000423D952C0.root',
+    #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/B428FF51-4C6A-DD11-87D8-000423D986C4.root',
+    #'rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/MW33_v1/000/056/742/DEAB1B33-4C6A-DD11-B83D-000423D99BF2.root'
+    #'rfio:/castor/cern.ch/cms/store/cmscaf/alca/alignment/CRUZET4-TkAlCosmics/57553/ALCARECOTkAlCosmics0T_1.root', 
+    #rfio:/castor/cern.ch/cms/store/cmscaf/alca/alignment/CRUZET4-TkAlCosmics/57553/ALCARECOTkAlCosmics0T_2.root'
+    'FILENAME'
+       				      )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('siPixelDigis',
+        'SiPixelCalibDigiProducer',
+        'SiPixelRawDataErrorSource', 
+        'SiPixelDigiSource', 
+        'SiPixelClusterSource', 
+        'SiPixelRecHitSource', 
+        'sipixelEDAClient'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.AdaptorConfig = cms.Service("AdaptorConfig")
+
+process.sipixelEDAClient = cms.EDFilter("SiPixelEDAClient",
+    EventOffsetForInit = cms.untracked.int32(10),
+    ActionOnLumiSection = cms.untracked.bool(True),
+    ActionOnRunEnd = cms.untracked.bool(True),
+    HighResolutionOccupancy = cms.untracked.bool(True)
+)
+
+process.qTester = cms.EDFilter("QualityTester",
+    qtList = cms.untracked.FileInPath('DQM/SiPixelMonitorClient/test/sipixel_qualitytest_config.xml'),
+    QualityTestPrescaler = cms.untracked.int32(1),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
+
+process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
+
+process.LockService = cms.Service("LockService",
+    labels = cms.untracked.vstring('source')
+)
+
+process.Reco = cms.Sequence(process.siPixelDigis)
+#process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters*process.siPixelRecHits)
+process.Calibration = cms.Sequence(process.siPixelCalibDigis*process.siPixelErrorsDigisToCalibDigis*process.siPixelGainCalibrationAnalysis*process.siPixelIsAliveCalibration*process.siPixelSCurveAnalysis)
+process.RAWmonitor = cms.Sequence(process.SiPixelRawDataErrorSource)
+process.DIGImonitor = cms.Sequence(process.SiPixelDigiSource)
+process.CLUmonitor = cms.Sequence(process.SiPixelClusterSource)
+process.HITmonitor = cms.Sequence(process.SiPixelRecHitSource)
+process.DQMmodules = cms.Sequence(process.qTester*process.dqmEnv*process.dqmSaver)
+process.p = cms.Path(process.Reco*process.DQMmodules*process.Calibration*process.RAWmonitor*process.DIGImonitor*process.sipixelEDAClient)
+#process.p = cms.Path(process.DQMmodules*process.DIGImonitor*process.sipixelEDAClient)
+process.DQM.collectorHost = ''
+process.dqmSaver.convention = 'Online'
+process.dqmSaver.producer = 'DQM'
+process.dqmEnv.subSystemFolder = 'Pixel'
+process.dqmSaver.dirName = '.'
+process.dqmSaver.saveByLumiSection = -1
+process.dqmSaver.saveByRun = 1
+process.dqmSaver.saveAtJobEnd = True
+
+#process.siPixelLocalReco = cms.Sequence(process.siPixelRecHits) 
+#process.siStripLocalReco = cms.Sequence(process.siStripMatchedRecHits)
+#process.trackerLocalReco = cms.Sequence(process.siPixelLocalReco*process.siStripLocalReco)
+#process.trackReconstruction = cms.Sequence(process.trackerLocalReco*process.offlineBeamSpot*process.recopixelvertexing*process.tracksP5) #*process.rstracks 
+
+#process.monitorTrack = cms.Sequence(process.SiPixelTrackResidualSource)
+#process.monitors = cms.Sequence(process.SiPixelRawDataErrorSource*process.SiPixelDigiSource*process.SiPixelClusterSource*process.SiPixelRecHitSource*process.SiPixelTrackResidualSource)
+
+#process.pathTrack = cms.Path(process.trackReconstruction*process.DQMmodules*process.monitors*process.sipixelEDAClient) 
---
+++ DQM/SiPixelCommon/test/client_template_physics_cfg.py
@@ -0,0 +1,177 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SIPIXELDQM")
+process.load("Geometry.TrackerSimData.trackerSimGeometryXML_cfi")
+process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+#process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
+
+##process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+process.siPixelDigis.InputLabel = 'source'
+process.siPixelDigis.IncludeErrors = True
+
+process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
+
+process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
+process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff")
+
+#process.load("EventFilter.SiStripRawToDigi.SiStripRawToDigis_standard_cff")
+#process.siStripDigis.ProductLabel = 'source'
+
+#process.load("RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi")
+#process.load("RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi")
+#process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi")
+
+#process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+#process.load("RecoPixelVertexing.Configuration.RecoPixelVertexing_cff")
+#process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
+  
+
+
+
+
+
+
+
+process.load("DQM.SiPixelMonitorRawData.SiPixelMonitorRawData_cfi")
+process.SiPixelRawDataErrorSource.saveFile = False
+process.SiPixelRawDataErrorSource.isPIB = False
+process.SiPixelRawDataErrorSource.slowDown = False
+
+process.load("DQM.SiPixelMonitorDigi.SiPixelMonitorDigi_cfi")
+process.SiPixelDigiSource.saveFile = False
+process.SiPixelDigiSource.isPIB = False
+process.SiPixelDigiSource.slowDown = False
+process.SiPixelDigiSource.modOn = True
+process.SiPixelDigiSource.twoDimOn = True
+process.SiPixelDigiSource.hiRes = True
+process.SiPixelDigiSource.ladOn = False
+process.SiPixelDigiSource.layOn = False
+process.SiPixelDigiSource.phiOn = False
+process.SiPixelDigiSource.bladeOn = False
+process.SiPixelDigiSource.diskOn = False
+process.SiPixelDigiSource.ringOn = False
+
+process.load("DQM.SiPixelMonitorCluster.SiPixelMonitorCluster_cfi")
+process.SiPixelClusterSource.saveFile = False
+process.SiPixelClusterSource.isPIB = False
+process.SiPixelClusterSource.modOn = True
+process.SiPixelClusterSource.twoDimOn = True
+process.SiPixelClusterSource.reducedSet = False
+process.SiPixelClusterSource.ladOn = False
+process.SiPixelClusterSource.layOn = False
+process.SiPixelClusterSource.phiOn = False
+process.SiPixelClusterSource.bladeOn = False
+process.SiPixelClusterSource.diskOn = False
+process.SiPixelClusterSource.ringOn = False
+
+process.load("DQM.SiPixelMonitorRecHit.SiPixelMonitorRecHit_cfi")
+process.SiPixelRecHitSource.saveFile = False
+process.SiPixelRecHitSource.isPIB = False
+process.SiPixelRecHitSource.modOn = True
+process.SiPixelRecHitSource.twoDimOn = True
+process.SiPixelRecHitSource.ladOn = False
+process.SiPixelRecHitSource.layOn = False
+process.SiPixelRecHitSource.phiOn = False
+process.SiPixelRecHitSource.bladeOn = False
+process.SiPixelRecHitSource.ringOn = False
+process.SiPixelRecHitSource.diskOn = False
+
+# process.load("DQM.SiPixelMonitorTrack.SiPixelMonitorTrack_cfi")
+# process.SiPixelTrackResidualSource.TrackCandidateProducer = 'ckfTrackCandidatesP5'
+# process.SiPixelTrackResidualSource.TrackProducer = 'ALCARECOTkAlCosmicsCosmicTF0T'
+# process.SiPixelTrackResidualSource.debug = True
+# process.SiPixelTrackResidualSource.outputFile = '/tmp/merkelp/Pixel_DQM_TrackResidual_cosmictrackfinderP5.root'
+
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.connect ="sqlite_file:/afs/cern.ch/user/m/malgeri/public/globtag/CRZT210_V1.db"
+process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.globaltag = "CRUZET4_V5P::All"
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+process.source = cms.Source("PoolSource",
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(1),
+    ONEPARAM
+    TWOPARAM
+    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/c/chiochia/cmssw/Muon_FullValidation_150pre3.root')
+    #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/2008/6/6/RelVal-RelValTTbar-1212531852-IDEAL_V1-2nd-02/0000/081018D5-EC33-DD11-A623-000423D6CA42.root')
+    fileNames = cms.untracked.vstring(
+	'FILENAME')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('siPixelDigis', 
+        'SiPixelRawDataErrorSource', 
+        'SiPixelDigiSource', 
+        'SiPixelClusterSource', 
+        'SiPixelRecHitSource', 
+        'sipixelEDAClient'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.AdaptorConfig = cms.Service("AdaptorConfig")
+
+process.sipixelEDAClient = cms.EDFilter("SiPixelEDAClient",
+    EventOffsetForInit = cms.untracked.int32(10),
+    ActionOnLumiSection = cms.untracked.bool(True),
+    ActionOnRunEnd = cms.untracked.bool(True),
+    HighResolutionOccupancy = cms.untracked.bool(True)
+)
+
+process.qTester = cms.EDFilter("QualityTester",
+    qtList = cms.untracked.FileInPath('DQM/SiPixelMonitorClient/test/sipixel_qualitytest_config.xml'),
+    QualityTestPrescaler = cms.untracked.int32(1),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
+
+process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
+
+process.LockService = cms.Service("LockService",
+    labels = cms.untracked.vstring('source')
+)
+
+#process.Reco = cms.Sequence(process.siPixelRecHits)
+process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters*process.siPixelRecHits)
+process.RAWmonitor = cms.Sequence(process.SiPixelRawDataErrorSource)
+process.DIGImonitor = cms.Sequence(process.SiPixelDigiSource)
+process.CLUmonitor = cms.Sequence(process.SiPixelClusterSource)
+process.HITmonitor = cms.Sequence(process.SiPixelRecHitSource)
+process.DQMmodules = cms.Sequence(process.qTester*process.dqmEnv*process.dqmSaver)
+process.p = cms.Path(process.Reco*process.DQMmodules*process.RAWmonitor*process.DIGImonitor*process.CLUmonitor*process.HITmonitor*process.sipixelEDAClient)
+#process.p = cms.Path(process.DQMmodules*process.DIGImonitor*process.sipixelEDAClient)
+process.DQM.collectorHost = ''
+process.dqmSaver.convention = 'Online'
+process.dqmSaver.producer = 'DQM'
+process.dqmEnv.subSystemFolder = 'Pixel'
+process.dqmSaver.dirName = '.'
+process.dqmSaver.saveByLumiSection = -1
+process.dqmSaver.saveByRun = 1
+process.dqmSaver.saveAtJobEnd = True
+
+#process.siPixelLocalReco = cms.Sequence(process.siPixelRecHits) 
+#process.siStripLocalReco = cms.Sequence(process.siStripMatchedRecHits)
+#process.trackerLocalReco = cms.Sequence(process.siPixelLocalReco*process.siStripLocalReco)
+#process.trackReconstruction = cms.Sequence(process.trackerLocalReco*process.offlineBeamSpot*process.recopixelvertexing*process.tracksP5) #*process.rstracks 
+
+#process.monitorTrack = cms.Sequence(process.SiPixelTrackResidualSource)
+#process.monitors = cms.Sequence(process.SiPixelRawDataErrorSource*process.SiPixelDigiSource*process.SiPixelClusterSource*process.SiPixelRecHitSource*process.SiPixelTrackResidualSource)
+
+#process.pathTrack = cms.Path(process.trackReconstruction*process.DQMmodules*process.monitors*process.sipixelEDAClient) 
