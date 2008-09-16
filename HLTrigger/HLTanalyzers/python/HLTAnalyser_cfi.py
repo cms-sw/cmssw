6cdb8c392bbbd51a1689d43457c889029d17e62a
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-09-16 17:45:15 +0000

    configuration cleanup

--- HLTrigger/HLTanalyzers/python/HLTAnalyser_cfi.py
+++ HLTrigger/HLTanalyzers/python/HLTAnalyser_cfi.py
@@ -1,72 +1,75 @@
 import FWCore.ParameterSet.Config as cms
 
 hltanalysis = cms.EDAnalyzer("HLTAnalyzer",
-    ### Generator objects
-    #mctruth = cms.InputTag("genParticleCandidates"),
-    mctruth = cms.InputTag("genParticles"),
-    genEventScale = cms.InputTag("genEventScale"),
-    ### Reconstructed objects
-    genjets = cms.InputTag("iterativeCone5GenJets"),
-    genmet = cms.InputTag("genMet"),
-    recjets = cms.InputTag("hltIterativeCone5CaloJets"),
-    recmet = cms.InputTag("hltMet"),
-    ht = cms.InputTag("hltHtMet"),
-    calotowers = cms.InputTag("towerMaker"),
-    muon = cms.InputTag("muons"),
-    Electron = cms.InputTag("pixelMatchGsfElectrons"),
-    #Photon = cms.InputTag("correctedPhotons"),
-    Photon = cms.InputTag("photons"),
+    ### generator objects
+    mctruth                         = cms.InputTag("genParticles"),
+    genEventScale                   = cms.InputTag("genEventScale"),
+    
     ### Trigger objects
-    #l1GctCounts = cms.InputTag("l1GctEmulDigis"),
-    l1GctCounts = cms.InputTag("hltGctDigis"),
-    #l1GtObjectMapRecord = cms.InputTag("l1GtEmulDigis"),
-    l1GtObjectMapRecord = cms.InputTag("hltL1GtObjectMap"),
-    #l1GtReadoutRecord = cms.InputTag("l1GmtEmulDigis"),
-    l1GtReadoutRecord = cms.InputTag("hltGtDigis"),
-    l1extramc = cms.string('hltL1extraParticles'),
-    l1extramu = cms.string('hltL1extraParticles'),
-##    hltresults = cms.InputTag("TriggerResults"),
-    hltresults = cms.InputTag("TriggerResults::HLT"),
-    ### Muon OpenHLT objects                             
-    MuCandTag2 = cms.InputTag("hltL2MuonCandidates"),
-    MuCandTag3 = cms.InputTag("hltL3MuonCandidates"),
-    MuIsolTag3 = cms.InputTag("hltL3MuonIsolations"),
-    MuIsolTag2 = cms.InputTag("hltL2MuonIsolations"),
-    MuLinkTag = cms.InputTag("hltL3Muons"),
-    ### Egamma OpenHLT objects                             
-    CandIso = cms.InputTag("hltL1IsoRecoEcalCandidate"),
-    CandNonIso = cms.InputTag("hltL1NonIsoRecoEcalCandidate"),
-    EcalIso = cms.InputTag("hltL1IsolatedPhotonEcalIsol"),
-    EcalNonIso = cms.InputTag("hltL1NonIsolatedPhotonEcalIsol"),
-    HcalIsoPho = cms.InputTag("hltL1IsolatedPhotonHcalIsol"),
-    HcalNonIsoPho = cms.InputTag("hltL1NonIsolatedPhotonHcalIsol"),
-    IsoPhoTrackIsol = cms.InputTag("hltL1IsoPhotonTrackIsol"),
-    NonIsoPhoTrackIsol = cms.InputTag("hltL1NonIsoPhotonTrackIsol"),
-    HcalIsoEle = cms.InputTag("hltL1IsolatedElectronHcalIsol"),
-    HcalNonIsoEle  = cms.InputTag("hltL1NonIsolatedElectronHcalIsol"),
-    #### Standard or Startup windows                         
-    #IsoElectrons = cms.InputTag("hltPixelMatchElectronsL1Iso"),
-    #NonIsoElectrons = cms.InputTag("hltPixelMatchElectronsL1NonIso"),
-    #PixelSeedL1Iso = cms.InputTag("hltL1IsoElectronPixelSeeds"),
-    #PixelSeedL1NonIso = cms.InputTag("hltL1NonIsoElectronPixelSeeds"),
-    #IsoEleTrackIsol = cms.InputTag("hltL1IsoElectronTrackIsol"),
-    #NonIsoEleTrackIsol = cms.InputTag("hltL1NonIsoElectronTrackIsol"),
-    IsoElectrons = cms.InputTag("hltPixelMatchStartUpElectronsL1Iso"),
-    NonIsoElectrons = cms.InputTag("hltPixelMatchStartUpElectronsL1NonIso"),
-    PixelSeedL1Iso = cms.InputTag("hltL1IsoStartUpElectronPixelSeeds"),
-    PixelSeedL1NonIso = cms.InputTag("hltL1NonIsoStartUpElectronPixelSeeds"),
-    IsoEleTrackIsol = cms.InputTag("hltL1IsoStartUpElectronTrackIsol"),
-    NonIsoEleTrackIsol = cms.InputTag("hltL1NonIsoStartupElectronTrackIsol"),
-    ### Large windows
-    IsoElectronsLargeWindows = cms.InputTag("hltPixelMatchElectronsL1IsoLargeWindow"),
-    NonIsoElectronsLargeWindows = cms.InputTag("hltPixelMatchElectronsL1NonIsoLargeWindow"),
-    PixelSeedL1IsoLargeWindows = cms.InputTag("hltL1IsoLargeWindowElectronPixelSeeds"),
-    PixelSeedL1NonIsoLargeWindows = cms.InputTag("hltL1NonIsoLargeWindowElectronPixelSeeds"),
-    IsoEleTrackIsolLargeWindows = cms.InputTag("hltL1IsoLargeWindowElectronTrackIsol"),
-    NonIsoEleTrackIsolLargeWindows = cms.InputTag("hltL1NonIsoLargeWindowElectronTrackIsol"),
-    ## Tau HLT related objects
-    HLTTau              = cms.InputTag("TauOpenHLT"),
-    ### b-jet HLT related objects
+   #l1GctCounts                     = cms.InputTag("l1GctEmulDigis"),
+    l1GctCounts                     = cms.InputTag("hltGctDigis"),
+   #l1GtObjectMapRecord             = cms.InputTag("l1GtEmulDigis"),
+    l1GtObjectMapRecord             = cms.InputTag("hltL1GtObjectMap"),
+   #l1GtReadoutRecord               = cms.InputTag("l1GmtEmulDigis"),
+    l1GtReadoutRecord               = cms.InputTag("hltGtDigis"),
+    l1extramc                       = cms.string('hltL1extraParticles'),
+    l1extramu                       = cms.string('hltL1extraParticles'),
+    hltresults                      = cms.InputTag("TriggerResults::HLT"),
+    
+    ### reconstructed objects
+    genjets                         = cms.InputTag("iterativeCone5GenJets"),
+    genmet                          = cms.InputTag("genMet"),
+    recjets                         = cms.InputTag("hltIterativeCone5CaloJets"),
+    recmet                          = cms.InputTag("hltMet"),
+    ht                              = cms.InputTag("hltHtMet"),
+    calotowers                      = cms.InputTag("towerMaker"),
+    muon                            = cms.InputTag("muons"),
+    Electron                        = cms.InputTag("pixelMatchGsfElectrons"),
+   #Photon                          = cms.InputTag("correctedPhotons"),
+    Photon                          = cms.InputTag("photons"),
+    
+    ### muon OpenHLT objects                             
+    MuCandTag2                      = cms.InputTag("hltL2MuonCandidates"),
+    MuCandTag3                      = cms.InputTag("hltL3MuonCandidates"),
+    MuIsolTag3                      = cms.InputTag("hltL3MuonIsolations"),
+    MuIsolTag2                      = cms.InputTag("hltL2MuonIsolations"),
+    MuLinkTag                       = cms.InputTag("hltL3Muons"),
+    ### egamma OpenHLT objects                             
+    CandIso                         = cms.InputTag("hltL1IsoRecoEcalCandidate"),
+    CandNonIso                      = cms.InputTag("hltL1NonIsoRecoEcalCandidate"),
+    EcalIso                         = cms.InputTag("hltL1IsolatedPhotonEcalIsol"),
+    EcalNonIso                      = cms.InputTag("hltL1NonIsolatedPhotonEcalIsol"),
+    HcalIsoPho                      = cms.InputTag("hltL1IsolatedPhotonHcalIsol"),
+    HcalNonIsoPho                   = cms.InputTag("hltL1NonIsolatedPhotonHcalIsol"),
+    IsoPhoTrackIsol                 = cms.InputTag("hltL1IsoPhotonTrackIsol"),
+    NonIsoPhoTrackIsol              = cms.InputTag("hltL1NonIsoPhotonTrackIsol"),
+    HcalIsoEle                      = cms.InputTag("hltL1IsolatedElectronHcalIsol"),
+    HcalNonIsoEle                   = cms.InputTag("hltL1NonIsolatedElectronHcalIsol"),
+    ### egamma - standard or startup windows                         
+    IsoElectrons                    = cms.InputTag("hltPixelMatchStartUpElectronsL1Iso"),
+   #IsoElectrons                    = cms.InputTag("hltPixelMatchElectronsL1Iso"),
+    NonIsoElectrons                 = cms.InputTag("hltPixelMatchStartUpElectronsL1NonIso"),
+   #NonIsoElectrons                 = cms.InputTag("hltPixelMatchElectronsL1NonIso"),
+    PixelSeedL1Iso                  = cms.InputTag("hltL1IsoStartUpElectronPixelSeeds"),
+   #PixelSeedL1Iso                  = cms.InputTag("hltL1IsoElectronPixelSeeds"),
+    PixelSeedL1NonIso               = cms.InputTag("hltL1NonIsoStartUpElectronPixelSeeds"),
+   #PixelSeedL1NonIso               = cms.InputTag("hltL1NonIsoElectronPixelSeeds"),
+    IsoEleTrackIsol                 = cms.InputTag("hltL1IsoStartUpElectronTrackIsol"),
+   #IsoEleTrackIsol                 = cms.InputTag("hltL1IsoElectronTrackIsol"),
+    NonIsoEleTrackIsol              = cms.InputTag("hltL1NonIsoStartupElectronTrackIsol"),
+   #NonIsoEleTrackIsol              = cms.InputTag("hltL1NonIsoElectronTrackIsol"),
+    ### egamma - large windows
+    IsoElectronsLargeWindows        = cms.InputTag("hltPixelMatchElectronsL1IsoLargeWindow"),
+    NonIsoElectronsLargeWindows     = cms.InputTag("hltPixelMatchElectronsL1NonIsoLargeWindow"),
+    PixelSeedL1IsoLargeWindows      = cms.InputTag("hltL1IsoLargeWindowElectronPixelSeeds"),
+    PixelSeedL1NonIsoLargeWindows   = cms.InputTag("hltL1NonIsoLargeWindowElectronPixelSeeds"),
+    IsoEleTrackIsolLargeWindows     = cms.InputTag("hltL1IsoLargeWindowElectronTrackIsol"),
+    NonIsoEleTrackIsolLargeWindows  = cms.InputTag("hltL1NonIsoLargeWindowElectronTrackIsol"),
+
+    ### tau OpenHLT related objects
+    HLTTau                          = cms.InputTag("TauOpenHLT"),
+    
+    ### b-jet OpenHLT related objects
     CommonBJetsL2                   = cms.InputTag("hltIterativeCone5CaloJets"),
     CorrectedBJetsL2                = cms.InputTag("hltMCJetCorJetIcone5"),
     LifetimeBJetsL25                = cms.InputTag("openHltBLifetimeL25BJetTags"),
@@ -77,14 +80,15 @@ hltanalysis = cms.EDAnalyzer("HLTAnalyzer",
     SoftmuonBJetsL3                 = cms.InputTag("openHltBSoftmuonL3BJetTags"),
     PerformanceBJetsL25             = cms.InputTag("openHltBSoftmuonL25BJetTags"),
     PerformanceBJetsL3              = cms.InputTag("openHltBPerfMeasL3BJetTags"),
+    
     ### Run parameters
     RunParameters = cms.PSet(
-        GenJetMin = cms.double(0.0),
-        Monte = cms.bool(True),
-        CalJetMin = cms.double(0.0),
-        HistogramFile = cms.string('TEST.root'),
-        EtaMin = cms.double(-5.2),
-        Debug = cms.bool(False),
-        EtaMax = cms.double(5.2)
+        HistogramFile = cms.string('openhlt.root'),
+        CalJetMin     = cms.double(0.0),
+        GenJetMin     = cms.double(0.0),
+        EtaMin        = cms.double(-5.2),
+        EtaMax        = cms.double( 5.2),
+        Monte         = cms.bool(True),
+        Debug         = cms.bool(False)
     )
 )
