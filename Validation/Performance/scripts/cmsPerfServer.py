d3b0dbf6389e3d0e771e9980ab62f0a7965fb678
nicolson <sha1-9909c8fa703f7523d20fd07c3520f811a373fb85@cern.ch>
2008-09-16 08:55:41 +0000

    Reduced font size

--- Validation/Performance/scripts/cmsPerfServer.py
+++ Validation/Performance/scripts/cmsPerfServer.py
@@ -21,25 +21,41 @@ def optionparse():
                       metavar='<PORT>',
                       )
 
+    parser.add_option('-mc',
+                      '--max-clients',
+                      type="int",
+                      dest='clients',
+                      default=-1,
+                      help='Maximum number of clients, after all these clients have returned results the program will terminate',
+                      metavar='<CLIENTS>',
+                      )    
+
     (options, args) = parser.parse_args()
 
+    maxclient = 1
     port = 0
+    if options
     if options.port == -1:
         port = 8000
     else:
         port = options.port
 
-    return (options, port)
+    return (options, port, maxclients)
 
 def runserv(sport):
-# Remember that localhost is the loopback network: it does not provide
-# or require any connection to the outside world. As such it is useful
-# for testing purposes. If you want your server to be seen on other
-# machines, you must use your real network address in stead of
-# 'localhost'.    
+    # Remember that localhost is the loopback network: it does not provide
+    # or require any connection to the outside world. As such it is useful
+    # for testing purposes. If you want your server to be seen on other
+    # machines, you must use your real network address in stead of
+    # 'localhost'.    
     try:
         server = SimpleXMLRPCServer.SimpleXMLRPCServer(('localhost',sport))
         server.register_function(testfn)
+        server.register_function(testfn2)        
+        # while
+        #   1 number of finished requests does not equal max clients
+        #   2 and timeout has not been reached
+        #   3 and user has not killed program
         server.serve_forever()        
     except socket.error, detail:
         print "ERROR: Could not communicate with server:", detail
@@ -48,8 +64,17 @@ def getBenchmarkNumbers():
     cps.main("")
     cspp.main()
 
-def testfn():
+def testfn(host):
+    # check host does not exist in host array    
+    # store host in host array
+    # return cmsPerfSuite.py configuration options
+    #      1 client runs perfsuite
+    #      2 client should then run store function to send it's benchmarking data to the server
     return "test"
+
+def testfn2(host,info):
+    # store information from client
+    # mark client as finished benchmarking
 
 def _main():
     (options, sport) = optionparse()
