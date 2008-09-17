9e0e9f30464d4dbc6fa6864673f1b282152c5585
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2008-09-17 16:39:15 +0000

    updated TrackingMonitor_cfi.py changing histogram limits

--- DQM/TrackingMonitor/python/TrackingMonitor_cfi.py
+++ DQM/TrackingMonitor/python/TrackingMonitor_cfi.py
@@ -6,76 +6,100 @@
 import FWCore.ParameterSet.Config as cms
 
 TrackMon = cms.EDFilter("TrackingMonitor",
+    TrackProducer = cms.InputTag("generalTracks"),
+    AlgoName = cms.string('GenTk'),
+                        
     OutputMEsInRootFile = cms.bool(False),
-    phiErrMax = cms.double(5.0),
+    OutputFileName = cms.string('MonitorTrack.root'),
+
+    FolderName = cms.string('Track/GlobalParameters'),
+
     MeasurementState = cms.string('default'),
+                        
+    TkSizeBin = cms.int32(500),
+    TkSizeMin = cms.double(-0.5),
+    TkSizeMax = cms.double(499.5),
+
+    TrackPtBin = cms.int32(100),
+    TrackPtMin = cms.double(-0.5),
+    TrackPtMax = cms.double(30.0),
+
+    ptErrBin = cms.int32(500),
+    ptErrMin = cms.double(0.0),
+    ptErrMax = cms.double(1000.0),
+
+    D0Max = cms.double(0.5),
+    D0Min = cms.double(-0.5),                        
+    D0Bin = cms.int32(100),
+                        
+    etaErrBin = cms.int32(500),
+    etaErrMin = cms.double(0.0),
     etaErrMax = cms.double(0.5),
-    TrackPxBin = cms.int32(100),
-    VXMax = cms.double(20.0),
+
     VXBin = cms.int32(20),
+    VXMin = cms.double(-20.0),
+    VXMax = cms.double(20.0),
+
     RecHitBin = cms.int32(50),
-    VZBin = cms.int32(50),
-    TrackPzMin = cms.double(-50.0),
+    RecHitMin = cms.double(-0.5),
+    RecHitMax = cms.double(99.5),
+                        
     Chi2Max = cms.double(500.0),
     Chi2Bin = cms.int32(100),
-    TrackPzBin = cms.int32(100),
-    pxErrBin = cms.int32(1000),
-    VYMin = cms.double(-20.0),
-    etaErrMin = cms.double(-0.5),
-    TrackPxMax = cms.double(50.0),
-    TrackPzMax = cms.double(50.0),
-    VYMax = cms.double(20.0),
-    ThetaBin = cms.int32(100),
-    RecHitMin = cms.double(0.0),
-    OutputFileName = cms.string('MonitorTrack.root'),
-    #
-    FolderName = cms.string('Track/GlobalParameters'),
-    pzErrMin = cms.double(-1000.0),
-    EtaMin = cms.double(-4.0),
-    pErrBin = cms.int32(1000),
-    pyErrMin = cms.double(-1000.0),
-    VZMax = cms.double(100.0),
-    phiErrBin = cms.int32(500),
-    EtaMax = cms.double(4.0),
-    etaErrBin = cms.int32(500),
     Chi2Min = cms.double(-0.5),
-    ThetaMin = cms.double(0.0),
-    PhiMin = cms.double(-3.2),
-    TrackPtMax = cms.double(30.0),
-    RecHitMax = cms.double(150.0),
-    TrackPyBin = cms.int32(100),
-    ptErrMin = cms.double(0.0),
-    TkSizeMin = cms.double(-0.5),
-    TrackPxMin = cms.double(-50.0),
-    VXMin = cms.double(-20.0),
-    pyErrMax = cms.double(1000.0),
-    ThetaMax = cms.double(3.2),
-    pzErrMax = cms.double(1000.0),
-    pzErrBin = cms.int32(1000),
-    pErrMin = cms.double(-1000.0),
-    EtaBin = cms.int32(100),
-    VZMin = cms.double(-100.0),
-    pErrMax = cms.double(1000.0),
+
     VYBin = cms.int32(20),
-    pxErrMax = cms.double(1000.0),
-    TkSizeBin = cms.int32(500),
-    AlgoName = cms.string('GenTk'),
-    TrackPyMin = cms.double(-50.0),
-    D0Max = cms.double(0.5),
-    D0Bin = cms.int32(100),
-    TrackProducer = cms.InputTag("generalTracks"),
-    TkSizeMax = cms.double(499.5),
-    TrackPtBin = cms.int32(100),
-    TrackPyMax = cms.double(50.0),
-    D0Min = cms.double(-0.5),
-    phiErrMin = cms.double(-5.0),
-    pyErrBin = cms.int32(1000),
-    pxErrMin = cms.double(-1000.0),
+    VYMin = cms.double(-20.0),
+    VYMax = cms.double(20.0),
+
+    VZBin = cms.int32(50),
+    VZMin = cms.double(-100.0),
+    VZMax = cms.double(100.0),
+
+    TrackPzBin = cms.int32(100),
+    TrackPzMin = cms.double(-50.0),
+    TrackPzMax = cms.double(50.0),
+                        
+    ThetaBin = cms.int32(100),
+    ThetaMin = cms.double(0.0),
+    ThetaMax = cms.double(3.2),
+
+    EtaBin = cms.int32(100),
+    EtaMin = cms.double(-4.0),
+    EtaMax = cms.double(4.0),
+
     PhiBin = cms.int32(100),
-    ptErrMax = cms.double(1000.0),
+    PhiMin = cms.double(-3.2),
     PhiMax = cms.double(3.2),
-    TrackPtMin = cms.double(-0.5),
-    ptErrBin = cms.int32(500)
+
+    phiErrBin = cms.int32(500),
+    phiErrMin = cms.double(-5.0),
+    phiErrMax = cms.double(0.5),
+
+    TrackPxBin = cms.int32(100),
+    TrackPxMin = cms.double(-50.0),
+    TrackPxMax = cms.double(50.0),
+
+    TrackPyBin = cms.int32(100),
+    TrackPyMin = cms.double(-50.0),
+    TrackPyMax = cms.double(50.0),
+
+    pErrBin = cms.int32(1000),
+    pErrMin = cms.double(-1000.0),
+    pErrMax = cms.double(1000.0),
+
+    pxErrBin = cms.int32(1000),
+    pxErrMax = cms.double(1000.0),
+    pxErrMin = cms.double(-1000.0),
+
+    pyErrBin = cms.int32(1000),                        
+    pyErrMin = cms.double(-1000.0),
+    pyErrMax = cms.double(1000.0),
+
+    pzErrBin = cms.int32(1000),
+    pzErrMin = cms.double(-1000.0),
+    pzErrMax = cms.double(1000.0)
+
 )
 
 
