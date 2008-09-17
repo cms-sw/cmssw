dd68361fbaf41cb0ef822a43c37a49f1938f240f
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-09-17 13:49:42 +0000

    changed InputTag

--- DQM/TrigXMonitor/python/L1Scalers_cfi.py
+++ DQM/TrigXMonitor/python/L1Scalers_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: L1Scalers_cfi.py,v 1.4 2008/09/04 19:44:01 wittich Exp $
+# $Id: L1Scalers_cfi.py,v 1.5 2008/09/16 17:19:13 wittich Exp $
 
 l1s = cms.EDFilter("L1Scalers",
                    l1GtData = cms.InputTag("l1GtUnpack","","HLT"),
@@ -10,5 +10,5 @@ l1s = cms.EDFilter("L1Scalers",
                    lastFED = cms.untracked.uint32(39),
                    fedRawData = cms.InputTag("source","", ""),
                    maskedChannels = cms.untracked.vint32(),
-                   HFRecHitCollection = cms.InputTag("HFRecHitCollection", "", "")
+                   HFRecHitCollection = cms.InputTag("hfreco", "", "")
                    )
