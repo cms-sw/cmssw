4966b80111f3dd177187af9586902d2d3fcfe676
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2008-09-17 17:43:15 +0000

    Python config file for test jobs

---
+++ L1Trigger/GlobalCaloTrigger/python/test/gctTest_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1TriggerConfig.GctConfigProducers.L1GctConfig_cff import *
+from L1TriggerConfig.L1ScalesProducers.L1CaloScalesConfig_cff import *
+from L1TriggerConfig.L1GeometryProducers.l1CaloGeomConfig_cff import *
+gctemu = cms.EDFilter("L1GctTest",
+    doFirmware = cms.untracked.bool(False),
+    doElectrons = cms.untracked.bool(False),
+    doEnergyAlgos = cms.untracked.bool(False),
+    inputFile = cms.untracked.string(''),
+    energySumsFile = cms.untracked.string(''),
+    referenceFile = cms.untracked.string(''),
+    preSamples = cms.uint32(2),
+    postSamples = cms.uint32(2)
+)
+
