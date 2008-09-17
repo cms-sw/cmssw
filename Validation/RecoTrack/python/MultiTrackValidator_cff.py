29c1edb6aae4fb2991e45a4495fda3ccaa69d131
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2008-09-17 16:45:36 +0000

    fix DQMstore parameter

--- Validation/RecoTrack/python/MultiTrackValidator_cff.py
+++ Validation/RecoTrack/python/MultiTrackValidator_cff.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.MagneticField_cff import *
 from DQMServices.Core.DQM_cfg import *
+DQMStore.collateHistograms =cms.untracked.bool(True)
 from Validation.RecoTrack.MultiTrackValidator_cfi import *
 
