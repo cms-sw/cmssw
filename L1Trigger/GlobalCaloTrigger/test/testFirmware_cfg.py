73dee840b324c727d600d0f8a728893740204b27
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2008-09-17 17:11:24 +0000

    Translating config files for test jobs to python

---
+++ L1Trigger/GlobalCaloTrigger/test/testFirmware_cfg.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TestGct")
+process.load("L1Trigger.GlobalCaloTrigger.test.gctTest_cff")
+
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+process.p1 = cms.Path(process.gctemu)
+process.gctemu.doFirmware = True
+process.gctemu.inputFile = 'PythiaData.txt'
+process.gctemu.referenceFile = 'PythiaJets.txt'
+process.gctemu.energySumsFile = 'PythiaEsums.txt'
+
