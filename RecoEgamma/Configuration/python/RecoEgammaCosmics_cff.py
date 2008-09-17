054823f95f4aa08e3dc0fab9e831ef60e3593254
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-09-17 20:55:05 +0000

    Include photonIDSequence

--- RecoEgamma/Configuration/python/RecoEgammaCosmics_cff.py
+++ RecoEgamma/Configuration/python/RecoEgammaCosmics_cff.py
@@ -4,9 +4,9 @@ from RecoEgamma.EgammaElectronProducers.cosmicElectronSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.cosmicPhotonSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.cosmicConversionSequence_cff import *
 #from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
-#from RecoEgamma.PhotonIdentification.photonId_cff import *
+from RecoEgamma.PhotonIdentification.photonId_cff import *
 #from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
 egammarecoCosmics_woConvPhotons = cms.Sequence(cosmicElectronSequence*cosmicPhotonSequence)
-egammarecoCosmics = cms.Sequence(cosmicElectronSequence*cosmicConversionSequence*cosmicPhotonSequence)
-egammarecoCosmics_woElectrons = cms.Sequence(cosmicConversionSequence*cosmicPhotonSequence)
+egammarecoCosmics = cms.Sequence(cosmicElectronSequence*cosmicConversionSequence*cosmicPhotonSequence*photonIDSequence)
+egammarecoCosmics_woElectrons = cms.Sequence(cosmicConversionSequence*cosmicPhotonSequence*photonIDSequence)
