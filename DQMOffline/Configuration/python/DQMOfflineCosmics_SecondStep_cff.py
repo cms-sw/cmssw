41744725ceaa179dbda5a0e18fbff4defccd6b28
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-09-17 15:53:10 +0000

    add a woHcal sequence

--- DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
@@ -6,5 +6,6 @@ from DQM.HcalMonitorModule.hcal_dqm_client_fileT0_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_client_cff import *
 from DQMOffline.Trigger.DQMOffline_Trigger_Client_cff import *
 
-DQMOfflineCosmics_SecondStep = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient)
 
+DQMOfflineCosmics_SecondStep = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient)
+DQMOfflineCosmics_SecondStep_woHcal = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*sipixelEDAClient*triggerOfflineDQMClient)
