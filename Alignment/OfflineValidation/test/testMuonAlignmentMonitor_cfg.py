c2c728d88d7f705a7f61da141a04c715d85f4c21
Javier Fernandez Menendez <sha1-96a07b193caeb9d667466687d23e726533fc5d4f@cern.ch>
2008-09-18 15:33:33 +0000

    Final python version for cfi file

--- Alignment/OfflineValidation/test/testMuonAlignmentMonitor_cfg.py
+++ Alignment/OfflineValidation/test/testMuonAlignmentMonitor_cfg.py
@@ -1,70 +1,29 @@
-# The following comments couldn't be translated into the new config version:
-
-# FRONTIER
-
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("MuonAlignmentMonitor")
-# Messages
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-# magnetic field
-process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
 
-# ideal geometry
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+# ideal geometry and interface
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
 
 process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
 
-process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
 process.load("Geometry.CommonDetUnit.bareGlobalTrackingGeometry_cfi")
 
-process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
-
-process.load("Geometry.DTGeometry.dtGeometry_cfi")
-
-process.load("Geometry.CSCGeometry.cscGeometry_cfi")
-
-process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
-
-process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
-
-# Muon alignment: from Frontier 
-# it contain replaces
-# Geometry to use in the extrapolator
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
-
-process.load("Alignment.CommonAlignmentProducer.GlobalPosition_Frontier_DevDB_cff")
-
 process.source = cms.Source("PoolSource",
     #AlCaReco File
-    fileNames = cms.untracked.vstring('/store/mc/CSA08/MuonPT11/ALCARECO/1PB_V2_RECO_MuAlZMuMu_v1/0029/04D75FB7-E624-DD11-A822-001D09F2437B.root')
+    fileNames = cms.untracked.vstring('MuAlZMuMu_ALCARECO.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
-)
-process.muonAlignment = cms.ESSource("PoolDBESSource",
-    process.CondDBSetup,
-    timetype = cms.string('runnumber'),
-    toGet = cms.VPSet(cms.PSet(
-        record = cms.string('DTAlignmentRcd'),
-        tag = cms.string('DT1InversepbScenario200v1_mc')
-    ), 
-        cms.PSet(
-            record = cms.string('DTAlignmentErrorRcd'),
-            tag = cms.string('DT1InversepbScenarioErrors200v1_mc')
-        ), 
-        cms.PSet(
-            record = cms.string('CSCAlignmentRcd'),
-            tag = cms.string('CSC1InversepbScenario200v1_mc')
-        ), 
-        cms.PSet(
-            record = cms.string('CSCAlignmentErrorRcd'),
-            tag = cms.string('CSC1InversepbScenarioErrors200v1_mc')
-        )),
-    connect = cms.string('frontier://FrontierDev/CMS_COND_ALIGNMENT')
 )
 
 process.load("Alignment.OfflineValidation.MuonAlignmentAnalyzer_cfi")
@@ -77,7 +36,6 @@ process.TFileService = cms.Service("TFileService",
 )
 
 process.p = cms.Path(process.MuonAlignmentMonitor)
-process.DTGeometryESModule.applyAlignment = True
-process.CSCGeometryESModule.applyAlignment = True
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
 
 
