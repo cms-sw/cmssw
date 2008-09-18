9a51db929d1fc964447f48f12d068d987fd602d5
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-09-18 17:21:26 +0000

    sequence for cosmic reco

--- RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
@@ -23,22 +23,32 @@ from RecoMuon.MuonIsolationProducers.muIsolation_EventContent_cff import *
 # AOD content
 RecoMuonAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoTracks_standAloneMuons_*_*', 
-        'keep recoTrackExtras_standAloneMuons_*_*', 
-        'keep TrackingRecHitsOwned_standAloneMuons_*_*', 
-        'keep recoTracks_globalMuons_*_*', 
-        'keep recoTrackExtras_globalMuons_*_*', 
-        'keep recoTracks_tevMuons_*_*', 
-        'keep recoTrackExtras_tevMuons_*_*', 
-        'keep recoTracksToOnerecoTracksAssociation_tevMuons_*_*', 
-        'keep recoTracks_generalTracks_*_*', 
-        'keep recoMuons_muons_*_*', 
-        'keep recoCaloMuons_calomuons_*_*')
+                                           'keep recoTrackExtras_standAloneMuons_*_*', 
+                                           'keep TrackingRecHitsOwned_standAloneMuons_*_*', 
+                                           'keep recoTracks_globalMuons_*_*', 
+                                           'keep recoTrackExtras_globalMuons_*_*', 
+                                           'keep recoTracks_tevMuons_*_*', 
+                                           'keep recoTrackExtras_tevMuons_*_*', 
+                                           'keep recoTracksToOnerecoTracksAssociation_tevMuons_*_*', 
+                                           'keep recoTracks_generalTracks_*_*', 
+                                           'keep recoMuons_muons_*_*', 
+                                           'keep recoCaloMuons_calomuons_*_*',
+                                           # Cosmics
+                                           'keep recoTracks_cosmicMuons_*_*',
+                                           'keep recoTrackExtras_cosmicMuons_*_*', 
+                                           'keep TrackingRecHitsOwned_cosmicMuons_*_*',
+                                           'keep recoTracks_globalCosmicMuons_*_*', 
+                                           'keep recoTrackExtras_globalCosmicMuons_*_*', 
+                                           'keep TrackingRecHitsOwned_globalCosmicMuons_*_*',
+                                           'keep recoMuons_muonsFromCosmics_*_*')
 )
 # RECO content
 RecoMuonRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_MuonSeed_*_*', 
-        'keep TrackingRecHitsOwned_globalMuons_*_*', 
-        'keep TrackingRecHitsOwned_tevMuons_*_*')
+                                           'keep TrackingRecHitsOwned_globalMuons_*_*', 
+                                           'keep TrackingRecHitsOwned_tevMuons_*_*',
+                                           # Cosmics
+                                           'keep *_CosmicMuonSeed_*_*')
 )
 # Full Event content 
 RecoMuonFEVT = cms.PSet(
