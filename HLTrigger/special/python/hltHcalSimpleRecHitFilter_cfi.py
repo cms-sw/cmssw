4c204ee9145f98540d91be46a582b49ca04b25cd
Bryan Dahmes <sha1-ad386f655f6fb04a44f83e832ee96bb49b44ff9d@cern.ch>
2008-09-18 09:37:18 +0000

    Fix name of hltHcalSimpleRecHitFilter

--- HLTrigger/special/python/hltHcalSimpleRecHitFilter_cfi.py
+++ HLTrigger/special/python/hltHcalSimpleRecHitFilter_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-hltHcalSimpleRecHITFilter = cms.EDFilter("HLTHcalSimpleRecHitFilter",
+hltHcalSimpleRecHitFilter = cms.EDFilter("HLTHcalSimpleRecHitFilter",
    threshold = cms.double(0),
    maskedChannels = cms.vint32(),
    HFRecHitCollection = cms.InputTag("","","")
