22130b754f43aabce5926fe1dabf39448c51febb
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-09-18 12:21:26 +0000

    unsigned int as in C++ code.

--- HLTrigger/special/python/hltFEDSizeFilter_cfi.py
+++ HLTrigger/special/python/hltFEDSizeFilter_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 # FED size first designed for Pixels, but can be generalized to any detector
 hltFEDSizeFilter = cms.EDFilter("HLTFEDSizeFilter",
-   threshold = cms.int32(0),
-   firstFED = cms.int32(0),
-   lastFED = cms.int32(39),
+   threshold = cms.uint32(0),
+   firstFED = cms.uint32(0),
+   lastFED = cms.uint32(39),
    rawData = cms.InputTag("","","")
 )
