45139449501ee010b8d969940dd0e69743e74ac6
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-09-18 01:50:31 +0000

    First commit

---
+++ HLTriggerOffline/Muon/python/fileList_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+fileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_2_1_8/RelValZMM/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v1/0002/F42E5F83-8E82-DD11-9E6E-00161757BF42.root'
+)
