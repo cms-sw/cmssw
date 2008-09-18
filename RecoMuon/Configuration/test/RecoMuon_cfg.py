87f425eb67ca4f87507a723be58f70fd7192b758
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-09-18 17:35:10 +0000

    test cosmics as well

--- RecoMuon/Configuration/test/RecoMuon_cfg.py
+++ RecoMuon/Configuration/test/RecoMuon_cfg.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("RecoMuon")
 # Messages
-process.load("RecoMuon.Configuration.MessageLogger_cfi")
+#process.load("RecoMuon.Configuration.MessageLogger_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # Muon Reco
 process.load("RecoLocalMuon.Configuration.RecoLocalMuon_cff")
@@ -17,19 +18,25 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/tmp/bellan/trunkedGLB_210_pre11.root')
+    fileNames = cms.untracked.vstring('file:muPt10_1.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(2)
 )
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('RecoMuons.root')
 )
 
-process.p = cms.Path(process.muonreco)
+process.p = cms.Path(process.RawToDigi*process.trackerlocalreco*process.ckftracks*process.muonreco_plus_isolation*process.muoncosmicreco)
 process.this_is_the_end = cms.EndPath(process.out)
-process.GlobalTag.globaltag = 'IDEAL_V5::All'
+
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
 
 
