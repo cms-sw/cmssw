cfee2c62377c445c37b5040a38ad414c10b59266
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2008-09-19 18:24:42 +0000

    updating config files chaning histogram range

--- DQM/SiStripMonitorCluster/python/SiStripMonitorCluster_cfi.py
+++ DQM/SiStripMonitorCluster/python/SiStripMonitorCluster_cfi.py
@@ -4,80 +4,94 @@ import FWCore.ParameterSet.Config as cms
 SiStripMonitorCluster = cms.EDFilter("SiStripMonitorCluster",
     # by default do not write out any file with histograms
     # can overwrite this in .cfg file with: replace SiStripMonitorCluster.OutputMEsInRootFile = true
+    ClusterProducer = cms.string('siStripClusters'),
+                                     
     OutputMEsInRootFile = cms.bool(False),
+    OutputFileName = cms.string('SiStripMonitorCluster.root'),
+                                     
     CreateTrendMEs = cms.bool(False),
+    ResetMEsEachRun = cms.bool(False),
+
+    StripQualityLabel = cms.string(''),
+
+    SelectAllDetectors = cms.bool(False),
+    ShowMechanicalStructureView = cms.bool(True),
+
+    ClusterLabel = cms.string(''),
+
     Trending = cms.PSet(
         UpdateMode = cms.int32(1),
-        Nbins = cms.int32(10),
-        ymax = cms.double(10000.0),
-        Steps = cms.int32(10),
-        xmax = cms.double(10.0),
-        xmin = cms.double(0.0),
-        ymin = cms.double(0.0)
+        Nbins      = cms.int32(10),
+        ymax       = cms.double(10000.0),
+        Steps      = cms.int32(10),
+        xmax       = cms.double(10.0),
+        xmin       = cms.double(0.0),
+        ymin       = cms.double(0.0)
     ),
     TH1ClusterNoise = cms.PSet(
-        xmin = cms.double(0.0),
-        layerswitchon = cms.bool(True),
-        Nbinx = cms.int32(20),
-        xmax = cms.double(10.0),
+        Nbinx          = cms.int32(20),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(9.5),
+        layerswitchon  = cms.bool(True),
         moduleswitchon = cms.bool(True)
     ),
-    ResetMEsEachRun = cms.bool(False),
-    ClusterConditions = cms.PSet(
-        minWidth = cms.double(0.0),
-        On = cms.bool(True),
-        maxStoN = cms.double(10000.0),
-        minStoN = cms.double(0.0),
-        maxWidth = cms.double(10000.0)
-    ),
+
     TH1NrOfClusterizedStrips = cms.PSet(
-        xmin = cms.double(0.0),
-        layerswitchon = cms.bool(True),
-        Nbinx = cms.int32(20),
-        xmax = cms.double(100.0),
+        Nbinx          = cms.int32(20),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(99.5),
+        layerswitchon  = cms.bool(True),
         moduleswitchon = cms.bool(True)
     ),
     TH1ClusterPos = cms.PSet(
-        xmin = cms.double(-0.5),
-        layerswitchon = cms.bool(False),
-        Nbinx = cms.int32(768),
-        xmax = cms.double(767.5),
+        Nbinx          = cms.int32(768),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(767.5),
+        layerswitchon  = cms.bool(False),
         moduleswitchon = cms.bool(True)
     ),
-    OutputFileName = cms.string('SiStripMonitorCluster.root'),
-    #
-    SelectAllDetectors = cms.bool(False),
-    ShowMechanicalStructureView = cms.bool(True),
     TH1ModuleLocalOccupancy = cms.PSet(
-        xmin = cms.double(-0.5),
-        layerswitchon = cms.bool(True),
-        Nbinx = cms.int32(20),
-        xmax = cms.double(0.95),
+        Nbinx          = cms.int32(20),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(0.95),
+        layerswitchon  = cms.bool(True),
         moduleswitchon = cms.bool(True)
     ),
     TH1nClusters = cms.PSet(
-        xmin = cms.double(-0.5),
-        layerswitchon = cms.bool(False),
-        Nbinx = cms.int32(11),
-        xmax = cms.double(10.5),
+        Nbinx          = cms.int32(11),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(10.5),
+        layerswitchon  = cms.bool(False),
         moduleswitchon = cms.bool(True)
     ),
-    ClusterLabel = cms.string(''),
     TH1ClusterStoN = cms.PSet(
-        xmin = cms.double(0.0),
-        layerswitchon = cms.bool(False),
-        Nbinx = cms.int32(60),
-        xmax = cms.double(200.0),
+        Nbinx          = cms.int32(100),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(299.5),
+        layerswitchon  = cms.bool(False),
         moduleswitchon = cms.bool(True)
     ),
-    StripQualityLabel = cms.string(''),
-    ClusterProducer = cms.string('siStripClusters'),
     TH1ClusterCharge = cms.PSet(
-        xmin = cms.double(0.0),
-        layerswitchon = cms.bool(True),
-        Nbinx = cms.int32(100),
-        xmax = cms.double(500.0),
+        Nbinx          = cms.int32(200),
+        xmin           = cms.double(-0.5),        
+        xmax           = cms.double(799.5),
+        layerswitchon  = cms.bool(True),
         moduleswitchon = cms.bool(True)
+    ),
+    TH1ClusterWidth = cms.PSet(
+        Nbinx          = cms.int32(20),
+        xmin           = cms.double(-0.5),
+        xmax           = cms.double(19.5),
+        layerswitchon  = cms.bool(True),        
+        moduleswitchon = cms.bool(True)
+    ),
+
+    ClusterConditions = cms.PSet(
+        minWidth   = cms.double(0.0),
+        On         = cms.bool(True),
+        maxStoN    = cms.double(10000.0),
+        minStoN    = cms.double(0.0),
+        maxWidth   = cms.double(10000.0)
     ),
     #select detectors
     detectorson = cms.PSet(
@@ -86,15 +100,7 @@ SiStripMonitorCluster = cms.EDFilter("SiStripMonitorCluster",
         tecon = cms.bool(True),
         tobon = cms.bool(True)
     ),
-    TH1ClusterWidth = cms.PSet(
-        xmin = cms.double(-0.5),
-        layerswitchon = cms.bool(True),
-        Nbinx = cms.int32(20),
-        xmax = cms.double(19.5),
-        moduleswitchon = cms.bool(True)
-    ),
+
     ShowControlView = cms.bool(False),
     ShowReadoutView = cms.bool(False)
 )
-
-
