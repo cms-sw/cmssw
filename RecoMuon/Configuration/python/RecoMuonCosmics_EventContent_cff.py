c06518ec6ecb1cd32836505fd4544b388a649516
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-09-19 10:28:33 +0000

    cleaning and added beam halo

--- RecoMuon/Configuration/python/RecoMuonCosmics_EventContent_cff.py
+++ RecoMuon/Configuration/python/RecoMuonCosmics_EventContent_cff.py
@@ -1,118 +1,97 @@
-# The following comments couldn't be translated into the new config version:
-
-# Stand Alone's tracks with extra and hits
-#inclusive cosmic reco
-
-# cosimic reco in barrel only
-
-# cosimic reco in endcaps only
-
-# cosimic reco "1 Leg type" in barrel only
-
-# cosimic reco in barrel only, based on DT no drift algo
-
-# lhc like reco in barrel only
-
-# lhc like reco in endcap only
-
-# Global's tracks with extra and hits
-#inclusive cosmic reco
-
-# cosimic reco in barrel only
-
-# cosimic reco "1 Leg type" in barrel only
-
-# cosimic reco in barrel only, based on DT no drift algo
-
-# Tracker's Tracks without extra and hits
-
-# Muon Id
-#inclusive cosmic reco
-
-# cosimic reco in barrel only
-
-# cosimic reco in endcaps only
-
-# cosimic reco "1 Leg type" in barrel only
-
-# cosimic reco in barrel only, based on DT no drift algo
-
-# Seed
-#inclusive cosmic reco
-
-# cosimic reco in barrel only
-
-# cosimic reco in endcaps only
-
-# cosimic reco in barrel only, based on DT no drift algo
-
-# lhc like reco in barrel only
-
-# lhc like reco in endcaps only
-
 import FWCore.ParameterSet.Config as cms
 
 #Add Isolation
 from RecoMuon.MuonIsolationProducers.muIsolation_EventContent_cff import *
 # AOD content
 RecoMuonAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoTracks_cosmicMuons_*_*', 
-        'keep recoTrackExtras_cosmicMuons_*_*', 
-        'keep TrackingRecHitsOwned_cosmicMuons_*_*', 
-        'keep recoTracks_cosmicMuonsBarrelOnly_*_*', 
-        'keep recoTrackExtras_cosmicMuonsBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_cosmicMuonsBarrelOnly_*_*', 
-        'keep recoTracks_cosmicMuonsEndCapsOnly_*_*', 
-        'keep recoTrackExtras_cosmicMuonsEndCapsOnly_*_*', 
-        'keep TrackingRecHitsOwned_cosmicMuonsEndCapsOnly_*_*', 
-        'keep recoTracks_cosmicMuons1LegBarrelOnly_*_*', 
-        'keep recoTrackExtras_cosmicMuons1LegBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_cosmicMuons1LegBarrelOnly_*_*', 
-        'keep recoTracks_cosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep recoTrackExtras_cosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_cosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep recoTracks_lhcStandAloneMuonsBarrelOnly_*_*', 
-        'keep recoTrackExtras_lhcStandAloneMuonsBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_lhcStandAloneMuonsBarrelOnly_*_*', 
-        'keep recoTracks_lhcStandAloneMuonsEndCapsOnly_*_*', 
-        'keep recoTrackExtras_lhcStandAloneMuonsEndCapsOnly_*_*', 
-        'keep TrackingRecHitsOwned_lhcStandAloneMuonsEndCapsOnly_*_*', 
-        'keep recoTracks_globalCosmicMuons_*_*', 
-        'keep recoTrackExtras_globalCosmicMuons_*_*', 
-        'keep TrackingRecHitsOwned_globalCosmicMuons_*_*', 
-        'keep recoTracks_globalCosmicMuonsBarrelOnly_*_*', 
-        'keep recoTrackExtras_globalCosmicMuonsBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_globalCosmicMuonsBarrelOnly_*_*', 
-        'keep recoTracks_globalCosmicMuons1LegBarrelOnly_*_*', 
-        'keep recoTrackExtras_globalCosmicMuons1LegBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_globalCosmicMuons1LegBarrelOnly_*_*', 
-        'keep recoTracks_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep recoTrackExtras_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep TrackingRecHitsOwned_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
-        'keep recoTracks_ctfWithMaterialTracksP5_*_*', 
-        'keep recoMuons_muons_*_*', 
-        'keep recoMuons_STAMuons_*_*', 
-        'keep recoMuons_TKMuons_*_*', 
-        'keep recoMuons_GLBMuons_*_*', 
-        'keep recoMuons_muonsBarrelOnly_*_*', 
-        'keep recoMuons_STAMuonsBarrelOnly_*_*', 
-        'keep recoMuons_GLBMuonsBarrelOnly_*_*', 
-        'keep recoMuons_STAMuonsEndCapsOnly_*_*', 
-        'keep recoMuons_muons1LegBarrelOnly_*_*', 
-        'keep recoMuons_STAMuons1LegBarrelOnly_*_*', 
-        'keep recoMuons_GLBMuons1LegBarrelOnly_*_*', 
-        'keep recoMuons_muonsNoDriftBarrelOnly_*_*', 
-        'keep recoMuons_STAMuonsNoDriftBarrelOnly_*_*', 
-        'keep recoMuons_GLBMuonsNoDriftBarrelOnly_*_*')
-)
+    
+    outputCommands = cms.untracked.vstring(
+    # inclusive cosmic reco
+    'keep recoTracks_cosmicMuons_*_*', 
+    'keep recoTrackExtras_cosmicMuons_*_*', 
+    'keep TrackingRecHitsOwned_cosmicMuons_*_*', 
+    'keep recoTracks_globalCosmicMuons_*_*', 
+    'keep recoTrackExtras_globalCosmicMuons_*_*', 
+    'keep TrackingRecHitsOwned_globalCosmicMuons_*_*', 
+    'keep recoMuons_muons_*_*', 
+    'keep recoMuons_STAMuons_*_*', 
+    'keep recoMuons_TKMuons_*_*', 
+    'keep recoMuons_GLBMuons_*_*', 
+
+
+    # cosimic reco in barrel only
+    'keep recoTracks_cosmicMuonsBarrelOnly_*_*', 
+    'keep recoTrackExtras_cosmicMuonsBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_cosmicMuonsBarrelOnly_*_*', 
+    'keep recoTracks_globalCosmicMuonsBarrelOnly_*_*', 
+    'keep recoTrackExtras_globalCosmicMuonsBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_globalCosmicMuonsBarrelOnly_*_*', 
+    'keep recoMuons_muonsBarrelOnly_*_*', 
+    'keep recoMuons_STAMuonsBarrelOnly_*_*', 
+    'keep recoMuons_GLBMuonsBarrelOnly_*_*',
+    
+
+    # cosimic reco "1 Leg type" in barrel only
+    'keep recoTracks_cosmicMuons1LegBarrelOnly_*_*', 
+    'keep recoTrackExtras_cosmicMuons1LegBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_cosmicMuons1LegBarrelOnly_*_*', 
+    'keep recoTracks_globalCosmicMuons1LegBarrelOnly_*_*', 
+    'keep recoTrackExtras_globalCosmicMuons1LegBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_globalCosmicMuons1LegBarrelOnly_*_*', 
+    'keep recoMuons_muons1LegBarrelOnly_*_*', 
+    'keep recoMuons_STAMuons1LegBarrelOnly_*_*', 
+    'keep recoMuons_GLBMuons1LegBarrelOnly_*_*', 
+
+    
+    # cosimic reco in barrel only, based on DT no drift algo
+    'keep recoTracks_cosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep recoTrackExtras_cosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_cosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep recoTracks_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep recoTrackExtras_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep TrackingRecHitsOwned_globalCosmicMuonsNoDriftBarrelOnly_*_*', 
+    'keep recoMuons_muonsNoDriftBarrelOnly_*_*', 
+    'keep recoMuons_STAMuonsNoDriftBarrelOnly_*_*', 
+    'keep recoMuons_GLBMuonsNoDriftBarrelOnly_*_*',
+
+
+    # cosimic reco in endcaps only
+    'keep recoTracks_cosmicMuonsEndCapsOnly_*_*', 
+    'keep recoTrackExtras_cosmicMuonsEndCapsOnly_*_*', 
+    'keep TrackingRecHitsOwned_cosmicMuonsEndCapsOnly_*_*', 
+    'keep recoTracks_globalCosmicMuonsEndCapsOnly_*_*', 
+    'keep recoTrackExtras_globalCosmicMuonsEndCapsOnly_*_*', 
+    'keep TrackingRecHitsOwned_globalCosmicMuonsEndCapsOnly_*_*', 
+    'keep recoMuons_muonsEndCapsOnly_*_*',
+    'keep recoMuons_STAMuonsEndCapsOnly_*_*',
+    'keep recoMuons_GLBMuonsEndCapsOnly_*_*',
+    
+    # Beam halo in Encaps only
+    'keep recoTracks_globalBeamHaloMuonEndCapslOnly_*_*', 
+    'keep recoTrackExtras_globalBeamHaloMuonEndCapslOnly_*_*', 
+    'keep TrackingRecHitsOwned_globalBeamHaloMuonEndCapslOnly_*_*',
+    'keep recoMuons_muonsBeamHaloEndCapsOnly_*_*',
+    
+    # lhc like reco
+    'keep recoTracks_lhcStandAloneMuonsBarrelOnly_*_*', 
+    'keep recoTrackExtras_lhcStandAloneMuonsBarrelOnly_*_*',
+    'keep TrackingRecHitsOwned_lhcStandAloneMuonsBarrelOnly_*_*', 
+    'keep recoTracks_lhcStandAloneMuonsEndCapsOnly_*_*', 
+    'keep recoTrackExtras_lhcStandAloneMuonsEndCapsOnly_*_*', 
+    'keep TrackingRecHitsOwned_lhcStandAloneMuonsEndCapsOnly_*_*', 
+
+    # Tracker Collections
+    'keep recoTracks_ctfWithMaterialTracksP5_*_*', 
+    'keep recoTracks_ctfWithMaterialTracksBeamHaloMuon_*_*')
+    )
 # RECO content
 RecoMuonRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_CosmicMuonSeed_*_*', 
-        'keep *_CosmicMuonSeedBarrelOnly_*_*', 
-        'keep *_CosmicMuonSeedEndCapsOnly_*_*', 
-        'keep *_CosmicMuonSeedNoDriftBarrelOnly_*_*', 
-        'keep *_lhcMuonSeedBarrelOnly_*_*', 
-        'keep *_lhcMuonSeedEndCapsOnly_*_*')
+                                           'keep *_CosmicMuonSeedBarrelOnly_*_*', 
+                                           'keep *_CosmicMuonSeedEndCapsOnly_*_*', 
+                                           'keep *_CosmicMuonSeedNoDriftBarrelOnly_*_*', 
+                                           'keep *_lhcMuonSeedBarrelOnly_*_*', 
+                                           'keep *_lhcMuonSeedEndCapsOnly_*_*')
 )
 # Full Event content 
 RecoMuonFEVT = cms.PSet(
