c1e357e8c72112bdf33805e8271e37df15625a29
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-09-19 19:43:05 +0000

    Added subsystemKey parameters.

--- CondTools/L1Trigger/python/L1TriggerKeyDummy_cfi.py
+++ CondTools/L1Trigger/python/L1TriggerKeyDummy_cfi.py
@@ -2,9 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 from CondTools.L1Trigger.L1SubsystemParams_cfi import *
 L1TriggerKeyDummy = cms.ESProducer("L1TriggerKeyDummyProd",
-    subsystemKeys = cms.VPSet(),
-    tscKey = cms.string('dummy')
+    objectKeys = cms.VPSet(),
+    tscKey = cms.string('dummy'),
+    csctfKey = cms.string('dummy'),
+    dttfKey = cms.string('dummy'),
+    rpcKey = cms.string('dummy'),
+    gmtKey = cms.string('dummy'),
+    rctKey = cms.string('dummy'),
+    gctKey = cms.string('dummy'),
+    gtKey = cms.string('dummy'),
+    tsp0Key = cms.string('dummy'),
+    subsystemKeysOnly = cms.bool(False)
 )
 
-L1TriggerKeyDummy.subsystemKeys.extend(L1SubsystemParams.recordInfo)
+L1TriggerKeyDummy.objectKeys.extend(L1SubsystemParams.recordInfo)
 
