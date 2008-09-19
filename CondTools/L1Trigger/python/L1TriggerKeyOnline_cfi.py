1311f551f64c287fb799d1b3f783ce0fdae8a44d
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2008-09-19 19:43:51 +0000

    Added subsystemLabels to specify which L1TriggerKeys to collate.

--- CondTools/L1Trigger/python/L1TriggerKeyOnline_cfi.py
+++ CondTools/L1Trigger/python/L1TriggerKeyOnline_cfi.py
@@ -1,10 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 L1TriggerKeyOnline = cms.ESProducer("L1TriggerKeyOnlineProd",
-    onlineAuthentication = cms.string('.'),
-    tscKey = cms.string('dummy'),
-    onlineDB = cms.string('oracle://CMS_OMDS_LB/CMS_TRG_R'),
-    recordsToInclude = cms.vstring()
+    subsystemLabels = cms.vstring( 'CSCTF', 'DTTF', 'RPC', 'GMT',
+                                   'RCT', 'GCT', 'GT', 'TSP0' )
 )
 
 
