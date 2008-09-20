c01c20452b274e4e201f49e51bc5b64532fc7d0d
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-09-20 19:14:01 +0000

    - fix SiStrip sequences

--- DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Muon.muonCosmicMonitors_cff import *
 from DQMOffline.Ecal.ecal_dqm_source_offline_cosmic_cff import *
-from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
+from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_Cosmic_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -2,16 +2,17 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Muon.muonMonitors_cff import *
 from DQMOffline.Ecal.ecal_dqm_source_offline_cff import *
-#from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
+from DQM.SiStripMonitorClient.SiStripSourceConfigTier0_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETAnalyzer_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
 from DQMOffline.EGamma.cosmicPhotonAnalyzer_cff import *
+#from DQMOffline.EGamma.egammaDQMOffline_cff import *
 from DQMOffline.Trigger.DQMOffline_Trigger_cff import *
 #from DQMOffline.RecoB.dqmAnalyzer_cff import *
 #from DQMOffline.RecoB.PrimaryVertexMonitor_cff import *
 
-#DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_source)
+#DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_source*egammaDQMOffline)
 
-DQMOffline = cms.Sequence(ecal_dqm_source_offline*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
+DQMOffline = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonMonitors*jetMETAnalyzer*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_source*egammaCosmicPhotonMonitors)
 
