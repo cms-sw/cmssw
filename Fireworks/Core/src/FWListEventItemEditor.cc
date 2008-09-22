44c82d2abf296e08b78d56d0e0b80217af4cebad
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-09-22 20:13:33 +0000

    deactivated unnecessary code

--- Fireworks/Core/src/FWListEventItemEditor.cc
+++ Fireworks/Core/src/FWListEventItemEditor.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Mar  3 09:36:01 EST 2008
-// $Id: FWListEventItemEditor.cc,v 1.3 2008/06/25 22:14:09 chrjones Exp $
+// $Id: FWListEventItemEditor.cc,v 1.4 2008/06/29 13:23:48 chrjones Exp $
 //
 
 // system include files
@@ -42,6 +42,8 @@ FWListEventItemEditor::FWListEventItemEditor(const TGWindow* p,
                                              Pixel_t back):
 TGedFrame(p, width, height, options | kVerticalFrame, back)
 {
+   //std::cout <<"FWListEventItemEditor"<<std::endl;
+   /*
    MakeTitle("FWListEventItem");
    TGGroupFrame* vf = new TGGroupFrame(this,"Object Filter",kVerticalFrame);
 
@@ -57,6 +59,7 @@ TGedFrame(p, width, height, options | kVerticalFrame, back)
    TGTextButton* removeItemButton = new TGTextButton(this,"Remove Item");
    removeItemButton->Connect("Clicked()", "FWListEventItemEditor",this,"removeItem()");
    AddFrame(removeItemButton, new TGLayoutHints(kLHintsTop,0,0,0,0));
+    */
 }
 
 // FWListEventItemEditor::FWListEventItemEditor(const FWListEventItemEditor& rhs)
@@ -86,23 +89,30 @@ FWListEventItemEditor::~FWListEventItemEditor()
 void
 FWListEventItemEditor::SetModel(TObject* iObj)
 {
+   //std::cout <<"SetModel "<<iObj<<std::endl;
    m_item = dynamic_cast<FWListEventItem*>(iObj);
-   assert(0!=m_item);
-   m_filterExpression->SetText(m_item->eventItem()->filterExpression().c_str());
-   FWGUIManager::getGUIManager()->updateEDI(m_item->eventItem());
+   if(0!=iObj) {
+      assert(0!=m_item);
+      
+      //m_filterExpression->SetText(m_item->eventItem()->filterExpression().c_str());
+      FWGUIManager::getGUIManager()->updateEDI(m_item->eventItem());
+   }
 }
 
 void
 FWListEventItemEditor::runFilter()
 {
+   /*
    if(m_item!=0) {
      m_item->eventItem()->setFilterExpression(m_filterExpression->GetText());
    }
+    */
 }
 
 void
 FWListEventItemEditor::removeItem()
 {
+   /*
   if (m_item != 0) {
     m_item->eventItem()->destroy();
     //    delete m_item;
@@ -110,6 +120,7 @@ FWListEventItemEditor::removeItem()
     gEve->EditElement(0);
     gEve->Redraw3D();
   }
+    */
 }
 //
 // const member functions
