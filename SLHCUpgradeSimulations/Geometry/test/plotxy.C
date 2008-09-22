298be396245b62acb1855cd8e5816de3fe8bc40e
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2008-09-22 18:24:49 +0000

    Including plotting of the TEC rechits

--- SLHCUpgradeSimulations/Geometry/test/plotxy.C
+++ SLHCUpgradeSimulations/Geometry/test/plotxy.C
@@ -6,6 +6,7 @@
   MyCanvas->cd(2);
   PixelNtuple->Draw("pixel_recHit.gy:pixel_recHit.gx", "pixel_recHit.gx < 30 && pixel_recHit.gx>20 && pixel_recHit.gy<30 && pixel_recHit.gy>20");
   MyCanvas->cd(3);
-  PixelNtuple->Draw("sqrt((pixel_recHit.gy*pixel_recHit.gy)+(pixel_recHit.gx*pixel_recHit.gx)):pixel_recHit.gz");
+  StripNtuple->Draw("sqrt((strip_recHit.gy*strip_recHit.gy)+(strip_recHit.gx*strip_recHit.gx)):strip_recHit.gz");
+  PixelNtuple->Draw("sqrt((pixel_recHit.gy*pixel_recHit.gy)+(pixel_recHit.gx*pixel_recHit.gx)):pixel_recHit.gz","","same");
 }
 
