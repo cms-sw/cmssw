f69aff9b4947226db26c3090ef3b20e3ac0f499f
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-09-22 16:35:23 +0000

    clean up

--- SimTracker/TrackerMaterialAnalysis/test/trackingMaterialAnalyser.py
+++ SimTracker/TrackerMaterialAnalysis/test/trackingMaterialAnalyser.py
@@ -1,18 +1,31 @@
-#! /bin/env cmsRun
+#! /usr/bin/env cmsRun
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("MaterialAnalyser")
 
-# Analyze and plot the tracking material
-process.load("SimTracker.TrackerMaterialAnalysis.trackingMaterialAnalyser_cff")
-
 process.MessageLogger = cms.Service("MessageLogger",
     destinations = cms.untracked.vstring('cout')
 )
 
+# Configuration and Conditions
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'IDEAL_V9::All'
+
+# Analyze and plot the tracking material
+process.load("SimTracker.TrackerMaterialAnalysis.trackingMaterialAnalyser_cff")
+process.trackingMaterialAnalyser.SplitMode         = "NearestLayer"
+process.trackingMaterialAnalyser.SaveParameters    = True
+process.trackingMaterialAnalyser.SaveXML           = True
+process.trackingMaterialAnalyser.SaveDetailedPlots = True
+  
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:material.root')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
 )
 
 process.path = cms.Path(process.trackingMaterialAnalyser)
