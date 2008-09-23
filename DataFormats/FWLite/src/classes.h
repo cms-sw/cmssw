d26072971293ea085cd1bceae5df3885f4e02feb
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-09-23 15:51:30 +0000

    added ErrorThrower

--- DataFormats/FWLite/src/classes.h
+++ DataFormats/FWLite/src/classes.h
@@ -3,3 +3,4 @@
 //Add includes for your classes here
 #include "DataFormats/FWLite/interface/Event.h"
 #include "DataFormats/FWLite/interface/ChainEvent.h"
+#include "DataFormats/FWLite/interface/ErrorThrower.h"
