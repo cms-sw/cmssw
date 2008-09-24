6259d8dd5d80c56f8ff94e68bf2de617f06273e3
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-09-24 15:44:38 +0000

    *** empty log message ***

--- HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_2_1_9/HLT/V5 (CMSSW_2_1_9)
+# /dev/CMSSW_2_1_9/HLT/V7 (CMSSW_2_1_9)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V5')
+  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V7')
 )
 
 block_hltDefaultOutputWithFEDs = cms.PSet(
