a24bad941bd3039c57799849f49cfce356a1deb7
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2008-09-24 11:14:40 +0000

    Update label for new sequence definitions

--- L1Trigger/GlobalCaloTrigger/python/l1GctValidation_cfi.py
+++ L1Trigger/GlobalCaloTrigger/python/l1GctValidation_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 l1GctValidation = cms.EDFilter("L1GctValidation",
-    inputTag = cms.untracked.InputTag("gctDigis")
+    inputTag  = cms.untracked.InputTag("simGctDigis"),
+    missHtTag = cms.untracked.InputTag("simGctDigis:missingHt")
 )
 
 
