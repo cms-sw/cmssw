3c62137c313b16c1cacea5ce76124398e416c467
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-09-24 21:06:53 +0000

    A few validators for paths and endpaths

---
+++ FWCore/ParameterSet/python/SequenceVisitors.py
@@ -0,0 +1,52 @@
+#from Modules import OutputModule, EDProducer, EDFilter
+from SequenceTypes import *
+from Modules import OutputModule, EDProducer, EDFilter, EDAnalyzer
+
+class PathValidator(object):
+    def enter(self,visitee):
+        if isinstance(visitee,OutputModule):
+            raise ValueError("Path cannot have a "+visitee.label_())
+    def leave(self,visitee):
+        pass
+
+class EndPathValidator(object):
+    def enter(self,visitee):
+        if isinstance(visitee,EDProducer) or isinstance(visitee,EDFilter):
+            raise ValueError("EndPath cannot have a "+visitee.label_())
+    def leave(self,visitee):
+        pass
+
+
+if __name__=="__main__":
+    import unittest
+    class TestModuleCommand(unittest.TestCase):
+        def setUp(self):
+            """Nothing to do """
+            pass
+        def testValidators(self):
+            producer = EDProducer("Producer")
+            analyzer = EDAnalyzer("Analyzer")
+            output = OutputModule("Out")
+            filter = EDFilter("Filter")
+            producer.setLabel("producer")
+            analyzer.setLabel("analyzer")
+            output.setLabel("output")
+            filter.setLabel("filter")
+            s1 = Sequence(analyzer*producer)
+            s2 = Sequence(output+filter)
+            p1 = Path(s1)
+            p2 = Path(s1*s2)
+            ep1 = EndPath(output+analyzer)
+            ep2 = EndPath(s1)
+            ep3 = EndPath(s2)
+            pathValidator = PathValidator()
+            endpathValidator = EndPathValidator()
+            p1.visit(pathValidator)
+            self.assertRaises(ValueError, p2.visit, pathValidator) 
+            ep1.visit(endpathValidator) 
+            self.assertRaises(ValueError, ep2.visit, endpathValidator)
+            self.assertRaises(ValueError, ep3.visit, endpathValidator)
+
+    unittest.main()
+
+
