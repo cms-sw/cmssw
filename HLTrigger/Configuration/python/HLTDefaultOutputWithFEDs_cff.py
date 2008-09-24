225ed770402321f4d5b9394735a0f334ef2133bb
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-09-24 15:06:02 +0000

    Update to V3 of ConfDB: EventContent update to online.

--- HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
@@ -1,20 +1,19 @@
-# /dev/CMSSW_2_1_9/HLT/V2 (CMSSW_2_1_9)
+# /dev/CMSSW_2_1_9/HLT/V3 (CMSSW_2_1_9)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V2')
+  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V3')
 )
 
 block_hltDefaultOutputWithFEDs = cms.PSet(
 outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*',
-  'keep FEDRawDataCollection_source_*_*',
   'keep FEDRawDataCollection_rawDataCollector_*_*',
   'keep edmTriggerResults_*_*_*',
   'keep triggerTriggerEvent_*_*_*',
-  'keep *_hltGtDigis_*_*',
   'keep *_hltGctDigis_*_*',
+  'keep *_hltL1extraParticles_*_*',
   'keep *_hltL1GtObjectMap_*_*',
-  'keep *_hltL1extraParticles_*_*' )
+  'keep *_hltGtDigis_*_*' )
 )
--- HLTrigger/Configuration/python/HLTDefaultOutput_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutput_cff.py
@@ -1,18 +1,18 @@
-# /dev/CMSSW_2_1_9/HLT/V2 (CMSSW_2_1_9)
+# /dev/CMSSW_2_1_9/HLT/V3 (CMSSW_2_1_9)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V2')
+  tableName = cms.string('/dev/CMSSW_2_1_9/HLT/V3')
 )
 
 block_hltDefaultOutput = cms.PSet(
 outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*',
   'keep edmTriggerResults_*_*_*',
   'keep triggerTriggerEvent_*_*_*',
-  'keep *_hltGtDigis_*_*',
   'keep *_hltGctDigis_*_*',
+  'keep *_hltL1extraParticles_*_*',
   'keep *_hltL1GtObjectMap_*_*',
-  'keep *_hltL1extraParticles_*_*' )
+  'keep *_hltGtDigis_*_*' )
 )
