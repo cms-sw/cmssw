62e96cf0ccfa8ecdc053932dafe3d2509ab583ab
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-09-24 14:49:18 +0000

    added the module DTnoiseDBValidation

--- DQMOffline/CalibMuon/src/SealModule.cc
+++ DQMOffline/CalibMuon/src/SealModule.cc
@@ -3,7 +3,9 @@
 
 #include "DQMOffline/CalibMuon/interface/DTt0DBValidation.h"
 #include "DQMOffline/CalibMuon/interface/DTtTrigDBValidation.h"
+#include "DQMOffline/CalibMuon/interface/DTnoiseDBValidation.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTt0DBValidation);
 DEFINE_ANOTHER_FWK_MODULE(DTtTrigDBValidation);
+DEFINE_ANOTHER_FWK_MODULE(DTnoiseDBValidation);
