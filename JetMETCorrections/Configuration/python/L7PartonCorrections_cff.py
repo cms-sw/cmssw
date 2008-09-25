152fca27a31210aaf77dee4c3b29701dff2b48bb
Attilio Santocchia <sha1-33bf5cd70d29e6022725c98029d8110dad65c921@cern.ch>
2008-09-25 16:07:58 +0000

    new fitting parameters

--- JetMETCorrections/Configuration/python/L7PartonCorrections_cff.py
+++ JetMETCorrections/Configuration/python/L7PartonCorrections_cff.py
@@ -18,55 +18,55 @@ import FWCore.ParameterSet.Config as cms
 #======================================//
 L7PartonJetCorrectorIC5gJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('gJ'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5gJet')
 )
 
 L7PartonJetCorrectorIC5qJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qJ'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5qJet')
 )
 
 L7PartonJetCorrectorIC5cJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cJ'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5cJet')
 )
 
 L7PartonJetCorrectorIC5bJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bJ'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5bJet')
 )
 
 L7PartonJetCorrectorIC5jJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('jJ'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5jJet')
 )
 
 L7PartonJetCorrectorIC5qTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qT'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5qTop')
 )
 
 L7PartonJetCorrectorIC5cTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cT'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5cTop')
 )
 
 L7PartonJetCorrectorIC5bTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bT'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5bTop')
 )
 
 L7PartonJetCorrectorIC5tTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('tT'),
-    tagName = cms.string('L7parton_IC5_080301'),
+    tagName = cms.string('L7parton_IC5_080921'),
     label = cms.string('L7PartonJetCorrectorIC5tTop')
 )
 
@@ -75,55 +75,55 @@ L7PartonJetCorrectorIC5tTop = cms.ESSource("L7PartonCorrectionService",
 #======================================//
 L7PartonJetCorrectorKT4gJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('gJ'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4gJet')
 )
 
 L7PartonJetCorrectorKT4qJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qJ'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4qJet')
 )
 
 L7PartonJetCorrectorKT4cJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cJ'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4cJet')
 )
 
 L7PartonJetCorrectorKT4bJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bJ'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4bJet')
 )
 
 L7PartonJetCorrectorKT4jJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('jJ'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4jJet')
 )
 
 L7PartonJetCorrectorKT4qTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qT'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4qTop')
 )
 
 L7PartonJetCorrectorKT4cTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cT'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4cTop')
 )
 
 L7PartonJetCorrectorKT4bTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bT'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4bTop')
 )
 
 L7PartonJetCorrectorKT4tTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('tT'),
-    tagName = cms.string('L7parton_KT4_080301'),
+    tagName = cms.string('L7parton_KT4_080921'),
     label = cms.string('L7PartonJetCorrectorKT4tTop')
 )
 
@@ -132,55 +132,55 @@ L7PartonJetCorrectorKT4tTop = cms.ESSource("L7PartonCorrectionService",
 #======================================//
 L7PartonJetCorrectorKT6gJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('gJ'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6gJet')
 )
 
 L7PartonJetCorrectorKT6qJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qJ'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6qJet')
 )
 
 L7PartonJetCorrectorKT6cJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cJ'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6cJet')
 )
 
 L7PartonJetCorrectorKT6bJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bJ'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6bJet')
 )
 
 L7PartonJetCorrectorKT6jJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('jJ'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6jJet')
 )
 
 L7PartonJetCorrectorKT6qTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qT'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6qTop')
 )
 
 L7PartonJetCorrectorKT6cTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cT'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6cTop')
 )
 
 L7PartonJetCorrectorKT6bTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bT'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6bTop')
 )
 
 L7PartonJetCorrectorKT6tTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('tT'),
-    tagName = cms.string('L7parton_KT6_080301'),
+    tagName = cms.string('L7parton_KT6_080921'),
     label = cms.string('L7PartonJetCorrectorKT6tTop')
 )
 
@@ -189,55 +189,55 @@ L7PartonJetCorrectorKT6tTop = cms.ESSource("L7PartonCorrectionService",
 #======================================//
 L7PartonJetCorrectorSC5gJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('gJ'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5gJet')
 )
 
 L7PartonJetCorrectorSC5qJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qJ'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5qJet')
 )
 
 L7PartonJetCorrectorSC5cJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cJ'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5cJet')
 )
 
 L7PartonJetCorrectorSC5bJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bJ'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5bJet')
 )
 
 L7PartonJetCorrectorSC5jJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('jJ'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5jJet')
 )
 
 L7PartonJetCorrectorSC5qTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qT'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5qTop')
 )
 
 L7PartonJetCorrectorSC5cTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cT'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5cTop')
 )
 
 L7PartonJetCorrectorSC5bTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bT'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5bTop')
 )
 
 L7PartonJetCorrectorSC5tTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('tT'),
-    tagName = cms.string('L7parton_SC5_080301'),
+    tagName = cms.string('L7parton_SC5_080921'),
     label = cms.string('L7PartonJetCorrectorSC5tTop')
 )
 
@@ -246,55 +246,55 @@ L7PartonJetCorrectorSC5tTop = cms.ESSource("L7PartonCorrectionService",
 #======================================//
 L7PartonJetCorrectorSC7gJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('gJ'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7gJet')
 )
 
 L7PartonJetCorrectorSC7qJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qJ'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7qJet')
 )
 
 L7PartonJetCorrectorSC7cJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cJ'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7cJet')
 )
 
 L7PartonJetCorrectorSC7bJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bJ'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7bJet')
 )
 
 L7PartonJetCorrectorSC7jJet = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('jJ'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7jJet')
 )
 
 L7PartonJetCorrectorSC7qTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('qT'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7qTop')
 )
 
 L7PartonJetCorrectorSC7cTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('cT'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7cTop')
 )
 
 L7PartonJetCorrectorSC7bTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('bT'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7bTop')
 )
 
 L7PartonJetCorrectorSC7tTop = cms.ESSource("L7PartonCorrectionService",
     section = cms.string('tT'),
-    tagName = cms.string('L7parton_SC7_080301'),
+    tagName = cms.string('L7parton_SC7_080921'),
     label = cms.string('L7PartonJetCorrectorSC7tTop')
 )
 
