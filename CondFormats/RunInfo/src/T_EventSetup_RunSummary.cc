4c5131e4b7f6402f8f07c3c2ef7efd2b058e231d
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-09-29 16:42:16 +0000

    runsummary class added

---
+++ CondFormats/RunInfo/src/T_EventSetup_RunSummary.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/RunInfo/interface/RunSummary.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(RunSummary);
--- CondFormats/RunInfo/src/classes.h
+++ CondFormats/RunInfo/src/classes.h
@@ -1,6 +1,10 @@
 #include "CondFormats/RunInfo/interface/RunNumber.h"
+#include "CondFormats/RunInfo/interface/RunSummary.h"
 #include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
+
 namespace {
   std::vector<runinfo_test::RunNumber::Item>::iterator tmp0;
   std::vector<L1TriggerScaler::Lumi>::iterator tmp1;
+  std::vector<RunSummary::Summary>::iterator tmp2;
+  RunSummary summary;
 }
