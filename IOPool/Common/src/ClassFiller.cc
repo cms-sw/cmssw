dc607544961f45ed91e818af33c57edfbdfc05c9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-09-29 23:06:12 +0000

    Properly initialize transient data members of provenance classes with custom streamers

--- IOPool/Common/src/ClassFiller.cc
+++ IOPool/Common/src/ClassFiller.cc
@@ -1,13 +1,28 @@
 #include "IOPool/Common/interface/ClassFiller.h"
+#include "DataFormats/Provenance/interface/BranchDescription.h"
+#include "DataFormats/Provenance/interface/EventEntryDescription.h"
+#include "DataFormats/Provenance/interface/EventEntryInfo.h"
+#include "DataFormats/Provenance/interface/FileIndex.h"
+#include "DataFormats/Provenance/interface/ProcessHistory.h"
+#include "DataFormats/Provenance/interface/ProductRegistry.h"
+#include "FWCore/RootAutoLibraryLoader/interface/TransientStreamer.h"
 #include "Cintex/Cintex.h"
 #include "TH1.h"
 #include "G__ci.h"
+#include "TTree.h"
 
 namespace edm {
   // ---------------------
   void ClassFiller() {
+    TTree::SetMaxTreeSize(kMaxLong64);
     TH1::AddDirectory(kFALSE);
     G__SetCatchException(0);
     ROOT::Cintex::Cintex::Enable();
+    SetTransientStreamer<Transient<BranchDescription::Transients> >();
+    SetTransientStreamer<Transient<EventEntryDescription::Transients> >();
+    SetTransientStreamer<Transient<EventEntryInfo::Transients> >();
+    SetTransientStreamer<Transient<FileIndex::Transients> >();
+    SetTransientStreamer<Transient<ProcessHistory::Transients> >();
+    SetTransientStreamer<Transient<ProductRegistry::Transients> >();
   }
 }
