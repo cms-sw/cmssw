94c9166500d1e6f3a39d335658ea8f251fcd776e
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-09-29 17:05:16 +0000

    run summary added

--- CondCore/RunInfoPlugins/src/plugin.cc
+++ CondCore/RunInfoPlugins/src/plugin.cc
@@ -1,6 +1,8 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/DataRecord/interface/RunNumberRcd.h"
 #include "CondFormats/RunInfo/interface/RunNumber.h"
+#include "CondFormats/DataRecord/interface/RunSummaryRcd.h"
+#include "CondFormats/RunInfo/interface/RunSummary.h"
 #include "CondFormats/DataRecord/interface/L1TriggerScalerRcd.h"
 #include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
 
@@ -8,4 +10,5 @@ using namespace runinfo_test;
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(RunNumberRcd,RunNumber);
+REGISTER_PLUGIN(RunSummaryRcd,RunSummary);
 REGISTER_PLUGIN(L1TriggerScalerRcd, L1TriggerScaler);
