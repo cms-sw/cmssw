9b80679f864f10415c4c9354cff9d98056618c40
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-09-30 18:38:38 +0000

    *** empty log message ***

--- Validation/RecoEgamma/python/tpSelection_cfi.py
+++ Validation/RecoEgamma/python/tpSelection_cfi.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 import PhysicsTools.RecoAlgos.trackingParticleSelector_cfi
-myTrackingParticle = PhysicsTools.RecoAlgos.trackingParticleSelector_cfi.trackingParticleSelector.clone()
-myTrackingParticle.chargedOnly = True
+tpSelection = PhysicsTools.RecoAlgos.trackingParticleSelector_cfi.trackingParticleSelector.clone()
+tpSelection.chargedOnly = True
 # trackingParticleSelector.pdgId = cms.vint32()
-myTrackingParticle.tip = 120
-myTrackingParticle.lip = 280
-myTrackingParticle.signalOnly = False
-myTrackingParticle.minRapidity = -2.5
-myTrackingParticle.ptMin =0.9
-myTrackingParticle.maxRapidity = 2.5
-myTrackingParticle.minHit = 0
+tpSelection.tip = 120
+tpSelection.lip = 280
+tpSelection.signalOnly = False
+tpSelection.minRapidity = -2.5
+tpSelection.ptMin =0.9
+tpSelection.maxRapidity = 2.5
+tpSelection.minHit = 0
 
 
