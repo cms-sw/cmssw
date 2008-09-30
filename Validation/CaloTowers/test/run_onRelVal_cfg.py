01d2d3e1eb0cd6b42905950760ecb62f0ee4e93f
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2008-09-30 12:46:55 +0000

    adding RelVal related stuff

---
+++ Validation/CaloTowers/test/run_onRelVal_cfg.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RelValValidation")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+
+process.load("DQMServices.Core.DQM_cfg")
+process.DQM.collectorHost = ''
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.source = cms.Source("PoolSource",
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(10),
+    fileNames = cms.untracked.vstring(
+      )
+)
+
+process.hcalRecoAnalyzer = cms.EDFilter("HcalRecHitsValidation",
+    eventype = cms.untracked.string('multi'),
+    outputFile = cms.untracked.string('HcalRecHitValidationRelVal.root'),
+    ecalselector = cms.untracked.string('yes'),
+    mc = cms.untracked.string('no'),
+    hcalselector = cms.untracked.string('all')
+)
+
+
+process.hcalTowerAnalyzer = cms.EDFilter("CaloTowersValidation",
+    outputFile = cms.untracked.string('CaloTowersValidationRelVal.root'),
+    CaloTowerCollectionLabel = cms.untracked.string('towerMaker'),
+    hcalselector = cms.untracked.string('all')
+)
+
+process.p = cms.Path(process.hcalRecoAnalyzer * process.hcalTowerAnalyzer)
+
