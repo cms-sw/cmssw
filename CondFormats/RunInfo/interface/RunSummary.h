677762979ff8a8f38c89d2ce308373b289fe0312
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2008-10-01 15:54:32 +0000

    interface improved

--- CondFormats/RunInfo/interface/RunSummary.h
+++ CondFormats/RunInfo/interface/RunSummary.h
@@ -15,33 +15,27 @@
 
 class RunSummary {
 public:
-  struct Summary{
-    Summary(){}
-    ~Summary(){}
-    int m_run;
-    std::string m_name;
-    long long m_start_time_ll;
-    std::string m_start_time_str;
-    long long m_stop_time_ll;
-    std::string m_stop_time_str;
-    int  m_lumisections;
-    std::vector<std::string> m_subdt_joining;
-    std::vector<int> m_subdt_in;
-    std::string m_hltkey;
-    long long m_nevents;
-    float m_rate;
-  };
-
- enum subdet { PIXEL, TRACKER, ECAL, HCAL, DT, CSC,RPC };  
- 
-  RunSummary();
-  virtual ~RunSummary(){}
-  Summary fake_Run();
-  typedef std::vector<Summary>::const_iterator SummaryIterator;
-  std::vector<Summary>  summary;
-
-  void printAllValues() const;
   
+  int m_run;
+  std::string m_name;
+  long long m_start_time_ll;
+  std::string m_start_time_str;
+  long long m_stop_time_ll;
+  std::string m_stop_time_str;
+  int  m_lumisections;
+  std::vector<int> m_subdt_in;
+  std::string m_hltkey;
+  long long m_nevents;
+  float m_rate;
+    
+  enum subdet { PIXEL, TRACKER, ECAL, HCAL, DT, CSC,RPC };  
+  
+  RunSummary();
+  virtual ~RunSummary(){};
+  static RunSummary* Fake_RunSummary();
+    
+  void printAllValues() const;
+  std::vector<std::string> getSubdtIn() const;
   
 };
 
