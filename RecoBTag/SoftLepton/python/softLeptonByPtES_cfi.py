4089319cd10bd941cce8a1f75d1893780d45b4eb
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-01 12:23:52 +0000

    *** empty log message ***

--- RecoBTag/SoftLepton/python/softLeptonByPtES_cfi.py
+++ RecoBTag/SoftLepton/python/softLeptonByPtES_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-softLeptonByPt = cms.ESProducer("LeptonTaggerByPtESProducer")
-
-
+softLeptonByPt = cms.ESProducer("LeptonTaggerByPtESProducer",
+    ipSign = cms.string("any")
+)
