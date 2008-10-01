50dcb7db9f5101a7979f0d9feaf6068b1cf71d87
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-10-01 16:19:05 +0000

    fix dict autoloading

--- CondCore/Utilities/bin/cmscond_export_iov.cpp
+++ CondCore/Utilities/bin/cmscond_export_iov.cpp
@@ -1,3 +1,5 @@
+#include "FWCore/PluginManager/interface/PluginManager.h"
+#include "FWCore/PluginManager/interface/standard.h"
 #include "CondCore/DBCommon/interface/ConnectionHandler.h"
 #include "CondCore/DBCommon/interface/CoralTransaction.h"
 #include "CondCore/DBCommon/interface/PoolTransaction.h"
@@ -40,6 +42,9 @@
 
 
 int main( int argc, char** argv ){
+  edmplugin::PluginManager::Config config;
+  edmplugin::PluginManager::configure(edmplugin::standard::config());
+
   cond::CommonOptions myopt("cmscond_export_iov");
   myopt.addDictionary();
   myopt.addAuthentication(false);
