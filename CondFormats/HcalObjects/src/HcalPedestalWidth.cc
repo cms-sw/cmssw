2b81bed0abb67a8cb2779b85cf3667b87b9c6d7a
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2008-10-02 09:45:36 +0000

    improved noise simulation

--- CondFormats/HcalObjects/src/HcalPedestalWidth.cc
+++ CondFormats/HcalObjects/src/HcalPedestalWidth.cc
@@ -1,10 +1,10 @@
-/** 
+/**
 \class HcalPedestalWidth
 \author Fedor Ratnikov (UMd)
 correlation matrix for pedestals
 $Author: ratnikov
-$Date: 2006/11/13 22:40:37 $
-$Revision: 1.5 $
+$Date: 2008/03/28 17:28:52 $
+$Revision: 1.6 $
 */
 
 #include <math.h>
@@ -42,10 +42,17 @@ void HcalPedestalWidth::setSigma (int fCapId1, int fCapId2, float fSigma) {
 void HcalPedestalWidth::makeNoise (unsigned fFrames, const double* fGauss, double* fNoise) const {
 
   double s_xx_mean = (getSigma (0,0) + getSigma (1,1) + getSigma (2,2) + getSigma (3,3)) / 4;
-  double s_xy_mean = (getSigma (1,0) + getSigma (2,1) + getSigma (3,2) + getSigma (3,0)) / 4;
+
+  // S. Abdullin, 22.09.2008
+  // replacing this sum (not quite correct, anyway, as it should be  
+  // (0,1), (1,2), (2,3), (3,0) from the full *asymmetric* matrix.
+  //  double s_xy_mean =  (getSigma (1,0) + getSigma (2,1)
+  //                     + getSigma (3,2) + getSigma (3,0)) / 4;
+  // with just a (fixed as optimal) fraction of diagonal elements:
+  double s_xy_mean = -0.5 * s_xx_mean;
 
   // added a simple protection against negative sqrt() arguments in case of 
-  // big off-diaigonal terms (S.Abdullin, 27.03.2008)
+  // improperly big off-diaigonal terms (S.Abdullin, 27.03.2008)
   double term  = s_xx_mean*s_xx_mean - 2.*s_xy_mean*s_xy_mean;
   if (term < 0.) term = 1.e-50 ;
   double sigma = sqrt (0.5 * (s_xx_mean + sqrt(term)));
