219b074cc9e4b970c7fcccd5f78e3815431f310f
Giovanni Abbiendi <sha1-03ca615cb6d62d8479399a2c21824cf89dd4fac5@cern.ch>
2008-10-02 10:58:33 +0000

    python example producer+analyzer for MuonAssociatorByHits

---
+++ SimMuon/MCTruth/test/testMuonAssociatorEDProducer_cfg.py
@@ -0,0 +1,110 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("myproc")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:RelVal.root')
+)
+
+# MessageLogger
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.MessageLogger.categories = cms.untracked.vstring('testReader', 'MuonAssociatorEDProducer',
+    'MuonAssociatorByHits', 'DTHitAssociator', 'RPCHitAssociator', 'MuonTruth', 'PSimHitMap',
+    'FwkJob', 'FwkReport', 'FwkSummary', 'Root_NoDictionary')
+
+process.MessageLogger.cout = cms.untracked.PSet(
+    noTimeStamps = cms.untracked.bool(True),
+    threshold = cms.untracked.string('INFO'),
+    INFO = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    default = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    testReader = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    MuonAssociatorEDProducer = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    MuonAssociatorByHits = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    DTHitAssociator = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    RPCHitAssociator = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    MuonTruth = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    PSimHitMap = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    FwkReport = cms.untracked.PSet(
+        reportEvery = cms.untracked.int32(1),
+        limit = cms.untracked.int32(10000000)
+    ),
+    FwkSummary = cms.untracked.PSet(
+        reportEvery = cms.untracked.int32(1),
+        limit = cms.untracked.int32(10000000)
+    ),
+    FwkJob = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    Root_NoDictionary = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    )
+)
+
+process.MessageLogger.statistics = cms.untracked.vstring('cout')
+
+process.MessageLogger.cerr = cms.untracked.PSet(
+    placeholder = cms.untracked.bool(True)
+)
+
+#process.Tracer = cms.Service("Tracer")
+
+# Mixing Module
+process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
+# MuonAssociatorByHits
+process.load("SimMuon.MCTruth.MuonAssociatorByHits_cfi")
+# default is standalone muons:
+#    process.muonAssociatorByHits.tracksTag = cms.InputTag("standAloneMuons")
+#    process.muonAssociatorByHits.SimToReco_useTracker = cms.bool(False)
+#    process.muonAssociatorByHits.SimToReco_useMuon = cms.bool(True)
+
+# test analysis
+process.testanalyzer = cms.EDAnalyzer("testReader",
+    tracksTag = cms.InputTag("standAloneMuons"),
+    tpTag = cms.InputTag("mergedtruth","MergedTrackTruth"),
+    assoMapsTag = cms.InputTag("muonAssociatorByHits")
+)
+
+# example output
+process.MyOut = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep recoTracks_standAloneMuons_*_*', 
+        'keep recoTracks_globalMuons_*_*', 
+        'keep TrackingParticles_mergedtruth_MergedTrackTruth_*',
+        'keep *_muonAssociatorByHits_*_*'),
+    fileName = cms.untracked.string('test.root')
+)
+
+# paths and schedule
+process.mixing = cms.Path(process.mix)
+process.muonAssociator = cms.Path(process.muonAssociatorByHits)
+process.test = cms.Path(process.testanalyzer)
+process.output = cms.EndPath(process.MyOut)
+
+process.schedule = cms.Schedule(process.mixing, process.muonAssociator, process.test, process.output)
