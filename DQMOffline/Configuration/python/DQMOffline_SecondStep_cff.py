f3f94fbf1f0be8167a46a85de8925194b067cd8c
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-10-02 21:16:34 +0000

    - add RecoB

--- DQMOffline/Configuration/python/DQMOffline_SecondStep_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_SecondStep_cff.py
@@ -5,9 +5,7 @@ from DQMOffline.Ecal.ecal_dqm_client_offline_cff import *
 from DQM.HcalMonitorModule.hcal_dqm_client_fileT0_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_client_cff import *
 from DQMOffline.Trigger.DQMOffline_Trigger_Client_cff import *
-#from DQMOffline.RecoB.dqmCollector_cff import *
+from DQMOffline.RecoB.dqmCollector_cff import *
 
-#DQMOffline_SecondStep = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient*bTagCollectorSequence)
-
-DQMOffline_SecondStep = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient)
+DQMOffline_SecondStep = cms.Sequence(ecal_dqm_client_offline*muonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient*bTagCollectorSequence)
 
