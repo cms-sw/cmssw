410b7e5d6bc97585fbe80cbad9a22cff91e32009
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2008-10-03 00:51:58 +0000

    Allow either spelling: HLT_L1MuOpen or HLT_L1_MuonOpen (new minimal HLT table for CRAFT)

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 ALCARECOMuAlCalIsolatedMuHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
 ALCARECOMuAlCalIsolatedMuHLT.andOr = True ## choose logical OR between Triggerbits
-ALCARECOMuAlCalIsolatedMuHLT.HLTPaths = ["HLT_L1MuOpen", "HLT_L1Mu", "HLT_Mu3", "HLT_Mu5", "HLT_Mu7", "HLT_Mu9", "HLT_Mu11", "HLT_Mu13", "HLT_Mu15", "HLT_Mu15_L1Mu7", "HLT_L2Mu9", "HLT_IsoMu9", "HLT_IsoMu11", "HLT_IsoMu13", "HLT_IsoMu15"] # ['HLT_IsoMu11','HLT_Mu15_L1Mu7']
+ALCARECOMuAlCalIsolatedMuHLT.HLTPaths = ["HLT_L1_MuonOpen", "HLT_L1MuOpen", "HLT_L1Mu", "HLT_Mu3", "HLT_Mu5", "HLT_Mu7", "HLT_Mu9", "HLT_Mu11", "HLT_Mu13", "HLT_Mu15", "HLT_Mu15_L1Mu7", "HLT_L2Mu9", "HLT_IsoMu9", "HLT_IsoMu11", "HLT_IsoMu13", "HLT_IsoMu15"] # ['HLT_IsoMu11','HLT_Mu15_L1Mu7']
 
 import Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi
 ALCARECOMuAlCalIsolatedMu = Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi.AlignmentMuonSelector.clone()
