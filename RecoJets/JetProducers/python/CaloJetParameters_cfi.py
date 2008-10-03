fa5f006b431409d5d193b0b85588be68517b5b7c
Andreas Oehler <sha1-bf17a625e96eed2dfa3f3b53d79864d479019360@cern.ch>
2008-10-03 19:14:03 +0000

    Switch off new features for 2_1 (no vertex correction, no improved ecal pos. towers)

--- RecoJets/JetProducers/python/CaloJetParameters_cfi.py
+++ RecoJets/JetProducers/python/CaloJetParameters_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Standard CalooJets parameters
-# $Id: CaloJetParameters_cfi.py,v 1.5 2008/09/20 17:49:54 oehler Exp $
+# $Id: CaloJetParameters_cfi.py,v 1.6 2008/09/25 16:31:19 oehler Exp $
 CaloJetParameters = cms.PSet(
     src = cms.InputTag("towerMaker"),
     verbose = cms.untracked.bool(False),
@@ -9,7 +9,7 @@ CaloJetParameters = cms.PSet(
     inputEtMin = cms.double(0.5),
     jetType = cms.untracked.string('CaloJet'),
     inputEMin = cms.double(0.0),
-    correctInputToSignalVertex = cms.bool(True),
+    correctInputToSignalVertex = cms.bool(False),
     pvCollection = cms.InputTag('offlinePrimaryVertices')
 )
 
