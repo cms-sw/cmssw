8df9a77a06e0b67332755e99e2735af1397de7d2
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-10-07 15:42:04 +0000

    switched off the detailed card

--- DQM/DTMonitorClient/python/dtSegmentAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtSegmentAnalysisTest_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 segmentTest = cms.EDAnalyzer("DTSegmentAnalysisTest",
                              folderRoot = cms.untracked.string(''),
-                             detailedAnalysis = cms.untracked.bool(True),
+                             detailedAnalysis = cms.untracked.bool(False),
                              #Names of the quality tests: they must match those specified in "qtList"
                              chi2TestName = cms.untracked.string('chi2InRange'),
                              segmRecHitTestName = cms.untracked.string('segmRecHitInRange'),
