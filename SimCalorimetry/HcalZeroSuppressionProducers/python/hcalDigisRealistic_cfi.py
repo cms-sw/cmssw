5bac2fc2809b3f2b0c0eaa683975dda9764600e3
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2008-10-07 17:15:23 +0000

    For new Realistic ZS

---
+++ SimCalorimetry/HcalZeroSuppressionProducers/python/hcalDigisRealistic_cfi.py
@@ -0,0 +1,21 @@
+# The following comments couldn't be translated into the new config version:
+
+# conservative choice
+# 0=per channel, 1=triggerOR, 2=depthOR
+# conservative choice
+# 0=per channel, 1=triggerOR, 2=depthOR
+# conservative choice
+# 0=per channel, 1=triggerOR, 2=depthOR
+import FWCore.ParameterSet.Config as cms
+
+simHcalDigis = cms.EDFilter("HcalRealisticZS",
+    digiLabel = cms.InputTag("simHcalUnsuppressedDigis"),
+    mode = cms.int32(0),
+    HBlevel = cms.int32(8),
+    HElevel = cms.int32(8),
+    HOlevel = cms.int32(8),
+    HFlevel = cms.int32(9)
+)
+
+
+
