5fa5d9f134ab9e326ba8565e659f67d368e9b1cc
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-10-08 07:21:43 +0000

    cleanup obsolete parameters

--- DQM/DTMonitorModule/python/dtDataIntegrityTask_cfi.py
+++ DQM/DTMonitorModule/python/dtDataIntegrityTask_cfi.py
@@ -1,13 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 DTDataIntegrityTask = cms.Service("DTDataIntegrityTask",
-    TBhistoGranularity = cms.untracked.int32(1),
-    getSCInfo = cms.untracked.bool(True),
-    debug = cms.untracked.bool(False),
-    doTimeHisto = cms.untracked.bool(True),
-    timeBoxLowerBound = cms.untracked.int32(0),
-    resetCycle = cms.untracked.int32(1000),
-    timeBoxUpperBound = cms.untracked.int32(10000)
+                                  getSCInfo = cms.untracked.bool(True)
 )
 
 
