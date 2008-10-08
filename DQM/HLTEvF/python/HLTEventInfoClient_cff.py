ceef5d8727a8d678a421c96ed05e2fa06eefbae6
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-10-08 12:16:55 +0000

    modified path to sequence

--- DQM/HLTEvF/python/HLTEventInfoClient_cff.py
+++ DQM/HLTEvF/python/HLTEventInfoClient_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.HLTEvF.HLTEventInfoClient_cfi import *
-hltEventInfo = cms.Path(hltEventInfoClient)
+hltEventInfoseqClient = cms.Sequence(hltEventInfoClient)
 
