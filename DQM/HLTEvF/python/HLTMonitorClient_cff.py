1696855dbfccd973772c5185b5f26663df327ede
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-10-08 12:17:48 +0000

    new hlt client

---
+++ DQM/HLTEvF/python/HLTMonitorClient_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+from DQM.HLTEvF.HLTEventInfoClient_cff import *
+from DQM.HLTEvF.HLTMonMuonClient_cff import *
+
+#    # use include file for dqmEnv dqmSaver
+#       include "DQMServices/Components/test/dqm_onlineEnv.cfi"
+from DQMServices.Components.DQMEnvironment_cfi import *
+
+hltmonitorClient = cms.Path(hltmonmuonseqClient*hltEventInfoseqClient*dqmEnv*dqmSaver)
+
+
+
+
