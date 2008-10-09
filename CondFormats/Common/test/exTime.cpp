a6d893c6f5fc57b1eb18eea7a095f5cca4d279c6
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-10-09 14:15:36 +0000

    compiles

--- CondFormats/Common/test/exTime.cpp
+++ CondFormats/Common/test/exTime.cpp
@@ -10,17 +10,20 @@ int main() {
     ::timeval stv;
     ::gettimeofday(&stv,0);
     
-    Cond::Time_t time = cond::time::from_timeval(stv);
+    cond::Time_t time = cond::time::from_timeval(stv);
     
     std::cout << stv.tv_sec << " " << stv.tv_usec << std::endl;
     std::cout << time << std::endl;
     stv = cond::time::to_timeval(time);
-    std::cout << cond::time::stv.tv_sec << " " << stv.tv_usec << std::endl;
+    std::cout << stv.tv_sec << " " << stv.tv_usec << std::endl;
     
     
     
     boost::posix_time::ptime bt = cond::time::to_boost(time);
-    bt +=  boost::posix_time::nanoseconds(19*25);
+
+
+    boost::posix_time::time_duration td = bt - cond::time::time0;
+ 
     
     std::cout << bt << std::endl;
     std::cout << "s. " << td.total_seconds()  << "." << td.fractional_seconds() << std::endl;
@@ -28,7 +31,11 @@ int main() {
     std::cout << "ns " << td.total_nanoseconds()  << std::endl;
     std::cout << std::endl;
     
-    boost::posix_time::time_duration td = bt - cond::time::time0;
+    
+    bt +=  boost::posix_time::nanoseconds(19*25);
+   
+    td = bt - cond::time::time0;
+ 
     
     std::cout << bt << std::endl;
     std::cout << "s. " << td.total_seconds()  << "." << td.fractional_seconds() << std::endl;
@@ -37,10 +44,10 @@ int main() {
     std::cout << std::endl;
     
     time = cond::time::from_boost(bt);
-    cond::UnpackedTime utime = unpack(time);
+    cond::UnpackedTime utime = cond::time::unpack(time);
     std::cout << "s. " << utime.first  << "." << utime.second << std::endl;
     stv = cond::time::to_timeval(time);
-    std::cout << cond::time::stv.tv_sec << " " << stv.tv_usec << std::endl;   
+    std::cout << stv.tv_sec << " " << stv.tv_usec << std::endl;   
  
   }
 
