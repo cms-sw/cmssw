6880c6db7a7c728c259a6cae01dfed7b47a65870
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-10-09 14:49:32 +0000

    sources for offline dqm analysis

---
+++ DQM/DTMonitorModule/python/DtDQMOfflineSources_cff.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+from DQM.DTMonitorModule.dtChamberEfficiencyTask_cfi import *
+from DQM.DTMonitorModule.dtSegmentTask_cfi import *
+
+dqmInfoDT = cms.EDFilter("DQMEventInfo",
+                         subSystemFolder = cms.untracked.string('DT')
+                         )
+
+dtSources = cms.Sequence(dtChamberEfficiencyMonitor*dtSegmentAnalysisMonitor*dqmInfoDT)
