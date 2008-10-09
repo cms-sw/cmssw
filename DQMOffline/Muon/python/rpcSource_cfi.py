eabd08a15636f9b56cea4c088ef5a090f1a5bde6
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2008-10-09 08:16:36 +0000

    Witout Sector

--- DQMOffline/Muon/python/rpcSource_cfi.py
+++ DQMOffline/Muon/python/rpcSource_cfi.py
@@ -1,30 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-rpcEfficiency = cms.EDFilter("RPCEfficiency",
+process.museg = cms.EDFilter("RPCEfficiency",
+
     incldt = cms.untracked.bool(True),
     incldtMB4 = cms.untracked.bool(True),
     inclcsc = cms.untracked.bool(True),
 
     debug = cms.untracked.bool(False),
+    inves = cms.untracked.bool(False),
 
     DuplicationCorrection = cms.untracked.int32(1),
 
-    MinimalResidual = cms.untracked.double(4.5),
-    MinimalResidualRB4 = cms.untracked.double(10.0),
+    rangestrips = cms.untracked.double(1.),
+    rangestripsRB4 = cms.untracked.double(4.),
     MinCosAng = cms.untracked.double(0.99),
     MaxD = cms.untracked.double(80.0),
     MaxDrb4 = cms.untracked.double(150.0),
-    MaxResStripToCountInAverage = cms.untracked.double(5.0),
-    MaxResStripToCountInAverageRB4 = cms.untracked.double(7.0),
 
     muonRPCDigis = cms.untracked.string('muonRPCDigis'),
     cscSegments = cms.untracked.string('cscSegments'),
     dt4DSegments = cms.untracked.string('dt4DSegments'),
 
     EffSaveRootFile = cms.untracked.bool(False),
-    EffRootFileName = cms.untracked.string('MuonSegEff.root'),
+    EffRootFileName = cms.untracked.string('-output-'),
     EffSaveRootFileEventsInterval = cms.untracked.int32(100)
-
 )
 
 rpcSource = cms.Sequence(rpcEfficiency)
