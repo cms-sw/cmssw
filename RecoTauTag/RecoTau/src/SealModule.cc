200944de3ef210f997bfaef2b2d36725464373ec
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-10-09 10:24:56 +0000

    *** empty log message ***

--- RecoTauTag/RecoTau/src/SealModule.cc
+++ RecoTauTag/RecoTau/src/SealModule.cc
@@ -16,7 +16,7 @@
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(PFRecoTauTagInfoProducer);
 DEFINE_ANOTHER_FWK_MODULE(PFRecoTauProducer);
-#DEFINE_ANOTHER_FWK_MODULE(PFRecoTauDecayModeDeterminator);
+//DEFINE_ANOTHER_FWK_MODULE(PFRecoTauDecayModeDeterminator);
 DEFINE_ANOTHER_FWK_MODULE(PFRecoTauDiscriminationByIsolation);
 DEFINE_ANOTHER_FWK_MODULE(PFRecoTauDiscriminationByLeadingTrackFinding);
 DEFINE_ANOTHER_FWK_MODULE(PFRecoTauDiscriminationByLeadingTrackPtCut);
