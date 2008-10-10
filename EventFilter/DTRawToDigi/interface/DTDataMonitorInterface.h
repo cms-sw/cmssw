86a7aedd95fd76c677a3668974c055a02f32cea7
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-10-10 09:48:32 +0000

    Add interface for FED integrity plots

--- EventFilter/DTRawToDigi/interface/DTDataMonitorInterface.h
+++ EventFilter/DTRawToDigi/interface/DTDataMonitorInterface.h
@@ -5,8 +5,8 @@
  *
  * Interface to the Data Quality Monitoring Module.
  *  
- *  $Date: 2006/04/13 17:14:28 $
- *  $Revision: 1.3 $
+ *  $Date: 2007/03/29 17:26:01 $
+ *  $Revision: 1.4 $
  *
  * \author M. Zanetti - INFN Padova
  *
@@ -26,6 +26,10 @@ public:
   virtual void processROS25(DTROS25Data & data, int ddu, int ros)=0;
   virtual void processFED(DTDDUData & dduData, const std::vector<DTROS25Data> & rosData,int ddu)=0;
   
+  virtual void fedEntry(int dduID) = 0;
+  virtual void fedFatal(int dduID) = 0;
+  virtual void fedNonFatal(int dduID) = 0;
+
 
 private:
   
