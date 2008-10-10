a1b1ec3280095e239e7ac47b70756b7951817a07
Dario Menasce <sha1-d1d0d0be7e5134a2688cb13fa6280d967dd0ddbd@cern.ch>
2008-10-10 11:54:50 +0000

    Added utility methods to compute elapsed times for benchmarks

--- CalibFormats/SiPixelObjects/interface/PixelTimeFormatter.h
+++ CalibFormats/SiPixelObjects/interface/PixelTimeFormatter.h
@@ -17,6 +17,30 @@ namespace pos{
   class PixelTimeFormatter
   {
   public:
+
+    PixelTimeFormatter(std::string source) 
+    {
+     origin_ = source ;
+     std::cout << "[PixelTimeFormatter::PixelTimeFormatter()]\t\t    Time counter started for " << origin_ << std::endl ;
+     startTime_ = getImSecTime() ;
+    }
+    
+    void stopTimer(void) 
+    {
+     endTime_ = getImSecTime() ;
+     unsigned int seconds  = (unsigned int)endTime_.tv_sec  - (unsigned int)startTime_.tv_sec  ;
+     double useconds = (double)(endTime_.tv_usec - startTime_.tv_usec) / 1000000. ;   
+     if( seconds == 0 )
+       {
+         std::cout << "[PixelTimeFormatter::stopTimer()]\t\t\t    Elapsed time: " << useconds << " seconds for " << origin_ << std::endl ;
+       }
+     else 
+       {
+         std::cout << "[PixelTimeFormatter::stopTimer()]\t\t\t    Elapsed time: " <<  seconds << " seconds for " << origin_ << std::endl ;
+       } 
+  
+    }
+
     static std::string getTime(void) 
     {
       char theDate[20] ;
@@ -38,6 +62,15 @@ namespace pos{
       return date ;
     }
 
+    struct tm * getITime(void) 
+    {
+      struct tm *thisTime;
+      time_t aclock;
+      time( &aclock );		  
+      thisTime = localtime( &aclock ); 
+      return thisTime ;
+    }
+
     static std::string getmSecTime(void) 
     {
       char theDate[20] ;
@@ -50,6 +83,23 @@ namespace pos{
 	      (unsigned int)msecTime.tv_usec ); 
       return std::string(theDate) ;
     }
+
+    struct timeval getImSecTime(void) 
+    {
+      struct timeval msecTime;
+      gettimeofday(&msecTime, (struct timezone *)0) ;
+      
+      return msecTime ;
+    }
+    
+    //---------------------------------------------------------------
+    
+    private:
+    
+     struct timeval startTime_ ;
+     struct timeval endTime_   ;
+     std::string    origin_    ;
+     bool           verbose_   ;
   } ;
 }
 
