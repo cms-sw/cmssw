7baa14c1e81cb1afd957bd47de2155d62f66e509
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-10-11 08:32:57 +0000

    first release

---
+++ CondFormats/RPCObjects/src/T_EventSetup_RPCObCond.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/RPCObjects/interface/RPCObCond.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+EVENTSETUP_DATA_REG(RPCObCond);
+
---
+++ CondFormats/RPCObjects/src/T_EventSetup_RPCObGas.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/RPCObjects/interface/RPCObGas.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+EVENTSETUP_DATA_REG(RPCObGas);
+
