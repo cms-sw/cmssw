37967c49d3da2c33a1ebabef286bc6ff8b7b2c18
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-10-11 08:43:19 +0000

    first release

---
+++ CondFormats/DataRecord/interface/RPCObCondRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_RPCObCondRcd_h
+#define DataRecord_RPCObCondRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCObCondRcd
+// 
+/**\class RPCObCondRcd RPCObCondRcd.h CondFormats/DataRecord/interface/RPCObCondRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Fri Oct 10 20:02:37 CEST 2008
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class RPCObCondRcd : public edm::eventsetup::EventSetupRecordImplementation<RPCObCondRcd> {};
+
+#endif
