dc82fabff8618a25dd7f8a47287f494483c4d45b
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-10-12 22:36:17 +0000

    module for seed collection merging is run insided tracking sequence again. Necessary for back-compatibility with electron reconstruction

--- RecoTracker/Configuration/python/RecoTracker_cff.py
+++ RecoTracker/Configuration/python/RecoTracker_cff.py
@@ -10,7 +10,16 @@ from RecoTracker.RoadSearchCloudMaker.RoadSearchClouds_cff import *
 from RecoTracker.RoadSearchTrackCandidateMaker.RoadSearchTrackCandidates_cff import *
 from RecoTracker.TrackProducer.RSFinalFitWithMaterial_cff import *
 
-ckftracks = cms.Sequence(iterTracking*trackCollectionMerging)
+### Not the Tracking uses the 2 seed collections separately. The merged seed collection is produced 
+### for backward compatibility with electron reconstruction
+newCombinedSeeds = RecoTracker.TkSeedGenerator.GlobalCombinedSeeds_cfi.globalCombinedSeeds.clone()
+newCombinedSeeds.seedCollections = cms.VInputTag(
+    cms.InputTag('newSeedFromTriplets'),
+    cms.InputTag('newSeedFromPairs'),
+)
+
+
+ckftracks = cms.Sequence(iterTracking*trackCollectionMerging*newCombinedSeeds)
 
 rstracks = cms.Sequence(roadSearchSeeds*
                         roadSearchClouds*rsTrackCandidates*
