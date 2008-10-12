248a6a9f6a52819a604acb726d22aee93e7f56b2
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-10-12 02:13:23 +0000

    2nd attempt to remove obsolete files

--- RecoTracker/IterativeTracking/python/SecVxFilter_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import RecoParticleFlow.PFTracking.vertexFilter_cfi
-secStep = RecoParticleFlow.PFTracking.vertexFilter_cfi.vertFilter.clone()
-
--- RecoTracker/IterativeTracking/python/ThVxFilter_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import RecoParticleFlow.PFTracking.vertexFilter_cfi
-thStep = RecoParticleFlow.PFTracking.vertexFilter_cfi.vertFilter.clone()
-thStep.recTracks = cms.InputTag("thWithMaterialTracks")
-thStep.DistZFromVertex = 0.1
-thStep.TrackAlgorithm = 'iter3'
-
