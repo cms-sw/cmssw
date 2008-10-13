5bb32b5bbeb7337fd435e3db4ee0c32ef1a8c74b
Maryam Zeinali <sha1-12edad2c77bcf6ef7f491a18b8472cb9870f9ed8@cern.ch>
2008-10-13 13:26:14 +0000

    IsoDeposit methods added

--- TopQuarkAnalysis/Examples/plugins/TopElecAnalyzer.cc
+++ TopQuarkAnalysis/Examples/plugins/TopElecAnalyzer.cc
@@ -3,7 +3,7 @@
 #include "DataFormats/Common/interface/TriggerResults.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
 #include "TopQuarkAnalysis/Examples/plugins/TopElecAnalyzer.h"
-
+#include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
 
 TopElecAnalyzer::TopElecAnalyzer(const edm::ParameterSet& cfg):
   elecs_(cfg.getParameter<edm::InputTag>("input"))
@@ -19,6 +19,22 @@ TopElecAnalyzer::TopElecAnalyzer(const edm::ParameterSet& cfg):
   denElec_= fs->make<TH1F>("denElec", "den_{Elec}", 100, -2.,  2.);
   genElec_= fs->make<TH1F>("genElec", "gen_{Elec}", 100, -2.,  2.);
   trgElec_= fs->make<TH1F>("trgElec", "trg_{Elec}", 100, -1.,  1.);
+
+  CountInSize_T=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_T","NumDepoInIsoConeVsConeSize_T",30,0.04,0.34,50,0.,50.);
+  CountInSize_E=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_E","NumDepoInIsoConeVsConeSize_E",30,0.04,0.34,50,0.,50.);
+  CountInSize_H=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_H","NumDepoInIsoConeVsConeSize_H",30,0.04,0.34,50,0.,50.);
+
+  DepoInSize_T =fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_T","AmountDepoInIsoConeVsConeSize_T",30,0.04,0.34,50,0.,50.);
+  DepoInSize_E =fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_E","AmountDepoInIsoConeVsConeSize_E",30,0.04,0.34,50,0.,50.);
+  DepoInSize_H =fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_H","AmountDepoInIsoConeVsConeSize_H",30,0.04,0.34,50,0.,50.);
+
+  Count_Threshold_T=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_Threshold_T","NumDepoInIsoConeVsConeSize_Threshold_T",30,0.04,0.34,50,0.,50.);
+  Count_Threshold_E=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_Threshold_E","NumDepoInIsoConeVsConeSize_Threshold_E",30,0.04,0.34,50,0.,50.);
+  Count_Threshold_H=fs->make<TH2D>("NumDepoInIsoConeVsConeSize_Threshold_H","NumDepoInIsoConeVsConeSize_Threshold_H",30,0.04,0.34,50,0.,50.);
+
+  Depo_Threshold_T=fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_Threshold_T","AmountDepoInIsoConeVsConeSize_Threshold_T",30,0.04,0.34,50,0.,50.);
+  Depo_Threshold_E=fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_Threshold_E","AmountDepoInIsoConeVsConeSize_Threshold_E",30,0.04,0.34,50,0.,50.);
+  Depo_Threshold_H=fs->make<TH2D>("AmountDepoInIsoConeVsConeSize_Threshold_H","AmountDepoInIsoConeVsConeSize_Threshold_H",30,0.04,0.34,50,0.,50.);
 }
 
 TopElecAnalyzer::~TopElecAnalyzer()
@@ -131,6 +147,79 @@ TopElecAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& setup)
     if(elec->genLepton()){
       genElec_->Fill( (elec->gsfTrack()->pt() - elec->genLepton()->pt())/elec->genLepton()->pt() );
     }
+
+    // --------------------------------------------------
+    // Isolation in the tracker
+    // --------------------------------------------------
+
+    const reco::IsoDeposit *TID=0;
+    TID = elec->trackerIsoDeposit();
+   
+    //Isolation Cone coordinates
+    double IsoCone_eta=TID->eta();
+    double IsoCone_phi=TID->phi();
+    
+    
+    //Veto Cone coordinates
+    double VetoCone_eta =TID->veto().vetoDir.eta();
+    double VetoCone_phi =TID->veto().vetoDir.phi();
+    float  VetoCone_size=TID->veto().dR;
+    
+   
+    //get the candidate tag
+    float Cand_Tag_T =TID->candEnergy();
+    
+    //get the deposit & count within the isolation cone with the different cone size
+    double radius=0.0;
+    for(int i=0;i<6;i++)
+      {
+
+       radius+=0.05;
+       CountInSize_T->Fill(radius,TID->depositAndCountWithin(radius).second);
+       DepoInSize_T ->Fill(radius,TID->depositAndCountWithin(radius).first);
+
+       //If the deposit should exceed some threshold
+       double Threshold=0.3;
+       Count_Threshold_T->Fill(radius,TID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).second);
+       Depo_Threshold_T ->Fill(radius,TID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).first);
+ 
+     }
+
+    // --------------------------------------------------
+    // Isolation in the ecal & hcal
+    // --------------------------------------------------
+    const reco::IsoDeposit *EID=0;
+    const reco::IsoDeposit *HID=0;
+    EID = elec->ecalIsoDeposit();
+    HID = elec->hcalIsoDeposit();
+
+
+    //get the candidate tag
+    float Cand_Tag_E =EID->candEnergy();
+    float Cand_Tag_H =HID->candEnergy();
+
+
+    //get the deposit & count within the isolation cone with the different cone size
+    for(int i=1;i<30;i++)
+      {
+
+       double radius=i/100;
+       CountInSize_E->Fill(radius,EID->depositAndCountWithin(radius).second);
+       DepoInSize_E ->Fill(radius,EID->depositAndCountWithin(radius).first);
+
+       CountInSize_H->Fill(radius,HID->depositAndCountWithin(radius).second);
+       DepoInSize_H ->Fill(radius,HID->depositAndCountWithin(radius).first);
+
+       //If the deposit should exceed some threshold
+       double Threshold=0.3;
+       Count_Threshold_E->Fill(radius,EID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).second);
+       Depo_Threshold_E ->Fill(radius,EID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).first);
+
+       Count_Threshold_H->Fill(radius,HID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).second);
+       Depo_Threshold_H ->Fill(radius,HID->depositAndCountWithin(radius,reco::IsoDeposit::Vetos(),Threshold).first);
+       
+      }
+
   }
 }
 
