6a44c66d2ac888012fda96ac99b9cc0e92d91bae
vpatras <sha1-abf92da7f7d41be4c755e76ff55d1a85f0d3e23d@cern.ch>
2008-10-13 13:07:15 +0000

    Introducing XmasToDQMEventInfo module

--- DQMServices/XdaqCollector/plugins/SealModule.cc
+++ DQMServices/XdaqCollector/plugins/SealModule.cc
@@ -1,6 +1,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 // The example source module
 #include "DQMServices/XdaqCollector/interface/XmasToDQMSource.h"
+#include "DQMServices/XdaqCollector/interface/XmasToDQMEventInfo.h"
 DEFINE_FWK_MODULE(XmasToDQMSource);
+DEFINE_FWK_MODULE(XmasToDQMEventInfo);
 
 
