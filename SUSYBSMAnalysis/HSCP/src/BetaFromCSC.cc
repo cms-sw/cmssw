2e4e6317993df5a5f041fe9d2acb383a6ef88440
Piotr Traczyk <sha1-f63c8ead76e5e4a129ecffbbd6f7786205ea2389@cern.ch>
2008-10-13 13:01:10 +0000

    Added Beta from CSC

--- SUSYBSMAnalysis/HSCP/src/BetaFromCSC.cc
+++ SUSYBSMAnalysis/HSCP/src/BetaFromCSC.cc
@@ -1,9 +1,9 @@
 // -*- C++ -*-
 //
-// Package:    BetaFromTOF
-// Class:      BetaFromTOF
+// Package:    BetaFromCSC
+// Class:      BetaFromCSC
 // 
-/**\class BetaFromTOF BetaFromTOF.cc SUSYBSMAnalysis/BetaFromTOF/src/BetaFromTOF.cc
+/**\class BetaFromCSC BetaFromCSC.cc SUSYBSMAnalysis/BetaFromCSC/src/BetaFromCSC.cc
 
 Description: <one line class summary>
 
@@ -13,7 +13,7 @@ Implementation:
 //
 // Original Author:  Traczyk Piotr
 //         Created:  Thu Oct 11 15:01:28 CEST 2007
-// $Id: BetaFromTOF.cc,v 1.20 2008/10/08 15:28:24 ptraczyk Exp $
+// $Id: BetaFromCSC.cc,v 1.1 2008/10/08 15:28:24 ptraczyk Exp $
 //
 //
 
@@ -36,26 +36,14 @@ Implementation:
 
 #include "Geometry/Records/interface/MuonGeometryRecord.h"
 #include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
-#include "Geometry/DTGeometry/interface/DTGeometry.h"
-#include "Geometry/DTGeometry/interface/DTLayer.h"
-#include "Geometry/DTGeometry/interface/DTChamber.h"
-#include "Geometry/DTGeometry/interface/DTSuperLayer.h"
-#include "DataFormats/DTRecHit/interface/DTSLRecSegment2D.h"
-#include "DataFormats/DTRecHit/interface/DTRecSegment2D.h"
-#include "RecoLocalMuon/DTSegment/src/DTSegmentUpdator.h"
-#include "RecoLocalMuon/DTSegment/src/DTSegmentCleaner.h"
-#include "RecoLocalMuon/DTSegment/src/DTHitPairForFit.h"
-#include "RecoLocalMuon/DTSegment/src/DTSegmentCand.h"
-#include "Geometry/DTGeometry/interface/DTTopology.h"
+#include "DataFormats/CSCRecHit/interface/CSCSegment.h"
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
 
 #include "Geometry/Records/interface/GlobalTrackingGeometryRecord.h"
 #include "Geometry/CommonDetUnit/interface/GlobalTrackingGeometry.h"
 
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/DTRecHit/interface/DTRecSegment2D.h"
-#include "DataFormats/DTRecHit/interface/DTRecSegment4DCollection.h"
-#include "DataFormats/DTRecHit/interface/DTRecSegment2DCollection.h"
 #include "DataFormats/TrackingRecHit/interface/TrackingRecHitFwd.h"
 #include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
 #include "SUSYBSMAnalysis/HSCP/interface/MuonSegmentMatcher.h"
@@ -105,10 +93,10 @@ typedef std::vector<segmData> segmVect;
 // class decleration
 //
 
-class BetaFromTOF : public edm::EDProducer {
+class BetaFromCSC : public edm::EDProducer {
 public:
-  explicit BetaFromTOF(const edm::ParameterSet&);
-  ~BetaFromTOF();
+  explicit BetaFromCSC(const edm::ParameterSet&);
+  ~BetaFromCSC();
 
 private:
   virtual void beginJob(const edm::EventSetup&) ;
@@ -121,7 +109,7 @@ private:
   edm::InputTag TKtrackTags_; 
   edm::InputTag STAtrackTags_; 
   edm::InputTag MuonTags_; 
-  edm::InputTag DTSegmentTags_; 
+  edm::InputTag CSCSegmentTags_; 
 
   unsigned int theHitsMin;
   double thePruneCut;
@@ -148,10 +136,10 @@ private:
 //
 // constructors and destructor
 //
-BetaFromTOF::BetaFromTOF(const edm::ParameterSet& iConfig)
+BetaFromCSC::BetaFromCSC(const edm::ParameterSet& iConfig)
   :
   MuonTags_(iConfig.getUntrackedParameter<edm::InputTag>("Muons")),
-  DTSegmentTags_(iConfig.getUntrackedParameter<edm::InputTag>("DTsegments")),
+  CSCSegmentTags_(iConfig.getUntrackedParameter<edm::InputTag>("CSCsegments")),
   theHitsMin(iConfig.getParameter<int>("HitsMin")),
   thePruneCut(iConfig.getParameter<double>("PruneCut")),
   debug(iConfig.getParameter<bool>("debug"))
@@ -169,7 +157,7 @@ BetaFromTOF::BetaFromTOF(const edm::ParameterSet& iConfig)
 }
 
 
-BetaFromTOF::~BetaFromTOF()
+BetaFromCSC::~BetaFromCSC()
 {
  
   if (theService) delete theService;
@@ -183,14 +171,14 @@ BetaFromTOF::~BetaFromTOF()
 
 // ------------ method called to produce the data  ------------
 void
-BetaFromTOF::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+BetaFromCSC::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
   //using namespace edm;
   using reco::TrackCollection;
 
   if (debug) 
-    cout << " *** Beta from TOF Start ***" << endl;
+    cout << " *** Beta from CSC Start ***" << endl;
 
   theService->update(iSetup);
 
@@ -239,207 +227,89 @@ BetaFromTOF::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
       cout << " STA Track:   RecHits: " << (*muonTrack).recHitsSize() 
            << " momentum: " << (*muonTrack).p() << endl;
 
-    // get the DT segments that were used to construct the muon
-    vector<const DTRecSegment4D*> range = theMatcher->matchDT(*(mi->standAloneMuon()),iEvent);
+    // get the CSC segments that were used to construct the muon
+    vector<const CSCSegment*> range = theMatcher->matchCSC(*(mi->standAloneMuon()),iEvent);
+
+    nStations=0;
+    vector <double> dstnc, dsegm, dtraj;
 
     // create a collection on TimeMeasurements for the track        
-    for (vector<const DTRecSegment4D*>::iterator rechit = range.begin(); rechit!=range.end();++rechit) {
-
+    for (vector<const CSCSegment*>::iterator rechit = range.begin(); rechit!=range.end();++rechit) {
+      nStations++;
+      
       // Create the ChamberId
       DetId id = (*rechit)->geographicalId();
-      DTChamberId chamberId(id.rawId());
-      DTLayerId layerId(id.rawId());
-      DTSuperLayerId slayerId(id.rawId());
+      CSCDetId chamberId(id.rawId());
       int station = chamberId.station();
 
-      if ( (!(*rechit)->hasPhi()) || (!(*rechit)->hasZed()) ) continue;
-      // TODO Check for showers in a cone around the 4D hit                 
+      if (!(*rechit)->specificRecHits().size()) continue;
 
-      // loop over (theta, phi) segments
-      for (int phi=0; phi<2; phi++) {
+      const GeomDet* geomDet = theTrackingGeometry->idToDet((*rechit)->geographicalId());
+      const vector<CSCRecHit2D> hits2d = (*rechit)->specificRecHits();
 
-	const DTRecSegment2D* segm;
-	if (phi) segm = dynamic_cast<const DTRecSegment2D*>((*rechit)->phiSegment()); 
-	else segm = dynamic_cast<const DTRecSegment2D*>((*rechit)->zSegment());
-	if(segm == 0){cout << "skip this" << endl;  continue; }   
-	if (!segm->specificRecHits().size()) continue;
+      // store all the hits from the segment
+      for (vector<CSCRecHit2D>::const_iterator hiti=hits2d.begin(); hiti!=hits2d.end(); hiti++) {
 
-	const GeomDet* geomDet = theTrackingGeometry->idToDet(segm->geographicalId());
-	const vector<DTRecHit1D> hits1d = segm->specificRecHits();
+	const GeomDet* csclayer = theTrackingGeometry->idToDet(hiti->geographicalId());
+        TimeMeasurement thisHit;
 
-	// store all the hits from the segment
-	for (vector<DTRecHit1D>::const_iterator hiti=hits1d.begin(); hiti!=hits1d.end(); hiti++) {
+	double t0_hit = hiti->tpeak()-212.; // corresponded to t0 in 2_1_9 RelVal MC
+	double dist = csclayer->toGlobal(hiti->localPosition()).mag();
 
-	  const GeomDet* dtcell = theTrackingGeometry->idToDet(hiti->geographicalId());
-	  TimeMeasurement thisHit;
-
-	  thisHit.driftCell = hiti->geographicalId();
-	  if (hiti->lrSide()==DTEnums::Left) thisHit.isLeft=true; else thisHit.isLeft=false;
-	  thisHit.isPhi = phi;
-	  thisHit.posInLayer = geomDet->toLocal(dtcell->toGlobal(hiti->localPosition())).x();
-	  thisHit.distIP = dtcell->toGlobal(hiti->localPosition()).mag();
-	  thisHit.station = station;
-	  tof.timeMeasurements.push_back(thisHit);
-	}
-
-      } // phi = (0,1) 	        
-    } // rechit
-
-      // Make a copy of the time measurement collection - we'll be cutting away hits, but we want to store all
-    vector<TimeMeasurement> tms = tof.timeMeasurements;
-      
-    bool modified = false;
-    totalWeight=0;
-    nStations=0;
-    vector <double> dstnc, dsegm, dtraj, hitWeight, left;
-    
-    // Now loop over the measurements, calculate 1/beta and cut away outliers
-    do {    
-
-      modified = false;
-      dstnc.clear();
-      dsegm.clear();
-      dtraj.clear();
-      hitWeight.clear();
-      left.clear();
-      
-      vector <int> hit_idx;
-      totalWeight=0;
-      nStations=0;
-      
-      // Rebuild segments
-      for (int sta=1;sta<5;sta++)
-        for (int phi=0;phi<2;phi++) {
-          vector <TimeMeasurement> seg;
-          vector <int> seg_idx;
-          int tmpos=0;
-          for (vector<TimeMeasurement>::iterator tm=tms.begin(); tm!=tms.end(); ++tm) {
-            if ((tm->station==sta) && (tm->isPhi==phi)) {
-              seg.push_back(*tm);
-              seg_idx.push_back(tmpos);
-            }
-            tmpos++;  
-          }
-          
-          unsigned int segsize = seg.size();
-          
-          if (segsize<theHitsMin) continue;
-
-          double a=0, b=0;
-          vector <double> hitxl,hitxr,hityl,hityr;
-
-          for (vector<TimeMeasurement>::iterator tm=seg.begin(); tm!=seg.end(); ++tm) {
- 
-            DetId id = tm->driftCell;
-            const GeomDet* dtcell = theTrackingGeometry->idToDet(id);
-            DTChamberId chamberId(id.rawId());
-            const GeomDet* dtcham = theTrackingGeometry->idToDet(chamberId);
-
-            double celly=dtcham->toLocal(dtcell->position()).z();
+        std::pair< TrajectoryStateOnSurface, double> tsos;
+        tsos=propag->propagateWithPath(muonFTS,csclayer->surface());
             
-            if (tm->isLeft) {
-	      hitxl.push_back(celly);
-	      hityl.push_back(tm->posInLayer);
-            } else {
-	      hitxr.push_back(celly);
-	      hityr.push_back(tm->posInLayer);
-            }    
-          }
+        if (tsos.first.isValid()) dist = tsos.second+posp.mag();
 
-          if (!fitT0(a,b,hitxl,hityl,hitxr,hityr)) {
-            if (debug)
-              cout << "     t0 = zero, Left hits: " << hitxl.size() << " Right hits: " << hitxr.size() << endl;
-            continue;
-          }
-          
-          // a segment must have at least one left and one right hit
-          if ((!hitxl.size()) || (!hityl.size())) continue;
-
-          nStations++;
-
-          int segidx=0;
-          for (vector<TimeMeasurement>::const_iterator tm=seg.begin(); tm!=seg.end(); ++tm) {
-
-            DetId id = tm->driftCell;
-            const GeomDet* dtcell = theTrackingGeometry->idToDet(id);
-            DTChamberId chamberId(id.rawId());
-            const GeomDet* dtcham = theTrackingGeometry->idToDet(chamberId);
-
-            double dist = tm->distIP;
-            double layerZ  = dtcham->toLocal(dtcell->position()).z();
-            double segmLocalPos = b+layerZ*a;
-            double hitLocalPos = tm->posInLayer;
-            int hitSide = -tm->isLeft*2+1;
-            double t0_segm = (-(hitSide*segmLocalPos)+(hitSide*hitLocalPos))/0.00543;
+	thisHit.driftCell = hiti->geographicalId();
+	thisHit.isLeft=false;
+	thisHit.isPhi =0;
+	thisHit.posInLayer = t0_hit;
+	thisHit.distIP = dist;
+	thisHit.station = station;
+	tof.timeMeasurements.push_back(thisHit);
             
-            std::pair< TrajectoryStateOnSurface, double> tsos;
-            tsos=propag->propagateWithPath(muonFTS,dtcell->surface());
-            
-            if (tsos.first.isValid()) dist = tsos.second+posp.mag();
-            
-            if ((debug) || (fabs(dist)>1500.)) {
-              cout << " Dist: " << dist << "   segm: " << segmLocalPos << "   hit: " << hitLocalPos;
-              if (tsos.first.isValid()) cout << " traj: " << dtcham->toLocal(tsos.first.globalPosition()) << " path: " << tsos.second+posp.mag();
-              cout << " Start: " << posp;
-              cout << endl;
-            }
-
-            dstnc.push_back(dist);
-            dsegm.push_back(t0_segm);
-            left.push_back(hitSide);
-            hitWeight.push_back(((double)seg.size()-2.)/(double)seg.size());
-            hit_idx.push_back(seg_idx.at(segidx));
-            segidx++;
-          }
-          
-          totalWeight+=seg.size()-2;
-
+        if (debug) {
+          cout << " Dist: " << dist;
+          if (tsos.first.isValid()) cout << " traj: " << csclayer->toLocal(tsos.first.globalPosition()) << " path: " << tsos.second+posp.mag();
+          cout << " Start: " << posp << " t0: " << t0_hit;
+          cout << endl;
         }
 
-      nStations/=2;
-      invbeta=0;
-
-      // calculate the value and error of 1/beta from the complete set of 1D hits
-      if (debug)
-	cout << " Points for global fit: " << dstnc.size() << endl;
-
-      // inverse beta - weighted average of the contributions from individual hits
-      for (unsigned int i=0;i<dstnc.size();i++) {
-	invbeta+=(1.+dsegm.at(i)/dstnc.at(i)*30.)*hitWeight.at(i)/totalWeight;
-	//          cout << "    Dstnc: " << dstnc.at(i) << "   delta t0(hit-segment): " << dsegm.at(i) << "   weight: " << hitWeight.at(i); 
-	//          cout << " Local 1/beta: " << 1.+dsegm.at(i)/dstnc.at(i)*30. << endl;
+        dstnc.push_back(dist);
+        dsegm.push_back(t0_hit);
+        totalWeight++;
       }
+    } // rechit
+    
+    invbeta=0;
 
-      double chimax=0.;
-      vector<TimeMeasurement>::iterator tmmax;
+    // calculate the value and error of 1/beta from the complete set of 1D hits
+    if (debug)
+      cout << " Points for global fit: " << dstnc.size() << endl;
+
+    // inverse beta - weighted average of the contributions from individual hits
+    for (unsigned int i=0;i<dstnc.size();i++) {
+      invbeta+=(1.+dsegm.at(i)/dstnc.at(i)*30.)/totalWeight;
+      //          cout << "    Dstnc: " << dstnc.at(i) << "   delta t0(hit-segment): " << dsegm.at(i) << "   weight: " << hitWeight.at(i); 
+      //          cout << " Local 1/beta: " << 1.+dsegm.at(i)/dstnc.at(i)*30. << endl;
+    }
     
-      // the dispersion of inverse beta
-      double invbetaerr=0,diff;
-      for (unsigned int i=0;i<dstnc.size();i++) {
-	diff=(1.+dsegm.at(i)/dstnc.at(i)*30.)-invbeta;
-	diff=diff*diff*hitWeight.at(i);
-	invbetaerr+=diff;
-	if (diff/totalWeight/totalWeight>chimax/100.) { 
-	  tmmax=tms.begin()+hit_idx.at(i);
-	  chimax=diff;
-	}
-      }
+    // the dispersion of inverse beta
+    double invbetaerr=0,diff;
+    for (unsigned int i=0;i<dstnc.size();i++) {
+      diff=(1.+dsegm.at(i)/dstnc.at(i)*30.)-invbeta;
+      diff=diff*diff;
+      invbetaerr+=diff;
+    }
     
-      invbetaerr=sqrt(invbetaerr/totalWeight);
+    invbetaerr=sqrt(invbetaerr/totalWeight);
  
-      // cut away the outliers
-      if (chimax>thePruneCut) {
-	tms.erase(tmmax);
-	modified=true;
-      }    
+    if (debug)
+      cout << " Measured 1/beta: " << invbeta << " +/- " << invbetaerr << endl;
 
-      if (debug)
-	cout << " Measured 1/beta: " << invbeta << " +/- " << invbetaerr << endl;
-
-      tof.invBeta=invbeta;
-      tof.invBetaErr=invbetaerr;      
-
-    } while (modified);
+    tof.invBeta=invbeta;
+    tof.invBetaErr=invbetaerr;      
 
     // unconstrained fit to the full set of points
     vector <double> x,y;
@@ -448,13 +318,12 @@ BetaFromTOF::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     for (unsigned int i=0;i<dstnc.size();i++) {
       x.push_back(dstnc.at(i)/30.);
       y.push_back(dsegm.at(i)+dstnc.at(i)/30.);
-      vertexTime+=dsegm.at(i)*hitWeight.at(i)/totalWeight;
+      vertexTime+=dsegm.at(i)/totalWeight;
     }
 
-    double diff;
     for (unsigned int i=0;i<dstnc.size();i++) {
       diff=dsegm.at(i)-vertexTime;
-      vertexTimeErr+=diff*diff*hitWeight.at(i);
+      vertexTimeErr+=diff*diff;
     }
     vertexTimeErr=sqrt(vertexTimeErr/totalWeight);
       
@@ -483,12 +352,12 @@ BetaFromTOF::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.put(res);
 
   if (debug) 
-    cout << " *** Beta from TOF End ***" << endl;
+    cout << " *** Beta from CSC End ***" << endl;
 
 }
 
 double
-BetaFromTOF::fitT0(double &a, double &b, vector<double> xl, vector<double> yl, vector<double> xr, vector<double> yr ) {
+BetaFromCSC::fitT0(double &a, double &b, vector<double> xl, vector<double> yl, vector<double> xr, vector<double> yr ) {
 
   double sx=0,sy=0,sxy=0,sxx=0,ssx=0,ssy=0,s=0,ss=0;
 
@@ -532,7 +401,7 @@ BetaFromTOF::fitT0(double &a, double &b, vector<double> xl, vector<double> yl, v
 
 
 void 
-BetaFromTOF::rawFit(double &a, double &da, double &b, double &db, const vector<double> hitsx, const vector<double> hitsy) {
+BetaFromCSC::rawFit(double &a, double &da, double &b, double &db, const vector<double> hitsx, const vector<double> hitsy) {
 
   double s=0,sx=0,sy=0,x,y;
   double sxx=0,sxy=0;
@@ -562,7 +431,7 @@ BetaFromTOF::rawFit(double &a, double &da, double &b, double &db, const vector<d
 }
 
 void 
-BetaFromTOF::textplot(vector<double> x, vector <double> y, vector <double> side)
+BetaFromCSC::textplot(vector<double> x, vector <double> y, vector <double> side)
 {
 
   int data[82][42];
@@ -608,14 +477,14 @@ BetaFromTOF::textplot(vector<double> x, vector <double> y, vector <double> side)
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-BetaFromTOF::beginJob(const edm::EventSetup&)
+BetaFromCSC::beginJob(const edm::EventSetup&)
 {
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-BetaFromTOF::endJob() {
+BetaFromCSC::endJob() {
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(BetaFromTOF);
+DEFINE_FWK_MODULE(BetaFromCSC);
