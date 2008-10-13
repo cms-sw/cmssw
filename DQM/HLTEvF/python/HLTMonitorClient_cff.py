a32fb09d8ea67806c67e4c4096ba3a2dc959f249
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-10-13 13:25:37 +0000

    bug fix

--- DQM/HLTEvF/python/HLTMonitorClient_cff.py
+++ DQM/HLTEvF/python/HLTMonitorClient_cff.py
@@ -5,9 +5,9 @@ from DQM.HLTEvF.HLTMonMuonClient_cff import *
 
 #    # use include file for dqmEnv dqmSaver
 #       include "DQMServices/Components/test/dqm_onlineEnv.cfi"
-from DQMServices.Components.DQMEnvironment_cfi import *
+#from DQMServices.Components.DQMEnvironment_cfi import *
 
-hltmonitorClient = cms.Path(hltmonmuonseqClient*hltEventInfoseqClient*dqmEnv*dqmSaver)
+hltmonitorClient = cms.Path(hltmonmuonseqClient*hltEventInfoseqClient)
 
 
 
