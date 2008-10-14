618bb1e41e0476fbf9b54a0d1def1e1a01690bd5
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-10-14 14:42:32 +0000

    This version uses genParticle instead of the whole HepMC product

--- HLTriggerOffline/Tau/src/HLTTauMCProducer.cc
+++ HLTriggerOffline/Tau/src/HLTTauMCProducer.cc
@@ -1,9 +1,8 @@
 #include "HLTriggerOffline/Tau/interface/HLTTauMCProducer.h"
-#include "TLorentzVector.h"
-
 
 using namespace edm;
 using namespace std;
+using namespace reco;
 
 HLTTauMCProducer::HLTTauMCProducer(const edm::ParameterSet& mc)
 {
@@ -25,7 +24,7 @@ HLTTauMCProducer::HLTTauMCProducer(const edm::ParameterSet& mc)
   produces<LorentzVectorCollection>("HadronicTauOneAndThreeProng");
   produces<LorentzVectorCollection>("TauOther");
   produces<LorentzVectorCollection>("Neutrina");
-  
+
 }
 
 HLTTauMCProducer::~HLTTauMCProducer(){ }
@@ -43,171 +42,181 @@ void HLTTauMCProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   auto_ptr<LorentzVectorCollection> product_Other(new LorentzVectorCollection);
   auto_ptr<LorentzVectorCollection> product_Neutrina(new LorentzVectorCollection);
   
-  edm::Handle<HepMCProduct> evt;
-  iEvent.getByLabel(MC_, evt);
-  HepMC::GenEvent*  myGenEvent = new  HepMC::GenEvent(*(evt->GetEvent()));
-  HepMC::GenEvent::particle_iterator p = myGenEvent->particles_begin();
+  edm::Handle<GenParticleCollection> genParticles;
+  iEvent.getByLabel(MC_, genParticles);
+
+  GenParticleCollection::const_iterator p = genParticles->begin();
   
-  HepMC::GenVertex* TauDecVtx = 0 ;
-  
-  for (;p != myGenEvent->particles_end(); ++p ) {
+  for (;p != genParticles->end(); ++p ) {
     //Check the PDG ID
-    bool pdg_ok = false;
+    bool pdg_ok = true;
     for(size_t pi =0;pi<m_PDG_.size();++pi)
       {
-	if(abs((*p)->pdg_id())== m_PDG_[pi]){
-	  pdg_ok = true;
-	}
+	if(abs((*p).pdgId())== m_PDG_[pi] && abs((*p).status()) == 3 ){
+   	  pdg_ok = true;
+	  //	  cout<<" Bsoson particles: "<< (*p).pdgId()<< " " <<(*p).status() << " "<< pdg_ok<<endl;
+   	}
       }
-
-    // Check if the boson is one of interest and if there is a valid end_vertex
-    if(  pdg_ok  &&
-	 (*p)->end_vertex() )
+    
+    // Check if the boson is one of interest and if there is a valid vertex
+    if(  pdg_ok )
       {
 	
-	//	TLorentzVector Boson((*p)->momentum().px(),(*p)->momentum().py(),(*p)->momentum().pz(),(*p)->momentum().e());
+	std::vector<GenParticle*> decayProducts;
+	
+	TLorentzVector Boson((*p).px(),(*p).py(),(*p).pz(),(*p).energy());	
+	
+	for (GenParticle::const_iterator BosonIt=(*p).begin(); BosonIt != (*p).end(); BosonIt++){
+	  // cout<<" Dparticles: "<< (*BosonIt).pdgId() << " "<< (*BosonIt).status()<<endl;
+	  
+	  if (abs((*BosonIt).pdgId()) == 15 && ((*BosonIt).status()==3)) //if it is a Tau and decayed
+	    {
+	      decayProducts.clear();	  
+	      //	      cout<<" Boson daugther particles: "<< (*BosonIt).pdgId() << " "<< (*BosonIt).status()<< endl;	      
+	      for (GenParticle::const_iterator TauIt = (*BosonIt).begin(); TauIt != (*BosonIt).end(); TauIt++) {
+		//	cout<<" Tau daughter particles: "<< (*TauIt).pdgId() << " "<< (*TauIt).status()<<endl;
 
-	HepMC::GenVertex::particle_iterator z = (*p)->end_vertex()->particles_begin(HepMC::descendants);  // Find the beginning of particles after the vertex to loop over there
-	for(; z != (*p)->end_vertex()->particles_end(HepMC::descendants); z++)
-	  {
-	    if(abs((*z)->pdg_id()) == 15 && (*z)->status()==2)  // if it is a Tau
-	      {
-
-		LorentzVector Visible_Taus(0.,0.,0.,0.);
-		LorentzVector TauDecayProduct(0.,0.,0.,0.);
-		
-		LorentzVector Neutrino(0.,0.,0.,0.);
-		
-		vector<HepMC::GenParticle*> TauDaught;
-		TauDaught = getGenStableDecayProducts(*z);
-		TauDecVtx = (*z)->end_vertex();
-
-		if ( TauDecVtx != 0 )
-		  {
-		    
-		    int numElectrons      = 0;
-		    int numMuons          = 0;
-		    int numChargedPions   = 0;
-		    int numNeutralPions   = 0;
-		    int numNeutrinos      = 0;
-		    int numOtherParticles = 0;
-		    
-		    for(vector<HepMC::GenParticle*>::const_iterator pit=TauDaught.begin() ; pit!=TauDaught.end() ; ++pit) 
-		      {	      
-			int pdg_id = abs((*pit)->pdg_id());
-			if (pdg_id == 11) numElectrons++;
-			else if (pdg_id == 13) numMuons++;
-			else if (pdg_id == 211) numChargedPions++;
-			else if (pdg_id == 111) numNeutralPions++;
-			else if (pdg_id == 12 || 
-				 pdg_id == 14 || 
-				 pdg_id == 16) {
-			  numNeutrinos++;
-			  if (pdg_id == 16) {
-			    Neutrino.SetPxPyPzE((*pit)->momentum().px(),(*pit)->momentum().py(),(*pit)->momentum().pz(),(*pit)->momentum().e());
-			  }
-			}
-			else if (pdg_id != 22) {
-			  numOtherParticles++;
-			}
-			
-			if (pdg_id != 12 &&
-			    pdg_id != 14 && 
-			    pdg_id != 16){
-			  TauDecayProduct.SetPxPyPzE((*pit)->momentum().px(),(*pit)->momentum().py(),(*pit)->momentum().pz(),(*pit)->momentum().e());
-			  Visible_Taus+=TauDecayProduct;
-			}	
-		      }
-		   		    
-		    int tauDecayMode = kOther;
-		    
-		    if ( numOtherParticles == 0 ){
-		      if ( numElectrons == 1 ){
-			//--- tau decays into electrons
-			tauDecayMode = kElectron;
-		      } else if ( numMuons == 1 ){
-			//--- tau decays into muons
-			tauDecayMode = kMuon;
-		      } else {
-			//--- hadronic tau decays
-			switch ( numChargedPions ){
-			case 1 : 
-			  switch ( numNeutralPions ){
-			  case 0 : 
-			    tauDecayMode = kOneProng0pi0;
-			    break;
-			  case 1 : 
-			    tauDecayMode = kOneProng1pi0;
-			    break;
-			  case 2 : 
-			    tauDecayMode = kOneProng2pi0;
-			    break;
-			  }
-			  break;
-			case 3 : 
-			  switch ( numNeutralPions ){
-			  case 0 : 
-			    tauDecayMode = kThreeProng0pi0;
-			    break;
-			  case 1 : 
-			    tauDecayMode = kThreeProng1pi0;
-			    break;
-			  }
-			  break;
-			}		    
-		      }
-		    }
-		    
-		  
-		    
-		    if(tauDecayMode == kElectron)
-		      {		          
-			if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCElectron_)){
-			  product_Electrons->push_back(Visible_Taus);
-			  product_Leptons->push_back(Visible_Taus);
-		    }
-		      }
-		    else if (tauDecayMode == kMuon)
-		      {
-			
-			if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCMuon_)){
-			  product_Muons->push_back(Visible_Taus);
-			  product_Leptons->push_back(Visible_Taus);
-			}
-		      }
-		    else if(tauDecayMode == kOneProng0pi0 || 
-			    tauDecayMode == kOneProng1pi0 || 
-			    tauDecayMode == kOneProng2pi0 ) 
-		      {
-			if ((abs(Visible_Taus.eta()) < etaMax) && (Visible_Taus.pt() > ptMinMCElectron_)){
-			  product_OneProng->push_back(Visible_Taus);
-			  product_OneAndThreeProng->push_back(Visible_Taus);
-			  product_Neutrina->push_back(Neutrino);
-			}
-		      }
-		    else if (tauDecayMode == kThreeProng0pi0 || 
-			     tauDecayMode == kThreeProng1pi0 )
-		      {
-			if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCTau_))  {
-			  product_ThreeProng->push_back(Visible_Taus);
-			  product_OneAndThreeProng->push_back(Visible_Taus);
-			  product_Neutrina->push_back(Neutrino);
-			}								
-		      }
-		    else if (tauDecayMode == kOther)
-		      {
-			if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCTau_))  {
-			  product_Other->push_back(Visible_Taus);
-			}
-		      }
+		if (abs((*TauIt).pdgId()) == 15 && ((*TauIt).status()==2)) //if it is a Tau and decayed
+		  {		    
+		    decayProducts = getGenStableDecayProducts((reco::GenParticle*) & (*TauIt));	 
+		    //  for (GenParticle::const_iterator TauIt2 = (*TauIt).begin(); TauIt2 != (*TauIt).end(); TauIt2++) {
+		    //		      cout<<" Real Tau particles: "<< (*TauIt2).pdgId() << " "<< (*TauIt2).status()<< " mother: "<< (*TauIt2).mother()->pdgId() << endl;
+		    // }
 		  }
 	      }
-	  }
+	      
+	      
+	      if ( !decayProducts.empty() )
+		{
+		  
+		  LorentzVector Visible_Taus(0.,0.,0.,0.);
+		  LorentzVector TauDecayProduct(0.,0.,0.,0.);
+		  LorentzVector Neutrino(0.,0.,0.,0.);
+		  
+		  int numElectrons      = 0;
+		  int numMuons          = 0;
+		  int numChargedPions   = 0;
+		  int numNeutralPions   = 0;
+		  int numNeutrinos      = 0;
+		  int numOtherParticles = 0;
+		  
+		  
+		  for (std::vector<GenParticle*>::iterator pit = decayProducts.begin(); pit != decayProducts.end(); pit++)
+		    {
+		      int pdg_id = abs((*pit)->pdgId());
+		      if (pdg_id == 11) numElectrons++;
+		      else if (pdg_id == 13) numMuons++;
+		      else if (pdg_id == 211) numChargedPions++;
+		      else if (pdg_id == 111) numNeutralPions++;
+		      else if (pdg_id == 12 || 
+			       pdg_id == 14 || 
+			       pdg_id == 16) {
+			numNeutrinos++;
+			if (pdg_id == 16) {
+			  Neutrino.SetPxPyPzE((*pit)->px(),(*pit)->py(),(*pit)->pz(),(*pit)->energy());
+			}
+		      }
+		      else if (pdg_id != 22) {
+			numOtherParticles++;
+		      }
+		      
+		      if (pdg_id != 12 &&
+			  pdg_id != 14 && 
+			  pdg_id != 16){
+			TauDecayProduct.SetPxPyPzE((*pit)->px(),(*pit)->py(),(*pit)->pz(),(*pit)->energy());
+			Visible_Taus+=TauDecayProduct;
+		      }	
+		      //		  cout<< "This has to be the same: " << (*pit)->pdgId() << " "<< (*pit)->status()<< " mother: "<< (*pit)->mother()->pdgId() << endl;
+		    }
+		  
+		  int tauDecayMode = kOther;
+		  
+		  if ( numOtherParticles == 0 ){
+		    if ( numElectrons == 1 ){
+		      //--- tau decays into electrons
+		      tauDecayMode = kElectron;
+		    } else if ( numMuons == 1 ){
+		      //--- tau decays into muons
+		      tauDecayMode = kMuon;
+		    } else {
+		      //--- hadronic tau decays
+		      switch ( numChargedPions ){
+		      case 1 : 
+			switch ( numNeutralPions ){
+			case 0 : 
+			  tauDecayMode = kOneProng0pi0;
+			  break;
+			case 1 : 
+			  tauDecayMode = kOneProng1pi0;
+			  break;
+			case 2 : 
+			  tauDecayMode = kOneProng2pi0;
+			  break;
+			}
+			break;
+		      case 3 : 
+			switch ( numNeutralPions ){
+			case 0 : 
+			  tauDecayMode = kThreeProng0pi0;
+			  break;
+			case 1 : 
+			  tauDecayMode = kThreeProng1pi0;
+			  break;
+			}
+			break;
+		      }		    
+		    }
+		  }
+		  
+		  
+		  //		  cout<< "So we have a: " << tauDecayMode <<endl;
+		  
+		  if(tauDecayMode == kElectron)
+		    {		          
+		      if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCElectron_)){
+			product_Electrons->push_back(Visible_Taus);
+			product_Leptons->push_back(Visible_Taus);
+		      }
+		    }
+		  else if (tauDecayMode == kMuon)
+		    {
+		      
+		      if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCMuon_)){
+			product_Muons->push_back(Visible_Taus);
+			product_Leptons->push_back(Visible_Taus);
+		      }
+		    }
+		  else if(tauDecayMode == kOneProng0pi0 || 
+			  tauDecayMode == kOneProng1pi0 || 
+			  tauDecayMode == kOneProng2pi0 ) 
+		    {
+		      if ((abs(Visible_Taus.eta()) < etaMax) && (Visible_Taus.pt() > ptMinMCElectron_)){
+			product_OneProng->push_back(Visible_Taus);
+			product_OneAndThreeProng->push_back(Visible_Taus);
+			product_Neutrina->push_back(Neutrino);
+		      }
+		    }
+		  else if (tauDecayMode == kThreeProng0pi0 || 
+			   tauDecayMode == kThreeProng1pi0 )
+		    {
+		      if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCTau_))  {
+			product_ThreeProng->push_back(Visible_Taus);
+			product_OneAndThreeProng->push_back(Visible_Taus);
+			product_Neutrina->push_back(Neutrino);
+		      }								
+		    }
+		  else if (tauDecayMode == kOther)
+		    {
+		      if((abs(Visible_Taus.eta())<etaMax)&&(Visible_Taus.pt()>ptMinMCTau_))  {
+			product_Other->push_back(Visible_Taus);
+		      }
+		    }	    				       
+		}
+	    }
+	}			       
+	//  
       }
   }
-  
-  delete myGenEvent;
-  
   iEvent.put(product_Leptons,"LeptonicTauLeptons");
   iEvent.put(product_Electrons,"LeptonicTauElectrons");
   iEvent.put(product_Muons,"LeptonicTauMuons");
@@ -216,37 +225,33 @@ void HLTTauMCProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   iEvent.put(product_OneAndThreeProng,"HadronicTauOneAndThreeProng");
   iEvent.put(product_Other, "TauOther");
   iEvent.put(product_Neutrina,"Neutrina"); 
-
+  
+  						       
 }
-
 // Helper Function
 
-std::vector<HepMC::GenParticle*> HLTTauMCProducer::getGenStableDecayProducts(const HepMC::GenParticle* particle)
+std::vector<reco::GenParticle*> HLTTauMCProducer::getGenStableDecayProducts(const reco::GenParticle* particle)
 {
-  HepMC::GenVertex* vertex = particle->end_vertex();
+  std::vector<GenParticle*> decayProducts;
+  decayProducts.clear();
 
-  std::vector<HepMC::GenParticle*> decayProducts;
-  for ( HepMC::GenVertex::particles_out_const_iterator daughter_particle = vertex->particles_out_const_begin(); 
-	daughter_particle != vertex->particles_out_const_end(); ++daughter_particle ){
-    int pdg_id = abs((*daughter_particle)->pdg_id());
+  //  std::cout << " Are we ever here?: "<< (*particle).numberOfDaughters() << std::endl;
+  for ( GenParticle::const_iterator daughter_particle = (*particle).begin();daughter_particle != (*particle).end(); ++daughter_particle ){   
 
-    // check if particle is stable
+    int pdg_id = abs((*daughter_particle).pdgId());
+
+//    // check if particle is stable
     if ( pdg_id == 11 || pdg_id == 12 || pdg_id == 13 || pdg_id == 14 || pdg_id == 16 ||  pdg_id == 111 || pdg_id == 211 ){
       // stable particle, identified by pdg code
-      decayProducts.push_back((*daughter_particle));
-    } else if ( (*daughter_particle)->end_vertex() != NULL ){
-      // unstable particle, identified by non-zero decay vertex
-
-      std::vector<HepMC::GenParticle*> addDecayProducts = getGenStableDecayProducts(*daughter_particle);
-
-      for ( std::vector<HepMC::GenParticle*>::const_iterator adddaughter_particle = addDecayProducts.begin(); adddaughter_particle != addDecayProducts.end(); ++adddaughter_particle ){
+      decayProducts.push_back((reco::GenParticle*) &(* daughter_particle));
+    } 
+    else {
+//      // unstable particle, identified by non-zero decay vertex
+      std::vector<GenParticle*> addDecayProducts = getGenStableDecayProducts((reco::GenParticle*) &(*daughter_particle));
+      for ( std::vector<GenParticle*>::const_iterator adddaughter_particle = addDecayProducts.begin(); adddaughter_particle != addDecayProducts.end(); ++adddaughter_particle ){
 	decayProducts.push_back((*adddaughter_particle));
       }
-    } else {
-      // stable particle, not identified by pdg code
-      decayProducts.push_back((*daughter_particle));
     }
   }
-   
   return decayProducts;
 }
