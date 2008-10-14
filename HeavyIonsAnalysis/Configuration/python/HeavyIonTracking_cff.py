e0132198bcb4b116929293a7a09edf6de1deafcd
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2008-10-14 16:43:23 +0000

    New Tracking configuration by Ed Wenger

---
+++ HeavyIonsAnalysis/Configuration/python/HeavyIonTracking_cff.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoPixelVertexing.PixelLowPtUtilities.common_cff import *
+from RecoPixelVertexing.PixelLowPtUtilities.firstStep_cff import *
+
+pixel3ProtoTracks.RegionFactoryPSet.RegionPSet.ptMin = 0.7
+pixel3ProtoTracks.RegionFactoryPSet.RegionPSet.originRadius = 0.1
+
+pixel3PrimTracks.RegionFactoryPSet.RegionPSet.ptMin = 1.5
+pixel3PrimTracks.RegionFactoryPSet.RegionPSet.originRadius = 0.2
+
+ckfBaseTrajectoryFilterForMinBias.filterPset.minimumNumberOfHits = 6
+ckfBaseTrajectoryFilterForMinBias.filterPset.minPt           = 2.0
+
+globalPrimTracks.Fitter = 'KFFittingSmoother'
+globalPrimTracks.useHitsSplitting = True
+
+firstStep = cms.Sequence(pixel3ProtoTracks
+						* pixelVertices
+						* pixel3PrimTracks
+						* primSeeds
+						* primTrackCandidates
+						* globalPrimTracks )
+heavyIonTracking = cms.Sequence(firstStep)
+
