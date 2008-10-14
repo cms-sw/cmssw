33e63dc56174fc3a621e72670053b618950a681e
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2008-10-14 17:31:02 +0000

    early 10TeV collisions in fake conditions

--- FastSimulation/Configuration/python/CommonInputsFake_cff.py
+++ FastSimulation/Configuration/python/CommonInputsFake_cff.py
@@ -14,7 +14,7 @@ from CalibMuon.Configuration.CSC_FakeDBConditions_cff import *
 
 # Services from the CondDB
 from CondCore.DBCommon.CondDBSetup_cfi import *
-from RecoVertex.BeamSpotProducer.BeamSpotFakeConditionsEarlyCollision_cff import *
+from RecoVertex.BeamSpotProducer.BeamSpotFakeConditionsEarly10TeVCollision_cff import *
 from RecoBTag.Configuration.RecoBTag_FakeConditions_cff import *
 from RecoBTau.Configuration.RecoBTau_FakeConditions_cff import *
 from CalibCalorimetry.Configuration.Hcal_FakeConditions_cff import *
