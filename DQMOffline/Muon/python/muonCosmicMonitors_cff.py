cf4d5c3b5599a3daeca653034621f0c14f9ba21c
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2008-10-14 06:22:55 +0000

    rpcSource Included

--- DQMOffline/Muon/python/muonCosmicMonitors_cff.py
+++ DQMOffline/Muon/python/muonCosmicMonitors_cff.py
@@ -19,8 +19,8 @@ from DQMOffline.Muon.muonQualityTests_cff import *
 muonTrackCosmicAnalyzers = cms.Sequence(standAloneCosmicMuonsMonitors*MonitorTrackTKCosmicMuons*MonitorTrackGLBCosmicMuons)
 muonTrackCosmicAnalyzersHLT = cms.Sequence(MonitorTrackSTACosmicMuonsHLTDT*MonitorTrackSTACosmicMuonsHLTCSC)
 
-### muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*rpcSource*cscMonitor*muonCosmicAnalyzer)
-muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*cscMonitor*muonCosmicAnalyzer)
+muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*rpcSource*cscMonitor*muonCosmicAnalyzer)
+##muonCosmicMonitors = cms.Sequence(muonTrackCosmicAnalyzers*dtSegmentsMonitor*cscMonitor*muonCosmicAnalyzer)
 
 muonCosmicMonitors_woCSC = cms.Sequence(cms.SequencePlaceholder("muonTrackAnalyzers")*dtSegmentsMonitor*rpcSource*muonCosmicMonitors)
 
--- DQMOffline/Muon/python/muonMonitors_cff.py
+++ DQMOffline/Muon/python/muonMonitors_cff.py
@@ -4,13 +4,16 @@ import FWCore.ParameterSet.Config as cms
 from DQM.TrackingMonitor.MonitorTrackSTAMuons_cfi import *
 from DQM.TrackingMonitor.MonitorTrackGLBMuons_cfi import *
 from DQMOffline.Muon.dtSegmTask_cfi import *
+from DQMOffline.Muon.rpcSource_cfi import *
+
 #dedicated analyzers for offline dqm 
 from DQMOffline.Muon.muonAnalyzer_cff import *
 from DQMOffline.Muon.CSCMonitor_cfi import *
 #dedicated clients for offline dqm 
 from DQMOffline.Muon.muonQualityTests_cff import *
+
 muonTrackAnalyzers = cms.Sequence(MonitorTrackSTAMuons*MonitorTrackGLBMuons)
-muonMonitors = cms.Sequence(muonTrackAnalyzers*dtSegmentsMonitor*muonAnalyzer)
+muonMonitors = cms.Sequence(rpcSource*muonTrackAnalyzers*dtSegmentsMonitor*muonAnalyzer)
 muonMonitorsAndQualityTests = cms.Sequence(muonMonitors*muonQualityTests)
 
 
