e5682f56d1613a77430f23a79ba206d021515c3f
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-14 09:18:09 +0000

    add softLeptonByPtBJetTags to default reco sequence

--- RecoBTag/Configuration/python/RecoBTag_cff.py
+++ RecoBTag/Configuration/python/RecoBTag_cff.py
@@ -3,7 +3,24 @@ import FWCore.ParameterSet.Config as cms
 # define the b-tag squences for offline reconstruction
 from RecoBTag.SoftLepton.softLepton_cff import *
 from RecoBTag.ImpactParameter.impactParameter_cff import *
-#include "RecoBTag/CombinedSV/data/combined.cff"
 from RecoBTag.SecondaryVertex.secondaryVertex_cff import *
-btagging = cms.Sequence(impactParameterTagInfos*jetBProbabilityBJetTags+jetProbabilityBJetTags+trackCountingHighPurBJetTags+trackCountingHighEffBJetTags+impactParameterMVABJetTags*secondaryVertexTagInfos*simpleSecondaryVertexBJetTags+combinedSecondaryVertexBJetTags+combinedSecondaryVertexMVABJetTags+btagSoftElectrons*softElectronTagInfos*softElectronBJetTags+softMuonTagInfos*softMuonBJetTags+softMuonNoIPBJetTags)
 
+btagging = cms.Sequence(
+    impactParameterTagInfos * 
+    jetBProbabilityBJetTags + 
+    jetProbabilityBJetTags + 
+    trackCountingHighPurBJetTags + 
+    trackCountingHighEffBJetTags + 
+    impactParameterMVABJetTags * 
+    secondaryVertexTagInfos * 
+    simpleSecondaryVertexBJetTags + 
+    combinedSecondaryVertexBJetTags + 
+    combinedSecondaryVertexMVABJetTags + 
+    btagSoftElectrons * 
+    softElectronTagInfos * 
+    softElectronBJetTags + 
+    softMuonTagInfos * 
+    softMuonBJetTags + 
+    softMuonNoIPBJetTags +
+    softLeptonByPtBJetTags
+)
