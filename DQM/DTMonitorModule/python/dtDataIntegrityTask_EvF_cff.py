384d3edd3d4d8522226d60bc1093f2ede9f35822
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-10-14 12:58:42 +0000

    Configuration of data integrity in the Ev. Filter (for integration)

---
+++ DQM/DTMonitorModule/python/dtDataIntegrityTask_EvF_cff.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+dtunpacker = cms.EDProducer("DTUnpackingModule",
+    dataType = cms.string('DDU'),
+    useStandardFEDid = cms.untracked.bool(True),
+    fedbyType = cms.untracked.bool(True),
+    readOutParameters = cms.PSet(
+        debug = cms.untracked.bool(False),
+        rosParameters = cms.PSet(
+            writeSC = cms.untracked.bool(True),
+            readingDDU = cms.untracked.bool(True),
+            performDataIntegrityMonitor = cms.untracked.bool(True),
+            readDDUIDfromDDU = cms.untracked.bool(True),
+            debug = cms.untracked.bool(False),
+            localDAQ = cms.untracked.bool(False)
+        ),
+        localDAQ = cms.untracked.bool(False),
+        performDataIntegrityMonitor = cms.untracked.bool(True)
+    )
+)
+
+from DQM.DTMonitorModule.dtDataIntegrityTask_cfi import *
+DTDataIntegrityTask.hltMode = True
+
+dtDQMEvF = cms.Sequence(dtunpacker)
+
+
