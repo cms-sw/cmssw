2071621e40866e5a1189261c509a539e545d8f1e
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-14 15:26:39 +0000

    python version of testLeptonAssociator.cfg

---
+++ RecoBTag/SoftLepton/test/testLeptonAssociator.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env cmsRun
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TestEfficiency")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("MagneticField.Engine.volumeBasedMagneticField_cfi")
+process.load("SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi")
+process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
+process.TrackAssociatorByHitsESProducer.MinHitFraction = 0.3
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_9/RelValBJets_Pt_50_120/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/STARTUP_V7_v2/0000/206C1E7D-0E86-DD11-9C2B-000423D98DB4.root')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.testAnalyzer = cms.EDFilter("testLeptonAssociator",
+    muons                = cms.InputTag("muons")
+    globalMuonTracks     = cms.InputTag("globalMuons"),
+    standAloneMuonTracks = cms.InputTag("standAloneMuons"),
+    tracks               = cms.InputTag("ctfWithMaterialTracks"),
+    trackingTruth        = cms.InputTag("trackingParticles"),
+    minPt                = cms.double(1.2),
+    leptonFlavour        = cms.uint32(13),
+)
+
+process.path = cms.Path(process.testAnalyzer)
