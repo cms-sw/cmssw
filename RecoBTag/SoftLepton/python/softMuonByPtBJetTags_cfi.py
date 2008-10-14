f565fd14d8a60bf5d191b4723f54d6cb6917a377
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-14 16:25:53 +0000

    fixed module name

--- RecoBTag/SoftLepton/python/softMuonByPtBJetTags_cfi.py
+++ RecoBTag/SoftLepton/python/softMuonByPtBJetTags_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-softLeptonByPtBJetTags = cms.EDProducer("JetTagProducer",
+softMuonByPtBJetTags = cms.EDProducer("JetTagProducer",
     jetTagComputer = cms.string('softLeptonByPt'),
     tagInfos = cms.VInputTag(cms.InputTag("softMuonTagInfos"))
 )
