e69eebe165811e551e081540afdd0f1912568782
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-10-14 16:25:49 +0000

    Live configuration added

---
+++ DQM/CSCMonitorModule/python/test/csc_hlt_dqm_sourceclient-live.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CSC HLT DQM")
+
+#-------------------------------------------------
+# DQM Module Configuration
+#-------------------------------------------------
+
+process.load("DQM.CSCMonitorModule.test.csc_hlt_dqm_sourceclient_cfi")
+
+#----------------------------
+# Event Source
+#-----------------------------
+
+process.load("DQM.Integration.test.inputsource_live_cfi")
+#process.EventStreamHttpReader.consumerName = 'CSC HLT DQM Consumer'
+#process.EventStreamHttpReader.sourceURL = "http://localhost:50082/urn:xdaq-application:lid=29"
+
+#----------------------------
+# DQM Environment
+#-----------------------------
+
+process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+#----------------------------
+# DQM Playback Environment
+#-----------------------------
+
+process.load("DQM.Integration.test.environment_playback_cfi")
+process.dqmEnv.subSystemFolder    = "CSC"
+
+process.DQM.collectorHost = 'pccmsdqm02.cern.ch'
+#process.DQM.collectorHost = 'localhost'
+process.dqmSaver.dirName = '.'
+
+#--------------------------
+# Sequences
+#--------------------------
+
+process.p = cms.Path(process.dqmClient+process.dqmEnv+process.dqmSaver)
+
+
