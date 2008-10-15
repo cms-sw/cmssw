9bfe0b522584b30edf14d8d0090052c0e70cfa46
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-10-15 16:36:30 +0000

    first release

---
+++ CondFormats/DataRecord/src/RPCObIDMapRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCObIDMapRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Wed Oct 15 11:25:34 CEST 2008
+// $Id$
+
+#include "CondFormats/DataRecord/interface/RPCObIDMapRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(RPCObIDMapRcd);
