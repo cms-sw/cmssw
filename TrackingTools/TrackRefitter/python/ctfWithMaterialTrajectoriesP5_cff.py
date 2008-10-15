8780be308f7be5e73c187c76b5889b45aca75251
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-10-15 18:34:34 +0000

    change the logic of the fit

--- TrackingTools/TrackRefitter/python/ctfWithMaterialTrajectoriesP5_cff.py
+++ TrackingTools/TrackRefitter/python/ctfWithMaterialTrajectoriesP5_cff.py
@@ -4,11 +4,11 @@ from TrackingTools.TrackRefitter.TracksToTrajectories_cff import *
 # IMPORTANT !!! ##
 # If you want to revert the fit direction, then
 # Case 1 #
-# string RefitDirection = "insideOut"
+# string RefitDirection = "alongMomentum"
 # KFTrajectoryFitterESProducer   ---> Fitter = "KFFitterForRefitInsideOut"
 # KFTrajectorySmootherESProducer ---> Smoother = "KFSmootherForRefitInsideOut"
 # Case 2 #
-# string RefitDirection = "outsideIn"
+# string RefitDirection = "oppositeToMomentum"
 # KFTrajectoryFitterESProducer   ---> Fitter = "KFFitterForRefitOutsideIn"
 # KFTrajectorySmootherESProducer ---> Smoother = "KFSmootherForRefitOutsideIn"
 # the propagator must be the same as the one used by the Fitter
@@ -21,7 +21,7 @@ ctfWithMaterialTracksP5 = cms.EDProducer("TracksToTrajectories",
                                                                      TrackerRecHitBuilder = cms.string('WithTrackAngle'),
                                                                      Smoother = cms.string('KFSmootherForRefitInsideOut'),
                                                                      MuonRecHitBuilder = cms.string('MuonRecHitBuilder'),
-                                                                     RefitDirection = cms.string('insideOut'),
+                                                                     RefitDirection = cms.string('alongMomentum'),
                                                                      RefitRPCHits = cms.bool(True),
                                                                      Propagator = cms.string('SmartPropagatorAnyRK')
                                                                      )
