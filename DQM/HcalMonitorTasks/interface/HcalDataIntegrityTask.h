151c172477fc26e44b27df1e7bf3d05d5c4bd2af
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2008-10-15 20:03:36 +0000

    added HcalDataIntegrityTask

---
+++ DQM/HcalMonitorTasks/interface/HcalDataIntegrityTask.h
@@ -0,0 +1,122 @@
+#ifndef DQM_HCALMONITORTASKS_HCALDATAINTEGRITYTASK_H
+#define DQM_HCALMONITORTASKS_HCALDATAINTEGRITYTASK_H
+
+#define  IETAMIN -43
+#define  IETAMAX 43
+#define  IPHIMIN 0
+#define  IPHIMAX 71
+#define  HBHE_LO_DCC 700
+#define  HBHE_HI_DCC 717
+#define  HF_LO_DCC   718
+#define  HF_HI_DCC   724
+#define  HO_LO_DCC   725
+#define  HO_HI_DCC   731
+
+#include "DQM/HcalMonitorTasks/interface/HcalBaseMonitor.h"
+#include "EventFilter/HcalRawToDigi/interface/HcalUnpacker.h"
+#include "EventFilter/HcalRawToDigi/interface/HcalHTRData.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+/** \class HcalDataIntegrityTask
+ *
+ * $Date: 2008/10/15 21:47:50 $
+ * $Revision: 1.1 $
+ * \author J. Temple -- University of Maryland
+ * copied from W. Fisher/J. St. John's DataFormat code
+ */
+
+class HcalDataIntegrityTask: public HcalBaseMonitor 
+{
+ public:
+  HcalDataIntegrityTask();
+  ~HcalDataIntegrityTask();
+  
+  void setup(const edm::ParameterSet& ps, DQMStore* dbe);
+  void processEvent(const FEDRawDataCollection& rawraw, const
+		    HcalUnpackerReport& report, const HcalElectronicsMap& emap);
+  void unpack(const FEDRawData& raw, const HcalElectronicsMap& emap);
+  void clearME();
+  void reset();
+
+ public: //Electronics map -> geographic channel map
+  std::map<uint32_t, std::vector<HcalDetId> > DCCtoCell;
+  std::map<uint32_t, std::vector<HcalDetId> > ::iterator thisDCC;
+  std::map<pair <int,int> , std::vector<HcalDetId> > HTRtoCell;
+  std::map<pair <int,int> , std::vector<HcalDetId> > ::iterator thisHTR;
+
+ private: 
+  //backstage accounting mechanisms for the ProblemMap
+  static size_t iphirange; // = IPHIMAX - IPHIMIN;
+  static size_t ietarange; // = IETAMAX - IETAMIN;
+  std::vector<std::vector<bool> > problemhere;  // Whole HCAL
+  std::vector<std::vector<bool> > problemHB;    //  
+  std::vector<std::vector<bool> > problemHE;    //  
+  std::vector<std::vector<bool> > problemHF;    // Includes ZDC?
+  std::vector<std::vector<bool> > problemHO;    //  
+  void mapHTRproblem (int dcc, int spigot) ;
+  void mapDCCproblem(int dcc) ;
+  void fillzoos(int bin, int dccid);
+  std::vector<std::vector<uint64_t> > phatmap;  // iphi/ieta projection of all hcal cells
+  std::vector<std::vector<uint64_t> > HBmap;    // iphi/ieta projection of hb
+  std::vector<std::vector<uint64_t> > HEmap;    // iphi/ieta projection of he
+  std::vector<std::vector<uint64_t> > HFmap;    // iphi/ieta projection of hf
+  std::vector<std::vector<uint64_t> > HOmap;    // iphi/ieta projection of ho
+  void UpdateMap();
+
+  // Data accessors
+  vector<int> fedUnpackList_;
+  vector<int> dccCrate_;
+  vector<HcalSubdetector> dccSubdet_;
+  int firstFED_;
+  int ievt_;
+  int lastEvtN_;
+  int lastBCN_;
+  //   int dccnum_;
+  //int cratenum_;
+
+  int prtlvl_;
+
+ private:  //Monitoring elements
+
+  MonitorElement* fedEntries_;
+  MonitorElement* fedFatal_;
+
+
+  //Member variables for reference values to be used in consistency checks.
+  std::map<int, short> CDFversionNumber_list;
+  std::map<int, short>::iterator CDFvers_it;
+  std::map<int, short> CDFEventType_list;
+  std::map<int, short>::iterator CDFEvT_it;
+  std::map<int, short> CDFReservedBits_list;
+  std::map<int, short>::iterator CDFReservedBits_it;
+  std::map<int, short> DCCEvtFormat_list;
+  std::map<int, short>::iterator DCCEvtFormat_it;
+  std::map<int, short> DCCRsvdBits_list;
+  std::map<int, short>::iterator DCCRsvdBits_it;
+  
+  //static member variables 
+  static float DIMbin[32];
+};
+
+// For crate numbers:
+float HcalDataIntegrityTask::DIMbin[]={ 4, 4.5, // FED 700, 701
+		       0, 0.5, // FED 702, 703
+		       1, 1.5, // FED 704, 705
+		       5, 5.5, // FED 706, 707
+		       11, 11.5, // FED 708, 709
+		       15, 15.5, // FED 710, 711
+		       17, 17.5, // FED 712, 713
+		       14, 14.5, // FED 714, 715
+		       10, 10.5, // FED 716, 717
+		       2, 2.5, // FED 718, 719
+		       9, 9.5, // FED 720, 721
+		       12, 12.5, // FED 722, 723
+		       3, 3.5, // FED 724, 725
+		       7, 7.5, // FED 726, 727
+		       6, 6.5, // FED 728, 729
+		       13, 13.5 // FED 730, 731
+};
+
+#endif
---
+++ DQM/HcalMonitorTasks/src/HcalDataIntegrityTask.cc
@@ -0,0 +1,213 @@
+#include "DQM/HcalMonitorTasks/interface/HcalDataIntegrityTask.h"
+#include "DataFormats/FEDRawData/interface/FEDNumbering.h"
+#include "DataFormats/FEDRawData/interface/FEDTrailer.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+HcalDataIntegrityTask::HcalDataIntegrityTask() 
+{
+  //Initialize phatmap to a vector of vectors of uint64_t 0
+  static size_t iphirange = IPHIMAX - IPHIMIN;
+  static size_t ietarange = IETAMAX - IETAMIN;
+ 
+  std::vector<uint64_t> phatv (iphirange + 1, 0);
+  // ... nothing goes at ieta=0, so an extra bin goes there.
+  phatmap = vector< vector < uint64_t> > ( ietarange + 1, phatv);
+  HBmap   = vector< vector < uint64_t> > ( ietarange + 1, phatv);
+  HEmap   = vector< vector < uint64_t> > ( ietarange + 1, phatv);
+  HFmap   = vector< vector < uint64_t> > ( ietarange + 1, phatv);
+  HOmap   = vector< vector < uint64_t> > ( ietarange + 1, phatv);
+  std::vector<bool> probvect (iphirange + 1, 0);
+  // ... nothing goes at ieta=0, so an extra bin goes there.
+  problemhere = vector< vector <bool> > ( ietarange + 1, probvect);
+  problemHB   = vector< vector <bool> > ( ietarange + 1, probvect);
+  problemHE   = vector< vector <bool> > ( ietarange + 1, probvect);
+  problemHF   = vector< vector <bool> > ( ietarange + 1, probvect);
+  problemHO   = vector< vector <bool> > ( ietarange + 1, probvect);
+
+} // HcalDataIntegrityTask::HcalDataIntegrityTask()
+
+HcalDataIntegrityTask::~HcalDataIntegrityTask() {}
+
+void HcalDataIntegrityTask::reset(){}
+
+void HcalDataIntegrityTask::clearME()
+{
+  if(m_dbe){
+    m_dbe->setCurrentFolder(baseFolder_);
+    m_dbe->removeContents();
+  }
+}
+
+
+void HcalDataIntegrityTask::setup(const edm::ParameterSet& ps,
+				  DQMStore* dbe)
+{
+  HcalBaseMonitor::setup(ps,dbe);
+  
+  ievt_=0;
+  baseFolder_ = rootFolder_+"HcalDataIntegrityTask";
+
+  if(fVerbosity) 
+    cout << "About to pushback fedUnpackList_" << endl;
+  
+  firstFED_ = FEDNumbering::getHcalFEDIds().first;
+  for (int i=FEDNumbering::getHcalFEDIds().first; 
+       i<=FEDNumbering::getHcalFEDIds().second;
+       ++i) 
+    {
+      if(fVerbosity) cout << "[DFMon:]Pushback for fedUnpackList_: " << i <<endl;
+      fedUnpackList_.push_back(i);
+    }
+
+  prtlvl_ = ps.getUntrackedParameter<int>("dfPrtLvl");
+
+  if ( m_dbe ) 
+    {
+      char* type;
+      
+      cout <<"SET TO HCAL/FEDIntegrity"<<endl;
+      m_dbe->setCurrentFolder("Hcal/FEDIntegrity/");
+      type="FEDEntries";
+      fedEntries_ = m_dbe->book1D(type,"# entries per HCAL FED",32,699.5,731.5);
+      type="FEDFatal";
+      fedFatal_ = m_dbe->book1D(type,"# fatal errors HCAL FED",32,699.5,731.5);
+    } // if (m_dbe)
+
+
+  return;
+}
+
+void HcalDataIntegrityTask::processEvent(const FEDRawDataCollection& rawraw, 
+					 const HcalUnpackerReport& report, 
+					 const HcalElectronicsMap& emap){
+  
+  if(!m_dbe) 
+    { 
+      cout<<"HcalDataIntegrityTask::processEvent DQMStore not instantiated!!!"<<endl;  
+      return;
+    }
+  
+  cout <<"DIT PROCESS"<<endl;
+  ievt_++;
+
+  // Loop over all FEDs reporting the event, unpacking if good.
+  for (vector<int>::const_iterator i=fedUnpackList_.begin();i!=fedUnpackList_.end(); i++) 
+    {
+      const FEDRawData& fed = rawraw.FEDData(*i);
+      if (fed.size()<12) continue; // Was 16. How do such tiny events even get here?
+      unpack(fed,emap);
+    }
+
+  return;
+} //void HcalDataIntegrityTask::processEvent()
+
+
+// Process one FED's worth (one DCC's worth) of the event data.
+void HcalDataIntegrityTask::unpack(const FEDRawData& raw, 
+				   const HcalElectronicsMap& emap){
+  // get the DCC header
+  cout <<"DIT UNPACK"<<endl;
+  const HcalDCCHeader* dccHeader=(const HcalDCCHeader*)(raw.data());
+  if(!dccHeader) return;
+
+  // get the DCC trailer 
+  unsigned char* trailer_ptr = (unsigned char*) (raw.data()+raw.size()-sizeof(uint64_t));
+  FEDTrailer trailer = FEDTrailer(trailer_ptr);
+
+  int dccid=dccHeader->getSourceId();
+
+  ////////// Histogram problems with the Common Data Format compliance;////////////
+  bool CDFProbThisDCC = false; 
+  /* 1 */ //There should always be a second CDF header word indicated.
+  if (!dccHeader->thereIsASecondCDFHeaderWord()) 
+    {
+      CDFProbThisDCC = true; 
+    }
+
+  /* 2 */ //Make sure a reference CDF Version value has been recorded for this dccid
+  CDFvers_it = CDFversionNumber_list.find(dccid);
+  if (CDFvers_it  == CDFversionNumber_list.end()) 
+    {
+      CDFversionNumber_list.insert(pair<int,short>
+				   (dccid,dccHeader->getCDFversionNumber() ) );
+      CDFvers_it = CDFversionNumber_list.find(dccid);
+    } // then check against it.
+
+  if (dccHeader->getCDFversionNumber()!= CDFvers_it->second) 
+    {
+      CDFProbThisDCC = true; 
+    }
+  
+  /* 3 */ //Make sure a reference CDF EventType value has been recorded for this dccid
+  CDFEvT_it = CDFEventType_list.find(dccid);
+  if (CDFEvT_it  == CDFEventType_list.end()) 
+    {
+      CDFEventType_list.insert(pair<int,short>
+			       (dccid,dccHeader->getCDFEventType() ) );
+      CDFEvT_it = CDFEventType_list.find(dccid);
+    } // then check against it.
+  
+  if (dccHeader->getCDFEventType()!= CDFEvT_it->second) 
+    {
+      CDFProbThisDCC = true; 
+    }
+
+  /* 4 */ //There should always be a '5' in CDF Header word 0, bits [63:60]
+  if (dccHeader->BOEshouldBe5Always()!=5) 
+    {
+      CDFProbThisDCC = true; 
+    }
+
+  /* 5 */ //There should never be a third CDF Header word indicated.
+  if (dccHeader->thereIsAThirdCDFHeaderWord()) 
+    {
+      CDFProbThisDCC = true; 
+    }
+
+  /* 6 */ //Make sure a reference value of Reserved Bits has been recorded for this dccid
+
+  CDFReservedBits_it = CDFReservedBits_list.find(dccid);
+  if (CDFReservedBits_it  == CDFReservedBits_list.end()) {
+    CDFReservedBits_list.insert(pair<int,short>
+				(dccid,dccHeader->getSlink64ReservedBits() ) );
+    CDFReservedBits_it = CDFReservedBits_list.find(dccid);
+  } // then check against it.
+  
+  if ((int) dccHeader->getSlink64ReservedBits()!= CDFReservedBits_it->second) 
+    {
+      CDFProbThisDCC = true; 
+    }
+
+  /* 7 */ //There should always be 0x0 in CDF Header word 1, bits [63:60]
+  if (dccHeader->BOEshouldBeZeroAlways() !=0) 
+    {
+      CDFProbThisDCC = true; 
+    }
+  
+  /* 8 */ //There should only be one trailer
+  if (trailer.moreTrailers()) 
+    {
+      CDFProbThisDCC = true; 
+    }
+  //  if trailer.
+
+  /* 9 */ //CDF Trailer [55:30] should be the # 64-bit words in the EvFragment
+  if ((uint64_t) raw.size() != ( (uint64_t) trailer.lenght()*sizeof(uint64_t)) )  //The function name is a typo! Awesome.
+    {
+      CDFProbThisDCC = true; 
+    }
+  /*10 */ //There is a rudimentary sanity check built into the FEDTrailer class
+  if (!trailer.check()) 
+    {
+      CDFProbThisDCC = true; 
+    }
+  cout <<"DIT FILL"<<endl;
+  if (CDFProbThisDCC)
+    fedFatal_->Fill(dccid);
+  fedEntries_->Fill(dccid);
+
+  return;
+} // void HcalDataIntegrityTask::unpack(
+
+
