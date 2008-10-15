d0b267f7601612292acbb482ed0dab9f5e7b0881
Jason St John <sha1-13e6ca3f617e2679b2e43c5867c3ac26779e685c@cern.ch>
2008-10-15 10:13:28 +0000

    EventInfo folder using EDFilter DQMEventInfo.  Seems sensible.

--- DQM/HcalMonitorModule/python/hcal_dqm_source_fileT0_cff.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_source_fileT0_cff.py
@@ -1,7 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
+#from DQMServices.Components.DQMEnvironment_cfi import *
+
+dqmInfoHcal = cms.EDFilter("DQMEventInfo",
+    subSystemFolder = cms.untracked.string('Hcal')
+)
 from DQM.HcalMonitorModule.HcalMonitorModule_cfi import *
-hcalOfflineDQMSource = cms.Sequence(hcalMonitor)
+hcalOfflineDQMSource = cms.Sequence(hcalMonitor + dqmInfoHcal)
 hcalMonitor.DigiOccThresh          = -999999999
 hcalMonitor.PedestalsPerChannel    = False
 hcalMonitor.PedestalsInFC          = True
