b1bd31550db7f238c2865cdfb3face63dadc81c2
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-10-16 09:19:16 +0000

    Update global tag for craft (contains l1 conditions)

--- DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
+++ DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
@@ -9,10 +9,10 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
 #off-line
-#GlobalTag.globaltag = 'CRUZET4_V5P::All'
+#GlobalTag.globaltag = 'CRAFT_V2P::All'
 #GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
 #on-line
-GlobalTag.globaltag = 'CRUZET4_V5H::All'
+GlobalTag.globaltag = 'CRAFT_V2H::All'
 GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
 
 #add'n
