0233aac89f3c33ca95063ae4523193db13f2a874
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2008-10-16 12:54:09 +0000

    moved sequence for discriminator producers to RecoTauTag/Configuration/python/RecoPFTauTag_cfi.py

--- RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
+++ RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
@@ -1,40 +1,28 @@
 import FWCore.ParameterSet.Config as cms
 import copy
 
-#
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from RecoJets.JetAssociationProducers.ic5PFJetTracksAssociatorAtVertex_cfi import *
 #PFTauTagInfo Producer
 from RecoTauTag.RecoTau.PFRecoTauTagInfoProducer_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauProducer_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByIsolation_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackFinding_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackPtCut_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationAgainstElectron_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationAgainstMuon_cfi import *
 
 from RecoTauTag.RecoTau.PFRecoTauHighEfficiency_cff import *
+
+from RecoTauTag.Configuration.RecoPFTauTag_cfi import *
 
 
 PFTau = cms.Sequence(
     ic5PFJetTracksAssociatorAtVertex*
     pfRecoTauTagInfoProducer*
     pfRecoTauProducer*
-    pfRecoTauDiscriminationByIsolation*
-    pfRecoTauDiscriminationByLeadingTrackFinding*
-    pfRecoTauDiscriminationByLeadingTrackPtCut*
-    pfRecoTauDiscriminationAgainstElectron*
-    pfRecoTauDiscriminationAgainstMuon
-    )
+    PFTauDiscrimination
+)
 
 
 PFTauHighEfficiency = cms.Sequence(
     ic5PFJetTracksAssociatorAtVertex*
     pfRecoTauTagInfoProducer*
     pfRecoTauProducerHighEfficiency*
-    pfRecoTauDiscriminationByIsolationHighEfficiency*
-    pfRecoTauDiscriminationByLeadingTrackFindingHighEfficiency*
-    pfRecoTauDiscriminationByLeadingTrackPtCutHighEfficiency*
-    pfRecoTauDiscriminationAgainstElectronHighEfficiency*
-    pfRecoTauDiscriminationAgainstMuonHighEfficiency
-    )
+    PFTauDiscriminationHighEfficiency
+)
