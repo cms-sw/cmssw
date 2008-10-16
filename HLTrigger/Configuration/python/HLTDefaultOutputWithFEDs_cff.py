9c10f09d27574e73338eebaa2a0ff41eaf9763be
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-10-16 07:02:05 +0000

    Update to V9 (new globaltag STARTUP V7)

--- HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_2_1_10/HLT/V8 (CMSSW_2_1_10)
+# /dev/CMSSW_2_1_10/HLT/V9 (CMSSW_2_1_10)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_1_10/HLT/V8')
+  tableName = cms.string('/dev/CMSSW_2_1_10/HLT/V9')
 )
 
 block_hltDefaultOutputWithFEDs = cms.PSet(
