7ff9d941c34af3f5c89dd29e279e27cf8c085d7e
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2008-10-16 13:18:57 +0000

    added filling of tau id. discriminators

--- PhysicsTools/PatAlgos/python/producersLayer1/tauProducer_cfi.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/tauProducer_cfi.py
@@ -37,6 +37,21 @@ allLayer1Taus = cms.EDProducer("PATTauProducer",
     isolation = cms.PSet(),
     isoDeposits = cms.PSet(),
 
+    # tau ID configurables
+    # (for efficiency studies)
+    addTauID = cms.bool(True),
+    tauIDSources = cms.PSet(
+        # configure many IDs as InputTag <someName> = <someTag>
+        # you can comment out those you don't want to save some disk space
+        leadingTrackFinding = cms.InputTag("patPFRecoTauDiscriminationByLeadingTrackFinding"),
+        leadingTrackPtCut = cms.InputTag("patPFRecoTauDiscriminationByLeadingTrackPtCut"),
+        trackIsolation = cms.InputTag("patPFRecoTauDiscriminationByTrackIsolation"),
+        ecalIsolation = cms.InputTag("patPFRecoTauDiscriminationByECALIsolation"),
+        byIsolation = cms.InputTag("patPFRecoTauDiscriminationByIsolation"),
+        againstElectron = cms.InputTag("patPFRecoTauDiscriminationAgainstElectron"),
+        againstMuon = cms.InputTag("patPFRecoTauDiscriminationAgainstMuon")
+    ),
+
     # Trigger matching configurables
     addTrigMatch = cms.bool(True),
     trigPrimMatch = cms.VInputTag(cms.InputTag("tauTrigMatchHLT1Tau")),
@@ -52,8 +67,6 @@ allLayer1Taus = cms.EDProducer("PATTauProducer",
     embedGenJetMatch = cms.bool(False),
     
     genJetMatch = cms.InputTag("tauGenJetMatch"), ## particles source to be used for the matching
-
-                               
 
     # Efficiencies
     addEfficiencies = cms.bool(False),
