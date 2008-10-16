650c5f7a2b970f67c882ed3bd10d7e3fb51b2888
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-10-16 16:46:37 +0000

    updated for craft

--- DQM/HLTEvF/python/HLTMonJetMET_cfi.py
+++ DQM/HLTEvF/python/HLTMonJetMET_cfi.py
@@ -1,19 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
 hltMonJetMET = cms.EDAnalyzer("HLTMon",
-    outputFile = cms.untracked.string('./L1TDQM.root'),
-    verbose = cms.untracked.bool(False),
-    MonitorDaemon = cms.untracked.bool(True),
-    reqNum = cms.uint32(1),
-    DaqMonitorBEInterface = cms.untracked.bool(True),
-    filters = cms.VPSet(
-        cms.PSet(
-            PlotBounds = cms.vdouble(0.0, 0.0),
-            HLTCollectionLabels = cms.InputTag("hlt1jet50","","FU"),
-            IsoCollections = cms.VInputTag(cms.InputTag("none")),
-            theHLTOutputTypes = cms.uint32(95)
-      ),
-    disableROOToutput = cms.untracked.bool(True)
-    )
+   outputFile = cms.untracked.string('./L1TDQM.root'),
+   verbose = cms.untracked.bool(False),
+   MonitorDaemon = cms.untracked.bool(True),
+   reqNum = cms.uint32(1),
+   DaqMonitorBEInterface = cms.untracked.bool(True),
+   filters = cms.VPSet(
+       cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sL1Jet15","","FU"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(84),
+     ),
+       cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet30","","FU"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(84),
+     ),
+       cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hlt1jet30","","FU"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(95),
+     ),
+   disableROOToutput = cms.untracked.bool(True)
+   )
 
 )
