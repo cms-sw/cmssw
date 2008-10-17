fcb6c2ab7d80f475964701c83efeec288cf38aaf
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-10-17 13:23:48 +0000

    CSCTF commented out

--- DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
@@ -5,7 +5,8 @@ from DQM.L1TMonitorClient.L1TEventInfoClient_cff import *
 from DQM.L1TMonitorClient.L1TGMTClient_cff import *
 from DQM.L1TMonitorClient.L1TGCTClient_cff import *
 from DQM.L1TMonitorClient.L1TDTTFClient_cff import *
-from DQM.L1TMonitorClient.L1TCSCTFClient_cff import *
+ #from DQM.L1TMonitorClient.L1TCSCTFClient_cff import * 
+ # out because looking for lots of ME which cannot be found!
 from DQM.L1TMonitorClient.L1TDEMONClient_cff import *
 from DQM.L1TMonitorClient.L1TRPCTFClient_cff import *
 #    # use include file for dqmEnv dqmSaver
@@ -14,5 +15,7 @@ from DQMServices.Components.DQMEnvironment_cfi import *
 #
 # END ################################################
 #
-l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tcsctfseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
+
+#l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tcsctfseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
+l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
 
