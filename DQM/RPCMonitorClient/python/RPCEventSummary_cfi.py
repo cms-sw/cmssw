209d1d3db67cab64f788127fae2f57d3016beab3
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-10-17 08:57:18 +0000

    bug fix

--- DQM/RPCMonitorClient/python/RPCEventSummary_cfi.py
+++ DQM/RPCMonitorClient/python/RPCEventSummary_cfi.py
@@ -4,5 +4,5 @@ rpcEventSummary = cms.EDAnalyzer("RPCEventSummary",
    EventInfoPath = cms.untracked.string('RPC/EventInfo'),
    RPCPrefixDir =  cms.untracked.string('RPC/RecHits'),
    PrescaleFactor = cms.untracked.int32(10),
-   MinimunHitsPerRoll = cms.untracked.int32(2000)
+   MinimunHitsPerRoll = cms.untracked.uint32(2000) 
 )
