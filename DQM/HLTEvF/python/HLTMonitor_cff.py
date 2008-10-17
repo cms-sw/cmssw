95e99d8363ce5e0a03b8d9c7797196dc5d7090ae
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-10-17 13:05:54 +0000

    new muonDQM cff

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.HLTEvF.HLTMonElectron_cff import *
-from DQM.HLTEvF.HLTMonMuon_cff import *
+#from DQM.HLTEvF.HLTMonMuon_cff import *
 from DQM.HLTEvF.HLTMonTau_cff import *
 from DQM.HLTEvF.FourVectorHLT_cff import *
 #from DQM.HLTEvF.HLTEventInfoClient_cff import *
 from DQM.HLTEvF.HLTMonJetMET_cff import *
+from DQM.HLTEvF.HLTMonMuonDQM_cff import *
