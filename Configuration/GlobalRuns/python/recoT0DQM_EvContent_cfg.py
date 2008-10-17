864dc46ee2272c0d2ce590a48cb88700fc2b5ed2
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-10-17 12:37:55 +0000

    remove simple mem chack

--- Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py
+++ Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py
@@ -6,10 +6,6 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-process.SimpleMemoryCheck=cms.Service("SimpleMemoryCheck",
-                                     ignoreTotal=cms.untracked.int32(1),
-                                     oncePerEventMode=cms.untracked.bool(False))
-
 process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -51,7 +47,7 @@ process.FEVT.outputCommands.append('keep recoCandidatesOwned_caloTowersOpt_*_*')
 process.FEVT.outputCommands.append('keep RPCDetIdRPCDigiMuonDigiCollection_muonRPCDigis_*_*')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.26 $'),
+    version = cms.untracked.string('$Revision: 1.27 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_cfg.py,v $'),
     annotation = cms.untracked.string('CRUZET Prompt Reco with DQM with Mag field at 0T')
 )
