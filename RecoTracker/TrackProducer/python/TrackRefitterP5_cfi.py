f051f9eecbfab79a05d062b70a5bcd8947630d3d
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2008-10-18 02:05:07 +0000

    module and file name are consistent. Also for BHM and cosmic refitters

---
+++ RecoTracker/TrackProducer/python/TrackRefitterP5_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+import RecoTracker.TrackProducer.TrackRefitter_cfi 
+TrackRefitterP5  = RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone(
+    src = cms.InputTag("ctfWithMaterialTracksP5"),
+    Fitter = cms.string('FittingSmootherRKP5')
+)
+
+
