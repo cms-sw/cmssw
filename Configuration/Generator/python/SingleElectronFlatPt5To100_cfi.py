ad1692e139b1d655bf825f4ab352cc037d787104
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2008-10-20 20:10:59 +0000

    add new applications per egamma group request

---
+++ Configuration/Generator/python/SingleElectronFlatPt5To100_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("FlatRandomPtGunSource",
+    PGunParameters = cms.untracked.PSet(
+        MaxPt = cms.untracked.double(5.),
+        MinPt = cms.untracked.double(100.),
+        PartID = cms.untracked.vint32(11),
+        MaxEta = cms.untracked.double(2.5),
+        MaxPhi = cms.untracked.double(3.14159265359),
+        MinEta = cms.untracked.double(-2.5),
+        MinPhi = cms.untracked.double(-3.14159265359) ## in radians
+
+    ),
+    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+
+    psethack = cms.string('single electron pt 35'),
+    AddAntiParticle = cms.untracked.bool(True),
+    firstRun = cms.untracked.uint32(1)
+)
+
+
---
+++ Configuration/Generator/python/SingleGammaFlatPt10To100_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("FlatRandomPtGunSource",
+    PGunParameters = cms.untracked.PSet(
+        MaxPt = cms.untracked.double(100.),
+        MinPt = cms.untracked.double(10.),
+        PartID = cms.untracked.vint32(22),
+        MaxEta = cms.untracked.double(2.5),
+        MaxPhi = cms.untracked.double(3.14159265359),
+        MinEta = cms.untracked.double(-2.5),
+        MinPhi = cms.untracked.double(-3.14159265359) ## in radians
+
+    ),
+    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+
+    psethack = cms.string('single gamma pt 10 to 100'),
+    firstRun = cms.untracked.uint32(1)
+)
+
+
