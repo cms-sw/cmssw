5d794af2497f8d42583c328ceeab350d61f2b4c7
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2008-10-20 18:10:10 +0000

    changed tracking from Tracking_PbPb_cff to HeavyIonTracking_cff

--- HeavyIonsAnalysis/Configuration/python/Reconstruction_HI_cff.py
+++ HeavyIonsAnalysis/Configuration/python/Reconstruction_HI_cff.py
@@ -1,10 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-# from HeavyIonsAnalysis.Configuration.HighPtTracking_PbPb_cff import *
-# from HeavyIonsAnalysis.Configuration.LowPtTracking_PbPb_cff import *
-
-from HeavyIonsAnalysis.Configuration.Tracking_PbPb_cff import *
-
 from RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalPreshowerRecHit_cfi import *
@@ -14,6 +9,7 @@ from RecoEcal.EgammaClusterProducers.preshowerClusteringSequence_cff import *
 from RecoLocalCalo.Configuration.hcalLocalReco_cff import *
 from RecoLocalMuon.Configuration.RecoLocalMuon_cff import *
 from RecoMuon.Configuration.RecoMuon_cff import *
+from HeavyIonsAnalysis.Configuration.HeavyIonTracking_cff import *
 from HeavyIonsAnalysis.Configuration.IterativeConePu5Jets_PbPb_cff import *
 from RecoHI.HiCentralityAlgos.HiCentrality_cfi import *
 from RecoHI.HiEvtPlaneAlgos.HiEvtPlane_cfi import *
