a1e6ee90935dc5ea464346ef2357563a2fe286df
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2008-10-20 22:56:08 +0000

    change eta-range according to DataCert specs

--- Configuration/Generator/python/SinglePi0E10_cfi.py
+++ Configuration/Generator/python/SinglePi0E10_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 source = cms.Source("FlatRandomEGunSource",
     PGunParameters = cms.untracked.PSet(
         PartID = cms.untracked.vint32(111),
-        MaxEta = cms.untracked.double(2.5),
+        MaxEta = cms.untracked.double(3.0),
         MaxPhi = cms.untracked.double(3.14159265359),
-        MinEta = cms.untracked.double(-2.5),
+        MinEta = cms.untracked.double(-3.0),
         MinE = cms.untracked.double(9.99),
         MinPhi = cms.untracked.double(-3.14159265359), ## in radians
 
