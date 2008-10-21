7ce9f007414dd6600fdad42e2e9524137c00d6fd
bechtel <sha1-ed5984511fca06e3b47a5f5254511ca13942a612@cern.ch>
2008-10-21 08:40:28 +0000

    CSA08 pthat bins

--- QCDAnalysis/UEAnalysis/python/UEJetValidationCSA08_cfi.py
+++ QCDAnalysis/UEAnalysis/python/UEJetValidationCSA08_cfi.py
@@ -28,14 +28,47 @@ CSA08_0_pT_30_threshold900 = cms.EDProducer("UEJetValidation",
                                             eventScaleMin           = cms.double(  0. ), 
                                             eventScaleMax           = cms.double( 30. )
                                             )
-CSA08_0_pT_30_threshold900_Corrected = cms.EDProducer("UEJetValidation",
-                                                      UEJetValidationParameters,
-                                                      eventScaleMin           = cms.double(  0. ), 
-                                                      eventScaleMax           = cms.double( 30. ) 
-                                                      )
-CSA08_0_pT_30_threshold900_Corrected.CaloJetCollectionName = 'L2L3CorJetScone5'
+CSA08_30_pT_45_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double( 30. ), 
+                                             eventScaleMax           = cms.double( 45. )
+                                             )
+CSA08_45_pT_75_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double( 45. ), 
+                                             eventScaleMax           = cms.double( 75. )
+                                             )
+CSA08_75_pT_120_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double(  75. ), 
+                                             eventScaleMax           = cms.double( 120. )
+                                             )
+CSA08_120_pT_160_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double( 120. ), 
+                                             eventScaleMax           = cms.double( 160. )
+                                             )
+CSA08_160_pT_220_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double( 160. ), 
+                                             eventScaleMax           = cms.double( 220. )
+                                             )
+CSA08_220_pT_threshold900 = cms.EDProducer("UEJetValidation",
+                                             UEJetValidationParameters,
+                                             eventScaleMin           = cms.double( 220.  ), 
+                                             eventScaleMax           = cms.double( 7000. )
+                                             )
 
-UEJetValidation = cms.Sequence(CSA08_0_pT_30_threshold900*CSA08_0_pT_30_threshold900_Corrected)
+
+#CSA08_0_pT_30_threshold900_Corrected = cms.EDProducer("UEJetValidation",
+#                                                      UEJetValidationParameters,
+#                                                      eventScaleMin           = cms.double(  0. ), 
+#                                                      eventScaleMax           = cms.double( 30. ) 
+#                                                      )
+#CSA08_0_pT_30_threshold900_Corrected.CaloJetCollectionName = 'L2L3CorJetScone5'
+
+#UEJetValidation = cms.Sequence(CSA08_0_pT_30_threshold900*CSA08_0_pT_30_threshold900_Corrected)
+UEJetValidation = cms.Sequence(CSA08_0_pT_30_threshold900*CSA08_30_pT_45_threshold900*CSA08_45_pT_75_threshold900*CSA08_75_pT_120_threshold900*CSA08_120_pT_160_threshold900*CSA08_160_pT_220_threshold900*CSA08_220_pT_threshold900)
 
 
 
