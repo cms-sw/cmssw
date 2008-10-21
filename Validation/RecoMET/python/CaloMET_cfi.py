902400134bd3dc2eef3ee23101da559d9c96ec77
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-21 14:53:40 +0000

    Added Modules to analyze CaloMET with/without HO.

--- Validation/RecoMET/python/CaloMET_cfi.py
+++ Validation/RecoMET/python/CaloMET_cfi.py
@@ -1,32 +1,65 @@
 import FWCore.ParameterSet.Config as cms
 
 # File: CaloMET.cfi
-# Author: B. Scurlock
+# Author: B. Scurlock & R. Remington
 # Date: 03.04.2008
 #
 # Fill validation histograms for MET.
-metAnalyzer = cms.EDFilter("METTester",
+metAnalyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_met.root'),
     InputMETLabel = cms.InputTag("met"),
     METType = cms.untracked.string('CaloMET')
-)
+    )
 
-metNoHFAnalyzer = cms.EDFilter("METTester",
+metHOAnalyzer = cms.EDAnalyzer(
+    "METTester",
+    OutputFile = cms.untracked.string('METTester_metHO.root'),
+    InputMETLabel = cms.InputTag("metHO"),
+    METType = cms.untracked.string('CaloMET')
+    )
+
+metNoHFAnalyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_metNoHF.root'),
     InputMETLabel = cms.InputTag("metNoHF"),
     METType = cms.untracked.string('CaloMET')
-)
+    )
 
-metOptAnalyzer = cms.EDFilter("METTester",
+metNoHFHOAnalyzer = cms.EDAnalyzer(
+    "METTester",
+    OutputFile = cms.untracked.string('METTester_metNoHFHO.root'),
+    InputMETLabel = cms.InputTag("metNoHFHO"),
+    METType = cms.untracked.string('CaloMET')
+    )
+
+metOptAnalyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_metOpt.root'),
     InputMETLabel = cms.InputTag("metOpt"),
     METType = cms.untracked.string('CaloMET')
-)
+    )
 
-metOptNoHFAnalyzer = cms.EDFilter("METTester",
+metOptHOAnalyzer = cms.EDAnalyzer(
+    "METTester",
+    OutputFile = cms.untracked.string('METTester_metOptHO.root'),
+    InputMETLabel = cms.InputTag("metOptHO"),
+    METType = cms.untracked.string('CaloMET')
+    )
+
+metOptNoHFAnalyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_metOptNoHF.root'),
     InputMETLabel = cms.InputTag("metOptNoHF"),
     METType = cms.untracked.string('CaloMET')
-)
+    )
+
+metOptNoHFHOAnalyzer = cms.EDAnalyzer(
+    "METTester",
+    OutputFile = cms.untracked.string('METTester_metOptNoHFHO.root'),
+    InputMETLabel = cms.InputTag("metOptNoHFHO"),
+    METType = cms.untracked.string('CaloMET')
+    )
+
 
 
