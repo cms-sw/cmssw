f5797fdbfc019196a11d5074776a4d664a00da32
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-21 14:52:09 +0000

    Added sequences to analyze caloMET with/without HO.

--- Validation/RecoMET/python/CaloMET_cff.py
+++ Validation/RecoMET/python/CaloMET_cff.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 # File: CaloMET.cfi
-# Author: B. Scurlock
+# Author: B. Scurlock & R. Remington
 # Date: 03.04.2008
 #
 # Fill validation histograms for MET. Assumes met, metNoHF, metOpt, metOptNoHF are in the event
 from Validation.RecoMET.CaloMET_cfi import *
-analyzeCaloMET = cms.Sequence(metAnalyzer*metNoHFAnalyzer*metOptAnalyzer*metOptNoHFAnalyzer)
-
+analyzeCaloMETNoHO = cms.Sequence(metAnalyzer*metNoHFAnalyzer*metOptAnalyzer*metOptNoHFAnalyzer)
+analyzeCaloMETHO = cms.Sequence(metHOAnalyzer*metNoHFHOAnalyzer*metOptHOAnalyzer*metOptNoHFHOAnalyzer)
+analyzeCaloMET = cms.Sequence(analyzeCaloMETNoHO*analyzeCaloMETHO)
