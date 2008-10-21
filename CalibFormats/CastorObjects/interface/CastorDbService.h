23f027410a6f21ba887ad6c26f9e426d04bf14a3
Panagiotis Katsas <sha1-163d0c4b196b62c2ba4e39987db97923b896ea8b@cern.ch>
2008-10-21 17:08:40 +0000

    getCastorCalibrations method added

--- CalibFormats/CastorObjects/interface/CastorDbService.h
+++ CalibFormats/CastorObjects/interface/CastorDbService.h
@@ -8,9 +8,12 @@
 #include "CalibFormats/CastorObjects/interface/CastorChannelCoder.h"
 #include "CalibFormats/CastorObjects/interface/QieShape.h"
 #include "CalibFormats/CastorObjects/interface/CastorCoder.h"
+#include "CalibFormats/CastorObjects/interface/CastorCalibrationsSet.h"
 
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
+
+#include "CondFormats/CastorObjects/interface/AllObjects.h"
 
 class CastorCalibrations;
 class CastorCalibrationWidths;
@@ -34,8 +37,12 @@ class CastorDbService {
   CastorDbService ();
   CastorDbService (const edm::ParameterSet&);
 
-  bool makeCastorCalibration (const HcalGenericDetId& fId, CastorCalibrations* fObject) const;
-  bool makeCastorCalibrationWidth (const HcalGenericDetId& fId, CastorCalibrationWidths* fObject) const;
+ bool makeCastorCalibration (const HcalGenericDetId& fId, CastorCalibrations* fObject) const;
+ bool makeCastorCalibrationWidth (const HcalGenericDetId& fId, CastorCalibrationWidths* fObject) const;
+
+ //add get method for Castor calibrations; affects reconstructor!
+const CastorCalibrations& getCastorCalibrations(const HcalGenericDetId& fId) const { return mCalibSet.getCalibrations(fId); }
+
   const CastorPedestal* getPedestal (const HcalGenericDetId& fId) const;
   const CastorPedestalWidth* getPedestalWidth (const HcalGenericDetId& fId) const;
   const CastorGain* getGain (const HcalGenericDetId& fId) const;
@@ -44,14 +51,21 @@ class CastorDbService {
   const CastorQIEShape* getCastorShape () const;
   const CastorElectronicsMap* getCastorMapping () const;
   
+  //  void setData (const CastorPedestals* fItem) {mPedestals = fItem; buildCalibrations();}
   void setData (const CastorPedestals* fItem) {mPedestals = fItem;}
   void setData (const CastorPedestalWidths* fItem) {mPedestalWidths = fItem;}
   void setData (const CastorGains* fItem) {mGains = fItem;}
   void setData (const CastorGainWidths* fItem) {mGainWidths = fItem;}
+  //  void setData (const CastorQIEData* fItem) {mQIEData = fItem; buildCalibrations();}
   void setData (const CastorQIEData* fItem) {mQIEData = fItem;}
   void setData (const CastorChannelQuality* fItem) {mChannelQuality = fItem;}
   void setData (const CastorElectronicsMap* fItem) {mElectronicsMap = fItem;}
+
  private:
+  //add methods for calibration; affect reconstructor!
+  //  bool makeCastorCalibration (const HcalGenericDetId& fId, CastorCalibrations* fObject) const;
+  //  void buildCalibrations();
+
   mutable QieShape* mQieShapeCache;
   const CastorPedestals* mPedestals;
   const CastorPedestalWidths* mPedestalWidths;
@@ -60,6 +74,7 @@ class CastorDbService {
   const CastorQIEData* mQIEData;
   const CastorChannelQuality* mChannelQuality;
   const CastorElectronicsMap* mElectronicsMap;
+  CastorCalibrationsSet mCalibSet;
 };
 
 #endif
