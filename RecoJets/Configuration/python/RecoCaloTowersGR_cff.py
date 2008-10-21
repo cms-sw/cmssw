2e0ee51ee6dbe54408bdfef1e7743acb1e3af6fe
Andreas Oehler <sha1-bf17a625e96eed2dfa3f3b53d79864d479019360@cern.ch>
2008-10-21 14:40:07 +0000

    fixed RecoCaloTowersGR for new towers, HBThreshold differs (0.6) to std. RECO setting

--- RecoJets/Configuration/python/RecoCaloTowersGR_cff.py
+++ RecoJets/Configuration/python/RecoCaloTowersGR_cff.py
@@ -1,56 +1,43 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoCaloTowersGR_cff.py,v 1.3 2008/09/09 19:10:26 arizzi Exp $
+# $Id: RecoCaloTowersGR_cff.py,v 1.4 2008/10/21 13:38:22 oehler Exp $
 #
 # create GR calotowers here
 #
 from RecoJets.Configuration.CaloTowersES_cfi import *
 towerMaker = cms.EDFilter("CaloTowersCreator",
-    EBSumThreshold = cms.double(0.2), ## GeV, Scheme B
-
+    EBSumThreshold = cms.double(0.2),
+    HF2Weight = cms.double(1.0),
     EBWeight = cms.double(1.0),
     hfInput = cms.InputTag("hfreco"),
-    AllowMissingInputs = cms.untracked.bool(False),
-    EESumThreshold = cms.double(0.45), ## GeV, Scheme B
-
-    HOThreshold = cms.double(1.1), ## GeV, Scheme B
-
-    HBThreshold = cms.double(0.6), ## GeV, (0.9 in scheme B)
-
-    EBThreshold = cms.double(0.09), ## GeV, ORCA value w/o selective readout
-
-    HcalThreshold = cms.double(-1000.0), ## GeV, -1000 means cut not used 
-
+    EESumThreshold = cms.double(0.45),
+    HOThreshold = cms.double(1.1),
+    HBThreshold = cms.double(0.6),
+    HF1Threshold = cms.double(1.2),
     HEDWeight = cms.double(1.0),
     EEWeight = cms.double(1.0),
-    UseHO = cms.bool(True),
-    HF1Weight = cms.double(1.0),
-    HOWeight = cms.double(1e-99), ## Disable HO
-
+    UseHO = cms.bool(False),
     HESWeight = cms.double(1.0),
+    HF1Weight = cms.double(1.0),
+    HOWeight = cms.double(1.0),
+    EBThreshold = cms.double(0.09),
     hbheInput = cms.InputTag("hbhereco"),
-    HF2Weight = cms.double(1.0),
-    HF2Threshold = cms.double(1.8), ## GeV, Oprimized on 10% occupancy
-
-    EEThreshold = cms.double(0.45), ## GeV, ORCA value w/o selective readout
-
-    HESThreshold = cms.double(1.4), ## GeV, Scheme B
-
+    HcalThreshold = cms.double(-1000.0),
+    HF2Threshold = cms.double(1.8),
+    EEThreshold = cms.double(0.45),
+    HESThreshold = cms.double(1.4),
     hoInput = cms.InputTag("horeco"),
-    HF1Threshold = cms.double(1.2), ## GeV, Oprimized on 10% occupancy
-
-    HEDThreshold = cms.double(1.4), ## GeV, Scheme B
-
-    EcutTower = cms.double(-1000.0), ## GeV, -1000 means cut not used
-
+    HEDThreshold = cms.double(1.4),
+    EcutTower = cms.double(-1000.0),
+    AllowMissingInputs = cms.untracked.bool(False),
     ecalInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE")),
     HBWeight = cms.double(1.0),
-    # Method for momentum reconstruction
     MomConstrMethod = cms.int32(1),
-    #Depth, fraction of the respective calorimeter [0,1]
-    MomEmDepth = cms.double(0),
-    MomHadDepth = cms.double(0),
-    MomTotDepth = cms.double(0)
+    MomHBDepth = cms.double(0.2),
+    MomHEDepth = cms.double(0.4),
+    MomEBDepth = cms.double(0.3),
+    MomEEDepth = cms.double(0.0)
+
 )
 
 from RecoJets.JetProducers.CaloTowerSchemeBWithHO_cfi import *
