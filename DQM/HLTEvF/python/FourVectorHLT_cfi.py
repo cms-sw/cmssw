ecaf551d4d05f1d43981a003eb944cfda3acefc9
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-10-22 14:35:13 +0000

    changed FU->HLT

--- DQM/HLTEvF/python/FourVectorHLT_cfi.py
+++ DQM/HLTEvF/python/FourVectorHLT_cfi.py
@@ -1,4 +1,4 @@
-# $Id: FourVectorHLT_cfi.py,v 1.4 2008/08/22 12:54:49 wittich Exp $
+# $Id: FourVectorHLT_cfi.py,v 1.5 2008/10/08 06:49:13 lorenzo Exp $
 import FWCore.ParameterSet.Config as cms
 
 hltResults = cms.EDAnalyzer("FourVectorHLT",
@@ -7,9 +7,7 @@ hltResults = cms.EDAnalyzer("FourVectorHLT",
     ptMin = cms.untracked.double(0.0),
     filters = cms.VPSet(),
     # data best guess
-    triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","FU")
-    # MC
-    #triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT")
+    triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","HLT")
 )
 
 
