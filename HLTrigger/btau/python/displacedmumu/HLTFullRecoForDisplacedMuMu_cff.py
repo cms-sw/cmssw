7a9d46a0e6ad948c1405d6b9969d2f38337aa70d
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-23 12:35:40 +0000

    remove configuration, it is now stored in ConfDB

--- HLTrigger/btau/python/displacedmumu/HLTFullRecoForDisplacedMuMu_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HLTrigger.btau.displacedmumu.L3ForMuMuk_cff import *
-from HLTrigger.btau.displacedmumu.L3ForDisplacedMumuTrigger_cff import *
-
--- HLTrigger/btau/python/displacedmumu/eventContent_DisplacedMumu_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-DisplacedMumuHLT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_hltMumuPixelSeedFromL2Candidate_*_*', 
-        'keep *_hltCkfTrackCandidatesMumu_*_*', 
-        'keep *_hltCtfWithMaterialTracksMumu_*_*', 
-        'keep *_hltMuTracks_*_*')
-)
-
