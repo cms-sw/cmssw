5c316852de57c0cb19bbda97e1a332d14e62f1e7
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-10-23 12:25:13 +0000

    remove configuration, it is now stored in ConfDB

--- HLTrigger/btau/python/jetTag/jetReco_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HLTrigger.Configuration.common.CaloTowers_cff import *
-from HLTrigger.Configuration.common.RecoJetMET_cff import *
-hltBCommonL2reco = cms.Sequence(doCalo+doHLTJetReco+doHLTHTReco)
-
--- HLTrigger/btau/python/jetTag/lifetimeL25_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import copy
-from HLTrigger.btau.jetTag.hltJetTag_cfi import *
-hltBLifetimeL25filter = copy.deepcopy(hltJetTag)
-hltBLifetimeL25reco = cms.Sequence(cms.SequencePlaceholder("hltBLifetimeL25jetselection")+cms.SequencePlaceholder("hltBLifetimeL25tracking")*cms.SequencePlaceholder("hltBLifetimeL25Associator")*cms.SequencePlaceholder("hltBLifetimeL25TagInfos")*cms.SequencePlaceholder("hltBLifetimeL25BJetTags"))
-hltBLifetimeL25filter.JetTag = 'hltBLifetimeL25BJetTags'
-hltBLifetimeL25filter.MinTag = 3.5
-
