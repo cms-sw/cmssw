46cdcc8207a8e5bcf0064fb0078956917c879147
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-10-23 16:01:58 +0000

    clean up of ordering and using proper clone-syntax => no difference in content

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMuonIsolated_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlMuonIsolated_cff.py
@@ -1,27 +1,26 @@
+# AlCaReco for track based alignment using isolated muon tracks
 import FWCore.ParameterSet.Config as cms
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
-# AlCaReco for track based alignment using isolated muon tracks
-ALCARECOTkAlMuonIsolatedHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+ALCARECOTkAlMuonIsolatedHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    andOr = True, ## choose logical OR between Triggerbits
+    # NOTE: this has to hold for all triggertables, so in order for this to not crash all bits of all triggertables 
+    #         have to be in all triggertables but switched off via prescale.
+    #for L = 10e30:  HLT_Mu3 , HLT_Mu5
+    #for L = 10e31:  unknown
+    #for L = 10e32:  HLT_IsoMu11 , HLT_Mu15_L1Mu7
+    HLTPaths = ['HLT_Mu3', 'HLT_Mu5', 'HLT_IsoMu11', 'HLT_Mu15']
+    )
 import Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi
-ALCARECOTkAlMuonIsolated = Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi.AlignmentTrackSelector.clone()
-seqALCARECOTkAlMuonIsolated = cms.Sequence(ALCARECOTkAlMuonIsolatedHLT+ALCARECOTkAlMuonIsolated)
-ALCARECOTkAlMuonIsolatedHLT.andOr = True ## choose logical OR between Triggerbits
-
-# NOTE: this has to hold for all triggertables, so in order for this to not crash all bits of all triggertables 
-#         have to be in all triggertables but switched off via prescale.
-#for L = 10e30:  HLT_Mu3 , HLT_Mu5
-#for L = 10e31:  unknown
-#for L = 10e32:  HLT_IsoMu11 , HLT_Mu15_L1Mu7
-ALCARECOTkAlMuonIsolatedHLT.HLTPaths = ['HLT_Mu3', 'HLT_Mu5', 'HLT_IsoMu11', 'HLT_Mu15']
-ALCARECOTkAlMuonIsolated.filter = True ##do not store empty events
-
-ALCARECOTkAlMuonIsolated.applyBasicCuts = True
-ALCARECOTkAlMuonIsolated.ptMin = 2.0 ##GeV 
-
-ALCARECOTkAlMuonIsolated.etaMin = -3.5
-ALCARECOTkAlMuonIsolated.etaMax = 3.5
-ALCARECOTkAlMuonIsolated.nHitMin = 0
+ALCARECOTkAlMuonIsolated = Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi.AlignmentTrackSelector.clone(
+    filter = True, ##do not store empty events
+    applyBasicCuts = True,
+    ptMin = 2.0, ##GeV 
+    etaMin = -3.5,
+    etaMax = 3.5,
+    nHitMin = 0
+    )
+# These unfortunately cannot be put into the clone(..): 
 ALCARECOTkAlMuonIsolated.GlobalSelector.applyIsolationtest = True
 ALCARECOTkAlMuonIsolated.GlobalSelector.minJetDeltaR = 0.1
 ALCARECOTkAlMuonIsolated.GlobalSelector.applyGlobalMuonFilter = True
@@ -29,3 +28,4 @@ ALCARECOTkAlMuonIsolated.TwoBodyDecaySelector.applyMassrangeFilter = False
 ALCARECOTkAlMuonIsolated.TwoBodyDecaySelector.applyChargeFilter = False
 ALCARECOTkAlMuonIsolated.TwoBodyDecaySelector.applyAcoplanarityFilter = False
 
+seqALCARECOTkAlMuonIsolated = cms.Sequence(ALCARECOTkAlMuonIsolatedHLT+ALCARECOTkAlMuonIsolated)
