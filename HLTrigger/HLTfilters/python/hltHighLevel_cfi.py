0307e6ed994554972ce43b075a1fa51925c998a8
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-10-24 13:13:45 +0000

    Add untracked throw to py file.

--- HLTrigger/HLTfilters/python/hltHighLevel_cfi.py
+++ HLTrigger/HLTfilters/python/hltHighLevel_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 hltHighLevel = cms.EDFilter("HLTHighLevel",
     HLTPaths = cms.vstring(), ## provide list of HLT paths you want
 
-    andOr = cms.bool(True), ## true: OR of those on your list 
+    andOr = cms.bool(True), ## true: OR of those on your list
+
+    throw = cms.untracked.bool(True), ## throw exception on unknown path names
 
     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
 )
