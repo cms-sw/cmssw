a829bc5943146b4200b1880621a515bb7d0e3ecc
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-10-24 07:10:17 +0000

    updated the PFTauSelectorDefinition.h to use more than one Discriminators

--- RecoTauTag/TauTagTools/plugins/PFTauSelectorDefinition.h
+++ RecoTauTag/TauTagTools/plugins/PFTauSelectorDefinition.h
@@ -13,53 +13,78 @@ struct PFTauSelectorDefinition {
   typedef std::vector< reco::PFTau *> container;
   typedef container::const_iterator const_iterator;
 
-  PFTauSelectorDefinition ( const edm::ParameterSet & cfg ) :
-  discriminatorTag_( cfg.getParameter<edm::InputTag>( "discriminator" ) ), discriminatorCut_(cfg.getParameter<double>("selectionCut")) { }
-
+  struct TDiscCutPair {
+    edm::Handle<reco::PFTauDiscriminator> m_discHandle;
+    double m_cut;
+  };    
+  typedef std::vector<TDiscCutPair> TDiscCutPairVec;
+  
+  PFTauSelectorDefinition ( const edm::ParameterSet & cfg ) {
+    discriminators_ = cfg.getParameter< std::vector<edm::ParameterSet> >( "discriminators" ); 
+  }
+  
   const_iterator begin() const { return selected_.begin(); }
 
   const_iterator end() const { return selected_.end(); }
 
   void select( const HandleToCollection & hc, 
 	       const edm::Event & e,
-	       const edm::EventSetup& s) {
+	       const edm::EventSetup& s) 
+  {
+           
     selected_.clear();
+    assert( hc.isValid() ); // collection of PFTaus
     
-    // collection of PFTaus
-    assert( hc.isValid() );
-
-    // associated discriminator
-    // does not have to be a discrimination by isolation, 
-    // despite the name. 
-    edm::Handle<reco::PFTauDiscriminator> 
-      hdiscri;
+    // get discriminators and coresponding cuts from stored vpset
+    static TDiscCutPairVec discriminators;
+    discriminators.clear();
     
-    bool found = e.getByLabel(discriminatorTag_,
-			      hdiscri);
-    
-    if(!found) assert(0);
-    
-    // the discriminator collection and the PFTau collection
-    // must have the same size
-    assert( hdiscri->size() ==  hc->size());
-
+    for(std::vector< edm::ParameterSet >::iterator it = discriminators_.begin(); 
+        it != discriminators_.end();
+        ++it)
+    {
+      TDiscCutPair disc;
+      // get discriminator, check if valid
+      // assert isn't a good method to do it
+      if(!(e.getByLabel( it->getParameter<edm::InputTag>("discriminator"), disc.m_discHandle))) assert(0);
+      disc.m_cut = it->getParameter<double>("selectionCut");
+      // the discriminator collection and the PFTau collection
+      // must have the same size
+      assert( disc.m_discHandle->size() ==  hc->size());
+      discriminators.push_back(disc);
+    }
+      
 
     unsigned key=0;
+    static bool passedAllCuts;
     for( collection::const_iterator pftau = hc->begin(); 
-         pftau != hc->end(); ++pftau, ++key) {
-
+          pftau != hc->end();
+          ++pftau, ++key) 
+    {
+      passedAllCuts = true;
       reco::PFTauRef pfTauRef(hc, key);
-      if( (*hdiscri)[pfTauRef] > discriminatorCut_ )
-	selected_.push_back( new reco::PFTau(*pftau) );
-    }
-  }
+      
+      //iterate over all discriminators, check the cuts
+      for (TDiscCutPairVec::iterator discIt = discriminators.begin();
+           discIt!=discriminators.end();
+           ++discIt)
+      {
+        if ( (*(discIt->m_discHandle))[pfTauRef] <= discIt->m_cut)
+          passedAllCuts = false;
+      }
+      
+         
+      if(passedAllCuts)
+        selected_.push_back( new reco::PFTau(*pftau) );
+    } // end collection iteration
+  } // end select()
 
   size_t size() const { return selected_.size(); }
 
 private:
   container selected_;
-  edm::InputTag  discriminatorTag_;
-  float discriminatorCut_;
+  std::vector< edm::ParameterSet > discriminators_;
+  
 };
 
 #endif
