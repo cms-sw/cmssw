74a91d7a2b3092882365404adf3c26adf163b7e7
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-10-25 10:27:06 +0000

    Update for next integration round /dev/220pre0/V2 of ConfDB

--- HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
+++ HLTrigger/Configuration/python/HLTDefaultOutputWithFEDs_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_2_2_0_pre0/HLT/V1 (CMSSW_3_0_X_2008-10-21-0200_HLT1)
+# /dev/CMSSW_2_2_0_pre0/HLT/V2 (CMSSW_3_0_X_2008-10-24-0200_HLT4)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_2_0_pre0/HLT/V1')
+  tableName = cms.string('/dev/CMSSW_2_2_0_pre0/HLT/V2')
 )
 
 block_hltDefaultOutputWithFEDs = cms.PSet(
