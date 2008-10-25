5e4b3ab28dc3e9a909a78aaec1b2c7e2e2ac1cf2
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-10-25 12:56:48 +0000

    - removed PFTauSelector, which has been included in the tau packages - renamed and cleaned up config files

--- PhysicsTools/PFCandProducer/python/pfTauSelector_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-pfTaus = cms.EDProducer("PFTauSelector",
-    src = cms.InputTag("pfRecoTauProducer"),
-    discriminator = cms.InputTag("pfRecoTauDiscriminationByIsolation")
-)
--- PhysicsTools/PFCandProducer/python/pfTaus_cff.py
+++ PhysicsTools/PFCandProducer/python/pfTaus_cff.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoTauTag.Configuration.RecoPFTauTag_cff import *
-from PhysicsTools.PFCandProducer.pfTauSelector_cfi  import *
+from RecoTauTag.TauTagTools.PFTauSelector_cfi  import pfTauSelector
 
 pfRecoTauProducer.DataType = cms.string('AOD')
 myic5PFJetTracksAssociatorAtVertex = ic5PFJetTracksAssociatorAtVertex.clone()
 
 myic5PFJetTracksAssociatorAtVertex.jets = 'pfJets'
 pfRecoTauTagInfoProducer.PFJetTracksAssociatorProducer = 'myic5PFJetTracksAssociatorAtVertex'
+
+pfTaus = pfTauSelector.clone()
+pfTaus.discriminators = cms.VPSet(
+      cms.PSet( discriminator=cms.InputTag("pfRecoTauDiscriminationByIsolation"),selectionCut=cms.double(0.5))
+   )
+
 
 pfTauSequence = cms.Sequence(
     myic5PFJetTracksAssociatorAtVertex + 
