8a97c4bae12c8cabfbbf97000c7dd884d737376b
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-10-26 08:54:31 +0000

    more cleaning on the jet benchmark side

--- Validation/RecoParticleFlow/python/jetBenchmarkGeneric_cff.py
+++ Validation/RecoParticleFlow/python/jetBenchmarkGeneric_cff.py
@@ -2,9 +2,20 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoParticleFlow.pfJetBenchmarkGeneric_cfi import pfJetBenchmarkGeneric
 from Validation.RecoParticleFlow.caloJetBenchmarkGeneric_cfi import caloJetBenchmarkGeneric
+from RecoJets.Configuration.GenJetParticles_cff import *
+from RecoJets.Configuration.RecoGenJets_cff import * 
+from PhysicsTools.HepMCCandAlgos.genParticles_cfi import *
+from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
+# should do a cloning
+genParticlesForJets.ignoreParticleIDs.append(14)
+genParticlesForJets.ignoreParticleIDs.append(12)
+genParticlesForJets.ignoreParticleIDs.append(16)
+genParticlesForJets.excludeResonances = False
 
 jetBenchmarkGeneric = cms.Sequence( 
+    process.genJetParticles*
+    process.recoGenJets*
     pfJetBenchmarkGeneric +
     caloJetBenchmarkGeneric
     )
