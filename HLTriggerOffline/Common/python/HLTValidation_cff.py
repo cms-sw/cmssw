8fe7675a927d4af52541a4931f7a5c23c6cbd06e
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-10-27 14:11:41 +0000

    enable egamma sequence (code restructured to incorporate filtering fuctionality, electron and photon validation)

--- HLTriggerOffline/Common/python/HLTValidation_cff.py
+++ HLTriggerOffline/Common/python/HLTValidation_cff.py
@@ -1,8 +1,10 @@
 from HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi import *
 from HLTriggerOffline.Tau.Validation.HLTTauValidation_cff import *
 from HLTriggerOffline.special.MonitorAlCaEcalPi0_cfi import *
+from HLTriggerOffline.Egamma.EgammaValidation_cff import *
 hltvalidation = cms.Sequence(
     muonTriggerRateTimeAnalyzer
     +HLTTauVal
     +EcalPi0Mon
+    +egammaValidationSequence
     )
