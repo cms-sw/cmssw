cd189905b5fdcf3710b3434fc3aabe5248815f0b
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-10-27 08:22:06 +0000

    Add missing #include <cassert>

--- CalibFormats/SiPixelObjects/interface/PixelDACScanRange.h
+++ CalibFormats/SiPixelObjects/interface/PixelDACScanRange.h
@@ -15,6 +15,7 @@
 
 #include <string>
 #include <vector>
+#include <cassert>
 
 namespace pos{
 /*! \class PixelConfigurationVerifier PixelConfigurationVerifier.h "interface/PixelConfigurationVerifier.h"
