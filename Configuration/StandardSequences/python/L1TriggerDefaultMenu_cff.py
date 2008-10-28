3deab65a22e4cc9c61102283dc5bcb9095b3df6b
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-10-28 10:36:16 +0000

    change default menu to version from DB for 22X

--- Configuration/StandardSequences/python/L1TriggerDefaultMenu_cff.py
+++ Configuration/StandardSequences/python/L1TriggerDefaultMenu_cff.py
@@ -1,3 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-# remove menu in preparation to take it from DB
+# Choose a menu/prescale/mask from one of the .cffs
+# in L1TriggerConfig/L1GtConfigProducers/data/Luminosity
+from  L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu_2008MC_2E30_Unprescaled_cff import *
