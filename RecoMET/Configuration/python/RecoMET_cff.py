a84cd631bbe7ae66f5c5c07e2d97460b72ff44a9
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-28 20:02:41 +0000

    Added metsignificance to the reconstruction sequence

--- RecoMET/Configuration/python/RecoMET_cff.py
+++ RecoMET/Configuration/python/RecoMET_cff.py
@@ -7,14 +7,17 @@ import FWCore.ParameterSet.Config as cms
 #         already written into the event.
 # Modification by F. Ratnikov and R. Remington
 # Date: 10/21/08 
+# Addition of MET significance by F.Blekman
+# Date: 10/23/08
 
 from RecoJets.Configuration.CaloTowersES_cfi import *
 from RecoMET.METProducers.CaloTowersOpt_cfi import *
 from RecoMET.METProducers.CaloMET_cfi import *
 from RecoMET.METProducers.HTMET_cfi import *
-#sequence metreco = {met, htMetIC5, htMetMC5}
+from RecoMET.METProducers.CaloMETSignif_cfi import *
+#sequence metreco = {met, metsig, htMetIC5, htMetMC5}
 metreco = cms.Sequence(
-        met+metNoHF+metHO+metNoHFHO+
+        met+metsignificance+metNoHF+metHO+metNoHFHO+
             calotoweroptmaker+metOpt+metOptNoHF+calotoweroptmakerWithHO+metOptHO+metOptNoHFHO+
             htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5
             )
