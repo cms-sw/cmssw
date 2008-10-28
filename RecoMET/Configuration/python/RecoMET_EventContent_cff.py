78f8f97416eb5e3b0c8c7b9a0e70be6ffb382c21
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-28 20:03:48 +0000

    Added calometsignificance (F. Blekman)

--- RecoMET/Configuration/python/RecoMET_EventContent_cff.py
+++ RecoMET/Configuration/python/RecoMET_EventContent_cff.py
@@ -3,11 +3,12 @@ import FWCore.ParameterSet.Config as cms
 # Name:   RecoMET_EventContent.cff
 # Author: R.Cavanaugh
 # Date:   05.11.2006
-# Notes:  
+# Notes:
+# Modificatin: F. Blekman (added CaloMETSignif)
 # Full Event content 
 RecoMETFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*')
+        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
 )
 RecoGenMETFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
@@ -15,7 +16,7 @@ RecoGenMETFEVT = cms.PSet(
 # RECO content
 RecoMETRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*')
+        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
 )
 RecoGenMETRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
@@ -23,7 +24,7 @@ RecoGenMETRECO = cms.PSet(
 # AOD content
 RecoMETAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*')
+        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
 )
 RecoGenMETAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
