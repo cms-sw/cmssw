304d11f7f6d9b73f86a018e083dd5ac84b715348
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2008-10-29 11:28:57 +0000

    Removing assert

--- L1Trigger/GlobalCaloTrigger/src/L1GctLut.h
+++ L1Trigger/GlobalCaloTrigger/src/L1GctLut.h
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <cassert>
  
 /*!
  * \author Greg Heath
@@ -98,7 +97,7 @@ L1GctLut<NAddressBits, NDataBits>::~L1GctLut() {}
 template <int NAddressBits, int NDataBits>
 uint16_t L1GctLut<NAddressBits, NDataBits>::lutValue(const uint16_t lutAddress) const
 {
-  assert (m_setupOk);
+  if (!m_setupOk) return (uint16_t) 0;
   uint16_t address=(lutAddress & MAX_ADDRESS_BITMASK);
   uint16_t data=(value(address) & MAX_DATA_BITMASK);
   return data;
