657e03c4b45a7d022d33d3d8fef6024a6697430f
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-10-29 18:41:18 +0000

    Removing #include <iostream> from header files in favor of #include <iosfwd> Move operator<< from inline to .cc

--- DataFormats/RPCDigi/interface/RPCDigi.h
+++ DataFormats/RPCDigi/interface/RPCDigi.h
@@ -5,14 +5,15 @@
  *
  * Digi for Rsisitive Plate Chamber
  *  
- *  $Date: 2006/04/05 15:03:07 $
- *  $Revision: 1.7 $
+ *  $Date: 2006/05/16 14:36:29 $
+ *  $Revision: 1.8 $
  *
  * \author I. Segoni -- CERN & M. Maggi -- INFN Bari
  *
  */
 
 #include <boost/cstdint.hpp>
+#include <iosfwd>
 
 class RPCDigi{
 
@@ -32,11 +33,7 @@ private:
   int32_t  bx_; 
 };
 
+std::ostream & operator<<(std::ostream & o, const RPCDigi& digi);
 
-#include<iostream>
-inline std::ostream & operator<<(std::ostream & o, const RPCDigi& digi) {
-  return o << " " << digi.strip()
-	   << " " << digi.bx();
-}
 #endif
 
--- DataFormats/RPCDigi/src/RPCDigi.cc
+++ DataFormats/RPCDigi/src/RPCDigi.cc
@@ -1,13 +1,14 @@
 /** \file
  * 
- *  $Date: 2006/05/03 23:51:58 $
- *  $Revision: 1.6 $
+ *  $Date: 2006/05/16 14:36:29 $
+ *  $Revision: 1.7 $
  *
  * \author Ilaria Segoni
  */
 
 
-#include <DataFormats/RPCDigi/interface/RPCDigi.h>
+#include "DataFormats/RPCDigi/interface/RPCDigi.h"
+#include <iostream>
 
 RPCDigi::RPCDigi (int strip, int bx) :
   strip_(strip),
@@ -38,6 +39,10 @@ RPCDigi::operator<(const RPCDigi& digi) const{
     return digi.bx()<this->bx();
 }
 
+std::ostream & operator<<(std::ostream & o, const RPCDigi& digi) {
+  return o << " " << digi.strip()
+           << " " << digi.bx();
+}
 
 int RPCDigi::strip() const { return strip_; }
 
