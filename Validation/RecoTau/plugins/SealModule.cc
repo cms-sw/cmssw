f20c3f9fe73c09036aa2874610d52bc39c51deae
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-10-29 21:15:06 +0000

    Files needed to produce GenJets and use them as reference in the Validation analyzer

--- Validation/RecoTau/plugins/SealModule.cc
+++ Validation/RecoTau/plugins/SealModule.cc
@@ -1,8 +1,8 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "Validation/RecoTau/interface/DecayModeCountingTool.h"
+#include "Validation/RecoTau/interface/GenJetRefProducer.h"
 
 DEFINE_SEAL_MODULE();
 
-DEFINE_ANOTHER_FWK_MODULE(DecayModeCountingTool);
+DEFINE_ANOTHER_FWK_MODULE(GenJetRefProducer);
--- Validation/RecoTau/src/SealModule.cc
+++ Validation/RecoTau/src/SealModule.cc
@@ -1,10 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "Validation/RecoTau/interface/PFTauTagVal.h"
-#include "Validation/RecoTau/interface/CaloTauTagVal.h"
 #include "Validation/RecoTau/interface/TauTagValidation.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE( PFTauTagVal );
-DEFINE_ANOTHER_FWK_MODULE( CaloTauTagVal );
 DEFINE_ANOTHER_FWK_MODULE( TauTagValidation );
+
