337f3f23642be3bfdbf22c9f40653950d3b486c9
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-10-29 11:52:37 +0000

    temporary solution for make the global valiation running on the 2nd step

--- Configuration/StandardSequences/python/Validation_cff.py
+++ Configuration/StandardSequences/python/Validation_cff.py
@@ -1,5 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
+
+RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+        mix = cms.PSet(initialSeed = cms.untracked.uint32(12345),
+                       engineName = cms.untracked.string('HepJamesRandom')
+        ),
+        restoreStateLabel = cms.untracked.string("randomEngineStateProducer"),
+)
+
+from SimGeneral.MixingModule.mixNoPU_cfi import *
+mix.playback=True
+
 from Validation.GlobalDigis.globaldigis_analyze_cfi import *
 from Validation.GlobalRecHits.globalrechits_analyze_cfi import *
 from Validation.GlobalHits.globalhits_analyze_cfi import *
@@ -7,4 +18,4 @@ from Validation.Configuration.globalValidation_cff import *
 
 from HLTriggerOffline.Common.HLTValidation_cff import *
 
-validation = cms.Sequence(globaldigisanalyze*globalhitsanalyze*hltvalidation)
+validation = cms.Sequence(mix+globaldigisanalyze*globalhitsanalyze*globalValidation*hltvalidation)
