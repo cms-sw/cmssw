41c565a9dbcb1eaf14cb798165dfb5436efe2d7c
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2008-10-29 09:20:03 +0000

    added dead CSC chambers

--- CalibMuon/Configuration/python/CSC_FakeDBConditions_cff.py
+++ CalibMuon/Configuration/python/CSC_FakeDBConditions_cff.py
@@ -4,4 +4,4 @@ from CalibMuon.CSCCalibration.CSCFakeDBGains_cfi import *
 from CalibMuon.CSCCalibration.CSCFakeDBPedestals_cfi import *
 from CalibMuon.CSCCalibration.CSCFakeDBNoiseMatrix_cfi import *
 from CalibMuon.CSCCalibration.CSCFakeDBCrosstalk_cfi import *
-
+from CalibMuon.CSCCalibration.CSC_BadChambers_cfi import *
