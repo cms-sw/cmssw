d80a3cde161208a8f7832d4c0a6cfbf4f48640ea
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-10-30 00:28:50 +0000

    Use standard geometries

--- SimMuon/CSCDigitizer/test/CSCDigitizerTest_cfg.py
+++ SimMuon/CSCDigitizer/test/CSCDigitizerTest_cfg.py
@@ -3,13 +3,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("CSCDigitizerTest")
 #untracked PSet maxEvents = {untracked int32 input = 100}
 process.load("SimGeneral.MixingModule.mixNoPU_cfi")
-
-process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
-
-process.load("Geometry.CSCGeometry.cscGeometry_cfi")
-
-process.load("MagneticField.Engine.volumeBasedMagneticField_cfi")
-
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
@@ -21,7 +16,9 @@ process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 #replace muonCSCDigis.strips.doNoise = false
 #replace muonCSCDigis.wires.doNoise = false
 #replace muonCSCDigis.strips.doCrosstalk = false
-process.load("CalibMuon.Configuration.CSC_FakeDBConditions_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "IDEAL_V9::All"
+
 
 #   include "CalibMuon/Configuration/data/CSC_FrontierConditions.cff"
 #   replace cscConditions.toGet =  {
@@ -41,7 +38,9 @@ process.load("SimMuon.CSCDigitizer.muonCSCDigis_cfi")
 process.source = cms.Source("PoolSource",
     debugFlag = cms.untracked.bool(True),
     debugVebosity = cms.untracked.uint32(10),
-    fileNames = cms.untracked.vstring('file:simevent.root')
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_9/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v2/0000/BA6DA407-A985-DD11-A0D8-000423D9A2AE.root',
+       '/store/relval/CMSSW_2_1_9/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v2/0000/CAADA54A-AB85-DD11-95D6-000423D98F98.root',
+       '/store/relval/CMSSW_2_1_9/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v2/0001/52BAB7DF-0487-DD11-95A3-000423D9989E.root')
 )
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
@@ -53,16 +52,17 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 process.DQMStore = cms.Service("DQMStore")
 
 process.dump = cms.EDFilter("CSCDigiDump",
-    wireDigiTag = cms.InputTag("muonCSCDigis","MuonCSCWireDigi"),
+    wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     empt = cms.InputTag(""),
-    stripDigiTag = cms.InputTag("muonCSCDigis","MuonCSCStripDigi"),
-    comparatorDigiTag = cms.InputTag("muonCSCDigis","MuonCSCComparatorDigi")
+    stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
+    comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi")
 )
 
-process.o1 = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('cscdigis.root')
-)
+#process.o1 = cms.OutputModule("PoolOutputModule",
+#    fileName = cms.untracked.string('cscdigis.root')
+#)
 
-process.p1 = cms.Path(process.mix*process.simMuonCSCDigis*process.cscDigiValidation)
-process.ep = cms.EndPath(process.o1)
+process.p1 = cms.Path(process.mix*process.simMuonCSCDigis*process.dump)
+#process.ep = cms.EndPath(process.o1)
+#
 
