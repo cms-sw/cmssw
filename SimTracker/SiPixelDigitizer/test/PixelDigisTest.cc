4299854add3e01e9176092f96043f43632dc805b
Danek Kotlinski <sha1-3a033e6407e48eb9fbe364eec828608ee1ba03ae@cern.ch>
2008-10-30 13:46:58 +0000

    *** empty log message ***

--- SimTracker/SiPixelDigitizer/test/PixelDigisTest.cc
+++ SimTracker/SiPixelDigitizer/test/PixelDigisTest.cc
@@ -16,7 +16,7 @@
 //
 // Original Author:  d.k.
 //         Created:  Jan CET 2006
-// $Id: PixelDigisTest.cc,v 1.15 2007/03/07 16:53:07 wmtan Exp $
+// $Id: PixelDigisTest.cc,v 1.16 2007/06/05 09:51:11 dkotlins Exp $
 //
 //
 // system include files
@@ -62,6 +62,11 @@
 // for simulated Tracker hits
 //#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 
+// To use root histos
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+
 // For ROOT
 #include <TROOT.h>
 #include <TChain.h>
@@ -75,6 +80,8 @@ using namespace std;
 // Enable this to look at simlinks (link simhit->digis)
 // Can be used only with simulated data.
 //#define USE_SIM_LINKS
+
+#define HISTOS
 
 //
 // class decleration
@@ -94,7 +101,9 @@ private:
   // ----------member data ---------------------------
   bool PRINT;
 
-  TFile* hFile;
+#ifdef HISTOS
+
+  //TFile* hFile;
   TH1F *hdetunit;
   TH1F *heloss1,*heloss2, *heloss3;
   TH1F *helossF1,*helossF2;
@@ -116,6 +125,8 @@ private:
 
   TH2F *htest, *htest2;
   TH2F *hdetMap3,*hdetMap2,*hdetMap1, *hpixMap1, *hpixMap2, *hpixMap3; 
+
+#endif
 
   edm::InputTag src_;  
 
@@ -160,104 +171,122 @@ void PixelDigisTest::beginJob(const edm::EventSetup& iSetup) {
    using namespace edm;
    cout << "Initialize PixelDigisTest " <<endl;
 
+#ifdef HISTOS
+
+ // NEW way to use root (from 2.0.0?)
+  edm::Service<TFileService> fs;
+
+  // Histos go to a subdirectory "PixRecHits")
+  //TFileDirectory subDir = fs->mkdir( "mySubDirectory" );
+  //TFileDirectory subSubDir = subDir.mkdir( "mySubSubDirectory" );
 
    // put here whatever you want to do at the beginning of the job
-    hFile = new TFile ( "histo.root", "RECREATE" );
+  //hFile = new TFile ( "histo.root", "RECREATE" );
      
-    hdetunit = new TH1F( "hdetunit", "Det unit", 1000,
+    hdetunit = fs->make<TH1F>( "hdetunit", "Det unit", 1000,
                               302000000.,302300000.);
-    hpixid = new TH1F( "hpixid", "Pix det id", 10, 0., 10.);
-    hpixsubid = new TH1F( "hpixsubid", "Pix Barrel id", 10, 0., 10.);
-    hlayerid = new TH1F( "hlayerid", "Pix layer id", 10, 0., 10.);
-    hladder1id = new TH1F( "hladder1id", "Ladder L1 id", 50, 0., 50.);
-    hladder2id = new TH1F( "hladder2id", "Ladder L2 id", 50, 0., 50.);
-    hladder3id = new TH1F( "hladder3id", "Ladder L3 id", 50, 0., 50.);
-    hz1id = new TH1F( "hz1id", "Z-index id L1", 10, 0., 10.);
-    hz2id = new TH1F( "hz2id", "Z-index id L2", 10, 0., 10.);
-    hz3id = new TH1F( "hz3id", "Z-index id L3", 10, 0., 10.);
+    hpixid = fs->make<TH1F>( "hpixid", "Pix det id", 10, 0., 10.);
+    hpixsubid = fs->make<TH1F>( "hpixsubid", "Pix Barrel id", 10, 0., 10.);
+    hlayerid = fs->make<TH1F>( "hlayerid", "Pix layer id", 10, 0., 10.);
+    hladder1id = fs->make<TH1F>( "hladder1id", "Ladder L1 id", 50, 0., 50.);
+    hladder2id = fs->make<TH1F>( "hladder2id", "Ladder L2 id", 50, 0., 50.);
+    hladder3id = fs->make<TH1F>( "hladder3id", "Ladder L3 id", 50, 0., 50.);
+    hz1id = fs->make<TH1F>( "hz1id", "Z-index id L1", 10, 0., 10.);
+    hz2id = fs->make<TH1F>( "hz2id", "Z-index id L2", 10, 0., 10.);
+    hz3id = fs->make<TH1F>( "hz3id", "Z-index id L3", 10, 0., 10.);
  
-    hdigisPerDet1 = new TH1F( "hdigisPerDet1", "Digis per det l1", 
+    hdigisPerDet1 = fs->make<TH1F>( "hdigisPerDet1", "Digis per det l1", 
 			      200, -0.5, 199.5);
-    hdigisPerDet2 = new TH1F( "hdigisPerDet2", "Digis per det l2", 
+    hdigisPerDet2 = fs->make<TH1F>( "hdigisPerDet2", "Digis per det l2", 
 			      200, -0.5, 199.5);
-    hdigisPerDet3 = new TH1F( "hdigisPerDet3", "Digis per det l3", 
+    hdigisPerDet3 = fs->make<TH1F>( "hdigisPerDet3", "Digis per det l3", 
 			      200, -0.5, 199.5);
-    hdigisPerLay1 = new TH1F( "hdigisPerLay1", "Digis per layer l1", 
+    hdigisPerLay1 = fs->make<TH1F>( "hdigisPerLay1", "Digis per layer l1", 
 			      2000, -0.5, 3999.5);
-    hdigisPerLay2 = new TH1F( "hdigisPerLay2", "Digis per layer l2", 
+    hdigisPerLay2 = fs->make<TH1F>( "hdigisPerLay2", "Digis per layer l2", 
 			      2000, -0.5, 3999.5);
-    hdigisPerLay3 = new TH1F( "hdigisPerLay3", "Digis per layer l3", 
+    hdigisPerLay3 = fs->make<TH1F>( "hdigisPerLay3", "Digis per layer l3", 
 			      2000, -0.5, 3999.5);
-    hdetsPerLay1 = new TH1F( "hdetsPerLay1", "Full dets per layer l1", 
+    hdetsPerLay1 = fs->make<TH1F>( "hdetsPerLay1", "Full dets per layer l1", 
 			      161, -0.5, 160.5);
-    hdetsPerLay3 = new TH1F( "hdetsPerLay3", "Full dets per layer l3", 
+    hdetsPerLay3 = fs->make<TH1F>( "hdetsPerLay3", "Full dets per layer l3", 
 			      353, -0.5, 352.5);
-    hdetsPerLay2 = new TH1F( "hdetsPerLay2", "Full dets per layer l2", 
+    hdetsPerLay2 = fs->make<TH1F>( "hdetsPerLay2", "Full dets per layer l2", 
 			      257, -0.5, 256.5);
 
-    hdigisPerDetF1 = new TH1F( "hdigisPerDetF1", "Digis per det d1", 
+    hdigisPerDetF1 = fs->make<TH1F>( "hdigisPerDetF1", "Digis per det d1", 
 			      200, -0.5, 199.5);
-    hdigisPerDetF2 = new TH1F( "hdigisPerDetF2", "Digis per det d2", 
+    hdigisPerDetF2 = fs->make<TH1F>( "hdigisPerDetF2", "Digis per det d2", 
 			      200, -0.5, 199.5);
-    hdigisPerLayF1 = new TH1F( "hdigisPerLayF1", "Digis per layer d1", 
+    hdigisPerLayF1 = fs->make<TH1F>( "hdigisPerLayF1", "Digis per layer d1", 
 			      2000, -0.5, 1999.5);
-    hdigisPerLayF2 = new TH1F( "hdigisPerLayF2", "Digis per layer d2", 
+    hdigisPerLayF2 = fs->make<TH1F>( "hdigisPerLayF2", "Digis per layer d2", 
 			      2000, -0.5, 1999.5);
-    hdetsPerLayF1 = new TH1F( "hdetsPerLayF1", "Full dets per layer d1", 
+    hdetsPerLayF1 = fs->make<TH1F>( "hdetsPerLayF1", "Full dets per layer d1", 
 			      161, -0.5, 160.5);
-    hdetsPerLayF2 = new TH1F( "hdetsPerLayF2", "Full dets per layer d2", 
+    hdetsPerLayF2 = fs->make<TH1F>( "hdetsPerLayF2", "Full dets per layer d2", 
 			      257, -0.5, 256.5);
 
-    heloss1 = new TH1F( "heloss1", "Pix charge l1", 100, 0., 300.);
-    heloss2 = new TH1F( "heloss2", "Pix charge l2", 100, 0., 300.);
-    heloss3 = new TH1F( "heloss3", "Pix charge l3", 100, 0., 300.);
-    heloss1bigx = new TH1F( "heloss1bigx", "L1 big-x pix", 100, 0., 300.);
-    heloss1bigy = new TH1F( "heloss1bigy", "L1 big-y pix", 100, 0., 300.);
+    heloss1 = fs->make<TH1F>( "heloss1", "Pix charge l1", 100, 0., 300.);
+    heloss2 = fs->make<TH1F>( "heloss2", "Pix charge l2", 100, 0., 300.);
+    heloss3 = fs->make<TH1F>( "heloss3", "Pix charge l3", 100, 0., 300.);
+    heloss1bigx = fs->make<TH1F>( "heloss1bigx", "L1 big-x pix", 100, 0., 300.);
+    heloss1bigy = fs->make<TH1F>( "heloss1bigy", "L1 big-y pix", 100, 0., 300.);
 
-    hcols1 = new TH1F( "hcols1", "Layer 1 cols", 500,-1.5,498.5);
-    hcols2 = new TH1F( "hcols2", "Layer 2 cols", 500,-1.5,498.5);
-    hcols3 = new TH1F( "hcols3", "Layer 3 cols", 500,-1.5,498.5);
-    hcols1big = new TH1F( "hcols1big", "Layer 1 big cols", 500,-1.5,498.5);
+    hcols1 = fs->make<TH1F>( "hcols1", "Layer 1 cols", 500,-1.5,498.5);
+    hcols2 = fs->make<TH1F>( "hcols2", "Layer 2 cols", 500,-1.5,498.5);
+    hcols3 = fs->make<TH1F>( "hcols3", "Layer 3 cols", 500,-1.5,498.5);
+    hcols1big = fs->make<TH1F>( "hcols1big", "Layer 1 big cols", 500,-1.5,498.5);
  
-    hrows1 = new TH1F( "hrows1", "Layer 1 rows", 200,-1.5,198.5);
-    hrows2 = new TH1F( "hrows2", "Layer 2 rows", 200,-1.5,198.5);
-    hrows3 = new TH1F( "hrows3", "layer 3 rows", 200,-1.5,198.5);
-    hrows1big = new TH1F( "hrows1big", "Layer 1 big rows", 200,-1.5,198.5);
+    hrows1 = fs->make<TH1F>( "hrows1", "Layer 1 rows", 200,-1.5,198.5);
+    hrows2 = fs->make<TH1F>( "hrows2", "Layer 2 rows", 200,-1.5,198.5);
+    hrows3 = fs->make<TH1F>( "hrows3", "layer 3 rows", 200,-1.5,198.5);
+    hrows1big = fs->make<TH1F>( "hrows1big", "Layer 1 big rows", 200,-1.5,198.5);
  
-    hblade1 = new TH1F( "hblade1", "blade num, disk1", 24, 0., 24.);
-    hblade2 = new TH1F( "hblade2", "blade num, disk2", 24, 0., 24.);
+    hblade1 = fs->make<TH1F>( "hblade1", "blade num, disk1", 24, 0., 24.);
+    hblade2 = fs->make<TH1F>( "hblade2", "blade num, disk2", 24, 0., 24.);
 
-    helossF1 = new TH1F( "helossF1", "Pix charge d1", 100, 0., 300.);
-    helossF2 = new TH1F( "helossF2", "Pix charge d2", 100, 0., 300.);
-    hcolsF1 = new TH1F( "hcolsF1", "Disk 1 cols", 500,-1.5,498.5);
-    hcolsF2 = new TH1F( "hcolsF2", "Disk 2 cols", 500,-1.5,498.5);
-    hrowsF1 = new TH1F( "hrowsF1", "Disk 1 rows", 200,-1.5,198.5);
-    hrowsF2 = new TH1F( "hrowsF2", "Disk 2 rows", 200,-1.5,198.5);
+    helossF1 = fs->make<TH1F>( "helossF1", "Pix charge d1", 100, 0., 300.);
+    helossF2 = fs->make<TH1F>( "helossF2", "Pix charge d2", 100, 0., 300.);
+    hcolsF1 = fs->make<TH1F>( "hcolsF1", "Disk 1 cols", 500,-1.5,498.5);
+    hcolsF2 = fs->make<TH1F>( "hcolsF2", "Disk 2 cols", 500,-1.5,498.5);
+    hrowsF1 = fs->make<TH1F>( "hrowsF1", "Disk 1 rows", 200,-1.5,198.5);
+    hrowsF2 = fs->make<TH1F>( "hrowsF2", "Disk 2 rows", 200,-1.5,198.5);
 
 
 
-    hdetr = new TH1F("hdetr","det r",150,0.,15.);
-    hdetz = new TH1F("hdetz","det z",520,-26.,26.);
-    hdetrF = new TH1F("hdetrF","det r",150,0.,15.);
-    hdetzF = new TH1F("hdetzF","det z",700,-70.,70.);
+    hdetr = fs->make<TH1F>("hdetr","det r",150,0.,15.);
+    hdetz = fs->make<TH1F>("hdetz","det z",520,-26.,26.);
+    hdetrF = fs->make<TH1F>("hdetrF","det r",150,0.,15.);
+    hdetzF = fs->make<TH1F>("hdetzF","det z",700,-70.,70.);
 
-    hcolsB = new TH1F("hcolsB","cols per bar det",450,0.,450.);
-    hrowsB = new TH1F("hrowsB","rows per bar det",200,0.,200.);
-    hcolsF = new TH1F("hcolsF","cols per for det",300,0.,300.);
-    hrowsF = new TH1F("hrowsF","rows per for det",200,0.,200.);
+    hcolsB = fs->make<TH1F>("hcolsB","cols per bar det",450,0.,450.);
+    hrowsB = fs->make<TH1F>("hrowsB","rows per bar det",200,0.,200.);
+    hcolsF = fs->make<TH1F>("hcolsF","cols per for det",300,0.,300.);
+    hrowsF = fs->make<TH1F>("hrowsF","rows per for det",200,0.,200.);
 
-    hsimlinks = new TH1F("hsimlinks"," track ids",200,0.,200.);
-    hfract = new TH1F("hfract"," track rractions",100,0.,1.);
+    hsimlinks = fs->make<TH1F>("hsimlinks"," track ids",200,0.,200.);
+    hfract = fs->make<TH1F>("hfract"," track rractions",100,0.,1.);
 
-    hdetMap1 = new TH2F("hdetMap1"," ",22,0.,22.,10,0.,10.);
-    hdetMap2 = new TH2F("hdetMap2"," ",34,0.,34.,10,0.,10.);
-    hdetMap3 = new TH2F("hdetMap3"," ",46,0.,46.,10,0.,10.);
-    hpixMap1 = new TH2F("hpixMap1"," ",416,0.,416.,160,0.,160.);
-    hpixMap2 = new TH2F("hpixMap2"," ",416,0.,416.,160,0.,160.);
-    hpixMap3 = new TH2F("hpixMap3"," ",416,0.,416.,160,0.,160.);
+    hdetMap1 = fs->make<TH2F>("hdetMap1"," ",22,0.,22.,10,0.,10.);
+    hdetMap1->SetOption("colz");
+    hdetMap2 = fs->make<TH2F>("hdetMap2"," ",34,0.,34.,10,0.,10.);
+    hdetMap2->SetOption("colz");
+    hdetMap3 = fs->make<TH2F>("hdetMap3"," ",46,0.,46.,10,0.,10.);
+    hdetMap3->SetOption("colz");
+    hpixMap1 = fs->make<TH2F>("hpixMap1"," ",416,0.,416.,160,0.,160.);
+    hpixMap1->SetOption("colz");
+    hpixMap2 = fs->make<TH2F>("hpixMap2"," ",416,0.,416.,160,0.,160.);
+    hpixMap2->SetOption("colz");
+    hpixMap3 = fs->make<TH2F>("hpixMap3"," ",416,0.,416.,160,0.,160.);
+    hpixMap3->SetOption("colz");
 
-    htest = new TH2F("htest"," ",10,0.,10.,20,0.,20.);
-    htest2 = new TH2F("htest2"," ",10,0.,10.,300,0.,300.);
+    htest = fs->make<TH2F>("htest"," ",10,0.,10.,20,0.,20.);
+    htest2 = fs->make<TH2F>("htest2"," ",10,0.,10.,300,0.,300.);
+    htest->SetOption("colz");
+    htest2->SetOption("colz");
+
+#endif
 
 }
 
@@ -316,9 +345,11 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
     if(PRINT) 
       cout<<"Det: "<<detId.rawId()<<" "<<detId.null()<<" "<<detType<<" "<<subid<<endl;
     
+#ifdef HISTOS
     hdetunit->Fill(float(detid));
     hpixid->Fill(float(detType));
     hpixsubid->Fill(float(subid));
+#endif
     
     if(detType!=1) continue; // look only at tracker
     ++numberOfDetUnits;
@@ -348,10 +379,12 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
    // Subdet it, pix barrel=1, forward=2
     if(subid==2) {   // forward
 
+#ifdef HISTOS
       hdetrF->Fill(detR);
       hdetzF->Fill(detZ);
       hcolsF->Fill(float(cols));
       hrowsF->Fill(float(rows));
+#endif
 
       PXFDetId pdetId = PXFDetId(detid);
       disk=pdetId.disk(); //1,2,3
@@ -363,17 +396,19 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
       if(PRINT) {
 	cout<<"Forward det "<<subid<<", disk "<<disk<<", blade "
 		    <<blade<<", module "<<zindex<<", side "<<side<<", panel "
-		    <<panel<<" pos = "<<detZ<<" "<<detR<<endl;
+		    <<panel<<endl;
 	cout<<" col/row, pitch "<<cols<<" "<<rows<<" "
 		    <<pitchX<<" "<<pitchY<<endl;
       }
 
     } else if(subid == 1) { // Barrel 
       
+#ifdef HISTOS
       hdetr->Fill(detR);
       hdetz->Fill(detZ);
       hcolsB->Fill(float(cols));
       hrowsB->Fill(float(rows));
+#endif
       
       PXBDetId pdetId = PXBDetId(detid);
       unsigned int detTypeP=pdetId.det();
@@ -385,16 +420,21 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
       // Barrel Z-index=1,8
       zindex=pdetId.module();
       if(PRINT) { 
-	cout<<" Barrel det, z/r "<<detZ<<" "<<detR<<" thick "<<detThick<<" "
-	    <<" layer, ladder, module "
+	cout<<" Barrel layer, ladder, module "
 	    <<layer<<" "<<ladder<<" "<<zindex<<endl;
-	cout<<" col/row, pitch "<<cols<<" "<<rows<<" "
-	    <<pitchX<<" "<<pitchY<<endl;
+	//cout<<" Barrel det, thick "<<detThick<<" "
+	//  <<" layer, ladder, module "
+	//  <<layer<<" "<<ladder<<" "<<zindex<<endl;
+	//cout<<" col/row, pitch "<<cols<<" "<<rows<<" "
+	//  <<pitchX<<" "<<pitchY<<endl;
       }      
+#ifdef HISTOS
       hlayerid->Fill(float(layer));
+#endif
 
     } // end fb-bar
 
+#ifdef HISTOS
     // Some histos
     if(layer==1) {
       hladder1id->Fill(float(ladder));
@@ -427,6 +467,7 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
       ++numberOfDetUnitsF2;
       numOfDigisPerDetF2=0;
    }
+#endif
       
 
 #ifdef USE_SIM_LINKS
@@ -449,8 +490,11 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
 	unsigned int chan = it->channel();
 	unsigned int simTrack = it->SimTrackId();
 	float frac = it->fraction();
+#ifdef HISTOS
 	hsimlinks->Fill(float(simTrack));
 	hfract->Fill(frac);
+#endif
+
 	if(PRINT) cout<<" Sim link "<<numberOfSimLinks<<" "<<chan<<" "
 		      <<simTrack<<" "<<frac<<endl;
       } // end simlink det loop
@@ -478,13 +522,16 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
        if(PRINT) cout <<numberOfDigis<< " Col: " << col << " Row: " << row 
 		      << " ADC: " << adc <<" channel = "<<channel<<endl;
 
-       if(col>415) cout<<" Error: column index too large "<<col<<endl;
+       if(col>415) cout<<" Error: column index too large "<<col<<" Barrel layer, ladder, module "
+		       <<layer<<" "<<ladder<<" "<<zindex<<endl;
        if(row>159) cout<<" Error: row index too large "<<row<<endl;
 
+#ifdef HISTOS
        if(layer==1) {
 	 heloss1->Fill(float(adc));
 	 hcols1->Fill(float(col));
 	 hrows1->Fill(float(row));
+	 hpixMap1->Fill(float(col),float(row));
 	 totalNumOfDigis1++;
 	 htest2->Fill(float(zindex),float(adc));
 	 numOfDigisPerDet1++;
@@ -503,6 +550,7 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
 	 heloss2->Fill(float(adc));
 	 hcols2->Fill(float(col));
 	 hrows2->Fill(float(row));
+	 hpixMap2->Fill(float(col),float(row));
 	 totalNumOfDigis2++;
 	 numOfDigisPerDet2++;
 
@@ -510,6 +558,7 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
 	 heloss3->Fill(float(adc));
 	 hcols3->Fill(float(col));
 	 hrows3->Fill(float(row));
+	 hpixMap3->Fill(float(col),float(row));
 	 if(ladder==11 && zindex==5 )  // r367 
 	   //if(ladder==8 && zindex==5 ) //r371
 	   hpixMap3->Fill(float(col),float(row));  
@@ -530,11 +579,13 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
 	 totalNumOfDigisF2++;
 	 numOfDigisPerDetF2++;
        } // end if layer
+#endif
         
      } // end for digis
       //if(PRINT) 
       //cout<<" for det "<<detid<<" digis = "<<numberOfDigis<<endl;
 
+#ifdef HISTOS
      if(layer==1) {
        hdigisPerDet1->Fill(float(numOfDigisPerDet1));
        htest->Fill(float(zindex),float(numOfDigisPerDet1));
@@ -542,6 +593,7 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
      else if(layer==3) hdigisPerDet3->Fill(float(numOfDigisPerDet3));
      else if(disk==1) hdigisPerDetF1->Fill(float(numOfDigisPerDetF1));
      else if(disk==2) hdigisPerDetF2->Fill(float(numOfDigisPerDetF2));
+#endif
 
    } // end for det-units
 
@@ -549,24 +601,25 @@ void PixelDigisTest::analyze(const edm::Event& iEvent,
      cout << " Number of full det-units = " <<numberOfDetUnits
 	  <<" total digis = "<<totalNumOfDigis<<endl;
 
+#ifdef HISTOS
    hdigisPerLay1 ->Fill(float(totalNumOfDigis1));
    hdigisPerLay2 ->Fill(float(totalNumOfDigis2));
    hdigisPerLay3 ->Fill(float(totalNumOfDigis3));
    hdetsPerLay1 ->Fill(float(numberOfDetUnits1));
    hdetsPerLay2 ->Fill(float(numberOfDetUnits2));
    hdetsPerLay3 ->Fill(float(numberOfDetUnits3));
-
    hdigisPerLayF1 ->Fill(float(totalNumOfDigisF1));
    hdigisPerLayF2 ->Fill(float(totalNumOfDigisF2));
    hdetsPerLayF1 ->Fill(float(numberOfDetUnitsF1));
    hdetsPerLayF2 ->Fill(float(numberOfDetUnitsF2));
+#endif
 
 }
 // ------------ method called to at the end of the job  ------------
 void PixelDigisTest::endJob(){
   cout << " End PixelDigisTest " << endl;
-  hFile->Write();
-  hFile->Close();
+  //hFile->Write();
+  //hFile->Close();
 }
 
 //define this as a plug-in
