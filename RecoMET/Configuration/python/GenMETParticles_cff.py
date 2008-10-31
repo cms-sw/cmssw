b55ead52d0a1950da2ea679b5a7fd5b7100dd410
Bobby Scurlock <sha1-c67b1d7b4e566e787956e1b18ccb7345708f3160@cern.ch>
2008-10-31 19:22:32 +0000

    Update to genMet module names. Included new collection genMetAllVisible.

--- RecoMET/Configuration/python/GenMETParticles_cff.py
+++ RecoMET/Configuration/python/GenMETParticles_cff.py
@@ -31,4 +31,16 @@ genCandidatesForMET = cms.EDProducer(
     )  
     )
 
-genMETParticles = cms.Sequence(genCandidatesForMET)
+genParticlesForMETAllVisible = cms.EDFilter(
+    "InputGenJetsParticleSelector",
+    src = cms.InputTag("genParticles"),
+
+    ignoreParticleIDs = cms.vuint32(
+    1000022, 2000012, 2000014,
+    2000016, 1000039, 5000039,
+    4000012, 9900012, 9900014,
+    9900016, 39, 12, 14, 16
+    )
+    )                                        
+
+genMETParticles = cms.Sequence(genCandidatesForMET+genParticlesForMETAllVisible)
--- RecoMET/Configuration/python/RecoGenMET_cff.py
+++ RecoMET/Configuration/python/RecoGenMET_cff.py
@@ -5,13 +5,15 @@ import FWCore.ParameterSet.Config as cms
 # Date:   05.11.2006
 # Notes:  CaloMET.cfi assumes that a product with label "caloTowers" is 
 #         already written into the event.
-from RecoMET.METProducers.genMet_cfi import *
-from RecoMET.METProducers.genMetNoNuBSM_cfi import *
+from RecoMET.METProducers.genMetCalo_cfi import *
+from RecoMET.METProducers.genMetCaloAndNonPrompt_cfi import *
+from RecoMET.METProducers.genMetAllVisible_cfi import *
 from RecoMET.METProducers.genMetFromGenJets_cfi import *
 #
 # ShR 27 Mar 2007: genJetParticles from "RecoJets/Configuration/data/GenJetParticles.cff"
 # must be executed before this sequence in order to work since genMetNoNugenMetNoNu
 # needs genParticlesAllStableNoNu from that sequence
 # can't append them here explicitly because of scheduling problem
-recoGenMET = cms.Sequence(genMet+genMetNoNuBSM*genMetIC5GenJets)
+#recoGenMET = cms.Sequence(genMet+genMetNoNuBSM*genMetIC5GenJets)
+recoGenMET = cms.Sequence(genMetCalo+genMetCaloAndNonPrompt+genMetAllVisible*genMetIC5GenJets)
 
