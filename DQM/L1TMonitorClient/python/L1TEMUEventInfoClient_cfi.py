c1c6c25ea8760226011fd9051e92f898a79deb96
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-10-31 16:24:34 +0000

    subsystem masking, ordering enforced for consistency, disable qt report for empty subsystems

--- DQM/L1TMonitorClient/python/L1TEMUEventInfoClient_cfi.py
+++ DQM/L1TMonitorClient/python/L1TEMUEventInfoClient_cfi.py
@@ -3,7 +3,10 @@ import FWCore.ParameterSet.Config as cms
 l1temuEventInfoClient = cms.EDFilter("L1TEMUEventInfoClient",
     prescaleLS = cms.untracked.int32(-1),
     monitorDir = cms.untracked.string(''),
-    prescaleEvt = cms.untracked.int32(1)
+    prescaleEvt = cms.untracked.int32(1),
+    maskedSystems = cms.untracked.vuint32(0,1,1,0,0,0, 1,1,1,0,1)
+    #masking sequence: dtf, dtp, ctf, ctp, rpc, gmt, etp, htp, rct, gct, gt
+
 )
 
 
