27b340a248cd0cd82ac8e136e134a74ea7e34313
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2008-10-31 19:18:46 +0000

    Migrated algorithmic part of CATopJet into RecoJets. The "tagging" part remains in TopQuarkAnalysis/TopPairBSM.

--- TopQuarkAnalysis/TopPairBSM/python/caTopJets_cff.py
+++ TopQuarkAnalysis/TopPairBSM/python/caTopJets_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from TopQuarkAnalysis.TopPairBSM.CATopJetParameters_cfi import *
+from RecoJets.JetProducers.CATopJetParameters_cfi import *
 from RecoJets.JetProducers.GenJetParameters_cfi import *
 from RecoJets.JetProducers.CaloJetParameters_cfi import *
 
