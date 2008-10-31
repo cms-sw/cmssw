9c1852c7a7ccb74dc7609d427011a795e703f75b
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-10-31 23:17:17 +0000

    We've stopped supporting config files, so readConfigFile can be renamed to readConfig, since it can handle both.  Other methods are deprecated

--- FWCore/ParameterSet/interface/MakeParameterSets.h
+++ FWCore/ParameterSet/interface/MakeParameterSets.h
@@ -5,7 +5,7 @@
 //----------------------------------------------------------------------
 // Declare functions used to create ParameterSets.
 //
-// $Id: MakeParameterSets.h,v 1.6 2007/08/06 22:16:55 rpw Exp $
+// $Id: MakeParameterSets.h,v 1.7 2007/08/07 20:23:51 wmtan Exp $
 //
 //----------------------------------------------------------------------
 
@@ -18,29 +18,21 @@
 
 namespace edm
 {
+  // input can either be a python file name or a python config string
+  boost::shared_ptr<edm::ProcessDesc>
+  readConfig(const std::string & config);
 
-  /// meant to be done before makerParameterSets,
-  /// if you want to run in strict mode
-  void setStrictParsing(bool strict);
 
-  // Parse the given configuration text, filling in as output:
-  //
-  //   1. main, the ParameterSet to be used to configure the
-  //      EventProcessor, and
-  //   2. servicesparams, a vector of parameter sets to be used
-  //      to configure services.
-  //
-  // Both of the output arguments will be over-written.
-  // Failure of parsing results in an exception.
-
-  void 
+  /// essentially the same as the previous method
+  void
   makeParameterSets(std::string const& configtext,
-		    boost::shared_ptr<ParameterSet>& main,
-		    boost::shared_ptr<std::vector<ParameterSet> >& serviceparams);
+                  boost::shared_ptr<ParameterSet>& main,
+                  boost::shared_ptr<std::vector<ParameterSet> >& serviceparams);
 
 
-  boost::shared_ptr<edm::ProcessDesc> 
-  readConfigFile(const std::string & fileName);
+  // deprecated
+  boost::shared_ptr<edm::ProcessDesc>
+  readConfigFile(const std::string & fileName) {return readConfig(fileName);}
 
 
 } // namespace edm
--- FWCore/ParameterSet/src/MakeParameterSets.cc
+++ FWCore/ParameterSet/src/MakeParameterSets.cc
@@ -1,46 +1,24 @@
 #include "FWCore/ParameterSet/interface/MakeParameterSets.h"
 #include "FWCore/ParameterSet/interface/PythonProcessDesc.h"
-#include "FWCore/ParameterSet/interface/ParseTree.h"
-#include "FWCore/ParameterSet/interface/parse.h"
-#include "FWCore/ParameterSet/interface/pythonFileToConfigure.h"
-
-#include <iostream>
 namespace edm
 {
 
-  void setStrictParsing(bool strict)
+  boost::shared_ptr<ProcessDesc>
+  readConfig(const std::string & config)
   {
-    edm::pset::ParseTree::setStrictParsing(strict);
+    PythonProcessDesc pythonProcessDesc(config);
+    return pythonProcessDesc.processDesc();
   }
-
 
   void
   makeParameterSets(std::string const& configtext,
-		    boost::shared_ptr<ParameterSet>& main,
-		    boost::shared_ptr<std::vector<ParameterSet> >& serviceparams)
+                  boost::shared_ptr<ParameterSet>& main,
+                  boost::shared_ptr<std::vector<ParameterSet> >& serviceparams)
   {
-    edm::ProcessDesc processDesc(configtext);
-
-    main = processDesc.getProcessPSet();
-    serviceparams = processDesc.getServicesPSets();
-
-  }
-
-  boost::shared_ptr<ProcessDesc>
-  readConfigFile(const std::string & fileName)
-  {
-    if (fileName.size() > 3 && fileName.substr(fileName.size()-3) == ".py") 
-    {
-      PythonProcessDesc pythonProcessDesc(fileName);
-      return pythonProcessDesc.processDesc();
-      //std::string configString(pythonFileToConfigure(fileName)); 
-      //return boost::shared_ptr<ProcessDesc>(new ProcessDesc(configString));
-    }
-    else
-    {
-      std::string configString(read_whole_file(fileName));
-      return boost::shared_ptr<ProcessDesc>(new ProcessDesc(configString));
-    }
+    PythonProcessDesc pythonProcessDesc(configtext);
+    boost::shared_ptr<edm::ProcessDesc> processDesc = pythonProcessDesc.processDesc();
+    main = processDesc->getProcessPSet();
+    serviceparams = processDesc->getServicesPSets();
   }
 
 } // namespace edm
