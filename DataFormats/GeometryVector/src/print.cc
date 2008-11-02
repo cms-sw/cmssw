9e466a62aac337277ea23e93691fc91efc78a72c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-11-02 14:08:52 +0000

    removed iostream

--- DataFormats/GeometryVector/src/print.cc
+++ DataFormats/GeometryVector/src/print.cc
@@ -5,7 +5,7 @@ namespace geometryDetails {
   std::ostream & print3D(std::ostream& s, double x, double y, double z) {
     return s << " (" << x << ',' << y << ',' << z << ") ";
   } 
-  std::ostream & print3D(std::ostream& s, double x, double y) {
+  std::ostream & print2D(std::ostream& s, double x, double y) {
     return s << " (" << x << ',' << y << ") ";
   } 
 
