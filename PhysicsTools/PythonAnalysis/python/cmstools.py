b90ed8d0416b59050f3a80df4546e2f28660e073
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2008-11-03 16:11:46 +0000

    fix EventAux problem

--- PhysicsTools/PythonAnalysis/python/cmstools.py
+++ PhysicsTools/PythonAnalysis/python/cmstools.py
@@ -54,7 +54,7 @@ def createBranchBuffer(branch):
     branchType = ROOT.branchToClass(branch)
     #buffer = eval ('ROOT.'+reColons.sub(".",reOpenTemplate.sub("(ROOT.",reCloseTemplate.sub(")",branchType.GetName())))+'()')
     buffer = ROOT.MakeRootClass(branchType.GetName()) ()
-    if( branch.GetName()[-1] != '.'):
+    if( branch.GetName()[-1] != '.') and (branch.GetName()!="EventAuxiliary"):
         branch.SetAddress(buffer)
     else:
         branch.SetAddress(ROOT.AddressOf(buffer))
