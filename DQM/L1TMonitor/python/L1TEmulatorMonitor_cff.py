556d340204701deaf003652f4ebccba1102d6baf
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-11-03 10:06:25 +0000

    global tag specificified only in _cfg

--- DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
+++ DQM/L1TMonitor/python/L1TEmulatorMonitor_cff.py
@@ -8,12 +8,9 @@ import FWCore.ParameterSet.Config as cms
 #global configuration
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
-#off-line
-#GlobalTag.globaltag = 'CRAFT_V2P::All'
-#GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
-#on-line
-GlobalTag.globaltag = 'CRAFT_V2H::All'
-GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
+#note: global tag specification moved top parent _cfg , ie
+# : DQM/Integration/pythin/test/l1temulator_dqm_sourceclient-*_cfg.py
+# DQM/L1TMonitor/test/test/testEmulMon_cfg.py : test example
 
 #add'n
 from Configuration.StandardSequences.Geometry_cff import *
