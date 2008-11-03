8e719d9f8603f02877bbe6f802f0b65204b9f9be
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-11-03 14:22:17 +0000

    Add flags to run on different occupancy plots

--- DQM/DTMonitorClient/python/dtOccupancyTest_cfi.py
+++ DQM/DTMonitorClient/python/dtOccupancyTest_cfi.py
@@ -1,6 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 dtOccupancyTest = cms.EDAnalyzer("DTOccupancyTest",
-                                 testPulseMode = cms.untracked.bool(False))
+                                 testPulseMode = cms.untracked.bool(False),
+                                 runOnAllHitsOccupancies = cms.untracked.bool(True),
+                                 runOnNoiseOccupancies = cms.untracked.bool(False),
+                                 runOnInTimeOccupancies = cms.untracked.bool(False)
+                                 )
 
 
