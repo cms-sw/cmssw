02afda3a655654110e366b9ba6dae3169f7e9ea4
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2008-11-03 21:38:54 +0000

    switch offlinePrimaryVerticesFromCosmicTracks bo offlinePrimaryVertices to avoid cascade of name changes

--- RecoVertex/Configuration/python/RecoVertexCosmicTracks_cff.py
+++ RecoVertex/Configuration/python/RecoVertexCosmicTracks_cff.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 # Reco Vertex
 # initialize magnetic field #########################
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
-from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesFromCosmicTracks_cfi import *
 
-vertexrecoCosmics = cms.Sequence(offlinePrimaryVerticesFromCosmicTracks)
+import RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesFromCosmicTracks_cfi
+offlinePrimaryVertices = RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesFromCosmicTracks_cfi.offlinePrimaryVerticesFromCosmicTracks.clone()
+
+vertexrecoCosmics = cms.Sequence(offlinePrimaryVertices)
 
