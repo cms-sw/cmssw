d5a813b532e48f3f7e0dedf876be4a3e7534a313
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-11-03 21:57:29 +0000

    First commit: this file replaces PhotonIDAlgo.h It now serves as calculator of isolation variables with configurable parameters and fiducial regions

---
+++ RecoEgamma/PhotonIdentification/interface/PhotonIsolationCalculator.h
@@ -0,0 +1,105 @@
+#ifndef PhotonIsolationCalculator_H
+#define PhotonIsolationCalculator_H
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "RecoEgamma/PhotonIdentification/interface/CutBasedPhotonID.h"
+
+#include <string>
+
+class PhotonIsolationCalculator {
+
+public:
+
+  PhotonIsolationCalculator(){};
+
+  virtual ~PhotonIsolationCalculator(){};
+
+  void setup(const edm::ParameterSet& conf);
+
+  void calculate(const reco::Photon*, 
+		 const edm::Event&, const edm::EventSetup& es,
+		 PhotonFiducialFlags& phofid, 
+		 PhotonIsolationVariables& phoisolR03, 
+		 PhotonIsolationVariables& phoisolR04 );
+
+  void classify(const reco::Photon* photon, 
+		bool &isEBPho,
+		bool &isEEPho,
+		bool &isEBGap,
+		bool &isEEGap,
+		bool &isEBEEGap);
+  void calculateTrackIso(const reco::Photon* photon,
+			 const edm::Event &e,
+			 double &trkCone,
+			 int &ntrkCone,
+			 double pTThresh=0,
+			 double RCone=.4,
+			 double RinnerCone=.1);
+
+
+  double calculateEcalRecHitIso(const reco::Photon* photon,
+				const edm::Event& iEvent,
+				const edm::EventSetup& iSetup,
+				double RCone,
+				double RConeInner,
+                                double etaSlice,
+				double eMin,
+				double etMin);
+  double calculateHcalTowerIso(const reco::Photon* photon,
+			       const edm::Event& iEvent,
+			       const edm::EventSetup& iSetup,
+			       double RCone,
+			       double RConeInner,
+			       double eMin);
+
+
+  
+ protected:
+
+
+  std::string barrelecalCollection_;
+  std::string barrelecalProducer_;
+  std::string endcapecalCollection_;
+  std::string endcapecalProducer_;
+  std::string hcalCollection_;
+  std::string hcalProducer_;
+
+  edm::InputTag trackInputTag_;
+  //  edm::InputTag gsfRecoInputTag_;
+
+  double modulePhiBoundary_;
+  std::vector<double> moduleEtaBoundary_;
+
+  //Isolation parameters
+  double photonEcalRecHitConeInnerRadiusA_;
+  double photonEcalRecHitConeOuterRadiusA_;
+  double photonEcalRecHitEtaSliceA_;
+  double photonEcalRecHitThreshEA_;
+  double photonEcalRecHitThreshEtA_;
+  double photonHcalTowerConeInnerRadiusA_;
+  double photonHcalTowerConeOuterRadiusA_;
+  double photonHcalTowerThreshEA_;
+  double trackConeOuterRadiusA_;
+  double trackConeInnerRadiusA_;
+  double isolationtrackThresholdA_;
+
+  double photonEcalRecHitConeInnerRadiusB_;
+  double photonEcalRecHitConeOuterRadiusB_;
+  double photonEcalRecHitEtaSliceB_;
+  double photonEcalRecHitThreshEB_;
+  double photonEcalRecHitThreshEtB_;
+  double photonHcalTowerConeInnerRadiusB_;
+  double photonHcalTowerConeOuterRadiusB_;
+  double photonHcalTowerThreshEB_;
+  double trackConeOuterRadiusB_;
+  double trackConeInnerRadiusB_;
+  double isolationtrackThresholdB_;
+
+
+
+  };
+
+#endif // PhotonIsolationCalculator_H
