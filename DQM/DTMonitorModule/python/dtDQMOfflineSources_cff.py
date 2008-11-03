dd02170d500bc00bd660728c3484b8ec5412ff15
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-11-03 09:31:44 +0000

    aded the residuals monitoring

--- DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
+++ DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
@@ -3,9 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from DQM.DTMonitorModule.dtChamberEfficiencyTask_cfi import *
 from DQM.DTMonitorModule.dtSegmentTask_cfi import *
 dtSegmentAnalysisMonitor.detailedAnalysis = True
+from DQM.DTMonitorModule.dtResolutionTask_cfi import *
 
 dqmInfoDT = cms.EDFilter("DQMEventInfo",
                          subSystemFolder = cms.untracked.string('DT')
                          )
 
-dtSources = cms.Sequence(dtChamberEfficiencyMonitor*dtSegmentAnalysisMonitor*dqmInfoDT)
+dtSources = cms.Sequence(dtChamberEfficiencyMonitor*dtSegmentAnalysisMonitor*dtResolutionAnalysisMonitor*dqmInfoDT)
