c7ae6189482e30db0681804b055cf72fabd1619e
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2008-11-04 14:38:10 +0000

    added use of GTPE to get global numbers

--- EventFilter/Utilities/interface/GlobalEventNumber.h
+++ EventFilter/Utilities/interface/GlobalEventNumber.h
@@ -26,6 +26,15 @@ namespace evf{
       const unsigned int EVM_FDL_PSCVSN_OFFSET  = 11; //offset in 32-bit words
       const unsigned int EVM_TCS_BCNRIN_MASK    = 0x00000fff; // 12 LSB
 
+      const unsigned int GTPE_BOARDID_OFFSET = 16;
+      const unsigned int GTPE_BOARDID_MASK = 0x000000ff;
+      const unsigned int GTPE_BOARDID_SHIFT = 0;
+      const unsigned int GTPE_BOARDID_VALUE = 0x1;
+      const unsigned int GTPE_TRIGNR_OFFSET  = 14; //offset in 32-bit words
+      const unsigned int GTPE_ORBTNR_OFFSET  = 18; //offset in 32-bit words
+      const unsigned int GTPE_BCNRIN_OFFSET  = 3; //offset in 32-bit words
+      const unsigned int GTPE_BCNRIN_MASK    = 0x00000fff; // 12 LSB
+
       unsigned int offset(bool);
       inline bool daq_board_sense(const unsigned char *p)
 	{
@@ -35,12 +44,20 @@ namespace evf{
 	{
 	  return (*(unsigned int*)(p + sizeof(fedh_t) + EVM_BOARDID_OFFSET * SLINK_WORD_SIZE / 2) >> EVM_BOARDID_SHIFT) == EVM_BOARDID_VALUE;
 	}
+      inline bool gtpe_board_sense(const unsigned char *p)
+	{
+	  return (*(unsigned int*)(p + GTPE_BOARDID_OFFSET * SLINK_WORD_SIZE / 2) >> GTPE_BOARDID_SHIFT) != 0;
+	}
       unsigned int get(const unsigned char *, bool);
+      unsigned int gtpe_get(const unsigned char *);
       unsigned int getlbn(const unsigned char *);
+      unsigned int gtpe_getlbn(const unsigned char *);
       unsigned int getgpslow(const unsigned char *);
       unsigned int getgpshigh(const unsigned char *);
       unsigned int getorbit(const unsigned char *);
+      unsigned int gtpe_getorbit(const unsigned char *);
       unsigned int getfdlbx(const unsigned char *);
+      unsigned int gtpe_getbx(const unsigned char *);
       unsigned int getfdlpsc(const unsigned char *);
   }
 }
