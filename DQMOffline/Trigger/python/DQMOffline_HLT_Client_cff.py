a35ef63b89a356146c68e59f07a984bcca6e7e79
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2008-11-04 17:28:29 +0000

    Use DQM/HLTEvF/HLTEventInfoClient

--- DQMOffline/Trigger/python/DQMOffline_HLT_Client_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_HLT_Client_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.FourVectorHLTOfflineClient_cff import *
-from DQMOffline.Trigger.HLTEventInfoClient_cfi import *
+#from DQMOffline.Trigger.HLTEventInfoOfflineClient_cfi import *
+from DQM.HLTEvF.HLTEventInfoClient_cfi import *
 
 hltOfflineDQMClient = cms.Sequence(hltFourVectorSeqClient*hltEventInfoClient)
