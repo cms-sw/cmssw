dcdd4a085e018df0e3d43885f97ef161a0b8f5d8
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-11-04 16:16:43 +0000

    added

---
+++ CondFormats/HLTObjects/src/T_EventSetup_AlCaRecoTriggerBits.cc
@@ -0,0 +1,6 @@
+// -*- C++ -*-
+// user include files
+#include "CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(AlCaRecoTriggerBits);
---
+++ CondFormats/HLTObjects/src/classes.h
@@ -0,0 +1,4 @@
+#include "CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h"
+namespace {
+  std::map<std::string, std::string> e;
+}
