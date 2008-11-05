dd0995094a6c811367a8ace9cfc088928f044f12
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-11-05 17:26:14 +0000

    test global muons

--- TrackingTools/TrackRefitter/test/CosmicTrajectoryReader_cfg.py
+++ TrackingTools/TrackRefitter/test/CosmicTrajectoryReader_cfg.py
@@ -5,8 +5,8 @@ process = cms.Process("TESTFIT")
 
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
 process.load("TrackingTools.TrackRefitter.globalCosmicMuonTrajectories_cff")
-process.load("TrackingTools.TrackRefitter.cosmicMuonTrajectories_cff")
-process.load("TrackingTools.TrackRefitter.ctfWithMaterialTrajectoriesP5_cff")
+#process.load("TrackingTools.TrackRefitter.cosmicMuonTrajectories_cff")
+#process.load("TrackingTools.TrackRefitter.ctfWithMaterialTrajectoriesP5_cff")
 
 process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
@@ -28,7 +28,8 @@ process.source = cms.Source("PoolSource",
     #'/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/005F51E5-0373-DD11-B6FA-001731AF6B7D.root',
     #'/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/006F3A6A-0373-DD11-A8E7-00304876A0FF.root',
     #'/store/data/CRUZET4_v1/Cosmics/RECO/CRZT210_V1_SuperPointing_v1/0000/02CF5B1E-6476-DD11-A034-003048769E65.root'
-    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamMuAlGlobalCosmics_CRUZET4_v2/0007/F85734BA-6572-DD11-B9F8-0019B9F72CC2.root'    
+#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamMuAlGlobalCosmics_CRUZET4_v2/0007/F85734BA-6572-DD11-B9F8-0019B9F72CC2.root'
+    'file:F85734BA-6572-DD11-B9F8-0019B9F72CC2.root'
     ))
 
 process.maxEvents = cms.untracked.PSet(
@@ -49,9 +50,9 @@ process.MessageLogger = cms.Service("MessageLogger",
     DEBUG = cms.untracked.PSet(
     limit = cms.untracked.int32(0)
     ),
-    INFO = cms.untracked.PSet(
-    limit = cms.untracked.int32(0)
-    ),
+#     INFO = cms.untracked.PSet(
+#     limit = cms.untracked.int32(0)
+#     ),
     TrajectoryReader = cms.untracked.PSet(
     limit = cms.untracked.int32(10000000)
         ),
@@ -79,14 +80,14 @@ process.GLBTrajectoriesReader = cms.EDProducer("TrajectoryReader",
 #                                              InputLabel = cms.InputTag("standAloneMuons","Refitted")
 # )
 
-process.CTFTrajectoriesReader = cms.EDProducer("TrajectoryReader",
-                                               rootFileName = cms.untracked.string('CTFTajectoriesReader.root'),
-                                               InputLabel = cms.InputTag("ctfWithMaterialTracksP5","Refitted")
-                                               )
+# process.CTFTrajectoriesReader = cms.EDProducer("TrajectoryReader",
+#                                                rootFileName = cms.untracked.string('CTFTajectoriesReader.root'),
+#                                                InputLabel = cms.InputTag("ctfWithMaterialTracksP5","Refitted")
+#                                                )
 
 process.glbMuons = cms.Sequence(process.MuAlGlobalCosmics*process.GLBTrajectoriesReader)
 #process.staMuons = cms.Sequence(process.standAloneMuons*process.STATrajectoriesReader)
-process.tk = cms.Sequence(process.ctfWithMaterialTracksP5*process.CTFTrajectoriesReader)
+#process.tk = cms.Sequence(process.ctfWithMaterialTracksP5*process.CTFTrajectoriesReader)
 
 #process.testSTA = cms.Path(process.staMuons+process.tk+process.glbMuons)
 #process.testSTA = cms.Path(process.staMuons+process.tk)
