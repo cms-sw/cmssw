9a2f417139f1907108a07a75f531e4f938158367
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-11-05 17:49:36 +0000

    added the resolution monitoring

--- DQM/DTMonitorClient/python/dtDQMOfflineClients_cff.py
+++ DQM/DTMonitorClient/python/dtDQMOfflineClients_cff.py
@@ -4,13 +4,13 @@ from DQM.DTMonitorClient.dtChamberEfficiencyTest_cfi import *
 from DQM.DTMonitorClient.dtSegmentAnalysisTest_cfi import *
 segmentTest.detailedAnalysis = True
 from DQM.DTMonitorClient.dtOfflineSummaryClients_cfi import *
+from DQM.DTMonitorClient.dtResolutionAnalysisTest_cfi import *
 
-dtqTester = cms.EDFilter("QualityTester",
+dtQualityTests = cms.EDFilter("QualityTester",
                          #reportThreshold = cms.untracked.string('red'),
                          prescaleFactor = cms.untracked.int32(1),
                          qtList = cms.untracked.FileInPath('DQM/DTMonitorClient/test/QualityTests.xml'),
                          getQualityTestsFromFile = cms.untracked.bool(True)
                          )
 
-
-dtClients = cms.Sequence(chamberEfficiencyTest*segmentTest*dtOfflineSummaryClients*dtqTester)
+dtClients = cms.Sequence(chamberEfficiencyTest*dtQualityTests*segmentTest*resolutionAnalysisTest*dtOfflineSummaryClients)
