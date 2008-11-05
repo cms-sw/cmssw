20c376342b7ec692ccd759da6be8d4a52ceab6dd
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-11-05 10:55:38 +0000

    added

---
+++ CondCore/HLTPlugins/src/plugin.cc
@@ -0,0 +1,8 @@
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h"
+#include "CondFormats/DataRecord/interface/AlCaRecoTriggerBitsRcd.h"
+
+DEFINE_SEAL_MODULE();
+REGISTER_PLUGIN(AlCaRecoTriggerBitsRcd,AlCaRecoTriggerBits);
+
+
---
+++ CondFormats/DataRecord/src/AlCaRecoTriggerBitsRcd.cc
@@ -0,0 +1,7 @@
+// -*- C++ -*-
+// $Id$
+
+#include "CondFormats/DataRecord/interface/AlCaRecoTriggerBitsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(AlCaRecoTriggerBitsRcd);
