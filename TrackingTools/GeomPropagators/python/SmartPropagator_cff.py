aa2ed2bdb6652356ca4cb54b3d1e0a1c98ffde30
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2008-11-05 13:41:43 +0000

    cleaning, added some comments to the various SmartPropagators

--- TrackingTools/GeomPropagators/python/SmartPropagator_cff.py
+++ TrackingTools/GeomPropagators/python/SmartPropagator_cff.py
@@ -1,21 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
-from TrackingTools.MaterialEffects.MaterialPropagator_cfi import *
-from TrackingTools.MaterialEffects.OppositeMaterialPropagator_cfi import *
-import copy
-from TrackingTools.MaterialEffects.MaterialPropagator_cfi import *
-RKTrackerPropagator = copy.deepcopy(MaterialPropagator)
+# Tracker's propagators 
+from TrackingTools.MaterialEffects.RungeKuttaTrackerPropagator_cfi import *
+from TrackingTools.MaterialEffects.RungeKuttaTrackerPropagatorOpposite_cfi import *
+
 # Muon's propagators
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi import *
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi import *
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi import *
-# Tracker's propagators 
-from TrackingTools.GeomPropagators.SmartPropagator_cfi import *
+
+# Smart propagators
+
+# RungeKuttaTrackerPropagator + SteppingHelixPropagatorAlong (dir = alongMomentum)
 from TrackingTools.GeomPropagators.SmartPropagatorRK_cfi import *
-from TrackingTools.GeomPropagators.SmartPropagatorOpposite_cfi import *
-from TrackingTools.GeomPropagators.SmartPropagatorAny_cfi import *
+
+# RungeKuttaTrackerPropagatorOpposite + SteppingHelixPropagatorOpposite (dir = oppositeToMomentum)
+from TrackingTools.GeomPropagators.SmartPropagatorRKOpposite_cfi import *
+
+# RungeKuttaTrackerPropagator + SteppingHelixPropagatorAny (dir = alongMomentum)
 from TrackingTools.GeomPropagators.SmartPropagatorAnyRK_cfi import *
+
+# PropagatorWithMaterial + SteppingHelixPropagatorAlong (dir = alongMomentum)
+from TrackingTools.GeomPropagators.SmartPropagator_cfi import *
+
+# PropagatorWithMaterialOpposite + SteppingHelixPropagatorOpposite (dir = oppositeToMomentum)
+from TrackingTools.GeomPropagators.SmartPropagatorOpposite_cfi import *
+
+# PropagatorWithMaterial + SteppingHelixPropagatorAny (dir = alongMomentum)
+from TrackingTools.GeomPropagators.SmartPropagatorAny_cfi import *
+
+# PropagatorWithMaterialOpposite + SteppingHelixPropagatorAny (dir = oppositeToMomentum)
 from TrackingTools.GeomPropagators.SmartPropagatorAnyOpposite_cfi import *
-RKTrackerPropagator.ComponentName = 'RKTrackerPropagator'
-RKTrackerPropagator.useRungeKutta = True
+
 
