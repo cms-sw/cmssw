6b6df48fa1d6246b8d1e324326111a17db029a87
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-11-05 09:08:25 +0000

    first submission

---
+++ Fireworks/Core/interface/FWCustomIconsButton.h
@@ -0,0 +1,67 @@
+#ifndef Fireworks_Core_FWCustomIconsButton_h
+#define Fireworks_Core_FWCustomIconsButton_h
+// -*- C++ -*-
+//
+// Package:     Core
+// Class  :     FWCustomIconsButton
+// 
+/**\class FWCustomIconsButton FWCustomIconsButton.h Fireworks/Core/interface/FWCustomIconsButton.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Thu Oct 23 13:05:30 EDT 2008
+// $Id$
+//
+
+// system include files
+#include "TGButton.h"
+
+// user include files
+
+// forward declarations
+class TGPicture;
+
+class FWCustomIconsButton : public TGButton
+{
+   
+public:
+   FWCustomIconsButton(const TGWindow* iParent,
+                       const TGPicture* iUpIcon,
+                       const TGPicture* iDownIcon,
+                       const TGPicture* iDisabledIcon,
+                       Int_t id = -1, 
+                       GContext_t norm = TGButton::GetDefaultGC()(), 
+                       UInt_t option=0);
+   virtual ~FWCustomIconsButton();
+   
+   // ---------- const member functions ---------------------
+   
+   // ---------- static member functions --------------------
+   
+   // ---------- member functions ---------------------------
+   void swapIcons(const TGPicture*& iUpIcon,
+                  const TGPicture*& iDownIcon,
+                  const TGPicture*& iDisabledIcon);
+   
+protected:
+   virtual void DoRedraw();
+private:
+   FWCustomIconsButton(const FWCustomIconsButton&); // stop default
+   
+   const FWCustomIconsButton& operator=(const FWCustomIconsButton&); // stop default
+   
+   // ---------- member data --------------------------------
+   const TGPicture* m_upIcon;
+   const TGPicture* m_downIcon;
+   const TGPicture* m_disabledIcon;
+   
+};
+
+
+#endif
---
+++ Fireworks/Core/src/FWCustomIconsButton.cc
@@ -0,0 +1,117 @@
+// -*- C++ -*-
+//
+// Package:     Core
+// Class  :     FWCustomIconsButton
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Thu Oct 23 13:05:35 EDT 2008
+// $Id$
+//
+
+// system include files
+#include <algorithm>
+#include "TGPicture.h"
+
+// user include files
+#include "Fireworks/Core/interface/FWCustomIconsButton.h"
+
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+FWCustomIconsButton::FWCustomIconsButton(const TGWindow* iParent,
+                                         const TGPicture* iUpIcon,
+                                         const TGPicture* iDownIcon,
+                                         const TGPicture* iDisabledIcon,
+                                         Int_t id, GContext_t norm, UInt_t option):
+TGButton(iParent,id, norm, option),
+m_upIcon(iUpIcon),
+m_downIcon(iDownIcon),
+m_disabledIcon(iDisabledIcon)
+{
+   assert(0!=iUpIcon);
+   assert(0!=iDownIcon);
+   assert(0!=iDisabledIcon);
+   gVirtualX->ShapeCombineMask(GetId(), 0, 0, iUpIcon->GetMask());
+   SetBackgroundPixmap(iUpIcon->GetPicture());
+   Resize(iUpIcon->GetWidth(),iUpIcon->GetHeight());
+   fTWidth = iUpIcon->GetWidth();
+   fTHeight = iUpIcon->GetHeight();
+}
+
+// FWCustomIconsButton::FWCustomIconsButton(const FWCustomIconsButton& rhs)
+// {
+//    // do actual copying here;
+// }
+
+FWCustomIconsButton::~FWCustomIconsButton()
+{
+}
+
+//
+// assignment operators
+//
+// const FWCustomIconsButton& FWCustomIconsButton::operator=(const FWCustomIconsButton& rhs)
+// {
+//   //An exception safe implementation is
+//   FWCustomIconsButton temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+void 
+FWCustomIconsButton::swapIcons(const TGPicture*& iUpIcon,
+                               const TGPicture*& iDownIcon,
+                               const TGPicture*& iDisabledIcon)
+{
+   std::swap(iUpIcon,m_upIcon);
+   std::swap(iDownIcon,m_downIcon);
+   std::swap(iDisabledIcon,m_disabledIcon);
+   fClient->NeedRedraw(this);
+}
+
+//
+// const member functions
+//
+void 
+FWCustomIconsButton::DoRedraw()
+{
+   //ChangeOptions(0);
+   //TGButton::DoRedraw();
+   //Stole this from TGPictureButton.
+   int x = (fWidth - fTWidth) >> 1;
+   int y = (fHeight - fTHeight) >> 1;
+
+   gVirtualX->FillRectangle(fId, fNormGC, 2,2,fWidth,fHeight);
+   switch(fState) {
+      case kButtonUp:
+         m_upIcon->Draw(fId, fNormGC,x,y);
+         break;
+      case kButtonEngaged:
+      case kButtonDown:
+         m_downIcon->Draw(fId, fNormGC,x,y);
+         break;
+      case kButtonDisabled:
+      default:
+         m_disabledIcon->Draw(fId, fNormGC,x,y);  
+   }
+}
+
+//
+// static member functions
+//
