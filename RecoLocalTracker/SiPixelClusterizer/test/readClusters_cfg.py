c3b92d1e17fa6e2c38b02ce42504a4662dede80e
Danek Kotlinski <sha1-3a033e6407e48eb9fbe364eec828608ee1ba03ae@cern.ch>
2008-11-05 10:29:13 +0000

    add the test python script

---
+++ RecoLocalTracker/SiPixelClusterizer/test/readClusters_cfg.py
@@ -0,0 +1,64 @@
+#
+# Last update: new version for python
+#
+#
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("cluTest")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('siPixelClusters'),
+    destinations = cms.untracked.vstring('cout'),
+#    destinations = cms.untracked.vstring("log","cout"),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR')
+    )
+#    log = cms.untracked.PSet(
+#        threshold = cms.untracked.string('DEBUG')
+#    )
+)
+
+process.source = cms.Source("PoolSource",
+#    fileNames =  cms.untracked.vstring('file:/scratch/dkotlins/digis.root')
+   fileNames =  cms.untracked.vstring('file:/scratch/dkotlins/promptrecoCosmics_1.root')
+)
+
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('histo.root')
+)
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+# what is this?
+# process.load("Configuration.StandardSequences.Services_cff")
+
+# what is this?
+#process.load("SimTracker.Configuration.SimTracker_cff")
+
+# needed for global transformation
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
+# Initialize magnetic field
+#  include "MagneticField/Engine/data/volumeBasedMagneticField.cfi"
+# Tracker SimGeometryXML
+#  include "Geometry/TrackerSimData/data/trackerSimGeometryXML.cfi"
+# Tracker Geometry Builder
+#  include "Geometry/TrackerGeometryBuilder/data/trackerGeometry.cfi"
+# Tracker Numbering Builder
+#  include "Geometry/TrackerNumberingBuilder/data/trackerNumberingGeometry.cfi"
+ 
+process.analysis = cms.EDAnalyzer("ReadPixClusters",
+#    VerboseLevel = cms.untracked.int32(1),
+    src = cms.InputTag("siPixelClusters"),
+)
+
+process.p = cms.Path(process.analysis)
+
+
+
