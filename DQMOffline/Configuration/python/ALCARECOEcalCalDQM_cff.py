60d0e1db426fee603a92768e3adccd2810a3bae7
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-11-05 13:18:03 +0000

    added electron calibration DQM

--- DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
+++ DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
@@ -2,7 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 import DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi
 import DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi
+import DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi
 
 ALCARECOEcalCalPhisymDQM = DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi.EcalPhiSymMon.clone()
 
 ALCARECOEcalCalPi0CalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0Mon.clone()
+
+ALCARECOEcalCalElectronCalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi.EcalEleCalibMon.clone()
