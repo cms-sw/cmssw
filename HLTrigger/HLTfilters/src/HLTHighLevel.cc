37c10aa90c9f93078aff5aac357e7fc47bc0aa72
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-11-05 14:03:10 +0000

    - add support for wildcards in trigger names  - move all the event-independent and string-heavy code in an 'init()'     function called eache time the configuration changes

--- HLTrigger/HLTfilters/src/HLTHighLevel.cc
+++ HLTrigger/HLTfilters/src/HLTHighLevel.cc
@@ -2,42 +2,50 @@
  *
  * See header file for documentation
  *
- *  $Date: 2008/01/09 14:30:05 $
- *  $Revision: 1.8 $
+ *  $Date: 2008/10/17 09:57:43 $
+ *  $Revision: 1.9 $
  *
  *  \author Martin Grunewald
  *
  */
 
-#include "HLTrigger/HLTfilters/interface/HLTHighLevel.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <boost/foreach.hpp>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include "DataFormats/Common/interface/Handle.h"
-
 #include "DataFormats/Common/interface/TriggerResults.h"
-
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include <cassert>
+
+#include "HLTrigger/HLTfilters/interface/HLTHighLevel.h"
+
+// static functions (partly taken from EventSelector) - see below
+bool is_glob(const std::string & pattern);
+  
+std::string glob2reg(const std::string & pattern);
+
+std::vector< std::vector<std::string>::const_iterator > 
+matching_triggers(const std::vector<std::string> & triggers, const std::string & pattern);
 
 //
 // constructors and destructor
 //
 HLTHighLevel::HLTHighLevel(const edm::ParameterSet& iConfig) :
-  inputTag_ (iConfig.getParameter<edm::InputTag> ("TriggerResultsTag")),
-  triggerNames_(),
-  andOr_    (iConfig.getParameter<bool> ("andOr" )),
-  throw_    (iConfig.getUntrackedParameter<bool> ("throw",true)),
-  n_        (0),
-  first_    (true)
-
+  inputTag_     (iConfig.getParameter<edm::InputTag> ("TriggerResultsTag")),
+  triggerNames_ (),
+  andOr_        (iConfig.getParameter<bool> ("andOr")),
+  throw_        (iConfig.getUntrackedParameter<bool> ("throw", true)),
+  HLTPatterns_  (iConfig.getParameter<std::vector<std::string> >("HLTPaths")),
+  HLTPathsByName_(),
+  HLTPathsByIndex_()
 {
-  // get names from module parameters, then derive slot numbers
-  HLTPathsByName_= iConfig.getParameter<std::vector<std::string > >("HLTPaths");
-  n_=HLTPathsByName_.size();
-  HLTPathsByIndex_.resize(n_);
-
-  // this is a user/analysis filter: it places no product into the event!
-
+  // names and slot numbers are computed during the event loop, 
+  // as they need to access the TriggerNames object via the TriggerResults
 }
 
 HLTHighLevel::~HLTHighLevel()
@@ -47,11 +55,73 @@ HLTHighLevel::~HLTHighLevel()
 //
 // member functions
 //
-bool
-HLTHighLevel:: beginRun(edm::Run& iRun, const edm::EventSetup& iSetup)
+
+// Initialize the internal trigger path representation (names and indices) from the 
+// patterns specified in the configuration.
+// This needs to be called once at startup and whenever the trigger table has changed
+void HLTHighLevel::init(const edm::TriggerResults & result)
 {
-  first_=true;
-  return true;
+   unsigned int n;
+   
+   // clean up old data
+   HLTPathsByName_.clear();
+   HLTPathsByIndex_.clear();
+
+   if (HLTPatterns_.empty()) {
+     // for empty input vector, default to all HLT trigger paths
+     n = result.size();
+     HLTPathsByName_.resize(n);
+     HLTPathsByIndex_.resize(n);
+     for (unsigned int i = 0; i < n; ++i) {
+       HLTPathsByName_[i] = triggerNames_.triggerName(i);
+       HLTPathsByIndex_[i] = i;
+     }
+   } else {
+     // otherwise, expand wildcards in trigger names...
+     BOOST_FOREACH(const std::string & pattern, HLTPatterns_) {
+       if (is_glob(pattern)) {
+         // found a glob pattern, expand it
+         std::vector< std::vector<std::string>::const_iterator > matches = matching_triggers(triggerNames_.triggerNames(), glob2reg(pattern));
+         if (matches.empty()) {
+           // pattern does not match any trigger paths
+           LogDebug("") << "requested pattern does not match any HLT paths: " << pattern;
+         } else {
+           // store the matching patterns
+           BOOST_FOREACH(std::vector<std::string>::const_iterator match, matches)
+             HLTPathsByName_.push_back(*match);
+         }
+       } else {
+         // found a trigger name, just copy it
+         HLTPathsByName_.push_back(pattern);
+       }
+     }
+     n = HLTPathsByName_.size();
+   
+     // ...and get hold of trigger indices
+     HLTPathsByIndex_.resize(n);
+     for (unsigned int i = 0; i < HLTPathsByName_.size(); i++) {
+       HLTPathsByIndex_[i] = triggerNames_.triggerIndex(HLTPathsByName_[i]);
+       if (HLTPathsByIndex_[i] >= result.size()) {
+         // trigger path not found
+         LogDebug("") << "requested HLT path does not exist: " << HLTPathsByName_[i];
+         HLTPathsByIndex_[i] = (unsigned int) -1;
+       }
+     }
+   }
+   
+   // report on what is finally used
+   LogDebug("") << "HLT trigger paths: " + inputTag_.encode()
+                << " - Number of paths: " << n
+                << " - andOr mode: " << andOr_
+                << " - throw mode: " << throw_;
+   
+   LogTrace("") << "The HLT trigger paths (# index name):";
+   for (unsigned int i = 0; i < n; ++i)
+     if (HLTPathsByIndex_[i] == (unsigned int) -1)
+       LogTrace("") << "    n/a   " << HLTPathsByName_[i];
+     else
+       LogTrace("") << "    " << std::setw(4) << HLTPathsByIndex_[i] << "  " << HLTPathsByName_[i];
+
 }
 
 // ------------ method called to produce the data  ------------
@@ -61,11 +131,9 @@ HLTHighLevel::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    using namespace std;
    using namespace edm;
 
-   const string invalid("@@invalid@@");
-
    // get hold of TriggerResults Object
    Handle<TriggerResults> trh;
-   iEvent.getByLabel(inputTag_,trh);
+   iEvent.getByLabel(inputTag_, trh);
    if (trh.isValid()) {
      LogDebug("") << "TriggerResults found, number of HLT paths: " << trh->size();
    } else {
@@ -73,84 +141,82 @@ HLTHighLevel::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
      return false;
    }
 
-   // get hold of trigger names and indices - based on TriggerResults object!
-   triggerNames_.init(*trh);
-   unsigned int n(n_);
-   for (unsigned int i=0; i!=n; i++) {
-     HLTPathsByIndex_[i]=triggerNames_.triggerIndex(HLTPathsByName_[i]);
-   }
-   
-   // for empty input vector (n==0), default to all HLT trigger paths!
-   if (n==0) {
-     n=trh->size();
-     HLTPathsByName_.resize(n);
-     HLTPathsByIndex_.resize(n);
-     for (unsigned int i=0; i!=n; i++) {
-       HLTPathsByName_[i]=triggerNames_.triggerName(i);
-       HLTPathsByIndex_[i]=i;
-     }
-   }
+   // init the TriggerNames with the TriggerResults
+   bool config_changed = triggerNames_.init(*trh);
 
-   // report on what is finally used
-   if (first_) {
-     LogDebug("") << "HLT trigger paths: " + inputTag_.encode()
-		  << " - Number of paths: " << n
-		  << " - andOr mode: " << andOr_
-		  << " - throw mode: " << throw_;
-     LogDebug("") << "   The HLT trigger paths (# index name):";
-   }
+   // if this is the first event or the HLT table has changed (re)run th initialization stuff
+   if (config_changed)
+     init(*trh);  
 
-   unsigned int nbad(0);
-   string message("   ");
-   for (unsigned int i=0; i!=n; i++) {
-     if (first_) {
-       LogTrace("") << " " << i 
-		    << " " << HLTPathsByIndex_[i]
-		    << " " << HLTPathsByName_[i];
-     }
-     if (HLTPathsByIndex_[i]>=trh->size()) {
-       nbad++;
-       message=message+" "+HLTPathsByName_[i];
-     }
-   }
+   unsigned int n     = HLTPathsByName_.size();
+   unsigned int nbad  = 0;
+   unsigned int fired = 0;
 
-   if (nbad>0) {
-     if (first_) {
-       LogTrace("") << "  Unknown Triggers: " << message;
-       cout
-	 << " HLTHighLevel [instance: " << *moduleLabel()
-	 << " - path: " << *pathName()
-	 << "] configured with " << nbad
-	 << "/" << n
-	 << " unknown HLT path names: " << message
-	 << "\n";
-       first_=false;
+   // count invalid and fired triggers
+   for (unsigned int i = 0; i < n; i++)
+     if (HLTPathsByIndex_[i] == (unsigned int) -1)
+       ++nbad;
+     else if (trh->accept(HLTPathsByIndex_[i]))
+       ++fired;
+
+   if ((nbad > 0) and (config_changed or throw_)) {
+     // only generate the error message if it's actually going to be used
+     std::string message;
+
+     for (unsigned int i = 0; i < n; i++)
+       if (HLTPathsByIndex_[i] == (unsigned int) -1)
+         message += HLTPathsByName_[i] + " ";
+
+     if (config_changed) {
+       LogTrace("")
+         << " HLTHighLevel [instance: " << *moduleLabel()
+         << " - path: " << *pathName()
+         << "] configured with " << nbad
+         << "/" << n
+         << " unknown HLT path names: " << message;
      }
+
      if (throw_) {
        throw cms::Exception("Configuration")
-	 << " HLTHighLevel [instance: " << *moduleLabel()
-	 << " - path: " << *pathName()
-	 << "] configured with " << nbad
-	 << "/" << n
-	 << " unknown HLT path names: " << message
-	 << "\n";
-     }
-   }
-
-   // count number of requested known HLT paths which have fired
-   unsigned int fired(0);
-   for (unsigned int i=0; i!=n; i++) {
-     if (HLTPathsByIndex_[i]<trh->size()) {
-       if (trh->accept(HLTPathsByIndex_[i])) {
-	 fired++;
-       }
+         << " HLTHighLevel [instance: " << *moduleLabel()
+         << " - path: " << *pathName()
+         << "] configured with " << nbad
+         << "/" << n
+         << " unknown HLT path names: " << message;
      }
    }
 
    // Boolean filter result (always at least one trigger)
-   const bool accept( (fired>0) && ( andOr_ || (fired==n-nbad) ) );
-   LogDebug("") << "Accept = " << accept;
+   const bool accept( (fired > 0) and ( andOr_ or (fired == n-nbad) ) );
+   LogDebug("") << "Accept = " << std::boolalpha << accept;
 
    return accept;
+}
 
+// ----------------------------------------------------------------------------
+// static functions copied from EventSelector
+
+bool is_glob(const std::string & pattern)
+{
+  return (pattern.find_first_of("*?") != pattern.npos);
+}
+
+std::string glob2reg(const std::string & pattern) 
+{
+  std::string regexp = pattern;
+  boost::replace_all(regexp, "*", ".*");
+  boost::replace_all(regexp, "?", ".");
+  return regexp;
+}
+
+std::vector< std::vector<std::string>::const_iterator > 
+matching_triggers(const std::vector<std::string> & triggers, const std::string & pattern) 
+{
+  std::vector< std::vector<std::string>::const_iterator > matches;
+  boost::regex regexp( glob2reg(pattern) );
+  for (std::vector<std::string>::const_iterator i = triggers.begin(); i != triggers.end(); ++i)
+    if (boost::regex_match((*i), regexp)) 
+      matches.push_back(i);
+
+  return matches;
 }
