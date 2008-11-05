c664074dcf8aa3a2171b16403413673a3a3cfd1f
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2008-11-05 14:00:29 +0000

    new config for fed percentages

--- DQMServices/Components/python/DQMOfflineCosmics_Certification_cfi.py
+++ DQMServices/Components/python/DQMOfflineCosmics_Certification_cfi.py
@@ -5,5 +5,6 @@ DaqData = cms.EDFilter("DQMDaqInfo",
     saveDCFile = cms.untracked.bool(True),
     outputFile = cms.string('dqmDaqInfo.txt'),
     saveRootFile = cms.bool(True),
+    FedGranularityInput = cms.untracked.bool(False),
     OutputFileName = cms.string('DQMDAQDataCert.root')
 )
