ff0768090512b8a0a67494f7dd57b6be0765edcb
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-11-06 16:09:34 +0000

    rename the module

--- DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-resolutionAnalysisTest = cms.EDAnalyzer("DTResolutionAnalysisTest",
+dtResolutionAnalysisTest = cms.EDAnalyzer("DTResolutionAnalysisTest",
     folderRoot = cms.untracked.string(''),
     sigmaTestName = cms.untracked.string('ResidualsSigmaInRange'),
     meanTestName = cms.untracked.string('ResidualsMeanInRange'),
