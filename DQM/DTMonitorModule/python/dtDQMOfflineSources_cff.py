23cecdeb49eba87147db171ee0c50cbcc8008761
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-11-06 15:59:48 +0000

    temporarly remove efficiency and resolution and add fedintegrity

--- DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
+++ DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
@@ -9,4 +9,31 @@ dqmInfoDT = cms.EDFilter("DQMEventInfo",
                          subSystemFolder = cms.untracked.string('DT')
                          )
 
-dtSources = cms.Sequence(dtChamberEfficiencyMonitor*dtSegmentAnalysisMonitor*dtResolutionAnalysisMonitor*dqmInfoDT)
+
+dtDataIntegrityUnpacker = cms.EDProducer("DTUnpackingModule",
+    dqmOnly = cms.untracked.bool(True),
+    dataType = cms.string('DDU'),
+    useStandardFEDid = cms.untracked.bool(True),
+    fedbyType = cms.untracked.bool(True),
+    readOutParameters = cms.PSet(
+        debug = cms.untracked.bool(False),
+        rosParameters = cms.PSet(
+            writeSC = cms.untracked.bool(True),
+            readingDDU = cms.untracked.bool(True),
+            performDataIntegrityMonitor = cms.untracked.bool(True),
+            readDDUIDfromDDU = cms.untracked.bool(True),
+            debug = cms.untracked.bool(False),
+            localDAQ = cms.untracked.bool(False)
+        ),
+        localDAQ = cms.untracked.bool(False),
+        performDataIntegrityMonitor = cms.untracked.bool(True)
+    )
+)
+
+from DQM.DTMonitorModule.dtDataIntegrityTask_cfi import *
+DTDataIntegrityTask.hltMode = True
+
+dtSources = cms.Sequence(dtDataIntegrityUnpacker *
+                         dtSegmentAnalysisMonitor*
+                         dtResolutionAnalysisMonitor*
+                         dqmInfoDT)
