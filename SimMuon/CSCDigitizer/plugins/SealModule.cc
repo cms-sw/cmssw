e83b92628d824ab7fee01fa8416106b5d5f02f75
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-11-06 04:56:04 +0000

    Move CSCDigiSuppressor from L1Trigger area

--- SimMuon/CSCDigitizer/plugins/SealModule.cc
+++ SimMuon/CSCDigitizer/plugins/SealModule.cc
@@ -3,9 +3,10 @@
 #include "SimMuon/CSCDigitizer/src/CSCDigiProducer.h"
 #include "SimMuon/CSCDigitizer/src/CSCDigiDump.h"
 #include "SimMuon/CSCDigitizer/src/CSCNeutronWriter.h"
+#include "SimMuon/CSCDigitizer/src/CSCDigiSuppressor.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiProducer);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiDump);
 DEFINE_ANOTHER_FWK_MODULE(CSCNeutronWriter);
-
+DEFINE_ANOTHER_FWK_MODULE(CSCDigiSuppressor);
