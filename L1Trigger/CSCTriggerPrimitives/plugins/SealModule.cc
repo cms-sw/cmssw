f607d3cb299e58ad96465bae09f8d6c3fdc90e97
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-11-06 04:58:05 +0000

    Remove DigiSuppressor

--- L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
+++ L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
@@ -1,7 +1,5 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <L1Trigger/CSCTriggerPrimitives/plugins/CSCTriggerPrimitivesProducer.h>
-#include "L1Trigger/CSCTriggerPrimitives/src/CSCDigiSuppressor.h"
 
 DEFINE_FWK_MODULE(CSCTriggerPrimitivesProducer);
-DEFINE_ANOTHER_FWK_MODULE(CSCDigiSuppressor);
