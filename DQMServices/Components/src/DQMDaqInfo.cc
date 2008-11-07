8005a365357a77dee8c482f2ce8c28d0d733bd59
segoni <sha1-0240e2b42b3adb5cde27c1a9c4f3ed2291031e73@cern.ch>
2008-11-07 22:11:29 +0000

    removed root file svaing with DQMStore

--- DQMServices/Components/src/DQMDaqInfo.cc
+++ DQMServices/Components/src/DQMDaqInfo.cc
@@ -14,8 +14,6 @@ DQMDaqInfo::DQMDaqInfo(const edm::ParameterSet& iConfig)
   FedGranularity = iConfig.getUntrackedParameter("FedGranularityInput",false);
   
   /// Standard output root file 
-  saveData = iConfig.getParameter<bool>("saveRootFile");
-  if(saveData) outputFileName = iConfig.getParameter<std::string>("OutputFileName");
    
 }
 
@@ -29,19 +27,32 @@ DQMDaqInfo::~DQMDaqInfo()
 void DQMDaqInfo::beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, const  edm::EventSetup& iSetup){
   
   if(saveDCFile_)  dataCertificationFile<<"\n"<<  lumiBlock.id().run() <<"  |  "<<lumiBlock.luminosityBlock()  <<std::endl;
-
+  
  
    
-   if(FedGranularity){
+  if(FedGranularity){
     
     edm::eventsetup::EventSetupRecordKey recordKey2(edm::eventsetup::EventSetupRecordKey::TypeTag::findType("RunInfoRcd"));
-    if( recordKey2.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()) {
-      //record not found
-      std::cout <<"Record \"RunInfoRcd"<<"\" does not exist "<<std::endl;
+    if( recordKey2.type() == edm::eventsetup::EventSetupRecordKey::TypeTag() ) {
+
+     for(int detIndex=0; detIndex<7; ++detIndex) { 
+    	DaqFraction[detIndex]->Fill(-1);           
+      }
+      return;
     }
+    
+    
     edm::ESHandle<RunInfo> sumFED;
-    iSetup.get<RunInfoRcd>().get(sumFED);
-    const RunInfo* summaryFED=sumFED.product();
+    iSetup.get<RunInfoRcd>().get(sumFED);    
+	
+    if (!sumFED.isValid()) {
+      for(int detIndex=0; detIndex<7; ++detIndex) { 
+	DaqFraction[detIndex]->Fill(-1);           
+      }
+      return;
+    }
+
+    //const RunInfo* summaryFED=sumFED.product();
 
     std::vector<int> FedsInIds= sumFED->m_fed_in;   
     
@@ -58,65 +69,70 @@ void DQMDaqInfo::beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, con
 
     float  FedCount[7]={0.,0.,0.,0.,0.,0.,0.};
     float  NumberOfFeds[7];
-NumberOfFeds[Pixel]   = PixelRange.second-PixelRange.first;
-NumberOfFeds[SiStrip] = TrackerRange.second-TrackerRange.first;
-NumberOfFeds[CSC]     = CSCRange.second-CSCRange.first ;
-NumberOfFeds[RPC]     = RPCRange.second-RPCRange.first ;
-NumberOfFeds[DT]      = DTRange.second-DTRange.first;
-NumberOfFeds[ECAL]    = ECALRange.second-ECALRange.first ;
-NumberOfFeds[HCAL]    = HCALRange.second-HCALRange.first;
-   
-
-
-
-
-
-
-
- 
+    NumberOfFeds[Pixel]   = PixelRange.second-PixelRange.first;
+    NumberOfFeds[SiStrip] = TrackerRange.second-TrackerRange.first;
+    NumberOfFeds[CSC]     = CSCRange.second-CSCRange.first ;
+    NumberOfFeds[RPC]     = RPCRange.second-RPCRange.first ;
+    NumberOfFeds[DT]      = DTRange.second-DTRange.first;
+    NumberOfFeds[ECAL]    = ECALRange.second-ECALRange.first ;
+    NumberOfFeds[HCAL]    = HCALRange.second-HCALRange.first;
    
     for(unsigned int fedItr=0;fedItr<FedsInIds.size(); ++fedItr) {
-	int fedID=FedsInIds[fedItr];
-    	if(fedID>PixelRange.first   &  fedID<=PixelRange.second)    ++FedCount[Pixel]  ;
-    	if(fedID>TrackerRange.first &  fedID<=TrackerRange.second)  ++FedCount[SiStrip];
-    	if(fedID>CSCRange.first     &  fedID<=CSCRange.second)      ++FedCount[CSC]    ;
-    	if(fedID>RPCRange.first     &  fedID<=RPCRange.second)      ++FedCount[RPC]    ;
-    	if(fedID>DTRange.first      &  fedID<=DTRange.second)       ++FedCount[DT]     ;
-    	if(fedID>ECALRange.first    &  fedID<=ECALRange.second)     ++FedCount[ECAL]   ;
-    	if(fedID>HCALRange.first    &  fedID<=HCALRange.second)     ++FedCount[HCAL]	; 
+      int fedID=FedsInIds[fedItr];
+      if(fedID>PixelRange.first   &  fedID<=PixelRange.second)    ++FedCount[Pixel]  ;
+      if(fedID>TrackerRange.first &  fedID<=TrackerRange.second)  ++FedCount[SiStrip];
+      if(fedID>CSCRange.first     &  fedID<=CSCRange.second)      ++FedCount[CSC]    ;
+      if(fedID>RPCRange.first     &  fedID<=RPCRange.second)      ++FedCount[RPC]    ;
+      if(fedID>DTRange.first      &  fedID<=DTRange.second)       ++FedCount[DT]     ;
+      if(fedID>ECALRange.first    &  fedID<=ECALRange.second)     ++FedCount[ECAL]   ;
+      if(fedID>HCALRange.first    &  fedID<=HCALRange.second)     ++FedCount[HCAL]	; 
 	
     }   
     
     for(int detIndex=0; detIndex<7; ++detIndex) { 
-    	DaqFraction[detIndex]->Fill( FedCount[detIndex]/NumberOfFeds[detIndex]);
-    	if(saveDCFile_)  dataCertificationFile<<"subdet " << detIndex<< " "<<FedCount[detIndex]/NumberOfFeds[detIndex]<<std::endl;
+      DaqFraction[detIndex]->Fill( FedCount[detIndex]/NumberOfFeds[detIndex]);
+      if(saveDCFile_)  dataCertificationFile<<"subdet " << detIndex<< " "<<FedCount[detIndex]/NumberOfFeds[detIndex]<<std::endl;
        
-    
+      
     }
 
     
-    }else{
+  }else{
     
     
-  edm::eventsetup::EventSetupRecordKey recordKey(edm::eventsetup::EventSetupRecordKey::TypeTag::findType("RunSummaryRcd"));
-  edm::ESHandle<RunSummary> sum;
-  iSetup.get<RunSummaryRcd>().get(sum);
-  const RunSummary* summary=sum.product();
-  std::vector<int> SubDetId= summary->m_subdt_in;    
-  std::vector<std::string> subdet = summary->getSubdtIn();
-
-    
-  for(int det=0;det<7;++det) DaqFraction[det]->Fill(0.);
-  
-  
-  for (size_t itrSubDet=0; itrSubDet<subdet.size(); itrSubDet++){
-    if(saveDCFile_)  dataCertificationFile<<SubDetId[itrSubDet]<< " "<<subdet[itrSubDet] << std::endl;   
-    DaqFraction[SubDetId[itrSubDet]]->Fill(1.);
-  } 
-    
-    
-    
+    edm::eventsetup::EventSetupRecordKey recordKey(edm::eventsetup::EventSetupRecordKey::TypeTag::findType("RunSummaryRcd"));
+    if( recordKey.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()  ) {
+      for(int detIndex=0; detIndex<7; ++detIndex) { 
+    	DaqFraction[detIndex]->Fill(-1);           
+      }
+      return;
     }
+    
+    edm::ESHandle<RunSummary> sum;
+    iSetup.get<RunSummaryRcd>().get(sum);
+    
+    if (!sum.isValid()) {
+      for(int detIndex=0; detIndex<7; ++detIndex) { 
+    	DaqFraction[detIndex]->Fill(-1);           
+      }
+      return;
+    }
+    const RunSummary* summary=sum.product();
+    std::vector<int> SubDetId= summary->m_subdt_in;    
+    std::vector<std::string> subdet = summary->getSubdtIn();
+    
+    
+    for(int det=0;det<7;++det) DaqFraction[det]->Fill(0.);
+    
+    
+    for (size_t itrSubDet=0; itrSubDet<subdet.size(); itrSubDet++){
+      if(saveDCFile_)  dataCertificationFile<<SubDetId[itrSubDet]<< " "<<subdet[itrSubDet] << std::endl;   
+      DaqFraction[SubDetId[itrSubDet]]->Fill(1.);
+    } 
+    
+    
+    
+  }
 }
 
 
@@ -176,7 +192,6 @@ DQMDaqInfo::beginJob(const edm::EventSetup& iSetup)
 
 void 
 DQMDaqInfo::endJob() {
-  if(saveData) dbe_->save(outputFileName);
 }
 
 
@@ -184,6 +199,8 @@ DQMDaqInfo::endJob() {
 void
 DQMDaqInfo::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 { 
+ 
+
 }
 
 
