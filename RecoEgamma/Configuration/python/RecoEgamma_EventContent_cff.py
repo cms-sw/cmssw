c8374e6b99cf38c6ae1f6ebd497473142f2d099a
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-11-07 20:12:20 +0000

    clean up oboslete iso deposits

--- RecoEgamma/Configuration/python/RecoEgamma_EventContent_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_EventContent_cff.py
@@ -19,12 +19,6 @@ RecoEgammaFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_pixelMatchGsfElectrons_*_*', 
         'keep *_pixelMatchGsfFit_*_*', 
         'keep *_electronPixelSeeds_*_*',
-        'keep *_eleIsoFromDepsEcalFromHits_*_*',
-        'keep *_eleIsoFromDepsHcalFromHits_*_*',
-        'keep *_eleIsoFromDepsTk_*_*',
-        'keep *_eleIsoDepositEcalFromHits_*_*',
-        'keep *_eleIsoDepositHcalFromHits_*_*',
-        'keep *_eleIsoDepositTk_*_*',
         'keep *_eidRobustLoose_*_*',
         'keep *_eidRobustTight_*_*',
         'keep *_eidRobustHighEnergy_*_*',
@@ -34,13 +28,8 @@ RecoEgammaFEVT = cms.PSet(
         'keep *_photons_*_*', 
         'keep *_ckfOutInTracksFromConversions_*_*', 
         'keep *_ckfInOutTracksFromConversions_*_*',
-        'keep *_gamIsoFromDepsEcalFromHits_*_*',
-        'keep *_gamIsoFromDepsHcalFromHits_*_*',
-        'keep *_gamIsoFromDepsTk_*_*',
-        'keep *_gamIsoDepositEcalFromHits_*_*',
-        'keep *_gamIsoDepositHcalFromHits_*_*',
-        'keep *_gamIsoDepositTk_*_*',
-        'keep *_PhotonIDProd_*_*')                                                                 
+        'keep *_PhotonIDProd_*_*'
+ )                                                                 
             
 )
 # RECO content
@@ -51,12 +40,6 @@ RecoEgammaRECO = cms.PSet(
         'keep recoTrackExtras_pixelMatchGsfFit_*_*', 
         'keep TrackingRecHitsOwned_pixelMatchGsfFit_*_*', 
         'keep recoElectronPixelSeeds_electronPixelSeeds_*_*',
-        'keep doubleedmValueMap_eleIsoFromDepsEcalFromHits_*_*',
-        'keep doubleedmValueMap_eleIsoFromDepsHcalFromHits_*_*',
-        'keep doubleedmValueMap_eleIsoFromDepsTk_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositEcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositHcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositTk_*_*',
         'keep floatedmValueMap_eidRobustLoose_*_*',
         'keep floatedmValueMap_eidRobustTight_*_*',
         'keep floatedmValueMap_eidRobustHighEnergy_*_*',
@@ -70,22 +53,15 @@ RecoEgammaRECO = cms.PSet(
         'keep recoTrackExtras_ckfInOutTracksFromConversions_*_*', 
         'keep TrackingRecHitsOwned_ckfOutInTracksFromConversions_*_*', 
         'keep TrackingRecHitsOwned_ckfInOutTracksFromConversions_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsEcalFromHits_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsHcalFromHits_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsTk_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositEcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositHcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositTk_*_*',
-        'keep *_PhotonIDProd_*_*')                                                                 
+        'keep *_PhotonIDProd_*_*'
+                                           
+ )                                                                 
+
 )
 # AOD content
 RecoEgammaAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGsfElectrons_pixelMatchGsfElectrons_*_*', 
         'keep recoGsfTracks_pixelMatchGsfFit_*_*',
-        'keep doubleedmValueMap_eleIsoFromDepsTk_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositEcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositHcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_eleIsoDepositTk_*_*',
         'keep floatedmValueMap_eidRobustLoose_*_*',
         'keep floatedmValueMap_eidRobustTight_*_*',
         'keep floatedmValueMap_eidRobustHighEnergy_*_*',
@@ -95,14 +71,10 @@ RecoEgammaAOD = cms.PSet(
         'keep recoConversions_conversions_*_*', 
         'keep recoTracks_ckfOutInTracksFromConversions_*_*', 
         'keep recoTracks_ckfInOutTracksFromConversions_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositEcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositHcalFromHits_*_*',
-        'keep recoIsoDepositedmValueMap_gamIsoDepositTk_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsEcalFromHits_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsHcalFromHits_*_*',
-        'keep doubleedmValueMap_gamIsoFromDepsTk_*_*',
-        'keep *_PhotonIDProd_*_*')                                                                 
-                                           
+        'keep *_photonCutBasedIDLooseLabel_*_*',
+        'keep *_photonCutBasedIDTightLabel_*_*'
+        'keep *_PhotonIDProd_*_*'
+  )                                                                 
                                   
 )
 
