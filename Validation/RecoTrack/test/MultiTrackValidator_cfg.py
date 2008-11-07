8d3bd17d597fc5d934f101306ee197af8388116e
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2008-11-07 14:40:26 +0000

    commented message logger debug

--- Validation/RecoTrack/test/MultiTrackValidator_cfg.py
+++ Validation/RecoTrack/test/MultiTrackValidator_cfg.py
@@ -3,29 +3,31 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("MULTITRACKVALIDATOR")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.categories = ['TrackAssociator', 'TrackValidator']
-process.MessageLogger.debugModules = ['*']
-process.MessageLogger.cout = cms.untracked.PSet(
-    threshold = cms.untracked.string('DEBUG'),
-    default = cms.untracked.PSet(
-        limit = cms.untracked.int32(0)
-    ),
-    TrackAssociator = cms.untracked.PSet(
-        limit = cms.untracked.int32(0)
-    ),
-    TrackValidator = cms.untracked.PSet(
-        limit = cms.untracked.int32(-1)
-    )
-)
-process.MessageLogger.cerr = cms.untracked.PSet(
-    placeholder = cms.untracked.bool(True)
-)
+#process.MessageLogger.categories = ['TrackAssociator', 'TrackValidator']
+#process.MessageLogger.debugModules = ['*']
+#process.MessageLogger.cout = cms.untracked.PSet(
+#    threshold = cms.untracked.string('DEBUG'),
+#    default = cms.untracked.PSet(
+#        limit = cms.untracked.int32(0)
+#    ),
+#    TrackAssociator = cms.untracked.PSet(
+#        limit = cms.untracked.int32(0)
+#    ),
+#    TrackValidator = cms.untracked.PSet(
+#        limit = cms.untracked.int32(-1)
+#    )
+#)
+#process.MessageLogger.cerr = cms.untracked.PSet(
+#    placeholder = cms.untracked.bool(True)
+#)
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/tmp/cerati/TTbar.root')
+   fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_7/RelValTTbar/GEN-SIM-RECO/IDEAL_V9_v2/0002/56B92AB9-8B7E-DD11-A821-000423D6AF24.root')
+#    fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_8/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v1/0002/04983078-9082-DD11-BB8C-0019DB2F3F9B.root')
+#    fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_7/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_v1/0001/02F50799-FB7D-DD11-80FC-000423D98DC4.root')
 )
 
 process.load("SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi")
@@ -61,8 +63,8 @@ process.load("Validation.RecoTrack.MultiTrackValidator_cff")
 
 process.evtInfo = cms.OutputModule("AsciiOutputModule")
 
-#process.p = cms.Path(process.cutsTPEffic+process.cutsTPFake+process.cutsRecoTracks+process.multiTrackValidator)
-process.p = cms.Path(process.multiTrackValidator)
+process.p = cms.Path(process.cutsTPEffic+process.cutsTPFake+process.cutsRecoTracks+process.multiTrackValidator)
+#process.p = cms.Path(process.multiTrackValidator)
 process.ep = cms.EndPath(process.evtInfo)
 
 
