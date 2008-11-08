ebdb12844998100861122b728ad8130c86c9a754
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2008-11-08 21:19:35 +0000

    new L1 trigger object

---
+++ CondFormats/HcalObjects/src/T_EventSetup_HcalL1TriggerObjects.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/HcalObjects/interface/HcalL1TriggerObjects.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(HcalL1TriggerObjects);
