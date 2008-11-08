d1b1683578188c9510ba6e0e8a4576f1cdeec982
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2008-11-08 21:19:27 +0000

    for global tag

---
+++ CalibCalorimetry/HcalPlugins/python/Hcal_Conditions_forGlobalTag_cff.py
@@ -0,0 +1,10 @@
+hcal_db_producer = cms.ESProducer("HcalDbProducer",
+    dump = cms.untracked.vstring(''),
+    file = cms.untracked.string('')
+)
+
+es_hardcode = cms.ESSource("HcalHardcodeCalibrations",
+    toGet = cms.untracked.vstring('GainWidths', 
+        'ZSThresholds')
+)
+
