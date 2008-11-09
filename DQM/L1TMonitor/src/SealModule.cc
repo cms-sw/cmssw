4a7ed2f7e7de67023023dafa1cb40a75a65c335c
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-11-09 13:23:47 +0000

    fix

--- DQM/L1TMonitor/src/SealModule.cc
+++ DQM/L1TMonitor/src/SealModule.cc
@@ -4,10 +4,66 @@
 
 DEFINE_SEAL_MODULE();
 
+#include <DQM/L1TMonitor/interface/L1TLTC.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TLTC);
+
+#include <DQM/L1TMonitor/interface/L1TFED.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TFED);
+
+#include <DQM/L1TMonitor/interface/L1TCSCTPG.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TCSCTPG);
+
+#include <DQM/L1TMonitor/interface/L1TCSCTF.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TCSCTF);
+
+#include <DQM/L1TMonitor/interface/L1TDTTPG.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TDTTPG);
+
+#include <DQM/L1TMonitor/interface/L1TDTTF.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TDTTF);
+
+#include <DQM/L1TMonitor/interface/L1TRPCTPG.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TRPCTPG);
+
+#include <DQM/L1TMonitor/interface/L1TRPCTF.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TRPCTF);
+
+#include <DQM/L1TMonitor/interface/L1TGMT.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TGMT);
+
+#include <DQM/L1TMonitor/interface/L1TECALTPG.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TECALTPG);
+
+#include <DQM/L1TMonitor/interface/L1THCALTPG.h>
+DEFINE_ANOTHER_FWK_MODULE(L1THCALTPG);
+
+#include <DQM/L1TMonitor/interface/L1THCALTPGXAna.h>
+DEFINE_ANOTHER_FWK_MODULE(L1THCALTPGXAna);
+
+#include <DQM/L1TMonitor/interface/L1TGCT.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TGCT);
+
 #include <DQM/L1TMonitor/interface/L1TRCT.h>
 DEFINE_ANOTHER_FWK_MODULE(L1TRCT);
+
+#include <DQM/L1TMonitor/interface/L1TGT.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TGT);
+
+#include <DQM/L1TMonitor/interface/L1TCompare.h>
+DEFINE_ANOTHER_FWK_MODULE(L1TCompare);
+
+#include "DQM/L1TMonitor/interface/L1TDEMON.h"
+DEFINE_FWK_MODULE(L1TDEMON);
+
+#include "DQM/L1TMonitor/interface/L1TdeECAL.h"
+DEFINE_FWK_MODULE(L1TdeECAL);
+
+#include "DQM/L1TMonitor/interface/L1TdeGCT.h"
+DEFINE_FWK_MODULE(L1TdeGCT);
 
 #include "DQM/L1TMonitor/interface/L1TdeRCT.h"
 DEFINE_FWK_MODULE(L1TdeRCT);
 
+#include "DQM/L1TMonitor/interface/BxTiming.h"
+DEFINE_FWK_MODULE(BxTiming);
 
