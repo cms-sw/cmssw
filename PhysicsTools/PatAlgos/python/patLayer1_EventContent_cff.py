936a410385281f4e6c9a9851ed51acb869d2232c
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2008-11-09 20:46:27 +0000

    Event content brought in line with the first PAT-skim proposal.

--- PhysicsTools/PatAlgos/python/patLayer1_EventContent_cff.py
+++ PhysicsTools/PatAlgos/python/patLayer1_EventContent_cff.py
@@ -1,26 +1,27 @@
 # The following comments couldn't be translated into the new config version:
 
-# to be removed when no external MC ref's left
-#"keep recoTrackIPTagInfos_*_*_*",         // no longer needed (embedded by value)
-#"keep recoSoftLeptonTagInfos_*_*_*",  
-#"keep recoSecondaryVertexTagInfos_*_*_*",
-
 import FWCore.ParameterSet.Config as cms
 
 patLayer1EventContent = cms.PSet(
-    outputCommands = cms.untracked.vstring(
-        'keep edmTriggerResults_TriggerResults_*_*', 
-        'keep *_genParticles_*_*', 
-        'keep recoTracks_generalTracks_*_*', 
-        'keep *_offlinePrimaryVertices_*_*', 
-        'keep *_selectedLayer1Photons_*_*', 
-        'keep *_selectedLayer1Electrons_*_*', 
-        'keep *_selectedLayer1Muons_*_*', 
-        'keep *_selectedLayer1Taus_*_*', 
-        'keep *_selectedLayer1Jets_*_*', 
-        'keep *_selectedLayer1METs_*_*',
-        'keep patPFParticles_*_*_*',
-        'keep *_selectedLayer1Hemispheres_*_*'
-        )
-    )
+  outputCommands = cms.untracked.vstring(
+    'keep recoGenParticles_genParticles_*_*',
+    'keep *_genEventScale_*_*',
+    'keep *_genEventWeight_*_*',
+    'keep *_genEventPdfInfo_*_*',
+    'keep edmTriggerResults_TriggerResults_*_HLT', 
+    'keep *_hltTriggerSummaryAOD_*_*',
+    'keep *_offlineBeamSpot_*_*',
+    'keep *_offlinePrimaryVertices_*_*',
+    'keep recoTracks_generalTracks_*_*', 
+    'keep *_towerMaker_*_*',
+    'keep *_selectedLayer1Photons_*_*', 
+    'keep *_selectedLayer1Electrons_*_*', 
+    'keep *_selectedLayer1Muons_*_*', 
+    'keep *_selectedLayer1Taus_*_*', 
+    'keep *_selectedLayer1Jets_*_*', 
+    'keep *_selectedLayer1METs_*_*',
+    'keep patPFParticles_*_*_*',
+    'keep *_selectedLayer1Hemispheres_*_*'
+  )
+)
 
