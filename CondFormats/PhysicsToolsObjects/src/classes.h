b9b53dbd435e5201051052656869acd45735e7ff
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2008-11-10 16:02:52 +0000

    Add histogram 2D class.

--- CondFormats/PhysicsToolsObjects/src/classes.h
+++ CondFormats/PhysicsToolsObjects/src/classes.h
@@ -1,10 +1,11 @@
 #include "CondFormats/PhysicsToolsObjects/interface/Histogram.h"
+#include "CondFormats/PhysicsToolsObjects/interface/Histogram2D.h"
 #include "CondFormats/PhysicsToolsObjects/interface/MVAComputer.h"
 
 using namespace PhysicsTools::Calibration;
 
 namespace { // anonymous
-namespace {
+struct dictionary {
 
 #ifdef STD_DICTIONARIES_STUFF_MISSING
 std::vector<unsigned char> v1;
@@ -15,19 +16,23 @@ std::vector<std::string> v7;
 // Histogram
 HistogramF::Range v8;
 HistogramF v9;
-std::vector<HistogramF> v10;
-HistogramD::Range v11;
-HistogramD v12;
-std::vector<HistogramD> v13;
+HistogramF2D v10;
+std::vector<HistogramF> v11;
+HistogramD::Range v12;
+HistogramD v13;
+HistogramD2D v14;
+std::vector<HistogramD> v15;
+std::vector<HistogramF2D> v16;
+std::vector<HistogramD2D> v17;
 
 // MVAComputer
-std::vector<Variable> v14;
-std::vector<ProcCategory::BinLimits> v15;
-std::vector<ProcLikelihood::SigBkg> v16;
-std::pair<double, std::vector<double> > v17;
-std::pair<std::vector<ProcMLP::Neuron>, bool> v18;
-MVAComputerContainer::Entry v19;
-std::vector<MVAComputerContainer::Entry> v20;
+std::vector<Variable> v18;
+std::vector<ProcCategory::BinLimits> v19;
+std::vector<ProcLikelihood::SigBkg> v20;
+std::pair<double, std::vector<double> > v21;
+std::pair<std::vector<ProcMLP::Neuron>, bool> v22;
+MVAComputerContainer::Entry v23;
+std::vector<MVAComputerContainer::Entry> v24;
 
-}
+};
 } // anonymous namespace
