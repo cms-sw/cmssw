d7407693614a1e2f1b193192974b14b904b5ebc4
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2008-11-10 06:24:56 +0000

    silence printout (30x compatible version)

--- RecoMET/METAlgorithms/src/significanceAlgo.cc
+++ RecoMET/METAlgorithms/src/significanceAlgo.cc
@@ -4,8 +4,6 @@
 #include "TMath.h"
 #include "math.h"
 #include "TROOT.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
 // -*- C++ -*-
 //
 // Package:    METAlgorithms
@@ -21,7 +19,7 @@
 //
 // Original Author:  Kyle Story, Freya Blekman (Cornell University)
 //         Created:  Fri Apr 18 11:58:33 CEST 2008
-// $Id: significanceAlgo.cc,v 1.2 2008/10/23 20:14:06 fblekman Exp $
+// $Id: significanceAlgo.cc,v 1.1 2008/04/18 10:12:55 fblekman Exp $
 //
 //
 
@@ -47,8 +45,7 @@ metsig::ASignificance(const std::vector<SigInputObj>& EventVec, double &met_r, d
 {
   
   if(EventVec.size()<1) {
-    edm::LogWarning("SignCaloSpecificAlgo") << "Event Vector is empty!  Return significance -1";
-    return(-1);
+    return (-1.);
   }
   //=== Analytical Generation of Chisq Contours ===//
   double set_worker = 0;
@@ -94,22 +91,19 @@ metsig::ASignificance(const std::vector<SigInputObj>& EventVec, double &met_r, d
   met_r = sqrt(xmet*xmet + ymet*ymet);
   met_set = set_worker;
   //--- Ensure met_phi is in [-pi, pi] ---//
-
-//   double tmp_met_phi;
-//   if( (xmet) >=0.0 ) {
-//     tmp_met_phi = TMath::ATan( (ymet) / (xmet) );
-//   }
-//   else {
-//     if( (ymet) >=0.0 ) {
-//       tmp_met_phi = TMath::ATan(ymet/xmet) + TMath::Pi();
-//     }
-//     else{ // => ymet<0
-//       tmp_met_phi = TMath::ATan(ymet/xmet) - TMath::Pi();
-//     }
-//   }
-//   met_phi = tmp_met_phi;
-
-  met_phi= TMath::ATan2(ymet, xmet);
+  double tmp_met_phi;
+  if( (xmet) >=0.0 ) {
+    tmp_met_phi = TMath::ATan( (ymet) / (xmet) );
+  }
+  else {
+    if( (ymet) >=0.0 ) {
+      tmp_met_phi = TMath::ATan(ymet/xmet) + TMath::Pi();
+    }
+    else{ // => ymet<0
+      tmp_met_phi = TMath::ATan(ymet/xmet) - TMath::Pi();
+    }
+  }
+  met_phi = tmp_met_phi;
   
   //--- Calculate Significance ---//
   v_tot.Invert();
