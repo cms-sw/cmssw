0953c55883ecf084755e1e0bc75b81503d2d98cf
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-11-10 09:57:24 +0000

    booking converters added

---
+++ DQM/CSCMonitorModule/interface/CSCDQM_Collection.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CSCDQM_Collection.h
+ *
+ *    Description:  Histogram Collection management class
+ *
+ *        Version:  1.0
+ *        Created:  10/30/2008 04:40:38 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Valdas Rapsevicius (VR), valdas.rapsevicius@cern.ch
+ *        Company:  CERN, CH
+ *
+ * =====================================================================================
+ */
+
+#ifndef CSCDQM_Collection_H
+#define CSCDQM_Collection_H
+
+namespace cscdqm {
+
+class Collection {
+
+
+
+};
+
+
+};
+
+#endif
