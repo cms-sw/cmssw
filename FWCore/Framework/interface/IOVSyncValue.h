c10eb186dcbc41920b6434cf4c55e1254e0b8f8c
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2008-11-10 22:51:37 +0000

    Fix redundant argument names in function declaration, flagged by gcc4.3.2

--- FWCore/Framework/interface/IOVSyncValue.h
+++ FWCore/Framework/interface/IOVSyncValue.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Wed Aug  3 18:35:24 EDT 2005
-// $Id: IOVSyncValue.h,v 1.6 2007/03/04 06:00:22 wmtan Exp $
+// $Id: IOVSyncValue.h,v 1.7 2008/08/11 22:51:44 chrjones Exp $
 //
 
 // system include files
@@ -38,7 +38,7 @@ class IOVSyncValue
       //virtual ~IOVSyncValue();
       explicit IOVSyncValue(const EventID& iID, LuminosityBlockNumber_t iLumi=0);
       explicit IOVSyncValue(const Timestamp& iTime);
-      IOVSyncValue(const EventID& iID, LuminosityBlockNumber_t iLumi, const Timestamp& iID);
+      IOVSyncValue(const EventID& iID, LuminosityBlockNumber_t iLumi, const Timestamp& iTime);
 
       // ---------- const member functions ---------------------
       const EventID& eventID() const { return eventID_;}
