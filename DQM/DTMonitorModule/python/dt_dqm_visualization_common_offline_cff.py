a3efb38f8eb02a9241a026e94b85cd40d9c95622
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-11-10 17:07:58 +0000

    CRAFT global tag

--- DQM/DTMonitorModule/python/dt_dqm_visualization_common_offline_cff.py
+++ DQM/DTMonitorModule/python/dt_dqm_visualization_common_offline_cff.py
@@ -42,7 +42,7 @@ from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
-GlobalTag.globaltag = "CRUZET4_V5P::All"
+GlobalTag.globaltag = "CRAFT_V3P::All"
 
 
 
