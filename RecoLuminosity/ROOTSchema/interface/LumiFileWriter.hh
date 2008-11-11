7cb526eed61ebcb0496f06f66f78301bb916162e
Adam Paul Hunt <sha1-2759bd6d16112107bec24bba4874010a9c4c834c@cern.ch>
2008-11-11 14:19:39 +0000

    Removed test programs.  Updates for 3_0_X.

--- RecoLuminosity/ROOTSchema/interface/LumiFileWriter.hh
+++ RecoLuminosity/ROOTSchema/interface/LumiFileWriter.hh
@@ -2,6 +2,7 @@
 #define __LUMIFILEWRITER_H__
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
+#include <string>
 
 namespace HCAL_HLX{
   class TCPReceiver;
@@ -10,27 +11,29 @@ namespace HCAL_HLX{
 }
 
 class LumiFileWriter : public edm::EDAnalyzer {
-   public:
-      explicit LumiFileWriter(const edm::ParameterSet&);
-      ~LumiFileWriter();
-
-
-   private:
-      virtual void beginJob(const edm::EventSetup&) ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-
- 
-      HCAL_HLX::TCPReceiver*      HLXTCP_;  
-      HCAL_HLX::LUMI_SECTION*     lumiSection_;
-      HCAL_HLX::ROOTSchema*       LumiSchema_;
-
-      unsigned int reconTime;
-
-      bool bMerge_;
-      bool bWBM_;
-      bool bTransfer_;
-      bool bTest_;
+public:
+  explicit LumiFileWriter(const edm::ParameterSet&);
+  ~LumiFileWriter();
+  
+  
+private:
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  
+  std::string DistribIP1;
+  std::string DistribIP2;
+  
+  HCAL_HLX::TCPReceiver*      HLXTCP_;  
+  HCAL_HLX::LUMI_SECTION*     lumiSection_;
+  HCAL_HLX::ROOTSchema*       LumiSchema_;
+  
+  unsigned int reconTime;
+  
+  bool bMerge_;
+  bool bWBM_;
+  bool bTransfer_;
+  bool bTest_;
 
 };
 
