88a5b1f3e23c991a38d5bbd5763f4a23fb3431c7
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2008-11-12 18:19:24 +0000

    Moved SiPixelCPEGeneric Error Parametrization db utilities to CondTools/SiPixel

--- CalibTracker/SiPixelErrorEstimation/plugins/Modules.cc
+++
@@ -1,6 +0,0 @@
-#include "CalibTracker/SiPixelErrorEstimation/interface/PxCPEdbUploader.h"
-#include "CalibTracker/SiPixelErrorEstimation/interface/PxCPEdbReader.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-DEFINE_ANOTHER_FWK_MODULE(PxCPEdbUploader);
-DEFINE_ANOTHER_FWK_MODULE(PxCPEdbReader);
