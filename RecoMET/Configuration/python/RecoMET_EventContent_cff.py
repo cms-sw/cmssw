34abcdc3cdd0c5704fbc69e37fc68f3a39cbbbdd
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-11-13 17:52:01 +0000

    Commented out lines that keep METSignificance as a separate caloMET collection

--- RecoMET/Configuration/python/RecoMET_EventContent_cff.py
+++ RecoMET/Configuration/python/RecoMET_EventContent_cff.py
@@ -8,23 +8,32 @@ import FWCore.ParameterSet.Config as cms
 # Full Event content 
 RecoMETFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
+                                           'keep recoMETs_*_*_*',
+                                           'keep recoPFMETs_*_*_*',
+                                           #'keep recoCaloMETSignifs_*_*_*'
+                                           )
 )
 RecoGenMETFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
 )
 # RECO content
 RecoMETRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*',
+                                           'keep recoMETs_*_*_*',
+                                           'keep recoPFMETs_*_*_*',
+                                           #'keep recoCaloMETSignifs_*_*_*'
+                                           )
 )
 RecoGenMETRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
 )
 # AOD content
 RecoMETAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*', 
-        'keep recoMETs_*_*_*','keep recoCaloMETSignifs_*_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCaloMETs_*_*_*',
+                                           'keep recoMETs_*_*_*',
+                                           'keep recoPFMETs_*_*_*',
+                                           #'keep recoCaloMETSignifs_*_*_*'
+                                           )
 )
 RecoGenMETAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoGenMETs_*_*_*')
