381f099e303185e62035a21aba3dada1b4505c11
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2008-11-13 10:45:34 +0000

    Load DQMOffline.JetMET.dataCertificationJetMET_cfi

--- DQMOffline/JetMET/test/datacertificationjetmet_cfg.py
+++ DQMOffline/JetMET/test/datacertificationjetmet_cfg.py
@@ -20,13 +20,7 @@ process.dqmInfoJetMET = cms.EDFilter("DQMEventInfo",
                                          subSystemFolder = cms.untracked.string('JetMET')
                                      )
 
-process.demo = cms.EDAnalyzer('DataCertificationJetMET',
-                              fileName       = cms.untracked.string("/uscms/home/chlebana/DQM_V0001_R000063463__BeamHalo__BeamCommissioning08-PromptReco-v1__RECO.root"),
-                              refFileName    = cms.untracked.string("/uscms/home/chlebana/DQM_V0001_R000063463__BeamHalo__BeamCommissioning08-PromptReco-v1__RECO.root"),
-                              OutputFile     = cms.untracked.bool(False),
-                              OutputFileName = cms.untracked.string("DQMResult.root"),
-                              Verbose        = cms.untracked.int32(0),
-                              TestType       = cms.untracked.int32(0)
-)
+process.load("DQMOffline.JetMET.dataCertificationJetMET_cfi")
+#process.dataCertificationJetMET.filename = '/uscms/home/chlebana/DQM_V0001_R000063463__BeamHalo__BeamCommissioning08-PromptReco-v1__RECO.root'
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.dqmInfoJetMET*process.dataCertificationJetMET)
