b7fb6300e52f67203cf35005daa3a0a928df1d06
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2008-11-13 11:35:35 +0000

    replaced old lable ttSemiHypothesisMaxSumPtWMass by new lable ttSemiLepHypMaxSumPtWMass for hypoKey (mainly to avoid confusion, this lable from the cfi is replaced in the cff anyway)

--- TopQuarkAnalysis/Examples/python/HypothesisAnalyzer_cfi.py
+++ TopQuarkAnalysis/Examples/python/HypothesisAnalyzer_cfi.py
@@ -4,8 +4,8 @@ import FWCore.ParameterSet.Config as cms
 # module to make simple analyses of top event hypothese
 #
 analyzeHypothesis = cms.EDAnalyzer("HypothesisAnalyzer",
-    hypoKey = cms.InputTag("ttSemiHypothesisMaxSumPtWMass","Key"),
-    semiLepEvent = cms.InputTag("ttSemiLepEvent")
+    semiLepEvent = cms.InputTag("ttSemiLepEvent"),
+    hypoKey      = cms.InputTag("ttSemiLepHypMaxSumPtWMass","Key")
 )
 
 
