e3d4655a8b351c8a4193d01663f71245e2ec5e8b
Ian Tomalin <sha1-3fa43f967df83df8ba717ce5a271c8449420497d@cern.ch>
2008-11-14 17:52:59 +0000

    Added file to merge optional large impact-parameter tracking iterations

---
+++ RecoTracker/FinalTrackSelectors/python/MergeLargeD0TrackCollections_cff.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# Merge standard tracking with additional, optional large impact-parameter tracking iterations
+#
+
+import RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi
+# Track filtering and quality.
+#   input:    generalTracks,largeD0step1WithMaterialTracks,largeD0step2WithMaterialTracks,largeD0step3WithMaterialTracks,largeD0step4WithMaterialTracks,largeD0step5WithMaterialTracks
+#   output:   mergeLargeD0step5Step
+#   sequence: largeD0TrackCollectionMerging
+
+#
+mergeLargeD0step1 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
+mergeLargeD0step1.TrackProducer1 = 'generalTracks'
+mergeLargeD0step1.TrackProducer2 = 'largeD0step1WithMaterialTracks'
+mergeLargeD0step1.promoteTrackQuality = True
+
+#
+mergeLargeD0step2 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
+mergeLargeD0step2.TrackProducer1 = 'mergeLargeD0step1'
+mergeLargeD0step2.TrackProducer2 = 'largeD0step2WithMaterialTracks'
+mergeLargeD0step2.promoteTrackQuality = True
+
+#
+mergeLargeD0step3 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
+mergeLargeD0step3.TrackProducer1 = 'mergeLargeD0step2'
+mergeLargeD0step3.TrackProducer2 = 'largeD0step3WithMaterialTracks'
+mergeLargeD0step3.promoteTrackQuality = True
+
+#
+mergeLargeD0step4 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
+mergeLargeD0step4.TrackProducer1 = 'mergeLargeD0step3'
+mergeLargeD0step4.TrackProducer2 = 'largeD0step4WithMaterialTracks'
+mergeLargeD0step4.promoteTrackQuality = True
+
+#
+mergeLargeD0step5 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
+mergeLargeD0step5.TrackProducer1 = 'mergeLargeD0step4'
+mergeLargeD0step5.TrackProducer2 = 'largeD0step5WithMaterialTracks'
+mergeLargeD0step5.promoteTrackQuality = True
+
+largeD0TrackCollectionMerging = cms.Sequence(mergeLargeD0step1 * mergeLargeD0step2 * mergeLargeD0step3 * mergeLargeD0step4 * mergeLargeD0step5) 
