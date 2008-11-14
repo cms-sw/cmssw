d9ba357ef0d4bf64a1d24a9fe9f288a1d4bb2558
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2008-11-14 15:06:16 +0000

    remove track refitter

--- Validation/RecoTrack/python/SiTrackingRecHitsValid_cff.py
+++ Validation/RecoTrack/python/SiTrackingRecHitsValid_cff.py
@@ -3,7 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from RecoLocalTracker.SiStripRecHitConverter.StripCPE_cfi import *
 from Validation.RecoTrack.SiPixelTrackingRecHitsValid_cfi import *
 from Validation.RecoTrack.SiStripTrackingRecHitsValid_cfi import *
-from RecoTracker.TrackProducer.RefitterWithMaterial_cff import *
-trackingRecHitsValid = cms.Sequence(TrackRefitter*PixelTrackingRecHitsValid*StripTrackingRecHitsValid)
-TrackRefitter.TrajectoryInEvent = True
+trackingRecHitsValid = cms.Sequence(PixelTrackingRecHitsValid*StripTrackingRecHitsValid)
 
