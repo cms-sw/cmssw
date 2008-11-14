71bd2a6c09e3daffdc44ea05f58314687cec907e
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2008-11-14 10:22:53 +0000

    dqmSaver options

--- Validation/RecoMuon/test/EDMtoMEConverter_cfg.py
+++ Validation/RecoMuon/test/EDMtoMEConverter_cfg.py
@@ -19,8 +19,14 @@ process.source = cms.Source("PoolSource",
 )
 
 process.DQMStore.referenceFileName = ""
+process.DQMStore.collateHistograms = False
 
 process.dqmSaver.convention = "Offline"
+#Settings equivalent to 'RelVal' convention:
+process.dqmSaver.saveByRun = cms.untracked.int32(-1)
+process.dqmSaver.saveAtJobEnd = cms.untracked.bool(True)
+process.dqmSaver.forceRunNumber = cms.untracked.int32(1)
+#End of 'RelVal convention settings
 process.dqmSaver.workflow = "/GlobalValidation/Test/RECO"
 
 process.p1 = cms.Path(process.EDMtoMEConverter*
