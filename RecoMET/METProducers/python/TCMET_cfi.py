28999938eb2188dd15b176b2da5dd4d44f5fd28c
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-11-14 21:55:56 +0000

    Added a TCMET module to make Track Corrected MET

--- RecoMET/METProducers/python/TCMET_cfi.py
+++ RecoMET/METProducers/python/TCMET_cfi.py
@@ -1,19 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 # File: TCMET.cff
-# Author: R.Remington
-# Date: 11.12.2008
+# Author: R. Remington & F. Golf 
+# Date: 11.14.2008
 #
 # Form Track Corrected MET
 
-tcMet = cms.EDProducer("METProducer",
-    src = cms.InputTag("towerMaker"),
+tcMet = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("towerMaker"), #This parameter does not get used for TCMET
     METType = cms.string('MET'),
     alias = cms.string('TCMET'),
     noHF = cms.bool(False),
     globalThreshold = cms.double(0.0),
-    InputType = cms.string('CandidateCollection')
-)
+    InputType = cms.string('CaloMET:Electron:Muon:Track'),  #This parameter does not get used for TCMET  
+    electronLabel = cms.InputTag("pixelMatchGsfElectrons"),
+    muonLabel     = cms.InputTag("muons"),
+    trackLabel    = cms.InputTag("tracks"),
+    metLabel      = cms.InputTag("met")
+    )
 
 
 
