7844340f7117b962e45105f39835b291c06537ad
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-11-16 16:43:01 +0000

    First impementation of tcMetAnalyzer module for validation purposes

---
+++ Validation/RecoMET/python/TCMET_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+# File: TCMET.cfi
+# Author: R. Remington
+# Date: 11.14.2008
+#
+# Fill validation histograms for MET.
+
+tcMetAnalyzer = cms.EDAnalyzer(
+    "METTester",
+    OutputFile = cms.untracked.string('output.root'),
+    InputMETLabel = cms.InputTag("tcMet"),
+    METType = cms.untracked.string('MET')
+   ) 
+
+
