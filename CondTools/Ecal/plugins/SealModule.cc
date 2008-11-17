712964aff7027297be173ec18761f4a722d6c0a0
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-11-17 16:33:18 +0000

    moved all plugin definition to "plugins"

--- CondTools/Ecal/plugins/SealModule.cc
+++ CondTools/Ecal/plugins/SealModule.cc
@@ -1,6 +1,15 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "CondTools/Ecal/plugins/StoreEcalCondition.h"
+#include "CondTools/Ecal/interface/EcalDBCopy.h"
+#include "CondTools/Ecal/interface/EcalTestDevDB.h"
+#include "CondTools/Ecal/interface/EcalGetLaserData.h"
+
+#include "CondCore/PopCon/interface/PopConAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(StoreEcalCondition);
+DEFINE_ANOTHER_FWK_MODULE(EcalDBCopy);
+DEFINE_ANOTHER_FWK_MODULE(EcalTestDevDB);
+DEFINE_ANOTHER_FWK_MODULE(EcalGetLaserData);
+
