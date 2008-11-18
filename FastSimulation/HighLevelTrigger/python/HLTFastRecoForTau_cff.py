36f48df82fbfa5cc76378e0d942ae0ee1f7ce0fa
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2008-11-18 14:55:35 +0000

    changes from Simone in the tau

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
@@ -4,48 +4,17 @@ import FWCore.ParameterSet.Config as cms
 from FastSimulation.Tracking.PixelVerticesProducer_cff import *
 # (Not-so) Regional Tracking
 from FastSimulation.Tracking.GlobalPixelTracking_cff import *
-# L2.5 pixel-seeded tracks for SingleTau(MET) collections (pT>5GeV/c)
-hltCtfWithMaterialTracksL25ElectronTau = cms.EDFilter("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
-    ptMin = cms.untracked.double(5.0)
-)
 
 # L3 pixel-seeded tracks for Single Tau Collection (pT>1GeV/c)
-hltCtfWithMaterialTracksL3SingleTau = cms.EDFilter("FastTrackMerger",
+hltL3TauCtfWithMaterialTracks = cms.EDFilter("FastTrackMerger",
     SaveTracksOnly = cms.untracked.bool(True),
     TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
     ptMin = cms.untracked.double(1.0)
 )
 
-hltCtfWithMaterialTracksL3SingleTauRelaxed = cms.EDFilter("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
-    ptMin = cms.untracked.double(1.0)
-)
-
-hltCtfWithMaterialTracksL3SingleTauMET = cms.EDFilter("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
-    ptMin = cms.untracked.double(1.0)
-)
-
-hltCtfWithMaterialTracksL3SingleTauMETRelaxed = cms.EDFilter("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
-    ptMin = cms.untracked.double(1.0)
-)
-
-hltCtfWithMaterialTracksL3ElectronTau = cms.EDFilter("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks")),
-    ptMin = cms.untracked.double(1.0)
-)
 
 #--- Fastsim sequences replacing modules in tau paths ---#
-hltCkfTrackCandidatesL3SingleTau = cms.Sequence(globalPixelTracking)
-hltCkfTrackCandidatesL3SingleTauRelaxed = cms.Sequence(globalPixelTracking)
-hltCkfTrackCandidatesL3SingleTauMET = cms.Sequence(globalPixelTracking)
-hltCkfTrackCandidatesL3SingleTauMETRelaxed = cms.Sequence(globalPixelTracking)
+#hltL3TauCkfTrackCandidates = cms.Sequence(globalPixelTracking)
+HLTL3TauTrackReconstructionSequence = cms.Sequence(globalPixelTracking + hltL3TauCtfWithMaterialTracks)
 
 
