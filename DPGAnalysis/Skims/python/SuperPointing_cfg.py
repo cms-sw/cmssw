64496d374762ac0edbfcafe4d1841407f14680b0
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2008-11-18 22:44:31 +0000

    change datatier to raw+reco and input files

--- DPGAnalysis/Skims/python/SuperPointing_cfg.py
+++ DPGAnalysis/Skims/python/SuperPointing_cfg.py
@@ -3,14 +3,18 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TEST")
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('file:reco_50908_210_CRZT210_V1P.root'),
-                            secondaryFileNames = cms.untracked.vstring('file:raw_50908_210_CRZT210_V1P.root')                            
-                            )
+                            fileNames = cms.untracked.vstring(
+       '/store/data/Commissioning08/Cosmics/RECO/v1/000/067/122/D424EBA5-55A0-DD11-A8BF-000423D9853C.root',
+       '/store/data/Commissioning08/Cosmics/RECO/v1/000/067/122/C67EDF0D-49A0-DD11-9403-001617DBD332.root'),
+                            secondaryFileNames = cms.untracked.vstring(
+       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/6E2601EC-3FA0-DD11-BA50-000423D986A8.root',
+       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/C240B0B2-47A0-DD11-A6AD-001617C3B654.root') 
+)                            
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.7 $'),
+    version = cms.untracked.string('$Revision: 1.9 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/SuperPointing_cfg.py,v $'),
-    annotation = cms.untracked.string('CRUZET4 SuperPointing skim')
+    annotation = cms.untracked.string('CRAFT SuperPointing skim')
 )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
@@ -20,7 +24,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'CRZT210_V1::All' 
+process.GlobalTag.globaltag = 'CRAFT_V4P::All' 
 process.prefer("GlobalTag")
 
 process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
@@ -103,6 +107,7 @@ process.ctfWithMaterialTracksP5Path = cms.Path(process.ctfWithMaterialTracksP5Fi
 
 
 process.out = cms.OutputModule("PoolOutputModule",
+                               outputCommands = cms.untracked.vstring('keep *'),
                                SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('cosmicMuonsBarrelOnlyPath',
                                                                                             'cosmicMuonsPath',
                                                                                             'cosmicMuons1LegBarrelOnlyPath',
@@ -113,7 +118,7 @@ process.out = cms.OutputModule("PoolOutputModule",
                                                                                             'globalCosmicMuons1LegBarrelOnlyPath',
                                                                                             'ctfWithMaterialTracksP5Path')),                               
                                dataset = cms.untracked.PSet(
-			                 dataTier = cms.untracked.string('RECO'),
+			                 dataTier = cms.untracked.string('RAW+RECO'),
                                          filterName = cms.untracked.string('SuperPointing')),
                                fileName = cms.untracked.string('superPointing.root')
                                )
