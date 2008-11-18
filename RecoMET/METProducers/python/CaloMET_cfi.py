6d3fb60d931e6e3f8e59f3940bb729f2700a24ec
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-11-18 01:17:33 +0000

    Added CaloMETSignificance input parameters

--- RecoMET/METProducers/python/CaloMET_cfi.py
+++ RecoMET/METProducers/python/CaloMET_cfi.py
@@ -12,57 +12,118 @@ import FWCore.ParameterSet.Config as cms
 # Additional modules available for MET Reconstruction using towers w/wo HO included
 
 
-met = cms.EDProducer("METProducer",
-                         src = cms.InputTag("towerMaker"),
-                         METType = cms.string('CaloMET'),
-                         alias = cms.string('RawCaloMET'),
-                         noHF = cms.bool(False),
-                         geomCut = cms.bool(False), ## make geometry cuts on input objects
-                         globalThreshold = cms.double(0.5),
-                         InputType = cms.string('CandidateCollection')
-                     )
+met = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("towerMaker"),
+    METType = cms.string('CaloMET'),
+    alias = cms.string('RawCaloMET'),
+    noHF = cms.bool(False),
+    globalThreshold = cms.double(0.5),
+    InputType = cms.string('CandidateCollection'),
+    
+    #For METSignificance
+    #ECAL:
+    EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EB_PhiResPar = cms.vdouble(0.00502),    # 0.0174/sqrt(12)
+    EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    #HCAL:
+    HB_EtResPar = cms.vdouble(0.,1.22,0.05),
+    HB_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HE_EtResPar = cms.vdouble(0.,1.3,0.05),
+    HE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HO_EtResPar = cms.vdouble(0.,1.3,0.005),
+    HO_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HF_EtResPar = cms.vdouble(0.,1.82,0.09),
+    HF_PhiResPar = cms.vdouble(0.05022)     # 0.174/sqrt(12)    
+    )
 
 metHO = met.clone()
 metHO.src = "towerMakerWithHO"
 metHO.alias = 'RawCaloMETHO'
 
-metOpt = cms.EDProducer("METProducer",
-                            src = cms.InputTag("calotoweroptmaker"),
-                            METType = cms.string('CaloMET'),
-                            alias = cms.string('RawCaloMETOpt'),
-                            noHF = cms.bool(False),
-                            geomCut = cms.bool(False), ## make geometry cuts on input objects
-                            globalThreshold = cms.double(0.0),
-                            InputType = cms.string('CandidateCollection')
-                        )
+metOpt = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("calotoweroptmaker"),
+    METType = cms.string('CaloMET'),
+    alias = cms.string('RawCaloMETOpt'),
+    noHF = cms.bool(False),
+    globalThreshold = cms.double(0.0),
+    InputType = cms.string('CandidateCollection'),
+
+    #ECAL:
+    EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EB_PhiResPar = cms.vdouble(0.00502),    # 0.0174/sqrt(12)
+    EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    #HCAL:
+    HB_EtResPar = cms.vdouble(0.,1.22,0.05),
+    HB_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HE_EtResPar = cms.vdouble(0.,1.3,0.05),
+    HE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HO_EtResPar = cms.vdouble(0.,1.3,0.005),
+    HO_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HF_EtResPar = cms.vdouble(0.,1.82,0.09),
+    HF_PhiResPar = cms.vdouble(0.05022)     # 0.174/sqrt(12)    
+    )
 
 metOptHO = metOpt.clone()
 metOptHO.src = "calotoweroptmakerWithHO"
 metOptHO.alias = 'RawCaloMETOptHO'
 
-metNoHF = cms.EDProducer("METProducer",
-                             src = cms.InputTag("towerMaker"),
-                             METType = cms.string('CaloMET'),
-                             alias = cms.string('RawCaloMETNoHF'),
-                             noHF = cms.bool(True),
-                             geomCut = cms.bool(False), ## make geometry cuts on input objects
-                             globalThreshold = cms.double(0.5),
-                             InputType = cms.string('CandidateCollection')
-                         )
+metNoHF = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("towerMaker"),
+    METType = cms.string('CaloMET'),
+    alias = cms.string('RawCaloMETNoHF'),
+    noHF = cms.bool(True),
+    globalThreshold = cms.double(0.5),
+    InputType = cms.string('CandidateCollection'),
+
+    #ECAL:
+    EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EB_PhiResPar = cms.vdouble(0.00502),    # 0.0174/sqrt(12)
+    EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    #HCAL:
+    HB_EtResPar = cms.vdouble(0.,1.22,0.05),
+    HB_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HE_EtResPar = cms.vdouble(0.,1.3,0.05),
+    HE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HO_EtResPar = cms.vdouble(0.,1.3,0.005),
+    HO_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HF_EtResPar = cms.vdouble(0.,1.82,0.09),
+    HF_PhiResPar = cms.vdouble(0.05022)     # 0.174/sqrt(12)    
+    )
 
 metNoHFHO = metNoHF.clone()
 metNoHFHO.src = "towerMakerWithHO"
 metNoHFHO.alias = 'RawCaloMETNoHFHO'
 
-metOptNoHF = cms.EDProducer("METProducer",
-                                src = cms.InputTag("calotoweroptmaker"),
-                                METType = cms.string('CaloMET'),
-                                alias = cms.string('RawCaloMETOptNoHF'),
-                                noHF = cms.bool(True),
-                                geomCut = cms.bool(False), ## make geometry cuts on input objects
-                                globalThreshold = cms.double(0.0),
-                                InputType = cms.string('CandidateCollection')
-                            )
+metOptNoHF = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("calotoweroptmaker"),
+    METType = cms.string('CaloMET'),
+    alias = cms.string('RawCaloMETOptNoHF'),
+    noHF = cms.bool(True),
+    globalThreshold = cms.double(0.0),
+    InputType = cms.string('CandidateCollection'),
+
+    #ECAL:
+    EB_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EB_PhiResPar = cms.vdouble(0.00502),    # 0.0174/sqrt(12)
+    EE_EtResPar = cms.vdouble(0.2,0.03,0.005),
+    EE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    #HCAL:
+    HB_EtResPar = cms.vdouble(0.,1.22,0.05),
+    HB_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HE_EtResPar = cms.vdouble(0.,1.3,0.05),
+    HE_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HO_EtResPar = cms.vdouble(0.,1.3,0.005),
+    HO_PhiResPar = cms.vdouble(0.02511),    # 0.087/sqrt(12)
+    HF_EtResPar = cms.vdouble(0.,1.82,0.09),
+    HF_PhiResPar = cms.vdouble(0.05022)     # 0.174/sqrt(12)    
+    )
 
 metOptNoHFHO = metOptNoHF.clone()
 metOptNoHFHO.src = "calotoweroptmakerWithHO"
