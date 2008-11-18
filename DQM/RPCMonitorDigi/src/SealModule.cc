5604e6c69890da315cc2772895e89b0ce01cb08f
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2008-11-18 23:58:26 +0000

    Cleaning Old Packages

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -1,6 +1,5 @@
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorSync.h"
-#include "DQM/RPCMonitorDigi/interface/RPCMonitorEfficiency.h"
 #include "DQM/RPCMonitorDigi/interface/MuonSegmentEff.h"
 //#include "DQM/RPCMonitorDigi/interface/RPCReadoutError.h"
 
@@ -9,6 +8,5 @@
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorDigi);
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorSync);
-DEFINE_ANOTHER_FWK_MODULE(RPCMonitorEfficiency);
 DEFINE_ANOTHER_FWK_MODULE(MuonSegmentEff);
 //DEFINE_ANOTHER_FWK_MODULE(RPCReadoutError);
