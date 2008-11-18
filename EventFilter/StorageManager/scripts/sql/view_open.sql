08356624666806974128cfca560c95f5b9a842f3
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2008-11-18 11:05:05 +0000

    Updated to use files_info table

--- EventFilter/StorageManager/scripts/sql/view_open.sql
+++ EventFilter/StorageManager/scripts/sql/view_open.sql
@@ -1,23 +1,18 @@
-
 create or replace view view_open as
 SELECT 'Number of open files' AS DESCRIPTION,
        TO_CHAR ( COUNT ( * ) ) AS COUNT
-  FROM FILES_CREATED 
- where not exists ( SELECT * from FILES_INJECTED where FILES_CREATED.FILENAME = FILES_INJECTED.FILENAME)
-   AND PRODUCER='StorageManager'
- UNION SELECT 'Oldest open file Start Time' AS DESCRIPTION,
-       TO_CHAR ( MIN ( CTIME ) ,
+  FROM FILES_INFO where state=0
+        and (CAST( CREATED_TIME as DATE) - sysdate)*86400 > -604800
+ UNION SELECT 'Oldest open file start time' AS DESCRIPTION,
+       TO_CHAR ( MIN ( CREATED_TIME ) ,
 		 'YYYY/MM/DD HH24:MI' ) AS COUNT
-  FROM FILES_CREATED 
- where not exists ( SELECT * from FILES_INJECTED where FILES_CREATED.FILENAME = FILES_INJECTED.FILENAME)
-   AND PRODUCER='StorageManager'
- UNION SELECT 'Oldest open file Run Number' AS DESCRIPTION,
+  FROM FILES_INFO where state=0
+        and (CAST( CREATED_TIME as DATE) - sysdate)*86400 > -604800
+ UNION SELECT 'Oldest open file run number' AS DESCRIPTION,
        TO_CHAR ( RUNNUMBER ) AS COUNT
-  FROM FILES_CREATED 
-  where CTIME = ( SELECT MIN ( CTIME )
-                   FROM FILES_CREATED 
-		   where not exists ( SELECT * from FILES_INJECTED where FILES_CREATED.FILENAME = FILES_INJECTED.FILENAME) 
-                      AND PRODUCER='StorageManager' )
-   AND PRODUCER='StorageManager';
-
+  FROM FILES_INFO where 
+        CREATED_TIME = ( SELECT MIN ( CREATED_TIME )
+                        FROM FILES_INFO where state=0 
+                and (CAST( CREATED_TIME as DATE) - sysdate)*86400 > -604800
+        );
 grant select on view_open to public;
