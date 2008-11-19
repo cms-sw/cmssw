20c69c80d0ba97ab5a17d33b394acdf889f6c049
jmuelmen <sha1-19c609fe3a4daa353fc8169ef2702fa017104e96@cern.ch>
2008-11-19 17:26:15 +0000

    switch to eve calo lego, with issues left to iron out

--- Fireworks/Core/interface/ElectronDetailView.h
+++ Fireworks/Core/interface/ElectronDetailView.h
@@ -4,7 +4,7 @@
 //
 // Package:     Calo
 // Class  :     ElectronDetailView
-//
+// 
 /**\class ElectronDetailView ElectronDetailView.h Fireworks/Calo/interface/ElectronDetailView.h
 
  Description: <one line class summary>
@@ -14,7 +14,7 @@
 
 */
 //
-// Original Author:
+// Original Author:  
 //         Created:  Sun Jan  6 23:42:33 EST 2008
 // $Id: ElectronDetailView.h,v 1.4 2008/07/01 05:03:25 jmuelmen Exp $
 //
@@ -26,6 +26,7 @@
 // user include files
 #include "TEveBoxSet.h"
 #include "Fireworks/Core/interface/FWDetailView.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 
 // forward declarations
 
@@ -36,11 +37,11 @@ namespace reco {
 }
 
 class ElectronDetailView : public FWDetailView {
-
+     
 public:
      ElectronDetailView();
      virtual ~ElectronDetailView();
-
+     
      virtual void build (TEveElementList **product, const FWModelId &id);
 
 protected:
@@ -54,35 +55,36 @@ protected:
 	vars[2] = rotation_center[2];
      }
      TEveElementList *makeLabels (const reco::GsfElectron &);
-     TEveElementList *getEcalCrystalsBarrel (const class DetIdToMatrix &,
-					     const std::vector<class DetId> &);
-     TEveElementList *getEcalCrystalsBarrel (const class DetIdToMatrix &,
-					     double eta, double phi,
-					     int n_eta = 5, int n_phi = 10);
-     TEveElementList *getEcalCrystalsEndcap (const class DetIdToMatrix &,
-					     const std::vector<class DetId> &);
-     TEveElementList *getEcalCrystalsEndcap (const class DetIdToMatrix &,
-					     double x, double y, int iz,
-					     int n_x = 5, int n_y = 5);
+//      TEveElementList *getEcalCrystalsBarrel (const class DetIdToMatrix &,
+// 					     const std::vector<class DetId> &);
+//      TEveElementList *getEcalCrystalsBarrel (const class DetIdToMatrix &,
+// 					     double eta, double phi,
+// 					     int n_eta = 5, int n_phi = 10);
+//      TEveElementList *getEcalCrystalsEndcap (const class DetIdToMatrix &,
+// 					     const std::vector<class DetId> &);
+//      TEveElementList *getEcalCrystalsEndcap (const class DetIdToMatrix &,
+// 					     double x, double y, int iz,
+// 					     int n_x = 5, int n_y = 5);
+     void fillData (const std::vector<DetId> &, class TEveCaloDataVec *);
 
 private:
      ElectronDetailView(const ElectronDetailView&); // stop default
      const ElectronDetailView& operator=(const ElectronDetailView&); // stop default
-
+     
      // ---------- member data --------------------------------
      const FWEventItem* m_item;
-     void resetCenter() {
+     void resetCenter() { 
 	rotation_center[0] = 0;
 	rotation_center[1] = 0;
 	rotation_center[2] = 0;
      }
-
-};
-
-class FWBoxSet : public TEveBoxSet {
-public:
-     FWBoxSet (const Text_t *n = "FWBoxSet", const Text_t *t = "")
-	  : TEveBoxSet(n, t) { fBoxType = kBT_AABox; }
+     const EcalRecHitCollection *barrel_hits;
+     const EcalRecHitCollection *endcap_hits;
+     std::vector<DetId> seed_detids;
+     double x_min;
+     double x_max;
+     double y_min;
+     double y_max;
 };
 
 #endif
