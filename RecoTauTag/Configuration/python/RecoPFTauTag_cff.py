8bf87f1a276eb0c77318dca560c6649270f5d77e
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-11-19 09:48:37 +0000

    adding missing discriminators in the sequences

--- RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
+++ RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
@@ -7,27 +7,37 @@ from RecoJets.JetAssociationProducers.ic5PFJetTracksAssociatorAtVertex_cfi impor
 from RecoTauTag.RecoTau.PFRecoTauTagInfoProducer_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauProducer_cfi import *
 
+#Discriminators
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByIsolation_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackFinding_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackPtCut_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingPionPtCut_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolation_cfi import *
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolationUsingLeadingPion_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationByECALIsolation_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationAgainstElectron_cfi import *
 from RecoTauTag.RecoTau.PFRecoTauDiscriminationAgainstMuon_cfi import *
 
+#Discriminators using leading Pion
+from RecoTauTag.RecoTau.PFRecoTauDiscriminationByIsolationUsingLeadingPion_cfi import *
+from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingPionPtCut_cfi import *
+from RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolationUsingLeadingPion_cfi import *
+from RecoTauTag.RecoTau.PFRecoTauDiscriminationByECALIsolationUsingLeadingPion_cfi import *
+
+
+
+#HighEfficiency collections
 from RecoTauTag.RecoTau.PFRecoTauHighEfficiency_cff import *
 
 
 PFTauDiscrimination = cms.Sequence(
     pfRecoTauDiscriminationByIsolation*
+    pfRecoTauDiscriminationByIsolationUsingLeadingPion*
     pfRecoTauDiscriminationByLeadingTrackFinding*
     pfRecoTauDiscriminationByLeadingTrackPtCut*
     pfRecoTauDiscriminationByLeadingPionPtCut*
     pfRecoTauDiscriminationByTrackIsolation*
     pfRecoTauDiscriminationByTrackIsolationUsingLeadingPion*
     pfRecoTauDiscriminationByECALIsolation*
+    pfRecoTauDiscriminationByECALIsolationUsingLeadingPion*
     pfRecoTauDiscriminationAgainstElectron*
     pfRecoTauDiscriminationAgainstMuon
 )
@@ -35,12 +45,14 @@ PFTauDiscrimination = cms.Sequence(
 
 PFTauDiscriminationHighEfficiency = cms.Sequence(
     pfRecoTauDiscriminationByIsolationHighEfficiency*
+    pfRecoTauDiscriminationByIsolationUsingLeadingPionHighEfficiency*
     pfRecoTauDiscriminationByLeadingTrackFindingHighEfficiency*
     pfRecoTauDiscriminationByLeadingTrackPtCutHighEfficiency*
     pfRecoTauDiscriminationByLeadingPionPtCutHighEfficiency*
     pfRecoTauDiscriminationByTrackIsolationHighEfficiency*
     pfRecoTauDiscriminationByTrackIsolationUsingLeadingPionHighEfficiency*
     pfRecoTauDiscriminationByECALIsolationHighEfficiency*
+    pfRecoTauDiscriminationByECALIsolationUsingLeadingPionHighEfficiency*
     pfRecoTauDiscriminationAgainstElectronHighEfficiency*
     pfRecoTauDiscriminationAgainstMuonHighEfficiency
 )
