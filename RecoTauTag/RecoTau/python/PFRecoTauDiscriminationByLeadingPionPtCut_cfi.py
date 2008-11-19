0b0fdcc522f21391b1cf74baee2981b37f5a61e9
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2008-11-19 09:17:24 +0000

    trivial bug fix

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByLeadingPionPtCut_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByLeadingPionPtCut_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pfRecoTauDiscriminationByLeadingTrackPtCut = cms.EDFilter("PFRecoTauDiscriminationByLeadingTrackPtCut",
+pfRecoTauDiscriminationByLeadingTrackPtCut = cms.EDFilter("PFRecoTauDiscriminationByLeadingPionPtCut",
     PFTauProducer = cms.InputTag('pfRecoTauProducer'),
     MinPtLeadingPion = cms.double(5.0)
 )
