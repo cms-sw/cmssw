c6afb4861b225c1a9153917f117b017b7b9748aa
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2008-11-19 14:33:23 +0000

    quick fix for missing parameter

--- L1TriggerConfig/RPCTriggerConfig/python/RPCHwConfigSource_cfi.py
+++ L1TriggerConfig/RPCTriggerConfig/python/RPCHwConfigSource_cfi.py
@@ -1,13 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 l1RPCHwConfig = cms.ESProducer("RPCTriggerHwConfig",
+    disableTowers = cms.vint32(),
     disableCrates = cms.vint32(),
+    disableTowersInCrates = cms.vint32(),
     enableTowers = cms.vint32(),
     enableCrates = cms.vint32(),
     disableAll = cms.bool(False),
     lastBX = cms.int32(0),
-    firstBX = cms.int32(0),
-    disableTowers = cms.vint32()
+    firstBX = cms.int32(0)
 )
 
 rpchwconfsrc = cms.ESSource("EmptyESSource",
