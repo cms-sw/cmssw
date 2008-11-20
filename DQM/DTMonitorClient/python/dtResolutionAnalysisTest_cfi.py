c23a52fb225bc18214b005d975953cd65c8357ab
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-11-20 09:16:41 +0000

    removed obsolete parameters

--- DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 dtResolutionAnalysisTest = cms.EDAnalyzer("DTResolutionAnalysisTest",
     folderRoot = cms.untracked.string(''),
-    sigmaTestName = cms.untracked.string('ResidualsSigmaInRange'),
-    meanTestName = cms.untracked.string('ResidualsMeanInRange'),
-    diagnosticPrescale = cms.untracked.int32(1)
+    diagnosticPrescale = cms.untracked.int32(1),
+    permittedMeanRange = cms.untracked.double(0.0005),
+    permittedSigmaRange = cms.untracked.double(0.001)
  )
 
 
--- DQM/DTMonitorModule/python/dtResolutionTask_cfi.py
+++ DQM/DTMonitorModule/python/dtResolutionTask_cfi.py
@@ -1,15 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 dtResolutionAnalysisMonitor = cms.EDAnalyzer("DTResolutionAnalysisTask",
-    # switch for verbosity
-    debug = cms.untracked.bool(False),
     # labels of 4D and 1D hits
     recHits4DLabel = cms.string('dt4DSegments'),
     recHitLabel = cms.string('dt1DRecHits'),
     # interval of lumi block after which we reset the histos
-    ResetCycle = cms.untracked.int32(10000),
-    # switch for sector summaries                                         
-    doSectorSummaries = cms.untracked.bool(False)
+    ResetCycle = cms.untracked.int32(10000)
 )
 
 
