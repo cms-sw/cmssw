2fc60094e43651f70fcaf97d4a381f352a6100be
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-11-20 11:29:29 +0000

    fix on test range

--- DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 dtResolutionAnalysisTest = cms.EDAnalyzer("DTResolutionAnalysisTest",
     folderRoot = cms.untracked.string(''),
     diagnosticPrescale = cms.untracked.int32(1),
-    permittedMeanRange = cms.untracked.double(0.0005),
-    permittedSigmaRange = cms.untracked.double(0.001)
+    permittedMeanRange = cms.untracked.double(0.01),
+    permittedSigmaRange = cms.untracked.double(0.1)
  )
 
 
