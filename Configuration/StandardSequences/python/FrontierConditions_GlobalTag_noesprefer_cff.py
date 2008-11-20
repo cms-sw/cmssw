430f095f8ffd7ecf7375bfcdd72781ffc8c53c13
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-11-20 22:15:17 +0000

    add fake sources for SiPixelCPEGenericErrorParm and SiPixelTemplateDBObject

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_noesprefer_cff.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_noesprefer_cff.py
@@ -10,3 +10,5 @@ from CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi import 
 from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_nofakes_cff import *
 from CalibCalorimetry.HcalPlugins.Hcal_Conditions_forGlobalTag_cff import *
 from L1Trigger.Configuration.L1Trigger_FakeConditions_cff import *
+from CalibTracker.SiPixelESProducers.SiPixelFakeCPEGenericErrorParmESSource_cfi import *
+from CalibTracker.SiPixelESProducers.SiPixelFakeTemplateDBObjectESSource_cfi import *
