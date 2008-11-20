54a6f92bcff7982906428eaad4d3ae220838d00a
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-11-20 10:33:33 +0000

    add egamma postvalidation to sequence

--- HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
+++ HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
@@ -1,6 +1,8 @@
 from HLTriggerOffline.Tau.Validation.HLTTauPostValidation_cfi import *
 from HLTriggerOffline.Muon.PostProcessor_cfi import *
+from HLTriggerOffline.Egamma.EgammaPostProcessor_cfi import *
 hltpostvalidation = cms.Sequence( 
     HLTMuonPostVal
     +HLTTauPostVal
+    +EgammaPostVal
     )
