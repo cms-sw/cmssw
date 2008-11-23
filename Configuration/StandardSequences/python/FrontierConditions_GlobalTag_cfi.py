0b3377780586058448b4110c2d0348cd744b0df0
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-11-23 11:12:24 +0000

    use sqlite_fip:CondCore/TagCollection/data/GlobalTag.db as default connect string for release integration

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
@@ -4,8 +4,8 @@ from CondCore.DBCommon.CondDBSetup_cfi import *
 
 GlobalTag = cms.ESSource("PoolDBESSource",
     CondDBSetup,
-    connect = cms.string('frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'), ##FrontierProd/CMS_COND_21X_GLOBALTAG"
-
-    globaltag = cms.string('IDEAL_V1::All'),
+#    connect = cms.string('frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'),
+    connect = cms.string('sqlite_fip:CondCore/TagCollection/data/GlobalTag.db'), #For use during release integration
+    globaltag = cms.string('IDEAL_30X::All'),
     BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService')
 )
