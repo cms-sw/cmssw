6747d495c2e4a37507d747eebd3056a28fe255d3
Ivan Mikulec <sha1-8d77057744976da3e68d7e182804494ea5541838@cern.ch>
2008-11-24 19:00:38 +0000

    add online producers

--- L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
+++ L1TriggerConfig/L1ScalesProducers/src/SealModule.cc
@@ -4,7 +4,9 @@
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTrivialProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1CaloInputScalesProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1MuTriggerScalesProducer.h"
+#include "L1TriggerConfig/L1ScalesProducers/interface/L1MuTriggerScalesOnlineProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1MuTriggerPtScaleProducer.h"
+#include "L1TriggerConfig/L1ScalesProducers/interface/L1MuTriggerPtScaleOnlineProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1MuGMTScalesProducer.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1ScalesTester.h"
 #include "L1TriggerConfig/L1ScalesProducers/interface/L1MuScalesTester.h"
@@ -15,6 +17,8 @@ DEFINE_FWK_EVENTSETUP_MODULE(L1ScalesTrivialProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1CaloInputScalesProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuTriggerScalesProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuTriggerPtScaleProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuTriggerScalesOnlineProducer);
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuTriggerPtScaleOnlineProducer);
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(L1MuGMTScalesProducer);
 
 DEFINE_ANOTHER_FWK_MODULE(L1ScalesTester);
