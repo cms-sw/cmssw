07868760214d9b84a6ad154fce77f56bfee22682
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-11-24 08:07:01 +0000

    vertex reco moved before jets and PFMET added

--- Configuration/StandardSequences/python/Reconstruction_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_cff.py
@@ -40,9 +40,11 @@ globalreco = cms.Sequence(offlineBeamSpot+recopixelvertexing*ckftracks+ecalClust
 globalreco_plusRS = cms.Sequence(globalreco*rstracks)
 globalreco_plusGSF = cms.Sequence(globalreco*GsfGlobalElectronTestSequence)
 globalreco_plusRS_plusGSF = cms.Sequence(globalreco*rstracks*GsfGlobalElectronTestSequence)
-highlevelreco = cms.Sequence(vertexreco*recoJetAssociations*btagging*tautagging*egammarecoFull*particleFlowReco*recoPFJets*PFTau)
+highlevelreco = cms.Sequence(recoJetAssociations*btagging*tautagging*egammarecoFull*particleFlowReco*recoPFJets*recoPFMET*PFTau)
 #emergency sequence wo conversions
-highlevelreco_woConv = cms.Sequence(vertexreco*recoJetAssociations*btagging*tautagging*egammareco_woConvPhotons*particleFlowReco*recoPFJets*PFTau)
+highlevelreco_woConv = cms.Sequence(recoJetAssociations*btagging*tautagging*egammareco_woConvPhotons*particleFlowReco*recoPFJets*recoPFMET*PFTau)
+
+
 #
 # "Export" Section
 #
