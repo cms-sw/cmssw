316853a16f0a09308b774d6081fc842f7b3e21ea
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-11-24 18:55:20 +0000

    remove un-neeeded fake sources

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
@@ -4,13 +4,9 @@ from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
 
 # the following are needed for non PoolDBESSources
 
-from CalibMuon.DTCalibration.DTFakeVDriftESProducer_cfi import *
-from CalibMuon.CSCCalibration.CSC_BadChambers_cfi import *
 from CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi import *
 from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_cff import *
 from CalibCalorimetry.HcalPlugins.Hcal_Conditions_forGlobalTag_cff import *
 from L1Trigger.Configuration.L1Trigger_FakeConditions_cff import *
-from CalibTracker.SiPixelESProducers.SiPixelFakeCPEGenericErrorParmESSource_cfi import *
-from CalibTracker.SiPixelESProducers.SiPixelFakeTemplateDBObjectESSource_cfi import *
 
 es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
