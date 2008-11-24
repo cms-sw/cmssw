5791e1fd39cf7f966340143aa8f7de3794dc516b
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2008-11-24 19:03:33 +0000

    Add EventSetup data registration for histogram classes. ----------------------------------------------------------------------

---
+++ CondFormats/PhysicsToolsObjects/src/T_EventSetup_Histogram.cc
@@ -0,0 +1,10 @@
+#include "CondFormats/PhysicsToolsObjects/interface/Histogram.h"
+#include "CondFormats/PhysicsToolsObjects/interface/Histogram2D.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+using namespace PhysicsTools::Calibration;
+
+EVENTSETUP_DATA_REG(HistogramF);
+EVENTSETUP_DATA_REG(HistogramD);
+EVENTSETUP_DATA_REG(HistogramF2D);
+EVENTSETUP_DATA_REG(HistogramD2D);
