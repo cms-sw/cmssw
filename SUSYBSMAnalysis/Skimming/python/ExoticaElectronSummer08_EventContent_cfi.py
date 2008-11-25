f6330a64d90b897abe6d26919088d177daffe6ad
Jie Chen <sha1-fd9183ca9be87d876e001fd5ccf1f012da0983ea@cern.ch>
2008-11-25 16:15:19 +0000

    Exotica electron skim for summer08 MC production

---
+++ SUSYBSMAnalysis/Skimming/python/ExoticaElectronSummer08_EventContent_cfi.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.EventContent.EventContent_cff import *
+
+
+exoticaEOutputModule = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring(),
+    SelectEvents = cms.untracked.PSet(
+       SelectEvents = cms.vstring("exoticaESkimPath") #the selector name to be same as the path name  in cfg.py
+      ),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('ExoticaE'),
+        dataTier = cms.untracked.string('USER')
+    ),
+    fileName = cms.untracked.string('exoticaetest.root')
+  )
+
+#default output content AODSIMEventContent or AODEventContent
+exoticaEOutputModule.outputCommands.extend(RECOEventContent.outputCommands)
+
+#add specifi content you need. 
+SpecifiedEvenetContent=cms.PSet(
+    outputCommands = cms.untracked.vstring(
+      "keep *_exoticaERecoQalityCut_*_*"
+      )
+    )
+exoticaEOutputModule.outputCommands.extend(SpecifiedEvenetContent.outputCommands)
+
+
+
