11865107cc355684684883e5aefdc51c0e55b4e9
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-11-25 11:18:25 +0000

    added new parameters for summary tests

--- DQMOffline/Muon/python/muonTestSummary_cff.py
+++ DQMOffline/Muon/python/muonTestSummary_cff.py
@@ -1,11 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-muTestSummary = cms.EDFilter("MuonTestSummary",
-                             #chose between "ppLike" or "cosmics"
-                             dataSample = cms.untracked.string('cosmics'),
+muonTestSummary = cms.EDFilter("MuonTestSummary",
                              # tests parameters
+                             etaExpected = cms.double(0.9),
+                             phiExpected = cms.double(0.01),
                              etaSpread = cms.double(0.4), 
                              phiSpread = cms.double(0.4),
                              chi2Fraction = cms.double(0.3),
-                             chi2Spread = cms.double(0.1)
+                             chi2Spread = cms.double(0.1),
+                             resEtaSpread_tkGlb = cms.double(0.001),
+                             resEtaSpread_glbSta = cms.double(0.05),
+                             resPhiSpread_tkGlb = cms.double(0.001),
+                             resPhiSpread_glbSta = cms.double(0.05),
+                             numMatchedExpected = cms.double(4.0),
+                             sigmaResSegmTrackExp = cms.double(1.0)
                              )
