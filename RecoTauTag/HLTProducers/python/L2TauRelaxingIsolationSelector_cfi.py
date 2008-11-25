be47dafa8d995ce0512186ebe1b6c59e3b446757
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2008-11-25 19:55:19 +0000

    Adding cfi file for relaxing selector

---
+++ RecoTauTag/HLTProducers/python/L2TauRelaxingIsolationSelector_cfi.py
@@ -0,0 +1,11 @@
+hltL2TauRelaxingIsolationSelector = cms.EDProducer( "L2TauRelaxingIsolationSelector",
+                                                L2InfoAssociation = cms.InputTag( 'hltL2TauIsolationProducer','L2TauIsolationInfoAssociator' ),
+                                                EcalIsolationEt = cms.vdouble( 5.0,0.025,0.0),
+                                                TowerIsolationEt = cms.vdouble( 1000.0,0.0,0.0 ),
+                                                ClusterEtaRMS = cms.vdouble( 1000.0,0.0,0.0),
+                                                ClusterPhiRMS = cms.vdouble( 1000.0,0.0,0.0),
+                                                ClusterDRRMS = cms.vdouble( 1000,0.0,0.0),
+                                                NumberOfClusters=cms.vdouble( 1000.0,0.0,0.0),
+                                                MinJetEt = cms.double( 0.0),
+                                                SeedTowerEt = cms.double( -10.0 )
+                                            )
