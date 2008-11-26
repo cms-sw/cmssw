77684073f0542e84dadf9ad825ca5164056857bc
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-11-26 13:15:26 +0000

    added optimized interface (not yet used by default

--- CondFormats/SiStripObjects/interface/SiStripFedCabling.h
+++ CondFormats/SiStripObjects/interface/SiStripFedCabling.h
@@ -319,6 +319,199 @@ inline SiStripFedCabling::ConnsRange SiStripFedCabling::range( SiStripFedCabling
 // -------------------------------------------------------------
 
 
+class SiStripFedCabling;
+
+/** Debug info for SiStripFedCabling class. */
+std::ostream& operator<< ( std::ostream&, const SiStripFedCabling& );
+
+/** 
+    \class SiStripFedCabling 
+    \brief Contains cabling info at the device level, including
+    DetId, APV pair numbers, hardware addresses, DCU id...
+*/
+class SiStripFedCabling {
+  
+ public:
+  
+  // -------------------- Typedefs and structs --------------------
+
+  typedef std::vector<uint16_t> Feds;
+
+  typedef Feds::iterator FedsIter;
+
+  typedef Feds::const_iterator FedsConstIter;
+
+  typedef boost::iterator_range<FedsIter> FedsIterRange;
+
+  typedef boost::iterator_range<FedsConstIter> FedsConstIterRange;
+
+  typedef std::vector<FedChannelConnection> Conns;
+
+  typedef Conns::iterator ConnsIter;
+
+  typedef Conns::const_iterator ConnsConstIter;
+  
+  typedef boost::iterator_range<ConnsIter> ConnsIterRange;
+
+  typedef boost::iterator_range<ConnsConstIter> ConnsConstIterRange;
+
+  typedef std::pair<uint32_t,uint32_t> ConnsPair;
+  
+  typedef std::vector<ConnsPair> Registry;
+  
+  // -------------------- Constructors, destructors --------------------
+
+  /** Constructor taking FED channel connection objects as input. */
+  SiStripFedCabling( ConnsConstIterRange );
+
+  /** Copy constructor. */
+  SiStripFedCabling( const SiStripFedCabling& ); 
+
+  /** Public default constructor. */
+  SiStripFedCabling();
+
+  /** Default destructor. */
+  ~SiStripFedCabling();
+
+  // -------------------- Methods to retrieve connections --------------------
+
+  /** Retrieve vector of active FED ids. */
+  FedsConstIterRange feds() const;
+  
+  /** Returns all connection info for a given FED id. */
+  ConnsConstIterRange connections( uint16_t fed_id ) const; 
+  
+  /** Returns Connection info for a given FED id and channel. */
+  FedChannelConnection connection( uint16_t fed_id, uint16_t fed_ch ) const; 
+  
+  /** Returns information for "detected, but unconnected" devices. */
+  ConnsConstIterRange detected() const; 
+  
+  /** Returns information for all "undetected" devices. */
+  ConnsConstIterRange undetected() const; 
+
+  // -------------------- Utility methods --------------------
+  
+  /** Builds FED cabling from vector of FED connections. */
+  void buildFedCabling( ConnsConstIterRange connections );
+  
+  /** Prints all connection information for this FED cabling object. */
+  void print( std::stringstream& ) const;
+  
+  /** Prints terse information for this FED cabling object. */
+  void terse( std::stringstream& ) const;
+  
+  /** Prints summary information for this FED cabling object. */
+  void summary( std::stringstream& ) const;
+
+  /// Builds range of iterators from pair of offsets
+  class ConnsRange {
+
+  public:
+    
+    ConnsRange( const Conns&, ConnsPair );
+    ConnsRange( const Conns& );
+    ConnsRange() {;}
+    
+    ConnsConstIter begin() const;
+    ConnsConstIter end() const;
+    ConnsConstIterRange range() const;
+    ConnsConstIterRange invalid() const;
+    
+    bool empty() const;
+    uint32_t size() const;
+
+    ConnsPair connsPair() const;
+    static ConnsPair emptyPair();
+
+    void print( std::stringstream& ) const;
+    
+  private:
+    
+    ConnsConstIterRange vector_;
+    ConnsConstIterRange range_;
+
+  };
+
+  /// Builds range of iterators from pair of offsets
+  ConnsRange range( ConnsPair ) const;
+  
+  // -------------------- Private member data --------------------
+
+ private:
+
+  /// "Active" FEDs that have connected FE devices
+  Feds feds_;
+  
+  /// Container of "ranges" indexed by FED id 
+  Registry registry_;
+  
+  /// Container of connection objects 
+  Conns connections_;
+
+  /// Connections to FE devices that are not detected
+  Conns detected_;
+  
+  /// FE devices that are detected
+  Conns undetected_;
+
+};
+
+std::ostream& operator<<( std::ostream&, const SiStripFedCabling::ConnsRange& );
+
+inline SiStripFedCabling::FedsConstIterRange SiStripFedCabling::feds() const {
+  return FedsConstIterRange( feds_.begin(), feds_.end() );
+}
+
+inline SiStripFedCabling::ConnsConstIterRange SiStripFedCabling::detected() const { 
+  return ConnsConstIterRange( detected_.begin(), detected_.end() ); 
+}
+
+inline SiStripFedCabling::ConnsConstIterRange SiStripFedCabling::undetected() const{ 
+  return ConnsConstIterRange( undetected_.begin(), undetected_.end() ); 
+}
+
+inline SiStripFedCabling::ConnsConstIter SiStripFedCabling::ConnsRange::begin() const { 
+  return range_.begin(); 
+}
+
+inline SiStripFedCabling::ConnsConstIter SiStripFedCabling::ConnsRange::end() const { 
+  return range_.end(); 
+}
+
+inline SiStripFedCabling::ConnsConstIterRange SiStripFedCabling::ConnsRange::range() const { 
+  return range_;
+}
+
+inline SiStripFedCabling::ConnsConstIterRange SiStripFedCabling::ConnsRange::invalid() const { 
+  return ConnsConstIterRange( vector_.end(), vector_.end() );
+}
+
+inline bool SiStripFedCabling::ConnsRange::empty() const { 
+  return ( range_.begin() == range_.end() );
+}
+
+inline uint32_t SiStripFedCabling::ConnsRange::size() const { 
+  return std::distance( range_.begin(), range_.end() );
+}
+
+inline SiStripFedCabling::ConnsPair SiStripFedCabling::ConnsRange::connsPair() const { 
+  return ( ( range_.begin() == vector_.end() && 
+	     range_.end()   == vector_.end() ) ?
+	   ConnsPair( sistrip::invalid32_, sistrip::invalid32_ ) :
+	   ConnsPair( std::distance( vector_.begin(), range_.begin() ), 
+		      std::distance( vector_.begin(), range_.end() ) ) );
+}
+
+inline SiStripFedCabling::ConnsPair SiStripFedCabling::ConnsRange::emptyPair() { 
+  return ConnsPair( sistrip::invalid32_, sistrip::invalid32_ );
+}
+
+inline SiStripFedCabling::ConnsRange SiStripFedCabling::range( SiStripFedCabling::ConnsPair p ) const {
+  return ConnsRange( connections_, p );
+}
+
+
 // -------------------------------------------------------------
 #endif // SISTRIPCABLING_USING_NEW_INTERFACE -------------------
 #endif // SISTRIPCABLING_USING_NEW_STRUCTURE -------------------
