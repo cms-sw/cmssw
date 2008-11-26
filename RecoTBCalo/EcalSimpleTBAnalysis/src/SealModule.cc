2b35afc42b45553b7b5758abd1811c2ebe6c9057
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2008-11-26 14:49:46 +0000

    Adding 2007 analysis example

--- RecoTBCalo/EcalSimpleTBAnalysis/src/SealModule.cc
+++ RecoTBCalo/EcalSimpleTBAnalysis/src/SealModule.cc
@@ -1,7 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTBCalo/EcalSimpleTBAnalysis/interface/EcalSimpleTBAnalyzer.h"
+#include "RecoTBCalo/EcalSimpleTBAnalysis/interface/EcalSimple2007H4TBAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE( EcalSimpleTBAnalyzer );
+DEFINE_ANOTHER_FWK_MODULE( EcalSimple2007H4TBAnalyzer );
