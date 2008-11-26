bed3a493d17237125733d300281e9bfaa77d517d
Jordan Tucker <sha1-28c503d75f00e3a5fae81611ebf3c00e81145ad8@cern.ch>
2008-11-26 21:37:29 +0000

    Module to replace the kinematic information of Muon objects with information from a selectable set of refit tracks.

---
+++ RecoMuon/MuonIdentification/python/refitMuons_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+# Produces a collection recoMuons_refitMuons__* consisting of the
+# reco::Muons from the src collection (default is the merged muons
+# collection, only taking those Muons for which isGlobalMuon() is
+# true) with the kinematics replaced by those from the refit tracks
+# chosen. The default below chooses the tracks using the "cocktail"
+# function found in DataFormats/MuonReco/src/MuonCocktails.cc.
+
+refitMuons = cms.EDProducer('MuonsFromRefitTracksProducer',
+    src           = cms.InputTag('muons'),
+    tevMuonTracks = cms.untracked.string('tevMuons'),
+    fromCocktail  = cms.untracked.bool(True)
+)
