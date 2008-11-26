7bb52e9ef8327bab8462157bb1e918c96bb7b7fc
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2008-11-26 21:34:15 +0000

    Build library for plugins.

--- L1Trigger/GlobalTriggerAnalyzer/plugins/Module.cc
+++ L1Trigger/GlobalTriggerAnalyzer/plugins/Module.cc
@@ -1,13 +1,11 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 #include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtAnalyzer.h"
 #include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtTrigReport.h"
 #include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtPackUnpackAnalyzer.h"
 #include "L1Trigger/GlobalTriggerAnalyzer/interface/L1GtDataEmulAnalyzer.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(L1GtAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(L1GtTrigReport);
-DEFINE_ANOTHER_FWK_MODULE(L1GtPackUnpackAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(L1GtDataEmulAnalyzer);
+DEFINE_FWK_MODULE(L1GtAnalyzer);
+DEFINE_FWK_MODULE(L1GtTrigReport);
+DEFINE_FWK_MODULE(L1GtPackUnpackAnalyzer);
+DEFINE_FWK_MODULE(L1GtDataEmulAnalyzer);
