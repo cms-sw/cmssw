af2b30fba7cd2ff5464e2ae965653dac2ea6d8af
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-11-26 01:50:46 +0000

    new plugin factory used to consolidate all RhoPhiZ proxy builders

---
+++ Fireworks/Core/interface/FWRPZDataProxyBuilderBaseFactory.h
@@ -0,0 +1,37 @@
+#ifndef Fireworks_Core_FWRPZDataProxyBuilderBaseFactory_h
+#define Fireworks_Core_FWRPZDataProxyBuilderBaseFactory_h
+// -*- C++ -*-
+//
+// Package:     Core
+// Class  :     FWRPZDataProxyBuilderBaseFactory
+//
+/**\class FWRPZDataProxyBuilderBaseFactory FWRPZDataProxyBuilderBaseFactory.h Fireworks/Core/interface/FWRPZDataProxyBuilderBaseFactory.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Thu Jun  5 20:13:37 EDT 2008
+// $Id: FWRPZDataProxyBuilderBaseFactory.h,v 1.2 2008/11/06 22:05:23 amraktad Exp $
+//
+
+// system include files
+
+// user include files
+#include "Fireworks/Core/interface/register_dataproxybuilder_macro.h"
+#include "FWCore/PluginManager/interface/PluginFactory.h"
+
+// forward declarations
+class FWRPZDataProxyBuilderBase;
+
+typedef edmplugin::PluginFactory<FWRPZDataProxyBuilderBase*()> FWRPZDataProxyBuilderBaseFactory;
+
+#define REGISTER_FWRPZDATAPROXYBUILDERBASE(_name_,_type_,_purpose_) \
+DEFINE_PROXYBUILDER_METHODS(_name_,_type_,_purpose_); \
+DEFINE_EDM_PLUGIN(FWRPZDataProxyBuilderBaseFactory,_name_,_name_::typeOfBuilder()+_name_::classTypeName()+"@"+_name_::classPurpose()+"@" #_name_)
+
+#endif
---
+++ Fireworks/Core/src/FWRPZDataProxyBuilderBaseFactory.cc
@@ -0,0 +1,20 @@
+// -*- C++ -*-
+//
+// Package:     Core
+// Class  :     FWRPZDataProxyBuilderBaseFactory
+//
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Fri Jun  6 13:51:44 EDT 2008
+// $Id: FWRPZDataProxyBuilderBaseFactory.cc,v 1.2 2008/11/06 22:05:26 amraktad Exp $
+//
+
+// system include files
+
+// user include files
+#include "Fireworks/Core/interface/FWRPZDataProxyBuilderBaseFactory.h"
+
+
+EDM_REGISTER_PLUGINFACTORY(FWRPZDataProxyBuilderBaseFactory,"cmsShow FWRPZDataProxyBuilderBase");
