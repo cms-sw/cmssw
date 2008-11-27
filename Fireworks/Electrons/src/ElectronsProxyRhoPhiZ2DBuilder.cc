e825b2ea8a91c6e2956099184ffd2bdf46edf712
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-11-27 00:45:59 +0000

    no longer register plugin since now use the new 'simple' electron proxy builder

--- Fireworks/Electrons/src/ElectronsProxyRhoPhiZ2DBuilder.cc
+++ Fireworks/Electrons/src/ElectronsProxyRhoPhiZ2DBuilder.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:
 //         Created:  Sun Jan  6 23:57:00 EST 2008
-// $Id: ElectronsProxyRhoPhiZ2DBuilder.cc,v 1.17 2008/11/06 22:05:28 amraktad Exp $
+// $Id: ElectronsProxyRhoPhiZ2DBuilder.cc,v 1.18 2008/11/26 16:19:14 chrjones Exp $
 //
 
 // system include files
@@ -207,4 +207,4 @@ void ElectronsProxyRhoPhiZ2DBuilder::buildElectronRhoZ(const FWEventItem* iItem,
 }
 
 
-REGISTER_FWRPZDATAPROXYBUILDERBASE(ElectronsProxyRhoPhiZ2DBuilder,reco::PixelMatchGsfElectronCollection,"Electrons");
+//REGISTER_FWRPZDATAPROXYBUILDERBASE(ElectronsProxyRhoPhiZ2DBuilder,reco::PixelMatchGsfElectronCollection,"Electrons");
