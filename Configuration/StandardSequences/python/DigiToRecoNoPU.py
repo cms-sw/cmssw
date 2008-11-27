c5ef431617de4804e8fc6fad355014a88e64594a
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-11-27 18:19:31 +0000

    latest refinment on the re-digi workflow

--- Configuration/StandardSequences/python/DigiToRecoNoPU.py
+++ Configuration/StandardSequences/python/DigiToRecoNoPU.py
@@ -5,10 +5,19 @@ def customise(process):
         inputCommands=cms.untracked.vstring('drop *')
         )
 
+    HLTCleaning= cms.PSet(
+        inputCommands=cms.untracked.vstring('drop FEDRawDataCollection_*_*_*')
+        )
+
+    REDIGIInputEventSkimming.inputCommands.extend(process.RecoGenJetsFEVT.outputCommands)
+    REDIGIInputEventSkimming.inputCommands.extend(process.RecoGenMETFEVT.outputCommands)
     REDIGIInputEventSkimming.inputCommands.extend(process.SimG4CoreRAW.outputCommands) 
     REDIGIInputEventSkimming.inputCommands.extend(process.GeneratorInterfaceRAW.outputCommands) 
     REDIGIInputEventSkimming.inputCommands.extend(process.IOMCRAW.outputCommands) 
-
+    REDIGIInputEventSkimming.inputCommands.extend(process.L1TriggerRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(process.HLTriggerRAW.outputCommands) 
+    REDIGIInputEventSkimming.inputCommands.extend(HLTCleaning.inputCommands)
+    
     process.source.inputCommands = REDIGIInputEventSkimming.inputCommands
     
     if hasattr(process,"RandomNumberGeneratorService"):
@@ -18,18 +27,17 @@ def customise(process):
         del process.RandomNumberGeneratorService.theSource
 
     process.RandomNumberGeneratorService.restoreStateLabel = cms.untracked.string('randomEngineStateProducer')
-
+    
     # Output definition for RAW
     process.outputRaw = cms.OutputModule("PoolOutputModule",
-       outputCommands = process.RAWSIMEventContent.outputCommands,
-       fileName = cms.untracked.string('New_RAWSIM.root'),
-       dataset = cms.untracked.PSet(
-           dataTier = cms.untracked.string(''),
-           filterName = cms.untracked.string('')
-       )
-    )
-
+                                         outputCommands = process.RAWSIMEventContent.outputCommands,
+                                         fileName = cms.untracked.string('New_RAWSIM.root'),
+                                         dataset = cms.untracked.PSet(dataTier = cms.untracked.string(''),
+                                                                      filterName = cms.untracked.string('')
+                                                                      )
+                                         )
+    
     process.out_step_raw = cms.EndPath(process.outputRaw)
     process.schedule.append(process.out_step_raw)
-    
+                
     return(process)
