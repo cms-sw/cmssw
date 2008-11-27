a6a60d27b20b8785761633044db75b5fd4674baf
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2008-11-27 16:28:03 +0000

    use standard cff

--- Validation/RecoMuon/test/muonValidation_cfg.py
+++ Validation/RecoMuon/test/muonValidation_cfg.py
@@ -7,11 +7,14 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( (
-    '/store/user/aeverett/note2112recob/SingleMuPt100-step2//step2-SingleMuPt100_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_40.root',
+    '/store/relval/CMSSW_2_2_0/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_V9_v1/0000/10C12A24-74B9-DD11-85B2-001617DBCF6A.root',
+    '/store/relval/CMSSW_2_2_0/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_V9_v1/0000/3A14ADED-B4B9-DD11-8F0B-001617E30D40.root' ,
     ))
-secFiles.extend( [
-    '/store/user/aeverett/note2112/SingleMuPt100//SingleMuPt100_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_40.root',
-    ] )
+secFiles.extend((
+    '/store/relval/CMSSW_2_2_0/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v1/0000/526D7CD6-68B9-DD11-886D-001617DBD224.root',
+    '/store/relval/CMSSW_2_2_0/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v1/0000/A2C70EEE-B4B9-DD11-8170-001617DBD316.root',
+    '/store/relval/CMSSW_2_2_0/RelValSingleMuPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_V9_v1/0000/D437AC21-6FB9-DD11-BEA1-001617E30CC8.root' 
+    ))
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -40,12 +43,17 @@ process.MessageLogger.cerr = cms.untracked.PSet(
     placeholder = cms.untracked.bool(True)
 )
 
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.raw2digi_step = cms.Path(process.RawToDigi)
+
+process.load("Configuration/StandardSequences/SimulationRandomNumberGeneratorSeeds_cff")
 
 process.load("DQMServices.Components.MEtoEDMConverter_cfi")
 process.MEtoEDMConverter_step = cms.Path(process.MEtoEDMConverter)
 
 process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
@@ -57,34 +65,11 @@ process.GlobalTag.globaltag = "IDEAL_V9::All"
 process.load("Configuration.StandardSequences.Validation_cff")
 process.validation_step = cms.Path(process.validation)
 ## Load muon validation modules
-process.load("Validation.RecoMuon.muonValidation_cff")
-
-## Redo the DigiLinks and TrackingParticles
-process.load("SimTracker.Configuration.SimTracker_cff")
-process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
-
-#process.RandomNumberGeneratorService.restoreStateLabel = ""
-process.RandomNumberGeneratorService.simSiPixelDigis = cms.PSet(
-    initialSeed = cms.untracked.uint32(1234567),
-    engineName = cms.untracked.string('HepJamesRandom'),
-    )
-process.RandomNumberGeneratorService.simSiStripDigis = cms.PSet(
-    initialSeed = cms.untracked.uint32(1234567),
-    engineName = cms.untracked.string('HepJamesRandom'),
-    )
-
-
-process.TPLink = cms.Sequence(process.mix*process.mergedtruth*process.trDigi) 
-process.TPLink_step = cms.Path(process.TPLink)
-
 #process.recoMuonVMuAssoc.outputFileName = 'validationME.root'
-
-process.postMuon_step = cms.Path(process.muonSelector_seq*process.muonAssociation_seq)
-process.muonValidation_step = cms.Path(process.muonValidation_seq)
+process.muonValidation_step = cms.Path(cms.SequencePlaceholder("mix")+process.recoMuonValidation)
 
 process.schedule = cms.Schedule(
-    process.TPLink_step,
-    process.postMuon_step,
+    process.raw2digi_step,
 #    process.validation_step,
     process.muonValidation_step,
     process.MEtoEDMConverter_step,process.outpath)
