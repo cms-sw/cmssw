e32bc16eb3a864e538238c08790ac451e136f230
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2008-11-28 15:55:45 +0000

    *** empty log message ***

--- EventFilter/RPCRawToDigi/src/classes.h
+++ EventFilter/RPCRawToDigi/src/classes.h
@@ -6,20 +6,16 @@ namespace {
   namespace { 
     std::map< std::pair<int,int>, int > a1;
     edm::Wrapper<std::map< std::pair<int,int>, int > > a2;
-    std::pair<int, std::vector<int> > b1;
-    edm::Wrapper<std::pair<int, std::vector<int> > > b2;
-    std::map<int, std::vector<int> > c1;
-    edm::Wrapper<std::map<int, std::vector<int> > > c2;
+
     RPCRawDataCounts d1;
     edm::Wrapper<RPCRawDataCounts> d2;
-
-
 
     LinkBoardElectronicIndex e1;
     edm::Wrapper<LinkBoardElectronicIndex> e2;
 
     std::pair<LinkBoardElectronicIndex,int> f1;
     edm::Wrapper< std::pair<LinkBoardElectronicIndex,int> > f2;
+
     std::vector< std::pair<LinkBoardElectronicIndex, int> > g1;
     edm::Wrapper< std::vector< std::pair<LinkBoardElectronicIndex, int> > > g2;
   }
