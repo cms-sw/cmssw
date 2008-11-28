e51c282eca7054a5440f648be12b5327dde68907
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-11-28 12:50:11 +0000

    Add PhotonOfflineClient

--- DQMOffline/EGamma/src/SealModule.cc
+++ DQMOffline/EGamma/src/SealModule.cc
@@ -2,8 +2,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DQMOffline/EGamma/interface/PhotonAnalyzer.h"
 #include "DQMOffline/EGamma/interface/ElectronAnalyzer.h"
+#include "DQMOffline/EGamma/interface/PhotonOfflineClient.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(PhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(PhotonOfflineClient);
