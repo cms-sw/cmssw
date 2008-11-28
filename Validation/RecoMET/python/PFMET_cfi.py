622b345ef96237cf476b6bbb231d94870043745f
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-11-28 21:54:30 +0000

    Validation module for pfmet

---
+++ Validation/RecoMET/python/PFMET_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+# File: PFMET.cfi
+# Author: R. Remington
+# Date: 11.14.2008
+#
+# Fill validation histograms for PFMET.
+
+pfMetAnalyzer = cms.EDAnalyzer(
+   "METTester",
+    OutputFile = cms.untracked.string('output.root'),
+    InputMETLabel = cms.InputTag("pfMet"),
+    METType = cms.untracked.string('PFMET')
+   ) 
+
+
