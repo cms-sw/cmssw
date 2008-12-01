919b3ba84d1518d09cd857595794718cce29f0d2
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-12-01 15:27:10 +0000

    Seeding for tracks used for electron isolation is more severe than I thought...

--- FastSimulation/EgammaElectronAlgos/python/l1IsoElectronsRegionalRecoTracker_cff.py
+++ FastSimulation/EgammaElectronAlgos/python/l1IsoElectronsRegionalRecoTracker_cff.py
@@ -3,17 +3,17 @@ import FWCore.ParameterSet.Config as cms
 # Take all pixel tracks but the potential electrons
 hltL1IsoElectronsRegionalCTFFinalFitWithMaterial = cms.EDFilter("FastTrackMerger",
     SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates"))
+    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracksForElectrons"),
+                                   cms.InputTag("globalPixelTrackCandidatesForElectrons"))
 )
 
 hltL1IsoStartUpElectronsRegionalCTFFinalFitWithMaterial = cms.EDFilter("FastTrackMerger",
     SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates"))
+    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracksForElectrons"),
+                                   cms.InputTag("globalPixelTrackCandidatesForElectrons"))
 )
 
 # The sequence
-HLTL1IsoElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTracking")+hltL1IsoElectronsRegionalCTFFinalFitWithMaterial)
-HLTL1IsoStartUpElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTracking")+hltL1IsoStartUpElectronsRegionalCTFFinalFitWithMaterial)
+HLTL1IsoElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTrackingForElectrons")+hltL1IsoElectronsRegionalCTFFinalFitWithMaterial)
+HLTL1IsoStartUpElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTrackingForElectrons")+hltL1IsoStartUpElectronsRegionalCTFFinalFitWithMaterial)
 
--- FastSimulation/EgammaElectronAlgos/python/l1NonIsoElectronsRegionalRecoTracker_cff.py
+++ FastSimulation/EgammaElectronAlgos/python/l1NonIsoElectronsRegionalRecoTracker_cff.py
@@ -3,17 +3,17 @@ import FWCore.ParameterSet.Config as cms
 # Take all pixel tracks but the potential electrons
 hltL1NonIsoElectronsRegionalCTFFinalFitWithMaterial = cms.EDFilter("FastTrackMerger",
     SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates"))
+    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracksForElectrons"),
+                                   cms.InputTag("globalPixelTrackCandidatesForElectrons"))
 )
 
 hltL1NonIsoStartUpElectronsRegionalCTFFinalFitWithMaterial = cms.EDFilter("FastTrackMerger",
     SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates"))
+    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracksForElectrons"),
+                                   cms.InputTag("globalPixelTrackCandidatesForElectrons"))
 )
 
 # The sequence
-HLTL1NonIsoElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTracking")+hltL1NonIsoElectronsRegionalCTFFinalFitWithMaterial)
-HLTL1NonIsoStartUpElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTracking")+hltL1NonIsoStartUpElectronsRegionalCTFFinalFitWithMaterial)
+HLTL1NonIsoElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTrackingForElectrons")+hltL1NonIsoElectronsRegionalCTFFinalFitWithMaterial)
+HLTL1NonIsoStartUpElectronsRegionalRecoTrackerSequence = cms.Sequence(cms.SequencePlaceholder("globalPixelTrackingForElectrons")+hltL1NonIsoStartUpElectronsRegionalCTFFinalFitWithMaterial)
 
