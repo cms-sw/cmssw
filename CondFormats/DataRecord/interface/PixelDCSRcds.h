950d31d41f657c720e637799733ab94c75b74ff8
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2008-12-01 16:53:50 +0000

    Added pixel DCS (low voltage monitoring) objects. Author: Chung Khim Lae.

---
+++ CondFormats/DataRecord/interface/PixelDCSRcds.h
@@ -0,0 +1,10 @@
+#ifndef DataRecord_PixelDCSRcds_h
+#define DataRecord_PixelDCSRcds_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class PixelCaenChannelIsOnRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIsOnRcd> {};
+class PixelCaenChannelIMonRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIMonRcd> {};
+class PixelCaenChannelRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelRcd> {};
+
+#endif
