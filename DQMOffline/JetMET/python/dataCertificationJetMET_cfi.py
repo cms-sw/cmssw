c36960a2ff6957d6fb720a5fd2d14c873e48bb56
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2008-12-02 06:20:23 +0000

    Input and reference files are blank by default

--- DQMOffline/JetMET/python/dataCertificationJetMET_cfi.py
+++ DQMOffline/JetMET/python/dataCertificationJetMET_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 dataCertificationJetMET = cms.EDAnalyzer('DataCertificationJetMET',
-                              fileName       = cms.untracked.string("/uscms/home/chlebana/DQM_V0001_R000063463__BeamHalo__BeamCommissioning08-PromptReco-v1__RECO.root"),
-                              refFileName    = cms.untracked.string("/uscms/home/chlebana/DQM_V0001_R000063463__BeamHalo__BeamCommissioning08-PromptReco-v1__RECO.root"),
+                              fileName       = cms.untracked.string(""),
+                              refFileName    = cms.untracked.string(""),
                               OutputFile     = cms.untracked.bool(False),
                               OutputFileName = cms.untracked.string("DQMResult.root"),
                               Verbose        = cms.untracked.int32(0),
