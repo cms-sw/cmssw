87fd07ac306bd2103d1c0da8c926303c0ac80989
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-02 17:24:07 +0000

    moved elsewhere SiStripDeDxMipBuilder.cc  SiStripDeDxMipBuilder.h  SiStripDeDxMipReader.cc  SiStripDeDxMipReader.h

--- CondTools/SiStrip/plugins/SealModules.cc
+++ CondTools/SiStrip/plugins/SealModules.cc
@@ -54,8 +54,3 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripFedCablingReader);
 //#include "CondTools/SiStrip/plugins/SiStripSummaryReader.h"
 //DEFINE_ANOTHER_FWK_MODULE(SiStripSummaryReader);
 
-#include "CondTools/SiStrip/plugins/SiStripDeDxMipBuilder.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripDeDxMipBuilder);
-
-#include "CondTools/SiStrip/plugins/SiStripDeDxMipReader.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripDeDxMipReader);
