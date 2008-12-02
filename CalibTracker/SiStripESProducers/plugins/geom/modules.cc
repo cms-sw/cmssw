141311e8b482d8a8dc1d542f85687ebf2076e581
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-02 16:35:29 +0000

    removed SiStripNoiseDummyCalculator

--- CalibTracker/SiStripESProducers/plugins/geom/modules.cc
+++ CalibTracker/SiStripESProducers/plugins/geom/modules.cc
@@ -7,5 +7,3 @@ DEFINE_SEAL_MODULE();
 #include "CalibTracker/SiStripESProducers/plugins/geom/SiStripHashedDetIdESModule.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(SiStripHashedDetIdESModule);
 
-#include "CalibTracker/SiStripESProducers/plugins/geom/SiStripNoiseDummyCalculator.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripNoiseDummyCalculator);
