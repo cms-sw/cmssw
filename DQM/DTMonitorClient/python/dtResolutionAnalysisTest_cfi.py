f068bd609f75e9fffe200a2c1d27054538a1cf6c
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-12-02 13:32:21 +0000

    changed the sigma test range

--- DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
+++ DQM/DTMonitorClient/python/dtResolutionAnalysisTest_cfi.py
@@ -4,7 +4,7 @@ dtResolutionAnalysisTest = cms.EDAnalyzer("DTResolutionAnalysisTest",
     folderRoot = cms.untracked.string(''),
     diagnosticPrescale = cms.untracked.int32(1),
     permittedMeanRange = cms.untracked.double(0.01),
-    permittedSigmaRange = cms.untracked.double(0.1)
+    permittedSigmaRange = cms.untracked.double(0.08)
  )
 
 
