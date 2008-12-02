70853eb00d2c3a43ee48cf8f07b838d881807d74
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-12-02 11:26:18 +0000

    changed parameter ranges

--- DQMOffline/Muon/python/muonTestSummary_cff.py
+++ DQMOffline/Muon/python/muonTestSummary_cff.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 muonTestSummary = cms.EDFilter("MuonTestSummary",
                              # tests parameters
-                             etaExpected = cms.double(0.9),
+                             etaExpected = cms.double(0.6),
                              phiExpected = cms.double(0.01),
-                             etaSpread = cms.double(0.4), 
-                             phiSpread = cms.double(0.4),
-                             chi2Fraction = cms.double(0.3),
-                             chi2Spread = cms.double(0.1),
+                             etaSpread = cms.double(0.3), 
+                             phiSpread = cms.double(0.01),
+                             chi2Fraction = cms.double(0.4),
+                             chi2Spread = cms.double(0.2),
                              resEtaSpread_tkGlb = cms.double(0.001),
                              resEtaSpread_glbSta = cms.double(0.05),
                              resPhiSpread_tkGlb = cms.double(0.001),
