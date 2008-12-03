b4da3c34d4b7ec537aa2a42d9cae174859aabf9a
Stefano Lacaprara <sha1-1aa582c8b612f9ee45a48bebe61f39053371bafc@cern.ch>
2008-12-03 15:35:26 +0000

    upload of cfi for cluster and extended segments

---
+++ RecoLocalMuon/DTSegment/python/dt1dClusters_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+# module to build the 1d Clusters
+
+process.dt1DClusters = cms.EDFilter("DTClusterer",
+    debug = cms.untracked.bool(False),
+    minLayers = cms.uint32(3),
+    minHits = cms.uint32(3),
+    recHits1DLabel = cms.InputTag("dt1DRecHits")
+)
+
