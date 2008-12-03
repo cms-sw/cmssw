993e542f09eb95e8084a33423743bb8436618f56
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2008-12-03 15:05:00 +0000

    *** empty log message ***

--- EventFilter/SiStripRawToDigi/python/SiStripFEDRawDataAnalyzer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-FEDRawDataAnalyzer = cms.EDAnalyzer("SiStripFEDRawDataAnalyzer",
-    ProductLabel = cms.untracked.string('source'),
-    ProductInstance = cms.untracked.string('')
-)
-
-
