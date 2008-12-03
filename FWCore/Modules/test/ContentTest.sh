2742471c30d76f4f5e729525cc1cc3d60b6a3ee4
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2008-12-03 20:53:40 +0000

    Fix minor bug in unit test script

--- FWCore/Modules/test/ContentTest.sh
+++ FWCore/Modules/test/ContentTest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-function die { ech $1; status $2; exit $2; }
+function die { echo $1: status $2; exit $2; }
 
 cmsRun ${LOCAL_TEST_DIR}/ContentTest_cfg.py || die 'failed running cmsRun ContentTest_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/printeventsetupcontent_cfg.py || die 'failed running cmsRun printeventsetupcontent_cfg.py' $?
