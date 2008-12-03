6ae9046810d9cabf92c460cb1c95f995ce963efb
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-12-03 20:48:07 +0000

    moved to own file to facilitate better reuse

---
+++ Fireworks/Core/interface/fw3dlego_xbins.h
@@ -0,0 +1,27 @@
+#ifndef Fireworks_Core_fw3dlego_xbins_h
+#define Fireworks_Core_fw3dlego_xbins_h
+// -*- C++ -*-
+//
+// Package:     Core
+// Class  :     fw3dlego_xbins
+// 
+/**\class fw3dlego_xbins fw3dlego_xbins.h Fireworks/Core/interface/fw3dlego_xbins.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Wed Dec  3 13:55:42 EST 2008
+// $Id$
+//
+
+namespace fw3dlego
+{
+   extern const double xbins[83];
+}
+
+#endif
