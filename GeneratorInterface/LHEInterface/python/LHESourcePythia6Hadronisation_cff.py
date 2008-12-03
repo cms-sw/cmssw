83b5ef17224978f309da821744194e4e1dcec366
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2008-12-03 14:25:49 +0000

    Add a few config files to show how to use cmsDriver to generate config files for simulation. ----------------------------------------------------------------------

---
+++ GeneratorInterface/LHEInterface/python/LHESourcePythia6Hadronisation_cff.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+source = cms.Source("LHESource",
+	fileNames = cms.untracked.vstring('file:events.lhe'),
+	seekEvent = cms.untracked.uint32(0)
+)
+
+generator = cms.EDProducer("LHEProducer",
+	hadronisation = cms.PSet(
+		pythiaUESettingsBlock,
+		generator = cms.string('Pythia6'),
+		parameterSets = cms.vstring(
+			'pythiaUESettings'
+		)
+	)
+)
+
+ProducerSourceSequence = cms.Sequence(generator)
