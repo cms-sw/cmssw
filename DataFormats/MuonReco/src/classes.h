cbbbaa0e6054fcfe6e9f74d5dd26267c9d5b7184
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-12-03 02:26:08 +0000

    remove outdated dictionaries used by mu-isolation eons ago

--- DataFormats/MuonReco/src/classes.h
+++ DataFormats/MuonReco/src/classes.h
@@ -14,10 +14,6 @@
 #include "DataFormats/TrackReco/interface/Track.h" 
 #include "DataFormats/Common/interface/AssociationMap.h"
 
-#define USE_MUISODEPOSIT_REQUIRED
-#include "DataFormats/MuonReco/interface/Direction.h"
-#include "DataFormats/MuonReco/interface/MuIsoDeposit.h"
-#include "DataFormats/MuonReco/interface/MuIsoDepositFwd.h" 
 
 #include <vector>
 #include <map>
@@ -32,80 +28,11 @@ namespace {
     edm::reftobase::Holder<reco::Candidate, reco::MuonRef> rb1;
     edm::helpers::Key<edm::RefProd<std::vector<reco::Muon> > > hkrv1;
 
-    std::multimap<muonisolation::Direction::Distance,float> v2b;
-    reco::MuIsoDeposit miso;
-    std::vector<reco::MuIsoDeposit> v2;
-    edm::Wrapper<std::vector<reco::MuIsoDeposit> > c2;
-    edm::Ref<std::vector<reco::MuIsoDeposit> > r2;
-    edm::RefProd<std::vector<reco::MuIsoDeposit> > rp2;
-    edm::Wrapper<edm::RefVector<std::vector<reco::MuIsoDeposit> > > wrv2;
-    
     reco::MuonIsolation rmi;
     reco::MuonTime rmt;
 
     std::vector<reco::MuonChamberMatch> vmm1;
     std::vector<reco::MuonSegmentMatch> vmm2;
-
-//defined in DataFormats/TrackReco
-//    reco::MuIsoAssociationMap v4;
-//    edm::Wrapper<reco::MuIsoAssociationMap> w4;
-
-    std::map<unsigned int, reco::MuIsoDeposit> m5;
-    reco::MuIsoDepositAssociationMap v5;
-    edm::Wrapper<reco::MuIsoDepositAssociationMap> w5;
-
-//defined in DataFormats/TrackReco
-//    reco::MuIsoIntAssociationMap v6;
-//    edm::Wrapper<reco::MuIsoIntAssociationMap> w6;
-
-//    reco::MuIsoFloatAssociationMap v7;
-//    edm::Wrapper<reco::MuIsoFloatAssociationMap> w7;
-   
-
-   
-    reco::MuIsoAssociationMapToMuon v8;
-    edm::Wrapper<reco::MuIsoAssociationMapToMuon> w8;
-
-    reco::MuIsoDepositAssociationMapToMuon v9;
-    edm::Wrapper<reco::MuIsoDepositAssociationMapToMuon> w9;
-
-    reco::MuIsoIntAssociationMapToMuon v91;
-    edm::Wrapper<reco::MuIsoIntAssociationMapToMuon> w91;
-
-    reco::MuIsoFloatAssociationMapToMuon v10;
-    edm::Wrapper<reco::MuIsoFloatAssociationMapToMuon> w10;
-   
-
-    reco::MuIsoAssociationVector v11;
-    edm::Wrapper<reco::MuIsoAssociationVector> w11;
-
-    reco::MuIsoDepositAssociationVector v12;
-    edm::Wrapper<reco::MuIsoDepositAssociationVector> w12;
-
-    //the two below will have to be in DataFormats/TrackReco at some point
-    reco::MuIsoIntAssociationVector v13;
-    edm::Wrapper<reco::MuIsoIntAssociationVector> w13;
-
-    reco::MuIsoFloatAssociationVector v14;
-    edm::Wrapper<reco::MuIsoFloatAssociationVector> w14;
-   
-
-    reco::MuIsoAssociationVectorToMuon v19;
-    edm::Wrapper<reco::MuIsoAssociationVectorToMuon> w19;
-
-    reco::MuIsoDepositAssociationVectorToMuon v20;
-    edm::Wrapper<reco::MuIsoDepositAssociationVectorToMuon> w20;
-
-    reco::MuIsoIntAssociationVectorToMuon v21;
-    edm::Wrapper<reco::MuIsoIntAssociationVectorToMuon> w21;
-
-    reco::MuIsoFloatAssociationVectorToMuon v22;
-    edm::Wrapper<reco::MuIsoFloatAssociationVectorToMuon> w22;
-   
-
-    reco::MuIsoDepositAssociationVectorToCandidateView v23;
-    edm::Wrapper<reco::MuIsoDepositAssociationVectorToCandidateView> w23;
-
 
     std::vector<reco::MuonTrackLinks> tl1;
     edm::Wrapper<std::vector<reco::MuonTrackLinks> > tl2;
@@ -121,22 +48,13 @@ namespace {
     edm::reftobase::VectorHolder<reco::Candidate, reco::MuonRefVector> hcc3;
     edm::reftobase::RefVectorHolder<reco::MuonRefVector> hcc4;
 
-    reco::CandIsoDepositAssociationPair candIsoAP;
-    reco::CandIsoDepositAssociationVector candIsoAV;
-    std::vector<reco::CandIsoDepositAssociationVector> candIsoAPV_root_seems_to_need_this;
-    edm::Wrapper<reco::CandIsoDepositAssociationVector> candIsoAV_w;
-
-    reco::MuIsoDepositMap idvm;
-    reco::MuIsoDepositMap::const_iterator idvmci;
-    edm::Wrapper<reco::MuIsoDepositMap> w_idvm;
-     
-     edm::RefToBase<reco::Muon> rtbm;
-     edm::reftobase::IndirectHolder<reco::Muon> ihm;
-     edm::RefToBaseProd<reco::Muon> rtbpm;
-     edm::RefToBaseVector<reco::Muon> rtbvm;
-     edm::Wrapper<edm::RefToBaseVector<reco::Muon> > rtbvm_w;
-     edm::reftobase::BaseVectorHolder<reco::Muon> *bvhm_p;
-     
+    edm::RefToBase<reco::Muon> rtbm;
+    edm::reftobase::IndirectHolder<reco::Muon> ihm;
+    edm::RefToBaseProd<reco::Muon> rtbpm;
+    edm::RefToBaseVector<reco::Muon> rtbvm;
+    edm::Wrapper<edm::RefToBaseVector<reco::Muon> > rtbvm_w;
+    edm::reftobase::BaseVectorHolder<reco::Muon> *bvhm_p;
+    
   };
 }
 
