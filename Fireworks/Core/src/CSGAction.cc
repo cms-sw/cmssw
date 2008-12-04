8d2f252597c90f0394fea7a58b4c43581f33e4d7
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2008-12-04 11:55:46 +0000

    Fix slider background color. In future slider code moves to CmsShowMain.

--- Fireworks/Core/src/CSGAction.cc
+++ Fireworks/Core/src/CSGAction.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Thu May 29 20:58:11 CDT 2008
-// $Id: CSGAction.cc,v 1.13 2008/12/01 15:51:06 amraktad Exp $
+// $Id: CSGAction.cc,v 1.14 2008/12/01 22:24:33 amraktad Exp $
 //
 
 // system include files
@@ -159,7 +159,7 @@ void CSGAction::createDelaySlider(TGCompositeFrame* p, Int_t min, Int_t max, con
    m_slider->SetRange(min, max);
    m_slider->SetPosition(0);
 
-   m_slider->SetBackgroundColor(0x111111);
+   m_slider->SetBackgroundColor(0x1b1b1b);
    m_slider->ChangeSliderPic(buttPath);
 }
 
