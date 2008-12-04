6e791bba818e8737a74f6e557205322d6a13f3c4
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-04 01:08:59 +0000

    corrected include

--- CalibTracker/Configuration/python/SiStripLorentzAngle/SiStripLorentzAngle_Fake_cff.py
+++ CalibTracker/Configuration/python/SiStripLorentzAngle/SiStripLorentzAngle_Fake_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # Dummy Lorentz angle producer (no data is put in the ES!)
 from CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi import *    
+import CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi
 siStripLAFakeESSourceforSimulation = CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi.siStripLorentzAngleFakeESSource.clone()
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 # alternative
