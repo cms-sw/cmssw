56533aa849437b0b3fdf4659063780c2a09b0f66
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-04 00:17:36 +0000

    first add

---
+++ CalibTracker/SiStripESProducers/test/python/DummyCondDBWriter_SiStripLorentzAngle_cfg.py
@@ -0,0 +1,50 @@
+# The following comments couldn't be translated into the new config version:
+
+# upload to database 
+
+#string timetype = "timestamp"    
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Builder")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring(''),
+    LorentzAngleBuilder = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    destinations = cms.untracked.vstring('LorentzAngleBuilder.log')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("EmptySource",
+    numberEventsInRun = cms.untracked.uint32(1),
+    firstRun = cms.untracked.uint32(1)
+)
+
+process.load("CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi")
+process.load("CalibTracker.SiStripESProducers.DBWriter.SiStripLorentzAngleDummyDBWriter_cfi")
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(2),
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:dummy2.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('SiStripLorentzAngleRcd'),
+        tag = cms.string('SiStripLorentzAngle_Fake_30X')
+    ))
+)
+
+
+process.p1 = cms.Path(process.siStripLorentzAngleDummyDBWriter)
+
+
