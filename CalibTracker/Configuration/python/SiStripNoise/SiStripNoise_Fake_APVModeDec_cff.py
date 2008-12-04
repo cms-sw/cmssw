8f55c3b1b2663d26adbd5fdc46376f4c0d42ddf8
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-04 01:07:49 +0000

    corrected definition of parameters

--- CalibTracker/Configuration/python/SiStripNoise/SiStripNoise_Fake_APVModeDec_cff.py
+++ CalibTracker/Configuration/python/SiStripNoise/SiStripNoise_Fake_APVModeDec_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from CalibTracker.SiStripESProducers.fake.SiStripNoisesFakeESSource_cfi import *    
-SiStripNoiseFakeESSource.NoiseStripLengthSlope = 51.
-SiStripNoiseFakeESSource.NoiseStripLengthQuote = 630.
+SiStripNoisesGenerator.NoiseStripLengthSlope = 51.
+SiStripNoisesGenerator.NoiseStripLengthQuote = 630.
 
--- CalibTracker/Configuration/python/SiStripNoise/SiStripNoise_Fake_APVModePeak_cff.py
+++ CalibTracker/Configuration/python/SiStripNoise/SiStripNoise_Fake_APVModePeak_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from CalibTracker.SiStripESProducers.fake.SiStripNoisesFakeESSource_cfi import *    
-SiStripNoiseFakeESSource.NoiseStripLengthSlope = 38.8
-SiStripNoiseFakeESSource.NoiseStripLengthQuote = 414.
+SiStripNoisesGenerator.NoiseStripLengthSlope = 38.8
+SiStripNoisesGenerator.NoiseStripLengthQuote = 414.
 
