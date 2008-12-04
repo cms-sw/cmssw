2b83542169954de71f2c9f25786d2642b655b4da
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2008-12-04 12:48:28 +0000

    Deleting MuonSegmentEff

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -1,11 +1,9 @@
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorSync.h"
-#include "DQM/RPCMonitorDigi/interface/MuonSegmentEff.h"
 //#include "DQM/RPCMonitorDigi/interface/RPCReadoutError.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorDigi);
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorSync);
-DEFINE_ANOTHER_FWK_MODULE(MuonSegmentEff);
 //DEFINE_ANOTHER_FWK_MODULE(RPCReadoutError);
