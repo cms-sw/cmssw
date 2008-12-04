b7deddd7c91a056ce5bfc0e16d43d0bc85776a54
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2008-12-04 10:31:23 +0000

    adding EcalMappingElectronics

--- CondCore/EcalPlugins/src/plugin.cc
+++ CondCore/EcalPlugins/src/plugin.cc
@@ -35,6 +35,9 @@
 #include "CondFormats/EcalObjects/interface/EcalChannelStatus.h"
 #include "CondFormats/DataRecord/interface/EcalChannelStatusRcd.h"
 
+#include "CondFormats/EcalObjects/interface/EcalMappingElectronics.h"
+#include "CondFormats/DataRecord/interface/EcalMappingElectronicsRcd.h"
+
 // #include "CondFormats/EcalObjects/interface/EcalLaserAPDPNRatiosRef.h"
 #include "CondFormats/DataRecord/interface/EcalLaserAPDPNRatiosRefRcd.h"
 
@@ -93,6 +96,7 @@ REGISTER_PLUGIN(EcalLaserAlphasRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalLaserAPDPNRatiosRcd,EcalLaserAPDPNRatios);
 REGISTER_PLUGIN(EcalLaserAPDPNRatiosRefRcd,EcalFloatCondObjectContainer);
 REGISTER_PLUGIN(EcalChannelStatusRcd,EcalChannelStatus);
+REGISTER_PLUGIN(EcalMappingElectronicsRcd,EcalMappingElectronics);
 
 REGISTER_PLUGIN(EcalTPGPedestalsRcd,EcalTPGPedestals);
 REGISTER_PLUGIN(EcalTPGFineGrainEBGroupRcd,EcalTPGFineGrainEBGroup);
