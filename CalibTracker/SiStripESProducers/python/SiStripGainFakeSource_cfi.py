cb3e77959b6cbf72cd1224378ad0c3ed0252f575
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-04 00:41:13 +0000

    moved in fake

--- CalibTracker/SiStripESProducers/python/SiStripGainFakeSource_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-siStripGainFakeESSource = cms.ESSource("SiStripGainFakeESSource",
-    appendToDataLabel = cms.string('fakeAPVGain'),
-    file = cms.FileInPath('CalibTracker/SiStripCommon/data/SiStripDetInfo.dat')
-)
-
-
