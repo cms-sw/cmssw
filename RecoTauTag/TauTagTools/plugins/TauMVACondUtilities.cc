29d306d3df5bb8513b46c43c9a6359ae02668fd2
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2008-12-04 22:56:38 +0000

    Adding central steering file (interface/TauMVADBConfiguration.h) so that we only have to branch 1 file for 2_2_x compatability

--- RecoTauTag/TauTagTools/plugins/TauMVACondUtilities.cc
+++ RecoTauTag/TauTagTools/plugins/TauMVACondUtilities.cc
@@ -1,16 +1,16 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 
-#include "CondFormats/DataRecord/interface/TauTagMVAComputerRcd.h"
+#include "RecoTauTag/TauTagTools/interface/TauMVADBConfiguration.h"
 #include "PhysicsTools/MVAComputer/interface/MVAComputerESSourceImpl.h"
 #include "PhysicsTools/MVATrainer/interface/MVATrainerSaveImpl.h"
 
 using namespace PhysicsTools;
 
-typedef MVAComputerESSourceImpl<TauTagMVAComputerRcd> TauMVAComputerESSource;
+typedef MVAComputerESSourceImpl<TauMVAFrameworkDBRcd> TauMVAComputerESSource;
 DEFINE_FWK_EVENTSETUP_SOURCE(TauMVAComputerESSource);
 
-typedef MVATrainerContainerSaveImpl<TauTagMVAComputerRcd> TauMVATrainerSave;
+typedef MVATrainerContainerSaveImpl<TauMVAFrameworkDBRcd> TauMVATrainerSave;
 DEFINE_FWK_MODULE(TauMVATrainerSave);
 
 
--- RecoTauTag/TauTagTools/plugins/TauMVADiscriminator.cc
+++ RecoTauTag/TauTagTools/plugins/TauMVADiscriminator.cc
@@ -13,10 +13,9 @@
 //
 // Original Author:  Evan K. Friis, UC Davis (friis@physics.ucdavis.edu)
 //         Created:  Fri Aug 15 11:22:14 PDT 2008
-// $Id: TauMVADiscriminator.cc,v 1.5 2008/11/07 19:38:36 friis Exp $
+// $Id: TauMVADiscriminator.cc,v 1.6 2008/11/20 22:51:09 friis Exp $
 //
 //
-
 
 // system include files
 #include <memory>
@@ -40,7 +39,7 @@
 #include "RecoTauTag/TauTagTools/interface/DiscriminantList.h"
 #include "RecoTauTag/TauTagTools/interface/PFTauDiscriminantManager.h"
 #include "CondFormats/PhysicsToolsObjects/interface/MVAComputer.h"
-#include "CondFormats/DataRecord/interface/TauTagMVAComputerRcd.h"
+#include "RecoTauTag/TauTagTools/interface/TauMVADBConfiguration.h"
 #include "PhysicsTools/MVAComputer/interface/MVAComputerRecord.h"
 #include "PhysicsTools/MVAComputer/interface/MVAComputer.h"
 
@@ -111,7 +110,7 @@ TauMVADiscriminator::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
    //get appropriate MVA setup (specified in CFG file)
    ESHandle<PhysicsTools::Calibration::MVAComputerContainer> mvaHandle;
-   iSetup.get<TauTagMVAComputerRcd>().get(mvaHandle);
+   iSetup.get<TauMVAFrameworkDBRcd>().get(mvaHandle);
    PhysicsTools::MVAComputer mvaComputer(&mvaHandle.product()->find(computerName_));
 
    DiscriminantHandleList                    otherDiscriminants;
