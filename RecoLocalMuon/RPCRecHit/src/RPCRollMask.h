0d8aff31515d632e55bcc2168e4f247fd710f3c0
Juan Carlos Sanabria Arenas <sha1-04ff7c5cd6db53fc49f98b88a17b7b904534f00b@cern.ch>
2008-12-04 13:30:36 +0000

    Include RPC masked strips, first release

---
+++ RecoLocalMuon/RPCRecHit/src/RPCRollMask.h
@@ -0,0 +1,10 @@
+#ifndef RecoLocalMuon_RPCRollMask_h
+#define RecoLocalMuon_RPCRollMask_h
+
+#include <bitset>
+#include <vector>
+
+const int maskSIZE=96;
+typedef std::bitset<maskSIZE> RollMask;
+
+#endif
