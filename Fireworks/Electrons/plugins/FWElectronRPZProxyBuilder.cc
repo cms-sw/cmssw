6abaa08fa43f13c98272b3e075b090d4f858373b
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-12-05 21:00:00 +0000

    use the makeSuperCluster functions in order to share code with Photons

--- Fireworks/Electrons/plugins/FWElectronRPZProxyBuilder.cc
+++ Fireworks/Electrons/plugins/FWElectronRPZProxyBuilder.cc
@@ -8,15 +8,10 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Wed Nov 26 14:52:01 EST 2008
-// $Id: FWElectronRPZProxyBuilder.cc,v 1.3 2008/11/29 03:01:47 dmytro Exp $
+// $Id: FWElectronRPZProxyBuilder.cc,v 1.4 2008/12/01 14:46:08 chrjones Exp $
 //
 
 // system include files
-#include "TEveGeoNode.h"
-#include "TGeoBBox.h"
-#include "TGeoTube.h"
-#include "TEveManager.h"
-#include "TH1F.h"
 #include "TColor.h"
 #include "TROOT.h"
 #include "TEveTrack.h"
@@ -32,8 +27,8 @@
 // user include files
 #include "Fireworks/Core/interface/FWRPZ2DSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/BuilderUtils.h"
-#include "Fireworks/Core/interface/FWEventItem.h"
-#include "Fireworks/Core/interface/DetIdToMatrix.h"
+#include "Fireworks/Electrons/interface/makeSuperCluster.h"
+
 
 class FWElectronRPZProxyBuilder : public FWRPZ2DSimpleProxyBuilderTemplate<reco::GsfElectron> {
       
@@ -105,25 +100,10 @@ FWElectronRPZProxyBuilder::~FWElectronRPZProxyBuilder()
 void 
 FWElectronRPZProxyBuilder::buildRhoPhi(const reco::GsfElectron& iData, unsigned int iIndex,TEveElement& oItemHolder) const
 {
-   if ( iData.superCluster().isAvailable() ) {
-      TEveGeoManagerHolder gmgr(TEveGeoShape::GetGeoMangeur());
-
-      std::vector<DetId> detids = iData.superCluster()->getHitsByDetId();
-      std::vector<double> phis;
-      for (std::vector<DetId>::const_iterator id = detids.begin(); id != detids.end(); ++id) {
-	 const TGeoHMatrix* matrix = item()->getGeom()->getMatrix( id->rawId() );
-	 if ( matrix ) phis.push_back( atan2(matrix->GetTranslation()[1], matrix->GetTranslation()[0]) );
-      }
-      std::pair<double,double> phiRange = fw::getPhiRange( phis, iData.phi() );
-      const double r = 122;
-      TGeoBBox *sc_box = new TGeoTubeSeg(r - 1, r + 1, 1,
-					 phiRange.first * 180 / M_PI - 0.5,
-					 phiRange.second * 180 / M_PI + 0.5 ); // 0.5 is roughly half size of a crystal
-      TEveGeoShape *sc = fw::getShape( "supercluster", sc_box, item()->defaultDisplayProperties().color() );
-      sc->SetPickable(kTRUE);
-      oItemHolder.AddElement(sc);
-   }
-   
+   fireworks::makeRhoPhiSuperCluster(*item(),
+                                     iData.superCluster(),
+                                     iData.phi(),
+                                     oItemHolder);
    TEveTrack* track(0);
    if ( iData.gsfTrack().isAvailable() )
       track = fw::getEveTrack( *(iData.gsfTrack()) );
@@ -135,29 +115,10 @@ FWElectronRPZProxyBuilder::buildRhoPhi(const reco::GsfElectron& iData, unsigned 
 void 
 FWElectronRPZProxyBuilder::buildRhoZ(const reco::GsfElectron& iData, unsigned int iIndex,TEveElement& oItemHolder) const
 {
-   TEveGeoManagerHolder gmgr(TEveGeoShape::GetGeoMangeur());
-
-   if ( iData.superCluster().isAvailable() ) {
-      double theta_max = 0;
-      double theta_min = 10;
-      std::vector<DetId> detids = iData.superCluster()->getHitsByDetId();
-      for (std::vector<DetId>::const_iterator id = detids.begin(); id != detids.end(); ++id) {
-	 const TGeoHMatrix* matrix = item()->getGeom()->getMatrix( id->rawId() );
-	 if ( matrix ) {
-	    double r = sqrt( matrix->GetTranslation()[0]*matrix->GetTranslation()[0] +
-                            matrix->GetTranslation()[1]*matrix->GetTranslation()[1] );
-	    double theta = atan2(r,matrix->GetTranslation()[2]);
-	    if ( theta > theta_max ) theta_max = theta;
-	    if ( theta < theta_min ) theta_min = theta;
-	 }
-      }
-      // expand theta range by the size of a crystal to avoid segments of zero length
-      double z_ecal = 302; // ECAL endcap inner surface
-      double r_ecal = 122;
-      fw::addRhoZEnergyProjection( &oItemHolder, r_ecal, z_ecal, theta_min-0.003, theta_max+0.003,
-                                  iData.phi(), item()->defaultDisplayProperties().color() );
-   }
-   
+   fireworks::makeRhoZSuperCluster(*item(),
+                                   iData.superCluster(),
+                                   iData.phi(),
+                                   oItemHolder);
    TEveTrack* track(0);
    if ( iData.gsfTrack().isAvailable() )
       track = fw::getEveTrack( *(iData.gsfTrack()) );
