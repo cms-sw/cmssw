a745aa390865bdb6f3e32aecdf28be4d094fa0b8
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-12-08 21:50:21 +0000

    Tuning of iterative tracking : pt min cut ...

--- FastSimulation/TrajectoryManager/python/TrackerSimHits_cfi.py
+++ FastSimulation/TrajectoryManager/python/TrackerSimHits_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 TrackerSimHitsBlock = cms.PSet(
     TrackerSimHits = cms.PSet(
         # Smallest charged particle pT for which SimHit's are saved (GeV/c)
-        pTmin = cms.untracked.double(0.3),
+        pTmin = cms.untracked.double(0.2),
         # Save SimHit's only for the first loop
         firstLoop = cms.untracked.bool(True)
     )
