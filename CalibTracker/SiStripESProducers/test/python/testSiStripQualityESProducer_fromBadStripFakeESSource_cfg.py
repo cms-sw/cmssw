d839b7017823ce01c85e143da61369c4f66c67db
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-08 12:41:22 +0000

    first commit

---
+++ CalibTracker/SiStripESProducers/test/python/testSiStripQualityESProducer_fromBadStripFakeESSource_cfg.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CALIB")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring(''),
+    QualityReader = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    destinations = cms.untracked.vstring('QualityReader.log')
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(100),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    interval = cms.uint64(90)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+process.load("CalibTracker.SiStripESProducers.fake.SiStripBadModuleFakeESSource_cfi")
+process.load("CalibTracker.SiStripESProducers.fake.SiStripBadFiberFakeESSource_cfi")
+process.load("CalibTracker.SiStripESProducers.fake.SiStripBadChannelFakeESSource_cfi")
+
+process.load("CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi")
+process.siStripQualityESProducer.ListOfRecordToMerge = cms.VPSet(
+     cms.PSet( record = cms.string("SiStripBadChannelRcd"), tag    = cms.string("") ),
+     cms.PSet( record = cms.string("SiStripBadFiberRcd"),   tag    = cms.string("") ),
+     cms.PSet( record = cms.string("SiStripBadModuleRcd"),  tag    = cms.string("") ),
+     )
+
+process.reader = cms.EDAnalyzer("SiStripQualityStatistics",
+                              dataLabel = cms.untracked.string(""),
+                              TkMapFileName = cms.untracked.string("")
+                              )
+
+process.p = cms.Path(process.reader)
+
