019b20c51e10b32b9f0589dc03610e82616ca5b8
Mario Pelliccioni <sha1-6718ce1a78c0cdb74a96323fbe0c3f1949046cf2@cern.ch>
2008-12-08 11:39:40 +0000

    Added class DTChamberEfficiencyClient to gather plots from DTMonitorModule/DTChamberEfficiency

---
+++ DQM/DTMonitorClient/src/DTChamberEfficiencyClient.h
@@ -0,0 +1,77 @@
+#ifndef DTChamberEfficiencyClient_H
+#define DTChamberEfficiencyClient_H
+
+
+/** \class DTChamberEfficiencyClient
+ * *
+ *  DQM Test Client
+ *
+ *  $Date: 2008/12/05 09:41:15 $
+ *  $Revision: 1.7 $
+ *  \author  M. Pelliccioni - INFN Torino
+ *   
+ */
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include <FWCore/Framework/interface/EDAnalyzer.h>
+#include "DataFormats/Common/interface/Handle.h"
+#include <FWCore/Framework/interface/ESHandle.h>
+#include <FWCore/Framework/interface/Event.h>
+#include <FWCore/Framework/interface/MakerMacros.h>
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <FWCore/Framework/interface/LuminosityBlock.h>
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+class DTGeometry;
+class DTChamberId;
+class DTLayerId;
+
+class DTChamberEfficiencyClient: public edm::EDAnalyzer{
+
+public:
+
+  /// Constructor
+  DTChamberEfficiencyClient(const edm::ParameterSet& ps);
+  
+  /// Destructor
+  virtual ~DTChamberEfficiencyClient();
+
+protected:
+
+  void beginJob(const edm::EventSetup& c);
+  void beginRun(const edm::Run& run, const edm::EventSetup& setup);
+  void analyze(const edm::Event& e, const edm::EventSetup& c);
+  void endJob();
+
+  /// book the report summary
+  void bookHistos();
+  
+  /// DQM Client Diagnostic
+  void beginLuminosityBlock(edm::LuminosityBlock const& lumiSeg, edm::EventSetup const& context);
+  void endLuminosityBlock(edm::LuminosityBlock const& lumiSeg, edm::EventSetup const& c);
+
+private:
+
+  int nevents;
+  unsigned int nLumiSegs;
+  int prescaleFactor;
+
+  DQMStore* dbe;
+
+  edm::ESHandle<DTGeometry> muonGeom;
+
+  std::map< int, MonitorElement* > summaryHistos[2];
+
+};
+
+#endif
