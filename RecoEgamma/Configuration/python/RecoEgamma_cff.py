7b56573d71e30689da5b81279f4e516a84844d51
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2008-12-08 09:49:08 +0000

    Add to egammarecoFull the sequence for producing the interesting rechits for isolation purposes

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -4,12 +4,13 @@ from RecoEgamma.EgammaElectronProducers.electronSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.conversionSequence_cff import *
 from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
+from RecoEgamma.EgammaIsolationAlgos.interestingEgammaIsoDetIdsSequence_cff import *
 from RecoEgamma.PhotonIdentification.photonId_cff import *
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
 
 egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
-egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*photonIDSequence*eIdSequence)
+egammarecoFull = cms.Sequence(egammareco*egammaIsolationSequence*interestingEgammaIsoDetIds*photonIDSequence*eIdSequence)
 egammarecoWithID = cms.Sequence(egammareco*photonIDSequence*eIdSequence)
 egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
 egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
