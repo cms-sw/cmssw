50df7d35e28e5c608a39587d54baa922154c959c
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2008-12-08 12:14:16 +0000

    Viola's additions.

--- DQMOffline/JetMET/python/jetMETAnalyzer_cfi.py
+++ DQMOffline/JetMET/python/jetMETAnalyzer_cfi.py
@@ -1,4 +1,3 @@
-
 import FWCore.ParameterSet.Config as cms
 
 jetMETAnalyzer = cms.EDAnalyzer("JetMETAnalyzer",
@@ -10,14 +9,14 @@ jetMETAnalyzer = cms.EDAnalyzer("JetMETAnalyzer",
     OutputFileName = cms.string('jetMETMonitoring.root'),
 
     #
-    #                                
     #
-    pfJetAnalysis = cms.PSet(
-    ),
-    DoCaloMETAnalysis          = cms.untracked.bool(True),
-    DoJetAnalysis              = cms.untracked.bool(True),
+    #                                                                   
     DoPFJetAnalysis            = cms.untracked.bool(True),
+    DoCaloMETAnalysis          = cms.untracked.bool(True),
+    DoJPTJetAnalysis           = cms.untracked.bool(True),
+    DoJetAnalysis              = cms.untracked.bool(True),
     PFJetsCollectionLabel      = cms.InputTag("iterativeCone5PFJets"),
+    JPTJetsCollectionLabel     = cms.InputTag("JetPlusTrackZSPCorJetIcone5"),
     SCJetsCollectionLabel      = cms.InputTag("sisCone5CaloJets"),
     ICJetsCollectionLabel      = cms.InputTag("iterativeCone5CaloJets"),
     CaloMETCollectionLabel     = cms.InputTag("met"),
@@ -25,20 +24,20 @@ jetMETAnalyzer = cms.EDAnalyzer("JetMETAnalyzer",
 
     TriggerResultsLabel        = cms.InputTag("TriggerResults::HLT"),
     JetLo                      = cms.string("HLT_Jet30"),
-    JetHi                      = cms.string("HLT_Jet110"),
+    JetHi                      = cms.string("HLT_Jet110")
 
     #
-    # For caloMETAnalysis                                
+    # For caloMETAnalysis
     #
     caloMETAnalysis = cms.PSet(
     HLTPathsJetMB = cms.vstring(),
 #   When it is empty, it accepts all the triggers
 #    HLTPathsJetMB = cms.vstring("HLT_L1Jet15",
-#	                     "HLT_Jet30", 
-#	                     "HLT_Jet50", 
-#                            "HLT_Jet80", 
-#	                     "HLT_Jet110", 
-#	                     "HLT_Jet180",
+#                            "HLT_Jet30",
+#                            "HLT_Jet50",
+#                            "HLT_Jet80",
+#                            "HLT_Jet110",
+#                            "HLT_Jet180",
 #                            "HLT_DiJetAve15",
 #                            "HLT_DiJetAve30",
 #                            "HLT_DiJetAve50",
@@ -68,6 +67,53 @@ jetMETAnalyzer = cms.EDAnalyzer("JetMETAnalyzer",
         etaMax  = cms.double(5.0),
         pMax    = cms.double(500.0),
         phiMax  = cms.double(3.2)
-    )
+    ),
+
+    #
+    # For PF jetAnalysis
+    #
+    pfJetAnalysis = cms.PSet(
+        ptThreshold = cms.double(3.),
+        eBin    = cms.int32(100),
+        phiMin  = cms.double(-3.2),
+        ptBin   = cms.int32(100),
+        eMin    = cms.double(0.0),
+        eMax    = cms.double(500.0),
+        pMin    = cms.double(0.0),
+        etaBin  = cms.int32(100),
+        etaMin  = cms.double(-5.0),
+        ptMin   = cms.double(0.0),
+        phiBin  = cms.int32(70),
+        pBin    = cms.int32(100),
+        ptMax   = cms.double(50.0),
+        etaMax  = cms.double(5.0),
+        pMax    = cms.double(500.0),
+        phiMax  = cms.double(3.2)
+    ),
+
+    #
+    # For JPT jetAnalysis
+    #
+    JPTJetAnalysis = cms.PSet(
+        ptThreshold = cms.double(3.),
+        eBin    = cms.int32(100),
+        phiMin  = cms.double(-3.2),
+        ptBin   = cms.int32(100),
+        eMin    = cms.double(0.0),
+        eMax    = cms.double(500.0),
+        pMin    = cms.double(0.0),
+        etaBin  = cms.int32(100),
+        etaMin  = cms.double(-5.0),
+        ptMin   = cms.double(0.0),
+        phiBin  = cms.int32(70),
+        pBin    = cms.int32(100),
+        ptMax   = cms.double(50.0),
+        etaMax  = cms.double(5.0),
+        pMax    = cms.double(500.0),
+        phiMax  = cms.double(3.2)
+    ),
+
 
 )
+
+
