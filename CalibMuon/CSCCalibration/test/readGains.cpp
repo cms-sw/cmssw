2625f96ac0788197847d91d3948182824ff53676
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2008-12-09 09:15:46 +0000

    added nonlinearity term as requirement for validation of gain values

--- CalibMuon/CSCCalibration/test/readGains.cpp
+++ CalibMuon/CSCCalibration/test/readGains.cpp
@@ -7,36 +7,36 @@
 using namespace std;
 
 int main(){
-  float gainSlope, gainIntercept,chi2;
+  float gainSlope, gainNonLin,normal;
   int index;
   int nrlines=0;
 
   std::vector<int>   index_id;
   std::vector<float> Slope;
-  std::vector<float> Intercept;
-  std::vector<float> gainChi2;
+  std::vector<float> NonLinearity;
+  std::vector<float> Normalization;
   
   std::ifstream dbdata; 
-  dbdata.open("gainSummary2008_07_15.dat",std::ios::in); 
+  dbdata.open("gainSummary2008_09_02.dat",std::ios::in); 
   if(!dbdata) {
-    std::cerr <<"Error: gainSummary2008_07_15.dat -> no such file!"<< std::endl;
+    std::cerr <<"Error: gainSummary2008_09_02.dat -> no such file!"<< std::endl;
     exit(1);
   }
 
   while (!dbdata.eof() ) { 
-    dbdata >> index >> gainSlope >> gainIntercept >>chi2 ; 
+    dbdata >> index >> gainSlope >> gainNonLin >> normal ; 
     index_id.push_back(index);
     Slope.push_back(gainSlope);
-    Intercept.push_back(gainIntercept);
-    gainChi2.push_back(chi2);
+    NonLinearity.push_back(gainNonLin);
+    Normalization.push_back(normal);
     nrlines++;
   }
   dbdata.close();
-  std::ofstream myGainsFile("goodGains2008_07_15.dat",std::ios::out);
+  std::ofstream myGainsFile("goodGains2008_09_02.dat",std::ios::out);
  
   for(int i=0; i<nrlines-1;++i){
-    if (Slope[i]!=0.0 && Slope[i]>6.0 && Slope[i]<11.0){
-      myGainsFile<<index_id[i]<<"  "<<Slope[i]<<"  "<<Intercept[i]<<"  "<<gainChi2[i]<<std::endl;
+    if (Slope[i]!=0.0 && Slope[i]>6.0 && Slope[i]<11.0 && NonLinearity[i]<50.0){
+      myGainsFile<<index_id[i]<<"  "<<Slope[i]<<"  "<<NonLinearity[i]<<"  "<<Normalization[i]<<std::endl;
       if (Slope[i]<6){
 	std::cout<<"Index "<<index_id[i]<<"  "<<Slope[i]<<std::endl;
       }
