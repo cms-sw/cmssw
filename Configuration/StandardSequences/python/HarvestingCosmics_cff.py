8af44d67a5a6be7e76c2b3cdb926c4dc65c2f313
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2008-12-09 08:02:22 +0000

    add hlt harvesting sequences for relval

--- Configuration/StandardSequences/python/HarvestingCosmics_cff.py
+++ Configuration/StandardSequences/python/HarvestingCosmics_cff.py
@@ -4,9 +4,10 @@ from DQMOffline.Configuration.DQMOfflineCosmics_SecondStep_cff import *
 from DQMOffline.Configuration.DQMOfflineCosmics_Certification_cff import *
 
 from Validation.Configuration.postValidation_cff import *
+from HLTriggerOffline.Common.HLTValidationHarvest_cff import *
 
 dqmHarvesting = cms.Path(DQMOfflineCosmics_SecondStep*DQMOfflineCosmics_Certification)
 
-validationHarvesting = cms.Path(postValidation)
+validationHarvesting = cms.Path(postValidation*hltpostvalidation)
 
 #alcaHarvesting = cms.Path()
--- Configuration/StandardSequences/python/Harvesting_cff.py
+++ Configuration/StandardSequences/python/Harvesting_cff.py
@@ -4,9 +4,10 @@ from DQMOffline.Configuration.DQMOffline_SecondStep_cff import *
 from DQMOffline.Configuration.DQMOffline_Certification_cff import *
 
 from Validation.Configuration.postValidation_cff import *
+from HLTriggerOffline.Common.HLTValidationHarvest_cff import *
 
 dqmHarvesting = cms.Path(DQMOffline_SecondStep*DQMOffline_Certification)
 
-validationHarvesting = cms.Path(postValidation)
+validationHarvesting = cms.Path(postValidation*hltpostvalidation)
 
 #alcaHarvesting = cms.Path()
