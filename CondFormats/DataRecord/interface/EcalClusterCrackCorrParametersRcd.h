8840c027973eacd8c5a71b6219ba9ac8e42cbdaf
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-12-10 16:15:47 +0000

    add records for crack and local containment ECAL cluster corrections

---
+++ CondFormats/DataRecord/interface/EcalClusterCrackCorrParametersRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalClusterCrackCorrParametersRcd_h
+#define CondFormats_DataRecord_EcalClusterCrackCorrParametersRcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalClusterCrackCorrParametersRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalClusterCrackCorrParametersRcd> {};
+#endif
