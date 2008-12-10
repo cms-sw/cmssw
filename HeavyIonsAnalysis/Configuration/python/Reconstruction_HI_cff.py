c613923712c3bf73e5d19a129436190e47560719
Yen-Jie Lee <sha1-f0ec4886d17e8de0a4ad98a0607824753c761f00@cern.ch>
2008-12-10 13:55:19 +0000

    Included Multi5x5 algorithm.

--- HeavyIonsAnalysis/Configuration/python/Reconstruction_HI_cff.py
+++ HeavyIonsAnalysis/Configuration/python/Reconstruction_HI_cff.py
@@ -10,6 +10,8 @@ from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalPreshowerRecHit_cfi import *
 from RecoEcal.EgammaClusterProducers.islandClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.hybridClusteringSequence_cff import *
+from RecoEcal.EgammaClusterProducers.multi5x5ClusteringSequence_cff import *
+from RecoEcal.EgammaClusterProducers.multi5x5PreshowerClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.preshowerClusteringSequence_cff import *
 from RecoLocalCalo.Configuration.hcalLocalReco_cff import *
 from RecoLocalMuon.Configuration.RecoLocalMuon_cff import *
@@ -17,9 +19,12 @@ from RecoMuon.Configuration.RecoMuon_cff import *
 from HeavyIonsAnalysis.Configuration.IterativeConePu5Jets_PbPb_cff import *
 from RecoHI.HiCentralityAlgos.HiCentrality_cfi import *
 from RecoHI.HiEvtPlaneAlgos.HiEvtPlane_cfi import *
+# ECAL Clustering Algorithm
+# Added Multi5x5 algorithm into the reconstruction by Yen-Jie
 ecalloc = cms.Sequence(ecalWeightUncalibRecHit*ecalRecHit*ecalPreshowerRecHit)
-ecalcst = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence)
+ecalcst = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence)
 caloReco = cms.Sequence(ecalloc*ecalcst*hcalLocalRecoSequence)
+
 reconstruct_PbPb = cms.Sequence(hiTrackingWithOfflineBeamSpot*caloReco*runjets*hiCentrality*hiEvtPlane)
 
 
