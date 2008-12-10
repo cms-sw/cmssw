6df0bbd21458c3c859bcf8d650bd4d2bd8f3d9cb
dorbaker <sha1-c0c861e1621fbd0445e7a5339f107fcc8dbfe745@cern.ch>
2008-12-10 19:58:00 +0000

    For Full Pattern Reco

--- FastSimulation/TrackingRecHitProducer/src/SealModules.cc
+++ FastSimulation/TrackingRecHitProducer/src/SealModules.cc
@@ -2,11 +2,29 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+//--- Our components which we want Framework to know about:                                                                    
+#include "RecoLocalTracker/ClusterParameterEstimator/interface/PixelClusterParameterEstimator.h"
+#include "RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h"
+#include "FastSimulation/TrackingRecHitProducer/interface/FastStripCPEESProducer.h"
+#include "FastSimulation/TrackingRecHitProducer/interface/FastPixelCPEESProducer.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "FastSimulation/TrackingRecHitProducer/interface/SiClusterTranslator.h"
 #include "FastSimulation/TrackingRecHitProducer/interface/SiTrackerGaussianSmearingRecHitConverter.h"
 #include "FastSimulation/TrackingRecHitProducer/interface/TrackingRecHitTranslator.h"
+
+#include "FastSimulation/TrackingRecHitProducer/interface/FastPixelCPE.h"
+#include "FastSimulation/TrackingRecHitProducer/interface/FastStripCPE.h"
+
+EVENTSETUP_DATA_REG(FastPixelCPE);
+EVENTSETUP_DATA_REG(FastStripCPE);
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(SiTrackerGaussianSmearingRecHitConverter);
 DEFINE_ANOTHER_FWK_MODULE(TrackingRecHitTranslator);
-
+DEFINE_ANOTHER_FWK_MODULE(SiClusterTranslator);
