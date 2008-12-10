c026c54410bcdb331bd16e700c71c4d5d2c1bcef
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2008-12-10 23:24:10 +0000

    adding concatenating and decomposition methods for trigger paths

--- CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h
+++ CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h
@@ -2,10 +2,20 @@
 #define CONDFORMATS_HLTOBJECTS_ALCARECOTRIGGERBITS_H
 #include <string>
 #include <map>
-class AlCaRecoTriggerBits{
+#include <vector>
+
+class AlCaRecoTriggerBits {
 public:
   AlCaRecoTriggerBits();
   ~AlCaRecoTriggerBits();
-  std::map<std::string,std::string> m_alcarecoTotrig;
+
+  /// Compose several paths into one string :
+  std::string compose(const std::vector<std::string> &paths) const;
+  /// Decompose one value of map from concatenated string
+  std::vector<std::string> decompose(const std::string &concatPaths) const;
+  /// Delimeter for composing paths to one string in DB:
+  static const std::string::value_type delimeter_;
+
+  std::map<std::string,std::string> m_alcarecoToTrig;
 };
 #endif
--- CondFormats/HLTObjects/src/AlCaRecoTriggerBits.cc
+++ CondFormats/HLTObjects/src/AlCaRecoTriggerBits.cc
@@ -1,3 +1,43 @@
 #include "CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h"
+
+
 AlCaRecoTriggerBits::AlCaRecoTriggerBits(){}
 AlCaRecoTriggerBits::~AlCaRecoTriggerBits(){}
+
+const std::string::value_type AlCaRecoTriggerBits::delimeter_ = ';'; // separator
+
+//_____________________________________________________________________
+std::string AlCaRecoTriggerBits::compose(const std::vector<std::string> &paths) const
+{
+  std::string mergedPaths;
+  for (std::vector<std::string>::const_iterator iPath = paths.begin();
+       iPath != paths.end(); ++iPath) {
+    if (iPath != paths.begin()) mergedPaths += delimeter_;
+    mergedPaths += *iPath;
+  }
+
+  return mergedPaths;
+}
+
+//_____________________________________________________________________
+std::vector<std::string> AlCaRecoTriggerBits::decompose(const std::string &s) const
+{
+  // decompose 's' into its parts that are separated by ';'
+  // (similar as in 
+  //  Alignment/CommonAlignmentAlgorithm/src/AlignmentParameterSelector.cc)
+
+  std::vector<std::string> result;
+
+  std::string::size_type previousPos = 0;
+  while (true) {
+    const std::string::size_type delimiterPos = s.find(delimeter_, previousPos);
+    if (delimiterPos == std::string::npos) {
+      result.push_back(s.substr(previousPos)); // until end
+      break;
+    }
+    result.push_back(s.substr(previousPos, delimiterPos - previousPos));
+    previousPos = delimiterPos + 1; // +1: skip delim
+  }
+
+  return result;
+}
