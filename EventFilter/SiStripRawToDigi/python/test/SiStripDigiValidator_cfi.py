07cdbb8be2c0d27a3bcaf1696027702f972d291a
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2008-12-11 16:52:48 +0000

    fixed default labels

--- EventFilter/SiStripRawToDigi/python/test/SiStripDigiValidator_cfi.py
+++ EventFilter/SiStripRawToDigi/python/test/SiStripDigiValidator_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 DigiValidator = cms.EDFilter("SiStripDigiValidator",
     FedRawDataMode = cms.untracked.bool(False),
-    Collection2 = cms.untracked.InputTag("SiStripDigis","ZeroSuppressed"),
-    Collection1 = cms.untracked.InputTag("SiStripDigiSource")
+    Collection2 = cms.untracked.InputTag("siStripDigis","ZeroSuppressed"),
+    Collection1 = cms.untracked.InputTag("DigiSource")
 )
 
 
