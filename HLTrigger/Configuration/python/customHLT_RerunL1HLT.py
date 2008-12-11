c45e89ef3d6497adc45186808e8402ed716bc522
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-12-11 08:08:24 +0000

    For rerunning L1+HLT on RelVal's step1 files.

---
+++ HLTrigger/Configuration/python/customHLT_RerunL1HLT.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1Trigger.Configuration import patchToRerunL1Emulator
+
+def customise(process):
+
+    process.options.wantSummary = cms.untracked.bool(True)
+
+# re-running HLT requires new process name!
+
+    process.setName_("HLT2")
+    process.hltTrigReport.HLTriggerResults = cms.InputTag( 'TriggerResults','','HLT2' )
+
+# run trigger primitive generation on unpacked digis, then central L1
+
+    process.load("L1Trigger.Configuration.CaloTriggerPrimitives_cff")
+    process.simEcalTriggerPrimitiveDigis.Label = 'ecalDigis'
+    process.simHcalTriggerPrimitiveDigis.inputLabel = 'hcalDigis'
+
+# patch the process to use 'sim*Digis' from the L1 emulator
+# instead of 'hlt*Digis' from the RAW data
+
+    patchToRerunL1Emulator.switchToSimGtDigis( process )
+
+    return(process)
