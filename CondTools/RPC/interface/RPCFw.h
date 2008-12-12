d48ed5d85b461b8a1f678b7df52405355719b6ec
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-12-12 18:46:43 +0000

    some bug fixes

--- CondTools/RPC/interface/RPCFw.h
+++ CondTools/RPC/interface/RPCFw.h
@@ -5,8 +5,8 @@
  * \class RPCFw
  *  Reads data from OMDS and creates conditioning objects
  *
- *  $Date: 2008/10/11 08:46:50 $
- *  $Revision: 1.4 $
+ *  $Date: 2008/12/12 17:46:45 $
+ *  $Revision: 1.5 $
  *  \author D. Pagano - Dip. Fis. Nucl. e Teo. & INFN Pavia
  */
 
@@ -14,15 +14,11 @@
 
 #include "CondTools/RPC/interface/RPCDBCom.h"
 #include "CoralBase/TimeStamp.h"
-<<<<<<< RPCFw.h
 #include "CondTools/RPC/interface/RPCImonSH.h"
 #include "CondTools/RPC/interface/RPCVmonSH.h"
 #include "CondTools/RPC/interface/RPCStatusSH.h"
 #include "CondTools/RPC/interface/RPCTempSH.h"
 
-=======
-#include "CondTools/RPC/interface/RPCCondSH.h"
->>>>>>> 1.4
 #include "CondTools/RPC/interface/RPCGasSH.h"
 #include "CondTools/RPC/interface/RPCIDMapSH.h"
 
@@ -46,7 +42,6 @@ public:
 
 
   coral::TimeStamp thr;
-<<<<<<< RPCFw.h
   std::vector<RPCObImon::I_Item> createIMON(int from);
   std::vector<RPCObVmon::V_Item> createVMON(int from); 
   std::vector<RPCObStatus::S_Item> createSTATUS(int from); 
@@ -54,14 +49,6 @@ public:
   std::vector<RPCObTemp::T_Item> createT(int from);
   std::vector<RPCObPVSSmap::Item> createIDMAP();
 
-=======
-  std::vector<RPCObCond::Item> createIMON(int from);
-  std::vector<RPCObCond::Item> createVMON(int from); 
-  std::vector<RPCObCond::Item> createSTATUS(int from); 
-  std::vector<RPCObGas::Item> createGAS(int from);
-  std::vector<RPCObCond::Item> createT(int from);
->>>>>>> 1.4
-  
 private:
   std::string m_connectionString;
   std::string m_userName;
