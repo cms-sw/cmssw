83a640bffd0b5f1261e480c03225a85e4712bae8
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-12-12 13:31:42 +0000

    edproducer

--- EventFilter/SiStripRawToDigi/python/SiStripRawToClusters_cfi.py
+++ EventFilter/SiStripRawToDigi/python/SiStripRawToClusters_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from CommonTools.SiStripClusterization.SiStripClusterization_cfi import *
 SiStripRawToClustersFacility = cms.EDProducer("SiStripRawToClusters",
     SiStripClusterization,
-    ProductLabel = cms.InputTag('rawDataCollector')
+    FedRawData = cms.InputTag('rawDataCollector')
 )
 
 
