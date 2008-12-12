fb4f08220331429df68e1080a9db8d042b7a8503
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-12-12 18:03:42 +0000

    Fix the sequence adding back reso and eff. clients

--- DQM/DTMonitorClient/python/dtDQMOfflineClients_cff.py
+++ DQM/DTMonitorClient/python/dtDQMOfflineClients_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.DTMonitorClient.dtChamberEfficiencyClient_cfi import *
-from DQM.DTMonitorClient.dtChamberEfficiencyTest_cfi import *
 from DQM.DTMonitorClient.dtSegmentAnalysisTest_cfi import *
 segmentTest.normalizeHistoPlots = True
 #segmentTest.detailedAnalysis = True
@@ -15,9 +14,9 @@ dtQualityTests = cms.EDFilter("QualityTester",
                          getQualityTestsFromFile = cms.untracked.bool(True)
                          )
 
-<<<<<<< dtDQMOfflineClients_cff.py
-dtClients = cms.Sequence(segmentTest*chamberEfficiencyClient*dtOfflineSummaryClients)
-=======
-dtClients = cms.Sequence(segmentTest*dtResolutionAnalysisTest*dtOfflineSummaryClients)
->>>>>>> 1.8
+
+dtClients = cms.Sequence(segmentTest*
+                         dtResolutionAnalysisTest*
+                         dtChamberEfficiencyClient*
+                         dtOfflineSummaryClients)
 
