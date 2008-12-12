624dba62d000297a22eb0eb83c14d3c444b884ce
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2008-12-12 13:57:35 +0000

    update HLT bits for startup menus

--- Calibration/HcalAlCaRecoProducers/python/isoMuonHLT_cfi.py
+++ Calibration/HcalAlCaRecoProducers/python/isoMuonHLT_cfi.py
@@ -2,7 +2,7 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi
 
 isoMuonHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
   
-    HLTPaths = ['HLT_L1MuOpen','HLT_IsoMu11'],
+    HLTPaths = ['HLT_L1MuOpen','HLT_IsoMu11','HLT_L2Mu9','HLT_Mu5','HLT_Mu9'],
     throw = False #dont throw except on unknown path name
 
 )
