92538c970e35389a9cda8f512f151438dadd5363
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2008-12-12 16:33:39 +0000

    Add new configuration files to read Offline DataBase

--- SimCalorimetry/EcalTrigPrimProducers/test/readDBTPConds_cfg.py
+++ SimCalorimetry/EcalTrigPrimProducers/test/readDBTPConds_cfg.py
@@ -20,55 +20,56 @@ process.ecalTPConditions = cms.ESSource("PoolDBESSource",
     loadAll = cms.bool(True),
     toGet = cms.VPSet(cms.PSet(
         record = cms.string('EcalTPGPedestalsRcd'),
-        tag = cms.string('EcalTPGPedestals_test')
+        tag = cms.string('EcalTPGPedestals_craft')
     ), 
         cms.PSet(
             record = cms.string('EcalTPGLinearizationConstRcd'),
-            tag = cms.string('EcalTPGLinearizationConst_test')
+            tag = cms.string('EcalTPGLinearizationConst_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGSlidingWindowRcd'),
-            tag = cms.string('EcalTPGSlidingWindow_test')
+            tag = cms.string('EcalTPGSlidingWindow_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGFineGrainEBIdMapRcd'),
-            tag = cms.string('EcalTPGFineGrainEBIdMap_test')
+            tag = cms.string('EcalTPGFineGrainEBIdMap_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGFineGrainStripEERcd'),
-            tag = cms.string('EcalTPGFineGrainStripEE_test')
+            tag = cms.string('EcalTPGFineGrainStripEE_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGFineGrainTowerEERcd'),
-            tag = cms.string('EcalTPGFineGrainTowerEE_test')
+            tag = cms.string('EcalTPGFineGrainTowerEE_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGLutIdMapRcd'),
-            tag = cms.string('EcalTPGLutIdMap_test')
+            tag = cms.string('EcalTPGLutIdMap_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGWeightIdMapRcd'),
-            tag = cms.string('EcalTPGWeightIdMap_test')
+            tag = cms.string('EcalTPGWeightIdMap_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGWeightGroupRcd'),
-            tag = cms.string('EcalTPGWeightGroup_test')
+            tag = cms.string('EcalTPGWeightGroup_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGLutGroupRcd'),
-            tag = cms.string('EcalTPGLutGroup_test')
+            tag = cms.string('EcalTPGLutGroup_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGFineGrainEBGroupRcd'),
-            tag = cms.string('EcalTPGFineGrainEBGroup_test')
+            tag = cms.string('EcalTPGFineGrainEBGroup_craft')
         ), 
         cms.PSet(
             record = cms.string('EcalTPGPhysicsConstRcd'),
-            tag = cms.string('EcalTPGPhysicsConst_test')
+            tag = cms.string('EcalTPGPhysicsConst_craft')
         )),
     messagelevel = cms.untracked.uint32(3),
     timetype = cms.string('runnumber'),
-    connect = cms.string('oracle://ecalh4db/TEST02'),
+#    connect = cms.string('oracle://ecalh4db/TEST02'),
+    connect = cms.string('sqlite_file:DB.db'),
     authenticationMethod = cms.untracked.uint32(1),
     loadBlobStreamer = cms.untracked.bool(True)
 )
