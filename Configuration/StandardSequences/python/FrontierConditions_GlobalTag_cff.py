6e295fd0c62b077c288c3168838c9379d947da77
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2008-12-14 18:45:34 +0000

    remove more fake sources (towards removing es_prefern global tag)

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
@@ -3,10 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
 
 # the following are needed for non PoolDBESSources
-
 from CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi import *
-from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_cff import *
 from CalibCalorimetry.HcalPlugins.Hcal_Conditions_forGlobalTag_cff import *
-from L1Trigger.Configuration.L1Trigger_FakeConditions_cff import *
+from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_cff import *
 
 es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
