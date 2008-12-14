1ac7f9148803451cb9ad8ff60ea8ddae7c307211
Paolo Azzurri <sha1-685b616caa4a1ff5ce8554bbb7f424196046512a@cern.ch>
2008-12-14 16:32:54 +0000

    Modified to allow disabling the adapted PV cuts and apply absolute cuts on d0, z0 and layers with or witout measurements (as requested by Ian Tomalin)

--- RecoTracker/FinalTrackSelectors/python/selectLoose_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/selectLoose_cfi.py
@@ -3,29 +3,37 @@ import FWCore.ParameterSet.Config as cms
 selectLoose = cms.EDFilter("AnalyticalTrackSelector",
     src = cms.InputTag("generalTracks"),
     keepAllTracks = cms.bool(False), ## if set to true tracks failing this filter are kept in the output
-
     beamspot = cms.InputTag("offlineBeamSpot"),
+                           
+    vertices = cms.InputTag("pixelVertices"),
+    vtxNumber = cms.int32(-1),
     vtxTracks = cms.uint32(3), ## at least 3 tracks
-
     vtxChi2Prob = cms.double(0.01), ## at least 1% chi2nprobability (if it has a chi2)
 
     #untracked bool copyTrajectories = true // when doing retracking before
     copyTrajectories = cms.untracked.bool(False),
-    vertices = cms.InputTag("pixelVertices"),
+    copyExtras = cms.untracked.bool(True), ## set to false on AOD
     qualityBit = cms.string('loose'), ## set to '' or comment out if you don't want to set the bit
 
-    vtxNumber = cms.int32(-1),
-    copyExtras = cms.untracked.bool(True), ## set to false on AOD
-
-    minNumberLayers = cms.uint32(0),
-    # parameters for cuts: loose
+    # parameters for adapted optimal cuts on chi2 and primary vertex compatibility
     chi2n_par = cms.double(2.0),
-    d0_par2 = cms.vdouble(0.55, 4.0),
+    res_par = cms.vdouble(0.003, 0.01),
     d0_par1 = cms.vdouble(0.55, 4.0),
     dz_par1 = cms.vdouble(0.65, 4.0),
-    # resolution parameters: normal 
-    res_par = cms.vdouble(0.003, 0.01),
-    dz_par2 = cms.vdouble(0.45, 4.0)
+    d0_par2 = cms.vdouble(0.55, 4.0),
+    dz_par2 = cms.vdouble(0.45, 4.0),
+    # Boolean indicating if adapted primary vertex compatibility cuts are to be applied.
+    applyAdaptedPVCuts = cms.bool(True),
+
+    # Impact parameter absolute cuts.
+    max_d0 = cms.double(100.),
+    max_z0 = cms.double(100.),
+
+    # Cuts on numbers of layers with hits/3D hits/lost hits. 
+    minNumberLayers = cms.uint32(0),
+    minNumber3DLayers = cms.uint32(0),
+    maxNumberLostLayers = cms.uint32(999)
+ 
 )
 
 
