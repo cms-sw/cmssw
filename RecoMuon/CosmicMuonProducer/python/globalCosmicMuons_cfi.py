be326d3a33ccb747c8790bf998e16f823a39a955
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2008-12-15 19:23:44 +0000

    import trackmatcher

--- RecoMuon/CosmicMuonProducer/python/globalCosmicMuons_cfi.py
+++ RecoMuon/CosmicMuonProducer/python/globalCosmicMuons_cfi.py
@@ -2,10 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
 from RecoMuon.TrackingTools.MuonTrackLoader_cff import *
+from RecoMuon.GlobalTrackingTools.GlobalMuonTrackMatcher_cff import *
+
 globalCosmicMuons = cms.EDProducer("GlobalCosmicMuonProducer",
     MuonTrackLoaderForCosmic,
     MuonServiceProxy,
     TrajectoryBuilderParameters = cms.PSet(
+        GlobalMuonTrackMatcher,
         MuonRecHitBuilder = cms.string('MuonRecHitBuilder'),
         TrackerRecHitBuilder = cms.string('WithTrackAngle'),
         TkTrackCollectionLabel = cms.string('generalTracks'),
@@ -13,15 +16,7 @@ globalCosmicMuons = cms.EDProducer("GlobalCosmicMuonProducer",
         SmootherParameters = cms.PSet(
             PropagatorAlong = cms.string('SmartPropagatorAny'),
             PropagatorOpposite = cms.string('SmartPropagatorAnyOpposite')
-        ),
-        GlobalMuonTrackMatcher = cms.PSet(
-            MinP = cms.double(2.5),
-            Chi2Cut = cms.double(50.0),
-            MinPt = cms.double(1.0),
-            DeltaDCut = cms.double(10.0),
-            Propagator = cms.string('SteppingHelixPropagatorAny'),
-            DeltaRCut = cms.double(0.2)
-        ) 
+        )
     ),
     MuonCollectionLabel = cms.InputTag("cosmicMuons")
 )
