d1eb06110bbbc92b6630fae4d09859e7630fee42
Panagiotis Katsas <sha1-163d0c4b196b62c2ba4e39987db97923b896ea8b@cern.ch>
2008-12-15 14:44:43 +0000

    config for simple reconstructor

---
+++ RecoLocalCalo/CastorReco/python/CastorSimpleReconstructor_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+castorreco = cms.EDFilter("CastorSimpleReconstructor",
+    correctionPhaseNS = cms.double(0.0),
+    digiLabel = cms.InputTag("castorDigis"),
+    samplesToAdd = cms.int32(1),
+    Subdetector = cms.string('CASTOR'),
+    firstSample = cms.int32(3),
+    correctForPhaseContainment = cms.bool(False),
+    correctForTimeslew = cms.bool(False)
+)
