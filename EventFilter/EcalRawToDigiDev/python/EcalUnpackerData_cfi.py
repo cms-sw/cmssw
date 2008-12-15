591741028863f29d847a37d5126baf60c39eedb6
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-12-15 16:30:39 +0000

    leave old config pointing to new unpacker filter in EcalRawToDigi

--- EventFilter/EcalRawToDigiDev/python/EcalUnpackerData_cfi.py
+++ EventFilter/EcalRawToDigiDev/python/EcalUnpackerData_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-ecalEBunpacker = cms.EDFilter("EcalRawToDigiDev",
+ecalEBunpacker = cms.EDFilter("EcalRawToDigi",
     orderedDCCIdList = cms.untracked.vint32(1, 2, 3, 4, 5, 
         6, 7, 8, 9, 10, 
         11, 12, 13, 14, 15, 
