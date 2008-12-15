3223d0b81a33295486d7cc1f0ee4a60a3d5671fa
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2008-12-15 11:38:04 +0000

    new version

---
+++ DQMOffline/CalibMuon/test/DTtTrigDBValidation_ORCONsqlite_cfg.py
@@ -0,0 +1,80 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("CALIB")
+process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
+
+process.load("Geometry.DTGeometry.dtGeometry_cfi")
+
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.load("DQMServices.Core.DQM_cfg")
+
+process.source = cms.Source("EmptyIOVSource",
+    lastRun = cms.untracked.uint32(100),
+    timetype = cms.string('runnumber'),
+    firstRun = cms.untracked.uint32(1),
+    interval = cms.uint32(90)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.ttrigRef = cms.ESSource("PoolDBESSource",
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0),
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.string('runnumber'),
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('DTTtrigRcd'),
+        tag = cms.string('tTrig_CRUZET_080708_2019'),
+        label = cms.untracked.string('ttrigRef')
+    ), 
+        cms.PSet(
+            record = cms.string('DTTtrigRcd'),
+            tag = cms.string('ttrig'),
+            connect = cms.untracked.string('sqlite_file:ttrig_second_52256.db'),
+            label = cms.untracked.string('ttrigToValidate')
+        )),
+    connect = cms.string('oracle://cms_orcoff_prod/CMS_COND_20X_DT'),
+    siteLocalConfig = cms.untracked.bool(False)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('dtTTrigAnalyzer'),
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        t0dbValidation = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000)
+        ),
+        noLineBreaks = cms.untracked.bool(True),
+        threshold = cms.untracked.string('DEBUG'),
+        FwkJob = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        DEBUG = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        )
+    ),
+    categories = cms.untracked.vstring('tTrigdbValidation'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.dtTTrigAnalyzer = cms.EDFilter("DTtTrigDBValidation",
+    labelDBRef = cms.untracked.string('ttrigRef'),
+    t0TestName = cms.untracked.string('tTrigDifferenceInRange'),
+    OutputFileName = cms.untracked.string('tTrigTestMonitoring.root'),
+    labelDB = cms.untracked.string('ttrigToValidate')
+)
+
+process.qTester = cms.EDFilter("QualityTester",
+    prescaleFactor = cms.untracked.int32(1),
+    qtList = cms.untracked.FileInPath('DQMOffline/CalibMuon/data/QualityTests_ttrig.xml')
+)
+
+process.p = cms.Path(process.dtTTrigAnalyzer*process.qTester)
+process.DQM.collectorHost = ''
+
+
