a25203295f851fa907f77193fa1a2c854543015c
Joanne Cole <sha1-fa3e5896900e286b105d4a33c21e2ef8d2c23547@cern.ch>
2008-12-16 11:07:27 +0000

    removing things that were committed accidentally

--- CalibTracker/SiStripDCS/test/plugins/SealModules.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_SEAL_MODULE();
-
-#include "CalibTracker/SiStripDCS/test/plugins/testSiStripDcuDetIdMap.h"
-DEFINE_ANOTHER_FWK_MODULE(testSiStripDcuDetIdMap);
-
-#include "CalibTracker/SiStripDCS/test/plugins/testbuilding.h"
-DEFINE_ANOTHER_FWK_MODULE(testbuilding);
-
