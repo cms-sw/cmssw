3d6692dd87e303771eaef74096080b52af0ca05a
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2008-12-16 14:01:21 +0000

    swapped class name sistrip::RawToDigiUnpacker with SiStripRawToDigiUnpacker

--- EventFilter/SiStripRawToDigi/interface/SiStripRawToDigiUnpacker.h
+++ EventFilter/SiStripRawToDigi/interface/SiStripRawToDigiUnpacker.h
@@ -11,9 +11,14 @@
 #include <string>
 #include <vector>
  
+/// Fed9U classes
 namespace Fed9U { class Fed9UEvent; }
-namespace sistrip { class RawToDigiUnpacker; }
+
+/// sistrip classes
 namespace sistrip { class RawToClustersLazyUnpacker; }
+namespace sistrip { class RawToDigiUnpacker; }
+
+/// other classes
 class FEDRawDataCollection;
 class FEDRawData;
 class SiStripDigi;
@@ -23,155 +28,48 @@ class SiStripFedCabling;
 
 /**
    @file EventFilter/SiStripRawToDigi/interface/SiStripRawToDigiUnpacker.h
-   @class SiStripRawToDigiUnpacker 
+   @class sistrip::RawToDigiUnpacker 
    @brief Takes collection of FEDRawData as input and creates digis.
 */
 
 class SiStripRawToDigiUnpacker {
   
-  friend class SiStripRawToClustersLazyUnpacker;
-  friend class sistrip::RawToDigiUnpacker;
-  friend class sistrip::RawToClustersLazyUnpacker;
-
  public:
   
   typedef edm::DetSetVector<SiStripDigi> Digis;
   typedef edm::DetSetVector<SiStripRawDigi> RawDigis;
-
+  
   /// constructor
   SiStripRawToDigiUnpacker( int16_t appended_bytes, int16_t fed_buffer_dump_freq, int16_t fed_event_dump_freq, int16_t trigger_fed_id, bool using_fed_key );
-
+  
   /// default constructor
   ~SiStripRawToDigiUnpacker();
-
+  
   /// creates digis
   void createDigis( const SiStripFedCabling&, const FEDRawDataCollection&, SiStripEventSummary&, RawDigis& scope_mode, RawDigis& virgin_raw, RawDigis& proc_raw, Digis& zero_suppr );
   
-  /// trigger info
-  void triggerFed( const FEDRawDataCollection&, SiStripEventSummary&, const uint32_t& event );
+  /// sets the SiStripEventSummary 
+  void updateEventSummary( const Fed9U::Fed9UEvent* const, SiStripEventSummary& );
   
-  /// Removes any data appended prior to FED buffer and reorders 32-bit words if swapped.
-  void locateStartOfFedBuffer( const uint16_t& fed_id, const FEDRawData& input, FEDRawData& output );
-
-  /// verbosity
-  inline void quiet( bool );
-
-  /// EventSummary update request -> not yet implemented for FEDBuffer class
-  inline void useDaqRegister( bool );
+  /// retrieve unpacker
+  inline sistrip::RawToDigiUnpacker* unpacker();
   
  private:
   
-  /// fill DetSetVectors using registries
-  void update( RawDigis& scope_mode, RawDigis& virgin_raw, RawDigis& proc_raw, Digis& zero_suppr );
+  sistrip::RawToDigiUnpacker* unpacker_;
+  Fed9U::Fed9UEvent* fedEvent_;
 
-  /// private default constructor
-  SiStripRawToDigiUnpacker();
-
-  /// sets the SiStripEventSummary -> not yet implemented for FEDBuffer class
-  void updateEventSummary( const sistrip::FEDBuffer&, SiStripEventSummary& );
-  
-  /// order of strips
-  inline void readoutOrder( uint16_t& physical_order, uint16_t& readout_order );
-
-  /// order of strips
-  inline void physicalOrder( uint16_t& readout_order, uint16_t& physical_order ); 
-
-  /// returns buffer format
-  inline sistrip::FedBufferFormat fedBufferFormat( const uint16_t& register_value );
-
-  /// returns buffer readout mode
-  inline sistrip::FedReadoutMode fedReadoutMode( const uint16_t& register_value );
-
-  /// dumps raw data to stdout (NB: payload is byte-swapped,headers/trailer are not).
-  void dumpRawData( uint16_t fed_id, const FEDRawData&, std::stringstream& );
-  
-  /// catches all possible exceptions and rethrows them as cms::Exceptions  
-  void handleException( std::string method_name, std::string extra_info = "" );
-  
-  /// method to clear registries and digi collections
-  void cleanupWorkVectors();
-
-  /// private class to register start and end index of digis in a collection
-  class Registry {
-  public:
-    /// constructor
-    Registry(uint32_t aDetid, uint16_t firstStrip, size_t indexInVector, uint16_t numberOfDigis) : 
-      detid(aDetid), first(firstStrip), index(indexInVector), length(numberOfDigis) {}
-    /// < operator to sort registries
-    bool operator<(const Registry &other) const {return (detid != other.detid ? detid < other.detid : first < other.first);}
-    /// public data members
-    uint32_t detid;
-    uint16_t first;
-    size_t index;
-    uint16_t length;
-  };
-
-  /// configurables
-  int16_t headerBytes_;
-  int16_t fedBufferDumpFreq_;
-  int16_t fedEventDumpFreq_;
-  int16_t triggerFedId_;
-  bool useFedKey_;
-
-  /// other values
-  sistrip::FEDBuffer* buffer_;
-  uint32_t event_;
-  bool once_;
-  bool first_;
-  bool useDaqRegister_;
-  bool quiet_;
-
-  /// registries
-  std::vector<Registry> zs_work_registry_;
-  std::vector<Registry> virgin_work_registry_;
-  std::vector<Registry> scope_work_registry_;
-  std::vector<Registry> proc_work_registry_;
-
-  /// digi collections
-  std::vector<SiStripDigi> zs_work_digis_;
-  std::vector<SiStripRawDigi> virgin_work_digis_; 
-  std::vector<SiStripRawDigi> scope_work_digis_; 
-  std::vector<SiStripRawDigi> proc_work_digis_;
 };
 
-void SiStripRawToDigiUnpacker::readoutOrder( uint16_t& physical_order, uint16_t& readout_order ) 
-{
-  readout_order = ( 4*((static_cast<uint16_t>((static_cast<float>(physical_order)/8.0)))%4) + static_cast<uint16_t>(static_cast<float>(physical_order)/32.0) + 16*(physical_order%8) );
-}
-
-void SiStripRawToDigiUnpacker::physicalOrder( uint16_t& readout_order, uint16_t& physical_order ) 
-{
-  physical_order = ( (32 * (readout_order%4)) + (8 * static_cast<uint16_t>(static_cast<float>(readout_order)/4.0)) - (31 * static_cast<uint16_t>(static_cast<float>(readout_order)/16.0)) );
-}
-
-sistrip::FedBufferFormat SiStripRawToDigiUnpacker::fedBufferFormat( const uint16_t& register_value ) 
-{
-  if ( (register_value&0xF) == 0x1 ) { return sistrip::FULL_DEBUG_FORMAT; }
-  else if ( (register_value&0xF) == 0x2 ) { return sistrip::APV_ERROR_FORMAT; }
-  else if ( (register_value&0xF) == 0x0 ) { return sistrip::UNDEFINED_FED_BUFFER_FORMAT; }
-  else { return sistrip::UNKNOWN_FED_BUFFER_FORMAT; }
-}
-
-sistrip::FedReadoutMode SiStripRawToDigiUnpacker::fedReadoutMode( const uint16_t& register_value ) 
-{
-  if ( ((register_value>>1)&0x7) == 0x0 ) { return sistrip::FED_SCOPE_MODE; }
-  else if ( ((register_value>>1)&0x7) == 0x1 ) { return sistrip::FED_VIRGIN_RAW; }
-  else if ( ((register_value>>1)&0x7) == 0x3 ) { return sistrip::FED_PROC_RAW; }
-  else if ( ((register_value>>1)&0x7) == 0x5 ) { return sistrip::FED_ZERO_SUPPR; }
-  else if ( ((register_value>>1)&0x7) == 0x6 ) { return sistrip::FED_ZERO_SUPPR_LITE; }
-  else { return sistrip::UNKNOWN_FED_READOUT_MODE; }
-}
-
-void SiStripRawToDigiUnpacker::quiet( bool quiet ) { quiet_ = quiet; }
-
-void SiStripRawToDigiUnpacker::useDaqRegister( bool use ) { useDaqRegister_ =  use; }
-
-
-#include "Fed9UUtils.hh"
+inline sistrip::RawToDigiUnpacker* SiStripRawToDigiUnpacker::unpacker() { return unpacker_; }
 
 namespace sistrip {
   
   class RawToDigiUnpacker {
+    
+    friend class SiStripRawToDigiUnpacker;
+    friend class sistrip::RawToClustersLazyUnpacker;
+    friend class SiStripRawToClustersLazyUnpacker;
     
   public:
     
@@ -186,22 +84,128 @@ namespace sistrip {
     
     /// creates digis
     void createDigis( const SiStripFedCabling&, const FEDRawDataCollection&, SiStripEventSummary&, RawDigis& scope_mode, RawDigis& virgin_raw, RawDigis& proc_raw, Digis& zero_suppr );
-
-    /// sets the SiStripEventSummary -> not yet implemented for FEDBuffer class
-    void updateEventSummary( const Fed9U::Fed9UEvent* const, SiStripEventSummary& );
-
-    /// retrieve unpacker
-    inline SiStripRawToDigiUnpacker* unpacker();
+    
+    /// trigger info
+    void triggerFed( const FEDRawDataCollection&, SiStripEventSummary&, const uint32_t& event );
+    
+    /// Removes any data appended prior to FED buffer and reorders 32-bit words if swapped.
+    void locateStartOfFedBuffer( const uint16_t& fed_id, const FEDRawData& input, FEDRawData& output );
+    
+    /// verbosity
+    inline void quiet( bool );
+    
+    /// EventSummary update request -> not yet implemented for FEDBuffer class
+    inline void useDaqRegister( bool );
     
   private:
     
-    SiStripRawToDigiUnpacker* unpacker_;
-    Fed9U::Fed9UEvent* fedEvent_;
+    /// fill DetSetVectors using registries
+    void update( RawDigis& scope_mode, RawDigis& virgin_raw, RawDigis& proc_raw, Digis& zero_suppr );
+    
+    /// private default constructor
+    RawToDigiUnpacker();
+    
+    /// sets the SiStripEventSummary -> not yet implemented for FEDBuffer class
+    void updateEventSummary( const sistrip::FEDBuffer&, SiStripEventSummary& );
+    
+    /// order of strips
+    inline void readoutOrder( uint16_t& physical_order, uint16_t& readout_order );
+    
+    /// order of strips
+    inline void physicalOrder( uint16_t& readout_order, uint16_t& physical_order ); 
+    
+    /// returns buffer format
+    inline sistrip::FedBufferFormat fedBufferFormat( const uint16_t& register_value );
+    
+    /// returns buffer readout mode
+    inline sistrip::FedReadoutMode fedReadoutMode( const uint16_t& register_value );
+    
+    /// dumps raw data to stdout (NB: payload is byte-swapped,headers/trailer are not).
+    void dumpRawData( uint16_t fed_id, const FEDRawData&, std::stringstream& );
+    
+    /// catches all possible exceptions and rethrows them as cms::Exceptions  
+    void handleException( std::string method_name, std::string extra_info = "" );
+    
+    /// method to clear registries and digi collections
+    void cleanupWorkVectors();
+    
+    /// private class to register start and end index of digis in a collection
+    class Registry {
+    public:
+      /// constructor
+      Registry(uint32_t aDetid, uint16_t firstStrip, size_t indexInVector, uint16_t numberOfDigis) : 
+	detid(aDetid), first(firstStrip), index(indexInVector), length(numberOfDigis) {}
+      /// < operator to sort registries
+      bool operator<(const Registry &other) const {return (detid != other.detid ? detid < other.detid : first < other.first);}
+      /// public data members
+      uint32_t detid;
+      uint16_t first;
+      size_t index;
+      uint16_t length;
+    };
+    
+    /// configurables
+    int16_t headerBytes_;
+    int16_t fedBufferDumpFreq_;
+    int16_t fedEventDumpFreq_;
+    int16_t triggerFedId_;
+    bool useFedKey_;
+    
+    /// other values
+    sistrip::FEDBuffer* buffer_;
+    uint32_t event_;
+    bool once_;
+    bool first_;
+    bool useDaqRegister_;
+    bool quiet_;
+    
+    /// registries
+    std::vector<Registry> zs_work_registry_;
+    std::vector<Registry> virgin_work_registry_;
+    std::vector<Registry> scope_work_registry_;
+    std::vector<Registry> proc_work_registry_;
+    
+    /// digi collections
+    std::vector<SiStripDigi> zs_work_digis_;
+    std::vector<SiStripRawDigi> virgin_work_digis_; 
+    std::vector<SiStripRawDigi> scope_work_digis_; 
+    std::vector<SiStripRawDigi> proc_work_digis_;
   };
   
 }
 
-inline SiStripRawToDigiUnpacker* sistrip::RawToDigiUnpacker::unpacker() { return unpacker_; }
+void sistrip::RawToDigiUnpacker::readoutOrder( uint16_t& physical_order, uint16_t& readout_order ) 
+{
+  readout_order = ( 4*((static_cast<uint16_t>((static_cast<float>(physical_order)/8.0)))%4) + static_cast<uint16_t>(static_cast<float>(physical_order)/32.0) + 16*(physical_order%8) );
+}
+
+void sistrip::RawToDigiUnpacker::physicalOrder( uint16_t& readout_order, uint16_t& physical_order ) 
+{
+  physical_order = ( (32 * (readout_order%4)) + (8 * static_cast<uint16_t>(static_cast<float>(readout_order)/4.0)) - (31 * static_cast<uint16_t>(static_cast<float>(readout_order)/16.0)) );
+}
+
+sistrip::FedBufferFormat sistrip::RawToDigiUnpacker::fedBufferFormat( const uint16_t& register_value ) 
+{
+  if ( (register_value&0xF) == 0x1 ) { return sistrip::FULL_DEBUG_FORMAT; }
+  else if ( (register_value&0xF) == 0x2 ) { return sistrip::APV_ERROR_FORMAT; }
+  else if ( (register_value&0xF) == 0x0 ) { return sistrip::UNDEFINED_FED_BUFFER_FORMAT; }
+  else { return sistrip::UNKNOWN_FED_BUFFER_FORMAT; }
+}
+
+sistrip::FedReadoutMode sistrip::RawToDigiUnpacker::fedReadoutMode( const uint16_t& register_value ) 
+{
+  if ( ((register_value>>1)&0x7) == 0x0 ) { return sistrip::FED_SCOPE_MODE; }
+  else if ( ((register_value>>1)&0x7) == 0x1 ) { return sistrip::FED_VIRGIN_RAW; }
+  else if ( ((register_value>>1)&0x7) == 0x3 ) { return sistrip::FED_PROC_RAW; }
+  else if ( ((register_value>>1)&0x7) == 0x5 ) { return sistrip::FED_ZERO_SUPPR; }
+  else if ( ((register_value>>1)&0x7) == 0x6 ) { return sistrip::FED_ZERO_SUPPR_LITE; }
+  else { return sistrip::UNKNOWN_FED_READOUT_MODE; }
+}
+
+void sistrip::RawToDigiUnpacker::quiet( bool quiet ) { quiet_ = quiet; }
+
+void sistrip::RawToDigiUnpacker::useDaqRegister( bool use ) { useDaqRegister_ =  use; }
+
 
 #endif // EventFilter_SiStripRawToDigi_SiStripRawToDigiUnpacker_H
 
