76a4552b6d906cbb972d0cc4be48a040dce3bcb2
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-12-16 12:41:40 +0000

    added SiStripDQMProfileToTkMapConverter

--- DQMOffline/CalibTracker/plugins/SealModules.cc
+++ DQMOffline/CalibTracker/plugins/SealModules.cc
@@ -1,6 +1,8 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DQMOffline/CalibTracker/plugins/SiStripQualityHotStripIdentifierRoot.h"
+#include "DQMOffline/CalibTracker/plugins/SiStripDQMProfileToTkMapConverter.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(SiStripQualityHotStripIdentifierRoot);
+DEFINE_ANOTHER_FWK_MODULE(SiStripDQMProfileToTkMapConverter);
