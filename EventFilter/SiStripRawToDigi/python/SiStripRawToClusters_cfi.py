74a12ecfef0dbcd3726a79c298249785b4d635d5
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2008-12-16 13:59:31 +0000

    changed default unpacker to use tkonlinesw libs

--- EventFilter/SiStripRawToDigi/python/SiStripRawToClusters_cfi.py
+++ EventFilter/SiStripRawToDigi/python/SiStripRawToClusters_cfi.py
@@ -1,3 +1,7 @@
+
+# module SiStripRawToClusters -> old Fed9UUtils tkonline sw unpacking libs
+# module RawToClusters -> new SistripFEDBuffer CMSSW unpacking libs
+
 import FWCore.ParameterSet.Config as cms
 
 from CommonTools.SiStripClusterization.SiStripClusterization_cfi import *
