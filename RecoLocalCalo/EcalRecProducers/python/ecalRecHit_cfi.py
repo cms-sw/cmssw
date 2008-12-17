8791406ec73fcfe841f50d9e2548242669042e0a
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-12-17 16:26:01 +0000

    fix label

--- RecoLocalCalo/EcalRecProducers/python/ecalRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalRecHit_cfi.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 # rechit producer
 ecalRecHit = cms.EDProducer("EcalRecHitProducer",
     EErechitCollection = cms.string('EcalRecHitsEE'),
-    EEuncalibRecHitCollection = cms.InputTag("ecalUncalibRecHit","EcalUncalibRecHitsEE"),
-    EBuncalibRecHitCollection = cms.InputTag("ecalUncalibRecHit","EcalUncalibRecHitsEB"),
+    EEuncalibRecHitCollection = cms.InputTag("ecalWeightUncalibRecHit","EcalUncalibRecHitsEE"),
+    EBuncalibRecHitCollection = cms.InputTag("ecalWeightUncalibRecHit","EcalUncalibRecHitsEB"),
     EBrechitCollection = cms.string('EcalRecHitsEB'),
     # channel flags to be exluded from reconstruction, e.g { 1, 2 }
     ChannelStatusToBeExcluded = cms.vint32(),
