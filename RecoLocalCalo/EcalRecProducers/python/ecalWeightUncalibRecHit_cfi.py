5a4955f2e2aa97cbf3c4e789c9095de2d652de34
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2008-12-17 15:41:53 +0000

    fix old plugin name

--- RecoLocalCalo/EcalRecProducers/python/ecalWeightUncalibRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalWeightUncalibRecHit_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # producer of rechits starting from digis
-ecalWeightUncalibRecHit = cms.EDProducer("EcalWeightUncalibRecHitProducer",
+ecalWeightUncalibRecHit = cms.EDProducer("EcalUncalibRecHitProducer",
     EBdigiCollection = cms.InputTag("ecalDigis","ebDigis"),
     EEhitCollection = cms.string('EcalUncalibRecHitsEE'),
     EEdigiCollection = cms.InputTag("ecalDigis","eeDigis"),
