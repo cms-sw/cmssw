f2c5ef66cbc59c9958d9e380af85fb717492c3f7
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-12-17 15:12:37 +0000

    change producer name

--- RecoLocalCalo/Configuration/python/ecalLocalRecoSequenceCosmics_cff.py
+++ RecoLocalCalo/Configuration/python/ecalLocalRecoSequenceCosmics_cff.py
@@ -7,11 +7,11 @@ import FWCore.ParameterSet.Config as cms
 #ECAL conditions
 #  include "CalibCalorimetry/EcalTrivialCondModules/data/EcalTrivialCondRetriever.cfi"
 #ECAL reconstruction
-from RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi import *
+from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalPreshowerRecHit_cfi import *
-ecalLocalRecoSequence = cms.Sequence(ecalFixedAlphaBetaFitUncalibRecHit*ecalWeightUncalibRecHit*ecalRecHit+ecalPreshowerRecHit)
+ecalLocalRecoSequence = cms.Sequence(ecalFixedAlphaBetaFitUncalibRecHit*ecalUncalibRecHit*ecalRecHit+ecalPreshowerRecHit)
 ecalRecHit.EBuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEB'
 ecalRecHit.EEuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEE'
 ecalRecHit.ChannelStatusToBeExcluded = [1]
