603860d04bd42ba9c68ff533a6436a730051e86c
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2008-12-17 20:20:51 +0000

    bug fix

--- CondTools/RPC/src/RPCDBCom.cc
+++ CondTools/RPC/src/RPCDBCom.cc
@@ -3,18 +3,27 @@
 #include "RelationalAccess/IConnection.h"
 #include "RelationalAccess/ISession.h"
 #include "RelationalAccess/RelationalServiceException.h"
-#include "CoralKernel/Context.h"
+#include "PluginManager/PluginManager.h"
+#include "SealKernel/Context.h"
+#include "SealKernel/ComponentLoader.h"
 
 RPCDBCom::RPCDBCom():
+  m_context( new seal::Context ),
   m_connection( 0 )
 {
-
+  seal::PluginManager* pm = seal::PluginManager::get();
+  pm->initialise();
+  seal::Handle<seal::ComponentLoader> loader = new seal::ComponentLoader( m_context.get() );
+  if ( ! loader->load( "CORAL/RelationalPlugins/oracle" ) ) {
+    throw std::runtime_error( "Could not load the OracleAccess plugin" );
+  }
 }
 
 
 RPCDBCom::~RPCDBCom()
 {
   if ( m_connection ) delete m_connection;
+  m_context = 0;
 }
 
 
@@ -23,12 +32,11 @@ RPCDBCom::connect( const std::string& connectionString,
                    const std::string& userName,
                    const std::string& password )
 {
-  coral::Context& ctx = coral::Context::instance();
-  ctx.loadComponent("CORAL/RelationalPlugins/oracle");
-  coral::IHandle<coral::IRelationalDomain> iHandle=ctx.query<coral::IRelationalDomain>("CORAL/RelationalPlugins/oracle");
-  if ( ! iHandle.isValid() ) {
-    throw std::runtime_error( "Could not load the OracleAccess plugin" );
+  seal::IHandle<coral::IRelationalDomain> iHandle = m_context->query<coral::IRelationalDomain>( "CORAL/RelationalPlugins/oracle" );
+  if ( ! iHandle ) {
+    throw coral::NonExistingDomainException( "oracle" );
   }
+
   std::pair<std::string, std::string> connectionAndSchema = iHandle->decodeUserConnectionString( connectionString );
 
   if ( ! m_connection )
@@ -36,19 +44,34 @@ RPCDBCom::connect( const std::string& connectionString,
 
   if ( ! m_connection->isConnected() )
     m_connection->connect();
-  
+
   coral::ISession* session = m_connection->newSession( connectionAndSchema.second );
-  
+
   if ( session ) {
     session->startUserSession( userName, password );
   }
-  //memory leaking
+
   return session;
 }
 
 
 void
-RPCDBCom::setVerbosityLevel( coral::MsgLevel level )
+RPCDBCom::setVerbosityLevel( seal::Msg::Level level )
 {
-  coral::MessageStream::setMsgVerbosity(level);
+  std::vector< seal::Handle<seal::IMessageService> > v_msgSvc;
+  m_context->query( v_msgSvc );
+  if ( v_msgSvc.empty() ) {
+    seal::Handle<seal::ComponentLoader> loader = new seal::ComponentLoader( m_context.get() );
+    if ( ! loader->load( "SEAL/Services/MessageService" ) ) {
+      throw std::runtime_error( "Could not load the seal message service" );
+    }
+    
+    m_context->query( v_msgSvc );
+    if ( v_msgSvc.empty() ) {
+      throw std::runtime_error( "Could not load the seal message service" );
+    }
+  }
+  seal::Handle<seal::IMessageService>& msgSvc = v_msgSvc.front();
+  msgSvc->setOutputLevel( level );
 }
+
