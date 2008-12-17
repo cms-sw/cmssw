1180dd3aaf50b9ec827b296d1048c75929bc615e
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2008-12-17 15:40:53 +0000

    revert

--- RecoLocalCalo/Configuration/python/ecalLocalRecoSequenceCosmics_cff.py
+++ RecoLocalCalo/Configuration/python/ecalLocalRecoSequenceCosmics_cff.py
@@ -1,17 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 # Calo geometry service model
+from Geometry.CaloEventSetup.CaloGeometry_cff import *
 #
 # removed by tommaso
 #
 #ECAL conditions
 #  include "CalibCalorimetry/EcalTrivialCondModules/data/EcalTrivialCondRetriever.cfi"
 #ECAL reconstruction
-from RecoLocalCalo.EcalRecProducers.ecalUncalibRecHit_cfi import *
+from RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalPreshowerRecHit_cfi import *
-ecalLocalRecoSequence = cms.Sequence(ecalFixedAlphaBetaFitUncalibRecHit*ecalUncalibRecHit*ecalRecHit+ecalPreshowerRecHit)
+ecalLocalRecoSequence = cms.Sequence(ecalFixedAlphaBetaFitUncalibRecHit*ecalWeightUncalibRecHit*ecalRecHit+ecalPreshowerRecHit)
 ecalRecHit.EBuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEB'
 ecalRecHit.EEuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEE'
 ecalRecHit.ChannelStatusToBeExcluded = [1]
