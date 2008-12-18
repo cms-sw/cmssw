79bdf47903bd739c391077ce189b6993abaf4c17
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-12-18 15:40:37 +0000

    turning it into a cmsRun loadable python, not quite done

--- CondTools/Ecal/scripts/loadConditions.py
+++ CondTools/Ecal/scripts/loadConditions.py
@@ -7,8 +7,27 @@
 # containing Ecal conditions is sent to DB
 #
 
+import FWCore.ParameterSet.Config as cms
 from   elementtree.ElementTree import parse
 import sys,os,getopt
+
+#file from which we are reading the 
+filename_  = '/tmp/EcalGainRatios.xml'
+record_    = 'EcalADCToGeVConstant'
+
+#select db parameters, dbconnect can be 'sqlite_file:bla' 
+dbconnect_    = None
+tag_ , since_ = ReadTagAndSince(filename_)
+
+#which analyzer to use for each record name
+analyzer_ =   {'EcalGainRatios':'EcalGainRatiosAnalyzer',             \
+               'EcalADCToGeVConstant':'EcalADCToGeVConstantAnalyzer', \
+               'EcalWeightXtalGroups':'EcalWeightGroupAnalyzer',      \
+               'EcalChannelStatus':'EcalChannelStatusHandler',        \
+               'EcalChannelStatus':'EcalChannelStatusAnalyzer',       \
+               'EcalTBWeights','EcalTBWeightsAnalyzer',               \
+               'EcalIntercalibConstants','EcalIntercalibConstantsAnalyzer', \
+               'EcalIntercalibErrors','EcalIntercalibErrorsAnalyzer'}
 
 
 def usage():
@@ -56,7 +75,32 @@ def readTagAndSince(filename, headertag='EcalCondHeader'):
 
     return tag,since
 
-    
+process.popconAnalyzer = cms.EDAnalyzer(analyzer_[record],
+    record = cms.string(record_+'Rcd'),
+    loggingOn= cms.untracked.bool(True),
+    SinceAppendMode=cms.bool(True),
+    Source=cms.PSet(
+    xmlFile = cms.untracked.string(filename_),
+    since = cms.untracked.int64(since_)
+    )                            
+)    
+
+
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.CondDBCommon.connect = cms.string(dbconnect_)
+process.CondDBCommon.DBParameters.authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    process.CondDBCommon,
+    timetype = cms.untracked.string('runnumber'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string(record_+'Rcd'),
+        tag = cms.string(tag_)
+         )), 
+)
+
+
+process.p = cms.Path(process.popconAnalyzer)
 
 if __name__ == "__main__":
     main()
