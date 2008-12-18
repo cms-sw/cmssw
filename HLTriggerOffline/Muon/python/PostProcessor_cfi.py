0cef2318137ff47051ef8019fb0c4ee61bf885c9
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-12-18 20:23:15 +0000

    Removed old postProcessor module name

--- HLTriggerOffline/Muon/python/PostProcessor_cfi.py
+++ HLTriggerOffline/Muon/python/PostProcessor_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#postProcessor = cms.EDFilter("PostProcessor",
 HLTMuonPostVal = cms.EDAnalyzer("PostProcessor",
     subDirs        = cms.untracked.vstring('HLT/Muon/Distributions/*'),
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
