13c4bca12dd9bb0daa44b18de69fffca7576bae6
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-12-18 19:00:46 +0000

    Added explicit call to python

--- HLTriggerOffline/Muon/test/runRelVal.sh
+++ HLTriggerOffline/Muon/test/runRelVal.sh
@@ -12,9 +12,9 @@
 # source /afs/cern.ch/user/v/valya/scratch0/setup.sh
 
 if [ "$DBSCMD_HOME" ] ; then 
-    DBS_CMD="$DBSCMD_HOME/dbsCommandLine.py -c " 
+    DBS_CMD="python $DBSCMD_HOME/dbsCommandLine.py -c " 
 else if [ "$DBS_CLIENT_ROOT" ] ; then 
-    DBS_CMD="$DBS_CLIENT_ROOT/lib/DBSAPI/dbsCommandLine.py -c"; 
+    DBS_CMD="python $DBS_CLIENT_ROOT/lib/DBSAPI/dbsCommandLine.py -c"; 
 else 
     exit
 fi
