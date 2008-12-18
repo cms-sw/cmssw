8a3891b54c8d955d46fdc076203f5abffc84c62b
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2008-12-18 11:53:35 +0000

    clean-up first step

--- RecoTracker/TkSeedGenerator/python/GlobalPixelLessSeeds_cff.py
+++ RecoTracker/TkSeedGenerator/python/GlobalPixelLessSeeds_cff.py
@@ -9,5 +9,11 @@ from RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilderWithoutRe
 from RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cfi import *
 from TrackingTools.MaterialEffects.MaterialPropagator_cfi import *
 from RecoTracker.TkSeedingLayers.PixelLessLayerPairs_cfi import *
-from RecoTracker.TkSeedGenerator.GlobalPixelLessSeeds_cfi import *
 
+from RecoTracker.TkSeedGenerator.SeedGeneratorFromRegionHitsEDProducer_cfi import *
+globalPixelLessSeeds = RecoTracker.TkSeedGenerator.SeedGeneratorFromRegionHitsEDProducer_cfi.seedGeneratorFromRegionHitsEDProducer.clone(
+    OrderedHitsFactoryPSet = cms.PSet(
+      ComponentName = cms.string('StandardHitPairGenerator'),
+      SeedingLayers = cms.string('PixelLessLayerPairs')
+    )
+)
