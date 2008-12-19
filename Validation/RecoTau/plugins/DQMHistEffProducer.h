de6c756be20f6cd2bde67c449fb3260e6bafe30b
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-12-19 19:05:50 +0000

    Plotting tools, updating BuildFile and finally removing files not needed

---
+++ Validation/RecoTau/plugins/DQMHistEffProducer.h
@@ -0,0 +1,45 @@
+#ifndef ElectroWeakAnalysis_EWKTau_DQMHistEffProducer_h
+#define ElectroWeakAnalysis_EWKTau_DQMHistEffProducer_h
+
+/** \class DQMHistEffProducer
+ *  
+ *  Class to produce efficiency histograms by dividing nominator by denominator histograms
+ *
+ *  $Date: 2008/11/18 16:41:39 $
+ *  $Revision: 1.1 $
+ *  \author Christian Veelken, UC Davis
+ */
+
+// framework & common header files
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include <string>
+#include <vector>
+
+class DQMHistEffProducer : public edm::EDAnalyzer
+{
+  struct cfgEntryPlot
+  {
+    explicit cfgEntryPlot(const edm::ParameterSet&);
+    explicit cfgEntryPlot(const std::string&, const std::string&, const std::string&);
+    std::string numerator_;
+    std::string denominator_;
+    std::string efficiency_;
+  };
+
+ public:
+  explicit DQMHistEffProducer(const edm::ParameterSet&);
+  virtual ~DQMHistEffProducer();
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob();  
+
+private:
+  std::vector<cfgEntryPlot> cfgEntryPlot_;
+};
+
+#endif
+
+
---
+++ Validation/RecoTau/plugins/DQMSimpleFileSaver.cc
@@ -0,0 +1,63 @@
+#include "Validation/RecoTau/plugins/DQMSimpleFileSaver.h"
+
+// framework & common header files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+//DQM services
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+#include <iostream>
+
+DQMSimpleFileSaver::DQMSimpleFileSaver(const edm::ParameterSet& cfg)
+{
+  //std::cout << "<DQMSimpleFileSaver::DQMSimpleFileSaver>:" << std::endl;
+
+  cfgError_ = 0;
+
+  outputFileName_ = cfg.getParameter<std::string>("outputFileName");
+  if ( outputFileName_ == "" ) {
+    edm::LogError("DQMSimpleFileSaver") << " No outputFileName specified --> histograms will NOT be saved !!";
+    cfgError_ = 1;
+  } else if ( outputFileName_.find(".root") == std::string::npos ) {
+    edm::LogError("DQMSimpleFileSaver") << " Invalid outputFileName = " << outputFileName_ << " --> histograms will NOT be saved !!";
+    cfgError_ = 1;
+  }
+}
+
+DQMSimpleFileSaver::~DQMSimpleFileSaver() 
+{
+// nothing to be done yet...
+}
+
+void DQMSimpleFileSaver::analyze(const edm::Event&, const edm::EventSetup&)
+{
+// nothing to be done yet...
+}
+
+void DQMSimpleFileSaver::endJob()
+{
+  std::cout << "<DQMSimpleFileSaver::endJob>:" << std::endl;
+
+//--- check that configuration parameters contain no errors
+  if ( cfgError_ ) {
+    edm::LogError ("endJob") << " Error in Configuration ParameterSet --> histograms will NOT be saved !!";
+    return;
+  }
+
+//--- check that DQMStore service is available
+  if ( !edm::Service<DQMStore>().isAvailable() ) {
+    edm::LogError ("endJob") << " Failed to access dqmStore --> histograms will NOT be saved !!";
+    return;
+  }  
+
+  DQMStore& dqmStore = (*edm::Service<DQMStore>());
+  dqmStore.save(outputFileName_);      
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(DQMSimpleFileSaver);
