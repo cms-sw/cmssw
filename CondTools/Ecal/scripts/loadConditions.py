c37d0f8ddf8c3f1a54563a09cf271ce7ccebc305
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2008-12-19 10:43:43 +0000

    turned into a cmsRun loadable python, still needs modification by hand, needs to read parameters from command line

--- CondTools/Ecal/scripts/loadConditions.py
+++ CondTools/Ecal/scripts/loadConditions.py
@@ -11,59 +11,31 @@ import FWCore.ParameterSet.Config as cms
 from   elementtree.ElementTree import parse
 import sys,os,getopt
 
-#file from which we are reading the 
-filename_  = '/tmp/EcalGainRatios.xml'
-record_    = 'EcalADCToGeVConstant'
 
-#select db parameters, dbconnect can be 'sqlite_file:bla' 
-dbconnect_    = None
-tag_ , since_ = ReadTagAndSince(filename_)
+##############################################################################
+# User Defined parameters to be modified
+# This will not be needed as soon as we will be able to pass arguments
+# to cmsRun
+#
 
-#which analyzer to use for each record name
-analyzer_ =   {'EcalGainRatios':'EcalGainRatiosAnalyzer',             \
-               'EcalADCToGeVConstant':'EcalADCToGeVConstantAnalyzer', \
-               'EcalWeightXtalGroups':'EcalWeightGroupAnalyzer',      \
-               'EcalChannelStatus':'EcalChannelStatusHandler',        \
-               'EcalChannelStatus':'EcalChannelStatusAnalyzer',       \
-               'EcalTBWeights','EcalTBWeightsAnalyzer',               \
-               'EcalIntercalibConstants','EcalIntercalibConstantsAnalyzer', \
-               'EcalIntercalibErrors','EcalIntercalibErrorsAnalyzer'}
 
+#file from which we are reading the conditions to load
+filename_  = '/tmp/EcalChannelStatus.xml'
+
+#record type, without 'Rcd'
+record_    = 'EcalChannelStatus'
+
+#db parameters, dbconnect can be 'sqlite_file:bla' 
+dbconnect_    = 'sqlite_file:testEcalChannelStatus.db'
+
+#
+# End User Defined parameters
+#############################################################################
 
 def usage():
 
-   print "Usage: "+sys.argv[0]+" Write me !"
-
+   print "Usage: cmsRun loadConditions.py "
    sys.exit(2)
-
-
-def main():
-
-   try:
-      opts, args = getopt.getopt(sys.argv[1:], "f:d", ["file=","dryrun"])
-
-   except getopt.GetoptError:
-      #print help information and exit
-      usage()
-      sys.exit(2)
-
-   file  = ''
-   dryrun= False
-   
-   for opt, arg in opts:   
-     if opt in ("-f", "--file"):
-         file = arg
-         if (not os.path.exists(file)) :
-            print sys.argv[0]+" File not found: "+file
-            sys.exit(2)
-
-   if file=='':
-       usage()
-       exit(2)
-   
-   tag,since = readTagAndSince(file)
-   print tag,since
-
 
 
 def readTagAndSince(filename, headertag='EcalCondHeader'):
@@ -75,13 +47,69 @@ def readTagAndSince(filename, headertag='EcalCondHeader'):
 
     return tag,since
 
-process.popconAnalyzer = cms.EDAnalyzer(analyzer_[record],
+
+tag_ , since_ = readTagAndSince(filename_)
+
+#which analyzer to use for each record name
+analyzer_ =   {'EcalGainRatios':'EcalGainRatiosAnalyzer',             \
+               'EcalADCToGeVConstant':'EcalADCToGeVConstantAnalyzer', \
+               'EcalWeightXtalGroups':'EcalWeightGroupAnalyzer',      \
+               'EcalChannelStatus':'EcalChannelStatusHandler',        \
+               'EcalChannelStatus':'EcalChannelStatusAnalyzer',       \
+               'EcalTBWeights':'EcalTBWeightsAnalyzer',               \
+               'EcalIntercalibConstants':'EcalIntercalibConstantsAnalyzer', \
+               'EcalIntercalibErrors':'EcalIntercalibErrorsAnalyzer'}
+
+
+
+
+## def main():
+
+##    try:
+##       opts, args = getopt.getopt(sys.argv[1:], "f:d", ["file=","dryrun"])
+
+##    except getopt.GetoptError:
+##       #print help information and exit
+##       usage()
+##       sys.exit(2)
+
+##    file  = ''
+##    dryrun= False
+   
+##    for opt, arg in opts:   
+##      if opt in ("-f", "--file"):
+##          file = arg
+##          if (not os.path.exists(file)) :
+##             print sys.argv[0]+" File not found: "+file
+##             sys.exit(2)
+
+##    if file=='':
+##        usage()
+##        exit(2)
+   
+##    tag,since = readTagAndSince(file)
+##    print tag,since
+
+
+process = cms.Process("LoadEcalConditions")
+
+process.source = cms.Source("EmptyIOVSource",
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    lastValue  = cms.uint64(1),
+    interval = cms.uint64(1)
+)
+
+
+
+process.popconAnalyzer = cms.EDAnalyzer(analyzer_[record_],
     record = cms.string(record_+'Rcd'),
     loggingOn= cms.untracked.bool(True),
     SinceAppendMode=cms.bool(True),
     Source=cms.PSet(
     xmlFile = cms.untracked.string(filename_),
-    since = cms.untracked.int64(since_)
+    since = cms.untracked.int64(int(since_)) #python will make the int as
+                                             #long as needed
     )                            
 )    
 
@@ -102,5 +130,5 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
 
 process.p = cms.Path(process.popconAnalyzer)
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    usage()
