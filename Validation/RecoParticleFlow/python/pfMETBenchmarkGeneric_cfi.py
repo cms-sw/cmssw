b35565af9017d4789b23d27064460dfe0c36bdb3
Rick Cavanaugh <sha1-821d96d34f96c9a702c199fc2c305113785482d3@cern.ch>
2008-12-21 18:50:07 +0000

    Added PFMET Benchmarks (python snippits and "Analyser" code) for Validation.  Modified BuildFile.  R.Cavanaugh

---
+++ Validation/RecoParticleFlow/python/pfMETBenchmarkGeneric_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+MET = 'pfMet'
+
+pfMETBenchmarkGeneric = cms.EDAnalyzer("GenericBenchmarkAnalyzer",
+    OutputFile = cms.untracked.string('metBenchmarkGeneric.root'),
+    InputTruthLabel = cms.InputTag('genParticles'),
+    InputCaloLabel = cms.InputTag('met'),
+    PlotAgainstRecoQuantities = cms.bool(False),
+    BenchmarkLabel = cms.string( MET ),
+    InputRecoLabel = cms.InputTag( MET )
+)
