66036da3635b9037d9e6a3f6b91803152a74a008
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2008-12-22 16:22:15 +0000

    CSCDaqInfo module included into SealModules

--- DQM/CSCMonitorModule/plugins/SealModule.cc
+++ DQM/CSCMonitorModule/plugins/SealModule.cc
@@ -2,7 +2,9 @@
 #include "DQM/CSCMonitorModule/interface/CSCMonitorModule.h"
 #include "DQM/CSCMonitorModule/interface/CSCHLTMonitorModule.h"
 #include "DQM/CSCMonitorModule/interface/CSCMonitorModuleCmn.h"
+#include "DQM/CSCMonitorModule/interface/CSCDaqInfo.h"
 
 DEFINE_FWK_MODULE(CSCMonitorModule);
 DEFINE_FWK_MODULE(CSCHLTMonitorModule);
 DEFINE_FWK_MODULE(CSCMonitorModuleCmn);
+DEFINE_FWK_MODULE(CSCDaqInfo);
