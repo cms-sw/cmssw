275509bb9358e4fb862db6db9f78e8af1d74698a
Junghwan Goh <sha1-a53d16ca32681a890c4e8971031f76fb9509968e@cern.ch>
2008-12-23 00:18:07 +0000

    header file path

--- Validation/Tools/plugins/SealModule.cc
+++ Validation/Tools/plugins/SealModule.cc
@@ -1,6 +1,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "Validation/Tools/src/PostProcessor.h"
+#include "Validation/Tools/interface/PostProcessor.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_FWK_MODULE(PostProcessor);
