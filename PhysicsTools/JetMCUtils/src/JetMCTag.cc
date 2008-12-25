98ec068d4bc224b060083fe4b523781d5d02b143
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2008-12-25 16:45:50 +0000

    Added the function that returns the decay mode from a tauGenJet produced by PhysicsTools/JetMCAlgos/plugins/TauGenJetProducer.h

--- PhysicsTools/JetMCUtils/src/JetMCTag.cc
+++ PhysicsTools/JetMCUtils/src/JetMCTag.cc
@@ -77,3 +77,62 @@ bool JetMCTagUtils::decayFromCHadron(const Candidate & c)
      }
    return isFromC;
 }
+
+std::string JetMCTagUtils::genTauDecayMode(const CompositePtrCandidate &c) {
+  
+  int numElectrons = 0;
+  int numMuons = 0;
+  int numChargedPions = 0;
+  int numPhotons = 0;
+  
+  const CompositePtrCandidate::daughters& daughters = c.daughterPtrVector();
+  for ( CompositePtrCandidate::daughters::const_iterator daughter = daughters.begin();
+ 	daughter != daughters.end(); ++daughter ) {
+    
+    int pdg_id = abs((*daughter)->pdgId());
+    
+    switch ( pdg_id ) {
+    case 211:
+      numChargedPions++;
+      break;
+    case 22: 
+      numPhotons++;
+      break;
+    case 11:
+      numElectrons++;
+      break;
+    case 13:
+      numMuons++;
+      break;
+    }
+  }
+  
+  if (numElectrons == 1) return std::string("electron");
+  else if (numMuons == 1) return std::string("muon");
+  switch ( numChargedPions ) {
+  case 1 : 
+    switch ( numPhotons ) {
+    case 0:
+      return std::string("oneProng0Pi0");
+    case 2:
+      return std::string("oneProng1Pi0");
+    case 4:
+      return std::string("oneProng2Pi0");
+    default:
+      return std::string("undefined");
+    }
+  case 3 : 
+    switch ( numPhotons ) {
+    case 0:
+      return std::string("threeProng0Pi0");
+    case 2:
+      return std::string("threeProng1Pi0");
+    default:
+      return std::string("undefined");
+    }
+  default:
+    return std::string("undefined");
+  }
+  
+}
+
