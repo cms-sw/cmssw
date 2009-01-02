775d1f29d0ec26fe15bb0a5d07a9e4b37b147c4c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-01-02 05:55:28 +0000

    Restore parameter set reduction

--- FWCore/ParameterSet/src/Registry.cc
+++ FWCore/ParameterSet/src/Registry.cc
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------
-// $Id: Registry.cc,v 1.10 2008/04/29 21:37:49 paterno Exp $
+// $Id: Registry.cc,v 1.13 2008/12/20 18:38:24 wmtan Exp $
 //
 // ----------------------------------------------------------------------
 
@@ -7,59 +7,40 @@
 #include "FWCore/Utilities/interface/EDMException.h"
 
 
-namespace edm
-{
-  namespace pset
-  {
-
-    bool
-    insertParameterSetIntoRegistry(Registry* reg, ParameterSet const& p)
-    {
-      ParameterSet tracked_part = p.trackedPart();
-      return reg->insertMapped(tracked_part);
-    }
-
-    void 
-    loadAllNestedParameterSets(Registry* reg, ParameterSet const& main)
-    {
-      std::vector<ParameterSet> all_main_psets;
-      explode(main, all_main_psets);
-      std::vector<ParameterSet>::const_iterator i = all_main_psets.begin();
-      std::vector<ParameterSet>::const_iterator e = all_main_psets.end();
-      for (; i != e; ++i) reg->insertMapped(*i);
-      reg->extra().setID(main.id());
-    }
-
-    edm::ParameterSetID
-    getProcessParameterSetID(Registry const* reg)
-    {
+namespace edm {
+  namespace pset {
+    ParameterSetID
+    getProcessParameterSetID(Registry const* reg) {
       return reg->extra().id();
     }
 
-    void fillMap(Registry* reg, regmap_type& fillme)
-    {
+    void fillMap(Registry* reg, regmap_type& fillme) {
       typedef Registry::const_iterator iter;
       fillme.clear();
-      for (iter i=reg->begin(), e=reg->end(); i!=e; ++i)
-	fillme[i->first].pset_ = i->second.toStringOfTracked();
+      // Note: The tracked part is in the registry.
+      // The full parameter set including the untracked parts may also be there.
+      for (iter i = reg->begin(), e = reg->end(); i != e; ++i) {
+	// Persist only fully tracked parameter sets.
+	if (i->second.isFullyTracked()) {
+	  fillme[i->first].pset_ = i->second.toString();
+	}
+      }
     }
   } // namespace pset
 
-  edm::ParameterSet getProcessParameterSet()
-  {
-    edm::pset::Registry* reg = edm::pset::Registry::instance();
-    edm::ParameterSetID id = edm::pset::getProcessParameterSetID(reg);
+  ParameterSet getProcessParameterSet() {
+    pset::Registry* reg = pset::Registry::instance();
+    ParameterSetID id = pset::getProcessParameterSetID(reg);
 
-    edm::ParameterSet result;
+    ParameterSet result;
     if (!reg->getMapped(id, result))
-      throw edm::Exception(errors::EventCorruption, "Uknown ParameterSetID")
+      throw edm::Exception(errors::EventCorruption, "Unknown ParameterSetID")
 	<< "Unable to find the ParameterSet for id: "
 	<< id
 	<< ";\nthis was supposed to be the process ParameterSet\n";
 
     return result;
   }
-
 
 } // namespace edm
 
