bbb098b52df892350c728e62abf9e2d1e982e4f1
Ian Tomalin <sha1-3fa43f967df83df8ba717ce5a271c8449420497d@cern.ch>
2009-01-07 17:06:01 +0000

    Now uses tracks with quality flag

--- RecoTracker/FinalTrackSelectors/python/MergeLargeD0TrackCollections_cff.py
+++ RecoTracker/FinalTrackSelectors/python/MergeLargeD0TrackCollections_cff.py
@@ -13,31 +13,31 @@ import RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi
 #
 mergeLargeD0step1 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
 mergeLargeD0step1.TrackProducer1 = 'generalTracks'
-mergeLargeD0step1.TrackProducer2 = 'largeD0step1WithMaterialTracks'
+mergeLargeD0step1.TrackProducer2 = 'largeD0step1Trk'
 mergeLargeD0step1.promoteTrackQuality = True
 
 #
 mergeLargeD0step2 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
 mergeLargeD0step2.TrackProducer1 = 'mergeLargeD0step1'
-mergeLargeD0step2.TrackProducer2 = 'largeD0step2WithMaterialTracks'
+mergeLargeD0step2.TrackProducer2 = 'largeD0step2Trk'
 mergeLargeD0step2.promoteTrackQuality = True
 
 #
 mergeLargeD0step3 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
 mergeLargeD0step3.TrackProducer1 = 'mergeLargeD0step2'
-mergeLargeD0step3.TrackProducer2 = 'largeD0step3WithMaterialTracks'
+mergeLargeD0step3.TrackProducer2 = 'largeD0step3Trk'
 mergeLargeD0step3.promoteTrackQuality = True
 
 #
 mergeLargeD0step4 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
 mergeLargeD0step4.TrackProducer1 = 'mergeLargeD0step3'
-mergeLargeD0step4.TrackProducer2 = 'largeD0step4WithMaterialTracks'
+mergeLargeD0step4.TrackProducer2 = 'largeD0step4Trk'
 mergeLargeD0step4.promoteTrackQuality = True
 
 #
 mergeLargeD0step5 = RecoTracker.FinalTrackSelectors.ctfrsTrackListMerger_cfi.ctfrsTrackListMerger.clone()
 mergeLargeD0step5.TrackProducer1 = 'mergeLargeD0step4'
-mergeLargeD0step5.TrackProducer2 = 'largeD0step5WithMaterialTracks'
+mergeLargeD0step5.TrackProducer2 = 'largeD0step5Trk'
 mergeLargeD0step5.promoteTrackQuality = True
 
 largeD0TrackCollectionMerging = cms.Sequence(mergeLargeD0step1 * mergeLargeD0step2 * mergeLargeD0step3 * mergeLargeD0step4 * mergeLargeD0step5) 
