43edea8fbb006a96fc7cafd4e98a63bdb6fbfde6
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-01-07 17:43:02 +0000

    Splitted in src and interface

--- MuonAnalysis/MomentumScaleCalibration/interface/MomentumScaleCorrector.h
+++ MuonAnalysis/MomentumScaleCalibration/interface/MomentumScaleCorrector.h
@@ -6,14 +6,21 @@
 #ifndef MomentumScaleCorrector_h
 #define MomentumScaleCorrector_h
 
+#include <fstream>
+#include <sstream>
 #include "MuonAnalysis/MomentumScaleCalibration/plugins/Functions.h"
 #include "FWCore/ParameterSet/interface/FileInPath.h"
-#include <fstream>
+#include "MuonAnalysis/MomentumScaleCalibrationObjects/interface/MuScleFitScale.h"
 
 /**
  * This is used to have a common set of functions for the specialized templates to use.
  * The constructor receives the name identifying the parameters for the correction function.
  * It reads the parameters from a txt file in data/.
+ *
+ * It handles multiple iterations. It is also possible to use different functions in
+ * different iterations.
+ *
+ * ATTENTION: it is important that iterations numbers in the txt file start from 0.
  */
 class MomentumScaleCorrector
 {
@@ -31,104 +38,66 @@ class MomentumScaleCorrector
     edm::FileInPath fileWithFullPath(identifier.Data());
     readParameters( fileWithFullPath.fullPath() );
   }
+  /**
+   * This constructor is used when reading parameters from the db.
+   * It receives a pointer to an object of type MuScleFitScale containing
+   * the parameters and the functions identifiers.
+   */
+  MomentumScaleCorrector( const MuScleFitScale * scaleObject )
+  {
+    scaleFunctionId_ = scaleObject->identifiers;
+    parScaleVec_ = scaleObject->parameters;
+    // Needed for the tests in convertToArrays
+    iterationNum_ = scaleFunctionId_.size()-1;
+    vector<int>::const_iterator id = scaleFunctionId_.begin();
+    for( ; id != scaleFunctionId_.end(); ++id ) {
+      scaleFunctionVec_.push_back( scaleFunctionArray[*id] );
+    }
+    // Fill the arrays that will be used when calling the correction function.
+    convertToArrays();
+  }
+
   ~MomentumScaleCorrector() {
     if( parScaleArray_ != 0 ) {
-      delete[] parScaleArray_;
+      for( unsigned int i=0; i<parScaleVec_.size(); ++i ) {
+        delete[] parScaleArray_[i];
+      }
+      delete parScaleArray_;
     }
+    delete[] scaleFunction_;
   }
   /// Method to do the corrections. It is templated to work with all the track types.
   template <class U>
   double operator()( const U & track ) {
-    // return ( scaleFunction_->scale( track.pt(), track.eta(), track.phi(), track.charge(), parScale_) );
-    return ( scaleFunction_->scale( track.pt(), track.eta(), track.phi(), track.charge(), parScaleArray_) );
+
+    // Loop on all the functions and apply them iteratively on the pt corrected by the previous function.
+    double pt = track.pt();
+    for( int i=0; i<iterationNum_; ++i ) {
+      // return ( scaleFunction_->scale( track.pt(), track.eta(), track.phi(), track.charge(), parScale_) );
+      pt = ( scaleFunction_[i]->scale( pt, track.eta(), track.phi(), track.charge(), parScaleArray_[i]) );
+    }
+    return pt;
+  }
+  /// Return the vectors of parameters
+  vector<vector<double> > parameters() const {
+    return parScaleVec_;
+  }
+  /// Return the vector of function identifiers
+  vector<int> identifiers() const {
+    return scaleFunctionId_;
   }
  protected:
   /// Parser of the parameters file
   void readParameters( TString fileName );
-  scaleFunctionBase<double * > * scaleFunction_;
-  vector<double> parScale_;
+  /// Convert vectors to arrays for faster random access.
+  void convertToArrays();
+  scaleFunctionBase<double * > ** scaleFunction_;
+  vector<scaleFunctionBase<double * > * > scaleFunctionVec_;
+  vector<int> scaleFunctionId_;
+  vector<vector<double> > parScaleVec_;
   // We will use the array for the function calls because it is faster than the vector for random access.
-  double * parScaleArray_;
+  double ** parScaleArray_;
+  int iterationNum_;
 };
-
-void MomentumScaleCorrector::readParameters( TString fileName )
-{
-  parScaleArray_ = 0;
-  // vector<double> parameterErrors;
-
-  // Read the parameters file
-  ifstream parametersFile(fileName.Data());
-  string line;
-
-  string iteration("Iteration ");
-  // Loop on the file lines
-  while (parametersFile) {
-    getline( parametersFile, line );
-    unsigned int lineInt = line.find("value");
-
-    // if( line.find(iteration) != string::npos ) {
-    unsigned int iterationSubStr = line.find(iteration);
-
-    // Take the iteration number
-    if( iterationSubStr != string::npos ) {
-
-      int scaleFunctionNum = 0;
-      // This can be used when dealing with multiple iterations
-      int iterationNum = 0;
-
-      // cout << "line = " << line << endl;
-      stringstream sLine(line);
-      string num;
-      int wordCounter = 0;
-      // Warning: this strongly depends on the parameters file structure.
-      while( sLine >> num ) {
-        ++wordCounter;
-        //         cout << "num["<<wordCounter<<"] = " << num << endl;
-        if( wordCounter == 9 ) {
-          stringstream in(num);
-          in >> scaleFunctionNum;
-        }
-        if( wordCounter == 13 ) {
-          stringstream in(num);
-          in >> iterationNum;
-        }
-      }
-      // cout << "iteration number = " << iterationNum << endl;
-      // cout << "scale function number = " << scaleFunctionNum << endl;
-
-      // Set the scaleFunction
-      // scaleFunction_ = scaleFunctionArrayForVec[scaleFunctionNum];
-      scaleFunction_ = scaleFunctionArray[scaleFunctionNum];
-    }
-    // Take the parameters for the current iteration
-    if ( (lineInt != string::npos) ) {
-      int subStr1 = line.find("value");
-      stringstream paramStr;
-      double param = 0;
-      // Even if all the rest of the line is taken, the following
-      // convertion to a double will stop at the end of the first number.
-      paramStr << line.substr(subStr1+5);
-      paramStr >> param;
-      parScale_.push_back(param);
-      // cout << "param = " << param << endl;
-
-      // This is to extract parameter errors
-      // int subStr2 = line.find("+-");
-      // stringstream parErrorStr;
-      // double parError = 0;
-      // parErrorStr << line.substr(subStr2+1);
-      // parErrorStr >> parError;
-      // parameterErrors.push_back(parError);
-      // cout << "parError = " << parError << endl;
-    }
-  }
-  parScaleArray_ = new double[parScale_.size()];
-  vector<double>::const_iterator par = parScale_.begin();
-  int parNum = 0;
-  for ( ; par != parScale_.end(); ++par, ++parNum ) {
-    parScaleArray_[parNum] = *par;
-  }
-  // return make_pair(parameters, parameterErrors);
-}
 
 #endif // MomentumScaleCorrector_h
