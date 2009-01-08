3c9a9d652c03094eee5ca3a93f8622e67a8bf592
ewidl <sha1-3c65bb8ad5ab20d6db077c9cd74c65f6de0120f6@cern.ch>
2009-01-08 18:00:06 +0000

    improved file IO

--- Alignment/CommonAlignmentAlgorithm/src/AlignmentParametersIORoot.cc
+++ Alignment/CommonAlignmentAlgorithm/src/AlignmentParametersIORoot.cc
@@ -1,24 +1,25 @@
 // this class's header
 #include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentParametersIORoot.h"
 
-#include "TTree.h"
-
 #include "Alignment/CommonAlignment/interface/Alignable.h" 
 #include "Alignment/CommonAlignment/interface/AlignmentParameters.h"
-//#include "Alignment/CommonAlignmentParametrization/interface/RigidBodyAlignmentParameters.h"
 #include "Alignment/CommonAlignmentParametrization/interface/AlignmentParametersFactory.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "TTree.h"
+
 
 // ----------------------------------------------------------------------------
 // constructor
-
 AlignmentParametersIORoot::AlignmentParametersIORoot()
 {
   treename = "AlignmentParameters";
   treetxt = "Alignment Parameters";
 }
 
-// ----------------------------------------------------------------------------
 
+// ----------------------------------------------------------------------------
 void AlignmentParametersIORoot::createBranches(void) 
 {
   tree->Branch("parSize",   &theCovRang,   "CovRang/I");
@@ -31,8 +32,8 @@ void AlignmentParametersIORoot::createBranches(void)
   tree->Branch("HieraLevel",&theHieraLevel,"HieraLevel/I");
 }
 
-// ----------------------------------------------------------------------------
 
+// ----------------------------------------------------------------------------
 void AlignmentParametersIORoot::setBranchAddresses(void) 
 {
   tree->SetBranchAddress("parSize",   &theCovRang);
@@ -43,19 +44,12 @@ void AlignmentParametersIORoot::setBranchAddresses(void)
   tree->SetBranchAddress("Cov",       &theCov);
   tree->SetBranchAddress("ObjId",     &theObjId);
   tree->SetBranchAddress("HieraLevel",&theHieraLevel);
+
+  int nIndices = tree->BuildIndex( "Id", "ObjId" );
+  edm::LogInfo( "Alignment" ) << "@SUB=AlignmentParametersIORoot::setBranchAddresses"
+			      << "number of indexed entries: " << nIndices;
 }
 
-// ----------------------------------------------------------------------------
-
-int AlignmentParametersIORoot::findEntry(align::ID id, align::StructureType comp)
-{
-  double noAliPar = tree->GetEntries();
-  for (int ev = 0;ev<noAliPar;ev++) {
-    tree->GetEntry(ev); 
-    if ( theId==id && theObjId==comp ) return (ev);
-  }
-  return -1;
-}
 
 // ----------------------------------------------------------------------------
 int AlignmentParametersIORoot::writeOne(Alignable* ali)
@@ -83,37 +77,43 @@ int AlignmentParametersIORoot::writeOne(Alignable* ali)
   return 0;
 }
 
-// ----------------------------------------------------------------------------
 
+// ----------------------------------------------------------------------------
 AlignmentParameters* AlignmentParametersIORoot::readOne( Alignable* ali, int& ierr )
 {
   
   AlgebraicVector par(nParMax,0);
   AlgebraicSymMatrix cov(nParMax,0);
-  const std::vector<bool> &sel = ali->alignmentParameters()->selector();
  
-  int entry = findEntry( ali->id(), ali->alignableObjectId() );
-  if( entry != -1 ) 
-	{
-	  tree->GetEntry(entry);
-	  int covsize = theCovRang;
-	  int count=0;
-	  for(int row=0;row<covsize;row++) 
-		{
-		  par[row]=thePar[row];
-		  for(int col=0; col < covsize;col++) {
-			if(row-1<col) {cov[row][col]=theCov[count];count++;}
-		  }
-		} 
-	  using namespace AlignmentParametersFactory;
-	  ParametersType parType = parametersType(theParamType);
-	  AlignmentParameters* alipar1 = createParameters(ali, parType, sel); 
-	  AlignmentParameters* alipar = alipar1->clone(par,cov);
-	  alipar->setValid(true); 
-	  ierr=0;
-	  delete alipar1;
-	  return alipar;
-	}
+  if( tree->GetEntryWithIndex( ali->id(), ali->alignableObjectId() ) > 0 ) 
+  {
+    int covsize = theCovRang;
+    int count=0;
+    for(int row=0;row<covsize;row++) 
+    {
+      par[row]=thePar[row];
+      for(int col=0; col < covsize;col++) {
+	if(row-1<col) {cov[row][col]=theCov[count];count++;}
+      }
+    }
+
+    using namespace AlignmentParametersFactory;
+    ParametersType parType = parametersType(theParamType);
+    AlignmentParameters* alipar1; 
+    if ( ali->alignmentParameters() )
+    {
+      const std::vector<bool>& sel = ali->alignmentParameters()->selector();
+      alipar1 = createParameters(ali, parType, sel); 
+    } else {
+      const std::vector<bool> sel( theCovRang, true );
+      alipar1 = createParameters(ali, parType, sel); 
+    }
+    AlignmentParameters* alipar = alipar1->clone(par,cov);
+    alipar->setValid(true); 
+    ierr=0;
+    delete alipar1;
+    return alipar;
+  }
 
   ierr=-1;
   return(0);
