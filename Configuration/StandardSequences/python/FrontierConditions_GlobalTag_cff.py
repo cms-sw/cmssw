bc991e75e8211b0d491b0f73449ea9c55afbdfb6
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2009-01-09 13:19:25 +0000

    remove include to CSC_BadChambers_cfi (IDEAL_V11 and STARTUP_V8 provide DB source)

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cff.py
@@ -2,7 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
 
-# the following are needed for non PoolDBESSources
+# the following is needed for non PoolDBESSources (fake calibrations)
+#
+from CalibMuon.DTCalibration.DTFakeVDriftESProducer_cfi import *
 from CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi import *
-from CalibCalorimetry.HcalPlugins.Hcal_Conditions_forGlobalTag_cff import *
-from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_nofakes_cff import *
+from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_cff import *
+es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
+
+hcal_db_producer = cms.ESProducer("HcalDbProducer",
+    dump = cms.untracked.vstring(''),
+    file = cms.untracked.string('')
+)
+
+es_hardcode = cms.ESSource("HcalHardcodeCalibrations",
+    toGet = cms.untracked.vstring('GainWidths', 
+        'channelQuality', 
+        'ZSThresholds')
+)
+
+# L1 Fake conditions
+# included for backwards compatibility with existing Global Tags
+# To be removed in CMSSW 22X series
+from L1Trigger.Configuration.L1Trigger_FakeConditions_cff import *
