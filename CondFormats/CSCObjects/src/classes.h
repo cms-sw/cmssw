dc4b881d89369c3ab4001bafd349c65dec022e3e
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-09 20:58:48 +0000

    Rearrange classes.h a bit Add a few missing dummy declarations in classes.h Update to use "struct dictionary" in namespace instead of doubly-nested anonymous namespace in classes.h

--- CondFormats/CSCObjects/src/classes.h
+++ CondFormats/CSCObjects/src/classes.h
@@ -1,60 +1,46 @@
 #include "CondFormats/CSCObjects/interface/CSCPedestals.h"
-namespace{
- std::map< int, std::vector<CSCPedestals::Item> > pedmap;
-}
 #include "CondFormats/CSCObjects/interface/CSCDBPedestals.h"
-namespace{
- std::vector<CSCDBPedestals::Item> pedcontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCGains.h"
-namespace{
-  std::map< int, std::vector<CSCGains::Item> > gmap;
-}
 #include "CondFormats/CSCObjects/interface/CSCDBGains.h"
-namespace{
-  std::vector<CSCDBGains::Item> gcontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCNoiseMatrix.h"
-namespace{
-  std::map< int, std::vector< CSCNoiseMatrix::Item> > mmap;
-}
 #include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
-namespace{
-  std::vector<CSCDBNoiseMatrix::Item> mcontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCcrosstalk.h"
-namespace{
-  std::map< int, std::vector< CSCcrosstalk::Item> > cmap;
-}
 #include "CondFormats/CSCObjects/interface/CSCDBCrosstalk.h"
-namespace{
-  std::vector<CSCDBCrosstalk::Item> ccontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCBadStrips.h"
-namespace{
-  std::vector<CSCBadStrips::BadChamber> bschmcontainer;
-  std::vector<CSCBadStrips::BadChannel> bschncontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCBadWires.h"
-namespace{
-  std::vector<CSCBadWires::BadChamber> bwchmcontainer;
-  std::vector<CSCBadWires::BadChannel> bwchncontainer;
-}
 #include "CondFormats/CSCObjects/interface/CSCChamberMap.h"
-namespace{
-  std::map< int, CSCMapItem::MapItem > chmap;
-}
 #include "CondFormats/CSCObjects/interface/CSCCrateMap.h"
-namespace{
-  std::map< int, CSCMapItem::MapItem > cratemap;
-}
 #include "CondFormats/CSCObjects/interface/CSCDDUMap.h"
-namespace{
-  std::map< int, CSCMapItem::MapItem > ddumap;
-}
 #include "CondFormats/CSCObjects/interface/CSCChamberIndex.h"
-namespace{
-  std::map< int, CSCMapItem::MapItem > indexmap;
+
+namespace {
+  struct dictionary {
+
+    std::vector<CSCPedestals::Item>   pedcontainer1;
+    std::map< int, std::vector<CSCPedestals::Item> > pedmap;
+    std::vector<CSCDBPedestals::Item> pedcontainer2;
+
+    std::vector<CSCGains::Item>   gcontainer1;
+    std::map< int, std::vector<CSCGains::Item> > gmap;
+    std::vector<CSCDBGains::Item> gcontainer2;
+
+    std::vector<CSCNoiseMatrix::Item> mcontainer1;
+    std::map< int, std::vector< CSCNoiseMatrix::Item> > mmap;
+    std::vector<CSCDBNoiseMatrix::Item> mcontainer2;
+
+    std::vector<CSCcrosstalk::Item> ccontainer1;
+    std::map< int, std::vector< CSCcrosstalk::Item> > cmap;
+    std::vector<CSCDBCrosstalk::Item> ccontainer2;
+
+    std::vector<CSCBadStrips::BadChamber> bschmcontainer;
+    std::vector<CSCBadStrips::BadChannel> bschncontainer;
+
+    std::vector<CSCBadWires::BadChamber> bwchmcontainer;
+    std::vector<CSCBadWires::BadChannel> bwchncontainer;
+
+    std::map< int, CSCMapItem::MapItem > chmap;
+
+  };
 }
 
 #include "CondFormats/CSCObjects/interface/CSCBadChambers.h"
