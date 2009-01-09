11c7baac4a47cb999b24d1b35e950967398c44b3
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-01-09 11:01:49 +0000

    remove redundant geomtry include

--- SimTracker/TrackAssociation/python/TrackAssociatorByPosition_cff.py
+++ SimTracker/TrackAssociation/python/TrackAssociatorByPosition_cff.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 # a propagator
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi import *
-# global tracking geometry
-from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
+
 from SimTracker.TrackAssociation.TrackAssociatorByPosition_cfi import *
 
