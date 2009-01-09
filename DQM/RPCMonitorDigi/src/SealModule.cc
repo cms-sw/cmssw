63b58b7eb0d82bf03de45e9e4e3b186ed6c44baa
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2009-01-09 15:28:41 +0000

    remove old files

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -1,9 +1,7 @@
-#include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
-#include "DQM/RPCMonitorDigi/interface/RPCMonitorSync.h"
-//#include "DQM/RPCMonitorDigi/interface/RPCReadoutError.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 DEFINE_SEAL_MODULE();
+#include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorDigi);
-DEFINE_ANOTHER_FWK_MODULE(RPCMonitorSync);
-//DEFINE_ANOTHER_FWK_MODULE(RPCReadoutError);
+
