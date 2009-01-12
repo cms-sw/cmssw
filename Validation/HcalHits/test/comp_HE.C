0961729b4b625c7214429c2ca9a948686a47b299
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-01-12 09:29:27 +0000

    Improved histo presentation. Added chi2 comparison and stat.boxes.

--- Validation/HcalHits/test/comp_HE.C
+++ Validation/HcalHits/test/comp_HE.C
@@ -5,24 +5,14 @@
       
    //
 
-   TFile f1("HE_histo_208.root");
-   TFile f2("HE_histo_210pre5.root");
+   TFile f1("HE_histo_pre5.root");
+   TFile f2("HE_histo_pre6.root");
 
-   //   TFile f3("HE_histo_180.root");
-   //   TFile f4("HB_QGSP_4821.root");
-   //   TFile f5("HB_QGSP_EMV_4821.root");
-
-   
    // service variables
    //
    const int Nhist = 9;
    TH1F* f1_hist[Nhist];
    TH1F* f2_hist[Nhist];
-   //   TH1F* f3_hist[Nhist];
-   //   TH1F* f4_hist[Nhist];
-   //   TH1F* f5_hist[Nhist];
-
-
    char *label[Nhist];
 
    label[0] = &"layer0.gif";
@@ -37,58 +27,30 @@
 
    f1_hist[0] = (TH1F*)f1.Get("hl0") ;
    f2_hist[0] = (TH1F*)f2.Get("hl0") ;
-   //   f3_hist[0] = (TH1F*)f3.Get("hl0") ;
-   //   f4_hist[0] = (TH1F*)f4.Get("hl0") ;
-   //   f5_hist[0] = (TH1F*)f5.Get("hl0") ;
 
    f1_hist[1] = (TH1F*)f1.Get("h45") ;
    f2_hist[1] = (TH1F*)f2.Get("h45") ;
-   //   f3_hist[1] = (TH1F*)f3.Get("h45") ;
-   //   f4_hist[1] = (TH1F*)f4.Get("h45") ;
-   //   f5_hist[1] = (TH1F*)f5.Get("h45") ;
 
    f1_hist[2] = (TH1F*)f1.Get("h43") ;
    f2_hist[2] = (TH1F*)f2.Get("h43") ;
-   //   f3_hist[2] = (TH1F*)f3.Get("h43") ;
-   //   f4_hist[2] = (TH1F*)f4.Get("h43") ;
-   //   f5_hist[2] = (TH1F*)f5.Get("h43") ;
-
 
    f1_hist[3] = (TH1F*)f1.Get("h40") ;
    f2_hist[3] = (TH1F*)f2.Get("h40") ;
-   //   f3_hist[3] = (TH1F*)f3.Get("h40") ;
-   //   f4_hist[3] = (TH1F*)f4.Get("h40") ;
-   //   f5_hist[3] = (TH1F*)f5.Get("h40") ;
 
    f1_hist[4] = (TH1F*)f1.Get("h41") ;
    f2_hist[4] = (TH1F*)f2.Get("h41") ;
-   //   f3_hist[4] = (TH1F*)f3.Get("h41") ;
-   //   f4_hist[4] = (TH1F*)f4.Get("h41") ;
-   //   f5_hist[4] = (TH1F*)f5.Get("h41") ;
 
    f1_hist[5] = (TH1F*)f1.Get("h39") ;
    f2_hist[5] = (TH1F*)f2.Get("h39") ;
-   //   f3_hist[5] = (TH1F*)f3.Get("h39") ;
-   //   f4_hist[5] = (TH1F*)f4.Get("h39") ;
-   //   f5_hist[5] = (TH1F*)f5.Get("h39") ;
 
    f1_hist[6] = (TH1F*)f1.Get("hl6") ;
    f2_hist[6] = (TH1F*)f2.Get("hl6") ;
-   //   f3_hist[6] = (TH1F*)f3.Get("hl6") ;
-   //   f4_hist[6] = (TH1F*)f4.Get("hl6") ;
-   //   f5_hist[6] = (TH1F*)f5.Get("hl6") ;
 
    f1_hist[7] = (TH1F*)f1.Get("h6") ;
    f2_hist[7] = (TH1F*)f2.Get("h6") ;
-   //   f3_hist[7] = (TH1F*)f3.Get("h6") ;
-   //   f4_hist[7] = (TH1F*)f4.Get("h6") ;
-   //   f5_hist[7] = (TH1F*)f5.Get("h6") ;
 
    f1_hist[8] = (TH1F*)f1.Get("h46") ;
    f2_hist[8] = (TH1F*)f2.Get("h46") ;
-   //   f3_hist[8] = (TH1F*)f3.Get("h46") ;
-   //   f4_hist[8] = (TH1F*)f4.Get("h46") ;
-   //   f5_hist[8] = (TH1F*)f5.Get("h46") ;
 
    f1_hist[0]->GetXaxis()->SetTitle("L0 Deposited energy (GeV)");
    f1_hist[0]->GetYaxis()->SetTitle("N_{ev}/bin");
@@ -117,22 +79,8 @@
    f1_hist[8]->GetXaxis()->SetTitle("Deposited energy (GeV)");
    f1_hist[8]->GetYaxis()->SetTitle("N_{ev}/bin");
 
-   /*
 
-   f1_hist[0]->SetMaximum(2500.);
-   f1_hist[1]->SetMaximum(300.);
-   f1_hist[2]->SetMaximum(150.);
-   //   f1_hist[3]->SetMaximum(300.);
-   f1_hist[4]->SetMaximum(70000.);
-   f1_hist[5]->SetMinimum(0.7);
-   f1_hist[5]->SetMaximum(1.02.);
-   f1_hist[6]->SetMaximum(1000.);
-   //   f1_hist[6]->SetMaximum(600.);
-   f1_hist[7]->SetMaximum(350.);
-   f1_hist[8]->SetMaximum(350.);
-   */
-
-   f1_hist[2]->GetXaxis()->SetRangeUser(0.,1000.);
+   f1_hist[2]->GetXaxis()->SetRangeUser(0.,2000.);
    f1_hist[7]->GetXaxis()->SetRangeUser(0.,100.);
    f1_hist[8]->GetXaxis()->SetRangeUser(0.,1.);
   // hist1->GetXaxis()->SetNdivisions(-21);
@@ -140,45 +88,28 @@
 
    for (int i = 0; i < Nhist; i++){
 
-    f1_hist[i]->SetStats(kFALSE);   
-    f2_hist[i]->SetStats(kFALSE); 
-    //    f3_hist[i]->SetStats(kFALSE); 
-    //    f4_hist[i]->SetStats(kFALSE); 
-    //    f5_hist[i]->SetStats(kFALSE); 
-
+     if(i == 0 ) { 
+       f1_hist[i]->SetStats(kFALSE);   
+       f2_hist[i]->SetStats(kFALSE); 
+     }
+     else {
+       f1_hist[i]->SetStats(kTRUE);   
+       f2_hist[i]->SetStats(kTRUE); 
+     }
  
      f1_hist[i]->SetTitle("");
      f2_hist[i]->SetTitle("");
-     //     f3_hist[i]->SetTitle("");
-     //     f4_hist[i]->SetTitle("");
-     //     f5_hist[i]->SetTitle("");
 
      f1_hist[i]->SetLineWidth(2); 
      f2_hist[i]->SetLineWidth(3); 
-     //     f3_hist[i]->SetLineWidth(3); 
-     //     f4_hist[i]->SetLineWidth(3); 
-     //     f5_hist[i]->SetLineWidth(3); 
-
 
      // diffferent histo colors and styles
      f1_hist[i]->SetLineColor(41);
      f1_hist[i]->SetLineStyle(1); 
 
-     f2_hist[i]->SetLineColor(42);
+     f2_hist[i]->SetLineColor(43);
      f2_hist[i]->SetLineStyle(2);  
 
-     /*     
-     f3_hist[i]->SetLineColor(44);
-     f3_hist[i]->SetLineStyle(3);  
-
-    
-     f4_hist[i]->SetLineColor(44);
-     f4_hist[i]->SetLineStyle(4);  
-
-     f5_hist[i]->SetLineColor(46);
-     f5_hist[i]->SetLineStyle(5);  
-     */
-     
      //...Set axis title - sizes are absolute 
      f1_hist[i]->GetXaxis()->SetTickLength(-0.01);
      f1_hist[i]->GetYaxis()->SetTickLength(-0.01);
@@ -192,28 +123,55 @@
      f1_hist[i]->GetYaxis()->SetTitleSize(0.045);
 
     
-     if(i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 8) {
-       TLegend *leg = new TLegend(0.67, 0.55, 0.97, 0.8, "","brNDC");
-     }
-     else {
-       TLegend *leg = new TLegend(0.08, 0.7, 0.38, 0.85, "","brNDC");
-     }
+     // Chi2 test
+     const float NCHI2MIN = 0.01;
+     
+     float pval;
+     stringstream mystream;
+     char tempbuff[30];
+     
+     pval = f1_hist[i]->Chi2Test(f2_hist[i]);
+     
+     sprintf(tempbuff,"Chi2 p-value: %6.3E%c",pval,'\0');
+     mystream<<tempbuff;
+     
+     TPaveText* ptchi2 = new TPaveText(0.25, 0.91, 0.5, 0.99, "NDC");
+     
+     if (pval > NCHI2MIN) ptchi2->SetFillColor(kGreen);
+     else                 ptchi2->SetFillColor(kRed);
+     
+     ptchi2->SetTextSize(0.03);
+     ptchi2->AddText(mystream.str().c_str());
+     //
+
+
+     TLegend *leg = new TLegend(0.55, 0.91, 0.84, 0.99, "","brNDC");
 
      leg->SetBorderSize(2);
      //  leg->SetFillColor(51); // see new color definition above
      leg->SetFillStyle(1001); //
-     leg->AddEntry(f1_hist[i],"CMSSW_2_0_8","l");
-     leg->AddEntry(f2_hist[i],"CMSSW_2_1_0pre5","l");
-     //     leg->AddEntry(f3_hist[i],"CMSSW_1_8_0","l");
-     //     leg->AddEntry(f3_hist[i],"CMSSW_1_2_0_4821","l");
-     //     leg->AddEntry(f4_hist[i],"QGSP_4.8.2p1","l");
-     //     leg->AddEntry(f5_hist[i],"QGSP_EMV_4.8.2p1","l");
+     leg->AddEntry(f1_hist[i],"CMSSW_300pre5","l");
+     leg->AddEntry(f2_hist[i],"CMSSW_300pre6","l");
      
-     f1_hist[i]->Draw("hist");   
-     f2_hist[i]->Draw("hist same");   
-     //     f3_hist[i]->Draw("hist same");   
-     //     f4_hist[i]->Draw("hist same");   
-     //     f5_hist[i]->Draw("hist same");   
+     if (i > 0) {
+       TPaveStats *ptstats = new TPaveStats(0.85,0.86,0.98,0.98,"brNDC");
+       ptstats->SetTextColor(41);
+       f1_hist[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f1_hist[i]->GetListOfFunctions());
+       TPaveStats *ptstats = new TPaveStats(0.85,0.74,0.98,0.86,"brNDC");
+       ptstats->SetTextColor(43);
+       f2_hist[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f2_hist[i]->GetListOfFunctions());
+
+       f1_hist[i]->Draw(""); // "stat"
+       f2_hist[i]->Draw("hist sames");
+     }
+     else {
+       f1_hist[i]->Draw("hist");
+       f2_hist[i]->Draw("hist same");
+     }
+
+     ptchi2->Draw();
      leg->Draw();   
      
      myc->SaveAs(label[i]);
@@ -224,9 +182,6 @@
    //
    f1.Close() ;  
    f2.Close() ;
-   //   f3.Close() ;
-   //   f4.Close() ;
-   //   f5.Close() ;
    
    return ;  
      
--- Validation/HcalHits/test/comp_HF.C
+++ Validation/HcalHits/test/comp_HF.C
@@ -5,23 +5,12 @@
 
    TCanvas *myc = new TCanvas("myc","",800,600);
       
-   //
-   //   TFile f1("histo_HF_164.root.root");
-   //   TFile f2("simg4hcal_500_ref344.root");
-   TFile f3("HF_histo_208.root");
-   TFile f4("HF_histo_210pre5.root");
-   //   TFile f5("HF_histo_180.root");
+   TFile f3("HF_histo_pre5.root");
+   TFile f4("HF_histo_pre6.root");
 
-   
-   // service variables
-   //
    const int Nhist = 4;
-   //   TH1F* f1_hist[Nhist];
-   //   TH1F* f2_hist[Nhist];
    TH1F* f3_hist[Nhist];
    TH1F* f4_hist[Nhist];
-   //   TH1F* f5_hist[Nhist];
-
    //   cout << "1" << endl;
 
    char *label[Nhist];
@@ -31,31 +20,19 @@
    label[2] = &"Long_pe.gif";
    label[3] = &"Short_pe.gif";
 
-   //   f1_hist[0] = (TH1F*)f1.Get("h15") ;
-   //   f2_hist[0] = (TH1F*)f2.Get("h15") ;
    f3_hist[0] = (TH1F*)f3.Get("h15") ;
    f4_hist[0] = (TH1F*)f4.Get("h15") ;
-   //   f5_hist[0] = (TH1F*)f5.Get("h15") ;
 
-   //  f1_hist[3] = (TH1F*)f1.Get("h17") ;
-   //  f2_hist[3] = (TH1F*)f2.Get("h17") ;
    f3_hist[1] = (TH1F*)f3.Get("h17") ;
    f4_hist[1] = (TH1F*)f4.Get("h17") ;
-   //   f5_hist[1] = (TH1F*)f5.Get("h17") ;
 
-   //   f1_hist[1] = (TH1F*)f1.Get("h18") ;
-   //   f2_hist[1] = (TH1F*)f2.Get("h18") ;
    f3_hist[2] = (TH1F*)f3.Get("h18") ;
    f4_hist[2] = (TH1F*)f4.Get("h18") ;
-   //   f5_hist[2] = (TH1F*)f5.Get("h18") ;
 
-   //   f1_hist[2] = (TH1F*)f1.Get("h19") ;
-   //   f2_hist[2] = (TH1F*)f2.Get("h19") ;
    f3_hist[3] = (TH1F*)f3.Get("h19") ;
    f4_hist[3] = (TH1F*)f4.Get("h19") ;
-   //   f5_hist[3] = (TH1F*)f5.Get("h19") ;
 
-
+   //
    f3_hist[0]->GetXaxis()->SetTitle("T_{hits} (ns)");
    f3_hist[0]->GetYaxis()->SetTitle("N_{ev}/bin");
 
@@ -74,40 +51,25 @@
    // f1_hist[5]->SetMaximum(1.02.);
 
    for (int i = 0; i < Nhist; i++){
+ 
+     if(i == 0) {
+       f3_hist[i]->SetStats(kFALSE);
+       f4_hist[i]->SetStats(kFALSE);
+     }
+     else {
+       f3_hist[i]->SetStats(kTRUE);
+       f4_hist[i]->SetStats(kTRUE);
+     }
 
-     //    f1_hist[i]->SetStats(kFALSE);   
-     //    f2_hist[i]->SetStats(kFALSE); 
-    f3_hist[i]->SetStats(kFALSE);
-    f4_hist[i]->SetStats(kFALSE);
-    //    f5_hist[i]->SetStats(kFALSE);
-
-    //     f1_hist[i]->SetTitle("");
-    //     f2_hist[i]->SetTitle("");
      f3_hist[i]->SetTitle("");
      f4_hist[i]->SetTitle("");
-     //     f5_hist[i]->SetTitle("");
-
-     /*
-     f1_hist[i]->SetLineColor(45);
-     f1_hist[i]->SetLineWidth(3); 
-     f1_hist[i]->SetLineStyle(1); 
-     f2_hist[i]->SetLineColor(41);
-     f2_hist[i]->SetLineWidth(3); 
-     f2_hist[i]->SetLineStyle(2);  
-     */
-
+    
      f3_hist[i]->SetLineColor(41);
      f3_hist[i]->SetLineWidth(2); 
      f3_hist[i]->SetLineStyle(1); 
-     f4_hist[i]->SetLineColor(42);
+     f4_hist[i]->SetLineColor(43);
      f4_hist[i]->SetLineWidth(3); 
      f4_hist[i]->SetLineStyle(2);
-     //     f5_hist[i]->SetLineColor(44);
-     //     f5_hist[i]->SetLineWidth(3); 
-     //     f5_hist[i]->SetLineStyle(3);
-     //  h->SetMinimum(0.01);
-     //  h->SetMinimum(0.8);
-
      
      //...Set axis title - sizes are absolute 
      f3_hist[i]->GetXaxis()->SetTickLength(-0.01);
@@ -121,22 +83,57 @@
      f3_hist[i]->GetXaxis()->SetTitleSize(0.045);
      f3_hist[i]->GetYaxis()->SetTitleSize(0.045);
 
-     //...Legend
-     TLegend *leg = new TLegend(0.65, 0.8, 0.93, 0.95, "","brNDC");
-     leg->SetBorderSize(2);
-     //  leg->SetFillColor(51); // see new color definition above
-     leg->SetFillStyle(1001); //
-     //     leg->AddEntry(f1_hist[i],"OSCAR_3_9_82","l");
-     //     leg->AddEntry(f2_hist[i],"OSCAR_5_0_0","l");
-     leg->AddEntry(f3_hist[i],"CMSSW_2_0_8","l");
-     leg->AddEntry(f4_hist[i],"CMSSW_2_1_0pre5","l");
-     //     leg->AddEntry(f5_hist[i],"CMSSW_1_8_0_re5","l");
+     // Chi2 test
+     const float NCHI2MIN = 0.01;
      
-     //     f1_hist[i]->Draw("hist");   
-     //     f2_hist[i]->Draw("hist same");   
-     f3_hist[i]->Draw("hist");   
-     f4_hist[i]->Draw("hist same");   
-     //     f5_hist[i]->Draw("hist same");   
+     float pval;
+     stringstream mystream;
+     char tempbuff[30];
+     
+     pval = f3_hist[i]->Chi2Test(f4_hist[i]);
+     
+     sprintf(tempbuff,"Chi2 p-value: %6.3E%c",pval,'\0');
+     mystream<<tempbuff;
+     
+     TPaveText* ptchi2 = new TPaveText(0.25, 0.91, 0.5, 0.99, "NDC");
+     
+     if (pval > NCHI2MIN) ptchi2->SetFillColor(kGreen);
+     else                 ptchi2->SetFillColor(kRed);
+     
+     ptchi2->SetTextSize(0.03);
+     ptchi2->AddText(mystream.str().c_str());
+     //
+
+
+
+     //...Legend
+     TLegend *leg = new TLegend(0.55, 0.91, 0.84, 0.99, "","brNDC");
+     leg->SetBorderSize(2);
+     leg->SetFillStyle(1001); //
+
+     leg->AddEntry(f3_hist[i],"CMSSW_300pre5","l");
+     leg->AddEntry(f4_hist[i],"CMSSW_300pre6","l");
+
+
+     if (i > 0) {
+       TPaveStats *ptstats = new TPaveStats(0.85,0.86,0.98,0.98,"brNDC");
+       ptstats->SetTextColor(41);
+       f3_hist[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f3_hist[i]->GetListOfFunctions());
+       TPaveStats *ptstats = new TPaveStats(0.85,0.74,0.98,0.86,"brNDC");
+       ptstats->SetTextColor(43);
+       f4_hist[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f4_hist[i]->GetListOfFunctions());
+       
+       f3_hist[i]->Draw(""); // "stat"   
+       f4_hist[i]->Draw("hist sames");   
+     }
+     else { 
+       f3_hist[i]->Draw("hist");   
+       f4_hist[i]->Draw("hist same");   
+     }
+
+     ptchi2->Draw();
      leg->Draw();   
 
      
@@ -145,13 +142,9 @@
 
 
    // close ROOT files
-   //
-   //   f1.Close() ;  
-   //   f2.Close() ;
+
    f3.Close() ;
    f4.Close() ;
-   //   f5.Close() ;
-   
    
    return ;  
      
