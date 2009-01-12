a03707b2ccb2066a771d3f6fb9b42bbecfdccd66
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2009-01-12 10:56:15 +0000

    Map of Candidates onto SubEvent ids

--- SimDataFormats/HiGenData/src/classes.h
+++ SimDataFormats/HiGenData/src/classes.h
@@ -1,12 +1,16 @@
 #include "SimDataFormats/HiGenData/interface/SubEvent.h"
 #include "SimDataFormats/HiGenData/interface/GenHIEvent.h"
+#include "SimDataFormats/HiGenData/interface/SubEventMap.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace { 
   struct dictionary {
     std::vector<edm::SubEvent> dummy2;
     edm::GenHIEvent dummy0;
+    edm::SubEventMap dummy3;
     edm::Wrapper<edm::GenHIEvent> dummy1;
+    edm::Wrapper<edm::SubEventMap> dummy4;
+    edm::helpers::Key<edm::RefProd<reco::GenParticleCollection> > dumdumy;
   };
 }
 
