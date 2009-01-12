87884f591615ba78b5112da9bddd710096153f56
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-01-12 15:49:12 +0000

    Updated Template to include Track Corrected MET ("tcMet")

--- Validation/RecoMET/test/setupRelValPy.sh
+++ Validation/RecoMET/test/setupRelValPy.sh
@@ -9,36 +9,25 @@ FullSimRootFileDirectory=${current_area}/FullSim/
 mkdir $FullSimRootFileDirectory -p
 
 #======= Define list of samples that you will be validating ========#
-#dirlist="ZDimu ZprimeDijets QCD_0-15 QCD_15-20 QCD_20-30 QCD_30-50 QCD_50-80 QCD_80-120 QCD_120-170 QCD_170-230 QCD_230-300 QCD_300-380 QCD_380-470 QCD_470-600 QCD_600-800 QCD_800-1000 ttbar QCD_3000-3500"
 dirlist="QCD_80-120 QCD_3000-3500 Wjets_80-120 Wjets_3000-3500 LM1_sfts ttbar"
 
 #======= Define list of modules that will be run for each sample ========#
-RunPath="fileSaver, calotoweroptmaker, analyzeRecHits, analyzecaloTowers, analyzeGenMET, analyzeGenMETFromGenJets, analyzeHTMET, analyzeCaloMET, analyzePFMET"
+RunPath="fileSaver, calotoweroptmaker, analyzeRecHits, analyzecaloTowers, analyzeGenMET, analyzeGenMETFromGenJets, analyzeHTMET, analyzeCaloMET, analyzeTCMET,OB analyzePFMET"
 
 
 echo "Run path = {" $RunPath "}"
 
 
-#========Make MaxEvents file===============#
-#echo "'untracked PSet maxEvents = {untracked int32 input = -1}'" >> MaxEvents.cfi
 #==========================================#
 cd $current_area
 
 for i in $dirlist; do
 
 
-#========Make path file====================#
-#echo "untracked vstring fileNames = {
-#
-#}" >> FilePaths-$i.cfi
 #==========================================#
 cd $current_area
 
-#mkdir $i 
-#cd $i
-#echo `pwd`
-
-#======Make RunAnalyzers.cfg=================#
+#======Make RunAnalyzers_cfg.py=================#
 echo "import FWCore.ParameterSet.Config as cms
 
 process = cms.Process(\"TEST\")
@@ -70,6 +59,8 @@ process.load(\"Validation.RecoMET.RecHits_cff\")
 
 process.load(\"Validation.RecoMET.PFMET_cff\")
 
+process.load(\"Validation.RecoMET.TCMET_cff\")
+
 process.load(\"Configuration.StandardSequences.Geometry_cff\")
 
 process.load(\"Configuration.StandardSequences.MagneticField_cff\")
@@ -79,7 +70,11 @@ process.DQMStore = cms.Service(\"DQMStore\")
 process.source = cms.Source(\"PoolSource\",
     debugFlag = cms.untracked.bool(True),
     debugVebosity = cms.untracked.uint32(10),
-    fileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring(
+
+
+
+    )
 
 
 )
@@ -91,7 +86,16 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.fileSaver = cms.EDFilter(\"METFileSaver\",
     OutputFile = cms.untracked.string('METTester_data_${i}.root')
 ) 
-process.p = cms.Path(process.fileSaver*process.calotoweroptmaker*process.analyzeRecHits*process.analyzecaloTowers*process.analyzeGenMET*process.analyzeGenMETFromGenJets*process.analyzeHTMET*process.analyzeCaloMET*process.analyzePFMET)
+process.p = cms.Path(process.fileSaver*
+                     process.calotoweroptmaker*
+                     process.analyzeRecHits*
+                     process.analyzecaloTowers*
+                     process.analyzeGenMET*
+                     process.analyzeGenMETFromGenJets*
+                     process.analyzeHTMET*
+                     process.analyzeCaloMET*
+                     process.analyzePFMET*
+                     process.analyzeTCMET)
 process.schedule = cms.Schedule(process.p)
 
 " > ${FullSimRootFileDirectory}/RunAnalyzers-${i}_cfg.py
