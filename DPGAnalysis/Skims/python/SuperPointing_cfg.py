7b055ce9c39b09904f29dd85167a5622c58f0e8a
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-01-13 20:25:06 +0000

    collecion renamed

--- DPGAnalysis/Skims/python/SuperPointing_cfg.py
+++ DPGAnalysis/Skims/python/SuperPointing_cfg.py
@@ -8,11 +8,12 @@ process.source = cms.Source("PoolSource",
        '/store/data/Commissioning08/Cosmics/RECO/v1/000/067/122/C67EDF0D-49A0-DD11-9403-001617DBD332.root'),
                             secondaryFileNames = cms.untracked.vstring(
        '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/6E2601EC-3FA0-DD11-BA50-000423D986A8.root',
-       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/C240B0B2-47A0-DD11-A6AD-001617C3B654.root') 
-)                            
+       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/C240B0B2-47A0-DD11-A6AD-001617C3B654.root')
+)
+
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 2.0 $'),
+    version = cms.untracked.string('$Revision: 1.10 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/SuperPointing_cfg.py,v $'),
     annotation = cms.untracked.string('CRAFT SuperPointing skim')
 )
@@ -24,7 +25,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'CRAFT_V4P::All' 
+process.GlobalTag.globaltag = 'CRAFT_ALL_V4::All' 
 process.prefer("GlobalTag")
 
 process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
@@ -44,8 +45,8 @@ process.cosmicMuonsFilter = cms.EDFilter("HLTMuonPointingFilter",
                                          maxZ = cms.double(50.0)
                                          )
 
-process.cosmicMuons1LegBarrelOnlyFilter = cms.EDFilter("HLTMuonPointingFilter",
-                                                       SALabel = cms.string("cosmicMuons1LegBarrelOnly"),
+process.cosmicMuons1LegFilter = cms.EDFilter("HLTMuonPointingFilter",
+                                                       SALabel = cms.string("cosmicMuons1Leg"),
                                                        PropagatorName = cms.string("SteppingHelixPropagatorAny"),
                                                        radius = cms.double(10.0),
                                                        maxZ = cms.double(50.0)
@@ -79,8 +80,8 @@ process.rsWithMaterialTracksP5Filter = cms.EDFilter("HLTMuonPointingFilter",
                                                     maxZ = cms.double(50.0)
                                                     )
 
-process.globalCosmicMuons1LegBarrelOnlyFilter = cms.EDFilter("HLTMuonPointingFilter",
-                                                             SALabel = cms.string("globalCosmicMuons1LegBarrelOnly"),
+process.globalCosmicMuons1LegFilter = cms.EDFilter("HLTMuonPointingFilter",
+                                                             SALabel = cms.string("globalCosmicMuons1Leg"),
                                                              PropagatorName = cms.string("SteppingHelixPropagatorAny"),
                                                              radius = cms.double(10.0),
                                                              maxZ = cms.double(50.0)
@@ -96,12 +97,12 @@ process.ctfWithMaterialTracksP5Filter = cms.EDFilter("HLTMuonPointingFilter",
 
 process.cosmicMuonsBarrelOnlyPath = cms.Path(process.cosmicMuonsBarrelOnlyFilter)
 process.cosmicMuonsPath = cms.Path(process.cosmicMuonsFilter)
-process.cosmicMuons1LegBarrelOnlyPath = cms.Path(process.cosmicMuons1LegBarrelOnlyFilter)
+process.cosmicMuons1LegPath = cms.Path(process.cosmicMuons1LegFilter)
 process.globalCosmicMuonsBarrelOnlyPath = cms.Path(process.globalCosmicMuonsBarrelOnlyFilter)
 process.cosmictrackfinderP5Path = cms.Path(process.cosmictrackfinderP5Filter)
 process.globalCosmicMuonsPath = cms.Path(process.globalCosmicMuonsFilter)
 process.rsWithMaterialTracksP5Path = cms.Path(process.rsWithMaterialTracksP5Filter)
-process.globalCosmicMuons1LegBarrelOnlyPath = cms.Path(process.globalCosmicMuons1LegBarrelOnlyFilter)
+process.globalCosmicMuons1LegPath = cms.Path(process.globalCosmicMuons1LegFilter)
 process.ctfWithMaterialTracksP5Path = cms.Path(process.ctfWithMaterialTracksP5Filter)
 
 
@@ -110,12 +111,12 @@ process.out = cms.OutputModule("PoolOutputModule",
                                outputCommands = cms.untracked.vstring('keep *'),
                                SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('cosmicMuonsBarrelOnlyPath',
                                                                                             'cosmicMuonsPath',
-                                                                                            'cosmicMuons1LegBarrelOnlyPath',
+                                                                                            'cosmicMuons1LegPath',
                                                                                             'globalCosmicMuonsBarrelOnlyPath',
                                                                                             'cosmictrackfinderP5Path',
                                                                                             'globalCosmicMuonsPath',
                                                                                             'rsWithMaterialTracksP5Path',
-                                                                                            'globalCosmicMuons1LegBarrelOnlyPath',
+                                                                                            'globalCosmicMuons1LegPath',
                                                                                             'ctfWithMaterialTracksP5Path')),                               
                                dataset = cms.untracked.PSet(
 			                 dataTier = cms.untracked.string('RAW-RECO'),
@@ -124,3 +125,6 @@ process.out = cms.OutputModule("PoolOutputModule",
                                )
 
 process.this_is_the_end = cms.EndPath(process.out)
+
+
+
