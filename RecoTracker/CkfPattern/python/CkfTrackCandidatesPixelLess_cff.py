da66fff2444a2ad0f39a96084ee23377f1d3b70b
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-01-13 04:51:44 +0000

    use grouped builder as fro standard tracking

--- RecoTracker/CkfPattern/python/CkfTrackCandidatesPixelLess_cff.py
+++ RecoTracker/CkfPattern/python/CkfTrackCandidatesPixelLess_cff.py
@@ -6,10 +6,13 @@ from RecoTracker.CkfPattern.CkfTrajectoryBuilderESProducer_cff import *
 from TrackingTools.TrajectoryCleaning.TrajectoryCleanerBySharedHits_cfi import *
 # navigation school
 from RecoTracker.TkNavigation.NavigationSchoolESProducer_cff import *
-import copy
-from RecoTracker.CkfPattern.CkfTrackCandidates_cfi import *
+
 # generate CTF track candidates ############
-ckfTrackCandidatesPixelLess = copy.deepcopy(ckfTrackCandidates)
-ckfTrackCandidatesPixelLess.TrajectoryBuilder = 'CkfTrajectoryBuilder'
-ckfTrackCandidatesPixelLess.src = cms.InputTag('globalPixelLessSeeds')
+import RecoTracker.CkfPattern.CkfTrackCandidates_cfi 
+ckfTrackCandidatesPixelLess = RecoTracker.CkfPattern.CkfTrackCandidates_cfi.ckfTrackCandidates.clone(
+    TrajectoryBuilder = cms.string('GroupedCkfTrajectoryBuilder'),
+    src = cms.InputTag('globalPixelLessSeeds')
+    )
+
+
 
