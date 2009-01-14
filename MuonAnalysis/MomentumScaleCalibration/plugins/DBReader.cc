d6e6b5c04c7efe41a866ee8abd698fdf9bb969de
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-01-14 13:16:34 +0000

    New db reader working for all the functions (scale, resolution and background).

---
+++ MuonAnalysis/MomentumScaleCalibration/plugins/DBReader.cc
@@ -0,0 +1,77 @@
+// #include "CondFormats/PhysicsToolsObjects/interface/Histogram2D.h"
+// #include "MuonAnalysis/MomentumScaleCalibrationObjects/interface/MuScleFitScale.h"
+#include "CondFormats/MomentumScaleCalibrationObjects/interface/MuScleFitScale.h"
+// #include "MuonAnalysis/MomentumScaleCalibrationObjects/interface/MuScleFitScaleRcd.h"
+#include "CondFormats/DataRecord/interface/MuScleFitScaleRcd.h"
+
+#include "DBReader.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <sys/time.h>
+#include <string>
+
+using namespace std;
+using namespace cms;
+
+DBReader::DBReader( const edm::ParameterSet& iConfig ) : type_(iConfig.getUntrackedParameter<string>("Type"))
+{
+}
+
+void DBReader::beginJob ( const edm::EventSetup& iSetup ) {
+  edm::ESHandle<MuScleFitScale> dbObject;
+  iSetup.get<MuScleFitScaleRcd>().get(dbObject);
+  edm::LogInfo("DBReader") << "[DBReader::analyze] End Reading MuScleFitScaleRcd" << endl;
+
+  cout << "identifiers size from dbObject = " << dbObject->identifiers.size() << endl;
+  cout << "parameters size from dbObject = " << dbObject->parameters.size() << endl;;
+
+  // This string is one of: scale, resolution, background.
+  // Create the corrector and set the parameters
+  if( type_ == "scale" ) corrector_.reset(new MomentumScaleCorrector( dbObject.product() ) );
+  else if( type_ == "resolution" ) corrector_.reset(new ResolutionFunction( dbObject.product() ) );
+  else if( type_ == "background" ) corrector_.reset(new BackgroundFunction( dbObject.product() ) );
+  else {
+    cout << "Error: unrecognized type. Use one of those: 'scale', 'resolution', 'background'" << endl;
+    exit(1);
+  }
+
+  cout << "pointer = " << corrector_.get() << endl;
+}
+
+//:  printdebug_(iConfig.getUntrackedParameter<uint32_t>("printDebug",1)){}
+
+DBReader::~DBReader(){}
+
+void DBReader::analyze( const edm::Event& e, const edm::EventSetup& iSetup){
+
+  cout << "checking size consistency" << endl;
+  if( corrector_->identifiers().size() != corrector_->parameters().size() ) {
+    cout << "Error: size of parameters("<<corrector_->parameters().size()<<") and identifiers("<<corrector_->identifiers().size()<<") don't match" << endl;
+    exit(1);
+  }
+
+  // Looping directly on it does not work, because it is returned by value
+  // and the iterator gets invalidated on the next line. Save it to a temporary object
+  // and iterate on it.
+  vector<vector<double> > parVecVec(corrector_->parameters());
+  // vector<vector<double> >::const_iterator parVec = corrector_->parameters().begin();
+  vector<vector<double> >::const_iterator parVec = parVecVec.begin();
+  vector<int> functionId(corrector_->identifiers());
+  vector<int>::const_iterator id = functionId.begin();
+  for( ; id != functionId.end(); ++id, ++parVec ) {
+    cout << "parVec.size() = " << parVec->size() << endl;
+    cout << "parVec[0] = " << (*parVec)[0] << endl;
+    cout << "id = " << *id << endl;
+    vector<double>::const_iterator par = parVec->begin();
+    int i=0;
+    for( ; par != parVec->end(); ++par, ++i ) {
+      cout << "par["<<i<<"] = " << *par << endl;
+    }
+  }
+}
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(DBReader);
---
+++ MuonAnalysis/MomentumScaleCalibration/plugins/DBReader.h
@@ -0,0 +1,37 @@
+#ifndef DBReader_H
+#define DBReader_H
+
+// system include files
+//#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "MuonAnalysis/MomentumScaleCalibration/interface/MomentumScaleCorrector.h"
+#include "MuonAnalysis/MomentumScaleCalibration/interface/ResolutionFunction.h"
+#include "MuonAnalysis/MomentumScaleCalibration/interface/BackgroundFunction.h"
+
+class DBReader : public edm::EDAnalyzer {
+
+ public:
+  explicit DBReader( const edm::ParameterSet& );
+  ~DBReader();
+
+  void beginJob ( const edm::EventSetup& iSetup );
+  
+  void analyze( const edm::Event&, const edm::EventSetup& );
+
+ private:
+  //  uint32_t printdebug_;
+  string type_;
+  auto_ptr<BaseFunction> corrector_;
+
+};
+#endif
