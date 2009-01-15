afb8ff7c7f90723321e7121f130e8aabb7777203
Juerg Eugster <sha1-53c36d8f1430febbefae6c7d17fd2800b558e123@cern.ch>
2009-01-15 14:14:56 +0000

    *** empty log message ***

--- Validation/RecoJets/analysis/validation.sh
+++ Validation/RecoJets/analysis/validation.sh
@@ -1,12 +1,12 @@
 #!/bin/sh 
 
 echo "This script produces all the histograms and plots..."
-echo -e "Enter the REFERNCE.root file, the NEW.root file and the directory name/histogram title([REFERNCE.root] [NEW.root] [title]): "
-read reference new title
+echo -e "Enter the REFERNCE.root file, the NEW.root file, the directory name/histogram title and the normalization ([REFERNCE.root] [NEW.root] [title] [normalization]): "
+read reference new title norm
 
 if [ -n "$reference" ]; then
     if [ -n "$new" ]; then
-	if [ -n "$title"]; then
+	if [ -n "$title" ]; then 
 	    echo "processing "$reference" and "$new"."
 	else 
 	    echo "Input not correct!!"
@@ -22,7 +22,7 @@ if [ -n $title ]; then
     if [ -e $reference ]; then
 	if [ -e $new ]; then
 
-	    TAG=(CaloJetTask_iterativeCone5CaloJets CaloJetTask_L2L3CorJet CaloJetTask_JetPlusTrackZSPCorJetIcone5 PFJetTask_iterativeCone5PFJets CaloJetTask_kt4CaloJets CaloJetTask_kt6CaloJets CaloJetTask_sisCone5CaloJets CaloJetTask_sisCone7CaloJets)
+	    TAG=(CaloJetTask_iterativeCone5CaloJets CaloJetTask_L2L3CorJetIcone5 CaloJetTask_JetPlusTrackZSPCorJetIcone5 PFJetTask_iterativeCone5PFJets CaloJetTask_kt4CaloJets CaloJetTask_kt6CaloJets CaloJetTask_sisCone5CaloJets CaloJetTask_sisCone7CaloJets)
 	    FOLDER=(Icone5 CorrIcone5 JPT PFlow kt4 kt6 Siscone5 Siscone7)
 	    ntag=${#TAG[@]}
 	    echo "Number of module tags: " $ntag
@@ -35,9 +35,25 @@ if [ -n $title ]; then
 		mkdir $title/${FOLDER[${i}]}/RefData	    
 		echo "folders are created"
 
+		if [ -z $norm ]; then
+		../../../../test/slc4_ia32_gcc345/compareHists $new $reference ${TAG[${i}]} $title
+		fi
+
+		if [ $norm = "y" ]; then
 		../../../../test/slc4_ia32_gcc345/compareHists $new $reference ${TAG[${i}]} $title y
+		fi
+
 		mv *.gif $title/${FOLDER[${i}]}
-		cp html/${FOLDER[${i}]}.html $title/${FOLDER[${i}]}
+
+		if [ ${FOLDER[${i}]} = ${FOLDER[3]} ]; then
+		    cp html/${FOLDER[${i}]}.html $title/${FOLDER[${i}]}
+		fi
+
+		if [ ${FOLDER[${i}]} =  ${FOLDER[2]} ]; then
+		    cp html/${FOLDER[${i}]}.html $title/${FOLDER[${i}]}
+		else
+		    cp html/htmlTemplate.html $title/${FOLDER[${i}]}
+		fi
 
 		../../../../test/slc4_ia32_gcc345/fixed_plotHists $new ${TAG[${i}]}
 		mv *.gif $title/${FOLDER[${i}]}/NewData
