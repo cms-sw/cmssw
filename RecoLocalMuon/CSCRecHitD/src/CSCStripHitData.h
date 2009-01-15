e84a6e1fc8de8afb1b88a06ad8ebd3906552235b
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2009-01-15 15:39:52 +0000

    fix vector lengths to 4

--- RecoLocalMuon/CSCRecHitD/src/CSCStripHitData.h
+++ RecoLocalMuon/CSCRecHitD/src/CSCStripHitData.h
@@ -17,26 +17,25 @@ class CSCStripHitData
 {	
  public:	
 
-	CSCStripHitData() : istrip_(-1), tmax_(-1), ph_(std::vector<float>()), phRaw_(ph_){};
+  CSCStripHitData() : istrip_(-1), tmax_(-1), phRaw_(nbins_), ph_(nbins_){};
   
-  CSCStripHitData( int istrip, int tmax, std::vector<float> ph,  std::vector<float> phRaw ) :
-           istrip_(istrip), tmax_(tmax), ph_(ph),  phRaw_(phRaw){};
+  CSCStripHitData( int istrip, int tmax, std::vector<float> phRaw,  std::vector<float> ph ) :
+           istrip_(istrip), tmax_(tmax), phRaw_(phRaw),  ph_(ph){};
   
   int strip() const {return istrip_;}
   int tmax() const {return  tmax_;}
-	std::vector<float> ph() const { return ph_;}
-	std::vector<float> phRaw() const { return phRaw_;}
+  std::vector<float> ph() const { return ph_;}
+  std::vector<float> phRaw() const { return phRaw_;}
 
   /// Order by 2nd ph bin
   bool operator<( const CSCStripHitData & data ) const { return ph_[1] < data.ph_[1]; }
 
  private:
-  
+  static const int nbins_ = 4; //@ Number of ph bins saved
   int istrip_;
   int tmax_;
-	std::vector<float> ph_;
-	std::vector<float> phRaw_;
-  
+  std::vector<float> phRaw_;
+  std::vector<float> ph_;  
 };
 
 #endif
