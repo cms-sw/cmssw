c58ab0b4c5d9eb97643f65dbb48739e1fbd7fc32
Sara Vanini <sha1-f121f5cdb382a17cbf25fd0c012d564692c6d07d@cern.ch>
2009-01-15 10:49:04 +0000

    Geometry dependencies removed

--- L1TriggerConfig/DTTPGConfigProducers/python/L1DTTPGConfigFromDB_cff.py
+++ L1TriggerConfig/DTTPGConfigProducers/python/L1DTTPGConfigFromDB_cff.py
@@ -1,5 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from L1TriggerConfig.DTTPGConfigProducers.L1DTConfigFromDB_cfi import *
 
