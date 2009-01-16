2eb1906aede55315160016a0f857b183268c5bb6
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-16 02:35:32 +0000

    - Remove pattern rules for map/pair/vector in classes_def.xml, adding     back specifically only those which are needed, this removes ~480kB     of code bloat

--- CondFormats/DTObjects/src/classes.h
+++ CondFormats/DTObjects/src/classes.h
@@ -11,55 +11,23 @@
 #include "CondFormats/DTObjects/interface/DTConfigData.h"
 
 namespace {
-//  std::map<int,DTConfigToken> refMap;
-//  std::map<DTT0Id,DTT0Data,DTT0Compare> t0Map;
-//  std::map<DTTtrigId,DTTtrigData,DTTtrigCompare> tTrigMap;
-//  std::map<DTMtimeId,DTMtimeData,DTMtimeCompare> mTimeMap;
-//  std::map<DTRangeT0Id,DTRangeT0Data,DTRangeT0Compare> rangeT0Map;
-//  std::map<DTStatusFlagId,DTStatusFlagData,DTStatusFlagCompare> statusFlagMap;
-//  std::map<DTDeadFlagId,DTDeadFlagData,DTDeadFlagCompare> deadFlagMap;
-//  std::map<DTPerformanceId,DTPerformanceData,DTPerformanceCompare> performanceMap;
-//  std::map<DTCCBId,std::vector<int>,DTCCBCompare> ccbData;
-  std::pair<int,DTConfigToken> refMap;
-  std::pair<DTT0Id,DTT0Data> t0Pair;
-  std::pair<DTTtrigId,DTTtrigData> tTrigPair;
-  std::pair<DTMtimeId,DTMtimeData> mTimePair;
-  std::pair<DTRangeT0Id,DTRangeT0Data> rangeT0Pair;
-  std::pair<DTStatusFlagId,DTStatusFlagData> statusFlagPair;
-  std::pair<DTDeadFlagId,DTDeadFlagData> deadFlagPair;
-  std::pair<DTPerformanceId,DTPerformanceData> performancePair;
-  std::pair<DTCCBId,int> ccbPair;
+  struct dictionary {
+    std::pair<int,DTConfigToken> refMap;
+    std::pair<DTT0Id,DTT0Data> t0Pair;
+    std::pair<DTTtrigId,DTTtrigData> tTrigPair;
+    std::pair<DTMtimeId,DTMtimeData> mTimePair;
+    std::pair<DTRangeT0Id,DTRangeT0Data> rangeT0Pair;
+    std::pair<DTStatusFlagId,DTStatusFlagData> statusFlagPair;
+    std::pair<DTDeadFlagId,DTDeadFlagData> deadFlagPair;
+    std::pair<DTPerformanceId,DTPerformanceData> performancePair;
+    std::pair<DTCCBId,int> ccbPair;
+ 
+    std::vector<DTReadOutGeometryLink> blah1;
+    std::vector<std::pair<DTT0Id,DTT0Data> > blah2;
+    std::vector< std::pair<DTTtrigId,DTTtrigData> > blah3;
+    std::vector< std::pair<DTMtimeId,DTMtimeData> > blah4;
+    std::vector< std::pair<DTStatusFlagId,DTStatusFlagData> > blah5;
+  };
 }
 
-/*
-// Declaration of the iterator (necessary for the generation of the dictionary)
-template std::vector<DTReadOutGeometryLink>::iterator;
-template std::vector<DTReadOutGeometryLink>::const_iterator;
-////template std::vector<DTCellT0Data>::iterator;
-////template std::vector<DTCellT0Data>::const_iterator;
-//template std::map<DTT0Id,DTT0Data,DTT0Compare>::iterator;
-//template std::map<DTT0Id,DTT0Data,DTT0Compare>::const_iterator;
-template std::vector< std::pair<DTT0Id,DTT0Data> >::iterator;
-template std::vector< std::pair<DTT0Id,DTT0Data> >::const_iterator;
-//template std::vector<DTSLTtrigData>::iterator;
-//template std::vector<DTSLTtrigData>::const_iterator;
-template std::map<DTTtrigId,DTTtrigData,DTTtrigCompare>::iterator;
-template std::map<DTTtrigId,DTTtrigData,DTTtrigCompare>::const_iterator;
-//template std::vector<DTSLMtimeData>::iterator;
-//template std::vector<DTSLMtimeData>::const_iterator;
-template std::map<DTMtimeId,DTMtimeData,DTMtimeCompare>::iterator;
-template std::map<DTMtimeId,DTMtimeData,DTMtimeCompare>::const_iterator;
-//template std::vector<DTSLRangeT0Data>::iterator;
-//template std::vector<DTSLRangeT0Data>::const_iterator;
-template std::map<DTRangeT0Id,DTRangeT0Data,DTRangeT0Compare>::iterator;
-template std::map<DTRangeT0Id,DTRangeT0Data,DTRangeT0Compare>::const_iterator;
-//template std::vector<DTCellStatusFlagData>::iterator;
-//template std::vector<DTCellStatusFlagData>::const_iterator;
-template std::map<DTStatusFlagId,DTStatusFlagData,
-                  DTStatusFlagCompare>::iterator;
-template std::map<DTStatusFlagId,DTStatusFlagData,
-                  DTStatusFlagCompare>::const_iterator;
-template std::map<DTPerformanceId,DTPerformanceData,DTPerformanceCompare>::iterator;
-template std::map<DTPerformanceId,DTPerformanceData,DTPerformanceCompare>::const_iterator;
-*/
 
