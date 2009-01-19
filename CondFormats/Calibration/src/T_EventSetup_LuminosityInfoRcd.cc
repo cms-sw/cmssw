a06e844fe9c03f6b3200b9951fdcd406eda40021
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-01-19 13:43:16 +0000

    added

---
+++ CondFormats/Calibration/src/T_EventSetup_LuminosityInfoRcd.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondFormats/Calibration/interface/LuminosityInfo.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(lumi::LuminosityInfo);
---
+++ CondFormats/DataRecord/interface/LuminosityInfoRcd.h
@@ -0,0 +1,6 @@
+#ifndef LUMINOSITYINFORCD_H
+#define LUMINOSITYINFORCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class LuminosityInfoRcd : public edm::eventsetup::EventSetupRecordImplementation<LuminosityInfoRcd> {};
+#endif
