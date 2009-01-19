76935a4f909575d0299016ee8f0f0c27bf9a8d5b
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-01-19 16:50:31 +0000

    first commit.

---
+++ HLTriggerOffline/special/python/EcalPhiSymHLTVal_cfi.py
@@ -0,0 +1,24 @@
+# The following comments couldn't be translated into the new config version:
+
+# prescale
+
+import FWCore.ParameterSet.Config as cms
+
+#
+# $Id: MonitorAlCaEcalPhisym_cfi.py,v 1.2 2008/08/14 09:33:36 argiro Exp $
+#
+# \author Stefano Argiro
+#
+EcalPhiSymMon = cms.EDAnalyzer("DQMSourcePhiSym",
+    # product to monitor
+    AlCaStreamEBTag = cms.untracked.InputTag("hltAlCaPhiSymStream","phiSymEcalRecHitsEB"),
+    SaveToFile = cms.untracked.bool(False),
+    FileName = cms.untracked.string('MonitorAlCaEcalPhiSym.root'),
+    AlCaStreamEETag = cms.untracked.InputTag("hltAlCaPhiSymStream","phiSymEcalRecHitsEE"),
+    prescaleFactor = cms.untracked.int32(1),
+    # DQM folder to write to
+    FolderName = cms.untracked.string('EcalPhiSymHLT')
+)
+
+
+
