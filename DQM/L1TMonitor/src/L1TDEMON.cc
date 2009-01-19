bc0dc66e596adc1edf3370e3a4ab1bd0d9b7d9ad
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-01-19 19:22:30 +0000

    fix memory leak

--- DQM/L1TMonitor/src/L1TDEMON.cc
+++ DQM/L1TMonitor/src/L1TDEMON.cc
@@ -561,16 +561,53 @@ L1TDEMON::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   // hBxDiffAllFed hBxDiffAllFedSpread hBxOccyAllFedSpread
 
   ///GT mon info
-  if(isComp[GLT]&&false) {
+  //if(isComp[GLT]&&false) {
+  if(isComp[GLT]) {
     const int w64=64;
     GltDEDigi gltdigimon = deRecord->getGlt();
-    std::vector<bool> edecbitv = (gltdigimon.gltDecBits)[0];
-    std::vector<bool> ddecbitv = (gltdigimon.gltDecBits)[1];
-    std::vector<bool> etchbitv = (gltdigimon.gltTchBits)[0];
-    std::vector<bool> dtchbitv = (gltdigimon.gltTchBits)[1];
-    
-    std::vector<bool> dedecbitv(2*w64,false), debitmaskv(2*w64,false), 
+
+    std::vector<bool> edecbitv(w64*2);
+    std::vector<bool> ddecbitv(w64*2);
+    std::vector<bool> etchbitv(w64);
+    std::vector<bool> dtchbitv(w64);
+    edecbitv.reserve(w64*2);
+    ddecbitv.reserve(w64*2);
+    etchbitv.reserve(w64);
+    dtchbitv.reserve(w64);
+
+    //decision word sizes defined in
+    //DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetupFwd.h
+    /*
+    std::cout << "xxx l1tdemon sizes:" 
+	      << " " << (gltdigimon.gltDecBits)[0].size()
+	      << " " << (gltdigimon.gltDecBits)[1].size()
+	      << " " << (gltdigimon.gltTchBits)[0].size()
+	      << " " << (gltdigimon.gltTchBits)[1].size()
+	      << "\n" << std::flush;
+    */
+
+    std::vector<bool>::iterator it;
+    for( it = (gltdigimon.gltDecBits)[0].begin();
+	 it!= (gltdigimon.gltDecBits)[0].end(); it++ )
+      edecbitv.push_back(*it);
+    for( it = (gltdigimon.gltDecBits)[1].begin();
+	 it!= (gltdigimon.gltDecBits)[1].end(); it++ )
+      ddecbitv.push_back(*it);
+    for( it = (gltdigimon.gltTchBits)[0].begin();
+	 it!= (gltdigimon.gltTchBits)[0].end(); it++ )
+      etchbitv.push_back(*it);
+    for( it = (gltdigimon.gltTchBits)[1].begin();
+	 it!= (gltdigimon.gltTchBits)[1].end(); it++ )
+      dtchbitv.push_back(*it);
+
+    std::vector<bool> 
+      dedecbitv  (2*w64,false), 
+      debitmaskv (2*w64,false), 
       gtbitmasked(2*w64,false);
+    for(int i=0; i<w64*2; i++) dedecbitv  [i]=false;
+    for(int i=0; i<w64*2; i++) debitmaskv [i]=false;
+    //for(int i=0; i<w64*2; i++) gtbitmasked[i]=false;
+
     for(int i=0; i<2*w64; i++)
       gtbitmasked[i] = false; //no masking!
     for(int i=0; i<2*w64; i++) {
