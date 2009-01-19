b559494c219d965bcfc02e1b4abf5ed769c304eb
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2009-01-19 20:40:44 +0000

    Adding playback options.

---
+++ SimTracker/TrackHistory/python/PlaybackWithReco_cff.py
@@ -0,0 +1,30 @@
+# Playback file
+import FWCore.ParameterSet.Config as cms
+
+# Import of standard configurations
+from Configuration.StandardSequences.Services_cff import *
+from FWCore.MessageService.MessageLogger_cfi import *
+from Configuration.StandardSequences.MixingNoPileUp_cff import *
+from Configuration.StandardSequences.GeometryPilot2_cff import *
+from Configuration.StandardSequences.MagneticField_38T_cff import *
+from Configuration.StandardSequences.RawToDigi_cff import *
+from Configuration.StandardSequences.Reconstruction_cff import *
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
+from Configuration.EventContent.EventContent_cff import *
+from Configuration.StandardSequences.Digi_cff import *
+
+# Playback
+del RandomNumberGeneratorService.theSource
+RandomNumberGeneratorService.restoreStateLabel = cms.untracked.string('randomEngineStateProducer')
+from SimGeneral.MixingModule.mixNoPU_cfi import *
+mix.playback = cms.untracked.bool(True)
+
+# TrackingTruth
+from SimGeneral.TrackingAnalysis.trackingParticles_cfi import *
+
+# Tracking truth and SiStrip(Pixel)DigiSimLinks
+trackingTruth = cms.Sequence(mix * doAllDigi * trackingParticles)
+
+# Reconstruction
+playback = cms.Sequence(RawToDigi * trackingTruth * reconstruction)
+
