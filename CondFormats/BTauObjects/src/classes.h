56ee4c3e0933538f74cae5dbd053218766156ccc
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-20 14:43:01 +0000

    Change dummy declarations in classes.h from instances within a single or   double anonymous namespace to declarations within a struct (within an   anonymous namespace). Avoids global instances in the dictionary library.

--- CondFormats/BTauObjects/src/classes.h
+++ CondFormats/BTauObjects/src/classes.h
@@ -6,24 +6,24 @@
 #include "CondFormats/BTauObjects/interface/CombinedTauTagCalibration.h"
 
 namespace {
+  struct dictionary {
+    std::vector<float> b1;
 
-std::vector<float> b1;
+    TrackProbabilityCategoryData c;
 
-TrackProbabilityCategoryData c;
+    TrackProbabilityCalibration d;
+    TrackProbabilityCalibration::Entry e;
+    std::vector<TrackProbabilityCalibration::Entry> f;
 
-TrackProbabilityCalibration d;
-TrackProbabilityCalibration::Entry e;
-std::vector<TrackProbabilityCalibration::Entry> f;
+    CombinedTauTagCategoryData g;
+    CombinedTauTagCalibration h;
+    CombinedTauTagCalibration::Entry i;
+    std::vector<CombinedTauTagCalibration::Entry> j;
 
-CombinedTauTagCategoryData g;
-CombinedTauTagCalibration h;
-CombinedTauTagCalibration::Entry i;
-std::vector<CombinedTauTagCalibration::Entry> j;
+    CombinedSVCategoryData cs1;
 
-CombinedSVCategoryData cs1;
-
-CombinedSVCalibration cs2;
-CombinedSVCalibration::Entry cs3;
-std::vector<CombinedSVCalibration::Entry> cs4;
-
+    CombinedSVCalibration cs2;
+    CombinedSVCalibration::Entry cs3;
+    std::vector<CombinedSVCalibration::Entry> cs4;
+  };
 }
