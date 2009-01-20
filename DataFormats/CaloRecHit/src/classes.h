e62935b1aef9ecf6f3214697081d5d5abdb4facc
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-20 00:24:38 +0000

    Change dummy declarations in classes.h from instances within a double   anonymous namespace to declarations within a struct (within an anonymous   namespace). Avoids global instances in the dictionary library.

--- DataFormats/CaloRecHit/src/classes.h
+++ DataFormats/CaloRecHit/src/classes.h
@@ -4,13 +4,13 @@
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "DataFormats/CaloRecHit/interface/CaloClusterFwd.h"
 
-/* #include <boost/cstdint.hpp>  */
-
 namespace {
+  struct dictionary {
     std::vector<reco::CaloCluster> v11;
     std::vector<reco::CaloClusterPtr> v12;
     reco::CaloClusterCollection v1;
     edm::Ptr<reco::CaloCluster> p1;
     edm::PtrVector<reco::CaloCluster> pv1;
     edm::Wrapper<edm::PtrVector<reco::CaloCluster> > wpv1;
+  };
 }
