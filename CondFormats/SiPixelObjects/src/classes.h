6ce467197044bed62ad6c438564c0167ea86f337
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-20 15:56:24 +0000

    Change dummy declarations in classes.h from instances within a single or   double anonymous namespace to declarations within a struct (within an   anonymous namespace). Avoids global instances in the dictionary library.

--- CondFormats/SiPixelObjects/src/classes.h
+++ CondFormats/SiPixelObjects/src/classes.h
@@ -1,59 +1,45 @@
 #include "CondFormats/SiPixelObjects/interface/PixelROC.h"
 
 #include "CondFormats/SiPixelObjects/interface/SiPixelFedCablingMap.h"
-namespace {
-  std::map<SiPixelFedCablingMap::Key, sipixelobjects::PixelROC> theMap; 
-  std::pair<const SiPixelFedCablingMap::Key, sipixelobjects::PixelROC> theMapValueT; 
-}
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelCPEGenericErrorParm.h"
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelPedestals.h"
-namespace{
-  std::map< unsigned int, SiPixelPedestals::SiPixelPedestalsVector> sipixped;
-}
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibration.h"
-namespace { 
-  std::vector<char>::iterator p1;
-  std::vector<char>::const_iterator p2;
-  std::vector< SiPixelGainCalibration::DetRegistry >::iterator p3;
-  std::vector< SiPixelGainCalibration::DetRegistry >::const_iterator p4;
-}
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibrationForHLT.h"
-namespace {
-  std::vector< SiPixelGainCalibrationForHLT::DetRegistry >::iterator p5;
-  std::vector< SiPixelGainCalibrationForHLT::DetRegistry >::const_iterator p6;
-}
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelGainCalibrationOffline.h"
-namespace {
-  std::vector< SiPixelGainCalibrationOffline::DetRegistry >::iterator p7;
-  std::vector< SiPixelGainCalibrationOffline::DetRegistry >::const_iterator p8;
-}
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelTemplate.h"
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelTemplateDBObject.h"
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelLorentzAngle.h"
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelPerformanceSummary.h"
-namespace{
-  std::vector<SiPixelPerformanceSummary::DetSummary>::iterator spps1;
-  std::vector<SiPixelPerformanceSummary::DetSummary>::const_iterator spps2;
-}  
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelCalibConfiguration.h"
-
 #include "CondFormats/SiPixelObjects/interface/SiPixelQuality.h"
-namespace{
-  std::vector<SiPixelQuality::disabledModuleType>::iterator p9;
-  std::vector<SiPixelQuality::disabledModuleType>::const_iterator p10;
-}
-
 #include "CondFormats/SiPixelObjects/interface/PixelDCSObject.h"
 template class PixelDCSObject<bool>;
 template class PixelDCSObject<float>;
 template class PixelDCSObject<CaenChannel>;
+
+namespace {
+  struct dictionary {
+    std::map<SiPixelFedCablingMap::Key, sipixelobjects::PixelROC> theMap; 
+    std::pair<const SiPixelFedCablingMap::Key, sipixelobjects::PixelROC> theMapValueT; 
+ 
+    std::map< unsigned int, SiPixelPedestals::SiPixelPedestalsVector> sipixped;
+ 
+    std::vector<char>::iterator p1;
+    std::vector<char>::const_iterator p2;
+    std::vector< SiPixelGainCalibration::DetRegistry >::iterator p3;
+    std::vector< SiPixelGainCalibration::DetRegistry >::const_iterator p4;
+ 
+    std::vector< SiPixelGainCalibrationForHLT::DetRegistry >::iterator p5;
+    std::vector< SiPixelGainCalibrationForHLT::DetRegistry >::const_iterator p6;
+ 
+    std::vector< SiPixelGainCalibrationOffline::DetRegistry >::iterator p7;
+    std::vector< SiPixelGainCalibrationOffline::DetRegistry >::const_iterator p8;
+ 
+    std::vector<SiPixelPerformanceSummary::DetSummary>::iterator spps1;
+    std::vector<SiPixelPerformanceSummary::DetSummary>::const_iterator spps2;
+ 
+    std::vector<SiPixelQuality::disabledModuleType>::iterator p9;
+    std::vector<SiPixelQuality::disabledModuleType>::const_iterator p10;
+  };
+}
+
--- CondFormats/SiStripObjects/src/classes.h
+++ CondFormats/SiStripObjects/src/classes.h
@@ -12,63 +12,63 @@
 #include "CondFormats/SiStripObjects/interface/SiStripSummary.h"
 
 namespace {
-
-  std::vector<FedChannelConnection>::iterator tmp0;
-  std::vector<FedChannelConnection>::const_iterator tmp1;
-  std::vector< std::vector<FedChannelConnection> >::iterator tmp2;
-  std::vector< std::vector<FedChannelConnection> >::const_iterator tmp3;
+  struct dictionary {
+    std::vector<FedChannelConnection>::iterator tmp0;
+    std::vector<FedChannelConnection>::const_iterator tmp1;
+    std::vector< std::vector<FedChannelConnection> >::iterator tmp2;
+    std::vector< std::vector<FedChannelConnection> >::const_iterator tmp3;
   
 #ifdef SISTRIPCABLING_USING_NEW_STRUCTURE
   
-  SiStripFedCabling::Feds                temp1;
-  SiStripFedCabling::FedsIter            temp2;
-  SiStripFedCabling::FedsIterRange       temp3;
-  SiStripFedCabling::FedsConstIter       temp4;
-  SiStripFedCabling::FedsConstIterRange  temp5;
-  SiStripFedCabling::Conns               temp6;
-  SiStripFedCabling::ConnsPair           temp7;
-  SiStripFedCabling::ConnsIter           temp8;
-  SiStripFedCabling::ConnsIterRange      temp9;
-  SiStripFedCabling::ConnsConstIter      temp10;
-  SiStripFedCabling::ConnsConstIterRange temp11;
-  SiStripFedCabling::Registry            temp12;
+    SiStripFedCabling::Feds                temp1;
+    SiStripFedCabling::FedsIter            temp2;
+    SiStripFedCabling::FedsIterRange       temp3;
+    SiStripFedCabling::FedsConstIter       temp4;
+    SiStripFedCabling::FedsConstIterRange  temp5;
+    SiStripFedCabling::Conns               temp6;
+    SiStripFedCabling::ConnsPair           temp7;
+    SiStripFedCabling::ConnsIter           temp8;
+    SiStripFedCabling::ConnsIterRange      temp9;
+    SiStripFedCabling::ConnsConstIter      temp10;
+    SiStripFedCabling::ConnsConstIterRange temp11;
+    SiStripFedCabling::Registry            temp12;
 
 #endif
   
-  std::vector<char>::iterator tmp4;
-  std::vector<char>::const_iterator tmp5;
-  std::vector< SiStripPedestals::DetRegistry >::iterator tmp6;
-  std::vector< SiStripPedestals::DetRegistry >::const_iterator tmp7;
-  
-  std::vector<unsigned char>::iterator tmp8;
-  std::vector<unsigned char>::const_iterator tmp9;
-  std::vector< SiStripNoises::DetRegistry >::iterator tmp10;
-  std::vector< SiStripNoises::DetRegistry >::const_iterator tmp11;
-
-  std::vector<unsigned int>::iterator tmp12;
-  std::vector<unsigned int>::const_iterator tmp13;
-
-  std::vector<int>::iterator tmp12a;
-  std::vector<int>::const_iterator tmp13a;
-  std::vector<uint32_t>::const_iterator tmp16;
-  std::vector<uint32_t>::iterator tmp17;
-  std::vector< SiStripBadStrip::DetRegistry >::iterator tmp14;
-  std::vector< SiStripBadStrip::DetRegistry >::const_iterator tmp15;
-
-  std::vector<float>::iterator tmp18;
-  std::vector<float>::const_iterator tmp19;
-  std::vector< SiStripPerformanceSummary::DetSummary >::iterator tmp20;
-  std::vector< SiStripPerformanceSummary::DetSummary >::const_iterator tmp21;
-
-  std::vector<SiStripThreshold::Container>::iterator tmp22;
-  std::vector<SiStripThreshold::Container>::const_iterator tmp23;
-  std::vector< SiStripThreshold::DetRegistry >::iterator tmp24;
-  std::vector< SiStripThreshold::DetRegistry >::const_iterator tmp25;
-
-  std::vector<std::string>::iterator tmp30;
-  std::vector<std::string>::const_iterator tmp31;
-  std::vector< SiStripSummary::DetRegistry >::iterator tmp32;
-  std::vector< SiStripSummary::DetRegistry >::const_iterator tmp33;
-
+    std::vector<char>::iterator tmp4;
+    std::vector<char>::const_iterator tmp5;
+    std::vector< SiStripPedestals::DetRegistry >::iterator tmp6;
+    std::vector< SiStripPedestals::DetRegistry >::const_iterator tmp7;
+    
+    std::vector<unsigned char>::iterator tmp8;
+    std::vector<unsigned char>::const_iterator tmp9;
+    std::vector< SiStripNoises::DetRegistry >::iterator tmp10;
+    std::vector< SiStripNoises::DetRegistry >::const_iterator tmp11;
+ 
+    std::vector<unsigned int>::iterator tmp12;
+    std::vector<unsigned int>::const_iterator tmp13;
+ 
+    std::vector<int>::iterator tmp12a;
+    std::vector<int>::const_iterator tmp13a;
+    std::vector<uint32_t>::const_iterator tmp16;
+    std::vector<uint32_t>::iterator tmp17;
+    std::vector< SiStripBadStrip::DetRegistry >::iterator tmp14;
+    std::vector< SiStripBadStrip::DetRegistry >::const_iterator tmp15;
+ 
+    std::vector<float>::iterator tmp18;
+    std::vector<float>::const_iterator tmp19;
+    std::vector< SiStripPerformanceSummary::DetSummary >::iterator tmp20;
+    std::vector< SiStripPerformanceSummary::DetSummary >::const_iterator tmp21;
+ 
+    std::vector<SiStripThreshold::Container>::iterator tmp22;
+    std::vector<SiStripThreshold::Container>::const_iterator tmp23;
+    std::vector< SiStripThreshold::DetRegistry >::iterator tmp24;
+    std::vector< SiStripThreshold::DetRegistry >::const_iterator tmp25;
+ 
+    std::vector<std::string>::iterator tmp30;
+    std::vector<std::string>::const_iterator tmp31;
+    std::vector< SiStripSummary::DetRegistry >::iterator tmp32;
+    std::vector< SiStripSummary::DetRegistry >::const_iterator tmp33;
+  };
 }  
   
