9c406bbab593810e2330f997c9f9bb4f0847d9f7
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-01-20 20:24:23 +0000

    avoid endless loop in case of failes mille jobs

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_auto.pl
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_auto.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 #     R. Mankel, DESY Hamburg     17-Oct-2007
 #     A. Parenti, DESY Hamburg    16-Apr-2008
-#     $Revision: 1.3 $
-#     $Date: 2008/07/29 15:36:18 $
+#     $Revision: 1.4 $
+#     $Date: 2009/01/07 18:24:07 $
 #
 #  Try periodically to fire merge job.
 #  Terminate when done
@@ -40,8 +40,8 @@ while (@ARGV) {
 
 if ($helpwanted != 0 ) {
   print "Usage:\n  mps_auto.pl [options] [seconds]";
-  print "\nTry to submit merge job every 'seconds' (default: 60) seconds.";
-  print "\nIn case of a problem in a mille job, will hang in an endless loop...";
+  print "\nTry to submit merge job every 'seconds' (default: 60, min: 20) seconds.";
+  print "\nStops in case of a problem in a mille job.";
   print "\nKnown options:";
   print "\n -h   This help.\n";
   exit;
@@ -58,12 +58,21 @@ $done = 0;
 $iter = 0;
 while ($done == 0) {
   if ($iter != 0) {
-    # In case mps_fetch.pl detects an error, it will never end!
     print  "mps_stat.pl > /dev/null; mps_fetch.pl\n";
     system "mps_stat.pl > /dev/null; mps_fetch.pl";
-    print  "In case of error, abort this mps_auto.pl, otherwise endless loop....\n";
-    print  "mps_fire.pl -m; sleep $seconds\n";
-    system "mps_fire.pl -m; sleep $seconds";
+    read_db();
+    # loop over mille jobs
+    for ($i=0; $i<$nJobs; ++$i) {
+	 if (@JOBSTATUS[$i] ne "RUN" && @JOBSTATUS[$i] ne "DONE" 
+	     && @JOBSTATUS[$i] ne "OK" && @JOBSTATUS[$i] ne  "PEND") {
+	     $done = 1;
+	     print "Mille job $i in unknown or bad status @JOBSTATUS[$i], stopping.\n";
+	 }
+    }
+    if ($done == 0) { # only in case of no problem...
+	print  "mps_fire.pl -m; sleep $seconds\n";
+	system "mps_fire.pl -m; sleep $seconds";
+    }
   }
   $iter = $iter + 1;
   # go to merge job 
