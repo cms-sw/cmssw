0fc2a26c0e4954b08881df897abb23415a90f2b0
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-20 00:54:00 +0000

    Change dummy declarations in classes.h from instances within a double   anonymous namespace to declarations within a struct (within an anonymous   namespace). Avoids global instances in the dictionary library.

--- DataFormats/HcalCalibObjects/src/classes.h
+++ DataFormats/HcalCalibObjects/src/classes.h
@@ -3,11 +3,11 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
-  namespace {
+  struct dictionary {
    HOCalibVariables                             rv1;
    std::vector<HOCalibVariables>                v1;
    edm::Wrapper<std::vector<HOCalibVariables> > wc1;
    //    HOCalibVariableCollection dummy0;
    //    edm::Wrapper< HOCalibVariableCollection > dummy1;
-  }
+  };
 }
--- DataFormats/V0Candidate/src/classes.h
+++ DataFormats/V0Candidate/src/classes.h
@@ -1,8 +1,8 @@
 #include "DataFormats/V0Candidate/interface/V0Candidate.h"
 
 namespace {
-  namespace {
+  struct dictionary {
     std::vector<reco::V0Candidate> v01;
     edm::Wrapper<std::vector<reco::V0Candidate> > wv01;
-  }
+  };
 }
