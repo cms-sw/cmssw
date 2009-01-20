6238ef52978c5d7ea9c0835913a198cab3481356
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2009-01-20 07:50:20 +0000

    Add new sequence by Stefano Argiro'

--- HLTriggerOffline/Common/python/HLTValidation_cff.py
+++ HLTriggerOffline/Common/python/HLTValidation_cff.py
@@ -1,12 +1,14 @@
 from HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi import *
 from HLTriggerOffline.Tau.Validation.HLTTauValidation_cff import *
 from HLTriggerOffline.special.MonitorAlCaEcalPi0_cfi import *
+from HLTriggerOffline.special.EcalPhiSymHLTVal_cfi import *
 from HLTriggerOffline.Egamma.EgammaValidation_cff import *
 from HLTriggerOffline.Top.topvalidation_cfi import *
 hltvalidation = cms.Sequence(
     muonTriggerRateTimeAnalyzer
     +HLTTauVal
     +EcalPi0Mon
+    +EcalPhiSymMon
     +egammaValidationSequence
     +HLTTopVal
     )
