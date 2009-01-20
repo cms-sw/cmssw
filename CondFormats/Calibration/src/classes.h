36cbc75bcbf956daec1427ce1dc26d12a875f4e5
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-01-20 14:56:31 +0000

    Change dummy declarations in classes.h from instances within a single or   double anonymous namespace to declarations within a struct (within an   anonymous namespace). Avoids global instances in the dictionary library.

--- CondFormats/Calibration/src/classes.h
+++ CondFormats/Calibration/src/classes.h
@@ -17,31 +17,33 @@
 
 #include "CondFormats/Calibration/interface/LuminosityInfo.h"
 namespace {
-  std::vector< Pedestals::Item >::iterator tmp0;
-  std::vector< Pedestals::Item >::const_iterator tmp1;
-  std::vector< std::vector<BlobNoises::DetRegistry> >::iterator tmp2;
-  std::vector< std::vector<BlobNoises::DetRegistry> >::const_iterator tmp3; 
-  std::vector< mySiStripNoises::DetRegistry >::iterator tmp6;
-  std::vector< mySiStripNoises::DetRegistry >::const_iterator tmp7;   
-  std::vector<unsigned int>::iterator tmp8;
-  std::vector<unsigned int>::const_iterator tmp9;
-  std::vector<short>::iterator tmp10;
-  std::vector<short>::const_iterator tmp11;
-  std::vector<float>::iterator tmp12;
-  std::vector<float>::const_iterator tmp13;
-  std::vector<CalibHistogram>::iterator tmp14;
-  std::vector<CalibHistogram>::const_iterator tmp15;
-  std::vector<BlobComplexData>::iterator tmp16;
-  std::vector<BlobComplexData>::const_iterator tmp17;
-  std::vector<BlobComplexContent>::iterator tmp18;
-  std::vector<BlobComplexContent>::const_iterator tmp19;
-  std::vector<BlobComplexObjects>::iterator tmp20;
-  std::vector<BlobComplexObjects>::const_iterator tmp21;
-  std::bitset<7> a;
-  std::bitset<8> b;
-  BitArray<9> c;
-  fixedArray<unsigned short,2097> d;
-  std::map<std::string, Algo> e;
-  std::map<std::string, Algo>::iterator tmp22;
-  std::map<std::string, Algo>::const_iterator tmp23;
+  struct dictionary {
+    std::vector< Pedestals::Item >::iterator tmp0;
+    std::vector< Pedestals::Item >::const_iterator tmp1;
+    std::vector< std::vector<BlobNoises::DetRegistry> >::iterator tmp2;
+    std::vector< std::vector<BlobNoises::DetRegistry> >::const_iterator tmp3; 
+    std::vector< mySiStripNoises::DetRegistry >::iterator tmp6;
+    std::vector< mySiStripNoises::DetRegistry >::const_iterator tmp7;   
+    std::vector<unsigned int>::iterator tmp8;
+    std::vector<unsigned int>::const_iterator tmp9;
+    std::vector<short>::iterator tmp10;
+    std::vector<short>::const_iterator tmp11;
+    std::vector<float>::iterator tmp12;
+    std::vector<float>::const_iterator tmp13;
+    std::vector<CalibHistogram>::iterator tmp14;
+    std::vector<CalibHistogram>::const_iterator tmp15;
+    std::vector<BlobComplexData>::iterator tmp16;
+    std::vector<BlobComplexData>::const_iterator tmp17;
+    std::vector<BlobComplexContent>::iterator tmp18;
+    std::vector<BlobComplexContent>::const_iterator tmp19;
+    std::vector<BlobComplexObjects>::iterator tmp20;
+    std::vector<BlobComplexObjects>::const_iterator tmp21;
+    std::bitset<7> a;
+    std::bitset<8> b;
+    BitArray<9> c;
+    fixedArray<unsigned short,2097> d;
+    std::map<std::string, Algo> e;
+    std::map<std::string, Algo>::iterator tmp22;
+    std::map<std::string, Algo>::const_iterator tmp23;
+  };
 }
