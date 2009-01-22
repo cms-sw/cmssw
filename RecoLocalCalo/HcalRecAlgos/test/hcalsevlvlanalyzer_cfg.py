206bdd970194ee9903fcdfd99b83b4a4873896b2
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2009-01-22 12:46:19 +0000

    first version of severity levels

--- RecoLocalCalo/HcalRecAlgos/test/hcalsevlvlanalyzer_cfg.py
+++ RecoLocalCalo/HcalRecAlgos/test/hcalsevlvlanalyzer_cfg.py
@@ -4,38 +4,13 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-#from RecoLocalCalo.HcalRecAlgos.HcalSeverityLevel_cfi import hcal_sev_producer
-
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
-#process.source = cms.Source("PoolSource",
-#    # replace 'myfile.root' with the source file you want to use
-#    fileNames = cms.untracked.vstring(
-#        'file:myfile.root'
-#    )
-#)
 
 process.source = cms.Source("EmptySource",
     numberEventsInRun = cms.untracked.uint32(1),
     firstRun = cms.untracked.uint32(1)
 )
-
-#process.hcal_sev_producer = cms.ESProducer("HcalSeverityLevelProducer",
-#     Levels = cms.VPSet(
-#        cms.PSet( Level = cms.int32(0),
-#                  RecHitFlags = cms.vstring(''),
-#                  ChannelStatus = cms.vstring('')
-#                ),
-#        cms.PSet( Level = cms.int32(10),
-#                  RecHitFlags = cms.vstring(''),
-#                  ChannelStatus = cms.vstring('HcalCellHot')
-#                ),
-#        cms.PSet( Level = cms.int32(20),
-#                  RecHitFlags = cms.vstring(''),
-#                  ChannelStatus = cms.vstring('HcalCellOff', 'HcalCellDead')
-#                )
-#        )
-#)
 
 process.demo = cms.EDAnalyzer('HcalSevLvlAnalyzer',
      Levels = cms.VPSet(
@@ -53,6 +28,15 @@ process.demo = cms.EDAnalyzer('HcalSevLvlAnalyzer',
                   ChannelStatus = cms.vstring('HcalCellOff', 'HcalCellDead')
                 )
         )
+# short instruction:
+#  severity levels are defined to grade a problem indicated through flags
+#
+#  The algorithm works from the highest level down. For each level, it determines whether any of the
+#  bits that is defined for its level is set. If yes, then - regardless of the setting of the other bits -
+#  it gives back the corresponding severity level, if no, it continues with the next lower level.
+#  If a defined bit vector is empty, the corresponding flag is not checked.
+#  This means that the highest level that has two empty vectors will be always the default level.
+# 
 )
 
 
