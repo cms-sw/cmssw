2a9a85e09feff6f3232be6610a12b3391317f47e
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-01-22 13:42:22 +0000

    removed commented-out code (clean-up)

--- PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
+++ PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
@@ -7,9 +7,9 @@
  * 
  * \author Luca Lista, INFN
  *
- * \version $Revision: 1.10 $
+ * \version $Revision: 1.11 $
  *
- * $Id: ObjectCountFilter.h,v 1.10 2007/06/18 18:33:52 llista Exp $
+ * $Id: ObjectCountFilter.h,v 1.11 2009/01/22 13:35:13 veelken Exp $
  *
  */
 
@@ -21,29 +21,6 @@
 #include "PhysicsTools/UtilAlgos/interface/CollectionFilterTrait.h"
 #include "PhysicsTools/UtilAlgos/interface/EventSelectorAdapter.h"
 #include "PhysicsTools/UtilAlgos/interface/ObjectCountEventSelector.h"
-
-/*
-template<typename C, 
-	 typename S = AnySelector,
-	 typename N = MinNumberSelector,
-	 typename CS = typename helper::CollectionFilterTrait<C, S, N>::type>
-class ObjectCountFilter : public edm::EDFilter 
-{
- public:
-  /// constructor 
-  explicit ObjectCountFilter( const edm::ParameterSet & cfg ) :
-    eventSelector_( cfg ) {
-  }
-  
- private:
-  /// process one event
-  bool filter( edm::Event& evt, const edm::EventSetup& ) {
-    return eventSelector_( evt );
-  }
-
-  ObjectCountEventSelector<C, S, N, CS> eventSelector_;
-};
-*/
 
 template<typename C, 
 	 typename S = AnySelector,
