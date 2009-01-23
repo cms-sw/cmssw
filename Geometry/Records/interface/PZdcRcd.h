21fc29b71341823ad261211972c1ed07a845c10c
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-01-23 16:15:15 +0000

    Add Zdc and Castor DB records

---
+++ Geometry/Records/interface/PZdcRcd.h
@@ -0,0 +1,6 @@
+#ifndef PZDCRCD_H
+#define PZDCRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class PZdcRcd : public edm::eventsetup::EventSetupRecordImplementation<PZdcRcd> {};
+#endif
