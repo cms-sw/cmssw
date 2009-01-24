8812b4cf5c7da41b2678abe269a36856ded0f920
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-01-24 04:22:44 +0000

    Make alignment param tracked

--- Geometry/HcalEventSetup/python/HcalHardcodeGeometry_cfi.py
+++ Geometry/HcalEventSetup/python/HcalHardcodeGeometry_cfi.py
@@ -1,16 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 HcalHardcodeGeometryEP = cms.ESProducer( "HcalHardcodeGeometryEP" ,
-                                         applyAlignment = cms.untracked.bool(False),
                                          appendToDataLabel = cms.string("_master")
                                          )
 
 HcalGeometryFromDBEP = cms.ESProducer( "HcalGeometryFromDBEP",
-                                       applyAlignment = cms.untracked.bool(False)
+                                       applyAlignment = cms.bool(False)
                                        )
 
 HcalGeometryToDBEP = cms.ESProducer( "HcalGeometryToDBEP" ,
-                                     applyAlignment = cms.untracked.bool(False) ,
+                                     applyAlignment = cms.bool(False) ,
                                      appendToDataLabel = cms.string("_toDB")
                                      )
 
