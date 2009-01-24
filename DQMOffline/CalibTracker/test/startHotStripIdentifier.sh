6b7d5672d108cb5162bd9a6cfb8245ed1ac733b7
kaussen <sha1-b58974427aa4d349705fe1a36f0e42a0fed605a3@cern.ch>
2009-01-24 16:12:41 +0000

    replaced old cfg-files by py-files

--- DQMOffline/CalibTracker/test/startHotStripIdentifier.sh
+++ DQMOffline/CalibTracker/test/startHotStripIdentifier.sh
@@ -14,15 +14,15 @@ do
 
   echo $file $run
 
-  cat template_SiStripQualityHotStripIdentifierRoot.cfg |sed -e "s@insertRun@$run@g"  -e "s@insertInputDQMfile@$file@"  -e "s@@@" > log/SiStripQualityHotStripIdentifierRoot_$run.cfg
+  cat template_SiStripQualityHotStripIdentifierRoot_cfg.py |sed -e "s@insertRun@$run@g"  -e "s@insertInputDQMfile@$file@"  -e "s@@@" > log/SiStripQualityHotStripIdentifierRoot_${run}_cfg.py
 
-  cmsRun log/SiStripQualityHotStripIdentifierRoot_$run.cfg > log/SiStripQualityHotStripIdentifierRoot_$run.log
+  cmsRun log/SiStripQualityHotStripIdentifierRoot_${run}_cfg.py > log/SiStripQualityHotStripIdentifierRoot_$run.log
 
 done
 
 rm -rf TkMaps
 mkdir TkMaps
-cmsRun SiStripQualityStatistics_offline.cfg > out
+cmsRun SiStripQualityStatistics_offline_cfg.py > out
 cat out | awk 'BEGIN{doprint=0}{if(match($0,"New IOV")!=0) doprint=1;if(match($0,"%MSG")!=0) {doprint=0;print "";} if(doprint==1) print $0}' > BadStrips_x_IOV_offline.txt
 mv TkMaps/* /storage/data2/SiStrip/quality/TkMaps_offline/
 mv BadStrips_x_IOV_offline.txt /storage/data2/SiStrip/quality
@@ -32,7 +32,7 @@ cd -
 
 rm -rf TkMaps
 mkdir TkMaps
-cmsRun SiStripQualityStatistics_full.cfg > out
+cmsRun SiStripQualityStatistics_full_cfg.py > out
 cat out | awk 'BEGIN{doprint=0}{if(match($0,"New IOV")!=0) doprint=1;if(match($0,"%MSG")!=0) {doprint=0;print "";} if(doprint==1) print $0}' > BadStrips_x_IOV_all.txt
 mv TkMaps/* /storage/data2/SiStrip/quality/TkMaps_all/
 mv BadStrips_x_IOV_all.txt /storage/data2/SiStrip/quality
---
+++ DQMOffline/CalibTracker/test/template_SiStripQualityHotStripIdentifierRoot_cfg.py
@@ -0,0 +1,63 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("ICALIB")
+process.load("DQMServices.Core.DQM_cfg")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('*'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    firstValue = cms.uint64(insertRun),
+    lastValue = cms.uint64(insertRun),
+    timetype = cms.string('runnumber'),
+    interval = cms.uint64(1)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:dbfile.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('SiStripBadStrip'),
+        tag = cms.string('SiStripBadChannel_v1')
+    ))
+)
+
+process.prod = cms.EDFilter("SiStripQualityHotStripIdentifierRoot",
+    OccupancyRootFile = cms.untracked.string('HotStripsOccupancy_insertRun.root'),
+    OccupancyH_Xmax = cms.untracked.double(1.0),
+    AlgoParameters = cms.PSet(
+        AlgoName = cms.string('SiStripHotStripAlgorithmFromClusterOccupancy'),
+        NumberOfEvents = cms.untracked.uint32(0),
+        ProbabilityThreshold = cms.untracked.double(1e-07),
+        MinNumEntriesPerStrip = cms.untracked.uint32(20),
+        MinNumEntries = cms.untracked.uint32(0),
+        OccupancyThreshold = cms.untracked.double(0.0001)
+    ),
+    SinceAppendMode = cms.bool(True),
+    verbosity = cms.untracked.uint32(0),
+    OccupancyH_Xmin = cms.untracked.double(-0.0005),
+    IOVMode = cms.string('Run'),
+    Record = cms.string('SiStripBadStrip'),
+    rootDirPath = cms.untracked.string(''),
+    rootFilename = cms.untracked.string('insertInputDQMfile'),
+    doStoreOnDB = cms.bool(True),
+    OccupancyH_Nbin = cms.untracked.uint32(1001)
+)
+
+process.out = cms.OutputModule("AsciiOutputModule")
+
+process.p = cms.Path(process.prod)
+process.ep = cms.EndPath(process.out)
+
