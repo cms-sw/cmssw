5a80671bc264341b8dc26adb8f0e57b5ad676ccf
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2009-01-26 13:47:26 +0000

    removed chi^2 from file

--- CalibMuon/CSCCalibration/test/readXtalk.cpp
+++ CalibMuon/CSCCalibration/test/readXtalk.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(){
   float leftslope,leftint,rightslope,rightint;
-  int index;
+  int index,extra;
   int nrlines1=0;
 
   std::vector<int> index_id;
@@ -22,13 +22,14 @@ int main(){
 
   std::ifstream newdata; 
   newdata.open("xtalkSummary2008_10_28.dat",std::ios::in); 
+  //newdata.open("dbxtalk.dat",std::ios::in); 
   if(!newdata) {
     std::cerr <<"Error: xtalkSummary2008_10_28.dat -> no such file!"<< std::endl;
     exit(1);
   }
 
   while (!newdata.eof() ) { 
-    newdata >> index >> leftslope >> leftint >> rightslope >> rightint ; 
+    newdata >> index >> leftslope >> leftint >> rightslope >> rightint >>extra; 
     index_id.push_back(index);
     leftSlope.push_back(leftslope);
     leftInt.push_back(leftint);
@@ -40,10 +41,11 @@ int main(){
 
   std::ofstream myXtalkFile("goodXtalk.dat",std::ios::out);
   for(int i=0; i<nrlines1-1;++i){
-    leftChi2[i]=0.0;
-     if (rightSlope[i]<-0.001 && rightInt[i]<0.1 && leftSlope[i]<-0.001 && leftInt[i]<0.1){
-       myXtalkFile<<index_id[i]<<"  "<<leftSlope[i]<<"  "<<leftInt[i]<<"   "<<leftChi2[i]<<"  "<<rightSlope[i]<<"   "<<rightInt[i]<<"  "<<leftChi2[i]<<std::endl;
-       //std::cout<<"Warning! Xtalk out of range!!"<<index_id1[i]<<"  "<<leftSlope[i]<<std::endl;
+    //leftChi2[i]=0.0;
+    //if (rightSlope[i]<-0.001 && rightSlope[i]!=-999 && rightInt[i]<0.1 && leftSlope[i]<-0.001 && leftInt[i]<0.1 ){
+    if (rightSlope[i]!=-999 && extra !=1){
+      myXtalkFile<<index_id[i]<<"  "<<leftSlope[i]<<"  "<<leftInt[i]<<"  "<<rightSlope[i]<<"   "<<rightInt[i]<<std::endl;
+      //std::cout<<"Warning! Xtalk out of range!!"<<index_id1[i]<<"  "<<leftSlope[i]<<std::endl;
     }
   }
 }
