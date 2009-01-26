a8a4b7c074c7ce7eaabe0669ea58897e063a4910
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-01-26 15:47:42 +0000

    - fix muon sequence for 22X

--- DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_SecondStep_cff.py
@@ -9,9 +9,7 @@ from DQM.SiStripMonitorClient.SiStripClientConfig_Tier0_cff import *
 from DQM.DTMonitorClient.dtDQMOfflineClients_cff import *
 from DQMOffline.Trigger.DQMOffline_HLT_Client_cff import *
 from DQM.RPCMonitorClient.RPCTier0Client_cff import *
-from DQMOffline.EGamma.photonOfflineDQMClient_cff import *
-
 from DQMServices.Components.DQMFEDIntegrityClient_cff import *
 
-DQMOfflineCosmics_SecondStep = cms.Sequence(ecal_dqm_client_offline*cosmicMuonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient*SiStripOfflineDQMClient*dtClients*hltOfflineDQMClient*rpcTier0Client*photonOfflineDQMClient*DQMFEDIntegrityClient)
+DQMOfflineCosmics_SecondStep = cms.Sequence(ecal_dqm_client_offline*cosmicMuonQualityTests*hcalOfflineDQMClient*sipixelEDAClient*triggerOfflineDQMClient*SiStripOfflineDQMClient*dtClients*hltOfflineDQMClient*rpcTier0Client*DQMFEDIntegrityClient)
 
