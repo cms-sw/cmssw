aed5d7afdcfb8f93d692972a027a1fa2d717637a
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-01-27 17:03:38 +0000

    First commit.

---
+++ AnalysisAlgos/SiStripClusterInfoProducer/python/SiStripProcessedRawDigiProducer_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+#defaults as in SiStripZeroSuppression_cfi.py
+siStripProcessedDigis = cms.EDProducer("SiStripProcessedRawDigiProducer",
+                                       CommonModeNoiseSubtractionMode = cms.string('Median'), 
+                                       CutToAvoidSignal = cms.double(3.0), ##This is just for CMNSub...Mode TT6
+                                       DigiProducersList = cms.VPSet( cms.PSet(DigiProducer = cms.InputTag('siStripDigis', 'ZeroSuppressed') ),
+                                                                      cms.PSet(DigiProducer = cms.InputTag('siStripDigis', 'VirginRaw'     ) ),
+                                                                      cms.PSet(DigiProducer = cms.InputTag('siStripDigis', 'ProcessedRaw'  ) ),
+                                                                      cms.PSet(DigiProducer = cms.InputTag('siStripDigis', 'ScopeMode'     ) )
+                                                                      )
+                                       )
