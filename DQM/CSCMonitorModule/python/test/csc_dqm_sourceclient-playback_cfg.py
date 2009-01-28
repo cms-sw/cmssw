e5a79f7f6135d293093011f6f608013dbef67461
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-01-28 10:58:46 +0000

    cfg update

--- DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-playback_cfg.py
+++ DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-playback_cfg.py
@@ -50,7 +50,7 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 #process.DQMStore.referenceFileName = '/home/dqmdevlocal/reference/csc_reference.root'
-process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/CMSSW_2_1_9/src/DQM/CSCMonitorModule/data/csc_reference.root'
+process.DQMStore.referenceFileName = '/afs/cern.ch/user/v/valdo/data/csc_reference.root'
 #process.DQMStore.referenceFileName = '/nfshome0/valdo/CMSSW_2_1_0/src/DQM/CSCMonitorModule/data/csc_reference.root'
 
 #----------------------------
