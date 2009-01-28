d2a7a72f1a26a3dfcd80dc388094c675467c669a
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2009-01-28 17:43:22 +0000

    upgrade with more abilities

--- HLTriggerOffline/special/python/hltHITval_cfi.py
+++ HLTriggerOffline/special/python/hltHITval_cfi.py
@@ -1,26 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
 hltHITval = cms.EDAnalyzer('DQMHcalIsoTrackAlCaRaw',
-sampleCrossSection=cms.double(3.658E7),
-luminosity=cms.double(2E31),
-outputTxtFileName=cms.string("rates.txt"),
+
+produceRates=cms.bool(False), 
+sampleCrossSection=cms.double(7.53E10),
+luminosity=cms.double(1E31),
+outputTxtFileName=cms.string("rates_minBias.txt"),
+gtDigiLabel=cms.InputTag("hltGtDigis"),
+l1seedNames=cms.vstring("L1_SingleJet30",
+                        "L1_SingleJet50",
+                        "L1_SingleJet70",
+                        "L1_SingleJet100",
+                        "L1_SingleTauJet30",
+                        "L1_SingleTauJet40",
+                        "L1_SingleTauJet60",
+                        "L1_SingleTauJet80"),
+hltL1extraJetLabel=cms.VInputTag("hltL1extraParticles:Tau",
+                                 "hltL1extraParticles:Central",
+                                 "hltL1extraParticles:Forward"),
+CheckL1TurnOn=cms.bool(False),
+genJetsLabel=cms.InputTag("iterativeCone5GenJets"),
+
 folderName=cms.string("HLT/AlCa_IsoTrack"),
 outputRootFileName=cms.string("hltHITval.root"),
-hltTriggerEventLabel=cms.InputTag("hltTriggerSummaryAOD"),
-hltL3FilterLabel=cms.InputTag("hltIsolPixelTrackFilter::HLT"),
-hltL1extraJetLabel=cms.VInputTag("hltL1extraParticles:Tau",
-				 "hltL1extraParticles:Central",
-				 "hltL1extraParticles:Forward"),
-l1seedNames=cms.vstring("L1_SingleJet30",
-			"L1_SingleJet50",
-			"L1_SingleJet70",
-			"L1_SingleJet100",
-			"L1_SingleTauJet30",
-			"L1_SingleTauJet40",
-			"L1_SingleTauJet60",
-			"L1_SingleTauJet80"),
-gtDigiLabel=cms.InputTag("hltGtDigis"),
-useReco=cms.bool(True),
-recoTracksLabel=cms.InputTag("generalTracks")
 
+DebugL2=cms.bool(False),
+L2producerLabel=cms.InputTag("hltIsolPixelTrackProd"),
+
+hltTriggerEventLabel=cms.InputTag("hltTriggerSummaryAOD"),
+hltL3FilterLabel=cms.InputTag("hltIsolPixelTrackFilter::HLT5"),
+
+useReco=cms.bool(False),
+recoTracksLabel=cms.InputTag("generalTracks")
 )
