3b2e7f3820366465d48c4a0ab5b5f9dba797992e
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-01-28 08:44:10 +0000

    adjust the name of muon global sequence

--- Validation/Configuration/python/postValidation_cff.py
+++ Validation/Configuration/python/postValidation_cff.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoMuon.PostProcessor_cff import *
 from Validation.RecoTrack.PostProcessorTracker_cfi import *
-postValidation = cms.Sequence(postProcessorMuonMultiTrack+postProcessorRecoMuon+postProcessorTrack)
+postValidation = cms.Sequence(recoMuonPostProcessors+postProcessorTrack)
