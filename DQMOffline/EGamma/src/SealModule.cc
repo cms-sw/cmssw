689c0b1ac5cea37a446e719382074e5323e62fb6
Louis James Antonelli <sha1-442197ce02a0252aaee7a57f46fd47b9778e7d2f@cern.ch>
2009-01-28 14:05:42 +0000

    added PiZero and DataCertification

--- DQMOffline/EGamma/src/SealModule.cc
+++ DQMOffline/EGamma/src/SealModule.cc
@@ -3,9 +3,14 @@
 #include "DQMOffline/EGamma/interface/PhotonAnalyzer.h"
 #include "DQMOffline/EGamma/interface/ElectronAnalyzer.h"
 #include "DQMOffline/EGamma/interface/PhotonOfflineClient.h"
+#include "DQMOffline/EGamma/interface/PhotonDataCertification.h"
+#include "DQMOffline/EGamma/interface/PiZeroAnalyzer.h"
+
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(PhotonAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(ElectronAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(PhotonOfflineClient);
+DEFINE_ANOTHER_FWK_MODULE(PhotonDataCertification);
+DEFINE_ANOTHER_FWK_MODULE(PiZeroAnalyzer);
