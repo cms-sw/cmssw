aa7122878c99098591f3192b30d5134a05a0aa8f
Davide Piccolo <sha1-4e195f5cd401b82ff84f28ccab0b0cfc64c1031c@cern.ch>
2009-01-28 10:39:11 +0000

    small fix to be sure that the daughters come from a Z

--- ElectroWeakAnalysis/ZMuMu/plugins/DimuonMCMatcher.cc
+++ ElectroWeakAnalysis/ZMuMu/plugins/DimuonMCMatcher.cc
@@ -1,5 +1,6 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
+#include <iostream>
 
 class DimuonMCMatcher : public edm::EDProducer {
 public:
@@ -32,7 +33,9 @@ void DimuonMCMatcher::produce(edm::Event& evt, const edm::EventSetup&) {
   evt.getByLabel(src_, src);
   auto_ptr<vector<GenParticleRef> > matched(new vector<GenParticleRef>);
   matched->reserve(src->size());
+  int j=0;
   for(CandidateView::const_iterator i = src->begin(); i != src->end(); ++i) {
+    j++;
     const Candidate * dau1 = i->daughter(0);
     const Candidate * dau2 = i->daughter(1);
     if(dau1 == 0|| dau2 == 0) 
@@ -43,6 +46,7 @@ void DimuonMCMatcher::produce(edm::Event& evt, const edm::EventSetup&) {
     const pat::Muon * mu1 = dynamic_cast<const pat::Muon*>(c1);
     if(mu1 != 0) {
       mc1 = mu1->genParticleRef();
+      //     if (mc1.isNonnull()) cout << "DimuonMCMatcher> genParticleRef1 " << mc1->pdgId() << endl;
     } else {
       const pat::GenericParticle * gp1 = dynamic_cast<const pat::GenericParticle*>(c1);
       if(gp1 == 0) 
@@ -55,6 +59,7 @@ void DimuonMCMatcher::produce(edm::Event& evt, const edm::EventSetup&) {
     const pat::Muon * mu2 = dynamic_cast<const pat::Muon*>(c2);
     if(mu2 != 0) {
       mc2 = mu2->genParticleRef();
+      //      if (mc2.isNonnull()) cout << "DimuonMCMatcher> genParticleRef2 " << mc2->pdgId() << endl;
     } else {
       const pat::GenericParticle * gp2 = dynamic_cast<const pat::GenericParticle*>(c2);
       if(gp2 == 0) 
@@ -63,15 +68,20 @@ void DimuonMCMatcher::produce(edm::Event& evt, const edm::EventSetup&) {
       mc2 = gp2->genParticleRef();
     }
     GenParticleRef dimuonMatch;
+    //    cout << "DimuonMatcher>  mc1 " << mc1.isNonnull() << "   mc2 " << mc2.isNonnull() << endl;
     if(mc1.isNonnull() && mc2.isNonnull()) {
+      int k=0;
       do {
+	k++;
 	mc1 = mc1->numberOfMothers() > 0 ? mc1->motherRef() : GenParticleRef();
 	mc2 = mc2->numberOfMothers() > 0 ? mc2->motherRef() : GenParticleRef();
+	//	cout << "DimuonMCMatcher> do loop: " << k << "  id1 " << mc1->pdgId() << "    id2 " << mc2->pdgId() << endl;
       } while(mc1 != mc2 && mc1.isNonnull() && mc2.isNonnull());
-      if(mc1.isNonnull() && mc2.isNonnull()) {
+      if(mc1.isNonnull() && mc2.isNonnull() && mc1->pdgId()==23) {
 	dimuonMatch = mc1;
       }
     }
+    //    cout << "DimuonMatcher> dimuonMatch " << dimuonMatch.isNonnull() << endl;
     matched->push_back(dimuonMatch);
   }
 
