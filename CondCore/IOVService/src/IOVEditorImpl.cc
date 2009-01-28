51486e078fec91d6f3213b4e9b89a349870245c1
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-01-28 16:00:49 +0000

    editor

--- CondCore/IOVService/src/IOVEditorImpl.cc
+++ CondCore/IOVService/src/IOVEditorImpl.cc
@@ -22,19 +22,19 @@ namespace cond {
 
     if(!validTime(lastTill, timetype))
       throw cond::Exception("cond::IOVEditorImpl::create time not in global range");
-      
-
-    m_iov=cond::TypedRef<cond::IOVSequence>(*m_pooldb,new cond::IOVSequence((int)timetype,lastTill,"");
     
+    
+    m_iov=cond::TypedRef<cond::IOVSequence>(*m_pooldb,new cond::IOVSequence((int)timetype,lastTill,""));
+					    
     m_iov.markWrite(cond::IOVNames::container());
     m_token=m_iov.token();
     m_isActive=true;
- 
+    
   }
- 
+  
 
 
-
+  
   void IOVEditorImpl::init(){
     if(m_token.empty()){
       // problem?
@@ -45,17 +45,17 @@ namespace cond {
     m_iov=cond::TypedRef<cond::IOVSequence>(*m_pooldb, m_token); 
     m_isActive=true;
     
-}
-
-
-
+  }
+  
+  
+  
   IOVEditorImpl::~IOVEditorImpl(){
   }
   
   Time_t IOVEditorImpl::firstSince() const {
     return m_iov->firstSince();
   }
-
+  
   Time_t IOVEditorImpl::lastTill() const {
     return m_iov->lastTill();
   }
@@ -64,32 +64,32 @@ namespace cond {
     return m_iov->timeType();
   }
   
-
+  
   bool IOVEditorImpl::validTime(cond::Time_t time, cond::TimeType timetype) const {
     return time>=timeTypeSpecs[timetype].beginValue && time<=timeTypeSpecs[timetype].endValue;   
-
+    
   }
-
+  
   bool IOVEditorImpl::validTime(cond::Time_t time) const {
     return validTime(time,timetype());
   }
-
-
+  
+  
   
   unsigned int
   IOVEditorImpl::insert( cond::Time_t tillTime,
 			 const std::string& payloadToken
 			 ){
     if(!m_isActive) this->init();
-
-    if( m_iov->iov.empty() ) throw cond::Exception("cond::IOVEditorImpl::insert cannot inser into empty IOV sequence");
-
+    
+    if( m_iov->iovs().empty() ) throw cond::Exception("cond::IOVEditorImpl::insert cannot inser into empty IOV sequence");
+    
     if(!validTime(tillTime))
       throw cond::Exception("cond::IOVEditorImpl::insert time not in global range");
-  
+    
     if(tillTime<=lastTill() )
-       )    throw cond::Exception("cond::IOVEditorImpl::insert IOV not in range");
- 
+      throw cond::Exception("cond::IOVEditorImpl::insert IOV not in range");
+    
     m_iov.markUpdate();
     updateClosure(tillTime);
     return m_iov->add(lastTill(), payloadToken);
@@ -101,7 +101,7 @@ namespace cond {
     if (values.empty()) return;
     if(!m_isActive) this->init();
     cond::Time_t firstTime = values.front().first;
-    cond::Time_t   lastTime = values.back().second;
+    cond::Time_t  lastTime = values.back().first;
 
     if(!validTime(firstTime))
       throw cond::Exception("cond::IOVEditorImpl::bulkInsert first time not in global range");
@@ -110,7 +110,7 @@ namespace cond {
       throw cond::Exception("cond::IOVEditorImpl::bulkInsert last time not in global range");
 
    if(LastTime>=lastTill() ||
-      ( !m_iov->iov.empty() && firstTime<=m_iov->iov.back().second) 
+      ( !m_iov->iovs().empty() && firstTime<=m_iov->iovs().back().first) 
        )    throw cond::Exception("cond::IOVEditorImpl::bulkInsert IOV not in range");
    for(std::vector< std::pair<cond::Time_t,std::string> >::const_iterator it=values.begin(); it!=values.end(); ++it){
      //     m_iov->iov.insert(m_iov->iov.end(), values.begin(), values.end());
@@ -132,8 +132,8 @@ namespace cond {
     if(!validTime(lastTime))
       throw cond::Exception("cond::IOVEditorImpl::bulkInsert last time not in global range");
 
-   if(LastTime>=lastTill() ||
-      ( !m_iov->iov.empty() && firstTime<=m_iov->iov.back().sinceTime()) 
+   if(lastTime>=lastTill() ||
+      ( !m_iov->iovs().empty() && firstTime<=m_iov->iovs().back().sinceTime()) 
        )    throw cond::Exception("cond::IOVEditorImpl::bulkInsert IOV not in range");
 
    // m_iov->iov.insert(m_iov->iov.end(), values.begin(), values.end());
@@ -161,25 +161,22 @@ namespace cond {
     if(!m_isActive) {
       this->init();
     }
-    
-    //    if( m_iov->iov.empty() ) throw cond::Exception("cond::IOVEditorImpl::appendIOV cannot append to empty IOV index");
-    
 
-   if(!validTime(sinceTime))
+    if(!validTime(sinceTime))
       throw cond::Exception("cond::IOVEditorImpl::append time not in global range");
     
     
     if(  m_iov->iov.size()>1 ){
       //range check in case 
-      cond::Time_t lastValidSince=(++m_iov->iov.rbegin())->sinceTime();
+      cond::Time_t lastValidSince=m_iov->iovs().back().sinceTime();
       //std::cout<<"lastValidTill "<<lastValidTill<<std::endl;
-      if( sinceTime<= lastValidTill){
+      if( sinceTime<= lastValidSince){
 	throw cond::Exception("IOVEditor::append Error: since time out of range: below last since");
       }
     }
 
     // does it make sense? (in case of mixed till and since insertions...)
-    if (lastTill()<sinceTime) updateClosure(timeTypeSpecs[timetype()].endValue);
+    if (lastTill()<=sinceTime) updateClosure(timeTypeSpecs[timetype()].endValue);
 
     m_iov.markUpdate();
     return m_iov->add(sinceTime,payloadToken);
@@ -213,7 +210,7 @@ namespace cond {
  
    cond::Time_t tillTime;
    IOV::iterator p = m_iov->find(sinceTime);
-   if (p!=m_iov->iov.end() &&  (*(p-1)).sinceTime()==sinceTime)
+   if (p!=m_iov->iovs().end() &&  (*(p-1)).sinceTime()==sinceTime)
      throw cond::Exception("cond::IOVEditorImpl::freeInsert sinceTime already existing");
 
 
@@ -234,10 +231,10 @@ namespace cond {
     if(!m_isActive) this->init();
     if(withPayload){
       std::string tokenStr;
-      IOVSequence::iterator payloadIt;
-      IOVSequence::iterator payloadItEnd=m_iov->iov.end();
-      for(payloadIt=m_iov->iov.begin();payloadIt!=payloadItEnd;++payloadIt){
-	tokenStr=payloadIt->second;
+      IOVSequence::const_iterator payloadIt;
+      IOVSequence::const_iterator payloadItEnd=m_iov->iovs().end();
+      for(payloadIt=m_iov->iovs().begin();payloadIt!=payloadItEnd;++payloadIt){
+	tokenStr=payloadIt->wrapperToken;
 	cond::GenericRef ref(*m_pooldb,tokenStr);
 	ref.markDelete();
 	ref.reset();
