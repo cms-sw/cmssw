8f64a51c1d74057d8a02535e76bdbfb83a2cb56b
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2009-01-29 11:29:59 +0000

    Set back to false the standlaone. This is the main cff which is used by the central sequence and all parameters related to standalone operation must be false. They can be replaced in /test/EgammaDQMOffline_cfg.py or in the ther .cfg

--- DQMOffline/EGamma/python/egammaDQMOffline_cff.py
+++ DQMOffline/EGamma/python/egammaDQMOffline_cff.py
@@ -3,13 +3,20 @@ import FWCore.ParameterSet.Config as cms
 
 
 from DQMOffline.EGamma.photonAnalyzer_cfi import *
+from DQMOffline.EGamma.piZeroAnalyzer_cfi import *
 from DQMOffline.EGamma.electronAnalyzerSequence_cff import *
 
 photonAnalysis.OutputMEsInRootFile = cms.bool(False)
 photonAnalysis.Verbosity = cms.untracked.int32(0)
-photonAnalysis.standAlone = cms.bool(True)
+photonAnalysis.standAlone = cms.bool(False)
+
+piZeroAnalysis.OutputMEsInRootFile = cms.bool(False)
+piZeroAnalysis.Verbosity = cms.untracked.int32(0)
+piZeroAnalysis.standAlone = cms.bool(False)
+
+
 
 gsfElectronAnalysis.OutputMEsInRootFile = cms.bool(False)
 gsfElectronAnalysis.Verbosity = cms.untracked.int32(0)
 
-egammaDQMOffline = cms.Sequence(photonAnalysis*electronAnalyzerSequence)
+egammaDQMOffline = cms.Sequence(photonAnalysis*piZeroAnalysis*electronAnalyzerSequence)
