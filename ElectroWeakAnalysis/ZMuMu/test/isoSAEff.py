143e6b8ebe31d746ad1473281978f3c4e161b052
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-01-29 11:47:41 +0000

    moved and renamed toy mc

--- ElectroWeakAnalysis/ZMuMu/test/isoSAEff.py
+++ ElectroWeakAnalysis/ZMuMu/test/isoSAEff.py
@@ -11,26 +11,32 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_1.root"
-    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_2.root"
-    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_3.root"
-    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_4.root"
-    )
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_1.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_2.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_3.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_4.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_5.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_6.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_7.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_8.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_9.root",
+    "rfio:/dpm/na.infn.it/home/cms/store/user/noli/reskim/zmm/zmumu_reskim_10.root"
+   )
 )
 
 process.isoAnalyzer = cms.EDAnalyzer(
     "ZGlobalVsSAIsolationAnalyzer",
     src = cms.InputTag("goodZToMuMu"),
-    isoCut = cms.double(1.7),
+    isoCut = cms.double(3),
     veto = cms.double(0.001),
-    ptThreshold = cms.double(1),
-    etEcalThreshold = cms.double(0.2),
-    etHcalThreshold = cms.double(0.5),
-    deltaRTrk = cms.double(0.2),
-    deltaREcal = cms.double(0.25),
-    deltaRHcal = cms.double(0.25),
-    alpha = cms.double(0.75),
-    beta = cms.double(-0.75)
+    ptThreshold = cms.double(1.5),
+    etEcalThreshold = cms.double(0),
+    etHcalThreshold = cms.double(0),
+    deltaRTrk = cms.double(0.3),
+    deltaREcal = cms.double(0.3),
+    deltaRHcal = cms.double(0.3),
+    alpha = cms.double(0),
+    beta = cms.double(0)
     )
 
 process.path = cms.Path(
