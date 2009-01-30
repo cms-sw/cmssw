d71f5f5240c335f80d2037a2f2890304d7406150
pwing <sha1-235c2d278ed635ba989fe002c2020915d4875fe1@cern.ch>
2009-01-30 13:38:34 +0000

    changed charge_ and sigmanoise2_ from double to float to fix discrepancy with ThreeThresholdStripClusterizer

--- RecoLocalTracker/SiStripClusterizer/interface/SiStripThreeThresholdAlgo.h
+++ RecoLocalTracker/SiStripClusterizer/interface/SiStripThreeThresholdAlgo.h
@@ -8,6 +8,7 @@
    @class SiStripThreeThresholdAlgo
    @brief Clusterizer algorithm
 */
+
 class SiStripThreeThresholdAlgo : public SiStripClusterizerAlgo {
   
  public:
@@ -16,47 +17,48 @@ class SiStripThreeThresholdAlgo : public SiStripClusterizerAlgo {
   
   virtual ~SiStripThreeThresholdAlgo();
   
-  virtual void clusterize( const edm::DetSet<SiStripDigi>&,
-			   edm::DetSetVector<SiStripCluster>& );
+  virtual void clusterize( const edm::DetSet<SiStripDigi>&, edm::DetSetVector<SiStripCluster>& );
   
  private:
   
-  /** Building of clusters on strip-by-strip basis. */
-  virtual void add( std::vector<SiStripCluster>& data, 
-		    const uint32_t& id,
-		    const uint16_t& strip,
-		    const uint16_t& adc );
+  /// Building of clusters on strip-by-strip basis.
+
+  virtual void add( std::vector<SiStripCluster>& data, const uint32_t& id, const uint16_t& strip, const uint16_t& adc );
   
-  virtual void endDet( std::vector<SiStripCluster>&, 
-		       const uint32_t& );
+  /// close clusters for this det
+
+  virtual void endDet( std::vector<SiStripCluster>&, const uint32_t& );
   
+  /// internal methods
+
   void strip(const uint16_t&, const uint16_t&, const double&, const double&);
   void pad(const uint16_t&, const uint16_t&);
   void endCluster(std::vector<SiStripCluster>&, const uint32_t&);
   bool proximity(const uint16_t&) const;
   bool threshold(const uint16_t&, const double&, const bool) const;
   
-  //cluster thresholds
-  double stripThr_; 
-  double seedThr_;
-  double clustThr_; 
+  /// cluster thresholds
+
+  float stripThr_; 
+  float seedThr_;
+  float clustThr_; 
   uint32_t maxHoles_;
 
-  //cluster 
-  double charge_;
+  /// cluster variables
+
+  float charge_;
   bool seed_;
-  double sigmanoise2_;
-
-  //last strip
-  uint16_t strip_;
-
-  //Current cluster
+  float sigmanoise2_;
   uint16_t first_;
   std::vector<uint16_t> amps_;
 
+  /// detector variables
+
+  uint16_t strip_;  
+
   //Record of dead digis
+
   std::vector<uint16_t> digis_;
-  
 };
 
 #endif // RecoLocalTracker_SiStripClusterizer_SiStripThreeThresholdAlgo_H
