f947cfd535cd4a151fd60681a94300ffb943154a
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-01-30 16:47:05 +0000

    Removed Ntuplizer files as they are now located in another package

--- RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
+++ RecoLocalTracker/SiPixelRecHits/test/SealModules.cc
@@ -4,11 +4,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalTracker/SiPixelRecHits/test/ReadPixelRecHit.h"
-#include "RecoLocalTracker/SiPixelRecHits/test/PixelNtuplizer.h"
 #include "RecoLocalTracker/SiPixelRecHits/test/CPEAccessTester.cc"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ReadPixelRecHit);
-DEFINE_ANOTHER_FWK_MODULE(PixelNtuplizer);
 DEFINE_ANOTHER_FWK_MODULE(CPEAccessTester);
