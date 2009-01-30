94dfaa4c98d5c92fb8eb1a54566484d6524bd254
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2009-01-30 18:36:41 +0000

    1st port of the new jet-matching code (MG-like)

---
+++ GeneratorInterface/PartonShowerVeto/src/JetMatching.cc
@@ -0,0 +1,46 @@
+#include <string>
+#include <memory>
+
+#include <boost/shared_ptr.hpp>
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "GeneratorInterface/LHEInterface/interface/LHERunInfo.h"
+#include "GeneratorInterface/LHEInterface/interface/LHEEvent.h"
+
+#include "GeneratorInterface/PartonShowerVeto/interface/JetMatchingMadgraph.h"
+
+
+namespace gen {
+
+JetMatching::JetMatching(const edm::ParameterSet &params)
+{
+   fMatchingStatus=false;
+}
+
+JetMatching::~JetMatching()
+{
+}
+
+void JetMatching::init(const lhef::LHERunInfo* runInfo)
+{
+}
+
+void JetMatching::beforeHadronisation(const LHEEventProduct* event)
+{
+}
+
+void JetMatching::beforeHadronisationExec()
+{
+}
+
+std::set<std::string> JetMatching::capabilities() const
+{
+	std::set<std::string> result;
+	result.insert("psFinalState");
+	result.insert("hepmc");
+	return result;
+}
+
+} // namespace gen
