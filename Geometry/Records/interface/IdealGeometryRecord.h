acbdfea4a3bb2eb02e9e8b1267d56d642ceeea71
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-02-02 03:48:29 +0000

    Needed for XMLIdealGeometryESProducer.

--- Geometry/Records/interface/IdealGeometryRecord.h
+++ Geometry/Records/interface/IdealGeometryRecord.h
@@ -16,12 +16,16 @@
 //
 // Author:      
 // Created:     Mon Jul 25 11:05:09 EDT 2005
-// $Id$
+// $Id: IdealGeometryRecord.h,v 1.1 2005/07/25 15:11:22 chrjones Exp $
 //
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+#include "FWCore/Framework/interface/DependentRecordImplementation.h"
+#include "Geometry/Records/interface/GeometryFileRcd.h"
+#include "boost/mpl/vector.hpp"
 
-class IdealGeometryRecord : public edm::eventsetup::EventSetupRecordImplementation<IdealGeometryRecord> {};
+class IdealGeometryRecord : public edm::eventsetup::DependentRecordImplementation<
+  IdealGeometryRecord, boost::mpl::vector<GeometryFileRcd> > { };
 
 #endif /* RECORDS_IDEALGEOMETRYRECORD_H */
 
