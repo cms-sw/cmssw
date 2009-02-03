2c2de6959841c466e6d2c8d8917ed705f276e4f7
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-02-03 15:18:09 +0000

    new wrapper

--- CondFormats/Common/src/classes.h
+++ CondFormats/Common/src/classes.h
@@ -1,2 +1,14 @@
 #include "CondFormats/Common/interface/IOVSequence.h"
+#include "CondFormats/Common/interface/PayloadWrapper.h"
+
+
+namespace {
+  namespace {
+    struct Dummy {
+      cond::DataAndSummaryWrapper<int,int> dummy;
+    };
+
+  }
+
+}
 
