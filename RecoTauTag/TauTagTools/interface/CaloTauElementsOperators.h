3d445de3061e11e4fe03d141d7bc20b0949fa375
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2009-02-03 00:54:44 +0000

    Making head forward/backward compatible w/ CMSSW 223.  Removing possibility for link error

--- RecoTauTag/TauTagTools/interface/CaloTauElementsOperators.h
+++ RecoTauTag/TauTagTools/interface/CaloTauElementsOperators.h
@@ -24,11 +24,14 @@ class CaloTauElementsOperators : public TauElementsOperators {
   ~CaloTauElementsOperators(){} 
   
   // return all Ecal RecHits in a cone of metric* "coneMetric" and size "coneSize" around a direction "coneAxis" 
-  vector<pair<math::XYZPoint,float> > EcalRecHitsInCone(const math::XYZVector& coneAxis,const string coneMetric,const double coneSize,const double EcalRecHit_minEt)const;
   vector<pair<math::XYZPoint,float> > EcalRecHitsInCone(const math::XYZVector& coneAxis,const string coneMetric,const double coneSize,const double EcalRecHit_minEt,const vector<pair<math::XYZPoint,float> >& myEcalRecHits)const;
   // return all Ecal RecHits in an annulus defined by inner(metric* "innerconeMetric" and size "innerconeSize") and outer(metric* "outerconeMetric" and size "outerconeSize") cones around a direction "coneAxis" 
-  vector<pair<math::XYZPoint,float> > EcalRecHitsInAnnulus(const math::XYZVector& coneAxis,const string innerconeMetric,const double innerconeSize,const string outerconeMetric,const double outerconeSize,const double EcalRecHit_minEt)const; 
   vector<pair<math::XYZPoint,float> > EcalRecHitsInAnnulus(const math::XYZVector& coneAxis,const string innerconeMetric,const double innerconeSize,const string outerconeMetric,const double outerconeSize,const double EcalRecHit_minEt,const vector<pair<math::XYZPoint,float> >& myEcalRecHits)const;
+
+  // These function kept for out-of-box compatability with 2_2_X
+  vector<pair<math::XYZPoint,float> > EcalRecHitsInCone(const math::XYZVector& coneAxis,const string coneMetric,const double coneSize,const double EcalRecHit_minEt)const;
+  vector<pair<math::XYZPoint,float> > EcalRecHitsInAnnulus(const math::XYZVector& coneAxis,const string innerconeMetric,const double innerconeSize,const string outerconeMetric,const double outerconeSize,const double EcalRecHit_minEt)const; 
+
   // return all neutral Ecal BasicClusters in a cone of metric* "coneMetric" and size "coneSize" around a direction "coneAxis" 
   vector<pair<math::XYZPoint,float> > neutralEcalBasicClustersInCone(const math::XYZVector& coneAxis,const string coneMetric,const double coneSize,const double neutralEcalBasicCluster_minEt)const;
   // return all neutral Ecal BasicClusters in an annulus defined by inner(metric* "innerconeMetric" and size "innerconeSize") and outer(metric* "outerconeMetric" and size "outerconeSize") cones around a direction "coneAxis" 
