0742c5487132060f9856032345f3e799f21c38b7
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2009-02-03 16:59:52 +0000

    py

---
+++ CondCore/PopCon/test/print_eff_example.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.CondDBCommon.connect = 'sqlite_file:pop_test.db'
+
+process.eff = cms.ESSource("PoolDBESSource",
+    process.CondDBCommon,
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('ExEfficiencyRcd'),
+        tag = cms.string('Example_tag1')
+    ))
+)
+
+process.source = cms.Source("EmptyIOVSource",
+    firstValue = cms.uint64(310),
+    lastValue = cms.uint64(1000),
+    timetype = cms.string('runnumber'),
+    interval = cms.uint64(100)
+)
+
+process.prod = cms.EDAnalyzer("EfficiencyByLabelAnalyzer")
+
+process.p = cms.Path(process.prod)
+
