bfcde84565e12dbb466a8b73a3a408070329c6e1
Romain Rougny <sha1-c2318cc2eb27e6595f33ebf66f6ee24a41c8f790@cern.ch>
2009-02-04 16:07:23 +0000

    *** empty log message ***

--- CalibTracker/SiPixelGainCalibration/test/SealModules.cc
+++ CalibTracker/SiPixelGainCalibration/test/SealModules.cc
@@ -2,9 +2,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "CalibTracker/SiPixelGainCalibration/test/SimpleTestPrintOutPixelCalibAnalyzer.h"
-#include "CalibTracker/SiPixelGainCalibration/test/SiPixelGainCalibrationReadDQMFile.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(SimpleTestPrintOutPixelCalibAnalyzer);
-DEFINE_ANOTHER_FWK_MODULE(SiPixelGainCalibrationReadDQMFile);
