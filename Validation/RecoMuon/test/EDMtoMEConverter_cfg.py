13034cc7fbd27d19edc2a55b5e62b2c41cc7fdc7
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2009-02-04 21:23:04 +0000

    use standard RelVal postProcessor

--- Validation/RecoMuon/test/EDMtoMEConverter_cfg.py
+++ Validation/RecoMuon/test/EDMtoMEConverter_cfg.py
@@ -7,8 +7,9 @@ process.load("DQMServices.Components.EDMtoMEConverter_cff")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 #process.load("Configuration.StandardSequences.FakeConditions_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "STARTUP_V5::All"
-process.load("Validation.RecoMuon.PostProcessor_cff")
+process.GlobalTag.globaltag = "IDEAL_V11::All"
+process.load("Validation.Configuration.postValidation_cff")
+process.load("Validation.RecoMuon.PostProcessorHLT_cff")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -30,5 +31,6 @@ process.dqmSaver.forceRunNumber = cms.untracked.int32(1)
 process.dqmSaver.workflow = "/GlobalValidation/Test/RECO"
 
 process.p1 = cms.Path(process.EDMtoMEConverter*
-                      process.postProcessorMuonMultiTrack*process.postProcessorRecoMuon*
+                      process.postValidation*
+#                      process.recoMuonPostProcessorsHLT*
                       process.dqmSaver)
