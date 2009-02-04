89e8cfdef3625cde912ed0855a93ec8494a5907f
Tulika Bose <sha1-8b8c25cc70f9a5790e3c425b034aa99249523492@cern.ch>
2009-02-04 08:31:22 +0000

    Cleaning up

--- SimCalorimetry/HcalTrigPrimProducers/src/SealModule.cc
+++ SimCalorimetry/HcalTrigPrimProducers/src/SealModule.cc
@@ -1,11 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "SimCalorimetry/HcalTrigPrimProducers/src/HcalTrigPrimDigiProducer.h"
-#include "SimCalorimetry/HcalTrigPrimProducers/src/HcalDummyHitProducer.h"
-#include "SimCalorimetry/HcalTrigPrimProducers/src/TPGntupler.h"
+
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalTrigPrimDigiProducer);
-DEFINE_ANOTHER_FWK_MODULE(HcalDummyHitProducer);
-DEFINE_ANOTHER_FWK_MODULE(TPGntupler);
+
