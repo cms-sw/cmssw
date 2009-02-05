eeac940376d60b575c693cdf332a4d39d681fc39
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-02-05 23:43:01 +0000

    Event content transition with comments for 3_1_X.

--- GeneratorInterface/Configuration/python/GeneratorInterface_EventContent_cff.py
+++ GeneratorInterface/Configuration/python/GeneratorInterface_EventContent_cff.py
@@ -2,40 +2,64 @@ import FWCore.ParameterSet.Config as cms
 
 #RAW content 
 GeneratorInterfaceRAW = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep LHERunInfoProduct_*_*_*', 'keep LHEEventProduct_*_*_*',
-        'keep edmHepMCProduct_source_*_*', 
-        'keep edmGenInfoProduct_source_*_*', 
+    outputCommands = cms.untracked.vstring(
+        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
+        'keep LHEEventProduct_*_*_*',
+        'keep GenRunInfoProduct_generator_*_*', 
+        'keep GenEventInfoProduct_generator_*_*', 
+        'keep edmHepMCProduct_generator_*_*', 
         'keep *_genParticles_*_*', 
+
+# These products will go away!
+        'keep edmGenInfoProduct_source_*_*', 
         'keep *_genEventWeight_*_*', 
         'keep *_genEventScale_*_*', 
         'keep *_genEventPdfInfo_*_*', 
         'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*', 
-        'keep edmAlpgenInfoProduct_source_*_*')
-)
-#RECO content
-GeneratorInterfaceRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep LHERunInfoProduct_*_*_*', 'keep LHEEventProduct_*_*_*',
-        'keep *_genParticles_*_*', 
-        'keep *_genEventWeight_*_*', 
-        'keep *_genEventScale_*_*', 
-        'keep *_genEventPdfInfo_*_*', 
+        'keep *_genEventRunInfo_*_*',
+# and these too, because they will be run as producers/filters as "generator"
         'keep edmHepMCProduct_source_*_*', 
-        'keep edmGenInfoProduct_source_*_*', 
-        'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*', 
-        'keep edmAlpgenInfoProduct_source_*_*')
-)
-#AOD content
-GeneratorInterfaceAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep LHERunInfoProduct_*_*_*', 'keep LHEEventProduct_*_*_*',
-        'keep edmGenInfoProduct_source_*_*', 
-        'keep recoGenParticles_genParticles_*_*', 
-        'keep *_genEventWeight_*_*', 
-        'keep *_genEventScale_*_*', 
-        'keep *_genEventPdfInfo_*_*', 
-        'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*', 
-        'keep edmAlpgenInfoProduct_source_*_*')
+        'keep GenRunInfoProduct_source_*_*')
 )
 
+#RECO content
+GeneratorInterfaceRECO = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
+        'keep LHEEventProduct_*_*_*',
+        'keep GenRunInfoProduct_generator_*_*', 
+        'keep GenEventInfoProduct_generator_*_*', 
+        'keep edmHepMCProduct_generator_*_*', 
+        'keep *_genParticles_*_*', 
+
+# These products will go away!
+        'keep edmGenInfoProduct_source_*_*', 
+        'keep *_genEventWeight_*_*', 
+        'keep *_genEventScale_*_*', 
+        'keep *_genEventPdfInfo_*_*', 
+        'keep *_genEventProcID_*_*', 
+        'keep *_genEventRunInfo_*_*',
+# and these too, because they will be run as producers/filters as "generator"
+        'keep edmHepMCProduct_source_*_*', 
+        'keep GenRunInfoProduct_source_*_*')
+)
+
+#AOD content
+GeneratorInterfaceAOD = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
+        'keep LHEEventProduct_*_*_*',
+        'keep GenRunInfoProduct_generator_*_*', 
+        'keep GenEventInfoProduct_generator_*_*', 
+        'keep *_genParticles_*_*', 
+
+# These products will go away!
+        'keep edmGenInfoProduct_source_*_*', 
+        'keep *_genEventWeight_*_*', 
+        'keep *_genEventScale_*_*', 
+        'keep *_genEventPdfInfo_*_*', 
+        'keep *_genEventProcID_*_*', 
+        'keep *_genEventRunInfo_*_*',
+# and these too, because they will be run as producers/filters as "generator"
+        'keep GenRunInfoProduct_source_*_*')
+)
