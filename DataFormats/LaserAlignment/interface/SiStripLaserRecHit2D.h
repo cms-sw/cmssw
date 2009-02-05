095838e024e263d1273970dea369d6dc2f045f34
Jan Olzem <sha1-713996176e893ae17e542633137001ed8a2b989e@cern.ch>
2009-02-05 11:44:56 +0000

    * changed from DetId to SiStripDetId in class SiStripLaserRecHit2D (changed ctor and removed setter) * removed beamId setter and changed constructor of TkLasBeam * added some convenience functions in TkLasBeam for interpreting the beamId * TkLasBeam now contains a vector<SiStripLaserRecHit2D> instead of being derived from it (+ some conv. functions)

--- DataFormats/LaserAlignment/interface/SiStripLaserRecHit2D.h
+++ DataFormats/LaserAlignment/interface/SiStripLaserRecHit2D.h
@@ -4,7 +4,7 @@
 
 #include "DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h"
 #include "DataFormats/GeometrySurface/interface/LocalError.h"
-#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
 
 ///
 ///
@@ -15,20 +15,19 @@ public:
 
   SiStripLaserRecHit2D(): RecHit2DLocalPos(0) {}
   ~SiStripLaserRecHit2D() {}
-  SiStripLaserRecHit2D( const LocalPoint& p, const LocalError& e, const DetId& id ) : RecHit2DLocalPos( id ), position( p ), error( e ) {}
+  SiStripLaserRecHit2D( const LocalPoint& p, const LocalError& e, const SiStripDetId& id ) : RecHit2DLocalPos( id ), position( p ), error( e ) { detId = id; }
 
   virtual LocalPoint localPosition() const { return position; }
   virtual LocalError localPositionError() const { return error; }
-  virtual SiStripLaserRecHit2D * clone() const { return new SiStripLaserRecHit2D( * this); }
+  virtual SiStripLaserRecHit2D* clone() const { return new SiStripLaserRecHit2D( *this ); }
 
-  DetId getDetId() const { return detId; };
-  void setDetId( DetId aDetId ) { detId = aDetId; }
+  const SiStripDetId& getDetId( void ) const { return detId; }
 
  private:
   
   LocalPoint position;
   LocalError error;
-  DetId detId;
+  SiStripDetId detId;
 
 };
 
--- DataFormats/LaserAlignment/interface/TkLasBeam.h
+++ DataFormats/LaserAlignment/interface/TkLasBeam.h
@@ -3,21 +3,65 @@
 #define __TKLASBEAM_H
 
 #include <vector>
+#include <cmath>
+
 #include "DataFormats/LaserAlignment/interface/SiStripLaserRecHit2D.h"
 
 
-///
-///
-///
-class TkLasBeam : public std::vector<SiStripLaserRecHit2D> {
+
+/// \class TkLasBeam
+/// a collection of tracker laser hits (SiStripLaserRecHit2D) originating from a single laser beam.
+/// documentation in TkLasTrackBasedInterface TWiki
+class TkLasBeam {
 
 public:
-  TkLasBeam() { }
-  int getBeamId( void ) const { return beamId; }
-  void setBeamId( int aBeamId ) { beamId = aBeamId; }
+
+  typedef std::vector<SiStripLaserRecHit2D>::const_iterator const_iterator;
+
+
+  TkLasBeam() {}
+
+  TkLasBeam( unsigned int aBeamId ) { beamId = aBeamId; }
+  
+  /// return the full beam identifier
+  unsigned int getBeamId( void ) const { return beamId; }
+
+  /// access the collection of hits
+  const std::vector<SiStripLaserRecHit2D>& getData( void ) const { return data; }
+
+  /// access iterator to the collection of hits
+  std::vector<SiStripLaserRecHit2D>::const_iterator begin( void ) const { return data.begin(); }
+
+  /// access iterator to the collection of hits
+  std::vector<SiStripLaserRecHit2D>::const_iterator end( void ) const { return data.end(); }
+
+  /// insert a hit in the data vector
+  void push_back( const SiStripLaserRecHit2D& aHit ) { data.push_back( aHit ); }
+
+  /// returns the beam number (10^1 digit of beamId)
+  unsigned int getBeamNumber( void ) const { return beamId%100/10; }
+  
+  /// true if this is a TEC internal beam (from 10^2 digit of beamId). side parameter: -1 = ask if TEC-, 1 = TEC+, 0 = any tec, don't care
+  bool isTecInternal( int side = 0 ) const {
+    switch( side ) { 
+      case  0: return beamId%1000/100  < 2; 
+      case -1: return beamId%1000/100 == 1; 
+      case  1: return beamId%1000/100 == 0; 
+      default: throw cms::Exception( "[TkLasBeam::isTecInternal]" ) << " ** ERROR: side=" << side << " undefined." << std::endl; 
+    }
+  }
+ 
+  /// true if this is an AT beam (from 10^2 digit of beamId)
+  bool isAlignmentTube( void ) const { return ( beamId%1000/100 ) == 2; }
+
+  /// true if this beam hits TEC R6 (last digit of beamId)
+  bool isRing6( void ) const { return (beamId%10) == 1; }
+
 
 private:
-  int beamId;
+
+  unsigned int beamId;
+  std::vector<SiStripLaserRecHit2D> data;
 
 };
 
