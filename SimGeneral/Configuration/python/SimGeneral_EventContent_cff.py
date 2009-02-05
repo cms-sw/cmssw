2d3d8a92f27547e59f7929eff93fffb68753a1fd
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-02-05 14:52:30 +0000

    Add the CrossingFramePlaybackInfo keep statement

--- SimGeneral/Configuration/python/SimGeneral_EventContent_cff.py
+++ SimGeneral/Configuration/python/SimGeneral_EventContent_cff.py
@@ -4,11 +4,12 @@ import FWCore.ParameterSet.Config as cms
 SimGeneralFEVTDEBUG = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *_trackingtruthprod_*_*', 
         'drop *_electrontruth_*_*', 
-        'keep *_mergedtruth_MergedTrackTruth_*')
+        'keep *_mergedtruth_MergedTrackTruth_*',
+        'keep CrossingFramePlaybackInfo_*_*_*')
 )
 #RAW content
 SimGeneralRAW = cms.PSet(
-    outputCommands = cms.untracked.vstring()
+    outputCommands = cms.untracked.vstring('keep CrossingFramePlaybackInfo_*_*_*')
 )
 #RECO content
 SimGeneralRECO = cms.PSet(
