e8e483c0c57167e9811a57a52b95d47eb378e1bc
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2009-02-06 13:41:44 +0000

    add removed by error file

---
+++ SimDataFormats/CrossingFrame/src/classes.h
@@ -0,0 +1,37 @@
+#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
+#include "SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfo.h"
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#include <map>
+#include <string>
+
+namespace {
+  struct dictionary {
+ 	CrossingFrame<PSimHit> dummy1;
+
+ 	CrossingFramePlaybackInfo dummy0;
+ 	CrossingFrame<PCaloHit> dummy2;
+ 	CrossingFrame<SimTrack> dummy3;
+ 	CrossingFrame<SimVertex> dummy4;
+ 	CrossingFrame<edm::HepMCProduct> dummy5;
+
+        std::vector<int> dummy9;
+        std::vector<PSimHit> dummy10;
+        std::vector<PCaloHit> dummy11;
+        std::vector<SimTrack> dummy12;
+        std::vector<SimVertex> dummy13;
+        std::vector<edm::HepMCProduct> dummy14;
+
+        edm::Wrapper<CrossingFramePlaybackInfo > dummy19;
+        edm::Wrapper<CrossingFrame<PSimHit> > dummy20;
+        edm::Wrapper<CrossingFrame<PCaloHit> > dummy21;
+        edm::Wrapper<CrossingFrame<SimTrack> > dummy22;
+        edm::Wrapper<CrossingFrame<SimVertex> > dummy23;
+        edm::Wrapper<CrossingFrame<edm::HepMCProduct> > dummy24;
+  };
+}
