041f6f689170e3783ea93ef10c01a57b8b559ee6
dorbaker <sha1-c0c861e1621fbd0445e7a5339f107fcc8dbfe745@cern.ch>
2009-02-06 09:14:24 +0000

    Updated to include GSMatchedRecHits

--- FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation.h
+++ FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation.h
@@ -17,8 +17,8 @@
 //--- for SimHit
 #include "SimDataFormats/TrackingHit/interface/PSimHit.h"
 
+#include "DataFormats/TrackerRecHit2D/interface/SiTrackerGSMatchedRecHit2DCollection.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiTrackerGSRecHit2DCollection.h"
-#include "MagneticField/Engine/interface/MagneticField.h"
 
 #include "Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h"
 
@@ -36,63 +36,67 @@ class GSRecHitValidation : public edm::EDAnalyzer {
   
  public:
   explicit GSRecHitValidation(const edm::ParameterSet& conf);
-  
-  virtual ~GSRecHitValidation();
-  
+  virtual ~GSRecHitValidation();  
   virtual void analyze(const edm::Event& event, const edm::EventSetup& setup);
-  virtual   void beginJob( const edm::EventSetup& );
-  virtual  void endJob();
+  virtual void beginJob( const edm::EventSetup& );
+  virtual void endJob();
+
  private:
-  void makeHitsPlots(TString prefix, const SiTrackerGSRecHit2D * rechit, PSimHit * simHit, int numpartners);
-  
-  std::pair<LocalPoint,LocalVector> projectHit( const PSimHit& hit, const StripGeomDetUnit* stripDet,
-						const BoundPlane& plane, int thesign) ;
-  
+  void fillHitsPlots(TString prefix, const SiTrackerGSMatchedRecHit2D * rechit, PSimHit * simHit);
   edm::ParameterSet conf_;
   const TrackerGeometry * trackerG;
-  edm::ESHandle<MagneticField>          theMagField;
-  
+    
   // for the SimHits
   std::vector<std::string> trackerContainers;
   
   std::map<TString, TH1F*> hMap;
   
-  
+  //PXB 
   double PXB_Res_AxisLim ;   
-  double PXF_Res_AxisLim ;   
   double PXB_RecPos_AxisLim ;
-  double PXF_RecPos_AxisLim ;
   double PXB_SimPos_AxisLim ;
-  double PXF_SimPos_AxisLim ;
   double PXB_Err_AxisLim;
+  
+  //PXF
+  double PXF_Res_AxisLim ;   
+  double PXF_RecPos_AxisLim ;
+  double PXF_SimPos_AxisLim ;
   double PXF_Err_AxisLim;
+
+  //TIB
+  double TIB_Pos_x_AxisLim; 
+  double TIB_Pos_y_AxisLim; 
+  double TIB_Res_x_AxisLim; 
+  double TIB_Res_y_AxisLim; 
+  double TIB_Pull_x_AxisLim;
+  double TIB_Pull_y_AxisLim;
   
-  double TIB_Res_AxisLim ;
-  double TIB_Resy_AxisLim ;
-  double TIB_Pos_AxisLim ;
-  double TID_Res_AxisLim ;
-  double TID_Resy_AxisLim ;
-  double TID_Pos_AxisLim ;
-  double TOB_Res_AxisLim ;
-  double TOB_Resy_AxisLim ;
-  double TOB_Pos_AxisLim ;
-  double TEC_Res_AxisLim ;
-  double TEC_Resy_AxisLim ;
-  double TEC_Pos_AxisLim ;
-  int NumTracks_AxisLim;
+  //TOB
+  double TOB_Pos_x_AxisLim; 
+  double TOB_Pos_y_AxisLim; 
+  double TOB_Res_x_AxisLim; 
+  double TOB_Res_y_AxisLim; 
+  double TOB_Pull_x_AxisLim;
+  double TOB_Pull_y_AxisLim;
   
-  double TIB_Err_AxisLim;
-  double TID_Err_AxisLim;
-  double TOB_Err_AxisLim;
-  double TEC_Err_AxisLim;
-  double TIB_Erry_AxisLim;
-  double TID_Erry_AxisLim;
-  double TOB_Erry_AxisLim;
-  double TEC_Erry_AxisLim;
-  
+  //TID
+  double TID_Pos_x_AxisLim; 
+  double TID_Pos_y_AxisLim; 
+  double TID_Res_x_AxisLim; 
+  double TID_Res_y_AxisLim; 
+  double TID_Pull_x_AxisLim;
+  double TID_Pull_y_AxisLim;
+
+  //TEC
+  double TEC_Pos_x_AxisLim; 
+  double TEC_Pos_y_AxisLim; 
+  double TEC_Res_x_AxisLim; 
+  double TEC_Res_y_AxisLim; 
+  double TEC_Pull_x_AxisLim;
+  double TEC_Pull_y_AxisLim;
+
   int iEventCounter;
   TString outfilename;
-  std::string trackProducer;
 };
 
 #endif
--- FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
+++ FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
@@ -9,54 +9,20 @@ process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
 process.GlobalTag.globaltag = "IDEAL_30X::All"
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
+
+process.load("FastSimulation.TrackingRecHitProducer.GSRecHitValidation_cfi")
+
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(
-    'file:Fast_Sim_Input_File'
-    )
+                            'file:Your_File.root'
                             )
+
+
 
 process.load("Configuration.StandardSequences.MagneticField_40T_cff")
 #process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
-
-process.testanalyzer = cms.EDAnalyzer("GSRecHitValidation",
-    TID_Erry_AxisLim = cms.double(0.1),
-    TID_Err_AxisLim = cms.double(0.02),
-    TIB_Resy_AxisLim = cms.double(0.5),
-    PXB_SimPos_AxisLim = cms.double(1.0),
-    TOB_Erry_AxisLim = cms.double(0.1),
-    PXF_SimPos_AxisLim = cms.double(1.0),
-    TEC_Pos_AxisLim = cms.double(10.0),
-    TIB_Pos_AxisLim = cms.double(10.0),
-    TIB_Erry_AxisLim = cms.double(0.1),
-    TEC_Err_AxisLim = cms.double(0.02),
-    PXF_Err_AxisLim = cms.double(0.0),
-    TOB_Err_AxisLim = cms.double(0.01),
-    TIB_Res_AxisLim = cms.double(0.01),
-    outfilename = cms.string('Output_Filename.root'),
-    TOB_Res_AxisLim = cms.double(0.05),
-    TID_Resy_AxisLim = cms.double(0.5),
-    PXB_RecPos_AxisLim = cms.double(1.0),
-    PXB_Res_AxisLim = cms.double(0.01),
-    PXF_RecPos_AxisLim = cms.double(1.0),
-    PXB_Err_AxisLim = cms.double(0.0),
-    TEC_Res_AxisLim = cms.double(0.05),
-    TOB_Resy_AxisLim = cms.double(0.5),
-    TID_Pos_AxisLim = cms.double(10.0),
-    TID_Res_AxisLim = cms.double(0.05),
-    TEC_Resy_AxisLim = cms.double(0.5),
-    TrackProducer = cms.string('generalTracks'),
-    PXF_Res_AxisLim = cms.double(0.01),
-    #increase for y errors
-    TIB_Err_AxisLim = cms.double(0.01),
-    NumTracks_AxisLim = cms.int32(10),
-    TEC_Erry_AxisLim = cms.double(0.1),
-    TOB_Pos_AxisLim = cms.double(10.0),
-    SimHitList = cms.vstring('famosSimHitsTrackerHits')
-)
-
 
 process.p = cms.Path(process.testanalyzer)
 process.MessageLogger.destinations = ['detailedInfo.txt']
