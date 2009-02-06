1f47ed5afce455c7b35e13a0ac797adff7611c21
Dayong Wang <sha1-60ba7de852fc97a17e519d817810ab99f2289e6c@cern.ch>
2009-02-06 14:47:11 +0000

    protect against empty csctf key

--- L1TriggerConfig/CSCTFConfigProducers/src/CSCTFObjectKeysOnlineProd.cc
+++ L1TriggerConfig/CSCTFConfigProducers/src/CSCTFObjectKeysOnlineProd.cc
@@ -4,41 +4,34 @@
 void
 CSCTFObjectKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
 {
-      // kXXX = kCSCTF, kDTTF, kRPC, kGMT, kRCT, mkGCT, kGT, or kTSP0
-      // subsystemKey = TRIGGERSUP_CONF.{CSCTF_KEY, DTTF_KEY, RPC_KEY, GMT_KEY, RCT_KEY, GCT_KEY, GT_KEY}
-      std::string csctfKey = pL1TriggerKey->subsystemKey( L1TriggerKey::kCSCTF ) ;
+     std::string csctfKey = pL1TriggerKey->subsystemKey( L1TriggerKey::kCSCTF ) ;
 
-      // Execute SQL queries to get data from OMDS (using key) and make C++ object.
-      // Example: SELECT A_PARAMETER FROM CMS_XXX.XXX_CONF WHERE XXX_CONF.XXX_KEY = csctfKey
-      l1t::OMDSReader::QueryResults objectKeyResults =
-//       m_omdsReader.basicQuery(
-//          "A_PARAMETER",
-//          "CMS_XXX",
-//          "XXX_CONF",
-//          "XXX_CONF.XXX_KEY",
-//          m_omdsReader.singleAttribute( csctfKey  ) );
+     // Execute SQL queries to get data from OMDS (using key) and make C++ object.
 
-      m_omdsReader.basicQuery(
-         "SP1_KEY",
-         "CMS_CSC_TF",
-         "CSCTF_CONF",
-         "CSCTF_CONF.CSCTF_KEY",
-         m_omdsReader.singleAttribute( csctfKey  ) );
+     if( !csctfKey.empty() )
+     {
+	  l1t::OMDSReader::QueryResults objectKeyResults =
+	       m_omdsReader.basicQuery(
+		    "SP1_KEY",
+		    "CMS_CSC_TF",
+		    "CSCTF_CONF",
+		    "CSCTF_CONF.CSCTF_KEY",
+		    m_omdsReader.singleAttribute( csctfKey  ) );
 
-      if( objectKeyResults.queryFailed() || objectKeyResults.numberRows() != 1 ) // check if query was successful
-      {
-         edm::LogError( "L1-O2O" ) << "Problem with CSCTF  key." ;
-         return ;
-      }
+	  if( objectKeyResults.queryFailed() || objectKeyResults.numberRows() != 1 ) // check if query was successful
+	  {
+	       edm::LogError( "L1-O2O" ) << "Problem with CSCTF  key." ;
+	       return ;
+	  }
 
-      long long int objectKey ;
-      objectKeyResults.fillVariable( objectKey ) ;
-       std::stringstream ss;
-       ss<<objectKey;
-       std::string strKey=ss.str();
+	  long long int objectKey ;
+	  objectKeyResults.fillVariable( objectKey ) ;
+	  std::stringstream ss;
+	  ss<<objectKey;
+	  std::string strKey=ss.str();
 
 
-      pL1TriggerKey->add( "L1MuCSCTFConfigurationRcd", "L1MuCSCTFConfiguration", strKey ) ;
+	  pL1TriggerKey->add( "L1MuCSCTFConfigurationRcd", "L1MuCSCTFConfiguration", strKey ) ;
+     }
 }
-
 
