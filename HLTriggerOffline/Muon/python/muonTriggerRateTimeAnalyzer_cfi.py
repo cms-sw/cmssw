f4e7aebe1bb1545ccd85f37e6bd0ca5f7f69b51d
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2009-02-08 21:53:32 +0000

    Added option for AOD; Gen/Reco labels now turn on/off matching

--- HLTriggerOffline/Muon/python/muonTriggerRateTimeAnalyzer_cfi.py
+++ HLTriggerOffline/Muon/python/muonTriggerRateTimeAnalyzer_cfi.py
@@ -3,29 +3,34 @@ import FWCore.ParameterSet.Config as cms
 muonTriggerRateTimeAnalyzer = cms.EDAnalyzer("MuonTriggerRateTimeAnalyzer",
 
     HltProcessName = cms.string("HLT"),
-    
-    UseMuonFromGenerator = cms.bool(True),
-    UseMuonFromReco      = cms.bool(True),
-    GenLabel   = cms.untracked.string('genParticles'),
+
+    # To disable gen or reco matching, set to an empty string
+    GenLabel   = cms.untracked.string('genParticles'), 
     RecoLabel  = cms.untracked.string('globalMuons'),
+
+    # If the RAW trigger summary is unavailable, you can use the AOD instead
+    UseAod     = cms.untracked.bool(True),
     AodL1Label = cms.untracked.string('hltMuLevel1PathL1Filtered'),
     AodL2Label = cms.untracked.string('hltSingleMuLevel2NoIsoL2PreFiltered'),
-                                             
+
+    # Save variables to an ntuple for more in-depth trigger studies
     NtuplePath         = cms.untracked.string('HLT_IsoMu9'),
     NtupleFileName     = cms.untracked.string(''),
     RootFileName       = cms.untracked.string(''),
 
+    # Set the ranges and numbers of bins for histograms
     MaxPtParameters    = cms.vdouble(40,0.,40.),
     PtParameters       = cms.vdouble(50,0.,1000.),
     EtaParameters      = cms.vdouble(50,-2.1,2.1),
     PhiParameters      = cms.vdouble(50,-3.15,3.15),
 
+    # Set cuts placed on the generated muons and matching criteria
     MinPtCut           = cms.untracked.double(10.0),
     MaxEtaCut          = cms.untracked.double(2.1),
+    MotherParticleId   = cms.untracked.uint32(0),
     L1DrCut            = cms.untracked.double(0.4),
     L2DrCut            = cms.untracked.double(0.25),
     L3DrCut            = cms.untracked.double(0.015),
-    MotherParticleId   = cms.untracked.uint32(0),
     TriggerResultLabel = cms.InputTag("TriggerResults","","HLT"),
 
     DQMStore = cms.untracked.bool(True),
@@ -33,9 +38,9 @@ muonTriggerRateTimeAnalyzer = cms.EDAnalyzer("MuonTriggerRateTimeAnalyzer",
     NSigmas90 = cms.untracked.vdouble(3.0, 3.0, 3.0, 3.0),
 
     TriggerNames = cms.vstring(
-        "HLT_L1Mu",
-        "HLT_L1MuOpen",
-        "HLT_L2Mu9",
+        # "HLT_L1Mu",
+        # "HLT_L1MuOpen",
+        # "HLT_L2Mu9",
         "HLT_IsoMu9",
         "HLT_IsoMu11",
         "HLT_IsoMu13",
