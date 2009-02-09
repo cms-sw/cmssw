b37f9de56a79d46f406f7dbbd2ddce7f9182b447
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2009-02-09 15:55:58 +0000

    *** empty log message ***

--- SimGeneral/MixingModule/plugins/MixingWorker.cc
+++ SimGeneral/MixingModule/plugins/MixingWorker.cc
@@ -1,5 +1,6 @@
 #include "SimDataFormats/Track/interface/SimTrackContainer.h"
 #include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "MixingWorker.h"
 
@@ -8,110 +9,108 @@ template <> const int  edm::MixingWorker<PSimHit>::highTrackTof = 36;
 template <> const int  edm::MixingWorker<PSimHit>::limHighLowTof = 36; 
 
 namespace edm {
+  template <>
+  void MixingWorker<PSimHit>::addPileups(const int bcr, EventPrincipal *ep,unsigned int eventNr,int vertexoffset)
+  {
+    //    default version changed for high/low treatment
 
-template <>
-void MixingWorker<PSimHit>::addPileups(const int bcr, Event* e,unsigned int eventNr,int vertexoffset)
-{
-  // default version changed for high/low treatment
-  if (!isTracker_) {
-    Handle<std::vector<PSimHit> >  result_t;
-    bool got =e->getByLabel(tag_,result_t);
+    if (!isTracker_) {
+      boost::shared_ptr<Wrapper<std::vector<PSimHit> > const> shPtr = 
+	getProductByTag<std::vector<PSimHit> >(*ep, tag_);
+      if (shPtr) {
+	LogDebug("MixingModule") <<shPtr->product()->size()<<"  pileup objects  added, eventNr "<<eventNr;
+
+	crFrame_->addPileups(bcr,const_cast< std::vector<PSimHit> * > (shPtr->product()),eventNr);
+      }
+    } else {
+      boost::shared_ptr<Wrapper<std::vector<PSimHit> > const> shPtrHigh, shPtrLow;
+      if(trackerHigh_) {
+	shPtrHigh=getProductByTag<std::vector<PSimHit> >(*ep, tag_);
+	crFrame_->setPileupPtr(shPtrHigh);
+	shPtrLow=edm::getProductByTag<std::vector<PSimHit> >(*ep, opp_);
+      }
+      else {
+	shPtrHigh=getProductByTag<std::vector<PSimHit> >(*ep, opp_);
+	shPtrLow=getProductByTag<std::vector<PSimHit> >(*ep, tag_);
+	crFrame_->setPileupPtr(shPtrLow);
+      }
+
+      // add HighTof simhits to high and low signals
+      float tof = bcr*crFrame_->getBunchSpace();
+      if (shPtrHigh) {
+	if ( !checktof_ || ((limHighLowTof +tof ) <= highTrackTof)) { 
+	  crFrame_->addPileups(bcr,const_cast< std::vector<PSimHit> * > (shPtrHigh->product()),eventNr,0,checktof_,trackerHigh_);
+	  LogDebug("MixingModule") <<"For bcr "<<bcr<<", "<<subdet_<<", evNr "<<eventNr<<", "<<shPtrHigh->product()->size()<<" Hits added from high";
+	}
+      }
+
+      // add LowTof simhits to high and low signals
+      if (shPtrLow) {
+	if (  !checktof_ || ((tof+limHighLowTof) >= lowTrackTof && tof <= highTrackTof)) {
+	  crFrame_->addPileups(bcr,const_cast< std::vector<PSimHit> * > (shPtrLow->product()),eventNr,0,checktof_,trackerHigh_);
+	  LogDebug("MixingModule") <<"For bcr "<<bcr<<", "<<subdet_<<", evNr "<<eventNr<<", "<<shPtrLow->product()->size()<<" Hits added from low";
+	}
+      }
+    }
+  }
+
+  template <>
+  void  MixingWorker<SimTrack>::addPileups(const int bcr, EventPrincipal *ep,unsigned int eventNr,int vertexoffset)
+  {
+    // default version changed to transmit vertexoffset
+    boost::shared_ptr<Wrapper<std::vector<SimTrack> > const> shPtr =
+      getProductByTag<std::vector<SimTrack> >(*ep, tag_);
+    if (shPtr) {
+      LogDebug("MixingModule") <<shPtr->product()->size()<<"  pileup objects  added, eventNr "<<eventNr;
+      crFrame_->setPileupPtr(shPtr);
+      crFrame_->addPileups(bcr,const_cast< std::vector<SimTrack> * > (shPtr->product()),eventNr,vertexoffset);
+    }
+  }
+
+  template <>
+  //  void MixingWorker<SimVertex>::addPileups(const int bcr, Event* e,unsigned int eventNr,int vertexoffset)
+  void MixingWorker<SimVertex>::addPileups(const int bcr, EventPrincipal * ep,unsigned int eventNr,int vertexoffset)
+  {
+    // default version changed to take care of vertexoffset
+    boost::shared_ptr<Wrapper<std::vector<SimVertex> > const> shPtr = 
+      getProductByTag<std::vector<SimVertex> >(*ep, tag_);
+    if (shPtr) {
+      LogDebug("MixingModule") <<shPtr->product()->size()<<"  pileup objects  added, eventNr "<<eventNr;
+      vertexoffset+=shPtr->product()->size();
+      crFrame_->setPileupPtr(shPtr);
+      crFrame_->addPileups(bcr,const_cast< std::vector<SimVertex> * > (shPtr->product()),eventNr);
+    }
+  }
+
+  template <>
+  void MixingWorker<HepMCProduct>::addPileups(const int bcr, EventPrincipal *ep,unsigned int eventNr,int vertexoffset)
+  {
+    // HepMCProduct does not come as a vector....
+    boost::shared_ptr<Wrapper<HepMCProduct> const> shPtr = 
+      getProductByTag<HepMCProduct>(*ep, tag_);
+       if (shPtr) {
+           LogDebug("MixingModule") <<"HepMC pileup objects  added, eventNr "<<eventNr;
+	   crFrame_->setPileupPtr(shPtr);
+           crFrame_->addPileups(bcr,const_cast<HepMCProduct*> (shPtr->product()),eventNr);
+       }
+  }
+
+  template <>
+  void MixingWorker<HepMCProduct>::addSignals(const Event &e)
+  {
+    //HepMC - here the interface is different!!!
+    Handle<HepMCProduct>  result_t;
+    bool got = e.getByLabel(tag_,result_t);
     if (got) {
-      LogDebug("MixingModule") <<"For "<<subdet_<<", "<<result_t.product()->size()<<"  pileup objects  added, eventNr "<<eventNr;
-      crFrame_->addPileups(bcr,result_t.product(),eventNr,vertexoffset);
+      LogDebug("MixingModule") <<" adding HepMCProduct from signal event  with "<<tag_;
+      crFrame_->addSignals(result_t.product(),e.id());  
     }
-  }else {
-    // Tracker treatment
-    // do not read branches if clearly outside of tof bounds (and verification is asked for, default)
-    // we have to add hits from this subdetector + opposite one
-
-    Handle<std::vector<PSimHit> >  simhitshigh,simhitslow;
-    bool gotHigh,gotLow;
-
-    if(trackerHigh_) {
-      gotHigh=e->getByLabel(tag_,simhitshigh);
-      gotLow=e->getByLabel(opp_,simhitslow);
-    }
-    else {
-      gotHigh=e->getByLabel(opp_,simhitshigh);
-      gotLow=e->getByLabel(tag_,simhitslow);
-    }
-
-    // add HighTof simhits to high and low signals
-    float tof = bcr*crFrame_->getBunchSpace();
-    if (gotHigh) {
-      if ( !checktof_ || ((limHighLowTof +tof ) <= highTrackTof)) { 
-	crFrame_->addPileups(bcr,simhitshigh.product(),eventNr,0,checktof_,trackerHigh_);
-	LogDebug("MixingModule") <<"For bcr "<<bcr<<", "<<subdet_<<", evNr "<<eventNr<<", "<<simhitshigh->size()<<" Hits added from high";
-      }
-    }
-
-    // add LowTof simhits to high and low signals
-    if (gotLow) {
-      if (  !checktof_ || ((tof+limHighLowTof) >= lowTrackTof && tof <= highTrackTof)) {
-	crFrame_->addPileups(bcr,simhitslow.product(),eventNr,0,checktof_,trackerHigh_);
-	LogDebug("MixingModule") <<"For bcr "<<bcr<<", "<<subdet_<<", evNr "<<eventNr<<", "<<simhitslow->size()<<" Hits added from low";
-      }
-    }
+    else	  LogWarning("MixingModule") <<"!!!!!!! Did not get any signal data for HepMCProduct with "<<tag_;
   }
 
-}
-
-
-template <>
-void MixingWorker<SimTrack>::addPileups(const int bcr, Event* e,unsigned int eventNr,int vertexoffset)
-{
-  // default version changed to transmit vertexoffset
-  Handle<std::vector<SimTrack> >  result_t;
-  bool got =   e->getByLabel(tag_,result_t);
-  if (got) {
-    LogDebug("MixingModule") <<result_t.product()->size()<<"  pileup tracks  added, eventNr "<<eventNr;
-    crFrame_->addPileups(bcr,result_t.product(),eventNr,vertexoffset);
-  }
-}
-
-template <>
-void MixingWorker<SimVertex>::addPileups(const int bcr, Event* e,unsigned int eventNr,int vertexoffset)
-{
-  // default version changed to take care of vertexoffset
-  Handle<std::vector<SimVertex> >  result;
-  bool got = e->getByLabel(tag_,result);
-  if (got) {
-    LogDebug("MixingModule") <<result.product()->size()<<"  pileup vertices  added, eventNr "<<eventNr;
-    crFrame_->addPileups(bcr,result.product(),eventNr);
-    vertexoffset+=result.product()->size();
+  template <>
+  void MixingWorker<PSimHit>::setTof(){
+    crFrame_->setTof();
   }
 
-}
-
-
-template <>
-void MixingWorker<HepMCProduct>::addPileups(const int bcr, Event* e,unsigned int eventNr,int vertexoffset)
-{
-  //HepMC - we are creating a dummy vector, to have the same interfaces as for the other objects
-  Handle<HepMCProduct>  result_mc;
-  bool got = e->getByLabel(tag_,result_mc);
-  if (got){
-    LogDebug("MixingModule") <<"  pileup HepMCProduct added";
-    std::vector<HepMCProduct> vec;
-    vec.push_back(*(result_mc.product()));
-    crFrame_->addPileups(bcr,&vec,eventNr);
-  }
-}
-
-template <>
-void MixingWorker<HepMCProduct>::addSignals(const Event &e){
-  // version for HepMCProduct is specific since it doesnt come as a vector
-  // we have to read it differently, and to store it in a (fake) vector
-  Handle<HepMCProduct>  result_t;
-  bool got = e.getByLabel(tag_,result_t);
-  if (got) {
-    LogDebug("MixingModule") <<" adding HepMCProduct from signal event  with "<<tag_;
-    std::vector<HepMCProduct> vec;
-    vec.push_back(*(result_t.product()));
-    crFrame_->addSignals(&vec,e.id());
-  }
-  else	  LogWarning("MixingModule") <<"!!!!!!! Did not get any signal data for HepMCProduct with "<<tag_;
-}
-
-}
+}//namespace edm
