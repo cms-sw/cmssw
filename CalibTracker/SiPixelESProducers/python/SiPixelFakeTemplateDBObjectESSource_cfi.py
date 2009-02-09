9cd5aedea125d4264849fd82a9235eb481862794
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-02-09 11:59:47 +0000

    Moved template calibration files to this package from RecoLocalTracker/SiPixelRecHits

--- CalibTracker/SiPixelESProducers/python/SiPixelFakeTemplateDBObjectESSource_cfi.py
+++ CalibTracker/SiPixelESProducers/python/SiPixelFakeTemplateDBObjectESSource_cfi.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 SiPixelFakeTemplateDBObjectESSource = cms.ESSource("SiPixelFakeTemplateDBObjectESSource",
     siPixelTemplateCalibrations = cms.vstring(
-    "RecoLocalTracker/SiPixelRecHits/data/template_summary_zp0001.out",
-    "RecoLocalTracker/SiPixelRecHits/data/template_summary_zp0004.out",
-    "RecoLocalTracker/SiPixelRecHits/data/template_summary_zp0010.out",
-    "RecoLocalTracker/SiPixelRecHits/data/template_summary_zp0012.out"),
-    Version = cms.double(1.2)
+    "CalibTracker/SiPixelESProducers/data/template_summary_zp0001.out",
+    "CalibTracker/SiPixelESProducers/data/template_summary_zp0004.out",
+    "CalibTracker/SiPixelESProducers/data/template_summary_zp0011.out",
+    "CalibTracker/SiPixelESProducers/data/template_summary_zp0012.out",
+    Version = cms.double(1.3)
 )
 
 
