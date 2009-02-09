4a94150fdf7dd7567924bd5a67bc12594b8978d5
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-02-09 10:52:23 +0000

    added unit tests

---
+++ RecoParticleFlow/PFClusterProducer/test/clustering_cfg.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+# runs only clustering.
+# PFRecHits should be in the input file
+
+process = cms.Process("PFC")
+
+process.source = cms.Source("PoolSource", 
+                            fileNames = cms.untracked.vstring("file:pfrechits.root") )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(2000)
+)
+
+process.load("RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff")
+
+process.particleFlowClusterECAL.verbose = True
+
+process.p1 = cms.Path(
+    process.particleFlowClusterECAL+
+    process.particleFlowClusterHCAL+
+    process.particleFlowClusterPS 
+    )
+
+
+
+process.load("Configuration.EventContent.EventContent_cff")
+process.reco = cms.OutputModule("PoolOutputModule",
+    process.RECOSIMEventContent,
+    fileName = cms.untracked.string('clustering_reco.root')
+)
+
+process.outpath = cms.EndPath( process.reco )
+
+#
