3f48624febbdc4e46d84a7d0506041be2d0af511
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2009-02-09 15:03:11 +0000

    tag V00-04-05: add python/PrescaleService_cfi.py

---
+++ FWCore/PrescaleService/python/PrescaleService_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+PrescaleService = cms.Service(
+    "PrescaleService",
+    prescaleTable = cms.VPSet(
+      cms.PSet(
+        pathName  = cms.string("HLTPath"),
+        prescales = cms.vuint32(1)
+        )
+      ),
+    lvl1Labels = cms.vstring('default'),
+    lvl1DefaultLabel = cms.untracked.string('default')
+    )
--- FWCore/PrescaleService/test/testPrescaleService_cfg.py
+++ FWCore/PrescaleService/test/testPrescaleService_cfg.py
@@ -2,45 +2,51 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TEST")
 
+
+# instantiate & configure message logger service
 process.MessageLogger = cms.Service("MessageLogger",
-    cout = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO')
-    ),
+    cout = cms.untracked.PSet( threshold = cms.untracked.string('INFO') ),
     destinations = cms.untracked.vstring('cout')
 )
 
-process.PrescaleService = cms.Service("PrescaleService",
-    prescaleTable = cms.VPSet(
-        cms.PSet(
-            pathName = cms.string('HLT2'),
-            prescales = cms.vuint32(2, 5, 10)
-        ), 
-        cms.PSet(
-            pathName = cms.string('HLT3'),
-            prescales = cms.vuint32(5, 10, 20)
-        ), 
-        cms.PSet(
-            pathName = cms.string('HLT4'),
-            prescales = cms.vuint32(10, 20, 0)
-        )),
-    lvl1Labels = cms.vstring('10E30', 
-        '10E31', 
-        '10E32'),
-    lvl1DefaultLabel = cms.untracked.string('10E31')
+
+# define prescale table: three rows (paths), three columns (scenarios)
+prescaleTable = cms.VPSet(
+    cms.PSet(
+      pathName = cms.string('HLT2'),
+      prescales = cms.vuint32(2, 5, 10)
+    ), 
+    cms.PSet(
+      pathName = cms.string('HLT3'),
+      prescales = cms.vuint32(5, 10, 20)
+    ), 
+    cms.PSet(
+      pathName = cms.string('HLT4'),
+      prescales = cms.vuint32(10, 20, 0)
+    )
 )
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
-)
 
+# instantiate prescale service and configure with above defined table
+process.load("FWCore.PrescaleService.PrescaleService_cfi")
+process.PrescaleService.prescaleTable = prescaleTable
+process.PrescaleService.lvl1Labels = cms.vstring('10E30','10E31','10E32')
+process.PrescaleService.lvl1DefaultLabel = cms.untracked.string('10E31')
+
+
+# empty source for testing
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
 process.source = cms.Source("EmptySource")
 
 
+# define modules
 process.psHLT1 = cms.EDFilter("HLTPrescaler")
 process.psHLT2 = cms.EDFilter("HLTPrescaler")
 process.psHLT3 = cms.EDFilter("HLTPrescaler")
 process.psHLT4 = cms.EDFilter("HLTPrescaler")
 
+
+# define paths
 process.HLT1 = cms.Path(process.psHLT1)
 process.HLT2 = cms.Path(process.psHLT2)
 process.HLT3 = cms.Path(process.psHLT3)
