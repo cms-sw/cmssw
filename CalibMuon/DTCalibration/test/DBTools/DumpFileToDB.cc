598c4a3e45c373b286f40e3e5409c31987a052c8
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-02-10 13:42:52 +0000

    Fix the field # of t0, noisy and dead cells

--- CalibMuon/DTCalibration/test/DBTools/DumpFileToDB.cc
+++ CalibMuon/DTCalibration/test/DBTools/DumpFileToDB.cc
@@ -2,8 +2,8 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2008/12/09 22:44:11 $
- *  $Revision: 1.12 $
+ *  $Date: 2009/01/20 11:01:46 $
+ *  $Revision: 1.13 $
  *  \author G. Cerminara - INFN Torino
  */
 
@@ -103,8 +103,8 @@ void DumpFileToDB::endJob() {
     for(DTCalibrationMap::const_iterator keyAndCalibs = theCalibFile->keyAndConsts_begin();
 	keyAndCalibs != theCalibFile->keyAndConsts_end();
 	++keyAndCalibs) {
-      float t0mean = (*keyAndCalibs).second[5];
-      float t0rms = (*keyAndCalibs).second[6];
+      float t0mean = (*keyAndCalibs).second[11];
+      float t0rms = (*keyAndCalibs).second[12];
       cout << "key: " << (*keyAndCalibs).first
 	   << " T0 mean (TDC counts): " << t0mean
 	   << " T0_rms (TDC counts): " << t0rms << endl;
@@ -126,9 +126,9 @@ void DumpFileToDB::endJob() {
 	keyAndCalibs != theCalibFile->keyAndConsts_end();
 	++keyAndCalibs) {
       cout << "key: " << (*keyAndCalibs).first
-	   << " Noisy flag: " << (*keyAndCalibs).second[7] << endl;
+	   << " Noisy flag: " << (*keyAndCalibs).second[13] << endl;
       statusMap->setCellNoise((*keyAndCalibs).first,
-			      (*keyAndCalibs).second[7]);
+			      (*keyAndCalibs).second[13]);
     }
 
     cout << "[DumpFileToDB]Writing Noise Map object to DB!" << endl;
@@ -143,9 +143,9 @@ void DumpFileToDB::endJob() {
 	keyAndCalibs != theCalibFile->keyAndConsts_end();
 	++keyAndCalibs) {
       cout << "key: " << (*keyAndCalibs).first
-	   << " dead flag: " << (*keyAndCalibs).second[7] << endl;
+	   << " dead flag: " << (*keyAndCalibs).second[13] << endl;
       deadMap->setCellDead_TP((*keyAndCalibs).first,
-			      (*keyAndCalibs).second[7]);
+			      (*keyAndCalibs).second[13]);
     }
 
     cout << "[DumpFileToDB]Writing Noise Map object to DB!" << endl;
