c37b9af5f2ee22264547125484396d89d82fdba2
Nicola De Filippis <sha1-50b8e21d12ea3a05a38a84ea0000270cd7663689@cern.ch>
2009-02-11 18:21:02 +0000

    *** empty log message ***

--- HiggsAnalysis/Skimming/src/HiggsToZZ4LeptonsHLTAnalysis.cc
+++ HiggsAnalysis/Skimming/src/HiggsToZZ4LeptonsHLTAnalysis.cc
@@ -75,6 +75,13 @@ HiggsToZZ4LeptonsHLTAnalysis::HiggsToZZ4LeptonsHLTAnalysis(const edm::ParameterS
 
 HiggsToZZ4LeptonsHLTAnalysis::~HiggsToZZ4LeptonsHLTAnalysis()
 {
+
+  std::cout << "HiggsToZZ4LeptonsHLTAnalysis: \n"
+  << " N_eventsInput_read= " << nEvents
+  << " N_eventsHLT_kept= " << npassed
+  << "     EfficiencyHLT= " << double(npassed)/double(nEvents) << std::endl;
+
+
 }
 
 //
@@ -85,118 +92,119 @@ HiggsToZZ4LeptonsHLTAnalysis::~HiggsToZZ4LeptonsHLTAnalysis()
 void HiggsToZZ4LeptonsHLTAnalysis::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
+  nEvents++;
   
-   const string invalid("@@invalid@@");
-
-   // get hold of TriggerResults Object
-   Handle<TriggerResults> trh;
-   try {iEvent.getByLabel(inputTag_,trh);} catch(...) {;}
-   if (trh.isValid()) {
-     cout << "TriggerResults found, number of HLT paths: " << trh->size() << endl;
-     
-     // get hold of trigger names - based on TriggerResults object!
-     triggerNames_.init(*trh);
-     if (firstevent_) {
-       for (unsigned int i=0; i<triggerNames_.size(); i++) {
-	 cout << "Found the trigger path= " << triggerNames_.triggerName(i) << endl;
-       }
-     }
-
-     unsigned int n(n_);
-     for (unsigned int i=0; i!=n; i++) {
-       HLTPathsByIndex_[i]=triggerNames_.triggerIndex(HLTPathsByName_[i]);
-     }
-     
-     // for empty input vectors (n==0), default to all HLT trigger paths!
-     if (n==0) {
-       n=trh->size();
-       HLTPathsByName_.resize(n);
-       HLTPathsByIndex_.resize(n);
-       for (unsigned int i=0; i!=n; i++) {
-	 HLTPathsByName_[i]=triggerNames_.triggerName(i);
-	 HLTPathsByIndex_[i]=i;
-       }
-     }
-     
-     // report on what is finally used
-     if (firstevent_){ 
-       cout << "HLT trigger paths: " + inputTag_.encode()
-	    << "\n - Number requested: " << n
-	    << "\n - andOr mode: " << andOr_ << endl; 
-       if (n>0) {
-	 cout << "  HLT trigger paths requested: index, name and valididty:" << endl;
-	 for (unsigned int i=0; i!=n; i++) {
-	   bool validity ( (HLTPathsByIndex_[i]<trh->size()) && (HLTPathsByName_[i]!=invalid) );
-	   
-	   cout << " " << HLTPathsByIndex_[i]
-		<< " " << HLTPathsByName_[i]
-		<< " " << validity << endl;
-	 }
-       }
-     }
-     
-     // count number of requested HLT paths which have fired
-     unsigned int fired(0);
-     for (unsigned int i=0; i!=n; i++) {
-       boolflag[i]=false;
-       if (HLTPathsByIndex_[i]<trh->size()) {
-	 if (trh->accept(HLTPathsByIndex_[i])) {
-	   fired++;
-	   cout << "Fired HLT path= " << HLTPathsByName_[i] << endl;
-	   ntrig[i]++;
-	   boolflag[i]=true;
-	 }
-       }
-     }
-     
-
-     // Boolean filter result
-     const bool accept( ((!andOr_) && (fired==n)) ||
-			(( andOr_) && (fired!=0)) );
-     cout << "Accept = " << accept << endl;
-
-     // Try to study events skipped at trigger level
-     if (!accept){
-       cout << "Perform sanity check analysis for trigger" << endl;
-
-       // Get the muons 
-       edm::Handle<edm::View<reco::Muon> >mus;
-       iEvent.getByLabel(muonlabel_.label(), mus);
-       for ( edm::View<reco::Muon>::const_iterator muons = mus->begin(); muons != mus->end(); ++muons ) {
-	 cout << "Not triggered event in Run=" << iEvent.id().run() << " and Event=" << iEvent.id().event()
-	      << " Muon from not-triggered event with pt= " << muons->pt() 
-	      << " and eta= " << muons->eta()
-	      << endl;
-       }
-
-       // Get the electrons
-       edm::Handle<edm::View<reco::GsfElectron> >pTracks;
-       iEvent.getByLabel(electronlabel_.label(),pTracks);
-       const edm::View<reco::GsfElectron>* eTracks = pTracks.product();       
-       for ( edm::View<reco::GsfElectron>::const_iterator electrons = eTracks->begin(); electrons != eTracks->end(); ++electrons ) {
-	 cout << "Not triggered event in Run=" << iEvent.id().run() << " and Event=" << iEvent.id().event()
-	      << " Electron from not-triggered event with pt= " << electrons->pt() 
-	      << " and eta= " << electrons->eta()
-	      << endl; 
-       }
-
-     }
-     else {
-       npassed++;
-     }
-     firstevent_=false;
-
-     // store the flags
-     for  (unsigned int j = 0; j < n_; ++ j ){
-       auto_ptr<bool> flag ( new bool );
-       *flag=boolflag[j];
-       iEvent.put(flag,valias.at(j));
-     }
-     auto_ptr<bool> flagaccept ( new bool );
-     *flagaccept=accept;
-     iEvent.put(flagaccept,aliasaccept);
-   }
-   
+  const string invalid("@@invalid@@");
+  
+  // get hold of TriggerResults Object
+  Handle<TriggerResults> trh;
+  try {iEvent.getByLabel(inputTag_,trh);} catch(...) {;}
+  if (trh.isValid()) {
+    cout << "TriggerResults found, number of HLT paths: " << trh->size() << endl;
+    
+    // get hold of trigger names - based on TriggerResults object!
+    triggerNames_.init(*trh);
+    if (firstevent_) {
+      for (unsigned int i=0; i<triggerNames_.size(); i++) {
+	cout << "Found the trigger path= " << triggerNames_.triggerName(i) << endl;
+      }
+    }
+    
+    unsigned int n(n_);
+    for (unsigned int i=0; i!=n; i++) {
+      HLTPathsByIndex_[i]=triggerNames_.triggerIndex(HLTPathsByName_[i]);
+    }
+    
+    // for empty input vectors (n==0), default to all HLT trigger paths!
+    if (n==0) {
+      n=trh->size();
+      HLTPathsByName_.resize(n);
+      HLTPathsByIndex_.resize(n);
+      for (unsigned int i=0; i!=n; i++) {
+	HLTPathsByName_[i]=triggerNames_.triggerName(i);
+	HLTPathsByIndex_[i]=i;
+      }
+    }
+    
+    // report on what is finally used
+    if (firstevent_){ 
+      cout << "HLT trigger paths: " + inputTag_.encode()
+	   << "\n - Number requested: " << n
+	   << "\n - andOr mode: " << andOr_ << endl; 
+      if (n>0) {
+	cout << "  HLT trigger paths requested: index, name and valididty:" << endl;
+	for (unsigned int i=0; i!=n; i++) {
+	  bool validity ( (HLTPathsByIndex_[i]<trh->size()) && (HLTPathsByName_[i]!=invalid) );
+	  
+	  cout << " " << HLTPathsByIndex_[i]
+	       << " " << HLTPathsByName_[i]
+	       << " " << validity << endl;
+	}
+      }
+    }
+    
+    // count number of requested HLT paths which have fired
+    unsigned int fired(0);
+    for (unsigned int i=0; i!=n; i++) {
+      boolflag[i]=false;
+      if (HLTPathsByIndex_[i]<trh->size()) {
+	if (trh->accept(HLTPathsByIndex_[i])) {
+	  fired++;
+	  cout << "Fired HLT path= " << HLTPathsByName_[i] << endl;
+	  ntrig[i]++;
+	  boolflag[i]=true;
+	}
+      }
+    }
+    
+    
+    // Boolean filter result
+    const bool accept( ((!andOr_) && (fired==n)) ||
+		       (( andOr_) && (fired!=0)) );
+    cout << "Accept = " << accept << endl;
+    
+    // Try to study events skipped at trigger level
+    if (!accept){
+      cout << "Perform sanity check analysis for trigger" << endl;
+      
+      // Get the muons 
+      edm::Handle<edm::View<reco::Muon> >mus;
+      iEvent.getByLabel(muonlabel_.label(), mus);
+      for ( edm::View<reco::Muon>::const_iterator muons = mus->begin(); muons != mus->end(); ++muons ) {
+	cout << "Not triggered event in Run=" << iEvent.id().run() << " and Event=" << iEvent.id().event()
+	     << " Muon from not-triggered event with pt= " << muons->pt() 
+	     << " and eta= " << muons->eta()
+	     << endl;
+      }
+      
+      // Get the electrons
+      edm::Handle<edm::View<reco::GsfElectron> >pTracks;
+      iEvent.getByLabel(electronlabel_.label(),pTracks);
+      const edm::View<reco::GsfElectron>* eTracks = pTracks.product();       
+      for ( edm::View<reco::GsfElectron>::const_iterator electrons = eTracks->begin(); electrons != eTracks->end(); ++electrons ) {
+	cout << "Not triggered event in Run=" << iEvent.id().run() << " and Event=" << iEvent.id().event()
+	     << " Electron from not-triggered event with pt= " << electrons->pt() 
+	     << " and eta= " << electrons->eta()
+	     << endl; 
+      }
+      
+    }
+    else {
+      npassed++;
+    }
+    firstevent_=false;
+    
+    // store the flags
+    for  (unsigned int j = 0; j < n_; ++ j ){
+      auto_ptr<bool> flag ( new bool );
+      *flag=boolflag[j];
+      iEvent.put(flag,valias.at(j));
+    }
+    auto_ptr<bool> flagaccept ( new bool );
+    *flagaccept=accept;
+    iEvent.put(flagaccept,aliasaccept);
+  }
+  
 }
 
 void HiggsToZZ4LeptonsHLTAnalysis::endJob() {
