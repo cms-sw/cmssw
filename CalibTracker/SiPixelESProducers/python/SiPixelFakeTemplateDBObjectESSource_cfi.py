9a0f4caf6036401dc5c825d425eb0afc8c062dce
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-02-11 00:19:07 +0000

    Added a missing ')'

--- CalibTracker/SiPixelESProducers/python/SiPixelFakeTemplateDBObjectESSource_cfi.py
+++ CalibTracker/SiPixelESProducers/python/SiPixelFakeTemplateDBObjectESSource_cfi.py
@@ -5,7 +5,7 @@ SiPixelFakeTemplateDBObjectESSource = cms.ESSource("SiPixelFakeTemplateDBObjectE
     "CalibTracker/SiPixelESProducers/data/template_summary_zp0001.out",
     "CalibTracker/SiPixelESProducers/data/template_summary_zp0004.out",
     "CalibTracker/SiPixelESProducers/data/template_summary_zp0011.out",
-    "CalibTracker/SiPixelESProducers/data/template_summary_zp0012.out",
+    "CalibTracker/SiPixelESProducers/data/template_summary_zp0012.out"),
     Version = cms.double(1.3)
 )
 
