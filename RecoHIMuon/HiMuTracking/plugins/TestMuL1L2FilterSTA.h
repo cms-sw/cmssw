0825eb8fa64c16bc27beb2b8b6ecc9379e3b18d4
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2009-02-11 15:42:33 +0000

    *** empty log message ***

--- RecoHIMuon/HiMuTracking/plugins/TestMuL1L2FilterSTA.h
+++ RecoHIMuon/HiMuTracking/plugins/TestMuL1L2FilterSTA.h
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 //
-// Package:    TestMuL1L2STA.h
-// Class:      TestMuL1L2STA
+// Package:    TestMuL1L2.h
+// Class:      TestMuL1L2
 /*/
 
  Description: <one line class summary>
@@ -12,12 +12,12 @@
 //
 // Original Author:  Dong Ho Moon
 //         Created:  Wed May  9 06:22:36 CEST 2007
-// $Id: TestMuL1L2Filter.h,v 1.1 2008/08/04 15:35:39 kodolova Exp $
+// $Id: TestMuL1L2Filter.h,v 1.3 2009/02/11 15:36:59 kodolova Exp $
 //
 //
 
-#ifndef TESTMU_L1L2_FILTER_STA_H
-#define TESTMU_L1L2_FILTER_STA_H
+#ifndef TESTMU_L1L2_FILTER_H
+#define TESTMU_L1L2_FILTER_H
 
 
 // system include files
@@ -26,61 +26,44 @@
 
 // user include files
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDFilter.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
-#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+// HI reco
+
 #include "RecoHIMuon/HiMuSeed/interface/HICConst.h"
 #include "RecoHIMuon/HiMuPropagator/interface/FmpConst.h"
 #include "RecoHIMuon/HiMuTracking/interface/HITrackVertexMaker.h"
-
-//-------------------------------------
-// L1 Trigger header files
-//-------------------------------------
-
-#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuRegionalCand.h"
-#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTReadoutCollection.h"
-//-------------------------------------
-// L2 Trigger Header files
-//-------------------------------------
-#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 
 //
 // class declaration
 //
 namespace cms{
-class TestMuL1L2FilterSTA : public edm::EDFilter {
+class TestMuL1L2Filter : public edm::EDFilter {
 
    private:
-
-  edm::InputTag m_inTag;
-  edm::InputTag candTag_; 
-  edm::InputTag rphirecHitsTag;
-  std::string recHitBuilderName;
+     edm::ParameterSet pset_;
+     HICConst * theHICConst;
+     FmpConst * theFmpConst;
+     HITrackVertexMaker * theTrackVertexMaker;
 
    public:
 
   //constructor
 
-      explicit TestMuL1L2FilterSTA(const edm::ParameterSet&);
-      ~TestMuL1L2FilterSTA();
+      explicit TestMuL1L2Filter(const edm::ParameterSet&);
+      ~TestMuL1L2Filter();
+      
+  // General Block
+  
       virtual bool filter(edm ::Event&, const edm::EventSetup&);
       virtual void beginJob(const edm::EventSetup& es1);
       virtual void endJob();
-
-  // General Block
-  int runno;
-  int eveno;
-  TrackerGeometry* trackerg;
-  edm::ParameterSet pset_;
-  HICConst * theHICConst;
-  FmpConst * theFmpConst;
-  HITrackVertexMaker * theTrackVertexMaker;
+  
 };
 }
 #endif
