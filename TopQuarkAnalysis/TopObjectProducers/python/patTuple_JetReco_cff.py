48f8b0bb50b3c97335a7def4be110351fbca61fd
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2009-02-12 13:58:22 +0000

    Removed Geometry dependence, which should only appear in top-level config, as requested by David Lange.

--- TopQuarkAnalysis/TopObjectProducers/python/patTuple_JetReco_cff.py
+++ TopQuarkAnalysis/TopObjectProducers/python/patTuple_JetReco_cff.py
@@ -3,11 +3,6 @@ import FWCore.ParameterSet.Config as cms
 # sequences for jet re-reco with the pat tuple
 #
 
-## add geometry needed for pileup corrections for jet reco
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-from Geometry.CaloEventSetup.CaloGeometry_cfi       import *
-from Geometry.CaloEventSetup.CaloTopology_cfi       import *
-
 ## do the jet reconstruction for kt6
 from RecoJets.JetProducers.kt6CaloJets_cff import kt6CaloJets
 
