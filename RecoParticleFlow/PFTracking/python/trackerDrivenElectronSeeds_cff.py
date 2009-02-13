ef7a49a7b909ade8f9bab844973ef5b47d9f9b61
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2009-02-13 12:13:58 +0000

    pf-eg integration

---
+++ RecoParticleFlow/PFTracking/python/trackerDrivenElectronSeeds_cff.py
@@ -0,0 +1,26 @@
+import TrackingTools.GsfTracking.GsfElectronMaterialEffects_cfi
+ElectronMaterialEffects_forPreId = TrackingTools.GsfTracking.GsfElectronMaterialEffects_cfi.ElectronMaterialEffects.clone()
+from RecoParticleFlow.PFTracking.trackerDrivenElectronSeeds_cfi import *
+CloseComponentsMerger_forPreId = cms.ESProducer("CloseComponentsMergerESProducer5D",
+    ComponentName = cms.string('CloseComponentsMerger_forPreId'),
+    MaxComponents = cms.int32(4),
+    DistanceMeasure = cms.string('KullbackLeiblerDistance5D')
+)
+
+GsfTrajectoryFitter_forPreId = cms.ESProducer("GsfTrajectoryFitterESProducer",
+    Merger = cms.string('CloseComponentsMerger_forPreId'),
+    ComponentName = cms.string('GsfTrajectoryFitter_forPreId'),
+    MaterialEffectsUpdator = cms.string('ElectronMaterialEffects_forPreId'),
+    GeometricalPropagator = cms.string('fwdAnalyticalPropagator')
+)
+
+GsfTrajectorySmoother_forPreId = cms.ESProducer("GsfTrajectorySmootherESProducer",
+    Merger = cms.string('CloseComponentsMerger_forPreId'),
+    ComponentName = cms.string('GsfTrajectorySmoother_forPreId'),
+    MaterialEffectsUpdator = cms.string('ElectronMaterialEffects_forPreId'),
+    ErrorRescaling = cms.double(100.0),
+    GeometricalPropagator = cms.string('bwdAnalyticalPropagator')
+)
+
+ElectronMaterialEffects_forPreId.ComponentName = 'ElectronMaterialEffects_forPreId'
+ElectronMaterialEffects_forPreId.BetheHeitlerParametrization = 'BetheHeitler_cdfmom_nC3_O5.par'
