13a636780be1a5e3e0c9956af20caedffa0a339c
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-02-13 18:22:10 +0000

    adapt to changes in CalibTracker/SiStripESProducers. Now it should work

--- SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py
+++ SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py
@@ -1,19 +1,19 @@
 # Auto generated configuration file
 # using: 
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # $Source: /cvs_server/repositories/CMSSW/CMSSW/SimTracker/Configuration/python/SimTracker_Deconvolution_cff.py,v $
 
 import FWCore.ParameterSet.Config as cms
 def customise(process):
     # Load and Prefer the FakeConditions for SiStripNoise
-    process.load("CalibTracker.SiStripESProducers.SiStripNoiseFakeSource_cfi")
-    process.prefer("SiStripNoiseFakeESSource")
+    process.load("CalibTracker.SiStripESProducers.fake.SiStripNoisesFakeESSource_cfi")
+    process.prefer("siStripNoisesFakeESSource")
                                                                                           
     # Signal in Deconvolution Mode
     process.simSiStripDigis.APVpeakmode = cms.bool(False)
 
     # Fake Conditions Noise in Deconvolution Mode
-    process.SiStripNoiseFakeESSource.NoiseStripLengthSlope = 51.
-    process.SiStripNoiseFakeESSource.NoiseStripLengthQuote = 630.
+    process.SiStripNoisesGenerator.NoiseStripLengthSlope = 51.
+    process.SiStripNoisesGenerator.NoiseStripLengthQuote = 630.
     
     return(process)
