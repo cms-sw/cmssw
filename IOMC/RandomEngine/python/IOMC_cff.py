a7d55e1ba4f4785d7d15496a978c58c0e0c766b5
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-02-13 14:05:55 +0000

    Reorder the entries Add instances for LHCTransport and simCastorDigis

--- IOMC/RandomEngine/python/IOMC_cff.py
+++ IOMC/RandomEngine/python/IOMC_cff.py
@@ -1,11 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
 RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    theSource = cms.PSet(
+        initialSeed = cms.untracked.uint32(123456789),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    evtgenproducer = cms.PSet( 
+        initialSeed = cms.untracked.uint32(93278151),
+        engineName = cms.untracked.string('HepJamesRandom') 
+    ),
+    VtxSmeared = cms.PSet(
+        initialSeed = cms.untracked.uint32(98765432),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    LHCTransport = cms.PSet(
+        initialSeed = cms.untracked.uint32(87654321),
+        engineName = cms.untracked.string('TRandom3')
+    ),
+    g4SimHits = cms.PSet(
+        initialSeed = cms.untracked.uint32(11),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
     mix = cms.PSet(
         initialSeed = cms.untracked.uint32(12345),
         engineName = cms.untracked.string('HepJamesRandom')
     ),
-    simMuonRPCDigis = cms.PSet(
+    simSiStripDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(1234567),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    simSiPixelDigis = cms.PSet(
         initialSeed = cms.untracked.uint32(1234567),
         engineName = cms.untracked.string('HepJamesRandom')
     ),
@@ -13,50 +37,34 @@ RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
         initialSeed = cms.untracked.uint32(1234567),
         engineName = cms.untracked.string('HepJamesRandom')
     ),
-    # to save the status of the last event (useful for crashes)
-    saveFileName = cms.untracked.string(''),
-    simSiStripDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(1234567),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
     simHcalUnsuppressedDigis = cms.PSet(
         initialSeed = cms.untracked.uint32(11223344),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    # to restore the status of the last event, 
-    # comment the line above and decomment the following one
-    #   untracked string restoreFileName = "RandomEngineState.log"  
-    # to reproduce events using the RandomEngineStateProducer (source excluded),
-    # comment the sourceSeed definition, decomment the following one
-    #   untracked string restoreStateLabel = "randomEngineStateProducer"
-    theSource = cms.PSet(
-        initialSeed = cms.untracked.uint32(123456789),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    evtgenproducer = cms.PSet( 
-    initialSeed = cms.untracked.uint32(93278151),
-    engineName = cms.untracked.string('HepJamesRandom') 
-    ),
-    simMuonCSCDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(11223344),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    VtxSmeared = cms.PSet(
-        initialSeed = cms.untracked.uint32(98765432),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    g4SimHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(11),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    simSiPixelDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(1234567),
         engineName = cms.untracked.string('HepJamesRandom')
     ),
     simMuonDTDigis = cms.PSet(
         initialSeed = cms.untracked.uint32(1234567),
         engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    simMuonCSCDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(11223344),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    simMuonRPCDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(1234567),
+        engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    simCastorDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(12345678),
+        engineName = cms.untracked.string('HepJamesRandom')
     )
+    # to save the status of the last event (useful for crashes)
+    ,saveFileName = cms.untracked.string('')
+    # to restore the status of the last event, 
+    # comment the line above and decomment the following one
+    #   ,restoreFileName = cms.untracked.string('RandomEngineState.log')  
+    # to reproduce events using the RandomEngineStateProducer (source excluded),
+    # comment the sourceSeed definition, decomment the following one
+    #   ,restoreStateLabel = cms.untracked.string('randomEngineStateProducer')
 )
 
 randomEngineStateProducer = cms.EDProducer("RandomEngineStateProducer")
