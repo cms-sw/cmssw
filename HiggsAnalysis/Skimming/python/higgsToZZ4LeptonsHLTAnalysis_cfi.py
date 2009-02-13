dab548d181d5602329ec3bda1a73cd043ea60e38
Nicola De Filippis <sha1-50b8e21d12ea3a05a38a84ea0000270cd7663689@cern.ch>
2009-02-13 13:52:21 +0000

    *** empty log message ***

--- HiggsAnalysis/Skimming/python/higgsToZZ4LeptonsHLTAnalysis_cfi.py
+++ HiggsAnalysis/Skimming/python/higgsToZZ4LeptonsHLTAnalysis_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 higgsToZZ4LeptonsHLTAnalysis = cms.EDProducer("HiggsToZZ4LeptonsHLTAnalysis",
     ElectronCollectionLabel = cms.InputTag("pixelMatchGsfElectrons"),
-    HLTPaths = cms.vstring('HLT_IsoMu11', 'HLT_DoubleMu3', 'HLT_IsoEle15_L1I', 'HLT_IsoEle18_L1R', 'HLT_DoubleIsoEle10_L1I', 'HLT_DoubleIsoEle12_L1R'),
+    HLTPaths = cms.vstring('HLT_IsoMu11', 'HLT_Mu15', 'HLT_DoubleMu3', 'HLT_IsoEle15_L1I', 'HLT_Ele15_SW_L1R','HLT_DoubleIsoEle10_L1I', 'HLT_DoubleEle10_LW_OnlyPixelM_L1R'),
     andOr = cms.bool(True),
     MuonCollectionLabel = cms.InputTag("muons"),
     TriggerResultsTag = cms.InputTag("TriggerResults","","HLTreprocess")
