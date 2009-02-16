dd2803cc3abb175c81e62022d749a7149728ad20
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-02-16 09:25:38 +0000

    Fixed Typo

--- Validation/RecoMET/python/TCMET_cfi.py
+++ Validation/RecoMET/python/TCMET_cfi.py
@@ -10,7 +10,7 @@ tcMetAnalyzer = cms.EDAnalyzer(
     "METTester",
     OutputFile = cms.untracked.string('output.root'),
     InputMETLabel = cms.InputTag("tcMet"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
     FineBinning = cms.untracked.bool(False)
 
     ) 
