3cd2c59585e748d3e8b576723debfe3a8c866de9
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-02-16 08:51:58 +0000

    Added FineBinning parameter

--- Validation/RecoMET/python/HTMET_cfi.py
+++ Validation/RecoMET/python/HTMET_cfi.py
@@ -5,34 +5,45 @@ import FWCore.ParameterSet.Config as cms
 # Date: 03.04.2008
 #
 # Fill validation histograms for MET.
-htMetSC5Analyzer = cms.EDFilter("METTester",
+htMetSC5Analyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_htMetSC5.root'),
     InputMETLabel = cms.InputTag("htMetSC5"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
+    FineBinning = cms.untracked.bool(False)
 )
 
-htMetSC7Analyzer = cms.EDFilter("METTester",
+
+htMetSC7Analyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_htMetSC7.root'),
     InputMETLabel = cms.InputTag("htMetSC7"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
+    FineBinning = cms.untracked.bool(False)
 )
 
-htMetIC5Analyzer = cms.EDFilter("METTester",
+htMetIC5Analyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_htMetIC5.root'),
     InputMETLabel = cms.InputTag("htMetIC5"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
+    FineBinning = cms.untracked.bool(False)
 )
 
-htMetKT4Analyzer = cms.EDFilter("METTester",
+htMetKT4Analyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_htMetKT4.root'),
     InputMETLabel = cms.InputTag("htMetKT4"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
+    FineBinning = cms.untracked.bool(False)
 )
 
-htMetKT6Analyzer = cms.EDFilter("METTester",
+htMetKT6Analyzer = cms.EDFilter(
+    "METTester",
     OutputFile = cms.untracked.string('METTester_htMetKT6.root'),
     InputMETLabel = cms.InputTag("htMetKT6"),
-    METType = cms.untracked.string('MET')
+    METType = cms.untracked.string('MET'),
+    FineBinning = cms.untracked.bool(False)
 )
 
 
