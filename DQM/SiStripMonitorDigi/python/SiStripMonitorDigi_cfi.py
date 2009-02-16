aa2410c69e0dc36e888c5ee8a64d940afdb35d53
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2009-02-16 16:18:45 +0000

    New histo on digi vs APV cycle added

--- DQM/SiStripMonitorDigi/python/SiStripMonitorDigi_cfi.py
+++ DQM/SiStripMonitorDigi/python/SiStripMonitorDigi_cfi.py
@@ -71,23 +71,34 @@ SiStripMonitorDigi = cms.EDFilter("SiStripMonitorDigi",
 
     CreateTrendMEs = cms.bool(False),
     Trending = cms.PSet(
-        UpdateMode = cms.int32(1),
-        Nbins = cms.int32(10),
-        ymax = cms.double(10000.0),
-        Steps = cms.int32(10),
-        xmax = cms.double(10.0),
+        Nbins = cms.int32(600),
         xmin = cms.double(0.0),
-        ymin = cms.double(0.0)
+        xmax = cms.double(1.0*60*60),        
+        ymin = cms.double(0.0),
+        ymax = cms.double(10000.0)        
     ),
 
-    Mod_On = cms.bool(True),
-    #select detectors
-    detectorson = cms.PSet(
-        tidon = cms.bool(True),
-        tibon = cms.bool(True),
-        tecon = cms.bool(True),
-        tobon = cms.bool(True)
+    TH2DigiApvCycle = cms.PSet(
+        Nbins = cms.int32(70),
+        xmin = cms.double(-0.5),
+        xmax = cms.double(69.5),
+        Nbinsy = cms.int32(200),
+        ymin = cms.double(0.0),
+        ymax = cms.double(1000.0),
+        subdetswitchon = cms.bool(False)
     ),
+
+    TProfDigiApvCycle = cms.PSet(
+        Nbins = cms.int32(70),
+        xmin = cms.double(-0.5),
+        xmax = cms.double(69.5),
+        Nbinsy = cms.int32(200),
+        ymin = cms.double(0.0),
+        ymax = cms.double(10000.0),
+        subdetswitchon = cms.bool(False)
+        ),
+
+    Mod_On = cms.bool(True),
 
     # rest of parameters
     SelectAllDetectors = cms.bool(False),
@@ -99,5 +110,5 @@ SiStripMonitorDigi = cms.EDFilter("SiStripMonitorDigi",
     # by default do not write out any file with histograms
     # can overwrite this in .cfg file with: replace SiStripMonitorDigi.OutputMEsInRootFile = true
     OutputMEsInRootFile = cms.bool(False),
-    OutputFileName = cms.string('/tmp/charaf/test_digi_sim.root'),
+    OutputFileName = cms.string('/tmp/borrell/test_digi_sim.root'),
 )
