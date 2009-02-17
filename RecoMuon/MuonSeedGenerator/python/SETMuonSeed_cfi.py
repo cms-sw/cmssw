57eb763a4f354e85e1cda2781a252936bd24c42e
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-02-17 23:19:29 +0000

    Include pT scaling parameters

--- RecoMuon/MuonSeedGenerator/python/SETMuonSeed_cfi.py
+++ RecoMuon/MuonSeedGenerator/python/SETMuonSeed_cfi.py
@@ -3,11 +3,14 @@ import FWCore.ParameterSet.Config as cms
 # The services
 from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
 # parametrization for initial pT
-from RecoMuon.MuonSeedGenerator.ptSeedParameterization_38T_cfi import *
+from RecoMuon.MuonSeedGenerator.ptSeedParameterization_38T_cfi import ptSeedParameterization
+from RecoMuon.MuonSeedGenerator.MuonSeedPtScale_cfi import dphiScale
+
 SETMuonSeed  = cms.EDProducer("SETMuonSeedProducer",
     MuonServiceProxy,
     SETTrajBuilderParameters = cms.PSet(
         ptSeedParameterization, 
+        dphiScale,
         Apply_prePruning = cms.bool(True),
 # Careful - next is used together with useSubRecHits in standAloneMuons_cfi.py (for now)
 #	UseSegmentsInTrajectory = cms.bool(True),
