ed6b0e5ccf29c783689eb86fe2468afc06a84673
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-02-17 11:03:23 +0000

    move luminosity

--- CondCore/RunInfoPlugins/src/plugin.cc
+++ CondCore/RunInfoPlugins/src/plugin.cc
@@ -5,8 +5,11 @@
 #include "CondFormats/DataRecord/interface/L1TriggerScalerRcd.h"
 #include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
 
+#include "CondFormats/RunInfo/interface/LuminosityInfo.h"
+#include "CondFormats/DataRecord/interface/LuminosityInfoRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(RunSummaryRcd,RunSummary);
 REGISTER_PLUGIN(RunInfoRcd,RunInfo);
 REGISTER_PLUGIN(L1TriggerScalerRcd, L1TriggerScaler);
+REGISTER_PLUGIN(LuminosityInfoRcd, lumi::LuminosityInfo);
