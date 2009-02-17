0b7f13872f47921d13143672aeef3f03dd61184a
benucci <sha1-daad2104d9852eedb2ba1d7c2d4eda3d03263c78@cern.ch>
2009-02-17 12:19:11 +0000

    analyser to monitor cond data on-demand

--- DQM/SiStripMonitorSummary/src/SiStripMonitorCondDataOnDemandExample.cc
+++ DQM/SiStripMonitorSummary/src/SiStripMonitorCondDataOnDemandExample.cc
@@ -53,6 +53,8 @@ void SiStripMonitorCondDataOnDemandExample::beginRun(edm::Run const& run, edm::E
   eventCounter_=0;
   condDataMonitoring_ = new SiStripClassToMonitorCondData(conf_);
   condDataMonitoring_->beginRun(eSetup);
+  
+  
 
 } // beginRun
 // -----
@@ -70,20 +72,21 @@ void SiStripMonitorCondDataOnDemandExample::beginJob(void){} //beginJob
 // ----- Analyze
 //
 void SiStripMonitorCondDataOnDemandExample::analyze(edm::Event const& iEvent, edm::EventSetup const& eSetup){
-
-  eventCounter_++; 
+  //eventCounter_++; 
 
   // on demand type I : eventSetup and detId to be passed
   // output : ME's corresponding to that detId
   
   
-  if(eventCounter_==1){ condDataMonitoring_ -> getModMEsOnDemand(eSetup,369125542);}
+  //if(eventCounter_==1){ condDataMonitoring_ -> getModMEsOnDemand(eSetup,369125542);}
   
   // on demand type II : eventSetup, subdetector-type(TIB/TOB/TEC/TID), 
-  //                                 subdetector-side for TEC/TID
-  //                                 layer_number
+  //                                 subdetector-side for TEC/TID (0 for TIB and TOB)
+  //                                 layer_number (0=all layers)
  
-  if(eventCounter_==2){ condDataMonitoring_ -> getLayerMEsOnDemand(eSetup,"TOB",0,1);}
+  //if(eventCounter_==2){ condDataMonitoring_ -> getLayerMEsOnDemand(eSetup,"TEC",0,1);}
+  //condDataMonitoring_ -> getModMEsOnDemand(eSetup,369125542);
+  condDataMonitoring_ -> getLayerMEsOnDemand(eSetup,"TEC",2,4);
 
   
   
