202632fa3e0db117fa13e9dcddcf5d10c47e466f
benucci <sha1-daad2104d9852eedb2ba1d7c2d4eda3d03263c78@cern.ch>
2009-02-17 13:39:53 +0000

    bug fixes after on-demand monitoring inclusion

--- DQM/SiStripMonitorSummary/src/SiStripNoisesDQM.cc
+++ DQM/SiStripMonitorSummary/src/SiStripNoisesDQM.cc
@@ -230,8 +230,7 @@ void SiStripNoisesDQM::fillMEsForLayer( std::map<uint32_t, ModMEs> selMEsMap_, u
     for(unsigned int i=0;i<sameLayerDetIds_.size();i++){
       if(sameLayerDetIds_[i]==selDetId_){iBin=i+1;}
     }  
-    // Cumulative distribution with average Noise value on a layer (not needed):
-    ////  selME_.SummaryDistr->Fill(iBin,meanNoise);
+    selME_.SummaryDistr->Fill(iBin,meanNoise);
 
     // Fill the Histo_TkMap with the mean Noise:
         if(HistoMaps_On_ ) Tk_HM_->fill(selDetId_, meanNoise);
