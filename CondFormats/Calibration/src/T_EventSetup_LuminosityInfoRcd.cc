c4ccc94c358a297ba844e36fa91348115ddf9157
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-02-17 11:02:05 +0000

    removed

--- CondFormats/Calibration/src/T_EventSetup_LuminosityInfoRcd.cc
+++
@@ -1,5 +0,0 @@
-// user include files
-#include "CondFormats/Calibration/interface/LuminosityInfo.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
-EVENTSETUP_DATA_REG(lumi::LuminosityInfo);
