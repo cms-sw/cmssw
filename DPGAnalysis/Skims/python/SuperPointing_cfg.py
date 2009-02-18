b3f89d990cab7d0e24567eb186471da0f781ecff
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-02-18 10:20:22 +0000

    add dorp of MEtoEDM stuff

--- DPGAnalysis/Skims/python/SuperPointing_cfg.py
+++ DPGAnalysis/Skims/python/SuperPointing_cfg.py
@@ -4,16 +4,13 @@ process = cms.Process("SKIM")
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-       '/store/data/Commissioning08/Cosmics/RECO/v1/000/067/122/D424EBA5-55A0-DD11-A8BF-000423D9853C.root',
-       '/store/data/Commissioning08/Cosmics/RECO/v1/000/067/122/C67EDF0D-49A0-DD11-9403-001617DBD332.root'),
+  '/store/data/Commissioning08/Cosmics/RECO/CRAFT_ALL_V9_225-v1/0002/0A12CE23-D7F9-DD11-819E-00E081348D21.root'),
                             secondaryFileNames = cms.untracked.vstring(
-       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/6E2601EC-3FA0-DD11-BA50-000423D986A8.root',
-       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/122/C240B0B2-47A0-DD11-A6AD-001617C3B654.root')
+        '/store/data/Commissioning08/Cosmics/RAW/v1/000/069/578/085EFED4-E5AB-DD11-9ACA-001617C3B6FE.root')
 )
 
-
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.11 $'),
+    version = cms.untracked.string('$Revision: 1.12 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/SuperPointing_cfg.py,v $'),
     annotation = cms.untracked.string('CRAFT SuperPointing skim')
 )
@@ -108,7 +105,7 @@ process.ctfWithMaterialTracksP5Path = cms.Path(process.ctfWithMaterialTracksP5Fi
 
 
 process.out = cms.OutputModule("PoolOutputModule",
-                               outputCommands = cms.untracked.vstring('keep *'),
+                               outputCommands = cms.untracked.vstring('keep *','drop *_MEtoEDMConverter_*_*'),
                                SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('cosmicMuonsBarrelOnlyPath',
                                                                                             'cosmicMuonsPath',
                                                                                             'cosmicMuons1LegPath',
