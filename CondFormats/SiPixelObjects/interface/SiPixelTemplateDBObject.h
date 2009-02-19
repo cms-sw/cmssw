3ec87c8c9a7f2107e90c999cda60e51106c3b986
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-02-19 10:50:56 +0000

    Removed SiPixelTemplateDBObject.cc. The functionality in this file has been moved to uploader and reader in CondTools/SiPixel. No datamembers were removed or added.

--- CondFormats/SiPixelObjects/interface/SiPixelTemplateDBObject.h
+++ CondFormats/SiPixelObjects/interface/SiPixelTemplateDBObject.h
@@ -17,13 +17,10 @@ public:
 	}
 	virtual ~SiPixelTemplateDBObject(){}
 	
-	typedef std::vector<std::string> vstring;
-	
-	void fillDB(const vstring& atitles);
-	
+	//- Allows the dbobject to be read out like cout
 	friend std::ostream& operator<<(std::ostream& s, const SiPixelTemplateDBObject& dbobject);
 
-	//- Fills interger from dbobject
+	//- Fills integer from dbobject
 	SiPixelTemplateDBObject& operator>>( int& i)
 		{
 			isInvalid_ = false;
@@ -52,12 +49,19 @@ public:
 	void setVersion(float version) {version_ = version;}
 	void setInvalid() {isInvalid_ = true;}
 	bool fail() {return isInvalid_;}
+
+	//- Setter functions
+	void push_back(float entry) {sVector_.push_back(entry);}
+	void setIndex(int index) {index_ = index;}
+	void setMaxIndex(int maxIndex) {maxIndex_ = maxIndex;}
+	void setNumOfTempl(int numOfTempl) {numOfTempl_ = numOfTempl;}
 	
 	//- Accessor functions
-	int index() {return index_;}
-	int numOfTempl() {return numOfTempl_;}
-	float version() const  {return version_;}
-	std::vector<float> sVector() {return sVector_;}
+	int index() const {return index_;}
+	int maxIndex() const {return maxIndex_;}
+	int numOfTempl() const {return numOfTempl_;}
+	float version() const {return version_;}
+	std::vector<float> sVector() const {return sVector_;}
 
 	//- Able to set the index for template header 
 	void incrementIndex(int i) {index_+=i;}
@@ -77,7 +81,8 @@ public:
 		{
 			std::map<unsigned int,short>::const_iterator id=templ_ID.find(detid);
 			if(id!=templ_ID.end()){
-				edm::LogError("SiPixelTemplateDBObject") << "Template ID for DetID " << detid << " is already stored. Skipping this put" << std::endl;
+				edm::LogError("SiPixelTemplateDBObject") << "Template ID for DetID " << detid
+																								 << " is already stored. Skipping this put" << std::endl;
 				return false;
 			}
 			else templ_ID[detid] = value;
@@ -88,20 +93,18 @@ public:
 		{
 			std::map<unsigned int,short>::const_iterator id=templ_ID.find(detid);
 			if(id!=templ_ID.end()) return id->second;
-			else edm::LogError("SiPixelTemplateDBObject") << "Template ID for DetID " << detid << " is not stored" << std::endl;
+			else edm::LogError("SiPixelTemplateDBObject") << "Template ID for DetID " << detid
+																										<< " is not stored" << std::endl;
 			return 0;
 		}
 	
-				
-
-	
 private:
-  int index_;
+	int index_;
 	int maxIndex_;
 	int numOfTempl_;
 	float version_;
 	bool isInvalid_;
 	std::vector<float> sVector_;
 	std::map<unsigned int,short> templ_ID;
-		};//end SiPixelTemplateDBObject
+};//end SiPixelTemplateDBObject
 #endif
