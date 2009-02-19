6853b5cc2b5a47f8cf351e35561e0953e8124643
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-02-19 17:39:58 +0000

    add HLTScalerRcd

---
+++ CondFormats/DataRecord/interface/HLTScalerRcd.h
@@ -0,0 +1,6 @@
+#ifndef HLTSCALERRCD_H
+#define HLTSCALERRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class HLTScalerRcd : public edm::eventsetup::EventSetupRecordImplementation<HLTScalerRcd> {};
+#endif
