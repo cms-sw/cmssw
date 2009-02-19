72ce78a4396165628c8a33865cdba3c0c1c84634
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2009-02-19 09:56:54 +0000

    StraightLinePropagator

--- RecoMuon/TrackingTools/python/MuonServiceProxy_cff.py
+++ RecoMuon/TrackingTools/python/MuonServiceProxy_cff.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+
+from TrackingTools.GeomPropagators.StraightLinePropagator_cfi import *
 from TrackingTools.MaterialEffects.MaterialPropagator_cfi import *
 from TrackingTools.MaterialEffects.OppositeMaterialPropagator_cfi import *
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi import *
@@ -13,26 +15,27 @@ from TrackingTools.GeomPropagators.SmartPropagatorAny_cfi import *
 MuonServiceProxy = cms.PSet(
     ServiceParameters = cms.PSet(
         Propagators = cms.untracked.vstring('SteppingHelixPropagatorAny', 
-            'SteppingHelixPropagatorAlong', 
-            'SteppingHelixPropagatorOpposite', 
-            'SteppingHelixPropagatorL2Any', 
-            'SteppingHelixPropagatorL2Along', 
-            'SteppingHelixPropagatorL2Opposite', 
-            #A bunch with error propagation turned off: can be about 2 times faster 
-            'SteppingHelixPropagatorAnyNoError',
-            'SteppingHelixPropagatorAlongNoError',
-            'SteppingHelixPropagatorOppositeNoError',
-            'SteppingHelixPropagatorL2AnyNoError',
-            'SteppingHelixPropagatorL2AlongNoError',
-            'SteppingHelixPropagatorL2OppositeNoError',
-            'PropagatorWithMaterial', 
-            'PropagatorWithMaterialOpposite', 
-            'SmartPropagator', 
-            'SmartPropagatorOpposite', 
-            'SmartPropagatorAnyOpposite', 
-            'SmartPropagatorAny', 
-            'SmartPropagatorRK', 
-            'SmartPropagatorAnyRK'),
+                                            'SteppingHelixPropagatorAlong', 
+                                            'SteppingHelixPropagatorOpposite', 
+                                            'SteppingHelixPropagatorL2Any', 
+                                            'SteppingHelixPropagatorL2Along', 
+                                            'SteppingHelixPropagatorL2Opposite', 
+                                            #A bunch with error propagation turned off: can be about 2 times faster 
+                                            'SteppingHelixPropagatorAnyNoError',
+                                            'SteppingHelixPropagatorAlongNoError',
+                                            'SteppingHelixPropagatorOppositeNoError',
+                                            'SteppingHelixPropagatorL2AnyNoError',
+                                            'SteppingHelixPropagatorL2AlongNoError',
+                                            'SteppingHelixPropagatorL2OppositeNoError',
+                                            'PropagatorWithMaterial', 
+                                            'PropagatorWithMaterialOpposite', 
+                                            'SmartPropagator', 
+                                            'SmartPropagatorOpposite', 
+                                            'SmartPropagatorAnyOpposite', 
+                                            'SmartPropagatorAny', 
+                                            'SmartPropagatorRK', 
+                                            'SmartPropagatorAnyRK',
+                                            'StraightLinePropagator'),
         RPCLayers = cms.bool(True),
         UseMuonNavigation = cms.untracked.bool(True)
     )
