21f1da0dbd9d3b953221c75802e9b456cd96fd41
Louis James Antonelli <sha1-442197ce02a0252aaee7a57f46fd47b9778e7d2f@cern.ch>
2009-02-19 16:30:17 +0000

    updated for 31X and turned on trigger filtering

--- DQMOffline/EGamma/test/PhotonAnalyzer_cfg.py
+++ DQMOffline/EGamma/test/PhotonAnalyzer_cfg.py
@@ -19,21 +19,19 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 
 
-##         '/store/relval/CMSSW_3_0_0_pre6/RelValQCD_Pt_80_120/GEN-SIM-RECO/IDEAL_30X_v1/0005/227BFA61-D5DD-DD11-88CE-001617C3B66C.root',
-##         '/store/relval/CMSSW_3_0_0_pre6/RelValQCD_Pt_80_120/GEN-SIM-RECO/IDEAL_30X_v1/0005/780BFC49-41DE-DD11-ABB7-000423D99896.root',
-##         '/store/relval/CMSSW_3_0_0_pre6/RelValQCD_Pt_80_120/GEN-SIM-RECO/IDEAL_30X_v1/0005/B6C941C9-D5DD-DD11-8F1A-001D09F25479.root',
-##         '/store/relval/CMSSW_3_0_0_pre6/RelValQCD_Pt_80_120/GEN-SIM-RECO/IDEAL_30X_v1/0005/C670EC9F-D4DD-DD11-9476-000423D98930.root',
-##         '/store/relval/CMSSW_3_0_0_pre6/RelValQCD_Pt_80_120/GEN-SIM-RECO/IDEAL_30X_v1/0005/CC9494FF-D4DD-DD11-95A9-001D09F28EC1.root'
 
+    '/store/relval/CMSSW_3_1_0_pre1/RelValSingleGammaPt35/GEN-SIM-RECO/IDEAL_30X_v1/0001/88C98CFC-06F8-DD11-AC35-000423D6CA6E.root'
 
-##    '/store/relval/CMSSW_3_0_0_pre6/RelValSingleGammaPt35/GEN-SIM-RECO/IDEAL_30X_v1/0005/98C45436-41DE-DD11-9B91-000423D95220.root'
+##          '/store/relval/CMSSW_3_1_0_pre1/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/0CAA1EC0-F7F7-DD11-911D-000423D9970C.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/58AF1BC2-06F8-DD11-91E3-000423D985B0.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/DE195AF6-F8F7-DD11-A490-001617E30F56.root',
+##          '/store/relval/CMSSW_3_1_0_pre1/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/E4EDF9CD-F7F7-DD11-825B-00304879FA4A.root'
 
-
-        '/store/relval/CMSSW_3_0_0_pre6/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0005/60FEA65F-2DDE-DD11-A12E-001617E30F56.root',
-        '/store/relval/CMSSW_3_0_0_pre6/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0005/C0E99B59-34DE-DD11-9194-000423D174FE.root',
-        '/store/relval/CMSSW_3_0_0_pre6/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0005/F41CD6A5-41DE-DD11-8049-000423D99AAA.root',
-        '/store/relval/CMSSW_3_0_0_pre6/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0005/FCFD0B48-2BDE-DD11-97C8-000423D99B3E.root'
-
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/1270CC97-CEF7-DD11-906D-001617C3B79A.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/3E40D281-CEF7-DD11-9052-000423D944FC.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/AE8A1048-E2F7-DD11-8FC4-000423D98B08.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/B65320D4-06F8-DD11-B177-0016177CA7A0.root',
+##         '/store/relval/CMSSW_3_1_0_pre1/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP_30X_v1/0001/E46D42A7-CEF7-DD11-BFC8-000423D99264.root'
 
 
 ))
@@ -49,7 +47,7 @@ from DQMOffline.EGamma.photonAnalyzer_cfi import *
 photonAnalysis.OutputMEsInRootFile = cms.bool(True)
 photonAnalysis.OutputFileName = 'DQMPhotonsStandaloneForMC.root'
 photonAnalysis.Verbosity = cms.untracked.int32(0)
-photonAnalysis.useTriggerFiltering = cms.bool(False)
+photonAnalysis.useTriggerFiltering = cms.bool(True)
 photonAnalysis.standAlone = cms.bool(True)
 
 
