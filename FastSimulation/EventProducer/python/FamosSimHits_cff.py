939513629e54743e57ad981028cc33cf5ce4cf2e
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-02-20 13:01:25 +0000

    Update to generator redesign 3_1_X. HepMC GenEvent is now per default in label "generator".

--- FastSimulation/EventProducer/python/FamosSimHits_cff.py
+++ FastSimulation/EventProducer/python/FamosSimHits_cff.py
@@ -20,7 +20,7 @@ famosSimHits = cms.EDProducer("FamosProducer",
     # Kinematic cuts for the particle filter in the SimEvent
     ParticleFilterBlock,
     # The HepMCProduct source
-    SourceLabel = cms.InputTag("source"),
+    SourceLabel = cms.InputTag("general"),
     # The genParticle source (in case there is no HepMCProduct)
     GenParticleLabel = cms.InputTag("genParticles"),
     # If false, no SimTrack collection for Muons is stored
