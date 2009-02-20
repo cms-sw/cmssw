d450a960fbe47075cc939cacb18aed04b6eb718e
Matthias Edelhoff <sha1-b4e200e941da071480460e77b2afa35e3ad47d9a@cern.ch>
2009-02-20 13:15:06 +0000

    switched to common HLT_TrackerCosmics selection

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlCosmics0THLT_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlCosmics0THLT_cff.py
@@ -1,23 +1,15 @@
-# last update: $Date: 2008/07/21 12:57:08 $ by $Author: flucke $
+# last update: $Date: 2008/10/29 17:28:42 $ by $Author: flucke $
 import FWCore.ParameterSet.Config as cms
 
 #_________________________________HLT bits___________________________________________
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 
-ALCARECOTkAlCosmicsCTF0THLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+
+
+ALCARECOTkAlCosmics0THLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
     andOr = True, ## choose logical OR between Triggerbits
-    HLTPaths = ['HLT_TrackerCosmics_CTF'],
+    HLTPaths = ['HLT TrackerCosmics'],
     throw = False # tolerate triggers stated above, but not available
-    )
-
-# same as above, but other path
-ALCARECOTkAlCosmicsCosmicTF0THLT = ALCARECOTkAlCosmicsCTF0THLT.clone(
-    HLTPaths = ['HLT_TrackerCosmics_CoTF']
-    )
-
-# same as above, but other path
-ALCARECOTkAlCosmicsRS0THLT = ALCARECOTkAlCosmicsCTF0THLT.clone(
-    HLTPaths = ['HLT_TrackerCosmics_RS']
     )
 
 #________________________________Track selection____________________________________
@@ -26,7 +18,7 @@ from Alignment.CommonAlignmentProducer.ALCARECOTkAlCosmics0T_cff import *
 
 #________________________________Sequences____________________________________
 # simply add HLT in front of HLT-less paths
-seqALCARECOTkAlCosmicsCTF0THLT = cms.Sequence(ALCARECOTkAlCosmicsCTF0THLT+seqALCARECOTkAlCosmicsCTF0T)
-seqALCARECOTkAlCosmicsCosmicTF0THLT = cms.Sequence(ALCARECOTkAlCosmicsCosmicTF0THLT+seqALCARECOTkAlCosmicsCosmicTF0T)
-seqALCARECOTkAlCosmicsRS0THLT = cms.Sequence(ALCARECOTkAlCosmicsRS0THLT+seqALCARECOTkAlCosmicsRS0T)
+seqALCARECOTkAlCosmicsCTF0THLT = cms.Sequence(ALCARECOTkAlCosmics0THLT+seqALCARECOTkAlCosmicsCTF0T)
+seqALCARECOTkAlCosmicsCosmicTF0THLT = cms.Sequence(ALCARECOTkAlCosmics0THLT+seqALCARECOTkAlCosmicsCosmicTF0T)
+seqALCARECOTkAlCosmicsRS0THLT = cms.Sequence(ALCARECOTkAlCosmics0THLT+seqALCARECOTkAlCosmicsRS0T)
 
--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlCosmicsHLT_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlCosmicsHLT_cff.py
@@ -1,31 +1,21 @@
-# last update: $Date: 2008/07/21 12:57:08 $ by $Author: flucke $
+# last update: $Date: 2008/10/29 17:28:42 $ by $Author: flucke $
 import FWCore.ParameterSet.Config as cms
 
 #_________________________________HLT bits___________________________________________
 import HLTrigger.HLTfilters.hltHighLevel_cfi
-ALCARECOTkAlCosmicsCTFHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+ALCARECOTkAlCosmicsHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
     andOr = True, ## choose logical OR between Triggerbits
-    HLTPaths = ['HLT_TrackerCosmics_CTF'],
+    HLTPaths = ['HLT_TrackerCosmics'],
     throw = False # tolerate triggers stated above, but not available
     )
     
-# same as above, but other path
-ALCARECOTkAlCosmicsCosmicTFHLT = ALCARECOTkAlCosmicsCTFHLT.clone(
-    HLTPaths = ['HLT_TrackerCosmics_CoTF']
-    )
-
-# same as above, but other path
-ALCARECOTkAlCosmicsRSHLT = ALCARECOTkAlCosmicsCTFHLT.clone(
-    HLTPaths = ['HLT_TrackerCosmics_RS']
-    )
-
 #________________________________Track selection____________________________________
 # take from stream that is HLT-less
 from Alignment.CommonAlignmentProducer.ALCARECOTkAlCosmics_cff import *
 
 #________________________________Sequences____________________________________
 # simply add HLT in front of HLT-less paths
-seqALCARECOTkAlCosmicsCTFHLT = cms.Sequence(ALCARECOTkAlCosmicsCTFHLT+seqALCARECOTkAlCosmicsCTF)
-seqALCARECOTkAlCosmicsCosmicTFHLT = cms.Sequence(ALCARECOTkAlCosmicsCosmicTFHLT+seqALCARECOTkAlCosmicsCosmicTF)
-seqALCARECOTkAlCosmicsRSHLT = cms.Sequence(ALCARECOTkAlCosmicsRSHLT+seqALCARECOTkAlCosmicsRS)
+seqALCARECOTkAlCosmicsCTFHLT = cms.Sequence(ALCARECOTkAlCosmicsHLT+seqALCARECOTkAlCosmicsCTF)
+seqALCARECOTkAlCosmicsCosmicTFHLT = cms.Sequence(ALCARECOTkAlCosmicsHLT+seqALCARECOTkAlCosmicsCosmicTF)
+seqALCARECOTkAlCosmicsRSHLT = cms.Sequence(ALCARECOTkAlCosmicsHLT+seqALCARECOTkAlCosmicsRS)
 
