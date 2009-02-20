4f4fbee18fa3c60fdbf5ee2abc36b8881e3352fe
benucci <sha1-daad2104d9852eedb2ba1d7c2d4eda3d03263c78@cern.ch>
2009-02-20 16:30:21 +0000

    cfi for on demand

--- DQM/SiStripMonitorSummary/python/SiStripMonitorCondDataOnDemandExample_cfi.py
+++ DQM/SiStripMonitorSummary/python/SiStripMonitorCondDataOnDemandExample_cfi.py
@@ -6,131 +6,295 @@
 import FWCore.ParameterSet.Config as cms
 
 myOnDemandExample = cms.EDFilter("SiStripMonitorCondDataOnDemandExample",
-    OutputMEsInRootFile = cms.bool(False),
-    MonitorSiStripLorentzAngle = cms.bool(True),
+
+     OutputMEsInRootFile = cms.bool(True),
+     OutputFileName = cms.string('SiStripMonitorCondDataOnDemandExample.root'),
+     
+     MonitorSiStripPedestal     = cms.bool(True),
+     MonitorSiStripNoise        = cms.bool(True),
+     MonitorSiStripQuality      = cms.bool(True),
+     MonitorSiStripApvGain      = cms.bool(False),
+     MonitorSiStripLorentzAngle = cms.bool(False),
+     
+     MonitorSiStripCabling      = cms.bool(False),
+     MonitorSiStripLowThreshold = cms.bool(False),
+     MonitorSiStripHighThreshold= cms.bool(False) ,    
+     
+
+     FillConditions_PSet = cms.PSet(     
+      FolderName_For_QualityAndCabling_SummaryHistos= cms.string("SiStrip/Tracks"),
+      Mod_On                  = cms.bool(False),
+      HistoMaps_On            = cms.bool(False),
+      SummaryOnStringLevel_On = cms.bool(False),
+      SummaryOnLayerLevel_On  = cms.bool(True),
+      GrandSummary_On         = cms.bool(False),
+      StripQualityLabel       = cms.string(''),
+        
+      #  exclude OR include a set of modules
+      restrictModules         = cms.bool(False),
+
+      ModulesToBeIncluded     = cms.vuint32(), #e.g. {369120277, 369120278, 369120282}
+      ModulesToBeExcluded     = cms.vuint32(),
+        
+      # exclude a subdetector
+      SubDetectorsToBeExcluded = cms.vstring('none'), #possibilities : "none" or
+                                                      #combinations of {"TIB","TOB","TID","TEC" }
+      ModulesToBeFilled = cms.string('all') 
+               
+    ),
+    # -----
+
+    SiStripCablingDQM_PSet = cms.PSet(
+    
+      CondObj_fillId       = cms.string('onlyProfile'),
+      CondObj_name         = cms.string('fedcabling')
+    ), 
+    
+    # -----
+    SiStripLowThresholdDQM_PSet = cms.PSet(
+
+      WhichThreshold= cms.string('Low'),
+
+      CondObj_fillId = cms.string('onlyProfile'), 
+      CondObj_name   = cms.string('lowthreshold'),  
+
+      FillSummaryAtLayerLevel= cms.bool(True),
+      FillSummaryProfileAtLayerLevel=cms.bool(False),
+
+
+      Profile_description = cms.string('Profile_LowThresholdFromCondDB'),
+      Profile_xTitle      = cms.string('Strip Number'),
+      Profile_yTitle      = cms.string('Low Threshold from CondDB(ADC)'),
+	
+      SummaryOfProfile_description = cms.string('ProfileSummary_LowThresholdFromCondDB'),
+      SummaryOfProfile_xTitle      = cms.string('Strip Number'),
+      SummaryOfProfile_yTitle      = cms.string('Low Threshold from CondDB(ADC)'),	
+      SummaryOfProfile_NchY        = cms.int32(100),
+      SummaryOfProfile_LowY        = cms.double(0),
+      SummaryOfProfile_HighY       = cms.double(10),
+
+	
+	
+      Summary_description   = cms.string('Summary_LowThresholdFromCondDB'),
+      Summary_xTitle        = cms.string('detId'),
+      Summary_yTitle        = cms.string('Low Threshold from CondDB(ADC)'),
+      Summary_NchY          = cms.int32(100),
+      Summary_LowY          = cms.double(0),
+      Summary_HighY         = cms.double(10)
+    ),
+    
+    # ----- 
+    SiStripHighThresholdDQM_PSet = cms.PSet(
+
+      WhichThreshold= cms.string('High'),
+
+      CondObj_fillId = cms.string('onlyProfile'), 
+      CondObj_name   = cms.string('highthreshold'),  
+
+      FillSummaryAtLayerLevel= cms.bool(True),
+      FillSummaryProfileAtLayerLevel=cms.bool(False),
+
+
+      Profile_description = cms.string('Profile_HighThresholdFromCondDB'),
+      Profile_xTitle      = cms.string('Strip Number'),
+      Profile_yTitle      = cms.string('High Threshold from CondDB(ADC)'),
+	
+      SummaryOfProfile_description = cms.string('ProfileSummary_HighThresholdFromCondDB'),
+      SummaryOfProfile_xTitle      = cms.string('Strip Number'),
+      SummaryOfProfile_yTitle      = cms.string('High Threshold from CondDB(ADC)'),	
+      SummaryOfProfile_NchY        = cms.int32(100),
+      SummaryOfProfile_LowY        = cms.double(0),
+      SummaryOfProfile_HighY       = cms.double(10),
+
+	
+	
+      Summary_description   = cms.string('Summary_HighThresholdFromCondDB'),
+      Summary_xTitle        = cms.string('detId'),
+      Summary_yTitle        = cms.string('High Threshold from CondDB(ADC)'),
+      Summary_NchY          = cms.int32(100),
+      Summary_LowY          = cms.double(0),
+      Summary_HighY         = cms.double(10)
+    ),
+       
     # -----
     SiStripApvGainsDQM_PSet = cms.PSet(
-        Profile_xTitle = cms.string('Apv Number'),
-        Summary_LowY = cms.double(0.5),
-        Summary_xTitle = cms.string('detId'),
-        Cumul_HighX = cms.double(1.5),
-        Cumul_description = cms.string('ApvGainFromCondDB'),
-        SummaryOfProfile_yTitle = cms.string('ApvGain from CondDB'),
-        SummaryOfProfile_NchY = cms.int32(50),
-        Summary_description = cms.string('Summary_ApvGainFromCondDB'),
-        SummaryOfProfile_LowY = cms.double(0.5),
-        Summary_yTitle = cms.string('ApvGain from CondDB'),
-        SummaryOfProfile_HighY = cms.double(1.5),
-        Cumul_NchX = cms.int32(50),
-        CondObj_name = cms.string('apvgain'),
-        CondObj_fillId = cms.string('ProfileAndCumul'),
-        Cumul_xTitle = cms.string('ApvGain from CondDB'),
-        Summary_HighY = cms.double(1.5),
-        SummaryOfProfile_description = cms.string('ProfileSummary_ApvGainFromCondDB'),
-        Summary_NchY = cms.int32(50),
-        Cumul_yTitle = cms.string(' '),
-        Profile_description = cms.string('Profile_ApvGainFromCondDB'),
-        SummaryOfProfile_xTitle = cms.string('Apv Number'),
-        Cumul_LowX = cms.double(0.5),
-        Profile_yTitle = cms.string('ApvGain from CondDB')
+    
+      CondObj_name   = cms.string('apvgain'),
+      CondObj_fillId = cms.string('onlyProfile'),
+
+      FillSummaryAtLayerLevel           = cms.bool(True),
+      FillSummaryProfileAtLayerLevel    = cms.bool(False),
+
+      Profile_description = cms.string('Profile_ApvGainFromCondDB'),
+      Profile_xTitle      = cms.string('Apv Number'),
+      Profile_yTitle      = cms.string('ApvGain from CondDB'),
+
+      Cumul_description   = cms.string('ApvGainFromCondDB'),
+      Cumul_xTitle        = cms.string('ApvGain from CondDB'),
+      Cumul_yTitle        = cms.string(' '),        
+      Cumul_NchX          = cms.int32(50),
+      Cumul_LowX          = cms.double(0.5),
+      Cumul_HighX         = cms.double(1.5),
+
+      SummaryOfProfile_description = cms.string('ProfileSummary_ApvGainFromCondDB'),
+      SummaryOfProfile_xTitle      = cms.string('Apv Number'),
+      SummaryOfProfile_yTitle      = cms.string('ApvGain from CondDB'),
+      SummaryOfProfile_NchY        = cms.int32(50),
+      SummaryOfProfile_LowY        = cms.double(0.5),
+      SummaryOfProfile_HighY       = cms.double(1.5),
+
+      Summary_description   = cms.string('Summary_ApvGainFromCondDB'),
+      Summary_xTitle        = cms.string('detId'),
+      Summary_yTitle        = cms.string('ApvGain from CondDB'),
+      Summary_NchY          = cms.int32(50),        
+      Summary_LowY          = cms.double(0.5),
+      Summary_HighY         = cms.double(1.5)
+    
     ),
-    MonitorSiStripPedestal = cms.bool(True),
+    
     # -----
     SiStripQualityDQM_PSet = cms.PSet(
-        Profile_xTitle = cms.string('Strip Number'),
-        CondObj_name = cms.string('quality'),
-        CondObj_fillId = cms.string('onlyProfile'),
-        Summary_HighY = cms.double(100.5),
-        Summary_xTitle = cms.string('detId'),
-        Summary_NchY = cms.int32(100),
-        Summary_description = cms.string('Summary_FractionOfBadStripsFromCondDB'),
-        Profile_description = cms.string('Profile_QualityFlagFromCondDB'),
-        Summary_yTitle = cms.string('Fraction of bad strips from CondDB(%)'),
-        Summary_LowY = cms.double(0.5),
-        Profile_yTitle = cms.string('Quality Flag from CondDB')
+
+      CondObj_name   = cms.string('quality'),
+      CondObj_fillId = cms.string('ProfileAndCumul'),
+      
+      FillSummaryAtLayerLevel           = cms.bool(True),
+
+      Profile_description = cms.string('Profile_QualityFlagFromCondDB'),
+      Profile_xTitle      = cms.string('Strip Number'),
+      Profile_yTitle      = cms.string('Quality Flag from CondDB'),
+      
+      Summary_description = cms.string('Summary_FractionOfBadStripsFromCondDB'),
+      Summary_xTitle      = cms.string('detId'),
+      Summary_yTitle      = cms.string('Fraction of bad strips from CondDB(%)'),
+      Summary_NchY        = cms.int32(100),
+      Summary_LowY        = cms.double(0.5),
+      Summary_HighY       = cms.double(100.5),
+
+      Summary_BadObjects_histo_xTitle      =cms.string('Sub Det And Layer'),
+      
+      Summary_BadModules_histo_name =cms.string('Summary_BadModules_FromCondDB'),
+      Summary_BadModules_histo_yTitle      =cms.string('Number of bad Modules from CondDB'),
+      
+      Summary_BadFibers_histo_name =cms.string('Summary_BadFibers_FromCondDB'),
+      Summary_BadFibers_histo_yTitle      =cms.string('Number of bad Fibers from CondDB'),
+      
+      Summary_BadApvs_histo_name =cms.string('Summary_BadApvs_FromCondDB'),
+      Summary_BadApvs_histo_yTitle      =cms.string('Number of bad Apvs from CondDB'),
+      
+      Summary_BadStrips_histo_name =cms.string('Summary_BadStrips_FromCondDB'),
+      Summary_BadStrips_histo_yTitle      =cms.string('Number of bad Strips from CondDB'),
+      
+      SummaryOfCumul_description   =cms.string('CumulativeSummary_SiStripQualityFromCondDB'),
+      SummaryOfCumul_xTitle        =cms.string('SiStripQualityfrom CondDB'),
+      SummaryOfCumul_yTitle        =cms.string(' '),
+      
+      SummaryOfCumul_NchX          = cms.int32(100),
+      SummaryOfCumul_LowX          = cms.double(0.0),
+      SummaryOfCumul_HighX         = cms.double(100.0)
+      
     ),
-    OutputFileName = cms.string('SiStripMonitorCondDataOnDemandExample.root'),
-    MonitorSiStripApvGain = cms.bool(True),
+    
     # -----
     SiStripLorentzAngleDQM_PSet = cms.PSet(
-        SummaryOfCumul_NchX = cms.int32(50),
-        SummaryOfCumul_xTitle = cms.string('LorentzAngle from CondDB'),
-        CondObj_name = cms.string('lorentzangle'),
-        CondObj_fillId = cms.string('none'),
-        SummaryOfCumul_LowX = cms.double(0.01),
-        SummaryOfCumul_HighX = cms.double(0.06),
-        SummaryOfProfile_yTitle = cms.string('LorentzAngle from CondDB'),
-        SummaryOfProfile_description = cms.string('Summary_LorentzAngleFromCondDB'),
-        SummaryOfCumul_yTitle = cms.string(' '),
-        SummaryOfCumul_description = cms.string('ProfileSummary_LorentzAngleFromCondDB'),
-        SummaryOfProfile_LowY = cms.double(0.01),
-        SummaryOfProfile_xTitle = cms.string('detId'),
-        SummaryOfProfile_NchY = cms.int32(50),
-        SummaryOfProfile_HighY = cms.double(0.06)
+
+      CondObj_name = cms.string('lorentzangle'),
+      CondObj_fillId = cms.string('none'),
+      
+      FillSummaryProfileAtLayerLevel = cms.bool(False),
+      FillCumulativeSummaryAtLayerLevel = cms.bool(True),
+
+      SummaryOfCumul_description = cms.string('ProfileSummary_LorentzAngleFromCondDB'),
+      SummaryOfCumul_xTitle      = cms.string('LorentzAngle from CondDB'),
+      SummaryOfCumul_yTitle      = cms.string(' '),
+      SummaryOfCumul_NchX        = cms.int32(50),      
+      SummaryOfCumul_LowX        = cms.double(0.01),
+      SummaryOfCumul_HighX       = cms.double(0.06),
+      
+      SummaryOfProfile_description = cms.string('Summary_LorentzAngleFromCondDB'),
+      SummaryOfProfile_xTitle      = cms.string('detId'),
+      SummaryOfProfile_yTitle      = cms.string('LorentzAngle from CondDB'),
+      SummaryOfProfile_NchY        = cms.int32(50),
+      SummaryOfProfile_LowY        = cms.double(0.01),
+      SummaryOfProfile_HighY       = cms.double(0.06)     
+      
     ),
-    MonitorSiStripNoise = cms.bool(True),
-    MonitorSiStripQuality = cms.bool(True),
+    
     # -----
     SiStripNoisesDQM_PSet = cms.PSet(
-        Profile_xTitle = cms.string('Strip Number'),
-        Summary_LowY = cms.double(0.0),
-        Summary_xTitle = cms.string('detId'),
-        Cumul_HighX = cms.double(5.0),
-        Cumul_description = cms.string('NoiseFromCondDB'),
-        SummaryOfProfile_yTitle = cms.string('Noise from CondDB(ADC)'),
-        SummaryOfProfile_NchY = cms.int32(50),
-        GainRenormalisation = cms.bool(True),
-        Summary_description = cms.string('Summary_NoiseFromCondDB'),
-        SummaryOfProfile_LowY = cms.double(0.0),
-        Summary_yTitle = cms.string('Noise from CondDB(ADC)'),
-        SummaryOfProfile_HighY = cms.double(5.0),
-        Cumul_NchX = cms.int32(50),
-        CondObj_name = cms.string('noise'),
-        CondObj_fillId = cms.string('ProfileAndCumul'),
-        Cumul_xTitle = cms.string('Noise from CondDB(ADC)'),
-        Summary_HighY = cms.double(5.0),
-        SummaryOfProfile_description = cms.string('ProfileSummary_NoiseFromCondDB'),
-        Summary_NchY = cms.int32(50),
-        Cumul_yTitle = cms.string(' '),
-        Profile_description = cms.string('Profile_NoiseFromCondDB'),
-        SummaryOfProfile_xTitle = cms.string('Strip Number'),
-        Cumul_LowX = cms.double(0.0),
-        Profile_yTitle = cms.string('Noise from CondDB(ADC)')
+
+      CondObj_fillId    = cms.string('onlyProfile'),
+      CondObj_name      = cms.string('noise'),
+
+      GainRenormalisation               = cms.bool(False),
+      
+      FillSummaryAtLayerLevel           = cms.bool(True),
+      FillSummaryProfileAtLayerLevel    = cms.bool(False),
+      FillCumulativeSummaryAtLayerLevel = cms.bool(True),
+      
+      Profile_description               = cms.string('Profile_NoiseFromCondDB'),
+      Profile_xTitle                    = cms.string('Strip Number'),
+      Profile_yTitle                    = cms.string('Noise from CondDB(ADC)'),
+
+      Cumul_description = cms.string('NoiseFromCondDB'),
+      Cumul_xTitle      = cms.string('Noise from CondDB(ADC)'),
+      Cumul_yTitle      = cms.string(' '),
+      Cumul_NchX        = cms.int32(50),
+      Cumul_LowX        = cms.double(0.0),
+      Cumul_HighX       = cms.double(6.0),
+      
+      SummaryOfProfile_description = cms.string('ProfileSummary_NoiseFromCondDB'),
+      SummaryOfProfile_xTitle      = cms.string('Strip Number'),      
+      SummaryOfProfile_yTitle      = cms.string('Noise from CondDB(ADC)'),
+      SummaryOfProfile_NchY        = cms.int32(50),
+      SummaryOfProfile_LowY        = cms.double(0.0),
+      SummaryOfProfile_HighY       = cms.double(6.0),
+
+      Summary_description          = cms.string('Summary_NoiseFromCondDB'),
+      Summary_xTitle               = cms.string('detId'),
+      Summary_yTitle               = cms.string('Noise from CondDB(ADC)'),
+      Summary_NchY                 = cms.int32(50),
+      Summary_LowY                 = cms.double(0.0),
+      Summary_HighY                = cms.double(6.0),
+      
+      SummaryOfCumul_description = cms.string('CumulativeSummary_NoiseFromCondDB'),
+      SummaryOfCumul_xTitle      = cms.string('Noise from CondDB'),
+      SummaryOfCumul_yTitle      = cms.string(' '),
+      SummaryOfCumul_NchX        = cms.int32(50),
+      SummaryOfCumul_LowX        = cms.double(0.0),
+      SummaryOfCumul_HighX       = cms.double(10.0)
+      
     ),
+    
     # -----
     SiStripPedestalsDQM_PSet = cms.PSet(
-        Profile_xTitle = cms.string('Strip Number'),
-        CondObj_name = cms.string('pedestal'),
-        CondObj_fillId = cms.string('onlyProfile'),
-        Summary_HighY = cms.double(1000.0),
-        SummaryOfProfile_yTitle = cms.string('Pedestal from CondDB(ADC)'),
-        SummaryOfProfile_description = cms.string('ProfileSummary_PedestalFromCondDB'),
-        Summary_NchY = cms.int32(100),
-        Summary_description = cms.string('Summary_PedestalFromCondDB'),
-        SummaryOfProfile_LowY = cms.double(0.0),
-        SummaryOfProfile_xTitle = cms.string('Strip Number'),
-        Profile_description = cms.string('Profile_PedestalFromCondDB'),
-        Summary_yTitle = cms.string('Pedestal from CondDB(ADC)'),
-        SummaryOfProfile_NchY = cms.int32(100),
-        SummaryOfProfile_HighY = cms.double(1000.0),
-        Profile_yTitle = cms.string('Pedestal from CondDB(ADC)'),
-        Summary_LowY = cms.double(0.0),
-        Summary_xTitle = cms.string('detId')
-    ),
-    FillConditions_PSet = cms.PSet(
-        Mod_On = cms.bool(False),
-        # *** exclude a subdetector 
-        SubDetectorsToBeExcluded = cms.vstring('none'),
-        ModulesToBeFilled = cms.string('all'),
-        StripQualityLabel = cms.string('test1'),
-        ModulesToBeIncluded = cms.vuint32(), ##e.g. {369120277, 369120278, 369120282}
 
-        ModulesToBeExcluded = cms.vuint32(),
-        # *** exclude OR include a set of modules
-        restrictModules = cms.bool(False),
-        SummaryOnStringLevel_On = cms.bool(False),
-        SummaryOnLayerLevel_On = cms.bool(True)
+      CondObj_fillId       = cms.string('onlyProfile'),
+      CondObj_name         = cms.string('pedestal'),
+
+      FillSummaryAtLayerLevel           = cms.bool(True),
+      FillSummaryProfileAtLayerLevel    = cms.bool(False),
+
+      Profile_description     = cms.string('Profile_PedestalFromCondDB'),
+      Profile_xTitle          = cms.string('Strip Number'),
+      Profile_yTitle          = cms.string('Pedestal from CondDB(ADC)'),
+      
+      SummaryOfProfile_description = cms.string('ProfileSummary_PedestalFromCondDB'),
+      SummaryOfProfile_xTitle = cms.string('Strip Number'),
+      SummaryOfProfile_yTitle = cms.string('Pedestal from CondDB(ADC)'),
+      SummaryOfProfile_NchY   = cms.int32(100),
+      SummaryOfProfile_LowY   = cms.double(0.0),
+      SummaryOfProfile_HighY  = cms.double(1000.0),
+
+      Summary_description     = cms.string('Summary_PedestalFromCondDB'),
+      Summary_xTitle          = cms.string('detId'),
+      Summary_yTitle          = cms.string('Pedestal from CondDB(ADC)'),
+      Summary_NchY            = cms.int32(100),
+      Summary_LowY            = cms.double(0.0),
+      Summary_HighY           = cms.double(1000.0) 
+      
     )
+
 )
 
 
