2aeac7e8c27b389a1316d051efab8a4ea3508fa2
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-02-20 09:26:44 +0000

    fixed math expression

--- PhysicsTools/Utilities/interface/Exponential.h
+++ PhysicsTools/Utilities/interface/Exponential.h
@@ -10,7 +10,7 @@ namespace funct {
     Exponential(const Parameter & l) : lambda(l.ptr()) { }
     Exponential(boost::shared_ptr<double> l) : lambda(l) { }
     Exponential(double l) : lambda(new double(l)) { }
-    double operator()(double x) const { return exp((*lambda)*x); }
+    double operator()(double x) const { return std::exp((*lambda)*x); }
     boost::shared_ptr<double> lambda;
   };
 
