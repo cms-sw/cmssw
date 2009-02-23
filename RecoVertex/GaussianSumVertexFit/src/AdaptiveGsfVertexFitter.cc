b0fa00fcc8e3e0d61fb6f62cd5a1c12c69f35e22
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2009-02-23 14:34:35 +0000

    Correct problem with configuration

--- RecoVertex/GaussianSumVertexFit/src/AdaptiveGsfVertexFitter.cc
+++ RecoVertex/GaussianSumVertexFit/src/AdaptiveGsfVertexFitter.cc
@@ -10,8 +10,7 @@
 AdaptiveGsfVertexFitter::AdaptiveGsfVertexFitter(const edm::ParameterSet& pSet,
 	const LinearizationPointFinder & linP )
 {
-  float theMaxShift = pSet.getParameter<double>("maxDistance"); //0.01
-  int theMaxStep = pSet.getParameter<int>("maxNbrOfIterations"); //10
+
   bool limitComponents_ = pSet.getParameter<bool>("limitComponents");
   bool useSmoothing = pSet.getParameter<bool>("smoothTracks");
 
@@ -21,17 +20,35 @@ AdaptiveGsfVertexFitter::AdaptiveGsfVertexFitter(const edm::ParameterSet& pSet,
     edm::ParameterSet mergerPSet = pSet.getParameter<edm::ParameterSet>("GsfMergerParameters");
     theMerger = new GsfVertexMerger(mergerPSet);
   }
+  VertexSmoother<5> * sm;
+
+  if (useSmoothing) sm  = new GsfVertexSmoother(limitComponents_, &*theMerger) ;
+   else sm = new DummyVertexSmoother<5>();
 
   theFitter = new AdaptiveVertexFitter(
       GeometricAnnealing(),
       linP,
       GsfVertexUpdator(limitComponents_, &*theMerger),
       GsfVertexTrackCompatibilityEstimator(),
-      GsfVertexSmoother(limitComponents_, &*theMerger),
+      *sm,
       MultiPerigeeLTSFactory() );
  
-//   theFitter->setMaximumDistance(theMaxShift);
-//   theFitter->setMaximumNumberOfIterations(theMaxStep);
+  /**
+   *   Reads the configurable parameters.
+   *   \param maxshift if the vertex moves further than this (in cm),
+   *   then we re-iterate.
+   *   \param maxlpshift if the vertex moves further than this,
+   *   then we re-linearize the tracks.
+   *   \param maxstep that's the maximum of iterations that we allow for.
+   *   \param weightthreshold that's the minimum track weight
+   *   for a track to be considered "significant".
+   *   If fewer than two tracks are significant, an exception is thrown.
+   */
+  theFitter->setParameters ( pSet.getParameter<double>("maxshift"),
+                    pSet.getParameter<double>("maxlpshift"),
+                    pSet.getParameter<int>("maxstep"),
+                    pSet.getParameter<double>("weightthreshold") );
+
 }
 
 AdaptiveGsfVertexFitter::AdaptiveGsfVertexFitter(const AdaptiveGsfVertexFitter & original)
