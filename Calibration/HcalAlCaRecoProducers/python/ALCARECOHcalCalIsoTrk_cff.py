37801f2607f9716d78133ef506918638d718d722
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2009-02-23 16:58:48 +0000

    fix MB HF, switch off AlCaRaw in IsoTrack

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalIsoTrk_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalIsoTrk_cff.py
@@ -4,71 +4,10 @@ import FWCore.ParameterSet.Config as cms
 #AlCaReco filtering for HCAL isotrk:
 #------------------------------------------------
 
-from Configuration.StandardSequences.Reconstruction_cff import *
-
-import EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi
-isoTrSiPixelDigis = EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi.siPixelDigis.clone()
-
-from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi import *
-import EventFilter.SiStripRawToDigi.SiStripDigis_cfi
-isoTrSiStripDigis = EventFilter.SiStripRawToDigi.SiStripDigis_cfi.siStripDigis.clone()
-
-import EventFilter.EcalRawToDigiDev.EcalUnpackerData_cfi
-isoTrEcalDigis = EventFilter.EcalRawToDigiDev.EcalUnpackerData_cfi.ecalEBunpacker.clone()
-
-import EventFilter.ESRawToDigi.esRawToDigi_cfi
-isoTrEcalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
-
-import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
-isoTrHcalDigis = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
-
-isoTrSiPixelDigis.InputLabel = 'hltSubdetFED'
-
-isoTrSiStripDigis.ProductLabel = 'hltSiStripRegFED'
-
-isoTrEcalDigis.DoRegional = False
-isoTrEcalDigis.InputLabel = 'hltEcalRegFED'
-isoTrEcalPreshowerDigis.sourceTag = 'hltEcalRegFED'
-
-isoTrHcalDigis.UnpackZDC = cms.untracked.bool(False)
-isoTrHcalDigis.InputLabel = 'hltSubdetFED'
-
-siPixelClusters.src = 'isoTrSiPixelDigis'
-siStripZeroSuppression.RawDigiProducersList = cms.VPSet(
-    cms.PSet(RawDigiProducer = cms.string('isoTrSiStripDigis'), RawDigiLabel = cms.string('VirginRaw')),
-    cms.PSet(RawDigiProducer = cms.string('isoTrSiStripDigis'), RawDigiLabel = cms.string('ProcessedRaw')),
-    cms.PSet(RawDigiProducer = cms.string('isoTrSiStripDigis'), RawDigiLabel = cms.string('ScopeMode'))
-     )
-siStripClusters.DigiProducersList = cms.VPSet(cms.PSet(
-    DigiLabel = cms.string('ZeroSuppressed'),
-    DigiProducer = cms.string('isoTrSiStripDigis')
-),
-    cms.PSet(
-        DigiLabel = cms.string('VirginRaw'),
-        DigiProducer = cms.string('siStripZeroSuppression')
-    ),
-    cms.PSet(
-        DigiLabel = cms.string('ProcessedRaw'),
-        DigiProducer = cms.string('siStripZeroSuppression')
-    ),
-    cms.PSet(
-        DigiLabel = cms.string('ScopeMode'),
-        DigiProducer = cms.string('siStripZeroSuppression')
-    ))
-ecalWeightUncalibRecHit.EBdigiCollection = 'isoTrEcalDigis:ebDigis'
-ecalWeightUncalibRecHit.EEdigiCollection = 'isoTrEcalDigis:eeDigis'
-ecalPreshowerRecHit.ESdigiCollection = 'isoTrEcalPreshowerDigis'
-hbhereco.digiLabel = 'isoTrHcalDigis'
-horeco.digiLabel = 'isoTrHcalDigis'
-hfreco.digiLabel = 'isoTrHcalDigis'
-
 from Calibration.HcalAlCaRecoProducers.alcaisotrk_cfi import *
 from Calibration.HcalAlCaRecoProducers.isoHLT_cfi import *
 
-doIsoTrDigi = cms.Sequence(isoTrSiPixelDigis+isoTrSiStripDigis+isoTrEcalDigis+isoTrEcalPreshowerDigis+isoTrHcalDigis)
-doLocalReco = cms.Sequence(trackerlocalreco+ecalLocalRecoSequence+hbhereco+horeco)
-doGlobalReco = cms.Sequence(offlineBeamSpot+recopixelvertexing*ckftracks)
-seqALCARECOHcalCalIsoTrk = cms.Sequence(isoHLT*doIsoTrDigi*doLocalReco*doGlobalReco*IsoProd)
+seqALCARECOHcalCalIsoTrk = cms.Sequence(isoHLT*IsoProd)
 
 
 
--- Calibration/HcalAlCaRecoProducers/python/isoHLT_NoEcalIsol_cfi.py
+++
@@ -1,9 +0,0 @@
-import HLTrigger.HLTfilters.hltHighLevel_cfi
-
-isoHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
-    HLTPaths = ['CandHLTHcalIsolatedTrackNoEcalIsol'],
-    andOr = cms.bool(True),
-    throw = False #dont throw except on unknown path name 
-)
-
-
