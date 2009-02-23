46f23d0ba679b9e9fa3ba8fead5db5b6e55a36a6
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2009-02-23 16:28:24 +0000

    Improving trend plots + optimization of the code

--- DQM/SiStripMonitorCluster/test/MonitorCluster_RealData_cfg.py
+++ DQM/SiStripMonitorCluster/test/MonitorCluster_RealData_cfg.py
@@ -6,16 +6,12 @@ process = cms.Process("DQMOnlineRealData")
 #--------------------------
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-      '/store/data/Commissioning08/Cosmics/RAW/v1/000/066/993/2A8E332E-769F-DD11-8E22-001617DBD224.root'
-#      '/store/data/Commissioning08/Cosmics/RAW/v1/000/066/993/4A64178D-799F-DD11-9C47-000423D996B4.root'
-#       'file:/afs/cern.ch/user/v/venturia/GlobalRuns/scratch/bigevents_66668.root'
-#       '/store/data/Commissioning08/Cosmics/RAW/v1/000/066/668/48F6B6B5-519C-DD11-B32A-000423D6B444.root'
 #        '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/647/0000721C-35A3-DD11-9132-001D09F291D7.root'
-#       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/647/22CBBD11-07A3-DD11-9DFB-001D09F2447F.root'
+       '/store/data/Commissioning08/Cosmics/RAW/v1/000/067/647/22CBBD11-07A3-DD11-9DFB-001D09F2447F.root'
     )
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(2000)
 )
 
 #-------------------------------------------------
@@ -29,15 +25,6 @@ process.MessageLogger = cms.Service("MessageLogger",
     cout = cms.untracked.PSet(threshold = cms.untracked.string('ERROR')),
     destinations = cms.untracked.vstring('cout')
 )
-#-------------------------------------------------
-# Magnetic Field
-#-------------------------------------------------
-# 0T field
-process.load("Configuration.StandardSequences.MagneticField_0T_cff")
-# 3.8T field
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-process.prefer("VolumeBasedMagneticFieldESProducer")
-
 
 #-------------------------------------------------
 # Geometry
@@ -48,8 +35,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 # Calibration
 #-------------------------------------------------
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "CRAFT_V3P::All"
+process.GlobalTag.globaltag = "CRAFT_30X::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #-----------------------
@@ -71,8 +57,10 @@ process.DQMStore = cms.Service("DQMStore",
 # SiStrip MonitorCluster
 #--------------------------
 process.load("DQM.SiStripMonitorCluster.SiStripMonitorCluster_cfi")
-process.SiStripMonitorCluster.OutputMEsInRootFile = True
+process.SiStripMonitorCluster.CreateTrendMEs = True
 process.SiStripMonitorCluster.SelectAllDetectors = True
+process.SiStripMonitorCluster.TProfTotalNumberOfClusters.subdetswitchon = True
+process.SiStripMonitorCluster.OutputMEsInRootFile = True
 process.SiStripMonitorCluster.OutputFileName = 'SiStripMonitorCluster.root'
 
 process.outP = cms.OutputModule("AsciiOutputModule")
