e89ee442dca76d785cc51d2eb39cdd339fc52010
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2009-02-23 15:20:19 +0000

    new L3 highest-pt skimming module

--- RecoMuon/L3MuonProducer/src/SealModule.cc
+++ RecoMuon/L3MuonProducer/src/SealModule.cc
@@ -3,8 +3,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoMuon/L3MuonProducer/src/L3MuonProducer.h"
+#include "RecoMuon/L3MuonProducer/src/L3TkMuonProducer.h"
 #include "RecoMuon/L3MuonProducer/src/L3MuonCandidateProducer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(L3MuonProducer);
+DEFINE_ANOTHER_FWK_MODULE(L3TkMuonProducer);
 DEFINE_ANOTHER_FWK_MODULE(L3MuonCandidateProducer);
