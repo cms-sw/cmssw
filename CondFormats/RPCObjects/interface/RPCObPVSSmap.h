1a1aabfb44c9053598ba565439781a4112b7ec3c
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-02-24 16:22:35 +0000

    new version

--- CondFormats/RPCObjects/interface/RPCObPVSSmap.h
+++ CondFormats/RPCObjects/interface/RPCObPVSSmap.h
@@ -7,15 +7,15 @@
 class RPCObPVSSmap {
     public:
       struct Item {
-        std::string since;
+        int since;
         int dpid;
-        std::string region;
-        std::string ring;
-        std::string station;
-        std::string sector;
-        std::string layer;
-        std::string subsector;
-        std::string suptype;
+        int region;
+        int ring;
+        int station;
+        int sector;
+        int layer;
+        int subsector;
+        int suptype;
     };
     RPCObPVSSmap(){}
     virtual ~RPCObPVSSmap(){}
