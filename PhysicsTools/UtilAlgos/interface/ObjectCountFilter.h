a0c2be34ee76ecf0ada7712b8bf72dc972dd98d4
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-02-24 15:54:51 +0000

    restored working code

--- PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
+++ PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h
@@ -1,15 +1,14 @@
-#ifndef PhysicsTools_UtilAlgos_ObjectCountFilter_h
-#define PhysicsTools_UtilAlgos_ObjectCountFilter_h
-
+#ifndef RecoAlgos_ObjectCountFilter_h
+#define RecoAlgos_ObjectCountFilter_h
 /** \class ObjectCountFilter
  *
  * Filters an event if a collection has at least N entries
  * 
  * \author Luca Lista, INFN
  *
- * \version $Revision: 1.11 $
+ * \version $Revision: 1.10 $
  *
- * $Id: ObjectCountFilter.h,v 1.11 2009/01/22 13:35:13 veelken Exp $
+ * $Id: ObjectCountFilter.h,v 1.10 2007/06/18 18:33:52 llista Exp $
  *
  */
 
@@ -19,15 +18,33 @@
 #include "FWCore/ParameterSet/interface/InputTag.h"
 #include "PhysicsTools/UtilAlgos/interface/ParameterAdapter.h"
 #include "PhysicsTools/UtilAlgos/interface/CollectionFilterTrait.h"
-#include "PhysicsTools/UtilAlgos/interface/EventSelectorAdapter.h"
-#include "PhysicsTools/UtilAlgos/interface/ObjectCountEventSelector.h"
 
 template<typename C, 
 	 typename S = AnySelector,
 	 typename N = MinNumberSelector,
 	 typename CS = typename helper::CollectionFilterTrait<C, S, N>::type>
-struct ObjectCountFilter {
-  typedef EventSelectorAdapter< ObjectCountEventSelector<C, S, N, CS> > type;
+class ObjectCountFilter : public edm::EDFilter {
+public:
+  /// constructor 
+  explicit ObjectCountFilter( const edm::ParameterSet & cfg ) :
+    src_( cfg.template getParameter<edm::InputTag>( "src" ) ),
+    select_( reco::modules::make<S>( cfg ) ),
+    sizeSelect_( reco::modules::make<N>( cfg ) ) {
+  }
+  
+private:
+  /// process one event
+  bool filter( edm::Event& evt, const edm::EventSetup& ) {
+    edm::Handle<C> source;
+    evt.getByLabel( src_, source );
+    return CS::filter( * source, select_, sizeSelect_ );
+  }
+  /// source collection label
+  edm::InputTag src_;
+  /// object filter
+  S select_;
+  /// minimum number of entries in a collection
+  N sizeSelect_;
 };
 
 #endif
