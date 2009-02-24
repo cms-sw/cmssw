b1caa05f2607089bff6cc9dc2871c6d5d3ae6d26
Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch>
2009-02-24 14:10:41 +0000

    DT calibration needs as many muons as possible, which in cosmic rays means standAloneMuons

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
@@ -14,7 +14,8 @@ import Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi
 ALCARECOMuAlCalIsolatedMu = Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi.AlignmentMuonSelector.clone(
     src = cms.InputTag("muons"),
     filter = True, # not strictly necessary, but provided for symmetry with MuAlStandAloneCosmics
-    nHitMinGB = 1, # muon collections now merge globalMuons, standAlone, and trackerMuons: this stream has always assumed globalMuons
+# DT calibration needs as many muons with DIGIs as possible, which in cosmic ray runs means standAloneMuons
+#    nHitMinGB = 1, # muon collections now merge globalMuons, standAlone, and trackerMuons: this stream has always assumed globalMuons
     )
 
 seqALCARECOMuAlCalIsolatedMu = cms.Sequence(ALCARECOMuAlCalIsolatedMuHLT + ALCARECOMuAlCalIsolatedMu)
