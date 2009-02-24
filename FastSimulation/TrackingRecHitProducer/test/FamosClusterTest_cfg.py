79cb97b6cf517479e00dd2c826a70b24b1bd2df9
dorbaker <sha1-c0c861e1621fbd0445e7a5339f107fcc8dbfe745@cern.ch>
2009-02-24 11:39:09 +0000

    Updated for 3_1_X

--- FastSimulation/TrackingRecHitProducer/test/FamosClusterTest_cfg.py
+++ FastSimulation/TrackingRecHitProducer/test/FamosClusterTest_cfg.py
@@ -5,16 +5,19 @@ process = cms.Process("FamosClusterTest")
 process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
 
 # Famos Common inputs 
-process.load("FastSimulation.Configuration.CommonInputsFake_cff")
+process.load("FastSimulation.Configuration.CommonInputs_cff")
 process.load("FastSimulation.Configuration.FamosSequences_cff")
+process.load("FastSimulation.Configuration.mixNoPU_cfi")
 
 # Magnetic Field (new mapping, 3.8 and 4.0T)
-# include "Configuration/StandardSequences/data/MagneticField_38T.cff"
 process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.famosSimHits.SimulateCalorimetry = False
+process.famosSimHits.SimulateTracking = True
 
 process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
 process.load("FastSimulation.TrackingRecHitProducer.SiClusterTranslator_cfi")
-process.load("Configuration.StandardSequences.FakeConditions_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("RecoTracker.Configuration.RecoTracker_cff")
 
 #CPEs
@@ -28,16 +31,12 @@ process.newClusters.stripClusters = cms.InputTag('siClusterTranslator')
 process.newMeasurementTracker.StripCPE = cms.string('FastStripCPE')
 process.newMeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
 
-
 #Second Step
 process.load("RecoTracker.IterativeTracking.SecStep_cff")
 process.secPixelRecHits.CPE = cms.string('FastPixelCPE')
 process.secStripRecHits.StripCPE = cms.string('FastStripCPE')
 process.secMeasurementTracker.StripCPE = cms.string('FastStripCPE')
 process.secMeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
-#process.secClusters.pixelClusters = cms.InputTag('siClusterTranslator')
-#process.secClusters.stripClusters = cms.InputTag('siClusterTranslator')
-
 
 #Third Step
 process.load("RecoTracker.IterativeTracking.ThStep_cff")
@@ -48,8 +47,17 @@ process.thMeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
 
 #Fourth Step
 process.load("RecoTracker.IterativeTracking.PixelLessStep_cff")
+process.fourthPixelRecHits.CPE = cms.string('FastPixelCPE')
+process.fourthStripRecHits.StripCPE = cms.string('FastStripCPE')
 process.fourthMeasurementTracker.StripCPE = cms.string('FastStripCPE')
 process.fourthMeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
+
+#Fifth Step
+process.load("RecoTracker.IterativeTracking.TobTecStep_cff")
+process.fifthPixelRecHits.CPE = cms.string('FastPixelCPE')
+process.fifthStripRecHits.StripCPE = cms.string('FastStripCPE')
+process.fifthMeasurementTracker.StripCPE = cms.string('FastStripCPE')
+process.fifthMeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
 
 #Strips
 process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi")
@@ -61,7 +69,7 @@ process.load("RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff")
 process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
 process.siPixelRecHits.src = cms.InputTag('siClusterTranslator')
 process.siPixelRecHits.CPE = cms.string('FastPixelCPE')
-process.load("RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cfi")
+process.load("RecoTracker.TkSeedGenerator.GlobalSeedsFromTripletsWithVertices_cff")
 process.globalSeedsFromTripletsWithVertices.TTRHBuilder = cms.string("FastPixelCPE")
 
 #Transient Rec Hits
@@ -88,27 +96,21 @@ process.MeasurementTracker.StripCPE = cms.string('FastStripCPE')
 process.MeasurementTracker.PixelCPE = cms.string('FastPixelCPE')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(10)
 )
-
-### standard includes
-process.load("Configuration.StandardSequences.RawToDigi_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("SimGeneral.MixingModule.mixNoPU_cfi")
-
-### validation-specific includes
-#process.load("SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi")
-process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
-process.load("Validation.RecoTrack.cuts_cff")
-#process.load("Validation.RecoTrack.MultiTrackValidator_cff")
-process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
 
 process.source = cms.Source("PoolSource",
                             debugFlag = cms.untracked.bool(True),
                             debugVebosity = cms.untracked.uint32(10),
                             fileNames = cms.untracked.vstring(
-    'Your FastSim Input File'
+    #'/store/relval/CMSSW_3_0_0_pre2/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_V9_v2/0001/4C3E7FD2-1CB4-DD11-BFAB-0016177CA7A0.root'
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_30X_v1/0001/5CD601D0-5EF4-DD11-BC14-000423D9A212.root'
+    '/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/2E564F0D-4BF4-DD11-9983-00304879FBB2.root',
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/4CF899E9-4AF4-DD11-9CC9-000423D9970C.root',
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/948D4DFA-49F4-DD11-AD18-0030487D0D3A.root',
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/E4EEE43D-4AF4-DD11-9E76-001D09F2AF96.root',
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/E8ED1E7B-4DF4-DD11-9A02-001617C3B6CC.root',
+    #'/store/relval/CMSSW_3_1_0_pre1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/ECEA3F75-5EF4-DD11-AF3B-001617E30D4A.root'
     )
                             )
 
@@ -118,7 +120,7 @@ process.FirstSecondTrackMerging = cms.EDFilter("QualityFilter",
                                                )
 
 
-process.FirstSecondThirdTrackMerging = cms.EDFilter("QualityFilter",
+process.FirstSecondThirdFourthTrackMerging = cms.EDFilter("QualityFilter",
                                                     TrackQuality = cms.string('highPurity'),
                                                     recTracks = cms.InputTag("generalTracks")
                                                     )
@@ -128,13 +130,15 @@ process.Output = cms.OutputModule("PoolOutputModule",
                                   outputCommands = cms.untracked.vstring('drop *',
                                                                          'keep *_*_*_FamosClusterTest',
                                                                          ),
-                                  fileName = cms.untracked.string('Your Output File')
+                                  fileName = cms.untracked.string('FastSim_FullTracking_committry_3_1_0_pre1.root')
                                   )
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-#process.MessageLogger.destinations = ['detailedInfoFullTk.txt']
+process.MessageLogger.destinations = ['detailedInfoFullTk.txt']
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.Path = cms.Path(process.siClusterTranslator*process.siPixelRecHits*process.siStripMatchedRecHits*process.ckftracks*process.FirstSecondThirdTrackMerging)
+#note: Include process.mix to run on a FastSim RelVal file
+#note: Include process.famosWithTrackerHits to run straight off a FullSim file
+process.Path = cms.Path(process.mix*process.siClusterTranslator*process.siPixelRecHits*process.siStripMatchedRecHits*process.iterTracking*process.trackCollectionMerging*process.newCombinedSeeds*process.FirstSecondThirdFourthTrackMerging)
 process.o = cms.EndPath(process.Output)
 process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 #process.FamosRecHitAnalysis.UseCMSSWPixelParametrization = False
