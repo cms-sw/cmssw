88ab91741cbc33425cbeaa37f442fa1de9310128
Thiago Tomei Fernandez <sha1-796b2659f539c78d0b46c6e2488664232960deff@cern.ch>
2009-02-24 19:15:12 +0000

    Moving AlpgenInterface to the 3_1_x generator scheme

--- GeneratorInterface/AlpgenInterface/test/AlpgenExtractor.cc
+++ GeneratorInterface/AlpgenInterface/test/AlpgenExtractor.cc
@@ -8,6 +8,7 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Run.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
---
+++ GeneratorInterface/AlpgenInterface/test/testAlpgenComplete_cfg.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env cmsRun
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("GEN")
+
+process.source = cms.Source("AlpgenSource",
+        fileNames = cms.untracked.vstring('file:w2j') 
+)
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+
+process.load("Configuration.StandardSequences.Services_cff")
+
+# Setup RandomNumberGeneratorService so it provides
+# random numbers to the Alpgen Producer.
+process.RandomNumberGeneratorService.generator = cms.PSet(
+        initialSeed = cms.untracked.uint32(123456789),
+        engineName = cms.untracked.string('HepJamesRandom')
+)
+
+# The Alpgen Producer.
+# Change the jet matching parameters as you see fit.
+process.load("GeneratorInterface.AlpgenInterface.generator_cfi")
+process.generator.jetMatching.applyMatching = True
+process.generator.jetMatching.exclusive = True
+process.generator.jetMatching.etMin = 25.0
+process.generator.jetMatching.drMin = 0.7
+
+process.p0 = cms.Path(process.generator)
+
+# Alternatively, you may also want to use these,
+# if you want a more complete generation.
+
+#process.load("Configuration.StandardSequences.Generator_cff")
+#process.load("Configuration.StandardSequences.VtxSmearedGauss_cff")
+
+#process.VtxSmeared.src = 'generator'
+#process.genEventWeight.src = 'generator'
+#process.genEventScale.src = 'generator'
+#process.genEventPdfInfo.src = 'generator'
+#process.genEventProcID.src = 'generator'
+#process.genParticles.src = 'generator'
+#process.genParticleCandidates.src = 'generator'
+
+# Needed for the SIM step.
+#process.g4SimHits.Generator.HepMCProductLabel = 'generator'
+#process.mergedtruth.HepMCDataLabels.append('generator')
+
+#process.p0 = (process.generator * process.pgen)
+
+process.load("Configuration.EventContent.EventContent_cff")
+
+process.GEN = cms.OutputModule("PoolOutputModule",
+	process.FEVTSIMEventContent,
+	dataset = cms.untracked.PSet(dataTier = cms.untracked.string('GEN')),
+	SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('p0')),
+	fileName = cms.untracked.string('test.root')
+)
+process.GEN.outputCommands.append("keep *_generator_*_*")
+
+process.outpath = cms.EndPath(process.GEN)
