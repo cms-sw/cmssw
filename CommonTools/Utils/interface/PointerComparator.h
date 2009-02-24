faab8bc24f6840c5487024a925896ef7ad7dd6ae
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-02-24 15:01:17 +0000

    migration from PhysicsTools/Utilities

---
+++ CommonTools/Utils/interface/PointerComparator.h
@@ -0,0 +1,30 @@
+#ifndef CommonTools_Utils_PointerComparator_h
+#define CommonTools_Utils_PointerComparator_h
+/** \class PoinetComparator
+ *
+ * adapt a comparator to take pointers as arguments
+ * 
+ * \author Luca Lista, INFN
+ *
+ * \version $Revision: 1.1 $
+ *
+ * $Id: PointerComparator.h,v 1.1 2007/04/23 20:43:09 llista Exp $
+ *
+ */
+#include "FWCore/Utilities/interface/EDMException.h"
+
+template<typename C>
+struct PointerComparator {
+  typedef typename C::first_argument_type first_argument_type;
+  typedef typename C::second_argument_type second_argument_type;
+    bool operator()( const first_argument_type * t1, const second_argument_type * t2 ) const {
+      if ( t1 == 0 || t2 == 0 )
+	throw edm::Exception( edm::errors::NullPointerError )
+	  << "PointerComparator: passed null pointer."; 
+	return cmp( *t1, *t2 );
+    }
+  C cmp;
+};
+
+
+#endif
---
+++ CommonTools/Utils/interface/deltaR.h
@@ -0,0 +1,41 @@
+#ifndef CommonTools_Utils_deltaR_h
+#define CommonTools_Utils_deltaR_h
+/* functions to compute deltaR
+ *
+ * Ported from original code in RecoJets 
+ * by Fedor Ratnikov, FNAL
+ */
+#include "CommonTools/Utils/interface/deltaPhi.h"
+#include <cmath>
+
+namespace reco {
+
+  inline double deltaR2(double eta1, double phi1, double eta2, double phi2) {
+    double deta = eta1 - eta2;
+    double dphi = deltaPhi(phi1, phi2);
+    return deta*deta + dphi*dphi;
+  }
+  
+  inline double deltaR(double eta1, double phi1, double eta2, double phi2) {
+    return sqrt(deltaR2 (eta1, phi1, eta2, phi2));
+  }
+  
+  template<typename T1, typename T2>
+  double deltaR2(const T1 & t1, const T2 & t2) {
+    return deltaR2(t1.eta(), t1.phi(), t2.eta(), t2.phi());
+  } 
+  
+  template<typename T1, typename T2>
+  double deltaR(const T1 & t1, const T2 & t2) {
+    return deltaR(t1.eta(), t1.phi(), t2.eta(), t2.phi());
+  } 
+
+}
+
+template<typename T1, typename T2 = T1>
+struct DeltaR {
+  double operator()( const T1 & t1, const T2 & t2 ) const {
+    return reco::deltaR(t1, t2);
+  }
+};
+#endif
