7213d21bbe40c6720a22ddd867e860259e18428d
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2009-02-24 19:59:39 +0000

    remove usage of aparently obsolete (and removed) header files

--- DQMOffline/Trigger/src/SealModule.cc
+++ DQMOffline/Trigger/src/SealModule.cc
@@ -1,15 +1,12 @@
-// $Id: SealModule.cc,v 1.13 2008/11/04 22:51:22 rekovic Exp $
+// $Id: SealModule.cc,v 1.14 2009/02/16 06:53:31 bachtis Exp $
 
 #include "DQMOffline/Trigger/interface/FourVectorHLTClient.h"
 #include "DQMOffline/Trigger/interface/FourVectorHLTOffline.h"
 #include "DQMOffline/Trigger/interface/EgammaHLTOffline.h"
 #include "DQMOffline/Trigger/interface/EgHLTOfflineClient.h"
+
 #include "DQMOffline/Trigger/interface/HLTTauRefProducer.h"
-#include "DQMOffline/Trigger/interface/HLTTauL1DQMOfflineSource.h"
 #include "DQMOffline/Trigger/interface/HLTTauDQMOfflineSource.h"
-#include "DQMOffline/Trigger/interface/HLTTauCaloDQMOfflineSource.h"
-#include "DQMOffline/Trigger/interface/HLTTauTrkDQMOfflineSource.h"
-#include "DQMOffline/Trigger/interface/HLTTauElDQMOfflineSource.h"
 #include "DQMOffline/Trigger/interface/HLTTauPostProcessor.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
