4d9ef83be817c838cc5d329903630519443fb7bf
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2009-02-25 17:37:58 +0000

    fixed generator

--- FastSimulation/Configuration/test/IntegrationTestFake_cfg.py
+++ FastSimulation/Configuration/test/IntegrationTestFake_cfg.py
@@ -11,7 +11,7 @@ process.maxEvents = cms.untracked.PSet(
 process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
 
 # Generate H -> ZZ -> l+l- l'+l'- (l,l'=e or mu), with mH=180GeV/c2
-process.load("FastSimulation.Configuration.HZZllll_cfi")
+process.load("Configuration.Generator.H200ZZ4L_cfi")
 # Generate ttbar events
 #  process.load("FastSimulation/Configuration/ttbar_cfi")
 # Generate multijet events with different ptHAT bins
