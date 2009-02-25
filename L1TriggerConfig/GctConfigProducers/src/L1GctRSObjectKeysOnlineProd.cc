44397a699593c2e062610e56b72da097a4b5eded
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-02-25 16:59:28 +0000

    test job runs

--- L1TriggerConfig/GctConfigProducers/src/L1GctRSObjectKeysOnlineProd.cc
+++ L1TriggerConfig/GctConfigProducers/src/L1GctRSObjectKeysOnlineProd.cc
@@ -1,34 +1,29 @@
 #include "CondTools/L1Trigger/interface/L1ObjectKeysOnlineProdBase.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-class L1GctRSKeysOnlineProd : public L1ObjectKeysOnlineProdBase {
+class L1GctRSObjectKeysOnlineProd : public L1ObjectKeysOnlineProdBase {
    public:
-      L1GctRSKeysOnlineProd(const edm::ParameterSet& iConfig) ;
-      ~L1GctRSKeysOnlineProd() {}
+      L1GctRSObjectKeysOnlineProd(const edm::ParameterSet& iConfig) ;
+      ~L1GctRSObjectKeysOnlineProd() {}
 
       virtual void fillObjectKeys( ReturnType pL1TriggerKey ) ;
    private:
       bool m_enableL1GctChannelMask ;
 };
 
-L1GctRSKeysOnlineProd::L1GctRSKeysOnlineProd(const edm::ParameterSet& iConfig)
+L1GctRSObjectKeysOnlineProd::L1GctRSObjectKeysOnlineProd(const edm::ParameterSet& iConfig)
    : L1ObjectKeysOnlineProdBase( iConfig ),
      m_enableL1GctChannelMask( iConfig.getParameter< bool >( "enableL1GctChannelMask" ) )
 {
 }
 
 void
-L1GctRSKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
+L1GctRSObjectKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
 {
    if( m_enableL1GctChannelMask )
    {
       // Execute SQL queries to get data from OMDS (using key) and make C++ object.
       // Example: SELECT A_KEY FROM CMS_XXX.XXX_RUN_SETTINGS_KEYS_CURRENT
-
-     std::vector< std::string > columns;
-     columns.push_back( "GCT_EM_MASK_CURRENT" );
-     columns.push_back( "GCT_RGN_MASK_CURRENT_KEY" );
-     columns.push_back( "GCT_ESUM_MASK_CURRENT_KEY" );
 
       l1t::OMDSReader::QueryResults objectKeyResults =
          m_omdsReader.basicQuery(
@@ -59,4 +54,4 @@ L1GctRSKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
    }
 }
 
-DEFINE_FWK_EVENTSETUP_MODULE(L1GctRSKeysOnlineProd);
+DEFINE_FWK_EVENTSETUP_MODULE(L1GctRSObjectKeysOnlineProd);
