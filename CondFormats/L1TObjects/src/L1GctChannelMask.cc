83f017d2dcd49581f74192d898f8985ffddfd53d
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-02-26 16:56:54 +0000

    add energy sum masks

--- CondFormats/L1TObjects/src/L1GctChannelMask.cc
+++ CondFormats/L1TObjects/src/L1GctChannelMask.cc
@@ -11,20 +11,68 @@ L1GctChannelMask::L1GctChannelMask() {
   }
 }
 
+
 void L1GctChannelMask::maskEmCrate(unsigned crate) {
   if (crate < 18) emCrateMask_[crate] = true;
 }
 
+
 void L1GctChannelMask::maskRegion(unsigned ieta, unsigned iphi) {
   if (ieta < 22 && iphi < 18) regionMask_[ieta][iphi] = true;
 }
+
+
+void L1GctChannelMask::maskTotalEt(unsigned ieta) {
+  if (ieta < 22) tetMask_[ieta] = true;
+}
+
+
+void L1GctChannelMask::maskMissingEt(unsigned ieta) {
+  if (ieta < 22) metMask_[ieta] = true;
+}
+
+
+void L1GctChannelMask::maskTotalHt(unsigned ieta) {
+  if (ieta < 22) htMask_[ieta] = true;
+}
+
+
+void L1GctChannelMask::maskMissingHt(unsigned ieta) {
+  if (ieta < 22) mhtMask_[ieta] = true;
+}
+
 
 bool L1GctChannelMask::emCrateMask(unsigned crate) const {
   if (crate < 18) { return emCrateMask_[crate]; }
   else return true;
 }
 
+
 bool L1GctChannelMask::regionMask(unsigned ieta, unsigned iphi) const {
   if (ieta < 22 && iphi < 18) { return regionMask_[ieta][iphi]; }
+  else return true;
+}
+
+
+bool L1GctChannelMask::totalEtMask(unsigned ieta) const {
+  if (ieta < 22) return tetMask_[ieta];
+  else return true;
+}
+
+
+bool L1GctChannelMask::missingEtMask(unsigned ieta) const {
+  if (ieta < 22) return metMask_[ieta];
+  else return true;
+}
+
+
+bool L1GctChannelMask::totalHtMask(unsigned ieta) const {
+  if (ieta < 22) return htMask_[ieta];
+  else return true;
+}
+
+
+bool L1GctChannelMask::missingHtMask(unsigned ieta) const {
+  if (ieta < 22) return mhtMask_[ieta];
   else return true;
 }
