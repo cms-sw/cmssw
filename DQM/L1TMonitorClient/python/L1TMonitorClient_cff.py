c83db0b569ec8f21c2ef6dc871398bc6520fe103
Darren Michael Puigh <sha1-c46d72cb3e01124e714a162bae3295f66e002006@cern.ch>
2009-02-26 22:22:09 +0000

    temporary disable qtests

--- DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TMonitorClient_cff.py
@@ -3,9 +3,11 @@ import FWCore.ParameterSet.Config as cms
 #include "DQM/L1TMonitorClient/data/L1TDTTPGClient.cff"
 from DQM.L1TMonitorClient.L1TEventInfoClient_cff import *
 from DQM.L1TMonitorClient.L1TGMTClient_cff import *
+from DQM.L1TMonitorClient.L1TGTClient_cff import *
 from DQM.L1TMonitorClient.L1TGCTClient_cff import *
+from DQM.L1TMonitorClient.L1TRCTClient_cff import *
 from DQM.L1TMonitorClient.L1TDTTFClient_cff import *
- #from DQM.L1TMonitorClient.L1TCSCTFClient_cff import * 
+from DQM.L1TMonitorClient.L1TCSCTFClient_cff import * 
  # out because looking for lots of ME which cannot be found!
 from DQM.L1TMonitorClient.L1TDEMONClient_cff import *
 from DQM.L1TMonitorClient.L1TRPCTFClient_cff import *
@@ -17,5 +19,6 @@ from DQMServices.Components.DQMEnvironment_cfi import *
 #
 
 #l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tcsctfseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
-l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
+#l1tmonitorClient = cms.Path(l1tgmtseqClient*l1tgtseqClient*l1tcsctfseqClient*l1tdttpgseqClient*l1trpctfseqClient*l1tdemonseqClient*l1tGctseqClient*l1tRctseqClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
+l1tmonitorClient = cms.Path(l1tgmtClient*l1tcsctfClient*l1tdttpgClient*l1trpctfClient*l1tdemonseqClient*l1tGctClient*l1tEventInfoseqClient*dqmEnv*dqmSaver)
 
