bcb58d1e432d47405064b487dea9ce667aeaa421
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-02-26 18:11:04 +0000

    - extended the tau benchmark. now making use of Comparator2, and of the new version of the GenericBenchmark

--- Validation/RecoParticleFlow/Benchmarks/TauBenchmarkGeneric/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/TauBenchmarkGeneric/plot.C
@@ -1,26 +1,42 @@
 {
+  
+  gROOT->LoadMacro("../Tools/NicePlot.C");
+  InitNicePlot();
+  gROOT->LoadMacro("../Tools/Comparator2.C");
+  //  Manager::Init();
 
-gROOT->LoadMacro("../Tools/NicePlot.C");
-InitNicePlot();
+  Comparator comp("benchmark.root",
+		  "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen",
+		  "benchmark.root",
+		  "DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen");
+  comp.SetStyles(s1, s2);
 
-TFile f("benchmark.root");
+  Comparator::Mode mode = Comparator::SCALE;
+  
+  TCanvas c1("c1", "Tau benchmark");
+  FormatPad( &c1, false ); 
+  comp.Draw("DeltaEt", mode);
+  gPad->SaveAs("tauBenchmarkGeneric.png");
 
+  TCanvas c2("c2", "Eta resolution");
+  FormatPad( &c2, false );
+  comp.Draw("DeltaEta", mode);
+  gPad->SaveAs("deltaEta.png");
 
-TCanvas c1;
-FormatPad( &c1, false );
+  TCanvas c3("c3", "Phi resolution");
+  FormatPad( &c3, false );
+  comp.Draw("DeltaPhi", mode);
+  gPad->SaveAs("deltaPhi.png");
 
-f.cd("DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen");
-TH1F* hpf = (TH1F*) gDirectory.Get("DeltaEt");
-hpf.Rebin(2);
-FormatHisto(hpf, s1);
-hpf->SetStats(1111);
-hpf.Draw();
-f.cd("DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen");
-TH1F* hcalo = (TH1F*) gDirectory.Get("DeltaEt");
-hcalo.Rebin(2);
-FormatHisto(hcalo, s2);
-hcalo.Draw("same");
+  TCanvas c4("c4", "Efficiency");
+  FormatPad( &c4, false );
+  unsigned rebin = 10;
 
-gPad->SaveAs("tauBenchmarkGeneric.png");
-
+  Comparator comp2("benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen",
+		   "benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen" ); 
+  comp2.SetStyles(s1, s2);
+  comp2.Draw("EtSeen","EtGen", Comparator::EFF);
+  gPad->SaveAs("efficiency_vs_pT.png");
 }
