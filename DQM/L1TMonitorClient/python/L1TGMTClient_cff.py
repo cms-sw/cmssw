60ec3a8ca758cdd8607c6dd38c3ac81859c555e7
Darren Michael Puigh <sha1-c46d72cb3e01124e714a162bae3295f66e002006@cern.ch>
2009-02-26 22:19:38 +0000

    added quality tester

--- DQM/L1TMonitorClient/python/L1TGMTClient_cff.py
+++ DQM/L1TMonitorClient/python/L1TGMTClient_cff.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.L1TMonitorClient.L1TGMTClient_cfi import *
-l1tgmtseqClient = cms.Sequence(l1tgmtClient)
+l1tgmtqTester = cms.EDFilter("QualityTester",
+    qtList = cms.untracked.FileInPath('DQM/L1TMonitorClient/data/L1TGMT_QualityTests.xml'),
+    QualityTestPrescaler = cms.untracked.int32(1),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
 
+
+l1tgmtseqClient = cms.Sequence(l1tgmtqTester*l1tgmtClient)
