200027bffa6f7749b9d9851543397cadc21fa0d9
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-02-26 19:41:51 +0000

    Tracking as in 1_8_4 with 1_8_4 seeding and only ctfWithMaterialTracks

---
+++ SLHCUpgradeSimulations/Geometry/python/oldTracking.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoTracker.Configuration.RecoTracker_cff import *
+
+from RecoTracker.CkfPattern.CkfTrackCandidates_cff import *
+
+ckfTrackCandidates.doSeedingRegionRebuilding = False
+#ckfTrackCandidates.TrajectoryBuilder = 'CkfTrajectoryBuilder'
+ckfTrackCandidates.TrajectoryBuilder = 'GroupedCkfTrajectoryBuilder'
+#ckfTrackCandidates.SeedProducer = 'newCombinedSeeds'
+ckfTrackCandidates.useHitsSplitting = False
+#GroupedCkfTrajectoryBuilder.bestHitOnly = False
+
+from RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cff import *
+ctfWithMaterialTracks.Fitter = 'KFFittingSmoother'
+#ctfWithMaterialTracks.Propagator = 'PropagatorWithMaterial'
+
+from RecoTracker.TkSeedGenerator.GlobalMixedSeeds_cfi import *
+from RecoTracker.TkSeedGenerator.GlobalPixelSeeds_cfi import *
+ckfTrackCandidates.SeedProducer = 'globalMixedSeeds'
+
+oldTracking = cms.Sequence(globalMixedSeeds*globalPixelSeeds*ckfTrackCandidates*ctfWithMaterialTracks)
+
+from Configuration.StandardSequences.RawToDigi_cff import siPixelDigis,SiStripRawToDigis
+from Configuration.StandardSequences.Reconstruction_cff import trackerlocalreco
+
+fullOldTracking = cms.Path(siPixelDigis+SiStripRawToDigis
+                              +trackerlocalreco
+                              +oldTracking)
