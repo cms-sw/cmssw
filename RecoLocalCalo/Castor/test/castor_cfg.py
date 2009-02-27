70274a9d55bd18a856ee653771952bb7f45932ca
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2009-02-27 16:14:57 +0000

    new structure

--- RecoLocalCalo/Castor/test/castor_cfg.py
+++ RecoLocalCalo/Castor/test/castor_cfg.py
@@ -5,28 +5,20 @@ process = cms.Process("CastorProducts")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
 process.source = cms.Source("PoolSource",
     fileNames = 
 cms.untracked.vstring('file:digiout.root')
 )
 
-process.fromRecHits = cms.EDFilter("Castor",
-    FullReco = cms.untracked.bool(True),
-    Egamma_maxDepth = cms.untracked.double(14488.0),
-    Egamma_maxWidth = cms.untracked.double(0.2),
-    KtRecombination = cms.untracked.uint32(2),
-    Egamma_minRatio = cms.untracked.double(0.5),
-    KtrParameter = cms.untracked.double(1.0),
-    towercut = cms.untracked.double(0.)
-)
+process.load('RecoLocalCalo.Castor.Castor_cff')
 
 process.MyOutputModule = cms.OutputModule("PoolOutputModule",
-    outputCommands = cms.untracked.vstring('keep *_fromRecHits_*_*'),
+    outputCommands = cms.untracked.vstring('keep *_Castor*Reco_*_*'),
     fileName = cms.untracked.string('recooutput.root')
 )
 
-process.producer = cms.Path(process.fromRecHits)
+process.producer = cms.Path(process.CastorFullReco)
 process.end = cms.EndPath(process.MyOutputModule)
 
