e23d3992c2c2b1cc8dd8f587bec8f8da9c3c5855
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-03-01 21:27:03 +0000

    removed outputfile parameter

--- Validation/RecoMET/python/GenMETFromGenJets_cfi.py
+++ Validation/RecoMET/python/GenMETFromGenJets_cfi.py
@@ -7,7 +7,6 @@ import FWCore.ParameterSet.Config as cms
 # Fill validation histograms for MET.
 genMetIC5GenJetsAnalyzer = cms.EDFilter(
     "METTester",
-    OutputFile = cms.untracked.string('METTester_genMetIC5GenJets.root'),
     InputMETLabel = cms.InputTag("genMetIC5GenJets"),
     METType = cms.untracked.string('MET'),
     FineBinning = cms.untracked.bool(False)
