3403d8787b264a777706dc2372a7d528fbc4624a
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-03-01 18:43:04 +0000

    - add Hcal

--- DQMOffline/Configuration/python/DQMOffline_CRT_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_CRT_cff.py
@@ -4,6 +4,7 @@ siStripCertificationInfo = cms.EDFilter("SiStripCertificationInfo")
 siPixelCertification = cms.EDFilter("SiPixelCertification")
 from DQM.EcalEndcapMonitorTasks.EEDataCertificationTask_cfi import *
 from DQM.EcalBarrelMonitorTasks.EBDataCertificationTask_cfi import *
+from DQM.HcalMonitorClient.HcalDataCertification_cfi import *
 
-crt_dqmoffline = cms.Sequence(siStripCertificationInfo*siPixelCertification*ecalEndcapDataCertificationTask*ecalBarrelDataCertificationTask)
+crt_dqmoffline = cms.Sequence(siStripCertificationInfo*siPixelCertification*ecalEndcapDataCertificationTask*ecalBarrelDataCertificationTask*hcalDataCertification)
 
