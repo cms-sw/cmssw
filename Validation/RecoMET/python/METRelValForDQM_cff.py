50d3d4e8d5a6d12c2128a7f0d65f283ee917be42
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-03-02 10:08:33 +0000

    Added Validation Modules for RelVal --->> DQM Merger

---
+++ Validation/RecoMET/python/METRelValForDQM_cff.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+# File: CaloMET.cfi
+# Author: B. Scurlock & R. Remington
+# Date: 03.04.2008
+#
+# Fill validation histograms for MET.
+
+from Validation.RecoMET.CaloMET_cfi import *
+metAnalyzer.FineBinning = cms.untracked.bool(False)
+metHOAnalyzer.FineBinning = cms.untracked.bool(False)
+metNoHFAnalyzer.FineBinning = cms.untracked.bool(False)
+metNoHFHOAnalyzer.FineBinning = cms.untracked.bool(False)
+metOptAnalyzer.FineBinning = cms.untracked.bool(False)
+metOptHOAnalyzer.FineBinning = cms.untracked.bool(False)
+metOptNoHFAnalyzer.FineBinning = cms.untracked.bool(False)
+metOptNoHFHOAnalyzer.FineBinning = cms.untracked.bool(False)
+
+from Validation.RecoMET.PFMET_cfi import *
+pfMetAnalyzer.FineBinning = cms.untracked.bool(False)
+
+from Validation.RecoMET.TCMET_cfi import *
+tcMetAnalyzer.FineBinning = cms.untracked.bool(False)
+
+from Validation.RecoMET.GenMET_cfi import *
+genMetTrueAnalyzer.FineBinning = cms.untracked.bool(False)
+genMetCaloAnalyzer.FineBinning = cms.untracked.bool(False)
+genMetCaloAndNonPromptAnalyzer.FineBinning = cms.untracked.bool(False)
+
+from Validation.RecoMET.caloTowers_cfi import *
+towerSchemeBAnalyzer.FineBinning = cms.untracked.bool(False)
+
+
+METRelValSequence = cms.Sequence(
+    metAnalyzer*
+    metHOAnalyzer*
+    metNoHFAnalyzer*
+    metNoHFHOAnalyzer*
+    metOptAnalyzer*
+    metOptHOAnalyzer*
+    metOptNoHFAnalyzer*
+    metOptNoHFHOAnalyzer*
+    pfMetAnalyzer*
+    tcMetAnalyzer*
+    genMetTrueAnalyzer*
+    genMetCaloAnalyzer*
+    genMetCaloAndNonPromptAnalyzer*
+    towerSchemeBAnalyzer
+    
+)
