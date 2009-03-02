83db23e965f9d6779b888be785fc997498ceabb0
Mikhail Kirsanov <sha1-e50056c329e7c13788b0e46607ccad6239d76f0b@cern.ch>
2009-03-02 16:55:27 +0000

    python example for CMSSW2 and CMSSW3

--- GeneratorInterface/Pythia8Interface/python/Pythia8Example1_cfi.py
+++ GeneratorInterface/Pythia8Interface/python/Pythia8Example1_cfi.py
@@ -1,21 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-generator = cms.EDFilter("Pythia8GeneratorFilter",
-    comEnergy = cms.double(10000.),
-
+source = cms.Source("Pythia8Source",
     maxEventsToPrint = cms.untracked.int32(1),
     pythiaPylistVerbosity = cms.untracked.int32(1),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-
     filterEfficiency = cms.untracked.double(1.0),
-
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
     PythiaParameters = cms.PSet(
-        parameterSets = cms.vstring(
-            'pythia8_example01'
-        ),
-        pythia8_example01 = cms.vstring(
-            'HardQCD:all = on',
-            'PhaseSpace:pTHatMin = 20.'
-        )
+        pythia8_example01 = cms.vstring('HardQCD:all = on',
+                                        'PhaseSpace:pTHatMin = 20.'),
+        parameterSets = cms.vstring('pythia8_example01')
     )
 )
