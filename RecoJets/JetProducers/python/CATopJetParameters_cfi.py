97b9e2042ce9f817c51908876305270504e671b4
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-03-02 03:12:22 +0000

    Turn off sum-et dependent cuts of CATopJet jet algorithm in RecoJets/JetProducers by default.

--- RecoJets/JetProducers/python/CATopJetParameters_cfi.py
+++ RecoJets/JetProducers/python/CATopJetParameters_cfi.py
@@ -10,10 +10,10 @@ CATopJetParameters = cms.PSet(
     etFrac = cms.double(0.7),               # fraction of event sumEt / 2 for a jet to be considered "hard"
     useAdjacency = cms.bool(False),         # veto adjacent subjets
     useMaxTower = cms.bool(False),          # use max tower in adjacency criterion, otherwise use centroid
-    ptBins = cms.vdouble(500,800,1300),     # pt bins over which cuts vary                                        
-    rBins = cms.vdouble(0.8,0.6,0.4),       # cone size bins                                                      
-    ptFracBins = cms.vdouble(0.1,0.05,0.05),# minimum fraction of central jet pt for subjets
-    nCellBins = cms.vint32(1,1,1) ,         # number of cells apart for two subjets to be considered "adjacent"
+    ptBins = cms.vdouble(0, 10e9),          # pt bins over which cuts vary                                        
+    rBins = cms.vdouble(0.8,0.8),           # cone size bins                                                      
+    ptFracBins = cms.vdouble(0.05,0.05),    # minimum fraction of central jet pt for subjets
+    nCellBins = cms.vint32(1,1) ,           # number of cells apart for two subjets to be considered "adjacent"
     debugLevel = cms.untracked.int32(0)     # debug level
 )
 
