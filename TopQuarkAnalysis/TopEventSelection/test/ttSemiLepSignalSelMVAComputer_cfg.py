39ee837b00ca2ecff13735382b94d8295928c2d6
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-03-03 09:33:05 +0000

    run tests on lxplus

--- TopQuarkAnalysis/TopEventSelection/test/ttSemiLepSignalSelMVAComputer_cfg.py
+++ TopQuarkAnalysis/TopEventSelection/test/ttSemiLepSignalSelMVAComputer_cfg.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 #-------------------------------------------------
-# test cfg file for mva training for jet parton 
-# association
+# test cfg file for mva training for event
+# selection 
 #-------------------------------------------------
 process = cms.Process("COMPUTER")
 
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = 'WARNINGS'
+process.MessageLogger.cerr.threshold = 'INFO'
 
 #-------------------------------------------------
 # process configuration
@@ -17,15 +17,14 @@ process.MessageLogger.cerr.threshold = 'WARNINGS'
 ## define your trainingfile(s)
 process.source = cms.Source("PoolSource",
      fileNames = cms.untracked.vstring(
-     "file:/tmp/mrenz/CMSSW_2_2_4/src/job_0_TQAFLayer1_Output.root",
-     "file:/tmp/mrenz/CMSSW_2_2_4/src/job_1_TQAFLayer1_Output.root",
+    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
      ),
      skipEvents = cms.untracked.uint32(0)
 )
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
 
 ## configure process options
@@ -57,9 +56,21 @@ process.out = cms.OutputModule(
 )
 
 #-------------------------------------------------
-# process paths;
+# tqaf configuration
 #-------------------------------------------------
-process.p = cms.Path(process.findTtSemiLepSignalSelMVA)
+
+## std sequence for tqaf layer1
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
+
+## necessary fixes to run 2.2.X on 2.1.X data
+## comment this when running on samples produced with 22X
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
+run22XonSummer08AODSIM(process)
+
+#-------------------------------------------------
+# process paths
+#-------------------------------------------------
+process.p = cms.Path(process.tqafLayer1 * process.findTtSemiLepSignalSelMVA)
 
 ## output
 process.outpath = cms.EndPath(process.out)
