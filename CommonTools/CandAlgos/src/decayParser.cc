cc59d0c00df29aa05c9683e38925b3058595b078
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-03-03 13:50:56 +0000

    migration from PhysicsTools

---
+++ CommonTools/CandAlgos/src/decayParser.cc
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+//
+// Package:     CandCombiner
+// Class  :     decayParser
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  
+//         Created:  Sun Aug  7 20:26:31 EDT 2005
+// $Id: decayParser.cc,v 1.5 2008/04/11 16:56:15 kaulmer Exp $
+//
+#include <boost/spirit/core.hpp>
+#include <boost/spirit/actor/push_back_actor.hpp>
+#include <vector>
+#include "CommonTools/CandAlgos/interface/decayParser.h"
+
+using namespace boost::spirit;
+using namespace std;
+
+namespace cand {
+  namespace parser {
+    
+    class ModeSetter {
+    public:
+      ModeSetter( vector<ConjInfo>& iVect, ConjInfo::Mode iMode) : 
+	conjInfo_(iVect), mode_(iMode) {}
+      void operator()( const char ) const {
+	conjInfo_.back().mode_ = mode_;
+      }
+    private:
+      vector<ConjInfo>& conjInfo_;
+      ConjInfo::Mode mode_;
+    };
+    
+    typedef scanner_policies<skip_parser_iteration_policy<nothing_parser, 
+							  iteration_policy>, 
+			     match_policy,action_policy> ScannerPolicy;
+    typedef scanner<const char*, ScannerPolicy > ScannerUsed_1;
+    typedef rule<ScannerUsed_1> Rule_1;
+    
+    bool decayParser( const string& iValue, vector<ConjInfo>& oStrings ) {
+      using namespace boost::spirit;
+      
+      Rule_1 label = ((+alnum_p) >> *ch_p(':') >> *ch_p('_') >>*alnum_p)[push_back_a(oStrings)];
+      Rule_1 conj =  (ch_p('@') >> !((ch_p('b')>>ch_p('a')>>ch_p('r')[ModeSetter(oStrings,ConjInfo::kBar)] )| 
+				     ch_p('+')[ModeSetter(oStrings,ConjInfo::kPlus)] | 
+				     ch_p('-')[ModeSetter(oStrings,ConjInfo::kMinus)]) );
+      
+      return parse( iValue.c_str(),
+		    (
+		     (label >> ! conj)
+		     % blank_p
+		     )
+		    ,
+		    nothing_p).full;
+    }
+  }
+}
