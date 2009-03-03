b843f8510e07e84cd802f524651136bf3ff54d72
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-03-03 16:12:58 +0000

    Fix Herwig6 array bounds for multi-index C<->Fortran array access.

--- GeneratorInterface/LHEInterface/plugins/Herwig6Hadronisation.cc
+++ GeneratorInterface/LHEInterface/plugins/Herwig6Hadronisation.cc
@@ -646,203 +646,203 @@ static bool hwgive(const std::string &paramString)
 	else if (!std::strncmp(param, "REPWT(0,0,0)", 12))
 		hwuwts.REPWT[0][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,0,1)", 12))
-		hwuwts.REPWT[0][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,0,2)", 12))
-		hwuwts.REPWT[0][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,0,3)", 12))
-		hwuwts.REPWT[0][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,0,4)", 12))
-		hwuwts.REPWT[0][0][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,1,0)", 12))
 		hwuwts.REPWT[0][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,1,1)", 12))
-		hwuwts.REPWT[0][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,1,2)", 12))
-		hwuwts.REPWT[0][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,1,3)", 12))
-		hwuwts.REPWT[0][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,1,4)", 12))
-		hwuwts.REPWT[0][1][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,2,0)", 12))
 		hwuwts.REPWT[0][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,2,1)", 12))
-		hwuwts.REPWT[0][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,2,2)", 12))
-		hwuwts.REPWT[0][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,2,3)", 12))
-		hwuwts.REPWT[0][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,2,4)", 12))
-		hwuwts.REPWT[0][2][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,3,0)", 12))
 		hwuwts.REPWT[0][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,3,1)", 12))
-		hwuwts.REPWT[0][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,3,2)", 12))
-		hwuwts.REPWT[0][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,3,3)", 12))
-		hwuwts.REPWT[0][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,3,4)", 12))
-		hwuwts.REPWT[0][3][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,4,0)", 12))
 		hwuwts.REPWT[0][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,4,1)", 12))
-		hwuwts.REPWT[0][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,4,2)", 12))
-		hwuwts.REPWT[0][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,4,3)", 12))
-		hwuwts.REPWT[0][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(0,4,4)", 12))
-		hwuwts.REPWT[0][4][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,0,0)", 12))
-		hwuwts.REPWT[1][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,0,1)", 12))
 		hwuwts.REPWT[1][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,0,2)", 12))
-		hwuwts.REPWT[1][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,0,3)", 12))
-		hwuwts.REPWT[1][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,0,4)", 12))
-		hwuwts.REPWT[1][0][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,1,0)", 12))
-		hwuwts.REPWT[1][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,1,1)", 12))
 		hwuwts.REPWT[1][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,1,2)", 12))
-		hwuwts.REPWT[1][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,1,3)", 12))
-		hwuwts.REPWT[1][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,1,4)", 12))
-		hwuwts.REPWT[1][1][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,2,0)", 12))
-		hwuwts.REPWT[1][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,2,1)", 12))
 		hwuwts.REPWT[1][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,2,2)", 12))
-		hwuwts.REPWT[1][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,2,3)", 12))
-		hwuwts.REPWT[1][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,2,4)", 12))
-		hwuwts.REPWT[1][2][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,3,0)", 12))
-		hwuwts.REPWT[1][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,3,1)", 12))
 		hwuwts.REPWT[1][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,3,2)", 12))
-		hwuwts.REPWT[1][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,3,3)", 12))
-		hwuwts.REPWT[1][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,3,4)", 12))
-		hwuwts.REPWT[1][3][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,4,0)", 12))
-		hwuwts.REPWT[1][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,4,1)", 12))
 		hwuwts.REPWT[1][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,4,2)", 12))
-		hwuwts.REPWT[1][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,4,3)", 12))
-		hwuwts.REPWT[1][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(1,4,4)", 12))
-		hwuwts.REPWT[1][4][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,0,0)", 12))
-		hwuwts.REPWT[2][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,0,1)", 12))
-		hwuwts.REPWT[2][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,0,2)", 12))
 		hwuwts.REPWT[2][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,0,3)", 12))
-		hwuwts.REPWT[2][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,0,4)", 12))
-		hwuwts.REPWT[2][0][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,1,0)", 12))
-		hwuwts.REPWT[2][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,1,1)", 12))
-		hwuwts.REPWT[2][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,1,2)", 12))
 		hwuwts.REPWT[2][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,1,3)", 12))
-		hwuwts.REPWT[2][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,1,4)", 12))
-		hwuwts.REPWT[2][1][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,2,0)", 12))
-		hwuwts.REPWT[2][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,2,1)", 12))
-		hwuwts.REPWT[2][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,2,2)", 12))
 		hwuwts.REPWT[2][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,2,3)", 12))
-		hwuwts.REPWT[2][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,2,4)", 12))
-		hwuwts.REPWT[2][2][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,3,0)", 12))
-		hwuwts.REPWT[2][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,3,1)", 12))
-		hwuwts.REPWT[2][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,3,2)", 12))
 		hwuwts.REPWT[2][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,3,3)", 12))
-		hwuwts.REPWT[2][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,3,4)", 12))
-		hwuwts.REPWT[2][3][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,4,0)", 12))
-		hwuwts.REPWT[2][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,4,1)", 12))
-		hwuwts.REPWT[2][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,4,2)", 12))
 		hwuwts.REPWT[2][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,4,3)", 12))
-		hwuwts.REPWT[2][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[3][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(2,4,4)", 12))
-		hwuwts.REPWT[2][4][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,0,0)", 12))
-		hwuwts.REPWT[3][0][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,0,1)", 12))
-		hwuwts.REPWT[3][0][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,0,2)", 12))
-		hwuwts.REPWT[3][0][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,0,3)", 12))
 		hwuwts.REPWT[3][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,0,4)", 12))
-		hwuwts.REPWT[3][0][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][0][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,1,0)", 12))
-		hwuwts.REPWT[3][1][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,1,1)", 12))
-		hwuwts.REPWT[3][1][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,1,2)", 12))
-		hwuwts.REPWT[3][1][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,1,3)", 12))
 		hwuwts.REPWT[3][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,1,4)", 12))
-		hwuwts.REPWT[3][1][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][1][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,2,0)", 12))
-		hwuwts.REPWT[3][2][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,2,1)", 12))
-		hwuwts.REPWT[3][2][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,2,2)", 12))
-		hwuwts.REPWT[3][2][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,2,3)", 12))
 		hwuwts.REPWT[3][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,2,4)", 12))
-		hwuwts.REPWT[3][2][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][2][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,3,0)", 12))
-		hwuwts.REPWT[3][3][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,3,1)", 12))
-		hwuwts.REPWT[3][3][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,3,2)", 12))
-		hwuwts.REPWT[3][3][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,3,3)", 12))
 		hwuwts.REPWT[3][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,3,4)", 12))
-		hwuwts.REPWT[3][3][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][3][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,4,0)", 12))
-		hwuwts.REPWT[3][4][0] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[0][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,4,1)", 12))
-		hwuwts.REPWT[3][4][1] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[1][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,4,2)", 12))
-		hwuwts.REPWT[3][4][2] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[2][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,4,3)", 12))
 		hwuwts.REPWT[3][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "REPWT(3,4,4)", 12))
-		hwuwts.REPWT[3][4][4] = std::atof(&param[std::strcspn(param, "=") + 1]);
+		hwuwts.REPWT[4][4][3] = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "SNGWT", 5))
 		hwuwts.SNGWT = std::atof(&param[std::strcspn(param, "=") + 1]);
 	else if (!std::strncmp(param, "DECWT", 5))
