6b593f68147d939b6fb151c9119dbc20b9aee671
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-03-04 14:33:54 +0000

    modify the source for the generated event (now generator)

--- Validation/EcalClusters/python/VerificationCommonParameters_cfi.py
+++ Validation/EcalClusters/python/VerificationCommonParameters_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 VerificationCommonParameters = cms.PSet(
 #    CMSSW_Version = cms.untracked.string('V3_1_0_pre1'),
-    MCTruthCollection = cms.InputTag("source"),
+    MCTruthCollection = cms.InputTag("generator"),
 #    outputFile = cms.untracked.string('EcalClustersValidation.root'),
     verboseDBE = cms.untracked.bool(True)
 )
