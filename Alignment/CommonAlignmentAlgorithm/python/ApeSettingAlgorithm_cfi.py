1f3780cc971fe0845194070c2b84fb05180f0afc
Ivan Reid <sha1-bcdf6703eff17f0d37bd913a6e288ff4eb3e8931@cern.ch>
2009-03-04 12:29:01 +0000

    To read/write APE Text files

---
+++ Alignment/CommonAlignmentAlgorithm/python/ApeSettingAlgorithm_cfi.py
@@ -0,0 +1,15 @@
+# ApeSettingAlgorithm
+# -------------------
+
+import FWCore.ParameterSet.Config as cms
+
+ApeSettingAlgorithm = cms.PSet(
+    algoName = cms.string('ApeSettingAlgorithm'),
+    saveApeToASCII = cms.untracked.bool(False),
+    apeASCIISaveFile = cms.untracked.string('ApeDump.txt'),
+    readApeFromASCII = cms.bool(False),
+    apeASCIIReadFile = cms.FileInPath('Alignment/CommonAlignmentAlgorithm/test/apeinput.txt')
+    #
+    # add your parameters here
+)
+
