26dc8b58706e8fdc8b8b2fd5a189e5f4fc3cacca
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-03-04 15:33:35 +0000

    updated to match changes in base class, made sort order icon smaller

--- Fireworks/TableWidget/src/FWColumnLabelCellRenderer.cc
+++ Fireworks/TableWidget/src/FWColumnLabelCellRenderer.cc
@@ -8,11 +8,12 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Feb  2 16:44:04 EST 2009
-// $Id$
+// $Id: FWColumnLabelCellRenderer.cc,v 1.1 2009/02/03 20:33:04 chrjones Exp $
 //
 
 // system include files
 #include "TVirtualX.h"
+#include "TGFont.h"
 
 // user include files
 #include "Fireworks/TableWidget/interface/FWColumnLabelCellRenderer.h"
@@ -29,9 +30,23 @@
 //
 // constructors and destructor
 //
-FWColumnLabelCellRenderer::FWColumnLabelCellRenderer(GContext_t iContext, FontStruct_t iFontStruct):
-FWTextTableCellRenderer(iContext,iFontStruct), 
-m_sortOrder(fireworks::table::kNotSorted) {}
+FWColumnLabelCellRenderer::FWColumnLabelCellRenderer(const TGGC* iContext):
+FWTextTableCellRenderer(iContext), 
+m_sortOrder(fireworks::table::kNotSorted),
+m_sizeOfOrderIcon(height())
+{
+   FontMetrics_t metrics;
+   font()->GetFontMetrics(&metrics);
+
+   m_sizeOfOrderIcon -= 2*metrics.fDescent;
+   m_sizeOfOrderIconStartX = metrics.fDescent;
+   //we want the number to be even so that the point of the triangle 
+   // will be right in the middle
+   if(1 == m_sizeOfOrderIcon % 2) {
+      ++m_sizeOfOrderIcon;
+   }
+}
+
 
 // FWColumnLabelCellRenderer::FWColumnLabelCellRenderer(const FWColumnLabelCellRenderer& rhs)
 // {
@@ -65,17 +80,19 @@ void
 FWColumnLabelCellRenderer::draw(Drawable_t iID, int iX, int iY, unsigned int iWidth, unsigned int iHeight)
 {
    using namespace fireworks::table;
-   UInt_t h = height();
+   UInt_t h = m_sizeOfOrderIcon;
+   const GContext_t c = graphicsContext()->GetGC();
+   int dY = m_sizeOfOrderIconStartX;
    
    if(kAscendingSort == m_sortOrder) {
-      gVirtualX->DrawLine(iID, graphicsContext(), iX+h/2, iY+2, iX, iY+h-2);
-      gVirtualX->DrawLine(iID, graphicsContext(), iX, iY+h-2, iX+h, iY+h-2);
-      gVirtualX->DrawLine(iID, graphicsContext(), iX+h/2, iY+2, iX+h, iY+h-2);
+      gVirtualX->DrawLine(iID, c, iX+h/2, iY+2+dY,   iX,   iY+h-2+dY);
+      gVirtualX->DrawLine(iID, c, iX,     iY+h-2+dY, iX+h, iY+h-2+dY);
+      gVirtualX->DrawLine(iID, c, iX+h/2, iY+2+dY,   iX+h, iY+h-2+dY);
    }
    if(kDescendingSort == m_sortOrder){
-      gVirtualX->DrawLine(iID, graphicsContext(), iX, iY+2, iX+h, iY+2);
-      gVirtualX->DrawLine(iID, graphicsContext(), iX+h/2, iY+h-2, iX+h, iY+2);
-      gVirtualX->DrawLine(iID, graphicsContext(), iX+h/2, iY+h-2, iX, iY+2);      
+      gVirtualX->DrawLine(iID, c, iX,     iY+2+dY,   iX+h, iY+2+dY);
+      gVirtualX->DrawLine(iID, c, iX+h/2, iY+h-2+dY, iX+h, iY+2+dY);
+      gVirtualX->DrawLine(iID, c, iX+h/2, iY+h-2+dY, iX,   iY+2+dY);      
    }
    FWTextTableCellRenderer::draw(iID,iX+kGap+h,iY,iWidth-kGap-h,iHeight);
 }
@@ -90,8 +107,7 @@ fireworks::table::SortOrder FWColumnLabelCellRenderer::sortOrder() const
 
 UInt_t FWColumnLabelCellRenderer::width() const
 {
-   UInt_t h = height();
-   return FWTextTableCellRenderer::width()+kGap+h;
+   return FWTextTableCellRenderer::width()+kGap+m_sizeOfOrderIcon;
 }
 
 //
