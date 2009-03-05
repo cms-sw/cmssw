5982333ff07ac7b39e3f821fc7a30829c1c393a4
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2009-03-05 12:08:55 +0000

    what worked yesterday

--- CondFormats/CSCObjects/test/readNoiseMatrix_cfg.py
+++ CondFormats/CSCObjects/test/readNoiseMatrix_cfg.py
@@ -1,4 +1,4 @@
-# Read Noise Matrix values - Tim Cox - 18.02.2009
+# Read Noise Matrix values - Tim Cox - 04.03.2009
 # I intend this to read from the standard cond data files, whatever they are.
 # This is for CMSSW_3xx
 
@@ -7,6 +7,19 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TEST")
 process.load("Configuration/StandardSequences/FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'IDEAL_30X::All'
+##process.GlobalTag.globaltag = 'IDEAL_V11::All'
+process.load("CalibMuon.Configuration.getCSCConditions_frontier_cff")
+process.cscConditions.toGet = cms.VPSet(
+        cms.PSet(record = cms.string('CSCDBGainsRcd'),
+                 tag = cms.string('CSCDBGains_ME42_offline')),
+        cms.PSet(record = cms.string('CSCDBNoiseMatrixRcd'),
+                 tag = cms.string('CSCDBNoiseMatrix_ME42_offline')),
+        cms.PSet(record = cms.string('CSCDBCrosstalkRcd'),
+                 tag = cms.string('CSCDBCrosstalk_ME42_offline')),
+        cms.PSet(record = cms.string('CSCDBPedestalsRcd'),
+                 tag = cms.string('CSCDBPedestals_ME42_offline'))
+)
+process.es_prefer_cscConditions = cms.ESPrefer("PoolDBESSource","cscConditions")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
