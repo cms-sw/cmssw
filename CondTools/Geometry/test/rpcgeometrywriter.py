5b27053d57a95d1c9b33d0b63b5f8e8ed930f9a1
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2009-03-06 16:17:44 +0000

    first release

---
+++ CondTools/Geometry/test/rpcgeometrywriter.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RPCGeometryWriter")
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+
+
+process.source = cms.Source("EmptyIOVSource",
+                            lastValue = cms.uint64(1),
+                            timetype = cms.string('runnumber'),
+                            firstValue = cms.uint64(1),
+                            interval = cms.uint64(1)
+                            )
+
+
+process.RPCGeometryWriter = cms.EDAnalyzer("RPCRecoIdealDBLoader")
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+                                          process.CondDBCommon,
+                                          BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+
+                                          timetype = cms.untracked.string('runnumber'),
+                                          connect = cms.string('sqlite_file:rpcgeo.db'),
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('RPCRecoGeometryRcd'),tag = cms.string('RPCRECO_Geometry_Test01'))
+                                                            )
+                                          )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+    )
+
+process.p1 = cms.Path(process.RPCGeometryWriter)
+
