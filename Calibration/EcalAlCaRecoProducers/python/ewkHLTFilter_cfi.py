a079e65a9b9dcd42d16812ec6033e0769edcb195
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-03-06 14:29:33 +0000

    removed to avoid clashes

--- Calibration/EcalAlCaRecoProducers/python/ewkHLTFilter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import HLTrigger.HLTfilters.hltHighLevel_cfi
-ewkHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-ewkHLTFilter.throw = False 
-
