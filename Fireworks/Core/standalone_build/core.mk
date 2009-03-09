c535f02908dd8234d7f0c5816f0735adfe33a2bf
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2009-03-09 18:45:35 +0000

    Split externals for Linux and Macintosh OS.

--- Fireworks/Core/standalone_build/core.mk
+++ Fireworks/Core/standalone_build/core.mk
@@ -1,2 +1,9 @@
-CoreLibs :=  boost_thread-gcc-mt boost_signals-gcc-mt boost_filesystem-gcc-mt boost_program_options-gcc-mt boost_regex-gcc-mt shift CLHEP-1.9.4.2 dcap gsl gslcblas HepMCfio HepMC pcre Cintex Cint Core RIO Net Tree TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex sigc-2.0 nsl crypt dl uuid z
-CoreIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/castor/2.1.7.14/include external/inc/clhep/1.9.4.2/include external/inc/dcap/1.7.0.48/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms2/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/uuid/1.38-cms/include external/inc/zlib/1.2.3-cms/include
+ifeq ($(shell uname), Linux)
+   CoreLibs :=  boost_thread-gcc-mt boost_signals-gcc-mt boost_filesystem-gcc-mt boost_program_options-gcc-mt boost_regex-gcc-mt shift CLHEP-1.9.4.2 dcap gsl gslcblas HepMCfio HepMC pcre Cintex Cint Core RIO Net Tree TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex sigc-2.0 nsl crypt dl uuid z
+   CoreIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/castor/2.1.7.14/include external/inc/clhep/1.9.4.2/include external/inc/dcap/1.7.0.48/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms2/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/uuid/1.38-cms/include external/inc/zlib/1.2.3-cms/include
+else 
+   ifeq ($(shell uname), Darwin)
+      CoreLibs :=  boost_thread-gcc-mt boost_regex-gcc-mt boost_filesystem-gcc-mt CLHEP-1.9.3.2 gsl gslcblas HepMCfio HepMC TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cintex Cint Core RIO Net Tree MathCore MathMore GenVector Reflex sigc-2.0 dl
+      CoreIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10/include external/inc/hepmc/2.01.10/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/uuid/1.38-cms/include external/inc/zlib/1.2.3-cms/include
+   endif     
+endif
--- Fireworks/Core/standalone_build/project.mk
+++ Fireworks/Core/standalone_build/project.mk
@@ -1,2 +1,9 @@
-ProjectLibs :=  boost_thread-gcc-mt boost_signals-gcc-mt boost_program_options-gcc-mt CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC pcre Cint Core RIO Net Tree Hist Matrix MathCore MathMore Reflex sigc-2.0 nsl crypt dl z EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
-ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/zlib/1.2.3-cms/include
+ifeq ($(shell uname), Linux)
+   ProjectLibs :=  boost_thread-gcc-mt boost_signals-gcc-mt boost_program_options-gcc-mt CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC pcre Cint Core RIO Net Tree Hist Matrix MathCore MathMore Reflex sigc-2.0 nsl crypt dl z EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
+   ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/zlib/1.2.3-cms/include
+else 
+   ifeq ($(shell uname), Darwin)
+      ProjectLibs := boost_filesystem-gcc-mt boost_regex-gcc-mt boost_program_options-gcc-mt CLHEP-1.9.3.2 gsl gslcblas HepMCfio HepMC pcre TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cint Cintex Core RIO Net Tree MathCore MathMore Reflex sigc-2.0 EG Eve FTGL Ged Geom GeomPainter Gui RGL Rint
+      ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10/include external/inc/hepmc/2.01.10/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/zlib/1.2.3-cms/include
+   endif     
+endif
