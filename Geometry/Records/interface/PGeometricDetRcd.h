c2b72ddbbd12d59368229db83d2bb1a4700df307
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-03-10 13:40:14 +0000

    remove the PGeometricDetRcd

--- Geometry/Records/interface/PGeometricDetRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef PGEOMETRICDETRCD_H
-#define PGEOMETRICDETRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class PGeometricDetRcd : public edm::eventsetup::EventSetupRecordImplementation<PGeometricDetRcd> {};
-#endif
