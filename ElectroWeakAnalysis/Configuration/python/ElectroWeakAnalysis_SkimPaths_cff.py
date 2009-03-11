90a26abd484a27fa43612f175adfb184d13e2b29
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2009-03-11 17:27:17 +0000

    removing import of deleted old CSA07 skim cfg files

--- ElectroWeakAnalysis/Configuration/python/ElectroWeakAnalysis_SkimPaths_cff.py
+++ ElectroWeakAnalysis/Configuration/python/ElectroWeakAnalysis_SkimPaths_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 #
 # ElectroWeakAnalysis standard sequences
 #
-from ElectroWeakAnalysis.ZReco.ZReco_SkimPaths_cff import *
+#from ElectroWeakAnalysis.ZReco.ZReco_SkimPaths_cff import *
 from ElectroWeakAnalysis.WReco.WReco_SkimPaths_cff import *
 
--- ElectroWeakAnalysis/Configuration/python/ElectroWeakAnalysis_cff.py
+++ ElectroWeakAnalysis/Configuration/python/ElectroWeakAnalysis_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 #
 # ElectroWeakAnalysis standard sequences
 #
-from ElectroWeakAnalysis.ZReco.ZReco_cff import *
+#from ElectroWeakAnalysis.ZReco.ZReco_cff import *
 from ElectroWeakAnalysis.WReco.WReco_cff import *
-electroWeakAnalysis = cms.Sequence(zReco*wReco)
+#electroWeakAnalysis = cms.Sequence(zReco*wReco)
+electroWeakAnalysis = cms.Sequence(wReco)
 
