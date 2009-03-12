15e36bee5500c4652b092ac1c211e5ce36a2e9f8
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-03-12 18:01:36 +0000

    added force option

--- Validation/RecoParticleFlow/Benchmarks/Tools/submit.py
+++ Validation/RecoParticleFlow/Benchmarks/Tools/submit.py
@@ -14,6 +14,10 @@ parser.add_option("-e", "--extension", dest="extension",
                   help="adds an extension to the name of this benchmark",
                   default=None)
 
+parser.add_option("-f", "--force", dest="force",action="store_true",
+                  help="force the submission. Be careful!",
+                  default=False)
+
 
 (options,args) = parser.parse_args()
  
@@ -31,11 +35,15 @@ print 'submitting benchmark:', bench
 website.writeAccess()
 bench.makeRelease( website )
 
-if( bench.exists( website ) == True ):
-    print 'please use the -e option to add an extention, or choose another extension'
+
+if( bench.exists( website ) == True and options.force == False ):
+    print 'please use the -e option to choose another extension'
     print '  e.g: submit.py -e Feb10'
+    print 'or force it using the -f option'
 else:
     print bench, bench.benchmarkOnWebSite(website)
+    if(options.force==True):
+        shutil.rmtree(bench.benchmarkOnWebSite(website))
     shutil.copytree(bench.__str__(), bench.benchmarkOnWebSite(website) ) 
     print 'done. Access your benchmark here:'
     print bench.benchmarkUrl( website )
