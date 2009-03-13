fff815a32e16c550fcf76fb428304adaeedb07b0
Mikhail Kirsanov <sha1-e50056c329e7c13788b0e46607ccad6239d76f0b@cern.ch>
2009-03-13 12:17:13 +0000

    example with GeneratorFilter added

---
+++ GeneratorInterface/Pythia8Interface/test/pythia8ex2_cfg.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PROD")
+
+process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
+
+process.source = cms.Source("EmptySource")
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    PythiaParameters = cms.PSet(
+        pythia8_example01 = cms.vstring('HardQCD:all = on',
+                                        'PhaseSpace:pTHatMin = 20.'),
+        parameterSets = cms.vstring('pythia8_example01')
+    )
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        )
+    ),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    moduleSeeds = cms.PSet(
+        g4SimHits = cms.untracked.uint32(123456788),
+        VtxSmeared = cms.untracked.uint32(123456789)
+    ),
+    sourceSeed = cms.untracked.uint32(98765)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.GEN = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN')
+    ),
+    fileName = cms.untracked.string('pythia8ex2.root')
+)
+
+process.outpath = cms.EndPath(process.GEN)
+process.schedule = cms.Schedule(process.outpath)
+
