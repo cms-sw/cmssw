87ae89a2d3bbd6f939d5b0996bb99be374abfbec
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-03-14 09:37:39 +0000

    added missing file

---
+++ CondFormats/DTObjects/src/T_EventSetup_DTTPGParameters.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondFormats/DTObjects/interface/DTTPGParameters.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(DTTPGParameters);
