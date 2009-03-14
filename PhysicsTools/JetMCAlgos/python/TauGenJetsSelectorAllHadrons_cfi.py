a0ff91958738d6e100303ff0263f272344e78246
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2009-03-14 22:43:26 +0000

    GenJetSelector example, for iterativeCone5GenJets with a eta and a pt cut.

--- PhysicsTools/JetMCAlgos/python/TauGenJetsSelectorAllHadrons_cfi.py
+++ PhysicsTools/JetMCAlgos/python/TauGenJetsSelectorAllHadrons_cfi.py
@@ -1,14 +1,32 @@
 import FWCore.ParameterSet.Config as cms
+import copy
 
-tauGenJetsSelectorAllHadrons = cms.EDFilter("TauGenJetDecayModeSelector",
+# module to select generator level tau-decays
+# See https://twiki.cern.ch/twiki/bin/view/CMS/SWGuidePhysicsCutParser
+# on how to use the cut-string
+
+#--------------------------------------------------------------------------------
+# selection of tau --> hadrons nu_tau decays
+#--------------------------------------------------------------------------------
+
+# require generated tau to decay hadronically
+selectedGenTauDecaysToHadrons = cms.EDFilter("TauGenJetDecayModeSelector",
      src = cms.InputTag("tauGenJets"),
-     select = cms.vstring('oneProng0Pi0', 
-                          'oneProng1Pi0', 
-                          'oneProng2Pi0', 
-                          'oneProngOther',
-                          'threeProng0Pi0', 
-                          'threeProng1Pi0', 
-                          'threeProngOther', 
-                          'rare'),
+     select = cms.vstring('oneProng0Pi0', 'oneProng1Pi0', 'oneProng2Pi0', 'oneProngOther',
+                          'threeProng0Pi0', 'threeProng1Pi0', 'threeProngOther', 'rare'),
+     filter = cms.bool(False)
+)
+
+# require generator level hadrons produced in tau-decay to be within muon acceptance
+selectedGenTauDecaysToHadronsEta25Cumulative = cms.EDFilter("GenJetSelector",
+     src = cms.InputTag("selectedGenTauDecaysToHadrons"),
+     cut = cms.string('abs(eta) < 2.5'),
+     filter = cms.bool(False)
+)
+
+# require generator level hadrons produced in tau-decay to have transverse momentum above threshold
+selectedGenTauDecaysToHadronsPt5Cumulative = cms.EDFilter("GenJetSelector",
+     src = cms.InputTag("selectedGenTauDecaysToHadronsEta25Cumulative"),
+     cut = cms.string('pt > 5.'),
      filter = cms.bool(False)
 )
