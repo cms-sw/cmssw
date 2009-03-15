0c5903a6cf1f0524d70a18606f68d569bfc3ac36
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2009-03-15 23:19:02 +0000

    Removed gren compatibility switch and replaced it with an unpacker auto-detection/force unpacker option

--- EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
+++ EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
@@ -4,7 +4,7 @@ l1GctHwDigis = cms.EDProducer("GctRawToDigi",
     inputLabel = cms.InputTag("source"),
     gctFedId = cms.int32(745),
     hltMode = cms.bool(False),
-    grenCompatibilityMode = cms.bool(False),
+    unpackerVersion = cms.uint32(0), # O=Auto-detect, or override with: 1=GREN 07 era, 2=CRAFT 08 era, or 3=Apr 2009 onwards.
     verbose = cms.untracked.bool(False)
 )
 
