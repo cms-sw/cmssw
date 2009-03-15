ab867cde8d26173bcefecbcee98c8292015e3c47
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-03-15 10:13:19 +0000

    Adding L1 report

---
+++ HLTrigger/Configuration/python/customL1T_Options.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+def customise(process):
+    
+    process.l1GtTrigReport = cms.EDAnalyzer( "L1GtTrigReport",
+        UseL1GlobalTriggerRecord = cms.bool( False ),
+        L1GtRecordInputTag = cms.InputTag( "simGtDigis" ),
+        PrintOutput = cms.untracked.int32( 0 )
+    )
+    process.L1AnalyzerEndpath = cmsEndPath (process.l1GtTrigReport)
+    process.schedule.append(process.L1AnalyzerEndpath)
+    
+    process.options.wantSummary = cms.untracked.bool(True)
+
+    process.MessageLogger.categories.append('L1GtTrigReport')
+
+    return(process)
