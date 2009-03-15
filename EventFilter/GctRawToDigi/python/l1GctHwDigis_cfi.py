6f2937c2f7ca574d0635e08924bb397ff9aeb8ed
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2009-03-15 16:02:04 +0000

    Removed unnecessary switches relating to what gets unpacked.

--- EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
+++ EventFilter/GctRawToDigi/python/l1GctHwDigis_cfi.py
@@ -5,13 +5,6 @@ l1GctHwDigis = cms.EDProducer("GctRawToDigi",
     gctFedId = cms.int32(745),
     hltMode = cms.bool(False),
     grenCompatibilityMode = cms.bool(False),
-    unpackRct = cms.untracked.bool(True),
-    unpackInternEm = cms.untracked.bool(False),
-    unpackInternJets = cms.untracked.bool(False),
-    unpackFibres = cms.untracked.bool(False),
-    unpackEm = cms.untracked.bool(True),
-    unpackJets = cms.untracked.bool(True),
-    unpackEtSums = cms.untracked.bool(True),
     verbose = cms.untracked.bool(False)
 )
 
