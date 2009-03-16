a99fbe871a89f53e5c7bcc865536f5c26ebdfe0d
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-03-16 15:47:31 +0000

    Add back Trigger reports

--- FastSimulation/Configuration/test/IntegrationTestWithHLT_cfg.py
+++ FastSimulation/Configuration/test/IntegrationTestWithHLT_cfg.py
@@ -107,7 +107,9 @@ process.schedule.append(process.outpath)
 
 # Keep the logging output to a nice level #
 # process.Timing =  cms.Service("Timing")
-#process.load("FWCore/MessageService/MessageLogger_cfi")
+process.load("FWCore/MessageService/MessageLogger_cfi")
+process.MessageLogger.categories.append('L1GtTrigReport')
+process.MessageLogger.categories.append('HLTrigReport')
 #process.MessageLogger.destinations = cms.untracked.vstring("pyDetailedInfo.txt","cout")
 #process.MessageLogger.categories.append("FamosManager")
 #process.MessageLogger.cout = cms.untracked.PSet(threshold=cms.untracked.string("INFO"),
