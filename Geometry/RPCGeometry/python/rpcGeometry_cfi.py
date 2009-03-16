cea7897e53c5762f5515c62ff1ec61775e59056d
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-03-16 17:01:02 +0000

    adjust the rpc geometry configuration adding a missing parameter

--- Geometry/RPCGeometry/python/rpcGeometry_cfi.py
+++ Geometry/RPCGeometry/python/rpcGeometry_cfi.py
@@ -4,7 +4,8 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the RPC geometry model.
 #
 RPCGeometryESModule = cms.ESProducer("RPCGeometryESModule",
-    compatibiltyWith11 = cms.untracked.bool(True)
+    compatibiltyWith11 = cms.untracked.bool(True),
+    useDDD = cms.untracked.bool(True)
 )
 
 
