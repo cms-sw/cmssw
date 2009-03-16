25c91af685d57f9ddb0045ceb0117c686217fd1a
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2009-03-16 15:11:14 +0000

    removing ESproducer duplicates using typedef

--- RecoTracker/TkSeedingLayers/plugins/PixelLayerTripletsESProducer.h
+++ RecoTracker/TkSeedingLayers/plugins/PixelLayerTripletsESProducer.h
@@ -1,27 +1,8 @@
 #ifndef PixelLayerTripletsESProducer_H
 #define PixelLayerTripletsESProducer_H
 
-#include  "FWCore/Framework/interface/ESProducer.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include <boost/shared_ptr.hpp>
+#include "RecoTracker/TkSeedingLayers/plugins/SeedingLayersESProducer.h"
 
-#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
-#include "RecoTracker/TkSeedingLayers/interface/SeedingLayerSetsBuilder.h"
+typedef SeedingLayersESProducer PixelLayerTripletsESProducer;
 
-class PixelLayerTripletsESProducer : public edm::ESProducer {
-public:
-
-  PixelLayerTripletsESProducer(const edm::ParameterSet & cfg) : theConfig(cfg) {
-    setWhatProduced(this, theConfig.getParameter<std::string>("ComponentName") );
-  }
-
-  boost::shared_ptr<SeedingLayerSetsBuilder> produce(const TrackerDigiGeometryRecord & r) {
-    theResult = boost::shared_ptr<SeedingLayerSetsBuilder>( new SeedingLayerSetsBuilder(theConfig));
-    return theResult;
-  }
-  
-private:
-  edm::ParameterSet theConfig;
-  boost::shared_ptr<SeedingLayerSetsBuilder> theResult;
-};
 #endif
