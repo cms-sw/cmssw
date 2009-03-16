c951bcc89f29dd039c14df7a362f140bcde3760a
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2009-03-16 13:32:56 +0000

    minor update to make it working again

--- CalibTracker/SiPixelConnectivity/test/write_cfg.py
+++ CalibTracker/SiPixelConnectivity/test/write_cfg.py
@@ -4,25 +4,10 @@ process = cms.Process("MapWriter")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.CondDBCommon.connect = cms.string("sqlite_file:cabling.db")
 
-#process.CondDBCommon.connect = cms.InputTag("sqlite_file","cabling.db")
-#process.CondDBCommon.catalog = cms.InputTag("file","testcatalog.xml")
-
-#process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cff")
-#process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
-#process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
-#process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
-
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FakeConditions_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'IDEAL_30X::All'
 
-
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = cms.string('IDEAL_V9::All')
-#process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
-#process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
-
-
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1))
 process.source = cms.Source("EmptyIOVSource",
     timetype = cms.string('runnumber'),
     firstValue = cms.uint64(1),
@@ -34,7 +19,7 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     process.CondDBCommon,
     toPut = cms.VPSet(cms.PSet(
         record =  cms.string('SiPixelFedCablingMapRcd'),
-        tag = cms.string('SiPixelFedCablingMap_v14')
+        tag = cms.string('SiPixelFedCablingMap_v16')
     )),
     loadBlobStreamer = cms.untracked.bool(False)
 )
