197cc2d93e72313a705db9facd7f10c274925da3
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-03-17 15:44:21 +0000

    Second merge of branch BURTS_IDEAL into HEAD.  The first merge wasn't exactly right.

--- RecoLocalTracker/SiStripClusterizer/plugins/SealModules.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/SealModules.cc
@@ -6,15 +6,5 @@ DEFINE_SEAL_MODULE();
 #include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterizer.h"
 DEFINE_ANOTHER_FWK_MODULE(SiStripClusterizer);
 
-<<<<<<< SealModules.cc
-using cms::SiStripClusterizer;
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(SiStripClusterizer);
-
 #include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterProducer.h"
 DEFINE_ANOTHER_FWK_MODULE(SiStripClusterProducer);
-=======
-#include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterProducer.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripClusterProducer);
->>>>>>> 1.2.2.2.2.2
