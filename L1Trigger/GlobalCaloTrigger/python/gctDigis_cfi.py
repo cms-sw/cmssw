3129f5223685bd5889385e042a5ed7b50b04204b
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2009-03-17 08:12:37 +0000

    Restoring 31X to HEAD, change EDFilter to EDProducer

--- L1Trigger/GlobalCaloTrigger/python/gctDigis_cfi.py
+++ L1Trigger/GlobalCaloTrigger/python/gctDigis_cfi.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-gctDigis = cms.EDFilter("L1GctEmulator",
+gctDigis = cms.EDProducer("L1GctEmulator",
     jetFinderType = cms.string('hardwareJetFinder'),
+    useImprovedTauAlgorithm = cms.bool(False),
+    ignoreRCTTauVetoBitsForIsolation = cms.bool(False),
     inputLabel = cms.InputTag("rctDigis"),
-    jetThresholdForHtSumGeV = cms.double(5.0),
     preSamples = cms.uint32(1),
     postSamples = cms.uint32(1)
 )
 
 
+  
