fa36d87eada6f222892f76881fb59b7fd251b5af
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-03-18 15:25:19 +0000

    added wrappers

--- CondCore/CalibPlugins/plugins/classes_cids.cc
+++ CondCore/CalibPlugins/plugins/classes_cids.cc
@@ -6,6 +6,8 @@ CLASS_ID(BlobPedestals);
 CLASS_ID(BlobNoises);
 CLASS_ID(BlobComplex);
 CLASS_ID(condex::Efficiency);
+CLASS_ID(condex::ParametricEfficiencyInPt);
+CLASS_ID(condex::ParametricEfficiencyInEta);
 
 CLASS_ID(cond::DataWrapper<Pedestals>);
 CLASS_ID(cond::DataWrapper<BlobPedestals>);
