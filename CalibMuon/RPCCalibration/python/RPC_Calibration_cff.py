23014c0c86f47ccb69be204fbdbf1d26553c0edd
Borislav Pavlov <sha1-f48829402f6317e4676b6fb3a08ee6762c7b5a95@cern.ch>
2009-03-18 20:46:24 +0000

    *** empty log message ***

--- CalibMuon/RPCCalibration/python/RPC_Calibration_cff.py
+++ CalibMuon/RPCCalibration/python/RPC_Calibration_cff.py
@@ -7,10 +7,11 @@ RPCCalibPerf = cms.ESSource("PoolDBESSource",
     timetype = cms.string('runnumber'),
     toGet = cms.VPSet(cms.PSet(
         record = cms.string('RPCStripNoisesRcd'),
-        tag = cms.string('RPCStripNoises_mc')
+#        tag = cms.string('RPCStripNoises_mc')
+        tag = cms.string('RPCStripNoise_CRAFTV00_mc')
     )),
-    connect = cms.string('frontier://FrontierProd/CMS_COND_20X_RPC'), ##FrontierProd/CMS_COND_20X_RPC"
-
+#    connect = cms.string('frontier://FrontierProd/CMS_COND_20X_RPC'), ##FrontierProd/CMS_COND_20X_RPC"
+    connect = cms.string('frontier://FrontierProd/CMS_COND_21X_RPC'), ##FrontierProd/CMS_COND_21X_RPC"
     authenticationMethod = cms.untracked.uint32(0)
 )
 
