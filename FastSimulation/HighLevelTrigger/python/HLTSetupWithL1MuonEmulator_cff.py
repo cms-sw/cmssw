469e56f9b82204e9f1c25cf627a69cef06b2463d
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2009-03-18 08:50:21 +0000

    Duplicate HLTSetup_cff.py to include L1MuonEmulator (instead of L1ParamMuons)

---
+++ FastSimulation/HighLevelTrigger/python/HLTSetupWithL1MuonEmulator_cff.py
@@ -0,0 +1,105 @@
+import FWCore.ParameterSet.Config as cms
+
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+#
+# Running the HLT table directly from Fast Simulation 
+#
+#--- Dummy replacements of HLT modules ---#
+hltEcalPreshowerDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRawToRecHitFacility = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEtaFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEtaRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalJetsFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalJetsDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalJetsWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalJetsRecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalMuonsFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalMuonsDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalMuonsWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalMuonsRecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEgammaFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEgammaDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEgammaWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalEgammaRecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalTausFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalTausDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalTausWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalTausRecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltHcalDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalRestFEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalRestDigis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalRestWeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalRestRecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltL3SingleTauPixelSeeds = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltL3SingleTauPixelSeedsRelaxed = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltL3SingleTauMETPixelSeeds = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltL3SingleTauMETPixelSeedsRelaxed = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltBLifetimeRegionalPixelSeedGeneratorStartupU = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltBLifetimeRegionalPixelSeedGeneratorStartup = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltBLifetimeRegionalPixelSeedGenerator = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltBLifetimeRegionalPixelSeedGeneratorRelaxed = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltMumuPixelSeedFromL2Candidate = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltMumukPixelSeedFromL2Candidate = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalPi0FEDs = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalPi0Digis = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalPi0WeightUncalibRecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+hltEcalRegionalPi0RecHitTmp = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+#import FastSimulation.HighLevelTrigger.DummyModule_cfi
+#hltEcalRegionalPi0RecHit = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+#
+# Tracker = Pixel and Strip
+#
+from FastSimulation.HighLevelTrigger.RecoLocalTracker_cff import *
+#
+# Calorimeter = Ecal and Hcal
+#
+from FastSimulation.HighLevelTrigger.RecoLocalCalo_cff import *
+from FastSimulation.HighLevelTrigger.EcalRegionalReco_cff import *
+# Specific reconstruction sequences for FastSimulation
+from FastSimulation.HighLevelTrigger.HLTFastRecoWithL1MuonEmulator_cff import *
+HLTDoLocalPixelSequence = cms.Sequence(pixeltrackerlocalreco)
+HLTDoLocalStripSequence = cms.Sequence(striptrackerlocalreco)
+
+
