bcd9e1ac3b937ccc131fee578d3ba58c5fea8a9e
Yurii Maravin <sha1-9850ca7e1f871752f13896386a78e9adc7312787@cern.ch>
2009-03-18 13:18:19 +0000

    Updated correction coefficients for hybrid superclusters in 31X

--- RecoEcal/EgammaClusterProducers/python/correctedHybridSuperClusters_cfi.py
+++ RecoEcal/EgammaClusterProducers/python/correctedHybridSuperClusters_cfi.py
@@ -13,15 +13,13 @@ correctedHybridSuperClusters = cms.EDFilter("EgammaSCCorrectionMaker",
     hyb_fCorrPset = cms.PSet(
 
         brLinearLowThr = cms.double(1.1),
-        fBremVec = cms.vdouble(-0.0589, 0.1499, 0.9094, -0.0008905, 1.327),
-        brLinearHighThr = cms.double(7.0),
+        fBremVec = cms.vdouble(-0.04382, 0.1169, 0.9267, -0.0009413, 1.419),
+        brLinearHighThr = cms.double(8.0),
 
-        fEtEtaVec = cms.vdouble(1.0005, -0.5407, 0.9820,    0,
-                                0,       0.5654, 11.78, 1.154,
-                                0,            0,     0,      0,
-                                0,            0,     0,      0,
-                                8.0,     1.023,  -0.00181,
-                                0, 0, 0)
+        fEtEtaVec = cms.vdouble(0,
+                                1.00121, -0.63672,      0,      0,
+                                0,         0.5655,  6.457, 0.5081,
+                                8.0,     1.023,  -0.00181)
     ),
     recHitProducer = cms.InputTag("ecalRecHit","EcalRecHitsEB")
 )
