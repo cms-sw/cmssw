5d5753fc628d69edb07beae601ad8d6e80569307
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2009-03-19 16:00:36 +0000

    python script for testing.

---
+++ SimTracker/SiPixelDigitizer/test/testPixelDigitizer.py
@@ -0,0 +1,126 @@
+# python script Pixel Digitizer testing:
+# author: V. Cuplov
+#
+# Process available:
+# 1) simulation of hits in the Tracker
+# 2) digitization + validation
+# 3) reconstruction or clusterization + validation 
+# 4) tracks + validation
+#
+##############################################################################
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DigiTest")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("Validation.TrackerHits.trackerHitsValidation_cff")
+process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+process.load("SimTracker.Configuration.SimTracker_cff")
+process.load("Validation.TrackerDigis.trackerDigisValidation_cff")
+process.load("SimG4Core.Configuration.SimG4Core_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+## For 31X use:
+# process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+process.load("RecoTracker.TrackProducer.RefitterWithMaterial_cff")
+
+process.load("Validation.TrackerRecHits.trackerRecHitsValidation_cff")
+process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
+process.load("Validation.TrackingMCTruth.trackingTruthValidation_cfi")
+process.load("Validation.RecoTrack.TrackValidation_cff")
+process.load("Validation.RecoTrack.SiTrackingRecHitsValid_cff")
+process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
+process.load("EventFilter.SiPixelRawToDigi.SiPixelDigiToRaw_cfi")
+process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+#process.MessageLogger = cms.Service("MessageLogger",
+#    debugModules = cms.untracked.vstring('PixelDigisTest'),
+#    destinations = cms.untracked.vstring('cout'),
+#    destinations = cms.untracked.vstring("log","cout"),
+#    cout = cms.untracked.PSet(
+#        threshold = cms.untracked.string('ERROR')
+#    )
+#    log = cms.untracked.PSet(
+#        threshold = cms.untracked.string('DEBUG')
+#    )
+#)
+
+
+# get the files from DBS:
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/1EF32A82-57E2-DD11-A475-000423D6B444.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/28804479-4BE2-DD11-A32D-000423D98EA8.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/44067402-4BE2-DD11-BD29-0030487C6090.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/6CCDD56F-4BE2-DD11-9078-001D09F27067.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/888637D0-4AE2-DD11-B5AD-000423D6CA6E.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/B6E15573-4BE2-DD11-B754-001D09F24E39.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/B8148F8C-4BE2-DD11-B0C8-001D09F28D54.root','/store/relval/CMSSW_3_0_0_pre6/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/IDEAL_30X_v3/0005/D4855570-4BE2-DD11-8793-000423D98930.root')
+)
+
+#process.PoolDBESSource = cms.ESSource("PoolDBESSource",
+#    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+#    DBParameters = cms.PSet(
+#        messageLevel = cms.untracked.int32(0),
+#        authenticationPath = cms.untracked.string('')
+#    ),
+#    timetype = cms.string('runnumber'),
+#    toGet = cms.VPSet(cms.PSet(
+#        record = cms.string('SiPixelQualityRcd'),
+#        tag = cms.string('SiPixelBadModule_test')
+#    )),
+#    connect = cms.string('sqlite_file:test.db')
+#)
+
+
+# Choose the global tag here:
+#process.GlobalTag.globaltag = 'COSMMC_21X_V1::All'
+#process.GlobalTag.globaltag = 'IDEAL_30X::All'
+process.GlobalTag.globaltag = 'STARTUP_30X::All'
+#process.GlobalTag.globaltag = 'CRAFT_30X::All'
+
+
+
+#rfio:/castor/cern.ch/user/v/vesna/test.root
+
+process.o1 = cms.OutputModule("PoolOutputModule",
+            fileName = cms.untracked.string('file:/afs/cern.ch/user/v/vesna/DigitizerWork/CMSSW_3_0_0_pre6/src/SimTracker/SiPixelDigitizer/test/Digis_test.root')                              
+)
+
+process.Timing = cms.Service("Timing")
+
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck")
+
+# Simulation of hits in the Tracker:
+#process.simhits = cms.Sequence(process.g4SimHits*process.trackerHitsValidation)
+
+# Digitization:
+process.digis = cms.Sequence(process.simSiPixelDigis*process.pixelDigisValid)
+
+# Reconstruction or Clusterization:
+#process.rechits = cms.Sequence(process.pixeltrackerlocalreco*process.pixRecHitsValid)
+process.rechits = cms.Sequence(process.pixeltrackerlocalreco*process.siStripMatchedRecHits*process.pixRecHitsValid)
+
+process.tracks = cms.Sequence(process.offlineBeamSpot*process.recopixelvertexing*process.trackingParticles*process.trackingTruthValid*process.ckftracks*process.trackerRecHitsValidation)
+process.trackinghits = cms.Sequence(process.TrackRefitter*process.trackingRecHitsValid)
+
+# To use when you want to look at Residuals and Pulls:
+#process.p1 = cms.Path(process.mix*process.digis*process.siPixelRawData*process.siPixelDigis*process.rechits*process.tracks*process.trackinghits)
+
+
+#This process to get events to be used to make DQM occupancy maps (run the Pixel_occupancy_digi.py after you ran testPixelDigitizer.py):
+#process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.siPixelRawData*process.siPixelDigis)
+
+#This process is to run the digis validation:
+#process.p1 = cms.Path(process.mix*process.simSiPixelDigis*process.pixelDigisValid)
+
+#This process is to run the digitizer:
+process.p1 = cms.Path(process.mix*process.simSiPixelDigis)
+
+process.outpath = cms.EndPath(process.o1)
+process.g4SimHits.Generator.HepMCProductLabel = 'source'
+
