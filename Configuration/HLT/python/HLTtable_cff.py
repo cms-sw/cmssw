348203b73950ee106bfefa7eebe22bd6933bfa90
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-03-19 21:13:43 +0000

    New 31X default is 8E29 table.

--- Configuration/HLT/python/HLTtable_cff.py
+++ Configuration/HLT/python/HLTtable_cff.py
@@ -6,10 +6,10 @@ import FWCore.ParameterSet.Config as cms
 # Pick one and only one from those exported from HLTrigger/Configuration
 #
 # 1: the fat old 160-paths HLT table used up to 22X
-from HLTrigger.Configuration.HLT_2E30_cff import *
+# from HLTrigger.Configuration.HLT_2E30_cff import *
 #
 # 2: the new lean low-lumi table (8e29)
-# from HLTrigger.Configuration.HLT_8E29_cff import *
+from HLTrigger.Configuration.HLT_8E29_cff import *
 #
 # 3: the new lean high-lumi table (1e31)
 # from HLTrigger.Configuration.HLT_1E31_cff import *
