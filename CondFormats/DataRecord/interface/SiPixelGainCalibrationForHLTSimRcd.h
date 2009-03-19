d75603ae361ea94d79be0dd9422947bdced620dd
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2009-03-19 15:54:44 +0000

    added SimRcd, used by simulation only.

---
+++ CondFormats/DataRecord/interface/SiPixelGainCalibrationForHLTSimRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_SiPixelGainCalibrationForHLTSimRcd_h
+#define DataRecord_SiPixelGainCalibrationForHLTSimRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiPixelGainCalibrationForHLTSimRcd
+// 
+/**\class SiPixelGainCalibrationForHLTSimRcd SiPixelGainCalibrationForHLTSimRcd.h CondFormats/DataRecord/interface/SiPixelGainCalibrationForHLTSimRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Fri Mar 13 12:13:06 CET 2009
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiPixelGainCalibrationForHLTSimRcd : public edm::eventsetup::EventSetupRecordImplementation<SiPixelGainCalibrationForHLTSimRcd> {};
+
+#endif
