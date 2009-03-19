7e80446ca00e2e1c0259f1d5b868b3a328236ca0
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-03-19 14:14:51 +0000

    udpated hlt paths and incorporated hlt filter in the same file

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalDijets_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalDijets_cff.py
@@ -3,7 +3,16 @@ import FWCore.ParameterSet.Config as cms
 #------------------------------------------------
 #AlCaReco filtering for HCAL minbias:
 #------------------------------------------------
+import HLTrigger.HLTfilters.hltHighLevel_cfi
 from Calibration.HcalAlCaRecoProducers.alcadijets_cfi import *
-from Calibration.HcalAlCaRecoProducers.dijetsHLT_cfi import *
+
+
+dijetsHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ['HLT_Jet30','HLT_Jet50','HLT_Jet80',     #1E31 Menu
+                'HLT_Jet15U','HLT_Jet30U','HLT_Jet50U'], #8E29 Menu
+    throw = False
+)
+
+
 seqALCARECOHcalCalDijets = cms.Sequence(dijetsHLT*DiJProd)
 
