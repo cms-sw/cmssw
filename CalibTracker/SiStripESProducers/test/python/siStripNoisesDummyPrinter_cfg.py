5a7e3987391e33f970c95f121c25b0fa96c5abb7
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-03-20 12:17:54 +0000

    added cfg for  templated DummyCondObjPrinter

---
+++ CalibTracker/SiStripESProducers/test/python/siStripNoisesDummyPrinter_cfg.py
@@ -0,0 +1,50 @@
+# The following comments couldn't be translated into the new config version:
+
+# upload to database 
+
+#string timetype = "timestamp"    
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Reader")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring(''),
+    NoisesReader = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    destinations = cms.untracked.vstring('NoisesReader.log')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("EmptySource",
+    numberEventsInRun = cms.untracked.uint32(1),
+    firstRun = cms.untracked.uint32(100000)
+)
+
+process.poolDBESSource = cms.ESSource("PoolDBESSource",
+   BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+   DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(2),
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:dbfile.db'),
+#    connect = cms.string('oracle://cms_orcoff_prod/CMS_COND_21X_STRIP'),
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('SiStripNoisesRcd'),
+#        tag = cms.string('SiStripNoise_CRAFT_21X_v4_offline')
+        tag = cms.string('SiStripNoises_Fake_30X')
+    ))
+)
+
+process.reader = cms.EDFilter("SiStripNoisesDummyPrinter")
+                              
+process.p1 = cms.Path(process.reader)
+
+
