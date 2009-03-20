5eb38e07fdf903f716f142ec0ee9b116e61a6080
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2009-03-20 09:45:49 +0000

    Restoring L1GctJetCounts

--- DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
@@ -15,6 +15,7 @@
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctJetCand.h"
 
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctEtSums.h"
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctJetCounts.h"
 
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctHFRingEtSums.h"
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctHFBitCounts.h"
@@ -33,6 +34,7 @@ typedef std::vector<L1GctEtHad> L1GctEtHadCollection;
 typedef std::vector<L1GctEtMiss> L1GctEtMissCollection;
 typedef std::vector<L1GctEtTotal> L1GctEtTotalCollection;
 typedef std::vector<L1GctHtMiss> L1GctHtMissCollection;
+typedef std::vector<L1GctJetCounts> L1GctJetCountsCollection;
 typedef std::vector<L1GctHFRingEtSums> L1GctHFRingEtSumsCollection;
 typedef std::vector<L1GctHFBitCounts> L1GctHFBitCountsCollection;
 
