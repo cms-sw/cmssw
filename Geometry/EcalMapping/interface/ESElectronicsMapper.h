b8228909bc81f21263c943657d16a4fffec378b7
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-03-20 15:45:35 +0000

    add functions
    - findXY (convert eta,phi to ES X,Y)
    - GetListOfFEDs

--- Geometry/EcalMapping/interface/ESElectronicsMapper.h
+++ Geometry/EcalMapping/interface/ESElectronicsMapper.h
@@ -10,23 +10,24 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/FileInPath.h"
 #include "DataFormats/EcalDetId/interface/ESDetId.h"
-
-using namespace std;
-using namespace edm;
+#include "RecoEcal/EgammaCoreTools/interface/EcalEtaPhiRegion.h"
 
 class ESElectronicsMapper {
 
  public:
 
-  ESElectronicsMapper(const ParameterSet& ps);
+  ESElectronicsMapper(const edm::ParameterSet& ps);
   ~ESElectronicsMapper() {};
 
   int getFED(const ESDetId& id);
   int getFED(int zside, int plane, int x, int y);
+  std::vector<int> GetListofFEDs(const EcalEtaPhiRegion region) const ;
+  void GetListofFEDs(const EcalEtaPhiRegion region, std::vector<int> & FEDs) const ;
+  void findXY(const int plane, const double eta, const double phi, int &row, int &col) const;
 
  private:
 
-  FileInPath lookup_;
+  edm::FileInPath lookup_;
 
   int fed_[2][2][40][40];
 
--- Geometry/EcalMapping/src/ESElectronicsMapper.cc
+++ Geometry/EcalMapping/src/ESElectronicsMapper.cc
@@ -1,8 +1,8 @@
 #include "Geometry/EcalMapping/interface/ESElectronicsMapper.h"
 
-ESElectronicsMapper::ESElectronicsMapper(const ParameterSet& ps) {
+ESElectronicsMapper::ESElectronicsMapper(const edm::ParameterSet& ps) {
 
-  lookup_ = ps.getUntrackedParameter<FileInPath>("LookupTable");
+  lookup_ = ps.getUntrackedParameter<edm::FileInPath>("LookupTable");
 
   for (int i=0; i<2; ++i) 
     for (int j=0; j<2; ++j) 
@@ -12,7 +12,7 @@ ESElectronicsMapper::ESElectronicsMapper(const ParameterSet& ps) {
  
   // read in look-up table
   int nLines, z, iz, ip, ix, iy, fed, kchip, pace, bundle, fiber, optorx;
-  ifstream file;
+  std::ifstream file;
   file.open(lookup_.fullPath().c_str());
   if( file.is_open() ) {
 
@@ -28,7 +28,7 @@ ESElectronicsMapper::ESElectronicsMapper(const ParameterSet& ps) {
     }
 
   } else {
-    cout<<"ESElectronicsMapper::ESElectronicsMapper : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<endl;
+    std::cout<<"ESElectronicsMapper::ESElectronicsMapper : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<std::endl;
   }
 
 }
@@ -46,3 +46,95 @@ int ESElectronicsMapper::getFED(int zside, int plane, int x, int y) {
 
   return fed_[zside-1][plane-1][x-1][y-1]; 
 } 
+
+std::vector<int> ESElectronicsMapper::GetListofFEDs(const EcalEtaPhiRegion region) const {
+  std::vector<int> FEDs;
+  GetListofFEDs(region, FEDs);
+  return FEDs;
+}
+
+void ESElectronicsMapper::GetListofFEDs(const EcalEtaPhiRegion region, std::vector<int> & FEDs) const {
+
+  double etahigh = region.etaHigh();
+  double phihigh = region.phiHigh();  
+  double etalow = region.etaLow();
+  double philow = region.phiLow();
+
+  int zside = (etahigh > 0) ? 1 : 2;
+
+  int x[4][2], y[4][2], fed[4][2];
+
+  // for plane 1
+  findXY(1, etahigh, phihigh, x[0][1], y[0][1]);
+  findXY(1, etahigh, philow,  x[1][1], y[1][1]);
+  findXY(1, etalow,  phihigh, x[2][1], y[2][1]);
+  findXY(1, etalow,  philow,  x[3][1], y[3][1]);
+
+  // for plane 2
+  findXY(2, etahigh, phihigh, x[0][2], y[0][2]);
+  findXY(2, etahigh, philow,  x[1][2], y[1][2]);
+  findXY(2, etalow,  phihigh, x[2][2], y[2][2]);
+  findXY(2, etalow,  philow,  x[3][2], y[3][2]);
+  
+  for (int i=0; i<4; ++i) 
+    for (int j=0; j<2; ++j) {
+      if (x[i][j]<0 || y[i][j]<0) continue;
+      fed[i][j] = fed_[zside-1][j][x[i][j]][y[i][j]];
+      FEDs.push_back(fed[i][j]);
+    }
+
+  sort(FEDs.begin(), FEDs.end());
+  unique(FEDs.begin(), FEDs.end());
+
+}
+
+void ESElectronicsMapper::findXY(const int plane, const double eta, const double phi, int &row, int &col) const {
+  
+  float zplane_[2]= { 303.353, 307.838 };
+  
+  float waf_w_ = 6.3; 
+  float act_w_ = 6.1; 
+  float intra_lad_gap_ = 0.04; 
+  float inter_lad_gap_ = 0.05;
+  float centre_gap_ = 0.05; 
+
+  float theta = 2.*atan(exp(-1.*eta));
+  float r = zplane_[plane-1]/cos(theta);
+  float x = r * sin(theta) * cos(phi);
+  float y = r * sin(theta) * sin(phi);
+  
+  float x0,y0;
+
+  if (plane == 1) {
+    x0 = x;
+    y0 = y;
+  } else {
+    y0 = x;
+    x0 = y;
+  }
+
+  //find row
+  int imul = (y0 < 0.) ? +1 : -1 ; 
+  float yr = -(y0 + imul*centre_gap_ )/act_w_;
+  row = (yr < 0.) ? (19 + int(yr) ) : (20 + int(yr));
+  row= 40 - row;
+
+  if (row < 1 || row > 40 ) row = -1;
+  
+  //find col
+  col = 40 ;
+  int nlad = (col < 20 ) ? (20-col)/2 :(19-col)/2 ;
+  float edge =  (20-col) * (waf_w_ + intra_lad_gap_)+ nlad * inter_lad_gap_;
+  edge = -edge;
+  while (x0 < edge && col > 0){
+    col--;
+    nlad = (col < 20 ) ? (20-col)/2 :(19-col)/2 ;
+    edge = (20-col) * (waf_w_ + intra_lad_gap_) + nlad * inter_lad_gap_;   
+    edge = -edge;
+  }
+  
+  col++;
+
+  if ( col < 1 || col > 40 || x0 < edge) col = -1;
+
+}
