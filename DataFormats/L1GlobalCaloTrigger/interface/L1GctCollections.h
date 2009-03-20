54b1d0f36d68da04d134595863055027c7a50d18
Robert Frazier <sha1-273486ace982b9e0b4eb051eb902f3dfb02026ef@cern.ch>
2009-03-20 01:35:20 +0000

    Updated to include an L1GctInternHtMissCollection

--- DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h
@@ -8,6 +8,7 @@
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctInternJetData.h"
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctInternEtSum.h"
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctInternHFData.h"
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctInternHtMiss.h"
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctFibreWord.h"
 
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctEmCand.h"
@@ -22,6 +23,7 @@ typedef std::vector<L1GctInternEmCand> L1GctInternEmCandCollection;
 typedef std::vector<L1GctInternJetData> L1GctInternJetDataCollection;
 typedef std::vector<L1GctInternEtSum> L1GctInternEtSumCollection;
 typedef std::vector<L1GctInternHFData> L1GctInternHFDataCollection;
+typedef std::vector<L1GctInternHtMiss> L1GctInternHtMissCollection;
 typedef std::vector<L1GctFibreWord> L1GctFibreCollection;
 
 typedef std::vector<L1GctEmCand> L1GctEmCandCollection;
