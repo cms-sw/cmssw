5feb5af21adf5e2e7f5339f8931fb94a8e48a8fe
noli <sha1-79077162a7b66ac5598c8f3f5066dbb65924e5d7@cern.ch>
2009-03-20 10:36:00 +0000

    add new flag

--- ElectroWeakAnalysis/ZMuMu/plugins/ZHLTMatchFilter.cc
+++ ElectroWeakAnalysis/ZMuMu/plugins/ZHLTMatchFilter.cc
@@ -26,13 +26,16 @@ namespace modules {
       bool bothTriggerFlag = false;
       bool atLeastOneTriggerFlag=false;
       bool FirstTriggerFlag = false;
-      if(((cond_ !="exactlyOneMatched" && cond_!="atLeastOneMatched") && cond_ !="bothMatched") && cond_ != "firstMatched")
+      bool globalisTriggerFlag =false;
+      if((((cond_ !="exactlyOneMatched" && cond_!="atLeastOneMatched") && cond_ !="bothMatched") && cond_ != "firstMatched") && cond_ != "globalisMatched")
 	throw edm::Exception(edm::errors::Configuration) 
 	  << "Invalid condition type: " << cond_ << ". Valid types are:"
-	  << " exactlyOneMatched, atLeastOneMatched, bothMatched, firstMatched\n";
+	  << " exactlyOneMatched, atLeastOneMatched, bothMatched, firstMatched,globalisMatched\n";
       const reco::Candidate* dau0 = z.daughter(0);
       const reco::Candidate * m0 = &*dau0->masterClone();
       const pat::Muon * mu0 = dynamic_cast<const pat::Muon*>(m0);//cast in patMuon
+      bool firstdismuon = (dau0->isGlobalMuon() ? true : false); 
+      bool firstdisStandAlone = (dau0->isStandAloneMuon() ? true : false); 
       if(mu0 != 0){
 	const std::vector<pat::TriggerPrimitive> & trig0 =mu0->triggerMatches();//vector of triggerPrimitive
 	int dimTrig0 = trig0.size();
@@ -47,9 +50,10 @@ namespace modules {
       }
       const reco::Candidate* dau1 = z.daughter(1);
       const reco::Candidate * m1 = &*dau1->masterClone();
-      const pat::Muon * mu1 = dynamic_cast<const pat::Muon*>(m1);
       bool secondismuon = (dau1->isGlobalMuon() ? true : false);    
-      if(mu1 != 0 && secondismuon){
+      bool secondisStandAlone = (dau1->isStandAloneMuon() ? true : false); 
+      const pat::Muon * mu1 = dynamic_cast<const pat::Muon*>(m1);
+      if(mu1 != 0 && (secondismuon ||secondisStandAlone) ){
 	const std::vector<pat::TriggerPrimitive> & trig1 =mu1->triggerMatches();
 	int dimTrig1 = trig1.size();
 	if(dimTrig1 !=0){
@@ -61,16 +65,17 @@ namespace modules {
 	  }
 	}
       }
-      
       if(!singleTrigFlag0 && !singleTrigFlag1)return false;
       if((singleTrigFlag0 && singleTrigFlag1) && secondismuon ) bothTriggerFlag = true;
       if(((singleTrigFlag0 && !singleTrigFlag1) && secondismuon) || ((!singleTrigFlag0 && singleTrigFlag1) && secondismuon)) exactlyOneTriggerFlag = true;
+      if(((singleTrigFlag0  && firstdismuon) && secondisStandAlone) || ((singleTrigFlag1 && secondismuon) && firstdisStandAlone))globalisTriggerFlag = true;
       if((singleTrigFlag0 && !singleTrigFlag1) && !secondismuon) FirstTriggerFlag = true;
       if((singleTrigFlag0 || singleTrigFlag1) && secondismuon) atLeastOneTriggerFlag=true;
       if(cond_=="exactlyOneMatched") return exactlyOneTriggerFlag;
       if(cond_=="atLeastOneMatched") return atLeastOneTriggerFlag;
       if(cond_=="bothMatched") return bothTriggerFlag;
       if(cond_=="firstMatched") return FirstTriggerFlag; 
+      if(cond_=="globalisMatched") return globalisTriggerFlag; 
       
       return false;
       }
