033453a2a5df8ff7369e42a13f7c6b4f0cf0ba08
Claude Charlot <sha1-5503bd15f2dc44299f7f663420dce7b3ea3927c4@cern.ch>
2009-03-21 22:59:45 +0000

    commented out unused parameters

--- RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeeds_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/ecalDrivenElectronSeeds_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 #
 # module to produce pixel seeds for electrons from super clusters
-# $Id: ecalDrivenElectronSeeds_cfi.py,v 1.8 2008/10/12 08:38:04 charlot Exp $
+# $Id: ecalDrivenElectronSeeds_cfi.py,v 1.1 2009/01/12 16:45:53 chamont Exp $
 # Author:  Ursula Berthon, Claude Charlot
 #
 from RecoEgamma.EgammaElectronProducers.ecalDrivenElectronSeedsParameters_cff import *
@@ -10,24 +10,24 @@ from RecoEgamma.EgammaElectronProducers.ecalDrivenElectronSeedsParameters_cff im
 ecalDrivenElectronSeeds = cms.EDProducer("ElectronSeedProducer",
     barrelSuperClusters = cms.InputTag("correctedHybridSuperClusters"),
     endcapSuperClusters = cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"),
-    SeedConfiguration = cms.PSet(
-        ecalDrivenElectronSeedsParameters,
-        OrderedHitsFactoryPSet = cms.PSet(
-            ComponentName = cms.string('StandardHitPairGenerator'),
-            SeedingLayers = cms.string('MixedLayerPairs') 
-
-        ),
-        TTRHBuilder = cms.string('WithTrackAngle'),
-        # eta-phi region
-        RegionPSet = cms.PSet(
-            deltaPhiRegion = cms.double(0.7),
-            originHalfLength = cms.double(15.0),
-            useZInVertex = cms.bool(True),
-            deltaEtaRegion = cms.double(0.3),
-            ptMin = cms.double(1.5),
-            originRadius = cms.double(0.2),
-            VertexProducer = cms.InputTag("pixelVertices")
-        )
+#    SeedConfiguration = cms.PSet(
+#        ecalDrivenElectronSeedsParameters,
+#        OrderedHitsFactoryPSet = cms.PSet(
+#            ComponentName = cms.string('StandardHitPairGenerator'),
+#            SeedingLayers = cms.string('MixedLayerPairs') 
+#
+#        ),
+#        TTRHBuilder = cms.string('WithTrackAngle'),
+#        # eta-phi region
+#        RegionPSet = cms.PSet(
+#            deltaPhiRegion = cms.double(0.7),
+#            originHalfLength = cms.double(15.0),
+#            useZInVertex = cms.bool(True),
+#            deltaEtaRegion = cms.double(0.3),
+#            ptMin = cms.double(1.5),
+#            originRadius = cms.double(0.2),
+#            VertexProducer = cms.InputTag("pixelVertices")
+#        )
     )
 )
 
