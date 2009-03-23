4ec259e5444c3b64f096544f6334238515c990d8
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-03-23 16:07:46 +0000

    we now have two maps for 3.8T, this is the updated one. Sooner or later it could become the official one

---
+++ Configuration/StandardSequences/python/MagneticField_38T_UpdatedMap_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+# This cfi contains everything needed to use the VolumeBased magnetic
+# field engine.
+#Default is version 85l
+from MagneticField.Engine.volumeBasedMagneticField_1103l_cfi import *
+
+
