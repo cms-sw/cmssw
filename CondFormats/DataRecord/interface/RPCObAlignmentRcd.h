932ec974eb313da0350928724c1b420ed9f57bb7
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-03-24 10:28:08 +0000

    first release

---
+++ CondFormats/DataRecord/interface/RPCObAlignmentRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_RPCObAlignmentRcd_h
+#define DataRecord_RPCObAlignmentRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCObAlignmentRcd
+// 
+/**\class RPCObAlignmentRcd RPCObAlignmentRcd.h CondFormats/DataRecord/interface/RPCObAlignmentRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Tue Mar 24 11:26:21 CET 2009
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class RPCObAlignmentRcd : public edm::eventsetup::EventSetupRecordImplementation<RPCObAlignmentRcd> {};
+
+#endif
--- CondFormats/RPCObjects/src/T_EventSetup_RPCObAlignment.cc
+++ CondFormats/RPCObjects/src/T_EventSetup_RPCObAlignment.cc
@@ -1,4 +1,4 @@
 #include "CondFormats/RPCObjects/interface/RPCObAlignment.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-EVENTSETUP_DATA_REG(RPCObFebmap);
+EVENTSETUP_DATA_REG(RPCObAlignment);
  
