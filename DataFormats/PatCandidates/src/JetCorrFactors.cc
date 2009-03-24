f535f09a84615271760f3651d982e1b543428b3b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-03-24 18:48:12 +0000

    port from B22X_Production_v1, cosmetics

--- DataFormats/PatCandidates/src/JetCorrFactors.cc
+++ DataFormats/PatCandidates/src/JetCorrFactors.cc
@@ -1,5 +1,5 @@
 //
-// $Id: JetCorrFactors.cc,v 1.3 2009/01/09 17:37:47 auterman Exp $
+// $Id: JetCorrFactors.cc,v 1.4 2009/02/19 15:39:29 rwolf Exp $
 //
 
 #include "FWCore/Utilities/interface/EDMException.h"
@@ -68,10 +68,13 @@ float
 JetCorrFactors::correction(CorrStep laststep, CorrStep begin) const 
 {
   // first make sure that begin < end
-  if (laststep == begin) return 1.0;
-  else if (begin > laststep) return 1.0/correction(begin,laststep);
-  
-  size_t step = istep(begin) + 1;
+  if (laststep == begin){
+    return 1.0;
+  }
+  else if (begin > laststep){
+    return 1.0/correction(begin,laststep);
+  }
+  size_t step = istep(begin)+1;
   size_t myflav = iflav(laststep), oldflav = iflav(begin);
   // begin==5 and flavor is not 0 as default
   if (myflav != oldflav && step==6) myflav = oldflav;
