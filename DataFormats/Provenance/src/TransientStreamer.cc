122f6b110c4b117ca16302094159ccb3343245bc
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-03-24 17:04:04 +0000

    Cache ProcessConfiguration and Parentage IDs

--- DataFormats/Provenance/src/TransientStreamer.cc
+++ DataFormats/Provenance/src/TransientStreamer.cc
@@ -1,6 +1,8 @@
 #include "DataFormats/Provenance/interface/BranchDescription.h"
 #include "DataFormats/Provenance/interface/ProductProvenance.h"
 #include "DataFormats/Provenance/interface/FileIndex.h"
+#include "DataFormats/Provenance/interface/Parentage.h"
+#include "DataFormats/Provenance/interface/ProcessConfiguration.h"
 #include "DataFormats/Provenance/interface/ProcessHistory.h"
 #include "DataFormats/Provenance/interface/ProductRegistry.h"
 #include "DataFormats/Provenance/interface/Transient.h"
@@ -11,6 +13,8 @@ namespace edm {
     SetTransientStreamer<Transient<BranchDescription::Transients> >();
     SetTransientStreamer<Transient<ProductProvenance::Transients> >();
     SetTransientStreamer<Transient<FileIndex::Transients> >();
+    SetTransientStreamer<Transient<Parentage::Transients> >();
+    SetTransientStreamer<Transient<ProcessConfiguration::Transients> >();
     SetTransientStreamer<Transient<ProcessHistory::Transients> >();
     SetTransientStreamer<Transient<ProductRegistry::Transients> >();
   }
