e01fadd77c7df9b103ca37e2679e3100fe2b6d66
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-03-24 10:26:08 +0000

    add geometry

--- DQM/HLTEvF/test/hlt_muon_dqm-file_cfg.py
+++ DQM/HLTEvF/test/hlt_muon_dqm-file_cfg.py
@@ -6,11 +6,17 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQM.HLTEvF.HLTMonitor_MuonDQM_cff")
 process.load("DQM.HLTEvF.HLTMonitorClient_cff")
 
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
+process.GlobalTag.globaltag = 'IDEAL_30X::All'
+process.prefer("GlobalTag")
+
+process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" )
 
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    #input = cms.untracked.int32(100)
     input = cms.untracked.int32(-1)
 )
 
@@ -18,14 +24,7 @@ process.source = cms.Source("PoolSource",
     debugVerbosity = cms.untracked.uint32(1),
     debugVebosity = cms.untracked.bool(True),
     fileNames = cms.untracked.vstring(
-    #'file:/tmp/wteo/BA6DA407-A985-DD11-A0D8-000423D9A2AE.root',
-    #'file:/tmp/wteo/52BAB7DF-0487-DD11-95A3-000423D9989E.root'
-    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_doubleMuonPath_v1_0.root',
-    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_doubleMuonPath_v1_1.root'
-    #'file:/tmp/hdyoo/sample/data/CRUZET3/CRUZET3.root'
-    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_multiCosmicMuon_v1.root'
-    'file:/tmp/hdyoo/sample/MC/CMSSW_2_1_9/RelValSingleMuPt100.root'
-    #'file:/tmp/hdyoo/sample/data/PreCRUZET4Test1/PreCRUZET4Test1.root'
+    'file:/tmp/hdyoo/28F5E252-180A-DE11-87E2-000423D94A20.root'
     )
 )
 
@@ -48,7 +47,7 @@ process.DQM.collectorHost = 'srv-c2d05-12'
 process.DQM.collectorPort = 9190
 process.dqmSaver.dirName = '.'
 process.dqmSaver.producer = 'Playback'
-process.hltResults.plotAll = True
+#process.hltResults.plotAll = True
 process.dqmSaver.convention = 'Online'
 process.dqmEnv.subSystemFolder = 'HLT'
 process.dqmSaver.saveByRun = 1
