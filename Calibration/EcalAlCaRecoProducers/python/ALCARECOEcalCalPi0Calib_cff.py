4e0287b01849841c91ed5ba1e2813322b9fc6a40
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-03-24 13:27:55 +0000

    Switched on laser correction

--- Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalPi0Calib_cff.py
+++ Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalPi0Calib_cff.py
@@ -4,5 +4,19 @@ import FWCore.ParameterSet.Config as cms
 #AlCaReco pi0 calibration:
 #------------------------------------------------
 from Calibration.EcalAlCaRecoProducers.alcastreamEcalPi0Calib_cff import *
-seqALCARECOEcalCalPi0Calib = cms.Sequence(ecalpi0CalibHLT)
+import RecoLocalCalo.EcalRecProducers.ecalRecalibRecHit_cfi
 
+#apply laser corrections
+
+ecalPi0Corrected =  RecoLocalCalo.EcalRecProducers.ecalRecalibRecHit_cfi.ecalRecHit.clone(
+            doEnergyScale = cms.bool(False),
+            doIntercalib = cms.bool(False),
+            EERecHitCollection = cms.InputTag("hltAlCaPi0RegRecHits","pi0EcalRecHitsEE"),
+            EBRecHitCollection = cms.InputTag("hltAlCaPi0RegRecHits","pi0EcalRecHitsEB"),
+            doLaserCorrections = cms.bool(True),
+            EBRecalibRecHitCollection = cms.string('pi0EcalRecHitsEB'),
+            EERecalibRecHitCollection = cms.string('pi0EcalRecHitsEE')
+)
+
+
+seqALCARECOEcalCalPi0Calib = cms.Sequence(ecalpi0CalibHLT*ecalPi0Corrected)
