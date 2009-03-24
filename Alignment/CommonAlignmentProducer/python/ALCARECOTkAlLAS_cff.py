4f96d2d664e4e19cb355127214317e8b8b976a3e
Jan Olzem <sha1-713996176e893ae17e542633137001ed8a2b989e@cern.ch>
2009-03-24 18:11:51 +0000

    * modified ALCARECOTkAlLAS_cff.py, now running the SiStripRawToDigi module from here

--- Alignment/CommonAlignmentProducer/python/ALCARECOTkAlLAS_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOTkAlLAS_cff.py
@@ -3,7 +3,24 @@ import FWCore.ParameterSet.Config as cms
 #---------------------------------------------------
 # AlCaReco filtering for the Tracker Laser ALignment
 #---------------------------------------------------
-# create sequence for rechit filtering for phi symmetry calibration
-from Alignment.LaserAlignment.LaserAlignmentT0Producer_cfi import *
-seqALCARECOTkAlLAS = cms.Sequence(laserAlignmentT0Producer)
+
+# need to run the digitizer on raw strip data first
+import EventFilter.SiStripRawToDigi.SiStripDigis_cfi
+TkAlLASsiStripDigis = EventFilter.SiStripRawToDigi.SiStripDigis_cfi.siStripDigis.clone(
+  ProductLabel = cms.untracked.string( 'source' )
+)
+
+# redefine the input digis according to the clone's name
+import Alignment.LaserAlignment.LaserAlignmentT0Producer_cfi
+laserAlignmentT0Producer = Alignment.LaserAlignment.LaserAlignmentT0Producer_cfi.laserAlignmentT0Producer.clone(
+  DigiProducerList = cms.VPSet(
+    cms.PSet(
+      DigiLabel = cms.string( 'ZeroSuppressed' ),
+      DigiType = cms.string( 'Processed' ),
+      DigiProducer = cms.string( 'TkAlLASsiStripDigis' )
+    )
+  )
+)
+
+seqALCARECOTkAlLAS = cms.Sequence( TkAlLASsiStripDigis + laserAlignmentT0Producer )
 
