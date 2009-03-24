4a5840655d9fe5c67462c19cf4eac9c27ab25702
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2009-03-24 10:01:21 +0000

    added the module DTPreCalibrationTask

--- DQMOffline/CalibMuon/src/SealModule.cc
+++ DQMOffline/CalibMuon/src/SealModule.cc
@@ -4,8 +4,10 @@
 #include "DQMOffline/CalibMuon/interface/DTt0DBValidation.h"
 #include "DQMOffline/CalibMuon/interface/DTtTrigDBValidation.h"
 #include "DQMOffline/CalibMuon/interface/DTnoiseDBValidation.h"
+#include "DQMOffline/CalibMuon/interface/DTPreCalibrationTask.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(DTt0DBValidation);
 DEFINE_ANOTHER_FWK_MODULE(DTtTrigDBValidation);
 DEFINE_ANOTHER_FWK_MODULE(DTnoiseDBValidation);
+DEFINE_ANOTHER_FWK_MODULE(DTPreCalibrationTask);
