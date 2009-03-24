6c3797d82867ea33ddc983cdb30264da5f2e7f2f
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-03-24 11:29:57 +0000

    clean-up (indent, cosmetics), no funtional changes

--- DQMOffline/Alignment/python/ALCARECOMuAlDQM_cff.py
+++ DQMOffline/Alignment/python/ALCARECOMuAlDQM_cff.py
@@ -1,138 +1,152 @@
 import FWCore.ParameterSet.Config as cms
 import DQM.TrackingMonitor.TrackingMonitor_cfi as _TrackMon
 import DQMOffline.Alignment.muonAlignment_cfi as _MuonAl
+
 from TrackingTools.MaterialEffects.Propagators_cff import *
-from TrackingTools.GeomPropagators.SmartPropagator_cff import * 
+from TrackingTools.GeomPropagators.SmartPropagator_cff import *
 
-#Below all DQM modules for MuonAlignment AlCaRecos are instanciated.
+#Below all DQM modules for MuonAlignment AlCaRecos are instantiated.
 
-###############MuAlStandAloneCosmics######################
+##########################################################
+############## MuAlStandAloneCosmics #####################
+##########################################################
 ALCARECOMuAlStandAloneCosmicsTrackingDQM = _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlStandAloneCosmics',
-AlgoName = 'ALCARECOMuAlStandAloneCosmics',
-FolderName = 'MuAlStandAloneCosmics',
-#sizes
-TkSizeBin = 5,
-TkSizeMin = 0,
-TkSizeMax = 5,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 100
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlStandAloneCosmics',
+    AlgoName = 'ALCARECOMuAlStandAloneCosmics',
+    FolderName = 'MuAlStandAloneCosmics',
+    #sizes
+    TkSizeBin = 5,
+    TkSizeMin = 0,
+    TkSizeMax = 5,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 100
+    )
 
 ALCARECOMuAlStandAloneCosmicsMuAlDQM = _MuonAl.muonAlignment.clone(
-    doSummary = cms.untracked.bool(True),
+    doSummary = True,
     FolderName ='MuAlStandAloneCosmics',
     MuonCollection = 'ALCARECOMuAlStandAloneCosmics'
  )
 
 ALCARECOMuAlStandAloneCosmicsDQM = cms.Sequence(ALCARECOMuAlStandAloneCosmicsTrackingDQM  + ALCARECOMuAlStandAloneCosmicsMuAlDQM )
 
+##########################################################
 #MuAlGlobalCosmics
+##########################################################
 ALCARECOMuAlGlobalCosmicsDQM = _TrackMon.TrackMon.clone(
-#names & designations    
-TrackProducer = 'ALCARECOMuAlGlobalCosmics:GlobalMuon',
-AlgoName = 'ALCARECOMuAlGlobalCosmics',
-FolderName = 'MuAlGlobalCosmics',
-#sizes  
-TkSizeBin = 5,
-TkSizeMin = 0,
-TkSizeMax = 5,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 30
-)
+    #names & designations    
+    TrackProducer = 'ALCARECOMuAlGlobalCosmics:GlobalMuon',
+    AlgoName = 'ALCARECOMuAlGlobalCosmics',
+    FolderName = 'MuAlGlobalCosmics',
+    #sizes  
+    TkSizeBin = 5,
+    TkSizeMin = 0,
+    TkSizeMax = 5,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 30
+    )
 
 
-###################MuAlBeamHalo################################
+##########################################################
+################## MuAlBeamHalo ##########################
+##########################################################
 ALCARECOMuAlBeamHaloDQM = _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlBeamHalo',
-AlgoName = 'ALCARECOMuAlBeamHalo',
-FolderName = 'MuAlBeamHalo',
-#sizes
-TkSizeBin = 5,
-TkSizeMin = 0,
-TkSizeMax = 5,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 30
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlBeamHalo',
+    AlgoName = 'ALCARECOMuAlBeamHalo',
+    FolderName = 'MuAlBeamHalo',
+    #sizes
+    TkSizeBin = 5,
+    TkSizeMin = 0,
+    TkSizeMax = 5,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 30
+    )
 
-##################MuAlBeamHaloOverlaps####################
+##########################################################
+################# MuAlBeamHaloOverlaps ###################
+##########################################################
 ALCARECOMuAlBeamHaloOverlapsDQM = _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlBeamHaloOverlaps',
-AlgoName = 'ALCARECOMuAlBeamHaloOverlaps',
-FolderName = 'MuAlBeamHaloOverlaps',
-#sizes
-TkSizeBin = 70,
-TkSizeMin = 0,
-TkSizeMax = 70,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 30
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlBeamHaloOverlaps',
+    AlgoName = 'ALCARECOMuAlBeamHaloOverlaps',
+    FolderName = 'MuAlBeamHaloOverlaps',
+    #sizes
+    TkSizeBin = 70,
+    TkSizeMin = 0,
+    TkSizeMax = 70,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 30
+    )
 
-##################MuAlCalIsolatedMu##########################
+##########################################################
+################ MuAlCalIsolatedMu #######################
+##########################################################
 ALCARECOMuAlCalIsolatedMuTrackingDQM = _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlCalIsolatedMu:StandAlone',
-AlgoName = 'ALCARECOMuAlCalIsolatedMu',
-FolderName = 'MuAlCalIsolatedMu',
-#sizes			    
-TkSizeBin = 5,
-TkSizeMin = 0,
-TkSizeMax = 5,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 100
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlCalIsolatedMu:StandAlone',
+    AlgoName = 'ALCARECOMuAlCalIsolatedMu',
+    FolderName = 'MuAlCalIsolatedMu',
+    #sizes			    
+    TkSizeBin = 5,
+    TkSizeMin = 0,
+    TkSizeMax = 5,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 100
+    )
 
 ALCARECOMuAlCalIsolatedMuMuAlDQM = _MuonAl.muonAlignment.clone(
-    doSummary = cms.untracked.bool(True),
+    doSummary = True,
     FolderName ='MuAlCalIsolatedMu',
     MuonCollection = 'ALCARECOMuAlCalIsolatedMu:StandAlone'
- )
+    )
 
 ALCARECOMuAlCalIsolatedMuDQM = cms.Sequence( ALCARECOMuAlCalIsolatedMuTrackingDQM + ALCARECOMuAlCalIsolatedMuMuAlDQM )
 
-######################MuAlOverlaps##########################
+##########################################################
+#################### MuAlOverlaps ########################
+##########################################################
 ALCARECOMuAlOverlapsDQM = _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlOverlaps',
-AlgoName = 'ALCARECOMuAlOverlaps',
-FolderName = 'MuAlOverlaps',
-#sizes                      
-TkSizeBin = 70,
-TkSizeMin = 0,
-TkSizeMax = 70,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 30
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlOverlaps',
+    AlgoName = 'ALCARECOMuAlOverlaps',
+    FolderName = 'MuAlOverlaps',
+    #sizes                      
+    TkSizeBin = 70,
+    TkSizeMin = 0,
+    TkSizeMax = 70,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 30
+    )
 
-###############MuAlZMuMu##################################
+##########################################################
+############### MuAlZMuMu ################################
+##########################################################
 ALCARECOMuAlZMuMuTrackingDQM= _TrackMon.TrackMon.clone(
-#names & designations  
-TrackProducer = 'ALCARECOMuAlZMuMu:StandAlone',
-AlgoName = 'ALCARECOMuAlZMuMu',
-FolderName = 'MuAlZMuMu',
-#sizes                      
-TkSizeBin = 5,
-TkSizeMin = 0,
-TkSizeMax = 5,
-TrackPtBin = 100,
-TrackPtMin = 0,
-TrackPtMax = 100
-)
+    #names & designations  
+    TrackProducer = 'ALCARECOMuAlZMuMu:StandAlone',
+    AlgoName = 'ALCARECOMuAlZMuMu',
+    FolderName = 'MuAlZMuMu',
+    #sizes                      
+    TkSizeBin = 5,
+    TkSizeMin = 0,
+    TkSizeMax = 5,
+    TrackPtBin = 100,
+    TrackPtMin = 0,
+    TrackPtMax = 100
+    )
 
 ALCARECOMuAlZMuMuMuAlDQM= _MuonAl.muonAlignment.clone(
-    doSummary = cms.untracked.bool(True),
+    doSummary = True,
     FolderName ='MuAlCaZMuMu',
     MuonCollection = 'ALCARECOMuAlZMuMu:StandAlone'
- )
+    )
 
 ALCARECOMuAlZMuMuDQM = cms.Sequence( ALCARECOMuAlZMuMuTrackingDQM + ALCARECOMuAlZMuMuMuAlDQM)
-
