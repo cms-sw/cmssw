25c8ebfe45ed9e8472f2c1cb0834d177be91a7e9
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2009-03-24 20:03:37 +0000

    removed useless plugin

--- RecoParticleFlow/PFTracking/python/gsfSeedClean_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-gsfSeedclean = cms.EDProducer("GsfSeedCleaner",
-    PreIdSeedLabel = cms.InputTag("elecpreid","SeedsForGsf"),
-    TkColList = cms.VInputTag(cms.InputTag("pixelMatchGsfElectrons"))
-)
-
-
