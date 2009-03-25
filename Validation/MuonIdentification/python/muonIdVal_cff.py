5a1b439626e042bea02e860ac017ecd1994942e1
jribnik <sha1-1f7b1d94f19f23f67adbbb4f5dcb8ced97706511@cern.ch>
2009-03-25 15:18:02 +0000

    Included the MuonIdDQM module in DQMOffline/Muon and created a sequence that can be used to run both modules and by default place their respective plots in the same base directory.

--- Validation/MuonIdentification/python/muonIdVal_cff.py
+++ Validation/MuonIdentification/python/muonIdVal_cff.py
@@ -1,3 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from Validation.MuonIdentification.muonIdVal_cfi import *
+from DQMOffline.Muon.muonIdDQM_cfi import *
+muonIdDQM.baseFolder = muonIdVal.baseFolder
+
+# MuonIdVal
+muonIdValSeq = cms.Sequence(muonIdVal)
+# MuonIdVal and MuonIdDQM
+muonIdValDQMSeq = cms.Sequence(muonIdVal*muonIdDQM)
