6ff5a57ae702f40c02399a858c32c0d59bbdabd8
jribnik <sha1-1f7b1d94f19f23f67adbbb4f5dcb8ced97706511@cern.ch>
2009-03-25 15:21:04 +0000

    Updated and now include all available configurable parameters for the MuonIdVal and MuonIdDQM modules.  If the two modules have common configurable parameters, set those in MuonIdDQM equal to those of MuonIdVal.  Include the new muonIdValDQMSeq sequence which runs both the MuonIdVal and MuonIdDQM modules.

--- Validation/MuonIdentification/test/MuonIdValToEDM_cfg.py
+++ Validation/MuonIdentification/test/MuonIdValToEDM_cfg.py
@@ -17,9 +17,21 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
 
 process.load("Validation.MuonIdentification.muonIdVal_cff")
+process.muonIdVal.inputMuonCollection = cms.InputTag("muons")
+process.muonIdVal.inputDTRecSegment4DCollection = cms.InputTag("dt4DSegments")
+process.muonIdVal.inputCSCSegmentCollection = cms.InputTag("cscSegments")
+process.muonIdVal.useTrackerMuons = cms.untracked.bool(True)
+process.muonIdVal.useGlobalMuons = cms.untracked.bool(True)
 process.muonIdVal.makeEnergyPlots = cms.untracked.bool(True)
-process.muonIdVal.makeIsoPlots = cms.untracked.bool(True)
 process.muonIdVal.make2DPlots = cms.untracked.bool(True)
+process.muonIdVal.makeAllChamberPlots = cms.untracked.bool(False)
+process.muonIdVal.baseFolder = cms.untracked.string("Muons/MuonIdVal")
+process.muonIdDQM.inputMuonCollection = process.muonIdVal.inputMuonCollection
+process.muonIdDQM.inputDTRecSegment4DCollection = process.muonIdVal.inputDTRecSegment4DCollection
+process.muonIdDQM.inputCSCSegmentCollection = process.muonIdVal.inputCSCSegmentCollection
+process.muonIdDQM.useTrackerMuons = process.muonIdVal.useTrackerMuons
+process.muonIdDQM.useGlobalMuons = process.muonIdVal.useGlobalMuons
+process.muonIdDQM.baseFolder = process.muonIdVal.baseFolder
 
 process.load("DQMServices.Components.MEtoEDMConverter_cff")
 
@@ -30,5 +42,5 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string("file:/tmp/jribnik/meh.root")
 )
 
-process.p = cms.Path(process.muonIdVal*process.MEtoEDMConverter)
+process.p = cms.Path(process.muonIdValDQMSeq*process.MEtoEDMConverter)
 process.e = cms.EndPath(process.out)
