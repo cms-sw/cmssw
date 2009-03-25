fb45aeebe3945e9f4c0badf1384c9470b307d4bc
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2009-03-25 12:53:14 +0000

    First inclusion of FastSim muon validation

---
+++ Validation/RecoMuon/python/muonValidationFastSim_cff.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.RecoMuon.selectors_cff import *
+from Validation.RecoMuon.associatorsFastSim_cff import *
+
+# Configurations for MultiTrackValidators
+from Validation.RecoMuon.muonValidationBase_cff import *
+
+# The muon association and validation sequence
+recoMuonValidation = cms.Sequence(muonAssociation_seq*muonValidation_seq)
+
---
+++ Validation/RecoMuon/python/muonValidationHLTFastSim_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.RecoMuon.selectors_cff import *
+from Validation.RecoMuon.associatorsFastSim_cff import *
+
+# Configurations for MultiTrackValidators
+from Validation.RecoMuon.muonValidationHLTBase_cff import *
+
+# The muon HLT association and validation sequence
+recoMuonValidationHLT_seq = cms.Sequence(
+     muonAssociationHLT_seq
+     *muonValidationHLT_seq
+     )
