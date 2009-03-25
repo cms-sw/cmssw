3bac1d9d56e9931dafbe7b1bb9f836497e8733f1
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2009-03-25 15:10:27 +0000

    Fixed some typos

--- RecoTauTag/HLTProducers/src/HLTTauProducer.cc
+++ RecoTauTag/HLTProducers/src/HLTTauProducer.cc
@@ -44,7 +44,8 @@ void HLTTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   int i=0;
   float eta_, phi_, pt_;
   int nTracksL25, nTracksL3;
-
+  float sumPtTracksL25,sumPtTracksL3;
+  double ptLeadTkL25, ptLeadTkL3;
  for(L2TauInfoAssociation::const_iterator p = tauL2Jets->begin();p!=tauL2Jets->end();++p)
 	   {
 	     //Retrieve The L2TauIsolationInfo Class from the AssociationMap
@@ -63,28 +64,46 @@ void HLTTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
 
     int trackIsolationL25 = (int)tauL25[i].discriminator(jetDirL25,matchingCone_, signalCone_, isolationCone_,1.,ptMin_,0);
     const TrackRef leadTkL25 = tauL25[i].leadingSignalTrack(jetDirL25,matchingCone_, 1.);
-    double ptLeadTkL25=0.;
-    
+    ptLeadTkL25 = 0.;
+    nTracksL25 = 1000;
     if(!leadTkL25) 
       {}else{
 	ptLeadTkL25 = (*leadTkL25).pt();
-	nTracksL25 = (tauL25[i].tracksInCone((*leadTkL25).momentum(), isolationCone_,   ptMin_ )).size() - (tauL25[i].tracksInCone((*leadTkL25).momentum(), signalCone_, ptMin_ )).size();	
+	const TrackRefVector signalTracks = tauL25[i].tracksInCone((*leadTkL25).momentum(), signalCone_, ptMin_ );
+	const TrackRefVector isolationTracks = tauL25[i].tracksInCone((*leadTkL25).momentum(), isolationCone_,   ptMin_ );
+	nTracksL25 = isolationTracks.size() - signalTracks.size();
+
+	for(unsigned int j=0;j<isolationTracks.size();j++)
+	  sumPtTracksL25 = sumPtTracksL25 + isolationTracks[j]->pt();
+	for(unsigned int j=0;j<signalTracks.size();j++)
+	  sumPtTracksL25 = sumPtTracksL25 - signalTracks[j]->pt();
+
       }
     jetTracks = tauL3[i].jtaRef();
     math::XYZVector jetDirL3(jetTracks->first->px(),jetTracks->first->py(),jetTracks->first->pz());	
     int  trackIsolationL3 = (int)tauL3[i].discriminator(jetDirL3,matchingCone_, signalCone_, isolationCone_,1.,ptMin_,0);
     
     const TrackRef leadTkL3 = tauL3[i].leadingSignalTrack(jetDirL3,matchingCone_,1.);
-    double ptLeadTkL3=0.;
+    ptLeadTkL3=0.;
+    nTracksL3 = 1000;
     if(!leadTkL3) 
       {}else{
 	ptLeadTkL3 = (*leadTkL3).pt();
-	nTracksL3 = (tauL3[i].tracksInCone((*leadTkL3).momentum(), isolationCone_,   ptMin_ )).size() - (tauL3[i].tracksInCone((*leadTkL3).momentum(), signalCone_, ptMin_ )).size();	
+	const TrackRefVector signalTracks = tauL3[i].tracksInCone((*leadTkL25).momentum(), signalCone_, ptMin_ );
+	const TrackRefVector isolationTracks = tauL3[i].tracksInCone((*leadTkL25).momentum(), isolationCone_,   ptMin_ );
+	nTracksL3 = isolationTracks.size() - signalTracks.size();
+	float sumPtTracksL3 = 0.;
+	for(unsigned int j=0;j<isolationTracks.size();j++)
+	  sumPtTracksL3 = sumPtTracksL3 + isolationTracks[j]->pt();
+	for(unsigned int j=0;j<signalTracks.size();j++)
+	  sumPtTracksL3 = sumPtTracksL3 - signalTracks[j]->pt();
       }
 
     HLTTau pippo(eta_,phi_,pt_,emIsol,trackIsolationL25,ptLeadTkL25,trackIsolationL3,ptLeadTkL3);
     pippo.setNL25TrackIsolation(nTracksL25);
     pippo.setNL3TrackIsolation(nTracksL3);
+    pippo.setSumPtTracksL25(sumPtTracksL25);
+    pippo.setSumPtTracksL3(sumPtTracksL3);
     pippo.setSeedEcalHitEt(l2info.seedEcalHitEt());
     pippo.setEcalClusterShape(l2info.ecalClusterShape());
     pippo.setNEcalHits(l2info.nEcalHits());		       
