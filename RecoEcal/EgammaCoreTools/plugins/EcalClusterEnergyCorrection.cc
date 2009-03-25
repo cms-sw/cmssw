8880a1e7ff11acedaec0a8b2712d3d0c7102dae1
Yurii Maravin <sha1-9850ca7e1f871752f13896386a78e9adc7312787@cern.ch>
2009-03-25 23:41:52 +0000

    SuperCluster energy correction using database access for correction parameters.

---
+++ RecoEcal/EgammaCoreTools/plugins/EcalClusterEnergyCorrection.cc
@@ -0,0 +1,157 @@
+#include "RecoEcal/EgammaCoreTools/plugins/EcalClusterEnergyCorrection.h"
+
+// Shower leakage corrections developed by Jungzhie et al. using TB data
+// Developed for EB only!
+float EcalClusterEnergyCorrection::fEta(float energy, float eta, int algorithm) const
+{
+
+  // this correction is setup only for EB
+  if ( algorithm != 0 ) return energy;
+  
+  float ieta = fabs(eta)*(5/0.087);
+  float p0 = (*params_)[0];  // should be 40.2198
+  float p1 = (*params_)[1];  // should be -3.03103e-6
+
+  if ( ieta < p0 ) return energy;
+  else return energy/(1.0 + p1*(ieta-p0)*(ieta-p0));
+}
+
+float EcalClusterEnergyCorrection::fBrem(float e, float brem, int algorithm) const 
+{
+  // brem == phiWidth/etaWidth of the SuperCluster 
+  // e  == energy of the SuperCluster 
+  // first parabola (for br > threshold) 
+  // p0 + p1*x + p2*x^2 
+  // second parabola (for br <= threshold) 
+  // ax^2 + bx + c, make y and y' the same in threshold 
+  // y = p0 + p1*threshold + p2*threshold^2  
+  // yprime = p1 + 2*p2*threshold 
+  // a = p3 
+  // b = yprime - 2*a*threshold 
+  // c = y - a*threshold^2 - b*threshold 
+
+  int offset;
+  if ( algorithm == 0 ) offset = 0;
+  else if ( algorithm == 1 ) offset = 20;
+  else {
+    // not supported, produce no correction
+    return e;
+  }
+
+  //Make No Corrections if brem is invalid! 
+  if ( brem == 0 ) return e; 
+
+  float bremLowThr  = (*params_)[2 + offset];
+  float bremHighThr = (*params_)[3 + offset];
+  if ( brem < bremLowThr  ) brem = bremLowThr;
+  if ( brem < bremHighThr ) brem = bremHighThr;
+
+  // Parameters provided in cfg file
+  float p0 = (*params_)[4 + offset];
+  float p1 = (*params_)[5 + offset];
+  float p2 = (*params_)[6 + offset];
+  float p3 = (*params_)[7 + offset];
+  float p4 = (*params_)[8 + offset];
+  // 
+  float threshold = p4;  
+   
+  float y = p0*threshold*threshold + p1*threshold + p2; 
+  float yprime = 2*p0*threshold + p1; 
+  float a = p3; 
+  float b = yprime - 2*a*threshold; 
+  float c = y - a*threshold*threshold - b*threshold; 
+   
+  float fCorr = 1; 
+  if ( brem < threshold )  
+    fCorr = p0*brem*brem + p1*brem + p2; 
+  else  
+    fCorr = a*brem*brem + b*brem + c; 
+ 
+  return e/fCorr; 
+}   
+
+
+float EcalClusterEnergyCorrection::fEtEta(float et, float eta, int algorithm) const
+{ 
+  // et -- Et of the SuperCluster (with respect to (0,0,0)) 
+  // eta -- eta of the SuperCluster 
+ 
+  float fCorr = 0.; 
+  int offset;
+  if ( algorithm == 0 ) offset = 0;
+  else if ( algorithm == 1 ) offset = 20;
+  else {
+    // not supported, produce no correction
+    return et;
+  }
+
+  // Barrel 
+  if ( algorithm == 0 ) { 
+    float p0 = (*params_)[ 9]  + (*params_)[10]/ (et + (*params_)[11]) + (*params_)[12]/(et*et);  
+    float p1 = (*params_)[13]  + (*params_)[14]/ (et + (*params_)[15]) + (*params_)[16]/(et*et);  
+ 
+    fCorr = p0 +  p1 * atan((*params_)[17]*((*params_)[18]-fabs(eta))) + (*params_)[19] * fabs(eta); 
+ 
+  } else if ( algorithm == 1 ) { // Endcap 
+    float p0 = (*params_)[ 9 + offset] + (*params_)[10 + offset]/sqrt(et); 
+    float p1 = (*params_)[11 + offset] + (*params_)[12 + offset]/sqrt(et); 
+    float p2 = (*params_)[13 + offset] + (*params_)[14 + offset]/sqrt(et); 
+    float p3 = (*params_)[15 + offset] + (*params_)[16 + offset]/sqrt(et); 
+  
+    fCorr = p0 + p1*fabs(eta) + p2*eta*eta + p3/fabs(eta); 
+  } 
+ 
+  // cap the correction at 50%
+  if ( fCorr < 0.5 ) fCorr = 0.5;  
+  if ( fCorr > 1.5 ) fCorr = 1.5;   
+ 
+  return et/fCorr; 
+} 
+
+
+float EcalClusterEnergyCorrection::getValue( const reco::SuperCluster & superCluster, const int mode ) const
+{
+        checkInit();
+
+	int algorithm = -1; // -1: not defined, 0 -- EB, 1 -- EE+ES
+
+	float energy = superCluster.energy(); 
+	float eta = fabs(superCluster.eta()); 
+	float brem = superCluster.phiWidth()/superCluster.etaWidth(); 
+
+	float correctedEnergy = 0;
+
+	if ( superCluster.algoID() == reco::hybrid || superCluster.algoID() == reco::dynamicHybrid ) {
+	  // algorithm: Barrel
+	  algorithm = 0;
+
+	  // first apply shower leakage corrections
+	  correctedEnergy = fEta(energy, eta, algorithm);
+
+	  // now apply F(brem)
+	  correctedEnergy = fBrem(correctedEnergy, brem, algorithm);
+
+	  float correctedEt = correctedEnergy/cosh(eta);
+	  correctedEt = fEtEta(correctedEt, eta, algorithm);
+	  correctedEnergy = correctedEt*cosh(eta);
+
+	} else if ( superCluster.algoID() == reco::multi5x5 ) {
+	  algorithm = 1;
+
+	  correctedEnergy = fBrem(energy, brem, algorithm);
+	  float correctedEt = correctedEnergy/cosh(eta);
+
+	  correctedEt = fEtEta(correctedEt, eta, algorithm);
+	  correctedEnergy = correctedEt*cosh(eta);
+	} else {
+
+	  // perform no correction
+	  correctedEnergy = energy;
+	}
+	
+	return correctedEnergy;
+}
+
+
+#include "RecoEcal/EgammaCoreTools/interface/EcalClusterFunctionFactory.h"
+DEFINE_EDM_PLUGIN( EcalClusterFunctionFactory, EcalClusterEnergyCorrection, "EcalClusterEnergyCorrection");
