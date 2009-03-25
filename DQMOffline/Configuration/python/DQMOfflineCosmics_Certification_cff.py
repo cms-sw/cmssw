cbb379066b75e05c844f82906030783124bae485
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-03-25 20:23:17 +0000

    - add DQM, DCS, CRT sequences (commented out)

--- DQMOffline/Configuration/python/DQMOfflineCosmics_Certification_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_Certification_cff.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMServices.Components.DQMDaqInfo_cfi import *
+from DQMOffline.Configuration.DQMOffline_Certification_cff import *
 
 DQMOfflineCosmics_Certification = cms.Sequence(dqmDaqInfo)
 
+#DQMOfflineCosmics_Certification = cms.Sequence(daq_dqmoffline*dcs_dqmoffline*crt_dqmoffline)
 
--- DQMOffline/Configuration/python/DQMOffline_Certification_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_Certification_cff.py
@@ -2,6 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMServices.Components.DQMDaqInfo_cfi import *
 
+from DQMOffline.Configuration.DQMOffline_DAQ_cff import *
+from DQMOffline.Configuration.DQMOffline_DCS_cff import *
+from DQMOffline.Configuration.DQMOffline_CRT_cff import *
+
 DQMOffline_Certification = cms.Sequence(dqmDaqInfo)
+
+#DQMOffline_Certification = cms.Sequence(daq_dqmoffline*dcs_dqmoffline*crt_dqmoffline)
 
 
