f83538018663761c13d62f9c200a60bb5b6fa043
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-03-25 23:31:22 +0000

    add file for static parameters

---
+++ DataFormats/L1GlobalCaloTrigger/interface/L1GctStaticParameters.h
@@ -0,0 +1,15 @@
+#ifndef L1GctStaticParameters_h
+#define L1GctStaticParameters_h
+
+class L1GctStaticParameters {
+ public:
+
+  static const unsigned kJetRankMax;
+  static const unsigned kJetRankNBits;
+
+  static const unsigned kEmRankMax;
+  static const unsigned kEmRankNBits;
+
+};
+
+#endif
---
+++ DataFormats/L1GlobalCaloTrigger/src/L1GctStaticParameters.cc
@@ -0,0 +1,7 @@
+#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctStaticParameters.h"
+
+const unsigned L1GctStaticParameters::kJetRankMax = 0x3f;
+const unsigned L1GctStaticParameters::kJetRankNBits = 6;
+
+const unsigned L1GctStaticParameters::kEmRankMax = 0x3f;
+const unsigned L1GctStaticParameters::kEmRankNBits = 6;
