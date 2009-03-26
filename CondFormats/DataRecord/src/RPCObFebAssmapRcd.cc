2f99031684961132045a7b9b995e858fff65a1fa
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-03-26 17:15:22 +0000

    first release

---
+++ CondFormats/DataRecord/src/RPCObFebAssmapRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCObFebAssmapRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Thu Mar 26 18:13:51 CET 2009
+// $Id$
+
+#include "CondFormats/DataRecord/interface/RPCObFebAssmapRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(RPCObFebAssmapRcd);
