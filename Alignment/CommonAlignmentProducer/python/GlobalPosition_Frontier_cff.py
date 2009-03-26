22e68138ad6cc431618260854350230d4a84fed4
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-03-26 10:44:39 +0000

    prepare for 31X

--- Alignment/CommonAlignmentProducer/python/GlobalPosition_Frontier_cff.py
+++ Alignment/CommonAlignmentProducer/python/GlobalPosition_Frontier_cff.py
@@ -1,14 +1,13 @@
-# The following comments couldn't be translated into the new config version:
-
-#FrontierProd/CMS_COND_20X_ALIGNMENT"
 import FWCore.ParameterSet.Config as cms
 
 import CalibTracker.Configuration.Common.PoolDBESSource_cfi
-GlobalPosition = CalibTracker.Configuration.Common.PoolDBESSource_cfi.poolDBESSource.clone()
-GlobalPosition.connect = 'frontier://FrontierProd/CMS_COND_20X_ALIGNMENT'
-# replace GlobalPosition.connect = "oracle://cms_orcoff_int2r/CMS_COND_ALIGNMENT" # oracle integration
-GlobalPosition.toGet = cms.VPSet(cms.PSet(
-    record = cms.string('GlobalPositionRcd'),
-    tag = cms.string('IdealGeometry')
-))
+GlobalPosition = CalibTracker.Configuration.Common.PoolDBESSource_cfi.poolDBESSource.clone(
+    connect = 'frontier://FrontierProd/CMS_COND_31X_ALIGNMENT',
+    #connect = 'frontier://FrontierPrep/CMS_COND_ALIGNMENT',
+    #connect = 'frontier://FrontierPrep/CMS_COND_PRESH', #temp. hack for 310pre4!
+    toGet = cms.VPSet(cms.PSet(record = cms.string('GlobalPositionRcd'),
+                               tag = cms.string('IdealGeometry')
+                               )
+                      )
+    )
 
