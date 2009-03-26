53a2d0a7f7972d8fe5d4607fcb5305bead01e476
kalavase <sha1-37b724a660ad78b379b1c02b36b874291d39b117@cern.ch>
2009-03-26 21:53:41 +0000

    Config file for value map producer

---
+++ JetMETCorrections/Type1MET/python/MuonMETValueMapProducer_cff.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from TrackingTools.TrackAssociator.default_cfi import *
+from TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff import *
+
+muonMETValueMapProducer = cms.EDProducer("MuonMETValueMapProducer",
+     TrackAssociatorParameterBlock,      
+     muonInputTag     = cms.InputTag("muons"),
+     beamSpotInputTag = cms.InputTag("offlineBeamSpot"),
+     useTrackAssociatorPositions = cms.bool(True),
+     useRecHits   = cms.bool(False), #if True, will use deposits in 3x3 recHits
+     useHO        = cms.bool(False), #if True, will correct for deposits in HO
+     isAlsoTkMu   = cms.bool(True),     #does the mu have to be a tracker mu?
+     towerEtThreshold = cms.double(0.5), #default MET calculated using towers with Et > 0.5 GeV only
+     minPt        = cms.double(10.0),   #min global Mu Pt is 10 GeV
+     maxEta       = cms.double(2.4),    #max global |Eta| is 2.5
+     maxNormChi2  = cms.double(10.0),   #max global chi2/ndof
+     maxd0        = cms.double(0.2),    #max global d0
+     minnHits     = cms.int32(11),      #minimum # of si hits
+     qOverPErr    = cms.double(999999.9),
+     delPtOverPt  = cms.double(0.2) # if the delta(globalpt, trackpt)/trackpt > 0.2, use track pt
+)
+muonMETValueMapProducer.TrackAssociatorParameters.useEcal = False
+muonMETValueMapProducer.TrackAssociatorParameters.useHcal = False
+muonMETValueMapProducer.TrackAssociatorParameters.useHO = False
+muonMETValueMapProducer.TrackAssociatorParameters.useCalo = True
+muonMETValueMapProducer.TrackAssociatorParameters.useMuon = False
+muonMETValueMapProducer.TrackAssociatorParameters.truthMatch = False
