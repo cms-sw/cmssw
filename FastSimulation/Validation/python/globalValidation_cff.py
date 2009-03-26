8573929c047aaf479f52a0ff002c2f70dc0ccf8f
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-03-26 16:07:18 +0000

    Added Muon validation sequence

--- FastSimulation/Validation/python/globalValidation_cff.py
+++ FastSimulation/Validation/python/globalValidation_cff.py
@@ -5,8 +5,13 @@ from SimGeneral.TrackingAnalysis.trackingParticles_cfi import *
 mergedtruth.TrackerHitLabels = ['famosSimHitsTrackerHits']
 mergedtruth.simHitLabel = 'famosSimHits'
 
+from Validation.RecoMET.METRelValForDQM_cff import *
+
 from Validation.TrackingMCTruth.trackingTruthValidation_cfi import *
 from Validation.RecoTrack.TrackValidation_fastsim_cff import *
-from Validation.RecoMuon.muonValidation_cff import *
+from Validation.RecoMuon.muonValidationFastSim_cff import *
 
-globalValidation = cms.Sequence(trackingParticles+trackingTruthValid+tracksValidation)
+globalValidation = cms.Sequence(trackingParticles+trackingTruthValid
+                                +tracksValidation
+                                +METRelValSequence
+                                +recoMuonValidationFastSim)
