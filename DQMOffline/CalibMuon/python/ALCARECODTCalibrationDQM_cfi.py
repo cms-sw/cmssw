7c5bd3d9a120fb85942517ab3889ed4160a4650c
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-03-26 13:06:18 +0000

    added dqm conf file

---
+++ DQMOffline/CalibMuon/python/ALCARECODTCalibrationDQM_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+ALCARECODTCalibrationDQM = cms.EDFilter("DTPreCalibrationTask",
+    SaveFile = cms.untracked.bool(False),
+    outputFileName = cms.untracked.string('DigiHistos.root'),
+    digiLabel = cms.untracked.string('muonDTDigis'),
+    minTriggerWidth = cms.untracked.int32(2000),
+    maxTriggerWidth = cms.untracked.int32(6000)
+)
