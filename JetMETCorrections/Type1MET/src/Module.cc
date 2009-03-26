1df2534b6a4d9546e222b2ea0e155712a37c7f19
kalavase <sha1-37b724a660ad78b379b1c02b36b874291d39b117@cern.ch>
2009-03-26 21:34:45 +0000

    added value map producer

--- JetMETCorrections/Type1MET/src/Module.cc
+++ JetMETCorrections/Type1MET/src/Module.cc
@@ -6,13 +6,16 @@
 #include "JetMETCorrections/Type1MET/interface/Type1MET.h"
 #include "JetMETCorrections/Type1MET/interface/MuonMET.h"
 #include "JetMETCorrections/Type1MET/interface/TauMET.h"
+#include "JetMETCorrections/Type1MET/interface/MuonMETValueMapProducer.h"
 
 using cms::Type1MET;
 using cms::MuonMET;
 using cms::TauMET;
+using cms::MuonMETValueMapProducer;
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(Type1MET);
 DEFINE_ANOTHER_FWK_MODULE(MuonMET);
 DEFINE_ANOTHER_FWK_MODULE(TauMET);
+DEFINE_ANOTHER_FWK_MODULE(MuonMETValueMapProducer);
