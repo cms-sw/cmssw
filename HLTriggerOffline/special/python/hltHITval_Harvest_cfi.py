6cd14f08fb777fe457844edd7719dc9dacc84bfa
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2009-03-26 11:08:43 +0000

    add harvesting for HcalIsoTrack

---
+++ HLTriggerOffline/special/python/hltHITval_Harvest_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+hltHITval_Harvest = cms.EDFilter("PostProcessor",
+    outputFileName = cms.untracked.string('HcalIsoTrackPostProcessor.root'),
+    commands       = cms.vstring(),
+    resolution     = cms.vstring(),                                    
+    subDirs         = cms.untracked.vstring('HLT/HcalIsoTrack/'),
+    efficiency     = cms.vstring(
+        "TurnOnLead 'L1 seeding turn on for leading jet; gen jet pT (GeV); N_L1/N_All' hpTgenLeadL1 hpTgenLead",
+	"TurnOnNextToLead 'L1 seeding turn on for next-to-leading jet; gen jet pT (GeV); N_L1/N_All' hpTgenNextL1 hpTgenNext"
+    )
+)
+
+
