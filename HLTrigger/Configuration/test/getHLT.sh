c54f40d87791e3eb21c828bfd5f856d46d513a7d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-03-26 07:44:17 +0000

    More fixes and development (Muons and trackiness, perpare for alias)

--- HLTrigger/Configuration/test/getHLT.sh
+++ HLTrigger/Configuration/test/getHLT.sh
@@ -5,7 +5,7 @@ cmsenv
 rehash
 
 setenv HLTmaster /dev/CMSSW_3_1_0/pre2
-setenv HLTversion V235
+setenv HLTversion V238
 
 echo "ConfDB path of master: $HLTmaster/HLT/$HLTversion"
 
@@ -25,7 +25,7 @@ else
 
 # for things NOT in CMSSW CVS:
 
-# ./getHLT.py $HLTmaster/HLT/$HLTversion  2E30
+  ./getHLT.py $HLTmaster/HLT/$HLTversion  FULL
   ./getHLT.py $HLTmaster/8E29_$HLTversion 8E29
   ./getHLT.py $HLTmaster/1E31_$HLTversion 1E31
 
