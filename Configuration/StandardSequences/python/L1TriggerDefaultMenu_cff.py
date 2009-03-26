d02c4d0eea85bafbd01962296df89c6abbc18db7
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2009-03-26 12:52:08 +0000

    Temporary solution to coherent L1 +HLT

--- Configuration/StandardSequences/python/L1TriggerDefaultMenu_cff.py
+++ Configuration/StandardSequences/python/L1TriggerDefaultMenu_cff.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 # Choose a menu/prescale/mask from one of the .cffs
 # in L1TriggerConfig/L1GtConfigProducers/data/Luminosity
-from  L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu_2008MC_2E30_Unprescaled_cff import *
+from L1TriggerConfig.L1GtConfigProducers.Luminosity.startup.L1Menu_Commissioning2009_v0_L1T_Scales_20080926_startup_Imp0_Unprescaled_cff import *
+from L1Trigger.Configuration.L1StartupConfig_cff import *
