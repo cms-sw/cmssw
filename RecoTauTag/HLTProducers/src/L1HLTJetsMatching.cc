13171275c162426d9f968c9724cb53a2fa73d8c5
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2009-03-26 15:32:35 +0000

    Removed a protection against exceptions

--- RecoTauTag/HLTProducers/src/L1HLTJetsMatching.cc
+++ RecoTauTag/HLTProducers/src/L1HLTJetsMatching.cc
@@ -4,8 +4,6 @@
 #include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
 #include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
 #include "FWCore/Utilities/interface/EDMException.h"
-#include "DataFormats/Candidate/interface/LeafCandidate.h" 
-#include "DataFormats/Math/interface/Point3D.h"
 //
 // class decleration
 //
@@ -20,8 +18,7 @@ L1HLTJetsMatching::L1HLTJetsMatching(const edm::ParameterSet& iConfig)
   tauTrigger = iConfig.getParameter<InputTag>("L1TauTrigger");
   mEt_Min = iConfig.getParameter<double>("EtMin");
   
-  //  produces<std::vector<reco::LeafCandidate> >("LeafCandidate");
-  produces<reco::CaloJetCollection>();
+  produces<CaloJetCollection>();
 }
 
 L1HLTJetsMatching::~L1HLTJetsMatching(){ }
@@ -35,20 +32,19 @@ void L1HLTJetsMatching::produce(edm::Event& iEvent, const edm::EventSetup& iES)
  using namespace trigger;
  using namespace l1extra;
 
- typedef vector<LeafCandidate> LeafCandidateCollection;
 
- auto_ptr<LeafCandidateCollection> tauL2LC(new LeafCandidateCollection);
  auto_ptr<CaloJetCollection> tauL2jets(new CaloJetCollection);
  
  double deltaR = 1.0;
  double matchingR = 0.5;
  //Getting HLT jets to be matched
- edm::Handle<edm::View<Candidate> > tauJets;
- if(iEvent.getByLabel( jetSrc, tauJets ))
-   {
+ edm::Handle<CaloJetCollection> tauJets;
+ iEvent.getByLabel( jetSrc, tauJets );
+   
+     const CaloJetCollection myJets = *(tauJets.product());
      
      Handle<trigger::TriggerFilterObjectWithRefs> l1TriggeredTaus;
-     if(iEvent.getByLabel(tauTrigger,l1TriggeredTaus)){
+     iEvent.getByLabel(tauTrigger,l1TriggeredTaus);
 	
        
        tauCandRefVec.clear();
@@ -56,23 +52,15 @@ void L1HLTJetsMatching::produce(edm::Event& iEvent, const edm::EventSetup& iES)
 	
        l1TriggeredTaus->getObjects( trigger::TriggerL1TauJet,tauCandRefVec);
        l1TriggeredTaus->getObjects( trigger::TriggerL1CenJet,jetCandRefVec);
-       math::XYZPoint a(0.,0.,0.);
-       CaloJet::Specific f;
 	
        for( unsigned int iL1Tau=0; iL1Tau <tauCandRefVec.size();iL1Tau++)
 	 {  
-	   for(unsigned int iJet=0;iJet<tauJets->size();iJet++)
+	   for(unsigned int iJet=0;iJet<myJets.size();iJet++)
 	     {
 	       //Find the relative L2TauJets, to see if it has been reconstructed
-	       const Candidate &  myJet = (*tauJets)[iJet];
-	       deltaR = ROOT::Math::VectorUtil::DeltaR(myJet.p4().Vect(), (tauCandRefVec[iL1Tau]->p4()).Vect());
+	       deltaR = ROOT::Math::VectorUtil::DeltaR(myJets[iJet].p4().Vect(), (tauCandRefVec[iL1Tau]->p4()).Vect());
 	       if(deltaR < matchingR ) {
-		 //		 LeafCandidate myLC(myJet);
-		 CaloJet myCaloJet(myJet.p4(),a,f);
-		if(myJet.pt() > mEt_Min) {
-		  //		  tauL2LC->push_back(myLC);
-		  tauL2jets->push_back(myCaloJet);
-		}
+		if(myJets[iJet].pt() > mEt_Min) tauL2jets->push_back(myJets[iJet]);
 		break;
 	       }
 	     }
@@ -80,29 +68,22 @@ void L1HLTJetsMatching::produce(edm::Event& iEvent, const edm::EventSetup& iES)
        
        for(unsigned int iL1Tau=0; iL1Tau <jetCandRefVec.size();iL1Tau++)
 	 {  
-	   for(unsigned int iJet=0;iJet<tauJets->size();iJet++)
+	   for(unsigned int iJet=0;iJet<myJets.size();iJet++)
 	     {
-	       const Candidate & myJet = (*tauJets)[iJet];
 	       //Find the relative L2TauJets, to see if it has been reconstructed
-	       deltaR = ROOT::Math::VectorUtil::DeltaR(myJet.p4().Vect(), (jetCandRefVec[iL1Tau]->p4()).Vect());
+	       deltaR = ROOT::Math::VectorUtil::DeltaR(myJets[iJet].p4().Vect(), (jetCandRefVec[iL1Tau]->p4()).Vect());
 	       if(deltaR < matchingR ) {
-		 //		 LeafCandidate myLC(myJet);
-		 CaloJet myCaloJet(myJet.p4(),a,f);
-		 if(myJet.pt() > mEt_Min) {
-		   //tauL2LC->push_back(myLC);
-		   tauL2jets->push_back(myCaloJet);
-		 }
+		if(myJets[iJet].pt() > mEt_Min) tauL2jets->push_back(myJets[iJet]);
 		break;
 	       }
 	     }
 	 }
-     }
+
     
-   }
+
     
  //  cout <<"Size of L2 jets "<<tauL2jets->size()<<endl;
 
  iEvent.put(tauL2jets);
- // iEvent.put(tauL2LC);
 
 }
