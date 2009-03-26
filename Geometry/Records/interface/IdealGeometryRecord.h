e04a733a3a7cb4f30f329b3674f0aeb51fc34245
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-03-26 08:17:06 +0000

    remove record dependencies

--- Geometry/Records/interface/IdealGeometryRecord.h
+++ Geometry/Records/interface/IdealGeometryRecord.h
@@ -16,16 +16,18 @@
 //
 // Author:      
 // Created:     Mon Jul 25 11:05:09 EDT 2005
-// $Id: IdealGeometryRecord.h,v 1.1 2005/07/25 15:11:22 chrjones Exp $
+// $Id: IdealGeometryRecord.h,v 1.2 2009/02/02 03:48:29 case Exp $
 //
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-#include "FWCore/Framework/interface/DependentRecordImplementation.h"
-#include "Geometry/Records/interface/GeometryFileRcd.h"
-#include "boost/mpl/vector.hpp"
+//#include "FWCore/Framework/interface/DependentRecordImplementation.h"
+//#include "Geometry/Records/interface/GeometryFileRcd.h"
+//#include "boost/mpl/vector.hpp"
 
-class IdealGeometryRecord : public edm::eventsetup::DependentRecordImplementation<
-  IdealGeometryRecord, boost::mpl::vector<GeometryFileRcd> > { };
+class IdealGeometryRecord : public edm::eventsetup::EventSetupRecordImplementation<IdealGeometryRecord> {};
+
+//class IdealGeometryRecord : public edm::eventsetup::DependentRecordImplementation<
+//  IdealGeometryRecord, boost::mpl::vector<GeometryFileRcd> > { };
 
 #endif /* RECORDS_IDEALGEOMETRYRECORD_H */
 
