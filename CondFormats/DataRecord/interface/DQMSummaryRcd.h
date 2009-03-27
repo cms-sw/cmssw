79f9cd0a67c2791fff7d3699e7e2c46fa17dbc61
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2009-03-27 16:04:31 +0000

    adding these two files for Salvatore Di Guida

---
+++ CondFormats/DataRecord/interface/DQMSummaryRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_DQMSummaryRcd_h
+#define DataRecord_DQMSummaryRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     DQMSummaryRcd
+// 
+/**\class DQMSummaryRcd DQMSummaryRcd.h CondFormats/DataRecord/interface/DQMSummaryRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Salvatore di Guida (CERN)
+// Created:     Wed Mar 18 15:11:00 CET 2009
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class DQMSummaryRcd : public edm::eventsetup::EventSetupRecordImplementation<DQMSummaryRcd> {};
+
+#endif
