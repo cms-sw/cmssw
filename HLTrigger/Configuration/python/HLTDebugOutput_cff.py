aa0ea7cd0ae4bfd6f4ebd639460a61f6d0450093
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-03-27 15:59:43 +0000

    Put prescale before HLTDEBUG output modules and use L1 alias names

--- HLTrigger/Configuration/python/HLTDebugOutput_cff.py
+++ HLTrigger/Configuration/python/HLTDebugOutput_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_1_0/pre2/HLT/V235 (CMSSW_3_1_X_2009-03-24-1100_HLT1)
+# /dev/CMSSW_3_1_0/pre2/HLT/V245 (CMSSW_3_1_X_2009-03-24-1100_HLT3)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_1_0/pre2/HLT/V235')
+  tableName = cms.string('/dev/CMSSW_3_1_0/pre2/HLT/V245')
 )
 
 block_hltDebugOutput = cms.PSet(
