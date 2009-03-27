866305e45e63a7fbf1606959be4cab32dc4997a0
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-03-27 16:01:35 +0000

    new addition of ES conditions

--- CalibCalorimetry/EcalTrivialCondModules/src/SealModule.cc
+++ CalibCalorimetry/EcalTrivialCondModules/src/SealModule.cc
@@ -1,9 +1,11 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "CalibCalorimetry/EcalTrivialCondModules/interface/EcalTrivialConditionRetriever.h"
+#include "CalibCalorimetry/EcalTrivialCondModules/interface/ESTrivialConditionRetriever.h"
 
 #include "CalibCalorimetry/EcalTrivialCondModules/interface/EcalTrivialObjectAnalyzer.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(EcalTrivialConditionRetriever);
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(ESTrivialConditionRetriever);
 DEFINE_ANOTHER_FWK_MODULE(EcalTrivialObjectAnalyzer);
