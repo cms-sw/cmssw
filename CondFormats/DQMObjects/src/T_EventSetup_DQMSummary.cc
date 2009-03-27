5e7a7d84117c0a9aa79cef2892cc23688426bb78
Salvatore Di Guida <sha1-744703a2e531bf35de4c8c886b1940546bb70d35@cern.ch>
2009-03-27 15:44:14 +0000

    first implementation of DQMSummary

---
+++ CondFormats/DQMObjects/src/T_EventSetup_DQMSummary.cc
@@ -0,0 +1,3 @@
+#include "CondFormats/DQMObjects/interface/DQMSummary.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+EVENTSETUP_DATA_REG(DQMSummary);
---
+++ CondFormats/DQMObjects/src/classes.h
@@ -0,0 +1,1 @@
+#include "CondFormats/DQMObjects/interface/DQMSummary.h"
