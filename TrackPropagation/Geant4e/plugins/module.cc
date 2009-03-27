8f66ae93e83170f91995ae1a2591d9cabf189873
Matthew Scott Rudolph <sha1-8a4a2363566e5844107d84f7aac1c40da4e1d47e@cern.ch>
2009-03-27 12:41:51 +0000

    Updated for current track propagation

---
+++ TrackPropagation/Geant4e/plugins/module.cc
@@ -0,0 +1,10 @@
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "GeantPropagatorESProducer.h"
+
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+DEFINE_FWK_EVENTSETUP_MODULE(GeantPropagatorESProducer);
+
