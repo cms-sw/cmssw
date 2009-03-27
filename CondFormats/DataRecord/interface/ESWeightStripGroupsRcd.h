969f0b3ce3ca5a67956f6bc51325a3943d75824e
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-03-27 15:29:17 +0000

    files for conditions of the preshower (ES)

---
+++ CondFormats/DataRecord/interface/ESWeightStripGroupsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_ESWeightStripGroupsRcd_H
+#define CondFormats_DataRecord_ESWeightStripGroupsRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class ESWeightStripGroupsRcd : public edm::eventsetup::EventSetupRecordImplementation<ESWeightStripGroupsRcd> {};
+#endif
