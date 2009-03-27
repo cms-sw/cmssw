2b59da05aa6c577554cdca192a339e78c74b757a
Ricardo Vasquez Sierra <sha1-f7e1e97cb11665c6bb054336d8a4a436897d2a02@cern.ch>
2009-03-27 19:23:02 +0000

    renaming the selector to a more descriptive name: GenJetSelector

--- PhysicsTools/JetMCAlgos/plugins/TauGenJetSelector.cc
+++
@@ -1,7 +0,0 @@
-#include "PhysicsTools/JetMCAlgos/plugins/TauGenJetSelector.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-DEFINE_FWK_MODULE(TauGenJetSelector);
-
-DEFINE_FWK_MODULE(TauGenJetRefSelector);
