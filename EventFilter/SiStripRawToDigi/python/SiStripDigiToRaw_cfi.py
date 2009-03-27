e3839f0bfd8dc8f66dc0bf9374389b097362ea8c
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-03-27 10:23:09 +0000

    changed plugin name to be used to maintain usage of old digitoraw module and important comments

--- EventFilter/SiStripRawToDigi/python/SiStripDigiToRaw_cfi.py
+++ EventFilter/SiStripRawToDigi/python/SiStripDigiToRaw_cfi.py
@@ -1,9 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+# WARNING: PPRESENTLY USING OLD DIGI-TO-RAW MODULE!!!
+# WARNING: SHOULD MIGRATE TO NEW sistrip::DigiToRaw MODULE ONCE VALIDATED!
+
 SiStripDigiToRaw = cms.EDProducer(
-    "SiStripDigiToRawModule",
+    "OldSiStripDigiToRawModule",
     InputModuleLabel = cms.string('simSiStripDigis'),
     InputDigiLabel = cms.string('ZeroSuppressed'),
     FedReadoutMode = cms.untracked.string('ZERO_SUPPRESSED'),
     UseFedKey = cms.untracked.bool(False)
     )
+
