44da7bc4cdbcb660a7dc90b34abb7ea61e1c84e1
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2009-03-27 09:47:38 +0000

    update example

--- RecoEgamma/ElectronIdentification/test/runElectronID_cfg.py
+++ RecoEgamma/ElectronIdentification/test/runElectronID_cfg.py
@@ -2,58 +2,86 @@ from FWCore.ParameterSet.Config import *
 
 process = cms.Process("runElectronID")
 
-process.extend(include("FWCore/MessageLogger/data/MessageLogger.cfi"))
-process.extend(include("RecoEcal/EgammaClusterProducers/data/geometryForClustering.cff"))
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration/StandardSequences/Geometry_cff')
+from Geometry.CaloEventSetup.CaloTopology_cfi import *
 
 process.maxEvents = cms.untracked.PSet(
-#    input = cms.untracked.int32(-1)
    input = cms.untracked.int32(100)
 )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_2_2_2/RelValZEE/GEN-SIM-RECO/IDEAL_V9_v2/0001/0ACC4ECE-26CA-DD11-A646-0030487A1FEC.root',
-    '/store/relval/CMSSW_2_2_2/RelValZEE/GEN-SIM-RECO/IDEAL_V9_v2/0001/0C8D2A94-FEC9-DD11-92B7-001617C3B76A.root',
-    '/store/relval/CMSSW_2_2_2/RelValZEE/GEN-SIM-RECO/IDEAL_V9_v2/0001/B87AC94B-31CA-DD11-9916-0019B9F72BAA.root',
-    '/store/relval/CMSSW_2_2_2/RelValZEE/GEN-SIM-RECO/IDEAL_V9_v2/0001/FC850F1F-1ECA-DD11-B0CC-000423D98E30.root'
-   ),
+    'file:/data/CMSSWtestFolder/Relval310pre3SingleElectronPt35_newElectronProto_100evts_newpresel.root'
+    ),
     secondaryFileNames = cms.untracked.vstring (
    )
 )
 
 process.load("RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi")
 from RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi import *
+process.load("RecoEgamma.ElectronIdentification.electronIdCutBasedClassesExt_cfi")
+from RecoEgamma.ElectronIdentification.electronIdCutBasedClassesExt_cfi import *
 
 process.eIDRobustLoose = eidCutBasedExt.clone()
-process.eIDRobustLoose.electronQuality = 'robust'
+process.eIDRobustLoose.electronIDType = 'robust'
+process.eIDRobustLoose.electronQuality = 'loose'
+
+process.eIDRobustLooseV00 = eidCutBasedExt.clone()
+process.eIDRobustLooseV00.electronIDType = 'robust'
+process.eIDRobustLooseV00.electronQuality = 'loose'
+process.eIDRobustLooseV00.electronVersion = 'V00'
 
 process.eIDRobustTight = eidCutBasedExt.clone()
-process.eIDRobustTight.electronQuality = 'robust'
-process.eIDRobustTight.robustEleIDCuts.barrel = [0.015, 0.0092, 0.020, 0.0025]
-process.eIDRobustTight.robustEleIDCuts.endcap = [0.018, 0.025, 0.020, 0.0040]
+process.eIDRobustTight.electronIDType = 'robust'
+process.eIDRobustTight.electronQuality = 'tight'
 
 process.eIDRobustHighEnergy = eidCutBasedExt.clone()
-process.eIDRobustHighEnergy.electronQuality = 'robust'
-process.eIDRobustHighEnergy.robustEleIDCuts.barrel = [0.050, 0.011, 0.090, 0.005]
-process.eIDRobustHighEnergy.robustEleIDCuts.endcap = [0.100, 0.0275, 0.090, 0.007]
+process.eIDRobustHighEnergy.electronIDType = 'robust'
+process.eIDRobustHighEnergy.electronQuality = 'highenergy'
 
 process.eIDLoose = eidCutBasedExt.clone()
+process.eIDLoose.electronIDType = 'classbased'
 process.eIDLoose.electronQuality = 'loose'
 
 process.eIDTight = eidCutBasedExt.clone()
+process.eIDTight.electronIDType = 'classbased'
 process.eIDTight.electronQuality = 'tight'
 
-eIDSequence = cms.Sequence(process.eIDRobustLoose+process.eIDRobustTight+process.eIDRobustHighEnergy+process.eIDLoose+process.eIDTight)
+process.eIDClassesLoose = eidCutBasedClassesExt.clone()
+process.eIDClassesLoose.electronQuality = 'loose'
+
+process.eIDClassesMedium = eidCutBasedClassesExt.clone()
+process.eIDClassesMedium.electronQuality = 'medium'
+
+process.eIDClassesTight = eidCutBasedClassesExt.clone()
+process.eIDClassesTight.electronQuality = 'tight'
+
+eIDSequence = cms.Sequence(process.eIDRobustLoose+
+                           process.eIDRobustLooseV00+
+                           process.eIDRobustTight+
+                           process.eIDRobustHighEnergy+
+                           process.eIDLoose+
+                           process.eIDTight+
+                           process.eIDClassesLoose+ 
+                           process.eIDClassesMedium+ 
+                           process.eIDClassesTight )
+
 process.p = cms.Path(eIDSequence)
 
 process.out = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring('drop *', 
-					   'keep *_pixelMatchGsfElectrons_*_*',
+					   'keep *_gsfElectrons_*_*',
 					   'keep *_eIDRobustLoose_*_*',
+					   'keep *_eIDRobustLooseV00_*_*',
 					   'keep *_eIDRobustTight_*_*',
 					   'keep *_eIDRobustHighEnergy_*_*',
 					   'keep *_eIDLoose_*_*',
-					   'keep *_eIDTight_*_*'),
+					   'keep *_eIDTight_*_*',
+					   'keep *_eIDClassesLoose_*_*',
+					   'keep *_eIDClassesMedium_*_*',
+					   'keep *_eIDClassesTight_*_*'),
 
     fileName = cms.untracked.string('electrons.root')
 )
