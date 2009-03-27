f3b87d58d5f9be791065423856a8e561467c46b5
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-03-27 22:54:37 +0000

    no need for separate sequence for FastSim

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorFastSimValidationSequence_cff.py
+++
@@ -1,6 +0,0 @@
-from HLTriggerOffline.HeavyFlavor.heavyFlavorValidation_cfi import *
-heavyFlavorValidation.L1Muons = cms.InputTag("l1extraParticles","","HLT")
-heavyFlavorValidation.RecoMuons = cms.InputTag("muons","","HLT")
-heavyFlavorValidation.GlobMuons = cms.InputTag("globalMuons","","HLT")
-
-heavyFlavorFastSimValidationSequence = cms.Sequence(heavyFlavorValidation)
