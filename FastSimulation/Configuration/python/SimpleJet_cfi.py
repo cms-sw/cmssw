7072cde3daf875ee3dc96514d8e849f60ee0b81c
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-03-27 22:27:47 +0000

    Port SimpleJet_cfi to the new jet gun.

--- FastSimulation/Configuration/python/SimpleJet_cfi.py
+++ FastSimulation/Configuration/python/SimpleJet_cfi.py
@@ -2,28 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 source = cms.Source("EmptySource")
 
-generator = cms.EDProducer("PythiaProducer",
-    pythiaFrame = cms.string("CMS"),
-    comEnergy = cms.double(14000.),
-    pythiaVerbosity = cms.untracked.bool(False),
-    # Generate a number of particles with some fermi motion
-    ParticleIDs = cms.vint32(211,-211,111,111,130),
-    Emin = cms.untracked.double(0.5),
-    Emax = cms.untracked.double(2.0),
-    # Then boost them
-    #   (Ptmin/Ptmax is the boost absolute value range)
-    Pmin = cms.untracked.double(20.0),
-    Pmax = cms.untracked.double(20.0),
-    #   (Etamin/Etamax is the boost eta range)
-    MinEta = cms.untracked.double(-2.4),
-    MaxEta = cms.untracked.double(2.4),
-    #   (Phimin/Phimax is the boost phi range)
-    MinPhi = cms.untracked.double(-3.1415926535),
-    MaxPhi = cms.untracked.double(+3.1415926535),
+generator = cms.EDProducer("Pythia6JetGun",
+    PGunParameters = cms.PSet(
+        ParticleID = cms.vint32(211, -211, 111, 111, 130),
+        # this defines "absolute" energy spead of particles in the jet
+        MinE = cms.double(0.5),
+        MaxE = cms.double(2.0),
+        # the following params define the boost
+        MinP = cms.double(20.0),
+        MaxP = cms.double(20.0),
+        MinPhi = cms.double(-3.1415926535),
+        MaxPhi = cms.double(+3.1415926535),
+        MinEta = cms.double(-2.4),
+        MaxEta = cms.double(2.4)
+    ),
+
+    # no detailed pythia6 settings necessary    
     PythiaParameters = cms.PSet(
         parameterSets = cms.vstring()
     )
-
 )
 
 ProductionFilterSequence = cms.Sequence(generator)
