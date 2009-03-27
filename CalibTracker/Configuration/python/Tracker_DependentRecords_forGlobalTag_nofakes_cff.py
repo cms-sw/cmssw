6194145eed735f5c71dc96e07d0e379b3a593ff0
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-03-27 19:13:14 +0000

    removed fake, added HV/LV record for the quality

--- CalibTracker/Configuration/python/Tracker_DependentRecords_forGlobalTag_nofakes_cff.py
+++ CalibTracker/Configuration/python/Tracker_DependentRecords_forGlobalTag_nofakes_cff.py
@@ -3,22 +3,8 @@ import FWCore.ParameterSet.Config as cms
 #Gain
 # first SiStripGainESProducer takes SiStripGainRcd from DB
 from CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi import *
-#second SiStripGainESProducer is used in the digitizer and takes SiStripGainRcd from SiStripGainFakeSource
-from CalibTracker.SiStripESProducers.fake.SiStripApvGainFakeESSource_cfi import *
-siStripApvGainFakeESSource.appendToDataLabel = 'fakeAPVGain'
-import CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi
-siStripGainESProducerforSimulation = CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi.siStripGainESProducer.clone()
-siStripGainESProducerforSimulation.appendToDataLabel = 'fake'
-siStripGainESProducerforSimulation.APVGain = 'fakeAPVGain'
 
-#Lorentz Angle
-#this SiStripLAFakeESSource is used by the digitizer
-from CalibTracker.SiStripESProducers.services.SiStripLorentzAngleGeneratorService_cfi import *
-import CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi
-siStripLAFakeESSourceforSimulation = CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi.siStripLorentzAngleFakeESSource.clone()
-
-siStripLAFakeESSourceforSimulation.appendToDataLabel = 'fake'
-
+#LorentzAngle
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
 
@@ -27,6 +13,8 @@ sistripconn = cms.ESProducer("SiStripConnectivity")
 
 from CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi import *
 siStripQualityESProducer.ListOfRecordToMerge = cms.VPSet(
+     cms.PSet( record = cms.string("SiStripModuleHVRcd"), tag    = cms.string("") ),
+     cms.PSet( record = cms.string("SiStripModuleLVRcd"), tag    = cms.string("") ),
      cms.PSet( record = cms.string("SiStripDetCablingRcd"), tag    = cms.string("") ),
      cms.PSet( record = cms.string("SiStripBadChannelRcd"), tag    = cms.string("") ),
      cms.PSet( record = cms.string("SiStripBadFiberRcd"),   tag    = cms.string("") )
