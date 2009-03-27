bac5f4dc397b35f7ded0fc351ac9fb029f021b9c
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-03-27 21:25:01 +0000

    Clean up event content. Only keep agreed final 3_1_X products.

--- GeneratorInterface/Configuration/python/GeneratorInterface_EventContent_cff.py
+++ GeneratorInterface/Configuration/python/GeneratorInterface_EventContent_cff.py
@@ -3,63 +3,34 @@ import FWCore.ParameterSet.Config as cms
 #RAW content 
 GeneratorInterfaceRAW = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
-        'keep LHEEventProduct_*_*_*',
-        'keep GenRunInfoProduct_generator_*_*', 
-        'keep GenEventInfoProduct_generator_*_*', 
-        'keep edmHepMCProduct_generator_*_*', 
-        'keep *_genParticles_*_*', 
-
-# These products will go away!
-        'keep edmGenInfoProduct_source_*_*', 
-        'keep *_genEventWeight_*_*', 
-        'keep *_genEventScale_*_*', 
-        'keep *_genEventPdfInfo_*_*', 
-        'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*',
-# and these too, because they will be run as producers/filters as "generator"
-        'keep edmHepMCProduct_source_*_*', 
-        'keep GenRunInfoProduct_source_*_*')
+        'keep LHERunInfoProduct_source_*_*',
+        'keep LHEEventProduct_source_*_*',
+        'keep GenRunInfoProduct_generator_*_*',
+        'keep GenEventInfoProduct_generator_*_*',
+        'keep edmHepMCProduct_generator_*_*',
+        'keep *_genParticles_*_*'
+    )
 )
 
 #RECO content
 GeneratorInterfaceRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
-        'keep LHEEventProduct_*_*_*',
-        'keep GenRunInfoProduct_generator_*_*', 
-        'keep GenEventInfoProduct_generator_*_*', 
-        'keep edmHepMCProduct_generator_*_*', 
-        'keep *_genParticles_*_*', 
-
-# These products will go away!
-        'keep edmGenInfoProduct_source_*_*', 
-        'keep *_genEventWeight_*_*', 
-        'keep *_genEventScale_*_*', 
-        'keep *_genEventPdfInfo_*_*', 
-        'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*',
-# and these too, because they will be run as producers/filters as "generator"
-        'keep edmHepMCProduct_source_*_*', 
-        'keep GenRunInfoProduct_source_*_*')
+        'keep LHERunInfoProduct_source_*_*',
+        'keep LHEEventProduct_source_*_*',
+        'keep GenRunInfoProduct_generator_*_*',
+        'keep GenEventInfoProduct_generator_*_*',
+        'keep edmHepMCProduct_generator_*_*',
+        'keep *_genParticles_*_*'
+    )
 )
 
 #AOD content
 GeneratorInterfaceAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep LHERunInfoProduct_*_*_*',	# arbitrary labels, not just "source"?
-        'keep LHEEventProduct_*_*_*',
-        'keep GenRunInfoProduct_generator_*_*', 
-        'keep GenEventInfoProduct_generator_*_*', 
-        'keep *_genParticles_*_*', 
-
-# These products will go away!
-        'keep edmGenInfoProduct_source_*_*', 
-        'keep *_genEventWeight_*_*', 
-        'keep *_genEventScale_*_*', 
-        'keep *_genEventPdfInfo_*_*', 
-        'keep *_genEventProcID_*_*', 
-        'keep *_genEventRunInfo_*_*',
-# and these too, because they will be run as producers/filters as "generator"
-        'keep GenRunInfoProduct_source_*_*')
+        'keep LHERunInfoProduct_source_*_*',
+        'keep LHEEventProduct_source_*_*',
+        'keep GenRunInfoProduct_generator_*_*',
+        'keep GenEventInfoProduct_generator_*_*',
+        'keep *_genParticles_*_*'
+    )
 )
