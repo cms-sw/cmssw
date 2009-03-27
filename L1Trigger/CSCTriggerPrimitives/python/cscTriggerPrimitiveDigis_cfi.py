430c25ede0e57af5f8cf4cb538552310e81185c1
Slava Valouev <sha1-839050e72a2de02d2671d623e8f589b9c52c11e4@cern.ch>
2009-03-27 17:02:02 +0000

    Update for completeness.

--- L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigis_cfi.py
+++ L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigis_cfi.py
@@ -16,8 +16,8 @@ cscTriggerPrimitiveDigis = cms.EDProducer("CSCTriggerPrimitivesProducer",
         isMTCC = cms.bool(True)
     ),
 
-    # Parameters for ALCT processors: default
-    alctParamDef = cms.PSet(
+    # Parameters for ALCT processors: old MC studies
+    alctParamOldMC = cms.PSet(
         alctFifoTbins   = cms.uint32(16),
         alctFifoPretrig = cms.uint32(10),
         alctDriftDelay  = cms.uint32(3),
@@ -31,23 +31,8 @@ cscTriggerPrimitiveDigis = cms.EDProducer("CSCTriggerPrimitivesProducer",
         verbosity = cms.untracked.int32(0)
     ),
 
-    # Parameters for ALCT processors: MTCC-I
-    alctParamMTCC1 = cms.PSet(
-        alctFifoTbins   = cms.uint32(10),
-        alctFifoPretrig = cms.uint32(12),
-        alctDriftDelay  = cms.uint32(3),
-        alctNplanesHitPretrig = cms.uint32(4),
-        alctNplanesHitPattern = cms.uint32(4),
-        alctNplanesHitAccelPretrig = cms.uint32(4),
-        alctNplanesHitAccelPattern = cms.uint32(4),
-        alctTrigMode       = cms.uint32(0),
-        alctAccelMode      = cms.uint32(2),
-        alctL1aWindowWidth = cms.uint32(3),
-        verbosity = cms.untracked.int32(0)
-    ),
-
     # Parameters for ALCT processors: MTCC-II
-    alctParamMTCC2 = cms.PSet(
+    alctParamMTCC = cms.PSet(
         alctFifoTbins   = cms.uint32(16),
         alctFifoPretrig = cms.uint32(10),
         alctDriftDelay  = cms.uint32(3),
@@ -61,8 +46,23 @@ cscTriggerPrimitiveDigis = cms.EDProducer("CSCTriggerPrimitivesProducer",
         verbosity = cms.untracked.int32(0)
     ),
 
-    # Parameters for CLCT processors: default
-    clctParamDef = cms.PSet(
+    # Parameters for ALCT processors: 2007 and later
+    alctParam07 = cms.PSet(
+        alctFifoTbins   = cms.uint32(16),
+        alctFifoPretrig = cms.uint32(10),
+        alctDriftDelay  = cms.uint32(2),
+        alctNplanesHitPretrig = cms.uint32(2),
+        alctNplanesHitPattern = cms.uint32(4),
+        alctNplanesHitAccelPretrig = cms.uint32(2),
+        alctNplanesHitAccelPattern = cms.uint32(4),
+        alctTrigMode       = cms.uint32(2),
+        alctAccelMode      = cms.uint32(0),
+        alctL1aWindowWidth = cms.uint32(7),
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    # Parameters for CLCT processors: old MC studies
+    clctParamOldMC = cms.PSet(
         clctFifoTbins   = cms.uint32(12),
         clctFifoPretrig = cms.uint32(7),
         clctHitPersist  = cms.uint32(6),
@@ -76,13 +76,27 @@ cscTriggerPrimitiveDigis = cms.EDProducer("CSCTriggerPrimitivesProducer",
     ),
 
     # Parameters for CLCT processors: MTCC-II
-    clctParamMTCC2 = cms.PSet(
+    clctParamMTCC = cms.PSet(
         clctFifoTbins   = cms.uint32(12),
         clctFifoPretrig = cms.uint32(7),
         clctHitPersist  = cms.uint32(6),
         clctDriftDelay  = cms.uint32(2),
         clctNplanesHitPretrig = cms.uint32(4),
         clctNplanesHitPattern = cms.uint32(1),
+        clctPidThreshPretrig  = cms.uint32(2),
+        clctMinSeparation     = cms.uint32(10),
+        # Debug
+        verbosity = cms.untracked.int32(0)
+    ),
+
+    # Parameters for CLCT processors: 2007 and later
+    clctParam07 = cms.PSet(
+        clctFifoTbins   = cms.uint32(12),
+        clctFifoPretrig = cms.uint32(7),
+        clctHitPersist  = cms.uint32(6),
+        clctDriftDelay  = cms.uint32(2),
+        clctNplanesHitPretrig = cms.uint32(2),
+        clctNplanesHitPattern = cms.uint32(4),
         clctPidThreshPretrig  = cms.uint32(2),
         clctMinSeparation     = cms.uint32(10),
         # Debug
