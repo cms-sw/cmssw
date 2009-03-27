a522d322091714e56e8656f0620f47362c2cf10d
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-03-27 23:34:56 +0000

    script to create a dbfiel with all the conditions

---
+++ CalibTracker/SiStripESProducers/test/python/UploadAll.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cmsRun DummyCondDBWriter_SiStripApvGain_cfg.py 
+
+cat DummyCondDBWriter_SiStripApvGain_cfg.py | sed -e "s@SiStripApvGain_Ideal@SiStripApvGain_IdealSim@" > DummyCondDBWriter_tmp_cfg.py
+cmsRun DummyCondDBWriter_tmp_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripBadChannel_cfg.py
+cmsRun DummyCondDBWriter_SiStripBadFiber_cfg.py
+cmsRun DummyCondDBWriter_SiStripBadModule_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripThreshold_cfg.py
+cmsRun DummyCondDBWriter_SiStripClusterThreshold_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripFedCabling_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripLorentzAngle_cfg.py
+cat DummyCondDBWriter_SiStripLorentzAngle_cfg.py | sed -e "s@SiStripLorentzAngle_Ideal@SiStripLorentzAngle_IdealSim@" -e s"@PerCent_Err=20.@PerCent_Err=0.@"> DummyCondDBWriter_tmp_cfg.py
+cmsRun DummyCondDBWriter_tmp_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripModuleHV_cfg.py
+cmsRun DummyCondDBWriter_SiStripModuleLV_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripNoises_cfg.py
+cmsRun DummyCondDBWriter_SiStripNoises_PeakMode_cfg.py
+
+cmsRun DummyCondDBWriter_SiStripPedestals_cfg.py
+
+cmsRun 
+cmsRun 
