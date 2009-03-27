a132ad37e640bbfd4bce2ea53759abdac550fe88
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-03-27 15:41:39 +0000

    classes for preshower ES

---
+++ CondCore/ESPlugins/src/plugin.cc
@@ -0,0 +1,37 @@
+/*
+ *  plugin.cc
+ *  CMSSW
+ *
+ *  Created by Chris Jones on 7/24/05.
+ *
+ */
+
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+
+#include "CondFormats/ESObjects/interface/ESPedestals.h"
+#include "CondFormats/DataRecord/interface/ESPedestalsRcd.h"
+
+#include "CondFormats/ESObjects/interface/ESWeightStripGroups.h"
+#include "CondFormats/DataRecord/interface/ESWeightStripGroupsRcd.h"
+
+#include "CondFormats/ESObjects/interface/ESTBWeights.h"
+#include "CondFormats/DataRecord/interface/ESTBWeightsRcd.h"
+
+#include "CondFormats/ESObjects/interface/ESCondObjectContainer.h"
+#include "CondFormats/DataRecord/interface/ESIntercalibConstantsRcd.h"
+
+
+#include "CondFormats/ESObjects/interface/ESADCToGeVConstant.h"
+#include "CondFormats/DataRecord/interface/ESADCToGeVConstantRcd.h"
+
+#include "CondFormats/ESObjects/interface/ESChannelStatus.h"
+#include "CondFormats/DataRecord/interface/ESChannelStatusRcd.h"
+
+DEFINE_SEAL_MODULE();
+REGISTER_PLUGIN(ESPedestalsRcd,ESPedestals);
+REGISTER_PLUGIN(ESWeightStripGroupsRcd,ESWeightStripGroups);
+REGISTER_PLUGIN(ESTBWeightsRcd,ESTBWeights);
+REGISTER_PLUGIN(ESIntercalibConstantsRcd,ESFloatCondObjectContainer);
+REGISTER_PLUGIN(ESADCToGeVConstantRcd,ESADCToGeVConstant);
+REGISTER_PLUGIN(ESChannelStatusRcd,ESChannelStatus);
+
