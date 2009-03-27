83e89e59157ed081bb558defa13a3cac2c3a2e3c
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2009-03-27 16:44:29 +0000

    Switch to "HitReconstructor" rather than "SimpleReconstructor" for normal reco

--- RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
+++ RecoLocalCalo/Configuration/python/hcalLocalReco_cff.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hbhe_cfi import *
-from RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi import *
-from RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi import *
-from RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_zdc_cfi import *
+from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
+
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hbhe_cfi import *
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_ho_cfi import *
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hf_cfi import *
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_zdc_cfi import *
 hcalLocalRecoSequence = cms.Sequence(hbhereco+hfreco+horeco+zdcreco)
 
