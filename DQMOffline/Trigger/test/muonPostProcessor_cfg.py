fbe8f198e4cbf51232121f7c144426e4690a3dd8
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-03-27 14:30:56 +0000

    OfflineClient for the HLTMuonOffline monitoring. Use DQMServices/DQMGenericClient to create ratio plots and store all plots.

---
+++ DQMOffline/Trigger/test/muonPostProcessor_cfg.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("EDMtoMEConvert")
+
+process.load("DQMServices.Components.EDMtoMEConverter_cff")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+process.load("DQMOffline.Trigger.MuonPostProcessor_cfi")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+									destinations = cms.untracked.vstring('MuonTrigPostTest.log'),
+									categories = cms.untracked.vstring('MuonTrigPostLog'),
+									debugModules = cms.untracked.vstring('*'),
+									MuonTrigPostLog = cms.untracked.PSet(
+	                                threshold = cms.untracked.string('DEBUG'),
+									  MuonTrigPostLog = cms.untracked.PSet(
+	                                   limit = cms.untracked.int32(100000))
+									  ),																	 									
+									)
+
+process.source = cms.Source("PoolSource",
+							# must precede with file:
+    #fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigOffline_nALL_useAodAndRAW_vMorePlots_DRStudy.root')
+							fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigOffline_nALL_p1_vMorePlots.root')
+)
+
+process.HLTMuonPostVal.outputFileName = cms.untracked.string('/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigPostProcessor_nALL_p1_histos_vMorePlots.root')
+
+process.path1 = cms.Path(process.EDMtoMEConverter*process.HLTMuonPostVal)
+process.DQMStore.referenceFileName = ''
