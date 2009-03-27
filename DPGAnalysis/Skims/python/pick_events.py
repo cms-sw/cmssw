f6d2d5ccf5abba16b0f85b3d2febe9353e092eba
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2009-03-27 14:01:32 +0000

    Modified to read from external file

--- DPGAnalysis/Skims/python/pick_events.py
+++ DPGAnalysis/Skims/python/pick_events.py
@@ -1,31 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("PICKEVENT")
+process = cms.Process("SKIMb")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/063B4FB4-1D82-DD11-97C1-000423D996B4.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/0C70D3E2-1B82-DD11-B005-000423D6CA42.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/16C53F54-1B82-DD11-A2B8-000423DD2F34.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/1AAAC808-9082-DD11-880E-001617E30D06.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/245F8A2A-1B82-DD11-B852-000423D8F63C.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/48AB7D04-1A82-DD11-B311-000423D6AF24.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/5C821132-1D82-DD11-9C65-000423D94990.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/5E91B795-2082-DD11-B973-001617DC1F70.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/6A85979B-1C82-DD11-B441-001617E30E28.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/7879CAA6-1882-DD11-82B4-000423D98750.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/7C5A3EA0-1B82-DD11-87C6-000423D9997E.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/A645BD82-1882-DD11-B023-000423D98EC8.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/A874067D-8F82-DD11-87AB-001617C3B710.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/AA880EB4-8F82-DD11-BD16-001617C3B778.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/B60CBD20-1C82-DD11-84E2-00161757BF42.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/B8B25E53-9082-DD11-BF63-001617E30D40.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/DC748397-1982-DD11-8AAC-000423D94494.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/EE4BD502-9182-DD11-9BE2-000423D98800.root',
-       '/store/data/BeamCommissioning08/Cosmics/RAW/v1/000/062/571/F8144871-1982-DD11-BB2F-000423D99E46.root'
+#              '/store/data/Commissioning09/Cosmics/RAW/v1/000/079/153/025F7681-391A-DE11-9556-0016177CA7A0.root'
+              '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/B8FB3273-5DFF-DD11-BEAB-00304875A7B5.root',
+              '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/C0E4F880-5CFF-DD11-B561-0030487624FD.root',
+              '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V9_SuperPointing_225-v3/0005/EAA9AE47-5FFF-DD11-A966-001A92810ADE.root'
       )
 )
 process.maxEvents = cms.untracked.PSet(
@@ -33,15 +18,6 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.load("Configuration.EventContent.EventContentCosmics_cff")
-process.FEVT = cms.OutputModule("PoolOutputModule",
-    process.FEVTEventContent,
-    dataset = cms.untracked.PSet(dataTier = cms.untracked.string('RAW')),
-    fileName = cms.untracked.string("/tmp/schmittm/cscunpacker_crash.root"),
-    SelectEvents = cms.untracked.PSet(
-       SelectEvents = cms.vstring('mySkim')
-       )
-)
-process.FEVT.outputCommands.append('keep FEDRawDataCollection_*_*_*')
 
 
 
@@ -52,14 +28,20 @@ process.FEVT.outputCommands.append('keep FEDRawDataCollection_*_*_*')
 #--------------------------------------------------
 process.pickEvents = cms.EDFilter(
     "PickEvents",
-    whichRun = cms.untracked.int32(62571),
-    whichEventFirst = cms.untracked.int32(150250),
-    whichEventLast  = cms.untracked.int32(150280)
+    RunEventList = cms.untracked.string("listrunev")
     )
 
+process.PickEventsPath  = cms.Path( process.pickEvents )
 
-
-
-process.mySkim  = cms.Path( process.pickEvents )
-
-process.outpath = cms.EndPath(process.FEVT)
+process.out = cms.OutputModule("PoolOutputModule",
+           outputCommands = cms.untracked.vstring('keep *','drop *_MEtoEDMConverter_*_*'),
+           SelectEvents = cms.untracked.PSet(
+                          SelectEvents = cms.vstring('PickEventsPath')
+                          ),
+           dataset = cms.untracked.PSet(
+		     dataTier = cms.untracked.string('RAW-RECO'),
+                     filterName = cms.untracked.string('PickEvents')),
+  
+          fileName = cms.untracked.string("PickEvents.root"),
+)
+process.this_is_the_end = cms.EndPath(process.out)
--- DPGAnalysis/Skims/src/PickEvents.cc
+++ DPGAnalysis/Skims/src/PickEvents.cc
@@ -13,13 +13,20 @@
 //
 // Original Author:  Michael Henry Schmitt
 //         Created:  Mon Sep 15 19:36:37 CEST 2008
-// $Id$
+// $Id: PickEvents.cc,v 1.1 2008/09/18 08:59:28 schmittm Exp $
+//         Modified: 27/03/2009 Luca Malgeri
+//                   reading external file, defining selection syntax
 //
 //
 
 
 // system include files
 #include <memory>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <limits>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -29,6 +36,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/FileInPath.h"
+
 
 //
 // class declaration
@@ -44,24 +53,31 @@ class PickEvents : public edm::EDFilter {
       virtual bool filter(edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
+  std::string listrunevents_; 
+  std::string listruneventsinpath_; 
+  
+  std::vector<bool> whattodo;
+  std::vector<edm::RunNumber_t> startrun;
+  std::vector<edm::RunNumber_t> endrun;
+  std::vector<edm::EventNumber_t> startevent;
+  std::vector<edm::EventNumber_t> endevent;
+  
   int nEventsAnalyzed;
   int nEventsSelected;
-  int whichRun;
-  int whichEventFirst;
-  int whichEventLast ;
       
 };
-
-using namespace std;
-using namespace edm;
 
 
 
 PickEvents::PickEvents(const edm::ParameterSet& iConfig)
 {
-  whichRun = iConfig.getUntrackedParameter<int>("whichRun",1);
-  whichEventFirst = iConfig.getUntrackedParameter<int>("whichEventFirst",11);
-  whichEventLast  = iConfig.getUntrackedParameter<int>("whichEventLast",19);
+
+  listruneventsinpath_=iConfig.getUntrackedParameter<std::string> ("RunEventList","");
+  edm::FileInPath listruneventstmp("DPGAnalysis/Skims/data/"+listruneventsinpath_);
+
+  listrunevents_=listruneventstmp.fullPath();
+
+  std::cout <<"File with run/event list:"<< listrunevents_<<std::endl;
 
 }
 
@@ -75,18 +91,26 @@ PickEvents::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
 
-   int kRun   = iEvent.id().run();
-   int kEvent = iEvent.id().event();
+   RunNumber_t kRun   = iEvent.id().run();
+   EventNumber_t kEvent = iEvent.id().event();
 
    bool selectThisEvent = false;
-   if (kRun == whichRun) {
-     if ( (kEvent >= whichEventFirst) && (kEvent <= whichEventLast) ) {
-       selectThisEvent = true;
-     }
-   }
 
+   for (unsigned int cond=0; cond<whattodo.size();cond++)
+     {
+       //       string what;
+       if ( kRun>=startrun[cond] && 
+	    kRun<=endrun[cond]   &&
+	    kEvent>=startevent[cond] && 
+	    kEvent<=endevent[cond] ) 
+	 { // it's in the range, use
+	   selectThisEvent=whattodo[cond];
+	 }
+     }
+	       
    nEventsAnalyzed++;
    if (selectThisEvent) nEventsSelected++;
+   //   if (selectThisEvent) std::cout << "Event selected: " << kRun << " " << kEvent << std::endl;
 
    return selectThisEvent;
 }
@@ -94,11 +118,93 @@ PickEvents::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 void 
 PickEvents::beginJob(const edm::EventSetup&)
 {
+  using namespace std;
+
+  string line;
+  string buf;
+
+  stringstream ss;
+  vector<string> tokens;
+
   nEventsAnalyzed = 0;
   nEventsSelected = 0;
+
+  // open file listevent file
+  ifstream listfile;
+  listfile.open(listrunevents_.c_str());
+  if (listfile.is_open())
+    {
+      while (! listfile.eof() )
+	{
+	  getline (listfile,line);
+	  ss.clear();
+	  ss.str(line);
+	  tokens.clear();
+	  while (ss>>buf)
+	    {
+	      tokens.push_back(buf);
+	      //	      cout << buf << endl;
+	    }	      
+	  // cout << tokens.size() << endl;
+	  if (tokens.size()<3)
+	    {
+	      //	      cout << "strange selection line:" << line << endl;
+	      //	      cout << "skipping it" << endl;
+	      continue;
+	    }
+	  if(tokens[0]=="-" || tokens[0]=="+")
+	    {
+	      // it's a selection line, use it
+	      if(tokens[0]=="-") whattodo.push_back(false);
+	      else whattodo.push_back(true);
+
+	      // start with run selecion
+	      int loc=tokens[1].find(":",0);
+
+	      string first=tokens[1].substr(0,loc);
+	      startrun.push_back((edm::RunNumber_t)atoi(first.c_str()));
+
+	      string last=tokens[1].substr(loc+1,tokens[1].size());
+	      if (last=="infty")
+		endrun.push_back(std::numeric_limits<unsigned int>::max());
+	      else
+		endrun.push_back((edm::RunNumber_t) atoi(last.c_str()));
+
+	      // then event number selecion
+	      loc=tokens[2].find(":",0);
+
+	      first=tokens[2].substr(0,loc);
+	      startevent.push_back((edm::EventNumber_t)atoi(first.c_str()));
+
+	      last=tokens[2].substr(loc+1,tokens[2].size());
+	      if (last=="infty")
+		endevent.push_back(edm::EventID::maxEventNumber());
+	      //		endevent.push_back(std::numeric_limits<long long int>::max());
+	      else
+		endevent.push_back((edm::EventNumber_t)atoi(last.c_str()));
+
+	    }
+	}
+      listfile.close();
+      // printout summary
+      cout << "Summary from list of run/event number selection" << endl;
+      for (unsigned int cond=0; cond<whattodo.size();cond++)
+	{
+	  string what;
+	  if(whattodo[cond]) what="select";
+	  else what="reject";
+	  cout << what << " "; 
+	  cout << "from run " << startrun[cond] << " to run " << endrun[cond] << " ";
+	  cout << "from eve " << startevent[cond] << " to eve " << endevent[cond] << endl; 
+	}
+    }
+
+  else cout << "Unable to open file"; 
+
 }
 void 
 PickEvents::endJob() {
+  using namespace std;
   cout << "================================================\n"
        << "  n Events Analyzed ............... " << nEventsAnalyzed << endl
        << "  n Events Selected ............... " << nEventsSelected<< endl
