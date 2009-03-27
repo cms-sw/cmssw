338f417e41dded6568610f01c419ecd40204f938
Soon Yung Jun <sha1-bc64213de5daffe38cb62f6b1e3fe18ade60c3a9@cern.ch>
2009-03-27 23:27:49 +0000

    add energy containment for Ecal

--- SimG4Core/GFlash/python/Gflash_cfi.py
+++ SimG4Core/GFlash/python/Gflash_cfi.py
@@ -1,6 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from SimG4Core.Application.g4SimHits_cfi import *
+from SimCalorimetry.EcalSimProducers.ecalNotContainmentSim_cff import *
+
+ecal_notCont_sim.EBs25notContainment = 1.0
+ecal_notCont_sim.EEs25notContainment = 1.0
 
 g4SimHits.Physics.type = 'SimG4Core/Physics/GFlash'
 g4SimHits.Physics.GFlash = cms.PSet(
