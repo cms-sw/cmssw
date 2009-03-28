f605f2a35525723202baef7f1bdd260b445c1e54
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2009-03-28 18:28:00 +0000

    Added L1_ZeroBias based on BPTX to MC2009 and Commissioning menus and produced version 1 (v1) for these menus.

--- L1TriggerConfig/L1GtConfigProducers/python/l1GtTriggerMenuXml_cfi.py
+++ L1TriggerConfig/L1GtConfigProducers/python/l1GtTriggerMenuXml_cfi.py
@@ -5,10 +5,10 @@ import FWCore.ParameterSet.Config as cms
 l1GtTriggerMenuXml = cms.ESProducer("L1GtTriggerMenuXmlProducer",
 
     # choose luminosity directory
-    TriggerMenuLuminosity = cms.string('lumi1030'),
+    TriggerMenuLuminosity = cms.string('startup'),
     
     # XML file for Global Trigger menu (def.xml) 
-    DefXmlFile = cms.string('L1Menu_2008MC_2E30.xml'),
+    DefXmlFile = cms.string('L1Menu_Commissioning2009_v1_L1T_Scales_20080926_startup_Imp0.xml'),
     
     # XML file for Global Trigger VME configuration (vme.xml)                 
     VmeXmlFile = cms.string('')
