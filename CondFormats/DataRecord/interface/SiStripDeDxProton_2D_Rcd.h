19d19730a2049fc3824194f79b5559eec1e675f0
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2009-03-30 21:22:52 +0000

    new records for DeDx particle  identification

---
+++ CondFormats/DataRecord/interface/SiStripDeDxProton_2D_Rcd.h
@@ -0,0 +1,8 @@
+#ifndef CondFormats_SiStripDeDxProton_2D_Rcd_h
+#define CondFormats_SiStripDeDxProton_2D_Rcd_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiStripDeDxProton_2D_Rcd : public edm::eventsetup::EventSetupRecordImplementation<SiStripDeDxProton_2D_Rcd> {};
+
+#endif
