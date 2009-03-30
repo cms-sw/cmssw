abb73d990697d5c0bcc77c87b5a792a253b40769
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-03-30 15:07:42 +0000

    bug fix concerning trackQuality cfg'able

--- RecoJets/JetAssociationProducers/python/j2tParametersVX_cfi.py
+++ RecoJets/JetAssociationProducers/python/j2tParametersVX_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: j2tParametersVX_cfi.py,v 1.2 2008/04/21 03:27:47 rpw Exp $
+# $Id: j2tParametersVX_cfi.py,v 1.3 2008/05/29 17:58:55 fedor Exp $
 j2tParametersVX = cms.PSet(
     tracks = cms.InputTag("generalTracks"),
-    trackQuality = "goodIterative",
+    trackQuality = cms.string("goodIterative"),
     coneSize = cms.double(0.5)
 )
 
