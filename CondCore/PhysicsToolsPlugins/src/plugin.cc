44aec76d6538deb68102a647674e0ea62b7829be
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2009-03-30 21:15:58 +0000

    CondCore/PhysicsToolsPlugins

--- CondCore/PhysicsToolsPlugins/src/plugin.cc
+++ CondCore/PhysicsToolsPlugins/src/plugin.cc
@@ -14,6 +14,15 @@ DEFINE_SEAL_MODULE();
 #include "CondFormats/PhysicsToolsObjects/interface/Histogram2D.h"
 #include "CondFormats/DataRecord/interface/SiStripDeDxMipRcd.h"
 REGISTER_PLUGIN(SiStripDeDxMipRcd, PhysicsTools::Calibration::HistogramD2D);
+#include "CondFormats/DataRecord/interface/SiStripDeDxProton_2D_Rcd.h"
+REGISTER_PLUGIN(SiStripDeDxProton_2D_Rcd, PhysicsTools::Calibration::VHistogramD2D);
+#include "CondFormats/DataRecord/interface/SiStripDeDxPion_2D_Rcd.h"
+REGISTER_PLUGIN(SiStripDeDxPion_2D_Rcd, PhysicsTools::Calibration::VHistogramD2D);
+#include "CondFormats/DataRecord/interface/SiStripDeDxKaon_2D_Rcd.h"
+REGISTER_PLUGIN(SiStripDeDxKaon_2D_Rcd, PhysicsTools::Calibration::VHistogramD2D);
+#include "CondFormats/DataRecord/interface/SiStripDeDxElectron_2D_Rcd.h"
+REGISTER_PLUGIN(SiStripDeDxElectron_2D_Rcd, PhysicsTools::Calibration::VHistogramD2D);
+
 
 
 
