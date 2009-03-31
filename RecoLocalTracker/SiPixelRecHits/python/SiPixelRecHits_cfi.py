5e958446a0c2e1473439f974257a5285710789ac
Gavril Giurgiu <sha1-f6994d9f32e89a2dbb5f0337c9e5f8a2f598a299@cern.ch>
2009-03-31 15:52:06 +0000

    remove obsolete parameters SiPixelRecHits_cfi.py

--- RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/SiPixelRecHits_cfi.py
@@ -5,17 +5,10 @@ siPixelRecHits = cms.EDFilter("SiPixelRecHitConverter",
     # untracked string ClusterCollLabel   = "siPixelClusters"
     CPE = cms.string('PixelCPEGeneric'),
     VerboseLevel = cms.untracked.int32(0),
-    TanLorentzAnglePerTesla = cms.double(0.106),
-    Alpha2Order = cms.bool(True),
-    speed = cms.int32(0)
+    #TanLorentzAnglePerTesla = cms.double(0.106),
+    #Alpha2Order = cms.bool(True),
+    #speed = cms.int32(0)
 
-    # Allows cuts to be optimized
-#    eff_charge_cut_lowX = cms.double(0.0),
- #   eff_charge_cut_lowY = cms.double(0.0),
-  #  eff_charge_cut_highX = cms.double(1.0),
-   # eff_charge_cut_highY = cms.double(1.0),
-  #  size_cutX = cms.double(3.0),
-   # size_cutY = cms.double(3.0)
 )
 
 
