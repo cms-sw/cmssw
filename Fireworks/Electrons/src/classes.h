bd9e4d2831ed6bfc440292350e1046282a4143ba
jmuelmen <sha1-19c609fe3a4daa353fc8169ef2702fa017104e96@cern.ch>
2009-03-31 15:26:35 +0000

    added dictionary so FWElectronDetailView can receive signals

---
+++ Fireworks/Electrons/src/classes.h
@@ -0,0 +1,12 @@
+// -*- C++ -*-
+
+//Add includes for your classes here
+#include "Fireworks/Electrons/plugins/FWElectronDetailView.h"
+
+namespace {
+   struct Fireworks_Electrons {
+      //add 'dummy' Wrapper variable for each class type you put into the Event
+      //FWDisplayEvent de;
+      FWElectronDetailView ed;
+   };
+}
