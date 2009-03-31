8fa88bc98e139ccc61e46bd1282cf61814c1561a
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-03-31 14:47:55 +0000

    removed pi0 plugins

--- Calibration/EcalAlCaRecoProducers/src/SealModule.cc
+++ Calibration/EcalAlCaRecoProducers/src/SealModule.cc
@@ -2,12 +2,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Calibration/EcalAlCaRecoProducers/interface/AlCaElectronsProducer.h"
 #include "Calibration/EcalAlCaRecoProducers/src/AlCaElectronsTest.h"
-#include "Calibration/EcalAlCaRecoProducers/interface/AlCaPi0RecHitsProducer.h"
-#include "Calibration/EcalAlCaRecoProducers/interface/AlCaPi0BasicClusterRecHitsProducer.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(AlCaElectronsProducer);
 DEFINE_ANOTHER_FWK_MODULE(AlCaElectronsTest);
-DEFINE_ANOTHER_FWK_MODULE(AlCaPi0RecHitsProducer);
-DEFINE_ANOTHER_FWK_MODULE(AlCaPi0BasicClusterRecHitsProducer);
+
