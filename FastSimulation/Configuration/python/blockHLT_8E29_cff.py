9b2f7f078ad335243aad95262bb9534f3840330b
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-04-01 09:25:54 +0000

    /dev/CMSSW_3_1_0/pre2/XXXX_V250/V2

--- FastSimulation/Configuration/python/blockHLT_8E29_cff.py
+++ FastSimulation/Configuration/python/blockHLT_8E29_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_1_0/pre2/8E29_V238/V2 (CMSSW_3_1_X_2009-03-24-1100_HLT2)
+# /dev/CMSSW_3_1_0/pre2/8E29_V250/V2 (CMSSW_3_1_X_2009-03-31-0100_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_1_0/pre2/8E29_V238/V2')
+  tableName = cms.string('/dev/CMSSW_3_1_0/pre2/8E29_V250/V2')
 )
 
 block_hltL1NonIsoLargeWindowElectronPixelSeeds = cms.PSet(
