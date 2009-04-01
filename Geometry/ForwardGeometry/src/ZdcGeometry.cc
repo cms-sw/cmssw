856ce78c1ab3630a757f57c3b86732b49ae5fe86
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-04-01 13:54:07 +0000

    redo getclosest cell and fix compiler warning

--- Geometry/ForwardGeometry/src/ZdcGeometry.cc
+++ Geometry/ForwardGeometry/src/ZdcGeometry.cc
@@ -47,7 +47,7 @@ ZdcGeometry::getValidDetIds( DetId::Detector det,
 
    if( m_validIds.empty() ) 
    {
-      for( int i ( 0 ) ; i != baseIds.size() ; ++i ) 
+      for( unsigned int i ( 0 ) ; i != baseIds.size() ; ++i ) 
       {
 	 const DetId id ( baseIds[i] );
 	 if( id.det()      == det    &&
@@ -63,57 +63,19 @@ ZdcGeometry::getValidDetIds( DetId::Detector det,
 
 DetId ZdcGeometry::getClosestCell(const GlobalPoint& r) const
 {
-  // first find the side
-  double z = r.z();
-  double x = r.x();
-  double y = r.y();
-  double dz = 0.;
-  double zt = 0.;
-
-  int zside = 0;
-  if(z >= 0)
-    zside = 1;
-  else
-    zside =-1;
-
-  bool isPositive = false;
-  if(z>0)isPositive = true;
-  z = fabs(z);
-  
-  // figure out if is closer to EM, HAD or LUM section
-  HcalZDCDetId::Section section = HcalZDCDetId::Unknown;
-  if(z<= theZSectionBoundaries[1])section = HcalZDCDetId::EM;
-  if(theZSectionBoundaries[1]<z<= theZSectionBoundaries[2])section = HcalZDCDetId::LUM;
-  if(z>theZSectionBoundaries[2])section = HcalZDCDetId::HAD;
-
-  // figure out channel
-  int channel = -1;
-  if(section ==HcalZDCDetId::EM){
-    if(x < theXChannelBoundaries[1]) channel = 1;
-    if(theXChannelBoundaries[1]<= x <theXChannelBoundaries[2])channel = 2;
-    if(theXChannelBoundaries[2]<= x <theXChannelBoundaries[3])channel = 3;
-    if(theXChannelBoundaries[3]<= x <theXChannelBoundaries[4])channel = 4;
-    if(x > theXChannelBoundaries[4])channel = 5;
-  }
-  
-  if(section == HcalZDCDetId::LUM){
-    if(z <= theZLUMChannelBoundaries[1])channel = 1;
-    if(z > theZLUMChannelBoundaries[1])channel = 2;
-  } 
-  if(section == HcalZDCDetId::HAD){
-    if(fabs(y) > dYPlate*sin(tiltangle))
-      dz = (y > 0.) ?  dYPlate*cos(tiltangle) : -  dYPlate*sin(tiltangle);
-    else
-      dz = (y > 0.) ?  y/tan(tiltangle) : -y/tan(tiltangle); 
-    zt = z - dz;
-    if(zt< theZHadChannelBoundaries[1]) channel = 1;
-    if(theZHadChannelBoundaries[1]<=  zt <theZHadChannelBoundaries[2])channel = 2;
-    if(theZHadChannelBoundaries[2]<=  zt <theZHadChannelBoundaries[3])channel = 3;
-    if(zt > theZHadChannelBoundaries[4])channel = 4;
-  }
-  
-  HcalZDCDetId bestId  = HcalZDCDetId(section,isPositive,channel);
-  return bestId;
+   DetId returnId ( 0 ) ;
+   const std::vector<DetId>& detIds ( getValidDetIds() ) ;
+   for( std::vector<DetId>::const_iterator it ( detIds.begin() ) ;
+	it != detIds.end(); ++it )
+   {
+      const CaloCellGeometry& cell ( *getGeometry( *it ) ) ;
+      if( cell.inside( r ) )
+      {
+	 returnId = *it ;
+	 break ;
+      }
+   }
+   return returnId ;
 }
 
 unsigned int
