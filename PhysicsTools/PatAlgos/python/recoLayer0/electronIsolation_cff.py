12acf8b87bff34a09ca022425e61da197a018f3d
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2009-04-01 10:35:39 +0000

    Used/needed isolation producers are provided in the dedicated package now.

--- PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
@@ -1,21 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-### OK, so apparently we need to make IsoDeposits for HCAL Towers ourselves in 2.2.X
-from RecoEgamma.EgammaIsolationAlgos.eleHcalExtractorBlocks_cff import *
-eleIsoDepositHcalFromTowers = cms.EDProducer("CandIsoDepositProducer",      
-        src = cms.InputTag("pixelMatchGsfElectrons"),   
-        MultipleDepositsFlag = cms.bool(False),     
-        trackType = cms.string('candidate'),    
-        ExtractorPSet = cms.PSet( EleIsoHcalFromTowersExtractorBlock )      
-)
-
 ### Compute isolation values, using POG modules
+from RecoEgamma.EgammaIsolationAlgos.eleIsoDeposits_cff import *
 from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDepsModules_cff  import *
 
 # sequence to run on AOD 
 patElectronIsolation = cms.Sequence(
-    eleIsoFromDepsTk +
-    eleIsoFromDepsEcalFromHits +
-    eleIsoDepositHcalFromTowers * eleIsoFromDepsHcalFromTowers 
+    eleIsoDepositTk * eleIsoFromDepsTk +
+    eleIsoDepositEcalFromHits * eleIsoFromDepsEcalFromHits +
+    eleIsoDepositHcalFromTowers * eleIsoFromDepsHcalFromTowers
 )
 
--- PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
@@ -1,20 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-### Apparently we need to make HCAL isolation from Towers ourselves
-from RecoEgamma.EgammaIsolationAlgos.gamHcalExtractorBlocks_cff import *
-gamIsoDepositHcalFromTowers = cms.EDProducer("CandIsoDepositProducer",   
-     src = cms.InputTag("photons"),      
-     MultipleDepositsFlag = cms.bool(False),     
-     trackType = cms.string('candidate'),    
-     ExtractorPSet = cms.PSet( GamIsoHcalFromTowersExtractorBlock )      
-)   
-
 ### Compute isolation values, using POG modules
+from RecoEgamma.EgammaIsolationAlgos.gamIsoDeposits_cff import *
 from RecoEgamma.EgammaIsolationAlgos.gamIsoFromDepsModules_cff  import *
 
 # sequence to run on AOD 
 patPhotonIsolation = cms.Sequence(
-    gamIsoFromDepsTk +
-    gamIsoFromDepsEcalFromHits +
-    gamIsoDepositHcalFromTowers * gamIsoFromDepsHcalFromTowers 
+    gamIsoDepositTk * gamIsoFromDepsTk +
+    gamIsoDepositEcalFromHits * gamIsoFromDepsEcalFromHits +
+    gamIsoDepositHcalFromTowers * gamIsoFromDepsHcalFromTowers
 )
