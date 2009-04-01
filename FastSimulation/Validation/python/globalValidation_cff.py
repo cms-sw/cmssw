64f72340ce8f09fa146eda1eb90db92484444907
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-04-01 17:24:55 +0000

    Btag Validation added

--- FastSimulation/Validation/python/globalValidation_cff.py
+++ FastSimulation/Validation/python/globalValidation_cff.py
@@ -9,9 +9,20 @@ from Validation.RecoMET.METRelValForDQM_cff import *
 
 from Validation.TrackingMCTruth.trackingTruthValidation_cfi import *
 from Validation.RecoTrack.TrackValidation_fastsim_cff import *
+###must be commented for automatic RelVal running
+multiTrackValidator.outputFile='valPlots_fastsim.root'
+
 from Validation.RecoMuon.muonValidationFastSim_cff import *
+
+from PhysicsTools.JetMCAlgos.CaloJetsMCFlavour_cfi import * 
+from Validation.RecoB.bTagAnalysis_cfi import *
+bTagValidation.jetMCSrc = 'IC5byValAlgo'
+bTagValidation.etaRanges = cms.vdouble(0.0, 1.1, 2.4)
 
 globalValidation = cms.Sequence(trackingParticles+trackingTruthValid
                                 +tracksValidation
                                 +METRelValSequence
-                                +recoMuonValidationFastSim)
+                                +recoMuonValidationFastSim
+                                +myPartons
+                                +iterativeCone5Flavour
+                                +bTagValidation)
