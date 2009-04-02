70fab10c9e6f68c5f95d1d764efb8f6020d5acba
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-04-02 16:38:10 +0000

    Bug fix: muon selectors no longer use just tracks but rather muon collections

--- DQMOffline/Trigger/python/muonSelector_cfi.py
+++ DQMOffline/Trigger/python/muonSelector_cfi.py
@@ -7,28 +7,37 @@ import FWCore.ParameterSet.Config as cms
 #									  )
 									  
 # This is how you would get muon tracks and cut on them
-highPtMuonTracks = cms.EDProducer ("TrackSelector",
-									  src = cms.InputTag("globalMuons"),
-									  cut = cms.string ("pt > 20")
-									  )
+#highPtMuonTracks = cms.EDProducer ("TrackSelector",
+#									  src = cms.InputTag("globalMuons"),
+#									  cut = cms.string ("pt > 20")
+#									  )
 
-barrelMuonTracks = cms.EDProducer ("TrackSelector",
-								   src = cms.InputTag("globalMuons"),
+#barrelMuonTracks = cms.EDProducer ("TrackSelector",
+#								   src = cms.InputTag("globalMuons"),
+#								   cut = cms.string ("abs(eta) < 0.9")
+#								   )
+
+barrelMuons = cms.EDProducer ("MuonSelector",
+								   src = cms.InputTag("muons"),
 								   cut = cms.string ("abs(eta) < 0.9")
 								   )
 
-overlapMuonTracks = cms.EDProducer ("TrackSelector",
-								   src = cms.InputTag("globalMuons"),
-								   cut = cms.string (" (abs(eta) > 0.9) & (abs(eta) < 1.3)")
-								   )
 
-endcapMuonTracks = cms.EDProducer ("TrackSelector",
-								   src = cms.InputTag("globalMuons"),
+
+overlapMuons = cms.EDProducer ("MuonSelector",
+									src = cms.InputTag("muons"),
+									cut = cms.string (" (abs(eta) > 0.9) & (abs(eta) < 1.3)")
+									)
+
+endcapMuons = cms.EDProducer ("MuonSelector",
+								   src = cms.InputTag("muons"),
 								   cut = cms.string ("(abs(eta) > 1.3) & (abs(eta) < 2.1) ")
 								   )
 									  
 
-externalMuonTracks = cms.EDProducer ("TrackSelector",
-								   src = cms.InputTag("globalMuons"),
-								   cut = cms.string ("(abs(eta) > 2.1) & (abs(eta) < 2.4) ")
-								   )
+externalMuons = cms.EDProducer ("MuonSelector",
+									 src = cms.InputTag("muons"),
+									 cut = cms.string ("(abs(eta) > 2.1) & (abs(eta) < 2.4) ")
+									 )
+
+
