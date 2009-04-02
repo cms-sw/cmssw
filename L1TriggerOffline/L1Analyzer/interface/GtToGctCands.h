891427ec368c0193a18b22e6b41bf6f5c24f341b
Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch>
2009-04-02 19:33:44 +0000

    Rewrote GT to GCT converter.

--- L1TriggerOffline/L1Analyzer/interface/GtToGctCands.h
+++ L1TriggerOffline/L1Analyzer/interface/GtToGctCands.h
@@ -14,7 +14,7 @@ Description: Convert GT candidates (electrons and jets) to GCT format
 //
 // Original Author:  Alex Tapper
 //         Created:  Mon Mar 30 17:31:03 CEST 2009
-// $Id: GtToGctCands.h,v 1.1 2009/04/01 12:32:05 tapper Exp $
+// $Id: GtToGctCands.h,v 1.2 2009/04/02 12:01:48 tapper Exp $
 //
 //
 
@@ -28,21 +28,9 @@ Description: Convert GT candidates (electrons and jets) to GCT format
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "DataFormats/Common/interface/Handle.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "DataFormats/Common/interface/OrphanHandle.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "DataFormats/Math/interface/LorentzVector.h"
-
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h"
-
-#include "CondFormats/L1TObjects/interface/L1CaloGeometry.h"
-#include "CondFormats/DataRecord/interface/L1CaloGeometryRecord.h"
-
-// forward declarations
-class L1CaloGeometry ;
 
 class GtToGctCands : public edm::EDProducer {
 
@@ -52,11 +40,6 @@ class GtToGctCands : public edm::EDProducer {
 
  private:
   virtual void produce(edm::Event&, const edm::EventSetup&);
-
-  math::PtEtaPhiMLorentzVector gctLorentzVector( const double& et,
-                                                 const L1GctCand& cand,
-                                                 const L1CaloGeometry* geom,
-                                                 bool central ) ;
 
   edm::InputTag m_GTInputTag;
       
--- L1TriggerOffline/L1Analyzer/src/GtToGctCands.cc
+++ L1TriggerOffline/L1Analyzer/src/GtToGctCands.cc
@@ -11,46 +11,54 @@
 //
 // Original Author:  Alex Tapper
 //         Created:  Mon Mar 30 17:31:03 CEST 2009
-// $Id: GtToGctCands.cc,v 1.2 2009/04/01 15:10:00 georgia Exp $
+// $Id: GtToGctCands.cc,v 1.3 2009/04/02 12:01:48 tapper Exp $
 //
 //
 
 #include "L1TriggerOffline/L1Analyzer/interface/GtToGctCands.h"
-
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
-
-#include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
-#include "CondFormats/DataRecord/interface/L1JetEtScaleRcd.h"
-
-#include "DataFormats/L1Trigger/interface/L1JetParticle.h"
-#include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
 
 GtToGctCands::GtToGctCands(const edm::ParameterSet& iConfig) :
   m_GTInputTag(iConfig.getParameter<edm::InputTag>("inputLabel"))
 {
-  using namespace l1extra;
-
   // For now I am making one electron collection and one jet collection with all electrons and jets from all 3 BXs.
   // This is the easiest format to analyse for CRAFT data.
   // In the future I should make different collections and treat the mutiple BXs properly, and add energy sums.
-  //  produces<L1GctEmCandCollection>();
-  produces<L1GctJetCandCollection>();
 
-  // Current hack since GCT collections are not in the RECO I make the L1Extra particles here too
-  produces<L1JetParticleCollection>( "Tau" ) ;
+  // list of products
+  produces<L1GctEmCandCollection>("isoEm");
+  produces<L1GctEmCandCollection>("nonIsoEm");
+  produces<L1GctJetCandCollection>("cenJets");
+  produces<L1GctJetCandCollection>("forJets");
+  produces<L1GctJetCandCollection>("tauJets");
+  produces<L1GctEtTotalCollection>();
+  produces<L1GctEtHadCollection>();
+  produces<L1GctEtMissCollection>();
+  produces<L1GctHtMissCollection>();
+  produces<L1GctHFBitCountsCollection>();
+  produces<L1GctHFRingEtSumsCollection>();
 }
 
 GtToGctCands::~GtToGctCands(){}
 
 void GtToGctCands::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  using namespace l1extra;
-  using namespace edm;
-  using namespace std;
-  
-  // create em and jet collections
-  std::auto_ptr<L1GctEmCandCollection> allElectrons (new L1GctEmCandCollection());
-  std::auto_ptr<L1GctJetCandCollection> allJets (new L1GctJetCandCollection());
+  // create the em and jet collections
+  std::auto_ptr<L1GctEmCandCollection>  isoEm    (new L1GctEmCandCollection);
+  std::auto_ptr<L1GctEmCandCollection>  nonIsoEm (new L1GctEmCandCollection);
+  std::auto_ptr<L1GctJetCandCollection> cenJet   (new L1GctJetCandCollection);
+  std::auto_ptr<L1GctJetCandCollection> forJet   (new L1GctJetCandCollection);
+  std::auto_ptr<L1GctJetCandCollection> tauJet   (new L1GctJetCandCollection);
+
+  // create the energy sum digis
+  std::auto_ptr<L1GctEtTotalCollection> etTot  (new L1GctEtTotalCollection);
+  std::auto_ptr<L1GctEtHadCollection>   etHad  (new L1GctEtHadCollection);
+  std::auto_ptr<L1GctEtMissCollection>  etMiss (new L1GctEtMissCollection);
+//  std::auto_ptr<L1GctHtMissCollection>  htMiss (new L1GctHtMissCollection));
+
+  // create the Hf sums digis
+  std::auto_ptr<L1GctHFBitCountsCollection>  hfBitCount  (new L1GctHFBitCountsCollection);
+  std::auto_ptr<L1GctHFRingEtSumsCollection> hfRingEtSum (new L1GctHFRingEtSumsCollection);
 
   // Get GT data
   edm::Handle<L1GlobalTriggerReadoutRecord> gtrr_handle;
@@ -68,7 +76,7 @@ void GtToGctCands::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      psbel.push_back(psb.bData(4));
      psbel.push_back(psb.bData(5));
      for(std::vector<int>::const_iterator ipsbel=psbel.begin(); ipsbel!=psbel.end(); ipsbel++) {
-       allElectrons->push_back(L1GctEmCand((*ipsbel)&0x3f,((*ipsbel)>>10)&0x1f,(((*ipsbel)>>6)&7) * ( ((*ipsbel>>9)&1) ? -1 : 1 ),0));
+       nonIsoEm->push_back(L1GctEmCand((*ipsbel)&0x3f,((*ipsbel)>>10)&0x1f,(((*ipsbel)>>6)&7) * ( ((*ipsbel>>9)&1) ? -1 : 1 ),0));
      }
     
      std::vector<int> psbjet;
@@ -77,69 +85,20 @@ void GtToGctCands::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      psbjet.push_back(psb.bData(2));
      psbjet.push_back(psb.bData(3));
      for(std::vector<int>::const_iterator ipsbjet=psbjet.begin(); ipsbjet!=psbjet.end(); ipsbjet++) {
-       allJets->push_back(L1GctJetCand((*ipsbjet)&0x3f,((*ipsbjet)>>10)&0x1f,(((*ipsbjet)>>6)&7) * ( ((*ipsbjet>>9)&1) ? -1 : 1 ),1,0));
+       tauJet->push_back(L1GctJetCand((*ipsbjet)&0x3f,((*ipsbjet)>>10)&0x1f,(((*ipsbjet)>>6)&7) * ( ((*ipsbjet>>9)&1) ? -1 : 1 ),1,0));
      }
   }
 
-  // Put the new collections into the event
-  iEvent.put(allElectrons);
-  iEvent.put(allJets);
-
-  // Get the collection out again... sigh
-  edm::Handle<L1GctJetCandCollection> jets;
-  iEvent.getByLabel("gctCandsFromGt",jets);
-
-  // Create L1Extra collections
-  auto_ptr <L1JetParticleCollection> tauJetColl(new L1JetParticleCollection);
-
-  // Geometry
-  ESHandle <L1CaloGeometry> caloGeomESH ;
-  iSetup.get <L1CaloGeometryRecord>().get( caloGeomESH ) ;
-  const L1CaloGeometry* caloGeom = &( *caloGeomESH ) ;
-
-  // L1 jet scale
-  ESHandle <L1CaloEtScale> jetScale ;
-  iSetup.get <L1JetEtScaleRcd>().get(jetScale) ;
-
-  // Now make the L1Extra collection
-  L1GctJetCandCollection::const_iterator jetItr = jets->begin() ;
-  L1GctJetCandCollection::const_iterator jetEnd = jets->end() ;
-  for( int i = 0 ; jetItr != jetEnd ; ++jetItr, ++i )
-    {
-      if(!jetItr->empty())
-        {
-          double et = jetScale->et(jetItr->rank());
-          L1JetParticle(gctLorentzVector(et,*jetItr,caloGeom,true),Ref<L1GctJetCandCollection>(jets,i),jetItr->bx());
-        }
-    }
-  
-  iEvent.put(tauJetColl,"Tau");
+  // put the collections into the event
+  iEvent.put(isoEm,"isoEm");
+  iEvent.put(nonIsoEm,"nonIsoEm");
+  iEvent.put(cenJet,"cenJets");
+  iEvent.put(forJet,"forJets");
+  iEvent.put(tauJet,"tauJets");
+  iEvent.put(etTot);
+  iEvent.put(etHad);
+  iEvent.put(etMiss);
+//  iEvent.put(htMiss);
+  iEvent.put(hfBitCount);
+  iEvent.put(hfRingEtSum);
 }
-
-math::PtEtaPhiMLorentzVector
-GtToGctCands::gctLorentzVector( const double& et,
-					const L1GctCand& cand,
-					const L1CaloGeometry* geom,
-					bool central )
-{
-   // To keep x and y components non-zero.
-   double etCorr = et + 1.e-6 ; // protect against roundoff, not only for et=0
-
-   double eta = geom->etaBinCenter( cand.etaIndex(), central ) ;
-
-//    double tanThOver2 = exp( -eta ) ;
-//    double ez = etCorr * ( 1. - tanThOver2 * tanThOver2 ) / ( 2. * tanThOver2 );
-//    double e  = etCorr * ( 1. + tanThOver2 * tanThOver2 ) / ( 2. * tanThOver2 );
-
-   double phi = geom->emJetPhiBinCenter( cand.phiIndex() ) ;
-
-//    return math::XYZTLorentzVector( etCorr * cos( phi ),
-// 				   etCorr * sin( phi ),
-// 				   ez,
-// 				   e ) ;
-   return math::PtEtaPhiMLorentzVector( etCorr,
-					eta,
-					phi,
-					0. ) ;
-}
-
