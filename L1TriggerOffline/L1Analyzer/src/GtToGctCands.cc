bcf1044c906460e22adbe6ec78e6e637602dd7db
Alex Tapper <sha1-11d34dd73d9f7a19fb1c333fabc2630e81f23142@cern.ch>
2009-04-02 12:01:48 +0000

    Horrible hack to make L1Extra Jets for timing study. Will need to go back and tidy all this up!

--- L1TriggerOffline/L1Analyzer/src/GtToGctCands.cc
+++ L1TriggerOffline/L1Analyzer/src/GtToGctCands.cc
@@ -11,29 +11,43 @@
 //
 // Original Author:  Alex Tapper
 //         Created:  Mon Mar 30 17:31:03 CEST 2009
-// $Id: GtToGctCands.cc,v 1.1 2009/04/01 12:32:05 tapper Exp $
+// $Id: GtToGctCands.cc,v 1.2 2009/04/01 15:10:00 georgia Exp $
 //
 //
 
 #include "L1TriggerOffline/L1Analyzer/interface/GtToGctCands.h"
-#include "DataFormats/L1GlobalCaloTrigger/interface/L1GctCollections.h"
+
 #include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
 
+#include "CondFormats/L1TObjects/interface/L1CaloEtScale.h"
+#include "CondFormats/DataRecord/interface/L1JetEtScaleRcd.h"
+
+#include "DataFormats/L1Trigger/interface/L1JetParticle.h"
+#include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
 
 GtToGctCands::GtToGctCands(const edm::ParameterSet& iConfig) :
   m_GTInputTag(iConfig.getParameter<edm::InputTag>("inputLabel"))
 {
+  using namespace l1extra;
+
   // For now I am making one electron collection and one jet collection with all electrons and jets from all 3 BXs.
   // This is the easiest format to analyse for CRAFT data.
   // In the future I should make different collections and treat the mutiple BXs properly, and add energy sums.
-  produces<L1GctEmCandCollection>();
+  //  produces<L1GctEmCandCollection>();
   produces<L1GctJetCandCollection>();
+
+  // Current hack since GCT collections are not in the RECO I make the L1Extra particles here too
+  produces<L1JetParticleCollection>( "Tau" ) ;
 }
 
 GtToGctCands::~GtToGctCands(){}
 
 void GtToGctCands::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  using namespace l1extra;
+  using namespace edm;
+  using namespace std;
+  
   // create em and jet collections
   std::auto_ptr<L1GctEmCandCollection> allElectrons (new L1GctEmCandCollection());
   std::auto_ptr<L1GctJetCandCollection> allJets (new L1GctJetCandCollection());
@@ -70,5 +84,62 @@ void GtToGctCands::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   // Put the new collections into the event
   iEvent.put(allElectrons);
   iEvent.put(allJets);
+
+  // Get the collection out again... sigh
+  edm::Handle<L1GctJetCandCollection> jets;
+  iEvent.getByLabel("gctCandsFromGt",jets);
+
+  // Create L1Extra collections
+  auto_ptr <L1JetParticleCollection> tauJetColl(new L1JetParticleCollection);
+
+  // Geometry
+  ESHandle <L1CaloGeometry> caloGeomESH ;
+  iSetup.get <L1CaloGeometryRecord>().get( caloGeomESH ) ;
+  const L1CaloGeometry* caloGeom = &( *caloGeomESH ) ;
+
+  // L1 jet scale
+  ESHandle <L1CaloEtScale> jetScale ;
+  iSetup.get <L1JetEtScaleRcd>().get(jetScale) ;
+
+  // Now make the L1Extra collection
+  L1GctJetCandCollection::const_iterator jetItr = jets->begin() ;
+  L1GctJetCandCollection::const_iterator jetEnd = jets->end() ;
+  for( int i = 0 ; jetItr != jetEnd ; ++jetItr, ++i )
+    {
+      if(!jetItr->empty())
+        {
+          double et = jetScale->et(jetItr->rank());
+          L1JetParticle(gctLorentzVector(et,*jetItr,caloGeom,true),Ref<L1GctJetCandCollection>(jets,i),jetItr->bx());
+        }
+    }
+  
+  iEvent.put(tauJetColl,"Tau");
+}
+
+math::PtEtaPhiMLorentzVector
+GtToGctCands::gctLorentzVector( const double& et,
+					const L1GctCand& cand,
+					const L1CaloGeometry* geom,
+					bool central )
+{
+   // To keep x and y components non-zero.
+   double etCorr = et + 1.e-6 ; // protect against roundoff, not only for et=0
+
+   double eta = geom->etaBinCenter( cand.etaIndex(), central ) ;
+
+//    double tanThOver2 = exp( -eta ) ;
+//    double ez = etCorr * ( 1. - tanThOver2 * tanThOver2 ) / ( 2. * tanThOver2 );
+//    double e  = etCorr * ( 1. + tanThOver2 * tanThOver2 ) / ( 2. * tanThOver2 );
+
+   double phi = geom->emJetPhiBinCenter( cand.phiIndex() ) ;
+
+//    return math::XYZTLorentzVector( etCorr * cos( phi ),
+// 				   etCorr * sin( phi ),
+// 				   ez,
+// 				   e ) ;
+   return math::PtEtaPhiMLorentzVector( etCorr,
+					eta,
+					phi,
+					0. ) ;
 }
 
