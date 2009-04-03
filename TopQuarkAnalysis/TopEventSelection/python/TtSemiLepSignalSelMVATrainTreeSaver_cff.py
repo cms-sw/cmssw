40d705d5d7f012be0221a91bdd085025613555d1
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-04-03 12:33:56 +0000

    the TrainTreeSaver should not write an mva-file

--- TopQuarkAnalysis/TopEventSelection/python/TtSemiLepSignalSelMVATrainTreeSaver_cff.py
+++ TopQuarkAnalysis/TopEventSelection/python/TtSemiLepSignalSelMVATrainTreeSaver_cff.py
@@ -7,11 +7,6 @@ import FWCore.ParameterSet.Config as cms
 ## import MVA trainer cfi
 from TopQuarkAnalysis.TopEventSelection.TtSemiLepSignalSelMVATrainer_cfi import *
 
-## define path for mva save file
-traintreeSaverSaveFile = cms.EDAnalyzer("TtSemiLepSignalSelMVASaveFile",
-    traintreeSaver = cms.string('TopQuarkAnalysis/TopEventSelection/data/TraintreeSaver.mva')
-)
-
 ## ------------------------------------------------------------------------------------------
 ## configuration of event looper for mva taining; take care to make the 
 ## looper known to the process. The way to do that is to add the following
@@ -22,7 +17,7 @@ traintreeSaverSaveFile = cms.EDAnalyzer("TtSemiLepSignalSelMVASaveFile",
 ## ------------------------------------------------------------------------------------------ 
 looper = cms.Looper("TtSemiLepSignalSelMVATrainerLooper",
     trainers = cms.VPSet(cms.PSet(
-        monitoring = cms.untracked.bool(True),
+        monitoring = cms.untracked.bool(False),
         loadState  = cms.untracked.bool(False),
         saveState  = cms.untracked.bool(True),
         calibrationRecord = cms.string('traintreeSaver'),
@@ -34,4 +29,4 @@ looper = cms.Looper("TtSemiLepSignalSelMVATrainerLooper",
 ## provide a sequence for the training
 ## remark: do not use this sequence if you want to call your trainer after an event filter
 ##         since the SaveFile module should be called in an unfiltered path!
-saveTrainTree = cms.Sequence(buildTraintree*traintreeSaverSaveFile)
+saveTrainTree = cms.Sequence(buildTraintree)
