7f704dcc5ab1db1068e9d37194858ba130d624a8
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-04-03 07:44:28 +0000

    order in useful way (lost during python migration), no changes in content

--- Alignment/CommonAlignmentProducer/python/AlignmentProducer_cff.py
+++ Alignment/CommonAlignmentProducer/python/AlignmentProducer_cff.py
@@ -1,66 +1,54 @@
-# The following comments couldn't be translated into the new config version:
-
-# can add more:
 import FWCore.ParameterSet.Config as cms
 
 # misalignment scenarios
 from Alignment.TrackerAlignment.Scenarios_cff import *
-#  replace TrackerShortTermScenario.TPBs.scale = 10.
-#  replace TrackerShortTermScenario.TPEs.scale = 10.
+
 # algorithms
 from Alignment.HIPAlignmentAlgorithm.HIPAlignmentAlgorithm_cfi import *
 from Alignment.MillePedeAlignmentAlgorithm.MillePedeAlignmentAlgorithm_cfi import *
 from Alignment.KalmanAlignmentAlgorithm.KalmanAlignmentAlgorithm_cfi import *
 # parameters
 from Alignment.CommonAlignmentAlgorithm.AlignmentParameterStore_cfi import *
+
 looper = cms.Looper("AlignmentProducer",
-    # include configuration for AlignmentParameterStore
-    AlignmentParameterStore,
-    # Save alignment corrections to DB: if true, requires configuration of PoolDBOutputService
-    # See DBConfiguration.cff for an example
-    saveToDB = cms.bool(False),
-    saveApeToDB = cms.bool(False),
-    doMuon = cms.untracked.bool(False),
-    MisalignmentScenario = cms.PSet(
-        NoMovementsScenario
-    ),
-    # number of selected alignables to be kept fixed
-    nFixAlignables = cms.int32(0),
-    # Misalignment from database: if true, requires configuration of PoolDBESSource
-    # See DBConfiguration.cff for an example
-    applyDbAlignment = cms.untracked.bool(False),
-    monitorConfig = cms.PSet(
-        monitors = cms.untracked.vstring()
-    ),
-    maxLoops = cms.untracked.uint32(1),
-    # Choose one algorithm
-    algoConfig = cms.PSet(
-        HIPAlignmentAlgorithm
-    ),
-    ParameterBuilder = cms.PSet(
-	parameterTypes = cms.vstring('Selector,RigidBody'),
-        Selector = cms.PSet(
-            # selection of alignables and their parameters:
-            # comma separated pairs of detector parts/levels as defined in AlinmentParameterSelector
-            # (note special meaning if the string contains "SS" or "DS" or ends with "Layers"
-            # followed by two digits)
-            # and of d.o.f. to be aligned (x,y,z,alpha,beta,gamma) in local frame:
-            # '0' means: deselect, '1' select. Others as 1, but might be interpreted in a special
-            # way in the used algorithm (e.g. 'f' means fixed for millepede)
-            alignParams = cms.vstring('PixelHalfBarrelLayers,111000')
-        )
-    ),
-    # simple misalignment applied to selected alignables and selected dof
-    parameterSelectorSimple = cms.string('-1'),
-    randomShift = cms.double(0.0),
-    doTracker = cms.untracked.bool(True),
-    # misalignment scenario
-    doMisalignmentScenario = cms.bool(False),
-    randomRotation = cms.double(0.0),
-    tjTkAssociationMapTag = cms.InputTag("TrackRefitter"),
-    # Read survey info from DB: if true, requires configuration of PoolDBESSource
-    # See Alignment/SurveyAnalysis/test/readDB.cfg for an example
-    useSurvey = cms.bool(False)
-)
+                    AlignmentParameterStore, # configuration of AlignmentParameterStore
+                    maxLoops = cms.untracked.uint32(1),
+                    doTracker = cms.untracked.bool(True),
+                    doMuon = cms.untracked.bool(False),
+                    # Read survey info from DB: true requires configuration of PoolDBESSource
+                    # See Alignment/SurveyAnalysis/test/readDB.cfg for an example
+                    useSurvey = cms.bool(False),
+                    
+                    # (Mis-)alignment from database: true requires configuration of PoolDBESSource
+                    applyDbAlignment = cms.untracked.bool(False),
+                    # misalignment scenario
+                    MisalignmentScenario = cms.PSet(NoMovementsScenario), # why not by reference?
+                    doMisalignmentScenario = cms.bool(False),
+                    # simple misalignment of selected alignables and selected dof (deprecated!)
+                    randomShift = cms.double(0.0),
+                    randomRotation = cms.double(0.0),
+                    parameterSelectorSimple = cms.string('-1'),
+                    
+                    # selection of alignables and their parameters
+                    # see twiki: SWGuideAlignmentAlgorithms
+                    ParameterBuilder = cms.PSet(parameterTypes = cms.vstring('Selector,RigidBody'),
+                                                Selector = cms.PSet(alignParams = cms.vstring('PixelHalfBarrelLayers,111000'))
+                                                ),
+                    # number of selected alignables to be kept fixed (deprecated!)
+                    nFixAlignables = cms.int32(0), # i.e. removed from selection above...
+
+                    # input
+                    tjTkAssociationMapTag = cms.InputTag("TrackRefitter"),
+                    
+
+                    # Choose one algorithm with configuration, HIP is default
+                    algoConfig = cms.PSet(HIPAlignmentAlgorithm), # why not by reference?
+                    # choose monitors (default is none)
+                    monitorConfig = cms.PSet(monitors = cms.untracked.vstring()),
+
+                    # Save alignment to DB: true requires configuration of PoolDBOutputService
+                    saveToDB = cms.bool(False),    # save alignment?
+                    saveApeToDB = cms.bool(False) # save APE?
+                    )
 
 
