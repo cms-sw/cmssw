5768e2dc5d6964d57f32618dc9ff7f83655f7bb9
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-03 09:31:01 +0000

    move parameters from untreacked to tracked

--- Geometry/EcalTestBeam/test/write_crystal_cfg.pl
+++ Geometry/EcalTestBeam/test/write_crystal_cfg.pl
@@ -1,5 +1,3 @@
-#! /usr/bin/perl -w
-
 # build the cff file for all the crystals corresponding to
 # their nominal maximum reference point as stored in the table                                                                                
 # F. Cossutti - 23-Aug-2006 15:19
@@ -30,13 +28,13 @@ if (! open(INPUT,"<$file") ) {
         print OUTFILE "import FWCore.ParameterSet.Config as cms\n";
         print OUTFILE "                                        \n";   
         print OUTFILE "common_beam_direction_parameters = cms.PSet(\n";
-        print OUTFILE "    MinEta = cms.untracked.double(".$eta,"),\n";
-        print OUTFILE "    MaxEta = cms.untracked.double(".$eta,"),\n";
-        print OUTFILE "    MinPhi = cms.untracked.double(".$phi,"),\n";
-        print OUTFILE "    MaxPhi = cms.untracked.double(".$phi,"),\n";
-        print OUTFILE "    BeamMeanX = cms.untracked.double(0.0),\n";
-        print OUTFILE "    BeamMeanY = cms.untracked.double(0.0),\n";
-        print OUTFILE "    BeamPosition = cms.untracked.double(-26733.5)\n";
+        print OUTFILE "    MinEta = cms.double(".$eta,"),\n";
+        print OUTFILE "    MaxEta = cms.double(".$eta,"),\n";
+        print OUTFILE "    MinPhi = cms.double(".$phi,"),\n";
+        print OUTFILE "    MaxPhi = cms.double(".$phi,"),\n";
+        print OUTFILE "    BeamMeanX = cms.double(0.0),\n";
+        print OUTFILE "    BeamMeanY = cms.double(0.0),\n";
+        print OUTFILE "    BeamPosition = cms.double(-26733.5)\n";
         print OUTFILE ")\n";
  
     }
