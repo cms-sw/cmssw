7dbde1f631cf4abafc4d63f549eb43802625e096
Anne-Catherine Le Bihan <sha1-264c07a2add6d826106132136f9458ca5720117d@cern.ch>
2009-04-04 12:30:33 +0000

    insertion of historiy DQM

--- CondCore/DQMPlugins/src/plugin.cc
+++ CondCore/DQMPlugins/src/plugin.cc
@@ -3,3 +3,6 @@
 #include "CondFormats/DataRecord/interface/DQMSummaryRcd.h"
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(DQMSummaryRcd, DQMSummary);
+#include "CondFormats/DQMObjects/interface/HDQMSummary.h"
+#include "CondFormats/DataRecord/interface/HDQMSummaryRcd.h"
+REGISTER_PLUGIN(HDQMSummaryRcd, HDQMSummary);
