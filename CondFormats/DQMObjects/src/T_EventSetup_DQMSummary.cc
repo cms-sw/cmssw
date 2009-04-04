2c1f06d71a09d71a7fc9bd85b0ca74c1bcd7cc51
Anne-Catherine Le Bihan <sha1-264c07a2add6d826106132136f9458ca5720117d@cern.ch>
2009-04-04 12:37:05 +0000

    implementation of HDQMSummary

--- CondFormats/DQMObjects/src/T_EventSetup_DQMSummary.cc
+++ CondFormats/DQMObjects/src/T_EventSetup_DQMSummary.cc
@@ -1,3 +1,5 @@
 #include "CondFormats/DQMObjects/interface/DQMSummary.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 EVENTSETUP_DATA_REG(DQMSummary);
+#include "CondFormats/DQMObjects/interface/HDQMSummary.h"
+EVENTSETUP_DATA_REG(HDQMSummary);
