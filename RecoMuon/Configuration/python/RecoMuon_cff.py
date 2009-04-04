391efb9863cc1fd6093a33d5e2715c288ebbe906
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2009-04-04 21:17:27 +0000

    add a muon sequence based on SET standalone muons

--- RecoMuon/Configuration/python/RecoMuon_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_cff.py
@@ -20,12 +20,24 @@ from RecoMuon.MuonIsolationProducers.muIsolation_cff import *
 muontracking = cms.Sequence(standAloneMuonSeeds*standAloneMuons*globalMuons)
 muontracking_with_TeVRefinement = cms.Sequence(muontracking*tevMuons)
 
+# sequence with SET standalone muons
+globalSETMuons = RecoMuon.GlobalMuonProducer.GlobalMuonProducer_cff.globalMuons.clone()
+globalSETMuons.MuonCollectionLabel = cms.InputTag("standAloneSETMuons","UpdatedAtVtx")
+from RecoMuon.MuonSeedGenerator.SETMuonSeed_cff import *
+muontracking_with_SET = cms.Sequence(SETMuonSeed*standAloneSETMuons*globalSETMuons)
+
 # Muon Reconstruction
 muonreco = cms.Sequence(muontracking*muonIdProducerSequence)
 muonrecowith_TeVRefinemen = cms.Sequence(muontracking_with_TeVRefinement*muonIdProducerSequence)
 
+muonsWithSET = RecoMuon.MuonIdentification.muons_cfi.muons.clone()
+muonsWithSET.inputCollectionLabels = ['generalTracks', 'globalSETMuons', cms.InputTag('standAloneSETMuons','UpdatedAtVtx')] 
+muonsWithSET.inputCollectionTypes = ['inner tracks', 'links', 'outer tracks']   
+muonreco_with_SET = cms.Sequence(muontracking_with_SET*muonsWithSET)
+
 # Muon Reconstruction plus Isolation
 muonreco_plus_isolation = cms.Sequence(muonrecowith_TeVRefinemen*muIsolation)
+muonreco_plus_isolation_plus_SET = cms.Sequence(muonrecowith_TeVRefinemen*muonreco_with_SET*muIsolation)
 
 ########################################################
 
