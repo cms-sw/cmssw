f7de6a074dcc318729fdef76b31e5217042e99e2
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-04-05 11:16:02 +0000

    Tag change in cfg file

--- DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
+++ DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
@@ -79,7 +79,7 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
 #process.GlobalTag.globaltag = "CRZT210_V1H::All"
 #process.GlobalTag.globaltag = 'CRAFT_V3P::All'
-process.GlobalTag.globaltag = "CRAFT_30X::All"
+process.GlobalTag.globaltag = "CRAFT_31X::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #--------------------------
