67aba24074aa6045219db39ec4f2754397118cf8
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-04-06 09:16:56 +0000

    changed for MWGR15

--- DQM/HLTEvF/python/HLTMonElectron_cff.py
+++ DQM/HLTEvF/python/HLTMonElectron_cff.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQM.HLTEvF.HLTMonElectron_cfi import *
+#from DQM.HLTEvF.HLTMonElectron_cfi import *
 #from DQM.HLTEvF.electronDQMIsoDistTrigger_cfi import *
 #from DQM.HLTEvF.electronDQMIsoDist_cfi import *
 #from DQM.HLTEvF.electronDQMPixelMatch_cfi import *
 #from DQM.HLTEvF.electronDQMConsumer_cfi import *
-hltMonElectronPath = cms.Path(hltMonE)
+#hltMonElectronPath = cms.Path(hltMonE)
 #hltMonElectronPath = cms.Path(hltMonE*electronDQMIsoDistTrigger*electronDQMIsoDist*electronDQMPixelMatch*electronDQMConsumer)
 
 
+from DQM.HLTEvF.electron_test_cff import *
+hltMonElectronPath = cms.Path(sourcePlots)
