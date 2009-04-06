f50fcdae4f2ba556d4be12859bf4100007614808
Gregory Heath <sha1-bf95c0421ce165ead7db2dfa09292af9303db7fd@cern.ch>
2009-04-06 15:17:00 +0000

    Add tag for input data from RCT

--- L1Trigger/GlobalCaloTrigger/python/l1GctValidation_cfi.py
+++ L1Trigger/GlobalCaloTrigger/python/l1GctValidation_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 l1GctValidation = cms.EDFilter("L1GctValidation",
-    inputTag  = cms.untracked.InputTag("simGctDigis")
+    rctInputTag  = cms.untracked.InputTag("simRctDigis"),
+    gctInputTag  = cms.untracked.InputTag("simGctDigis")
 )
 
 
