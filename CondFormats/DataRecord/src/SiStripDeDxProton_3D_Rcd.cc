516697bcf7582c677428d62f96e9ae03187b58df
Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch>
2009-04-06 09:11:16 +0000

    new DeDx records

---
+++ CondFormats/DataRecord/src/SiStripDeDxProton_3D_Rcd.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/DataRecord/interface/SiStripDeDxProton_3D_Rcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiStripDeDxProton_3D_Rcd);
