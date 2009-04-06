34bb68a566dabe245431478a8fdcf6c3dac4e446
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-06 13:21:06 +0000

    Set all the needed customizations for g4SimHits in order to run with ZDC

--- Configuration/StandardSequences/python/SimExtended_cff.py
+++ Configuration/StandardSequences/python/SimExtended_cff.py
@@ -5,6 +5,16 @@ from SimTransport.HectorProducer.HectorTransport_cfi import *
 
 # CMSSW/Geant4 interface
 from SimG4Core.Configuration.SimG4Core_cff import *
+
+g4SimHits.Generator.HepMCProductLabel = cms.string('LHCTransport')
+g4SimHits.StackingAction.MaxTimeNames = cms.vstring('ZDCRegion')
+g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0)
+g4SimHits.SteppingAction.MaxTimeNames = cms.vstring('ZDCRegion')
+g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0)
+g4SimHits.CaloSD.HCNames = cms.vstring('EcalHitsEB','EcalHitsEE','EcalHitsES','HcalHits','ZDCHITS')
+g4SimHits.CaloSD.EminHits = cms.vdouble(0.015,0.010,0.0,0.0,0.0)
+g4SimHits.CaloSD.TmaxHits = cms.vdouble(500.0,500.0,500.0,500.0,2000.0)
+
 psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*LHCTransport*g4SimHits)
 
 
