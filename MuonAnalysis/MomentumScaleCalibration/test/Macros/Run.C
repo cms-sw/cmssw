9441c3a8383408da2d036158cd55164699e2358f
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-04-07 08:04:31 +0000

    Updated file names

--- MuonAnalysis/MomentumScaleCalibration/test/Macros/Run.C
+++ MuonAnalysis/MomentumScaleCalibration/test/Macros/Run.C
@@ -4,14 +4,14 @@
   // rebinX, rebinY, fitType (1:gaussian; 2:lorentzian, ...), output file name (default "filegraph.root")
 
   // Y: gaussian fit
-  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPt\", \"0_MuScleFit.root\", \"2_MuScleFit.root\", \"Resonance mass vs pt\", \"Y\", 4, 4, 1, \"filegraph_pt.root\")" );
-  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSEta\", \"0_MuScleFit.root\", \"2_MuScleFit.root\", \"Resonance mass vs #eta\", \"Y\", 2, 2, 1, \"filegraph_eta.root\")" );
-  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPhiPlus\", \"0_MuScleFit.root\", \"2_MuScleFit.root\", \"Resonance mass vs #phi\", \"Y\", 2, 2, 1, \"filegraph_phi.root\")" );
+  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPt\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs pt\", \"Y\", 4, 4, 1, \"filegraph_pt.root\")" );
+  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSEta\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs #eta\", \"Y\", 2, 2, 1, \"filegraph_eta.root\")" );
+  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPhiPlus\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs #phi\", \"Y\", 2, 2, 1, \"filegraph_phi.root\")" );
 
   // Z: lorentzian fit
-  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPt\", \"0_MuScleFit.root\", \"1_MuScleFit.root\", \"Resonance mass vs pt\", \"Z\", 4, 4, 2)" );
-  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSEta\", \"0_MuScleFit.root\", \"1_MuScleFit.root\", \"Resonance mass vs #eta\", \"Z\", 4, 4, 2)" );
-  // gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPhiPlus\", \"0_MuScleFit.root\", \"1_MuScleFit.root\", \"Resonance mass vs #phi\", \"Z\", 4, 4, 2)" );
+  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPt\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs pt\", \"Z\", 4, 4, 2, \"filegraph_pt.root\")" );
+  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSEta\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs #eta\", \"Z\", 4, 4, 2, \"filegraph_eta.root\")" );
+  gROOT->ProcessLine( "macroPlot(\"hRecBestResVSMu_MassVSPhiPlus\", \"0_MuScleFit.root\", \"3_MuScleFit.root\", \"Resonance mass vs #phi\", \"Z\", 4, 4, 2, \"filegraph_phi.root\")" );
 
   // gROOT->Reset();
 }
