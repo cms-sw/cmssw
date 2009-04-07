ff2a64df926f5a33449a1bbcf73da2a7f1c2e7ee
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-04-07 09:42:13 +0000

    bug fix

--- OnlineDB/EcalCondDB/src/DCUIDarkPedDat.cc
+++ OnlineDB/EcalCondDB/src/DCUIDarkPedDat.cc
@@ -88,7 +88,7 @@ void DCUIDarkPedDat::fetchData(std::map< EcalLogicID, DCUIDarkPedDat >* fillMap,
 
     m_readStmt->setSQL("SELECT cv.name, cv.logic_id, cv.id1, cv.id2, cv.id3, cv.maps_to, "
 		 "d.capsule_temp "
-		 "FROM channelview cv JOIN dcu_capsule_temp_dat d "
+		 "FROM channelview cv JOIN dcu_idark_ped_dat d "
 		 "ON cv.logic_id = d.logic_id AND cv.name = cv.maps_to "
 		 "WHERE d.iov_id = :iov_id");
     m_readStmt->setInt(1, iovID);
