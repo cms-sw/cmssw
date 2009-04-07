b1bf4519f3afd2148f0aedbffb6aea411471c216
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-04-07 14:35:34 +0000

    removed a few whitespaces during testing

--- PhysicsTools/PatAlgos/plugins/DuplicatedElectronCleaner.cc
+++ PhysicsTools/PatAlgos/plugins/DuplicatedElectronCleaner.cc
@@ -1,5 +1,5 @@
 //
-// $Id: DuplicatedElectronCleaner.cc,v 1.1.2.2.2.1 2009/01/12 22:08:06 gpetrucc Exp $
+// $Id: DuplicatedElectronCleaner.cc,v 1.2 2009/03/26 05:02:41 hegner Exp $
 //
 
 /**
@@ -15,83 +15,78 @@
    which can be read through edm::View<reco::GsfElectron>
 
   \author   Giovanni Petrucciani
-  \version  $Id: DuplicatedElectronCleaner.cc,v 1.1.2.2.2.1 2009/01/12 22:08:06 gpetrucc Exp $
+  \version  $Id: DuplicatedElectronCleaner.cc,v 1.2 2009/03/26 05:02:41 hegner Exp $
 */
 
-
-#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
-
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 //#include "DataFormats/Common/interface/RefVector.h"
 #include "DataFormats/Common/interface/RefToBaseVector.h"
 //#include "DataFormats/Common/interface/PtrVector.h"
 
-#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "PhysicsTools/PatUtils/interface/DuplicatedElectronRemover.h"
 
-namespace pat {
-
-  class DuplicatedElectronCleaner : public edm::EDProducer {
-    public:
-      explicit DuplicatedElectronCleaner(const edm::ParameterSet & iConfig);
-      ~DuplicatedElectronCleaner();  
-
-      virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
-      virtual void endJob();
-
-    private:
-      edm::InputTag electronSrc_;
-      pat::DuplicatedElectronRemover duplicateRemover_; 
-      uint64_t try_, pass_;
+namespace pat{
+  class DuplicatedElectronCleaner : public edm::EDProducer{
+  public:
+    explicit DuplicatedElectronCleaner(const edm::ParameterSet & iConfig);
+    ~DuplicatedElectronCleaner();  
+    
+    virtual void produce(edm::Event & iEvent, const edm::EventSetup & iSetup);
+    virtual void endJob();
+    
+  private:
+    edm::InputTag electronSrc_;
+    pat::DuplicatedElectronRemover duplicateRemover_; 
+    uint64_t try_, pass_;
   };
-
 } // namespace
 
-pat::DuplicatedElectronCleaner::DuplicatedElectronCleaner(const edm::ParameterSet & iConfig) :
-    electronSrc_(iConfig.getParameter<edm::InputTag>("electronSource")),
-    try_(0), pass_(0)
+pat::DuplicatedElectronCleaner::DuplicatedElectronCleaner(const edm::ParameterSet & iConfig):
+  electronSrc_(iConfig.getParameter<edm::InputTag>("electronSource")),
+  try_(0), pass_(0)
 {
-    //produces<edm::RefVector<reco::GsfElectronCollection> >();
-    produces<edm::RefToBaseVector<reco::GsfElectron> >();
-    //produces<edm::PtrVector<reco::GsfElectron> >();
+  //produces<edm::RefVector<reco::GsfElectronCollection> >();
+  produces<edm::RefToBaseVector<reco::GsfElectron> >();
+  //produces<edm::PtrVector<reco::GsfElectron> >();
 }
 
-pat::DuplicatedElectronCleaner::~DuplicatedElectronCleaner() {
+pat::DuplicatedElectronCleaner::~DuplicatedElectronCleaner()
+{
 }
 
 void 
-pat::DuplicatedElectronCleaner::produce(edm::Event & iEvent, const edm::EventSetup & iSetup) {
+pat::DuplicatedElectronCleaner::produce(edm::Event & iEvent, const edm::EventSetup & iSetup)
+{
   using namespace edm;
   Handle<View<reco::GsfElectron> > electrons;
   iEvent.getByLabel(electronSrc_, electrons);
   try_ += electrons->size();
-
+  
   //std::auto_ptr<RefVector<reco::GsfElectronCollection> > result(new RefVector<reco::GsfElectronCollection>());
   std::auto_ptr<RefToBaseVector<reco::GsfElectron> > result(new RefToBaseVector<reco::GsfElectron>());
   //std::auto_ptr<PtrVector<reco::GsfElectron> > result(new PtrVector<reco::GsfElectron>());
-
   std::auto_ptr< std::vector<size_t> > duplicates = duplicateRemover_.duplicatesToRemove(*electrons);
-
+  
   std::vector<size_t>::const_iterator itdup = duplicates->begin(), enddup = duplicates->end();
   for (size_t i = 0, n = electrons->size(); i < n; ++i) {
-        while ((itdup != enddup) && (*itdup < i)) { ++itdup; }
-        if ((itdup != enddup) && (*itdup == i)) continue;
-        //result->push_back(electrons->refAt(i).castTo<edm::Ref<reco::GsfElectronCollection> >());
-        result->push_back(electrons->refAt(i));
-        //result->push_back(electrons->ptrAt(i));
+    while ((itdup != enddup) && (*itdup < i)) { ++itdup; }
+    if ((itdup != enddup) && (*itdup == i)) continue;
+    //result->push_back(electrons->refAt(i).castTo<edm::Ref<reco::GsfElectronCollection> >());
+    result->push_back(electrons->refAt(i));
+    //result->push_back(electrons->ptrAt(i));
   }
-
   pass_ += result->size(); 
   iEvent.put(result);
 }
 
-
 void 
-pat::DuplicatedElectronCleaner::endJob() { 
+pat::DuplicatedElectronCleaner::endJob() 
+{ 
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
