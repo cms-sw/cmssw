5d147400af49fb9b22d100ea4a23913132bd6579
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2009-04-07 16:18:23 +0000

    added ecal 5x5 energy; max hit energy for ECAL and HCAL; trajectory position at ECAL and HCAL surface; HCAL timing

--- DataFormats/MuonReco/interface/MuonEnergy.h
+++ DataFormats/MuonReco/interface/MuonEnergy.h
@@ -1,7 +1,8 @@
 #ifndef MuonReco_MuonEnergy_h
 #define MuonReco_MuonEnergy_h
 
-
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/Math/interface/Point3D.h"
 namespace reco {
     struct MuonEnergy {
        /// CaloTower based energy
@@ -10,31 +11,48 @@ namespace reco {
        /// total energy in 3x3 tower shape
        float towerS9; 
        
-       /// RecHit based energy (eta-phi size don't match!)
+       /// ECAL crystal based energy (RecHits)
        /// energy deposited in crossed ECAL crystals
        float em; 
-       /// energy deposited in 3x3 ECAL crystal shape around 
-       /// crossed crystal
+       /// energy deposited in 3x3 ECAL crystal shape around central crystal
        float emS9;
-       /// energy deposited in crossed HCAL tower (RecHits)
+       /// energy deposited in 5x5 ECAL crystal shape around central crystal
+       float emS25;
+       /// maximal energy of ECAL crystal in the 5x5 shape
+       float emMax;
+       
+       /// HCAL tower based energy (RecHits)
+       /// energy deposited in crossed HCAL towers
        float had;
-       /// energy deposited in 3x3 HCAL tower shape around 
-       /// crossed tower (RecHits)
+       /// energy deposited in 3x3 HCAL tower shape around central tower
        float hadS9;
-       /// energy deposited in crossed HO tower (RecHits)
+       /// maximal energy of HCAL tower in the 3x3 shape
+       float hadMax;
+       /// energy deposited in crossed HO towers
        float ho;
-       /// energy deposited in 3x3 HO tower shape around 
-       /// crossed tower (RecHits)
+       /// energy deposited in 3x3 HO tower shape around central tower
        float hoS9;
        
-       /// Time for the crossed ECAL crystall.
+       /// Calorimeter timing
        float ecal_time;
+       float hcal_time;
+       
+       /// Trajectory position at the calorimeter
+       math::XYZPoint ecal_position;
+       math::XYZPoint hcal_position;
+       
+       /// DetId of the central ECAL crystal
+       DetId ecal_id;
+       
+       /// DetId of the central HCAL tower with smallest depth
+       DetId hcal_id;
        
        MuonEnergy():
        tower(0), towerS9(0),
-       em(0), emS9(0),
-       had(0), hadS9(0),
-       ho(0), hoS9(0), ecal_time(0){ }
+       em(0), emS9(0), emS25(0), emMax(0),
+       had(0), hadS9(0), hadMax(0),
+       ho(0), hoS9(0), ecal_time(0), hcal_time(0)
+	 { }
        
     };
 }
