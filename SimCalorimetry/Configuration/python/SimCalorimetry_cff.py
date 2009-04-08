f65e6843e8bcc6337b63a409eed536372b33006f
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-08 15:11:39 +0000

    add a placeholder for the CASTOR digitization

--- SimCalorimetry/Configuration/python/SimCalorimetry_cff.py
+++ SimCalorimetry/Configuration/python/SimCalorimetry_cff.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from SimCalorimetry.Configuration.ecalDigiSequence_cff import *
 from SimCalorimetry.Configuration.hcalDigiSequence_cff import *
+from SimCalorimetry.Configuration.castorDigiSequence_cff import *
+#calDigi = cms.Sequence(ecalDigiSequence+hcalDigiSequence+castorDigiSequence)
 calDigi = cms.Sequence(ecalDigiSequence+hcalDigiSequence)
 
