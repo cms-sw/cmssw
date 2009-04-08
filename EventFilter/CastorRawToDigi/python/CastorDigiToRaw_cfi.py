c770746eb86f6c237ba2c7c001266b86ae67e33b
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-08 15:07:19 +0000

    correct the name of the input digi collection for the default simulation name

--- EventFilter/CastorRawToDigi/python/CastorDigiToRaw_cfi.py
+++ EventFilter/CastorRawToDigi/python/CastorDigiToRaw_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 castorRawData = cms.EDFilter("CastorDigiToRaw",
-    CASTOR = cms.untracked.InputTag("castorDigis"),
+    CASTOR = cms.untracked.InputTag("simCastorDigis"),
 )
