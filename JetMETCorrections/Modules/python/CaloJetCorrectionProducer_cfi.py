9ada246755436331363906408d2cec239ce5e110
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2009-04-08 16:21:10 +0000

    Removed the import statements

--- JetMETCorrections/Modules/python/CaloJetCorrectionProducer_cfi.py
+++ JetMETCorrections/Modules/python/CaloJetCorrectionProducer_cfi.py
@@ -1,8 +1,6 @@
 # Example cfi file for the CaloJet correction producer. 
 # It is used for the HLT confguration database.
 import FWCore.ParameterSet.Config as cms
-from JetMETCorrections.Modules.JetCorrectionServiceChain_cfi import *
-
 L2L3CorJetIC5Calo = cms.EDProducer("CaloJetCorrectionProducer",
     src = cms.InputTag("iterativeCone5CaloJets"),
     correctors = cms.vstring('L2L3JetCorrectorIC5Calo')
