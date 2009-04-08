c5f5830e5ebe419ef3ca9703b9d67c0329753c6c
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-04-08 22:30:11 +0000

    handling of intercalib MC constants for ecal

--- CalibCalorimetry/CaloMiscalibTools/plugins/SealModule.cc
+++ CalibCalorimetry/CaloMiscalibTools/plugins/SealModule.cc
@@ -3,12 +3,16 @@
 #include "FWCore/Framework/interface/SourceFactory.h"
 #include "CalibCalorimetry/CaloMiscalibTools/interface/EcalRecHitRecalib.h"
 #include "CalibCalorimetry/CaloMiscalibTools/interface/CaloMiscalibTools.h"
+#include "CalibCalorimetry/CaloMiscalibTools/interface/CaloMiscalibToolsMC.h"
 #include "CalibCalorimetry/CaloMiscalibTools/interface/HcalRecHitRecalib.h"
 #include "CalibCalorimetry/CaloMiscalibTools/interface/WriteEcalMiscalibConstants.h"
+#include "CalibCalorimetry/CaloMiscalibTools/interface/WriteEcalMiscalibConstantsMC.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CaloMiscalibTools);
+DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(CaloMiscalibToolsMC);
 DEFINE_ANOTHER_FWK_MODULE(EcalRecHitRecalib);
 DEFINE_ANOTHER_FWK_MODULE(HcalRecHitRecalib);
 DEFINE_ANOTHER_FWK_MODULE(WriteEcalMiscalibConstants);
+DEFINE_ANOTHER_FWK_MODULE(WriteEcalMiscalibConstantsMC);
 
