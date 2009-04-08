fb08e8d748106a7873d22643eab5a3c6d014ade8
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2009-04-08 12:24:25 +0000

    TMVA default output is [-1, 1], and the benchmark point cuts are computed accordingly.
    In general, the user will enable RemapOutput in teh TauMVADiscriminator, which will screw up the cuts.
    
    Add code to remap the benchmark points added to the producers if the discriminators ahve this feature enabled.

--- RecoTauTag/TauTagTools/python/TauNeuralClassifiers_cfi.py
+++ RecoTauTag/TauTagTools/python/TauNeuralClassifiers_cfi.py
@@ -11,7 +11,14 @@ from RecoTauTag.TauTagTools.BenchmarkPointCuts_cfi import *
 
 def UpdateCuts(TheProducer, TheCuts):
    for aComputer in TheProducer.computers:
-      aComputer.cut = cms.double(TheCuts[aComputer.computerName.value()])
+      TheCut = TheCuts[aComputer.computerName.value()]
+      # By default TMVA produces an output from [-1, 1].  The benchmark points are computed using this range.
+      #  if the user has specified to remap the output to [0, 1] (usual case), remap the benchmark point used 
+      #  to produce the binary decision
+      if TheProducer.RemapOutput:
+         TheCut += 1.0
+         TheCut /= 2.0
+      aComputer.cut = cms.double(TheCut)
 
 shrinkingConePFTauDiscriminationByTaNCfrOnePercent = copy.deepcopy(shrinkingConePFTauDiscriminationByTaNC)
 shrinkingConePFTauDiscriminationByTaNCfrOnePercent.MakeBinaryDecision = cms.bool(True)
