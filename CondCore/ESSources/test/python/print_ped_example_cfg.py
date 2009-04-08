301f56b15ac287d6df10c4f28e1a2d21e437cfbd
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-04-08 11:03:24 +0000

    adopt first since changes in DBOutputService test

--- CondCore/ESSources/test/python/print_ped_example_cfg.py
+++ CondCore/ESSources/test/python/print_ped_example_cfg.py
@@ -14,9 +14,9 @@ process.PoolDBESSource = cms.ESSource("PoolDBESSource",
 )
 
 process.source = cms.Source("EmptyIOVSource",
-    lastValue = cms.uint64(10),
+    lastValue = cms.uint64(19),
     timetype = cms.string('runnumber'),
-    firstValue = cms.uint64(1),
+    firstValue = cms.uint64(16),
     interval = cms.uint64(1)
 )
 
