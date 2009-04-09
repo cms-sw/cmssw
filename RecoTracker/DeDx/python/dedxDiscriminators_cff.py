45c871cdc2f96cbb6ea42d6fa300d97f3c6bedc8
Loic Quertenmont <sha1-87ca56b44ac7447a9c679691ad6ed79ac661d179@cern.ch>
2009-04-09 12:43:37 +0000

    Update cff

--- RecoTracker/DeDx/python/dedxDiscriminators_cff.py
+++ RecoTracker/DeDx/python/dedxDiscriminators_cff.py
@@ -1,17 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-dedxDiscrimProd = cms.EDProducer("DeDxDiscriminatorProducer",
-    tracks                     = cms.InputTag("generalTracks"),
-    trajectoryTrackAssociation = cms.InputTag("generalTracks"),
 
-    Reccord            = cms.untracked.string("SiStripDeDxMip_3D_Rcd"),
-    Formula            = cms.untracked.uint32(0),
-
-    UseStrip           = cms.bool(True),
-    UsePixel           = cms.bool(True),
-    MeVperADCStrip     = cms.double(3.61e-06*250),
-    MeVperADCPixel     = cms.double(3.61e-06)
-)
+from RecoTracker.DeDx.dedxDiscriminator_Prod_cfi import *
 
 dedxDiscrimBTag         = dedxDiscrimProd.clone()
 dedxDiscrimBTag.Formula = cms.untracked.uint32(1)
