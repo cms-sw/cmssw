924228ac14809daa366036db57b8257bb3e4b53f
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-04-09 15:33:46 +0000

    - changed label of ValueMap containing all particle flow based IsoDeposits - removal of unused configs

--- PhysicsTools/PatAlgos/python/recoLayer0/tauIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/tauIsolation_cff.py
@@ -34,68 +34,34 @@ tauIsoDepositPFCandidates = cms.EDProducer("CandIsoDepositProducer",
     )                                             
 )
 
-#tauIsoFromDepsPFCandidates = cms.EDFilter("CandIsolatorFromDeposits",
-#    deposits = cms.VPSet(
-#        # compute sum of IsoDeposits within isolation cone of size 0.5 in eta-phi
-#        cms.PSet(
-#            src = cms.InputTag("tauIsoDepositPFCandidates"),
-#            deltaR = cms.double(0.5),
-#            weight = cms.string('1'),
-#            vetos = cms.vstring('0.01', 'Threshold(2.0)'),
-#            skipDefaultVeto = cms.bool(True),
-#            mode = cms.string('sum')
-#        )
-#    )
-#)
-
 # compute IsoDeposits from PFChargedHadrons
 tauIsoDepositPFChargedHadrons = copy.deepcopy(tauIsoDepositPFCandidates)
 tauIsoDepositPFChargedHadrons.ExtractorPSet.candidateSource = cms.InputTag("pfAllChargedHadrons")
-
-#tauIsoFromDepsPFChargedHadrons = copy.deepcopy(tauIsoFromDepsPFCandidates)
-#tauIsoFromDepsPFChargedHadrons.deposits[0].src = cms.InputTag("tauIsoDepositPFChargedHadrons")
 
 # compute IsoDeposits from PFNeutralHadrons
 tauIsoDepositPFNeutralHadrons = copy.deepcopy(tauIsoDepositPFCandidates)
 tauIsoDepositPFNeutralHadrons.ExtractorPSet.candidateSource = cms.InputTag("pfAllNeutralHadrons")
 
-#tauIsoFromDepsPFNeutralHadrons = copy.deepcopy(tauIsoFromDepsPFCandidates)
-#tauIsoFromDepsPFNeutralHadrons.deposits[0].src = cms.InputTag("tauIsoDepositPFNeutralHadrons")
-
 # compute IsoDeposits from PFGammas
 tauIsoDepositPFGammas = copy.deepcopy(tauIsoDepositPFCandidates)
 tauIsoDepositPFGammas.ExtractorPSet.candidateSource = cms.InputTag("pfAllPhotons")
 
-#tauIsoFromDepsPFGammas = copy.deepcopy(tauIsoFromDepsPFCandidates)
-#tauIsoFromDepsPFGammas.deposits[0].src = cms.InputTag("tauIsoDepositPFGammas")
-
-# define module labels for old (tk-based isodeposit) POG isolation
-patAODPFTauIsolationLabels = cms.VInputTag(
-    cms.InputTag("tauIsoDepositPFCandidates"),
-    cms.InputTag("tauIsoDepositPFChargedHadrons"),
-    cms.InputTag("tauIsoDepositPFNeutralHadrons"),
-    cms.InputTag("tauIsoDepositPFGammas")
-)
-
 # read and convert to ValueMap<IsoDeposit> keyed to Candidate;
 # to be run on AOD before PAT
-patAODPFTauIsolation = cms.EDFilter("MultipleIsoDepositsToValueMaps",
-    collection   = cms.InputTag("pfRecoTauProducer"),
-    associations = patAODPFTauIsolationLabels
+pfTauIsolationValueMap = cms.EDFilter("MultipleIsoDepositsToValueMaps",
+    collection = cms.InputTag("pfRecoTauProducer"),
+    associations = cms.VInputTag(
+        cms.InputTag("tauIsoDepositPFCandidates"),
+        cms.InputTag("tauIsoDepositPFChargedHadrons"),
+        cms.InputTag("tauIsoDepositPFNeutralHadrons"),
+        cms.InputTag("tauIsoDepositPFGammas")
+    )
 )
 
-# re-key to the candidates;
-# to be run at the end of PAT Layer 0
-patLayer0PFTauIsolation = cms.EDFilter("CandManyValueMapsSkimmerIsoDeposits",
-    collection   = cms.InputTag("allLayer0Taus"),
-    backrefs     = cms.InputTag("allLayer0Taus"),
-    commonLabel  = cms.InputTag("patAODPFTauIsolation"),
-    associations = patAODPFTauIsolationLabels
-)
+patPFTauIsolation = cms.Sequence( tauIsoDepositPFCandidates
+                                 * tauIsoDepositPFChargedHadrons
+                                 * tauIsoDepositPFNeutralHadrons
+                                 * tauIsoDepositPFGammas
+                                 * pfTauIsolationValueMap )
 
-patPFTauIsolation = cms.Sequence( tauIsoDepositPFCandidates #* tauIsoFromDepsPFCandidates
-                                * tauIsoDepositPFChargedHadrons #* tauIsoFromDepsPFChargedHadrons
-                                * tauIsoDepositPFNeutralHadrons #* tauIsoFromDepsPFNeutralHadrons
-                                * tauIsoDepositPFGammas #* tauIsoFromDepsPFGammas
-                                )
 
