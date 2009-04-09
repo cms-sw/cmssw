7654233eeb26ba47d62e979f08d8c7dc436ba092
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-04-09 02:15:24 +0000

    add sequence for fastsim

--- HLTriggerOffline/special/python/hltAlCaVal_cff.py
+++ HLTriggerOffline/special/python/hltAlCaVal_cff.py
@@ -1,9 +1,20 @@
 from HLTriggerOffline.special.hltHITval_cfi import *
 from HLTriggerOffline.special.MonitorAlCaEcalPi0_cfi import *
 from HLTriggerOffline.special.EcalPhiSymHLTVal_cfi import *
+
 HLTAlCaVal = cms.Sequence(
     EcalPi0Mon
     +EcalPhiSymMon
     +hltHITval
     )
+
+import HLTriggerOffline.special.EcalPhiSymHLTVal_cfi
+hltHITvalFastSim = HLTriggerOffline.special.hltHITval_cfi.hltHITval.clone()
+hltHITvalFastSim.gtDigiLabel = cms.InputTag("gtDigis")
+HLTAlCaVal_FastSim = cms.Sequence(
+    EcalPi0Mon
+    +EcalPhiSymMon
+    +hltHITvalFastSim
+    )
+
 
