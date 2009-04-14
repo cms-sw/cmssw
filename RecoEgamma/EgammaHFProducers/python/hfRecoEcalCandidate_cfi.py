49d8e31a38f9f404018a07edc1fcca551c3bd28f
Kevin.Klapoetke Klapoetke <sha1-00ae6b1fd57e2673af62ea5af6540d64f8b247c9@cern.ch>
2009-04-14 15:35:07 +0000

    Updated the default EM ID values

--- RecoEgamma/EgammaHFProducers/python/hfRecoEcalCandidate_cfi.py
+++ RecoEgamma/EgammaHFProducers/python/hfRecoEcalCandidate_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 # HF RecoEcalCandidate Producer
 hfRecoEcalCandidate = cms.EDProducer("HFRecoEcalCandidateProducer",
-    e9e25Cut = cms.double(0.88),
+    e9e25Cut = cms.double(0.94),
     hfclusters = cms.untracked.InputTag("hfEMClusters"),
-    intercept2DCut = cms.double(0.16),
+    intercept2DCut = cms.double(0.5),
     Correct = cms.bool(True)
 )
 
