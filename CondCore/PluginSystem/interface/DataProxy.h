145a03f33329112618c952254928a2932481c8a0
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-04-14 16:43:29 +0000

    does not compile yet

--- CondCore/PluginSystem/interface/DataProxy.h
+++ CondCore/PluginSystem/interface/DataProxy.h
@@ -17,14 +17,10 @@
 template< class RecordT, class DataT >
   class DataProxy : public edm::eventsetup::DataProxyTemplate<RecordT, DataT>{
   public:
-  typedef cond::DataWrapper<DataT> DataWrapper;
-  /*  DataProxy( pool::IDataSvc* svc, std::map<std::string,std::string>::iterator& pProxyToToken ): m_svc(svc), m_pProxyToToken(pProxyToToken) { 
-  //NOTE: We do this so that the type 'DataT' will get registered
-  // when the plugin is dynamically loaded
-  edm::eventsetup::DataKey::makeTypeTag<DataT>(); 
-  }
+  typedef  boost::shared_ptr<cond::PayloadProxy<DataT> > DataP;
+
   */
-  DataProxy( cond::Connection* connection, std::map<std::string,std::string>::iterator& pDatumToToken ): m_connection(connection), m_pDatumToToken(pDatumToToken) { 
+  DataProxy(DatatP pdata) : m_data(pdata) { 
     //NOTE: We do this so that the type 'DataT' will get registered
     // when the plugin is dynamically loaded
     //std::cout<<"DataProxy constructor"<<std::endl;
@@ -41,34 +37,8 @@ template< class RecordT, class DataT >
   
   protected:
   virtual const DataT* make(const RecordT&, const edm::eventsetup::DataKey&) {
-    DataT const * result=0;
-    //std::cout<<"DataT make "<<std::endl;
-    cond::PoolTransaction& pooldb=m_connection->poolTransaction();
-    pooldb.start(true);      
-    // FIXME (clean this mess)
-    try {
-      pool::Ref<DataWrapper> mydata(&(pooldb.poolDataSvc()),m_pDatumToToken->second);
-      if (mydata) {
-	try{
-	  result = &mydata->data();
-	}
-	catch( const pool::Exception& e) {
-	throw cond::Exception("DataProxy::make: null result");
-	}
-	m_data.copyShallow(mydata);
-	pooldb.commit();
-	return result;
-      }
-    } catch(const pool::Exception&){}
-
-    // compatibility mode....
-    pool::Ref<DataT> myodata(&(pooldb.poolDataSvc()),m_pDatumToToken->second);
-    result = myodata.ptr();
-    if (!result) throw cond::Exception("DataProxy::make: null result");
-    m_OldData.copyShallow(myodata);
-
-    pooldb.commit();
-    return result;
+    m_data->make();
+    return (*m_data)();
   }
   virtual void invalidateCache() {
     m_data.clear();
@@ -81,8 +51,38 @@ template< class RecordT, class DataT >
   cond::Connection* m_connection;
   std::map<std::string,std::string>::iterator m_pDatumToToken;
 
-  pool::Ref<DataWrapper> m_data;
-  // Backward compatibility
-  pool::Ref<DataT> m_OldData;
+  DatatP m_data;
+
 };
+
+class DataProxyWrapperBase {
+public:
+  typedef boost::shared_ptr<cond::BasePayloadProxy> ProxyP;
+  typedef boost::shared_ptr<edm::eventsetup::DataProxy> edmProxyP;
+
+  virtual ProxyP proxy() const=0;
+  virtual edmProxyP emdProxy() const=0;
+
+};
+
+template< class RecordT, class DataT >
+class DataProxyWrapper : public  DataProxyWrapperBase {
+public:  
+  typedef  boost::shared_ptr<cond::PayloadProxy<DataT> > DataP;
+
+  
+  DataProxyWrapper(cond::Connection& conn,
+		   const std::string & token);
+
+  virtual ProxyP proxy() const { return m_proxy;}
+  virtual edmProxyP emdProxy() const { return m_edmProxy;}
+ 
+private:
+
+  DataP m_proxy;
+  edmProxyP m_edmProxy;
+
+};
+
+
 #endif /* CONDCORE_PLUGINSYSTEM_DATAPROXY_H */
