462e5a1f43154f15fe890f46f2bbce488b321010
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-04-15 12:40:37 +0000

    ready to compile

--- CondCore/ESSources/plugins/SealModule.cc
+++ CondCore/ESSources/plugins/SealModule.cc
@@ -1,6 +1,6 @@
 #include "FWCore/Framework/interface/SourceFactory.h"
-#include "CondCore/ESSources/interface/oldPoolDBESSource.h"
+#include "CondCore/ESSources/interface/PoolDBESSource.h"
 //define this as a plug-in
-using namespace oldcond;
+using namespace cond;
 //DEFINE_SEAL_MODULE();
 DEFINE_FWK_EVENTSETUP_SOURCE(PoolDBESSource);
