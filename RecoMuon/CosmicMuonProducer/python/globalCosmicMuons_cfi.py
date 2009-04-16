2cb80b9d90c5c82e56b86e820d820865ebe784a2
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2009-04-16 21:24:47 +0000

    tmp fix on 22x

--- RecoMuon/CosmicMuonProducer/python/globalCosmicMuons_cfi.py
+++ RecoMuon/CosmicMuonProducer/python/globalCosmicMuons_cfi.py
@@ -8,14 +8,25 @@ globalCosmicMuons = cms.EDProducer("GlobalCosmicMuonProducer",
     MuonTrackLoaderForCosmic,
     MuonServiceProxy,
     TrajectoryBuilderParameters = cms.PSet(
-        GlobalMuonTrackMatcher,
+        GlobalMuonTrackMatcher = cms.PSet(
+          MinP = cms.double(2.5),
+          MinPt = cms.double(1.0),
+          Pt_threshold= cms.double(35.0),
+          Eta_threshold= cms.double(1.0),
+          Chi2Cut= cms.double(50.0),
+          LocChi2Cut= cms.double(.008),
+          DeltaDCut= cms.double(10.0),
+          DeltaRCut= cms.double(.2),
+          Propagator = cms.string('SteppingHelixPropagatorAny')
+        ),
         MuonRecHitBuilder = cms.string('MuonRecHitBuilder'),
         TrackerRecHitBuilder = cms.string('WithTrackAngle'),
-        TkTrackCollectionLabel = cms.string('generalTracks'),
+        TkTrackCollectionLabel = cms.InputTag("generalTracks"),
         Propagator = cms.string('SteppingHelixPropagatorAny'),
         SmootherParameters = cms.PSet(
             PropagatorAlong = cms.string('SmartPropagatorAny'),
-            PropagatorOpposite = cms.string('SmartPropagatorAnyOpposite')
+            PropagatorOpposite = cms.string('SmartPropagatorAnyOpposite'),
+            RescalingFactor = cms.double(5.0)
         )
     ),
     MuonCollectionLabel = cms.InputTag("cosmicMuons")
