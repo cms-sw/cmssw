565565b6cc2b83e7a6cbcc27f22f792d1cae9dfe
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-04-16 14:33:37 +0000

    remove 2E30 from and add extra syntax into python/ConfigBuilder.py

--- HLTrigger/Configuration/python/ConfigBuilder.py
+++ HLTrigger/Configuration/python/ConfigBuilder.py
@@ -1,11 +1,6 @@
 # mapping with the available HLT tables supported by cmsDriver.py
 __triggerTable = {
 
-    # old HLT trigger table (pre-reviews) for the 2008 MC productions
-    '2E30': ( 
-        'HLTrigger/Configuration/HLT_2E30_cff', 
-    ),
-
     # HLT trigger table for the 2009 STARTUP 8E29 menu
     '8E29': ( 
         'HLTrigger/Configuration/HLT_8E29_cff', 
@@ -17,29 +12,44 @@ __triggerTable = {
     )
 }
 
+# default trigger, used if none is specified
+__default = '8E29'
+
+
+def getDefaultTrigger():
+    """
+    Retrieves the default trigger name
+    """
+    return __default
+    
+
+def getHelp():
+    """
+    Retrieves the list of available triggers
+    """
+    return __triggerTable.iterkeys()
+  
 
 def getConfigsForScenario(sequence = None):
     """
     Retrieves the list of files needed to run a given trigger menu.
     If no trigger or an invalid trigger is given, use the default one. 
     """
-    # default trigger, used if none is given
-    default = '8E29'
 
     if not sequence:
         # no trigger was specified, use the default one
-        trigger = default
+        trigger = __default
     else:
         # check if the specified trigger is valid
         trigger = sequence
         if trigger not in __triggerTable:
             print 'An unsupported trigger has been requested: %s' % sequence
-            print 'The default one will be used instead: %s' % default
+            print 'The default one will be used instead: %s' % __default
             print 'The supported triggers are:'
             for key in __triggerTable.iterkeys():
                 print '\t%s' % key
             print
-            trigger = default
+            trigger = __default
 
     return __triggerTable[trigger]
 
