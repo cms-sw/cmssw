b24af0302c4ff8099c082917d0b1d52063aa8155
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2009-04-17 14:34:04 +0000

    added fast sim sequence

--- HLTriggerOffline/Muon/python/HLTMuonVal_cff.py
+++ HLTriggerOffline/Muon/python/HLTMuonVal_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoMuon.muonValidationHLT_cff import *
 from HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi import *
+from Validation.RecoMuon.muonValidationHLTFastSim_cff import *
 
 HLTMuonVal = cms.Sequence(
     recoMuonValidationHLT_seq + 
@@ -9,6 +10,7 @@ HLTMuonVal = cms.Sequence(
     )
 
 HLTMuonVal_FastSim = cms.Sequence(
+    recoMuonValidationHLTFastSim_seq +
     muonTriggerRateTimeAnalyzer
     )
 
