0aa34648675660a1059646d71a9cadf2a1744c40
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2009-04-20 03:51:36 +0000

    more memory leaks (perhaps the real bug is in the data format class that does not take ownership)

--- FastSimulation/TrackingRecHitProducer/src/GSRecHitMatcher.cc
+++ FastSimulation/TrackingRecHitProducer/src/GSRecHitMatcher.cc
@@ -216,13 +216,20 @@ SiTrackerGSMatchedRecHit2D * GSRecHitMatcher::projectOnly( const SiTrackerGSRecH
   SiTrackerGSRecHit2D *otherRH = new SiTrackerGSRecHit2D(LocalPoint(-10000,-10000,-10000), LocalError(0,0,0),DetId(000000000), 0,0,0,monoRH->cluster(),0,0);
   if ((isMono && isStereo)||(!isMono&&!isStereo)) throw cms::Exception("GSRecHitMatcher") << "Something wrong with DetIds.";
   else if (isMono) {
-    return new SiTrackerGSMatchedRecHit2D(projectedHitPos, rotatedError, gluedDet->geographicalId(), 
-					  monoRH->simhitId(),  monoRH->simtrackId(), monoRH->eeId(), monoRH->cluster(),
-					  monoRH->simMultX(), monoRH->simMultY(), false, adjustedRH, otherRH);
+    SiTrackerGSMatchedRecHit2D *rV= new SiTrackerGSMatchedRecHit2D(projectedHitPos, rotatedError, gluedDet->geographicalId(), 
+								   monoRH->simhitId(),  monoRH->simtrackId(), monoRH->eeId(), monoRH->cluster(),
+								   monoRH->simMultX(), monoRH->simMultY(), false, adjustedRH, otherRH);
+    delete adjustedRH;
+    delete otherRH;
+    return rV;
   }
   
-  else return new SiTrackerGSMatchedRecHit2D(projectedHitPos, rotatedError, gluedDet->geographicalId(), 
-					     monoRH->simhitId(),  monoRH->simtrackId(), monoRH->eeId(), monoRH->cluster(),
-					     monoRH->simMultX(), monoRH->simMultY(), false, otherRH, adjustedRH);
-  
+  else{
+    SiTrackerGSMatchedRecHit2D *rV=new SiTrackerGSMatchedRecHit2D(projectedHitPos, rotatedError, gluedDet->geographicalId(), 
+								 monoRH->simhitId(),  monoRH->simtrackId(), monoRH->eeId(), monoRH->cluster(),
+								 monoRH->simMultX(), monoRH->simMultY(), false, otherRH, adjustedRH);
+    delete adjustedRH;
+    delete otherRH;
+    return rV;
+  }
 }
