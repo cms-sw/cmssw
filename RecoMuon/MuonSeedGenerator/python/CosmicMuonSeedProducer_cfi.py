0f26ba864a0b6a8788ffe08322b98df240bace61
cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch>
2009-04-20 10:20:45 +0000

    untracked to tracked

--- RecoMuon/MuonSeedGenerator/python/CosmicMuonSeedProducer_cfi.py
+++ RecoMuon/MuonSeedGenerator/python/CosmicMuonSeedProducer_cfi.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 CosmicMuonSeed = cms.EDFilter("CosmicMuonSeedGenerator",
     MaxSeeds = cms.int32(10),
-    CSCRecSegmentLabel = cms.untracked.InputTag("cscSegments"),
-    EnableDTMeasurement = cms.untracked.bool(True),
+    CSCRecSegmentLabel = cms.InputTag("cscSegments"),
+    EnableDTMeasurement = cms.bool(True),
     MaxCSCChi2 = cms.double(300.0),
     MaxDTChi2 = cms.double(300.0),
-    DTRecSegmentLabel = cms.untracked.InputTag("dt4DSegments"),
-    EnableCSCMeasurement = cms.untracked.bool(True)
+    DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
+    EnableCSCMeasurement = cms.bool(True)
 )
 
 
