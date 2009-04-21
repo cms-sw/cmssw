2aa40ce8cefce492827b8d9456968994e6bd63c5
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-04-21 19:13:58 +0000

    fix non-functioning domcount test

--- DetectorDescription/RegressionTest/scripts/domcount.sh
+++ DetectorDescription/RegressionTest/scripts/domcount.sh
@@ -1,6 +1,5 @@
 #! /bin/csh -f
 eval `scramv1 runtime -csh`
-grep ".xml" $CMSSW_RELEASE_BASE/src/Geometry/CMSCommonData/data/cmsIdealGeometryXML.cfi | replace '"' ' ' | replace "," " " | sed '{s/ //g}' | sed '{s/\t//g\
-}' | grep -v "#" >! /tmp/tmpcmsswdddxmlfileslist
+grep ".xml" $CMSSW_RELEASE_BASE/src/Geometry/CMSCommonData/python/cmsIdealGeometryXML_cfi.py | sed "{s/'//g}" | sed '{s/,//g}' | sed '{s/ //g}' | sed '{s/\t//g}' | sed '{s/geomXMLFiles=cms.vstring(//g}' | sed '{s/)//g}' | grep -v "#" >! /tmp/tmpcmsswdddxmlfileslist
 cd $CMSSW_RELEASE_BASE/src
 DOMCount -v=always -n -s -l /tmp/tmpcmsswdddxmlfileslist
