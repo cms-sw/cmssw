f2b86999ebf84340c97f9269f7dee98072d0ec4d
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2009-04-21 12:20:31 +0000

    Using sharesInput instead of the comparator by position (which is fishy) https://hypernews.cern.ch/HyperNews/CMS/get/recoTracking/639/1/1.html

--- TrackingTools/TrajectoryCleaning/src/TrajectoryCleanerBySharedHits.cc
+++ TrackingTools/TrajectoryCleaning/src/TrajectoryCleanerBySharedHits.cc
@@ -3,51 +3,69 @@
 #include "TrackingTools/TransientTrackingRecHit/interface/RecHitComparatorByPosition.h"
 #include <map>
 #include <vector>
+#include <boost/unordered_map.hpp>
+
+// Define when two rechits are equals
+struct EqualsBySharesInput { 
+    bool operator()(const TransientTrackingRecHit *h1, const TransientTrackingRecHit *h2) const {
+        return (h1 == h2) || ((h1->geographicalId() == h2->geographicalId()) && (h1->hit()->sharesInput(h2->hit(), TrackingRecHit::all)));
+    }
+};
+// Define a hash, i.e. a number that must be equal if hits are equal, and should be different if they're not
+struct HashByDetId : std::unary_function<const TransientTrackingRecHit *, std::size_t> {
+    std::size_t operator()(const TransientTrackingRecHit *hit) const { 
+        boost::hash<uint32_t> hasher; 
+        return hasher(hit->geographicalId().rawId());
+    }
+};
 
 using namespace std;
 
 void TrajectoryCleanerBySharedHits::clean( TrajectoryPointerContainer & tc) const
 {
   typedef vector<Trajectory*>::iterator TI;
-  typedef map<const TransientTrackingRecHit*,vector<TI>,RecHitComparatorByPosition> RecHitMap; 
-  typedef map<Trajectory*,int,less<Trajectory*> > TrajMap;  // for each Trajectory it stores the number of shared hits
-  RecHitMap theRecHitMap;
+  typedef vector<TI> TIs;
+  typedef boost::unordered_map<const TransientTrackingRecHit*, TIs, HashByDetId, EqualsBySharesInput> RecHitMap;
+  //typedef map<Trajectory*,int,less<Trajectory*> > TrajMap;  // for each Trajectory it stores the number of shared hits
+  typedef boost::unordered_map<Trajectory*, int> TrajMap;  // for each Trajectory it stores the number of shared hits
+  RecHitMap theRecHitMap(20*tc.size()); 
 
   // start filling theRecHit
   for (TrajectoryCleaner::TrajectoryPointerIterator
-	 it = tc.begin(); it != tc.end(); it++) {
-    Trajectory::DataContainer pd = (*it)->data();
-    for (Trajectory::DataContainer::iterator im = pd.begin();
+	 it = tc.begin(); it != tc.end(); ++it) {
+    const Trajectory::DataContainer & pd = (*it)->data();
+    for (Trajectory::DataContainer::const_iterator im = pd.begin();
     	 im != pd.end(); im++) {
       //RC const TransientTrackingRecHit* theRecHit = ((*im).recHit());
       const TransientTrackingRecHit* theRecHit = &(*(*im).recHit());
-      if (theRecHit->isValid())
+      if (theRecHit->isValid()) {
         theRecHitMap[theRecHit].push_back(it);
+      }
     }
   }
-  // end filling theRecHit
 
   // for each trajectory fill theTrajMap
 
+  TrajMap theTrajMap(20);
   for (TrajectoryCleaner::TrajectoryPointerIterator
-	 itt = tc.begin(); itt != tc.end(); itt++) {
+	 itt = tc.begin(); itt != tc.end(); ++itt) {
     if((*itt)->isValid()){  
-      TrajMap theTrajMap;
-      Trajectory::DataContainer pd = (*itt)->data();
-      for (Trajectory::DataContainer::iterator im = pd.begin();
-	   im != pd.end(); im++) {
+      theTrajMap.clear();
+      const Trajectory::DataContainer & pd = (*itt)->data();
+      for (Trajectory::DataContainer::const_iterator im = pd.begin();
+	   im != pd.end(); ++im) {
 	//RC const TransientTrackingRecHit* theRecHit = ((*im).recHit());
 	const TransientTrackingRecHit* theRecHit = &(*(*im).recHit());
         if (theRecHit->isValid()) {
-	  const vector<TI>& hitTrajectories( theRecHitMap[theRecHit]);
-	  for (vector<TI>::const_iterator ivec=hitTrajectories.begin(); 
-	       ivec!=hitTrajectories.end(); ivec++) {
-	    if (*ivec != itt){
-	      if ((**ivec)->isValid()){
-		theTrajMap[**ivec]++;
-	      }
-	    }
-	  }
+          const TIs & hitTrajectories = theRecHitMap[theRecHit];
+	  for (TIs::const_iterator ivec=hitTrajectories.begin(); 
+	     ivec!=hitTrajectories.end(); ivec++) {
+              if (*ivec != itt){
+                if ((**ivec)->isValid()){
+                    theTrajMap[**ivec]++;
+                }
+              }
+          }
 	}
       }
       //end filling theTrajMap
@@ -55,7 +73,7 @@ void TrajectoryCleanerBySharedHits::clean( TrajectoryPointerContainer & tc) cons
       // check for duplicated tracks
       if(!theTrajMap.empty() > 0){
 	for(TrajMap::iterator imapp = theTrajMap.begin(); 
-	    imapp != theTrajMap.end(); imapp++){
+	    imapp != theTrajMap.end(); ++imapp){
 	  //          int nhit1 = (*itt).data().size();
 	  //          int nhit2 = (*imapp).first->data().size();
           int nhit1 = (*itt)->foundHits();
@@ -76,6 +94,5 @@ void TrajectoryCleanerBySharedHits::clean( TrajectoryPointerContainer & tc) cons
       } 
     }
   }
-
 }
 
