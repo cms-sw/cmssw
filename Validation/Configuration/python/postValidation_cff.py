9e2f02c02ad7070252f882ac326206762277f335
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-04-21 08:11:57 +0000

    add the muon isolation

--- Validation/Configuration/python/postValidation_cff.py
+++ Validation/Configuration/python/postValidation_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoMuon.PostProcessor_cff import *
 from Validation.RecoTrack.PostProcessorTracker_cfi import *
 
-postValidation = cms.Sequence(recoMuonPostProcessors+postProcessorTrack)
+postValidation = cms.Sequence(recoMuonPostProcessorsWithIsolation+postProcessorTrack)
 
-postValidation_pu = cms.Sequence(postProcessorTrack)
+postValidation_pu = cms.Sequence(recoMuonPostProcessors+postProcessorTrack)
