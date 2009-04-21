0ce5c7292a65abeb88a5664623b40a9855a98f2f
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2009-04-21 15:11:46 +0000

    new PFTau naming convention

--- PhysicsTools/PFCandProducer/python/pfTaus_cff.py
+++ PhysicsTools/PFCandProducer/python/pfTaus_cff.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 from RecoTauTag.Configuration.RecoPFTauTag_cff import *
 from RecoTauTag.TauTagTools.PFTauSelector_cfi  import pfTauSelector
 
-pfRecoTauProducerHighEfficiency.DataType = cms.string('AOD')
+shrinkingConePFTauProducer.DataType = cms.string('AOD')
 ic5PFJetTracksAssociatorAtVertex.jets = 'pfJets'
 
 allLayer0Taus = pfTauSelector.clone()
-allLayer0Taus.src = cms.InputTag("pfRecoTauProducerHighEfficiency")
+allLayer0Taus.src = cms.InputTag("shrinkingConePFTauProducer")
 allLayer0Taus.discriminators = cms.VPSet(
-      cms.PSet( discriminator=cms.InputTag("pfRecoTauDiscriminationByIsolationHighEfficiency"),selectionCut=cms.double(0.5))
+      cms.PSet( discriminator=cms.InputTag("shrinkingConePFTauDiscriminationByIsolation"),selectionCut=cms.double(0.5))
    )
 
 pfRecoTauTagInfoProducer.PFCandidateProducer = 'pfNoMuonsNoPileUp:PFCandidates'
@@ -18,8 +18,8 @@ pfRecoTauTagInfoProducer.PFCandidateProducer = 'pfNoMuonsNoPileUp:PFCandidates'
 pfTauSequence = cms.Sequence(
     ic5PFJetTracksAssociatorAtVertex + 
     pfRecoTauTagInfoProducer + 
-    pfRecoTauProducerHighEfficiency + 
-    pfRecoTauDiscriminationByIsolationHighEfficiency + 
+    shrinkingConePFTauProducer + 
+    shrinkingConePFTauDiscriminationByIsolation + 
     allLayer0Taus
     )
 
