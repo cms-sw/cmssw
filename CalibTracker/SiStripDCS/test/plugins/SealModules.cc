5ae9c38f72c7edf3b3fc24c699d28c9b5d927617
Joanne Cole <sha1-fa3e5896900e286b105d4a33c21e2ef8d2c23547@cern.ch>
2009-04-22 12:29:48 +0000

    add DP id-name mapping plugin

--- CalibTracker/SiStripDCS/test/plugins/SealModules.cc
+++ CalibTracker/SiStripDCS/test/plugins/SealModules.cc
@@ -4,3 +4,5 @@ DEFINE_SEAL_MODULE();
 #include "CalibTracker/SiStripDCS/test/plugins/testbuilding.h"
 DEFINE_ANOTHER_FWK_MODULE(testbuilding);
 
+#include "CalibTracker/SiStripDCS/test/plugins/dpLocationMap.h"
+DEFINE_ANOTHER_FWK_MODULE(dpLocationMap);
