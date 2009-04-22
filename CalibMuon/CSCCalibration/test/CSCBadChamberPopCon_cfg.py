bba9f4cfdd3fe7e5d009fb683bec5e41da5d7f6c
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2009-04-22 07:55:59 +0000

    changed connect and DB parameters

--- CalibMuon/CSCCalibration/test/CSCBadChamberPopCon_cfg.py
+++ CalibMuon/CSCCalibration/test/CSCBadChamberPopCon_cfg.py
@@ -12,7 +12,9 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("ProcessOne")
 #PopCon config
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
-
+process.CondDBCommon.connect = 'sqlite_file:BadChambers_5ME42.db'
+process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
+#process.CondDBCommon.DBParameters.messageLevel = cms.untracked.int32(3)
 process.MessageLogger = cms.Service("MessageLogger",
     cout = cms.untracked.PSet(
         default = cms.untracked.PSet(
@@ -34,7 +36,7 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     logconnect = cms.untracked.string('sqlite_file:badChamberslog.db'),
     toPut = cms.VPSet(cms.PSet(
         record = cms.string('CSCBadChambersRcd'),
-        tag = cms.string('CSCBadChambers_empty_mc')
+        tag = cms.string('CSCBadChambers_none_FiveLiveME42')
     ))
 )
 
@@ -48,7 +50,6 @@ process.WriteBadChambersWithPopCon = cms.EDAnalyzer("CSCBadChambersPopConAnalyze
 )
 
 process.p = cms.Path(process.WriteBadChambersWithPopCon)
-process.CondDBCommon.connect = 'sqlite_file:BadChambers.db'
-process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
+
 
 
