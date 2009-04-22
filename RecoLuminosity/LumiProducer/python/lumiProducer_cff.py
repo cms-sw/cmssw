9620cd8ca36b9a5a150503ae4748485092ba0f55
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-04-22 17:41:53 +0000

    added

---
+++ RecoLuminosity/LumiProducer/python/lumiProducer_cff.py
@@ -0,0 +1,4 @@
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
+from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
