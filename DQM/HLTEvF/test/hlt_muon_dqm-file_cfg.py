e8d9539a4de52f45e20b0b71fee01044dcdb5ee9
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-04-22 22:18:59 +0000

    add tags for updated source

--- DQM/HLTEvF/test/hlt_muon_dqm-file_cfg.py
+++ DQM/HLTEvF/test/hlt_muon_dqm-file_cfg.py
@@ -9,7 +9,7 @@ process.load("DQM.HLTEvF.HLTMonitorClient_cff")
 process.load("Configuration.StandardSequences.GeometryPilot2_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
-process.GlobalTag.globaltag = 'IDEAL_30X::All'
+process.GlobalTag.globaltag = 'CRAFT_V14P::All'
 process.prefer("GlobalTag")
 
 process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" )
@@ -17,14 +17,23 @@ process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeomet
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
+    #input = cms.untracked.int32(-1)
 )
 
 process.source = cms.Source("PoolSource",
     debugVerbosity = cms.untracked.uint32(1),
     debugVebosity = cms.untracked.bool(True),
     fileNames = cms.untracked.vstring(
-    'file:/tmp/hdyoo/28F5E252-180A-DE11-87E2-000423D94A20.root'
+    '/store/data/Commissioning09/Monitor/RAW/v1/000/082/380/021C1449-4D2F-DE11-95CB-0019B9F72CE5.root'
+    #'file:/tmp/wteo/BA6DA407-A985-DD11-A0D8-000423D9A2AE.root',
+    #'file:/tmp/wteo/52BAB7DF-0487-DD11-95A3-000423D9989E.root'
+    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_doubleMuonPath_v1_0.root',
+    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_doubleMuonPath_v1_1.root'
+    #'file:/tmp/hdyoo/sample/data/CRUZET3/CRUZET3.root'
+    #'file:/tmp/hdyoo/sample/data/CRUZET4_V6P/CRUZET4_V6P_multiCosmicMuon_v1.root'
+    #'file:/tmp/hdyoo/sample/MC/CMSSW_2_1_9/RelValSingleMuPt100.root'
+    #'file:/tmp/hdyoo/sample/data/PreCRUZET4Test1/PreCRUZET4Test1.root'
     )
 )
 
