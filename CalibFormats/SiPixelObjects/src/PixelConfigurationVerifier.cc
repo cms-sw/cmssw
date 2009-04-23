0c71cf98e93481c6ec5ef0e09ee1912a0b7c03df
Joshua Thompson <sha1-53298ae1bcd80c8ae6466a8448c81e7a5b3563a0@cern.ch>
2009-04-23 10:36:38 +0000

    disable fed channel if noAnalogSignal is set

--- CalibFormats/SiPixelObjects/src/PixelConfigurationVerifier.cc
+++ CalibFormats/SiPixelObjects/src/PixelConfigurationVerifier.cc
@@ -5,6 +5,7 @@
 
 #include "CalibFormats/SiPixelObjects/interface/PixelConfigurationVerifier.h"
 #include "CalibFormats/SiPixelObjects/interface/PixelChannel.h"
+#include "CalibFormats/SiPixelObjects/interface/PixelROCStatus.h"
 #include <set>
 #include <assert.h>
 
@@ -42,30 +43,58 @@ void PixelConfigurationVerifier::checkChannelEnable(PixelFEDCard *theFEDCard,
     }
   }
 
+  map<PixelROCName, PixelROCStatus> roclistcopy = theDetConfig->getROCsList();
   //Now check the channels
 
-  for(unsigned int iChannel=1;iChannel<37;iChannel++){
-    bool used=theFEDCard->useChannel(iChannel);
-    //if (!used) cout << "Channel="<<iChannel<<" is not used"<<endl;
+  for(unsigned int jChannel=1;jChannel<37;jChannel++){
+    bool used=theFEDCard->useChannel(jChannel);
+    //    if (!used) cout << "Channel="<<jChannel<<" is not used"<<endl;
     if (used) { 
-      //      cout << "Channel="<<iChannel<<" is used"<<endl;
+      //            cout << "Channel="<<jChannel<<" is used"<<endl;
       //check that nROCs is the same from theNameTranslation and theFEDCard
-      if (nrocs[iChannel] != theFEDCard->NRocs[iChannel-1]) {
-	cout<<"[PixelConfigurationVerifier] Warning in FED#"<<fedid<<", channel#"<<iChannel<<": number of ROCs mismatch: theNameTranslation="<<nrocs[iChannel]<<"; theFEDCard="<<theFEDCard->NRocs[iChannel-1]<<endl;
-	//	assert(nrocs[iChannel] == theFEDCard->NRocs[iChannel-1]);
+      if (int(nrocs[jChannel]) != theFEDCard->NRocs[jChannel-1]) {
+	cout<<"[PixelConfigurationVerifier] Warning in FED#"<<fedid<<", channel#"<<jChannel
+	    <<": number of ROCs mismatch: theNameTranslation="<<nrocs[jChannel]<<"; theFEDCard="<<theFEDCard->NRocs[jChannel-1]<<endl;
       }
     }
-    if (used!=usedChannel[iChannel]) {
-      cout << __LINE__ << "]\t" << mthn << "*******************************************************"     << endl;
-      cout << __LINE__ << "]\t" << mthn << "WARNING for fedid=" << fedid << " and channel=" << iChannel  <<
-                                           " found that fedcard has channel as "                         << endl;
-      if (used)  cout << __LINE__ << "]\t" << mthn << "used while configuration not using this channel"
-		                                                                                         << endl;
-      if (!used) cout << __LINE__ << "]\t" << mthn << "not used while configuration uses this channel"
-		                                                                                     	 << endl;
-      cout << __LINE__ << "]\t" << mthn << "The fedcard will be modifed to agree with configuration" 	 << endl;
-      cout << __LINE__ << "]\t" << mthn << "*******************************************************" 	 << endl;
-      theFEDCard->setChannel(iChannel,usedChannel[iChannel]);
+     
+    //only do these checks if the channel exists
+    if (theNameTranslation->FEDChannelExist(fedid,jChannel)) {
+      
+      //make sure that all rocs on a channel have the same noAnalogSignal status
+      vector<PixelROCName> rocsOnThisChannel= theNameTranslation->getROCsFromFEDChannel(fedid,jChannel);
+      bool onehasNAS=false,onedoesnothaveNAS=false;
+      vector<PixelROCName>::const_iterator jROC=rocsOnThisChannel.begin();
+      for (; jROC!= rocsOnThisChannel.end(); ++jROC) {
+	PixelROCStatus thisROCstatus = roclistcopy[*jROC];
+	if ( thisROCstatus.get(PixelROCStatus::noAnalogSignal) ) onehasNAS=true;
+	else onedoesnothaveNAS=true;
+      }
+      if (onehasNAS && onedoesnothaveNAS) {
+	cout<<"[PixelConfigurationVerifier] Error in FED#"<<fedid<<", channel#"<<jChannel
+	    <<": not all ROCs have the same noAnalogSignal state."<<endl;
+	assert(0);
+      }
+      
+      //now if onehasNAS is true, then all must be noAnalogSignal --> turn off this FED channel!
+      if (onehasNAS) {
+	cout<<"[PixelConfigurationVerifier] FEDid="<<fedid<<", channel="<<jChannel
+	    <<": Channel disabled because ROCs are set to noAnalogSignal"<<endl;
+	theFEDCard->setChannel(jChannel,false); //false should disable the channel
+      }
+      
+      if (!onehasNAS && (used!=usedChannel[jChannel])) {
+	cout << __LINE__ << "]\t" << mthn << "*******************************************************"     << endl;
+	cout << __LINE__ << "]\t" << mthn << "WARNING for fedid=" << fedid << " and channel=" << jChannel  <<
+	  " found that fedcard has channel as "                         << endl;
+	if (used)  cout << __LINE__ << "]\t" << mthn << "used while configuration not using this channel"
+			<< endl;
+	if (!used) cout << __LINE__ << "]\t" << mthn << "not used while configuration uses this channel"
+			<< endl;
+	cout << __LINE__ << "]\t" << mthn << "The fedcard will be modifed to agree with configuration" 	 << endl;
+	cout << __LINE__ << "]\t" << mthn << "*******************************************************" 	 << endl;
+	theFEDCard->setChannel(jChannel,usedChannel[jChannel]);
+      }
     }
   }
 
