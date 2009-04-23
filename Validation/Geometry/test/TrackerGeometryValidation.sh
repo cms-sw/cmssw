a1b5ea65fa4c25a854b6c90d955e81782b5450ed
Gilles De Lentdecker <sha1-80604295d5eeb45c20305bc86b83b6110467a14b@cern.ch>
2009-04-23 08:31:31 +0000

    update script because some paths have changed

--- Validation/Geometry/test/TrackerGeometryValidation.sh
+++ Validation/Geometry/test/TrackerGeometryValidation.sh
@@ -17,8 +17,13 @@ eval `scramv1 runtime -sh`
 
 # OLD: from CMSSW_1_6_* no more updated:
 #export referenceDir=/afs/cern.ch/cms/data/CMSSW/Validation/Geometry/reference/Tracker
+
 # NEW: the reference files can be chosen
-export referenceDir=/afs/cern.ch/cms/performance/tracker/activities/validation/ReferenceFiles/$Version/Geometry.tgz
+#export referenceDir=/afs/cern.ch/cms/performance/tracker/activities/validation/ReferenceFiles/$Version/Geometry.tgz
+
+# from 31X, the reference files are on Castor
+export referenceDir=/castor/cern.ch/user/d/dldecker/ReferenceFiles/$Version/Geometry
+
 echo "Reference area:" $referenceDir | tee -a $reportFile
 
 # Create Images/ directory if it does not exist
@@ -39,24 +44,24 @@ fi
 
 # Download the reference files and rename them to 'old'
 echo "Download the reference 'old' files..." | tee -a $reportFile
-echo "Untar the 'old' files..." | tee -a $reportFile
-tar -xvzf $referenceDir
-mv Geometry/matbdg_TkStrct.root       matbdg_TkStrct_old.root
-mv Geometry/matbdg_PixBar.root        matbdg_PixBar_old.root 
-mv Geometry/matbdg_PixFwdPlus.root    matbdg_PixFwdPlus_old.root 
-mv Geometry/matbdg_PixFwdMinus.root   matbdg_PixFwdMinus_old.root 
-mv Geometry/matbdg_TIB.root           matbdg_TIB_old.root 
-mv Geometry/matbdg_TIDF.root          matbdg_TIDF_old.root 
-mv Geometry/matbdg_TIDB.root          matbdg_TIDB_old.root 
-mv Geometry/matbdg_InnerServices.root matbdg_InnerServices_old.root 
-mv Geometry/matbdg_TOB.root           matbdg_TOB_old.root 
-mv Geometry/matbdg_TEC.root           matbdg_TEC_old.root 
-mv Geometry/matbdg_Tracker.root       matbdg_Tracker_old.root 
-mv Geometry/matbdg_BeamPipe.root      matbdg_BeamPipe_old.root 
+#echo "Untar the 'old' files..." | tee -a $reportFile
+#tar -xvzf $referenceDir
+rfcp $referenceDir/matbdg_TkStrct.root       matbdg_TkStrct_old.root
+rfcp $referenceDir/matbdg_PixBar.root        matbdg_PixBar_old.root 
+rfcp $referenceDir/matbdg_PixFwdPlus.root    matbdg_PixFwdPlus_old.root 
+rfcp $referenceDir/matbdg_PixFwdMinus.root   matbdg_PixFwdMinus_old.root 
+rfcp $referenceDir/matbdg_TIB.root           matbdg_TIB_old.root 
+rfcp $referenceDir/matbdg_TIDF.root          matbdg_TIDF_old.root 
+rfcp $referenceDir/matbdg_TIDB.root          matbdg_TIDB_old.root 
+rfcp $referenceDir/matbdg_InnerServices.root matbdg_InnerServices_old.root 
+rfcp $referenceDir/matbdg_TOB.root           matbdg_TOB_old.root 
+rfcp $referenceDir/matbdg_TEC.root           matbdg_TEC_old.root 
+rfcp $referenceDir/matbdg_Tracker.root       matbdg_Tracker_old.root 
+rfcp $referenceDir/matbdg_BeamPipe.root      matbdg_BeamPipe_old.root 
  
-mv Geometry/ModuleInfo.log            ModuleInfo_old.log
-mv Geometry/ModuleNumbering.dat       ModuleNumbering_old.dat
-mv Geometry/trackerOverlap.log        trackerOverlap_old.log
+rfcp $referenceDir/ModuleInfo.log            ModuleInfo_old.log
+rfcp $referenceDir/ModuleNumbering.dat       ModuleNumbering_old.dat
+rfcp $referenceDir/trackerOverlap.log        trackerOverlap_old.log
 echo "...done" | tee -a $reportFile
 #
 
@@ -169,7 +174,7 @@ echo "...done" | tee -a $reportFile
 
 # Run the Tracker ModuleInfo analyzer (to compare position/orientation of Tracker Modules)
 echo "Run the Tracker ModuleInfo analyzer to print Tracker Module info (position/orientation)..." | tee -a $reportFile
-cmsRun $CMSSW_RELEASE_BASE/src/Geometry/TrackerGeometryBuilder/test/trackerModuleInfo_cfg.py
+cmsRun $CMSSW_RELEASE_BASE/src/Geometry/TrackerGeometryBuilder/python/trackerModuleInfo_cfg.py
 echo "...done" | tee -a $reportFile
 #
 
@@ -190,7 +195,7 @@ echo "...done" | tee -a $reportFile
 
 # Run the Module Numbering (only Microstrip) check algorithm and print the tail
 echo "Run the Tracker ModuleNumbering analyzer to print Tracker Numbering check..." | tee -a $reportFile
-cmsRun $CMSSW_RELEASE_BASE/src/Geometry/TrackerNumberingBuilder/test/trackerModuleNumbering_cfg.py
+cmsRun $CMSSW_RELEASE_BASE/src/Geometry/TrackerNumberingBuilder/python/trackerModuleNumbering_cfg.py
 echo "TRACKER MICROSTRIP NUMBERING... LOOK AT THE RESULTS" | tee -a $reportFile
 tail -7 ModuleNumbering.log | tee -a $reportFile
 if [ -e num.log ]; then
