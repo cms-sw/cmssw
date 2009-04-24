33910e72a5c110afad05311680cfd20a7867d922
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2009-04-24 08:57:37 +0000

    added the interesting energy deposits

--- RecoParticleFlow/PFProducer/python/pfElectronTranslator_cff.py
+++ RecoParticleFlow/PFProducer/python/pfElectronTranslator_cff.py
@@ -1,2 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 from RecoParticleFlow.PFProducer.pfElectronTranslator_cfi import *
+from RecoParticleFlow.PFProducer.pfElectronInterestingDetIds_cfi import *
+
+pfElectronTranslatorSequence = cms.Sequence(
+    pfElectronTranslator+
+    pfElectronInterestingEcalDetIdEB+
+    pfElectronInterestingEcalDetIdEE
+    )
