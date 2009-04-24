725bec1e95e5f6c9a4e8d3d99115b92574213745
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2009-04-24 16:10:59 +0000

    add L1TScalersSCAL

--- DQM/TrigXMonitor/src/SealModule.cc
+++ DQM/TrigXMonitor/src/SealModule.cc
@@ -10,3 +10,6 @@ DEFINE_ANOTHER_FWK_MODULE(HLTScalers);
 #include "DQM/TrigXMonitor/interface/L1Scalers.h" 
 DEFINE_ANOTHER_FWK_MODULE(L1Scalers);
 
+#include "DQM/TrigXMonitor/interface/L1TScalersSCAL.h" 
+DEFINE_ANOTHER_FWK_MODULE(L1TScalersSCAL);
+
