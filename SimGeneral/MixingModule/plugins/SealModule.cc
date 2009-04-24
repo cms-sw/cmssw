297d8848f817dc21294d02c06a65470296ace8c3
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2009-04-24 14:00:19 +0000

    add new plugins

--- SimGeneral/MixingModule/plugins/SealModule.cc
+++ SimGeneral/MixingModule/plugins/SealModule.cc
@@ -5,11 +5,19 @@
 
 #include "MixingModule.h"
 #include "TestMix.h"
+#include "CFWriter.h"
+#include "InputAnalyzer.h"
+#include "SecSourceAnalyzer.h"
 
   using edm::MixingModule;
   using edm::TestMix;
-
+  using edm::CFWriter;
+  using edm::InputAnalyzer;
+  using edm::SecSourceAnalyzer;
+ 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(MixingModule);
 DEFINE_ANOTHER_FWK_MODULE(TestMix);
-
+DEFINE_ANOTHER_FWK_MODULE(CFWriter);
+DEFINE_ANOTHER_FWK_MODULE(InputAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(SecSourceAnalyzer);
