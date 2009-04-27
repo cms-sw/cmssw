1dc200f4933ec2ec49601b37deedbc99028019dc
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2009-04-27 19:06:10 +0000

    bug fix

--- TrackingTools/GsfTracking/python/GsfElectronTracking_cff.py
+++ TrackingTools/GsfTracking/python/GsfElectronTracking_cff.py
@@ -4,9 +4,9 @@ from RecoParticleFlow.PFTracking.trackerDrivenElectronSeeds_cff import *
 from RecoEgamma.EgammaElectronProducers.ecalDrivenElectronSeeds_cfi import *
 from RecoParticleFlow.PFTracking.mergedElectronSeeds_cfi import *
 
-electronSeeds=(trackerDrivenElectronSeeds*ecalDrivenElectronSeeds*electronMergedSeeds) 
+electronSeeds = cms.Sequence(trackerDrivenElectronSeeds*ecalDrivenElectronSeeds*electronMergedSeeds) 
 
 
 from TrackingTools.GsfTracking.CkfElectronCandidateMaker_cff import *
 from TrackingTools.GsfTracking.GsfElectronGsfFit_cff import *
-electronGsfTracking=(electronSeeds*electronCkfTrackCandidates*electronGsfTracks)
+electronGsfTracking = cms.Sequence(electronSeeds*electronCkfTrackCandidates*electronGsfTracks)
