a6fa35c7651ea07e341a293b891ccabc3901510f
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2009-04-27 09:50:42 +0000

    roll back after Simon's changes

--- Validation/RecoParticleFlow/Benchmarks/TauBenchmarkGeneric/benchmark_cfg.py
+++ Validation/RecoParticleFlow/Benchmarks/TauBenchmarkGeneric/benchmark_cfg.py
@@ -7,19 +7,17 @@ process = cms.Process("TEST")
 process.load("DQMServices.Core.DQM_cfg")
 
 process.source = cms.Source("PoolSource",
-                            #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/g/gennai/CMSSW_310_pre2/ZTT_fastsim.root' )
-                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre4/RelValZEE/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/16D601C5-7416-DE11-A0B8-001A92971AAA.root' )
-                            #fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre4/RelValZTT/GEN-SIM-DIGI-RECO/IDEAL_30X_FastSim_v1/0001/12EEDF82-6516-DE11-A513-001A928116D2.root' )
+                            fileNames = cms.untracked.vstring('file:aod.root' )
                             )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(300)
+    input = cms.untracked.int32(-1)
 )
 
 
-process.load("Validation.RecoParticleFlow.electronBenchmarkGeneric_cff")
+process.load("Validation.RecoParticleFlow.tauBenchmarkGeneric_cff")
 process.p =cms.Path(
-    process.electronBenchmarkGeneric
+    process.tauBenchmarkGeneric
     )
 
 
