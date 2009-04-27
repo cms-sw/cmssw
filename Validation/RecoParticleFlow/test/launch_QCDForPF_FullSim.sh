098f664d4817689bbd5b2cb48396fe89f468a225
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2009-04-27 09:32:09 +0000

    Some update for pre6 + give rights to Florian

--- Validation/RecoParticleFlow/test/launch_QCDForPF_FullSim.sh
+++ Validation/RecoParticleFlow/test/launch_QCDForPF_FullSim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-castorDir=/castor/cern.ch/user/p/pjanot/CMSSW310pre5/
-cmsswDir=/afs/cern.ch/user/p/pjanot/scratch0/CMSSW_3_1_0_pre5/src
+castorDir=/castor/cern.ch/user/p/pjanot/CMSSW310pre6/
+cmsswDir=/afs/cern.ch/user/p/pjanot/scratch0/CMSSW_3_1_0_pre6/src
 
 for ((job=0;job<90;job++));
   do
