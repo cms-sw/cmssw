98e23f5aaafd9e66563521f2f65b64baae1623ed
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-27 15:56:53 +0000

    Add parameters to switch off effectively teh simulation in CASTOR until it is not fully operational in production and in the passive material in the forward region to avoid losses in computing time

--- Configuration/StandardSequences/python/SimExtended_cff.py
+++ Configuration/StandardSequences/python/SimExtended_cff.py
@@ -7,10 +7,13 @@ from SimTransport.HectorProducer.HectorTransport_cfi import *
 from SimG4Core.Configuration.SimG4Core_cff import *
 
 g4SimHits.Generator.HepMCProductLabel = cms.string('LHCTransport')
-g4SimHits.StackingAction.MaxTimeNames = cms.vstring('ZDCRegion')
-g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0)
-g4SimHits.SteppingAction.MaxTimeNames = cms.vstring('ZDCRegion')
-g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0)
+
+# kill particles in CASTOR as default until CASTOR simulation is not fully operational in standard production
+
+g4SimHits.StackingAction.MaxTimeNames = cms.vstring('ZDCRegion','CastorRegion','QuadRegion')
+g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0,0.,0.)
+g4SimHits.SteppingAction.MaxTimeNames = cms.vstring('ZDCRegion','CastorRegion','QuadRegion')
+g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0,0.,0.)
 g4SimHits.CaloSD.HCNames = cms.vstring('EcalHitsEB','EcalHitsEE','EcalHitsES','HcalHits','ZDCHITS')
 g4SimHits.CaloSD.EminHits = cms.vdouble(0.015,0.010,0.0,0.0,0.0)
 g4SimHits.CaloSD.TmaxHits = cms.vdouble(500.0,500.0,500.0,500.0,2000.0)
