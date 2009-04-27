6d5ad3b60da3ffb92b40faf38ad8b18228a097e8
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-04-27 22:42:53 +0000

    changed triggertype filter

--- DQM/L1TMonitor/python/L1TMonitor_unpacking_cff.py
+++ DQM/L1TMonitor/python/L1TMonitor_unpacking_cff.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from DQM.L1TMonitor.L1TGT_unpack_cff import *
 from DQM.L1TMonitor.L1TGCT_unpack_cff import *
 from EventFilter.CSCTFRawToDigi.csctfunpacker_cfi import *
-from HLTrigger.special.TriggerTypeFilter_cfi import *
-triggerTypeFilter.SelectedTriggerType = 1
+from HLTrigger.special.HLTTriggerTypeFilter_cfi import *
+hltTriggerTypeFilter.SelectedTriggerType = 1
 
 l1tdttfunpack = cms.EDFilter("DTTFFEDReader",
     DTTF_FED_Source = cms.InputTag("source")
@@ -19,6 +19,8 @@ l1trpctfpath = cms.Path(l1GtUnpack*cms.SequencePlaceholder("l1trpctf"))
 l1tcsctfpath = cms.Path(csctfunpacker*cms.SequencePlaceholder("l1tcsctf"))
 l1tdttpgpath = cms.Path(l1tdttfunpack*cms.SequencePlaceholder("l1tdttf"))
 l1tgmtpath = cms.Path(l1GtUnpack*cms.SequencePlaceholder("l1tgmt"))
-l1trctpath = cms.Path(triggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1trct"))
-l1tgctpath = cms.Path(triggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1tgct"))
+#l1trctpath = cms.Path(triggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1trct"))
+#l1tgctpath = cms.Path(triggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1tgct"))
+l1trctpath = cms.Path(hltTriggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1trct"))
+l1tgctpath = cms.Path(hltTriggerTypeFilter*l1GctHwDigis*cms.SequencePlaceholder("l1tgct"))
 
