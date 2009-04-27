1114b9bee2ab9a7a8737d837cc46b1f190a5580e
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-27 15:48:22 +0000

    Add ZDC to the digi2raw sequence

--- EventFilter/HcalRawToDigi/python/HcalDigiToRaw_cfi.py
+++ EventFilter/HcalRawToDigi/python/HcalDigiToRaw_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 hcalRawData = cms.EDFilter("HcalDigiToRaw",
     HBHE = cms.untracked.InputTag("simHcalDigis"),
     HF = cms.untracked.InputTag("simHcalDigis"),
-    HO = cms.untracked.InputTag("simHcalDigis")
+    HO = cms.untracked.InputTag("simHcalDigis"),
+    ZDC = cms.untracked.InputTag("simHcalUnsuppressedDigis")                       
 )
 
 
