f82b5813e164a4fbbc223ed07a49e7d79f88269f
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-04-28 08:34:04 +0000

    *** empty log message ***

--- DQM/HLTEvF/python/HLTMonScalers_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from EventFilter.ScalersRawToDigi.ScalersRawToDigi_cfi import *
-from DQM.TrigXMonitor.L1TScalersSCAL_cfi import *
-hltScalRawToDigi = cms.Path(scalersRawToDigi)
-hltMonScal = cms.EndPath(l1tscalers)
-
