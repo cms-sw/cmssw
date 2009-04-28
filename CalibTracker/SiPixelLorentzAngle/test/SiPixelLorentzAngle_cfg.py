7c4310962042915e41d75461f8903b2c948fe7db
wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch>
2009-04-28 09:44:41 +0000

    config file for CMSSW_2_2_9

--- CalibTracker/SiPixelLorentzAngle/test/SiPixelLorentzAngle_cfg.py
+++ CalibTracker/SiPixelLorentzAngle/test/SiPixelLorentzAngle_cfg.py
@@ -12,7 +12,8 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "STARTUP_V7::All"
+# check for the correct tag on https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
+process.GlobalTag.globaltag = "STARTUP_V11::All"
 
 
 process.load("RecoTracker.Configuration.RecoTracker_cff")
@@ -22,7 +23,10 @@ process.offlineBeamSpot = offlineBeamSpot
 
 
 process.load("RecoTracker.TrackProducer.RefitterWithMaterial_cff")
-process.TrackRefitter.src = "ALCARECOTkAlZMuMu"
+# put here the tag of the tracks you want to use
+# alcareco samples have special names for the tracks, in normal reco samples generalTracks can be used
+process.TrackRefitter.src = "generalTracks"
+# process.TrackRefitter.src = "ALCARECOTkAlZMuMu"
 process.TrackRefitter.TrajectoryInEvent = True
 process.load("RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilderWithoutRefit_cfi")
 
@@ -41,16 +45,18 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("PoolSource",
+	#put here the sample you want to use
     fileNames = cms.untracked.vstring(
-	   '/store/relval/CMSSW_2_1_10/RelValZMM/ALCARECO/STARTUP_V7_StreamALCARECOTkAlZMuMu_v3/0000/0A311E46-B39B-DD11-A832-000423D6C8EE.root'
+	   '/store/relval/CMSSW_2_2_9/RelValZMM/GEN-SIM-RECO/STARTUP_V11_v1/0001/FE948375-4D32-DE11-B1DD-001617C3B654.root'
 	),   
 #   skipEvents = cms.untracked.uint32(100) 
 )
 
+
 process.lorentzAngle = cms.EDAnalyzer("SiPixelLorentzAngle",
-	TTRHBuilder= cms.string("WithTrackAngle"),
-	Fitter = cms.string("KFFittingSmoother"),
-  Propagator = cms.string("PropagatorWithMaterial"),
+#	TTRHBuilder= cms.string("WithTrackAngle"),
+#	Fitter = cms.string("KFFittingSmoother"),
+#  Propagator = cms.string("PropagatorWithMaterial"),
 #   Propagator = cms.string("RungeKuttaTrackerPropagator"),
 	src = cms.string("TrackRefitter"),
 	fileName = cms.string("lorentzangle.root"),
@@ -58,9 +64,9 @@ process.lorentzAngle = cms.EDAnalyzer("SiPixelLorentzAngle",
 	binsDepth	= cms.int32(50),
 	binsDrift =	cms.int32(200),
 	ptMin = cms.double(3),
-	#in case of MC set this to true to save the simhits
+	#in case of MC set this to true to save the simhits (does not work currently, Mixing Module needs to be included correctly)
 	simData = cms.bool(False),
-  normChi2Max = cms.double(2),	
+  	normChi2Max = cms.double(2),	
 	clustSizeYMin = cms.int32(4),
 	residualMax = cms.double(0.005),
 	clustChargeMax = cms.double(120000)
@@ -72,4 +78,5 @@ process.myout = cms.OutputModule("PoolOutputModule",
 
 process.p = cms.Path(process.offlineBeamSpot*process.TrackRefitter*process.lorentzAngle)
 
+# uncomment this if you want to write out the new CMSSW root file (very large)
 # process.outpath = cms.EndPath(process.myout)
