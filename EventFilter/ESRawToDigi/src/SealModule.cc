fac6ca8519b6b6bcc92f5961f6970d89d4549267
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2009-04-28 12:27:21 +0000

    implementation of ESRawtoRecHit

--- EventFilter/ESRawToDigi/src/SealModule.cc
+++ EventFilter/ESRawToDigi/src/SealModule.cc
@@ -4,5 +4,7 @@
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ESRawToDigi);
 
+#include "EventFilter/ESRawToDigi/interface/ESUnpackerWorkerESProducer.h"
+DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(ESUnpackerWorkerESProducer);
 
 
