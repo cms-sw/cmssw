75b9b441b5cdde7a6207790621f05c4d9163e392
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2009-04-28 09:54:15 +0000

    *** empty log message ***

--- L1Trigger/CSCTrackFinder/plugins/CSCTFanalyzer.h
+++ L1Trigger/CSCTrackFinder/plugins/CSCTFanalyzer.h
@@ -8,6 +8,7 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
+#include "CondFormats/L1TObjects/interface/L1MuTriggerScales.h"
 
 #include "TTree.h"
 #include "TFile.h"
@@ -18,12 +19,19 @@ private:
 	TTree *tree;
 	TFile *file;
 	int nDataMuons, nEmulMuons, verbose;
-	int dphi1, deta1, dpt1, dch1, dbx1;
-	int dphi2, deta2, dpt2, dch2, dbx2;
-	int dphi3, deta3, dpt3, dch3, dbx3;
-	int ephi1, eeta1, ept1, ech1, ebx1;
-	int ephi2, eeta2, ept2, ech2, ebx2;
-	int ephi3, eeta3, ept3, ech3, ebx3;
+	double dphi1, deta1; int dpt1, dch1, dbx1;
+	double dphi2, deta2; int dpt2, dch2, dbx2;
+	double dphi3, deta3; int dpt3, dch3, dbx3;
+	int drank1, drank2, drank3;
+	int dmode1, dmode2, dmode3;
+	int dlcts1, dlcts2, dlcts3;
+	double ephi1, eeta1; int ept1, ech1, ebx1;
+	double ephi2, eeta2; int ept2, ech2, ebx2;
+	double ephi3, eeta3; int ept3, ech3, ebx3;
+	int erank1, erank2, erank3;
+	int emode1, emode2, emode3;
+
+	const L1MuTriggerScales *ts;
 
 public:
 	virtual void analyze(edm::Event const& e, edm::EventSetup const& iSetup);
