5f76c503a6f326e758200ea666cc2443fe5b81e8
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-04-28 08:32:47 +0000

    added scalers

--- DQM/L1TMonitor/python/L1TMonitor_cff.py
+++ DQM/L1TMonitor/python/L1TMonitor_cff.py
@@ -7,4 +7,4 @@ from DQM.L1TMonitor.L1TLTC_cff import *
 from DQM.L1TMonitor.L1TGT_readout_cff import *
 from DQM.L1TMonitor.L1TGCT_readout_cff import *
 from DQM.L1TMonitor.L1TMonitor_unpacking_cff import *
-
+from DQM.L1TMonitor.L1TMonScalers_cff import *
