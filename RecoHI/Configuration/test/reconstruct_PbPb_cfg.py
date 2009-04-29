d0fc540167b059c43be6b69fc0f72232e6feabe4
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2009-04-29 16:23:43 +0000

    Full reco cfg

---
+++ RecoHI/Configuration/test/reconstruct_PbPb_cfg.py
@@ -0,0 +1,84 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RECO")
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.Simulation_cff")
+process.load("Configuration.StandardSequences.L1Emulator_cff")
+process.load("Configuration.StandardSequences.L1TriggerDefaultMenu_cff")
+process.load("Configuration.StandardSequences.DigiToRaw_cff")
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
+# For including Standard Reco + Heavy Ion Objects
+process.load("RecoHI.Configuration.Reconstruction_HI_cff")
+#process.load("HeavyIonsAnalysis.Configuration.HIAnalysisEventContent_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/pgun_jpsi2muons_d20080423/pgun_jpsi2muons_d20080423_r000001.root')
+fileNames = cms.untracked.vstring(           "rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/hydjet_sim_x2_c5_d20080425/hydjet_sim_x2_c5_d20080425_r000002.root")
+
+)
+
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
+
+#from HeavyIonsAnalysis.Configuration.EventEmbedding_cff import *
+#process.mix=mixSim
+#process.mix.input.fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/pgun_jpsi2muons_d20080423/pgun_jpsi2muons_d20080423_r000005.root')
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    moduleSeeds = cms.PSet(
+        simMuonRPCDigis = cms.untracked.uint32(6),
+        simEcalUnsuppressedDigis = cms.untracked.uint32(8),
+        simSiStripDigis = cms.untracked.uint32(7),
+        mix = cms.untracked.uint32(4),
+        simHcalUnsuppressedDigis = cms.untracked.uint32(9),
+        simMuonCSCDigis = cms.untracked.uint32(6),
+        VtxSmeared = cms.untracked.uint32(2),
+        g4SimHits = cms.untracked.uint32(3),
+        simMuonDTDigis = cms.untracked.uint32(6),
+        simSiPixelDigis = cms.untracked.uint32(7)
+    ),
+    sourceSeed = cms.untracked.uint32(1)
+)
+
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+    oncePerEventMode = cms.untracked.bool(False),
+    ignoreTotal = cms.untracked.int32(0)
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('towerMaker', 
+        'caloTowers', 
+        'iterativeConePu5CaloJets'),
+    destinations = cms.untracked.vstring('cout', 
+        'cerr'),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    fwkJobReports = cms.untracked.vstring('FrameworkJobReport.xml')
+)
+
+process.Timing = cms.Service("Timing")
+
+process.output = cms.OutputModule("PoolOutputModule",
+#    process.HIRecoObjects,
+    compressionLevel = cms.untracked.int32(2),
+    commitInterval = cms.untracked.uint32(1),
+    fileName = cms.untracked.string('jpsi2muons_PbPb_RECO.root')
+)
+
+process.p = cms.Path(process.mix*process.doAllDigi*process.L1Emulator*process.DigiToRaw*process.RawToDigi*process.reconstruct_PbPb)
+#process.p = cms.Path(process.mix*process.doAllDigi*process.L1Emulator*process.DigiToRaw*process.RawToDigi*process.hiTrackingWithOfflineBeamSpot)
+process.outpath = cms.EndPath(process.output)
+process.output.outputCommands.append('keep *_*_*_RECO')
+
+
