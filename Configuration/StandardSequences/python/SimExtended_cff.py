f2a569a2fa89ef350c8468521e68a0441790623b
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-04-29 11:51:19 +0000

    Add the beam transport done by Hector (LHCTransport) in the default scenario, and protect NOBEAM for this

--- Configuration/StandardSequences/python/SimExtended_cff.py
+++ Configuration/StandardSequences/python/SimExtended_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Hector transport of particles along the beam pipe for very forward detectors
-from SimTransport.HectorProducer.HectorTransport_cfi import *
+from SimTransport.HectorProducer.HectorTransportZDC_cfi import *
 
 # CMSSW/Geant4 interface
 from SimG4Core.Configuration.SimG4Core_cff import *
