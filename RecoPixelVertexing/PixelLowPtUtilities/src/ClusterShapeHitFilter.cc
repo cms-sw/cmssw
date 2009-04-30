477b66fd9dba091b32c82d8beca68fc3b1ce388c
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2009-04-30 09:25:05 +0000

    registration of ClusterShapeHitFilter moved

--- RecoPixelVertexing/PixelLowPtUtilities/src/ClusterShapeHitFilter.cc
+++ RecoPixelVertexing/PixelLowPtUtilities/src/ClusterShapeHitFilter.cc
@@ -385,12 +385,11 @@ bool ClusterShapeHitFilter::isCompatible
   return isCompatible(recHit, ldir);
 }
 
-/*
-  #include "FWCore/PluginManager/interface/ModuleDef.h"
-  #include "FWCore/Framework/interface/MakerMacros.h"
-  DEFINE_SEAL_MODULE();
-  
-  #include "RecoPixelVertexing/PixelLowPtUtilities/interface/ClusterShapeHitFilter.h"
-  #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-  EVENTSETUP_DATA_REG(ClusterShapeHitFilter);
-*/
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_SEAL_MODULE();
+
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+EVENTSETUP_DATA_REG(ClusterShapeHitFilter);
+
--- RecoPixelVertexing/PixelLowPtUtilities/src/ClusterShapeHitRegister.cc
+++
@@ -1,9 +0,0 @@
-// HitFilter
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_SEAL_MODULE();
-
-#include "RecoPixelVertexing/PixelLowPtUtilities/interface/ClusterShapeHitFilter.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-EVENTSETUP_DATA_REG(ClusterShapeHitFilter);
-
