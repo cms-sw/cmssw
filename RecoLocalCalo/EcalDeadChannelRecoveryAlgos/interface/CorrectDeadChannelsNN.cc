5e2c996b1f17ca1aa387e8d5022c7d68a23ef0fe
Stephanie Beauceron <sha1-c117a3d4e0f055ce820584107fcb2695f540f4e1@cern.ch>
2009-04-30 13:14:34 +0000

    Correct Bug and modify function to avoid bug in rare case

--- RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/CorrectDeadChannelsNN.cc
+++ RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/CorrectDeadChannelsNN.cc
@@ -66,7 +66,7 @@ double CorrectDeadChannelsNN(double *M5x5Input){
   M5x5[2]=M5x5Input[49];
   M5x5[3]=M5x5Input[61];
   M5x5[4]=M5x5Input[72];
-  M5x5[5]=M5x5Input[51];
+  M5x5[5]=M5x5Input[50];
   M5x5[6]=M5x5Input[59];
   M5x5[7]=M5x5Input[70];
   M5x5[8]=M5x5Input[48];
@@ -112,7 +112,7 @@ double CorrectDeadChannelsNN(double *M5x5Input){
   //First Find the position of the Dead Channel in 3x3 matrix
   int IndDeadCha=-1;
   for(int i =0 ; i< 9;i++){
-    if(fabs(M5x5[i])<epsilon && IndDeadCha >0){cout<<" Problem 2 dead channels in sum9! Can not correct "<<endl; return 0.0;}
+    if(fabs(M5x5[i])<epsilon && IndDeadCha >0){/*cout<<" Problem 2 dead channels in sum9! Can not correct "<<endl;*/ return 0.0;}
     if(fabs(M5x5[i])<epsilon && IndDeadCha==-1)IndDeadCha=i;
   }
 
@@ -206,7 +206,7 @@ double CorrectDeadChannelsNN(double *M5x5Input){
     voisin1=6;voisin2=2;voisin3=18;voisin4=14;
     break;
   default:
-    cout<<" Error, Dead Channel to far from main containement one, no correction is applied"<<endl;
+    //    cout<<" Error, Dead Channel to far from main containement one, no correction is applied"<<endl;
     return 0.0;
     break;
   }//end switch
