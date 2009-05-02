4e88d177da74a846b270b1224eb3d2d825e79703
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2009-05-02 23:44:20 +0000

    upgrade ref files ; add a way to work from local input files in Configuration

--- RecoEgamma/Examples/test/dbs_discovery.py
+++ RecoEgamma/Examples/test/dbs_discovery.py
@@ -9,40 +9,50 @@ import httplib, urllib, urllib2, types, string, os, sys
 
 def search():
 
-	url = "https://cmsweb.cern.ch:443/dbs_discovery/aSearch"
-	input = "find file where release = " + os.environ['DBS_RELEASE']
-	input = input + " and primds = " + os.environ['DBS_SAMPLE']
-	input = input + " and dataset like " + os.environ['DBS_LIKE']
-	final_input = urllib.quote(input) ;
+  if os.environ['DBS_RELEASE'] == "LOCAL":
+    if os.environ['DBS_SAMPLE'] == "RelValSingleElectronPt10":
+      result = ['file:../../Configuration/test/SingleElectronPt10Reco.root']
+    elif os.environ['DBS_SAMPLE'] == "RelValSingleElectronPt35":
+      result = ['file:../../Configuration/test/SingleElectronPt35Reco.root']
+    elif os.environ['DBS_SAMPLE'] == "RelValQCD_Pt_80_120":
+      result = ['file:../../Configuration/test/QcdPt80-120Reco.root']
+    else:
+      result = ['NONE']
+  else:
+    url = "https://cmsweb.cern.ch:443/dbs_discovery/aSearch"
+    input = "find file where release = " + os.environ['DBS_RELEASE']
+    input = input + " and primds = " + os.environ['DBS_SAMPLE']
+    input = input + " and dataset like " + os.environ['DBS_LIKE']
+    final_input = urllib.quote(input) ;
 
-	agent   = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
-	ctypes  = "text/plain"
-	headers = { 'User-Agent':agent, 'Accept':ctypes}
-	params  = {'dbsInst':'cms_dbs_prod_global',
-	           'html':0,'caseSensitive':'on','_idx':0,'pagerStep':-1,
-						 'userInput':final_input,
-						 'xml':0,'details':0,'cff':0,'method':'dbsapi'}
-	data    = urllib.urlencode(params,doseq=True)
-	req     = urllib2.Request(url, data, headers)
-	data    = ""
+    agent   = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
+    ctypes  = "text/plain"
+    headers = { 'User-Agent':agent, 'Accept':ctypes}
+    params  = {'dbsInst':'cms_dbs_prod_global',
+               'html':0,'caseSensitive':'on','_idx':0,'pagerStep':-1,
+               'userInput':final_input,
+               'xml':0,'details':0,'cff':0,'method':'dbsapi'}
+    data    = urllib.urlencode(params,doseq=True)
+    req     = urllib2.Request(url, data, headers)
+    data    = ""
 
-	try:
-			response = urllib2.urlopen(req)
-			data = response.read()
-	except urllib2.HTTPError, e:
-			if e.code==201:
-				 print e.headers       
-				 print e.msg
-				 pass
-			else:
-				 raise e
-				 
-	result = []
-	for line in data.split("\n"):
-	  if line != "" and line[0] =="/":
-		  result.append(line)
-			
-	return result
+    try:
+      response = urllib2.urlopen(req)
+      data = response.read()
+    except urllib2.HTTPError, e:
+      if e.code==201:
+        print e.headers       
+        print e.msg
+        pass
+      else:
+        raise e
+
+    result = []
+    for line in data.split("\n"):
+      if line != "" and line[0] =="/":
+        result.append(line)
+
+  return result
 
 if __name__ == "__main__":
   for file in search():
