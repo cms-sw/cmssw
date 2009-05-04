4fb622a24cf8e0571ec413bbbb7e5705ff8b6cd3
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-05-04 19:24:29 +0000

    Removed isolation stuff from eventcontent, isolation variables are now with gsfElectrons

--- Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
+++ Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
@@ -15,26 +15,6 @@ OutALCARECOEcalCalElectron = cms.PSet(
         'keep recoCaloMETs_met_*_*',
         'keep edmTriggerResults_TriggerResults__*', 
         'keep edmHepMCProduct_*_*_*',                                   
-
-#        'keep *_egammaEcalIsolation_*_*', 
-#        'keep *_egammaEcalRelIsolation_*_*', 
-#        'keep *_egammaElectronSqPtTkIsolation_*_*', 
-#        'keep *_egammaElectronTkIsolation_*_*',
-#        'keep *_egammaElectronTkNumIsolation_*_*', 
-#        'keep *_egammaElectronTkRelIsolation_*_*', 
-#        'keep *_egammaHOETower_*_*', 
-#        'keep *_egammaHOE_*_*', 
-#        'keep *_egammaHcalIsolation_*_*', 
-#        'keep *_egammaTowerIsolation_*_*',
-   
-
-        'keep *_eleIsoFromDepsTk_*_*',                       
-        'keep *_eleIsoFromDepsEcalFromHits_*_*',                       
-        'keep *_eleIsoFromDepsHcalFromTowers_*_*',                       
-        'keep *_eleIsoFromDepsHcalDepth1FromTowers_*_*',                       
-        'keep *_eleIsoFromDepsHcalDepth2FromTowers_*_*',                       
-
-
         'keep *_MEtoEDMConverter_*_*')
 )
 
