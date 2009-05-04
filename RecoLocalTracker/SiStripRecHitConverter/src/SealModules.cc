e55a91a9a1e08062c862169e8133f69cd3805ed1
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-05-04 22:26:34 +0000

    Removed unnecessary includes

--- RecoLocalTracker/SiStripRecHitConverter/src/SealModules.cc
+++ RecoLocalTracker/SiStripRecHitConverter/src/SealModules.cc
@@ -1,20 +1,5 @@
-#include "RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h"
-#include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEESProducer.h"
-#include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngleESProducer.h"
-#include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngle2ESProducer.h"
-
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/SiStripRecHitMatcherESProducer.h"
-
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/ModuleFactory.h"
-#include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
-
-#include "RecoLocalTracker/SiStripRecHitConverter/interface/SiStripRecHitConverter.h"
 
 EVENTSETUP_DATA_REG(SiStripRecHitMatcher);
 
