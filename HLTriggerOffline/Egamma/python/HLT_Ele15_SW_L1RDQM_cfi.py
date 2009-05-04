2723e1d4a434c939fd23afada3eb6b29b1287f40
Matthias Ulrich Mozer <sha1-03a50c2c36d55df29636c6c60b13205b37358346@cern.ch>
2009-05-04 11:16:08 +0000

    added new path for 1e31 menu

---
+++ HLTriggerOffline/Egamma/python/HLT_Ele15_SW_L1RDQM_cfi.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+HLT_Ele15_SW_L1RDQM = cms.EDFilter("EmDQM",
+    genEtaAcc = cms.double(2.5),
+    genEtAcc = cms.double(2.0),
+    reqNum = cms.uint32(1),
+    filters = cms.VPSet(cms.PSet(
+        PlotBounds = cms.vdouble(0.0, 0.0),
+        HLTCollectionLabels = cms.InputTag("hltL1sRelaxedSingleEgammaEt8","","HLT"),
+        IsoCollections = cms.VInputTag(cms.InputTag("none")),
+        theHLTOutputTypes = cms.int32(-82)
+    ), 
+        cms.PSet(
+            PlotBounds = cms.vdouble(0.0, 0.0),
+            HLTCollectionLabels = cms.InputTag("hltL1NonIsoHLTNonIsoSingleElectronEt15L1MatchFilterRegional","","HLT"),
+            IsoCollections = cms.VInputTag(cms.InputTag("none")),
+            theHLTOutputTypes = cms.int32(92)
+        ), 
+        cms.PSet(
+            PlotBounds = cms.vdouble(0.0, 0.0),
+            HLTCollectionLabels = cms.InputTag("hltL1NonIsoHLTNonIsoSingleElectronEt15EtFilter","","HLT"),
+            IsoCollections = cms.VInputTag(cms.InputTag("none")),
+            theHLTOutputTypes = cms.int32(92)
+        ), 
+        cms.PSet(
+            PlotBounds = cms.vdouble(0.0, 10.0),
+            HLTCollectionLabels = cms.InputTag("hltL1NonIsoHLTNonIsoSingleElectronEt15HcalIsolFilter","","HLT"),
+            IsoCollections = cms.VInputTag(cms.InputTag("hltL1IsolatedElectronHcalIsol","","HLT"), cms.InputTag("hltL1NonIsolatedElectronHcalIsol","","HLT")),
+            theHLTOutputTypes = cms.int32(92)
+        ), 
+        cms.PSet(
+            PlotBounds = cms.vdouble(0.0, 0.0),
+            HLTCollectionLabels = cms.InputTag("hltL1NonIsoHLTNonIsoSingleElectronEt15PixelMatchFilter","","HLT"),
+            IsoCollections = cms.VInputTag(cms.InputTag("none")),
+            theHLTOutputTypes = cms.int32(92)
+        )),
+    PtMax = cms.untracked.double(100.0),
+    pdgGen = cms.int32(11)
+)
+
+
+
