7938b01d9134452952a9d5f281ee58a78830d740
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-05-04 19:36:28 +0000

    fix mispelled parameter

--- DQMOffline/CalibCalo/python/MonitorHcalDiJetsAlCaReco_cfi.py
+++ DQMOffline/CalibCalo/python/MonitorHcalDiJetsAlCaReco_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 MonitorHcalDiJetsAlCaReco = cms.EDAnalyzer("DQMHcalDiJetsAlCaReco",
-folderName=cms.string("AlCaReco/HcalDiJets"),
+FolderName=cms.string("AlCaReco/HcalDiJets"),
 SaveToFile=cms.untracked.bool(False),
 FileName=cms.untracked.string("HcalDiJetsAlCaRecoMon.root"),
 jetsInput = cms.InputTag('DiJProd:DiJetsBackToBackCollection'),
