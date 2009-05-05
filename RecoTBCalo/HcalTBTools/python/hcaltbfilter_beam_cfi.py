41c16df6bf9f6ca99db089cb9a635348fcedfcf7
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-05-05 18:00:45 +0000

    fix syntax error

--- RecoTBCalo/HcalTBTools/python/hcaltbfilter_beam_cfi.py
+++ RecoTBCalo/HcalTBTools/python/hcaltbfilter_beam_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-hcaltbfilter-beam = cms.EDFilter("HcalTBTriggerFilter",
+hcaltbfilter_beam = cms.EDFilter("HcalTBTriggerFilter",
     AllowLED = cms.bool(False),
     AllowPedestalOutSpill = cms.bool(False),
     AllowLaser = cms.bool(False),
