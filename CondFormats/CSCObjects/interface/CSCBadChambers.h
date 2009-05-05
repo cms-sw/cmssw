e946a2bc5e76a2d8a78e6c6c5151c13ea3e8113b
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2009-05-05 14:16:19 +0000

    add isInBadChamber to allow external use

--- CondFormats/CSCObjects/interface/CSCBadChambers.h
+++ CondFormats/CSCObjects/interface/CSCBadChambers.h
@@ -1,15 +1,34 @@
 #ifndef CSCBadChambers_h
 #define CSCBadChambers_h
 
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
+
 #include <vector>
 
 class CSCBadChambers{
  public:
-  CSCBadChambers();
-  ~CSCBadChambers();
+  CSCBadChambers() : numberOfBadChambers_( 0 ), skipBadChambers_( true ), chambers_( std::vector<int>() ){};
+  ~CSCBadChambers(){};
 
-  int numberOfBadChambers;
-  std::vector<int> chambers;
+  /// How many bad chambers are there>
+  int numberOfChambers() const { return numberOfBadChambers_; }
+
+  /// Return the flag for skipping bad chambers or not
+  bool skipBadChambers() const { return skipBadChambers_; }
+
+  /// Return the container of bad chambers
+  std::vector<int> chambers() const { return chambers_; }
+
+  /// Set the flag for skipping bad chambers or not (activate/deactivate call to isInBadChamber)
+  void switchSkipBadChambers( bool onoff ) { skipBadChambers_ = onoff; }
+
+  /// Is the gven chamber flagged as bad?
+  bool isInBadChamber( const CSCDetId& id ) const;
+
+ private:
+  int numberOfBadChambers_;
+  bool skipBadChambers_;
+  std::vector<int> chambers_;
 };
 
 #endif
--- CondFormats/CSCObjects/src/CSCBadChambers.cc
+++ CondFormats/CSCObjects/src/CSCBadChambers.cc
@@ -1,5 +1,19 @@
 #include "CondFormats/CSCObjects/interface/CSCBadChambers.h"
+#include "DataFormats/MuonDetId/interface/CSCIndexer.h"
 
-CSCBadChambers::CSCBadChambers():numberOfBadChambers( 0 ){}
+bool CSCBadChambers::isInBadChamber( const CSCDetId& id ) const {
 
-CSCBadChambers::~CSCBadChambers(){}
+  if ( skipBadChambers() || ( numberOfChambers() == 0 ) ) return false;
+
+  short int iri = id.ring();
+  //@@ Beware future ME11 changes
+  if ( iri == 4 ) iri = 1; // reset ME1A to ME11
+  CSCIndexer indexer;
+  int ilin = indexer.chamberIndex( id.endcap(), id.station(), iri, id.chamber() );
+  std::vector<int>::const_iterator badbegin = chambers_.begin();
+  std::vector<int>::const_iterator badend   = chambers_.end();
+  std::vector<int>::const_iterator it = std::find( badbegin, badend, ilin );
+  if ( it != badend ) return true; // id is in the list of bad chambers
+  else return false;
+}
+
