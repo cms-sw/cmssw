96f9e6b22c0b3556693288b4ba3af1827f51d711
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-05-06 14:37:45 +0000

    update comments and input

--- Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/benchmark_cfg.py
+++ Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/benchmark_cfg.py
@@ -1,5 +1,5 @@
-# Runs PFBenchmarkAnalyzer and PFJetBenchmark on PFJet sample to
-# monitor performance of PFJets
+# Runs the GenericBenchmark to
+# monitor performance of pfMET
 
 import FWCore.ParameterSet.Config as cms
 
@@ -9,15 +9,7 @@ process.load("DQMServices.Core.DQM_cfg")
 
 process.source = cms.Source("PoolSource",
 fileNames = cms.untracked.vstring(
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_000.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_001.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_002.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_003.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_004.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_005.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_006.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_007.root',
-'file:/uscms_data/d2/cavanaug/DATA/CMSSW300pre6/aod_QCDForPF_Full_008.root'
+'/../user/l/lacroix/MET_Validation/ttbar_fastsim_310_pre6_muonAndJEC/aod.root'
 )
 )
 process.maxEvents = cms.untracked.PSet(
