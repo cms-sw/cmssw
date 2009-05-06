defa0ecacce2c984c1c95db389fabe49b77c6b47
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-05-06 12:25:50 +0000

    fist version of packer/unpacker test

---
+++ Validation/HcalDigis/interface/Digi2Raw2Digi.h
@@ -0,0 +1,25 @@
+#ifndef DIGI2RAW2DIGI_H
+#define DIGI2RAW2DIGI_H
+
+// user include files
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include <map>
+
+class Digi2Raw2Digi : public edm::EDAnalyzer {
+public:
+  explicit Digi2Raw2Digi(const edm::ParameterSet&);
+  ~Digi2Raw2Digi();
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  template<class Digi>  void compare(const edm::Event&, const edm::EventSetup&);
+ private:
+
+  edm::InputTag inputTag1_;
+  edm::InputTag inputTag2_;
+
+};
+
+#endif
