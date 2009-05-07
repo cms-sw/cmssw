bd360baece0514dd66a389c090b37286ed82c875
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-05-07 22:23:07 +0000

    partially cleanup of obsolete python configs

--- TopQuarkAnalysis/TopEventProducers/python/sequences/numJetFilter_ttFullLeptonic_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-## import module
-from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import countLayer1Jets
-
-## configure for tqaf
-countLayer1Jets.src       = 'selectedLayer1Jets'
-countLayer1Jets.maxNumber = 999999
-countLayer1Jets.minNumber = 2
