bf0def0b35fbdd528ef4d7e58404072f9899264f
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-05-07 22:29:28 +0000

    adding new config files as fwd ports from B22X_v2

---
+++ TopQuarkAnalysis/TopEventProducers/python/tqafEventContent_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+tqafEventContent = [
+  ## genEvt
+  'keep *_decaySubset_*_*',
+  'keep *_initSubset_*_*', 
+  'keep *_genEvt_*_*',
+  ## extras for event selection
+  'keep *_kinFitTtSemiLepEventSelection_*_*',
+  'keep *_findTtSemiLepSignalSelMVA_*_*',
+  ## hypotheses & event structure
+  'keep *_ttSemiLepHyp*_*_*',
+  'keep *_ttSemiLepEvent_*_*'
+]
