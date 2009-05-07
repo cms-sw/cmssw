ab7a2e8c2403e7bb3ea8688e2cdd8274a8c9962a
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2009-05-07 11:25:01 +0000

    This Class is from now on to test the RPCHSCP Candidate

--- SUSYBSMAnalysis/HSCP/src/RPCHSCP.cc
+++ SUSYBSMAnalysis/HSCP/src/RPCHSCP.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Camilo Carrillo camilo.carrillo AT cern.ch
 //         Created:  Wed Aug  6 17:45:45 CEST 2008
-// $Id$
+// $Id: RPCHSCP.cc,v 1.1 2008/08/06 17:04:27 carrillo Exp $
 //
 //
 
@@ -40,6 +40,39 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include <Geometry/Records/interface/MuonGeometryRecord.h>
 #include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandMatchMap.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "Geometry/CommonDetUnit/interface/GeomDet.h"
+#include <DataFormats/GeometrySurface/interface/LocalError.h>
+#include <DataFormats/GeometryVector/interface/LocalPoint.h>
+#include "DataFormats/GeometrySurface/interface/Surface.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/GeomPropagators/interface/AnalyticalPropagator.h"
+#include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
+
+#include "SimDataFormats/Track/interface/SimTrack.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "SimDataFormats/Vertex/interface/SimVertex.h"
+#include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
+#include "FastSimulation/Tracking/test/FastTrackAnalyzer.h"
+
+#include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
+#include "RecoMuon/TrackingTools/interface/MuonPatternRecoDumper.h"
+
+
+#include "SUSYBSMAnalysis/HSCP/interface/RPCHSCPCANDIDATE.h"
+
 //Root
 #include "TFile.h"
 #include "TF1.h"
@@ -60,8 +93,6 @@ class RPCHSCP : public edm::EDProducer {
       explicit RPCHSCP(const edm::ParameterSet&);
       ~RPCHSCP();
   
-
-
    private:
       virtual void beginJob(const edm::EventSetup&) ;
       virtual void produce(edm::Event&, const edm::EventSetup&);
@@ -72,9 +103,26 @@ class RPCHSCP : public edm::EDProducer {
       //int matrixbitTOTAL[4][7];
       TFile* theFile;
       TH1F * matrixHisto;
+
+      TH1F * efficiencyeta;
+      TH1F * efficiencyphi;
+      TH1F * efficiencybeta;
+
+      TH1F * expectedeta;
+      TH1F * expectedphi;
+      TH1F * expectedbeta;
+
+      TH1F * observedeta;
+      TH1F * observedphi;
+      TH1F * observedbeta;
+
+      TH1F * residualeta;
+      TH1F * residualphi;
+
       int Maxbx[7];
       int totalHSCP;
       std::string fileMatrixname; 
+      std::string partLabel;
       // ----------member data ---------------------------
 };
 
@@ -92,28 +140,14 @@ class RPCHSCP : public edm::EDProducer {
 //
 RPCHSCP::RPCHSCP(const edm::ParameterSet& iConfig)
 {
-  
+  partLabel = iConfig.getUntrackedParameter<std::string>("partLabel");
   fileMatrixName= iConfig.getUntrackedParameter<std::string>("fileMatrixname");
   rootFileName = iConfig.getUntrackedParameter<std::string>("rootFileName");
-  produces<int>("JustATest");
-  
-   //register your products
-/* Examples
-   produces<ExampleData2>();
-
-   //if do put with a label
-   produces<ExampleData2>("label");
-*/
-   //now do what ever other initialization is needed
-
+  produces<int>("JustATest"); 
 }
 
 
-RPCHSCP::~RPCHSCP()
-{
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+RPCHSCP::~RPCHSCP(){
 
 }
 
@@ -124,140 +158,76 @@ RPCHSCP::~RPCHSCP()
 
 // ------------ method called to produce the data  ------------
 void
-RPCHSCP::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
-  
+RPCHSCP::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
   edm::Handle<RPCRecHitCollection> rpcHits;
   iEvent.getByLabel("rpcRecHits",rpcHits);
   edm::ESHandle<RPCGeometry> rpcGeo;
   iSetup.get<MuonGeometryRecord>().get(rpcGeo);
-  RPCRecHitCollection::const_iterator recIt;
   
-  //Identifing in the RecHits-HSPC
-   
-  int const layitLimit=7;
-  int const bxsitLimit=4;
-  int matrixbit[bxsitLimit][layitLimit];
-  for(int lay=0;lay<layitLimit;lay++){//looping layers
-    for(int bxs=0;bxs<bxsitLimit;bxs++){//looping bx
-      matrixbit[bxs][lay]=0;
-    }
-  }
+  std::cout<<"Reading the particles container"<<std::endl;
 
+  edm::Handle<reco::GenParticleCollection> genParticles;
+  iEvent.getByLabel(partLabel,genParticles );
   
-  for(recIt=rpcHits->begin();recIt!=rpcHits->end();++recIt){
-    RPCDetId RPCId((*recIt).rpcId());
-    std::cout<<"RPCId"<<RPCId<<std::endl;
-    
-    int BX = (*recIt).BunchX();
-    int layer=0;
-    int mySt = RPCId.station();
-    
-    if(RPCId.region()==0){
-      int myLa = RPCId.layer();
-      if(mySt==1&&myLa==1){layer=1;}
-      if(mySt==1&&myLa==2){layer=2;}
-      if(mySt==2&&myLa==1){layer=3;}
-      if(mySt==2&&myLa==2){layer=4;}
-      if(mySt==3){layer=5;}
-      if(mySt==4){layer=6;}
-      matrixbit[BX][layer]++;
-    }else{
-      if(mySt==1){layer=1;}
-      if(mySt==2){layer=2;}
-      if(mySt==3){layer=3;}
-    }
-  }
-  
-  fileMatrix<<"\n";
+  std::cout << " Number of Particles in this event: " << genParticles->size() << std::endl;
 
-  std::cout<<"Printing matrix Event "<<iEvent.id().event()<<std::endl;
-  fileMatrix<<"Printing matrix Event "<<iEvent.id().event()<<std::endl;
-  
-  for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
-    for(int lay=1;lay<layitLimit;lay++){//looping layers
-      fileMatrix<<matrixbit[bxs][lay];
-    }
-    fileMatrix<<"\n";
-  }
+  reco::GenParticleCollection::const_iterator partIt;
+  for(partIt=genParticles->begin();partIt!=genParticles->end();++partIt) {
+    std::cout<<" Particle Id="<<partIt->pdgId()<<std::endl;
+    if(partIt->pdgId()==-2000015){
+      //ABOUT ETA
+      //std::cout<<"\t\t Filling the histogram Eta="<<partIt->eta()<<std::endl;
+      //etaHisto->Fill(partIt->eta(),countHitsInRPC);
+      //soloEtaHisto->Fill(partIt->eta());
+      //if(hscp)soloEtaMyHisto->Fill(partIt->eta());
+      //ABOUT BETA
+      float p=partIt->p();
+      float e=partIt->energy();
+      float beta=p/e;
+      //betaHisto->Fill(beta);
+      //if(hscp)betaMyHisto->Fill(beta);
+      float pt = partIt->pt();
+      //float et = partIt->et();
+      float betaT=pt/e;
+      //betaTHisto->Fill(betaT);
+      //if(count!=0)bxLayerFile<<"\t"<<" eta="<<partIt->eta()<<" beta="<<beta<<"c";
+      //if(count!=0)fileMatrix<<" eta="<<partIt->eta()<<" beta="<<beta<<"c Event "<<iEvent.id().event()<<"\n";
+      //if(fabs(partIt->eta()>1.14))etaout++;
+      std::cout<<"\t phi="<<partIt->phi()<<" eta="<<partIt->eta()<<"beta="<<beta<<" p="<<p<<"GeV pt="<<pt<<"GeV m="<<sqrt(e*e-p*p)<<"GeV"<<std::endl;
+      
+      int event = iEvent.id().event();
+      float eta = partIt->eta();
+      float phi = partIt->phi();
+      
+      RPCHSCPCANDIDATE rpcCandidate(iEvent,iSetup);
 
-  //When we count endcaps this should work
-  //assert(counter==rpcHits->size());
- 
-  bool hscp = false;
-  int layersHited = 0;
-  std::cout<<"Cleaning array Maxbx"<<std::endl;
-  for(int i=0;i<7;i++)Maxbx[i]=0;
-  std::cout<<"Cleaned"<<std::endl;
+      expectedeta->Fill(eta);
+      expectedphi->Fill(phi);
+      expectedbeta->Fill(beta);
+      
+      if(rpcCandidate.found()){
+	std::cout<<" eta="<<eta<<" phi="<<phi<<std::endl;
+	std::cout<<" eta="<<rpcCandidate.eta()<<" phi="<<rpcCandidate.phi()<<std::endl;
+	
+	float diffeta = eta - rpcCandidate.eta();
+	float diffphi = phi - rpcCandidate.phi();
 
-  for(int lay=1;lay<7;lay++){//looping layers
-    bool anyhit = false;
-    int maxbx = 0;
-    for(int bxs=bxsitLimit-1;bxs>=0;bxs--){//looping bx
-      if(matrixbit[bxs][lay]!=0.){
-	//fileMatrix<<"Increasing "<<bxs<<" "<<lay<<" "<<matrixbit[bxs][lay]<<std::endl;
-	matrixHisto->Fill(bxs+(lay-1)*5,matrixbit[bxs][lay]);
-	if(matrixbit[bxs][lay]!=0){
-	  anyhit=true;
-	  if(maxbx<bxs)maxbx=bxs;
+	if(fabs(diffeta)<=0.3 && fabs(diffphi)<=0.03){
+	  std::cout<<"Coincidence!!"<<std::endl;
+	  observedeta->Fill(eta);
+	  observedphi->Fill(phi);
+	  observedbeta->Fill(beta);
+	  residualphi->Fill(diffphi);
+	  residualeta->Fill(diffeta);
+	}else{
+	  std::cout<<" Identified but in different direction"<<std::endl;
 	}
+      }else{
+	std::cout<<" eta="<<eta<<" phi="<<phi<<std::endl;
+	std::cout<<" Not identified"<<std::endl;
       }
-    }	
-    if(anyhit)layersHited++;
-    std::cout<<"Writing the Max array"<<" lay = "<<lay<<" maxbx = "<<maxbx<<std::endl;
-    Maxbx[lay]=maxbx;
-  }
-
-  fileMatrix<<"Number Of layers "<<layersHited<<std::endl;
-  std::cout<<"Number Of layers "<<layersHited<<std::endl;
-  
-  for(int i=1;i<7;i++)fileMatrix<<" L"<<i<<" "<<Maxbx[i];
-  fileMatrix<<"\n";
-
-  fileMatrix<<"pendpos ";
-  
-  int pendpos=0;
-  for(int i=1;i<6;i++){
-    if(Maxbx[i]<Maxbx[i+1]){
-      pendpos++;
     }
-    if(Maxbx[i]>Maxbx[i+1]){
-      pendpos--;
-    }
-    fileMatrix<<" "<<pendpos;
   }
-
-  float average=0;
-  for(int i=1;i<7;i++)average=average+(float)(Maxbx[i]);
-  average=average/6;
-  
-
-    
-  if(layersHited>=3&&(pendpos>0||average>=1))hscp=true;
-  
-  std::cout<<" pendpos= "<<pendpos<<" average= "<<average<<" boolean hscp= "<<hscp<<std::endl;
-  fileMatrix<<" pendpos= "<<pendpos<<" average= "<<average<<" boolean hscp= "<<hscp<<std::endl;
-  
-  if(hscp) totalHSCP++;
-
-
-
-/* This is an event example
-   //Read 'ExampleData' from the Event
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-
-   //Use the ExampleData to create an ExampleData2 which 
-   // is put into the Event
-   std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
-   iEvent.put(pOut);
-*/
-
-/* this is an EventSetup example
-   //Read SetupData from the SetupRecord in the EventSetup
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-*/
  
 }
 
@@ -269,24 +239,76 @@ RPCHSCP::beginJob(const edm::EventSetup&){
   theFile = new TFile(rootFileName.c_str(),"RECREATE");
   //matrixHisto = new TH1F("LayersandBX","Histogram 2D Layers and BX",6,0.5,6.5,4,-0.5,3.5);
   matrixHisto = new TH1F("LayersandBX","Histogram 2D Layers and BX",30,-0.5,29.5);
-  totalHSCP=0;
   
-  //for(int lay=0;lay<4;lay++){//looping layers
-  //  for(int bxs=0;bxs<7;bxs++){//looping bx
-  //    matrixbitTOTAL[bxs][lay]=0;
-  //  }
-  //}
+   efficiencyeta = new TH1F("EtaEff","Eta Efficiency",100,-2.5,2.5);		 
+   efficiencyphi = new TH1F("PhiEff","Phi Efficiency",100,-3.1415926,3.1415926); 
+  efficiencybeta = new TH1F("BetaEff","Beta Efficiency",100,0,1);                
+
+   expectedeta = new TH1F("EtaExpected","Eta Expected",100,-2.5,2.5);		 
+   expectedphi = new TH1F("PhiExpected","Phi Expected",100,-3.1415926,3.1415926); 
+  expectedbeta = new TH1F("BetaExpected","Beta Expected",100,0,1);                
+
+   observedeta = new TH1F("EtaObserved","Eta Observed",100,-2.5,2.5);		 
+   observedphi = new TH1F("PhiObserved","Phi Observed",100,-3.1415926,3.1415926); 
+  observedbeta = new TH1F("BetaObserved","Beta Observed",100,0,1);                
+
+  residualeta = new TH1F("Residual Eta","Eta Residuals",100,-0.2,0.2);
+  residualphi = new TH1F("Residual Phi","Phi Residuals",100,-0.02,0.02);
+
+  totalHSCP=0;
 
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
 RPCHSCP::endJob() {
-  std::cout<<"\n TOTAL HSPCs = "<<totalHSCP<<std::endl;
-  fileMatrix<<"\n TOTAL HSPCs = "<<totalHSCP<<std::endl;
+  for(int k=1;k<=100;k++){
+    float effeta = 0;
+    float erreta = 0;
+    if(expectedeta->GetBinContent(k)!=0){
+      effeta = observedeta->GetBinContent(k)/expectedeta->GetBinContent(k);
+      erreta = sqrt(effeta*(1-effeta)/expectedeta->GetBinContent(k));
+    }
+    efficiencyeta->SetBinContent(k,effeta);
+    efficiencyeta->SetBinError(k,erreta);
+    
+    float effbeta = 0;
+    float errbeta = 0;
+    if(expectedbeta->GetBinContent(k)!=0){
+      effbeta = observedbeta->GetBinContent(k)/expectedbeta->GetBinContent(k);
+      errbeta = sqrt(effbeta*(1-effbeta)/expectedbeta->GetBinContent(k));
+    }
+    efficiencybeta->SetBinContent(k,effbeta);
+    efficiencybeta->SetBinError(k,errbeta);
 
+    float effphi = 0;
+    float errphi = 0;
+    if(expectedphi->GetBinContent(k)!=0){
+      effphi = observedphi->GetBinContent(k)/expectedphi->GetBinContent(k);
+      errphi = sqrt(effphi*(1-effphi)/expectedphi->GetBinContent(k));
+    }
+    efficiencyphi->SetBinContent(k,effphi);
+    efficiencyphi->SetBinError(k,errphi);
+  }
+  
   theFile->cd();
   matrixHisto->Write();
+
+  efficiencyeta->Write();
+  efficiencyphi->Write();
+  efficiencybeta->Write();
+
+  expectedeta->Write();
+  expectedphi->Write();
+  expectedbeta->Write();
+
+  observedeta->Write();
+  observedphi->Write();
+  observedbeta->Write();
+
+  residualeta->Write();
+  residualphi->Write();
+
   theFile->Close();
   fileMatrix.close();
 }
