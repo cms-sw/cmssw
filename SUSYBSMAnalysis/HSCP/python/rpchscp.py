b4615fa5d0f091fde6c3d3a2286b5ba8a4976e5b
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2009-05-07 09:25:51 +0000

    configuration file for RPCHSCP

---
+++ SUSYBSMAnalysis/HSCP/python/rpchscp.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+process.load("Geometry.CMSCommonData.cmsSimIdealGeometryXML_cfi")
+
+process.load("MagneticField.Engine.volumeBasedMagneticField_cfi")
+
+process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
+
+process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
+
+process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
+
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/tmp/carrillo/kktau300GeVx1000WithRPC.root')
+)
+
+process.MessageLogger = cms.Service("MessageLogger")
+
+process.rpcHSCP = cms.EDFilter("RPCHSCP",
+    rootFileName = cms.untracked.string('hscp.root'),
+    fileMatrixname = cms.untracked.string('matrix.txt')
+)
+
+process.p = cms.Path(process.rpcHSCP)
+
