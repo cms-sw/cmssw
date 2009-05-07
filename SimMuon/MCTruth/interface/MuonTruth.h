25645a0ad564a6a55d5c4fb556ae7ecdf07fc99e
Giovanni Abbiendi <sha1-03ca615cb6d62d8479399a2c21824cf89dd4fac5@cern.ch>
2009-05-07 08:58:03 +0000

    fix a missing include (for backward compatibility)

--- SimMuon/MCTruth/interface/MuonTruth.h
+++ SimMuon/MCTruth/interface/MuonTruth.h
@@ -12,6 +12,7 @@
 #include "SimDataFormats/TrackerDigiSimLink/interface/StripDigiSimLink.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
+#include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 #include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
 #include "SimDataFormats/CrossingFrame/interface/MixCollection.h"
 #include "Geometry/CSCGeometry/interface/CSCGeometry.h"
