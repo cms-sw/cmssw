0b3d0d8f70af784a0de17429dd77a45dfb9da810
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2009-05-07 10:41:18 +0000

    Changing HB/HF SOI shifts to 0 (22X series)

--- L1Trigger/RegionalCaloTrigger/python/rctDigis_cfi.py
+++ L1Trigger/RegionalCaloTrigger/python/rctDigis_cfi.py
@@ -1,12 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 rctDigis = cms.EDProducer("L1RCTProducer",
-    hcalDigis = cms.VInputTag(cms.InputTag("hcalTriggerPrimitiveDigis")),
+    hcalDigisLabel = cms.InputTag("hcalTriggerPrimitiveDigis"),
     useDebugTpgScales = cms.bool(False),
+    useEcalCosmicTiming = cms.bool(False),
+    postSamples = cms.uint32(0),
+    preSamples = cms.uint32(0),
+    useHcalCosmicTiming = cms.bool(False),
     useEcal = cms.bool(True),
     useHcal = cms.bool(True),
-    ecalDigis = cms.VInputTag(cms.InputTag("ecalTriggerPrimitiveDigis")),
-    BunchCrossings = cms.vint32(0)                      
+    ecalDigisLabel = cms.InputTag("ecalTriggerPrimitiveDigis"),
+    UseMCAsInput = cms.bool(True),
+    HFShift = cms.int32(0),
+    HBShift = cms.int32(0)
 )
 
 
