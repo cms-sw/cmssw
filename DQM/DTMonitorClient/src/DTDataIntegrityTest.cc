93fbc6b4ed0fc1ef0f4dee9ae393b8da28243714
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-05-07 08:21:05 +0000

    Cleanup, remove TTSValue_percent histo

--- DQM/DTMonitorClient/src/DTDataIntegrityTest.cc
+++ DQM/DTMonitorClient/src/DTDataIntegrityTest.cc
@@ -2,8 +2,8 @@
 /*
  * \file DTDataIntegrityTest.cc
  * 
- * $Date: 2009/03/27 13:19:58 $
- * $Revision: 1.26 $
+ * $Date: 2009/03/27 13:22:21 $
+ * $Revision: 1.27 $
  * \author S. Bolognesi - CERN
  *
  */
@@ -151,49 +151,12 @@ void DTDataIntegrityTest::endLuminosityBlock(LuminosityBlock const& lumiSeg, Eve
     }
 
     string histoType;
-    //1D histo: % of tts values 
-    MonitorElement * tts_histo = dbe->get(getMEName("TTSValues",dduId));
-    if (tts_histo) {
-        LogTrace ("DTDQM|DTRawToDigi|DTMonitorClient|DTDataIntegrityTest") <<"[DTDataIntegrityTest]:histo DDUTTSValues found";
 
-	histoType = "TTSValues_Percent";   
-	if (dduHistos[histoType].find(dduId) == dduHistos[histoType].end()) {
-	  bookHistos(histoType,dduId);
-	} 
-	//Loop on possible tts values
-	for(int i=1;i<8;i++){
-	  (dduHistos.find(histoType)->second).find(dduId)->second->
-	    setBinContent(i,tts_histo->getBinContent(i)/tts_histo->getEntries());
-
-	  if(doTimeHisto){
-	    //Fill timing histos and set x label with luminosity block number
-	    if( dduVectorHistos["TTSVSTime"].find(dduId) == dduVectorHistos["TTSVSTime"].end() ){
-	      bookTimeHistos("TTSVSTime",dduId,nLumiSegs); 
-	    }
-	    (dduVectorHistos.find("TTSVSTime")->second).find(dduId)->second[i-1]->
-	      setBinContent(counter,tts_histo->getBinContent(i)/tts_histo->getEntries());
-	    (dduVectorHistos.find("TTSVSTime")->second).find(dduId)->second[i-1]->
-	      setBinLabel(counter, nLumiSegs_s.str(), 1);
-	  }
-	}
-
-	//Check if there are too many events with wrong tts value
-	double alert_tts1 = 0.5, alert_tts4 = 0.5, alert_tts20 = 0.5;
-	if((dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(2) > alert_tts1)
-	  LogWarning ("DTDQM|DTRawToDigi|DTMonitorClient|DTDataIntegrityTest") <<"[DTDataIntegrityTest]:WARNING: "<<
-	    (dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(2)<<" % events with warning overflow";
-
-   	if(((dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(1) +
-	    (dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(3)) > alert_tts20 )
-	  LogWarning ("DTDQM|DTRawToDigi|DTMonitorClient|DTDataIntegrityTest") <<"[DTDataIntegrityTest]:WARNING: "<<
-	    ((dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(1) +
-	     (dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(3))<<" % events with out of synch or disconnected";
-
-	if((dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(4) > alert_tts4)
-	  LogWarning ("DTDQM|DTRawToDigi|DTMonitorClient|DTDataIntegrityTest") <<"[DTDataIntegrityTest]:WARNING: "<<
-	    (dduHistos.find(histoType)->second).find(dduId)->second->getBinContent(4)<<" % events with busy";
-	//FIXME: how to notify this warning in a LogFile?
-         }
+//     //1D histo: % of tts values 
+//     MonitorElement * tts_histo = dbe->get(getMEName("TTSValues",dduId));
+//     if (tts_histo) {
+//       // FIXME: test?
+//     }
 
     //Check if the list of ROS is compatible with the channels enabled
     MonitorElement * ros_histo = dbe->get(getMEName("ROSStatus",dduId));
@@ -338,17 +301,6 @@ void DTDataIntegrityTest::bookHistos(string histoType, int dduId){
   dbe->setCurrentFolder("DT/00-DataIntegrity/FED" + dduId_s.str());
   string histoName;
 
-  if(histoType == "TTSValues_Percent"){
-    histoName = "FED" + dduId_s.str() + histoType;
-    (dduHistos[histoType])[dduId] = dbe->book1D(histoName,histoName,7,0,7);
-    ((dduHistos[histoType])[dduId])->setBinLabel(1,"disconnected",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(2,"warning overflow",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(3,"out of synch",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(4,"busy",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(5,"ready",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(6,"error",1);	
-    ((dduHistos[histoType])[dduId])->setBinLabel(7,"disconnected",1);	
-  }
 }
 
 void DTDataIntegrityTest::bookTimeHistos(string histoType, int dduId, int nLumiSegs){
