6caf75f5b5e0f243a68622df1720ab8b42fa7763
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-05-08 13:29:00 +0000

    Fix the TB configuration for the new additions to the EcalRecHit producer

--- RecoLocalCalo/EcalRecProducers/python/ecalTBSimRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalTBSimRecHit_cfi.py
@@ -7,5 +7,30 @@ ecalTBSimRecHit = cms.EDProducer("EcalRecHitProducer",
     EBuncalibRecHitCollection = cms.InputTag("ecalTBSimWeightUncalibRecHit","EcalUncalibRecHitsEB"),
     EBrechitCollection = cms.string('EcalRecHitsEB'),
     ChannelStatusToBeExcluded = cms.vint32(),
-    algo = cms.string("EcalRecHitWorkerSimple")
+    algo = cms.string("EcalRecHitWorkerSimple"),
+    # reco flags association to DB flag
+    # the vector index corresponds to the DB flag
+    # the value correspond to the reco flag
+    flagsMapDBReco = cms.vint32(
+             0,   0,   0,  0, # standard reco
+             4,               # faulty hardware (noisy)
+            -1,  -1,  -1,     # not yet assigned
+             4,   4,          # faulty hardware (fixed gain)
+             6,   6,   6,     # dead channel with trigger
+             7,               # dead FE
+             8                # dead or recovery failed
+            ),
+    # for channel recovery
+    algoRecover = cms.string("EcalRecHitWorkerRecover"),
+    recoverEBIsolatedChannels = cms.bool(False),
+    recoverEEIsolatedChannels = cms.bool(False),
+    recoverEBFE = cms.bool(False),
+    recoverEEFE = cms.bool(False),
+    ebDetIdToBeRecovered = cms.InputTag("ecalDetIdToBeRecovered:ebDetId"),
+    eeDetIdToBeRecovered = cms.InputTag("ecalDetIdToBeRecovered:eeDetId"),
+    ebFEToBeRecovered = cms.InputTag("ecalDetIdToBeRecovered:ebFE"),
+    eeFEToBeRecovered = cms.InputTag("ecalDetIdToBeRecovered:eeFE"),
+    singleChannelRecoveryMethod = cms.string("NeuralNetworks"),
+    singleChannelRecoveryThreshold = cms.double(0),
+    triggerPrimitiveDigiCollection = cms.InputTag("ecalDigis")
 )
