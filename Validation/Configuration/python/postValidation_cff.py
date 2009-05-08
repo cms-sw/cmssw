11458536965649b125249a589a29a39c29b452a1
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-05-08 09:46:27 +0000

    adapt the postValidation to the new muon sequence

--- Validation/Configuration/python/postValidation_cff.py
+++ Validation/Configuration/python/postValidation_cff.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoMuon.PostProcessor_cff import *
 from Validation.RecoTrack.PostProcessorTracker_cfi import *
+from Validation.MuonIsolation.PostProcessor_cff import *
 
-postValidation = cms.Sequence(recoMuonPostProcessorsWithIsolation+postProcessorTrack)
+postValidation = cms.Sequence(recoMuonPostProcessors+postProcessorTrack+MuIsoValPostProcessor)
 
 postValidation_pu = cms.Sequence(recoMuonPostProcessors+postProcessorTrack)
