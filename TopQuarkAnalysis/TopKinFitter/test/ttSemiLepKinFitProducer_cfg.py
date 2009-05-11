eda32724b806f110b37c23ea12c958be8f1a4c33
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-05-11 13:01:05 +0000

    include OutputModule and outpath

--- TopQuarkAnalysis/TopKinFitter/test/ttSemiLepKinFitProducer_cfg.py
+++ TopQuarkAnalysis/TopKinFitter/test/ttSemiLepKinFitProducer_cfg.py
@@ -17,7 +17,8 @@ process.MessageLogger.cerr.INFO = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
     '/store/relval/CMSSW_3_1_0_pre6/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0002/50D4BADB-FA32-DE11-BA01-000423D98DC4.root'    
-    )
+    ),
+     skipEvents = cms.untracked.uint32(0)                            
 )
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
@@ -36,6 +37,7 @@ process.GlobalTag.globaltag = cms.string('IDEAL_31X::All')
 
 ## std sequence for pat
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
 ## std sequence to produce the kinematic fit for semi-leptonic events
 process.load("TopQuarkAnalysis.TopKinFitter.TtSemiLepKinFitProducer_Muons_cfi")
 
@@ -43,3 +45,14 @@ process.load("TopQuarkAnalysis.TopKinFitter.TtSemiLepKinFitProducer_Muons_cfi")
 process.p = cms.Path(process.patDefaultSequence *
                      process.kinFitTtSemiLepEvent
                      )
+
+## configure output module
+process.out = cms.OutputModule("PoolOutputModule",
+    SelectEvents   = cms.untracked.PSet(SelectEvents = cms.vstring('p') ),                               
+    fileName = cms.untracked.string('ttSemiLepKinFitProducer.root'),
+    outputCommands = cms.untracked.vstring('drop *')
+)
+process.out.outputCommands += ['keep *_kinFitTtSemiLepEvent_*_*']
+
+## output path
+process.outpath = cms.EndPath(process.out)
