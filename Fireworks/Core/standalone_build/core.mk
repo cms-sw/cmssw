b150b8b6dfcb56fa349826c315e328324ba77be9
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2009-05-11 19:15:24 +0000

    Add variable for boost and root libraries to make *.mk files more easy to read.

--- Fireworks/Core/standalone_build/core.mk
+++ Fireworks/Core/standalone_build/core.mk
@@ -1,9 +1,13 @@
 ifeq ($(shell uname), Linux)
-   CoreLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38  CLHEP-1.9.4.2  gsl gslcblas HepMCfio HepMC pcre Cintex Cint Core  Net Tree TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex sigc-2.0 nsl crypt dl uuid
-   CoreIncludes :=  external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2-cms2/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms3/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/uuid/1.38-cms/include external/inc/zlib/1.2.3-cms/include
+   BoostLibs := boost_system-gcc34-mt-1_38 boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38  
+   RootLibs := Cintex Cint Core  Net Tree Treeplayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex
+   CoreLibs := CLHEP-1.9.4.2  gsl gslcblas HepMCfio HepMC pcre sigc-2.0 nsl crypt dl uuid
+   CoreIncludes := external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2-cms2/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms3/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0
 else 
    ifeq ($(shell uname), Darwin)
-      CoreLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38   boost_regex-gcc34-mt-1_38  CLHEP-1.9.3.2 gsl gslcblas HepMCfio HepMC TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cintex Cint Core Net Tree MathCore MathMore GenVector Reflex sigc-2.0 dl
-      CoreIncludes :=  external/inc/boost/1.38.0/inComclude external/inc/clhep/1.9.4.2-cms2/include external/inc/gsl/1.10/include external/inc/hepmc/2.03.06-cms3/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/uuid/1.38-cms/include external/inc/zlib/1.2.3-cms/include
+      BoostLibs := boost_system-xgcc40-mt-1_38 boost_thread-xgcc40-mt-1_38  boost_signals-xgcc40-mt-1_38  boost_filesystem-xgcc40-mt-1_38  boost_program_options-xgcc40-mt-1_38 boost_regex-xgcc40-mt-1_38 
+      RootLibs :=  RIO TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cintex Cint Core Net Tree MathCore MathMore GenVector Reflex 
+      CoreLibs :=  $(BoostLibs) $(RootLibs) CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC sigc-2.0.0 dl
+      CoreIncludes :=  external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2/include external/inc/gsl/1.10/include external/inc/hepmc/2.03.06/include external/inc/sigcpp/2.0.17/include/sigc++-2.0
    endif     
 endif
--- Fireworks/Core/standalone_build/project.mk
+++ Fireworks/Core/standalone_build/project.mk
@@ -1,9 +1,13 @@
-ifeq ($(shell uname), Linux)
-   ProjectLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38 CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC pcre Cint Core  Net Tree Hist Matrix MathCore MathMore Reflex sigc-2.0 nsl crypt dl z EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
-   ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/zlib/1.2.3-cms/include
+ifeq ($(shell uname), Linux) 
+   BoostLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38
+   RootLibs := Cint Core  Net Tree Hist Matrix MathCore MathMore Reflex EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
+   ProjectLibs := $(BoostLibs) $(RootLibs) CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC pcre  sigc-2.0 nsl crypt dl z 
+   ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0
 else 
    ifeq ($(shell uname), Darwin)
-      ProjectLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38 CLHEP-1.9.3.2 gsl gslcblas HepMCfio HepMC pcre TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cint Cintex Core Net Tree MathCore MathMore Reflex sigc-2.0 EG Eve FTGL Ged Geom GeomPainter Gui RGL Rint
-      ProjectIncludes :=  external/inc/boost/1.34.1-cms/include external/inc/clhep/1.9.3.2-cms/include external/inc/gsl/1.10/include external/inc/hepmc/2.01.10/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 external/inc/zlib/1.2.3-cms/include
+      BoostLibs := boost_system-xgcc40-mt-1_38 boost_thread-xgcc40-mt-1_38  boost_signals-xgcc40-mt-1_38  boost_filesystem-xgcc40-mt-1_38 boost_program_options-xgcc40-mt-1_38 boost_regex-xgcc40-mt-1_38 
+      RootLibs :=  Core RIO TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cintex Cint Core Net Tree MathCore MathMore GenVector Reflex
+      ProjectLibs := $(BoostLibs) $(RootLibs) CLHEP-1.9.4.2 gsl gslcblas Core RIO HepMCfio HepMC sigc-2.0.0 dl
+      ProjectIncludes :=  external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2/include external/inc/gsl/1.10/include external/inc/hepmc/2.03.06/include external/inc/sigcpp/2.0.17/include/sigc++-2.0
    endif     
 endif
