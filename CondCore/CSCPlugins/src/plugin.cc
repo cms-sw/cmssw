9f4845e2292a28cbc9e14cb9b7fe3bb7fbdc9a1e
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-05-11 11:15:37 +0000

    CSC DQM DCS record added

--- CondCore/CSCPlugins/src/plugin.cc
+++ CondCore/CSCPlugins/src/plugin.cc
@@ -43,6 +43,8 @@
 #include "CondFormats/DataRecord/interface/CSCReadoutMappingRcd.h"
 #include "CondFormats/CSCObjects/interface/CSCL1TPParameters.h"
 #include "CondFormats/DataRecord/interface/CSCL1TPParametersRcd.h"
+#include "CondFormats/CSCObjects/interface/CSCDQM_DCSData.h"
+#include "CondFormats/DataRecord/interface/CSCDCSDataRcd.h"
 
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(CSCPedestalsRcd,CSCPedestals);
@@ -63,3 +65,4 @@ REGISTER_PLUGIN(CSCBadWiresRcd,CSCBadWires);
 REGISTER_PLUGIN(CSCIdentifierRcd,CSCIdentifier);
 REGISTER_PLUGIN(CSCReadoutMappingRcd,CSCReadoutMapping);
 REGISTER_PLUGIN(CSCL1TPParametersRcd,CSCL1TPParameters);
+REGISTER_PLUGIN(CSCDCSDataRcd,  cscdqm::DCSData);
