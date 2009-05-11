6982ecf2ae53d52d7c5d0eabd4766d29b116b18b
Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch>
2009-05-11 15:22:42 +0000

    Small bug fix for the pixel DCS object definition.

--- CondFormats/DataRecord/interface/PixelDCSRcds.h
+++ CondFormats/DataRecord/interface/PixelDCSRcds.h
@@ -3,8 +3,26 @@
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 
-class PixelCaenChannelIsOnRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIsOnRcd> {};
-class PixelCaenChannelIMonRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIMonRcd> {};
-class PixelCaenChannelRcd : public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelRcd> {};
+class CaenChannel;
+
+template <class> class PixelDCSObject;
+
+struct PixelCaenChannelIsOnRcd:
+  public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIsOnRcd>
+{
+  typedef PixelDCSObject<bool> Object;
+};
+
+struct PixelCaenChannelIMonRcd:
+  public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelIMonRcd>
+{
+  typedef PixelDCSObject<float> Object;
+};
+
+struct PixelCaenChannelRcd:
+  public edm::eventsetup::EventSetupRecordImplementation<PixelCaenChannelRcd>
+{
+  typedef PixelDCSObject<CaenChannel> Object;
+};
 
 #endif
