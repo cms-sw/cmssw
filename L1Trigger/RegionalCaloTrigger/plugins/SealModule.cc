a61b114e3c8bddf07ef2191a28359d1de1daee4a
Kira Suzanne Grogg <sha1-94e78cd61007e7f8ab5cff2f29804efd4155bc88@cern.ch>
2009-05-11 14:44:35 +0000

    Added pattern test files

--- L1Trigger/RegionalCaloTrigger/plugins/SealModule.cc
+++ L1Trigger/RegionalCaloTrigger/plugins/SealModule.cc
@@ -2,19 +2,23 @@
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTProducer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTInputProducer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTTestAnalyzer.h"
+#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTPatternTestAnalyzer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTSaveInput.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTRelValAnalyzer.h"
 #include "L1Trigger/RegionalCaloTrigger/interface/L1RCTLutWriter.h"
-#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTTPGProvider.h"
-
+//#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTGenCalibrator.h"
+#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTFilter.h"
+#include "L1Trigger/RegionalCaloTrigger/interface/L1RCTPatternProducer.h"
 
 DEFINE_SEAL_MODULE();
+//DEFINE_ANOTHER_FWK_MODULE(L1RCTGenCalibrator);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTProducer);
-DEFINE_ANOTHER_FWK_MODULE(L1RCTTPGProvider);
-
 DEFINE_ANOTHER_FWK_MODULE(L1RCTInputProducer);
+DEFINE_ANOTHER_FWK_MODULE(L1RCTPatternTestAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTTestAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTSaveInput);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTRelValAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(L1RCTLutWriter);
+DEFINE_ANOTHER_FWK_MODULE(L1RCTFilter);
+DEFINE_ANOTHER_FWK_MODULE(L1RCTPatternProducer);
 
