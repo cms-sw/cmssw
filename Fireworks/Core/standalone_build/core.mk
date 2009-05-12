82128d29f6fe394c271efcef00fd84dd41b600bb
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2009-05-12 09:43:30 +0000

    Split include paths in new lines.

--- Fireworks/Core/standalone_build/core.mk
+++ Fireworks/Core/standalone_build/core.mk
@@ -1,13 +1,20 @@
 ifeq ($(shell uname), Linux)
-   BoostLibs := boost_system-gcc34-mt-1_38 boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38  
-   RootLibs := Cintex Cint Core  Net Tree Treeplayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex
+   BoostLibs :=  boost_thread-gcc34-mt-1_38  boost_signals-gcc34-mt-1_38  boost_filesystem-gcc34-mt-1_38  boost_program_options-gcc34-mt-1_38  boost_regex-gcc34-mt-1_38  
+   RootLibs := Cintex Cint Core  Net Tree TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Hist Matrix MathCore MathMore GenVector Minuit Minuit2 Physics Reflex
    CoreLibs :=   $(BoostLibs) $(RootLibs) CLHEP-1.9.4.2  gsl gslcblas HepMCfio HepMC pcre sigc-2.0 nsl crypt dl uuid
-   CoreIncludes := external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2-cms2/include external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms3/include external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0
+   
+   CoreIncludes := external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2-cms2/include 
+   CoreIncludes += external/inc/gsl/1.10-cms/include external/inc/hepmc/2.03.06-cms3/include
+   CoreIncludes += external/inc/pcre/4.4-cms/include external/inc/sigcpp/2.0.17-cms/include/sigc++-2.0 
+   CoreIncludes += external/inc/uuid/1.38-cms/include
 else 
    ifeq ($(shell uname), Darwin)
       BoostLibs := boost_system-xgcc40-mt-1_38 boost_thread-xgcc40-mt-1_38  boost_signals-xgcc40-mt-1_38  boost_filesystem-xgcc40-mt-1_38  boost_program_options-xgcc40-mt-1_38 boost_regex-xgcc40-mt-1_38 
       RootLibs :=  RIO TreePlayer Gpad Graf3d Graf Hist Matrix Physics Postscript Cintex Cint Core Net Tree MathCore MathMore GenVector Reflex 
       CoreLibs :=  $(BoostLibs) $(RootLibs) CLHEP-1.9.4.2 gsl gslcblas HepMCfio HepMC sigc-2.0.0 dl
-      CoreIncludes :=  external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2/include external/inc/gsl/1.10/include external/inc/hepmc/2.03.06/include external/inc/sigcpp/2.0.17/include/sigc++-2.0
+
+      CoreIncludes :=  external/inc/boost/1.38.0/include external/inc/clhep/1.9.4.2/include 
+      CoreIncludes +=  external/inc/gsl/1.10/include external/inc/hepmc/2.03.06/include
+      CoreIncludes += external/inc/sigcpp/2.0.17/include/sigc++-2.0
    endif     
 endif
