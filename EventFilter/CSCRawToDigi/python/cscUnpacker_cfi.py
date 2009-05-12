5ae16fea938ac5698ec95abec4aa73434d4d8933
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2009-05-12 16:45:45 +0000

    untarcked to tracked parameters

--- EventFilter/CSCRawToDigi/python/cscUnpacker_cfi.py
+++ EventFilter/CSCRawToDigi/python/cscUnpacker_cfi.py
@@ -1,26 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
-# This is a generic cfi file for CSC unpacking
+# This is the generic cfi file for CSC unpacking
 
 muonCSCDigis = cms.EDFilter("CSCDCCUnpacker",
-    PrintEventNumber = cms.untracked.bool(False),
-    # Use CSC examiner for corrupt or semi-corrupt data to avoid unpacker crashes
-    UseExaminer = cms.untracked.bool(True),
-    # This mask simply reduces error reporting
-    ErrorMask = cms.untracked.uint32(0x0),
     # Define input to the unpacker
-    #InputTag InputObjects = cscpacker:CSCRawData
     InputObjects = cms.InputTag("source"),
+    # Use CSC examiner to check for corrupt or semi-corrupt data & avoid unpacker crashes
+    UseExaminer = cms.bool(True),
     # This mask is needed by the examiner 
-    ExaminerMask = cms.untracked.uint32(0x1FEBF3F6),
-    # Unpack general status digis?
-    UnpackStatusDigis = cms.untracked.bool(False),
-    # Unpack FormatStatus digi?
-    UseFormatStatus = cms.untracked.bool(True),                        
+    ExaminerMask = cms.uint32(0x1FEBF3F6),
     # Use Examiner to unpack good chambers and skip only bad ones
-    UseSelectiveUnpacking = cms.untracked.bool(True),
+    UseSelectiveUnpacking = cms.bool(True),
+    # This mask simply reduces error reporting
+    ErrorMask = cms.uint32(0x0),
+    # Unpack general status digis?
+    UnpackStatusDigis = cms.bool(False),
+    # Unpack FormatStatus digi?
+    UseFormatStatus = cms.bool(True),                        
     # Turn on lots of output                            
-    Debug = cms.untracked.bool(False)
+    Debug = cms.untracked.bool(False),
+    PrintEventNumber = cms.untracked.bool(False)
 )
 
 
