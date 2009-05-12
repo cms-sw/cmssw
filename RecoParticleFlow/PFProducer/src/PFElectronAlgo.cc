5ae4b2a837e9a45447ce0b07f368339ce859b7e6
Daniele Benedetti <sha1-b78b7786b135946381de7085dafdd1b4e3149539@cern.ch>
2009-05-12 11:55:42 +0000

    bug fix multiple kf associated to the same gsf

--- RecoParticleFlow/PFProducer/src/PFElectronAlgo.cc
+++ RecoParticleFlow/PFProducer/src/PFElectronAlgo.cc
@@ -231,6 +231,7 @@ bool PFElectronAlgo::SetLinks(const reco::PFBlockRef&  blockRef,
 
       // Find Associated Kf Track elements and Ecal to KF elements
       unsigned int KfGsf_index = CutIndex;
+      unsigned int KfGsf_secondIndex = CutIndex; 
       std::multimap<double, unsigned int> kfElems;
       block.associatedElements( gsfIs[iEle],linkData,
 				kfElems,
@@ -247,6 +248,9 @@ bool PFElectronAlgo::SetLinks(const reco::PFBlockRef&  blockRef,
 				    ecalKfElems ,
 				    reco::PFBlockElement::ECAL,
 				    reco::PFBlock::LINKTEST_ALL );  
+	}
+	else {	  
+	  KfGsf_secondIndex = itkf->second;
 	}
       }
       
@@ -513,7 +517,11 @@ bool PFElectronAlgo::SetLinks(const reco::PFBlockRef&  blockRef,
       }
 
       EcalIndex.insert(EcalIndex.end(),GsfElemIndex.begin(),GsfElemIndex.end());
-      if(KfGsf_index < CutIndex) GsfElemIndex.push_back(KfGsf_index);
+      if(KfGsf_index < CutIndex) 
+	GsfElemIndex.push_back(KfGsf_index);
+      else if(KfGsf_secondIndex < CutIndex) 
+	GsfElemIndex.push_back(KfGsf_secondIndex);
+      
       GsfElemIndex.insert(GsfElemIndex.end(),keyBremIndex.begin(),keyBremIndex.end());
       associatedToGsf_.insert(pair<unsigned int, vector<unsigned int> >(gsfIs[iEle],GsfElemIndex));
       
