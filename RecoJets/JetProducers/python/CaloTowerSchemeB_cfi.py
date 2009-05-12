2ad29d58ca3ade8d5514d100321016c84802192a
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-05-12 19:59:47 +0000

    Change RecoJets/JetProducers/python/CaloTowerSchemeB_cfi.py to simply call the RecoLocalCalo/CaloTowersCreator/python/calotowersmaker_cfi.py and change the "useHO" boolean from "true" to "false". This will allow the HCAL and ECAL group to change parameters, and we will pick them up by default without having to change the config files.

--- RecoJets/JetProducers/python/CaloTowerSchemeB_cfi.py
+++ RecoJets/JetProducers/python/CaloTowerSchemeB_cfi.py
@@ -1,51 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-towerMaker = cms.EDFilter("CaloTowersCreator",
-    EBSumThreshold = cms.double(0.2),
-    HF2Weight = cms.double(1.0),
-    EBWeight = cms.double(1.0),
-    hfInput = cms.InputTag("hfreco"),
-    EESumThreshold = cms.double(0.45),
-    # Energy threshold for HO cell inclusion [GeV]
-    HOThreshold0 = cms.double(1.1),
-    HOThresholdPlus1 = cms.double(1.1),
-    HOThresholdMinus1 = cms.double(1.1),
-    HOThresholdPlus2 = cms.double(1.1),
-    HOThresholdMinus2 = cms.double(1.1),
-    HBThreshold = cms.double(0.9),
-    HF1Threshold = cms.double(1.2),
-    HEDWeight = cms.double(1.0),
-    EEWeight = cms.double(1.0),
-    UseHO = cms.bool(False),
-    HESWeight = cms.double(1.0),
-    HF1Weight = cms.double(1.0),
-    HOWeight = cms.double(1.0),
-    EBThreshold = cms.double(0.09),
-    hbheInput = cms.InputTag("hbhereco"),
-    HcalThreshold = cms.double(-1000.0),
-    HF2Threshold = cms.double(1.8),
-    EEThreshold = cms.double(0.45),
-    HESThreshold = cms.double(1.4),
-    hoInput = cms.InputTag("horeco"),
-    HEDThreshold = cms.double(1.4),
-    EcutTower = cms.double(-1000.0),
-    AllowMissingInputs = cms.untracked.bool(False),
-    ecalInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE")),
-    HBWeight = cms.double(1.0),
-    MomConstrMethod = cms.int32(1),
-    MomHBDepth = cms.double(0.2),
-    MomHEDepth = cms.double(0.4),
-    MomEBDepth = cms.double(0.3),
-    MomEEDepth = cms.double(0.0),
+from RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi import *
 
-# add new parameters for handling of anomalous cells
-# EXAMPLE 
-# 
-    # acceptable severity level
-    HcalAcceptSeverityLevel = cms.uint32(9),
-    EcalAcceptSeverityLevel = cms.uint32(1),
+towerMaker = calotowermaker.clone()
 
-    # use of recovered hits
-    UseHcalRecoveredHits = cms.bool(True),
-    UseEcalRecoveredHits = cms.bool(True)
-)
+towerMaker.UseHO = False
