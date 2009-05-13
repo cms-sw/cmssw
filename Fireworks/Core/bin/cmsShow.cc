3d922f8b34ab4e39af38823ceae9f439c4bd7177
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2009-05-13 00:16:10 +0000

    enable ROOT prompt by default

--- Fireworks/Core/bin/cmsShow.cc
+++ Fireworks/Core/bin/cmsShow.cc
@@ -19,11 +19,11 @@ int main (int argc, char **argv)
    char* dummyArgv[] = {"cmsShow"};
    int dummyArgc = 1;
    gEnv->SetValue("Gui.BackgroundColor", "#9f9f9f");
-#ifdef FW_ROOT_INTERACTIVE
-    TRint app("cmsShow", &dummyArgc, dummyArgv);
-#else
-    TApplication app("cmsShow", &dummyArgc, dummyArgv);
-#endif
+   //#ifdef FW_ROOT_INTERACTIVE
+   TRint app("cmsShow", &dummyArgc, dummyArgv);
+   //#else
+   //TApplication app("cmsShow", &dummyArgc, dummyArgv);
+   //#endif
    AutoLibraryLoader::enable();
    std::auto_ptr<CmsShowMain> pMain( new CmsShowMain(argc,argv) );
    app.Run();
