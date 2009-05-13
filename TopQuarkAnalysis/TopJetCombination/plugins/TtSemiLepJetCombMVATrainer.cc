eef91f660cf1409d649e5460b66b7b3b0cc3835a
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-05-13 11:59:17 +0000

    throw an exception if the vector from jet-parton matching is empty (this should not happen)

--- TopQuarkAnalysis/TopJetCombination/plugins/TtSemiLepJetCombMVATrainer.cc
+++ TopQuarkAnalysis/TopJetCombination/plugins/TtSemiLepJetCombMVATrainer.cc
@@ -80,6 +80,9 @@ TtSemiLepJetCombMVATrainer::analyze(const edm::Event& evt, const edm::EventSetup
   // get jet-parton matching if signal channel
   if(genEvt->semiLeptonicChannel() == leptonType_) {
     evt.getByLabel(matching_, matchingHandle);
+    if( matchingHandle->empty() )
+      throw cms::Exception("EmptyProduct")
+	<< "Empty vector from jet-parton matching. This should not happen! \n";
     matching = *(matchingHandle->begin());
     if(matching.size() < nPartons) return;
     // skip events that were affected by the outlier 
