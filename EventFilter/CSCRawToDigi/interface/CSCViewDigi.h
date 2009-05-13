43ecd346b1fe2f87fcfabb7a4f7615b2cc717588
Alexandre Sakharov <sha1-0bde2329cd157e445190ea2513ef0394244e73e7@cern.ch>
2009-05-13 15:20:23 +0000

    CSC digi dumper module

---
+++ EventFilter/CSCRawToDigi/interface/CSCViewDigi.h
@@ -0,0 +1,82 @@
+/// -*- C++ -*-
+//
+// Package:    CSCViewDigi
+// Class:      CSCViewDigi
+// 
+/**\class CSCViewDigi CSCViewDigi.h 
+
+Location: EventFilter/CSCRawToDigi/interface/ViewDigi.h
+
+Description: <one line class summary>
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Alexandre Sakharov
+//         Created:  Sun May 10 15:43:28 CEST 2009
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "EventFilter/CSCRawToDigi/interface/CSCALCTHeader.h"
+#include "DataFormats/CSCDigi/interface/CSCWireDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCRPCDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCALCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCCLCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCDCCFormatStatusDigiCollection.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+//#include <EventFilter/CSCRawToDigi/interface/CSCDCCUnpacker.h>
+//#include <iostream> CSCCorrelatedLCTDigi
+
+using std::endl;
+using std::cout;
+using std::string;
+
+//
+// class decleration
+//
+
+#ifndef SCViewDigi_H
+#define SCViewDigi_H
+
+class CSCViewDigi : public edm::EDAnalyzer {
+   public:
+      explicit CSCViewDigi(const edm::ParameterSet&);
+      ~CSCViewDigi();
+
+
+   private:
+      edm::InputTag wireDigiTag_;
+      edm::InputTag alctDigiTag_;
+      edm::InputTag clctDigiTag_;
+      edm::InputTag corrclctDigiTag_;
+      edm::InputTag stripDigiTag_;
+      edm::InputTag comparatorDigiTag_;
+      edm::InputTag rpcDigiTag_;
+      edm::InputTag statusDigiTag_;
+      bool WiresDigiDump, AlctDigiDump, ClctDigiDump, CorrClctDigiDump;
+      bool StripDigiDump, ComparatorDigiDump, RpcDigiDump, StatusDigiDump;
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+};
+
+#endif
+
--- EventFilter/CSCRawToDigi/plugins/module.cc
+++ EventFilter/CSCRawToDigi/plugins/module.cc
@@ -3,8 +3,10 @@
 #include <EventFilter/CSCRawToDigi/interface/DigiAnalyzer.h>
 #include <EventFilter/CSCRawToDigi/src/CSCDigiToRawModule.h>
 #include <EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h>
+#include <EventFilter/CSCRawToDigi/interface/CSCViewDigi.h>
 
 DEFINE_FWK_MODULE(CSCDCCUnpacker);
 DEFINE_ANOTHER_FWK_MODULE(DigiAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiToRawModule);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiToPattern);
+DEFINE_ANOTHER_FWK_MODULE(CSCViewDigi);
