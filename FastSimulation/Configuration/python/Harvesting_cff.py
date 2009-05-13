a9ab581ba456448e05b4b05e2a978fcf24f0c61c
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-05-13 14:14:30 +0000

    *** empty log message ***

--- FastSimulation/Configuration/python/Harvesting_cff.py
+++ FastSimulation/Configuration/python/Harvesting_cff.py
@@ -3,6 +3,6 @@ from HLTriggerOffline.Common.HLTValidationHarvest_cff import *
 
 from Validation.Configuration.postValidation_cff import *
 
-HarvestingFastSim = cms.Sequence(postvValidation_fastsim
+HarvestingFastSim = cms.Sequence(postValidation_fastsim
                                  + hltpostvalidation_fastsim)
 
