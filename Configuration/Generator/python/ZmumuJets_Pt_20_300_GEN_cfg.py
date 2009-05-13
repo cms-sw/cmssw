cd782455ad0585516c6b96a9e2b1a8d945073120
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2009-05-13 06:27:22 +0000

    correct decay

--- Configuration/Generator/python/ZmumuJets_Pt_20_300_GEN_cfg.py
+++ Configuration/Generator/python/ZmumuJets_Pt_20_300_GEN_cfg.py
@@ -30,9 +30,9 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
                                     'MDME(177,1)=0     !Z decay into c cbar',
                                     'MDME(178,1)=0     !Z decay into b bbar',
                                     'MDME(179,1)=0     !Z decay into t tbar',
-                                    'MDME(182,1)=1     !Z decay into e- e+',
+                                    'MDME(182,1)=0     !Z decay into e- e+',
                                     'MDME(183,1)=0     !Z decay into nu_e nu_ebar',
-                                    'MDME(184,1)=0     !Z decay into mu- mu+',
+                                    'MDME(184,1)=1     !Z decay into mu- mu+',
                                     'MDME(185,1)=0     !Z decay into nu_mu nu_mubar',
                                     'MDME(186,1)=0     !Z decay into tau- tau+',
                                     'MDME(187,1)=0     !Z decay into nu_tau nu_taubar',
@@ -47,7 +47,7 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
 )
 
 configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.2 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Generator/python/ZmumuJets_Pt_20_300_GEN_cfg.py,v $'),
     annotation = cms.untracked.string('ZmumuJets pt hat 20-300')
 )
