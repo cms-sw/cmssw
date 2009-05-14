df3a2ac364a5fef209c9ef8ef6f225f668b46d2c
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-05-14 15:54:44 +0000

    Removed independent module definition. Now cloning central calotowercreator module from RecoLocalCalo.  This will keep the module up to date.

--- RecoMET/METProducers/python/CaloTowersOpt_cfi.py
+++ RecoMET/METProducers/python/CaloTowersOpt_cfi.py
@@ -9,126 +9,16 @@ import FWCore.ParameterSet.Config as cms
 # Creates new calotowers with optimized Energy thresholds for MET.
 # === Modification: 09/30/08 by R. Remington
 # === Made modifications to accomodate changes to towerMaker (done by A. Oehler)
+# === Modification : 05/14/09 by R.Remington
+# === Now cloning the central calotowermaker module and changing parameters for optimized thresholds instead of writing another independent module   
 
-calotoweroptmaker = cms.EDProducer(
-    "CaloTowersCreator",
-    # Depth, fraction of the respective calorimeter [0,1]
-    MomEBDepth = cms.double(0.3),
-    MomEEDepth = cms.double(0.0),
-    MomHBDepth = cms.double(0.2),
-    MomHEDepth = cms.double(0.4),
+from RecoLocalCalo.CaloTowersCreator.calotowermaker_cfi import *
 
-  # Energy threshold for EB 5x5 crystal inclusion [GeV]
-    EBSumThreshold = cms.double(0.2),
-    # Weighting factor for HF short-fiber readouts
-    HF2Weight = cms.double(1.0),
-    # Weighting factor for EB   
-    EBWeight = cms.double(1.0),
-    # Label of HFRecHitCollection to use
-    hfInput = cms.InputTag("hfreco"),
-    # Energy threshold for EE crystals-in-tower inclusion [GeV]
-    EESumThreshold = cms.double(0.45),
-
-    ###### NEW PARAMETERS FOR 31X #########
-    
-    #Energy threshold for HO cell inclusion [GeV]
-    HOThreshold0 = cms.double(1.1),
-    HOThresholdPlus1 = cms.double(1.1),
-    HOThresholdMinus1 = cms.double(1.1),
-    HOThresholdPlus2 = cms.double(1.1),
-    HOThresholdMinus2 = cms.double(1.1),
-
-    HcalAcceptSeverityLevel = cms.uint32(9),
-    EcalAcceptSeverityLevel = cms.uint32(1),
-
-    # use of recovered hits
-    UseHcalRecoveredHits = cms.bool(True),
-    UseEcalRecoveredHits = cms.bool(True),
-
-    AllowMissingInputs = cms.bool(False),
-
-    # new parameters
-
-    # Flags specifying how to handle EE/EB RecHit thresholds
-    # (request by the JES group).
-    # Optoions:
-    # use Et (UseEtEXTreshold='True'), or E ('False')
-    # Use double-sided thresholds (|e|>threshold):
-    # UseSymEXTreshold='True';
-    # for e>threshold set to 'False'
-
-    UseEtEBTreshold = cms.bool(False),
-    UseSymEBTreshold = cms.bool(False),
-    UseEtEETreshold = cms.bool(False),
-    UseSymEETreshold = cms.bool(False),
-    
-
-    ##########################################
-
-    #Old HO Parameter ( To be discontinued in 31X )
-    HOThreshold = cms.double(0.5), 
-
-    HBGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    # Energy threshold for HB cell inclusion [GeV]
-    HBThreshold = cms.double(0.5),
-    EEWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    # Energy threshold for long-fiber HF readout inclusion [GeV]
-    HF1Threshold = cms.double(1.2),
-    HF2Weights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    HOWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    EEGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    # Weighting factor for HE 10-degree cells   
-    HEDWeight = cms.double(1.0),
-    # Method for momentum reconstruction
-    MomConstrMethod = cms.int32(0),
-    # Weighting factor for EE   
-    EEWeight = cms.double(1.0),
-    # HO on/off flag for tower energy reconstruction
-    UseHO = cms.bool(True),
-    HBWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    # Weighting factor for HE 5-degree cells   
-    HESWeight = cms.double(1.0),
-    # Weighting factor for HF long-fiber readouts 
-    HF1Weight = cms.double(1.0),
-    HF2Grid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    HEDWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    HF1Grid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    EBWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    # Weighting factor for HO 
-    HOWeight = cms.double(1.0),
-    # Energy threshold for EB crystal inclusion [GeV]
-    EBThreshold = cms.double(0.09),
-    # Label of HBHERecHitCollection to use
-    hbheInput = cms.InputTag("hbhereco"),
-    # Global energy threshold on Hcal [GeV]
-    HcalThreshold = cms.double(-1000.0),
-    # Energy threshold for short-fiber HF readout inclusion [GeV]
-    HF2Threshold = cms.double(1.8),
-    # Energy threshold for EE crystal inclusion [GeV]
-    EEThreshold = cms.double(0.45),
-    # Energy threshold for 5-degree (phi) HE cell inclusion [GeV]
-    HESThreshold = cms.double(0.7),
-    HF1Weights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    # Label of HORecHitCollection to use
-    hoInput = cms.InputTag("horeco"),
-    HESGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    #
-    HESWeights = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0),
-    # Energy threshold for 10-degree (phi) HE cel inclusion [GeV]
-    HEDThreshold = cms.double(0.5),
-    #
-    # Global energy threshold on tower [GeV]
-    EcutTower = cms.double(-1000.0),
-    HEDGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    # Label of EcalRecHitCollections to use
-    ecalInputs = cms.VInputTag(cms.InputTag("ecalRecHit","EcalRecHitsEB"), cms.InputTag("ecalRecHit","EcalRecHitsEE")),
-    # Weighting factor for HB   
-    HBWeight = cms.double(1.0),
-    HOGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0),
-    # Energy dependent weights and energy scale to be used
-    EBGrid = cms.vdouble(-1.0, 1.0, 10.0, 100.0, 1000.0)
-)
-
+calotoweroptmaker = calotowermaker.clone() 
+calotoweroptmaker.UseHO = False
+calotoweroptmaker.HBThreshold = cms.double(0.5)
+calotoweroptmaker.HESThreshold = cms.double(0.7)
+calotoweroptmaker.HEDThreshold = cms.double(0.5)
 
 calotoweroptmakerWithHO = calotoweroptmaker.clone()
 calotoweroptmakerWithHO.UseHO = True
