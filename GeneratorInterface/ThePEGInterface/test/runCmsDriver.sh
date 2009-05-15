2e4ebcd81f9c69ade806a50c2e7c74752e43354b
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-05-15 23:00:13 +0000

    Remove obsolete source and producer.

--- GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
+++ GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
@@ -7,8 +7,6 @@ CONDITIONS="FrontierConditions_GlobalTag,IDEAL_31X::All"
 CMSOPTS="-n 100 --eventcontent RAWSIM --conditions $CONDITIONS $CMSOPTS --no_exec --mc --customise=$MYPATH/custom"
 
 cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN:ProductionFilterSequence --datatier GEN $CMSOPTS
-cmsDriver.py "$MYPATH/testThePEGProducer"        -s GEN:ProductionFilterSequence --datatier GEN $CMSOPTS
-cmsDriver.py "$MYPATH/testThePEGSource"          -s GEN --datatier GEN $CMSOPTS
 
 cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN:ProductionFilterSequence --datatier GEN $CMSOPTS
 cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN:ProductionFilterSequence,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN $CMSOPTS
