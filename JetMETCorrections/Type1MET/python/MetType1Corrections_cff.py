367193ee578d1f2ddcb292c2922f7dcde6c581cd
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-05-15 12:43:18 +0000

    Added JES MET Corrections using KT and Siscone Jet algorithms. Removed midpoint jet corrections.

--- JetMETCorrections/Type1MET/python/MetType1Corrections_cff.py
+++ JetMETCorrections/Type1MET/python/MetType1Corrections_cff.py
@@ -4,33 +4,37 @@ import FWCore.ParameterSet.Config as cms
 # Author: R. Cavanaugh
 # Date: 08.08.2006
 #
-# Met corrections for the icone5, mcone5, and mcone7 MC corrected jets.
-corMetType1Icone5 = cms.EDFilter("Type1MET",
-    inputUncorJetsLabel = cms.string('iterativeCone5CaloJets'),
-    jetEMfracLimit = cms.double(0.9),
-    metType = cms.string('CaloMET'),
-    jetPTthreshold = cms.double(20.0),
-    inputUncorMetLabel = cms.string('met'),
-    corrector = cms.string('MCJetCorrectorIcone5')
-)
+# Updated:  Added modules for MET corrections with KT, Siscone jet algorithms
 
-corMetType1Mcone5 = cms.EDFilter("Type1MET",
-    inputUncorJetsLabel = cms.string('midPointCone5CaloJets'),
-    jetEMfracLimit = cms.double(0.9),
-    metType = cms.string('CaloMET'),
-    jetPTthreshold = cms.double(20.0),
-    inputUncorMetLabel = cms.string('met'),
-    corrector = cms.string('MCJetCorrectorMcone5')
-)
+metJESCorIC5CaloJet = cms.EDProducer("Type1MET",
+                                   inputUncorJetsLabel = cms.string('iterativeCone5CaloJets'),
+                                   jetEMfracLimit = cms.double(0.9),
+                                   metType = cms.string('CaloMET'),
+                                   jetPTthreshold = cms.double(20.0),
+                                   inputUncorMetLabel = cms.string('met'),
+                                   corrector = cms.string('MCJetCorrectorIcone5')
+                                   )
 
-corMetType1Mcone7 = cms.EDFilter("Type1MET",
-    inputUncorJetsLabel = cms.string('midPointCone7CaloJets'),
-    jetEMfracLimit = cms.double(0.9),
-    metType = cms.string('CaloMET'),
-    jetPTthreshold = cms.double(20.0),
-    inputUncorMetLabel = cms.string('met'),
-    corrector = cms.string('MCJetCorrectorMcone7')
-)
+metJESCorKT4CaloJet = metJESCorIC5CaloJet.clone()
+metJESCorKT4CaloJet.inputUncorJetsLabel = "kt4CaloJets"
+metJESCorKT4CaloJet.corrector           = "L2L3JetCorrectorKT4Calo"
 
-MetType1Corrections = cms.Sequence(corMetType1Icone5*corMetType1Mcone5*corMetType1Mcone7)
+metJESCorKT6CaloJet = metJESCorIC5CaloJet.clone()
+metJESCorKT6CaloJet.inputUncorJetsLabel = "kt6CaloJets"
+metJESCorKT6CaloJet.corrector           = "L2L3JetCorrectorKT6Calo"
 
+metJESCorSC5CaloJet = metJESCorIC5CaloJet.clone()
+metJESCorSC5CaloJet.inputUncorJetsLabel = "sisCone5CaloJets"
+metJESCorSC5CaloJet.inputUncorJetsLabel = "L2L3JetCorrectorSC5Calo"
+
+metJESCorSC7CaloJet = metJESCorIC5CaloJet.clone()
+metJESCorSC7CaloJet.inputUncorJetsLabel = "sisCone7CaloJets"
+metJESCorSC7CaloJet.inputUncorJetsLabel = "L2L3JetCorrectorSC7Calo"
+
+#MetType1Corrections = cms.Sequence(corMetType1Icone5*corMetType1Mcone5*corMetType1Mcone7)
+
+MetType1Corrections = cms.Sequence( metJESCorIC5CaloJet*
+                                    metJESCorKT4CaloJet*
+                                    metJESCorKT6CaloJet*
+                                    metJESCorSC5CaloJet*
+                                    metJESCorSC7CaloJet)
