6f26f40ca92b81889383a0d1b9d5910108839a28
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2009-05-16 10:59:03 +0000

    Adapt to 310pre7

--- Validation/RecoParticleFlow/test/launch_QCDForPF_FastSim.sh
+++ Validation/RecoParticleFlow/test/launch_QCDForPF_FastSim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-castorDir=/castor/cern.ch/user/p/pjanot/CMSSW310pre6/
-cmsswDir=/afs/cern.ch/user/p/pjanot/scratch0/CMSSW_3_1_0_pre6/src
+castorDir=/castor/cern.ch/user/p/pjanot/CMSSW310pre7/
+cmsswDir=/afs/cern.ch/user/p/pjanot/scratch0/CMSSW_3_1_0_pre7/src
 
 for ((job=0;job<=10;job++));
   do
