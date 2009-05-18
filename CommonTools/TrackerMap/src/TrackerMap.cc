a9e78806314816d7712f2bcac3b4bfcc23ea2144
Giuseppe Zito <sha1-7380d6786446b2a2af7dbb0310b9de8281c37b4e@cern.ch>
2009-05-18 16:56:50 +0000

    corrected bug in getcolor

--- CommonTools/TrackerMap/src/TrackerMap.cc
+++ CommonTools/TrackerMap/src/TrackerMap.cc
@@ -1099,9 +1099,9 @@ int TrackerMap::getcolor(float value,int palette){
    if(palette==1){//palette1 1 - raibow
    float delta=(maxvalue-minvalue);
    float x =(value-minvalue);
-   if(x<minvalue){red=0;green=0;blue=255;}
-   if(x>maxvalue){red=255;green=0;blue=0;}
-   if(x>=minvalue&&x<=maxvalue){ 
+   if(value<minvalue){red=0;green=0;blue=255;}
+   if(value>maxvalue){red=255;green=0;blue=0;}
+   if(value>=minvalue&&value<=maxvalue){ 
    red = (int) ( x<(delta/2) ? 0 : ( x > ((3./4.)*delta) ?  255 : 255/(delta/4) * (x-(2./4.)*delta)  ) );
    green= (int) ( x<delta/4 ? (x*255/(delta/4)) : ( x > ((3./4.)*delta) ?  255-255/(delta/4) *(x-(3./4.)*delta) : 255 ) );
    blue = (int) ( x<delta/4 ? 255 : ( x > ((1./2.)*delta) ?  0 : 255-255/(delta/4) * (x-(1./4.)*delta) ) );
