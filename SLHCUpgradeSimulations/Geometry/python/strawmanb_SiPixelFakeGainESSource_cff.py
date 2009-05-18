b8e62eee3d149c0c5b9578802cf87dc3ca9748d7
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-05-18 21:48:21 +0000

    remove obsolete files

--- SLHCUpgradeSimulations/Geometry/python/strawmanb_SiPixelFakeGainESSource_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CalibTracker.SiPixelESProducers.SiPixelFakeGainESSource_cfi import *
-
-SiPixelFakeGainESSource.file = cms.FileInPath(
-"SLHCUpgradeSimulations/Geometry/data/strawmanb/PixelSkimmedGeometry.txt"
-)
