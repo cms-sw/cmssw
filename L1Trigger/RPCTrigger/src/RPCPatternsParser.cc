3600595e01f71cfe5db0e4751c44052f57031d0f
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2009-05-18 15:37:46 +0000

    Follow changes in L1TObject/RPCPattern.h

--- L1Trigger/RPCTrigger/src/RPCPatternsParser.cc
+++ L1Trigger/RPCTrigger/src/RPCPatternsParser.cc
@@ -158,7 +158,10 @@ void RPCPatternsParser::startElement(const XMLCh* const uri,
     
     
     quality.m_QualityTabNumber = rpcconst.stringToInt(xMLCh2String(attrs.getValue(Char2XMLCh("id"))));
-    quality.m_FiredPlanes = xMLCh2String(attrs.getValue(Char2XMLCh("planes")));
+    std::bitset<8> firedPl( xMLCh2String(attrs.getValue(Char2XMLCh("planes")) )) ;
+    unsigned long fpUL = firedPl.to_ulong();
+    quality.m_FiredPlanes = (unsigned char) (fpUL & 0xFF );
+    //quality.m_FiredPlanes = xMLCh2String(attrs.getValue(Char2XMLCh("planes")));
     quality.m_QualityValue = rpcconst.stringToInt(xMLCh2String(attrs.getValue(Char2XMLCh("val"))));
 
     m_QualityVec.push_back(quality);
