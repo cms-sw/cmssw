fe09d603ce907db0c46745ee8c72b60ffad33eed
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-05-19 14:57:31 +0000

    use electrons from filtered GsfElectrons collection

--- RecoBTag/SoftLepton/python/softElectronTagInfos_cfi.py
+++ RecoBTag/SoftLepton/python/softElectronTagInfos_cfi.py
@@ -4,7 +4,7 @@ import RecoBTag.SoftLepton.muonSelection
 # SoftLeptonTagInfo producer for tagging caloJets with dedicated "soft" electrons
 softElectronTagInfos = cms.EDFilter("SoftLepton",
     jets = cms.InputTag("iterativeCone5CaloJets"),
-    leptons = cms.InputTag("btagSoftElectrons"),
+    leptons = cms.InputTag("softElectronSelector"),
     primaryVertex = cms.InputTag("offlinePrimaryVertices"),
 
     refineJetAxis = cms.uint32(0),          # use calorimetric jet direction by default
