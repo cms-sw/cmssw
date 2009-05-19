e2bddf4bf44671c81a350698aa2b8c7a9bda28fe
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-05-19 21:58:24 +0000

    Re-added photon paths to validation

--- HLTriggerOffline/Egamma/python/EgammaValidation_cff.py
+++ HLTriggerOffline/Egamma/python/EgammaValidation_cff.py
@@ -27,14 +27,15 @@ samples.num   = [1,
 
 paths.Wenu = ['HLT_Ele10_LW_L1RDQM',
               'HLT_Ele15_SW_L1RDQM',
-              'HLT_Ele10_LW_EleId_L1RDQM'
-              ]
+              'HLT_Ele10_LW_EleId_L1RDQM']
 
 paths.Zee = paths.Wenu + ['HLT_DoubleEle5_SW_L1RDQM']
 
-paths.GammaJet = ['HLT_Photon15_TrackIso_L1R_DQM']
+paths.GammaJet = ['HLT_Photon10_L1R_DQM',
+                  'HLT_Photon15_TrackIso_L1R_DQM',
+                  'HLT_Photon15_LooseEcalIso_L1R_DQM']
 
-paths.DiGamma  = paths.GammaJet 
+paths.DiGamma  = ['HLT_Photon10_L1R_DQM','HLT_DoublePhoton10_L1R_DQM']
 
 
 ##########################################################
