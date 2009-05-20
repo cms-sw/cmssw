33b276080e1a5f671b6ff993b585ef63f0c62126
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-05-20 14:22:42 +0000

    Set the standard simulation sequence without Hector transport Update the SimExtended sequence (cuts already included in g4SimHits as default)

--- Configuration/StandardSequences/python/SimExtended_cff.py
+++ Configuration/StandardSequences/python/SimExtended_cff.py
@@ -6,17 +6,8 @@ from SimTransport.HectorProducer.HectorTransportZDC_cfi import *
 # CMSSW/Geant4 interface
 from SimG4Core.Configuration.SimG4Core_cff import *
 
+# use Hector output instead of the generator one
 g4SimHits.Generator.HepMCProductLabel = cms.string('LHCTransport')
-
-# kill particles in CASTOR as default until CASTOR simulation is not fully operational in standard production
-
-g4SimHits.StackingAction.MaxTimeNames = cms.vstring('ZDCRegion','CastorRegion','QuadRegion')
-g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0,0.,0.)
-g4SimHits.SteppingAction.MaxTimeNames = cms.vstring('ZDCRegion','CastorRegion','QuadRegion')
-g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0,0.,0.)
-g4SimHits.CaloSD.HCNames = cms.vstring('EcalHitsEB','EcalHitsEE','EcalHitsES','HcalHits','ZDCHITS')
-g4SimHits.CaloSD.EminHits = cms.vdouble(0.015,0.010,0.0,0.0,0.0)
-g4SimHits.CaloSD.TmaxHits = cms.vdouble(500.0,500.0,500.0,500.0,2000.0)
 
 psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*LHCTransport*g4SimHits)
 
--- Configuration/StandardSequences/python/Sim_cff.py
+++ Configuration/StandardSequences/python/Sim_cff.py
@@ -1,11 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 # Hector transport of particles along the beam pipe for very forward detectors
-from SimTransport.HectorProducer.HectorTransportZDC_cfi import *
+# from SimTransport.HectorProducer.HectorTransportZDC_cfi import *
 
 # CMSSW/Geant4 interface
 from SimG4Core.Configuration.SimG4Core_cff import *
 
-psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*LHCTransport*g4SimHits)
+#psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*LHCTransport*g4SimHits)
+psim = cms.Sequence(cms.SequencePlaceholder("randomEngineStateProducer")*g4SimHits)
 
 
