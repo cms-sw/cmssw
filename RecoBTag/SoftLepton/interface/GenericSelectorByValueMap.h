b9c2eee6cfed5f08ce496f1b42b0af61405e43b1
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-05-20 14:23:19 +0000

    use RefToBase instead of Ptr to work around bug https://hypernews.cern.ch/HyperNews/CMS/get/edmFramework/1868.html

--- RecoBTag/SoftLepton/interface/GenericSelectorByValueMap.h
+++ RecoBTag/SoftLepton/interface/GenericSelectorByValueMap.h
@@ -12,23 +12,45 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 
-template <typename T>
+namespace edm {
+
+namespace details {
+
+  // which type should be used in edm::ParameterSet::getParameter<_> to read a parameter compatible with T ?
+
+  // most types can use themselves
+  template <typename C>
+  struct CompatibleConfigurationType {
+    typedef C type;
+  };
+
+  // "float" is not allowed, as it conflicts with "double"
+  template <>
+  struct CompatibleConfigurationType<float> {
+    typedef double type;
+  };
+
+} // namespace details;
+
+template <typename T, typename C>
 class GenericSelectorByValueMap : public edm::EDProducer {
 public:
-
   explicit GenericSelectorByValueMap(edm::ParameterSet const & config);
-  ~GenericSelectorByValueMap();
 
 private:
+  typedef T candidate_type;
+  typedef C selection_type;
+  typedef typename details::template CompatibleConfigurationType<selection_type>::type cut_type;
 
   void produce(edm::Event & event, edm::EventSetup const & setup);
 
   edm::InputTag m_electrons;
   edm::InputTag m_selection;
 
-  double m_cut;
+  cut_type m_cut;
 };
 
+} // namespace edm;
 
 //------------------------------------------------------------------------------
 
@@ -40,44 +62,41 @@ private:
 #include "DataFormats/Common/interface/View.h"
 #include "DataFormats/Common/interface/Ptr.h"
 #include "DataFormats/Common/interface/PtrVector.h"
+#include "DataFormats/Common/interface/RefToBaseVector.h"
 
 //------------------------------------------------------------------------------
 
-template <typename T>
-GenericSelectorByValueMap<T>::GenericSelectorByValueMap<T>(edm::ParameterSet const & config) :
+namespace edm {
+
+template <typename T, typename C>
+GenericSelectorByValueMap<T,C>::GenericSelectorByValueMap<T,C>(edm::ParameterSet const & config) :
   m_electrons(config.getParameter<edm::InputTag>("input")),
   m_selection(config.getParameter<edm::InputTag>("selection")),
-  m_cut(config.getParameter<double>("cut"))
+  m_cut(config.getParameter<cut_type>("cut"))
 {
   // register the product
-  produces<edm::PtrVector<T> >();
+  produces<edm::RefToBaseVector<candidate_type> >();
 }
 
 //------------------------------------------------------------------------------
 
-template <typename T>
-GenericSelectorByValueMap<T>::~GenericSelectorByValueMap<T>() {
-}
-
-//------------------------------------------------------------------------------
-
-template <typename T>
-void GenericSelectorByValueMap<T>::produce(edm::Event & event, const edm::EventSetup & setup)
+template <typename T, typename C>
+void GenericSelectorByValueMap<T, C>::produce(edm::Event & event, const edm::EventSetup & setup)
 {
-  std::auto_ptr<edm::PtrVector<T> > candidates(new edm::PtrVector<T>());
+  std::auto_ptr<edm::RefToBaseVector<candidate_type> > candidates(new edm::RefToBaseVector<candidate_type>());
 
   // read the collection of GsfElectrons from the Event
-  edm::Handle<edm::View<T> > h_electrons;
+  edm::Handle<edm::View<candidate_type> > h_electrons;
   event.getByLabel(m_electrons, h_electrons);
-  edm::View<T> const & electrons = * h_electrons;
+  edm::View<candidate_type> const & electrons = * h_electrons;
 
   // read the selection map from the Event
-  edm::Handle<edm::ValueMap<float> > h_selection;
+  edm::Handle<edm::ValueMap<selection_type> > h_selection;
   event.getByLabel(m_selection, h_selection);
-  edm::ValueMap<float> const & selectionMap = * h_selection;
+  edm::ValueMap<selection_type> const & selectionMap = * h_selection;
  
   for (unsigned int i = 0; i < electrons.size(); ++i) {
-    edm::Ptr<T> ptr = electrons.ptrAt(i);
+    edm::RefToBase<candidate_type> ptr = electrons.refAt(i);
     if (selectionMap[ptr] > m_cut)
      candidates->push_back(ptr); 
   }
@@ -85,5 +104,7 @@ void GenericSelectorByValueMap<T>::produce(edm::Event & event, const edm::EventS
   // put the product in the event
   event.put(candidates);
 }
+
+} // namespace edm;
 
 #endif // GenericSelectorByValueMap_h
