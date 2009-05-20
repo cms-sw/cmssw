c0aaea1b6c11ee92ea24f4bc36400b458e0b3d5a
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-05-20 14:26:02 +0000

    use a (more) generic selector

--- RecoBTag/SoftLepton/plugins/GsfElectronSelector.cc
+++ RecoBTag/SoftLepton/plugins/GsfElectronSelector.cc
@@ -4,7 +4,8 @@
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "RecoBTag/SoftLepton/interface/GenericSelectorByValueMap.h"
 
-typedef GenericSelectorByValueMap<reco::GsfElectron> GsfElectronSelector;
+// "float" is the type stored in the ValueMap
+typedef edm::GenericSelectorByValueMap<reco::GsfElectron, float> GsfElectronSelector;
 
 //------------------------------------------------------------------------------
 
--- RecoBTag/SoftLepton/python/softElectronSelector_cfi.py
+++ RecoBTag/SoftLepton/python/softElectronSelector_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # select a subset of the GsfElectron collection based on the quality stored in a ValueMap
 softElectronSelector = cms.EDProducer("GsfElectronSelector",
-    electrons = cms.InputTag('gsfElectrons'),
+    input     = cms.InputTag('gsfElectrons'),
     selection = cms.InputTag('eidLoose'),
     cut       = cms.double(0.5)
 )
