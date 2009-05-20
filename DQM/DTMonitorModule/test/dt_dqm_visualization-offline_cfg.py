3034df29c2b30eed6ffde538ced3985684d79d3d
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-05-20 15:11:30 +0000

    Add geometry cfi in top level cfg

--- DQM/DTMonitorModule/test/dt_dqm_visualization-offline_cfg.py
+++ DQM/DTMonitorModule/test/dt_dqm_visualization-offline_cfg.py
@@ -5,9 +5,7 @@ process = cms.Process("VisDT")
 # the source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/data/CRUZET3/Cosmics/RAW/v1/000/050/781/986D05EF-D44C-DD11-8F6C-000423D985E4.root',
-    '/store/data/CRUZET3/Cosmics/RAW/v1/000/050/781/B284117B-CA4C-DD11-B34D-000423D9939C.root',
-    '/store/data/CRUZET3/Cosmics/RAW/v1/000/050/781/C0D9C572-CA4C-DD11-AAE5-000423D98804.root'
+    '/store/data/Commissioning09/Cosmics/RAW/v1/000/084/189/46C061A8-2E3B-DE11-8608-001D09F24FEC.root'
     ))
 
 
@@ -18,7 +16,7 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
     )
 
-
+process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("DQM.DTMonitorModule.dt_dqm_visualization_common_offline_cff")
 #process.load("Configuration.StandardSequences.MagneticField_cff")
 
