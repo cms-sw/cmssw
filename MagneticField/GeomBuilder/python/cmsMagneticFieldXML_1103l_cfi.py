ed0c4311b44026681bcde32491ceae451944b1df
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2009-05-20 10:12:32 +0000

    turn error into warning

--- MagneticField/GeomBuilder/python/cmsMagneticFieldXML_1103l_cfi.py
+++ MagneticField/GeomBuilder/python/cmsMagneticFieldXML_1103l_cfi.py
@@ -1,6 +1,6 @@
 import sys
 print "MagneticField/GeomBuilder/data/cmsMagneticField_1103l.cfi is obsolete and will soon be removed."
-print "Now exiting with -1 error. \nplease remove inclusion of this file and try again.\n"
-sys.exit(-1);
+print "\nplease remove inclusion of this file and try again.\n"
+#sys.exit(-1);
 
 
