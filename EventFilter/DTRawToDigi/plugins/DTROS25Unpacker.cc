e26c771223cc2a35987051514cd6d4e6a9049ba6
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-05-20 17:51:13 +0000

    back to 31X LocalTrigger format

--- EventFilter/DTRawToDigi/plugins/DTROS25Unpacker.cc
+++ EventFilter/DTRawToDigi/plugins/DTROS25Unpacker.cc
@@ -1,7 +1,7 @@
 /** \file
  *
- *  $Date: 2009/05/14 13:36:38 $
- *  $Revision: 1.13 $
+ *  $Date: 2009/05/20 16:40:08 $
+ *  $Revision: 1.14 $
  *  \author  M. Zanetti - INFN Padova
  *  \revision FRC 060906
  */
@@ -346,7 +346,7 @@ void DTROS25Unpacker::interpretRawData(const unsigned int* index, int datasize,
 			else                      SCstation = 3;
 
 			// construct localtrigger for first station of this "group" ...
-			DTLocalTrigger localtrigger(bx_counter,(scDataWord.SCData()) & 0xFF);
+			DTLocalTrigger localtrigger(scEventBX, bx_counter,(scDataWord.SCData()) & 0xFF);
 			// ... and commit it to the event
 			DTChamberId chamberId (SCwheel,SCstation,SCsector);
 			triggerProduct->insertDigi(chamberId,localtrigger);
@@ -360,7 +360,7 @@ void DTROS25Unpacker::interpretRawData(const unsigned int* index, int datasize,
 			else                      SCstation = 4;
 
 			// construct localtrigger for second station of this "group" ...
-			DTLocalTrigger localtrigger(bx_counter,(scDataWord.SCData()) >> 8);
+			DTLocalTrigger localtrigger(scEventBX, bx_counter,(scDataWord.SCData()) >> 8);
 			// ... and commit it to the event
 			DTChamberId chamberId (SCwheel,SCstation,SCsector);
 			triggerProduct->insertDigi(chamberId,localtrigger);
