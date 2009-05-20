7a24d437fe42241ffe16e07db915cf80f20e6950
William Badgett Jr <sha1-0342ea9cbf4e6781ff6fc4596018aec98179df76@cern.ch>
2009-05-20 14:45:36 +0000

    Add L1AcceptBunchCrossing and L1AcceptBunchCrossingCollection

--- EventFilter/ScalersRawToDigi/python/Scalers_EventContent_cff.py
+++ EventFilter/ScalersRawToDigi/python/Scalers_EventContent_cff.py
@@ -3,45 +3,55 @@ import FWCore.ParameterSet.Config as cms
 # Name:   Scalers_EventContent_cff.py
 # Author: K.Maeshima, help from Lenny Apanasevich
 # Date:   03.23.2009
-# Notes:
+# Notes:  Add L1AcceptBunchCrossing and Collection (W.Badgett,2009.05.20)
 # 
 # RAW content 
 EvtScalersRAW = cms.PSet(
    outputCommands = cms.untracked.vstring(
+       'keep L1AcceptBunchCrossing_*_*_*',
        'keep L1TriggerScalers_*_*_*',
        'keep LumiScalers_*_*_*',
+       'keep L1AcceptBunchCrossingCollection_*_*_*',
        'keep L1TriggerScalersCollection_*_*_*',
        'keep LumiScalersCollection_*_*_*')
 )
 # Full Event content 
 EvtScalersFEVT = cms.PSet(
    outputCommands = cms.untracked.vstring(
+       'keep L1AcceptBunchCrossing_*_*_*',
        'keep L1TriggerScalers_*_*_*',
        'keep LumiScalers_*_*_*',
+       'keep L1AcceptBunchCrossingCollection_*_*_*',
        'keep L1TriggerScalersCollection_*_*_*',
        'keep LumiScalersCollection_*_*_*')
 )
 # RECO content
 EvtScalersRECO = cms.PSet(
    outputCommands = cms.untracked.vstring(
+       'keep L1AcceptBunchCrossing_*_*_*',
        'keep L1TriggerScalers_*_*_*',
        'keep LumiScalers_*_*_*',
+       'keep L1AcceptBunchCrossingCollection_*_*_*',
        'keep L1TriggerScalersCollection_*_*_*',
        'keep LumiScalersCollection_*_*_*')
 )
 # AOD content
 EvtScalersAOD = cms.PSet(
    outputCommands = cms.untracked.vstring(
+       'keep L1AcceptBunchCrossing_*_*_*',
        'keep L1TriggerScalers_*_*_*',
        'keep LumiScalers_*_*_*',
+       'keep L1AcceptBunchCrossingCollection_*_*_*',
        'keep L1TriggerScalersCollection_*_*_*',
        'keep LumiScalersCollection_*_*_*')
 )
 # FEVTDEBUG content
 EvtScalersFEVTDEBUG = cms.PSet(
    outputCommands = cms.untracked.vstring(
+       'keep L1AcceptBunchCrossing_*_*_*',
        'keep L1TriggerScalers_*_*_*',
        'keep LumiScalers_*_*_*',
+       'keep L1AcceptBunchCrossingCollection_*_*_*',
        'keep L1TriggerScalersCollection_*_*_*',
        'keep LumiScalersCollection_*_*_*')
 )
