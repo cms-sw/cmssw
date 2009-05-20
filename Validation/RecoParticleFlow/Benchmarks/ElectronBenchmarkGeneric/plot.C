6797e2f15f17a0a29449f5e98635a9e4bfc6bbdc
siward <sha1-66b073dee9070245c77c08d5b2a6d4559928f641@cern.ch>
2009-05-20 12:56:54 +0000

    *** empty log message ***

--- Validation/RecoParticleFlow/Benchmarks/ElectronBenchmarkGeneric/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/ElectronBenchmarkGeneric/plot.C
@@ -1,161 +1,74 @@
 {
-  
+  TFile *f = new TFile("benchmark.root"); 
   gROOT->LoadMacro("../Tools/NicePlot.C");
   InitNicePlot();
-  gROOT->LoadMacro("../Tools/Comparator.C");
-  //  Manager::Init();
 
-  gStyle->SetOptStat(1111);
-
-//   string dirpf = "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen";
-//   string dir2 = "DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen";
-  string dir1 = "DQMData/PFTask/Benchmarks/PFlowTaus/Gen";
-  //  string dir2 = "DQMData/PFTask/Benchmarks/PFlowTaus/Gen";
-  string dir2 = "DQMData/PFTask/Benchmarks/CaloTaus/Gen";
-  //  const char* file1 = "benchmark_oldmethod.root";
-  const char* file1 = "benchmark.root";
-  const char* file2 = "benchmark.root";
-  // const char* file2 = "benchmark_newmethod.root";
-  // const char* file1 = "ztautau.root";
-  // const char* file1 = "singleTaus.root";
-
-  enum EtaModes {
-    BARREL,
-    ENDCAP,
-    ALL
-  };
+  f->cd("DQMData/PFTask/Benchmarks/PFlowElectrons/Gen");  
+  Style *spf = spred;  
+  Style *sgen = sback;
   
-  bool highpt = false;
-  float ptMin = 0;
-  float ptMax = 9999;
-  //Style* style1 = sback;
- 
-  // Style* style2 = s1;
-  Style* style1 = spred;
-  Style* style2 = spblue;
-  Comparator::Mode mode = Comparator::SCALE;
+  TCanvas *c1 = new TCanvas;
+  FormatPad(c1,false);  
 
-  int etamode = BARREL;
+  TH1F* h = (TH1F*) gDirectory->Get("DeltaEt");
+  h->Rebin(2);
+  FormatHisto(h,spf);
+  h->SetStats(1111);
+  h->GetXaxis()->SetRangeUser(-30,30);
+  h->Draw();
+  SavePlot("electronbenchmark","Plots");
+  c1->Clear();
 
-  string outdir = "Plots_BarrelAndEndcap";
+  c1->SetLogy(1);
+  TH1F* h = (TH1F*) gDirectory->Get("DeltaEta");
+  h->Rebin(4);
+  FormatHisto(h,spf);
+  h->SetStats(1111);
+  h->GetXaxis()->SetRangeUser(-0.3,0.3);
+  h->Draw();
+  SavePlot("deltaeta","Plots");
+  c1->Clear();
+  //c1->SetLogy(0);
 
-  switch( etamode ) {
-  case BARREL:
-    dir1 = "DQMData/PFTask/Benchmarks/PFlowTaus_barrel/Gen";
-    dir2 = "DQMData/PFTask/Benchmarks/CaloTaus_barrel/Gen";
-    outdir = "Plots_Barrel";
-    break;
-  case ENDCAP:
-    dir1 = "DQMData/PFTask/Benchmarks/PFlowTaus_endcap/Gen";
-    dir2 = "DQMData/PFTask/Benchmarks/CaloTaus_endcap/Gen";
-    outdir = "Plots_Endcap";
-    break;
-  default:
-    break;
-  }   
+  //c1->SetLogy(1);
+  TH1F* h = (TH1F*) gDirectory->Get("DeltaPhi");
+  h->Rebin(4);
+  FormatHisto(h,spf);
+  h->SetStats(1111);
+  h->GetXaxis()->SetRangeUser(-0.3,0.3);
+  h->Draw();
+  SavePlot("deltaphi","Plots");
+  c1->Clear();
+  c1->SetLogy(0);
 
-  Comparator comp(file1,
-		  dir1.c_str(),
-		  file2,
-		  dir2.c_str());
-  comp.SetStyles(style1, style2, "Particle Flow Taus", "Calo Taus");
+  TH1F* h = (TH1F*) gDirectory->Get("EtGen");
+  //h->Rebin(2);
+  FormatHisto(h,sgen);
+  h->SetStats(1111);
+  h->GetXaxis()->SetRangeUser(0,120);
+  h->Draw();
+  SavePlot("etgen","Plots");
+  c1->Clear();
 
-
-  TCanvas c0("c0", "legend", 400, 200);
-  FormatPad( &c0, false ); 
-  comp.Legend().Draw();
-  SavePlot("legend", outdir.c_str() );
-    
-  comp.SetAxis(1, -30,30);
-  TCanvas c1a("c1a", "Tau benchmark, low pT");
-  FormatPad( &c1a, false ); 
-  comp.DrawSlice("DeltaEtvsEt", ptMin, ptMax, mode);
-  SavePlot("tauBenchmarkGeneric_lowpT", outdir.c_str() );
-  comp.SetAxis(5);
-
-
-  if(highpt) {
-    TCanvas c1b("c1b", "Tau benchmark, high pT");
-    FormatPad( &c1b, false );
-    comp.SetAxis(5);
-    comp.DrawSlice("DeltaEtOverEtvsEt", 400, 600, mode);
-    SavePlot("tauBenchmarkGeneric_highpT", outdir.c_str() );
-  }
-
-  comp.SetAxis(2, -0.2,0.2);
-
-  TCanvas c2a("c2a", "Eta resolution, low pT");
-  FormatPad( &c2a, false );
-  comp.DrawSlice("DeltaEtavsEt", ptMin, ptMax, mode);
-  SavePlot("deltaEta_lowpT", outdir.c_str() );
-
-  if(highpt) {
-    TCanvas c2b("c2b", "Eta resolution, high pT");
-    FormatPad( &c2b, false );
-    comp.DrawSlice("DeltaEtavsEt", 400, 600, mode);
-    SavePlot("deltaEta_highpT", outdir.c_str() );
-  }
-
-  TCanvas c3a("c3a", "Phi resolution, low pT");
-  FormatPad( &c3a, false );
-  comp.DrawSlice("DeltaPhivsEt", ptMin, ptMax, mode);
-  SavePlot("deltaPhi_lowpT", outdir.c_str() );
-
-  if(highpt) {
-    TCanvas c3b("c3b", "Phi resolution, high pT");
-    FormatPad( &c3b, false );
-    comp.DrawSlice("DeltaPhivsEt", 400, 600, mode);
-    SavePlot("deltaPhi_highpT", outdir.c_str() );
-  }
-
-  comp.SetAxis(1);
-
-
-  bool efficiencies = false;
-
-  if (efficiencies ) {
-
-    TCanvas c4("c4", "Efficiency PF");
-    FormatPad( &c4, false );
-    unsigned rebin = 10;
-
-    Comparator comp2(file1,
-		     dir1.c_str(),
-		     file1,
-		     dir1.c_str() ); 
-    comp2.SetStyles(style1, style2);
-    comp2.Draw("EtSeen","EtGen", Comparator::EFF);
-    SavePlot("efficiency_vs_pT_pf", outdir.c_str() );
-
-    TCanvas c5("c5", "Efficiency Calo");
-    FormatPad( &c5, false );
-
-    Comparator comp3(file1,
-		     dir2.c_str(),
-		     file1,
-		     dir2.c_str() ); 
-    comp3.SetStyles(style1, style2);
-    comp3.Draw("EtSeen","EtGen", Comparator::EFF);
-    SavePlot("efficiency_vs_pT_calo", outdir.c_str() );
-  }
-
-  TCanvas c6("c6", "pT");
-  FormatPad( &c6, false );
-  comp.SetAxis(2,0,120);
-  comp.Draw("EtGen", mode);
-  SavePlot("etgen", outdir.c_str() );
-  comp.SetAxis(1);
-
+  TH1F* h = (TH1F*) gDirectory->Get("EtaGen");
+  //h->Rebin(2);
+  FormatHisto(h,sgen);
+  h->SetStats(1111);
+  //h->GetXaxis()->SetRangeUser(-4,4);
+  h->Draw();
+  SavePlot("etagen","Plots");
+  c1->Clear();
   
-  TCanvas c7("c7", "eta");
-  FormatPad( &c7, false );
-  comp.Draw("EtaGen", mode);
-  SavePlot("etagen", outdir.c_str() );
+  TH2F* g = (TH2F*) gDirectory->Get("DeltaEtOverEtvsEt");
+  TH1D* p = g->ProjectionY();
+  p->Rebin(4);
+  FormatHisto(p,spf);
+  p->SetStats(1111);
+  //p->GetXaxis()->SetRangeUser(-1,1);
+  p->Draw();
+  SavePlot("etresolution","Plots");
+  //c1->Clear();
 
-//   TCanvas c8("c8", "nGen");
-//   FormatPad( &c8, false );
-//   comp.Draw("NGen", mode);
-//   SavePlot("ngen", outdir.c_str() );
-
-  
 }
+
+
