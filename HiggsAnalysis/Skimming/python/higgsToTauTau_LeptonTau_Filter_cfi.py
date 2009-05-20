b20274d5e50d52b60662ee67ed90de25afd3a853
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2009-05-20 16:14:54 +0000

    First commit of the Higgs TauTauLeptonTau skim

--- HiggsAnalysis/Skimming/python/higgsToTauTau_LeptonTau_Filter_cfi.py
+++ HiggsAnalysis/Skimming/python/higgsToTauTau_LeptonTau_Filter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 higgsToTauTauLeptonTauFilter = cms.EDFilter("HiggsToTauTauLeptonTauSkim",
     # Collection to be accessed
-    DebugHiggsToTauTauLeptonTauSkim = cms.bool(True),
+    DebugHiggsToTauTauLeptonTauSkim = cms.bool(False),
     HLTResultsCollection = cms.InputTag("TriggerResults::HLT"),
     HLTEventCollection = cms.InputTag("hltTriggerSummaryAOD"),
     HLTFilterCollections = cms.vstring('hltSingleMuIsoL3IsoFiltered15',
