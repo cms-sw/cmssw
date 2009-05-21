f13cf3c755fec35b2b21d0394573d92ffc882e12
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2009-05-21 09:43:48 +0000

    change name to the GsfElectronSelector to BtagGsfElectronSelector

--- RecoBTag/SoftLepton/plugins/BtagGsfElectronSelector.cc
+++ RecoBTag/SoftLepton/plugins/BtagGsfElectronSelector.cc
@@ -1,13 +1,13 @@
-/** \class GsfElectronSelector
+/** \class BtagGsfElectronSelector
  */
 
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "RecoBTag/SoftLepton/interface/GenericSelectorByValueMap.h"
 
 // "float" is the type stored in the ValueMap
-typedef edm::GenericSelectorByValueMap<reco::GsfElectron, float> GsfElectronSelector;
+typedef edm::GenericSelectorByValueMap<reco::GsfElectron, float> BtagGsfElectronSelector;
 
 //------------------------------------------------------------------------------
 
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(GsfElectronSelector);
+DEFINE_FWK_MODULE(BtagGsfElectronSelector);
--- RecoBTag/SoftLepton/python/softElectronSelector_cfi.py
+++ RecoBTag/SoftLepton/python/softElectronSelector_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # select a subset of the GsfElectron collection based on the quality stored in a ValueMap
-softElectronSelector = cms.EDProducer("GsfElectronSelector",
+softElectronSelector = cms.EDProducer("BtagGsfElectronSelector",
     input     = cms.InputTag('gsfElectrons'),
     selection = cms.InputTag('eidLoose'),
     cut       = cms.double(0.5)
