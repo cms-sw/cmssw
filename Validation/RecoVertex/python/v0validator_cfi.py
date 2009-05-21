6635e08500f0f7d8e6fbede1f28dda0cd1ca3318
Brian Drell <sha1-c7cbb7ddebb3a56d09a58e9398ceff52182b647f@cern.ch>
2009-05-21 21:35:22 +0000

    fixed file naming issue

--- Validation/RecoVertex/python/v0validator_cfi.py
+++ Validation/RecoVertex/python/v0validator_cfi.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-v0Validator = cms.EDAnalyzer('V0Validator'
+v0Validator = cms.EDAnalyzer('V0Validator',
+    DQMRootFileName = cms.string('V0DQM.root')
 )
