5c6147b743db356411aba66633e77e7415f54e59
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-05-21 08:26:35 +0000

    added sterne sorter

--- PhysicsTools/RooStatsCms/src/FeldmanCousinsBinomialInterval.cc
+++ PhysicsTools/RooStatsCms/src/FeldmanCousinsBinomialInterval.cc
@@ -1,7 +1,6 @@
 #if (defined (STANDALONE) or defined (__CINT__) )
 #include "FeldmanCousinsBinomialInterval.h"
 
-ClassImpT(BinomialNoncentralInterval, FeldmanCousinsSorter)
 ClassImp(FeldmanCousinsBinomialInterval)
 
 #else
