406c3358debbf44565416602783728cdf1d2adc5
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-05-21 13:25:28 +0000

    METManager computes MET quantities and uses the GenericBenchmark to make plots

--- RecoParticleFlow/PFRootEvent/interface/METManager.h
+++ RecoParticleFlow/PFRootEvent/interface/METManager.h
@@ -6,26 +6,64 @@
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
+#include "DataFormats/METReco/interface/MET.h"
+#include "DataFormats/METReco/interface/METCollection.h"
 #include "DataFormats/METReco/interface/PFMETCollection.h"
 #include "DataFormats/METReco/interface/GenMETCollection.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
 
 class METManager {
 
  public:
-  /// arguments: to initialize the GenericBenchmark
-  METManager();
-  
-  double computePFMET( const reco::PFCandidateCollection& pfCands );
 
-  double computeGenMET( const reco::GenParticleCollection& genParticles );
+  METManager(std::string outmetfilename);
   
+  /// setup a genericBenchmark
+  void addGenBenchmark(std::string GenBenchmarkName);
+
+  /// Fill the genericBenchmark histograms
+  /// string = name of the folder in the root file (=name of the GenBenchmark)
+  void FillHisto(std::string);
+
+  /// Write the output root file of the genericBenchmark
+  void write();
+
+  void setMET1(const reco::MET*);
+  void setMET2(const reco::MET*);
+
+  /// set METX with the GenMet computed from the GenParticleCollection with computeGenMET(...)
+  void setMET1(const reco::GenParticleCollection*);
+  void setMET2(const reco::GenParticleCollection*);
+
+  /// set METX with the pfMet recomputed from the pfCandidates_ with recomputePFMET(...)
+  void setMET1(const reco::PFCandidateCollection&);
+  void setMET2(const reco::PFCandidateCollection&);
+
+  /// cout events in tail of Delta(MET1,MET2)
+  void coutTailEvents(const int entry, const double DeltaMETcut,
+		      const double DeltaPhicut, const double MET1cut) const;
+
+  /// propagate the Jet Energy Corrections to the MET
+  void propagateJECtoMET1(const std::vector<reco::CaloJet> caloJets,
+		    const std::vector<reco::CaloJet> corr_caloJets);
+  void propagateJECtoMET2(const std::vector<reco::CaloJet> caloJets,
+		    const std::vector<reco::CaloJet> corr_caloJets);
+
  private:
-  /// plots comparing pf and gen MET 
-  GenericBenchmark  pfVsGenBenchmark_;
+ 
+  /// private functions
+  reco::MET computeGenMET( const reco::GenParticleCollection* ) const;
+  reco::MET recomputePFMET( const reco::PFCandidateCollection&) const;
+  reco::MET propagateJEC(const reco::MET&, const std::vector<reco::CaloJet> caloJets,
+			 const std::vector<reco::CaloJet> corr_caloJets) const;
 
-  reco::GenMETCollection genMETs_;
-  reco::PFMETCollection  pfMETs_;
-
+  /// data members
+  reco::MET MET1_;
+  reco::MET MET2_;
+  /// map of GenericBenchmarks, the key is his name
+  std::map<std::string,GenericBenchmark> GenBenchmarkMap_;
+  std::string outmetfilename_;
+  TFile* outfile_;
 
 };
 
