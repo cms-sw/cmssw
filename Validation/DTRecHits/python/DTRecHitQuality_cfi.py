5ca215cae8df93923aef80b6e7a3053c8195f525
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-05-21 18:13:05 +0000

    Switch to input tag

--- Validation/DTRecHits/python/DTRecHitQuality_cfi.py
+++ Validation/DTRecHits/python/DTRecHitQuality_cfi.py
@@ -5,19 +5,19 @@ rechivalidation = cms.EDFilter("DTRecHitQuality",
     # Switches for analysis at various steps
     doStep1 = cms.untracked.bool(True),
     # Lable to retrieve RecHits from the event
-    recHitLabel = cms.untracked.string('dt1DRecHits'),
+    recHitLabel = cms.untracked.InputTag('dt1DRecHits'),
     doStep3 = cms.untracked.bool(True),
-    simHitLabel = cms.untracked.string('g4SimHits'),
-    segment2DLabel = cms.untracked.string('dt2DSegments'),
+    simHitLabel = cms.untracked.InputTag('g4SimHits',"MuonDTHits"),
+    segment2DLabel = cms.untracked.InputTag('dt2DSegments'),
     rootFileName = cms.untracked.string('DTRecHitQualityPlots.root'),
     debug = cms.untracked.bool(False),
-    segment4DLabel = cms.untracked.string('dt4DSegments')
+    segment4DLabel = cms.untracked.InputTag('dt4DSegments')
 )
 
 seg2dvalidation = cms.EDFilter("DTSegment2DQuality",
     sigmaResPos = cms.double(0.013),
-    simHitLabel = cms.untracked.string('g4SimHits'),
-    segment2DLabel = cms.untracked.string('dt2DSegments'),
+    simHitLabel = cms.untracked.InputTag('g4SimHits',"MuonDTHits"),
+    segment2DLabel = cms.untracked.InputTag('dt2DSegments'),
     rootFileName = cms.untracked.string('DTSeg2DQualityPlots.root'),
     debug = cms.untracked.bool(False),
     sigmaResAngle = cms.double(0.008)
@@ -25,24 +25,24 @@ seg2dvalidation = cms.EDFilter("DTSegment2DQuality",
 
 seg2dsuperphivalidation = cms.EDFilter("DTSegment2DSLPhiQuality",
     sigmaResPos = cms.double(0.013),
-    simHitLabel = cms.untracked.string('g4SimHits'),
+    simHitLabel = cms.untracked.InputTag('g4SimHits',"MuonDTHits"),
     sigmaResAngle = cms.double(0.008),
     rootFileName = cms.untracked.string('DTSeg2DSLPhiQualityPlots.root'),
     debug = cms.untracked.bool(False),
-    segment4DLabel = cms.untracked.string('dt4DSegments')
+    segment4DLabel = cms.untracked.InputTag('dt4DSegments')
 )
 
 seg4dvalidation = cms.EDFilter("DTSegment4DQuality",
     #resolution on angle
     sigmaResAlpha = cms.double(0.001),
     sigmaResBeta = cms.double(0.007),
-    simHitLabel = cms.untracked.string('g4SimHits'),
+    simHitLabel = cms.untracked.InputTag('g4SimHits',"MuonDTHits"),
     rootFileName = cms.untracked.string('DTSeg4DQualityPlots.root'),
     debug = cms.untracked.bool(False),
     #resolution on position
     sigmaResX = cms.double(0.01),
     sigmaResY = cms.double(0.05),
-    segment4DLabel = cms.untracked.string('dt4DSegments')
+    segment4DLabel = cms.untracked.InputTag('dt4DSegments')
 )
 
 dtLocalRecoValidation = cms.Sequence(rechivalidation*seg2dvalidation*seg2dsuperphivalidation*seg4dvalidation)
