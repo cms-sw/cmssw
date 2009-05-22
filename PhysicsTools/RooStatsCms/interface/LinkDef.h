2daf25c53b93b262d1e5a2ba961ea4e0b87f84bf
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-05-22 12:16:20 +0000

    added ROOT CINT link definition file

---
+++ PhysicsTools/RooStatsCms/interface/LinkDef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+
+#pragma link C++ class BinomialInterval;
+#pragma link C++ class BinomialNoncentralInterval<FeldmanCousinsSorter>;
+#pragma link C++ class BinomialNoncentralInterval<SterneSorter>;
+#pragma link C++ class FeldmanCousinsBinomialInterval;
+#pragma link C++ class SterneBinomialInterval;
+#pragma link C++ class ClopperPearsonBinomialInterval;
+
+#endif
