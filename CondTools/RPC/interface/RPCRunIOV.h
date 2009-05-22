af01f0526e36ea7c088f93b56bc57f9ad6a2400b
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-05-22 18:39:48 +0000

    new release

--- CondTools/RPC/interface/RPCRunIOV.h
+++ CondTools/RPC/interface/RPCRunIOV.h
@@ -5,8 +5,8 @@
  * \class RPCRunIOV
  *  Reads data from ORCOFF and sqlite file
  *
- *  $Date: 2009/05/20 10:13:28 $
- *  $Revision: 1.1 $
+ *  $Date: 2009/05/20 11:16:49 $
+ *  $Revision: 1.2 $
  *  \author D. Pagano - Dip. Fis. Nucl. e Teo. & INFN Pavia
  */
 
@@ -23,20 +23,26 @@
 #include "DataFormats/MuonDetId/interface/RPCDetId.h"
 #include "CondFormats/RPCObjects/interface/RPCObCond.h"
 #include "CondFormats/DataRecord/interface/RPCObCondRcd.h"
-#include "CondTools/RPC/interface/RPCIOVReader.h"
 #include "CoralBase/TimeStamp.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CondTools/RPC/interface/RPCFw.h"
 
 class  RPCRunIOV {
 public:
 
   RPCRunIOV(unsigned long long m_since, unsigned long long m_till);
-  
+  RPCRunIOV(const edm::EventSetup& evtSetup);
   ~RPCRunIOV();
-  std::vector<RPCObImon::I_Item> getData();
+  std::vector<RPCObImon::I_Item> getImon();
+  bool isReadingNeeded(unsigned long long);
+  unsigned long long toDAQ(unsigned long long);
+  unsigned long long toUNIX(int, int);
+  unsigned long long min;
+  unsigned long long max;
 
 private:
+  const edm::EventSetup* eventSetup;
   unsigned long long since;
   unsigned long long till;
   bool imon;
