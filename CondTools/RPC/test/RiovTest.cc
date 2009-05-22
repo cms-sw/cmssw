338e976f851cf5c7e36ee2186e99271f4320a07a
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-05-22 18:14:11 +0000

    new release

--- CondTools/RPC/test/RiovTest.cc
+++ CondTools/RPC/test/RiovTest.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Davide Pagano
 //         Created:  Wed May 20 12:47:20 CEST 2009
-// $Id$
+// $Id: RiovTest.cc,v 1.1 2009/05/20 14:32:04 dpagano Exp $
 //
 //
 
@@ -24,12 +24,9 @@
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include "CondFormats/RPCObjects/interface/RPCObCond.h"
 #include "CondFormats/DataRecord/interface/RPCObCondRcd.h"
 #include "CondTools/RPC/interface/RPCIOVReader.h"
@@ -40,6 +37,8 @@
 //
 // class decleration
 //
+
+
 
 class RiovTest : public edm::EDAnalyzer {
 public:
@@ -55,7 +54,11 @@ private:
   
   RPCRunIOV* connection;
   std::vector<RPCObImon::I_Item> imon;
+  std::vector<RPCObImon::I_Item> imon_;
   std::vector<RPCObImon::I_Item> filtImon;
+  std::vector<unsigned long long> listIOV;
+  unsigned long long min;
+  unsigned long long max;
 };
 
 RiovTest::RiovTest(const edm::ParameterSet& iConfig)
@@ -74,21 +77,35 @@ void
 RiovTest::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
-   
-   unsigned long long since = 1242731700;
-   unsigned long long till  = 1242733500;
-   
-   connection = new RPCRunIOV(since, till);
-   // get data
-   imon = connection->getData();
 
-   // filter data
-   this->filterData(since, till);
-
-   // print data
-   for (std::vector<RPCObImon::I_Item>::iterator it=filtImon.begin(); it < filtImon.end(); it++ ) {
-     //std::cout<<"dpid = " << it->dpid << " - value = " << it->value << " - day = " << it->day << " - time = " << it->time << std::endl;
+   // TIME OF EVENT-------------------------
+   std::cout << "=== Event " << iEvent.id().event() << " ===" << std::endl;
+   TimeValue_t timeD = iEvent.time().value();
+   std::cout << "DAQ  = " << timeD << std::endl;
+   timeval tmval=(timeval&)timeD;
+   unsigned long long int timeU;
+   timeU = (tmval.tv_usec*1000000LL)+tmval.tv_sec;
+   timeU = (unsigned long long int)trunc(timeU/1000000);
+   std::cout << "UNIX = " << timeU << std::endl;
+   int a;
+   // CONNECTION TO DATABASE----------------
+   RPCRunIOV* list = new RPCRunIOV(iSetup);
+   if (imon.size() == 0) {
+     imon = list->getImon();
+     min = list->min;
+     max = list->max;
+   } else if (timeU < min || timeU > max) {
+     imon_ = list->getImon();
+     std::cout << "Before [size] " << imon.size();  
+     for(std::vector<RPCObImon::I_Item>::iterator it = imon_.begin(); it < imon_.end(); ++it)
+       imon.push_back(*it);
+     std::cout << " <--> After [size] " << imon.size() << std::endl;  
+     if (min > list->min) min = list->min;
+     if (max < list->max) max = list->max;
    }
+   //----------------------------------------
+   std::cout << "MIN = " << min << std::endl;
+   std::cout << "MAX = " << max << std::endl;
 }
 
 
@@ -117,7 +134,7 @@ RiovTest::filterData(unsigned long long since, unsigned long long till)
     coral::TimeStamp timeD = coral::TimeStamp(yea, mon, day, hou, min, sec, nan);
     unsigned long long timeU = conv.TtoUT(timeD);
     
-    //std::cout << n << " dpid = " << it->dpid << " - value = " << it->value << " - day = " << it->day << " (" << day << "/" << mon << "/" << yea << ") - time = " << it->time << " (" << hou << ":" << min << "." << sec << ") - UT = " << timeU << std::endl;
+    std::cout << n << " dpid = " << it->dpid << " - value = " << it->value << " - day = " << it->day << " (" << day << "/" << mon << "/" << yea << ") - time = " << it->time << " (" << hou << ":" << min << "." << sec << ") - UT = " << timeU << std::endl;
 
     if (timeU < till && timeU > since) filtImon.push_back(*it);
     
