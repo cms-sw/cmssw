b038ec6c6e2c383131bba458651c6b6c6009148a
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-05-22 16:46:40 +0000

    new release

--- CondTools/RPC/test/riovtest_cfg.py
+++ CondTools/RPC/test/riovtest_cfg.py
@@ -1,15 +1,32 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.source = cms.Source("EmptyIOVSource",
-    timetype = cms.string('timestamp'),
-    firstValue = cms.uint64(1220227200),
-    lastValue = cms.uint64(1220227200),
-    interval = cms.uint64(1)
+process.CondDBCommon.connect = 'sqlite_file:dati.db'
+process.CondDBCommon.DBParameters.authenticationPath = './'
+
+
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(10)
 )
+
+process.rn = cms.ESSource("PoolDBESSource",
+    process.CondDBCommon,
+    timetype = cms.string('timestamp'),
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('RPCObImonRcd'),
+        tag = cms.string('Imon_v3')
+    ))
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+'/store/data/Commissioning08/Cosmics/RECO/CRAFT_ALL_V9_225-v2/0010/BE62FAE1-44FC-DD11-8484-003048769E5F.root'
+)
+)
+
 
 process.demo = cms.EDAnalyzer('RiovTest')
 
