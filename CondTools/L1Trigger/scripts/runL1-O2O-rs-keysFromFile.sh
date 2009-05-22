ab2d15db2bfd5ed47b9fa44bf4bd3e7cfafc7b1b
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2009-05-22 15:10:01 +0000

    First submission.

---
+++ CondTools/L1Trigger/scripts/runL1-O2O-rs-keysFromFile.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+source /nfshome0/cmssw2/scripts/setup.sh
+eval `scramv1 run -sh`
+#export TNS_ADMIN=/nfshome0/xiezhen/conddb
+export TNS_ADMIN=/nfshome0/l1emulator/o2o/conddb
+
+xflag=0
+while getopts 'xh' OPTION
+  do
+  case $OPTION in
+      x) xflag=1
+          ;;
+      h) echo "Usage: [-x] tsckey runnum tagbase"
+          echo "  -x: write to ORCON instead of sqlite file"
+          exit
+          ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+tagbase=$1
+
+exec<rskeys.txt
+
+while read line
+do
+
+  if [ ${xflag} -eq 0 ]
+      then
+      echo "Writing to sqlite_file:l1config.db instead of ORCON."
+      cmsRun $CMSSW_BASE/src/CondTools/L1Trigger/test/L1ConfigWriteRSPayloadOnline_cfg.py tagBase=${tagbase}_hlt outputDBConnect=sqlite_file:l1config.db outputDBAuth=. keysFromDB=0 ${line} print
+      cmsRun $CMSSW_BASE/src/CondTools/L1Trigger/test/L1ConfigWriteRSIOVOnline_cfg.py tagBase=${tagbase}_hlt outputDBConnect=sqlite_file:l1config.db outputDBAuth=. keysFromDB=0 ${line} print
+  else
+      cmsRun $CMSSW_BASE/src/CondTools/L1Trigger/test/L1ConfigWriteRSPayloadOnline_cfg.py tagBase=${tagbase}_hlt outputDBConnect=oracle://cms_orcoff_prep/CMS_COND_L1T outputDBAuth=/nfshome0/l1emulator/o2o/conddb keysFromDB=0 ${line} print
+      cmsRun $CMSSW_BASE/src/CondTools/L1Trigger/test/L1ConfigWriteRSIOVOnline_cfg.py tagBase=${tagbase}_hlt outputDBConnect=oracle://cms_orcoff_prep/CMS_COND_L1T outputDBAuth=/nfshome0/l1emulator/o2o/conddb keysFromDB=0 ${line} print
+  fi
+done
+
+exit
