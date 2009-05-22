9d618fd458652e16671ffa946cbf2a942645039b
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2009-05-22 21:32:39 +0000

    Correct the run scripts

--- SimG4Core/PrintGeomInfo/test/python/run_MaterialBudgeInfo_cfg.py
+++ SimG4Core/PrintGeomInfo/test/python/run_MaterialBudgeInfo_cfg.py
@@ -29,21 +29,22 @@ process.maxEvents = cms.untracked.PSet(
 
 process.load("SimGeneral.HepPDTESSource.pdt_cfi")
 
-process.source = cms.Source("FlatRandomPtGunSource",
-    PGunParameters = cms.untracked.PSet(
-        PartID = cms.untracked.vint32(13),
-        MinEta = cms.untracked.double(-2.5),
-        MaxEta = cms.untracked.double(2.5),
-        MinPhi = cms.untracked.double(-3.14159265359),
-        MaxPhi = cms.untracked.double(3.14159265359),
-        MinPt  = cms.untracked.double(9.99),
-        MaxPt  = cms.untracked.double(10.01)
-    ),
-    Verbosity = cms.untracked.int32(0),
-    AddAntiParticle = cms.untracked.bool(False),
-    firstRun = cms.untracked.uint32(1)
-)
+process.source = cms.Source("EmptySource")
 
+process.generator = cms.EDProducer("FlatRandomPtGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(13),
+        MinEta = cms.double(-2.5),
+        MaxEta = cms.double(2.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinPt  = cms.double(9.99),
+        MaxPt  = cms.double(10.01)
+    ),
+    AddAntiParticle = cms.bool(False),
+    Verbosity       = cms.untracked.int32(0),
+    firstRun        = cms.untracked.uint32(1)
+)
 
 process.EnableFloatingPointExceptions = cms.Service("EnableFloatingPointExceptions",
     enableDivByZeroEx = cms.untracked.bool(False),
@@ -54,6 +55,7 @@ process.EnableFloatingPointExceptions = cms.Service("EnableFloatingPointExceptio
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
     moduleSeeds = cms.PSet(
+        generator = cms.untracked.uint32(456789),
         g4SimHits = cms.untracked.uint32(9876),
         VtxSmeared = cms.untracked.uint32(98765432)
     ),
@@ -62,13 +64,12 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 
 process.load("SimG4Core.Application.g4SimHits_cfi")
 
-process.p1 = cms.Path(process.g4SimHits)
+process.p1 = cms.Path(process.generator*process.g4SimHits)
 
 process.g4SimHits.Physics.type            = 'SimG4Core/Physics/DummyPhysics'
 process.g4SimHits.UseMagneticField        = False
 process.g4SimHits.Physics.DummyEMPhysics  = True
 process.g4SimHits.Physics.DefaultCutValue = 10. 
-process.g4SimHits.Generator.HepMCProductLabel = 'source'
 process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
 	Name           = cms.untracked.string('TIDF'),
 	type           = cms.string('PrintMaterialBudgetInfo')
--- SimG4Core/PrintGeomInfo/test/python/run_cfg.py
+++ SimG4Core/PrintGeomInfo/test/python/run_cfg.py
@@ -22,26 +22,27 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(0)
+    input = cms.untracked.int32(1)
 )
 
 process.load("SimGeneral.HepPDTESSource.pdt_cfi")
 
-process.source = cms.Source("FlatRandomPtGunSource",
-    PGunParameters = cms.untracked.PSet(
-        PartID = cms.untracked.vint32(13),
-        MinEta = cms.untracked.double(-2.5),
-        MaxEta = cms.untracked.double(2.5),
-        MinPhi = cms.untracked.double(-3.14159265359),
-        MaxPhi = cms.untracked.double(3.14159265359),
-        MinPt  = cms.untracked.double(9.99),
-        MaxPt  = cms.untracked.double(10.01)
-    ),
-    Verbosity = cms.untracked.int32(0),
-    AddAntiParticle = cms.untracked.bool(False),
-    firstRun = cms.untracked.uint32(1)
-)
+process.source = cms.Source("EmptySource")
 
+process.generator = cms.EDProducer("FlatRandomPtGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(13),
+        MinEta = cms.double(-2.5),
+        MaxEta = cms.double(2.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinPt  = cms.double(9.99),
+        MaxPt  = cms.double(10.01)
+    ),
+    AddAntiParticle = cms.bool(False),
+    Verbosity       = cms.untracked.int32(0),
+    firstRun        = cms.untracked.uint32(1)
+)
 
 process.EnableFloatingPointExceptions = cms.Service("EnableFloatingPointExceptions",
     enableDivByZeroEx = cms.untracked.bool(False),
@@ -52,6 +53,7 @@ process.EnableFloatingPointExceptions = cms.Service("EnableFloatingPointExceptio
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
     moduleSeeds = cms.PSet(
+        generator = cms.untracked.uint32(456789),
         g4SimHits = cms.untracked.uint32(9876),
         VtxSmeared = cms.untracked.uint32(98765432)
     ),
@@ -60,13 +62,12 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 
 process.load("SimG4Core.Application.g4SimHits_cfi")
 
-process.p1 = cms.Path(process.g4SimHits)
+process.p1 = cms.Path(process.generator*process.g4SimHits)
 
 process.g4SimHits.Physics.type            = 'SimG4Core/Physics/DummyPhysics'
 process.g4SimHits.UseMagneticField        = False
 process.g4SimHits.Physics.DummyEMPhysics  = True
 process.g4SimHits.Physics.DefaultCutValue = 10. 
-process.g4SimHits.Generator.HepMCProductLabel = 'source'
 process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
 	DumpSummary    = cms.untracked.bool(True),
 	DumpLVTree     = cms.untracked.bool(True),
