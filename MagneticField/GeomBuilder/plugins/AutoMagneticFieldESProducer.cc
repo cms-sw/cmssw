348a89b89108663ebd32ea189cef7a40ed359518
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2009-05-23 22:52:55 +0000

    full redesign

--- MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.cc
+++ MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.cc
@@ -1,7 +1,7 @@
 /** \file
  *
- *  $Date: 2009/03/03 12:58:02 $
- *  $Revision: 1.4 $
+ *  $Date: 2009/03/19 10:51:40 $
+ *  $Revision: 1.5 $
  *  \author Nicola Amapane 11/08
  */
 
@@ -19,7 +19,6 @@
 
 #include "CondFormats/RunInfo/interface/RunInfo.h"
 #include "CondFormats/DataRecord/interface/RunSummaryRcd.h"
-#include "MagneticField/Engine/interface/MagneticFieldHelpers.h"
 
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
@@ -38,6 +37,12 @@ using namespace magneticfield;
 
 AutoMagneticFieldESProducer::AutoMagneticFieldESProducer(const edm::ParameterSet& iConfig) : pset(iConfig) {
   setWhatProduced(this, pset.getUntrackedParameter<std::string>("label",""));
+  nominalCurrents = pset.getUntrackedParameter<vector<int> >("nominalCurrents"); 
+  maps = pset.getUntrackedParameter<vector<string> >("mapLabels");
+
+  if (maps.size()==0 || (maps.size() != nominalCurrents.size())) {
+    throw cms::Exception("InvalidParameter") << "Invalid values for parameters \"nominalCurrents\" and \"maps\"";
+  }
 }
 
 
@@ -49,124 +54,46 @@ AutoMagneticFieldESProducer::~AutoMagneticFieldESProducer()
 std::auto_ptr<MagneticField>
 AutoMagneticFieldESProducer::produce(const IdealMagneticFieldRecord& iRecord)
 {
-  MagneticField * result=0;
+  float current = pset.getParameter<int>("valueOverride");
 
+  string message;
 
-  int value = pset.getParameter<int>("valueOverride");
-
-  if (value < 0) {
+  if (current < 0) {
     ESHandle<RunInfo> rInfo;
     iRecord.getRecord<RunInfoRcd>().get(rInfo);
-
-    float current = rInfo->m_avg_current;
-    value = magneticFieldHelpers::closerNominalField(current);
-    
-    edm::LogInfo("MagneticField|AutoMagneticField") << "Recorded avg current: " << current << "; using map for " << value/10. << " T";
+    current = rInfo->m_avg_current;
+    message = " (from RunInfo DB)";
   } else {
-    edm::LogInfo("MagneticField|AutoMagneticField") << "Ignoring DB current readings; using map for " << value/10. << " T";
+    message = " (from valueOverride card)";
   }
 
-  MagneticField * paramField=0;
+  string model  = closerModel(current);
 
-  string version;
+  edm::LogInfo("MagneticField|AutoMagneticField") << "Current: " << current << message << "; using map with label: " << model;
+
+
+  edm::ESHandle<MagneticField> map;
   
-  if (value == 0) {
-    // Use UniformMagneticField instead of the inner region parametrization.
-    // This trick allows to keep a VBF field to be used by propagators to check the geometry.
-    paramField = new UniformMagneticField(0.);
+  iRecord.get(model,map);
 
-    // Temporary hack: set 3.8T underlying VBF map so that SHP can decide if volume is iron
-    //    version = "fake";
-    stringstream str;
-    string model = pset.getParameter<string>("model");
-    str << model << "_" << "3_8t";
-    version = str.str();
+  MagneticField* result = map.product()->clone();
 
-  } else {
-    // Handle different labelling conventions
-    string VBFValue;
-    string OAEValue;
-    
-    if (value == 20) {
-      VBFValue = "2t";
-      OAEValue = "2_0T";
-    } else if (value == 30){
-      VBFValue = "3t";
-      OAEValue = "3_0T";
-    } else if (value == 35){
-      VBFValue = "3_5t";
-      OAEValue = "3_5T";
-    } else if (value == 38){
-      VBFValue = "3_8t";
-      OAEValue = "3_8T";
-    } else if (value == 40){
-      VBFValue = "4t";
-      OAEValue = "4_0T";
-    } else {
-      throw cms::Exception("InvalidParameter")<<"Invalid field value: requested : " << value << " kGauss";
-    }
-
-    stringstream str;
-    string model = pset.getParameter<string>("model");
-    str << model << "_" << VBFValue;
-    version = str.str();
-
-    // Build slave field
-    if (pset.getParameter<bool>("useParametrizedTrackerField")) {
-
-      string parVersion = pset.getParameter<string>("subModel");
-      
-      if (parVersion=="OAE_1103l_071212") { 
-	// V. Karimaki's off-axis expansion fitted to v1103l TOSCA computation
-	ParameterSet ppar;
-	ppar.addParameter<string>("BValue", OAEValue);
-	paramField =  new OAEParametrizedMagneticField(ppar);
-	//      } else if (parVersion=="PolyFit2D") {
- 	// V. Maroussov polynomial fit to mapping data
-	// 	ParameterSet ppar;
-	// 	ppar.addParameter<double>("BValue", 4.01242188708911); //FIXME
-	// 	paramField = new PolyFit2DParametrizedMagneticField(ppar);
-	//       }
-      } else {
-	throw cms::Exception("InvalidParameter")<<"Invalid parametrization version " << parVersion;
-      }
-    }
-  }
-  
-  ParameterSet VBFPset;
-  VBFPset.addUntrackedParameter<bool>("debugBuilder",false);
-  VBFPset.addUntrackedParameter<bool>("cacheLastVolume",true);
-  VBFPset.addParameter<string>("version",version);
-    
-  edm::ESHandle<DDCompactView> cpv;
-  iRecord.get("magfield",cpv );
-  MagGeoBuilderFromDDD builder(VBFPset.getParameter<std::string>("version"),
-			       VBFPset.getUntrackedParameter<bool>("debugBuilder", false));
-
-    
-  // Get scaling factors
-  vector<int> keys = pset.getParameter<vector<int> >("scalingVolumes");
-  vector<double> values = pset.getParameter<vector<double> >("scalingFactors");
-
-  if (keys.size() != 0) {
-    builder.setScaling(keys, values);
-  }
-
-  builder.build(*cpv);
-  
-  result = new VolumeBasedMagneticField(VBFPset,
-					builder.barrelLayers(), 
-					builder.endcapSectors(), 
-					builder.barrelVolumes(), 
-					builder.endcapVolumes(),
-					builder.maxR(), 
-					builder.maxZ(), 
-					paramField, true);
-
-  
   std::auto_ptr<MagneticField> s(result);
+  
   return s;
 }
+
+
+std::string AutoMagneticFieldESProducer::closerModel(float current) {
+  int i=0;
+  for(;i<(int)maps.size()-1;i++) {
+    if(2*current < nominalCurrents[i]+nominalCurrents[i+1] )
+      return maps[i];
+  }
+  return  maps[i];
+}
+
+
 
 #include "FWCore/Framework/interface/ModuleFactory.h"
 DEFINE_FWK_EVENTSETUP_MODULE(AutoMagneticFieldESProducer);
--- MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.h
+++ MagneticField/GeomBuilder/plugins/AutoMagneticFieldESProducer.h
@@ -6,7 +6,7 @@
  *  Produce a magnetic field map corresponding to the current 
  *  recorded in the condidtion DB.
  *
- *  $Date: 2008/04/02 15:51:43 $
+ *  $Date: 2008/11/14 10:42:40 $
  *  $Revision: 1.1 $
  *  \author Nicola Amapane 11/08
  */
@@ -15,6 +15,8 @@
 
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <vector>
+#include <string>
 
 class IdealMagneticFieldRecord;
 
@@ -27,6 +29,10 @@ namespace magneticfield {
     
     std::auto_ptr<MagneticField> produce(const IdealMagneticFieldRecord&);
     edm::ParameterSet pset;
+  private:
+    std::string closerModel(float current);
+    std::vector<int> nominalCurrents;
+    std::vector<std::string> maps;
   };
 }
 
