e438015135521f9c93ebcb79c293221bc384ba9f
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-05-23 09:43:14 +0000

    update prefixME

--- DQM/EcalPreshowerMonitorModule/python/ESIntegrityTask_cfi.py
+++ DQM/EcalPreshowerMonitorModule/python/ESIntegrityTask_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
     
 ecalPreshowerIntegrityTask = cms.EDAnalyzer('ESIntegrityTask',
+                                            prefixME = cms.untracked.string('EcalPreshower'),
                                             ESDCCCollections = cms.InputTag("esRawToDigi"),
                                             ESKChipCollections = cms.InputTag("esRawToDigi"),
                                             OutputFile = cms.untracked.string("")
