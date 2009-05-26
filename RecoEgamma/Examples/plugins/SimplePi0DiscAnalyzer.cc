7683d6289d40e0f65e2570272d4e1c33038731d7
Aristoteles Kyriakis <sha1-032d3cd00cff641a3de1489eb42a57a26bd497e0@cern.ch>
2009-05-26 10:45:45 +0000

    pi0 dicrimination algorithm example code

--- RecoEgamma/Examples/plugins/SimplePi0DiscAnalyzer.cc
+++ RecoEgamma/Examples/plugins/SimplePi0DiscAnalyzer.cc
@@ -1,129 +1,127 @@
 // -*- C++ -*-
 //
-// Package:    SimplePi0DiscAnalyzer
+// Package:    RecoEgamma/Examples
 // Class:      SimplePi0DiscAnalyzer
 //
-//
-// Original Authors:  A. Kyriakis NCSR "Demokritos" Athens
-//                    D Maletic, "Vinca" Belgrade
-//
-//         Created:  Wed Sep 12 13:36:27 CEST 2007
-// $Id: SimplePi0DiscAnalyzer.cc,v 1.5 2008/05/16 18:17:36 nancy Exp $
-//
-//
+/**\class SimplePi0DiscAnalyzer RecoEgamma/Examples/src/SimplePi0DiscAnalyzer.cc
 
+ Description: GsfElectrons analyzer using reco data
 
-// system include files
-#include <memory>
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Ursula Berthon
+//         Created:  Mon Mar 27 13:22:06 CEST 2006
+// $Id: SimplePi0DiscAnalyzer.cc,v 1.12 2009/03/06 12:42:16 chamont Exp $
+//
+//
 
 // user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "RecoEgamma/Examples/plugins/SimplePi0DiscAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+#include "DataFormats/EgammaReco/interface/ElectronSeed.h"
+#include "DataFormats/EgammaReco/interface/ElectronSeedFwd.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
 
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/EgammaCandidates/interface/Photon.h"
-#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
-
-#include "DataFormats/EgammaCandidates/interface/Conversion.h"
-#include "DataFormats/EgammaCandidates/interface/ConversionFwd.h"
-//#include "DataFormats/EgammaReco/interface/SuperCluster.h"
-#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
-
-#include "DataFormats/EgammaCandidates/interface/PhotonPi0DiscriminatorAssociation.h"
-
+#include "CLHEP/Units/PhysicalConstants.h"
+#include <iostream>
+#include "TMath.h"
 #include "TFile.h"
 #include "TH1F.h"
+#include "TH1I.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TTree.h"
+#include <iostream>
 
-using namespace std;
 using namespace reco;
-using namespace edm;
 
-//
-// class decleration
-//
-
-class SimplePi0DiscAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit SimplePi0DiscAnalyzer(const edm::ParameterSet&);
-
-      ~SimplePi0DiscAnalyzer();
-
-
-   private:
-
-     virtual void beginJob() ;
-     virtual void analyze(const edm::Event&, const edm::EventSetup&);
-     virtual void endJob() ;
-      // ----------member data ---------------------------
-
-     string photonCollectionProducer_;
-     string photonCorrCollectionProducer_;
-     string uncorrectedPhotonCollection_;
-     string correctedPhotonCollection_;
-
-     string outputFile_;
-     TFile*  rootFile_;
-
-     TH1F* hConv_ntracks_;
-
-     TH1F* hAll_nnout_Assoc_;
-     TH1F* hAll_nnout_NoConv_Assoc_;
-     TH1F* hBarrel_nnout_Assoc_;
-     TH1F* hBarrel_nnout_NoConv_Assoc_;
-     TH1F* hEndcNoPresh_nnout_Assoc_;
-     TH1F* hEndcNoPresh_nnout_NoConv_Assoc_;
-     TH1F* hEndcWithPresh_nnout_Assoc_;
-     TH1F* hEndcWithPresh_nnout_NoConv_Assoc_;
-
-};
-
-SimplePi0DiscAnalyzer::SimplePi0DiscAnalyzer(const edm::ParameterSet& iConfig)
-
+SimplePi0DiscAnalyzer::SimplePi0DiscAnalyzer(const edm::ParameterSet& conf)
 {
 
-  photonCollectionProducer_ = iConfig.getParameter<string>("phoProducer");
-  photonCorrCollectionProducer_ = iConfig.getParameter<string>("corrPhoProducer");
-  uncorrectedPhotonCollection_ = iConfig.getParameter<string>("uncorrectedPhotonCollection");
-  correctedPhotonCollection_ = iConfig.getParameter<string>("correctedPhotonCollection");
+  outputFile_ = conf.getParameter<std::string>("outputFile");
+  rootFile_ = new TFile(outputFile_.c_str(),"RECREATE");
 
-    outputFile_   = iConfig.getParameter<string>("outputFile");
 
-    rootFile_ = TFile::Open(outputFile_.c_str(),"RECREATE"); // open output file to store histograms
+  photonCollectionProducer_ = conf.getParameter<string>("phoProducer");
+  photonCollection_ = conf.getParameter<string>("photonCollection");
 
+
+ 
 }
 
 SimplePi0DiscAnalyzer::~SimplePi0DiscAnalyzer()
 {
-   delete rootFile_;
+
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
+   rootFile_->Write();
+   rootFile_->Close();
+}
+
+void SimplePi0DiscAnalyzer::beginJob(){
+
+  rootFile_->cd();
+  cout << "beginJob() ->  Book the Histograms" << endl;
+  
+  hConv_ntracks_ = new TH1F("nConvTracks","Number of tracks of converted Photons ",10,0.,10);
+  hAll_nnout_Assoc_ = new TH1F("All_nnout_Assoc","NNout for All Photons(AssociationMap)",100,0.,1.);
+  hAll_nnout_NoConv_Assoc_ = new TH1F("All_nnout_NoConv_Assoc","NNout for Unconverted Photons(AssociationMap)",100,0.,1.);
+  hAll_nnout_NoConv_Assoc_R9_ = new TH1F("All_nnout_NoConv_Assoc_R9","NNout for Unconverted Photons with R9>0.93 (AssociationMap)",100,0.,1.);
+  hBarrel_nnout_Assoc_ = new TH1F("barrel_nnout_Assoc","NNout for Barrel Photons(AssociationMap)",100,0.,1.);
+  hBarrel_nnout_NoConv_Assoc_ = new TH1F("barrel_nnout_NoConv_Assoc","NNout for Barrel Unconverted Photons(AssociationMap)",100,0.,1.);
+  hBarrel_nnout_NoConv_Assoc_R9_ = new TH1F("barrel_nnout_NoConv_Assoc_R9","NNout for Barrel Unconverted Photons with R9>0.93 (AssociationMap)",100,0.,1.);
+  hEndcNoPresh_nnout_Assoc_ = new TH1F("endcNoPresh_nnout_Assoc","NNout for Endcap NoPresh Photons(AssociationMap)",100,0.,1.);
+  hEndcNoPresh_nnout_NoConv_Assoc_ = new TH1F("endcNoPresh_nnout_NoConv_Assoc","NNout for Endcap Unconverted NoPresh Photons(AssociationMap)",100,0.,1.);
+  hEndcNoPresh_nnout_NoConv_Assoc_R9_ = new TH1F("endcNoPresh_nnout_NoConv_Assoc_R9","NNout for Endcap Unconverted NoPresh Photons with R9>0.93 (AssociationMap)",100,0.,1.);
+  hEndcWithPresh_nnout_Assoc_ = new TH1F("endcWithPresh_nnout_Assoc","NNout for Endcap WithPresh Photons(AssociationMap)",100,0.,1.);
+  hEndcWithPresh_nnout_NoConv_Assoc_ = new TH1F("endcWithPresh_nnout_NoConv_Assoc","NNout for Endcap Unconverted WithPresh Photons(AssociationMap)",100,0.,1.);
+  hEndcWithPresh_nnout_NoConv_Assoc_R9_ = new TH1F("endcWithPresh_nnout_NoConv_Assoc_R9","NNout for Endcap Unconverted WithPresh Photons with R9>0.93 (AssociationMap)",100,0.,1.);
 
 }
 
-//
-// member functions
-//
+void
+SimplePi0DiscAnalyzer::endJob(){
 
-// ------------ method called to for each event  ------------
+  rootFile_->cd();
+  cout << "endJob() ->  Write the Histograms" << endl;
+  hConv_ntracks_->Write();
+
+  hAll_nnout_Assoc_->Write();
+  hAll_nnout_NoConv_Assoc_->Write();
+  hAll_nnout_NoConv_Assoc_R9_->Write();
+  hBarrel_nnout_Assoc_->Write();
+  hBarrel_nnout_NoConv_Assoc_->Write();
+  hBarrel_nnout_NoConv_Assoc_R9_->Write();
+  hEndcNoPresh_nnout_Assoc_->Write();
+  hEndcNoPresh_nnout_NoConv_Assoc_->Write();
+  hEndcNoPresh_nnout_NoConv_Assoc_R9_->Write();
+  hEndcWithPresh_nnout_Assoc_->Write();
+  hEndcWithPresh_nnout_NoConv_Assoc_->Write();
+  hEndcWithPresh_nnout_NoConv_Assoc_R9_->Write();
+
+}
+
 void
 SimplePi0DiscAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   cout << endl;
   cout << " -------------- NEW EVENT : Run, Event =  " << iEvent.id() << endl;
 
-  // Get the ConvertedPhotonCollection
-  Handle<ConversionCollection> convertedPhotonHandle; // get the Converted Photon info
-  iEvent.getByLabel("conversions", "", convertedPhotonHandle);
-  const reco::ConversionCollection phoCollection = *(convertedPhotonHandle.product());
-
-  cout << " ---> ConvertedPhotonCollection.size() = " << phoCollection.size() << endl;
-
-  // Get the  corrected  photon collection
-  Handle<reco::PhotonCollection> correctedPhotonHandle;
-  iEvent.getByLabel(photonCorrCollectionProducer_, correctedPhotonCollection_ , correctedPhotonHandle);
-  const reco::PhotonCollection photons = *(correctedPhotonHandle.product());
+ Handle<reco::PhotonCollection> PhotonHandle;
+  iEvent.getByLabel(photonCollectionProducer_, photonCollection_ , PhotonHandle);
+  const reco::PhotonCollection photons = *(PhotonHandle.product());
 
   cout <<"----> Photons size: "<< photons.size()<<endl;
 
@@ -131,109 +129,75 @@ SimplePi0DiscAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
   iEvent.getByLabel("piZeroDiscriminators","PhotonPi0DiscriminatorAssociationMap",  map);
   reco::PhotonPi0DiscriminatorAssociationMap::const_iterator mapIter;
 
-  //UNUSED int PhoInd = 0;
-
-  /*
+  int PhoInd = 0;
 
   for( reco::PhotonCollection::const_iterator  iPho = photons.begin(); iPho != photons.end(); iPho++) { // Loop over Photons
 
     reco::Photon localPho(*iPho);
 
-    float Photon_eta = localPho.eta(); float Photon_phi = localPho.phi();
-    cout << "Photon Id = " << PhoInd << " with Energy = " << localPho.energy()
-         << " Et = " <<  localPho.energy()*sin(2*atan(exp(-Photon_eta)))
-         << " Eta = " << Photon_eta << " and Phi = " << Photon_phi << endl;
+    float Photon_et = localPho.et(); float Photon_eta = localPho.eta(); 
+    float Photon_phi = localPho.phi(); float Photon_r9 = localPho.r9();
+    bool isPhotConv  = localPho.hasConversionTracks();
+    cout << "Photon Id = " << PhoInd 
+         << " with Et = " << Photon_et 
+         << " Eta = " << Photon_eta 
+	 << " Phi = " << Photon_phi 
+	 << " R9 = " << Photon_r9 
+	 << " and conv_id = " << isPhotConv << endl;
+
 
     SuperClusterRef it_super = localPho.superCluster(); // get the SC related to the Photon candidate
 
-    bool isPhotConv = false;
-    int Ntrk_conv = 0;
-    int Conv_SCE_id = 0;
-    for( reco::ConversionCollection::const_iterator iCPho = phoCollection.begin();
-	 iCPho != phoCollection.end(); iCPho++) {
-       SuperClusterRef it_superConv = (*iCPho).superCluster();// get the SC related to the  Converted Photon candidate
-       if(it_super == it_superConv) {
-         isPhotConv = (*iCPho).isConverted();
-         Ntrk_conv = (*iCPho).tracks().size();
-	 break;
-       }
-       Conv_SCE_id++;
-    } // End of Photon Conversion Loop
-    hConv_ntracks_->Fill(Ntrk_conv);
+//    hConv_ntracks_->Fill(Ntrk_conv);
 
-    mapIter = map->find(edm::Ref<reco::PhotonCollection>(correctedPhotonHandle,PhoInd));
-    float nn = mapIter->val;
-    if(fabs(Photon_eta) <= 1.442) {
+    float nn = -10;
+    mapIter = map->find(edm::Ref<reco::PhotonCollection>(PhotonHandle,PhoInd));
+    if(mapIter!=map->end()) {
+      nn = mapIter->val;
+    }
+    if(fabs(it_super->eta()) <= 1.442) {
        hBarrel_nnout_Assoc_->Fill(nn);
        hAll_nnout_Assoc_->Fill(nn);
        cout << "AssociationMap Barrel NN = " << nn << endl;
-       if(isPhotConv) {
+       if(!isPhotConv) {
 	  hBarrel_nnout_NoConv_Assoc_->Fill(nn);
 	  hAll_nnout_NoConv_Assoc_->Fill(nn);
        }
-    } else if( (fabs(Photon_eta) >= 1.556 && fabs(Photon_eta) < 1.65) || fabs(Photon_eta) > 2.5) {
+       if(Photon_r9>0.93) {
+	  hBarrel_nnout_NoConv_Assoc_R9_->Fill(nn);
+	  hAll_nnout_NoConv_Assoc_R9_->Fill(nn);
+       }
+    } else if( (fabs(it_super->eta()) >= 1.556 && fabs(it_super->eta()) < 1.65) || fabs(it_super->eta()) > 2.5) {
        hEndcNoPresh_nnout_Assoc_->Fill(nn);
        hAll_nnout_Assoc_->Fill(nn);
        cout << "AssociationMap EndcNoPresh NN = " << nn << endl;
-       if(isPhotConv) {
+       if(!isPhotConv) {
 	  hEndcNoPresh_nnout_NoConv_Assoc_->Fill(nn);
 	  hAll_nnout_NoConv_Assoc_->Fill(nn);
        }
-    } else if(fabs(Photon_eta) >= 1.65 && fabs(Photon_eta) <= 2.5 ) {
+       if(Photon_r9>0.93) {
+	  hEndcNoPresh_nnout_NoConv_Assoc_R9_->Fill(nn);
+	  hAll_nnout_NoConv_Assoc_R9_->Fill(nn);
+       }       
+    } else if(fabs(it_super->eta()) >= 1.65 && fabs(it_super->eta()) <= 2.5 ) {
        hEndcWithPresh_nnout_Assoc_->Fill(nn);
        hAll_nnout_Assoc_->Fill(nn);
        cout << "AssociationMap EndcWithPresh NN = " << nn << endl;
-       if(isPhotConv) {
+       if(!isPhotConv) {
 	  hEndcWithPresh_nnout_NoConv_Assoc_->Fill(nn);
 	  hAll_nnout_NoConv_Assoc_->Fill(nn);
        }
+       if(Photon_r9>0.93) {
+	  hEndcWithPresh_nnout_NoConv_Assoc_R9_->Fill(nn);
+	  hAll_nnout_NoConv_Assoc_R9_->Fill(nn);
+       }       
     }
+
 
     PhoInd++;
   } // End Loop over Photons
 
-
-
-  */
-
 }
-
-// ------------ method called once each job just before starting event loop  ------------
-void SimplePi0DiscAnalyzer::beginJob()
-{
-  rootFile_->cd();
-
-  hConv_ntracks_ = new TH1F("nConvTracks","Number of tracks of converted Photons ",10,0.,10);
-  hAll_nnout_Assoc_ = new TH1F("All_nnout_Assoc","NNout for All Photons(AssociationMap)",100,0.,1.);
-  hAll_nnout_NoConv_Assoc_ = new TH1F("All_nnout_NoConv_Assoc","NNout for Unconverted Photons(AssociationMap)",100,0.,1.);
-  hBarrel_nnout_Assoc_ = new TH1F("barrel_nnout_Assoc","NNout for Barrel Photons(AssociationMap)",100,0.,1.);
-  hBarrel_nnout_NoConv_Assoc_ = new TH1F("barrel_nnout_NoConv_Assoc","NNout for Barrel Unconverted Photons(AssociationMap)",100,0.,1.);
-  hEndcNoPresh_nnout_Assoc_ = new TH1F("endcNoPresh_nnout_Assoc","NNout for Endcap NoPresh Photons(AssociationMap)",100,0.,1.);
-  hEndcNoPresh_nnout_NoConv_Assoc_ = new TH1F("endcNoPresh_nnout_NoConv_Assoc","NNout for Endcap Unconverted NoPresh Photons(AssociationMap)",100,0.,1.);
-  hEndcWithPresh_nnout_Assoc_ = new TH1F("endcWithPresh_nnout_Assoc","NNout for Endcap WithPresh Photons(AssociationMap)",100,0.,1.);
-  hEndcWithPresh_nnout_NoConv_Assoc_ = new TH1F("endcWithPresh_nnout_NoConv_Assoc","NNout for Endcap Unconverted WithPresh Photons(AssociationMap)",100,0.,1.);
-
-}
-
-// ------------ method called once each job just after ending the event loop  ------------
-void
-SimplePi0DiscAnalyzer::endJob() {
-  rootFile_->cd();
-
-  hConv_ntracks_->Write();
-
-  hAll_nnout_Assoc_->Write();
-  hAll_nnout_NoConv_Assoc_->Write();
-  hBarrel_nnout_Assoc_->Write();
-  hBarrel_nnout_NoConv_Assoc_->Write();
-  hEndcNoPresh_nnout_Assoc_->Write();
-  hEndcNoPresh_nnout_NoConv_Assoc_->Write();
-  hEndcWithPresh_nnout_Assoc_->Write();
-  hEndcWithPresh_nnout_NoConv_Assoc_->Write();
-
-  rootFile_->Close();
-
-}
-
 //define this as a plug-in
 DEFINE_FWK_MODULE(SimplePi0DiscAnalyzer);
+
