1820828f690b56e5036ed0a5885a2b29d7933fa3
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2009-05-26 12:53:56 +0000

    add parameters to manage the production and the use of the mixed source

--- SimGeneral/MixingModule/python/mixLowLumPU_cfi.py
+++ SimGeneral/MixingModule/python/mixLowLumPU_cfi.py
@@ -13,22 +13,26 @@ mix = cms.EDProducer("MixingModule",
 
     bunchspace = cms.int32(25), ## nsec
     checktof = cms.bool(False),
+    mixProdStep1 = cms.bool(False),
+    mixProdStep2 = cms.bool(False),
+
     playback = cms.untracked.bool(False),
     useCurrentProcessOnly = cms.bool(False),
                    
     input = cms.SecSource("PoolSource",
-        nbPileupEvents = cms.PSet(
+        #type = cms.string('fixed'),
+	#nbPileupEvents = cms.PSet(
+        #averageNumber = cms.double(1.0)
+    	#),
+	
+	nbPileupEvents = cms.PSet(
             sigmaInel = cms.double(80.0),
             Lumi = cms.double(2.8)
         ),
         seed = cms.int32(1234567),
         type = cms.string('poisson'),
-        fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/18890F4C-FD99-DD11-BFF9-000423D996C8.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/1A423C16-6099-DD11-9320-000423D9853C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/5EDA8A7F-5D99-DD11-B1CD-001617C3B706.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/600D1E6A-5F99-DD11-A7D5-000423D9890C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/68ECAE92-5F99-DD11-ACAB-000423D98E6C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/8802D325-5E99-DD11-B858-000423D98A44.root')
+        fileNames = cms.untracked.vstring(
+        'file:/data/becheva/MixingModule/dataFiles/relval/02C5A172-8203-DE11-86D7-001617C3B5D8TTBar.root')
     ),
     mixObjects = cms.PSet(
         mixCH = cms.PSet(
