aa3574e9013d8a84c5a0ca34cb054a0a20257eff
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-05-27 17:53:49 +0000

    Use GlobalTags for conditions

--- EventFilter/CSCRawToDigi/test/packer_cfg.py
+++ EventFilter/CSCRawToDigi/test/packer_cfg.py
@@ -1,16 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 # packs and unpacks data from a dataset which already has digis
 process = cms.Process("ANAL")
-process.load("EventFilter.CSCRawToDigi.cscFrontierCablingPack_cff")
-process.load("CalibMuon.Configuration.CSC_FakeDBConditions_cff")
-process.load("EventFilter.CSCRawToDigi.cscFrontierCablingUnpck_cff")
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'IDEAL_30X::All'
 
 
 process.load("EventFilter.CSCRawToDigi.cscPacker_cfi")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:simevent.root')
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre5/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/EC0724B2-AC2B-DE11-BDB4-000423D991F0.root')
 )
+process.maxEvents = cms.untracked.PSet(
+     input = cms.untracked.int32(1000)
+     )
+
 
 process.load("EventFilter.CSCRawToDigi.cscUnpacker_cfi")
 process.DQMStore = cms.Service("DQMStore")
