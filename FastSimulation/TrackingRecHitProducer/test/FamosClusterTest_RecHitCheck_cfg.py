10a45fef8f8005c2744fedda3c2632f6db6746b2
dorbaker <sha1-c0c861e1621fbd0445e7a5339f107fcc8dbfe745@cern.ch>
2009-05-27 17:46:35 +0000

    Updated config file to run in 3_1_0_pre7

--- FastSimulation/TrackingRecHitProducer/test/FamosClusterTest_RecHitCheck_cfg.py
+++ FastSimulation/TrackingRecHitProducer/test/FamosClusterTest_RecHitCheck_cfg.py
@@ -1,54 +1,70 @@
+#Config
+
 import FWCore.ParameterSet.Config as cms
 
-##################################################################
-# Put here the globaltag the file name and the number of events:
-
-gtag=cms.string('IDEAL_30X::All')
-
-inputfiles=cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre6/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0002/B2B57D9D-1933-DE11-A285-000423D99CEE.root')
-#'file:/d1/dorbaker/test_mu_10GeV_3_1_0_pre1.root')
-secinputfiles=cms.untracked.vstring()
-nevents=cms.untracked.int32(10)
-###################################################################
-
 process = cms.Process("TrackerValidationOnly")
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.destinations = ['detailedInfoRecHitVal.txt']
-process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
+
+# The number of events to be processed.
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
 process.load("Configuration.StandardSequences.Services_cff")
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+# Include the RandomNumberGeneratorService definition
+process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
+
+process.load("FastSimulation/Configuration/CommonInputs_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = gtag
-process.load("SimTracker.Configuration.SimTracker_cff")
+process.GlobalTag.globaltag = "IDEAL_31X::All"
+process.load("FastSimulation/Configuration/FamosSequences_cff")
+process.load("FastSimulation/TrackingRecHitProducer/FamosClusterTest_cff")
+
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+#process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = gtag
+#process.load("SimTracker.Configuration.SimTracker_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("RecoTracker.Configuration.RecoTracker_cff")
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
 process.load("Validation.TrackerRecHits.trackerRecHitsValidation_cff")
 process.load("Validation.RecoTrack.SiTrackingRecHitsValid_cff")
-process.load("FastSimulation.Configuration.FamosSequences_cff")
-process.load("FastSimulation.TrackingRecHitProducer.FamosClusterTest_cff")
-process.load("FastSimulation.Configuration.mixNoPU_cfi")
+
 
 #CPEs
 process.load("FastSimulation.TrackingRecHitProducer.FastPixelCPE_cfi")
 process.load("FastSimulation.TrackingRecHitProducer.FastStripCPE_cfi")
 
-process.maxEvents = cms.untracked.PSet(
-    input = nevents
-)
+#process.maxEvents = cms.untracked.PSet(
+#    input = nevents
+#    )
+#process.source = cms.Source("PoolSource",
+#                            fileNames = inputfiles,
+#     secondaryFileNames = secinputfiles                          
+ #                           )
+
 process.source = cms.Source("PoolSource",
-    fileNames = inputfiles,
-     secondaryFileNames = secinputfiles                          
-)
+                            debugFlag = cms.untracked.bool(True),
+                            debugVebosity = cms.untracked.uint32(10),
+                            fileNames = cms.untracked.vstring(
+    '/store/relval/CMSSW_3_1_0_pre7/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0004/B89FA4AB-CC41-DE11-8348-000423D98B28.root',
+    '/store/relval/CMSSW_3_1_0_pre7/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0004/58AEC160-E741-DE11-AE53-001D09F2AF1E.root',
+    '/store/relval/CMSSW_3_1_0_pre7/RelValSingleMuPt10/GEN-SIM-RECO/IDEAL_31X_v1/0004/50F087DB-CB41-DE11-98F2-0030487C5CFA.root'
+    )
+                            )
+
 
 process.PixelTrackingRecHitsValid.src = 'TrackRefitter'
 process.StripTrackingRecHitsValid.trajectoryInput = 'TrackRefitter'
 
-process.pixRecHitsValid.outputFile='pixelrechitshisto_310_pre6.root'
-process.stripRecHitsValid.outputFile='sistriprechitshisto_310_pre6.root'
-process.PixelTrackingRecHitsValid.outputFile='pixeltrackingrechitshist_310_pre6.root'
-process.StripTrackingRecHitsValid.outputFile='striptrackingrechitshisto_310_pre6.root'
-process.rechits = cms.Sequence(process.famosWithTrackerHits*process.siClusterTranslator*process.siPixelRecHits*process.siStripMatchedRecHits*process.trackerRecHitsValidation)
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.destinations = ['detailedInfoRecHitVal.txt']
+
+process.pixRecHitsValid.outputFile='pixelrechitshisto_310_pre7.root'
+process.stripRecHitsValid.outputFile='sistriprechitshisto_310_pre7.root'
+process.PixelTrackingRecHitsValid.outputFile='pixeltrackingrechitshist_310_pre7.root'
+process.StripTrackingRecHitsValid.outputFile='striptrackingrechitshisto_310_pre7.root'
 process.trackinghits = cms.Sequence(process.TrackRefitter*process.trackingRecHitsValid)
+process.rechits = cms.Sequence(process.famosWithTrackerHits*process.siClusterTranslator*process.siPixelRecHits*process.siStripMatchedRecHits*process.trackerRecHitsValidation*process.ckftracks*process.trackinghits)
 process.p1 = cms.Path(process.rechits)
