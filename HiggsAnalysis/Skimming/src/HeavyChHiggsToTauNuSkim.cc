c1a58cbffcf604aeb49bac58b69706e71cdf6b2f
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2009-05-28 21:48:32 +0000

    Changed the heavy H+ skim to select HLT singleTau trigger bit fired and 4 offline jets (one of which is the tau). Added the background analysis skim: HLT single muon trigger fired + 3 jets, needed for muon->tau replacement study.

--- HiggsAnalysis/Skimming/src/HeavyChHiggsToTauNuSkim.cc
+++ HiggsAnalysis/Skimming/src/HeavyChHiggsToTauNuSkim.cc
@@ -5,6 +5,8 @@
  *
  *  \author Sami Lehti  -  HIP Helsinki
  *
+ *  Updated May 25, 2009/S.Lehti
+ *
  */
 
 #include "HiggsAnalysis/Skimming/interface/HeavyChHiggsToTauNuSkim.h"
@@ -12,7 +14,6 @@
 #include "DataFormats/BTauReco/interface/IsolatedTauTagInfo.h"
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "DataFormats/BTauReco/interface/JetTag.h"
-//#include "DataFormats/BTauReco/interface/JetTagFwd.h"
 #include "DataFormats/JetReco/interface/Jet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 
@@ -20,23 +21,25 @@
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
+#include "DataFormats/L1Trigger/interface/L1JetParticle.h"
+#include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "FWCore/Framework/interface/TriggerNames.h"
+
 using namespace edm;
 using namespace std;
 using namespace reco;
-
 
 HeavyChHiggsToTauNuSkim::HeavyChHiggsToTauNuSkim(const edm::ParameterSet& iConfig) {
 
 	// Local Debug flag
 	debug           = iConfig.getParameter<bool>("DebugHeavyChHiggsToTauNuSkim");
 
-	hltTauLabel	= iConfig.getParameter<InputTag>("HLTTauCollection");
 	jetLabel        = iConfig.getParameter<InputTag>("JetTagCollection");
 	minNumberOfjets = iConfig.getParameter<int>("minNumberOfJets");
 	jetEtMin        = iConfig.getParameter<double>("jetEtMin");
 	jetEtaMin       = iConfig.getParameter<double>("jetEtaMin");
 	jetEtaMax       = iConfig.getParameter<double>("jetEtaMax");
-	minDRFromTau    = iConfig.getParameter<double>("minDRFromTau");
 
         nEvents         = 0;
         nSelectedEvents = 0;
@@ -45,66 +48,41 @@ HeavyChHiggsToTauNuSkim::HeavyChHiggsToTauNuSkim(const edm::ParameterSet& iConfi
 
 
 HeavyChHiggsToTauNuSkim::~HeavyChHiggsToTauNuSkim(){
-  edm::LogVerbatim("HeavyChHiggsToTauNuSkim") 
-  << " Number_events_read " << nEvents
-  << " Number_events_kept " << nSelectedEvents
-  << " Efficiency         " << ((double)nSelectedEvents)/((double) nEvents + 0.01) << std::endl;
+  	edm::LogVerbatim("HeavyChHiggsToTauNuSkim") 
+  	<< " Number_events_read " << nEvents
+  	<< " Number_events_kept " << nSelectedEvents
+  	<< " Efficiency         " << ((double)nSelectedEvents)/((double) nEvents + 0.01) << std::endl;
 
 }
 
 
 bool HeavyChHiggsToTauNuSkim::filter(edm::Event& iEvent, const edm::EventSetup& iSetup ){
 
-  nEvents++;
+  	nEvents++;
 
-  Handle<IsolatedTauTagInfoCollection> tauTagL3Handle;
-  iEvent.getByLabel(hltTauLabel, tauTagL3Handle);
-
-  if ( !tauTagL3Handle.isValid() ) return false;
-
-
-  Jet theTau;
-  double maxEt = 0;
-  if (tauTagL3Handle.isValid() ) {
-    const IsolatedTauTagInfoCollection & L3Taus = *(tauTagL3Handle.product());
-    IsolatedTauTagInfoCollection::const_iterator i;
-    for ( i = L3Taus.begin(); i != L3Taus.end(); i++ ) {
-      if (i->discriminator() == 0) continue;
-      Jet taujet = *(i->jet().get());
-      if (taujet.et() > maxEt) {
-        maxEt = taujet.et();
-        theTau = taujet;
-      }
-    }
-  }
+  	// jets
 	
-  if (maxEt == 0) return false;
-
-  // jets
-	
-  Handle<CaloJetCollection> jetHandle;	
-  iEvent.getByLabel(jetLabel,jetHandle);
+  	Handle<CaloJetCollection> jetHandle;	
+  	iEvent.getByLabel(jetLabel,jetHandle);
   
-  if ( !jetHandle.isValid() ) return false;
+  	if ( !jetHandle.isValid() ) return false;
 	
-  bool accepted = false;
-	
-  if (jetHandle.isValid() ) {
-    int nJets = 0;
-    const reco::CaloJetCollection & jets = *(jetHandle.product());
-    CaloJetCollection::const_iterator iJet;
-    for (iJet = jets.begin(); iJet!= jets.end(); iJet++ ) {
-      if (iJet->et()  > jetEtMin  &&
-          iJet->eta() > jetEtaMin &&
-	  iJet->eta() < jetEtaMax ) {
-        double DR = deltaR(theTau.eta(),iJet->eta(),theTau.phi(),iJet->phi());
-        if (DR > minDRFromTau) nJets++;		
-      }
-    }
-    if (nJets >= minNumberOfjets) {
-      accepted = true;
-      nSelectedEvents++;
-    }	
-  }
-  return accepted;
+  	bool accepted = false;
+
+  	if (jetHandle.isValid() ) {
+    		int nJets = 0;
+    		const reco::CaloJetCollection & jets = *(jetHandle.product());
+    		CaloJetCollection::const_iterator iJet;
+    		for (iJet = jets.begin(); iJet!= jets.end(); iJet++ ) {
+      			if (iJet->et()  > jetEtMin  &&
+          		    iJet->eta() > jetEtaMin &&
+	  		    iJet->eta() < jetEtaMax ) nJets++;
+    		}
+    		if (nJets >= minNumberOfjets) {
+      			accepted = true;
+      			nSelectedEvents++;
+    		}	
+  	}
+
+  	return accepted;
 }
