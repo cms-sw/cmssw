e50e19834cd49d03fee2f49703e1e57210a3d20c
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2009-05-29 17:58:43 +0000

    Removed RPCHwConfig_cff in order to remove dependence of production executables on CondTool.  L1RPCHwConfig is included in global tag now.

--- L1Trigger/RPCTrigger/python/rpcTriggerDigis_cff.py
+++ L1Trigger/RPCTrigger/python/rpcTriggerDigis_cff.py
@@ -2,4 +2,3 @@ import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.RPCTrigger.rpcTriggerDigis_cfi import *
 from L1TriggerConfig.RPCTriggerConfig.RPCConeConfig_cff import *
-from L1TriggerConfig.RPCTriggerConfig.RPCHwConfig_cff import *
