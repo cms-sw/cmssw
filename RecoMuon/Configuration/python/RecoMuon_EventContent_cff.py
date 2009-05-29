a84d8bd4e98a7cf55238db1a95beae21210fbef9
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2009-05-29 20:58:18 +0000

    keep muIDmaps  in AOD (boolean value maps corresponding to muon::isGood(muon, type))

--- RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
@@ -32,6 +32,7 @@ RecoMuonAOD = cms.PSet(
                                            'keep recoTracksToOnerecoTracksAssociation_tevMuons_*_*', 
                                            'keep recoTracks_generalTracks_*_*', 
                                            'keep recoMuons_muons_*_*', 
+                                           'keep booledmValueMap_muid*_*_*',
                                            'keep recoMuonTimeExtraedmValueMap_muontiming_*_*',
                                            'keep recoCaloMuons_calomuons_*_*',
                                            # Cosmics
