accca6326e2d02ac699645d68804a9f59b83acdf
Tulika Bose <sha1-8b8c25cc70f9a5790e3c425b034aa99249523492@cern.ch>
2009-06-02 23:32:19 +0000

    Setting correct LUT generation mode for offline

--- SimCalorimetry/HcalTrigPrimProducers/python/hcaltpdigi_cff.py
+++ SimCalorimetry/HcalTrigPrimProducers/python/hcaltpdigi_cff.py
@@ -9,7 +9,7 @@ from CalibCalorimetry.CaloTPG.CaloTPGTranscoder_cfi import *
 HcalTPGCoderULUT = cms.ESProducer("HcalTPGCoderULUT",
     read_Ascii_LUTs = cms.bool(False),
     read_XML_LUTs = cms.bool(False),
-    LUTGenerationMode = cms.bool(False),
+    LUTGenerationMode = cms.bool(True),
     TagName = cms.string("LUTFromHCALDb"),
     AlgoName = cms.string("LUTFromHCALDb"),
     inputLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/inputLUTcoder_physics.dat'),
