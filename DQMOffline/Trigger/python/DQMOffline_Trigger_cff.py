82efc0e2f39136716159bbb7c797625c5bc030c3
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-06-04 15:50:26 +0000

    Add jetMET source and client to sequences. Using MuonTrigRateAnalyzer_cosmics_cfi

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
@@ -19,12 +19,14 @@ l1tgct.gctIsoEmSource = 'gctDigis:isoEm:'
 l1tgct.gctNonIsoEmSource = 'gctDigis:nonIsoEm:'
 l1trct.rctSource = 'gctDigis::'
 
-from DQMOffline.Trigger.FourVectorHLTOffline_cfi import *
-from DQMOffline.Trigger.HLTTauDQMOffline_cff import *
-from DQMOffline.Trigger.EgHLTOfflineSource_cfi import *
-from DQMOffline.Trigger.MuonTrigRateAnalyzer_cfi import *
 from DQM.L1TMonitor.L1TDEMON_cfi import *
+from DQMOffline.Trigger.FourVectorHLTOffline_cfi import *
+from DQMOffline.Trigger.EgHLTOfflineSource_cfi import *
+#from DQMOffline.Trigger.MuonTrigRateAnalyzer_cfi import *
+from DQMOffline.Trigger.MuonTrigRateAnalyzer_cosmics_cfi import *
+from DQMOffline.Trigger.HLTTauDQMOffline_cff import *
+from DQMOffline.Trigger.JetMETHLTOfflineSource_cfi import *
 l1temumonitor = cms.Sequence(l1demon) 
 
-triggerOfflineDQMSource = cms.Sequence(l1temumonitor*l1tmonitor*hltResults*egHLTOffDQMSource*offlineDQMMuonTrig*HLTTauDQMOffline)
+triggerOfflineDQMSource = cms.Sequence(l1temumonitor*l1tmonitor*hltResults*egHLTOffDQMSource*offlineDQMMuonTrig*HLTTauDQMOffline*jetMETHLTOfflineSource )
 
