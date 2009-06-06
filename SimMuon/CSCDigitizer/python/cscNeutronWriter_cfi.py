800971217ee2a9f7bd967f533749611433d6a47a
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-06-06 02:30:31 +0000

    Use EDMNeutronWriter

--- SimMuon/CSCDigitizer/python/cscNeutronWriter_cfi.py
+++ SimMuon/CSCDigitizer/python/cscNeutronWriter_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 cscNeutronWriter = cms.EDFilter("CSCNeutronWriter",
     timeWindow = cms.double(20.0),
     nChamberTypes = cms.int32(10),
-    writer = cms.string('ROOT'),
+    writer = cms.string('EDM'),
     output = cms.string('CSCNeutronHits.root'),
     neutronTimeCut = cms.double(250.0),
     input = cms.InputTag("g4SimHits","MuonCSCHits")
