7ddff20b3c55dfaa52f8f29630910f077f694ef7
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-07 14:06:39 +0000

    one more crap

---
+++ CondCore/IOVService/src/T_EventSetup_KeyList.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondCore/IOVService/interface/KeyList.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(cond::KeyList);
---
+++ CondFormats/Common/src/T_EventSetup_BaseKeyed.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondFormats/Common/interface/BaseKeyed.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(condex::BaseKeyed);
