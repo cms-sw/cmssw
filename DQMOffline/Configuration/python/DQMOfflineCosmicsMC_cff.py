c0213526f0319255d9fe0e1381ac0d7707736f64
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-06-07 19:21:24 +0000

    update SiStrip and Hcal

--- DQMOffline/Configuration/python/DQMOfflineCosmicsMC_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmicsMC_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.Configuration.DQMOfflineCosmics_cff import *
 
 siStripFEDCheck.RawDataTag = 'rawDataCollector'
+siStripFEDMonitor.RawDataTag = 'rawDataCollector'
 SiPixelHLTSource.RawInput = 'rawDataCollector'
 dqmCSCClient.InputObjects = 'rawDataCollector'
 cscDQMEvF.InputObjects = 'rawDataCollector'
@@ -10,5 +11,6 @@ ecalBarrelHltTask.FEDRawDataCollection = 'rawDataCollector'
 ecalBarrelSelectiveReadoutTask.FEDRawDataCollection = 'rawDataCollector'
 ecalEndcapHltTask.FEDRawDataCollection = 'rawDataCollector'
 ecalEndcapSelectiveReadoutTask.FEDRawDataCollection = 'rawDataCollector'
-dtDataIntegrityUnpacker.inputLabel = cms.untracked.InputTag('rawDataCollector') 
+dtDataIntegrityUnpacker.inputLabel = cms.untracked.InputTag('rawDataCollector')
+hcalMonitor.FEDRawDataCollection = cms.untracked.InputTag('rawDataCollector')
 
