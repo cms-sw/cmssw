8511713c9e573e7718959d51092392d09b03aa8c
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-06-08 15:54:03 +0000

    new unit test for checking that InputTag cache is correct even if data product not found

---
+++ FWCore/Framework/test/test_InputTag_cache_failure_cfg.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Test")
+
+#we want to continue processing after a 'ProductNotFound' exception in order
+# to test what happens to the cache held by the InputTag
+import FWCore.Framework.test.cmsExceptionsFatalOption_cff
+process.options = cms.untracked.PSet(
+    Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
+)
+process.options.Rethrow.remove('ProductNotFound')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(3)
+)
+process.source = cms.Source("EmptySource",
+    timeBetweenEvents = cms.untracked.uint32(10),
+    firstTime = cms.untracked.uint32(1000000)
+)
+
+process.double = cms.EDProducer("ToyDoubleProducer", 
+                                dvalue = cms.double(1.0)
+)
+
+process.doubleTwo = cms.EDProducer("ToyDoubleProducer", 
+                                    dvalue = cms.double(1.0)
+)
+
+#Need to add an Int producer whose module label is 'beyond' what IntTestAnalyzer is looking for
+process.zInt = cms.EDProducer("IntProducer", ivalue=cms.int32(26))
+
+process.getOne = cms.EDAnalyzer("IntTestAnalyzer",
+    valueMustMatch = cms.untracked.int32(1),
+    moduleLabel = cms.untracked.string('one')
+)
+
+process.p = cms.Path(process.double+process.doubleTwo+process.zInt+process.getOne)
