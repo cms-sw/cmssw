b9968d0fc85282ff392918d61f8a09ab03f497f8
Dirk Dammann <sha1-69d6dd543b0d405e6ee646084bf67e930e968dab@cern.ch>
2009-06-08 12:59:45 +0000

    test sequence for TtFullLeptonicKinSolutionProducer

---
+++ TopQuarkAnalysis/TopKinFitter/test/ttFullLepKinSolutionProducer_cfg.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+
+## add message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+
+## define input
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    '/store/mc/Fall08/TTJets-madgraph/GEN-SIM-RECO/IDEAL_V11_redigi_v10/0000/06FC3959-4DFC-DD11-B504-00E08178C091.root'
+    )
+)
+## define maximal number of events to loop over
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(False)
+)
+
+## configure geometry & conditions
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('IDEAL_V12::All')
+
+## std sequence for pat
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+## std sequence to produce the kinematic solution for fully leptonic events
+process.load("TopQuarkAnalysis.TopKinFitter.TtFullLepKinSolutionProducer_cfi")
+
+## process path
+process.p = cms.Path(process.patDefaultSequence *
+                     process.kinSolutionTtFullLepEvent
+                     )
+
+## configure output module
+process.out = cms.OutputModule("PoolOutputModule",
+    SelectEvents   = cms.untracked.PSet(SelectEvents = cms.vstring('p') ),                               
+    fileName = cms.untracked.string('ttFullLepKinSolutionProducer.root'),
+    outputCommands = cms.untracked.vstring('drop *')
+)
+process.out.outputCommands += ['keep *_kinSolutionTtFullLepEvent_*_*']
+
+## output path
+process.outpath = cms.EndPath(process.out)
