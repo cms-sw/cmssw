d731367f6dd080bdd2ad883eb7e7edfe16c2c5a3
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-06-08 19:01:51 +0000

    added as example for the use of electrons with PAT

---
+++ PhysicsTools/PatExamples/plugins/PatElectronAnalzyer.cc
@@ -0,0 +1,182 @@
+// to be run with this thing:
+// /Zee_M20/Summer08_IDEAL_V11_redigi_v1/GEN-SIM-RECO
+
+#include "TH1.h"
+#include "TH2.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+class PatElectronAnalyzer : public edm::EDAnalyzer {
+
+ public:
+  
+  explicit PatElectronAnalyzer(const edm::ParameterSet&);
+  ~PatElectronAnalyzer();
+  
+ private:
+  
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+  // source of electrons
+  edm::InputTag electronSrc_;
+
+  // decide in which mode to run the analyzer
+  // 0:plain, 1:genMatch, 2:tagAndProbe are 
+  // supported depending on the line comments
+  unsigned int mode_;
+  edm::ParameterSet genMatchMode_;
+  edm::ParameterSet tagAndProbeMode_;
+
+  // internal variables for genMatchMode and
+  // tagAndProbeMode 
+  double maxDeltaR_;
+  double maxDeltaM_;
+  double maxTagIso_;
+  
+  // book histograms of interest
+  TH1I *nr_;
+  TH1F *pt_;
+  TH1F *eta_;
+  TH1F *phi_;
+  TH1F *deltaR_;
+  TH1F *isoTag_;
+  TH1F *invMass_;
+  TH1F *deltaPhi_;
+};  
+
+#include "Math/VectorUtil.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+
+PatElectronAnalyzer::PatElectronAnalyzer(const edm::ParameterSet& cfg):
+  electronSrc_ (cfg.getParameter<edm::InputTag>("electronSrc")),
+  genMatchMode_(cfg.getParameter<edm::ParameterSet>("genMatchMode")),
+  tagAndProbeMode_(cfg.getParameter<edm::ParameterSet>("tagAndProbeMode"))
+{
+  // complete the configuration of the analyzer
+  maxDeltaR_ = genMatchMode_   .getParameter<double>("maxDeltaR"); 
+  maxDeltaM_ = tagAndProbeMode_.getParameter<double>("maxDeltaM"); 
+  maxTagIso_ = tagAndProbeMode_.getParameter<double>("maxTagIso"); 
+
+  // register histograms to the TFileService
+  edm::Service<TFileService> fs;
+  
+  nr_      = fs->make<TH1I>("nr",       "nr(e)",     10,   0 ,  10 );
+  pt_      = fs->make<TH1F>("pt",       "p(e)",      20,   0., 100.);
+  eta_     = fs->make<TH1F>("eta",      "eta(e)",    30,  -3.,   3.);
+  phi_     = fs->make<TH1F>("phi",      "phi",       35, -3.5,  3.5);
+  deltaR_  = fs->make<TH1F>("deltaR",   "deltaR",    50,   0.,   5.);
+  isoTag_  = fs->make<TH1F>("isoTag",   "iso(e)",    50,   0.,  10.);
+  invMass_ = fs->make<TH1F>("invMass",  "m(ee)",    100,  50., 150.);
+  deltaPhi_= fs->make<TH1F>("deltaPhi", "deltaPhi", 100, -3.5,  3.5);
+}
+
+PatElectronAnalyzer::~PatElectronAnalyzer()
+{
+}
+
+void
+PatElectronAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& setup)
+{       
+  // get electron collection
+  edm::Handle<std::vector<pat::Electron> > electrons;
+  evt.getByLabel(electronSrc_, electrons); 
+
+  nr_->Fill( electrons->size() );
+  for( std::vector<pat::Electron>::const_iterator elec=electrons->begin();
+       elec!=electrons->end(); ++elec){
+    //
+    // uncomment the following lines to enable >> Mode 0: plain
+    //
+    if( mode_==0 ){
+      // uncomment the following line and choose a type
+      // to require a certain electronID; the following 
+      // types are available:
+      // * eidRobustLoose
+      // * eidRobustTight
+      // * eidLoose
+      // * eidTight
+      // * eidRobustHighEnergy
+      //if( !elec->electronID("dummyName")>0.5 ) continue;
+      pt_ ->Fill( elec->pt()  );
+      eta_->Fill( elec->eta() );
+      phi_->Fill( elec->phi() );
+    }
+
+    //
+    // uncomment the following lines to enable >> Mode 1: genMatch
+    //
+    if( mode_==1 ){
+      if( elec->genLepton() ){
+	float deltaR = ROOT::Math::VectorUtil::DeltaR(elec->genLepton()->p4(), elec->p4());
+	deltaR_->Fill(deltaR);	
+	if( deltaR<maxDeltaR_ ){
+	  // uncomment the following line and choose a type
+	  // to require a certain electronID; the following 
+	  // types are available:
+	  // * eidRobustLoose
+	  // * eidRobustTight
+	  // * eidLoose
+	  // * eidTight
+	  // * eidRobustHighEnergy
+	  //if( !elec->electronID("dummyName")>0.5 ) continue;
+	  pt_ ->Fill( elec->pt()  );
+	  eta_->Fill( elec->eta() );
+	  phi_->Fill( elec->phi() );
+	}
+      }
+
+    //
+    // uncomment the following lines to enable >> Mode 2: tagAndProbe
+    //
+      if( mode_==2 ){
+	isoTag_->Fill(elec->trackIso());	
+	if( elec->trackIso()<maxTagIso_  && elec->electronID("eidTight")>0.5 ){
+	  for( std::vector<pat::Electron>::const_iterator probe=electrons->begin();
+	       probe!=electrons->end(); ++probe){
+	    // skip the tagged electron itself
+	    if( probe==elec ) continue;
+
+	    float zMass = (probe->p4()+elec->p4()).mass();
+	    invMass_ ->Fill(zMass);	
+	    float deltaPhi = ROOT::Math::VectorUtil::DeltaPhi(elec->p4(), probe->p4());
+	    deltaPhi_->Fill(deltaPhi);	
+	    
+	    // check for the Z mass
+	    if( fabs( zMass-90. )<maxDeltaM_ ){
+	      // uncomment the following line and choose a type
+	      // to require a certain electronID; the following 
+	      // types are available:
+	      // * eidRobustLoose
+	      // * eidRobustTight
+	      // * eidLoose
+	      // * eidTight
+	      // * eidRobustHighEnergy
+	      //if( !elec->electronID("dummyName")>0.5 ) continue;
+	      pt_ ->Fill( elec->pt()  );
+	      eta_->Fill( elec->eta() );
+	      phi_->Fill( elec->phi() );
+	    }
+	  }
+	}
+      }
+    }
+  }
+}
+
+void PatElectronAnalyzer::beginJob(const edm::EventSetup&)
+{
+}
+
+void PatElectronAnalyzer::endJob()
+{
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(PatElectronAnalyzer);
