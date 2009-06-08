28dbea015128a15c6bf70f898be36eaaa4ef9c0b
Dirk Dammann <sha1-69d6dd543b0d405e6ee646084bf67e930e968dab@cern.ch>
2009-06-08 12:51:34 +0000

    module for kinematic solution for fully leptonic evts

---
+++ TopQuarkAnalysis/TopKinFitter/python/TtFullLepKinSolutionProducer_cfi.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+
+kinSolutionTtFullLepEvent = cms.EDProducer("TtFullLepKinSolutionProducer",
+    jets = cms.InputTag("selectedLayer1Jets"),    
+    electrons = cms.InputTag("selectedLayer1Electrons"),
+    muons = cms.InputTag("selectedLayer1Muons"),       
+    mets = cms.InputTag("layer1METs"),
+
+    # ------------------------------------------------
+    # maximum number of jets to be considered in the
+    # jet combinatorics (has to be >= 2
+    # ------------------------------------------------
+    maxNJets = cms.int32(2),
+    
+    #-------------------------------------------------       
+    # maximum number of jet combinations finally
+    # written into the event, starting from the "best"
+    # (has to be >= 1, can be set to -1 if you want to 
+    # take all)
+    #-------------------------------------------------
+    maxNComb = cms.int32(1),
+
+    # ------------------------------------------------
+    # Select if also solutions with wrong charge lepton 
+    # combinations are to be looked for, e.g. solutions
+    # with 2 equally charged leptons
+    # ------------------------------------------------
+    searchWrongCharge = cms.bool(False),
+    
+    # ------------------------------------------------
+    # Channel(s) for which solutions are searched
+    # ------------------------------------------------
+    eeChannel     = cms.bool(False),
+    emuChannel    = cms.bool(False),
+    mumuChannel   = cms.bool(False),
+    etauChannel   = cms.bool(False),
+    mutauChannel  = cms.bool(False),
+    tautauChannel = cms.bool(False),
+    
+    # ------------------------------------------------
+    # settings for the KinSolver
+    # ------------------------------------------------    
+    tmassbegin = cms.double(100),
+    tmassend   = cms.double(300),
+    tmassstep  = cms.double(  1)
+)
+
+
