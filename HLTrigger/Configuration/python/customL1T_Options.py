545fd64071d24cf0774f434bc5da68fe5ec8bfe4
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-06-08 15:02:58 +0000

    Fixup of test cfg file generation

--- HLTrigger/Configuration/python/customL1T_Options.py
+++ HLTrigger/Configuration/python/customL1T_Options.py
@@ -1,16 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 def customise(process):
-    
-    process.l1GtTrigReport = cms.EDAnalyzer( "L1GtTrigReport",
-        UseL1GlobalTriggerRecord = cms.bool( False ),
-        L1GtRecordInputTag = cms.InputTag( "simGtDigis" ),
-        PrintOutput = cms.untracked.int32( 0 )
-    )
-    process.L1AnalyzerEndpath = cms.EndPath (process.l1GtTrigReport)
-    process.schedule.append(process.L1AnalyzerEndpath)
-    
+
     process.options.wantSummary = cms.untracked.bool(True)
+
+    process.load('L1Trigger.GlobalTriggerAnalyzer.l1GtTrigReport_cfi')
+    process.l1GtTrigReport.L1GtRecordInputTag = cms.InputTag( "simGtDigis" )
+
+    process.L1AnalyzerEndpath = cms.EndPath( process.l1GtTrigReport )
+    process.schedule.append(process.L1AnalyzerEndpath)
 
     process.MessageLogger.categories.append('L1GtTrigReport')
 
