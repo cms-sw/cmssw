512caf059b36168551a376a55efa19961f133b99
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-06-09 23:32:54 +0000

    Geometry routine to place the modules in the stack layers with the central gap (ring stack layers)

---
+++ SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinearGap.h
@@ -0,0 +1,40 @@
+#ifndef DD_PixBarStackLinearGap_h
+#define DD_PixBarStackLinearGap_h
+
+#include <map>
+#include <string>
+#include <vector>
+#include "DetectorDescription/Base/interface/DDTypes.h"
+#include "DetectorDescription/Algorithm/interface/DDAlgorithm.h"
+
+class DDPixBarStackLinearGap : public DDAlgorithm {
+ public:
+  //Constructor and Destructor
+  DDPixBarStackLinearGap(); 
+  virtual ~DDPixBarStackLinearGap();
+  
+  void initialize(const DDNumericArguments & nArgs,
+		  const DDVectorArguments & vArgs,
+		  const DDMapArguments & mArgs,
+		  const DDStringArguments & sArgs,
+		  const DDStringVectorArguments & vsArgs);
+
+  void execute();
+
+private:
+
+  std::string              idNameSpace; //Namespace of this and ALL sub-parts
+  std::string              childName;   //Child name
+  int                      number;      //Number of positioning
+  double                   theta;       //Direction of translation
+  double                   phi;         //  ......
+  double                   offset;      //Offset    along (theta,phi) direction
+  double                   delta;       //Increment     ................
+  std::vector<double>      centre;      //Centre
+  std::string              rotMat;      //Rotation matrix
+  double                   zoffset;     //Offset of modules in y
+  double		   stackoffset; //Offset of modules to compensate for eta
+  int			   stackoffsetT;//Period of offset of modules to compensate for eta
+};
+
+#endif
