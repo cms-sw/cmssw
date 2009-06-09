d46178e585ba42584327cebf98950a1728530cd6
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-06-09 11:50:53 +0000

    loosened HF noise cuts

--- RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hf_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hf_cfi.py
@@ -19,13 +19,13 @@ hfreco = cms.EDFilter("HcalHitReconstructor",
                       digistat= cms.PSet(
                         HFpulsetimemin     = cms.int32(0),
                         HFpulsetimemax     = cms.int32(10), # min/max time slice values for peak
-                        HFratio_beforepeak = cms.double(0.1), # max allowed ratio
+                        HFratio_beforepeak = cms.double(0.6), # max allowed ratio (started at 0.1, loosened to 0.6 after pion studies)
                         HFratio_afterpeak  = cms.double(1.0), # max allowed ratio
                         HFadcthreshold       = cms.int32(10), # minimum size of peak (in ADC counts, after ped subtraction) to be considered noisy
                       ),
                       rechitstat=cms.PSet(
                         HFlongshortratio = cms.double(0.99), # max allowed ratio of (L-S)/(L+S)
-                        HFthresholdET = cms.double(0.50), # minimum energy (in GeV) required for a cell to be considered hot
+                        HFthresholdET = cms.double(2.0), # minimum energy (in GeV) required for a cell to be considered hot (started at 0.5, loosened to 2.0 after pion studies)
                       ),
                       saturationParameters=  cms.PSet(maxADCvalue=cms.int32(127))                      
                   )
