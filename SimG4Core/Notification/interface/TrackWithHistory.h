8648bcda74397143bf292f34b0b39a3c5b6d34fc
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-06-10 08:17:09 +0000

    Add a method to set externally the genParticleID

--- SimG4Core/Notification/interface/TrackWithHistory.h
+++ SimG4Core/Notification/interface/TrackWithHistory.h
@@ -38,8 +38,9 @@ public:
     double properTime() const                       { return properTime_; }
     const G4VProcess * creatorProcess() const       { return creatorProcess_; }
     double weight() const                           { return weight_; }
-    void setTrackID(int i)  			    { trackID_ = i; }
-    void setParentID(int i)			    { parentID_ = i; }
+    void setTrackID(int i)  			            { trackID_ = i; }
+    void setParentID(int i)			                { parentID_ = i; }
+    void setGenParticleID(int i)                    { genParticleID_ = i; }
     bool storeTrack() const                         { return storeTrack_; }
     bool saved() const                              { return saved_; }
     /** Internal consistency check (optional).
