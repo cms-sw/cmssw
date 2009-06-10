75912fac83cbe15d000983d19758d510a4c1250f
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-06-10 10:07:37 +0000

    remove obsolete parameters

--- DQM/DTMonitorClient/python/dtDataIntegrityTest_cfi.py
+++ DQM/DTMonitorClient/python/dtDataIntegrityTest_cfi.py
@@ -1,13 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 dataIntegrityTest = cms.EDAnalyzer("DTDataIntegrityTest",
-    outputFile = cms.untracked.string('DataIntegrityTest.root'),
-    folderRoot = cms.untracked.string(''),
-    nTimeBin = cms.untracked.int32(9),
-    doTimeHisto = cms.untracked.bool(False),
-    writeHisto = cms.untracked.bool(False),
-    debug = cms.untracked.bool(False),
-    diagnosticPrescale = cms.untracked.int32(1)
+                                   diagnosticPrescale = cms.untracked.int32(1)
 )
 
 
