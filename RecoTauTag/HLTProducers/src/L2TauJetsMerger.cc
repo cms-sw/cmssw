55375c14b114dd667ad0fa5e36c09e13b2a2c310
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2009-06-11 11:26:15 +0000

    Removed the restriction to take only the first jet in the incoming regional jet collections

--- RecoTauTag/HLTProducers/src/L2TauJetsMerger.cc
+++ RecoTauTag/HLTProducers/src/L2TauJetsMerger.cc
@@ -38,55 +38,35 @@ void L2TauJetsMerger::produce(edm::Event& iEvent, const edm::EventSetup& iES)
  //Getting the Collections of L2ReconstructedJets from L1Seeds
  //and removing the collinear jets
  myL2L1JetsMap.clear();
+ CaloJetCollection myTmpJets;
+ myTmpJets.clear();
+
  int iL1Jet = 0;
  for( vtag::const_iterator s = jetSrc.begin(); s != jetSrc.end(); ++ s ) {
    edm::Handle<CaloJetCollection> tauJets;
    iEvent.getByLabel( * s, tauJets );
-   CaloJetCollection::const_iterator iTau = tauJets->begin();
-   if(iTau != tauJets->end()){
+   for(CaloJetCollection::const_iterator iTau = tauJets->begin();iTau !=tauJets->end();iTau++)
+     { 
      //Create a Map to associate to every Jet its L1SeedId, i.e. 0,1,2 or 3
      if(iTau->et() > mEt_Min)
-       myL2L1JetsMap.insert(pair<int, const CaloJet>(iL1Jet, *(iTau)));
-   }
+       //       myL2L1JetsMap.insert(pair<int, const CaloJet>(iL1Jet, *(iTau)));
+       myTmpJets.push_back(*(iTau));
+     }
    iL1Jet++;
  }
 
  auto_ptr<CaloJetCollection> tauL2jets(new CaloJetCollection); 
  //Removing the collinear jets
- for(int iJet =0;iJet<iL1Jet;iJet++)
-   {
-     map<int, const reco::CaloJet>::const_iterator myL2itr = myL2L1JetsMap.find(iJet);
-     if(myL2itr!=myL2L1JetsMap.end()){
-       
-       const CaloJet my1stJet = myL2itr->second;
-       for(int i2Jet = iJet+1;i2Jet<iL1Jet;i2Jet++)
-	 {
-	   map<int, const reco::CaloJet>::const_iterator my2L2itr = myL2L1JetsMap.find(i2Jet);
-	   if(my2L2itr!=myL2L1JetsMap.end()){
-	     const CaloJet my2ndJet = my2L2itr->second;
-	     
-	     double deltaR = ROOT::Math::VectorUtil::DeltaR(my1stJet.p4().Vect(), my2ndJet.p4().Vect());
-	     if(deltaR < 0.1) 
-	       {
-		 //		 cout <<"Collinear jets "<<deltaR<<endl;
-		 myL2L1JetsMap.erase(my2L2itr->first);
-	       }
-	   }
+
+ for(unsigned int iTau1 = 0; iTau1 <  myTmpJets.size();iTau1++)
+     { 
+       for(unsigned int iTau2 = iTau1; iTau2 <  myTmpJets.size();iTau2++)
+	 { 
+	   double deltaR = ROOT::Math::VectorUtil::DeltaR(myTmpJets[iTau1].p4().Vect(), myTmpJets[iTau2].p4().Vect());
+	   if(deltaR < 0.1) tauL2jets->push_back(myTmpJets[iTau1]);
 	 }
      }
-   }
-  for(int iJet =0;iJet<iL1Jet;iJet++)
-   {
-     map<int, const reco::CaloJet>::const_iterator myL2itr = myL2L1JetsMap.find(iJet);
-     if(myL2itr!=myL2L1JetsMap.end()){
-       
-       const CaloJet my1stJet = myL2itr->second;
-       tauL2jets->push_back(my1stJet);
-     }
-   }
-
-  
-  //  cout <<"Size of L2 jets "<<tauL2jets->size()<<endl;
+//    cout <<"Size of L2 jets "<<tauL2jets->size()<<endl;
 
   iEvent.put(tauL2jets);
 
