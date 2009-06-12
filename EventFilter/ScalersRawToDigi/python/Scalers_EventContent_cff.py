aa2bf6b9c1c42ac36b79298996cf1805edda74db
William Badgett Jr <sha1-0342ea9cbf4e6781ff6fc4596018aec98179df76@cern.ch>
2009-06-12 21:44:05 +0000

    Remove RAW and FEVT keep statements

--- EventFilter/ScalersRawToDigi/python/Scalers_EventContent_cff.py
+++ EventFilter/ScalersRawToDigi/python/Scalers_EventContent_cff.py
@@ -5,26 +5,6 @@ import FWCore.ParameterSet.Config as cms
 # Date:   03.23.2009
 # Notes:  Add L1AcceptBunchCrossing and Collection (W.Badgett,2009.05.20)
 # 
-# RAW content 
-EvtScalersRAW = cms.PSet(
-   outputCommands = cms.untracked.vstring(
-       'keep L1AcceptBunchCrossing_*_*_*',
-       'keep L1TriggerScalers_*_*_*',
-       'keep LumiScalers_*_*_*',
-       'keep L1AcceptBunchCrossingCollection_*_*_*',
-       'keep L1TriggerScalersCollection_*_*_*',
-       'keep LumiScalersCollection_*_*_*')
-)
-# Full Event content 
-EvtScalersFEVT = cms.PSet(
-   outputCommands = cms.untracked.vstring(
-       'keep L1AcceptBunchCrossing_*_*_*',
-       'keep L1TriggerScalers_*_*_*',
-       'keep LumiScalers_*_*_*',
-       'keep L1AcceptBunchCrossingCollection_*_*_*',
-       'keep L1TriggerScalersCollection_*_*_*',
-       'keep LumiScalersCollection_*_*_*')
-)
 # RECO content
 EvtScalersRECO = cms.PSet(
    outputCommands = cms.untracked.vstring(
