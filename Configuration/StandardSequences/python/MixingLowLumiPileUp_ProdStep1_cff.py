41746c069adb3c5c0bcce2106b776a63a2a8b0c9
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-06-12 16:06:30 +0000

    add the new master sequences for the new MM flow

---
+++ Configuration/StandardSequences/python/MixingLowLumiPileUp_ProdStep1_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from SimGeneral.MixingModule.mixLowLumPU_mixProdStep1_cfi import *
+
---
+++ Configuration/StandardSequences/python/MixingProdStep2_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from SimGeneral.MixingModule.mixProdStep2_cfi import *
+
