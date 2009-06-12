833a136fe14ebc33d4b4b3d009c684c8d1eeccb8
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2009-06-12 11:38:01 +0000

    Addeded validatio_prod sequence

--- Configuration/StandardSequences/python/Validation_cff.py
+++ Configuration/StandardSequences/python/Validation_cff.py
@@ -15,5 +15,33 @@ from Validation.Configuration.globalValidation_cff import *
 
 from HLTriggerOffline.Common.HLTValidation_cff import *
 
-validation = cms.Sequence(cms.SequencePlaceholder("mix")+globaldigisanalyze*globalhitsanalyze*globalrechitsanalyze*globalValidation*hltvalidation)
 
+# for ValidationProd
+from SimGeneral.TrackingAnalysis.trackingParticles_cfi import *
+from Validation.RecoMET.METRelValForDQM_cff import *
+from Validation.TrackingMCTruth.trackingTruthValidation_cfi import *
+from Validation.RecoTrack.TrackValidation_cff import *
+from Validation.RecoMuon.muonValidation_cff import *
+from Validation.MuonIsolation.MuIsoVal_cff import *
+from Validation.RecoMuon.muonValidationHLT_cff import *
+from Validation.RecoB.bTagAnalysis_cfi import *
+bTagValidation.jetMCSrc = 'IC5byValAlgo'
+bTagValidation.etaRanges = cms.vdouble(0.0, 1.1, 2.4)
+from RecoBTag.SoftLepton.softElectronBJetTags_cfi import  *
+
+# to be changed
+from PhysicsTools.JetMCAlgos.CaloJetsMCFlavour_cfi import * 
+
+
+validation = cms.Sequence(cms.SequencePlaceholder("mix")+globaldigisanalyze*globalhitsanalyze*globalrechitsanalyze*globalValidation*hltvalidation)
+validation_prod = cms.Sequence(trackingTruthValid
+                          +tracksValidation
+                          +METRelValSequence
+                          +recoMuonValidation
+                          +muIsoVal_seq
+                          +myPartons
+                          +iterativeCone5Flavour
+                          +softElectronBJetTags 
+                          +bTagValidation
+                          +hltvalidation_prod
+                          )
