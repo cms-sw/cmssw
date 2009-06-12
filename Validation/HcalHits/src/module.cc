f9f1744c45c905f2c44010bd28ede26a0e03e78f
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2009-06-12 19:45:30 +0000

    add zdchitstudy

--- Validation/HcalHits/src/module.cc
+++ Validation/HcalHits/src/module.cc
@@ -7,5 +7,6 @@ DEFINE_SEAL_MODULE ();
 DEFINE_SIMWATCHER (SimG4HcalValidation);
 
 #include "Validation/HcalHits/interface/HcalSimHitStudy.h"
+#include "Validation/HcalHits/interface/ZdcSimHitStudy.h"
 
-DEFINE_ANOTHER_FWK_MODULE (HcalSimHitStudy);
+DEFINE_ANOTHER_FWK_MODULE (ZdcSimHitStudy);
