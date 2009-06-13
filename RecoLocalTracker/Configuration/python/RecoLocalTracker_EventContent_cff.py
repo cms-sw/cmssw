e737ba4dbbc4f6d58c58fcd0efe37079469089d9
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-06-13 01:13:19 +0000

    Add bad module list to RECO and FEVT event content

--- RecoLocalTracker/Configuration/python/RecoLocalTracker_EventContent_cff.py
+++ RecoLocalTracker/Configuration/python/RecoLocalTracker_EventContent_cff.py
@@ -1,23 +1,20 @@
-# The following comments couldn't be translated into the new config version:
-
-#Clusters
-
-#Clusters
-
 import FWCore.ParameterSet.Config as cms
 
 #Full Event content 
 RecoLocalTrackerFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_siPixelClusters_*_*', 
-        'keep *_siStripClusters_*_*')
+    outputCommands = cms.untracked.vstring(
+    'keep DetIdedmEDCollection_siStripDigis_*_*',
+    'keep *_siPixelClusters_*_*', 
+    'keep *_siStripClusters_*_*')
 )
 #RECO content
 RecoLocalTrackerRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_siPixelClusters_*_*', 
-        'keep *_siStripClusters_*_*')
+    outputCommands = cms.untracked.vstring(
+    'keep DetIdedmEDCollection_siStripDigis_*_*',
+    'keep *_siPixelClusters_*_*', 
+    'keep *_siStripClusters_*_*')
 )
 #AOD content
 RecoLocalTrackerAOD = cms.PSet(
     outputCommands = cms.untracked.vstring()
 )
-
