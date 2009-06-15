9aea5151e9d30adb784511fa568ed9c7363db7b4
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-06-15 19:20:01 +0000

    For 2007 H4 EE test beam

--- Geometry/EcalTestBeam/python/TBH4_2007_GeometryIdeal_cfi.py
+++ Geometry/EcalTestBeam/python/TBH4_2007_GeometryIdeal_cfi.py
@@ -1,11 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# Geometry master configuration
-#
-# Ideal geometry, needed for simulation
 from Geometry.EcalTestBeam.TBH4_2007_EE_XML_cfi import *
 from Geometry.CaloEventSetup.CaloGeometry_cff import *
+
+CaloGeometryBuilder.SelectedCalos = ['EcalEndcap']
+
 from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
 from Geometry.EcalMapping.EcalMapping_cfi import *
 from Geometry.EcalMapping.EcalMappingRecord_cfi import *
