7a117f1b8c675cade462069721f7d180bf5817e4
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-06-15 19:47:00 +0000

    For 2007 EE test beam

--- SimCalorimetry/EcalTestBeam/src/SealModule.cc
+++ SimCalorimetry/EcalTestBeam/src/SealModule.cc
@@ -1,15 +1,17 @@
 /*  
- *  $Id: SealModule.cc,v 1.1 2006/06/05 14:00:52 fabiocos Exp $
+ *  $Id: SealModule.cc,v 1.2 2007/04/08 03:18:55 wmtan Exp $
  */
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "SimCalorimetry/EcalTestBeam/interface/EcalTBDigiProducer.h"
+#include "SimCalorimetry/EcalTestBeam/interface/EETBDigiProducer.h"
 
 DEFINE_SEAL_MODULE () ;
 
 using edm::EDProducer;
 DEFINE_ANOTHER_FWK_MODULE (EcalTBDigiProducer) ;
+DEFINE_ANOTHER_FWK_MODULE (EETBDigiProducer) ;
 
 
