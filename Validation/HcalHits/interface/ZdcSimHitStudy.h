7f273b315ae96b58e33993b5ed6684b1d9331bf6
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2009-06-16 21:40:13 +0000

    added tot energy histos

--- Validation/HcalHits/interface/ZdcSimHitStudy.h
+++ Validation/HcalHits/interface/ZdcSimHitStudy.h
@@ -36,22 +36,37 @@ protected:
   void beginJob ();
   void endJob   ();
   void analyze  (const edm::Event& e, const edm::EventSetup& c);
-
   void analyzeHits  (std::vector<PCaloHit> &);
+  int FillHitValHist (int side,int section,int channel,double energy,double time);
 
 private:
+  double enetotEmN, enetotHadN,enetotN;  
+  double enetotEmP,enetotHadP,enetotP;
+  double enetot;
+
 
   std::string    g4Label, zdcHits, outFile_;
   bool           verbose_, checkHit_;
   DQMStore       *dbe_;
 
   MonitorElement *meAllZdcNHit_, *meBadZdcDetHit_, *meBadZdcSecHit_, *meBadZdcIdHit_;
-  MonitorElement *meZdcNHit_,*meZdcDetectHit_, *meZdcSideHit_,*meZdcETime_;
+  MonitorElement *meZdcNHit_,*meZdcDetectHit_,*meZdcSideHit_,*meZdcETime_;
   MonitorElement *meZdcNHitEM_,*meZdcNHitHad_,*meZdcNHitLum_,*meZdc10Ene_;
   MonitorElement *meZdcSectionHit_,*meZdcChannelHit_,*meZdcEnergyHit_,*meZdcTimeWHit_;
   MonitorElement *meZdcHadEnergyHit_, *meZdcEMEnergyHit_, *meZdcTimeHit_, *meZdcHadL10EneP_;
   MonitorElement *meZdc10EneP_, *meZdcEHadCh_, *meZdcEEMCh_,*meZdcEML10EneP_;
-
+  MonitorElement *meZdcEneEmN1_,*meZdcEneEmN2_,*meZdcEneEmN3_,*meZdcEneEmN4_,*meZdcEneEmN5_; 
+  MonitorElement *meZdcEneHadN1_,*meZdcEneHadN2_,*meZdcEneHadN3_,*meZdcEneHadN4_;
+  MonitorElement *meZdcEneTEmN1_,*meZdcEneTEmN2_,*meZdcEneTEmN3_,*meZdcEneTEmN4_,*meZdcEneTEmN5_; 
+  MonitorElement *meZdcEneTHadN1_,*meZdcEneTHadN2_,*meZdcEneTHadN3_,*meZdcEneTHadN4_;
+  MonitorElement *meZdcEneHadNTot_,*meZdcEneEmNTot_,*meZdcEneNTot_;
+  MonitorElement *meZdcCorEEmNEHadN_;
+  MonitorElement *meZdcEneEmP1_,*meZdcEneEmP2_,*meZdcEneEmP3_,*meZdcEneEmP4_,*meZdcEneEmP5_; 
+  MonitorElement *meZdcEneHadP1_,*meZdcEneHadP2_,*meZdcEneHadP3_,*meZdcEneHadP4_;
+  MonitorElement *meZdcEneTEmP1_,*meZdcEneTEmP2_,*meZdcEneTEmP3_,*meZdcEneTEmP4_,*meZdcEneTEmP5_; 
+  MonitorElement *meZdcEneTHadP1_,*meZdcEneTHadP2_,*meZdcEneTHadP3_,*meZdcEneTHadP4_;
+  MonitorElement *meZdcEneHadPTot_,*meZdcEneEmPTot_, *meZdcEnePTot_;
+  MonitorElement *meZdcCorEEmPEHadP_,*meZdcCorEtotNEtotP_,*meZdcEneTot_;
 };
 
 #endif
