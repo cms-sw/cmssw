94ef025b6d8adcf0ddf353b32e00a5c0dd6d01b9
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2009-06-16 21:55:02 +0000

    Fixing the update.

--- PhysicsTools/PatExamples/plugins/PatTriggerAnalyzer.h
+++ PhysicsTools/PatExamples/plugins/PatTriggerAnalyzer.h
@@ -7,20 +7,12 @@
 #include <string>
 #include <map>
 
-#include "FWCore/Framework/interface/Run.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
-#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
-
 #include "DataFormats/PatCandidates/interface/TriggerEvent.h"
-#include "DataFormats/PatCandidates/interface/Photon.h"
-#include "DataFormats/PatCandidates/interface/Electron.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
-#include "DataFormats/PatCandidates/interface/Tau.h"
-#include "DataFormats/PatCandidates/interface/Jet.h"
-#include "DataFormats/PatCandidates/interface/MET.h"
 
 #include "TH1D.h"
 #include "TH2D.h"
@@ -42,7 +34,7 @@ namespace pat {
       edm::InputTag trigger_;
       edm::InputTag triggerEvent_;
       edm::InputTag muons_;
-      srd::string   muonMatch_;
+      std::string   muonMatch_;
 
       std::map< std::string, TH1D* > histos1D_;
       std::map< std::string, TH2D* > histos2D_;
