6bb61f221d9f8f7732f50f68cf244619639c5abf
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2009-06-16 21:39:34 +0000

    added total energy histos

--- Validation/HcalHits/src/ZdcSimHitStudy.cc
+++ Validation/HcalHits/src/ZdcSimHitStudy.cc
@@ -35,7 +35,6 @@ ZdcSimHitStudy::~ZdcSimHitStudy() {
 }
 
 void ZdcSimHitStudy::beginJob() {
-
   if (dbe_) {
     dbe_->setCurrentFolder("ZdcHitsV/ZdcSimHitsTask");
     //Histograms for Hits
@@ -62,6 +61,52 @@ void ZdcSimHitStudy::beginJob() {
       meZdcEHadCh_ = dbe_->book2D("Hit20","Zdc Had Section Energy vs Channel", 4000, 0., 8000., 6, 0., 6. );
       meZdcEEMCh_ = dbe_->book2D("Hit21","Zdc EM Section Energy vs Channel", 4000, 0., 8000., 6, 0., 6. );
       meZdcETime_ = dbe_->book2D("Hit22","Hits Zdc Energy vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneEmN1_  = dbe_->book1D("HitVal01","Energy EM module N1",4000,0.,8000.);
+      meZdcEneEmN2_  = dbe_->book1D("HitVal02","Energy EM module N2",4000,0.,8000.);
+      meZdcEneEmN3_  = dbe_->book1D("HitVal03","Energy EM module N3",4000,0.,8000.);
+      meZdcEneEmN4_  = dbe_->book1D("HitVal04","Energy EM module N4",4000,0.,8000.);
+      meZdcEneEmN5_  = dbe_->book1D("HitVal05","Energy EM module N5",4000,0.,8000.);
+      meZdcEneHadN1_ = dbe_->book1D("HitVal06","Energy HAD module N1",4000,0.,8000.);
+      meZdcEneHadN2_ = dbe_->book1D("HitVal07","Energy HAD module N2",4000,0.,8000.);
+      meZdcEneHadN3_ = dbe_->book1D("HitVal08","Energy HAD module N3",4000,0.,8000.);
+      meZdcEneHadN4_ = dbe_->book1D("HitVal09","Energy HAD module N4",4000,0.,8000.);
+      meZdcEneTEmN1_ = dbe_->book2D("HitVal11","Energy EM mod N1 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmN2_ = dbe_->book2D("HitVal12","Energy EM mod N2 vs Time", 4000, 0., 8000., 300, 0., 600. ); 
+      meZdcEneTEmN3_ = dbe_->book2D("HitVal13","Energy EM mod N3 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmN4_ = dbe_->book2D("HitVal14","Energy EM mod N4 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmN5_ = dbe_->book2D("HitVal15","Energy EM mod N5 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadN1_ = dbe_->book2D("HitVal16","Energy HAD mod N1 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadN2_ = dbe_->book2D("HitVal17","Energy HAD mod N2 vs Time", 4000, 0., 8000., 300, 0., 600. ); 
+      meZdcEneTHadN3_ = dbe_->book2D("HitVal18","Energy HAD mod N3 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadN4_ = dbe_->book2D("HitVal19","Energy HAD mod N4 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneHadNTot_ = dbe_->book1D("HitVal20","Total HAD Energy N",4000,0.,8000.);
+      meZdcEneEmNTot_  = dbe_->book1D("HitVal21","Total EM Energy N",4000,0.,8000.);
+      meZdcEneNTot_    = dbe_->book1D("HitVal22","Total Energy N",4000,0.,8000.);
+      meZdcEneEmP1_  = dbe_->book1D("HitVal23","Energy EM module P1",4000,0.,8000.);
+      meZdcEneEmP2_  = dbe_->book1D("HitVal24","Energy EM module P2",4000,0.,8000.);
+      meZdcEneEmP3_  = dbe_->book1D("HitVal25","Energy EM module P3",4000,0.,8000.);
+      meZdcEneEmP4_  = dbe_->book1D("HitVal26","Energy EM module P4",4000,0.,8000.);
+      meZdcEneEmP5_  = dbe_->book1D("HitVal27","Energy EM module P5",4000,0.,8000.);
+      meZdcEneHadP1_ = dbe_->book1D("HitVal29","Energy HAD module P1",4000,0.,8000.);
+      meZdcEneHadP2_ = dbe_->book1D("HitVal29","Energy HAD module P2",4000,0.,8000.);
+      meZdcEneHadP3_ = dbe_->book1D("HitVal30","Energy HAD module P3",4000,0.,8000.);
+      meZdcEneHadP4_ = dbe_->book1D("HitVal31","Energy HAD module P4",4000,0.,8000.);
+      meZdcEneTEmP1_ = dbe_->book2D("HitVal32","Energy EM mod P1 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmP2_ = dbe_->book2D("HitVal33","Energy EM mod P2 vs Time", 4000, 0., 8000., 300, 0., 600. ); 
+      meZdcEneTEmP3_ = dbe_->book2D("HitVal34","Energy EM mod P3 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmP4_ = dbe_->book2D("HitVal35","Energy EM mod P4 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTEmP5_ = dbe_->book2D("HitVal36","Energy EM mod P5 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadP1_ = dbe_->book2D("HitVal37","Energy HAD mod P1 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadP2_ = dbe_->book2D("HitVal38","Energy HAD mod P2 vs Time", 4000, 0., 8000., 300, 0., 600. ); 
+      meZdcEneTHadP3_ = dbe_->book2D("HitVal39","Energy HAD mod P3 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneTHadP4_ = dbe_->book2D("HitVal40","Energy HAD mod P4 vs Time", 4000, 0., 8000., 300, 0., 600. );
+      meZdcEneHadPTot_ = dbe_->book1D("HitVal41","Total HAD Energy P",4000,0.,8000.);
+      meZdcEneEmPTot_  = dbe_->book1D("HitVal42","Total EM Energy P",4000,0.,8000.);
+      meZdcEnePTot_    = dbe_->book1D("HitVal43","Total Energy P",4000,0.,8000.);
+      meZdcCorEEmNEHadN_= dbe_->book2D("HitVal47","Energy EMN vs HADN", 4000, 0., 8000.,4000, 0., 8000.);
+      meZdcCorEEmPEHadP_= dbe_->book2D("HitVal44","Energy EMP vs HADP", 4000, 0., 8000.,4000, 0., 8000.);
+      meZdcCorEtotNEtotP_ = dbe_->book2D("HitVal45","Energy N vs P", 4000, 0., 8000.,4000, 0., 8000.);
+      meZdcEneTot_ = dbe_->book1D("HitVal46","Total Energy ZDCs",4000,0.,8000.);
     }
   }
 }
@@ -74,7 +119,7 @@ void ZdcSimHitStudy::analyze(const edm::Event& e, const edm::EventSetup& ) {
     //std::cout
     << "Run = " << e.id().run() << " Event = " 
     << e.id().event();
-    //<<std::endl;
+  //std::cout<<std::endl;
   
   std::vector<PCaloHit>               caloHits;
   edm::Handle<edm::PCaloHitContainer> hitsZdc;
@@ -98,8 +143,7 @@ void ZdcSimHitStudy::analyze(const edm::Event& e, const edm::EventSetup& ) {
   }
 }
 
-void ZdcSimHitStudy::analyzeHits (std::vector<PCaloHit>& hits){
-
+void ZdcSimHitStudy::analyzeHits(std::vector<PCaloHit>& hits){
   int nHit = hits.size();
   int nZdcEM = 0, nZdcHad = 0, nZdcLum = 0; 
   int nBad1=0, nBad2=0, nBad=0;
@@ -107,6 +151,14 @@ void ZdcSimHitStudy::analyzeHits (std::vector<PCaloHit>& hits){
   std::vector<double> encontZdcHad(140, 0.);
   double entotZdcEM = 0;
   double entotZdcHad = 0;
+ 
+  enetotEmN = 0;
+  enetotHadN = 0.;
+  enetotN = 0;  
+  enetotEmP = 0;
+  enetotHadP = 0;
+  enetotP = 0;
+  enetot = 0;
   
   for (int i=0; i<nHit; i++) {
     double energy    = hits[i].energy();
@@ -119,6 +171,9 @@ void ZdcSimHitStudy::analyzeHits (std::vector<PCaloHit>& hits){
     int side         = id.zside();
     int section      = id.section();
     int channel      = id.channel();
+
+    FillHitValHist(side,section,channel,energy,time);
+  
     
     LogDebug("ZdcSimHitStudy") 
       //std::cout
@@ -154,28 +209,36 @@ void ZdcSimHitStudy::analyzeHits (std::vector<PCaloHit>& hits){
 	meZdcHadEnergyHit_->Fill(energy);
 	meZdcEHadCh_->Fill(energy,channel);
 	if( log10i >=0 && log10i < 140 )encontZdcHad[log10i] += energy;
-	entotZdcHad += energy; 
+	entotZdcHad += energy;
       }	
       meZdcTimeHit_->Fill(time);
       meZdcTimeWHit_->Fill(double(time),energy);
       meZdc10Ene_->Fill(log10en);
       meZdcETime_->Fill(energy, double(time));
-      
       }
     }
   }
-  
+
   if( entotZdcEM  != 0 ) for( int i=0; i<140; i++ ) meZdcEML10EneP_->Fill( -10.+(float(i)+0.5)/10., encontZdcEM[i]/entotZdcEM);
   if( entotZdcHad != 0 ) for( int i=0; i<140; i++ ) meZdcHadL10EneP_->Fill( -10.+(float(i)+0.5)/10.,encontZdcHad[i]/entotZdcHad);
   
-  if (dbe_) {
+  if (dbe_ && nHit>0) {
     meAllZdcNHit_->Fill(double(nHit));
     meBadZdcDetHit_->Fill(double(nBad1));
     meBadZdcSecHit_->Fill(double(nBad2));
     meBadZdcIdHit_->Fill(double(nBad));
     meZdcNHitEM_->Fill(double(nZdcEM));
     meZdcNHitHad_->Fill(double(nZdcHad));
-    meZdcNHitLum_->Fill(double(nZdcLum));
+    meZdcNHitLum_->Fill(double(nZdcLum)); 
+    meZdcEnePTot_->Fill(enetotP);
+    meZdcEneHadNTot_->Fill(enetotHadN);
+    meZdcEneHadPTot_->Fill(enetotHadP);
+    meZdcEneEmNTot_->Fill(enetotEmN);
+    meZdcEneEmPTot_->Fill(enetotEmP);
+    meZdcCorEEmNEHadN_->Fill(enetotEmN,enetotHadN);
+    meZdcCorEEmPEHadP_->Fill(enetotEmP,enetotHadP);
+    meZdcCorEtotNEtotP_->Fill(enetotN,enetotP);
+    meZdcEneTot_->Fill(enetot);
   }
   LogDebug("HcalSimHitStudy") 
   //std::cout
@@ -184,3 +247,122 @@ void ZdcSimHitStudy::analyzeHits (std::vector<PCaloHit>& hits){
     << " Bad " << nBad << " All " << nHit;
     //<<std::endl;
 }
+
+int ZdcSimHitStudy::FillHitValHist(int side,int section,int channel,double energy,double time){  
+  enetot += enetot;
+  if(side == -1){
+    enetotN += energy;
+    if(section == HcalZDCDetId::EM){
+      enetotEmN += energy;
+      switch(channel){
+      case 1 :
+	meZdcEneEmN1_->Fill(energy);
+	meZdcEneTEmN1_->Fill(energy,time);
+	break;
+      case 2 :
+       meZdcEneEmN2_->Fill(energy);
+       meZdcEneTEmN2_->Fill(energy,time);
+       	break;
+      case 3 :
+	meZdcEneEmN3_->Fill(energy);
+       meZdcEneTEmN3_->Fill(energy,time);
+       	break;
+      case 4 :
+	meZdcEneEmN4_->Fill(energy);
+	meZdcEneTEmN4_->Fill(energy,time);
+	break; 
+     case 5 :
+	meZdcEneEmN4_->Fill(energy);
+	meZdcEneTEmN4_->Fill(energy,time);
+	break;
+      }
+    }
+    if(section == HcalZDCDetId::HAD){
+      enetotHadN += energy;
+      switch(channel){
+      case 1 :
+	meZdcEneHadN1_->Fill(energy);
+	meZdcEneTHadN1_->Fill(energy,time);
+	break;
+      case 2 :
+	meZdcEneHadN2_->Fill(energy);
+	meZdcEneTHadN2_->Fill(energy,time);
+	break;
+      case 3 :
+	meZdcEneHadN3_->Fill(energy);
+	meZdcEneTHadN3_->Fill(energy,time);
+	break;
+      case 4 :
+	meZdcEneHadN4_->Fill(energy);
+	meZdcEneTHadN4_->Fill(energy,time);
+	break;
+      }
+    }
+  }
+  if(side == 1){
+    enetotP += energy;
+    if(section == HcalZDCDetId::EM){
+      enetotEmP += energy;
+      switch(channel){
+      case 1 :
+	meZdcEneEmP1_->Fill(energy);
+	meZdcEneTEmP1_->Fill(energy,time);
+	break;
+      case 2 :
+	meZdcEneEmP2_->Fill(energy);
+	meZdcEneTEmP2_->Fill(energy,time);
+	break;
+      case 3 :
+	meZdcEneEmP3_->Fill(energy);
+	meZdcEneTEmP3_->Fill(energy,time);
+	break;
+      case 4 :
+	meZdcEneEmP4_->Fill(energy);
+	meZdcEneTEmP4_->Fill(energy,time);
+	break; 
+      case 5 :
+	meZdcEneEmP4_->Fill(energy);
+	meZdcEneTEmP4_->Fill(energy,time);
+	break;
+      }
+    }
+    if(section == HcalZDCDetId::HAD){
+      enetotHadP += energy;
+      switch(channel){
+      case 1 :
+	meZdcEneHadP1_->Fill(energy);
+	meZdcEneTHadP1_->Fill(energy,time);
+	break;
+      case 2 :
+	meZdcEneHadP2_->Fill(energy);
+	meZdcEneTHadP2_->Fill(energy,time);
+	break;
+      case 3 :
+	meZdcEneHadP3_->Fill(energy);
+	meZdcEneTHadP3_->Fill(energy,time);
+	break;
+      case 4 :
+	meZdcEneHadP4_->Fill(energy);
+	meZdcEneTHadP4_->Fill(energy,time);
+	break;
+      }
+    }
+  }       
+  return 0;
+}
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
