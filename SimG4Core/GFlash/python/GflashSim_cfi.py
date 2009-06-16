7b823798bbfd7f07f8d22ffd8eac2e7d9983606a
Soon Yung Jun <sha1-bc64213de5daffe38cb62f6b1e3fe18ade60c3a9@cern.ch>
2009-06-16 15:55:20 +0000

    rename Gflash_cfi.py to GflashSim_cfi.py after moving the digi part of configuration to GflashDigi_cfi.py

---
+++ SimG4Core/GFlash/python/GflashSim_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+from SimG4Core.Application.g4SimHits_cfi import *
+
+g4SimHits.Physics.type = 'SimG4Core/Physics/GFlash'
+g4SimHits.Physics.GFlash = cms.PSet(
+    GflashHadronPhysics = cms.string('QGSP_BERT'),
+    GflashEMShowerModel = cms.bool(True),
+    GflashHadronShowerModel = cms.bool(True),
+    GflashHcalOuter = cms.bool(True),
+    GflashHistogram = cms.bool(True),
+    GflashHistogramName = cms.string('gflash_histogram.root'),
+    Verbosity = cms.untracked.int32(0),
+    bField = cms.double(3.8),
+    watcherOn = cms.bool(False),
+    tuning_pList = cms.vdouble()
+)
