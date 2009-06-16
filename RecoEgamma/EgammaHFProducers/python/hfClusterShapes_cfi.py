7dbeffb30796d3342f7033fae47f9c8b5a72ef07
Matteo Sani <sha1-b1a59c6d8d0f0b19695380739157bd7a38c2f52f@cern.ch>
2009-06-16 14:42:15 +0000

    Make tracked an InpuTag

--- RecoEgamma/EgammaHFProducers/python/hfClusterShapes_cfi.py
+++ RecoEgamma/EgammaHFProducers/python/hfClusterShapes_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # HFEMClusterShape producer
 hfEMClusters = cms.EDProducer("HFEMClusterProducer",
-    hits = cms.untracked.InputTag("hfreco"),
+    hits = cms.InputTag("hfreco"),
     minTowerEnergy = cms.double(3.0),
     seedThresholdET = cms.double(5.0)
 )
