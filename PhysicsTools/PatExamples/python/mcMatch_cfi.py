78de2aa0ca4bb693bce89e3abcc3624bd047ee4b
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2009-06-17 10:16:52 +0000

    Solution. Task 1

--- PhysicsTools/PatExamples/python/mcMatch_cfi.py
+++ PhysicsTools/PatExamples/python/mcMatch_cfi.py
@@ -1,24 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
-myMuonMatch = cms.EDFilter(  ,                   # MC matcher module instance
-    src                   = cms.InputTag(  ),    # RECO objects to match
-    matched               = cms.InputTag(  ),    # mc-truth particle collection
-    mcPdgId               = cms.vint32(  ),      # one or more PDG ID; absolute values (see below)
-    checkCharge           = cms.bool(  ),        # True = require RECO and MC objects to have the same charge
-    mcStatus              = cms.vint32(  ),      # PYTHIA status code (1 = stable, 2 = shower, 3 = hard scattering)
-    maxDeltaR             = cms.double(  ),      # Minimum deltaR for the match
-    maxDPtRel             = cms.double(  ),      # Minimum deltaPt/Pt for the match
-    resolveAmbiguities    = cms.bool(  ),        # Forbid two RECO objects to match to the same GEN object
-    resolveByMatchQuality = cms.bool(  )         # False = just match input in order; True = pick lowest deltaR pair first
+myMuonMatch = cms.EDFilter("MCMatcher", # cut on deltaR, deltaPt/Pt; pick best by deltaR
+    src     = cms.InputTag("muons"), # RECO objects to match
+    matched = cms.InputTag("genParticles"),   # mc-truth particle collection
+    mcPdgId     = cms.vint32(13), # one or more PDG ID (13 = muon); absolute values (see below)
+    checkCharge = cms.bool(True), # True = require RECO and MC objects to have the same charge
+    mcStatus = cms.vint32(1),     # PYTHIA status code (1 = stable, 2 = shower, 3 = hard scattering)
+    maxDeltaR = cms.double(0.5),  # Minimum deltaR for the match
+    maxDPtRel = cms.double(0.5),  # Minimum deltaPt/Pt for the match
+    resolveAmbiguities = cms.bool(True),     # Forbid two RECO objects to match to the same GEN object
+    resolveByMatchQuality = cms.bool(False) # False = just match input in order; True = pick lowest deltaR pair first
 )
-myJetGenJetMatch = cms.EDFilter(  ,                        # MC matcher module instance
-    src                   = cms.InputTag(  ),     # RECO jets (any View<Jet> is ok)
-    matched               = cms.InputTag(  ),     # GEN jets  (must be GenJetCollection)
-    mcPdgId               = cms.vint32(  ),       # n/a
-    mcStatus              = cms.vint32(  ),       # n/a
-    checkCharge           = cms.bool(  ),         # n/a
-    maxDeltaR             = cms.double(  ),       # Minimum deltaR for the match
-    maxDPtRel             = cms.double(  ),       # Minimum deltaPt/Pt for the match
-    resolveAmbiguities    = cms.bool(  ),         # Forbid two RECO objects to match to the same GEN object
-    resolveByMatchQuality = cms.bool(  )          # False = just match input in order; True = pick lowest deltaR pair first
+myJetGenJetMatch = cms.EDFilter("GenJetMatcher", # cut on deltaR, deltaPt/Pt; pick best by deltaR
+    src      = cms.InputTag("iterativeCone5CaloJets"),         ## RECO jets (any View<Jet> is ok)
+    matched  = cms.InputTag("iterativeCone5GenJets"), ## GEN jets  (must be GenJetCollection)
+    mcPdgId  = cms.vint32(),       # n/a
+    mcStatus = cms.vint32(),       # n/a
+    checkCharge = cms.bool(False), # n/a
+    maxDeltaR = cms.double(0.4), # Minimum deltaR for the match
+    maxDPtRel = cms.double(3.0), # Minimum deltaPt/Pt for the match
+    resolveAmbiguities = cms.bool(True),     # Forbid two RECO objects to match to the same GEN object
+    resolveByMatchQuality = cms.bool(False) # False = just match input in order; True = pick lowest deltaR pair first
 )
