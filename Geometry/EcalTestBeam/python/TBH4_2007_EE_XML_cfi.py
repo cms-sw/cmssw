4ca016eb50a58e14160b1bde553485bbb224954e
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-06-17 21:53:08 +0000

    Fixes for EE TestBeam

--- Geometry/EcalTestBeam/python/TBH4_2007_EE_XML_cfi.py
+++ Geometry/EcalTestBeam/python/TBH4_2007_EE_XML_cfi.py
@@ -10,10 +10,10 @@ XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
         'Geometry/EcalTestBeam/data/2007/eregalgo.xml', 
         'Geometry/EcalTestBeam/data/2007/tbrot.xml', 
         'Geometry/EcalTestBeam/data/2007/TBH4.xml', 
-        'Geometry/EcalTestBeam/data/2007/TBH4ecalsens.xml', 
 
         'Geometry/EcalCommonData/data/eecon.xml', 
         'Geometry/EcalCommonData/data/eealgo.xml', 
+        'Geometry/EcalTestBeam/data/2007/TBH4ecalsens.xml', 
         'Geometry/EcalSimData/data/EcalProdCuts.xml', 
         'Geometry/CMSCommonData/data/FieldParameters.xml'),
     rootNodeName = cms.string('TBH4:OCMS')
