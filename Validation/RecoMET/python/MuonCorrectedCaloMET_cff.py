653dd2ceb04a0cb7bc9f9cbe23ae238e0687bd53
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-06-17 11:27:23 +0000

    Fixed typo

--- Validation/RecoMET/python/MuonCorrectedCaloMET_cff.py
+++ Validation/RecoMET/python/MuonCorrectedCaloMET_cff.py
@@ -5,6 +5,6 @@ import FWCore.ParameterSet.Config as cms
 # Date: 11.14.2008
 #
 # Fill validation histograms for MET. Assumes tcMet is in the event.
-from Validation.RecoMET.MuonCorrectedCaloMET import *
+from Validation.RecoMET.MuonCorrectedCaloMET_cfi import *
 analyzeMuonCorrectedCaloMET = cms.Sequence(corMetGlobalMuonsAnalyzer)
 
