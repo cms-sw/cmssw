c747bb30e25ae4d9ffd1ab4e0db77093e6872697
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2009-06-18 07:12:27 +0000

    Renamed heavyChHiggsToTauNuBackroundAnalysisSkim.py and added skim for the QCD background analysis

---
+++ HiggsAnalysis/Skimming/test/heavyChHiggsToTauNuMuonBackgroundAnalysisSkim.py
@@ -0,0 +1,36 @@
+# Skim for heavy H+->taunu background analysis with muon events
+# Filter events with HLT single muon + three jets Et > 20, abs(eta) < 2.5
+# then produces AODSIM selected events
+# Created by S.Lehti
+# Tested on 28 May 2009
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("HChBSkim")
+
+process.maxEvents = cms.untracked.PSet(
+        input = cms.untracked.int32(100)
+)
+
+
+process.load("HiggsAnalysis.Skimming.heavyChHiggsToTauNu_SkimPaths_cff")
+process.load("HiggsAnalysis.Skimming.heavyChHiggsToTauNu_OutputModule_cff")
+
+process.load("FWCore/MessageService/MessageLogger_cfi")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+	"file:heavyChHiggsToTauNuSkim.root"
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/HiggsAnalysis/Skimming/test/heavyChHiggsToTauNuMuonBackgroundAnalysisSkim.py,v $'),
+    annotation = cms.untracked.string('Skim for heavy H+->tau nu background events with muon selected instead of tau')
+)
+process.heavyChHiggsToTauNuHLTFilter.HLTPaths = ['HLT_IsoMu15']
+process.heavyChHiggsToTauNuFilter.minNumberOfJets = cms.int32(3)
+
+process.outpath = cms.EndPath(process.heavyChHiggsToTauNuOutputModuleRECOSIM)
+
