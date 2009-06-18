d94865260ef43127eae4d0ce6c3952c74e874de5
Andrea Lucaroni <sha1-a17ddc562d56307fc4abaf6c9898c35059089bf9@cern.ch>
2009-06-18 12:49:08 +0000

    switch from source to generator (tag)

--- QCDAnalysis/UEAnalysis/python/UEAnalysisRootpleOnlyMC_cfi.py
+++ QCDAnalysis/UEAnalysis/python/UEAnalysisRootpleOnlyMC_cfi.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 UEAnalysisRootpleOnlyMC = cms.EDFilter("AnalysisRootpleProducerOnlyMC",
     ChgGenJetCollectionName = cms.untracked.InputTag("IC5ChgGenJet"),
-    MCEvent = cms.untracked.InputTag("source"),
+    MCEvent = cms.untracked.InputTag("generator"),
     ChgGenPartCollectionName = cms.untracked.InputTag("chargeParticles"),
     GenJetCollectionName = cms.untracked.InputTag("IC5GenJet")
 )
 
 UEAnalysisRootpleOnlyMC500 = cms.EDFilter("AnalysisRootpleProducerOnlyMC",
     ChgGenJetCollectionName = cms.untracked.InputTag("IC5ChgGenJet500"),
-    MCEvent = cms.untracked.InputTag("source"),
+    MCEvent = cms.untracked.InputTag("generator"),
     ChgGenPartCollectionName = cms.untracked.InputTag("chargeParticles"),
     GenJetCollectionName = cms.untracked.InputTag("IC5GenJet500")
 )
