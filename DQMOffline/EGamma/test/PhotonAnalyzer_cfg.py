e0b1d60612551d223b685ac42b7de7f9d6242208
Louis James Antonelli <sha1-442197ce02a0252aaee7a57f46fd47b9778e7d2f@cern.ch>
2009-06-19 14:27:34 +0000

    *** empty log message ***

--- DQMOffline/EGamma/test/PhotonAnalyzer_cfg.py
+++ DQMOffline/EGamma/test/PhotonAnalyzer_cfg.py
@@ -23,6 +23,10 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 
+##             '/store/relval/CMSSW_3_1_0_pre10/RelValSingleGammaPt35/GEN-SIM-RECO/IDEAL_31X_v1/0008/E60958D5-0458-DE11-B0B7-000423D98E30.root',
+##         '/store/relval/CMSSW_3_1_0_pre10/RelValSingleGammaPt35/GEN-SIM-RECO/IDEAL_31X_v1/0008/C0188C68-4157-DE11-AE7E-001D09F250AF.root'
+
+
         '/store/relval/CMSSW_3_1_0_pre9/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_31X_v1/0006/6AA9BBFD-9F4E-DE11-8064-001617C3B79A.root',
         '/store/relval/CMSSW_3_1_0_pre9/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_31X_v1/0006/3A31C266-A14E-DE11-80AB-001D09F28F11.root',
         '/store/relval/CMSSW_3_1_0_pre9/RelValGammaJets_Pt_80_120/GEN-SIM-RECO/STARTUP_31X_v1/0006/2E9E0AED-A34E-DE11-86D2-001D09F251D1.root',
@@ -50,8 +54,6 @@ photonAnalysis.useTriggerFiltering = cms.bool(True)
 from DQMOffline.EGamma.photonOfflineClient_cfi import *
 
 photonOfflineClient.standAlone = cms.bool(True)
-
-
 
 #from DQMServices.Components.DQMStoreStats_cfi import *
 
