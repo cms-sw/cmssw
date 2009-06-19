eadf95e4ebca691e8a9ea4a3062e668ff2eeeb55
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2009-06-19 21:20:45 +0000

    remove stdsequence, geometry import

--- Validation/RecoTrack/python/MultiTrackValidator_cff.py
+++ Validation/RecoTrack/python/MultiTrackValidator_cff.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.StandardSequences.MagneticField_cff import *
 from DQMServices.Core.DQM_cfg import *
 DQMStore.collateHistograms =cms.untracked.bool(True)
 from Validation.RecoTrack.MultiTrackValidator_cfi import *
