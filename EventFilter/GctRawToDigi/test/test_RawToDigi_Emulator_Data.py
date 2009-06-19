4f1bce9e7d9a979c23e5f1b08e542c361db8c754
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-06-19 10:26:12 +0000

    fix test jobs for 31X

--- EventFilter/GctRawToDigi/test/test_RawToDigi_Emulator_Data.py
+++ EventFilter/GctRawToDigi/test/test_RawToDigi_Emulator_Data.py
@@ -10,11 +10,11 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process('testRawToDigiEmulatorData')
 
 #Logger thingy
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger = cms.Service ("MessageLogger", 
-  destinations = cms.untracked.vstring( "detailedInfo.txt" ),
-  threshold = cms.untracked.string ( 'WARNING' )
-)
+#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger = cms.Service ("MessageLogger", 
+#  destinations = cms.untracked.vstring( "detailedInfo.txt" ),
+#  threshold = cms.untracked.string ( 'WARNING' )
+#)
 
 # Number of events
 process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 ( -1 ) )
@@ -38,7 +38,7 @@ process.valGctDigis.postSamples = cms.uint32(0)
 # comparator
 process.load('L1Trigger.HardwareValidation.L1Comparator_cfi')
 process.l1compare.GCTsourceData = cms.InputTag( "l1GctHwDigis" )
-process.l1compare.GCTsourceEmul = cms.InputTag( "simGctDigis" )
+process.l1compare.GCTsourceEmul = cms.InputTag( "valGctDigis" )
 process.l1compare.VerboseFlag = cms.untracked.int32(0)
 process.l1compare.DumpMode = cms.untracked.int32(0) #was -1 (shows failed + worked) or 1 (shows failed only)
 process.l1compare.DumpFile = cms.untracked.string( "l1compare_dump.txt" )
@@ -84,6 +84,17 @@ process.l1demongct)
 process.p = cms.Path(process.defaultPath)
 
 process.source = cms.Source ( "PoolSource",
-   fileNames = cms.untracked.vstring()
+   fileNames = cms.untracked.vstring(
+        '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/FA4886DB-7FAB-DD11-95F4-000423D98BC4.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/FA30500C-89AB-DD11-B100-000423D99AAE.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/F8BF1FBB-7DAB-DD11-AE60-001617E30D06.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/F42665F5-81AB-DD11-8346-000423D6AF24.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/F2E0F9E8-78AB-DD11-8164-001617C3B778.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/EA691726-7FAB-DD11-812B-001617E30D06.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/EA1D3022-7FAB-DD11-A103-000423D6B2D8.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/E8A7682D-86AB-DD11-8346-000423D94990.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/E6F3E436-7AAB-DD11-A381-000423D9517C.root',
+                '/store/data/Commissioning08/Calo/RAW/v1/000/069/522/E6A50648-81AB-DD11-9898-000423D6C8EE.root'
+        )
 )
 
