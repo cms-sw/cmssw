2575b73ff1b5b8daa37c67f41d825918fdcb9ba3
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-06-19 15:20:53 +0000

    configuration parameter in py file

--- HLTriggerOffline/Common/python/FourVectorHLTriggerOffline_cfi.py
+++ HLTriggerOffline/Common/python/FourVectorHLTriggerOffline_cfi.py
@@ -1,22 +1,43 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: FourVectorHLTriggerOffline_cfi.py,v 1.10 2009/06/08 12:48:49 rekovic Exp $
+# $Id: FourVectorHLTriggerOffline_cfi.py,v 1.11 2009/06/10 13:19:10 rekovic Exp $
 hltriggerResults = cms.EDFilter("FourVectorHLTriggerOffline",
     dirname = cms.untracked.string("HLT/FourVector_Val/source"),
     plotAll = cms.untracked.bool(False),
     ptMax = cms.untracked.double(100.0),
     ptMin = cms.untracked.double(0.0),
 
-		muonEtaMax = cms.untracked.double(2.1),
-    muonDRMatch = cms.untracked.double(0.3),
+  electronEtaMax = cms.untracked.double(2.5),
+  electronEtMin = cms.untracked.double(3.0),
+  electronDRMatch  =cms.untracked.double(0.5),
 
-    electronDRMatch = cms.untracked.double(0.5),
-    photonDRMatch = cms.untracked.double(0.5),
-    #tauDRMatch = cms.untracked.double(0.1),
-    #jetDRMatch = cms.untracked.double(0.1),
-    #bjetDRMatch = cms.untracked.double(0.1),
-    #photonDRMatch = cms.untracked.double(0.1),
-    #trackDRMatch = cms.untracked.double(0.1),
+  muonEtaMax = cms.untracked.double(2.1),
+  muonEtMin = cms.untracked.double(3.0),
+  muonDRMatch  =cms.untracked.double(0.3),
+
+  tauEtaMax = cms.untracked.double(5.0),
+  tauEtMin = cms.untracked.double(3.0),
+  tauDRMatch  =cms.untracked.double(0.5),
+
+  jetEtaMax = cms.untracked.double(5.0),
+  jetEtMin = cms.untracked.double(10.0),
+  jetDRMatch  =cms.untracked.double(0.3),
+
+  bjetEtaMax = cms.untracked.double(2.5),
+  bjetEtMin = cms.untracked.double(10.0),
+  bjetDRMatch  =cms.untracked.double(0.3),
+
+  photonEtaMax = cms.untracked.double(2.5),
+  photonEtMin = cms.untracked.double(3.0),
+  photonDRMatch  =cms.untracked.double(0.5),
+
+  trackEtaMax = cms.untracked.double(2.5),
+  trackEtMin = cms.untracked.double(3.0),
+  trackDRMatch  =cms.untracked.double(0.3),
+
+  metMin = cms.untracked.double(10.0),
+  htMin = cms.untracked.double(10.0),
+  sumEtMin = cms.untracked.double(10.0),
 
     paths = cms.VPSet(
             # cms.PSet(
