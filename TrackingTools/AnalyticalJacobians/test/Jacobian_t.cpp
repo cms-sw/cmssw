efe868c7d41f47a7e2d8a512484256c58ff5b50f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-21 06:26:15 +0000

    more tests

--- TrackingTools/AnalyticalJacobians/test/Jacobian_t.cpp
+++ TrackingTools/AnalyticalJacobians/test/Jacobian_t.cpp
@@ -10,9 +10,24 @@
 int main() {
 
   Surface::RotationType rot;
+  std::cout << rot << std::endl;
+
+
   Surface::PositionType pos( 0., 0., 0.);
 
   Plane plane(pos,rot);
+
+  GlobalVector g1 = plane.toGlobal(LocalVector(1., 0., 0.));
+  GlobalVector g2 = plane.toGlobal(LocalVector(0., 1., 0.));
+  GlobalVector g3 = plane.toGlobal(LocalVector(0., 0., 1.));
+  AlgebraicMatrix33 Rsub;
+  Rsub(0,0) = g1.x(); Rsub(0,1) = g2.x(); Rsub(0,2) = g3.x();
+  Rsub(1,0) = g1.y(); Rsub(1,1) = g2.y(); Rsub(1,2) = g3.y();
+  Rsub(2,0) = g1.z(); Rsub(2,1) = g2.z(); Rsub(2,2) = g3.z();
+  
+  std::cout << Rsub << std::endl;
+  
+
 
   LocalTrajectoryParameters tp(1.1.,1.,0.,0.,1.);
 
@@ -21,6 +36,8 @@ int main() {
   emd::HRTimeType e = hrRealTime();
 
   std::cout << e-s << std::endl;
+
+  std::cout << jl2c.jacobian() << std::endl;
 
   return 0;
 
