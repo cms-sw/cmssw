e9798285658a7d0ce18caccb35b6af56988a99f8
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-21 17:11:24 +0000

    one more test

---
+++ DataFormats/GeometrySurface/test/Bounds_t.cpp
@@ -0,0 +1,37 @@
+#include "DataFormats/GeometrySurface/interface/RectangularPlaneBounds.h"
+#include "DataFormats/GeometrySurface/interface/LocalError.h" 
+
+
+#include "FWCore/Utilities/interface/HRRealTime.h"
+#include<iotream>
+
+int main() {
+
+  RectangularPlaneBounds bound(1.,1.,1);
+
+  Local3DPoint in(0.,0.,0.);
+
+  Local3DPoint outY(0.,3.,0.);
+
+  LocalError err(0.1,0.0,0.1);
+
+  bool ok;
+
+  {
+    edm::HRTimeType s= edm::hrRealTime();
+    ok = bound.inside(in,err);
+    edm::HRTimeType e = edm::hrRealTime();
+    std::cout << e-s << std::endl;
+  }
+
+  {
+    edm::HRTimeType s= edm::hrRealTime();
+    ok = bound.inside(outY,err);
+    edm::HRTimeType e = edm::hrRealTime();
+    std::cout << e-s << std::endl;
+  }
+
+  
+  return 0;
+
+}
