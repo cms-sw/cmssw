7efc3d7a9d5102ae4eeec78daa82733abf0fa87b
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-21 06:07:36 +0000

    a test

--- TrackingTools/AnalyticalJacobians/test/Jacobian_t.cpp
+++ TrackingTools/AnalyticalJacobians/test/Jacobian_t.cpp
@@ -4,14 +4,23 @@
 
 #include "DataFormats/GeometrySurface/interface/Plane.h"
 
+
+
+#include "FWCore/ Utilities/ interface/ HRRealTime.h"
 int main() {
 
   Surface::RotationType rot;
   Surface::PositionType pos( 0., 0., 0.);
 
-  Plane(pos,rot);
+  Plane plane(pos,rot);
 
+  LocalTrajectoryParameters tp(1.1.,1.,0.,0.,1.);
 
+  emd::HRTimeType s= hrRealTime();
+  JacobianLocalToCartesia jl2c(plane,tp);
+  emd::HRTimeType e = hrRealTime();
+
+  std::cout << e-s << std::endl;
 
   return 0;
 
