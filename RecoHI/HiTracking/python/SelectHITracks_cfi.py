bc8dd74c099b2519215448f27fdc38ecd1d1ebbc
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-06-22 11:55:55 +0000

    track quality cuts and minpt selector

---
+++ RecoHI/HiTracking/python/SelectHITracks_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+# reco track quality cuts
+selectHiTracks = cms.EDFilter("TrackSelector",
+  src = cms.InputTag("globalPrimTracks"),
+  cut = cms.string('pt > 0.9 && d0/d0Error<3 && recHitsSize>12 &chi2prob(chi2,ndof)>0.01')
+)
