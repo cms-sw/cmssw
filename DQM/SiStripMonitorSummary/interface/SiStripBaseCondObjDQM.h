f3a5b397faf7872dd81d3a5d1f8bc64afbb719c0
benucci <sha1-daad2104d9852eedb2ba1d7c2d4eda3d03263c78@cern.ch>
2009-06-22 16:21:15 +0000

    configurable TkMap added

--- DQM/SiStripMonitorSummary/interface/SiStripBaseCondObjDQM.h
+++ DQM/SiStripMonitorSummary/interface/SiStripBaseCondObjDQM.h
@@ -110,6 +110,8 @@ class SiStripBaseCondObjDQM {
     bool SummaryOnStringLevel_On_;
     bool GrandSummary_On_;
 
+    //bool ActiveDetIds_On_;
+
     std::string CondObj_fillId_;
     std::string CondObj_name_;
 
@@ -133,7 +135,7 @@ class SiStripBaseCondObjDQM {
     void bookSummaryMEs(SiStripBaseCondObjDQM::ModMEs& CondObj_ME, const uint32_t& detId_);
     void bookTkMap(const std::string& TkMapname);
 
-    void saveTkMap(const std::string& TkMapname);
+    void saveTkMap(const std::string& TkMapname, const double& minValue, const double& maxValue);
 
     std::vector<uint32_t> ModulesToBeExcluded_;
     std::vector<uint32_t> ModulesToBeIncluded_;
