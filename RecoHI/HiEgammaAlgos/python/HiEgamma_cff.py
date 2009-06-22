2472bb9576b4d770867414a486a2d7159962418c
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-06-22 10:08:04 +0000

    include photon reco sequence

--- RecoHI/HiEgammaAlgos/python/HiEgamma_cff.py
+++ RecoHI/HiEgammaAlgos/python/HiEgamma_cff.py
@@ -1,9 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
+# clustering sequence
 from RecoEcal.EgammaClusterProducers.islandClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.hybridClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.multi5x5ClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.multi5x5PreshowerClusteringSequence_cff import *
 from RecoEcal.EgammaClusterProducers.preshowerClusteringSequence_cff import *
+from RecoEcal.EgammaClusterProducers.dynamicHybridClusteringSequence_cff import *
 
-hiEcalClusters = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence)
+hiEcalClusteringSequence = cms.Sequence(islandClusteringSequence*hybridClusteringSequence*preshowerClusteringSequence*dynamicHybridClusteringSequence*multi5x5ClusteringSequence*multi5x5PreshowerClusteringSequence)
+
+# reco photon producer
+from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
+photons.primaryVertexProducer = cms.string('pixelVertices') # replace the primary vertex
+photons.scHybridBarrelProducer = cms.InputTag("correctedIslandBarrelSuperClusters") # use island for the moment
+photons.scIslandEndcapProducer = cms.InputTag("correctedIslandEndcapSuperClusters") # use island for the moment
+hiPhotonSequence = cms.Sequence(photons)
+
+# HI Ecal reconstruction
+hiEcalClusters = cms.Sequence(hiEcalClusteringSequence * hiPhotonSequence)
