de9e6fc84a5beeecc68316d98ce77898122b06c0
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-06-23 16:12:32 +0000

    getting closer and closer..

--- PhysicsTools/PFCandProducer/python/PF2PAT_cff.py
+++ PhysicsTools/PFCandProducer/python/PF2PAT_cff.py
@@ -3,14 +3,13 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PFCandProducer.genMetTrue_cff  import *
 
 from PhysicsTools.PFCandProducer.pfMET_cfi  import *
-from PhysicsTools.PFCandProducer.pfPileUp_cfi  import *
+from PhysicsTools.PFCandProducer.pfNoPileUp_cff  import *
 from PhysicsTools.PFCandProducer.pfElectrons_cff import *
 from PhysicsTools.PFCandProducer.pfMuons_cff import *
 from PhysicsTools.PFCandProducer.pfJets_cff import *
 from PhysicsTools.PFCandProducer.pfTaus_cff import *
 
 # sequential top projection cleaning
-from PhysicsTools.PFCandProducer.TopProjectors.noPileUp_cfi import *
 from PhysicsTools.PFCandProducer.ParticleSelectors.sortByType_cff import *
 from PhysicsTools.PFCandProducer.TopProjectors.noMuon_cfi import * 
 from PhysicsTools.PFCandProducer.TopProjectors.noElectron_cfi import * 
@@ -21,8 +20,7 @@ from PhysicsTools.PFCandProducer.TopProjectors.noTau_cfi import *
 PF2PAT = cms.Sequence(
     genMetTrueSequence + 
     pfMET +
-    pfPileUp +
-    noPileUp + 
+    pfNoPileUpSequence + 
     sortByTypeSequence +
     pfElectronSequence +
     pfMuonSequence + 
