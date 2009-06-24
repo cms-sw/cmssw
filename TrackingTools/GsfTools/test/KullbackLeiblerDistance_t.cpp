ea898c59245ae6cae7338fca650dc5a070639fad
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-24 06:23:43 +0000

    real matrix

--- TrackingTools/GsfTools/test/KullbackLeiblerDistance_t.cpp
+++ TrackingTools/GsfTools/test/KullbackLeiblerDistance_t.cpp
@@ -1,6 +1,17 @@
 #include "TrackingTools/GsfTools/interface/KullbackLeiblerDistance.h"
 #include "TrackingTools/GsfTools/interface/DistanceBetweenComponents.h"
 
+#include "TrackingTools/AnalyticalJacobians/interface/JacobianLocalToCartesian.h"
+#include "TrackingTools/AnalyticalJacobians/interface/JacobianCartesianToLocal.h"
+#include "TrackingTools/AnalyticalJacobians/interface/JacobianLocalToCurvilinear.h"
+
+
+#include "DataFormats/GeometrySurface/interface/Surface.h"
+#include "TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryParameters.h"
+
+#include "DataFormats/GeometrySurface/interface/Plane.h"
+
+
 #include "FWCore/Utilities/interface/HRRealTime.h"
 #include<iostream>
 
@@ -19,10 +30,27 @@ void en(){}
       return *d;
   }
 
+Matrix buildCovariance() {
+
+  // build a resonable covariance matrix as JIJ
+
+  Basic3DVector<float>  axis(0.5,1.,1);
+  
+  Surface::RotationType rot(axis,0.5*M_PI);
+
+  Surface::PositionType pos( 0., 0., 0.);
+
+  Plane plane(pos,rot);
+  JacobianLocalToCartesian jl2c(plane,tp);
+  return  jl2c.jacobian().transpose()* jl2c.jacobian();
+
+}
+
 int main() {
 
   Distance const & d = distance();
 
+  Matrix cov = buildCovariance();
 
   GS * gs1 = new GS(Vector(1., 1.,1., 1.,1.),Matrix(ROOT::Math::SMatrixIdentity()));
 
@@ -30,7 +58,7 @@ int main() {
   GS * gsP = new GS(Vector(1., 1.,1., 10.,10.),Matrix(ROOT::Math::SMatrixIdentity()));
 
 
-  GS * gs2 = new GS(Vector(2., 2., 2., 2.,2.),ROOT::Math::SMatrixIdentity());
+  GS * gs2 = new GS(Vector(2., 2., 2., 2.,2.),cov);
 
   // make sure we load all code...
   edm::HRTimeType s0= edm::hrRealTime();
