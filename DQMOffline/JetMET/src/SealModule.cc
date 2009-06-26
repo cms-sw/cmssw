9489b7fbfee5b81101c971a0db0a913506ad0ef3
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-06-26 10:05:19 +0000

    Added Calotower,ECAL,HCAL Analyzers from Validation/RecoMET

--- DQMOffline/JetMET/src/SealModule.cc
+++ DQMOffline/JetMET/src/SealModule.cc
@@ -2,10 +2,15 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "DQMOffline/JetMET/src/JetMETAnalyzer.h"
-
+#include "DQMOffline/JetMET/interface/CaloTowerAnalyzer.h"
+#include "DQMOffline/JetMET/interface/ECALRecHitAnalyzer.h"
+#include "DQMOffline/JetMET/interface/HCALRecHitAnalyzer.h"
 // the clients
 // #include "DQMOffline/Muon/src/MuonTrackResidualsTest.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(JetMETAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(CaloTowerAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(HCALRecHitAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(ECALRecHitAnalyzer);
 // DEFINE_ANOTHER_FWK_MODULE(MuonTrackResidualsTest);
