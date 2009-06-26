18026575ea785f9095b3d80bf9cb5f58e1f975ac
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-06-26 11:33:29 +0000

    Moved Calotower, ECAL, HCAL analyzers to DQMOffline/JetMET

--- Validation/RecoMET/src/SealModule.cc
+++ Validation/RecoMET/src/SealModule.cc
@@ -1,8 +1,8 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "Validation/RecoMET/interface/CaloTowerAnalyzer.h"
-#include "Validation/RecoMET/interface/ECALRecHitAnalyzer.h"
-#include "Validation/RecoMET/interface/HCALRecHitAnalyzer.h"
+//#include "Validation/RecoMET/interface/CaloTowerAnalyzer.h"
+//#include "Validation/RecoMET/interface/ECALRecHitAnalyzer.h"
+//#include "Validation/RecoMET/interface/HCALRecHitAnalyzer.h"
 #include "Validation/RecoMET/interface/METTester.h"
 #include "Validation/RecoMET/interface/METFileSaver.h"
 
@@ -10,7 +10,7 @@ DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE (METFileSaver) ;
 DEFINE_ANOTHER_FWK_MODULE (METTester) ;
-DEFINE_ANOTHER_FWK_MODULE (CaloTowerAnalyzer) ;
-DEFINE_ANOTHER_FWK_MODULE (ECALRecHitAnalyzer) ;
-DEFINE_ANOTHER_FWK_MODULE (HCALRecHitAnalyzer) ;
+//DEFINE_ANOTHER_FWK_MODULE (CaloTowerAnalyzer) ;
+//DEFINE_ANOTHER_FWK_MODULE (ECALRecHitAnalyzer) ;
+//DEFINE_ANOTHER_FWK_MODULE (HCALRecHitAnalyzer) ;
 
