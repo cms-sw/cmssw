553c7d0afaa7740abdbced017e93fd9b52e68312
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-06-26 15:24:48 +0000

    Changed name for QCD photons module

--- DQM/Physics/src/SealModule.cc
+++ DQM/Physics/src/SealModule.cc
@@ -3,8 +3,8 @@
 
 
 #include "DQM/Physics/src/BPhysicsOniaDQM.h"
-#include "DQM/Physics/src/QcdPhotons.h"
+#include "DQM/Physics/src/QcdPhotonsPhotonJetDQM.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(BPhysicsOniaDQM);
-DEFINE_ANOTHER_FWK_MODULE(QcdPhotons);
+DEFINE_ANOTHER_FWK_MODULE(QcdPhotonsPhotonJetDQM);
