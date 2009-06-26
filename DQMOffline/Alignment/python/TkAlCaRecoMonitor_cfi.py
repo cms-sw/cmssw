d6b619603d9b13e36a263df214936c80b120adc8
Matthias Edelhoff <sha1-b4e200e941da071480460e77b2afa35e3ad47d9a@cern.ch>
2009-06-26 14:01:29 +0000

    set default value of OutputMEsInRootFile to False

--- DQMOffline/Alignment/python/TkAlCaRecoMonitor_cfi.py
+++ DQMOffline/Alignment/python/TkAlCaRecoMonitor_cfi.py
@@ -52,7 +52,7 @@ TkAlCaRecoMonitor = cms.EDAnalyzer("TkAlCaRecoMonitor",
                                    #
                                    daughterMass = cms.double(0.10565836),#Gev
                                    FolderName = cms.string("TkAlCaRecoMonitor"),
-                                   OutputMEsInRootFile = cms.bool(True),
+                                   OutputMEsInRootFile = cms.bool(False),
                                    OutputFileName = cms.string("TkAlCaRecoDQM.root")
                                    )
                                    
