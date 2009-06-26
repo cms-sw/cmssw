55d40e1234534fa1984d728314ec23ca25fbe60b
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2009-06-26 10:04:23 +0000

    Adding first filter (i.e. L1/gen) in the root file

--- HLTriggerOffline/Egamma/src/EmDQMPostProcessor.cc
+++ HLTriggerOffline/Egamma/src/EmDQMPostProcessor.cc
@@ -128,8 +128,11 @@ void EmDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& es)
       ///////////////////////////////////////////
       // compute per-object efficiencies       //
       ///////////////////////////////////////////
-      MonitorElement *eff, *num, *denom, *genPlot, *effVsGen;
-      std::vector<std::string> varNames; varNames.push_back("eta"); varNames.push_back("et");
+      MonitorElement *eff, *num, *denom, *genPlot, *effVsGen, *effL1VsGen;
+      std::vector<std::string> varNames; 
+      varNames.push_back("eta"); 
+      varNames.push_back("et");
+
       std::string filterName;
       std::string filterName2;
       std::string denomName;
@@ -138,6 +141,31 @@ void EmDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& es)
       // Get the gen-level plots
       std::string genName;
 
+      // Get the L1 over gen filter first
+      filterName2= total->getTH1F()->GetXaxis()->GetBinLabel(1);
+	
+      //loop over variables (eta/et)
+      for(std::vector<std::string>::iterator var = varNames.begin(); var != varNames.end() ; var++){
+	numName   = dqm->pwd() + "/" + filterName2 + *var + *postfix;
+	genName   = dqm->pwd() + "/gen_" + *var ;
+	num       = dqm->get(numName);
+	genPlot   = dqm->get(genName);
+        effL1VsGen = dqm->book1D("efficiency_"+filterName2+"_vs_"+*var +*postfix, dqm->get(numName)->getTH1F());
+
+	// Check if histograms actually exist
+	if(!num || !genPlot) break; 
+	
+	// Make sure we are able to book new element
+	if (!dqm) break;
+
+	// Create the efficiency plot
+	effL1VsGen->setTitle("efficiency_"+filterName2+"_vs_"+*var + *postfix);
+	effL1VsGen->getTH1F()->SetMaximum(1.2);
+	effL1VsGen->getTH1F()->SetMinimum(0);
+	effL1VsGen->getTH1F()->GetXaxis()->SetTitle(var->c_str());
+	effL1VsGen->getTH1F()->Divide(num->getTH1F(),genPlot->getTH1F(),1,1,"b" );
+      }
+    
       // get the filter names from the bin-labels of the master-histogram
       for (int filter=1; filter < total->getNbinsX()-2; filter++) {
 	filterName = total->getTH1F()->GetXaxis()->GetBinLabel(filter);
@@ -158,11 +186,12 @@ void EmDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& es)
 
 	  // Is this the last filter? Book efficiency vs gen level
 	  std::string temp = *postfix;
-          if (filter==total->getNbinsX()-2-1 && temp.find("matched")!=std::string::npos) {
+          if (filter==total->getNbinsX()-3 && temp.find("matched")!=std::string::npos) {
             genName = dqm->pwd() + "/gen_" + *var;
             genPlot = dqm->get(genName);
             effVsGen = dqm->book1D("final_eff_vs_"+*var, dqm->get(genName)->getTH1F());
             if (!dqm) break;
+
             effVsGen->setTitle("Efficiency Compared to Gen vs "+*var);
 	    effVsGen->getTH1F()->SetMaximum(1.2); effVsGen->getTH1F()->SetMinimum(0.0);
 	    effVsGen->getTH1F()->GetXaxis()->SetTitle(var->c_str());
@@ -175,6 +204,10 @@ void EmDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& es)
           if (!dqm) break;
 
           // Create the efficiency plot
+          /* num->getTH1F()->Sumw2();
+	  denom->getTH1F()->Sumw2(); 
+          eff->getTH1F()->Sumw2(); */
+
 	  eff->setTitle("efficiency_"+filterName2+"_vs_"+*var + *postfix);
 	  eff->getTH1F()->SetMaximum(1.2);
 	  eff->getTH1F()->SetMinimum(0);
