271dd4f948857ce15fc3951c53cfc72888ecd3e5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-06-27 17:05:13 +0000

    one more test

---
+++ TrackingTools/GsfTracking/test/GsfMaterialEffectsUpdator_t.cpp
@@ -0,0 +1,80 @@
+// test of  GsfMaterialEffectsUpdator
+
+
+#include "TrackingTools/GsfTracking/interface/GsfMaterialEffectsUpdator.h"
+
+
+
+#include "TrackingTools/GsfTracking/interface/GsfBetheHeitlerUpdator.h"
+
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "DataFormats/TrajectorySeed/interface/PropagationDirection.h"
+#include "DataFormats/GeometrySurface/interface/Surface.h"
+
+#include "TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryParameters.h"
+
+#include "DataFormats/GeometrySurface/interface/Plane.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+
+namespace {
+
+  struct M5T : public  MagneticField {
+    M5T() :  m(0.,0.,5.){}
+    virtual GlobalVector inTesla (const GlobalPoint&) const {
+      return m;
+    }
+
+    GlobalVector m;
+  };
+
+}
+
+
+#include "FWCore/Utilities/interface/HRRealTime.h"
+#include<iostream>
+#include<vector>
+
+
+void st(){}
+void en(){}
+
+
+
+
+int main(int argc, char * arg[]) {
+
+  if (argc<2) {
+    srd::cerr << "give parameter file as first argument" << std::endl;
+    return 1;
+  }
+
+
+  GsfBetheHeitlerUpdator bhu(arg[1],0); 
+  
+  GsfMaterialEffectsUpdator * meu = bhu;
+
+
+  Basic3DVector<float>  axis(0.5,1.,1);
+  
+  Surface::RotationType rot(axis,0.5*M_PI);
+
+  Surface::PositionType pos( 0., 0., 0.);
+
+  Plane plane(pos,rot);
+  plane.setMediumProperties(MediumProperties(0.1,0.3));
+
+  LocalTrajectoryParameters tp(1., 1.,1., 0.,0.,0.);
+  
+  M5T const m; 
+  
+  
+  TrajectoryStateOnSurface tsos(tp,plane, &m);
+  
+  st();
+  meu->compute(tsos,alongMomentum);
+  en();
+
+  return 0;
+
+}
