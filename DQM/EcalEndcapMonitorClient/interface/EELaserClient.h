678934609c8ebd360a9aba558e6fee22eda0d288
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2009-06-29 13:27:50 +0000

    book only the enabled laser/led colors (configurable)

--- DQM/EcalEndcapMonitorClient/interface/EELaserClient.h
+++ DQM/EcalEndcapMonitorClient/interface/EELaserClient.h
@@ -4,8 +4,8 @@
 /*
  * \file EELaserClient.h
  *
- * $Date: 2009/06/09 15:36:37 $
- * $Revision: 1.30 $
+ * $Date: 2009/06/18 12:27:01 $
+ * $Revision: 1.31 $
  * \author G. Della Ricca
  *
 */
@@ -85,6 +85,7 @@ std::string prefixME_;
 bool enableCleanup_;
 
 std::vector<int> superModules_;
+std::vector<int> laserWavelengths_;
 
 DQMStore* dqmStore_;
 
