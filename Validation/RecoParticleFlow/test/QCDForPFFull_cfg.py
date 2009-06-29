1ac937e7ddc8c2d54d2abc905aa363f68b109559
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2009-06-29 09:07:21 +0000

    Change global tag name (in 310pre11)

--- Validation/RecoParticleFlow/test/QCDForPFFull_cfg.py
+++ Validation/RecoParticleFlow/test/QCDForPFFull_cfg.py
@@ -28,7 +28,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_40T_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "IDEAL_31X::All"
+process.GlobalTag.globaltag = "MC_31X_V1::All"
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
