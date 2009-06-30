b411376dfc059180db854918a499b4e50ad54d72
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2009-06-30 14:17:42 +0000

    example python config file for JetMET DQM jobs

---
+++ DQMOffline/JetMET/test/cruzet09.py
@@ -0,0 +1,97 @@
+# The following comments couldn't be translated into the new config version:
+
+#  include "RecoMuon/TrackingTools/data/MuonServiceProxy.cff"
+#  include "Geometry/CMSCommonData/data/cmsIdealGeometryXML.cff"
+#  include "MagneticField/Engine/data/volumeBasedMagneticField.cfi"
+#  include "Geometry/CommonDetUnit/data/globalTrackingGeometry.cfi"
+#  include "RecoMuon/DetLayers/data/muonDetLayerGeometry.cfi"
+
+# the clients
+#  include "DQMOffline/Muon/data/trackResidualsTest.cfi"
+#  module qTester = QualityTester{
+#     untracked int32 prescaleFactor = 1
+#     untracked FileInPath qtList = "DQMOffline/Muon/data/QualityTests.xml" 
+#  }
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Reco")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+#
+# DQM
+#
+process.load("DQMServices.Core.DQM_cfg")
+
+process.load("DQMServices.Components.MEtoEDMConverter_cfi")
+
+# the task
+#process.load("DQMOffline.JetMET.jetMETAnalyzer_cff")
+process.load("DQMOffline.JetMET.jetMETDQMOfflineSource_cff")
+process.jetMETAnalyzer.OutputMEsInRootFile = cms.bool(True)
+process.jetMETAnalyzer.OutputFileName = cms.string('jetMETMonitoring_cruzet98154_log7.root')
+
+# added caloTower-layer plots (now included in jetMETDQMOfflineSource_cff)
+#process.load("DQMOffline.JetMET.caloTowers_cff")
+#process.load("Validation.RecoMET.caloTowers_cff")
+
+# check # of bins
+process.load("DQMServices.Components.DQMStoreStats_cfi")
+
+# for igprof
+process.IgProfService = cms.Service("IgProfService",
+  reportFirstEvent            = cms.untracked.int32(0),
+  reportEventInterval         = cms.untracked.int32(25),
+  reportToFileAtPostEvent     = cms.untracked.string("| gzip -c > igdqm.%I.gz")
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    '/store/data/CRUZET09/Calo/RECO/v1/000/098/154/EADF3BE3-BE4F-DE11-8BB8-000423D9870C.root'
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32( 100 )
+)
+process.Timing = cms.Service("Timing")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('jetMETAnalyzer'),
+    cout = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        jetMETAnalyzer = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000)
+        ),
+        noLineBreaks = cms.untracked.bool(True),
+        DEBUG = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        #FwkJob = cms.untracked.PSet(
+        #    limit = cms.untracked.int32(0)
+        #),
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    categories = cms.untracked.vstring('jetMETAnalyzer'),
+    destinations = cms.untracked.vstring('cout')
+)
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('reco-grumm.root')
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True) ## default is false
+
+)
+
+#process.p = cms.Path(process.jetMETAnalyzer*process.analyzecaloTowers)
+#process.p = cms.Path(process.jetMETAnalyzer*process.towerSchemeBAnalyzer)
+#process.p = cms.Path(process.jetMETAnalyzer*process.towerSchemeBAnalyzer)
+process.p = cms.Path(process.jetMETDQMOfflineSource*process.dqmStoreStats)
+process.outpath = cms.EndPath(process.FEVT)
+process.DQM.collectorHost = ''
+
