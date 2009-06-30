6d1a636ac6cb09beebad3681e21e0363fa3289fc
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-06-30 08:52:44 +0000

    for disabling module from release tag

--- HLTriggerOffline/Common/src/SealModule.cc
+++ HLTriggerOffline/Common/src/SealModule.cc
@@ -1,12 +1,12 @@
-// $Id: SealModule.cc,v 1.1 2009/01/24 17:10:44 berryhil Exp $
+// $Id: SealModule.cc,v 1.2 2009/06/22 20:11:40 wittich Exp $
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "HLTriggerOffline/Common/interface/FourVectorHLTriggerOffline.h"
 DEFINE_ANOTHER_FWK_MODULE(FourVectorHLTriggerOffline);
 
-#include "HLTriggerOffline/Common/interface/HltComparator.h"
-DEFINE_ANOTHER_FWK_MODULE(HltComparator);
+//#include "HLTriggerOffline/Common/interface/HltComparator.h"
+//DEFINE_ANOTHER_FWK_MODULE(HltComparator);
 
 
 
