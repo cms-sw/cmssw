39bb5b2dd22bf1dd6f185dc98f3ada32a784afb6
Dayong Wang <sha1-60ba7de852fc97a17e519d817810ab99f2289e6c@cern.ch>
2009-06-30 10:09:41 +0000

    fix the link to caloTowers_cfi

--- Validation/RecoMET/python/METRelValForDQM_cff.py
+++ Validation/RecoMET/python/METRelValForDQM_cff.py
@@ -25,7 +25,7 @@ genMetTrueAnalyzer.FineBinning = cms.untracked.bool(False)
 genMetCaloAnalyzer.FineBinning = cms.untracked.bool(False)
 genMetCaloAndNonPromptAnalyzer.FineBinning = cms.untracked.bool(False)
 
-from Validation.RecoMET.caloTowers_cfi import *
+from DQMOffline.JetMET.caloTowers_cfi import *
 towerSchemeBAnalyzer.FineBinning = cms.untracked.bool(False)
 
 METRelValSequence = cms.Sequence(
