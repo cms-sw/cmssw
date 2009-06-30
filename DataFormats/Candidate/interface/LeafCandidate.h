7648fd4edb7ac086f953e021ffdb8b892507ea11
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-06-30 11:07:10 +0000

    Move implementations of a (virtual) function from LeafCandidate.h to LeafCandidate.cc to avoid unnecessary out-of-line inline functions. Making these explicit also allows us to turn on -fvisibility-inlines-hidden.

--- DataFormats/Candidate/interface/LeafCandidate.h
+++ DataFormats/Candidate/interface/LeafCandidate.h
@@ -6,7 +6,7 @@
  *
  * \author Luca Lista, INFN
  *
- * \version $Id: LeafCandidate.h,v 1.19 2008/12/16 01:32:04 hegner Exp $
+ * \version $Id: LeafCandidate.h,v 1.20 2009/03/28 19:52:21 hegner Exp $
  *
  */
 #include "DataFormats/Candidate/interface/Candidate.h"
@@ -131,7 +131,7 @@ namespace reco {
     /// z coordinate of momentum vector                                                   
     virtual double pz() const { cacheCartesian(); return p4Cartesian_.Pz(); }
     /// transverse momentum                                                               
-    virtual double pt() const { return pt_; }
+    virtual double pt() const; 
     /// momentum azimuthal angle                                                          
     virtual double phi() const { return phi_; }
     /// momentum polar angle                                                              
