b7c96c2438a12d7fe2f59749a320f9f5e2fcb14f
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-07-01 19:54:00 +0000

    cosmic sequence

--- Configuration/GlobalRuns/python/promptReco_RAW2DIGI_RECO_DQM.py
+++ Configuration/GlobalRuns/python/promptReco_RAW2DIGI_RECO_DQM.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.123 
 # Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: promptReco -s RAW2DIGI,RECO,DQM --datatier RECO --eventcontent RECO --conditions FrontierConditions_GlobalTag,GR09_31X_V1P::All -n 1 --no_exec --data --magField 0T
+# with command line options: promptReco -s RAW2DIGI,RECO,DQM --datatier RECO --eventcontent RECO --conditions FrontierConditions_GlobalTag,GR09_31X_V1P::All -n 1 --no_exec --data --magField 0T --scenario cosmics
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('RECO')
@@ -14,14 +14,14 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
 process.load('Configuration/StandardSequences/MagneticField_0T_cff')
 process.load('Configuration/StandardSequences/RawToDigi_Data_cff')
-process.load('Configuration/StandardSequences/Reconstruction_cff')
-process.load('DQMOffline/Configuration/DQMOffline_cff')
+process.load('Configuration/StandardSequences/ReconstructionCosmics_cff')
+process.load('DQMOffline/Configuration/DQMOfflineCosmics_cff')
 process.load('Configuration/StandardSequences/EndOfProcess_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.load('Configuration/EventContent/EventContent_cff')
+process.load('Configuration/EventContent/EventContentCosmics_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.123 $'),
     annotation = cms.untracked.string('promptReco nevts:1'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -34,12 +34,11 @@ process.options = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Commissioning09/Cosmics/RAW/v2/000/101/725/FA919C5D-FC65-DE11-B8F3-001617C3B6CE.root')
+'/store/data/Commissioning09/Cosmics/RAW/v2/000/101/725/FA919C5D-FC65-DE11-B8F3-001617C3B6CE.root')
 )
 
 # Output definition
 process.FEVT = cms.OutputModule("PoolOutputModule",
-    splitLevel = cms.untracked.int32(0),
     outputCommands = process.RECOEventContent.outputCommands,
     fileName = cms.untracked.string('promptReco_RAW2DIGI_RECO_DQM.root'),
     dataset = cms.untracked.PSet(
@@ -55,8 +54,8 @@ process.GlobalTag.globaltag = 'GR09_31X_V1P::All'
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
-process.reconstruction_step = cms.Path(process.reconstruction)
-process.dqmoffline_step = cms.Path(process.DQMOffline)
+process.reconstruction_step = cms.Path(process.reconstructionCosmics)
+process.dqmoffline_step = cms.Path(process.DQMOfflineCosmics)
 process.endjob_step = cms.Path(process.endOfProcess)
 process.out_step = cms.EndPath(process.FEVT)
 
