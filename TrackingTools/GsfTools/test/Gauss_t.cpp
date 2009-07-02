793167cc62f24d48048de617e42a330cd91e5f40
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-02 10:40:28 +0000

    one more test

--- TrackingTools/GsfTools/test/Gauss_t.cpp
+++ TrackingTools/GsfTools/test/Gauss_t.cpp
@@ -14,12 +14,12 @@ int main(int argc, char * argv[]) {
 
  
   MultiGaussianState1D::SingleState1dContainer v(6);
-  v[0] = SingleGaussianState1DGS(0.,1.,1.);
-  v[1] = SingleGaussianState1DGS(0.,2.,0.5);
-  v[2] = SingleGaussianState1DGS(0.2,2.,0.5);
-  v[3] = SingleGaussianState1DGS(-0.2,2.,0.5);
-  v[4] = SingleGaussianState1DGS(0.,4.,1.0);
-  v[5] = SingleGaussianState1DGS(0.1,4.,0.3);
+  v[0] = SingleGaussianState1D(0.,1.,1.);
+  v[1] = SingleGaussianState1D(0.,2.,0.5);
+  v[2] = SingleGaussianState1D(0.2,2.,0.5);
+  v[3] = SingleGaussianState1D(-0.2,2.,0.5);
+  v[4] = SingleGaussianState1D(0.,4.,1.0);
+  v[5] = SingleGaussianState1D(0.1,4.,0.3);
 
   MultiGaussianState1D mgs(v);
 
