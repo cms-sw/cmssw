faebfbe4d4d8d57b2d4bcfcb3b91eaf9b101dd7c
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-07-02 15:13:29 +0000

    just make it run... also need some files from ecal/hcal but that is a separate issue.

--- Geometry/CMSCommonData/test/geometryTestRun_cfg.py
+++ Geometry/CMSCommonData/test/geometryTestRun_cfg.py
@@ -49,7 +49,16 @@ process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
 process.etta = cms.EDFilter("dumpEcalTrigTowerMapping")
 
 process.ctgw = cms.EDFilter("testEcalGetWindow")
+
+process.cga = cms.EDAnalyzer("CaloGeometryAnalyzer",
+                                 fullEcalDump = cms.untracked.bool(True)
+                             )
+process.load("Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi")
+
+
+process.ctgw = cms.EDFilter("testEcalGetWindow")
 process.mfa = cms.EDFilter("testMagneticField")
+
 process.TFileService = cms.Service("TFileService", fileName = cms.string('calogeom.root') )
 
 process.cga = cms.EDAnalyzer("CaloGeometryAnalyzer",
@@ -65,7 +74,7 @@ process.MessageLogger = cms.Service("MessageLogger",
                                         warning = cms.untracked.PSet(
             threshold = cms.untracked.string('WARNING')
                 ),
-                                        debugModules = cms.untracked.vstring('DDLParser'),
+                                        debugModules = cms.untracked.vstring('*'),
                                         critical = cms.untracked.PSet(
             threshold = cms.untracked.string('ERROR')
                 ),
