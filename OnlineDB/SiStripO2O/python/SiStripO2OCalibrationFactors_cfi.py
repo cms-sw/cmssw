986fc02989e68b55e648fa1d14872239b67569ab
alinn <sha1-1ccfb7a1878c5f91cc562c39cc64f98a5d05b691@cern.ch>
2009-07-02 13:50:00 +0000

    Moved from hardcoded calibration factors to cfi file

---
+++ OnlineDB/SiStripO2O/python/SiStripO2OCalibrationFactors_cfi.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+SiStripO2OCalibrationFactors = cms.PSet(
+    # Default values written when SiStrip objects are not found
+    DefaultPedestal=cms.untracked.double(0),
+    DefaultNoise=cms.untracked.double(51),
+    DefaultThresholdLow=cms.untracked.double(2),
+    DefaultThresholdHigh=cms.untracked.double(5),
+    DefaultTickHeight=cms.untracked.double(640),
+
+    # Normalization Factor needed to convert Tick Height to Gain
+    GainNormalizationFactor=cms.untracked.double(640)
+    
+    )
+
