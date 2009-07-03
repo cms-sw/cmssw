6ae47e2e71201ab2b5427fc95108324b5d56e67b
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-07-03 12:12:01 +0000

    Adapted for changes in DummyCondDBWriter

--- CalibTracker/SiStripESProducers/plugins/DBWriter/modules.cc
+++ CalibTracker/SiStripESProducers/plugins/DBWriter/modules.cc
@@ -15,68 +15,59 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripFedCablingManipulator);
 #include "CalibTracker/SiStripESProducers/plugins/DBWriter/DummyCondDBWriter.h"
 
 #include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
-struct CabRcdName{ static const char* name(){return "SiStripFedCablingRcd";}};
-typedef DummyCondDBWriter<SiStripFedCabling,SiStripFedCabling,SiStripFedCablingRcd,CabRcdName> SiStripFedCablingDummyDBWriter;
+typedef DummyCondDBWriter<SiStripFedCabling,SiStripFedCabling,SiStripFedCablingRcd> SiStripFedCablingDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripFedCablingDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
-struct PedRcdName{ static const char* name(){return "SiStripPedestalsRcd";}};
-typedef DummyCondDBWriter<SiStripPedestals,SiStripPedestals,SiStripPedestalsRcd,PedRcdName> SiStripPedestalsDummyDBWriter;
+typedef DummyCondDBWriter<SiStripPedestals,SiStripPedestals,SiStripPedestalsRcd> SiStripPedestalsDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripPedestalsDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
-struct NoiseRcdName{ static const char* name(){return "SiStripNoisesRcd";}};
-typedef DummyCondDBWriter<SiStripNoises,SiStripNoises,SiStripNoisesRcd,NoiseRcdName> SiStripNoisesDummyDBWriter;
+typedef DummyCondDBWriter<SiStripNoises,SiStripNoises,SiStripNoisesRcd> SiStripNoisesDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripNoisesDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
-struct GainRcdName{ static const char* name(){return "SiStripApvGainRcd";}};
-typedef DummyCondDBWriter<SiStripApvGain,SiStripApvGain,SiStripApvGainRcd,GainRcdName> SiStripApvGainDummyDBWriter;
+typedef DummyCondDBWriter<SiStripApvGain,SiStripApvGain,SiStripApvGainRcd> SiStripApvGainDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripApvGainDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h"
-struct LARcdName{ static const char* name(){return "SiStripLorentzAngleRcd";}};
-typedef DummyCondDBWriter<SiStripLorentzAngle,SiStripLorentzAngle,SiStripLorentzAngleRcd,LARcdName> SiStripLorentzAngleDummyDBWriter;
+typedef DummyCondDBWriter<SiStripLorentzAngle,SiStripLorentzAngle,SiStripLorentzAngleRcd> SiStripLorentzAngleDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripLorentzAngleDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripThreshold.h"
-struct ThRcdName{ static const char* name(){return "SiStripThresholdRcd";}};
-typedef DummyCondDBWriter<SiStripThreshold,SiStripThreshold,SiStripThresholdRcd,ThRcdName> SiStripThresholdDummyDBWriter;
+typedef DummyCondDBWriter<SiStripThreshold,SiStripThreshold,SiStripThresholdRcd> SiStripThresholdDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripThresholdDummyDBWriter);
 
 
 #include "CondFormats/SiStripObjects/interface/SiStripBadStrip.h"
-struct BadStripRcdName{ static const char* name(){return "SiStripBadStrip";}};
-typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadStripRcd,BadStripRcdName> SiStripBadStripDummyDBWriter;
+typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadStripRcd> SiStripBadStripDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadStripDummyDBWriter);
 
-typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadModuleRcd,BadStripRcdName> SiStripBadModuleDummyDBWriter;
+typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadModuleRcd> SiStripBadModuleDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadModuleDummyDBWriter);
 
-typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadFiberRcd,BadStripRcdName> SiStripBadFiberDummyDBWriter;
+typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadFiberRcd> SiStripBadFiberDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadFiberDummyDBWriter);
 
-typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadChannelRcd,BadStripRcdName> SiStripBadChannelDummyDBWriter;
+typedef DummyCondDBWriter<SiStripBadStrip,SiStripBadStrip,SiStripBadChannelRcd> SiStripBadChannelDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadChannelDummyDBWriter);
 
 #include "CalibFormats/SiStripObjects/interface/SiStripQuality.h"
 #include "CalibTracker/Records/interface/SiStripQualityRcd.h"
-typedef DummyCondDBWriter<SiStripQuality,SiStripBadStrip,SiStripQualityRcd,BadStripRcdName> SiStripBadStripFromQualityDummyDBWriter;
+typedef DummyCondDBWriter<SiStripQuality,SiStripBadStrip,SiStripQualityRcd> SiStripBadStripFromQualityDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripBadStripFromQualityDummyDBWriter);
 
 #include "CondFormats/SiStripObjects/interface/SiStripModuleHV.h"
-struct ModuleHVRcdName{ static const char* name(){return "SiStripModuleHVRcd";}};
-typedef DummyCondDBWriter<SiStripModuleHV,SiStripModuleHV,SiStripModuleHVRcd,ModuleHVRcdName> SiStripModuleHVDummyDBWriter;
+typedef DummyCondDBWriter<SiStripModuleHV,SiStripModuleHV,SiStripModuleHVRcd> SiStripModuleHVDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripModuleHVDummyDBWriter);
 
 #include "CondFormats/SiStripObjects/interface/SiStripDetVOff.h"
-struct DetVOffRcdName{ static const char* name(){return "SiStripDetVOffRcd";}};
-typedef DummyCondDBWriter<SiStripDetVOff,SiStripDetVOff,SiStripDetVOffRcd,DetVOffRcdName> SiStripDetVOffDummyDBWriter;
+typedef DummyCondDBWriter<SiStripDetVOff,SiStripDetVOff,SiStripDetVOffRcd> SiStripDetVOffDummyDBWriter;
 DEFINE_ANOTHER_FWK_MODULE(SiStripDetVOffDummyDBWriter);
 
 
