5b2aa7adba403113648476e211fa1d7ccbcac002
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-07-03 01:11:55 +0000

    new record for Track fitters

---
+++ TrackingTools/TrackFitters/src/TrajectoryFitterRecord.cc
@@ -0,0 +1,4 @@
+#include "TrackingTools/TrackFitters/interface/TrajectoryFitterRecord.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(TrajectoryFitterRecord);
