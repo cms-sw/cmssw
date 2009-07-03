1e5707a1503477e30267e8db1500dc5e309f5f96
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2009-07-03 07:50:22 +0000

    Add validationprodHarvesting to the harvesting sequences

--- Configuration/StandardSequences/python/Harvesting_cff.py
+++ Configuration/StandardSequences/python/Harvesting_cff.py
@@ -12,6 +12,8 @@ dqmHarvesting = cms.Path(DQMOffline_SecondStep*DQMOffline_Certification)
 
 validationHarvesting = cms.Path(postValidation*hltpostvalidation)
 
+validationprodHarvesting = cms.Path(postValidation*hltpostvalidation_prod)
+
 validationHarvestingFS = cms.Path(HarvestingFastSim)
 
 #alcaHarvesting = cms.Path()
