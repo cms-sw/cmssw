fedde52b1e4a0967e3d4589b999859d51a363b29
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-07-03 15:06:00 +0000

    Truncate sorted input calo towers instead of copying them. Saves some timing.

--- RecoJets/JetProducers/src/SISConeJetProducer.cc
+++ RecoJets/JetProducers/src/SISConeJetProducer.cc
@@ -1,12 +1,13 @@
 // File: SISConeJetProducer.cc
 // Description:  see SISConeJetProducer.h
 // Author:  Fedor Ratnikov, Maryland, June 30, 2007
-// $Id: SISConeJetProducer.cc,v 1.2 2007/08/02 17:42:58 fedor Exp $
+// $Id: SISConeJetProducer.cc,v 1.3 2009/07/02 21:21:15 srappocc Exp $
 //
 //--------------------------------------------
 #include <memory>
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "CommonTools/Utils/interface/PtComparator.h"
 #include "SISConeJetProducer.h"
 
 using namespace std;
@@ -19,6 +20,14 @@ namespace {
 
 namespace cms
 {
+
+
+  struct GreaterByPtCaloTower {
+    bool operator()( const JetReco::InputItem & t1, const JetReco::InputItem & t2 ) const {
+      return t1->pt() > t2->pt();
+    }
+  };
+
 
   // Constructor takes input parameters now: to be replaced with parameter set.
 
@@ -33,8 +42,17 @@ namespace cms
 		     JetReco::OutputCollection* fOutput) {
      //do not run algorithm for more than 1000 input elements
     if(fInput.size() > 1000) {
-      edm::LogError("SISConeNotRun") << "Too many calo tower in the event, sisCone collection will be empty";
-      return false;
+      // sort in pt
+      JetReco::InputCollection & fInputMutable = const_cast<JetReco::InputCollection &>( fInput );
+      GreaterByPtCaloTower                   pTComparator;
+      std::sort(fInputMutable.begin(), fInputMutable.end(), pTComparator);
+      // now restrict first 1000 events
+      fInputMutable.resize(1000);
+      edm::LogWarning("SISConeTooManyEntries") << "Too many calo towers in the event, sisCone is limiting to first 1000. Output is suspect.";
+//       edm::LogError("SISConeNotRun") << "Too many calo tower in the event, sisCone collection will be empty";
+//       return false;
+      alg_.run( fInputMutable, fOutput );
+      return true;
     }
     
     alg_.run (fInput, fOutput);
