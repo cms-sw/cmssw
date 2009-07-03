90179bb56d88854bc831ee3c2149f90f5bce3e8e
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-07-03 00:30:26 +0000

    add file collecting all records

---
+++ TrackingTools/Records/interface/Records.h
@@ -0,0 +1,4 @@
+#include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
+#include "TrackingTools/Records/interface/DetIdAssociatorRecord.h"
+#include "TrackingTools/Records/interface/TransientRecHitRecord.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
