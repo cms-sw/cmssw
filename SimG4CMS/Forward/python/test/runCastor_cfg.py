112c6d9ec79b1f9f8bb0a2a42f90690dc27de7b1
Benoit Roland <sha1-2200346e613b7795d3e973c9eb4d9c0d63733b86@cern.ch>
2009-07-03 16:43:41 +0000

    common_maximum_time local definition

--- SimG4CMS/Forward/python/test/runCastor_cfg.py
+++ SimG4CMS/Forward/python/test/runCastor_cfg.py
@@ -1,15 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("CastorTest")
+
 process.load("SimGeneral.HepPDTESSource.pdt_cfi")
 
 process.load("IOMC.EventVertexGenerators.VtxSmearedGauss_cfi")
 
-process.load("Configuration.StandardSequences.GeometryExtended_cff")
-#process.load("SimG4CMS.Forward.castorGeometryXML_cfi")
+# process.load("Configuration.StandardSequences.GeometryExtended_cff")
+process.load("SimG4CMS.Forward.castorGeometryXML_cfi")
+
 process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
 
 process.RandomNumberGeneratorService.theSource.initialSeed = 113456789
+
+process.load("Configuration.EventContent.EventContent_cff")
+
 process.load("SimG4Core.Application.g4SimHits_cfi")
 
 process.MessageLogger = cms.Service("MessageLogger",
@@ -20,18 +25,17 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10)
 )
 
-
 process.source = cms.Source("EmptySource")
 
 process.generator = cms.EDProducer("FlatRandomEGunProducer",
     PGunParameters = cms.PSet(
-        PartID = cms.vint32(13),
-        MaxEta = cms.double(5.9),
-        MaxPhi = cms.double(0.0),
+        PartID = cms.vint32(11),
         MinEta = cms.double(5.9),
+        MaxEta = cms.double(5.9),
+        MinPhi = cms.double(0.0),
+        MaxPhi = cms.double(0.392),
         MinE = cms.double(50.00),
-        MinPhi = cms.double(0.392),
-        MaxE = cms.double(50.01)
+        MaxE = cms.double(50.00)
     ),
     AddAntiParticle = cms.bool(True),
     Verbosity = cms.untracked.int32(0)
@@ -44,23 +48,53 @@ process.CaloSD = cms.PSet(
     UseMap = cms.bool(True),
     CheckHits = cms.int32(25)
 )
+
 process.o1 = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('sim_muon.root')
+    fileName = cms.untracked.string('sim_electron.root')
 )
 
+process.common_maximum_timex = cms.PSet( # need to be localy redefined
+   MaxTrackTime  = cms.double(10000.0),  # need to be localy redefined
+   MaxTimeNames  = cms.vstring(), # need to be localy redefined
+   MaxTrackTimes = cms.vdouble()  # need to be localy redefined
+)
 process.p1 = cms.Path(process.generator*process.VtxSmeared*process.g4SimHits)
 process.outpath = cms.EndPath(process.o1)
+
 process.g4SimHits.UseMagneticField = False
-process.g4SimHits.Physics.DefaultCutValue = 10.
+process.g4SimHits.Physics.DefaultCutValue = 10. 
 process.g4SimHits.Generator.MinEtaCut = -7.0
 process.g4SimHits.Generator.MaxEtaCut = 7.0
 process.g4SimHits.CaloTrkProcessing.TestBeam = True
+
+process.g4SimHits.StackingAction = cms.PSet(
+   process.common_heavy_suppression,
+   process.common_maximum_timex,        # need to be localy redefined
+   TrackNeutrino = cms.bool(False),
+   KillHeavy     = cms.bool(False),
+   SaveFirstLevelSecondary = cms.untracked.bool(True),
+   SavePrimaryDecayProductsAndConversionsInTracker = cms.untracked.bool(True),
+   SavePrimaryDecayProductsAndConversionsInCalo    = cms.untracked.bool(True),
+   SavePrimaryDecayProductsAndConversionsInMuon    = cms.untracked.bool(True)
+)
+
+process.g4SimHits.SteppingAction = cms.PSet(
+   process.common_maximum_timex, # need to be localy redefined
+   KillBeamPipe            = cms.bool(True),
+   CriticalEnergyForVacuum = cms.double(2.0),
+   CriticalDensity         = cms.double(1e-15),
+   EkinNames               = cms.vstring(),
+   EkinThresholds          = cms.vdouble(),
+   EkinParticles           = cms.vstring(),
+   Verbosity               = cms.untracked.int32(0)
+)
+
 process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
     type = cms.string('CastorTestAnalysis'),
     CastorTestAnalysis = cms.PSet(
-        EventNtupleFileName = cms.string('eventNtuple_muon.root'),
+        EventNtupleFileName = cms.string('eventNtuple_electron.root'),
         Verbosity = cms.int32(0),
-        StepNtupleFileName = cms.string('stepNtuple.root'),
+        StepNtupleFileName = cms.string('stepNtuple_electron.root'),
         StepNtupleFlag = cms.int32(0),
         EventNtupleFlag = cms.int32(1)
     )
