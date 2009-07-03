4ec1d21db637a8f6cd0d457b30bc3742c856942d
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-07-03 17:40:07 +0000

    fix message level

--- CondTools/RunInfo/plugins/HLTScalerDBReader.cc
+++ CondTools/RunInfo/plugins/HLTScalerDBReader.cc
@@ -9,7 +9,6 @@
 #include "CondCore/DBCommon/interface/Connection.h"
 #include "CondCore/DBCommon/interface/CoralTransaction.h"
 #include "CondCore/DBCommon/src/CoralConnectionProxy.h"
-#include "CondFormats/RunInfo/interface/LuminosityInfo.h"
 #include "CoralBase/AttributeList.h"
 #include "CoralBase/Attribute.h"
 #include "CoralBase/AttributeSpecification.h"
@@ -44,7 +43,6 @@ lumi::HLTScalerDBReader::HLTScalerDBReader(const edm::ParameterSet&pset):lumi::H
   default:
     m_session->configuration().setMessageLevel( cond::Error );
   }
-  m_session->configuration().setMessageLevel(cond::Debug);
   m_session->configuration().setAuthenticationMethod(cond::XML);
   m_session->configuration().setAuthenticationPath(authPath);
 }
@@ -57,11 +55,12 @@ lumi::HLTScalerDBReader::fill(int startRun,
 			   int numberOfRuns,
 			   std::vector< std::pair<lumi::HLTScaler*,cond::Time_t> >& result){
   //fill hlt scaler info with 2 queries
-  //select max(lsnumber) from HLT_SUPERVISOR_LUMISECTIONS_V2;
-  //select distinct PATHNAME,LSNUMBER,L1PASS,PACCEPT from hlt_supervisor_lumisections_v2 where RUNNR=83037 order by LSNUMBER;
+  //select count(distinct PATHNAME ) as npath from HLT_SUPERVISOR_LUMISECTIONS_V2;
+  //select l.PATHNAME,l.LSNUMBER,l.L1PASS,l.PACCEPT,m.PSVALUE from hlt_supervisor_lumisections_v2 l, hlt_supervisor_scalar_map m where l.RUNNR=m.RUNNR and l.PSINDEX=m.PSINDEX and l.PATHNAME=m.PATHNAME and l.RUNNR=83037 order by l.LSNUMBER;
   //
+  
   try{
-    lumi::HLTScaler* h=0;
+    //    lumi::HLTScaler* h=0;
     m_session->open();
     cond::Connection con(m_constr,-1);
     con.connect(m_session);
@@ -75,9 +74,9 @@ lumi::HLTScalerDBReader::fill(int startRun,
     listoftabs=transaction.nominalSchema().listTables();
     std::string tabname("HLT_SUPERVISOR_LUMISECTIONS_V2");
     std::string maptabname("HLT_SUPERVISOR_SCALAR_MAP");
-    for( std::set<std::string>::iterator it=listoftabs.begin(); it!=listoftabs.end();++it ){
-      std::cout<<"tab: "<<*it<<std::endl;
-    }
+    //for( std::set<std::string>::iterator it=listoftabs.begin(); it!=listoftabs.end();++it ){
+    //  std::cout<<"tab: "<<*it<<std::endl;
+    //}
     if( !transaction.nominalSchema().existsTable(tabname) ) throw cond::TransactionException("coral","table not found");
 
     for( int currentRun=startRun;currentRun<stopRun;++currentRun){
@@ -91,21 +90,25 @@ lumi::HLTScalerDBReader::fill(int startRun,
       query0->defineOutput(nls);
       coral::ICursor& c=query0->execute();
       if( !c.next() ){
-	std::cout<<"run "<<currentRun<<" doesn't exist, filling null data"<<std::endl;
-	edm::LuminosityBlockID lu(currentRun,1);
-	cond::Time_t current=(cond::Time_t)(lu.value());
-	h=new lumi::HLTScaler;
-	h->setHLTNULL();
-	result.push_back(std::make_pair<lumi::HLTScaler*,cond::Time_t>(h,current));
-	c.close();
-	delete query0;
+	std::cout<<"requested run "<<currentRun<<" doesn't exist, do nothing"<<std::endl;
+	//edm::LuminosityBlockID lu(currentRun,1);
+	//cond::Time_t current=(cond::Time_t)(lu.value());
+	//h=new lumi::HLTScaler;
+	//h->setHLTNULL();
+	//result.push_back(std::make_pair<lumi::HLTScaler*,cond::Time_t>(h,current));
+	//c.close();
+	//delete query0;
 	//do not do the second query, break to the next run
-	break;
+	continue;
       }else{
 	npath=c.currentRow()["npath"].data<unsigned int>();
 	std::cout<<"number of paths "<<npath<<std::endl;
 	c.close();
 	delete query0;
+	if (npath==0) { 
+	  std::cout<<"requested run is empty "<<currentRun<<" do nothing"<<std::endl;
+	  continue;
+	}
       }
       //queries per run
       coral::IQuery* query1 = transaction.nominalSchema().newQuery();
@@ -120,36 +123,36 @@ lumi::HLTScalerDBReader::fill(int startRun,
       query1->addToOrderList("l.LSNUMBER");
       query1->setRowCacheSize( 10692 );
       coral::ICursor& cursor1 = query1->execute();
-
+      
       std::vector< lumi::HLTInfo > hltinfoPerLumi;
-      hltinfoPerLumi.reserve(100);
-      int currentLumiSection=1;
-      int currentPath=1;
-      while( cursor1.next() ){
-	const coral::AttributeList& row=cursor1.currentRow();
-	currentLumiSection=(int)row["lsnumber"].data<long long>();//updateLumiSection
-	std::cout<<"current run "<<currentRun<<std::endl;
-	std::cout<<"currentLumiSection "<<currentLumiSection<<std::endl;
-	std::cout<<"current path number"<<currentPath<<std::endl;
-	int hltinput=(int)row["hltinput"].data<long long>();
-	int hltratecounter=(int)row["hltratecounter"].data<long long>();
-	std::string pathname=row["pathname"].data<std::string>();
-	int prescale=(int)row["prescale"].data<long long>();
-	lumi::HLTInfo hltinfo(pathname,hltinput,hltratecounter,prescale);	  
-	hltinfoPerLumi.push_back( hltinfo );
-	if(currentPath==npath){
-	  lumi::HLTScaler* h=new lumi::HLTScaler;
-	  edm::LuminosityBlockID lu(currentRun,currentLumiSection);
-	  h->setHLTData(lu,hltinfoPerLumi);
-	  cond::Time_t current=(cond::Time_t)(lu.value());
-	  result.push_back(std::make_pair<lumi::HLTScaler*,cond::Time_t>(h,current));
-	  hltinfoPerLumi.clear();
-	  currentPath=0;
-	}//end if it's last path in the current lumisection	 
-	++currentPath;
-      }
-      cursor1.close();
-      delete query1;
+	hltinfoPerLumi.reserve(100);
+	int currentLumiSection=1;
+	int currentPath=1;
+	while( cursor1.next() ){
+	  const coral::AttributeList& row=cursor1.currentRow();
+	  currentLumiSection=(int)row["lsnumber"].data<long long>();//updateLumiSection
+	  std::cout<<"current run "<<currentRun<<std::endl;
+	  std::cout<<"currentLumiSection "<<currentLumiSection<<std::endl;
+	  std::cout<<"current path number"<<currentPath<<std::endl;
+	  int hltinput=(int)row["hltinput"].data<long long>();
+	  int hltratecounter=(int)row["hltratecounter"].data<long long>();
+	  std::string pathname=row["pathname"].data<std::string>();
+	  int prescale=(int)row["prescale"].data<long long>();
+	  lumi::HLTInfo hltinfo(pathname,hltinput,hltratecounter,prescale);	  
+	  hltinfoPerLumi.push_back( hltinfo );
+	  if(currentPath==npath){
+	    lumi::HLTScaler* h=new lumi::HLTScaler;
+	    edm::LuminosityBlockID lu(currentRun,currentLumiSection);
+	    h->setHLTData(lu,hltinfoPerLumi);
+	    cond::Time_t current=(cond::Time_t)(lu.value());
+	    result.push_back(std::make_pair<lumi::HLTScaler*,cond::Time_t>(h,current));
+	    hltinfoPerLumi.clear();
+	    currentPath=0;
+	  }//end if it's last path in the current lumisection	 
+	  ++currentPath;
+	}
+	cursor1.close();
+	delete query1;
     }
     std::cout<<"commit transaction"<<std::endl;
     transaction.commit(); 
