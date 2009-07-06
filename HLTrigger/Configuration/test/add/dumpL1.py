c99343786c7873d850fb5fdd4f31d6d14bbbf558
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-07-06 09:15:39 +0000

    add support for confdb: URLs

--- HLTrigger/Configuration/test/add/dumpL1.py
+++ HLTrigger/Configuration/test/add/dumpL1.py
@@ -4,7 +4,58 @@ import sys
 
 import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.SequenceTypes import *
-from table import process
+
+import urllib, urllib2
+import copy
+
+class ConfDBHandler(urllib2.BaseHandler):
+  confdb_gateway = 'http://cms-project-confdb-hltdev.web.cern.ch/cms-project-confdb-hltdev/get.jsp'
+  confdb_data    = { 'format' : 'python', 'dbName' : 'hltdev', 'configName' : '' }
+
+  def __init__(self):
+    pass
+
+  def confdb_open(self, req):
+    if isinstance(req, urllib2.Request):
+      url = req.get_selector()
+    else:
+      url = req
+
+    data = copy.deepcopy(self.confdb_data)
+    if ':' in url:
+      data['dbName'], data['configName'] = url.split(':')
+    else:
+      data['configName'] = url
+    return self.parent.open(self.confdb_gateway, urllib.urlencode(data))
+
+
+def extract_process(buffer):
+  local = locals()
+  exec buffer in globals(), local
+  if 'process' in local:
+    return local['process']
+  else:
+    raise Exception('No "process" object defined in the configuration')
+
+
+def read_config(url):
+  # for non-local files, use urllib2 with a custom handler for ConfDB urls ('confdb:[hltdev:]/dev/CMSSW_3_1_0/...')
+  read_config.opener = urllib2.build_opener(ConfDBHandler)
+
+  if ':' in url:
+    input = read_config.opener.open(url)
+  else:
+    input = open(url, 'r')
+  config = input.read()
+  if 'Exhausted Resultset' in config:
+    raise IOError('Could not read the configuration %s' % url)
+  input.close()
+  return config
+
+
+url = sys.argv[1]
+config = read_config(url)
+process = extract_process(config)
 
 for (name,path) in process.paths.iteritems():
   modules = []
