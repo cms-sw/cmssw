3588c7e20f17d4907206b1bebc55d219c172dca9
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2009-07-06 12:06:39 +0000

    make saturation detection more robust

--- DataFormats/EcalDigi/src/EcalDataFrame.cc
+++ DataFormats/EcalDigi/src/EcalDataFrame.cc
@@ -6,7 +6,7 @@ int EcalDataFrame::lastUnsaturatedSample() const
         for ( size_t i = 3; i < m_data.size(); ++i ) {
                 cnt = 0;
                 for ( size_t j = i; j < (i + 5) && j < m_data.size(); ++j ) {
-                        if ( ((EcalMGPASample)m_data[i]).gainId() == 0 ) ++cnt;
+                        if ( ((EcalMGPASample)m_data[j]).gainId() == 0 ) ++cnt;
                 }
                 if ( cnt == 5 ) return i-1; // the last unsaturated sample
         }
