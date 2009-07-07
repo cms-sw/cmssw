74ccd34efd6808d83e14298195ffcefbccf7d046
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2009-07-07 14:14:50 +0000

    Open file dialog if no data file given.

--- Fireworks/Core/src/CmsShowMain.cc
+++ Fireworks/Core/src/CmsShowMain.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:
 //         Created:  Mon Dec  3 08:38:38 PST 2007
-// $Id: CmsShowMain.cc,v 1.78 2009/05/17 12:29:29 amraktad Exp $
+// $Id: CmsShowMain.cc,v 1.79 2009/05/27 14:08:53 chrjones Exp $
 //
 
 // system include files
@@ -196,20 +196,22 @@ CmsShowMain::CmsShowMain(int argc, char *argv[]) :
          exit(0);
       }
 
-      if (vm.count(kInputFileOpt)) {
-         m_inputFileName = vm[kInputFileOpt].as<std::string>();
-      } else {
-#if !defined(__APPLE__)
-         printf("No data file name.\n");
-         std::cout << desc <<std::endl;
-         exit(0);
-#endif
-      }
       const char* cmspath = gSystem->Getenv("CMSSW_BASE");
       if(0 == cmspath) {
          throw std::runtime_error("CMSSW_BASE environment variable not set");
       }
 
+      // input file
+      if (vm.count(kInputFileOpt)) {
+         m_inputFileName = vm[kInputFileOpt].as<std::string>();
+      }      
+
+      if (!m_inputFileName.size())
+         std::cout << "No data file given." << std::endl;
+      else
+         std::cout << "Input: " << m_inputFileName.c_str() << std::endl;
+
+      // configuration file
       if (vm.count(kConfigFileOpt)) {
          m_configFileName = vm[kConfigFileOpt].as<std::string>();
       } else {
@@ -220,7 +222,9 @@ CmsShowMain::CmsShowMain(int argc, char *argv[]) :
             m_configFileName = "src/Fireworks/Core/macros/default.fwc";
          }
       }
+      std::cout << "Config: "  <<  m_configFileName.c_str() << std::endl;
 
+      // geometry
       if (vm.count(kGeomFileOpt)) {
          m_geomFileName = vm[kGeomFileOpt].as<std::string>();
       } else {
@@ -228,6 +232,8 @@ CmsShowMain::CmsShowMain(int argc, char *argv[]) :
          // m_geomFileName =cmspath;
          m_geomFileName.append("cmsGeom10.root");
       }
+      std::cout << "Geom: " <<  m_geomFileName.c_str() << std::endl;
+
       bool eveMode = vm.count(kEveOpt);
 
       //Delay creating guiManager until here so that if we have a 'help' request we don't
@@ -243,9 +249,6 @@ CmsShowMain::CmsShowMain(int argc, char *argv[]) :
          TEveLine::SetDefaultSmooth(kTRUE);
       }
 
-      printf("Input: %s\n", m_inputFileName.c_str());
-      printf("Config: %s\n", m_configFileName.c_str());
-      printf("Geom: %s\n", m_geomFileName.c_str());
       //connect up the managers
       m_eiManager->newItem_.connect(boost::bind(&FWModelChangeManager::newItemSlot,
                                                 m_changeManager.get(), _1) );
@@ -262,9 +265,9 @@ CmsShowMain::CmsShowMain(int argc, char *argv[]) :
       macPath += "/src/Fireworks/Core/macros";
       const char* base = gSystem->Getenv("CMSSW_RELEASE_BASE");
       if(0!=base) {
-	macPath+=":";
-	macPath +=base;
-	macPath +="/src/Fireworks/Core/macros";
+         macPath+=":";
+         macPath +=base;
+         macPath +="/src/Fireworks/Core/macros";
       }
       gROOT->SetMacroPath((std::string("./:")+macPath).c_str());
 
@@ -399,8 +402,7 @@ void CmsShowMain::draw(const fwlite::Event& event)
 
 void CmsShowMain::openData()
 {
-   const char* kRootType[] = {"ROOT files","*.root",
-                              0,0};
+   const char* kRootType[] = {"ROOT files","*.root", 0, 0};
    TGFileInfo fi;
    fi.fFileTypes = kRootType;
    /* this is how things used to be done:
@@ -713,6 +715,9 @@ CmsShowMain::setupDataHandling()
    if(m_inputFileName.size()) {
       m_guiManager->updateStatus("loading data file...");
       m_navigator->loadFile(m_inputFileName);
+   }
+   else {
+      openData();
    }
 }
 
