acd6c6e44d3a65594ae39d301d24e7805313b722
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-07-07 15:54:02 +0000

    Fixed typo

--- DQM/Physics/python/qcdHighPtDQM_cfi.py
+++ DQM/Physics/python/qcdHighPtDQM_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process.qcdHighPtDQM = cms.EDAnalyzer("QcdHighPtDQM",
+qcdHighPtDQM = cms.EDAnalyzer("QcdHighPtDQM",
                                      jetTag = cms.untracked.InputTag("sisCone5CaloJets"),
                                      metTag = cms.untracked.InputTag("met")
 )
