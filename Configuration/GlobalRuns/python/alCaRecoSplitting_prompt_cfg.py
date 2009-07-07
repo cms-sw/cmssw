130890887eac05262d25f4ca85bbf56da8c9709b
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2009-07-07 13:53:04 +0000

    as alCaRecoSplitting_express_cfg.py but SelectEvents uses paths from process RECO

---
+++ Configuration/GlobalRuns/python/alCaRecoSplitting_prompt_cfg.py
@@ -0,0 +1,161 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.123 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: alCaRecoSplitting -s ALCA:MuAlCalIsolatedMu+RpcCalHLT+TkAlCosmics0T+MuAlStandAloneCosmics+MuAlGlobalCosmics+HcalCalHOCosmics+DQM --datatier RECO --eventcontent RECO --conditions FrontierConditions_GlobalTag,GR09_31X_V2P::All -n -1 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ALCA')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    annotation = cms.untracked.string('step3_RELVAL nevts:-1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound'),
+    wantSummary = cms.untracked.bool(True)
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:ALCACombined.root')
+)
+
+# Additional output definition
+process.ALCARECOStreamMuAlStandAloneCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlStandAloneCosmics:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlStandAloneCosmics_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*'),
+    fileName = cms.untracked.string('MuAlStandAloneCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlStandAloneCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamHcalCalHOCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOHcalCalHOCosmics:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep HOCalibVariabless_*_*_*'),
+    fileName = cms.untracked.string('HcalCalHOCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('HcalCalHOCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamTkAlCosmics0T = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOTkAlCosmicsCTF0T:RECO', 
+            'pathALCARECOTkAlCosmicsCosmicTF0T:RECO', 
+            'pathALCARECOTkAlCosmicsRS0T:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOTkAlCosmics*0T_*_*', 
+        'keep *_eventAuxiliaryHistoryProducer_*_*', 
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*', 
+        'keep L1MuGMTReadoutCollection_gtDigis_*_*', 
+        'keep Si*Cluster*_si*Clusters_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('TkAlCosmics0T.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('TkAlCosmics0T'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamRpcCalHLT = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECORpcCalHLT:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_muonDTDigis_*_*', 
+        'keep CSCDetIdCSCWireDigiMuonDigiCollection_*_*_*', 
+        'keep CSCDetIdCSCStripDigiMuonDigiCollection_*_*_*', 
+        'keep DTLayerIdDTDigiMuonDigiCollection_*_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*', 
+        'keep RPCDetIdRPCDigiMuonDigiCollection_*_*_*', 
+        'keep L1MuRegionalCands_*_RPCb_*', 
+        'keep L1MuRegionalCands_*_RPCf_*', 
+        'keep L1MuGMTCands_*_*_*', 
+        'keep L1MuGMTReadoutCollection_*_*_*'),
+    fileName = cms.untracked.string('RpcCalHLT.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('RpcCalHLT'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamMuAlGlobalCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlGlobalCosmics:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlGlobalCosmics_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*'),
+    fileName = cms.untracked.string('MuAlGlobalCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlGlobalCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamMuAlCalIsolatedMu = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlCalIsolatedMu:RECO')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlCalIsolatedMu_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*'),
+    fileName = cms.untracked.string('MuAlCalIsolatedMu.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlCalIsolatedMu'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+
+# Path and EndPath definitions
+process.endjob_step = cms.Path(process.endOfProcess)
+process.ALCARECOStreamMuAlStandAloneCosmicsOutPath = cms.EndPath(process.ALCARECOStreamMuAlStandAloneCosmics)
+process.ALCARECOStreamHcalCalHOCosmicsOutPath = cms.EndPath(process.ALCARECOStreamHcalCalHOCosmics)
+process.ALCARECOStreamTkAlCosmics0TOutPath = cms.EndPath(process.ALCARECOStreamTkAlCosmics0T)
+process.ALCARECOStreamRpcCalHLTOutPath = cms.EndPath(process.ALCARECOStreamRpcCalHLT)
+process.ALCARECOStreamMuAlGlobalCosmicsOutPath = cms.EndPath(process.ALCARECOStreamMuAlGlobalCosmics)
+process.ALCARECOStreamMuAlCalIsolatedMuOutPath = cms.EndPath(process.ALCARECOStreamMuAlCalIsolatedMu)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.endjob_step,process.ALCARECOStreamMuAlStandAloneCosmicsOutPath,process.ALCARECOStreamHcalCalHOCosmicsOutPath,process.ALCARECOStreamTkAlCosmics0TOutPath,process.ALCARECOStreamRpcCalHLTOutPath,process.ALCARECOStreamMuAlGlobalCosmicsOutPath,process.ALCARECOStreamMuAlCalIsolatedMuOutPath)
