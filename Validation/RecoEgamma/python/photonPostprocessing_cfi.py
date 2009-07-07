ff9293a6d71bd0b000265523cc49d8587d4d7871
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2009-07-07 16:54:37 +0000

    First commit: here efficiency from statistics accumulated on multiple files or runs can be calculated. Same structure as for DQMOffline.The postprocessing module is configured after the photon validator as fas as histograms goes so that default binnings and ranges are guarantted to be the same

---
+++ Validation/RecoEgamma/python/photonPostprocessing_cfi.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.RecoEgamma.photonValidator_cfi import *
+
+
+photonPostprocessing = cms.EDAnalyzer("PhotonPostprocessing",
+
+    Name = cms.untracked.string('photonPostprocessing'),
+
+    standAlone = cms.bool(False),
+    batch = cms.bool(False),                                     
+
+
+    etBin = photonValidation.etBin,
+    etMin = photonValidation.etMin,
+    etMax = photonValidation.etMax,
+                             
+    etaBin = photonValidation.etaBin,
+    etaBin2 = photonValidation.etaBin2,
+    etaMin = photonValidation.etaMin,
+    etaMax = photonValidation.etaMax,
+
+    phiBin = photonValidation.phiBin,
+    phiMin = photonValidation.phiMin,
+    phiMax = photonValidation.phiMax,
+
+    rBin = photonValidation.rBin,
+    rMin = photonValidation.rMin,
+    rMax = photonValidation.rMax,
+
+    zBin = photonValidation.zBin,
+    zMin = photonValidation.zMin,
+    zMax = photonValidation.zMax,
+
+                                      
+                                     
+    InputFileName = cms.untracked.string("DQMOfflinePhotonsBatch.root"),
+                                     
+    OutputFileName = cms.string('DQMOfflinePhotonsStandAlone.root'),
+)
