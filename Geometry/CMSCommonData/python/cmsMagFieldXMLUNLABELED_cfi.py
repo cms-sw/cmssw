057937144823b6a0fcc7e40b1d493a17d4e04533
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-07-07 14:35:17 +0000

    obsolete file

--- Geometry/CMSCommonData/python/cmsMagFieldXMLUNLABELED_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
-    geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/normal/cmsextent.xml', 'Geometry/CMSCommonData/data/cms.xml', 'Geometry/CMSCommonData/data/cmsMagneticField.xml', 'Geometry/CMSCommonData/data/MagneticFieldVolumes.xml'),
-    rootNodeName = cms.string('MagneticFieldVolumes:MAGF')
-)
-
-
--- Geometry/CMSCommonData/python/cmsMagneticFieldXML_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-#Temporary workaround, should be removed!
-from MagneticField.Engine.volumeBasedMagneticField_85l_cfi import *
-
