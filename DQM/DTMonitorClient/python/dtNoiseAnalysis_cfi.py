5cdac0ba1ad110b498305e7173c0c79e8e5d378d
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-07-08 16:29:34 +0000

    threshold for noisy at 500 Hz

--- DQM/DTMonitorClient/python/dtNoiseAnalysis_cfi.py
+++ DQM/DTMonitorClient/python/dtNoiseAnalysis_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 dtNoiseAnalysisMonitor = cms.EDAnalyzer("DTNoiseAnalysisTest",
-                                        noisyCellDef = cms.untracked.int32(200),
+                                        noisyCellDef = cms.untracked.int32(500),
                                         doSynchNoise = cms.untracked.bool(False),
                                         maxSynchNoiseRate = cms.untracked.double(0.001)
                                         )
