3e1ae4c84c493ab871f77361b60b54c2d14e966c
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2009-07-08 17:18:03 +0000

    First commit: sequence to be eventually included in the central Validation sequence

---
+++ Validation/RecoEgamma/python/photonValidationSequence_cff.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.RecoEgamma.tpSelection_cfi import *
+from Validation.RecoEgamma.photonValidator_cfi import *
+from SimTracker.TrackAssociation.TrackAssociatorByHits_cfi import *
+import SimTracker.TrackAssociation.TrackAssociatorByHits_cfi
+TrackAssociatorByHits.Cut_RecoToSim = 0.5
+TrackAssociatorByHits.Quality_SimToReco = 0.5
+
+
+
+photonValidation.minPhoEtCut = 20
+photonValidation.eMax  = 500
+photonValidation.etMax = 500
+photonValidation.signal = True
+## same for all
+photonValidation.convTrackMinPtCut = 1.
+photonValidation.useTP = True
+photonValidation.rBin = 48
+photonValidation.eoverpMin = 0.
+photonValidation.eoverpMax = 5.
+#
+photonValidation.OutputMEsInRootFile = True
+photonValidation.OutputFileName = 'photonValidation.root'
+photonValidationSequence = cms.Sequence(tpSelection*photonValidation)
+
