cde0008a9b084d82308b122e6827475b213223f8
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-07-08 10:52:09 +0000

    improved timestamp handling

--- CondTools/Ecal/src/EcalLaserHandler.cc
+++ CondTools/Ecal/src/EcalLaserHandler.cc
@@ -5,6 +5,7 @@
 #include "OnlineDB/EcalCondDB/interface/Tm.h"
 
 
+
 #include<iostream>
 
 
@@ -86,6 +87,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 	  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp; 
 	  apdpnpair_temp.p1 = apdpnpair.p1;
 	  apdpnpair_temp.p2 = apdpnpair.p2;
+	  apdpnpair_temp.p3 = apdpnpair.p3;
 	  apdpns_temp->setValue(ebdetid, apdpnpair_temp);
 	  if (hiee%1000 == 0 )std::cout <<"hiee = "<< hiee << "    p1 = " << apdpnpair.p1  <<"    p2 = " << apdpnpair.p2  <<endl;
 	}
@@ -93,6 +95,7 @@ void popcon::EcalLaserHandler::getNewObjects()
   }
   
   std::cout << "going to access Endcap objects in the last payload "  << endl;
+
 
   // loop through ecal endcap      
   for(int iX=EEDetId::IX_MIN; iX<=EEDetId::IX_MAX ;++iX) {
@@ -107,6 +110,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 	    EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp; 
 	    apdpnpair_temp.p1 = apdpnpair.p1;
 	    apdpnpair_temp.p2 = apdpnpair.p2;
+	    apdpnpair_temp.p3 = apdpnpair.p3;
 	    apdpns_temp->setValue(eedetidpos, apdpnpair_temp);
 	    
 	  } else {
@@ -121,6 +125,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 	    EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp; 
 	    apdpnpair_temp.p1 = apdpnpair.p1;
 	    apdpnpair_temp.p2 = apdpnpair.p2;
+	    apdpnpair_temp.p3 = apdpnpair.p3;
 	    apdpns_temp->setValue(eedetidneg, apdpnpair_temp);
 	    
 	  } else {
@@ -136,6 +141,8 @@ void popcon::EcalLaserHandler::getNewObjects()
   edm::Timestamp t_min= edm::Timestamp(start_time_old);
 
 
+
+
   //loop through light modules
 
   for (int i=0; i<92; i++){
@@ -144,10 +151,11 @@ void popcon::EcalLaserHandler::getNewObjects()
     EcalLaserAPDPNRatios::EcalLaserTimeStamp timestamp_temp;
     timestamp_temp.t1 = timestamp.t1;
     timestamp_temp.t2 = timestamp.t2;
+    timestamp_temp.t3 = timestamp.t3;
     
     apdpns_temp->setTime(i,timestamp_temp);
 
-    if(t_min<timestamp.t2) t_min=timestamp.t2;
+    if(t_min<timestamp.t3   ) t_min=timestamp.t3;
   }
   
   cout <<"WOW: we just retrieved the last valid record from DB "<< endl;
@@ -193,10 +201,11 @@ void popcon::EcalLaserHandler::getNewObjects()
   lmf_list.setRunTag(my_runtag);
   //    mon_list=econn->fetchMonRunList(my_runtag, mon_tag);
 
-  uint64_t t_min_val= (uint64_t)t_min.value();
+  //  uint64_t t_min_val= (uint64_t)t_min.value();
+  uint64_t t_min_val= (t_min.value()>>32 )*1000000;
+  
 
-
-  lmf_list=econn->fetchLMFRunList(my_runtag, lmf_tag,t_min_val, m_lastRun );
+  lmf_list=econn->fetchLMFRunList(my_runtag, lmf_tag,t_min_val,m_firstRun,  m_lastRun );
   
   std::vector<LMFRunIOV> lmf_run_vec=  lmf_list.getRuns();
   int lmf_runs=lmf_run_vec.size();
@@ -205,6 +214,7 @@ void popcon::EcalLaserHandler::getNewObjects()
   if(lmf_runs>0){
     
     Time_t snc=lmf_run_vec[0].getSubRunStart().microsTime();
+    Time_t snc_old=lmf_run_vec[0].getSubRunStart().microsTime();
     
     std::vector<int> nsubruns;
     nsubruns.reserve(lmf_runs);
@@ -249,46 +259,81 @@ void popcon::EcalLaserHandler::getNewObjects()
 
     */
 
-
-    std::cout << "done with Ecal logic id and lmr"<< endl;
-
     for(int kt=0; kt<lmf_runs; kt++){
       last_lmr[kt]=0;
-      nsubruns[kt]=lmf_run_vec[kt].getSubRunNumber();
-      std::cout<< "nsubr="<<nsubruns[kt]<<endl;
-      
-      int i=lmf_run_vec[kt].getSequenceNumber();
-      int i_lmr=lmf_run_vec[kt].getLMRNumber();
-      
-      if(last_lmr[i-1]<i_lmr) last_lmr[i-1]=i_lmr;
     }
 
-    std::vector<int> updated_channels;
-    updated_channels.reserve(75848);
 
     int old_seq=0;
     unsigned long long irun_old=0;
+    int last_lmr_x=0;
+    int run_saved=0;
+
+
+    for(int kt=0; kt<lmf_runs; kt++){
+      nsubruns[kt]=lmf_run_vec[kt].getSubRunNumber();
+      //  std::cout<< "nsubr="<<nsubruns[kt]<<endl;
+      int iseq=lmf_run_vec[kt].getSequenceNumber();
+      int i_lmr=lmf_run_vec[kt].getLMRNumber();
+      unsigned long long irun=(unsigned long long) lmf_run_vec[kt].getRunIOV().getRunNumber();
+
+      if(old_seq!=iseq || irun!=irun_old) {
+	// we enter here at the first sub run of a sequence
+	// we can do some initialization
+	old_seq=iseq;
+	irun_old=irun;
+	if(kt !=0) {
+	  last_lmr[kt-1]=last_lmr_x;
+	}
+	last_lmr_x=0;
+      }
+      if(last_lmr_x<= i_lmr) last_lmr_x=i_lmr;
+    }
+
+    last_lmr[lmf_runs-1]=last_lmr_x;
+
+
+
+    std::vector<int> updated_channels;
+    updated_channels.reserve(75848);
+    std::vector<int> updated_lmr;
+    updated_lmr.reserve(92);
+
+    old_seq=0;
+    irun_old=0;
     for(int kr=0; kr<lmf_runs; kr++){
       
       int i=lmf_run_vec[kr].getSequenceNumber();
+      int iseq=lmf_run_vec[kr].getSequenceNumber();
       int i_lmr=lmf_run_vec[kr].getLMRNumber();
       Tm time_lmf_subrun=lmf_run_vec[kr].getSubRunStart();
       unsigned long long time_lmf_subrun_micro=time_lmf_subrun.microsTime();
       
+
+      time_lmf_subrun_micro=time_lmf_subrun_micro/1000000;
+      time_lmf_subrun_micro=time_lmf_subrun_micro << 32;
+
+
+
       unsigned long long irun=(unsigned long long) lmf_run_vec[kr].getRunIOV().getRunNumber();
-      cout << "here is the run number: "<< lmf_run_vec[kr].getRunIOV().getRunNumber() <<"."<<i<<"/"<<nsubruns[kr]<< endl;
       
-      if(old_seq!=i || irun!=irun_old) {
+      if(old_seq!=iseq || irun!=irun_old) {
 	// we enter here at the first sub run of a sequence 
 	// we can do some initialization
-	old_seq=i;
+	old_seq=iseq;
 	irun_old=irun;
 	// setting right since for the first subrun 
+
+	snc_old = snc;
+
 	snc = time_lmf_subrun_micro;
+
 	for(int id=0; id<75848; id++){
 	  updated_channels[id] =0;
 	}
-
+	for(int id=0; id<92; id++){
+	  updated_lmr[id] =0;
+	}
       }
       
 	    
@@ -296,7 +341,7 @@ void popcon::EcalLaserHandler::getNewObjects()
       
       map<EcalLogicID, LMFLaserPrimDat > dataset_lmf;
       econn->fetchDataSet(&dataset_lmf, &lmf_run_vec[kr]);
-      cout <<"OMDS record for run "<<irun  <<" is made of "<< dataset_lmf.size() << " records"<< endl;
+      // cout <<"OMDS record for run "<<irun  <<" is made of "<< dataset_lmf.size() << " records"<< endl;
       
       
       typedef map<EcalLogicID, LMFLaserPrimDat>::const_iterator CIlmf;
@@ -322,20 +367,22 @@ void popcon::EcalLaserHandler::getNewObjects()
 	  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp;
 	  // p1 in a new object should be equal to the p2 in the previos object
 	  float x=rd_apdnorm.getAPDOverPNMean();
-	  float old_x=apdpnpair.p2;
+	  float old_x=apdpnpair.p3;
 	  if(checkAPDPN(x,old_x)){
 	    apdpnpair_temp.p1 = apdpnpair.p2;
-	    apdpnpair_temp.p2 = x;
+	    apdpnpair_temp.p2 = apdpnpair.p3;
+	    apdpnpair_temp.p3 = x;
 	  // here we change in the apdpns_temp object only the values that are available in the online DB 
 	  // otherwise we keep the old value 
 	    apdpns_temp->setValue(ebdetid, apdpnpair_temp);
 	    updated_channels[hiee]=1;
 	    ich++;
-	    if (ich<10) std::cout<< "updating channel "<< x<<endl;
+	    //    if (ich<2) std::cout<< "updating channel "<< x<<endl;
 	  } else {
 	    // FC here we must decide what to do.
 	    apdpnpair_temp.p1 = apdpnpair.p2;
-	    apdpnpair_temp.p2 = apdpnpair.p2;
+	    apdpnpair_temp.p2 = apdpnpair.p3;
+	    apdpnpair_temp.p3 = apdpnpair.p3;
             apdpns_temp->setValue(ebdetid, apdpnpair_temp);
 	    updated_channels[hiee]=2; // 2 means channel was bad and we propagate the old value 
 	    //std::cout<< "NOT updating channel "<< x<<endl;
@@ -352,7 +399,8 @@ void popcon::EcalLaserHandler::getNewObjects()
 	  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp;
 	  // p1 in a new object should be equal to the p2 in the previos object
 	  apdpnpair_temp.p1 = apdpnpair.p2;
-	  apdpnpair_temp.p2 = rd_apdnorm.getAPDOverPNMean();
+	  apdpnpair_temp.p2 = apdpnpair.p3;
+	  apdpnpair_temp.p3 = rd_apdnorm.getAPDOverPNMean();
 	  // here we change in the apdpns_temp object only the values that are available in the online DB 
 	  // otherwise we keep the old value 
 	  apdpns_temp->setValue(eedetid, apdpnpair_temp);
@@ -362,14 +410,15 @@ void popcon::EcalLaserHandler::getNewObjects()
 	}
       }    
 
-      std::cout << " setting timestamp for LMR "<< i_lmr << " to "<<time_lmf_subrun_micro << endl; 
+      if(i_lmr==1 || i_lmr == last_lmr[kr] ) std::cout << "timestamp for run"<<lmf_run_vec[kr].getRunIOV().getRunNumber() <<"."<<iseq<<"/"<<last_lmr[kr] <<" LMR "<< i_lmr << " to "<<time_lmf_subrun_micro << endl; 
       const EcalLaserAPDPNRatios::EcalLaserTimeStampMap& laserTimeMap = apdpns_temp->getTimeMap();
-      timestamp = laserTimeMap[i_lmr-1];
+      EcalLaserAPDPNRatios::EcalLaserTimeStamp timestamp = laserTimeMap[i_lmr-1];
       EcalLaserAPDPNRatios::EcalLaserTimeStamp timestamp_temp;
       timestamp_temp.t1 = timestamp.t2;
-      timestamp_temp.t2 = edm::Timestamp(time_lmf_subrun_micro);
+      timestamp_temp.t2 = timestamp.t3;
+      timestamp_temp.t3 = edm::Timestamp(time_lmf_subrun_micro);
       apdpns_temp->setTime( (i_lmr-1) , timestamp_temp);
-
+      updated_lmr[i_lmr-1]=1;
       // missing a part that checks for each LMR the 
       // corresponding crystals and eventually 
       // extends the values to the new interval
@@ -378,11 +427,11 @@ void popcon::EcalLaserHandler::getNewObjects()
       
     
 
-      if(i_lmr==last_lmr[i-1]){
+      if(i_lmr==last_lmr[kr]){
 
 	// this is the last subrun of a run we save to offline DB 
 	
-	cout << "Generating popcon record for run " << irun << "..." << flush;
+	cout << "Generating popcon record for run " << irun << "..." << endl;
 	
 	
 	// now I copy apdpns_temp in apdpns_popcon and I ship apdpns_popcon to popcon
@@ -414,6 +463,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 		  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp;
 		  apdpnpair_temp.p1 = apdpnpair.p1;
 		  apdpnpair_temp.p2 = apdpnpair.p2;
+		  apdpnpair_temp.p3 = apdpnpair.p3;
 		  
 		  apdpns_popcon->setValue(ebdetid, apdpnpair_temp);
 		} else {
@@ -436,6 +486,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 		  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp; 
 		  apdpnpair_temp.p1 = apdpnpair.p1;
 		  apdpnpair_temp.p2 = apdpnpair.p2;
+		  apdpnpair_temp.p3 = apdpnpair.p3;
 		  apdpns_popcon->setValue(eedetidpos, apdpnpair_temp);
 		  
 		} else {
@@ -450,6 +501,7 @@ void popcon::EcalLaserHandler::getNewObjects()
 		  EcalLaserAPDPNRatios::EcalLaserAPDPNpair apdpnpair_temp; 
 		  apdpnpair_temp.p1 = apdpnpair.p1;
 		  apdpnpair_temp.p2 = apdpnpair.p2;
+		  apdpnpair_temp.p3 = apdpnpair.p3;
 		  apdpns_popcon->setValue(eedetidneg, apdpnpair_temp);
 		  
 		} else {
@@ -461,20 +513,59 @@ void popcon::EcalLaserHandler::getNewObjects()
 	  }
 	}
 	
+
+
+
+	unsigned long long start_time_late=0ULL;
+	Time_t t_late= start_time_late;
+	unsigned long long start_time_early=18446744073709551615ULL;
+	Time_t t_early= start_time_early;
+	
 	
 	for (int j=0; j<92; j++){
 	  const EcalLaserAPDPNRatios::EcalLaserTimeStampMap& laserTimeMap = apdpns_temp->getTimeMap();
-	  timestamp = laserTimeMap[j];
+	  EcalLaserAPDPNRatios::EcalLaserTimeStamp timestampx = laserTimeMap[j];
 	  EcalLaserAPDPNRatios::EcalLaserTimeStamp timestamp_temp;
-	  timestamp_temp.t1 = timestamp.t1;
-	  timestamp_temp.t2 = timestamp.t2;
+	  timestamp_temp.t1 = timestampx.t1;
+	  timestamp_temp.t2 = timestampx.t2;
+	  timestamp_temp.t3 = timestampx.t3;
 	  apdpns_popcon->setTime(j,timestamp_temp);
+
+	  if(timestamp_temp.t2.value()>t_late    ) t_late=timestamp_temp.t2.value();
+    
 	}
+
+
+	uint64_t t_late_val= (t_late >>32 ); // timestamp in seconds
+
+	for (int j=0; j<92; j++){
+	  const EcalLaserAPDPNRatios::EcalLaserTimeStampMap& laserTimeMap = apdpns_popcon->getTimeMap();
+	  EcalLaserAPDPNRatios::EcalLaserTimeStamp timestamp_temp = laserTimeMap[j];
+
+	  uint64_t t_val= (timestamp_temp.t2.value()>>32 ); // timestamp in seconds
+	  
+	  if(timestamp_temp.t2.value()<t_early && updated_lmr[j]==1 && (t_late_val-t_val)<1400 ) 
+				  t_early=timestamp_temp.t2.value();
+    
+	}
+
 	
-	//	snc=i;
-	m_to_transfer.push_back(std::make_pair(apdpns_popcon,snc));
-	
-	std::cout <<" snc : "<< snc << endl;
+
+	std::cout <<" t_early : "<< t_early << endl;
+	std::cout <<" snc_old : "<< snc_old << endl;
+	std::cout <<" t_late= : "<< t_late << endl;
+
+
+	if(t_early<=1) t_early=2;
+
+	if(run_saved!=0) {
+	  t_early=snc_old; 
+	}
+
+	std::cout <<" chosen : "<< t_early << endl;
+
+	m_to_transfer.push_back(std::make_pair(apdpns_popcon,t_early));
+	run_saved++; 
 	
       }
       
