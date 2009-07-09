e2b7a99538955f547824345b994bcde24898a3e1
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-07-09 14:05:40 +0000

    Used Global conditions tag update

--- FastSimulation/Configuration/test/IntegrationTest_cfg.py
+++ FastSimulation/Configuration/test/IntegrationTest_cfg.py
@@ -29,8 +29,8 @@ process.famosSimHits.SimulateCalorimetry = True
 process.famosSimHits.SimulateTracking = True
 
 # Get frontier conditions    - not applied in the HCAL, see below
-# Values for globaltag are "STARTUP_31X::All","IDEAL_31X::All"
-process.GlobalTag.globaltag = "STARTUP_31X::All"
+# Values for globaltag are "STARTUP31X_V1::All","MC_31X_V2::All"
+process.GlobalTag.globaltag = "STARTUP31X_V1::All"
 
 # Apply ECAL miscalibration
 process.ecalRecHit.doMiscalib = True
