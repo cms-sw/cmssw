715a2a9ff4225a64affc48227416b0537caf19d2
Yen-Jie Lee <sha1-f0ec4886d17e8de0a4ad98a0607824753c761f00@cern.ch>
2009-07-09 16:05:07 +0000

    Reordered the photon producer sequence and jet producer sequence

--- RecoHI/Configuration/python/Reconstruction_HI_cff.py
+++ RecoHI/Configuration/python/Reconstruction_HI_cff.py
@@ -43,7 +43,7 @@ localReco = cms.Sequence(offlineBeamSpot*trackerlocalreco*caloReco)
 #--------------------------------------------------------------------------
 # Main Sequence
 reconstruct_PbPb_CaloOnly = cms.Sequence(caloReco*hiEcalClusters*runjets*hiCentrality*hiEvtPlane)
-reconstruct_PbPb = cms.Sequence(localReco*heavyIonTracking*hiEcalClusters*runjets*hiCentrality*hiEvtPlane)
+reconstruct_PbPb = cms.Sequence(localReco*heavyIonTracking*hiEcalClusters*runjets*hiEgammaSequence*hiCentrality*hiEvtPlane)
 
 #--------------------------------------------------------------------------
 
