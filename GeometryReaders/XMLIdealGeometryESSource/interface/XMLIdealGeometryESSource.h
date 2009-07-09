5b556ee074fd05ddcb387e418efbea8dd71abad7
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-07-09 23:44:48 +0000

    at least temporarily, swap the registry of objects from Singletons of DetecorDescription Store type to local data members so the "stomping on memory" does not occur.

--- GeometryReaders/XMLIdealGeometryESSource/interface/XMLIdealGeometryESSource.h
+++ GeometryReaders/XMLIdealGeometryESSource/interface/XMLIdealGeometryESSource.h
@@ -13,6 +13,13 @@
 #include <memory>
 #include <string>
 
+namespace DDI {
+  class Material;
+  class Solid;
+  class LogicalPart;
+  class Specific;
+}
+
 class XMLIdealGeometryESSource : public edm::ESProducer, 
                                  public edm::EventSetupRecordIntervalFinder
 {
@@ -31,6 +38,14 @@ private:
     std::string rootNodeName_;
     bool userNS_,cpvavailable_;
     GeometryConfiguration geoConfig_;
+
+    // 2009-07-09 memory patch
+    // for copying and protecting DD Store's after parsing is complete.
+    DDI::Store<DDName, DDI::Material*>::registry_type matStore_;
+    DDI::Store<DDName, DDI::Solid*>::registry_type solidStore_;
+    DDI::Store<DDName, DDI::LogicalPart*>::registry_type lpStore_;
+    DDI::Store<DDName, DDI::Specific*>::registry_type specStore_;
+    DDI::Store<DDName, DDRotationMatrix*>::registry_type rotStore_;    
 };
 
 
--- GeometryReaders/XMLIdealGeometryESSource/src/XMLIdealGeometryESSource.cc
+++ GeometryReaders/XMLIdealGeometryESSource/src/XMLIdealGeometryESSource.cc
@@ -6,6 +6,11 @@
 #include "DetectorDescription/Core/interface/DDCompactView.h"
 #include "DetectorDescription/Core/interface/DDRoot.h"
 
+#include "DetectorDescription/Core/interface/DDMaterial.h"
+#include "DetectorDescription/Core/interface/DDSolid.h"
+#include "DetectorDescription/Core/interface/DDSpecifics.h"
+#include "DetectorDescription/Base/interface/DDRotationMatrix.h"
+
 #include "FWCore/ParameterSet/interface/FileInPath.h"
 
 #include <memory>
@@ -23,9 +28,11 @@ XMLIdealGeometryESSource::XMLIdealGeometryESSource(const edm::ParameterSet & p):
     setWhatProduced(this, &XMLIdealGeometryESSource::produceMagField, 
                     edm::es::Label(p.getParameter<std::string>("@module_label")));
     findingRecord<IdealMagneticFieldRecord>();
+    std::cout <<"finding Mag field" << std::endl;
   } else {
     setWhatProduced(this, &XMLIdealGeometryESSource::produceGeom);
     findingRecord<IdealGeometryRecord>();
+    std::cout << "finding ideal geom" << std::endl;
   }
 }
 
@@ -55,12 +62,17 @@ XMLIdealGeometryESSource::produce() {
   cpvavailable_ = true;
 
   DDLParser * parser = DDLParser::instance();
+  // 2009-07-09 memory patch
+  parser->clearFiles();
 
   parser->getDDLSAX2FileHandler()->setUserNS(userNS_);
   DDRootDef::instance().set(DDName(rootNodeName_));
   
   int result2 = parser->parse(geoConfig_);
-  
+  const std::vector<std::string> & whatsparsed = geoConfig_.getFileList();
+  for (std::vector<std::string>::const_iterator it = whatsparsed.begin(); it != whatsparsed.end(); ++it ) {
+    std::cout << *it << std::endl;
+  }  
   if (result2 != 0) throw DDException("DDD-Parser: parsing failed!");
 
   if ( !bool(DDLogicalPart( DDName(rootNodeName_) )) ) {
@@ -76,6 +88,22 @@ XMLIdealGeometryESSource::produce() {
     throw cms::Exception("Geometry")<<"There is no valid node named \""
                                     <<rootNodeName_<<"\"";
   }
+
+  // at this point we should have a valid store of DDObjects and we will move these
+  // to the local storage area using swaps with the existing Singleton<Store...>'s
+  // NOTE TO SELF:  this is similar to the below explicit copy of the graph of the
+  // DDCompactView at this point with this XMLIdealGeometryESSource so as not to
+  // share the Store's anymore.
+  // 2009-07-09 memory patch
+  std::cout << "before swap size = " << DDMaterial::StoreT::instance().size() << std::endl;
+   DDMaterial::StoreT::instance().swap(matStore_);
+   std::cout << "after swap size = " << DDMaterial::StoreT::instance().size() << std::endl;
+   std::cout << "and matStore_.size() = " << matStore_.size() << std::endl; 
+   DDSolid::StoreT::instance().swap(solidStore_);
+   DDLogicalPart::StoreT::instance().swap(lpStore_);
+   DDSpecifics::StoreT::instance().swap(specStore_);
+   DDRotation::StoreT::instance().swap(rotStore_);
+
   std::auto_ptr<DDCompactView> returnValue(new DDCompactView(rootNode));
   
   // NOTE TO SELF:  Mike, DO NOT try to fix the memory leak here by going global again!!!
