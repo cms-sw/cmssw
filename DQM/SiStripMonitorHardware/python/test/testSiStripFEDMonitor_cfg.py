d8253af4b17dc9ce1b70fa8e9d3f10ebbdf11017
Anne-Marie Magnan <sha1-4ead6f5ca8b64f8c6a1efe9d1d4dd2e2421521d1@cern.ch>
2009-07-09 11:49:10 +0000

    updated configuration file

--- DQM/SiStripMonitorHardware/python/test/testSiStripFEDMonitor_cfg.py
+++ DQM/SiStripMonitorHardware/python/test/testSiStripFEDMonitor_cfg.py
@@ -4,28 +4,106 @@ process = cms.Process('TEST')
 
 process.source = cms.Source(
   "PoolSource",
-  #fileNames = cms.untracked.vstring("/store/data/Commissioning08/Cosmics/RAW/v1/000/066/722/005AC873-4C9D-DD11-968E-000423D98F98.root")
-  #fileNames = cms.untracked.vstring("/store/data/CRUZET3/Cosmics/RAW/v4/000/051/218/12E90673-E354-DD11-A5A5-001617C3B70E.root")
-  #fileNames = cms.untracked.vstring("/store/data/CRUZET3/Cosmics/RAW/v1/000/050/900/A4ED1C68-1C4D-DD11-A472-000423D986A8.root")
-)
-process.load("DQM.SiStripMonitorHardware.test.source_cff")
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+  fileNames = cms.untracked.vstring(
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run13.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run85269.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run96164.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/SiStripCommissioningSource_00096164_137.138.192.137_16715.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/Commissioning08/FEED31F3-58AC-DD11-BF73-000423D99658.root'
+        'file:/home/magnan/SOFTWARE/CMS/data/FED/Commissioning09/A6F7D0D3-4560-DE11-A52A-001D09F2545B.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run82983.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run77848.root'
+        #'file:/home/magnan/SOFTWARE/CMS/data/FED/edmOutput_run79635.root'
+        )
+  )
 
-process.load('DQM.SiStripCommon.MessageLogger_cfi')
+#process.load("DQM.SiStripMonitorHardware.test.source_cff")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+    )
+
+#process.service = cms.ProfilerService {
+#    untracked int32 firstEvent = 1
+#    untracked int32 lastEvent = 50
+#    untracked vstring paths = { "p"}
+#    }
+
+#process.load('DQM.SiStripCommon.MessageLogger_cfi')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    info = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO'),
+        #limit = cms.untracked.int32(100000),
+        noLineBreaks = cms.untracked.bool(False)
+        ),
+    suppressInfo = cms.untracked.vstring(),
+    # allows to suppress output from specific modules 
+    suppressDebug = cms.untracked.vstring(),
+    debug = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG'),
+        #limit = cms.untracked.int32(100000),
+        noLineBreaks = cms.untracked.bool(False)
+        ),
+    warning = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING'),
+        #limit = cms.untracked.int32(100000),
+        noLineBreaks = cms.untracked.bool(False)
+        ),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR'),
+        #limit = cms.untracked.int32(100000),
+        noLineBreaks = cms.untracked.bool(False)
+        ),
+    error = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR'),
+        #limit = cms.untracked.int32(100000),
+        noLineBreaks = cms.untracked.bool(False)
+        ),
+    suppressWarning = cms.untracked.vstring(),
+    #debugModules = cms.untracked.vstring('*'),#'siStripFEDMonitor'),
+    destinations = cms.untracked.vstring('cerr', 
+                                         'debug', 
+                                         'info', 
+                                         'warning', 
+                                         'error')
+
+    )
 
 process.DQMStore = cms.Service("DQMStore")
 
+#needed to produce tkHistoMap
+process.TkDetMap = cms.Service("TkDetMap")
+process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
+
+# Conditions (Global Tag is used here):
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "CRAFT_30X::All"
+process.GlobalTag.globaltag = "GR09_31X_V1P::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
 
 process.load('DQM.SiStripMonitorHardware.siStripFEDMonitor_cfi')
 #process.load('DQM.SiStripMonitorHardware.siStripFEDMonitor_Tier0_cff')
-#process.load('DQM.SiStripMonitorHardware.siStripFEDMonitor_P5_cff')
-process.siStripFEDMonitor.PrintDebugMessages = True
+process.siStripFEDMonitor.PrintDebugMessages = 2
+process.siStripFEDMonitor.nTotalBadChannelsvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nTotalBadActiveChannelsvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nFEDErrorsvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nFEDCorruptBuffersvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nFEDsWithFEProblemsvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nAPVStatusBitvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nAPVErrorvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nAPVAddressErrorvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nUnlockedvsTimeHistogramConfig.Max = 60;
+process.siStripFEDMonitor.nOutOfSyncvsTimeHistogramConfig.Max = 60;
 process.siStripFEDMonitor.WriteDQMStore = True
-
+process.siStripFEDMonitor.DQMStoreFileName = "DQMStore_comm09.root"
+process.siStripFEDMonitor.FillAllDetailedHistograms = False
 process.Tracer = cms.Service('Tracer',indentation = cms.untracked.string('$$'))
 
-process.p = cms.Path( process.siStripFEDMonitor )
+process.load('PerfTools.Callgrind.callgrindSwitch_cff')
+
+process.p = cms.Path( #process.profilerStart*
+                      process.siStripFEDMonitor
+                      #*process.profilerStop 
+                      )
