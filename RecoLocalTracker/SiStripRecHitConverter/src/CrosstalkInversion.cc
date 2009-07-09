c48805e32be995dfa0f5889beb806a7242342d95
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-07-09 14:39:29 +0000

    Pad Q with a 'suppressed' charge before the first and after the last strips

--- RecoLocalTracker/SiStripRecHitConverter/src/CrosstalkInversion.cc
+++ RecoLocalTracker/SiStripRecHitConverter/src/CrosstalkInversion.cc
@@ -4,30 +4,32 @@ namespace reco {
 
 std::vector<stats_t<float> > InverseCrosstalkMatrix::
 unfold(const std::vector<uint8_t>& q, const float x) {
+  const stats_t<float> suppressed(-5,25);
   const stats_t<float> saturated(254,400);
   #define STATS(value) ( (value<254) ? stats_t<float>(value) : saturated )
 
   const unsigned N=q.size();
-  std::vector<stats_t<float> > Q(N,stats_t<float>(0));
+  std::vector<stats_t<float> > Q(N+2,stats_t<float>(0));
+  Q[0] = Q[N+1] = suppressed;
 
   if(N==1)                                 //optimize N==1
-    Q[0] = STATS(q[0])/(1-2*x);  
+    Q[1] = STATS(q[0])/(1-2*x);  
   else if(N==2) {                          //optimize N==2
     const double A=1-2*x; 
     const stats_t<float> q0 = STATS(q[0]);
     const stats_t<float> q1 = STATS(q[1]);
-    Q[0] = ( A*q0 -x*q1 ) / (A*A-x*x);
-    Q[1] = ( A*q1 -x*q0 ) / (A*A-x*x);
+    Q[1] = ( A*q0 -x*q1 ) / (A*A-x*x);
+    Q[2] = ( A*q1 -x*q0 ) / (A*A-x*x);
   } 
   else {                                   //general case
     const InverseCrosstalkMatrix inverse(N,x);  
     for(unsigned i=0; i<(N+1)/2; i++) {
       for(unsigned j=i; j<N-i; j++) {
 	const float Cij = inverse(i+1,j+1);
-	Q[  i  ] += Cij * STATS(q[  j  ]) ;  if( i!=j)   
-	Q[  j  ] += Cij * STATS(q[  i  ]) ;  if( N!=i+j+1) {
-	Q[N-i-1] += Cij * STATS(q[N-j-1]) ;  if( i!=j)
-	Q[N-j-1] += Cij * STATS(q[N-i-1]) ;
+	Q[i+1] += Cij * STATS(q[  j  ]) ;  if( i!=j)   
+	Q[j+1] += Cij * STATS(q[  i  ]) ;  if( N!=i+j+1) {
+	Q[N-i] += Cij * STATS(q[N-j-1]) ;  if( i!=j)
+	Q[N-j] += Cij * STATS(q[N-i-1]) ;
 	}
       }
     }
