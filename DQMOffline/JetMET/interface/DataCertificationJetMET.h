50fdb2b02737fa8888465ca1a5e4559d029601cf
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2009-07-10 14:33:58 +0000

    header file for DataCertificationJetMET

---
+++ DQMOffline/JetMET/interface/DataCertificationJetMET.h
@@ -0,0 +1,56 @@
+#ifndef DATACERTIFICATIONJETMET_H
+#define DATACERTIFICATIONJETMET_H
+
+// author: Kenichi Hatakeyama (Rockefeller U.)
+
+// system include files
+#include <memory>
+#include <stdio.h>
+#include <math.h>
+#include <sstream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+//
+// class decleration
+//
+
+class DataCertificationJetMET : public edm::EDAnalyzer {
+   public:
+      explicit DataCertificationJetMET(const edm::ParameterSet&);
+      ~DataCertificationJetMET();
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      virtual void beginRun(const edm::Run&, const edm::EventSetup&) ;
+      virtual void endRun(const edm::Run&, const edm::EventSetup&) ;
+
+      virtual void beginLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&);
+      virtual void endLuminosityBlock(const edm::LuminosityBlock&, const edm::EventSetup&);
+
+   // ----------member data ---------------------------
+
+   edm::ParameterSet conf_;
+   DQMStore * dbe_;
+   edm::Service<TFileService> fs_;
+
+   int verbose_;
+   int testType_;
+
+};
+
+#endif
