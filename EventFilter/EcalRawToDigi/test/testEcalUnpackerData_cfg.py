d19cf1beeb34f331292dc5fd8de8eb75e4948724
nalmeida <sha1-503ed86a7512692e45da5b2d6fd04f9879577ff8@cern.ch>
2009-07-10 12:12:10 +0000

    File updated to work with 31X

--- EventFilter/EcalRawToDigi/test/testEcalUnpackerData_cfg.py
+++ EventFilter/EcalRawToDigi/test/testEcalUnpackerData_cfg.py
@@ -5,7 +5,11 @@ process = cms.Process("TESTECALUNPACKERDATA")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
- fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0/RelValQCD_Pt_3000_3500/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V1-v1/0001/1A1C4478-5866-DE11-A907-001D09F27067.root')
+ fileNames = 
+  cms.untracked.vstring('/store/relval/CMSSW_3_1_0/RelValQCD_Pt_3000_3500/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V1-v1/0001/1A1C4478-5866-DE11-A907-001D09F27067.root')
+#cms.untracked.vstring('file:/tmp/nalmeida/1A1C4478-5866-DE11-A907-001D09F27067.root')
+
+
 )
 
 process.load("Geometry.EcalMapping.EcalMapping_cfi")
@@ -15,11 +19,24 @@ process.load("Geometry.EcalMapping.EcalMappingRecord_cfi")
 process.load("EventFilter.EcalRawToDigi.EcalUnpackerMapping_cfi")
 process.load("EventFilter.EcalRawToDigi.EcalUnpackerData_cfi")
 
+process.load("CalibCalorimetry.EcalTrivialCondModules.EcalTrivialCondRetriever_cfi")
+
+
+# this is for MC
+process.ecalEBunpacker.InputLabel = cms.string('rawDataCollector')
+# this is for real data
+# process.ecalEBunpacker.InputLabel = cms.string('source')
+
+# print out debug info ( put silent mode = False)
+#process.ecalEBunpacker.silentMode =  True 
+process.ecalEBunpacker.silentMode = False 
 
 
 
 process.ecalDataSequence = cms.Sequence(process.ecalEBunpacker)
 
 process.p = cms.Path(process.ecalDataSequence)
+
+
 
 
