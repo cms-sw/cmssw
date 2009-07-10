a2f29daba85fe798710a82cbcb7b1c3990eb7653
Silvia Taroni <sha1-594a1eea0f66179b7306a89225ccd2076b3637c5@cern.ch>
2009-07-10 09:35:59 +0000

    configuration file to process RAW2DIGI, RECO

---
+++ GeneratorInterface/GenExtensions/test/Py6HadRECO-NoCuts_cfg.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('RECO')
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.99.2.3 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/BctoJPsiPi_cfi.py nevts:100'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:BctoJPsiPi_cfi_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root')
+)
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('BctoJPsiPi_RAW2DIGI_RECO.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string(''),
+        filterName = cms.untracked.string('')
+    )
+)
+process.output.outputCommands.append('keep *_source_*_*')
+process.output.outputCommands.append('keep *_generator_*_*')
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'MC_31X_V2::All'
+
+
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.output)
+
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.output)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.out_step)
+
