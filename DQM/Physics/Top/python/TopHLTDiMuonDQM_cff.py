42fe690ae2f43131eb7825512f9c9f6cd892ce2d
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-07-13 13:35:07 +0000

    removing dir Top as we dont want a dir substructure

--- DQM/Physics/Top/python/TopHLTDiMuonDQM_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-topHLTDiMuonDQM = cms.EDAnalyzer("TopHLTDiMuonDQM",
-    candCollection = cms.untracked.InputTag('hltL1extraParticles'),
-    monitorName    = cms.untracked.string('Top/HLTDiMuon')
-)
-
-topHLTDiMuonAnalyzer = cms.Sequence(topHLTDiMuonDQM)
