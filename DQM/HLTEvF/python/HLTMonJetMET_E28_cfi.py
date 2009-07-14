4f0e0e230e8abf055196e12b26d7e12a512d3ced
Shabnam Jabeen <sha1-b26fd77b6d710f07332af4ea583458a78d2bb0d6@cern.ch>
2009-07-14 14:00:40 +0000

    Configuration files tested online.

--- DQM/HLTEvF/python/HLTMonJetMET_E28_cfi.py
+++ DQM/HLTEvF/python/HLTMonJetMET_E28_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-#hltMonJetMET = cms.EDAnalyzer("HLTMon",
+#path names from from http://cms-project-confdb-hltdev.web.cern.ch/cms-project-confdb-hltdev/browser/
+#commisssioning09/MWGR/MWGR26/HLTallDQM
+
 hltMonJetMET = cms.EDAnalyzer("HLTMonJetMETDQMSource",
    outputFile = cms.untracked.string('./L1TDQM.root'),
    verbose = cms.untracked.bool(False),
@@ -8,196 +10,216 @@ hltMonJetMET = cms.EDAnalyzer("HLTMonJetMETDQMSource",
    reqNum = cms.uint32(1),
    DaqMonitorBEInterface = cms.untracked.bool(True),
    filters = cms.VPSet(      
-      cms.PSet(
+
+     cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sL1Jet6U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(84),
-           theSubDir = cms.string("Jet"),
+           theSubDir = cms.string("L1Jet"),
       ),
       cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sL1Jet6U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(85),
-           theSubDir = cms.string("Jet"),
+           theSubDir = cms.string("L1Jet"),
       ),cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sL1Jet6U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(86),
-           theSubDir = cms.string("Jet"),
+           theSubDir = cms.string("L1Jet"),
       ),
+     
       cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sJet15U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(84),
-           theSubDir = cms.string("Jet"),
-      ),      
+           theSubDir = cms.string("L1Jet"),
+      ),
       cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sJet15U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(85),
-           theSubDir = cms.string("Jet"),
+           theSubDir = cms.string("L1Jet"),
       ),cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sJet15U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(86),
-           theSubDir = cms.string("Jet"),
+           theSubDir = cms.string("L1Jet"),
       ),
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(84),
-##           theSubDir = cms.string("Jet"),
-##      ),       
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(85),
-##           theSubDir = cms.string("Jet"),
-##      ),cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(86),
-##           theSubDir = cms.string("Jet"),
-##      ),
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(84),
-##           theSubDir = cms.string("Jet"),
-##      ),       
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(85),
-##           theSubDir = cms.string("Jet"),
-##      ),cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(86),
-##           theSubDir = cms.string("Jet"),
-##      ),
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sFwdJet20U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(84),
-##           theSubDir = cms.string("QuadJet"),
-##      ),       
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sFwdJet20U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(85),
-##           theSubDir = cms.string("QuadJet"),
-##      ),       
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sFwdJet20U","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(86),
-##           theSubDir = cms.string("QuadJet"),
-##      ),
+     
       cms.PSet(
-          PlotBounds = cms.vdouble(0.0, 0.0),
-           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U","","HLT"),
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(84),
-           theSubDir = cms.string("DiJet"),
-      ),       
+           theSubDir = cms.string("L1Jet"),
+      ),
       cms.PSet(
-          PlotBounds = cms.vdouble(0.0, 0.0),
-           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U","","HLT"),
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(85),
-           theSubDir = cms.string("DiJet"),
-      ),       
-      cms.PSet(
-          PlotBounds = cms.vdouble(0.0, 0.0),
-           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U","","HLT"),
+           theSubDir = cms.string("L1Jet"),
+      ),cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet30U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(86),
-           theSubDir = cms.string("DiJet"),
-      ),       
-##       cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(84),
-##           theSubDir = cms.string("DiJet"),
-##      ),       
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(85),
-##           theSubDir = cms.string("DiJet"),
-##      ),
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(86),
-##           theSubDir = cms.string("DiJet"),
-##      ),
+           theSubDir = cms.string("L1Jet"),
+      ),
+     
+
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(84),
+           theSubDir = cms.string("L1Jet"),
+      ),
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(85),
+           theSubDir = cms.string("L1Jet"),
+      ),cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sJet50U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(86),
+           theSubDir = cms.string("L1Jet"),
+      ),
+     
+
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(84),
+           theSubDir = cms.string("L1DiJet"),
+      ),
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(85),
+           theSubDir = cms.string("L1DiJet"),
+      ),cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve15U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(86),
+           theSubDir = cms.string("L1DiJet"),
+      ),
+     
+
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(84),
+           theSubDir = cms.string("L1DiJet"),
+      ),
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(85),
+           theSubDir = cms.string("L1DiJet"),
+      ),cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sDiJetAve30U8E29","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(86),
+           theSubDir = cms.string("L1DiJet"),
+      ),
+     
 
       cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltL1sL1MET20","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(87),
-           theSubDir = cms.string("MET"),
+           theSubDir = cms.string("L1MET"),
       ),       
  
-##      cms.PSet(
-##           PlotBounds = cms.vdouble(0.0, 0.0),
-##           HLTCollectionLabels = cms.InputTag("hltL1sMET35","","HLT"),
-##           IsoCollections = cms.VInputTag(cms.InputTag("none")),
-##           theHLTOutputTypes = cms.uint32(87),
-##           theSubDir = cms.string("MET"),
-##      ),       
-       cms.PSet(
+     cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
-           HLTCollectionLabels = cms.InputTag("hltPreL1Jet6U","","HLT"),
+           HLTCollectionLabels = cms.InputTag("hltL1sMET35","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
-           theHLTOutputTypes = cms.uint32(95),
-           theSubDir = cms.string("Jet"),
+           theHLTOutputTypes = cms.uint32(87),
+           theSubDir = cms.string("L1MET"),
       ),       
-      cms.PSet(
+ 
+     cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltL1sMET100","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(87),
+           theSubDir = cms.string("L1MET"),
+      ),       
+ 
+
+     cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hlt1jet15U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
-           theHLTOutputTypes = cms.uint32(95),
+           theHLTOutputTypes = cms.uint32(97),
            theSubDir = cms.string("Jet"),
       ),       
       cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
-           HLTCollectionLabels = cms.InputTag("hltDiJetAve15U8E29","","HLT"),
+           HLTCollectionLabels = cms.InputTag("hlt1jet30U","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
-           theHLTOutputTypes = cms.uint32(95),
+           theHLTOutputTypes = cms.uint32(97),
+           theSubDir = cms.string("Jet"),
+      ),       
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hlt1jet50U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(97),
+           theSubDir = cms.string("Jet"),
+      ),       
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltDiJetAve15U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(97),
            theSubDir = cms.string("DiJet"),
-      ),
-     cms.PSet(
+      ),       
+      cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hltDiJetAve30U","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(97),
+           theSubDir = cms.string("DiJet"),
+      ),       
+      cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hltPreL1MET20","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(97),
            theSubDir = cms.string("MET"),
       ),       
-      cms.PSet(
+
+    cms.PSet(
            PlotBounds = cms.vdouble(0.0, 0.0),
            HLTCollectionLabels = cms.InputTag("hlt1MET35","","HLT"),
+           IsoCollections = cms.VInputTag(cms.InputTag("none")),
+           theHLTOutputTypes = cms.uint32(97),
+           theSubDir = cms.string("MET"),
+      ),       
+
+     cms.PSet(
+           PlotBounds = cms.vdouble(0.0, 0.0),
+           HLTCollectionLabels = cms.InputTag("hlt1MET100","","HLT"),
            IsoCollections = cms.VInputTag(cms.InputTag("none")),
            theHLTOutputTypes = cms.uint32(97),
            theSubDir = cms.string("MET"),
