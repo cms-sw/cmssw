b5999ee85ba10dac7f808f217515a8dbbf56a281
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-14 13:28:03 +0000

    change test

--- CondCore/PopCon/test/print_eff_example.py
+++ CondCore/PopCon/test/print_eff_example.py
@@ -13,11 +13,23 @@ process.eff = cms.ESSource("PoolDBESSource",
     ))
 )
 
-process.source = cms.Source("EmptyIOVSource",
-    firstValue = cms.uint64(310),
-    lastValue = cms.uint64(1000),
-    timetype = cms.string('runnumber'),
-    interval = cms.uint64(100)
+#process.source = cms.Source("EmptyIOVSource",
+#    firstValue = cms.uint64(310),
+#    lastValue = cms.uint64(1000),
+#    timetype = cms.string('runnumber'),
+#    interval = cms.uint64(100)
+#)
+
+process.source = cms.Source("EmptySource",
+  firstLuminosityBlock = cms.untracked.uint32(1),
+  numberEventsInLuminosityBlock = cms.untracked.uint32(3),
+  firstEvent = cms.untracked.uint32(1),
+  firstRun = cms.untracked.uint32(310),
+  numberEventsInRun = cms.untracked.uint32(12)
+)
+
+process.maxEvents = cms.untracked.PSet(
+ input = cms.untracked.int32(1000)
 )
 
 process.prod = cms.EDAnalyzer("EfficiencyByLabelAnalyzer")
