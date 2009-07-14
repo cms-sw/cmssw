12bfffd1948e054f2d1396b9759924b698304a19
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-07-14 18:07:50 +0000

    Add PFFilter

--- Validation/RecoParticleFlow/plugins/SealModule.cc
+++ Validation/RecoParticleFlow/plugins/SealModule.cc
@@ -2,8 +2,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoParticleFlow/plugins/PFTester.h"
 #include "Validation/RecoParticleFlow/plugins/GenericBenchmarkAnalyzer.h"
+#include "Validation/RecoParticleFlow/plugins/PFFilter.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE (PFTester) ;
 DEFINE_ANOTHER_FWK_MODULE (GenericBenchmarkAnalyzer) ;
+DEFINE_ANOTHER_FWK_MODULE (PFFilter) ;
