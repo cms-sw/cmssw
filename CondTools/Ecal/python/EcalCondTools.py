d6f721eca39d701e1790285a74305e9542738441
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-07-15 14:05:16 +0000

    Added endcap in comparisons

--- CondTools/Ecal/python/EcalCondTools.py
+++ CondTools/Ecal/python/EcalCondTools.py
@@ -1,7 +1,7 @@
 #
 # Misc functions to manipulate Ecal records
 # author: Stefano Argiro
-# id: $Id: EcalCondTools.py,v 1.4 2009/07/14 16:52:11 argiro Exp $
+# id: $Id: EcalCondTools.py,v 1.5 2009/07/14 17:49:20 argiro Exp $
 #
 #
 # WARNING: we assume that the list of iovs for a given tag
@@ -65,8 +65,12 @@ def compare(tag1,db1,since1,till1,
             tag2,db2,since2,till2,filename='compare.root'):
   '''Produce comparison plots for two records. Save plots to file \
      according to format. tag can be an xml file'''
-  coeff_1=[]
-  coeff_2=[]
+
+  coeff_1_b=[]
+  coeff_2_b=[]
+
+  coeff_1_e=[]
+  coeff_2_e=[]
   
   if  tag1.find(".xml") < 0:
       try:
@@ -76,12 +80,18 @@ def compare(tag1,db1,since1,till1,
         ex = Plug.Extractor(w)
         p = getObject(db1,tag1,since1,till1)
         p.extract(ex)
-        coeff_1 = [i for i in ex.values()]# first set of coefficients
+        coeff_1_b = [i for i in ex.values()]# first set of coefficients
+        what = {'how':'endcap'}
+        w = inspect.setWhat(Plug.What(),what)
+        ex = Plug.Extractor(w)
+        p.extract(ex)
+        coeff_1_e = [i for i in ex.values()]# first set of coefficients
+
 
       except Exception,er :
           print er
   else:
-      coeff_1,coeff_1_ee = EcalPyUtils.fromXML(tag1)
+      coeff_1_b,coeff_1_e = EcalPyUtils.fromXML(tag1)
 
   if  tag2.find(".xml")<0:
       try:  
@@ -91,31 +101,39 @@ def compare(tag1,db1,since1,till1,
         ex = Plug.Extractor(w)
         p = getObject(db2,tag2,since2,till2)
         p.extract(ex)
-        coeff_2 = [i for i in ex.values()]# 2nd set of coefficients
+        coeff_2_b = [i for i in ex.values()]# 2nd set of coefficients
+        what = {'how':'endcap'}
+        w = inspect.setWhat(Plug.What(),what)
+        ex = Plug.Extractor(w)
+        p.extract(ex)
+        coeff_2_e = [i for i in ex.values()]# first set of coefficients
+
         
       except Exception, er :
           print er
   else:
-      coeff_2,coeff_2_ee = EcalPyUtils.fromXML(tag2)
+      coeff_2_b,coeff_2_e = EcalPyUtils.fromXML(tag2)
 
     
   diff_distro_can = TCanvas("difference","difference")
-  diff_distro_can.Divide(2)
-  diff_distro_h   = TH1F("diffh","diffh",100,-2,2)
-  diff_distro_m   = TH2F("diffm","diffm",171,-85,86,360,1,361)
-  diff_distro_m.SetStats(0)
-  
-  for i,c in enumerate(coeff_1):  
-      diff = c - coeff_2[i]
-      ieta,iphi= EcalPyUtils.unhashEBIndex(i)
-      diff_distro_h.Fill(diff) 
-      diff_distro_m.Fill(ieta,iphi,diff)
-
+  diff_distro_can.Divide(2,3)
+ 
+  ebhisto,ebmap= compareBarrel(coeff_1_b,coeff_2_b)
+  eephisto,eepmap,eemhisto,eemmap=compareEndcap(coeff_1_e,coeff_2_e)
+ 
   diff_distro_can.cd(1)
-  diff_distro_h.Draw()
+  ebhisto.Draw()
   diff_distro_can.cd(2)
-  diff_distro_m.Draw("colz")
-
+  ebmap.Draw("colz")
+  diff_distro_can.cd(3)
+  eephisto.Draw()
+  diff_distro_can.cd(4)
+  eepmap.Draw("colz")
+  diff_distro_can.cd(5)
+  eemhisto.Draw()
+  diff_distro_can.cd(6)
+  eemmap.Draw("colz")
+  
   diff_distro_can.SaveAs(filename)
 
 
@@ -204,3 +222,55 @@ def getObject(db,tag,since,till):
         print "Could not retrieve payload for tag: " , tag, " since: ", since,\
           " till: " ,till
         sys.exit(0)
+
+
+
+
+
+def compareBarrel(coeff_barl_1,coeff_barl_2) :
+  '''Return an histogram and a map of the differences '''
+
+  diff_distro_h   = TH1F("diffh","diffh",100,-2,2)
+  diff_distro_m   = TH2F("diffm","diffm",171,-85,86,360,1,361)
+  diff_distro_m.SetStats(0)
+  
+  for i,c in enumerate(coeff_barl_1):  
+      diff = c - coeff_barl_2[i]      
+      ieta,iphi= EcalPyUtils.unhashEBIndex(i)
+      diff_distro_h.Fill(diff) 
+      diff_distro_m.Fill(ieta,iphi,diff)
+
+  return diff_distro_h, diff_distro_m 
+
+
+
+
+def compareEndcap(coeff_endc_1, coeff_endc_2) :
+    ''' Return an histogram and a map of the differences for each endcap'''
+
+    diff_distro_h_eep   = TH1F("diff EE+","diff EE+",100,-2,2)
+    diff_distro_h_eem   = TH1F("diff EE-","diff EE-",100,-2,2)
+
+    
+    diff_distro_m_eep   = TH2F("map EE+","map EE+",100,1,101,100,1,101)
+    diff_distro_m_eem   = TH2F("map EE-","map EE-",100,1,101,100,1,101)
+    
+    diff_distro_m_eep.SetStats(0)
+    diff_distro_m_eem.SetStats(0)
+
+
+    for i,c in enumerate(coeff_endc_1):  
+      diff = c - coeff_endc_2[i]
+      ix,iy,iz = EcalPyUtils.unhashEEIndex(i)
+      
+      if iz >0:
+          diff_distro_h_eep.Fill(diff)
+          diff_distro_m_eep.Fill(ix,iy,diff)
+      else:
+          diff_distro_h_eem.Fill(diff)
+          diff_distro_m_eem.Fill(ix,iy,diff)
+
+    return diff_distro_h_eep, \
+           diff_distro_m_eep, \
+           diff_distro_h_eem, \
+           diff_distro_m_eem
