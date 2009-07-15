86a87f4e0b6c812220b72aaa3eab6465887a223e
dorbaker <sha1-c0c861e1621fbd0445e7a5339f107fcc8dbfe745@cern.ch>
2009-07-15 14:11:17 +0000

    Included mixing module to run on Fast Sim Rel Val files.

--- FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
+++ FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
@@ -6,27 +6,29 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("FastSimulation.Configuration.CommonInputs_cff")
 process.load("FastSimulation.Configuration.FamosSequences_cff")
 process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
+process.load("FastSimulation.Configuration.mixNoPU_cfi")
+
 process.GlobalTag.globaltag = "MC_31X_V2::All"
 
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
+    input = cms.untracked.int32(15)
+    )
 
 process.load("FastSimulation.TrackingRecHitProducer.GSRecHitValidation_cfi")
 
 process.source = cms.Source("PoolSource",
-    debugFlag = cms.untracked.bool(True),
-    debugVebosity = cms.untracked.uint32(10),
-    fileNames = cms.untracked.vstring('file:rechits.root')
-)
-
-
+                            fileNames = cms.untracked.vstring(
+    '/store/relval/CMSSW_3_1_1/RelValSingleMuPt10/GEN-SIM-DIGI-RECO/MC_31X_V2_FastSim-v1/0002/D2EFE4C6-E16B-DE11-96D1-001D09F28E80.root'
+    )
+                            )
 
 #process.load("Configuration.StandardSequences.MagneticField_40T_cff")
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
 process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 
-process.p = cms.Path(process.testanalyzer)
+process.p = cms.Path(process.mix*process.testanalyzer)
 process.MessageLogger.destinations = ['detailedInfo.txt']
 
 
