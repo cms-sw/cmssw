2d736a3826ed08d24c858a9d2f3e8e6f6361ce70
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2009-07-15 10:51:32 +0000

    update for 311

--- FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
+++ FastSimulation/TrackingRecHitProducer/test/GSRecHitValidation_cfg.py
@@ -6,7 +6,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("FastSimulation.Configuration.CommonInputs_cff")
 process.load("FastSimulation.Configuration.FamosSequences_cff")
 process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
-process.GlobalTag.globaltag = "IDEAL_30X::All"
+process.GlobalTag.globaltag = "MC_31X_V2::All"
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -15,13 +15,15 @@ process.maxEvents = cms.untracked.PSet(
 process.load("FastSimulation.TrackingRecHitProducer.GSRecHitValidation_cfi")
 
 process.source = cms.Source("PoolSource",
-                            'file:Your_File.root'
-                            )
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(10),
+    fileNames = cms.untracked.vstring('file:rechits.root')
+)
 
 
 
-process.load("Configuration.StandardSequences.MagneticField_40T_cff")
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
 
 process.p = cms.Path(process.testanalyzer)
