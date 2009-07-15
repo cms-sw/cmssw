c54f585d4de2e8b3387f0a45b9a7822f77fda314
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-15 15:35:09 +0000

    a test

--- DataFormats/Math/interface/invertPosDefMatrix.h
+++ DataFormats/Math/interface/invertPosDefMatrix.h
@@ -50,9 +50,10 @@ namespace MathSSE {
     __m128d const & r1() const { return r[1]; }
     
     
+    // assume second row is already shuffle
     void invert() {
       //  load 2-3 as 3-2
-      __m128d tmp = _mm_shuffle_pd(r1(),r1(),1);
+      // __m128d tmp = _mm_shuffle_pd(r1(),r1(),1);
       // mult and sub
       __m128d det  = _mm_mul_pd(r0(),tmp);
       __m128d det2 = _mm_shuffle_pd(det,det,1);
@@ -61,7 +62,7 @@ namespace MathSSE {
       // m0 /det, m1/-det -> m3, m2
       r1() = _mm_div_pd(r0(),det);
       //back in order
-      r1() = _mm_shuffle_pd(r1(),r1(),1);
+      // r1() = _mm_shuffle_pd(r1(),r1(),1);
       // m3/det, m2/-det -> m0 m1
       r0() = _mm_div_pd(tmp,det);
     } 
@@ -71,12 +72,13 @@ namespace MathSSE {
 
 template<>
 bool invertPosDefMatrix<double,2>(ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > & m) {
-  MathSSE::M2 mm = { m.Array()[0], m.Array()[1], m.Array()[1], m.Array()[2]  };
+  // load shuffled
+  MathSSE::M2 mm = { m.Array()[0], m.Array()[1], m.Array()[2], m.Array()[1]  };
 
   mm.invert();
   m.Array()[0] = mm[0];
   m.Array()[1] = mm[1];
-  m.Array()[2] = mm[3];
+  m.Array()[2] = mm[2];
 
   return true;
 }
@@ -85,12 +87,12 @@ template<>
 bool invertPosDefMatrix<double,2>(ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > const & mIn,
 				  ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > & mOut) {
  
-  MathSSE::M2 mm = { mIn.Array()[0], mIn.Array()[1], mIn.Array()[1], mIn.Array()[2]  };
+  MathSSE::M2 mm = { mIn.Array()[0], mIn.Array()[1], mIn.Array()[2], mIn.Array()[1]  };
 
   mm.invert();
   mOut.Array()[0] = mm[0];
   mOut.Array()[1] = mm[1];
-  mOut.Array()[2] = mm[3];
+  mOut.Array()[2] = mm[2];
 
   return true;
 }
