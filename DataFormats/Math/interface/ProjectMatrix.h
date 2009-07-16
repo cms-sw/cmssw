8cdbdafa5ff1f3fc2762bfb885e979a5abb8807c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-16 07:27:42 +0000

    add a new utilities

---
+++ DataFormats/Math/interface/ProjectMatrix.h
@@ -0,0 +1,40 @@
+#ifndef DataFormat_Math_ProjectMatrix_H
+#define DataFormat_Math_ProjectMatrix_H
+
+#include "Math/SMatrix.h"
+
+
+
+template<typename T, unsigned int N, unsigned int D>
+struct ProjectMatrix{
+  typedef  ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> > SMatDD;
+  typedef  ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepSym<T,N> > SMatNN;
+  typedef  ROOT::Math::SMatrix<T,N,D > SMatND;
+
+  // no constructor
+  
+  // H*S
+  SMatND project(SmatDD const & s) {
+    SMatND r;
+    for (unsigned int i=0; i<D; i++)
+      for (unsigned int j=0; j<D; j++)
+	r(index(i),j) = s(i,j);
+    return r;
+  }
+
+  // K*H
+  SMatNN project(SmatND const & k) {
+    SMatNN s;
+    for (unsigned int i=0; i<N; i++)
+      for (unsigned int j=0; j<D; j++)
+	s(i,index(j)) = k(i,j);
+    return rs
+  }
+
+  // only H(i,index(i))=1.
+  unsigned int index[D];
+
+}
+
+
+#endif
