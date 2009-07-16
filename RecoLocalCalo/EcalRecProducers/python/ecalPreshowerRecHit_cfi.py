e4b15bb70e0c8f10e37193bc67c1ff039982baba
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-07-16 14:03:36 +0000

    the variables are set to be tracked now we add variables here

--- RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHit_cfi.py
@@ -2,7 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 # Ecal Preshower rechit producer
 ecalPreshowerRecHit = cms.EDProducer("ESRecHitProducer",
-    ESrechitCollection = cms.string('EcalRecHitsES'),
-    ESdigiCollection = cms.InputTag("ecalPreshowerDigis"),
-    algo = cms.string("ESRecHitWorker")
+                                     ESrechitCollection = cms.string('EcalRecHitsES'),
+                                     ESdigiCollection = cms.InputTag("ecalPreshowerDigis"),
+                                     algo = cms.string("ESRecHitWorker"),
+                                     ESGain = cms.int32(1),
+                                     ESBaseline = cms.int32(1000),
+                                     ESMIPADC = cms.double(50),
+                                     ESMIPkeV = cms.double(81.08)
 )
