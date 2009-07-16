5cac91e72affcc1a96f71454e0c2dc25a14f9a51
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-07-16 19:34:38 +0000

    change to PFMET Filter

--- Validation/RecoParticleFlow/plugins/SealModule.cc
+++ Validation/RecoParticleFlow/plugins/SealModule.cc
@@ -2,12 +2,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "Validation/RecoParticleFlow/plugins/PFTester.h"
 #include "Validation/RecoParticleFlow/plugins/GenericBenchmarkAnalyzer.h"
-#include "Validation/RecoParticleFlow/plugins/PFFilter.h"
+#include "Validation/RecoParticleFlow/plugins/PFMETFilter.h"
 #include "Validation/RecoParticleFlow/plugins/PFJetFilter.h"
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE (PFTester) ;
 DEFINE_ANOTHER_FWK_MODULE (GenericBenchmarkAnalyzer) ;
-DEFINE_ANOTHER_FWK_MODULE (PFFilter) ;
+DEFINE_ANOTHER_FWK_MODULE (PFMETFilter) ;
 DEFINE_ANOTHER_FWK_MODULE (PFJetFilter) ;
