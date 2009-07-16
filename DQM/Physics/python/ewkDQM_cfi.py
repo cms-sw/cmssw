3d76bb05b5719dda19b348c5cd610efa7fcbfa8c
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-07-16 16:39:05 +0000

    HLT path changes

--- DQM/Physics/python/ewkDQM_cfi.py
+++ DQM/Physics/python/ewkDQM_cfi.py
@@ -2,12 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 # DQM monitor module for EWK
 ewkDQM = cms.EDAnalyzer("EwkDQM",
-                            triggerPathToPass        = cms.string("HLT_Ele15_LW_L1R"),
-                            triggerResultsCollection = cms.InputTag("TriggerResults", "", "HLT"),
-                            muonCollection           = cms.InputTag("muons"),
-                            electronCollection       = cms.InputTag("gsfElectrons"),
-                            caloJetCollection        = cms.InputTag("sisCone5CaloJets"),
-                            caloMETCollection        = cms.InputTag("met"),
-                            genParticleCollection    = cms.InputTag("genParticles")
-#                           caloJetCollection        = cms.InputTag("L2L3CorJetSC5Calo"),
+            elecTriggerPathToPass    = cms.string("HLT_Ele15_SW_L1R"),
+            muonTriggerPathToPass    = cms.string("HLT_Mu9"),
+            triggerResultsCollection = cms.InputTag("TriggerResults", "", "HLT"),
+            muonCollection           = cms.InputTag("muons"),
+            electronCollection       = cms.InputTag("gsfElectrons"),
+            caloJetCollection        = cms.InputTag("sisCone5CaloJets"),
+            caloMETCollection        = cms.InputTag("met"),
+            genParticleCollection    = cms.InputTag("genParticles")
+#           caloJetCollection        = cms.InputTag("L2L3CorJetSC5Calo"),
 )
