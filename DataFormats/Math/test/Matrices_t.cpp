21bbf772702b1c690800a090c87cbf4ee2e898b7
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-17 09:33:00 +0000

    add timing

--- DataFormats/Math/test/Matrices_t.cpp
+++ DataFormats/Math/test/Matrices_t.cpp
@@ -11,8 +11,10 @@ typedef ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > Matrix2
 typedef ROOT::Math::SMatrix<double,3,3,ROOT::Math::MatRepSym<double,3> > Matrix3;
 
 
+void st(){}
+void en(){}
 
-int main() {
+int main(int argc, char * argv[] ) {
 
   double v[3] = {1.,-0.2,0.5};
   Matrix2 & m = *(new Matrix2(v,3));
@@ -27,34 +29,43 @@ int main() {
   std::cout << m << std::endl;
 
  
-  {
-    edm::HRTimeType s= edm::hrRealTime();
-    invertPosDefMatrix(m);
-    edm::HRTimeType e = edm::hrRealTime();
+  if (argc>1) {
+    {
+      edm::HRTimeType s= edm::hrRealTime();
+      st();
+      invertPosDefMatrix(m);
+      en();
+      edm::HRTimeType e = edm::hrRealTime();
+      std::cout << e-s << std::endl;
+    }
+    
+    {
+      edm::HRTimeType s= edm::hrRealTime();
+      st();
+      invertPosDefMatrix(m);
+      en();
+      edm::HRTimeType e = edm::hrRealTime();
     std::cout << e-s << std::endl;
+    }
+  } else {
+    {
+      edm::HRTimeType s= edm::hrRealTime();
+      st();
+      m.Invert();
+      en();
+      edm::HRTimeType e = edm::hrRealTime();
+      std::cout << e-s << std::endl;
+    }
+    
+    {
+      edm::HRTimeType s= edm::hrRealTime();
+      st();
+      m.Invert();
+      en();
+      edm::HRTimeType e = edm::hrRealTime();
+      std::cout << e-s << std::endl;
+    }
   }
-
-  {
-    edm::HRTimeType s= edm::hrRealTime();
-    invertPosDefMatrix(m);
-    edm::HRTimeType e = edm::hrRealTime();
-    std::cout << e-s << std::endl;
-  }
-
-  {
-    edm::HRTimeType s= edm::hrRealTime();
-    m.Invert();
-    edm::HRTimeType e = edm::hrRealTime();
-    std::cout << e-s << std::endl;
-  }
-
-  {
-    edm::HRTimeType s= edm::hrRealTime();
-    m.Invert();
-    edm::HRTimeType e = edm::hrRealTime();
-    std::cout << e-s << std::endl;
-  }
-
   return 0;
 
 
