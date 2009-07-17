4728ebe5b1b5cd41a34ae24cf1e3a7cd1a3a1f51
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-07-17 19:18:59 +0000

    add ESRawDataTask

--- DQM/EcalPreshowerMonitorModule/python/EcalPreshowerMonitorTasks_cfi.py
+++ DQM/EcalPreshowerMonitorModule/python/EcalPreshowerMonitorTasks_cfi.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
+from DQM.EcalPreshowerMonitorModule.ESRawDataTask_cfi import *
 from DQM.EcalPreshowerMonitorModule.ESIntegrityTask_cfi import *
 #from DQM.EcalPreshowerMonitorModule.ESPedestalTask_cfi import *
-#from DQM.EcalPreshowerMonitorModule.ESOccupancyTask_cfi import *
+from DQM.EcalPreshowerMonitorModule.ESOccupancyTask_cfi import *
 
 #ecalPreshowerDefaultTasksSequence = cms.Sequence(ecalPreshowerOccupancyTask*ecalPreshowerPedestalTask)
-ecalPreshowerDefaultTasksSequence = cms.Sequence(ecalPreshowerIntegrityTask)
+ecalPreshowerDefaultTasksSequence = cms.Sequence(ecalPreshowerRawDataTask*ecalPreshowerIntegrityTask*ecalPreshowerOccupancyTask)
 
