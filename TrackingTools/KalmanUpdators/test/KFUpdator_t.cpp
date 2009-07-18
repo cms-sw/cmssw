2aeab6dcdee03d73d3971056ae80b201e0ca4de4
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-18 08:54:38 +0000

    test also chi2

--- TrackingTools/KalmanUpdators/test/KFUpdator_t.cpp
+++ TrackingTools/KalmanUpdators/test/KFUpdator_t.cpp
@@ -143,7 +143,7 @@ struct KFUTest {
 struct Chi2Test {
   MeasurementEstimator const & chi2;
   mutable std::pair<bool, double> res;
-  Chi2Test(MeasurementEstimator const * ichi2) : chi2(&ichi2){}
+  Chi2Test(MeasurementEstimator const * ichi2) : chi2(*ichi2){}
 
 
   void print(const TrajectoryStateOnSurface& tsos,
