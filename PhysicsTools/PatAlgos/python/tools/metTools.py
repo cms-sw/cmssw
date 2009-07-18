134da586349e5095a4e921ee1de6c0426ed208e4
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-07-18 15:06:15 +0000

    added addPfMET to the metTools

--- PhysicsTools/PatAlgos/python/tools/metTools.py
+++ PhysicsTools/PatAlgos/python/tools/metTools.py
@@ -27,3 +27,30 @@ def addTcMET(process,
 
     ## add new met collections output to the pat summary
     process.allLayer1Summary.candidates += [ cms.InputTag('layer1METs'+postfixLabel) ]
+
+def addPfMET(process,
+             postfixLabel = 'PF'):
+    """
+    ------------------------------------------------------------------
+    add pflow MET collection to patEventContent:
+
+    process : process
+    ------------------------------------------------------------------
+    """
+    ## add module as process to the default sequence
+    def addAlso (label,value):
+        existing = getattr(process, label)
+        setattr( process, label+postfixLabel, value)
+        process.patDefaultSequence.replace( existing, existing*value )        
+
+    ## clone and add a module as process to the
+    ## default sequence
+    def addClone(label,**replaceStatements):
+        new = getattr(process, label).clone(**replaceStatements)
+        addAlso(label, new)
+
+    ## addClone('corMetType1Icone5Muons', uncorMETInputTag = cms.InputTag("tcMet"))
+    addClone('layer1METs', metSource = cms.InputTag("pfMet"), addMuonCorrections = False)
+
+    ## add new met collections output to the pat summary
+    process.allLayer1Summary.candidates += [ cms.InputTag('layer1METs'+postfixLabel) ]
