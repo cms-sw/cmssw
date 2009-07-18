6fb2274fbd47543d5524a10184d6d080d29c035e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-07-18 15:06:40 +0000

    removed obsolete function

--- PhysicsTools/PatAlgos/python/tools/cmsswVersionTools.py
+++ PhysicsTools/PatAlgos/python/tools/cmsswVersionTools.py
@@ -1,23 +1,2 @@
 import FWCore.ParameterSet.Config as cms
 
-def run22XonSummer08AODSIM(process, layers=[0,1]) :
-    ## (A): Drop FLow on input
-    process.source.inputCommands = cms.untracked.vstring(
-            'keep *',
-            'drop *_particleFlow_*_*',
-            #'drop *_particleFlowBlock_*_*',
-    )
-    ## (B): Also switch to CaloTau (temporary workaround)
-    print "WARNING: to run on Summer08AODSIM from 2.2.X requirs to drop ParticleFlow,\n\tso PAT will switch from PFTau to CaloTau"
-    from PhysicsTools.PatAlgos.tools.tauTools import switchToCaloTau
-    switchToCaloTau(process)
-    ## (C): Compute CaloTau discriminators which are missing in 21X AOD
-    process.patAODExtraReco += process.patCaloTauDiscrimination
-    process.allLayer1Taus.tauIDSources = cms.PSet(  
-            leadingTrackFinding = cms.InputTag("caloRecoTauDiscriminationByLeadingTrackFinding"),
-            leadingTrackPtCut   = cms.InputTag("caloRecoTauDiscriminationByLeadingTrackPtCut"),
-            byIsolation         = cms.InputTag("caloRecoTauDiscriminationByIsolation"),
-           #againstElectron     = cms.InputTag("caloRecoTauDiscriminationAgainstElectron"),  # Not on AOD
-    )
-
-
