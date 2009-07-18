89b8078ad102c2bead5eab2acdf6efc36ec6cf41
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-07-18 14:47:44 +0000

    - rename ES digitization module name - add ES energy reconstruction for CRAFT scenario

--- DQM/EcalPreshowerMonitorClient/test/es_dqm_sourceclient-file_cfg.py
+++ DQM/EcalPreshowerMonitorClient/test/es_dqm_sourceclient-file_cfg.py
@@ -12,17 +12,51 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("NewEventStreamFileReader",
                             fileNames = cms.untracked.vstring(
-    'file:/esdata/MWGR_29.00105579.0001.A.storageManager.00.0000.dat',
-    'file:/esdata/MWGR_29.00105579.0006.A.storageManager.01.0000.dat',
-    'file:/esdata/MWGR_29.00105579.0011.A.storageManager.02.0000.dat',
-    'file:/esdata/MWGR_29.00105579.0016.A.storageManager.03.0000.dat'
+    #    'file:/esdata/MWGR_29.00105579.0001.A.storageManager.00.0000.dat',
+    #    'file:/esdata/MWGR_29.00105579.0006.A.storageManager.01.0000.dat',
+    #    'file:/esdata/MWGR_29.00105579.0011.A.storageManager.02.0000.dat',
+    #    'file:/esdata/MWGR_29.00105579.0016.A.storageManager.03.0000.dat'
+    #    'file:/esdata/MWGR_29.00105699.0001.A.storageManager.00.0000.dat',
+    #    'file:/esdata/MWGR_29.00105699.0006.A.storageManager.01.0000.dat',
+    #    'file:/esdata/MWGR_29.00105699.0011.A.storageManager.02.0000.dat'
+    'file:/esdata/MWGR_29.00105692.0001.A.storageManager.00.0000.dat', #(bad file)
+    'file:/esdata/MWGR_29.00105703.0001.A.storageManager.00.0000.dat' #(bad file)
+    #'file:/esdata/MWGR_29.00105765.0001.A.storageManager.00.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0006.A.storageManager.01.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0011.A.storageManager.02.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0016.A.storageManager.03.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0021.A.storageManager.04.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0026.A.storageManager.05.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0031.A.storageManager.06.0000.dat',
+    #'file:/esdata/MWGR_29.00105765.0036.A.storageManager.07.0000.dat'
+#    'file:/esdata/MWGR_29.00105812.0001.A.storageManager.00.0000.dat'
+#    'file:/esdata/MWGR_29.00105820.0001.A.storageManager.00.0000.dat'
+#    'file:/esdata/MWGR_29.00106019.0001.A.storageManager.00.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0006.A.storageManager.01.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0011.A.storageManager.02.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0016.A.storageManager.03.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0021.A.storageManager.04.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0026.A.storageManager.05.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0031.A.storageManager.06.0000.dat',
+#    'file:/esdata/MWGR_29.00106019.0036.A.storageManager.07.0000.dat'    
     )
                             )
 
 
-process.load('EventFilter/ESRawToDigi/esRawToDigi_cfi')
-process.esRawToDigi.sourceTag = 'source'
-process.esRawToDigi.debugMode = False
+#process.load('EventFilter/ESRawToDigi/esRawToDigi_cfi')
+#process.esRawToDigi.sourceTag = 'source'
+#process.esRawToDigi.debugMode = False
+#process.esRawToDigi.debugMode = True
+
+import EventFilter.ESRawToDigi.esRawToDigi_cfi
+process.ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
+process.ecalPreshowerDigis.sourceTag = 'source'
+process.ecalPreshowerDigis.debugMode = False
+
+process.load('RecoLocalCalo/EcalRecProducers/ecalPreshowerRecHit_cfi')
+process.ecalPreshowerRecHit.ESGain = cms.int32(2)
+process.ecalPreshowerRecHit.ESBaseline = cms.int32(0)
+process.ecalPreshowerRecHit.ESMIPADC = cms.double(50)
 
 process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
 process.preScaler.prescaleFactor = 1
@@ -45,7 +79,7 @@ process.dqmSaver = cms.EDAnalyzer("DQMFileSaver",
 process.load("DQM/EcalPreshowerMonitorModule/EcalPreshowerMonitorTasks_cfi")
 process.load("DQM/EcalPreshowerMonitorClient/EcalPreshowerMonitorClient_cfi")
 
-process.p = cms.Path(process.preScaler*process.esRawToDigi*process.ecalPreshowerDefaultTasksSequence*process.ecalPreshowerMonitorClient*process.dqmSaver*process.dqmInfoES)
+process.p = cms.Path(process.preScaler*process.ecalPreshowerDigis*process.ecalPreshowerRecHit*process.ecalPreshowerDefaultTasksSequence*process.ecalPreshowerMonitorClient*process.dqmSaver*process.dqmInfoES)
 
 process.DQM.collectorHost = 'srv-c2c04-07'
 process.DQM.collectorPort = 9190
