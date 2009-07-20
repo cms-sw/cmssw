c630b993662922ee831c34366a3423ac04a936eb
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2009-07-20 17:10:35 +0000

    pickup rndm config directly from IOMC/RandomEngine

--- Configuration/StandardSequences/python/Services_cff.py
+++ Configuration/StandardSequences/python/Services_cff.py
@@ -6,7 +6,10 @@ import FWCore.ParameterSet.Config as cms
 
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 # Random numbers initialization service
-from Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff import *
+# pick it up directly
+from IOMC.RandomEngine.IOMC_cff import *
+#an "intermediate layer" remains, just in case somebody is using it...
+# from Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff import *
 DQMStore = cms.Service("DQMStore")
 
 
