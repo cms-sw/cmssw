1bc6dee22e831bca501df8dc04564dabf2613911
Avishek Chatterjee <sha1-69ad2298847d4cb6875643a102d6c79972e7f892@cern.ch>
2009-07-20 10:17:55 +0000

    Use Electron MonBitSummary

--- DQM/HLTEvF/python/HLTMonElectron_cff.py
+++ DQM/HLTEvF/python/HLTMonElectron_cff.py
@@ -1,13 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
-#from DQM.HLTEvF.HLTMonElectron_cfi import *
-#from DQM.HLTEvF.electronDQMIsoDistTrigger_cfi import *
-#from DQM.HLTEvF.electronDQMIsoDist_cfi import *
-#from DQM.HLTEvF.electronDQMPixelMatch_cfi import *
-#from DQM.HLTEvF.electronDQMConsumer_cfi import *
-#hltMonElectronPath = cms.Path(hltMonE)
-#hltMonElectronPath = cms.Path(hltMonE*electronDQMIsoDistTrigger*electronDQMIsoDist*electronDQMPixelMatch*electronDQMConsumer)
-
-
-from DQM.HLTEvF.electron_test_cff import *
-hltMonElectronPath = cms.Path(sourcePlots)
+from DQM.HLTEvF.HLTMonEleBits_cfi import *
+hltMonElectronPath = cms.Path(hltMonEleBits)
