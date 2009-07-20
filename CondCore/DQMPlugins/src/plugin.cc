644fd7a8e11e9ca7d2f4f42368567795804f6023
Salvatore Di Guida <sha1-744703a2e531bf35de4c8c886b1940546bb70d35@cern.ch>
2009-07-20 17:10:29 +0000

    Duplicated record for DQM root file exportation into Cond DB using GeometryFile

--- CondCore/DQMPlugins/src/plugin.cc
+++ CondCore/DQMPlugins/src/plugin.cc
@@ -6,3 +6,6 @@ REGISTER_PLUGIN(DQMSummaryRcd, DQMSummary);
 #include "CondFormats/DQMObjects/interface/HDQMSummary.h"
 #include "CondFormats/DataRecord/interface/HDQMSummaryRcd.h"
 REGISTER_PLUGIN(HDQMSummaryRcd, HDQMSummary);
+#include "CondFormats/GeometryObjects/interface/GeometryFile.h"
+#include "CondFormats/DataRecord/interface/DQMReferenceHistogramRootFileRcd.h"
+REGISTER_PLUGIN(DQMReferenceHistogramRootFileRcd, GeometryFile);
