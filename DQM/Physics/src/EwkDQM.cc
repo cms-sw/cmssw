5b5398d84d15103448365b1b3501587b096081fe
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-07-20 20:02:04 +0000

    Simplified, hopefully

--- DQM/Physics/src/EwkDQM.cc
+++ DQM/Physics/src/EwkDQM.cc
@@ -1,8 +1,8 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2009/07/17 09:30:28 $
- *  $Revision: 1.5 $
+ *  $Date: 2009/07/17 20:17:06 $
+ *  $Revision: 1.6 $
  *  \author Michael B. Anderson, University of Wisconsin-Madison
  *  \author Will Parker, University of Wisconsin-Madison
  */
@@ -71,18 +71,19 @@ void EwkDQM::beginJob(EventSetup const& iSetup) {
   theDbe = Service<DQMStore>().operator->();
   theDbe->setCurrentFolder("Physics/EwkDQM");  // Use folder with name of PAG
 
+  const float pi = 3.14159265;
+
   // Keep the number of plots and number of bins to a minimum!
-  h_mumu_invMass = theDbe->book1D("h_mumu_invMass", "#mu#mu Invariant Mass;InvMass (GeV)", 20, 40.0, 140.0);
-  h_ee_invMass   = theDbe->book1D("h_ee_invMass",   "ee Invariant Mass;InvMass (Gev)"    , 20, 40.0, 140.0);
+  h_mumu_invMass = theDbe->book1D("h_mumu_invMass", "#mu#mu Invariant Mass;InvMass (GeV)"    , 20, 40.0, 140.0);
+  h_ee_invMass   = theDbe->book1D("h_ee_invMass",   "ee Invariant Mass;InvMass (Gev)"        , 20, 40.0, 140.0);
   h_jet_et       = theDbe->book1D("h_jet_et",       "Jet with highest E_{T} (from "+theCaloJetCollectionLabel.label()+");E_{T}(jet) (GeV)",    20, 0., 200.0);
   h_jet_count    = theDbe->book1D("h_jet_count",    "Number of "+theCaloJetCollectionLabel.label()+" (E_{T} > 15 GeV);Number of Jets", 8, -0.5, 7.5);
-//WCP: Added histos
-  h_e1_et        = theDbe->book1D("h_e1_et",  "E_{T} of Leading Electron (GeV);E_{T} (GeV)" , 20, 0.0 , 100.0);
-  h_e2_et        = theDbe->book1D("h_e2_et",  "E_{T} of Second Electron (GeV);E_{T} (GeV)"  , 20,  0.0 , 100.0);
-  h_e1_eta       = theDbe->book1D("h_e1_eta", "#eta of Leading Electron;#eta"               , 20, -4.0 , 4.0);
-  h_e2_eta       = theDbe->book1D("h_e2_eta", "#eta of Second Electron;#eta"                , 20, -4.0 , 4.0);
-  h_e1_phi       = theDbe->book1D("h_e1_phi", "#phi of Leading Electron;#phi"               , 20, -4.0 , 4.0);
-  h_e2_phi       = theDbe->book1D("h_e2_phi", "#phi of Second Electron;#phi"                , 20, -4.0 , 4.0);
+  h_e1_et        = theDbe->book1D("h_e1_et",  "E_{T} of Leading Electron;E_{T} (GeV)"        , 20,  0.0 , 100.0);
+  h_e2_et        = theDbe->book1D("h_e2_et",  "E_{T} of Second Electron;E_{T} (GeV)"         , 20,  0.0 , 100.0);
+  h_e1_eta       = theDbe->book1D("h_e1_eta", "#eta of Leading Electron;#eta"                , 20, -4.0 , 4.0);
+  h_e2_eta       = theDbe->book1D("h_e2_eta", "#eta of Second Electron;#eta"                 , 20, -4.0 , 4.0);
+  h_e1_phi       = theDbe->book1D("h_e1_phi", "#phi of Leading Electron;#phi"                , 22, (-1.-1./10.)*pi, (1.+1./10.)*pi );
+  h_e2_phi       = theDbe->book1D("h_e2_phi", "#phi of Second Electron;#phi"                 , 22, (-1.-1./10.)*pi, (1.+1./10.)*pi );
   h_m1_pt        = theDbe->book1D("h_m1_pt",  "p_{T} of Leading Muon;p_{T}(1^{st} #mu) (GeV)", 20,  0.0 , 100.0);
   h_m2_pt        = theDbe->book1D("h_m2_pt",  "p_{T} of Second Muon;p_{T}(2^{nd} #mu) (GeV)" , 20,  0.0 , 100.0);
   h_m1_eta       = theDbe->book1D("h_m1_eta", "#eta of Leading Muon;#eta(1^{st} #mu)"        , 20, -4.0 , 4.0);
@@ -92,10 +93,10 @@ void EwkDQM::beginJob(EventSetup const& iSetup) {
 //  h_t1_et          = theDbe->book1D("h_t1_et",           "E_{T} of Leading Tau;E_{T} (GeV)" , 20, 0.0 , 100.0);
 //  h_t1_eta         = theDbe->book1D("h_t1_eta",          "#eta of Leading Tau;#eta"               , 20, -4.0, 4.0);
 //  h_t1_phi         = theDbe->book1D("h_t1_phi",          "#phi of Leading Tau;#phi"               , 20, -4.0, 4.0);
-  h_met          = theDbe->book1D("h_met",    "Missing E_{T} (GeV)"                         , 20, 0.0 , 100);
-  h_met_phi      = theDbe->book1D("h_met_phi","Missing E_{T} #phi;#phi"                     , 20, -4.0, 4.0);
-  h_e_invWMass       = theDbe->book1D("h_e_invWMass", "W-> e #nu Transverse Mass;M_{T} (GeV)", 60, 0.0, 140.0); 
-  h_m_invWMass       = theDbe->book1D("h_m_invWMass", "W-> e #nu Transverse Mass;M_{T} (GeV)", 60, 0.0, 140.0); 
+  h_met          = theDbe->book1D("h_met",        "Missing E_{T}; GeV"                       , 20,  0.0 , 100);
+  h_met_phi      = theDbe->book1D("h_met_phi",    "Missing E_{T} #phi;#phi(MET)"             , 22, (-1.-1./10.)*pi, (1.+1./10.)*pi );
+  h_e_invWMass   = theDbe->book1D("h_e_invWMass", "W-> e #nu Transverse Mass;M_{T} (GeV)"    , 20,  0.0, 140.0); 
+  h_m_invWMass   = theDbe->book1D("h_m_invWMass", "W-> #mu #nu Transverse Mass;M_{T} (GeV)"  , 20,  0.0, 140.0); 
 }
 
 
@@ -132,63 +133,48 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
   iEvent.getByLabel(theElectronCollectionLabel, electronCollection);
   if ( !electronCollection.isValid() ) return;
 
-  // Find the electron pair closest to z mass
-  float zMass = 91.1876;
-  float closestEEInvMasstoZFound = -9.0;
-  float W_mt_e = -9.0;
-  float electron_et   = -9.0;
-  float electron_eta  = -9.0;
-  float electron_phi  = -9.0;
+  // Find the highest and 2nd highest electron
+  float electron_et   = -8.0;
+  float electron_eta  = -8.0;
+  float electron_phi  = -8.0;
   float electron2_et  = -9.0;
   float electron2_eta = -9.0;
   float electron2_phi = -9.0;
-  float electronW_et  = -9.0;
-  float electronW_eta = -9.0;
-  float electronW_phi = -9.0;
+  float ee_invMass    = -9.0;
+  TLorentzVector e1, e2;
 
   // If it passed electron HLT and the collection was found, find electrons near Z mass
-  if( passed_electron_HLT && electronCollection.isValid() ) {
+  if( passed_electron_HLT ) {
+
     for (reco::GsfElectronCollection::const_iterator recoElectron=electronCollection->begin(); recoElectron!=electronCollection->end(); recoElectron++){
+
       // Require electron to pass some basic cuts
-      if (recoElectron->et() < 20 || fabs(recoElectron->eta())>2.5) continue;
+      if ( recoElectron->et() < 20 || fabs(recoElectron->eta())>2.5 ) continue;
 
-      if (recoElectron->et()>electronW_et){
-        electronW_et = recoElectron->et();
-        electronW_eta = recoElectron->eta();
-        electronW_phi = recoElectron->phi();
-        double dphiW = fabs(met_phi-electronW_phi); 
-        W_mt_e = sqrt(2*missing_et*electronW_et*(1-cos(dphiW)));
+      // Tighter electron cuts
+      if ( recoElectron->deltaPhiSuperClusterTrackAtVtx() > 0.58 || 
+           recoElectron->deltaEtaSuperClusterTrackAtVtx() > 0.01 || 
+           recoElectron->sigmaIetaIeta() > 0.027 ) continue;
+
+      if (recoElectron->et() > electron_et){
+	electron2_et  = electron_et;  // 2nd highest gets values from current highest
+	electron2_eta = electron_eta;
+        electron2_phi = electron_phi;
+        electron_et   = recoElectron->et();  // 1st highest gets values from new highest
+        electron_eta  = recoElectron->eta();
+        electron_phi  = recoElectron->phi();
+        e1 = TLorentzVector(recoElectron->momentum().x(),recoElectron->momentum().y(),recoElectron->momentum().z(),recoElectron->p());
+      } else if (recoElectron->et() > electron2_et) {
+	electron2_et  = recoElectron->et();
+        electron2_eta = recoElectron->eta();
+        electron2_phi = recoElectron->phi();
+	e2 = TLorentzVector(recoElectron->momentum().x(),recoElectron->momentum().y(),recoElectron->momentum().z(),recoElectron->p());
       }
-
-      // loop over all the other electrons
-      for (reco::GsfElectronCollection::const_iterator recoElectron2=recoElectron+1; recoElectron2!=electronCollection->end(); recoElectron2++){
-        // Require electron to pass some basic cuts
-        if (recoElectron2->et() < 20 || fabs(recoElectron2->eta())>2.5) continue;
-
-        TLorentzVector e1 = TLorentzVector(recoElectron->momentum().x(),recoElectron->momentum().y(),recoElectron->momentum().z(),recoElectron->p());
-        TLorentzVector e2 = TLorentzVector(recoElectron2->momentum().x(),recoElectron2->momentum().y(),recoElectron2->momentum().z(),recoElectron2->p());
-        TLorentzVector pair=e1+e2;
-        float currentInvMass = pair.M();
-        if (fabs(currentInvMass-zMass) < fabs(closestEEInvMasstoZFound-zMass)) {
-          closestEEInvMasstoZFound = currentInvMass;
-          if (recoElectron->et()>recoElectron2->et()) {
-	    electron_et   = recoElectron->et();
-	    electron_eta  = recoElectron->eta();
-	    electron_phi  = recoElectron->phi();
-	    electron2_et  = recoElectron2->et();
-            electron2_eta = recoElectron2->eta();
-            electron2_phi = recoElectron2->phi();
-          } else {
-	    electron_et   = recoElectron2->et();
-	    electron_eta  = recoElectron2->eta();
-	    electron_phi  = recoElectron2->phi();
-	    electron2_et  = recoElectron->et();
-            electron2_eta = recoElectron->eta();
-            electron2_phi = recoElectron->phi();
-          }
-        } // end of is close to Z mass
-      } // end of 2nd loop over electrons
     } // end of loop over electrons
+    if (electron2_et>0.0) {
+      TLorentzVector pair=e1+e2;
+      ee_invMass = pair.M();
+    }
   } // end of "are electrons valid"
   ////////////////////////////////////////////////////////////////////////////////
 
@@ -200,63 +186,43 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
   iEvent.getByLabel(theMuonCollectionLabel,muonCollection);
   if ( !muonCollection.isValid() ) return;
 
-  // Find the muon pair closest to z mass
-  //  float zMass = 91.1876;
-  float W_mt_m = -9.0;
-  float closestMMInvMasstoZFound = -9.0;
-  float muon_pt  = -9.0;
-  float muon_eta = -9.0;
-  float muon_phi = -9.0;
+  // Find the highest pt muons
+  float mm_invMass = -9.0;
+  float muon_pt   = -9.0;
+  float muon_eta  = -9.0;
+  float muon_phi  = -9.0;
   float muon2_pt  = -9.0;
   float muon2_eta = -9.0;
   float muon2_phi = -9.0;
-  float muonW_pt = -9.0;
-  float muonW_eta = -9.0;
-  float muonW_phi = -9.0;
+  TLorentzVector m1, m2;
 
-  if( passed_muon_HLT && muonCollection.isValid() ) {
+  if( passed_muon_HLT ) {
     for (reco::MuonCollection::const_iterator recoMuon=muonCollection->begin(); recoMuon!=muonCollection->end(); recoMuon++){
 
-    // Require muon to pass some basic cuts
-    if (recoMuon->pt() < 20) continue;
-      if (recoMuon->pt()>muonW_pt) {
-        muonW_pt = recoMuon->pt();
-        muonW_eta = recoMuon->eta();
-        muonW_phi = recoMuon->phi();
-      }
-      double dphiW = fabs(met_phi-muonW_phi); 
-      W_mt_m = sqrt(2*missing_et*muonW_pt*(1-cos(dphiW)));
-  
-      // loop over all the other muons
-      for (reco::MuonCollection::const_iterator recoMuon2=recoMuon+1; recoMuon2!=muonCollection->end(); recoMuon2++){
+      // Require muon to pass some basic cuts
+      if (recoMuon->pt() < 20) continue;
+      // Some tighter muon cuts
+      //if ( recoMuon->globalTrack()->normalizedChi2() > 10 ) continue;
 
-        // Require muon to pass some basic cuts
-        if (recoMuon2->pt() < 20) continue;
-
-        TLorentzVector m1 = TLorentzVector(recoMuon->momentum().x(),recoMuon->momentum().y(),recoMuon->momentum().z(),recoMuon->p());
-        TLorentzVector m2 = TLorentzVector(recoMuon2->momentum().x(),recoMuon2->momentum().y(),recoMuon2->momentum().z(),recoMuon2->p());
-        TLorentzVector pairm=m1+m2;
-        float currentInvMassm = pairm.M();
-        if (fabs(currentInvMassm-zMass) < fabs(closestMMInvMasstoZFound-zMass)) {
-          closestMMInvMasstoZFound = currentInvMassm;
-          if (recoMuon->pt()>recoMuon2->pt()) {
-	    muon_pt   = recoMuon->pt();
-	    muon_eta  = recoMuon->eta();
-	    muon_phi  = recoMuon->phi();
-	    muon2_pt  = recoMuon2->pt();
-            muon2_eta = recoMuon2->eta();
-            muon2_phi = recoMuon2->phi();
-          } else {
-	    muon_pt   = recoMuon2->pt();
-	    muon_eta  = recoMuon2->eta();
-	    muon_phi  = recoMuon2->phi();
-	    muon2_pt  = recoMuon->pt();
-            muon2_eta = recoMuon->eta();
-            muon2_phi = recoMuon->phi();
-          }
-        }
+      if (recoMuon->pt() > muon_pt){
+        muon2_pt  = muon_pt;  // 2nd highest gets values from current highest    
+        muon2_eta = muon_eta;
+        muon2_phi = muon_phi;
+        muon_pt   = recoMuon->pt();  // 1st highest gets values from new highest
+        muon_eta  = recoMuon->eta();
+        muon_phi  = recoMuon->phi();
+        m1 = TLorentzVector(recoMuon->momentum().x(),recoMuon->momentum().y(),recoMuon->momentum().z(),recoMuon->p());
+      } else if (recoMuon->et() > muon2_pt) {
+        muon2_pt  = recoMuon->pt();
+        muon2_eta = recoMuon->eta();
+        muon2_phi = recoMuon->phi();
+	m2 = TLorentzVector(recoMuon->momentum().x(),recoMuon->momentum().y(),recoMuon->momentum().z(),recoMuon->p());
       }
     }
+  }
+  if (muon2_pt>0.0) {
+    TLorentzVector pair=m1+m2;
+    mm_invMass = pair.M();
   }
   ////////////////////////////////////////////////////////////////////////////////  
 
@@ -306,25 +272,21 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
   ////////////////////////////////////////////////////////////////////////////////
   //                 Fill Histograms                                            //
   ////////////////////////////////////////////////////////////////////////////////
-    h_jet_et   ->Fill(jet_et);
-    h_jet_count->Fill(jet_count);
-    h_met      ->Fill(missing_et);
-    h_met_phi  ->Fill(met_phi);
-    h_e_invWMass ->Fill(W_mt_e);
-    h_m_invWMass ->Fill(W_mt_m);
 
-  if ( electron_et>0 && electron2_et>0 ) {
-    h_ee_invMass->Fill(closestEEInvMasstoZFound);
+  // Was Z->ee found?
+  if (ee_invMass>0.0) {
+    h_ee_invMass ->Fill(ee_invMass);
     h_e1_et      ->Fill(electron_et);
-    h_e2_et      ->Fill(electron2_et);
     h_e1_eta     ->Fill(electron_eta);
-    h_e2_eta     ->Fill(electron2_eta);
     h_e1_phi     ->Fill(electron_phi);
+    h_e2_et      ->Fill(electron2_et);
+    h_e2_eta     ->Fill(electron2_eta);
     h_e2_phi     ->Fill(electron2_phi);
   }
 
-  if ( muon_pt>0 && muon2_pt>0 ) {
-    h_mumu_invMass->Fill(closestMMInvMasstoZFound);
+  // Was Z->mu mu found?
+  if (mm_invMass > 0.0) {
+    h_mumu_invMass->Fill(mm_invMass);
     h_m1_pt      ->Fill(muon_pt);
     h_m2_pt      ->Fill(muon2_pt);
     h_m1_eta     ->Fill(muon_eta);
@@ -332,6 +294,33 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
     h_m1_phi     ->Fill(muon_phi);
     h_m2_phi     ->Fill(muon2_phi);
   }
+
+  // Was W->e nu found?
+  if (electron_et>0.0&&missing_et>20.0) {
+    double dphiW = fabs(met_phi-electron_phi);
+    float W_mt_e = sqrt(2*missing_et*electron_et*(1-cos(dphiW)));
+    h_m_invWMass ->Fill(W_mt_e);
+    h_e1_et      ->Fill(electron_et);
+    h_e1_eta     ->Fill(electron_eta);
+    h_e1_phi     ->Fill(electron_phi);
+    h_met        ->Fill(missing_et);
+    h_met_phi    ->Fill(met_phi);
+  }
+
+  // Was W->mu nu found?
+  if (muon_pt>0.0&&missing_et>20.0) {
+    double dphiW = fabs(met_phi-muon_phi);
+    float W_mt_m = sqrt(2*missing_et*muon_pt*(1-cos(dphiW)));
+    h_e_invWMass ->Fill(W_mt_m);
+    h_m1_pt      ->Fill(muon_pt);
+    h_m1_eta     ->Fill(muon_eta);
+    h_m1_phi     ->Fill(muon_phi);
+    h_met        ->Fill(missing_et);
+    h_met_phi    ->Fill(met_phi);
+  }
+
+  h_jet_et   ->Fill(jet_et);
+  h_jet_count->Fill(jet_count);
   ////////////////////////////////////////////////////////////////////////////////
 }
 
