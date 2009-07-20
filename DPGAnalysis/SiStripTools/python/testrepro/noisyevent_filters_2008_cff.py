fa4bf546c3ccc6f8178569b77ad1cd313ca28a84
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2009-07-20 18:36:41 +0000

    files updated to use the new classes and plugins in DPGAnalysis/SiStripTools

--- DPGAnalysis/SiStripTools/python/testrepro/noisyevent_filters_2008_cff.py
+++ DPGAnalysis/SiStripTools/python/testrepro/noisyevent_filters_2008_cff.py
@@ -4,20 +4,19 @@ import FWCore.ParameterSet.Config as cms
 # Filter against APV-induced noisy events
 #------------------------------------------------------------------
 
-from myTools.HistoricizedEventProducer.historicizedeventproducer_cfi import *
-from myTools.SimpleAPVPhaseProducer.simpleapvphaseproducer_CRAFT_cfi import *
-#from myTools.SimpleAPVPhaseProducer.configurableapvphaseproducer_CRAFT08_cfi import *
+from DPGAnalysis.SiStripTools.eventwithhistoryproducer_cfi import *
+from DPGAnalysis.SiStripTools.configurableapvcyclephaseproducer_CRAFT08_cfi import *
 
-from myTools.APVLatencyInfoESSource.apvlatencyinfofromconddb_CRAFT_cff import *
-essapvlatency.connect = cms.string("sqlite_file:/afs/cern.ch/cms/tracker/sistrlocrec/CRAFTReproIn31X/latency_31X.db")
+from DPGAnalysis.SiStripTools.apvlatency.apvlatencyfromconddb_CRAFT_cff import *
+essapvlatency.connect = cms.string("sqlite_file:/afs/cern.ch/cms/tracker/sistrlocrec/CRAFTReproIn31X/latency08_fromDPG.db")
 
-from myTools.HistoricizedEventFilter.Potential_TOB_HugeEvents_cfi import *
-from myTools.HistoricizedEventFilter.Potential_TIBTEC_HugeEvents_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TOB_HugeEvents_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_cfi import *
 
-from myTools.HistoricizedEventFilter.Potential_TOB_FrameHeaderEvents_firstpeak_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TOB_FrameHeaderEvents_firstpeak_cfi import *
 potentialTOBFrameHeaderEventsFPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(19,21)
 
-from myTools.HistoricizedEventFilter.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi import *
 potentialTIBTECFrameHeaderEventsFPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(19,21)
 
 potentialTOBFrameHeaderEventsAdditionalPeak = potentialTOBFrameHeaderEventsFPeak.clone() 
--- DPGAnalysis/SiStripTools/python/testrepro/noisyevent_filters_2009_cff.py
+++ DPGAnalysis/SiStripTools/python/testrepro/noisyevent_filters_2009_cff.py
@@ -4,21 +4,21 @@ import FWCore.ParameterSet.Config as cms
 # Filter against APV-induced noisy events
 #------------------------------------------------------------------
 
-from myTools.HistoricizedEventProducer.historicizedeventproducer_cfi import *
-from myTools.SimpleAPVPhaseProducer.configurableapvphaseproducer_GR09_cfi import *
+from DPGAnalysis.SiStripTools.eventwithhistoryproducer_cfi import *
+from DPGAnalysis.SiStripTools.configurableapvphaseproducer_GR09_cfi import *
 
-from myTools.APVLatencyInfoESSource.apvlatencyinfofromconddb_CRAFT_cff import *
-essapvlatency.connect = cms.string("sqlite_file:/afs/cern.ch/cms/tracker/sistrlocrec/CRAFTReproIn31X/latency09_31X.db")
+from DPGAnalysis.SiStripTools.apvlatency.apvlatencyinfofromconddb_CRAFT_cff import *
+essapvlatency.connect = cms.string("sqlite_file:/afs/cern.ch/cms/tracker/sistrlocrec/CRAFTReproIn31X/latency09_fromDPG.db")
 #
-from myTools.HistoricizedEventFilter.Potential_TIBTEC_HugeEvents_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_cfi import *
 potentialTIBTECHugeEvents.partitionName = cms.untracked.string("TM")
 #
-from myTools.HistoricizedEventFilter.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi import *
 potentialTIBTECFrameHeaderEventsFPeak.partitionName = cms.untracked.string("TM")
 potentialTIBTECFrameHeaderEventsFPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(19,21)
 #
 potentialTIBTECFrameHeaderEventsAdditionalPeak = potentialTIBTECFrameHeaderEventsFPeak.clone() 
 potentialTIBTECFrameHeaderEventsAdditionalPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(24,25)
 #
-from myTools.HistoricizedEventFilter.Potential_TIBTEC_FrameHeaderEvents_widerange_cfi import *
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_widerange_cfi import *
 potentialTIBTECFrameHeaderEventsWide.partitionName = cms.untracked.string("TM")
