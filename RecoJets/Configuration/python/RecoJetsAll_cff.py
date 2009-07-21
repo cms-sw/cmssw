2c121887ade159a708ddaa8265924ab5291a5e63
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2009-07-21 14:03:34 +0000

    add cambridge 4 and 6 and anti-kt 5 and 7

--- RecoJets/Configuration/python/RecoJetsAll_cff.py
+++ RecoJets/Configuration/python/RecoJetsAll_cff.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoJetsAll.cff,v 1.1 2007/08/02 21:47:55 fedor Exp $
+# $Id: RecoJetsAll_cff.py,v 1.2 2008/04/21 03:27:23 rpw Exp $
 #
 # reconstruct CaloTowers here as well
 from RecoJets.JetProducers.kt6CaloJets_cff import *
 from RecoJets.JetProducers.kt10CaloJets_cff import *
 from RecoJets.JetProducers.kt10E1CaloJets_cff import *
+from RecoJets.JetProducers.antikt5CaloJets_cff import *
+from RecoJets.JetProducers.antikt7CaloJets_cff import *
 from RecoJets.JetProducers.iterativeCone5CaloJets_cff import *
 from RecoJets.JetProducers.iterativeCone7CaloJets_cff import *
 from RecoJets.JetProducers.midPointCone5CaloJets_cff import *
@@ -13,5 +15,8 @@ from RecoJets.JetProducers.midPointCone7CaloJets_cff import *
 from RecoJets.JetProducers.sisCone5CaloJets_cff import *
 from RecoJets.JetProducers.sisCone7CaloJets_cff import *
 from RecoJets.JetProducers.cdfMidpointCone5CaloJets_cff import *
-recoJetsAll = cms.Sequence(kt6CaloJets+kt10CaloJets+kt10E1CaloJets+iterativeCone5CaloJets+iterativeCone7CaloJets+midPointCone5CaloJets+midPointCone7CaloJets+sisCone5CaloJets+sisCone7CaloJets+cdfMidpointCone5CaloJets)
+from RecoJets.JetProducers.cambridge4CaloJets_cff import *
+from RecoJets.JetProducers.cambridge6CaloJets_cff import *
+
+recoJetsAll = cms.Sequence(kt6CaloJets+kt10CaloJets+kt10E1CaloJets+antikt5CaloJets+antikt7CaloJets+iterativeCone5CaloJets+iterativeCone7CaloJets+midPointCone5CaloJets+midPointCone7CaloJets+sisCone5CaloJets+sisCone7CaloJets+cdfMidpointCone5CaloJets+cambridge4CaloJets+cambridge6CaloJets)
 
