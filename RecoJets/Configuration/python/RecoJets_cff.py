831195a3076e1d46d08b4331588df90bac63f336
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2009-07-21 14:01:51 +0000

    add antikt5 to jet reco

--- RecoJets/Configuration/python/RecoJets_cff.py
+++ RecoJets/Configuration/python/RecoJets_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoJets.cff,v 1.19 2008/02/29 00:42:40 fedor Exp $
+# $Id: RecoJets_cff.py,v 1.2 2008/04/21 03:27:24 rpw Exp $
 #
 # reconstruct CaloTowers here as well
 from RecoJets.JetProducers.kt4CaloJets_cff import *
 from RecoJets.JetProducers.kt6CaloJets_cff import *
+from RecoJets.JetProducers.antikt5CaloJets_cff import *
 from RecoJets.JetProducers.iterativeCone5CaloJets_cff import *
 from RecoJets.JetProducers.sisCone5CaloJets_cff import *
 from RecoJets.JetProducers.sisCone7CaloJets_cff import *
-recoJets = cms.Sequence(kt4CaloJets+kt6CaloJets+iterativeCone5CaloJets+sisCone5CaloJets+sisCone7CaloJets)
+recoJets = cms.Sequence(kt4CaloJets+kt6CaloJets+antikt5CaloJets+iterativeCone5CaloJets+sisCone5CaloJets+sisCone7CaloJets)
 
