d2fd63b85906f09504c4b9d51196842b3b13a491
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-07-21 20:49:42 +0000

    added test for the EventSetupRecordDataGetter

--- FWCore/Modules/test/ContentTest.sh
+++ FWCore/Modules/test/ContentTest.sh
@@ -4,3 +4,4 @@ function die { echo $1: status $2; exit $2; }
 
 cmsRun ${LOCAL_TEST_DIR}/ContentTest_cfg.py || die 'failed running cmsRun ContentTest_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/printeventsetupcontent_cfg.py || die 'failed running cmsRun printeventsetupcontent_cfg.py' $?
+cmsRun ${LOCAL_TEST_DIR}/geteventsetupcontent_cfg.py || die 'failed running cmsRun geteventsetupcontent_cfg.py' $?
