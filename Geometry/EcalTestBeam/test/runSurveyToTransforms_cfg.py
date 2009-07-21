b0d8b00e6fa131677f504bb44480c8dd9a4aad80
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-07-21 16:09:13 +0000

    Files for evaluation of 2007 H4 survey geometry

--- Geometry/EcalTestBeam/test/runSurveyToTransforms_cfg.py
+++ Geometry/EcalTestBeam/test/runSurveyToTransforms_cfg.py
@@ -1,11 +1,18 @@
 
 import FWCore.ParameterSet.Config as cms
 
+
 process = cms.Process("SurveyToTransforms")
+
+#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger.cout.placeholder = cms.untracked.bool(False)
+#process.MessageLogger.cout.threshold = cms.untracked.string('INFO')
+#process.MessageLogger.debugModules = cms.untracked.vstring('*')
 
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 
-process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
+#process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
+process.load("Geometry.EcalTestBeam.idealGeomPlusEE_cfi")
 
 process.load("Geometry.CaloEventSetup.CaloGeometry_cff")
 
@@ -29,6 +36,11 @@ process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck")
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string('survey.root')
                                    )
+
+process.testendcap = cms.ESProducer( "testEcalEndcapGeometryEP",
+                                     applyAlignment = cms.bool(False) )
+
+process.es_prefer_endcap = cms.ESPrefer( "testEcalEndcapGeometryEP", "testendcap" )
 
 process.p1 = cms.Path(process.cga)
 
