7d8cacdf495e18f8d5d62ebf68f16a24f99786fe
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-07-21 19:31:46 +0000

    improved suggested by Chris and implemented by Giuseppe to make the mergeProduct method faster and use less memory

--- DataFormats/Histograms/interface/MEtoEDMFormat.h
+++ DataFormats/Histograms/interface/MEtoEDMFormat.h
@@ -6,8 +6,8 @@
  *  DataFormat class to hold the information from a ME tranformed into
  *  ROOT objects as appropriate
  *
- *  $Date: 2009/06/24 10:32:54 $
- *  $Revision: 1.12 $
+ *  $Date: 2009/07/21 17:52:46 $
+ *  $Revision: 1.13 $
  *  \author M. Strang SUNY-Buffalo
  */
 
@@ -74,23 +74,37 @@ class MEtoEDM
   bool mergeProduct(const MEtoEDM<T> &newMEtoEDM) {
     const MEtoEdmObjectVector &newMEtoEDMObject = 
       newMEtoEDM.getMEtoEdmObject();
-    bool warn = false;
-    std::vector<bool> tmp(newMEtoEDMObject.size(), false);
-    for (unsigned int i = 0; i < MEtoEdmObject.size(); ++i) {
-      unsigned int j = 0;
-      while (j < newMEtoEDMObject.size() && (strcmp(MEtoEdmObject[i].name.c_str(), newMEtoEDMObject[j].name.c_str()) != 0)) ++j;
-      if (j < newMEtoEDMObject.size()) {
-        if (MEtoEdmObject[i].object.GetNbinsX() == newMEtoEDMObject[j].object.GetNbinsX() &&
-            MEtoEdmObject[i].object.GetXaxis()->GetXmin() == newMEtoEDMObject[j].object.GetXaxis()->GetXmin() &&
-            MEtoEdmObject[i].object.GetXaxis()->GetXmax() == newMEtoEDMObject[j].object.GetXaxis()->GetXmax() &&
-            MEtoEdmObject[i].object.GetNbinsY() == newMEtoEDMObject[j].object.GetNbinsY() &&
-            MEtoEdmObject[i].object.GetYaxis()->GetXmin() == newMEtoEDMObject[j].object.GetYaxis()->GetXmin() &&
-            MEtoEdmObject[i].object.GetYaxis()->GetXmax() == newMEtoEDMObject[j].object.GetYaxis()->GetXmax() &&
-            MEtoEdmObject[i].object.GetNbinsZ() == newMEtoEDMObject[j].object.GetNbinsZ() &&
-            MEtoEdmObject[i].object.GetZaxis()->GetXmin() == newMEtoEDMObject[j].object.GetZaxis()->GetXmin() &&
-            MEtoEdmObject[i].object.GetZaxis()->GetXmax() == newMEtoEDMObject[j].object.GetZaxis()->GetXmax()) {
-          MEtoEdmObject[i].object.Add(&newMEtoEDMObject[j].object);
-        } else {
+    const size_t nObjects = newMEtoEDMObject.size();
+    //  NOTE: we remember the present size since we will only add content
+    //        from newMEtoEDMObject after this point
+    const size_t nOldObjects = MEtoEdmObject.size();
+
+   // if the old and new are not the same size, we want to report a problem
+   bool warn = (nObjects == nOldObjects);
+
+   for (unsigned int i = 0; i < nObjects; ++i) {
+     unsigned int j = 0;
+     // see if the name is already in the old container up to the point where
+     // we may have added new entries in the container
+     const std::string& name =newMEtoEDMObject[i].name;
+     while (j <  nOldObjects && (MEtoEdmObject[j].name != name) ) ++j;
+     if (j >= nOldObjects) {
+       // this value is only in the new container, not the old one
+       MEtoEdmObject.push_back(newMEtoEDMObject[i]);
+       warn = true;
+     } else {
+       // this value is also in the new container: add the two 
+       if (MEtoEdmObject[i].object.GetNbinsX() == newMEtoEDMObject[j].object.GetNbinsX() &&
+           MEtoEdmObject[i].object.GetXaxis()->GetXmin() == newMEtoEDMObject[j].object.GetXaxis()->GetXmin() &&
+           MEtoEdmObject[i].object.GetXaxis()->GetXmax() == newMEtoEDMObject[j].object.GetXaxis()->GetXmax() &&
+           MEtoEdmObject[i].object.GetNbinsY() == newMEtoEDMObject[j].object.GetNbinsY() &&
+           MEtoEdmObject[i].object.GetYaxis()->GetXmin() == newMEtoEDMObject[j].object.GetYaxis()->GetXmin() &&
+           MEtoEdmObject[i].object.GetYaxis()->GetXmax() == newMEtoEDMObject[j].object.GetYaxis()->GetXmax() &&
+           MEtoEdmObject[i].object.GetNbinsZ() == newMEtoEDMObject[j].object.GetNbinsZ() &&
+           MEtoEdmObject[i].object.GetZaxis()->GetXmin() == newMEtoEDMObject[j].object.GetZaxis()->GetXmin() &&
+           MEtoEdmObject[i].object.GetZaxis()->GetXmax() == newMEtoEDMObject[j].object.GetZaxis()->GetXmax()) {
+         MEtoEdmObject[i].object.Add(&newMEtoEDMObject[j].object);
+       } else {
           std::cout << "ERROR MEtoEDM::mergeProducts(): different axis limits - DQM ME '" << MEtoEdmObject[i].name << "' not merged" <<  std::endl;
 #if 0
           std::cout << MEtoEdmObject[i].object.GetNbinsX() << " " << newMEtoEDMObject[j].object.GetNbinsX() << std::endl;
@@ -103,22 +117,13 @@ class MEtoEDM
           std::cout << MEtoEdmObject[i].object.GetZaxis()->GetXmin() << " " << newMEtoEDMObject[j].object.GetZaxis()->GetXmin() << std::endl;
           std::cout << MEtoEdmObject[i].object.GetZaxis()->GetXmax() << " " << newMEtoEDMObject[j].object.GetZaxis()->GetXmax() << std::endl;
 #endif
-        }
-        tmp[j] = true;
-      } else {
-        warn = true;
-      }
-    }
-    for (unsigned int j = 0; j < newMEtoEDMObject.size(); ++j) {
-      if (!tmp[j]) {
-        warn = true;
-        MEtoEdmObject.push_back(newMEtoEDMObject[j]);
-      }
-    }
-    if (warn) {
-      std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
-    }
-    return true;
+       }
+     }
+   }
+   if (warn) {
+     std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
+   }
+   return true;
   }
 
   void swap(MEtoEDM<T>& iOther) {
@@ -134,97 +139,107 @@ template <>
 inline bool
 MEtoEDM<double>::mergeProduct(const MEtoEDM<double> &newMEtoEDM)
 {
- const MEtoEdmObjectVector &newMEtoEDMObject = 
-   newMEtoEDM.getMEtoEdmObject();
- bool warn = false;
- std::vector<bool> tmp(newMEtoEDMObject.size(), false);
- for (unsigned int i = 0; i < MEtoEdmObject.size(); ++i) {
-   unsigned int j = 0;
-   while (j < newMEtoEDMObject.size() && (strcmp(MEtoEdmObject[i].name.c_str(), newMEtoEDMObject[j].name.c_str()) != 0)) ++j;
-   if (j < newMEtoEDMObject.size()) {
-     tmp[j] = true;
-   } else {
-     warn = true;
-   }
- }
- for (unsigned int j = 0; j < newMEtoEDMObject.size(); ++j) {
-   if (!tmp[j]) {
-     warn = true;
-     MEtoEdmObject.push_back(newMEtoEDMObject[j]);
-   }
- }
- if (warn) {
-   std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
- }
- return true;
+  const MEtoEdmObjectVector &newMEtoEDMObject =
+    newMEtoEDM.getMEtoEdmObject();
+  const size_t nObjects = newMEtoEDMObject.size();
+  //  NOTE: we remember the present size since we will only add content
+  //        from newMEtoEDMObject after this point
+  const size_t nOldObjects = MEtoEdmObject.size();
+
+  // if the old and new are not the same size, we want to report a problem
+  bool warn = (nObjects == nOldObjects);
+
+  for (unsigned int i = 0; i < nObjects; ++i) {
+    unsigned int j = 0;
+    // see if the name is already in the old container up to the point where
+    // we may have added new entries in the container
+    const std::string& name =newMEtoEDMObject[i].name;
+    while (j <  nOldObjects && (MEtoEdmObject[j].name != name) ) ++j;
+    if (j >= nOldObjects) {
+      // this value is only in the new container, not the old one
+      MEtoEdmObject.push_back(newMEtoEDMObject[i]);
+      warn = true;
+    }
+  }
+  if (warn) {
+    std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
+  }
+  return true;
 }
 
 template <>
 inline bool
 MEtoEDM<int>::mergeProduct(const MEtoEDM<int> &newMEtoEDM)
 {
- const MEtoEdmObjectVector &newMEtoEDMObject =
-   newMEtoEDM.getMEtoEdmObject();
- bool warn = false;
- std::vector<bool> tmp(newMEtoEDMObject.size(), false);
- for (unsigned int i = 0; i < MEtoEdmObject.size(); ++i) {
-   unsigned int j = 0;
-   while (j < newMEtoEDMObject.size() && (strcmp(MEtoEdmObject[i].name.c_str(), newMEtoEDMObject[j].name.c_str()) != 0)) ++j;
-   if (j < newMEtoEDMObject.size()) {
-     if ( MEtoEdmObject[i].name.find("EventInfo/processedEvents") != std::string::npos ) {
-       MEtoEdmObject[i].object += (newMEtoEDMObject[j].object);
-     }
-     if ( MEtoEdmObject[i].name.find("EventInfo/iEvent") != std::string::npos ||
-          MEtoEdmObject[i].name.find("EventInfo/iLumiSection") != std::string::npos) {
-       if (MEtoEdmObject[i].object < newMEtoEDMObject[j].object) {
-         MEtoEdmObject[i].object = (newMEtoEDMObject[j].object);
-       }
-     }
-     tmp[j] = true;
-   } else {
-     warn = true;
-   }
- }
- for (unsigned int j = 0; j < newMEtoEDMObject.size(); ++j) {
-   if (!tmp[j]) {
-     warn = true;
-     MEtoEdmObject.push_back(newMEtoEDMObject[j]);
-   }
- }
- if (warn) {
-   std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
- }
- return true;
+  const MEtoEdmObjectVector &newMEtoEDMObject =
+    newMEtoEDM.getMEtoEdmObject();
+  const size_t nObjects = newMEtoEDMObject.size();
+  //  NOTE: we remember the present size since we will only add content
+  //        from newMEtoEDMObject after this point
+  const size_t nOldObjects = MEtoEdmObject.size();
+
+  // if the old and new are not the same size, we want to report a problem
+  bool warn = (nObjects == nOldObjects);
+
+  for (unsigned int i = 0; i < nObjects; ++i) {
+    unsigned int j = 0;
+    // see if the name is already in the old container up to the point where
+    // we may have added new entries in the container
+    const std::string& name =newMEtoEDMObject[i].name;
+    while (j <  nOldObjects && (MEtoEdmObject[j].name != name) ) ++j;
+    if (j >= nOldObjects) {
+      // this value is only in the new container, not the old one
+      MEtoEdmObject.push_back(newMEtoEDMObject[i]);
+      warn = true;
+    } else {
+      // this value is also in the new container: add the two
+      if ( MEtoEdmObject[i].name.find("EventInfo/processedEvents") != std::string::npos ) {
+        MEtoEdmObject[i].object += (newMEtoEDMObject[j].object);
+      }
+      if ( MEtoEdmObject[i].name.find("EventInfo/iEvent") != std::string::npos ||
+           MEtoEdmObject[i].name.find("EventInfo/iLumiSection") != std::string::npos) {
+        if (MEtoEdmObject[i].object < newMEtoEDMObject[j].object) {
+          MEtoEdmObject[i].object = (newMEtoEDMObject[j].object);
+        }
+      }
+    }
+  }
+  if (warn) {
+    std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
+  }
+  return true;
 }
 
 template <>
 inline bool
 MEtoEDM<TString>::mergeProduct(const MEtoEDM<TString> &newMEtoEDM)
 {
- const MEtoEdmObjectVector &newMEtoEDMObject =
-   newMEtoEDM.getMEtoEdmObject();
- bool warn = false;
- std::vector<bool> tmp(newMEtoEDMObject.size(), false);
- for (unsigned int i = 0; i < MEtoEdmObject.size(); ++i) {
-   unsigned int j = 0;
-   while (j < newMEtoEDMObject.size() &&
-          (strcmp(MEtoEdmObject[i].name.c_str(), newMEtoEDMObject[j].name.c_str()) != 0)) ++j;
-   if (j < newMEtoEDMObject.size()) {
-     tmp[j] = true;
-   } else {
-     warn = true;
-   }
- }
- for (unsigned int j = 0; j < newMEtoEDMObject.size(); ++j) {
-   if (!tmp[j]) {
-     warn = true;
-     MEtoEdmObject.push_back(newMEtoEDMObject[j]);
-   }
- }
- if (warn) {
-   std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
- }
- return true;
+  const MEtoEdmObjectVector &newMEtoEDMObject =
+    newMEtoEDM.getMEtoEdmObject();
+  const size_t nObjects = newMEtoEDMObject.size();
+  //  NOTE: we remember the present size since we will only add content
+  //        from newMEtoEDMObject after this point
+  const size_t nOldObjects = MEtoEdmObject.size();
+
+  // if the old and new are not the same size, we want to report a problem
+  bool warn = (nObjects == nOldObjects);
+
+  for (unsigned int i = 0; i < nObjects; ++i) {
+    unsigned int j = 0;
+    // see if the name is already in the old container up to the point where
+    // we may have added new entries in the container
+    const std::string& name =newMEtoEDMObject[i].name;
+    while (j <  nOldObjects && (MEtoEdmObject[j].name != name) ) ++j;
+    if (j >= nOldObjects) {
+      // this value is only in the new container, not the old one
+      MEtoEdmObject.push_back(newMEtoEDMObject[i]);
+      warn = true;
+    }
+  }
+  if (warn) {
+    std::cout << "WARNING MEtoEDM::mergeProducts(): problem found" << std::endl;
+  }
+  return true;
 }
 
 #endif
