d47d3aaf863046ede4d02299af8f8ad019de0987
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2009-07-21 13:57:16 +0000

    new config files for anti-kt jets with 0.5 and 0.7 cone sizes

---
+++ RecoJets/JetProducers/python/antikt5CaloJets_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+# $Id: antikt4CaloJets_cff.py,v 1.1 2008/11/10 19:05:03 srappocc Exp $
+from RecoJets.JetProducers.CaloJetParameters_cfi import *
+from RecoJets.JetProducers.FastjetParameters_cfi import *
+from RecoJets.JetProducers.AntiKtJetParameters_cfi import *
+antikt5CaloJets = cms.EDProducer("AntiKtJetProducer",
+    FastjetNoPU,
+    AntiKtJetParameters,
+    CaloJetParameters,
+    
+    alias = cms.untracked.string('ANTIKT5CaloJet'),
+    FJ_ktRParam = cms.double(0.5)
+)
+
+
