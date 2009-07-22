4dc760524f59f32faf8d0bb7d78ebc671c71fb4e
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2009-07-22 12:44:21 +0000

    Move to the same BeamSpot label as in reco

--- Validation/RecoMuon/python/muonValidationHLT_cff.py
+++ Validation/RecoMuon/python/muonValidationHLT_cff.py
@@ -13,7 +13,7 @@ l2MuonTrackV.label = ('hltL2Muons',)
 l2MuonTrackV.associatormap = 'tpToL2TrackAssociation'
 l2MuonTrackV.associators = ('TrackAssociatorByDeltaR',)
 l2MuonTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l2MuonTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l2MuonTrackV.beamSpot = 'hltOfflineBeamSpot'
 l2MuonTrackV.ignoremissingtrackcollection=True
 
 l2UpdMuonTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -23,7 +23,7 @@ l2UpdMuonTrackV.label = ('hltL2Muons:UpdatedAtVtx',)
 l2UpdMuonTrackV.associatormap = 'tpToL2UpdTrackAssociation'
 l2UpdMuonTrackV.associators = ('TrackAssociatorByDeltaR',)
 l2UpdMuonTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l2UpdMuonTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l2UpdMuonTrackV.beamSpot = 'hltOfflineBeamSpot'
 l2UpdMuonTrackV.ignoremissingtrackcollection=True
 
 l3MuonTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -33,7 +33,7 @@ l3MuonTrackV.associatormap = 'tpToL3TrackAssociation'
 l3MuonTrackV.label = ('hltL3Muons',)
 l3MuonTrackV.associators = ('TrackAssociatorByDeltaR',)
 l3MuonTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l3MuonTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l3MuonTrackV.beamSpot = 'hltOfflineBeamSpot'
 l3MuonTrackV.ignoremissingtrackcollection=True
 
 l3TkMuonTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -44,7 +44,7 @@ l3TkMuonTrackV.associatormap = 'tpToL3TkTrackTrackAssociation'
 l3TkMuonTrackV.label = ('hltL3TkTracksFromL2',)
 l3TkMuonTrackV.associators = ('OnlineTrackAssociatorByHits',)
 l3TkMuonTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l3TkMuonTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l3TkMuonTrackV.beamSpot = 'hltOfflineBeamSpot'
 l3TkMuonTrackV.ignoremissingtrackcollection=True
 
 l3TkMuonMuTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -54,7 +54,7 @@ l3TkMuonMuTrackV.associatormap = 'tpToL3TkMuonAssociation'
 l3TkMuonMuTrackV.label = ('hltL3TkTracksFromL2:',)
 l3TkMuonMuTrackV.associators = ('tpToL3TkMuonAssociation',)
 l3TkMuonMuTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l3TkMuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l3TkMuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
 l3TkMuonMuTrackV.ignoremissingtrackcollection=True
 
 l2MuonMuTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -64,7 +64,7 @@ l2MuonMuTrackV.associatormap = 'tpToL2MuonAssociation'
 l2MuonMuTrackV.label = ('hltL2Muons',)
 l2MuonMuTrackV.associators = ('tpToL2MuonAssociation',)
 l2MuonMuTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l2MuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l2MuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
 l2MuonMuTrackV.ignoremissingtrackcollection=True
 
 l2UpdMuonMuTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -74,7 +74,7 @@ l2UpdMuonMuTrackV.associatormap = 'tpToL2UpdMuonAssociation'
 l2UpdMuonMuTrackV.label = ('hltL2Muons:UpdatedAtVtx',)
 l2UpdMuonMuTrackV.associators = ('tpToL2UpdMuonAssociation',)
 l2UpdMuonMuTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l2UpdMuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l2UpdMuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
 l2UpdMuonMuTrackV.ignoremissingtrackcollection=True
 
 l3MuonMuTrackV = Validation.RecoMuon.MultiTrackValidator_cfi.RMmultiTrackValidator.clone()
@@ -84,7 +84,7 @@ l3MuonMuTrackV.associatormap = 'tpToL3MuonAssociation'
 l3MuonMuTrackV.label = ('hltL3Muons:',)
 l3MuonMuTrackV.associators = ('tpToL3MuonAssociation',)
 l3MuonMuTrackV.dirName = 'HLT/Muon/MultiTrack/'
-l3MuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
+#l3MuonMuTrackV.beamSpot = 'hltOfflineBeamSpot'
 l3MuonMuTrackV.ignoremissingtrackcollection=True
 
 
