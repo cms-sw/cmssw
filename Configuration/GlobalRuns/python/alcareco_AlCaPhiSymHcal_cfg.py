5d0c69d8654fe093db088b6aaeea6d6955ae35d2
David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch>
2009-07-22 14:55:13 +0000

    Use combined AlCa file as output, for subsequent splitting, in order to fit T0 prompt workflow

--- Configuration/GlobalRuns/python/alcareco_AlCaPhiSymHcal_cfg.py
+++ Configuration/GlobalRuns/python/alcareco_AlCaPhiSymHcal_cfg.py
@@ -5,7 +5,7 @@
 # with command line options: alcareco_AlCaPhiSymHcal -s ALCA:HcalCalMinBias+DQM --conditions=FrontierConditions_GlobalTag,GR09_31X_V4P::All --scenario cosmics --data --magField 0T -n 1000 --no_exec
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('ALCA')
+process = cms.Process('RECO')
 
 # import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
@@ -18,12 +18,12 @@ process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/EventContent/EventContentCosmics_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.131 $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('alcareco_AlCaPhiSymHcal nevts:1000'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
@@ -33,24 +33,17 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('/store/data/Commissioning09/AlCaPhiSymHcal/RAW/v3/000/106/019/F43B1369-5172-DE11-AF53-001D09F24448.root')
 )
 
-# Additional output definition
-process.ALCARECOStreamHcalCalMinBias = cms.OutputModule("PoolOutputModule",
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('pathALCARECOHcalCalMinBias')
-    ),
-    outputCommands = cms.untracked.vstring('drop *', 
-        'keep HBHERecHitsSorted_hbherecoMB_*_*', 
-        'keep HORecHitsSorted_horecoMB_*_*', 
-        'keep HFRecHitsSorted_hfrecoMB_*_*', 
-        'keep HBHERecHitsSorted_hbherecoNoise_*_*', 
-        'keep HORecHitsSorted_horecoNoise_*_*', 
-        'keep HFRecHitsSorted_hfrecoNoise_*_*'),
-    fileName = cms.untracked.string('ALCARECOHcalCalMinBias.root'),
-    dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string('StreamALCARECOHcalCalMinBias'),
-        dataTier = cms.untracked.string('ALCARECO')
-    )
+# Combined AlCaReco output
+process.ALCARECOStreamCombined = cms.OutputModule("PoolOutputModule",
+   outputCommands = process.ALCARECOEventContent.outputCommands,
+   fileName = cms.untracked.string('ALCACombined.root'),
+   dataset = cms.untracked.PSet(
+       filterName = cms.untracked.string('StreamALCACombined'),
+       dataTier = cms.untracked.string('ALCARECO')
+   )
 )
+from Configuration.EventContent.AlCaRecoOutput_cff import *
+process.ALCARECOStreamCombined.outputCommands.extend(OutALCARECOHcalCalMinBias_noDrop.outputCommands)
 
 # Other statements
 process.GlobalTag.globaltag = 'GR09_31X_V4P::All'
@@ -99,7 +92,7 @@ process.pathALCARECOEcalCalPhiSym = cms.Path(process.seqALCARECOEcalCalPhiSym*pr
 process.pathALCARECOMuAlGlobalCosmics = cms.Path(process.seqALCARECOMuAlGlobalCosmics*process.ALCARECOMuAlGlobalCosmicsDQM)
 process.pathALCARECOTkAlJpsiMuMu = cms.Path(process.seqALCARECOTkAlJpsiMuMu*process.ALCARECOTkAlJpsiMuMuDQM)
 process.endjob_step = cms.Path(process.endOfProcess)
-process.ALCARECOStreamHcalCalMinBiasOutPath = cms.EndPath(process.ALCARECOStreamHcalCalMinBias)
+process.ALCARECOStreamCombinedOutPath = cms.EndPath(process.ALCARECOStreamCombined)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.pathALCARECODQM,process.pathALCARECOHcalCalMinBias,process.endjob_step,process.ALCARECOStreamHcalCalMinBiasOutPath)
+process.schedule = cms.Schedule(process.pathALCARECODQM,process.pathALCARECOHcalCalMinBias,process.endjob_step,process.ALCARECOStreamCombinedOutPath)
