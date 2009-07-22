75c725ff3e04208638fa07c0cedca9da7b1a9e92
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2009-07-22 11:18:31 +0000

    Remove unnecessary includes and BuildFile dependencies

--- Utilities/Timing/src/LinuxCPUTime.cc
+++ Utilities/Timing/src/LinuxCPUTime.cc
@@ -1,10 +1,5 @@
 #include "Utilities/Timing/interface/LinuxCPUTime.h"
 #include <iostream>
-#include <fstream>
-#include <limits>
-#include "Utilities/General/interface/CMSexception.h"
-#include "Utilities/General/interface/ioutils.h"
-
  
 std::ostream * LinuxElapsedTime::dout = &std::cout;
 
