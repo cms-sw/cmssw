1a6889f119f45ffde6543d70fbf558346e7c3d26
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-07-24 18:10:29 +0000

    set BINCHECKER_MODE_DDU = False

--- DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
+++ DQM/CSCMonitorModule/python/test/csc_dqm_sourceclient-file_cfg.py
@@ -225,8 +225,9 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
 #process.GlobalTag.globaltag = "CRAFT_V17H::All"
 #process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://localhost:8000/FrontierOnProd)(serverurl=http://localhost:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
-process.GlobalTag.globaltag = 'GR09_31X_V1H::All' 
-#process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+#process.GlobalTag.globaltag = 'GR09_31X_V1H::All' 
+process.GlobalTag.globaltag = 'GR09_31X_V1P::All' 
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 
 #--------------------------
