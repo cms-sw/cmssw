21767302245ba83bf7fa85ea334462000420faa2
benucci <sha1-daad2104d9852eedb2ba1d7c2d4eda3d03263c78@cern.ch>
2009-07-24 15:33:30 +0000

    cfg for analysis of specific records for quality

--- DQM/SiStripMonitorSummary/test/DBReader_Quality_cfg.py
+++ DQM/SiStripMonitorSummary/test/DBReader_Quality_cfg.py
@@ -20,9 +20,9 @@ process.load("CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_n
 )
 
 process.source = cms.Source("EmptyIOVSource",
-                            lastValue = cms.uint64(70873),
+                            lastValue = cms.uint64(1000000),
                             timetype = cms.string('runnumber'),
-                            firstValue = cms.uint64(70783),
+                            firstValue = cms.uint64(1000000),
                             interval = cms.uint64(1)
                             )
 
@@ -35,43 +35,42 @@ process.a = cms.ESSource("PoolDBESSource",
         authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
     ),
     timetype = cms.untracked.string('runnumber'),
-#    connect = cms.string('frontier://cmsfrontier.cern.ch:8000/FrontierProd/CMS_COND_31X_FROM21X'),
-    connect = cms.string('frontier://cmsfrontier.cern.ch:8000/FrontierProd/CMS_COND_31X_STRIP'),
-#    connect = cms.string('frontier://cmsfrontier.cern.ch:8000/FrontierProd/CMS_COND_STRIP'),
+    connect = cms.string('frontier://cmsfrontier.cern.ch:8000/FrontierProd/CMS_COND_31X_FROM21X'),
                          toGet = cms.VPSet(
     cms.PSet(
     record = cms.string('SiStripBadChannelRcd'), 
-    tag = cms.string('SiStripBadComponents_realisticMC_31X_v1')
-##        tag = cms.string('SiStripBadChannel_CRAFT_31X_v1_offline')        
-    ),
-    cms.PSet(
-    record = cms.string('SiStripDetVOffRcd'),
-    tag = cms.string('SiStripDetVOff_Ideal_31X_v2')
-    ),
-    cms.PSet(
-    record = cms.string('SiStripFedCablingRcd'),
-    tag = cms.string('SiStripFedCabling_CRAFT_31X_v1_offline')
-    ),
-    cms.PSet(
-    record = cms.string('SiStripBadFiberRcd'),
-    tag = cms.string('SiStripBadFiber_Ideal_31X_v2')
-    ),
-    cms.PSet(
-    record = cms.string('SiStripBadModuleRcd'),
-    tag = cms.string('SiStripBadModule_Ideal_31X_v2')
-    ),
+##    tag = cms.string('SiStripBadChannel_GR09_31X_v1_hlt')
+        tag = cms.string('SiStripBadChannel_HotStrip_CRAFT_v3_offline')
+##        tag = cms.string('SiStripBadChannel_HotStrip_GR09_31X_v1_hlt')    
+    )
+##     cms.PSet(
+##     record = cms.string('SiStripDetVOffRcd'),
+##     tag = cms.string('SiStripDetVOff_Ideal_31X_v2')
+##     ),
+##     cms.PSet(
+##     record = cms.string('SiStripFedCablingRcd'),
+##     tag = cms.string('SiStripFedCabling_GR09_31X_v1_hlt')
+##     ),
+##     cms.PSet(
+##     record = cms.string('SiStripBadFiberRcd'),
+##     tag = cms.string('SiStripBadFiber_Ideal_31X_v2')
+##     ),
+##     cms.PSet(
+##     record = cms.string('SiStripBadModuleRcd'),
+##     tag = cms.string('SiStripBadModule_Ideal_31X_v2')
+##     ),
   
     )
 )
 
-siStripQualityESProducer = cms.ESProducer("SiStripQualityESProducer",
+process.siStripQualityESProducer = cms.ESProducer("SiStripQualityESProducer",
     ThresholdForReducedGranularity = cms.double(0.2),
     appendToDataLabel = cms.string(''),
-    ReduceGranularity = cms.bool(True),
+    ReduceGranularity = cms.bool(False),
     ListOfRecordToMerge = cms.VPSet(cms.PSet(
-    record = cms.string('SiStripDetVOffRcd'),
+##    record = cms.string('SiStripDetVOffRcd'),
 ##    record = cms.string('SiStripDetCablingRcd'),
-##    record = cms.string('SiStripBadChannelRcd'),        
+    record = cms.string('SiStripBadChannelRcd'),        
     tag = cms.string('')
     ))
 )
@@ -118,49 +117,23 @@ process.CondDataMonitoring.MonitorSiStripLowThreshold  = False
 process.CondDataMonitoring.MonitorSiStripHighThreshold = False
 
 process.CondDataMonitoring.OutputMEsInRootFile         = True
+process.CondDataMonitoring.FillConditions_PSet.Mod_On            = False
 process.CondDataMonitoring.FillConditions_PSet.OutputSummaryAtLayerLevelAsImage           = False
 process.CondDataMonitoring.FillConditions_PSet.OutputSummaryProfileAtLayerLevelAsImage    = False # This should be saved only in case of LA (because for LA no SummaryAtLayerLevel is available)
 
 ## --- TkMap specific Configurable options:
 
-process.CondDataMonitoring.SiStripCablingDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripCablingDQM_PSet.TkMapName     = 'CablingTkMap.png'
-process.CondDataMonitoring.SiStripCablingDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripCablingDQM_PSet.maxValue     = 6.
+process.CondDataMonitoring.SiStripQualityDQM_PSet.TkMap_On     = True
+process.CondDataMonitoring.SiStripQualityDQM_PSet.TkMapName     = 'QualityTkMap.svg'
+process.CondDataMonitoring.SiStripQualityDQM_PSet.minValue     = 0.
+process.CondDataMonitoring.SiStripQualityDQM_PSet.maxValue     = 1.
 
-process.CondDataMonitoring.SiStripPedestalsDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripPedestalsDQM_PSet.TkMapName     = 'PedestalTkMap.svg'
-process.CondDataMonitoring.SiStripPedestalsDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripPedestalsDQM_PSet.maxValue     = 400.
 
-process.CondDataMonitoring.SiStripNoisesDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripNoisesDQM_PSet.TkMapName     = 'NoiseTkMap.svg'
-process.CondDataMonitoring.SiStripNoisesDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripNoisesDQM_PSet.maxValue     = 6.
 
-process.CondDataMonitoring.SiStripApvGainsDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripApvGainsDQM_PSet.TkMapName     = 'GainTkMap.svg'
-process.CondDataMonitoring.SiStripApvGainsDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripApvGainsDQM_PSet.maxValue     = 1.5
-
-process.CondDataMonitoring.SiStripLorentzAngleDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripLorentzAngleDQM_PSet.TkMapName     = 'LorentzAngleTkMap.svg'
-process.CondDataMonitoring.SiStripLorentzAngleDQM_PSet.minValue     = 0.01
-process.CondDataMonitoring.SiStripLorentzAngleDQM_PSet.maxValue     = 0.03
-
-process.CondDataMonitoring.SiStripLowThresholdDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripLowThresholdDQM_PSet.TkMapName     = 'LowThresholdTkMap.svg'
-process.CondDataMonitoring.SiStripLowThresholdDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripLowThresholdDQM_PSet.maxValue     = 10.
-
-process.CondDataMonitoring.SiStripHighThresholdDQM_PSet.TkMap_On     = True
-process.CondDataMonitoring.SiStripHighThresholdDQM_PSet.TkMapName     = 'HighThresholdTkMap.svg'
-process.CondDataMonitoring.SiStripHighThresholdDQM_PSet.minValue     = 0.
-process.CondDataMonitoring.SiStripHighThresholdDQM_PSet.maxValue     = 10.
 
 
 ## ---
 
 
-#process.p = cms.Path(process.CondDataMonitoring*process.qTester)
-process.p = cms.Path(process.CondDataMonitoring)
+process.p = cms.Path(process.CondDataMonitoring*process.qTester)
+#process.p = cms.Path(process.CondDataMonitoring)
