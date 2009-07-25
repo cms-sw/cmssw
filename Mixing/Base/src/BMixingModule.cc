614273ae4e4148dcd9acbff9a5fa11a96d9f720f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-07-25 02:20:00 +0000

    GetReleaseVersion.h was moved from FWCore/Utilities to FWCore/Version

--- Mixing/Base/src/BMixingModule.cc
+++ Mixing/Base/src/BMixingModule.cc
@@ -6,7 +6,7 @@
 
 #include "Mixing/Base/interface/BMixingModule.h"
 #include "FWCore/Utilities/interface/GetPassID.h"
-#include "FWCore/Utilities/interface/GetReleaseVersion.h"
+#include "FWCore/Version/interface/GetReleaseVersion.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
