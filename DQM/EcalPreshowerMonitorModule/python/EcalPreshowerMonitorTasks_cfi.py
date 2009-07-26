0162f64e25b14fe925f9ea7b4658b63fbd075980
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-07-26 14:27:57 +0000

    add ESFEDIntegrityTask_cfi

--- DQM/EcalPreshowerMonitorModule/python/EcalPreshowerMonitorTasks_cfi.py
+++ DQM/EcalPreshowerMonitorModule/python/EcalPreshowerMonitorTasks_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.EcalPreshowerMonitorModule.ESRawDataTask_cfi import *
 from DQM.EcalPreshowerMonitorModule.ESIntegrityTask_cfi import *
+#from DQM.EcalPreshowerMonitorModule.ESFEDIntegrityTask_cfi import *
 #from DQM.EcalPreshowerMonitorModule.ESPedestalTask_cfi import *
 from DQM.EcalPreshowerMonitorModule.ESOccupancyTask_cfi import *
 
