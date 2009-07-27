2e6e37de0cbf696d52a274dc824076ae294514d2
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-07-27 13:36:04 +0000

    reader

---
+++ CondCore/ESSources/test/stubs/TestUpdater.cc
@@ -0,0 +1,55 @@
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include <vector>
+
+typedef std::vector<int> Payload;
+
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+namespace condtest {
+
+  class TestUpdater : public edm::EDAnalyzer {
+  public:
+    explicit TestUpdater(edm::ParameterSet const&);
+    virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
+    virtual void beginRun(const edm::Run&, const edm::EventSetup&);
+    virtual void endRun(const edm::Run& r, const edm::EventSetup&);
+
+    static void update(int run);
+    
+    int evCount;
+
+  };
+
+
+  TestUpdater::TestUpdater(edm::ParameterSet const&) : evCount(0){}
+  void TestUpdater::beginRun(const edm::Run&, const edm::EventSetup&) {
+    evCount=0;
+  }
+
+  void TestUpdater::analyze(const edm::Event& e, const edm::EventSetup& c) {
+    ++evCount;
+
+    if (evCount==3) update(e.id().run()+1);
+    
+
+    edm::ESHandle<std::vector<int> > h;
+    c.get<OneIntRcd>().get(h);
+    int number = (*h.product()).front();
+  }
+
+  void TestUpdater::update(int run) {
+
+    // write run in db
+
+  }
+
+
+  DEFINE_FWK_MODULE(TestUpdater);
+}
