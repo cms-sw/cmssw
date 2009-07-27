ce7bf50ea6cc8fa86becdb1345c4216bae9d4b6e
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2009-07-27 20:07:30 +0000

    typo fixed

--- DPGAnalysis/SiStripTools/python/largesistripclusterevents_cfi.py
+++ DPGAnalysis/SiStripTools/python/largesistripclusterevents_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-largeSiStripClusterEvents = cms.EDFilter('LargeSiStripDigiEvents',
+largeSiStripClusterEvents = cms.EDFilter('LargeSiStripClusterEvents',
                                          collectionName = cms.InputTag("siStripClusters"),
                                          absoluteThreshold = cms.untracked.int32(300),
                                          moduleThreshold = cms.untracked.int32(20)
