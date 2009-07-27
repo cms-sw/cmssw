ec8d72f2c0860b5faa81a4330b89fc8846c66067
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-07-27 12:45:09 +0000

    put back (and extend) input check in setParameters

--- DataFormats/LaserAlignment/src/TkFittedLasBeam.cc
+++ DataFormats/LaserAlignment/src/TkFittedLasBeam.cc
@@ -26,13 +26,19 @@ void TkFittedLasBeam::setParameters(unsigned int parametrisation,
   chi2_ = chi2;
 
   // check integrity
-//   if (parameters_.size() != static_cast<unsigned int>(derivatives_.num_col()) || firstFixedParameter_ > parameters_.size()
-//       || static_cast<unsigned int>(paramCovariance_.num_row()) != firstFixedParameter_) {
-//     throw cms::Exception("BadInput")
-//       << "[TkFittedLasBeam::setParameters]  with inconsistent sizes: " 
-//       << parameters_.size() << " parameters, " << derivatives_.num_col()
-//       << " derivatives, " << " firstFixed = " << firstFixedParameter_ 
-//       << " cov. matrix size " << paramCovariance_.num_row() <<  ".";
-//   }
+  if (parameters_.size() != static_cast<unsigned int>(derivatives_.num_col()) // # parameters
+      || static_cast<unsigned int>(derivatives_.num_row()) != this->getData().size() // # hits
+      || firstFixedParameter_ > parameters_.size() // index 'fixed' might be the
+      || static_cast<unsigned int>(paramCovariance_.num_row()) != firstFixedParameter_) {
+    throw cms::Exception("BadInput")
+      << "[TkFittedLasBeam::setParameters] with inconsistent sizes: (parametrisation " 
+      << parametrisation << "):\n"
+      << parameters_.size() << " parameters,\n"
+      << derivatives_.num_row() << "x" << derivatives_.num_col() << " derivatives,\n"
+      << "firstFixed = " << firstFixedParameter_ << " (i.e. "
+      << static_cast<int>(parameters_.size()) - static_cast<int>(firstFixedParameter_)
+      << " global parameters),\n" // cast on line before to allow difference to be < 0, [un]signed!
+      << "cov. matrix size " << paramCovariance_.num_row() <<  ".\n";
+  }
 }
 
