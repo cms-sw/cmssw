fe83e2fda64067232a0c99e0c960f0b7c650e470
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2009-07-27 17:20:52 +0000

    new config variables and local mode for dbs_discovery.py

--- RecoEgamma/Examples/test/dbs_discovery.py
+++ RecoEgamma/Examples/test/dbs_discovery.py
@@ -5,26 +5,27 @@ import httplib, urllib, urllib2, types, string, os, sys
 # return the list of files obtained from the data discovery and based upon environnement variables:
 # DBS_RELEASE, for example CMSSW_2_2_0_pre1
 # DBS_SAMPLE, for example RelValSingleElectronPt35
-# DBS_LIKE , for example *RECO
+# DBS_COND , for example MC_31X_V2-v1
+# DBS_TIER , for example RECO
 
 def search():
 
   if os.environ['DBS_RELEASE'] == "LOCAL":
-    if os.environ['DBS_SAMPLE'] == "RelValSingleElectronPt10":
-      result = ['file:../../Configuration/test/SingleElectronPt10Reco.root']
-    elif os.environ['DBS_SAMPLE'] == "RelValSingleElectronPt35":
-      result = ['file:../../Configuration/test/SingleElectronPt35Reco.root']
-    elif os.environ['DBS_SAMPLE'] == "RelValQCD_Pt_80_120":
-      result = ['file:../../Configuration/test/QcdPt80-120Reco.root']
-    else:
-      result = ['NONE']
+    result = []
+    for line in  open('dbs_discovery.txt').readlines():
+      line = line.strip()
+      if line == "": continue
+      if line.find(os.environ['DBS_SAMPLE'])== -1: continue
+      if line.find(os.environ['DBS_COND'])== -1: continue
+      if line.find(os.environ['DBS_TIER'])== -1: continue
+      result.append('file:'+line)
   else:
     url = "https://cmsweb.cern.ch:443/dbs_discovery/aSearch"
     if os.environ['DBS_RELEASE'] != "Any":
       input = "find file where release = " + os.environ['DBS_RELEASE']
     if os.environ['DBS_SAMPLE'] != "Any":
       input = input + " and primds = " + os.environ['DBS_SAMPLE']
-    input = input + " and dataset like " + os.environ['DBS_LIKE']
+    input = input + " and dataset like *" + os.environ['DBS_COND'] + "*" + os.environ['DBS_TIER'] + "*"
     final_input = urllib.quote(input) ;
 
     agent   = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
