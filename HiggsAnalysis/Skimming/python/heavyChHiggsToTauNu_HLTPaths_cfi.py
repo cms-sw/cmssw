707e9e3cd622815f27b99ccc548795af6d206339
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2009-07-28 12:15:02 +0000

    Updated the trigger bit

--- HiggsAnalysis/Skimming/python/heavyChHiggsToTauNu_HLTPaths_cfi.py
+++ HiggsAnalysis/Skimming/python/heavyChHiggsToTauNu_HLTPaths_cfi.py
@@ -3,5 +3,9 @@ import FWCore.ParameterSet.Config as cms
 import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 heavyChHiggsToTauNuHLTFilter = copy.deepcopy(hltHighLevel)
-heavyChHiggsToTauNuHLTFilter.HLTPaths = ['HLT_IsoTau_MET65_Trk20']
-
+#CMSSW_2_2_X:
+#heavyChHiggsToTauNuHLTFilter.HLTPaths = ['HLT_IsoTau_MET65_Trk20']
+#8E29:
+#heavyChHiggsToTauNuHLTFilter.HLTPaths = ['HLT_SingleLooseIsoTau20']
+#1E31:
+heavyChHiggsToTauNuHLTFilter.HLTPaths = ['HLT_SingleIsoTau30_Trk5']
