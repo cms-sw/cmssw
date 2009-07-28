68949325dcdd4b8b5b5d3662dd4d2c8f96757b48
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2009-07-28 07:26:10 +0000

    make eta range consistent with MultiTrack

--- Validation/RecoMuon/python/RecoMuonValidator_cfi.py
+++ Validation/RecoMuon/python/RecoMuonValidator_cfi.py
@@ -31,10 +31,10 @@ recoMuonValidator = cms.EDAnalyzer("RecoMuonValidator",
     minPt = cms.untracked.double(0.0),
     maxPt = cms.untracked.double(200.0),
 
-    doAbsEta = cms.untracked.bool(True),
-    nBinEta = cms.untracked.uint32(25),
-    minEta = cms.untracked.double(0.0),
-    maxEta = cms.untracked.double(2.4),
+    doAbsEta = cms.untracked.bool(False),
+    nBinEta = cms.untracked.uint32(50),
+    minEta = cms.untracked.double(-2.5),
+    maxEta = cms.untracked.double(2.5),
 
     nBinPhi = cms.untracked.uint32(25),
 
