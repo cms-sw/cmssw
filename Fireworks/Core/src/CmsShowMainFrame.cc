36b17a030b1ba7d9fc77a5b9b3e215bef6a9db4e
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2009-07-28 16:15:56 +0000

    made time-stamp field bigger to have enough room for full expression

--- Fireworks/Core/src/CmsShowMainFrame.cc
+++ Fireworks/Core/src/CmsShowMainFrame.cc
@@ -9,7 +9,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Thu May 29 20:58:23 CDT 2008
-// $Id: CmsShowMainFrame.cc,v 1.52 2009/06/06 12:59:29 amraktad Exp $
+// $Id: CmsShowMainFrame.cc,v 1.53 2009/07/02 19:09:00 amraktad Exp $
 //
 // hacks
 #define private public
@@ -332,7 +332,7 @@ CmsShowMainFrame::CmsShowMainFrame(const TGWindow *p,UInt_t w,UInt_t h,FWGUIMana
    fullbar->AddFrame(texts, new TGLayoutHints(kLHintsNormal| kLHintsCenterY, 20, 5, 5, 5));
 
    /**************************************************************************/
-   TGVerticalFrame *texts2 = new TGVerticalFrame(fullbar, 150, 44, kFixedSize, backgroundColor);
+   TGVerticalFrame *texts2 = new TGVerticalFrame(fullbar, 200, 44, kFixedSize, backgroundColor);
 
    // Lumi
    m_lumiBlock = new TGLabel(texts2, "Lumi block id: ");
