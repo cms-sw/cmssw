f3694da1d49c1b3bbd05e75d04be5af7c12db729
Darren Michael Puigh <sha1-c46d72cb3e01124e714a162bae3295f66e002006@cern.ch>
2009-07-28 08:37:23 +0000

    Includes masks controlled in cfg file

--- DQM/L1TMonitorClient/python/L1TEventInfoClient_cfi.py
+++ DQM/L1TMonitorClient/python/L1TEventInfoClient_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 l1tEventInfoClient = cms.EDFilter("L1TEventInfoClient",
     prescaleLS = cms.untracked.int32(-1),
     monitorDir = cms.untracked.string(''),
-    prescaleEvt = cms.untracked.int32(1)
+    prescaleEvt = cms.untracked.int32(1),
+    dataMaskedSystems = cms.untracked.vstring("empty"),
+    emulMaskedSystems = cms.untracked.vstring("empty")
 )
 
 
