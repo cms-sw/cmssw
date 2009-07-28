615d5d2cf3a4018c72186d9d5fa31ef7817a1035
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-07-28 12:14:27 +0000

    add vector size checks to ctor and correct index getting for orca corrections

--- CondFormats/L1TObjects/src/L1GctJetFinderParams.cc
+++ CondFormats/L1TObjects/src/L1GctJetFinderParams.cc
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include <math.h>
 
+#include "FWCore/Utilities/interface/Exception.h"
+
 #include "DataFormats/L1GlobalCaloTrigger/interface/L1GctStaticParameters.h"
 
 
@@ -55,7 +57,34 @@ L1GctJetFinderParams::L1GctJetFinderParams(double rgnEtLsb,
   tauCorrCoeffs_(tauCorrCoeffs),
   convertToEnergy_(convertToEnergy),
   energyConversionCoeffs_(energyConvCoeffs)
-{ }
+{ 
+  // check number of eta bins
+  if (jetCorrCoeffs_.size() != NUMBER_ETA_VALUES ||
+      tauCorrCoeffs_.size() != N_CENTRAL_ETA_VALUES ||
+      energyConversionCoeffs_.size() != NUMBER_ETA_VALUES) {
+    throw cms::Exception("InconsistentConfig") << "L1GctJetFinderParams constructed with inconsistent data. Jet corrections has wrong number of eta bins!" << std::endl;
+  }
+
+  // check number of coefficients
+  if (corrType_ != 1) {
+    std::vector< std::vector<double> >::const_iterator itr;
+    unsigned nCoeffs=0;
+    if (corrType_ == 2) nCoeffs=4;  // ORCA style corrections need 4 coefficients
+    if (corrType_ == 3) nCoeffs=11;  // piecewise-cubic corrections need 11 coefficients?!?
+      
+    for (itr=jetCorrCoeffs_.begin(); itr!=jetCorrCoeffs_.end(); ++itr) {
+      if (itr->size() != nCoeffs) {
+	throw cms::Exception("InconsistentConfig") << "L1GctJetFinderParams constructed with inconsistent data. Wrong number of correction coefficients!" << std::endl;
+      }
+    }
+    for (itr=tauCorrCoeffs_.begin(); itr!=tauCorrCoeffs_.end(); ++itr) {
+      if (itr->size() != nCoeffs) {
+	throw cms::Exception("InconsistentConfig") << "L1GctJetFinderParams constructed with inconsistent data. Wrong number of correction coefficients!" << std::endl;
+      }
+    }
+  }
+  
+}
 
 
 L1GctJetFinderParams::~L1GctJetFinderParams() {}
@@ -217,21 +246,19 @@ double L1GctJetFinderParams::powerSeriesCorrect(const double Et, const std::vect
 double L1GctJetFinderParams::orcaStyleCorrect(const double Et, const std::vector<double>& coeffs) const
 {
   // The coefficients are arranged in groups of four. The first in each group is a threshold value of Et.
-  std::vector<double>::const_iterator next_coeff=coeffs.begin();
-  while (next_coeff != coeffs.end()) {
-    double threshold = *next_coeff++;
-    double A = *next_coeff++;
-    double B = *next_coeff++;
-    double C = *next_coeff++;
-    if (Et>threshold) {
-      // This function is an inverse quadratic:
-      //   (input Et) = A + B*(output Et) + C*(output Et)^2
-      return 2*(Et-A)/(B+sqrt(B*B-4*A*C+4*Et*C));
-    }
-    // If we are below all specified thresholds (or the vector is empty), return output=input.
+  double threshold = coeffs.at(0);
+  double A = coeffs.at(1);
+  double B = coeffs.at(2);
+  double C = coeffs.at(3);
+  if (Et>threshold) {
+    // This function is an inverse quadratic:
+    //   (input Et) = A + B*(output Et) + C*(output Et)^2
+    return 2*(Et-A)/(B+sqrt(B*B-4*A*C+4*Et*C));
   }
+  // If we are below all specified thresholds (or the vector is empty), return output=input.
   return Et;
 }
+
 
 double L1GctJetFinderParams::piecewiseCubicCorrect(const double Et, const std::vector<double>& coeffs) const
 {
