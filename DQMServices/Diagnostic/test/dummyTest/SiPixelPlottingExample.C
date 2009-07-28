9453e25214d3524cd072fbaac8b86c253a6d9493
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-07-28 14:27:39 +0000

    update to be in line with current DQM file

--- DQMServices/Diagnostic/test/dummyTest/SiPixelPlottingExample.C
+++ DQMServices/Diagnostic/test/dummyTest/SiPixelPlottingExample.C
@@ -20,15 +20,15 @@
 
   HDQMInspectorConfigSiPixel PixelConfig;
   HDQMInspector A;
-  A.setDB("sqlite_file:dbfile.db","HDQM_Pixel","cms_cond_strip","w3807dev","");
+  A.setDB("sqlite_file:dbfile.db","HDQM_test","cms_cond_strip","w3807dev","");
 
 
   A.setDebug(1);
   A.setDoStat(1);
 
 
-  A.createTrendLastRuns("1@SUMOFF_ClustX@yMean", "1ClusterXsize_yMean.gif", 0, "", NRuns);
-  A.createTrendLastRuns("1@SUMOFF_adc@yMean", "adc_yMean.gif", 0, "", NRuns);
+  A.createTrend("1@SUMOFF_adc@yMean", "adc_yMean.gif", 0, "", NRuns);
+  A.createTrend("1@SUMOFF_charge_OffTrack@yMean", "charge_OffTrack_yMean.gif", 0, "", NRuns);
 
   A.closeFile();
 
