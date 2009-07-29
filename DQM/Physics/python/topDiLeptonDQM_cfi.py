16aa2b59172a1311a2bcbe5fe64f0444a67e74e9
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-07-29 21:01:29 +0000

    fix module folder

--- DQM/Physics/python/topDiLeptonDQM_cfi.py
+++ DQM/Physics/python/topDiLeptonDQM_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 topDiLeptonDQM = cms.EDAnalyzer("TopDiLeptonDQM",
 
-    moduleName     = cms.untracked.string('Top/DiLepton'),
+    moduleName     = cms.untracked.string('Physics/Top/DiLepton'),
     TriggerResults = cms.InputTag('TriggerResults','','HLT'),
     hltPaths_L3    = cms.vstring('HLT_Mu9','HLT_Mu15','HLT_IsoMu9','HLT_DoubleMu3','HLT_Ele15_SW_L1R','HLT_Ele20_SW_L1R'),
     hltPaths_L3_mu = cms.vstring('HLT_Mu9','HLT_Mu15','HLT_IsoMu9','HLT_DoubleMu3'),
