02659301ff82472fb2ed7c2ffa8de1305e105a37
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-07-29 13:28:00 +0000

    At least make the dummy work for stips and work from there...

--- DQMServices/Diagnostic/test/dummyTest/test_Inspector.cc
+++ DQMServices/Diagnostic/test/dummyTest/test_Inspector.cc
@@ -27,15 +27,15 @@ A.setDoStat(1);
 
 
 //createTrend(std::string ListItems, std::string CanvasName="", int logy=0,std::string Conditions="", unsigned int firstRun=1, unsigned int lastRun=0xFFFFFFFE);
-A.createTrend("268435456@Chi2_CKFTk@entries", "number_of_tracks.gif",1);//,"268435456@NumberOfTracks_CKFTk@entries>10000&&268435456@NumberOfRecHitsPerTrack_CKFTk@entries>0");
+//A.createTrend("268435456@Chi2_CKFTk@entries", "number_of_tracks.gif",1);//,"268435456@NumberOfTracks_CKFTk@entries>10000&&268435456@NumberOfRecHitsPerTrack_CKFTk@entries>0");
 
 
 
 A.createTrend("
-369098752@Summary_TotalNumberOfClusters_OnTrack@mean,369098752@Summary_ClusterChargeCorr_OnTrack@landauPeak,
-436207616@Summary_TotalNumberOfClusters_OnTrack@mean,436207616@Summary_ClusterChargeCorr_OnTrack@landauPeak,
-402653184@Summary_TotalNumberOfClusters_OnTrack@mean,402653184@Summary_ClusterChargeCorr_OnTrack@landauPeak,
-469762048@Summary_TotalNumberOfClusters_OnTrack@mean,469762048@Summary_ClusterChargeCorr_OnTrack@landauPeak",
+369098752@Summary_TotalNumberOfClusters_OnTrack@mean,
+436207616@Summary_TotalNumberOfClusters_OnTrack@mean,
+402653184@Summary_TotalNumberOfClusters_OnTrack@mean,
+469762048@Summary_TotalNumberOfClusters_OnTrack@mean",
 	      "OnTrackClusters.gif",1);//,"268435456@NumberOfTracks_CKFTk@entries>10000&&268435456@NumberOfRecHitsPerTrack_CKFTk@entries>0");
 
 
