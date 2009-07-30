3fe177c8ea72ec4393f9f37dc965c0ea10efbb4d
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2009-07-30 08:33:54 +0000

    update for new options

--- CalibCalorimetry/EcalLaserSorting/python/LmfSource_cfi.py
+++ CalibCalorimetry/EcalLaserSorting/python/LmfSource_cfi.py
@@ -4,5 +4,8 @@ source = cms.Source("LmfSource",
   fileNames = cms.vstring("in.lmf"),
   preScale  = cms.uint32(1),
   orderedRead = cms.bool(True),
+  watchFileList = cms.bool(False),
+  fileListName = cms.string("fileList.txt"),
+  nSecondsToSleep = cms.int32(5),
   verbosity = cms.untracked.int32(0)
 )
