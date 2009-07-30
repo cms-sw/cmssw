0ee17125329a03003077ae338c141f9347f8b0ec
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-07-30 06:18:56 +0000

    Add file for 3_1_X

---
+++ TopQuarkAnalysis/TopPairBSM/python/BooTopPatTuple.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+#
+# pat tuple sequence for fullsim
+#
+from PhysicsTools.PatAlgos.patSequences_cff import *
+
+## std sequence for patTuple production
+BooTopPatTuple = cms.Sequence(patDefaultSequence)
+
