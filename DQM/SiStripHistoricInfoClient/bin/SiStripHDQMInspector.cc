37abfdd2a23879fcbbea706d7ffc871e2563e73d
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-07-30 12:47:55 +0000

    getting plotting for strips going... naming on plot labels should work now

--- DQM/SiStripHistoricInfoClient/bin/SiStripHDQMInspector.cc
+++ DQM/SiStripHistoricInfoClient/bin/SiStripHDQMInspector.cc
@@ -20,9 +20,10 @@ void SiStripHDQMInspector (const std::string & tagName, int const NRuns) {
   //AutoLibraryLoader::enable();
 
   HDQMInspectorConfigSiStrip StripConfig;
-  //HDQMInspector A(&StripConfig);
-  HDQMInspector A;
-  A.setDB("sqlite_file:dbfile.db",tagName,"cms_cond_strip","w3807dev","");
+  HDQMInspector A(&StripConfig);
+  //HDQMInspector A;
+  //A.setDB("sqlite_file:dbfile.db",tagName,"cms_cond_strip","w3807dev","");
+  A.setDB("oracle://cms_orcoff_prep/CMS_COND_STRIP",tagName,"cms_cond_strip","WCYE6II08K530GPK","");
 
 
   A.setDebug(1);
@@ -31,7 +32,36 @@ void SiStripHDQMInspector (const std::string & tagName, int const NRuns) {
   //A.setBlackList("68286");
 
   A.createTrendLastRuns("369098752@Summary_TotalNumberOfClusters_OnTrack@mean,436207616@Summary_TotalNumberOfClusters_OnTrack@mean,402653184@Summary_TotalNumberOfClusters_OnTrack@mean,469762048@Summary_TotalNumberOfClusters_OnTrack@mean", "OnTrackClusters.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_TotalNumberOfClusters_OffTrack@mean,436207616@Summary_TotalNumberOfClusters_OffTrack@mean,402653184@Summary_TotalNumberOfClusters_OffTrack@mean,469762048@Summary_TotalNumberOfClusters_OffTrack@mean", "TotalNumberOfClusters_OffTrack.gif", 0, "", NRuns);
 
+
+
+
+
+  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr_OnTrack@mean,436207616@Summary_ClusterChargeCorr_OnTrack@mean,402653184@Summary_ClusterChargeCorr_OnTrack@mean,469762048@Summary_ClusterChargeCorr_OnTrack@mean", "ClusterChargeCorr_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterCharge_OffTrack@mean,436207616@Summary_ClusterCharge_OffTrack@mean,402653184@Summary_ClusterCharge_OffTrack@mean,469762048@Summary_ClusterCharge_OffTrack@mean", "ClusterCharge_OffTrack.gif", 0, "", NRuns);
+
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OnTrack@mean,436207616@Summary_ClusterNoise_OnTrack@mean,402653184@Summary_ClusterNoise_OnTrack@mean,469762048@Summary_ClusterNoise_OnTrack@mean", "ClusterNoise_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OffTrack@mean,436207616@Summary_ClusterNoise_OffTrack@mean,402653184@Summary_ClusterNoise_OffTrack@mean,469762048@Summary_ClusterNoise_OffTrack@mean", "ClusterNoise_OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr_OnTrack@mean,436207616@Summary_ClusterStoNCorr_OnTrack@mean,402653184@Summary_ClusterStoNCorr_OnTrack@mean,469762048@Summary_ClusterStoNCorr_OnTrack@mean", "ClusterStoNCorr_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoN_OffTrack@mean,436207616@Summary_ClusterStoN_OffTrack@mean,402653184@Summary_ClusterStoN_OffTrack@mean,469762048@Summary_ClusterStoN_OffTrack@mean", "ClusterStoN_OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterWidth_OnTrack@mean,436207616@Summary_ClusterWidth_OnTrack@mean,402653184@Summary_ClusterWidth_OnTrack@mean,469762048@Summary_ClusterWidth_OnTrack@mean", "ClusterWidth_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterWidth_OffTrack@mean,436207616@Summary_ClusterWidth_OffTrack@mean,402653184@Summary_ClusterWidth_OffTrack@mean,469762048@Summary_ClusterWidth_OffTrack@mean", "ClusterWidth_OffTrack.gif", 0, "", NRuns);
+
+
+  A.createTrendLastRuns("369098752@Summary_TotalNumberOfDigis@mean,436207616@Summary_TotalNumberOfDigis@mean,402653184@Summary_TotalNumberOfDigis@mean,469762048@Summary_TotalNumberOfDigis@mean", "TotalNumberOfDigis.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr__OnTrack@mean,436207616@Summary_ClusterChargeCorr__OnTrack@mean,402653184@Summary_ClusterChargeCorr__OnTrack@mean,469762048@Summary_ClusterChargeCorr__OnTrack@mean", "_ClusterChargeCorr__OnTrack.gif", 0, "", NRuns);
+
+  /*
+  A.createTrendLastRuns("369098752@Summary_ClusterCharge__OffTrack@mean,436207616@Summary_ClusterCharge__OffTrack@mean,402653184@Summary_ClusterCharge__OffTrack@mean,469762048@Summary_ClusterCharge__OffTrack@mean", "ClusterCharge__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OnTrack@mean,436207616@Summary_ClusterNoise__OnTrack@mean,402653184@Summary_ClusterNoise__OnTrack@mean,469762048@Summary_ClusterNoise__OnTrack@mean", "ClusterNoise__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OffTrack@mean,436207616@Summary_ClusterNoise__OffTrack@mean,402653184@Summary_ClusterNoise__OffTrack@mean,469762048@Summary_ClusterNoise__OffTrack@mean", "ClusterNoise__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr__OnTrack@mean,436207616@Summary_ClusterStoNCorr__OnTrack@mean,402653184@Summary_ClusterStoNCorr__OnTrack@mean,469762048@Summary_ClusterStoNCorr__OnTrack@mean", "ClusterStoNCorr__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoN__OffTrack@mean,436207616@Summary_ClusterStoN__OffTrack@mean,402653184@Summary_ClusterStoN__OffTrack@mean,469762048@Summary_ClusterStoN__OffTrack@mean", "ClusterStoN__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterWidth__OnTrack@mean,436207616@Summary_ClusterWidth__OnTrack@mean,402653184@Summary_ClusterWidth__OnTrack@mean,469762048@Summary_ClusterWidth__OnTrack@mean", "ClusterWidth__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterWidth__OffTrack@mean,436207616@Summary_ClusterWidth__OffTrack@mean,402653184@Summary_ClusterWidth__OffTrack@mean,469762048@Summary_ClusterWidth__OffTrack@mean", "ClusterWidth__OffTrack.gif", 0, "", NRuns);
+  */
+  //A.createTrendLastRuns("369098752@XXX@mean,436207616@XXX@mean,402653184@XXX@mean,469762048@XXX@mean", "OnTrackClusters.gif", 0, "", NRuns);
 
   A.closeFile();
 
@@ -62,7 +92,8 @@ void SiStripHDQMInspector (const std::string &tagName, int const Start, int cons
   HDQMInspectorConfigSiStrip StripConfig;
   //HDQMInspector A(&StripConfig);
   HDQMInspector A(&StripConfig);
-  A.setDB("sqlite_file:dbfile.db",tagName,"cms_cond_strip","w3807dev","");
+  //A.setDB("sqlite_file:dbfile.db",tagName,"cms_cond_strip","w3807dev","");
+  A.setDB("oracle://cms_orcoff_prep/CMS_COND_STRIP",tagName,"cms_cond_strip","WCYE6II08K530GPK","");
 
 
   A.setDebug(1);
