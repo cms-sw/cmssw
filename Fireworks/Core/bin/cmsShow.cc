92c7fb11b8c8f6593dc1c4b4bff0c56cff59f5a8
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-07-30 16:10:13 +0000

    Emergency work around to keep the message logger from freezing the application when its message buffer fills up

--- Fireworks/Core/bin/cmsShow.cc
+++ Fireworks/Core/bin/cmsShow.cc
@@ -12,8 +12,60 @@
 #include <string.h>
 #include <memory>
 
+/* NOTE: This is a short term work around until FWLite can properly handle the MessageLogger
+ */
+#include "FWCore/MessageLogger/interface/AbstractMLscribe.h"
+#include "FWCore/MessageLogger/interface/ErrorObj.h"
+#include "FWCore/MessageLogger/interface/MessageLoggerQ.h"
+#include "FWCore/MessageLogger/interface/MessageDrop.h"
+
+namespace {
+   class SilentMLscribe : public edm::service::AbstractMLscribe {
+      
+   public:
+      SilentMLscribe() {}
+            
+      // ---------- member functions ---------------------------
+      virtual
+      void  runCommand(edm::MessageLoggerQ::OpCode  opcode, void * operand);
+      
+   private:
+      SilentMLscribe(const SilentMLscribe&); // stop default
+      
+      const SilentMLscribe& operator=(const SilentMLscribe&); // stop default
+      
+      // ---------- member data --------------------------------
+      
+   };      
+   
+   void  
+   SilentMLscribe::runCommand(edm::MessageLoggerQ::OpCode  opcode, void * operand) {
+      //even though we don't print, have to clean up memory
+      switch (opcode) {
+         case edm::MessageLoggerQ::LOG_A_MESSAGE: {
+            edm::ErrorObj *  errorobj_p = static_cast<edm::ErrorObj *>(operand);
+            delete errorobj_p;
+            break;
+         }
+         case edm::MessageLoggerQ::JOBREPORT:
+         case edm::MessageLoggerQ::JOBMODE:
+         case edm::MessageLoggerQ::GROUP_STATS:
+            std::string* string_p = static_cast<std::string*> (operand);
+            delete string_p;
+            break;
+         default:
+            break;
+      }
+   }   
+}
+
 void run_app(TApplication &app, int argc, char **argv)
 {
+   //Remove when FWLite handles the MessageLogger
+   SilentMLscribe scribe;
+   edm::MessageLoggerQ::setMLscribe_ptr(&scribe);
+   edm::MessageDrop::instance()->messageLoggerScribeIsRunning = edm::MLSCRIBE_RUNNING_INDICATOR;
+   //---------------------
    AutoLibraryLoader::enable();
    std::auto_ptr<CmsShowMain> pMain( new CmsShowMain(argc,argv) );
    app.Run();
@@ -21,6 +73,9 @@ void run_app(TApplication &app, int argc, char **argv)
 
    TEveManager::Terminate();
    app.Terminate();
+
+   //Remove when FWLite handled the MessageLogger
+   edm::MessageLoggerQ::MLqEND();
 }
 
 int main (int argc, char **argv)
