7add8882812466579c82d72174b4b52233c65394
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2009-07-31 11:39:15 +0000

    Improved printing for L1 GT trigger masks.

--- CondFormats/L1TObjects/src/L1GtTriggerMask.cc
+++ CondFormats/L1TObjects/src/L1GtTriggerMask.cc
@@ -18,6 +18,8 @@
 #include "CondFormats/L1TObjects/interface/L1GtTriggerMask.h"
 
 // system include files
+#include <iostream>
+#include <iomanip>
 
 // user include files
 //   base class
@@ -48,12 +50,21 @@ void L1GtTriggerMask::setGtTriggerMask(std::vector<unsigned int>& maskValue)
 }
 
 // print the mask
-void L1GtTriggerMask::print(std::ostream& s) const
-{
-    s << "\nL1 GT Trigger mask" << std::endl;
+void L1GtTriggerMask::print(std::ostream& outputStream) const {
+    outputStream << "\nL1 GT Trigger masks are printed for all L1 partitions. "
+            << "\n  Partition numbering: partition \"i\" -> bit i"
+            << " (bit 0 is LSB)\n"
+            << "\n If mask value is 1 for a given algorithm/technical trigger in a given partition "
+            << "\n then the algorithm/technical trigger is masked (has value 0 = false) in the evaluation "
+            << "\n of FinalOR.\n"
+            << "\n For veto masks, if the mask is set to 1 and the result of the trigger for that bit is true, "
+            << "\n then the FinalOR is set to false (no L1A).\n"
+            << std::endl;
 
     for (unsigned i = 0; i < m_triggerMask.size(); i++) {
-        s << "  Bit number " << i << ":\t mask: " << m_triggerMask[i] << std::endl;
+        outputStream << "  Algorithm/technical trigger bit number "
+                << std::setw(3) << i << ":\t mask: 0x" << std::hex
+                << std::setw(2) << m_triggerMask[i] << std::dec << std::endl;
     }
 
 }
