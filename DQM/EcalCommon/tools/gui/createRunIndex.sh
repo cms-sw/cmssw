ca1b1e9b4b81e276492f451ae19b7e5872963ba3
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-07-31 16:17:43 +0000

    *** empty log message ***

--- DQM/EcalCommon/tools/gui/createRunIndex.sh
+++ DQM/EcalCommon/tools/gui/createRunIndex.sh
@@ -8,24 +8,24 @@ LD_LIBRARY_PATH=
 source sw/cmsset_default.sh
 source sw/slc4_ia32_gcc345/cms/dqmgui/5.0.1/etc/profile.d/env.sh
 
-if [ -e /tmp/updateRunIndex.lock ]; then
+if [ -e /tmp/createRunIndex.lock ]; then
   echo "Lock file is present, exit"
   exit 1
 fi
 
-touch /tmp/updateRunIndex.lock
+touch /tmp/createRunIndex.lock
 
 rm -fr /data/ecalod-disk01/dqm-GUI/idx
 
 visDQMIndex create /data/ecalod-disk01/dqm-GUI/idx
 
-echo "Index update: begin"
+echo "Index create: begin"
 
 ./visDQMImport /data/ecalod-disk01/dqm-GUI/idx /data/ecalod-disk01/dqm-data/root
 
-echo "Index update: end"
+echo "Index create: end"
 
-rm /tmp/updateRunIndex.lock
+rm /tmp/createRunIndex.lock
 
 exit 0
 
