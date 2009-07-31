7ecdf80cf93e60c2c143fb05e357c74ad1545f8b
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-07-31 14:47:46 +0000

    add the mix in the sequence

--- Configuration/StandardSequences/python/CrossingFrameWriter_cff.py
+++ Configuration/StandardSequences/python/CrossingFrameWriter_cff.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from SimGeneral.MixingModule.cfwriter_cfi import *
 
-pcfw = cms.Sequence(cfWriter)
+pcfw = cms.Sequence(cms.SequencePlaceholder("mix")*cfWriter)
