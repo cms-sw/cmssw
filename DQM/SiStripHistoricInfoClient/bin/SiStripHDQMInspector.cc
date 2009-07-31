8ed4b762aeef2e67daa72632a535c541e07bdd61
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-07-31 15:00:06 +0000

    Correct names for what is in the DB

--- DQM/SiStripHistoricInfoClient/bin/SiStripHDQMInspector.cc
+++ DQM/SiStripHistoricInfoClient/bin/SiStripHDQMInspector.cc
@@ -1,5 +1,7 @@
 #include "DQMServices/Diagnostic/test/HDQMInspector.h"
 #include "DQM/SiStripHistoricInfoClient/test/HDQMInspectorConfigSiStrip.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include <string>
 
 void SiStripHDQMInspector (const std::string & tagName, int const NRuns) {
@@ -18,6 +20,9 @@ void SiStripHDQMInspector (const std::string & tagName, int const NRuns) {
   //pixelTranslator Translator;
 
   //AutoLibraryLoader::enable();
+
+
+
 
   HDQMInspectorConfigSiStrip StripConfig;
   HDQMInspector A(&StripConfig);
@@ -38,29 +43,27 @@ void SiStripHDQMInspector (const std::string & tagName, int const NRuns) {
 
 
 
-  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr_OnTrack@mean,436207616@Summary_ClusterChargeCorr_OnTrack@mean,402653184@Summary_ClusterChargeCorr_OnTrack@mean,469762048@Summary_ClusterChargeCorr_OnTrack@mean", "ClusterChargeCorr_OnTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterCharge_OffTrack@mean,436207616@Summary_ClusterCharge_OffTrack@mean,402653184@Summary_ClusterCharge_OffTrack@mean,469762048@Summary_ClusterCharge_OffTrack@mean", "ClusterCharge_OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr_OnTrack@userExample_XMax,436207616@Summary_ClusterChargeCorr_OnTrack@userExample_XMax,402653184@Summary_ClusterChargeCorr_OnTrack@userExample_XMax,469762048@Summary_ClusterChargeCorr_OnTrack@userExample_XMax", "ClusterChargeCorr_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterCharge_OffTrack@userExample_XMax,436207616@Summary_ClusterCharge_OffTrack@userExample_XMax,402653184@Summary_ClusterCharge_OffTrack@userExample_XMax,469762048@Summary_ClusterCharge_OffTrack@userExample_XMax", "ClusterCharge_OffTrack.gif", 0, "", NRuns);
 
-  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OnTrack@mean,436207616@Summary_ClusterNoise_OnTrack@mean,402653184@Summary_ClusterNoise_OnTrack@mean,469762048@Summary_ClusterNoise_OnTrack@mean", "ClusterNoise_OnTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OffTrack@mean,436207616@Summary_ClusterNoise_OffTrack@mean,402653184@Summary_ClusterNoise_OffTrack@mean,469762048@Summary_ClusterNoise_OffTrack@mean", "ClusterNoise_OffTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr_OnTrack@mean,436207616@Summary_ClusterStoNCorr_OnTrack@mean,402653184@Summary_ClusterStoNCorr_OnTrack@mean,469762048@Summary_ClusterStoNCorr_OnTrack@mean", "ClusterStoNCorr_OnTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterStoN_OffTrack@mean,436207616@Summary_ClusterStoN_OffTrack@mean,402653184@Summary_ClusterStoN_OffTrack@mean,469762048@Summary_ClusterStoN_OffTrack@mean", "ClusterStoN_OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OnTrack@userExample_XMax,436207616@Summary_ClusterNoise_OnTrack@userExample_XMax,402653184@Summary_ClusterNoise_OnTrack@userExample_XMax,469762048@Summary_ClusterNoise_OnTrack@userExample_XMax", "ClusterNoise_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise_OffTrack@userExample_XMax,436207616@Summary_ClusterNoise_OffTrack@userExample_XMax,402653184@Summary_ClusterNoise_OffTrack@userExample_XMax,469762048@Summary_ClusterNoise_OffTrack@userExample_XMax", "ClusterNoise_OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr_OnTrack@userExample_XMax,436207616@Summary_ClusterStoNCorr_OnTrack@userExample_XMax,402653184@Summary_ClusterStoNCorr_OnTrack@userExample_XMax,469762048@Summary_ClusterStoNCorr_OnTrack@userExample_XMax", "ClusterStoNCorr_OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoN_OffTrack@userExample_XMax,436207616@Summary_ClusterStoN_OffTrack@userExample_XMax,402653184@Summary_ClusterStoN_OffTrack@userExample_XMax,469762048@Summary_ClusterStoN_OffTrack@userExample_XMax", "ClusterStoN_OffTrack.gif", 0, "", NRuns);
   A.createTrendLastRuns("369098752@Summary_ClusterWidth_OnTrack@mean,436207616@Summary_ClusterWidth_OnTrack@mean,402653184@Summary_ClusterWidth_OnTrack@mean,469762048@Summary_ClusterWidth_OnTrack@mean", "ClusterWidth_OnTrack.gif", 0, "", NRuns);
   A.createTrendLastRuns("369098752@Summary_ClusterWidth_OffTrack@mean,436207616@Summary_ClusterWidth_OffTrack@mean,402653184@Summary_ClusterWidth_OffTrack@mean,469762048@Summary_ClusterWidth_OffTrack@mean", "ClusterWidth_OffTrack.gif", 0, "", NRuns);
 
 
-  A.createTrendLastRuns("369098752@Summary_TotalNumberOfDigis@mean,436207616@Summary_TotalNumberOfDigis@mean,402653184@Summary_TotalNumberOfDigis@mean,469762048@Summary_TotalNumberOfDigis@mean", "TotalNumberOfDigis.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr__OnTrack@mean,436207616@Summary_ClusterChargeCorr__OnTrack@mean,402653184@Summary_ClusterChargeCorr__OnTrack@mean,469762048@Summary_ClusterChargeCorr__OnTrack@mean", "_ClusterChargeCorr__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_TotalNumberOfDigis@userExample_XMax,436207616@Summary_TotalNumberOfDigis@userExample_XMax,402653184@Summary_TotalNumberOfDigis@userExample_XMax,469762048@Summary_TotalNumberOfDigis@userExample_XMax", "TotalNumberOfDigis.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterChargeCorr__OnTrack@userExample_XMax,436207616@Summary_ClusterChargeCorr__OnTrack@userExample_XMax,402653184@Summary_ClusterChargeCorr__OnTrack@userExample_XMax,469762048@Summary_ClusterChargeCorr__OnTrack@userExample_XMax", "_ClusterChargeCorr__OnTrack.gif", 0, "", NRuns);
 
-  /*
-  A.createTrendLastRuns("369098752@Summary_ClusterCharge__OffTrack@mean,436207616@Summary_ClusterCharge__OffTrack@mean,402653184@Summary_ClusterCharge__OffTrack@mean,469762048@Summary_ClusterCharge__OffTrack@mean", "ClusterCharge__OffTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OnTrack@mean,436207616@Summary_ClusterNoise__OnTrack@mean,402653184@Summary_ClusterNoise__OnTrack@mean,469762048@Summary_ClusterNoise__OnTrack@mean", "ClusterNoise__OnTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OffTrack@mean,436207616@Summary_ClusterNoise__OffTrack@mean,402653184@Summary_ClusterNoise__OffTrack@mean,469762048@Summary_ClusterNoise__OffTrack@mean", "ClusterNoise__OffTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr__OnTrack@mean,436207616@Summary_ClusterStoNCorr__OnTrack@mean,402653184@Summary_ClusterStoNCorr__OnTrack@mean,469762048@Summary_ClusterStoNCorr__OnTrack@mean", "ClusterStoNCorr__OnTrack.gif", 0, "", NRuns);
-  A.createTrendLastRuns("369098752@Summary_ClusterStoN__OffTrack@mean,436207616@Summary_ClusterStoN__OffTrack@mean,402653184@Summary_ClusterStoN__OffTrack@mean,469762048@Summary_ClusterStoN__OffTrack@mean", "ClusterStoN__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterCharge__OffTrack@userExample_XMax,436207616@Summary_ClusterCharge__OffTrack@userExample_XMax,402653184@Summary_ClusterCharge__OffTrack@userExample_XMax,469762048@Summary_ClusterCharge__OffTrack@userExample_XMax", "ClusterCharge__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OnTrack@userExample_XMax,436207616@Summary_ClusterNoise__OnTrack@userExample_XMax,402653184@Summary_ClusterNoise__OnTrack@userExample_XMax,469762048@Summary_ClusterNoise__OnTrack@userExample_XMax", "ClusterNoise__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterNoise__OffTrack@userExample_XMax,436207616@Summary_ClusterNoise__OffTrack@userExample_XMax,402653184@Summary_ClusterNoise__OffTrack@userExample_XMax,469762048@Summary_ClusterNoise__OffTrack@userExample_XMax", "ClusterNoise__OffTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoNCorr__OnTrack@userExample_XMax,436207616@Summary_ClusterStoNCorr__OnTrack@userExample_XMax,402653184@Summary_ClusterStoNCorr__OnTrack@userExample_XMax,469762048@Summary_ClusterStoNCorr__OnTrack@userExample_XMax", "ClusterStoNCorr__OnTrack.gif", 0, "", NRuns);
+  A.createTrendLastRuns("369098752@Summary_ClusterStoN__OffTrack@userExample_XMax,436207616@Summary_ClusterStoN__OffTrack@userExample_XMax,402653184@Summary_ClusterStoN__OffTrack@userExample_XMax,469762048@Summary_ClusterStoN__OffTrack@userExample_XMax", "ClusterStoN__OffTrack.gif", 0, "", NRuns);
   A.createTrendLastRuns("369098752@Summary_ClusterWidth__OnTrack@mean,436207616@Summary_ClusterWidth__OnTrack@mean,402653184@Summary_ClusterWidth__OnTrack@mean,469762048@Summary_ClusterWidth__OnTrack@mean", "ClusterWidth__OnTrack.gif", 0, "", NRuns);
   A.createTrendLastRuns("369098752@Summary_ClusterWidth__OffTrack@mean,436207616@Summary_ClusterWidth__OffTrack@mean,402653184@Summary_ClusterWidth__OffTrack@mean,469762048@Summary_ClusterWidth__OffTrack@mean", "ClusterWidth__OffTrack.gif", 0, "", NRuns);
-  */
   //A.createTrendLastRuns("369098752@XXX@mean,436207616@XXX@mean,402653184@XXX@mean,469762048@XXX@mean", "OnTrackClusters.gif", 0, "", NRuns);
 
   A.closeFile();
@@ -101,7 +104,7 @@ void SiStripHDQMInspector (const std::string &tagName, int const Start, int cons
 
   //A.setBlackList("68286");
 
-  A.createTrend("369098752@Summary_TotalNumberOfClusters_OnTrack@mean,436207616@Summary_TotalNumberOfClusters_OnTrack@mean,402653184@Summary_TotalNumberOfClusters_OnTrack@mean,469762048@Summary_TotalNumberOfClusters_OnTrack@mean", "OnTrackClusters.gif", 0, "", Start, End);
+  A.createTrend("369098752@Summary_ClusterChargeCorr__OnTrack@mean,436207616@Summary_ClusterChargeCorr__OnTrack@mean,402653184@Summary_ClusterChargeCorr__OnTrack@mean,469762048@Summary_ClusterChargeCorr__OnTrack@mean", "_ClusterChargeCorr__OnTrack.gif", 0, "", Start, End);
 
 
   A.closeFile();
