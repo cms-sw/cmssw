ed0621ef2b3a45105fbac5cc3f9ee36e36eeb65d
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-07-31 03:44:23 +0000

    Removing old config files.

--- RecoJets/JetProducers/python/IconeJetParameters_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# Standard Iterative Cone Jets parameters
-# $Id
-IconeJetParameters = cms.PSet(
-    seedThreshold = cms.double(1.0),
-    debugLevel = cms.untracked.int32(0)
-)
-
