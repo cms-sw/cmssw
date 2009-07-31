071d218c879ec599d560bcf01769c5286a6575e0
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-07-31 13:21:40 +0000

    add refresh script

---
+++ DQM/EcalCommon/tools/gui/refreshRunIndex.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+  echo 'Usage: refreshRunIndex.sh NUMBER-OF-FILES'
+  exit
+fi
+
+N=$1
+
+ulimit -c 0
+
+cd ${HOME}/work/cms/dqm-GUI
+
+LD_LIBRARY_PATH=
+source sw/cmsset_default.sh
+source sw/slc4_ia32_gcc345/cms/dqmgui/5.0.0/etc/profile.d/env.sh
+
+if [ -e /tmp/refreshRunIndex.lock ]; then
+  echo "Lock file is present, exit"
+  exit 1
+fi
+
+touch /tmp/refreshRunIndex.lock
+
+echo "Index refresh: begin"
+
+#find /data/ecalod-disk01/dqm-data/root/ -name 'DQM_V*.root' | xargs -r ls -tr | tail -$N | xargs -n 1 -r visDQMIndex -d add --dataset /Global/Online/ALL /data/ecalod-disk01/dqm-GUI/idx
+
+find ${HOME}/work/cms/CMSSW_3_2_0 -name 'DQM_V*.root' | xargs -r ls -tr | tail -$N | xargs -n 1 -r visDQMIndex -d add --dataset /Global/Online/ALL ${HOME}/work/cms/dqm-GUI/idx
+
+echo "Index refresh: end"
+
+rm /tmp/refreshRunIndex.lock
+
+exit 0
+
