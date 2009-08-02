ee10c810fbf74b9a517293ca1ecace8f3db5d3b7
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-08-02 16:25:15 +0000

    now pointing to a set of relvals

--- RecoParticleFlow/Configuration/test/analyzePFCandidates_cfg.py
+++ RecoParticleFlow/Configuration/test/analyzePFCandidates_cfg.py
@@ -2,17 +2,45 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("ANALYSIS")
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(100)
 )
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:aod.root')
-)
+
+
+process.source = cms.Source (
+    "PoolSource",    
+    fileNames = cms.untracked.vstring(
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_0.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_1.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_10.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_2.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_3.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_4.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_5.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_6.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_7.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_8.root',
+      'rfio:/castor/cern.ch/user/c/cbern/CMSSW312/Fast/aod_QCDForPF_Fast_9.root',
+      ),
+    secondaryFileNames = cms.untracked.vstring(),
+    noEventSort = cms.untracked.bool(True),
+    duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+    )
+
 
 process.pfCandidateAnalyzer = cms.EDAnalyzer("PFCandidateAnalyzer",
     PFCandidates = cms.InputTag("particleFlow"),
     verbose = cms.untracked.bool(True),
     printBlocks = cms.untracked.bool(False)
 )
+
+process.load("FastSimulation.Configuration.EventContent_cff")
+process.aod = cms.OutputModule("PoolOutputModule",
+    process.AODSIMEventContent,
+    fileName = cms.untracked.string('aod.root')
+)
+
+process.outpath = cms.EndPath(process.aod )
+
 
 process.p = cms.Path(process.pfCandidateAnalyzer)
 
