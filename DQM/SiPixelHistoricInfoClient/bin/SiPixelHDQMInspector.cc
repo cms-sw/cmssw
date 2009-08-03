a18d44972aaf608ad8ed3fdab05ec5ad3ae72271
Dean Andrew Hidas <sha1-6c92d0e9fd6974661f32e167cae070c19b465ec2@cern.ch>
2009-08-03 10:49:31 +0000

    minor plot update before tagging

--- DQM/SiPixelHistoricInfoClient/bin/SiPixelHDQMInspector.cc
+++ DQM/SiPixelHistoricInfoClient/bin/SiPixelHDQMInspector.cc
@@ -85,15 +85,17 @@ void SiPixelHDQMInspector (const std::string &tagName, std::string const& Passwo
 
   //A.setBlackList("68286");
 
-  A.createTrend("1@SUMOFF_adc@yMean", "adc_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_charge_OffTrack@yMean", "charge_OffTrack_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_charge_OnTrack@yMean", "charge_OnTrack_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_nRecHits@yMean", "nRecHits_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_nclusters_OffTrack@yMean", "nclusters_OffTrack_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_nclusters_OnTrack@yMean", "nclusters_OnTrack_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_ndigis@yMean", "ndigis_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_size_OffTrack@yMean", "size_OffTrack_yMean.gif", 0, "", Start, End);
-  A.createTrend("1@SUMOFF_size_OnTrack@yMean", "size_OnTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_adc@yMean", "adc_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_charge_OffTrack@yMean", "charge_OffTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_charge_OnTrack@yMean", "charge_OnTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_nRecHits@yMean", "nRecHits_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_nclusters_OffTrack@yMean", "nclusters_OffTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_nclusters_OnTrack@yMean", "nclusters_OnTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_ndigis@yMean", "ndigis_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_size_OffTrack@yMean", "size_OffTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@SUMOFF_size_OnTrack@yMean", "size_OnTrack_yMean.gif", 0, "", Start, End);
+  A.createTrend("0@ntracks_rsWithMaterialTracksP5@NTracksPixOverAll", "NTracksPixOverAll.gif", 0, "", Start, End);
+  A.createTrend("0@ntracks_rsWithMaterialTracksP5@NTracksFPixOverBPix", "NTracksFPixOverBPix.gif", 0, "", Start, End);
 
 
   A.closeFile();
