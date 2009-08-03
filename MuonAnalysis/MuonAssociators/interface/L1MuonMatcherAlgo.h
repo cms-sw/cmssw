1eaaec7590faafe8539adf03d68019bcdf2c8c11
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2009-08-03 09:57:58 +0000

    Refactored propagation from matching

--- MuonAnalysis/MuonAssociators/interface/L1MuonMatcherAlgo.h
+++ MuonAnalysis/MuonAssociators/interface/L1MuonMatcherAlgo.h
@@ -1,7 +1,7 @@
 #ifndef MuonAnalysis_MuonAssociators_interface_L1MuonMatcherAlgo_h
 #define MuonAnalysis_MuonAssociators_interface_L1MuonMatcherAlgo_h
 //
-// $Id: L1MuonMatcherAlgo.h,v 1.4 2009/05/12 17:20:45 gpetrucc Exp $
+// $Id: L1MuonMatcherAlgo.h,v 1.1 2009/05/18 16:38:45 gpetrucc Exp $
 //
 
 /**
@@ -9,24 +9,17 @@
   \brief    Matcher of reconstructed objects to L1 Muons 
             
   \author   Giovanni Petrucciani
-  \version  $Id: L1MuonMatcher.h,v 1.4 2009/05/12 17:20:45 gpetrucc Exp $
+  \version  $Id: L1MuonMatcherAlgo.h,v 1.1 2009/05/18 16:38:45 gpetrucc Exp $
 */
 
 
 #include "DataFormats/Candidate/interface/Candidate.h"
-#include "DataFormats/GeometrySurface/interface/BoundCylinder.h"
-#include "DataFormats/GeometrySurface/interface/BoundDisk.h"
-#include "DataFormats/L1Trigger/interface/L1MuonParticle.h"
 #include "DataFormats/TrackReco/interface/Track.h"
-#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/L1Trigger/interface/L1MuonParticle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "MagneticField/Engine/interface/MagneticField.h"
 #include "PhysicsTools/Utilities/interface/StringCutObjectSelector.h"
-#include "RecoMuon/DetLayers/interface/MuonDetLayerGeometry.h"
-#include "TrackingTools/DetLayers/interface/DetLayer.h"
-#include "TrackingTools/GeomPropagators/interface/Propagator.h"
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+#include "MuonAnalysis/MuonAssociators/interface/PropagateToMuon.h"
 
 class L1MuonMatcherAlgo {
     public:
@@ -37,13 +30,18 @@ class L1MuonMatcherAlgo {
         void init(const edm::EventSetup &iSetup) ;
 
         /// Extrapolate reco::Track to the muon station 2, return an invalid TSOS if it fails
-        TrajectoryStateOnSurface extrapolate(const reco::Track &tk)     const { return extrapolate(startingState(tk)); }
+        TrajectoryStateOnSurface extrapolate(const reco::Track &tk)     const { return prop_.extrapolate(tk); }
 
         /// Extrapolate reco::Candidate to the muon station 2, return an invalid TSOS if it fails
-        TrajectoryStateOnSurface extrapolate(const reco::Candidate &tk) const { return extrapolate(startingState(tk)); }
+        TrajectoryStateOnSurface extrapolate(const reco::Candidate &tk) const { return prop_.extrapolate(tk); }
 
         /// Extrapolate a FreeTrajectoryState to the muon station 2, return an invalid TSOS if it fails
-        TrajectoryStateOnSurface extrapolate(const FreeTrajectoryState &state) const ;
+        TrajectoryStateOnSurface extrapolate(const FreeTrajectoryState &state) const { return prop_.extrapolate(state); }
+
+        /// Return the propagator to second muon station (in case it's needed)
+        PropagateToMuon & propagatorToMuon() { return prop_; }
+        /// Return the propagator to second muon station (in case it's needed)
+        const PropagateToMuon & propagatorToMuon() const { return prop_; }
 
         /// Try to match one track to one L1. Return true if succeeded (and update deltaR, deltaPhi and propagated TSOS accordingly)
         /// The preselection cut on L1, if specified in the config, is applied before the match
@@ -85,15 +83,9 @@ class L1MuonMatcherAlgo {
         int match(TrajectoryStateOnSurface &propagated, const std::vector<l1extra::L1MuonParticle> &l1, float &deltaR, float &deltaPhi) const ;
 
     private:
+        PropagateToMuon prop_;
+
         typedef StringCutObjectSelector<l1extra::L1MuonParticle> Selector;
-        enum WhichTrack { None, TrackerTk, MuonTk, GlobalTk };
-        enum WhichState { AtVertex, Innermost, Outermost };
-
-        /// Labels for input collections
-        bool useSimpleGeometry_;
-        WhichTrack whichTrack_;
-        WhichState whichState_;
-
         /// Preselection cut to apply to L1 candidates before matching
         Selector preselectionCut_;
 
@@ -102,29 +94,6 @@ class L1MuonMatcherAlgo {
 
         /// Sort by deltaPhi instead of deltaR
         bool sortByDeltaPhi_;
-
-        /// for cosmics, some things change: the along-opposite is not in-out, nor the innermost/outermost states are in-out really
-        bool cosmicPropagation_;
-
-        // needed services for track propagation
-        edm::ESHandle<MagneticField> magfield_;
-        edm::ESHandle<Propagator> propagator_, propagatorAny_, propagatorOpposite_;
-        edm::ESHandle<MuonDetLayerGeometry> muonGeometry_;
-        // simplified geometry for track propagation
-        const  BoundCylinder *barrelCylinder_;
-        const  BoundDisk *endcapDiskPos_, *endcapDiskNeg_;
-        double barrelHalfLength_;
-        std::pair<float,float> endcapRadii_;
-
-        /// Starting state for the propagation
-        FreeTrajectoryState startingState(const reco::Candidate &reco) const ;
-
-        /// Starting state for the propagation
-        FreeTrajectoryState startingState(const reco::Track &tk) const ;
-
-        /// Get the best TSOS on one of the chambres of this DetLayer, or an invalid TSOS if none match
-        TrajectoryStateOnSurface getBestDet(const TrajectoryStateOnSurface &tsos, const DetLayer *station) const;
-
 };
 
 #endif
