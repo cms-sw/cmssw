75b69d6e65613271deef0510f932833236cee67b
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2009-08-04 10:48:33 +0000

    turn on outliers rejection in CTF for cosmics

--- RecoTracker/TrackProducer/python/CTFFinalFitWithMaterialP5_cff.py
+++ RecoTracker/TrackProducer/python/CTFFinalFitWithMaterialP5_cff.py
@@ -4,17 +4,20 @@ from TrackingTools.KalmanUpdators.KFUpdatorESProducer_cfi import *
 from TrackingTools.KalmanUpdators.Chi2MeasurementEstimatorESProducer_cfi import *
 from TrackingTools.MaterialEffects.Propagators_cff import *
 from TrackingTools.TrackFitters.TrackFitters_cff import *
-import TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi
-FittingSmootherRKP5 = TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi.KFFittingSmoother.clone()
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
 from RecoLocalTracker.SiPixelRecHits.PixelCPEParmError_cfi import *
 from RecoTracker.TransientTrackingRecHit.TransientTrackingRecHitBuilder_cfi import *
 
+
+#could it just use the standard KFFittingSmootherWithOutliersRejectionAndRK ??
+import TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi
+FittingSmootherRKP5 = TrackingTools.TrackFitters.KFFittingSmootherESProducer_cfi.KFFittingSmoother.clone()
 FittingSmootherRKP5.ComponentName = 'FittingSmootherRKP5'
 FittingSmootherRKP5.Fitter = 'RKFitter'
 FittingSmootherRKP5.Smoother = 'RKSmoother'
-FittingSmootherRKP5.MinNumberOfHits = 4
+FittingSmootherRKP5.MinNumberOfHits = 4 #why is this set to 4??
+FittingSmootherRKP5.EstimateCut = cms.double(20.0)
 
 import RecoTracker.TrackProducer.TrackProducer_cfi
 ctfWithMaterialTracksP5 = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProducer.clone(
