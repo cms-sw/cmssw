342ca158eacac16ea1367bb588024e7a50512881
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-08-04 08:48:29 +0000

    mixNoPU_cfi.py update ro comply with the new version of the MixingModule

--- FastSimulation/Configuration/python/mixNoPU_cfi.py
+++ FastSimulation/Configuration/python/mixNoPU_cfi.py
@@ -1,13 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 from FastSimulation.Configuration.mixFastSimObjects_cfi import *
-mix = cms.EDFilter("MixingModule",
+mix = cms.EDProducer("MixingModule",
     LabelPlayback = cms.string(''),
     maxBunch = cms.int32(3),
     minBunch = cms.int32(-5),
     bunchspace = cms.int32(25),
     checktof = cms.bool(False),                   
     playback = cms.untracked.bool(False),
+    mixProdStep1 = cms.bool(True),
+    mixProdStep2 = cms.bool(False),
     mixObjects = cms.PSet(
         mixSH = cms.PSet(
             mixSimHits
