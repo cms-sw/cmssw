037f50302f80105df82557ffbc105e02e129e332
Simone Gennai <sha1-c06466410f2ad16966d114488822880c0effa70e@cern.ch>
2009-08-04 09:46:54 +0000

    *** empty log message ***

--- DataFormats/TauReco/interface/HLTTau.h
+++ DataFormats/TauReco/interface/HLTTau.h
@@ -15,19 +15,17 @@ namespace reco {
 
   class HLTTau {
   public:
-    HLTTau() {
-      emIsolation_ = -1000.;
-      sumPtTracksL25_ = -1000.;
-      sumPtTracksL3_ = -1000.;
-      trackIsolationL25_ = -1;
-      nTrackIsolationL25_ = -1;
-      leadTrackPtL25_ = 0.;
-      trackIsolationL3_ = -1;
-      nTrackIsolationL3_ = -1;
-      leadTrackPtL3_ = 0.;
+    HLTTau() { 
       eta_ = 0.;
       phi_ = 0.;
       pt_ = -1.;
+      emIsolation_ = -1000.;
+      leadTrackPtL25_ = 0.;
+      leadPionPtL25_ = 0.;
+      ptL25_ = -1.;
+      trackIsolationL3_ = -1;
+      nTrackIsolationL3_ = -1;
+      sumPtTracksL3_ = -1000.;     
       seedEcalHitEt_ = -1.;
       ecalClusterShape_.clear();
       nEcalHits_ = -1;
@@ -37,15 +35,10 @@ namespace reco {
       nHcalHits_=-1;
     }
 
-    HLTTau(float eta, float phi, float pt, float emIsolation, int trackIsolationL25, float leadTrackPtL25, int trackIsolationL3, float leadTrackPtL3) {
+    HLTTau(float eta, float phi, float pt) {
       eta_ = eta;
       phi_ = phi;
       pt_ = pt;
-      emIsolation_ = emIsolation;
-      trackIsolationL25_ = trackIsolationL25;
-      leadTrackPtL25_ = leadTrackPtL25;
-      trackIsolationL3_ = trackIsolationL3 ;
-      leadTrackPtL3_ = leadTrackPtL3;
     }
 
     virtual ~HLTTau() { }
@@ -55,48 +48,56 @@ namespace reco {
     float getPt() const { return pt_; }
 
     float getEMIsolationValue() const { return emIsolation_;}
-    int   getL25TrackIsolationResponse() const { return trackIsolationL25_; }
-    int   getNL25TrackIsolation() const { return nTrackIsolationL25_; }
-    float getL25LeadTrackPtValue() const { return leadTrackPtL25_; }
-    int   getL3TrackIsolationResponse()const { return trackIsolationL3_; }
-    int   getNL3TrackIsolation() const { return nTrackIsolationL3_; }
-    float getL3LeadTrackPtValue() const { return leadTrackPtL3_; }
-    float getSumPtTracksL25() const {return sumPtTracksL25_;}
-    float getSumPtTracksL3() const {return sumPtTracksL3_;}
-
     double getSeedEcalHitEt() const {return seedEcalHitEt_;} //Lead PF Cluster Et /or simple cluster/or crystal
     std::vector<double> getEcalClusterShape() const {return ecalClusterShape_;} //cluster shapes eta [0], Phi[0] DeltaR [1]
     int getNEcalHits() const {return nEcalHits_;} //N Ecal PF Clusters or simple clusters or crystals
-    
     double getHcalIsolEt() const {return hcalIsolEt_;}
     double getSeedHcalHitEt() const {return seedHcalHitEt_;}
     std::vector<double> getHcalClusterShape() const {return hcalClusterShape_;}
     int getNHcalHits() const {return nHcalHits_;}
+    
+    float getL25TauPt() const {return ptL25_;}
+    float getL25LeadTrackPtValue() const { return leadTrackPtL25_; }
+    float getL25LeadPionPtValue() const { return leadPionPtL25_; }
+    int   getL3TrackIsolationResponse()const { return trackIsolationL3_; }
+    int   getNL3TrackIsolation() const { return nTrackIsolationL3_; }
+    float getSumPtTracksL3() const {return sumPtTracksL3_;}
 
 
-    void  setNL25TrackIsolation(int nTracks)  { nTrackIsolationL25_ = nTracks; }
-    void  setNL3TrackIsolation(int nTracks)  { nTrackIsolationL3_ = nTracks; }  
-    void setSumPtTracksL25(double sumPt) {sumPtTracksL25_ = sumPt;}
-    void setSumPtTracksL3(double sumPt) {sumPtTracksL3_ = sumPt;}    
+    //L2 quantities
+    void setEMIsolationValue(double emiso) { emIsolation_ = emiso;}
     void setSeedEcalHitEt(double  seed)   {seedEcalHitEt_ = seed;} 
     void setEcalClusterShape(std::vector<double> clusters)  {ecalClusterShape_ = clusters;} 
-    void setNEcalHits(int nhits)  { nEcalHits_ = nhits;} 
-    
+    void setNEcalHits(int nhits)  { nEcalHits_ = nhits;}     
     void setHcalIsolEt(double hcalIso)  { hcalIsolEt_ = hcalIso;}
     void setSeedHcalHitEt(double seed)  { seedHcalHitEt_ = seed;}
     void setHcalClusterShape(std::vector<double> clusters)  { hcalClusterShape_ = clusters;}
     void setNHcalHits(int nhits)  { nHcalHits_ = nhits;}
+    
+    //L2.5 quantities
+    void setL25TauPt(double pt)  { ptL25_ = pt;}
+    void setL25LeadTrackPtValue(double pt)  {  leadTrackPtL25_ = pt; }
+    void setL25LeadPionPtValue(double pt)  {  leadPionPtL25_ = pt; }
+
+    //L3 quantities
+    void setL3TrackIsolationResponse(int response) {  trackIsolationL3_ = response; }
+    void setNL3TrackIsolation(int nTracks)  { nTrackIsolationL3_ = nTracks; }  
+    void setSumPtTracksL3(double sumPt) {sumPtTracksL3_ = sumPt;}    
+   
+
+
+
+
 
 
   private:
     float eta_, phi_, pt_;
     float emIsolation_;
-    int   trackIsolationL25_;
+    float ptL25_;
     float leadTrackPtL25_;
-    int nTrackIsolationL25_;
+    float leadPionPtL25_;
     int   trackIsolationL3_;
     int nTrackIsolationL3_;
-    float leadTrackPtL3_;
     double seedEcalHitEt_;
     std::vector<double> ecalClusterShape_;
     int nEcalHits_;
--- RecoTauTag/HLTProducers/src/HLTTauProducer.cc
+++ RecoTauTag/HLTProducers/src/HLTTauProducer.cc
@@ -35,8 +35,7 @@ void HLTTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
   edm::Handle<PFTauCollection> tauL3Jets;
   iEvent.getByLabel(trackIsolatedJetsL3_, tauL3Jets );
 
-  PFTauCollection tauL25 = *(tauL25Jets.product());
-  PFTauCollection tauL3 = *(tauL3Jets.product());
+  
   
   int i=0;
   
@@ -47,28 +46,45 @@ void HLTTauProducer::produce(edm::Event& iEvent, const edm::EventSetup& iES)
 	     //Retrieve the Jet
 	     const CaloJet& jet =*(p->key);
 	     
+	     float eta_ = jet.eta();
+	     float phi_ = jet.phi();
+	     float pt_ = jet.pt();
+	     HLTTau pippo(eta_,phi_,pt_);
+	     pippo.setEMIsolationValue(l2info.ecalIsolEt());
+	     pippo.setSeedEcalHitEt(l2info.seedEcalHitEt());
+	     pippo.setEcalClusterShape(l2info.ecalClusterShape());
+	     pippo.setNEcalHits(l2info.nEcalHits());		       
+	     pippo.setHcalIsolEt(l2info.hcalIsolEt());
+	     pippo.setSeedHcalHitEt(l2info.seedHcalHitEt());
+	     pippo.setHcalClusterShape(l2info.hcalClusterShape());
+	     pippo.setNHcalHits(l2info.nHcalHits());
+	     //getting the pftau reference
+	     PFTauRef thePFTauRef(tauL25Jets,i);
+	     //setting up L2.5 quantities
+	     pippo.setL25TauPt(thePFTauRef->pt());
+	     if(thePFTauRef->leadPFChargedHadrCand().isNonnull()) {
+	       pippo.setL25LeadTrackPtValue(thePFTauRef->leadPFChargedHadrCand()->pt());
+	     }else{
+	       pippo.setL25LeadTrackPtValue(0.);
+	     }
 
-	     double emIsol  = l2info.ecalIsolEt();
+	     if(thePFTauRef->leadPFCand().isNonnull()) {
+	       pippo.setL25LeadPionPtValue(thePFTauRef->leadPFCand()->pt());
+	     }else{
+	       pippo.setL25LeadPionPtValue(0.);
+	     }
+	     
+       //setting up L3 quantities
+	     PFTauRef thePFTauL3Ref(tauL3Jets,i);
+	     pippo.setNL3TrackIsolation(thePFTauL3Ref->isolationTracks().size());
+	     float sumPtL3 = 0.;
+	     for(unsigned int j=0;j<thePFTauL3Ref->isolationTracks().size();j++){
+	       sumPtL3 = sumPtL3 + thePFTauL3Ref->isolationTracks()[j]->pt();
+	     }
+	     pippo.setSumPtTracksL3(sumPtL3);
 
-    float eta_ = jet.eta();
-    float phi_ = jet.phi();
-    float pt_ = jet.pt();
-
-   
-    HLTTau pippo(eta_,phi_,pt_);
-    /*
-    pippo.setNL3TrackIsolation(nTracksL3);
-    pippo.setSumPtTracksL3(sumPtTracksL3);
-    pippo.setSeedEcalHitEt(l2info.seedEcalHitEt());
-    pippo.setEcalClusterShape(l2info.ecalClusterShape());
-    pippo.setNEcalHits(l2info.nEcalHits());		       
-    pippo.setHcalIsolEt(l2info.hcalIsolEt());
-    pippo.setSeedHcalHitEt(l2info.seedHcalHitEt());
-    pippo.setHcalClusterShape(l2info.hcalClusterShape());
-    pippo.setNHcalHits(l2info.nHcalHits());
-    */
-    jetCollection->push_back(pippo);
-      i++;
+	     jetCollection->push_back(pippo);
+	     i++;
   }
   
   auto_ptr<reco::HLTTauCollection> selectedTaus(jetCollection);
