a8a61c4f70cfb7132c438529b822589b0494d84c
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-08-05 14:49:28 +0000

    removed unused FED ids

--- DQM/L1TMonitor/python/L1TFED_cfi.py
+++ DQM/L1TMonitor/python/L1TFED_cfi.py
@@ -4,7 +4,7 @@ l1tfed = cms.EDFilter("L1TFED",
     disableROOToutput = cms.untracked.bool(True),
     verbose = cms.untracked.bool(False),
     DQMStore = cms.untracked.bool(True),
-    L1FEDS = cms.vint32(745,746,747,748,749,760,780,812,813),
+    L1FEDS = cms.vint32(745,760,780,812,813),
     rawTag = cms.InputTag( "source" )
 )
 
