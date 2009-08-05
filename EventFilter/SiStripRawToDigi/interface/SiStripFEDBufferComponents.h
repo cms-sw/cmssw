ab5bc26afbcc6b0cb79435a0c69ed88b72002e2e
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-08-05 14:38:05 +0000

    Fixed typo

--- EventFilter/SiStripRawToDigi/interface/SiStripFEDBufferComponents.h
+++ EventFilter/SiStripRawToDigi/interface/SiStripFEDBufferComponents.h
@@ -521,7 +521,7 @@ namespace sistrip {
 
   inline uint8_t FEDStripOrdering::physicalOrderForStripInChannel(const uint8_t readoutOrderStripIndexInChannel)
     {
-      return physicalOrderForStripInAPV(readoutOrderStripInChannel/2) + (readoutOrderStripIndexInChannel%2)*STRIPS_PER_APV;
+      return physicalOrderForStripInAPV(readoutOrderStripIndexInChannel/2) + (readoutOrderStripIndexInChannel%2)*STRIPS_PER_APV;
     }
   
   inline uint8_t FEDStripOrdering::readoutOrderForStripInChannel(const uint8_t physicalOrderStripIndexInChannel)
