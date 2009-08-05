0f6aa72337e966200ffb74032880cd6bcfa8709f
Pratima Jindal <sha1-bbb50ea1f248b1c44c36d4acf8c39259e9305eaf@cern.ch>
2009-08-05 20:34:19 +0000

    adding Pix Fwd Phase I Files

--- SLHCUpgradeSimulations/Geometry/src/module.cc
+++ SLHCUpgradeSimulations/Geometry/src/module.cc
@@ -1,12 +1,14 @@
 // define the geometry plugin modules for new layers
 
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinear.h"
-#include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinearGap.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLayerAlgo.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackTrigLayerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
-
+#include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdInnerDisks.h"
+#include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdOuterDisks.h"
+#
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinear,      "track:DDPixBarStackLinear");
-DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinearGap,   "track:DDPixBarStackLinearGap");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLayerAlgo,   "track:DDPixBarStackLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackTrigLayerAlgo, "track:DDPixBarStackTrigLayerAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdInnerDisks,      "track:DDPixFwdInnerDisks");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdOuterDisks,      "track:DDPixFwdOuterDisks");
