29335957194758ddfb8e208ab7c72fd7a18c694e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-08-06 19:54:36 +0000

    recowrk of EventShapeVariables

--- TopQuarkAnalysis/TopTools/interface/EventShapeVariables.h
+++ TopQuarkAnalysis/TopTools/interface/EventShapeVariables.h
@@ -1,47 +1,55 @@
 #ifndef EventShapeVariables_h
 #define EventShapeVariables_h
 
-#include <memory>
 #include <vector>
-#include <string>
-#include <iostream>
-
-#include "TMath.h"
-#include "TVector3.h"
-#include "TVectorD.h"
 #include "TMatrixDSym.h"
+#include "DataFormats/Math/interface/Vector3D.h"
 
+
+/**
+   \class   EventShapeVariables EventShapeVariables.h "TopQuarkAnalysis/TopTools/interface/EventShapeVariables.h"
+
+   \brief   Class for the calculation of event shape variables
+
+   Class for the calculation of several event shape variables. Isotropy, sphericity,
+   aplanarity and circularity are supported. The class has a vector of 3 dimentional 
+   vectors as input which can be given in cartesian, cylindrical or polar coordinates. 
+*/
 
 class EventShapeVariables {
 
  public:
-  
-  explicit EventShapeVariables();
+  /// contructor from XYZ coordinates
+  explicit EventShapeVariables(const std::vector<math::XYZVector>& inputVectors);  
+  /// contructor from rho eta phi coordinates
+  explicit EventShapeVariables(const std::vector<math::RhoEtaPhiVector>& inputVectors);  
+  /// contructor from r theta phi coordinates
+  explicit EventShapeVariables(const std::vector<math::RThetaPhiVector>& inputVectors);  
+  /// default destructir
   ~EventShapeVariables(){};
 
-  int nStep() {return nStep_;};  
-  double sphericity()  const {return sph_; };
-  double aplanarity()  const {return apl_; };
-  double circularity() const {return cir_; };
-  double isotropy()    const {return iso_; };
-
-  void setNStep(int value) {nStep_=value;};
-  double sphericity(const std::vector<TVector3>&);
-  double aplanarity(const std::vector<TVector3>&);
-  double circularity(const std::vector<TVector3>&);
-  double isotropy(const std::vector<TVector3>&);
+  /// the return value is 1 for spherical and 0 for events linear in r-phi. This function needs
+  /// the number of steps to determined how fine the grnularity of the algorithm in phi should be
+  double isotropy(const unsigned int& numberOfSteps = 1000) const;
+  /// 1.5*(q1+q2) where 0<=q1<=q2<=q3 are the eigenvalues of the momemtum tensor 
+  /// sum{pj[a]*pj[b]}/sum{pj**2} normalized to 1. Return values are 1 for spherical, 3/4 for 
+  /// plane and 0 for linear events
+  double sphericity()  const;
+  /// 1.5*q1 where 0<=q1<=q2<=q3 are the eigenvalues of the momemtum tensor 
+  /// sum{p_j[a]*p_j[b]}/sum{p_j**2} normalized to 1. Return values are 0.5 for spherical and 0 
+  /// for plane and linear events
+  double aplanarity()  const;
+  /// the return value is 1 for spherical and 0 linear events in r-phi. This function needs the 
+  /// number of steps to determined how fine the grnularity of the algorithm in phi should be
+  double circularity(const unsigned int& numberOfSteps = 1000) const;
   
  private:
+  /// helper function to fill the 3 dimensional momentum tensor from the inputVecotrs where 
+  /// needed
+  TMatrixDSym momentumTensor() const;
 
-  TMatrixDSym momentumTensor(const std::vector<TVector3>&);
-
- private:
-
-  int nStep_;
-  double sph_;
-  double apl_;
-  double cir_;
-  double iso_;
+  /// cashing of input vectors
+  std::vector<math::XYZVector> inputVectors_;
 };
 
 #endif
