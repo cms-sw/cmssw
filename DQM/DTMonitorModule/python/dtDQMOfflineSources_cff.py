cd018d7f16c9332de097c4086ce9d462db942d9f
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-08-07 14:30:39 +0000

    add trigger efficiency

--- DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
+++ DQM/DTMonitorModule/python/dtDQMOfflineSources_cff.py
@@ -37,8 +37,11 @@ dtDataIntegrityUnpacker = cms.EDProducer("DTUnpackingModule",
 from DQM.DTMonitorModule.dtDataIntegrityTask_cfi import *
 DTDataIntegrityTask.hltMode = True
 
+from DQM.DTMonitorModule.dtTriggerEfficiencyTask_cfi import *
+
 dtSources = cms.Sequence(dtDataIntegrityUnpacker  +
                          dtSegmentAnalysisMonitor +
                          dtResolutionAnalysisMonitor +
                          dtEfficiencyMonitor +
+                         dtTriggerEfficiencyMonitor +
                          dqmInfoDT)
