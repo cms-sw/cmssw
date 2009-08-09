8a4b9269a749bf3b56266a54fa3f3f5147afbf14
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-08-09 13:52:19 +0000

    *** empty log message ***

--- DQM/HcalMonitorModule/python/hcal_dqm_source_fileT0_cff.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_source_fileT0_cff.py
@@ -7,18 +7,14 @@ dqmInfoHcal = cms.EDFilter("DQMEventInfo",
 )
 from DQM.HcalMonitorModule.HcalMonitorModule_cfi import *
 hcalOfflineDQMSource = cms.Sequence(hcalMonitor + dqmInfoHcal)
-# Not needed in new Digi Monitor
-#hcalMonitor.DigiOccThresh          = -999999999
 
 hcalMonitor.PedestalMonitor        = False
-hcalMonitor.PedestalMonitor_pedestalsPerChannel    = False
-hcalMonitor.PedestalMonitor_pedestalsInFC          = True
-
 hcalMonitor.DataFormatMonitor      = True
 hcalMonitor.DigiMonitor            = True
 hcalMonitor.RecHitMonitor          = True
 hcalMonitor.BeamMonitor            = True
 hcalMonitor.TrigPrimMonitor        = False # disabled until we find why it crashes
+hcalMonitor.DetDiagNoiseMonitor    = True
 hcalMonitor.DeadCellMonitor        = True
 hcalMonitor.HotCellMonitor         = True
 hcalMonitor.LEDMonitor             = False
