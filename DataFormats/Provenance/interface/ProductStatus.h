505c5ac5ceca04d34c899fc2eb815368cd54c877
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-08-10 21:22:36 +0000

    Add new info for reasons for fail to create product

--- DataFormats/Provenance/interface/ProductStatus.h
+++ DataFormats/Provenance/interface/ProductStatus.h
@@ -9,22 +9,32 @@ ProductStatus:
 /*
   ProductStatus
 */
-#include <vector>
 
 namespace edm {
   typedef unsigned char byte_t;
   typedef byte_t ProductStatus;
   namespace productstatus {
     inline ProductStatus present() {return 0x0;} // Product was made successfully
-    inline ProductStatus neverCreated() {return 0x1;} // Product was not made successfully
-    inline ProductStatus dropped() {return 0x2;} // Product was not made successfully
-    inline ProductStatus unknown() {return 0xfe;} // Status unknown (used for backward compatibility)
-    inline ProductStatus uninitialized() {return 0xff;} // Status not set
+    inline ProductStatus neverCreated() {return 0x1;} // Product was not made successfully, reason not known.
+    inline ProductStatus dropped() {return 0x2;} // Product was in a dropped branch.
+    inline ProductStatus producerNotRun() {return 0x3;} // Producer was never run
+    inline ProductStatus producerNotCompleted() {return 0x4;} // Producer was run but did not finish (i.e. threw)
+    inline ProductStatus producerDidNotPutProduct() {return 0x5;} // Producer did not put product
+    inline ProductStatus unscheduledProducerNotRun() {return 0x6;} // Producer was never run
+    inline ProductStatus unknown() {return 0xfe;} // Status unknown 
+    inline ProductStatus uninitialized() {return 0xff;} // Status not yet set
     inline bool present(ProductStatus status) {return status == present();}
     inline bool neverCreated(ProductStatus status) {return status == neverCreated();}
     inline bool dropped(ProductStatus status) {return status == dropped();}
+    inline bool producerNotRun(ProductStatus status) {return status == producerNotRun();}
+    inline bool producerNotCompleted(ProductStatus status) {return status == producerNotCompleted();}
+    inline bool producerDidNotPutProduct(ProductStatus status) {return status == producerDidNotPutProduct();}
+    inline bool unscheduledProducerNotRun(ProductStatus status) {return status == unscheduledProducerNotRun();}
     inline bool unknown(ProductStatus status) {return status == unknown();}
     inline bool uninitialized(ProductStatus status) {return status == uninitialized();}
+
+    inline bool presenceUnknown(ProductStatus status) {return uninitialized(status) || unknown(status);}
+    inline bool notPresent(ProductStatus status) {return !present(status) && !presenceUnknown(status);}
   }
 }
 #endif
