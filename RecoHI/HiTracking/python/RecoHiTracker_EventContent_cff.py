124704992205434bb1f0a3a6e1164c861e201c94
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-11 10:42:03 +0000

    tidied up and added vertices

--- RecoHI/HiTracking/python/RecoHiTracker_EventContent_cff.py
+++ RecoHI/HiTracking/python/RecoHiTracker_EventContent_cff.py
@@ -2,26 +2,23 @@ import FWCore.ParameterSet.Config as cms
 
 #Full Event content 
 RecoHiTrackerFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoTracks_globalPrimTracks_*_*', 
-        'keep recoTrackExtras_globalPrimTracks_*_*', 
-        'keep TrackingRecHitsOwned_globalPrimTracks_*_*',
-		'keep recoTracks_selectHiTracks_*_*', 
-        'keep recoTrackExtras_selectHiTracks_*_*', 
-        'keep TrackingRecHitsOwned_selectHiTracks_*_*'
+    outputCommands = cms.untracked.vstring('keep *_globalPrimTracks_*_*', 
+		'keep *_selectHiTracks_*_*', 
+		'keep *_pixel3PrimTracks_*_*',
+		'keep recoVertexs_pixel3Vertices_*_*'
     )
 )
 #RECO content
 RecoHiTrackerRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoTracks_globalPrimTracks_*_*', 
-        'keep recoTrackExtras_globalPrimTracks_*_*', 
-        'keep TrackingRecHitsOwned_globalPrimTracks_*_*',
-		'keep recoTracks_selectHiTracks_*_*', 
-        'keep recoTrackExtras_selectHiTracks_*_*', 
-        'keep TrackingRecHitsOwned_selectHiTracks_*_*'
+    outputCommands = cms.untracked.vstring('keep *_globalPrimTracks_*_*', 
+		'keep *_selectHiTracks_*_*', 
+		'keep *_pixel3PrimTracks_*_*',		
+		'keep recoVertexs_pixel3Vertices_*_*'
     )
 )
 #AOD content
 RecoHiTrackerAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoTracks_selectHiTracks_*_*'
+    outputCommands = cms.untracked.vstring('keep recoTracks_selectHiTracks_*_*',
+		'keep recoVertexs_pixel3Vertices_*_*'	
     )
 )
