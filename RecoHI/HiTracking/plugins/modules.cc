75c20eabd171510c42c6a84e3b29c9741fa0bd44
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-11 10:35:17 +0000

    added median vtx and offline region producer modules

--- RecoHI/HiTracking/plugins/modules.cc
+++ RecoHI/HiTracking/plugins/modules.cc
@@ -5,9 +5,15 @@
 
 DEFINE_SEAL_MODULE();
 
-// Heavy-Ions                                                                                                               
+// Median Vertexing
+#include "HIPixelMedianVtxProducer.h"
+DEFINE_ANOTHER_FWK_MODULE(HIPixelMedianVtxProducer);
+
+// Restricted HI tracking regions                                                                                                              
 #include "HITrackingRegionProducer.h"
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducerFactory.h"
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducer.h"
 DEFINE_EDM_PLUGIN(TrackingRegionProducerFactory, HITrackingRegionProducer, "HITrackingRegionProducer");
 
+#include "HITrackingRegionForPrimaryVtxProducer.h"
+DEFINE_EDM_PLUGIN(TrackingRegionProducerFactory, HITrackingRegionForPrimaryVtxProducer, "HITrackingRegionForPrimaryVtxProducer");
