1759fece568200cece4340721a4565642d92cf69
anderson <sha1-a9de63af5eb1e4196aba47de6639138a5a7552ac@cern.ch>
2009-08-11 23:18:08 +0000

    max events to -1

--- DQM/Physics/test/ewkDQM_cfg.py
+++ DQM/Physics/test/ewkDQM_cfg.py
@@ -10,7 +10,7 @@ process.DQM.collectorHost = ''
 process.dqmSaver.workflow = cms.untracked.string('/My/Test/DataSet')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
 
 process.source = cms.Source("PoolSource",
