496e1a9ceecbf158159d6e0cecac742f34f9702c
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2009-08-12 10:54:50 +0000

    fix

--- Calibration/HcalIsolatedTrackReco/python/iptCorrector_cfi.py
+++ Calibration/HcalIsolatedTrackReco/python/iptCorrector_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 iptCorrector = cms.EDFilter("IPTCorrector",
-    corTracksLabel = cms.InputTag("generalTracks"),
-    corrIsolRadiusHB = cms.untracked.double(0.3),
-    corrIsolRadiusHE = cms.untracked.double(0.3),
-    filterLabel = cms.InputTag("hltIsolPixelTrackFilter"),
-    corrIsolMaxP = cms.untracked.double(2)
+    corTracksLabel = cms.InputTag( "hltHITCtfWithMaterialTracks" ),
+    filterLabel = cms.InputTag( "hltIsolPixelTrackL2Filter" ),
+    corrIsolRadiusHB = cms.double( 0.4 ),
+    corrIsolRadiusHE = cms.double( 0.5 ),
+    corrIsolMaxP = cms.double( 2.0 )
 )
 
 
