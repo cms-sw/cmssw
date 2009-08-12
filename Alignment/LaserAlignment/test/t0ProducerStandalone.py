d94cf6fea6d2429fc5437c7cd8804e5ad243e1c0
Adrian Perieanu <sha1-33bfe4a5a1b8f3c155c1ee211e2a85a8aa07ba34@cern.ch>
2009-08-12 14:05:48 +0000

    Update of the t0 producer for LAS to CRAFT09 conditions and new Event Filter.

--- Alignment/LaserAlignment/test/t0ProducerStandalone.py
+++ Alignment/LaserAlignment/test/t0ProducerStandalone.py
@@ -14,45 +14,25 @@ process.MessageLogger = cms.Service( "MessageLogger",
 )
 
 process.source = cms.Source( "PoolSource",
-  skipEvents = cms.untracked.uint32( 7000 ), #################################
   fileNames = cms.untracked.vstring(
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/068DFAE6-7EAF-DD11-9B0A-001617DBD288.root' #1
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/084D6BEF-84AF-DD11-9B5C-000423D991D4.root' #2
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/0ED4E41F-7BAF-DD11-8AD0-000423D6B42C.root' #3
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/24F2178A-7CAF-DD11-BAB8-001617E30F58.root' #4
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/287142EC-7BAF-DD11-84D9-001617C3B6C6.root' #5
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/2C7978EC-84AF-DD11-B849-000423D98E54.root' #6
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/3018A1C0-80AF-DD11-ACA8-001617E30CA4.root' #7
-    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/3E304C78-7AAF-DD11-BD61-000423D98FBC.root' #8
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/4C4F4870-81AF-DD11-ABF5-000423DD2F34.root' #9
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/54AB9B26-82AF-DD11-8226-000423D99F3E.root' #10
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/54D61709-80AF-DD11-9EB3-0016177CA7A0.root' #11
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/72514E86-83AF-DD11-95BE-001617DBD316.root' #12
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/763E1498-83AF-DD11-B29E-001617DC1F70.root' #13
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/86EBF63A-7DAF-DD11-9887-001617C3B6DE.root' #14
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/A031B957-7FAF-DD11-BB48-001617C3B5D8.root' #15
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/B2ADC13B-7DAF-DD11-84CE-001617C3B5F4.root' #16
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/D68DA36E-9BAF-DD11-9804-001D09F24448.root' #17
-#    '/store/data/Commissioning08/TestEnables/RAW/v1/000/070/664/DA2534F1-7DAF-DD11-82A5-001617C3B5D8.root' #18
-  )
+        '/store/data/CRAFT09/TestEnables/RAW/v1/000/110/440/FCEEFAF3-8385-DE11-B5AF-001D09F23944.root',
+    ),
 )
 
-
-
-
 process.load( "EventFilter.SiStripRawToDigi.SiStripDigis_cfi" )
-process.siStripDigis.ProductLabel = "hltCalibrationRaw"
+process.siStripDigis.ProductLabel = "source"
+#process.siStripDigis.ProductLabel = "hltCalibrationRaw"
 
-#process.load( "CondCore.DBCommon.CondDBSetup_cfi" )
-#process.allSource = cms.ESSource( "PoolDBESSource",
-#  process.CondDBSetup,
-#  connect = cms.string( 'frontier://FrontierProd/CMS_COND_20X_GLOBALTAG' ),
-#  globaltag = cms.string( 'IDEAL_v2::All' )                                     
-#)
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
-process.GlobalTag.globaltag = 'CRAFT_V4P::All'
+process.GlobalTag.globaltag = 'GR09_31X_V5P::All'
 
-
+process.eventFilter = cms.EDFilter("LaserAlignmentEventFilter",
+    #pick Run and Event range
+    RunFirst = cms.untracked.int32(110440),
+    RunLast = cms.untracked.int32(110440), 
+    EventFirst = cms.untracked.int32(10811622),
+    EventLast = cms.untracked.int32(10974656)
+)
 
 # multiple sets can be given, only those will be output
 process.load( "Alignment.LaserAlignment.LaserAlignmentT0Producer_cfi" )
@@ -60,7 +40,7 @@ process.laserAlignmentT0Producer.DigiProducerList = cms.VPSet(
   cms.PSet(
     DigiLabel = cms.string( 'ZeroSuppressed' ),
     DigiType = cms.string( 'Processed' ),
-    DigiProducer = cms.string( 'siStripDigis' )
+    DigiProducer = cms.string( 'siStripDigis' ),
   )
 )
 
@@ -75,9 +55,11 @@ process.LaserAlignmentT0ProducerDQM.DigiProducerList = cms.VPSet(
 )
 process.LaserAlignmentT0ProducerDQM.OutputInPlainROOT = True;
 process.LaserAlignmentT0ProducerDQM.UpperAdcThreshold = cms.uint32( 280 )
+process.LaserAlignmentT0ProducerDQM.PlainOutputFileName = cms.string( "TkAlLAS_Run110440_Ev10811622_F1.dqm.root" )
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32( 10000 )
+  input = cms.untracked.int32( -1 )
+#  input = cms.untracked.int32( 100)
 )
 
 process.out = cms.OutputModule( "PoolOutputModule",
@@ -85,19 +67,11 @@ process.out = cms.OutputModule( "PoolOutputModule",
     'drop *', 
     'keep *_laserAlignmentT0Producer_*_*'
   ),
-  fileName = cms.untracked.string( 'TkAlLAS.root' )
+  fileName = cms.untracked.string( 'TkAlLAS_Run110440_Ev10811622_F1.root' )
 )
 
-
-
-
-process.seqDigitization = cms.Path( process.siStripDigis )
-
-#process.dump  = cms.EDAnalyzer("EventContentAnalyzer")
-#process.dumpP = cms.Path( process.dump )
-
-process.seqAnalysis = cms.Path( process.laserAlignmentT0Producer + process.LaserAlignmentT0ProducerDQM )
-process.outputPath = cms.EndPath( process.out )
-
-
-
+process.p = cms.Path( process.eventFilter+
+                      process.siStripDigis+
+                      process.laserAlignmentT0Producer+
+                      process.LaserAlignmentT0ProducerDQM+
+                      process.out )
