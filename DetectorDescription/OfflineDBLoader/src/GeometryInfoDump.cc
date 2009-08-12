85758a8e0e70e43ee2660007fd8b4dbfad4fcbe8
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2009-08-12 00:03:49 +0000

    Changed to "uglier" output but more consistent with "locked" memory stores.

--- DetectorDescription/OfflineDBLoader/src/GeometryInfoDump.cc
+++ DetectorDescription/OfflineDBLoader/src/GeometryInfoDump.cc
@@ -14,6 +14,7 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <set>
 
 GeometryInfoDump::GeometryInfoDump () { }
 
@@ -80,16 +81,107 @@ void GeometryInfoDump::dumpInfo ( bool dumpHistory, bool dumpSpecs, bool dumpPos
     dump.close();
   }
   if ( dumpSpecs ) {
-    DDSpecifics::iterator<DDSpecifics> spit(DDSpecifics::begin()), spend(DDSpecifics::end());
-    // ======= For each DDSpecific...
+    // dump specifics at every compact-view nodes to have the most detailed "true" 
+    // final destination of the DDSpecifics
     std::string dsname = "dumpSpecs" + fname;
     std::ofstream dump(dsname.c_str());
-    for (; spit != spend; ++spit) {
-      if ( !spit->isDefined().second ) continue;  
-      const DDSpecifics & sp = *spit;
-      dump << sp << std::endl;
-    }
-    dump.close();
+    DDCompactView::DDCompactView::graph_type gra = cpv.graph();
+    std::vector<std::pair< DDPartSelection*, DDsvalues_type* > > specStore;
+    std::set<DDLogicalPart> lpStore;
+    typedef  DDCompactView::graph_type::const_adj_iterator adjl_iterator;
+    adjl_iterator git = gra.begin();
+    adjl_iterator gend = gra.end();        
+    DDCompactView::graph_type::index_type i=0;
+    git = gra.begin();
+    for (; git != gend; ++git) 
+    {
+      const DDLogicalPart & ddLP = gra.nodeData(git);
+      if ( lpStore.find(ddLP) != lpStore.end() && ddLP.attachedSpecifics().size() != 0 ) {
+	dump << ddLP.toString() << " : " << std::endl;
+	specStore.reserve(specStore.size()+ddLP.attachedSpecifics().size());
+	std::copy(ddLP.attachedSpecifics().begin(), ddLP.attachedSpecifics().end(), std::back_inserter(specStore));//, specStore.end()); //
+	std::vector<std::pair< DDPartSelection*, DDsvalues_type*> >::const_iterator bit(ddLP.attachedSpecifics().begin()), eit(ddLP.attachedSpecifics().end());
+	for ( ; bit != eit; ++bit ) {
+	  // DDsvalues_type is typedef std::vector< std::pair<unsigned int, DDValue> > DDsvalues_type;  
+	  DDsvalues_type::iterator bsit(bit->second->begin()), bseit(bit->second->end());
+	  for ( ; bsit != bseit; ++bsit ) {
+	    dump << bsit->second.name() << " ";
+	    dump << ( bsit->second.isEvaluated() ?  "evaluated" : "NOT eval." );
+	    const std::vector<std::string>& strs = bsit->second.strings();
+	    std::vector<double> ldbls;
+	    ldbls.resize(strs.size(), 0.0);
+	    if ( bsit->second.isEvaluated() ) {
+	      ldbls = bsit->second.doubles();
+	    }
+	    if ( strs.size() != ldbls.size() ) std::cout << "CRAP! " << bsit->second.name() << " does not have equal number of doubles and strings." << std::endl;
+	    size_t sdind(0);
+	    for ( ; sdind != strs.size() ; ++sdind ) {
+	      dump << " [" << strs[sdind] << "," << ldbls[sdind] << "]";
+	    }
+	  }
+	  dump << std::endl;
+	}
+      }
+      lpStore.insert(ddLP);
+
+      ++i;
+      if (git->size()) 
+	{
+	  // ask for children of ddLP  
+	  DDCompactView::graph_type::edge_list::const_iterator cit  = git->begin();
+	  DDCompactView::graph_type::edge_list::const_iterator cend = git->end();
+	  for (; cit != cend; ++cit) 
+	    {
+	      const DDLogicalPart & ddcurLP = gra.nodeData(cit->first);
+	      if (lpStore.find(ddcurLP) != lpStore.end() && ddcurLP.attachedSpecifics().size() != 0 ) {
+		specStore.reserve(specStore.size()+ddcurLP.attachedSpecifics().size());
+		std::copy(ddcurLP.attachedSpecifics().begin(), ddcurLP.attachedSpecifics().end(), std::back_inserter(specStore));
+		std::vector<std::pair< DDPartSelection*, DDsvalues_type*> >::const_iterator bit(ddcurLP.attachedSpecifics().begin()), eit(ddcurLP.attachedSpecifics().end());
+		dump << ddcurLP.toString() << " : " << std::endl;
+		for ( ; bit != eit; ++bit ) {
+		  DDsvalues_type::iterator bsit(bit->second->begin()), bseit(bit->second->end());
+		  for ( ; bsit != bseit; ++bsit ) {
+		    dump << bsit->second.name() << " ";
+		    dump << ( bsit->second.isEvaluated() ? "evaluated" : "NOT eval." );
+		    const std::vector<std::string>& strs = bsit->second.strings();
+		    std::vector<double> ldbls;
+		    ldbls.resize(strs.size(), 0.0);
+		    if ( bsit->second.isEvaluated() ) {
+		      ldbls = bsit->second.doubles();
+		    }
+		    if ( strs.size() != ldbls.size() ) std::cout << "CRAP! " << bsit->second.name() << " does not have equal number of doubles and strings." << std::endl;
+		    size_t sdind(0);
+		    for ( ; sdind != strs.size() ; ++sdind ) {
+		      dump << " [" << strs[sdind] << "," << ldbls[sdind] << "]";
+		    }
+		    dump << std::endl;
+		  }
+		  dump << std::endl;
+		}
+	      }
+	      lpStore.insert(ddcurLP);
+	    } // iterate over children
+	} // if (children)
+    } // iterate over graph nodes  
+//     std::vector<std::pair<DDPartSelection*, DDsvalues_type*> >::iterator spit(specStore.begin()), spend (specStore.end());
+//     for (; spit != spend; ++spit) {
+//       if ( !spit->isDefined().second ) continue;  
+//       const DDSpecifics & sp = *spit;
+//       dump << sp << std::endl;
+//     }
+//     dump.close();
   }
+//   if ( dumpSpecs ) {
+//     DDSpecifics::iterator<DDSpecifics> spit(DDSpecifics::begin()), spend(DDSpecifics::end());
+//     // ======= For each DDSpecific...
+//     std::string dsname = "dumpSpecs" + fname;
+//     std::ofstream dump(dsname.c_str());
+//     for (; spit != spend; ++spit) {
+//       if ( !spit->isDefined().second ) continue;  
+//       const DDSpecifics & sp = *spit;
+//       dump << sp << std::endl;
+//     }
+//     dump.close();
+//   }
 }
 
