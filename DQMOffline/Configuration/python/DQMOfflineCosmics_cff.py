198dfb93596e6ec20aab61bd99e2ec83270747e5
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-08-13 05:13:08 +0000

    sequence migration, as of V06-00-14 DQMOffline/Trigger

--- DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
+++ DQMOffline/Configuration/python/DQMOfflineCosmics_cff.py
@@ -7,11 +7,11 @@ from DQM.HcalMonitorModule.hcal_dqm_source_fileT0_cff import *
 from DQMOffline.JetMET.jetMETDQMOfflineSourceCosmic_cff import *
 from DQM.SiPixelCommon.SiPixelOfflineDQM_source_cff import *
 from DQMOffline.EGamma.cosmicPhotonAnalyzer_cff import *
-from DQMOffline.Trigger.DQMOffline_Trigger_cff import *
+from DQMOffline.Trigger.DQMOffline_Trigger_cosmics_cff import *
 from DQM.DTMonitorModule.dtDQMOfflineSources_cff import *
 from DQM.CSCMonitorModule.test.csc_dqm_sourceclient_offline_cff import *
 from DQM.RPCMonitorClient.RPCTier0Source_cff import *
 from DQM.Physics.DQMPhysics_cff import *
 
-DQMOfflineCosmics = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETDQMOfflineSourceCosmic*hcalOfflineDQMSource*triggerOfflineDQMSource*siPixelOfflineDQM_cosmics_source*egammaCosmicPhotonMonitors*dtSources*cscSources*rpcTier0Source*dqmPhysics)
+DQMOfflineCosmics = cms.Sequence(SiStripDQMTier0*ecal_dqm_source_offline*muonCosmicMonitors*jetMETDQMOfflineSourceCosmic*hcalOfflineDQMSource*triggerCosmicOfflineDQMSource*siPixelOfflineDQM_cosmics_source*egammaCosmicPhotonMonitors*dtSources*cscSources*rpcTier0Source*dqmPhysics)
 
