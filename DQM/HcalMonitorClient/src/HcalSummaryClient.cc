73ebb9990c951ed532904e5ce2bc90479624ea8e
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-08-14 09:44:45 +0000

    removed extra line

--- DQM/HcalMonitorClient/src/HcalSummaryClient.cc
+++ DQM/HcalMonitorClient/src/HcalSummaryClient.cc
@@ -412,7 +412,6 @@ void HcalSummaryClient::analyze(void)
  resetSummaryPlots();
 
  // Calculate status values for individual tasks
-
  if (dataFormatMon_.IsOn()) analyze_subtask(dataFormatMon_);
  if (digiMon_.IsOn()) analyze_subtask(digiMon_);
  if (recHitMon_.IsOn()) analyze_subtask(recHitMon_);
