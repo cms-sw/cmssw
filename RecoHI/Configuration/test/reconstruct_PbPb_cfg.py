bd4ad16925f8ddad70fc0bc0978533b650514697
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-19 12:49:55 +0000

    update to run on recent samples

--- RecoHI/Configuration/test/reconstruct_PbPb_cfg.py
+++ RecoHI/Configuration/test/reconstruct_PbPb_cfg.py
@@ -1,60 +1,24 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 process = cms.Process("RECO")
 
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.Simulation_cff")
-process.load("Configuration.StandardSequences.L1Emulator_cff")
-process.load("Configuration.StandardSequences.L1TriggerDefaultMenu_cff")
-process.load("Configuration.StandardSequences.DigiToRaw_cff")
-process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
-#global tags for conditions data: https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions#31X_pre_releases_and_integration
+#global tags for conditions data: https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions#3XY_Releases_MC
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'IDEAL_31X::All'
+process.GlobalTag.globaltag = 'MC_31X_V5::All'
 
-# For including Standard Reco + Heavy Ion Objects
-process.load("RecoHI.Configuration.Reconstruction_HI_cff")
-#process.load("HeavyIonsAnalysis.Configuration.HIAnalysisEventContent_cff")
+##################################################################################
+# Some services
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
-)
-process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/pgun_jpsi2muons_d20080423/pgun_jpsi2muons_d20080423_r000001.root')
-fileNames = cms.untracked.vstring(           "rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/hydjet_sim_x2_c5_d20080425/hydjet_sim_x2_c5_d20080425_r000002.root")
-#fileNames = cms.untracked.vstring("dcache:/pnfs/cmsaf.mit.edu/hibat/cms/users/davidlw/HYDJET_Minbias_4TeV_31X/sim/HYDJET_Minbias_4TeV_seq11_31X.root")
-)
-
-process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
-
-#from HeavyIonsAnalysis.Configuration.EventEmbedding_cff import *
-#process.mix=mixSim
-#process.mix.input.fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/cmshi/mc/sim/pgun_jpsi2muons_d20080423/pgun_jpsi2muons_d20080423_r000005.root')
-
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-    moduleSeeds = cms.PSet(
-        simMuonRPCDigis = cms.untracked.uint32(6),
-        simEcalUnsuppressedDigis = cms.untracked.uint32(8),
-        simSiStripDigis = cms.untracked.uint32(7),
-        mix = cms.untracked.uint32(4),
-        simHcalUnsuppressedDigis = cms.untracked.uint32(9),
-        simMuonCSCDigis = cms.untracked.uint32(6),
-        VtxSmeared = cms.untracked.uint32(2),
-        g4SimHits = cms.untracked.uint32(3),
-        simMuonDTDigis = cms.untracked.uint32(6),
-        simSiPixelDigis = cms.untracked.uint32(7)
-    ),
-    sourceSeed = cms.untracked.uint32(1)
-)
-
-process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
-    oncePerEventMode = cms.untracked.bool(False),
-    ignoreTotal = cms.untracked.int32(0)
-)
-
+process.SimpleMemoryCheck = cms.Service('SimpleMemoryCheck',
+                                        ignoreTotal=cms.untracked.int32(0),
+                                        oncePerEventMode = cms.untracked.bool(False)
+                                        )
+										
 process.MessageLogger = cms.Service("MessageLogger",
     debugModules = cms.untracked.vstring('towerMaker', 
         'caloTowers', 
@@ -72,15 +36,50 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 process.Timing = cms.Service("Timing")
 
-process.output = cms.OutputModule("PoolOutputModule",
-#    process.HIRecoObjects,
-    compressionLevel = cms.untracked.int32(2),
-    commitInterval = cms.untracked.uint32(1),
-    fileName = cms.untracked.string('jpsi2muons_PbPb_RECO.root')
+##################################################################################
+
+# setup 'standard'  options
+options = VarParsing.VarParsing ('standard')
+
+# setup any defaults you want
+options.output = 'test_output_RECO.root'
+#options.files= 'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/mc/Summer09/Hydjet_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V3-GaussianVtx_312_ver1/0005/FECC5F18-1982-DE11-ACF9-001EC94BA3AE.root'
+#options.files= 'rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_2_4/RelValHydjetQ_MinBias_4TeV/GEN-SIM-RAW/MC_31X_V3-v1/0010/D62F586C-4E84-DE11-80D3-000423D98E54.root'
+options.files= 'rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_2_4/RelValHydjetQ_B0_4TeV/GEN-SIM-RAW/MC_31X_V3-v1/0010/FC70A0E1-6A84-DE11-AE66-000423D98DB4.root'
+options.maxEvents = 1 
+
+# get and parse the command line arguments
+options.parseArguments()
+
+##################################################################################
+# Input Source
+process.source = cms.Source('PoolSource',
+	fileNames = cms.untracked.vstring(options.files)
+)
+							
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
 )
 
-process.p = cms.Path(process.mix*process.doAllDigi*process.L1Emulator*process.DigiToRaw*process.RawToDigi*process.reconstruct_PbPb)
+##################################################################################
+# Digi + Reconstruction
+
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("RecoHI.Configuration.Reconstruction_HI_cff")
+
+##############################################################################
+# Output EDM File
+process.load("RecoHI.Configuration.RecoHI_EventContent_cff")
+process.output = cms.OutputModule("PoolOutputModule",
+    process.RECODEBUGEventContent,
+    compressionLevel = cms.untracked.int32(2),
+    commitInterval = cms.untracked.uint32(1),
+    fileName = cms.untracked.string(options.output)
+)
+
+##################################################################################
+# Paths
+process.p = cms.Path(process.RawToDigi*process.reconstruct_PbPb)
 process.outpath = cms.EndPath(process.output)
-#process.output.outputCommands.append('keep *_*_*_RECO')
 
 
