f51d3ee13e863fa624b474c2ff73571904a4a13c
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-19 09:21:13 +0000

    hiSelectedTracks label change

--- RecoHI/HiTracking/python/RecoHiTracker_EventContent_cff.py
+++ RecoHI/HiTracking/python/RecoHiTracker_EventContent_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 #Full Event content 
 RecoHiTrackerFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_hiGlobalPrimTracks_*_*', 
-		'keep *_selectHiTracks_*_*', 
+		'keep *_hiSelectedTracks_*_*', 
 		'keep *_hiPixel3PrimTracks_*_*', 		
 		'keep recoVertexs_hiPixelMedianVertex_*_*',
 		'keep recoVertexs_hiPixelAdaptiveVertex_*_*'		
@@ -12,7 +12,7 @@ RecoHiTrackerFEVT = cms.PSet(
 #RECO content
 RecoHiTrackerRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_hiGlobalPrimTracks_*_*', 
-		'keep *_selectHiTracks_*_*', 
+		'keep *_hiSelectedTracks_*_*', 
 		'keep *_hiPixel3PrimTracks_*_*', 		
 		'keep recoVertexs_hiPixelMedianVertex_*_*',
 		'keep recoVertexs_hiPixelAdaptiveVertex_*_*'		
@@ -20,7 +20,7 @@ RecoHiTrackerRECO = cms.PSet(
 )
 #AOD content
 RecoHiTrackerAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoTracks_selectHiTracks_*_*',
+    outputCommands = cms.untracked.vstring('keep recoTracks_hiSelectedTracks_*_*',
 		'keep recoVertexs_hiPixelMedianVertex_*_*',
 		'keep recoVertexs_hiPixelAdaptiveVertex_*_*'		
     )
