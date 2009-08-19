50d20878a6b9fbad25efaa98e8b41ecc63509a69
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-08-19 14:10:52 +0000

    Add commented out config for new sistrip::DigiToRaw

--- EventFilter/SiStripRawToDigi/python/SiStripDigiToRaw_cfi.py
+++ EventFilter/SiStripRawToDigi/python/SiStripDigiToRaw_cfi.py
@@ -3,6 +3,17 @@ import FWCore.ParameterSet.Config as cms
 # WARNING: PPRESENTLY USING OLD DIGI-TO-RAW MODULE!!!
 # WARNING: SHOULD MIGRATE TO NEW sistrip::DigiToRaw MODULE ONCE VALIDATED!
 
+""" #Config for sistrip::DigiToRaw
+SiStripDigiToRaw = cms.EDProducer(
+    "sistrip::DigiToRawModule",
+    InputModuleLabel = cms.string('simSiStripDigis'),
+    InputDigiLabel = cms.string('ZeroSuppressed'),
+    FedReadoutMode = cms.string('ZERO_SUPPRESSED'),
+    UseFedKey = cms.bool(False),
+    UseWrongDigiType = cms.bool(False)
+    )
+"""
+
 SiStripDigiToRaw = cms.EDProducer(
     "OldSiStripDigiToRawModule",
     InputModuleLabel = cms.string('simSiStripDigis'),
