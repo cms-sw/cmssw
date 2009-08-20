779f6c4c08e5b10298ed6a38c5fbb719ec076f38
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-08-20 13:47:37 +0000

    Start of a unit test for ResourceMonitorCollection.

---
+++ EventFilter/StorageManager/test/ResourceMonitorCollection_t.cpp
@@ -0,0 +1,94 @@
+#include "Utilities/Testing/interface/CppUnit_testdriver.icpp"
+#include "cppunit/extensions/HelperMacros.h"
+
+#include "EventFilter/StorageManager/interface/ResourceMonitorCollection.h"
+#include "EventFilter/StorageManager/test/MockAlarmHandler.h"
+
+using namespace stor;
+
+class stor::testResourceMonitorCollection : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(testResourceMonitorCollection);
+
+  CPPUNIT_TEST(sataBeasts);
+  CPPUNIT_TEST(sataBeastsOnSpecialNode);
+
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp();
+  void tearDown();
+
+  void sataBeasts();
+  void sataBeastsOnSpecialNode(); // can only be used on specially perpared SATA beast host
+
+private:
+
+  boost::shared_ptr<MockAlarmHandler> _ah;
+  ResourceMonitorCollection* _rmc;
+
+};
+
+void
+testResourceMonitorCollection::setUp()
+{
+  _ah.reset(new MockAlarmHandler());
+  _rmc = new ResourceMonitorCollection(1, _ah);
+}
+
+void
+testResourceMonitorCollection::tearDown()
+{
+  delete _rmc;
+}
+
+
+void
+testResourceMonitorCollection::sataBeasts()
+{
+  _rmc->checkSataBeasts();
+  CPPUNIT_ASSERT( _rmc->_sataBeastStatus == 0 );
+  CPPUNIT_ASSERT( _ah->noAlarmSet() );
+}
+
+
+void
+testResourceMonitorCollection::sataBeastsOnSpecialNode()
+{
+  ResourceMonitorCollection::SATABeasts sataBeasts;
+  bool foundSataBeasts =
+    _rmc->getSataBeasts(sataBeasts);
+  CPPUNIT_ASSERT( foundSataBeasts );
+  CPPUNIT_ASSERT( sataBeasts.size() == 1 );
+  std::string sataBeast = *(sataBeasts.begin());
+  CPPUNIT_ASSERT( sataBeast == "satab-c2c07-06" );
+
+  CPPUNIT_ASSERT(! _rmc->checkSataDisks(sataBeast,"-00.cms") );
+  CPPUNIT_ASSERT( _rmc->checkSataDisks(sataBeast,"-10.cms") );
+
+  CPPUNIT_ASSERT( _rmc->_sataBeastStatus == 101 );
+
+  std::vector<MockAlarmHandler::Alarms> alarms;
+  bool alarmsAreSet = _ah->getActiveAlarms(sataBeast, alarms);
+  CPPUNIT_ASSERT( alarmsAreSet );
+
+  std::cout << "\nActive alarms for " << sataBeast << std::endl;
+  for (std::vector<MockAlarmHandler::Alarms>::iterator it = alarms.begin(),
+         itEnd = alarms.end();
+       it != itEnd;
+       ++it)
+  {
+    std::cout << "   " << it->first << "\t" << it->second.message() << std::endl;
+  }
+}
+
+// This macro writes the 'main' for this test.
+CPPUNIT_TEST_SUITE_REGISTRATION(testResourceMonitorCollection);
+
+
+/// emacs configuration
+/// Local Variables: -
+/// mode: c++ -
+/// c-basic-offset: 2 -
+/// indent-tabs-mode: nil -
+/// End: -
