98491ace6e46bc27640519ef9716ac3828b33172
Yongsun Kim <sha1-cf1309631de6d8b3179fc471ac6b6474eee4ad5d@cern.ch>
2009-08-20 22:35:35 +0000

    RecoHiEgamma_EventContent_cff.py

--- RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
+++ RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
@@ -1,11 +1,32 @@
-# Full Ecal Event content 
+import FWCore.ParameterSet.Config as cms
+
 RecoHiEgammaFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep recoSuperClusters_*_*_*',
-    'keep recoCaloClusters_*_*_*',
-    'keep recoClusterShapes_*_*_*',
-    'keep recoCaloClustersToOnerecoClusterShapesAssociation_*_*_*',
-    'keep *_ecalRecHit_*_*',
-    )                                                                 
-)
+    'keep recoTracks_globalPrimTracks_*_*',
+    'keep EcalRecHitsSorted_*_*_*',
+    'keep recoVertexs_pixel3Vertices_*_*'
+    'keep recoPhotons_photons_*_*',
+    'keep floatedmValueMap_*_*_*',
+    )
+    )
 
+RecoHiEgammaRECO = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+    'keep recoSuperClusters_*_*_*',
+    'keep recoTracks_globalPrimTracks_*_*',
+    'keep EcalRecHitsSorted_*_*_*',
+    'keep recoVertexs_pixel3Vertices_*_*'
+    'keep *_hfreco_*_*',
+    'keep *_horeco_*_*',
+    'keep *_hbhereco_*_*',
+    )
+    )
+
+RecoHiEgammaAOD = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+    'keep recoPhotons_photons_*_*',
+    'keep floatedmValueMap_*_*_*',
+    'keep *_genParticles_*_*'
+    )
+    )
