10d1b67ae7020245662dffaf88830cde50bb2c50
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-20 11:22:17 +0000

    changed spelling of event content and removed redundant keep commands

--- RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
+++ RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
@@ -1,23 +1,11 @@
 # Full Ecal Event content 
-RecoHIEgammaFEVT = cms.PSet(
+RecoHiEgammaFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep recoSuperClusters_*_*_*',
     'keep recoCaloClusters_*_*_*',
     'keep recoClusterShapes_*_*_*',
     'keep recoCaloClustersToOnerecoClusterShapesAssociation_*_*_*',
     'keep *_ecalRecHit_*_*',
-    'keep *_ecalPreshowerRecHit_*_*',
-    'keep *_hfreco_*_*',
-    'keep *_horeco_*_*',
-    'keep *_hbhereco_*_*',
-    'keep *_globalPrimTracks_*_*',
-    'keep *_photons_*_*',
-    'keep *_photonCore_*_*',
-    'keep *_genParticles_*_*',
-    'keep *_hiGenParticles_*_*',
-    'keep edmHepMCProduct_*_*_*'
-    
-
     )                                                                 
 )
 
