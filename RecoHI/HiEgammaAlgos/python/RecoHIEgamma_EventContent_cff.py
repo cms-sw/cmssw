e87727b090bb77e8321d384c6d74d7b364b65c70
Yongsun Kim <sha1-cf1309631de6d8b3179fc471ac6b6474eee4ad5d@cern.ch>
2009-08-20 10:10:40 +0000

    *** empty log message ***

--- RecoHI/HiEgammaAlgos/python/RecoHIEgamma_EventContent_cff.py
+++ RecoHI/HiEgammaAlgos/python/RecoHIEgamma_EventContent_cff.py
@@ -1,13 +1,12 @@
 # Full Ecal Event content 
 RecoHIEgammaFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
-    'keep *_islandBasicClusters_*_*',
-    'keep *_multi5x5BasicClusters_*_*',
+    'keep recoSuperClusters_*_*_*',
+    'keep recoCaloClusters_*_*_*',
+    'keep recoClusterShapes_*_*_*',
+    'keep recoCaloClustersToOnerecoClusterShapesAssociation_*_*_*',
     'keep *_ecalRecHit_*_*',
     'keep *_ecalPreshowerRecHit_*_*',
-    'keep *_simEcalTriggerPrimitiveDigis_*_*',
-    'keep *_ecalDigis_*_*',
-    'keep *_ecalGlobalUncalibRecHit_*_*',
     'keep *_hfreco_*_*',
     'keep *_horeco_*_*',
     'keep *_hbhereco_*_*',
@@ -15,8 +14,9 @@ RecoHIEgammaFEVT = cms.PSet(
     'keep *_photons_*_*',
     'keep *_photonCore_*_*',
     'keep *_genParticles_*_*',
-    'keep *_hiGenParticles_*_*'
-    'keep *_pixel3Vertices_*_*'
+    'keep *_hiGenParticles_*_*',
+    'keep edmHepMCProduct_*_*_*'
+    
 
     )                                                                 
 )
