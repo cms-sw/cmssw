e8588ef72a583aaf8e4b1c8e9cb85ab7667d823b
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-21 11:19:47 +0000

    cleaned up duplicate content.  added isolation info to RECO in case this is available at this step in the future

--- RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
+++ RecoHI/HiEgammaAlgos/python/RecoHiEgamma_EventContent_cff.py
@@ -3,30 +3,21 @@ import FWCore.ParameterSet.Config as cms
 RecoHiEgammaFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep recoSuperClusters_*_*_*',
-    'keep recoTracks_globalPrimTracks_*_*',
     'keep EcalRecHitsSorted_*_*_*',
-    'keep recoVertexs_pixel3Vertices_*_*'
-    'keep recoPhotons_photons_*_*',
-    'keep floatedmValueMap_*_*_*',
+    'keep floatedmValueMap_*_*_*'
     )
     )
 
 RecoHiEgammaRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep recoSuperClusters_*_*_*',
-    'keep recoTracks_globalPrimTracks_*_*',
     'keep EcalRecHitsSorted_*_*_*',
-    'keep recoVertexs_pixel3Vertices_*_*'
-    'keep *_hfreco_*_*',
-    'keep *_horeco_*_*',
-    'keep *_hbhereco_*_*',
+    'keep floatedmValueMap_*_*_*'  # isolation not created yet in RECO step, but in case it is later
     )
     )
 
 RecoHiEgammaAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
-    'keep recoPhotons_photons_*_*',
     'keep floatedmValueMap_*_*_*',
-    'keep *_genParticles_*_*'
     )
     )
