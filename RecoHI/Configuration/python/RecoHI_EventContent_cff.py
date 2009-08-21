2aca715992bc30897c47a756c5e19f30ccec0d1e
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-08-21 12:31:03 +0000

    added RecoHiEgamma eventcontent

--- RecoHI/Configuration/python/RecoHI_EventContent_cff.py
+++ RecoHI/Configuration/python/RecoHI_EventContent_cff.py
@@ -6,7 +6,7 @@ from Configuration.EventContent.EventContent_cff import *
 # include from RecoHI packages
 from RecoHI.HiTracking.RecoHiTracker_EventContent_cff import *
 from RecoHI.HiJetAlgos.RecoHiJets_EventContent_cff import *
-#from RecoHI.HiEgammaAlgos.RecoHiEgamma_EventContent_cff import *
+from RecoHI.HiEgammaAlgos.RecoHiEgamma_EventContent_cff import *
 #from RecoHI.HiCentralityAlgos.RecoHiCentrality_EventContent_cff import *
 #from RecoHI.HiEvtPlaneAlgos.RecoHiEvtPlane_EventContent_cff import *
 #from RecoHI.HiMuonAlgos.RecoHiMuon_EventContent_cff import *
@@ -29,24 +29,32 @@ from RecoHI.HiJetAlgos.RecoHiJets_EventContent_cff import *
 # extend existing data tiers with HI-specific content
 RECOEventContent.outputCommands.extend(RecoHiTrackerRECO.outputCommands)
 RECOEventContent.outputCommands.extend(RecoHiJetsRECO.outputCommands)
+RECOEventContent.outputCommands.extend(RecoHiEgammaRECO.outputCommands)
 
 RECOSIMEventContent.outputCommands.extend(RecoHiTrackerRECO.outputCommands)
 RECOSIMEventContent.outputCommands.extend(RecoHiJetsRECO.outputCommands)
+RECOSIMEventContent.outputCommands.extend(RecoHiEgammaRECO.outputCommands)
 
 RECODEBUGEventContent.outputCommands.extend(RecoHiTrackerRECO.outputCommands)
 RECODEBUGEventContent.outputCommands.extend(RecoHiJetsRECO.outputCommands)
+RECODEBUGEventContent.outputCommands.extend(RecoHiEgammaRECO.outputCommands)
 
 AODEventContent.outputCommands.extend(RecoHiTrackerAOD.outputCommands)
 AODEventContent.outputCommands.extend(RecoHiJetsRECO.outputCommands)
+AODEventContent.outputCommands.extend(RecoHiEgammaAOD.outputCommands)
 
 AODSIMEventContent.outputCommands.extend(RecoHiTrackerAOD.outputCommands)
 AODSIMEventContent.outputCommands.extend(RecoHiJetsRECO.outputCommands)
+AODSIMEventContent.outputCommands.extend(RecoHiEgammaAOD.outputCommands)
 
 FEVTEventContent.outputCommands.extend(RecoHiTrackerFEVT.outputCommands)
 FEVTEventContent.outputCommands.extend(RecoHiJetsFEVT.outputCommands)
+FEVTEventContent.outputCommands.extend(RecoHiEgammaFEVT.outputCommands)
 
 FEVTSIMEventContent.outputCommands.extend(RecoHiTrackerFEVT.outputCommands)
 FEVTSIMEventContent.outputCommands.extend(RecoHiJetsFEVT.outputCommands)
+FEVTSIMEventContent.outputCommands.extend(RecoHiEgammaFEVT.outputCommands)
 
 FEVTDEBUGEventContent.outputCommands.extend(RecoHiTrackerFEVT.outputCommands)
 FEVTDEBUGEventContent.outputCommands.extend(RecoHiJetsFEVT.outputCommands)
+FEVTDEBUGEventContent.outputCommands.extend(RecoHiEgammaFEVT.outputCommands)
