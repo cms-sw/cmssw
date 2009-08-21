8b7dec9ea5e64402b54f1f8b01f8a979488ec3f6
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-08-21 14:17:54 +0000

    CSC offline harvesting client renamed, diverged configurations for cosmics and collisions

--- DQM/CSCMonitorModule/python/csc_dqm_offlineclient_collisions_cff.py
+++ DQM/CSCMonitorModule/python/csc_dqm_offlineclient_collisions_cff.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.CSCMonitorModule.csc_dqm_offlineclient_cfi import *
 
-cscOfflineSources = cms.Sequence(dqmCSCOfflineClient)
+cscOfflineCollisionsClients = cms.Sequence(dqmCSCOfflineClient)
