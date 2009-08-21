a203f5c33a756708a101f15437fcf0be5fb4f53a
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2009-08-21 20:38:20 +0000

    Added bins to 500 for pT turn-on curves; modified eta and phi binning

--- HLTriggerOffline/Muon/python/muonTriggerRateTimeAnalyzer_cfi.py
+++ HLTriggerOffline/Muon/python/muonTriggerRateTimeAnalyzer_cfi.py
@@ -23,11 +23,12 @@ muonTriggerRateTimeAnalyzer = cms.EDAnalyzer("MuonTriggerRateTimeAnalyzer",
                           1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
                          11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                          22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
-                         45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100
-                       ),                       
+                         45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+                         110, 120, 140, 170, 200, 250, 300, 380, 500
+                       ), 
     PtParameters       = cms.vdouble(50,0.,1000.),
-    EtaParameters      = cms.vdouble(50,-2.1,2.1),
-    PhiParameters      = cms.vdouble(50,-3.15,3.15),
+    EtaParameters      = cms.vdouble(42,-2.1,2.1),
+    PhiParameters      = cms.vdouble(50,-3.142,3.142),
 
     # Set cuts placed on the generated muons and matching criteria
     # Use pt cut just below 10 to allow through SingleMuPt10 muons  
