1cfd0754edaaf25223c36d9b3671feb8ce6ea6d6
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-08-24 20:13:45 +0000

    Move DQM style plugins to plugins/ directory

--- CalibTracker/SiStripLorentzAngle/plugins/modules.cc
+++ CalibTracker/SiStripLorentzAngle/plugins/modules.cc
@@ -11,3 +11,10 @@ DEFINE_ANOTHER_FWK_MODULE(sistrip::EnsembleCalibrationLA);
 
 #include "CalibTracker/SiStripLorentzAngle/plugins/MeasureLA.h"
 DEFINE_ANOTHER_FWK_EVENTSETUP_MODULE(sistrip::MeasureLA);
+
+////////
+#include "CalibTracker/SiStripLorentzAngle/interface/SiStripCalibLorentzAngle.h"
+DEFINE_FWK_MODULE(SiStripCalibLorentzAngle);
+
+#include "CalibTracker/SiStripLorentzAngle/interface/SiStripLAProfileBooker.h"
+DEFINE_FWK_MODULE(SiStripLAProfileBooker);
