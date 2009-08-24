3cfe81b93b25264367beccc4ce69911bf0adc16d
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-08-24 18:48:12 +0000

    re-enabled trigprim monitor

--- DQM/HcalMonitorModule/python/hcal_dqm_client_fileT0_cff.py
+++ DQM/HcalMonitorModule/python/hcal_dqm_client_fileT0_cff.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.HcalMonitorClient.HcalMonitorClient_cfi import *
 hcalOfflineDQMClient = cms.Sequence(hcalClient)
-#hcalClient.plotPedRAW        = True
-#hcalClient.DoPerChanTests    = False
+
 hcalClient.baseHtmlDir       = ''
 hcalClient.SummaryClient     = True
 hcalClient.DataFormatClient  = True
@@ -11,7 +10,7 @@ hcalClient.DigiClient        = True
 hcalClient.RecHitClient      = True
 hcalClient.DeadCellClient    = True
 hcalClient.HotCellClient     = True
-hcalClient.TrigPrimClient    = False # disabled until we find why it crashes
+hcalClient.TrigPrimClient    = True
 hcalClient.PedestalClient    = False
 hcalClient.LEDClient         = False
 hcalClient.CaloTowerClient   = False
