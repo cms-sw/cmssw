505d4235c6711507c320034fc69c3e3b8d2a87e5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-08-25 13:44:24 +0000

    fix config

--- CondCore/Utilities/bin/cmscond_duplicate_iov.cpp
+++ CondCore/Utilities/bin/cmscond_duplicate_iov.cpp
@@ -26,7 +26,6 @@
 #include "CondCore/DBCommon/interface/ObjectRelationalMappingUtility.h"
 #include "CondCore/IOVService/interface/IOVNames.h"
 
-#include "FWCore/PluginManager/interface/SharedLibrary.h"
 #include "FWCore/PluginManager/interface/PluginManager.h"
 #include "FWCore/PluginManager/interface/standard.h"
 
@@ -41,6 +40,9 @@
 
 
 int main( int argc, char** argv ){
+  edmplugin::PluginManager::Config config;
+  edmplugin::PluginManager::configure(edmplugin::standard::config());
+
   cond::CommonOptions myopt("cmscond_duplicate_iov");
   myopt.addConnect();
   myopt.addAuthentication(true);
