8bc04c95a9b2fb0637ab25bfd82cf27b83beee75
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-08-25 18:20:15 +0000

    Add utility functions to translate and print GLPK codes

--- RecoLocalMuon/DTSegment/src/DTLPPatternRecoAlgorithm.h
+++ RecoLocalMuon/DTSegment/src/DTLPPatternRecoAlgorithm.h
@@ -8,7 +8,7 @@ namespace DTLPPatternReco {
     ~ResultLPAlgo(){lambdas.clear();};
     double mVar;
     double qVar;
-      double chi2Var;
+    double chi2Var;
     std::vector<int> lambdas;  
   };
 }
@@ -20,3 +20,9 @@ bool lpAlgorithm(DTLPPatternReco::ResultLPAlgo& theAlgoResults,
 		 const double m_min, const double m_max,
 		 const double q_min, const double q_max,
 		 const double BIG_M, const double theDeltaFactor);
+
+void printGLPReturnCode(int returnCode);
+
+void printGLPSolutionStatus(int status);
+
+void printLPXReturnCode(int returnCode);
