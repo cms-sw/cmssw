1005bbd00741da24b51115f12d29693742ef2315
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-08-25 08:57:49 +0000

    ported changes from B22X_v2_PatTutorial_June2009

--- TopQuarkAnalysis/Examples/plugins/HypothesisAnalyzer.h
+++ TopQuarkAnalysis/Examples/plugins/HypothesisAnalyzer.h
@@ -4,12 +4,7 @@
 #include "TH1F.h"
 #include "TH2F.h"
 
-#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
 
 class HypothesisAnalyzer : public edm::EDAnalyzer {
 
@@ -28,14 +23,27 @@ class HypothesisAnalyzer : public edm::EDAnalyzer {
   edm::InputTag hypoClassKey_;
 
   TH1F* hadWPt_;
+  TH1F* hadWEta_;
   TH1F* hadWMass_;
+
+  TH1F* hadWPullPt_;
+  TH1F* hadWPullEta_;
+  TH1F* hadWPullMass_;
+
   TH1F* hadTopPt_;
+  TH1F* hadTopEta_;
   TH1F* hadTopMass_;
 
-  TH1F* lepWPt_;
-  TH1F* lepWMass_;
-  TH1F* lepTopPt_;
-  TH1F* lepTopMass_;
+  TH1F* hadTopPullPt_;
+  TH1F* hadTopPullEta_;
+  TH1F* hadTopPullMass_;
+
+  TH1F* genMatchDr_;
+  TH1F* mvaDisc_;
+
+  TH2F* genMatchDrVsHadTopPullMass_;
+  TH2F* mvaDiscVsHadTopPullMass_;
+
 };
 
 #endif
