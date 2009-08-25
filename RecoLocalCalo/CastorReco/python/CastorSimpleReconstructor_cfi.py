d7dcb367b6b03411648434cf95b23c7253e53244
Silvia Ochesanu <sha1-8187e4cdd06700eabaa2c7f22b4e81482bf9c724@cern.ch>
2009-08-25 12:59:42 +0000

    fix label

--- RecoLocalCalo/CastorReco/python/CastorSimpleReconstructor_cfi.py
+++ RecoLocalCalo/CastorReco/python/CastorSimpleReconstructor_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 castorreco = cms.EDFilter("CastorSimpleReconstructor",
     correctionPhaseNS = cms.double(0.0),
-    digiLabel = cms.InputTag("castorDigis"),
+    digiLabel = cms.InputTag("simCastorDigis"),
     samplesToAdd = cms.int32(1),
     Subdetector = cms.string('CASTOR'),
     firstSample = cms.int32(3),
