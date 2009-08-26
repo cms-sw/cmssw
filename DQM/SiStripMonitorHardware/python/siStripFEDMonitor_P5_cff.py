1f72ad2ea1d0ee4d242feb4e6a4a8652adb1c621
Anne-Marie Magnan <sha1-4ead6f5ca8b64f8c6a1efe9d1d4dd2e2421521d1@cern.ch>
2009-08-26 12:25:35 +0000

    add APVError histo in P5+Tier0 config

--- DQM/SiStripMonitorHardware/python/siStripFEDMonitor_P5_cff.py
+++ DQM/SiStripMonitorHardware/python/siStripFEDMonitor_P5_cff.py
@@ -101,10 +101,10 @@ siStripFEDMonitor.nAPVStatusBitHistogramConfig = cms.untracked.PSet(
   #Max = cms.untracked.double(500)
 )
 siStripFEDMonitor.nAPVErrorHistogramConfig = cms.untracked.PSet(
-  Enabled = cms.untracked.bool(False),
-  #NBins = cms.untracked.uint32(250),
-  #Min = cms.untracked.double(0),
-  #Max = cms.untracked.double(500)
+  Enabled = cms.untracked.bool(True),
+  NBins = cms.untracked.uint32(250),
+  Min = cms.untracked.double(0),
+  Max = cms.untracked.double(500)
 )
 siStripFEDMonitor.nAPVAddressErrorHistogramConfig = cms.untracked.PSet(
   Enabled = cms.untracked.bool(True),
@@ -161,10 +161,10 @@ siStripFEDMonitor.nAPVStatusBitvsTimeHistogramConfig = cms.untracked.PSet(
   #Max = cms.untracked.double(3600)
 )
 siStripFEDMonitor.nAPVErrorvsTimeHistogramConfig = cms.untracked.PSet(
-  Enabled = cms.untracked.bool(False),
-  #NBins = cms.untracked.uint32(600),
-  #Min = cms.untracked.double(0),
-  #Max = cms.untracked.double(3600)
+  Enabled = cms.untracked.bool(True),
+  NBins = cms.untracked.uint32(600),
+  Min = cms.untracked.double(0),
+  Max = cms.untracked.double(3600)
 )
 siStripFEDMonitor.nAPVAddressErrorvsTimeHistogramConfig = cms.untracked.PSet(
   Enabled = cms.untracked.bool(True),
--- DQM/SiStripMonitorHardware/python/siStripFEDMonitor_Tier0_cff.py
+++ DQM/SiStripMonitorHardware/python/siStripFEDMonitor_Tier0_cff.py
@@ -103,10 +103,10 @@ siStripFEDMonitor.nAPVStatusBitHistogramConfig = cms.untracked.PSet(
   #Max = cms.untracked.double(500)
 )
 siStripFEDMonitor.nAPVErrorHistogramConfig = cms.untracked.PSet(
-  Enabled = cms.untracked.bool(False),
-  #NBins = cms.untracked.uint32(250),
-  #Min = cms.untracked.double(0),
-  #Max = cms.untracked.double(500)
+  Enabled = cms.untracked.bool(True),
+  NBins = cms.untracked.uint32(250),
+  Min = cms.untracked.double(0),
+  Max = cms.untracked.double(500)
 )
 siStripFEDMonitor.nAPVAddressErrorHistogramConfig = cms.untracked.PSet(
   Enabled = cms.untracked.bool(True),
