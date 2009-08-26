110514807889c7b5a5f0b1eedb160e6ecb6c6c2c
Anne-Marie Magnan <sha1-4ead6f5ca8b64f8c6a1efe9d1d4dd2e2421521d1@cern.ch>
2009-08-26 12:38:29 +0000

    disable debug output completely for P5

--- DQM/SiStripMonitorHardware/python/siStripFEDMonitor_P5_cff.py
+++ DQM/SiStripMonitorHardware/python/siStripFEDMonitor_P5_cff.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.SiStripMonitorHardware.siStripFEDMonitor_cfi import *
 
+#disable error output: enabled by default.
+siStripFEDMonitor.PrintDebugMessages = 0
 #Global/summary histograms
 siStripFEDMonitor.DataPresentHistogramConfig.Enabled = True
 siStripFEDMonitor.AnyFEDErrorsHistogramConfig.Enabled = True
