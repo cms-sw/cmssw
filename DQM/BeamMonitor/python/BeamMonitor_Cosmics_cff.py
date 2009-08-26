5dbbb3721d5535a837cd83c14cc32af538b2e918
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-08-26 06:41:24 +0000

    add cff file

---
+++ DQM/BeamMonitor/python/BeamMonitor_Cosmics_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+dqmBeamMonitor = cms.EDFilter("BeamMonitor",
+                              monitorName = cms.untracked.string('BeamMonitor'),
+                              beamSpot = cms.untracked.string('offlineBeamSpot'), ## hltOfflineBeamSpot for HLTMON
+                              fitEveryNLumi = cms.untracked.int32(1),
+                              resetEveryNLumi = cms.untracked.int32(5),
+                              Debug = cms.untracked.bool(False),
+                              BeamFitter = cms.PSet(
+        			Debug = cms.untracked.bool(False),
+        			TrackCollection = cms.untracked.InputTag('ctfWithMaterialTracksP5'), ## ctfWithMaterialTracksP5 for CRAFT
+                                WriteAscii = cms.untracked.bool(False),
+                                AsciiFileName = cms.untracked.string('BeamFit.txt'),
+                                MinimumPt = cms.untracked.double(1.2),
+                                MaximumEta = cms.untracked.double(2.4),
+                                MinimumTotalLayers = cms.untracked.int32(0),
+                                MinimumPixelLayers = cms.untracked.int32(0),
+                                MaximumNormChi2 = cms.untracked.double(100.0),
+                                TrackAlgorithm = cms.untracked.vstring(), ## ctf,rs,cosmics,iter0,iter1...; for all algos, leave it blank
+                                TrackQuality = cms.untracked.vstring(), ## loose, tight, highPurity...; for all qualities, leave it blank
+			        InputBeamWidth = cms.untracked.double(-1.0) ## if -1 use the value calculated by the analyzer
+                                ),
+                              dxBin = cms.int32(200),
+                              dxMin = cms.double(-1.0),
+                              dxMax = cms.double(1.0),
+
+                              vxBin = cms.int32(100),
+                              vxMin = cms.double(-.1),
+                              vxMax = cms.double(.1),
+                              
+                              phiBin = cms.int32(63),
+                              phiMin = cms.double(-3.15),
+                              phiMax = cms.double(3.15)
+                              )
