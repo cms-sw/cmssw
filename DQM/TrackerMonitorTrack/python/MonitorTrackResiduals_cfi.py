bd837565a6379198ccfda299145ef1c02721c70b
Johannes Hauk <sha1-79adb14f6701f120a4bd8908168f6f2ed5256e2c@cern.ch>
2009-08-28 17:05:26 +0000

    Optimized binning of histograms

--- DQM/TrackerMonitorTrack/python/MonitorTrackResiduals_cfi.py
+++ DQM/TrackerMonitorTrack/python/MonitorTrackResiduals_cfi.py
@@ -9,14 +9,14 @@ MonitorTrackResiduals = cms.EDFilter("MonitorTrackResiduals",
     OutputFileName = cms.string('test_monitortracks.root'),
     # bining and range for absolute and normalized residual histogramms
     TH1ResModules = cms.PSet(
-        xmin = cms.double(-2.0),
-        Nbinx = cms.int32(120),
-        xmax = cms.double(2.0)
+        xmin = cms.double(-0.05),   # native unit in CMS is [cm], so these are 500um
+        Nbinx = cms.int32(100),
+        xmax = cms.double(0.05)
     ),
     TH1NormResModules = cms.PSet(
-        xmin = cms.double(-3.0),
-        Nbinx = cms.int32(120),
-        xmax = cms.double(3.0)
+        xmin = cms.double(-5.0),
+        Nbinx = cms.int32(100),
+        xmax = cms.double(5.0)
     ),
     # input for Tracks and Trajectories, should be TrackRefitter
     # or similar
