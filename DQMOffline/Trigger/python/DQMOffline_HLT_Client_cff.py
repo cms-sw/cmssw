40ba278476d7dfd3459487f91cd1572c907dd860
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-08-28 13:09:44 +0000

    Added Tag and probe to output path

--- DQMOffline/Trigger/python/DQMOffline_HLT_Client_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_HLT_Client_cff.py
@@ -5,6 +5,7 @@ from DQM.HLTEvF.HLTEventInfoClient_cfi import *
 from DQMOffline.Trigger.FourVectorHLTOfflineClient_cff import *
 from DQMOffline.Trigger.EgHLTOfflineClient_cfi import *
 from DQMOffline.Trigger.MuonPostProcessor_cfi import *
+from DQMOffline.Trigger.BPAGPostProcessor_cff import *
 from DQMOffline.Trigger.JetMETHLTOfflineClient_cfi import *
 
 #    # use include file for dqmEnv 
@@ -12,4 +13,4 @@ from DQMServices.Components.DQMEnvironment_cfi import *
 dqmEnv.subSystemFolder = 'HLT'
 
 #hltOfflineDQMClient = cms.Sequence(hltFourVectorSeqClient*egHLTOffDQMClient*hltEventInfoClient*hLTMuonPostVal*jetMETHLTOfflineClient*dqmEnv)
-hltOfflineDQMClient = cms.Sequence(hltFourVectorSeqClient*egHLTOffDQMClient*hltEventInfoClient*hLTMuonPostVal*jetMETHLTOfflineClient*dqmEnv)
+hltOfflineDQMClient = cms.Sequence(hltFourVectorSeqClient*egHLTOffDQMClient*hltEventInfoClient*hLTMuonPostVal*bPAGPostProcessor*jetMETHLTOfflineClient*dqmEnv)
