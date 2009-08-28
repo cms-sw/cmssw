e16393052805de1ba207acb42ac3b4ff62fe58b7
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2009-08-28 13:30:43 +0000

    changed the logic of how the field is estimated - seems to work even with HLT muons

--- Fireworks/Tracks/interface/estimate_field.h
+++ Fireworks/Tracks/interface/estimate_field.h
@@ -4,33 +4,15 @@
 //
 // Package:     Tracks
 // Class  :     estimate_field
+// $Id: estimate_field.h,v 1.2 2009/01/23 21:35:46 amraktad Exp $
 //
-/**\class estimate_field estimate_field.h Fireworks/Tracks/interface/estimate_field.h
-
-   Description: estimate the magnetic field strength based on the curvature of a reco::Track
-
-   Usage:
-    <usage>
-
- */
-//
-// Original Author:  Chris Jones
-//         Created:  Tue Jan  6 16:15:51 EST 2009
-// $Id: estimate_field.h,v 1.1 2009/01/06 21:38:41 chrjones Exp $
-//
-
-// system include files
-
-// user include files
-
-// forward declarations
-
 namespace reco {
    class Track;
 }
 
 namespace fw {
    double estimate_field( const reco::Track& track );
+   double estimate_field( double vx1, double vy1, double vx2, double vy2, double px, double py );
 }
 
 #endif
--- Fireworks/Tracks/src/estimate_field.cc
+++ Fireworks/Tracks/src/estimate_field.cc
@@ -2,42 +2,56 @@
 //
 // Package:     Tracks
 // Class  :     estimate_field
-//
-// Implementation:
-//     <Notes on implementation>
-//
-// Original Author:  Chris Jones
-//         Created:  Tue Jan  6 16:15:57 EST 2009
-// $Id: estimate_field.cc,v 1.1 2009/01/06 21:38:41 chrjones Exp $
+// $Id: estimate_field.cc,v 1.2 2009/01/23 21:35:47 amraktad Exp $
 //
 
-// system include files
 #include "Math/Vector3D.h"
 #include "DataFormats/TrackReco/interface/Track.h"
-
-// user include files
 #include "Fireworks/Tracks/interface/estimate_field.h"
-
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
 
 double
 fw::estimate_field( const reco::Track& track )
 {
    if ( !track.extra().isAvailable() ) return -1;
-   math::XYZVector displacement(track.outerPosition().x()-track.innerPosition().x(),
-                                track.outerPosition().y()-track.innerPosition().y(),
-                                0);
-   math::XYZVector transverseMomentum(track.innerMomentum().x(),
-                                      track.innerMomentum().y(),
-                                      0);
-   double cosAlpha = transverseMomentum.Dot(displacement)/transverseMomentum.r()/displacement.r();
-   return 200*sqrt(1-cosAlpha*cosAlpha)/0.2998*transverseMomentum.r()/displacement.r();
+   // We have 3 states so we can form 3 pairs to check for curvature
+   // In practice we want to avoid estimates over lost of material and
+   // in regions with small field. Therefore we should look at the pair
+   // based on POCA and closest state to it. Than we will estimate the
+   // filed inside the solenoid. 
+   if ( pow(track.outerPosition().x()-track.vx(),2)+pow(track.outerPosition().y()-track.vy(),2) <
+	pow(track.innerPosition().x()-track.vx(),2)+pow(track.innerPosition().y()-track.vy(),2) )
+     {
+       double estimate = estimate_field( track.outerPosition().x(), 
+					 track.outerPosition().y(),
+					 track.vx(),
+					 track.vy(),
+					 track.px(),
+					 track.py() );
+       /* printf("outer-poca: \tPOCA Pt: %0.2f, \tinner Rho: %0.1f, \touter Rho: %0.1f, \tchange in pt: %0.1f%%, \testimate: %0.2f\n",
+	      track.pt(), track.innerPosition().rho(), track.outerPosition().rho(), 
+	      fabs( track.outerMomentum().rho()/track.pt()-1 )*100, estimate); */
+       return estimate;
+     }
+   else
+     {
+       double estimate = estimate_field( track.innerPosition().x(), 
+					 track.innerPosition().y(),
+					 track.vx(),
+					 track.vy(),
+					 track.px(),
+					 track.py() );
+       /* printf("inner-poca: \tPOCA Pt: %0.2f, \tinner Rho: %0.1f, \touter Rho: %0.1f, \tchange in pt: %0.1f%%, \testimate: %0.2f\n",
+	  track.pt(), track.innerPosition().rho(), track.outerPosition().rho(), 
+	  fabs( track.innerMomentum().rho()/track.pt()-1 )*100, estimate); */
+       return estimate;
+     }
 }
 
+double
+fw::estimate_field( double vx1, double vy1, double vx2, double vy2, double px, double py )
+{
+  math::XYZVector displacement(vx2-vx1, vy2-vy1, 0);
+  math::XYZVector transverseMomentum(px, py, 0);
+  double cosAlpha = transverseMomentum.Dot(displacement)/transverseMomentum.r()/displacement.r();
+  return 200*sqrt(1-cosAlpha*cosAlpha)/0.2998*transverseMomentum.r()/displacement.r();
+}
