c17d9fb747f2384657c04417df31e2d3232aca89
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2009-08-31 13:35:35 +0000

    event content fixed

--- RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
+++ RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
@@ -2,13 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 #Full Event content
 RecoHiJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoJets_iterativeConePu5CaloJets_*_*'
+    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*'
                                            )
     )
 
 RecoHiJetsFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoJets_iterativeConePu5CaloJets_*_*',
-                                           'keep recoJets_iterativeCone5HiGenJets_*_*'
+    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*',
+                                           'keep recoGenJets_iterativeCone5HiGenJets_*_*'
                                            )
     )
 
