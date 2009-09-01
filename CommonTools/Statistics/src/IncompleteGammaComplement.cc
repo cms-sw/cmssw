798d6d9610fee8e664fd0ebe0a2f5502724be693
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2009-09-01 06:14:47 +0000

    change a cerr to logwarning

--- CommonTools/Statistics/src/IncompleteGammaComplement.cc
+++ CommonTools/Statistics/src/IncompleteGammaComplement.cc
@@ -2,13 +2,17 @@
 #include "CommonTools/Statistics/src/GammaContinuedFraction.h"
 #include "CommonTools/Statistics/src/GammaSeries.h"
 #include "CommonTools/Statistics/src/GammaLn.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+
 #include <iostream>
 #include <cmath>
 
 float IncompleteGammaComplement::value(float a, float x)
 {
   if( x < 0.0 || a <= 0.0 ) 
-    std::cerr << "IncompleteGammaComplement::invalid arguments" << std::endl;
+    edm::LogWarning("IncompleteGammaComplement")<< "IncompleteGammaComplement::invalid arguments";
   if( x < (a+1.0) )
     // take the complement of the series representation
     return 1.-GammaSeries(a,x)*(exp(-x + a*log(x) - GammaLn(a)));
@@ -21,7 +25,7 @@ float IncompleteGammaComplement::value(float a, float x)
 float IncompleteGammaComplement::ln(float a, float x)
 {
   if( x < 0.0 || a <= 0.0 ) 
-    std::cerr << "IncompleteGammaComplement::invalid arguments" << std::endl;
+edm::LogWarning("IncompleteGammaComplement")<< "IncompleteGammaComplement::invalid arguments";
   if( x < (a+1.0) )
     // take the complement of the series representation    
     return log(1.-GammaSeries(a,x)*(exp(-x + a*log(x) - GammaLn(a))));
