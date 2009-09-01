e88cdaddaaaffe6d21fcee20e304204298c344da
Louis James Antonelli <sha1-442197ce02a0252aaee7a57f46fd47b9778e7d2f@cern.ch>
2009-09-01 12:00:57 +0000

    changed to run centrally

--- DQMOffline/EGamma/src/PhotonDataCertification.cc
+++ DQMOffline/EGamma/src/PhotonDataCertification.cc
@@ -7,7 +7,7 @@
 //
 // Original Author:  Louis James Antonelli
 //         Created:  Thu Jan 22 13:42:28CET 2009
-// $Id: PhotonDataCertification.cc,v 1.3 2009/02/27 09:07:46 lantonel Exp $
+// $Id: PhotonDataCertification.cc,v 1.4 2009/03/12 16:11:27 lantonel Exp $
 //
 
 
@@ -91,34 +91,61 @@ PhotonDataCertification::endRun(const edm::Run& run, const edm::EventSetup& c)
   std::string reffilename = parameters_.getUntrackedParameter<std::string>("refFileName");
   std::string outputFileName = parameters_.getUntrackedParameter<std::string>("outputFileName");
 
-  dbe_->open(reffilename);
+  //dbe_->open(reffilename);
 
   dbe_->setCurrentFolder("/");
 
-  std::vector<std::string> subDirVec;
-  std::string RunDir;
-  std::string RefRunDir;
+//   std::vector<std::string> subDirVec;
+//   std::string RunDir;
+//   std::string RefRunDir;
   
-  bool standalone = true;
+  //bool standalone = true;
+
+  //bool standalone = false; 
+
+//   std::vector<MonitorElement*> mes;
+
+
+//   mes = dbe_->getAllContents("");
+//   std::cout << "1 >>> found " <<  mes.size() << " monitoring elements!" << std::endl;
   
-  subDirVec = dbe_->getSubdirs();
+//   //dbe_->setCurrentFolder("EGamma");
+//   subDirVec = dbe_->getSubdirs();
+//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin();ic != subDirVec.end(); ic++) {    
+//     std::cout << "-AAA- Dir = >>" << ic->c_str() << "<<" << std::endl;
+//   }
+//   dbe_->setCurrentFolder("EGamma");
+//   std::cout << "asdfasdfa\n";
+//   subDirVec = dbe_->getSubdirs();
+//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin();ic != subDirVec.end(); ic++) {    
+//     std::cout << "-AAA- Dir = >>" << ic->c_str() << "<<" << std::endl;
+//   }
+
+
+ 
+  //  subDirVec = dbe_->getSubdirs();
    
 
-  for (std::vector<std::string>::const_iterator ic = subDirVec.begin(); ic != subDirVec.end(); ic++) {
+//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin(); ic != subDirVec.end(); ic++) {
     
-     //std::cout << "Dir = >>" << ic->c_str() << "<<" << std::endl;
-    if (ic->find( "Run" ) != std::string::npos) {
-      RefRunDir = *ic;
-      //std::cout << "found RefRunDir" << std::endl;
-    }
-    if (ic->find( "Egamma" ) != std::string::npos) {
-      standalone = false;
-      //cout << " found Egamma" << endl;
-    }
-  }
+//     std::cout << "Dir = >>" << ic->c_str() << "<<" << std::endl;
+//     if (ic->find( "Run" ) != std::string::npos) {
+//       RefRunDir = *ic;
+//       std::cout << "found RefRunDir" << std::endl;
+//       //standalone = false;
+//     }
+// //     if (ic->find( "Egamma" ) != std::string::npos) {
+// //       standalone = false;
+// //       cout << " found Egamma" << endl;
+// //     }
+//   }
 
-  if(standalone) dbe_->setCurrentFolder(RefRunDir+"/Egamma/EventInfo/Certification/");  
-  else dbe_->setCurrentFolder("Egamma/EventInfo/Certification/");
+  //if(standalone) dbe_->setCurrentFolder(RefRunDir+"/Egamma/EventInfo/Certification/");  
+  //else dbe_->setCurrentFolder("Egamma/EventInfo/Certification/");
+  //else dbe_->setCurrentFolder(RefRunDir+"/Egamma/EventInfo/Certification/");  
+
+  dbe_->setCurrentFolder("Egamma/EventInfo/Certification/");  
+
 
   MonitorElement* KolmogorovTest = dbe_->bookFloat("KolTest");
   MonitorElement* ChiSquaredTest = dbe_->bookFloat("Chi2Test");
@@ -128,35 +155,40 @@ PhotonDataCertification::endRun(const edm::Run& run, const edm::EventSetup& c)
   //std::cout << "getting Histos" << std::endl;
   
   
-  MonitorElement * RefHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
+  //MonitorElement * RefHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
   //cout << "got first one " << endl;
-  TH1F * RefHistTH1 = RefHist->getTH1F();
+  //TH1F * RefHistTH1 = RefHist->getTH1F();
   
-  MonitorElement * TestHist=0;
-  if(standalone) TestHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
-  else TestHist = new MonitorElement(*(dbe_->get("Egamma/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
+  //MonitorElement * TestHist=0;
+  //if(standalone) TestHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
+  //else TestHist = new MonitorElement(*(dbe_->get("Egamma/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
   //cout << "got second one " << endl;
-  TH1F * TestHistTH1 = TestHist->getTH1F();
+  //TH1F * TestHistTH1 = TestHist->getTH1F();
   
-  double KolTest;
-  double Chi2Test;
+  //double KolTest;
+  //double Chi2Test;
   
-  if(TestHistTH1->GetEntries() != 0){
- 
-    KolTest = TestHistTH1->KolmogorovTest(RefHistTH1);
-    Chi2Test = TestHistTH1->Chi2Test(RefHistTH1);
-    //cout << KolTest << "   " << Chi2Test << endl;
- 
-    KolmogorovTest->Fill(KolTest);
-    ChiSquaredTest->Fill(Chi2Test);
-  
-  }
-  else{
-    KolmogorovTest->Fill(-1);
-    ChiSquaredTest->Fill(-1);
-  }
+  KolmogorovTest->Fill(-1);
+  ChiSquaredTest->Fill(-1);
 
-  if(standalone) dbe_->save(outputFileName);
+
+
+//   if(TestHistTH1->GetEntries() != 0){
+ 
+//     KolTest = TestHistTH1->KolmogorovTest(RefHistTH1);
+//     Chi2Test = TestHistTH1->Chi2Test(RefHistTH1);
+//     //cout << KolTest << "   " << Chi2Test << endl;
+ 
+//     KolmogorovTest->Fill(KolTest);
+//     ChiSquaredTest->Fill(Chi2Test);
+  
+//   }
+//   else{
+//     KolmogorovTest->Fill(-1);
+//     ChiSquaredTest->Fill(-1);
+//   }
+
+//   if(standalone) dbe_->save(outputFileName);
   
   
 }
