4af4455ced3e9b558e3e5008d113cc90e5e388bd
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-09-01 21:42:12 +0000

    Use new parametrizations for 33X

--- RecoMuon/MuonSeedGenerator/python/ancientMuonSeed_cfi.py
+++ RecoMuon/MuonSeedGenerator/python/ancientMuonSeed_cfi.py
@@ -1,11 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# Update this part asap!
-# Old field map
-#include "RecoMuon/MuonSeedGenerator/data/ptSeedParameterization_40T_851.cfi"
-# New map at 3.8 T
-#from RecoMuon.MuonSeedGenerator.ptSeedParameterization_cfi import *
-from RecoMuon.MuonSeedGenerator.ptSeedParameterization_31X_cfi import *
+from RecoMuon.MuonSeedGenerator.ptSeedParameterization_cfi import *
 from RecoMuon.MuonSeedGenerator.MuonSeedPtScale_cfi import *
 
 # module standAloneMuonSeeds = MuonSeedGenerator {
