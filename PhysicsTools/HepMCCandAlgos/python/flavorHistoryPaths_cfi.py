71826c3134a6eb22feab1c6c9106949221f3bad4
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-09-01 18:41:58 +0000

    Reflect changes in reco jets package.

--- PhysicsTools/HepMCCandAlgos/python/flavorHistoryPaths_cfi.py
+++ PhysicsTools/HepMCCandAlgos/python/flavorHistoryPaths_cfi.py
@@ -5,10 +5,7 @@ from PhysicsTools.HepMCCandAlgos.flavorHistoryFilter_cfi import *
 
 
 from RecoJets.Configuration.GenJetParticles_cff import *
-from RecoJets.JetProducers.SISConeJetParameters_cfi import *
-from RecoJets.JetProducers.GenJetParameters_cfi import *
-from RecoJets.JetProducers.FastjetParameters_cfi import *
-from RecoJets.JetProducers.sisCone5GenJets_cff import *
+from RecoJets.JetProducers.sc5GenJets_cfi import *
 
 # Set up correct sequence for flavorHistoryFilter
 flavorHistorySeq = cms.Sequence(genJetParticles*sisCone5GenJets*
