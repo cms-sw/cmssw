631b090cd52bde790c45c654f10cb231792e05fc
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-09-02 02:06:04 +0000

    Changing to reflect new reco jets package structure

--- PhysicsTools/PFCandProducer/python/pfJets_cff.py
+++ PhysicsTools/PFCandProducer/python/pfJets_cff.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-import RecoJets.Configuration.ic5PFJets_cfi
+import RecoJets.JetProducers.ic5PFJets_cfi
 from PhysicsTools.PFCandProducer.ParticleSelectors.ptMinPFJetSelector_cfi import ptMinPFJets as pfJets
 
 
-allPfJets = RecoJets.Configuration.iterativeCone5PFJets_cff.iterativeCone5PFJets.clone()
+allPfJets = RecoJets.JetProducers.ic5PFJets_cfi.iterativeCone5PFJets.clone()
 allPfJets.src = 'pfNoElectron'
 
 pfJets.src = 'allPfJets'
