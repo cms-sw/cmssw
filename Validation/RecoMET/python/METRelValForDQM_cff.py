01849bc1a05a4f61489b66f406903ebc9995c1b7
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-09-02 09:20:31 +0000

    Changed DQM Folder from "JetMET/MET" to "JetMET/METv" per K. Hatekeyama's request.

--- Validation/RecoMET/python/METRelValForDQM_cff.py
+++ Validation/RecoMET/python/METRelValForDQM_cff.py
@@ -6,50 +6,50 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.RecoMET.CaloMET_cfi import *
 metAnalyzer.FineBinning = cms.untracked.bool(False)
-metAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metHOAnalyzer.FineBinning = cms.untracked.bool(False)
-metHOAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metHOAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metNoHFAnalyzer.FineBinning = cms.untracked.bool(False)
-metNoHFAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metNoHFAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metNoHFHOAnalyzer.FineBinning = cms.untracked.bool(False)
-metNoHFHOAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metNoHFHOAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metOptAnalyzer.FineBinning = cms.untracked.bool(False)
-metOptAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metOptAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metOptHOAnalyzer.FineBinning = cms.untracked.bool(False)
-metOptHOAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metOptHOAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metOptNoHFAnalyzer.FineBinning = cms.untracked.bool(False)
-metOptNoHFAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metOptNoHFAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 metOptNoHFHOAnalyzer.FineBinning = cms.untracked.bool(False)
-metOptNoHFHOAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+metOptNoHFHOAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 from Validation.RecoMET.PFMET_cfi import *
 pfMetAnalyzer.FineBinning = cms.untracked.bool(False)
-pfMetAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+pfMetAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 from Validation.RecoMET.TCMET_cfi import *
 tcMetAnalyzer.FineBinning = cms.untracked.bool(False)
-tcMetAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+tcMetAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 from Validation.RecoMET.MuonCorrectedCaloMET_cff import *
 corMetGlobalMuonsAnalyzer.FineBinning = cms.untracked.bool(False)
-corMetGlobalMuonsAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+corMetGlobalMuonsAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 from Validation.RecoMET.GenMET_cfi import *
 genMetTrueAnalyzer.FineBinning = cms.untracked.bool(False)
-genMetTrueAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+genMetTrueAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 genMetCaloAnalyzer.FineBinning = cms.untracked.bool(False)
-genMetCaloAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+genMetCaloAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 genMetCaloAndNonPromptAnalyzer.FineBinning = cms.untracked.bool(False)
-genMetCaloAndNonPromptAnalyzer.FolderName = cms.untracked.string("JetMET/MET/")
+genMetCaloAndNonPromptAnalyzer.FolderName = cms.untracked.string("JetMET/METv/")
 
 METRelValSequence = cms.Sequence(
     metAnalyzer*
