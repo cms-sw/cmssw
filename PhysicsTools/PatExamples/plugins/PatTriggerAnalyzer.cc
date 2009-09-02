a88ffa277b1fec477b063f88918f3f1b3cfcdc8c
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2009-09-02 20:04:13 +0000

    Files needed for PAT eLearning, module 4, task 4.

---
+++ PhysicsTools/PatExamples/plugins/PatTriggerAnalyzer.cc
@@ -0,0 +1,74 @@
+
+#include "PhysicsTools/PatExamples/plugins/PatTriggerAnalyzer.h"
+
+#include "PhysicsTools/PatUtils/interface/TriggerHelper.h"
+
+#include "TMath.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+using namespace pat;
+using namespace pat::helper;
+using namespace TMath;
+
+PatTriggerAnalyzer::PatTriggerAnalyzer( const edm::ParameterSet & iConfig ) :
+  trigger_( iConfig.getParameter< edm::InputTag >( "trigger" ) ),
+  triggerEvent_( iConfig.getParameter< edm::InputTag >( "triggerEvent" ) ),
+  muons_( iConfig.getParameter< edm::InputTag >( "muons" ) ),
+  muonMatch_( iConfig.getParameter< std::string >( "muonMatch" ) ),
+  histos1D_(),
+  histos2D_()
+{
+}
+
+PatTriggerAnalyzer::~PatTriggerAnalyzer()
+{
+}
+
+void PatTriggerAnalyzer::beginJob( const edm::EventSetup & iSetup )
+{
+  edm::Service< TFileService > fileService;
+  // histogram definitions
+//   YOUR HISTOGRAM DEFINITIONS GO HERE!
+//   histos1D_[ "histoName" ] = fileService->make< TH1D >( "[normal TH1D constructor]" ); // EXAMPLE CODE
+//   histos1D_[ "histoName" ]->SetXTitle( "x-axis label" );                               // EXAMPLE CODE
+//   histos1D_[ "histoName" ]->SetYTitle( "y-axis label" );                               // EXAMPLE CODE
+}
+
+void PatTriggerAnalyzer::analyze( const edm::Event & iEvent, const edm::EventSetup & iSetup )
+{
+  // PAT trigger information
+  edm::Handle< TriggerEvent > triggerEvent;
+  iEvent.getByLabel( triggerEvent_, triggerEvent );
+  edm::Handle< TriggerPathCollection > triggerPaths;
+  iEvent.getByLabel( trigger_, triggerPaths );
+  edm::Handle< TriggerFilterCollection > triggerFilters;
+  iEvent.getByLabel( trigger_, triggerFilters );
+  edm::Handle< TriggerObjectCollection > triggerObjects;
+  iEvent.getByLabel( trigger_, triggerObjects );
+
+  // PAT object collection
+  edm::Handle< MuonCollection > muons;
+  iEvent.getByLabel( muons_, muons );
+
+  // PAT trigger helper for trigger matching information
+  const TriggerMatchHelper matchHelper;
+
+//   YOUR ANALYSIS CODE GOES HERE!
+
+  // kinematics comparison
+  for ( size_t iMuon = 0; iMuon < muons->size(); ++iMuon ) { // loop over muon references (PAT muons have been used in the matcher in task 3)
+    const reco::CandidateBaseRef candBaseRef( MuonRef( muons, iMuon ) );
+  } // iMuon
+
+  // mean pt
+
+}
+
+void PatTriggerAnalyzer::endJob()
+{
+}
+
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE( PatTriggerAnalyzer );
