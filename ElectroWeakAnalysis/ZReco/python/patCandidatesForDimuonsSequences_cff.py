c856d18ba2f216efd2c1f5f3b470611ea79fd611
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2009-09-02 15:22:24 +0000

    cleanup for tutorial

--- ElectroWeakAnalysis/ZReco/python/patCandidatesForDimuonsSequences_cff.py
+++ ElectroWeakAnalysis/ZReco/python/patCandidatesForDimuonsSequences_cff.py
@@ -5,12 +5,10 @@ from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 # PAT TRACKS (sequence compatible with PAT v2)
 
 # before layer 1: conversion to track candidates for pat; isolation 
-#from PhysicsTools.PatAlgos.recoLayer0.genericTrackCandidates_cff import *
 from ElectroWeakAnalysis.ZReco.patAODTrackCandSequence_cff import *
 patAODTrackCands.cut = 'pt > 10.'
 
 # before layer 1: MC match
-#from PhysicsTools.PatAlgos.mcMatchLayer0.trackMuMatch_cfi import *
 from ElectroWeakAnalysis.ZReco.trackMuMatch_cfi import *
 trackMuMatch.maxDeltaR = 0.15
 trackMuMatch.maxDPtRel = 1.0
@@ -69,7 +67,6 @@ allLayer1Muons.isolation.tracker = cms.PSet(
     cut = cms.double(3.0),
     threshold = cms.double(1.5)
 )
-#allLayer1Muons.addTrigMatch = cms.bool(False)
 
 from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import *
 selectedLayer1Muons.cut = 'pt > 0. & abs(eta) < 100.0'
@@ -82,21 +79,6 @@ from PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cfi import *
 # to access 1E31 menus
 patTrigger.triggerResults = cms.InputTag( "TriggerResults::HLT" )
 patTrigger.triggerEvent = cms.InputTag( "hltTriggerSummaryAOD::HLT" )
-
-#muonTriggerMatchHLTMu15 = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
-#    src     = cms.InputTag( "selectedLayer1Muons" ),
-#    matched = cms.InputTag( "patTrigger" ),
-#    andOr          = cms.bool( False ),
-#    filterIdsEnum  = cms.vstring( '*' ),
-#    filterIds      = cms.vint32( 0 ),
-#    filterLabels   = cms.vstring( '*' ),
-#    pathNames      = cms.vstring( 'HLT_Mu15' ),
-#    collectionTags = cms.vstring( '*' ),
-#    maxDPtRel = cms.double( 1.0 ),
-#    maxDeltaR = cms.double( 0.5 ),
-#    resolveAmbiguities    = cms.bool( True ),
-#    resolveByMatchQuality = cms.bool( False )
-#)
 
 muonTriggerMatchHLTMuons = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
     src     = cms.InputTag( "selectedLayer1Muons" ),
@@ -114,14 +96,11 @@ muonTriggerMatchHLTMuons = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
 )
 
 from PhysicsTools.PatAlgos.triggerLayer1.triggerEventProducer_cfi import *
-#patTriggerEvent.patTriggerMatches  = [ "muonTriggerMatchHLTMu15" ]
 patTriggerEvent.patTriggerMatches  = cms.VInputTag( "muonTriggerMatchHLTMuons" )
-#patTriggerEvent.patTriggerMatches  = cms.VInputTag( "muonTriggerMatchHLTMu15" )
 
 patTriggerSequence = cms.Sequence(
     patTrigger *
     muonTriggerMatchHLTMuons *
-#    muonTriggerMatchHLTMu15 *
     patTriggerEvent
 )
 
@@ -142,8 +121,7 @@ beforeLayer1Tracks = cms.Sequence(
 )
 
 beforeLayer1Muons = cms.Sequence(
-    muonMatch # +
-#    patTrigMatch
+    muonMatch
 )
 
 beforePatLayer1 = cms.Sequence(
@@ -154,7 +132,6 @@ beforePatLayer1 = cms.Sequence(
 patLayer1 = cms.Sequence(
     allLayer1Muons *
     selectedLayer1Muons *
-#    cleanLayer1Muons *
     allLayer1TrackCands *
     selectedLayer1TrackCands
 )
@@ -162,7 +139,6 @@ patLayer1 = cms.Sequence(
 goodMuonRecoForDimuon = cms.Sequence(
     beforePatLayer1 *
     patLayer1 *
-    patTriggerSequence # *
-#    muonTriggerMatchEmbedder
+    patTriggerSequence
 )
 
