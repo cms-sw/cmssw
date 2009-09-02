3e606712a88078c79a88867c7b364230640ffc3d
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-09-02 16:59:01 +0000

    Add "tracksForJets" into sequence, was missing by mistake.

--- RecoJets/Configuration/python/RecoTrackJets_cff.py
+++ RecoJets/Configuration/python/RecoTrackJets_cff.py
@@ -6,6 +6,7 @@ from RecoJets.JetProducers.ak5TrackJets_cfi import ak5TrackJets
 from RecoJets.JetProducers.gk5TrackJets_cfi import gk5TrackJets
 from RecoJets.JetProducers.kt4TrackJets_cfi import kt4TrackJets
 from RecoJets.JetProducers.ca4TrackJets_cfi import ca4TrackJets
+from RecoJets.JetProducers.TracksForJets_cfi import *
 
 
 sisCone7TrackJets = sisCone5TrackJets.clone( rParam = 0.7 )
@@ -15,14 +16,16 @@ kt6TrackJets = kt4TrackJets.clone( rParam = 0.6 )
 ca6TrackJets = ca4TrackJets.clone( rParam = 0.6 )
 
 
-recoTrackJets   =cms.Sequence(sisCone5TrackJets+sisCone7TrackJets+
+recoTrackJets   =cms.Sequence(tracksForJets+
+                              sisCone5TrackJets+sisCone7TrackJets+
                               kt4TrackJets+kt6TrackJets+
                               iterativeCone5TrackJets+
                               ak5TrackJets+ak7TrackJets)
 
-recoAllTrackJets=cms.Sequence(sisCone5TrackJets+sisCone7TrackJets+
-                             kt4TrackJets+kt6TrackJets+
-                             iterativeCone5TrackJets+
-                             ak5TrackJets+ak7TrackJets+
-                             gk5TrackJets+gk7TrackJets+
-                             ca4TrackJets+ca6TrackJets)
+recoAllTrackJets=cms.Sequence(tracksForJets+
+                              sisCone5TrackJets+sisCone7TrackJets+
+                              kt4TrackJets+kt6TrackJets+
+                              iterativeCone5TrackJets+
+                              ak5TrackJets+ak7TrackJets+
+                              gk5TrackJets+gk7TrackJets+
+                              ca4TrackJets+ca6TrackJets)
