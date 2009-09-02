8b460dfcbf773e0a1ecbdb2cc87e080fbc0f2852
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2009-09-02 15:47:37 +0000

    cleanup for tutorial

--- ElectroWeakAnalysis/ZReco/test/dimuonsSkim.py
+++ ElectroWeakAnalysis/ZReco/test/dimuonsSkim.py
@@ -8,8 +8,7 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 # source
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-    'file:/scratch1/cms/data/summer09/aodsim/ppMuX/0010/9C519151-5883-DE11-8BC8-001AA0095119.root'
-#    'file:/data1/home/fabozzi/cmsrel/skim3_1/CMSSW_3_1_2/src/ElectroWeakAnalysis/Skimming/test/testEWKMuSkim_HLTFilterAndGlobalMuonAndPt10AndEta25.root'
+    "file:mycollection.root"
     )
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
@@ -23,7 +22,7 @@ process.load("ElectroWeakAnalysis.ZReco.dimuons_SkimPaths_cff")
 
 # Output module configuration
 process.load("ElectroWeakAnalysis.ZReco.dimuonsOutputModule_cfi")
-process.dimuonsOutputModule.fileName = 'file:testSkim_fromOriginal.root'
+process.dimuonsOutputModule.fileName = 'file:/tmp/fabozzi/testSkim.root'
 
 process.outpath = cms.EndPath(process.dimuonsOutputModule)
 
