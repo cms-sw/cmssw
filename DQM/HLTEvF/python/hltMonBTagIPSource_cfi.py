13d4ceca2c4c440401ef6e2cba3f64a580a23818
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-09-03 17:33:44 +0000

    add process name, trigger results and filters

--- DQM/HLTEvF/python/hltMonBTagIPSource_cfi.py
+++ DQM/HLTEvF/python/hltMonBTagIPSource_cfi.py
@@ -1,13 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 hltMonBTagIPSource = cms.EDFilter('HLTMonBTagIPSource',
-    monitorName = cms.string('HLT/HLTMonBJet'),
-    pathName    = cms.string('HLT_BTagIP_Jet80'),
-    L2Jets      = cms.InputTag('hltMCJetCorJetIcone5Regional'),
-    L25TagInfo  = cms.InputTag('hltBLifetimeL25TagInfosStartup'),
-    L25JetTags  = cms.InputTag('hltBLifetimeL25BJetTagsStartup'),
-    L3TagInfo   = cms.InputTag('hltBLifetimeL3TagInfosStartup'),
-    L3JetTags   = cms.InputTag('hltBLifetimeL3BJetTagsStartup'),
-    outputFile  = cms.untracked.string('HLTMonBTag.root'),
-    storeROOT   = cms.untracked.bool(False)
+    triggerResults  = cms.InputTag('TriggerResults', '', 'HLT'),
+    monitorName     = cms.string('HLT/HLTMonBJet'),
+    processName     = cms.string('HLT'),
+    pathName        = cms.string('HLT_BTagIP_Jet50U'),
+    interestingJets = cms.uint32( 4 ),
+    L1Filter        = cms.InputTag('hltL1sBTagIPJet50U'),
+    L2Filter        = cms.InputTag('hltBJet50U'),
+    L2Jets          = cms.InputTag('hltMCJetCorJetIcone5HF07'),
+    L25TagInfo      = cms.InputTag('hltBLifetimeL25TagInfosStartupU'),
+    L25JetTags      = cms.InputTag('hltBLifetimeL25BJetTagsStartupU'),
+    L25Filter       = cms.InputTag('hltBLifetimeL25FilterStartupU'),
+    L3TagInfo       = cms.InputTag('hltBLifetimeL3TagInfosStartupU'),
+    L3JetTags       = cms.InputTag('hltBLifetimeL3BJetTagsStartupU'),
+    L3Filter        = cms.InputTag('hltBLifetimeL3FilterStartupU'),
+    storeROOT       = cms.untracked.bool(False),
+    outputFile      = cms.untracked.string('HLTMonBTag.root')
 )
--- DQM/HLTEvF/python/hltMonBTagMuSource_cfi.py
+++ DQM/HLTEvF/python/hltMonBTagMuSource_cfi.py
@@ -1,13 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 hltMonBTagMuSource = cms.EDFilter('HLTMonBTagMuSource',
-    monitorName = cms.string('HLT/HLTMonBJet'),
-    pathName    = cms.string('HLT_BTagMu_Jet20'),
-    L2Jets      = cms.InputTag('hltMCJetCorJetIcone5'),
-    L25TagInfo  = cms.InputTag('hltBSoftMuonL25TagInfos'),
-    L25JetTags  = cms.InputTag('hltBSoftMuonL25BJetTagsByDR'),
-    L3TagInfo   = cms.InputTag('hltBSoftMuonL3TagInfos'),
-    L3JetTags   = cms.InputTag('hltBSoftMuonL3BJetTagsByDR'),
-    outputFile  = cms.untracked.string('HLTMonBTag.root'),
-    storeROOT   = cms.untracked.bool(False)
+    triggerResults  = cms.InputTag('TriggerResults', '', 'HLT'),
+    monitorName     = cms.string('HLT/HLTMonBJet'),
+    processName     = cms.string('HLT'),
+    pathName        = cms.string('HLT_BTagMu_Jet10U'),
+    interestingJets = cms.uint32( 4 ),
+    L1Filter        = cms.InputTag('hltL1sBTagMuJet10U'),
+    L2Filter        = cms.InputTag('hltBJet10U'),
+    L2Jets          = cms.InputTag('hltMCJetCorJetIcone5HF07'),
+    L25TagInfo      = cms.InputTag('hltBSoftMuonL25TagInfosU'),
+    L25JetTags      = cms.InputTag('hltBSoftMuonL25BJetTagsUByDR'),
+    L25Filter       = cms.InputTag('hltBSoftMuonL25FilterUByDR'),
+    L3TagInfo       = cms.InputTag('hltBSoftMuonL3TagInfosU'),
+    L3JetTags       = cms.InputTag('hltBSoftMuonL3BJetTagsUByDR'),
+    L3Filter        = cms.InputTag('hltBSoftMuonL3FilterUByDR'),
+    storeROOT       = cms.untracked.bool(False),
+    outputFile      = cms.untracked.string('HLTMonBTag.root')
 )
