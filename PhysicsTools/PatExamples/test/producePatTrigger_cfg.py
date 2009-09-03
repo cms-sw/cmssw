7d4f9a5f3c5d5bc9cfea1e2e27d244fd13574669
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2009-09-03 12:47:30 +0000

    Solution for PAT eLearning, module 4, task 3

--- PhysicsTools/PatExamples/test/producePatTrigger_cfg.py
+++ PhysicsTools/PatExamples/test/producePatTrigger_cfg.py
@@ -4,6 +4,10 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 # Load the standard PAT config
 process.load( "PhysicsTools.PatAlgos.patSequences_cff" )
 
+# Switch to selected PAT objects
+from PhysicsTools.PatAlgos.tools.coreTools import removeCleaning
+removeCleaning( process )
+
 # Define the path
 process.p = cms.Path(
     process.patDefaultSequence
@@ -12,3 +16,27 @@ process.p = cms.Path(
 process.maxEvents.input     = 1000 # Reduce number of events for testing.
 process.out.fileName        = 'edmPatTrigger.root'
 process.options.wantSummary = False # to suppress the long output at the end of the job
+
+# PAT trigger
+process.muonTriggerMatchHLTMuons = cms.EDFilter( "PATTriggerMatcherDRDPtLessByR",
+    src     = cms.InputTag( "selectedLayer1Muons" ),
+    matched = cms.InputTag( "patTrigger" ),
+    andOr          = cms.bool( False ),
+    filterIdsEnum  = cms.vstring( 'TriggerMuon' ),
+    filterIds      = cms.vint32( 0 ),
+    filterLabels   = cms.vstring( '*' ),
+    pathNames      = cms.vstring( '*' ),
+    collectionTags = cms.vstring( '*' ),
+    maxDPtRel = cms.double( 0.5 ),
+    maxDeltaR = cms.double( 0.5 ),
+    resolveAmbiguities    = cms.bool( True ),
+    resolveByMatchQuality = cms.bool( True )
+)
+process.load( "PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff" )
+process.patTriggerMatcher += process.muonTriggerMatchHLTMuons
+process.patTriggerMatcher.remove( process.patTriggerElectronMatcher )
+process.patTriggerMatcher.remove( process.patTriggerMuonMatcher )
+process.patTriggerMatcher.remove( process.patTriggerTauMatcher )
+process.patTriggerEvent.patTriggerMatches = [ "muonTriggerMatchHLTMuons" ]
+from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
+switchOnTrigger( process )
