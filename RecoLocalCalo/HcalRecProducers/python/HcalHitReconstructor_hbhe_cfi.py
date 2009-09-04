b4d75e726479d3d56718d0ee5156b52d6e7533ae
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-09-04 09:11:51 +0000

    Correctly added Ted's new parameters

--- RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hbhe_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hbhe_cfi.py
@@ -22,10 +22,11 @@ hbhereco = cms.EDFilter(
     flagParameters= cms.PSet(nominalPedestal=cms.double(3.0),  #fC
                              hitEnergyMinimum=cms.double(1.0), #GeV
                              hitMultiplicityThreshold=cms.int32(17),
-                             pulseShapeParameterSets = cms.VPSet(    
-    cms.PSet(pulseShapeParameters=cms.vdouble(-100.0, 20.0,-50.0,0.0,-15.0,0.0)),
-    cms.PSet(pulseShapeParameters=cms.vdouble( 100.0,2.0e3,-50.0,0.0,-15.0,0.05)),
-    cms.PSet(pulseShapeParameters=cms.vdouble( 2.0e3,1.0e6,-50.0,0.0, 85.0,0.0))
+                             pulseShapeParameterSets = cms.VPSet(
+    cms.PSet(pulseShapeParameters=cms.vdouble(   0.0, 100.0, -50.0, 0.0, -15.0, 0.15)),
+    cms.PSet(pulseShapeParameters=cms.vdouble( 100.0, 2.0e3, -50.0, 0.0,  -5.0, 0.05)),
+    cms.PSet(pulseShapeParameters=cms.vdouble( 2.0e3, 1.0e6, -50.0, 0.0,  95.0, 0.0 )),
+    cms.PSet(pulseShapeParameters=cms.vdouble(-1.0e6, 1.0e6,  45.0, 0.1, 1.0e6, 0.0 )),
     )
                              ),
     saturationParameters=  cms.PSet(maxADCvalue=cms.int32(127)),
