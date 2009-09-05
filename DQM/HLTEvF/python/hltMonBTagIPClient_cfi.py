fdaa9c7be8502b4108270ce002379fe7934fe7e9
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-09-05 11:04:18 +0000

    configuration for HLTMonBTagIPClient

---
+++ DQM/HLTEvF/python/hltMonBTagIPClient_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+hltMonBTagIPClient = cms.EDFilter('HLTMonBTagIPClient',
+    monitorName     = cms.string('HLT/HLTMonBJet'),
+    pathName        = cms.string('HLT_BTagIP_Jet50U'),
+    storeROOT       = cms.untracked.bool(False),
+    outputFile      = cms.untracked.string('HLTMonBTag.root')
+)
