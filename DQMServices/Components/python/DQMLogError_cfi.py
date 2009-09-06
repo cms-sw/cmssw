2e52251daa0d91e5de9238f3e196e01c0b60d3a5
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-09-06 10:45:33 +0000

    cleanup cfi for LogError modules defining histogram folder to be LogMsg

--- DQMServices/Components/python/DQMLogError_cfi.py
+++ DQMServices/Components/python/DQMLogError_cfi.py
@@ -1,12 +1,13 @@
 
 import FWCore.ParameterSet.Config as cms
 
-
-logErrorDQM = cms.EDAnalyzer("DQMLogError",
-                             Categories = cms.vstring(
-    'PFTrackTransformer', 'RPCHitAssociator', 'GaussianSumUtilities'
-    ),
-                             Directory = cms.string("Test")
-                             )
+logErrorDQM = cms.EDAnalyzer( "DQMLogError",
+              Categories = cms.vstring (
+                      'PFTrackTransformer', 
+		      'RPCHitAssociator', 
+		      'GaussianSumUtilities'
+                  ),
+              Directory = cms.string("LogMsg")
+              )
 
 
