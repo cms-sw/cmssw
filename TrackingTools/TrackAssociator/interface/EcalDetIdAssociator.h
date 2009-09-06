ccdb0b8845a80d65fe70e1c8b478127394dded37
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2009-09-06 16:36:52 +0000

    replaced numerical subdetector code with a proper enum

--- TrackingTools/TrackAssociator/interface/EcalDetIdAssociator.h
+++ TrackingTools/TrackAssociator/interface/EcalDetIdAssociator.h
@@ -15,12 +15,12 @@
 //
 // Original Author:  Dmytro Kovalskyi
 //         Created:  Fri Apr 21 10:59:41 PDT 2006
-// $Id: EcalDetIdAssociator.h,v 1.4.8.1 2007/10/06 05:50:12 jribnik Exp $
+// $Id: EcalDetIdAssociator.h,v 1.5.4.1 2009/07/01 09:58:47 dmytro Exp $
 //
 //
 
 #include "TrackingTools/TrackAssociator/interface/CaloDetIdAssociator.h"
-
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
 class EcalDetIdAssociator: public CaloDetIdAssociator{
  public:
    EcalDetIdAssociator():CaloDetIdAssociator(360,300,0.02){};
@@ -31,12 +31,12 @@ class EcalDetIdAssociator: public CaloDetIdAssociator{
 
    virtual std::set<DetId> getASetOfValidDetIds() const {
       std::set<DetId> setOfValidIds;
-      std::vector<DetId> vectOfValidIds = geometry_->getValidDetIds(DetId::Ecal, 1);//EB
+      std::vector<DetId> vectOfValidIds = geometry_->getValidDetIds(DetId::Ecal, EcalBarrel);//EB
       for(std::vector<DetId>::const_iterator it = vectOfValidIds.begin(); it != vectOfValidIds.end(); ++it)
          setOfValidIds.insert(*it);
 
       vectOfValidIds.clear();
-      vectOfValidIds = geometry_->getValidDetIds(DetId::Ecal, 2);//EE
+      vectOfValidIds = geometry_->getValidDetIds(DetId::Ecal, EcalEndcap);//EE
       for(std::vector<DetId>::const_iterator it = vectOfValidIds.begin(); it != vectOfValidIds.end(); ++it)
          setOfValidIds.insert(*it);
 
