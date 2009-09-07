12197f7d8a9b8346488b8893d290daeb4b2090f3
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2009-09-07 15:15:48 +0000

    support for multiple algos

--- RecoLocalCalo/Castor/python/CastorClusterReco_cfi.py
+++ RecoLocalCalo/Castor/python/CastorClusterReco_cfi.py
@@ -1,10 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
-CastorClusterReco = cms.EDProducer('CastorClusterProducer',
-	inputprocess = cms.untracked.string("CastorTowerReco"),
+CastorClusterRecoCustomKt = cms.EDProducer('CastorClusterProducer',
+	inputtowers = cms.untracked.string("CastorTowerReco"),
+	basicjets = cms.untracked.string(""),
 	KtAlgo = cms.untracked.bool(True),
 	ClusterAlgo = cms.untracked.bool(False),
 	KtRecombination = cms.untracked.uint32(2),
 	KtrParameter = cms.untracked.double(1.) )
+
+CastorClusterRecoKt = cms.EDProducer('CastorClusterProducer',
+	inputtowers = cms.untracked.string(""),
+	basicjets = cms.untracked.string("CastorFastjetRecoKt"))
+
+CastorClusterRecoSISCone = cms.EDProducer('CastorClusterProducer',
+	inputtowers = cms.untracked.string(""),
+	basicjets = cms.untracked.string("CastorFastjetRecoSISCone")) 
 	
 
