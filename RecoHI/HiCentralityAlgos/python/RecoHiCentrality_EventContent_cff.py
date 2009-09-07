c93c56017bcba1aa94ae1dd1a87e21ebaa4e97a2
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-09-07 11:37:08 +0000

    fix typo

--- RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
+++ RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 RecoHiCentralityFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentralityCollection_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentralitys_hiCentrality_*_*')
     )
 
 RecoHiCentralityRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentralityCollection_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentralitys_hiCentrality_*_*')
     )
 
 RecoHiCentralityAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentralityCollection_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentralitys_hiCentrality_*_*')
     )
