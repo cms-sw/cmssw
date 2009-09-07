6e8d1667f41a50557945cc552bc8aee6d6b5d489
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2009-09-07 15:17:12 +0000

    support multiple algos

--- RecoLocalCalo/Castor/python/CastorJetEgammaReco_cfi.py
+++ RecoLocalCalo/Castor/python/CastorJetEgammaReco_cfi.py
@@ -1,9 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-CastorJetEgammaReco = cms.EDProducer('CastorJetEgammaProducer',
-	inputprocess = cms.untracked.string("CastorClusterReco"),
-	fastsim = cms.untracked.bool(False),
-	KtAlgo = cms.untracked.bool(True),
-	ClusterAlgo = cms.untracked.bool(False) )
+CastorJetEgammaRecoCustomKt = cms.EDProducer('CastorJetEgammaProducer',
+	input = cms.untracked.string("CastorClusterRecoCustomKt"),
+	fastsim = cms.untracked.bool(False) )
+	
+CastorJetEgammaRecoKt = cms.EDProducer('CastorJetEgammaProducer',
+	input = cms.untracked.string("CastorClusterRecoKt"),
+	fastsim = cms.untracked.bool(False) )
+	
+CastorJetEgammaRecoSISCone = cms.EDProducer('CastorJetEgammaProducer',
+	input = cms.untracked.string("CastorClusterRecoSISCone"),
+	fastsim = cms.untracked.bool(False) )
 	
 
