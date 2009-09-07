4b6a5e0a5cee10904fb457d68e3be2e847de0a5a
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-09-07 12:42:54 +0000

    genjets needed in RECOSIM etc

--- RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
+++ RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 #Full Event content
 RecoHiJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*'
+    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*',
+                                           'keep recoGenJets_iterativeCone5HiGenJets_*_*'
                                            )
     )
 
