7186bfa7066c0099536dd53baedba99da41a3e67
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-09-07 11:41:50 +0000

    separating simhits from hepmcproduct

--- SimGeneral/MixingModule/python/HiMixing_EventContent_cff.py
+++ SimGeneral/MixingModule/python/HiMixing_EventContent_cff.py
@@ -1,7 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-HiMixSIM = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_hiSignal_*_*',
-                                           'keep *_hiSignalG4SimHits_*_*'
-                                           )
+HiMixRAW = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+        'keep *_hiSignal_*_*',
+        'keep *_hiSignalG4SimHits_*_*'
     )
+)
+
+HiMixRECO = cms.PSet(
+    outputCommands = cms.untracked.vstring(
+        'keep *_hiSignal_*_*'
+    )
+)
+
+HiMixAOD = cms.PSet(
+    outputCommands = cms.untracked.vstring()
+)
+
+
+
