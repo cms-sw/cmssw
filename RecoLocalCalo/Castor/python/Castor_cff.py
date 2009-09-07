964c3cac88fb7063f15de2c79d6a11a6d162cf92
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2009-09-07 15:15:09 +0000

    redesign of reco chain

--- RecoLocalCalo/Castor/python/Castor_cff.py
+++ RecoLocalCalo/Castor/python/Castor_cff.py
@@ -2,8 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.Castor.CastorCellReco_cfi import *
 from RecoLocalCalo.Castor.CastorTowerReco_cfi import *
+from RecoLocalCalo.Castor.CastorTowerCandidateReco_cfi import *
 from RecoLocalCalo.Castor.CastorClusterReco_cfi import *
 from RecoLocalCalo.Castor.CastorJetEgammaReco_cfi import *
+from RecoLocalCalo.Castor.CastorFastjetReco_cfi import * 
 
-CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*CastorClusterReco*CastorJetEgammaReco)
+#CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone+CastorFastjetRecoKtGen+CastorFastjetRecoSISConeGen)
+CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone)
+CastorClusterReco = cms.Sequence(CastorClusterRecoCustomKt+CastorClusterRecoKt+CastorClusterRecoSISCone)
+CastorJetEgammaReco = cms.Sequence(CastorJetEgammaRecoCustomKt+CastorJetEgammaRecoKt+CastorJetEgammaRecoSISCone)
+
+CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*CastorTowerCandidateReco*CastorFastjetReco*CastorClusterReco*CastorJetEgammaReco)
 
