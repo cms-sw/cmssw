a735fbbf941f377d92cf2dd78e465f900859b16e
parenti <sha1-be2f6dcd229cd422d656cf38b6e4a1f56f65f345@cern.ch>
2009-09-07 14:22:59 +0000

    Checks in alignment.log that the job has come to a normal end.

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_check.pl
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_check.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 #     R. Mankel, DESY Hamburg     09-Jul-2007
 #     A. Parenti, DESY Hamburg    24-Apr-2008
-#     $Revision: 1.17 $ by $Author: flucke $
-#     $Date: 2009/06/24 10:12:17 $
+#     $Revision: 1.18 $ by $Author: parenti $
+#     $Date: 2009/07/31 09:53:21 $
 #
 #  Check output from jobs that have FETCH status
 #  
@@ -22,6 +22,7 @@ for ($i=0; $i<@JOBID; ++$i) {
   $batchSuccess = 0;
   $batchExited = 0;
   $finished = 0;
+  $endofjob = 0;
   $eofile = 1;  # do not deal with timel yet
   $timel = 0;
   $killed = 0;
@@ -107,6 +108,8 @@ for ($i=0; $i<@JOBID; ++$i) {
 	if (($line =~ m/failed RFIO error/) eq 1) { $rfioerr = 1;}
 	if (($line =~ m/Request exceeds quota/) eq 1) { $quota = 1;}
 	if (($line =~ m/Exception caught in cmsRun/) eq 1) { $exceptionCaught = 1;}
+# AP 07.09.2009 - Check that the job got to a normal end
+	if (($line =~ m/AlignmentProducer::endOfJob()/) eq 1) { $endofjob = 1;}
 	if (($line =~ m/FwkReport            -i main_input:sourc/) eq 1) {
 	  @array = split(' ',$line);
 	  $nEvent = $array[5];
@@ -128,6 +131,8 @@ for ($i=0; $i<@JOBID; ++$i) {
       }
     } else {
       print "mps_check.pl cannot find $eazeLog to test\n";
+# AP 07.09.2009 - The following check cannot be done: set to 1 to avoid fake error type
+      $endofjob = 1;
     }
 
     # for mille jobs checks that milleBinary file is not empty
@@ -269,6 +274,12 @@ for ($i=0; $i<@JOBID; ++$i) {
 	$remark = "pede error";
 	$okStatus = "FAIL";
     }
+    if ($endofjob ne 1) {
+	print "@JOBDIR[$i] @JOBID[$i] Job not ended\n";
+	$remark = "job not ended";
+	$okStatus = "FAIL";
+    }
+
 
     # print warning line to stdout
     if ($okStatus ne "OK") {
