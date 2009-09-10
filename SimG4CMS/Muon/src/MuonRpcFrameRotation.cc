c7cc8565253e3d2683666d3272004bf5c12ff001
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2009-09-10 10:54:09 +0000

    Change negative endcap rotation since it not anymore a mirrow of the positive part

--- SimG4CMS/Muon/src/MuonRpcFrameRotation.cc
+++ SimG4CMS/Muon/src/MuonRpcFrameRotation.cc
@@ -20,28 +20,10 @@ MuonRpcFrameRotation::~MuonRpcFrameRotation(){
 Local3DPoint MuonRpcFrameRotation::transformPoint(Local3DPoint & point,G4Step * aStep=0) const {
   if (aStep) {
     //check if endcap
-    G4StepPoint * preStepPoint = aStep->GetPreStepPoint();
-    G4TouchableHistory * theTouchable=(G4TouchableHistory *)
-                                      (preStepPoint->GetTouchable());
-    const G4ThreeVector trans=theTouchable->GetTranslation();
-    
-    //    G4VPhysicalVolume * pv = s->GetPreStepPoint()->GetPhysicalVolume();
-
-    //old way with strings
-    //string name=v->GetName();
-    //string det_name=name(1,1);
-    //bool endcap_muon=(det_name == "E");
-    
-    //new way with base number
     MuonBaseNumber num = g4numbering->PhysicalVolumeToBaseNumber(aStep);
     bool endcap_muon = (num.getSuperNo(theRegion)!=1);
     if (endcap_muon){
-      if (trans.z()<0.) {
-      //      return Local3DPoint(point.x(),point.y(),-point.z());
-	return Local3DPoint(point.x(),-point.z(),point.y());
-      } else {
-	return Local3DPoint(point.x(),point.z(),-point.y());
-      }
+      return Local3DPoint(point.x(),point.z(),-point.y());
     } else {
       return point; 
     }
