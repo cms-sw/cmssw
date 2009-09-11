417923c110c23faa6efd628acdc634511e8c2969
kaussen <sha1-b58974427aa4d349705fe1a36f0e42a0fed605a3@cern.ch>
2009-09-11 16:05:46 +0000

    Removed Producers from DQM sequence. Included new cfi's to configure the Filters.

--- DQMOffline/CalibTracker/python/ALCARECOSiStripCalZeroBiasDQM_cff.py
+++ DQMOffline/CalibTracker/python/ALCARECOSiStripCalZeroBiasDQM_cff.py
@@ -11,51 +11,23 @@ SiStripCalZeroBiasMonitorCluster.TopFolderName = cms.string('AlCaReco/SiStrip')
 # Filters to remove APV induced noisy events #
 #---------------------------------------------
 
-import DPGAnalysis.SiStripTools.eventwithhistoryproducer_cfi
-ConsecutiveHEs = DPGAnalysis.SiStripTools.eventwithhistoryproducer_cfi.consecutiveHEs.clone()
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_AlCaReco_cfi import *
 
-import DPGAnalysis.SiStripTools.configurableapvcyclephaseproducer_GR09_cfi
-apvPhases = DPGAnalysis.SiStripTools.configurableapvcyclephaseproducer_GR09_cfi.APVPhases.clone()
-apvPhases.defaultPhases = cms.vint32(30,30,30,30)
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_AlCaReco_cfi import *
 
-from DPGAnalysis.SiStripTools.apvlatency.fakeapvlatencyessource_cff import *
-fakeapvlatency.APVLatency = cms.untracked.int32(143)
-
-import DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_cfi
-PotentialTIBTECHugeEvents = DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_cfi.potentialTIBTECHugeEvents.clone()
-PotentialTIBTECHugeEvents.partitionName              = cms.untracked.string("TM")
-PotentialTIBTECHugeEvents.absBXInCycleRangeLtcyAware = cms.untracked.vint32(8,11)
-PotentialTIBTECHugeEvents.historyProduct             = cms.untracked.InputTag("ConsecutiveHEs")
-PotentialTIBTECHugeEvents.APVPhaseLabel              = cms.untracked.string("apvPhases")
-
-import DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi
-PotentialTIBTECFrameHeaderEventsFPeak = DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi.potentialTIBTECFrameHeaderEventsFPeak.clone()
-PotentialTIBTECFrameHeaderEventsFPeak.partitionName              = cms.untracked.string("TM")
-PotentialTIBTECFrameHeaderEventsFPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(19,22)
-PotentialTIBTECFrameHeaderEventsFPeak.historyProduct             = cms.untracked.InputTag("ConsecutiveHEs")
-PotentialTIBTECFrameHeaderEventsFPeak.APVPhaseLabel              = cms.untracked.string("apvPhases")
-
-PotentialTIBTECFrameHeaderEventsAdditionalPeak = DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_cfi.potentialTIBTECFrameHeaderEventsFPeak.clone()
-PotentialTIBTECFrameHeaderEventsAdditionalPeak.partitionName              = cms.untracked.string("TI")
-PotentialTIBTECFrameHeaderEventsAdditionalPeak.absBXInCycleRangeLtcyAware = cms.untracked.vint32(24,26)
-PotentialTIBTECFrameHeaderEventsAdditionalPeak.historyProduct             = cms.untracked.InputTag("ConsecutiveHEs")
-PotentialTIBTECFrameHeaderEventsAdditionalPeak.APVPhaseLabel              = cms.untracked.string("apvPhases")
+from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_additionalpeak_AlCaReco_cfi import *
 
 #--------------------------------------------
 # Filter to remove high multiplicity events #
 #--------------------------------------------
 
-import DPGAnalysis.SiStripTools.largesistripclusterevents_cfi
-LargeSiStripClusterEvents = DPGAnalysis.SiStripTools.largesistripclusterevents_cfi.largeSiStripClusterEvents.clone()
-LargeSiStripClusterEvents.collectionName = cms.InputTag("calZeroBiasClusters")
+from DPGAnalysis.SiStripTools.largesistripclusterevents_AlCaReco_cfi import *
 
 #------------
 # Sequences #
 #------------
 
-seqAPVCycleFilter = cms.Sequence(apvPhases+
-                                 ConsecutiveHEs+
-                                 ~PotentialTIBTECHugeEvents*
+seqAPVCycleFilter = cms.Sequence(~PotentialTIBTECHugeEvents*
                                  ~PotentialTIBTECFrameHeaderEventsFPeak*
                                  ~PotentialTIBTECFrameHeaderEventsAdditionalPeak)
 
