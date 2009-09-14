3909bf85e6790fa0a3e1e445ff40e1053077c2b1
Sinjini Sen Gupta <sha1-a8162a1ef929931d2165005f5cb68256a7904f73@cern.ch>
2009-09-14 11:00:21 +0000

    added switch for using ZS threshold from DB

--- SimCalorimetry/HcalZeroSuppressionProducers/python/hcalDigisRealistic_cfi.py
+++ SimCalorimetry/HcalZeroSuppressionProducers/python/hcalDigisRealistic_cfi.py
@@ -8,13 +8,16 @@
 # 0=per channel, 1=triggerOR, 2=depthOR
 import FWCore.ParameterSet.Config as cms
 
+
+
 simHcalDigis = cms.EDFilter("HcalRealisticZS",
     digiLabel = cms.InputTag("simHcalUnsuppressedDigis"),
     mode = cms.int32(0),
     HBlevel = cms.int32(8),
     HElevel = cms.int32(9),
     HOlevel = cms.int32(8),
-    HFlevel = cms.int32(10)
+    HFlevel = cms.int32(10),
+    notUsingDBzsValues = cms.int32(0)
 )
 
 
