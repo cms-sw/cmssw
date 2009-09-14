8c8e7c58e65e1c3e2083f93cccbfa620b8194cde
Sinjini Sen Gupta <sha1-a8162a1ef929931d2165005f5cb68256a7904f73@cern.ch>
2009-09-14 11:03:38 +0000

    Added ability to get the channel-by-channel ZS threshold from DB

--- SimCalorimetry/HcalZeroSuppressionAlgos/interface/HcalZeroSuppressionAlgo.h
+++ SimCalorimetry/HcalZeroSuppressionAlgos/interface/HcalZeroSuppressionAlgo.h
@@ -2,13 +2,18 @@
 #define SimCalorimetry_HcalZeroSuppressionAlgos_HCALZEROSUPPESSIONALGO_H 1
 
 #include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
 
 /** \class HcalZeroSuppessionAlgo
   *  
-  * $Date: 2007/07/12 19:33:49 $
-  * $Revision: 1.1 $
+  * $Date: 2007/09/24 15:28:01 $
+  * $Revision: 1.2 $
   * \author J. Mans - Minnesota
   */
+
+class HcalDbService;
+
 class HcalZeroSuppressionAlgo {
 public:
   enum ZSMode { zs_SingleChannel=0, zs_TriggerTowerOR=1, zs_AllDepthsOR=2 };
@@ -18,8 +23,14 @@ public:
   virtual bool shouldKeep(const HBHEDataFrame& digi) const = 0;
   virtual bool shouldKeep(const HODataFrame& digi) const = 0;
   virtual bool shouldKeep(const HFDataFrame& digi) const = 0;
+  void setDbService(const HcalDbService* db) { m_dbService=db; }
+  void clearDbService() { m_dbService=0; }
+  //  template <class DIGI> bool keepMe(const DIGI& inp, int threshold);
+
 protected:
   HcalZeroSuppressionAlgo(ZSMode mode);
+  const HcalDbService* m_dbService;
+  
 private:
   ZSMode m_mode;
 };
