f2b7deb1a3a8137562a10aa1d85298fbc889008e
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-09-15 12:49:21 +0000

    Et -> Pt

--- HLTrigger/JetMET/python/hltDiJetAveFilter_cfi.py
+++ HLTrigger/JetMET/python/hltDiJetAveFilter_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 hltDiJetAveFilter = cms.EDFilter("HLTDiJetAveFilter",
     inputJetTag = cms.InputTag("iterativeCone5CaloJets"),
-    minEtAve = cms.double(60.0),
+    minPtAve = cms.double(60.0),
     minDphi = cms.double(0.0),
-    minEtJet3 = cms.double(3000.0)
+    minPtJet3 = cms.double(99999.0)
 )
 
 
