89ab8542dfeb0377db79735c8380726827b724b2
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-09-15 20:27:46 +0000

    fix typo (repeated parameter name)

--- CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h
+++ CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h
@@ -21,7 +21,7 @@ class JetCorrectionUncertainty {
   int findPtBin(std::vector<double> v, double fPt) const;
   double uncertaintyBandPtEta (unsigned fBand, double fPt, double fEta, std::string fDirection) const;
   double quadraticInterpolation (double fVar, const double fBinMiddle[3], const double fBinValue[3]) const; 
-  double linearInterpolation (double fVar, const double x1, const double x2, const double y1, const double y1) const;
+  double linearInterpolation (double fVar, const double x1, const double x2, const double y1, const double y2) const;
   SimpleJetCorrectorParameters* mParameters;
 };
 
