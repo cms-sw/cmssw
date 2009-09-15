e0370c934b35e5b08acf5cb2fd84e52635f8c6eb
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-09-15 17:00:14 +0000

    clean if statement

--- DQMOffline/Trigger/src/HLTEventInfoClient.cc
+++ DQMOffline/Trigger/src/HLTEventInfoClient.cc
@@ -72,7 +72,8 @@ void HLTEventInfoClient::beginJob(const EventSetup& context){
   dbe_->setCurrentFolder("HLT/EventInfo");
 
 //  sprintf(histo, "reportSummary");
-  if ( reportSummary_ == dbe_->get("HLT/EventInfo/reportSumamry") ) {
+  reportSummary_ = dbe_->get("HLT/EventInfo/reportSumamry");
+  if ( reportSummary_  ) {
       dbe_->removeElement(reportSummary_->getName()); 
    }
   
@@ -132,7 +133,8 @@ void HLTEventInfoClient::beginJob(const EventSetup& context){
 
   dbe_->setCurrentFolder("HLT/EventInfo");
 
-  if ( reportSummaryMap_ == dbe_->get("HLT/EventInfo/reportSummaryMap") ) {
+  reportSummaryMap_ = dbe_->get("HLT/EventInfo/reportSummaryMap");
+  if ( reportSummaryMap_  ) {
   dbe_->removeElement(reportSummaryMap_->getName());
   }
 
