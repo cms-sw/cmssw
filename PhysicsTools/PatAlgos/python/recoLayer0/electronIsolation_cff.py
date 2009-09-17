e7615e47aef726b0e4935048d03ed7a6f6500d07
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-09-17 10:46:14 +0000

    replaced ECAL iso deposit collection from ecalRecHits, to reducedEcalRecHits which are also on AOD now

--- PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
@@ -2,7 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 ## compute isolation, using POG modules
 from RecoEgamma.EgammaIsolationAlgos.eleIsoDeposits_cff import *
-from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDepsModules_cff  import *
+from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDeposits_cff  import *
+
+eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
+eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")
 
 ## sequence to run on AOD 
 patElectronIsolation = cms.Sequence(
--- PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
@@ -4,6 +4,9 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaIsolationAlgos.gamIsoDeposits_cff import *
 from RecoEgamma.EgammaIsolationAlgos.gamIsoFromDepsModules_cff  import *
 
+gamIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
+gamIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")
+
 # sequence to run on AOD 
 patPhotonIsolation = cms.Sequence(
     gamIsoDepositTk * gamIsoFromDepsTk +
