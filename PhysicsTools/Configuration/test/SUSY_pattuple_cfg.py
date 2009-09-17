70762c619890d58905f61e6d94ab935e739a5462
Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch>
2009-09-17 11:47:39 +0000

    New version, including anti-KT.

--- PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py
+++ PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py
@@ -3,7 +3,7 @@
 #
 #  PAT configuration for the SUSY group - 3X series
 #  More information here:
-#  https://twiki.cern.ch/twiki/bin/view/CMS/SusyPatLayer1DefV6 
+#  https://twiki.cern.ch/twiki/bin/view/CMS/SusyPatLayer1DefV6
 #
 
 # Starting with a skeleton process which gets imported with the following line
@@ -12,17 +12,18 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 #-- Message Logger ------------------------------------------------------------
 process.MessageLogger.categories.append('PATSummaryTables')
-process.MessageLogger.cerr = cms.untracked.PSet(
-    default          = cms.untracked.PSet( limit = cms.untracked.int32(-1)  ),
-    PATSummaryTables = cms.untracked.PSet( limit = cms.untracked.int32(-1) )
-)
+process.MessageLogger.cerr.PATSummaryTables = cms.untracked.PSet(
+    limit = cms.untracked.int32(-1),
+    reportEvery = cms.untracked.int32(1)
+    )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 #-- Input Source --------------------------------------------------------------
 process.source.fileNames = [
     'rfio://?svcclass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/fronga/V6production/PYTHIA6_SUSY_LM0_sftsht_10TeV_cff_py_RAW2DIGI_RECO_1.root'
     ]
-process.maxEvents.input = -1
-# Due to problem in production of LM samples
+process.maxEvents.input = 100
+# Due to problem in production of LM samples: same event number appears multiple times
 process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
 #-- Calibration tag -----------------------------------------------------------
@@ -35,15 +36,15 @@ process.load("PhysicsTools.PatAlgos.patSequences_cff")
 #-- Changes for electron and photon ID ----------------------------------------
 # Turn off photon-electron cleaning (i.e., flag only)
 process.cleanLayer1Photons.checkOverlaps.electrons.requireNoOverlaps = False
-# Remove embedding of superClusters, will keep entire superCluster collection 
-process.allLayer1Electrons.embedSuperCluster = cms.bool(False)
-process.allLayer1Photons.embedSuperCluster   = cms.bool(False)
+# Remove embedding of superClusters, will keep entire superCluster collection
+process.allLayer1Electrons.embedSuperCluster = False
+process.allLayer1Photons.embedSuperCluster   = False
 
 #-- Tuning of Monte Carlo matching --------------------------------------------
 # Also match with leptons of opposite charge
 process.electronMatch.checkCharge = False
-process.muonMatch.checkCharge = False
-process.tauMatch.checkCharge = False
+process.muonMatch.checkCharge     = False
+process.tauMatch.checkCharge      = False
 
 #-- JetPlusTrack --------------------------------------------------------------
 # produce jpt corrected calo jets, which are not on AOD per default
@@ -52,6 +53,26 @@ process.jpt = cms.Path( process.jptCaloJets )
 
 #-- Extra Jet/MET collections -------------------------------------------------
 from PhysicsTools.PatAlgos.tools.jetTools import *
+# Correct default KT settings (default has new input tag names from 33X)
+switchJetCollection(process,
+                    cms.InputTag('antikt5CaloJets'),   
+                    doJTA            = True,            
+                    doBTagging       = True,            
+                    jetCorrLabel     = ('AK5','Calo'),  
+                    doType1MET       = True,            
+                    genJetCollection = cms.InputTag("antikt5GenJets")
+                    )
+# Add a few jet collections...
+addJetCollection(process,cms.InputTag('iterativeCone5CaloJets'),
+                 'IC5',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = None,
+                 doType1MET   = True,
+                 doL1Cleaning = True,
+                 doL1Counters = True,
+                 genJetCollection=cms.InputTag("iterativeCone5GenJets")
+                 )
 addJetCollection(process,cms.InputTag('iterativeCone5PFJets'),
                  'IC5PF',
                  doJTA        = True,
@@ -72,11 +93,12 @@ addJetCollection(process,cms.InputTag('sisCone5CaloJets'),
                  doL1Counters = True,
                  genJetCollection=cms.InputTag("sisCone5GenJets")
                  )
+# FIXME: Need Summer09 JPT corrections!
 addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetIcone5'),
                  'IC5JPT',
                  doJTA        = True,
                  doBTagging   = True,
-                 jetCorrLabel = ('IC5','JPT'),
+                 jetCorrLabel = None, # ('IC5','JPT'), <-- HERE
                  doType1MET   = False,
                  doL1Cleaning = True,
                  doL1Counters = True,
@@ -85,19 +107,21 @@ addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetIcone5'),
 
 # Add tcMET and PFMET
 from PhysicsTools.PatAlgos.tools.metTools import *
-addTcMET(process, 'TC')
+addTcMET(process,'TC')
 addPfMET(process,'PF')
 
 #-- Tune contents of jet collections  -----------------------------------------
-for jetName in ( '', 'IC5PF', 'SC5', 'IC5JPT' ):
+for jetName in ( '', 'IC5', 'IC5PF', 'SC5', 'IC5JPT' ):
     module = getattr(process,'allLayer1Jets'+jetName)
     module.addTagInfos = False    # Remove tag infos
     module.addJetID    = True     # Add JetID variables
 
 #-- Output module configuration -----------------------------------------------
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
-process.out.fileName = 'rfio://?svcclass=cmscafuser&path=/castor/cern.ch/cms/store/caf/user/fronga/V6production/LM0-312-SUSYPAT-V6a.root'
-process.out.splitLevel = cms.untracked.int32(99)  # Turn on split level (smaller files)
+process.out.fileName = 'file://./test.root'       # <-- CHANGE THIS TO SUIT YOUR NEEDS
+
+# Custom settings
+process.out.splitLevel = cms.untracked.int32(99)  # Turn on split level (smaller files???)
 process.out.overrideInputFileSplitLevels = cms.untracked.bool(True)
 process.out.dropMetaData = cms.untracked.string('DROPPED')   # Get rid of metadata related to dropped collections
 process.out.outputCommands = [ 'drop *' ]
@@ -123,6 +147,7 @@ process.out.outputCommands.extend( [ # PAT Objects
                                      'keep edmTriggerResults_TriggerResults_*_HLT',
                                      'keep *_hltTriggerSummaryAOD_*_*',
                                      'keep L1GlobalTriggerObjectMapRecord_*_*_*',
+                                     'keep L1GlobalTriggerReadoutRecord_*_*_*',
                                      # Others
                                      'keep *_muon*METValueMapProducer_*_*',   # Muon corrections to MET
                                      'keep *_offlinePrimaryVertices_*_*',
@@ -145,21 +170,21 @@ process.patTriggerEvent.patTriggerMatches  = ()
 
 
 #-- Execution path ------------------------------------------------------------
-# Re-name IC5 collections for uniformity
-process.cleanLayer1JetsIC5 = process.cleanLayer1Jets
-process.layer1METsIC5      = process.layer1METs
+# Rename default anti-kt collection for uniformity
+process.cleanLayer1JetsAK5 = process.cleanLayer1Jets
+process.layer1METsAK5      = process.layer1METs
 
 # Modify subsequent modules
-process.cleanLayer1Hemispheres.patJets = process.cleanLayer1JetsIC5.label()
-process.countLayer1Jets.src            = process.cleanLayer1JetsIC5.label()
+process.cleanLayer1Hemispheres.patJets = process.cleanLayer1JetsAK5.label()
+process.countLayer1Jets.src            = process.cleanLayer1JetsAK5.label()
 
 # Modify counters' input
 process.allLayer1Summary.candidates.remove(cms.InputTag('layer1METs'))
-process.allLayer1Summary.candidates.append(cms.InputTag('layer1METsIC5'))
+process.allLayer1Summary.candidates.append(cms.InputTag('layer1METsAK5'))
 process.cleanLayer1Summary.candidates.remove(cms.InputTag('cleanLayer1Jets'))
-process.cleanLayer1Summary.candidates.append(cms.InputTag('cleanLayer1JetsIC5'))
+process.cleanLayer1Summary.candidates.append(cms.InputTag('cleanLayer1JetsAK5'))
 # Add new jet collections to counters (MET done automatically)
-for jets in ( 'SC5','IC5PF','IC5JPT' ):
+for jets in ( 'IC5', 'SC5','IC5PF','IC5JPT' ):
     process.allLayer1Summary.candidates.append(cms.InputTag('allLayer1Jets'+jets))
     process.selectedLayer1Summary.candidates.append(cms.InputTag('selectedLayer1Jets'+jets))
     process.cleanLayer1Summary.candidates.append(cms.InputTag('cleanLayer1Jets'+jets))
