742e2c34de7daf9ef673a73b06a9343ae10aeb87
Piotr Traczyk <sha1-f63c8ead76e5e4a129ecffbbd6f7786205ea2389@cern.ch>
2009-09-18 09:53:32 +0000

    Example to run the timing test code

--- RecoMuon/MuonIdentification/test/test_timing.py
+++ RecoMuon/MuonIdentification/test/test_timing.py
@@ -1,34 +1,57 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("test")
-
-process.load("FWCore.MessageService.MessageLogger_cfi")
-
-process.load('Configuration/StandardSequences/GeometryPilot2_cff')
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-
-#process.prefer("GlobalTag")
-#process.GlobalTag.globaltag = 'IDEAL_V11::All'
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
+process = cms.Process("Test")
 
 process.source = cms.Source("PoolSource",
-  fileNames = cms.untracked.vstring(
-'/store/relval/CMSSW_3_1_0_pre4/RelValSingleMuPt100/GEN-SIM-RECO/IDEAL_30X_v1/0003/1C780F8D-AB16-DE11-B436-001617E30E2C.root'
-#'file:/data/ptraczyk/relval/310_100/80B3394A-180A-DE11-BC92-000423D99AAE.root'
-  
-  )
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(1),
+	
+    fileNames = cms.untracked.vstring(
+
+        '/store/relval/CMSSW_3_2_1/RelValSingleMuPt100/GEN-SIM-RECO/MC_31X_V3-v1/0006/DC15F12B-9477-DE11-B1E0-000423D98C20.root',
+        '/store/relval/CMSSW_3_2_1/RelValSingleMuPt100/GEN-SIM-RECO/MC_31X_V3-v1/0006/40D6FEFD-8F77-DE11-95A7-001D09F27067.root',
+        '/store/relval/CMSSW_3_2_1/RelValSingleMuPt100/GEN-SIM-RECO/MC_31X_V3-v1/0005/50EE1208-8177-DE11-8B17-001D09F231B0.root'
+
+    )
 )
 
-process.load("RecoMuon.MuonIdentification.muonTiming_cfi")
-
-process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('myOutputFile.root')
+process.maxEvents = cms.untracked.PSet(
+	input=cms.untracked.int32(100)
 )
 
-#process.p = cms.Path(process.siPixelRecHits+process.siStripMatchedRecHits+process.ckftracks+process.muonrecowith_TeVRefinemen+process.muontiming)
-process.p = cms.Path(process.muontiming)
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
+process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+from Configuration.StandardSequences.Reconstruction_cff import *
 
-process.e = cms.EndPath(process.out)
+process.muonAnalyzer = cms.EDFilter("MuonTimingValidator",
+  TKtracks = cms.untracked.InputTag("generalTracks"),
+  STAtracks = cms.untracked.InputTag("standAloneMuons"),
+  Muons = cms.untracked.InputTag("muons"),
+  nbins = cms.int32(60),
+  PtresMax = cms.double(2000.0),
+  Timing = cms.untracked.InputTag("muontiming"),
+  simPtMin = cms.double(5.0),
+  PtresMin = cms.double(-1000.0),
+  PtCut = cms.double(1.0),
+  etaMax = cms.double(2.4),
+  etaMin = cms.double(0.0),
+  open = cms.string('recreate'),
+  out = cms.string('test_timingv.root')
+)
+
+process.prefer("GlobalTag")
+
+process.GlobalTag.globaltag = 'MC_31X_V3::All'
+#process.GlobalTag.globaltag = 'STARTUP_V7::All'
+
+process.p = cms.Path(muontiming)
+
+process.mutest = cms.Path(process.muonAnalyzer)
+
+process.schedule = cms.Schedule(process.p,process.mutest)	
+# process.schedule = cms.Schedule(process.mutest)	
+
