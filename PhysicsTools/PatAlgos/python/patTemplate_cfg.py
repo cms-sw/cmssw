377508afc518b5ce35e411746da89636791001f9
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-09-18 12:54:27 +0000

    cleaned up the patTemplate: a) try to prevent the use of tools in the template file, 2) even better: as ak5 is the default we did not need the switchJetCollection tool here anymore ;-)

--- PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -2,50 +2,38 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("PAT")
 
-# initialize MessageLogger and output report
+## MessageLogger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+## Options and Output Report
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
-# source
+## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_3_3_0_pre2/RelValTTbar/GEN-SIM-RECO/STARTUP31X_V7-v1/0002/BA958CA5-B19B-DE11-90C6-0018F3D0961A.root'
     )
 )
-
+## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
+## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string('STARTUP31X_V1::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
-
-# load the standard PAT config 
+## Standard PAT Configuration File
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-from PhysicsTools.PatAlgos.tools.jetTools import *
-print "*********************************************************************"
-print "Switching all processes to use the anti-kT algorithm by default."
-print "Switch the jet collection to your desired algorithm if this is not"
-print "what you want to use."
-print "*********************************************************************"
-switchJetCollection(process, 
-                    cms.InputTag('ak5CaloJets'),   
-                    doJTA            = True,            
-                    doBTagging       = True,            
-                    jetCorrLabel     = ('AK5','Calo'),  
-                    doType1MET       = True,            
-                    genJetCollection = cms.InputTag("ak5GenJets")
-                    ) 
-
-# Output module configuration
+# Output Module Configuration (expects a path 'p')
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
 process.out = cms.OutputModule("PoolOutputModule",
-                                   fileName = cms.untracked.string('PATLayer1_Output.fromAOD_full.root'),
-                                   # save only events passing the full path
-                                   SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
-                                   # save PAT Layer 1 output
-                                   outputCommands = cms.untracked.vstring('drop *', *patEventContent ) # you need a '*' to unpack the list of commands 'patEventContent'
+                               fileName = cms.untracked.string('PATLayer1_Output.fromAOD_full.root'),
+                               # save only events passing the full path
+                               SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+                               # save PAT Layer 1 output; you need a '*' to
+                               # unpack the list of commands 'patEventContent'
+                               outputCommands = cms.untracked.vstring('drop *', *patEventContent ) 
                                )
 process.outpath = cms.EndPath(process.out)
