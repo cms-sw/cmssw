d948772ab14fd73ebc77146631daf37f15209918
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-09-18 20:11:36 +0000

    adapt for new corrector setup

--- PhysicsTools/PatAlgos/python/recoLayer0/jetPlusTrack_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/jetPlusTrack_cff.py
@@ -4,6 +4,10 @@ from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
 from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
 
 jptCaloJets = cms.Sequence(
-    ZSPJetCorrections *
-    JetPlusTrackCorrections
+    ZSPJetCorrectionsIcone5 *
+    ZSPJetCorrectionsSisCone5 *
+    ZSPJetCorrectionsAntiKt5 *
+    JetPlusTrackCorrectionsIcone5 *
+    JetPlusTrackCorrectionsSisCone5 *
+    JetPlusTrackCorrectionsAntiKt5
     )
