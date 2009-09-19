e47366ccdd44bcfcb8f62d6659b0982d27da6873
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-09-19 03:30:25 +0000

    Add capability to change cuts, indexed by string for bitset.

--- PhysicsTools/Utilities/interface/Selector.h
+++ PhysicsTools/Utilities/interface/Selector.h
@@ -23,13 +23,29 @@ template<class T>
 class Selector : public std::unary_function<T, bool>  {
   
  public:
+  typedef std::map<size_t, int>      int_map;
+  typedef std::map<size_t, double>   double_map;
 
   /// Constructor clears the bits
-  Selector( ) { bits_.clear();}
+  Selector( ) { bits_.clear(); intCuts_.clear(); doubleCuts_.clear();}
+
+  virtual ~Selector() {}
 
   /// This is the registration of an individual cut string
   void push_back( std::string s) {
     bits_.push_back(s);
+  }
+
+  /// This is the registration of an individual cut string, with an int cut value
+  void push_back( std::string s, int cut) {
+    bits_.push_back(s);
+    intCuts_[bits_[s]] = cut;
+  }
+
+  /// This is the registration of an individual cut string, with a double cut value
+  void push_back( std::string s, double cut) {
+    bits_.push_back(s);
+    doubleCuts_[bits_[s]] = cut;
   }
 
   /// This provides the interface for base classes to select objects
@@ -40,21 +56,46 @@ class Selector : public std::unary_function<T, bool>  {
     bits_[s] = val;
   }
 
+  /// Set a given selection cut, on or off, and reset int cut value
+  void set(std::string s, int cut, bool val = true) {
+    bits_[s] = val;
+    intCuts_[bits_[s]] = cut;
+  }
+
+  /// Set a given selection cut, on or off, and reset int cut value
+  void set(std::string s, double cut, bool val = true) {
+    bits_[s] = val;
+    doubleCuts_[bits_[s]] = cut;
+  }
+
   /// Turn off a given selection cut. 
   void clear(std::string s) {
     bits_[s] = false;
   }
 
+  /// Access the selector cut at index "s"
   bool operator[] ( std::string s ) const {
     return bits_[s];
   }
+
+  /// Access the int cut values at index "s"
+  int cut( std::string s, int val ) const {
+    return intCuts_.find( bits_[s] )->second;
+  };
+
+  /// Access the double cut values at index "s"
+  double cut( std::string s, double val ) const {
+    return doubleCuts_.find( bits_[s] )->second;
+  };
 
   /// Print the bitset
   void print(std::ostream & out) const { bits_.print(out);
   }
 
  protected:
-  strbitset     bits_; //!< the bitset indexed by strings
+  strbitset     bits_;        //!< the bitset indexed by strings
+  int_map       intCuts_;     //!< the int-value cut map
+  double_map    doubleCuts_;  //!< the double-value cut map
 };
 
 #endif
