e5c5c1277a0980672abf07603ea1ff6a85218a52
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-09-21 10:42:14 +0000

    Completed the SiStripLatency Object

--- CondFormats/SiStripObjects/interface/SiStripLatency.h
+++ CondFormats/SiStripObjects/interface/SiStripLatency.h
@@ -2,6 +2,7 @@
 #define SiStripLatency_h
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,30 +11,36 @@ using namespace std;
  * The latency is stored per apv and the information is compressed by reducing
  * sequences of apvs with the same latency to a single value plus information on the
  * start and end of the sequence. <br>
- * The mode is a single value, stored as a uint16_t defined as: <br>
- * - mode == 0 : deconvolution mode <br>
- * - mode == 1 : peak mode <br>
- * The put method requires the latency values for a given apv and detId. <br>
+ * The mode is a single value, stored as a char defined as: <br>
+ * - mode == 37 : deconvolution mode <br>
+ * - mode == 47 : peak mode <br>
+ * See here http://www.id.clrc.ac.uk/INS/Electronic_Systems/Microelectronics_Design/Projects/High_Energy_Physics/CMS/APV25-S1/files/User_Guide_2.0.pdf
+ * page 13 section 6.5 table 7 for the definition of possible modes. <br>
+ * The put method requires the latency and mode values for a given apv and detId. <br>
  * <br>
  * The internal Latency object stores the detId and apv value in a compressed
  * (bit shifted) uint32_t holding both the values. It stores the latency value
- * in a float. <br>
- * the Apv where the latency assumes a value different than the one before. <br>
- * To save space, since typically the latency is the same for all modules, the ranges
+ * in a float and the mode value in an unsigned char. <br>
+ * To save space, since typically the latency and mode is the same for all apvs, the ranges
  * of consecutive detIds and apvs are collapsed in the last value, so that the lower_bound
- * binary search will return the correct latency.
+ * binary search will return the correct latency. <br>
+ * <br>
+ * Methods are provided to extract latency and mode (separately or together in a pair)
+ * for each apv. <br>
+ * If the value of latency (mode) is the same for the whole Tracker, the singleLatency()
+ * (singleMode()) method will return it, otherwise it will return -1 (0). <br>
  */
 
 class SiStripLatency
 {
  public:
 
-  SiStripLatency() :
-    mode_(-1)
-  {}
-  SiStripLatency(const int16_t mode) :
-    mode_(mode)
-  {}
+//   SiStripLatency() :
+//     mode_(-1)
+//   {}
+//   SiStripLatency(const int16_t mode) :
+//     mode_(mode)
+//   {}
 
   /** Saves the detIdAndApv and latency values in the vector of Latency objects.
    * At the end of the filling phase, the compress method should be called to
@@ -41,47 +48,62 @@ class SiStripLatency
    * if the compress method is not called, only the space used would be more than
    * needed.
    */
-  bool put( const uint32_t detId, const uint16_t apv, const float & latency );
-  float get(const uint32_t detId, const uint16_t apv);
+  bool put( const uint32_t detId, const uint16_t apv, const float & latency, const uint16_t mode );
+  float latency(const uint32_t detId, const uint16_t apv) const;
+  uint16_t mode(const uint32_t detId, const uint16_t apv) const;
+  pair<float, uint16_t> latencyAndMode(const uint32_t detId, const uint16_t apv) const;
+
   /** Reduce ranges of consecutive detIdsAndApvs with the same latency to one value (the latest)
    * so that lower_bound will return the correct value for latency.
    */
   void compress();
   /// If all the latency values stored are equal return that value, otherwise return -1
-  float getSingleLatency();
+  float singleLatency() const;
+  uint16_t singleMode() const;
+  //   pair<float, uint16_t> singleLatencyAndMode() const;
 
-  inline void setMode( const int16_t mode ) {
-    mode_ = mode;
-  }
-  inline bool mode() {
-    return mode_;
-  }
  private:
   struct Latency
   {
-    Latency(const uint32_t inputDetIdAndApv, const float & inputLatency) :
+    Latency(const uint32_t inputDetIdAndApv, const float & inputLatency, const uint16_t inputMode) :
+      detIdAndApv(inputDetIdAndApv),
       latency(inputLatency),
-      detIdAndApv(inputDetIdAndApv)
+      mode(inputMode)
     {}
-    float latency;
     uint32_t detIdAndApv;
+    float latency;
+    unsigned char mode;
   };
   typedef vector<Latency>::iterator latIt;
   typedef vector<Latency>::const_iterator latConstIt;
+
   struct OrderByDetIdAndApv
   {
     bool operator()(const Latency & lat1, const uint32_t detIdAndApv) const {
       return lat1.detIdAndApv < detIdAndApv;
     }
   };
+
   struct EqualByLatency
   {
     bool operator()(const Latency & lat1, const Latency & lat2) {
       return( lat1.latency == lat2.latency );
     }
   };
+
+  /// Used to compute the position with the lower_bound binary search
+  // If put in the cc file it will not know about the typedefs and the Latency class
+  const latConstIt position(const uint32_t detId, const uint16_t apv) const
+  {
+    if( latencies_.empty() ) {
+      // cout << "SiStripLatency: Error, range is empty" << endl;
+      return latencies_.end();
+    }
+    uint32_t detIdAndApv = (detId << 2) | apv;
+    latConstIt pos = lower_bound(latencies_.begin(), latencies_.end(), detIdAndApv, OrderByDetIdAndApv());
+    return pos;
+  }
   vector<Latency> latencies_;
-  int16_t mode_;
 };
 
 #endif
--- CondFormats/SiStripObjects/src/SiStripLatency.cc
+++ CondFormats/SiStripObjects/src/SiStripLatency.cc
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-bool SiStripLatency::put( const uint32_t detId, const uint16_t apv, const float & latency )
+bool SiStripLatency::put( const uint32_t detId, const uint16_t apv, const float & latency, const uint16_t mode )
 {
   // Store all the values in the vectors
   uint32_t detIdAndApv = (detId << 2) | apv;
@@ -16,20 +16,21 @@ bool SiStripLatency::put( const uint32_t detId, const uint16_t apv, const float 
     cout << "Value already inserted, skipping insertion" << endl;
     return false;
   }
-  latencies_.insert(pos, Latency(detIdAndApv, latency));
+  // cout << "Filling with: latency = " << latency << ", mode = " << mode << endl;
+  latencies_.insert(pos, Latency(detIdAndApv, latency, mode));
 
   return true;
 }
 
 void SiStripLatency::compress()
 {
-  cout << "Starting compression" << endl;
-  cout << "Total number of elements before compression = " << latencies_.size() << endl;
+  // cout << "Starting compression" << endl;
+  // cout << "Total number of elements before compression = " << latencies_.size() << endl;
 
-  int i = 0;
-  for( latIt it = latencies_.begin(); it != latencies_.end(); ++it, ++i ) {
-    cout << "latency["<<i<<"] = " << it->latency << ", for detIdAndApv = " << it->detIdAndApv << endl;;
-  }
+  // int i = 0;
+  // for( latIt it = latencies_.begin(); it != latencies_.end(); ++it, ++i ) {
+  //   cout << "latency["<<i<<"] = " << it->latency << ", mode["<<i<<"] = " << (int)it->mode << " for detIdAndApv = " << it->detIdAndApv << endl;;
+  // }
   // Remove latency duplicates. Note that unique is stable.
   // CANNOT USE THIS: it will leave one element, but you do not know which one.
   // unique(latencies_.begin(), latencies_.end(), EqualByLatency());
@@ -40,42 +41,99 @@ void SiStripLatency::compress()
   // for( latIt lat = latencies_.begin(); lat != latencies_.end(); ++lat ) {
   latIt lat = latencies_.begin();
   while( lat != latencies_.end() ) {
-    // If it is not the the last and it has the same latency as the next one
-    if( ((lat+1) != latencies_.end()) && ((lat+1)->latency == lat->latency) ) {
-      // Remove the current one
+    // If it is not the last and it has the same latency and mode as the next one remove it
+    if( ((lat+1) != latencies_.end()) && ((lat+1)->mode == lat->mode) && ((lat+1)->latency == lat->latency) ) {
       lat = latencies_.erase(lat);
     }
     else {
       ++lat;
     }
   }
-  cout << "Total number of elements after compression = " << latencies_.size() << endl;
-  i = 0;
-  for( latIt it = latencies_.begin(); it != latencies_.end(); ++it, ++i ) {
-    cout << "latency["<<i<<"] = " << it->latency << ", for detIdAndApv = " << it->detIdAndApv << endl;;
-  }
+  // cout << "Total number of elements after compression = " << latencies_.size() << endl;
+  // i = 0;
+  // for( latIt it = latencies_.begin(); it != latencies_.end(); ++it, ++i ) {
+  //   cout << "latency["<<i<<"] = " << it->latency << ", mode["<<i<<"] = " << (int)it->mode  << ", for detIdAndApv = " << it->detIdAndApv << endl;;
+  // }
 }
 
-float SiStripLatency::get(const uint32_t detId, const uint16_t apv)
-{
-  if( latencies_.empty() ) {
-    cout << "SiStripLatency: Error, range is empty" << endl;
-    return -1;
-  }
-  uint32_t detIdAndApv = (detId << 2) | apv;
-  latIt pos = lower_bound(latencies_.begin(), latencies_.end(), detIdAndApv, OrderByDetIdAndApv());
+// const latConstIt SiStripLatency::position(const uint32_t detId, const uint16_t apv) const
+// {
+//   if( latencies_.empty() ) {
+//     // cout << "SiStripLatency: Error, range is empty" << endl;
+//     return latencies_.end();
+//   }
+//   uint32_t detIdAndApv = (detId << 2) | apv;
+//   latConstIt pos = lower_bound(latencies_.begin(), latencies_.end(), detIdAndApv, OrderByDetIdAndApv());
+//   return pos;
+// }
 
-  // When the position is after the last one return -1
+float SiStripLatency::latency(const uint32_t detId, const uint16_t apv) const
+{
+  const latConstIt & pos = position(detId, apv);
   if( pos == latencies_.end() ) {
     return -1.;
   }
   return pos->latency;
 }
 
-float SiStripLatency::getSingleLatency()
+uint16_t SiStripLatency::mode(const uint32_t detId, const uint16_t apv) const
+{
+  const latConstIt & pos = position(detId, apv);
+  if( pos == latencies_.end() ) {
+    return 0;
+  }
+  return pos->mode;
+}
+
+pair<float, uint16_t> SiStripLatency::latencyAndMode(const uint32_t detId, const uint16_t apv) const
+{
+  const latConstIt & pos = position(detId, apv);
+  if( pos == latencies_.end() ) {
+    return make_pair(-1., 0);
+  }
+  return make_pair(pos->latency, pos->mode);
+}
+
+float SiStripLatency::singleLatency() const
 {
   if( latencies_.size() == 1 ) {
     return latencies_[0].latency;
   }
+  int differentLatenciesNum = 0;
+  // Count the number of different latencies
+  for( latConstIt it = latencies_.begin(); it != latencies_.end()-1; ++it ) {
+    if( it->latency != (it+1)->latency ) {
+      ++differentLatenciesNum;
+    }
+  }
+  if( differentLatenciesNum == 0 ) {
+    return latencies_[0].latency;
+  }
   return -1;
 }
+
+uint16_t SiStripLatency::singleMode() const
+{
+  if( latencies_.size() == 1 ) {
+    return latencies_[0].mode;
+  }
+  int differentModesNum = 0;
+  // Count the number of different modes
+  for( latConstIt it = latencies_.begin(); it != latencies_.end()-1; ++it ) {
+    if( it->mode != (it+1)->mode ) {
+      ++differentModesNum;
+    }
+  }
+  if( differentModesNum == 0 ) {
+    return latencies_[0].mode;
+  }
+  return 0;
+}
+
+// pair<float, uint16_t> SiStripLatency::singleLatencyAndMode() const
+// {
+//   if( latencies_.size() == 1 ) {
+//     return make_pair(latencies_[0].latency, latencies_[0].mode);
+//   }
+//   return make_pair(-1, 0);
+// }
