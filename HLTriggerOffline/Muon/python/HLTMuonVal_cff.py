cb117064cfa72c42f3b5f89cce40e91ebd857520
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2009-09-21 19:22:40 +0000

    Added relvalMuonBits module

--- HLTriggerOffline/Muon/python/HLTMuonVal_cff.py
+++ HLTriggerOffline/Muon/python/HLTMuonVal_cff.py
@@ -4,13 +4,27 @@ from Validation.RecoMuon.muonValidationHLT_cff import *
 from HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi import *
 from Validation.RecoMuon.muonValidationHLTFastSim_cff import *
 
+print "Initializing muonBits"
+from DQM.HLTEvF.HLTMonMuonBits_cfi import *
+relvalMuonBits = hltMonMuBits.clone(
+    directory = cms.untracked.string('HLT/Muon/'),
+    HLTPaths = cms.vstring('HLT_L1Mu+',
+                           'HLT_L2Mu+',
+                           'HLT_Mu+',
+                           'HLT_IsoMu+',
+                           'HLT_DoubleMu+'
+                           )
+    )
+
 HLTMuonVal = cms.Sequence(
     recoMuonValidationHLT_seq + 
-    muonTriggerRateTimeAnalyzer
+    muonTriggerRateTimeAnalyzer +
+    relvalMuonBits
     )
 
 HLTMuonVal_FastSim = cms.Sequence(
     recoMuonValidationHLTFastSim_seq +
-    muonTriggerRateTimeAnalyzer
+    muonTriggerRateTimeAnalyzer +
+    relvalMuonBits
     )
 
