3be0cee3ea80fc52d2fbdfd7309d9973e0400fbe
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2009-09-21 15:22:37 +0000

    Make the SM actually write out the file in CMSSW_3_X_Y.

--- DQM/SiStripCommissioningSources/python/StorageManager_cfg.py
+++ DQM/SiStripCommissioningSources/python/StorageManager_cfg.py
@@ -9,7 +9,8 @@ process.out = cms.OutputModule("EventStreamFileWriter",
     maxSize = cms.int32(1000),
     compression_level = cms.untracked.int32(1),
     use_compression = cms.untracked.bool(False),
-    max_event_size = cms.untracked.int32(25000000)
+    max_event_size = cms.untracked.int32(25000000),
+    SelectHLTOutput = cms.untracked.string('consumer')
 )
 
 process.e1 = cms.EndPath(process.out)
