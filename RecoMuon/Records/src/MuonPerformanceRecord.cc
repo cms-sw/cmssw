2cff0c4b7b292e433bd2557eed56e5180f23605f
Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch>
2009-09-21 12:21:31 +0000

    ES registration macro for reading tag/probe efficiencies from DB.

---
+++ RecoMuon/Records/src/MuonPerformanceRecord.cc
@@ -0,0 +1,5 @@
+#include "RecoMuon/Records/interface/MuonPerformanceRecord.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(MuonPerformanceRecord);
+
