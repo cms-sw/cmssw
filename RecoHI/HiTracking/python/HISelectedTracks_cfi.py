5f25cb7cddeb9ef53f5ff40931a9e577f3c31890
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-09-22 09:45:22 +0000

    d0 is a signed quantity!

--- RecoHI/HiTracking/python/HISelectedTracks_cfi.py
+++ RecoHI/HiTracking/python/HISelectedTracks_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 # reco track quality cuts (dca, nhits, prob) and minpt selection
 hiSelectedTracks = cms.EDFilter("TrackSelector",
   src = cms.InputTag("hiGlobalPrimTracks"),
-  cut = cms.string('pt > 0.9 && d0/d0Error<3 && recHitsSize>12 && chi2prob(chi2,ndof)>0.01')
+  cut = cms.string('pt > 0.9 && abs(d0/d0Error)<3 && recHitsSize>12 && chi2prob(chi2,ndof)>0.01')
 )
