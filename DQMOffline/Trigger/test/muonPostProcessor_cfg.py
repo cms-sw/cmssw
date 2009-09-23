148ff7979f3a81d8a706ebae6450cc518ad62508
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-09-23 15:43:08 +0000

    Updated to be more flexible and use command line arguments

--- DQMOffline/Trigger/test/muonPostProcessor_cfg.py
+++ DQMOffline/Trigger/test/muonPostProcessor_cfg.py
@@ -1,15 +1,45 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
 
 process = cms.Process("EDMtoMEConvert")
 
-process.load("DQMServices.Components.EDMtoMEConverter_cff")
+#process.load("DQMServices.Components.EDMtoMEConverter_cff")
+process.load('Configuration/StandardSequences/EDMtoMEAtJobEnd_cff')
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("DQMOffline.Trigger.MuonPostProcessor_cfi")
+process.load("DQMOffline.Trigger.BPAGPostProcessor_cff")
 process.load("DQMServices.Components.DQMStoreStats_cfi")
+
+## parse some command line arguments
+
+options = VarParsing.VarParsing ('standard')
+
+options.register ('outputDir',
+                  -1, # default value
+                  VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  "Directory for DQM saver to put your output")
+
+options.register ('workflow',
+                  -1, # default value
+                  VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  "One string w/ 3 slashes: /Blah/fu/mEh")
+
+
+options.output = 'file:/data/ndpc0/b/slaunwhj/scratch0/EDM_ttbar_n2000_NewPath.root'
+options.maxEvents = 10
+options.outputDir = '/data/ndpc1/b/slaunwhj/scratch0/'
+options.workflow  = '/DQMGeneric/BPAG/Post'
+options.parseArguments()
+
+
+
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(options.maxEvents)
 )
 
 process.MessageLogger = cms.Service("MessageLogger",
@@ -28,14 +58,21 @@ process.source = cms.Source("PoolSource",
 							#fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigOffline_nALL_useAodAndRAW_vMorePlots_DRStudy.root')
 							#fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/EDM_cosmic_vMoreTrigs_newAna.root'),
 							#fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/EDM_jpsi_pre10.root'),
-							fileNames = cms.untracked.vstring('file:/data/ndpc0/b/slaunwhj/scratch0/EDM_ttbar_n2000_NewPath.root'),
+							fileNames = cms.untracked.vstring(options.files),
 							#fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigOffline_nALL_p1_vMorePlots.root')
 )
 
 
-#process.hLTMuonPostVal.outputFileName = cms.untracked.string('/afs/cern.ch/user/s/slaunwhj/scratch0/MuonTrigPostProcessor_histos_vMoreTrigs_newAna.root')
-#process.hLTMuonPostVal.outputFileName = cms.untracked.string('/afs/cern.ch/user/s/slaunwhj/scratch0/Histos_cosmic_vMoreTrigs_newAna.root')
-#process.hLTMuonPostVal.outputFileName = cms.untracked.string('/afs/cern.ch/user/s/slaunwhj/scratch0/Histos_jpsi_pre10.root')
-process.hLTMuonPostVal.outputFileName = cms.untracked.string('file:/data/ndpc0/b/slaunwhj/scratch0/Histos_ttbar_n2000_NewPath.root')
-process.path1 = cms.Path(process.EDMtoMEConverter*process.hLTMuonPostVal*process.dqmStoreStats)
-process.DQMStore.referenceFileName = ''
+process.dqmSaver.workflow = options.workflow
+process.dqmSaver.dirName = options.outputDir
+
+
+
+
+process.path = cms.Path(process.EDMtoME*process.hLTMuonPostVal*process.bPAGPostProcessor*process.dqmStoreStats)
+#process.path = cms.Path(process.EDMtoME*process.bPAGPostProcessor*process.dqmStoreStats)
+
+process.endpath = cms.EndPath(process.dqmSaver)
+
+
+
