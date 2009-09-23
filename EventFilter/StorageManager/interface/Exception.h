c8b36ed08a169db087e5a689f48c426950e71f71
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-09-23 13:01:59 +0000

    Add InvalidEventSelection exception/alarm.

--- EventFilter/StorageManager/interface/Exception.h
+++ EventFilter/StorageManager/interface/Exception.h
@@ -1,4 +1,4 @@
-// $Id: Exception.h,v 1.7 2009/09/18 09:10:52 mommsen Exp $
+// $Id: Exception.h,v 1.8 2009/09/18 11:08:59 mommsen Exp $
 /// @file: Exception.h 
 
 #ifndef StorageManager_Exception_h
@@ -32,8 +32,8 @@ namespace stor {
      List of exceptions thrown by the StorageManager
 
      $Author: mommsen $
-     $Revision: 1.7 $
-     $Date: 2009/09/18 09:10:52 $
+     $Revision: 1.8 $
+     $Date: 2009/09/18 11:08:59 $
     
      @file: Exception.h
   */
@@ -83,6 +83,11 @@ XCEPT_DEFINE_EXCEPTION(stor, IncompleteInitMessage)
  * Exception raised in case of requesting information from a faulty or incomplete event message
  */
 XCEPT_DEFINE_EXCEPTION(stor, IncompleteEventMessage)
+
+/**
+ * Exception raised if event selector cannot be initialized
+ */
+XCEPT_DEFINE_EXCEPTION(stor, InvalidEventSelection)
 
 /**
  * Exception raised when the SM is unable to determine which resource
