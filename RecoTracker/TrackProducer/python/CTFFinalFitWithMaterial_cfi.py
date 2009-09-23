b270048cde198dc660ac5041c3de60c00978f90b
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2009-09-23 15:47:40 +0000

    switching on the 1D updator in generalTracks and module cloning the default TrackProducer

--- RecoTracker/TrackProducer/python/CTFFinalFitWithMaterial_cfi.py
+++ RecoTracker/TrackProducer/python/CTFFinalFitWithMaterial_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 import RecoTracker.TrackProducer.TrackProducer_cfi
-ctfWithMaterialTracks = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProducer.clone()
+ctfWithMaterialTracks = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProducer.clone(
+    Fitter = cms.string('KFFittingSmootherWithOutliersRejectionAndRK')
+    )
 
 #ctfWithMaterialTracks = cms.EDProducer("TrackProducer",
 #    src = cms.InputTag("ckfTrackCandidates"),
