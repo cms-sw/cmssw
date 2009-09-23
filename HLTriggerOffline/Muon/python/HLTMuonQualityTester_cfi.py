1d2c408be5f938262b83f00281d05c58448b6bdb
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2009-09-23 19:23:21 +0000

    Initial commit

---
+++ HLTriggerOffline/Muon/python/HLTMuonQualityTester_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+hltMuonQualityTester = cms.EDFilter("QualityTester",
+    qtList = cms.untracked.FileInPath('HLTriggerOffline/Muon/data/QualityTests.xml'),
+    reportThreshold         = cms.untracked.string('black'),
+    prescaleFactor          = cms.untracked.int32(1),
+    getQualityTestsFromFile = cms.untracked.bool(True),
+    qtestOnEndJob           = cms.untracked.bool(True),
+    qtestOnEndLumi          = cms.untracked.bool(False),
+    testInEventloop         = cms.untracked.bool(False),
+    verboseQT               = cms.untracked.bool(False)
+)
