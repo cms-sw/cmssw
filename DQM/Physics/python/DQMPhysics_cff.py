1dee07cb69d249f13979eba7a014a8c820a3e38d
Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch>
2009-09-24 13:13:15 +0000

    added ewkWMuNuDQM sequqnce

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -2,12 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.Physics.bphysicsOniaDQM_cfi import *
 from DQM.Physics.ewkDQM_cfi import *
+from DQM.Physics.ewkWMuNuDQM_cfi import *
 from DQM.Physics.qcdPhotonsDQM_cfi import *
 from DQM.Physics.qcdHighPtDQM_cfi import *
 from DQM.Physics.topDiLeptonDQM_cfi import *
 
 dqmPhysics = cms.Sequence(bphysicsOniaDQM
 			 *ewkDQM
+                         *ewkWMuNuDQM
 			 *qcdPhotonsDQM
                          *qcdHighPtDQM
 			 *topDiLeptonDQM
