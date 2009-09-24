f72b07d77b4b1c99941b8f4d352f5128143a949d
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-09-24 16:02:44 +0000

    tidy up, use common default param set in _cfi.py

--- JetMETCorrections/Configuration/python/JetPlusTrackCorrections_cff.py
+++ JetMETCorrections/Configuration/python/JetPlusTrackCorrections_cff.py
@@ -1,54 +1,63 @@
 import FWCore.ParameterSet.Config as cms
 
-# File: JetCorrections.cff
-# Author: O. Kodolova
-# Date: 1/24/07
-#
-# Jet corrections with tracks for the icone5 jets with ZSP corrections.
-# 
 from JetMETCorrections.Configuration.JetCorrectionsRecord_cfi import *
 from RecoJets.Configuration.RecoJetAssociations_cff import *
 
-from JetMETCorrections.Configuration.JetPlusTrackCorrections_mod_cff import *
+# ---------- Tight Electron ID
 
-# Tight Electron ID
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import eidTight
 JPTeidTight = eidTight.clone()
-# Service definition 
+
+# ---------- Service definition 
+
+from JetMETCorrections.Configuration.JetPlusTrackCorrections_cfi import *
+
 JetPlusTrackZSPCorrectorIcone5 = cms.ESSource(
     "JetPlusTrackCorrectionService",
     JPTZSPCorrectorICone5,
     label = cms.string('JetPlusTrackZSPCorrectorIcone5'),
     )
+JetPlusTrackZSPCorrectorIcone5.JetSplitMerge = cms.int32(0)
+
 JetPlusTrackZSPCorrectorSiscone5 = cms.ESSource(
     "JetPlusTrackCorrectionService",
-    JPTZSPCorrectorSisCone5,
+    JPTZSPCorrectorICone5,
     label = cms.string('JetPlusTrackZSPCorrectorSiscone5'),
     )
+JetPlusTrackZSPCorrectorSiscone5.JetSplitMerge = cms.int32(1)
+
 JetPlusTrackZSPCorrectorAntiKt5 = cms.ESSource(
     "JetPlusTrackCorrectionService",
-    JPTZSPCorrectorAntiKt5,
+    JPTZSPCorrectorICone5,
     label = cms.string('JetPlusTrackZSPCorrectorAntiKt5'),
     )
+JetPlusTrackZSPCorrectorAntiKt5.JetSplitMerge = cms.int32(2)
 
-# Module definition
-JetPlusTrackZSPCorJetIcone5 = cms.EDProducer("CaloJetCorrectionProducer",
+# ---------- Module definition
+
+JetPlusTrackZSPCorJetIcone5 = cms.EDProducer(
+    "CaloJetCorrectionProducer",
     src = cms.InputTag("ZSPJetCorJetIcone5"),
     correctors = cms.vstring('JetPlusTrackZSPCorrectorIcone5'),
     alias = cms.untracked.string('JetPlusTrackZSPCorJetIcone5')
-)
-JetPlusTrackZSPCorJetSiscone5 = cms.EDProducer("CaloJetCorrectionProducer",
+    )
+
+JetPlusTrackZSPCorJetSiscone5 = cms.EDProducer(
+    "CaloJetCorrectionProducer",
     src = cms.InputTag("ZSPJetCorJetSiscone5"),
     correctors = cms.vstring('JetPlusTrackZSPCorrectorSiscone5'),
     alias = cms.untracked.string('JetPlusTrackZSPCorJetSiscone5')
-)
-JetPlusTrackZSPCorJetAntiKt5 = cms.EDProducer("CaloJetCorrectionProducer",
+    )
+
+JetPlusTrackZSPCorJetAntiKt5 = cms.EDProducer(
+    "CaloJetCorrectionProducer",
     src = cms.InputTag("ZSPJetCorJetAntiKt5"),
     correctors = cms.vstring('JetPlusTrackZSPCorrectorAntiKt5'),
     alias = cms.untracked.string('JetPlusTrackZSPCorJetAntiKt5')
-)
+    )
 
-# Jet-Track association
+# ---------- Jet-Track association
+
 # IC
 from RecoJets.JetAssociationProducers.iterativeCone5JTA_cff import*
 
@@ -91,10 +100,49 @@ ZSPAntiKt5JetExtender.jets = cms.InputTag("ZSPJetCorJetAntiKt5")
 ZSPAntiKt5JetExtender.jet2TracksAtCALO = cms.InputTag("ZSPAntiKt5JetTracksAssociatorAtCaloFace")
 ZSPAntiKt5JetExtender.jet2TracksAtVX = cms.InputTag("ZSPAntiKt5JetTracksAssociatorAtVertex")
 
-### Sequences
-ZSPrecoJetAssociationsIcone5 = cms.Sequence(JPTeidTight*ZSPiterativeCone5JetTracksAssociatorAtVertex*ZSPiterativeCone5JetTracksAssociatorAtCaloFace*ZSPiterativeCone5JetExtender)
-JetPlusTrackCorrectionsIcone5 = cms.Sequence(ZSPrecoJetAssociationsIcone5*JetPlusTrackZSPCorJetIcone5)
-ZSPrecoJetAssociationsSisCone5 = cms.Sequence(JPTeidTight*ZSPSisCone5JetTracksAssociatorAtVertex*ZSPSisCone5JetTracksAssociatorAtCaloFace*ZSPSisCone5JetExtender)
-JetPlusTrackCorrectionsSisCone5 = cms.Sequence(ZSPrecoJetAssociationsSisCone5*JetPlusTrackZSPCorJetSiscone5)
-ZSPrecoJetAssociationsAntiKt5 = cms.Sequence(JPTeidTight*ZSPAntiKt5JetTracksAssociatorAtVertex*ZSPAntiKt5JetTracksAssociatorAtCaloFace*ZSPAntiKt5JetExtender)
-JetPlusTrackCorrectionsAntiKt5 = cms.Sequence(ZSPrecoJetAssociationsAntiKt5*JetPlusTrackZSPCorJetAntiKt5)
+### ---------- Sequences
+
+# IC5
+
+ZSPrecoJetAssociationsIcone5 = cms.Sequence(
+    JPTeidTight*
+    ZSPiterativeCone5JetTracksAssociatorAtVertex*
+    ZSPiterativeCone5JetTracksAssociatorAtCaloFace*
+    ZSPiterativeCone5JetExtender
+    )
+
+JetPlusTrackCorrectionsIcone5 = cms.Sequence(
+    ZSPrecoJetAssociationsIcone5*
+    JetPlusTrackZSPCorJetIcone5
+    )
+
+# SC5
+
+ZSPrecoJetAssociationsSisCone5 = cms.Sequence(
+    JPTeidTight*
+    ZSPSisCone5JetTracksAssociatorAtVertex*
+    ZSPSisCone5JetTracksAssociatorAtCaloFace*
+    ZSPSisCone5JetExtender
+    )
+
+JetPlusTrackCorrectionsSisCone5 = cms.Sequence(
+    ZSPrecoJetAssociationsSisCone5*
+    JetPlusTrackZSPCorJetSiscone5
+    )
+
+# Anti-Kt
+
+ZSPrecoJetAssociationsAntiKt5 = cms.Sequence(
+    JPTeidTight*
+    ZSPAntiKt5JetTracksAssociatorAtVertex*
+    ZSPAntiKt5JetTracksAssociatorAtCaloFace*
+    ZSPAntiKt5JetExtender
+    )
+
+JetPlusTrackCorrectionsAntiKt5 = cms.Sequence(
+    ZSPrecoJetAssociationsAntiKt5*
+    JetPlusTrackZSPCorJetAntiKt5
+    )
+
+# For backward-compatiblity (but to be deprecated!)
+JetPlusTrackCorrections = JetPlusTrackCorrectionsIcone5
