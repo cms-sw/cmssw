e602eaa4b7817f72d5acaf1c844327e533d7d742
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-09-25 14:37:42 +0000

    fix comma

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
@@ -2,7 +2,7 @@ from HLTriggerOffline.HeavyFlavor.heavyFlavorValidation_cfi import *
 
 hfv1 = heavyFlavorValidation.clone(
     TriggerProcessName = cms.untracked.string("HLT"),
-    TriggerPathName = cms.untracked.string("HLT_Mu3")
+    TriggerPathName = cms.untracked.string("HLT_Mu3"),
     MuonPtBins = cms.untracked.vdouble(1., 2.5, 3.5, 4.5, 6., 8., 10., 20., 50.),
     MuonEtaBins = cms.untracked.vdouble(24, -2.4, 2.4)
 )
