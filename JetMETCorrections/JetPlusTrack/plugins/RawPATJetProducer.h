a5ba8e9d4d77277dfee0ced30cf0e29191d06e48
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-09-25 14:39:19 +0000

    gives uncorrected pat jets

---
+++ JetMETCorrections/JetPlusTrack/plugins/RawPATJetProducer.h
@@ -0,0 +1,28 @@
+#ifndef JetMETCorrections_JetPlusTrack_RawPATJetProducer_h
+#define JetMETCorrections_JetPlusTrack_RawPATJetProducer_h
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+
+namespace edm {
+  class Event;
+  class EventSetup;
+  class ParameterSet;
+}
+
+class RawPATJetProducer : public edm::EDProducer {
+
+ public:
+  
+  explicit RawPATJetProducer( const edm::ParameterSet& );
+  ~RawPATJetProducer();
+
+  virtual void produce( edm::Event&, const edm::EventSetup& );
+  
+ private:
+  
+  edm::InputTag jets_;
+  
+};
+
+#endif // JetMETCorrections_JetPlusTrack_RawPATJetProducer_h
