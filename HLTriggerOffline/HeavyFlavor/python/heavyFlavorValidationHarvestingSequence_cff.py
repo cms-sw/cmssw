b72007cb6822d228905e35406cc82c6bfb692877
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-09-25 14:22:40 +0000

    specifies what paths to harvest

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvestingSequence_cff.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvestingSequence_cff.py
@@ -1,3 +1,17 @@
 from HLTriggerOffline.HeavyFlavor.heavyFlavorValidationHarvesting_cfi import *
 
-heavyFlavorValidationHarvestingSequence = cms.Sequence(heavyFlavorValidationHarvesting)
+hfv1 = heavyFlavorValidationHarvesting.clone(
+  MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT/HLT_Mu3')
+)
+hfv2 = heavyFlavorValidationHarvesting.clone(
+  MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT/HLT_IsoMu3')
+)
+hfv3 = heavyFlavorValidationHarvesting.clone(
+  MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT/HLT_DoubleMu0')
+)
+hfv4 = heavyFlavorValidationHarvesting.clone(
+  MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT/HLT_DoubleMu3')
+)
+
+heavyFlavorValidationHarvestingSequence = cms.Sequence(hfv1+hfv2+hfv3+hfv4)
+
