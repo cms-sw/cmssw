28126f818e9c7311f0ee363bd9c409a5cb4bf72f
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-09-25 09:49:45 +0000

    Add trackingParticle customisation for HeavyIons

--- Configuration/StandardSequences/python/HiSignalExtended_cff.py
+++ Configuration/StandardSequences/python/HiSignalExtended_cff.py
@@ -3,3 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from SimGeneral.MixingModule.mixHiSignalExtended_cff import *
 
 phisignal = cms.Sequence(hiSignalSequence)
+
+from SimGeneral.TrackingAnalysis.HiTrackingParticles_cff import *
--- Configuration/StandardSequences/python/HiSignal_cff.py
+++ Configuration/StandardSequences/python/HiSignal_cff.py
@@ -3,3 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from SimGeneral.MixingModule.mixHiSignal_cff import *
 
 phisignal = cms.Sequence(hiSignalSequence)
+
+from SimGeneral.TrackingAnalysis.HiTrackingParticles_cff import *
