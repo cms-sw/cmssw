535db2b0abafaa3aa0dacca602bf5f9639dab28a
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-09-28 12:37:32 +0000

    move the sequence definition in Generator_cff (as needed also by pgen_hi) keep only the hiSignal customization

--- SimGeneral/MixingModule/python/himixGEN_cff.py
+++ SimGeneral/MixingModule/python/himixGEN_cff.py
@@ -1,12 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-##################################################################################
-# Match vertex of the hiSignal event to the background event
-from SimGeneral.MixingModule.MatchVtx_cfi import *
-
-##################################################################################
 # Produce GenParticles of the two HepMCProducts
 from PhysicsTools.HepMCCandAlgos.HiGenParticles_cfi import *
 hiGenParticles.srcVector = ["hiSignal","generator"]
 
-hiSignalGenSequence = cms.Sequence(matchVtx*hiGenParticles)
