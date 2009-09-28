3273c178f3be9b9b97289322ad55a82145d87b52
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2009-09-28 14:48:21 +0000

    Validation sequence cleanup - more easy-to-understand naming

--- Validation/RecoJets/python/JetValidation_cff.py
+++ Validation/RecoJets/python/JetValidation_cff.py
@@ -14,12 +14,10 @@ from JetMETCorrections.Configuration.L2L3Corrections_Summer08Redigi_cff import *
 
 from Validation.RecoJets.JetValidation_cfi import *
 
-#prefer("L2L3JetCorrectorIcone5")
-
-L2L3CorJetIcone5 = cms.EDProducer("CaloJetCorrectionProducer",
-    src = cms.InputTag("iterativeCone5CaloJets"),
-    correctors = cms.vstring('L2L3JetCorrectorIcone5')
-)
-
-
-JetValidation = cms.Sequence(L2L3CorJetIcone5*ZSPJetCorrections*JetPlusTrackCorrections*JetAnalyzer1*JetAnalyzer2*JetAnalyzer3*JetAnalyzer4*JetAnalyzer5*JetAnalyzer6*JetAnalyzer7*JetAnalyzer8)
+JetValidation = cms.Sequence(
+                       process.JetAnalyzerIC5Calo
+                      *process.JetAnalyzerIC5PF
+                      *process.JetAnalyzerKt4Calo*process.JetAnalyzerKt6Calo
+                      *process.JetAnalyzerSc5Calo*process.JetAnalyzerSc7Calo
+                      *process.JetAnalyzerAk5Calo*process.JetAnalyzerAk7Calo
+                      *process.JetAnalyzerAk5PF)
