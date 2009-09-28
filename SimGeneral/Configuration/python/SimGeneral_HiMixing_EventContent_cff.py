b82738b55edb1c1863a56322fc5e92e8419bc9b8
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-09-28 12:35:23 +0000

    Restore hiSignalG4SimHits in the RAW event content

--- SimGeneral/Configuration/python/SimGeneral_HiMixing_EventContent_cff.py
+++ SimGeneral/Configuration/python/SimGeneral_HiMixing_EventContent_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 HiMixRAW = cms.PSet(
     outputCommands = cms.untracked.vstring(
         'keep *_hiSignal_*_*',
-	'keep *_hiGenParticles_*_*'
+        'keep *_hiSignalG4SimHits_*_*',
+        'keep *_hiGenParticles_*_*'
     )
 )
 
