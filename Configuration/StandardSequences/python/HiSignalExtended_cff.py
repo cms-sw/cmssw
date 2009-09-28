3e8068050bb00903f0d4cc48de981b5f4dfd3ef1
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-09-28 08:05:06 +0000

    remove unused sequences for Heavy Ions signal mixing

--- Configuration/StandardSequences/python/HiSignalExtended_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimGeneral.MixingModule.mixHiSignalExtended_cff import *
-
-phisignal = cms.Sequence(hiSignalSequence)
-
-from SimGeneral.TrackingAnalysis.HiTrackingParticles_cff import *
--- Configuration/StandardSequences/python/HiSignal_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimGeneral.MixingModule.mixHiSignal_cff import *
-
-phisignal = cms.Sequence(hiSignalSequence)
-
-from SimGeneral.TrackingAnalysis.HiTrackingParticles_cff import *
