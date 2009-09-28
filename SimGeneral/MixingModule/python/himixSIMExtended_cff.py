c67338e92e7f69f83b6429bfb9100a0d28f9680d
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2009-09-28 12:38:15 +0000

    clone g4SimHits sequence for signal collection only

--- SimGeneral/MixingModule/python/himixSIMExtended_cff.py
+++ SimGeneral/MixingModule/python/himixSIMExtended_cff.py
@@ -4,4 +4,6 @@ from Configuration.StandardSequences.SimExtended_cff import *
 
 LHCTransport.HepMCProductLabel = 'hiSignal' # By default it's "generator" in 3_x_y
 
+hiSignalG4SimHits = g4SimHits.clone()
 
+psim.replace(g4SimHits,hiSignalG4SimHits)
