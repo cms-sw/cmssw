5f145a707549a6e80ba42364d1753fb8aa8d378a
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2009-09-29 15:09:56 +0000

    updated validation scripts

--- Validation/RecoParticleFlow/Benchmarks/ElectronBenchmarkGeneric/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/ElectronBenchmarkGeneric/plot.C
@@ -1,203 +1,168 @@
 {
-  // Get input file name from environment variable; set a default otherwise
-  TString file_name = gSystem->Getenv("TEST_OUTPUT_FILE");
-  if(file_name=="")
-    file_name="benchmark.root";
-
-  TFile *f = new TFile(file_name); 
+  
   gROOT->LoadMacro("../Tools/NicePlot.C");
   InitNicePlot();
+  gROOT->LoadMacro("../Tools/Comparator.C");
+  //  Manager::Init();
 
-  // Get environment variables for output directory name
-  TString dir_name = gSystem->Getenv("DIR_NAME");
-  TString release_name = gSystem->Getenv("DBS_RELEASE");
-  if(release_name=="")
-    release_name="";
-  if(dir_name=="")
-    {
-      cout << " No directory name in DIR_NAME; using Plots " << endl;
-      dir_name="Plots";
-    }
-  else
-    {
-      gSystem->MakeDirectory(release_name);
-      dir_name=release_name+"/"+dir_name;
-      gSystem->MakeDirectory(dir_name);
-      cout << dir_name << endl;
-    }
+  gStyle->SetOptStat(1111);
 
-  f->cd("DQMData/PFTask/Benchmarks/PFlowElectrons/Gen");  
-  Style *spf = spred;  
-  Style *sgen = sback;
+  // we don't do comparisons between two objects, only PF electrons are shown
+  string dir1 = "DQMData/PFTask/Benchmarks/PFlowElectrons/Gen";
+  string dir2 = "DQMData/PFTask/Benchmarks/PFlowElectrons/Gen";
+  const char* file1 = "benchmark.root";
+  const char* file2 = "benchmark.root";
+
+
+  enum EtaModes {
+    BARREL,
+    ENDCAP,
+    ALL
+  };
   
-  TCanvas *c1 = new TCanvas;
-  FormatPad(c1,false);  
+  bool highpt = false;
+  float ptMin = 0;
+  float ptMax = 9999;
+  //Style* style1 = sback;
+ 
+  // Style* style2 = s1;
+  Style* style1 = spred;
+  Style* style2 = spred;
+  Comparator::Mode mode = Comparator::SCALE;
 
-  TH1F* h = (TH1F*) gDirectory->Get("DeltaEt");
-  h->Rebin(2);
-  FormatHisto(h,spf);
-  h->SetStats(1111);
-  h->GetXaxis()->SetRangeUser(-30,30);
-  h->Draw();
-  SavePlot("electronbenchmark",dir_name);
-  c1->Clear();
+ int etamode = BARREL;
+//  int etamode = ENDCAP;
 
-  c1->SetLogy(1);
-  TH1F* h = (TH1F*) gDirectory->Get("DeltaEta");
-  h->Rebin(2);
-  FormatHisto(h,spf);
-  h->SetStats(1111);
-  h->GetXaxis()->SetRangeUser(-0.3,0.3);
-  h->Draw();
-  SavePlot("deltaeta",dir_name);
-  c1->Clear();
-  //c1->SetLogy(0);
+ string outdir = "Plots_BarrelAndEndcap";
+ 
+ // create directory if needed 
+ TString release_name = gSystem->Getenv("DBS_RELEASE");
+ gSystem->MakeDirectory(release_name);
+ dir_name=release_name+"/"+TString(outdir);
+ gSystem->MakeDirectory(dir_name); 
 
-  //c1->SetLogy(1);
-  TH1F* h = (TH1F*) gDirectory->Get("DeltaPhi");
-  h->Rebin(2);
-  FormatHisto(h,spf);
-  h->SetStats(1111);
-  h->GetXaxis()->SetRangeUser(-0.3,0.3);
-  h->Draw();
-  SavePlot("deltaphi",dir_name);
-  c1->Clear();
-  c1->SetLogy(0);
+//  switch( etamode ) {
+//  case BARREL:
+//    dir1 = "DQMData/PFTask/Benchmarks/PFlowTaus_barrel/Gen";
+//    dir2 = "DQMData/PFTask/Benchmarks/CaloTaus_barrel/Gen";
+//    outdir = "Plots_Barrel";
+//    break;
+//  case ENDCAP:
+//    dir1 = "DQMData/PFTask/Benchmarks/PFlowTaus_endcap/Gen";
+//    dir2 = "DQMData/PFTask/Benchmarks/CaloTaus_endcap/Gen";
+//    outdir = "Plots_Endcap";
+//    break;
+//  default:
+//    break;
+//  }   
 
-  TH1F* h = (TH1F*) gDirectory->Get("EtGen");
-  //h->Rebin(2);
-  FormatHisto(h,sgen);
-  h->SetStats(1111);
-  h->GetXaxis()->SetRangeUser(0,120);
-  h->Draw();
-  SavePlot("etgen",dir_name);
-  c1->Clear();
+  Comparator comp(file1,
+		  dir1.c_str(),
+		  file2,
+		  dir2.c_str());
+  comp.SetStyles(style1, style2, "Particle Flow Electrons", "");
 
-  TH1F* h = (TH1F*) gDirectory->Get("EtaGen");
-  //h->Rebin(2);
-  FormatHisto(h,sgen);
-  h->SetStats(1111);
-  //h->GetXaxis()->SetRangeUser(-4,4);
-  h->Draw();
-  SavePlot("etagen",dir_name);
-  c1->Clear();
+
+  TCanvas c0("c0", "legend", 400, 200);
+  FormatPad( &c0, false ); 
+  comp.Legend().Draw();
+  SavePlot("legend", outdir.c_str() );
+    
+  comp.SetAxis(4, -30,30);
+  TCanvas c1a("c1a", "PF Electrons, pT");
+  FormatPad( &c1a, false ); 
+  comp.DrawSlice("DeltaEtvsEt", ptMin, ptMax, mode);
+  SavePlot("deltaEt", outdir.c_str() );
+
+
+  comp.SetAxis(2, -0.2,0.2);
+
+  TCanvas c2a("c2a", "Eta resolution");
+  FormatPad( &c2a, false,false,true );
+  comp.DrawSlice("DeltaEtavsEt", ptMin, ptMax, mode);
+  SavePlot("deltaEta", outdir.c_str() );
+
+  if(highpt) {
+    TCanvas c2b("c2b", "Eta resolution, high pT");
+    FormatPad( &c2b, false );
+    comp.DrawSlice("DeltaEtavsEt", 400, 600, mode);
+    SavePlot("deltaEta_highpT", outdir.c_str() );
+  }
+
+  TCanvas c3a("c3a", "Phi resolution");
+  FormatPad( &c3a, false,false,true );
+  comp.DrawSlice("DeltaPhivsEt", ptMin, ptMax, mode);
+  SavePlot("deltaPhi", outdir.c_str() );
+
+  if(highpt) {
+    TCanvas c3b("c3b", "Phi resolution, high pT");
+    FormatPad( &c3b, false );
+    comp.DrawSlice("DeltaPhivsEt", 400, 600, mode);
+    SavePlot("deltaPhi_highpT", outdir.c_str() );
+  }
+
+  comp.SetAxis(1);
+
+
+  bool efficiencies = true;
+
+  if (efficiencies ) {
+
+    TCanvas c4("c4", "Efficiency PF vs Pt low");
+    FormatPad( &c4, false );
+    unsigned rebin = 5;
+
+    Comparator comp2(file1,
+		     dir1.c_str(),
+		     file1,
+		     dir1.c_str() ); 
+    comp2.SetStyles(style1, style2,"PFElectrons", "");
+    comp2.SetAxis(rebin,0.,40.);
+    comp2.Draw("EtGen","EtSeen", Comparator::EFF);
+    SavePlot("efficiency_vs_pT_low", outdir.c_str() );
+
+    TCanvas c5("c5", "Efficiency PF vs Pt high");
+    FormatPad( &c5, false );
+
+    comp2.SetStyles(style1, style2,"PFElectrons","");
+    comp2.SetAxis(rebin,0,100);
+    comp2.Draw("EtGen","EtSeen", Comparator::EFF);
+    SavePlot("efficiency_vs_pT_high", outdir.c_str() );
+
+    TCanvas c8("c8", "Efficiency PF vs eta");
+    FormatPad( &c8, false );
+    rebin = 2;
+
+    comp2.SetAxis(rebin,-3,3);
+    comp2.Draw("EtaGen","EtaSeen", Comparator::EFF);
+    SavePlot("efficiency_vs_Eta", outdir.c_str() );
+
+    TCanvas c9("c9", "Efficiency PF vs phi");
+    FormatPad( &c9, false );
+    rebin = 4;
+
+    comp2.SetAxis(rebin,-TMath::Pi(),TMath::Pi());
+    comp2.Draw("PhiGen","PhiSeen", Comparator::EFF);
+    SavePlot("efficiency_vs_Phi", outdir.c_str() );
+  }
+
+  TCanvas c6("c6", "pT");
+  FormatPad( &c6, false );
+  comp.SetAxis(2,0,120);
+  comp.Draw("EtGen", mode);
+  SavePlot("etgen", outdir.c_str() );
+  comp.SetAxis(1);
+
   
-  TH2F* g = (TH2F*) gDirectory->Get("DeltaEtOverEtvsEt");
-  TH1D* p = g->ProjectionY();
-  p->Rebin(4);
-  FormatHisto(p,spf);
-  p->SetStats(1111);
-  //p->GetXaxis()->SetRangeUser(-1,1);
-  p->Draw();
-  SavePlot("etresolution",dir_name);
-  c1->Clear();
+  TCanvas c7("c7", "eta");
+  FormatPad( &c7, false );
+  comp.Draw("EtaGen", mode);
+  SavePlot("etagen", outdir.c_str() );
 
-  //   Efficiency plots
-  // Eta 
-  TH1F* EffEtaNum = (TH1F*) gDirectory->Get("EtaSeen")->Clone();
-  TH1F* EffEtaDenom = (TH1F*) gDirectory->Get("EtaGen")->Clone();
-  TH1F* EffEta = (TH1F*) gDirectory->Get("EtaSeen")->Clone();
-  EffEtaNum->Rebin(2);
-  EffEtaDenom->Rebin(2);
-  EffEta->Rebin(2);
-  EffEta->Clear();
-  //  FormatHisto(EffEta,sgen);
-  EffEta->SetMarkerStyle(23);
-  EffEta->SetMarkerColor(2);
-  EffEta->GetXaxis()->SetRangeUser(-3.,3.);
-  EffEta->GetXaxis()->SetTitle("#eta_{gen}");
-  EffEta->GetYaxis()->SetTitle("Efficiency");
-  gStyle->SetOptStat(0);
+  TCanvas c10("c10", "Delta Et/Et");
+  FormatPad( &c10, false );
+  comp.DrawSlice("DeltaEtOverEtvsEt", ptMin, ptMax, mode);
+  SavePlot("deltaEtoEt", outdir.c_str() );
 
-  EffEtaNum->Sumw2();
-  EffEtaDenom->Sumw2();
-  EffEta->Divide(EffEtaNum,EffEtaDenom,1.,1.,"B");
-  EffEta->SetMinimum(0.7);
-  EffEta->Draw("E");
-  SavePlot("etaeff",dir_name);
-  c1->Clear();
-
-  // Phi
-  TH1F* EffPhiNum = (TH1F*) gDirectory->Get("PhiSeen")->Clone();
-  TH1F* EffPhiDenom = (TH1F*) gDirectory->Get("PhiGen")->Clone();
-  TH1F* EffPhi = (TH1F*) gDirectory->Get("PhiSeen")->Clone();
-  EffPhiNum->Rebin(4);
-  EffPhiDenom->Rebin(4);
-  EffPhi->Rebin(4);
-  EffPhi->Clear();
-  //  FormatHisto(EffPhi,sgen);
-  EffPhi->SetMarkerStyle(23);
-  EffPhi->SetMarkerColor(2);
-  EffPhi->GetXaxis()->SetRangeUser(-TMath::Pi(),TMath::Pi());
-  EffPhi->GetXaxis()->SetTitle("#phi_{gen}");
-  EffPhi->GetYaxis()->SetTitle("Efficiency");
-  gStyle->SetOptStat(0);
-
-  EffPhiNum->Sumw2();
-  EffPhiDenom->Sumw2();
-  EffPhi->Divide(EffPhiNum,EffPhiDenom,1.,1.,"B");
-  EffPhi->SetMinimum(0.7);
-  EffPhi->Draw("E");
-  SavePlot("phieff",dir_name);
-  c1->Clear();
-
-  // Et - low
-  TH1F* EffEtNum = (TH1F*) gDirectory->Get("EtSeen")->Clone();
-  TH1F* EffEtDenom = (TH1F*) gDirectory->Get("EtGen")->Clone();
-  TH1F* EffEt = (TH1F*) gDirectory->Get("EtSeen")->Clone();
-  EffEtNum->Rebin(5);
-  EffEtDenom->Rebin(5);
-  EffEt->Rebin(5);
-  EffEt->Clear();
-  EffEtNum->Sumw2();
-  EffEtDenom->Sumw2();
-  //  FormatHisto(EffEt,sgen);
-  EffEt->SetMarkerStyle(23);
-  EffEt->SetMarkerColor(2);
-  EffEt->GetXaxis()->SetRangeUser(0,40.);
-  EffEt->GetXaxis()->SetTitle("p_{T}_{gen}");
-  EffEt->GetYaxis()->SetTitle("Efficiency");
-  gStyle->SetOptStat(0);
-
-  //  EffEtNum->Sumw2();
-  //  EffEtDenom->Sumw2();
-  EffEt->Divide(EffEtNum,EffEtDenom,1.,1.,"B");
-  EffEt->SetMinimum(0.7);
-  EffEt->Draw("E");
-  SavePlot("ptefflow",dir_name);
-  c1->Clear();
-
-  // Et - high
-  EffEtNum = (TH1F*) gDirectory->Get("EtSeen")->Clone();
-  EffEtDenom = (TH1F*) gDirectory->Get("EtGen")->Clone();
-  EffEt = (TH1F*) gDirectory->Get("EtSeen")->Clone();
-  EffEtNum->Rebin(2,"EffHighEtNum");
-  EffEtDenom->Rebin(2,"EffHighEtDenom");
-  EffEt->Rebin(2,"EffHighEt");
-
-  TH1F * EffHighEtNum=gDirectory->Get("EffHighEtNum");
-  TH1F * EffHighEtDenom=gDirectory->Get("EffHighEtDenom");
-  TH1F * EffHighEt=gDirectory->Get("EffHighEt");
-  EffHighEt->Clear();
-//  //  FormatHisto(EffEtHigh,sgen);
-  EffHighEt->SetMarkerStyle(23);
-  EffHighEt->SetMarkerColor(2);
-  EffHighEt->GetXaxis()->SetRangeUser(0,100.);
-  EffHighEt->GetXaxis()->SetTitle("p_{T}_{gen}");
-  EffHighEt->GetYaxis()->SetTitle("Efficiency");
-  gStyle->SetOptStat(0);
-
-
-  EffHighEtNum->Sumw2();
-  EffHighEtDenom->Sumw2();
-  EffHighEt->Divide(EffHighEtNum,EffHighEtDenom,1.,1.,"B");
-  EffHighEt->SetMinimum(0.7);
-  EffHighEt->Draw("E");
-  SavePlot("pteffhigh",dir_name);
-  c1->Clear();
-
-  cout << "ok " << endl;
   gROOT->ProcessLine(".q");
 }
-
-
