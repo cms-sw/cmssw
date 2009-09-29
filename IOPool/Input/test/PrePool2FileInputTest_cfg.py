6d63428df0095cc3f0d8803adde58c2202703b83
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-09-29 21:37:39 +0000

    added simple unit test for two file solution

---
+++ IOPool/Input/test/PrePool2FileInputTest_cfg.py
@@ -0,0 +1,35 @@
+# The following comments couldn't be translated into the new config version:
+
+# Test storing OtherThing as well
+# Configuration file for PrePoolInputTest 
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST2ND")
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+
+#process.maxEvents = cms.untracked.PSet(
+#    input = cms.untracked.int32(11)
+#)
+#process.Thing = cms.EDProducer("ThingProducer",
+#    debugLevel = cms.untracked.int32(1)
+#)
+
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('keep *', 
+        'drop *_Thing_*_*'),
+    fileName = cms.untracked.string('PoolInput2FileTest.root')
+)
+
+process.OtherThing = cms.EDProducer("OtherThingProducer",
+    debugLevel = cms.untracked.int32(1)
+)
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring("file:PoolInputOther.root") )
+
+
+process.p = cms.Path(process.OtherThing)
+process.ep = cms.EndPath(process.output)
+
+
