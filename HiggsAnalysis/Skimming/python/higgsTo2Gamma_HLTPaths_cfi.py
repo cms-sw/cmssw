49a5f7efa7da541731865c945f744e27b7881792
Marco Pieri <sha1-0c70a541ce30dda16c91989063058bef3a5a7ba0@cern.ch>
2009-09-30 16:55:41 +0000

    Changed TriggerResults to HLT8E29 for Oct Exercise

--- HiggsAnalysis/Skimming/python/higgsTo2Gamma_HLTPaths_cfi.py
+++ HiggsAnalysis/Skimming/python/higgsTo2Gamma_HLTPaths_cfi.py
@@ -3,7 +3,12 @@ import FWCore.ParameterSet.Config as cms
 import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 higgsTo2GammaHLTFilter = copy.deepcopy(hltHighLevel)
+
+#Added for oct ex
+higgsTo2GammaHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT8E29")
+
 #This is for 2_2_X
 #higgsTo2GammaHLTFilter.HLTPaths = ['HLT_IsoPhoton30_L1I', 'HLT_IsoPhoton40_L1R', 'HLT_DoubleIsoPhoton20_L1I', 'HLT_DoubleIsoPhoton20_L1R','HLT_Photon25_L1R','HLT_Photon15_L1R']
 #This is for 3_1_X
 higgsTo2GammaHLTFilter.HLTPaths = ['HLT_Photon15_L1R', 'HLT_DoublePhoton10_L1R', 'HLT_Photon20_L1R']
+
