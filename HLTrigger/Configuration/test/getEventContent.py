cc85c439d8e160f4830a8c1930b28bd94d14d91c
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-09-30 20:05:30 +0000

    add back hltOutputALCAPHISYMHCAL for 3.3.0

--- HLTrigger/Configuration/test/getEventContent.py
+++ HLTrigger/Configuration/test/getEventContent.py
@@ -23,7 +23,7 @@ def extractBlock(config, blocks, target):
 
 def extractBlocks(config):
   outputA    = ( 'hltOutputA', )
-  outputALCA = ( 'hltOutputALCAPHISYM', 'hltOutputALCAP0', 'hltOutputRPCMON' )
+  outputALCA = ( 'hltOutputALCAPHISYM', 'hltOutputALCAPHISYMHCAL', 'hltOutputALCAP0', 'hltOutputRPCMON' )
   outputMON  = ( 'hltOutputDQM', 'hltOutputHLTDQM', 'hltOutputHLTMON', 'hltOutput8E29', 'hltOutput1E31', 'hltOutputHIon' )
   extractBlock(config, outputA,    'hltOutputA_cff.py')
   extractBlock(config, outputALCA, 'hltOutputALCA_cff.py')
