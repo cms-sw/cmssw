ab727165fa2e86238d57d293b357b4792ea5c737
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-10-01 20:01:02 +0000

    added HLT_Mu5 and HLT_Mu9 triggers, changed pT binning

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationSequence_cff.py
@@ -3,19 +3,25 @@ from HLTriggerOffline.HeavyFlavor.heavyFlavorValidation_cfi import *
 hfv1 = heavyFlavorValidation.clone(
     TriggerProcessName = cms.untracked.string("HLT"),
     TriggerPathName = cms.untracked.string("HLT_Mu3"),
-    MuonPtBins = cms.untracked.vdouble(1., 2.5, 3.5, 4.5, 6., 8., 10., 20., 50.),
+    MuonPtBins = cms.untracked.vdouble(1., 3., 4., 5., 7., 10., 20., 50.),
     MuonEtaBins = cms.untracked.vdouble(24, -2.4, 2.4)
 )
 hfv2 = hfv1.clone(
     TriggerPathName = cms.untracked.string("HLT_IsoMu3")
 )
 hfv3 = hfv1.clone(
-    TriggerPathName = cms.untracked.string("HLT_DoubleMu0")
+    TriggerPathName = cms.untracked.string("HLT_Mu5")
 )
 hfv4 = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_Mu9")
+)
+hfv5 = hfv1.clone(
+    TriggerPathName = cms.untracked.string("HLT_DoubleMu0")
+)
+hfv6 = hfv1.clone(
     TriggerPathName = cms.untracked.string("HLT_DoubleMu3")
 )
 
 heavyFlavorValidationSequence = cms.Sequence(
-  hfv1+hfv2+hfv3+hfv4
+  hfv1+hfv2+hfv3+hfv4+hfv5+hfv6
 )
