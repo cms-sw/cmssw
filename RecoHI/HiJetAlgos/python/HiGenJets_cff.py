195c95659ae0830a89f553855e80ccd512196f77
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2009-10-01 21:42:42 +0000

    GenJets modified to be included in StandardSequence

--- RecoHI/HiJetAlgos/python/HiGenJets_cff.py
+++ RecoHI/HiJetAlgos/python/HiGenJets_cff.py
@@ -1,12 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoJets.JetProducers.ic5GenJets_cfi import iterativeCone5GenJets
-from RecoJets.Configuration.GenJetParticles_cff import *
 from RecoJets.JetProducers.GenJetParameters_cfi import *
 from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
-
-hiGenParticlesForJets = genParticlesForJets.clone()
-hiGenParticlesForJets.src = cms.InputTag("hiGenParticles")
 
 iterativeCone5HiGenJets = cms.EDProducer("SubEventGenJetProducer",
                                          GenJetParameters,
@@ -18,8 +13,4 @@ iterativeCone5HiGenJets = cms.EDProducer("SubEventGenJetProducer",
 
 iterativeCone5HiGenJets.src = cms.InputTag("hiGenParticlesForJets")
 
-signalJets = cms.Sequence(genJetParticles*iterativeCone5GenJets)
-
-subEventJets = cms.Sequence(hiGenParticlesForJets*iterativeCone5HiGenJets)
-
-allGenJets = cms.Sequence(signalJets+subEventJets)
+hiGenJets = cms.Sequence(hiGenParticlesForJets*iterativeCone5HiGenJets)
