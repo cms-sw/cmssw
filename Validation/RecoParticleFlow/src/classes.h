25f614b15c5aedb8ef14566b88371f730586f91b
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-10-01 17:17:52 +0000

    Add back the Comparator

--- Validation/RecoParticleFlow/src/classes.h
+++ Validation/RecoParticleFlow/src/classes.h
@@ -1,6 +1,6 @@
 #include "Validation/RecoParticleFlow/interface/TH2Analyzer.h"
 #include "Validation/RecoParticleFlow/interface/NicePlot.h"
-/* #include "Validation/RecoParticleFlow/interface/Comparator.h" */
+#include "Validation/RecoParticleFlow/interface/Comparator.h"
 
 namespace {
   namespace {
