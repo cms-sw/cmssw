c17b5a51b7944a4e07128f864d4d91063fd0e468
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-10-01 07:39:00 +0000

    Added code to fill variables from DataFormats/METReco/interface/SpecificGenData.h

--- RecoMET/METAlgorithms/src/GenSpecificAlgo.cc
+++ RecoMET/METAlgorithms/src/GenSpecificAlgo.cc
@@ -1,6 +1,6 @@
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "RecoMET/METAlgorithms/interface/GenSpecificAlgo.h"
-
+#include "TMath.h"
 using namespace reco;
 using namespace std;
 
@@ -11,15 +11,97 @@ using namespace std;
 // information will be added in the future. 
 //-------------------------------------
 //reco::GenMET GenSpecificAlgo::addInfo(const CandidateCollection *particles, CommonMETData met)
-reco::GenMET GenSpecificAlgo::addInfo(edm::Handle<edm::View<Candidate> > particles, CommonMETData met)
+reco::GenMET GenSpecificAlgo::addInfo(edm::Handle<edm::View<Candidate> > particles, CommonMETData met, bool onlyFiducial)
 { 
   // Instantiate the container to hold the calorimeter specific information
   SpecificGenMETData specific;
+
   // Initialise the container 
-  specific.m_EmEnergy        = 0.0;        // EM Energy
-  specific.m_HadEnergy       = 0.0;        // Hadronic Energy
-  specific.m_InvisibleEnergy = 0.0;        // Invisible energy
-  specific.m_AuxiliaryEnergy = 0.0;        // Other Energy
+  specific.NeutralEMEtFraction     = 0.0;
+  specific.NeutralHadEtFraction    = 0.0;
+  specific.ChargedEMEtFraction     = 0.0;
+  specific.ChargedHadEtFraction    = 0.0;
+  specific.MuonEtFraction          = 0.0;
+  specific.InvisibleEtFraction     = 0.0;
+
+  // tally Et contribution from undecayed genparticles that don't fall into the following classifications (for normalization purposes)
+  double Et_unclassified = 0.;
+  
+  for( edm::View<reco::Candidate>::const_iterator iParticle = (particles.product())->begin() ; iParticle != (particles.product())->end() ; iParticle++ )
+    {
+      //Fiducial requirement
+      if( onlyFiducial )
+	{
+	  if( TMath::Abs(iParticle->eta()) > 5.0  )
+	    {
+	      // Add back the Et that was subtracted in the calculation of MET from METAlgo.cc
+	      // The CMS detector won't see this Et
+	      met.mex += iParticle->et()*TMath::Cos( iParticle->phi() );
+	      met.mey += iParticle->et()*TMath::Sin( iParticle->phi() ); 
+	      continue;
+	    }
+	}
+
+      int pdgId = TMath::Abs( iParticle->pdgId() ) ;
+      
+      switch (pdgId) {
+      case 22 : // photon
+	specific.NeutralEMEtFraction += iParticle->et();
+	break;
+      case 11 : // e
+	specific.ChargedEMEtFraction += iParticle->et();
+	break;
+      case 130 : // K_long
+      case 310 : // K_short
+      case 3122: // Lambda
+      case 2112: // n
+      case 3222: // Neutral Cascade
+	specific.NeutralHadEtFraction += iParticle->et();
+	break;
+      case 211 : // pi
+      case 321 : // K+/K-
+      case 2212: // p
+      case 3312: // Cascade -
+      case 3112: // Sigma -
+      case 3322: // Sigma + 
+      case 3334: // Omega -	
+	specific.ChargedHadEtFraction += iParticle->et();
+	break;
+      case 13 : //muon
+	specific.MuonEtFraction += iParticle->et();
+	break;
+      case 12 : // e_nu
+      case 14 : // mu_nu
+      case 16 : // tau_nu
+      case 1000022 : // Neutral ~Chi_0 
+      case 1000012 :  // LH ~e_nu  
+      case 1000014 :  // LH ~mu_nu
+      case 1000016 :  // LH ~tau_nu
+      case 1000039 :  // ~G
+      case 4000012 :  // excited e_nu
+      case 39 :  //  G
+	specific.InvisibleEtFraction   += iParticle->et();
+	break;
+      default : 
+	Et_unclassified += iParticle->et();
+	cout << "PdgId : "<< iParticle->pdgId() << "    " << iParticle->status() << "  does not fall into a category " << endl;
+      }
+    }
+
+  double Et_Total = specific.NeutralEMEtFraction + specific.NeutralHadEtFraction + specific.ChargedEMEtFraction + 
+    specific.ChargedHadEtFraction + specific.MuonEtFraction + specific.InvisibleEtFraction + Et_unclassified;
+  
+  //Normalize
+  if( Et_Total ) 
+    {
+      specific.NeutralEMEtFraction /= Et_Total;
+      specific.NeutralHadEtFraction /= Et_Total;
+      specific.ChargedEMEtFraction /= Et_Total;
+      specific.ChargedHadEtFraction /= Et_Total;
+      specific.MuonEtFraction /= Et_Total;
+      specific.InvisibleEtFraction /= Et_Total;
+    }
+
   // Instantiate containers for the MET candidate and initialise them with
   // the MET information in "met" (of type CommonMETData)
   const LorentzVector p4( met.mex, met.mey, 0.0, met.met );
