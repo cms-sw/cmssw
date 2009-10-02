bc93a253a74ba6e52f7c3736ee80e236c7bd7e52
kalavase <sha1-37b724a660ad78b379b1c02b36b874291d39b117@cern.ch>
2009-10-02 08:49:13 +0000

    Updated sequence

--- TopQuarkAnalysis/TopSkimming/python/topMuSkimFilterOctoberX_cff.py
+++ TopQuarkAnalysis/TopSkimming/python/topMuSkimFilterOctoberX_cff.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 from TopQuarkAnalysis.TopSkimming.topMuSkimFilterOctoberX_cfi import *
 
-#Define group sequence, using HLT/Reco quality cut. 
+#Define group sequence, using HLT/Reco quality cut.
+#Removing it from the sequence for now
 topMuHLTSeq = cms.Sequence(
 	topMuHLT+topMuHLTPtFilter
-#    	topMuHLTPtFilter
-#        topMuHLT
 )
