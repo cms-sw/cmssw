0f1d58db46e5a7a9f9a0150806ce76fb05a909a7
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-10-02 19:42:38 +0000

    Avoid unnecessary vector copy in getValidDetIds()

--- SimCalorimetry/CastorSim/plugins/CastorDigiProducer.cc
+++ SimCalorimetry/CastorSim/plugins/CastorDigiProducer.cc
@@ -146,7 +146,7 @@ edm::ESHandle<CaloGeometry> geometry;
 eventSetup.get<CaloGeometryRecord>().get(geometry);
 theCastorResponse->setGeometry(&*geometry);
 
-vector<DetId> castorCells = geometry->getValidDetIds(DetId::Calo, HcalCastorDetId::SubdetectorId);
+const vector<DetId>& castorCells = geometry->getValidDetIds(DetId::Calo, HcalCastorDetId::SubdetectorId);
 
 std::cout<<"CastorDigiProducer::CheckGeometry number of cells: "<<castorCells.size()<<std::endl;
 theCastorDigitizer->setDetIds(castorCells);
