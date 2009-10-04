9a145a9cf5eae0b3d8be778d6da8fdec5c0c2deb
Andres Felipe Osorio Oliveros <sha1-af3634eb466cada2f9cfebb1ae2813131fcbd258@cern.ch>
2009-10-04 19:14:43 +0000

    bug fixed: hardware-pseudoconfig* files had the wrong label for wheels causing trouble to run bit 25 added rpcTechnicalTriggerData_cfg.py as an example on running on data updated README file with explanation on what's going on in the hardwareconfig files

---
+++ L1Trigger/RPCTechnicalTrigger/test/rpcTechnicalTriggerData_cfg.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+import os
+
+maxevts   = 50
+globaltag = 'GR09_31X_V4P::All'
+inputfile = '/store/data/CRAFT09/Cosmics/RECO/v1/000/109/131/B68E147D-C27C-DE11-B561-0019B9F72CC2.root'
+
+process   = cms.Process("RPCTechnicalTrigger")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.categories = ['*']
+process.MessageLogger.destinations = ['cout']
+process.MessageLogger.cout = cms.untracked.PSet(
+    	threshold = cms.untracked.string('DEBUG'),
+	INFO = cms.untracked.PSet(
+        limit = cms.untracked.int32(-1) ) )
+
+#.. Geometry and Global Tags
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string( globaltag )
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(maxevts) )
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring( inputfile ) )
+
+process.load("L1Trigger.RPCTechnicalTrigger.rpcTechnicalTrigger_cfi")
+
+#.. use the provided hardware configuration parameters
+process.rpcTechnicalTrigger.UseDatabase   = cms.untracked.int32(0)
+process.rpcTechnicalTrigger.ConfigFile    = cms.untracked.string("hardware-pseudoconfig.txt")
+process.rpcTechnicalTrigger.RPCDigiLabel  = cms.InputTag("muonRPCDigis")
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               fileName = cms.untracked.string('rpcttbits.root'),
+                               outputCommands = cms.untracked.vstring('drop *','keep L1GtTechnicalTriggerRecord_*_*_*') )
+
+process.p = cms.Path(process.rpcTechnicalTrigger)
+
+process.e = cms.EndPath(process.out)
+
