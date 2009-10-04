974c17d6a824aa8528bc2f6a41fceef93f130bd1
Andres Felipe Osorio Oliveros <sha1-af3634eb466cada2f9cfebb1ae2813131fcbd258@cern.ch>
2009-10-04 19:17:44 +0000

    added a couple of more comments for debugging

--- L1Trigger/RPCTechnicalTrigger/src/TTUWedgeORLogic.cc
+++ L1Trigger/RPCTechnicalTrigger/src/TTUWedgeORLogic.cc
@@ -1,4 +1,4 @@
-// $Id: TTUWedgeORLogic.cc,v 1.2 2009/08/19 15:04:02 aosorio Exp $
+// $Id: TTUWedgeORLogic.cc,v 1.3 2009/09/15 14:42:10 aosorio Exp $
 // Include files 
 
 
@@ -47,12 +47,14 @@ void TTUWedgeORLogic::setBoardSpecs( const TTUBoardSpecs::TTUBoardConfig & board
   if ( (boardspecs.m_MaxNumWheels > 1) && (boardspecs.m_Wheel2Id != 0) )
     m_wheelMajority[ boardspecs.m_Wheel2Id ] = 3;
 
-  if ( m_debug ) std::cout << "TTUWedgeORLogic::setBoardSpecs> " 
+  if ( m_debug ) std::cout << "TTUWedgeORLogic::setBoardSpecs> intialization: " 
                            << m_wheelMajority.size() << '\t'
                            << boardspecs.m_MaxNumWheels << '\t'
                            << boardspecs.m_Wheel1Id << '\t'
-                           << boardspecs.m_Wheel2Id <<  '\n';
-  
+                           << boardspecs.m_Wheel2Id << '\t'
+                           << m_wheelMajority[ boardspecs.m_Wheel1Id ] << '\t'
+                           << m_wheelMajority[ boardspecs.m_Wheel2Id ] << '\n';
+    
 }
 
 bool TTUWedgeORLogic::process( const TTUInput & inmap )
@@ -77,8 +79,8 @@ bool TTUWedgeORLogic::process( const TTUInput & inmap )
 
   int majority = m_wheelMajority[ inmap.m_wheelId ];
 
-  if ( m_debug ) std::cout << "TTUWedgeORLogic::setBoardSpecs> majority: " 
-                           << inmap.m_wheelId << '\t' << majority << '\n';
+  if ( m_debug ) std::cout << "TTUWedgeORLogic::setBoardSpecs> configuration W: " 
+                           << inmap.m_wheelId << '\t' << "M: " << majority << '\n';
   
   if ( nhits >= majority) m_triggersignal = true;
   
@@ -86,7 +88,8 @@ bool TTUWedgeORLogic::process( const TTUInput & inmap )
     std::cout << "TTUWedgeORLogic wedge decision:" 
               << m_option << '\t' 
               << firstsector << '\t' 
-              << m_triggersignal << std::endl;
+              << "nhits: " << nhits << '\t'
+              << "Dec: "   << m_triggersignal << std::endl;
   
   if( m_debug ) std::cout << "TTUWedgeORLogic>process ends" << std::endl;
   
