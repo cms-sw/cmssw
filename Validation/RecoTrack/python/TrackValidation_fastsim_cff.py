906820c73294f789ba22f93a33fc05d58acc9a4d
Chiara Genta <sha1-5a1ddac03dd3e96834db2f498eca6aad5396cb8e@cern.ch>
2009-10-05 16:40:11 +0000

    fix minpt and maxpt also for fast simulation

--- Validation/RecoTrack/python/TrackValidation_fastsim_cff.py
+++ Validation/RecoTrack/python/TrackValidation_fastsim_cff.py
@@ -8,9 +8,6 @@ TrackAssociatorByHitsRecoDenom.ROUList = ['famosSimHitsTrackerHits']
 trackValidator.UseAssociators = True
 trackValidator.skipHistoFit=cms.untracked.bool(True)
 trackValidator.useLogPt=cms.untracked.bool(True)
-trackValidator.minpT = cms.double(-1)
-trackValidator.maxpT = cms.double(3)
-trackValidator.nintpT = cms.int32(40)
 ###must be commented in normal running
 ###multiTrackValidator.outputFile='validationPlots.root'
 
