637d2f11b24ede9235c7151632e0ad2ebda25d30
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-10-05 07:46:30 +0000

    fixed labels

--- JetMETCorrections/JetPlusTrack/python/NtuplizeJetCollections_cff.py
+++ JetMETCorrections/JetPlusTrack/python/NtuplizeJetCollections_cff.py
@@ -4,6 +4,10 @@ import FWCore.ParameterSet.Config as cms
 InputTagDistributorService = cms.Service("InputTagDistributorService")
 VariableHelperService = cms.Service("VariableHelperService")
 UpdaterService = cms.Service("UpdaterService")
+TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string('ntuple.root'),
+    )
 
 # Common variables to ntuplizer
 kine = cms.PSet(
@@ -26,150 +30,137 @@ ntuplizeJetCollections = cms.EDFilter(
     "NTuplingDevice",
     Ntupler = cms.PSet(
     ComponentName = cms.string('StringBasedNTupler'),
-    useTFileService = cms.bool(False), 
+    useTFileService = cms.bool(True), 
     branchesPSet = cms.PSet(
     treeName = cms.string('event'),
 
     # reco::GenJets
     RecoGenJet = cms.PSet(
     Class = cms.string('reco::GenJet'),
-    src = cms.InputTag('sort:cleanLayer1Jets'),
+    src = cms.InputTag('sortByGenJetPt:allLayer1Jets'),
     leaves = cms.PSet(kine),
     ),
 
     # Uncorrected reco::CaloJets (IC5)
     RecoCaloJetIC5 = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:iterativeCone5CaloJets'),
+    src = cms.InputTag('sortByGenJetPt:iterativeCone5CaloJets'),
     leaves = cms.PSet(kine),
     ),
 
     # ZSP-corrected reco::CaloJets
     RecoCaloJetZSP = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:ZSPJetCorJetIcone5'),
+    src = cms.InputTag('sortByGenJetPt:ZSPJetCorJetIcone5'),
     leaves = cms.PSet(kine),
     ),
 
-    # JPT-corrected reco::CaloJets (old JPT)
-    RecoCaloJetJPT = cms.PSet(
-    Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JetPlusTrackZSPCorJetIcone5'),
-    leaves = cms.PSet(kine),
-    ),
-
-    # JPT-corrected reco::CaloJets (new JPT, default corrections)
+    # JPT-corrected reco::CaloJets (default corrections)
     RecoCaloJetJPTDefault = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorDefault'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloDefault'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (no corrections)
     RecoCaloJetJPTNone = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorNone'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloNone'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (in-cone only)
     RecoCaloJetJPTInCone = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorInCone'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloInCone'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ out-of-cone)
     RecoCaloJetJPTOutOfCone = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorOutOfCone'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloOutOfCone'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ out-of-vertex)
     RecoCaloJetJPTOutOfVertex = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorOutOfVertex'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloOutOfVertex'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ pion efficiency)
     RecoCaloJetJPTPionEff = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorPionEff'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloPionEff'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ muons)
     RecoCaloJetJPTMuons = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorMuons'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloMuons'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ electrons)
     RecoCaloJetJPTElectrons = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorElectrons'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloElectrons'),
     leaves = cms.PSet(kine),
     ),
 
     # JPT-corrected reco::CaloJets (+ vectorial using tracks only)
     RecoCaloJetJPTVecTracks = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorVecTracks'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloVecTracks'),
     leaves = cms.PSet(kine),
     ),
     
     # JPT-corrected reco::CaloJets (+ vectorial using tracks and response)
     RecoCaloJetJPTVecResponse = cms.PSet(
     Class = cms.string('reco::CaloJet'),
-    src = cms.InputTag('sort:JPTCorrectorVecResponse'),
+    src = cms.InputTag('sortByGenJetPt:JPTCorJetIC5CaloVecResponse'),
     leaves = cms.PSet(kine),
     ),
 
     # Uncorrected pat::Jets
     PatJetIC5 = cms.PSet(
     Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:RawPATJets'),
+    src = cms.InputTag('sortByGenJetPt:uncorrectedLayer1JetsIC5'),
     leaves = cms.PSet(kine),
     ),
 
     # ZSP-corrected pat::Jets
     PatJetZSP = cms.PSet(
     Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:PATZSPJetCorJetIcone5'),
+    src = cms.InputTag('sortByGenJetPt:PatZSPCorJetIC5Calo'),
     leaves = cms.PSet(kine),
     ),
 
-    # JPT-corrected pat::Jets (old JPT)
-    PatJetJPTOld = cms.PSet(
-    Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:PATJetPlusTrackZSPCorJetIcone5'),
-    leaves = cms.PSet(kine),
-    ),
-
-    # JPT-corrected pat::Jets (new JPT)
+    # JPT-corrected pat::Jets
     PatJetJPT = cms.PSet(
     Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:PATJPTCorrectionIC5'),
+    src = cms.InputTag('sortByGenJetPt:PatJPTCorJetIC5Calo'),
     leaves = cms.PSet(kine),
     ),
-
+    
     # JPT-corrected pat::Jets (JTA on-the-fly)
-    PatJetJTA = cms.PSet(
-    Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:JTAJPTCorrectionIC5'),
-    leaves = cms.PSet(kine),
-    ),
+    #PatJetJTA = cms.PSet(
+    #Class = cms.string('pat::Jet'),
+    #src = cms.InputTag('sortByGenJetPt:JTAJPTCorrectionIC5Calo'),
+    #leaves = cms.PSet(kine),
+    #),
     
     # MC-corrected pat::Jets
     PatJetMC = cms.PSet(
     Class = cms.string('pat::Jet'),
-    src = cms.InputTag('sort:cleanLayer1Jets'),
+    src = cms.InputTag('sortByGenJetPt:allLayer1Jets'),
     leaves = cms.PSet(kine),
     ),
 
     ),
     ),
     )
+
