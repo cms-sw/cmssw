296504e48f5fb12466a6b7ea9b33c76f9f5fade3
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-06 17:43:11 +0000

    moved

--- CondTools/RunInfo/src/HLTScalerReaderFactory.cc
+++
@@ -1,2 +0,0 @@
-#include "CondTools/RunInfo/interface/HLTScalerReaderFactory.h"
-EDM_REGISTER_PLUGINFACTORY(lumi::HLTScalerReaderFactory,"HLTScalerReaderPlugin");
--- CondTools/RunInfo/src/LumiReaderFactory.cc
+++
@@ -1,2 +0,0 @@
-#include "CondTools/RunInfo/interface/LumiReaderFactory.h"
-EDM_REGISTER_PLUGINFACTORY(lumi::LumiReaderFactory,"LumiReaderPlugin");
