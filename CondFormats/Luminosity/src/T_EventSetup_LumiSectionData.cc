3c48339401581705b123efe4412b850985c7f6bc
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-06 18:37:10 +0000

    added

---
+++ CondFormats/Luminosity/src/T_EventSetup_LumiSectionData.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/Luminosity/interface/LumiSectionData.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(lumi::LumiSectionData);
