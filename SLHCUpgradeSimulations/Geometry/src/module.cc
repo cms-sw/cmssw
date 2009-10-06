6a8308c854f111a3d60e973f5b285c654aeea165
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-10-06 18:53:53 +0000

    Define a new module to print out material information from full geometry to use to define the fastsim interaction geometry

--- SLHCUpgradeSimulations/Geometry/src/module.cc
+++ SLHCUpgradeSimulations/Geometry/src/module.cc
@@ -1,14 +1,22 @@
 // define the geometry plugin modules for new layers
 
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinear.h"
+#include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinearGap.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLayerAlgo.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackTrigLayerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdInnerDisks.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdOuterDisks.h"
+
+#include "SLHCUpgradeSimulations/Geometry/interface/PrintGeomMatInfo.h"
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinear,      "track:DDPixBarStackLinear");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinearGap,   "track:DDPixBarStackLinearGap");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLayerAlgo,   "track:DDPixBarStackLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackTrigLayerAlgo, "track:DDPixBarStackTrigLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdInnerDisks,      "track:DDPixFwdInnerDisks");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdOuterDisks,      "track:DDPixFwdOuterDisks");
+DEFINE_SEAL_MODULE ();
+DEFINE_SIMWATCHER(PrintGeomMatInfo);
