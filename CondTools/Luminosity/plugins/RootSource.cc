9b85b898be675c97428acee703083ee1446fb146
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-06 13:39:47 +0000

    list all trees

--- CondTools/Luminosity/plugins/RootSource.cc
+++ CondTools/Luminosity/plugins/RootSource.cc
@@ -10,11 +10,54 @@ lumi::RootSource::RootSource(const edm::ParameterSet& pset):LumiRetrieverBase(ps
   m_filename=pset.getParameter<std::string>("lumiFileName");
   m_source=TFile::Open(m_filename.c_str(),"READ");
   std::cout<<"got file "<<m_source<<std::endl;
-  std::cout<<"file size "<<m_source->GetBytesRead()<<std::endl;
   m_source->ls();
-  TTree *t = (TTree*)m_source->Get("RunSummary");
-  t->Print();
+  m_source->GetListOfKeys()->Print();
 
+  TTree *runtree = (TTree*)m_source->Get("RunSummary");
+  if(runtree){
+    runtree->Print();
+    std::cout<<"tot size bytes "<<runtree->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<runtree->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<runtree->GetEntries()<<std::endl;
+  }
+
+  TTree *rcmsconfig = (TTree*)m_source->Get("RCMSConfig");
+  if(rcmsconfig){
+    rcmsconfig->Print();
+    std::cout<<"tot size bytes "<<rcmsconfig->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<rcmsconfig->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<rcmsconfig->GetEntries()<<std::endl;
+  }
+  TTree *hlxdata = (TTree*)m_source->Get("HLXData");
+  if(hlxdata){
+    hlxdata->Print();
+    std::cout<<"tot size bytes "<<hlxdata->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<hlxdata->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<hlxdata->GetEntries()<<std::endl;
+  }
+
+  TTree *l1trg = (TTree*)m_source->Get("L1Trigger");
+  if(l1trg){
+    l1trg->Print();
+    std::cout<<"tot size bytes "<<l1trg->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<l1trg->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<l1trg->GetEntries()<<std::endl;
+  }
+
+  TTree *hlttrg = (TTree*)m_source->Get("HLTTrigger");
+  if(hlttrg){
+    hlttrg->Print();
+    std::cout<<"tot size bytes "<<hlttrg->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<hlttrg->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<hlttrg->GetEntries()<<std::endl;
+  }
+  TTree *vdm = (TTree*)m_source->Get("VdMScan");
+  if(vdm){
+    vdm->Print();
+    std::cout<<"tot size bytes "<<vdm->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<vdm->GetNbranches()<<std::endl;
+    std::cout<<"tot entries "<<vdm->GetEntries()<<std::endl;
+  }
 }
 void
 lumi::RootSource::fill(std::vector< std::pair<lumi::LumiSectionData*,cond::Time_t> >& result){
