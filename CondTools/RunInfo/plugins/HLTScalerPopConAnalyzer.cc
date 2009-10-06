ffd0d71f2bfbe07c671a0ffb94ff3e1ad0020155
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-06 17:55:05 +0000

    moved out lumi

--- CondTools/RunInfo/plugins/HLTScalerPopConAnalyzer.cc
+++
@@ -1,6 +0,0 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
-#include "CondTools/RunInfo/interface/HLTScalerHandler.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-typedef popcon::PopConAnalyzer<lumi::HLTScalerHandler> HLTScalerPopConAnalyzer;
-DEFINE_FWK_MODULE(HLTScalerPopConAnalyzer);
--- CondTools/RunInfo/plugins/LuminosityPopConAnalyzer.cc
+++
@@ -1,7 +0,0 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
-#include "CondTools/RunInfo/interface/LuminosityInfoHandler.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-typedef popcon::PopConAnalyzer<lumi::LuminosityInfoHandler> LuminosityPopConAnalyzer;
-
-DEFINE_FWK_MODULE(LuminosityPopConAnalyzer);
