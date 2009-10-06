49fcc187cffc69e8665c9fd94085ab28ad7cb7be
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-10-06 14:24:38 +0000

    moved code for the october exercise from the MAIN branch (for CMSSW_3_3_X and beyond) to B31X

--- TopQuarkAnalysis/TopSkimming/python/topMuSkimFilterOctoberX_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from TopQuarkAnalysis.TopSkimming.topMuSkimFilterOctoberX_cfi import *
-
-#Define group sequence, using HLT/Reco quality cut.
-#Removing it from the sequence for now
-topMuHLTSeq = cms.Sequence(
-	topMuHLT+topMuHLTPtFilter
-)
