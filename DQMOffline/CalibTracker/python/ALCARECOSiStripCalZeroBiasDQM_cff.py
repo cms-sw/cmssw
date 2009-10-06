96cab4905764467f050c9f68e51787da78c86071
kaussen <sha1-b58974427aa4d349705fe1a36f0e42a0fed605a3@cern.ch>
2009-10-06 16:08:27 +0000

    Moved the definition of the filter sequences to DPGAnalysis/SiStripTools/python/FilterSequenceForAlCaRecoDQM_cfi.py

--- DQMOffline/CalibTracker/python/ALCARECOSiStripCalZeroBiasDQM_cff.py
+++ DQMOffline/CalibTracker/python/ALCARECOSiStripCalZeroBiasDQM_cff.py
@@ -7,32 +7,15 @@ import FWCore.ParameterSet.Config as cms
 from DQM.SiStripMonitorCluster.SiStripMonitorClusterAlca_cfi import *
 SiStripCalZeroBiasMonitorCluster.TopFolderName = cms.string('AlCaReco/SiStrip')
 
-#---------------------------------------------
-# Filters to remove APV induced noisy events #
-#---------------------------------------------
+#--------------------------------------------------------------------------
+# Filters to remove APV induced noisy events and high multiplicity events #
+#--------------------------------------------------------------------------
 
-from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_AlCaReco_cfi import *
-
-from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_AlCaReco_cfi import *
-
-from DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_additionalpeak_AlCaReco_cfi import *
-
-#--------------------------------------------
-# Filter to remove high multiplicity events #
-#--------------------------------------------
-
-from DPGAnalysis.SiStripTools.largesistripclusterevents_AlCaReco_cfi import *
+from DPGAnalysis.SiStripTools.FilterSequenceForAlCaRecoDQM_cfi import *
 
 #------------
-# Sequences #
+# Sequence #
 #------------
 
-seqAPVCycleFilter = cms.Sequence(~PotentialTIBTECHugeEvents*
-                                 ~PotentialTIBTECFrameHeaderEventsFPeak*
-                                 ~PotentialTIBTECFrameHeaderEventsAdditionalPeak)
-
-seqMultiplicityFilter = cms.Sequence(~LargeSiStripClusterEvents)
-
-ALCARECOSiStripCalZeroBiasDQM = cms.Sequence(seqAPVCycleFilter*
-                                             seqMultiplicityFilter*
+ALCARECOSiStripCalZeroBiasDQM = cms.Sequence(seqFilters*
                                              SiStripCalZeroBiasMonitorCluster)
