057cc5f8b40642881dd22bb81373fe0621469f3a
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-06 18:12:32 +0000

    move lumi out

--- CondCore/RunInfoPlugins/plugins/classes_cids.cc
+++ CondCore/RunInfoPlugins/plugins/classes_cids.cc
@@ -4,6 +4,4 @@
 CLASS_ID(RunInfo);
 CLASS_ID(RunNumber);
 CLASS_ID(RunSummary);
-CLASS_ID(HLTScaler);
-CLASS_ID(LuminosityInfo);
 CLASS_ID(L1TriggerScaler);
--- CondCore/RunInfoPlugins/src/plugin.cc
+++ CondCore/RunInfoPlugins/src/plugin.cc
@@ -5,14 +5,7 @@
 #include "CondFormats/DataRecord/interface/L1TriggerScalerRcd.h"
 #include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
 
-#include "CondFormats/RunInfo/interface/LuminosityInfo.h"
-#include "CondFormats/DataRecord/interface/LuminosityInfoRcd.h"
-#include "CondFormats/RunInfo/interface/HLTScaler.h"
-#include "CondFormats/DataRecord/interface/HLTScalerRcd.h"
-
 DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(RunSummaryRcd,RunSummary);
 REGISTER_PLUGIN(RunInfoRcd,RunInfo);
 REGISTER_PLUGIN(L1TriggerScalerRcd, L1TriggerScaler);
-REGISTER_PLUGIN(LuminosityInfoRcd, lumi::LuminosityInfo);
-REGISTER_PLUGIN(HLTScalerRcd, lumi::HLTScaler);
