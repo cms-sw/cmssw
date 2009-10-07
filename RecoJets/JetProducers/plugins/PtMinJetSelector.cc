625ba169640f565670d95f2126f5a995fa27c9d6
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2009-10-07 08:38:21 +0000

    add missing file to register min pt jet selectors

---
+++ RecoJets/JetProducers/plugins/PtMinJetSelector.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "PtMinJetSelector.h"
+
+DEFINE_ANOTHER_FWK_MODULE(PtMinCaloJetSelector);
+DEFINE_ANOTHER_FWK_MODULE(PtMinGenJetSelector);
+DEFINE_ANOTHER_FWK_MODULE(PtMinPFJetSelector);
+DEFINE_ANOTHER_FWK_MODULE(PtMinBasicJetSelector);
