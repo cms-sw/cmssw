cd6851107ccc4bb12c39f565387574662eb639fa
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2009-10-07 08:51:57 +0000

    *** empty log message ***

--- DQM/RPCMonitorDigi/python/RPCEfficiency_cfi.py
+++ DQM/RPCMonitorDigi/python/RPCEfficiency_cfi.py
@@ -1,13 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 
-rpcEfficiency = cms.EDFilter("RPCEfficiencySecond",
-    SaveFile = cms.untracked.bool(False),
-    NameFile = cms.untracked.string('/tmp/carrillo/RPCEfficiency.root'),
+rpcEfficiency = cms.EDFilter("RPCEfficiency",
+    incldt = cms.untracked.bool(True),
+    incldtMB4 = cms.untracked.bool(True),
+    inclcsc = cms.untracked.bool(True),
+
     debug = cms.untracked.bool(False),
-    barrel = cms.untracked.bool(True),
-    endcap = cms.untracked.bool(True)
+    inves = cms.untracked.bool(True),
+
+    DuplicationCorrection = cms.untracked.int32(1),
+
+    rangestrips = cms.untracked.double(1.),
+    rangestripsRB4 = cms.untracked.double(4.),
+    MinCosAng = cms.untracked.double(0.99),
+    MaxD = cms.untracked.double(80.0),
+    MaxDrb4 = cms.untracked.double(150.0),
+
+    muonRPCDigis = cms.untracked.string('muonRPCDigis'),
+    cscSegments = cms.untracked.string('cscSegments'),
+    dt4DSegments = cms.untracked.string('dt4DSegments'),
+
+    EffSaveRootFile = cms.untracked.bool(False),
+    EffRootFileName = cms.untracked.string('/tmp/carrillo/RPCEfficiencyFIRST.root'),
+    EffSaveRootFileEventsInterval = cms.untracked.int32(100)
 )
 
-rpcefficiency = cms.Sequence(rpcEfficiency)
+rpcSource = cms.Sequence(rpcEfficiency)
 
 
