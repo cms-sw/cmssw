9149aeeee7524f2fc70e2e861c6b0f6197a73862
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2009-10-07 10:12:36 +0000

    read lumi raw data ok

---
+++ CondTools/Luminosity/interface/LumiStructures.hh
@@ -0,0 +1,243 @@
+#ifndef LUMISTRUCTURES_HH
+#define LUMISTRUCTURES_HH
+
+/*
+  LumiNibble structure definitions
+*/
+
+// The string and stream definitions
+// Type definitions used by the HAL, etc...
+
+#define HCAL_HLX_MAX_BUNCHES 4096
+#define HCAL_HLX_MAX_HLXS 36
+
+//#define HCAL_HLX_NUM_BUNCHES 3564
+//#define HCAL_HLX_NUM_HLXS 36
+
+// Changes
+// Namespace for the HCAL HLX
+#include <stdint.h>
+
+namespace HCAL_HLX{
+
+  struct VDM_SCAN_DATA {
+    uint32_t runNumber;
+    uint32_t timestamp;
+    uint32_t timestamp_micros;
+
+    int MessageQuality;     
+    bool VdmMode; //True when a scan at one of the IPs is imminent, false otherwise
+    int IP;
+    bool RecordDataFlag; // True while data for one of the scan points at one of the IPs is being taken, false otherwise   
+    float BeamSeparation; //separation in sigma for the scan point  
+    bool IsXaxis; //true if scanning xaxis, otherwise yaxis is being scanned 
+  };
+
+  struct RCMS_CONFIG {
+    
+    uint32_t runNumber;
+
+    bool UseConfigDB;
+    char CfgDBTag[32];
+
+    char CMSSW_Tag[32];
+    char TriDAS_Tag[32];
+
+    uint16_t FirmwareVersion;
+    
+    uint16_t OCCMaskTop;
+    uint16_t OCCMaskBottom;
+
+    uint16_t LHCMaskLowBottom;
+    uint16_t LHCMaskLowTop;
+
+    uint16_t LHCMaskHighBottom;
+    uint16_t LHCMaskHighTop;
+
+    uint16_t SumETMaskLowBottom;
+    uint16_t SumETMaskLowTop;
+    uint16_t SumETMaskHighBottom;
+    uint16_t SumETMaskHighTop;
+
+    uint16_t OccThresholdLowBottom;
+    uint16_t OccThresholdLowTop;
+    uint16_t OccThresholdHighBottom;
+    uint16_t OccThresholdHighTop;
+
+    uint16_t LHCThresholdBottom;
+    uint16_t LHCThresholdTop;
+    uint16_t ETSumCutoffBottom;
+    uint16_t ETSumCutoffTop;
+  };
+
+
+  struct LUMI_SUMMARY {
+    float DeadtimeNormalization; 
+    float LHCNormalization; // recieved from LHC 
+
+    float InstantLumi;
+    float InstantLumiErr;
+    int16_t InstantLumiQlty;
+
+    float InstantETLumi;
+    float InstantETLumiErr;
+    int16_t InstantETLumiQlty;
+    float ETNormalization;  // Calculated
+
+    float InstantOccLumi[2];
+    float InstantOccLumiErr[2];
+    int16_t InstantOccLumiQlty[2];
+    float OccNormalization[2];
+
+    float lumiNoise[2];
+  };
+
+  struct LUMI_DETAIL {
+    float LHCLumi[HCAL_HLX_MAX_BUNCHES]; // Sum of LHC.data over all HLX's
+
+    float ETLumi[HCAL_HLX_MAX_BUNCHES];
+    float ETLumiErr[HCAL_HLX_MAX_BUNCHES];
+    int16_t ETLumiQlty[HCAL_HLX_MAX_BUNCHES];
+    float ETBXNormalization[HCAL_HLX_MAX_BUNCHES];
+
+    float OccLumi[2][HCAL_HLX_MAX_BUNCHES];
+    float OccLumiErr[2][HCAL_HLX_MAX_BUNCHES];
+    int16_t OccLumiQlty[2][HCAL_HLX_MAX_BUNCHES];
+    float OccBXNormalization[2][HCAL_HLX_MAX_BUNCHES];
+  };
+
+  /**************** Trigger *********************/
+
+
+  struct LEVEL1_PATH {
+
+    char pathName[128];
+    uint64_t counts;
+    uint64_t prescale;
+  };
+
+  struct LEVEL1_TRIGGER {
+    uint32_t runNumber;
+    uint32_t sectionNumber;
+
+    char GTLumiInfoFormat[32];
+   
+    LEVEL1_PATH GTAlgo[128];
+    LEVEL1_PATH GTTech[64];
+
+    uint32_t GTPartition0TriggerCounts[10];
+    uint32_t GTPartition0DeadTime[10];
+  };
+
+  struct HLTPath {  // only object that uses STL and is variable size.
+    char PathName[128]; //This is the name of trigger path
+    uint32_t L1Pass;      //Number of times the path was entered
+    uint32_t PSPass;      //Number after prescaling
+    uint32_t PAccept;     //Number of accepts by the trigger path
+    uint32_t PExcept;     //Number of exceptional event encountered
+    uint32_t PReject;     
+    char PrescalerModule[64]; //Name of the prescale module in the path
+    uint32_t PSIndex;     //Index into the set of pre defined prescales
+  };
+
+  struct HLTrigger {
+    uint32_t runNumber;
+    uint32_t sectionNumber;
+
+    char HLTConfigKey[32];
+
+    HLTPath HLTPaths[256];
+  };
+
+  /***************** Internal use ****************/
+  struct LUMI_RAW_HEADER { // Used in NibbleCollector 
+    uint16_t marker;
+    uint8_t  hlxID;
+    uint8_t  packetID;
+    uint32_t startOrbit;
+    uint16_t numOrbits;
+    uint16_t startBunch; // Starting bunch in this packet
+    uint16_t numBunches; // Total number of bunches in histogram
+    uint8_t  histogramSet;
+    uint8_t  histogramSequence;
+    uint16_t allA;
+    uint16_t allF;
+  };
+
+  struct LUMI_NIBBLE_HEADER {
+    uint32_t  startOrbit;
+    uint16_t  numOrbits;
+    uint16_t  numBunches; // Number of bunches histogrammed
+    bool bCMSLive;
+    bool bOC0;
+  };
+  
+  struct ET_SUM_NIBBLE {
+    LUMI_NIBBLE_HEADER hdr;
+    uint32_t data[HCAL_HLX_MAX_BUNCHES];
+  };
+  
+  struct OCCUPANCY_NIBBLE {
+    LUMI_NIBBLE_HEADER hdr;
+    uint16_t data[6][HCAL_HLX_MAX_BUNCHES];
+  };
+
+  struct LHC_NIBBLE {
+    LUMI_NIBBLE_HEADER hdr;
+    uint16_t data[HCAL_HLX_MAX_BUNCHES];
+  };
+
+  struct RUN_SUMMARY {
+    uint32_t runNumber;
+    char runSequenceName[32];
+  };
+
+  struct LUMI_SECTION_HEADER {
+    uint32_t timestamp;
+    uint32_t timestamp_micros;
+
+    uint32_t  runNumber;   // Run number
+    uint32_t  sectionNumber; // Section number
+
+    uint32_t  startOrbit;  // Start orbit of lumi section
+    uint32_t  numOrbits;   // Total number of orbits recorded in lumi section
+    uint16_t  numBunches;  // Total number of bunches (from start of orbit)
+    uint16_t  numHLXs;     // Number of HLXs in lumi section
+
+    bool bCMSLive;    // Is CMS taking data?
+    bool bOC0;        // Was section initialised by an OC0?
+  };
+
+  struct LUMI_SECTION_SUB_HEADER {
+    uint32_t  numNibbles;  // Number of nibbles in this histogram
+    bool bIsComplete; // Is this histogram complete (i.e. no missing nibbles)
+  };
+
+  struct ET_SUM_SECTION {
+    LUMI_SECTION_SUB_HEADER hdr;
+    float data[HCAL_HLX_MAX_BUNCHES];
+  };
+
+  struct OCCUPANCY_SECTION {
+    LUMI_SECTION_SUB_HEADER hdr;
+    uint32_t data[6][HCAL_HLX_MAX_BUNCHES];
+  };
+
+  struct LHC_SECTION {
+    LUMI_SECTION_SUB_HEADER hdr;
+    uint32_t data[HCAL_HLX_MAX_BUNCHES];
+  };
+
+  struct LUMI_SECTION {
+    LUMI_SECTION_HEADER hdr;
+    LUMI_SUMMARY lumiSummary;
+    LUMI_DETAIL  lumiDetail;
+
+    ET_SUM_SECTION etSum[HCAL_HLX_MAX_HLXS];
+    OCCUPANCY_SECTION occupancy[HCAL_HLX_MAX_HLXS];
+    LHC_SECTION lhc[HCAL_HLX_MAX_HLXS];
+  };
+
+}//~namespace HCAL_HLX
+
+#endif //~LUMISTRUCTURES_HH
--- CondTools/Luminosity/plugins/RootSource.cc
+++ CondTools/Luminosity/plugins/RootSource.cc
@@ -19,32 +19,58 @@ lumi::RootSource::RootSource(const edm::ParameterSet& pset):LumiRetrieverBase(ps
     runtree->Print();
     std::cout<<"tot size bytes "<<runtree->GetTotBytes()<<std::endl;
     std::cout<<"n branches "<<runtree->GetNbranches()<<std::endl;
-    std::cout<<"tot entries "<<runtree->GetEntries()<<std::endl;
+    HCAL_HLX::RUN_SUMMARY* runsummarydata=0;
+    runtree->SetBranchAddress("RunSummary.",&runsummarydata);
+    size_t nentries=runtree->GetEntries();
+    for(size_t i=0;i<nentries;++i){
+      runtree->GetEntry(i);
+      std::cout<<runsummarydata->runNumber<<std::endl;
+    }
   }
 
-  TTree *hlxdata = (TTree*)m_source->Get("HLXData");
-  if(hlxdata){
-    hlxdata->Print();
-    std::cout<<"tot size bytes "<<hlxdata->GetTotBytes()<<std::endl;
-    std::cout<<"n branches "<<hlxdata->GetNbranches()<<std::endl;
-    std::cout<<"tot entries "<<hlxdata->GetEntries()<<std::endl;
+  TTree *hlxtree = (TTree*)m_source->Get("HLXData");
+  if(hlxtree){
+    hlxtree->Print();
+    std::cout<<"tot size bytes "<<hlxtree->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<hlxtree->GetNbranches()<<std::endl;
+    //HCAL_HLX::LUMI_SECTION* hlxdata=0;
+    HCAL_HLX::LUMI_SECTION_HEADER* lumiheader=0;
+    HCAL_HLX::LUMI_SUMMARY* lumisummary=0;
+    HCAL_HLX::LUMI_DETAIL* lumidetail=0;
+    hlxtree->SetBranchAddress("Header.",&lumiheader);
+    hlxtree->SetBranchAddress("Summary.",&lumisummary);
+    hlxtree->SetBranchAddress("Detail.",&lumidetail);
+    size_t nentries=hlxtree->GetEntries();
+    std::cout<<"hlxdata entries "<<nentries<<std::endl;
+    for(size_t i=0;i<nentries;++i){
+      hlxtree->GetEntry(i);
+      std::cout<<lumiheader->runNumber<<std::endl;
+      std::cout<<lumiheader->sectionNumber<<std::endl;
+      std::cout<<lumiheader->startOrbit<<std::endl;
+      std::cout<<lumisummary->InstantLumi<<std::endl;
+    }
   }
 
-  TTree *l1trg = (TTree*)m_source->Get("L1Trigger");
-  if(l1trg){
-    l1trg->Print();
-    std::cout<<"tot size bytes "<<l1trg->GetTotBytes()<<std::endl;
-    std::cout<<"n branches "<<l1trg->GetNbranches()<<std::endl;
-    std::cout<<"tot entries "<<l1trg->GetEntries()<<std::endl;
+  TTree *l1tree = (TTree*)m_source->Get("L1Trigger");
+  if(l1tree){
+    l1tree->Print();
+    std::cout<<"tot size bytes "<<l1tree->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<l1tree->GetNbranches()<<std::endl;
+    HCAL_HLX::LEVEL1_TRIGGER* l1data=0;
+    l1tree->SetBranchAddress("L1Trigger.",&l1data);
+    size_t nentries=l1tree->GetEntries();
+    std::cout<<"l1tree entries "<<nentries<<std::endl;
   }
 
-  TTree *hlttrg = (TTree*)m_source->Get("HLTrigger");
-  if(hlttrg){
-    hlttrg->Print();
-    std::cout<<"tot size bytes "<<hlttrg->GetTotBytes()<<std::endl;
-    std::cout<<"n branches "<<hlttrg->GetNbranches()<<std::endl;
-    std::cout<<"tot entries "<<hlttrg->GetEntries()<<std::endl;
-    
+  TTree *hlttree = (TTree*)m_source->Get("HLTrigger");
+  if(hlttree){
+    hlttree->Print();
+    std::cout<<"tot size bytes "<<hlttree->GetTotBytes()<<std::endl;
+    std::cout<<"n branches "<<hlttree->GetNbranches()<<std::endl;
+    HCAL_HLX::HLTrigger* hltdata=0;
+    hlttree->SetBranchAddress("HLTrigger.",&hltdata);
+    size_t nentries=hlttree->GetEntries();
+    std::cout<<"hlttree entries "<<nentries<<std::endl;
   }
 }
 void
