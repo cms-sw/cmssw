5b7c4a419a0e70bd6b56f666242e9ca638931bd1
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-10-08 08:52:41 +0000

    add hltMonBTagClient sequence

--- DQM/HLTEvF/python/HLTMonitorClient_cff.py
+++ DQM/HLTEvF/python/HLTMonitorClient_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.HLTEvF.HLTEventInfoClient_cff import *
 from DQM.HLTEvF.HLTMonMuonClient_cff import *
+from DQM.HLTEvF.hltMonBTagClient_cff import *
 from DQM.HLTEvF.hltBitsQualityTests_cff import *
 
 # MVA remove until bug fixes
@@ -13,9 +14,4 @@ from DQM.HLTEvF.hltBitsQualityTests_cff import *
 
 # hltmonitorClient = cms.Path(hltmonmuonseqClient*hltEventInfoseqClient)
 #hltmonitorClient = cms.Path(hltmonmuonClient*hltqtest*hltEventInfoseqClient)
-hltmonitorClient = cms.Path(hltmonmuonseqClient*hltqtest*hltEventInfoseqClient)
-
-
-
-
-
+hltmonitorClient = cms.Path(hltmonmuonseqClient + hltMonBTagClient + hltqtest + hltEventInfoseqClient)
