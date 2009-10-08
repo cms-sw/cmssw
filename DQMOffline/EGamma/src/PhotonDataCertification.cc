61e86153d342ecc7eb0c35639007cb9badbcf387
Louis James Antonelli <sha1-442197ce02a0252aaee7a57f46fd47b9778e7d2f@cern.ch>
2009-10-08 12:02:06 +0000

    new, improved version aimed at 34X

--- DQMOffline/EGamma/src/PhotonDataCertification.cc
+++ DQMOffline/EGamma/src/PhotonDataCertification.cc
@@ -7,7 +7,7 @@
 //
 // Original Author:  Louis James Antonelli
 //         Created:  Thu Jan 22 13:42:28CET 2009
-// $Id: PhotonDataCertification.cc,v 1.4 2009/03/12 16:11:27 lantonel Exp $
+// $Id: PhotonDataCertification.cc,v 1.5 2009/09/01 12:00:57 lantonel Exp $
 //
 
 
@@ -18,22 +18,20 @@ using namespace std;
 PhotonDataCertification::PhotonDataCertification(const edm::ParameterSet& pset)
 
 {
-  //cout << "entering constructor" << endl;
+
   dbe_ = 0;
   dbe_ = edm::Service<DQMStore>().operator->();
   dbe_->setVerbose(0);
   parameters_ = pset;
-  //cout << "exiting constructor" << endl;
+  verbose_ = parameters_.getParameter<bool>("verbose");
+  
+  if(verbose_) cout << ">>> Constructor (PhotonDataCertification) <<<" << endl;
 
 }
 
 
 PhotonDataCertification::~PhotonDataCertification()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
 }
 
 
@@ -46,14 +44,10 @@ void
 PhotonDataCertification::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
-   
-   
 
-   //std::cout << "analyze" << std::endl;
-
+   if(verbose_) std::cout << ">>> Analyze (PhotonDataCertification) <<<" << std::endl;
 
 }
-
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
@@ -63,19 +57,17 @@ PhotonDataCertification::beginJob(const edm::EventSetup&)
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-PhotonDataCertification::endJob() {
+PhotonDataCertification::endJob() 
+{
 }
-
-
-
 
 // ------------ method called just before starting a new run  ------------
 void 
 PhotonDataCertification::beginRun(const edm::Run& run, const edm::EventSetup& c)
 {
   
-  //std::cout << ">>> BeginRun (PhotonDataCertification) <<<" << std::endl;
-  //std::cout << ">>> run = " << run.id() << std::endl;
+  if(verbose_) std::cout << ">>> BeginRun (PhotonDataCertification) <<<" << std::endl;
+  if(verbose_) std::cout << ">>> "<< run.id() << std::endl;
 
 }
 
@@ -83,112 +75,154 @@ PhotonDataCertification::beginRun(const edm::Run& run, const edm::EventSetup& c)
 void 
 PhotonDataCertification::endRun(const edm::Run& run, const edm::EventSetup& c)
 {
-  //std::cout << ">>> EndRun (PhotonDataCertification) <<<" << std::endl;
-  //std::cout << ">>> run = " << run.id() << std::endl;
-  //std::cout << "endJob" << std::endl;
-  
-  std::string filename    = parameters_.getUntrackedParameter<std::string>("fileName");
-  std::string reffilename = parameters_.getUntrackedParameter<std::string>("refFileName");
-  std::string outputFileName = parameters_.getUntrackedParameter<std::string>("outputFileName");
+  if(verbose_) std::cout << ">>> EndRun (PhotonDataCertification) <<<" << std::endl;
 
-  //dbe_->open(reffilename);
-
-  dbe_->setCurrentFolder("/");
-
-//   std::vector<std::string> subDirVec;
-//   std::string RunDir;
-//   std::string RefRunDir;
-  
-  //bool standalone = true;
-
-  //bool standalone = false; 
-
-//   std::vector<MonitorElement*> mes;
-
-
-//   mes = dbe_->getAllContents("");
-//   std::cout << "1 >>> found " <<  mes.size() << " monitoring elements!" << std::endl;
-  
-//   //dbe_->setCurrentFolder("EGamma");
-//   subDirVec = dbe_->getSubdirs();
-//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin();ic != subDirVec.end(); ic++) {    
-//     std::cout << "-AAA- Dir = >>" << ic->c_str() << "<<" << std::endl;
-//   }
-//   dbe_->setCurrentFolder("EGamma");
-//   std::cout << "asdfasdfa\n";
-//   subDirVec = dbe_->getSubdirs();
-//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin();ic != subDirVec.end(); ic++) {    
-//     std::cout << "-AAA- Dir = >>" << ic->c_str() << "<<" << std::endl;
-//   }
-
+  std::vector<string> histoNameVector;
 
  
-  //  subDirVec = dbe_->getSubdirs();
-   
+  //booking histograms according to naming conventions
 
-//   for (std::vector<std::string>::const_iterator ic = subDirVec.begin(); ic != subDirVec.end(); ic++) {
+  if(!dbe_) return;
+
+  dbe_->setCurrentFolder("Egamma/EventInfo/");  
+
+  MonitorElement*  reportSummary = dbe_->bookFloat("reportSummary");
+  MonitorElement*  CertificationSummary = dbe_->bookFloat("CertificationSummary");
+
+  //for now these will hold values from eta/phi tests for spikes/holes
+  MonitorElement*  reportSummaryMap = dbe_->book2D("reportSummaryMap","reportSummaryMap",2,0,2,2,0,2);
+  MonitorElement*  CertificationSummaryMap = dbe_->book2D("CertificationSummaryMap","CertificationSummaryMap",2,0,2,2,0,2);
+
+  TH2 * reportSummaryMapTH2 = reportSummaryMap->getTH2F();
+
+  reportSummaryMapTH2->GetXaxis()->SetBinLabel(1,"Eta");
+  reportSummaryMapTH2->GetXaxis()->SetBinLabel(2,"Phi");
+  reportSummaryMapTH2->GetYaxis()->SetBinLabel(1,"SpikeTest");
+  reportSummaryMapTH2->GetYaxis()->SetBinLabel(2,"HoleTest");
+
+  TH2 * CertificationSummaryMapTH2 = CertificationSummaryMap->getTH2F();
+
+  CertificationSummaryMapTH2->GetXaxis()->SetBinLabel(1,"Eta");
+  CertificationSummaryMapTH2->GetXaxis()->SetBinLabel(2,"Phi");
+  CertificationSummaryMapTH2->GetYaxis()->SetBinLabel(1,"SpikeTest");
+  CertificationSummaryMapTH2->GetYaxis()->SetBinLabel(2,"HoleTest");
+
+
+
+  //histograms to be tested and results put into Egamma/EventInfo/reportSummaryContents
+  histoNameVector.push_back("nPhoAllEcal");
+  histoNameVector.push_back("r9AllEcal");
+  histoNameVector.push_back("phoEta");
+  histoNameVector.push_back("phoPhi");
+
+
+//   histoNameVector.push_back("phoEtAllEcal");
+//   histoNameVector.push_back("EfficiencyVsEtHLT");
+//   histoNameVector.push_back("EfficiencyVsEtaHLT");
+//   histoNameVector.push_back("EfficiencyVsEtLoose");
+//   histoNameVector.push_back("EfficiencyVsEtaLoose");
+
+
+  // to do:  what do we want in certification contents?
+  //  dbe_->setCurrentFolder("Egamma/EventInfo/CertificationContents/"); 
+
+
+  //looping over histograms to be tested
+  if(verbose_) std::cout << "\n>>> looping over histograms to be tested <<<\n\n";
+  for(std::vector<string>::iterator it=histoNameVector.begin();it!=histoNameVector.end();++it){
+
+    string HistoName = (*it);
+    if(verbose_) std::cout << ">>> " << HistoName;        
     
-//     std::cout << "Dir = >>" << ic->c_str() << "<<" << std::endl;
-//     if (ic->find( "Run" ) != std::string::npos) {
-//       RefRunDir = *ic;
-//       std::cout << "found RefRunDir" << std::endl;
-//       //standalone = false;
-//     }
-// //     if (ic->find( "Egamma" ) != std::string::npos) {
-// //       standalone = false;
-// //       cout << " found Egamma" << endl;
-// //     }
-//   }
+    MonitorElement * TestHist=0;
+    
+    if(HistoName.find("Efficiency")!=std::string::npos){
+      TestHist = dbe_->get("Egamma/PhotonAnalyzer/Efficiencies/"+HistoName);
+    }
+    else{
+      TestHist = dbe_->get("Egamma/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/"+HistoName);
+    }
+    bool validMe = TestHist!=0;
+    if(verbose_)  std::cout << " is valid? " << validMe << "\n";
+    if(!validMe) continue;
 
-  //if(standalone) dbe_->setCurrentFolder(RefRunDir+"/Egamma/EventInfo/Certification/");  
-  //else dbe_->setCurrentFolder("Egamma/EventInfo/Certification/");
-  //else dbe_->setCurrentFolder(RefRunDir+"/Egamma/EventInfo/Certification/");  
+    if(verbose_)  std::cout << ">>> TestHist Name: " << TestHist->getName() << "\n\n";
 
-  dbe_->setCurrentFolder("Egamma/EventInfo/Certification/");  
+  
 
+    //get QReports associated to each ME
+    std::vector<QReport *> myQReports = TestHist->getQReports();  
+    if(verbose_)  cout << TestHist->getName() <<": myQReports.size() = " << myQReports.size() << "\n\n";
+    for(uint i=0;i!=myQReports.size();++i) {
+      
+      std::string qtname = myQReports[i]->getQRName() ; // get QT name
+      float qtresult = myQReports[i]->getQTresult(); // get QT result value
+      int qtstatus = myQReports[i]->getStatus() ; // get QT status value 
+      
+      if(verbose_) std::cout << "\tTest " << i << ":  qtname: " << qtname   << "\n";
+      if(verbose_) std::cout << "\tTest " << i << ":  qtresult: " << qtresult  << std::endl;
+      if(verbose_) std::cout << "\tTest " << i << ":  qtstatus: " << qtstatus    << "\n\n";
 
-  MonitorElement* KolmogorovTest = dbe_->bookFloat("KolTest");
-  MonitorElement* ChiSquaredTest = dbe_->bookFloat("Chi2Test");
-  
-  
-  //std::cout << dbe_->pwd() << std::endl;
-  //std::cout << "getting Histos" << std::endl;
-  
-  
-  //MonitorElement * RefHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
-  //cout << "got first one " << endl;
-  //TH1F * RefHistTH1 = RefHist->getTH1F();
-  
-  //MonitorElement * TestHist=0;
-  //if(standalone) TestHist = new MonitorElement(*(dbe_->get(RefRunDir+"/Egamma/Run summary/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
-  //else TestHist = new MonitorElement(*(dbe_->get("Egamma/PhotonAnalyzer/GoodCandidatePhotons/Et above 0 GeV/phoEtAllEcal")));
-  //cout << "got second one " << endl;
-  //TH1F * TestHistTH1 = TestHist->getTH1F();
-  
-  //double KolTest;
-  //double Chi2Test;
-  
-  KolmogorovTest->Fill(-1);
-  ChiSquaredTest->Fill(-1);
+      
+      //book and fill float for each test done
+      dbe_->setCurrentFolder("Egamma/EventInfo/reportSummaryContents/");  
+      MonitorElement * Float  = dbe_->bookFloat(HistoName+"_"+qtname);
+      Float->Fill(qtresult);
+
+    }
+  }
 
 
 
-//   if(TestHistTH1->GetEntries() != 0){
- 
-//     KolTest = TestHistTH1->KolmogorovTest(RefHistTH1);
-//     Chi2Test = TestHistTH1->Chi2Test(RefHistTH1);
-//     //cout << KolTest << "   " << Chi2Test << endl;
- 
-//     KolmogorovTest->Fill(KolTest);
-//     ChiSquaredTest->Fill(Chi2Test);
-  
-//   }
-//   else{
-//     KolmogorovTest->Fill(-1);
-//     ChiSquaredTest->Fill(-1);
-//   }
+  //filling summaries based on geometric spikes/holes
 
-//   if(standalone) dbe_->save(outputFileName);
-  
-  
+  float etaSpikeTestResult=0;
+  float phiSpikeTestResult=0;
+  float etaHoleTestResult=0;
+  float phiHoleTestResult=0;
+
+  if(dbe_->get("Egamma/EventInfo/reportSummaryContents/phoEta_SpikeTest")!=0)
+    etaSpikeTestResult = dbe_->get("Egamma/EventInfo/reportSummaryContents/phoEta_SpikeTest")->getFloatValue();
+  else  etaSpikeTestResult = -1;
+
+  if(dbe_->get("Egamma/EventInfo/reportSummaryContents/phoPhi_SpikeTest")!=0)
+    phiSpikeTestResult = dbe_->get("Egamma/EventInfo/reportSummaryContents/phoPhi_SpikeTest")->getFloatValue();
+  else  phiSpikeTestResult = -1;
+
+  if(dbe_->get("Egamma/EventInfo/reportSummaryContents/phoEta_HoleTest")!=0)
+    etaHoleTestResult = dbe_->get("Egamma/EventInfo/reportSummaryContents/phoEta_HoleTest")->getFloatValue();
+  else  etaHoleTestResult = -1;
+
+  if(dbe_->get("Egamma/EventInfo/reportSummaryContents/phoPhi_HoleTest")!=0)
+    phiHoleTestResult = dbe_->get("Egamma/EventInfo/reportSummaryContents/phoPhi_HoleTest")->getFloatValue();
+  else  phiHoleTestResult = -1;
+
+
+
+  if(verbose_) std::cout << ">>>  Results of tests to be put into Summary Maps  <<<\n\n";
+  if(verbose_) std::cout << "\tetaSpikeTestResult= " << etaSpikeTestResult << "\n";
+  if(verbose_) std::cout << "\tphiSpikeTestResult= " << phiSpikeTestResult << "\n";
+  if(verbose_) std::cout << "\tetaHoleTestResult= " << etaHoleTestResult << "\n";
+  if(verbose_) std::cout << "\tphiHoleTestResult= " << phiHoleTestResult << "\n\n";
+
+
+  //fill reportSummary & CertificationSummary with average of hole/spike tests 
+  float reportSummaryFloat = (etaSpikeTestResult+etaHoleTestResult+phiSpikeTestResult+phiHoleTestResult)/4.;
+  if(reportSummaryFloat<0) reportSummaryFloat = -1;
+  reportSummary->Fill(reportSummaryFloat);
+  CertificationSummary->Fill(reportSummaryFloat);
+
+  reportSummaryMap->Fill(0,0,etaSpikeTestResult);
+  reportSummaryMap->Fill(0,1,etaHoleTestResult);
+  reportSummaryMap->Fill(1,0,phiSpikeTestResult);
+  reportSummaryMap->Fill(1,1,phiHoleTestResult);
+
+  CertificationSummaryMap->Fill(0,0,etaSpikeTestResult);
+  CertificationSummaryMap->Fill(0,1,etaHoleTestResult);
+  CertificationSummaryMap->Fill(1,0,phiSpikeTestResult);
+  CertificationSummaryMap->Fill(1,1,phiHoleTestResult);
+
+
+
+
 }
