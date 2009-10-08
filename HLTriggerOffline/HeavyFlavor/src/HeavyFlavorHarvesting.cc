9c777ef3542feee4561f75e6d86331b644e268e7
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-10-08 01:29:01 +0000

    store efficiency as TProfile instead of histogram to avoid normalization, clean up

--- HLTriggerOffline/HeavyFlavor/src/HeavyFlavorHarvesting.cc
+++ HLTriggerOffline/HeavyFlavor/src/HeavyFlavorHarvesting.cc
@@ -23,8 +23,10 @@ class HeavyFlavorHarvesting : public edm::EDAnalyzer, public TGraphAsymmErrors{
     virtual ~HeavyFlavorHarvesting();
     virtual void analyze(const edm::Event& event, const edm::EventSetup& eventSetup) {};
     virtual void endJob();
-    void calculateEfficiency(const ParameterSet& pset);
   private:
+    void calculateEfficiency(const ParameterSet& pset);
+    void calculateEfficiency1D( TH1* num, TH1* den, string name );
+    void calculateEfficiency2D( TH2F* num, TH2F* den, string name );
     DQMStore * dqmStore;
     string myDQMrootFolder;
     const VParameterSet efficiencies;
@@ -60,12 +62,12 @@ void HeavyFlavorHarvesting::calculateEfficiency(const ParameterSet& pset){
   }
   TH1 *den = denME->getTH1();
   TH1 *num = numME->getTH1();
-//check dimensionality of the histograms  
+  //check compatibility of the histograms  
   if( den->GetNbinsX() != num->GetNbinsX() || den->GetNbinsY() != num->GetNbinsY() ||  den->GetNbinsZ() != num->GetNbinsZ() ){
     LogError("HLTriggerOfflineHeavyFlavor") << "Monitoring elements "<<numMEname<<" and "<<denMEname<<"are incompatible"<<endl;
     return;
   }
-//book the efficiency histogram  
+  //figure out the directory and efficiency name  
   string effName = pset.getUntrackedParameter<string>("EfficiencyMEname");
   string effDir = myDQMrootFolder;
   string::size_type slashPos = effName.rfind('/');
@@ -74,99 +76,81 @@ void HeavyFlavorHarvesting::calculateEfficiency(const ParameterSet& pset){
     effName.erase(0, slashPos+1);
   }
   dqmStore->setCurrentFolder(effDir);
-  MonitorElement * effME;
-  int numberOfCells = 0;
-  TH1 * eff = (TH1*)num->Clone(effName.c_str());
-  eff->SetTitle(effName.c_str());
-  int dimensions = eff->GetDimension();
+  //calculate the efficiencies
+  int dimensions = num->GetDimension();
   if(dimensions==1){
-      numberOfCells = ((TH1F*)eff)->GetSize();
-      effME = dqmStore->book1D(effName,(TH1F*)eff);
-      delete eff;
-      eff = effME->getTH1();
-      eff->SetOption("PE");
-      eff->SetLineColor(2);
-      eff->SetLineWidth(2);
-      eff->GetYaxis()->SetRangeUser(-0.001,1.001);
-      eff->SetStats(kFALSE);
+    calculateEfficiency1D( num, den, effName );
   }else if(dimensions==2){
-      numberOfCells = ((TH2F*)eff)->GetSize();
-      effME = dqmStore->book2D(effName,(TH2F*)eff);
-      delete eff;
-      eff = effME->getTH1();
-      eff->SetOption("colztexte");
-      eff->GetZaxis()->SetRangeUser(-0.001,1.001);
-      eff->SetStats(kFALSE);
-
-      TH1D* numX = ((TH2F*)num)->ProjectionX(); 
-      TH1D* denX = ((TH2F*)den)->ProjectionX();
-      TH1F* effX;
-      if(numX->GetXaxis()->GetXbins()->GetSize()==0){
-        effX = new TH1F((effName+"X").c_str(),(effName+"X").c_str(),numX->GetXaxis()->GetNbins(),numX->GetXaxis()->GetXmin(),numX->GetXaxis()->GetXmax());
-      }else{
-        effX = new TH1F((effName+"X").c_str(),(effName+"X").c_str(),numX->GetXaxis()->GetNbins(),numX->GetXaxis()->GetXbins()->GetArray());
-      }
-      effX->SetXTitle(num->GetXaxis()->GetTitle());
-      effX->SetYTitle("Efficiency");
-      MonitorElement *effMEX = dqmStore->book1D(effName+"X",effX);
-      delete effX;
-      effX = (TH1F*)effMEX->getTH1();
-      effX->SetOption("PE");
-      effX->SetLineColor(2);
-      effX->SetLineWidth(2);
-      effX->GetYaxis()->SetRangeUser(-0.001,1.001);
-      effX->SetStats(kFALSE);
-      for(int i=1;i<=numX->GetNbinsX();i++){
-        double e;
-        double low;
-        double high;
-        Efficiency((int)numX->GetBinContent(i),(int)denX->GetBinContent(i),0.683,e,low,high);
-        effX->SetBinContent(i,e);
-        effX->SetBinError( i, e-low>high-e ? e-low : high-e );
-      }
-
-      TH1D* numY = ((TH2F*)num)->ProjectionY();
-      TH1D* denY = ((TH2F*)den)->ProjectionY();
-      TH1F* effY;
-      if(numY->GetXaxis()->GetXbins()->GetSize()==0){
-        effY = new TH1F((effName+"Y").c_str(),(effName+"Y").c_str(),numY->GetXaxis()->GetNbins(),numY->GetXaxis()->GetXmin(),numY->GetXaxis()->GetXmax());
-      }else{
-        effY = new TH1F((effName+"Y").c_str(),(effName+"Y").c_str(),numY->GetXaxis()->GetNbins(),numY->GetXaxis()->GetXbins()->GetArray());
-      }
-      effY->SetXTitle(num->GetYaxis()->GetTitle());
-      effY->SetYTitle("Efficiency");
-      MonitorElement *effMEY = dqmStore->book1D(effName+"Y",effY);
-      delete effY;
-      effY = (TH1F*)effMEY->getTH1();
-      effY->SetOption("PE");
-      effY->SetLineColor(2);
-      effY->SetLineWidth(2);
-      effY->GetYaxis()->SetRangeUser(-0.001,1.001);
-      effY->SetStats(kFALSE);
-      for(int i=1;i<=numY->GetNbinsX();i++){
-        double e;
-        double low;
-        double high;
-        Efficiency((int)numY->GetBinContent(i),(int)denY->GetBinContent(i),0.683,e,low,high);
-        effY->SetBinContent(i,e);
-        effY->SetBinError( i, e-low>high-e ? e-low : high-e );
-      }
-/*  }else if(dimensions==3){
-    numberOfCells = ((TH3F*)eff)->GetSize();
-    effME = dqmStore->book3D(effName,(TH3F*)eff);
-    delete eff;
-    eff = effME->getTH1();*/
+    calculateEfficiency2D( (TH2F*)num, (TH2F*)den, effName );
+    TH1D* numX = ((TH2F*)num)->ProjectionX(); 
+    TH1D* denX = ((TH2F*)den)->ProjectionX();
+    calculateEfficiency1D( numX, denX, effName+"X" );
+    delete numX;
+    delete denX;
+    TH1D* numY = ((TH2F*)num)->ProjectionY();
+    TH1D* denY = ((TH2F*)den)->ProjectionY();
+    calculateEfficiency1D( numY, denY, effName+"Y" );
+    delete numY;
+    delete denY;
   }else{
     return;
   }
-  for(int i=0;i<numberOfCells;i++){
-    double e;
-    double low;
-    double high;
-    Efficiency((int)num->GetBinContent(i),(int)den->GetBinContent(i),0.683,e,low,high);
-    eff->SetBinContent(i,e);
-    eff->SetBinError( i, e-low>high-e ? e-low : high-e );
+}
+
+void HeavyFlavorHarvesting::calculateEfficiency1D( TH1* num, TH1* den, string effName ){
+  TProfile* eff;
+  if(num->GetXaxis()->GetXbins()->GetSize()==0){
+    eff = new TProfile(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXmin(),num->GetXaxis()->GetXmax());
+  }else{
+    eff = new TProfile(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXbins()->GetArray());
   }
+  eff->SetTitle(effName.c_str());
+  eff->SetOption("PE");
+  eff->SetLineColor(2);
+  eff->SetLineWidth(2);
+  eff->SetMarkerStyle(20);
+  eff->SetMarkerSize(0.8);
+  eff->GetYaxis()->SetRangeUser(-0.001,1.001);
+  eff->SetStats(kFALSE);
+  for(int i=1;i<=num->GetNbinsX();i++){
+    double e, low, high;
+    Efficiency( (int)num->GetBinContent(i), (int)den->GetBinContent(i), 0.683, e, low, high );
+    double err = e-low>high-e ? e-low : high-e;
+    //here is the trick to store info in TProfile:
+    eff->SetBinContent( i, e );
+    eff->SetBinEntries( i, 1 );
+    eff->SetBinError( i, sqrt(e*e+err*err) );
+  }
+  dqmStore->bookProfile(effName,eff);
+  delete eff;
+}
+
+void HeavyFlavorHarvesting::calculateEfficiency2D( TH2F* num, TH2F* den, string effName ){
+  TProfile2D* eff;
+  if(num->GetXaxis()->GetXbins()->GetSize()==0 && num->GetYaxis()->GetXbins()->GetSize()==0){
+    eff = new TProfile2D(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXmin(),num->GetXaxis()->GetXmax(),num->GetYaxis()->GetNbins(),num->GetYaxis()->GetXmin(),num->GetYaxis()->GetXmax());
+  }else if(num->GetXaxis()->GetXbins()->GetSize()!=0 && num->GetYaxis()->GetXbins()->GetSize()==0){
+    eff = new TProfile2D(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXbins()->GetArray(),num->GetYaxis()->GetNbins(),num->GetYaxis()->GetXmin(),num->GetYaxis()->GetXmax());
+  }else if(num->GetXaxis()->GetXbins()->GetSize()==0 && num->GetYaxis()->GetXbins()->GetSize()!=0){
+    eff = new TProfile2D(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXmin(),num->GetXaxis()->GetXmax(),num->GetYaxis()->GetNbins(),num->GetYaxis()->GetXbins()->GetArray());
+  }else{
+    eff = new TProfile2D(effName.c_str(),effName.c_str(),num->GetXaxis()->GetNbins(),num->GetXaxis()->GetXbins()->GetArray(),num->GetYaxis()->GetNbins(),num->GetYaxis()->GetXbins()->GetArray());
+  }
+  eff->SetTitle(effName.c_str());
+  eff->SetOption("colztexte");
+  eff->GetZaxis()->SetRangeUser(-0.001,1.001);
+  eff->SetStats(kFALSE);
+  for(int i=0;i<num->GetSize();i++){
+    double e, low, high;
+    Efficiency( (int)num->GetBinContent(i), (int)den->GetBinContent(i), 0.683, e, low, high );
+    double err = e-low>high-e ? e-low : high-e;
+    //here is the trick to store info in TProfile:
+    eff->SetBinContent( i, e );
+    eff->SetBinEntries( i, 1 );
+    eff->SetBinError( i, sqrt(e*e+err*err) );
+  }
+  dqmStore->bookProfile2D(effName,eff);
+  delete eff;
 }
 
 HeavyFlavorHarvesting::~HeavyFlavorHarvesting(){
