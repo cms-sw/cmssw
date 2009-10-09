385749232652b8674248d1bc9616bba853f4c980
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2009-10-09 08:31:56 +0000

    *** empty log message ***

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
@@ -3,12 +3,37 @@ import FWCore.ParameterSet.Config as cms
 #------------------------------------------------
 #AlCaReco filtering for HCAL minbias:
 #------------------------------------------------
-import HLTrigger.HLTfilters.hltHighLevel_cfi
 
+import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
+hcalDigiAlCaMB = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
+import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hbhe_cfi
+hbherecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hbhe_cfi.hbhereco.clone()
+import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi
+hfrecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi.hfreco.clone()
+import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi
+horecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi.horeco.clone()
+import HLTrigger.HLTfilters.hltHighLevel_cfi
 hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
 #    HLTPaths = ['AlCa_HcalPhiSym'],
     eventSetupPathsKey='HcalCalMinBias',
-    throw = False #dont throw except on unknown path name
+    throw = False #dont throw except on unknown path name 
 )
 
-seqALCARECOHcalCalMinBias = cms.Sequence(hcalminbiasHLT)
+seqALCARECOHcalCalMinBias = cms.Sequence(hcalminbiasHLT*hbherecoNoise*hfrecoNoise*horecoNoise)
+
+hcalDigiAlCaMB.firstSample = 0
+hcalDigiAlCaMB.lastSample = 9
+hbherecoNoise.firstSample = 0
+hbherecoNoise.samplesToAdd = 4
+hbherecoNoise.digiLabel = 'hcalDigiAlCaMB'
+hfrecoNoise.firstSample = 1
+hfrecoNoise.samplesToAdd = 1
+hfrecoNoise.digiLabel = 'hcalDigiAlCaMB'
+horecoNoise.firstSample = 0
+horecoNoise.samplesToAdd = 4
+horecoNoise.digiLabel = 'hcalDigiAlCaMB'
+
+#switch off "ZS in reco":
+hbherecoNoise.dropZSmarkedPassed = cms.bool(False)
+hfrecoNoise.dropZSmarkedPassed = cms.bool(False)
+horecoNoise.dropZSmarkedPassed = cms.bool(False)
