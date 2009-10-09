5bfbf6777291ff772db57791c34d267919dd6aed
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2009-10-09 13:32:36 +0000

    package dependency updated

--- TopQuarkAnalysis/Examples/plugins/HypothesisAnalyzer.cc
+++ TopQuarkAnalysis/Examples/plugins/HypothesisAnalyzer.cc
@@ -1,6 +1,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "AnalysisDataFormats/TopObjects/interface/TtSemiLeptonicEvent.h"
