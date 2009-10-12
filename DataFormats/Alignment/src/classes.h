eb5ee0d906ffdb23f7db4a99c403ae8c31c842b0
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-10-12 13:11:34 +0000

    clean up

--- DataFormats/Alignment/src/classes.h
+++ DataFormats/Alignment/src/classes.h
@@ -1,17 +1,18 @@
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Common/interface/AssociationVector.h"
+//#include "DataFormats/Common/interface/AssociationVector.h"
 #include "DataFormats/Alignment/interface/AlignmentClusterFlag.h"
 #include "DataFormats/Alignment/interface/AliClusterValueMap.h"
 #include <vector>
 #include <map>
 
 namespace {
-  namespace {
-    AlignmentClusterFlag                                 ahf;
-     edm::Wrapper<AlignmentClusterFlag>                   wahf;
+  struct dictionary { // namespace {
+    // not needed (not instance of template):
+    // AlignmentClusterFlag                              ahf;
+    edm::Wrapper<AlignmentClusterFlag>                   wahf;
     AliClusterValueMap                                        ahvm1;
-     edm::Wrapper<AliClusterValueMap>                          wahvm1;
+    edm::Wrapper<AliClusterValueMap>                          wahvm1;
     //  AliTrackTakenClusterValueMap                              atthvm1;
     //  edm::Wrapper<AliTrackTakenClusterValueMap>                watthvm1;
-  }
+  };
 }
