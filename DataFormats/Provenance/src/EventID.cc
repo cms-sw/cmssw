e2b0d8f8dc66f723eeb775edb53a17c05bb00ced
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-10-12 22:27:46 +0000

    Add luminosity block number to printout of EventID object

--- DataFormats/Provenance/src/EventID.cc
+++ DataFormats/Provenance/src/EventID.cc
@@ -3,7 +3,7 @@
 
 namespace edm {
   std::ostream& operator<<(std::ostream& oStream, EventID const& iID) {
-    oStream << "run: " << iID.run() << " event: " << iID.event();
+    oStream << "run: " << iID.run() << " lumi: " << iID.luminosityBlock() << " event: " << iID.event();
     return oStream;
   }
 }
