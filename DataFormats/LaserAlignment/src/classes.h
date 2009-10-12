3552a8f62e5a1dcdfcdfcf279d4b9a1186d3900d
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-10-12 12:46:52 +0000

    remove obsolete data formats

--- DataFormats/LaserAlignment/src/classes.h
+++ DataFormats/LaserAlignment/src/classes.h
@@ -2,7 +2,6 @@
 #ifndef DataFormats_LaserAlignment_classes_h
 #define DataFormats_LaserAlignment_classes_h
 
-// this is the new section, the rest is obsolete ///////////
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/LaserAlignment/interface/SiStripLaserRecHit2D.h"
 #include "DataFormats/LaserAlignment/interface/TkLasBeam.h"
@@ -20,43 +19,6 @@ namespace {
     edm::Wrapper<TkFittedLasBeamCollection> fitBeamCollection2;
 
   };
-}
-// end of new section //////////////////////////////////////
-
-
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/LaserAlignment/interface/LASBeamProfileFit.h"
-#include "DataFormats/LaserAlignment/interface/LASBeamProfileFitCollection.h"
-
-namespace {
-  namespace {
-    edm::Wrapper<LASBeamProfileFit> beamprofilefit;
-  }
-}
-
-#include "DataFormats/LaserAlignment/interface/LASBeamProfileFitCollection.h"
-
-namespace {
-  namespace {
-    edm::Wrapper<LASBeamProfileFitCollection> collection;
-  }
-}
-
-#include "DataFormats/LaserAlignment/interface/LASAlignmentParameter.h"
-#include "DataFormats/LaserAlignment/interface/LASAlignmentParameterCollection.h"
-
-namespace {
-	namespace {
-		edm::Wrapper<LASAlignmentParameter> alignmentParameter;
-	}
-}
-
-#include "DataFormats/LaserAlignment/interface/LASAlignmentParameterCollection.h"
-
-namespace {
-	namespace {
-		edm::Wrapper<LASAlignmentParameterCollection> collection2;
-	}
 }
 
 #endif
