8c367ed313cf0f282c8438aa78b0b95c3d888f6d
Roberto Salerno <sha1-7c519629ee467d4f0f5dfc5154ab77f8eca4747e@cern.ch>
2009-10-13 09:53:47 +0000

    remove process name, fix ecal iso

--- PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
@@ -3,16 +3,14 @@ import FWCore.ParameterSet.Config as cms
 ## compute isolation, using POG modules
 from RecoEgamma.EgammaIsolationAlgos.eleIsoDeposits_cff import *
 from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDeposits_cff  import *
-from RecoEgamma.EgammaIsolationAlgos.electronEcalRecHitIsolationScone_cfi import *
 
-eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
-eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")
+eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "")
+eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "")
 
 ## sequence to run on AOD 
 patElectronIsolation = cms.Sequence(
     eleIsoDepositTk * eleIsoFromDepsTk +
-    eleIsoDepositEcalFromHits * eleIsoFromDepsEcalFromHits +
-    electronEcalRecHitIsolationScone +
+    eleIsoDepositEcalFromHits * eleIsoFromDepsEcalFromHitsByCrystal +
     eleIsoDepositHcalFromTowers * eleIsoFromDepsHcalFromTowers
 )
 
