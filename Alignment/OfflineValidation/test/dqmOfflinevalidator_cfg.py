069c5aaf93be8784e8d258dab656c44da70589ae
Johannes Hauk <sha1-79adb14f6701f120a4bd8908168f6f2ed5256e2c@cern.ch>
2009-10-13 14:36:07 +0000

    Some cosmetics are removed automatically CVS:  CVS: Committing in . CVS:  CVS: Modified Files: CVS:  dqmOfflinevalidator_cfg.py CVS: ----------------------------------------------------------------------

--- Alignment/OfflineValidation/test/dqmOfflinevalidator_cfg.py
+++ Alignment/OfflineValidation/test/dqmOfflinevalidator_cfg.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+
 process = cms.Process("Demo")
+
 
 ##
 ## Message Logger
@@ -13,6 +15,7 @@ process.MessageLogger.cout = cms.untracked.PSet(INFO = cms.untracked.PSet(
     ))
 process.MessageLogger.statistics.append('cout')
 
+
 ##
 ## Process options
 ##
@@ -20,6 +23,7 @@ process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring("ProductNotFound") # make this exception fatal
     , fileMode  =  cms.untracked.string('NOMERGE')
 )
+
 
 ##
 ## Conditions
@@ -31,10 +35,12 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'DESIGN_31X_V2::All' # 'CRUZET4_V3P::All'
 #process.prefer("GlobalTag")
 
+
 ##
 ## Geometry
 ##
 process.load("Configuration.StandardSequences.Geometry_cff")
+
 
 ##
 ## Magnetic Field
@@ -43,10 +49,12 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 # 0 T:
 #process.load("Configuration.StandardSequences.MagneticField_0T_cff")
 
+
 ##
 ## BeamSpot from database (i.e. GlobalTag), needed for Refitter
 ##
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+
 
 ##
 ## Load DBSetup (if needed)
@@ -74,6 +82,7 @@ process.myTrackerAlignmentErr = poolDBESSource.clone(
       )
     )
 process.es_prefer_trackerAlignmentErr = cms.ESPrefer("PoolDBESSource","myTrackerAlignmentErr")
+
 
 ##
 ## Input File(s)
@@ -111,21 +120,22 @@ process.source = cms.Source("PoolSource",
     ##
     ## 218 RelVal Sample, please replace accordingly
     ##
-####################    '/store/relval/CMSSW_2_1_8/RelValZMM/ALCARECO/STARTUP_V7_StreamALCARECOTkAlMuonIsolated_v1/0003/A8583C5E-0283-DD11-8D18-000423D987FC.root'
-#    ##
+    #'/store/relval/CMSSW_2_1_8/RelValZMM/ALCARECO/STARTUP_V7_StreamALCARECOTkAlMuonIsolated_v1/0003/A8583C5E-0283-DD11-8D18-000423D987FC.root'
+    ##
     ## CRUZET 4 ALCARECO (on CAF)
     ##
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/000690AE-E771-DD11-93D6-000423D996C8.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/002C355B-3372-DD11-A93C-000423D98DD4.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/00FD0190-7A71-DD11-A95F-000423D98844.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/0237113C-1F71-DD11-95B9-000423D8FA38.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/024501B6-CE71-DD11-88F6-000423D98930.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/043D4A5B-1E71-DD11-8C00-001D09F2441B.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/048E64E3-9371-DD11-8C2C-001D09F2AF1E.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/04E932C2-A471-DD11-8CEC-001D09F2924F.root',
-#    '/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/06075384-6171-DD11-975E-001617C3B65A.root' #,...
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/000690AE-E771-DD11-93D6-000423D996C8.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/002C355B-3372-DD11-A93C-000423D98DD4.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/00FD0190-7A71-DD11-A95F-000423D98844.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/0237113C-1F71-DD11-95B9-000423D8FA38.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/024501B6-CE71-DD11-88F6-000423D98930.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/043D4A5B-1E71-DD11-8C00-001D09F2441B.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/048E64E3-9371-DD11-8C2C-001D09F2AF1E.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/04E932C2-A471-DD11-8CEC-001D09F2924F.root',
+    #'/store/data/Commissioning08/Cosmics/ALCARECO/CRUZET4_V2_StreamTkAlCosmics0T_CRUZET4_v2/0006/06075384-6171-DD11-975E-001617C3B65A.root' #,...
     )
 )
+
 
 ##
 ## Maximum number of Events
@@ -134,21 +144,24 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(101)
 )
 
+
 ##
-## Output File Configuration
+## Output File Configuration (not used in DQM mode)
 ##
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('/afs/cern.ch/user/h/hauk/scratch0/rootFiles/AlignmentValidation.root')
-)
+#process.TFileService = cms.Service("TFileService",
+#    fileName = cms.string('AlignmentValidation.root')
+#)
+
 
 ##
 ## Load and Configure track selection for alignment
 ##
 process.load("Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi")
 process.AlignmentTrackSelector.src = "ALCARECOTkAlCosmicsCTF0T" #e.g. for cosmics ALCARECO
-####################process.AlignmentTrackSelector.src = "ALCARECOTkAlMuonIsolated" #e.g. for cosmics ALCARECO
+#process.AlignmentTrackSelector.src = "ALCARECOTkAlMuonIsolated" #e.g. for cosmics ALCARECO
 process.AlignmentTrackSelector.applyBasicCuts = False #True
 #process.AlignmentTrackSelector.ptMin   = .3
+
 
 ##
 ## Load and Configure TrackRefitter
@@ -156,56 +169,36 @@ process.AlignmentTrackSelector.applyBasicCuts = False #True
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
 #process.TrackRefitter.src = 'AlignmentTrackSelector'
 process.TrackRefitterP5.src = 'AlignmentTrackSelector'
-#process.TrackRefitter.TrajectoryInEvent = True # is default
-process.TrackRefitterP5.TTRHBuilder = 'WithTrackAngle'
-process.TrackRefitterP5.TrajectoryInEvent = True
 
 
 ##
-## Load and Configure OfflineValidation
+## Load and Configure TrackerOfflineValidation
 ##
 #process.load("Alignment.OfflineValidation.TrackerOfflineValidation_cfi")
-#process.TrackerOfflineValidation.useInDqmMode = True
-#process.TrackerOfflineValidation.Tracks = 'TrackRefitter'
-#process.TrackerOfflineValidation.trajectoryInput = 'TrackRefitter'
-##process.TrackerOfflineValidation.moduleLevelHistsTransient = True
-#process.TrackerOfflineValidation.TH1XprimeResStripModules.xmin = -0.2
-#process.TrackerOfflineValidation.TH1XprimeResStripModules.Nbinx = 100
-#process.TrackerOfflineValidation.TH1XprimeResStripModules.xmax = 0.2
-##
-#process.TrackerOfflineValidation.TH1XprimeResPixelModules.xmin = -0.1
-#process.TrackerOfflineValidation.TH1XprimeResPixelModules.Nbinx = 100
-#process.TrackerOfflineValidation.TH1XprimeResPixelModules.xmax = 0.1
-## Other used binnings you might want to replace:
-##process.TrackerOfflineValidation.TH1YResPixelModules
-##process.TrackerOfflineValidation.TH1NormYResPixelModules
-##process.TrackerOfflineValidation.TH1NormXprimeResPixelModules
-##process.TrackerOfflineValidation.TH1XResPixelModules
-##process.TrackerOfflineValidation.TH1NormXResPixelModules
-##process.TrackerOfflineValidation.TH1YResStripModules
-##process.TrackerOfflineValidation.TH1NormYResStripModules
-##process.TrackerOfflineValidation.TH1NormXprimeResStripModules
-##process.TrackerOfflineValidation.TH1XResStripModules
-##process.TrackerOfflineValidation.TH1NormXResStripModules
-
 process.load("Alignment.OfflineValidation.TrackerOfflineValidation_Dqm_cff")
 process.TrackerOfflineValidationDqm.Tracks = 'TrackRefitterP5'
 process.TrackerOfflineValidationDqm.trajectoryInput = 'TrackRefitterP5'
 
 
+##
+## Load and Configure TrackerOfflineValidationSummary
+##
 process.load("Alignment.OfflineValidation.TrackerOfflineValidationSummary_cfi")
 
 
-# DQM backend
+##
+## DQM backend
+##
 process.load("DQMServices.Core.DQM_cfg")
 
 
-# DQM file saver
+##
+## DQM file saver
+##
 process.dqmSaverMy = cms.EDFilter("DQMFileSaver",
           convention=cms.untracked.string("Offline"),
           workflow=cms.untracked.string("/Cosmics/TrackAlign_322patch2_R000100000_R000100050_v1/ALCARECO"),   # /primaryDatasetName/WorkflowDescription/DataTier; Current Convention: Indicate run range (first and last run) in file name 
-          dirName=cms.untracked.string("/afs/cern.ch/user/h/hauk/scratch0/rootFiles/."),
-	  #dirName=cms.untracked.string("."),
+	  dirName=cms.untracked.string("."),
           saveByRun=cms.untracked.int32(-1),
 	  saveAtJobEnd=cms.untracked.bool(True),                        
           forceRunNumber=cms.untracked.int32(100000)   # Current Convention: Take first processed run
