8e09ae9483845016f3161bfe991346f5fc4681ba
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-10-13 14:21:05 +0000

    took out the blank quotation marks

--- PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/electronIsolation_cff.py
@@ -4,8 +4,8 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaIsolationAlgos.eleIsoDeposits_cff import *
 from RecoEgamma.EgammaIsolationAlgos.eleIsoFromDeposits_cff  import *
 
-eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "")
-eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "")
+eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB")
+eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE")
 
 ## sequence to run on AOD 
 patElectronIsolation = cms.Sequence(
