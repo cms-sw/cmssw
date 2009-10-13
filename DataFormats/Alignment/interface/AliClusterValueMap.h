88e5354e1247dcd9f5c7164837e925624cb10edd
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-10-13 12:42:29 +0000

    clean-up, move typedef's to extra header to reduce dependencies (include the *Fwd.h where you would like to forward declare, but cannot since it's a typedef)

--- DataFormats/Alignment/interface/AliClusterValueMap.h
+++ DataFormats/Alignment/interface/AliClusterValueMap.h
@@ -1,11 +1,14 @@
-#ifndef CommonAlignment_AliHitAssoMapV_h
-#define CommonAlignment_AliHitAssoMapV_h
+#ifndef DataFormatsAlignment_AliClusterValueMap_h
+#define DataFormatsAlignment_AliClusterValueMap_h
 
 #include "DataFormats/Common/interface/ValueMap.h"
+// The content of the container:
 #include "DataFormats/Alignment/interface/AlignmentClusterFlag.h"
+// The typedef of the container:
+#include "DataFormats/Alignment/interface/AliClusterValueMapFwd.h"
 
-
-typedef edm::ValueMap<AlignmentClusterFlag >   AliClusterValueMap; 
-typedef edm::ValueMap<int >   AliTrackTakenClusterValueMap; // needed?
+// Still used in AlignmentPrescaler, but to be removed
+// (no dictionary generated).
+typedef edm::ValueMap<int> AliTrackTakenClusterValueMap;
 
 #endif
