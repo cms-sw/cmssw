31954c6cb370dcbfd0824ed091a25fdbef88afbf
Johannes Hauk <sha1-79adb14f6701f120a4bd8908168f6f2ed5256e2c@cern.ch>
2009-10-13 14:34:17 +0000

    Parameter "useFit" added

--- Alignment/OfflineValidation/python/TrackerOfflineValidationSummary_cfi.py
+++ Alignment/OfflineValidation/python/TrackerOfflineValidationSummary_cfi.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 TrackerOfflineValidationSummary = cms.EDFilter("TrackerOfflineValidationSummary",
    moduleDirectoryInOutput = cms.string("AlCaReco/TkAl"),  # has to be the same as in TrackerOfflineValidation_Dqm_cff
+   useFit                  = cms.bool(False),
 )
