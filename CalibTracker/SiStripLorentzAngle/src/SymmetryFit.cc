cf9d997e737bd5fc84f1d2cb1b20932fe12b949d
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-10-14 20:38:26 +0000

    Allow addition of multiple symmetry fits

--- CalibTracker/SiStripLorentzAngle/src/SymmetryFit.cc
+++ CalibTracker/SiStripLorentzAngle/src/SymmetryFit.cc
@@ -1,27 +1,39 @@
 #include "CalibTracker/SiStripLorentzAngle/interface/SymmetryFit.h"
 #include <cmath>
+#include "boost/foreach.hpp"
+
+TH1* SymmetryFit::symmetryChi2(std::string basename, const std::vector<TH1*>& candidates, const std::pair<unsigned,unsigned> range)
+{
+  TH1* fake = (TH1*)(candidates[0]->Clone(basename.c_str())); 
+  fake->Reset();
+  SymmetryFit combined(fake,range);
+  delete fake;
+
+  BOOST_FOREACH(const TH1* candidate, candidates) {
+    SymmetryFit sf(candidate,range); 
+    combined+=sf; 
+    delete sf.chi2_; 
+  }
+
+  int status = combined.fit();
+  if(status) { delete combined.chi2_; combined.chi2_=0;}
+  return combined.chi2_;
+}
 
 TH1* SymmetryFit::symmetryChi2(const TH1* candidate, const std::pair<unsigned,unsigned> range) 
 {
-  std::pair<unsigned, unsigned> usable = range;
-  while(usable.first  > 1                                  && candidate->GetBinError(usable.first-1)) --usable.first;
-  while(usable.second < (unsigned)(candidate->GetNbinsX()) && candidate->GetBinError(usable.second+1)) ++usable.second;
-  unsigned ndf = std::min(range.first-usable.first, usable.second-range.second);
-
-  if( ndf < 10 || ndf < (range.second-range.first)/2 ) { std::cout << "symm_chi2 bad range" << std::endl
-								   << "usable: " << usable.first <<","<<usable.second << std::endl
-								   << "range: " << range.first <<"," << range.second << std::endl; return 0;}
-
-  SymmetryFit sf(candidate, range, ndf);
+  SymmetryFit sf(candidate, range);
   int status = sf.fit();
   if(status) { delete sf.chi2_; sf.chi2_=0; }
   return sf.chi2_;
 }
 
-SymmetryFit::SymmetryFit(const TH1* h, const std::pair<unsigned,unsigned> r, const unsigned ndf)
+SymmetryFit::SymmetryFit(const TH1* h, const std::pair<unsigned,unsigned> r)
   : symm_candidate_(h), 
+    minDF_(10),
     range_(r),
-    ndf_(ndf),
+    minmaxUsable_(findUsableMinMax()),
+    ndf_(minmaxUsable_.second-minmaxUsable_.first+1),
     chi2_(0)
 {
   makeChi2Histogram();
@@ -38,8 +50,31 @@ void SymmetryFit::makeChi2Histogram()
   chi2_ = new TH1F(XXname.c_str(),"", Nbins, low, up);
 }
 
+std::pair<unsigned,unsigned> SymmetryFit::findUsableMinMax()
+{
+  std::pair<unsigned, unsigned> bestL(0,0), bestR(0,0),test(0,0), notfound(0,0);
+  unsigned bins = symm_candidate_->GetNbinsX();
+  for(unsigned i = 1; i <= bins+1; i++) {
+    float err = symm_candidate_->GetBinError(i);
+    if( !test.first && err ) test.first = i;
+    if( test.first && !test.second && (!err||i==bins+1) ) {
+      test.second = i-1;
+      if( test.first  < range_.first-minDF_  && (test.second-test.first) > (bestL.second-bestL.first) ) bestL = test;
+      if( test.second > range_.second+minDF_ && (test.second-test.first) > (bestR.second-bestR.first) ) bestR = test;
+      test.first=test.second=0;
+    }
+  }
+  if( bestL == notfound || bestR == notfound ) return std::make_pair(0,0);
+  
+  return std::make_pair( std::max( bestL.second > range_.second-1 ? 0 : range_.second-1-bestL.second,
+				   bestR.first < range_.first+1 ? 0 : bestR.first-(range_.first+1)),
+			 std::min( range_.first-1-bestL.first, bestR.second+1-range_.second) );
+}
+
 void SymmetryFit::fillchi2()
 {
+  if(ndf_<minDF_) return;
+
   for(int i=1;  i<=chi2_->GetNbinsX() ; ++i) {
     const unsigned L( range_.first-1+(i-1)/2 ), R( range_.first+i/2 );
     chi2_->SetBinContent( i, chi2( std::make_pair(L,R)) );
@@ -48,6 +83,8 @@ void SymmetryFit::fillchi2()
 
 float SymmetryFit::chi2(std::pair<unsigned,unsigned> point)
 {
+  point.first -= minmaxUsable_.first;
+  point.second += minmaxUsable_.first;
   float XX=0;
   unsigned i=ndf_;
   while(i-->0) {
