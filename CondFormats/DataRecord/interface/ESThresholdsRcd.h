969a459bbbb299b76872cfc9b327e365bdd6fdf3
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-10-14 22:28:45 +0000

    first entry - conditions for Ecal preshower

---
+++ CondFormats/DataRecord/interface/ESThresholdsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_ESThresholds_H
+#define CondFormats_DataRecord_ESThresholds_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class ESThresholdsRcd : public edm::eventsetup::EventSetupRecordImplementation<ESThresholdsRcd> {};
+#endif
