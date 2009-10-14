1572fc2d896b7c1fbcce0c3af29eee7ff74fa44d
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2009-10-14 16:17:28 +0000

    fix typo in X322Y5000 sample

--- IOMC/EventVertexGenerators/python/VtxSmearedEarly10TeVX322Y5000_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedEarly10TeVX322Y5000_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 VtxSmeared = cms.EDFilter("BetafuncEvtVtxGenerator",
-    Early10TeVCollisionVtxSmearingParameters,
+    Early10TeVX322Y5000VtxSmearingParameters,
     VtxSmearedCommon
 )
 
