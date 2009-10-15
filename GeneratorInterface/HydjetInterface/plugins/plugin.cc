e92e549a2e14f6ead770d259b51302159122ec13
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2009-10-15 11:32:35 +0000

    Common HI tools moved to HiGenCommon

--- GeneratorInterface/HydjetInterface/plugins/plugin.cc
+++ GeneratorInterface/HydjetInterface/plugins/plugin.cc
@@ -1,17 +1,9 @@
 //
-// $Id: plugin.cc,v 1.3 2009/05/20 23:15:30 yilmaz Exp $
+// $Id: plugin.cc,v 1.4 2009/05/28 18:54:03 yilmaz Exp $
 //
 
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-//#include "GeneratorInterface/HydjetInterface/interface/HydjetSource.h"
-//#include "GeneratorInterface/HydjetInterface/interface/HydjetProducer.h"
 #include "GeneratorInterface/HydjetInterface/interface/HydjetGeneratorFilter.h"
-
-//using edm::HydjetSource;
-//using edm::HydjetProducer;
 using gen::HydjetGeneratorFilter;
-
-//DEFINE_FWK_INPUT_SOURCE(HydjetSource);
-//DEFINE_FWK_MODULE(HydjetProducer);
 DEFINE_FWK_MODULE(HydjetGeneratorFilter);
