3e2cb7208a790dd57e1c38553f498ab5910d22b2
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2009-10-15 13:16:01 +0000

    details

--- Validation/RecoEgamma/python/egammaValidation_cff.py
+++ Validation/RecoEgamma/python/egammaValidation_cff.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoEgamma.ElectronMcSignalValidator_cfi import *
 from Validation.RecoEgamma.ElectronMcFakeValidator_cfi import *
 from Validation.RecoEgamma.photonValidationSequence_cff import *
-recoEgammaValidation = cms.Sequence(electronMcSignalValidator+electronMcFakeValidator+photonValidationSequence)
+egammaValidation = cms.Sequence(electronMcSignalValidator+electronMcFakeValidator+photonValidationSequence)
