5a490370e2b9d126293fb663d5279b4b3140abc4
Yeong-Jyi Lei <sha1-4336116f62c995f0b2b10f48d64d3d17454deeff@cern.ch>
2009-10-15 13:48:08 +0000

    Fix bug

--- DQM/EcalPreshowerMonitorModule/python/ESDaqInfoTask_cfi.py
+++ DQM/EcalPreshowerMonitorModule/python/ESDaqInfoTask_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-ecalEndcapDaqInfoTask = cms.EDAnalyzer("ESDaqInfoTask",
+ecalPreshowerDaqInfoTask = cms.EDAnalyzer("ESDaqInfoTask",
       esMapping = cms.PSet(LookupTable = cms.untracked.FileInPath("EventFilter/ESDigiToRaw/data/ES_lookup_table.dat")),
       prefixME = cms.untracked.string('EcalPreshower'),
       enableCleanup = cms.untracked.bool(False),
