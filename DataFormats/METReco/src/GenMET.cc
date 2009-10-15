43db009c7060fe35c76590cc36c663e4666f0ea3
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-10-15 17:48:53 +0000

    Initiailzed variables in the default constructor

--- DataFormats/METReco/src/GenMET.cc
+++ DataFormats/METReco/src/GenMET.cc
@@ -2,6 +2,17 @@
 
 using namespace reco;
 
+GenMET::GenMET()
+{
+  gen_data.NeutralEMEtFraction     = 0.0;
+  gen_data.NeutralHadEtFraction    = 0.0;
+  gen_data.ChargedEMEtFraction     = 0.0;
+  gen_data.ChargedHadEtFraction    = 0.0;
+  gen_data.MuonEtFraction          = 0.0;
+  gen_data.InvisibleEtFraction     = 0.0;
+
+}
+
 bool GenMET::overlap( const Candidate & ) const 
 {
   return false;
