30cfe7ed852437a1b5c2d1c1c07f8007584c6fb3
Charles Plager <sha1-ca639ee8adf7ee0915fef7e2e829d2fa8d50ba78@cern.ch>
2009-10-15 15:19:46 +0000

    annotated Z Peak example

--- DataFormats/FWLite/examples/patZpeak.py
+++ DataFormats/FWLite/examples/patZpeak.py
@@ -4,32 +4,39 @@ import ROOT
 import sys
 from DataFormats.FWLite import Events, Handle
 
+# Make VarParsing object
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideAboutPythonConfigFile#VarParsing_Example
 from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing ('python')
 options.parseArguments()
 
-ROOT.gSystem.Load("libPhysicsToolsObjectSelectors")
-reg = ROOT.objsel.Registry()
-eventSelector = ROOT.objsel.WZEventSelector(reg)
-eventSelector.setZbosonPoint();
+# Events takes either
+# - single file name
+# - list of file names
+# - VarParsing options
 
-
+# use Varparsing object
 events = Events (options)
+
+# create handle outside of loop
 handle  = Handle ("std::vector<pat::Muon>")
 
-ROOT.gROOT.SetBatch()
-ROOT.gROOT.SetStyle('Plain')
+# for now, label is just a tuple of strings that is initialized just
+# like and edm::InputTag
+label = ("selectedLayer1Muons")
+
+# Create histograms, etc.
+ROOT.gROOT.SetBatch()        # don't pop up canvases
+ROOT.gROOT.SetStyle('Plain') # white background
 zmassHist = ROOT.TH1F ("zmass", "Z Candidate Mass", 50, 20, 220)
 
-# # Allows me to run everything up to here and then get an interactive
-# # prompt if I run with 'python -i scriptName.py'
-# raise RuntimeError, "hi mom"
-
+# loop over events
 for event in events:
-    reg.setEvent (event.object())
-    if not eventSelector.passes( event.object() ):
-        continue
-    muons = eventSelector.tightMuonVec()
+    # use getByLabel, just like in cmsRun
+    event.getByLabel (label, handle)
+    # get the product
+    muons = handle.product()
+    # use muons to make Z peak
     numMuons = len (muons)
     if muons < 2: continue
     for outer in xrange (numMuons - 1):
@@ -44,6 +51,7 @@ for event in events:
                                            outerMuon.pz(), outerMuon.energy())
             zmassHist.Fill( (inner4v + outer4v).M() )
 
+# make a canvas, draw, and save it
 c1 = ROOT.TCanvas()
 zmassHist.Draw()
 c1.Print ("zmass_py.png")
