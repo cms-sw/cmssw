d2056ccbe1b14f057ce9bf1deb05720d5ebcdbdc
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-10-15 21:34:01 +0000

    Remove obsolute header file

--- SimCalorimetry/EcalTestBeam/src/SealModule.cc
+++ SimCalorimetry/EcalTestBeam/src/SealModule.cc
@@ -1,12 +1,11 @@
 /*  
- *  $Id: SealModule.cc,v 1.3 2009/06/15 19:47:00 heltsley Exp $
+ *  $Id: SealModule.cc,v 1.4 2009/09/11 17:37:39 heltsley Exp $
  */
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "SimCalorimetry/EcalTestBeam/interface/EcalTBDigiProducer.h"
-#include "SimCalorimetry/EcalTestBeam/interface/EETBDigiProducer.h"
 
 DEFINE_SEAL_MODULE () ;
 
