988fa0cb60d5a0d3ff06b02450929a16e55b6393
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2009-10-16 16:04:36 +0000

    added ttu

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -6,3 +6,5 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RPCMonitorDigi);
 #include "DQM/RPCMonitorDigi/interface/RPCEfficiency.h"
 DEFINE_ANOTHER_FWK_MODULE(RPCEfficiency);
+#include "DQM/RPCMonitorDigi/interface/RPCTTUMonitor.h"
+DEFINE_ANOTHER_FWK_MODULE(RPCTTUMonitor);
