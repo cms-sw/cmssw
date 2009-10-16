98a13cade01f6aeabbf9caffa1841cce5c6eee1f
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-10-16 16:37:02 +0000

    this is filled now with a few necessary comments for the user...

--- PhysicsTools/PatAlgos/python/tools/cmsswVersionTools.py
+++ PhysicsTools/PatAlgos/python/tools/cmsswVersionTools.py
@@ -15,6 +15,22 @@ def run33xOn31xMC(process,
     jetID   : jet ID to make
     ------------------------------------------------------------------    
     """
+    print "*********************************************************************"
+    print "NOTE TO USER: when running on 31X samples with this CMSSW version    "
+    print "              of PAT the collection label for anti-kt has to be      "
+    print "              switched from \'ak*\' to \'antikt*\'. This is going    "
+    print "              to be done now. Also note that the *JetId collections  "
+    print "              are not stored on these input files in contrary to     "
+    print "              input files in 33X. Please use the _addJetId_ tool     "
+    print "              as described on SWGuidePATTools, when adding new jet   "
+    print "              collections! Such a line could look like this:         "
+    print ""
+    print "  addJetID( process, \"sisCone5CaloJets\", \"sc5\")"
+    print "  from PhysicsTools.PatAlgos.tools.jetTools import *"
+    print "  addJetCollection(process,cms.InputTag('sisCone5CaloJets'),"
+    print "  ..."
+    print "  )"
+    print "*********************************************************************"
     addJetID( process, jetSrc, jetIdTag )
     # in PAT (iterativeCone5) to ak5 (anti-kt cone = 0.5)
     switchJetCollection(process, 
