58aa54866ece47838651e6cb0c26878f591126e0
Piotr Traczyk <sha1-f63c8ead76e5e4a129ecffbbd6f7786205ea2389@cern.ch>
2009-10-16 09:03:28 +0000

    Added CSC/DT/ECAL combination

--- RecoMuon/MuonIdentification/python/MuonTimingFiller_cfi.py
+++ RecoMuon/MuonIdentification/python/MuonTimingFiller_cfi.py
@@ -6,7 +6,14 @@ from RecoMuon.MuonIdentification.CSCTimingExtractor_cfi import *
 TimingFillerBlock = cms.PSet(
   TimingFillerParameters = cms.PSet(
     DTTimingExtractorBlock,
-    CSCTimingExtractorBlock
+    CSCTimingExtractorBlock,
+    
+    # Single hit time measurement precition in ns
+    ErrorDT  = cms.double(3.1),
+    ErrorCSC = cms.double(7.),
+    
+    # Ecal minimum energy cut
+    EcalEnergyCut = cms.double(0.4)
   )
 )
 
