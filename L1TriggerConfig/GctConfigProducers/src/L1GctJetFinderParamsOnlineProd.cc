6244c30331fa4efaad569e59cfab8aaa34b4c6bb
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-10-16 15:19:52 +0000

    partial implementation of jet coeffs o2o

--- L1TriggerConfig/GctConfigProducers/src/L1GctJetFinderParamsOnlineProd.cc
+++ L1TriggerConfig/GctConfigProducers/src/L1GctJetFinderParamsOnlineProd.cc
@@ -29,6 +29,7 @@ L1GctJetFinderParamsOnlineProd::newObject( const std::string& objectKey )
   columns.push_back( "GCT_MHT_JET_ET_THRESHOLD" );
   columns.push_back( "GCT_TAU_ISO_ET_THRESHOLD" );
   columns.push_back( "GCT_CEN_JET_ETA_MAX" );
+  columns.push_back( "GCT_JET_CORR_KEY" );
   
   l1t::OMDSReader::QueryResults results =
     m_omdsReader.basicQuery(
@@ -44,9 +45,6 @@ L1GctJetFinderParamsOnlineProd::newObject( const std::string& objectKey )
       return boost::shared_ptr< L1GctJetFinderParams >() ;
    }
 
-   // TODO - get calibration coefficients
-   
-
    // fill values
    double rgnEtLsb=0.;
    double htLsb=0.;
@@ -57,11 +55,12 @@ L1GctJetFinderParamsOnlineProd::newObject( const std::string& objectKey )
    double htJetEtThresh=0.;
    double mhtJetEtThresh=0.;
    short int etaBoundary=7;
-   unsigned corrType=0;
+   int corrType=0;
    std::vector< std::vector<double> > jetCorrCoeffs(11);
    std::vector< std::vector<double> > tauCorrCoeffs(7);
    bool convertToEnergy=false;            // Not in OMDS
    std::vector<double> energyConvCoeffs(11);  // Not in OMDS
+   std::string jetCorrKey;
 
    results.fillVariable( "GCT_RGN_ET_LSB", rgnEtLsb );
    results.fillVariable( "GCT_HT_LSB", htLsb );
@@ -72,6 +71,112 @@ L1GctJetFinderParamsOnlineProd::newObject( const std::string& objectKey )
    results.fillVariable( "GCT_HT_JET_ET_THRESHOLD", htJetEtThresh );
    results.fillVariable( "GCT_MHT_JET_ET_THRESHOLD", mhtJetEtThresh );
    results.fillVariable( "GCT_CEN_JET_ETA_MAX", etaBoundary );
+   results.fillVariable( "GCT_JET_CORR_KEY", jetCorrKey );
+
+   edm::LogInfo("L1-O2O") << "L1 jet corrections key : " << jetCorrKey << std::endl;
+
+   // get jet corr coefficients
+   std::vector< std::string > jetCorrColumns;
+   jetCorrColumns.push_back( "GCT_JETCORR_TYPE" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_10" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_9" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_8" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_7" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_6" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_5" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_4" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_3" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_2" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_1" );
+   jetCorrColumns.push_back( "GCT_JETCORR_NETA_0" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_0" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_1" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_2" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_3" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_4" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_5" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_6" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_7" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_8" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_9" );
+//    jetCorrColumns.push_back( "GCT_JETCORR_PETA_10" );
+
+   l1t::OMDSReader::QueryResults jetCorrResults =
+    m_omdsReader.basicQuery(
+			    jetCorrColumns,
+			    "CMS_GCT",
+			    "GCT_JET_CORRECTIONS",
+			    "GCT_JET_CORRECTIONS.CONFIG_KEY",
+			    m_omdsReader.singleAttribute( jetCorrKey ) ) ;
+
+   if( jetCorrResults.queryFailed() ) // check if query was successful
+   {
+      edm::LogError( "L1-O2O" ) << "Problem getting L1 jet corrections" ;
+      return boost::shared_ptr< L1GctJetFinderParams >() ;
+   }
+
+   // fill jet corr type
+   jetCorrResults.fillVariable( "GCT_JETCORR_TYPE", corrType );
+
+   edm::LogInfo("L1-O2O") << "L1 jet corrections type : " << corrType << std::endl;
+
+   // get coefficients
+   for (unsigned i=0; i < L1GctJetFinderParams::NUMBER_ETA_VALUES; ++i) {
+
+     // get corr key for eta value
+     std::stringstream etaCol;
+     etaCol << "GCT_JETCORR_NETA_" << std::dec << i;
+     std::string etaKey;
+     jetCorrResults.fillVariable( etaCol.str(), etaKey );
+
+     std::vector< std::string > coeffColumns;
+     coeffColumns.push_back( "GCT_JETCORR_C0" );
+     coeffColumns.push_back( "GCT_JETCORR_C1" );
+     coeffColumns.push_back( "GCT_JETCORR_C2" );
+     coeffColumns.push_back( "GCT_JETCORR_C3" );
+     coeffColumns.push_back( "GCT_JETCORR_C4" );
+     coeffColumns.push_back( "GCT_JETCORR_C5" );
+     coeffColumns.push_back( "GCT_JETCORR_C6" );
+     coeffColumns.push_back( "GCT_JETCORR_C7" );
+     coeffColumns.push_back( "GCT_JETCORR_C8" );
+     coeffColumns.push_back( "GCT_JETCORR_C9" );
+     coeffColumns.push_back( "GCT_JETCORR_C10" );
+     coeffColumns.push_back( "GCT_JETCORR_C11" );
+     coeffColumns.push_back( "GCT_JETCORR_C12" );
+     coeffColumns.push_back( "GCT_JETCORR_C13" );
+     coeffColumns.push_back( "GCT_JETCORR_C14" );
+     coeffColumns.push_back( "GCT_JETCORR_C15" );
+     coeffColumns.push_back( "GCT_JETCORR_C16" );
+     coeffColumns.push_back( "GCT_JETCORR_C17" );
+     coeffColumns.push_back( "GCT_JETCORR_C18" );
+     coeffColumns.push_back( "GCT_JETCORR_C19" );
+     
+     l1t::OMDSReader::QueryResults jetCorrResults =
+       m_omdsReader.basicQuery(
+			       coeffColumns,
+			       "CMS_GCT",
+			       "GCT_JET_COEFFS",
+			       "GCT_JET_COEFFS.CONFIG_KEY",
+			       m_omdsReader.singleAttribute( etaKey ) ) ;
+     
+     if( results.queryFailed() ) // check if query was successful
+       {
+	 edm::LogError( "L1-O2O" ) << "Problem getting L1 jet correction coefficients" ;
+	 return boost::shared_ptr< L1GctJetFinderParams >() ;
+       }
+
+     // fill coeffs - TODO
+     std::vector<double> coeffs;
+
+
+     jetCorrCoeffs.push_back(coeffs);
+    
+     // copy to tau coeffs
+     if (i < L1GctJetFinderParams::N_CENTRAL_ETA_VALUES) tauCorrCoeffs.push_back(coeffs);
+     
+   }
+   
+   
 
    return boost::shared_ptr< L1GctJetFinderParams >( 
 						    new L1GctJetFinderParams( rgnEtLsb,
