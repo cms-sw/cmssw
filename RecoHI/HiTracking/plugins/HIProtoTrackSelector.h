0c29802eaabe4e18a804f02b8afbd1a04953d604
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2009-10-16 14:59:16 +0000

    implemented beamspot selection on prototracks

--- RecoHI/HiTracking/plugins/HIProtoTrackSelector.h
+++ RecoHI/HiTracking/plugins/HIProtoTrackSelector.h
@@ -7,6 +7,9 @@
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include <algorithm>
@@ -17,103 +20,124 @@ using namespace edm;
 
 /**
  Selector to select prototracks that pass certain kinematic cuts based on fast vertex
- 
- Inspired by CommonTools.RecoAlgos.TrackingParticleSelector.h 
- and SimTracker.TrackHistory.BTrackingParticleSelector.h
- and UserCode.CmsHi.TrackAnalysis.HitPixelLayersTPSelector.h
- and https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideGenericSelectors#Generic_Object_Selector_and_User
- **/
+**/
 
 class HIProtoTrackSelector
+{
+  
+ public:
+  // input collection type
+  typedef reco::TrackCollection collection;
+  
+  // output collection type
+  typedef vector<const reco::Track *> container;
+  
+  // iterator over result collection type.
+  typedef container::const_iterator const_iterator;
+  
+  // constructor from parameter set configurability
+  HIProtoTrackSelector(const edm::ParameterSet & iConfig) : 
+    vertexCollection_(iConfig.getParameter<InputTag>("VertexCollection")),
+    beamSpotLabel_(iConfig.getParameter<InputTag>("beamSpotLabel")),
+    ptMin_(iConfig.getParameter<double>("ptMin")), 	
+    nSigmaZ_(iConfig.getParameter<double>("nSigmaZ")),
+    minZCut_(iConfig.getParameter<double>("minZCut")),
+    maxD0Significance_(iConfig.getParameter<double>("maxD0Significance"))
+    {};
+  
+  // select object from a collection and possibly event content
+  void select( edm::Handle<reco::TrackCollection>& TCH, const edm::Event & iEvent, const edm::EventSetup & iSetup)
+    {
+      selected_.clear();
+      
+      const collection & c = *(TCH.product());
+      
+      // Get fast reco vertex 
+      edm::Handle<reco::VertexCollection> vc;
+      iEvent.getByLabel(vertexCollection_, vc);
+      const reco::VertexCollection * vertices = vc.product();
+      
+      math::XYZPoint vtxPoint(0.0,0.0,0.0);
+      double vzErr =0.0;
+      
+      if(vertices->size()>0) {
+	vtxPoint=vertices->begin()->position();
+	vzErr=vertices->begin()->zError();
+	LogInfo("HeavyIonVertexing") << "Select prototracks compatible with median vertex"
+				     << "\n   vz = " << vtxPoint.Z()  
+				     << "\n   " << nSigmaZ_ << " vz sigmas = " << vzErr*nSigmaZ_
+				     << "\n   cut at = " << max(vzErr*nSigmaZ_,minZCut_);
+      } else {
+	LogError("HeavyIonVertexing") << "No vertex found in collection '" << vertexCollection_ << "'";
+      }
+      
+      // Get beamspot
+      reco::BeamSpot beamSpot;
+      edm::Handle<reco::BeamSpot> beamSpotHandle;
+      iEvent.getByLabel(beamSpotLabel_, beamSpotHandle);
+      
+      math::XYZPoint bsPoint(0.0,0.0,0.0);
+      double bsWidth = 0.0;
+      
+      if ( beamSpotHandle.isValid() ) {
+	beamSpot = *beamSpotHandle;
+	bsPoint = beamSpot.position();
+	bsWidth = sqrt(beamSpot.BeamWidthX()*beamSpot.BeamWidthY());
+	LogInfo("HeavyIonVertexing") << "Select prototracks compatible with beamspot"
+				     << "\n   (x,y,z) = (" << bsPoint.X() << "," << bsPoint.Y() << "," << bsPoint.Z() << ")"  
+				     << "\n   width = " << bsWidth
+				     << "\n   cut at d0/d0sigma = " << maxD0Significance_;
+      } else {
+	edm::LogError("HeavyIonVertexing") << "No beam spot available from '" << beamSpotLabel_ << "'\n";
+      }
+      
+      
+      // Do selection
+      int nSelected=0;
+      int nRejected=0;
+      double d0=0.0; 
+      double d0sigma=0.0;
+      for (reco::TrackCollection::const_iterator trk = c.begin(); trk != c.end(); ++ trk)
 	{
-		
-	public:
-		// input collection type
-		typedef reco::TrackCollection collection;
-		
-		// output collection type
-		typedef vector<const reco::Track *> container;
-		
-		// iterator over result collection type.
-		typedef container::const_iterator const_iterator;
-		
-		// constructor from parameter set configurability
-		HIProtoTrackSelector(const edm::ParameterSet & iConfig) : 
-		vertexCollection_(iConfig.getParameter<string>("VertexCollection")),
-		ptMin_(iConfig.getParameter<double>("ptMin")), 	
-		nSigmaZ_(iConfig.getParameter<double>("nSigmaZ")),
-		minZCut_(iConfig.getParameter<double>("minZCut")),
-		maxD0Significance_(iConfig.getParameter<double>("maxD0Significance"))
-		{};
-		
-		// select object from a collection and possibly event content
-		void select( edm::Handle<reco::TrackCollection>& TCH, const edm::Event & iEvent, const edm::EventSetup & iSetup)
-		{
-			selected_.clear();
-			
-			const collection & c = *(TCH.product());
-			
-			// Get fast reco vertex 
-			edm::Handle<reco::VertexCollection> vc;
-			iEvent.getByLabel(vertexCollection_, vc);
-			const reco::VertexCollection * vertices = vc.product();
-			
-			if(vertices->size()>0) {
-				vtx_=vertices->begin()->position();
-				vzErr_=vertices->begin()->zError();
-				LogInfo("HeavyIonVertexing") << "Select prototracks compatible with median vertex"
-				<< "\n   vz = " << vtx_.Z()  
-				<< "\n   " << nSigmaZ_ << " vz sigmas = " << vzErr_*nSigmaZ_
-				<< "\n   cut at = " << max(vzErr_*nSigmaZ_,minZCut_);
-			} else {
-				LogError("HeavyIonVertexing") << "No vertex found in collection '" << vertexCollection_ << "'";
-			}
-			
-			int nSelected=0;
-			int nRejected=0;
-			for (reco::TrackCollection::const_iterator trk = c.begin(); trk != c.end(); ++ trk)
-			{
-				
-				if ( trk->pt() > ptMin_ && // keep only tracks above ptMin
-					 fabs(trk->d0()/trk->d0Error()) < maxD0Significance_ && // keep only tracks with D0 significance less than cut
-					 fabs(trk->dz(vtx_)) <  max(vzErr_*nSigmaZ_,minZCut_) // keep all tracks within minZCut or within nSigmaZ zErrors of fast vertex
-					) 
-				{
-					//LogTrace("HeavyIonVertexing") << "SELECTED: dz=" << trk->dz(vtx_) << "\t d0/d0err=" << trk->d0()/trk->d0Error() << "\t pt=" << trk->pt();
-					nSelected++;
-					selected_.push_back( & * trk );
-				} else {
-					//LogTrace("HeavyIonVertexing") << "\t REJECTED: dz=" << trk->dz(vtx_) << "\t d0/d0err=" << trk->d0()/trk->d0Error() << "\t pt=" << trk->pt();
-					nRejected++;
-				}
-				
-			}
-			
-			LogInfo("HeavyIonVertexing") << "selected " << nSelected << " prototracks out of " << nRejected+nSelected << endl;
-			
-		}
-		
-		// iterators over selected objects: collection begin
-		const_iterator begin() const { return selected_.begin(); }
-		
-		// iterators over selected objects: collection end
-		const_iterator end() const { return selected_.end(); }
-		
-		// true if no object has been selected
-		size_t size() const { return selected_.size(); }
-		
-		
-private:
-		container selected_;		
-		string vertexCollection_; 
-		double ptMin_; 
-		double nSigmaZ_;
-		double minZCut_; 
-		math::XYZPoint vtx_;
-		double vzErr_;
-		double maxD0Significance_;
-		
-	};
+	  
+	  d0 = -1.*trk->dxy(bsPoint);
+	  d0sigma = sqrt(trk->d0Error()*trk->d0Error() + bsWidth*bsWidth);
+	  if ( trk->pt() > ptMin_ // keep only tracks above ptMin
+	       && fabs(d0/d0sigma) < maxD0Significance_ // keep only tracks with D0 significance less than cut
+	       && fabs(trk->dz(vtxPoint)) <  max(vzErr*nSigmaZ_,minZCut_) // within minZCut, nSigmaZ of fast vertex
+	       ) 
+	  {
+	    nSelected++;
+	    selected_.push_back( & * trk );
+	  } 
+	  else 
+	    nRejected++;
+	}
+      
+      LogInfo("HeavyIonVertexing") << "selected " << nSelected << " prototracks out of " << nRejected+nSelected << endl;
+      
+    }
+  
+  // iterators over selected objects: collection begin
+  const_iterator begin() const { return selected_.begin(); }
+  
+  // iterators over selected objects: collection end
+  const_iterator end() const { return selected_.end(); }
+  
+  // true if no object has been selected
+  size_t size() const { return selected_.size(); }
+  
+  
+ private:
+  container selected_;		
+  InputTag vertexCollection_; 
+  InputTag beamSpotLabel_;
+  double ptMin_; 
+  double nSigmaZ_;
+  double minZCut_; 
+  double maxD0Significance_;
+  
+};
 
 
 #endif
--- RecoHI/HiTracking/python/HISelectedProtoTracks_cfi.py
+++ RecoHI/HiTracking/python/HISelectedProtoTracks_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 hiSelectedProtoTracks = cms.EDFilter("HIProtoTrackSelection",
     src = cms.InputTag("hiPixel3ProtoTracks"),
-	VertexCollection = cms.string("hiPixelMedianVertex"),
+	VertexCollection = cms.InputTag("hiPixelMedianVertex"),
+        beamSpotLabel = cms.InputTag("offlineBeamSpot"),
 	ptMin = cms.double(1.0),
 	nSigmaZ = cms.double(5.0),
 	minZCut = cms.double(0.2),
