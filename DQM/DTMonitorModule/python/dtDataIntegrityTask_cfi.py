3ce4d94a81fd04f5614a32b404ab2fe3aa2639c6
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2009-10-19 15:52:24 +0000

    introduce dedicated mode for offline

--- DQM/DTMonitorModule/python/dtDataIntegrityTask_cfi.py
+++ DQM/DTMonitorModule/python/dtDataIntegrityTask_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 DTDataIntegrityTask = cms.Service("DTDataIntegrityTask",
                                   getSCInfo = cms.untracked.bool(True),
-                                  hltMode = cms.untracked.bool(False)
+                                  hltMode = cms.untracked.bool(False),
+                                  offlineMode = cms.untracked.bool(False)
 )
 
 
