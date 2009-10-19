c4550908a6068d3313554a1de2f72cbeac2f6738
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-10-19 15:22:58 +0000

    Added records for magnetic-field-dependent local reco templates (0,3.8,4)

---
+++ CondFormats/DataRecord/src/SiPixelTemplateDBObject38TRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiPixelTemplateDBObject38TRcd
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      
+// Created:     Mon Sep 28 15:40:47 CEST 2009
+// $Id$
+
+#include "CondFormats/DataRecord/interface/SiPixelTemplateDBObject38TRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(SiPixelTemplateDBObject38TRcd);
