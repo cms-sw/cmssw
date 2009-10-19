484d86f3c1cb9a1ecac0734cffc8e7cc49dabb98
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-10-19 11:45:22 +0000

    add sistrip local reco

---
+++ DQM/HLTEvF/python/HLTLocalRecoSiStrip_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from DQM.SiStripMonitorTrack.SiStripMonitorMuonHLT_cfi import *
+
+hltHighLevelSiStrip = cms.EDFilter("HLTHighLevel",
+    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+    HLTPaths = cms.vstring('HLT_L1MuOpen','HLT_L1Mu', 'HLT_L1Mu20',
+    			'HLT_L2Mu9','HLT_L2Mu11',
+			'HLT_Mu3', 'HLT_Mu5', 'HLT_Mu9'),
+    eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
+    andOr = cms.bool(True), # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
+    throw = cms.bool(True)    # throw exception on unknown path names
+)
+
+hltLocalRecoSiStrip = cms.Path(hltHighLevelSiStrip*sistripMonitorMuonHLT)
+
