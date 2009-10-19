56a000da2406f93aa014d11ed0389dabfac5b875
Mario Pelliccioni <sha1-6718ce1a78c0cdb74a96323fbe0c3f1949046cf2@cern.ch>
2009-10-19 13:16:04 +0000

    minor cleanups

--- DQM/DTMonitorModule/test/DTkFactValidation_2_DQM_TEMPL_cfg.py
+++ DQM/DTMonitorModule/test/DTkFactValidation_2_DQM_TEMPL_cfg.py
@@ -6,27 +6,71 @@ process.options = cms.untracked.PSet(
  fileMode = cms.untracked.string('FULLMERGE')
 )
 
-
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Geometry.DTGeometry.dtGeometry_cfi")
 process.DTGeometryESModule.applyAlignment = False
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 process.load("DQMServices.Core.DQM_cfg")
-process.load("RecoLocalMuon.Configuration.RecoLocalMuonCosmics_cff")
-
 
 process.source = cms.Source("PoolSource",
     processingMode = cms.untracked.string("RunsLumisAndEvents"),
     fileNames = cms.untracked.vstring(
-	'file:/data/maselli/31X/Run67838/Ttrig/Validation/crab_0_090515_132857/res/DQM_10.root',
-	'file:/data/maselli/31X/Run67838/Ttrig/Validation/crab_0_090515_132857/res/DQM_11.root',
-	'file:/data/maselli/31X/Run67838/Ttrig/Validation/crab_0_090515_132857/res/DQM_12.root',
-	'file:/data/maselli/31X/Run67838/Ttrig/Validation/crab_0_090515_132857/res/DQM_13.root',
-	'file:/data/maselli/31X/Run67838/Ttrig/Validation/crab_0_090515_132857/res/DQM_14.root'
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_1.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_10.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_11.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_12.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_13.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_14.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_15.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_16.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_17.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_18.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_19.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_2.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_20.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_21.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_22.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_23.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_24.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_25.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_26.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_27.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_28.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_29.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_3.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_30.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_31.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_32.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_33.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_34.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_35.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_36.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_37.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_38.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_39.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_4.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_40.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_41.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_42.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_43.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_44.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_45.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_46.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_47.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_48.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_49.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_5.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_50.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_51.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_6.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_7.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_8.root',
+	'file:/afs/cern.ch/cms/CAF/CMSALCA/ALCA_MUONCALIB/DTCALIB/Mario/test/Run110440/Ttrig/Validation/crab_0_091015_181621/res/DQM_9.root'
+
+
     )
 )
-
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -44,9 +88,6 @@ process.MessageLogger = cms.Service("MessageLogger",
         default = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
         ),
-        FwkJob = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
         resolution = cms.untracked.PSet(
             limit = cms.untracked.int32(10000000)
         ),
@@ -61,23 +102,11 @@ process.qTester = cms.EDFilter("QualityTester",
     qtList = cms.untracked.FileInPath('DQM/DTMonitorClient/test/QualityTests_ttrig.xml')
 )
 
-process.load("DQM.DTMonitorClient.dtResolutionTest_cfi")
-process.modulo1=process.resolutionTest.clone()
-process.modulo1.histoTag2D = 'hResDistVsDist_STEP1' 
-process.modulo1.histoTag  = 'hResDist_STEP1'
-process.modulo1.STEP = 'STEP1'
-
-process.load("DQM.DTMonitorClient.dtResolutionTest_cfi")
-process.modulo2=process.resolutionTest.clone()
-process.modulo2.histoTag2D = 'hResDistVsDist_STEP2' 
-process.modulo2.histoTag  = 'hResDist_STEP2'
-process.modulo2.STEP = 'STEP2'
-
-process.load("DQM.DTMonitorClient.dtResolutionTest_cfi")
-process.modulo3=process.resolutionTest.clone()
-process.modulo3.histoTag2D = 'hResDistVsDist_STEP3' 
-process.modulo3.histoTag  = 'hResDist_STEP3'
-process.modulo3.STEP = 'STEP3'
+process.load("DQM.DTMonitorClient.dtResolutionTestFinalCalib_cfi")
+process.modulo=process.resolutionTest.clone()
+#process.modulo.histoTag2D = 'hResDistVsDist_STEP3' 
+#process.modulo.histoTag  = 'hResDist_STEP3'
+#process.modulo.STEP = 'STEP3'
 
 process.source.processingMode = "RunsAndLumis"
 process.DQMStore.referenceFileName = ''
@@ -87,6 +116,6 @@ process.DQMStore.collateHistograms = False
 process.EDMtoMEConverter.convertOnEndLumi = True
 process.EDMtoMEConverter.convertOnEndRun = False
 
-process.p = cms.Path(process.EDMtoMEConverter*process.modulo1*process.modulo2*process.modulo3*process.qTester*process.dqmSaver)
+process.p = cms.Path(process.EDMtoMEConverter*process.modulo*process.qTester*process.dqmSaver)
 process.DQM.collectorHost = ''
 
