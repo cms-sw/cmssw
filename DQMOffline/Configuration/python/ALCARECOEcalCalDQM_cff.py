d2bdfccbc38bf6711f3bf4e85023fc66781e5c1b
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-10-20 14:27:46 +0000

    accomodate for module changes to avoid conflicts

--- DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
+++ DQMOffline/Configuration/python/ALCARECOEcalCalDQM_cff.py
@@ -4,9 +4,9 @@ import DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi
 import DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi
 import DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi
 
-ALCARECOEcalCalPhisymDQM = DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi.EcalPhiSymMon.clone()
+ALCARECOEcalCalPhisymDQM = DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi.EcalPhiSymMonDQM.clone()
 
-ALCARECOEcalCalPi0CalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0Mon.clone()
-ALCARECOEcalCalEtaCalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0Mon.clone()
+ALCARECOEcalCalPi0CalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0MonDQM.clone()
+ALCARECOEcalCalEtaCalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0MonDQM.clone()
 ALCARECOEcalCalElectronCalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi.EcalEleCalibMon.clone()
 
