b57c057ae640682ab4f35da2fb1771eb458edc10
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2009-10-21 15:37:28 +0000

    fixedCone->shrinkingCone

--- PhysicsTools/PFCandProducer/python/pfTaus_cff.py
+++ PhysicsTools/PFCandProducer/python/pfTaus_cff.py
@@ -5,17 +5,17 @@ from RecoTauTag.Configuration.RecoPFTauTag_cff import *
 from RecoTauTag.TauTagTools.PFTauSelector_cfi  import pfTauSelector
 
 
-fixedConePFTauProducer.DataType = cms.string('AOD')
+shrinkingConePFTauProducer.DataType = cms.string('AOD')
 ic5PFJetTracksAssociatorAtVertex.jets = 'pfJets'
 
 # Clone tau discriminants to avoid further problems with PAT
-allLayer0TausDiscriminationByLeadTrackPt = fixedConePFTauDiscriminationByLeadingTrackPtCut.clone()
-allLayer0TausDiscriminationByIsolation = fixedConePFTauDiscriminationByIsolation.clone()
+allLayer0TausDiscriminationByLeadTrackPt = shrinkingConePFTauDiscriminationByLeadingTrackPtCut.clone()
+allLayer0TausDiscriminationByIsolation = shrinkingConePFTauDiscriminationByIsolation.clone()
 
 allLayer0Taus = pfTauSelector.clone()
-allLayer0Taus.src = cms.InputTag("fixedConePFTauProducer")
+allLayer0Taus.src = cms.InputTag("shrinkingConePFTauProducer")
 allLayer0Taus.discriminators = cms.VPSet(
-      #cms.PSet( discriminator=cms.InputTag("fixedConePFTauDiscriminationByIsolation"),selectionCut=cms.double(0.5))
+      #cms.PSet( discriminator=cms.InputTag("shrinkingConePFTauDiscriminationByIsolation"),selectionCut=cms.double(0.5))
       cms.PSet( discriminator=cms.InputTag("allLayer0TausDiscriminationByLeadTrackPt"),selectionCut=cms.double(0.5) ),
       cms.PSet( discriminator=cms.InputTag("allLayer0TausDiscriminationByIsolation"),selectionCut=cms.double(0.5) )
    )
@@ -26,8 +26,8 @@ pfRecoTauTagInfoProducer.PFCandidateProducer = 'pfNoElectron'
 pfTauSequence = cms.Sequence(
     ic5PFJetTracksAssociatorAtVertex + 
     pfRecoTauTagInfoProducer + 
-    fixedConePFTauProducer + 
-    #fixedConePFTauDiscriminationByIsolation +
+    shrinkingConePFTauProducer + 
+    #shrinkingConePFTauDiscriminationByIsolation +
     allLayer0TausDiscriminationByIsolation +
     allLayer0TausDiscriminationByLeadTrackPt +
     allLayer0Taus 
