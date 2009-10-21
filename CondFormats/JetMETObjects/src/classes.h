75bc8e35fae3898a833c171a4239c46091f1ebb1
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2009-10-21 13:29:10 +0000

    Added L1Offset and JetCorrectionUncertainty classes

--- CondFormats/JetMETObjects/src/classes.h
+++ CondFormats/JetMETObjects/src/classes.h
@@ -2,6 +2,7 @@
 #include "CondFormats/JetMETObjects/interface/SimpleMCJetCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleZSPJetCorrector.h"
 #include "CondFormats/JetMETObjects/interface/Simple3DMCJetCorrector.h"
+#include "CondFormats/JetMETObjects/interface/SimpleL1OffsetCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleL2RelativeCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleL3AbsoluteCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleL3PFAbsoluteCorrector.h"
@@ -9,5 +10,6 @@
 #include "CondFormats/JetMETObjects/interface/SimpleL5FlavorCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleL7PartonCorrector.h"
 #include "CondFormats/JetMETObjects/interface/CombinedJetCorrector.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
 #include "CondFormats/JetMETObjects/interface/SmearedJet.h"
 
