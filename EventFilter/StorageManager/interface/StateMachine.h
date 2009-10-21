d3ebe87b3238ad212e219c9024e2879f139b098d
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-10-21 10:34:56 +0000

    Add Constructed state which is entered instead of Halter when the state machine is constructed. The Constructed state is identical to Halted except that it does not (try to) send the state notification to run control.

--- EventFilter/StorageManager/interface/StateMachine.h
+++ EventFilter/StorageManager/interface/StateMachine.h
@@ -1,4 +1,4 @@
-// $Id: StateMachine.h,v 1.8 2009/07/20 13:06:11 mommsen Exp $
+// $Id: StateMachine.h,v 1.9 2009/09/29 07:54:45 mommsen Exp $
 /// @file: StateMachine.h 
 
 #ifndef StorageManager_StateMachine_h
@@ -39,6 +39,7 @@ namespace stor
   class Normal;
 
   // Inner states of Normal:
+  class Constructed;
   class Halted;
   class Ready;
   class Stopped;
@@ -77,8 +78,8 @@ namespace stor
      Abstract base for state classes
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
 
   class Operations
@@ -119,8 +120,8 @@ namespace stor
      State machine class
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
 
   class StateMachine: public bsc::state_machine<StateMachine,Normal>
@@ -166,8 +167,8 @@ namespace stor
      Failed state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Failed: public bsc::state<Failed,StateMachine>, public Operations
   {
@@ -190,10 +191,10 @@ namespace stor
      Normal state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
-  class Normal: public bsc::state<Normal,StateMachine,Halted>, public Operations
+  class Normal: public bsc::state<Normal,StateMachine,Constructed>, public Operations
   {
 
   public:
@@ -214,11 +215,38 @@ namespace stor
   };
 
   /**
+     Constructed state
+
+     $Author: mommsen $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
+  */
+  class Constructed: public bsc::state<Constructed,Normal>, public Operations
+  {
+
+  public:
+
+    typedef bsc::transition<Configure,Ready> RT;
+    typedef boost::mpl::list<RT> reactions;
+
+    Constructed( my_context );
+    virtual ~Constructed();
+
+  private:
+
+    virtual std::string do_stateName() const;
+    virtual void do_entryActionWork();
+    virtual void do_exitActionWork();
+    virtual void do_moveToFailedState( xcept::Exception& exception ) const;
+
+  };
+
+  /**
      Halted state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Halted: public bsc::state<Halted,Normal>, public Operations
   {
@@ -244,8 +272,8 @@ namespace stor
      Ready state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Ready: public bsc::state<Ready,Normal,Stopped>, public Operations
   {
@@ -273,8 +301,8 @@ namespace stor
      Stopped state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Stopped: public bsc::state<Stopped,Ready>, public Operations
   {
@@ -303,8 +331,8 @@ namespace stor
      Enabled state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Enabled: public bsc::state<Enabled,Ready,Starting>, public Operations
   {
@@ -335,8 +363,8 @@ namespace stor
      Starting state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Starting: public bsc::state<Starting,Enabled>, public Operations
   {
@@ -371,8 +399,8 @@ namespace stor
      Stopping state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Stopping: public bsc::state<Stopping,Enabled>, public Operations
   {
@@ -403,8 +431,8 @@ namespace stor
      Halting state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Halting: public bsc::state<Halting,Enabled>, public Operations
   {
@@ -435,8 +463,8 @@ namespace stor
      Running state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Running: public bsc::state<Running,Enabled,Processing>, public Operations
   {
@@ -469,8 +497,8 @@ namespace stor
      Processing state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class Processing: public bsc::state<Processing,Running>, public Operations
   {
@@ -501,8 +529,8 @@ namespace stor
      DrainingQueues state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class DrainingQueues: public bsc::state<DrainingQueues,Running>, public Operations
   {
@@ -533,8 +561,8 @@ namespace stor
      FinishingDQM state
 
      $Author: mommsen $
-     $Revision: 1.8 $
-     $Date: 2009/07/20 13:06:11 $
+     $Revision: 1.9 $
+     $Date: 2009/09/29 07:54:45 $
   */
   class FinishingDQM: public bsc::state<FinishingDQM,Running>, public Operations
   {
