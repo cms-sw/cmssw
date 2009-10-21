37ebbad0ac5652ca1fb2b70ed3a2e0d01715a6fc
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-10-21 13:21:05 +0000

    Added a default list of files

--- DQMOffline/Trigger/test/muonTest_cfg.py
+++ DQMOffline/Trigger/test/muonTest_cfg.py
@@ -30,58 +30,24 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     skipEvents = cms.untracked.uint32(0),
 
-							fileNames = cms.untracked.vstring(options.files),
-							#  --- one cosmic file to run on
+							# this uses the files provided as arguments to
+							# the script
+							# fileNames = cms.untracked.vstring(options.files),
+
+
+							# These files are part of /RelValTTbar/CMSSW_3_4_0_pre1-MC_31X_V9-v1/GEN-SIM-RECO
 							
-							#fileNames = cms.untracked.vstring ( '/store/data/Commissioning08/Cosmics/RAW-RECO/CRAFT_ALL_V11_227_Tosca090216_ReReco_FromSuperPointing_v2/0004/26F7DEA2-E81F-DE11-9686-0018F3D096E6.root'),
-
-
-							#  --- one relval
-
-							#fileNames = cms.untracked.vstring ( '/store/relval/CMSSW_3_1_0_pre7/RelValZMM/GEN-SIM-RECO/STARTUP_31X_v1/0004/F40BA55C-E641-DE11-858D-001D09F28E80.root'),
-							#fileNames = cms.untracked.vstring ( '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/STARTUP_31X_v1/0004/F69E8351-CE41-DE11-84E4-001D09F23944.root'),
-
-							#---------- There is only ZMM relval with startup
-							#---------- no ideal is available.
-							
-							#fileNames = cms.untracked.vstring ('/store/relval/CMSSW_3_1_0_pre10/RelValZMM/GEN-SIM-RECO/STARTUP_31X_v1/0008/6ABB6AD6-E357-DE11-8EBC-001D09F2437B.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValZMM/GEN-SIM-RECO/STARTUP_31X_v1/0008/4C1E17B0-0458-DE11-A15D-001D09F26509.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValZMM/GEN-SIM-RECO/STARTUP_31X_v1/0008/36A83DC8-E357-DE11-B8DC-000423D996B4.root'
-							#								   ),
-
-							#fileNames = cms.untracked.vstring ('file:/afs/cern.ch/user/s/slaunwhj/scratch0/crafT_RAW2DIGI_RECO_DQM.root'
-							#								   )
-
-							#fileNames = cms.untracked.vstring ('file:/data2/ndpc0/slaunwhj/3_1_0_pre10_RelValZMM/6ABB6AD6-E357-DE11-8EBC-001D09F2437B.root'
-							#								   ),
-
-							#fileNames = cms.untracked.vstring ('file:/data/ndpc0/b/slaunwhj/RelValTTbar/744E4482-1158-DE11-BDF0-001D09F2AF1E.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/AECEC48F-EC57-DE11-90D2-001D09F28F1B.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/AC4882A3-EE57-DE11-A925-001D09F24D8A.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/AA43DBAE-0458-DE11-B90D-001D09F23D1D.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/9CAF0AC0-ED57-DE11-85FE-000423D33970.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/68A9C5C9-EF57-DE11-8562-001D09F2503C.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/28B36B90-F057-DE11-BA80-0030487C6062.root',
-							#								   'file:/data/ndpc0/b/slaunwhj/RelValTTbar/08DAD031-F257-DE11-A493-001D09F24D8A.root')
+							fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0008/2C8CD8FE-B6B5-DE11-ACB8-001D09F2905B.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/DC830720-06B5-DE11-A22D-001D09F292D1.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/A49B5C4C-09B5-DE11-AA7F-001D09F297EF.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/9A88D6A4-08B5-DE11-8302-001D09F23174.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/7EF8FC1B-0DB5-DE11-B0D3-0019B9F6C674.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/5622E1E5-07B5-DE11-8029-001D09F2960F.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/0E7CA853-07B5-DE11-9FAE-000423D99AA2.root',
+															  '/store/relval/CMSSW_3_4_0_pre1/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0007/06355619-0BB5-DE11-A99B-001D09F2924F.root')
 
 							
-
-							#fileNames = cms.untracked.vstring ('/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/CC80B73A-CA57-DE11-BC2F-000423D99896.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/C68B7F1A-CD57-DE11-B706-00304879FA4A.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/9CA9BBC1-CD57-DE11-B62D-001D09F2424A.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/88AD5382-C657-DE11-831F-001D09F24498.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/7C7CDD0F-C457-DE11-8EEE-000423D951D4.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/4C30BDFF-B657-DE11-907A-001D09F24600.root',
-							#								   '/store/relval/CMSSW_3_1_0_pre10/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0008/383036B6-0458-DE11-819F-001D09F29524.root'
-							#								   )
-
-							#fileNames = cms.untracked.vstring ( '/store/relval/CMSSW_3_1_0_pre10/RelValJpsiMM_Pt_0_20/GEN-SIM-RECO/STARTUP_31X_v1/0001/EA6BFAD3-505A-DE11-8313-0018F3D0961A.root',
-							#									'/store/relval/CMSSW_3_1_0_pre10/RelValJpsiMM_Pt_0_20/GEN-SIM-RECO/STARTUP_31X_v1/0001/E4CFF5DE-6658-DE11-B9C1-0018F3D0970A.root',
-							#									'/store/relval/CMSSW_3_1_0_pre10/RelValJpsiMM_Pt_0_20/GEN-SIM-RECO/STARTUP_31X_v1/0001/3ACDB108-C859-DE11-B62D-0018F3D09608.root'
-							#									),
-
-
-
+							
 
 )
 
