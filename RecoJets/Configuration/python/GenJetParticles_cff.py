618968ab1236440f465a51ddcb7885c331f9930b
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-10-22 18:06:42 +0000

    Update ignore list in gen particles to be consistent with the gen met (added a few BSM particles).

--- RecoJets/Configuration/python/GenJetParticles_cff.py
+++ RecoJets/Configuration/python/GenJetParticles_cff.py
@@ -1,16 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
 #
-# $Id: GenJetParticles_cff.py,v 1.2 2008/04/21 03:27:15 rpw Exp $
+# $Id: GenJetParticles_cff.py,v 1.3 2009/10/02 19:24:22 srappocc Exp $
 #
 # ShR 27 Mar 07: move modules producing candidates for Jets from RecoGenJets.cff
 # 
 #
 genParticlesForJets = cms.EDFilter("InputGenJetsParticleSelector",
     src = cms.InputTag("genParticles"),
-    ignoreParticleIDs = cms.vuint32(1000022, 2000012, 2000014, 2000016, 1000039, 
-        5000039, 4000012, 9900012, 9900014, 9900016, 
-        39),
+    ignoreParticleIDs = cms.vuint32(
+         1000022,
+         1000012, 1000014, 1000016,
+         2000012, 2000014, 2000016,
+         1000039, 5100039,
+         4000012, 4000014, 4000016,
+         9900012, 9900014, 9900016,
+         39),
     partonicFinalState = cms.bool(False),
     excludeResonances = cms.bool(True),
     excludeFromResonancePids = cms.vuint32(12, 13, 14, 16),
