1920d2841e97527262955841967fe42cfac42d31
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2009-10-22 17:28:09 +0000

    added plot combiner to overlay efficiencies

--- HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvestingSequence_cff.py
+++ HLTriggerOffline/HeavyFlavor/python/heavyFlavorValidationHarvestingSequence_cff.py
@@ -19,5 +19,16 @@ hfv6 = heavyFlavorValidationHarvesting.clone(
   MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT/HLT_DoubleMu3')
 )
 
-heavyFlavorValidationHarvestingSequence = cms.Sequence(hfv1+hfv2+hfv3+hfv4+hfv5+hfv6)
+combiner = cms.EDAnalyzer('PlotCombiner',
+  MyDQMrootFolder = cms.untracked.string('HLT/HeavyFlavor/HLT'),
+  Plots = cms.untracked.VPSet(
+    cms.untracked.PSet(
+      InputMEnames = cms.untracked.vstring('HLT_Mu3/effPathDiglobOR_recoRapPtY','HLT_Mu5/effPathDiglobOR_recoRapPtY','HLT_Mu9/effPathDiglobOR_recoRapPtY','HLT_DoubleMu0/effPathDiglobAND_recoRapPtY','HLT_DoubleMu3/effPathDiglobAND_recoRapPtY'),
+      InputLabels = cms.untracked.vstring('HLT_Mu3','HLT_Mu5','HLT_Mu9','HLT_DoubleMu0','HLT_DoubleMu3'),
+      OutputMEname = cms.untracked.string('effPathGlob_recoPt')
+    )
+  )
+)
+
+heavyFlavorValidationHarvestingSequence = cms.Sequence(hfv1+hfv2+hfv3+hfv4+hfv5+hfv6+combiner)
 
