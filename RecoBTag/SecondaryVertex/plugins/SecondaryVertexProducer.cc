a9396df34da874bc715b95530805d0a473a8f964
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-10-22 13:32:36 +0000

    Allow for more types of fit "hints" using beam spot constraints and fake beam spots constructed using the primary vertex.

--- RecoBTag/SecondaryVertex/plugins/SecondaryVertexProducer.cc
+++ RecoBTag/SecondaryVertex/plugins/SecondaryVertexProducer.cc
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
 
 #include <boost/iterator/transform_iterator.hpp>
 
@@ -40,6 +42,20 @@
 
 using namespace reco;
 
+namespace {
+	template<typename T>
+	struct RefToBaseLess : public std::binary_function<edm::RefToBase<T>,
+							   edm::RefToBase<T>,
+							   bool> {
+		inline bool operator()(const edm::RefToBase<T> &r1,
+				       const edm::RefToBase<T> &r2) const
+		{
+			return r1.id() < r2.id() ||
+			       (r1.id() == r2.id() && r1.key() < r2.key());
+		}
+	};
+}
+
 class SecondaryVertexProducer : public edm::EDProducer {
     public:
 	explicit SecondaryVertexProducer(const edm::ParameterSet &params);
@@ -48,10 +64,22 @@ class SecondaryVertexProducer : public edm::EDProducer {
 	virtual void produce(edm::Event &event, const edm::EventSetup &es);
 
     private:
+	enum ConstraintType {
+		CONSTRAINT_NONE	= 0,
+		CONSTRAINT_BEAMSPOT,
+		CONSTRAINT_PV_BEAMSPOT_SIZE,
+		CONSTRAINT_PV_BS_Z_ERRORS_SCALED,
+		CONSTRAINT_PV_ERROR_SCALED,
+		CONSTRAINT_PV_PRIMARIES_IN_FIT
+	};
+
+	static ConstraintType getConstraintType(const std::string &name);
+
 	const edm::InputTag		trackIPTagInfoLabel;
 	TrackIPTagInfo::SortCriteria	sortCriterium;
 	TrackSelector			trackSelector;
-	bool				useBeamConstraint;
+	ConstraintType			constraint;
+	double				constraintScaling;
 	edm::ParameterSet		vtxRecoPSet;
 	bool				withPVError;
 	double				minTrackWeight;
@@ -59,18 +87,45 @@ class SecondaryVertexProducer : public edm::EDProducer {
 	VertexSorting			vertexSorting;
 };
 
+SecondaryVertexProducer::ConstraintType
+SecondaryVertexProducer::getConstraintType(const std::string &name)
+{
+	if (name == "None")
+		return CONSTRAINT_NONE;
+	else if (name == "BeamSpot")
+		return CONSTRAINT_BEAMSPOT;
+	else if (name == "BeamSpot+PVPosition")
+		return CONSTRAINT_PV_BEAMSPOT_SIZE;
+	else if (name == "BeamSpotZ+PVErrorScaledXY")
+		return CONSTRAINT_PV_BS_Z_ERRORS_SCALED;
+	else if (name == "PVErrorScaled")
+		return CONSTRAINT_PV_ERROR_SCALED;
+	else if (name == "BeamSpot+PVTracksInFit")
+		return CONSTRAINT_PV_PRIMARIES_IN_FIT;
+	else
+		throw cms::Exception("InvalidArgument")
+			<< "SecondaryVertexProducer: ``constraint'' parameter "
+			   "value \"" << name << "\" not understood."
+			<< std::endl;
+}
+
 SecondaryVertexProducer::SecondaryVertexProducer(
 					const edm::ParameterSet &params) :
 	trackIPTagInfoLabel(params.getParameter<edm::InputTag>("trackIPTagInfos")),
 	sortCriterium(TrackSorting::getCriterium(params.getParameter<std::string>("trackSort"))),
 	trackSelector(params.getParameter<edm::ParameterSet>("trackSelection")),
-	useBeamConstraint(params.getParameter<bool>("useBeamConstraint")),
+	constraint(getConstraintType(params.getParameter<std::string>("constraint"))),
+	constraintScaling(1.0),
 	vtxRecoPSet(params.getParameter<edm::ParameterSet>("vertexReco")),
 	withPVError(params.getParameter<bool>("usePVError")),
 	minTrackWeight(params.getParameter<double>("minimumTrackWeight")),
 	vertexFilter(params.getParameter<edm::ParameterSet>("vertexCuts")),
 	vertexSorting(params.getParameter<edm::ParameterSet>("vertexSelection"))
 {
+	if (constraint == CONSTRAINT_PV_ERROR_SCALED ||
+	    constraint == CONSTRAINT_PV_BS_Z_ERRORS_SCALED)
+		constraintScaling = params.getParameter<double>("pvErrorScaling");
+
 	produces<SecondaryVertexTagInfoCollection>();
 }
 
@@ -116,6 +171,9 @@ namespace {
 void SecondaryVertexProducer::produce(edm::Event &event,
                                       const edm::EventSetup &es)
 {
+	typedef std::map<TrackBaseRef, TransientTrack,
+	                 RefToBaseLess<Track> > TransientTrackMap;
+
 	edm::ESHandle<TransientTrackBuilder> trackBuilder;
 	es.get<TransientTrackRecord>().get("TransientTrackBuilder",
 	                                   trackBuilder);
@@ -124,10 +182,39 @@ void SecondaryVertexProducer::produce(edm::Event &event,
 	event.getByLabel(trackIPTagInfoLabel, trackIPTagInfos);
 
 	edm::Handle<BeamSpot> beamSpot;
-	if (useBeamConstraint)
+	unsigned int bsCovSrc[7] = { 0, };
+	double sigmaZ = 0.0, beamWidth = 0.0;
+	switch(constraint) {
+	    case CONSTRAINT_PV_BEAMSPOT_SIZE:
 		event.getByType(beamSpot);
+		bsCovSrc[3] = bsCovSrc[4] = bsCovSrc[5] = bsCovSrc[6] = 1;
+		sigmaZ = beamSpot->sigmaZ();
+		beamWidth = beamSpot->BeamWidthX();
+		break;
+
+	    case CONSTRAINT_PV_BS_Z_ERRORS_SCALED:
+		event.getByType(beamSpot);
+		bsCovSrc[0] = bsCovSrc[1] = 2;
+		bsCovSrc[3] = bsCovSrc[4] = bsCovSrc[5] = 1;
+		sigmaZ = beamSpot->sigmaZ();
+		break;
+
+	    case CONSTRAINT_PV_ERROR_SCALED:
+		bsCovSrc[0] = bsCovSrc[1] = bsCovSrc[2] = 2;
+		break;
+
+	    case CONSTRAINT_BEAMSPOT:
+	    case CONSTRAINT_PV_PRIMARIES_IN_FIT:
+		event.getByType(beamSpot);
+		break;
+
+	    default:
+		/* nothing */;
+	}
 
 	ConfigurableVertexReconstructor vertexReco(vtxRecoPSet);
+
+	TransientTrackMap primariesMap;
 
 	// result secondary vertices
 
@@ -141,6 +228,27 @@ void SecondaryVertexProducer::produce(edm::Event &event,
 		std::vector<SecondaryVertexTagInfo::IndexedTrackData> trackData;
 
 		const Vertex &pv = *iterJets->primaryVertex();
+
+		std::set<TransientTrack> primaries;
+		if (constraint == CONSTRAINT_PV_PRIMARIES_IN_FIT) {
+			for(Vertex::trackRef_iterator iter = pv.tracks_begin();
+			    iter != pv.tracks_end(); ++iter) {
+				TransientTrackMap::iterator pos =
+					primariesMap.lower_bound(*iter);
+
+				if (pos != primariesMap.end() &&
+				    pos->first == *iter)
+					primaries.insert(pos->second);
+				else {
+					TransientTrack track =
+						trackBuilder->build(
+							iter->castTo<TrackRef>());
+					primariesMap.insert(pos,
+						std::make_pair(*iter, track));
+					primaries.insert(track);
+				}
+			}
+		}
 
 		edm::RefToBase<Jet> jetRef = iterJets->jet();
 
@@ -169,25 +277,73 @@ void SecondaryVertexProducer::produce(edm::Event &event,
 
 			// select tracks for SV finder
 
-			if (trackSelector(*trackRef, ipData[i], *jetRef,
-				          RecoVertex::convertPos(
-			                  		pv.position()))) {
-				fitTracks.push_back(
-					trackBuilder->build(trackRef));
-				trackData.back().second.svStatus =
-					SecondaryVertexTagInfo::TrackData::trackUsedForVertexFit;
-			} else
+			if (!trackSelector(*trackRef, ipData[i], *jetRef,
+			                   RecoVertex::convertPos(
+			                   		pv.position()))) {
 				trackData.back().second.svStatus =
 					SecondaryVertexTagInfo::TrackData::trackSelected;
+				continue;
+			}
+
+			TransientTrackMap::const_iterator pos =
+					primariesMap.find(
+						TrackBaseRef(trackRef));
+			if (pos != primariesMap.end()) {
+				primaries.erase(pos->second);
+				fitTracks.push_back(pos->second);
+			} else
+				fitTracks.push_back(
+					trackBuilder->build(trackRef));
+
+			trackData.back().second.svStatus =
+				SecondaryVertexTagInfo::TrackData::trackUsedForVertexFit;
 		}
 
 		// perform actual vertex finding
 
 		std::vector<TransientVertex> fittedSVs;
-		if (useBeamConstraint)
-			fittedSVs = vertexReco.vertices(fitTracks, *beamSpot);
-		else
+		switch(constraint) {
+		    case CONSTRAINT_NONE:
 			fittedSVs = vertexReco.vertices(fitTracks);
+			break;
+
+		    case CONSTRAINT_BEAMSPOT:
+			fittedSVs = vertexReco.vertices(fitTracks, *beamSpot);
+			break;
+
+		    case CONSTRAINT_PV_BEAMSPOT_SIZE:
+		    case CONSTRAINT_PV_BS_Z_ERRORS_SCALED:
+		    case CONSTRAINT_PV_ERROR_SCALED: {
+			BeamSpot::CovarianceMatrix cov;
+			for(unsigned int i = 0; i < 7; i++) {
+				unsigned int covSrc = bsCovSrc[i];
+				for(unsigned int j = 0; j < 7; j++) {
+					double v;
+					if (!covSrc || bsCovSrc[j] != covSrc)
+						v = 0.0;
+					else if (covSrc == 1)
+						v = beamSpot->covariance(i, j);
+					else
+						v = pv.covariance(i, j) *
+						    constraintScaling;
+					cov(i, j) = v;
+				}
+			}
+
+			BeamSpot bs(pv.position(), sigmaZ,
+			            beamSpot->dxdz(), beamSpot->dydz(),
+			            beamWidth, cov, BeamSpot::Unknown);
+
+			fittedSVs = vertexReco.vertices(fitTracks, bs);
+		    }	break;
+
+		    case CONSTRAINT_PV_PRIMARIES_IN_FIT: {
+			std::vector<TransientTrack> primaries_(
+					primaries.begin(), primaries.end());
+			fittedSVs = vertexReco.vertices(primaries_, fitTracks,
+			                                *beamSpot);
+		    }	break;
+		}
 
 		// build combined SV information and filter
 
