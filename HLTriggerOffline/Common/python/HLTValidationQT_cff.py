cc387585ff90e49cd60f9ef97a732777b08b3ec1
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-10-22 10:32:16 +0000

    activate tau qt

--- HLTriggerOffline/Common/python/HLTValidationQT_cff.py
+++ HLTriggerOffline/Common/python/HLTValidationQT_cff.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 #from HLTriggerOffline.Common.HLTValidationQTExample_cfi import *
 from HLTriggerOffline.Muon.HLTMuonQualityTester_cfi import *
+from HLTriggerOffline.Tau.Validation.HLTTauQualityTests_cff import *
 
 hltvalidationqt = cms.Sequence(
     #hltQTExample
     hltMuonQualityTester
+    + hltTauRelvalQualityTests
     )
