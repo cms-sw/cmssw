92a5918431d57249c7c816022ec135d51fd0ce94
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-10-23 10:51:01 +0000

    Minor Changes to improve binning and match trigger menus

--- DQMOffline/Trigger/python/MuonOffline_Trigger_cosmics_cff.py
+++ DQMOffline/Trigger/python/MuonOffline_Trigger_cosmics_cff.py
@@ -2,9 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from DQMOffline.Trigger.MuonTrigRateAnalyzer_cosmics_cfi import *
 from DQMOffline.Trigger.TopTrigRateAnalyzer_cosmics_cfi import *
-from DQMOffline.Trigger.BPAGTrigRateAnalyzer_cfi import *
+from DQMOffline.Trigger.TnPEfficiency_cff import *
+
 
 muonFullOfflineDQM = cms.Sequence( offlineDQMMuonTrigCosmics
 								   + topTrigOfflineDQMCosmics
-								   + bpagTrigOffDQM )
+								   + TnPEfficiency )
 
