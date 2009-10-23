ca4e34e68e58cb4ee96e6dbdbaa154b570fe66c6
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-10-23 09:33:51 +0000

    Explicited some variables

--- CalibTracker/SiStripESProducers/test/python/DummyCondDBWriter_SiStripLatency_cfg.py
+++ CalibTracker/SiStripESProducers/test/python/DummyCondDBWriter_SiStripLatency_cfg.py
@@ -11,7 +11,7 @@ process = cms.Process("Builder")
 process.MessageLogger = cms.Service(
     "MessageLogger",
     debugModules = cms.untracked.vstring('siStripLatencyDummyDBWriter'),
-    threshold = cms.untracked.string('DEBUG'),
+    threshold = cms.untracked.string('INFO'),
     destinations = cms.untracked.vstring('LatencyBuilder.log')
 )
 
@@ -20,11 +20,18 @@ process.maxEvents = cms.untracked.PSet(
 )
 process.source = cms.Source("EmptySource",
     numberEventsInRun = cms.untracked.uint32(1),
-    firstRun = cms.untracked.uint32(1)
+    firstRun = cms.untracked.uint32(1),
 )
 
 process.load("CalibTracker.SiStripESProducers.fake.SiStripLatencyFakeESSource_cfi")
 process.load("CalibTracker.SiStripESProducers.DBWriter.SiStripLatencyDummyDBWriter_cfi")
+
+process.SiStripLatencyGenerator.latency = 255
+process.SiStripLatencyGenerator.mode = 0
+# process.SiStripLatencyGenerator.latency = 143
+# process.SiStripLatencyGenerator.mode = 47
+# process.SiStripLatencyGenerator.latency = 146
+# process.SiStripLatencyGenerator.mode = 37
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
@@ -36,11 +43,17 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     connect = cms.string('sqlite_file:dbfile.db'),
     toPut = cms.VPSet(cms.PSet(
         record = cms.string('SiStripLatencyRcd'),
-        tag = cms.string('SiStripLatency_Ideal_31X')
+        tag = cms.string('SiStripLatency_Ideal_31X_v1')
     ))
 )
 
 process.siStripLatencyDummyDBWriter.record=process.PoolDBOutputService.toPut[0].record 
+
+# This specifies the IOV, not the EmptySource (to see why look at the DummyDBWriter code)
+# process.siStripLatencyDummyDBWriter.OpenIovAt = ""
+# process.siStripLatencyDummyDBWriter.OpenIovAtTime = 1
+# process.siStripLatencyDummyDBWriter.OpenIovAtTime = 108449
+# process.siStripLatencyDummyDBWriter.OpenIovAtTime = 110177
+# process.siStripLatencyDummyDBWriter.OpenIovAtTime = 110213
+
 process.p1 = cms.Path(process.siStripLatencyDummyDBWriter)
-
-
