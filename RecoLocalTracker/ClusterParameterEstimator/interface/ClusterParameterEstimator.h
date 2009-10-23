7daa4c61de8f0558e458cfc7c94b18869002cb68
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-10-23 14:38:23 +0000

    Add virtual methods localParametersV which return a vector of local parameters, for the possibility of merged cluster, or ambiguous position.  Default is to return size 1 vector containing result of localParameters

--- RecoLocalTracker/ClusterParameterEstimator/interface/ClusterParameterEstimator.h
+++ RecoLocalTracker/ClusterParameterEstimator/interface/ClusterParameterEstimator.h
@@ -7,24 +7,35 @@
 #include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
 #include "DataFormats/TrajectoryState/interface/LocalTrajectoryParameters.h"
 
-template <class T> class ClusterParameterEstimator {
+template <class T> 
+class ClusterParameterEstimator {
   
  public:
-   typedef std::pair<LocalPoint,LocalError>  LocalValues; 
-   virtual LocalValues localParameters( const T&,const GeomDetUnit&) const = 0; 
-   virtual LocalValues localParameters( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters & ltp) const {
-     return localParameters(cluster,gd);
-   } 
-
+  typedef std::pair<LocalPoint,LocalError>  LocalValues;
+  typedef std::vector<LocalValues> VLocalValues;
+  virtual LocalValues localParameters( const T&,const GeomDetUnit&) const = 0; 
+  virtual LocalValues localParameters( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters& ltp) const {
+    return localParameters(cluster,gd);
+  }
+  virtual VLocalValues localParametersV( const T& cluster, const GeomDetUnit& gd) const {
+    VLocalValues vlp;
+    vlp.push_back(localParameters(cluster,gd));
+    return vlp;
+  }
+  virtual VLocalValues localParametersV( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters& ltp) const {
+    VLocalValues vlp;
+    vlp.push_back(localParameters(cluster,gd,ltp));
+    return vlp;
+  }
+  
   virtual ~ClusterParameterEstimator(){}
-
+  
   //methods needed by FastSim
   virtual void enterLocalParameters(unsigned int id, std::pair<int,int>
-				    &row_col, LocalValues pos_err_info) const{}
+				    &row_col, LocalValues pos_err_info) const {}
   virtual void enterLocalParameters(uint32_t id, uint16_t firstStrip,
-				    LocalValues pos_err_info) const{}
+				    LocalValues pos_err_info) const {}
   virtual void clearParameters() const {}
-
   
 };
 
