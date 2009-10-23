0320d66076b7e7743230bbe28d5d9aa76e6c2ffe
Gheorghe Lungu <sha1-606bf777ce3d07bc318c7a6398423464f1ca36c7@cern.ch>
2009-10-23 20:06:22 +0000

    moved code to EndRun routine for JetMETDQMPostProcessor.cc

--- HLTriggerOffline/JetMET/src/JetMETDQMPostProcessor.cc
+++ HLTriggerOffline/JetMET/src/JetMETDQMPostProcessor.cc
@@ -20,8 +20,7 @@ JetMETDQMPostProcessor::JetMETDQMPostProcessor(const edm::ParameterSet& pset)
 
 void JetMETDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& es)
 {
-  /*
-  //////////////////////////////////
+ //////////////////////////////////
   // setup DQM stor               //
   //////////////////////////////////
 
@@ -48,22 +47,26 @@ void JetMETDQMPostProcessor::endRun(edm::Run const& run, edm::EventSetup const& 
     MonitorElement*_meTurnOnMET = dqm->book1D("_meTurnOnMET","Missing ET Turn-On",100,0,500);
     MonitorElement*_meTurnOnJetPt = dqm->book1D("_meTurnOnJetPt","Jet Pt Turn-On",100,0,500);
     
-    //_meTurnOnMET->getTH1F()->Add(dqm->get(dqm->pwd() + "_meGenMETTrg")->getTH1F(),1);
-    //_meTurnOnMET->getTH1F()->Sumw2();
-    // dqm->get(dqm->pwd() + "_meGenMET")->getTH1F()->Sumw2();
-//     _meTurnOnMET->getTH1F()->Divide(_meTurnOnMET->getTH1F(),dqm->get(dqm->pwd() + "_meGenMET")->getTH1F(),1,1,"B");
+    //std::vector<std::string> mes = dqm->getMEs();
+    //for(std::vector<std::string>::iterator me = mes.begin() ;me!= mes.end(); me++ )
+    //  std::cout <<*me <<std::endl;
+    //std::cout <<std::endl;
     
-//     _meTurnOnJetPt->getTH1F()->Add(_meGenJetPtTrg->getTH1F(),1);
-//     _meTurnOnJetPt->getTH1F()->Sumw2();
-//     _meGenJetPt->getTH1F()->Sumw2();
-//     _meTurnOnJetPt->getTH1F()->Divide(_meTurnOnJetPt->getTH1F(),_meGenJetPt->getTH1F(),1,1,"B");
+    _meTurnOnMET->getTH1F()->Add(dqm->get(dqm->pwd() + "/_meGenMETTrg")->getTH1F(),1);
+    _meTurnOnMET->getTH1F()->Sumw2();
+    dqm->get(dqm->pwd() + "/_meGenMET")->getTH1F()->Sumw2();
+    _meTurnOnMET->getTH1F()->Divide(_meTurnOnMET->getTH1F(),dqm->get(dqm->pwd() + "/_meGenMET")->getTH1F(),1,1,"B");
     
-    dqm->goUp();
-  }
-  */
+    _meTurnOnJetPt->getTH1F()->Add(dqm->get(dqm->pwd() + "/_meGenJetPtTrg")->getTH1F(),1);
+    _meTurnOnJetPt->getTH1F()->Sumw2();
+    dqm->get(dqm->pwd() + "/_meGenJetPt")->getTH1F()->Sumw2();
+    _meTurnOnJetPt->getTH1F()->Divide(_meTurnOnJetPt->getTH1F(),dqm->get(dqm->pwd() + "/_meGenJetPt")->getTH1F(),1,1,"B");
+    
+    dqm->goUp(); 
 }
 void JetMETDQMPostProcessor::endJob()
 {
+  /*
   //////////////////////////////////
   // setup DQM stor               //
   //////////////////////////////////
@@ -108,5 +111,6 @@ void JetMETDQMPostProcessor::endJob()
     
     dqm->goUp();
   }
+  */
 }
 DEFINE_FWK_MODULE(JetMETDQMPostProcessor);
