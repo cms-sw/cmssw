d6d7e51feff993d663b8cb44e0f03ce9459c6cc5
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2009-10-23 13:31:59 +0000

    new version of DQM for MessageLogger added

--- DQMServices/Components/plugins/SealModule.cc
+++ DQMServices/Components/plugins/SealModule.cc
@@ -16,6 +16,10 @@ DEFINE_ANOTHER_FWK_MODULE(DQMFEDIntegrityClient);
 DEFINE_ANOTHER_FWK_MODULE(DQMStoreStats);
 #include "DQMServices/Components/src/DQMLogError.h"
 DEFINE_ANOTHER_FWK_MODULE(DQMLogError);
+#include "DQMServices/Components/src/DQMMessageLogger.h"
+DEFINE_ANOTHER_FWK_MODULE(DQMMessageLogger);
+#include "DQMServices/Components/src/DQMMessageLoggerClient.h"
+DEFINE_ANOTHER_FWK_MODULE(DQMMessageLoggerClient);
 #include "DQMServices/Components/src/DQMFileReader.h"
 DEFINE_ANOTHER_FWK_MODULE(DQMFileReader);
 
