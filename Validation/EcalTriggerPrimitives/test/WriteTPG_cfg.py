c44b014d63e4ae8ab42f131209ed610976d0e2b5
Emilia Lubenova Becheva <sha1-d966cd138a0288d3ff259f3f2cdb9f6c54000612@cern.ch>
2009-10-23 12:24:15 +0000

    call the ecalTriggerPrimitiveDigis_mc_cff instead of ecalTriggerPrimitiveDigis_with_suppressed_cff

--- Validation/EcalTriggerPrimitives/test/WriteTPG_cfg.py
+++ Validation/EcalTriggerPrimitives/test/WriteTPG_cfg.py
@@ -17,21 +17,24 @@ process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
 
 process.load("CalibCalorimetry.Configuration.Ecal_FakeConditions_cff")
 
-process.load("SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_with_suppressed_cff")
+process.load("SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_mc_cff")
 
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_0_0_pre6/RelValSingleElectronPt35/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_30X_v1/0005/28116A15-E9DD-DD11-9BA6-001617E30F4C.root')
+    fileNames = cms.untracked.vstring(
+       '/store/relval/CMSSW_3_3_0/RelValSingleElectronPt35/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/F6794108-E5BC-DE11-8639-0018F3D096DE.root',
+       '/store/relval/CMSSW_3_3_0/RelValSingleElectronPt35/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/EED7DC04-4CBD-DE11-BC61-002354EF3BE0.root',
+       '/store/relval/CMSSW_3_3_0/RelValSingleElectronPt35/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/82484FAB-E3BC-DE11-B79C-001731AF67E9.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10000)
 )
 
 process.out = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring('drop *_*_*_*', 
 	'keep *_simEcalTriggerPrimitiveDigis_*_*'),
-    fileName = cms.untracked.string('file:TrigPrim.root')
+    fileName = cms.untracked.string('file:TrigPrim_330rel.root')
 )
 
 process.Timing = cms.Service("Timing")
