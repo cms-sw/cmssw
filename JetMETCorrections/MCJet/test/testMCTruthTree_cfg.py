511b305b3f720ce6ca3c2d4d51b83f7ab38ced06
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2009-10-24 12:13:07 +0000

    Updated to CMSSW_3_4_X

--- JetMETCorrections/MCJet/test/testMCTruthTree_cfg.py
+++ JetMETCorrections/MCJet/test/testMCTruthTree_cfg.py
@@ -4,24 +4,24 @@ process = cms.Process("Ana")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 #############   Set the number of events #############
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(1000)
 )
 #############   Define the source file ###############
-#process.load("JetMETCorrections.MCJet.QCDDiJet_50_80_cfi") 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/mc/Summer08/QCDDiJetPt50to80/GEN-SIM-RECO/IDEAL_V9_v3/0001/0079B347-9E9A-DD11-ADD9-003048C17FC0.root')
-)
+process.load("JetMETCorrections.MCJet.RelValQCD_cfi") 
+#process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_4_0_pre2/RelValQCD_FlatPt_15_3000/GEN-SIM-RECO/MC_3XY_V10-v1/0003/D085615A-A5BD-DE11-8897-0026189437E8.root')
+#)
 #############    CaloJets ############################
 process.caloMctruthTree = cms.EDAnalyzer("CaloMCTruthTreeProducer",
-    jets               = cms.string('iterativeCone5CaloJets'),
-    genjets            = cms.string('iterativeCone5GenJets'),
-    histogramFile      = cms.string('ic05CaloMctruthTree.root')
+    jets               = cms.string('ak5CaloJets'),
+    genjets            = cms.string('ak5GenJets'),
+    histogramFile      = cms.string('ak5CaloMctruthTree.root')
 )
 #############    PFJets   ############################
 process.pfMctruthTree = cms.EDAnalyzer("PFMCTruthTreeProducer",
-    jets               = cms.string('iterativeCone5PFJets'),
-    genjets            = cms.string('iterativeCone5GenJets'),
-    histogramFile      = cms.string('ic05PFMctruthTree.root')
+    jets               = cms.string('ak5PFJets'),
+    genjets            = cms.string('ak5GenJets'),
+    histogramFile      = cms.string('ak5PFMctruthTree.root')
 )
 #############   Path       ###########################
 process.p = cms.Path(process.caloMctruthTree * process.pfMctruthTree)
