defd712dd8db91edcded666828534bddfd846fa4
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2009-10-25 17:23:59 +0000

    apply patch from Slava for recent python compilation problems in 34x

--- RecoHI/HiMuonAlgos/python/HiRecoMuon_cff.py
+++ RecoHI/HiMuonAlgos/python/HiRecoMuon_cff.py
@@ -6,14 +6,13 @@ hiTracks = 'hiGlobalPrimTracks' #heavy ion track label
 
 # replace with heavy ion track label
 muons.inputCollectionLabels = [hiTracks, 'globalMuons', 'standAloneMuons:UpdatedAtVtx']
-muons.TrackExtractorPSet.inputTrackCollection = hiTracks
+
 calomuons.inputTracks = hiTracks
-muIsoDepositTk.ExtractorPSet.inputTrackCollection = hiTracks
+
 globalMuons.TrackerCollectionLabel = hiTracks
 
 # replace with heavy ion jet label
 muons.JetExtractorPSet.JetCollectionLabel = cms.InputTag("iterativeConePu5CaloJets")
-muIsoDepositJets.ExtractorPSet.JetCollectionLabel = cms.InputTag("iterativeConePu5CaloJets")
 
 # HI muon sequence (passed to RecoHI.Configuration.Reconstruction_HI_cff)
 muonRecoPbPb = cms.Sequence(muonreco_plus_isolation)
