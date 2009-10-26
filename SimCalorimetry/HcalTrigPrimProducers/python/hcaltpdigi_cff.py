f9e4f7bb07e20a61596269687030cac412c153d5
kvtsang <sha1-fbc135b22cf7aa9240456e4bef616030b236c1e9@cern.ch>
2009-10-26 16:43:33 +0000

    LUTs for HB/HE FG bits.

--- SimCalorimetry/HcalTrigPrimProducers/python/hcaltpdigi_cff.py
+++ SimCalorimetry/HcalTrigPrimProducers/python/hcaltpdigi_cff.py
@@ -9,10 +9,9 @@ from CalibCalorimetry.CaloTPG.CaloTPGTranscoder_cfi import *
 HcalTPGCoderULUT = cms.ESProducer("HcalTPGCoderULUT",
     read_Ascii_LUTs = cms.bool(False),
     read_XML_LUTs = cms.bool(False),
+    read_FG_LUTs = cms.bool(False),
     LUTGenerationMode = cms.bool(True),
-    DumpL1TriggerObjects = cms.bool(False),
-    TagName = cms.string("LUTFromHCALDb"),
-    AlgoName = cms.string("LUTFromHCALDb"),
     inputLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/inputLUTcoder_physics.dat'),
-    filename = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/RecHit-TPG-calib.dat')
+    FGLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/HBHE_FG_LUT.dat'),
+    RCalibFile = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/RecHit-TPG-calib.dat')
 )
