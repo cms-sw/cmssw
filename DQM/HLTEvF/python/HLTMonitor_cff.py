43dc86440f4a3c176bcd2e66b56dc16f3e9ea49f
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-10-27 09:47:59 +0000

    temporary fix

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -2,15 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.HLTEvF.HLTMonElectron_cff import *
 #from DQM.HLTEvF.HLTMonMuon_cff import *
-from DQM.HLTEvF.HLTMonTau_cff import *
+#from DQM.HLTEvF.HLTMonTau_cff import *
 from DQM.HLTEvF.FourVectorHLTOnline_cff import *
 #from DQM.HLTEvF.HLTEventInfoClient_cff import *
 #from DQM.HLTEvF.HLTMonJetMET_cff import *
 from DQM.HLTEvF.HLTMonJetMETDQMSource_cff import * 
 from DQM.HLTEvF.HLTMonMuonDQM_cff import *
 from DQM.HLTEvF.HLTLocalRecoDT_cff import *
-from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
+#from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
 from DQM.HLTEvF.HLTMonAlCaPi0_cff import *
 from DQM.HLTEvF.HLTMonAlCaPhySim_cff import *
-from DQM.HLTEvF.hltMonBTag_cff import *
+#from DQM.HLTEvF.hltMonBTag_cff import *
 
