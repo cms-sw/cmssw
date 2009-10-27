f4186bd245a2f7170154c2da569929dd0dfabac1
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-10-27 01:08:57 +0000

    Add method with LocalTrajectoryError, ignored by default

--- RecoLocalTracker/ClusterParameterEstimator/interface/ClusterParameterEstimator.h
+++ RecoLocalTracker/ClusterParameterEstimator/interface/ClusterParameterEstimator.h
@@ -6,6 +6,7 @@
 
 #include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
 #include "DataFormats/TrajectoryState/interface/LocalTrajectoryParameters.h"
+#include "TrackingTools/TrajectoryParametrization/interface/LocalTrajectoryError.h"
 
 template <class T> 
 class ClusterParameterEstimator {
@@ -14,8 +15,11 @@ class ClusterParameterEstimator {
   typedef std::pair<LocalPoint,LocalError>  LocalValues;
   typedef std::vector<LocalValues> VLocalValues;
   virtual LocalValues localParameters( const T&,const GeomDetUnit&) const = 0; 
-  virtual LocalValues localParameters( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters& ltp) const {
+  virtual LocalValues localParameters( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters&) const {
     return localParameters(cluster,gd);
+  }
+  virtual LocalValues localParameters( const T& cluster, const GeomDetUnit& gd, const LocalTrajectoryParameters& ltp, const LocalTrajectoryError&) const {
+    return localParameters(cluster,gd,ltp);
   }
   virtual VLocalValues localParametersV( const T& cluster, const GeomDetUnit& gd) const {
     VLocalValues vlp;
