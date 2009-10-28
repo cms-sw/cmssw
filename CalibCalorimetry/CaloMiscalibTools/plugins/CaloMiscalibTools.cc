4f0e19390c03b1a370b07ae4b23d6afb55d77edf
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2009-10-28 14:48:41 +0000

    modified to use new xml tools and format

--- CalibCalorimetry/CaloMiscalibTools/plugins/CaloMiscalibTools.cc
+++ CalibCalorimetry/CaloMiscalibTools/plugins/CaloMiscalibTools.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Lorenzo AGOSTINO
 //         Created:  Wed May 31 10:37:45 CEST 2006
-// $Id: CaloMiscalibTools.cc,v 1.3 2008/01/22 18:52:16 muzaffar Exp $
+// $Id: CaloMiscalibTools.cc,v 1.4 2008/03/26 14:07:42 fra Exp $
 //
 // Modified       : Luca Malgeri 
 // Date:          : 11/09/2006 
@@ -27,32 +27,24 @@
 // user include files
 #include "CalibCalorimetry/CaloMiscalibTools/interface/CaloMiscalibTools.h"
 #include "FWCore/ParameterSet/interface/FileInPath.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "CalibCalorimetry/CaloMiscalibTools/interface/MiscalibReaderFromXMLEcalBarrel.h"
-#include "CalibCalorimetry/CaloMiscalibTools/interface/MiscalibReaderFromXMLEcalEndcap.h"
-
+#include "CondTools/Ecal/interface/EcalIntercalibConstantsXMLTranslator.h"
 //
 // constructors and destructor
 //
 CaloMiscalibTools::CaloMiscalibTools(const edm::ParameterSet& iConfig)
 {
-   //the following line is needed to tell the framework what
-   // data is being produced
-  map_.prefillMap();
 
-  barrelfileinpath_=iConfig.getUntrackedParameter<std::string> ("fileNameBarrel","");
-  endcapfileinpath_=iConfig.getUntrackedParameter<std::string> ("fileNameEndcap","");
+  constantsfile_=iConfig.getUntrackedParameter<std::string> ("fileName","");
 
-  edm::FileInPath barrelfiletmp("CalibCalorimetry/CaloMiscalibTools/data/"+barrelfileinpath_);
-  edm::FileInPath endcapfiletmp("CalibCalorimetry/CaloMiscalibTools/data/"+endcapfileinpath_);
+  edm::FileInPath filetmp("CalibCalorimetry/CaloMiscalibTools/data/"+constantsfile_);
   
   
-  barrelfile_=barrelfiletmp.fullPath();
-  endcapfile_=endcapfiletmp.fullPath();
+  constantsfile_=filetmp.fullPath();
 
-  std::cout <<"Barrel file is:"<< barrelfile_<<std::endl;
-  std::cout <<"endcap file is:"<< endcapfile_<<std::endl;
 
+  edm::LogInfo("CaloMiscalibTools: using")<<constantsfile_;
 
    // added by Zhen (changed since 1_2_0)
    setWhatProduced(this,&CaloMiscalibTools::produce);
@@ -78,17 +70,17 @@ CaloMiscalibTools::~CaloMiscalibTools()
 CaloMiscalibTools::ReturnType
 CaloMiscalibTools::produce(const EcalIntercalibConstantsRcd& iRecord)
 {
-    map_.prefillMap();
-    MiscalibReaderFromXMLEcalBarrel barrelreader_(map_);
-    MiscalibReaderFromXMLEcalEndcap endcapreader_(map_);
-    if(!barrelfile_.empty()) barrelreader_.parseXMLMiscalibFile(barrelfile_);
-    if(!endcapfile_.empty())endcapreader_.parseXMLMiscalibFile(endcapfile_);
-    map_.print();
-    // Added by Zhen, need a new object so to not be deleted at exit
-    //    std::cout<<"about to copy"<<std::endl;
-    EcalIntercalibConstants* mydata=new EcalIntercalibConstants(map_.get());
+   
+  
+  EcalCondHeader h;
+  EcalIntercalibConstants rcd ;
+  int ret=EcalIntercalibConstantsXMLTranslator::readXML(constantsfile_,h,rcd);
+
+  if (ret)   edm::LogError("CaloMiscalibReader: cannot parse xml file");
+
+  EcalIntercalibConstants* mydata=new EcalIntercalibConstants(rcd);
     //    std::cout<<"mydata "<<mydata<<std::endl;
-    return mydata;
+  return mydata;
 }
 
  void CaloMiscalibTools::setIntervalFor(const edm::eventsetup::EventSetupRecordKey &, const edm::IOVSyncValue&,
