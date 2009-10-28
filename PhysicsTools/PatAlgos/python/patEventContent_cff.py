45930b35024542c10c62f2f822339d79538ce257
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-10-28 18:36:21 +0000

    added heavyIon event content vectors

--- PhysicsTools/PatAlgos/python/patEventContent_cff.py
+++ PhysicsTools/PatAlgos/python/patEventContent_cff.py
@@ -1,5 +1,3 @@
-# The following comments couldn't be translated into the new config version:
-
 import FWCore.ParameterSet.Config as cms
 
 patEventContentNoLayer1Cleaning = [
@@ -34,7 +32,7 @@ patExtraAodEventContent = [
     'keep *_offlineBeamSpot_*_*',
     'keep *_offlinePrimaryVertices_*_*',
     # TRIGGER
-    'keep edmTriggerResults_TriggerResults_*_HLT', 
+    'keep edmTriggerResults_TriggerResults_*_*', 
     'keep *_hltTriggerSummaryAOD_*_*'
 ]
 
@@ -56,4 +54,17 @@ patEventContentTriggerMatch = [
     'keep *_cleanLayer1TausTriggerMatch_*_*', 
     'keep *_cleanLayer1JetsTriggerMatch_*_*', 
     'keep *_layer1METsTriggerMatch_*_*'
+]
+
+patHiEventContent = [
+    'keep patPhotons_selected*_*_*',
+    'keep patMuons_selected*_*_*',
+    'keep patJets_selected*_*_*',
+    'keep patHeavyIon_heavyIon_*_*'
+]
+
+patHiExtraAodEventContent = [
+    'keep recoGenParticles_hiGenParticles_*_*',
+    'keep recoGenJets_iterativeCone5HiGenJets_*_*', # until a better solution
+    'keep recoTracks_hiSelectedTracks_*_*'
 ]
