f38fc963e8c33a98e9f67d4482bb42a1add2fedb
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-10-28 10:20:46 +0000

    bug fix

--- HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
+++ HLTriggerOffline/Common/python/HLTValidationHarvest_cff.py
@@ -7,7 +7,8 @@ from HLTriggerOffline.HeavyFlavor.heavyFlavorValidationHarvestingSequence_cff im
 from HLTriggerOffline.JetMET.Validation.JetMETPostProcessor_cff import *
 from HLTriggerOffline.special.hltAlCaPostVal_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_postProcessor_cff import *
-from HLTriggerOffline.Higgs.HiggsValidation_cff import *
+from HLTriggerOffline.Higgs.HLTHiggsPostVal_cff import *
+
 #from HLTriggerOffline.Common.PostProcessorExample_cfi import *
 from HLTriggerOffline.Common.HLTValidationQT_cff import *
 
