091bbcabd0e0178b896cea1114bb55e0f0657860
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2009-10-29 15:46:37 +0000

    Updated trigger menus.

--- CondTools/L1Trigger/test/L1ConfigWritePayloadDummy_cfg.py
+++ CondTools/L1Trigger/test/L1ConfigWritePayloadDummy_cfg.py
@@ -37,10 +37,12 @@ process.load("CondTools.L1Trigger.L1TriggerKeyListDummy_cff")
 # Generate dummy configuration data
 if options.startup == 0:
     process.load("L1Trigger.Configuration.L1DummyConfig_cff")
-    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1031.L1Menu_MC2009_v2_L1T_Scales_20090624_Imp0_Unprescaled_cff")
+#    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1031.L1Menu_MC2009_v2_L1T_Scales_20090624_Imp0_Unprescaled_cff")
+    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1031.L1Menu_MC2009_v4_L1T_Scales_20090624_Imp0_Unprescaled_cff")
 else:
     process.load("L1Trigger.Configuration.L1StartupConfig_cff")
-    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.startup.L1Menu_Commissioning2009_v3_L1T_Scales_20080926_startup_Imp0_Unprescaled_cff")
+#    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.startup.L1Menu_Commissioning2009_v3_L1T_Scales_20080926_startup_Imp0_Unprescaled_cff")
+    process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.startup.L1Menu_Commissioning2009_v5_L1T_Scales_20080926_startup_Imp0_Unprescaled_cff")
 
 # writer modules
 from CondTools.L1Trigger.L1CondDBPayloadWriter_cff import initPayloadWriter
