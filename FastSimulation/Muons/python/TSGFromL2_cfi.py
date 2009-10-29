ddf22b8c048045b5081be215a0662f578ef20c91
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-10-29 22:36:41 +0000

    update with OIHit fastsim

--- FastSimulation/Muons/python/TSGFromL2_cfi.py
+++ FastSimulation/Muons/python/TSGFromL2_cfi.py
@@ -9,8 +9,8 @@ from RecoMuon.TrackerSeedGenerator.TrackerSeedCleaner_cff import *
 # include  "RecoMuon/TrackerSeedGenerator/data/TSGs.cff"
 
 
-def l3seeds(old):
-    if (old):
+def l3seeds(tsg = "old"):
+    if (tsg == "old"):
         return cms.EDFilter("FastTSGFromL2Muon",
                             # ServiceParameters
                             MuonServiceProxy,
@@ -19,15 +19,12 @@ def l3seeds(old):
                             # The STA muons for which seeds are looked for in the tracker
                             MuonCollectionLabel = cms.InputTag("hltL2Muons","UpdatedAtVtx"),
                             #using TrackerSeedCleanerCommon
-                            MuonTrackingRegionBuilder = cms.PSet(
-                             block_hltL3TrajectorySeed
-                            ),
                             # Keep tracks with pT > 1 GeV 
                             PtCut = cms.double(1.0),
                             # The Tracks from which seeds are looked for
                             SeedCollectionLabels = cms.VInputTag(cms.InputTag("pixelTripletSeeds","PixelTriplet"), cms.InputTag("globalPixelSeeds","GlobalPixel"))
                             )
-    else:
+    elif( tsg == "OIState" ):
         return cms.EDProducer("TSGFromL2Muon",
     tkSeedGenerator = cms.string('TSGForRoadSearchOI'),
     TSGFromCombinedHits = cms.PSet(    ),
@@ -41,7 +38,6 @@ def l3seeds(old):
     TSGFromPixelTriplets = cms.PSet(    ),
     MuonCollectionLabel = cms.InputTag("hltL2Muons","UpdatedAtVtx"),
     TSGForRoadSearchOI = cms.PSet(
-#    TkSeedGenerator = cms.PSet(
         propagatorCompatibleName = cms.string('SteppingHelixPropagatorOpposite'),
         option = cms.uint32(3),
         maxChi2 = cms.double(40.0),
@@ -156,8 +152,142 @@ def l3seeds(old):
     PtCut = cms.double(1.0),
     TSGForRoadSearchIOpxl = cms.PSet(    ),
     TSGFromPixelPairs = cms.PSet(    )
+    )
+                              
+    elif( tsg == "OIHit" ):
+        return cms.EDProducer("TSGFromL2Muon",
+    tkSeedGenerator = cms.string('FastTSGFromPropagation'),
+    TSGFromCombinedHits = cms.PSet(    ),
+    ServiceParameters = cms.PSet(
+        RPCLayers = cms.bool(True),
+        UseMuonNavigation = cms.untracked.bool(True),
+        Propagators = cms.untracked.vstring('SteppingHelixPropagatorOpposite', 
+            'SteppingHelixPropagatorAlong', 'PropagatorWithMaterial', 'SmartPropagatorAnyOpposite')
+    ),
+    TSGFromPixelTriplets = cms.PSet(    ),
+    MuonCollectionLabel = cms.InputTag("hltL2Muons","UpdatedAtVtx"),
+    TSGForRoadSearchOI = cms.PSet(),
+    FastTSGFromPropagation = cms.PSet(
+        errorMatrixPset = cms.PSet(
+            atIP = cms.bool(True),
+            action = cms.string('use'),
+            errorMatrixValuesPSet = cms.PSet(
+                pf3_V12 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V13 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V11 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(3, 3, 3, 5, 4, 
+                        5, 10, 7, 10, 10, 
+                        10, 10)
+                ),
+                pf3_V45 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V14 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                yAxis = cms.vdouble(0, 1.0, 1.4, 10),
+                pf3_V15 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V35 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                zAxis = cms.vdouble(-3.14159, 3.14159),
+                pf3_V44 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(3, 3, 3, 5, 4, 
+                        5, 10, 7, 10, 10, 
+                        10, 10)
+                ),
+                xAxis = cms.vdouble(0, 13, 30, 70, 1000),
+                pf3_V23 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V22 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(3, 3, 3, 5, 4, 
+                        5, 10, 7, 10, 10, 
+                        10, 10)
+                ),
+                pf3_V55 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(3, 3, 3, 5, 4, 
+                        5, 10, 7, 10, 10, 
+                        10, 10)
+                ),
+                pf3_V34 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V33 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(3, 3, 3, 5, 4, 
+                        5, 10, 7, 10, 10, 
+                        10, 10)
+                ),
+                pf3_V25 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                ),
+                pf3_V24 = cms.PSet(
+                    action = cms.string('scale'),
+                    values = cms.vdouble(1, 1, 1, 1, 1, 
+                        1, 1, 1, 1, 1, 
+                        1, 1)
+                )
+            )
+        ),
+	ComponentName = cms.string( "FastTSGFromPropagation" ),       
+	Propagator = cms.string( "SmartPropagatorAnyOpposite" ),
+	MaxChi2 = cms.double( 15.0 ),
+	ResetMethod = cms.string("matrix"),
+	ErrorRescaling = cms.double(3.0),
+	SigmaZ = cms.double(25.0),       
+        SimTrackCollectionLabel = cms.InputTag("famosSimHits"),
+	HitProducer = cms.InputTag("siTrackerGaussianSmearingRecHits","TrackerGSMatchedRecHits"),
+	UseVertexState = cms.bool( True ),
+	UpdateState = cms.bool( True ), #fixme
+	SelectState = cms.bool( False )
+    ),
+    MuonTrackingRegionBuilder = cms.PSet(    ),
+    TSGFromMixedPairs = cms.PSet(    ),
+    PCut = cms.double(2.5),
+    TrackerSeedCleaner = cms.PSet(    ),
+    PtCut = cms.double(1.0),
+    TSGForRoadSearchIOpxl = cms.PSet(    ),
+    TSGFromPixelPairs = cms.PSet(    )
                               
 )
                                      
-hltL3TrajectorySeed = l3seeds(False)
+hltL3TrajectorySeed = l3seeds("OIHit")
 
