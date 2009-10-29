051b1a41afe494ee46ede8255377388407b93760
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-10-29 22:42:51 +0000

    Update for OIHit fastsim

--- FastSimulation/Muons/plugins/SealModules.cc
+++ FastSimulation/Muons/plugins/SealModules.cc
@@ -2,8 +2,12 @@
 #include "FastSimulation/Muons/plugins/FastL3MuonProducer.h"
 #include "FastSimulation/Muons/plugins/FastTSGFromL2Muon.h"
 #include "FastSimulation/Muons/plugins/FastL1MuonProducer.h"
+#include "FastSimulation/Muons/plugins/FastTSGFromPropagation.h"
+#include "RecoMuon/TrackerSeedGenerator/interface/TrackerSeedGeneratorFactory.h"
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(FastL1MuonProducer);
 DEFINE_ANOTHER_FWK_MODULE(FastTSGFromL2Muon);
 DEFINE_ANOTHER_FWK_MODULE(FastL3MuonProducer);
+DEFINE_EDM_PLUGIN(TrackerSeedGeneratorFactory, FastTSGFromPropagation, "FastTSGFromPropagation");
+
