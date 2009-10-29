0a8602ae65a64b19dc7f0269711998beddafbece
fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch>
2009-10-29 10:34:40 +0000

    fix a bug on DATAMIXER event content

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -30,7 +30,7 @@ import FWCore.ParameterSet.Config as cms
 #
 #  FEVT (RAW+RECO), FEVTSIM (RAWSIM+RECOSIM), FEVTDEBUG (FEVTSIM+ALL_SIM_INFO), FEVTDEBUGHLT (FEVTDEBUG+HLTDEBUG)
 #
-#  $Id: EventContent_cff.py,v 1.21 2009/07/31 12:29:06 fambrogl Exp $
+#  $Id: EventContent_cff.py,v 1.22 2009/10/05 16:47:19 futyand Exp $
 #
 #
 #
@@ -265,7 +265,7 @@ DATAMIXEREventContent = cms.PSet(
                                                'keep CSCDetIdCSCWireDigiMuonDigiCollection_muonCSCDigis_MuonCSCWireDigi_*',
                                                'keep DTLayerIdDTDigiMuonDigiCollection_muonDTDigis_*_*',
                                                'keep PixelDigiedmDetSetVector_siPixelDigis_*_*',
-                                               'keep StripDigiedmDetSetVector_siStripDigis_*_*',
+                                               'keep SiStripDigiedmDetSetVector_siStripDigis_*_*',
                                                'keep RPCDetIdRPCDigiMuonDigiCollection_muonRPCDigis_*_*',
                                                'keep HBHEDataFramesSorted_hcalDigis_*_*',
                                                'keep HFDataFramesSorted_hcalDigis_*_*',
