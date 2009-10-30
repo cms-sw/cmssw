1187e17e60d8740198d1f8f77318d96ace41ed08
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-10-30 12:41:58 +0000

    update pedestal task

--- DQM/EcalPreshowerMonitorModule/interface/ESPedestalTask.h
+++ DQM/EcalPreshowerMonitorModule/interface/ESPedestalTask.h
@@ -1,41 +1,46 @@
 #ifndef ESPedestalTask_H
 #define ESPedestalTask_H
 
-
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-//
-// class decleration
-//
 class MonitorElement;
 class DQMStore;
 
 class ESPedestalTask : public edm::EDAnalyzer {
+
    public:
+
       ESPedestalTask(const edm::ParameterSet& ps);
       virtual ~ESPedestalTask();
 
-
    private:
-      virtual void beginJob(const edm::EventSetup & c);
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob(void);
 
-      // ----------member data ---------------------------
+      void beginJob(const edm::EventSetup & c);
+      void analyze(const edm::Event&, const edm::EventSetup&);
+      void endJob(void);
+      void setup(void);
+      void reset(void);
+      void cleanup(void);
+      void beginRun(const edm::Run & r, const edm::EventSetup & c);
+      void endRun(const edm::Run& r, const edm::EventSetup& c);
+
       edm::InputTag digilabel_;
       edm::FileInPath lookup_;
       std::string outputFile_;
       std::string prefixME_;
 
+      bool enableCleanup_;
+      bool mergeRuns_;
 
       DQMStore* dqmStore_;
-      MonitorElement* hADC_[2][2][40][40][32];
+      MonitorElement* meADC_[4288][32];
 
-      int nLines_, runNum_, eCount_; 
+      bool init_;
+      int nLines_, runNum_, ievt_, senCount_[2][2][40][40]; 
       int runtype_, seqtype_, dac_, gain_, precision_;
-      int firstDAC_, nDAC_, isPed_, vDAC_[5], layer_;
+      int firstDAC_, nDAC_, isPed_, vDAC_[5];
 
 };
 
--- DQM/EcalPreshowerMonitorModule/src/ESPedestalTask.cc
+++ DQM/EcalPreshowerMonitorModule/src/ESPedestalTask.cc
@@ -1,9 +1,6 @@
-
 #include <memory>
 #include <fstream>
 #include <iostream>
-
-
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -18,137 +15,156 @@
 #include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
 #include "DataFormats/EcalRawData/interface/EcalRawDataCollections.h"
 
-
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "DQMServices/Core/interface/DQMStore.h"
 
 #include "DQM/EcalPreshowerMonitorModule/interface/ESPedestalTask.h"
 
-
 using namespace cms;
 using namespace edm;
 using namespace std;
 
-ESPedestalTask::ESPedestalTask(const edm::ParameterSet& ps)
+ESPedestalTask::ESPedestalTask(const edm::ParameterSet& ps) {
 
-{
+  init_ = false;
 
-   digilabel_    	= ps.getParameter<InputTag>("DigiLabel");
-   lookup_       	= ps.getUntrackedParameter<FileInPath>("LookupTable");
-   outputFile_ 	= ps.getUntrackedParameter<string>("OutputFile","");
-   prefixME_	= ps.getUntrackedParameter<string>("prefixME", "EcalPreshower"); 
+  dqmStore_	= Service<DQMStore>().operator->();
+  
+  digilabel_ 	= ps.getParameter<InputTag>("DigiLabel");
+  lookup_     	= ps.getUntrackedParameter<FileInPath>("LookupTable");
+  outputFile_ 	= ps.getUntrackedParameter<string>("OutputFile","");
+  prefixME_	= ps.getUntrackedParameter<string>("prefixME", "EcalPreshower"); 
 
-   dqmStore_	= Service<DQMStore>().operator->();
-
-   int iz, ip, ix, iy, fed, kchip, pace, bundle, fiber, optorx;
-
-   int senZ_[4288], senP_[4288], senX_[4288], senY_[4288];
-
-
-   // read in look-up table
-   ifstream file;
-   file.open(lookup_.fullPath().c_str());
-   if( file.is_open() ) {
-
-      file >> nLines_;
-
-      for (int i=0; i<nLines_; ++i) {
-	 file>> iz >> ip >> ix >> iy >> fed >> kchip >> pace >> bundle >> fiber >> optorx;
-
-	 senZ_[i] = iz;
-	 senP_[i] = ip;
-	 senX_[i] = ix;
-	 senY_[i] = iy;
-      }
-   } 
-   else {
-      cout<<"ESUnpackerV4::ESUnpackerV4 : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<endl;
-   }
-
-
-
-   //Histogram init  
-   dqmStore_->setCurrentFolder(prefixME_ + "/ESPedestalTask");
-
-
-   for(int i=0; i<2 ; i++){
-      for(int j=0; j<2 ; j++){
-	 for(int k=0; k<40 ; k++){	
-	    for(int l=0; l<40 ; l++){
-	       for(int m=0; m<32 ; m++){
-		  hADC_[i][j][k][l][m] = 0;
-	       }}}}}
-
-
-   char hname[300];
-   int count = 0;
-   for (int i=0; i<nLines_; ++i) {
-      iz = (senZ_[i]==1) ? 0:1;
-      for (int is=0; is<32; ++is) {
-	 sprintf(hname, "ADC Z %d P %d X %d Y %d Str %d", senZ_[i], senP_[i], senX_[i], senY_[i], is+1);
-	 hADC_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1][is] = dqmStore_->book1D(hname, hname, 4100, 0, 4100);
-	 dqmStore_->tag(hADC_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1][is]->getFullname(),++count);
-      }
-   }
+  for (int i=0; i<2; ++i) 
+    for (int j=0; j<2; ++j) 
+      for (int k=0; k<40; ++k)
+	for (int l=0; l<40; ++l)
+	  senCount_[i][j][k][l] = -1;
+  
 }
 
 ESPedestalTask::~ESPedestalTask() {
 }
 
-void ESPedestalTask::analyze(const edm::Event& e, const edm::EventSetup& iSetup) {
-
-   runNum_ = e.id().run();
-   eCount_++;
-
-   Handle<ESDigiCollection> digis;
-   if ( e.getByLabel(digilabel_, digis) ) {
-
-     runtype_ = 1; // Let runtype_ = 1
-     
-     // Digis
-     int zside, plane, ix, iy, strip, iz;
-     for (ESDigiCollection::const_iterator digiItr = digis->begin(); digiItr != digis->end(); ++digiItr) {
-       
-       ESDataFrame dataframe = (*digiItr);
-       ESDetId id = dataframe.id();
-       
-       zside = id.zside();
-       plane = id.plane();
-       ix    = id.six();
-       iy    = id.siy();
-       strip = id.strip();
-       iz = (zside==1) ? 0:1;
-       
-       layer_ = plane;
-       
-       
-       if(hADC_[iz][plane-1][ix-1][iy-1][strip-1]){
-	 if(runtype_ == 1){		
-	   hADC_[iz][plane-1][ix-1][iy-1][strip-1]->Fill(dataframe.sample(0).adc());
-	   hADC_[iz][plane-1][ix-1][iy-1][strip-1]->Fill(dataframe.sample(1).adc());
-	   hADC_[iz][plane-1][ix-1][iy-1][strip-1]->Fill(dataframe.sample(2).adc());
-	 } else if(runtype_ == 3) {
-	   hADC_[iz][plane-1][ix-1][iy-1][strip-1]->Fill(dataframe.sample(1).adc());
-	 }
-       }	
-     }
-   } else {
-     LogWarning("ESPedestalTask") << digilabel_ << " not available";
-   }     
-
-}
-
 void ESPedestalTask::beginJob(const edm::EventSetup & c) {
+  ievt_ = 0;
 }
 
-void ESPedestalTask::endJob() {
+void ESPedestalTask::beginRun(const Run& r, const EventSetup& c) {
 
-   if(outputFile_.size() != 0){
-      cout<<"Save result to "<<outputFile_<<endl;
-      dqmStore_->save(outputFile_);
-   }
+  if ( ! mergeRuns_ ) this->reset();
 
 }
 
-//define this as a plug-in
+void ESPedestalTask::endRun(const Run& r, const EventSetup& c) {
+}
+
+void ESPedestalTask::reset(void) {
+
+}
+
+void ESPedestalTask::setup(void) {
+  
+  init_ = true;
+
+  int iz, ip, ix, iy, fed, kchip, pace, bundle, fiber, optorx;
+  int senZ_[4288], senP_[4288], senX_[4288], senY_[4288];
+  
+  // read in look-up table
+  ifstream file;
+  file.open(lookup_.fullPath().c_str());
+  if( file.is_open() ) {
+    
+    file >> nLines_;
+    
+    for (int i=0; i<nLines_; ++i) {
+      file>> iz >> ip >> ix >> iy >> fed >> kchip >> pace >> bundle >> fiber >> optorx;
+      
+      senZ_[i] = iz;
+      senP_[i] = ip;
+      senX_[i] = ix;
+      senY_[i] = iy;
+
+      iz = (senZ_[i]==1) ? 0:1;
+      senCount_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1] = i; 
+    }
+  } 
+  else {
+    cout<<"ESPedestalTask : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<endl;
+  }
+ 
+  char hname[300];
+  
+  if (dqmStore_) {
+    dqmStore_->setCurrentFolder(prefixME_ + "/ESPedestalTask");
+    
+    for (int i=0; i<nLines_; ++i) {
+      for (int is=0; is<32; ++is) {
+	sprintf(hname, "ADC Z %d P %d X %d Y %d Str %d", senZ_[i], senP_[i], senX_[i], senY_[i], is+1);
+	meADC_[i][is] = dqmStore_->book1D(hname, hname, 1000, 899.5, 1899.5);
+      }
+    }
+  }
+
+}
+
+void ESPedestalTask::cleanup(void){
+
+  if ( ! init_ ) return;
+
+  init_ = false;
+}
+
+void ESPedestalTask::endJob(void) {
+
+  LogInfo("ESPedestalTask") << "analyzed " << ievt_ << " events";
+
+  if ( enableCleanup_ ) this->cleanup();
+
+}
+
+void ESPedestalTask::analyze(const edm::Event& e, const edm::EventSetup& iSetup) {
+  
+  if ( ! init_ ) this->setup();
+
+  ievt_++;  
+  runNum_ = e.id().run();
+  
+  Handle<ESDigiCollection> digis;
+  try {
+    e.getByLabel(digilabel_, digis);
+  } catch ( cms::Exception &e ) {
+    LogDebug("") << "Error! can't get digi collection !" << std::endl;
+  }
+  
+  runtype_ = 1; // Let runtype_ = 1
+  
+  // Digis
+  int zside, plane, ix, iy, strip, iz;
+  for (ESDigiCollection::const_iterator digiItr = digis->begin(); digiItr != digis->end(); ++digiItr) {
+    
+    ESDataFrame dataframe = (*digiItr);
+    ESDetId id = dataframe.id();
+    
+    zside = id.zside();
+    plane = id.plane();
+    ix    = id.six();
+    iy    = id.siy();
+    strip = id.strip();
+    iz = (zside==1) ? 0:1;
+
+    if (meADC_[senCount_[iz][plane-1][ix-1][iy-1]][strip-1]) {
+      if(runtype_ == 1){		
+	meADC_[senCount_[iz][plane-1][ix-1][iy-1]][strip-1]->Fill(dataframe.sample(0).adc());
+	meADC_[senCount_[iz][plane-1][ix-1][iy-1]][strip-1]->Fill(dataframe.sample(1).adc());
+	meADC_[senCount_[iz][plane-1][ix-1][iy-1]][strip-1]->Fill(dataframe.sample(2).adc());
+      } else if(runtype_ == 3) {
+	meADC_[senCount_[iz][plane-1][ix-1][iy-1]][strip-1]->Fill(dataframe.sample(1).adc());
+      }
+    }	
+
+  }
+  
+}
+
 DEFINE_FWK_MODULE(ESPedestalTask);
