52e413b7709839c0af46e4217eea8663a760e389
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-10-30 14:04:05 +0000

    update pedestal client

--- DQM/EcalPreshowerMonitorClient/src/ESPedestalClient.cc
+++ DQM/EcalPreshowerMonitorClient/src/ESPedestalClient.cc
@@ -4,48 +4,37 @@
 #include <string>
 #include <vector>
 
-
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "DQMServices/Core/interface/DQMStore.h"
-
 
 #include "DQM/EcalPreshowerMonitorClient/interface/ESPedestalClient.h"
 
 #include <TH1F.h>
 
-
 using namespace cms;
 using namespace edm;
 using namespace std;
 
-ESPedestalClient::ESPedestalClient(const edm::ParameterSet& ps){
-
+ESPedestalClient::ESPedestalClient(const edm::ParameterSet& ps) {
 
    verbose_       = ps.getUntrackedParameter<bool>("verbose", true);
    debug_         = ps.getUntrackedParameter<bool>("debug", false);
-   prefixME_	= ps.getUntrackedParameter<string>("prefixME", "EcalPreshower");
-   lookup_		= ps.getUntrackedParameter<FileInPath>("LookupTable");
+   prefixME_	  = ps.getUntrackedParameter<string>("prefixME", "EcalPreshower");
+   lookup_	  = ps.getUntrackedParameter<FileInPath>("LookupTable");
    enableCleanup_ = ps.getUntrackedParameter<bool>("enableCleanup", false);
-
-   fitPedestal_ = ps.getUntrackedParameter<bool>("fitPedestal", false);
-
-
-
+   fitPedestal_   = ps.getUntrackedParameter<bool>("fitPedestal", false);
 
    for (int i=0; i<2; i++) 
       for (int j=0; j<2; j++) 
 	 for (int k=0; k<40; k++) 
-	    for (int m=0; m<40; m++){
+	    for (int m=0; m<40; m++) {
 	       hPed_[i][j][k][m] = 0;
 	       hTotN_[i][j][k][m] = 0;
 	    }
 
-
 }
 
-
-ESPedestalClient::~ESPedestalClient(){
-
+ESPedestalClient::~ESPedestalClient() {
 }
 
 void ESPedestalClient::beginJob(DQMStore* dqmStore) {
@@ -83,7 +72,6 @@ void ESPedestalClient::endRun(void) {
 
 void ESPedestalClient::setup(void) {
 
-
    // read in look-up table
    int iz, ip, ix, iy, fed, kchip, pace, bundle, fiber, optorx;
    ifstream file;
@@ -103,36 +91,22 @@ void ESPedestalClient::setup(void) {
       }
 
    } else {
-      cout<<"ESUnpackerV4::ESUnpackerV4 : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<endl;
+      cout<<"ESPedestalClient : Look up table file can not be found in "<<lookup_.fullPath().c_str()<<endl;
    }
 
    // define histograms
    dqmStore_->setCurrentFolder(prefixME_+"/ESPedestalClient");
 
    char hname[300];
-   MonitorElement *me;
    for (int i=0; i<nLines_; ++i) {
-      int taskNStp = 0;
+
       iz = (senZ_[i]==1) ? 0:1;
 
-      for (int is=0; is<32; ++is) {
-	 string dirname = prefixME_ + "/ESPedestalTask/";
-	 sprintf(hname, "ADC Z %d P %d X %d Y %d Str %d", senZ_[i], senP_[i], senX_[i], senY_[i], is+1);
-	 me = dqmStore_->get(dirname+hname);
-	 if (me==0) continue;
-	 taskNStp++;
-      }
-
-
-      if(taskNStp!=0){
-
-	 sprintf(hname, "Ped Z %d P %d X %d Y %d", senZ_[i], senP_[i], senX_[i], senY_[i]);
-	 hPed_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1] = dqmStore_->book1D(hname, hname, 32, 0, 32);
-
-	 sprintf(hname, "Total Noise Z %d P %d X %d Y %d", senZ_[i], senP_[i], senX_[i], senY_[i]);
-	 hTotN_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1] = dqmStore_->book1D(hname, hname, 32, 0, 32);
-
-      }
+      sprintf(hname, "Ped Z %d P %d X %d Y %d", senZ_[i], senP_[i], senX_[i], senY_[i]);
+      hPed_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1] = dqmStore_->book1D(hname, hname, 32, 0, 32);
+      
+      sprintf(hname, "Total Noise Z %d P %d X %d Y %d", senZ_[i], senP_[i], senX_[i], senY_[i]);
+      hTotN_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1] = dqmStore_->book1D(hname, hname, 32, 0, 32);
    }
 
    fg = new TF1("fg", "gaus");
@@ -146,36 +120,37 @@ void ESPedestalClient::cleanup(void) {
    for (int i=0; i<2; i++)
       for (int j=0; j<2; j++)
 	 for (int k=0; k<40; k++)
-	    for (int m=0; m<40; m++){
+	    for (int m=0; m<40; m++) {
 	       hPed_[i][j][k][m] = 0;
 	       hTotN_[i][j][k][m] = 0;
 	    }
 
-
-
 }
 
-void ESPedestalClient::analyze()
-{
+void ESPedestalClient::analyze() {
+
    ievt_++;
    jevt_++;
 
    // Preform pedestal fit
    char hname[300];
-   MonitorElement *me;
    int iz = 0;
-   if(fitPedestal_){
+   if (fitPedestal_) {
+
       if ( verbose_ ) cout<<"ESPedestalClient: Fit Pedestal"<<endl;
+
       for (int i=0; i<nLines_; ++i) {
+
 	 iz = (senZ_[i]==1) ? 0:1; 
+
 	 for (int is=0; is<32; ++is) {
 
 	    string dirname = prefixME_ + "/ESPedestalTask/";
 	    sprintf(hname, "ADC Z %d P %d X %d Y %d Str %d", senZ_[i], senP_[i], senX_[i], senY_[i], is+1);
-	    me = dqmStore_->get(dirname+hname);
+	    MonitorElement *meFit = dqmStore_->get(dirname+hname);
 
-	    if (me==0) continue;
-	    TH1F *rootHisto = me->getTH1F();
+	    if (meFit==0) continue;
+	    TH1F *rootHisto = meFit->getTH1F();
 	    rootHisto->Fit("fg", "Q", "", 500, 1800);
 	    rootHisto->Fit("fg", "RQ", "", fg->GetParameter(1)-2.*fg->GetParameter(2),fg->GetParameter(1)+2.*fg->GetParameter(2));
 	    hPed_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1]->setBinContent(is+1, (int)(fg->GetParameter(1)+0.5));
@@ -183,24 +158,28 @@ void ESPedestalClient::analyze()
 
 	 }
       } 
-   }
 
-   else{
+   } else {
+
       if ( verbose_ ) cout<<"ESPedestalClient: Use Histogram Mean"<<endl;
+
       for (int i=0; i<nLines_; ++i) {
+
 	 iz = (senZ_[i]==1) ? 0:1; 
+
 	 for (int is=0; is<32; ++is) {
 
 	    string dirname = prefixME_ + "/ESPedestalTask/";
 	    sprintf(hname, "ADC Z %d P %d X %d Y %d Str %d", senZ_[i], senP_[i], senX_[i], senY_[i], is+1);
-	    me = dqmStore_->get(dirname+hname);
+	    MonitorElement *meMean = dqmStore_->get(dirname+hname);
 
-	    if (me==0) continue;
-	    TH1F *rootHisto = me->getTH1F();
+	    if (meMean==0) continue;
+	    TH1F *rootHisto = meMean->getTH1F();
 	    hPed_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1]->setBinContent(is+1, (int)(rootHisto->GetMean()+0.5));
 	    hTotN_[iz][senP_[i]-1][senX_[i]-1][senY_[i]-1]->setBinContent(is+1, rootHisto->GetRMS());
 
 	 } 
       }
    }
+
 }
