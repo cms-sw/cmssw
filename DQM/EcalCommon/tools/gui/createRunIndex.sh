92b26c0712ea04aa1622a0d985c3847815ebf82d
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-11-03 08:23:33 +0000

    upgrade to DQM GUI 5.1.1

--- DQM/EcalCommon/tools/gui/createRunIndex.sh
+++ DQM/EcalCommon/tools/gui/createRunIndex.sh
@@ -6,7 +6,7 @@ cd ${HOME}/DQM/dqm-GUI
 
 LD_LIBRARY_PATH=
 source rpms/cmsset_default.sh
-source rpms/slc4_ia32_gcc345/cms/dqmgui/5.1.0/etc/profile.d/env.sh
+source rpms/slc4_ia32_gcc345/cms/dqmgui/5.1.1/etc/profile.d/env.sh
 
 if [ -e /tmp/createRunIndex.lock ]; then
   echo "Lock file is present, exit"
