ad907e98724e57717a06f7f779d140a88fd63f68
Wing To <sha1-74de9817a2e897af4c0ccf44ba0c9b943cf645fd@cern.ch>
2009-11-03 17:37:58 +0000

    Added PedsFullNoise

---
+++ DQM/SiStripCommissioningClients/interface/PedsFullNoiseHistograms.h
@@ -0,0 +1,23 @@
+#ifndef DQM_SiStripCommissioningClients_PedsFullNoiseHistograms_H
+#define DQM_SiStripCommissioningClients_PedsFullNoiseHistograms_H
+
+#include "DQM/SiStripCommissioningClients/interface/CommissioningHistograms.h"
+
+class DQMOldReceiver;
+class DQMStore;
+
+class PedsFullNoiseHistograms : public virtual CommissioningHistograms {
+
+  public:
+
+    PedsFullNoiseHistograms( const edm::ParameterSet& pset, DQMOldReceiver* );
+    PedsFullNoiseHistograms( const edm::ParameterSet& pset, DQMStore* );
+    virtual ~PedsFullNoiseHistograms();
+ 
+    void histoAnalysis( bool debug );
+
+    void printAnalyses(); // override
+
+};
+
+#endif // DQM_SiStripCommissioningClients_PedsFullNoiseHistograms_H
