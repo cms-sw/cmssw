0f363afc014116fcd28271edee14f6629f443288
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2009-11-03 22:19:40 +0000

    update beam parameters for 900GeV, 7TeV collisions, and add 2.2 TeV beam conditions

--- IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
@@ -40,16 +40,16 @@ FlatVtxSmearingParameters = cms.PSet(
 # alpha = angle of the crossing plane 0 degrees means XZ plane
 # phi = half-crossing beam angle
 #
-# Emittance is the no normalized emittance in cm
-#
+# Emittance is the no normalized emittance in cm = normalized emittance/gamma (beta=1)
+# 
 # length variables are in cm
 #
 
-# 900 GeV collisions, transverse beam size = 283.2 microns 
+# 900 GeV collisions, transverse beam size = 293 microns 
 Early900GeVCollisionVtxSmearingParameters = cms.PSet(
     Phi = cms.double(0.0),
     BetaStar = cms.double(1100.0),
-    Emittance = cms.double(7.29e-07),
+    Emittance = cms.double(7.82e-07),
     Alpha = cms.double(0.0),
     SigmaZ = cms.double(7.4),
     TimeOffset = cms.double(0.0),
@@ -57,9 +57,32 @@ Early900GeVCollisionVtxSmearingParameters = cms.PSet(
     X0 = cms.double(0.0322),
     Z0 = cms.double(0.0)
 )
-#  7 TeV collisions, transverse beam size with betastar=  2m is  44.8 microns,
-#                                         with betastar= 11m is 105.2 microns
+#  2.2 TeV collisions, transverse beam size 188 microns
 Early7TeVCollisionVtxSmearingParameters = cms.PSet(
+    Phi = cms.double(0.0),
+    BetaStar = cms.double(1100.0),
+    Emittance = cms.double(3.2e-07),
+    Alpha = cms.double(0.0),
+    SigmaZ = cms.double(5.5),
+    TimeOffset = cms.double(0.0),
+    Y0 = cms.double(0.0),
+    X0 = cms.double(0.0322),
+    Z0 = cms.double(0.0)
+)
+#  7 TeV collisions, transverse beam size with betastar=  11m is 105 microns,
+Early7TeVCollisionVtxSmearingParameters = cms.PSet(
+    Phi = cms.double(0.0),
+    BetaStar = cms.double(1100.0),
+    Emittance = cms.double(1.0e-07),
+    Alpha = cms.double(0.0),
+    SigmaZ = cms.double(4.2),
+    TimeOffset = cms.double(0.0),
+    Y0 = cms.double(0.0),
+    X0 = cms.double(0.0322),
+    Z0 = cms.double(0.0)
+)
+#  7 TeV collisions, transverse beam size with betastar=  2m is  45 microns,
+Nominal7TeVCollisionVtxSmearingParameters = cms.PSet(
     Phi = cms.double(0.0),
     BetaStar = cms.double(200.0),
     Emittance = cms.double(1.0e-07),
