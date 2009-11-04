a41f263d977c0926d30a424aa0c2bc71d36b7d7f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-11-04 14:40:38 +0000

    try labels

--- CondCore/PopCon/test/print_eff_example.py
+++ CondCore/PopCon/test/print_eff_example.py
@@ -7,6 +7,7 @@ process.CondDBCommon.connect = 'sqlite_file:pop_test.db'
 process.eff = cms.ESSource("PoolDBESSource",
     process.CondDBCommon,
     RefreshEachRun=cms.untracked.bool(True),
+    DumpStat=cms.untracked.bool(True),
     toGet = cms.VPSet(
     cms.PSet(
         record = cms.string('ExEfficiencyRcd'),
