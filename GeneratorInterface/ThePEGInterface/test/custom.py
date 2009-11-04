97475b314dbf9ffec8be9646a0ca66836319b20e
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2009-11-04 18:48:24 +0000

    comments about process.genParticles.abortOnUnknownPDGCode

--- GeneratorInterface/ThePEGInterface/test/custom.py
+++ GeneratorInterface/ThePEGInterface/test/custom.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+# This statement is necessary to make sure CMSSW doesn't throw an exception
+# due to its outdated particle table
 def customise(process):
 	process.genParticles.abortOnUnknownPDGCode = False
 
--- GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
+++ GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
@@ -4,7 +4,8 @@
 MYPATH="Configuration/GenProduction"
 CONDITIONS="FrontierConditions_GlobalTag,MC_31X_V9::All"
 
-CMSOPTS="-n 100 --eventcontent RAWSIM --conditions $CONDITIONS $CMSOPTS --no_exec --mc --customise=$MYPATH/custom"
+# The customise parameter is important - without it the GEN step will fail!
+CMSOPTS="-n 100 --eventcontent RAWSIM --conditions $CONDITIONS $CMSOPTS --no_exec --mc --customise=$MYPATH/custom.py"
 
 cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN --datatier GEN $CMSOPTS
 cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN-SIM-RAW $CMSOPTS
