debf7c8854575da01da0e69a5b35ae52862ef439
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-11-04 14:19:48 +0000

    fixed

--- CondCore/DBCommon/interface/DBSession.h
+++ CondCore/DBCommon/interface/DBSession.h
@@ -1,112 +1,62 @@
-#ifndef COND_DBCommon_DbSession_h
-#define COND_DBCommon_DbSession_h
-
-#include <string>
-#include <boost/shared_ptr.hpp>
-#include "DataSvc/Ref.h"
-
-
+#ifndef COND_DBCommon_DBSession_h
+#define COND_DBCommon_DBSession_h
 //
 // Package:    CondCore/DBCommon
-// Class:      DbSession
+// Class:      DBSession
 //
-/**\class DbSession DbSession.h CondCore/DBCommon/interface/DbSession.h
+/**\class DBSession DBSession.h CondCore/DBCommon/interface/DBSession.h
  Description: Class to prepare database connection setup
 */
+//
+// Author:      Zhen Xie
+//
+#include <string>
+//#include "CoralKernel/Context.h"
+//#include "CoralKernel/IHandle.h"
 
-namespace coral {
+#include "CondCore/DBCommon/interface/SessionConfiguration.h"
+#include "CondCore/DBCommon/interface/CoralServiceManager.h"
+
+
+namespace coral{
   class IConnectionService;
-  class ISchema;
-  class ISessionProxy;
+  class IRelationalService;
+  class IAuthenticationService;
+  class IMonitoringReporter;
+  class IWebCacheControl;
 }
-
-namespace pool {
-  class IDataSvc;
+namespace pool{
+  class IBlobStreamingService;
 }
-
 namespace cond{
 
-  class DbTransaction;
-  class DbConnection;
-  
+  // session configurartion, later more code, now just one set of defaults
+  // move to SessionConfiguration?
+  enum ConfDefaults { coralDefaults, cmsDefaults, prodDefaults, toolDefaults, webDefaults}; 
+
   /*
   **/
-  class DbSession{
+  class DBSession{
   public:
-    DbSession();
+    DBSession(ConfDefaults confDef=cmsDefaults);
+    ~DBSession();
+    void open();
+    //void close();
 
-    explicit DbSession( const DbConnection& connection );
+    void config(ConfDefaults confDef);
 
-    DbSession( const DbSession& rhs );
-
-    virtual ~DbSession();
-
-    DbSession& operator=( const DbSession& rhs );
-
-    const std::string& connectionString() const;
-
-    void setBlobStreamingService( const std::string& streamingService );
-    
-    const std::string& blobStreamingService() const;
-
-    void open( const std::string& connectionString, bool readOnly=false );
-
-    void close();
-
-    bool isOpen() const;
-
-    DbTransaction& transaction();
-
-    coral::ISchema& schema(const std::string& schemaName);
-
-    coral::ISchema& nominalSchema();
-
-    coral::ISessionProxy& coralSession();
-
-    pool::IDataSvc& poolCache();
-
-    bool initializeMapping(const std::string& mappingVersion, const std::string& xmlStream);
-
-    bool deleteMapping( const std::string& mappingVersion, bool removeTables );
-
-    bool importMapping( cond::DbSession& fromDatabase,
-                        const std::string& contName,
-                        const std::string& classVersion="",
-                        bool allVersions=false );
-
-    pool::RefBase getObject( const std::string& objectId );
-
-    template <typename T> pool::Ref<T> getTypedObject( const std::string& objectId );
-
-    template <typename T> pool::Ref<T> storeObject( T* object, const std::string& containerName );
-
-    bool deleteObject( const std::string& objectId );
-
-    std::string importObject( cond::DbSession& fromDatabase, const std::string& objectId );
-
-    private:
-    class SessionImpl;
-
-    private:
-
-    bool storeObject( pool::RefBase& objectRef, const std::string& containerName  );
-
-    private:
-
-    boost::shared_ptr<SessionImpl> m_implementation;
+    coral::IConnectionService& connectionService();
+    coral::IRelationalService& relationalService();
+    coral::IAuthenticationService& authenticationService() ;
+    const coral::IMonitoringReporter& monitoringReporter() const;
+    coral::IWebCacheControl& webCacheControl();
+    pool::IBlobStreamingService& blobStreamingService();
+    cond::SessionConfiguration& configuration();
+  private:
+    //    coral::IHandle<coral::Context> m_context;
+    SessionConfiguration m_sessionConfig;
+    CoralServiceManager m_pluginmanager;
   };
-
-template <typename T> inline pool::Ref<T> DbSession::getTypedObject( const std::string& objectId ){
-  return pool::Ref<T>(&poolCache(),objectId );
-}
-
-template <typename T> inline pool::Ref<T> DbSession::storeObject( T* object, const std::string& containerName ){
-  pool::Ref<T> objectRef(&poolCache(), object );
-  storeObject( objectRef, containerName );
-  return objectRef;
-}
- 
-}
-
+}//ns cond
 #endif
 // DBSESSION_H
