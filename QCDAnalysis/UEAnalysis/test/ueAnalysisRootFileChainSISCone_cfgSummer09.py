3f15d56e384d56ea7d6eb7cfb1cc6687bb78fe20
Andrea Lucaroni <sha1-a17ddc562d56307fc4abaf6c9898c35059089bf9@cern.ch>
2009-11-04 20:58:06 +0000

    it uses trigger tables of 3_1_X HLT1e31

--- QCDAnalysis/UEAnalysis/test/ueAnalysisRootFileChainSISCone_cfgSummer09.py
+++ QCDAnalysis/UEAnalysis/test/ueAnalysisRootFileChainSISCone_cfgSummer09.py
@@ -24,29 +24,22 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
 process.source = cms.Source("PoolSource",
       #                      fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_2_4/RelValMinBias_2M_PROD/GEN-SIM-RECO/MC_31X_V5-v1/0010/34A6537C-0F84-DE11-BD90-001617E30F50.root',
    #     '/store/relval/CMSSW_3_2_4/RelValMinBias_2M_PROD/GEN-SIM-RECO/MC_31X_V5-v1/0009/D0E36D2D-C583-DE11-A8FF-001D09F2426D.root',
  #       '/store/relval/CMSSW_3_2_4/RelValMinBias_2M_PROD/GEN-SIM-RECO/MC_31X_V5-v1/0009/34DA0EF9-C683-DE11-BC85-001D09F231C9.root')
-    fileNames =cms.untracked.vstring('file:/afs/cern.ch/user/l/lucaroni/scratch0/QCDSkim/CMSSW_3_2_4/src/QCDAnalysis/UEAnalysis/test/crab_0_090828_171426/res/lowpTRECOSkim_1.root')
-,
-#skipEvents = cms.untracked.uint32(2)
+    fileNames =cms.untracked.vstring("/store/relval/CMSSW_3_1_2/RelValMinBias_2M_PROD/GEN-SIM-RECO/MC_31X_V3-v1/0007/E03C0F5F-B278-DE11-AAC0-001D09F291D2.root")
 )
 
 process.p1 = cms.Path(process.UEAnalysisParticles*process.UEAnalysisTracks
-#process.p1 = cms.Path(process.UEAnalysisTracks
-          #           + process.UEAnalysisJetsOnlyReco
                       +process.UEAnalysisJets
                       +process.UEAnalysis)
 
 
 process.UEAnalysisRootple.OnlyRECO     = False
-#process.UEAnalysisRootple.OnlyRECO     = True
-#process.UEAnalysisRootple500.OnlyRECO  = True
 process.UEAnalysisRootple500.OnlyRECO  = False
-#process.UEAnalysisRootple1500.OnlyRECO = True
 process.UEAnalysisRootple1500.OnlyRECO = False
 
 process.UEAnalysisRootple.GenJetCollectionName      = 'ueSisCone5GenJet'
