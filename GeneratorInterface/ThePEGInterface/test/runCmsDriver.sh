648e764c0d72d3db5dcacb00df20ce0a56d579bc
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2009-11-04 18:37:15 +0000

    new examples for 34X

--- GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
+++ GeneratorInterface/ThePEGInterface/test/runCmsDriver.sh
@@ -2,11 +2,11 @@
 
 #MYPATH="GeneratorInterface/ThePEGInterface/test"
 MYPATH="Configuration/GenProduction"
-CONDITIONS="FrontierConditions_GlobalTag,IDEAL_31X::All"
+CONDITIONS="FrontierConditions_GlobalTag,MC_31X_V9::All"
 
 CMSOPTS="-n 100 --eventcontent RAWSIM --conditions $CONDITIONS $CMSOPTS --no_exec --mc --customise=$MYPATH/custom"
 
-cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN:ProductionFilterSequence --datatier GEN $CMSOPTS
-cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN:ProductionFilterSequence,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN $CMSOPTS
-cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN:ProductionFilterSequence --datatier GEN $CMSOPTS
-cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN:ProductionFilterSequence,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN $CMSOPTS
+cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN --datatier GEN $CMSOPTS
+cmsDriver.py "$MYPATH/testThePEGGeneratorFilter" -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN $CMSOPTS
+cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN --datatier GEN $CMSOPTS
+cmsDriver.py "$MYPATH/testThePEGHadronisation"   -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN $CMSOPTS
