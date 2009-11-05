48e303c63250c2e858aea68c3a5af2b8b7674805
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-11-05 10:19:21 +0000

    now really collisions

--- Configuration/GlobalRuns/python/promptCollisionReco_RAW2DIGI_L1Reco_RECO_DQM_ALCA.py
+++ Configuration/GlobalRuns/python/promptCollisionReco_RAW2DIGI_L1Reco_RECO_DQM_ALCA.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.149 
 # Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: promptReco -s RAW2DIGI,L1Reco,RECO,DQM,ALCA:SiStripCalZeroBias --datatier RECO --eventcontent RECO --conditions CRAFT09_R_V4::All --scenario cosmics --no_exec --data --magField AutoFromDBCurrent -n 100
+# with command line options: promptCollisionReco -s RAW2DIGI,L1Reco,RECO,DQM,ALCA:SiStripCalZeroBias --datatier RECO --eventcontent RECO --conditions CRAFT09_R_V4::All --scenario pp --no_exec --data --magField AutoFromDBCurrent -n 100
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('RECO')
@@ -14,16 +14,16 @@ process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
 process.load('Configuration/StandardSequences/RawToDigi_Data_cff')
 process.load('Configuration/StandardSequences/L1Reco_cff')
-process.load('Configuration/StandardSequences/ReconstructionCosmics_cff')
-process.load('DQMOffline/Configuration/DQMOfflineCosmics_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('DQMOffline/Configuration/DQMOffline_cff')
 process.load('Configuration/StandardSequences/AlCaRecoStreams_cff')
 process.load('Configuration/StandardSequences/EndOfProcess_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.load('Configuration/EventContent/EventContentCosmics_cff')
+process.load('Configuration/EventContent/EventContent_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
-    annotation = cms.untracked.string('promptReco nevts:100'),
+    version = cms.untracked.string('$Revision: 1.149 $'),
+    annotation = cms.untracked.string('promptCollisionReco nevts:100'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 process.maxEvents = cms.untracked.PSet(
@@ -39,6 +39,7 @@ process.source = cms.Source("PoolSource",
 
 # Output definition
 process.FEVT = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
     outputCommands = process.RECOEventContent.outputCommands,
     fileName = cms.untracked.string('promptReco_RAW2DIGI_L1Reco_RECO_DQM_ALCA.root'),
     dataset = cms.untracked.PSet(
@@ -71,8 +72,8 @@ process.GlobalTag.globaltag = 'CRAFT09_R_V4::All'
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
 process.L1Reco_step = cms.Path(process.L1Reco)
-process.reconstruction_step = cms.Path(process.reconstructionCosmics)
-process.dqmoffline_step = cms.Path(process.DQMOfflineCosmics)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.dqmoffline_step = cms.Path(process.DQMOffline)
 process.pathALCARECOHcalCalHOCosmics = cms.Path(process.seqALCARECOHcalCalHOCosmics)
 process.pathALCARECOMuAlStandAloneCosmics = cms.Path(process.seqALCARECOMuAlStandAloneCosmics*process.ALCARECOMuAlStandAloneCosmicsDQM)
 process.pathALCARECOTkAlZMuMu = cms.Path(process.seqALCARECOTkAlZMuMu*process.ALCARECOTkAlZMuMuDQM)
