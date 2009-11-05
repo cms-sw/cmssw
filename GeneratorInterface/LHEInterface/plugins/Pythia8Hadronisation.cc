d30787f020ba9460b04739bc1787a5f5530c5afc
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2009-11-05 15:09:03 +0000

    fix for py8 external 1.30

--- GeneratorInterface/LHEInterface/plugins/Pythia8Hadronisation.cc
+++ GeneratorInterface/LHEInterface/plugins/Pythia8Hadronisation.cc
@@ -116,7 +116,7 @@ bool Pythia8Hadronisation::LHAupLesHouches::setEvent(int inProcId)
 		this->setPdf(pdf->id.first, pdf->id.second,
 		             pdf->x.first, pdf->x.second,
 		             pdf->scalePDF,
-		             pdf->xPDF.first, pdf->xPDF.second);
+		             pdf->xPDF.first, pdf->xPDF.second, true);
 
 	hadronisation->onBeforeHadronisation().emit();
 
