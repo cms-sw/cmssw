bfb30f5c9d3c995cf21ea02e3a07ad7d47b91612
Silvia Goy Lopez <sha1-19636c8d0fff02d4478682d30422a2b12444268e@cern.ch>
2009-11-05 17:21:45 +0000

    Two different cfi for QTs parameters in Collisions and Cosmics

--- DQMOffline/Muon/python/muonQualityTests_cff.py
+++ DQMOffline/Muon/python/muonQualityTests_cff.py
@@ -5,7 +5,8 @@ from DQM.TrackingMonitor.ClientTrackEfficiencySTACosmicMuons_cff import *
 from DQM.TrackingMonitor.ClientTrackEfficiencyTkTracks_cff import *
 from DQMOffline.Muon.trackResidualsTest_cfi import *
 from DQMOffline.Muon.muonRecoTest_cfi import *
-from DQMOffline.Muon.muonTestSummary_cff import *
+from DQMOffline.Muon.muonTestSummary_cfi import *
+from DQMOffline.Muon.muonTestSummaryCosmics_cfi import *
 muonSourcesQualityTests = cms.EDFilter("QualityTester",
     prescaleFactor = cms.untracked.int32(1),
     qtList = cms.untracked.FileInPath('DQMOffline/Muon/data/QualityTests1.xml')
@@ -16,7 +17,7 @@ muonClientsQualityTests = cms.EDFilter("QualityTester",
     qtList = cms.untracked.FileInPath('DQMOffline/Muon/data/QualityTests2.xml')
 )
 
-cosmicMuonQualityTests = cms.Sequence(ClientTrackEfficiencyTkTracks*ClientTrackEfficiencySTACosmicMuons*muonSourcesQualityTests*muTrackResidualsTest*muRecoTest*muonClientsQualityTests*muonTestSummary)
+cosmicMuonQualityTests = cms.Sequence(ClientTrackEfficiencyTkTracks*ClientTrackEfficiencySTACosmicMuons*muonSourcesQualityTests*muTrackResidualsTest*muRecoTest*muonClientsQualityTests*muonCosmicTestSummary)
 
 muonQualityTests = cms.Sequence(muonSourcesQualityTests*muTrackResidualsTest*muRecoTest*muonClientsQualityTests*muonTestSummary)
 
