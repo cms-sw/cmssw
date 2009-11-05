635d7d25a69b6a00ae6e49f19851348174d1749e
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-11-05 22:39:27 +0000

    update for 3_3_0

--- DQM/BeamMonitor/test/DQM_RAW_cfg.py
+++ DQM/BeamMonitor/test/DQM_RAW_cfg.py
@@ -15,7 +15,7 @@ process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/EventContent/EventContent_cff')
 
 ### conditions
-process.GlobalTag.globaltag = 'MC_31X_V3::All'
+process.GlobalTag.globaltag = 'MC_31X_V9::All'
 
 
 process.maxEvents = cms.untracked.PSet(
@@ -24,36 +24,30 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/EC49FE0B-DB90-DE11-8434-001D09F2545B.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/C0F33D87-D990-DE11-8490-000423D6B444.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/B69156C8-D790-DE11-B825-0030487C6090.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/8273D625-D990-DE11-ADA8-0019B9F709A4.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/74268A17-DA90-DE11-8DB4-001D09F2545B.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/5C52DCBD-D790-DE11-BB37-000423D992A4.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/503DBE8C-D990-DE11-9BDC-000423D9939C.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/46D0F7EC-D990-DE11-9EC2-001D09F231B0.root',
-        '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v2/0011/400CCE1C-DA90-DE11-A2B0-001D09F231B0.root'
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/B49925AC-E3BC-DE11-B2B4-00261894395C.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/9E031261-E4BC-DE11-B247-001A92971B68.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/8E5D9592-4EBD-DE11-880E-001A92971B0E.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/7E33DF20-DEBC-DE11-ABF9-002618943875.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/78493C41-E2BC-DE11-86CD-0018F3D096E6.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/6E42191F-DEBC-DE11-B187-00248C0BE013.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/5C2F915E-DDBC-DE11-BD89-001BFCDBD15E.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/463F7C19-E3BC-DE11-A8CC-002618943833.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/2AB8BCFB-E5BC-DE11-B550-00261894389A.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/20D9811D-DEBC-DE11-B67E-002618943829.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/00864FB5-E1BC-DE11-8613-0018F3D0961A.root',
+       '/store/relval/CMSSW_3_3_0/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V9-v2/0002/0012B75C-DDBC-DE11-81E2-002618943921.root'
     )
 )
 
-process.raw2digi_step = cms.Sequence(process.siPixelDigis*
-                                     process.SiStripRawToDigis
-                                     )
-process.tracking_step = cms.Sequence(process.trackerlocalreco+
-                                     process.offlineBeamSpot+
-                                     process.recopixelvertexing*
-                                     process.ckftracks
-                                     )
+process.RecoForDQM = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
 
-process.pt = cms.Path(process.raw2digi_step+process.tracking_step)
-process.pp = cms.Path(process.dqmBeamMonitor+process.dqmBeamCondMonitor+process.dqmEnv+process.dqmSaver)
+process.pp = cms.Path(process.RecoForDQM*process.dqmBeamMonitor+process.dqmBeamCondMonitor+process.dqmEnv+process.dqmSaver)
 
 process.DQMStore.verbose = 0
-process.DQM.collectorHost = 'cmslpc02.fnal.gov'
+process.DQM.collectorHost = 'cmslpc08.fnal.gov'
 process.DQM.collectorPort = 9190
 process.dqmSaver.dirName = '.'
 process.dqmSaver.producer = 'Playback'
-#process.hltResults.plotAll = True
 process.dqmSaver.convention = 'Online'
 process.dqmEnv.subSystemFolder = 'BeamMonitor'
 process.dqmSaver.saveByRun = 1
@@ -64,17 +58,4 @@ process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
     )
 
-process.out = cms.OutputModule("PoolOutputModule",
-                               fileName = cms.untracked.string('test.root'),
-			       outputCommands = cms.untracked.vstring(
-				'drop *',
-				'keep *_offlineBeamSpot_*_*',
-				'keep *_generalTracks_*_*'
-			       )
-                              )
-
-process.end = cms.EndPath(process.out)
-
-#process.schedule = cms.Schedule(process.pt,process.pp,process.end)
-process.schedule = cms.Schedule(process.pt,process.pp)
 
