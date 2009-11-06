3bb3ca3be1df59b9b090f717e225b896fa8e3a85
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2009-11-06 16:28:48 +0000

    Changes required for QcdLowPtDQM

--- DQM/Physics/plugins/plugins.cc
+++ DQM/Physics/plugins/plugins.cc
@@ -4,6 +4,7 @@
 #include "DQM/Physics/src/BPhysicsOniaDQM.h"
 #include "DQM/Physics/src/EwkDQM.h"
 #include "DQM/Physics/src/QcdPhotonsDQM.h"
+#include "DQM/Physics/src/QcdLowPtDQM.h"
 #include "DQM/Physics/src/QcdHighPtDQM.h"
 #include "DQM/Physics/src/TopDiLeptonDQM.h"
 
@@ -11,5 +12,6 @@ DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(BPhysicsOniaDQM);
 DEFINE_ANOTHER_FWK_MODULE(EwkDQM);
 DEFINE_ANOTHER_FWK_MODULE(QcdPhotonsDQM);
+DEFINE_ANOTHER_FWK_MODULE(QcdLowPtDQM);
 DEFINE_ANOTHER_FWK_MODULE(QcdHighPtDQM);
 DEFINE_ANOTHER_FWK_MODULE(TopDiLeptonDQM);
