8014205e43d081c54b6ae665fbbbda4452cbae94
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2009-11-08 04:45:11 +0000

    Add jetMETDQMOfflineClient to postprocessor sequence

--- DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
+++ DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from DQMOffline.JetMET.jetMETDQMOfflineClient_cfi import *
 from DQMOffline.JetMET.dataCertificationJetMET_cfi import *
 
-dataCertificationJetMETSequence = cms.Sequence(qTesterJet + qTesterMET + dataCertificationJetMET)
+dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET + dataCertificationJetMET)
