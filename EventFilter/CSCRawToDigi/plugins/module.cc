651f46ab07b48280623eb63a5d0704f0e0cb67e1
Lindsey Gray <sha1-3ec7abf2a9e8e001c55b41ebd09d9de6b752a31e@cern.ch>
2009-11-09 20:29:44 +0000

    added CSCDigiValidator for packing/unpacking closure tests. Modded buildfile, module.cc accordingly.

--- EventFilter/CSCRawToDigi/plugins/module.cc
+++ EventFilter/CSCRawToDigi/plugins/module.cc
@@ -4,9 +4,11 @@
 #include <EventFilter/CSCRawToDigi/src/CSCDigiToRawModule.h>
 #include <EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h>
 #include <EventFilter/CSCRawToDigi/interface/CSCViewDigi.h>
+#include <EventFilter/CSCRawToDigi/interface/CSCDigiValidator.h>
 
 DEFINE_FWK_MODULE(CSCDCCUnpacker);
 DEFINE_ANOTHER_FWK_MODULE(DigiAnalyzer);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiToRawModule);
 DEFINE_ANOTHER_FWK_MODULE(CSCDigiToPattern);
 DEFINE_ANOTHER_FWK_MODULE(CSCViewDigi);
+DEFINE_ANOTHER_FWK_MODULE(CSCDigiValidator);
