8ec91f787fe7b5b83e9e8cffd737c20bf2f0ee81
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-11-09 16:28:10 +0000

    use LogWarning to report single wrong paths/patterns, and raise a LogError if no valid paths/pattern are given

--- HLTrigger/HLTfilters/src/HLTHighLevelDev.cc
+++ HLTrigger/HLTfilters/src/HLTHighLevelDev.cc
@@ -2,8 +2,8 @@
  *
  * See header file for documentation
  *
- *  $Date: 2009/09/22 15:48:36 $
- *  $Revision: 1.3 $
+ *  $Date: 2009/09/22 15:52:21 $
+ *  $Revision: 1.4 $
  *
  *  \author Martin Grunewald
  *
@@ -77,86 +77,101 @@ HLTHighLevelDev::~HLTHighLevelDev()
 // or in case of paths from eventsetup and IOV changed
 void HLTHighLevelDev::init(const edm::TriggerResults & result, const edm::EventSetup& iSetup)
 {
-   unsigned int n;
-  
-   // clean up old data
-   HLTPathsByName_.clear();
-   HLTPathsByIndex_.clear();
+  unsigned int n;
 
-   // Overwrite paths from EventSetup via AlCaRecoTriggerBitsRcd if configured:
-   if (eventSetupPathsKey_.size()) {
-     HLTPatterns_ = this->pathsFromSetup(eventSetupPathsKey_, iSetup);
-   }
+  // clean up old data
+  HLTPathsByName_.clear();
+  HLTPathsByIndex_.clear();
 
-   // check the HLTPatterns_ and HLTPrescales_ have the same number of elements
-   if (HLTPatterns_.size() != HLTPrescales_.size())
-     throw cms::Exception("Configuration") << "HLTPrescales must have the same number of entries as HLTPatterns.";
+  // Overwrite paths from EventSetup via AlCaRecoTriggerBitsRcd if configured:
+  if (eventSetupPathsKey_.size()) {
+    HLTPatterns_ = this->pathsFromSetup(eventSetupPathsKey_, iSetup);
+  }
 
-   if (HLTPatterns_.empty()) {
-     // for empty input vector, default to all HLT trigger paths
-     n = result.size();
-     HLTPathsByName_.resize(n);
-     HLTPathsByIndex_.resize(n);
-     HLTPrescalesExpanded_.resize(n);
-     for (unsigned int i = 0; i < n; ++i) {
-       HLTPathsByName_[i] = triggerNames_.triggerName(i);
-       HLTPathsByIndex_[i] = i;
-       HLTPrescalesExpanded_[i] = 1;
-     }
-   } else {
-     // otherwise, expand wildcards in trigger names...
-     std::vector<uint32_t>::const_iterator i_scale = HLTPrescales_.begin();
-     BOOST_FOREACH(const std::string & pattern, HLTPatterns_) {
-       uint32_t scale = *(i_scale++);
-       if (edm::is_glob(pattern)) {
-         // found a glob pattern, expand it
-         std::vector< std::vector<std::string>::const_iterator > matches = edm::regexMatch(triggerNames_.triggerNames(), pattern);
-         if (matches.empty()) {
-           // pattern does not match any trigger paths
-           LogDebug("") << "requested pattern does not match any HLT paths: " << pattern;
-         } else {
-           // store the matching patterns
-           BOOST_FOREACH(std::vector<std::string>::const_iterator match, matches) {
-             HLTPathsByName_.push_back(*match);
-             HLTPrescalesExpanded_.push_back(scale);
-           }
-         }
-       } else {
-         // found a trigger name, just copy it
-         HLTPathsByName_.push_back(pattern);
-         HLTPrescalesExpanded_.push_back(scale);
-       }
-     }
-     n = HLTPathsByName_.size();
+  // check the HLTPatterns_ and HLTPrescales_ have the same number of elements
+  if (HLTPatterns_.size() != HLTPrescales_.size())
+    throw cms::Exception("Configuration") << "HLTPrescales must have the same number of entries as HLTPatterns.";
 
-     // resizing and initializing the scalers
-     HLTPrescalesScalers.resize(n, 0);
-     HLTOverallPrescalesScaler_ = 0;
-   
-     // ...and get hold of trigger indices
-     HLTPathsByIndex_.resize(n);
-     for (unsigned int i = 0; i < HLTPathsByName_.size(); i++) {
-       HLTPathsByIndex_[i] = triggerNames_.triggerIndex(HLTPathsByName_[i]);
-       if (HLTPathsByIndex_[i] >= result.size()) {
-         // trigger path not found
-         LogDebug("") << "requested HLT path does not exist: " << HLTPathsByName_[i];
-         HLTPathsByIndex_[i] = (unsigned int) -1;
-       }
-     }
-   }
-   
-   // report on what is finally used
-   LogDebug("") << "HLT trigger paths: " + inputTag_.encode()
-                << " - Number of paths: " << n
-                << " - andOr mode: " << andOr_
-                << " - throw mode: " << throw_;
-   
-   LogTrace("") << "The HLT trigger paths (# index name):";
-   for (unsigned int i = 0; i < n; ++i)
-     if (HLTPathsByIndex_[i] == (unsigned int) -1)
-       LogTrace("") << "    n/a   " << HLTPathsByName_[i];
-     else
-       LogTrace("") << "    " << std::setw(4) << HLTPathsByIndex_[i] << "  " << HLTPathsByName_[i];
+  if (HLTPatterns_.empty()) {
+    // for empty input vector, default to all HLT trigger paths
+    n = result.size();
+    HLTPathsByName_.resize(n);
+    HLTPathsByIndex_.resize(n);
+    HLTPrescalesExpanded_.resize(n);
+    for (unsigned int i = 0; i < n; ++i) {
+      HLTPathsByName_[i] = triggerNames_.triggerName(i);
+      HLTPathsByIndex_[i] = i;
+      HLTPrescalesExpanded_[i] = 1;
+    }
+  } else {
+    // otherwise, expand wildcards in trigger names...
+    std::vector<uint32_t>::const_iterator i_scale = HLTPrescales_.begin();
+    BOOST_FOREACH(const std::string & pattern, HLTPatterns_) {
+      uint32_t scale = *(i_scale++);
+      if (edm::is_glob(pattern)) {
+        // found a glob pattern, expand it
+        std::vector< std::vector<std::string>::const_iterator > matches = edm::regexMatch(triggerNames_.triggerNames(), pattern);
+        if (matches.empty()) {
+          // pattern does not match any trigger paths
+          if (throw_)
+            throw cms::Exception("Configuration") << "requested pattern \"" << pattern <<  "\"does not match any HLT paths";
+          else
+            edm::LogWarning("Configuration") << "requested pattern \"" << pattern <<  "\"does not match any HLT paths";
+        } else {
+          // store the matching patterns
+          BOOST_FOREACH(std::vector<std::string>::const_iterator match, matches) {
+            HLTPathsByName_.push_back(*match);
+            HLTPrescalesExpanded_.push_back(scale);
+          }
+        }
+      } else {
+        // found a trigger name, just copy it
+        HLTPathsByName_.push_back(pattern);
+        HLTPrescalesExpanded_.push_back(scale);
+      }
+    }
+    n = HLTPathsByName_.size();
+
+    // resizing and initializing the scalers
+    HLTPrescalesScalers.resize(n, 0);
+    HLTOverallPrescalesScaler_ = 0;
+
+    // ...and get hold of trigger indices
+    bool valid = false;
+    HLTPathsByIndex_.resize(n);
+    for (unsigned int i = 0; i < HLTPathsByName_.size(); i++) {
+      HLTPathsByIndex_[i] = triggerNames_.triggerIndex(HLTPathsByName_[i]);
+      if (HLTPathsByIndex_[i] < result.size()) {
+        valid = true;
+      } else {
+        // trigger path not found
+        HLTPathsByIndex_[i] = (unsigned int) -1;
+        if (throw_)
+          throw cms::Exception("Configuration") << "requested HLT path \"" << HLTPathsByName_[i] << "\" does not exist";
+        else
+          edm::LogWarning("Configuration") << "requested HLT path \"" << HLTPathsByName_[i] << "\" does not exist";
+      }
+    }
+
+    if (not valid) {
+      // no point in throwing - if requested, it should have already happened
+      edm::LogError("Configuration") << "none of the requested paths and pattern match any HLT path - no events will be selected";
+    }
+    
+  }
+
+  // report on what is finally used
+  LogDebug("HLTHighLevelDev") << "HLT trigger paths: " + inputTag_.encode()
+    << " - Number of paths: " << n
+    << " - andOr mode: " << andOr_
+    << " - throw mode: " << throw_;
+
+  LogTrace("HLTHighLevelDev") << "The HLT trigger paths (# index name):";
+  for (unsigned int i = 0; i < n; ++i)
+    if (HLTPathsByIndex_[i] == (unsigned int) -1)
+      LogTrace("HLTHighLevelDev") << "    n/a   " << HLTPathsByName_[i];
+    else
+      LogTrace("HLTHighLevelDev") << "    " << std::setw(4) << HLTPathsByIndex_[i] << "  " << HLTPathsByName_[i];
 
 }
 
@@ -183,80 +198,80 @@ HLTHighLevelDev::pathsFromSetup(const std::string &key, const edm::EventSetup &i
 }
 
 // ------------ method called to produce the data  ------------
-bool
+  bool
 HLTHighLevelDev::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace std;
-   using namespace edm;
+  using namespace std;
+  using namespace edm;
 
-   // get hold of TriggerResults Object
-   Handle<TriggerResults> trh;
-   iEvent.getByLabel(inputTag_, trh);
-   if (trh.isValid()) {
-     LogDebug("") << "TriggerResults found, number of HLT paths: " << trh->size();
-   } else {
-     LogDebug("") << "TriggerResults product not found - returning result=false!";
-     return false;
-   }
+  // get hold of TriggerResults Object
+  Handle<TriggerResults> trh;
+  iEvent.getByLabel(inputTag_, trh);
+  if (trh.isValid()) {
+    LogDebug("HLTHighLevelDev") << "TriggerResults found, number of HLT paths: " << trh->size();
+  } else {
+    LogDebug("HLTHighLevelDev") << "TriggerResults product not found - returning result=false!";
+    return false;
+  }
 
-   // init the TriggerNames with the TriggerResults
-   bool config_changed = triggerNames_.init(*trh);
+  // init the TriggerNames with the TriggerResults
+  bool config_changed = triggerNames_.init(*trh);
 
-   // (re)run the initialization stuff if 
-   // - this is the first event 
-   // - or the HLT table has changed 
-   // - or selected trigger bits come from AlCaRecoTriggerBitsRcd and these changed
-   if (config_changed or (watchAlCaRecoTriggerBitsRcd_ and watchAlCaRecoTriggerBitsRcd_->check(iSetup))) {
-     this->init(*trh, iSetup);
-   }
-   unsigned int n     = HLTPathsByName_.size();
-   unsigned int nbad  = 0;
-   unsigned int fired = 0;
-   bool allFired = true;
+  // (re)run the initialization stuff if 
+  // - this is the first event 
+  // - or the HLT table has changed 
+  // - or selected trigger bits come from AlCaRecoTriggerBitsRcd and these changed
+  if (config_changed or (watchAlCaRecoTriggerBitsRcd_ and watchAlCaRecoTriggerBitsRcd_->check(iSetup))) {
+    this->init(*trh, iSetup);
+  }
+  unsigned int n     = HLTPathsByName_.size();
+  unsigned int nbad  = 0;
+  unsigned int fired = 0;
+  bool allFired = true;
 
-   // count invalid and fired triggers
-   for (unsigned int i = 0; i < n; i++) {
-     if (HLTPathsByIndex_[i] == (unsigned int) -1)
-       ++nbad;
-     else if (trh->accept(HLTPathsByIndex_[i]) and HLTPrescalesExpanded_[i] and (HLTPrescalesScalers[i]++ % HLTPrescalesExpanded_[i] == 0))
-       ++fired;
-     else
-       allFired = false;
-   }
+  // count invalid and fired triggers
+  for (unsigned int i = 0; i < n; i++) {
+    if (HLTPathsByIndex_[i] == (unsigned int) -1)
+      ++nbad;
+    else if (trh->accept(HLTPathsByIndex_[i]) and HLTPrescalesExpanded_[i] and (HLTPrescalesScalers[i]++ % HLTPrescalesExpanded_[i] == 0))
+      ++fired;
+    else
+      allFired = false;
+  }
 
-   if ((nbad > 0) and (config_changed or throw_)) {
-     // only generate the error message if it's actually going to be used
-     std::string message;
+  if ((nbad > 0) and (config_changed or throw_)) {
+    // only generate the error message if it's actually going to be used
+    std::string message;
 
-     for (unsigned int i = 0; i < n; i++)
-       if (HLTPathsByIndex_[i] == (unsigned int) -1)
-         message += HLTPathsByName_[i] + " ";
+    for (unsigned int i = 0; i < n; i++)
+      if (HLTPathsByIndex_[i] == (unsigned int) -1)
+        message += HLTPathsByName_[i] + " ";
 
-     if (config_changed) {
-       LogTrace("")
-         << " HLTHighLevelDev [instance: " << *moduleLabel()
-         << " - path: " << *pathName()
-         << "] configured with " << nbad
-         << "/" << n
-         << " unknown HLT path names: " << message;
-     }
+    if (config_changed) {
+      LogTrace("HLTHighLevelDev")
+        << " HLTHighLevelDev [instance: " << *moduleLabel()
+        << " - path: " << *pathName()
+        << "] configured with " << nbad
+        << "/" << n
+        << " unknown HLT path names: " << message;
+    }
 
-     if (throw_) {
-       throw cms::Exception("Configuration")
-         << " HLTHighLevelDev [instance: " << *moduleLabel()
-         << " - path: " << *pathName()
-         << "] configured with " << nbad
-         << "/" << n
-         << " unknown HLT path names: " << message;
-     }
-   }
+    if (throw_) {
+      throw cms::Exception("Configuration")
+        << " HLTHighLevelDev [instance: " << *moduleLabel()
+        << " - path: " << *pathName()
+        << "] configured with " << nbad
+        << "/" << n
+        << " unknown HLT path names: " << message;
+    }
+  }
 
-   // Boolean filter result (always at least one trigger)
-// const bool accept( (fired > 0) and ( andOr_ or (fired == n-nbad) ) );
-   const bool accept( (fired > 0) and ( andOr_ or allFired ) );
-   LogDebug("") << "Accept = " << std::boolalpha << accept;
+  // Boolean filter result (always at least one trigger)
+  // const bool accept( (fired > 0) and ( andOr_ or (fired == n-nbad) ) );
+  const bool accept( (fired > 0) and ( andOr_ or allFired ) );
+  LogDebug("HLTHighLevelDev") << "Accept = " << std::boolalpha << accept;
 
-   return (accept and HLTOverallPrescale_ and ((HLTOverallPrescalesScaler_++) % HLTOverallPrescale_ == 0));
+  return (accept and HLTOverallPrescale_ and ((HLTOverallPrescalesScaler_++) % HLTOverallPrescale_ == 0));
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
