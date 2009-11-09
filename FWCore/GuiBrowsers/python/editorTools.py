8f0daab8603d04d9ae3be71126b1c521fe3dd14f
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2009-11-09 17:07:03 +0000

    synchronize editorTools with other patTools

--- FWCore/GuiBrowsers/python/editorTools.py
+++ FWCore/GuiBrowsers/python/editorTools.py
@@ -1,15 +1,25 @@
+import copy
+import FWCore.ParameterSet.Config as cms
 from FWCore.GuiBrowsers.ConfigToolBase import ConfigToolBase
 
 class UserCodeTool(ConfigToolBase):
     """ User code tool """
     _label="User code"
+    _defaultParameters={}
+    def __init__(self):
+        ConfigToolBase.__init__(self)
+        self.addParameter(self._defaultParameters,'code','', 'User code modifying the process: e.g. process.maxevents=1')
+        self._parameters=copy.deepcopy(self._defaultParameters)  
     def dumpPython(self):
         return ("",self._parameters['code'].value)
-    def __call__(self,code):
-        self.addParameter(self._parameters,'code',code, 'User code modifying the process: e.g. process.maxevents=1')
+    def __call__(self,process,code):
+        self.setParameter(self._parameters,'code',code, 'User code modifying the process: e.g. process.maxevents=1')
+        self.apply(process)
         return self
     def apply(self,process):
         code=self._parameters['code'].value
         exec code
     def typeError(self,name,bool):
         pass
+
+userCodeTool=UserCodeTool()
