9d34e6dcbc7b4968b0c0f3de55059bcbd48c154a
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2009-11-10 11:45:10 +0000

    corrected bug - it was always retrieving the last version due to a repeated clear - now ok

--- OnlineDB/EcalCondDB/src/FEConfigBadXTDat.cc
+++ OnlineDB/EcalCondDB/src/FEConfigBadXTDat.cc
@@ -80,7 +80,7 @@ void FEConfigBadXTDat::fetchData(std::vector< FEConfigBadXTDat >* p, FEConfigBad
   }
 
   try {
-    m_readStmt->setSQL("SELECT * FROM " + getTable() + "WHERE rec_id = :rec_id order by tcc_id, fed_id, tt_id , cry_id ");
+    m_readStmt->setSQL("SELECT * FROM " + getTable() + " WHERE rec_id = :rec_id order by tcc_id, fed_id, tt_id , cry_id ");
     m_readStmt->setInt(1, iovID);
     ResultSet* rset = m_readStmt->executeQuery();
     
