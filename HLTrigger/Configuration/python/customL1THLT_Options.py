17951ae90a417635b98afd6bb25206910b47fb8b
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-11-10 17:36:31 +0000

    fix process name in hltTrigReport and hltDQMHLTScalers only if they are prersent in the HLT menu

--- HLTrigger/Configuration/python/customL1THLT_Options.py
+++ HLTrigger/Configuration/python/customL1THLT_Options.py
@@ -1,7 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 def customise(process):
+    if 'hltTrigReport' in process.__dict__:
+        process.hltTrigReport.HLTriggerResults = cms.InputTag( 'TriggerResults','',process.name_() )
 
-    process.hltTrigReport.HLTriggerResults = cms.InputTag( 'TriggerResults','',process.name_() )
+    if 'hltDQMHLTScalers' in process.__dict__:
+        process.hltDQMHLTScalers.triggerResults = cms.InputTag( 'TriggerResults','',process.name_() )
 
     process.options.wantSummary = cms.untracked.bool(True)
     process.MessageLogger.categories.append('TriggerSummaryProducerAOD')
