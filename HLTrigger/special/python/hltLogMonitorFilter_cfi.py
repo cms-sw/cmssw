18644506abd241216364f5d8f917bc5a22530383
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-11-10 14:19:47 +0000

    clean example configuration

--- HLTrigger/special/python/hltLogMonitorFilter_cfi.py
+++ HLTrigger/special/python/hltLogMonitorFilter_cfi.py
@@ -2,18 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 hltLogMonitorFilter = cms.EDFilter("HLTLogMonitorFilter",
     default_threshold = cms.uint32(10),
-    categories = cms.VPSet(
-        cms.PSet(
-            name = cms.string('Category'),
-            threshold = cms.uint32(20)
-        ),
-        cms.PSet(
-            name = cms.string('Unprescaled'),
-            threshold = cms.uint32(1)
-        ),
-        cms.PSet(
-            name = cms.string('Disabled'),
-            threshold = cms.uint32(0)
-        )
-    )
+    categories = cms.VPSet( )
 )
