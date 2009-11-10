39f8d52fc0fe3295b71ad29baeb08c0858575ef2
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-11-10 11:08:17 +0000

    add service

--- CondCore/DBCommon/test/testTechnologyPlugin.cc
+++ CondCore/DBCommon/test/testTechnologyPlugin.cc
@@ -1,5 +1,8 @@
 #include "FWCore/PluginManager/interface/PluginManager.h"
 #include "FWCore/PluginManager/interface/standard.h"
+#include "FWCore/ServiceRegistry/interface/ServiceRegistry.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "CondCore/DBCommon/interface/Exception.h"
 #include "CondCore/DBCommon/interface/DbSession.h"
 #include "CondCore/DBCommon/interface/DbConnection.h"
@@ -9,7 +12,13 @@
 int main(){
   edmplugin::PluginManager::Config config;
   edmplugin::PluginManager::configure(edmplugin::standard::config());
-
+  std::vector<edm::ParameterSet> psets;
+  edm::ParameterSet pSet;
+  pSet.addParameter("@service_type",std::string("SiteLocalConfigService"));
+  psets.push_back(pSet);
+  edm::ServiceToken services(edm::ServiceRegistry::createSet(psets));
+  edm::ServiceRegistry::Operate operate(services);
+  
   std::cout<<"testing Connection Handler "<<std::endl;
   cond::DbConnection conn;
   conn.configure( cond::CmsDefaults );
@@ -17,7 +26,7 @@ int main(){
   const char * connects[] = {
     "sqlite_file:mydata.db",
     "sqlite_fip:CondCore/SQLiteData/data/mydata.db",
-    "frontier:FrontierDev/CMS_COND_PRESH",
+    "frontier://FrontierDev/CMS_COND_PRESH",
     "frontier://cmsfrontier.cern.ch:8000/FrontierDev/CMS_COND_PRESH"
   };
   for (int i=0; i<4; ++i) {
