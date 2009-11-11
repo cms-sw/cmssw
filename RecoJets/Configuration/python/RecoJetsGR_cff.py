00106f58c7e69220a2069a09256317fbcd762c01
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-11-11 15:21:27 +0000

    Add SISCone back to GR sequence (keep AK also)

--- RecoJets/Configuration/python/RecoJetsGR_cff.py
+++ RecoJets/Configuration/python/RecoJetsGR_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 
-# $Id: RecoJetsGR_cff.py,v 1.5 2009/08/13 15:02:34 srappocc Exp $
+# $Id: RecoJetsGR_cff.py,v 1.6.2.2 2009/11/11 15:17:51 srappocc Exp $
 
 # Standard set:
 from RecoJets.Configuration.RecoJets_cff import *
@@ -11,5 +11,5 @@ from RecoJets.Configuration.RecoJets_cff import *
 iterativeCone15CaloJets = iterativeCone5CaloJets.clone( rParam = cms.double(0.15) )
 
 
-recoJetsGR = cms.Sequence(iterativeCone15CaloJets+kt4CaloJets+kt6CaloJets+iterativeCone5CaloJets+sisCone5CaloJets+sisCone7CaloJets)
+recoJetsGR = cms.Sequence(iterativeCone15CaloJets+kt4CaloJets+kt6CaloJets+iterativeCone5CaloJets+ak5CaloJets+ak7CaloJets+sisCone5CaloJets+sisCone7CaloJets)
 
