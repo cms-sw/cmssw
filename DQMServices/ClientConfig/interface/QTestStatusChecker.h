ec3bd5d698445dd382aa938469271775507a1ed3
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-11-12 11:24:34 +0000

    fix comment

--- DQMServices/ClientConfig/interface/QTestStatusChecker.h
+++ DQMServices/ClientConfig/interface/QTestStatusChecker.h
@@ -4,11 +4,11 @@
 /** \class  QTestStatusChecker
  * *
  *  Class that checks the staus of Quality tests (takes a pointer to the
- *  DQMOldReceiver) and fills string maps containing the alarms
+ *  DQMStore) and fills string maps containing the alarms
  *
  * 
- *  $Date: 2008/02/21 03:26:44 $
- *  $Revision: 1.5 $
+ *  $Date: 2008/02/22 23:52:23 $
+ *  $Revision: 1.6 $
  *  \author Ilaria Segoni
   */
 
