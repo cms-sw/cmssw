0146fa6176e166aa56b8a7785005a1caad01bd75
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-11-12 14:53:04 +0000

    fixed (hopefully) last path to old UserCode

--- Validation/Generator/plugins/GenMatchedMuonsProducer.cc
+++ Validation/Generator/plugins/GenMatchedMuonsProducer.cc
@@ -1,6 +1,6 @@
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-#include "UserCode/TopMCValidation/plugins/GenMatchedCandsProducerBase.h"
+#include "Validation/Generator/plugins/GenMatchedCandsProducerBase.h"
 typedef GenMatchedCandsProducerBase<reco::Muon, reco::GenParticle> GenMatchedMuonsProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
