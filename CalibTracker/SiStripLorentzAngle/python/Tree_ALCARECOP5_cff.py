746235b00e65d35190f136371b97782f29f7fd44
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-11-12 22:14:51 +0000

    fixed input

--- CalibTracker/SiStripLorentzAngle/python/Tree_ALCARECOP5_cff.py
+++ CalibTracker/SiStripLorentzAngle/python/Tree_ALCARECOP5_cff.py
@@ -5,6 +5,7 @@ from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
 shallowTrackClusters.Tracks = "CalibrationTracksRefitP5"
 shallowTrackClusters.Clusters = 'CalibrationTracksP5'
 shallowClusters.Clusters = 'CalibrationTracksP5'
+CalibrationTracksP5.src = 'ALCARECOTkAlCosmicsCTF0T'
 
 #Schedule
 filter_refit_ntuplize_step = cms.Path( trackFilterRefitP5 + ntuple )
