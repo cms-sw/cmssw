d39a1b14b5ec0e3560865dd310ffa6332e83f906
Yeong-Jyi Lei <sha1-4336116f62c995f0b2b10f48d64d3d17454deeff@cern.ch>
2009-11-12 15:14:04 +0000

    add dcsStatus

--- DQM/EcalPreshowerMonitorModule/interface/ESDcsInfoTask.h
+++ DQM/EcalPreshowerMonitorModule/interface/ESDcsInfoTask.h
@@ -5,36 +5,59 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+class MonitorElement;
+class DQMStore;
+
 class ESDcsInfoTask: public edm::EDAnalyzer{
 
- public:
-  
-  ESDcsInfoTask(const edm::ParameterSet& ps);
-  virtual ~ESDcsInfoTask();
-  
- protected:
-  
-  void analyze(const edm::Event& e, const edm::EventSetup& c);
-  void beginJob(void);
-  void endJob(void);
-  void beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, const  edm::EventSetup& iSetup);
-  void endLuminosityBlock(const edm::LuminosityBlock&  lumiBlock, const  edm::EventSetup& iSetup);
-  void reset(void);
-  void cleanup(void);
-  
- private:
-  
-  DQMStore* dqmStore_;
-  
-  std::string prefixME_;
-  
-  bool enableCleanup_;
-  
-  bool mergeRuns_;
-  
-  MonitorElement* meESDcsFraction_;
-  MonitorElement* meESDcsActiveMap_;
-  
+   public:
+
+      /// Constructor
+      ESDcsInfoTask(const edm::ParameterSet& ps);
+
+      /// Destructor
+      virtual ~ESDcsInfoTask();
+
+   protected:
+
+      /// Analyze
+      void analyze(const edm::Event& e, const edm::EventSetup& c);
+
+      /// BeginJob
+      void beginJob(void);
+
+      /// EndJob
+      void endJob(void);
+
+      /// BeginLuminosityBlock
+      void beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, const  edm::EventSetup& iSetup);
+
+      /// EndLuminosityBlock
+      void endLuminosityBlock(const edm::LuminosityBlock&  lumiBlock, const  edm::EventSetup& iSetup);
+
+      /// Reset
+      void reset(void);
+
+      /// Cleanup
+      void cleanup(void);
+
+   private:
+
+      DQMStore* dqmStore_;
+
+      std::string prefixME_;
+
+      bool enableCleanup_;
+
+      bool mergeRuns_;
+
+      edm::InputTag dcsStatuslabel_;
+
+      MonitorElement* meESDcsFraction_;
+      MonitorElement* meESDcsActiveMap_;
+
+      int ievt_;
+
 };
 
 #endif
--- DQM/EcalPreshowerMonitorModule/src/ESDcsInfoTask.cc
+++ DQM/EcalPreshowerMonitorModule/src/ESDcsInfoTask.cc
@@ -2,20 +2,16 @@
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
 
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-
-#include "DataFormats/EcalDetId/interface/ESDetId.h"
-
-#include "CondFormats/DataRecord/interface/RunSummaryRcd.h"
-#include "CondFormats/RunInfo/interface/RunSummary.h"
-#include "CondFormats/RunInfo/interface/RunInfo.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "DQMServices/Core/interface/DQMStore.h"
+
+#include "DataFormats/Scalers/interface/DcsStatus.h"
 
 #include "DQM/EcalPreshowerMonitorModule/interface/ESDcsInfoTask.h"
 
@@ -25,16 +21,18 @@ using namespace std;
 
 ESDcsInfoTask::ESDcsInfoTask(const ParameterSet& ps) {
 
-  dqmStore_ = Service<DQMStore>().operator->();
+   dqmStore_ = Service<DQMStore>().operator->();
 
-  prefixME_ = ps.getUntrackedParameter<string>("prefixME", "");
+   prefixME_ = ps.getUntrackedParameter<string>("prefixME", "");
 
-  enableCleanup_ = ps.getUntrackedParameter<bool>("enableCleanup", false);
+   enableCleanup_ = ps.getUntrackedParameter<bool>("enableCleanup", false);
 
-  mergeRuns_ = ps.getUntrackedParameter<bool>("mergeRuns", false);
+   mergeRuns_ = ps.getUntrackedParameter<bool>("mergeRuns", false);
 
-  meESDcsFraction_ = 0;
-  meESDcsActiveMap_ = 0;
+   dcsStatuslabel_ 	= ps.getParameter<InputTag>("DcsStatusLabel");
+
+   meESDcsFraction_ = 0;
+   meESDcsActiveMap_ = 0;
 
 }
 
@@ -42,36 +40,40 @@ ESDcsInfoTask::~ESDcsInfoTask() {
 
 }
 
-void ESDcsInfoTask::beginJob(void) {
+void ESDcsInfoTask::beginJob(void){
 
-  char histo[200];
-  
-  if ( dqmStore_ ) {
+   char histo[200];
 
-    dqmStore_->setCurrentFolder(prefixME_ + "/EventInfo");
-    
-    sprintf(histo, "DCSSummary");
-    meESDcsFraction_ = dqmStore_->bookFloat(histo);
-    meESDcsFraction_->Fill(0.0);
+   if ( dqmStore_ ) {
 
-    sprintf(histo, "DCSSummaryMap");
-    meESDcsActiveMap_ = dqmStore_->book2D(histo,histo, 40, 0., 40., 40, 0., 40.);
-    meESDcsActiveMap_->setAxisTitle("X", 1);
-    meESDcsActiveMap_->setAxisTitle("Y", 2);
+      dqmStore_->setCurrentFolder(prefixME_ + "/EventInfo");
 
-  }
+      sprintf(histo, "DCSSummary");
+      meESDcsFraction_ = dqmStore_->bookFloat(histo);
+      meESDcsFraction_->Fill(-1.0);
+
+      sprintf(histo, "DCSSummaryMap");
+      meESDcsActiveMap_ = dqmStore_->book1D(histo,histo, 2, 0., 2.);
+      meESDcsActiveMap_->setAxisTitle("(ES+/ES-)", 1);
+
+
+   }
 
 }
 
 void ESDcsInfoTask::endJob(void) {
 
-  if ( enableCleanup_ ) this->cleanup();
+   if ( enableCleanup_ ) this->cleanup();
 
 }
 
 void ESDcsInfoTask::beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, const  edm::EventSetup& iSetup){
 
-  this->reset();
+   this->reset();
+
+   for( int i = 0; i < 2; i++) {
+      meESDcsActiveMap_->setBinContent(i+1 , -1.0);
+   }	    
 
 }
 
@@ -81,28 +83,57 @@ void ESDcsInfoTask::endLuminosityBlock(const edm::LuminosityBlock&  lumiBlock, c
 
 void ESDcsInfoTask::reset(void) {
 
-  if ( meESDcsFraction_ ) meESDcsFraction_->Reset();
+   if ( meESDcsFraction_ ) meESDcsFraction_->Reset();
 
-  if ( meESDcsActiveMap_ ) meESDcsActiveMap_->Reset();
-  
+   if ( meESDcsActiveMap_ ) meESDcsActiveMap_->Reset();
+
 }
 
+
 void ESDcsInfoTask::cleanup(void){
-  
-  if ( dqmStore_ ) {
 
-    dqmStore_->setCurrentFolder(prefixME_ + "/EventInfo");
-    
-    if ( meESDcsFraction_ ) dqmStore_->removeElement( meESDcsFraction_->getName() );
+   if ( dqmStore_ ) {
 
-    if ( meESDcsActiveMap_ ) dqmStore_->removeElement( meESDcsActiveMap_->getName() );
+      dqmStore_->setCurrentFolder(prefixME_ + "/EventInfo");
 
-  }
+      if ( meESDcsFraction_ ) dqmStore_->removeElement( meESDcsFraction_->getName() );
+
+      if ( meESDcsActiveMap_ ) dqmStore_->removeElement( meESDcsActiveMap_->getName() );
+
+   }
 
 }
 
 void ESDcsInfoTask::analyze(const Event& e, const EventSetup& c){ 
 
+   ievt_++;
+
+   float ESpDcsStatus = 0;
+   float ESmDcsStatus = 0;
+
+   Handle<DcsStatusCollection> dcsStatus;
+   try{
+      e.getByLabel(dcsStatuslabel_, dcsStatus);
+
+      for(DcsStatusCollection::const_iterator dcsStatusItr = dcsStatus->begin(); dcsStatusItr != dcsStatus->end(); ++dcsStatusItr) 
+      {
+	 ESpDcsStatus = dcsStatusItr->ready(DcsStatus::ESp);
+	 ESmDcsStatus = dcsStatusItr->ready(DcsStatus::ESm);
+      }
+
+   } catch ( cms::Exception &e  ) {
+      LogDebug("") << "Error! can't get dcsStatus collection !" << std::endl;
+   }
+
+   ESpDcsStatus = (ESpDcsStatus + float(ievt_-1)*meESDcsActiveMap_->getBinContent(1))/float(ievt_);	
+   ESmDcsStatus = (ESmDcsStatus + float(ievt_-1)*meESDcsActiveMap_->getBinContent(2))/float(ievt_);
+
+   meESDcsActiveMap_->setBinContent(1, ESpDcsStatus);	
+   meESDcsActiveMap_->setBinContent(2, ESmDcsStatus);
+
+   meESDcsFraction_->Fill( (ESpDcsStatus + ESmDcsStatus)/2. );
+
 }
 
+//define this as a plug-in
 DEFINE_FWK_MODULE(ESDcsInfoTask);
