ce859bd5bb42470491826976d8a3020d208c4ac6
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-11-13 17:08:12 +0000

    use it for L1T Cert

--- DQMOffline/Trigger/python/DQMOffline_Trigger_Cert_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_Cert_cff.py
@@ -1,13 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Trigger.MuonHLTValidation_cfi import *
+#  This is  where we want to do L1T  certificaion
+#
+###################################################
 
-from DQMOffline.Trigger.EgHLTOfflineSummaryClient_cfi import *
+#from DQMOffline.Trigger.MuonL1TValidation_cfi import *
 
-from DQMOffline.Trigger.DQMOffline_Trigger_SummaryCert_cfi import *
+#from DQMOffline.Trigger.EgL1TOfflineSummaryClient_cfi import *
+
+#from DQMOffline.Trigger.DQMOffline_Trigger_SummaryCert_cfi import *
 
 #--- Note: hltOverallCertSeq must be the last sequence!
 #-- it relies on bits set in the other sequences
-dqmOfflineTriggerCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*hltOverallCertSeq)
+
+#dqmOfflineTriggerCert = cms.Sequence(muonL1TCertSeq*egL1TOffCertSeq)
 
 
