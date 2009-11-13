6b8c40c016af60bd45fda4faa713e99436ba729b
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-11-13 02:22:55 +0000

    *** empty log message ***

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 # electron
 from DQM.HLTEvF.HLTMonElectron_cff import *
 # tau
-from DQM.HLTEvF.HLTMonTau_cff import *
+#from DQM.HLTEvF.HLTMonTau_cff import *
 # four vector
 from DQM.HLTEvF.FourVectorHLTOnline_cff import *
 # jet met
@@ -11,11 +11,11 @@ from DQM.HLTEvF.HLTMonJetMETDQMSource_cff import *
 # muon
 from DQM.HLTEvF.HLTMonMuonDQM_cff import *
 from DQM.HLTEvF.HLTLocalRecoDT_cff import *
-from DQM.HLTEvF.HLTLocalRecoRPC_cff import *
+#from DQM.HLTEvF.HLTLocalRecoRPC_cff import *
 #from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
+from DQM.HLTEvF.HLTLocalRecoSiPixel_cff import *
 # alca
 from DQM.HLTEvF.HLTMonAlCaPi0_cff import *
 from DQM.HLTEvF.HLTMonAlCaPhySim_cff import *
 # btag
-#from DQM.HLTEvF.hltMonBTag_cff import *
-
+from DQM.HLTEvF.hltMonBTag_cff import *
