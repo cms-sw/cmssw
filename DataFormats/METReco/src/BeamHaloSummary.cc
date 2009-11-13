45636f4bd8d36fc861fad6f870ae01cafb4ab130
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-11-13 15:56:15 +0000

    *** empty log message ***

---
+++ DataFormats/METReco/src/BeamHaloSummary.cc
@@ -0,0 +1,20 @@
+#include "DataFormats/METReco/interface/BeamHaloSummary.h"
+
+/*
+  [class]:  BeamHaloSummary
+  [authors]: R. Remington, The University of Florida
+  [description]: See BeamHaloSummary.h
+  [date]: October 15, 2009
+*/
+
+using namespace reco;
+
+BeamHaloSummary::BeamHaloSummary()
+{
+
+}
+
+BeamHaloSummary::BeamHaloSummary(CSCHaloData& CSCData, EcalHaloData& EcalData, HcalHaloData& HcalData, GlobalHaloData& GlobalData)
+{ 
+
+}
---
+++ DataFormats/METReco/src/CSCHaloData.cc
@@ -0,0 +1,113 @@
+#include "DataFormats/METReco/interface/CSCHaloData.h"
+
+/*
+  [class]:  CSCHaloData
+  [authors]: R. Remington, The University of Florida
+  [description]: See CSCHaloData.h 
+  [date]: October 15, 2009
+*/
+
+#include "CondFormats/CSCObjects/interface/CSCDBCrosstalk.h"
+#include "CondFormats/CSCObjects/interface/CSCDBGains.h"
+#include "CondFormats/CSCObjects/interface/CSCDBNoiseMatrix.h"
+#include "CondFormats/CSCObjects/interface/CSCDBPedestals.h"
+#include "CondFormats/DataRecord/interface/CSCDBCrosstalkRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBGainsRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBNoiseMatrixRcd.h"
+#include "CondFormats/DataRecord/interface/CSCDBPedestalsRcd.h"
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCWireDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCWireDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
+#include "DataFormats/CSCDigi/interface/CSCComparatorDigi.h"
+#include "DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2D.h"
+#include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
+#include "DataFormats/CSCRecHit/interface/CSCRecHit2DCollection.h"
+#include "DataFormats/CSCRecHit/interface/CSCSegment.h"
+#include "DataFormats/GeometrySurface/interface/Cylinder.h"
+#include "DataFormats/GeometrySurface/interface/Plane.h"
+#include "DataFormats/GeometrySurface/interface/Cone.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+#include "DataFormats/GeometryVector/interface/GlobalVector.h"
+#include "DataFormats/GeometryVector/interface/LocalPoint.h"
+#include "DataFormats/GeometryVector/interface/LocalVector.h"
+#include "DataFormats/L1CSCTrackFinder/interface/L1CSCTrackCollection.h"
+#include "DataFormats/L1CSCTrackFinder/interface/L1CSCStatusDigiCollection.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuRegionalCand.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTReadoutRecord.h"
+#include "DataFormats/L1GlobalMuonTrigger/interface/L1MuGMTReadoutCollection.h"
+#include "DataFormats/MuonDetId/interface/CSCIndexer.h"
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+#include "DataFormats/MuonDetId/interface/DTWireId.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/TrackingRecHit/interface/RecSegment.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2DCollection.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2DCollection.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+
+#include "DataFormats/TrajectorySeed/interface/TrajectorySeedCollection.h"
+#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
+#include "Geometry/CSCGeometry/interface/CSCChamber.h"
+#include "Geometry/CSCGeometry/interface/CSCLayer.h"
+#include "Geometry/CSCGeometry/interface/CSCLayerGeometry.h"
+#include "L1Trigger/CSCTrackFinder/interface/CSCSectorReceiverLUT.h"
+#include "L1Trigger/CSCTrackFinder/interface/CSCSectorReceiverLUT.h"
+#include "L1Trigger/CSCCommonTrigger/interface/CSCTriggerGeometry.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
+#include "RecoMuon/TrackingTools/interface/MuonPatternRecoDumper.h"
+#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHitBuilder.h"
+#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
+#include "TrackingTools/GeomPropagators/interface/Propagator.h"
+#include "TrackingTools/GeomPropagators/interface/AnalyticalPropagator.h"
+#include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+
+
+using namespace reco;
+CSCHaloData::CSCHaloData()
+{
+  nTriggers_PlusZ = 0;
+  nTriggers_MinusZ = 0 ;
+  nTracks_PlusZ = 0 ;
+  nTracks_MinusZ = 0;
+}
+int CSCHaloData::NumberOfHaloTriggers(int z) const
+{
+  if( z == 1 )
+    return nTriggers_PlusZ;
+  else if( z == -1 )
+    return nTriggers_MinusZ;
+  else 
+    return nTriggers_MinusZ + nTriggers_PlusZ;
+}
+
+int CSCHaloData::NumberOfHaloTracks(int z) const 
+{
+  int n = 0 ;
+  for(unsigned int i = 0 ; i < TheTrackRefs.size() ; i++ )
+    {
+      edm::Ref<reco::TrackCollection> iTrack( TheTrackRefs, i ) ;
+      // Does the track go through both endcaps ? 
+      bool Traversing =  (iTrack->outerPosition().z() > 0 &&  iTrack->innerPosition().z() < 0) ||  (iTrack->outerPosition().z() < 0 &&  iTrack->innerPosition().z() > 0);
+      // Does the track go through only +Z endcap ?
+      bool PlusZ =  (iTrack->outerPosition().z() > 0 && iTrack->innerPosition().z() > 0 ) ;
+      // Does the track go through only -Z endcap ? 
+      bool MinusZ = (iTrack->outerPosition().z()< 0 && iTrack->innerPosition().z() < 0) ;
+
+      if( (z == 1) && ( PlusZ || Traversing) ) 
+	n++;
+      else if( (z == -1) && ( MinusZ || Traversing ) )
+	n++;
+      else if( (TMath::Abs(z) != 1) && (PlusZ || MinusZ || Traversing) ) 
+	n++ ;
+    }
+  return n;
+}
