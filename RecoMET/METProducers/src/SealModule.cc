1ee619f8c4d4f2b21c3993a1a6ed8df8ff69b812
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-11-13 20:46:18 +0000

    Added halo data producers

--- RecoMET/METProducers/src/SealModule.cc
+++ RecoMET/METProducers/src/SealModule.cc
@@ -3,9 +3,21 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoMET/METProducers/interface/METProducer.h"
+#include "RecoMET/METProducers/interface/BeamHaloSummaryProducer.h"
+#include "RecoMET/METProducers/interface/CSCHaloDataProducer.h" 
+#include "RecoMET/METProducers/interface/HcalHaloDataProducer.h" 
+#include "RecoMET/METProducers/interface/EcalHaloDataProducer.h" 
+#include "RecoMET/METProducers/interface/GlobalHaloDataProducer.h" 
+using cms::METProducer;
+
 
 using cms::METProducer;
 
 DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(METProducer);
+DEFINE_ANOTHER_FWK_MODULE(BeamHaloSummaryProducer);
+DEFINE_ANOTHER_FWK_MODULE(CSCHaloDataProducer);
+DEFINE_ANOTHER_FWK_MODULE(HcalHaloDataProducer);
+DEFINE_ANOTHER_FWK_MODULE(EcalHaloDataProducer);
+DEFINE_ANOTHER_FWK_MODULE(GlobalHaloDataProducer);
