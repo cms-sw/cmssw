7fd853914e4b8d823544e2d6c7c855bbe95d22d8
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2009-11-13 12:39:59 +0000

    Added overall client

--- DQMOffline/Trigger/python/DQMOffline_Trigger_Cert_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_Cert_cff.py
@@ -4,6 +4,10 @@ from DQMOffline.Trigger.MuonHLTValidation_cfi import *
 
 from DQMOffline.Trigger.EgHLTOfflineSummaryClient_cfi import *
 
-dqmOfflineTriggerCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq)
+from DQMOffline.Trigger.DQMOffline_Trigger_SummaryCert_cfi import *
+
+#--- Note: hltOverallCertSeq must be the last sequence!
+#-- it relies on bits set in the other sequences
+dqmOfflineTriggerCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*hltOverallCertSeq)
 
 
