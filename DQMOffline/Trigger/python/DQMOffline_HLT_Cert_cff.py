9b00ea93612a6a3674a134824b0258e3ee71aaa8
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-11-14 00:13:22 +0000

    EventInfoClient

--- DQMOffline/Trigger/python/DQMOffline_HLT_Cert_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_HLT_Cert_cff.py
@@ -5,10 +5,11 @@ from DQMOffline.Trigger.MuonHLTValidation_cfi import *
 from DQMOffline.Trigger.EgHLTOfflineSummaryClient_cfi import *
 
 from DQMOffline.Trigger.DQMOffline_HLT_SummaryCert_cfi import *
+#from DQMOffline.Trigger.DQMOffline_Trigger_SummaryCert_cfi import *
 
 #--- Note: hltOverallCertSeq must be the last sequence!
 #-- it relies on bits set in the other sequences
-#dqmOfflineTriggerCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*hltOverallCertSeq)
-dqmOfflineHLTCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*hltOverallCertSeq)
+dqmOfflineTriggerCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*HLTOverallCertSeq)
+#dqmOfflineHLTCert = cms.Sequence(muonHLTCertSeq*egHLTOffCertSeq*hltOverallCertSeq)
 
 
--- DQMOffline/Trigger/python/DQMOffline_HLT_SummaryCert_cfi.py
+++ DQMOffline/Trigger/python/DQMOffline_HLT_SummaryCert_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 
 
-hltOverallSummary = cms.EDAnalyzer("DQMOfflineHLTEventInfoClient",
+HLTOverallSummary = cms.EDAnalyzer("DQMOfflineHLTEventInfoClient",
 									verbose = cms.untracked.bool(False)
 									)
 
-hltOverallCertSeq = cms.Sequence(hltOverallSummary)
+HLTOverallCertSeq = cms.Sequence(HLTOverallSummary)
