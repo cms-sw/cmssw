c2682e26ca27e011ae674f6e499ab6d3670bfb97
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-11-15 10:26:54 +0000

    moved here where they belong

--- CondCore/TagCollection/src/TagDBNames.cc
+++ CondCore/TagCollection/src/TagDBNames.cc
@@ -1,4 +1,4 @@
-#include "TagDBNames.h"
+#include "CondCore/TagCollection/interface/TagDBNames.h"
 namespace cond {
 const std::string tagTreeTablePrefix("TAGTREE_TABLE");
 const std::string tagInventoryTable("TAGINVENTORY_TABLE");
