030b71e64cf06fa3a52710f57751865c9f123f9b
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-11-17 15:18:58 +0000

    fix threshold

--- HLTrigger/special/python/hltCaloTowerFilter_cfi.py
+++ HLTrigger/special/python/hltCaloTowerFilter_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hltCaloTowerFilter = cms.EDFilter( "HLTCaloTowerFilter",
    inputTag = cms.InputTag( "hltTowerMakerForEcal" ),
    saveTag  = cms.untracked.bool( False ),
-   MinPt  = cms.double( 2.0 ),
+   MinPt  = cms.double( 3.0 ),
    MaxEta = cms.double( 3.0 ),
    MinN   = cms.uint32( 1 )
 )
