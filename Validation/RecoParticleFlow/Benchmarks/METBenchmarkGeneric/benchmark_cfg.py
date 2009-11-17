a3acd7117b83bca053c2538a4ee0dac2f9931447
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2009-11-17 21:03:54 +0000

    Use DQMOffline

--- Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/benchmark_cfg.py
+++ Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/benchmark_cfg.py
@@ -1,54 +1,46 @@
-# Runs the GenericBenchmark to
-# monitor performance of pfMET
+# test file for PFCandidate validation
+# performs a matching with the genParticles collection. 
+# creates a root file with histograms filled with PFCandidate data,
+# present in the Candidate, and in the PFCandidate classes, for matched
+# PFCandidates. Matching histograms (delta pt etc) are also available. 
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TEST")
 process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
 
+fa = 'RelValQCD'
+fb = 'FlatPt_15_3000_Fast'
+fc = 'ParticleFlow'
 
-process.source = cms.Source("PoolSource",
-fileNames = cms.untracked.vstring(
-'/../user/l/lacroix/MET_Validation/ttbar_fastsim_310_pre6_muonAndJEC/aod.root'
-)
-)
+process.load("RecoParticleFlow.Configuration.DBS_Samples.%s_%s_cfi" % (fa, fb) )
+#process.source = cms.Source("PoolSource",
+#fileNames = cms.untracked.vstring(
+#'/../user/l/lacroix/MET_Validation/ttbar_fastsim_310_pre6_muonAndJEC/aod.root'
+#)
+#)
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(3000)
 )
 
-process.load("Validation.RecoParticleFlow.metBenchmarkGeneric_cff")
+process.load("Validation.RecoParticleFlow.metBenchmark_cff")
+
+process.dqmSaver.convention = 'Offline'
+#process.dqmSaver.workflow = '/%s/%s/%s' % (fa, fb, fc)
+process.dqmSaver.workflow = '/A/B/C'
+process.dqmEnv.subSystemFolder = 'ParticleFlow'
 
 process.p =cms.Path(
-    process.metBenchmarkGeneric
+    process.dqmEnv +
+    process.metBenchmarkSequence +
+    process.dqmSaver
     )
 
 
 process.schedule = cms.Schedule(process.p)
 
 
-
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.options = cms.untracked.PSet(
-    makeTriggerResults = cms.untracked.bool(True),
-    wantSummary = cms.untracked.bool(True),
-    Rethrow = cms.untracked.vstring('Unknown', 
-        'ProductNotFound', 
-        'DictionaryNotFound', 
-        'InsertFailure', 
-        'Configuration', 
-        'LogicError', 
-        'UnimplementedFeature', 
-        'InvalidReference', 
-        'NullPointerError', 
-        'NoProductSpecified', 
-        'EventTimeout', 
-        'EventCorruption', 
-        'ModuleFailure', 
-        'ScheduleExecutionFailure', 
-        'EventProcessorFailure', 
-        'FileInPathError', 
-        'FatalRootError', 
-        'NotFound')
-)
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 50
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
--- Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/METBenchmarkGeneric/plot.C
@@ -6,10 +6,16 @@
 
   gStyle->SetOptStat(1111);
 
-  string dir1 = "DQMData/PFTask/Benchmarks/pfMet/Gen";
-  string dir2 = "DQMData/PFTask/Benchmarks/met/Gen";
-  const char* file1 = "benchmark.root";
-  const char* file2 = "benchmark.root";
+//string dir1 = "DQMData/PFTask/Benchmarks/pfMet/Gen";
+//string dir2 = "DQMData/PFTask/Benchmarks/met/Gen";
+  const string dir1 = "DQMData/Run\ 1/PFTask/Run\ summary/pfMet";
+  const string dir2 = "DQMData/Run\ 1/PFTask/Run\ summary/met";
+  const string dir3 = "DQMData/Run\ 1/PFTask/Run\ summary/genMetTrue";
+
+//const char* file1 = "benchmark.root";
+//const char* file2 = "benchmark.root";
+  const char* file1 = "DQM_V0001_R000000001__A__B__C.root";
+  const char* file2 = "DQM_V0001_R000000001__A__B__C.root";
 
   float ptMin = 0;
   float ptMax = 9999;
@@ -32,53 +38,52 @@
   comp.Legend().Draw();
   Styles::SavePlot("legend", outdir.c_str() );
 
-  comp.SetAxis(5, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c1a("c1a", "DeltaMET");
   Styles::FormatPad( &c1a, false); 
-  comp.DrawSlice("DeltaEtvsEt", 20., 10000., mode);
+  comp.DrawSlice("delta_et_VS_et_", 2, 1000, mode);
 //comp.Draw("DeltaEt", mode);
   Styles::SavePlot("deltaMET", outdir.c_str() );
 
-  comp.SetAxis(5, -400,400);
+  comp.SetAxis(1, -400,400);
   TCanvas c1al("c1al", "DeltaMET_log");
   Styles::FormatPad( &c1al, false , false, true  ); 
-  comp.DrawSlice("DeltaEtvsEt", 20., 10000., mode);
+  comp.DrawSlice("delta_et_VS_et_", 2, 1000, mode);
   Styles::SavePlot("deltaMET_log", outdir.c_str() );
-  comp.SetAxis(5);
+//comp.SetAxis(1);
 
   TCanvas c1b("c1b", "MET");
   Styles::FormatPad( &c1b, false );
-  comp.SetAxis(2,0,120);
-  comp.Draw("EtRec", mode);
+  comp.SetAxis(1,0,120);
+  comp.Draw("pt_", mode);
   Styles::SavePlot("MET", outdir.c_str() );
 
   TCanvas c2a("c2a", "MEX");
   Styles::FormatPad( &c2a, false );
-  comp.SetAxis(2,-100,100);
-  comp.Draw("ExRec", mode);
+  comp.SetAxis(1,-100,100);
+  comp.Draw("px_", mode);
   Styles::SavePlot("MEX", outdir.c_str() );
 
-  TCanvas c2b("c2b", "MEY");
-  Styles::FormatPad( &c2b, false );
-  comp.Draw("EyRec", mode);
-  Styles::SavePlot("MEY", outdir.c_str() );
+//TCanvas c2b("c2b", "MEY");
+//Styles::FormatPad( &c2b, false );
+//comp.Draw("EyRec", mode);
+//Styles::SavePlot("MEY", outdir.c_str() );
 
   TCanvas c2c("c2c", "DeltaMEX");
   Styles::FormatPad( &c2c, false );
-  comp.SetAxis(2,-100,100);
-  comp.Draw("DeltaEx", mode);
+  comp.SetAxis(1,-100,100);
+  comp.Draw("delta_ex_", mode);
   Styles::SavePlot("DeltaMEX", outdir.c_str() );
 
-  TCanvas c2d("c2d", "DeltaMEY");
-  Styles::FormatPad( &c2d, false );
-  comp.Draw("DeltaEy", mode);
-  Styles::SavePlot("DeltaMEY", outdir.c_str() );
+//TCanvas c2d("c2d", "DeltaMEY");
+//Styles::FormatPad( &c2d, false );
+//comp.Draw("DeltaEy", mode);
+//Styles::SavePlot("DeltaMEY", outdir.c_str() );
 
-  comp.SetAxis(10, -3.2,3.2);
+  comp.SetAxis(1, -3.2,3.2);
   TCanvas c3a("c3a", "DeltaPhi");
   Styles::FormatPad( &c3a, false );
-  comp.DrawSlice("DeltaPhivsEt", 20., 10000., mode);
-//comp.Draw("DeltaPhi", mode);
+  comp.DrawSlice("delta_phi_VS_et_", 2, 1000, mode);
   Styles::SavePlot("deltaPhi", outdir.c_str() );
 
   TCanvas c3b("c3b", "pf vs Gen");
@@ -86,7 +91,7 @@
   gStyle->SetPalette(1);
   TDirectory* dir = comp.dir0();
   dir->cd();
-  TH2F *h2 = (TH2F*) dir->Get("EtRecvsEt");
+  TH2F *h2 = (TH2F*) dir->Get("RecEt_VS_TrueEt_");
   h2->Draw("colz");
   Styles::SavePlot("PF_vs_Gen", outdir.c_str() );
 
@@ -95,7 +100,7 @@
   gStyle->SetPalette(1);
   TDirectory* dir = comp.dir1();
   dir->cd();
-  TH2F *h2 = (TH2F*) dir->Get("EtRecvsEt");
+  TH2F *h2 = (TH2F*) dir->Get("RecEt_VS_TrueEt_");
   h2->Draw("colz");
   Styles::SavePlot("Calo_vs_Gen", outdir.c_str() );
 
@@ -106,7 +111,7 @@
   comp.SetAxis(1, 0.0,200.);
   TCanvas c5b("c5b", "MET response");
   Styles::FormatPad( &c5b, false );
-  comp.DrawMeanSlice("DeltaEtOverEtvsEt", 20, mode);
+  comp.DrawMeanSlice("delta_et_Over_et_VS_et_", 2, mode);
   Styles::SavePlot("MET_Response2", outdir.c_str() );
 
   comp.SetStyles(style1, style2, "Particle Flow Met", "Calo Met");
@@ -114,45 +119,48 @@
 
   TCanvas c6("c6", "genMET");
   Styles::FormatPad( &c6, false );
-  comp.SetAxis(2,0,120);
-  comp.Draw("EtGen", mode);
+  Comparator comp3(file1, dir3.c_str(),
+		   file2, dir3.c_str());
+  comp3.SetStyles(style1, style2, "Particle Flow Met", "Calo Met");
+  comp3.SetAxis(1,0,120);
+  comp3.Draw("pt_", mode);
   Styles::SavePlot("genMET", outdir.c_str() );
-  comp.SetAxis(1);
+//  comp.SetAxis(1);
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c7("c7", "DeltaMET_20_50");
   Styles::FormatPad( &c7, false );
-  comp.DrawSlice("DeltaEtvsEt", 20., 50., mode);
+  comp.DrawSlice("delta_et_VS_et_", 2, 5, mode);
   Styles::SavePlot("deltaMET_20_50", outdir.c_str() );
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -1000,1000);
   TCanvas c8("c8", "DeltaMET_50_100");
   Styles::FormatPad( &c8, false );
-  comp.DrawSlice("DeltaEtvsEt", 50., 100., mode);
+  comp.DrawSlice("delta_et_VS_et_", 5, 10, mode);
   Styles::SavePlot("deltaMET_50_100", outdir.c_str() );
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c9("c9", "DeltaMET_100_200");
   Styles::FormatPad( &c9, false );
-  comp.DrawSlice("DeltaEtvsEt", 100., 200., mode);
+  comp.DrawSlice("delta_et_VS_et_", 10, 20, mode);
   Styles::SavePlot("deltaMET_100_200", outdir.c_str() );
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c10("c10", "DeltaPhi_20_50");
   Styles::FormatPad( &c10, false );
-  comp.DrawSlice("DeltaPhivsEt", 20., 50., mode);
+  comp.DrawSlice("delta_phi_VS_et_", 2, 5, mode);
   Styles::SavePlot("deltaPhi_20_50", outdir.c_str() );
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c11("c11", "DeltaPhi_50_100");
   Styles::FormatPad( &c11, false );
-  comp.DrawSlice("DeltaPhivsEt", 50., 100., mode);
+  comp.DrawSlice("delta_phi_VS_et_", 5, 10, mode);
   Styles::SavePlot("deltaPhi_50_100", outdir.c_str() );
 
-  comp.SetAxis(10, -200,200);
+  comp.SetAxis(1, -200,200);
   TCanvas c12("c12", "DeltaPhi_100_200");
   Styles::FormatPad( &c12, false );
-  comp.DrawSlice("DeltaPhivsEt", 100., 200., mode);
+  comp.DrawSlice("delta_phi_VS_et_", 10, 20, mode);
   Styles::SavePlot("deltaPhi_100_200", outdir.c_str() );
 
   mode = Comparator::GRAPH;
@@ -163,12 +171,12 @@
 
   TCanvas c13("c13", "sigmaMET_MET");
   Styles::FormatPad( &c13, false );
-  comp.DrawGaussSigmaOverMeanXSlice("DeltaEtvsEt", 20, 21, 200, false, mode);
+  comp.DrawGaussSigmaOverMeanXSlice("delta_et_VS_et_", 2, 2, 20, false, mode);
   Styles::SavePlot("sigmaMET_MET", outdir.c_str() );
 
   TCanvas c14b("c14b", "sigmaPhi");
   Styles::FormatPad( &c14b, false );
-  comp.DrawGaussSigmaSlice("DeltaPhivsEt", 30, 21, 200, false, mode);
+  comp.DrawGaussSigmaSlice("delta_phi_VS_et_", 3, 2, 20, false, mode);
   Styles::SavePlot("sigmaPhib", outdir.c_str() );
 
 // mode = Comparator::SCALE;
