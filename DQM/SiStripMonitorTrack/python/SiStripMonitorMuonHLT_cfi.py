1ca3ba4d2708a53d5214553c79e683cb920e3282
Eric Chabert <sha1-5a78a125e1b71f85483595bc384ab8c900813250@cern.ch>
2009-11-18 12:12:04 +0000

    Add normalisation - add possibility to run on track collection - add bug-fix comming from 33X release

--- DQM/SiStripMonitorTrack/python/SiStripMonitorMuonHLT_cfi.py
+++ DQM/SiStripMonitorTrack/python/SiStripMonitorMuonHLT_cfi.py
@@ -3,9 +3,15 @@ import FWCore.ParameterSet.Config as cms
 sistripMonitorMuonHLT = cms.EDAnalyzer("SiStripMonitorMuonHLT",
     outputFile = cms.untracked.string('./SiStripMuonHLTDQM.root'),
     #disableROOToutput = cms.untracked.bool(False),
-    verbose = cms.untracked.bool(False),
+    verbose = cms.untracked.bool(True),
+    normalize = cms.untracked.bool(True),
+    printNormalize = cms.untracked.bool(False),
     monitorName = cms.untracked.string("HLT/HLTMonMuon"),
     prescaleEvt = cms.untracked.int32(-1),
+    runOnClusters = cms.untracked.bool(True),
     clusterCollectionTag = cms.untracked.InputTag ("hltSiStripRawToClustersFacility"),
-    l3MuonTag = cms.untracked.InputTag ("hltL3MuonCandidates")
+    runOnMuonCandidates = cms.untracked.bool(True),
+    l3MuonTag = cms.untracked.InputTag ("hltL3MuonCandidates"),
+    runOnTracks = cms.untracked.bool(False),
+    trackCollectionTag = cms.untracked.InputTag ("hltL3TkTracksFromL2")
 )
