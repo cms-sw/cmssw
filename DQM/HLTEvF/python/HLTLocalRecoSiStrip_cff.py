200b13b3c3ddb186be33ef27f2a3de4af6f8bad9
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-11-18 12:50:54 +0000

    bug fix

--- DQM/HLTEvF/python/HLTLocalRecoSiStrip_cff.py
+++ DQM/HLTEvF/python/HLTLocalRecoSiStrip_cff.py
@@ -5,10 +5,10 @@ from DQM.SiStripMonitorTrack.SiStripMonitorMuonHLT_cfi import *
 hltHighLevelSiStrip = cms.EDFilter("HLTHighLevel",
     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
     HLTPaths = cms.vstring('HLT_L1Mu[^_]*$',
-                           'HLT_L2Mu[^_]*&',
-			   'HLT_Mu[^_]*&',
-			   'HLT_IsoMu[^_]*&',
-			   'HLT_DoubleMu[^_]*&',
+                           'HLT_L2Mu[^_]*$',
+			   'HLT_Mu[^_]*$',
+			   'HLT_IsoMu[^_]*$',
+			   'HLT_DoubleMu[^_]*$',
     ),
 
     eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
