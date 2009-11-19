dead1a153f17ec2767e0c028dace73cf21b8b454
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2009-11-19 22:32:41 +0000

    Added trigger list

--- DQM/Physics/python/qcdLowPtDQM_cfi.py
+++ DQM/Physics/python/qcdLowPtDQM_cfi.py
@@ -1,4 +1,4 @@
-# $Id: qcdLowPtDQM_cfi.py,v 1.4 2009/11/15 19:45:54 loizides Exp $
+# $Id: qcdLowPtDQM_cfi.py,v 1.5 2009/11/17 06:45:14 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -22,7 +22,37 @@ myRecoSeq2 = cms.Sequence(
 myRecoSeq = cms.Sequence(myRecoSeq1 * myRecoSeq2)
 
 QcdLowPtDQM = cms.EDAnalyzer("QcdLowPtDQM",
-    hltTrgNames  = cms.untracked.vstring('HLT_MinBiasHcal',
-                                         'HLT_MinBiasEcal',
-                                         'HLT_MinBiasPixel'),
+    hltTrgNames  = cms.untracked.vstring(
+    'HLT_Activity_EcalREM',
+    'HLT_Activity_L1A',
+    'HLT_Activity_PixelClusters',
+    'HLT_BackwardBSC',
+    'HLT_CSCBeamHalo',
+    'HLT_ForwardBSC',
+    'HLT_FwdJet20U',
+    'HLT_HFThreshold',
+    'HLT_HighMultiplicityBSC',
+    'HLT_L1SingleEG2',
+    'HLT_L1SingleEG2_NoBPTX',
+    'HLT_L1SingleEG5',
+    'HLT_L1SingleForJet',
+    'HLT_L1_BPTX',
+    'HLT_L1_BPTX_MinusOnly',
+    'HLT_L1_BPTX_PlusOnly',
+    'HLT_L1_BSC',
+    'HLT_L1_HFtech',
+    'HLT_MinBias',
+    'HLT_MinBiasBSC',
+    'HLT_MinBiasBSC_OR',
+    'HLT_MinBiasEcal',
+    'HLT_MinBiasHcal',
+    'HLT_MinBiasPixel_DoubleIsoTrack5',
+    'HLT_MinBiasPixel_DoubleTrack',
+    'HLT_MinBiasPixel_SingleTrack',
+    'HLT_Mu3',
+    'HLT_Random',
+    'HLT_ZeroBias'
+    ),
+                             
+    verbose = cms.untracked.int32(3)
 )
