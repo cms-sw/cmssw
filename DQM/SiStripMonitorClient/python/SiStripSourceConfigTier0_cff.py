72a5bc12ae442beba4e75da0094930c4c5174dcd
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2009-11-19 20:37:44 +0000

    updating config files and QTests for collision

--- DQM/SiStripMonitorClient/python/SiStripSourceConfigTier0_cff.py
+++ DQM/SiStripMonitorClient/python/SiStripSourceConfigTier0_cff.py
@@ -15,6 +15,12 @@ from DQM.SiStripMonitorHardware.siStripFEDMonitor_Tier0_cff import *
 # SiStripMonitorDigi ####
 from DQM.SiStripMonitorDigi.SiStripMonitorDigi_cfi import *
 SiStripMonitorDigi.Mod_On = False
+SiStripMonitorDigi.TProfDigiApvCycle.subdetswitchon = True
+
+# SiStripMonitorCluster ####
+from DQM.SiStripMonitorCluster.SiStripMonitorCluster_cfi import *
+SiStripMonitorCluster.Mod_On = False
+SiStripMonitorCluster.TProfClustersApvCycle.subdetswitchon = True
 
 # SiStripMonitorTrack ####
 from DQM.SiStripMonitorTrack.SiStripMonitorTrack_cfi import *
@@ -29,13 +35,11 @@ MonitorTrackResiduals.OutputMEsInRootFile = False
 MonitorTrackResiduals.Mod_On = False
 
 # TrackingMonitor ####
-from DQM.TrackingMonitor.TrackingMonitor_cfi import *
-TrackMon.TrackProducer = 'generalTracks'
-TrackMon.AlgoName = 'CKFTk'
-TrackMon.FolderName = 'Tracking/TrackParameters'
-TrackMon.doSeedParameterHistos = True
-TrackMon.doBeamSpotPlots       = True
-TrackMon.BSFolderName          = 'Tracking/TrackParameters/BeamSpotParameters'
+# TrackingMonitor ####
+import DQM.TrackingMonitor.TrackingMonitor_cfi
+TrackMon_gentk = DQM.TrackingMonitor.TrackingMonitor_cfi.TrackMon.clone()
+TrackMon_gentk.FolderName          = 'Tracking/TrackParameters'
+TrackMon_gentk.BSFolderName        = 'Tracking/TrackParameters/BeamSpotParameters'
 
 # DQM Services
 dqmInfoSiStrip = cms.EDFilter("DQMEventInfo",
