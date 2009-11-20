f5931d6756649d21149dc87e24a1b39dbed80034
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2009-11-20 13:25:24 +0000

    move number of DI errors from client to task

--- DQM/EcalPreshowerMonitorClient/interface/ESIntegrityClient.h
+++ DQM/EcalPreshowerMonitorClient/interface/ESIntegrityClient.h
@@ -80,7 +80,6 @@ class ESIntegrityClient : public ESClient {
 
   MonitorElement* meFED_[2][2];
   MonitorElement* meKCHIP_[2][2];
-  MonitorElement* meDIErrors_[2][2];
 
   TH1F *hFED_;  
   TH2F *hFiberOff_;
