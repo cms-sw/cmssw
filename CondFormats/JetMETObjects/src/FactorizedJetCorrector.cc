e7306b191f3bdc945bc2539665b9169395e0e0b9
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2009-11-20 15:40:20 +0000

    *** empty log message ***

--- CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc
+++ CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc
@@ -281,7 +281,7 @@ std::vector<float> FactorizedJetCorrector::getSubCorrections()
       if (mLevels[i]==kL2||mLevels[i]==kL6)
         mCorrectors[i]->setInterpolation(true); 
       scale = mCorrectors[i]->correction(vx,vy); 	
-      if (mLevels[i]==kL6) scale *= 1.0 + getLepPt() / mJetPt;
+      if (mLevels[i]==kL6&&mAddLepToJet) scale *= 1.0 + getLepPt() / mJetPt;
       factor*=scale; 
       factors.push_back(factor);	
       mJetE *=scale;
