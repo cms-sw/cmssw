6af9316eceaa9ff2e642114b942674acd90f6d47
Keith Ulmer <sha1-e5cc6de54e5ce1a7124403d8a15b94d81ab85c60@cern.ch>
2009-11-20 23:19:41 +0000

    add valid hit status to hit efficiency analysis

--- CalibTracker/SiStripHitEfficiency/src/HitEff.cc
+++ CalibTracker/SiStripHitEfficiency/src/HitEff.cc
@@ -101,6 +101,7 @@ void HitEff::beginJob(const edm::EventSetup& c){
   traj->Branch("ModIsBad",&ModIsBad,"ModIsBad/i");
   traj->Branch("SiStripQualBad",&SiStripQualBad,"SiStripQualBad/i");
   traj->Branch("withinAcceptance",&withinAcceptance, "withinAcceptance/O");
+  traj->Branch("trajHitValid", &trajHitValid, "trajHitValid/i");
   traj->Branch("Id",&Id,"Id/i");
   traj->Branch("run",&run,"run/i");
   traj->Branch("event",&event,"event/i");
@@ -337,6 +338,8 @@ void HitEff::analyze(const edm::Event& e, const edm::EventSetup& es){
 	yglob = TM->globalY();
 	zglob = TM->globalZ();
 	withinAcceptance = TM->withinAcceptance();
+
+	trajHitValid = TM->validHit();
 
 	if ( (layers == TKlayers) || (layers == 0) ) {   // Look at the layer not used to reconstruct the track
 	  whatlayer = TKlayers;
@@ -595,8 +598,6 @@ bool HitEff::check2DPartner(uint iidd, std::vector<TrajectoryMeasurement> traj) 
   }
   return found2DPartner;
 }
-
-//  const edmNew::DetSetVector<SiStripCluster>& input = *theClusters;
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(HitEff);
