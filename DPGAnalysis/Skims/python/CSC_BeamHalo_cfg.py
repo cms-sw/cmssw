90392e4ff1f3bbe7d27208ed403ac576ae703ba9
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-11-22 16:20:18 +0000

    first version of csc halo skim

---
+++ DPGAnalysis/Skims/python/CSC_BeamHalo_cfg.py
@@ -0,0 +1,65 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SKIM")
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.7 $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/CSCSkim_trial_cfg.py,v $'),
+    annotation = cms.untracked.string('CRAFT CSCSkim skim')
+)
+
+#
+#
+# This is for testing purposes.
+#
+#
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring("rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/Cosmics/RECO/v2/000/122/045/58063204-36D7-DE11-8B0E-001617DBD556.root"),
+                            secondaryFileNames = cms.untracked.vstring()
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+
+#------------------------------------------
+# Load standard sequences.
+#------------------------------------------
+process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'GR09_P_V6::All' 
+
+process.load("Configuration/StandardSequences/RawToDigi_Data_cff")
+process.load("Configuration/StandardSequences/ReconstructionCosmics_cff")
+
+#------------------------------------------
+# parameters for the CSCSkim module
+#------------------------------------------
+process.load("DPGAnalysis/Skims/CSCSkim_cfi")
+
+
+#set to minimum activity
+process.cscSkim.minimumSegments = 1
+process.cscSkim.minimumHitChambers = 1
+
+#### the path
+process.cscHaloSkim = cms.Path(process.cscSkim)
+
+
+#### output 
+process.outputSkim = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring('keep *','drop *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string("/tmp/arizzi/cscskimEvents.root"),
+    dataset = cms.untracked.PSet(
+      dataTier = cms.untracked.string('RAW-RECO'),
+      filterName = cms.untracked.string('CSCSkim_BeamHalo')
+    ),
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('cscHaloSkim'))
+)
+
+process.outpath = cms.EndPath(process.outputSkim)
+
