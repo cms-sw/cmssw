300f6c000a3c5ccab90eac5bac3f031e4d8ec3a6
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2009-11-23 13:55:14 +0000

    update ConfigEditor tests

--- FWCore/GuiBrowsers/test/Plugins/ConfigEditor/testConfigDataAccessor.py
+++ FWCore/GuiBrowsers/test/Plugins/ConfigEditor/testConfigDataAccessor.py
@@ -9,10 +9,8 @@ logging.root.setLevel(logging.DEBUG)
 import Path
 from Vispa.Main.Directories import *
 examplesDirectory = os.path.join(baseDirectory,"examples/ConfigEditor")
-test=1
 if not os.path.exists(examplesDirectory):
     examplesDirectory = os.path.abspath(os.path.join(os.path.join(baseDirectory,".."),"examples"))
-    test=2
 
 from Vispa.Main.Exceptions import *
 from Vispa.Plugins.ConfigEditor.ConfigDataAccessor import *
@@ -24,28 +22,19 @@ class ConfigDataAccessorTestCase(unittest.TestCase):
         logging.debug(self.__class__.__name__ +': testExample()')
 
         accessor=ConfigDataAccessor()
-        if test==1:
-            accessor.open(os.path.join(examplesDirectory,"cleanLayer1Objects_cff_CMSSW_3_1_X.py"))
+        accessor.open(os.path.join(examplesDirectory,"cleanLayer1Objects_cff.py"))
 
-            self.assertEqual(len(accessor.topLevelObjects()),2)
-            self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),7)
-            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),7)
-            self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"cleanLayer1Objects")
+        self.assertEqual(len(accessor.topLevelObjects()),1)
+        self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),7)
+        self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),7)
+        self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"cleanLayer1Objects")
 
-            accessor.open(os.path.join(examplesDirectory,"patLayer1_fromAOD_full_cfg_CMSSW_3_1_X.py"))
+        accessor.open(os.path.join(examplesDirectory,"patLayer1_fromAOD_full_cfg.py"))
 
-            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),9)
-            self.assertEqual(len(accessor.properties(accessor.children(accessor.topLevelObjects()[0])[0])),5)
-            self.assertEqual(len(accessor.children(accessor.children(accessor.topLevelObjects()[0])[0])),1)
-            self.assertEqual(accessor.label(accessor.children(accessor.topLevelObjects()[0])[0]),"source")
-            
-        if test==2:
-            accessor.open(os.path.join(examplesDirectory,"cleanLayer1Objects_cff_CMSSW_3_1_X.py"))
-
-            self.assertEqual(len(accessor.topLevelObjects()),2)
-            self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),7)
-            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),7)
-            self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"cleanLayer1Objects")
+        self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),9)
+        self.assertEqual(len(accessor.properties(accessor.children(accessor.topLevelObjects()[0])[0])),6)
+        self.assertEqual(len(accessor.children(accessor.children(accessor.topLevelObjects()[0])[0])),1)
+        self.assertEqual(accessor.label(accessor.children(accessor.topLevelObjects()[0])[0]),"source")
 
 if __name__ == "__main__":
     Profiling.analyze("unittest.main()",__file__)
