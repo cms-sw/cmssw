700d3775deab4874935cafb65bd4103572593f5b
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2009-11-23 15:21:50 +0000

    put new naming convention

--- DQMOffline/RecoB/test/dqmPVVertexing_cfg.py
+++ DQMOffline/RecoB/test/dqmPVVertexing_cfg.py
@@ -22,11 +22,12 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring()
 )
 
+
 process.plots = cms.Path(process.pvMonitor*process.dqmSaver)
 process.dqmEnv.subSystemFolder = 'BTAG'
 process.dqmSaver.producer = 'DQM'
 process.dqmSaver.workflow = '/POG/BTAG/PV'
-process.dqmSaver.convention = 'RelVal'
-process.PoolSource.fileNames = ['/store/relval/CMSSW_2_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V6_v1/0000/0620969E-3469-DD11-89ED-000423D987FC.root']
+process.dqmSaver.convention = 'Offline'
+process.PoolSource.fileNames = ['/store/relval/CMSSW_3_3_3/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v2/0002/D84DEA71-0CD2-DE11-AB3C-002618943958.root']
 
 
