d2248095f9736291d8eb39dbe027fa32cb1f2001
Annapaola De Cosa <sha1-4f7f601e1310d1f94a8ce176474325c5ff77ac0c@cern.ch>
2009-11-24 10:14:13 +0000

    add value check method for parameters supporting just some values

--- FWCore/GuiBrowsers/python/ConfigToolBase.py
+++ FWCore/GuiBrowsers/python/ConfigToolBase.py
@@ -1,7 +1,6 @@
 import copy
 import FWCore.ParameterSet.Config as cms
 
-#import PhysicsTools.PatAlgos.Config as cms
 class parameter:
     pass
 
@@ -16,6 +15,7 @@ class ConfigToolBase(object) :
         self._parameters={}
         self._description=self.__doc__
         self._comment = ''
+        self.parAccepted=True
     def __call__(self):
         """ Call the istance 
         """
@@ -38,7 +38,7 @@ class ConfigToolBase(object) :
         """
         return self._description
 
-    def addParameter(self,dict,parname, parvalue, description,Type=None):
+    def addParameter(self,dict,parname, parvalue, description,Type=None, Range=None):
         """ Add a parameter with its label, value, description and type to self._parameters
         """
         par=parameter()
@@ -48,6 +48,7 @@ class ConfigToolBase(object) :
         if Type==None:
             par.type=type(parvalue)
         else: par.type=Type
+        par.range=Range
         dict[par.name]=par
         
     def getParameters(self):
@@ -59,11 +60,12 @@ class ConfigToolBase(object) :
         Possible types are: 'Category','String','Text','File','FileVector','Boolean','Integer','Float'.
         """
         return copy.deepcopy(self._parameters)
-    def setParameter(self, name, value, bool=False):
+    def setParameter(self, name, value, typeNone=False):
         """ Change parameter 'name' to a new value
         """
         self._parameters[name].value=value
-        self.typeError(name, bool)
+        self.typeError(name,typeNone )
+        if self._defaultParameters[name].range is not None: self.isAllowed(name,value )
     def setParameters(self, parameters):
         self._parameters=copy.deepcopy(parameters)
     def dumpPython(self):
@@ -79,6 +81,28 @@ class ConfigToolBase(object) :
     def errorMessage(self,value,type):
         return "The type for parameter "+'"'+str(value)+'"'+" is not "+'"'+str(type)+'"'
 
+    def isAllowed(self,name,value):
+        self.parAccepted=True
+        if value==[]:
+            self.parAccepted=False
+        elif (isinstance(value,dict)) and (isinstance(self._parameters[name].range,list)):
+            
+            for key in value.keys():
+                if key not in self._parameters[name].range:
+                    raise ValueError("The input key value "+'"'+str(key)+'"'+" for parameter "+'"'+name+'"'+" is not supported. Supported ones are: "+str(self._parameters[name].range))
+        elif (isinstance(value,list)) and (isinstance(self._parameters[name].range,list )):
+            for i in value:
+                if i not in self._parameters[name].range:
+                   self.parAccepted=False
+        elif (not isinstance(value,list))and (isinstance(self._parameters[name].range,list)):
+            if value not in self._parameters[name].range:
+                self.parAccepted=False
+        elif not isinstance(self._parameters[name].range,list):
+            if value!=self._parameters[name].range:
+              self.parAccepted=False  
+        if self.parAccepted==False:
+            raise ValueError("The input value "+'"'+str(value)+'"'+" for parameter "+'"'+name+'"'+" is not supported. Supported ones are: "+str(self._parameters[name].range))
+            
     def typeError(self,name, bool=False):
         if bool is False:
             if not isinstance(self._parameters[name].value,self._parameters[name].type):
