2ec0cf3cc9685b9f499cc5e2350fe677bbe7554c
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2009-11-24 22:32:34 +0000

    added sc5 and ak5

--- JetMETCorrections/JetPlusTrack/python/JetPlusTrackCorrectionsPAT_cfg.py
+++ JetMETCorrections/JetPlusTrack/python/JetPlusTrackCorrectionsPAT_cfg.py
@@ -5,17 +5,16 @@ process = cms.Process("TEST")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('STARTUP31X_V4::All')
+process.GlobalTag.globaltag = cms.string('STARTUP31X_V8::All')
 
 process.source = cms.Source (
     "PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/DCAE40E8-CA78-DE11-8F20-001D09F2305C.root',
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/D099AB85-CA78-DE11-9A5E-001D09F2503C.root',
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/B2E190DF-CA78-DE11-9F35-001D09F2532F.root',
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/90A07B7B-CA78-DE11-A4B7-001D09F26C5C.root',
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/4C5F86D1-CB78-DE11-8650-000423D6B42C.root',
-    '/store/relval/CMSSW_3_1_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V2-v1/0007/2247873A-B378-DE11-8F5B-001D09F24664.root',
+    '/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V8-v2/0000/D8D6F277-C5C7-DE11-A59F-002618943962.root',
+    '/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V8-v2/0000/C8072F59-59C8-DE11-BB0A-00261894393B.root',
+    '/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V8-v2/0000/BC410BBC-C4C7-DE11-BA4F-002618FDA237.root',
+    '/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V8-v2/0000/32E84E16-C4C7-DE11-A181-002618943833.root',
+    '/store/relval/CMSSW_3_3_2/RelValQCD_Pt_80_120/GEN-SIM-RECO/STARTUP31X_V8-v2/0000/0295FB14-C4C7-DE11-834B-002618943862.root',
     )
     )
 
@@ -27,21 +26,58 @@ process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
 
-addJetCollection(process,
-                 cms.InputTag('JetPlusTrackZSPCorJetIcone5'),
-                 'IC5JPT',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = None,
-                 doType1MET   = False,
-                 doL1Cleaning = True,
-                 doL1Counters = True,
-                 genJetCollection = cms.InputTag("iterativeCone5GenJets")
-                 )
+addJetCollection( process,
+                  cms.InputTag('JetPlusTrackZSPCorJetIcone5'),
+                  'IC5JPT',
+                  doJTA        = True,
+                  doBTagging   = True,
+                  jetCorrLabel = None,
+                  doType1MET   = False,
+                  doL1Cleaning = True,
+                  doL1Counters = True,
+                  genJetCollection = cms.InputTag("iterativeCone5GenJets"),
+                  doJetID      = False,
+                  jetIdLabel   = "",
+                  )
+
+addJetCollection( process,
+                  cms.InputTag('JetPlusTrackZSPCorJetSiscone5'),
+                  'SC5JPT',
+                  doJTA        = True,
+                  doBTagging   = True,
+                  jetCorrLabel = None,
+                  doType1MET   = False,
+                  doL1Cleaning = True,
+                  doL1Counters = True,
+                  genJetCollection = cms.InputTag("sisCone5GenJets"),
+                  doJetID      = False,
+                  jetIdLabel   = "",
+                  )
+
+addJetCollection( process,
+                  cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
+                  'AK5JPT',
+                  doJTA        = True,
+                  doBTagging   = True,
+                  jetCorrLabel = None,
+                  doType1MET   = False,
+                  doL1Cleaning = True,
+                  doL1Counters = True,
+                  genJetCollection = cms.InputTag("ak5GenJets"),
+                  doJetID      = False,
+                  jetIdLabel   = "",
+                  )
+
+#process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 
 process.p = cms.Path (
-    process.ZSPJetCorrections *
-    process.JetPlusTrackCorrections *
+#    process.dump *
+    process.ZSPJetCorrectionsIcone5 *
+    process.JetPlusTrackCorrectionsIcone5 *
+    process.ZSPJetCorrectionsSisCone5 *
+    process.JetPlusTrackCorrectionsSisCone5 *
+    process.ZSPJetCorrectionsAntiKt5 *
+    process.JetPlusTrackCorrectionsAntiKt5 *
     process.patDefaultSequence
     )
 
