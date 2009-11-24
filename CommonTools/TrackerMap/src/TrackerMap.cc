8df7519b1c560d3517c67104ebf385c9113d4ba9
Giuseppe Zito <sha1-7380d6786446b2a2af7dbb0310b9de8281c37b4e@cern.ch>
2009-11-24 17:33:28 +0000

    new trackermap layout

--- CommonTools/TrackerMap/src/TrackerMap.cc
+++ CommonTools/TrackerMap/src/TrackerMap.cc
@@ -158,6 +158,7 @@ void TrackerMap::init() {
   posrel=true;
   palette = 1;
   printflag=true;
+  addPixelFlag=false;
   temporary_file=false;
   gminvalue=0.; gmaxvalue=0.;//default global range for online rendering
 
@@ -350,7 +351,7 @@ if(!print_total)mod->value=mod->value*mod->count;//restore mod->value
 void TrackerMap::save(bool print_total,float minval, float maxval,std::string s,int width, int height){
   std::string filetype=s,outputfilename=s;
   vector<TPolyLine*> vp;
-  TGaxis *axis ;
+  TGaxis *axis = 0 ;
   filetype.erase(0,filetype.find(".")+1);
   outputfilename.erase(outputfilename.begin()+outputfilename.find("."),outputfilename.end());
   temporary_file=true;
@@ -438,7 +439,7 @@ void TrackerMap::save(bool print_total,float minval, float maxval,std::string s,
     TCanvas *MyC = new TCanvas("MyC", "TrackerMap",width,height);
     gPad->SetFillColor(38);
     
-    gPad->Range(0,0,3200,1600);
+    if(addPixelFlag)gPad->Range(0,0,3800,1600);else gPad->Range(800,0,3800,1600);
     
     //First  build palette
     ncolor=0;
@@ -480,32 +481,40 @@ void TrackerMap::save(bool print_total,float minval, float maxval,std::string s,
       }
     }
     if (printflag) {
-      axis = new TGaxis(3060,36,3060,1530,minvalue,maxvalue,510,"+L");
+      axis = new TGaxis(3660,36,3660,1530,minvalue,maxvalue,510,"+L");
       axis->SetLabelSize(0.02);
       axis->Draw();
     }
     TLatex l;
+    l.SetTextSize(0.03);
+    l.DrawLatex(950,1330,"TID");
+    l.DrawLatex(2300,1330,"TEC");
+    l.DrawLatex(300,1330,"FPIX");
+    l.DrawLatex(20,560,"BPIX L1");
+    l.DrawLatex(500,385,"BPIX L2");
+    l.DrawLatex(500,945,"BPIX L3");
     l.SetTextSize(0.04);
-    l.DrawLatex(500,50,"-z");
-    l.DrawLatex(500,1430,"+z");
-    l.DrawLatex(900,330,"TIB L1");
-    l.DrawLatex(900,1000,"TIB L2");
-    l.DrawLatex(1300,330,"TIB L3");
-    l.DrawLatex(1300,1000,"TIB L4");
-    l.DrawLatex(1700,330,"TOB L1");
-    l.DrawLatex(1700,1000,"TOB L2");
-    l.DrawLatex(2100,330,"TOB L3");
-    l.DrawLatex(2100,1000,"TOB L4");
-    l.DrawLatex(2500,330,"TOB L5");
-    l.DrawLatex(2500,1000,"TOB L6");
-    TArrow arx(2900,1190,2900,1350,0.01,"|>");
-    l.DrawLatex(2915,1350,"x");
-    TArrow ary(2900,1190,2790,1190,0.01,"|>");
-    l.DrawLatex(2790,1210,"y");
-    TArrow arz(2790,373,2790,672,0.01,"|>");
-    l.DrawLatex(2820,667,"z");
-    TArrow arphi(2790,511,2447,511,0.01,"|>");
-    l.DrawLatex(2433,520,"#Phi");
+    l.DrawLatex(850,1500,title.c_str());
+    l.DrawLatex(1730,40,"-z");
+    l.DrawLatex(1730,1360,"+z");
+    l.DrawLatex(1085,330,"TIB L1");
+    l.DrawLatex(1085,1000,"TIB L2");
+    l.DrawLatex(1585,330,"TIB L3");
+    l.DrawLatex(1585,1000,"TIB L4");
+    l.DrawLatex(2085,330,"TOB L1");
+    l.DrawLatex(2085,1000,"TOB L2");
+    l.DrawLatex(2585,330,"TOB L3");
+    l.DrawLatex(2585,1000,"TOB L4");
+    l.DrawLatex(3085,330,"TOB L5");
+    l.DrawLatex(3085,1000,"TOB L6");
+    TArrow arx(3448,1190,3448,1350,0.01,"|>");
+    l.DrawLatex(3460,1350,"x");
+    TArrow ary(3448,1190,3312,1190,0.01,"|>");
+    l.DrawLatex(3312,1210,"y");
+    TArrow arz(3485,373,3485,676,0.01,"|>");
+    l.DrawLatex(3510,667,"z");
+    TArrow arphi(3485,511,3037,511,0.01,"|>");
+    l.DrawLatex(3023,520,"#Phi");
     arx.SetLineWidth(3);
     ary.SetLineWidth(3);
     arz.SetLineWidth(3);
@@ -760,7 +769,7 @@ void TrackerMap::save_as_fedtrackermap(bool print_total,float minval, float maxv
 }
   
   if(temporary_file){
-    if(printflag)drawPalette(savefile);
+    //if(printflag)drawPalette(savefile);
   savefile->close(); delete savefile;
 
   const char * command1;
@@ -771,7 +780,7 @@ void TrackerMap::save_as_fedtrackermap(bool print_total,float minval, float maxv
     TCanvas *MyC = new TCanvas("MyC", "TrackerMap",width,height);
     gPad->SetFillColor(38);
     
-    gPad->Range(0,0,3700,1600);
+    gPad->Range(0,0,3750,1600);
     
     //First  build palette
     ncolor=0;
@@ -959,16 +968,25 @@ void TrackerMap::drawPalette(ofstream * svgfile){
      red=(color>>16)&0xFF;
      green=(color>>8)&0xFF;
      blue=(color)&0xFF;
-    if(!temporary_file)*svgfile <<"<svg:rect  x=\"3010\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"6\" fill=\"rgb("<<red<<","<<green<<","<<blue<<")\" />\n"; 
-    else *svgfile << red << " " << green << " " << blue << " 4 " << (6*i)+40 << " 3010. " <<//
-              (6*i)+40 << " 3060. " <<//
-              (6*(i-1))+40 << " 3060. " <<//
-              (6*(i-1))+40 <<" 3010. " << endl; //
+ //   if(!temporary_file)*svgfile <<"<svg:rect  x=\"3010\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"6\" fill=\"rgb("<<red<<","<<green<<","<<blue<<")\" />\n"; 
+  //  else *svgfile << red << " " << green << " " << blue << " 4 " << (6*i)+40 << " 3010. " <<//
+   //           (6*i)+40 << " 3060. " <<//
+    //          (6*(i-1))+40 << " 3060. " <<//
+     //         (6*(i-1))+40 <<" 3010. " << endl; //
+
+   // if(i%50 == 0){
+    //  if(!temporary_file)*svgfile <<"<svg:rect  x=\"3010\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"1\" fill=\"black\" />\n";
+     // if(i%50==0&&!temporary_file)*svgfile << " <svg:text  class=\"normalText\"  x=\"3060\" y=\""<<(1560-6*i)<<"\">" <<val<<"</svg:text>"<<endl;
+
+    if(!temporary_file)*svgfile <<"<svg:rect  x=\"3610\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"6\" fill=\"rgb("<<red<<","<<green<<","<<blue<<")\" />\n"; 
+    else *svgfile << red << " " << green << " " << blue << " 4 " << (6*i)+40 << " 3610. " <<//
+              (6*i)+40 << " 3660. " <<//
+              (6*(i-1))+40 << " 3660. " <<//
+              (6*(i-1))+40 <<" 3610. " << endl; //
 
     if(i%50 == 0){
-      if(!temporary_file)*svgfile <<"<svg:rect  x=\"3010\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"1\" fill=\"black\" />\n";
-      if(i%50==0&&!temporary_file)*svgfile << " <svg:text  class=\"normalText\"  x=\"3060\" y=\""<<(1560-6*i)<<"\">" <<val<<"</svg:text>"<<endl;
-
+     if(!temporary_file)*svgfile <<"<svg:rect  x=\"3610\" y=\""<<(1550-6*i)<<"\" width=\"50\" height=\"1\" fill=\"black\" />\n";
+      if(i%50==0&&!temporary_file)*svgfile << " <svg:text  class=\"normalText\"  x=\"3660\" y=\""<<(1560-6*i)<<"\">" <<val<<"</svg:text>"<<endl;
        }
     val = val + dval;
    }
