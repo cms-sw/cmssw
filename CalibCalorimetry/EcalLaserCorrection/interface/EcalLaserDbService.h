9cf5dc491c8dfec70822de990d7279bc1774e80f
Jan Veverka <sha1-5bc4759244b0b5d8db8955c497cf365c52e12cb7@cern.ch>
2009-11-24 15:18:16 +0000

    use same LM mapping as EcalLaserAnalyzer

--- CalibCalorimetry/EcalLaserCorrection/interface/EcalLaserDbService.h
+++ CalibCalorimetry/EcalLaserCorrection/interface/EcalLaserDbService.h
@@ -37,8 +37,6 @@ class EcalLaserDbService {
 
  private:
 
-  int getLMNumber(DetId const & xid) const;
-
   const EcalLaserAlphas* mAlphas_;
   const EcalLaserAPDPNRatiosRef* mAPDPNRatiosRef_;
   const EcalLaserAPDPNRatios* mAPDPNRatios_;  
