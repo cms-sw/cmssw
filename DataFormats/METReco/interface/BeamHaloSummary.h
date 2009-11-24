4cf531234557c61354fde0d1852bc77bb9854c37
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-11-24 10:13:13 +0000

    First complete prototype of the BeamHaloSummary object

--- DataFormats/METReco/interface/BeamHaloSummary.h
+++ DataFormats/METReco/interface/BeamHaloSummary.h
@@ -31,8 +31,55 @@ namespace reco {
     //destructor
     virtual ~BeamHaloSummary() {}
   
-  private: 
+    const bool HcalLooseHaloId()const { return  HcalHaloReport.size() ?  HcalHaloReport[0] : false ; }
+    const bool HcalTightHaloId()const { return  HcalHaloReport.size() > 1 ? HcalHaloReport[1] : false ; }
     
+    const bool EcalLooseHaloId()const { return  EcalHaloReport.size() ?  EcalHaloReport[0] : false ; }
+    const bool EcalTightHaloId()const { return  EcalHaloReport.size() > 1 ? EcalHaloReport[1] : false ; }
+
+    const bool CSCLooseHaloId() const { return CSCHaloReport.size() ? CSCHaloReport[0] : false ; }
+    const bool CSCTightHaloId() const { return CSCHaloReport.size() > 1 ? CSCHaloReport[1] : false ; }
+    
+    const bool GlobalLooseHaloId() const { return GlobalHaloReport.size() ? GlobalHaloReport[0] : false ; }
+    const bool GlobalTightHaloId() const { return GlobalHaloReport.size() > 1 ? GlobalHaloReport[1] : false ; }
+
+    const bool EventSmellsLikeHalo() const { return HcalLooseHaloId() || EcalLooseHaloId() || CSCLooseHaloId() || GlobalLooseHaloId() ; }
+    const bool LooseId() const { return EventSmellsLikeHalo(); }
+    const bool TightId() const { return HcalTightHaloId() || EcalTightHaloId() || CSCTightHaloId() || GlobalTightHaloId() ; }
+    const bool ExtremeTightId ()const { return  GlobalTightHaloId() ; }
+
+    //  Getters
+    std::vector<char>& GetHcalHaloReport(){return HcalHaloReport;}
+    const std::vector<char>& GetHcalHaloReport() const { return HcalHaloReport;}
+    
+    std::vector<char>& GetEcalHaloReport(){return EcalHaloReport;}
+    const std::vector<char>& GetEcalHaloReport() const {return EcalHaloReport;}
+
+    std::vector<char>& GetCSCHaloReport() { return CSCHaloReport ;}
+    const std::vector<char>& GetCSCHaloReport() const { return CSCHaloReport ; }
+
+    std::vector<char>& GetGlobalHaloReport() { return GlobalHaloReport ;} 
+    const std::vector<char>& GetGlobalHaloReport() const {return GlobalHaloReport ; }
+
+    std::vector<int>& GetHcaliPhiSuspects() { return HcaliPhiSuspects ; }
+    const std::vector<int>& GetHcaliPhiSuspects() const { return HcaliPhiSuspects ; }
+
+    std::vector<int>& GetEcaliPhiSuspects() { return EcaliPhiSuspects ; }
+    const std::vector<int>& GetEcaliPhiSuspects() const { return EcaliPhiSuspects ; }
+
+    std::vector<int>& GetGlobaliPhiSuspects() { return GlobaliPhiSuspects ;}
+    const std::vector<int>& GetGlobaliPhiSuspects() const { return GlobaliPhiSuspects ;}
+
+  private: 
+    std::vector<char>  HcalHaloReport;
+    std::vector<char>  EcalHaloReport;
+    std::vector<char>  CSCHaloReport;
+    std::vector<char>  GlobalHaloReport;
+
+    std::vector<int>  HcaliPhiSuspects;
+    std::vector<int>  EcaliPhiSuspects;
+    std::vector<int>  GlobaliPhiSuspects;
+
   };
   
 }
