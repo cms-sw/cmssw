806987a477be509bd57507481eed38779d88af8e
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-11-25 14:29:52 +0000

    can print all IOVs - and use output file instead of MessageLogger if desired

--- CondTools/HLT/src/AlCaRecoTriggerBitsRcdRead.cc
+++ CondTools/HLT/src/AlCaRecoTriggerBitsRcdRead.cc
@@ -1,8 +1,17 @@
 ///////////////////////////////////////////////////////////////////////
 ///
 /// Module to read trigger bit mappings (AlCaRecoTriggerBits) from
-/// DB and put as text. If configured, a python snippet to be inserted
-/// into configuration of the AlCaRecoTriggerBitsRcdWrite is put out.
+/// DB and put out as text. 
+/// Several output formats can be configured via parameter 'outputType':
+/// - simple text ('text'),
+/// - text in format of a Twiki table for cut and paste ('twiki')
+/// - or a python snippet to be inserted into configuration of the
+///   AlCaRecoTriggerBitsRcdWrite ('python').
+///
+/// Where to put the text is decided by parameter 'rawFileName':
+/// - if empty, use MessageLogger
+/// - otherwise open file <rawFileName>.<suffix> where the suffix
+///   is chosen to match 'outputType'.
 ///
 ///////////////////////////////////////////////////////////////////////
 
@@ -10,11 +19,13 @@
 #include <map>
 //#include <vector>
 #include <sstream>
+#include <fstream>
 
 // Framework
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -27,18 +38,30 @@
 
 class  AlCaRecoTriggerBitsRcdRead : public edm::EDAnalyzer {
 public:
-  explicit  AlCaRecoTriggerBitsRcdRead(const edm::ParameterSet& cfg);
+  explicit  AlCaRecoTriggerBitsRcdRead(const edm::ParameterSet &cfg);
   ~AlCaRecoTriggerBitsRcdRead() {}
   
-  virtual void analyze(const edm::Event& evt, const edm::EventSetup& evtSetup) {}
-  virtual void beginRun(const edm::Run& run, const edm::EventSetup& evtSetup);
+  virtual void analyze(const edm::Event &evt, const edm::EventSetup &evtSetup) {}
+  virtual void beginRun(const edm::Run &run, const edm::EventSetup &evtSetup);
+  virtual void endJob();
 
   
 private:
-  enum OutputType {kText, kTwiki, kPython}; //kHtml
-  OutputType stringToEnum(const std::string &outputType) const;
+  // types
+  enum OutputType {kText, kTwiki, kPython}; //kHtml};
 
+  // methods
+  OutputType stringToEnum(const std::string &outputType) const;
+  void printMap(edm::RunNumber_t firstRun, edm::RunNumber_t lastRun,
+		const AlCaRecoTriggerBits &triggerMap) const;
+
+  // members
   const OutputType outputType_;
+  edm::ESWatcher<AlCaRecoTriggerBitsRcd> watcher_;
+  edm::RunNumber_t firstRun_; 
+  edm::RunNumber_t lastRun_;
+  AlCaRecoTriggerBits lastTriggerBits_;
+  std::auto_ptr<std::ofstream> output_;
 };
 
 ///////////////////////////////////////////////////////////////////////
@@ -46,10 +69,26 @@ private:
 ///////////////////////////////////////////////////////////////////////
 
 AlCaRecoTriggerBitsRcdRead::AlCaRecoTriggerBitsRcdRead(const edm::ParameterSet& cfg)
-  : outputType_(this->stringToEnum(cfg.getUntrackedParameter<std::string>("outputType")))
+  : outputType_(this->stringToEnum(cfg.getUntrackedParameter<std::string>("outputType"))),
+    firstRun_(0), lastRun_(0)
 {
   //   edm::LogInfo("") << "@SUB=AlCaRecoTriggerBitsRcdRead" 
   // 		   << cfg.getParameter<std::string>("@module_label");
+
+  std::string fileName(cfg.getUntrackedParameter<std::string>("rawFileName"));
+  switch (outputType_) { // now append suffix
+  case kText:   fileName += ".txt";   break;
+  case kPython: fileName += ".py";    break;
+  case kTwiki:  fileName += ".twiki"; break;
+  }
+  if (fileName.size()) {
+    output_.reset(new std::ofstream(fileName.c_str()));
+    if (!output_->good()) {
+      edm::LogError("IOproblem") << "Could not open output file " << fileName << ".";
+      output_.reset();
+    }
+  }
+
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -71,17 +110,54 @@ AlCaRecoTriggerBitsRcdRead::stringToEnum(const std::string &outputTypeStr) const
 ///////////////////////////////////////////////////////////////////////
 void AlCaRecoTriggerBitsRcdRead::beginRun(const edm::Run& run, const edm::EventSetup& iSetup)
 {
-  // Get map of strings to concatenated list of names of HLT paths from EventSetup:
-  edm::ESHandle<AlCaRecoTriggerBits> triggerBits;
-  iSetup.get<AlCaRecoTriggerBitsRcd>().get(triggerBits);
-  typedef std::map<std::string, std::string> TriggerMap;
-  const TriggerMap &triggerMap = triggerBits->m_alcarecoToTrig;
+  if (watcher_.check(iSetup)) { // new IOV for this run
+    // Print last IOV - if there has already been one:
+    if (lastRun_ != 0) this->printMap(firstRun_, lastRun_, lastTriggerBits_);
+  
+    // Get AlCaRecoTriggerBits from EventSetup:
+    edm::ESHandle<AlCaRecoTriggerBits> triggerBits;
+    iSetup.get<AlCaRecoTriggerBitsRcd>().get(triggerBits);
+    lastTriggerBits_ = *triggerBits; // copy for later use
+    firstRun_ = run.run();           // keep track where it started
+  }
 
-  // Collect output in an ostringstream, starting with run number.
-  // Format depends on outputType_ configuration:
+  lastRun_ = run.run(); // keep track of last visited run
+}
+
+///////////////////////////////////////////////////////////////////////
+void AlCaRecoTriggerBitsRcdRead::endJob()
+{
+  // Print for very last IOV, not treated yet in beginRun(..):
+  this->printMap(firstRun_, lastRun_, lastTriggerBits_);
+}
+
+///////////////////////////////////////////////////////////////////////
+void AlCaRecoTriggerBitsRcdRead::printMap(edm::RunNumber_t firstRun,
+					  edm::RunNumber_t lastRun, 
+					  const AlCaRecoTriggerBits &triggerBits) const
+{
+  // Get map of strings to concatenated list of names of HLT paths:
+  typedef std::map<std::string, std::string> TriggerMap;
+  const TriggerMap &triggerMap = triggerBits.m_alcarecoToTrig;
+
+  // Collect output for given run numbers via ostringstream.
+  // Format depends on outputType_ configuration.
   std::ostringstream output;
-  output << "#\n# Run number " << run.run() << " has these AlCaRecoTriggerBits settings:\n#\n";
-  if (outputType_ == kPython) output << "  triggerLists = cms.VPSet(\n";
+  switch (outputType_) {
+  case kPython:
+    output << "  triggerLists = cms.VPSet(\n";
+    // no 'break;'!
+  case kText:
+    output << "#\n# AlCaRecoTriggerBits settings for IOV "
+	   << firstRun << "-" << lastRun << ":\n#\n";
+    break;
+  case kTwiki:
+    output << "---+++++ *IOV*: " << firstRun << "-" << lastRun << "\n"
+	   << "| *TriggerBits list key* | *HLT paths* |\n";
+    break;
+  }
+
+  //  if (outputType_ == kPython) output << "  triggerLists = cms.VPSet(\n";
 
   // loop over entries in map
   for (TriggerMap::const_iterator i = triggerMap.begin(); i != triggerMap.end(); ++i) {
@@ -98,12 +174,10 @@ void AlCaRecoTriggerBitsRcdRead::beginRun(const edm::Run& run, const edm::EventS
       break;
     case kTwiki:
       output << "| '" << i->first << "' | ";
-      //'HLT_Ele15_SW_L1R', 'HLT_DoubleEle10_SW_L1R', 'HLT_Ele10_LW_L1R',
-      //'HLT_DoubleEle5_SW_L1R' |
     }
     // We must avoid a map<string,vector<string> > in DB for performance reason,
     // so the paths are mapped into one string separated by ';':
-    const std::vector<std::string> paths = triggerBits->decompose(i->second);
+    const std::vector<std::string> paths = triggerBits.decompose(i->second);
     for (unsigned int iPath = 0; iPath < paths.size(); ++iPath) {
       if (iPath != 0) {
 	output << ", "; // next path
@@ -132,11 +206,14 @@ void AlCaRecoTriggerBitsRcdRead::beginRun(const edm::Run& run, const edm::EventS
       output << " |\n";
     }
   }
-  if (outputType_ == kPython) output << "\n      ) # closing of VPSet triggerLists"; 
+  if (outputType_ == kPython) output << "\n      ) # closing of VPSet triggerLists\n"; 
   
-  // Final output:
-  edm::LogInfo("") << "@SUB=beginRun" << output.str();
+  // Final output - either message logger or output file:
+  if (output_.get()) *output_ << output.str();
+  else edm::LogInfo("") << output.str();
 }
+
+
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(AlCaRecoTriggerBitsRcdRead);
--- CondTools/HLT/test/AlCaRecoTriggerBitsRcdRead_cfg.py
+++ CondTools/HLT/test/AlCaRecoTriggerBitsRcdRead_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("WRITEDB")
+process = cms.Process("READ")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr = cms.untracked.PSet(placeholder = cms.untracked.bool(True))
@@ -11,29 +11,37 @@ process.MessageLogger.cout = cms.untracked.PSet(INFO = cms.untracked.PSet(
 # the module writing to DB
 process.load("CondTools.HLT.AlCaRecoTriggerBitsRcdRead_cfi")
 # 'twiki' is default - others are text, python (future: html?)
-process.AlCaRecoTriggerBitsRcdRead.outputType = 'twiki'
+#process.AlCaRecoTriggerBitsRcdRead.outputType = 'twiki'
+# If rawFileName empty, use message logger, otherwise add suffix according to outputType:
+process.AlCaRecoTriggerBitsRcdRead.rawFileName = 'triggerBits'
 
-# No data, but have to specify run number (default is 1):
+# No data, but might want to specify 'firstRun' to check (default is 1):
 process.source = cms.Source("EmptySource",
-                            numberEventsInRun = cms.untracked.uint32(1),
-                            #firstRun = cms.untracked.uint32(5)
+                            numberEventsInRun = cms.untracked.uint32(1), # do not change!
+                            firstRun = cms.untracked.uint32(1)
                             )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+# With 'numberEventsInRun = 1' above,
+# this will check IOVs until run (!) number specified as 'input' here,
+# so take care not to choose a one that is not too small...:
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(250000) )
 
 # Input for AlCaRecoTriggerBitsRcd,
 # either via GloblalTag (use of _cfi instead of _cff sufficient and faster):
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi")
-process.GlobalTag.globaltag = "MC_3XY_V14::All" # choose your tag
+process.GlobalTag.globaltag = "DESIGN_3X_V13::All" # choose your tag
 
 # ...or specify database and tag:  
 #import CondCore.DBCommon.CondDBSetup_cfi
 #process.dbInput = cms.ESSource(
 #    "PoolDBESSource",
 #    CondCore.DBCommon.CondDBSetup_cfi.CondDBSetup,
-#    connect = cms.string('sqlite_file:AlCaRecoTriggerBits.db'),
+##    connect = cms.string('sqlite_file:AlCaRecoTriggerBits.db'),
+#    connect = cms.string('frontier://FrontierProd/CMS_COND_31X_HLT'),
 #    toGet = cms.VPSet(cms.PSet(
 #        record = cms.string('AlCaRecoTriggerBitsRcd'),
-#        tag = cms.string('AlCaRecoHLTpaths8e29_1e31_v2') # choose tag you want
+##        tag = cms.string('AlCaRecoHLTpaths8e29_1e31_v2') # choose tag you want
+#        tag = cms.string('AlCaRecoHLTpaths8e29_1e31_v5_hlt') # choose tag you want
+#
 #        )
 #                      )
 #    )
