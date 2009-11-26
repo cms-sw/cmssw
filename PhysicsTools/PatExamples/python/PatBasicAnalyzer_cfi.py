3f1071852cbdb672b96f86b8f1dfe8d4524e48ed
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-11-26 15:05:22 +0000

    slight modifications to the PatBasicExample, introduced for the forst time for the dec09 tutorial

---
+++ PhysicsTools/PatExamples/python/PatBasicAnalyzer_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+analyzeBasicPat = cms.EDFilter("PatBasicAnalyzer",
+  photonSrc   = cms.untracked.InputTag("cleanLayer1Photons"),
+  electronSrc = cms.untracked.InputTag("cleanLayer1Electrons"),
+  muonSrc     = cms.untracked.InputTag("cleanLayer1Muons"),                                             
+  tauSrc      = cms.untracked.InputTag("cleanLayer1Taus"),
+  jetSrc      = cms.untracked.InputTag("cleanLayer1Jets"),
+  metSrc      = cms.untracked.InputTag("layer1METs")
+)
