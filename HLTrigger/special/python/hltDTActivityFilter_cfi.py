14edc0410b8df0c522ceee8747293f2de8bcbff9
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-11-26 16:52:08 +0000

    rename HLTDTActivityFilter_cfi.py to hltDTActivityFilter_cfi.py

---
+++ HLTrigger/special/python/hltDTActivityFilter_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+hltDTActivityFilter = cms.EDFilter( "HLTDTActivityFilter",
+   inputDCC         = cms.InputTag( "hltDTTFUnpacker" ), 
+   inputDDU         = cms.InputTag( "hltMuonDTDigis" ), 
+   inputDigis       = cms.InputTag( "hltMuonDTDigis" ), 
+   processDCC       = cms.bool( True ), 
+   processDDU       = cms.bool( True ), 
+   processDigis     = cms.bool( True ), 
+   processingMode   = cms.int32( 2 ),   # 0=(DCC | DDU) | Digis
+                                        # 1=(DCC & DDU) | Digis
+                                        # 2=(DCC | DDU) & Digis
+                                        # 3=(DCC & DDU) & Digis
+   minChamberLayers = cms.int32( 6 ),
+   maxStation       = cms.int32( 3 ),
+   minQual          = cms.int32( 2 ),   # 0-1=L 2-3=H 4=LL 5=HL 6=HH
+   minDDUBX         = cms.int32( 9 ),
+   maxDDUBX         = cms.int32( 14 ),
+   minActiveChamb   = cms.int32( 1 )
+)
