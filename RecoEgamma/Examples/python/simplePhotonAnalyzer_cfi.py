b3736f7ee59629d39cb51d4d572568edef26d514
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2009-11-26 19:37:36 +0000

    Update for cmssw 334

--- RecoEgamma/Examples/python/simplePhotonAnalyzer_cfi.py
+++ RecoEgamma/Examples/python/simplePhotonAnalyzer_cfi.py
@@ -2,13 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 #
 #  Author: N. Marinelli, U. of Notre Dame, US
-#  $Id: simplePhotonAnalyzer.cfi,v 1.8 2008/06/03 13:53:55 nancy Exp $ 
+#  $Id: simplePhotonAnalyzer_cfi.py,v 1.3 2008/06/04 19:40:01 rpw Exp $ 
 #
 simplePhotonAnalyzer = cms.EDAnalyzer("SimplePhotonAnalyzer",
     phoProducer = cms.string('photons'),
-    mcProducer = cms.string('source'),
+    mcProducer = cms.string('generator'),
     endcapEcalHits = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-    # 1=pt10,2=pt35,3=E10,4=E1000
+    # 1=pt10,2=pt35,3=Hgg, 4=gamJetHighEnergy
     sample = cms.int32(2),
     photonCollection = cms.string(''),
     primaryVertexProducer = cms.string('offlinePrimaryVerticesWithBS'),
