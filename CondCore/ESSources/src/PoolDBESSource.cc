3ddc10d57a7cdb355ced668370c35ff6644c126a
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-11-28 10:32:01 +0000

    fix iovsync

--- CondCore/ESSources/src/PoolDBESSource.cc
+++ CondCore/ESSources/src/PoolDBESSource.cc
@@ -274,17 +274,19 @@ PoolDBESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey& iKey
     
     recordValidity.first = std::max(recordValidity.first,validity.first);
     recordValidity.second = std::min(recordValidity.second,validity.second);
+ 
+   //std::cout<<"setting validity "<<recordValidity.first<<" "<<recordValidity.second<<" for ibtime "<<abtime<< std::endl;
+ 
   }      
    
   // to force refresh we set end-value to the minimum such an IOV can exend to: current run or lumiblock
     
-  if (!userTime) {
+  if ( (!userTime) && recordValidity.second!=0) {
     edm::IOVSyncValue start = cond::toIOVSyncValue(recordValidity.first, timetype, true);
     edm::IOVSyncValue stop = doRefresh ? cond::limitedIOVSyncValue (iTime, timetype)
       : cond::toIOVSyncValue(recordValidity.second, timetype, false);
     
-    //std::cout<<"setting validity "<<recordValidity.first<<" "<<recordValidity.second<<" for ibtime "<<abtime<< std::endl;
-    
+   
     oInterval = edm::ValidityInterval( start, stop );
    }
 }
@@ -313,7 +315,8 @@ PoolDBESSource::registerProxies(const edm::eventsetup::EventSetupRecordKey& iRec
 
 // required by the EventSetup System
 void 
-PoolDBESSource::newInterval(const edm::eventsetup::EventSetupRecordKey& iRecordType,const edm::ValidityInterval& iInterval) 
+PoolDBESSource::newInterval(const edm::eventsetup::EventSetupRecordKey& iRecordType,
+			    const edm::ValidityInterval&) 
 {
   //LogDebug ("PoolDBESSource")<<"newInterval";
   invalidateProxies(iRecordType);
