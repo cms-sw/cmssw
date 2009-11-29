b392dc828e41816b0cab076e1a6ff07903f5e04d
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2009-11-29 10:18:45 +0000

    New set of triggers.

--- DQM/Physics/python/qcdLowPtDQM_cfi.py
+++ DQM/Physics/python/qcdLowPtDQM_cfi.py
@@ -1,4 +1,4 @@
-# $Id: qcdLowPtDQM_cfi.py,v 1.6 2009/11/19 22:32:41 loizides Exp $
+# $Id: qcdLowPtDQM_cfi.py,v 1.7 2009/11/23 10:06:49 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -23,17 +23,16 @@ myRecoSeq = cms.Sequence(myRecoSeq1 * myRecoSeq2)
 
 QcdLowPtDQM = cms.EDAnalyzer("QcdLowPtDQM",
     hltTrgNames  = cms.untracked.vstring(
-    'HLT_ZeroBias'
-#    'HLT_MinBias',
     'HLT_MinBiasBSC',
+    'HLT_L1_BSC',
     'HLT_MinBiasBSC_OR',
     'HLT_MinBiasEcal',
     'HLT_MinBiasHcal',
-    'HLT_MinBiasPixel_SingleTrack',
-    'HLT_HFThreshold',
+    'HLT_ZeroBiasPixel_SingleTrack',
+    'HLT_HighMultiplicity_BSC',
+#    'HLT_HFThreshold3',
 #    'HLT_Activity_Ecal',
-    'HLT_Activity_EcalREM',
-    'HLT_Activity_PixelClusters',
+#    'HLT_Activity_PixelClusters',
     ),
                              
     verbose = cms.untracked.int32(3)
