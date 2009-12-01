068dee52c4d262fe3ce62b2cb97c1dcefaa63426
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-12-01 19:49:01 +0000

    handle case where one finder uses time-only and another one uses run/lumi/event-only sync values

--- FWCore/Framework/src/DependentRecordIntervalFinder.cc
+++ FWCore/Framework/src/DependentRecordIntervalFinder.cc
@@ -8,7 +8,7 @@
 //
 // Author:      Chris Jones
 // Created:     Sat Apr 30 19:37:22 EDT 2005
-// $Id: DependentRecordIntervalFinder.cc,v 1.7 2007/01/19 05:25:11 wmtan Exp $
+// $Id: DependentRecordIntervalFinder.cc,v 1.8 2007/06/13 11:10:26 chrjones Exp $
 //
 
 // system include files
@@ -31,7 +31,8 @@ namespace edm {
 // constructors and destructor
 //
 DependentRecordIntervalFinder::DependentRecordIntervalFinder(const EventSetupRecordKey& iKey) :
-  providers_()
+  providers_(),
+  m_previousSyncTo(IOVSyncValue::invalidIOVSyncValue())
 {
    findingRecordWithKey(iKey);
 }
@@ -93,12 +94,18 @@ DependentRecordIntervalFinder::setIntervalFor(const EventSetupRecordKey& iKey,
        newInterval = test;
      }
    }
+   bool intervalsWereComparible = true;
    for(Providers::iterator itProvider = providers_.begin(), itProviderEnd = providers_.end();
        itProvider != itProviderEnd;
        ++itProvider) {
       if((*itProvider)->setValidityIntervalFor(iTime)) {
          haveAValidDependentRecord=true;
          ValidityInterval providerInterval = (*itProvider)->validityInterval();
+	 if( (!newInterval.first().comparable(providerInterval.first())) ||
+	     (!newInterval.last().comparable(providerInterval.last()))) {
+	   intervalsWereComparible=false;
+	   break;
+	 }
          if(newInterval.first() < providerInterval.first()) {
             newInterval.setFirst(providerInterval.first());
          }
@@ -107,11 +114,33 @@ DependentRecordIntervalFinder::setIntervalFor(const EventSetupRecordKey& iKey,
          }
       }
    }
-   if(!haveAValidDependentRecord) {
-     //If no Finder has no valid time, then this record is also invalid for this time
-     newInterval = ValidityInterval::invalidInterval();
+   if(intervalsWereComparible) {
+     if(!haveAValidDependentRecord) {
+       //If no Finder has no valid time, then this record is also invalid for this time
+       newInterval = ValidityInterval::invalidInterval();
+     }
+     oInterval = newInterval;
+     return;
    }
-   oInterval = newInterval;
+   //handle the case where some providers use time and others use run/lumi/event
+   // in this case all we can do is find an IOV which changed since last time
+   // and use its start time to do the synching and use an 'invalid' end time
+   // so the system always checks back to see if something has changed
+
+   for(Providers::iterator itProvider = providers_.begin(), itProviderEnd = providers_.end();
+       itProvider != itProviderEnd;
+       ++itProvider) {
+      if((*itProvider)->setValidityIntervalFor(iTime)) {
+         ValidityInterval providerInterval = (*itProvider)->validityInterval();
+	 if(!providerInterval.validFor(m_previousSyncTo)) {
+	   //NOTE if the above is never true than old interval should be fine
+	   providerInterval.setLast(IOVSyncValue::invalidIOVSyncValue());
+	   oInterval = providerInterval;
+	   break;
+         }
+      }
+   }
+   m_previousSyncTo = iTime;
 }
 
 //
