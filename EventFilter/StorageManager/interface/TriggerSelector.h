d9f37aa3fb59e9d9444882410a68014c21df1f79
Srecko Morovic <sha1-6e7a2f6c841eef3f837cbf689427b24074fc8e72@cern.ch>
2009-12-01 16:18:18 +0000

    indenting

--- EventFilter/StorageManager/interface/TriggerSelector.h
+++ EventFilter/StorageManager/interface/TriggerSelector.h
@@ -1,4 +1,4 @@
-// $Id: Configuration.h,v 1.11 2009/11/09 15:40:55 mommsen Exp $
+// $Id: TriggerSelector.h,v 1.1 2009/12/01 13:58:08 mommsen Exp $
 /// @file: TriggerSelector.h 
 
 #ifndef EventFilter_StorageManager_TriggerSelector_h
@@ -22,88 +22,88 @@ namespace stor
    * Event selector allowing for and/not combination of triggers/paths
    *
    * $Author: mommsen $
-   * $Revision: 1.11 $
-   * $Date: 2009/11/09 15:40:55 $
+   * $Revision: 1.1 $
+   * $Date: 2009/12/01 13:58:08 $
    */
 
-	class TriggerSelector
-	{
-		public:
+  class TriggerSelector
+  {
+    public:
 
-			typedef std::vector<std::string> Strings;
+      typedef std::vector<std::string> Strings;
 
-			//old mode only
-			TriggerSelector(Strings const& pathspecs,
-					Strings const& names);
+      //old mode only
+      TriggerSelector(Strings const& pathspecs,
+	  Strings const& names);
 
-			TriggerSelector(edm::ParameterSet const& pset,
-					Strings const& triggernames, bool old_ = false);
+      TriggerSelector(edm::ParameterSet const& pset,
+	  Strings const& triggernames, bool old_ = false);
 
-			TriggerSelector(std::string const& expression, Strings const& triggernames);
+      TriggerSelector(std::string const& expression, Strings const& triggernames);
 
-			~TriggerSelector() {};
+      ~TriggerSelector() {};
 
-			bool wantAll() const { return accept_all_; }
-			bool acceptEvent(edm::TriggerResults const&) const;
-			bool acceptEvent(unsigned char const*, int) const;
+      bool wantAll() const { return accept_all_; }
+      bool acceptEvent(edm::TriggerResults const&) const;
+      bool acceptEvent(unsigned char const*, int) const;
 
-			//for testing purposes
-			bool returnStatus(edm::HLTGlobalStatus const& trStatus) const {
-				return masterElement_->returnStatus(trStatus);
-			}
+      //for testing purposes
+      bool returnStatus(edm::HLTGlobalStatus const& trStatus) const {
+	return masterElement_->returnStatus(trStatus);
+      }
 
-			static  std::string makeXMLString(std::string const& input);
+      static  std::string makeXMLString(std::string const& input);
 
-			static std::vector<std::string>
-				getEventSelectionVString(edm::ParameterSet const& pset);
+      static std::vector<std::string>
+	getEventSelectionVString(edm::ParameterSet const& pset);
 
-		private:
+    private:
 
-			bool accept_all_;
+      bool accept_all_;
 
-			void init(std::string const& path, Strings const& triggernames);
+      void init(std::string const& path, Strings const& triggernames);
 
-			static std::string trim(std::string input);
+      static std::string trim(std::string input);
 
-			class TreeElement {
+      class TreeElement {
 
-				enum TreeOperator {
-					NonInit = 0,
-					AND = 1,
-					OR  = 2,
-					NOT = 3,
-					BR = 4
-				};
-
-				public:
-
-				TreeElement(std::string const& inputString,Strings const& tr,TreeElement* parentElement = NULL);
-				~TreeElement();
-
-				bool returnStatus(edm::HLTGlobalStatus const& trStatus) const;
-
-				TreeOperator op() const {return op_;}
-				TreeElement * parent() const {return parent_;}
-
-				private:
-
-				TreeElement * parent_;
-				std::vector<TreeElement*> children_;
-				TreeOperator op_;
-				int trigBit_;
-			};
-
-			boost::shared_ptr<TreeElement> masterElement_;
-
-			//keep a copy of initialization string
-			std::string expression_;
-
-			boost::shared_ptr<edm::EventSelector> eventSelector_;
-			bool useOld_;
-
-			static const bool debug_ = false;
-
+	enum TreeOperator {
+	  NonInit = 0,
+	  AND = 1,
+	  OR  = 2,
+	  NOT = 3,
+	  BR = 4
 	};
+
+	public:
+
+	TreeElement(std::string const& inputString,Strings const& tr,TreeElement* parentElement = NULL);
+	~TreeElement();
+
+	bool returnStatus(edm::HLTGlobalStatus const& trStatus) const;
+
+	TreeOperator op() const {return op_;}
+	TreeElement * parent() const {return parent_;}
+
+	private:
+
+	TreeElement * parent_;
+	std::vector<TreeElement*> children_;
+	TreeOperator op_;
+	int trigBit_;
+      };
+
+      boost::shared_ptr<TreeElement> masterElement_;
+
+      //keep a copy of initialization string
+      std::string expression_;
+
+      boost::shared_ptr<edm::EventSelector> eventSelector_;
+      bool useOld_;
+
+      static const bool debug_ = false;
+
+  };
 }
 
 #endif // EventFilter_StorageManager_TriggerSelector_h
