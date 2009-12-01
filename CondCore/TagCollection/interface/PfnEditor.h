9b987428b37cd8eb99670565fc613527d02769c3
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-12-01 13:42:37 +0000

    pfn editor

---
+++ CondCore/TagCollection/interface/PfnEditor.h
@@ -0,0 +1,38 @@
+#ifndef CondCore_TagCollection_PfnEditor
+#define CondCore_TagCollection_PfnEditor
+#endif // CondCore_TagCollection_PfnEditor
+
+
+
+#include <string>
+
+namespace cond{
+
+  // edit the pfn accordind to rules given in its constructor
+  class PfnEditor {
+  public:
+    PfnEditor(std::string const & ipre, 
+	      std::string const & ipos) : 
+      prefix(ipre), 
+      postfix(ipos),
+      off(prefix.empty() && postfix.empty())
+    {}
+    
+
+    std::string operator(std::string const & pfn) {
+      if (off) return pfn;
+      size_t pos=std::string::npos;
+      if (!prefix.empty()) pos = pfn.rfind('/');
+      return ( (pos == std::string::npos) ? (prefix+pfn) :
+	       pfn.replace(0,pos,prefix)
+	       ) + postfix;
+    }
+
+  private:
+    std::string prefix;
+    std::string postfix;
+    bool off;
+  };
+
+
+}
