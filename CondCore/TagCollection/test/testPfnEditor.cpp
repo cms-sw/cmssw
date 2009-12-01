fdfaa09f8209457c0965b2d65bd0cdd317c66ab3
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-12-01 13:45:23 +0000

    pfn editor

--- CondCore/TagCollection/test/testPfnEditor.cpp
+++ CondCore/TagCollection/test/testPfnEditor.cpp
@@ -10,41 +10,41 @@ void error(char * m) {
 }
 
 int main() {
-
+  
   std::string det("STRIP");
-
+  
   std::string pfn("oracle://cmsarc_lb/CMS_COND_31X_STRIP");
   std::string pre1("oracle://cmsarc_lb/");
   std::string pre2("oracle://cmsprod/");
-
+  
   std::post("_0911");
-
+  
   {
-    PfnEditor ed("","");
+    cond::PfnEditor ed("","");
     if (ed.pfn(pfn)!=pfn) 
       error("error adding null pre and null post");
   }
 
   {
-    PfnEditor ed("",post);
+    cond::PfnEditor ed("",post);
     if (ed.pfn(pfn)!=pfn+post) 
       error("error adding null pre and post");
   }
 
   {
-    PfnEditor ed(pre2,"");
+    cond::PfnEditor ed(pre2,"");
     if (ed.pfn(pfn)!=pre2+de) 
       error("error changin pre and null post");
   }
 
   {
-    PfnEditor ed(pre2,post);
+    cond::PfnEditor ed(pre2,post);
     if (ed.pfn(pfn)!=pre2+det+post) 
       error("error changing pre and post");
   }
 
   {
-    PfnEditor ed(pre1,post);
+    cond::PfnEditor ed(pre1,post);
     if (ed.pfn(det)!=pre1+det+post) 
       error("error adding pre and post");
   }
