25885147f6316e200bbd232a46d43bd6efbe6bde
Srecko Morovic <sha1-6e7a2f6c841eef3f837cbf689427b24074fc8e72@cern.ch>
2009-12-01 21:04:07 +0000

    added doxygen documentation

--- EventFilter/StorageManager/interface/TriggerSelector.h
+++ EventFilter/StorageManager/interface/TriggerSelector.h
@@ -1,4 +1,4 @@
-// $Id: TriggerSelector.h,v 1.2 2009/12/01 16:18:18 smorovic Exp $
+// $Id: TriggerSelector.h,v 1.3 2009/12/01 17:56:15 smorovic Exp $
 /// @file: TriggerSelector.h 
 
 #ifndef EventFilter_StorageManager_TriggerSelector_h
@@ -22,8 +22,8 @@ namespace stor
    * Event selector allowing for and/not combination of triggers/paths
    *
    * $Author: smorovic $
-   * $Revision: 1.2 $
-   * $Date: 2009/12/01 16:18:18 $
+   * $Revision: 1.3 $
+   * $Date: 2009/12/01 17:56:15 $
    */
 
   class TriggerSelector
@@ -32,28 +32,57 @@ namespace stor
 
       typedef std::vector<std::string> Strings;
 
-      //old mode only
+      /**
+       * Obsolete: Initializes TriggerSelector to use edm::EventSelector for selection.
+       */
       TriggerSelector(Strings const& pathspecs,
 	  Strings const& names);
 
+      /**
+       * Takes ParameterSet wth TriggerSelector string or EventSelection list, and a list of triggers.
+       * if old_ is true, it is forced to use EventSelection.
+       */
       TriggerSelector(edm::ParameterSet const& pset,
 	  Strings const& triggernames, bool old_ = false);
 
+      /**
+       * Takes selection string and list of triggers
+       */
       TriggerSelector(std::string const& expression, Strings const& triggernames);
 
       ~TriggerSelector() {};
 
+      /**
+       * Returns status of always positive bit
+       */
       bool wantAll() const { return accept_all_; }
+
+      /**
+       * Evaluates if trigger results pass selection
+       */
       bool acceptEvent(edm::TriggerResults const&) const;
+
+      /*
+       * Takes array of trigger results and a number of triggers in array and returns
+       * if it passes selection
+       */
       bool acceptEvent(unsigned char const*, int) const;
 
-      //for testing purposes
+      /*
+       * Returns if HLTGlobalStatus passes selection
+       */
       bool returnStatus(edm::HLTGlobalStatus const& trStatus) const {
 	return masterElement_->returnStatus(trStatus);
       }
 
+      /*
+       * Does XMl compatible formatting of the selection string
+       */
       static std::string makeXMLString(std::string const& input);
 
+      /*
+       * Obsolete: Returns SelectedEvents vector from ParameterSet
+       */
       static std::vector<std::string>
 	getEventSelectionVString(edm::ParameterSet const& pset);
 
@@ -61,10 +90,19 @@ namespace stor
 
       bool accept_all_;
 
+      /*
+       * Starts parsing selection string
+       */
       void init(std::string const& path, Strings const& triggernames);
 
+      /* 
+       * Removes extra spaces from string
+       */
       static std::string trim(std::string input);
 
+      /*
+       * Class used for storing internal representation of the selection string
+       */
       class TreeElement {
 
 	enum TreeOperator {
@@ -77,12 +115,25 @@ namespace stor
 
 	public:
 
+	/*
+	 * Parser of selection string. Splits string into tokens and initializes new elements to parse them.
+	 */
 	TreeElement(std::string const& inputString,Strings const& tr,TreeElement* parentElement = NULL);
 	~TreeElement();
 
+	/*
+	 * Returns selection status of current element calculated recursively from it's child elements
+	 */
 	bool returnStatus(edm::HLTGlobalStatus const& trStatus) const;
 
+	/*
+	 * Returns operator type of the element
+	 */
 	TreeOperator op() const {return op_;}
+
+	/*
+	 * Returns parent element
+	 */
 	TreeElement * parent() const {return parent_;}
 
 	private:
