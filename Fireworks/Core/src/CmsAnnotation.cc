b37e4e616d5e742f26aad1aae966d5f50b63347c
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2009-12-01 20:54:02 +0000

    Add new overlay class for CMS info.

---
+++ Fireworks/Core/src/CmsAnnotation.cc
@@ -0,0 +1,223 @@
+#include "TGLIncludes.h"
+#include "TROOT.h"
+#include "TColor.h"
+#include "TGLUtil.h"
+#include "TGLCamera.h"
+#include "TGLRnrCtx.h"
+#include "TGLSelectRecord.h"
+#include "TGLViewerBase.h"
+#include "TObjString.h"
+#include "TGLViewer.h"
+#include "TMath.h"
+
+
+#include "TImage.h"
+#include <KeySymbols.h>
+
+#include "Fireworks/Core/interface/CmsAnnotation.h"
+
+CmsAnnotation::CmsAnnotation(TGLViewerBase *parent, Float_t posx, Float_t posy) :
+   TGLOverlayElement(TGLOverlayElement::kAnnotation),
+
+   fPosX(posx), fPosY(posy),
+   fMouseX(0),  fMouseY(0),
+   fDrag(kNone),
+   fParent(0),
+   fSize(0.15),
+   fActive(false)
+{
+   // Constructor.
+   // Create annotation as plain text
+
+   parent->AddOverlayElement(this);
+   fParent = (TGLViewer*)parent;
+}
+
+void CmsAnnotation::Render(TGLRnrCtx& rnrCtx)
+{
+   static UInt_t ttid = 0;
+
+   if (ttid == 0)
+   {
+      TImage* img = TImage::Open("$(CMSSW_BASE)/src/Fireworks/Core/icons/CMSlogo.png");
+      // !!!!! Check if 0 !!!!!
+
+      glGenTextures(1, &ttid);
+
+      glBindTexture(GL_TEXTURE_2D, ttid);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+      glPixelStorei(GL_UNPACK_ALIGNMENT,  1);
+      glPixelStorei(GL_UNPACK_SWAP_BYTES, 1);
+
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->GetWidth(), img->GetHeight(), 0,
+                   GL_BGRA, GL_UNSIGNED_BYTE, img->GetArgbArray());
+
+      glPixelStorei(GL_UNPACK_SWAP_BYTES, 0);
+
+      delete img;
+   }
+
+
+   glPushAttrib(GL_ENABLE_BIT | GL_LINE_BIT | GL_POLYGON_BIT );
+   TGLCapabilitySwitch lights_off(GL_LIGHTING, kFALSE);
+
+
+   // reset matrix
+   glMatrixMode(GL_PROJECTION);
+   glPushMatrix();
+   glLoadIdentity();
+
+   if (rnrCtx.Selection())
+   {
+      TGLRect rect(*rnrCtx.GetPickRectangle());
+      rnrCtx.GetCamera()->WindowToViewport(rect);
+      gluPickMatrix(rect.X(), rect.Y(), rect.Width(), rect.Height(),
+                    (Int_t*) rnrCtx.GetCamera()->RefViewport().CArr());
+   }
+   const TGLRect& vp = rnrCtx.RefCamera().RefViewport();
+   glOrtho(vp.X(), vp.Width(), vp.Y(), vp.Height(), 0, 1);
+   glMatrixMode(GL_MODELVIEW);
+   glPushMatrix();
+   glLoadIdentity();
+
+   // move to pos
+   Float_t posX = vp.Width()  * fPosX;
+   Float_t posY = vp.Height() * fPosY;
+   glTranslatef(posX, posY, -0.99);
+
+   glDisable(GL_DEPTH_TEST);
+   glEnable(GL_BLEND);
+   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+   glEnable(GL_TEXTURE_2D);
+   glBindTexture(GL_TEXTURE_2D, ttid);
+   glTexEnvi (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+   glPushName(kMove);
+   glColor4f(1,1,1,1);
+   glBegin(GL_QUADS);
+   Float_t z =  0.9;
+   Float_t a = fSize * vp.Width();
+   glTexCoord2f(0, 1); glVertex3f(0, 0, z);
+   glTexCoord2f(1, 1); glVertex3f(a, 0, z);
+   glTexCoord2f(1, 0); glVertex3f(a, a, z);
+   glTexCoord2f(0, 0); glVertex3f(0, a, z);   
+   glEnd();
+   glPopName();
+
+   glDisable(GL_TEXTURE_2D);
+
+   glPushName(kResize);
+   if (fActive)
+   {
+      glTranslatef(a, 0, 0);
+      a *= 0.1;
+      z = 0.95;
+      TGLUtil::ColorTransparency(kGreen, 60);
+      glBegin(GL_QUADS);
+      glVertex3f( 0, 0, z);
+      glVertex3f( 0, a, z);
+      glVertex3f(-a, a, z);
+      glVertex3f(-a, 0, z);
+      glEnd();
+   }
+   glPopName();
+
+   glEnable(GL_DEPTH_TEST);
+   glMatrixMode(GL_PROJECTION);
+   glPopMatrix();
+   glMatrixMode(GL_MODELVIEW);
+   glPopMatrix();
+
+   glPopAttrib();
+}
+
+//______________________________________________________________________
+Bool_t CmsAnnotation::Handle(TGLRnrCtx&          rnrCtx,
+                             TGLOvlSelectRecord& selRec,
+                             Event_t*            event)
+{
+   // Handle overlay event.
+   // Return TRUE if event was handled.
+
+   if (selRec.GetN() < 2) return kFALSE;
+   EDrag recID = selRec.GetItem(1) ? kResize :  kMove;
+   switch (event->fType)
+   {
+      case kButtonPress:
+      {
+         fMouseX = event->fX;
+         fMouseY = event->fY;
+         fDrag = recID;
+         return kTRUE;
+      }
+      case kButtonRelease:
+      {
+         fDrag = kNone;
+      }
+      case kMotionNotify:
+      {
+         const TGLRect& vp = rnrCtx.RefCamera().RefViewport();
+         if (vp.Width() == 0 || vp.Height() == 0) return false;
+         if (fDrag != kNone)
+         {
+            if (fDrag == kMove)
+            {
+               fPosX += (Float_t)(event->fX - fMouseX) / vp.Width();
+               fPosY -= (Float_t)(event->fY - fMouseY) / vp.Height();
+               fMouseX = event->fX;
+               fMouseY = event->fY;
+
+               Float_t w = fSize;
+               Float_t h = fSize*vp.Aspect();
+
+               // Make sure we don't go offscreen (use fDraw variables set in draw)
+               if (fPosX < 0)
+                  fPosX = 0;
+               else if (fPosX + w > 1.0f)
+                  fPosX = 1.0f - w;
+               if (fPosY < 0)
+                  fPosY = 0;
+               else if (fPosY > (1.0f - h))
+                  fPosY = 1.0f - h;
+            }
+            else
+            {
+               Float_t diff = (Float_t)(event->fX - fMouseX) / vp.Width();
+               fMouseX = event->fX;
+               fMouseY = event->fY;
+               Float_t size = fSize + diff;
+               fSize = TMath::Max(size, 0.01f);
+
+            }
+         }
+         return kTRUE;
+      }
+      default:
+      {
+         return kFALSE;
+      }
+   }
+}
+
+//______________________________________________________________________________
+Bool_t CmsAnnotation::MouseEnter(TGLOvlSelectRecord& /*rec*/)
+{
+   // Mouse has entered overlay area.
+
+   fActive = kTRUE;
+   return kTRUE;
+}
+
+//______________________________________________________________________
+void CmsAnnotation::MouseLeave()
+{
+   // Mouse has left overlay area.
+
+   fActive = kFALSE;
+}
