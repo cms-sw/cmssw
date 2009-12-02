f4b74b22dcb53a5b3a7e761eb8333c1ec877c3a8
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-12-02 18:22:39 +0000

    minor bug fix for pixelless tracking sequence

--- DQM/BeamMonitor/test/beam_dqm_sourceclient-live_cfg.py
+++ DQM/BeamMonitor/test/beam_dqm_sourceclient-live_cfg.py
@@ -72,7 +72,7 @@ process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
 #--------------------------
 process.tracking = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
 process.monitor = cms.Sequence(process.dqmBeamMonitor*process.dqmEnv)
-process.tracking_pixelless = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ctfTracksPixelLess)
+process.tracking_pixelless = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.ctfTracksPixelLess)
 process.monitor_pixelless = cms.Sequence(process.dqmBeamMonitor_pixelless*process.dqmEnvPixelLess)
 
 process.p = cms.Path(process.hltTriggerTypeFilter*process.tracking*process.monitor)
