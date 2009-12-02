ba0c038bb9d3d1d8c2ad291c494e62163f9fa620
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2009-12-02 19:55:05 +0000

    Make all exception default actions Rethrow

--- FWCore/Framework/src/Actions.cc
+++ FWCore/Framework/src/Actions.cc
@@ -11,40 +11,34 @@
 namespace edm {
   namespace actions {
     namespace {
-      struct ActionNames
-      {
-	ActionNames():table_(LastCode + 1)
-	{
-	  table_[IgnoreCompletely]="IgnoreCompletely";
-	  table_[Rethrow]="Rethrow";
-	  table_[SkipEvent]="SkipEvent";
-	  table_[FailModule]="FailModule";
-	  table_[FailPath]="FailPath";
+      struct ActionNames {
+	ActionNames():table_(LastCode + 1) {
+	  table_[IgnoreCompletely] = "IgnoreCompletely";
+	  table_[Rethrow] = "Rethrow";
+	  table_[SkipEvent] = "SkipEvent";
+	  table_[FailModule] = "FailModule";
+	  table_[FailPath] = "FailPath";
 	}
 
-	typedef std::vector<const char*> Table;
+	typedef std::vector<char const*> Table;
 	Table table_;
       };      
     }
 
-    const char* actionName(ActionCodes code)
-    {
+    char const* actionName(ActionCodes code) {
       static ActionNames tab;
-      return static_cast<unsigned int>(code) < tab.table_.size() ? 
-	tab.table_[code] : "UnknownAction";
+      return static_cast<unsigned int>(code) < tab.table_.size() ?  tab.table_[code] : "UnknownAction";
     }
   }
 
-  ActionTable::ActionTable() : map_()
-  {
+  ActionTable::ActionTable() : map_() {
     addDefaults();
   }
 
   namespace {
     inline void install(actions::ActionCodes code,
 			ActionTable::ActionMap& out,
-			const ParameterSet& pset)
-    {
+			ParameterSet const& pset) {
       using boost::lambda::_1;
       using boost::lambda::var;
       typedef std::vector<std::string> vstring;
@@ -60,18 +54,15 @@ namespace edm {
 //	cerr << pset.toString() << std::endl;
 
       ParameterSet defopts;
-      ParameterSet opts = 
-	pset.getUntrackedParameter<ParameterSet>("options", defopts);
+      ParameterSet opts = pset.getUntrackedParameter<ParameterSet>("options", defopts);
       //cerr << "looking for " << actionName(code) << std::endl;
-      vstring v = 
-	opts.getUntrackedParameter(actionName(code),vstring());
+      vstring v = opts.getUntrackedParameter(actionName(code),vstring());
       for_all(v, var(out)[_1] = code);      
 
     }  
   }
 
-  ActionTable::ActionTable(const ParameterSet& pset) : map_()
-  {
+  ActionTable::ActionTable(ParameterSet const& pset) : map_() {
     addDefaults();
 
     install(actions::SkipEvent, map_, pset);
@@ -81,48 +72,30 @@ namespace edm {
     install(actions::FailPath, map_, pset);
   }
 
-  void ActionTable::addDefaults()
-  {
+  void ActionTable::addDefaults() {
     using namespace boost::lambda;
     // populate defaults that are not 'Rethrow'
+    // (There are none as of CMSSW_3_4_X.)
     // 'Rethrow' is the default default.
-    map_[edm::Exception::codeToString(errors::ProductNotFound)]=
-      actions::SkipEvent;
-    map_[edm::Exception::codeToString(errors::InvalidReference)]=
-      actions::SkipEvent;
-    map_[edm::Exception::codeToString(errors::NullPointerError)]=
-      actions::SkipEvent;
-    map_[edm::Exception::codeToString(errors::EventTimeout)]=
-      actions::SkipEvent;
-    map_[edm::Exception::codeToString(errors::EventCorruption)]=
-      actions::SkipEvent;
-    map_[edm::Exception::codeToString(errors::NotFound)]=
-      actions::SkipEvent;
-
-    if(2 <= debugit())
-      {
+    if(2 <= debugit()) {
 	ActionMap::const_iterator ib(map_.begin()),ie(map_.end());
-	for(;ib!=ie;++ib)
+	for(;ib != ie; ++ib) {
 	  std::cerr << ib->first << ',' << ib->second << '\n';
+	}
 	std::cerr << std::endl;
-      }
-
+    }
   }
 
-  ActionTable::~ActionTable()
-  {
+  ActionTable::~ActionTable() {
   }
 
-  void ActionTable::add(const std::string& category,
-			actions::ActionCodes code)
-  {
+  void ActionTable::add(std::string const& category, actions::ActionCodes code) {
     map_[category] = code;
   }
 
-  actions::ActionCodes ActionTable::find(const std::string& category) const
-  {
+  actions::ActionCodes ActionTable::find(std::string const& category) const {
     ActionMap::const_iterator i(map_.find(category));
-    return i!=map_.end() ? i->second : actions::Rethrow;
+    return i != map_.end() ? i->second : actions::Rethrow;
   }
 
 }
