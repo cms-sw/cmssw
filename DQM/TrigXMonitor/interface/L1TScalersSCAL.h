658dc330ec7d432307ba525df0f4be4a9469204b
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-12-03 09:41:08 +0000

    added plots

--- DQM/TrigXMonitor/interface/L1TScalersSCAL.h
+++ DQM/TrigXMonitor/interface/L1TScalersSCAL.h
@@ -59,6 +59,8 @@ private:
   std::vector<double> bufferAlgoRates_;
   std::vector<double> technicalRates_;
   std::vector<double> bufferTechRates_;                                 
+  std::vector<double> integral_algo_;                                 
+  std::vector<double> integral_tech_;                                 
   
   MonitorElement * orbitNum;
   MonitorElement * trigNum;
@@ -70,6 +72,8 @@ private:
   MonitorElement * lostFinalTriggers;
   MonitorElement * algoRate[128];
   MonitorElement * techRate[64];
+  MonitorElement * integralAlgo[128];
+  MonitorElement * integralTech[64];
 
 
   MonitorElement * physRate;
