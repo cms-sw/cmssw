0091dc46775948efe1d5dafc056782c943d5ee08
Maria Cepeda Hermida <sha1-4a727c1fdf5a92d567ee4b2317070045307b41b3@cern.ch>
2009-12-04 16:53:14 +0000

    Changed name to EwkWMuNuDQM to EwkMuDQM

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.Physics.bphysicsOniaDQM_cfi import *
 from DQM.Physics.ewkDQM_cfi import *
-from DQM.Physics.ewkWMuNuDQM_cfi import *
+from DQM.Physics.ewkMuDQM_cfi import *
 from DQM.Physics.qcdPhotonsDQM_cfi import *
 from DQM.Physics.qcdHighPtDQM_cfi import *
 from DQM.Physics.topDiLeptonDQM_cfi import *
@@ -10,7 +10,7 @@ from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
 
 dqmPhysics = cms.Sequence(bphysicsOniaDQM
 			 *ewkDQM
-                         *ewkWMuNuDQM
+                         *ewkMuDQM
 			 *qcdPhotonsDQM
                          *qcdHighPtDQM
 			 *topDiLeptonDQM
