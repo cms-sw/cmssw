4c625c968d05d14e982069a5e7ac3657d69ba82c
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-12-04 17:32:06 +0000

    added gain shallow tree

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from CalibTracker.Configuration.Filter_Refit_cff import *
 from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
+from CalibTracker.SiStripChannelGain.ntuple_cff import *
 shallowTrackClusters.Tracks = "CalibrationTracksRefit"
 shallowTrackClusters.Clusters = 'CalibrationTracks'
 shallowClusters.Clusters = 'CalibrationTracks'
@@ -9,5 +10,11 @@ shallowClusters.Clusters = 'CalibrationTracks'
 from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
 
 #Schedule
-filter_refit_ntuplize_step = cms.Path( trackFilterRefit + ntuple + hiteff )
+filter_refit_ntuplize_step = cms.Path( trackFilterRefit + ntuple + hiteff + OfflineGainNtuple)
 schedule = cms.Schedule( filter_refit_ntuplize_step )
+
+
+
+
+
+
