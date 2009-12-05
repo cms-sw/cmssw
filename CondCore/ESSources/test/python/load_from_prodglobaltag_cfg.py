74c2deb2e0b592d75e69c7b97c6b81d1a06f63db
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-12-05 10:51:06 +0000

    even fancier

--- CondCore/ESSources/test/python/load_from_prodglobaltag_cfg.py
+++ CondCore/ESSources/test/python/load_from_prodglobaltag_cfg.py
@@ -1,16 +1,48 @@
+import DLFCN, sys, os, time
+sys.setdlopenflags(DLFCN.RTLD_GLOBAL+DLFCN.RTLD_LAZY)
+
+from pluginCondDBPyInterface import *
+a = FWIncantation()
+rdbms = RDBMS("/afs/cern.ch/cms/DB/conddb")
+logName = "oracle://cms_orcoff_prod/CMS_COND_31X_POPCONLOG"
+gdbName = "oracle://cms_orcoff_prod/CMS_COND_31X_GLOBALTAG"
+gName = 'CRAFT09_R_V9::All'
+globalTag = rdbms.globalTag(gdbName,gName,"","")
+
+
 import FWCore.ParameterSet.Config as cms
+
+records = cms.VPSet()
+for tag in globalTag.elements:
+    records.append(
+      cms.PSet(
+        record = cms.string(tag.record),
+        data = cms.vstring(tag.object)
+        )
+      )
+
 
 process = cms.Process("TEST")
 
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'CRAFT09_R_V9::All'
-#'GR09_P_V6::All'
+process.GlobalTag.globaltag = gname
+process.GlobalTag.DumpStat =  True
+# 'GR09_P_V6::All'
+#'CRAFT09_R_V9::All'
 #'MC_31X_V9::All'
 #'GR09_31X_V5P::All'
-process.GlobalTag.DumpStat =  True
-process.GlobalTag.pfnPrefix = "frontier://FrontierArc/"
-process.GlobalTag.pfnPostfix = "_0911"
+#process.GlobalTag.pfnPrefix = "frontier://FrontierArc/"
+#process.GlobalTag.pfnPostfix = "_0911"
+#process.GlobalTag.toGet = cms.VPSet()
+#process.GlobalTag.toGet.append(
+#   cms.PSet(record = cms.string("BeamSpotObjectsRcd"),
+#            tag = cms.string("firstcollisions"),
+#             connect = cms.untracked.string("frontier://PromptProd/CMS_COND_31X_BEAMSPOT")
+#           )
+#)
+
+
 
 process.source = cms.Source("EmptyIOVSource",
     lastValue = cms.uint64(3),
@@ -21,12 +53,7 @@ process.source = cms.Source("EmptyIOVSource",
 
 
 process.get = cms.EDFilter("EventSetupRecordDataGetter",
-                           toGet = cms.VPSet(
-    cms.PSet(
-    record = cms.string('RunInfoRcd'),
-    data = cms.vstring('RunInfo')
-    )
-    ),
+                           toGet = records,
                            verbose = cms.untracked.bool(True)
                            )
 
