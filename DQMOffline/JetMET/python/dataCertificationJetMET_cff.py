6e9d89b46b1976006af6b7499e17247b6e249e4b
Kenichi Hatakeyama <sha1-69d5d1a598c1a8bbb35d497fd6c82ae9f60f5668@cern.ch>
2009-12-06 11:31:47 +0000

    Put certification step back

--- DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
+++ DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.JetMET.jetMETDQMOfflineClient_cfi import *
 from DQMOffline.JetMET.dataCertificationJetMET_cfi import *
 
-#dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET + dataCertificationJetMET)
-dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET )
+dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET + dataCertificationJetMET)
+#dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET )
