bf3665e838c97c9e4c72fe83a2c92f2cc91f877e
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-12-06 11:45:19 +0000

    commented rpc

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -1,21 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
+# electron
+from DQM.HLTEvF.HLTMonElectron_cff import *
+# tau
+from DQM.HLTEvF.HLTMonTau_cff import *
 # four vector
 from DQM.HLTEvF.FourVectorHLTOnline_cff import *
-## electron
-#from DQM.HLTEvF.HLTMonElectron_cff import *
-## tau
-#from DQM.HLTEvF.HLTMonTau_cff import *
-## jet met
-#from DQM.HLTEvF.HLTMonJetMETDQMSource_cff import * 
-## muon
-#from DQM.HLTEvF.HLTMonMuonDQM_cff import *
-#from DQM.HLTEvF.HLTLocalRecoDT_cff import *
+# jet met
+from DQM.HLTEvF.HLTMonJetMETDQMSource_cff import * 
+# muon
+from DQM.HLTEvF.HLTMonMuonDQM_cff import *
+from DQM.HLTEvF.HLTLocalRecoDT_cff import *
 #from DQM.HLTEvF.HLTLocalRecoRPC_cff import *
-#from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
-#from DQM.HLTEvF.HLTLocalRecoSiPixel_cff import *
-## alca
-#from DQM.HLTEvF.HLTMonAlCaPi0_cff import *
-#from DQM.HLTEvF.HLTMonAlCaPhySim_cff import *
-## btag
-##from DQM.HLTEvF.hltMonBTag_cff import *
+from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
+from DQM.HLTEvF.HLTLocalRecoSiPixel_cff import *
+# alca
+from DQM.HLTEvF.HLTMonAlCaPi0_cff import *
+from DQM.HLTEvF.HLTMonAlCaPhySim_cff import *
+# btag
+#from DQM.HLTEvF.hltMonBTag_cff import *
