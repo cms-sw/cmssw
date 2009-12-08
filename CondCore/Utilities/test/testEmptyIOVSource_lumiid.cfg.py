a110c376fb6accf9954f201b5855d16a0aa3c323
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-12-08 09:48:02 +0000

    more realisitc values

--- CondCore/Utilities/test/testEmptyIOVSource_lumiid.cfg.py
+++ CondCore/Utilities/test/testEmptyIOVSource_lumiid.cfg.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TEST")
 process.source = cms.Source("EmptyIOVSource",
     timetype = cms.string('lumiid'),
-    firstValue = cms.uint64(4294967298),
-    lastValue = cms.uint64(4294967302),
+    firstValue = cms.uint64((1211<<32)+1),
+    lastValue = cms.uint64((1211<<32)+8),
     interval = cms.uint64(2)
 )
 
