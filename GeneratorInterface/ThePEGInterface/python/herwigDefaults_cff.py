b5fa4953aac1e69477093ff0b830567e319e8ca1
Fred Stober <sha1-db50f1308e5907a2b60f5ac0211e423e19429ef0@cern.ch>
2009-12-08 10:29:42 +0000

    Fix for 0 decays

--- GeneratorInterface/ThePEGInterface/python/herwigDefaults_cff.py
+++ GeneratorInterface/ThePEGInterface/python/herwigDefaults_cff.py
@@ -27,8 +27,8 @@ herwigDefaultsBlock = cms.PSet(
 		'cd /Herwig/Particles',
 		'set p+:PDF /cmsPDFSet',
 		'set pbar-:PDF /cmsPDFSet',
-		'set K0:DefaultWidth 1e300*GeV',
-		'set Kbar0:DefaultWidth 1e300*GeV',
+		'set K0:Width 1e300*GeV',
+		'set Kbar0:Width 1e300*GeV',
 		'cd /',
 	),
 
