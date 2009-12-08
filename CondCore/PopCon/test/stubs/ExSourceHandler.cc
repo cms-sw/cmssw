90de88918b703cecaa8482a3484f4e59a7ffe863
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2009-12-08 11:08:09 +0000

    make python happy

--- CondCore/PopCon/test/stubs/ExSourceHandler.cc
+++ CondCore/PopCon/test/stubs/ExSourceHandler.cc
@@ -37,40 +37,40 @@ popcon::ExPedestalSource::~ExPedestalSource()
 }
 
 void popcon::ExPedestalSource::getNewObjects() {
-   edm::LogInfo   ("ExPedestalsSource") << "------- " << m_name 
-	     << " - > getNewObjects\n" << 
-  //check whats already inside of database
-      "got offlineInfo"<<
-    tagInfo().name << ", size " << tagInfo().size 
+  edm::LogInfo ("ExPedestalsSource") 
+    << "------- " << m_name 
+    << " - > getNewObjects\n"
+    //check whats already inside of database
+    << "got offlineInfo"
+    << tagInfo().name << ", size " << tagInfo().size    
+    << ", last object valid since " 
+    << tagInfo().lastInterval.first << " token "   
+    << tagInfo().lastPayloadToken << std::endl;
 
-            << ", last object valid since " 
-	    << tagInfo().lastInterval.first << " token "   
-            << tagInfo().lastPayloadToken << std::endl;
-
-   edm::LogInfo ("ExPedestalsSource")<< " ------ last entry info regarding the payload (if existing): " <<logDBEntry().usertext<< 
-        "; last record with the correct tag (if existing) has been written in the db: " <<logDBEntry().destinationDB<< std::endl; 
-
+  edm::LogInfo ("ExPedestalsSource")
+    << " ------ last entry info regarding the payload (if existing): " <<logDBEntry().usertext 
+    << "; last record with the correct tag (if existing) has been written in the db: " 
+    <<logDBEntry().destinationDB<< std::endl; 
+  
   if (tagInfo().size>0) {
     Ref payload = lastPayload();
     edm::LogInfo   ("ExPedestalsSource")<<"size of last payload  "<< 
       payload->m_pedestals.size()<<std::endl;
+    
+  }
+
  
- }
-
-
- 
-
   std::cout<<"first since = "<< m_since <<std::endl;
   
   
   Pedestals * p0 = new Pedestals;
   fill(*p0,3);
-  m_to_transfer.push_back(std::make_pair((Pedestals*)p0,m_since));
+  m_to_transfer.push_back(std::make_pair((Pedestals*)p0, m_since));
   
-  unsigned long long since = (unsigned long long)(m_since+m_increment*m_number);
-
+  unsigned long long since = (unsigned long long)(m_since+m_increment*(m_number-1));
+  
   std::cout<<"last since = "<< since <<std::endl;
-
+  
   unsigned long long size = 5;
   for (unsigned long long j=1; j<(unsigned long long)m_number;++j) {
     Pedestals * p1 = new Pedestals;
