a0e8c29f2aff13112892dae197c92917ad3edff8
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-12-09 15:52:16 +0000

    Specify input collections for hiteff and gain, (order oops)

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from CalibTracker.Configuration.Filter_Refit_cff import *
 from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
 from CalibTracker.SiStripChannelGain.ntuple_cff import *
+from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
 
 shallowTrackClusters.Tracks = "CalibrationTracksRefit"
 shallowTrackClusters.Clusters = 'CalibrationTracks'
@@ -10,8 +11,6 @@ shallowClusters.Clusters = 'CalibrationTracks'
 shallowGainCalibration.Tracks = "CalibrationTracksRefit"
 anEff.combinatorialTracks = "CalibrationTracksRefit"
 anEff.trajectories = "CalibrationTracksRefit"
-
-from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
 
 #Schedule
 filter_refit_ntuplize_step = cms.Path( trackFilterRefit + LorentzAngleNtuple + hiteff + OfflineGainNtuple)
