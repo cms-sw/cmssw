0f5389a260576a3b2e30aa565bf573ad0c78c1bd
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-12-09 15:50:44 +0000

    Specify input collections for hiteff and gain

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -3,9 +3,13 @@ import FWCore.ParameterSet.Config as cms
 from CalibTracker.Configuration.Filter_Refit_cff import *
 from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
 from CalibTracker.SiStripChannelGain.ntuple_cff import *
+
 shallowTrackClusters.Tracks = "CalibrationTracksRefit"
 shallowTrackClusters.Clusters = 'CalibrationTracks'
 shallowClusters.Clusters = 'CalibrationTracks'
+shallowGainCalibration.Tracks = "CalibrationTracksRefit"
+anEff.combinatorialTracks = "CalibrationTracksRefit"
+anEff.trajectories = "CalibrationTracksRefit"
 
 from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
 
