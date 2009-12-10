632718be50ac03bc15b31e840ab08c768eac2159
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2009-12-10 13:26:36 +0000

    add leading track prediscriminants

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationLowPt_cff.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationLowPt_cff.py
@@ -1,40 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoTauTag.RecoTau.PFRecoTauQualityCuts_cfi import *
-from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
-
-# Apply a lead track finding discriminant - only checks for a signal track >
-# 0.5 GeV in the signal cone.  Required for a consistent definition of signal
-# and isolation cone geometries.
-
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByLeadingTrackFinding_cfi \
-        import pfRecoTauDiscriminationByLeadingTrackFinding
-
-lowptpfTauDiscrByLeadTrackFinding = pfRecoTauDiscriminationByLeadingTrackFinding.clone()
-lowptpfTauDiscrByLeadTrackFinding.PFTauProducer = cms.InputTag('pfLayer0Taus')
-
-# Set up lead track finding as a common prediscriminant
-findLeadTrack = cms.PSet(
-    BooleanOperator = cms.string("and"),
-    leadTrack = cms.PSet(
-        Producer = cms.InputTag("lowptpfTauDiscrByLeadTrackFinding"),
-        cut = cms.double(0.5)
-    )
-)
-
-from RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolation_cfi  import pfRecoTauDiscriminationByTrackIsolation
+from RecoTauTag.RecoTau.TauDiscriminatorTools import requireLeadTrack
+from  RecoTauTag.RecoTau.PFRecoTauDiscriminationByTrackIsolation_cfi  import pfRecoTauDiscriminationByTrackIsolation
 lowptpfTauDiscrByTrackIsolation = pfRecoTauDiscriminationByTrackIsolation.clone()
 lowptpfTauDiscrByTrackIsolation.PFTauProducer  = cms.InputTag('pfLayer0Taus')
-lowptpfTauDiscrByTrackIsolation.Prediscriminants = findLeadTrack
+lowptpfTauDiscrByTrackIsolation.Prediscriminants.leadTrack.Producer = cms.InputTag('fixedConePFTauDiscriminationByLeadingTrackFinding')
 lowptpfTauDiscrByTrackIsolation.ApplySumPtCut = cms.bool(True)
 lowptpfTauDiscrByTrackIsolation.ApplyRelativeSumPtCut = cms.bool(False)
 lowptpfTauDiscrByTrackIsolation.maximumSumPtCut                       = cms.double(1.0)
 lowptpfTauDiscrByTrackIsolation.applyOccupancyCut                     = cms.bool(False)
 lowptpfTauDiscrByTrackIsolation.qualityCuts.isolationQualityCuts.minTrackPt=cms.double(0.0)
-
+ 
 lowptpfTauDiscrByRelTrackIsolation = pfRecoTauDiscriminationByTrackIsolation.clone() 
 lowptpfTauDiscrByRelTrackIsolation.PFTauProducer  = cms.InputTag('pfLayer0Taus')
-lowptpfTauDiscrByRelTrackIsolation.Prediscriminants = findLeadTrack
+lowptpfTauDiscrByRelTrackIsolation.Prediscriminants.leadTrack.Producer = cms.InputTag('fixedConePFTauDiscriminationByLeadingTrackFinding')
 lowptpfTauDiscrByRelTrackIsolation.ApplySumPtCut = cms.bool(False)
 lowptpfTauDiscrByRelTrackIsolation.ApplyRelativeSumPtCut = cms.bool(True)
 lowptpfTauDiscrByRelTrackIsolation.maximumSumPtCut                       = cms.double(0.05)
@@ -45,6 +25,8 @@ from RecoTauTag.RecoTau.PFRecoTauDiscriminationByCharge_cfi import pfRecoTauDisc
 pfRecoTauByCharge = pfRecoTauDiscriminationByCharge.clone()
 pfRecoTauByCharge.PFTauProducer  = cms.InputTag('pfLayer0Taus')
 pfRecoTauByCharge.ApplyOneOrThreeProngCut                    = cms.bool(True)
+
+
 
 DiscrLowPtTau = cms.EDFilter(
     "PFTauDiscriminatorLogicalAndProducer",
@@ -68,7 +50,6 @@ DiscrLowPtTau = cms.EDFilter(
 
 TauDiscrForLowPt = cms.Sequence(
     pfRecoTauByCharge+
-    lowptpfTauDiscrByLeadTrackFinding+
     lowptpfTauDiscrByRelTrackIsolation+
     lowptpfTauDiscrByTrackIsolation+
     DiscrLowPtTau
