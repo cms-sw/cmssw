979247eac9fa8f4c61fc9d0fc560ff4fb1ddfbd5
alinn <sha1-1ccfb7a1878c5f91cc562c39cc64f98a5d05b691@cern.ch>
2009-12-10 10:12:49 +0000

    New config parameters allow individual use of FED,FEC,Analysis O2O

--- OnlineDB/SiStripO2O/python/SiStripO2OCalibrationFactors_cfi.py
+++ OnlineDB/SiStripO2O/python/SiStripO2OCalibrationFactors_cfi.py
@@ -7,9 +7,19 @@ SiStripO2OCalibrationFactors = cms.PSet(
     DefaultThresholdLow=cms.untracked.double(2),
     DefaultThresholdHigh=cms.untracked.double(5),
     DefaultTickHeight=cms.untracked.double(690),
+    DefaultAPVLatency=cms.untracked.uint32(142),
+    DefaultAPVMode=cms.untracked.uint32(37),
 
     # Normalization Factor needed to convert Tick Height to Gain
-    GainNormalizationFactor=cms.untracked.double(690)
+    GainNormalizationFactor=cms.untracked.double(690),
+
+    # Enable/Disable O2O of the following Objects
+    UseAnalysis=cms.untracked.bool(False),
+    UseFED=cms.untracked.bool(False),
+    UseFEC=cms.untracked.bool(False),
+
+    # Print additional debug information
+    DebugMode=cms.untracked.bool(True)
     
     )
 
