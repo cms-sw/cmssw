7ee970608eaa21d7a3338a64aca2f5549f0ce1b7
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-12-11 05:51:10 +0000

    Use Salavats plots

--- SimCalorimetry/HcalSimProducers/test/hcalTest_cfg.py
+++ SimCalorimetry/HcalSimProducers/test/hcalTest_cfg.py
@@ -1,11 +1,16 @@
+# New Mixer test #1 - based on:
+#
 # Auto generated configuration file
 # using: 
-# Revision: 1.108 
+# Revision: 1.125 
 # Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: MinBias.cfi --step GEN,SIM --eventcontent FEVTSIM
+# with command line options: SinglePiPt100.cfi -s GEN:ProductionFilterSequence,SIM,DIGI,DATAMIX,L1,DIGI2RAW,HLT -n 10 --conditions FrontierConditions_GlobalTag,STARTUP31X_V1::All --eventcontent FEVTDEBUG --no_exec
+#
+# Hacked to accept exising MC files as input
+#
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('SIM')
+process = cms.Process('MIXTEST3')
 
 # import of standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
@@ -16,52 +21,66 @@ process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.load('Configuration/StandardSequences/Generator_cff')
 process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
 process.load('Configuration/StandardSequences/Sim_cff')
-process.load('RecoLocalCalo/Configuration//hcalLocalReco_cff')
-process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/Digi_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(50)
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.125 $'),
+    annotation = cms.untracked.string('SinglePiPt100.cfi nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
 )
 
 
-readFiles = cms.untracked.vstring()
-secFiles = cms.untracked.vstring() 
-readFiles.extend( [
-#       '/store/relval/CMSSW_3_1_0_pre9/RelValSingleMuPt100/GEN-SIM-RECO/IDEAL_31X_v1/0007/2CAA01DE-514F-DE11-9807-0030487A1FEC.root',
-#       '/store/relval/CMSSW_3_1_0_pre9/RelValSingleMuPt100/GEN-SIM-RECO/IDEAL_31X_v1/0006/0A13DB82-884E-DE11-8562-000423D6A6F4.root' ] );
-#'/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-RECO/IDEAL_31X_v1/0000/FA7A805B-584E-DE11-A682-0018F3D096E6.root',
-#       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-RECO/IDEAL_31X_v1/0000/7E5EBE7B-5F4F-DE11-A2A3-003048678B84.root' ] );
-       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/DABD367E-574E-DE11-9B60-001A92971B26.root',
-       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/6A5D4F75-594E-DE11-93E2-0018F3D095EC.root',
-       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/5224C821-584E-DE11-AC6C-0018F3D096D4.root',
-       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/50FAB27C-574E-DE11-985A-0018F3D096C8.root',
-       '/store/relval/CMSSW_3_1_0_pre9/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG/IDEAL_31X_v1/0000/500B2875-5F4F-DE11-80AB-003048678A72.root' ] );
-
-process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+# Input source
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5000)
+)
+process.source = cms.Source( 
+    "PoolSource"
+,fileNames = cms.untracked.vstring('/store/relval/CMSSW_3_4_0_pre4/RelValQCD_Pt_3000_3500/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_3XY_V12-v1/0001/D0B6FD82-12C9-DE11-9245-00304879FBB2.root')
+)
 
 
-process.load('SimCalorimetry.Configuration.hcalDigiSequence_cff')
-process.simHcalUnsuppressedDigis.doNoise = False
-process.simHcalUnsuppressedDigis.doEmpty = False
 
-process.hcalRecHitDump = cms.EDAnalyzer("HcalRecHitDump")
-process.hcalHitAnalyzer =  cms.EDAnalyzer("HcalHitAnalyzer", process.hcalSimBlock)
+# Additional output definition
+
 # Other statements
-process.GlobalTag.globaltag = 'STARTUP_31X::All'
-process.hbhereco.digiLabel = 'simHcalDigis'
-process.horeco.digiLabel = 'simHcalDigis'
-process.hfreco.digiLabel = 'simHcalDigis'
-process.zdcreco.digiLabel = 'simHcalUnsuppressedDigis'
+process.GlobalTag.globaltag = 'MC_3XY_V12::All'
+process.simHcalUnsuppressedDigis.ho.siPMCode = 0
+
+#process.load("CalibCalorimetry/HcalPlugins/Hcal_FrontierConditions_cff")
+##process.es_pool.toGet[2].tag = 'HcalGains_v4.04_mc'
+#from CondCore.DBCommon.CondDBSetup_cfi import *
+
+#process.es_pool = cms.ESSource("PoolDBESSource",
+#    CondDBSetup,
+#   connect = cms.string('frontier://FrontierProd/CMS_COND_31X_HCAL'), ##FrontierDev/CMS_COND_HCAL"
+#    toGet = cms.VPSet(
+#        cms.PSet(
+#           record = cms.string('HcalGainsRcd'),
+#           tag = cms.string('HcalGains_v4.04_mc')
+#       )
+#   )
+#)
+#process.es_prefer_es_pool = cms.ESPrefer("PoolDBESSource","es_pool")
 
 
-# Path and EndPath definitions
-process.eca = cms.EDAnalyzer("EventContentAnalyzer")
-process.prod = cms.EDFilter("SimHitCaloHitDumper")
+#process.generation_step = cms.Path(process.pgen)
+#process.simulation_step = cms.Path(process.psim)
+process.dump = cms.EDAnalyzer("HcalDigiDump")
+#process.load("RecoLocalCalo/HcalRecProducers/HcalHitReconstructor_ho_cfi")
+process.load("RecoLocalCalo/Configuration/hcalLocalReco_cff")
+process.horeco.digiLabel = 'simHcalUnsuppressedDigis'
+process.hbhereco.digiLabel = 'simHcalUnsuppressedDigis'
+process.hfreco.digiLabel = 'simHcalUnsuppressedDigis'
 
-#process.psim to run geant4
-process.simulation_step = cms.Path(process.mix*process.hcalLocalRecoSequence*process.hcalHitAnalyzer)
-
-# Schedule definition
-#process.schedule = cms.Schedule(process.generation_step,process.simulation_step)
-process.schedule = cms.Schedule(process.simulation_step)
+process.eventContent = cms.EDAnalyzer("EventContentAnalyzer")
+process.load("Validation/HcalRecHits/HcalRecHitParam_cfi")
+process.hcalRecoAnalyzer.eventype = 'multi'
+process.hcalRecoAnalyzer.outputFile = 'ho.root'
+process.hcalRecoAnalyzer.hcalselector = 'HO'
+process.hcalRecoAnalyzer.ecalselector = 'no'
+process.path = cms.Path(process.mix+process.simHcalUnsuppressedDigis+process.horeco+process.hcalRecoAnalyzer)
