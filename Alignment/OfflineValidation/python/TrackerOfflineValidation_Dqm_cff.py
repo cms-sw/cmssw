78398bca7e51ffbddf657fe60edb375d692ea1f6
Johannes Hauk <sha1-79adb14f6701f120a4bd8908168f6f2ed5256e2c@cern.ch>
2009-12-11 19:02:19 +0000

    Sequence added

--- Alignment/OfflineValidation/python/TrackerOfflineValidation_Dqm_cff.py
+++ Alignment/OfflineValidation/python/TrackerOfflineValidation_Dqm_cff.py
@@ -1,46 +1,74 @@
 import FWCore.ParameterSet.Config as cms
 
 
-from Alignment.OfflineValidation.TrackerOfflineValidation_cfi import *
-
+##
+## Set standard binning for the DMR histograms
+##
+from Alignment.OfflineValidation.TrackerOfflineValidationSummary_cfi import *
 
 # do the parameter setting before cloning, so the clone gets these values
+TrackerOfflineValidationSummary.TH1DmrXprimeStripModules.Nbinx = 200
+TrackerOfflineValidationSummary.TH1DmrXprimeStripModules.xmin = -0.05
+TrackerOfflineValidationSummary.TH1DmrXprimeStripModules.xmax = 0.05
 
-TrackerOfflineValidation.TH1NormXprimeResStripModules.Nbinx = 120
-TrackerOfflineValidation.TH1NormXprimeResStripModules.xmin = -3.0
-TrackerOfflineValidation.TH1NormXprimeResStripModules.xmax = 3.0
+TrackerOfflineValidationSummary.TH1DmrYprimeStripModules.Nbinx = 200
+TrackerOfflineValidationSummary.TH1DmrYprimeStripModules.xmin = -0.05
+TrackerOfflineValidationSummary.TH1DmrYprimeStripModules.xmax = 0.05
 
-TrackerOfflineValidation.TH1XprimeResStripModules.Nbinx = 2000
-TrackerOfflineValidation.TH1XprimeResStripModules.xmin = -0.5
-TrackerOfflineValidation.TH1XprimeResStripModules.xmax = 0.5
+TrackerOfflineValidationSummary.TH1DmrXprimePixelModules.Nbinx = 200
+TrackerOfflineValidationSummary.TH1DmrXprimePixelModules.xmin = -0.05
+TrackerOfflineValidationSummary.TH1DmrXprimePixelModules.xmax = 0.05
 
-TrackerOfflineValidation.TH1NormYResStripModules.Nbinx = 120
-TrackerOfflineValidation.TH1NormYResStripModules.xmin = -3.0
-TrackerOfflineValidation.TH1NormYResStripModules.xmax = 3.0
+TrackerOfflineValidationSummary.TH1DmrYprimePixelModules.Nbinx = 200
+TrackerOfflineValidationSummary.TH1DmrYprimePixelModules.xmin = -0.05
+TrackerOfflineValidationSummary.TH1DmrYprimePixelModules.xmax = 0.05
 
-TrackerOfflineValidation.TH1YResStripModules.Nbinx = 2000
-TrackerOfflineValidation.TH1YResStripModules.xmin = -10.0
-TrackerOfflineValidation.TH1YResStripModules.xmax = 10.0
-
-TrackerOfflineValidation.TH1NormXprimeResPixelModules.Nbinx = 120
-TrackerOfflineValidation.TH1NormXprimeResPixelModules.xmin = -3.0
-TrackerOfflineValidation.TH1NormXprimeResPixelModules.xmax = 3.0
-
-TrackerOfflineValidation.TH1XprimeResPixelModules.Nbinx = 2000
-TrackerOfflineValidation.TH1XprimeResPixelModules.xmin = -0.5
-TrackerOfflineValidation.TH1XprimeResPixelModules.xmax = 0.5
-
-TrackerOfflineValidation.TH1NormYResPixelModules .Nbinx = 120
-TrackerOfflineValidation.TH1NormYResPixelModules .xmin = -3.0
-TrackerOfflineValidation.TH1NormYResPixelModules .xmax = 3.0
-
-TrackerOfflineValidation.TH1YResPixelModules.Nbinx = 2000
-TrackerOfflineValidation.TH1YResPixelModules.xmin = -0.5
-TrackerOfflineValidation.TH1YResPixelModules.xmax = 0.5
+# First clone
+TrackerOfflineValidationSummaryBinned = TrackerOfflineValidationSummary.clone()
 
 
-TrackerOfflineValidationDqm = TrackerOfflineValidation.clone(
+##
+## TrackerOfflineValidation (DQM mode)
+##
+from Alignment.OfflineValidation.TrackerOfflineValidation_Standalone_cff import TrackerOfflineValidationBinned
+TrackerOfflineValidationDqm = TrackerOfflineValidationBinned.clone(
     useInDqmMode              = True,
-    moduleDirectoryInOutput   = "AlCaReco/TkAl",
+    moduleDirectoryInOutput   = "Alignment/Tracker",
+    Tracks =  'TrackRefitterForOfflineValidation',
+    trajectoryInput = 'TrackRefitterForOfflineValidation'
 )
 
+
+##
+## TrackerOfflineValidationSummary
+##
+TrackerOfflineValidationSummaryDqm = TrackerOfflineValidationSummaryBinned.clone(
+    removeModuleLevelHists = True,
+    minEntriesPerModuleForDmr = 100
+)
+
+
+##
+## Output File Configuration
+##
+# DQM backend
+from DQMServices.Core.DQM_cfg import *
+# DQM file saver
+DqmSaverTkAl = cms.EDFilter("DQMFileSaver",
+          convention=cms.untracked.string("Offline"),
+          workflow=cms.untracked.string("/Cosmics/TkAl09-AlignmentSpecification_R000100000_R000100050_ValSkim-v1/ALCARECO"),   # /primaryDatasetName/WorkflowDescription/DataTier; Current Convention: Indicate run range (first and last run) in file name
+	                                                                                                                       # WorkflowDescription should match namespace conventions, must have a year indicated by 2 digits in first word (aquisition era)) 
+	  dirName=cms.untracked.string("."),
+          saveByRun=cms.untracked.int32(-1),
+	  saveAtJobEnd=cms.untracked.bool(True),                        
+          forceRunNumber=cms.untracked.int32(100000)   # Current Convention: Take first processed run
+)
+
+
+##
+## Sequence
+##
+seqTrackerOfflineValidationDqm = cms.Sequence(TrackerOfflineValidationDqm
+                                              *TrackerOfflineValidationSummaryDqm
+					      *DqmSaverTkAl
+)
