78ff18ae6c4b5985ae5a205be4d9f970e5adfa1e
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2009-12-11 09:23:52 +0000

    change hf reconstruction, add L1 product

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
@@ -10,8 +10,11 @@ import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hbhe_cfi
 hbherecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hbhe_cfi.hbhereco.clone()
 import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi
 hfrecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi.hfreco.clone()
+import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi
+hfrecoMB = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_hf_cfi.hfreco.clone()
 import RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi
 horecoNoise = RecoLocalCalo.HcalRecProducers.HcalSimpleReconstructor_ho_cfi.horeco.clone()
+
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
 #    HLTPaths = ['AlCa_HcalPhiSym'],
@@ -24,12 +27,19 @@ seqALCARECOHcalCalMinBias = cms.Sequence(hcalminbiasHLT*hcalDigiAlCaMB*hbherecoN
 hcalDigiAlCaMB.firstSample = 0
 hcalDigiAlCaMB.lastSample = 9
 hcalDigiAlCaMB.InputLabel = 'source'
+
 hbherecoNoise.firstSample = 0
 hbherecoNoise.samplesToAdd = 4
 hbherecoNoise.digiLabel = 'hcalDigiAlCaMB'
-hfrecoNoise.firstSample = 1
-hfrecoNoise.samplesToAdd = 1
+
+hfrecoNoise.firstSample = 0
+hfrecoNoise.samplesToAdd = 3
 hfrecoNoise.digiLabel = 'hcalDigiAlCaMB'
+
+hfrecoMB.firstSample = 3
+hfrecoMB.samplesToAdd = 3
+hfrecoMB.digiLabel = 'hcalDigiAlCaMB'
+
 horecoNoise.firstSample = 0
 horecoNoise.samplesToAdd = 4
 horecoNoise.digiLabel = 'hcalDigiAlCaMB'
