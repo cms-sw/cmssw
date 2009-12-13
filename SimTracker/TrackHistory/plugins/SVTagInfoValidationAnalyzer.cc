e1f67812e58480ffe6a2fea48c3b39e726772902
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-12-13 00:20:53 +0000

    fix BuildFile and includes

--- SimTracker/TrackHistory/plugins/SVTagInfoValidationAnalyzer.cc
+++ SimTracker/TrackHistory/plugins/SVTagInfoValidationAnalyzer.cc
@@ -16,7 +16,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "SimTracker/TrackHistory/interface/VertexClassifierByProxy.h"
 #include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
--- SimTracker/TrackHistory/plugins/TrackingParticleCategoriesAnalyser.cc
+++ SimTracker/TrackHistory/plugins/TrackingParticleCategoriesAnalyser.cc
@@ -13,7 +13,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "SimTracker/TrackHistory/interface/TrackClassifier.h"
 
