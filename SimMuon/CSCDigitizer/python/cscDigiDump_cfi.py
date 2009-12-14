3743dcbbe6065d4f789aad430a307971c3971400
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2009-12-14 22:33:51 +0000

    MAke it an EDAnalyzer

--- SimMuon/CSCDigitizer/python/cscDigiDump_cfi.py
+++ SimMuon/CSCDigitizer/python/cscDigiDump_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-cscDigiDump = cms.EDFilter("CSCDigiDump",
+cscDigiDump = cms.EDAnalyzer("CSCDigiDump",
     wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
     stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi"),
     comparatorDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi")
