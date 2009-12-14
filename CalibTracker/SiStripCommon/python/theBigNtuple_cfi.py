2112532571f22e54ad63ca360b58387e1f26d83d
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-12-14 18:16:42 +0000

    improved cfi. cloned ShallowProducers that could be used in other calibration tasks, after  filters

--- CalibTracker/SiStripCommon/python/theBigNtuple_cfi.py
+++ CalibTracker/SiStripCommon/python/theBigNtuple_cfi.py
@@ -1,25 +1,51 @@
 import FWCore.ParameterSet.Config as cms
 
 from CalibTracker.SiStripCommon.ShallowEventDataProducer_cfi import *
+from CalibTracker.SiStripCommon.ShallowDigisProducer_cfi import *
 from CalibTracker.SiStripCommon.ShallowClustersProducer_cfi import *
 from CalibTracker.SiStripCommon.ShallowTrackClustersProducer_cfi import *
 from CalibTracker.SiStripCommon.ShallowRechitClustersProducer_cfi import *
 from CalibTracker.SiStripCommon.ShallowTracksProducer_cfi import *
 
-shallowTree = cms.EDAnalyzer("ShallowTree",
-                             outputCommands = cms.untracked.vstring(
-    'drop *',
-    'keep *_shallowEventRun_*_*',
-    'keep *_shallowClusters_*_*',
-    'keep *_shallowRechitClusters_*_*',
-    'keep *_shallowTracks_*_*',
-    'keep *_shallowTrackClusters_*_*'
-    ))
+from RecoVertex.BeamSpotProducer.BeamSpot_cff import *
+from RecoTracker.TrackProducer.TrackRefitters_cff import *
 
-theBigNtuple = cms.Sequence( (shallowEventRun +
-                              shallowClusters +
-                              shallowRechitClusters +
-                              shallowTracks +
-                              shallowTrackClusters) *
-                             shallowTree
+bigNtupleTrackCollectionTag = cms.InputTag("bigNtupleTracksRefit")
+bigNtupleClusterCollectionTag = cms.InputTag("siStripClusters")
+
+bigNtupleTracksRefit =  RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone(src = bigNtupleTrackCollectionTag)
+
+
+bigNtupleEventRun        = shallowEventRun.clone()
+bigNtupleDigis           = shallowDigis.clone()
+bigNtupleClusters        = shallowClusters.clone(Clusters=bigNtupleClusterCollectionTag)
+bigNtupleRecHits         = shallowRechitClusters.clone(Clusters=bigNtupleClusterCollectionTag)
+bigNtupleTrackClusters   = shallowTrackClusters.clone(Tracks = bigNtupleTrackCollectionTag,Clusters=bigNtupleClusterCollectionTag)
+bigNtupleTracks          = shallowTracks.clone(Tracks = bigNtupleTrackCollectionTag)
+    
+
+bigShallowTree = cms.EDAnalyzer("ShallowTree",
+                                outputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep *_bigNtupleEventRun_*_*',
+    'keep *_bigNtupleDigis_*_*',
+    'keep *_bigNtupleClusters_*_*' ,
+    'keep *_bigNtupleRechits_*_*',
+    'keep *_bigNtupleTracks_*_*',
+        'keep *_bigNtupleTrackClusters_*_*'
+    )
+                                )
+
+
+theBigNtuple = cms.Sequence( (siStripMatchedRecHits + offlineBeamSpot + bigNtupleTracksRefit)
+                             * (bigNtupleEventRun +
+                                #bigNtupleDigis +
+                                bigNtupleClusters +
+                                bigNtupleRecHits +
+                                bigNtupleTracks +
+                                bigNtupleTrackClusters
+                                )
+                             *
+                             bigShallowTree
                              )
+
