45be2ed9232dc0db6be59ed3fba49da662e63eb1
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-12-14 13:49:46 +0000

    Added DetVOff O2O module

--- CalibTracker/SiStripDCS/plugins/SealModules.cc
+++ CalibTracker/SiStripDCS/plugins/SealModules.cc
@@ -6,6 +6,7 @@ DEFINE_SEAL_MODULE();
 #include "CondCore/PopCon/interface/PopConAnalyzer.h"
 #include "CalibTracker/SiStripDCS/interface/SiStripModuleHVHandler.h"
 
+//OLD DCS O2O
 typedef popcon::PopConAnalyzer<popcon::SiStripModuleHVHandler> SiStripPopConModuleHV;
 DEFINE_ANOTHER_FWK_MODULE(SiStripPopConModuleHV);
 
@@ -13,3 +14,11 @@ DEFINE_ANOTHER_FWK_MODULE(SiStripPopConModuleHV);
 #include "CalibTracker/SiStripDCS/interface/SiStripModuleHVBuilder.h"
 DEFINE_ANOTHER_FWK_SERVICE(SiStripModuleHVBuilder);
 
+//ReWritten DCS O2O
+#include "CalibTracker/SiStripDCS/interface/SiStripDetVOffHandler.h"
+typedef popcon::PopConAnalyzer<popcon::SiStripDetVOffHandler> SiStripPopConDetVOff;
+DEFINE_ANOTHER_FWK_MODULE(SiStripPopConDetVOff);
+
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "CalibTracker/SiStripDCS/interface/SiStripDetVOffBuilder.h"
+DEFINE_ANOTHER_FWK_SERVICE(SiStripDetVOffBuilder);
