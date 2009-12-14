5596618340d6aa3f91bc38eb746d3b2ce800eea4
Davide Pagano <sha1-e26cfdfa7f699a044348a57484e4dcb81e829826@cern.ch>
2009-12-14 12:44:42 +0000

    new release

--- CondFormats/RPCObjects/interface/RPCObUXC.h
+++ CondFormats/RPCObjects/interface/RPCObUXC.h
@@ -1,7 +1,7 @@
 /*
  * Payload definition(s): UXC Temperature, Pressure and Humidity (RPCObUXC)
  *
- *  $Date: 2009/11/16 12:53:47 $
+ *  $Date: 2009/11/16 13:04:37 $
  *  $Revision: 1.3 $
  *  \author D. Pagano - Dip. Fis. Nucl. e Teo. & INFN Pavia
  */
@@ -15,7 +15,7 @@ class RPCObUXC {
       struct Item {
         float temperature;
         float pressure;
-        float humidity;
+        float dewpoint;
 	int unixtime;
       };
     RPCObUXC(){}
