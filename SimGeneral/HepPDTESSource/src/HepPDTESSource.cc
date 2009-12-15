8b1e0aba5c02f587e0a038d526b2a5b3a6551c61
Todd Adams <sha1-701f8de8162420686577eb61e6718f6876a7f91f@cern.ch>
2009-12-15 14:57:05 +0000

    change PDT constructor to use HeavyIon method for unknown ID

--- SimGeneral/HepPDTESSource/src/HepPDTESSource.cc
+++ SimGeneral/HepPDTESSource/src/HepPDTESSource.cc
@@ -1,4 +1,5 @@
 #include "SimGeneral/HepPDTESSource/interface/HepPDTESSource.h"
+#include "HepPDT/HeavyIonUnknownID.hh"
 
 HepPDTESSource::HepPDTESSource( const edm::ParameterSet& cfg ) :
   pdtFileName( cfg.getParameter<edm::FileInPath>( "pdtFileName" ) ) {
@@ -12,7 +13,7 @@ HepPDTESSource::~HepPDTESSource() {
 HepPDTESSource::ReturnType
 HepPDTESSource::produce( const PDTRecord & iRecord ) {
   using namespace edm::es;
-  std::auto_ptr<PDT> pdt( new PDT( "PDG table" ) ); 
+  std::auto_ptr<PDT> pdt( new PDT( "PDG table" , new HepPDT::HeavyIonUnknownID) ); 
   std::ifstream pdtFile( pdtFileName.fullPath().c_str() );
   if( ! pdtFile ) 
     throw cms::Exception( "FileNotFound", "can't open pdt file" )
