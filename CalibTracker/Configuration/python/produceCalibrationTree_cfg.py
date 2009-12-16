65b01213e89b2fb53f844589edfed1eccf90ad97
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2009-12-16 00:50:26 +0000

    Reorder, put process.source last, since it is the least relevant (ignored by CRAB)

--- CalibTracker/Configuration/python/produceCalibrationTree_cfg.py
+++ CalibTracker/Configuration/python/produceCalibrationTree_cfg.py
@@ -13,18 +13,21 @@ process.add_( cms.Service( "TFileService",
                            fileName = cms.string( 'calibTreeTest.root' ),
                            closeFileFast = cms.untracked.bool(True)  ) )
 
+process.load('CalibTracker.SiStripCommon.theBigNtuple_cfi')
+process.TkCalPath = cms.Path( process.theBigNtuple * process.TkCalFullSequence     )
+
+process.schedule = cms.Schedule( process.TkCalPath )
+
+
+
+
 #following ignored by CRAB
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.source = cms.Source (
     "PoolSource",
     #from run = 123596, dataset = /MinimumBias/BeamCommissioning09-SD_AllMinBias-skim_GR09_P_V7_v1/RAW-RECO 
-    fileNames = cms.untracked.vstring('/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/Dec9thReReco_BSCNOBEAMHALO-v1/0000/5A8A4A57-22E5-DE11-81B0-0026189438B5.root',
-                                      '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/Dec9thReReco_BSCNOBEAMHALO-v1/0000/1A202DCE-1DE5-DE11-A176-002618943985.root'
-                                      ),
-
+    fileNames=cms.untracked.vstring('/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/Dec9thReReco_BSCNOBEAMHALO-v1/0000/5A8A4A57-22E5-DE11-81B0-0026189438B5.root',
+                                    '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/Dec9thReReco_BSCNOBEAMHALO-v1/0000/1A202DCE-1DE5-DE11-A176-002618943985.root'
+                                    ),
     secondaryFileNames = cms.untracked.vstring())
 
-process.load('CalibTracker.SiStripCommon.theBigNtuple_cfi')
-process.TkCalPath = cms.Path( process.theBigNtuple * process.TkCalFullSequence     )
-
-process.schedule = cms.Schedule( process.TkCalPath )
