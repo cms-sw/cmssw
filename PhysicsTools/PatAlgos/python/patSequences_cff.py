b4c071c4ead5bad81e3937012eddbbd6e012b2d6
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-12-17 15:24:43 +0000

    adaption comments in file

--- PhysicsTools/PatAlgos/python/patSequences_cff.py
+++ PhysicsTools/PatAlgos/python/patSequences_cff.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-# make allLayer1Objects
+# make patCandidates
 from PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff import *
 
-# make selectedLayer1Objects
+# make selectedPatCandidates
 from PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff import *
 
-# make cleanLayer1Objects
+# make cleanPatCandidates
 from PhysicsTools.PatAlgos.cleaningLayer1.cleanPatCandidates_cff import *
 
-# count selected layer 1 objects (including total number of leptons)
+# count cleanPatCandidates (including total number of leptons)
 from PhysicsTools.PatAlgos.selectionLayer1.countPatCandidates_cff import *
 
 patDefaultSequence = cms.Sequence(
