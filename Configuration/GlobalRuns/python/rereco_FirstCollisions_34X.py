5838105750d8d08969eeb7fe633709af486ee5a5
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2009-12-17 14:44:16 +0000

    34X version

---
+++ Configuration/GlobalRuns/python/rereco_FirstCollisions_34X.py
@@ -0,0 +1,158 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.149 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: promptCollisionReco -s RAW2DIGI,L1Reco,RECO,DQM,ALCA:SiStripCalZeroBias --datatier RECO --eventcontent RECO --conditions CRAFT09_R_V4::All --scenario pp --no_exec --data --magField AutoFromDBCurrent -n 100
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/GeometryExtended_cff')
+process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration/StandardSequences/RawToDigi_Data_cff')
+process.load('Configuration/StandardSequences/L1Reco_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('DQMOffline/Configuration/DQMOffline_cff')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.6 $'),
+    annotation = cms.untracked.string('rereco nevts:100'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound'),
+    wantSummary = cms.untracked.bool(True) 
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring( 
+'/store/data/BeamCommissioning09/MinimumBias/RAW/v1/000/124/120/DC0FA50D-6BE8-DE11-8A92-000423D94E70.root'
+#'file:/data/withvertex732.root'
+#'/store/express/BeamCommissioning09/ExpressPhysics/FEVT/v2/000/123/615/38379AF1-B4E2-DE11-BB10-001617C3B706.root'
+#'rfio:/castor.cern.ch/cms/store/data/BeamCommissioning09/castor/MinimumBias/RAW/v1/000/122/314/CC89C4BC-DE11-B365-0030487D0D3A.root'
+    )
+)
+
+process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*", "drop L1GlobalTriggerObjectMapRecord_hltL1GtObjectMap__HLT")
+
+# Output definition
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.RECOEventContent.outputCommands,
+    fileName = cms.untracked.string('promptReco_RAW2DIGI_L1Reco_RECO_DQM_ALCA.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    )
+)
+
+# Other statements
+process.GlobalTag.globaltag = 'GR09_P_V8_34X::All'
+
+
+
+#####################################################################################################
+####
+####  Top level replaces for handling strange scenarios of early collisions
+####
+
+## TRACKING:
+## Skip events with HV off
+process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=1000
+process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 5000
+process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+
+## Primary Vertex
+process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconHits = 6
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelHits = 1
+process.offlinePrimaryVerticesWithBS.TkClusParameters.zSeparation = 10
+process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
+process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
+process.offlinePrimaryVertices.TkFilterParameters.minSiliconHits = 6
+process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
+process.offlinePrimaryVertices.TkFilterParameters.minPixelHits = 1
+process.offlinePrimaryVertices.TkClusParameters.zSeparation = 10
+
+## ECAL 
+process.load('RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi')
+process.ecalLocalRecoSequence.replace(process.ecalGlobalUncalibRecHit,process.ecalFixedAlphaBetaFitUncalibRecHit)
+process.ecalFixedAlphaBetaFitUncalibRecHit.alphaEB = 1.138
+process.ecalFixedAlphaBetaFitUncalibRecHit.betaEB = 1.655
+process.ecalFixedAlphaBetaFitUncalibRecHit.alphaEE = 1.890
+process.ecalFixedAlphaBetaFitUncalibRecHit.betaEE = 1.400
+process.ecalRecHit.EBuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEB'
+process.ecalRecHit.EEuncalibRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEE'
+process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 78, 142 ]
+process.ecalBarrelCosmicTask.EcalUncalibratedRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEB'
+process.ecalEndcapCosmicTask.EcalUncalibratedRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEE'
+process.ecalBarrelTimingTask.EcalUncalibratedRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEB'
+process.ecalEndcapTimingTask.EcalUncalibratedRecHitCollection = 'ecalFixedAlphaBetaFitUncalibRecHit:EcalUncalibRecHitsEE'
+
+##Preshower
+process.ecalPreshowerRecHit.ESGain = 2
+process.ecalPreshowerRecHit.ESBaseline = 0
+process.ecalPreshowerRecHit.ESMIPADC = 55
+
+##only for 34X
+process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+
+## HCAL temporary fixes
+process.hfreco.firstSample  = 3
+process.hfreco.samplesToAdd = 4
+
+process.hbhereco.firstSample = 1
+process.hbhereco.samplesToAdd = 8
+
+## EGAMMA
+process.ecalDrivenElectronSeeds.SCEtCut = cms.double(1.0)
+process.ecalDrivenElectronSeeds.applyHOverECut = cms.bool(False)
+process.ecalDrivenElectronSeeds.SeedConfiguration.z2MinB = cms.double(-0.9)
+process.ecalDrivenElectronSeeds.SeedConfiguration.z2MaxB = cms.double(0.9)
+process.ecalDrivenElectronSeeds.SeedConfiguration.r2MinF = cms.double(-1.5)
+process.ecalDrivenElectronSeeds.SeedConfiguration.r2MaxF = cms.double(1.5)
+process.ecalDrivenElectronSeeds.SeedConfiguration.rMinI = cms.double(-2.)
+process.ecalDrivenElectronSeeds.SeedConfiguration.rMaxI = cms.double(2.)
+process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi1Low = cms.double(0.3)
+process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi1High = cms.double(0.3)
+process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi2 = cms.double(0.3)
+process.gsfElectrons.applyPreselection = cms.bool(False)
+process.photons.minSCEtBarrel = 1.
+process.photons.minSCEtEndcap =1.
+process.photonCore.minSCEt = 1.
+process.conversionTrackCandidates.minSCEt =1.
+process.conversions.minSCEt =1.
+process.trackerOnlyConversions.AllowTrackBC = cms.bool(False)
+process.trackerOnlyConversions.AllowRightBC = cms.bool(False)
+process.trackerOnlyConversions.MinApproach = cms.double(-.25)
+process.trackerOnlyConversions.DeltaCotTheta = cms.double(.07)
+process.trackerOnlyConversions.DeltaPhi = cms.double(.2)
+
+###
+###  end of top level replacements
+###
+###############################################################################################
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction_withPixellessTk)
+process.dqmoffline_step = cms.Path(process.DQMOffline)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.FEVT)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.dqmoffline_step,process.endjob_step,process.out_step)
