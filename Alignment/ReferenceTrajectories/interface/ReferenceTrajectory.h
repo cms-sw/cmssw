6fd8cf836fb1a758677791eefc88d8e8e6313d0e
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2009-12-17 12:27:31 +0000

    arguments by reference

--- Alignment/ReferenceTrajectories/interface/ReferenceTrajectory.h
+++ Alignment/ReferenceTrajectories/interface/ReferenceTrajectory.h
@@ -4,8 +4,8 @@
 /**
  * Author     : Gero Flucke (based on code by Edmund Widl replacing ORCA's TkReferenceTrack)
  * date       : 2006/09/17
- * last update: $Date: 2009/10/14 14:31:27 $
- * by         : $Author: flucke $
+ * last update: $Date: 2009/11/30 10:12:34 $
+ * by         : $Author: ckleinw $
  *
  *  Class implementing the reference trajectory of a single charged
  *  particle, i.e. a helix with 5 parameters. Given the
@@ -126,14 +126,14 @@ protected:
 				     const std::vector<AlgebraicSymMatrix> &allDeltaParaCovs,
 				     const std::vector<AlgebraicMatrix> &allLocalToCurv,
 				     const std::vector<double> &allSteps,
-				     const GlobalTrajectoryParameters gtp);
+				     const GlobalTrajectoryParameters &gtp);
   /** internal methods to add material effects using broken lines (coarse version)
    */
   virtual void addMaterialEffectsBrl(const std::vector<AlgebraicMatrix> &allProjections,
 				     const std::vector<AlgebraicSymMatrix> &allDeltaParaCovs,
 				     const std::vector<AlgebraicMatrix> &allLocalToCurv,
 				     const std::vector<double> &allSteps,
-				     const GlobalTrajectoryParameters gtp,   
+				     const GlobalTrajectoryParameters &gtp,   
 				     const double minStep = 1.0);
           
   // Don't care for propagation direction 'anyDirection' - in that case the material effects
