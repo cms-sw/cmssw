347671fb1983b42373c73484b9959084cea83c9c
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-12-18 01:48:55 +0000

    fix python24->2.6 migration problem

--- Configuration/PyReleaseValidation/python/ConfigBuilder.py
+++ Configuration/PyReleaseValidation/python/ConfigBuilder.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 
-__version__ = "$Revision: 1.154 $"
+__version__ = "$Revision: 1.155 $"
 __source__ = "$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v $"
 
 import FWCore.ParameterSet.Config as cms
@@ -137,6 +137,7 @@ class ConfigBuilder(object):
             evt_type = self._options.evt_type.rstrip(".py").replace(".","_")
             if "/" in evt_type:
                 evt_type = evt_type.replace("python/","")
+                evt_type = evt_type.replace("/",".")
             else:
                 evt_type = ('Configuration.Generator.'+evt_type).replace('/','.') 
             __import__(evt_type)
@@ -816,7 +817,7 @@ class ConfigBuilder(object):
     def build_production_info(self, evt_type, evtnumber):
         """ Add useful info for the production. """
         prod_info=cms.untracked.PSet\
-              (version=cms.untracked.string("$Revision: 1.154 $"),
+              (version=cms.untracked.string("$Revision: 1.155 $"),
                name=cms.untracked.string("PyReleaseValidation"),
                annotation=cms.untracked.string(evt_type+ " nevts:"+str(evtnumber))
               )
