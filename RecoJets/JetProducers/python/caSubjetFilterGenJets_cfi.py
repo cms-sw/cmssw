0970cf48f9ff04b19954b4c5c1cbbcb589316c22
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2009-12-18 19:04:12 +0000

    add Subjet/Filter algorithm.

---
+++ RecoJets/JetProducers/python/caSubjetFilterGenJets_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoJets.JetProducers.GenJetParameters_cfi import *
+from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
+
+caSubjetFilterGenJets = cms.EDProducer(
+    "SubjetFilterJetProducer",
+    GenJetParameters,
+    AnomalousCellParameters,
+    jetAlgorithm = cms.string("CambridgeAachen"),
+    rParam       = cms.double(1.2),
+    massDropCut  = cms.double(0.667),
+    asymmCut     = cms.double(0.3)
+    )
