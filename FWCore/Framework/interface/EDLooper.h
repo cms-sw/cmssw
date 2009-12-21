6c0f1248b2311d734347eedefc485bf96e047dc1
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2009-12-21 16:18:27 +0000

    added much needed documentation

--- FWCore/Framework/interface/EDLooper.h
+++ FWCore/Framework/interface/EDLooper.h
@@ -5,14 +5,17 @@
 // Package:     Framework
 // Module:      EDLooper
 // 
-/**\class EDLooper EDLooper.h package/EDLooper.h
+/**\class EDLooper EDLooper.h FWCore/Framework/interface/EDLooper.h
 
  Description: Base class for all looping components
+ 
+ This abstract class forms the basis of being able to loop through a list of events multiple times.
+ 
 */
 //
 // Author:      Valentin Kuznetsov
 // Created:     Wed Jul  5 11:42:17 EDT 2006
-// $Id: EDLooper.h,v 1.10 2009/02/23 21:34:29 wmtan Exp $
+// $Id: EDLooper.h,v 1.11 2009/06/04 18:21:45 chrjones Exp $
 //
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -48,14 +51,34 @@ namespace edm {
       //This interface is deprecated
       virtual void beginOfJob(const edm::EventSetup&); 
       virtual void beginOfJob();
+     
+      /**Called before system starts to loop over the events. The argument is a count of
+       how many loops have been processed.  For the first time through the events the argument
+       will be 0.
+       */
       virtual void startingNewLoop(unsigned int ) = 0; 
+     
+      /**Called after all event modules have had a chance to process the edm::Event.
+       */
       virtual Status duringLoop(const edm::Event&, const edm::EventSetup&) = 0; 
+     
+      /**Called after the system has finished one loop over the events. Thar argument is a 
+       count of how many loops have been processed before this loo.  For the first time through
+       the events the argument will be 0.
+       */
       virtual Status endOfLoop(const edm::EventSetup&, unsigned int iCounter) = 0; 
       virtual void endOfJob();
 
+      ///Called after all event modules have processed the begin of a Run
       virtual void beginRun(Run const&, EventSetup const&);
+     
+      ///Called after all event modules have processed the end of a Run
       virtual void endRun(Run const&, EventSetup const&);
+     
+      ///Called after all event modules have processed the begin of a LuminosityBlock
       virtual void beginLuminosityBlock(LuminosityBlock const&, EventSetup const&);
+
+      ///Called after all event modules have processed the end of a LuminosityBlock
       virtual void endLuminosityBlock(LuminosityBlock const&, EventSetup const&);
 
       void setActionTable(ActionTable* actionTable) { act_table_ = actionTable; }
