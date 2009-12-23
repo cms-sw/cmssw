c30d71c7de00826e52bbfe87f62ec32f3dafc2a5
Andrea Lucaroni <sha1-a17ddc562d56307fc4abaf6c9898c35059089bf9@cern.ch>
2009-12-23 21:38:49 +0000

    buf fix dN_vs_dphi and dpT_vs_dphi

--- QCDAnalysis/UEAnalysis/root/UEActivity.C
+++ QCDAnalysis/UEAnalysis/root/UEActivity.C
@@ -14,7 +14,8 @@ UEActivity::UEActivity()
   h_pTChg       = new TH1D*[3];
   h_dN_vs_dphi  = new TH1D*[3];
   h_dpT_vs_dphi = new TH1D*[3];
-
+  h_NChg        = new TH1D*[3];
+  h_pTSChg      = new TH1D*[3];
   char buffer[200];
   for ( unsigned int iregion(0); iregion<3; ++iregion )
     {
@@ -26,6 +27,24 @@ UEActivity::UEActivity()
 
       sprintf ( buffer, "h_dpT_vs_dphi[%i]", iregion );
       h_dpT_vs_dphi[iregion] = new TH1D( buffer, buffer, 360, -180., 180.);
+      //new
+      sprintf( buffer,"N chg[%i]", iregion);
+      h_NChg[iregion]= new TH1D( buffer, buffer, 100, 0., 100.);
+      sprintf( buffer,"pTS chg[%i]", iregion);
+      h_pTSChg[iregion] = new TH1D( buffer, buffer, 1000, 0., 100.);
+ 
+      if(iregion==1)
+	{
+	  sprintf ( buffer, "h_dNMax_vs_dphi[%i]", iregion );
+	  h_dNMax_vs_dphi= new TH1D( buffer, buffer, 360, -180., 180.);
+	  sprintf ( buffer, "h_dNMin_vs_dphi[%i]", iregion );
+	  h_dNMin_vs_dphi= new TH1D( buffer, buffer, 360, -180., 180.);
+	  sprintf ( buffer, "h_dpTMax_vs_dphi[%i]", iregion );
+	  h_dpTMax_vs_dphi= new TH1D( buffer, buffer, 360, -180., 180.);
+	  sprintf ( buffer, "h_dpTMin_vs_dphi[%i]", iregion );
+	  h_dpTMin_vs_dphi= new TH1D( buffer, buffer, 360, -180., 180.);
+	
+	}
     }
 }
 
@@ -41,6 +60,14 @@ UEActivityFinder::UEActivityFinder( double theEtaRegion, double thePtThreshold )
   _h_pTChg       = new TH1D*[3];
   _h_dN_vs_dphi  = new TH1D*[3];
   _h_dpT_vs_dphi = new TH1D*[3];
+  //new 
+  _h_NChg        = new TH1D*[3];
+  _h_pTSChg      = new TH1D*[3];
+ 
+  _h_dN_vs_dphiN1 = new TH1D[1];
+  _h_dN_vs_dphiN2 =new TH1D[1];
+  _h_dpT_vs_dphiN1 = new TH1D[1];
+  _h_dpT_vs_dphiN2 =new TH1D[1];
 
   char buffer[200];
   for ( unsigned int iregion(0); iregion<3; ++iregion )
@@ -53,6 +80,26 @@ UEActivityFinder::UEActivityFinder( double theEtaRegion, double thePtThreshold )
 
       sprintf ( buffer, "_h_dpT_vs_dphi[%i]", iregion );
       _h_dpT_vs_dphi[iregion] = new TH1D( buffer, buffer, 360, -180., 180.);
+      //new
+      sprintf ( buffer, "_h_NChg[%i]", iregion );
+      _h_NChg[iregion] = new TH1D( buffer, buffer, 100,  0. , 100. );
+      sprintf ( buffer, "_h_pTSChg[%i]", iregion );
+      _h_pTSChg[iregion] = new TH1D( buffer, buffer, 1000,  0. , 100. );
+
+      if(iregion==1)
+	{
+	  sprintf ( buffer, "_h_dN_vs_dphiN1[%i]", iregion );
+	  _h_dN_vs_dphiN1= new TH1D( buffer, buffer, 360, -180., 180.);
+	  sprintf ( buffer, "_h_dN_vs_dphiN2[%i]", iregion );
+	  _h_dN_vs_dphiN2= new TH1D( buffer, buffer, 360, -180., 180.);
+	} 
+      if(iregion==1)
+      {
+	sprintf ( buffer, "_h_dpT_vs_dphiN1[%i]", iregion );
+	_h_dpT_vs_dphiN1= new TH1D( buffer, buffer, 360, -180., 180.);
+	sprintf ( buffer, "_h_dpT_vs_dphiN2[%i]", iregion );
+	_h_dpT_vs_dphiN2= new TH1D( buffer, buffer, 360, -180., 180.);
+      } 
     }
 }
 
@@ -68,7 +115,17 @@ UEActivityFinder::find( TClonesArray& Jet, TClonesArray& Particles, UEActivity& 
       _h_pTChg      [iregion]->Reset();
       _h_dN_vs_dphi [iregion]->Reset();
       _h_dpT_vs_dphi[iregion]->Reset();
-    }
+      //new
+      _h_NChg      [iregion]->Reset();
+      _h_pTSChg      [iregion]->Reset();
+      if(iregion==1)
+	{
+	  _h_dN_vs_dphiN1->Reset();
+	  _h_dN_vs_dphiN2->Reset();
+	  _h_dpT_vs_dphiN1->Reset();
+	  _h_dpT_vs_dphiN2->Reset();
+	} 
+   }
 
   ///
   /// find leading jet in chosen (pT, eta)-range
@@ -110,10 +167,16 @@ UEActivityFinder::find( TClonesArray& Jet, TClonesArray& Particles, UEActivity& 
 	  cout << "[UEActivityFinder] Error: dphi = " << dphi << endl;
 	  return kFALSE;
 	}
-
+      // cout<<"entrato"<<endl;
       _h_pTChg      [iregion]->Fill(       pT );
       _h_dN_vs_dphi [iregion]->Fill( dphi     );
       _h_dpT_vs_dphi[iregion]->Fill( dphi, pT );
+      //Max Min transverse
+     
+      if ( dphi >=  60. && dphi < 120)   _h_dN_vs_dphiN1->Fill( dphi );
+      if ( dphi <= -60. && dphi > -120)  _h_dN_vs_dphiN2->Fill( dphi );
+      if ( dphi >=  60. && dphi < 120)   _h_dpT_vs_dphiN1->Fill( dphi,pT );
+      if ( dphi <= -60. && dphi > -120)  _h_dpT_vs_dphiN2->Fill( dphi,pT );
     }
 
   ///
@@ -126,10 +189,57 @@ UEActivityFinder::find( TClonesArray& Jet, TClonesArray& Particles, UEActivity& 
       _h_pTChg      [iregion]->Scale( 1./totalEtaRange );
       _h_dN_vs_dphi [iregion]->Scale( 1./totalEtaRange );
       _h_dpT_vs_dphi[iregion]->Scale( 1./totalEtaRange );
+     
       
+     
+
       theUEActivity.SetTH1D_pTChg      ( iregion, _h_pTChg[iregion]       );
       theUEActivity.SetTH1D_dN_vs_dphi ( iregion, _h_dN_vs_dphi[iregion]  );
       theUEActivity.SetTH1D_dpT_vs_dphi( iregion, _h_dpT_vs_dphi[iregion] );
+      
+      //new
+      _h_NChg[iregion]->Fill(totalEtaRange*_h_dN_vs_dphi[iregion]->Integral());
+      _h_pTSChg[iregion]->Fill(totalEtaRange*_h_dpT_vs_dphi[iregion]->Integral());
+      theUEActivity.SetTH1D_NChg        ( iregion, _h_NChg[iregion]       );
+      theUEActivity.SetTH1D_pTSChg      ( iregion, _h_pTSChg[iregion]     );
+
+      double N1=0; 
+      double N2=0; 
+      if(iregion==1)
+	{
+	  _h_dN_vs_dphiN1->Scale( 1./totalEtaRange );
+	  _h_dN_vs_dphiN2->Scale( 1./totalEtaRange );
+	  N1 =_h_dN_vs_dphiN1->Integral();       
+	  N2 =_h_dN_vs_dphiN2->Integral();       
+	  
+	  if(N1>N2)
+	    {
+	      theUEActivity.SetTH1D_dNMax_vs_dphi(_h_dN_vs_dphiN1);
+	      theUEActivity.SetTH1D_dNMin_vs_dphi(_h_dN_vs_dphiN2);
+	    }
+	  else
+	    {
+	      theUEActivity.SetTH1D_dNMax_vs_dphi(_h_dN_vs_dphiN2);
+	      theUEActivity.SetTH1D_dNMin_vs_dphi(_h_dN_vs_dphiN1);
+	    }
+	  N1=0;
+	  N2=0;
+	  _h_dpT_vs_dphiN1->Scale( 1./totalEtaRange );
+	  _h_dpT_vs_dphiN2->Scale( 1./totalEtaRange );
+	  N1 = _h_dpT_vs_dphiN1->Integral();       
+	  N2 = _h_dpT_vs_dphiN2->Integral();       
+	  //  cout<<N1<<" N1 "<<N2<<" N2 "<<_h_dpT_vs_dphi[iregion]->Integral()<<" all "<<endl;
+	  if(N1>N2)
+	    {
+	      theUEActivity.SetTH1D_dpTMax_vs_dphi(_h_dpT_vs_dphiN1);
+	      theUEActivity.SetTH1D_dpTMin_vs_dphi(_h_dpT_vs_dphiN2);
+	    }
+	  else
+	    {
+	      theUEActivity.SetTH1D_dpTMax_vs_dphi(_h_dpT_vs_dphiN2);
+	      theUEActivity.SetTH1D_dpTMin_vs_dphi(_h_dpT_vs_dphiN1);
+	    }
+	}
     }
 
   return kTRUE;
@@ -150,28 +260,34 @@ UEActivityHistograms::UEActivityHistograms( const char* fileName, string *trigge
   file = TFile::Open( fileName, "recreate" );
 
   TDirectory*  dir    = file->mkdir( "UEActivity" );
-  TDirectory** subdir = new TDirectory*[12];
+  TDirectory** subdir = new TDirectory*[13];
 
 
   ///
   /// Reserve space for histograms.
   ///
-  h_pTJet             = new TH1D*[12];
-  h_etaJet            = new TH1D*[12];
-  h_dN_vs_dphi        = new TH2D*[12];
-  h_dpT_vs_dphi       = new TH2D*[12];
-  h_averagePt_vs_nChg = new TH2D*[12];
-  h_pTChg             = new TH2D*[36]; /// one for each region
-  h_dN_vs_dpTjet      = new TH2D*[36];
-  h_dpT_vs_dpTjet     = new TH2D*[36];
+  h_pTJet             = new TH1D*[13];
+  h_etaJet            = new TH1D*[13];
+  h_dN_vs_dphi        = new TH2D*[13];
+  h_dpT_vs_dphi       = new TH2D*[13];
+  h_averagePt_vs_nChg = new TH2D*[13];
+  h_pTChg             = new TH2D*[39]; /// one for each region
+  h_dN_vs_dpTjet      = new TH2D*[39];
+  h_dpT_vs_dpTjet     = new TH2D*[39];
+  //new
+  h_NChg              = new TH1D*[39];
+  h_pTSChg            = new TH1D*[39];
+  h_dNMax_vs_dpTjet   = new TH2D*[13];
+  h_dNMin_vs_dpTjet   = new TH2D*[13];
+  h_dpTMax_vs_dpTjet  = new TH2D*[13];
+  h_dpTMin_vs_dpTjet  = new TH2D*[13];
 
-  
   ///
-  /// 11 HLT bits :
-  /// 4 Min-Bias (Pixel, Hcal, Ecal, general), Zero-Bias, 6 Jet (30, 50, 80, 110, 180, 250)
+  /// 12 HLT bits :
+  /// 4 Min-Bias (Pixel, Hcal, Ecal, Pixel_Trk5), Zero-Bias, 6 Jet (L115, 30, 80, 110, 140, 180)
   ///
   unsigned int iHLTbit(0);
-  for ( ; iHLTbit<11; ++iHLTbit )
+  for ( ; iHLTbit<12; ++iHLTbit )
     {
       HLTBitNames[iHLTbit] = triggerNames[iHLTbit];
 
@@ -214,22 +330,53 @@ UEActivityHistograms::UEActivityHistograms( const char* fileName, string *trigge
 	  file->cd( buffer );
 
 	  sprintf ( buffer, "h_pTChg;p_{T}(charged) (GeV/c);%s 1/#sigma d#sigma/dp_{T} (1 / GeV/c)", regions[i].c_str() );
-	  h_pTChg[i*12+iHLTbit]
+	  h_pTChg[i*13+iHLTbit]
 	    = new TH2D("h_pTChg",
 		       buffer,
 		       1000, 0., 100., 1000, 0., 100. );
 	  
 	  sprintf ( buffer, "h_dN_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
-	  h_dN_vs_dpTjet[i*12+iHLTbit]
+	  h_dN_vs_dpTjet[i*13+iHLTbit]
 	    = new TH2D("h_dN_vs_dpTjet",
 		       buffer,
-		       300, 0., 600., 1000, 0., 100. );
+		       600, 0., 600., 1000, 0., 100. );
 	  
 	  sprintf ( buffer, "h_dpT_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < d#Sigmap_{T}/d#phid#eta > / 2 GeV/c (GeV/c / rad)", regions[i].c_str() );
-	  h_dpT_vs_dpTjet[i*12+iHLTbit]
+	  h_dpT_vs_dpTjet[i*13+iHLTbit]
 	    = new TH2D("h_dpT_vs_dpTjet",
 		       buffer,
-		   300, 0., 600., 1000, 0., 100. );
+		   600, 0., 600., 1000, 0., 100. );
+	  //new
+	  sprintf( buffer,"N_Chg_%s", regions[i].c_str() );
+	  h_NChg[i*13+iHLTbit]= new TH1D( buffer, "N Chg", 100, 0., 100.);
+	  sprintf( buffer,"pTSChg_%s", regions[i].c_str() );
+	  h_pTSChg[i*13+iHLTbit] = new TH1D( buffer, "pT-Sum 0.1 GeV/c ", 1000, 0., 100.);
+	  
+	  if(i==1)
+	    {
+	     sprintf ( buffer, "h_dNMax_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	  h_dNMax_vs_dpTjet[iHLTbit]
+	    = new TH2D("h_dNMax_vs_dpTjet",
+		       buffer,
+		       600, 0., 600., 1000, 0., 100. );
+	  sprintf ( buffer, "h_dNMin_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	  h_dNMin_vs_dpTjet[iHLTbit]
+	    = new TH2D("h_dNMin_vs_dpTjet",
+		       buffer,
+		       600, 0., 600., 1000, 0., 100. );
+
+	  sprintf ( buffer, "h_dpTMax_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dpT/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	  h_dpTMax_vs_dpTjet[iHLTbit]
+	    = new TH2D("h_dpTMax_vs_dpTjet",
+		       buffer,
+		       600, 0., 600., 1000, 0., 100. );
+
+	  sprintf ( buffer, "h_dpTMin_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dpT/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	  h_dpTMin_vs_dpTjet[iHLTbit]
+	    = new TH2D("h_dpTMin_vs_dpTjet",
+		       buffer,
+		       600, 0., 600., 1000, 0., 100. );
+	    }
 	}
     }
 
@@ -237,7 +384,7 @@ UEActivityHistograms::UEActivityHistograms( const char* fileName, string *trigge
   ///
   /// Hadron level
   ///
-  iHLTbit         = 11;
+  iHLTbit         = 12;
   subdir[iHLTbit] = dir->mkdir( "Gen" );
   file->cd( "UEActivity/Gen" );
 
@@ -275,22 +422,52 @@ UEActivityHistograms::UEActivityHistograms( const char* fileName, string *trigge
       file->cd( buffer );
 
       sprintf ( buffer, "h_pTChg;p_{T}(charged) (GeV/c);%s 1/#sigma d#sigma/dp_{T} (1 / GeV/c)", regions[i].c_str() );
-      h_pTChg[i*12+iHLTbit]
+      h_pTChg[i*13+iHLTbit]
 	= new TH2D("h_pTChg",
 		   buffer,
 		   1000, 0., 100., 1000, 0., 100. );
       
       sprintf ( buffer, "h_dN_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
-      h_dN_vs_dpTjet[i*12+iHLTbit]
+      h_dN_vs_dpTjet[i*13+iHLTbit]
 	= new TH2D("h_dN_vs_dpTjet",
 		   buffer,
-		   300, 0., 600., 1000, 0., 100. );
+		   600, 0., 600., 1000, 0., 100. );
       
       sprintf ( buffer, "h_dpT_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < d#Sigmap_{T}/d#phid#eta > / 2 GeV/c (GeV/c / rad)", regions[i].c_str() );
-          h_dpT_vs_dpTjet[i*12+iHLTbit]
+          h_dpT_vs_dpTjet[i*13+iHLTbit]
             = new TH2D("h_dpT_vs_dpTjet",
                        buffer,
-		       300, 0., 600., 1000, 0., 100. );
+		       600, 0., 600., 1000, 0., 100. );
+	  //new
+	  sprintf( buffer,"NChg_%s", regions[i].c_str() );
+	  h_NChg[i*13+iHLTbit]= new TH1D( buffer, "N Chg", 100, 0., 100.);
+	  sprintf( buffer,"pTSChg_%s", regions[i].c_str() );
+	  h_pTSChg[i*13+iHLTbit] = new TH1D( buffer, "pT-Sum 0.1 GeV/c ", 1000, 0., 100.);
+
+	  if(i==1)
+	    {
+	      sprintf ( buffer, "h_dNMax_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	      h_dNMax_vs_dpTjet[iHLTbit]
+		= new TH2D("h_dNMax_vs_dpTjet",
+			   buffer,
+			   600, 0., 600., 1000, 0., 100. );
+	      sprintf ( buffer, "h_dNMin_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dN/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	      h_dNMin_vs_dpTjet[iHLTbit]
+		= new TH2D("h_dNMin_vs_dpTjet",
+			   buffer,
+			   600, 0., 600., 1000, 0., 100. );
+
+	     sprintf ( buffer, "h_dpTMax_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dpT/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	      h_dpTMax_vs_dpTjet[iHLTbit]
+		= new TH2D("h_dpTMax_vs_dpTjet",
+			   buffer,
+			   600, 0., 600., 1000, 0., 100. );
+	      sprintf ( buffer, "h_dpTMin_vs_dpTjet;p_{T}(track jet) (GeV/c);%s < dpT/d#phid#eta > / 2 GeV/c (1/rad)", regions[i].c_str() );
+	      h_dpTMin_vs_dpTjet[iHLTbit]
+		= new TH2D("h_dpTMin_vs_dpTjet",
+			   buffer,
+			   600, 0., 600., 1000, 0., 100. );
+	    }
     }
 
   
@@ -307,6 +484,8 @@ UEActivityHistograms::UEActivityHistograms( const char* fileName, string *trigge
   _xmaxPtChg     = h_pTChg[0]->GetXaxis()->GetXmax();
   _binwidthPtChg = (_xmaxPtChg-_xminPtChg)/double(_nbinsPtChg);
 
+  //  cout<<"_nbinsDphi"<<_nbinsDphi<<"_binwidthDphi"<<_binwidthDphi<<endl;
+
 }
 
 ///
@@ -320,7 +499,7 @@ UEActivityHistograms::fill( UEActivity& activity )
   /// i.e. no trigger bits are available
   ///
 
-  unsigned int iHLTbit( 11 );
+  unsigned int iHLTbit( 12 );
 
   ///
   /// pT-distribution of leading jet
@@ -354,12 +533,15 @@ UEActivityHistograms::fill( UEActivity& activity )
       /// divide y-entry by binwidth
       /// to get density per degree
       double y(666.);
-      y = TH1D_dN_vs_dphi->GetBinContent(i)/_binwidthDphi;
+      y = TH1D_dN_vs_dphi->GetBinContent(i)/(_binwidthDphi*TMath::Pi()/180);
       h_dN_vs_dphi[iHLTbit]->Fill( x, y );
       
-      y = TH1D_dpT_vs_dphi->GetBinContent(i)/_binwidthDphi;
+      y = TH1D_dpT_vs_dphi->GetBinContent(i)/(_binwidthDphi*TMath::Pi()/180);
       h_dpT_vs_dphi[iHLTbit]->Fill( x, y );
-    }
+      //  cout<<_binwidthDphi<<"larghezza del bin"<<endl;
+      //cout<<_xminDphi<<"inizio"<<endl;
+      //  cout<<_nbinsDphi<<"numero bin"<<endl;
+ }
       
 
   ///
@@ -372,8 +554,8 @@ UEActivityHistograms::fill( UEActivity& activity )
       ///
       /// sum up overall activities per unit phi/rad and eta
       ///
-      nChg  += activity.GetNumParticles(iregion) /(TMath::Pi()/3.);
-      pTsum += activity.GetParticlePtSum(iregion)/(TMath::Pi()/3.);
+      nChg  += activity.GetNumParticles(iregion) /(2*TMath::Pi()/3.);
+      pTsum += activity.GetParticlePtSum(iregion)/(2*TMath::Pi()/3.);
 
 
       ///
@@ -382,9 +564,26 @@ UEActivityHistograms::fill( UEActivity& activity )
       /// ( divide activity per eta by phi-range (pi/3)
       ///   to get density per eta and phi in radian ) 
       ///
-      h_dN_vs_dpTjet [iregion*12+iHLTbit]->Fill( pTjet, activity.GetNumParticles(iregion) /(TMath::Pi()/3.) );
-      h_dpT_vs_dpTjet[iregion*12+iHLTbit]->Fill( pTjet, activity.GetParticlePtSum(iregion)/(TMath::Pi()/3.) );
+      h_dN_vs_dpTjet [iregion*13+iHLTbit]->Fill( pTjet, activity.GetNumParticles(iregion) /(2*TMath::Pi()/3.) );
+      h_dpT_vs_dpTjet[iregion*13+iHLTbit]->Fill( pTjet, activity.GetParticlePtSum(iregion)/(2*TMath::Pi()/3.) );
+      
+      //new
+      TH1D* TH1D_NChg = activity.GetTH1D_NChg(iregion);
+      h_NChg[iregion*13+iHLTbit]->Add(TH1D_NChg);
+      TH1D* TH1D_pTSChg = activity.GetTH1D_pTSChg(iregion);
+      h_pTSChg[iregion*13+iHLTbit]->Add(TH1D_pTSChg);
+	
 
+      if(iregion==1)
+	{
+	
+	  h_dNMax_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetNumParticleTraMax()/(TMath::Pi()/3.) );
+	  h_dNMin_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetNumParticleTraMin()/(TMath::Pi()/3.) );
+	
+	
+	  h_dpTMax_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetPtSumParticleTraMax()/(TMath::Pi()/3.) );
+	  h_dpTMin_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetPtSumParticleTraMin()/(TMath::Pi()/3.) );
+	}
 
       ///
       /// pT-distributions of particles/tracks in different regions
@@ -395,7 +594,7 @@ UEActivityHistograms::fill( UEActivity& activity )
 	  double x( _xminPtChg + (i-1)*_binwidthPtChg + _binwidthPtChg/2. );
 	  double y( TH1D_pTChg->GetBinContent(i) / _binwidthPtChg         );
 
-	  h_pTChg[iregion*12+iHLTbit]->Fill( x, y );
+	  h_pTChg[iregion*13+iHLTbit]->Fill( x, y );
 	}
 
     }
@@ -420,7 +619,7 @@ UEActivityHistograms::fill( UEActivity& activity, TClonesArray& acceptedTriggers
   /// 4 Min-Bias (Pixel, Hcal, Ecal, general), Zero-Bias, 6 Jet (30, 50, 80, 110, 180, 250)
   ///
   unsigned int iHLTbit(0);
-  for ( ; iHLTbit<11; ++iHLTbit )
+  for ( ; iHLTbit<12; ++iHLTbit )
     {
       ///
       /// ask if trigger was accepted
@@ -466,10 +665,10 @@ UEActivityHistograms::fill( UEActivity& activity, TClonesArray& acceptedTriggers
 	  /// divide y-entry by binwidth
 	  /// to get density per degree
 	  double y(666.);
-	  y = TH1D_dN_vs_dphi->GetBinContent(i)/_binwidthDphi;
+	  y = TH1D_dN_vs_dphi->GetBinContent(i)/(_binwidthDphi*TMath::Pi()/180);
 	  h_dN_vs_dphi[iHLTbit]->Fill( x, y );
 	  
-	  y = TH1D_dpT_vs_dphi->GetBinContent(i)/_binwidthDphi;
+	  y = TH1D_dpT_vs_dphi->GetBinContent(i)/(_binwidthDphi*TMath::Pi()/180);
 	  h_dpT_vs_dphi[iHLTbit]->Fill( x, y );
 	}
       
@@ -484,8 +683,8 @@ UEActivityHistograms::fill( UEActivity& activity, TClonesArray& acceptedTriggers
 	  ///
 	  /// sum up overall activities per unit phi/rad and eta
 	  ///
-	  nChg  += activity.GetNumParticles(iregion) /(TMath::Pi()/3.);
-	  pTsum += activity.GetParticlePtSum(iregion)/(TMath::Pi()/3.);
+	  nChg  += activity.GetNumParticles(iregion) /(2*TMath::Pi()/3.);
+	  pTsum += activity.GetParticlePtSum(iregion)/(2*TMath::Pi()/3.);
 	  
 	  
 	  ///
@@ -494,9 +693,28 @@ UEActivityHistograms::fill( UEActivity& activity, TClonesArray& acceptedTriggers
 	  /// ( divide activity per eta by phi-range (pi/3)
 	  ///   to get density per eta and phi in radian ) 
 	  ///
-	  h_dN_vs_dpTjet [iregion*12+iHLTbit]->Fill( pTjet, activity.GetNumParticles(iregion) /(TMath::Pi()/3.) );
-	  h_dpT_vs_dpTjet[iregion*12+iHLTbit]->Fill( pTjet, activity.GetParticlePtSum(iregion)/(TMath::Pi()/3.) );
+	  h_dN_vs_dpTjet [iregion*13+iHLTbit]->Fill( pTjet, activity.GetNumParticles(iregion) /(2*TMath::Pi()/3.) );
+	  h_dpT_vs_dpTjet[iregion*13+iHLTbit]->Fill( pTjet, activity.GetParticlePtSum(iregion)/(2*TMath::Pi()/3.) );
+
+//new
+      TH1D* TH1D_NChg = activity.GetTH1D_NChg(iregion);
+      h_NChg[iregion*13+iHLTbit]->Add(TH1D_NChg);
+      TH1D* TH1D_pTSChg = activity.GetTH1D_pTSChg(iregion);
+      h_pTSChg[iregion*13+iHLTbit]->Add(TH1D_pTSChg);
+	  if(iregion==1)
+	{
+	  //cout<<iHLTbit<<endl;
+	  //cout<<activity.GetNumParticleTraMax()<<endl;
+	  //cout<<activity.GetNumParticleTraMin()<<endl;
+	  h_dNMax_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetNumParticleTraMax()/(TMath::Pi()/3.) );
+	  h_dNMin_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetNumParticleTraMin()/(TMath::Pi()/3.) );
+	
 	  
+	  //cout<<activity.GetPtSumParticleTraMax()<<endl;
+	  //cout<<activity.GetPtSumParticleTraMin()<<endl;
+	  h_dpTMax_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetPtSumParticleTraMax()/(TMath::Pi()/3.) );
+	  h_dpTMin_vs_dpTjet[iHLTbit]->Fill( pTjet,activity.GetPtSumParticleTraMin()/(TMath::Pi()/3.) ); 
+	}
 	  
 	  ///
 	  /// pT-distributions of particles/tracks in different regions
@@ -507,7 +725,7 @@ UEActivityHistograms::fill( UEActivity& activity, TClonesArray& acceptedTriggers
 	      double x( _xminPtChg + (i-1)*_binwidthPtChg + _binwidthPtChg/2. );
 	      double y( TH1D_pTChg->GetBinContent(i) / _binwidthPtChg         );
 	      
-	      h_pTChg[iregion*12+iHLTbit]->Fill( x, y );
+	      h_pTChg[iregion*13+iHLTbit]->Fill( x, y );
 	    }
 	  
 	}
