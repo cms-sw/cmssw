bf71b53d82d18a6c51055f09306b14106724790b
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-12-29 00:10:09 +0000

    fix include

--- DPGAnalysis/Skims/src/PixelCountFilter.cc
+++ DPGAnalysis/Skims/src/PixelCountFilter.cc
@@ -8,10 +8,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
-#include "PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h"
+#include "CommonTools/UtilAlgos/interface/ObjectCountFilter.h"
 
  typedef ObjectCountFilter<
            SiPixelClusterCollectionNew
-         > PixelCountFilter;
+         >::type PixelCountFilter;
 
 DEFINE_FWK_MODULE( PixelCountFilter );
