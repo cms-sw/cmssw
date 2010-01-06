7d4dcbe1cb236d6f40eeed3e2d4dd3e377e9521d
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2010-01-06 15:19:05 +0000

    revert PhysicsTools -> CommonTools migration of TFileService include until new version of CommonTools/UtilAlgos/ is in 3_4_X

--- TopQuarkAnalysis/TopObjectResolutions/src/ResolutionCreator.cc
+++ TopQuarkAnalysis/TopObjectResolutions/src/ResolutionCreator.cc
@@ -11,7 +11,8 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 //needed for TFileService
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"   // to be replaced by the following line as soon as
+//#include "CommonTools/UtilAlgos/interface/TFileService.h"  // a new tag of CommonTools/UtilAlgos/ is in 3_4_X
 #include "FWCore/ServiceRegistry/interface/Service.h"
 //needed for MessageLogger
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
