ad074146f749e1498c4524dc833b2c176b9f82a4
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-01-06 23:54:42 +0000

    remove AK5CaloJet label from patJetPartons, as these do not dependent on the jet algo or type

--- PhysicsTools/PatAlgos/python/mcMatchLayer0/jetFlavourId_cff.py
+++ PhysicsTools/PatAlgos/python/mcMatchLayer0/jetFlavourId_cff.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-patAK5CaloJetPartons = cms.EDFilter("PartonSelector",
+patJetPartons = cms.EDFilter("PartonSelector",
     withLeptons = cms.bool(False),
     src = cms.InputTag("genParticles")                            
 )
 
 patAK5CaloJetPartonAssociation = cms.EDFilter("JetPartonMatcher",
     jets    = cms.InputTag("ak5CaloJets"),
-    partons = cms.InputTag("patAK5CaloJetPartons"),
+    partons = cms.InputTag("patJetPartons"),
     coneSizeToAssociate = cms.double(0.3),
 )
 
@@ -17,5 +17,5 @@ patAK5CaloJetFlavourAssociation = cms.EDFilter("JetFlavourIdentifier",
 )
 
 # default PAT sequence for jet flavour identification
-patJetFlavourId = cms.Sequence(patAK5CaloJetPartons * patAK5CaloJetPartonAssociation * patAK5CaloJetFlavourAssociation)
+patJetFlavourId = cms.Sequence(patJetPartons * patAK5CaloJetPartonAssociation * patAK5CaloJetFlavourAssociation)
 
