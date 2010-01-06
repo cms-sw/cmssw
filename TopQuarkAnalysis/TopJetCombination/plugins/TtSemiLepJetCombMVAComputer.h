4c6b5fbb3433b7e8a8bcbc21ee57425912d8cea9
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2010-01-06 13:49:34 +0000

    cleaning up includes

--- TopQuarkAnalysis/TopJetCombination/plugins/TtSemiLepJetCombMVAComputer.h
+++ TopQuarkAnalysis/TopJetCombination/plugins/TtSemiLepJetCombMVAComputer.h
@@ -2,15 +2,7 @@
 #define TtSemiLepJetCombMVAComputer_h
 
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
 
 #include "PhysicsTools/MVAComputer/interface/HelperMacros.h"
 #include "PhysicsTools/MVAComputer/interface/MVAComputerCache.h"
