82b3749e49dc331d6346422cf9e47eba60a45999
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2010-01-07 18:04:33 +0000

    Add more checks for msg validity.

--- EventFilter/StorageManager/src/EndLumiSectMsgData.cc
+++ EventFilter/StorageManager/src/EndLumiSectMsgData.cc
@@ -1,9 +1,11 @@
-// $Id: EndLumiSectMsgData.cc,v 1.1 2009/10/12 13:15:29 dshpakov Exp $
+// $Id: EndLumiSectMsgData.cc,v 1.2 2009/12/17 18:28:58 mommsen Exp $
 
 #include "EventFilter/StorageManager/src/ChainData.h"
 
 #include "interface/evb/version.h"
 #include "interface/evb/i2oEVBMsgs.h"
+#include "interface/shared/i2oXFunctionCodes.h"
+#include "interface/shared/version.h"
 
 
 namespace stor
@@ -12,17 +14,28 @@ namespace stor
   namespace detail
   {
 
-    EndLumiSectMsgData::EndLumiSectMsgData( toolbox::mem::Reference* pRef ):
-      ChainData( pRef ),
+    EndLumiSectMsgData::EndLumiSectMsgData(toolbox::mem::Reference* pRef):
+      #if (INTERFACESHARED_VERSION_MAJOR*1000 + INTERFACESHARED_VERSION_MINOR)>1010
+      ChainData(pRef, I2O_EVM_LUMISECTION),
+      #else
+      ChainData(pRef),
+      #endif
       _runNumber(0),
       _lumiSection(0)
     {
       #if (INTERFACEEVB_VERSION_MAJOR*1000 + INTERFACEEVB_VERSION_MINOR)>1008
 
-      I2O_EVM_END_OF_LUMISECTION_MESSAGE_FRAME* msg_frame
-	= (I2O_EVM_END_OF_LUMISECTION_MESSAGE_FRAME*)( pRef->getDataLocation() );
-      _runNumber = msg_frame->runNumber;
-      _lumiSection = msg_frame->lumiSection;
+      if (validateDataLocation(pRef, INVALID_INITIAL_REFERENCE) &&
+          validateMessageCode(pRef, _i2oMessageCode))
+      {
+        I2O_EVM_END_OF_LUMISECTION_MESSAGE_FRAME* msg_frame =
+          (I2O_EVM_END_OF_LUMISECTION_MESSAGE_FRAME*)( pRef->getDataLocation() );
+        if (msg_frame)
+        {
+          _runNumber = msg_frame->runNumber;
+          _lumiSection = msg_frame->lumiSection;
+        }
+      }
 
       #endif
     }
