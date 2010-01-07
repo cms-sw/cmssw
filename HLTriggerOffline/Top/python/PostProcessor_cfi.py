0d8302e3f1e751b4ab313875bda91d0b81903487
Patricia Lobelle Pardo <sha1-3dd526f680a6b9402d4d1ca132358364c3ecf037@cern.ch>
2010-01-07 16:44:48 +0000

    EDFilter defined as EDAnalyzer

--- HLTriggerOffline/Top/python/PostProcessor_cfi.py
+++ HLTriggerOffline/Top/python/PostProcessor_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-#HLTTopPostVal = cms.EDFilter("DQMGenericClient",
-HLTTopPostProcessor = cms.EDFilter("DQMGenericClient",
+
+HLTTopPostProcessor = cms.EDAnalyzer("DQMGenericClient",
     subDirs        = cms.untracked.vstring('HLT/Top/'),
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
     outputFileName = cms.untracked.string(''),
