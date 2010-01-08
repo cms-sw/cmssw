7c0a0247cbf52c770c4c30bfb43969c8c374026e
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2010-01-08 15:45:42 +0000

    rearranged so to fix untracked vs tracked problem

--- Calibration/EcalAlCaRecoProducers/python/alcastreamEcalPhiSym_cff.py
+++ Calibration/EcalAlCaRecoProducers/python/alcastreamEcalPhiSym_cff.py
@@ -6,16 +6,17 @@
 #
 # Passes events that are coming from the online phi-symmetry stream 
 # 
-# Id: $Id: alcastreamEcalPhiSym_cff.py,v 1.7 2009/03/26 09:01:53 argiro Exp $
+# Id: $Id: alcastreamEcalPhiSym_cff.py,v 1.8 2009/04/23 11:57:48 beaucero Exp $
 #
 
 import FWCore.ParameterSet.Config as cms
+import HLTrigger.HLTfilters.hltHighLevel_cfi
 
-ecalphiSymHLT = cms.EDFilter("HLTHighLevel",
-    HLTPaths = cms.vstring('AlCa_EcalPhiSym*'),
-    andOr = cms.bool(True),
-    throw = cms.untracked.bool(False),
-TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-)
+ecalphiSymHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+  HLTPaths = ['AlCa_EcalPhiSym*'],
+  andOr = True,
+  throw = False
+  )
+
 
 
