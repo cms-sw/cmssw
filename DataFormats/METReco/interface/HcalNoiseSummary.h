35b4208216d341ff7b76450122a6e0adc35f6ae3
John Paul Chou <sha1-d128ca71a1bf075281a5dede2e83919de6462f3d@cern.ch>
2010-01-11 22:39:53 +0000

    added max E(2TS)/E(10TS) and refvectors to noisy calotowers

--- DataFormats/METReco/interface/HcalNoiseSummary.h
+++ DataFormats/METReco/interface/HcalNoiseSummary.h
@@ -14,6 +14,7 @@
 #include "DataFormats/Common/interface/Ref.h"
 #include "DataFormats/Common/interface/RefProd.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
 
 //
 // forward declaration
@@ -72,11 +73,14 @@ class HcalNoiseSummary {
   int num10GeVHits(void) const;
   int num25GeVHits(void) const;
   
-  // E(2TS), E(10TS), and E(2TS)/E(10TS) for the minimum E(2TS)/E(10TS) found in an RBX in the event
+  // E(2TS), E(10TS), and E(2TS)/E(10TS) for the minimum and maximum E(2TS)/E(10TS) found in an RBX in the event
   // the total energy in the RBX must be > 50 GeV
   float minE2TS(void) const;
   float minE10TS(void) const;
   float minE2Over10TS(void) const;
+  float maxE2TS(void) const;
+  float maxE10TS(void) const;
+  float maxE2Over10TS(void) const;
 
   // largest number of zeros found in a single RBX in the event
   // total energy in the RBX must be > 10 GeV
@@ -86,6 +90,9 @@ class HcalNoiseSummary {
   // each hit is >= 1.5 GeV
   int maxHPDHits(void) const;
   int maxRBXHits(void) const;
+
+  // largest number of hits in a single HPD when no other hits are present in the RBX
+  int maxHPDNoOtherHits(void) const;
 
   // smallest EMF found in an HPD/RBX in the event
   // the total energy in the HPD/RBX must be >50 GeV
@@ -98,6 +105,11 @@ class HcalNoiseSummary {
   // reference to problematic jets
   edm::RefVector<reco::CaloJetCollection> problematicJets(void) const;
 
+  // reference to calotowers which fail loose, tight, and high-level noise criteria
+  edm::RefVector<CaloTowerCollection> looseNoiseTowers(void) const;
+  edm::RefVector<CaloTowerCollection> tightNoiseTowers(void) const;
+  edm::RefVector<CaloTowerCollection> highLevelNoiseTowers(void) const;
+
  private:
 
   // data members corresponding to the values above
@@ -107,12 +119,17 @@ class HcalNoiseSummary {
   float min25_, max25_, rms25_;
   int cnthit10_, cnthit25_;
   float mine2ts_, mine10ts_;
+  float maxe2ts_, maxe10ts_;
   int maxzeros_;
-  int maxhpdhits_, maxrbxhits_;
+  int maxhpdhits_, maxhpdhitsnoother_, maxrbxhits_;
   float minhpdemf_, minrbxemf_;
   int nproblemRBXs_;
 
   edm::RefVector<reco::CaloJetCollection> problemjets_;
+
+  edm::RefVector<CaloTowerCollection> loosenoisetwrs_;
+  edm::RefVector<CaloTowerCollection> tightnoisetwrs_;
+  edm::RefVector<CaloTowerCollection> hlnoisetwrs_;
 
 };
 
