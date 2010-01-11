6b83b7184feef5a341567348fdddd7f0a1c5465a
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-01-11 09:49:57 +0000

    Correct/add *cfi.py files for ConfDB py parsing.

--- HLTrigger/HLTfilters/python/hlt1CaloJetEnergy_cfi.py
+++ HLTrigger/HLTfilters/python/hlt1CaloJetEnergy_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 hlt1CaloJetEnergy = cms.EDFilter("HLT1CaloJetEnergy",
-                                 inputTag = cms.InputTag("iterativeCone5CaloJets"),
-                                 MinE = cms.double(30.0),
-                                 MaxEta = cms.double(3.0),
-                                 MinN = cms.int32(1)
+    saveTag = cms.untracked.bool( False ),
+    inputTag = cms.InputTag("iterativeCone5CaloJets"),
+    MinE = cms.double(30.0),
+    MaxEta = cms.double(3.0),
+    MinN = cms.int32(1)
 )
