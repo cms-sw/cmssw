642aca80e1051b1e7429c6c871a7ffef542a6bf8
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-01-12 13:38:51 +0000

    PhysicsTools -> CommonTools migration

--- DQM/Physics/src/EwkMuDQM.cc
+++ DQM/Physics/src/EwkMuDQM.cc
@@ -5,7 +5,7 @@
 #include "DataFormats/Common/interface/Handle.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
