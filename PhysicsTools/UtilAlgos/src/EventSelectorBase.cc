02a134f72b54a9b3a3781f051b6a32ce9f524fe9
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-01-12 05:17:42 +0000

    deprecated

--- PhysicsTools/UtilAlgos/src/EventSelectorBase.cc
+++
@@ -1,6 +0,0 @@
-#include "PhysicsTools/UtilAlgos/interface/EventSelectorBase.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-EDM_REGISTER_PLUGINFACTORY(EventSelectorPluginFactory, "EventSelectorPluginFactory");
-
