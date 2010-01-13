6ca9d206856cfc650d66d83a0599f33d2b626121
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-13 13:47:04 +0000

    added EwkTauDQM module

--- DQM/Physics/plugins/plugins.cc
+++ DQM/Physics/plugins/plugins.cc
@@ -3,6 +3,7 @@
 
 #include "DQM/Physics/src/BPhysicsOniaDQM.h"
 #include "DQM/Physics/src/EwkDQM.h"
+#include "DQM/Physics/src/EwkTauDQM.h"
 #include "DQM/Physics/src/QcdPhotonsDQM.h"
 #include "DQM/Physics/src/QcdLowPtDQM.h"
 #include "DQM/Physics/src/QcdHighPtDQM.h"
@@ -11,6 +12,7 @@
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(BPhysicsOniaDQM);
 DEFINE_ANOTHER_FWK_MODULE(EwkDQM);
+DEFINE_ANOTHER_FWK_MODULE(EwkTauDQM);
 DEFINE_ANOTHER_FWK_MODULE(QcdPhotonsDQM);
 DEFINE_ANOTHER_FWK_MODULE(QcdLowPtDQM);
 DEFINE_ANOTHER_FWK_MODULE(QcdHighPtDQM);
