dc0c49ce6d8947996a70344345efc792c0e243d7
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2010-01-13 11:09:32 +0000

    *** empty log message ***

--- RecoEgamma/EgammaPhotonProducers/python/trackerOnlyConversions_cfi.py
+++ RecoEgamma/EgammaPhotonProducers/python/trackerOnlyConversions_cfi.py
@@ -9,6 +9,8 @@ trackerOnlyConversions = cms.EDProducer('TrackerOnlyConversionProducer',
     bcEndcapCollection = cms.InputTag("multi5x5BasicClusters","multi5x5EndcapBasicClusters"),
     bcBarrelCollection = cms.InputTag("hybridSuperClusters","hybridBarrelBasicClusters"),
 
+    primaryVertexProducer = cms.string('offlinePrimaryVerticesWithBS'),
+
     HalfwayEta = cms.double(.1),# Track pairing search range on Eta
 
     #tight cut
@@ -31,25 +33,27 @@ trackerOnlyConversions = cms.EDProducer('TrackerOnlyConversionProducer',
     dPhiTrackBC = cms.double(1.), # Track-Basic cluster matching, position diff on phi
     EnergyBC = cms.double(0.3), # Track-Basic cluster matching, BC energy lower cut
     EnergyTotalBC = cms.double(.3), # Track-Basic cluster matching, two BC energy summation cut
-    MaxChi2Left = cms.double(5.), #Track quality
-    MaxChi2Right = cms.double(5.),
+    MaxChi2Left = cms.double(10.), #Track quality
+    MaxChi2Right = cms.double(10.),
     MinHitsLeft = cms.int32(5),
-    MinHitsRight = cms.int32(2),
-    DeltaCotTheta = cms.double(.036), #Track pair opening angle on R-Z
-    DeltaPhi = cms.double(.5), #Track pair opening angle on X-Y (not a final selection cut)
+    MinHitsRight = cms.int32(3),
+    DeltaCotTheta = cms.double(.1), #Track pair opening angle on R-Z
+    DeltaPhi = cms.double(.2), #Track pair opening angle on X-Y (not a final selection cut)
     MinApproach = cms.double(-.25), #Track pair min distance at approaching point on X-Y
     maxDistance = cms.double(0.001),#vertex fitting quality
     maxOfInitialValue = cms.double(1.4),#fitting initial value
     maxNbrOfIterations = cms.int32(40),#fitting steps
     rCut = cms.double(2.9),#analytical track cross point
-    dz = cms.double(4.0),#track pair inner position difference
+    dz = cms.double(5.0),#track pair inner position difference
 
+    UsePvtx = cms.bool(True),
+    
     AllowD0 = cms.bool(True), #Allow d0*charge cut
     AllowTrackBC = cms.bool(False), #Allow to match track-basic cluster
-    AllowDeltaCot = cms.bool(False), #Allow pairing using delta cot theta cut
+    AllowDeltaCot = cms.bool(True), #Allow pairing using delta cot theta cut
     AllowMinApproach = cms.bool(True), #Allow pairing using min approach cut
     AllowOppCharge = cms.bool(True), #use opposite charge tracks to pair
     AllowVertex = cms.bool(True),
     AllowSingleLeg = cms.bool(False), #Allow single track conversion
-    AllowRightBC = cms.bool(True) #Require second leg matching basic cluster
+    AllowRightBC = cms.bool(False) #Require second leg matching basic cluster
 )
