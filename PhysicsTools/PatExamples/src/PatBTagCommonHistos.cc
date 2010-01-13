10a85ff52db3f22c939c0d5d553424d0b91c303b
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-01-13 15:49:41 +0000

    include cleanup

--- PhysicsTools/PatExamples/src/PatBTagCommonHistos.cc
+++ PhysicsTools/PatExamples/src/PatBTagCommonHistos.cc
@@ -27,7 +27,7 @@
 #include "PhysicsTools/PatExamples/interface/PatBTagCommonHistos.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 //
 // constructors and destructor
