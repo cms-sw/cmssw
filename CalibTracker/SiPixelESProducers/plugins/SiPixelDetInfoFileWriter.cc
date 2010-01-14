a404fe3eb192246e3ef2ccd4780375098fbcdce9
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2010-01-14 09:36:57 +0000

    beginJob() migration

--- CalibTracker/SiPixelESProducers/plugins/SiPixelDetInfoFileWriter.cc
+++ CalibTracker/SiPixelESProducers/plugins/SiPixelDetInfoFileWriter.cc
@@ -3,7 +3,7 @@
 // Class:      SiPixelDetInfoFileWriter
 // Original Author:  V.Chiochia (adapted from the Strip version by G.Bruno)
 //         Created:  Mon May 20 10:04:31 CET 2007
-// $Id: SiPixelDetInfoFileWriter.cc,v 1.2 2008/01/22 19:15:07 muzaffar Exp $
+// $Id: SiPixelDetInfoFileWriter.cc,v 1.3 2010/01/13 16:25:39 ursl Exp $
 
 #include "CalibTracker/SiPixelESProducers/interface/SiPixelDetInfoFileWriter.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -85,5 +85,9 @@ void SiPixelDetInfoFileWriter::beginRun(const edm::Run &run , const edm::EventSe
 
 
 void SiPixelDetInfoFileWriter::beginJob() {
+
+}
+
+void SiPixelDetInfoFileWriter::analyze(const edm::Event &, const edm::EventSetup &) {
 
 }
