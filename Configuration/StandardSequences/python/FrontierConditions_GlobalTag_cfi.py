c4870b3e1d5037ebeb8338dad4e509dfb6ebbfcb
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-01-14 13:59:11 +0000

    remove untracked params

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
@@ -6,9 +6,5 @@ GlobalTag = cms.ESSource("PoolDBESSource",
     CondDBSetup,
     connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'),
 #    connect = cms.string('sqlite_fip:CondCore/TagCollection/data/GlobalTag.db'), #For use during release integration
-    globaltag = cms.string('UNSPECIFIED::All'),
-    RefreshEachRun=cms.untracked.bool(False),
-    DumpStat=cms.untracked.bool(False),
-    pfnPrefix=cms.untracked.string(''),   
-    pfnPostfix=cms.untracked.string('')
+    globaltag = cms.string('UNSPECIFIED::All')
 )
