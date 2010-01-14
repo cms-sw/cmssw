2d6744d054e4dd721f59b659629fa0c57b9c1d88
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2010-01-14 15:20:16 +0000

    beginJob fix for the current tag in release

--- EventFilter/EcalDigiToRaw/interface/EcalDigiToRaw.h
+++ EventFilter/EcalDigiToRaw/interface/EcalDigiToRaw.h
@@ -15,7 +15,7 @@
 //
 // Original Author:  Emmanuelle Perez
 //         Created:  Sat Nov 25 13:59:51 CET 2006
-// $Id: EcalDigiToRaw.h,v 1.9 2010/01/13 21:55:15 wmtan Exp $
+// $Id: EcalDigiToRaw.h,v 1.6 2008/07/23 18:17:34 franzoni Exp $
 //
 //
 
@@ -63,7 +63,6 @@ class EcalDigiToRaw : public edm::EDProducer {
 	int* GetOrbit() {return &orbit_number_ ;}
 	int* GetBX() {return &bx_ ;}
 	int* GetLV1() {return &lv1_ ;}
-	int* GetFOV() {return &dccFOV_ ;}
 	int* GetRunNumber() {return &runnumber_ ;}
 	bool GetDoBarrel() {return doBarrel_ ;}
 	bool GetDoEndCap() {return doEndCap_ ;}
@@ -87,7 +86,6 @@ class EcalDigiToRaw : public edm::EDProducer {
 	int runnumber_;
 	int bx_;
 	int lv1_;
-        int dccFOV_;
 
 	bool doTCC_;
 	bool doSR_;
