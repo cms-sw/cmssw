f90cfa7db96e49b706cca62137077909bf4a9212
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-01-14 16:41:32 +0000

    fix conflict btw. PAT and fast sim when running in the same job

--- PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/photonIsolation_cff.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 ### Compute isolation values, using POG modules
-from RecoEgamma.EgammaIsolationAlgos.gamIsoDeposits_cff import *
-from RecoEgamma.EgammaIsolationAlgos.gamIsoFromDepsModules_cff  import *
+from RecoEgamma.EgammaIsolationAlgos.gamIsoDeposits_cff import gamIsoDepositTk, gamIsoDepositEcalFromHits, gamIsoDepositHcalFromTowers
+from RecoEgamma.EgammaIsolationAlgos.gamIsoFromDepsModules_cff import gamIsoFromDepsTk, gamIsoFromDepsEcalFromHits, gamIsoFromDepsHcalFromTowers
+
 
 gamIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB")
 gamIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE")
