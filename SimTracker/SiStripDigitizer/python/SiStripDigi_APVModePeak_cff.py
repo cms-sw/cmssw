dd221659c79a723e4680ce91e3faa06a6cc59a81
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2010-01-15 13:40:10 +0000

    \n\e\w\ \e\l\e\c\t\r\o\n\P\e\r\A\d\c\ \v\a\l\u\e\s\ \i\n\ \d\e\c\o\ \m\o\d\e\,\ \f\r\o\m\ \c\r\a\f\t\0\9

--- SimTracker/SiStripDigitizer/python/SiStripDigi_APVModePeak_cff.py
+++ SimTracker/SiStripDigitizer/python/SiStripDigi_APVModePeak_cff.py
@@ -3,5 +3,4 @@ import FWCore.ParameterSet.Config as cms
 # Silicon Strip Digitizer running with APV Mode Peak
 from SimTracker.SiStripDigitizer.SiStripDigi_cfi import *
 simSiStripDigis.APVpeakmode = True
-simSiStripDigis.electronPerAdc = 262.0
-
+simSiStripDigis.electronPerAdc = 262.0 #from CRAFT08; see CFT-09-002 and https://twiki.cern.ch/twiki/bin/view/CMS/TRKTuningPeakMC
