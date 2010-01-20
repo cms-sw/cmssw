232f8589f96105cdd80c9bd27c75fd1de6fb5f57
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2010-01-20 17:39:56 +0000

    For CMSSW_3_5_0_pre3

--- Validation/RecoParticleFlow/Benchmarks/JetBenchmarkSpecific/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/JetBenchmarkSpecific/plot.C
@@ -8,28 +8,28 @@ ROOT::Cintex::Cintex::Enable();
 //InitNicePlot();
 
 gROOT->ProcessLine(".L makeJetResolutionPlot.C");
-Resolution(1,"JetBenchmark_Fast_341.root","JetBenchmark_Fast_Barrel.png","Fast simulation - Barrel resolution");
-Resolution(1,"JetBenchmark_Full_341.root","JetBenchmark_Full_Barrel.png","Full simulation - Barrel resolution");
-Resolution(2,"JetBenchmark_Fast_341.root","JetBenchmark_Fast_Endcap.png","Fast simulation - Endcap resolution");
-Resolution(2,"JetBenchmark_Full_341.root","JetBenchmark_Full_Endcap.png","Full simulation - Endcap resolution");
-//Resolution(3,"JetBenchmark_Fast_341.root","JetBenchmark_Fast_Forward_341.png");
-//Resolution(3,"JetBenchmark_Fast_341.root","JetBenchmark_Fast_Forward_341.png");
+Resolution(1,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Fast_Barrel.png","Fast simulation - Barrel resolution");
+Resolution(1,"JetBenchmark_Full_350pre3.root","JetBenchmark_Full_Barrel.png","Full simulation - Barrel resolution");
+Resolution(2,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Fast_Endcap.png","Fast simulation - Endcap resolution");
+Resolution(2,"JetBenchmark_Full_350pre3.root","JetBenchmark_Full_Endcap.png","Full simulation - Endcap resolution");
+//Resolution(3,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Fast_Forward_350pre3.png");
+//Resolution(3,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Fast_Forward_350pre3.png");
 
 
 gROOT->ProcessLine(".L Compare.C");
-Compare(1,"JetBenchmark_Fast_341.root","JetBenchmark_Full_341.root","BarrelComparison_FastFull.png","Barrel Fast-Full comparison");
-Compare(2,"JetBenchmark_Fast_341.root","JetBenchmark_Full_341.root","EndcapComparison_FastFull.png","Endcap Fast-Full comparison");
-Compare(3,"JetBenchmark_Fast_341.root","JetBenchmark_Full_341.root","ForwardComparison_FastFull.png","Forward Fast-Full comparison");
+Compare(1,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Full_350pre3.root","BarrelComparison_FastFull.png","Barrel Fast-Full comparison");
+Compare(2,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Full_350pre3.root","EndcapComparison_FastFull.png","Endcap Fast-Full comparison");
+Compare(3,"JetBenchmark_Fast_350pre3.root","JetBenchmark_Full_350pre3.root","ForwardComparison_FastFull.png","Forward Fast-Full comparison");
 
 gROOT->ProcessLine(".L ResolutionDirection.C");
-ResolutionDirection(1,"JetBenchmark_Fast_341.root","AngularResolution_Fast_Barrel.png");
-ResolutionDirection(0,"JetBenchmark_Fast_341.root","AngularResolution_Fast_Endcap.png");
-ResolutionDirection(1,"JetBenchmark_Full_341.root","AngularResolution_Full_Barrel.png");
-ResolutionDirection(0,"JetBenchmark_Full_341.root","AngularResolution_Full_Endcap.png");
+ResolutionDirection(1,"JetBenchmark_Fast_350pre3.root","AngularResolution_Fast_Barrel.png");
+ResolutionDirection(0,"JetBenchmark_Fast_350pre3.root","AngularResolution_Fast_Endcap.png");
+ResolutionDirection(1,"JetBenchmark_Full_350pre3.root","AngularResolution_Full_Barrel.png");
+ResolutionDirection(0,"JetBenchmark_Full_350pre3.root","AngularResolution_Full_Endcap.png");
 
 gROOT->ProcessLine(".L TrackMult.C");
-TrackMult("JetBenchmark_Fast_341.root","JetBenchmark_Full_341.root");
+TrackMult("JetBenchmark_Fast_350pre3.root","JetBenchmark_Full_350pre3.root");
 
 gROOT->ProcessLine(".L Fractions.C");
-Fractions("JetBenchmark_Fast_341.root","JetBenchmark_Full_341.root");
+Fractions("JetBenchmark_Fast_350pre3.root","JetBenchmark_Full_350pre3.root");
 }
