092d509fc6b1f96bf1001d8475ac3cf694e48359
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2010-01-21 21:01:57 +0000

    Updated L-S/L+S algorithm to allow different thresholds for long, short, and to allow E or ET cuts

--- RecoLocalCalo/HcalRecAlgos/interface/HcalHFStatusBitFromRecHits.h
+++ RecoLocalCalo/HcalRecAlgos/interface/HcalHFStatusBitFromRecHits.h
@@ -13,8 +13,8 @@
     
    This class sets status bit in the status words for the revised CaloRecHit objets using comparisons between the rec hit energies of long and short fibers for a given HF (ieat, iphi)
     
-   $Date: 2009/03/27 14:47:00 $
-   $Revision: 1.2 $
+   $Date: 2010/01/21 15:43:19 $
+   $Revision: 1.3 $
    \author J. Temple -- University of Maryland and E. Yazgan
 */
 
@@ -22,7 +22,8 @@ class HcalHFStatusBitFromRecHits {
 public:
   /** Full featured constructor for HB/HE and HO (HPD-based detectors) */
   HcalHFStatusBitFromRecHits();
-  HcalHFStatusBitFromRecHits(double HFlongshort, double thresholdET=20.); 
+  HcalHFStatusBitFromRecHits(double shortR, double shortET, double shortE,
+			     double longR, double longET, double longE);
   
   // Destructor
   ~HcalHFStatusBitFromRecHits();
@@ -33,18 +34,31 @@ public:
 			    const HcalSeverityLevelComputer* mySeverity);
 
   // getter functions
-  double hflongshortratio(){return HFlongshortratio_;}
-  double threshold(){return thresholdET_;}
+  double long_hflongshortratio(){return long_HFlongshortratio_;}
+  double long_energythreshold(){return long_thresholdEnergy_;}
+  double long_ETthreshold(){return long_thresholdET_;}
+  double short_hflongshortratio(){return short_HFlongshortratio_;}
+  double short_energythreshold(){return short_thresholdEnergy_;}
+  double short_ETthreshold(){return short_thresholdET_;}
+
   double bit(){return HcalCaloFlagLabels::HFLongShort;}
 
   // setter functions
-  void sethflongshortratio(double x){HFlongshortratio_=x; return;}
-  void setthreshold(double x){thresholdET_=x; return;}
+  void set_long_hflongshortratio(double x){long_HFlongshortratio_=x; return;}
+  void set_long_energythreshold(double x){long_thresholdEnergy_=x; return;}
+  void set_long_ETthreshold(double x){long_thresholdET_=x; return;}
+  void set_short_hflongshortratio(double x){short_HFlongshortratio_=x; return;}
+  void set_short_energythreshold(double x){short_thresholdEnergy_=x; return;}
+  void set_short_ETthreshold(double x){short_thresholdET_=x; return;}
 
 private:
   // variables for cfg files
-  double HFlongshortratio_;
-  double thresholdET_;  // minimum energy needed before the noise algorithm is run
+  double long_HFlongshortratio_;
+  double long_thresholdET_;  // minimum energy needed before the noise algorithm is run
+  double long_thresholdEnergy_;
+  double short_HFlongshortratio_;
+  double short_thresholdET_;  // minimum energy needed before the noise algorithm is run
+  double short_thresholdEnergy_;
 };
 
 #endif
--- RecoLocalCalo/HcalRecAlgos/src/HcalHFStatusBitFromRecHits.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalHFStatusBitFromRecHits.cc
@@ -10,14 +10,23 @@ using namespace std;
 HcalHFStatusBitFromRecHits::HcalHFStatusBitFromRecHits()
 {
   // use simple values in default constructor
-  HFlongshortratio_   = .99;
-  thresholdET_    = 0.5; // default energy requirement
+  long_HFlongshortratio_   = 0.99;
+  short_HFlongshortratio_  = 0.99;
+  long_thresholdET_    = 0.5; // default energy requirement
+  short_thresholdET_   = 0.5;
+  long_thresholdEnergy_ = 100;
+  short_thresholdEnergy_ = 100;
 }
 
-HcalHFStatusBitFromRecHits::HcalHFStatusBitFromRecHits(double HFlongshort,  double thresholdET)
+HcalHFStatusBitFromRecHits::HcalHFStatusBitFromRecHits(double shortR, double shortET, double shortE,
+						       double longR, double longET, double longE)
 {
-  HFlongshortratio_   = HFlongshort;
-  thresholdET_    = thresholdET;
+  long_HFlongshortratio_   = longR; 
+  short_HFlongshortratio_  = shortR;
+  long_thresholdET_        = longET;
+  short_thresholdET_       = shortET;
+  long_thresholdEnergy_    = longE;
+  short_thresholdEnergy_   = shortE;
 }
 
 HcalHFStatusBitFromRecHits::~HcalHFStatusBitFromRecHits(){}
@@ -28,67 +37,74 @@ void HcalHFStatusBitFromRecHits::hfSetFlagFromRecHits(HFRecHitCollection& rec,
 {
   // Compares energies from long & short fibers
   int status;
-  float enL, enS;
+  float en, en2;
   int ieta, iphi, depth;
   float ratio; // ratio of (L-S)/(L+S) energy magnitudes
   double coshEta;
-  // Probably a faster way to do this than a double loop?
+
+  // Is there a faster way to do this than a double loop?
   for (HFRecHitCollection::iterator iHF=rec.begin(); iHF!=rec.end();++iHF)
     {
-      // skip cells that have already been tagged
+      // skip cells that have already been tagged -- shouldn't happen in current algorithm
       if (iHF->flagField(HcalCaloFlagLabels::HFLongShort, HcalCaloFlagLabels::HFLongShort+1)) continue;
-      /* 
-	 Don't run noise algorithm on cells with ET (energy?) < threshold
-	 If energy> threshold and noise algorithm passed, mark both long and short fibers. 
-      */
-      ieta =iHF->id().ieta();  // int between 29-41
-      coshEta=fabs(cosh(0.5*(theHFEtaBounds[abs(ieta)-29]+theHFEtaBounds[abs(ieta)-28])));
-      // requre ET> thresholdET_; ETcosh(eta) = E;
-      if (iHF->energy()<thresholdET_*coshEta) continue;
-      status=0; // status bit for rechit
-      enL=-999; // dummy starting values for long, short fiber energies
-      enS=-999;
 
+      ieta =iHF->id().ieta();  // int between 29-41
+      // eta = average value between cell eta bounds
+      coshEta=fabs(cosh(0.5*(theHFEtaBounds[abs(ieta)-29]+theHFEtaBounds[abs(ieta)-28])));
+
+      status=0; // status bit for rechit
+
+      en2=-999; // dummy starting value for partner energy
       depth=iHF->id().depth();
-      
-      if (depth==1) 
-	enL=iHF->energy();
-      else 
-	enS =iHF->energy();
+      en=iHF->energy(); // energy of current rechit
+
+      if (depth==1) // check long fiber
+	{
+	  if (en<1.2) continue;  // never flag long rechits < 1.2 GeV
+	  if (long_thresholdEnergy_>0. && en<long_thresholdEnergy_) continue;
+	  if (long_thresholdET_>0. && en<long_thresholdET_*coshEta) continue;
+	}
+
+      else if (depth==2) // check short fiber
+	{
+	  if (en<1.8) continue;  // never flag short rechits < 1.8 GeV
+	  if (short_thresholdEnergy_>0. && en<short_thresholdEnergy_) continue;
+	  if (short_thresholdET_>0. && en<short_thresholdET_*coshEta) continue;
+	}
       
       iphi =iHF->id().iphi();
 
       // Check for cells whose partners have been excluded from the rechit collections
+      // Such cells will not get flagged (since we can't make an L vs. S comparison)
+
       HcalDetId partner(HcalForward, ieta, iphi, 3-depth); // if depth=1, 3-depth =2, and vice versa
       DetId detpartner=DetId(partner);
       const HcalChannelStatus* partnerstatus=myqual->getValues(detpartner.rawId());
       if (mySeverity->dropChannel(partnerstatus->getValue() ) ) continue;  // partner was dropped; don't set flag
 
+      // inner loop will find 'partner' channel (same ieta, iphi, different depth)
       for (HFRecHitCollection::iterator iHF2=rec.begin(); iHF2!=rec.end();++iHF2)
 	{
-	  if ((iHF2->id().depth()+depth)!=3) continue; // require short/long combo
 	  if (iHF2->id().ieta()!=ieta) continue; // require ieta match
 	  if (iHF2->id().iphi()!=iphi) continue; // require iphi match
-	  if (iHF2->id().depth()==1)
-	    enL=iHF2->energy();
-	  else
-	    enS=iHF2->energy();
+	  if (iHF2->id().depth()==depth) continue;  // require short/long combo
 
-	  if (enS<1.8 && enL<1.2) break; // energy too low for both; skip 
-	  ratio = ((fabs)(enL) - fabs(enS))/((fabs)(enL)+(fabs)(enS));
+	  en2=iHF2->energy();
+
+	  ratio = ((fabs)(en) - fabs(en2))/((fabs)(en)+(fabs)(en2));
 	  
-	  if (fabs(ratio)>HFlongshortratio_)
-	    {
-	      status=1;
-	      // set flags for both long and short fiber
-	      iHF->setFlagField(status,HcalCaloFlagLabels::HFLongShort, 1);
-              iHF2->setFlagField(status,HcalCaloFlagLabels::HFLongShort, 1);
-	    }	
-	  break;
+	  if (depth==1 && ratio>long_HFlongshortratio_)
+	    status=1;
+	  else if (depth==2 && ratio>short_HFlongshortratio_)
+	    status=1;
+	  break; // once partner channel found, break out of loop
 	} // inner loop
+
       // Consider the case where only one depth present
-      if (enS==-999 && enL>=thresholdET_*coshEta) status=1;
-      else if (enL==-999 && enS>=thresholdET_*coshEta) status=1;
+      if (en2==-999) // outer rechit has already passed energy, ET thresholds above; no partner cell means this looks like isolated energy in one channel -- flag it!
+	status=1;
+
+      // flag rechit as potential PMT window hit
       if (status==1) 
 	iHF->setFlagField(status,HcalCaloFlagLabels::HFLongShort, 1);
     } // outer loop
