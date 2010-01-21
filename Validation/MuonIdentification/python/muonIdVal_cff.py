12a09f0e8f55af8c1700ea914fbf7e6e247c870d
jribnik <sha1-1f7b1d94f19f23f67adbbb4f5dcb8ced97706511@cern.ch>
2010-01-21 22:39:12 +0000

    When including MuonIdDQM turn on the global muon plotting as well by default [in the validation].

--- Validation/MuonIdentification/python/muonIdVal_cff.py
+++ Validation/MuonIdentification/python/muonIdVal_cff.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Validation.MuonIdentification.muonIdVal_cfi import *
 from DQMOffline.Muon.muonIdDQM_cfi import *
 muonIdDQMInVal = muonIdDQM.clone()
+muonIdDQMInVal.useGlobalMuons = cms.untracked.bool(True)
+muonIdDQMInVal.useGlobalMuonsNotTrackerMuons = cms.untracked.bool(True)
 muonIdDQMInVal.baseFolder = muonIdVal.baseFolder
 
 # MuonIdVal
