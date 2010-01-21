9eb12c3cb243f6c73d9886329828b40b7871431f
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-01-21 21:53:49 +0000

    add implementation

--- HLTrigger/HLTfilters/src/TriggerExpressionL1TechReader.cc
+++ HLTrigger/HLTfilters/src/TriggerExpressionL1TechReader.cc
@@ -15,15 +15,75 @@ namespace triggerExpression {
 
 // define the result of the module from the L1 reults
 bool L1TechReader::operator()(const Data & data) {
+  if (not data.hasL1T())
+    return false;
+
+  if (data.l1tConfigurationUpdated())
+    init(data);
+
+  typedef std::pair<std::string, unsigned int> value_type;
+  BOOST_FOREACH(const value_type & trigger, m_triggers)
+    if (data.l1tResults().decisionWord()[trigger.second])
+      return true;
+
   return false;
 }
 
 void L1TechReader::dump(std::ostream & out) const {
-  out << "FALSE";
+  if (m_triggers.size() == 0) {
+    out << "FALSE";
+  } else if (m_triggers.size() == 1) {
+    out << m_triggers[0].first;
+  } else {
+    out << "(" << m_triggers[0].first;
+    for (unsigned int i = 1; i < m_triggers.size(); ++i)
+      out << " OR " << m_triggers[i].first;
+    out << ")";
+  }
 }
 
 void L1TechReader::init(const Data & data) {
+  const L1GtTriggerMenu & menu = data.l1tMenu();
+  const L1GtTriggerMask & mask = data.l1tTechMask();
+
+  // clear the previous configuration
+  m_triggers.clear();
+
+  // check if the pattern has is a glob expression, or a single trigger name 
+  if (not edm::is_glob(m_pattern)) {
+    // no wildcard expression
+    const AlgorithmMap & triggerMap = menu.gtTechnicalTriggerMap();
+    AlgorithmMap::const_iterator entry = triggerMap.find(m_pattern);
+    if (entry != triggerMap.end()) {
+      // single L1 bit
+      m_triggers.push_back( std::make_pair(m_pattern, entry->second.algoBitNumber()) );
+    } else
+      // trigger not found in the current menu
+      if (data.shouldThrow())
+        throw cms::Exception("Configuration") << "requested L1 trigger \"" << m_pattern << "\" does not exist in the current L1 menu";
+      else
+        edm::LogInfo("Configuration") << "requested L1 trigger \"" << m_pattern << "\" does not exist in the current L1 menu";
+  } else {
+    // expand wildcards in the pattern 
+    bool match = false;
+    boost::regex re(edm::glob2reg(m_pattern));
+    const AlgorithmMap & triggerMap = menu.gtTechnicalTriggerMap();
+    BOOST_FOREACH(const AlgorithmMap::value_type & entry, triggerMap)
+      if (boost::regex_match(entry.first, re)) {
+        match = true;
+        if (data.ignoreL1Mask() or (mask.gtTriggerMask()[entry.second.algoBitNumber()] & data.daqPartitions()) == data.daqPartitions())
+          m_triggers.push_back( std::make_pair(entry.first, entry.second.algoBitNumber()) );
+      }
+
+    if (not match) {
+      // m_pattern does not match any L1 bits
+      if (data.shouldThrow())
+        throw cms::Exception("Configuration") << "requested pattern \"" << m_pattern <<  "\" does not match any L1 trigger in the current menu";
+      else
+        edm::LogInfo("Configuration") << "requested pattern \"" << m_pattern <<  "\" does not match any L1 trigger in the current menu";
+    }
+  }
+
 }
 
 } // namespace triggerExpression
-
