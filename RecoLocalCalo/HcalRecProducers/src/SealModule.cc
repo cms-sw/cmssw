fbab50375c110ae729c2daf92d328f1cc18ef560
Edmundo Garcia-Solis <sha1-48a9f9178707b364dbca769ce4c5a7c73a4b2840@cern.ch>
2010-01-21 14:37:13 +0000

    impl of alt zdc reco algo

--- RecoLocalCalo/HcalRecProducers/src/SealModule.cc
+++ RecoLocalCalo/HcalRecProducers/src/SealModule.cc
@@ -1,11 +1,14 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "HcalSimpleReconstructor.h"
+#include "ZdcSimpleReconstructor.h"
 #include "HcalHitReconstructor.h"
-
+#include "ZdcHitReconstructor.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalSimpleReconstructor);
 DEFINE_ANOTHER_FWK_MODULE(HcalHitReconstructor);
+DEFINE_ANOTHER_FWK_MODULE(ZdcSimpleReconstructor);
+DEFINE_ANOTHER_FWK_MODULE(ZdcHitReconstructor);
