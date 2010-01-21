7a9eb9bec884bebcc2fc50b87b5127bc302fd1cc
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2010-01-21 10:53:44 +0000

    better solution: use the delimeter only to indicate empty list

--- CondFormats/HLTObjects/src/AlCaRecoTriggerBits.cc
+++ CondFormats/HLTObjects/src/AlCaRecoTriggerBits.cc
@@ -13,29 +13,31 @@ std::string AlCaRecoTriggerBits::compose(const std::vector<std::string> &paths) 
   for (std::vector<std::string>::const_iterator iPath = paths.begin();
        iPath != paths.end(); ++iPath) {
     if (iPath != paths.begin()) mergedPaths += delimeter_;
+    if (iPath->find(delimeter_) != std::string::npos) {
+      // What to do in CondFormats?
+      // cms::Exception? std::cerr? edm::LogError?
+    }
     mergedPaths += *iPath;
   }
   
-  // Special case: DB cannot store empty strings, so choose a single space for that,
-  // see e.g. https://hypernews.cern.ch/HyperNews/CMS/get/database/674.html .
-  // But of course that means that a single space cannot be specified...
-  if (mergedPaths == " ") {
-    // cms::Exception? std::cerr? edm::LogError? What to do in CondFormats?
-  } else if (mergedPaths.empty()) {
-    mergedPaths = " ";
-  }
+  // Special case: DB cannot store empty strings, see e.g. 
+  // https://hypernews.cern.ch/HyperNews/CMS/get/database/674.html ,
+  // so choose delimeter_ for that - it cannot appear in a path anyway.
+  if (mergedPaths.empty()) mergedPaths = delimeter_;
+
   return mergedPaths;
 }
 
 //_____________________________________________________________________
 std::vector<std::string> AlCaRecoTriggerBits::decompose(const std::string &s) const
 {
-  // decompose 's' into its parts that are separated by ';'
+  // decompose 's' into its parts that are separated by 'delimeter_'
   // (similar as in 
   //  Alignment/CommonAlignmentAlgorithm/src/AlignmentParameterSelector.cc)
 
   std::vector<std::string> result;
-  if (s != " ") { // single space indicates an empty list as DB cannot store empty strings
+  if (!(s.size() == 1 && s[0] == delimeter_)) {
+    // delimeter_ only indicates an empty list as DB cannot store empty strings
     std::string::size_type previousPos = 0;
     while (true) {
       const std::string::size_type delimiterPos = s.find(delimeter_, previousPos);
