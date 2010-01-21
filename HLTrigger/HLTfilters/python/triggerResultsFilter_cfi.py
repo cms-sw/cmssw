05f31e695340979f3ac38baa7935a6534e748ce6
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-01-21 20:54:12 +0000

    add DAQ partition setting

--- HLTrigger/HLTfilters/python/triggerResultsFilter_cfi.py
+++ HLTrigger/HLTfilters/python/triggerResultsFilter_cfi.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 triggerResultsFilter = cms.EDFilter('TriggerResultsFilter',
-    hltResults    = cms.InputTag('TriggerResults','','HLT'),    # HLT results
-    l1tResults    = cms.InputTag('gtDigis'),                    # L1 GT results
+    hltResults    = cms.InputTag('TriggerResults','','HLT'),    # HLT results   - set to empty to ignore HLT
+    l1tResults    = cms.InputTag('gtDigis'),                    # L1 GT results - set to empty to ignore L1
     l1tIgnoreMask = cms.bool(False),                            # use L1 mask
-    throw         = cms.bool(True),                             # throw exception on unknown path names
+    daqPartitions = cms.uint32(0xFF),                           # used by the definition of the L1 mask
+    throw         = cms.bool(True),                             # throw exception on unknown trigger names
     triggerConditions = cms.vstring(
         'HLT_*',
     )
