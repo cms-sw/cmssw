989e083268d95ee5b8646a89459efe67c4a8c578
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2010-01-21 10:59:36 +0000

    cosmetics

--- CondTools/HLT/test/AlCaRecoTriggerBitsRcdRead_cfg.py
+++ CondTools/HLT/test/AlCaRecoTriggerBitsRcdRead_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("READ")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr = cms.untracked.PSet(placeholder = cms.untracked.bool(True))
 process.MessageLogger.cout = cms.untracked.PSet(INFO = cms.untracked.PSet(
-    reportEvery = cms.untracked.int32(1)
+    reportEvery = cms.untracked.int32(1000)
     ))
 
 # the module writing to DB
@@ -29,7 +29,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(250000) )
 # Input for AlCaRecoTriggerBitsRcd,
 # either via GloblalTag (use of _cfi instead of _cff sufficient and faster):
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi")
-process.GlobalTag.globaltag = "DESIGN_3X_V13::All" # choose your tag
+process.GlobalTag.globaltag = "GR09_P_V8_34X::All"#"DESIGN_3X_V13::All" #choose your tag
 
 # ...or specify database and tag:  
 #import CondCore.DBCommon.CondDBSetup_cfi
@@ -40,7 +40,7 @@ process.GlobalTag.globaltag = "DESIGN_3X_V13::All" # choose your tag
 #    connect = cms.string('frontier://FrontierProd/CMS_COND_31X_HLT'),
 #    toGet = cms.VPSet(cms.PSet(
 #        record = cms.string('AlCaRecoTriggerBitsRcd'),
-##        tag = cms.string('AlCaRecoHLTpaths8e29_1e31_v2') # choose tag you want
+##        tag = cms.string('TestTag') # choose tag you want
 #        tag = cms.string('AlCaRecoHLTpaths8e29_1e31_v5_hlt') # choose tag you want
 #
 #        )
