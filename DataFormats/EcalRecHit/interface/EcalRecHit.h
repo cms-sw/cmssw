e0da60e3713a38aa453cbc5a4584448cb428b2e4
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2010-01-21 20:22:23 +0000

    add some more flags: kNoisy, kFake, kKilled, kUnknown

--- DataFormats/EcalRecHit/interface/EcalRecHit.h
+++ DataFormats/EcalRecHit/interface/EcalRecHit.h
@@ -15,16 +15,20 @@ public:
 
   // recHit flags
   enum Flags { 
-          kGood,                     // channel ok, the energy and time measurement are reliable
+          kGood=0,                   // channel ok, the energy and time measurement are reliable
           kPoorReco,                 // the energy is available from the UncalibRecHit, but approximate (bad shape, large chi2)
           kOutOfTime,                // the energy is available from the UncalibRecHit (sync reco), but the event is out of time
           kFaultyHardware,           // The energy is available from the UncalibRecHit, channel is faulty at some hardware level (e.g. noisy)
+          kNoisy,                    // the channel is very noisy
           kPoorCalib,                // the energy is available from the UncalibRecHit, but the calibration of the channel is poor
           kSaturated,                // saturated channel (recovery not tried)
           kLeadingEdgeRecovered,     // saturated channel: energy estimated from the leading edge before saturation
           kNeighboursRecovered,      // saturated/isolated dead: energy estimated from neighbours
           kTowerRecovered,           // channel in TT with no data link, info retrieved from Trigger Primitive
-          kDead                      // channel is dead and any recovery fails
+          kFake,                     // the signal in the channel is a fake (e.g. a so-called spike)
+          kDead,                     // channel is dead and any recovery fails
+          kKilled,                   // MC only flag: the channel is killed in the real detector
+          kUnknown                   // to easy the interface with functions returning flags
   };
 
   EcalRecHit();
