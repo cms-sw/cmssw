6faad664cfe5b1c0a8a968fc677ac0daf6ea8298
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2010-01-21 21:01:31 +0000

    New class inherited from TEveFieldObj, used for track propagation.

---
+++ Fireworks/Core/interface/FWMagField.h
@@ -0,0 +1,56 @@
+#ifndef Fireworks_Tracks_FWMagField_h
+#define Fireworks_Tracks_FWMagField_h
+// -*- C++ -*-
+// 
+// Simplified model of the CMS detector magnetic field
+// 
+// $id$
+#include "TEveTrackPropagator.h"
+class TH1F;
+
+class FWMagField: public TEveMagField
+{
+public:
+   FWMagField();
+   virtual ~FWMagField();
+
+   // get field values
+   virtual TEveVector GetField(Float_t x, Float_t y, Float_t z) const;
+   double GetMaxFieldMag() const;
+
+   // auto/user behaviour
+   void   setUserField(double b) { m_userField = b; }
+   double getUserField() const { return  m_userField; }
+   void   setAutodetect(bool state) { m_autodetect = state; }
+   bool   getAutodetect() const { return m_autodetect; }
+
+   // field model
+   void   setReverseState( bool state ){ m_reverse = state; }
+   bool   isReverse() const { return m_reverse;}
+   void   setSimpleModel( bool simpleModel ){ m_simpleModel = simpleModel; }
+   bool   isSimpleModel() const { return m_simpleModel;}
+
+   // field estimate
+   void   guessFieldIsOn( bool guess ) const;
+   void   guessField( double estimate ) const;
+   void   resetFieldEstimate() const;
+
+private:
+   FWMagField(const FWMagField&); // stop default
+   const FWMagField& operator=(const FWMagField&); // stop default
+
+   bool   m_autodetect;
+   double m_userField;
+
+   bool   m_reverse;
+   bool   m_simpleModel;
+
+   // runtime estimate , have to be mutable becuse of GetField() is const
+   mutable TH1F  *m_guessValHist;
+   mutable int    m_numberOfFieldIsOnEstimates;
+   mutable int    m_numberOfFieldEstimates;
+   mutable bool   m_updateFieldEstimate;
+   mutable double m_guessedField;
+};
+
+#endif
---
+++ Fireworks/Core/src/FWMagField.cc
@@ -0,0 +1,139 @@
+#include <iostream>
+
+#include "TH1F.h"
+#include "Fireworks/Core/interface/FWMagField.h"
+#include "Fireworks/Core/interface/fwLog.h"
+
+
+FWMagField::FWMagField() :
+   TEveMagField(),
+
+   m_autodetect(true),
+   m_userField(3.8),
+
+   m_reverse(true),
+   m_simpleModel(false),
+
+   m_guessValHist(0),
+   m_numberOfFieldIsOnEstimates(0),
+   m_numberOfFieldEstimates(0),
+   m_updateFieldEstimate(true)
+{
+   m_guessValHist = new TH1F("FieldEstimations", "Field estimations from tracks and muons",
+                             200, -4.5, 4.5);
+   m_guessValHist->SetDirectory(0);
+}
+
+FWMagField::~FWMagField()
+{
+   delete m_guessValHist;
+}
+
+//______________________________________________________________________________
+
+TEveVector
+FWMagField::GetField(Float_t x, Float_t y, Float_t z) const
+{
+   // Virtual method of TEveMagField class.
+
+   double R = sqrt(x*x+y*y);
+   double field = m_reverse ? -GetMaxFieldMag() : GetMaxFieldMag();
+
+   //barrel
+   if ( TMath::Abs(z)<724 ){
+    
+      //inside solenoid
+      if ( R < 300) return TEveVector(0,0,field);
+      // outside solinoid
+      if ( m_simpleModel ||
+           ( R>461.0 && R<490.5 ) ||
+           ( R>534.5 && R<597.5 ) ||
+           ( R>637.0 && R<700.0 ) )
+         return TEveVector(0,0,-field/3.8*1.2);
+  
+   } else {
+      // endcaps
+      if (m_simpleModel){
+         if ( R < 50 ) return TEveVector(0,0,field);
+         if ( z > 0 )
+            return TEveVector(x/R*field/3.8*2.0, y/R*field/3.8*2.0, 0);
+         else
+            return TEveVector(-x/R*field/3.8*2.0, -y/R*field/3.8*2.0, 0);
+      }
+      // proper model
+      if ( ( ( TMath::Abs(z)>724 ) && ( TMath::Abs(z)<786 ) ) ||
+           ( ( TMath::Abs(z)>850 ) && ( TMath::Abs(z)<910 ) ) ||
+           ( ( TMath::Abs(z)>975 ) && ( TMath::Abs(z)<1003 ) ) )
+      {
+         if ( z > 0 )
+            return TEveVector(x/R*field/3.8*2.0, y/R*field/3.8*2.0, 0);
+         else
+            return TEveVector(-x/R*field/3.8*2.0, -y/R*field/3.8*2.0, 0);
+      }
+   }
+   return TEveVector(0,0,0);
+}
+
+//______________________________________________________________________________
+
+double
+FWMagField::GetMaxFieldMag() const
+{
+   if ( m_autodetect )
+   {
+      if ( m_updateFieldEstimate )
+      {
+         if ( m_guessValHist->GetEntries() > 2  && m_guessValHist->GetRMS()  < 0.5 )
+         {
+            m_guessedField = m_guessValHist->GetMean();
+            fwLog(fwlog::kDebug) << "FWMagField::GetMaxFieldMag(), get average "
+                                << m_guessValHist->GetMean() << " guessed value: RMS= "<< m_guessValHist->GetRMS()
+                                <<" samples "<< m_guessValHist->GetEntries() << std::endl;
+            //  m_guessedField.Draw("apl");
+         }
+         else if ( m_numberOfFieldIsOnEstimates > m_numberOfFieldEstimates/2 || m_numberOfFieldEstimates == 0 )
+         {
+            m_guessedField = m_userField;
+            fwLog(fwlog::kDebug) << "FWMagField::GetMaxFieldMag() get default field, number estimates "
+                                << m_numberOfFieldEstimates << " number fields is on  m_numberOfFieldIsOnEstimates" <<std::endl;
+         }
+         else
+         {
+            m_guessedField = 0;
+            fwLog(fwlog::kDebug) << "Update field estimate, guess field is OFF." <<std::endl;
+         }
+         m_updateFieldEstimate  = false;
+      }
+      return m_guessedField;
+   }
+   else
+   {
+      return m_userField;   
+   }
+}
+
+//______________________________________________________________________________
+
+void FWMagField::guessFieldIsOn(bool isOn) const
+{
+   if ( isOn ) ++m_numberOfFieldIsOnEstimates;
+   ++m_numberOfFieldEstimates;
+   m_updateFieldEstimate  = true;
+}
+
+void FWMagField::guessField(double val) const
+{
+   fwLog(fwlog::kDebug) <<  "FWMagField::guessField "<< val << std::endl;
+   m_guessValHist->Fill(val);
+   m_updateFieldEstimate = true;   
+}
+
+void FWMagField::resetFieldEstimate() const
+{
+   m_guessValHist->Reset();
+   m_guessValHist->SetAxisRange(-4, 4);
+   m_numberOfFieldIsOnEstimates = 0;
+   m_numberOfFieldEstimates = 0;
+   m_updateFieldEstimate = true;   
+}
+
