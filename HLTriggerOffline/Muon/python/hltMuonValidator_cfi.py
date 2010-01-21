61180827942b4441dd2cb88ab9ea7033439bcf42
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2010-01-21 18:13:23 +0000

    Modified regexp to exclude HighMultiplicity triggers

--- HLTriggerOffline/Muon/python/hltMuonValidator_cfi.py
+++ HLTriggerOffline/Muon/python/hltMuonValidator_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hltMuonValidator = cms.EDAnalyzer("HLTMuonValidator",
 
     hltProcessName = cms.string("HLT"),
-    hltPathsToCheck = cms.vstring("HLT_[^_]*Mu[^_]*$"),
+    hltPathsToCheck = cms.vstring("HLT_[^H_]*Mu[^_]*$"),
 
     parametersTurnOn = cms.vdouble(0,
                                    1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
