4a505ad5d5805dd9f36d453cff7cdcb7bbfcfef2
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2010-01-22 15:18:32 +0000

    Fixing python file

--- DQMOffline/Trigger/python/HLTTauCertifier_cfi.py
+++ DQMOffline/Trigger/python/HLTTauCertifier_cfi.py
@@ -3,12 +3,13 @@ import FWCore.ParameterSet.Config as cms
 HLTTauCertification = cms.EDFilter("HLTTauCertifier",
                                    targetDir = cms.string("EventInfo"),
                                    targetME  = cms.string("HLT_Tau"),
-                                   inputMEs_ = cms.vstring(
-                                      "HLT/TauOffline/Photons/DoubleTau/EfficiencyRefPrevious",
-                                      "HLT/TauOffline/Photons/SingleTau/EfficiencyRefPrevious"
+                                   inputMEs = cms.vstring(
+                                      "HLT/TauOffline/Inclusive/DoubleLooseIsoTauTau/TriggerBits",
+                                      "HLT/TauOffline/Inclusive/DoubleLooseIsoTauTau/TriggerBits",
+
                                    ),
                                    setBadRunOnWarnings = cms.bool(False),
-                                   setBadRunOnErrors   = cms.bool(True),
+                                   setBadRunOnErrors   = cms.bool(True)
 )
 
 
