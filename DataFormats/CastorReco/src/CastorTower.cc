970d18e6073a9b4f26028e9c88d89ba89deafe5e
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2010-01-22 14:15:18 +0000

    CastorTower based on Candidate Committing in . CVS: CVS: Modified Files: CVS:  CastorTower.cc CVS: ----------------------------------------------------------------------

--- DataFormats/CastorReco/src/CastorTower.cc
+++ DataFormats/CastorReco/src/CastorTower.cc
@@ -4,7 +4,7 @@ using namespace reco;
 
 CastorTower::CastorTower(const double energy, const ROOT::Math::XYZPoint& position, const double emEnergy, const double hadEnergy,
 			 const double fem, const double depth, const double fhot, 
-			 const CastorCellRefVector& usedCells){
+			 const CastorCellRefVector& usedCells) : LeafCandidate(0,LorentzVector(math::PtEtaPhiMLorentzVector(energy*sin(position.theta()),position.eta(),position.phi(),0)),Point(0,0,0)) {
 
   position_ = position;
   energy_ = energy;
