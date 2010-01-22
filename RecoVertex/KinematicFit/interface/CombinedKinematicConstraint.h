b2edde05d948754bc8f53447779802c2f6da04a1
Lars Perchalla <sha1-33d4055c8560ac1d30535bc0038854133dd249fa@cern.ch>
2010-01-22 18:25:16 +0000

    small fix

--- RecoVertex/KinematicFit/interface/CombinedKinematicConstraint.h
+++ RecoVertex/KinematicFit/interface/CombinedKinematicConstraint.h
@@ -6,15 +6,16 @@
 #include "DataFormats/CLHEP/interface/AlgebraicObjects.h"
 #include "RecoVertex/VertexPrimitives/interface/VertexException.h"
 
-
-// This class combines several user defined constraints (by expanding the vector d and the matrices D and E).
-// Usage:
-// Add each constraint to a std::vector<MultiTrackKinematicConstraint* >.
-// This vector has to be used in the constructor:
-// MultiTrackKinematicConstraint *combiC = new CombinedKinematicConstraint(std::vector<MultiTrackKinematicConstraint* >)
-// The produced object can be used by KinematicConstrainedVertexFitter.fit()
-//
-// Lars Perchalla, Philip Sauerland, Dec 2009
+/**
+ * This class combines several user defined constraints (by expanding the vector d and the matrices D and E).
+ * Usage:
+ * Add each constraint to a std::vector<MultiTrackKinematicConstraint* >.
+ * This vector has to be used in the constructor:
+ * MultiTrackKinematicConstraint *combiC = new CombinedKinematicConstraint(std::vector<MultiTrackKinematicConstraint* >)
+ * The produced object can be used by KinematicConstrainedVertexFitter.fit()
+ *
+ * Lars Perchalla, Philip Sauerland, Dec 2009
+ */
 
 class CombinedKinematicConstraint : public MultiTrackKinematicConstraint{
 	
