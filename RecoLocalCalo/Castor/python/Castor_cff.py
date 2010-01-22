ef1e0b9632450468051db99870eef0d7a3d8b434
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2010-01-22 14:07:40 +0000

    update general chain

--- RecoLocalCalo/Castor/python/Castor_cff.py
+++ RecoLocalCalo/Castor/python/Castor_cff.py
@@ -2,15 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.Castor.CastorCellReco_cfi import *
 from RecoLocalCalo.Castor.CastorTowerReco_cfi import *
-from RecoLocalCalo.Castor.CastorTowerCandidateReco_cfi import *
 from RecoLocalCalo.Castor.CastorClusterReco_cfi import *
 from RecoLocalCalo.Castor.CastorJetEgammaReco_cfi import *
 from RecoLocalCalo.Castor.CastorFastjetReco_cfi import * 
 
-#CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone+CastorFastjetRecoKtGen+CastorFastjetRecoSISConeGen)
-CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone)
-CastorClusterReco = cms.Sequence(CastorClusterRecoCustomKt+CastorClusterRecoKt+CastorClusterRecoSISCone)
-CastorJetEgammaReco = cms.Sequence(CastorJetEgammaRecoCustomKt+CastorJetEgammaRecoKt+CastorJetEgammaRecoSISCone)
+CastorFastjetRecoAntiKt07.src	   = cms.InputTag("CastorTowerReco")
+CastorFastjetRecoAntiKt07.jetType      = cms.string("BasicJet")
+# minimum jet pt
+CastorFastjetRecoAntiKt07.jetPtMin       = cms.double(0.0)
+# minimum calo tower input et
+CastorFastjetRecoAntiKt07.inputEtMin     = cms.double(0.0)
+# minimum calo tower input energy
+CastorFastjetRecoAntiKt07.inputEMin      = cms.double(0.0)
+# primary vertex correction
+CastorFastjetRecoAntiKt07.doPVCorrection = cms.bool(False)
 
-CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*CastorTowerCandidateReco*CastorFastjetReco*CastorClusterReco*CastorJetEgammaReco)
+#CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone+CastorFastjetRecoKtGen+CastorFastjetRecoSISConeGen)
+#CastorFastjetReco = cms.Sequence(CastorFastjetRecoKt+CastorFastjetRecoSISCone)
+#CastorClusterReco = cms.Sequence(CastorClusterRecoCustomKt+CastorClusterRecoKt+CastorClusterRecoSISCone)
+#CastorJetEgammaReco = cms.Sequence(CastorJetEgammaRecoCustomKt+CastorJetEgammaRecoKt+CastorJetEgammaRecoSISCone)
+
+CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*CastorFastjetRecoAntiKt07*CastorClusterRecoAntiKt07*CastorJetEgammaRecoAntiKt07)
 
