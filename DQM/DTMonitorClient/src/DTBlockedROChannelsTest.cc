dc8d4c47a306af07de47973a51ad28786a81e042
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2010-01-22 17:18:38 +0000

    removed perLS plot for 34X fix

--- DQM/DTMonitorClient/src/DTBlockedROChannelsTest.cc
+++ DQM/DTMonitorClient/src/DTBlockedROChannelsTest.cc
@@ -2,8 +2,8 @@
 /*
  * \file DTBlockedROChannelsTest.cc
  * 
- * $Date: 2010/01/05 10:15:45 $
- * $Revision: 1.7 $
+ * $Date: 2009/10/19 16:18:47 $
+ * $Revision: 1.6 $
  * \author G. Cerminara - University and INFN Torino
  *
  */
@@ -51,7 +51,7 @@ DTBlockedROChannelsTest::~DTBlockedROChannelsTest() {
 
 
 // book histos
-void DTBlockedROChannelsTest::beginJob() {
+void DTBlockedROChannelsTest::beginJob(const EventSetup& context) {
   LogTrace("DTDQM|DTRawToDigi|DTMonitorClient|DTBlockedROChannelsTest")
     << "[DTBlockedROChannelsTest]: BeginJob";
 
@@ -248,21 +248,22 @@ void DTBlockedROChannelsTest::endJob(){
 
 void DTBlockedROChannelsTest::endRun(edm::Run const& run, edm::EventSetup const& eSetup) {
 
-  if(offlineMode) {
-    // fill a trend plot based on the results stored in the map
-    float fBin = resultsPerLumi.begin()->first;
-    float lBin = resultsPerLumi.rbegin()->first;
-    dbe->setCurrentFolder("DT/00-ROChannels");
+//     Commeneted to fix almost infinite loop on MC
+//   if(offlineMode) {
+//     // fill a trend plot based on the results stored in the map
+//     float lBin = resultsPerLumi.rbegin()->first;
+//     dbe->setCurrentFolder("DT/00-ROChannels");
   
-    //   MonitorElement* hSystFractionVsLS =  dbe->book1D("EnabledROChannelsVsLS", "% RO channels vs LS", nBins,fBin,lBin);
-    hSystFractionVsLS = new DTTimeEvolutionHisto(dbe, "EnabledROChannelsVsLS", "% RO channels",
-						 (int)lBin-(int)fBin, fBin, 1, false, 2);
+//     //   MonitorElement* hSystFractionVsLS =  dbe->book1D("EnabledROChannelsVsLS", "% RO channels vs LS", nBins,fBin,lBin);
+
+//     hSystFractionVsLS = new DTTimeEvolutionHisto(dbe, "EnabledROChannelsVsLS", "% RO channels",
+// 						 (int)lBin, 1, false, 2);
 								 
-    for(map<int, double>::const_iterator bin = resultsPerLumi.begin();
-	bin != resultsPerLumi.end(); ++bin) {
-      hSystFractionVsLS->setTimeSlotValue((*bin).second, (*bin).first);
-    }
-  }
+//     for(map<int, double>::const_iterator bin = resultsPerLumi.begin();
+// 	bin != resultsPerLumi.end(); ++bin) {
+//       hSystFractionVsLS->setTimeSlotValue((*bin).second, (*bin).first);
+//     }
+//   }
 }
 
 
