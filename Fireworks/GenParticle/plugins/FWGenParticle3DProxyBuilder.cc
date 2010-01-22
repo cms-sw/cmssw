9052d7a38cd06cce10fdaa9270abd38b50516971
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2010-01-22 19:52:57 +0000

    Use default track propagator from Context.

--- Fireworks/GenParticle/plugins/FWGenParticle3DProxyBuilder.cc
+++ Fireworks/GenParticle/plugins/FWGenParticle3DProxyBuilder.cc
@@ -14,99 +14,76 @@
 //
 // Original Author:
 //         Created:  Thu Dec  6 18:01:21 PST 2007
-// $Id: FWGenParticle3DProxyBuilder.cc,v 1.2 2009/05/26 21:35:46 yanjuntu Exp $
+// $Id: FWGenParticle3DProxyBuilder.cc,v 1.1 2009/05/27 00:39:17 yanjuntu Exp $
 //
 
 // system include files
 #include "TEveManager.h"
 #include "TEveTrack.h"
-#include "TEveTrackPropagator.h"
 #include "RVersion.h"
 #include "TDatabasePDG.h"
-
-// user include files
-
+#include "TEveVSDStructs.h"
+#include "TRandom.h"
+#include "TEveStraightLineSet.h"
+#include "TEveVSDStructs.h"
 
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/FW3DDataProxyBuilder.h"
+
 
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
 
+class TEveTrack;
+class TEveTrackPropagator;
 
-class FWGenParticle3DProxyBuilder : public FW3DDataProxyBuilder {
+class FWGenParticle3DProxyBuilder : public  FW3DDataProxyBuilder {
 
 public:
    FWGenParticle3DProxyBuilder();
-   virtual ~FWGenParticle3DProxyBuilder() {
-   }
-
-   // ---------- const member functions ---------------------
-
-   // ---------- static member functions --------------------
+   virtual ~FWGenParticle3DProxyBuilder() {}
 
    // ---------- member functions ---------------------------
    REGISTER_PROXYBUILDER_METHODS();
 
 private:
-   virtual void build(const FWEventItem* iItem, TEveElementList** product);
+   FWGenParticle3DProxyBuilder(const FWGenParticle3DProxyBuilder&); // stop default
 
-   FWGenParticle3DProxyBuilder(const FWGenParticle3DProxyBuilder&);    // stop default
+   const FWGenParticle3DProxyBuilder& operator=(const FWGenParticle3DProxyBuilder&); // stop default
 
-   const FWGenParticle3DProxyBuilder& operator=(const FWGenParticle3DProxyBuilder&);    // stop default
+   virtual void build(const FWEventItem* iItem,
+                      TEveElementList** product);
 
    // ---------- member data --------------------------------
    TDatabasePDG* m_pdg;
 };
 
+//______________________________________________________________________________
+
 FWGenParticle3DProxyBuilder::FWGenParticle3DProxyBuilder()
+ :m_pdg(0)
 {
    m_pdg = new TDatabasePDG();
 }
 
 void FWGenParticle3DProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
-{
-   //since we created it, we know the type (would like to do this better)
-   TEveTrackList* tlist = dynamic_cast<TEveTrackList*>(*product);
-   if ( !tlist && *product ) {
-      std::cout << "incorrect type" << std::endl;
-      return;
-   }
-
-   if(0 == tlist) {
-      tlist =  new TEveTrackList(iItem->name().c_str());
-      *product = tlist;
-      tlist->SetMainColor(iItem->defaultDisplayProperties().color());
-      TEveTrackPropagator* rnrStyle = tlist->GetPropagator();
-      //units are Tesla
-      rnrStyle->SetMagField( -4.0);
-      //get this from geometry, units are CM
-      rnrStyle->SetMaxR(120.0);
-      rnrStyle->SetMaxZ(300.0);
-
-      gEve->AddElement(tlist);
-   } else {
-      tlist->DestroyElements();
-   }
-
-
+{ 
    reco::GenParticleCollection const * genParticles=0;
    iItem->get(genParticles);
-   //fwlite::Handle<reco::TrackCollection> tracks;
-   //tracks.getByLabel(*iEvent,"ctfWithMaterialTracks");
-
    if(0 == genParticles ) return;
 
-   TEveTrackPropagator* rnrStyle = tlist->GetPropagator();
+   TEveElementList* tlist = *product;
+   if(tlist == 0) {
+      tlist = new TEveElementList();
+      *product = tlist;
+   }
 
    int index=0;
-   //cout <<"----"<<endl;
    TEveRecTrack t;
-
    t.fBeta = 1.;
    reco::GenParticleCollection::const_iterator it = genParticles->begin(),
-                                               end = genParticles->end();
+      end = genParticles->end();
    for( ; it != end; ++it,++index) {
       t.fP = TEveVector(it->px(),
                         it->py(),
@@ -115,7 +92,8 @@ void FWGenParticle3DProxyBuilder::build(const FWEventItem* iItem, TEveElementLis
                         it->vy(),
                         it->vz());
       t.fSign = it->charge();
-      TEveTrack* genPart = new TEveTrack(&t,rnrStyle);
+
+      TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
      
       char s[1024];
       TParticlePDG* pID = m_pdg->GetParticle(it->pdgId());
@@ -123,20 +101,13 @@ void FWGenParticle3DProxyBuilder::build(const FWEventItem* iItem, TEveElementLis
          sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), it->pt());
       else
          sprintf(s,"gen pdg %d, Pt: %0.1f GeV", it->pdgId(), it->pt());
-      genPart->SetTitle(s);
-      genPart->SetMainColor(iItem->defaultDisplayProperties().color());
-      genPart->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
-      genPart->SetRnrChildren(iItem->defaultDisplayProperties().isVisible());
-     
-      genPart->MakeTrack();
-      tlist->AddElement(genPart);
-     
-      // std::cout << it->px()<<" "
-// 	   <<it->py()<<" "
-// 	   <<it->pz()<<std::endl;
-//       std::cout <<" *";
-   }
 
+      track->SetMainColor(iItem->defaultDisplayProperties().color());
+      track->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
+      track->SetTitle(s);
+      track->MakeTrack();
+      tlist->AddElement(track);
+   }
 }
 
 REGISTER_FW3DDATAPROXYBUILDER(FWGenParticle3DProxyBuilder,reco::GenParticleCollection,"GenParticles");
--- Fireworks/GenParticle/plugins/FWGenParticleRPZProxyBuilder.cc
+++ Fireworks/GenParticle/plugins/FWGenParticleRPZProxyBuilder.cc
@@ -14,15 +14,16 @@
 //
 // Original Author:
 //         Created:  Thu Dec  6 18:01:21 PST 2007
-// $Id: FWGenParticleRPZProxyBuilder.cc,v 1.1 2009/01/23 11:10:19 amraktad Exp $
+// $Id: FWGenParticleRPZProxyBuilder.cc,v 1.2 2009/01/23 21:35:46 amraktad Exp $
 //
 
 // system include files
 #include "TEveManager.h"
 #include "TEveTrack.h"
-#include "TEveTrackPropagator.h"
+#include "TEveVSDStructs.h"
 #include "RVersion.h"
 #include "TDatabasePDG.h"
+#include "TEveVSDStructs.h"
 
 // user include files
 #include "Fireworks/Core/interface/FWRPZDataProxyBuilder.h"
@@ -67,46 +68,21 @@ FWGenParticleRPZProxyBuilder::FWGenParticleRPZProxyBuilder()
 
 void FWGenParticleRPZProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
 {
-   //since we created it, we know the type (would like to do this better)
-   TEveTrackList* tlist = dynamic_cast<TEveTrackList*>(*product);
-   if ( !tlist && *product ) {
-      std::cout << "incorrect type" << std::endl;
-      return;
-   }
-
-   if(0 == tlist) {
-      tlist =  new TEveTrackList(iItem->name().c_str());
-      *product = tlist;
-      tlist->SetMainColor(iItem->defaultDisplayProperties().color());
-      TEveTrackPropagator* rnrStyle = tlist->GetPropagator();
-      //units are Tesla
-      rnrStyle->SetMagField( -4.0);
-      //get this from geometry, units are CM
-      rnrStyle->SetMaxR(120.0);
-      rnrStyle->SetMaxZ(300.0);
-
-      gEve->AddElement(tlist);
-   } else {
-      tlist->DestroyElements();
-   }
-
-
    reco::GenParticleCollection const * genParticles=0;
    iItem->get(genParticles);
-   //fwlite::Handle<reco::TrackCollection> tracks;
-   //tracks.getByLabel(*iEvent,"ctfWithMaterialTracks");
-
    if(0 == genParticles ) return;
 
-   TEveTrackPropagator* rnrStyle = tlist->GetPropagator();
+   TEveElementList* tlist = *product;
+   if(tlist == 0) {
+      tlist = new TEveElementList();
+      *product = tlist;
+   }
 
    int index=0;
-   //cout <<"----"<<endl;
    TEveRecTrack t;
-
    t.fBeta = 1.;
    reco::GenParticleCollection::const_iterator it = genParticles->begin(),
-                                               end = genParticles->end();
+      end = genParticles->end();
    for( ; it != end; ++it,++index) {
       t.fP = TEveVector(it->px(),
                         it->py(),
@@ -115,24 +91,22 @@ void FWGenParticleRPZProxyBuilder::build(const FWEventItem* iItem, TEveElementLi
                         it->vy(),
                         it->vz());
       t.fSign = it->charge();
-      TEveTrack* genPart = new TEveTrack(&t,rnrStyle);
+
+      TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
+     
       char s[1024];
       TParticlePDG* pID = m_pdg->GetParticle(it->pdgId());
       if ( pID )
          sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), it->pt());
       else
          sprintf(s,"gen pdg %d, Pt: %0.1f GeV", it->pdgId(), it->pt());
-      genPart->SetTitle(s);
-      genPart->SetMainColor(iItem->defaultDisplayProperties().color());
-      genPart->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
-      genPart->SetRnrChildren(iItem->defaultDisplayProperties().isVisible());
-      gEve->AddElement(genPart,tlist);
-      //cout << it->px()<<" "
-      //   <<it->py()<<" "
-      //   <<it->pz()<<endl;
-      //cout <<" *";
-   }
 
+      track->SetMainColor(iItem->defaultDisplayProperties().color());
+      track->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
+      track->SetTitle(s);
+      track->MakeTrack();
+      tlist->AddElement(track);
+   }
 }
 
 REGISTER_FWRPZDATAPROXYBUILDERBASE(FWGenParticleRPZProxyBuilder,reco::GenParticleCollection,"GenParticles");
