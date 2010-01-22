9c1bb1948d6b903d17e66e6a48f962090595e77d
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2010-01-22 16:27:48 +0000

    typos fixed

--- RecoLocalCalo/Configuration/python/RecoLocalCalo_EventContentCosmics_cff.py
+++ RecoLocalCalo/Configuration/python/RecoLocalCalo_EventContentCosmics_cff.py
@@ -11,8 +11,8 @@ RecoLocalCaloFEVT = cms.PSet(
         'keep *_horeco_*_*',
         'keep HBHERecHitsSorted_hbherecoMB_*_*',
         'keep HORecHitsSorted_horecoMB_*_*',
-        'keep HFRecHitsSorted_hfrecoMB_*_*'
-         keep ZDCDataFramesSorted_*Digis_*_*',
+        'keep HFRecHitsSorted_hfrecoMB_*_*',
+        'keep ZDCDataFramesSorted_*Digis_*_*',
         'keep ZDCRecHitsSorted_*_*_*'
         )
 )
@@ -23,7 +23,7 @@ RecoLocalCaloRECO = cms.PSet(
         'keep *_horeco_*_*',
         'keep HBHERecHitsSorted_hbherecoMB_*_*',
         'keep HORecHitsSorted_horecoMB_*_*',
-        'keep HFRecHitsSorted_hfrecoMB_*_*'
+        'keep HFRecHitsSorted_hfrecoMB_*_*',
         'keep ZDCDataFramesSorted_*Digis_*_*',
         'keep ZDCRecHitsSorted_*_*_*' 
         )
