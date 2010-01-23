09d205a6228789a5e69816491b924fa298cc3cbd
Jared Sturdy <sha1-db53c096984bc383f729541321a2e7ec9133743e@cern.ch>
2010-01-23 08:29:27 +0000

    Removed BeamHaloAnalyzer from process path

--- DQMOffline/JetMET/python/jetMETDQMOfflineSourceCosmic_cff.py
+++ DQMOffline/JetMET/python/jetMETDQMOfflineSourceCosmic_cff.py
@@ -4,5 +4,5 @@ from DQMOffline.JetMET.jetMETAnalyzerCosmic_cff import *
 from DQMOffline.JetMET.caloTowers_cff           import *
 from DQMOffline.JetMET.BeamHaloAnalyzer_cfi     import *
 
-jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerCosmicSequence)
-#jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerCosmicSequence)
+#jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerCosmicSequence)
+jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerCosmicSequence)
