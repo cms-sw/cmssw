188bb6527cb45fe8b71da859bcdb828715faefe0
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2010-01-25 08:26:54 +0000

    Removed (commented) the ES producer - configuration will be taken from the global tag.

--- SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_cff.py
+++ SimCalorimetry/EcalTrigPrimProducers/python/ecalTriggerPrimitiveDigis_cff.py
@@ -2,6 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 # Trigger Primitive Producer
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cfi import *
+
+
 # esmodule creating  records + corresponding empty essource
-from SimCalorimetry.EcalTrigPrimProducers.ecalTrigPrimESProducer_cff import *
+# when commented, one takes the configuration from the global tag
+#
+#from SimCalorimetry.EcalTrigPrimProducers.ecalTrigPrimESProducer_cff import *
 
