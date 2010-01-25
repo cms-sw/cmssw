55a1c7bb222437b798325e4df61fb8488c777229
Frank Golf <sha1-e884805f6c312aa2407c5005ebbb36cbb0c23396@cern.ch>
2010-01-25 15:51:49 +0000

    updated tcMET value map, now uses default fit in most cases (which is tracker fit)

--- JetMETCorrections/Type1MET/interface/MuonTCMETValueMapProducer.h
+++ JetMETCorrections/Type1MET/interface/MuonTCMETValueMapProducer.h
@@ -21,10 +21,8 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/Common/interface/Handle.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EventSetup.h"
+//#include "FWCore/Framework/interface/ESHandle.h"
+//#include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/MuonReco/interface/MuonFwd.h"
 #include "DataFormats/BeamSpot/interface/BeamSpot.h"
@@ -37,7 +35,9 @@ class MuonTCMETValueMapProducer : public edm::EDProducer {
    public:
       explicit MuonTCMETValueMapProducer(const edm::ParameterSet&);
       ~MuonTCMETValueMapProducer();
-      TH2D* getResponseFunction ( );
+
+      TH2D* getResponseFunction_fit ( );
+      TH2D* getResponseFunction_mode ( );
 
    private:
       virtual void beginJob() ;
@@ -56,26 +56,32 @@ class MuonTCMETValueMapProducer : public edm::EDProducer {
 
       class TH2D* response_function;
 
+      bool muonGlobal_;
+      bool muonTracker_;
+      bool useCaloMuons_;
+
+      int rfType_;
+
       double  minpt_;
       double  maxpt_;
       double  maxeta_;
       double  maxchi2_;
       double  minhits_;
       double  maxd0_;
+      double  maxPtErr_;
+      std::vector<int> trkQuality_;
+      std::vector<int> trkAlgos_;
 
       double  muonpt_;
       double  muoneta_;
       double  muonchi2_;
       double  muonhits_;
       double  muond0_;
-      bool    muonGlobal_ ;
-      bool    muonTracker_;
-      double  muonQoverPerror_;
-      double  muondeltaPt_;
+      double  muonDeltaR_;
 
       //functions
       bool isGoodMuon( const reco::Muon* );
-      bool isGoodGlobalMuon( const reco::Muon* );
+      bool isGoodCaloMuon( const reco::Muon*, const int );
       bool isGoodTrack( const reco::Muon* );
       class TVector3 propagateTrack( const reco::Muon* );
   };
