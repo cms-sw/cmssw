c421739e2cdbf15f19ad59b72c1468434363c86f
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2010-01-25 20:42:20 +0000

    first release RPCPointProducer

---
+++ RecoLocalMuon/RPCRecHit/interface/RPCPointProducer.h
@@ -0,0 +1,42 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <DataFormats/DTRecHit/interface/DTRecSegment4DCollection.h>
+#include <DataFormats/CSCRecHit/interface/CSCSegmentCollection.h>
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include <DataFormats/RPCRecHit/interface/RPCRecHit.h>
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+#include "RecoLocalMuon/RPCRecHit/interface/DTSegtoRPC.h"
+#include "RecoLocalMuon/RPCRecHit/interface/CSCSegtoRPC.h"
+
+//
+// class decleration
+//
+
+class RPCPointProducer : public edm::EDProducer {
+   public:
+      explicit RPCPointProducer(const edm::ParameterSet&);
+      ~RPCPointProducer();
+      std::string dt4DSegments;
+      std::string cscSegments;
+   private:
+      virtual void beginJob() ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      bool incldt;
+      bool inclcsc;
+      bool debug;
+      double MinCosAng;
+      double MaxD;
+      double MaxDrb4;
+      double MaxDistanceBetweenSegments;
+      double ExtrapolatedRegion;
+  
+      // ----------member data ---------------------------
+};
+
