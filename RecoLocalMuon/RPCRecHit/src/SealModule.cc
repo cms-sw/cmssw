91d9608fe8986227715b2acfcd8f8fd76fff68f5
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2010-01-25 20:42:52 +0000

    Adding DT and CSC Geo for RPCPoint Producer

--- RecoLocalMuon/RPCRecHit/src/SealModule.cc
+++ RecoLocalMuon/RPCRecHit/src/SealModule.cc
@@ -3,10 +3,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalMuon/RPCRecHit/src/RPCRecHitProducer.h"
+#include "RecoLocalMuon/RPCRecHit/interface/RPCPointProducer.h"
 
 #include "RecoLocalMuon/RPCRecHit/interface/RPCRecHitAlgoFactory.h"
 #include "RecoLocalMuon/RPCRecHit/src/RPCRecHitStandardAlgo.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(RPCRecHitProducer);
+DEFINE_ANOTHER_FWK_MODULE(RPCPointProducer);
 DEFINE_EDM_PLUGIN (RPCRecHitAlgoFactory, RPCRecHitStandardAlgo, "RPCRecHitStandardAlgo");
