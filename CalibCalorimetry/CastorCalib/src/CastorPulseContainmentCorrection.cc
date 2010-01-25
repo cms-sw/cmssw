4cc09fc82b2a6c4ea440141c500c0e3149e096c6
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-01-25 12:56:54 +0000

    centralise a template class

--- CalibCalorimetry/CastorCalib/src/CastorPulseContainmentCorrection.cc
+++ CalibCalorimetry/CastorCalib/src/CastorPulseContainmentCorrection.cc
@@ -6,136 +6,11 @@
 #include <math.h>
 #include <iostream>
 
-// Function generates a lookup map for a passed-in function (via templated object algoObject,
-// which must contain method "calcpair" that spits out (x,y) pair from a type float seed.
-// Each map y-value is separated from the previous value by a programmable fractional error
-// relative to the previous value.
-//
-// Currently this function coded for only monotonically increasing or
-// decreasing functions...
+#include "CalibCalorimetry/HcalAlgos/interface/genlkupmap.h"
 
-template <class T_algo>
-void genlkupmap(double smin,
-                double smax,
-                double max_fracerror,
-                double min_sstep,
-                T_algo& algoObject,
-                std::map<double,double>& m_ylookup)
-{
-  std::pair<double,double> thisxy, lastxy, laststoredxy;
-  std::pair<double,double> minxy = algoObject.calcpair(smin);
-  std::pair<double,double> maxxy = algoObject.calcpair(smax);
-  
-  double slope = maxxy.second - minxy.second;
-  slope = (slope >= 0.0) ? 1.0 : -1.0;
 
-  double sstep = min_sstep;
-
-  for (double s=smin; lastxy.first<smax; s += sstep) {
-
-    thisxy = algoObject.calcpair(s);
-
-    double fracerror  = slope*(thisxy.second - laststoredxy.second)/ thisxy.second;
-    double fracchange = slope*(thisxy.second - lastxy.second)/ thisxy.second;
-
-    bool store_cur_pair  = false;
-    bool store_prev_pair = false;
-
-#if 0
-    char str[80];
-    sprintf(str, "%7.1f %7.1f (%8.3f %8.4f) %8.5f %8.5f",
-	    s, sstep, thisxy.first, thisxy.second, fracerror, fracchange);
-    cout << str;
-#endif
-
-    if (s == smin) {
-      store_cur_pair = true;
-    }
-    else if ((fracerror > 2*max_fracerror) ||
-             (thisxy.first > smax) ) {
-      if (sstep > min_sstep) {
-        // possibly overstepped the next entry, back up and reduce the step size
-        s -= sstep;
-        sstep = std::max(0.5*sstep, min_sstep);
-#if 0
-	cout << endl;
-#endif
-        continue;
-      }
-      else if (lastxy.second == laststoredxy.second) {
-        store_cur_pair = true;
-
-        // current step size is too big for the max allowed fractional error,
-        // store current value and issue a warning.
-        //
-	edm::LogWarning("CastorPulseContainmentCorrection::genlkupmap") << " fractional error max exceeded";
-      }
-      else {
-        store_prev_pair = true;
-
-        // re-evaluate current yval with prev yval.
-        fracerror = slope*(thisxy.second - lastxy.second)/ thisxy.second;
-
-        if (fracerror > 2*max_fracerror) {
-          store_cur_pair = true;
-
-          // current step size is too big for the max allowed fractional error,
-          // store current value and issue a warning.
-          //
-	  edm::LogWarning("CastorPulseContainmentCorrection::genlkupmap") << " fractional error max exceeded";
-        }
-      }
-    }
-    else if ((fracerror  < 1.9*max_fracerror) &&
-             (fracchange < 0.1*max_fracerror) &&
-             (thisxy.first < 0.99 * smax) ) {
-      // adapt step size to reduce iterations
-      sstep *= 2.0;
-    }
-
-    if (thisxy.first > smax)
-      store_cur_pair = true;
-      
-    if (store_prev_pair) {
-      m_ylookup[lastxy.first] = lastxy.second;
-      laststoredxy            = lastxy;
-    }
-    if (store_cur_pair) {
-      m_ylookup[thisxy.first] = thisxy.second;
-      laststoredxy            = thisxy;
-    }
-
-    lastxy = thisxy;
-    
-#if 0
-    sprintf(str, " %c %c",
-	    store_cur_pair ? 'C' : ' ',
-	    store_prev_pair ? 'P' : ' ');
-    cout << str << endl;
-#endif
-
-  }
-
-}
-
-//======================================================================
-// Fixed Phase mode amplitude correction factor generation routines:
-
-class RecoFCcorFactorAlgo {
-public:
-  RecoFCcorFactorAlgo(int    num_samples,
-                      double fixedphase_ns);
-
-  std::pair<double,double> calcpair(double);
-
-private:
-  double fixedphasens_;
-  double integrationwindowns_;
-  double time0shiftns_;
-  CastorPulseShapes::Shape shape_;
-};
-
-RecoFCcorFactorAlgo::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
+template<>
+RecoFCcorFactorAlgo<CastorPulseShapes::Shape>::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
 {
   fixedphasens_ = fixedphase_ns;
   CastorPulseShapes shapes;
@@ -176,9 +51,8 @@ RecoFCcorFactorAlgo::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
 #endif
 }
 
-static const double max_recofc = 5000.0f;
-
-std::pair<double,double> RecoFCcorFactorAlgo::calcpair(double truefc)
+template <>
+std::pair<double,double> RecoFCcorFactorAlgo<CastorPulseShapes::Shape>::calcpair(double truefc)
 {
   double timeslew_ns = CastorTimeSlew::delay(std::max(0.0,(double)truefc),
                                            CastorTimeSlew::Medium);
@@ -208,12 +82,12 @@ CastorPulseContainmentCorrection::CastorPulseContainmentCorrection(int num_sampl
                                                                float fixedphase_ns,
                                                                float max_fracerror)
 {
-  RecoFCcorFactorAlgo corFalgo(num_samples, (double)fixedphase_ns);
+  RecoFCcorFactorAlgo<CastorPulseShapes::Shape> corFalgo(num_samples, (double)fixedphase_ns);
 
   // Generate lookup map for the correction function, never exceeding
   // a maximum fractional error for lookups.
   //
-  genlkupmap< RecoFCcorFactorAlgo > (1.0, max_recofc,  // generation domain
+  genlkupmap< RecoFCcorFactorAlgo<CastorPulseShapes::Shape> > (1.0, 5000.0f,  // generation domain
                                      max_fracerror,     // maximum fractional error
                                      1.0,   // min_xstep = minimum true fC increment
                                      corFalgo,
--- CalibCalorimetry/HcalAlgos/src/HcalPulseContainmentCorrection.cc
+++ CalibCalorimetry/HcalAlgos/src/HcalPulseContainmentCorrection.cc
@@ -14,128 +14,10 @@
 // Currently this function coded for only monotonically increasing or
 // decreasing functions...
 
-template <class T_algo>
-void genlkupmap(double smin,
-                double smax,
-                double max_fracerror,
-                double min_sstep,
-                T_algo& algoObject,
-                std::map<double,double>& m_ylookup)
-{
-  std::pair<double,double> thisxy, lastxy, laststoredxy;
-  std::pair<double,double> minxy = algoObject.calcpair(smin);
-  std::pair<double,double> maxxy = algoObject.calcpair(smax);
-  
-  double slope = maxxy.second - minxy.second;
-  slope = (slope >= 0.0) ? 1.0 : -1.0;
+#include "CalibCalorimetry/HcalAlgos/interface/genlkupmap.h"
 
-  double sstep = min_sstep;
-
-  for (double s=smin; lastxy.first<smax; s += sstep) {
-
-    thisxy = algoObject.calcpair(s);
-
-    double fracerror  = slope*(thisxy.second - laststoredxy.second)/ thisxy.second;
-    double fracchange = slope*(thisxy.second - lastxy.second)/ thisxy.second;
-
-    bool store_cur_pair  = false;
-    bool store_prev_pair = false;
-
-#if 0
-    char str[80];
-    sprintf(str, "%7.1f %7.1f (%8.3f %8.4f) %8.5f %8.5f",
-	    s, sstep, thisxy.first, thisxy.second, fracerror, fracchange);
-    cout << str;
-#endif
-
-    if (s == smin) {
-      store_cur_pair = true;
-    }
-    else if ((fracerror > 2*max_fracerror) ||
-             (thisxy.first > smax) ) {
-      if (sstep > min_sstep) {
-        // possibly overstepped the next entry, back up and reduce the step size
-        s -= sstep;
-        sstep = std::max(0.5*sstep, min_sstep);
-#if 0
-	cout << endl;
-#endif
-        continue;
-      }
-      else if (lastxy.second == laststoredxy.second) {
-        store_cur_pair = true;
-
-        // current step size is too big for the max allowed fractional error,
-        // store current value and issue a warning.
-        //
-	edm::LogWarning("HcalPulseContainmentCorrection::genlkupmap") << " fractional error max exceeded";
-      }
-      else {
-        store_prev_pair = true;
-
-        // re-evaluate current yval with prev yval.
-        fracerror = slope*(thisxy.second - lastxy.second)/ thisxy.second;
-
-        if (fracerror > 2*max_fracerror) {
-          store_cur_pair = true;
-
-          // current step size is too big for the max allowed fractional error,
-          // store current value and issue a warning.
-          //
-	  edm::LogWarning("HcalPulseContainmentCorrection::genlkupmap") << " fractional error max exceeded";
-        }
-      }
-    }
-    else if ((fracerror  < 1.9*max_fracerror) &&
-             (fracchange < 0.1*max_fracerror) &&
-             (thisxy.first < 0.99 * smax) ) {
-      // adapt step size to reduce iterations
-      sstep *= 2.0;
-    }
-
-    if (thisxy.first > smax)
-      store_cur_pair = true;
-      
-    if (store_prev_pair) {
-      m_ylookup[lastxy.first] = lastxy.second;
-      laststoredxy            = lastxy;
-    }
-    if (store_cur_pair) {
-      m_ylookup[thisxy.first] = thisxy.second;
-      laststoredxy            = thisxy;
-    }
-
-    lastxy = thisxy;
-    
-#if 0
-    sprintf(str, " %c %c",
-	    store_cur_pair ? 'C' : ' ',
-	    store_prev_pair ? 'P' : ' ');
-    cout << str << endl;
-#endif
-
-  }
-
-}
-
-//======================================================================
-// Fixed Phase mode amplitude correction factor generation routines:
-
-class RecoFCcorFactorAlgo {
-public:
-  RecoFCcorFactorAlgo(int    num_samples,
-                      double fixedphase_ns);
-
-  std::pair<double,double> calcpair(double);
-
-private:
-  double fixedphasens_;
-  double integrationwindowns_;
-  double time0shiftns_;
-  HcalPulseShapes::Shape shape_;
-};
-
-RecoFCcorFactorAlgo::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
+template <>
+RecoFCcorFactorAlgo<HcalPulseShapes::Shape>::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
 {
   fixedphasens_ = fixedphase_ns;
   HcalPulseShapes shapes;
@@ -176,9 +58,9 @@ RecoFCcorFactorAlgo::RecoFCcorFactorAlgo(int num_samples, double fixedphase_ns)
 #endif
 }
 
-static const double max_recofc = 5000.0f;
-
-std::pair<double,double> RecoFCcorFactorAlgo::calcpair(double truefc)
+template <>
+std::pair<double,double> 
+RecoFCcorFactorAlgo<HcalPulseShapes::Shape>::calcpair(double truefc)
 {
   double timeslew_ns = HcalTimeSlew::delay(std::max(0.0,(double)truefc),
                                            HcalTimeSlew::Medium);
@@ -208,12 +90,13 @@ HcalPulseContainmentCorrection::HcalPulseContainmentCorrection(int num_samples,
                                                                float fixedphase_ns,
                                                                float max_fracerror)
 {
-  RecoFCcorFactorAlgo corFalgo(num_samples, (double)fixedphase_ns);
+  RecoFCcorFactorAlgo<HcalPulseShapes::Shape> corFalgo(num_samples, (double)fixedphase_ns);
 
   // Generate lookup map for the correction function, never exceeding
   // a maximum fractional error for lookups.
   //
-  genlkupmap< RecoFCcorFactorAlgo > (1.0, max_recofc,  // generation domain
+  //  static const double max_recofc = 5000.0f;
+  genlkupmap< RecoFCcorFactorAlgo<HcalPulseShapes::Shape> > (1.0, 5000.0f,  // generation domain
                                      max_fracerror,     // maximum fractional error
                                      1.0,   // min_xstep = minimum true fC increment
                                      corFalgo,
