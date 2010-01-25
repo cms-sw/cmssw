79d9089569dd3c5f5cd9c31119f266ce68aa2e87
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-01-25 08:40:24 +0000

    make sure to run also at endRun

--- DQMServices/Components/python/DQMFEDIntegrityClient_cff.py
+++ DQMServices/Components/python/DQMFEDIntegrityClient_cff.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 dqmFEDIntegrity = cms.EDFilter("DQMFEDIntegrityClient",
-   fillOnEndRun = cms.untracked.bool(False),
-   fillOnEndLumi = cms.untracked.bool(True),
-   fillOnEndJob = cms.untracked.bool(False),
    fillInEventloop = cms.untracked.bool(True),
+   fillOnEndLumi = cms.untracked.bool(True),
+   fillOnEndRun = cms.untracked.bool(True),
+   fillOnEndJob = cms.untracked.bool(False),
    moduleName = cms.untracked.string('FED')
    )
 
@@ -12,7 +12,9 @@ dqmQTestDQMFED = cms.EDFilter("QualityTester",
    prescaleFactor = cms.untracked.int32(1),
    qtList = cms.untracked.FileInPath('DQMServices/Components/data/DQMFEDQualityTests.xml'),
    getQualityTestsFromFile = cms.untracked.bool(True),
-   qtestOnEndRun = cms.untracked.bool(True)
+   qtestOnEndLumi = cms.untracked.bool(True),
+   qtestOnEndRun = cms.untracked.bool(True),
+   qtestOnEndJob = cms.untracked.bool(False)
    )
 
 dqmFEDIntegrityClient = cms.Sequence(dqmFEDIntegrity*dqmQTestDQMFED)
