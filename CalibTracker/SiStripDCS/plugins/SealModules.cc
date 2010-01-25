e8903828b345bbd6a30d8dd7a28a5f22fcc53f5b
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2010-01-25 14:11:50 +0000

    Removed obsolete filesù

--- CalibTracker/SiStripDCS/plugins/SealModules.cc
+++ CalibTracker/SiStripDCS/plugins/SealModules.cc
@@ -4,15 +4,8 @@
 DEFINE_SEAL_MODULE();
 
 #include "CondCore/PopCon/interface/PopConAnalyzer.h"
-#include "CalibTracker/SiStripDCS/interface/SiStripModuleHVHandler.h"
-
-//OLD DCS O2O
-typedef popcon::PopConAnalyzer<popcon::SiStripModuleHVHandler> SiStripPopConModuleHV;
-DEFINE_ANOTHER_FWK_MODULE(SiStripPopConModuleHV);
 
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-#include "CalibTracker/SiStripDCS/interface/SiStripModuleHVBuilder.h"
-DEFINE_ANOTHER_FWK_SERVICE(SiStripModuleHVBuilder);
 
 //ReWritten DCS O2O
 #include "CalibTracker/SiStripDCS/interface/SiStripDetVOffHandler.h"
