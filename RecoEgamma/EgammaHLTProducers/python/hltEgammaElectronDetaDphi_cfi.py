a3c46a3aa4c55b8cee8717deb91aa13be63ea764
Roberto Covarelli <sha1-195c8adc73af39d9dbe4407aec6ff1a79a6f51ef@cern.ch>
2010-01-25 09:27:56 +0000

    Added missing cfi files

---
+++ RecoEgamma/EgammaHLTProducers/python/hltEgammaElectronDetaDphi_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+hltEgammaElectronDetaDphi = cms.EDProducer( "EgammaHLTElectronDetaDphiProducer",
+    electronProducer = cms.InputTag( "hltPixelMatchElectronsL1NonIso" ),
+    BSProducer = cms.InputTag( "hltOfflineBeamSpot" ),
+    useTrackProjectionToEcal = cms.untracked.bool( False )
+)
+
