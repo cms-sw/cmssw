2eceee58912dd37e5803886a12376fafebdc0cc9
Juan Alcaraz Maestre <sha1-00c52069b91662a716b700699899914afb62bf4e@cern.ch>
2010-01-26 11:17:25 +0000

    Fix ISRGamma weight producer

--- ElectroWeakAnalysis/Utilities/src/ISRGammaWeightProducer.cc
+++ ElectroWeakAnalysis/Utilities/src/ISRGammaWeightProducer.cc
@@ -64,40 +64,69 @@ void ISRGammaWeightProducer::produce(edm::Event& iEvent, const edm::EventSetup&)
       // Set a default weight to start with
       (*weight) = 1.;
 
-      const reco::Candidate* parton = 0;
+      // Find the boson at the hard scattering level
       const reco::GenParticle* boson = 0;
-      const reco::GenParticle* photon = 0;
+      int parton1Key = -1;
+      int parton2Key = -1;
       for (unsigned int i = 0; i<gensize; ++i) {
             const reco::GenParticle& part = (*genParticles)[i];
+            int status = abs(part.status());
+            if (status!=3) continue;
+            if (part.numberOfMothers()!=2) continue;
             int partId = abs(part.pdgId());
-            if (partId==23 || abs(partId)==24) {
+            if (status==3 && (partId==23||abs(partId)==24)) {
                   boson = &(*genParticles)[i];
-            } else if (partId==22) {
-                  if (part.numberOfMothers()!=1) continue;
-                  const reco::Candidate * mother = part.mother();
-                  if (mother->status()!=3) continue;
-                  if (abs(mother->pdgId())>6 && mother->pdgId()!=2212) continue;
-                  photon = &(*genParticles)[i];
-                  parton = mother;
-                  if (boson && photon && parton) break;
-            } 
-            if (boson && photon && parton) break;
+                  parton1Key = part.motherRef(0).key();
+                  parton2Key = part.motherRef(1).key();
+                  break;
+            }
+      }
+      
+      // Consider only photons near the hard-scattering process
+      const reco::GenParticle* photon = 0;
+      if (boson) {
+        for (unsigned int i = 0; i<gensize; ++i) {
+            photon = 0;
+            const reco::GenParticle& part = (*genParticles)[i];
+            int status = abs(part.status());
+            if (status!=1) continue;
+            int partId = abs(part.pdgId());
+            if (partId!=22)  continue;
+            if (part.numberOfMothers()!=1) continue;
+            int keyM = part.motherRef(0).key();
+            const reco::GenParticle* mother = &(*genParticles)[keyM];
+            if (mother->status()!=3) continue;
+            int mId = mother->pdgId();
+            if (abs(mId)>6 && mId!=2212) continue;
+            for (unsigned int j=0; j<mother->numberOfDaughters(); ++j){ 
+                  int keyD = mother->daughterRef(j).key();
+                  if (keyD==parton1Key || keyD==parton2Key) {
+                        photon = &part;
+                        break;
+                  }
+            }
+            if (photon) break;
+        }  
       }
 
-      if (boson && photon && parton) {
-            double en_s = boson->energy() + photon->energy();
-            double px_s = boson->px() + photon->px();
-            double py_s = boson->py() + photon->py();
-            double pz_s = boson->pz() + photon->pz();
-            double s = en_s*en_s - px_s*px_s -py_s*py_s - pz_s*pz_s;
-            double en_t = parton->energy() - photon->energy();
-            double px_t = parton->px() - photon->px();
-            double py_t = parton->py() - photon->py();
-            double pz_t = parton->pz() - photon->pz();
-            double t = en_t*en_t - px_t*px_t -py_t*py_t - pz_t*pz_t;
+      if (boson && photon) {
+            math::XYZTLorentzVector smom = boson->p4() + photon->p4();
+            double s = smom.M2();
+            double sqrts = smom.M();
+
+            // Go to CM using the boost direction of the boson+photon system
+            ROOT::Math::Boost cmboost(smom.BoostToCM());
+            math::XYZTLorentzVector photonCM(cmboost(photon->p4()));
+            double pcostheta = (  smom.x()*photonCM.x()
+                               + smom.y()*photonCM.y()
+                               + smom.z()*photonCM.z() ) / smom.P();
+            
+            // Determine kinematic invariants
+            double t = - sqrts * (photonCM.t()-pcostheta);
             double MV = boson->mass();
             double u = MV*MV - s - t;
-            (*weight) = 1. - 2*t*u/(s*s+pow(MV,4));
+            (*weight) = 1. - 2*t*u/(s*s+MV*MV*MV*MV);
+            //printf(">>>>>>>>> s %f t %f u %f, MV %f, weight = %f\n", s, t, u, MV, (*weight));
       }
 
       iEvent.put(weight);
