3752fc90001e7fc1556abec75e6962f9fb4189f5
kaussen <sha1-b58974427aa4d349705fe1a36f0e42a0fed605a3@cern.ch>
2010-01-26 17:33:51 +0000

    Change of producer input tag to access the correct collection

--- DPGAnalysis/SiStripTools/python/filters/Potential_TIBTEC_FrameHeaderEvents_firstpeak_AlCaReco_cfi.py
+++ DPGAnalysis/SiStripTools/python/filters/Potential_TIBTEC_FrameHeaderEvents_firstpeak_AlCaReco_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 PotentialTIBTECFrameHeaderEventsFPeak = cms.EDFilter('EventWithHistoryEDFilter',
                                                  commonConfiguration = cms.untracked.PSet(
-                                                    historyProduct             = cms.untracked.InputTag("ConsecutiveHEs"),
-                                                    APVPhaseLabel              = cms.untracked.string("apvPhases"),
+                                                    historyProduct             = cms.untracked.InputTag("consecutiveHEs"),
+                                                    APVPhaseLabel              = cms.untracked.string("APVPhases"),
                                                  ),
                                                  filterConfigurations = cms.untracked.VPSet(
                                                      cms.PSet(
--- DPGAnalysis/SiStripTools/python/largesistripclusterevents_AlCaReco_cfi.py
+++ DPGAnalysis/SiStripTools/python/largesistripclusterevents_AlCaReco_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 LargeSiStripClusterEvents = cms.EDFilter('LargeSiStripClusterEvents',
                                          collectionName    = cms.InputTag("calZeroBiasClusters"),
-                                         absoluteThreshold = cms.untracked.int32(300),
+                                         absoluteThreshold = cms.untracked.int32(10000),
                                          moduleThreshold   = cms.untracked.int32(20)
 )	
