d76dd5946ec7f36a581d9951cb27c5b62e7210b9
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2010-01-26 21:19:28 +0000

    added code for QT from DB

--- CondCore/DQMPlugins/src/plugin.cc
+++ CondCore/DQMPlugins/src/plugin.cc
@@ -1,4 +1,4 @@
-#include "CondCore/ESSources/interface/registration_macros.h"
+#include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/DQMObjects/interface/DQMSummary.h"
 #include "CondFormats/DataRecord/interface/DQMSummaryRcd.h"
 DEFINE_SEAL_MODULE();
@@ -6,8 +6,8 @@ REGISTER_PLUGIN(DQMSummaryRcd, DQMSummary);
 #include "CondFormats/DQMObjects/interface/HDQMSummary.h"
 #include "CondFormats/DataRecord/interface/HDQMSummaryRcd.h"
 REGISTER_PLUGIN(HDQMSummaryRcd, HDQMSummary);
-#include "CondFormats/Common/interface/FileBlob.h"
+#include "CondFormats/GeometryObjects/interface/GeometryFile.h"
 #include "CondFormats/DataRecord/interface/DQMReferenceHistogramRootFileRcd.h"
-REGISTER_PLUGIN(DQMReferenceHistogramRootFileRcd, FileBlob);
+REGISTER_PLUGIN(DQMReferenceHistogramRootFileRcd, GeometryFile);
 #include "CondFormats/DataRecord/interface/DQMXMLFileRcd.h"
-REGISTER_PLUGIN(DQMXMLFileRcd, FileBlob);
+REGISTER_PLUGIN(DQMXMLFileRcd, GeometryFile);
