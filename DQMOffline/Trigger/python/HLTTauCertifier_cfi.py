3cee19165f09b11455b40c2489f67b186e545eba
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-01-27 15:39:15 +0000

    Move HLT_Tau certification ME location

--- DQMOffline/Trigger/python/HLTTauCertifier_cfi.py
+++ DQMOffline/Trigger/python/HLTTauCertifier_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 hltTauOfflineCertification = cms.EDFilter("HLTTauCertifier",
-                                   targetDir = cms.string("EventInfo"),
+                                   targetDir = cms.string("HLT/EventInfo/reportSummaryContents"),
                                    targetME  = cms.string("HLT_Tau"),
                                    inputMEs = cms.vstring(
                                       "HLT/TauOffline/Inclusive/DoubleLooseIsoTauTau/TriggerBits",
