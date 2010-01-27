d3219e79c8f007adaa24925a44919b9feb3448ef
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2010-01-27 09:43:25 +0000

    updating SiStripMonitorClusterAlca_cfi.py following serent changes in SiStripMonitorCluster analyzer

--- DQM/SiStripMonitorCluster/python/SiStripMonitorClusterAlca_cfi.py
+++ DQM/SiStripMonitorCluster/python/SiStripMonitorClusterAlca_cfi.py
@@ -144,7 +144,7 @@ SiStripCalZeroBiasMonitorCluster = cms.EDFilter("SiStripMonitorCluster",
         subdetswitchon = cms.bool(True)
     ),
     TProfClustersApvCycle = cms.PSet(
-        Nbins = cms.int32(70),
+        Nbinsx = cms.int32(70),
         xmin = cms.double(-0.5),
         xmax = cms.double(69.5),
         Nbinsy = cms.int32(200),
