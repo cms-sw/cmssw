fa968947faf8e65586863d0595194928afaa95db
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-01-27 09:48:46 +0000

    add files needed for HLT ConfDB parsing

---
+++ CommonTools/RecoAlgos/python/EtMinCaloJetSelector_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+hltBSoftMuonL25Jets = cms.EDProducer( "EtMinCaloJetSelector",
+    src = cms.InputTag( "hltSelector4Jets" ),
+    filter = cms.bool( False ),
+    etMin = cms.double( 20.0 )
+)
+
