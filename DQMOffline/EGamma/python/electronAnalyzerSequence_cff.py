d26bab6f9ff713f99b50d3c7752db7fc9e6aa961
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2010-01-28 14:39:14 +0000

    use ele prefix instead of Ele for DQM folders

--- DQMOffline/EGamma/python/electronAnalyzerSequence_cff.py
+++ DQMOffline/EGamma/python/electronAnalyzerSequence_cff.py
@@ -8,7 +8,7 @@ mergedSuperClusters = cms.EDFilter("SuperClusterMerger",
 )
 
 from DQMOffline.EGamma.electronGeneralAnalyzer_cfi import *
-dqmElectronGeneralAnalysis.OutputFolderName = cms.string("Ele1_General") ;
+dqmElectronGeneralAnalysis.OutputFolderName = cms.string("ele1_General") ;
 
 from DQMOffline.EGamma.electronAnalyzer_cfi import *
 dqmElectronAnalysis.MinEt = cms.double(10.) ;
@@ -16,24 +16,24 @@ dqmElectronAnalysis.MaxTkIso03 = cms.double(1.) ;
 
 dqmElectronAnalysisAllElectrons = dqmElectronAnalysis.clone() ;
 dqmElectronAnalysisAllElectrons.Selection = 0 ;
-dqmElectronAnalysisAllElectrons.OutputFolderName = cms.string("Ele2_All") ;
+dqmElectronAnalysisAllElectrons.OutputFolderName = cms.string("ele2_All") ;
 
 dqmElectronAnalysisSelectionEt = dqmElectronAnalysis.clone() ;
 dqmElectronAnalysisSelectionEt.Selection = 1 ;
-dqmElectronAnalysisSelectionEt.OutputFolderName = cms.string("Ele3_Et10") ;
+dqmElectronAnalysisSelectionEt.OutputFolderName = cms.string("ele3_Et10") ;
 
 dqmElectronAnalysisSelectionEtIso = dqmElectronAnalysis.clone() ;
 dqmElectronAnalysisSelectionEtIso.Selection = 2 ;
-dqmElectronAnalysisSelectionEtIso.OutputFolderName = cms.string("Ele4_Et10TkIso1") ;
+dqmElectronAnalysisSelectionEtIso.OutputFolderName = cms.string("ele4_Et10TkIso1") ;
 
 #dqmElectronAnalysisSelectionEtIsoElID = dqmElectronAnalysis.clone() ;
 #dqmElectronAnalysisSelectionEtIsoElID.Selection = 3 ;
-#dqmElectronAnalysisSelectionEtIsoElID.OutputFolderName = cms.string("Ele4_Et10TkIso1ElID") ;
+#dqmElectronAnalysisSelectionEtIsoElID.OutputFolderName = cms.string("ele4_Et10TkIso1ElID") ;
 
 from DQMOffline.EGamma.electronTagProbeAnalyzer_cfi import *
 dqmElectronTagProbeAnalysis.MinEt = cms.double(10.) ;
 dqmElectronTagProbeAnalysis.MaxTkIso03 = cms.double(1.) ;
-dqmElectronTagProbeAnalysis.OutputFolderName = cms.string("Ele5_TagAndProbe") ;
+dqmElectronTagProbeAnalysis.OutputFolderName = cms.string("ele5_TagAndProbe") ;
 
 electronAnalyzerSequence = cms.Sequence(
    mergedSuperClusters
--- DQMOffline/EGamma/python/electronOfflineClientSequence_cff.py
+++ DQMOffline/EGamma/python/electronOfflineClientSequence_cff.py
@@ -3,24 +3,24 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.EGamma.electronOfflineClient_cfi import *
 
 dqmElectronClientAllElectrons = dqmElectronOfflineClient.clone() ;
-dqmElectronClientAllElectrons.InputFolderName = cms.string("Ele2_All") ;
-dqmElectronClientAllElectrons.OutputFolderName = cms.string("Ele2_All") ;
+dqmElectronClientAllElectrons.InputFolderName = cms.string("ele2_All") ;
+dqmElectronClientAllElectrons.OutputFolderName = cms.string("ele2_All") ;
 
 dqmElectronClientSelectionEt = dqmElectronOfflineClient.clone() ;
-dqmElectronClientSelectionEt.InputFolderName = cms.string("Ele3_Et10") ;
-dqmElectronClientSelectionEt.OutputFolderName = cms.string("Ele3_Et10") ;
+dqmElectronClientSelectionEt.InputFolderName = cms.string("ele3_Et10") ;
+dqmElectronClientSelectionEt.OutputFolderName = cms.string("ele3_Et10") ;
 
 dqmElectronClientSelectionEtIso = dqmElectronOfflineClient.clone() ;
-dqmElectronClientSelectionEtIso.InputFolderName = cms.string("Ele4_Et10TkIso1") ;
-dqmElectronClientSelectionEtIso.OutputFolderName = cms.string("Ele4_Et10TkIso1") ;
+dqmElectronClientSelectionEtIso.InputFolderName = cms.string("ele4_Et10TkIso1") ;
+dqmElectronClientSelectionEtIso.OutputFolderName = cms.string("ele4_Et10TkIso1") ;
 
 #dqmElectronClientSelectionEtIsoElID = dqmElectronOfflineClient.clone() ;
-#dqmElectronClientSelectionEtIsoElID.InputFolderName = cms.string("Ele4_Et10TkIso1ElID") ;
-#dqmElectronClientSelectionEtIsoElID.OutputFolderName = cms.string("Ele4_Et10TkIso1ElID") ;
+#dqmElectronClientSelectionEtIsoElID.InputFolderName = cms.string("ele4_Et10TkIso1ElID") ;
+#dqmElectronClientSelectionEtIsoElID.OutputFolderName = cms.string("ele4_Et10TkIso1ElID") ;
 
 dqmElectronClientTagAndProbe = dqmElectronOfflineClient.clone() ;
-dqmElectronClientTagAndProbe.InputFolderName = cms.string("Ele5_TagAndProbe") ;
-dqmElectronClientTagAndProbe.OutputFolderName = cms.string("Ele5_TagAndProbe") ;
+dqmElectronClientTagAndProbe.InputFolderName = cms.string("ele5_TagAndProbe") ;
+dqmElectronClientTagAndProbe.OutputFolderName = cms.string("ele5_TagAndProbe") ;
 dqmElectronClientTagAndProbe.EffHistoTitle = cms.string("")
 
 electronOfflineClientSequence = cms.Sequence(
