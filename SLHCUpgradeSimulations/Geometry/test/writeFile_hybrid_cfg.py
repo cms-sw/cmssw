2cbf7fa81d4316cf2f9947ffc336b5f89793899f
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2010-01-28 04:52:13 +0000

    Changes for 3_3_6

--- SLHCUpgradeSimulations/Geometry/test/writeFile_hybrid_cfg.py
+++ SLHCUpgradeSimulations/Geometry/test/writeFile_hybrid_cfg.py
@@ -1,10 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("ICALIB")
-process.load("Configuration.StandardSequences.FakeConditions_cff")
+#process.load("Configuration.StandardSequences.FakeConditions_cff")
+process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
-
 process.load("SLHCUpgradeSimulations.Geometry.hybrid_cmsIdealGeometryXML_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MC_31X_V8::All'
+process.siPixelFakeGainOfflineESSource = cms.ESSource("SiPixelFakeGainOfflineESSource",
+    file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/hybrid/PixelSkimmedGeometry.txt')
+)
+process.es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFakeGainOfflineESSource")
+
+process.siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
+    file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/hybrid/PixelSkimmedGeometry.txt')
+)
+process.es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
+process.TrackerDigiGeometryESModule.applyAlignment = False
 
 process.source = cms.Source("EmptyIOVSource",
     firstValue = cms.uint64(1),
@@ -13,12 +26,9 @@ process.source = cms.Source("EmptyIOVSource",
     interval = cms.uint64(1)
 )
 
-process.MessageLogger = cms.Service("MessageLogger",
-    insert_logfile = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO')
-    ),
-    destinations = cms.untracked.vstring('./logfile.txt')
-)
+process.load("FWCore/MessageService/MessageLogger_cfi")
+process.MessageLogger.destinations = cms.untracked.vstring("logfile")
+process.MessageLogger.logfile = cms.untracked.PSet(threshold = cms.untracked.string('INFO'))
 
 process.prodstrip = cms.EDFilter("SiStripDetInfoFileWriter",
     FilePath = cms.untracked.string('SiStripDetInfo_hybrid.dat'),
@@ -26,6 +36,7 @@ process.prodstrip = cms.EDFilter("SiStripDetInfoFileWriter",
 
 process.prodpixel = cms.EDFilter("SiPixelDetInfoFileWriter",
     FilePath = cms.untracked.string('PixelSkimmedGeometry_hybrid.txt'),
+    WriteROCInfo = cms.untracked.bool(True)
 )
 
 process.asciiPrint = cms.OutputModule("AsciiOutputModule")
