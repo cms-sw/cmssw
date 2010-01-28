b1bc1d423d523633ca1a8644a097427d7c384497
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-01-28 17:31:15 +0000

    misspelled

--- FWCore/Utilities/interface/Likely.h
+++ FWCore/Utilities/interface/Likely.h
@@ -3,7 +3,7 @@
 
 #if defined(NO_LIKELY)
 #define likely(x) (x)
-#define UNlikely(x) (x)   
+#define unlikely(x) (x)   
 #elif defined(REVERSE_LIKELY)
 #define unlikely(x) (__builtin_expect(x, true))
 #define likely(x) (__builtin_expect(x, false))
