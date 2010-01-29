025ad65571bb16ba279ea42ee50a2cface7cf3c9
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2010-01-29 22:35:56 +0000

    For new FPIX disks

--- SLHCUpgradeSimulations/Geometry/src/module.cc
+++ SLHCUpgradeSimulations/Geometry/src/module.cc
@@ -3,11 +3,9 @@
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinear.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinearGap.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLayerAlgo.h"
-#include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarTPGStackLayerAlgo.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackTrigLayerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
-#include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdInnerDisks.h"
-#include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdOuterDisks.h"
+#include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdDiskAlgo.h"
 
 #include "SLHCUpgradeSimulations/Geometry/interface/PrintGeomMatInfo.h"
 #include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
@@ -16,9 +14,7 @@
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinear,      "track:DDPixBarStackLinear");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinearGap,   "track:DDPixBarStackLinearGap");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLayerAlgo,   "track:DDPixBarStackLayerAlgo");
-DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarTPGStackLayerAlgo,"track:DDPixBarTPGStackLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackTrigLayerAlgo, "track:DDPixBarStackTrigLayerAlgo");
-DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdInnerDisks,      "track:DDPixFwdInnerDisks");
-DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdOuterDisks,      "track:DDPixFwdOuterDisks");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdDiskAlgo,      "track:DDPixFwdDiskAlgo");
 DEFINE_SEAL_MODULE ();
 DEFINE_SIMWATCHER(PrintGeomMatInfo);
