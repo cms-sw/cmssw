234cf23c5f2f74ccb72ac32f10390dc70894df2f
Dave Evans <sha1-0a127cb30770fd84c4fd981547c09a2be8db1484@cern.ch>
2010-02-02 16:55:29 +0000

    Provide a call through the scenario object to access the standard merge process

--- Configuration/DataProcessing/python/Scenario.py
+++ Configuration/DataProcessing/python/Scenario.py
@@ -14,6 +14,7 @@ configurations for the various types of job
 """
 
 import FWCore.ParameterSet.Config as cms
+from Configuration.DataProcessing.Merge import mergeProcess
 
 class Scenario(object):
     """
@@ -23,6 +24,8 @@ class Scenario(object):
     def __init__(self):
         pass
 
+    def merge(self, *inputFiles, **options):
+        return mergeProcess(*inputFiles, **options)
 
     def dropOutputModule(self, processRef, moduleName):
         """
