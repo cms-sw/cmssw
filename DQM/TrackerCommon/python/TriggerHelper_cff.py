aa82dcfeda08dba8699672440fad28baa6f70485
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2010-02-02 22:31:11 +0000

    Code based (trigger) filter completed: - L1 and DCS (scaler) filters added; - possibility to choose AND or OR combination of L1, HLT and DCS decision; - code restructured and simplified; - configurations extended according to added functionality and template PSets for SiPixel DQM modules added; - remaining testing/debugging output removed.

--- DQM/TrackerCommon/python/TriggerHelper_cff.py
+++ DQM/TrackerCommon/python/TriggerHelper_cff.py
@@ -1,57 +1,233 @@
 import FWCore.ParameterSet.Config as cms
 
-# "hltInputTag":
-# The inout tag has to contain also the process name.
-# Removing this parameter completly switches the filter off.
-# "hltPaths":
-# The filter has no effect, if the vector is empty.
-# Paths can be negated by prepending a '~'.
-# "andOr":
-# False = AND, True = OR
-# "errorReply":
-#
-
-SiStripHltFilter_SiStripMonitorHardware = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiStripFilter_SiStripMonitorHardware = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
 
-SiStripHltFilter_SiStripMonitorDigi = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiStripFilter_SiStripMonitorDigi = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
 
-SiStripHltFilter_SiStripMonitorCluster = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring(),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiStripFilter_SiStripMonitorCluster = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
 
-SiStripHltFilter_SiStripMonitorTrack = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
-                             ),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiStripFilter_SiStripMonitorTrack = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
 
-SiStripHltFilter_TrackerMonitorTrack = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
-                             ),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiPixelFilter_SiPixelMonitorDigi = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 28
+                              , 29
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
 
-SiStripHltFilter_TrackingMonitor = cms.PSet(
-    hltInputTag = cms.InputTag( "TriggerResults::HLT" ),
-    hltPaths    = cms.vstring( 'HLT_PhysicsDeclared'
-                             ),
-    andOr       = cms.bool( False ),
-    errorReply  = cms.bool( True )
+SiPixelFilter_SiPixelMonitorCluster = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 28
+                              , 29
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
+)
+
+SiPixelFilter_SiPixelMonitorRecHit = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 28
+                              , 29
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
+)
+
+SiPixelFilter_SiPixelMonitorTrack = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 28
+                              , 29
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
+)
+
+TrackerFilter_TrackerMonitorTrack = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
+)
+
+TrackingFilter_TrackingMonitor = cms.PSet(
+    # General
+    andOr = cms.bool( False ),
+    # HLT
+    hltInputTag   = cms.InputTag( "TriggerResults::HLT" ),
+    hltPaths      = cms.vstring( 'HLT_PhysicsDeclared'
+                               ),
+    andOrHlt      = cms.bool( False ),
+    errorReplyHlt = cms.bool( False ),
+    # L1
+    l1Algorithms = cms.vstring(),
+    andOrL1      = cms.bool( False ),
+    errorReplyL1 = cms.bool( False ),
+    # DCS
+    dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+    dcsPartitions = cms.vint32( 24
+                              , 25
+                              , 26
+                              , 27
+                              ),
+    andOrDcs      = cms.bool( False ),
+    errorReplyDcs = cms.bool( False )
 )
