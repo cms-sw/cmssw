197ce3ff043483e3fc08f13bef69b2ac3a6b88c9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-04 21:12:24 +0000

    Remove obsolete ROOT versioning, and various style standardizations

--- FWCore/Utilities/interface/UseReflex.h
+++ FWCore/Utilities/interface/UseReflex.h
@@ -1,29 +1,17 @@
 #ifndef FWCore_Utilities_UseReflex_h
 #define FWCore_Utilities_UseReflex_h
 
-// The purpose of this header is to isolate the changes that need to be made 
+// The original purpose of this header was to isolate the changes that need to be made 
 // when migrating from ROOT 5.18 to ROOT 5.21 due to the fact that the 
 // namespace ROOT::Reflex in 5.18 is simply Reflex: in 5.21.
 
-// This header may be used as is in 5.18.
-// To use this header, do the following, and include this header in any files 
-// where any of these apply:
-// 1) In any file where ROOT::Reflex is explicitly used, replace ROOT::Reflex 
-//    with Reflex.
-// 2) In any file using "namespace ROOT { namespace Reflex { ... } }", remove 
-//    this.
-//      Any declarations in ... not duplicated in this header would need to 
-//      be added below.
-// 3) In any file containing "using nameapace ROOT::Reflex", remove this 
-//    directive, and add "Reflex::" where necessary to compile.
+// However, the conditional code for ROOT 5.19 and prior releases (now obsolete in CMSSW)
+// has been removed, so this header is just a convenience for files that use reflex.
 
 #include "Reflex/Type.h"
 #include "Reflex/TypeTemplate.h"
 #include "RVersion.h"
 
-#if ROOT_VERSION_CODE <= ROOT_VERSION(5,19,0)
-namespace ROOT { // Remove this line for ROOT 5.21.
-#endif
 namespace Reflex {
   class Base;
   class Member;
@@ -33,22 +21,14 @@ namespace Reflex {
   class Type;
   class TypeTemplate;
   inline
-  std::ostream& operator<< (std::ostream& os, Type const& t) {
+  std::ostream& operator<<(std::ostream& os, Type const& t) {
     os << t.Name();
     return os;
   } 
   inline
-  std::ostream& operator<< (std::ostream& os, TypeTemplate const& tt) {
+  std::ostream& operator<<(std::ostream& os, TypeTemplate const& tt) {
     os << tt.Name();
     return os;
   }
 }
-#if ROOT_VERSION_CODE <= ROOT_VERSION(5,19,0)
-} // Remove this line for ROOT 5.21.
-#endif
-
-#if ROOT_VERSION_CODE <= ROOT_VERSION(5,19,0)
-using namespace ROOT; // Remove this line for ROOT 5.21.
-#endif
-
 #endif
