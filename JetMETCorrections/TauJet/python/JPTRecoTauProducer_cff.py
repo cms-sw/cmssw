8ecd3770e8c09343a0e67f2987e19480a728c14f
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2010-02-05 11:51:42 +0000

    Added producer for JPT corrected taus

---
+++ JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
+from JetMETCorrections.Configuration.ZSPJetCorrections332_cff import *
+JPTZSPCorrectorICone5.ResponseMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt")
+JPTZSPCorrectorICone5.EfficiencyMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt")
+JPTZSPCorrectorICone5.LeakageMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt")
+ZSPJetCorJetIcone5.src = cms.InputTag("iterativeCone5CaloJets")
+
+from Configuration.StandardSequences.Reconstruction_cff import *
+from RecoTauTag.RecoTau.CaloRecoTauTagInfoProducer_cfi import *
+caloRecoTauTagInfoProducer.CaloJetTracksAssociatorProducer = cms.InputTag('ZSPiterativeCone5JetTracksAssociatorAtVertex')
+
+jptRecoTauProducer = cms.Sequence(
+        ZSPJetCorrectionsIcone5*
+        JetPlusTrackCorrectionsIcone5*
+        caloRecoTauProducer
+)
