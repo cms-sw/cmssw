2e213147ef5f806a1c5730054ae3074dc4fa1fc9
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-02-05 12:36:23 +0000

    fix construcotr

--- DataFormats/Math/interface/invertPosDefMatrix.h
+++ DataFormats/Math/interface/invertPosDefMatrix.h
@@ -37,10 +37,16 @@ inline bool invertPosDefMatrix(ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepSym<T
 namespace MathSSE {
   struct M2 {
     union {
-      __m128d r[2];
       double m[4];
+      __m128d r[2];
+      struct {double m00,m01,m11,m10;};
     };
     
+
+    // load shuffled
+    inline M2(double i00, double i01, double i10, double i11) : 
+      m00(i00), m01(i01), m11(i11), m10(i10){}
+
     double & operator[](int i) { return m[i];}
     __m128d & r0() { return r[0]; }
     __m128d & r1() { return r[1]; }
@@ -76,7 +82,8 @@ namespace MathSSE {
 template<>
 inline bool invertPosDefMatrix<double,2>(ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > & m) {
   // load shuffled
-  MathSSE::M2 mm = { m.Array()[0], m.Array()[1], m.Array()[2], m.Array()[1]  };
+  // MathSSE::M2 mm = { m.Array()[0], m.Array()[1], m.Array()[2], m.Array()[1]  };
+  MathSSE::M2 mm(m.Array()[0], m.Array()[1], m.Array()[1], m.Array()[2]);
 
   bool ok = mm.invert();
   if (ok) {
@@ -91,7 +98,8 @@ template<>
 inline bool invertPosDefMatrix<double,2>(ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > const & mIn,
 				  ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2> > & mOut) {
  
-  MathSSE::M2 mm = { mIn.Array()[0], mIn.Array()[1], mIn.Array()[2], mIn.Array()[1]  };
+  //  MathSSE::M2 mm = { mIn.Array()[0], mIn.Array()[1], mIn.Array()[2], mIn.Array()[1]  };
+  MathSSE::M2 mm(m.Array()[0], m.Array()[1], m.Array()[1], m.Array()[2]);
 
   bool ok = mm.invert();
   mOut.Array()[0] = mm[0];
