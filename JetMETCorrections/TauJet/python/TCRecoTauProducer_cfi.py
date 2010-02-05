b8a67326cacc7d04242a5d5e556d9bc8b0f71544
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2010-02-05 11:50:20 +0000

    Making the discriminators work with tcTau

--- JetMETCorrections/TauJet/python/TCRecoTauProducer_cfi.py
+++ JetMETCorrections/TauJet/python/TCRecoTauProducer_cfi.py
@@ -2,14 +2,35 @@ import FWCore.ParameterSet.Config as cms
 
 from TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff import *
 from TrackingTools.TrackAssociator.default_cfi import TrackAssociatorParameterBlock
+#from JetMETCorrections.TauJet.TCTauAlgo_cfi import *
 
 tcRecoTauProducer = cms.EDProducer("TCRecoTauProducer",
 	CaloRecoTauProducer = cms.InputTag("caloRecoTauProducer"),
         ## TauJet jet energy correction parameters
-        src            = cms.InputTag("iterativeCone5CaloJets"),
-        tagName        = cms.string("IterativeCone0.4_EtScheme_TowerEt0.5_E0.8_Jets871_2x1033PU_tau"),
-        TauTriggerType = cms.int32(1),
-	TrackAssociatorParameters = TrackAssociatorParameterBlock.TrackAssociatorParameters	
+#        src            = cms.InputTag("iterativeCone5CaloJets"),
+#        tagName        = cms.string("IterativeCone0.4_EtScheme_TowerEt0.5_E0.8_Jets871_2x1033PU_tau"),
+#        TauTriggerType = cms.int32(1),
+#	TrackAssociatorParameters = TrackAssociatorParameterBlock.TrackAssociatorParameters	
+        EtCaloOverTrackMin    = cms.double(-0.9),
+        EtCaloOverTrackMax    = cms.double(0.0),
+        EtHcalOverTrackMin    = cms.double(-0.3),
+        EtHcalOverTrackMax    = cms.double(1.0),
+        SignalConeSize        = cms.double(0.2),
+        EcalConeSize          = cms.double(0.5),
+        MatchingConeSize      = cms.double(0.1),
+        Track_minPt           = cms.double(1.0),
+        tkmaxipt              = cms.double(0.03),
+        tkmaxChi2             = cms.double(100.),
+        tkminPixelHitsn       = cms.int32(2),
+        tkminTrackerHitsn     = cms.int32(8),
+        TrackCollection       = cms.InputTag("generalTracks"),
+        PVProducer            = cms.InputTag("offlinePrimaryVertices"),
+        EBRecHitCollection    = cms.InputTag("ecalRecHit:EcalRecHitsEB"),
+        EERecHitCollection    = cms.InputTag("ecalRecHit:EcalRecHitsEE"),
+        HBHERecHitCollection  = cms.InputTag("hbhereco"),
+        HORecHitCollection    = cms.InputTag("horeco"),
+        HFRecHitCollection    = cms.InputTag("hfreco"),
+        TrackAssociatorParameters = TrackAssociatorParameterBlock.TrackAssociatorParameters
 )
 
 
---
+++ JetMETCorrections/TauJet/python/TCTauProducer_cff.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+from JetMETCorrections.TauJet.JPTRecoTauProducer_cff import *
+from JetMETCorrections.TauJet.TCRecoTauProducer_cfi import *
+
+from RecoTauTag.RecoTau.CaloRecoTauDiscriminationByLeadingTrackFinding_cfi import *
+caloRecoTauDiscriminationByLeadingTrackFinding.CaloTauProducer = cms.InputTag('tcRecoTauProducer')
+from RecoTauTag.RecoTau.CaloRecoTauDiscriminationByLeadingTrackPtCut_cfi import *
+caloRecoTauDiscriminationByLeadingTrackPtCut.CaloTauProducer = cms.InputTag('tcRecoTauProducer')
+from RecoTauTag.RecoTau.CaloRecoTauDiscriminationByIsolation_cfi import *
+caloRecoTauDiscriminationByIsolation.CaloTauProducer = cms.InputTag('tcRecoTauProducer')
+from RecoTauTag.RecoTau.CaloRecoTauDiscriminationAgainstElectron_cfi import *
+caloRecoTauDiscriminationAgainstElectron.CaloTauProducer = cms.InputTag('tcRecoTauProducer')
+
+TCTau = cms.Sequence(jptRecoTauProducer*
+                     tcRecoTauProducer *
+                     caloRecoTauTagInfoProducer *
+                     caloRecoTauDiscriminationByLeadingTrackFinding *
+                     caloRecoTauDiscriminationByLeadingTrackPtCut *
+                     caloRecoTauDiscriminationByIsolation *
+                     caloRecoTauDiscriminationAgainstElectron)
+
