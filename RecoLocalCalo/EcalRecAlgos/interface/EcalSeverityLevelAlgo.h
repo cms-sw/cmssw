a8aaed1069856255dc1c80e7a851a6086a0cb513
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2010-02-08 14:22:46 +0000

    add spike estimator from neighbours

--- RecoLocalCalo/EcalRecAlgos/interface/EcalSeverityLevelAlgo.h
+++ RecoLocalCalo/EcalRecAlgos/interface/EcalSeverityLevelAlgo.h
@@ -14,13 +14,63 @@ class EcalSeverityLevelAlgo {
                 // - 2 --> recovered (e.g. dead or saturated)
                 // - 3 --> bad, not suitable to be used in the reconstruction
 
-                enum EcalSeverityLevel { kGood, kProblematic, kRecovered, kBad };
+                enum EcalSeverityLevel { kGood, kProblematic, kRecovered, kBad, kWeird };
                 
                 static int severityLevel( const DetId , const EcalRecHitCollection &, const EcalChannelStatus & );
-                static int severityLevel( const EcalRecHit &, const EcalChannelStatus & );
                 static int severityLevel( uint32_t rhFlag, uint16_t dbStatus );
+                static int severityLevel( const EcalRecHit &, const EcalChannelStatus & );
+
         private:
                 static uint16_t retrieveDBStatus( const DetId , const EcalChannelStatus &chStatus );
+
+                /** return the estimator of the signal being a spike
+                 *  based on the neighbours energies
+                 */
+                static float spikeFromNeighbours( const DetId id , const EcalRecHitCollection & );
+                /** return energy of a recHit (if in the collection)
+                 */
+                static float recHitEnergy( const DetId id, const EcalRecHitCollection &recHits );
+
+                // HOME MADE NAVIGATION - to be generalized
+                // (and so moved in a more appropriate place)
+
+                /** Converts a std CMSSW crystal eta index to a c-array index (starting from
+                 * zero and without hole).
+                 */
+                static int iEta2cIndex(int iEta){
+                        return (iEta<0)?iEta+85:iEta+84;
+                }
+                /** Converts a std CMSSW crystal phi index to a c-array index (starting from
+                 * zero and without hole).
+                 */
+                static int iPhi2cIndex(int iPhi){
+                        int iPhi0 = iPhi - 11;
+                        if(iPhi0<0) iPhi0 += 360;
+                        return iPhi0;
+                }
+                /** converse of iEta2cIndex() method.
+                */
+                static int cIndex2iEta(int i){
+                        return (i<85)?i-85:i-84;
+                }
+                /** converse of iPhi2cIndex() method.
+                */
+                static int cIndex2iPhi(int i){
+                        return 1+ ((i+10) % 360);
+                }
+                /** Converts a std CMSSW crystal x or y index to a c-array index (starting
+                 * from zero and without hole).
+                 */
+                int iXY2cIndex(int iX) const{
+                        return iX-1;
+                }
+                /** converse of iXY2cIndex() method.
+                */
+                int cIndex2iXY(int iX0) const{
+                        return iX0+1;
+                }
+
+
 };
 
 #endif
--- RecoLocalCalo/EcalRecAlgos/src/EcalSeverityLevelAlgo.cc
+++ RecoLocalCalo/EcalRecAlgos/src/EcalSeverityLevelAlgo.cc
@@ -24,6 +24,7 @@ int EcalSeverityLevelAlgo::severityLevel( const DetId id,
                 }
         } else {
                 // the channel is in the recHit collection
+                if ( spikeFromNeighbours(id, recHits) > 0.95  ) return kWeird;
                 return severityLevel( *it, chStatus );
         }
         return 0;
@@ -81,4 +82,38 @@ uint16_t EcalSeverityLevelAlgo::retrieveDBStatus( const DetId id, const EcalChan
                         << "! something wrong with EcalChannelStatus in your DB? ";
         }
         return dbStatus;
+}
+
+float EcalSeverityLevelAlgo::spikeFromNeighbours( const DetId id, const EcalRecHitCollection & recHits )
+{
+        // compute R9
+        if ( id.subdetId() == EcalBarrel ) {
+                EBDetId ebId( id );
+                float s9 = 0;
+                int ieta0 = iEta2cIndex( ebId.ieta() );
+                int iphi0 = iPhi2cIndex( ebId.iphi() );
+                for ( int deta = -1; deta <= +1; ++deta ) {
+                        for ( int dphi = -1; dphi <= +1; ++dphi ) {
+                                int eta = ieta0 + deta;
+                                int phi = ( iphi0 + dphi ) % 360;
+                                if ( phi < 0  ) phi += 360;
+                                if ( ! EBDetId::validDetId( cIndex2iEta(eta), cIndex2iPhi(phi) ) ) continue;
+                                EBDetId hitId( cIndex2iEta(eta), cIndex2iPhi(phi) );
+                                s9 += recHitEnergy( hitId, recHits );
+                        }
+                }
+                return recHitEnergy(id, recHits) / s9;
+        }
+        return 0;
+}
+
+float EcalSeverityLevelAlgo::recHitEnergy( const DetId id, const EcalRecHitCollection &recHits )
+{
+        if ( id == DetId(0) ) {
+                return 0;
+        } else {
+                EcalRecHitCollection::const_iterator it = recHits.find( id );
+                if ( it != recHits.end() ) return (*it).energy();
+        }
+        return 0;
 }
