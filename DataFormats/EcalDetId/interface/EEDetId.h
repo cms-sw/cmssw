b6198c69b434d63e276847553a42ee70081cfd6a
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2010-02-08 15:23:02 +0000

    documentation

--- DataFormats/EcalDetId/interface/EEDetId.h
+++ DataFormats/EcalDetId/interface/EEDetId.h
@@ -10,49 +10,160 @@
  *  Crystal/cell identifier class for the ECAL endcap
  *
  *
- *  $Id: EEDetId.h,v 1.21 2009/10/12 18:29:27 meridian Exp $
+ *  $Id: EEDetId.h,v 1.22 2010/02/04 17:09:27 heltsley Exp $
  */
-
-
 class EEDetId : public DetId {
-   public: 
-      enum { Subdet=EcalEndcap};
-      /** Constructor of a null id */
+   public:
+      enum {
+	 /** Sudetector type. Here it is ECAL endcap.
+	  */
+	 Subdet=EcalEndcap
+      };
+      
+      /** Constructor of a null id
+       */
       EEDetId() {}
-      /** Constructor from a raw value */
+      
+      /** Constructor from a raw value
+       * @param rawid det ID number
+       */
       EEDetId(uint32_t rawid) : DetId(rawid) {}
-      /** Constructor from crystal ix,iy,iz (iz=+1/-1) 
-	  or from sc,cr,iz */
-      EEDetId(int i, int j, int iz, int mode = XYMODE);  
-      /** Constructor from a generic cell id */
+      
+      /** Constructor from crystal ix,iy,iz (iz=+1/-1) (mode = XYMODE)
+       * or from sc,cr,iz (mode = SCCRYSTALMODE).
+       * <p>ix runs from 1 to 100 along x-axis of standard CMS coordinates<br>
+       * iy runs from 1 to 100 along y-axis of standard CMS coordinates<br>
+       * iz is -1 for EE- and +1 for EE+<br>
+       * <p>For isc see isc(), for ic see ic()
+       * @see isc(), ic()
+       * @param i ix or isc index
+       * @param j iy or isc index
+       * @param iz iz/zside index: -1 for EE-, +1 for EE+
+       * @param mode pass XYMODE if i j refer to ix, iy, SCCRYSTALMODE if thery refer to isc, ic
+       */
+      EEDetId(int i, int j, int iz, int mode = XYMODE);
+      
+      /** Constructor from a generic cell id
+       * @param id source detid
+       */
       EEDetId(const DetId& id);
-      /// assignment operator
+      
+      /** Assignment operator
+       * @param id source det id
+       */ 
       EEDetId& operator=(const DetId& id);
-
-      /// get the subdetector
-      //  EcalSubdetector subdet() const { return EcalSubdetector(subdetId()); }
+      
+      /** Gets the subdetector
+       * @return subdetectot ID, that is EcalEndcap
+       */
       static EcalSubdetector subdet() { return EcalEndcap;}
-
-      /// get the z-side of the crystal (1/-1)
+      
+      /** Gets the z-side of the crystal (1/-1)
+       * @return -1 for EE-, +1 for EE+
+       */
       int zside() const { return (id_&0x4000)?(1):(-1); }
-      /// get the crystal ix
+      
+      /** Gets the crystal x-index.
+       * @see EEDetId(int, int, int, int) for x-index definition
+       * @return x-index
+       */
       int ix() const { return (id_>>7)&0x7F; }
-      /// get the crystal iy
-      int iy() const { return id_&0x7F; }  
-      /// get the SuperCrystal number
+      
+      /** Get the crystal y-index
+       * @see EEDetId(int, int, int, int) for y-index definition.
+       * @return y-index
+       */
+      int iy() const { return id_&0x7F; }
+      
+      /** Gets the SuperCrystal number within the endcap. This number runs from 1 to 316,
+       * numbers 70 149 228 307 are not used.
+       *
+       * BEWARE: This number is not consistent with indices used in constructor:  see details below.
+       *
+       * Numbering in quadrant 1 of EE+ is the following
+       * \verbatim 
+       *  08 17 27        
+       *  07 16 26 36 45 54     
+       *  06 15 25 35 44 53 62    
+       *  05 14 24 34 43 52 61 69   
+       *  04 13 23 33 42 51 60 68 76  
+       *  03 12 22 32 41 50 59 67 75  
+       *  02 11 21 31 40 49 58 66 74  
+       *  01 10 20 30 39 48 57 65 73 79 
+       *     09 19 29 38 47 56 64 72 78 
+       *        18 28 37 46 55 63 71 77
+       *  
+       *        == THERE IS NO INDEX 70! ==
+       * \endverbatim
+       *
+       * Quadrant 2 indices are deduced by a symetry about y-axis and by adding an offset
+       * of 79.<br>
+       * Quadrant 3 and 4 indices are deduced from quadrant 1 and 2 by a symetry
+       * about x-axis and adding an offset. Quadrant N starts with index 1 + (N-1)*79.
+       *
+       * <p>EE- indices are deduced from EE+ by a symetry about (x,y)-plane (mirrored view). <b>It is
+       * inconsistent with indices used in constructor EEDetId(int, int,int) in
+       * SCCRYSTALMODE</b>. Indices of constructor uses a symetry along y-axis: in principal it
+       * considers the isc as a local index. The discrepancy is most probably due to a bug in the
+       * implementation of this isc() method.
+       */
       int isc() const ;
-      /// get crystal number inside SuperCrystal
+      
+      /** Gets crystal number inside SuperCrystal.
+       * Crystal numbering withing a supercrystal in each quadrant:
+       * \verbatim
+       *                       A y
+       *  (Q2)                 |                    (Q1)
+       *       25 20 15 10 5   |     5 10 15 20 25
+       *       24 19 14  9 4   |     4  9 14 19 24
+       *       23 18 13  8 3   |     3  8 13 18 23
+       *       22 17 12  7 2   |     2  7 12 17 22
+       *       21 16 11  6 1   |     1  6 11 16 21
+       *                       |
+       * ----------------------o---------------------------> x
+       *                       |
+       *       21 16 11  6 1   |     1  6 11 16 21
+       *       22 17 12  7 2   |     2  7 12 17 22
+       *       23 18 13  8 3   |     3  8 13 18 23
+       *       24 19 14  9 4   |     4  9 14 19 24
+       *       25 20 15 10 5   |     5 10 15 20 25
+       *  (Q3)                                       (Q4)
+       * \endverbatim
+       *
+       * @return crystal number from 1 to 25
+       */
       int ic() const;
-      /// get the quadrant of the DetId
+      
+      /** Gets the quadrant of the DetId.
+       * Quadrant number definition, x and y in std CMS coordinates, for EE+:
+       *
+       * \verbatim
+       *                 A y
+       *                 |
+       *          Q2     |    Q1
+       *                 |
+       *       ----------o---------> x
+       *                 |
+       *          Q3     |    Q4
+       *                 |
+       * \endverbatim
+       *
+       * @return quadrant number
+       */
       int iquadrant() const ;
-      // is z positive?
+      
+      /** Checks if crystal is in EE+
+       * @return true for EE+, false for EE-
+       */
       bool positiveZ() const { return id_&0x4000;}
-
+      
       int iPhiOuterRing() const ; // 1-360 else==0 if not on outer ring!
 
       static EEDetId idOuterRing( int iPhi , int zEnd ) ;
 
-      /// get a compact index for arrays
+      /** Gets a compact index for arrays
+       * @return compact index from 0 to kSizeForDenseIndexing-1
+       */
       int hashedIndex() const 
       {
 	 const uint32_t jx ( ix() ) ;
@@ -60,53 +171,127 @@ class EEDetId : public DetId {
 	 return (  ( zside()<0 ? 0 : kEEhalf ) + kdi[jd] + jx - kxf[jd] ) ;
       }
 
+      /** Same as hashedIndex()
+       * @return compact index from 0 to kSizeForDenseIndexing-1
+       */
       uint32_t denseIndex() const { return hashedIndex() ; }
 
+      /** Checks validity of a dense/hashed index
+       * @param din dense/hashed index as returned by hashedIndex() or denseIndex()
+       * method
+       * @return true if index is valid, false otherwise
+       */
       static bool validDenseIndex( uint32_t din ) { return validHashIndex( din ) ; }
 
+      /** Converts a hashed/dense index as defined in hashedIndex() and denseIndex()
+       * methods to a det id.
+       * @param din hashed/dense index
+       * @return det id
+       */
       static EEDetId detIdFromDenseIndex( uint32_t din ) { return unhashIndex( din ) ; }
 
       static bool isNextToBoundary(     EEDetId id ) ;
-
+      
       static bool isNextToDBoundary(    EEDetId id ) ;
 
       static bool isNextToRingBoundary( EEDetId id ) ;
 
-      /// get a DetId from a compact index for arrays
+      /** Gets a DetId from a compact index for arrays. Converse of hashedIndex() method.
+       * @param hi dense/hashed index
+       * @return det id
+       */
       static EEDetId unhashIndex( int hi ) ;
 
-      /// check if a valid hash index
+      /** Checks if a hashed/dense index is valid
+       * @see hashedIndex(), denseIndex()
+       * @param i hashed/dense index
+       * @return true if the index is valid, false otherwise
+       */
       static bool validHashIndex( int i ) { return ( i < kSizeForDenseIndexing ) ; }
 
-      /// check if a valid index combination
-      static bool validDetId( int i, int j, int iz ) ;
+      /** Checks validity of a crystal (x,y.z) index triplet.
+       * @param crystal_ix crystal x-index
+       * @param crystal_iy crystal y-index
+       * @param iz crystal z-index
+       * @see EEDetId(int, int, int, int) for index definition
+       * @return true if valid, false otherwise
+       */
+      static bool validDetId(int crystal_ix, int crystal_iy, int iz);
 
-      //return the distance in x units between two EEDetId
-      static int distanceX(const EEDetId& a,const EEDetId& b); 
-      //return the distance in y units between two EEDetId
+      /** Returns the distance along x-axis in crystal units between two EEDetId
+       * @param a det id of first crystal
+       * @param b det id of second crystal
+       * @return distance
+       */
+      static int distanceX(const EEDetId& a,const EEDetId& b);
+      
+      /** Returns the distance along y-axis in crystal units between two EEDetId
+       * @param a det id of first crystal
+       * @param b det id of second crystal
+       * @return distance
+       */
       static int distanceY(const EEDetId& a,const EEDetId& b); 
 
 
+      /** Gives supercrystal index from endcap *supercrystal* x and y indexes.
+       * @see isc() for the index definition
+       * @param iscCol supercrystal column number: supecrystal x-index for EE+
+       * @param iscRow: supecrystal y-index
+       * @return supercystal index
+       */
       static int isc( int iscCol,   // output is 1-316
-		      int iscRow ) ; // give supercrystal index from endcap *supercrystal* x and y indexes
+		      int iscRow ) ; // 
 
-      
+      /** Lower bound of EE crystal x-index
+       */
       static const int IX_MIN =1;
+
+      /** Lower bound of EE crystal y-index
+       */
       static const int IY_MIN =1;
+      
+      /** Upper bound of EE crystal y-index
+       */
       static const int IX_MAX =100;
+
+      /** Upper bound of EE crystal y-index
+       */
       static const int IY_MAX =100;
+
+      /** Lower bound of supercystal index as defined in isc()
+       */
       static const int ISC_MIN=1;
+
+      /** Lower bound of crystal index within a supercrystal
+       */
       static const int ICR_MIN=1;
+
+      /** Upper bound of supercystal index defined in isc()
+       * <p>Beware it differs from the number of supercrystals in one endcap,
+       * which is 312, because the numbering is not dense.
+       */
       static const int ISC_MAX=316;
+
+      /** Upper bound of crystal index within a supercrystal
+       */
       static const int ICR_MAX=25;
+      
+      enum {
+	 /** Number of crystals per Dee
+	  */
+	 kEEhalf = 7324 ,
+	 /** Number of dense crystal indices, that is number of
+	  * crystals per endcap.
+	  */
+	 kSizeForDenseIndexing = 2*kEEhalf
+      };
 
-
-      enum { kEEhalf = 7324 ,
-	     kSizeForDenseIndexing = 2*kEEhalf } ;
-
-      // function modes for (int, int) constructor
+      /*@{*/
+      /** function modes for EEDetId(int, int, int, int) constructor
+       */
       static const int XYMODE        = 0;
       static const int SCCRYSTALMODE = 1;
+      /*@}*/
 
    private:
 
--- DataFormats/EcalDetId/interface/EcalScDetId.h
+++ DataFormats/EcalDetId/interface/EcalScDetId.h
@@ -1,6 +1,8 @@
 // -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: t; tab-width: 8; -*-
-//$Id: EcalScDetId.h,v 1.4 2009/12/18 13:41:51 fra Exp $
-
+//$Id: EcalScDetId.h,v 1.5 2010/02/04 17:09:27 heltsley Exp $
+//
+// \author Philippe Gras (CEA/Saclay). Code adapted from EEDetId.
+//
 #ifndef EcalDetId_EcalScDetId_h
 #define EcalDetId_EcalScDetId_h
 
@@ -12,61 +14,165 @@
 /** \class EcalScDetId
  *  Supercrystal identifier class for the ECAL endcap.
  *  <P>Note: internal representation of ScDetId:
- *  <CODE>
+ *  \verbatim
  *  31              .               15              .              0
  *  |-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-| 
  *  |  det  |sudet|         0       |1|z|     ix      |     iy      |
  *  +-------+-----+-----------------+-+-+-------------+-------------+
- *  </CODE>
+ *  \endverbatim
  */
 
 class EcalScDetId : public DetId {
  public:
-  /** Constructor of a null id */
+
+  /** Constructor of a null id
+   */
   EcalScDetId();
-  /** Constructor from a raw value */
+  
+  /** Constructor from a raw value
+   * @param rawid det ID number of the supecrystal, as defined in this class
+   * description.
+   */
   EcalScDetId(uint32_t rawid);
-  /** Constructor from crystal ix,iy,iz (iz=+1/-1) 
-   or from sc,cr,iz */
-  EcalScDetId(int ix, int iy, int iz);  
-  /** Constructor from a generic cell id */
+
+  /** Constructor from supercrystal ix,iy,iz (iz=+1/-1)
+   * ix x-index runs from 1 to 20 along x-axis of standard CMS coordinates
+   * iy y-index runs from 1 to 20 along y-axis of standard CMS coordinates
+   * iz z-index (also called "z-side") is -1 for EE- and +1 for EE+
+   * @param ix x-index
+   * @param iy y-index
+   * @param iz z-side /z-index: -1 for EE-, +1 for EE+
+   */
+  EcalScDetId(int ix, int iy, int iz);
+  
+  /** Constructor from a raw value
+   * @param id det ID number
+   */
   EcalScDetId(const DetId& id);
-  /// assignment operator
+
+  /** Assignment operator
+   * @param id source det id
+   */ 
   EcalScDetId& operator=(const DetId& id);
 
-  /// Gets the subdetector
+  /** Gets the subdetector
+   * @return subdetectot ID, that is EcalEndcap
+   */
   EcalSubdetector subdet() const { return EcalSubdetector(subdetId()); }
-  /// Gets the z-side of the crystal (1/-1)
+  
+  /** Gets the z-side of the crystal (1/-1)
+   * @return -1 for EE-, +1 for EE+
+   */
   int zside() const { return (id_&0x4000)?(1):(-1); }
-  /// Gets the crystal ix
+  
+  /** Gets the crystal x-index.
+   * @see EcalDetId(int, int, int) for x-index definition
+   * @return x-index
+   */
   int ix() const { return (id_>>7)&0x7F; }
-  /// Gets the crystal iy
-  int iy() const { return id_&0x7F; }  
-  /// Gets the SuperCrystal number
-  int isc() const ; // within endcap, from 1-316, #70,149,228,307 are EMPTY
-  /// Gets the quadrant of the DetId
+  
+  /** Get the crystal y-index
+   * @see EcalDetId(int, int, int) for y-index definition.
+   * @return y-index
+   */
+  int iy() const { return id_&0x7F; }
+
+  
+  /** Gets the SuperCrystal number within the endcap.
+   * @see EEDetId::isc() for the index description
+   * @return the supercrystal index, a number from 1 to 316. Numbers 
+   * 70,149,228,307 are not used.
+   */
+  int isc() const ;
+  
+  /** Gets the quadrant of the DetId
+   * @see EEDetId::iquadrant()
+   * @return quadrant number, from 1 to 4.
+   */
   int iquadrant() const ;
-  /// Gets a compact index for arrays
+  
+  /** Gets a compact index for arrays. This index runs from 0 to kSizeForDenseIndexing-1
+   *
+   * BEWARE: this index contains 8 holes. Numbers 69, 148, 227, 306, 385, 464, 543, 622
+   * are not used
+   * @return the hased index.
+   */
   int hashedIndex() const { return isc() + ( zside() + 1 )*ISC_MAX/2 - 1 ; } // from 0-631
 
+  /** Same as hashed index.
+   *
+   * BEWARE: despite the method name is suggesting the returned index is not dense! See hashedIndex()
+   */
   uint32_t denseIndex() const { return hashedIndex() ; }
 
+  /** Validates a hashed index.
+   *
+   * BEWARE: this method is broken. It will returns true for the 8 invalid numbers called "holes"
+   * in hashIndex() documentation.
+   * @param din hashed index to validate
+   * @return true if the index is valid, false if it is invalid.
+   */
   static bool validDenseIndex( uint32_t din ) { return ( MIN_HASH<=(int)din && MAX_HASH>=(int)din ) ; }
+
+  
+  /** Validates a hashed index.
+   *
+   * BEWARE: this method is broken. It will returns true for the 8 invalid numbers called "holes"
+   * in hashIndex() documentation.
+   * @param hi hashed index to validate
+   * @return true if the index is valid, false if it is invalid.
+   */
   static bool validHashIndex( int hi ) { return validDenseIndex( hi ) ; }
 
+  /** Lower bound of EE supercrystal x-index
+   */
   static const int IX_MIN=1;
+
+  /** Lower bound of EE supercrystal y-index
+   */
   static const int IY_MIN=1;
+
+  /** Upper bound of EE crystal y-index
+   */
   static const int IX_MAX=20;
+
+  /** Upper bound of EE crystal y-index
+   */
   static const int IY_MAX=20;
+
+  /** Lower bound of supercystal index defined in isc()
+   */
   static const int ISC_MIN=EEDetId::ISC_MIN ;
+  
+  /** Upper bound of supercystal index defined in isc()
+   * Beware it differs from the number of supercrystals in one endcap,
+   * which is itseld 312, because the numbering is not dense.
+   */
   static const int ISC_MAX=EEDetId::ISC_MAX ;
+
+  /** Lower bound of hashed index returned by hashedIndex() and denseIndex() methods
+   */
   static const int MIN_HASH=0;
+
+  /** Upper bound of hashed index returned by hashedIndex() and denseIndex() methods
+   */
   static const int MAX_HASH=2*ISC_MAX - 1;
 
-  /// check if a valid index combination
-  static bool validDetId(int i, int j, int iz) ;
+  /** Checks validity of a crystal (x,y.z) index triplet.
+   * @param ix supercrystal x-index
+   * @param iy supercrystal y-index
+   * @param iz supercrystal z-index (aka z-side)
+   * @see EEDetId(int, int, int) for index definition
+   * @return true if valid, false otherwise
+   */
+  static bool validDetId(int ix, int iy, int iz) ;
 
-  enum { kSizeForDenseIndexing = 2*ISC_MAX } ;
+  enum {
+    /** Number of dense supercrystal indices. This number differs
+     * from the total number of supercystals. See hashedIndex() method.
+     */
+    kSizeForDenseIndexing = 2*ISC_MAX
+  };
 
 // private:
 };
