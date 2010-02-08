00859a52483ccf7d34cd6e3601739910c71ccb6d
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2010-02-08 14:21:23 +0000

    add spike estimator

--- DataFormats/EcalDigi/src/EBDataFrame.cc
+++ DataFormats/EcalDigi/src/EBDataFrame.cc
@@ -1,5 +1,15 @@
 #include "DataFormats/EcalDigi/interface/EBDataFrame.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include<iostream>
+
+float EBDataFrame::spikeEstimator() const
+{
+        if ( size() != 10 ) {
+                edm::LogError("InvalidNumberOfSamples") << "This method only applies to signals sampled 10 times ("
+                        << size() << " samples found)";
+        }
+        return 0.18*sample(4).adc()/sample(5).adc() + sample(6).adc()/sample(5).adc();
+}
 
 
 std::ostream& operator<<(std::ostream& s, const EBDataFrame& digi) {
