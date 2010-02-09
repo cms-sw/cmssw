a17db69892b6e5ee4626ee97ef2d14f5f56d8b3d
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-02-09 11:08:23 +0000

    V01-15-20 HLTrigger/Configuration - ConfDb /dev/CMSSW_3_5_0/XXXX/V16

--- FastSimulation/Configuration/python/blockHLT_8E29_cff.py
+++ FastSimulation/Configuration/python/blockHLT_8E29_cff.py
@@ -1,10 +1,9 @@
-# /dev/CMSSW_3_5_0/8E29/V6 (CMSSW_3_5_0_pre3_HLT2)
+# /dev/CMSSW_3_5_0/8E29/V16 (CMSSW_3_5_0)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_5_0/8E29/V6')
+  tableName = cms.string('/dev/CMSSW_3_5_0/8E29/V16')
 )
-
 
