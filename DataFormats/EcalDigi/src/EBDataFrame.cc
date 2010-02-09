5edcbf36d1368f19d71592fb6df4d667243f1f53
Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch>
2010-02-09 15:24:24 +0000

    code improvement

--- DataFormats/EcalDigi/src/EBDataFrame.cc
+++ DataFormats/EcalDigi/src/EBDataFrame.cc
@@ -8,7 +8,6 @@ float EBDataFrame::spikeEstimator() const
                 edm::LogError("InvalidNumberOfSamples") << "This method only applies to signals sampled 10 times ("
                         << size() << " samples found)";
         }
-        float ped = 1./3. * (sample(0).adc() + sample(1).adc() + sample(2).adc());
         // skip faulty channels
         if ( sample(5).adc() == 0 ) return 10.;
         size_t imax = 0;
@@ -21,6 +20,7 @@ float EBDataFrame::spikeEstimator() const
         }
         // skip early signals
         if ( imax < 4 ) return 10.;
+        float ped = 1./3. * (sample(0).adc() + sample(1).adc() + sample(2).adc());
         return 0.18*(sample(4).adc()-ped)/(sample(5).adc()-ped) + (sample(6).adc()-ped)/(sample(5).adc()-ped);
 }
 
