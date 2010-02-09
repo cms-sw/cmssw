8445fd67fbd1f7309abff52d7a97937bf8835c9e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-09 18:13:40 +0000

    Invalidate forwarding header

--- FWCore/Framework/interface/TriggerNames.h
+++ FWCore/Framework/interface/TriggerNames.h
@@ -1,12 +1,1 @@
-#ifndef FWCore_Framework_TriggerNames_h
-#define FWCore_Framework_TriggerNames_h
-
-// This header is deprecated and will be deleted as
-// soon as possible. We are keeping it now only
-// for backward compatibility reasons.  All code
-// including this header should be modified to 
-// directly include the header in FWCore/Common
-
-#include "FWCore/Common/interface/TriggerNames.h"
-
-#endif
+#error moved to #include "FWCore/Common/interface/TriggerNames.h"
