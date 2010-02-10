915de076df9d9f450543811b5ba51d1e08505484
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-10 17:47:52 +0000

    Can't invalidate header until migration is complete

--- FWCore/Framework/interface/TriggerNames.h
+++ FWCore/Framework/interface/TriggerNames.h
@@ -1,2 +1,12 @@
+#ifndef FWCore_Framework_TriggerNames_h
+#define FWCore_Framework_TriggerNames_h
+
+// This header is deprecated and will be deleted as
+// soon as possible. We are keeping it now only
+// for backward compatibility reasons.  All code
+// including this header should be modified to 
+// directly include the header in FWCore/Common
+
 #include "FWCore/Common/interface/TriggerNames.h"
-//#error moved to #include "FWCore/Common/interface/TriggerNames.h"
+
+#endif
--- FWCore/ParameterSet/interface/InputTag.h
+++ FWCore/ParameterSet/interface/InputTag.h
@@ -1,3 +1,2 @@
 #include "FWCore/Utilities/interface/InputTag.h"
-//#error moved to "FWCore/Utilities/interface/InputTag.h"
 
