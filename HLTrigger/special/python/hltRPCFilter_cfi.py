f3c515e035bd4f1543a80087301fa59bd56d0b70
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2010-02-11 11:38:27 +0000

    adding tag for HLT products

--- HLTrigger/special/python/hltRPCFilter_cfi.py
+++ HLTrigger/special/python/hltRPCFilter_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 hltRPCFilter= cms.EDFilter("HLTRPCFilter",
   rangestrips = cms.untracked.double(4.),
-  rpcRecHits = cms.InputTag('rpcRecHits'),
+# rpcRecHits = cms.InputTag('rpcRecHits'),
+  rpcRecHits = cms.InputTag('hltRpcRecHits'),
   rpcDTPoints = cms.InputTag("rpcPointProducer","RPCDTExtrapolatedPoints"),
   rpcCSCPoints = cms.InputTag("rpcPointProducer","RPCCSCExtrapolatedPoints")
 )
