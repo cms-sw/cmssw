472ae8819fc4e0f61a26935f982692604d41156c
Jie Chen <sha1-fd9183ca9be87d876e001fd5ccf1f012da0983ea@cern.ch>
2010-02-11 15:54:16 +0000

    updated OctX single pho skim

--- SUSYBSMAnalysis/Skimming/python/EXOSinglePhoOct09_EventContent_cfi.py
+++ SUSYBSMAnalysis/Skimming/python/EXOSinglePhoOct09_EventContent_cfi.py
@@ -4,7 +4,8 @@ from Configuration.EventContent.EventContent_cff import *
 
 exoticaSinglePhoOutputModule = cms.OutputModule("PoolOutputModule",
                                           outputCommands = cms.untracked.vstring(),
-                                          SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('exoticaSinglePhoHighetPath')),
+#                                          SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('exoticaSinglePhoHighetPath')),
+                                          SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('exoticaRecoSinglePhoHighetPath')),
                                           dataset = cms.untracked.PSet(filterName = cms.untracked.string('EXOSinglePho'),
                                                                        dataTier = cms.untracked.string('EXOGroup')
     ),
--- SUSYBSMAnalysis/Skimming/python/EXOSinglePhoOct09_cff.py
+++ SUSYBSMAnalysis/Skimming/python/EXOSinglePhoOct09_cff.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 singlePhotonMod = cms.EDFilter("HLTSummaryFilter",
-    summary = cms.InputTag("hltTriggerSummaryAOD","","HLT8E29"), # trigger summary
-    member  = cms.InputTag("hltL1NonIsoHLTNonIsoSinglePhotonEt20HcalIsolFilter","","HLT8E29"),      # filter or collection
-    cut     = cms.string("pt>50"),                     # cut on trigger object
+    summary = cms.InputTag("hltTriggerSummaryAOD","","HLT"), # trigger summary
+    member  = cms.InputTag("hltL1NonIsoHLTNonIsoSinglePhotonEt15HcalIsolFilter","","HLT"),      # filter or collection
+    cut     = cms.string("pt>15"),                     # cut on trigger object
     minN    = cms.int32(1)                  # min. # of passing objects needed
 )
 
