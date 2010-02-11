f1283991c294d5c0ec651d36d2832ed034236387
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2010-02-11 11:09:46 +0000

    test from RECO

--- HLTrigger/special/test/testHLTRPCFilter.py
+++ HLTrigger/special/test/testHLTRPCFilter.py
@@ -1,20 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("HLTRPCFilter")
-
 process.maxEvents = cms.untracked.PSet(
   input = cms.untracked.int32(-1)
 )
 
-process.selRPC = cms.EDFilter("HLTRPCFilter", 
-  rangestrips = cms.untracked.double(4.),
-  rpcRecHits = cms.InputTag('rpcRecHits'),
-  rpcDTPoints = cms.InputTag("rpcPointProducer","RPCDTExtrapolatedPoints"),
-  rpcCSCPoints = cms.InputTag("rpcPointProducer","RPCCSCExtrapolatedPoints")
-)
+process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
+process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
+process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+process.load("Geometry.DTGeometry.dtGeometry_cfi")
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "MC_3XY_V15::All"
+process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.source = cms.Source("PoolSource",
-   fileNames = cms.untracked.vstring('file:/tmp/carrillo/outs/output.root')
+   fileNames = cms.untracked.vstring(
+#	'file:/tmp/carrillo/outs/output.root'
+        '/store/data/Commissioning10/Cosmics/RECO/v3/000/127/155/005F9301-2E16-DF11-B60B-0030487CD6B4.root'
+)
 )
 
 process.FEVT = cms.OutputModule("PoolOutputModule",
@@ -25,6 +29,9 @@ process.FEVT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('/tmp/carrillo/afterfilter.root')
 )
 
-process.selrpc = cms.Path(process.selRPC)
+process.load("RecoLocalMuon.RPCRecHit.rpcPointProducer_cfi")
+process.load("HLTrigger.special.hltRPCFilter_cfi")
+
+process.selrpc = cms.Path(process.rpcPointProducer*process.hltRPCFilter)
 process.outpath = cms.EndPath(process.FEVT)
 
