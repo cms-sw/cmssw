5dff69dba396549f5f683dfc0234a9de85d1aee3
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2010-02-13 07:15:06 +0000

    350p2 rereco cfg

--- Configuration/GlobalRuns/python/rereco_FirstCollisions_ZeroBias_35X.py
+++ Configuration/GlobalRuns/python/rereco_FirstCollisions_ZeroBias_35X.py
@@ -23,7 +23,7 @@ process.load('Configuration/StandardSequences/AlCaRecoStreams_cff')
 process.load('Configuration/EventContent/AlCaRecoOutput_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.2 $'),
     annotation = cms.untracked.string('rereco nevts:100'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -91,7 +91,6 @@ process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
 ### pixelTracks
 #---- replaces ----
 process.pixelTracks.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
-process.pixelTracks.OrderedHitsFactoryPSet.GeneratorPSet.useFixedPreFiltering = True     # was False
 #---- new parameters ----
 process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
 process.pixelTracks.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
@@ -99,7 +98,6 @@ process.pixelTracks.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlin
 ### 0th step of iterative tracking
 #---- replaces ----
 process.newSeedFromTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
-process.newSeedFromTriplets.OrderedHitsFactoryPSet.GeneratorPSet.useFixedPreFiltering = True     # was False
 #---- new parameters ----
 process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)  # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
 process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
@@ -107,7 +105,6 @@ process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag
 ### 2nd step of iterative tracking
 #---- replaces ----
 process.secTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
-process.secTriplets.OrderedHitsFactoryPSet.GeneratorPSet.useFixedPreFiltering = True     # was False
 #---- new parameters ----
 process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)  # was originHalfLength = 17.5; translated assuming sigmaZ ~ 3.8
 process.secTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
@@ -145,18 +142,6 @@ process.zdcreco.firstSample = 4
 process.zdcreco.samplesToAdd = 3
 
 ## EGAMMA
-#removed from upon Egamma request 12 Feb 2010
-#process.ecalDrivenElectronSeeds.SCEtCut = cms.double(1.0)
-#process.ecalDrivenElectronSeeds.applyHOverECut = cms.bool(False)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.z2MinB = cms.double(-0.9)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.z2MaxB = cms.double(0.9)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.r2MinF = cms.double(-1.5)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.r2MaxF = cms.double(1.5)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.rMinI = cms.double(-2.)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.rMaxI = cms.double(2.)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi1Low = cms.double(0.3)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi1High = cms.double(0.3)
-#process.ecalDrivenElectronSeeds.SeedConfiguration.DeltaPhi2 = cms.double(0.3)
 
 process.gsfElectrons.applyPreselection = cms.bool(False)
 process.photons.minSCEtBarrel = 1.
