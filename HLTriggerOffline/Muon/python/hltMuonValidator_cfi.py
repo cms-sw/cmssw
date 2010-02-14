1a364e50446f086e4f33a11ed224d81970be7011
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2010-02-14 16:20:10 +0000

    Added string-based selector members, added configuration parameters for collection names

--- HLTriggerOffline/Muon/python/hltMuonValidator_cfi.py
+++ HLTriggerOffline/Muon/python/hltMuonValidator_cfi.py
@@ -5,19 +5,28 @@ hltMuonValidator = cms.EDAnalyzer("HLTMuonValidator",
     hltProcessName = cms.string("HLT"),
     hltPathsToCheck = cms.vstring("HLT_[^H_]*Mu[^_]*$"),
 
+    genParticleLabel = cms.string("genParticles"       ),
+        recMuonLabel = cms.string("muons"              ),
+         l1CandLabel = cms.string("hltL1extraParticles"),
+         l2CandLabel = cms.string("hltL2MuonCandidates"),
+         l3CandLabel = cms.string("hltL3MuonCandidates"),
+
     parametersTurnOn = cms.vdouble(0,
                                    1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
                                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                                    22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
                                    45, 50, 55, 60, 65, 70,
                                    80, 100, 200, 500
-                       ), 
+                                   ), 
     parametersEta      = cms.vdouble(48, -2.400, 2.400),
     parametersPhi      = cms.vdouble(50, -3.142, 3.142),
 
     # Set cuts placed on the generated muons and matching criteria
-    cutMotherId        = cms.uint32(0),
-    cutsDr             = cms.vdouble(0.4, 0.4, 0.015),
-    TriggerResultLabel = cms.InputTag("TriggerResults","","HLT"),
+    cutMotherId = cms.uint32(0),
+    cutsDr      = cms.vdouble(0.4, 0.4, 0.015),
+    genMuonCut  = cms.string("abs(pdgId) > 12.5 && abs(pdgId) < 13.5 && "
+                             "status     >  0.5 && status     <  1.5    "),
+    #recMuonCut  = cms.string("isGlobalMuon > 0.5"),
+    recMuonCut  = cms.string("pt > 1"),
 
 )
