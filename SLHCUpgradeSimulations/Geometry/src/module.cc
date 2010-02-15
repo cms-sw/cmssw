319806aabfb3d4d3181ebcb6717fb5863776d579
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2010-02-15 19:35:41 +0000

    Put back the DDPixBarTPGStackLayerAlgo definition that was removed from the Phase 1 update for this file

--- SLHCUpgradeSimulations/Geometry/src/module.cc
+++ SLHCUpgradeSimulations/Geometry/src/module.cc
@@ -3,6 +3,7 @@
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinear.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLinearGap.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackLayerAlgo.h"
+#include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarTPGStackLayerAlgo.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixBarStackTrigLayerAlgo.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 #include "SLHCUpgradeSimulations/Geometry/interface/DDPixFwdDiskAlgo.h"
@@ -14,6 +15,7 @@
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinear,      "track:DDPixBarStackLinear");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLinearGap,   "track:DDPixBarStackLinearGap");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackLayerAlgo,   "track:DDPixBarStackLayerAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarTPGStackLayerAlgo,"track:DDPixBarTPGStackLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixBarStackTrigLayerAlgo, "track:DDPixBarStackTrigLayerAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDPixFwdDiskAlgo,      "track:DDPixFwdDiskAlgo");
 DEFINE_SEAL_MODULE ();
