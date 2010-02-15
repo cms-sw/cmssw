e830f8a3cc2c02430e0c32a5bbb738daa91a6adb
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-02-15 12:57:50 +0000

    add unittest

--- RecoLuminosity/LumiDB/python/idDealer.py
+++ RecoLuminosity/LumiDB/python/idDealer.py
@@ -9,6 +9,9 @@ class idDealer(object):
         self.__idTableColumnName = 'NEXTID'
         self.__idTableColumnType = 'unsigned long long'
         
+    def getIDColumnDefinition( self ):
+        return (self.__idTableColumnName, self.__idTableColumnType)
+    
     def getIDforTable( self, tableName ):
         """
         get the new id value to use for the given table
@@ -27,7 +30,7 @@ class idDealer(object):
         except Exception, e:
             raise Exception, str(e)
 
-    def generateNextID( self, tableName ):
+    def generateNextIDForTable( self, tableName ):
         """
         Set the nextID in the IDTableName to current id value + 1 .\n
         Input: ID table name.
@@ -42,13 +45,45 @@ class idDealer(object):
             result = 0
             while ( cursor.next() ):
                 result = cursor.currentRow()[0].data()
-            del query
+            del query            
             dataEditor = tableHandle.dataEditor()
             inputData = coral.AttributeList()
-            inputData.extend( 'newid', self.__idTableColumnType )
-            inputData['newid'].setData(result+1)
-            dataEditor.updateRows('NEXTID = :newid','',inputData)
+            if result==0: #is a new table, no entry in the id table
+                dataEditor.rowBuffer(inputData)
+                inputData[self.__idTableColumnName].setData(result+1)
+                dataEditor.insertRow(inputData) 
+            else:                
+                inputData.extend( 'newid', self.__idTableColumnType )
+                inputData['newid'].setData(result+1)
+                dataEditor.updateRows('NEXTID = :newid','',inputData)
+            return result+1
         except Exception, e:
             raise Exception, str(e)
 
-    pass
+if __name__ == "__main__":
+    fakeIDtableName='Fake_ID'
+    svc=coral.ConnectionService()
+    session=svc.connect('sqlite_file:fake.db')
+    transaction=session.transaction()
+    try:
+        transaction.start(False)
+        schema=session.nominalSchema()
+        idor=idDealer(schema)
+        if schema.existsTable(fakeIDtableName) is False:
+          description=coral.TableDescription()
+          description.setName(fakeIDtableName)
+          description.setPrimaryKey(idor.getIDColumnDefinition()[0])        
+          description.insertColumn(idor.getIDColumnDefinition()[0],idor.getIDColumnDefinition()[1])
+          idtableHandle=schema.createTable(description)
+          idtableHandle.privilegeManager().grantToPublic(coral.privilege_Select)
+        idor.generateNextIDForTable('Fake')
+        print idor.getIDforTable('Fake')
+        transaction.commit()
+        del session
+    except coral.Exception,e:
+        transaction.rollback()
+        del session
+    except Exception, e:
+        print 'failed in unit test'
+        print str(e)
+        del session
