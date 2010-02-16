df7425433f12d00dcd53f9ae596a68f7c6d5d114
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2010-02-16 16:18:00 +0000

    Changes to make the code run in CMSSW_3_1_6

--- JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
+++ JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
@@ -1,17 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.JetPlusTrackCorrections_cfi import *
-JPTZSPCorrectorICone5.ResponseMap =   'JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt'
-JPTZSPCorrectorICone5.EfficiencyMap = 'JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt'
-JPTZSPCorrectorICone5.LeakageMap =    'JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt'
+#JPTZSPCorrectorICone5.ResponseMap =   'JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt'
+#JPTZSPCorrectorICone5.EfficiencyMap = 'JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt'
+#JPTZSPCorrectorICone5.LeakageMap =    'JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt'
 #For CMSSW_3_1_6:
-#JPTZSPCorrectorICone5.NonEfficiencyFile     = 'CMSSW_167_TrackNonEff_one'
-#JPTZSPCorrectorICone5.NonEfficiencyFileResp = 'CMSSW_167_TrackLeakage_one'
-#JPTZSPCorrectorICone5.ResponseFile          = 'CMSSW_31X_resptowers'
+JPTZSPCorrectorICone5.NonEfficiencyFile     = 'CMSSW_167_TrackNonEff_one'
+JPTZSPCorrectorICone5.NonEfficiencyFileResp = 'CMSSW_167_TrackLeakage_one'
+JPTZSPCorrectorICone5.ResponseFile          = 'CMSSW_31X_resptowers'
 
 from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
-from JetMETCorrections.Configuration.ZSPJetCorrections332_cff import *
-#from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
+#from JetMETCorrections.Configuration.ZSPJetCorrections332_cff import *
+from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
 JPTZSPCorrectorICone5.ResponseMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt")
 JPTZSPCorrectorICone5.EfficiencyMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt")
 JPTZSPCorrectorICone5.LeakageMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt")
