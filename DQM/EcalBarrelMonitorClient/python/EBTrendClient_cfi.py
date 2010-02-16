d795616237b06dbafc4a19c864ea59c3e2a7a2f6
Dongwook Jang <sha1-238fd1bb7a3396d331821fd7ca6808ad9eef1aff@cern.ch>
2010-02-16 18:41:43 +0000

    change module name to be consistent with Task modules

--- DQM/EcalBarrelMonitorClient/python/EBTrendClient_cfi.py
+++ DQM/EcalBarrelMonitorClient/python/EBTrendClient_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-eBTrendClient = cms.EDAnalyzer("EBTrendClient",
+ecalBarrelTrendClient = cms.EDAnalyzer("EBTrendClient",
     prefixME = cms.untracked.string('EcalBarrel'),
     enableCleanup = cms.untracked.bool(False),
     mergeRuns = cms.untracked.bool(False),
