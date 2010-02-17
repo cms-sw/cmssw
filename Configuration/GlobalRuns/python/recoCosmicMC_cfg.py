71198c47cb7a6249519ba1808ec0469cd138c107
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-02-17 17:20:00 +0000

    revive this configuration for 3.5

--- Configuration/GlobalRuns/python/recoCosmicMC_cfg.py
+++ Configuration/GlobalRuns/python/recoCosmicMC_cfg.py
@@ -1,56 +1,262 @@
-# The following comments couldn't be translated into the new config version:
-
-#    service = Tracer {}
-
+# Auto generated configuration file
+# using: 
+# Revision: 1.164 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: recoCosmicMC -s RAW2DIGI,RECO,DQM,ALCA:MuAlCalIsolatedMu+RpcCalHLT+TkAlCosmics0T+MuAlStandAloneCosmics+MuAlGlobalCosmics+HcalCalHOCosmics+SiStripCalZeroBias+DQM --datatier RECO --eventcontent RECO --conditions FrontierConditions_GlobalTag,GR09_R_35_V2A::All -n -1 --no_exec --mc --magField 38T --scenario cosmics
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Rec3")
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process = cms.Process('RECO')
 
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.MixingNoPileUp_cff')
+process.load('Configuration.StandardSequences.GeometryExtended_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.ReconstructionCosmics_cff')
+process.load('DQMOffline.Configuration.DQMOfflineCosmicsMC_cff')
+process.load('Configuration.StandardSequences.AlCaRecoStreamsMC_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.EventContent.EventContentCosmics_cff')
 
-# service = Timing {}
-# use Fake cond for MC
-process.load("Configuration.StandardSequences.FakeConditions_cff")
-
-# Conditions (Global Tag is used here):
-#include "Configuration/GlobalRuns/data/FrontierConditionsGRGlobalTag.cff"
-#replace GlobalTag.connect = "oracle://cms_orcoff_int2r/CMS_COND_GENERAL"
-#replace GlobalTag.DBParameters.authenticationPath="/afs/cern.ch/cms/DB/conddb"
-#replace GlobalTag.timetype = "runnumber"
-#replace GlobalTag.globaltag = "GRUMM_V4::All"
-# Magnetic fiuld: force mag field to be 0.0 tesla
-process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
-
-# reconstruction sequence for Global Run
-process.load("Configuration.GlobalRuns.ReconstructionGR_cff")
-
-# offline raw to digi for real data
-#include "Configuration/GlobalRuns/data/RawToDigiGR.cff"
-# offline raw to digi for MC
-process.load("Configuration.StandardSequences.RawToDigi_cff")
-
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.164 $'),
+    annotation = cms.untracked.string('recoCosmicMC nevts:-1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
-process.Timing = cms.Service("Timing")
-
-process.source = cms.Source("PoolSource",
-    #       untracked vstring fileNames = {'file:GlobalMar08_37965_A.root'}
-    fileNames = cms.untracked.vstring('file:gen_sim_digi_raw_100k_200pre9.root')
-)
-
-process.FEVT = cms.OutputModule("PoolOutputModule",
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('RECO')
-    ),
-    fileName = cms.untracked.string('reco-gr.root')
-)
-
 process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True) ## default is false
 
 )
-process.allPath = cms.Path(process.RawToDigi*process.reconstructionGR)
-process.outpath = cms.EndPath(process.FEVT)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('recoCosmicMC_DIGI2RAW.root')
+)
 
+# Output definition
+process.output = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.RECOEventContent.outputCommands,
+    fileName = cms.untracked.string('recoCosmicMC_RAW2DIGI_RECO_DQM_ALCA.root'),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    )
+)
+
+# Additional output definition
+process.ALCARECOStreamMuAlStandAloneCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlStandAloneCosmics')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlStandAloneCosmics_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*', 
+        'keep L1AcceptBunchCrossings_*_*_*', 
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*', 
+        'keep *_TriggerResults_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('MuAlStandAloneCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlStandAloneCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamHcalCalHOCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOHcalCalHOCosmics')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep HOCalibVariabless_*_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('HcalCalHOCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('HcalCalHOCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamTkAlCosmics0T = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOTkAlCosmicsCTF0T', 
+            'pathALCARECOTkAlCosmicsCosmicTF0T', 
+            'pathALCARECOTkAlCosmicsRS0T')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOTkAlCosmics*0T_*_*', 
+        'keep siStripDigis_DetIdCollection_*_*', 
+        'keep L1AcceptBunchCrossings_*_*_*', 
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*', 
+        'keep *_TriggerResults_*_*', 
+        'keep Si*Cluster*_si*Clusters_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('TkAlCosmics0T.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('TkAlCosmics0T'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamSiStripCalZeroBias = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOSiStripCalZeroBias')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOSiStripCalZeroBias_*_*', 
+        'keep *_calZeroBiasClusters_*_*', 
+        'keep *_MEtoEDMConverter_*_*', 
+        'keep L1AcceptBunchCrossings_*_*_*', 
+        'keep *_TriggerResults_*_*'),
+    fileName = cms.untracked.string('SiStripCalZeroBias.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('SiStripCalZeroBias'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamRpcCalHLT = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECORpcCalHLT')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_muonDTDigis_*_*', 
+        'keep CSCDetIdCSCWireDigiMuonDigiCollection_*_*_*', 
+        'keep CSCDetIdCSCStripDigiMuonDigiCollection_*_*_*', 
+        'keep DTLayerIdDTDigiMuonDigiCollection_*_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*', 
+        'keep RPCDetIdRPCDigiMuonDigiCollection_*_*_*', 
+        'keep recoMuons_muonsNoRPC_*_*', 
+        'keep L1MuRegionalCands_*_RPCb_*', 
+        'keep L1MuRegionalCands_*_RPCf_*', 
+        'keep L1MuGMTCands_*_*_*', 
+        'keep L1MuGMTReadoutCollection_*_*_*'),
+    fileName = cms.untracked.string('RpcCalHLT.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('RpcCalHLT'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamMuAlGlobalCosmics = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlGlobalCosmics')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlGlobalCosmics_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*', 
+        'keep L1AcceptBunchCrossings_*_*_*', 
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*', 
+        'keep *_TriggerResults_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('MuAlGlobalCosmics.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlGlobalCosmics'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+process.ALCARECOStreamMuAlCalIsolatedMu = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('pathALCARECOMuAlCalIsolatedMu')
+    ),
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_ALCARECOMuAlCalIsolatedMu_*_*', 
+        'keep *_muonCSCDigis_*_*', 
+        'keep *_muonDTDigis_*_*', 
+        'keep *_muonRPCDigis_*_*', 
+        'keep *_dt1DRecHits_*_*', 
+        'keep *_dt2DSegments_*_*', 
+        'keep *_dt4DSegments_*_*', 
+        'keep *_csc2DRecHits_*_*', 
+        'keep *_cscSegments_*_*', 
+        'keep *_rpcRecHits_*_*', 
+        'keep L1AcceptBunchCrossings_*_*_*', 
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*', 
+        'keep *_TriggerResults_*_*', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('MuAlCalIsolatedMu.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('MuAlCalIsolatedMu'),
+        dataTier = cms.untracked.string('ALCARECO')
+    )
+)
+
+# Other statements
+process.GlobalTag.globaltag = 'GR09_R_35_V2A::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstructionCosmics)
+process.dqmoffline_step = cms.Path(process.DQMOfflineCosmics)
+process.pathALCARECOHcalCalHOCosmics = cms.Path(process.seqALCARECOHcalCalHOCosmics)
+process.pathALCARECOMuAlStandAloneCosmics = cms.Path(process.seqALCARECOMuAlStandAloneCosmics*process.ALCARECOMuAlStandAloneCosmicsDQM)
+process.pathALCARECOTkAlZMuMu = cms.Path(process.seqALCARECOTkAlZMuMu*process.ALCARECOTkAlZMuMuDQM)
+process.pathALCARECOTkAlCosmicsCTF0T = cms.Path(process.seqALCARECOTkAlCosmicsCTF0T*process.ALCARECOTkAlCosmicsCTF0TDQM)
+process.pathALCARECOMuAlBeamHalo = cms.Path(process.seqALCARECOMuAlBeamHalo*process.ALCARECOMuAlBeamHaloDQM)
+process.pathALCARECOTkAlCosmicsRS0THLT = cms.Path(process.seqALCARECOTkAlCosmicsRS0THLT*process.ALCARECOTkAlCosmicsRS0TDQM)
+process.pathALCARECOTkAlCosmicsCTF = cms.Path(process.seqALCARECOTkAlCosmicsCTF*process.ALCARECOTkAlCosmicsCTFDQM)
+process.pathALCARECOHcalCalIsoTrk = cms.Path(process.seqALCARECOHcalCalIsoTrk*process.ALCARECOHcalCalIsoTrackDQM)
+process.pathALCARECOHcalCalHO = cms.Path(process.seqALCARECOHcalCalHO*process.ALCARECOHcalCalHODQM)
+process.pathALCARECOTkAlCosmicsCTFHLT = cms.Path(process.seqALCARECOTkAlCosmicsCTFHLT*process.ALCARECOTkAlCosmicsCTFDQM)
+process.pathALCARECOTkAlCosmicsRS0T = cms.Path(process.seqALCARECOTkAlCosmicsRS0T*process.ALCARECOTkAlCosmicsRS0TDQM)
+process.pathALCARECOTkAlCosmicsCosmicTFHLT = cms.Path(process.seqALCARECOTkAlCosmicsCosmicTFHLT*process.ALCARECOTkAlCosmicsCosmicTFDQM)
+process.pathALCARECOHcalCalMinBias = cms.Path(process.seqALCARECOHcalCalMinBias*process.ALCARECOHcalCalPhisymDQM)
+process.pathALCARECOTkAlMuonIsolated = cms.Path(process.seqALCARECOTkAlMuonIsolated*process.ALCARECOTkAlMuonIsolatedDQM)
+process.pathALCARECOTkAlUpsilonMuMu = cms.Path(process.seqALCARECOTkAlUpsilonMuMu*process.ALCARECOTkAlUpsilonMuMuDQM)
+process.pathALCARECOHcalCalDijets = cms.Path(process.seqALCARECOHcalCalDijets*process.ALCARECOHcalCalDiJetsDQM)
+process.pathALCARECOMuAlZMuMu = cms.Path(process.seqALCARECOMuAlZMuMu*process.ALCARECOMuAlZMuMuDQM)
+process.pathALCARECOEcalCalPi0Calib = cms.Path(process.seqALCARECOEcalCalPi0Calib*process.ALCARECOEcalCalPi0CalibDQM)
+process.pathALCARECOTkAlBeamHalo = cms.Path(process.seqALCARECOTkAlBeamHalo*process.ALCARECOTkAlBeamHaloDQM)
+process.pathALCARECOSiPixelLorentzAngle = cms.Path(process.seqALCARECOSiPixelLorentzAngle)
+process.pathALCARECOTkAlCosmicsCosmicTF0T = cms.Path(process.seqALCARECOTkAlCosmicsCosmicTF0T*process.ALCARECOTkAlCosmicsCosmicTF0TDQM)
+process.pathALCARECOEcalCalElectron = cms.Path(process.seqALCARECOEcalCalElectron*process.ALCARECOEcalCalElectronCalibDQM)
+process.pathALCARECOTkAlCosmicsCTF0THLT = cms.Path(process.seqALCARECOTkAlCosmicsCTF0THLT*process.ALCARECOTkAlCosmicsCTF0TDQM)
+process.pathALCARECOMuAlCalIsolatedMu = cms.Path(process.seqALCARECOMuAlCalIsolatedMu*process.ALCARECOMuAlCalIsolatedMuDQM*process.ALCARECODTCalibrationDQM)
+process.pathALCARECOSiStripCalZeroBias = cms.Path(process.seqALCARECOSiStripCalZeroBias*process.ALCARECOSiStripCalZeroBiasDQM)
+process.pathALCARECOTkAlCosmicsRSHLT = cms.Path(process.seqALCARECOTkAlCosmicsRSHLT*process.ALCARECOTkAlCosmicsRSDQM)
+process.pathALCARECOEcalCalEtaCalib = cms.Path(process.seqALCARECOEcalCalEtaCalib*process.ALCARECOEcalCalEtaCalibDQM)
+process.pathALCARECOSiStripCalMinBias = cms.Path(process.seqALCARECOSiStripCalMinBias)
+process.pathALCARECODQM = cms.Path(process.MEtoEDMConverter)
+process.pathALCARECOTkAlLAS = cms.Path(process.seqALCARECOTkAlLAS*process.ALCARECOTkAlLASDQM)
+process.pathALCARECOTkAlMinBias = cms.Path(process.seqALCARECOTkAlMinBias*process.ALCARECOTkAlMinBiasDQM)
+process.pathALCARECOTkAlCosmicsRS = cms.Path(process.seqALCARECOTkAlCosmicsRS*process.ALCARECOTkAlCosmicsRSDQM)
+process.pathALCARECORpcCalHLT = cms.Path(process.seqALCARECORpcCalHLT)
+process.pathALCARECOHcalCalGammaJet = cms.Path(process.seqALCARECOHcalCalGammaJet)
+process.pathALCARECOMuAlBeamHaloOverlaps = cms.Path(process.seqALCARECOMuAlBeamHaloOverlaps*process.ALCARECOMuAlBeamHaloOverlapsDQM)
+process.pathALCARECOTkAlCosmicsCosmicTF0THLT = cms.Path(process.seqALCARECOTkAlCosmicsCosmicTF0THLT*process.ALCARECOTkAlCosmicsCosmicTF0TDQM)
+process.pathALCARECOHcalCalNoise = cms.Path(process.seqALCARECOHcalCalNoise)
+process.pathALCARECOMuAlOverlaps = cms.Path(process.seqALCARECOMuAlOverlaps*process.ALCARECOMuAlOverlapsDQM)
+process.pathALCARECOTkAlCosmicsCosmicTF = cms.Path(process.seqALCARECOTkAlCosmicsCosmicTF*process.ALCARECOTkAlCosmicsCosmicTFDQM)
+process.pathALCARECOEcalCalPhiSym = cms.Path(process.seqALCARECOEcalCalPhiSym*process.ALCARECOEcalCalPhisymDQM)
+process.pathALCARECOMuAlGlobalCosmics = cms.Path(process.seqALCARECOMuAlGlobalCosmics*process.ALCARECOMuAlGlobalCosmicsDQM)
+process.pathALCARECOTkAlJpsiMuMu = cms.Path(process.seqALCARECOTkAlJpsiMuMu*process.ALCARECOTkAlJpsiMuMuDQM)
+process.endjob_step = cms.Path(process.endOfProcess)
+process.out_step = cms.EndPath(process.output)
+process.ALCARECOStreamMuAlStandAloneCosmicsOutPath = cms.EndPath(process.ALCARECOStreamMuAlStandAloneCosmics)
+process.ALCARECOStreamHcalCalHOCosmicsOutPath = cms.EndPath(process.ALCARECOStreamHcalCalHOCosmics)
+process.ALCARECOStreamTkAlCosmics0TOutPath = cms.EndPath(process.ALCARECOStreamTkAlCosmics0T)
+process.ALCARECOStreamSiStripCalZeroBiasOutPath = cms.EndPath(process.ALCARECOStreamSiStripCalZeroBias)
+process.ALCARECOStreamRpcCalHLTOutPath = cms.EndPath(process.ALCARECOStreamRpcCalHLT)
+process.ALCARECOStreamMuAlGlobalCosmicsOutPath = cms.EndPath(process.ALCARECOStreamMuAlGlobalCosmics)
+process.ALCARECOStreamMuAlCalIsolatedMuOutPath = cms.EndPath(process.ALCARECOStreamMuAlCalIsolatedMu)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.dqmoffline_step,process.pathALCARECOTkAlCosmicsCTF0T,process.pathALCARECOTkAlCosmicsCosmicTF0T,process.pathALCARECOTkAlCosmicsRS0T,process.pathALCARECOMuAlStandAloneCosmics,process.pathALCARECORpcCalHLT,process.pathALCARECOMuAlGlobalCosmics,process.pathALCARECOMuAlCalIsolatedMu,process.pathALCARECOHcalCalHOCosmics,process.pathALCARECODQM,process.pathALCARECOSiStripCalZeroBias,process.endjob_step,process.out_step,process.ALCARECOStreamMuAlStandAloneCosmicsOutPath,process.ALCARECOStreamHcalCalHOCosmicsOutPath,process.ALCARECOStreamTkAlCosmics0TOutPath,process.ALCARECOStreamSiStripCalZeroBiasOutPath,process.ALCARECOStreamRpcCalHLTOutPath,process.ALCARECOStreamMuAlGlobalCosmicsOutPath,process.ALCARECOStreamMuAlCalIsolatedMuOutPath)
