21a3269407dec4e4aa27a86f9a1953bb2e3eddac
Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch>
2010-02-17 14:31:56 +0000

    Changing trigger to 8e29-menu path

--- DQM/Physics/python/ewkElecDQM_cfi.py
+++ DQM/Physics/python/ewkElecDQM_cfi.py
@@ -13,7 +13,7 @@ ewkElecDQM = cms.EDAnalyzer("EwkElecDQM",
 
       # Main cuts ->
 #      MuonTrig = cms.untracked.string("HLT_Mu9"),
-      ElecTrig = cms.untracked.string("HLT_Ele10_SW_L1R"), #NOT HLT_Ele10_LW_L1R ???
+      ElecTrig = cms.untracked.string("HLT_Ele10_LW_L1R"), 
 #      UseTrackerPt = cms.untracked.bool(True), # ??
 #      PtCut = cms.untracked.double(25.0),
       PtCut = cms.untracked.double(10.0),
