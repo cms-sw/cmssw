72c837c398f3a2ffa806b630291eadfeb94635fc
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-02-18 09:37:43 +0000

    *** empty log message ***

--- DQM/EcalCommon/tools/gui/refreshRunIndex.sh
+++ DQM/EcalCommon/tools/gui/refreshRunIndex.sh
@@ -12,6 +12,7 @@ ulimit -c 0
 cd ${HOME}/DQM/dqm-GUI
 
 LD_LIBRARY_PATH=
+VO_CMS_SW_DIR=${PWD}/rpms
 source rpms/cmsset_default.sh
 source rpms/slc5_amd64_gcc434/cms/dqmgui/5.1.7b/etc/profile.d/env.sh
 
