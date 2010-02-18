07a250d04ac2986c9e0da5799bf926f51670fc7b
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2010-02-18 15:06:22 +0000

    first commit

---
+++ CondFormats/ESObjects/src/T_EventSetup_ESRecHitRatioCuts.cc
@@ -0,0 +1,5 @@
+// user include files
+#include "CondFormats/ESObjects/interface/ESRecHitRatioCuts.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+EVENTSETUP_DATA_REG(ESRecHitRatioCuts);
