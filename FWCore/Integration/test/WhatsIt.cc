dae59e5871b519323a7e0cfad4f265fc54399d3c
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-18 23:05:02 +0000

    Migration of EventSetup registration

--- FWCore/Integration/test/WhatsIt.cc
+++ FWCore/Integration/test/WhatsIt.cc
@@ -8,7 +8,6 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Fri Jun 24 13:53:00 EDT 2005
-// $Id: WhatsIt.cc,v 1.1 2005/10/15 01:46:18 wmtan Exp $
 //
 
 // system include files
@@ -16,6 +15,6 @@
 // user include files
 #include "FWCore/Integration/test/WhatsIt.h"
 
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+#include "FWCore/Utilities/interface/typelookup.h"
 
-EVENTSETUP_DATA_REG(edmtest::WhatsIt);
+TYPELOOKUP_DATA_REG(edmtest::WhatsIt);
