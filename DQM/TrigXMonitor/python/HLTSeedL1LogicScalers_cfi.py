af729b20ce1e71d2fbec83e43aeb8b4a18b8c518
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-02-18 13:37:29 +0000

    add MinBiasEcal, L1SingleForJet, SingleLooseIsoTau20,  also change flag to BeforeMask=False

--- DQM/TrigXMonitor/python/HLTSeedL1LogicScalers_cfi.py
+++ DQM/TrigXMonitor/python/HLTSeedL1LogicScalers_cfi.py
@@ -6,11 +6,14 @@ hltSeedL1Logic = cms.EDAnalyzer('HLTSeedL1LogicScalers',
     l1GtLabel = cms.InputTag("l1GtUnpack","","HLT"),
     triggerResultsLabel = cms.InputTag("TriggerResults","","HLT"),
     processname = cms.untracked.string("HLT"),
-    l1BeforeMask = cms.untracked.bool(True),
+    l1BeforeMask = cms.untracked.bool(False),
     monitorPaths = cms.vstring(
             'HLT_L1MuOpen',
             'HLT_L1Mu',
-            'HLT_Mu3'
+            'HLT_Mu3',
+            'HLT_L1SingleForJet',
+            'HLT_SingleLooseIsoTau20',
+            'HLT_MinBiasEcal'
       )
 
 )
