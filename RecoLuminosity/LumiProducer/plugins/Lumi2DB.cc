91739a4afe304358ee0cdf1c4508f3019e843f62
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-02-19 14:14:49 +0000

    add writing

--- RecoLuminosity/LumiProducer/plugins/Lumi2DB.cc
+++ RecoLuminosity/LumiProducer/plugins/Lumi2DB.cc
@@ -10,6 +10,7 @@
 #include "CoralBase/AttributeList.h"
 #include "CoralBase/Attribute.h"
 #include "CoralBase/AttributeSpecification.h"
+#include "CoralBase/Blob.h"
 #include "CoralBase/Exception.h"
 #include "CoralBase/TimeStamp.h"
 #include "CoralBase/MessageStream.h"
@@ -26,8 +27,12 @@
 #include "RelationalAccess/ITable.h"
 #include "RelationalAccess/ITableDataEditor.h"
 #include "RelationalAccess/IBulkOperation.h"
-#include "RecoLuminosity/LumiProducer/interface/DataPipe.h"
 #include "RecoLuminosity/LumiProducer/interface/LumiRawDataStructures.h"
+#include "RecoLuminosity/LumiProducer/interface/DataPipe.h"
+#include "RecoLuminosity/LumiProducer/interface/LumiNames.h"
+#include "RecoLuminosity/LumiProducer/interface/idDealer.h"
+#include <iostream>
+#include <cstring>
 #include "TFile.h"
 #include "TTree.h"
 namespace lumi{
@@ -136,6 +141,122 @@ void lumi::Lumi2DB::retrieveData( unsigned int runnumber){
     }
     lumiresult.push_back(h);
   }
+  coral::ConnectionService* svc=new coral::ConnectionService;
+  coral::ISessionProxy* session=svc->connect(m_dest,coral::Update);
+  try{
+    unsigned int totallumils=35;
+    unsigned int totalcmsls=32;
+    session->transaction().start(false);
+    coral::ISchema& schema=session->nominalSchema();
+    lumi::idDealer idg(schema);
+    coral::ITable& summarytable=schema.tableHandle(LumiNames::lumisummaryTableName());
+    coral::ITable& detailtable=schema.tableHandle(LumiNames::lumidetailTableName());
+    coral::AttributeList summaryData;
+    summaryData.extend<unsigned long long>("LUMISUMMARY_ID");
+    summaryData.extend<unsigned int>("RUNNUM");
+    summaryData.extend<std::string>("LUMIVERSION");
+    summaryData.extend<float>("DTNORM");
+    summaryData.extend<float>("LUMINORM");
+    summaryData.extend<float>("INSTLUMI");
+    summaryData.extend<float>("INSTLUMIERROR");
+    summaryData.extend<short>("INSTLUMIQUALITY");
+    summaryData.extend<short>("LUMISECTIONQUALITY");
+    summaryData.extend<bool>("CMSALIVE");
+    summaryData.extend<unsigned int>("LUMILSNUM");
+    coral::IBulkOperation* summaryInserter=summarytable.dataEditor().bulkInsert(summaryData,totallumils);
+    
+    coral::AttributeList detailData;
+    detailData.extend<unsigned long long>("LUMIDETAIL_ID");
+    detailData.extend<unsigned long long>("LUMISUMMARY_ID");
+    detailData.extend<coral::Blob>("BXLUMIVALUE");
+    detailData.extend<coral::Blob>("BXLUMIERROR");
+    detailData.extend<coral::Blob>("BXLUMIQUALITY");
+    detailData.extend<std::string>("ALGONAME");
+    coral::IBulkOperation* detailInserter=detailtable.dataEditor().bulkInsert(detailData,totallumils);
+    //loop over lumi LS
+    unsigned long long& lumisummary_id=summaryData["LUMISUMMARY_ID"].data<unsigned long long>();
+    unsigned int& lumirunnum = summaryData["RUNNUM"].data<unsigned int>();
+    std::string& lumiversion = summaryData["LUMIVERSION"].data<std::string>();
+    float& dtnorm = summaryData["DTNORM"].data<float>();
+    float& luminorm = summaryData["LUMINORM"].data<float>();
+    float& instlumi = summaryData["INSTLUMI"].data<float>();
+    float& instlumierror = summaryData["INSTLUMIERROR"].data<float>();
+    short& instlumiquality = summaryData["INSTLUMIQUALITY"].data<short>();
+    short& lumisectionquality = summaryData["LUMISECTIONQUALITY"].data<short>();
+    bool& cmsalive = summaryData["CMSALIVE"].data<bool>();
+    unsigned int& lumilsnum = summaryData["LUMILSNUM"].data<unsigned int>();
+    
+    unsigned long long& lumidetail_id=detailData["LUMIDETAIL_ID"].data<unsigned long long>();
+    unsigned long long& d2lumisummary_id=detailData["LUMISUMMARY_ID"].data<unsigned long long>();
+    coral::Blob& bxlumivalue=detailData["BXLUMIVALUE"].data<coral::Blob>();
+    coral::Blob& bxlumierror=detailData["BXLUMIERROR"].data<coral::Blob>();
+    coral::Blob& bxlumiquality=detailData["BXLUMIQUALITY"].data<coral::Blob>();
+    std::string& algoname=detailData["ALGONAME"].data<std::string>();
+    for(unsigned int i=1;i<=totallumils;++i){
+      lumisummary_id = idg.generateNextIDForTable(LumiNames::lumisummaryTableName());
+      lumirunnum = runnumber;
+      lumiversion = "0";
+      dtnorm = 1.05;
+      luminorm = 1.2;
+      instlumi = 0.9;
+      instlumierror = 0.01;
+      instlumiquality = 8;
+      lumisectionquality = 16;
+      unsigned int cmslsnum = 0;
+      bool iscmsalive = false;
+      if(i<=totalcmsls){
+	iscmsalive=true;
+	cmslsnum=i;
+      }
+      cmsalive=iscmsalive;
+      lumilsnum=cmslsnum;
+      //fetch a new id value 
+      //insert the new row
+      summaryInserter->processNextIteration();
+      d2lumisummary_id=i;
+      for( unsigned int j=0; j<3; ++j ){
+	lumidetail_id=idg.generateNextIDForTable(LumiNames::lumidetailTableName());
+	if(j==0) algoname=std::string("ET");
+	if(j==1) algoname=std::string("OCC1");
+	if(j==2) algoname=std::string("OCC2");
+	float lumivalue[3564];
+	std::memset((void*)&lumivalue,0,sizeof(float)*3564 );
+	float lumierror[3564];
+	std::memset((void*)&lumierror,0,sizeof(float)*3564 );
+	int lumiquality[3564];
+	std::memset((void*)&lumiquality,0,sizeof(int)*3564 );
+	bxlumivalue.resize(sizeof(float)*3564);
+	bxlumierror.resize(sizeof(float)*3564);
+	bxlumiquality.resize(sizeof(int)*3564);
+	void* bxlumivalueStartAddress=bxlumivalue.startingAddress();
+	void* bxlumierrorStartAddress=bxlumierror.startingAddress();
+	void* bxlumiqualityStartAddress=bxlumiquality.startingAddress();
+	for( unsigned int k=0; k<3546; ++k ){	    
+	  lumivalue[k]=1.5;
+	  lumierror[k]=0.1;
+	  lumiquality[k]=1;
+	}
+	std::memmove(bxlumivalueStartAddress,lumivalue,sizeof(float)*3564);
+	std::memmove(bxlumierrorStartAddress,lumierror,sizeof(float)*3564);
+	std::memmove(bxlumiqualityStartAddress,lumiquality,sizeof(int)*3564);
+	detailInserter->processNextIteration();
+      }
+    }
+    detailInserter->flush();
+    summaryInserter->flush();
+    delete summaryInserter;
+    delete detailInserter;
+  }catch( const coral::Exception& er){
+    std::cout<<"database problem "<<er.what()<<std::endl;
+    session->transaction().rollback();
+    delete session;
+    delete svc;
+    throw er;
+  }
+  //delete detailInserter;
+  session->transaction().commit();
+  delete session;
+  delete svc;
 }
 const std::string lumi::Lumi2DB::dataType() const{
   return "LUMI";
