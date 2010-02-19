33e76ac014c71ea2746939d85186605013304837
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-19 21:20:32 +0000

    Do not attempt to unnecessarily access provenance that may have been dropped

--- FWCore/Framework/src/Principal.cc
+++ FWCore/Framework/src/Principal.cc
@@ -590,10 +590,9 @@ namespace edm {
     provenances.clear();
     for (const_iterator i = begin(), iEnd = end(); i != iEnd; ++i) {
       if ((*i)->provenanceAvailable()) {
-	// The folowing "and" should be done in this order.  In rare cases, not fully understood,
-	// where there is no product, there is no per event/lumi/run provenance for the product,
-	// and the isPresent() call asserts.
-	if ((*i)->provenance()->product().present() && (*i)->provenance()->isPresent()) {
+	// We do not attempt to get the event/lumi/run status from the provenance,
+	// because the per event provenance may have been dropped.
+	if ((*i)->provenance()->product().present()) {
 	   provenances.push_back((*i)->provenance());
 	}
       }
