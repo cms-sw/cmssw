7dc5954c6692651a9a1b6f7192865a0d5f1a8880
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2010-02-22 13:31:27 +0000

    fist save, then commit. not vice-versa

--- MuonAnalysis/MuonAssociators/python/muonHLTL1Match_cfi.py
+++ MuonAnalysis/MuonAssociators/python/muonHLTL1Match_cfi.py
@@ -5,25 +5,24 @@ from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi i
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
 
-muonL1Match = cms.EDProducer("L1MuonMatcher",
+muonHLTL1Match = cms.EDProducer("HLTL1MuonMatcher",
     src = cms.InputTag("muons"),
 
     # L1 Muon collection, and preselection on that collection
-    matched      = cms.InputTag("hltL1extraParticles"),
-    preselection = cms.string("pt >= 3"),
+    matched     = cms.InputTag("patTrigger"),
+    pathName    = cms.string('HLT_L1MuOpen'),
+    filterLabel = cms.string(''), # set to a non-empty string to select L1 objects also on the filter label
 
     # Choice of matching algorithm
     useTrack = cms.string("tracker"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
-    useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra
+    useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra (not AOD)
     useSimpleGeometry = cms.bool(True), # just use a cylinder plus two disks.
 
     # Matching Criteria
-    maxDeltaPhi = cms.double(6),
-    maxDeltaR   = cms.double(0.5),
+    maxDeltaR = cms.double(0.3),
 
-    # Fake filter lavels for output
-    setL1Label = cms.string("l1"),
-    setPropLabel = cms.string("propagated"),
+    # Fake filter lavels for the object propagated to the second muon station
+    setPropLabel = cms.string("propagatedToM2"),
 
     # Write extra ValueMaps
     writeExtraInfo = cms.bool(False),
