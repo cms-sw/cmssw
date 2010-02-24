1a3c6b9fb42b4080eeb3fa6f55d48c9839d0f2fc
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2010-02-24 16:52:26 +0000

    Moved to dqmSaver to correctly merge multiple runs

--- HLTriggerOffline/Muon/test/hltMuonPostProcessor_cfg.py
+++ HLTriggerOffline/Muon/test/hltMuonPostProcessor_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("EDMtoMEConvert")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 2000
 
-process.load("DQMServices.Components.EDMtoMEConverter_cff")
+process.load('Configuration.StandardSequences.EDMtoMEAtJobEnd_cff')
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("HLTriggerOffline.Muon.HLTMuonPostVal_cff")
 process.load("HLTriggerOffline.Muon.HLTMuonQualityTester_cfi")
@@ -18,17 +18,16 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:hltMuonValidator.root')
 )
 
-process.output = process.hltMuonPostMain.clone()
-process.output.outputFileName = cms.untracked.string(
-    'hltMuonPostProcessor.root'
-)
-
-
-process.postprocessorpath = cms.Path(
-    process.EDMtoMEConverter *
+process.postprocessor_path = cms.Path(
     process.hltMuonPostProcessors *
-    process.hltMuonQualityTester *
-    process.output
+    process.hltMuonQualityTester
 )
+
+process.edmtome_path = cms.Path(process.EDMtoME)
+process.dqmsave_path = cms.Path(process.DQMSaver)
+
+process.schedule = cms.Schedule(process.edmtome_path,
+                                process.postprocessor_path,
+                                process.dqmsave_path)
 
 process.DQMStore.referenceFileName = ''
