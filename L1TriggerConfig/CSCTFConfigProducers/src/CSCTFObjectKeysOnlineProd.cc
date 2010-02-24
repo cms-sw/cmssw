db80886fbb4589b81669c5d3b26d8431cbabc571
Gian Piero Di Giovanni <sha1-97d7914cdbb84beaaa6fa850086d502bb992ecda@cern.ch>
2010-02-24 17:53:10 +0000

    keys producer to account for new Configuration SP-by-SP and new Pt LUTs implementation

--- L1TriggerConfig/CSCTFConfigProducers/src/CSCTFObjectKeysOnlineProd.cc
+++ L1TriggerConfig/CSCTFConfigProducers/src/CSCTFObjectKeysOnlineProd.cc
@@ -1,37 +1,54 @@
 #include "L1TriggerConfig/CSCTFConfigProducers/interface/CSCTFObjectKeysOnlineProd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-void
-CSCTFObjectKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
+void CSCTFObjectKeysOnlineProd::fillObjectKeys( ReturnType pL1TriggerKey )
 {
-     std::string csctfKey = pL1TriggerKey->subsystemKey( L1TriggerKey::kCSCTF ) ;
-
-     // Execute SQL queries to get data from OMDS (using key) and make C++ object.
-
-     if( !csctfKey.empty() )
-     {
-	  l1t::OMDSReader::QueryResults objectKeyResults =
-	       m_omdsReader.basicQuery(
-		    "SP1_KEY",
-		    "CMS_CSC_TF",
-		    "CSCTF_CONF",
-		    "CSCTF_CONF.CSCTF_KEY",
-		    m_omdsReader.singleAttribute( csctfKey  ) );
-
-	  if( objectKeyResults.queryFailed() || objectKeyResults.numberRows() != 1 ) // check if query was successful
-	  {
-	       edm::LogError( "L1-O2O" ) << "Problem with CSCTF  key." ;
-	       return ;
-	  }
-
-	  long long int objectKey ;
-	  objectKeyResults.fillVariable( objectKey ) ;
-	  std::stringstream ss;
-	  ss<<objectKey;
-	  std::string strKey=ss.str();
+  std::string csctfKey = pL1TriggerKey->subsystemKey( L1TriggerKey::kCSCTF ) ;
 
 
-	  pL1TriggerKey->add( "L1MuCSCTFConfigurationRcd", "L1MuCSCTFConfiguration", strKey ) ;
-     }
+
+  if( !csctfKey.empty() )
+    {
+      //----------------------------------------------------------------------------
+      // register the main CSCTF key
+      pL1TriggerKey->add( "L1MuCSCTFConfigurationRcd", 
+			  "L1MuCSCTFConfiguration", 
+			  csctfKey ) ;
+      //----------------------------------------------------------------------------
+      
+      //----------------------------------------------------------------------------
+      // PT LUT
+      //
+      // while the sp configuration can change from sector to sector, the pt LUT file 
+      //loaded in the CSCTF boards is the same for each SP => the same PTLUT_VERSION
+      // RETRIEVE THE VERSION FROM THE SP1 CONFIGURATION 
+      //
+      // e.g., SELECT PTLUT_VERSION FROM CMS_CSC_TF.CSCTF_SP_CONF WHERE CSCTF_SP_CONF.SP_KEY = '1702100001'
+      // e.g., CSCTF key of the type day/month/year, e.g. 170210 = 17th February 2010
+      std::string sp1key = csctfKey + "0001";
+
+      // query
+      l1t::OMDSReader::QueryResults objectKeyResults =
+	m_omdsReader.basicQuery(
+				"PTLUT_VERSION",
+				"CMS_CSC_TF",
+				"CSCTF_SP_CONF",
+				"CSCTF_SP_CONF.SP_KEY",
+				m_omdsReader.singleAttribute( sp1key  ) );
+
+      // check if query was successful
+      if( objectKeyResults.queryFailed() || objectKeyResults.numberRows() != 1 ) 
+	{
+	  edm::LogError( "L1-O2O" ) << "Problem with CSCTF key while retrieving "
+				    << "the PTLUT_VERSION" ;
+	  return ;
+	}
+	 
+      // register the pt LUT key
+      std::string ptLutKey;
+      objectKeyResults.fillVariable( ptLutKey ) ;
+      pL1TriggerKey->add( "L1MuCSCPtLutRcd", "L1MuCSCPtLut", ptLutKey ) ;
+      //----------------------------------------------------------------------------
+    }
 }
 
