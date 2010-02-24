0982370be5551178aaac195ba45fbdfa3e31efe2
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-24 18:02:20 +0000

    remove unnecessary and obsolete include

--- TrackingTools/KalmanUpdators/plugins/module.cc
+++ TrackingTools/KalmanUpdators/plugins/module.cc
@@ -7,7 +7,6 @@
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
-#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
 
 DEFINE_FWK_EVENTSETUP_MODULE(KFUpdatorESProducer);
 DEFINE_FWK_EVENTSETUP_MODULE(KFSwitching1DUpdatorESProducer);
