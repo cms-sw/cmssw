1c6d8425068737608b730b01636e148ed28a09bd
Nikolaos Rompotis <sha1-013c00d56849712eaa9efb4912fb1ea8ac9bfbf7@cern.ch>
2010-02-25 15:06:21 +0000

    additions for egamma conversion rejection etc

--- ElectroWeakAnalysis/ZEE/test/pat_ZeePlots.py
+++ ElectroWeakAnalysis/ZEE/test/pat_ZeePlots.py
@@ -17,20 +17,23 @@ process.options = cms.untracked.PSet(
 process.source = cms.Source("PoolSource", 
      #fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/r/rompotis/RedigiSummer08RootTrees/WenuRedigi_RECO_SAMPLE.root')
      fileNames = cms.untracked.vstring(
-    'file:zee_Summer09-MC_31X_V3_AODSIM_v1_AODSIM.root'
+    'rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_5_1/RelValZEE/GEN-SIM-RECO/MC_3XY_V21-v1/0001/005DD63C-B51C-DF11-BA60-0030487CD6E6.root',
+    #'file:zee_Summer09-MC_31X_V3_AODSIM_v1_AODSIM.root'
     #'file:/tmp/rompotis/Run123505_LS70-80_BscMinBiasInnerThreshold.root',
     )
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 ## Load additional processes
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 ## global tags:
+## >>>> to run with relval
+process.GlobalTag.globaltag = cms.string('MC_3XY_V21::All')
 ## >>>> to run with data
 # process.GlobalTag.globaltag = cms.string('GR09_P_V7::All')
 ## >>>> to run MC summer09 production
-process.GlobalTag.globaltag = cms.string('MC_31X_V5::All')
+#process.GlobalTag.globaltag = cms.string('MC_31X_V5::All')
 #process.GlobalTag.globaltag = cms.string('STARTUP31X_V4::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
@@ -47,12 +50,12 @@ process.load("PhysicsTools.PatAlgos.patSequences_cff")
 #
 # for ecal isolation: set the correct name of the ECAL rechit collection
 # 
-process.eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
-process.eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")
+#process.eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
+#process.eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")
 #
 #
-process.eidRobustHighEnergy.reducedBarrelRecHitCollection = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
-process.eidRobustHighEnergy.reducedEndcapRecHitCollection = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")     
+#process.eidRobustHighEnergy.reducedBarrelRecHitCollection = cms.InputTag("reducedEcalRecHitsEB", "", "RECO")
+#process.eidRobustHighEnergy.reducedEndcapRecHitCollection = cms.InputTag("reducedEcalRecHitsEE", "", "RECO")     
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## CHOICE OF THE HLT PATH     this section is not used from PAT
 ##                            
@@ -72,8 +75,8 @@ process.eidRobustHighEnergy.reducedEndcapRecHitCollection = cms.InputTag("reduce
 ##                                specify the names of the MET collections that you need here %%%%
 ##                                                                                             #%%
 ## if you don't specify anything the default MET is the raw Calo MET                           #%%
-process.layer1RawCaloMETs = process.layer1METs.clone(                                          #%%
-    metSource = cms.InputTag("met"),
+process.layer1RawCaloMETs = process.patMETs.clone(                                          #%%
+    metSource = cms.InputTag("met","","RECO"),
     addTrigMatch = cms.bool(False),
     addMuonCorrections = cms.bool(False),
     addGenMET = cms.bool(False),
@@ -81,62 +84,57 @@ process.layer1RawCaloMETs = process.layer1METs.clone(                           
 ## specify here what you want to have on the plots! <===== MET THAT YOU WANT ON THE PLOTS  %%%%%%%
 myDesiredMetCollection = 'layer1RawCaloMETs'
 ## modify the sequence of the MET creation:                                                    #%%
-process.makeLayer1METs = cms.Sequence(process.layer1RawCaloMETs)
+process.makePatMETs = cms.Sequence(process.patMETCorrections * process.patMETs *
+                                   process.layer1RawCaloMETs)
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## modify the final pat sequence: keep only electrons + METS (muons are needed for met corrections)
 process.load("RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff")
-process.electronEcalRecHitIsolationLcone.ecalBarrelRecHitProducer = cms.InputTag("reducedEcalRecHitsEB")
-process.electronEcalRecHitIsolationScone.ecalBarrelRecHitProducer = cms.InputTag("reducedEcalRecHitsEB")
-process.electronEcalRecHitIsolationLcone.ecalEndcapRecHitProducer = cms.InputTag("reducedEcalRecHitsEE")
-process.electronEcalRecHitIsolationScone.ecalEndcapRecHitProducer = cms.InputTag("reducedEcalRecHitsEE")
-#
-process.electronEcalRecHitIsolationLcone.ecalBarrelRecHitCollection = cms.InputTag("")
-process.electronEcalRecHitIsolationScone.ecalBarrelRecHitCollection = cms.InputTag("")
-process.electronEcalRecHitIsolationLcone.ecalEndcapRecHitCollection = cms.InputTag("")
-process.electronEcalRecHitIsolationScone.ecalEndcapRecHitCollection = cms.InputTag("")
-#
-#
 process.patElectronIsolation = cms.Sequence(process.egammaIsolationSequence)
-process.allLayer1Electrons.userIsolation = cms.PSet(
-    tracker = cms.PSet(
-      src = cms.InputTag("electronTrackIsolationScone"),
-    ),
-    ecal = cms.PSet(
-      src = cms.InputTag("electronEcalRecHitIsolationLcone"),
-    ),
-    hcal = cms.PSet(
-      src = cms.InputTag("electronHcalTowerIsolationLcone"),
-    ),
-    )
-process.allLayer1Electrons.isoDeposits = cms.PSet()
-process.allLayer1Electrons.addElectronID = cms.bool(False)
-process.allLayer1Electrons.electronIDSources = cms.PSet()
-process.allLayer1Electrons.addGenMatch = cms.bool(False)
-process.allLayer1Electrons.embedGenMatch = cms.bool(False)
-process.allLayer1Electrons.embedHighLevelSelection = cms.bool(False)
-##
-process.allLayer1Muons.addGenMatch = cms.bool(False)
-process.allLayer1Muons.embedGenMatch = cms.bool(False)
-##
-process.makeAllLayer1Electrons = cms.Sequence(process.patElectronIsolation*process.allLayer1Electrons)
-process.makeAllLayer1Muons = cms.Sequence(process.allLayer1Muons)
 #
-process.allLayer1Objects = cms.Sequence(process.makeAllLayer1Electrons+process.makeAllLayer1Muons+process.makeLayer1METs)
-process.selectedLayer1Objects = cms.Sequence(process.selectedLayer1Electrons+process.selectedLayer1Muons)
-process.cleanLayer1Objects  = cms.Sequence(process.cleanLayer1Muons*process.cleanLayer1Electrons)
-process.countLayer1Objects  = cms.Sequence(process.countLayer1Electrons+process.countLayer1Muons)
+process.patElectrons.isoDeposits = cms.PSet()
+process.patElectrons.userIsolation = cms.PSet(
+       tracker = cms.PSet(
+            src = cms.InputTag("electronTrackIsolationScone"),
+        ),
+        ecal = cms.PSet(
+            src = cms.InputTag("electronEcalRecHitIsolationLcone"),
+        ),
+        hcal = cms.PSet(
+            src = cms.InputTag("electronHcalTowerIsolationLcone"),
+        ),
+        user = cms.VPSet(),
 
-process.patDefaultSequence = cms.Sequence(process.allLayer1Objects * process.selectedLayer1Objects *
-                                          process.cleanLayer1Objects*process.countLayer1Objects
-                                          )
+    )
+
+##
+## Pre-calculated electron identification selections
+##
+## set the variable false if you don't need them, or if you use your own PSet
+##
+## any input tag you set corresponds to a valuemap that either it is stored in the event
+## or you create it yourself
+process.patElectrons.addElectronID = cms.bool(True)
+process.patElectrons.electronIDSources = cms.PSet(
+    eidRobustLoose      = cms.InputTag("eidRobustLoose"),
+    eidRobustTight      = cms.InputTag("eidRobustTight"),
+    eidLoose            = cms.InputTag("eidLoose"),
+    eidTight            = cms.InputTag("eidTight"),
+    )
+##
+process.patElectrons.addGenMatch = cms.bool(False)
+process.patElectrons.embedGenMatch = cms.bool(False)
+process.makePatElectrons = cms.Sequence(process.patElectronIsolation*process.patElectrons)
+process.makePatCandidates = cms.Sequence(process.makePatElectrons+process.makePatMuons+process.makePatMETs)
+
+process.patDefaultSequence = cms.Sequence(process.makePatCandidates)
 
 ##  ################################################################################
 ##
 ##  the filter to select the candidates from the data samples
 ##
 ## WARNING: you may want to modify this item:  T R I G G E R     S E L E C T I O N
-HLT_process_name = "HLT8E29"   # options: HLT or HLT8E29 >>>>>>> to run with summer09 samples
-# HLT_process_name = "HLT"   # >>>>>>> to run with data
+#HLT_process_name = "HLT8E29"   # options: HLT or HLT8E29 >>>>>>> to run with summer09 samples
+HLT_process_name = "HLT"   # >>>>>>> to run with data
 # trigger path selection
 HLT_path_name    = "HLT_Ele15_LW_L1R"
 # trigger filter name
@@ -154,14 +152,23 @@ process.zeeFilter = cms.EDFilter('ZeeCandidateFilter',
                                  hltpathFilter = cms.untracked.InputTag(HLT_filter_name,"",HLT_process_name),
                                  electronMatched2HLT = cms.untracked.bool(True),
                                  electronMatched2HLT_DR = cms.untracked.double(0.2),
+                                 # exra variable calculation
+                                 calculateConversionRejection = cms.untracked.bool(True),
+                                 calculateValidFirstPXBHit = cms.untracked.bool(True),
+                                 calculateExpectedMissingHits = cms.untracked.bool(True),
                                  # electrons and MET
-                                 electronCollectionTag = cms.untracked.InputTag("selectedLayer1Electrons","","PAT"),
+                                 electronCollectionTag = cms.untracked.InputTag("patElectrons","","PAT"),
                                  metCollectionTag = cms.untracked.InputTag(myDesiredMetCollection,"","PAT")
                                  )
 ####################################################################################
 ##
 ## the Z selection that you prefer
 selection_a2 = cms.PSet (
+    useConversionRejection = cms.untracked.bool(True),
+    useValidFirstPXBHit = cms.untracked.bool(True),
+    useExpectedMissingHits =cms.untracked.bool(True),
+    maxNumberOfExpectedMissingHits = cms.untracked.int32(1),
+    
     trackIso_EB = cms.untracked.double(7.2),
     ecalIso_EB = cms.untracked.double(5.7),
     hcalIso_EB = cms.untracked.double(8.1),
@@ -186,6 +193,17 @@ selection_inverse = cms.PSet (
 selection_secondLeg = cms.PSet (
     ## set this to true if you want to switch on diff 2nd leg selection
     useDifferentSecondLegSelection = cms.untracked.bool(True),
+    ## preselection criteria are independent of useDifferentSecondLegSelection
+    #  set them to False if you don't want them
+    useConversionRejection2 = cms.untracked.bool(True),
+    useValidFirstPXBHit2 = cms.untracked.bool(True),
+    useExpectedMissingHits2 =cms.untracked.bool(True),
+    maxNumberOfExpectedMissingHits2 = cms.untracked.int32(1),    
+    ##
+    usePrecalcID2 = cms.untracked.bool(False),
+    usePrecalcIDType2 = cms.untracked.string('eidLoose'),
+    usePrecalcIDSign2 = cms.untracked.string('='),
+    usePrecalcIDValue2 = cms.untracked.double(1),    
     ##
     trackIso2_EB = cms.untracked.double(7.2),
     ecalIso2_EB = cms.untracked.double(5.7),
@@ -210,6 +228,10 @@ selection_secondLeg = cms.PSet (
 process.plotter = cms.EDAnalyzer('ZeePlots',
                                  selection_a2,
                                  selection_secondLeg,
+                                 usePrecalcID = cms.untracked.bool(False),
+                                 usePrecalcIDType = cms.untracked.string('eidTight'),
+                                 usePrecalcIDSign = cms.untracked.string('='),
+                                 usePrecalcIDValue = cms.untracked.double(1),
                                  zeeCollectionTag = cms.untracked.InputTag("zeeFilter","selectedZeeCandidates","PAT")
                                  )
 
