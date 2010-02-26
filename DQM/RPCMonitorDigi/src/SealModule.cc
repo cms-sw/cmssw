1304467f484e3a5bbf814c50ef282b49010d648f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-02-26 20:37:06 +0000

    Use current macros, not deprecated ones

--- DQM/RPCMonitorDigi/src/SealModule.cc
+++ DQM/RPCMonitorDigi/src/SealModule.cc
@@ -1,11 +1,10 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-DEFINE_SEAL_MODULE();
 #include "DQM/RPCMonitorDigi/interface/RPCMonitorDigi.h"
-DEFINE_ANOTHER_FWK_MODULE(RPCMonitorDigi);
+DEFINE_FWK_MODULE(RPCMonitorDigi);
 #include "DQM/RPCMonitorDigi/interface/RPCTTUMonitor.h"
-DEFINE_ANOTHER_FWK_MODULE(RPCTTUMonitor);
+DEFINE_FWK_MODULE(RPCTTUMonitor);
 #include "DQM/RPCMonitorDigi/interface/RPCEfficiency.h"
-DEFINE_ANOTHER_FWK_MODULE(RPCEfficiency);
+DEFINE_FWK_MODULE(RPCEfficiency);
 
