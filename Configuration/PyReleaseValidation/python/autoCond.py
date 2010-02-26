ce1708c6fd151b4e1b1cf70890ca795a3af81c31
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-02-26 15:25:50 +0000

    Update to V24 of the GT

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,6 +1,6 @@
 
-autoCond = { 'mc'      : 'MC_3XY_V21::All',
-             'startup' : 'START3X_V21::All',
+autoCond = { 'mc'      : 'MC_3XY_V24::All',
+             'startup' : 'START3X_V24::All',
              'craft08' : 'CRAFT08_R_V9::All',
              'craft09' : 'CRAFT09_R_V10::All',
              }
