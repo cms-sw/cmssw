733709382c4a30f33664f13de5a1b53fce4aceeb
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-02-26 14:16:45 +0000

    use string hltkey

--- RecoLuminosity/LumiProducer/plugins/HLTConfDummy2DB.cc
+++ RecoLuminosity/LumiProducer/plugins/HLTConfDummy2DB.cc
@@ -4,6 +4,8 @@
 #include "RelationalAccess/ISessionProxy.h"
 #include "RelationalAccess/ITransaction.h"
 #include "RelationalAccess/ISchema.h"
+#include "RelationalAccess/IQuery.h"
+#include "RelationalAccess/ICursor.h"
 #include "RelationalAccess/ITable.h"
 #include "RelationalAccess/ITableDataEditor.h"
 #include "RelationalAccess/IBulkOperation.h"
@@ -20,7 +22,7 @@ namespace lumi{
   class HLTConfDummy2DB : public DataPipe{
   public:
     explicit HLTConfDummy2DB(const std::string& dest);
-    virtual void retrieveData( unsigned int hltconfigId);
+    virtual void retrieveData( unsigned int runnumber);
     virtual const std::string dataType() const;
     virtual const std::string sourceType() const;
     virtual ~HLTConfDummy2DB();
@@ -29,10 +31,11 @@ namespace lumi{
   //implementation
   //
   HLTConfDummy2DB::HLTConfDummy2DB(const std::string& dest):DataPipe(dest){}
-  void HLTConfDummy2DB::retrieveData( unsigned int hltconfigId){
+  void HLTConfDummy2DB::retrieveData( unsigned int runnumber){
     //
     //generate dummy configuration data for the given hltconfid and write data to LumiDB
     //
+    std::string fakehltkey("/cdaq/Cosmic/V12");
     coral::ConnectionService* svc=new coral::ConnectionService;
     lumi::DBConfig dbconf(*svc);
     if(!m_authpath.empty()){
@@ -41,29 +44,78 @@ namespace lumi{
     coral::ISessionProxy* session=svc->connect(m_dest,coral::Update);
     try{
       unsigned int totalhltpath=126;
+      bool writeKeyToSunSummary=false;
+      session->transaction().start(true);
+      //
+      //check if this hltkey is also registered in runsummary table
+      //
+      coral::AttributeList rqBindVariables;
+      rqBindVariables.extend("runnumber",typeid(unsigned int));
+      rqBindVariables["runnumber"].data<unsigned int>()=runnumber;
+      coral::AttributeList rqResult;
+      rqResult.extend("HLTKEY",typeid(std::string));
+      coral::IQuery* rq=session->nominalSchema().tableHandle(LumiNames::runsummaryTableName()).newQuery();
+      rq->addToOutputList("HLTKEY");
+      rq->setCondition("RUNNUM = :runnumber",rqBindVariables);
+      rq->defineOutput(rqResult);
+      coral::ICursor& rqCursor=rq->execute();
+      std::string hltkey;
+      unsigned int s=0;
+      while( rqCursor.next() ){
+	const coral::AttributeList& row=rqCursor.currentRow();
+	hltkey=row["HLTKEY"].data<std::string>();
+	++s;
+      }
+      if(s==0 || hltkey.empty()){
+	std::cout<<"hltkey is not registered for requested run"<<std::endl;
+	writeKeyToSunSummary=true;
+      }
+      if(s>1){
+	throw lumi::Exception("hltkey is not unique for the requested run","retrieveData","HLTConfDummy2DB");
+      }
+      if( s!=0 && hltkey!=fakehltkey ){
+	throw lumi::Exception("exist a different hltkey for the requested run","retrieveData","HLTConfDummy2DB");
+      }
+      session->transaction().commit();
+      delete rq;
       session->transaction().start(false);
       coral::ISchema& schema=session->nominalSchema();
+
       coral::ITable& hltconftable=schema.tableHandle(LumiNames::trghltMapTableName());
       coral::AttributeList hltconfData;
-      hltconfData.extend<unsigned int>("HLTCONFID");
+      hltconfData.extend<std::string>("HLTKEY");
       hltconfData.extend<std::string>("HLTPATHNAME");
       hltconfData.extend<std::string>("L1SEED");
       coral::IBulkOperation* hltconfInserter=hltconftable.dataEditor().bulkInsert(hltconfData,200);
       //
       //loop over hltpaths
       //
-      hltconfData["HLTCONFID"].data<unsigned int>()=hltconfigId;
+      hltconfData["HLTKEY"].data<std::string>()=fakehltkey;
       std::string& hltpathname=hltconfData["HLTPATHNAME"].data<std::string>();
       std::string& l1seed=hltconfData["L1SEED"].data<std::string>();
       for(unsigned int i=1;i<=totalhltpath;++i){
-	  char c[10];
-	  ::sprintf(c,"-%d",i);
-	  hltpathname=std::string("FakeHLTPATH_")+std::string(c);
-	  l1seed==std::string("FakeL1SeedExpression_")+std::string(c);
-	  hltconfInserter->processNextIteration();
+	char c[10];
+	::sprintf(c,"-%d",i);
+	hltpathname=std::string("FakeHLTPATH_")+std::string(c);
+	l1seed==std::string("FakeL1SeedExpression_")+std::string(c);
+	hltconfInserter->processNextIteration();
       }
       hltconfInserter->flush();
       delete hltconfInserter;
+      if(writeKeyToSunSummary){
+	coral::ITable& rtable=session->nominalSchema().tableHandle(LumiNames::runsummaryTableName());
+	coral::ITableDataEditor& rEditor = rtable.dataEditor();
+	coral::AttributeList rdata;
+	rdata.extend("hltkey",typeid(std::string));
+	rdata.extend("runnumber",typeid(unsigned int));
+	long nrows=rEditor.updateRows("HLTKEY =:hltkey","RUNNUM=:runnumber",rdata);
+	rdata["hltkey"].data<std::string>()=fakehltkey;
+	rdata["runnumber"].data<unsigned int>()=runnumber;
+	if(nrows==0){
+	  throw lumi::Exception("could not update the row","retrieveData","HLTConfDummy2DB");
+	}
+      }
+
     }catch( const coral::Exception& er){
       std::cout<<"database problem "<<er.what()<<std::endl;
       session->transaction().rollback();
@@ -71,7 +123,6 @@ namespace lumi{
       delete svc;
       throw er;
     }
-    //delete detailInserter;
     session->transaction().commit();
     delete session;
     delete svc;
