07d2228aaf60ab8801384506c61db49b501f266e
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2010-02-27 00:35:48 +0000

    Make random #s mutable, and support CaloVPECorrection interface, and apply thermal noise to signal

--- SimCalorimetry/HcalSimAlgos/interface/HPDIonFeedbackSim.h
+++ SimCalorimetry/HcalSimAlgos/interface/HPDIonFeedbackSim.h
@@ -6,52 +6,52 @@
 //
 // Project: HPD ion feedback
 // Author: T.Yetkin University of Iowa, Feb. 16, 2010
-// $Id: HPDIonFeedbackSim.h,v 1.1 2010/02/23 23:57:03 rpw Exp $
+// $Id: HPDIonFeedbackSim.h,v 1.1 2010/02/24 01:56:49 rpw Exp $
 // --------------------------------------------------------
 
 #ifndef HcalSimAlgos_HPDIonFeedbackSim_h
 #define HcalSimAlgos_HPDIonFeedbackSim_h
 
-#include <memory>
-#include <utility>
-#include <iostream>
-#include <vector>
-#include <string>
 #include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
 #include "DataFormats/HcalDetId/interface/HcalDetId.h"
 #include "CalibFormats/HcalObjects/interface/HcalDbService.h"
 #include "CalibFormats/CaloObjects/interface/CaloSamples.h"
-
+#include "DataFormats/DetId/interface/DetId.h"
+#include "SimCalorimetry/CaloSimAlgos/interface/CaloVPECorrection.h"
 // CLHEP Random numbers
 #include "CLHEP/Random/RandBinomial.h"
 #include "CLHEP/Random/RandFlat.h"
 #include "CLHEP/Random/RandGaussQ.h"
 #include "CLHEP/Random/RandPoissonQ.h"
+class CaloVShape;
 
-class HPDIonFeedbackSim{
-  
+class HPDIonFeedbackSim: public CaloVPECorrection
+{
   public:
-    HPDIonFeedbackSim(const edm::ParameterSet&);
+    explicit HPDIonFeedbackSim(const edm::ParameterSet&);
     ~HPDIonFeedbackSim();
     
     //copied from HFSimParameters.h
     void setDbService(const HcalDbService * service) {theDbService = service;}
+    /// need a shaper in order to set thermal noise
+    void setShape(const CaloVShape * shape) {theShape = shape;}
     void setRandomEngine(CLHEP::HepRandomEngine & engine);
 
     // in units of fC
-    double getIonFeedbackFromPE(DetId detId, double npe, bool doThermal);
+    virtual double correctPE(const DetId & detId, double npe) const;
     double getIonFeedback(DetId detId, double signal, double pedWidth, bool doThermal, bool isInGeV);
 
-    void applyToSignalInPE(CaloSamples & samples);
+    void addThermalNoise(CaloSamples & samples);
 
   private:
     double fCtoGeV(const DetId & detId) const;
     const HcalDbService * theDbService;
+    const CaloVShape * theShape;
     
-    CLHEP::RandBinomial * theRandBinomial;
-    CLHEP::RandFlat*      theRandFlat;
-    CLHEP::RandGaussQ *   theRandGauss;
-    CLHEP::RandPoissonQ*  theRandPoissonQ; 
+    mutable CLHEP::RandBinomial * theRandBinomial;
+    mutable CLHEP::RandFlat*      theRandFlat;
+    mutable CLHEP::RandGaussQ *   theRandGauss;
+    mutable CLHEP::RandPoissonQ*  theRandPoissonQ; 
 
 };
 #endif
