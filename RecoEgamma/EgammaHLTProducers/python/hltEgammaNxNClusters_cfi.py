225415e06a1c394fa2aad45085a0021c1178823d
Alessio Ghezzi <sha1-3da5f3e84c1124d2ab879fd4cbeb5c72c62ea780@cern.ch>
2010-02-28 17:07:20 +0000

    added new cfi for the pi0 clustering

---
+++ RecoEgamma/EgammaHLTProducers/python/hltEgammaNxNClusters_cfi.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+hltSimple3x3Clusters= cms.EDProducer("EgammaHLTNxNClusterProducer",
+   barrelHitProducer = cms.InputTag('hltEcalRegionalPi0EtaRecHit','EcalRecHitsEB' ),
+   endcapHitProducer = cms.InputTag('hltEcalRegionalPi0EtaRecHit','EcalRecHitsEE' ),
+   useRecoFlag = cms.bool( False ),
+   flagLevelRecHitsToUse = cms.int32( 1 ),
+   useDBStatus = cms.bool( True ),
+   statusLevelRecHitsToUse = cms.int32( 1 ),
+   barrelClusterCollection  = cms.string("Simple3x3ClustersBarrel"),
+   endcapClusterCollection  = cms.string("Simple3x3ClustersEndcap"),
+   clusSeedThr = cms.double( 0.5 ),
+   clusSeedThrEndCap = cms.double( 1.0),
+   doBarrel = cms.bool( True ),
+   doEndcaps = cms.bool( True ),
+   clusEtaSize = cms.int32(3),
+   clusPhiSize = cms.int32(3),
+   posCalc_logweight = cms.bool( True ),
+   posCalc_t0_barl = cms.double( 7.4 ),
+   posCalc_t0_endc = cms.double( 3.1 ),
+   posCalc_t0_endcPresh = cms.double( 1.2 ),
+   posCalc_w0 = cms.double( 4.2 ),
+   posCalc_x0 = cms.double( 0.89 ),
+   maxNumberofSeeds = cms.int32( 1000 ),
+   maxNumberofClusters = cms.int32( 200 ),
+   debugLevel = cms.int32( 0 )
+)
