1987de7a7dba9e6380025eb4596f9fabd710eb67
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2010-03-01 10:52:26 +0000

    Fix typo ProjectLibs/CoreLibs.

--- Fireworks/Core/standalone_build/project.mk
+++ Fireworks/Core/standalone_build/project.mk
@@ -1,7 +1,7 @@
 ifeq ($(shell uname), Linux)
    BoostLibs := boost_thread  boost_signals  boost_filesystem boost_program_options boost_regex
    RootLibs  := Cint Core  Net Tree Hist Matrix MathCore MathMore Reflex EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
-   CoreLibs  := $(BoostLibs) $(RootLibs) CLHEP-2.0.4.2 gsl gslcblas HepMC pcre sigc-2.0 nsl crypt dl uuid
+   ProjectLibs  := $(BoostLibs) $(RootLibs) CLHEP-2.0.4.2 gsl gslcblas HepMC pcre sigc-2.0 nsl crypt dl uuid
   
    ProjectIncludes := external/inc/boost/1.40.0-cms3/include
    ProjectIncludes += external/inc/clhep/2.0.4.2-cms/include 
@@ -14,12 +14,12 @@ ifeq ($(shell uname), Linux)
    ifeq ($(shell uname), Darwin)
       BoostLibs := boost_thread  boost_iostreams boost_signals  boost_filesystem boost_program_options boost_regex
       RootLibs  := Cint Core  Net Tree Hist Matrix MathCore MathMore Reflex EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer
-      CoreLibs :=  $(BoostLibs) $(RootLibs) CLHEP-2.0.4.2 gsl gslcblas HepMC sigc-2.0.0 dl
+      ProjectLibs :=  $(BoostLibs) $(RootLibs) CLHEP-2.0.4.2 gsl gslcblas HepMC sigc-2.0.0 dl
 
-      CoreIncludes := external/inc/boost/1.40.0/include
-      CoreIncludes += external/inc/clhep/2.0.4.2/include 
-      CoreIncludes += external/inc/g sl/1.10/include
-      CoreIncludes += external/inc/hepmc/2.03.06/include
-      CoreIncludes += external/inc/sigcpp/2.2.3/include/sigc++-2.0 
+      ProjectIncludes := external/inc/boost/1.40.0/include
+      ProjectIncludes += external/inc/clhep/2.0.4.2/include 
+      ProjectIncludes += external/inc/g sl/1.10/include
+      ProjectIncludes += external/inc/hepmc/2.03.06/include
+      ProjectIncludes += external/inc/sigcpp/2.2.3/include/sigc++-2.0 
    endif     
 endif
