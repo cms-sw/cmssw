45bb792d67a937ef766e885dc4cd646fa60bd474
Annapaola De Cosa <sha1-4f7f601e1310d1f94a8ce176474325c5ff77ac0c@cern.ch>
2010-03-01 15:26:48 +0000

    added evenNumber,RunNumber, LumiBlock branches - added lazyParser boolean

--- CommonTools/UtilAlgos/interface/NtpProducer.h
+++ CommonTools/UtilAlgos/interface/NtpProducer.h
@@ -33,20 +33,32 @@ private:
   edm::InputTag src_;
   /// variable tags
   std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > > tags_;
+  bool lazyParser_;
+  std::string prefix_;
+  bool eventInfo_;
 };
 
 template<typename C>
 NtpProducer<C>::NtpProducer( const edm::ParameterSet& par ) : 
-  src_( par.template getParameter<edm::InputTag>( "src" ) ) {
-   std::vector<edm::ParameterSet> variables = 
+  src_( par.template getParameter<edm::InputTag>( "src" ) ),
+  lazyParser_( par.template getUntrackedParameter<bool>( "lazyParser", false ) ),
+  prefix_( par.template getUntrackedParameter<std::string>( "prefix","" ) ),
+  eventInfo_( par.template getUntrackedParameter<bool>( "eventInfo",true ) ){
+  std::vector<edm::ParameterSet> variables = 
                                    par.template getParameter<std::vector<edm::ParameterSet> >("variables");
    std::vector<edm::ParameterSet>::const_iterator 
      q = variables.begin(), end = variables.end();
+   if(eventInfo_){
+     produces<unsigned int>( prefix_+"EventNumber" ).setBranchAlias( prefix_ + "EventNumber" );
+     produces<unsigned int>( prefix_ + "RunNumber" ).setBranchAlias( prefix_ + "RunNumber" );
+     produces<unsigned int>( prefix_ + "LumiBlock" ).setBranchAlias( prefix_ + "Lumiblock" );
+   }
    for (; q!=end; ++q) {
-     std::string tag = q->getUntrackedParameter<std::string>("tag");
-     StringObjectFunction<typename C::value_type> quantity(q->getUntrackedParameter<std::string>("quantity"));
+     std::string tag = prefix_ + q->getUntrackedParameter<std::string>("tag");
+     StringObjectFunction<typename C::value_type> quantity(q->getUntrackedParameter<std::string>("quantity"), lazyParser_);
      tags_.push_back(std::make_pair(tag, quantity));
      produces<std::vector<float> >(tag).setBranchAlias(tag);
+     
    }   
 }
 
@@ -58,7 +70,17 @@ template<typename C>
 void NtpProducer<C>::produce( edm::Event& iEvent, const edm::EventSetup& ) {
    edm::Handle<C> coll;
    iEvent.getByLabel(src_, coll);
-
+   if(eventInfo_){   
+     std::auto_ptr<unsigned int> event( new unsigned int );
+     std::auto_ptr<unsigned int> run( new unsigned int );
+     std::auto_ptr<unsigned int> lumi( new unsigned int );
+     *event = iEvent.id().event();
+     *run = iEvent.id().run();
+     *lumi = iEvent.luminosityBlock();
+     iEvent.put( event, prefix_ + "EventNumber" );
+     iEvent.put( run, prefix_ + "RunNumber" );
+     iEvent.put( lumi, prefix_ + "LumiBlock" );
+   }
    typename std::vector<std::pair<std::string, StringObjectFunction<typename C::value_type> > >::const_iterator 
      q = tags_.begin(), end = tags_.end();
    for(;q!=end; ++q) {
