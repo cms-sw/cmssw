8580a648efc7e3178c532c57346265b07396a101
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-03-01 18:36:45 +0000

    Dropping a branch not sufficient reason to add process to process history

--- IOPool/Output/src/PoolOutputModule.cc
+++ IOPool/Output/src/PoolOutputModule.cc
@@ -203,19 +203,16 @@ namespace edm {
   }
 
   void PoolOutputModule::write(EventPrincipal const& e) {
-      if(hasNewlyDroppedBranch()[InEvent]) e.addToProcessHistory();
       rootOutputFile_->writeOne(e);
   }
 
   void PoolOutputModule::writeLuminosityBlock(LuminosityBlockPrincipal const& lb) {
-      if(hasNewlyDroppedBranch()[InLumi]) lb.addToProcessHistory();
       rootOutputFile_->writeLuminosityBlock(lb);
       Service<JobReport> reportSvc;
       reportSvc->reportLumiSection(lb.id().run(), lb.id().luminosityBlock());
   }
 
   void PoolOutputModule::writeRun(RunPrincipal const& r) {
-      if(hasNewlyDroppedBranch()[InRun]) r.addToProcessHistory();
       rootOutputFile_->writeRun(r);
       Service<JobReport> reportSvc;
       reportSvc->reportRunNumber(r.run());
