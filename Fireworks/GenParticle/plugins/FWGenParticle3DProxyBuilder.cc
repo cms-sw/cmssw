11d92f6127ab3298ccdbafcc09095e11745dd78a
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2010-03-02 20:23:20 +0000

    rewrote to be a simple proxy builder

--- Fireworks/GenParticle/plugins/FWGenParticle3DProxyBuilder.cc
+++ Fireworks/GenParticle/plugins/FWGenParticle3DProxyBuilder.cc
@@ -14,7 +14,7 @@
 //
 // Original Author:
 //         Created:  Thu Dec  6 18:01:21 PST 2007
-// $Id: FWGenParticle3DProxyBuilder.cc,v 1.2 2010/01/22 19:52:57 amraktad Exp $
+// $Id: FWGenParticle3DProxyBuilder.cc,v 1.3 2010/01/22 20:56:59 amraktad Exp $
 //
 
 // system include files
@@ -28,7 +28,7 @@
 #include "TEveVSDStructs.h"
 
 #include "Fireworks/Core/interface/FWEventItem.h"
-#include "Fireworks/Core/interface/FW3DDataProxyBuilder.h"
+#include "Fireworks/Core/interface/FW3DSimpleProxyBuilderTemplate.h"
 
 
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
@@ -38,7 +38,7 @@
 class TEveTrack;
 class TEveTrackPropagator;
 
-class FWGenParticle3DProxyBuilder : public  FW3DDataProxyBuilder {
+class FWGenParticle3DProxyBuilder : public  FW3DSimpleProxyBuilderTemplate<reco::GenParticle> {
 
 public:
    FWGenParticle3DProxyBuilder();
@@ -52,8 +52,9 @@ private:
 
    const FWGenParticle3DProxyBuilder& operator=(const FWGenParticle3DProxyBuilder&); // stop default
 
-   virtual void build(const FWEventItem* iItem,
-                      TEveElementList** product);
+   virtual void build(const reco::GenParticle& iData,
+                      unsigned int iIndex,
+                      TEveElement& oItheHolder) const;
 
    // ---------- member data --------------------------------
    TDatabasePDG* m_pdg;
@@ -67,49 +68,34 @@ FWGenParticle3DProxyBuilder::FWGenParticle3DProxyBuilder()
    m_pdg = new TDatabasePDG();
 }
 
-void FWGenParticle3DProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
+void FWGenParticle3DProxyBuilder::build(const reco::GenParticle& iData,
+                                        unsigned int iIndex,
+                                        TEveElement& oItemHolder) const
 { 
-   reco::GenParticleCollection const * genParticles=0;
-   iItem->get(genParticles);
-   if(0 == genParticles ) return;
-
-   if(0==*product) {
-      *product = new TEveElementList();
-   } else {
-      (*product)->DestroyElements();
-   }
-   TEveElementList* tlist = *product;
-
-   int index=0;
    TEveRecTrack t;
    t.fBeta = 1.;
-   reco::GenParticleCollection::const_iterator it = genParticles->begin(),
-      end = genParticles->end();
-   for( ; it != end; ++it,++index) {
-      t.fP = TEveVector(it->px(),
-                        it->py(),
-                        it->pz());
-      t.fV = TEveVector(it->vx(),
-                        it->vy(),
-                        it->vz());
-      t.fSign = it->charge();
+   t.fP = TEveVector(iData.px(),
+                     iData.py(),
+                     iData.pz());
+   t.fV = TEveVector(iData.vx(),
+                     iData.vy(),
+                     iData.vz());
+   t.fSign = iData.charge();
 
-      TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
-     
-      char s[1024];
-      TParticlePDG* pID = m_pdg->GetParticle(it->pdgId());
-      if ( pID )
-         sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), it->pt());
-      else
-         sprintf(s,"gen pdg %d, Pt: %0.1f GeV", it->pdgId(), it->pt());
-
-      track->SetMainColor(iItem->defaultDisplayProperties().color());
-      track->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
-      track->SetTitle(s);
-      track->MakeTrack();
-      tlist->AddElement(track);
-   }
+   TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
+   
+   char s[1024];
+   TParticlePDG* pID = m_pdg->GetParticle(iData.pdgId());
+   if ( pID )
+      sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), iData.pt());
+   else
+      sprintf(s,"gen pdg %d, Pt: %0.1f GeV", iData.pdgId(), iData.pt());
+   
+   track->SetTitle(s);
+   oItemHolder.SetElementTitle(s);
+   track->MakeTrack();
+   oItemHolder.AddElement(track);
 }
 
-REGISTER_FW3DDATAPROXYBUILDER(FWGenParticle3DProxyBuilder,reco::GenParticleCollection,"GenParticles");
+REGISTER_FW3DDATAPROXYBUILDER(FWGenParticle3DProxyBuilder,reco::GenParticle,"GenParticles");
 
--- Fireworks/GenParticle/plugins/FWGenParticleRPZProxyBuilder.cc
+++ Fireworks/GenParticle/plugins/FWGenParticleRPZProxyBuilder.cc
@@ -14,7 +14,7 @@
 //
 // Original Author:
 //         Created:  Thu Dec  6 18:01:21 PST 2007
-// $Id: FWGenParticleRPZProxyBuilder.cc,v 1.3 2010/01/22 19:52:57 amraktad Exp $
+// $Id: FWGenParticleRPZProxyBuilder.cc,v 1.4 2010/01/22 20:56:59 amraktad Exp $
 //
 
 // system include files
@@ -26,17 +26,16 @@
 #include "TEveVSDStructs.h"
 
 // user include files
-#include "Fireworks/Core/interface/FWRPZDataProxyBuilder.h"
+#include "Fireworks/Core/interface/FWRPZSimpleProxyBuilderTemplate.h"
 
-#include "Fireworks/Core/interface/FWEventItem.h"
-#include "Fireworks/Core/interface/FWRPZDataProxyBuilder.h"
+#include "Fireworks/Core/interface/Context.h"
 
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
 
 
-class FWGenParticleRPZProxyBuilder : public FWRPZDataProxyBuilder {
+class FWGenParticleRPZProxyBuilder : public FWRPZSimpleProxyBuilderTemplate<reco::GenParticle> {
 
 public:
    FWGenParticleRPZProxyBuilder();
@@ -51,7 +50,7 @@ public:
    REGISTER_PROXYBUILDER_METHODS();
 
 private:
-   virtual void build(const FWEventItem* iItem, TEveElementList** product);
+   virtual void build(const reco::GenParticle& iData, unsigned int iIndex,TEveElement& oItemHolder) const;
 
    FWGenParticleRPZProxyBuilder(const FWGenParticleRPZProxyBuilder&);    // stop default
 
@@ -66,49 +65,33 @@ FWGenParticleRPZProxyBuilder::FWGenParticleRPZProxyBuilder()
    m_pdg = new TDatabasePDG();
 }
 
-void FWGenParticleRPZProxyBuilder::build(const FWEventItem* iItem, TEveElementList** product)
+void FWGenParticleRPZProxyBuilder::build(const reco::GenParticle& iData, unsigned int iIndex,TEveElement& oItemHolder) const
+
 {
-   reco::GenParticleCollection const * genParticles=0;
-   iItem->get(genParticles);
-   if(0 == genParticles ) return;
-
-  if(0==*product) {
-      *product = new TEveElementList();
-   } else {
-      (*product)->DestroyElements();
-   }
-   TEveElementList* tlist = *product;
-
-   int index=0;
    TEveRecTrack t;
    t.fBeta = 1.;
-   reco::GenParticleCollection::const_iterator it = genParticles->begin(),
-      end = genParticles->end();
-   for( ; it != end; ++it,++index) {
-      t.fP = TEveVector(it->px(),
-                        it->py(),
-                        it->pz());
-      t.fV = TEveVector(it->vx(),
-                        it->vy(),
-                        it->vz());
-      t.fSign = it->charge();
+   t.fP = TEveVector(iData.px(),
+                     iData.py(),
+                     iData.pz());
+   t.fV = TEveVector(iData.vx(),
+                     iData.vy(),
+                     iData.vz());
+   t.fSign = iData.charge();
 
-      TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
-     
-      char s[1024];
-      TParticlePDG* pID = m_pdg->GetParticle(it->pdgId());
-      if ( pID )
-         sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), it->pt());
-      else
-         sprintf(s,"gen pdg %d, Pt: %0.1f GeV", it->pdgId(), it->pt());
-
-      track->SetMainColor(iItem->defaultDisplayProperties().color());
-      track->SetRnrSelf(iItem->defaultDisplayProperties().isVisible());
-      track->SetTitle(s);
-      track->MakeTrack();
-      tlist->AddElement(track);
-   }
+   TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
+   
+   char s[1024];
+   TParticlePDG* pID = m_pdg->GetParticle(iData.pdgId());
+   if ( pID )
+      sprintf(s,"gen %s, Pt: %0.1f GeV", pID->GetName(), iData.pt());
+   else
+      sprintf(s,"gen pdg %d, Pt: %0.1f GeV", iData.pdgId(), iData.pt());
+   
+   oItemHolder.SetElementTitle(s);
+   track->MakeTrack();
+   oItemHolder.AddElement(track);
 }
 
-REGISTER_FWRPZDATAPROXYBUILDERBASE(FWGenParticleRPZProxyBuilder,reco::GenParticleCollection,"GenParticles");
+
+REGISTER_FWRPZDATAPROXYBUILDERBASE(FWGenParticleRPZProxyBuilder,reco::GenParticle,"GenParticles");
 
