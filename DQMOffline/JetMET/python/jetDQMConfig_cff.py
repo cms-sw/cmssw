676c3a7809e9c4b15bccaaec3e5d21900b8168b7
Jared Sturdy <sha1-db53c096984bc383f729541321a2e7ec9133743e@cern.ch>
2010-03-02 00:58:54 +0000

    restructuring configuration files, breaking up into common parts

---
+++ DQMOffline/JetMET/python/jetDQMConfig_cff.py
@@ -0,0 +1,85 @@
+import FWCore.ParameterSet.Config as cms
+
+jetDQMParameters = cms.PSet(
+    eBin        = cms.int32(100),
+    eMax        = cms.double(500.0),
+    eMin        = cms.double(0.0),
+
+    etaBin      = cms.int32(100),
+    etaMax      = cms.double(5.0),
+    etaMin      = cms.double(-5.0),
+
+    pBin        = cms.int32(100),
+    pMax        = cms.double(500.0),
+    pMin        = cms.double(0.0),
+
+    phiBin      = cms.int32(70),
+    phiMax      = cms.double(3.2),
+    phiMin      = cms.double(-3.2),
+
+    ptBin       = cms.int32(100),
+    ptMax       = cms.double(50.0),
+    ptMin       = cms.double(0.0),
+
+    ptThreshold = cms.double(3.),
+    n90HitsMin  = cms.int32(-1),
+    fHPDMax     = cms.double(1.),
+    resEMFMin   = cms.double(0.),
+    iscleaned   = cms.int32(0),
+
+    makedijetselection  = cms.int32(0),
+
+    jIDeffptBins = cms.int32(5),        
+    JetIDParams  = cms.PSet(
+        useRecHits      = cms.bool(True),
+        hbheRecHitsColl = cms.InputTag("hbhereco"),
+        hoRecHitsColl   = cms.InputTag("horeco"),
+        hfRecHitsColl   = cms.InputTag("hfreco"),
+        ebRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEB"),
+        eeRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEE")
+    )
+)
+
+
+cleanedJetDQMParameters = jetDQMParameters.clone(
+    iscleaned   = cms.int32(1),
+    ptThreshold = cms.double(10.),
+    n90HitsMin  = cms.int32(2),
+    fHPDMax     = cms.double(0.98),
+    resEMFMin   = cms.double(0.01)
+),
+#
+#
+#cms.PSet(
+#    eBin        = cms.int32(100),
+#    eMax        = cms.double(500.0),
+#    eMin        = cms.double(0.0),
+#    etaBin      = cms.int32(100),
+#    etaMax      = cms.double(5.0),
+#    etaMin      = cms.double(-5.0),
+#    pBin        = cms.int32(100),
+#    pMax        = cms.double(500.0),
+#    pMin        = cms.double(0.0),
+#    phiBin      = cms.int32(70),
+#    phiMax      = cms.double(3.2),
+#    ptMax       = cms.double(50.0),
+#    phiMin      = cms.double(-3.2),
+#    ptBin       = cms.int32(100),
+#    ptMin       = cms.double(0.0),
+#    ptThreshold = cms.double(10.),
+#    n90HitsMin  = cms.int32(2),
+#    fHPDMax     = cms.double(0.98),
+#    resEMFMin   = cms.double(0.01),
+#    iscleaned   = cms.int32(1),
+#    makedijetselection  = cms.int32(0),
+#    jIDeffptBins = cms.int32(5),        
+#    JetIDParams  = cms.PSet(
+#        useRecHits      = cms.bool(True),
+#        hbheRecHitsColl = cms.InputTag("hbhereco"),
+#        hoRecHitsColl   = cms.InputTag("horeco"),
+#        hfRecHitsColl   = cms.InputTag("hfreco"),
+#        ebRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEB"),
+#        eeRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEE")
+#    )
+#    
+#)
