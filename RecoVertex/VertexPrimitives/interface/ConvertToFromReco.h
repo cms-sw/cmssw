9a76315adbf1f3d23d6c5f42fb3a2660ec7329be
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2010-03-03 19:37:00 +0000

    Replace static by inline to silence compiler warnings.

--- RecoVertex/VertexPrimitives/interface/ConvertToFromReco.h
+++ RecoVertex/VertexPrimitives/interface/ConvertToFromReco.h
@@ -4,7 +4,7 @@
 #include "RecoVertex/VertexPrimitives/interface/ConvertError.h"
 
 namespace RecoVertex{
-  static reco::Vertex::Point convertPos(const GlobalPoint& p) 
+  inline reco::Vertex::Point convertPos(const GlobalPoint& p) 
     {
       reco::Vertex::Point pos;
       pos.SetX(p.x());
@@ -12,7 +12,8 @@ namespace RecoVertex{
       pos.SetZ(p.z());
       return pos;
     }
-  static GlobalPoint convertPos(const reco::Vertex::Point& p)
+
+  inline GlobalPoint convertPos(const reco::Vertex::Point& p)
     { return GlobalPoint(p.x(), p.y(), p.z()); }
 }
 
