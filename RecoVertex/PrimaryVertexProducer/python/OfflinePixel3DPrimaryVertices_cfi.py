f20ec8f7e63392a44da1ac6b7049559cc3f7ff4f
Wolfram Erdmann <sha1-3bf1149ebfd3d4f6b688e49eec204911cf01dc05@cern.ch>
2010-03-03 07:53:52 +0000

    imported from V01-01-06 and adapted to new steering

--- RecoVertex/PrimaryVertexProducer/python/OfflinePixel3DPrimaryVertices_cfi.py
+++ RecoVertex/PrimaryVertexProducer/python/OfflinePixel3DPrimaryVertices_cfi.py
@@ -4,26 +4,39 @@ pixelVertices = cms.EDProducer("PrimaryVertexProducer",
     PVSelParameters = cms.PSet(
         maxDistanceToBeam = cms.double(2), ## 2 cms in case there is no beamspot (ONLY FOR STARTUP!)
 
-        minVertexFitProb = cms.double(0.01) ## 1% vertex fit probability
+        minVertexFitProb = cms.double(0.01) ## 1% vertex fit probability, not used (and meaningless with the AdaptiveFitter)
 
     ),
     verbose = cms.untracked.bool(False),
     algorithm = cms.string('AdaptiveVertexFitter'), ## 100 is for when the beamspot is not well known (ONLY FOR STARTUP)
     TkFilterParameters = cms.PSet(
         maxNormalizedChi2 = cms.double(100.0),
-        minSiliconHits = cms.int32(2), ## hits > 2 - for when the beamspot is not well known (ONLY FOR STARTUP)
 
         maxD0Significance = cms.double(100.0), ## 100 is for when the beamspot is not well known (ONLY FOR STARTUP)
 
         minPt = cms.double(0.0), ## better for softish events
 
-        minPixelHits = cms.int32(2) ## hits > 2
+        minPixelLayersWithHits = cms.int32(3),   # >=3  three or more pixel layers
+        minSiliconLayersWithHits = cms.int32(3), # >=3  (includes pixel layers)
+
+        # no longer used
+        #minSiliconHits = cms.int32(2), ## hits > 2 - for when the beamspot is not well known (ONLY FOR STARTUP)
+        # minPixelHits = cms.int32(2) ## hits > 2
 
     ),
     beamSpotLabel = cms.InputTag("offlineBeamSpot"),
     # label of tracks to be used
     TrackLabel = cms.InputTag("pixelTracks"),
-    useBeamConstraint = cms.bool(True),  # default for long term data and MC prod
+
+    useBeamConstraint = cms.bool(False),
+    TkClusParameters = cms.PSet(
+        algorithm   = cms.string("gap"),
+        TkGapClusParameters = cms.PSet(
+            zSeparation = cms.double(0.1)        # 1 mm max separation betw.tracks inside clusters
+        )
+    ),
+                               
+    # probably not needed here ...
     VtxFinderParameters = cms.PSet(
         ptCut = cms.double(0.0),
         vtxFitProbCut = cms.double(0.01), ## 1% vertex fit probability
@@ -31,11 +44,8 @@ pixelVertices = cms.EDProducer("PrimaryVertexProducer",
         trackCompatibilityToPVcut = cms.double(0.05), ## 5%
         maxNbOfVertices = cms.int32(0) ## search all vertices in each cluster
 
-    ),
-    TkClusParameters = cms.PSet(
-        zSeparation = cms.double(1) ## 1 cm max separation betw. clusters
-
     )
+
 )
 
 
