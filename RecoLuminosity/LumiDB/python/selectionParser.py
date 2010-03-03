af05cdd270d2db39a5e2c7273064ae8cc880bf8f
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-03-03 18:09:14 +0000

    add str result

--- RecoLuminosity/LumiDB/python/selectionParser.py
+++ RecoLuminosity/LumiDB/python/selectionParser.py
@@ -2,6 +2,7 @@ import json
 class selectionParser(object):
     def __init__(self,selectStr):
         self.__result={}
+        self.__strresult={}
         strresult=json.loads(selectStr)
         for k,v in strresult.items():
             expandedvalues=[]
@@ -16,12 +17,17 @@ class selectionParser(object):
                     for i in range(w[0],w[1]+1):
                         expandedvalues.append(i)
             self.__result[int(k)]=expandedvalues
+            self.__strresult[k]=[str(x) for x in expandedvalues]
     def runs(self):
         return self.__result.keys()
     def runsandls(self):
         '''return expanded {run:lslist}
         '''
         return self.__result
+    def runsandlsStr(self):
+        '''return expanded {'run':lslist}
+        '''
+        return self.__strresult
     def numruns(self):
         return len(self.__result.keys())
     def numls(self,run):
@@ -30,5 +36,6 @@ if __name__ == "__main__":
     s=selectionParser('{"1":[[3,45]],"2":[[4,8],[10,10]]}')
     print 'runs : ',s.runs()
     print 'full result : ',s.runsandls()
+    print 'str result : ',s.runsandlsStr()
     print 'num runs : ',s.numruns()
     print 'numls in run : ',s.numls(1)
