ff5ff9f6bc7816ac134ff397da3c45ad3f7986fe
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-03-03 14:11:48 +0000

    add expand ls list

--- RecoLuminosity/LumiDB/python/selectionParser.py
+++ RecoLuminosity/LumiDB/python/selectionParser.py
@@ -4,17 +4,31 @@ class selectionParser(object):
         self.__result={}
         strresult=json.loads(selectStr)
         for k,v in strresult.items():
-            self.__result[int(k)]=v
+            expandedvalues=[]
+            for w in v:
+            ###weed out [10]-like stuff just in case they exist
+                if len(w)==1:
+                    expandedvalues.append(w[0])
+            ##weed out [10,10]-like stuff
+                elif len(w)==2 and w[0]==w[1]:
+                    expandedvalues.append(w[0])
+                else:
+                    for i in range(w[0],w[1]+1):
+                        expandedvalues.append(i)
+            self.__result[int(k)]=expandedvalues
     def runs(self):
         return self.__result.keys()
     def runsandls(self):
+        '''return expanded {run:lslist}
+        '''
         return self.__result
     def numruns(self):
         return len(self.__result.keys())
-    def numls(self):
-        pass
+    def numls(self,run):
+        return len(self.__result[run])
 if __name__ == "__main__":
     s=selectionParser('{"1":[[3,45]],"2":[[4,8],[10,10]]}')
     print 'runs : ',s.runs()
     print 'full result : ',s.runsandls()
     print 'num runs : ',s.numruns()
+    print 'numls in run : ',s.numls(1)
