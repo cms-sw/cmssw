ef04cb774cfd1bf9bb943ee480bc3839b6e93f4e
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2010-03-03 10:45:41 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
+++ ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
@@ -17,14 +17,14 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START3X_V18::All')
+process.GlobalTag.globaltag = cms.string('START3X_V21::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPaths_cff")
 
 # Output module configuration
 process.load("ElectroWeakAnalysis.Skimming.zMuMuSubskimOutputModule_cfi")
-process.zMuMuSubskimOutputModule.fileName = 'file:/tmp/fabozzi/testZMuMuSubskim.root'
+process.zMuMuSubskimOutputModule.fileName = 'testZMuMuSubskim.root'
 
 process.outpath = cms.EndPath(process.zMuMuSubskimOutputModule)
 
