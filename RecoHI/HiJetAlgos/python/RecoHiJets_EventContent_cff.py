3b8ed5e9e1a286598677c40456793b88629caa74
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-03-04 04:27:06 +0000

    Temporary event content

--- RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
+++ RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-#Full Event content
+#Full Event content ---- temporary
 RecoHiJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*',
-                                           'keep recoGenJets_iterativeCone5HiGenJets_*_*'
+    outputCommands = cms.untracked.vstring('keep recoCaloJets_*Pu*CaloJets_*_*',
+                                           'keep recoGenJets_*HiGenJets_*_*'
                                            )
     )
 
 RecoHiJetsFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_iterativeConePu5CaloJets_*_*',
-                                           'keep recoGenJets_iterativeCone5HiGenJets_*_*'
+    outputCommands = cms.untracked.vstring('keep recoCaloJets_*Pu*CaloJets_*_*',
+                                           'keep recoGenJets_*HiGenJets_*_*'
                                            )
     )
 
