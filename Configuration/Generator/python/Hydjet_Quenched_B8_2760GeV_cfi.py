8c351c94cd2c6c61f3fa1e720f08c7c96aa871e5
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2010-03-04 16:37:19 +0000

    Add version with high impact parameter

---
+++ Configuration/Generator/python/Hydjet_Quenched_B8_2760GeV_cfi.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.PyquenDefaultSettings_cff import *
+
+generator = cms.EDFilter("HydjetGeneratorFilter",
+                         collisionParameters,
+                         qgpParameters,
+                         hydjetParameters,
+                         hydjetMode = cms.string('kHydroQJets'),
+                         PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
+                                                     # Quarkonia and Weak Bosons removed upon dilepton group's request.
+                                                     parameterSets = cms.vstring('pythiaUESettings','ppDefault','pythiaPromptPhotons'),
+                                                     ),
+                         cFlag = cms.int32(0),
+                         bMin = cms.double(0),
+                         bMax = cms.double(0),
+                         bFixed = cms.double(8)
+                         )
+
