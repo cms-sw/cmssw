636fcdd63a7c0397523ee0c3f786e9acaae26ced
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-03-04 17:51:35 +0000

    remove dead wood

--- Configuration/Skimming/python/SkimPaths_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HiggsAnalysis.Configuration.HiggsAnalysis_SkimPaths_cff import *
-from TopQuarkAnalysis.Configuration.TopQuarkAnalysis_SkimPaths_cff import *
-
