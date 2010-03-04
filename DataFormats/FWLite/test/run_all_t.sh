e359d73442e42340b2cf8f9eccae987fea708bdb
Daniel Sherman Riley <sha1-159aefc003aa44e4c9724c4c97b8e9a2524e86c8@cern.ch>
2010-03-04 17:08:53 +0000

    use prodmerge.root instead of good.root in runlumi test, as it has two runs and two lumi sections, not one of each (and the lumis aren't both 1); test retrieval of begin and end run and lumi Thing products, since ThingProducer does create run and lumi products

--- DataFormats/FWLite/test/run_all_t.sh
+++ DataFormats/FWLite/test/run_all_t.sh
@@ -6,13 +6,13 @@ function die { echo $1: status $2 ;  exit $2; }
 
 ${LOCAL_TEST_DIR}/RefTest.sh || die 'Failed to create file' $?
 root -b -n -q ${LOCAL_TEST_DIR}/event_looping_cint.C || die 'Failed in event_looping_cint.C' $?
-root -b -n -q ${LOCAL_TEST_DIR}/runlumi_looping_cint.C || die 'Failed in runlumi_looping_cint.C' $?
 root -b -n -q ${LOCAL_TEST_DIR}/chainevent_looping_cint.C || die 'Failed in chainevent_looping_cint.C' $?
 python ${LOCAL_TEST_DIR}/chainEvent_python.py || die 'Failed in chainEvent_python.py' $?
 #root -b -n -q ${LOCAL_TEST_DIR}/autoload_with_std.C || die 'Failed in autoload_with_std.C' $?
 #root -b -n -q ${LOCAL_TEST_DIR}/autoload_with_missing_std.C || die 'Failed in autoload_with_missing_std.C' $?
 
 ${LOCAL_TEST_DIR}/MergeTest.sh || die 'Failed to create file' $?
+root -b -n -q ${LOCAL_TEST_DIR}/runlumi_looping_cint.C || die 'Failed in runlumi_looping_cint.C' $?
 root -b -n -q ${LOCAL_TEST_DIR}/productid_cint.C || die 'Failed in productid_cint.C' $?
 root -b -n -q ${LOCAL_TEST_DIR}/triggernames_cint.C || die 'Failed in triggernames_cint.C' $?
 root -b -n -q ${LOCAL_TEST_DIR}/triggernames_multi_cint.C || die 'Failed in triggernames_multi_cint.C' $?
--- DataFormats/FWLite/test/runlumi_looping_cint.C
+++ DataFormats/FWLite/test/runlumi_looping_cint.C
@@ -22,66 +22,94 @@ static loadFWLite lfw;
 
 void runlumi_looping_cint()
 {
-TFile f("good.root");
-fwlite::Run r(&f);
-fwlite::LuminosityBlock l(&f);
+    TFile f("prodmerge.root");
+    fwlite::Run r(&f);
+    fwlite::LuminosityBlock l(&f);
 
-int i =0;
-int returnValue = 0;
-for( ;r.isValid();++r,++i) {
-  cout << r.run() << endl;
-}  
-if (i==0) {
-  cout <<"First run loop failed!"<<endl;
-  returnValue = 1;
-}
+    int i =0;
+    int returnValue = 0;
+    for( ;r.isValid();++r,++i) {
+        cout << r.run() << endl;
+        fwlite::Handle<vector<edmtest::Thing> > pThing;
+        pThing.getByLabel(r,"Thing","beginRun");
+   
+        for(int i=0; i!=pThing.ref().size();++i) {
+            cout <<pThing.ref().at(i).a<<" ";
+        }
+        cout << endl;
+    }  
+    if (i==0) {
+        cout <<"First run loop failed!"<<endl;
+        returnValue = 1;
+    }
 
-int i =0;
-int returnValue = 0;
-for( ;l.isValid();++l,++i) {
-  cout << l.id().run() << " " << l.id().luminosityBlock() << endl;
-}  
-if (i==0) {
-  cout <<"First lumi loop failed!"<<endl;
-  returnValue = 1;
-}
+    int i =0;
+    int returnValue = 0;
+    for( ;l.isValid();++l,++i) {
+        cout << l.id().run() << " " << l.id().luminosityBlock() << endl;
+        fwlite::Handle<vector<edmtest::Thing> > pThing;
+        pThing.getByLabel(l,"Thing","beginLumi");
+   
+        for(int i=0; i!=pThing.ref().size();++i) {
+            cout <<pThing.ref().at(i).a<<" ";
+        }
+        cout << endl;
+    }  
+    if (i==0) {
+        cout <<"First lumi loop failed!"<<endl;
+        returnValue = 1;
+    }
 
 
-r.toBegin();
-i=0;
-for( ;r;++r,++i) { 
-}
-if (i==0) {
-  cout <<"Second run loop failed!"<<endl;
-  returnValue = 1;
-}
+    r.toBegin();
+    i=0;
+    for( ;r;++r,++i) { 
+    }
+    if (i==0) {
+        cout <<"Second run loop failed!"<<endl;
+        returnValue = 1;
+    }
 
-l.toBegin();
-i=0;
-for( ;l;++l,++i) { 
-}
-if (i==0) {
-  cout <<"Second lumi loop failed!"<<endl;
-  returnValue = 1;
-}
+    l.toBegin();
+    i=0;
+    for( ;l;++l,++i) { 
+    }
+    if (i==0) {
+        cout <<"Second lumi loop failed!"<<endl;
+        returnValue = 1;
+    }
 
-i=0;
-for(r.toBegin(); !r.atEnd();++r,++i) {
-  cout << r.run() << endl;
-}
-if (i==0) {
-  cout <<"Third run loop failed!"<<endl;
-  returnValue = 1;
-}
+    i=0;
+    for(r.toBegin(); !r.atEnd();++r,++i) {
+        cout << r.run() << endl;
+        fwlite::Handle<vector<edmtest::Thing> > pThing;
+        pThing.getByLabel(r,"Thing","endRun");
+   
+        for(int i=0; i!=pThing.ref().size();++i) {
+            cout <<pThing.ref().at(i).a<<" ";
+        }
+        cout << endl;
+    }
+    if (i==0) {
+        cout <<"Third run loop failed!"<<endl;
+        returnValue = 1;
+    }
 
-i=0;
-for(l.toBegin(); !l.atEnd();++l,++i) {
-  cout << l.id().run() << " " << l.id().luminosityBlock() << endl;
-}
-if (i==0) {
-  cout <<"Third lumi loop failed!"<<endl;
-  returnValue = 1;
-}
+    i=0;
+    for(l.toBegin(); !l.atEnd();++l,++i) {
+        cout << l.id().run() << " " << l.id().luminosityBlock() << endl;
+        fwlite::Handle<vector<edmtest::Thing> > pThing;
+        pThing.getByLabel(l,"Thing","endLumi");
+   
+        for(int i=0; i!=pThing.ref().size();++i) {
+            cout <<pThing.ref().at(i).a<<" ";
+        }
+        cout << endl;
+    }
+    if (i==0) {
+        cout <<"Third lumi loop failed!"<<endl;
+        returnValue = 1;
+    }
 
-exit(returnValue);
+    exit(returnValue);
 }
