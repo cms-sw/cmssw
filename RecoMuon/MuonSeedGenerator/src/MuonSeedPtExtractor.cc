0018c3a8bfe1655796434831053b31d5937d3452
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2010-03-04 07:07:18 +0000

    more descriptive warning category (note that now with these descriptive categories, it is now not as trivial to silence warning messages based on the general area of RECO

--- RecoMuon/MuonSeedGenerator/src/MuonSeedPtExtractor.cc
+++ RecoMuon/MuonSeedGenerator/src/MuonSeedPtExtractor.cc
@@ -308,7 +308,8 @@ std::vector<double> MuonSeedPtExtractor::pT_extract(MuonTransientTrackingRecHit:
         //std::cout<<" combination = "<<combination<<" eta = "<<eta<<" dPhi = "<<dPhi<<std::endl;
     ParametersMap::const_iterator parametersItr = theParametersForCombo.find(combination);
     if(parametersItr == theParametersForCombo.end()) {
-      edm::LogWarning("RecoMuon|MuonSeedGenerator|MuonSeedPtExtractor") << "Cannot find parameters for combo " << combination;
+      //      edm::LogWarning("RecoMuon|MuonSeedGenerator|MuonSeedPtExtractor") << "Cannot find parameters for combo " << combination;
+      edm::LogWarning("BadSegmentCombination") << "Cannot find parameters for combo " << combination;
       pTestimate[0] = pTestimate[1] = 100;
       //       throw cms::Exception("MuonSeedPtEstimator") << "Cannot find parameters for combo " << combination;
     } else {
