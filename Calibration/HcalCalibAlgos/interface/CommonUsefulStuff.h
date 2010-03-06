3ca7d844114933afed42f5b7a0168868ad23f60c
Andrey Pozdnyakov <sha1-f3abd5a42b16440417a1672b658ebc7010576be4@cern.ch>
2010-03-06 18:40:33 +0000

    TFileService moved for CMSSW_3_5_3

--- Calibration/HcalCalibAlgos/interface/CommonUsefulStuff.h
+++ Calibration/HcalCalibAlgos/interface/CommonUsefulStuff.h
@@ -25,8 +25,9 @@
 #include "DataFormats/GeometryVector/interface/GlobalVector.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
 #include "CommonTools/UtilAlgos/interface/DeltaR.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+//#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
 
 #include "TROOT.h"
 #include "TFile.h"
