2fef75c4f308b550c3bd43ecfc90aa3f4833eb17
Mauro Dinardo <sha1-23954716fb14557ae777b97baf045487503aec21@cern.ch>
2010-03-06 19:20:24 +0000

    3D gauss fit

--- DQM/BeamMonitor/plugins/Vx3DHLTAnalyzer.cc
+++ DQM/BeamMonitor/plugins/Vx3DHLTAnalyzer.cc
@@ -41,6 +41,7 @@ Vx3DHLTAnalyzer::Vx3DHLTAnalyzer(const ParameterSet& iConfig)
 {
   vertexCollection = edm::InputTag("pixelVertices");
   nLumiReset       = 1;
+  dataFromFit      = false;
   xRange           = 0.;
   xStep            = 1.;
   yRange           = 0.;
@@ -48,9 +49,10 @@ Vx3DHLTAnalyzer::Vx3DHLTAnalyzer(const ParameterSet& iConfig)
   zRange           = 0.;
   zStep            = 1.;
   fileName         = "BeamSpot_3DVxPixels.txt";
- 
+
   vertexCollection = iConfig.getParameter<InputTag>("vertexCollection");
   nLumiReset       = iConfig.getParameter<unsigned int>("nLumiReset");
+  dataFromFit      = iConfig.getParameter<bool>("dataFromFit");
   xRange           = iConfig.getParameter<double>("xRange");
   xStep            = iConfig.getParameter<double>("xStep");
   yRange           = iConfig.getParameter<double>("yRange");
@@ -71,80 +73,134 @@ void Vx3DHLTAnalyzer::analyze(const Event& iEvent, const EventSetup& iSetup)
   Handle<VertexCollection> Vx3DCollection;
   iEvent.getByLabel(vertexCollection,Vx3DCollection);
 
-  for (vector<Vertex>::const_iterator it3DVx = Vx3DCollection->begin(); it3DVx != Vx3DCollection->end(); it3DVx++) {
+  if (beginTimeOfFit != 0)
+    {
+      for (vector<Vertex>::const_iterator it3DVx = Vx3DCollection->begin(); it3DVx != Vx3DCollection->end(); it3DVx++) {
+	
+	if ((it3DVx->isValid() == true) && (it3DVx->isFake() == false))
+	  {
+	    Distribution3D->Fill(it3DVx->x(), it3DVx->y(), it3DVx->z());
 
-    if ((it3DVx->isValid() == true) && (it3DVx->isFake() == false))
-      {
-	Vx_X->Fill(it3DVx->x());
-	Vx_Y->Fill(it3DVx->y());
-	Vx_Z->Fill(it3DVx->z());
-
-	Vx_ZX->Fill(it3DVx->z(), it3DVx->x());
-	Vx_ZY->Fill(it3DVx->z(), it3DVx->y());
-	Vx_XY->Fill(it3DVx->x(), it3DVx->y());
-
-	Vx_ZX_profile->Fill(it3DVx->z(), it3DVx->x());
-	Vx_ZY_profile->Fill(it3DVx->z(), it3DVx->y());
-
-	reportSummary->Fill(1.0);
-	reportSummaryMap->Fill(0.5, 0.5, 1.0);
+	    Vx_X->Fill(it3DVx->x());
+	    Vx_Y->Fill(it3DVx->y());
+	    Vx_Z->Fill(it3DVx->z());
+	    
+	    Vx_ZX->Fill(it3DVx->z(), it3DVx->x());
+	    Vx_ZY->Fill(it3DVx->z(), it3DVx->y());
+	    Vx_XY->Fill(it3DVx->x(), it3DVx->y());
+	   
+	    Vx_ZX_profile->Fill(it3DVx->z(), it3DVx->x());
+	    Vx_ZY_profile->Fill(it3DVx->z(), it3DVx->y());
+	   
+	    reportSummary->Fill(1.0);
+	    reportSummaryMap->Fill(0.5, 0.5, 1.0);
+	  }
       }
-  }
+
+      runNumber = iEvent.id().run();
+    }
+}
+
+
+double Vx3DHLTAnalyzer::Gauss3DFunc(double* x,
+				    double* par)
+{ 
+  double a,b,c,d,e,f;
+
+  a = cos(par[1])*cos(par[1]) / (2.*par[2]*par[2]) + sin(par[1])*sin(par[1]) / (2.*par[4]*par[4]) +
+      cos(par[3])*cos(par[3]) / (2.*par[2]*par[2]) + sin(par[3])*sin(par[3]) / (2.*par[6]*par[6]);
+  b = cos(par[1])*cos(par[1]) / (2.*par[4]*par[4]) + sin(par[1])*sin(par[1]) / (2.*par[2]*par[2]) +
+      cos(par[5])*cos(par[5]) / (2.*par[4]*par[4]) + sin(par[5])*sin(par[5]) / (2.*par[6]*par[6]);
+  c = cos(par[3])*cos(par[3]) / (2.*par[6]*par[6]) + sin(par[3])*sin(par[3]) / (2.*par[2]*par[2]) +
+      cos(par[5])*cos(par[5]) / (2.*par[6]*par[6]) + sin(par[5])*sin(par[5]) / (2.*par[4]*par[4]);
+  d = -sin(2*par[1]) / (4*par[2]*par[2]) + sin(2*par[1]) / (4*par[4]*par[4]);
+  e = -sin(2*par[5]) / (4*par[4]*par[4]) + sin(2*par[5]) / (4*par[6]*par[6]);
+  f = -sin(2*par[3]) / (4*par[6]*par[6]) + sin(2*par[3]) / (4*par[2]*par[2]);
+
+  return par[0] + exp(-(a*(x[0]-par[7])*(x[0]-par[7]) + b*(x[1]-par[8])*(x[1]-par[8]) + c*(x[2]-par[9])*(x[2]-par[9]) +
+			2*d*(x[0]-par[7])*(x[1]-par[8]) + 2*e*(x[1]-par[8])*(x[2]-par[9]) + 2*f*(x[0]-par[7])*(x[2]-par[9])));
 }
 
 
 void Vx3DHLTAnalyzer::writeToFile(vector<double>* vals,
-				  string fileName)
+				  string fileName,
+				  edm::TimeValue_t BeginTimeOfFit,
+				  edm::TimeValue_t EndTimeOfFit,
+				  unsigned int BeginLumiOfFit,
+				  unsigned int EndLumiOfFit)
 {
   ofstream outputFile;
-  outputFile.open(fileName.c_str(), ios::out); // To append: ios::app
+  outputFile.open(fileName.c_str(), ios::app); // To append: ios::app, to overwrite ios::out
 
   stringstream BufferString;
   BufferString.precision(5);
   
-  if (outputFile.is_open() == true)
+  if ((outputFile.is_open() == true) && (vals != NULL))
     {
       vector<double>::const_iterator it = vals->begin();
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
-      BufferString.str("");
-      it++;
+      outputFile << "Runnumber " << runNumber << endl;
+      outputFile << "BeginTimeOfFit " << beginTimeOfFit << endl;
+      outputFile << "EndTimeOfFit " << endTimeOfFit << endl;
+      outputFile << "LumiRange " << beginLumiOfFit << " - " << endLumiOfFit << endl;
+      outputFile << "Type 3" << endl; // 3D Vertexing with Pixel Tracks = type 3
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+0);
+      outputFile << "X0 " << BufferString.str().c_str() << endl;
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+1);
+      outputFile << "Y0 " << BufferString.str().c_str() << endl;
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+2);
+      outputFile << "Z0 " << BufferString.str().c_str() << endl;
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+5);
+      outputFile << "sigmaZ0 " << BufferString.str().c_str() << endl;
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+6);
+      outputFile << "dxdz " << BufferString.str().c_str() << endl; // @@@@@@ To be completed @@@@@@
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+7);
+      outputFile << "dydz " << BufferString.str().c_str() << endl; // @@@@@@ To be completed @@@@@@
       BufferString.str("");
-      it++;
 
-      BufferString << *it;
-      outputFile << BufferString.str().c_str() << endl;
+      BufferString << *(it+3);
+      outputFile << "BeamWidthX " << BufferString.str().c_str() << endl;
+      BufferString.str("");
+
+      BufferString << *(it+4);
+      outputFile << "BeamWidthY " << BufferString.str().c_str() << endl;
+      BufferString.str("");
+
+      outputFile << "Cov(0,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(1,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(2,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(3,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(4,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(5,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+      outputFile << "Cov(6,j) 0.0 0.0 0.0 0.0 0.0 0.0 0.0" << endl;
+
+      outputFile << "EmittanceX 0.0" << endl;
+      outputFile << "EmittanceY 0.0" << endl;
+      outputFile << "BetaStar 0.0" << endl;
 
       outputFile.close();
+    }
+}
+
+
+void Vx3DHLTAnalyzer::beginLuminosityBlock(const LuminosityBlock& lumiBlock, 
+					   const EventSetup& iSetup)
+{
+  if (lumiCounter == 0)
+    {
+      beginTimeOfFit = lumiBlock.beginTime().value();
+      beginLumiOfFit = lumiBlock.luminosityBlock();
     }
 }
 
@@ -154,23 +210,11 @@ void Vx3DHLTAnalyzer::endLuminosityBlock(const LuminosityBlock& lumiBlock,
 {
   // To know the lumisection: lumiBlock.luminosityBlock()
   lumiCounter++;
-
-  if ((lumiCounter == nLumiReset) && (nLumiReset != 0))
+  
+  if ((lumiCounter == nLumiReset) && (nLumiReset != 0) && (beginTimeOfFit != 0))
     {
-      TF1* Gauss = new TF1("Gauss","gaus");
-
-      // Write the 8 beam spot parameters into a file
-      vector<double> vals;
-      vals.push_back(Vx_X->getTH1F()->GetMean());
-      vals.push_back(Vx_Y->getTH1F()->GetMean());
-      vals.push_back(Vx_Z->getTH1F()->GetMean());
-      vals.push_back(Vx_X->getTH1F()->GetRMS());
-      vals.push_back(Vx_Y->getTH1F()->GetRMS());
-      vals.push_back(Vx_Z->getTH1F()->GetRMS());
-      vals.push_back(0.);
-      vals.push_back(0.);
-      writeToFile(&vals, fileName);
-      vals.clear();
+      TF1* Gauss   = new TF1("Gauss","gaus");
+      TF1* Gauss3D = new TF1("Gauss3D","Gauss3DFunc");
 
       mXlumi->ShiftFillLast(Vx_X->getTH1F()->GetMean(), Vx_X->getTH1F()->GetMeanError(), nLumiReset);
       mYlumi->ShiftFillLast(Vx_Y->getTH1F()->GetMean(), Vx_Y->getTH1F()->GetMeanError(), nLumiReset);
@@ -196,16 +240,56 @@ void Vx3DHLTAnalyzer::endLuminosityBlock(const LuminosityBlock& lumiBlock,
 				 (Vx_Z->getTH1F()->GetRMS() / Gauss->GetParameter(2)) * sqrt(powf(Vx_Z->getTH1F()->GetRMSError() / Vx_Z->getTH1F()->GetRMS(),2.) +
 											     powf(Gauss->GetParError(2) / Gauss->GetParError(2),2.)), nLumiReset);
 
-      Vx_X->Reset();
-      Vx_Y->Reset();
-      Vx_Z->Reset();
+      // Write the 8 beam spot parameters into a file
+      endTimeOfFit = lumiBlock.endTime().value();
+      endLumiOfFit = lumiBlock.luminosityBlock();
+      vector<double> vals;
+      Gauss->SetParameters(Distribution3D->getTH1()->GetMaximum(),
+			   0.0,
+			   Distribution3D->getTH1()->GetRMS(1),
+			   0.0,
+			   Distribution3D->getTH1()->GetRMS(2),
+			   0.0,
+			   Distribution3D->getTH1()->GetRMS(3),
+			   Distribution3D->getTH1()->GetMean(1),
+			   Distribution3D->getTH1()->GetMean(2),
+			   Distribution3D->getTH1()->GetMean(3));
+      Distribution3D->getTH1()->Fit("Gauss3D","QN0");
+      if (dataFromFit == true)
+	{
+	  vals.push_back(Gauss3D->GetParameter(8));
+	  vals.push_back(Gauss3D->GetParameter(9));
+	  vals.push_back(Gauss3D->GetParameter(10));
+	  vals.push_back(Gauss3D->GetParameter(3));
+	  vals.push_back(Gauss3D->GetParameter(5));
+	  vals.push_back(Gauss3D->GetParameter(7));
+	  vals.push_back(Gauss3D->GetParameter(4));
+	  vals.push_back(Gauss3D->GetParameter(6));
+	}
+      else
+	{
+	  vals.push_back(Vx_X->getTH1F()->GetMean());
+	  vals.push_back(Vx_Y->getTH1F()->GetMean());
+	  vals.push_back(Vx_Z->getTH1F()->GetMean());
+	  vals.push_back(Vx_X->getTH1F()->GetRMS());
+	  vals.push_back(Vx_Y->getTH1F()->GetRMS());
+	  vals.push_back(Vx_Z->getTH1F()->GetRMS());
+	  vals.push_back(0.);
+	  vals.push_back(0.);
+	}
+      writeToFile(&vals, fileName, beginTimeOfFit, endTimeOfFit, beginLumiOfFit, endLumiOfFit);
+      vals.clear();
 
-      Vx_ZX->Reset();
-      Vx_ZY->Reset();
-      Vx_XY->Reset();
+//       Vx_X->Reset();
+//       Vx_Y->Reset();
+//       Vx_Z->Reset();
 
-      Vx_ZX_profile->Reset();
-      Vx_ZY_profile->Reset();
+//       Vx_ZX->Reset();
+//       Vx_ZY->Reset();
+//       Vx_XY->Reset();
+
+//       Vx_ZX_profile->Reset();
+//       Vx_ZY_profile->Reset();
 
       reportSummary->Fill(0.5);
       reportSummaryMap->Fill(0.5, 0.5, 0.5);
@@ -232,6 +316,11 @@ void Vx3DHLTAnalyzer::beginJob()
   if ( dbe ) 
     {
       dbe->setCurrentFolder("BeamPixel");
+
+      Distribution3D = dbe->book3D("Distribution3D", "Distribution3D of the Vertex Coordinates",
+				   (int)(xRange/xStep), -xRange/2., xRange/2.,
+				   (int)(yRange/yStep), -yRange/2., yRange/2.,
+				   (int)(zRange/zStep), -zRange/2., zRange/2.);
 
       Vx_X = dbe->book1D("Vertex_X", "Primary Vertex X Coordinate Distribution", (int)(xRange/xStep), -xRange/2., xRange/2.);
       Vx_Y = dbe->book1D("Vertex_Y", "Primary Vertex Y Coordinate Distribution", (int)(yRange/yStep), -yRange/2., yRange/2.);
@@ -316,7 +405,12 @@ void Vx3DHLTAnalyzer::beginJob()
       dbe->setCurrentFolder("BeamPixel/EventInfo/reportSummaryContents");
     }
 
+  runNumber = 0;
   lumiCounter = 0;
+  beginTimeOfFit = 0;
+  endTimeOfFit = 0;
+  beginLumiOfFit = 0;
+  endLumiOfFit = 0;
 }
 
 
