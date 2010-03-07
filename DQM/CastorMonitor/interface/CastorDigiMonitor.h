be80ffb212cce0fa19460202fd27940c7d8d2d5d
Dmytro Volyanskyy <sha1-ee228e6b0533b5d2566d9787bda6c58e75d066a4@cern.ch>
2010-03-07 21:09:27 +0000

    add a Digi Monitor

---
+++ DQM/CastorMonitor/interface/CastorDigiMonitor.h
@@ -0,0 +1,70 @@
+#ifndef DQM_CASTORMONITOR_CASTORDIGIMONITOR_H
+#define DQM_CASTORMONITOR_CASTORDIGIMONITOR_H
+
+#include "DQM/CastorMonitor/interface/CastorBaseMonitor.h"
+
+#include "CondFormats/CastorObjects/interface/CastorPedestal.h"
+#include "CondFormats/CastorObjects/interface/CastorPedestalWidth.h"
+#include "CalibFormats/CastorObjects/interface/CastorCoderDb.h"
+#include "CalibFormats/CastorObjects/interface/CastorDbService.h"
+
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
+
+
+class CastorDigiMonitor: public CastorBaseMonitor {
+public:
+  CastorDigiMonitor(); 
+  ~CastorDigiMonitor(); 
+
+  void setup(const edm::ParameterSet& ps, DQMStore* dbe);
+
+  void processEvent(const CastorDigiCollection& cast,const CastorDbService& cond);
+
+
+  void done();
+  void reset();
+
+private: 
+  void perChanHists(vector<HcalCastorDetId> detID, vector<int> capID, vector<float> peds,
+		    map<HcalCastorDetId, map<int, MonitorElement*> > &toolP, 
+		    ////// map<HcalCastorDetId, map<int, MonitorElement*> > &toolS,
+		    string baseFolder);
+
+  bool doPerChannel_;
+  bool doFCpeds_;
+  map<HcalCastorDetId, map<int,MonitorElement*> >::iterator meo_;
+  vector<HcalCastorDetId> detID_;
+  vector<int> capID_;
+  vector<float> pedVals_;
+
+  string outputFile_;
+   
+  const CastorQIEShape* shape_;
+  const CastorQIECoder* channelCoder_;
+  //CastorCalibrations calibs_; 
+  
+
+  MonitorElement* meEVT_;
+  int ievt_;
+    
+  map<HcalCastorDetId,bool> REG;
+  MonitorElement* PEDESTAL_REFS;
+  MonitorElement* WIDTH_REFS;
+
+  struct{
+    map<HcalCastorDetId,map<int, MonitorElement*> > PEDVALS;
+    map<HcalCastorDetId,map<int, MonitorElement*> > SUBVALS;
+
+    MonitorElement* ALLPEDS;
+    MonitorElement* PEDRMS;
+
+    ////---- data from the Condition Database
+    MonitorElement* PEDESTAL_REFS;
+    MonitorElement* WIDTH_REFS;
+
+  } castHists ;
+
+};
+
+#endif
