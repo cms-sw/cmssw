f4bb5e91d14db21046e8a4069032c973cd9aa86c
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-03-08 17:11:05 +0000

    Referring to blobified product

--- CondCore/HLTPlugins/src/plugin.cc
+++ CondCore/HLTPlugins/src/plugin.cc
@@ -3,8 +3,8 @@
 #include "CondFormats/HLTObjects/interface/AlCaRecoTriggerBits.h"
 #include "CondFormats/DataRecord/interface/AlCaRecoTriggerBitsRcd.h"
 
-#include "CondFormats/HLTObjects/interface/HLTPrescaleTable.h"
+#include "CondFormats/HLTObjects/interface/HLTPrescaleTableCond.h"
 #include "CondFormats/DataRecord/interface/HLTPrescaleTableRcd.h"
 
 REGISTER_PLUGIN(AlCaRecoTriggerBitsRcd,AlCaRecoTriggerBits);
-REGISTER_PLUGIN(HLTPrescaleTableRcd,trigger::HLTPrescaleTable);
+REGISTER_PLUGIN(HLTPrescaleTableRcd,trigger::HLTPrescaleTableCond);
