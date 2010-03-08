f9f16ea0d550bcc1a43d1da6caf7852b7f2a6778
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2010-03-08 22:53:46 +0000

    Bug fix in logics.

--- DQM/TrackerCommon/src/TriggerHelper.cc
+++ DQM/TrackerCommon/src/TriggerHelper.cc
@@ -1,5 +1,5 @@
 //
-// $Id: TriggerHelper.cc,v 1.11 2010/03/03 12:34:42 vadler Exp $
+// $Id: TriggerHelper.cc,v 1.6 2010/03/08 20:53:57 vadler Exp $
 //
 
 
@@ -123,7 +123,7 @@ bool TriggerHelper::acceptDcs( const Event & event )
   // Getting the and/or DCS switch from the configuration
   // If it does not exist, the configuration is considered not to be present,
   // and the filter dos not have any effect
-  if ( onDcs_ ) return ( ! andOr_ ); // logically neutral, depending on base logical connective
+  if ( ! onDcs_ ) return ( ! andOr_ ); // logically neutral, depending on base logical connective
 
   // Accessing the DcsStatusCollection
   Handle< DcsStatusCollection > dcsStatus;
