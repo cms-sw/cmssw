c914b0d6497ef20504f32120c3ca67ce2c029147
Annapaola De Cosa <sha1-4f7f601e1310d1f94a8ce176474325c5ff77ac0c@cern.ch>
2010-03-08 17:22:26 +0000

    added distances from PV and BS

--- ElectroWeakAnalysis/Skimming/plugins/ZMuMuMuonUserData.cc
+++ ElectroWeakAnalysis/Skimming/plugins/ZMuMuMuonUserData.cc
@@ -12,6 +12,10 @@
 #include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
 #include "DataFormats/RecoCandidate/interface/IsoDepositDirection.h"
 
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+
 #include <vector>
 
 using namespace edm;
@@ -26,7 +30,7 @@ public:
 private:
   void produce( edm::Event &, const edm::EventSetup & );
   
-  InputTag src_;
+  InputTag src_,zGenParticlesMatch_,beamSpot_, primaryVertices_;
   double alpha_, beta_; 
   
   template<typename T>
@@ -50,13 +54,26 @@ double ZMuMuMuonUserData::isolation(const T & t, double alpha, double beta, bool
 ZMuMuMuonUserData::ZMuMuMuonUserData( const ParameterSet & cfg ):
   src_( cfg.getParameter<InputTag>( "src" ) ),
   alpha_(cfg.getParameter<double>("alpha")),
-  beta_(cfg.getParameter<double>("beta")) {
+  beta_(cfg.getParameter<double>("beta")), 
+  zGenParticlesMatch_(cfg.getParameter<InputTag>( "zGenParticlesMatch" )),
+  beamSpot_(cfg.getParameter<InputTag>( "beamSpot" )),
+  primaryVertices_(cfg.getParameter<InputTag>( "primaryVertices" )){
   produces<std::vector<pat::Muon> >();
 }
 
 void ZMuMuMuonUserData::produce( Event & evt, const EventSetup & ) {
   Handle<vector<pat::Muon>  > muons;
   evt.getByLabel(src_,muons);
+
+  Handle<BeamSpot> beamSpotHandle;
+  if (!evt.getByLabel(beamSpot_, beamSpotHandle)) {
+    std::cout << ">>> No beam spot found !!!"<<std::endl;
+  }
+
+  Handle<VertexCollection> primaryVertices;  // Collection of primary Vertices
+  if (!evt.getByLabel(primaryVertices_, primaryVertices)){
+    std::cout << ">>> No primary vertices  found !!!"<<std::endl;
+    }
 
   auto_ptr<vector<pat::Muon> > muonColl( new vector<pat::Muon> (*muons) );
   for (unsigned int i = 0; i< muonColl->size();++i){
@@ -65,6 +82,21 @@ void ZMuMuMuonUserData::produce( Event & evt, const EventSetup & ) {
     float relIso = isolation(m,alpha_, beta_, true);
     m.setIsolation(pat::User1Iso, iso);
     m.setIsolation(pat::User2Iso, relIso);
+    float dummy = 44;
+    m.addUserFloat("dummy",dummy);
+    TrackRef muTrkRef = m.innerTrack();
+    float zDaudxyFromBS = muTrkRef->dxy(beamSpotHandle->position());
+    float zDaudzFromBS = muTrkRef->dz(beamSpotHandle->position());
+    float zDaudxyFromPV = muTrkRef->dxy(primaryVertices->begin()->position() );
+    float zDaudzFromPV = muTrkRef->dz(primaryVertices->begin()->position() );	
+    cout<<"dxy from BS "<<zDaudxyFromBS<<endl;
+    cout<<"dz from BS "<<zDaudzFromBS<<endl;
+    cout<<"dxy from PV "<<zDaudxyFromPV<<endl;
+    cout<<"dz from PV "<<zDaudzFromPV<<endl;
+    m.addUserFloat("zDau_dxyFromBS", zDaudxyFromBS);
+    m.addUserFloat("zDau_dzFromBS", zDaudzFromBS);
+    m.addUserFloat("zDau_dxyFromPV", zDaudxyFromPV);
+    m.addUserFloat("zDau_dzFromPV", zDaudzFromPV);
   }
 
   evt.put( muonColl);
--- ElectroWeakAnalysis/Skimming/python/zMuMuMuonUserData.py
+++ ElectroWeakAnalysis/Skimming/python/zMuMuMuonUserData.py
@@ -4,16 +4,19 @@ import FWCore.ParameterSet.Config as cms
 userDataMuons = cms.EDProducer(
     "ZMuMuMuonUserData",
     src = cms.InputTag("selectedPatMuonsTriggerMatch"),
-    ptThreshold = cms.double("1.5"),
-    etEcalThreshold = cms.double("0.2"),
-    etHcalThreshold = cms.double("0.5"),
-    deltaRVetoTrk = cms.double("0.015"),
-    deltaRTrk = cms.double("0.3"),
-    deltaREcal = cms.double("0.25"),
-    deltaRHcal = cms.double("0.25"),
+    zGenParticlesMatch = cms.InputTag(""),
+    #ptThreshold = cms.double("1.5"),
+    #etEcalThreshold = cms.double("0.2"),
+    #etHcalThreshold = cms.double("0.5"),
+    #deltaRVetoTrk = cms.double("0.015"),
+    #deltaRTrk = cms.double("0.3"),
+    #deltaREcal = cms.double("0.25"),
+    #deltaRHcal = cms.double("0.25"),
     alpha = cms.double("0."),
     beta = cms.double("-0.75"),
-    relativeIsolation = cms.bool(False)
+    #relativeIsolation = cms.bool(False)
+    beamSpot = cms.InputTag("offlineBeamSpot"),
+    primaryVertices = cms.InputTag("offlinePrimaryVerticesWithBS")
     
     )
 
