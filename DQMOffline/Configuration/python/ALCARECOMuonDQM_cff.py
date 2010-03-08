1101327b5b882b69ad7d89d9a7882af0e3663861
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2010-03-08 10:39:32 +0000

    added import

--- DQMOffline/Configuration/python/ALCARECOMuonDQM_cff.py
+++ DQMOffline/Configuration/python/ALCARECOMuonDQM_cff.py
@@ -3,8 +3,9 @@
 #
 # Author Stefano Argiro'
 #
-# $Id$
+# $Id: ALCARECOMuonDQM_cff.py,v 1.1 2009/03/26 10:59:40 argiro Exp $
 #
 #
 
 from DQMOffline.CalibMuon.ALCARECODTCalibrationDQM_cfi import *
+from DQM.DTMonitorModule.ALCARECODTCalibSynchDQM_cff import *
