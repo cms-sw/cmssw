62c3fcb49bb32e72ccce6f38b007ad1654fb1c54
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-03-08 16:36:06 +0000

    fix GTs for 36X real data

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,6 +1,6 @@
 
 autoCond = { 'mc'      : 'MC_36Y_V1::All',
              'startup' : 'START36_V1::All',
-             'craft08' : 'CRAFT08_R_V10::All',
-             'craft09' : 'CRAFT09_R_V11::All',
+             'craft08' : 'CRAFT08_R_V11::All',
+             'craft09' : 'CRAFT09_R_V12::All',
              }
