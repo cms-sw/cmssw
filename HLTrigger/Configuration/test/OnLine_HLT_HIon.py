cb66a4013f1d10f4edfd2adbf426f296cfb0a122
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-03-09 12:36:14 +0000

    autCond expansion also for cmdline global tag

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -2557,7 +2557,7 @@ process.options = cms.untracked.PSet(
 )
 
 if 'GlobalTag' in process.__dict__:
-    process.GlobalTag.globaltag         = 'MC_36Y_V1::All'
+    process.GlobalTag.globaltag         = 'MC_3XY_V24::All'
     process.GlobalTag.connect           = 'frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'
 
 if 'Level1MenuOverride' in process.__dict__:
