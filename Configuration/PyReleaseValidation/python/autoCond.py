4b0c5d6a8ea6cabb3309faeb25d9cd27ffbd1fdd
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-03-09 13:17:46 +0000

    Update MC/STARTUP tags for 36X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,6 +1,6 @@
 
-autoCond = { 'mc'      : 'MC_36Y_V1::All',
-             'startup' : 'START36_V1::All',
+autoCond = { 'mc'      : 'MC_36Y_V2::All',
+             'startup' : 'START36_V2::All',
              'craft08' : 'CRAFT08_R_V11::All',
              'craft09' : 'CRAFT09_R_V12::All',
              }
