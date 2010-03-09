3e2f999551fb13917f1d9abd3703155130a3f586
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-03-09 03:30:46 +0000

    Adding JPT

--- RecoJets/Configuration/python/RecoJPTJets_cff.py
+++ RecoJets/Configuration/python/RecoJPTJets_cff.py
@@ -1,11 +1,9 @@
-
-# $Id: RecoJetsGR_cff.py,v 1.6 2009/08/13 15:07:46 srappocc Exp $
-from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
-from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
+# $Id: RecoJPTJets_cff.py,v 1.1 2009/10/19 17:53:21 stadie Exp $
+from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
 
 #define jetPlusTrackZSPCorJet sequences
-jetPlusTrackZSPCorJetIcone5   = cms.Sequence(ZSPJetCorrectionsIcone5+JetPlusTrackCorrectionsIcone5)
-jetPlusTrackZSPCorJetSiscone5 = cms.Sequence(ZSPJetCorrectionsSisCone5+JetPlusTrackCorrectionsSisCone5)
-jetPlusTrackZSPCorJetAntiKt5  = cms.Sequence(ZSPJetCorrectionsAntiKt5+JetPlusTrackCorrectionsAntiKt5)
+jetPlusTrackZSPCorJetIcone5   = cms.Sequence(JetPlusTrackCorrectionsIcone5)
+jetPlusTrackZSPCorJetSiscone5 = cms.Sequence(JetPlusTrackCorrectionsSisCone5)
+jetPlusTrackZSPCorJetAntiKt5  = cms.Sequence(JetPlusTrackCorrectionsAntiKt5)
  
-recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetIcone5+jetPlusTrackZSPCorJetSiscone5+jetPlusTrackZSPCorJetAntiKt5)
+recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetAntiKt5)
