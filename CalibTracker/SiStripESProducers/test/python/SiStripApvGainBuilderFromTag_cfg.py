4a4f6bfa7966d4279877474b6032202d6de78ae9
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2010-03-10 14:17:02 +0000

    Using an EmptySource

--- CalibTracker/SiStripESProducers/test/python/SiStripApvGainBuilderFromTag_cfg.py
+++ CalibTracker/SiStripESProducers/test/python/SiStripApvGainBuilderFromTag_cfg.py
@@ -6,12 +6,17 @@ process.MessageLogger = cms.Service("MessageLogger",
     destinations = cms.untracked.vstring('cout')
 )
 
-process.source = cms.Source("EmptyIOVSource",
-    firstValue = cms.uint64(1),
-    lastValue = cms.uint64(1),
-    timetype = cms.string('runnumber'),
-    interval = cms.uint64(1)
+process.source = cms.Source("EmptySource",
+    numberEventsInRun = cms.untracked.uint32(1),
+    firstRun = cms.untracked.uint32(128408)
 )
+
+# process.source = cms.Source("EmptyIOVSource",
+#     firstValue = cms.uint64(128409),
+#     lastValue = cms.uint64(128409),
+#     timetype = cms.string('runnumber'),
+#     interval = cms.uint64(1)
+# )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
@@ -24,10 +29,10 @@ process.poolDBESSource = cms.ESSource("PoolDBESSource",
         authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
     ),
     timetype = cms.untracked.string('runnumber'),
-    connect = cms.string('sqlite_file:dbfile.db'),
+    connect = cms.string('oracle://cms_orcoff_prod/CMS_COND_31X_STRIP'),
     toGet = cms.VPSet(cms.PSet(
         record = cms.string('SiStripApvGainRcd'),
-        tag = cms.string('SiStripApvGain_Ideal_31X')
+        tag = cms.string('SiStripApvGain_GR10_v1_hlt')
     ))
 )
 
@@ -40,14 +45,14 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     connect = cms.string('sqlite_file:dbfile.db'),
     toPut = cms.VPSet(cms.PSet(
         record = cms.string('SiStripApvGainRcd2'),
-        tag = cms.string('SiStripApvGain_v2')
+        tag = cms.string('SiStripApvGain_gaussian')
     ))
 )
 
 process.prod = cms.EDFilter("SiStripApvGainBuilderFromTag",
                             genMode = cms.string("gaussian"),
                             MeanGain = cms.double(1.),
-                            SigmaGain = cms.double(1.),
+                            SigmaGain = cms.double(0.1),
                             MinPositiveGain = cms.double(0.)
 )
 
