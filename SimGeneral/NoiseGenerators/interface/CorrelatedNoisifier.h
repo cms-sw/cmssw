0917a15703200053a30afda6cb3122d7a34ffa2d
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2010-03-10 20:23:15 +0000

    Clean up to clarify function, elim unused features, and move implementation to separate icc file

--- SimGeneral/NoiseGenerators/interface/CorrelatedNoisifier.h
+++ SimGeneral/NoiseGenerators/interface/CorrelatedNoisifier.h
@@ -4,283 +4,60 @@
 /**
    \class CorrelatedNoisifier
 
-   \brief adds noise to the given frame
+   \brief adds noise to the given frame.
+
+Takes input correlation matrix C and from it creates
+an upper-triangular matrix H such that H*Htranspose = C.
 
 Algorithm taken from
-http://cg.scs.carleton.ca/~luc/chapter_eleven.pdf
+http://cg.scs.carleton.ca/~luc/chapter_eleven.pdf, p 564-567
 Uses a Cholesky decomposition
+
+Any input array f is "noisified" with f += H*r
+where r is an array of random numbers
+
+The above matrix multiplication is expedited in the 
+trivial cases of a purely diagonal or identity correlation matrix.
 */
-#include "CLHEP/Random/JamesRandom.h"
+
 #include "CLHEP/Random/RandGaussQ.h"
 #include "DataFormats/Math/interface/Error.h"
 
-#include <valarray>
-#include <vector>
-#include <utility>
-#include <algorithm>
-#include <ostream>
-#include <cassert>
-
-
-
-template<class M> class CorrelatedNoisifier
+template<class M> 
+class CorrelatedNoisifier
 {
-public:
+   public:
 
-  explicit CorrelatedNoisifier(M & matrix);
-  CorrelatedNoisifier(M & matrix, CLHEP::HepRandomEngine & engine);
+      CorrelatedNoisifier( const M&                symCorMat , 
+			   CLHEP::HepRandomEngine* engine      = 0 ) ;
 
-  virtual ~CorrelatedNoisifier() { delete theRandomGaussian;}
+      virtual ~CorrelatedNoisifier() ;
 
-  /// sets all elements along the diagonal of
-  /// the correlation matrix to be value
-  void setDiagonal(double value);
-  
-  void setOffDiagonal(int neighbor, double value);
+      void resetCorrelationMatrix( const M& symCorMat ) ;
 
-  void setRandomEngine();
-  void setRandomEngine(CLHEP::HepRandomEngine & engine);
+      template<class T>
+      void noisify( T& frame ) const ; // applies random noise to frame
 
-  template<class T>
-  void noisify(T & frame) const
-  {
-    // make a vector of random values
-    assert(frame.size() == (int)theSize);
-    std::valarray<double> uncorrelated(0.,theSize);
-    for (unsigned int i=0; i<theSize; i++)
-      uncorrelated[i]=theRandomGaussian->fire();
-    if ( isIdentity_ ) 
-    {
-      for(unsigned int i = 0; i < theSize; ++i)
-      { 
-        frame[i] += uncorrelated[i]; 
-      }
-    }
-    else if ( isDiagonal_ )
-    {
-      for(unsigned int i = 0; i < theSize; ++i)
-      {
-        frame[i] += uncorrelated[i] * std::sqrt(theCovarianceMatrix(i,i)); 
-      }
-    }
-    else 
-    {
+   private:
 
-      // rotate them to make a correlated noise vector
-      //std::valarray<double> correlated = theMatrix * uncorrelated;
-      std::valarray<double> correlated(0., theSize);
-      for (unsigned int i = 0; i < theSize; ++i) 
-        {
-          //@@ Not sure why the old version only used the lower half, but it worked
-          for (unsigned int j = 0; j <= i; ++j) 
-            {
-              correlated[i] += theMatrix(i,j)*uncorrelated[j];
-            }
-        }
+      static const double k_precision ; // precision to which 0 & 1 are compared
 
-      // stuff 'em in the frame
-      for(unsigned int i = 0; i < theSize; ++i)
-      {
-        frame[i] += correlated[i];
-      }
-    }
-  }
+      void init( const M& symCorMat ) ;
 
+      bool computeDecomposition( const M& symCorMat ) ;
 
+      bool checkDecomposition( const M& symCorMat ,
+			       M&       HHtDiff     ) const ;
 
-  void computeDecomposition();
+      void checkOffDiagonal( const M& symCorMat ) ;
 
-  // for test purpose
-  const M& covmatrix() const { return theCovarianceMatrix ; }
+      CLHEP::RandGaussQ* m_rangau ;
 
-private:
-  void init();
-  void checkOffDiagonal();
+      bool m_isDiagonal ;
+      bool m_isIdentity ;
 
-  const M theCovarianceMatrix;
-  M theMatrix;
-  mutable CLHEP::RandGaussQ * theRandomGaussian;
-  unsigned int theSize; 
-  bool isDiagonal_;
-  bool isIdentity_;
-
+      M m_H ;
 };
 
-#include "FWCore/Utilities/interface/Exception.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "FWCore/Utilities/interface/RandomNumberGenerator.h"
-#include "FWCore/ServiceRegistry/interface/ServiceRegistry.h"
-#include "FWCore/PluginManager/interface/PluginManager.h"
-#include "FWCore/PluginManager/interface/standard.h"
-
-template<class M> CorrelatedNoisifier<M>::CorrelatedNoisifier(M & matrix)
-  :  theCovarianceMatrix(matrix),
-     theRandomGaussian(0),
-     theSize(matrix.kRows),
-     isDiagonal_(true),
-     isIdentity_(true)
-{
-  init();
-  setRandomEngine();
-}
- 
-
-template<class M> CorrelatedNoisifier<M>::CorrelatedNoisifier(M & matrix, CLHEP::HepRandomEngine & engine)
-  :  theCovarianceMatrix(matrix),
-     theRandomGaussian(0),
-     theSize(matrix.kRows),
-     isDiagonal_(true),
-     isIdentity_(true)
-{
-  init();
-  setRandomEngine(engine);
-}
-
-
-template<class M>
-void CorrelatedNoisifier<M>::init()
-{
-  for ( unsigned int i = 0 ; i < theSize ; i++ ) {
-    theMatrix(i,i) = 0.;
-    for ( unsigned int j = 0 ; j < theSize ; j++ ) {
-      theMatrix(i,j) = 0.;
-      theMatrix(j,i) = 0.;
-    }
-  }
-
-  checkOffDiagonal();
-
-  if ( ! isDiagonal_ ) computeDecomposition();
-}
-
-
-template<class M> void CorrelatedNoisifier<M>::setRandomEngine()
-{
-   edm::Service<edm::RandomNumberGenerator> rng;
-   if ( ! rng.isAvailable()) {
-     throw cms::Exception("Configuration")
-       << "CorrelatedNoisifier requires the RandomNumberGeneratorService\n"
-          "which is not present in the configuration file.  You must add the service\n"
-          "in the configuration file or remove the modules that require it.";
-   }
-   setRandomEngine(rng->getEngine());
-}
-
-
-template<class M> void CorrelatedNoisifier<M>::setRandomEngine(CLHEP::HepRandomEngine & engine)
-{
-  if(theRandomGaussian) delete theRandomGaussian;
-  theRandomGaussian = new CLHEP::RandGaussQ(engine);
-}
-
-
-template<class M> void CorrelatedNoisifier<M>::setDiagonal(double value) 
-{
-  for(unsigned int i = 0; i < theSize; ++i) 
-  {
-    theCovarianceMatrix(i,i) = value;
-  }
-
-  checkOffDiagonal();
-
-  if ( ! isDiagonal_ ) computeDecomposition();
-
-} 
-
-template<class M> void CorrelatedNoisifier<M>::setOffDiagonal(int distance, double value)
-{
-  for(unsigned int column = 0; column < theSize; ++column)
-  {
-    unsigned int row = column - distance;
-    if(row < 0) continue;
-    theCovarianceMatrix(row,column) = value;
-    theCovarianceMatrix(column,row) = value;
-
-  }
-
-  checkOffDiagonal();
-
-  if ( ! isDiagonal_ ) computeDecomposition();
-
-}
-
-
-template<class M> void CorrelatedNoisifier<M>::computeDecomposition()
-{
-
-  for ( unsigned int i = 0 ; i < theSize ; i++ ) {
-    for ( unsigned int j = 0 ; j < theSize ; j++ ) {
-      theMatrix(i,j) = 0.;
-    }
-  }
-
-  double sqrtSigma00 = theCovarianceMatrix(0,0);
-  if ( sqrtSigma00 <= 0. ) {
-    throw cms::Exception("CorrelatedNoisifier") << "non positive variance.";
-  }
-  sqrtSigma00 = std::sqrt(sqrtSigma00);
-
-  for ( unsigned int i = 0 ; i < theSize ; i++ )
-    {
-      double hi0 = theCovarianceMatrix(i,0)/sqrtSigma00;
-      theMatrix(i,0) = hi0;
-    }
-
-  for ( unsigned int i = 1 ; i < theSize ; i++ ) 
-    {
-
-      for ( unsigned int j = 1 ; j < i ; j++ )
-        {
-          double hij = theCovarianceMatrix(i,j);
-          for ( unsigned int k = 0 ; k <= j-1 ; k++ ) hij -= theMatrix(i,k)*theMatrix(j,k);
-          hij /= theMatrix(j,j);
-          theMatrix(i,j) = hij;
-        }
-      
-      double hii = theCovarianceMatrix(i,i);
-      for ( unsigned int j = 0 ; j <= i-1 ; j++ ) {
-        double hij = theMatrix(i,j);
-        hii -= hij*hij;
-      }
-      if(hii <= 0.) {
-        throw cms::Exception("CorrelatedNoisifier")
-            << "Bad covariance matrix. " << theCovarianceMatrix;
-      }
-      hii = sqrt(hii);
-      theMatrix(i,i) = hii;
-
-    }
-}
-
-template<class M>
-void CorrelatedNoisifier<M>::checkOffDiagonal() {
-
-  isDiagonal_ = true;
-  isIdentity_ = true;
-
-  for ( unsigned int i = 0 ; i < theSize ; i++ ) {
-    for ( unsigned int j = 0 ; j < theSize ; j++ ) {
-      if ( i == j )
-      {
-        if( theCovarianceMatrix(i,j) != 1.)
-        {
-          isIdentity_ = false;
-        }
-        if( theCovarianceMatrix(i,j) < 0.)
-        {
-          throw cms::Exception("CorrelatedNoisifier") 
-            << "Bad correlation matrix.  Negative diagonal";
-        }
-      }
-      if ( i != j && theCovarianceMatrix(i,j) != 0. ) 
-      { 
-        isDiagonal_ = false ; 
-        isIdentity_ = false;
-        return ; 
-      }
-    }
-  }
-  
-}
 
 #endif
