0ffb1aaafe51a3d0cc95cc22b04af6c9a977ccf1
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2010-03-10 20:24:27 +0000

    Improve correlation test by printout of figure of merit

--- SimGeneral/NoiseGenerators/test/CorrelatedNoisifierTest.cpp
+++ SimGeneral/NoiseGenerators/test/CorrelatedNoisifierTest.cpp
@@ -1,68 +1,72 @@
 #include "SimGeneral/NoiseGenerators/interface/CorrelatedNoisifier.h"
+#include "SimGeneral/NoiseGenerators/interface/CorrelatedNoisifier.icc"
 #include "CommonTools/Statistics/interface/AutocorrelationAnalyzer.h"
 #include "CLHEP/Random/JamesRandom.h"
 
+typedef math::ErrorD<10>::type MyMat;
 
+template class CorrelatedNoisifier< MyMat > ;
 
-class BoringSignal
-{
-public:
-  BoringSignal(int size)
-    : theVector(size, 0.)
-    {
-    }
-
-  double & operator[](int i) { return theVector[i]; }
-  const double & operator[](int i) const  { return theVector[i]; }
-  int size() const {return theVector.size();}
-private:
-  std::vector<double> theVector;
-};
-
+template
+void
+CorrelatedNoisifier< MyMat >::noisify(std::vector<double>&) const ;
 
 int main()
 {
-  math::ErrorD<10>::type input;
-  for (int k = 0; k < 10; k++) {
-    for (int kk = k; kk < 10; kk++) {
-      input(k,kk) =
-        kk == k ? 1
-        : kk == k+1 ? 0.67
-        : kk == k+2 ? 0.53
-        : kk == k+3 ? 0.44
-        : kk == k+4 ? 0.39
-        : kk == k+5 ? 0.36
-        : kk == k+6 ? 0.38
-        : kk == k+7 ? 0.35
-        : kk == k+8 ? 0.36
-        : kk == k+9 ? 0.32
-        : 0.;
-    }
-  }
-
-  std::cout << std::endl << "Initial correlations:" << "\n" << input << std::endl;;
-  CLHEP::HepJamesRandom  engine;
-  CorrelatedNoisifier<math::ErrorD<10>::type> noisifier(input, engine);
-  AutocorrelationAnalyzer analyzer(10);
-
-  int nTotal = 10000;
-  for (int i=0; i<nTotal; i++) {
-    BoringSignal samples(10);
-    noisifier.noisify(samples);
-    analyzer.analyze(samples);
-  }
-
-  std::cout << analyzer << std::endl;
-  ROOT::Math::SMatrixIdentity a;
-  math::ErrorD<10>::type input2(a);
-  for (int k = 0; k < 10; k++) {
-    for (int kk = k; kk < 10; kk++) {
-      for (int ix = 0; ix < 10; ix++) {
-        input2(k,kk)  += input(k,ix)*input(ix,kk);
+   math::ErrorD<10>::type input;
+   for (int k = 0; k < 10; k++) 
+   {
+      for (int kk = k; kk < 10; kk++) 
+      {
+	 input(k,kk) =
+	    kk == k ? 1
+	    : kk == k+1 ? 0.67
+	    : kk == k+2 ? 0.53
+	    : kk == k+3 ? 0.44
+	    : kk == k+4 ? 0.39
+	    : kk == k+5 ? 0.36
+	    : kk == k+6 ? 0.38
+	    : kk == k+7 ? 0.35
+	    : kk == k+8 ? 0.36
+	    : kk == k+9 ? 0.32
+	    : 0.;
       }
-    }
-  }
-  std::cout << std::endl << "SQUARE of initial matrix:" << std::endl << input2;
+   }
+   CLHEP::HepJamesRandom  engine;
+   CorrelatedNoisifier<math::ErrorD<10>::type> noisifier(input, &engine);
+   AutocorrelationAnalyzer analyzer(10);
+
+   const unsigned int nTotal = 1000000;
+   for( unsigned int i=0; i<nTotal; ++i ) 
+   {
+      std::vector<double> samples(10);
+      noisifier.noisify(samples);
+      analyzer.analyze(samples);
+   }
+
+   double big ( -999 ) ;
+   math::ErrorD<10>::type ratdif ;
+   for (int k = 0; k < 10; k++) 
+   {
+      for (int kk = k; kk < 10; kk++) 
+      {
+	 const double diff ( input(k,kk)-analyzer.correlation(k,kk) ) ;
+	 ratdif(k,kk) = diff/input(k,kk) ;
+	 if( fabs( ratdif(k,kk) ) > big ) big = fabs( ratdif(k,kk) ) ;
+      }
+   }
+
+   std::cout<<"In "<<nTotal<<" trials, the biggest fractional deviation\n"
+	    <<"of observed correlations from input correlations ="
+	    <<big<<std::endl ;
+
+   std::cout << std::endl << "Initial correlations:" << "\n" 
+	     << input << std::endl;;
+
+   std::cout << analyzer << std::endl;
+
+   std::cout<< ratdif<<std::endl;
+   std::cout << std::endl << "\nSQUARE of initial matrix:\n" << input*input<<std::endl ;
 }
 
 
