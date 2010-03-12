0935fa0668e4e6c0702001ef5065a896aedbd18c
Ferenc Sikler <sha1-753db23f0bb46b999a71a8a004b971875b5ec09c@cern.ch>
2010-03-12 07:34:05 +0000

    Fixed int-uint comparison.

--- RecoPixelVertexing/PixelLowPtUtilities/src/TrackCleaner.cc
+++ RecoPixelVertexing/PixelLowPtUtilities/src/TrackCleaner.cc
@@ -44,80 +44,61 @@ TrackCleaner::~TrackCleaner()
 }
 
 /*****************************************************************************/
-bool TrackCleaner::areSame(const TrackingRecHit * a,
-                           const TrackingRecHit * b)
+int TrackCleaner::getLayer(const DetId & id)
 {
-  if(a->geographicalId() != b->geographicalId())
-    return false;
-
-  if(fabs(a->localPosition().x() - b->localPosition().x()) < 1e-5 &&
-     fabs(a->localPosition().y() - b->localPosition().y()) < 1e-5)
-    return true;
+  if(id.subdetId() == int(PixelSubdetector::PixelBarrel))
+  {
+    PXBDetId pid(id);
+    return 0 + ((pid.layer() - 1)*2) + ((pid.ladder() - 1)%2);
+  } 
   else
-    return false;
+  {
+    PXFDetId pid(id);
+    return 6 + ((pid.disk()  - 1)*2) + ((pid.panel()  - 1)%2);
+  } 
 }
 
 /*****************************************************************************/
-bool TrackCleaner::isCompatible(const DetId & i1,
-                                const DetId & i2)
+bool TrackCleaner::hasCommonDetUnit
+  (vector<const TrackingRecHit *> recHitsA,
+   vector<const TrackingRecHit *> recHitsB,
+   vector<DetId> detIds)
 {
-  // different subdet
-  if(i1.subdetId() != i2.subdetId()) return true;
-
-  if(i1.subdetId() == int(PixelSubdetector::PixelBarrel))
-  { // barrel
-    PXBDetId p1(i1);
-    PXBDetId p2(i2);
-
-    if(p1.layer() != p2.layer()) return true;
-
-    int dphi = abs(int(p1.ladder() - p2.ladder()));
-    static int max[3] = {20, 32, 44};
-    if(dphi > max[p1.layer()-1] / 2) dphi = max[p1.layer()-1] - dphi;
-
-    int dz   = abs(int(p1.module() - p2.module()));
-
-    if(dphi == 1 && dz <= 1) return true;
-  }
-  else
-  { // endcap
-    PXFDetId p1(i1);
-    PXFDetId p2(i2);
-
-    if(p1.side() != p2.side() ||
-       p1.disk() != p2.disk()) return true;
-
-    int dphi = abs(int(p1.blade() - p2.blade()));
-    static int max = 24;
-    if(dphi > max / 2) dphi = max - dphi;
-
-    int dr   = abs(int( ((p1.module()-1) * 2 + (p1.panel()-1)) -
-                        ((p2.module()-1) * 2 + (p2.panel()-1)) ));
-
-    if(dphi <= 1 && dr <= 1 && !(dphi == 0 && dr == 0)) return true;
-  }
+  for(vector<const TrackingRecHit *>::const_iterator
+      recHit = recHitsB.begin(); recHit!= recHitsB.end(); recHit++)
+    if(find(recHitsA.begin(), recHitsA.end(), *recHit) == recHitsA.end())
+    if(find(detIds.begin(),detIds.end(),
+            (*recHit)->geographicalId()) != detIds.end())
+      return true;
 
   return false;
 }
 
 /*****************************************************************************/
-bool TrackCleaner::canBeMerged
+bool TrackCleaner::hasCommonLayer
   (vector<const TrackingRecHit *> recHitsA,
-   vector<const TrackingRecHit *> recHitsB)
+   vector<const TrackingRecHit *> recHitsB,
+   vector<int> detLayers)
 {
- bool ok = true;
+  for(vector<const TrackingRecHit *>::const_iterator
+      recHit = recHitsB.begin(); recHit!= recHitsB.end(); recHit++)
+    if(find(recHitsA.begin(), recHitsA.end(), *recHit) == recHitsA.end())
+    if(find(detLayers.begin(),detLayers.end(),
+            getLayer((*recHit)->geographicalId())) != detLayers.end())
+      return true;
 
- for(vector<const TrackingRecHit *>::const_iterator
-     recHitA = recHitsA.begin(); recHitA!= recHitsA.end(); recHitA++)
- for(vector<const TrackingRecHit *>::const_iterator
-     recHitB = recHitsB.begin(); recHitB!= recHitsB.end(); recHitB++)
-   if(!areSame(*recHitA,*recHitB))
-     if(!isCompatible((*recHitA)->geographicalId(),
-                      (*recHitB)->geographicalId()))
-        ok = false;
-
-  return ok;
+  return false;
 }
+
+/*****************************************************************************/
+struct RadiusComparator
+{ 
+  bool operator() (const TrackingRecHit * h1,
+                   const TrackingRecHit * h2)
+  { 
+    return (h1 < h2);
+  };
+};
 
 /*****************************************************************************/
 TracksWithRecHits TrackCleaner::cleanTracks
@@ -137,7 +118,7 @@ TracksWithRecHits TrackCleaner::cleanTracks
     << " [TrackCleaner] initial tracks : " << tracks.size();
 
   for(unsigned int i = 0; i < tracks.size(); i++)
-  LogTrace("TrackCleaner")
+  LogTrace("MinBiasTracking")
     << "   Track #" << i << " : " << HitInfo::getInfo(tracks[i].second);
 
   do
@@ -145,11 +126,6 @@ TracksWithRecHits TrackCleaner::cleanTracks
   changes = 0;
 
   RecHitMap recHitMap;
-
-/*
-  LogTrace("MinBiasTracking")
-    << " [TrackCleaner] fill rechit map";
-*/
 
   // Fill the rechit map
   for(unsigned int i = 0; i < tracks.size(); i++)
@@ -169,13 +145,9 @@ TracksWithRecHits TrackCleaner::cleanTracks
     // Skip if 'i' already removed
     if(!keep[i]) continue;
 
-    bool addedNewHit = false;
-
-/*
-    do
-    {
-*/
     TrackMap trackMap;
+    vector<DetId> detIds;
+    vector<int> detLayers;
 
     // Go trough all rechits of this track
     for(vector<const TrackingRecHit *>::const_iterator
@@ -187,8 +159,11 @@ TracksWithRecHits TrackCleaner::cleanTracks
 
       for(vector<unsigned int>::iterator j = sharing.begin();
                                          j!= sharing.end(); j++)
-        if(i < *j)
-           trackMap[*j]++;
+        if(i < *j) trackMap[*j]++;
+
+      // Fill detLayers vector
+      detIds.push_back((*recHit)->geographicalId());
+      detLayers.push_back(getLayer((*recHit)->geographicalId()));
     }
 
     // Check for tracks with shared rechits
@@ -200,33 +175,21 @@ TracksWithRecHits TrackCleaner::cleanTracks
 
       if(tracks[i].second.size() >=3) 
       { // triplet tracks
-        if((*sharing).second > min(int(tracks[i].second.size()),
-                                   int(tracks[j].second.size()))/2)
+        if( (*sharing).second > int(min(tracks[i].second.size(),
+                                        tracks[j].second.size())/2) )
         { // more than min(hits1,hits2)/2 rechits are shared
-          if(canBeMerged(tracks[i].second,tracks[j].second))
-          { // no common layer
+
+          if(!hasCommonLayer(tracks[i].second,tracks[j].second,detLayers))
+          { 
             // merge tracks, add separate hits of the second to the first one
             for(vector<const TrackingRecHit *>::const_iterator
                 recHit = tracks[j].second.begin();
                 recHit!= tracks[j].second.end(); recHit++)
-            {
-              bool ok = true;
-              for(vector<const TrackingRecHit *>::const_iterator
-                recHitA = tracks[i].second.begin();
-                recHitA!= tracks[i].second.end(); recHitA++)
-                if(areSame(*recHit,*recHitA)) ok = false;
-
-              if(ok)
-              {
+              if(find(tracks[i].second.begin(),
+                      tracks[i].second.end(),*recHit) == tracks[i].second.end())
                 tracks[i].second.push_back(*recHit);
-                recHitMap[*recHit].push_back(i);
-                addedNewHit = true;
-              }
-            }
 
-            LogTrace("TrackCleaner") 
-              << "   Merge #" << i << " #" << j
-              << ", first now has " << tracks[i].second.size();
+            LogTrace("MinBiasTracking") << "   Merge #" << i << " #" << j;
   
             // Remove second track
             keep[j] = false;
@@ -234,44 +197,34 @@ TracksWithRecHits TrackCleaner::cleanTracks
            changes++;
           }
           else
-          { // there is a common layer, keep smaller impact
-            if(fabs(tracks[i].first->d0())
-             < fabs(tracks[j].first->d0()))
+          { // remove track with higher impact / chi2
+            if(tracks[i].first->chi2() < tracks[j].first->chi2())
               keep[j] = false;
             else
               keep[i] = false;
 
-            LogTrace("TrackCleaner")
-              << "   Clash #" << i << " #" << j
-              << " keep lower d0 " << tracks[i].first->d0()
-                            << " " << tracks[j].first->d0()
-              << ", keep #" << (keep[i] ? i : ( keep[j] ? j : 9999 ) );
+            LogTrace("MinBiasTracking") << "   Clash #" << i << " #" << j << " keep lower chi2";
   
             changes++;
           }
         }
         else
-        { // note more than 50%, but at least two are shared
+        {
           if((*sharing).second > 1)
           {
             if(tracks[i].second.size() != tracks[j].second.size())
-            { // keep longer
+            {
               if(tracks[i].second.size() > tracks[j].second.size()) 
                 keep[j] = false; else keep[i] = false; 
               changes++;
-
-              LogTrace("TrackCleaner")
-                << "   Sharing " << (*sharing).second << " remove by size";
+            LogTrace("MinBiasTracking") << "   Sharing " << (*sharing).second << " remove by size";
             }
             else
-            { // keep smaller impact
-              if(fabs(tracks[i].first->d0())
-               < fabs(tracks[j].first->d0()))
+            { 
+              if(tracks[i].first->chi2() < tracks[j].first->chi2())
                 keep[j] = false; else keep[i] = false; 
               changes++;
-
-              LogTrace("TrackCleaner")
-                << "   Sharing " << (*sharing).second << " remove by d0";
+            LogTrace("MinBiasTracking") << "   Sharing " << (*sharing).second << " remove by chi2";
             } 
           }
         }
@@ -287,10 +240,6 @@ TracksWithRecHits TrackCleaner::cleanTracks
         }
       }
     }
-/*
-    }
-    while(addedNewHit);
-*/
   }
   }
   while(changes > 0);
@@ -307,7 +256,7 @@ TracksWithRecHits TrackCleaner::cleanTracks
     << " [TrackCleaner] cleaned tracks : " << cleaned.size();
 
   for(unsigned int i = 0; i < cleaned.size(); i++)
-  LogTrace("TrackCleaner")
+  LogTrace("MinBiasTracking")
     << "   Track #" << i << " : " << HitInfo::getInfo(cleaned[i].second);
 
   return cleaned;
