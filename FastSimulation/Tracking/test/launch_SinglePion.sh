f5e763b412b000fa3bb0b3a8bcf611cbcbbaae23
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2010-03-12 13:46:00 +0000

    360pre2 update

--- FastSimulation/Tracking/test/launch_SinglePion.sh
+++ FastSimulation/Tracking/test/launch_SinglePion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-castorDir=/castor/cern.ch/user/a/azzi/CMSSW352/
-cmsswDir=/afs/cern.ch/user/a/azzi/scratch0/CMSSW_3_5_2/src/
+castorDir=/castor/cern.ch/user/a/azzi/CMSSW360pre2/
+cmsswDir=/afs/cern.ch/user/a/azzi/scratch0/FastSimTrackTuning/CMSSW_3_6_0_pre2/src/
 
 njobs=0
 nevt=0
