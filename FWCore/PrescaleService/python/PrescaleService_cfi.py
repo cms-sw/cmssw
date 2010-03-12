a6ec84a3aa7e16476db393f22ee285dc6f0c7224
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-03-12 13:00:10 +0000

    Add special saveToConfig_ parameter for saving into provenance.

--- FWCore/PrescaleService/python/PrescaleService_cfi.py
+++ FWCore/PrescaleService/python/PrescaleService_cfi.py
@@ -1,13 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
-PrescaleService = cms.Service(
-    "PrescaleService",
+PrescaleService = cms.Service("PrescaleService",
     prescaleTable = cms.VPSet(
       cms.PSet(
         pathName  = cms.string("HLTPath"),
         prescales = cms.vuint32(1)
-        )
-      ),
-    lvl1Labels = cms.vstring('default'),
-    lvl1DefaultLabel = cms.untracked.string('default')
-    )
+      )
+    ),
+    lvl1Labels       = cms.vstring('default'),
+    lvl1DefaultLabel = cms.untracked.string('default'),
+#
+# The following is a special Service class parameter. It's value (if !="")
+# denotes the name of a new PSet to store the Service configuration, which
+# will be inserted into the ProcessPSet.
+#
+    saveConfigTo_    = cms.string("@prescale_table")
+)
