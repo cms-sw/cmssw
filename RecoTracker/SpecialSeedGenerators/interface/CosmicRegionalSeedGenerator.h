f6c9940dbf33ecc161b631050c0ad4e9eceaf3f5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-03-15 22:04:47 +0000

    Don't use deprecated headers

--- RecoTracker/SpecialSeedGenerators/interface/CosmicRegionalSeedGenerator.h
+++ RecoTracker/SpecialSeedGenerators/interface/CosmicRegionalSeedGenerator.h
@@ -9,7 +9,7 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
 #include "DataFormats/Common/interface/Handle.h"
