f6769a98dd477bbbb25548eede31e9ef95da35db
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-03-15 08:05:06 +0000

    Removed JPT related classes

--- CondFormats/JetMETObjects/src/classes.h
+++ CondFormats/JetMETObjects/src/classes.h
@@ -3,7 +3,6 @@
 #include "CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h"
 #include "CondFormats/JetMETObjects/interface/SimpleJetCorrectionUncertainty.h"
 #include "CondFormats/JetMETObjects/interface/SimpleJetCorrector.h"
-
 #include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
 #include <vector>
  
