7cca919ee48d330ab8435b34b0257735cba83101
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2010-03-15 21:49:50 +0000

    Restore HEAD.

--- PhysicsTools/PatExamples/test/analyzePatTrigger_cfg.py
+++ PhysicsTools/PatExamples/test/analyzePatTrigger_cfg.py
@@ -5,28 +5,33 @@ process = cms.Process( "TEST" )
 process.load( "FWCore.MessageService.MessageLogger_cfi" )
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool( False )
-)
+  )
 
-process.source = cms.Source( "PoolSource",
-    fileNames = cms.untracked.vstring(
+process.source = cms.Source(
+    "PoolSource"
+  , fileNames = cms.untracked.vstring(
         'file:edmPatTrigger.root'
-    )
-)
+      )
+  )
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32( -1 )
-)
+  )
 
-process.TFileService = cms.Service( "TFileService",
-    fileName = cms.string( 'rootPatTrigger.root' )
-)
+process.TFileService = cms.Service(
+    "TFileService"
+  , fileName = cms.string( 'rootPatTrigger.root' )
+  )
 
-process.triggerAnalysis = cms.EDAnalyzer( "PatTriggerAnalyzer",
-    trigger      = cms.InputTag( "patTrigger" ),
-    triggerEvent = cms.InputTag( "patTriggerEvent" ),
-    muons        = cms.InputTag( "selectedPatMuons" ),
-    muonMatch    = cms.string( 'muonTriggerMatchHLTMuons' )
-)
+process.triggerAnalysis = cms.EDAnalyzer(
+    "PatTriggerAnalyzer"
+  , trigger      = cms.InputTag( "patTrigger" )
+  , triggerEvent = cms.InputTag( "patTriggerEvent" )
+  , muons        = cms.InputTag( "selectedPatMuons" )
+  , muonMatch    = cms.string( 'muonTriggerMatchHLTMuons' )
+  , minID = cms.uint32( 81 )
+  , maxID = cms.uint32( 96 )
+  )
 
 process.p = cms.Path(
     process.triggerAnalysis
-)
+  )
