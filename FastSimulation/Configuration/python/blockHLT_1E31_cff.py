31eb2954f460c09e57ff1e64ed29e2012ba31fd2
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-03-15 08:33:41 +0000

    V01-15-53 HLTrigger/Configuration - ConfDB /dev/CMSSW_3_5_0/XXXX/V47

--- FastSimulation/Configuration/python/blockHLT_1E31_cff.py
+++ FastSimulation/Configuration/python/blockHLT_1E31_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_5_0/1E31/V39 (CMSSW_3_5_3)
+# /dev/CMSSW_3_5_0/1E31/V47 (CMSSW_3_5_3_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_5_0/1E31/V39')
+  tableName = cms.string('/dev/CMSSW_3_5_0/1E31/V47')
 )
 
 
--- FastSimulation/Configuration/python/blockHLT_8E29_cff.py
+++ FastSimulation/Configuration/python/blockHLT_8E29_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_5_0/8E29/V39 (CMSSW_3_5_3)
+# /dev/CMSSW_3_5_0/8E29/V47 (CMSSW_3_5_3_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_5_0/8E29/V39')
+  tableName = cms.string('/dev/CMSSW_3_5_0/8E29/V47')
 )
 
 
