eddc52c5a4504948b1a69e6bdbdc35fdd6effe5a
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2010-03-15 16:39:46 +0000

    Added AnalyzeBeamHalo module to the DQM sequence (adds 3040 bins)

--- DQMOffline/JetMET/python/jetMETDQMOfflineSourceCosmic_cff.py
+++ DQMOffline/JetMET/python/jetMETDQMOfflineSourceCosmic_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from DQMOffline.JetMET.jetMETAnalyzerCosmic_cff import *
 from DQMOffline.JetMET.caloTowers_cff           import *
 from DQMOffline.JetMET.BeamHaloAnalyzer_cfi     import *
+AnalyzeBeamHalo.StandardDQM = cms.bool(True)
 
-#jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerCosmicSequence)
-jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerCosmicSequence)
+jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerCosmicSequence)
+#jetMETDQMOfflineSourceCosmic = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerCosmicSequence)
--- DQMOffline/JetMET/python/jetMETDQMOfflineSource_cff.py
+++ DQMOffline/JetMET/python/jetMETDQMOfflineSource_cff.py
@@ -4,5 +4,7 @@ from DQMOffline.JetMET.jetMETAnalyzer_cff import *
 from DQMOffline.JetMET.caloTowers_cff     import *
 from DQMOffline.JetMET.BeamHaloAnalyzer_cfi import *
 
-#jetMETDQMOfflineSource = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerSequence)
-jetMETDQMOfflineSource = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerSequence)
+AnalyzeBeamHalo.StandardDQM = cms.bool(True)
+
+jetMETDQMOfflineSource = cms.Sequence(analyzecaloTowersDQM*AnalyzeBeamHalo*jetMETAnalyzerSequence)
+#jetMETDQMOfflineSource = cms.Sequence(analyzecaloTowersDQM*jetMETAnalyzerSequence)
