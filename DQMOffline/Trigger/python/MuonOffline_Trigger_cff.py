9daf27dc22c1cec29c0ac0dfe1fb188e42dda35c
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2010-03-17 18:28:19 +0000

    Removed dimuon dqm

--- DQMOffline/Trigger/python/MuonOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/MuonOffline_Trigger_cff.py
@@ -7,8 +7,12 @@ from DQMOffline.Trigger.TnPEfficiency_cff import *
 
 from DQMOffline.Trigger.topHLTDiMuonDQM_cfi import *
 
+# muonFullOfflineDQM = cms.Sequence(offlineDQMMuonTrig
+# 								  + topTrigOfflineDQM
+# 								  + topHLTDiMuonAnalyzer
+# 								  + TnPEfficiency)
+
 muonFullOfflineDQM = cms.Sequence(offlineDQMMuonTrig
-								  + topTrigOfflineDQM
-								  + topHLTDiMuonAnalyzer
+								  + topTrigOfflineDQM								  
 								  + TnPEfficiency)
 
