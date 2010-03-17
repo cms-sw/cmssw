e589061e399ff2b5c44488a7b5871929f360dba1
Maxime Gouzevitch <sha1-6d6c49995332c5d88975de3ea22da0524073fc9a@cern.ch>
2010-03-17 00:49:32 +0000

    Producers uniformized for PFV0, PFConversion, PFDisplacedTrackerVertex

--- RecoParticleFlow/PFTracking/plugins/modules.cc
+++ RecoParticleFlow/PFTracking/plugins/modules.cc
@@ -4,19 +4,20 @@
 #include "RecoParticleFlow/PFTracking/interface/PFElecTkProducer.h"
 #include "RecoParticleFlow/PFTracking/interface/LightPFTrackProducer.h"
 #include "RecoParticleFlow/PFTracking/interface/PFNuclearProducer.h"
-#include "RecoParticleFlow/PFTracking/interface/PFConversionsProducer.h"
-#include "RecoParticleFlow/PFTracking/interface/PFV0Producer.h"
+#include "RecoParticleFlow/PFTracking/plugins/PFConversionProducer.h"
+#include "RecoParticleFlow/PFTracking/plugins/PFV0Producer.h"
 #include "RecoParticleFlow/PFTracking/interface/ElectronSeedMerger.h"
 #include "RecoParticleFlow/PFTracking/plugins/PFDisplacedVertexCandidateProducer.h"
 #include "RecoParticleFlow/PFTracking/plugins/PFDisplacedVertexProducer.h"
-
+#include "RecoParticleFlow/PFTracking/plugins/PFDisplacedTrackerVertexProducer.h"
 
 DEFINE_FWK_MODULE(GoodSeedProducer);
 DEFINE_FWK_MODULE(PFElecTkProducer);
 DEFINE_FWK_MODULE(LightPFTrackProducer);
 DEFINE_FWK_MODULE(PFNuclearProducer);
-DEFINE_FWK_MODULE(PFConversionsProducer);
+DEFINE_FWK_MODULE(PFConversionProducer);
 DEFINE_FWK_MODULE(PFV0Producer);
 DEFINE_FWK_MODULE(ElectronSeedMerger);
 DEFINE_FWK_MODULE(PFDisplacedVertexCandidateProducer);
 DEFINE_FWK_MODULE(PFDisplacedVertexProducer);
+DEFINE_FWK_MODULE(PFDisplacedTrackerVertexProducer);
