218b21e8d4dec96e5d1155ea14f30a4e74b61295
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-03-17 15:31:01 +0000

    *** empty log message ***

--- CondFormats/JetMETObjects/test/TestCorrections.C
+++ CondFormats/JetMETObjects/test/TestCorrections.C
@@ -6,29 +6,14 @@ void TestCorrections()
   AutoLibraryLoader::enable(); 
   TRandom *rnd = new TRandom();
   rnd->SetSeed(0);
-  ////////////// Define the JetCorrector ///////////////////////
-  string Levels = "L4";
-  string Tags = "../data/L4EMF_AK5Calo.txt:";
-  FactorizedJetCorrector *JetCorrector = new FactorizedJetCorrector(Levels,Tags);
-  ////////////// Fill demo histogram  ////////////////////////////
-  double vx[1000],vy[1000];
-  for(unsigned index = 0; index < 1000; index++ ) 
-    {
-      double pt  = rnd->Uniform(10,500);    
-      double eta = rnd->Uniform(-5,5);  
-      double emf = rnd->Uniform(-1,1);
-      JetCorrector->setJetEta(eta); 
-      JetCorrector->setJetPt(pt);
-      JetCorrector->setJetEMF(emf);  
-      double scale = JetCorrector->getCorrection();
-      vx[index] = pt;
-      vy[index] = scale;
-    }
-  TGraph *g = new TGraph(1000,vx,vy);
-  ////////////// Draw demo histograms /////////////////////////////
-  TCanvas *c = new TCanvas("Correction","Correction");
-  g->SetTitle("Correction");
-  g->GetXaxis()->SetTitle("p_{T} (GeV)");
-  g->GetYaxis()->SetTitle("Correction Factor");
-  g->Draw("AP"); 
+  ////////////// Construct the JetCorrectorParameters objects ///////////////////////
+  string L3Tag = "../data/Summer09_7TeV_ReReco332_L3Absolute_AK5Calo.txt";
+  string L2Tag = "../data/Summer09_7TeV_ReReco332_L2Relative_AK5Calo.txt";
+  JetCorrectorParameters *L3JetPar = new JetCorrectorParameters(L3Tag);
+  JetCorrectorParameters *L2JetPar = new JetCorrectorParameters(L2Tag);
+  vector<JetCorrectorParameters> *vPar;
+  vPar.push_back(L2JetPar);
+  vPar.push_back(L3JetPar);
+  ////////////// Construct a FactorizedJetCorrector object //////////////////////
+  FactorizedJetCorrector *JetCorrector = new FactorizedJetCorrector(vPar) 
 }
