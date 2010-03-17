c68324d5dbe89829acfe88001dc15d4400e280ce
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-03-17 15:18:30 +0000

    Removed the JEC from txt files functionality

--- JetMETCorrections/Modules/interface/JetCorrectionService.h
+++ JetMETCorrections/Modules/interface/JetCorrectionService.h
@@ -37,14 +37,11 @@ class JetCorrectionService : public edm::ESProducer,
 			     public edm::EventSetupRecordIntervalFinder
 {
   private:
-    //boost::shared_ptr<JetCorrector> mCorrector;
     edm::ParameterSet mParameterSet;
     std::string mLevel;
-    std::string mEra;
     std::string mAlgo;
     std::string mSection;
     std::string mPayloadName;
-    bool mUseCondDB;
     bool mDebug;
 
   public:
@@ -53,50 +50,29 @@ class JetCorrectionService : public edm::ESProducer,
       {
         std::string label = fConfig.getParameter<std::string>("@module_label"); 
         mLevel            = fConfig.getParameter<std::string>("level");
-        mEra              = fConfig.getParameter<std::string>("era");
         mAlgo             = fConfig.getParameter<std::string>("algorithm");
         mSection          = fConfig.getParameter<std::string>("section");
-        mUseCondDB        = fConfig.getUntrackedParameter<bool>("useCondDB",false);
         mDebug            = fConfig.getUntrackedParameter<bool>("debug",false);
-        mPayloadName      = mLevel;
         
+        mPayloadName = mLevel;
         if (!mAlgo.empty())
           mPayloadName += "_"+mAlgo;
         if (!mSection.empty())
           mPayloadName += "_"+mSection; 
-        setWhatProduced(this, label);
-        findingRecord <JetCorrectionsRecord> ();
-        
+        if (mDebug)
+          std::cout<<"Payload: "<<mPayloadName<<std::endl;
+        setWhatProduced(this,label);
+        findingRecord<JetCorrectionsRecord>();      
       }
     //------------- destruction ----------------------------------------
     ~JetCorrectionService () {}
     //------------- member functions -----------------------------------
     boost::shared_ptr<JetCorrector> produce(const JetCorrectionsRecord& iRecord) 
       {
-        if (mUseCondDB)
-          {
-            edm::ESHandle<JetCorrectorParameters> JetCorPar;
-            iRecord.get(mPayloadName,JetCorPar); 
-            boost::shared_ptr<JetCorrector> mCorrector(new Corrector(*JetCorPar,mParameterSet));
-            return mCorrector;
-          }
-        else
-          {
-            std::string fileName("CondFormats/JetMETObjects/data/");
-            if (!mEra.empty())
-              fileName += mEra;
-            if (!mLevel.empty())
-              fileName += "_"+mLevel;
-            if (!mAlgo.empty())
-              fileName += "_"+mAlgo;
-            fileName += ".txt";
-            if (mDebug)
-              std::cout<<"Parameter File: "<<fileName<<std::endl;
-            edm::FileInPath fip(fileName);
-            JetCorrectorParameters *tmpJetCorPar = new JetCorrectorParameters(fip.fullPath(),mSection);
-            boost::shared_ptr<JetCorrector> mCorrector(new Corrector(*tmpJetCorPar,mParameterSet));
-            return mCorrector;
-          }
+        edm::ESHandle<JetCorrectorParameters> JetCorPar;
+        iRecord.get(mPayloadName,JetCorPar); 
+        boost::shared_ptr<JetCorrector> mCorrector(new Corrector(*JetCorPar,mParameterSet));
+        return mCorrector;
       }
     void setIntervalFor(const edm::eventsetup::EventSetupRecordKey&,const edm::IOVSyncValue&,edm::ValidityInterval& fIOV)
       {
