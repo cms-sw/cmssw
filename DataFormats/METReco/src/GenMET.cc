66e2a3bd46777fe8939117b0f8dca10b7c3e7f11
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2010-03-18 15:26:09 +0000

    bug fix: Obsolete data-members still need to be initialized :)

--- DataFormats/METReco/src/GenMET.cc
+++ DataFormats/METReco/src/GenMET.cc
@@ -10,7 +10,12 @@ GenMET::GenMET()
   gen_data.ChargedHadEtFraction    = 0.0;
   gen_data.MuonEtFraction          = 0.0;
   gen_data.InvisibleEtFraction     = 0.0;
-
+  
+  //obsolete variables that need to be removed in 4_X_Y
+  gen_data.m_EmEnergy = 0.0;
+  gen_data.m_HadEnergy = 0.0;
+  gen_data.m_InvisibleEnergy = 0.0;
+  gen_data.m_AuxiliaryEnergy = 0.0;
 }
 
 bool GenMET::overlap( const Candidate & ) const 
