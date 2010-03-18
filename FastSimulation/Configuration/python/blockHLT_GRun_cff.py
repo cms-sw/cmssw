77874a93292ce971e0c6d224da1defd9eddcfb91
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-03-18 18:35:28 +0000

    first attempt at adding the GRun maneu to FastSim

---
+++ FastSimulation/Configuration/python/blockHLT_GRun_cff.py
@@ -0,0 +1,10 @@
+# /dev/CMSSW_3_5_5/GRun/V9 (CMSSW_3_5_3_HLT5)
+
+import FWCore.ParameterSet.Config as cms
+
+
+HLTConfigVersion = cms.PSet(
+  tableName = cms.string('/dev/CMSSW_3_5_5/GRun/V9')
+)
+
+
