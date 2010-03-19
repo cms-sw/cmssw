5f371d5b3862f08261e81d506a9234a4e8759f7a
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2010-03-19 20:15:06 +0000

    Updated parameters for reflagger

--- RecoLocalCalo/HcalRecAlgos/test/hcalrechitreflagger_cfi.py
+++ RecoLocalCalo/HcalRecAlgos/test/hcalrechitreflagger_cfi.py
@@ -15,7 +15,7 @@ hcalrechitReflagger = cms.EDProducer('HcalRecHitReflagger',
 
                                    hf_PET_params  = cms.PSet( PET_EnergyThreshLong=cms.untracked.vdouble([162.4,-10.19,0.21]),
                                                               PET_ETThreshLong=cms.untracked.vdouble([0]),
-                                                              PET_EnergyThreshShort=cms.untracked.vdouble([130,-6.61,0.1153]),
+                                                              PET_EnergyThreshShort=cms.untracked.vdouble([129.9,-6.61,0.1153]),
                                                               PET_ETThreshShort=cms.untracked.vdouble([0]),
                                                               PET_ratiocut = cms.vdouble([0.98]) # parameterized as polynomial of E, not ieta!!
                                                               ),
@@ -23,9 +23,9 @@ hcalrechitReflagger = cms.EDProducer('HcalRecHitReflagger',
                                    hf_S9S1_params = cms.PSet( S9S1_EnergyThreshLong=cms.untracked.vdouble([162.4,-10.19,0.21]),
                                                               S9S1_ETThreshLong=cms.untracked.vdouble([0]),
                                                               # No explicit cuts on S9S1 energies?
-                                                              S9S1_EnergyThreshShort=cms.untracked.vdouble([130,-6.61,0.1153]),
+                                                              S9S1_EnergyThreshShort=cms.untracked.vdouble([129.9,-6.61,0.1153]),
                                                               S9S1_ETThreshShort=cms.untracked.vdouble([0]),
-                                                              S9S1_optimumslope=cms.vdouble([0.095, 0.062, 0.2974,-0.02498, 0.0005243]) # this is the parameterization of optimum slope vs. ieta.  First two values are the slopes at |ieta| = 40 & 41, followed by the polynomial parameterization of slope with ieta
+                                                              S9S1_optimumslope=cms.vdouble([0.0913756, 0.0589927, 0.3084,-0.02577, 0.0005351]) # this is the parameterization of optimum slope vs. ieta.  First two values are the slopes at |ieta| = 40 & 41, followed by the polynomial parameterization of slope with ieta
                                                               ),
                                        
                                    debug          = cms.untracked.int32(0)
