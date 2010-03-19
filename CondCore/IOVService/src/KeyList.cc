990ff47fe437e8ed923bd78be0f8d8ba13a9280f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-03-19 13:01:12 +0000

    protect keylist

--- CondCore/IOVService/src/KeyList.cc
+++ CondCore/IOVService/src/KeyList.cc
@@ -14,7 +14,7 @@ namespace cond {
       m_data[i].clear();
       if (keys[i]!=0) {
         IOVSequence::const_iterator p = m_sequence.iov().findSince(keys[i]);
-	if (p!=m_sequence.iovs().end()) { 
+	if (p!=m_sequence.iov().end()) { 
 	  pool::Ref<Wrapper> ref = m_sequence.db().getTypedObject<Wrapper>( (*p).wrapperToken() );
 	  m_data[i].copyShallow(ref);
 	  m_data[i]->loadAll();
