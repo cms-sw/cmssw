bf6b4e316d43840f19f737dcd0288d947d2a1876
Maxime Gouzevitch <sha1-6d6c49995332c5d88975de3ea22da0524073fc9a@cern.ch>
2010-03-19 11:13:47 +0000

    Conversion production moved out and Lambdas included into V0 collections

--- RecoParticleFlow/PFTracking/python/particleFlowTrackWithDisplacedVertex_cff.py
+++ RecoParticleFlow/PFTracking/python/particleFlowTrackWithDisplacedVertex_cff.py
@@ -8,28 +8,10 @@ from RecoParticleFlow.PFTracking.pfV0_cfi import *
 from RecoParticleFlow.PFTracking.particleFlowDisplacedVertexCandidate_cff import *
 from RecoParticleFlow.PFTracking.particleFlowDisplacedVertex_cff import *
 
-from RecoEgamma.EgammaPhotonProducers.trackerOnlyConversionSequence_cff import *
-#TRAJECTORIES IN THE EVENT
 
-
-#UNCOMMENT THE LINES THAT START WITH #DON# IN ORDER TO ADD CONVERSION FROM PF CLUSTERS
-#DON#from RecoEgamma.EgammaPhotonProducers.softConversionSequence_cff import *
-#DON#softConversionIOTracks.TrajectoryInEvent = cms.bool(True)
-#DON#softConversionOITracks.TrajectoryInEvent = cms.bool(True)
-#DON#pfConversions.OtherConversionCollection =cms.VInputTag(cms.InputTag("softConversions:softConversionCollection"))
-#DON#pfConversions.OtherOutInCollection      =           cms.VInputTag(cms.InputTag("softConversionOITracks"))
-#DON#pfConversions.OtherInOutCollection      =           cms.VInputTag(cms.InputTag("softConversionIOTracks"))
-
-#UNCOMMENT THE LINES THAT START WITH #HON# IN ORDER TO ADD CONVERSION FROM GENERAL TRACKS
-#HON#from RecoEgamma.EgammaPhotonProducers.trackerOnlyConversionSequence_cff import *
-
-#HON#pfConversions.OtherConversionCollection =cms.VInputTag(cms.InputTag("trackerOnlyConversions"))
-#HON#pfConversions.OtherOutInCollection      =           cms.VInputTag(cms.InputTag("generalTracks"))
-#HON#pfConversions.OtherInOutCollection      =           cms.VInputTag(cms.InputTag("generalTracks"))
 
 particleFlowTrackWithDisplacedVertex =cms.Sequence(
     pfTrackElec*
-    trackerOnlyConversionSequence*
     pfConversions*
     pfV0*
     particleFlowDisplacedVertexCandidate*
--- RecoParticleFlow/PFTracking/python/pfV0_cfi.py
+++ RecoParticleFlow/PFTracking/python/pfV0_cfi.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 pfV0 = cms.EDProducer(
     "PFV0Producer", 
-    V0List = cms.VInputTag(cms.InputTag("generalV0Candidates","Kshort")
-#                           ,cms.InputTag("generalV0Candidates","LambdaBar"),
-#                           cms.InputTag("generalV0Candidates","Lambda")
+    V0List = cms.VInputTag(cms.InputTag("generalV0Candidates","Kshort"),
+                           cms.InputTag("generalV0Candidates","Lambda")
                            )
                   
 )
