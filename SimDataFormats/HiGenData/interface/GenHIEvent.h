b0bfc22d50f2ffea8190bfd1e3fac534cffe66ff
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-03-20 14:19:45 +0000

    New code

--- SimDataFormats/HiGenData/interface/GenHIEvent.h
+++ SimDataFormats/HiGenData/interface/GenHIEvent.h
@@ -1,26 +1,58 @@
 #ifndef SimDataFormats_GenHIEvent_h
 #define SimDataFormats_GenHIEvent_h
 
-#include "SimDataFormats/HiGenData/interface/SubEvent.h"
-#include "DataFormats/Candidate/interface/CandidateFwd.h"
-#include "HepMC/GenVertex.h"
-#include "HepMC/GenEvent.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
 namespace edm {
-   
    class GenHIEvent {
    public:
-      GenHIEvent() : evts_(0), sel_(0) {}
-      GenHIEvent(std::vector<SubEvent> evs , int x = 0) : evts_(evs), sel_(x) {}
+
+      typedef std::vector<std::vector<reco::GenParticleRef> > SubEventCollection;
+
+      GenHIEvent() :
+	 b_(-99),
+	 npart_(-99),
+	 ncoll_(-99),
+	 nhard_(-99),
+	 phi_(-99) 
+	    {
+	       subevents_.reserve(0);
+	       ;}
+
+      GenHIEvent(double b, int npart, int ncoll, int nhard, double phi) : 
+	 b_(b), 
+	 npart_(npart), 
+	 ncoll_(ncoll), 
+	 nhard_(nhard), 
+	 phi_(phi) 
+	    {
+               subevents_.reserve(0);
+	       ;}
+
       virtual                    ~GenHIEvent() {}
-      SubEvent                  getSubEvent(int sub_id) const;
-      std::vector<HepMC::GenParticle*>    getParticles(const HepMC::GenEvent& evt, int sub_id) const;
-      const HepMC::GenVertex*                 getVertex(const HepMC::GenEvent& evt, int sub_id) const;
-      int           getNsubs() const {return evts_.size();}
+
+      double b() const {return b_;}
+      int Npart() const {return npart_;}
+      int Ncoll() const {return ncoll_;}
+      int Nhard() const {return nhard_;}
+      double evtPlane() const {return phi_;}
+
+      void setGenParticles(const reco::GenParticleCollection*) const;
+      const std::vector<reco::GenParticleRef>    getSubEvent(int sub_id) const;
+
+      int           getNsubs() const {return subevents_.size();}
 
    private:
-      std::vector<SubEvent> evts_;
+
+      mutable SubEventCollection subevents_;
       int sel_;
+
+      double b_;
+      int npart_;
+      int ncoll_;
+      int nhard_;
+      double phi_;
    };
 }
 #endif
