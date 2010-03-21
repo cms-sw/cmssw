5eda691fa3e271b348764a9071731f2e896839bf
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-03-21 15:38:14 +0000

    warning removed

--- DataFormats/HeavyIonEvent/src/CentralityBins.cc
+++ DataFormats/HeavyIonEvent/src/CentralityBins.cc
@@ -5,7 +5,7 @@ int CentralityBins::getBin(double value) const {
    if(value < 0) return -1;
 
    int bin = table_.size() - 1;
-   for(int i = 0; i < table_.size(); ++i){
+   for(unsigned int i = 0; i < table_.size(); ++i){
       if(value >= table_[i].bin_edge){
 	 bin = i;
 	 return bin;
