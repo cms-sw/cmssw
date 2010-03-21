538386bda1071380b92130a068d2a1f4e06246c8
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2010-03-21 18:50:01 +0000

    *** empty log message ***

--- DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
+++ DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
@@ -9,12 +9,13 @@ from DQM.RPCMonitorClient.RPCDqmClient_cfi import *
 
 from  DQM.RPCMonitorClient.RPCChamberQuality_cfi import *
 
-from DQM.RPCMonitorClient.RPCEfficiencySecondStep_cfi import *
+from  DQM.RPCMonitorClient.RPCEfficiencySecondStep_cfi import *
 
-qTesterRPC = cms.EDAnalyzer("QualityTester",
+qTesterRPC = cms.EDFilter("QualityTester",
     qtList = cms.untracked.FileInPath('DQM/RPCMonitorClient/test/RPCQualityTests.xml'),
     prescaleFactor = cms.untracked.int32(10)
 )
 
 
-rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcEventSummary*rpcefficiencysecond)
+
+rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcEventSummary*rpcefficiencysecond )
