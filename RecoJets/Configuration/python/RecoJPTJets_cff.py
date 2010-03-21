6b962e774c15bcba401ae3dfbd69ea3a3b3548de
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2010-03-21 19:05:55 +0000

    adding recoJetAssociations to highlevelreco and jetplustrackzspcorjeticone5 to recojptjets

--- RecoJets/Configuration/python/RecoJPTJets_cff.py
+++ RecoJets/Configuration/python/RecoJPTJets_cff.py
@@ -1,4 +1,4 @@
-# $Id: RecoJPTJets_cff.py,v 1.1 2009/10/19 17:53:21 stadie Exp $
+# $Id: RecoJPTJets_cff.py,v 1.2 2010/03/09 03:30:46 srappocc Exp $
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
 
 #define jetPlusTrackZSPCorJet sequences
@@ -6,4 +6,4 @@ jetPlusTrackZSPCorJetIcone5   = cms.Sequence(JetPlusTrackCorrectionsIcone5)
 jetPlusTrackZSPCorJetSiscone5 = cms.Sequence(JetPlusTrackCorrectionsSisCone5)
 jetPlusTrackZSPCorJetAntiKt5  = cms.Sequence(JetPlusTrackCorrectionsAntiKt5)
  
-recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetAntiKt5)
+recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetIcone5*jetPlusTrackZSPCorJetAntiKt5)
