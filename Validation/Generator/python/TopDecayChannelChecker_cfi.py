66203ed5b209bc4fa7c7962eda1340b569ab384e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-22 21:01:36 +0000

    reduced number of configurables; added a few comments

--- Validation/Generator/python/TopDecayChannelChecker_cfi.py
+++ Validation/Generator/python/TopDecayChannelChecker_cfi.py
@@ -1,8 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 topDecayChannelChecker = cms.EDAnalyzer('TopDecayChannelChecker',
+  ## input collection; this maybe any collection of type
+  ## reco::GenParticles.
   src = cms.InputTag('genParticles'),
-  outputFileName = cms.string('TopDecayChannelChecker.root'),
-  saveDQMMEs     = cms.bool(True),
-  logEvents = cms.uint32(10),                                        
+                                        
+  ## name of the root output file that will contain the
+  ## histograms.
+  outputFile = cms.string('TopDecayChannelChecker.root'),
+
+  ## number of events for which a full partile listing
+  ## will be printed into the logfile. 0 will switch
+  ## this kind of logging off.
+  logEvents = cms.uint32(10)
 )
