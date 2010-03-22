206f2c89abece25e7e4d856ff9293cfd6b35f509
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2010-03-22 13:10:29 +0000

    Switched to using parameters 	JPTZSPCorrectorICone5.UseEfficiency = cms.bool(False) 	JetPlusTrackZSPCorJetAntiKt5.UseZSP = cms.bool(False) instead of txt files with 1. everythere

--- JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
+++ JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
@@ -1,27 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cfi import *
-JPTZSPCorrectorICone5.ResponseMap =   'CondFormats/JetMETObjects/data/CMSSW_31X_resptowers.txt'
-JPTZSPCorrectorICone5.EfficiencyMap = 'CondFormats/JetMETObjects/data/CMSSW_167_TrackNonEff_one.txt'
-JPTZSPCorrectorICone5.LeakageMap =    'CondFormats/JetMETObjects/data/CMSSW_167_TrackLeakage_one.txt'
+JPTZSPCorrectorICone5.ResponseMap   = 'CondFormats/JetMETObjects/data/CMSSW_31X_resptowers.txt'
+JPTZSPCorrectorICone5.UseEfficiency = cms.bool(False)
 
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
-#from RecoJets.JetPlusTracks.ZSPJetCorrections332_cff import *
-
-#from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
-#JPTZSPCorrectorICone5.ResponseMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt")
-#JPTZSPCorrectorICone5.EfficiencyMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt")
-#JPTZSPCorrectorICone5.LeakageMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt")
-#ZSPJetCorJetIcone5.src = cms.InputTag("iterativeCone5CaloJets")
+JetPlusTrackZSPCorJetAntiKt5.UseZSP = cms.bool(False)
 
 from Configuration.StandardSequences.Reconstruction_cff import *
 from RecoTauTag.RecoTau.CaloRecoTauTagInfoProducer_cfi import *
 caloRecoTauTagInfoProducer.CaloJetTracksAssociatorProducer = cms.InputTag('JPTAntiKt5JetTracksAssociatorAtVertex')
-#caloRecoTauTagInfoProducer.CaloJetTracksAssociatorProducer = cms.InputTag('JPTiterativeCone5JetTracksAssociatorAtVertex')
 
 jptRecoTauProducer = cms.Sequence(
 	JetPlusTrackCorrectionsAntiKt5 *
-#	JetPlusTrackCorrectionsIcone5 *
         caloRecoTauTagInfoProducer *
         caloRecoTauProducer
 )
