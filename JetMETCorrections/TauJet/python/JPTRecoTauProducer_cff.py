3a17d5030ffba0ae716ec1f059c8e072bcf5cda9
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2010-03-22 11:52:18 +0000

    Changed using JPT jets from RecoJets instead of JetMETCorrections

--- JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
+++ JetMETCorrections/TauJet/python/JPTRecoTauProducer_cff.py
@@ -1,32 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-from JetMETCorrections.Configuration.JetPlusTrackCorrections_cfi import *
-JPTZSPCorrectorICone5.ResponseMap =   'JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt'
-JPTZSPCorrectorICone5.EfficiencyMap = 'JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt'
-JPTZSPCorrectorICone5.LeakageMap =    'JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt'
-#For CMSSW_3_1_6:
-#JPTZSPCorrectorICone5.NonEfficiencyFile     = 'CMSSW_167_TrackNonEff_one'
-#JPTZSPCorrectorICone5.NonEfficiencyFileResp = 'CMSSW_167_TrackLeakage_one'
-#JPTZSPCorrectorICone5.ResponseFile          = 'CMSSW_31X_resptowers'
+#from JetMETCorrections.Configuration.JetPlusTrackCorrections_cfi import *
+from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cfi import *
+#JPTZSPCorrectorICone5.ResponseMap =   'JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt'
+#JPTZSPCorrectorICone5.EfficiencyMap = 'JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt'
+#JPTZSPCorrectorICone5.LeakageMap =    'JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt'
+JPTZSPCorrectorICone5.ResponseMap =   'CondFormats/JetMETObjects/data/CMSSW_31X_resptowers.txt'
+#JPTZSPCorrectorICone5.EfficiencyMap = 'CondFormats/JetMETObjects/data/CMSSW_167_TrackNonEff_one.txt'
+#JPTZSPCorrectorICone5.LeakageMap =    'CondFormats/JetMETObjects/data/CMSSW_167_TrackLeakage_one.txt'
 
-from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
-from JetMETCorrections.Configuration.ZSPJetCorrections332_cff import *
+from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
+#from RecoJets.JetPlusTracks.ZSPJetCorrections332_cff import *
+
 #from JetMETCorrections.Configuration.ZSPJetCorrections219_cff import *
-JPTZSPCorrectorICone5.ResponseMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt")
-JPTZSPCorrectorICone5.EfficiencyMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt")
-JPTZSPCorrectorICone5.LeakageMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt")
-ZSPJetCorJetIcone5.src = cms.InputTag("iterativeCone5CaloJets")
+#JPTZSPCorrectorICone5.ResponseMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_31X_resptowers.txt")
+#JPTZSPCorrectorICone5.EfficiencyMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackNonEff_one.txt")
+#JPTZSPCorrectorICone5.LeakageMap = cms.string("JetMETCorrections/Configuration/data/CMSSW_167_TrackLeakage_one.txt")
+#ZSPJetCorJetIcone5.src = cms.InputTag("iterativeCone5CaloJets")
 
 from Configuration.StandardSequences.Reconstruction_cff import *
 from RecoTauTag.RecoTau.CaloRecoTauTagInfoProducer_cfi import *
-caloRecoTauTagInfoProducer.CaloJetTracksAssociatorProducer = cms.InputTag('ZSPiterativeCone5JetTracksAssociatorAtVertex')
+caloRecoTauTagInfoProducer.CaloJetTracksAssociatorProducer = cms.InputTag('JPTiterativeCone5JetTracksAssociatorAtVertex')
 
 jptRecoTauProducer = cms.Sequence(
-#        ZSPJetCorrectionsIcone5 *
-#        JetPlusTrackCorrectionsIcone5 *
-# in order to work with CMSSW_3_1_6: (to be deprecated)
-	ZSPJetCorrections *
-	JetPlusTrackCorrections *
+	JetPlusTrackCorrectionsIcone5 *
         caloRecoTauTagInfoProducer *
         caloRecoTauProducer
 )
