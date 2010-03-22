8f4f0d2e73a25b1e970799528f0fc3e81e4d3e42
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-03-22 18:22:06 +0000

    temporary event content for fastjet PU developments (keep everything)

--- RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
+++ RecoHI/HiJetAlgos/python/RecoHiJets_EventContent_cff.py
@@ -2,14 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 #Full Event content ---- temporary
 RecoHiJetsRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_*Pu*CaloJets_*_*',
-                                           'keep recoGenJets_*HiGenJets_*_*'
+    outputCommands = cms.untracked.vstring('keep *_*Pu*CaloJets_*_*',
+                                           'keep *_*HiGenJets_*_*'
                                            )
     )
 
 RecoHiJetsFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCaloJets_*Pu*CaloJets_*_*',
-                                           'keep recoGenJets_*HiGenJets_*_*'
+    outputCommands = cms.untracked.vstring('keep *_*Pu*CaloJets_*_*',
+                                           'keep *_*HiGenJets_*_*'
                                            )
     )
 
