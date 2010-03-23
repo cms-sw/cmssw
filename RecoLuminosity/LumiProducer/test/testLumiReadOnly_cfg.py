fef7290ec6db2c1c4f3388d696655397ee0c9f89
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-03-23 17:53:46 +0000

    update

--- RecoLuminosity/LumiProducer/test/testLumiReadOnly_cfg.py
+++ RecoLuminosity/LumiProducer/test/testLumiReadOnly_cfg.py
@@ -9,14 +9,14 @@ process.options = cms.untracked.PSet(
   Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
 )
 process.maxLuminosityBlocks=cms.untracked.PSet(
-    input=cms.untracked.int32(3)
+    input=cms.untracked.int32(-1)
 )
 #process.maxEvents = cms.untracked.PSet(
 #  input = cms.untracked.int32(3)
 #)
 process.source= cms.Source("PoolSource",
               fileNames=cms.untracked.vstring('file:testLumiProd.root'),
-              firstRun=cms.untracked.uint32(120020),
+              firstRun=cms.untracked.uint32(124025),
               firstLuminosityBlock = cms.untracked.uint32(1),                           
               firstEvent=cms.untracked.uint32(1),
              )
