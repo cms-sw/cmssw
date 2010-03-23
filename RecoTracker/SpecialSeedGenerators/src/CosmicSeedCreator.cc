d35e95815eb159d253ca6d28df2ee8eeffc6b96f
tschudi <sha1-b2c7dad895ec9e4e12654a2b4a94508b7307887c@cern.ch>
2010-03-23 08:34:51 +0000

    Regional Reconstruction for cosmics during collision

--- RecoTracker/SpecialSeedGenerators/src/CosmicSeedCreator.cc
+++ RecoTracker/SpecialSeedGenerators/src/CosmicSeedCreator.cc
@@ -20,8 +20,7 @@
 
 template <class T> T sqr( T t) {return t*t;}
 
-const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
-							 TrajectorySeedCollection & seedCollection,
+const TrajectorySeed * CosmicSeedCreator::trajectorySeed(TrajectorySeedCollection & seedCollection,
 							 const SeedingHitSet & ordered,
 							 const TrackingRegion & region,
 							 const edm::EventSetup& es)
@@ -44,19 +43,11 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
   TransientTrackingRecHit::ConstRecHitPointer tth1 = hits[0];
   TransientTrackingRecHit::ConstRecHitPointer tth2 = hits[1];
 
-  //  LogDebug("CosmicSeedCreator") 
-  // <<"first hit at:" <<tth1->globalPosition()
-  // <<" on detector: "<<DetIdInfo::info(tth1->hit()->geographicalId())
-  // <<"\nsecond hit at: "<<tth2->globalPosition()
-  // <<" on detector: "<<DetIdInfo::info(tth2->hit()->geographicalId());
- 
   TransientTrackingRecHit::ConstRecHitPointer usedHit;
 
   //definition of position & momentum
   //++++++++++++++++++++++++++++++++++
-  //const GlobalPoint& hitPos = region.origin();
-  //GlobalVector initialMomentum(tth2->globalPosition() - tth1->globalPosition());//direction of the trajectory seed given by the segment between the 2 hits
-  GlobalVector initialMomentum(region.direction());//direction of the trajectory seed given by the momentum of the reco track (*-1 depending of the position of the hits seeded)
+  GlobalVector initialMomentum(region.direction());//direction of the trajectory seed given by the direction of the region
   //fix the momentum scale
   //initialMomentum = initialMomentum.basicVector.unitVector() * region.origin().direction().mag();
   //initialMomentum = region.origin().direction(); //alternative.
@@ -79,7 +70,7 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
   //++++++
   bool reverseAll = false;
   if ( fabs(tth1->globalPosition().perp()) < fabs(tth2->globalPosition().perp()) )  
-    //comparison of the position of the 2 hits by checking comparing their radius
+    //comparison of the position of the 2 hits by checking/comparing their radius
     {
       usedHit=tth1;
       reverseAll = true;
@@ -99,11 +90,12 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
   edm::OwnVector<TrackingRecHit> seedHits;
 
   if (reverseAll){
-    LogDebug("CosmicSeedCreator") 
-      <<"Reverse all applied";
+    LogDebug("CosmicSeedCreator") <<"Reverse all applied";
+
     seedHits.push_back(tth2->hit()->clone());
     seedHits.push_back(tth1->hit()->clone());
   }
+
   else {
     seedHits.push_back(tth1->hit()->clone());
     seedHits.push_back(tth2->hit()->clone());
@@ -115,6 +107,7 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
 
   PropagationDirection seedDirection = alongMomentum; //by default
 
+  /*
   if (reverseAll) initialMomentum *=-1;	
 
   if (bottomSeed){
@@ -124,7 +117,8 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
     //and change the direction of the seed
     seedDirection = oppositeToMomentum;
   }
-  
+  */
+
   for (int charge=-1;charge<=1;charge+=2){
     //fixme, what hit do you want to use ?
     
@@ -133,10 +127,9 @@ const TrajectorySeed * CosmicSeedCreator::trajectorySeed(
 				  CurvilinearTrajectoryError(AlgebraicSymMatrix(5,1))
 				  );
 
-    LogDebug("CosmicSeedCreator") 
-      <<"Position freeState: " << usedHit->globalPosition()
-      <<"\nCharge: "<< charge
-      <<"\nInitial momentum :" << initialMomentum ;
+    LogDebug("CosmicSeedCreator")<<"Position freeState: " << usedHit->globalPosition()
+				 <<"\nCharge: "<< charge
+				 <<"\nInitial momentum :" << initialMomentum ;
 
     TrajectoryStateOnSurface tsos(freeState, *usedHit->surface());
     
