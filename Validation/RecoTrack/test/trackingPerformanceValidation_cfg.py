fbf1844fae7ba6aec2f384f162a986d37e500be3
Boris Mangano <sha1-31f7197f6c74d1a321371e5d851e2132479bb924@cern.ch>
2010-03-23 16:14:32 +0000

    update to newer Geometry

--- Validation/RecoTrack/test/trackingPerformanceValidation_cfg.py
+++ Validation/RecoTrack/test/trackingPerformanceValidation_cfg.py
@@ -4,11 +4,11 @@ process = cms.Process("TkVal")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 ### standard includes
-process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.load("Configuration.EventContent.EventContent_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("SimGeneral.MixingModule.mixNoPU_cfi")
 
 ### conditions
