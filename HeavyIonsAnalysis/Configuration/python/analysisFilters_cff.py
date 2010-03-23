1b840a4fe7d0fea53f27d571e66e51e8d0090142
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-03-23 15:45:14 +0000

    example of filters for analysis skimming, starting with L1 bits and HLT paths

---
+++ HeavyIonsAnalysis/Configuration/python/analysisFilters_cff.py
@@ -0,0 +1,49 @@
+import FWCore.ParameterSet.Config as cms
+
+######################################################
+# A set of filters for heavy-ion analysis skimming:
+#   (1) L1 algo and technical bits
+#   (2) HLT paths
+#   (3) Event selection
+#   (4) Physics objects
+
+
+##### L1 selections #####
+from L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff import *
+import HLTrigger.HLTfilters.hltLevel1GTSeed_cfi as hltLevel1GTSeed
+
+# tech bit 0 - BPTX coincidence
+bptxAnd = hltLevel1GTSeed.clone(
+    L1TechTriggerSeeding = cms.bool(True),
+    L1SeedsLogicalExpression = cms.string('0')
+    )
+
+# tech bit 34 - BSC single-side - bits 36,37,38,39 - BSC beam halo veto
+bscOr = hltLevel1GTSeed.clone(
+    L1TechTriggerSeeding = cms.bool(True),
+    L1SeedsLogicalExpression = cms.string('(34) AND NOT (36 OR 37 OR 38 OR 39)')
+    )
+
+# algo bit 124 - BSC OR + BPTX OR
+bscOrBptxOr = hltLevel1GTSeed.clone()
+    L1TechTriggerSeeding = cms.bool(False)
+    L1SeedsLogicalExpression = cms.string('L1_BscMinBiasOR_BptxPlusORMinus')
+    )
+
+
+##### HLT selections #####
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+
+# jet trigger
+hltJets = hltHighLevel.clone(
+    HLTPaths = cms.vstring('HLT_HIJet35U'),
+    andOr = cms.bool(True)
+    )
+
+# photon trigger
+hltPhoton = hltHighLevel.clone(
+    HLTPaths = cms.vstring('HLT_HIPhoton15'),
+    andOr = cms.bool(True)
+    )
+
+
