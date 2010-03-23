d2358d3d61adcc3ccc05ca8bfc5b3073addb3404
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-03-23 00:21:51 +0000

    new HLT master table: /dev/CMSSW_3_5_5/HLT/V34

--- FastSimulation/Configuration/python/blockHLT_8E29_cff.py
+++ FastSimulation/Configuration/python/blockHLT_8E29_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_5_5/8E29/V13 (CMSSW_3_5_3_HLT5)
+# /dev/CMSSW_3_5_5/8E29/V14 (CMSSW_3_5_3_HLT7)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_5_5/8E29/V13')
+  tableName = cms.string('/dev/CMSSW_3_5_5/8E29/V14')
 )
 
 
