531c24ccf7b343e7807eb79b5d627ccbaf09fe18
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-03-23 11:48:54 +0000

    revert to the head

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,6 +1,6 @@
 
-autoCond = { 'mc'      : 'MC_3XY_V25::All',
-             'startup' : 'START3X_V25::All',
-             'craft08' : 'CRAFT08_R_V9::All',
-             'craft09' : 'CRAFT09_R_V10::All',
+autoCond = { 'mc'      : 'MC_36Y_V3::All',
+             'startup' : 'START36_V3::All',
+             'craft08' : 'CRAFT08_R_V12::All',
+             'craft09' : 'CRFT9_36R_V01::All',
              }
