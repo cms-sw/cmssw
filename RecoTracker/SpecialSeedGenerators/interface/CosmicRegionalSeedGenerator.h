7295d01b3b9a3a86049afe035f3c1c0536b95a67
tschudi <sha1-b2c7dad895ec9e4e12654a2b4a94508b7307887c@cern.ch>
2010-03-23 08:34:49 +0000

    Regional Reconstruction for cosmics during collision

--- RecoTracker/SpecialSeedGenerators/interface/CosmicRegionalSeedGenerator.h
+++ RecoTracker/SpecialSeedGenerators/interface/CosmicRegionalSeedGenerator.h
@@ -9,7 +9,7 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
 #include "DataFormats/Common/interface/Handle.h"
@@ -25,7 +25,7 @@
 #include "DQM/HLTEvF/interface/FourVectorHLT.h"
  
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
-#include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/Framework/interface/TriggerNames.h"
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 #include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
 #include "TrackingTools/GeomPropagators/interface/StateOnTrackerBound.h"
@@ -66,7 +66,8 @@ class CosmicRegionalSeedGenerator : public TrackingRegionProducer {
   std::string triggerSummaryLabel_;
   std::string thePropagatorName_;
   std::string seeding_;
-
+  edm::InputTag l2MuonsRecoTracksCollection_;
+  edm::InputTag staMuonsCollection_;
 };
 
 #endif
