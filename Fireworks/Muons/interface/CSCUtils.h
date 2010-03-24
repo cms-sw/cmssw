abbe504fa43924e9148b40ba7af7e55fded4a4b7
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2010-03-24 14:04:57 +0000

    add CSC chamber parameters for drawing of CSC segments

---
+++ Fireworks/Muons/interface/CSCUtils.h
@@ -0,0 +1,9 @@
+#ifndef FIREWORKS_MUONS_CSCUTILS_H
+#define FIREWORKS_MUONS_CSCUTILS_H
+
+namespace fireworks
+{
+  void fillCSCChamberParameters(int station, int ring, double& length, double& thickness);
+}
+
+#endif
