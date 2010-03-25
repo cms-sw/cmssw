e4b2bf738d744901bee04ec401fd47ba5af48193
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2010-03-25 20:44:07 +0000

    the removed parameters should be taken from DB

--- SimCalorimetry/EcalZeroSuppressionProducers/python/ecalPreshowerDigis_cfi.py
+++ SimCalorimetry/EcalZeroSuppressionProducers/python/ecalPreshowerDigis_cfi.py
@@ -1,12 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 simEcalPreshowerDigis = cms.EDProducer("ESZeroSuppressionProducer",
-    ESNoiseSigma = cms.untracked.double(6.0),
-    ESMIPkeV = cms.untracked.double(81.08),
-    ESGain = cms.untracked.int32(2),
-    ESMIPADC = cms.untracked.double(55.0),
     ESdigiCollection = cms.string(''),
-    ESBaseline = cms.untracked.int32(1000),
     digiProducer = cms.string('simEcalUnsuppressedDigis'),
     ESZSdigiCollection = cms.string('')
 )
