d51a6276aab94d3182972a050aab04173584ab84
Michele Selvaggi <sha1-b7e73fa76111215c535271b5dc26ec998950c6eb@cern.ch>
2010-03-25 13:29:59 +0000

    fed crate1 problem solved

--- CommonTools/TrackerMap/data/jqviewer.js
+++ CommonTools/TrackerMap/data/jqviewer.js
@@ -482,7 +482,7 @@ $('.place').click(function(e){
           cornerPoint.heigth=$cdetail.height+100; 
           cornerPoint.top=0;
           cornerPoint.left=688;
-          if(index>0&&index<25)
+          if(index>-1&&index<25)
           $cdetail.attr('src',tmapname+'crate'+(index+1)+'.xml').show();
           $tipButton1
             .css('left',cornerPoint.left-30)
