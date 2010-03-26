7093e80eb67ad6262c2c61eb3cb91e64bebd49a9
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-03-26 15:29:06 +0000

    fix helix propagation

--- TrackingTools/AnalyticalJacobians/test/ErrorProp_t.cpp
+++ TrackingTools/AnalyticalJacobians/test/ErrorProp_t.cpp
@@ -1,6 +1,10 @@
 #include "TrackingTools/GeomPropagators/interface/HelixForwardPlaneCrossing.h"
+#include "TrackingTools/GeomPropagators/interface/HelixArbitraryPlaneCrossing.h"
 #include "TrackingTools/AnalyticalJacobians/interface/AnalyticalCurvilinearJacobian.h"
+#include "TrackingTools/AnalyticalJacobians/interface/JacobianLocalToCurvilinear.h"
+#include "TrackingTools/AnalyticalJacobians/interface/JacobianCurvilinearToLocal.h"
 #include "TrackingTools/TrajectoryParametrization/interface/GlobalTrajectoryParameters.h"
+
 
 
 
@@ -14,7 +18,7 @@
 namespace {
 
   struct M5T : public  MagneticField {
-    M5T() :  m(0.1,0.1,5.){}
+    explicit M5T(double br) :  m(br,br,5.){}
     virtual GlobalVector inTesla (const GlobalPoint&) const {
       return m;
     }
@@ -30,49 +34,46 @@ void en(){}
 
 #include<iostream>
 
-int main() {
+int main(int argc, char** argv) {
+  double br=0.;
+  if (argc>1) br=0.1;
+  M5T const m(br);
 
-  M5T const m;
-
-  // GlobalVector xx(0.5,1.,1.);
-  // GlobalVector yy(-1.,0.5,1.);
-
-  Basic3DVector<float>  axis(0.5,1.,1);
-  
-  Surface::RotationType rot(axis,0.5*M_PI);
-  std::cout << rot << std::endl;
 
   Surface::PositionType pos( 0., 0., 0.);
 
-  Plane plane(pos,rot);
-
   GlobalVector mg(2.,3.,1.);
   GlobalTrajectoryParameters tpg(pos,mg,-1., &m);
-  std::cout << tpg.position() << " " << tpg.momentum() << std::endl;
-
   double curv =   tpg.transverseCurvature();
+  std::cout << tpg.position() << " " << tpg.momentum() << std::endl;
 
   AlgebraicMatrix55 fullJacobian = AlgebraicMatrixID();
   AlgebraicMatrix55 deltaJacobian = AlgebraicMatrixID();
-  GlobalTrajectoryParameters tpg0(pos,mg,1., &m);
+  GlobalTrajectoryParameters tpg0(tpg);
 
   HelixForwardPlaneCrossing::PositionType zero(0.,0.,0.); 
   std::cout << std::endl;
+
+  double totalStep(0.);
+  double singleStep(1.);
   for (int i=0; i<10;++i) {
+    double s = singleStep;
+    totalStep += singleStep;
 
     GlobalVector h = tpg.magneticFieldInInverseGeV(tpg.position());
     Surface::RotationType rot(Basic3DVector<float>(h),0);
     Plane plane(tpg.position(),rot);
-    HelixForwardPlaneCrossing::DirectionType dir (plane.toLocal(tpg.momentum()));
-    curv =  -2.99792458e-3 * h.mag()/dir.perp()*tpg.charge();
-    std::cout << curv << " " <<  dir.mag() << std::endl;
-    HelixForwardPlaneCrossing prop(zero, dir, curv);
-    double s = 0.1;
+    HelixForwardPlaneCrossing::DirectionType p (plane.toLocal(tpg.momentum()));
+    curv =  -2.99792458e-3 * h.mag()/p.perp()*tpg.charge();
+    std::cout << curv << " " <<  p.mag() << std::endl;
+    HelixForwardPlaneCrossing prop(zero, p, curv);
     LocalPoint x(prop.position(s));
-    LocalVector p(prop.direction(s));
-    std::cout <<  p.mag() << std::endl;
+    LocalVector dir(prop.direction(s));
+    std::cout <<  dir.mag() << std::endl;
 
-    GlobalTrajectoryParameters tpg2( plane.toGlobal(x), (dir.mag()/p.mag())*plane.toGlobal(p), tpg.charge(), &m);
+    GlobalTrajectoryParameters tpg2( plane.toGlobal(x), 
+				     (p.mag()/dir.mag())*plane.toGlobal(dir), 
+				     tpg.charge(), &m);
 
     std::cout << tpg2.position() << " " << tpg2.momentum() << std::endl;
     AnalyticalCurvilinearJacobian full;
@@ -87,6 +88,7 @@ int main() {
     fullJacobian *=full.jacobian();
     deltaJacobian *=delta.jacobian();
   }
+  std::cout << "---------------------------" << std::endl;
   std::cout << std::endl;
   std::cout << std::endl;
   std::cout <<  fullJacobian << std::endl;
@@ -95,11 +97,88 @@ int main() {
   std::cout << std::endl;
   AnalyticalCurvilinearJacobian full;
   GlobalVector h = tpg0.magneticFieldInInverseGeV(tpg0.position());
-  full.computeFullJacobian(tpg0,tpg.position(),tpg.momentum(),h,1.);
+  full.computeFullJacobian(tpg0,tpg.position(),tpg.momentum(),h,totalStep);
   std::cout <<  full.jacobian() << std::endl;
   std::cout << std::endl;
 
+  std::cout << "---------------------------" << std::endl;
+
+  AlgebraicMatrix55 div;
+  for ( unsigned int i=0; i<5; ++i ) {
+    for ( unsigned int j=0; j<5; ++j ) {
+//       div(i,j) = fabs(full.jacobian()(i,j))>1.e-20 ? fullJacobian(i,j)/full.jacobian()(i,j)-1 : 0;
+      div(i,j) = fullJacobian(i,j)-full.jacobian()(i,j);
+    }
+  }
+  std::cout << "Full relative" << std::endl << div << std::endl;
+  for ( unsigned int i=0; i<5; ++i ) {
+    for ( unsigned int j=0; j<5; ++j ) {
+//       div(i,j) = fabs(full.jacobian()(i,j))>1.e-20 ? deltaJacobian(i,j)/full.jacobian()(i,j)-1 : 0;
+      div(i,j) = deltaJacobian(i,j)-full.jacobian()(i,j);
+    }
+  }
+  std::cout << "Delta relative" << std::endl << div << std::endl;
 
 
+
+
+  //
+  // plane at end of propagation (equivalent to Curvilinear definition up to rotation in the plane)
+  //
+  Basic3DVector<float>  newaxis(tpg.momentum().unit());
+  Surface::RotationType newrot(axis,0.);
+  Surface::PositionType newpos(tpg.position());
+  Plane newplane(newpos,newrot);
+  //
+  // total jacobian (local to local)
+  //
+  JacobianLocalToCurvilinear jlc(plane,tpl,m);
+  LocalTrajectoryParameters newlpg(newplane.toLocal(tpg.position()),
+				   newplane.toLocal(tpg.momentum()),1.);
+  JacobianCurvilinearToLocal jcl(newplane,newlpg,m);
+//   AlgebraicMatrix55 jacobianL2L = jcl.jacobian()*full.jacobian()*jlc.jacobian();
+  AlgebraicMatrix55 jacobianL2L = jcl.jacobian()*deltaJacobian*jlc.jacobian();
+  //
+  // redo propagation to target plane (should give identical position ...)
+  //
+  HelixArbitraryPlaneCrossing aprop(HelixForwardPlaneCrossing::PositionType(tpg0.position()), 
+				    HelixForwardPlaneCrossing::DirectionType(tpg0.momentum()), curv);
+  std::pair<bool,double> as = aprop.pathLength(newplane);
+  std::cout << "as = " << as.second << std::endl;
+  GlobalPoint ax(aprop.position(as.second));
+  GlobalVector ap(aprop.direction(as.second)*tpg0.momentum().perp());
+  //
+  // change initial q/p and redo propagation
+  //
+  double qpmod = 1./tpg0.momentum().mag()+0.01;
+  GlobalTrajectoryParameters tpg0mod(tpg0.position(),tpg0.momentum().unit()/fabs(qpmod),
+				     qpmod>0?1.:-1.,&m);
+  HelixArbitraryPlaneCrossing aprop2(HelixForwardPlaneCrossing::PositionType(tpg0mod.position()), 
+				     HelixForwardPlaneCrossing::DirectionType(tpg0mod.momentum()),
+				     tpg0mod.transverseCurvature());
+  std::pair<bool,double> as2 = aprop2.pathLength(newplane);
+  std::cout << "as2 = " << as2.second << std::endl;
+  GlobalPoint ax2(aprop2.position(as2.second));
+  GlobalVector ap2(aprop2.direction(as2.second)*tpg0mod.momentum().perp());
+  //
+  // local coordinates after 2nd propagation
+  LocalVector newDx = newplane.toLocal(ax2-ax);
+  LocalVector newDp = newplane.toLocal(ap2/ap2.z()-ap/ap.z());
+  std::cout << newDx << std::endl;
+  std::cout << newDp << std::endl;
+  //
+  // prediction of variations with jacobian
+  //
+  AlgebraicVector5 dp;
+  dp(0) = qpmod-1./tpg0.momentum().mag();
+  dp(1) = dp(2) = dp(3) = dp(4) = 0;
+  AlgebraicVector5 dpPred =  jacobianL2L*dp;
+  std::cout << dpPred << std::endl;
+  //
+  // comparison with difference in parameters 
+  //
+  std::cout << newDx.x()-dpPred[3] << " " << newDx.y()-dpPred[4] << std::endl;
+  std::cout << newDp.x()-dpPred[1] << " " << newDp.y()-dpPred[2] << std::endl;
+  std::cout << (1./ap2.mag()-1./ap.mag())-dpPred[0] << std::endl;
   return 0;
 }
