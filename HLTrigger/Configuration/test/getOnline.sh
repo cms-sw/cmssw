296e12d1bb17b6a3133dcbaeaedf396025a9a137
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-03-26 05:02:01 +0000

    switch to week13 for 7 TeV collisions

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-HLT='/online/collisions/2010/week12/HLT'
+HLT='/online/collisions/2010/week13/HLT'
+L1T='L1GtTriggerMenu_L1Menu_Commissioning2010_v0_mc'
 
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-./getHLT.py --process TEST --full --offline --mc   $HLT TEST
+./getHLT.py --process TEST --full --offline --mc   $HLT --l1 $L1T TEST
 mv OnLine_HLT_TEST.py offline_mc.py
-./getHLT.py --process TEST --full --offline --data $HLT TEST
+./getHLT.py --process TEST --full --offline --data $HLT --l1 $L1T TEST
 mv OnData_HLT_TEST.py offline_data.py
-./getHLT.py --process TEST --full --online  --data $HLT TEST
+./getHLT.py --process TEST --full --online  --data $HLT --l1 $L1T TEST
 mv OnData_HLT_TEST.py online_data.py
 
 {
