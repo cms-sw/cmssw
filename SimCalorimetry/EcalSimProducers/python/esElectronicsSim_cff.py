c3d7a9d9b4b792564aa481518317c0819dcdd78f
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2010-03-27 20:36:57 +0000

    Restore APD-handling & more efficient hit response

--- SimCalorimetry/EcalSimProducers/python/esElectronicsSim_cff.py
+++ SimCalorimetry/EcalSimProducers/python/esElectronicsSim_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 es_electronics_sim = cms.PSet(
-    doESNoise = cms.bool(True),
+    doESNoise  = cms.bool(True),
     numESdetId = cms.int32(137216),
-    doFast = cms.bool(False)
+    doFast     = cms.bool(False)
 )
 
