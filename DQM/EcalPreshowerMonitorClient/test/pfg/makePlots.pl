094048752b4657c921b47fd6698424acc0c074fc
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2010-03-27 21:34:14 +0000

    codes/scripts for ECAL PFG

---
+++ DQM/EcalPreshowerMonitorClient/test/pfg/makePlots.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use strict;
+
+system("cp $ARGV[0]/crab_0_*/res/DQM_V0001_EcalPreshower_$ARGV[0].root .");
+system(". /afs/cern.ch/cms/sw/cmsset_default.sh; eval `scramv1 runtime -sh`; root -l -b -q xPlotES.C\\($ARGV[0]\\)");
+system("mv *$ARGV[0]*.gif test");
+
+# make HTML file
+system("cat > test/index.html<<EOF
+
+<HTML>
+<Center>
+<h1><FONT color=\"Blue\"> ECAL Preshower Analysis (Run $ARGV[0]) </FONT></h1>
+</Center>
+
+<h2><FONT color=\"Red\"> ES Occupancy with selected hits </FONT></h2>
+
+<img width=\"400\" src=\"ES_Occupancy_SelHits_1D_ESpF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_SelHits_1D_ESpR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Occupancy_SelHits_1D_ESmF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_SelHits_1D_ESmR_$ARGV[0].gif\">
+<br>
+
+<img width=\"400\" src=\"ES_Occupancy_SelHits_2D_ESpF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_SelHits_2D_ESpR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Occupancy_SelHits_2D_ESmF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_SelHits_2D_ESmR_$ARGV[0].gif\">
+<br>
+
+<h2><FONT color=\"Red\"> ES Occupancy with all readout hits </FONT></h2>
+
+<img width=\"400\" src=\"ES_Occupancy_1D_ESpF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_1D_ESpR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Occupancy_1D_ESmF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_1D_ESmR_$ARGV[0].gif\">
+<br>
+
+<img width=\"400\" src=\"ES_Occupancy_2D_ESpF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_2D_ESpR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Occupancy_2D_ESmF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Occupancy_2D_ESmR_$ARGV[0].gif\">
+<br>
+
+<h2><FONT color=\"Red\"> ES Timing </FONT></h2>
+
+<img width=\"400\" src=\"ES_Timing_ESpF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Timing_ESpR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Timing_ESmF_$ARGV[0].gif\">
+<img width=\"400\" src=\"ES_Timing_ESmR_$ARGV[0].gif\">
+<br>
+<img width=\"400\" src=\"ES_Timing_2D_$ARGV[0].gif\">
+
+</HTML>
+
+EOF
+");
