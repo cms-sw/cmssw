56599fb711d4307b2b40cb11c1196a7986ffaaf0
Alexander Spiridonov <sha1-02781a4f55a8da18358401e1fba71af25406897b@cern.ch>
2010-03-29 13:40:24 +0000

    Add comment

--- Alignment/CommonAlignmentProducer/python/GlobalTrackerMuonAlignment_cfi.py
+++ Alignment/CommonAlignmentProducer/python/GlobalTrackerMuonAlignment_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 GlobalTrackerMuonAlignment = cms.EDAnalyzer('GlobalTrackerMuonAlignment',
-                                            cosmics = cms.bool(False),
+# can be redefined in GlobalTrackerMuonAlignment_test_cfg.py
                                             isolated = cms.bool(False),
+                                            cosmics = cms.bool(False),
                                             rootOutFile = cms.untracked.string('outfile.root'),
                                             txtOutFile = cms.untracked.string('outglobal.txt'),
                                             writeDB = cms.untracked.bool(False),
