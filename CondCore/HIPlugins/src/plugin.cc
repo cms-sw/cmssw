152ebb6d2bb07aa97c6020e2055e9d989cefa582
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-03-30 18:41:21 +0000

    Don't use deprecated framework macros

--- CondCore/HIPlugins/src/plugin.cc
+++ CondCore/HIPlugins/src/plugin.cc
@@ -1,5 +1,4 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/DataRecord/interface/HeavyIonRcd.h"
 #include "CondFormats/HIObjects/interface/CentralityTable.h"
-DEFINE_SEAL_MODULE();
 REGISTER_PLUGIN(HeavyIonRcd,CentralityTable);
--- RecoJets/JetPlusTracks/plugins/SealModule.cc
+++ RecoJets/JetPlusTracks/plugins/SealModule.cc
@@ -3,5 +3,5 @@
 #include "RecoJets/JetPlusTracks/plugins/JetPlusTrackProducer.h"
 #include "RecoJets/JetPlusTracks/plugins/JetPlusTrackProducerAA.h"
 
-DEFINE_ANOTHER_FWK_MODULE(JetPlusTrackProducer);
-DEFINE_ANOTHER_FWK_MODULE(JetPlusTrackProducerAA);
+DEFINE_FWK_MODULE(JetPlusTrackProducer);
+DEFINE_FWK_MODULE(JetPlusTrackProducerAA);
