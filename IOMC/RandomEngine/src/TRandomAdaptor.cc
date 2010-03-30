1a1979ae469b4de5e7457521674389d1c3fd2b56
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2010-03-30 20:35:07 +0000

    Bug fix so that the TRandom3 random engine states are properly stored in an executable that has been built in true 64 bit mode

--- IOMC/RandomEngine/src/TRandomAdaptor.cc
+++ IOMC/RandomEngine/src/TRandomAdaptor.cc
@@ -1,10 +1,13 @@
-#include "CLHEP/Random/defs.h"
-#include "CLHEP/Random/Random.h"
-#include "CLHEP/Random/engineIDulong.h"
 #include "IOMC/RandomEngine/src/TRandomAdaptor.h"
 #include "FWCore/Utilities/interface/EDMException.h"
 
+#include "CLHEP/Random/defs.h"
+#include "CLHEP/Random/Random.h"
+#include "CLHEP/Random/engineIDulong.h"
+
 #include "TBufferFile.h"
+
+#include <boost/cstdint.hpp>
 
 #include <string>
 #include <cstddef>
@@ -28,16 +31,29 @@ std::ostream & TRandomAdaptor::put (std::ostream& os) const {
 }
 
 std::vector<unsigned long> TRandomAdaptor::put () const {
-  int32_t itemSize = sizeof(unsigned long);
   std::vector<unsigned long> v;
   v.push_back (CLHEP::engineIDulong<TRandomAdaptor>());
-  TBufferFile buffer(TBuffer::kWrite,1024*itemSize);
+
+  int32_t itemSize = sizeof(uint32_t);
+  TBufferFile buffer(TBuffer::kWrite, 2048 * itemSize);
   trand_->Streamer(buffer);
   buffer.SetReadMode();
   char* bufferPtr = buffer.Buffer();
-  int32_t numItems = (buffer.Length()+itemSize-1)/itemSize;
-  for( int i=0; i<(int)numItems; ++i)  {
-    v.push_back(*(unsigned long*)(bufferPtr+i*itemSize));
+  int32_t numItems = (buffer.Length() + itemSize - 1) / itemSize;
+  for( int i = 0; i < numItems; ++i) {
+
+    // Here we do some ugly manipulations to the data to match the format
+    // of the output of the CLHEP put function (the whole point of this adaptor
+    // is to make TRandom3 work through the CLHEP interface as if it were a
+    // a CLHEP engine ...).  In CLHEP, the vector returned by put contains
+    // unsigned long's, but these always contain only 32 bits of information.
+    // In the case of a 64 bit build the top 32 bits is only padding (all 0's).
+
+    // Get the next 32 bits of data from the buffer
+    uint32_t value32 = *reinterpret_cast<uint32_t *>(bufferPtr + i * itemSize);
+
+    // Push it into the vector in an unsigned long which may be 32 or 64 bits
+    v.push_back(static_cast<unsigned long>(value32));
   }
   return v;
 }
@@ -56,13 +72,22 @@ bool TRandomAdaptor::get (std::vector<unsigned long> const& v) {
   if(v.empty())  return false;
   if(v[0] != CLHEP::engineIDulong<TRandomAdaptor>()) return false;
   int32_t numItems = v.size()-1;
-  int32_t itemSize = sizeof(unsigned long);
-  TBufferFile buffer(TBuffer::kRead,numItems*itemSize+1024);
+
+  int32_t itemSize = sizeof(uint32_t);
+  TBufferFile buffer(TBuffer::kRead, numItems * itemSize + 1024);
   char* bufferPtr = buffer.Buffer();
-  for(int i=0; i<(int)numItems; ++i) {
-    *(unsigned long*)(bufferPtr+i*itemSize) = v[i+1];
+  for(int32_t i = 0; i < numItems; ++i) {
+
+    *reinterpret_cast<uint32_t *>(bufferPtr + i * itemSize) = static_cast<uint32_t>(v[i+1] & 0xffffffff);
   }
+
+  // Note that this will fail if the TRandom3 version (specifically the TStreamerInfo)
+  // has changed between the time the state was saved and the time the following call
+  // is made.  Because we are manually calling the Streamer function, the original
+  // TStreamerInfo is not saved anywhere. Normally ROOT saves the TStreamerInfo
+  // automatically.
   trand_->Streamer(buffer);
+
   return true;
 }
 
