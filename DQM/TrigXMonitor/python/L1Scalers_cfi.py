cda7e0017d0292269e08bbdfb632f6614b1aafa5
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2010-03-31 22:19:35 +0000

    add bx synch plots

--- DQM/TrigXMonitor/python/L1Scalers_cfi.py
+++ DQM/TrigXMonitor/python/L1Scalers_cfi.py
@@ -1,14 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: L1Scalers_cfi.py,v 1.6 2008/09/17 13:49:42 lorenzo Exp $
+# $Id: L1Scalers_cfi.py,v 1.7 2010/02/16 17:04:29 wmtan Exp $
 
 l1s = cms.EDAnalyzer("L1Scalers",
-                   l1GtData = cms.InputTag("l1GtUnpack","","HLT"),
+                   l1GtData = cms.InputTag("l1GtUnpack","","DQM"),
                    dqmFolder = cms.untracked.string("L1T/L1Scalers_EvF"),
                    verbose = cms.untracked.bool(False),
                    firstFED = cms.untracked.uint32(0),
                    lastFED = cms.untracked.uint32(39),
                    fedRawData = cms.InputTag("source","", ""),
                    maskedChannels = cms.untracked.vint32(),
-                   HFRecHitCollection = cms.InputTag("hfreco", "", "")
+                   HFRecHitCollection = cms.InputTag("hfreco", "", ""),
+		   denomIsTech = cms.untracked.bool(True),
+		   denomBit = cms.untracked.uint32(40),
+		   algoMonitorBits = cms.untracked.vuint32(8,9,15,46,54,55,100,124),
+		   techMonitorBits = cms.untracked.vuint32(0,9)
                    )
