a98acb4048e3c17d8477b8e6581f23470650d711
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2010-04-01 01:09:51 +0000

    *** empty log message ***

--- DQM/L1TMonitor/python/L1TFED_cfi.py
+++ DQM/L1TMonitor/python/L1TFED_cfi.py
@@ -6,7 +6,7 @@ l1tfed = cms.EDAnalyzer("L1TFED",
     DQMStore = cms.untracked.bool(True),
     L1FEDS = cms.vint32(745,760,780,812,813),
     rawTag = cms.InputTag( "source" ),
-    FEDDirName = cms.untracked.string("FEDIntegrity")
+    FEDDirName = cms.untracked.string("L1T/FEDIntegrity")
 )
 
 
