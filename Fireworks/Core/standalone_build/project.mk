55c72ca9d8303314b6b3e904e35250d043e5192d
Matevz Tadel <sha1-3c3651c44304837c4421d46b5b430a5900d92bbd@cern.ch>
2010-04-01 19:45:03 +0000

    Update external includes for linux for 3.6.x externals.

--- Fireworks/Core/standalone_build/project.mk
+++ Fireworks/Core/standalone_build/project.mk
@@ -3,10 +3,10 @@ ifeq ($(shell uname), Linux)
    RootLibs  := Cint Core  Net Tree Hist Matrix MathCore MathMore Reflex EG Eve FTGL Ged Geom GeomPainter Gpad Graf Graf3d Gui Physics RGL Rint TreePlayer RIO GuiHtml
    ProjectLibs  := $(BoostLibs) $(RootLibs) CLHEP-2.0.4.2 gsl gslcblas HepMC pcre sigc-2.0 nsl crypt dl uuid
 
-   ProjectIncludes := external/inc/boost/1.40.0-cms3/include
+   ProjectIncludes := external/inc/boost/current/include
    ProjectIncludes += external/inc/clhep/2.0.4.2-cms/include
-   ProjectIncludes += external/inc/gsl/1.10/include
-   ProjectIncludes += external/inc/hepmc/2.03.06/include
+   ProjectIncludes += external/inc/gsl/current/include
+   ProjectIncludes += external/inc/hepmc/current/include
    ProjectIncludes += external/inc/pcre/4.4/include
    ProjectIncludes += external/inc/sigcpp/2.2.3/include/sigc++-2.0
    ProjectIncludes += external/inc/uuid/1.38/include
