2b4eb60a89a8e154d65563d3ad1a5b62bfbe6818
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-01 22:28:21 +0000

    fix unit test to actually work!

--- IOPool/TFileAdaptor/test/tfileTest.cpp
+++ IOPool/TFileAdaptor/test/tfileTest.cpp
@@ -11,111 +11,56 @@
 #include "FWCore/PluginManager/interface/standard.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
-int main (int argc, char *argv[]) 
-{
+int main(int argc, char* argv[]) {
   /* TestMain tfileTest zip-member:file:bha.zip#bha
      TestMain tfileTest rfio:suncmsc.cern.ch:/data/valid/test/vincenzo/testPool/evData/EVD0_EventData.56709894d26a11d78dd20040f45cca94.1.h300eemm.TestSignalHits
      TestMain tfileTest zip-member:rfio:suncmsc.cern.ch:/data/valid/test/vincenzo/testZip/test1.zip#file.5 */
 
-  edmplugin::PluginManager::configure (edmplugin::standard::config());
+  char const* protocols[] = {"^file:", "^http:", "^ftp:", "^web:", "^gsiftp:", "^sfn:", "^rfio:", "^dcache:", "^dcap:", "^gsidcap:"};
+
+  char const* tStorageFactoryFileFunc = "TStorageFactoryFile(char const*, Option_t*, char const*, Int_t)"; 
+
+  edmplugin::PluginManager::configure(edmplugin::standard::config());
 
   gEnv->SetValue("Root.Stacktrace", "0");
   // set our own root plugin
-  gROOT->GetPluginManager()->AddHandler("TFile", "^file:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^http:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^ftp:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^web:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-
-  gROOT->GetPluginManager()->AddHandler("TFile", "^gsiftp:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-
-  gROOT->GetPluginManager()->AddHandler("TFile", "^sfn:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-
-  gROOT->GetPluginManager()->AddHandler("TFile", "^rfio:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^dcache:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^dcap:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  gROOT->GetPluginManager()->AddHandler("TFile", "^gsidcap:",     
-					"TStorageFactoryFile", "TFileAdaptorModule",     
-					"TStorageFactoryFile(const char*,Option_t*,const char*,Int_t)");   
-  
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^file:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^http:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^ftp:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^web:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^gsiftp:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^sfn:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^rfio:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^dcache:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");  
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^dcap:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
-  gROOT->GetPluginManager()->AddHandler("TSystem", "^gsidcap:",     
-					"TStorageFactorySystem", "TFileAdaptorModule",     
-					"TStorageFactorySystem()");
+  typedef char const* Cp;
+  Cp* begin = protocols;
+  Cp* end = &protocols[sizeof(protocols)/sizeof(protocols[0])];
+  for (Cp* i = begin; i != end; ++i) {
+    gROOT->GetPluginManager()->AddHandler("TFile", *i, "TStorageFactoryFile", "pluginIOPoolTFileAdaptor", tStorageFactoryFileFunc);   
+    gROOT->GetPluginManager()->AddHandler("TSystem", *i, "TStorageFactorySystem", "pluginIOPoolTFileAdaptor", "TStorageFactorySystem()");
+  }
 
   gROOT->GetPluginManager()->Print(); // use option="a" to see ctors 
  
-  std::string fname("file:file:bha");
+  std::string fname("file:bha.root");
 
-  if (argc>1) fname = argv[1];
+  if (argc > 1) fname = argv[1];
 
-  TFile *f = 0;
-  try
   {
+    std::auto_ptr<TFile> g(TFile::Open(fname.c_str(), "recreate", "", 1));
+    g->Close();
+  }
+  try {
     Bool_t result = gSystem->AccessPathName(fname.c_str(), kFileExists);
     std::cout << "file " << fname << (result ? " does not exist\n" : " exists\n");
-    if (const char *err = gSystem->GetErrorStr ())
+    char const* err = gSystem->GetErrorStr();
+    if (err != 0 && *err != '\0')
       std::cout << "error was " << err << "\n";
 
-    f = TFile::Open(fname.c_str());
-    std::cout << "file size " << f->GetSize() << std::endl;
-    f->ls();
+    if (!result) {
+      std::auto_ptr<TFile> f(TFile::Open(fname.c_str()));
+      std::cout << "file size " << f->GetSize() << std::endl;
+      f->ls();
+    }
   }
-  catch (cms::Exception &e)
-  {
+  catch (cms::Exception& e) {
     std::cout << "*ERROR*: " << e.what() << std::endl;
   }
-  catch (...)
-  {
+  catch (...) {
     std::cout << "*ERROR*\n";
   }
 
-  delete f;
   return 0;
 }
