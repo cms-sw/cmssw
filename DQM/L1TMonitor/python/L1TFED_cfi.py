bc05c4c6b3178c626a05292e0a7c683a4387905c
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2010-04-06 01:17:00 +0000

    add stable r/o config flag

--- DQM/L1TMonitor/python/L1TFED_cfi.py
+++ DQM/L1TMonitor/python/L1TFED_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tfed = cms.EDAnalyzer("L1TFED",
-    disableROOToutput = cms.untracked.bool(False),
-    verbose = cms.untracked.bool(True),
+    disableROOToutput = cms.untracked.bool(True),
+    verbose = cms.untracked.bool(False),
     DQMStore = cms.untracked.bool(True),
     L1FEDS = cms.vint32(745,760,780,812,813),
     rawTag = cms.InputTag( "source" ),
