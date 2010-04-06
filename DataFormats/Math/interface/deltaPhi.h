0cbaf760c029afadb116a801d0293fdd6a98304c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-04-06 13:16:00 +0000

    fix delphi in case of PHI

--- DataFormats/Math/interface/deltaPhi.h
+++ DataFormats/Math/interface/deltaPhi.h
@@ -26,7 +26,7 @@ namespace reco {
   
 
   inline float deltaPhi(float phi1, float phi2) { 
-    double result = phi1 - phi2;
+    float result = phi1 - phi2;
     while (result > float(M_PI)) result -= float(2*M_PI);
     while (result <= -float(M_PI)) result += float(2*M_PI);
     return result;
