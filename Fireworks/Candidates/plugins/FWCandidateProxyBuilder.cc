17ecbac8c4fb21a0d418e72d0a093f1bfdb89a9c
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-04-07 14:16:40 +0000

    Collect draw functions in one file

--- Fireworks/Candidates/plugins/FWCandidateProxyBuilder.cc
+++ Fireworks/Candidates/plugins/FWCandidateProxyBuilder.cc
@@ -8,21 +8,18 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Fri Dec  5 09:56:09 EST 2008
-// $Id: FWCandidateProxyBuilder.cc,v 1.4 2010/01/25 20:55:05 amraktad Exp $
+// $Id: FWCandidateProxyBuilder.cc,v 1.1 2010/04/07 08:05:46 yana Exp $
 //
 
 #include "TEveTrack.h"
-#include "TEveVSDStructs.h"
-#include "Fireworks/Core/interface/Context.h"
 
 // user include files
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
+#include "Fireworks/Core/interface/Context.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Candidates/interface/CandidateUtils.h"
 
 #include "DataFormats/Candidate/interface/Candidate.h"
-
-class Track;
-class TEveTrackPropagator;
 
 class FWCandidateProxyBuilder : public FWSimpleProxyBuilderTemplate<reco::Candidate>  {
       
@@ -36,20 +33,15 @@ private:
    FWCandidateProxyBuilder(const FWCandidateProxyBuilder&); // stop default
    const FWCandidateProxyBuilder& operator=(const FWCandidateProxyBuilder&); // stop default
 
-   void build(const reco::Candidate& iData, unsigned int iIndex,TEveElement& oItemHolder) const;
+   void build(const reco::Candidate& iData, unsigned int iIndex, TEveElement& oItemHolder) const;
 };
 
 
 void 
-FWCandidateProxyBuilder::build(const reco::Candidate& iData, unsigned int iIndex,TEveElement& oItemHolder) const
+FWCandidateProxyBuilder::build(const reco::Candidate& iData, unsigned int iIndex, TEveElement& oItemHolder) const
 {
-   TEveRecTrack t;
-   t.fBeta = 1.;
-   t.fP = TEveVector( iData.px(), iData.py(), iData.pz() );
-   t.fV = TEveVector( iData.vertex().x(), iData.vertex().y(), iData.vertex().z() );
-   t.fSign = iData.charge();
-   TEveTrack* trk = new TEveTrack(&t, context().getTrackPropagator());
-   trk->SetMainColor( item()->defaultDisplayProperties().color());
+   TEveTrack* trk = fireworks::prepareCandidate( iData, context().getTrackPropagator(), item()->defaultDisplayProperties().color() ); 
+   
    trk->MakeTrack();
    oItemHolder.AddElement( trk );
 }
@@ -57,4 +49,4 @@ FWCandidateProxyBuilder::build(const reco::Candidate& iData, unsigned int iIndex
 //
 // static member functions
 //
-REGISTER_FWPROXYBUILDER(FWCandidateProxyBuilder,reco::Candidate,"Candidates", FWViewType::k3DBit | FWViewType::kRhoPhiBit  | FWViewType::kRhoZBit);
+REGISTER_FWPROXYBUILDER(FWCandidateProxyBuilder, reco::Candidate, "Candidates", FWViewType::k3DBit | FWViewType::kRhoPhiBit  | FWViewType::kRhoZBit);
