d3405c000fa849c066d4890c5f34722f165012cf
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-04-08 13:36:41 +0000

    Make an energy spectrum as defined in a file

--- IOMC/ParticleGuns/src/SealModule.cc
+++ IOMC/ParticleGuns/src/SealModule.cc
@@ -13,6 +13,7 @@
 //#include "IOMC/ParticleGuns/interface/ExpoRandomPtGunSource.h"
 //#include "IOMC/ParticleGuns/interface/MultiParticleInConeGunSource.h"
 
+#include "IOMC/ParticleGuns/interface/FileRandomKEThetaGunProducer.h"
 #include "IOMC/ParticleGuns/interface/FlatRandomEGunProducer.h"
 #include "IOMC/ParticleGuns/interface/FlatRandomPtGunProducer.h"
 #include "IOMC/ParticleGuns/interface/FlatRandomEThetaGunProducer.h"
@@ -42,6 +43,8 @@ using edm::MultiParticleInConeGunSource;
 DEFINE_FWK_INPUT_SOURCE(MultiParticleInConeGunSource);
 */
 
+using edm::FileRandomKEThetaGunProducer;
+DEFINE_FWK_MODULE(FileRandomKEThetaGunProducer);
 using edm::FlatRandomEGunProducer;
 DEFINE_FWK_MODULE(FlatRandomEGunProducer);
 using edm::FlatRandomPtGunProducer;
