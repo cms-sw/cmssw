fa0f15209ca09d012e58faa0ac502e9ed4834c5a
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-04-08 05:26:08 +0000

    Add dqmEnv into RECO sequences, different instances of dqmEnv for L1 and HLT sequences

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
@@ -1,9 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
+import DQMServices.Components.DQMEnvironment_cfi
+dqmEnvL1T = DQMServices.Components.DQMEnvironment_cfi.dqmEnv.clone()
+dqmEnvL1T.subSystemFolder = 'L1T'
+
+import DQMServices.Components.DQMEnvironment_cfi
+dqmEnvHLT = DQMServices.Components.DQMEnvironment_cfi.dqmEnv.clone()
+dqmEnvHLT.subSystemFolder = 'HLT'
+
+# L1 -----------------------------------
 from DQM.L1TMonitor.L1TGT_readout_cff import *
 from DQM.L1TMonitor.L1TGCT_readout_cff import *
 from DQM.L1TMonitor.L1TFED_cfi import *
-l1tmonitor = cms.Sequence(l1tgt*l1tgmt*l1trpctf*l1tcsctf*l1tdttf*l1trct*l1tgct*l1tfed)
+
 l1tgt.gtSource = 'gtDigis::'
 l1tgmt.gmtSource = 'gtDigis::'
 l1tdttf.dttpgSource = 'dttfDigis::'
@@ -19,9 +28,13 @@ l1tgct.gctIsoEmSource = 'gctDigis:isoEm:'
 l1tgct.gctNonIsoEmSource = 'gctDigis:nonIsoEm:'
 l1trct.rctSource = 'gctDigis::'
 
+l1tmonitor = cms.Sequence(l1tgt*l1tgmt*l1trpctf*l1tcsctf*l1tdttf*l1trct*l1tgct*l1tfed*dqmEnvL1T)
+
+# L1 Emulator -----------------------------------
 from DQM.L1TMonitor.L1TDEMON_cfi import *
 l1temumonitor = cms.Sequence(l1demon) 
 
+# HLT Online -----------------------------------
 # AlCa
 from DQM.HLTEvF.HLTAlCaMonPi0_cfi import *
 from DQM.HLTEvF.HLTAlCaMonEcalPhiSym_cfi import *
@@ -48,6 +61,7 @@ from DQM.HLTEvF.HLTMonTau_cfi import *
 #onlineHLTSource = cms.Sequence(EcalPi0Mon*EcalPhiSymMon*hltMonEleBits*hltMonMuBits*hltMonTauReco*hltMonBTagIPSource*hltMonBTagMuSource)
 onlineHLTSource = cms.Sequence(EcalPi0Mon*EcalPhiSymMon*hltMonMuBits*hltMonTauReco)
 
+# HLT Offline -----------------------------------
 
 # FourVector
 from DQMOffline.Trigger.FourVectorHLTOffline_cfi import *
@@ -65,13 +79,11 @@ from DQMOffline.Trigger.JetMETHLTOfflineSource_cfi import *
 # TnP
 from DQMOffline.Trigger.TnPEfficiency_cff import *
 
-
-
 #offlineHLTSource = cms.Sequence(hltResults*egHLTOffDQMSource*topElectronHLTOffDQMSource*muonFullOfflineDQM*quadJetAna*HLTTauDQMOffline*jetMETHLTOfflineSource*TnPEfficiency)
 
 # Remove topElectronHLTOffDQMSource
 # remove quadJetAna
-offlineHLTSource = cms.Sequence(hltResults*egHLTOffDQMSource*muonFullOfflineDQM*HLTTauDQMOffline*jetMETHLTOfflineSource*TnPEfficiency)
+offlineHLTSource = cms.Sequence(hltResults*egHLTOffDQMSource*muonFullOfflineDQM*HLTTauDQMOffline*jetMETHLTOfflineSource*TnPEfficiency*dqmEnvHLT)
 
-triggerOfflineDQMSource =  cms.Sequence(offlineHLTSource*l1temumonitor*l1tmonitor*onlineHLTSource)
+triggerOfflineDQMSource =  cms.Sequence(l1temumonitor*l1tmonitor*onlineHLTSource*offlineHLTSource)
  
