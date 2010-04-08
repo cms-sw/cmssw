dc52f7bada958268f2b491847ec828514e79398f
Elizabeth Sexton-Kennedy <sha1-3498bc7485b3587b4b87ba552af7f44c37610528@cern.ch>
2010-04-08 16:53:56 +0000

    Add a new alias for commissioning 10 data, which at the moment uses GR09_R_36X_V3

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -3,4 +3,5 @@ autoCond = { 'mc'      : 'MC_36Y_V3::All',
              'startup' : 'START36_V3::All',
              'craft08' : 'CRAFT08_R_V12::All',
              'craft09' : 'CRFT9_36R_V01::All',
+             'com10'   : 'GR09_R_36X_V3::All'
              }
