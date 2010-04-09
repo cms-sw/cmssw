7570e1539c83f38cef91275d3fb6425a5274b06e
John Paul Chou <sha1-d128ca71a1bf075281a5dede2e83919de6462f3d@cern.ch>
2010-04-09 03:00:10 +0000

    added HBHE Noise Filter

---
+++ CommonTools/RecoAlgos/python/HBHENoiseFilter_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+HBHENoiseFilter = cms.EDFilter(
+    'HBHENoiseFilter',
+    minRatio = cms.double(0.70),
+    maxRatio = cms.double(0.96),
+    minHPDHits = cms.int32(17),
+    minRBXHits = cms.int32(999),
+    minHPDNoOtherHits = cms.int32(10),
+    minZeros = cms.int32(10),
+    minHighEHitTime = cms.double(-9999.0),
+    maxHighEHitTime = cms.double(9999.0),
+    maxRBXEMF = cms.double(0.01)
+    )
