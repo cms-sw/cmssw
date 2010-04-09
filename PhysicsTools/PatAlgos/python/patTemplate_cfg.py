85241503c4511764981d7d824240ecd5131cf312
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2010-04-09 18:45:18 +0000

    input file from 360pre5 RelVal

--- PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -11,8 +11,7 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
-      #  '/store/relval/CMSSW_3_6_0_pre4/RelValProdMinBias/GEN-SIM-RECO/MC_36Y_V3-v1/0002/2A655C2D-E737-DF11-87C2-0030487C8CBE.root'
+      '/store/relval/CMSSW_3_6_0_pre5/RelValTTbar/GEN-SIM-RECO/START36_V3-v1/0009/0A36A3F4-D63D-DF11-8B35-002354EF3BE4.root'
     )
 )
 ## Maximal Number of Events
@@ -21,8 +20,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = cms.string('MC_3XY_V20::All')
-process.GlobalTag.globaltag = cms.string('START3X_V20::All')
+process.GlobalTag.globaltag = cms.string('START36_V4::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 ## Standard PAT Configuration File
