5a7eb866765aa5916d75d50de92bc6713e609d55
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2010-04-09 17:41:52 +0000

    moved some msgq-related classes here for common use + eworked Vulture interface

--- EventFilter/Utilities/src/Vulture.cc
+++ EventFilter/Utilities/src/Vulture.cc
@@ -18,7 +18,6 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 
-
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -28,25 +27,36 @@ namespace evf{
 
   const std::string Vulture::FS="/tmp";
 
-  Vulture::Vulture(std::string &url, bool push) :
-    wlPrey_(0)
-    , asPrey_(0)
-    , preying_(false)
-    , iDieUrl_(url)
+  Vulture::Vulture(bool push) :
+      wlCtrl_(0)
+    , asCtrl_(0)
+    , running_(false)
+    , wlProwl_(0)
+    , asProwl_(0)
+    , prowling_(false)
+    , iDieUrl_("")
     , updateMode_(push)
     , vulturePid_(0)
     , tmp_(0)
     , newCores_(0)
     , poster_(0)
+    , mq_(new MasterQueue(vulture_queue_offset))
+    , sq_(0) // this is only defined in the forked process
+    , started_(-1)
+    , stopped_(-1)
   {
+    // create command file for gdb
     FILE *outf = fopen("/tmp/vulture.cmd","w");
     fprintf(outf,"where\n");
     fclose(outf);
-  }
-
-  pid_t Vulture::start(int run){
     pid_t retval = fork();
     if(retval==0){ // we are in the forked process
+      int success = prctl( PR_SET_DUMPABLE, 0 );
+      if(success != 0) ::exit(-1);
+      success = prctl( PR_SET_PDEATHSIG, SIGKILL );
+      if(success != 0) ::exit(-1);
+      tmp_ = opendir(FS.c_str());
+      success = prctl ( PR_SET_NAME , "vulture");
       try{
 	pt::PeerTransport * ptr =
 	  pt::getPeerTransportAgent()->getPeerTransport("http","soap",pt::Receiver);
@@ -55,93 +65,183 @@ namespace evf{
       catch (pt::exception::PeerTransportNotFound & e ){
 	//do nothing here since we don't know what to do... ?
       }
-      int success = prctl( PR_SET_DUMPABLE, 0 );
-      if(success != 0) ::exit(-1);
-      success = prctl( PR_SET_PDEATHSIG, SIGKILL );
-      if(success != 0) ::exit(-1);
-      success = prctl( PR_SET_NAME, "Vulture" );
-      tmp_ = opendir("/tmp");
-      poster_ = new CurlPoster(iDieUrl_);
-      if(poster_->check(run)){
-	try{
-	  startPreying();
-	}
-	catch(evf::Exception &e)
-	  {
-	    ::exit(-1);
-	  }
+      freopen("/dev/null","w",stderr);
+      sq_ = new SlaveQueue(vulture_queue_offset);
+      // start the ctrl workloop
+      try {
+	wlCtrl_=
+	  toolbox::task::getWorkLoopFactory()->getWorkLoop("Ctrll",
+							   "waiting");
+	if (!wlCtrl_->isActive()) wlCtrl_->activate();
+	
+	asCtrl_ = toolbox::task::bind(this,&Vulture::control,
+				       "Ctrl");
+	wlCtrl_->submit(asCtrl_);
       }
-      else{
-	::exit(0);
+      catch (xcept::Exception& e) {
+	std::string msg = "Failed to start workloop 'Ctrl'.";
+	::exit(-1);
       }
-      success = prctl ( PR_SET_NAME , "vulture");
     }
-    else
-      {
-	vulturePid_ = retval;
+    else{
+      vulturePid_ = retval;
+    }
+
+  }
+  
+  Vulture::~Vulture()
+  {
+    delete mq_;
+    if(sq_ != 0) delete sq_;
+    if(poster_ != 0) delete poster_;
+  }
+
+
+  int Vulture::hasStarted(){
+    if(started_<0){
+      MsgBuf msg2(MAX_MSG_SIZE,MSQS_VULTURE_TYPE_ACK);
+      try{
+	mq_->rcvNonBlocking(msg2);
+	started_ = 0;
       }
-    return retval;
+      catch(evf::Exception &e){
+      }
+    } else {started_ = 1;}
+    return started_;	
+  }
+
+  int Vulture::hasStopped(){
+    if(stopped_<0){
+      MsgBuf msg2(MAX_MSG_SIZE,MSQS_VULTURE_TYPE_ACK);
+      try{
+	mq_->rcvNonBlocking(msg2);
+	stopped_ = 0;
+      }
+      catch(evf::Exception &e){
+      }
+    } else {stopped_ = 1;}
+    return stopped_;	
+  }
+
+  pid_t Vulture::start(std::string url, int run){
+
+    //communicate start-of-run to Vulture
+    vulture_start_message stamsg;
+    strncpy(stamsg.url_,url.c_str(),url.length()); 
+    stamsg.run_ = run;
+    MsgBuf msg1(sizeof(vulture_start_message),MSQM_VULTURE_TYPE_STA);
+    memcpy(msg1->mtext,&stamsg,sizeof(vulture_start_message));
+    mq_->post(msg1);
+    stopped_ = -1;
+    return vulturePid_;
   }
   
   pid_t Vulture::stop()
   {
-    preying_ = false;
-    // int retval = kill (vulturePid_, SIGKILL); // retval is unused
-    kill (vulturePid_, SIGKILL);
+
+    MsgBuf msg1(NUMERIC_MESSAGE_SIZE,MSQM_VULTURE_TYPE_STP);
+    mq_->post(msg1);
+    started_ = -1;
+    return vulturePid_;
+  }
+
+ pid_t Vulture::kill() // eventually *could* be called by master app - it isn't now
+  {
+    ::kill (vulturePid_, SIGKILL);
     int sl;
     pid_t killedOrNot = waitpid(vulturePid_,&sl,WNOHANG);
     vulturePid_ = 0;
-    char messageDie[5];
-    sprintf(messageDie,"Dead");
-    poster_ = new CurlPoster(iDieUrl_);
-    try{
-      poster_->postString(messageDie,5,0);
-    }
-    catch(evf::Exception &e){
-      //do nothing just swallow the exception
-    }
-    delete poster_;
-    poster_=0;
     return killedOrNot;
   }
-  
-  void Vulture::startPreying()
+
+  void Vulture::startProwling()
   {
     timeval now;
     gettimeofday(&now,0);
     lastUpdate_ = now.tv_sec;
-    preying_ = true;
+    prowling_ = true;
     try {
-      wlPrey_=
-	toolbox::task::getWorkLoopFactory()->getWorkLoop("Prey",
+      wlProwl_=
+	toolbox::task::getWorkLoopFactory()->getWorkLoop("Prowl",
 							 "waiting");
-      if (!wlPrey_->isActive()) wlPrey_->activate();
+      if (!wlProwl_->isActive()) wlProwl_->activate();
       
-      asPrey_ = toolbox::task::bind(this,&Vulture::preying,
-					 "Prey");
-      wlPrey_->submit(asPrey_);
+      asProwl_ = toolbox::task::bind(this,&Vulture::prowling,
+					 "Prowl");
+      wlProwl_->submit(asProwl_);
     }
     catch (xcept::Exception& e) {
-      std::string msg = "Failed to start workloop 'Prey'.";
+      std::string msg = "Failed to start workloop 'Prowl'.";
       XCEPT_RETHROW(evf::Exception,msg,e);
     }
 
   }
-  bool Vulture::preying(toolbox::task::WorkLoop*wl)
+
+  bool Vulture::control(toolbox::task::WorkLoop*wl)
+  {
+    MsgBuf msg;
+    unsigned long mtype = MSQM_MESSAGE_TYPE_NOP;
+    try{mtype = sq_->rcv(msg);}catch(evf::Exception &e){::exit(-1);}
+    switch(mtype){
+    case MSQM_VULTURE_TYPE_STA:
+      {
+
+	vulture_start_message *sta = (vulture_start_message*)msg->mtext;
+	if(poster_ == 0) poster_ = new CurlPoster(sta->url_);
+	if(poster_->check(sta->run_)){
+	  try{
+	    startProwling();
+	  }
+	  catch(evf::Exception &e)
+	    {
+	      //@EM ToDo generate some message here
+	    }	  
+	}else{
+	  ::exit(0);
+	}
+      
+	break;
+      }
+    case MSQM_VULTURE_TYPE_STP:
+      {
+	prowling_ = false;
+	break;
+      }
+    default:
+      {
+	// do nothing @EM ToDo generate an appropriate error message
+      }
+    }
+    return true;
+    
+  }
+
+  bool Vulture::prowling(toolbox::task::WorkLoop*wl)
   {
 
-    if(!preying_) return false;
+    if(!prowling_){
+      char messageDie[5];
+      sprintf(messageDie,"Dead");
+      
+      try{
+	poster_->postString(messageDie,5,0);
+      }
+      catch(evf::Exception &e){
+	  //do nothing just swallow the exception
+      }
+      delete poster_;
+      poster_=0;
+      
+      return false;
+    }
+    
     newCores_ = 0;
+    
     struct stat filestat;    
-//     stat(FS.c_str(),&filestat);
-//     if(filestat.st_mtime < lastUpdate_){ 
-//       std::cout << "tmp has not changed !!!" << std::endl;
-//       ::sleep(60); 
-//       return true;
-//     }
+    
     timeval now;
-
     gettimeofday(&now,0);
+    
     // examine /tmp looking for new coredumps
     dirent *dirp;
     while((dirp = readdir(tmp_))!=0){
@@ -149,7 +249,7 @@ namespace evf{
 	stat(dirp->d_name,&filestat);
 	if(filestat.st_mtime > lastUpdate_){
 	  currentCoreList_.push_back(dirp->d_name);
-	    newCores_++;
+	  newCores_++;
 	}
       }
     }
@@ -165,6 +265,7 @@ namespace evf{
     ::sleep(60);
     return true;
   }
+
   void Vulture::analyze()
   {
     // do a first analysis of the coredump
@@ -175,7 +276,7 @@ namespace evf{
       std::string command = "gdb /opt/xdaq/bin/xdaq.exe -batch -x /tmp/vulture.cmd -c /tmp/";
       std::string cmdout;
       command += currentCoreList_[i];
-      std::string filePathAndName = "/tmp/";
+      std::string filePathAndName = FS + "/";
       filePathAndName += currentCoreList_[i];
       std::string pid = 
 	currentCoreList_[i].substr(currentCoreList_[i].find_first_of(".")+1,
