c1009765ff69f53bac35e29e739d600e129914c5
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-04-09 11:55:25 +0000

    Update MC GT

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,6 +1,6 @@
 
-autoCond = { 'mc'      : 'MC_36Y_V3::All',
-             'startup' : 'START36_V3::All',
+autoCond = { 'mc'      : 'MC_36Y_V4::All',
+             'startup' : 'START36_V4::All',
              'craft08' : 'CRAFT08_R_V12::All',
              'craft09' : 'CRFT9_36R_V01::All',
              'com10'   : 'GR09_R_36X_V3::All'
