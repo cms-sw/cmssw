819b50aeb36a81a420f0560c31a8829fe07ba5c2
Bernard Fabbro <sha1-3d2b826b539eaacd22efa9afd4ff0e5b4516b3f9@cern.ch>
2010-04-09 08:57:35 +0000

    New version. New Analyzer. EB and EE.

--- CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/src/SealModule.cc
+++ CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/src/SealModule.cc
@@ -1,5 +1,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/interface/EcnaAnalyzer.h"
 
-#include "CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/interface/EcalCorrelatedNoisePedestalRunAnalyzer.h"
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(EcnaAnalyzer);
 
-DEFINE_FWK_MODULE(EcalCorrelatedNoisePedestalRunAnalyzer);
