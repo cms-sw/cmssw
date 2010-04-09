5e169e957991ef3f32a13d905c30b951a3c9062f
Fedor Ratnikov <sha1-cf070837f75a2e740f971de86d9d2e3a75a81342@cern.ch>
2010-04-09 11:38:37 +0000

    Modules to store coordinates of stopping points in the event, and convert them to ASCII file later

--- SimG4Core/CustomPhysics/plugins/module.cc
+++ SimG4Core/CustomPhysics/plugins/module.cc
@@ -1,8 +1,15 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "SimG4Core/Watcher/interface/SimWatcherFactory.h"
+
 #include "SimG4Core/Physics/interface/PhysicsListFactory.h"
 #include "SimG4Core/CustomPhysics/interface/CustomPhysics.h"
 
-#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "RHStopDump.h"
+#include "RHStopTracer.h"
 
 
 DEFINE_PHYSICSLIST(CustomPhysics);
+DEFINE_ANOTHER_FWK_MODULE(RHStopDump) ;
+DEFINE_SIMWATCHER(RHStopTracer);
 
