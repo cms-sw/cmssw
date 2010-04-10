03fe11777ebe00a2fb486ff5f84e17552095e45f
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2010-04-10 19:26:11 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/Skimming/test/EWK_MuSkimFirstCollisions.py
+++ ElectroWeakAnalysis/Skimming/test/EWK_MuSkimFirstCollisions.py
@@ -9,21 +9,31 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/009FD47A-14EA-DE11-A16C-001D09F276CF.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/04B52832-08EA-DE11-B565-001D09F23F2A.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/10183EFB-14EA-DE11-AA9B-000423D6B444.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/2015E326-19EA-DE11-9AB2-000423D99614.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/40BA430D-17EA-DE11-B5B0-003048D2BE08.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/44D74360-16EA-DE11-AB79-0030486730C6.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/66C694A9-15EA-DE11-A243-001D09F2AD84.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/6CA422E0-12EA-DE11-9F02-001D09F24399.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/78BF3FE5-19EA-DE11-8C08-001D09F2AF1E.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/7A01076A-0CEA-DE11-AA59-001D09F24600.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/AE9C108A-13EA-DE11-86FF-001D09F2905B.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/C22CBF0B-17EA-DE11-83E6-001617C3B77C.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/C83608FB-14EA-DE11-9FE5-000423D98B08.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/E8FBAD94-09EA-DE11-8FA9-001D09F2438A.root",
-"rfio:/castor/cern.ch/cms/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/124/275/F0CEF7FA-14EA-DE11-BCB4-000423D99AA2.root",
+
+###  files at 18:30
+
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/168948BD-B744-DF11-A4E8-0030487A3C9A.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/22EF0D53-BD44-DF11-AE35-0030487A18F2.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/248A55D1-B944-DF11-970A-0030487CD7C6.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/2E2D7B48-B644-DF11-A8B8-0030487CD7C6.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/2EFC219A-BC44-DF11-A1D3-0030487CD6E6.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/30AECA94-B544-DF11-B704-000423D98EA8.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/32F2902F-B444-DF11-A6A8-0030487CD180.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/3657A301-B744-DF11-B091-00304879BAB2.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/48B4C9BB-B744-DF11-8A62-00304879FA4A.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/6807CCE4-B444-DF11-BC8F-0030487C8CB8.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/94B4ECCC-B944-DF11-B658-0030487CD162.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/9E3D6030-B444-DF11-9855-0030487CD7C0.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/A2655C4C-B644-DF11-BB9F-0030487C635A.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/A807DED3-B944-DF11-9F4D-0030487C6A66.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/C63058CC-B944-DF11-B541-0030487CD7C0.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/CC550189-BA44-DF11-89BA-0030487D0D3A.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/D017E593-B544-DF11-9F04-001617C3B5F4.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/D4234400-B744-DF11-8A1C-0030487C8CB8.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/D844C567-B844-DF11-A0CC-0030487C5CFA.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/EA497C88-BA44-DF11-B1A8-0030487CD700.root",
+"rfio:/castor/cern.ch/cms/store/data/Commissioning10/MinimumBias/RECO/v8/000/132/968/F64CC0CC-B944-DF11-9F19-0030487CAF0E.root",
+
     )
 )
 # to handle some format problem  with some of the first CMS collsion runs
@@ -35,11 +45,11 @@ process.source.inputCommands = cms.untracked.vstring(
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.threshold = ''
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('GR09_R_35X_V4::All')
+process.GlobalTag.globaltag = cms.string('GR_R_35X_V6::All')
 #process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
@@ -48,7 +58,7 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi
 process.EWK_MuHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
 # Uncomment this to access 8E29 menu and filter on it
 process.EWK_MuHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
-process.EWK_MuHLTFilter.HLTPaths=["HLT_MinBiasBSC_OR", "HLT_L1Mu", "HLT_L1MuOpen"] 
+process.EWK_MuHLTFilter.HLTPaths=["HLT_MinBiasBSC", "HLT_L1Mu", "HLT_L1MuOpen"] 
 # Uncomment this to filter on 1E31 HLT menu
 #process.EWK_MuHLTFilter.HLTPaths = ["HLT_Mu9", "HLT_DoubleMu3"]
 
@@ -75,16 +85,36 @@ process.options = cms.untracked.PSet(
 
 
 # Muon filter
-process.goodMuons = cms.EDFilter("MuonViewRefSelector",
+process.goodMuons = cms.EDFilter("MuonSelector",
   src = cms.InputTag("muons"),
-  cut = cms.string('pt > 1.0 && ( isGlobalMuon=1 || (isTrackerMuon =1  && numberOfMatches>=1 ) || isStandAloneMuon=1)'), # also || (isCaloMuon=1) ??
+  cut = cms.string('pt > 10.0 && ( isGlobalMuon=1 || (isTrackerMuon =1  && numberOfMatches>=1 ))'), # also || (isCaloMuon=1) ??
   filter = cms.bool(True)                                
 )
+
+process.rmCosmicFromGoodMuons = cms.EDFilter("MuonSelector",
+  src = cms.InputTag("goodMuons"),
+  cut = cms.string('abs(innerTrack().dxy)<1.0'),
+  filter = cms.bool(True)                                
+)
+
+
+process.goodMuonsPt15 = cms.EDFilter("MuonSelector",
+  src = cms.InputTag("goodMuons"),
+  cut = cms.string('(isGlobalMuon=1 || isTrackerMuon =1) &&  pt > 15.0'), 
+  filter = cms.bool(False)                                
+)
+
+# Dump of interesting events, with mu pt>15
+process.eventDump = cms.EDAnalyzer(
+    "EventDumper",
+     srcMuons = cms.InputTag("goodMuonsPt15")
+    )
+
 
 # require at least two tracks with pt>.5, to hopefully remove further cosmic contaminations  
 process.tracks = cms.EDFilter("TrackSelector",
   src=cms.InputTag("generalTracks"),
-  cut = cms.string('abs(dxy)<0.5 && pt > 1.0 && hitPattern().numberOfValidPixelHits>0'),
+  cut = cms.string('abs(dxy)<0.5 && pt > 0.5 && hitPattern().numberOfValidPixelHits>0'),
   filter = cms.bool(True)                                
 )
 
@@ -105,13 +135,15 @@ process.dimuonsAOD = cms.EDFilter("CandViewShallowCloneCombiner",
 process.load("ElectroWeakAnalysis.WMuNu.wmunusProducer_cfi")
 
 
+
+
 # Output module configuration
 from Configuration.EventContent.EventContent_cff import *
 EWK_MuSkimEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring()
 )
 
-EWK_MuSkimEventContent.outputCommands.extend(AODEventContent.outputCommands)
+EWK_MuSkimEventContent.outputCommands.extend(FEVTEventContent.outputCommands)
 
 
 EWK_MuSkimEventSelection = cms.PSet(
@@ -125,6 +157,7 @@ EWK_MuSkimEventSelection = cms.PSet(
 dimuonsEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep *_dimuonsAOD_*_*',
+    'keep *_CosmicFromGoodMuons_*_*', 
     'keep recoWMuNuCandidates_*_*_*'
     )
  )
@@ -141,7 +174,8 @@ process.EWK_MuSkimOutputModule = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('EWKMuSkim'),
         dataTier = cms.untracked.string('USER')
    ),
-   fileName = cms.untracked.string('testEWKMuSkim_L1TG0_4041AllMuAtLeastThreeTracks124275.root')
+   fileName = cms.untracked.string('EWKMuSkim_L1TG04041_AllMuAtLeastThreeTracks132968_1.root')
+#   fileName = cms.untracked.string('test.root')
 )
 
 
@@ -151,14 +185,17 @@ process.EWK_MuSkimPath = cms.Path(
   process.EWK_MuHLTFilter +
   process.hltLevel1GTSeed+
   process.goodMuons +
+  process.rmCosmicFromGoodMuons +
   process.tracks +
   process.tracksFilter +
   process.dimuonsAOD +
-  process.allWMuNus
+  process.allWMuNus +
+  process.goodMuonsPt15 +
+  process.eventDump
+
+
 )
 
 
 process.outpath = cms.EndPath(process.EWK_MuSkimOutputModule)
-
-
 
