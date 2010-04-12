f3b59f10c2e9eb88552da5e3901fa8a2ee407c83
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-04-12 18:01:16 +0000

    moved to most actual pre-release inputfiles

--- PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -11,8 +11,7 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-      '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
-     #'/store/relval/CMSSW_3_6_0_pre5/RelValTTbar/GEN-SIM-RECO/START36_V3-v1/0009/0A36A3F4-D63D-DF11-8B35-002354EF3BE4.root'
+      '/store/relval/CMSSW_3_6_0_pre6/RelValTTbar/GEN-SIM-RECO/START36_V4-v1/0010/323F69C9-9F44-DF11-8BA0-002618943943.root'
     )
 )
 ## Maximal Number of Events
