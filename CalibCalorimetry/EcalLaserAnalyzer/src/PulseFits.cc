b269d851f344b64da0597c0edf725dc5e3bb0524
Julie Malcles <sha1-2597814f27f9a62af47364ec73303bc025115982@cern.ch>
2010-04-12 08:38:39 +0000

    Add new classes: MENLS.cc for final corrections histograms, TCalibData for access to shapes, pn corrections, ab, etc

---
+++ CalibCalorimetry/EcalLaserAnalyzer/src/PulseFits.cc
@@ -0,0 +1,24 @@
+/* 
+ *  \class PulseFits
+ *
+ *  $Date: 2009/06/02 12:55:20 $
+ *  \author: Julie Malcles - CEA/Saclay
+ */
+
+
+// File PulseFits.cxx
+
+#include <CalibCalorimetry/EcalLaserAnalyzer/interface/PulseFits.h>
+
+
+//ClassImp(PulseFits)
+
+// Constructor...
+PulseFits::PulseFits()
+{ 
+}
+
+// Destructor
+PulseFits::~PulseFits()
+{ 
+}
