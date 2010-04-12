64ad2aa20b281b9e77d3caec42f9516119ade6ee
kalavase <sha1-37b724a660ad78b379b1c02b36b874291d39b117@cern.ch>
2010-04-12 07:47:29 +0000

    updated with functions that now also return the signed radius of conversion and the point of conversion

--- RecoEgamma/EgammaTools/interface/ConversionFinder.h
+++ RecoEgamma/EgammaTools/interface/ConversionFinder.h
@@ -1,59 +1,54 @@
-#ifndef RecoEgamma_EgammaTools_ConversionFinder_h
-#define RecoEgamma_EgammaTools_ConversionFinder_h
+#ifndef EgammaTools_ConversionFinder_h
+#define EgammaTools_ConversionFinder_h
 /** \class reco::ConversionFinder ConversionFinder.h RecoEgamma/EgammaTools/interface/ConversionFinder.h
   *  
   * Conversion finding and rejection code 
-  * Returns true or false given a GsfElectron and track collection
   * Uses simple geometric methods to determine whether or not the 
   * electron did indeed come from a conversion
   * \author Puneeth Kalavase, University Of California, Santa Barbara
   *
-  * \version $Id: ConversionFinder.h,v 1.3 2009/11/16 08:10:46 kalavase Exp $
+  * \version $Id: ConversionFinder.h,v 1.4 2009/11/18 20:40:25 kalavase Exp $
   *
   */
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/Math/interface/Point3D.h"
 #include "MagneticField/Engine/interface/MagneticField.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrackFwd.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 #include "Math/VectorUtil.h"
+#include "ConversionInfo.h"
 
-class ConversionFinder {
+
+/* 
+   Class Looks for oppositely charged track in the 
+   track collection with the minimum delta cot(theta) between the track
+   and the electron's CTF track (if it doesn't exist, we use the electron's
+   GSF track). Calculate the dist, dcot, point of conversion and the 
+   radius of conversion for this pair and fill the ConversionInfo
+*/
+
+class ConversionFinder {    
  public:
   ConversionFinder();
   ~ConversionFinder();
   //bField has to be supplied in Tesla
-  
-  static reco::TrackRef getConversionPartnerTrack(const reco::GsfElectron& gsfElectron, 
-						  const edm::Handle<reco::TrackCollection>& track_h,
-						  const float bFieldAtOrigin,
-						  const float maxAbsDist = 0.02,
-						  const float maxAbsDCot = 0.02,
-						  const float minFracSharedHits = 0.45);
-
-  //very simple function that calls getConversionPartnerTrack
-  //if such a conversion partner track is found, will return true, else false
-  static bool isElFromConversion(const reco::GsfElectron& gsfElectron, 
-				 const edm::Handle<reco::TrackCollection>& track_h,
-				 const float bFieldAtOrigin,
-				 const float maxAbsDist = 0.02,
-				 const float maxAbsDCot = 0.02,
-				 const float minFracSharedHits = 0.45);
-
-  
-  static std::pair<double, double> getConversionInfo(math::XYZTLorentzVector trk1_p4, 
-						     int trk1_q, float trk1_d0, 
-						     math::XYZTLorentzVector trk2_p4,
-						     int trk2_q, float trk2_d0,
-						     float bFieldAtOrigin);
-
-  //returns GSF track if associated CTF track does not have fraction of shared hits greater than minFracSharedHits
-  static const reco::Track* getElectronTrack(const reco::GsfElectron& gsfElectron, const float minFracSharedHits);
-
+  ConversionInfo getConversionInfo(const reco::GsfElectron& gsfElectron, 
+				   const edm::Handle<reco::TrackCollection>& track_h, 
+				   const double bFieldAtOrigin,
+				   const double minFracSharedHits = 0.45);
+  /*
+    cuts tuned for high pt ( pt > 20 GeV )electrons
+    fnc must be called after getConversionInfo is called
+  */
+  bool isFromConversion(double maxAbsDist = 0.2, double maxAbsDcot = 0.02);
+  const reco::Track* getElectronTrack(const reco::GsfElectron& electron, const float minFracSharedHits = 0.45);
+ private:
+  ConversionInfo convInfo_;
 };
 #endif
