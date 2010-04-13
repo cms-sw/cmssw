be66265a51633e204dd8b0dd63013245fc03c3c3
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-04-13 14:55:50 +0000

    Make the JetIDSelectionFunctor compile

--- PhysicsTools/SelectorUtils/interface/JetIDSelectionFunctor.h
+++ PhysicsTools/SelectorUtils/interface/JetIDSelectionFunctor.h
@@ -13,7 +13,7 @@
   for a general overview of the selectors. 
 
   \author Salvatore Rappoccio (Update: Amnon Harel)
-  \version  $Id: JetIDSelectionFunctor.h,v 1.5 2010/02/10 20:06:25 srappocc Exp $
+  \version  $Id: JetIDSelectionFunctor.h,v 1.7 2010/04/13 14:38:33 srappocc Exp $
 */
 
 
@@ -23,6 +23,7 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "PhysicsTools/SelectorUtils/interface/Selector.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include <TMath.h>
 class JetIDSelectionFunctor : public Selector<pat::Jet>  {
@@ -31,11 +32,53 @@ class JetIDSelectionFunctor : public Selector<pat::Jet>  {
 
   enum Version_t { CRAFT08, PURE09, DQM09, N_VERSIONS };
   enum Quality_t { MINIMAL, LOOSE_AOD, LOOSE, TIGHT, N_QUALITY};
-  
 
- JetIDSelectionFunctor( Version_t version, Quality_t quality ) :
-  version_(version), quality_(quality)
+
+  JetIDSelectionFunctor( edm::ParameterSet const & parameters ) {
+    std::string versionStr = parameters.getParameter<std::string>("version");
+    std::string qualityStr = parameters.getParameter<std::string>("quality");
+    Quality_t quality = N_QUALITY;
+
+    if ( versionStr == "CRAFT08" ) {
+      if      ( qualityStr == "MINIMAL" )   quality = MINIMAL;
+      else if ( qualityStr == "LOOSE_AOD" ) quality = LOOSE_AOD;
+      else if ( qualityStr == "LOOSE" )     quality = LOOSE;
+      else                                  quality = TIGHT;
+      
+      initialize( CRAFT08, quality );
+    } 
+    else if ( versionStr == "PURE09" ) {
+      if      ( qualityStr == "MINIMAL" )   quality = MINIMAL;
+      else if ( qualityStr == "LOOSE_AOD" ) quality = LOOSE_AOD;
+      else if ( qualityStr == "LOOSE" )     quality = LOOSE;
+      else                                  quality = TIGHT;
+
+      initialize( PURE09, quality );
+      
+    }
+    else if ( versionStr == "DQM09" ) {
+      
+      if      ( qualityStr == "MINIMAL" )   quality = MINIMAL;
+      else if ( qualityStr == "LOOSE_AOD" ) quality = LOOSE_AOD;
+      else if ( qualityStr == "LOOSE" )     quality = LOOSE;
+      else                                  quality = TIGHT;
+
+      initialize( DQM09, quality );
+    }
+    else {
+      throw cms::Exception("InvalidInput") << "Expect version to be one of SUMMER08, FIRSTDATA," << std::endl;
+    }
+  }
+
+  JetIDSelectionFunctor( Version_t version, Quality_t quality ) {
+    initialize(version, quality);
+  }
+
+ void initialize( Version_t version, Quality_t quality )
   {
+
+    version_ = version;
+    quality_ = quality;
 
     push_back("MINIMAL_EMF");
 
@@ -157,7 +200,7 @@ class JetIDSelectionFunctor : public Selector<pat::Jet>  {
   {
     unsigned int nHit = 0;
     for ( unsigned int iTower = 0; iTower < towers.size() ; ++iTower ) {
-      const vector<DetId>& cellIDs = towers[iTower]->constituents();  // cell == recHit
+      const std::vector<DetId>& cellIDs = towers[iTower]->constituents();  // cell == recHit
       nHit += cellIDs.size();
     }
     return nHit;
