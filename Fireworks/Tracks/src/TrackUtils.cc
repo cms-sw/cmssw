687c740f929cc1f048c133b88e5eaa5deb8791b1
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-04-14 12:41:54 +0000

    Format to 3 spaces in tab

--- Fireworks/Tracks/src/TrackUtils.cc
+++ Fireworks/Tracks/src/TrackUtils.cc
@@ -2,7 +2,7 @@
 //
 // Package:     Core
 // Class  :     TrackUtils
-// $Id: TrackUtils.cc,v 1.18 2010/04/13 08:52:22 yana Exp $
+// $Id: TrackUtils.cc,v 1.19 2010/04/14 11:52:46 yana Exp $
 //
 
 // system include files
@@ -1542,209 +1542,209 @@ namespace fireworks {
       return 200*sqrt(1-cosAlpha*cosAlpha)/0.2998*transverseMomentum.r()/displacement.r();
    }
 
-  std::string
-  info(const DetId& id) {
-     std::ostringstream oss;
+   std::string
+   info(const DetId& id) {
+      std::ostringstream oss;
    
-     oss << "DetId: " << id.rawId() << "\n";
+      oss << "DetId: " << id.rawId() << "\n";
    
-     switch ( id.det() ) {
+      switch ( id.det() ) {
     
-     case DetId::Tracker:
-        switch ( id.subdetId() ) {
-	case StripSubdetector::TIB:
-	   {
-	      oss <<"TIB "<<TIBDetId(id).layer();
-	   }
-	   break;
-	case StripSubdetector::TOB:
-	   {
-	      oss <<"TOB "<<TOBDetId(id).layer();
-	   }
-	   break;
-	case StripSubdetector::TEC:
-	   {
-	      oss <<"TEC "<<TECDetId(id).wheel();
-	   }
-	   break;
-	case StripSubdetector::TID:
-	   {
-	      oss <<"TID "<<TIDDetId(id).wheel();
-	   }
-	   break;
-	case (int) PixelSubdetector::PixelBarrel:
-	   {
-	      oss <<"PixBarrel "<< PXBDetId(id).layer();
-	   }
-	   break;
-	case (int) PixelSubdetector::PixelEndcap:
-	   {
-	      oss <<"PixEndcap "<< PXBDetId(id).layer();
-	   }
-	   break;
-	}
-	break;
+      case DetId::Tracker:
+         switch ( id.subdetId() ) {
+	 case StripSubdetector::TIB:
+	    {
+	       oss <<"TIB "<<TIBDetId(id).layer();
+	    }
+	    break;
+	 case StripSubdetector::TOB:
+	    {
+	       oss <<"TOB "<<TOBDetId(id).layer();
+	    }
+	    break;
+	 case StripSubdetector::TEC:
+	    {
+	       oss <<"TEC "<<TECDetId(id).wheel();
+	    }
+	    break;
+	 case StripSubdetector::TID:
+	    {
+	       oss <<"TID "<<TIDDetId(id).wheel();
+	    }
+	    break;
+	 case (int) PixelSubdetector::PixelBarrel:
+	    {
+	       oss <<"PixBarrel "<< PXBDetId(id).layer();
+	    }
+	    break;
+	 case (int) PixelSubdetector::PixelEndcap:
+	    {
+	       oss <<"PixEndcap "<< PXBDetId(id).layer();
+	    }
+	    break;
+	 }
+	 break;
 
-     case DetId::Muon:
-        switch ( id.subdetId() ) {
-	case MuonSubdetId::DT:
-	   { 
-	      DTChamberId detId(id.rawId());
-	      oss << "DT chamber (wheel, station, sector): "
-		  << detId.wheel() << ", "
-		  << detId.station() << ", "
-		  << detId.sector();
-	   }
-	   break;
-	case MuonSubdetId::CSC:
-	   {
-	      CSCDetId detId(id.rawId());
-	      oss << "CSC chamber (endcap, station, ring, chamber, layer): "
-		  << detId.endcap() << ", "
-		  << detId.station() << ", "
-		  << detId.ring() << ", "
-		  << detId.chamber() << ", "
-		  << detId.layer();
-	   }
-	   break;
-	case MuonSubdetId::RPC:
-	   { 
-	      RPCDetId detId(id.rawId());
-	      oss << "RPC chamber ";
-	      switch ( detId.region() ) {
-	      case 0:
-		 oss << "/ barrel / (wheel, station, sector, layer, subsector, roll): "
-		     << detId.ring() << ", "
-		     << detId.station() << ", "
-		     << detId.sector() << ", "
-		     << detId.layer() << ", "
-		     << detId.subsector() << ", "
-		     << detId.roll();
-		 break;
-	      case 1:
-		 oss << "/ forward endcap / (wheel, station, sector, layer, subsector, roll): "
-		     << detId.ring() << ", "
-		     << detId.station() << ", "
-		     << detId.sector() << ", "
-		     << detId.layer() << ", "
-		     << detId.subsector() << ", "
-		     << detId.roll();
-		 break;
-	      case -1:
-		 oss << "/ backward endcap / (wheel, station, sector, layer, subsector, roll): "
-		     << detId.ring() << ", "
-		     << detId.station() << ", "
-		     << detId.sector() << ", "
-		     << detId.layer() << ", "
-		     << detId.subsector() << ", "
-		     << detId.roll();
-		 break;
-	      }
-	   }
-	   break;
-	}
-	break;
+      case DetId::Muon:
+         switch ( id.subdetId() ) {
+	 case MuonSubdetId::DT:
+	    { 
+	       DTChamberId detId(id.rawId());
+	       oss << "DT chamber (wheel, station, sector): "
+		   << detId.wheel() << ", "
+		   << detId.station() << ", "
+		   << detId.sector();
+	    }
+	    break;
+	 case MuonSubdetId::CSC:
+	    {
+	       CSCDetId detId(id.rawId());
+	       oss << "CSC chamber (endcap, station, ring, chamber, layer): "
+		   << detId.endcap() << ", "
+		   << detId.station() << ", "
+		   << detId.ring() << ", "
+		   << detId.chamber() << ", "
+		   << detId.layer();
+	    }
+	    break;
+	 case MuonSubdetId::RPC:
+	    { 
+	       RPCDetId detId(id.rawId());
+	       oss << "RPC chamber ";
+	       switch ( detId.region() ) {
+	       case 0:
+		  oss << "/ barrel / (wheel, station, sector, layer, subsector, roll): "
+		      << detId.ring() << ", "
+		      << detId.station() << ", "
+		      << detId.sector() << ", "
+		      << detId.layer() << ", "
+		      << detId.subsector() << ", "
+		      << detId.roll();
+		  break;
+	       case 1:
+		  oss << "/ forward endcap / (wheel, station, sector, layer, subsector, roll): "
+		      << detId.ring() << ", "
+		      << detId.station() << ", "
+		      << detId.sector() << ", "
+		      << detId.layer() << ", "
+		      << detId.subsector() << ", "
+		      << detId.roll();
+		  break;
+	       case -1:
+		  oss << "/ backward endcap / (wheel, station, sector, layer, subsector, roll): "
+		      << detId.ring() << ", "
+		      << detId.station() << ", "
+		      << detId.sector() << ", "
+		      << detId.layer() << ", "
+		      << detId.subsector() << ", "
+		      << detId.roll();
+		  break;
+	       }
+	    }
+	    break;
+	 }
+	 break;
     
-     case DetId::Calo:
-        {
-	   CaloTowerDetId detId( id.rawId() );
-	   oss << "CaloTower (ieta, iphi): "
-	       << detId.ieta() << ", "
-	       << detId.iphi();
-	}
-	break;
+      case DetId::Calo:
+         {
+	    CaloTowerDetId detId( id.rawId() );
+	    oss << "CaloTower (ieta, iphi): "
+		<< detId.ieta() << ", "
+		<< detId.iphi();
+	 }
+	 break;
     
-     case DetId::Ecal:
-        switch ( id.subdetId() ) {
-	case EcalBarrel:
-	   {
-	      EBDetId detId(id);
-	      oss << "EcalBarrel (ieta, iphi, tower_ieta, tower_iphi): "
-		  << detId.ieta() << ", "
-		  << detId.iphi() << ", "
-		  << detId.tower_ieta() << ", "
-		  << detId.tower_iphi();
-	   }
-	   break;
-	case EcalEndcap:
-	   {
-	      EEDetId detId(id);
-	      oss << "EcalEndcap (ix, iy, SuperCrystal, crystal, quadrant): "
-		  << detId.ix() << ", "
-		  << detId.iy() << ", "
-		  << detId.isc() << ", "
-		  << detId.ic() << ", "
-		  << detId.iquadrant();
-	   }
-	   break;
-	case EcalPreshower:
-	   oss << "EcalPreshower";
-	   break;
-	case EcalTriggerTower:
-	   oss << "EcalTriggerTower";
-	   break;
-	case EcalLaserPnDiode:
-	  oss << "EcalLaserPnDiode";
-	  break;
-	}
-	break;
+      case DetId::Ecal:
+         switch ( id.subdetId() ) {
+	 case EcalBarrel:
+	    {
+	       EBDetId detId(id);
+	       oss << "EcalBarrel (ieta, iphi, tower_ieta, tower_iphi): "
+		   << detId.ieta() << ", "
+		   << detId.iphi() << ", "
+		   << detId.tower_ieta() << ", "
+		   << detId.tower_iphi();
+	    }
+	    break;
+	 case EcalEndcap:
+	    {
+	       EEDetId detId(id);
+	       oss << "EcalEndcap (ix, iy, SuperCrystal, crystal, quadrant): "
+		   << detId.ix() << ", "
+		   << detId.iy() << ", "
+		   << detId.isc() << ", "
+		   << detId.ic() << ", "
+		   << detId.iquadrant();
+	    }
+	    break;
+	 case EcalPreshower:
+	    oss << "EcalPreshower";
+	    break;
+	 case EcalTriggerTower:
+	    oss << "EcalTriggerTower";
+	    break;
+	 case EcalLaserPnDiode:
+	    oss << "EcalLaserPnDiode";
+	    break;
+	 }
+	 break;
       
-     case DetId::Hcal:
-        {
-	   HcalDetId detId(id);
-	   switch ( detId.subdet() ) {
-	   case HcalEmpty:
-	      oss << "HcalEmpty ";
-	      break;
-	   case HcalBarrel:
-	      oss << "HcalBarrel ";
-	      break;
-	   case HcalEndcap:
-	      oss << "HcalEndcap ";
-	      break;
-	   case HcalOuter:
-	      oss << "HcalOuter ";
-	      break;
-	   case HcalForward:
-	      oss << "HcalForward ";
-	      break;
-	   case HcalTriggerTower:
-	      oss << "HcalTriggerTower ";
-	      break;
-	   case HcalOther:
-	      oss << "HcalOther ";
-	      break;
-	   }
-	   oss << "(ieta, iphi, depth):"
-	       << detId.ieta() << ", "
-	       << detId.iphi() << ", "
-	       << detId.depth();
-	}
-	break;
-     default :;
-     }
-     return oss.str();
-  }
+      case DetId::Hcal:
+         {
+	    HcalDetId detId(id);
+	    switch ( detId.subdet() ) {
+	    case HcalEmpty:
+	       oss << "HcalEmpty ";
+	       break;
+	    case HcalBarrel:
+	       oss << "HcalBarrel ";
+	       break;
+	    case HcalEndcap:
+	       oss << "HcalEndcap ";
+	       break;
+	    case HcalOuter:
+	       oss << "HcalOuter ";
+	       break;
+	    case HcalForward:
+	       oss << "HcalForward ";
+	       break;
+	    case HcalTriggerTower:
+	       oss << "HcalTriggerTower ";
+	       break;
+	    case HcalOther:
+	       oss << "HcalOther ";
+	       break;
+	    }
+	    oss << "(ieta, iphi, depth):"
+		<< detId.ieta() << ", "
+		<< detId.iphi() << ", "
+		<< detId.depth();
+	 }
+	 break;
+      default :;
+      }
+      return oss.str();
+   }
  
-  std::string
-  info(const std::set<DetId>& idSet) {
-     std::string text;
-     for(std::set<DetId>::const_iterator id = idSet.begin(), idEnd = idSet.end(); id != idEnd; ++id)
-     {
-        text += info(*id);
-	text += "\n";
-     }
-     return text;
-  }
+   std::string
+   info(const std::set<DetId>& idSet) {
+      std::string text;
+      for(std::set<DetId>::const_iterator id = idSet.begin(), idEnd = idSet.end(); id != idEnd; ++id)
+      {
+         text += info(*id);
+	 text += "\n";
+      }
+      return text;
+   }
 
-  std::string
-  info(const std::vector<DetId>& idSet) {
-     std::string text;
-     for(std::vector<DetId>::const_iterator id = idSet.begin(), idEnd = idSet.end(); id != idEnd; ++id)
-     {
-        text += info(*id);
-	text += "\n";
-     }
-     return text;
-  }   
+   std::string
+   info(const std::vector<DetId>& idSet) {
+      std::string text;
+      for(std::vector<DetId>::const_iterator id = idSet.begin(), idEnd = idSet.end(); id != idEnd; ++id)
+      {
+         text += info(*id);
+	 text += "\n";
+      }
+      return text;
+   }   
 }
