bbde9e330b2814ff7dca14d55aa34d1b9f2d4a5b
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-04-15 13:52:25 +0000

    Merge Electron proxy builders.

--- Fireworks/Electrons/plugins/FWElectronProxyBuilder.cc
+++ Fireworks/Electrons/plugins/FWElectronProxyBuilder.cc
@@ -8,11 +8,12 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Tue Dec  2 14:17:03 EST 2008
-// $Id: FWElectronProxyBuilder.cc,v 1.2 2010/04/13 13:58:04 yana Exp $
+// $Id: FWElectronProxyBuilder.cc,v 1.3 2010/04/14 11:53:41 yana Exp $
 //
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/FWEvePtr.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Core/interface/FWEveScalableStraightLineSet.h"
 
 #include "Fireworks/Candidates/interface/CandidateUtils.h"
 #include "Fireworks/Tracks/interface/TrackUtils.h"
@@ -52,4 +53,34 @@ FWElectronProxyBuilder::build(const reco::GsfElectron& iData, unsigned int iInde
    oItemHolder.AddElement( track );
 }
 
+class FWElectronGlimpseProxyBuilder : public FWSimpleProxyBuilderTemplate<reco::GsfElectron> {
+
+public:
+   FWElectronGlimpseProxyBuilder() {}
+   virtual ~FWElectronGlimpseProxyBuilder() {}
+
+   REGISTER_PROXYBUILDER_METHODS();
+
+private:
+   FWElectronGlimpseProxyBuilder(const FWElectronGlimpseProxyBuilder&); // stop default
+
+   const FWElectronGlimpseProxyBuilder& operator=(const FWElectronGlimpseProxyBuilder&); // stop default
+
+   virtual void build(const reco::GsfElectron& iData, unsigned int iIndex, TEveElement& oItemHolder) const;
+};
+
+void
+FWElectronGlimpseProxyBuilder::build( const reco::GsfElectron& iData, unsigned int iIndex,TEveElement& oItemHolder ) const
+{
+   FWEveScalableStraightLineSet* marker = new FWEveScalableStraightLineSet("", "");
+   marker->SetLineWidth(2);
+   fireworks::addStraightLineSegment( marker, &iData, 1.0 );
+   oItemHolder.AddElement(marker);
+   //add to scaler at end so that it can scale the line after all ends have been added
+   // FIXME: It's not a part of a standard FWSimpleProxyBuilderTemplate: the scaler is not set!
+//    assert(scaler());
+//    scaler()->addElement(marker);
+}
+
 REGISTER_FWPROXYBUILDER(FWElectronProxyBuilder, reco::GsfElectron, "Electrons", FWViewType::k3DBit | FWViewType::kRPZBit);
+REGISTER_FWPROXYBUILDER(FWElectronGlimpseProxyBuilder, reco::GsfElectron, "Electrons", FWViewType::kGlimpseBit);
