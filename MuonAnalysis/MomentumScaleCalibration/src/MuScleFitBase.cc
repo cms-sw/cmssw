6d1d223540ad74266ab0091b00a64c1321fad30e
Sara Bolognesi <sha1-51b5699b46d20d34b344d09c5009ce0ed77efe29@cern.ch>
2010-04-15 16:01:11 +0000

    read the histogram of the model only if resfound[i] is true

--- MuonAnalysis/MomentumScaleCalibration/src/MuScleFitBase.cc
+++ MuonAnalysis/MomentumScaleCalibration/src/MuScleFitBase.cc
@@ -114,119 +114,144 @@ void MuScleFitBase::readProbabilityDistributionsFromFile()
   TH2D * GLZ[24];
   TH2D * GL[6];
   TFile * ProbsFile;
-  if ( theMuonType_!=2 ) {
-    if( probabilitiesFile_ != "" ) {
-      ProbsFile = new TFile (probabilitiesFile_.c_str());
-      std::cout << "[MuScleFit-Constructor]: Reading TH2D probabilities from " << probabilitiesFile_ << std::endl;
-    }
-    else {
-      // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_new_1000_CTEQ.root");
-      // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_new_Horace_CTEQ_1000.root");
-      // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_merge.root");
-      edm::FileInPath file(probabilitiesFileInPath_.c_str());
-      ProbsFile = new TFile (file.fullPath().c_str());
-      std::cout << "[MuScleFit-Constructor]: Reading TH2D probabilities from " << probabilitiesFileInPath_ << std::endl;
-    }
+  if( probabilitiesFile_ != "" ) {
+    ProbsFile = new TFile (probabilitiesFile_.c_str());
+    std::cout << "[MuScleFit-Constructor]: Reading TH2D probabilities from " << probabilitiesFile_ << std::endl;
   }
   else {
-    edm::FileInPath fileSM("MuonAnalysis/MomentumScaleCalibration/test/Probs_SM_1000.root");
-    ProbsFile = new TFile (fileSM.fullPath().c_str()); // NNBB need to reset this if MuScleFitUtils::nbins changes
-    // ProbsFile = new TFile ("Probs_SM_1000.root"); // NNBB need to reset this if MuScleFitUtils::nbins changes
-    std::cout << "[MuScleFit-Constructor]: Reading TH2D probabilities from Probs_new_SM_1000.root file" << std::endl;
+    // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_new_1000_CTEQ.root");
+    // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_new_Horace_CTEQ_1000.root");
+    // edm::FileInPath file("MuonAnalysis/MomentumScaleCalibration/test/Probs_merge.root");
+    edm::FileInPath file(probabilitiesFileInPath_.c_str());
+    ProbsFile = new TFile (file.fullPath().c_str());
+    std::cout << "[MuScleFit-Constructor]: Reading TH2D probabilities from " << probabilitiesFileInPath_ << std::endl;
   }
+
   ProbsFile->cd();
-  if( theMuonType_!=2 ) {
+  if( theMuonType_!=2 && MuScleFitUtils::resfind[0]) {
     for ( int i=0; i<24; i++ ) {
       char nameh[6];
       sprintf (nameh,"GLZ%d",i);
       GLZ[i] = dynamic_cast<TH2D*>(ProbsFile->Get(nameh));
     }
   }
-  GL[0] = dynamic_cast<TH2D*> (ProbsFile->Get("GL0"));
-  GL[1] = dynamic_cast<TH2D*> (ProbsFile->Get("GL1"));
-  GL[2] = dynamic_cast<TH2D*> (ProbsFile->Get("GL2"));
-  GL[3] = dynamic_cast<TH2D*> (ProbsFile->Get("GL3"));
-  GL[4] = dynamic_cast<TH2D*> (ProbsFile->Get("GL4"));
-  GL[5] = dynamic_cast<TH2D*> (ProbsFile->Get("GL5"));
+  if( theMuonType_==2 && MuScleFitUtils::resfind[0]) 
+    GL[0] = dynamic_cast<TH2D*> (ProbsFile->Get("GL0"));
+  if(MuScleFitUtils::resfind[1]) 
+    GL[1] = dynamic_cast<TH2D*> (ProbsFile->Get("GL1"));
+  if(MuScleFitUtils::resfind[2]) 
+    GL[2] = dynamic_cast<TH2D*> (ProbsFile->Get("GL2"));
+  if(MuScleFitUtils::resfind[3]) 
+    GL[3] = dynamic_cast<TH2D*> (ProbsFile->Get("GL3"));
+  if(MuScleFitUtils::resfind[4]) 
+    GL[4] = dynamic_cast<TH2D*> (ProbsFile->Get("GL4"));
+  if(MuScleFitUtils::resfind[5]) 
+    GL[5] = dynamic_cast<TH2D*> (ProbsFile->Get("GL5"));
 
   // Read the limits for M and Sigma axis for each pdf
   // Note: we assume all the Z histograms to have the same limits
   // x is mass, y is sigma
-  // We also take ResMass as the mean value between the two axis
-  if( theMuonType_!=2 ) {
+  if(MuScleFitUtils::resfind[0] && theMuonType_!=2) {
     MuScleFitUtils::ResHalfWidth[0] = (GLZ[0]->GetXaxis()->GetXmax() - GLZ[0]->GetXaxis()->GetXmin())/2.;
     MuScleFitUtils::ResMaxSigma[0] = (GLZ[0]->GetYaxis()->GetXmax() - GLZ[0]->GetYaxis()->GetXmin());
-    MuScleFitUtils::ResMass[0] = (GLZ[0]->GetXaxis()->GetXmax() + GLZ[0]->GetXaxis()->GetXmin())/2.;
+    MuScleFitUtils::ResMinMass[0] = (GLZ[0]->GetXaxis()->GetXmin());
   }
-  else {
+  if(MuScleFitUtils::resfind[0] && theMuonType_==2) {
     MuScleFitUtils::ResHalfWidth[0] = (GL[0]->GetXaxis()->GetXmax() - GL[0]->GetXaxis()->GetXmin())/2.;
     MuScleFitUtils::ResMaxSigma[0] = (GL[0]->GetYaxis()->GetXmax() - GL[0]->GetYaxis()->GetXmin());
-    MuScleFitUtils::ResMass[0] = (GL[0]->GetXaxis()->GetXmax() + GL[0]->GetXaxis()->GetXmin())/2.;
+    MuScleFitUtils::ResMinMass[0] = (GL[0]->GetXaxis()->GetXmin());
   }
   for( int i=1; i<6; ++i ) {
-    MuScleFitUtils::ResHalfWidth[i] = (GL[i]->GetXaxis()->GetXmax() - GL[i]->GetXaxis()->GetXmin())/2.;
-    MuScleFitUtils::ResMaxSigma[i] = (GL[i]->GetYaxis()->GetXmax() - GL[i]->GetYaxis()->GetXmin());
-    MuScleFitUtils::ResMass[i] = (GL[i]->GetXaxis()->GetXmax() + GL[i]->GetXaxis()->GetXmin())/2.;
+    if(MuScleFitUtils::resfind[i]){
+      MuScleFitUtils::ResHalfWidth[i] = (GL[i]->GetXaxis()->GetXmax() - GL[i]->GetXaxis()->GetXmin())/2.;
+      MuScleFitUtils::ResMaxSigma[i] = (GL[i]->GetYaxis()->GetXmax() - GL[i]->GetYaxis()->GetXmin());
+      MuScleFitUtils::ResMinMass[i] = (GL[i]->GetXaxis()->GetXmin());
+     // if( debug_>2 ) {
+      std::cout << "MuScleFitUtils::ResHalfWidth["<<i<<"] = " << MuScleFitUtils::ResHalfWidth[i] << std::endl;
+      std::cout << "MuScleFitUtils::ResMaxSigma["<<i<<"] = " << MuScleFitUtils::ResMaxSigma[i] << std::endl;
+      // }
+    }
   }
-  // if( debug_>2 ) {
-  for( int i=0; i<6; ++i ) {
-    std::cout << "MuScleFitUtils::ResMass["<<i<<"] = " << MuScleFitUtils::ResMass[i] << std::endl;
-    std::cout << "MuScleFitUtils::ResHalfWidth["<<i<<"] = " << MuScleFitUtils::ResHalfWidth[i] << std::endl;
-    std::cout << "MuScleFitUtils::ResMaxSigma["<<i<<"] = " << MuScleFitUtils::ResMaxSigma[i] << std::endl;
-  }
-  // }
 
   // Extract normalization for mass slice in Y bins of Z
   // ---------------------------------------------------
-  for (int iY=0; iY<24; iY++) {
-    int nBinsX = GLZ[iY]->GetNbinsX();
-    int nBinsY = GLZ[iY]->GetNbinsY();
-    if( nBinsX != MuScleFitUtils::nbins+1 || nBinsY != MuScleFitUtils::nbins+1 ) {
-      std::cout << "Error: for histogram \"" << GLZ[iY]->GetName() << "\" bins are not " << MuScleFitUtils::nbins << std::endl;
-      std::cout<< "nBinsX = " << nBinsX << ", nBinsY = " << nBinsY << std::endl;
-      exit(1);
-    }
-    for (int iy=0; iy<=MuScleFitUtils::nbins; iy++) {
-      MuScleFitUtils::GLZNorm[iY][iy] = 0.;
-      for (int ix=0; ix<=MuScleFitUtils::nbins; ix++) {
-        MuScleFitUtils::GLZValue[iY][ix][iy] = GLZ[iY]->GetBinContent(ix+1, iy+1);
-        MuScleFitUtils::GLZNorm[iY][iy] += MuScleFitUtils::GLZValue[iY][ix][iy]*(2*MuScleFitUtils::ResHalfWidth[0])/MuScleFitUtils::nbins;
+  if(MuScleFitUtils::resfind[0] && theMuonType_!=2) {
+    for (int iY=0; iY<24; iY++) {
+      int nBinsX = GLZ[iY]->GetNbinsX();
+      int nBinsY = GLZ[iY]->GetNbinsY();
+      if( nBinsX != MuScleFitUtils::nbins+1 || nBinsY != MuScleFitUtils::nbins+1 ) {
+	std::cout << "Error: for histogram \"" << GLZ[iY]->GetName() << "\" bins are not " << MuScleFitUtils::nbins << std::endl;
+	std::cout<< "nBinsX = " << nBinsX << ", nBinsY = " << nBinsY << std::endl;
+	exit(1);
+      }
+      for (int iy=0; iy<=MuScleFitUtils::nbins; iy++) {
+	MuScleFitUtils::GLZNorm[iY][iy] = 0.;
+	for (int ix=0; ix<=MuScleFitUtils::nbins; ix++) {
+	  MuScleFitUtils::GLZValue[iY][ix][iy] = GLZ[iY]->GetBinContent(ix+1, iy+1);
+	  MuScleFitUtils::GLZNorm[iY][iy] += MuScleFitUtils::GLZValue[iY][ix][iy]*(2*MuScleFitUtils::ResHalfWidth[0])/MuScleFitUtils::nbins;
+	}
       }
     }
   }
+
+  if(MuScleFitUtils::resfind[0] && theMuonType_==2){
+      int nBinsX = GL[0]->GetNbinsX();
+      int nBinsY = GL[0]->GetNbinsY();
+      if( nBinsX != MuScleFitUtils::nbins+1 || nBinsY != MuScleFitUtils::nbins+1 ) {
+	std::cout << "Error: for histogram \"" << GL[0]->GetName() << "\" bins are not " << MuScleFitUtils::nbins << std::endl;
+	std::cout<< "nBinsX = " << nBinsX << ", nBinsY = " << nBinsY << std::endl;
+	exit(1);
+      }
+
+      for (int iy=0; iy<=MuScleFitUtils::nbins; iy++) {
+	MuScleFitUtils::GLNorm[0][iy] = 0.;
+	for (int ix=0; ix<=MuScleFitUtils::nbins; ix++) {
+	  MuScleFitUtils::GLValue[0][ix][iy] = GL[0]->GetBinContent(ix+1, iy+1);
+	  // N.B. approximation: we should compute the integral of the function used to compute the probability (linear
+	  // interpolation of the mass points). This computation could be troublesome because the points have a steep
+	  // variation near the mass peak and the normal integral is not precise in these conditions.
+	  // Furthermore it is slow.
+	  MuScleFitUtils::GLNorm[0][iy] += MuScleFitUtils::GLValue[0][ix][iy]*(2*MuScleFitUtils::ResHalfWidth[0])/MuScleFitUtils::nbins;
+	}
+      }
+    }  
   // Extract normalization for each mass slice
   // -----------------------------------------
-  for (int ires=0; ires<6; ires++) {
-    int nBinsX = GL[ires]->GetNbinsX();
-    int nBinsY = GL[ires]->GetNbinsY();
-    if( nBinsX != MuScleFitUtils::nbins+1 || nBinsY != MuScleFitUtils::nbins+1 ) {
-      std::cout << "Error: for histogram \"" << GL[ires]->GetName() << "\" bins are not " << MuScleFitUtils::nbins << std::endl;
-      std::cout<< "nBinsX = " << nBinsX << ", nBinsY = " << nBinsY << std::endl;
-      exit(1);
-    }
+  for (int ires=1; ires<6; ires++) {
+    if(MuScleFitUtils::resfind[ires]){
+      int nBinsX = GL[ires]->GetNbinsX();
+      int nBinsY = GL[ires]->GetNbinsY();
+      if( nBinsX != MuScleFitUtils::nbins+1 || nBinsY != MuScleFitUtils::nbins+1 ) {
+	std::cout << "Error: for histogram \"" << GL[ires]->GetName() << "\" bins are not " << MuScleFitUtils::nbins << std::endl;
+	std::cout<< "nBinsX = " << nBinsX << ", nBinsY = " << nBinsY << std::endl;
+	exit(1);
+      }
 
-    for (int iy=0; iy<=MuScleFitUtils::nbins; iy++) {
-      MuScleFitUtils::GLNorm[ires][iy] = 0.;
-      for (int ix=0; ix<=MuScleFitUtils::nbins; ix++) {
-        MuScleFitUtils::GLValue[ires][ix][iy] = GL[ires]->GetBinContent(ix+1, iy+1);
-        // N.B. approximation: we should compute the integral of the function used to compute the probability (linear
-        // interpolation of the mass points). This computation could be troublesome because the points have a steep
-        // variation near the mass peak and the normal integral is not precise in these conditions.
-        // Furthermore it is slow.
-        MuScleFitUtils::GLNorm[ires][iy] += MuScleFitUtils::GLValue[ires][ix][iy]*(2*MuScleFitUtils::ResHalfWidth[ires])/MuScleFitUtils::nbins;
+      for (int iy=0; iy<=MuScleFitUtils::nbins; iy++) {
+	MuScleFitUtils::GLNorm[ires][iy] = 0.;
+	for (int ix=0; ix<=MuScleFitUtils::nbins; ix++) {
+	  MuScleFitUtils::GLValue[ires][ix][iy] = GL[ires]->GetBinContent(ix+1, iy+1);
+	  // N.B. approximation: we should compute the integral of the function used to compute the probability (linear
+	  // interpolation of the mass points). This computation could be troublesome because the points have a steep
+	  // variation near the mass peak and the normal integral is not precise in these conditions.
+	  // Furthermore it is slow.
+	  MuScleFitUtils::GLNorm[ires][iy] += MuScleFitUtils::GLValue[ires][ix][iy]*(2*MuScleFitUtils::ResHalfWidth[ires])/MuScleFitUtils::nbins;
+	}
       }
     }
   }
   // Free all the memory for the probability histograms.
-  for ( int i=0; i<24; i++ ) {
-    delete GLZ[i];
+  if(MuScleFitUtils::resfind[0] && theMuonType_!=2) {
+    for ( int i=0; i<24; i++ ) {
+      delete GLZ[i];
+    }
   }
-  delete GL[0];
-  delete GL[1];
-  delete GL[2];
-  delete GL[3];
-  delete GL[4];
-  delete GL[5];
+  if(MuScleFitUtils::resfind[0] && theMuonType_==2)
+    delete GL[0];
+  for (int ires=1; ires<6; ires++) {
+    if(MuScleFitUtils::resfind[ires])
+      delete GL[ires];
+  }
   delete ProbsFile;
 }
 
