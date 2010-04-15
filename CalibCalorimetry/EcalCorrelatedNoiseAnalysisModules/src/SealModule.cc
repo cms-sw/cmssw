13bce498b8f993d4555f7b91af9b3b1a0b3016fa
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-15 20:47:57 +0000

    Don't use deprecated macros

--- CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/src/SealModule.cc
+++ CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/src/SealModule.cc
@@ -1,6 +1,5 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/interface/EcnaAnalyzer.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(EcnaAnalyzer);
+DEFINE_FWK_MODULE(EcnaAnalyzer);
 
