034376869f261bb7e7a8e189dd3e06a2e4b78560
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-04-15 07:19:23 +0000

    Remove obsolete services

--- JetMETCorrections/Modules/python/L3AbsoluteCorrectionService_cfi.py
+++
@@ -1,7 +0,0 @@
-# Example cfi file for the L3Absolute correction service. 
-# It is used for the HLT confguration database.
-import FWCore.ParameterSet.Config as cms
-L3JetCorrectorIC5Calo = cms.ESSource("L3AbsoluteCorrectionService", 
-    tagName = cms.string('Summer08Redigi_L3Absolute_IC5Calo'),
-    label = cms.string('L3AbsoluteJetCorrectorIC5Calo')
-)
