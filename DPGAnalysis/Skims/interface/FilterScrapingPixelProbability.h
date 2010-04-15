e756b36fefcd956917f40567c3c81ffc4ff03b56
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-15 21:04:17 +0000

    Don't use deprecated framework header

--- DPGAnalysis/Skims/interface/FilterScrapingPixelProbability.h
+++ DPGAnalysis/Skims/interface/FilterScrapingPixelProbability.h
@@ -31,7 +31,7 @@
 #include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/TrackReco/interface/Track.h"
