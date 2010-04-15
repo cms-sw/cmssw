75011dbde5b02cf0b22d64ae3cbc946b606593d9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-15 21:11:58 +0000

    Whoops. Previous change touched the wrong line

--- DPGAnalysis/Skims/interface/FilterScrapingPixelProbability.h
+++ DPGAnalysis/Skims/interface/FilterScrapingPixelProbability.h
@@ -26,12 +26,12 @@
 #include <set>
 
 // user include files
-#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Utilities/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/TrackReco/interface/Track.h"
