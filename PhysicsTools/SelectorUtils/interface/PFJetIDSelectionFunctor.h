0cbc72b314cf2abbf359090a0b190fb0c948763a
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-04-15 19:17:10 +0000

    Updated to Nikis latest prescription for PF Jet ID

--- PhysicsTools/SelectorUtils/interface/PFJetIDSelectionFunctor.h
+++ PhysicsTools/SelectorUtils/interface/PFJetIDSelectionFunctor.h
@@ -13,7 +13,7 @@
   for a general overview of the selectors. 
 
   \author Salvatore Rappoccio
-  \version  $Id: PFJetIDSelectionFunctor.h,v 1.3 2010/03/25 03:35:21 srappocc Exp $
+  \version  $Id: PFJetIDSelectionFunctor.h,v 1.4 2010/03/31 15:37:55 hegner Exp $
 */
 
 
@@ -51,18 +51,37 @@ class PFJetIDSelectionFunctor : public Selector<pat::Jet>  {
     push_back("NHF" );
     push_back("CEF" );
     push_back("NEF" );
+    push_back("NCH" );
+    push_back("nConstituents");
 
-    // all on by default
-    set("CHF", params.getParameter<double>("CHF") );
-    set("NHF", params.getParameter<double>("NHF") );
-    set("CEF", params.getParameter<double>("CEF") );
-    set("NEF", params.getParameter<double>("NEF") );
 
+    // Set some default cuts for LOOSE, TIGHT
     if ( quality_ == LOOSE ) {
-      set("NHF", false );
-      set("NEF", false );
-      set("CEF", false );
+      set("CHF", 0.0);
+      set("NHF", 1.0);
+      set("CEF", 1.0);
+      set("NEF", 1.0);
+      set("NCH", 0);
+      set("nConstituents", 0);
+    } else if ( quality_ == TIGHT ) {
+      set("CHF", 0.0);
+      set("NHF", 0.9);
+      set("CEF", 1.0);
+      set("NEF", 0.9);
+      set("NCH", 0);
+      set("nConstituents", 0);      
     }
+
+
+    // Now check the configuration to see if the user changed anything
+    if ( params.exists("CHF") ) set("CHF", params.getParameter<double>("CHF") );
+    if ( params.exists("NHF") ) set("NHF", params.getParameter<double>("NHF") );
+    if ( params.exists("CEF") ) set("CEF", params.getParameter<double>("CEF") );
+    if ( params.exists("NEF") ) set("NEF", params.getParameter<double>("NEF") );
+    if ( params.exists("NCH") ) set("NCH", params.getParameter<int>   ("NCH") );
+    if ( params.exists("nConstuents") ) set("nConstituents", params.getParameter<int> ("nConstituents") );
+
+    retInternal_ = getBitTemplate();
     
   }
 
@@ -76,6 +95,7 @@ class PFJetIDSelectionFunctor : public Selector<pat::Jet>  {
       return false;
     }
   }
+  using Selector<pat::Jet>::operator();
 
   // 
   // Accessor from *CORRECTED* 4-vector, EMF, and Jet ID. 
@@ -102,6 +122,8 @@ class PFJetIDSelectionFunctor : public Selector<pat::Jet>  {
     double nhf = 0.0;
     double cef = 0.0;
     double nef = 0.0;
+    int    nch = 0;
+    int    nconstituents = 0;
 
     // Have to do this because pat::Jet inherits from reco::Jet but not reco::PFJet
     reco::PFJet const * pfJet = dynamic_cast<reco::PFJet const *>(&jet);
@@ -112,28 +134,26 @@ class PFJetIDSelectionFunctor : public Selector<pat::Jet>  {
       nhf = patJet->neutralHadronEnergyFraction();
       cef = patJet->chargedEmEnergyFraction();
       nef = patJet->neutralEmEnergyFraction();
+      nch = patJet->chargedMultiplicity();
+      nconstituents = patJet->numberOfDaughters();
     } else if ( pfJet != 0 ) {
       chf = pfJet->chargedHadronEnergyFraction();
       nhf = pfJet->neutralHadronEnergyFraction();
       cef = pfJet->chargedEmEnergyFraction();
       nef = pfJet->neutralEmEnergyFraction();
+      nch = pfJet->chargedMultiplicity();
+      nconstituents = pfJet->numberOfDaughters();
     }
 
-    if ( ignoreCut("CHF") || (std::abs(jet.eta()) <= 2.4 && chf > cut("CHF", double())) || (std::abs(jet.eta()) > 2.4) ) {
-      passCut( ret, "CHF");
 
-      if ( ignoreCut("NHF") || nhf < cut("NHF", double()) ) {
-	passCut( ret, "NHF");
-      }
-
-      if ( ignoreCut("CEF") || cef < cut("CEF", double()) ) {
-	passCut( ret, "CEF");
-      }
-
-      if ( ignoreCut("NEF") || nef < cut("NEF", double()) ) {
-	passCut( ret, "NEF");
-      }
-    }
+    // Cuts for all |eta|:
+    if ( ignoreCut("nConstituents") || nconstituents > cut("nConstituents", int() ) ) passCut( ret, "nConstituents");
+    if ( ignoreCut("NEF")           || ( nef < cut("NEF", double()) ) ) passCut( ret, "NEF");
+    if ( ignoreCut("NHF")           || ( nhf < cut("NHF", double()) ) ) passCut( ret, "NHF");    
+    // Cuts for |eta| < 2.4:
+    if ( ignoreCut("CEF")           || ( cef < cut("CEF", double()) || std::abs(jet.eta()) > 2.4 ) ) passCut( ret, "CEF");
+    if ( ignoreCut("CHF")           || ( chf > cut("CHF", double()) || std::abs(jet.eta()) > 2.4 ) ) passCut( ret, "CHF");
+    if ( ignoreCut("NCH")           || ( nch > cut("NCH", int())    || std::abs(jet.eta()) > 2.4 ) ) passCut( ret, "NCH");    
 
     setIgnored( ret );
     return (bool)ret;
