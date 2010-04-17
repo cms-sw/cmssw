0ef595d954046ad25afadbd5701192a97fe4f733
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-17 14:31:40 +0000

    Restore previous backout

--- DQMOffline/Trigger/interface/TopHLTDiMuonDQM.h
+++ DQMOffline/Trigger/interface/TopHLTDiMuonDQM.h
@@ -4,25 +4,25 @@
 /*
  *  DQM HLT Dimuon Test Client
  *
- *  $Date: 2010/01/12 16:29:21 $
- *  $Revision: 1.2 $
+ *  $Date: 2010/03/02 17:29:11 $
+ *  $Revision: 1.5 $
  *  \author  M. Vander Donckt CERN
  *   
  */
 
-#include <memory>
+//#include <memory>
 #include <string>
-#include <functional>
+#include <vector>
+//#include <functional>
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -34,20 +34,19 @@
 
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
-#include <DataFormats/Common/interface/Ref.h>
-
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/HLTReco/interface/TriggerObject.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+#include "DataFormats/Candidate/interface/Particle.h"
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
 #include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
 #include "DataFormats/RecoCandidate/interface/IsoDepositFwd.h"
-
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/MuonReco/interface/MuonFwd.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
 #include "DataFormats/TrajectorySeed/interface/TrajectorySeed.h"
-#include "DataFormats/MuonSeed/interface/L2MuonTrajectorySeed.h"
-#include "DataFormats/MuonSeed/interface/L2MuonTrajectorySeedCollection.h"
 #include "DataFormats/MuonSeed/interface/L3MuonTrajectorySeed.h"
 #include "DataFormats/MuonSeed/interface/L3MuonTrajectorySeedCollection.h"
 #include "DataFormats/L1Trigger/interface/L1MuonParticleFwd.h"
@@ -67,7 +66,7 @@ class TopHLTDiMuonDQM : public edm::EDAnalyzer {
 
  protected:   
 
-  void beginJob(void);
+  void beginJob();
   void beginRun(const edm::Run& r, const edm::EventSetup& c);
   void analyze(const edm::Event& e, const edm::EventSetup& c);
   void beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg, const edm::EventSetup& context);
@@ -89,6 +88,9 @@ class TopHLTDiMuonDQM : public edm::EDAnalyzer {
   float Eff[100];
 
   edm::InputTag triggerResults_;
+  edm::InputTag triggerEvent_;
+  edm::InputTag triggerFilter_;
+
   edm::InputTag L1_Collection_;
   edm::InputTag L3_Collection_;
   edm::InputTag L3_Isolation_;
@@ -98,6 +100,9 @@ class TopHLTDiMuonDQM : public edm::EDAnalyzer {
   std::vector<std::string> hltPaths_sig_;
   std::vector<std::string> hltPaths_trig_;
 
+  double vertex_X_cut_;
+  double vertex_Y_cut_;
+  double vertex_Z_cut_;
   double muon_pT_cut_;
   double muon_eta_cut_;
   double MassWindow_up_;
@@ -131,6 +136,8 @@ class TopHLTDiMuonDQM : public edm::EDAnalyzer {
   MonitorElement * MuonEfficiency_pT;
   MonitorElement * MuonEfficiency_eta;
   MonitorElement * TriggerEfficiencies;
+
+  //  typedef std::vector<reco::Particle> ParticleCollection;
 
 };
 
