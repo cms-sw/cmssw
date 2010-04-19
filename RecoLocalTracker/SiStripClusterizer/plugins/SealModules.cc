d61387ca3e9736ee7ae634a65ee611a8cf656228
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-04-19 21:48:46 +0000

    Temporary backout to get minimal tag

--- RecoLocalTracker/SiStripClusterizer/plugins/SealModules.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/SealModules.cc
@@ -1,5 +1,7 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterizer.h"
 DEFINE_FWK_MODULE(SiStripClusterizer);
+
+#include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterProducer.h"
+DEFINE_FWK_MODULE(SiStripClusterProducer);
