6fbc8f1194f14296713671666c8bbd6537a6d1c6
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2010-04-20 10:21:57 +0000

    Fix names of ic5Calo, ic5PF and ak5JPT jet collections

--- JetMETCorrections/Configuration/python/DefaultJEC_cff.py
+++ JetMETCorrections/Configuration/python/DefaultJEC_cff.py
@@ -14,7 +14,7 @@ ak5CaloJetsL2L3 = cms.EDProducer('CaloJetCorrectionProducer',
 ak7CaloJetsL2L3 = ak5CaloJetsL2L3.clone(src = 'ak7CaloJets', correctors = ['ak7CaloL2L3'])
 kt4CaloJetsL2L3 = ak5CaloJetsL2L3.clone(src = 'kt4CaloJets', correctors = ['kt4CaloL2L3'])
 kt6CaloJetsL2L3 = ak5CaloJetsL2L3.clone(src = 'kt6CaloJets', correctors = ['kt6CaloL2L3'])
-ic5CaloJetsL2L3 = ak5CaloJetsL2L3.clone(src = 'ic5CaloJets', correctors = ['ic5CaloL2L3'])
+ic5CaloJetsL2L3 = ak5CaloJetsL2L3.clone(src = 'iterativeCone5CaloJets', correctors = ['ic5CaloL2L3'])
 ##------------------  PF JETS -----------------------------------
 ak5PFJetsL2L3   = cms.EDProducer('PFJetCorrectionProducer',
      src        = cms.InputTag('ak5PFJets'),
@@ -24,14 +24,12 @@ ak5PFJetsL2L3   = cms.EDProducer('PFJetCorrectionProducer',
 ak7PFJetsL2L3   = ak5PFJetsL2L3.clone(src = 'ak7PFJets', correctors = ['ak7PFL2L3'])
 kt4PFJetsL2L3   = ak5PFJetsL2L3.clone(src = 'kt4PFJets', correctors = ['kt4PFL2L3'])
 kt6PFJetsL2L3   = ak5PFJetsL2L3.clone(src = 'kt6PFJets', correctors = ['kt6PFL2L3'])
-ic5PFJetsL2L3   = ak5PFJetsL2L3.clone(src = 'ic5PFJets', correctors = ['ic5PFL2L3'])
+ic5PFJetsL2L3   = ak5PFJetsL2L3.clone(src = 'iterativeCone5PFJets', correctors = ['ic5PFL2L3'])
 ##------------------  JPT JETS ----------------------------------
 ak5JPTJetsL2L3  = cms.EDProducer('JPTJetCorrectionProducer',
-    src         = cms.InputTag('ak5JPTJets'),
+    src         = cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
     correctors  = cms.vstring('ak5JPTL2L3')
     )
-
-ic5JPTJetsL2L3  = ak5JPTJetsL2L3.clone(src = 'ic5JPTJets', correctors = ['ic5JPTL2L3'])
 ##------------------  TRACK JETS --------------------------------
 ak5TrackJetsL2L3  = cms.EDProducer('TrackJetCorrectionProducer',
     src         = cms.InputTag('ak5TrackJets'),
