3a7fcd6b66243ffb9415a1ed9685d0b992412168
Annapaola De Cosa <sha1-4f7f601e1310d1f94a8ce176474325c5ff77ac0c@cern.ch>
2010-04-21 13:32:07 +0000

    working on data

--- ElectroWeakAnalysis/Skimming/python/zMuMu_SubskimPaths_cff.py
+++ ElectroWeakAnalysis/Skimming/python/zMuMu_SubskimPaths_cff.py
@@ -2,28 +2,40 @@ import FWCore.ParameterSet.Config as cms
 
 from ElectroWeakAnalysis.Skimming.dimuonsHLTFilter_cfi import *
 from ElectroWeakAnalysis.Skimming.patCandidatesForZMuMuSubskim_cff import *
+from ElectroWeakAnalysis.ZMuMu.zMuMuMuonUserData import *
 from ElectroWeakAnalysis.Skimming.dimuons_cfi import *
+dimuons.decay = cms.string('userDataMuons@+ userDataMuons@-')
 from ElectroWeakAnalysis.Skimming.dimuonsOneTrack_cfi import *
+dimuonsOneTrack.decay = cms.string('userDataMuons@+ userDataTracks@-') 
 from ElectroWeakAnalysis.Skimming.dimuonsGlobal_cfi import *
+dimuonsGlobal.src = cms.InputTag("userDataDimuons")
 from ElectroWeakAnalysis.Skimming.dimuonsOneStandAloneMuon_cfi import *
+dimuonsOneStandAloneMuon.src = cms.InputTag("userDataDimuons") 
 from ElectroWeakAnalysis.Skimming.dimuonsOneTrackerMuon_cfi import *
+dimuonsOneTrackerMuon.src = cms.InputTag("userDataDimuons")
 from ElectroWeakAnalysis.Skimming.dimuonsFilter_cfi import *
+dimuonsFilter.src = cms.InputTag("userDataDimuons") 
 from ElectroWeakAnalysis.Skimming.dimuonsOneTrackFilter_cfi import *
+dimuonsOneTrackFilter.src = cms.InputTag("userDataDimuonsOneTrack")
 
-dimuonsPath = cms.Path(
-    dimuonsHLTFilter *
-    goodMuonRecoForDimuon *
-    dimuons *
-    dimuonsGlobal *
-    dimuonsOneStandAloneMuon *
-    dimuonsOneTrackerMuon *
-    dimuonsFilter    
-)
+dimuonsPath = cms.Path(dimuonsHLTFilter *
+                       goodMuonRecoForDimuon *
+                       userDataMuons*
+                       dimuons *
+                       userDataDimuons*
+                       dimuonsGlobal *
+                       dimuonsOneStandAloneMuon *
+                       dimuonsOneTrackerMuon *
+                       dimuonsFilter    
+                       )
 
-dimuonsOneTrackPath = cms.Path(dimuonsHLTFilter+
-                               goodMuonRecoForDimuon+
-                               dimuonsOneTrack+
+dimuonsOneTrackPath = cms.Path(dimuonsHLTFilter *
+                               goodMuonRecoForDimuon*
+                               userDataMuons*
+                               userDataTracks*
+                               dimuonsOneTrack*
+                               userDataDimuonsOneTrack*
                                dimuonsOneTrackFilter
-)
+                               )
 
 
