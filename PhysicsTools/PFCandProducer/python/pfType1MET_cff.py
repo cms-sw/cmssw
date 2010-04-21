739a303853ce7a6157c15a47f07f7453b61ecdf3
Florent Sylvain Lacroix <sha1-5409e8c75bd2848b166bf5d20fd255bdb07f3883@cern.ch>
2010-04-21 14:38:54 +0000

    Use official MET producer

--- PhysicsTools/PFCandProducer/python/pfType1MET_cff.py
+++ PhysicsTools/PFCandProducer/python/pfType1MET_cff.py
@@ -4,7 +4,7 @@ from PhysicsTools.PFCandProducer.pfMET_cfi  import *
 from PhysicsTools.PFCandProducer.pfType1MET_cfi  import *
 
 pfRawMET = pfMET.clone()
-pfRawMET.hfCalibFactor = 1.
+#pfRawMET.hfCalibFactor = 1.
 
 pfType1MET.inputUncorMetLabel = 'pfRawMET'
 
