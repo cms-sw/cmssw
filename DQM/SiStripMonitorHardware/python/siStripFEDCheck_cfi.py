1fe703a5bba2b6672f69e6e4b44e9584cee225ad
Anne-Marie Magnan <sha1-4ead6f5ca8b64f8c6a1efe9d1d4dd2e2421521d1@cern.ch>
2010-04-21 10:40:25 +0000

    Revert to revision 1.5 with only rawDataTag as tracked parameter

--- DQM/SiStripMonitorHardware/python/siStripFEDCheck_cfi.py
+++ DQM/SiStripMonitorHardware/python/siStripFEDCheck_cfi.py
@@ -2,25 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 siStripFEDCheck = cms.EDAnalyzer("SiStripFEDCheckPlugin",
   #Directory to book histograms in
-  DirName = cms.string('SiStrip/FEDIntegrity/'),
+  DirName = cms.untracked.string('SiStrip/FEDIntegrity/'),
   #Raw data collection
   RawDataTag = cms.InputTag('source'),
   #Number of events to cache info before updating histograms
   #(set to zero to disable cache)
-  #HistogramUpdateFrequency = cms.uint32(0),
-  HistogramUpdateFrequency = cms.uint32(1000),
+  #HistogramUpdateFrequency = cms.untracked.uint32(0),
+  HistogramUpdateFrequency = cms.untracked.uint32(1000),
   #Print info about errors buffer dumps to LogInfo(SiStripFEDCheck)
-  PrintDebugMessages = cms.bool(False),
+  PrintDebugMessages = cms.untracked.bool(False),
   #Write the DQM store to a file (DQMStore.root) at the end of the run
-  WriteDQMStore = cms.bool(False),
+  WriteDQMStore = cms.untracked.bool(False),
   #Use to disable all payload (non-fatal) checks
-  DoPayloadChecks = cms.bool(True),
+  DoPayloadChecks = cms.untracked.bool(True),
   #Use to disable check on channel lengths
-  CheckChannelLengths = cms.bool(True),
+  CheckChannelLengths = cms.untracked.bool(True),
   #Use to disable check on channel packet codes
-  CheckChannelPacketCodes = cms.bool(True),
+  CheckChannelPacketCodes = cms.untracked.bool(True),
   #Use to disable check on FE unit lengths in full debug header
-  CheckFELengths = cms.bool(True),
+  CheckFELengths = cms.untracked.bool(True),
   #Use to disable check on channel status bits
-  CheckChannelStatus = cms.bool(True),
+  CheckChannelStatus = cms.untracked.bool(True),
 )
