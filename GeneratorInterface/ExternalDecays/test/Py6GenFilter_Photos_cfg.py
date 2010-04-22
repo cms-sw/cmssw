94c88466597880b32919767d5122a13cd207a2f4
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2010-04-22 21:07:44 +0000

    restore settings

--- GeneratorInterface/ExternalDecays/test/Py6GenFilter_Photos_cfg.py
+++ GeneratorInterface/ExternalDecays/test/Py6GenFilter_Photos_cfg.py
@@ -40,10 +40,10 @@ process.generator = cms.EDFilter("Pythia6GeneratorFilter",
     ## --> displayPythiaCards = cms.untracked.bool(True),
     comEnergy = cms.double(7000.0),
 
-#    ExternalDecays = cms.PSet(
-#        Photos = cms.untracked.PSet(),
-#        parameterSets = cms.vstring( "Photos" )
-#    ),
+    ExternalDecays = cms.PSet(
+        Photos = cms.untracked.PSet(),
+        parameterSets = cms.vstring( "Photos" )
+    ),
 
     PythiaParameters = cms.PSet(
 
@@ -64,7 +64,7 @@ process.generator = cms.EDFilter("Pythia6GeneratorFilter",
             'MSTP(61)=0              ! no initial-state showers', 
             'MSTP(71)=0              ! no final-state showers', 
             'MSTP(81)=0              ! no multiple interactions', 
-            ###'MSTP(111)=0             ! no hadronization', 
+            'MSTP(111)=0             ! no hadronization', 
             'MSTU(21)=1              !Check on possible errors during program execution', 
             'MSUB(102)=1             !ggH', 
             'MSUB(123)=1             !ZZ fusion to H', 
