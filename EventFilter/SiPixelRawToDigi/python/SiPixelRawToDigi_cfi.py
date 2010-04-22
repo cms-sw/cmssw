120340df7f1c9f09b68b41892be4b95a5b729c2b
Andrew York <sha1-3a5714195ac460c33b89c67d050e2d8afc5fe72c@cern.ch>
2010-04-22 21:43:27 +0000

    changed cabling map to transient, changed cfi to look at errors by default

--- EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
+++ EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 siPixelDigis = cms.EDProducer("SiPixelRawToDigi",
     Timing = cms.untracked.bool(False),
-    IncludeErrors = cms.bool(False),
+    IncludeErrors = cms.bool(True),
     InputLabel = cms.InputTag("siPixelRawData"),
     CheckPixelOrder = cms.bool(False),
     UseCablingTree = cms.untracked.bool(True)
