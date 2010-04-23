d23c28d730757d9d9cfc35f8a3597a8cc9794c3a
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2010-04-23 21:41:51 +0000

    bsolete, now that we have pretriggers in the event

--- SimMuon/CSCDigitizer/python/simMuonCSCSuppressedDigis_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from SimMuon.CSCDigitizer.muonCSCDigis_cfi import simMuonCSCDigis
-
-simMuonCSCSuppressedDigis = cms.EDProducer("CSCDigiSuppressor",
-    simMuonCSCDigis.strips,
-    digiLabel = cms.string("simMuonCSCDigis"),
-    lctTag = cms.InputTag("simCscTriggerPrimitiveDigis")
-)
-
