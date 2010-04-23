0b8b058385d1190999aa84b481b4d0d22775ec91
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2010-04-23 16:39:59 +0000

    the constants should be taken from DB from now on

--- RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHitFit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHitFit_cfi.py
@@ -5,9 +5,5 @@ ecalPreshowerRecHitFit = cms.EDProducer("ESRecHitProducer",
                                         ESrechitCollection = cms.string('EcalRecHitsESFit'),
                                         ESdigiCollection = cms.InputTag("ecalPreshowerDigis"),
                                         algo = cms.string("ESRecHitWorker"),
-                                        ESRecoAlgo = cms.untracked.int32(1),
-                                        ESGain = cms.int32(2),
-                                        ESBaseline = cms.int32(0),
-                                        ESMIPADC = cms.double(55),
-                                        ESMIPkeV = cms.double(81.08)
+                                        ESRecoAlgo = cms.untracked.int32(1)
                                         )
--- RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalPreshowerRecHit_cfi.py
@@ -5,6 +5,5 @@ ecalPreshowerRecHit = cms.EDProducer("ESRecHitProducer",
                                      ESrechitCollection = cms.string('EcalRecHitsES'),
                                      ESdigiCollection = cms.InputTag("ecalPreshowerDigis"),
                                      algo = cms.string("ESRecHitWorker"),
-                                     ESRecoAlgo = cms.untracked.int32(0),
-                                     ESWeights = cms.untracked.vdouble(0.0, 0.725, 0.4525)
+                                     ESRecoAlgo = cms.untracked.int32(0)
 )
