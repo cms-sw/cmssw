60317a9f8545642fb5f42f9f39411b528d06d1b2
Jie Chen <sha1-fd9183ca9be87d876e001fd5ccf1f012da0983ea@cern.ch>
2010-04-23 16:12:33 +0000

    backport to v1.1

--- SUSYBSMAnalysis/Skimming/python/EXOHT_cff.py
+++ SUSYBSMAnalysis/Skimming/python/EXOHT_cff.py
@@ -1,40 +1,37 @@
+
+
 import FWCore.ParameterSet.Config as cms
 
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 exoticaHTHLT = hltHighLevel
 #Define the HLT path to be used.
-#exoticaHTHLT.HLTPaths =['HLT_L1Jet6U']
-exoticaHTHLT.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
+exoticaHTHLT.HLTPaths =['HLT_HT100U']
+exoticaHTHLT.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT8E29")
 
 #Define the HLT quality cut 
 
 exoticaHLTHTFilter = cms.EDFilter("HLTSummaryFilter",
-    summary = cms.InputTag("hltTriggerSummaryAOD","","HLT"),
-    member  = cms.InputTag("hltJet15UHt","","HLT"),				
+    summary = cms.InputTag("hltTriggerSummaryAOD","","HLT8E29"),
+    member  = cms.InputTag("hltJet15UHt","","HLT8E29"),				
     cut     = cms.string("pt>250"),                     
     minN    = cms.int32(1)                  
 )
                                
-# Define the Reco quality cut using L2L3CorJetAK5Calo
-from JetMETCorrections.Configuration.L2L3Corrections_Summer09_7TeV_ReReco332_cff import *
-#from JetMETCorrections.Configuration.L2L3Corrections_900GeV_cff import *
-exoticaRecoHT = cms.EDProducer("METProducer",
-   src = cms.InputTag('L2L3CorJetAK5Calo'),
-   METType = cms.string('MET'),
-   alias = cms.string('EXOHT'),
-   noHF = cms.bool(False),
-   globalThreshold = cms.double(30.0),
-   InputType = cms.string('CaloJetCollection')
-)
-
+#Define the Reco quality cut
 exoticaRecoHTFilter = cms.EDFilter("HLTGlobalSumsMET",
-   inputTag = cms.InputTag("exoticaRecoHT"),
-   saveTag = cms.untracked.bool( True ),                             
-   observable = cms.string( "sumEt" ),                           
-   Min = cms.double(150.0),  
-   Max = cms.double( -1.0 ),                             
-   MinN = cms.int32(1)
+    inputTag = cms.InputTag("htMetKT4"),
+	saveTag = cms.untracked.bool( True ),							  
+	observable = cms.string( "sumEt" ),							  
+    Min = cms.double(250.0),  
+	Max = cms.double( -1.0 ),							  
+    MinN = cms.int32(1)
 )
+## exoticaRecoHTFilter = cms.EDFilter("PATMETSelector",
+##      src = cms.InputTag("htMetKT4"),
+##      cut = cms.string("sumEt > 250.0"),
+##      filter = cms.bool(True)
+##  )
+
 
 #Define group sequence, using HLT bits + either HLT/Reco quality cut. 
 exoticaHTHLTQualitySeq = cms.Sequence(
@@ -42,6 +39,5 @@ exoticaHTHLTQualitySeq = cms.Sequence(
    
 )
 exoticaHTRecoQualitySeq = cms.Sequence(
-	exoticaHTHLT + L2L3CorJetAK5Calo + exoticaRecoHT + exoticaRecoHTFilter
+	exoticaHTHLT+exoticaRecoHTFilter
 )
-
