1c46e31a9d14054622299f2db182ee9f028f2d9e
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-04-26 20:42:28 +0000

    roll back to not using the reco_TLR for 360

--- Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_36X.py
+++ Configuration/GlobalRuns/python/rereco_FirstCollisions_MinimumBias_36X.py
@@ -274,13 +274,89 @@ process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,proces
 # Automatic addition of the customisation function
 
 def customise(process):
-    from Configuration.GlobalRuns.reco_TLR_36X import customisePPData
-    process=customisePPData(process)
-    return process
+    
+    #####################################################################################################
+    ####
+    ####  Top level replaces for handling strange scenarios of early collisions
+    ####
 
+    ## TRACKING:
+    ## Skip events with HV off
+    process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+    process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+    process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+    process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
+    process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
 
+    ###### FIXES TRIPLETS FOR LARGE BS DISPLACEMENT ######
 
+    ### prevent bias in pixel vertex
+    process.pixelVertices.useBeamConstraint = False
+    
+    ### pixelTracks
+    #---- new parameters ----
+    process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
+    
+    ### 0th step of iterative tracking
+    #---- replaces ----
+    process.newSeedFromTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
+    #---- new parameters ----
+    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)  # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
+    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
 
+    ### 2nd step of iterative tracking
+    #---- replaces ----
+    process.secTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
+    #---- new parameters ----
+    process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)  # was originHalfLength = 17.5; translated assuming sigmaZ ~ 3.8
+    process.secTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
+
+    ## Primary Vertex
+    process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelLayersWithHits = 1
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconLayersWithHits = 6
+    process.offlinePrimaryVerticesWithBS.TkClusParameters.TkGapClusParameters.zSeparation = 1
+    process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
+    process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
+    process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
+    process.offlinePrimaryVertices.TkFilterParameters.minPixelLayersWithHits = 1
+    process.offlinePrimaryVertices.TkFilterParameters.minSiliconLayersWithHits = 6
+    process.offlinePrimaryVertices.TkClusParameters.TkGapClusParameters.zSeparation = 1
+
+    ## ECAL 
+    process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 78, 142 ]
+
+    ##Preshower
+    process.ecalPreshowerRecHit.ESBaseline = 0
+
+    ##Preshower algo for data is different than for MC
+    process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+
+    ## HCAL temporary fixes
+    process.hfreco.firstSample  = 3
+    process.hfreco.samplesToAdd = 4
+    
+    ## EGAMMA
+    process.gsfElectrons.applyPreselection = cms.bool(False)
+    process.photons.minSCEtBarrel = 2.
+    process.photons.minSCEtEndcap =2.
+    process.photonCore.minSCEt = 2.
+    process.conversionTrackCandidates.minSCEt =1.
+    process.conversions.minSCEt =1.
+    process.trackerOnlyConversions.AllowTrackBC = cms.bool(False)
+    process.trackerOnlyConversions.AllowRightBC = cms.bool(False)
+    process.trackerOnlyConversions.MinApproach = cms.double(-.25)
+    process.trackerOnlyConversions.DeltaCotTheta = cms.double(.07)
+    process.trackerOnlyConversions.DeltaPhi = cms.double(.2)
+    
+    ###
+    ###  end of top level replacements
+    ###
+    ###############################################################################################
+
+    return (process)
 
 
 # End of customisation function definition
