05bd295f86d10cbd453bee74972ee0ad560496b5
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2010-04-26 19:48:34 +0000

    update config files using TSGs_cff

--- RecoMuon/TrackerSeedGenerator/python/TSGFromL2_cfi.py
+++ RecoMuon/TrackerSeedGenerator/python/TSGFromL2_cfi.py
@@ -5,17 +5,18 @@ from RecoMuon.TrackingTools.MuonServiceProxy_cff import *
 from RecoMuon.TrackingTools.MuonErrorMatrixValues_cff import *
 from RecoMuon.TrackerSeedGenerator.TrackerSeedCleaner_cff import *
 from RecoMuon.TrackerSeedGenerator.TSGs_cff import *
+
 hltL3TrajectorySeedFromL2 = cms.EDProducer("TSGFromL2Muon",
     MuonTrackingRegionCommon,
-    #defines a bunch of PSet with name the name of the Seed generator
-    # to redefine do
-    # replace hltL3TrajectorySeedFromL2.TSGFromPixelPairs.TTRHBuilder = ""
-#    TSGsBlock,
     TrackerSeedCleanerCommon,
-    #ServiceParameters
+    # ServiceParameters
     MuonServiceProxy,
-#    tkSeedGenerator = cms.string('TSGFromCombinedHits'),
+    # MuonTrackingRegionBuilder and  TrackerSeedCleaner should be empty for TSGForRoadSearchOI
+    # MuonTrackingRegionBuilder should be empty for TSGFromPropagation
+    MuonTrackingRegionBuilder = cms.PSet(),
+    TrackerSeedCleaner = cms.PSet(),
     TkSeedGenerator = TSGsBlock.TSGForRoadSearchOI,
+    
     MuonCollectionLabel = cms.InputTag("hltL2Muons","UpdatedAtVtx"),
     #this should not exist there !
     PtCut = cms.double(1.0),
