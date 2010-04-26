c79d397b2658f535265af2f46c7a96d2e93adbef
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-04-26 19:30:05 +0000

    Update GTs for 37X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
-autoCond = { 'mc'      : 'MC_37Y_V0::All',
-             'startup' : 'START37_V0::All',
+autoCond = { 'mc'      : 'MC_37Y_V1::All',
+             'startup' : 'START37_V1::All',
              'craft08' : 'CRFT8_37R_V0::All',
-             'craft09' : 'CRFT9_37R_V0::All',
-             'com10'   : 'GR_R_37X_V0::All'
+             'craft09' : 'CRFT9_37R_V1::All',
+             'com10'   : 'GR_R_37X_V1::All'
              }
