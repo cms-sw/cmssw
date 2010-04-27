03897a3105e1c30e921bac91fe8d45e76b9b678e
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-04-27 13:37:34 +0000

    add playback variable

--- SimGeneral/MixingModule/python/HiEventMixing_cff.py
+++ SimGeneral/MixingModule/python/HiEventMixing_cff.py
@@ -10,6 +10,7 @@ eventEmbeddingSourceParameters = cms.PSet(
 )
 eventEmbeddingMixParameters = cms.PSet(
     LabelPlayback = cms.string(''),
+    playback = cms.bool(False),
     maxBunch = cms.int32(0),
     minBunch = cms.int32(0),
     Label = cms.string(''),
