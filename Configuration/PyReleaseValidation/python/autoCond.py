91c20849227c75c2e237c396d9baf79d51ff51b5
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-04-28 20:57:27 +0000

    Fix the STARTUP GT for 37X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,7 +1,7 @@
 
 
 autoCond = { 'mc'      : 'MC_37Y_V1::All',
-             'startup' : 'START37_V1::All',
+             'startup' : 'START37_V1A::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_37R_V1::All',
              'com10'   : 'GR_R_37X_V1::All'
