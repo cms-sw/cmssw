27f7e70010d197eeaed2361ffba4aa03b518cbb4
Zoltan Gecse <sha1-a0fcc5ded3c110ede739e8012ee7070ef7d0aea5@cern.ch>
2010-04-28 19:49:57 +0000

    added filtering capability

--- PhysicsTools/TagAndProbe/plugins/ProbeTreeProducer.cc
+++ PhysicsTools/TagAndProbe/plugins/ProbeTreeProducer.cc
@@ -14,7 +14,7 @@
 #include <memory>
 #include <ctype.h>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -25,20 +25,23 @@
 #include <set>
 #include "FWCore/ParameterSet/interface/Registry.h"
 
-class ProbeTreeProducer : public edm::EDAnalyzer {
+class ProbeTreeProducer : public edm::EDFilter {
   public:
     explicit ProbeTreeProducer(const edm::ParameterSet&);
     ~ProbeTreeProducer();
 
   private:
-    virtual void analyze(const edm::Event&, const edm::EventSetup&);
-    virtual void endJob() ;
+    virtual bool filter(edm::Event&, const edm::EventSetup&);
+    virtual void endJob();
 
     /// InputTag to the collection of all probes
     edm::InputTag probesTag_;
 
     /// The selector object
     StringCutObjectSelector<reco::Candidate, true> cut_;
+
+    /// Specifies whether this module should filter
+    bool filter_;
 
     /// The object that actually computes variables and fills the tree for the probe
     std::auto_ptr<tnp::BaseTreeFiller> probeFiller_;
@@ -47,6 +50,7 @@ class ProbeTreeProducer : public edm::EDAnalyzer {
 ProbeTreeProducer::ProbeTreeProducer(const edm::ParameterSet& iConfig) :
   probesTag_(iConfig.getParameter<edm::InputTag>("src")),
   cut_(iConfig.existsAs<std::string>("cut") ? iConfig.getParameter<std::string>("cut") : ""),
+  filter_(iConfig.existsAs<bool>("filter") ? iConfig.getParameter<bool>("filter") : false),
   probeFiller_(new tnp::BaseTreeFiller("probe_tree", iConfig))
 {
 }
@@ -54,7 +58,8 @@ ProbeTreeProducer::ProbeTreeProducer(const edm::ParameterSet& iConfig) :
 ProbeTreeProducer::~ProbeTreeProducer(){
 }
 
-void ProbeTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+bool ProbeTreeProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup){
+  bool result = !filter_;
   edm::Handle<reco::CandidateView> probes;
   iEvent.getByLabel(probesTag_, probes);
   probeFiller_->init(iEvent);
@@ -62,8 +67,10 @@ void ProbeTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup&
     const reco::CandidateBaseRef &probe = probes->refAt(i);
     if(cut_(*probe)){
       probeFiller_->fill(probe);
+      result = true;
     }
   }
+  return result;
 }
 
 void ProbeTreeProducer::endJob(){
