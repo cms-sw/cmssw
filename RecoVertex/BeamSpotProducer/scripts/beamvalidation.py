85d8ab2fe7c882c4bd599dc8457025e3495b33b5
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2010-04-28 15:16:52 +0000

    update

--- RecoVertex/BeamSpotProducer/scripts/beamvalidation.py
+++ RecoVertex/BeamSpotProducer/scripts/beamvalidation.py
@@ -74,16 +74,30 @@ def parse(docstring, arglist=None):
 
 def cmp_tags(a,b):
 
-    tmpa = a.replace("BeamSpotObjects_2009_v","")
-    tmpa = tmpa.replace("_offline","")
-    tmpb = b.replace("BeamSpotObjects_2009_v","")
-    tmpb = tmpb.replace("_offline","")
+    yeara = int(a.split('_')[1])
+    yearb = int(b.split('_')[1])
+
+    if yeara < yearb: return -1
+    if yeara > yearb: return 1
     
+    suffix = "_offline"
+    if a.find("_express") != -1:
+        suffix = "_express"
+    if a.find("_prompt") != -1:
+        suffix = "_prompt"
+
+    tmpa = a.replace("BeamSpotObjects_2009_v","")
+    tmpa = tmpa.replace(suffix,"")
+    
+    tmpb = b.replace("BeamSpotObjects_2009_v","")
+    tmpb = tmpb.replace(suffix,"")
+        
     na = int(tmpa)
     nb = int(tmpb)
     if na < nb: return -1
     if na == nb: return 0
     if na > nb: return 1
+    
 #___
 
 def dump_header(lines):
@@ -112,12 +126,22 @@ def write_tags(tags, lines):
 
     end = '\n'
     br = '<BR>'+end
+
+    lines.append('<tr>'+end)
+    for i in tags.keys():
         
-    for i in tags:
+        lines.append('<th>'+i)
+        lines.append('</th>'+end)
+    lines.append('</tr>'+end)
+
+    for ntags in range(0,len(tags['offline'])):
         lines.append('<tr>'+end)
-        lines.append('<td>'+end)
-        lines.append(i)
-        lines.append('</td>'+end)
+        for i in tags.keys():
+            alist = tags[i]
+            if ntags < len(tags[i]):
+                lines.append('<td> '+alist[ntags]+' </td>'+end)
+            else:
+                lines.append('<td> </td>')
         lines.append('</tr>'+end)
 
 #______________
@@ -125,26 +149,26 @@ def write_iovs(iovs, lines):
 
     end = '\n'
     br = '<BR>'+end
+
+    lines.append('<tr>'+end)
+    for i in iovs.keys():
         
-    for i in iovs:
+        lines.append('<th>'+i)
+        lines.append('</th>'+end)
+    lines.append('</tr>'+end)
+
+    for ntags in range(0,len(iovs[iovs.keys()[0]])):
         lines.append('<tr>'+end)
-        lines.append('<td>'+end)
-        lines.append(i[0] + " - " + i[1])
-        lines.append('</td>'+end)
+        for i in iovs.keys():
+            aIOVlist = iovs[i]
+            for iIOV in aIOVlist:
+                lines.append('<td> '+iIOV.IOVfirst +' - '+iIOV.IOVlast+' </td>'+end)
         lines.append('</tr>'+end)
 
-#______________________________
-if __name__ == '__main__':
+#______________
+def get_listoftags(dest, auth,):
 
-    
-    # COMMAND LINE OPTIONS
-    #################################
-    option,args = parse(__doc__)
-    if not args and not option: exit()
-
-    
-    ## Get the latest tags
-    queryTags_cmd = "cmscond_list_iov -c frontier://cmsfrontier.cern.ch:8000/Frontier/CMS_COND_31X_BEAMSPOT -P /afs/cern.ch/cms/DB/conddb -a | grep BeamSpotObjects"
+    queryTags_cmd = "cmscond_list_iov -c "+dest+" -P "+auth+" -a | grep BeamSpotObjects"
     
     outcmd = commands.getstatusoutput( queryTags_cmd )
     
@@ -162,36 +186,89 @@ if __name__ == '__main__':
     for itag in listtags:
         if itag[len(itag)-6:len(itag)] == "prompt":
             listtags_prompt.append(itag)
+
+    listtags_offline.sort( cmp = cmp_tags )
+    listtags_offline.reverse()
+    listtags_express.sort( cmp = cmp_tags )
+    listtags_express.reverse()
+    listtags_prompt.sort( cmp = cmp_tags )
+    listtags_prompt.reverse()
+
+    result = {}
+    result['offline'] = listtags_offline
+    result['express'] = listtags_express
+    result['prompt'] = listtags_prompt
+
+    return result
+
+#______________________
+class IOV:
+    def __init__(self):
+        self.type = "runnumber"
+        self.IOVfirst = '1'
+        self.IOVlast  = '1'
+
+#_________________
+def get_lastIOVs( listoftags, dest, auth ):
+
+    dbtags = ['offline','express','prompt']
+
+    results = {}
+    for itag in dbtags:
+        
+        lasttag = listoftags[itag][0]
     
-    listtags = listtags_offline        
-    listtags.sort( cmp = cmp_tags )
-    listtags.reverse()
-
-    #print listtags
-
-    # Get the latest IOVs
-    lasttag = listtags[0]
-
-    queryIOVs_cmd = "cmscond_list_iov -c frontier://cmsfrontier.cern.ch:8000/Frontier/CMS_COND_31X_BEAMSPOT -P /afs/cern.ch/cms/DB/conddb -t "+ lasttag
-
-    outcmd = commands.getstatusoutput( queryIOVs_cmd )
-
-    tmparr = outcmd[1].split('\n')
+        queryIOVs_cmd = "cmscond_list_iov -c "+dest+" -P "+auth+" -t "+ lasttag
+        print queryIOVs_cmd
+        
+        outcmd = commands.getstatusoutput( queryIOVs_cmd )
+        
+        tmparr = outcmd[1].split('\n')
     
-    TimeType = tmparr[1].split()[1]
-    listIOVs = []
+        TimeType = tmparr[1].split()[1]
+        listIOVs = []
 
-    # pick the last three IOVs
-    for i in range(0,3):
-	tmpline = tmparr[len(tmparr) -2 -i]
-	aIOV = []
-	aIOV.append( tmpline.split()[0] )
-	aIOV.append( tmpline.split()[1] )
-	
-	listIOVs.append( aIOV )
+        # look at number of payloads
+        lastline =  tmparr[len(tmparr)-1].split()
+        npayloads = int( lastline[len(lastline)-1] )
 
-    #print listIOVs
+        maxIOVs = 3
+        if npayloads < 3:
+            maxIOVs = npayloads
+        # pick the last three IOVs
+        for i in range(0,maxIOVs):
+            tmpline = tmparr[len(tmparr) -2 -i]
+            aIOV = IOV()
+            aIOV.IOVfirst = tmpline.split()[0] 
+            aIOV.IOVlast =  tmpline.split()[1] 
+            aIOV.type = TimeType
+            listIOVs.append( aIOV )
 
+        results[lasttag] = listIOVs
+
+    return results
+
+    
+#______________________________
+if __name__ == '__main__':
+
+    
+    # COMMAND LINE OPTIONS
+    #################################
+    option,args = parse(__doc__)
+    if not args and not option: exit()
+
+    
+    ## Get the latest tags
+    dest = "frontier://cmsfrontier.cern.ch:8000/Frontier/CMS_COND_31X_BEAMSPOT"
+    auth = "/afs/cern.ch/cms/DB/conddb"
+    #cmscond_list_iov -c oracle://cms_orcoff_prep/CMS_COND_BEAMSPOT -P /afs/cern.ch/cms/DB/conddb  -a
+    
+    list_tags = get_listoftags( dest, auth)
+        
+    # Get the latest IOVs from last tag
+    list_lastIOVs = get_lastIOVs( list_tags, dest, auth)
+    
     # create web page
     lines = []
     end = '\n'
@@ -199,23 +276,20 @@ if __name__ == '__main__':
     
     dump_header(lines)
 
-    lines.append('Latest IOVs for tag:'+listtags[0]+end)
+    lines.append('Latest IOVs: '+end)
     lines.append(br)
     lines.append('''
 <table border="1">
 ''')
-    write_iovs( listIOVs, lines )
+    write_iovs( list_lastIOVs, lines )
     lines.append('</table>'+end)
     
     lines.append('Latest tags:'+end)
     lines.append(br)
     lines.append('''
 <table border="1">
-<tr>
-<th> offline </th>
-</tr>
 ''')
-    write_tags( listtags, lines)
+    write_tags( list_tags, lines)
     lines.append('</table>'+end)
 
     dump_footer(lines)
