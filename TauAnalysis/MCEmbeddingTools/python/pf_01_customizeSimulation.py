77a06c5d2fe25169db5527ab596cab838568bead
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2010-04-29 16:05:50 +0000

    Single step version

--- TauAnalysis/MCEmbeddingTools/python/pf_01_customizeSimulation.py
+++ TauAnalysis/MCEmbeddingTools/python/pf_01_customizeSimulation.py
@@ -1,7 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from FWCore.ParameterSet.Modules import _Module
+
+
+
+
+
+
+
+# Searches for self.lookFor module in cms.Path. When found, next and prev module is stored
+class SeqVisitor(object):
+    def __init__(self, lookFor):
+	self.lookFor=lookFor
+	self.nextInChain="NONE"
+	self.prevInChain="NONE"
+	self.prevInChainCandidate="NONE"
+	self.catch=0   # 1 - we have found self.lookFor, at next visit write visitee
+	self.found=0
+
+    def prepareSearch(self): # this should be called on beggining of each iteration 
+	self.found=0 
+      
+    def setLookFor(self, lookFor):
+	self.lookFor = lookFor
+      
+    def giveNext(self):
+	return self.nextInChain
+    def givePrev(self):
+	return self.prevInChain
+      
+    def enter(self,visitee):
+	if isinstance(visitee, _Module):
+	  if self.catch == 1:
+	      self.catch=0
+	      self.nextInChain=visitee
+	      self.found=1
+	  if visitee == self.lookFor:
+	      self.catch=1
+	      self.prevInChain=self.prevInChainCandidate
+	      
+	  self.prevInChainCandidate=visitee
+	
+    def leave(self,visitee):
+	    pass
 
 
 
 def customise(process):
+  
+  
   process._Process__name="SELECTandSIM"
 
   process.TFileService = cms.Service("TFileService",  fileName = cms.string("histo.root")          )
@@ -13,62 +60,56 @@ def customise(process):
 
   process.offlinePrimaryVerticesWithBS.TrackLabel = cms.InputTag("tmfTracks")
   process.offlinePrimaryVertices.TrackLabel = cms.InputTag("tmfTracks")
-  
 
-  from FWCore.ParameterSet.Modules import _Module
-  class SeqVisitor(object):
-      def __init__(self, lookFor):
-         self.lookFor=lookFor
-         self.nextInChain="NONE"
-         self.catch=0
-         self.found=0
-
-      def prepareSearch(self): # this should be called on beggining of each iteration 
-         self.found=0 
-       
-      def setLookFor(self, lookFor):
-         self.lookFor = lookFor
-       
-      def giveNext(self):
-         return self.nextInChain
-       
-      def enter(self,visitee):
-         if isinstance(visitee, _Module):
-            if self.catch == 1:
-               self.catch=0
-               self.nextInChain=visitee
-               self.found=1
-            if visitee == self.lookFor:
-               self.catch=1
-         
-      def leave(self,visitee):
-             pass
                                             
 
+  process.iterativeTracking.__iadd__(process.tmfTracks)
+
+  process.particleFlowORG = process.particleFlow.clone()
+  process.famosParticleFlowSequence.remove(process.pfElectronTranslatorSequence)
+  process.famosParticleFlowSequence.remove(process.particleFlow)
+  process.famosParticleFlowSequence.__iadd__(process.particleFlowORG)
+  process.famosParticleFlowSequence.__iadd__(process.particleFlow)
+  process.famosParticleFlowSequence.__iadd__(process.pfElectronTranslatorSequence)
+    
+  process.particleFlow =  cms.EDProducer('PFCandidateMixer',
+          col1 = cms.untracked.InputTag("dimuonsGlobal","forMixing"),
+          col2 = cms.untracked.InputTag("particleFlowORG", "")
+  )
+
+ 
+
+  from FWCore.ParameterSet.Types import InputTag
   for p in process.paths:
      i =  getattr(process,p)
-     target = process.generalTracks
-     source = process.tmfTracks 
-##
-     seqVis = SeqVisitor(source)
+     target = process.particleFlow
+     
+     seqVis = SeqVisitor(target)
      seqVis.prepareSearch()
      seqVis.setLookFor(target)
-     i.visit(seqVis) # finds next module in path after self.lookFor
-   
-  
-     while ( seqVis.catch != 1 and seqVis.found == 1 ): # the module we are looking for is allready at the end of path
-       target = seqVis.giveNext()
-       #     print "Replaceing " + target.label() + " with " + source.label()
-       i.replace(target, source) # replace target with source 
-       seqVis.prepareSearch()
-       seqVis.setLookFor(source)
-       i.visit(seqVis) # finds next module in path after "source"
-       source = target # prepare to replace module we have just found  with the module we have overwritten
-     
-     if (seqVis.catch==1):
-       seqVis.catch=0
-       #     print "Adding " + source.label() + " to path"
-       i.__iadd__(source)
-       
-  return(process)
+     i.visit(seqVis) 
+     while ( seqVis.catch != 1 and seqVis.found == 1 ): 
 
+       target = seqVis.giveNext()
+
+       targetAttributes =  dir(target)
+       for targetAttribute in targetAttributes:
+         attr=getattr(target,targetAttribute) # get actual attribute, not just  the name
+         if isinstance(attr, InputTag) and attr.getModuleLabel()=="particleFlow":
+           if ( attr.getProductInstanceLabel()!=""  ):
+             print "Changing: ", target, " ", targetAttribute, " ", attr, " to particleFlowORG", 
+             attr.setModuleLabel("particleFlowORG")
+
+
+       #i.replace(target, source) 
+       seqVis.prepareSearch()
+       seqVis.setLookFor(target)
+       i.visit(seqVis) 
+            
+     #if (seqVis.catch==1):
+       #seqVis.catch=0
+       #i.__iadd__(source)
+
+
+
+  return(process)
