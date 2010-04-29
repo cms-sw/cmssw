fea29361636e66984b1456ba38a03d37c5cbe3c0
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2010-04-29 13:06:01 +0000

    add SiPixelClusters

--- Fireworks/Tracks/plugins/FWItemTrackAccessors.cc
+++ Fireworks/Tracks/plugins/FWItemTrackAccessors.cc
@@ -7,7 +7,7 @@
 //
 // Original Author:  Tom McCauley
 //         Created:  Thu Feb 18 15:19:44 EDT 2008
-// $Id: FWItemTrackAccessors.cc,v 1.3 2010/04/23 17:01:19 yana Exp $
+// $Id: FWItemTrackAccessors.cc,v 1.4 2010/04/29 10:03:52 eulisse Exp $
 //
 
 #include <assert.h>
@@ -19,9 +19,12 @@
 #include "DataFormats/SiPixelDigi/interface/PixelDigi.h"
 #include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
 #include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+#include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
+
 #include "Fireworks/Core/interface/FWItemRandomAccessor.h"
 
 REGISTER_TEMPLATE_FWITEMACCESSOR( FWItemDetSetAccessor<edm::DetSetVector<SiStripDigi> >,edm::DetSetVector<SiStripDigi>, "SiStripDigiCollectionAccessor" );
 REGISTER_TEMPLATE_FWITEMACCESSOR( FWItemDetSetAccessor<edm::DetSetVector<PixelDigi> >, edm::DetSetVector<PixelDigi>, "SiPixelDigiCollectionAccessor" );
 REGISTER_TEMPLATE_FWITEMACCESSOR( FWItemNewDetSetAccessor<edmNew::DetSetVector<SiStripCluster> >, edmNew::DetSetVector<SiStripCluster>, "SiStripClusterCollectionNewAccessor" );
+REGISTER_TEMPLATE_FWITEMACCESSOR( FWItemNewDetSetAccessor<edmNew::DetSetVector<SiPixelCluster> >, edmNew::DetSetVector<SiPixelCluster>, "SiPixelClusterCollectionNewAccessor" );
 
