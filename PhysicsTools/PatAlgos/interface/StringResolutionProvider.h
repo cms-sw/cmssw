0f511e68fa0914fd381fd5df85432ab1843bfb7e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-04-30 15:38:55 +0000

    added doxygen documentation to class definitions -- include a description of how the new class should look like

--- PhysicsTools/PatAlgos/interface/StringResolutionProvider.h
+++ PhysicsTools/PatAlgos/interface/StringResolutionProvider.h
@@ -7,16 +7,81 @@
 
 #include "CommonTools/Utils/interface/StringObjectFunction.h"
 
+/**
+   \class   StringResolutionProvider StringResolutionProvider.h "PhysicsTools/PatAlgos/interface/StringResolutionProvider.h"
+
+   \brief   Class to provide resolution factors for PAT candidates
+
+   A class to provide resolution functions for PAT candidates. The class is derived from the 
+   KinematicResolutionProvider class. It expects the following parameters:
+
+   * parametrization :   indicates the used representation (e.g. EtEtaPhi). This parameter 
+                         is MANDATORY.
+   * resolutions     :   a vector of edm::ParameterSets, that contain the resolutions func-
+                         tionspotentially in multiple bins of a cetain parameter. For the 
+			 default implementeation we have bins of reco::Candidate's eta in 
+			 mind, though might be any kind of variable that is accessible as 
+			 memeber function of reco::Candidate. This parameter is MANDATORY.
+
+   The edm::ParameterSets of the _resolutions_ parameter are expected to be of form: 
+
+   * bin             :   Indicated the binning compatible with the StringCutObjectSelector. 
+                         If omitted no binning is applied and the obtained resolution func-
+                         tions are assumed to be valid for all candidates. This parameter 
+			 is OPTIONAL.
+   * et/eta/phi      :   Contain the parametrization in et if working in the EtEtaPhi pa-
+                         rametrization. These parameters are OPTIONAL. They are expected to 
+			 be present in the et/eta/phi representation though. 
+   * constraint      :   adding further information on constraints. It needs to investigated 
+                         further what the meaning of this parameter exactly is before we can 
+			 decide about its future. This parameter is OPTIONAL.
+   
+   A potential module definition as expects by thisd class will look like this: 
+
+   ...
+   parametrization = cms.string ('EtEtaPhi'),
+                                  
+   functions = VPSet(
+     ...
+     PSet(
+       bin = cms.string("0.145<=eta && eta<0.375"),
+       et  = cms.string("et * (sqrt(5.6*5.6/(et*et) + 1.25/et + 0.033)"),
+       eta = cms.string("0.03  + 1.0/et"),
+       phi = cms.string("0.015 + 1.5/et"),
+       contstraint = cms.vdouble(0)
+    ),
+   ...    
+   )
+
+   We expect that cfi files of this form will be generated automatically. edm::ParameterSets 
+   for other represenations will be implemented on request.
+*/
+
 class StringResolutionProvider : public KinematicResolutionProvider {
-    public:
-        typedef StringObjectFunction<reco::Candidate> Function;
-        StringResolutionProvider(const edm::ParameterSet &iConfig) ;
-        virtual ~StringResolutionProvider() ; 
-        virtual pat::CandKinResolution getResolution(const reco::Candidate &c) const ;
-    private:
-        std::auto_ptr<Function> resols_[4]; // StringObjectFunction is not default constructible :-(
-	std::vector<pat::CandKinResolution::Scalar> constraints_;
-        pat::CandKinResolution::Parametrization parametrization_;
-        int dimension_;
+
+ public:
+  /// short cut within the common namespace
+  typedef StringObjectFunction<reco::Candidate> Function;
+
+  /// default constructor
+  StringResolutionProvider(const edm::ParameterSet& cfg);
+  /// default destructor
+  virtual ~StringResolutionProvider();
+  /// get a CandKinResolution object from the service 
+  virtual pat::CandKinResolution getResolution(const reco::Candidate& cand) const;
+
+ private:
+  /// resolution functions (this needs an extension)
+  /// StringObjectFunction is not default constructible :-(
+  std::auto_ptr<Function> resols_[4]; 
+  /// a vector of constrtaints for the CanKinResolution 
+  /// object
+  std::vector<pat::CandKinResolution::Scalar> constraints_;
+  /// a parametrization for the CanKinResolution object
+  /// (this needs an extension)
+  pat::CandKinResolution::Parametrization parametrization_;
+  /// determines the dimenaion for variable transformations
+  int dimension_;
 };
+
 #endif
