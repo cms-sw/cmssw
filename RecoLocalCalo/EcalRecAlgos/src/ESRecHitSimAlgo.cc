55ffac4112a1b3e4d9afbb19caafe0478922ef85
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2010-05-02 10:12:10 +0000

    use analytic method to evaluate timing and set energy for out-of-time

--- RecoLocalCalo/EcalRecAlgos/src/ESRecHitSimAlgo.cc
+++ RecoLocalCalo/EcalRecAlgos/src/ESRecHitSimAlgo.cc
@@ -10,7 +10,7 @@ ESRecHitSimAlgo::ESRecHitSimAlgo() {
 
 double* ESRecHitSimAlgo::EvalAmplitude(const ESDataFrame& digi, const double& ped, const double& w0, const double& w1, const double& w2) const {
   
-  double *results = new double[3];
+  double *results = new double[4];
   float energy = 0;
   double adc[3];
   float pw[3];
@@ -27,7 +27,7 @@ double* ESRecHitSimAlgo::EvalAmplitude(const ESDataFrame& digi, const double& pe
   
   double status = 0;
   if (adc[0] > 20) status = 14;
-  if (adc[1] < 0 || adc[2] < 0) status = 10;
+  if (adc[1] <= 0 || adc[2] <= 0) status = 10;
   if (adc[0] > adc[1] && adc[0] > adc[2]) status = 8;
   if (adc[2] > adc[1] && adc[2] > adc[0]) status = 9;
   double r12 = (adc[1] != 0) ? adc[0]/adc[1] : 99;
@@ -36,15 +36,32 @@ double* ESRecHitSimAlgo::EvalAmplitude(const ESDataFrame& digi, const double& pe
   if (r23 > ratioCuts_->getR23High()) status = 6;
   if (r23 < ratioCuts_->getR23Low()) status = 7;
 
+  double A1 = adc[1];
+  double A2 = adc[2];
+
+  // t0 from analytical formula:
+  double n = 1.798;
+  double w = 0.07291;
+  double DeltaT = 25.;
+  double aaa = (A1 != 0.) ? log(A2/A1)/n : 20.; // if A1=0, t0=20
+  double bbb = w/n*DeltaT;
+  double ccc= exp(aaa+bbb);
+
+  double t0 = (2.-ccc)/(1.-ccc) * DeltaT - 5;
+
+  // A from analytical formula:
+  double t1 = 20.;
+  double A_1 =  pow(w/n*(t1),n) * exp(n-w*(t1));
+  double AA1 = (A_1 != 0.) ? A1 / A_1 : 0.;
+
   if (adc[1] > 2800 && adc[2] > 2800) status = 11;
   else if (adc[1] > 2800) status = 12;
   else if (adc[2] > 2800) status = 13;
 
-  double t0 = 19.2787 - 18.2603*r23 + 8.30438*pow(r23,2) - 4.19468*pow(r23,3) + 0.561087*pow(r23,4);
-
-  results[0] = energy;
-  results[1] = t0;
-  results[2] = status;
+  results[0] = energy; // energy with weight method
+  results[1] = t0;     // timing
+  results[2] = status; // hit status
+  results[3] = AA1;    // energy with analytic method
 
   return results;
 }
@@ -61,18 +78,21 @@ EcalRecHit ESRecHitSimAlgo::reconstruct(const ESDataFrame& digi) const {
 
   results = EvalAmplitude(digi, it_ped->getMean(), w0_, w1_, w2_);
 
-  double energy = results[0];
-  double t0 = results[1];
-  int status = (int) results[2];
+  double energy   = results[0];
+  double t0       = results[1];
+  int status      = (int) results[2];
+  double otenergy = results[3] * 1000000.; // set out-of-time energy to keV
   delete results;
 
   energy *= MIPGeV_/(*it_mip);
+  otenergy *= MIPGeV_/(*it_mip);
 
   DetId detId = digi.id();
 
   LogDebug("ESRecHitSimAlgo") << "ESRecHitSimAlgo : reconstructed energy "<<energy;
 
   EcalRecHit rechit(digi.id(), energy, t0);
+  rechit.setOutOfTimeEnergy(otenergy);
 
   if (it_status->getStatusCode() == 1) {
     rechit.setRecoFlag(EcalRecHit::kESDead);
