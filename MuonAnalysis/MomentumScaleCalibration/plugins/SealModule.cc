b45acee22f5254c2ec5a8d0a324c53440bf1b144
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2010-05-03 17:03:47 +0000

    Added Generation filter

--- MuonAnalysis/MomentumScaleCalibration/plugins/SealModule.cc
+++ MuonAnalysis/MomentumScaleCalibration/plugins/SealModule.cc
@@ -7,10 +7,12 @@
 
 #include "MuonAnalysis/MomentumScaleCalibration/plugins/MuScleFit.h"
 #include "MuonAnalysis/MomentumScaleCalibration/plugins/MuScleFitFilter.h"
+#include "MuonAnalysis/MomentumScaleCalibration/plugins/MuScleFitGenFilter.h"
 #include "MuonAnalysis/MomentumScaleCalibration/plugins/TestCorrection.h"
 
 DEFINE_FWK_LOOPER(MuScleFit);
 DEFINE_FWK_MODULE(MuScleFitFilter);
+DEFINE_FWK_MODULE(MuScleFitGenFilter);
 DEFINE_FWK_MODULE(TestCorrection);
 
 
