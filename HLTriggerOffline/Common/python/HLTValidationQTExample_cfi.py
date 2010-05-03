c92462c9e1126d067c8c4a994e027bf2641f8053
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2010-05-03 15:45:59 +0000

    thresholdReport

--- HLTriggerOffline/Common/python/HLTValidationQTExample_cfi.py
+++ HLTriggerOffline/Common/python/HLTValidationQTExample_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 hltQTExample = cms.EDAnalyzer("QualityTester",
     qtList = cms.untracked.FileInPath('HLTriggerOffline/Common/data/HltQTExample.xml'),
     #QualityTestPrescaler = cms.untracked.int32(1)
-    reportThreshold = cms.untracked.string('black'),
+    ##check verbose levels in http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/DQMServices/Components/src/QualityTester.cc 
+    #reportThreshold = cms.untracked.string('black'),
     prescaleFactor = cms.untracked.int32(1),
     getQualityTestsFromFile = cms.untracked.bool(True),
     qtestOnEndJob=cms.untracked.bool(True),
