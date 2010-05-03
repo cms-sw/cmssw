f3d7a976be7ce96a1d6ab73d1bf36d4795b13f2e
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2010-05-03 13:12:30 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
+++ ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
@@ -8,8 +8,8 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 # source
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-"file:/tmp/degrutto/F8EE38AF-1EBE-DE11-8D19-00304891F14E.root"
-#     'rfio:/castor/cern.ch/user/f/fabozzi/mc7tev/F8EE38AF-1EBE-DE11-8D19-00304891F14E.root'
+#"file:/tmp/degrutto/F8EE38AF-1EBE-DE11-8D19-00304891F14E.root"
+     'rfio:/castor/cern.ch/user/f/fabozzi/mc7tev/F8EE38AF-1EBE-DE11-8D19-00304891F14E.root'
 #    'file:/scratch1/cms/data/summer09/aodsim/zmumu/0016/889E7356-0084-DE11-AF48-001E682F8676.root'
 #    'file:testEWKMuSkim.root'
     )
@@ -18,23 +18,23 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START3X_V21::All')
+process.GlobalTag.globaltag = cms.string('START37_V1A::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 ############
 ## no MC truth and  on data
-#process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPaths_cff")
+process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPaths_cff")
 
 # Output module configuration
 process.load("ElectroWeakAnalysis.Skimming.zMuMuSubskimOutputModule_cfi")
-process.zMuMuSubskimOutputModule.fileName = 'testZMuMuSubskimWithMC.root'
+process.zMuMuSubskimOutputModule.fileName = 'testZMuMuSubskim.root'
 
 
 
 ############
 # MC truth matching sequence
-process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPathsWithMCTruth_cff")
-process.zMuMuSubskimOutputModule.outputCommands.extend(process.mcEventContent.outputCommands)
+#process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPathsWithMCTruth_cff")
+#process.zMuMuSubskimOutputModule.outputCommands.extend(process.mcEventContent.outputCommands)
 ############
 
 process.outpath = cms.EndPath(process.zMuMuSubskimOutputModule)
