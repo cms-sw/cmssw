94b479742327ab5fb2871cb51a3a0b3cdfe5c302
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2010-05-04 09:42:20 +0000

    less trivial test

--- CondFormats/SiPixelObjects/test/testSummary_cfg.py
+++ CondFormats/SiPixelObjects/test/testSummary_cfg.py
@@ -4,12 +4,17 @@ process = cms.Process("bla")
 process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1))
 
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-process.MessageLogger = cms.Service("MessageLogger",
-    debugModules = cms.untracked.vstring('*'),
-    destinations = cms.untracked.vstring('read'),
-    read = cms.untracked.PSet( threshold = cms.untracked.string('INFO'))
-)
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR10_P_V4::All"
+
 
 process.testSummary =  cms.EDAnalyzer("SiPixelTestSummary")
 
