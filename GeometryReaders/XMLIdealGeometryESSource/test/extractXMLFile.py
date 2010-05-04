9fac68086d5f97fe640389fe6ba0a810be295106
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2010-05-04 22:22:10 +0000

    fix one py to work for 3.7
    remove an unnecessary cc file that looks like a test.

--- GeometryReaders/XMLIdealGeometryESSource/test/extractXMLFile.py
+++ GeometryReaders/XMLIdealGeometryESSource/test/extractXMLFile.py
@@ -5,7 +5,7 @@ process = cms.Process("Demo")
 process.load('Configuration/StandardSequences/GeometryDB_cff')
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'MC_GEOM_RPCV2::All'
+process.GlobalTag.globaltag = 'MC_37Y_V0::All'
 
 
 process.maxEvents = cms.untracked.PSet(
@@ -19,10 +19,11 @@ process.source = cms.Source("EmptySource")
 
 process.MessageLogger = cms.Service("MessageLogger")
 
-process.test1 = cms.EDFilter("RPCGEO")
-process.test2 = cms.EDFilter("RPCGeometryAnalyzer")
-process.demo = cms.EDAnalyzer("PrintEventSetupContent")
-process.test3 = cms.EDFilter("ExtractXMLFile", label=cms.untracked.string("Extended"), fname=cms.untracked.string("fred2.xml"))
+#process.test1 = cms.EDAnalyzer("RPCGEO")
+#process.test2 = cms.EDAnalyzer("RPCGeometryAnalyzer")
+#process.demo = cms.EDAnalyzer("PrintEventSetupContent")
+process.test3 = cms.EDAnalyzer("ExtractXMLFile", label=cms.untracked.string("Extended"), fname=cms.untracked.string("fred.xml"))
 
-process.p = cms.Path(process.test1+process.test2+process.demo+process.test3)
+#process.p = cms.Path(process.test1+process.test2+process.demo+process.test3)
+process.p = cms.Path(process.test3)
 
