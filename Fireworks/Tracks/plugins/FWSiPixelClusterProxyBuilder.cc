64d863af2f127d472fa6b5ee7697835cd1d5b094
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2010-05-04 17:52:07 +0000

    separate dets and clusters for sipixels and add sipixel accessor

--- Fireworks/Tracks/plugins/FWSiPixelClusterProxyBuilder.cc
+++ Fireworks/Tracks/plugins/FWSiPixelClusterProxyBuilder.cc
@@ -6,22 +6,17 @@
 //
 // Original Author:
 //         Created:  Thu Dec  6 18:01:21 PST 2007
-// $Id: FWSiPixelClusterProxyBuilder.cc,v 1.8 2010/04/25 09:48:21 amraktad Exp $
+// $Id: FWSiPixelClusterProxyBuilder.cc,v 1.9 2010/05/03 15:47:44 amraktad Exp $
 //
 
-// system include files
-#include "TEveManager.h"
 #include "TEveCompound.h"
-#include "TEveGeoNode.h"
-#include "TEveStraightLineSet.h"
 
-// user include files
 #include "Fireworks/Core/interface/FWProxyBuilderBase.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/DetIdToMatrix.h"
+#include "Fireworks/Tracks/interface/TrackUtils.h"
 
 #include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
-#include "Fireworks/Tracks/interface/TrackUtils.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
 class FWSiPixelClusterProxyBuilder : public FWProxyBuilderBase
@@ -31,67 +26,49 @@ public:
    virtual ~FWSiPixelClusterProxyBuilder() {}
 
    REGISTER_PROXYBUILDER_METHODS();
-private:
-   virtual void build( const FWEventItem* iItem, TEveElementList* product , const FWViewContext*);
-   FWSiPixelClusterProxyBuilder( const FWSiPixelClusterProxyBuilder& );    // stop default
-   const FWSiPixelClusterProxyBuilder& operator=( const FWSiPixelClusterProxyBuilder& );    // stop default
-   //void modelChanges( const FWModelIds& iIds, TEveElement* iElements, int);
-   //void applyChangesToAllModels( TEveElement* iElements, int );
 
-protected:
-   enum Mode { Clusters, Modules };
-   virtual Mode getMode() { return Clusters; }
+private:
+   virtual void build(const FWEventItem* iItem, TEveElementList* product, const FWViewContext*);
+   FWSiPixelClusterProxyBuilder( const FWSiPixelClusterProxyBuilder& );
+   const FWSiPixelClusterProxyBuilder& operator=( const FWSiPixelClusterProxyBuilder& );
 };
 
 //______________________________________________________________________________
 
 void FWSiPixelClusterProxyBuilder::build( const FWEventItem* iItem, TEveElementList* product , const FWViewContext*)
 {
-   const SiPixelClusterCollectionNew* pixels = 0;
-   iItem->get( pixels );
-   if( 0 == pixels ) return;
-   
-   TEveCompound* top = createCompound();
-   int index(0);
-   for( SiPixelClusterCollectionNew::const_iterator set = pixels->begin(), setEnd = pixels->end();
-       set != setEnd; ++set, ++index ) {
-      TEveCompound* compound = createCompound();
-      unsigned int id = set->detId();
-      DetId detid(id);
+  const SiPixelClusterCollectionNew* pixels = 0;
+  
+  iItem->get(pixels);
+  
+  if( 0 == pixels ) 
+    return;
+
+  for( SiPixelClusterCollectionNew::const_iterator set = pixels->begin(), setEnd = pixels->end();
+       set != setEnd; ++set ) 
+  {   
+    TEveCompound* compound = createCompound();
+ 
+    unsigned int id = set->detId();
+    DetId detid(id);
       
-      if( iItem->getGeom() ) {
-         Mode m = getMode();
-         if( m == Clusters ) {
-            const TGeoHMatrix* matrix = iItem->getGeom()->getMatrix( detid );
-            std::vector<TVector3> pixelPoints;
-            const edmNew::DetSet<SiPixelCluster> & clusters = *set;
-            for( edmNew::DetSet<SiPixelCluster>::const_iterator itc = clusters.begin(), edc = clusters.end(); itc != edc; ++itc ) {
-               fireworks::pushPixelCluster( pixelPoints, matrix, detid, *itc );
-            }
-            fireworks::addTrackerHits3D( pixelPoints, compound, iItem->defaultDisplayProperties().color(), 1 );
-         } else if( m == Modules ) {
-            TEveGeoShape* shape = iItem->getGeom()->getShape( id );
-            if( 0 != shape ) {
-               shape->SetMainTransparency( 50 );
-               setupAddElement( shape, compound );
-            }
-         }
-      }
-      setupAddElement(compound, top);
-   }
-   
-   setupAddElement(top, product);
+    if( iItem->getGeom() ) 
+    {
+      const TGeoHMatrix* matrix = iItem->getGeom()->getMatrix( detid );
+      std::vector<TVector3> pixelPoints;
+      
+      const edmNew::DetSet<SiPixelCluster> & clusters = *set;
+       
+      for( edmNew::DetSet<SiPixelCluster>::const_iterator itc = clusters.begin(), edc = clusters.end(); 
+           itc != edc; ++itc ) 
+        fireworks::pushPixelCluster( pixelPoints, matrix, detid, *itc );
+      
+      
+      fireworks::addTrackerHits3D( pixelPoints, compound, iItem->defaultDisplayProperties().color(), 1 );
+    }
+  
+    setupAddElement(compound, product);
+  }    
 }
 
-class FWSiPixelClusterModProxyBuilder : public FWSiPixelClusterProxyBuilder {
-public:
-   FWSiPixelClusterModProxyBuilder() {}
-   virtual ~FWSiPixelClusterModProxyBuilder() {}
-   
-   REGISTER_PROXYBUILDER_METHODS();
-protected:
-   virtual Mode getMode() { return Modules; }
-};
-
 REGISTER_FWPROXYBUILDER( FWSiPixelClusterProxyBuilder, SiPixelClusterCollectionNew, "SiPixelCluster", FWViewType::kAll3DBits | FWViewType::kAllRPZBits );
-REGISTER_FWPROXYBUILDER( FWSiPixelClusterModProxyBuilder, SiPixelClusterCollectionNew, "SiPixelClusterDets", FWViewType::kAll3DBits | FWViewType::kAllRPZBits );
