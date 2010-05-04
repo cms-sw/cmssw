f22d1e72bc318083dbcb0c2d39a707c36494327e
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-05-04 13:18:15 +0000

    initial versions for 3.7 cycle of customise fragments. using the centralized cofiguration

---
+++ Configuration/GlobalRuns/python/reco_TLR_37X.py
@@ -0,0 +1,112 @@
+import FWCore.ParameterSet.Config as cms
+
+def customiseCommon(process):
+    
+    #####################################################################################################
+    ####
+    ####  Top level replaces for handling strange scenarios of early collisions
+    ####
+
+    ## TRACKING:
+    ## Skip events with HV off
+    process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+    process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+    process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+    process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
+    process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+    process.thTripletsA.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
+    process.thTripletsB.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
+
+    ###### FIXES TRIPLETS FOR LARGE BS DISPLACEMENT ######
+
+    ### prevent bias in pixel vertex
+    process.pixelVertices.useBeamConstraint = False
+    
+    ### pixelTracks
+    #---- new parameters ----
+    process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = 4.06
+    process.pixelTracks.RegionFactoryPSet.RegionPSet.originHalfLength = cms.double(40.6)
+
+    ### 0th step of iterative tracking
+    #---- new parameters ----
+    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)  
+    process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 40.6
+
+    ### 2nd step of iterative tracking
+    #---- new parameters ----
+    process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)  
+    process.secTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 44.7
+
+    ## Primary Vertex
+    process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelLayersWithHits = 2
+    process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconLayersWithHits = 5
+    process.offlinePrimaryVerticesWithBS.TkClusParameters.TkGapClusParameters.zSeparation = 1
+    process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
+    process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
+    process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
+    process.offlinePrimaryVertices.TkFilterParameters.minPixelLayersWithHits = 2
+    process.offlinePrimaryVertices.TkFilterParameters.minSiliconLayersWithHits = 5
+    process.offlinePrimaryVertices.TkClusParameters.TkGapClusParameters.zSeparation = 1
+
+    ## ECAL 
+    process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 78, 142 ]
+
+
+    ## HCAL temporary fixes
+    process.hfreco.samplesToAdd = 4
+    
+    ## EGAMMA
+    process.photons.minSCEtBarrel = 5.
+    process.photons.minSCEtEndcap =5.
+    process.photonCore.minSCEt = 5.
+    process.conversionTrackCandidates.minSCEt =5.
+    process.conversions.minSCEt =5.
+    process.trackerOnlyConversions.rCut = 2.
+    process.trackerOnlyConversions.vtxChi2 = 0.0005
+    
+    ###
+    ###  end of top level replacements
+    ###
+    ###############################################################################################
+
+    return (process)
+
+
+##############################################################################
+def customisePPData(process):
+    process= customiseCommon(process)
+    process.hfreco.firstSample=3
+
+    ##Preshower algo for data is different than for MC
+    process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+
+    return process
+
+
+##############################################################################
+def customisePPMC(process):
+    process=customiseCommon(process)
+    process.hfreco.firstSample=1
+    
+    return process
+
+##############################################################################
+def customiseCosmicData(process):
+    process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+    
+    return process
+
+##############################################################################
+def customiseCosmicMC(process):
+    
+    return process
+        
+
+##############################################################################
+def customiseExpress(process):
+    process= customisePPData(process)
+
+    return process
