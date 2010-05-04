06309edb890729286ba2b1def70d8a4103be32ec
Frank Golf <sha1-e884805f6c312aa2407c5005ebbb36cbb0c23396@cern.ch>
2010-05-04 11:44:00 +0000

    removed muon met corrections and associated value maps as they have now been migrated to RecoMET/METAlgorithms at request of reco coordinators

--- JetMETCorrections/Type1MET/src/Module.cc
+++ JetMETCorrections/Type1MET/src/Module.cc
@@ -4,21 +4,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "JetMETCorrections/Type1MET/interface/Type1MET.h"
-#include "JetMETCorrections/Type1MET/interface/MuonMET.h"
 #include "JetMETCorrections/Type1MET/interface/TauMET.h"
-#include "JetMETCorrections/Type1MET/interface/MuonMETValueMapProducer.h"
-#include "JetMETCorrections/Type1MET/interface/MuonTCMETValueMapProducer.h"
 
 using cms::Type1MET;
-using cms::MuonMET;
 using cms::TauMET;
-using cms::MuonMETValueMapProducer;
-using cms::MuonTCMETValueMapProducer;
 
 
 
 DEFINE_FWK_MODULE(Type1MET);
-DEFINE_FWK_MODULE(MuonMET);
 DEFINE_FWK_MODULE(TauMET);
-DEFINE_FWK_MODULE(MuonMETValueMapProducer);
-DEFINE_FWK_MODULE(MuonTCMETValueMapProducer);
