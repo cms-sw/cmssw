685395b42a06cff2e71e5264d464f8f9c06e695a
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2010-05-05 14:13:59 +0000

    added CSC for Cables

---
+++ CondFormats/DataRecord/interface/CSCCablesRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCCABLESRCD_H
+#define CSCCABLESRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCCablesRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCCablesRcd> {};
+#endif
