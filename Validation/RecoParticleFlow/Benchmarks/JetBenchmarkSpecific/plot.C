cdd3377250d8d15add2abeb1ebdd9ca69f3bd8ec
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2010-05-05 16:26:14 +0000

    Prepare for validation of 370pre4

--- Validation/RecoParticleFlow/Benchmarks/JetBenchmarkSpecific/plot.C
+++ Validation/RecoParticleFlow/Benchmarks/JetBenchmarkSpecific/plot.C
@@ -8,28 +8,28 @@ ROOT::Cintex::Cintex::Enable();
 //InitNicePlot();
 
 gROOT->ProcessLine(".L makeJetResolutionPlot.C");
-Resolution(1,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Fast_Barrel.png","Fast simulation - Barrel resolution");
- Resolution(1,"JetBenchmark_Full_360pre6.root","JetBenchmark_Full_Barrel.png","Full simulation - Barrel resolution");
-Resolution(2,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Fast_Endcap.png","Fast simulation - Endcap resolution");
- Resolution(2,"JetBenchmark_Full_360pre6.root","JetBenchmark_Full_Endcap.png","Full simulation - Endcap resolution");
-//Resolution(3,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Fast_Forward_360pre6.png");
-//Resolution(3,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Fast_Forward_360pre6.png");
+Resolution(1,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Fast_Barrel.png","Fast simulation - Barrel resolution");
+ Resolution(1,"JetBenchmark_Full_370pre4.root","JetBenchmark_Full_Barrel.png","Full simulation - Barrel resolution");
+Resolution(2,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Fast_Endcap.png","Fast simulation - Endcap resolution");
+ Resolution(2,"JetBenchmark_Full_370pre4.root","JetBenchmark_Full_Endcap.png","Full simulation - Endcap resolution");
+//Resolution(3,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Fast_Forward_370pre4.png");
+//Resolution(3,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Fast_Forward_370pre4.png");
 
 
 gROOT->ProcessLine(".L Compare.C");
-Compare(1,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Full_360pre6.root","BarrelComparison_FastFull.png","Barrel Fast-Full comparison");
-Compare(2,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Full_360pre6.root","EndcapComparison_FastFull.png","Endcap Fast-Full comparison");
-Compare(3,"JetBenchmark_Fast_360pre6.root","JetBenchmark_Full_360pre6.root","ForwardComparison_FastFull.png","Forward Fast-Full comparison");
+Compare(1,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Full_370pre4.root","BarrelComparison_FastFull.png","Barrel Fast-Full comparison");
+Compare(2,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Full_370pre4.root","EndcapComparison_FastFull.png","Endcap Fast-Full comparison");
+Compare(3,"JetBenchmark_Fast_370pre4.root","JetBenchmark_Full_370pre4.root","ForwardComparison_FastFull.png","Forward Fast-Full comparison");
 
 gROOT->ProcessLine(".L ResolutionDirection.C");
-ResolutionDirection(1,"JetBenchmark_Fast_360pre6.root","AngularResolution_Fast_Barrel.png");
-ResolutionDirection(0,"JetBenchmark_Fast_360pre6.root","AngularResolution_Fast_Endcap.png");
-ResolutionDirection(1,"JetBenchmark_Full_360pre6.root","AngularResolution_Full_Barrel.png");
-ResolutionDirection(0,"JetBenchmark_Full_360pre6.root","AngularResolution_Full_Endcap.png");
+ResolutionDirection(1,"JetBenchmark_Fast_370pre4.root","AngularResolution_Fast_Barrel.png");
+ResolutionDirection(0,"JetBenchmark_Fast_370pre4.root","AngularResolution_Fast_Endcap.png");
+ResolutionDirection(1,"JetBenchmark_Full_370pre4.root","AngularResolution_Full_Barrel.png");
+ResolutionDirection(0,"JetBenchmark_Full_370pre4.root","AngularResolution_Full_Endcap.png");
 
 gROOT->ProcessLine(".L TrackMult.C");
-TrackMult("JetBenchmark_Fast_360pre6.root","JetBenchmark_Full_360pre6.root");
+TrackMult("JetBenchmark_Fast_370pre4.root","JetBenchmark_Full_370pre4.root");
 
 gROOT->ProcessLine(".L Fractions.C");
-Fractions("JetBenchmark_Fast_360pre6.root","JetBenchmark_Full_360pre6.root");
+Fractions("JetBenchmark_Fast_370pre4.root","JetBenchmark_Full_370pre4.root");
 }
--- Validation/RecoParticleFlow/test/launch_QCDForPF_FullSim.sh
+++ Validation/RecoParticleFlow/test/launch_QCDForPF_FullSim.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-castorDir=/castor/cern.ch/user/p/pjanot/CMSSW360pre6/
+castorDir=/castor/cern.ch/user/p/pjanot/CMSSW370pre4/
 cmsswDir=$CMSSW_BASE/src
 
 for ((job=0;job<90;job++));
