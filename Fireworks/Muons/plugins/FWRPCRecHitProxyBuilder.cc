7f4f2b468bd96565923c8e1b0b3fd10898478e13
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2010-05-05 08:35:08 +0000

    fix RPC rec hit drawing

--- Fireworks/Muons/plugins/FWRPCRecHitProxyBuilder.cc
+++ Fireworks/Muons/plugins/FWRPCRecHitProxyBuilder.cc
@@ -6,11 +6,11 @@
 //
 // Original Author:
 //         Created:  Sun Jan  6 23:42:33 EST 2008
-// $Id: FWRPCRecHitProxyBuilder.cc,v 1.4 2010/04/23 14:31:31 mccauley Exp $
+// $Id: FWRPCRecHitProxyBuilder.cc,v 1.5 2010/05/04 11:40:19 amraktad Exp $
 //
 
 #include "TEveGeoNode.h"
-#include "TEvePointSet.h"
+#include "TEveStraightLineSet.h"
 
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
@@ -49,33 +49,65 @@ FWRPCRecHitProxyBuilder::build(const RPCRecHit& iData,
     return;
   }
  
+  std::stringstream s;
+  s << "rec hit" << iIndex;
+
+  TEveStraightLineSet* recHitSet = new TEveStraightLineSet(s.str().c_str());
+  recHitSet->SetLineWidth(3);
+
   TEveGeoShape* shape = item()->getGeom()->getShape(rpcId);
-
-  double localPoint[3];
-  double globalPoint[3];
-
-  localPoint[0] = iData.localPosition().x();
-  localPoint[1] = iData.localPosition().y();
-  localPoint[2] = iData.localPosition().z();
-	 
-  TEvePointSet* pointSet = new TEvePointSet;
-  pointSet->SetMarkerStyle(2);
-  pointSet->SetMarkerSize(3);
-  setupAddElement(pointSet, &oItemHolder);
 
   if ( shape ) 
   {
     shape->SetMainTransparency(75);
     shape->SetMainColor(item()->defaultDisplayProperties().color());
-    pointSet->AddElement(shape);
+    recHitSet->AddElement(shape);
   }
 
-  if( rpcId.layer() == 1 && rpcId.station() < 3 )
-    localPoint[0] = -localPoint[0];
-	
-  matrix->LocalToMaster(localPoint, globalPoint);
+  double localX = iData.localPosition().x();
+  double localY = iData.localPosition().y();
+  double localZ = iData.localPosition().z();
+  
+  double localXerr = sqrt(iData.localPositionError().xx());
+  double localYerr = sqrt(iData.localPositionError().yy());
 
-  pointSet->SetNextPoint(globalPoint[0], globalPoint[1], globalPoint[2]);
+  double localU1[3] = 
+    {
+      localX - localXerr, localY, localZ 
+    };
+  
+  double localU2[3] = 
+    {
+      localX + localXerr, localY, localZ 
+    };
+  
+  double localV1[3] = 
+    {
+      localX, localY - localYerr, localZ
+    };
+  
+  double localV2[3] = 
+    {
+      localX, localY + localYerr, localZ
+    };
+
+  double globalU1[3];
+  double globalU2[3];
+  double globalV1[3];
+  double globalV2[3];
+ 
+  matrix->LocalToMaster(localU1, globalU1);
+  matrix->LocalToMaster(localU2, globalU2);
+  matrix->LocalToMaster(localV1, globalV1);
+  matrix->LocalToMaster(localV2, globalV2);
+ 
+  recHitSet->AddLine(globalU1[0], globalU1[1], globalU1[2],
+                     globalU2[0], globalU2[1], globalU2[2]);
+
+  recHitSet->AddLine(globalV1[0], globalV1[1], globalV1[2],
+                     globalV2[0], globalV2[1], globalV2[2]);
+
+  setupAddElement(recHitSet, &oItemHolder);
 }
 
 REGISTER_FWPROXYBUILDER( FWRPCRecHitProxyBuilder, RPCRecHit, "RPC RecHits", FWViewType::kAll3DBits | FWViewType::kRhoPhiBit  | FWViewType::kRhoZBit);
