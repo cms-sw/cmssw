0cdcedbfe94f2ee267eaded32daf73de388e2b26
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2010-05-05 12:35:52 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/Skimming/python/dimuonsOneTrack_cfi.py
+++ ElectroWeakAnalysis/Skimming/python/dimuonsOneTrack_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 dimuonsOneTrack = cms.EDProducer("CandViewShallowCloneCombiner",
-    checkCharge = cms.bool(False),
+    checkCharge = cms.bool(True),
     cut = cms.string('mass > 20'),
 #    decay = cms.string('selectedLayer1Muons@+ goodTracks@-')
 #    decay = cms.string('selectedLayer1Muons@+ selectedLayer1TrackCands@-')
