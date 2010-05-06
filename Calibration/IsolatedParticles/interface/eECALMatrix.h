899d6c1301c18de9e25ca23c9534976782b71267
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-05-06 21:31:50 +0000

    Introduce spike rejection and track selection

--- Calibration/IsolatedParticles/interface/eECALMatrix.h
+++ Calibration/IsolatedParticles/interface/eECALMatrix.h
@@ -27,6 +27,8 @@ Created: August 2009
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "CondFormats/EcalObjects/interface/EcalChannelStatus.h"
 
 #include "Calibration/IsolatedParticles/interface/MatrixECALDetIds.h"
 #include "RecoCaloTools/Navigation/interface/CaloNavigator.h"
@@ -41,6 +43,8 @@ namespace spr{
 
   template< typename T>
   double eECALmatrix(const DetId& detId, edm::Handle<T>& hitsEB, edm::Handle<T>& hitsEE, const CaloGeometry* geo, const CaloTopology* caloTopology, int ietaE, int ietaW, int iphiN, int iphiS, double ebThr=-100, double eeThr=-100, bool debug=false);
+
+  std::pair <double,bool> eECALmatrix(const DetId& detId, edm::Handle<EcalRecHitCollection>& hitsEB, edm::Handle<EcalRecHitCollection>& hitsEE, const EcalChannelStatus& chStatus, const CaloGeometry* geo, const CaloTopology* caloTopology, int ieta, int iphi, double ebThr=-100, double eeThr=-100, bool debug=false);
   
   // returns vector of hits in NxN matrix 
   template <typename T>
