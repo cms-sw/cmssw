1e7fc27493471ecce4857cfd95821a2684f64e63
Igor Vorobiev <sha1-436f6aeeec4e80918d79e6e50e21cb4844ade16c@cern.ch>
2010-05-06 11:38:59 +0000

    forgot to uncomment lines

--- OnlineDB/CSCCondDB/interface/CSCCableValues.h
+++ OnlineDB/CSCCondDB/interface/CSCCableValues.h
@@ -54,16 +54,15 @@ inline CSCCables *  CSCCableValues::fillCables()
   /* This is version for 481 chambers. */
   
   for(i=1;i<=481;++i){
-    //    cable->cable_read(i, &chamber_label, &cfeb_length, &cfeb_rev, &alct_length,
-    //    &alct_rev, &cfeb_tmb_skew_delay, &cfeb_timing_corr);
-    /*   cableobj->cables[i].chamber_label=chamber_label;
+    cable->cable_read(i, &chamber_label, &cfeb_length, &cfeb_rev, &alct_length,
+    &alct_rev, &cfeb_tmb_skew_delay, &cfeb_timing_corr);
+    cableobj->cables[i].chamber_label=chamber_label;
     cableobj->cables[i].cfeb_length=cfeb_length;
     cableobj->cables[i].cfeb_rev=cfeb_rev;
     cableobj->cables[i].alct_length=alct_length;
     cableobj->cables[i].alct_rev=alct_rev;
     cableobj->cables[i].cfeb_tmb_skew_delay=cfeb_tmb_skew_delay;
     cableobj->cables[i].cfeb_timing_corr=cfeb_timing_corr;
-    */
     count=count+1;
   }
   return cableobj;
