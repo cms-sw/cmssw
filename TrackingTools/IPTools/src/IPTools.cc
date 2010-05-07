f6aa526af316e44f4e7b76a31f72faa5537ee838
Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch>
2010-05-07 13:55:35 +0000

    check status src/IPTools.cc

--- TrackingTools/IPTools/src/IPTools.cc
+++ TrackingTools/IPTools/src/IPTools.cc
@@ -56,6 +56,7 @@ namespace IPTools
     //Compute absolute value
     VertexDistanceXY dist;
     pair<bool,Measurement1D> result = absoluteImpactParameter(closestOnTransversePlaneState, vertex,  dist);
+    if(!result.first) return result;
 
     //Compute Sign
     GlobalPoint impactPoint    = closestOnTransversePlaneState.globalPosition();
@@ -76,6 +77,7 @@ namespace IPTools
     //Compute absolute value
     VertexDistance3D dist;
     pair<bool,Measurement1D> result = absoluteImpactParameter(closestIn3DSpaceState, vertex,  dist);
+    if(!result.first) return result;
 
     //Compute Sign
     GlobalPoint impactPoint = closestIn3DSpaceState.globalPosition();
