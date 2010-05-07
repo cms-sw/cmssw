7e1fedd3f531021d5026c9552c4c54c7ea282df2
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2010-05-07 18:59:58 +0000

    update script

--- RecoVertex/BeamSpotProducer/python/tools/queryDBS_cron.sh
+++ RecoVertex/BeamSpotProducer/python/tools/queryDBS_cron.sh
@@ -3,7 +3,7 @@ echo "starting:"
 date
 export STAGE_HOST=castorcms.cern.ch
 source /afs/cern.ch/cms/sw/cmsset_default.sh
-cd /afs/cern.ch/user/y/yumiceva/scratch0/CMSSW_3_5_7/src/
+cd /afs/cern.ch/user/y/yumiceva/scratch0/CMSSW_3_5_8_patch3/src/
 eval `scramv1 runtime -sh`
 
 echo "CMSSW configured"
