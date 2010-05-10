565d2bca657f3b767e861dd993443593f3ce6273
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-05-10 15:31:23 +0000

    added test for MDet

---
+++ RecoTracker/MeasurementDet/test/MeasurementDetSize.cpp
@@ -0,0 +1,17 @@
+#include "RecoTracker/MeasurementDet/interface/TkStripMeasurementDet.h"
+#include "RecoTracker/MeasurementDet/interface/TkPixelMeasurementDet.h"
+#include "RecoTracker/MeasurementDet/interface/TkGluedMeasurementDet.h"
+
+#include<iostream>
+
+
+int main() {
+
+  std::cout << "size of TkStripMeasurementDet " << sizeof(TkStripMeasurementDet) std::endl;
+  std::cout << "size of TkPixelMeasurementDet "<< sizeof(TkPixelMeasurementDet) std::endl;
+  std::cout << "size of TkGluedMeasurementDet "<< sizeof(TkGluedMeasurementDet) std::endl;
+
+  return 0;
+
+
+}
