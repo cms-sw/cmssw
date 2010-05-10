485f35cbd8c33f6016bb803f6e4cf020345b1fec
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-05-10 18:05:32 +0000

    pdate to V3 GTs for 37X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
-autoCond = { 'mc'      : 'MC_37Y_V2::All',
-             'startup' : 'START37_V2::All',
+autoCond = { 'mc'      : 'MC_37Y_V3::All',
+             'startup' : 'START37_V3::All',
              'craft08' : 'CRFT8_37R_V0::All',
-             'craft09' : 'CRFT9_37R_V2::All',
-             'com10'   : 'GR_R_37X_V2::All'
+             'craft09' : 'CRFT9_37R_V3::All',
+             'com10'   : 'GR_R_37X_V3::All'
              }
