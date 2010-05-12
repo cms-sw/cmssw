66511a8edc61e42bdbc0b2a28618e826779125a4
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2010-05-12 14:21:14 +0000

    tell WhatsItAnalyzer exactly what values are expected

--- FWCore/Integration/test/EventSetupAppendLabelTest_cfg.py
+++ FWCore/Integration/test/EventSetupAppendLabelTest_cfg.py
@@ -10,7 +10,8 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("EmptySource")
 
-process.WhatsItAnalyzer = cms.EDAnalyzer("WhatsItAnalyzer")
+process.WhatsItAnalyzer = cms.EDAnalyzer("WhatsItAnalyzer",
+    expectedValues = cms.untracked.vint32(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 
 process.WhatsItESProducer = cms.ESProducer("WhatsItESProducer",
     doodadLabel = cms.string('Two')
