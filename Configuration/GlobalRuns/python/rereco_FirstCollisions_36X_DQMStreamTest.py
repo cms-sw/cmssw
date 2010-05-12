67ad0ae9d7abc127cee977518d66ba1c2e924a02
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-05-12 16:27:51 +0000

    update against rereco_FirstCollisions_36X.py rev. 1.9

--- Configuration/GlobalRuns/python/rereco_FirstCollisions_36X_DQMStreamTest.py
+++ Configuration/GlobalRuns/python/rereco_FirstCollisions_36X_DQMStreamTest.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.177 
 # Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: reco_FirstCollisions_36X -s RAW2DIGI,L1Reco,RECO,DQM --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent RECO --conditions GR_R_36X_V6::All --customise Configuration/GlobalRuns/customise_Collision_36X.py --no_exec --python_filename=rereco_FirstCollisions_36X.py --process RERECO
+# with command line options: reco_FirstCollisions_36X -s RAW2DIGI,L1Reco,RECO,DQM --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent RECO --customise Configuration/GlobalRuns/customise_Collision_36X.py --no_exec --python_filename=rereco_FirstCollisions_36X.py --conditions GR_R_36X_V9::All --process RERECO
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('RERECO')
@@ -22,7 +22,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
+    version = cms.untracked.string('$Revision: 1.9 $'),
     annotation = cms.untracked.string('reco_FirstCollisions_36X nevts:1'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -34,7 +34,7 @@ process.options = cms.untracked.PSet(
 )
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/Commissioning10/MinimumBias/RAW/v4/000/132/440/EA896425-F93B-DF11-B971-000423D99AAE.root')
+    fileNames = cms.untracked.vstring('reco_FirstCollisions_36X_DIGI2RAW.root')
 )
 
 # Output definition
@@ -62,7 +62,7 @@ process.outputDQMStream = cms.OutputModule("PoolOutputModule",
 )
 
 # Other statements
-process.GlobalTag.globaltag = 'GR_R_36X_V6::All'
+process.GlobalTag.globaltag = 'GR_R_36X_V9::All'
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
@@ -141,12 +141,11 @@ def customise(process):
     process.ecalPreshowerRecHit.ESBaseline = 0
 
     ##Preshower algo for data is different than for MC
-    process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+    process.ecalPreshowerRecHit.ESRecoAlgo = 1
 
     ## HCAL temporary fixes
     process.hfreco.firstSample  = 3
     process.hfreco.samplesToAdd = 4
-    process.hfreco.PETstat.short_R = cms.vdouble([0.8])
     
     ## EGAMMA
     process.photons.minSCEtBarrel = 5.
