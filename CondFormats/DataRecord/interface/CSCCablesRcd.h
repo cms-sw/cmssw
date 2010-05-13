ca570477d74046aae2f839a2b1e3bb6dd6229658
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2010-05-13 20:21:47 +0000

    removed CSCCablesRcd.h

--- CondFormats/DataRecord/interface/CSCCablesRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef CSCCABLESRCD_H
-#define CSCCABLESRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class CSCCablesRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCCablesRcd> {};
-#endif
