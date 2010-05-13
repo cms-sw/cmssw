f299a4d8167d7d2c4145088349606696861491eb
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2010-05-13 17:17:52 +0000

    add new record for PGeometricDetExtra

---
+++ Geometry/Records/interface/GeometricDetExtraRcd.h
@@ -0,0 +1,6 @@
+#ifndef GEOMETRICDETEXTRARCD_H
+#define GEOMETRICDETEXTRARCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class GeometricDetExtraRcd : public edm::eventsetup::EventSetupRecordImplementation<GeometricDetExtraRcd> {};
+#endif
