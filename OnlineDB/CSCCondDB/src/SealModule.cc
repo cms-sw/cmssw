90cb1463dd439b5c0f768337bb634d8b7fb38bcc
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2010-05-13 20:38:26 +0000

    changed to new names

--- OnlineDB/CSCCondDB/src/SealModule.cc
+++ OnlineDB/CSCCondDB/src/SealModule.cc
@@ -3,16 +3,16 @@
 #include "OnlineDB/CSCCondDB/interface/CSCChamberMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCChamberIndexValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCCrateMapValues.h"
-#include "OnlineDB/CSCCondDB/interface/CSCCableValues.h"
+#include "OnlineDB/CSCCondDB/interface/CSCChamberTimeCorrectionsValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCDDUMapValues.h"
 #include "OnlineDB/CSCCondDB/interface/CSCMap1Read.h"
-#include "OnlineDB/CSCCondDB/interface/CSCCableReadTest.h"
+#include "OnlineDB/CSCCondDB/interface/CSCChamberTimeCorrectionsReadTest.h"
 
 
 DEFINE_FWK_MODULE(CSCMap1Read);
-DEFINE_FWK_MODULE(CSCCableReadTest);
+DEFINE_FWK_MODULE(CSCChamberTimeCorrectionsReadTest);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCChamberMapValues);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCChamberIndexValues);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCCrateMapValues);
 DEFINE_FWK_EVENTSETUP_SOURCE(CSCDDUMapValues);
-DEFINE_FWK_EVENTSETUP_SOURCE(CSCCableValues);
+DEFINE_FWK_EVENTSETUP_SOURCE(CSCChamberTimeCorrectionsValues);
