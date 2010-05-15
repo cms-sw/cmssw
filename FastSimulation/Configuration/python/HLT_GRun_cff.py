792f7c0932236f014c491bab9bfc56e70845f8a7
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2010-05-15 14:04:45 +0000

    V01-17-36 HLTrigger/Configuration - ConfDB /dev/CMSSW_3_6_0/XXXX/V24

--- FastSimulation/Configuration/python/HLT_GRun_cff.py
+++ FastSimulation/Configuration/python/HLT_GRun_cff.py
@@ -1,4 +1,4 @@
-# /dev/CMSSW_3_6_0/GRun/V18 (CMSSW_3_6_0_HLT6)
+# /dev/CMSSW_3_6_0/GRun/V24 (CMSSW_3_6_0_HLT8)
 # Begin replace statements specific to the FastSim HLT
 # For all HLTLevel1GTSeed objects, make the following replacements:
 #   - L1GtReadoutRecordTag changed from hltGtDigis to gtDigis
@@ -30,7 +30,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_6_0/GRun/V18')
+  tableName = cms.string('/dev/CMSSW_3_6_0/GRun/V24')
 )
 
 
