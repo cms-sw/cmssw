0d42fe0951af97ad21547b77a644ccef0ba7ec09
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2010-05-17 09:42:02 +0000

    add GLB match to MuonQuality

--- DataFormats/MuonReco/interface/MuonQuality.h
+++ DataFormats/MuonReco/interface/MuonQuality.h
@@ -13,14 +13,27 @@ namespace reco {
       float trkRelChi2;
       /// chi2 value for the outer track stub with respect to the global track
       float staRelChi2;
-      
+      /// chi2 value for the STA-TK matching of local position
+      float chi2LocalPosition;
+      /// chi2 value for the STA-TK matching of local momentum
+      float chi2LocalMomentum;
+      /// local distance seperation for STA-TK TSOS matching on same surface
+      float localDistance;
+      /// global delta-Eta-Phi of STA-TK matching
+      float globalDeltaEtaPhi;
+      /// if the STA-TK matching passed the tighter matching criteria
+      bool tightMatch;
+
       /// Kink position for the tracker stub and global track
       math::XYZPoint tkKink_position;
       math::XYZPoint glbKink_position;
       
       MuonQuality():
 	trkKink(0), glbKink(0),
-	trkRelChi2(0), staRelChi2(0)
+	trkRelChi2(0), staRelChi2(0),
+	chi2LocalPosition(0),chi2LocalMomentum(0),
+	localDistance(0),globalDeltaEtaPhi(0),
+	tightMatch(0)
       { }
        
     };
