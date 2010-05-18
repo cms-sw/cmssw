0cfb55c121dd19df1aaeea900a450c679468d7a7
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-05-18 20:00:38 +0000

    cleanup and improved message logger configuration for testing

--- DQM/Physics/test/topDQM_cfg.py
+++ DQM/Physics/test/topDQM_cfg.py
@@ -17,16 +17,6 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source(
     "PoolSource"
     ,fileNames = cms.untracked.vstring(
-##     '/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/14920B0A-0DE8-DE11-B138-002618943926.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/1AD1F37E-0BE8-DE11-8D83-00261894396A.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0006/AC476888-0CE8-DE11-8EDC-0026189438D4.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/4ADBBCAE-37E8-DE11-AE89-00304867C1BA.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/6ABDD43B-13E8-DE11-8A47-001A92971BA0.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/744B08B2-12E8-DE11-A729-001A928116B8.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/A2CC4B57-11E8-DE11-B413-003048678D9A.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/B69516B8-12E8-DE11-982F-00304867BFAE.root'
-##    ,'/store/relval/CMSSW_3_5_0_pre1/RelValTTbar/GEN-SIM-RECO/STARTUP3X_V14-v1/0007/CEFA8143-12E8-DE11-A51F-0018F3D096E4.root' 
-
       '/store/mc/Spring10/TTbar/GEN-SIM-RECO/MC_3XY_V25_S09_preproduction-v2/0106/1A19B479-BA3A-DF11-8E43-0017A4770410.root'
      ,'/store/mc/Spring10/TTbar/GEN-SIM-RECO/MC_3XY_V25_S09_preproduction-v2/0106/1A2CED78-BA3A-DF11-98CD-0017A4771010.root'
      ,'/store/mc/Spring10/TTbar/GEN-SIM-RECO/MC_3XY_V25_S09_preproduction-v2/0106/3AE61B7A-BA3A-DF11-BA4C-0017A477040C.root'
@@ -41,8 +31,16 @@ process.source = cms.Source(
 )
 
 ## load jet corrections
-process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
-process.prefer("ak5CaloL2L3")
+#process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
+#process.prefer("ak5CaloL2L3")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.categories.append('TopSingleLeptonDQM'   )
+process.MessageLogger.cerr.TopSingleLeptonDQM    = cms.untracked.PSet(limit = cms.untracked.int32(1))
+process.MessageLogger.categories.append('TopDiLeptonOfflineDQM')
+process.MessageLogger.cerr.TopDiLeptonOfflineDQM = cms.untracked.PSet(limit = cms.untracked.int32(1))
 
 #process.content = cms.EDAnalyzer("EventContentAnalyzer")
 
