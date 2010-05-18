3c9124afb6ecb93d678460cd69576864ce6ce3ca
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2010-05-18 13:26:22 +0000

    module type corrected (EDProducer->EDFilter)

--- PhysicsTools/PFCandProducer/python/ParticleSelectors/pfElectronsPtGt5_cfi.py
+++ PhysicsTools/PFCandProducer/python/ParticleSelectors/pfElectronsPtGt5_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pfElectronsPtGt5 = cms.EDProducer("PtMinPFCandidateSelector",
+pfElectronsPtGt5 = cms.EDFilter("PtMinPFCandidateSelector",
     src = cms.InputTag("pfAllElectrons"),
     ptMin = cms.double(5.0)
 )
--- PhysicsTools/PFCandProducer/python/ParticleSelectors/pfMuonsPtGt5_cfi.py
+++ PhysicsTools/PFCandProducer/python/ParticleSelectors/pfMuonsPtGt5_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-pfMuonsPtGt5 = cms.EDProducer("PtMinPFCandidateSelector",
+pfMuonsPtGt5 = cms.EDFilter("PtMinPFCandidateSelector",
     src = cms.InputTag("pfAllMuons"),
     ptMin = cms.double(5.0)
 )
