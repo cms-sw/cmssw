95a9fe7be11e0da026ae42f490b922fe8e8ad9dc
Hongxuan Liu <sha1-b70f13a6c621c4c85f2bad0c1693be1e66e48d4f@cern.ch>
2010-05-18 04:14:36 +0000

    Add cfi, cff for standard sequence

--- Validation/HcalRecHits/python/hcalRecHitsValidationSequence_cff.py
+++ Validation/HcalRecHits/python/hcalRecHitsValidationSequence_cff.py
@@ -3,10 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Validation.HcalRecHits.HcalRecHitParam_cfi import *
 
 import Validation.HcalRecHits.HcalRecHitParam_cfi
-AllRecHitsValidation = Validation.HcalRecHits.HcalRecHitParam_cfi.hcalRecoAnalyzer.clone()
-hcalRecHitsValidationSequence = cms.Sequence(AllRecHitsValidation)
 
-AllRecHitsValidation.hcalselector = 'all'
-AllRecHitsValidation.outputFile = ''
+RecHitsValidation = Validation.HcalRecHits.HcalRecHitParam_cfi.hcalRecoAnalyzer.clone()
+NoiseRatesValidation = Validation.HcalRecHits.HcalRecHitParam_cfi.hcalNoiseRates.clone()
 
-
+hcalRecHitsValidationSequence = cms.Sequence(NoiseRatesValidation*RecHitsValidation)
