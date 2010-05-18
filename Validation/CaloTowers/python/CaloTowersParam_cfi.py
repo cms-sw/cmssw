8b756b042a03ad63ce09b773e04f894c3cb660c0
Hongxuan Liu <sha1-b70f13a6c621c4c85f2bad0c1693be1e66e48d4f@cern.ch>
2010-05-18 04:15:12 +0000

    Add cff, cfi for standard sequence

--- Validation/CaloTowers/python/CaloTowersParam_cfi.py
+++ Validation/CaloTowers/python/CaloTowersParam_cfi.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 calotowersAnalyzer = cms.EDAnalyzer("CaloTowersValidation",
-    outputFile = cms.untracked.string(''),
-    CaloTowerCollectionLabel = cms.untracked.string('towerMaker'),
-    hcalselector = cms.untracked.string('HB')
+    outputFile               = cms.untracked.string(''),
+    CaloTowerCollectionLabel = cms.untracked.InputTag('towerMaker'),
+    hcalselector             = cms.untracked.string('all'),
+    mc                       = cms.untracked.string('no')
 )
 
 
--- Validation/CaloTowers/python/calotowersValidationSequence_cff.py
+++ Validation/CaloTowers/python/calotowersValidationSequence_cff.py
@@ -4,7 +4,3 @@ from Validation.CaloTowers.CaloTowersParam_cfi import *
 import Validation.CaloTowers.CaloTowersParam_cfi
 AllCaloTowersValidation = Validation.CaloTowers.CaloTowersParam_cfi.calotowersAnalyzer.clone()
 calotowersValidationSequence = cms.Sequence(AllCaloTowersValidation)
-AllCaloTowersValidation.hcalselector = 'All'
-AllCaloTowersValidation.outputFile = ''
-
-
