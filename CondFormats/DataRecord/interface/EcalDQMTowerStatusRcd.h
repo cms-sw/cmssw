97410cd7b466e4ad11081bbe59331fe1292f5e0d
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2010-05-19 09:33:21 +0000

    definition of the DQM status for ECAL

---
+++ CondFormats/DataRecord/interface/EcalDQMTowerStatusRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALDQMTOWERSTATUSRCD_H
+#define ECALDQMTOWERSTATUSRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalDQMTowerStatusRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalDQMTowerStatusRcd> {};
+#endif
