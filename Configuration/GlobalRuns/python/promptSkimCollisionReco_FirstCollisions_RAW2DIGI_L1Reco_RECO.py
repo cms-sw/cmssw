1ef4933f4637618ff69ac3ac2b9017626198dd1d
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-05-19 15:14:38 +0000

    processing cfg in sync with 1.8 customise_Collision_36X.py

--- Configuration/GlobalRuns/python/promptSkimCollisionReco_FirstCollisions_RAW2DIGI_L1Reco_RECO.py
+++ Configuration/GlobalRuns/python/promptSkimCollisionReco_FirstCollisions_RAW2DIGI_L1Reco_RECO.py
@@ -22,7 +22,7 @@ process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/EventContent/EventContent_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.5 $'),
+    version = cms.untracked.string('$Revision: 1.6 $'),
     annotation = cms.untracked.string('promptSkimCollisionReco nevts:100'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -67,13 +67,17 @@ process.GlobalTag.globaltag = 'GR10_P_V4::All'
 ## TRACKING:
 ## Skip events with HV off
 process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
-process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=20000
 process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
-process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
-process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
-process.thPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
-process.thPLSeeds.ClusterCheckPSet.MaxNumberOfPixelClusters = 2000
+process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 20000
+process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=20000
+process.thTripletsA.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
+process.thTripletsB.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
 
+## local tracker strip reconstruction
+process.OutOfTime.TOBlateBP=0.071
+process.OutOfTime.TIBlateBP=0.036
+        
 ###### FIXES TRIPLETS FOR LARGE BS DISPLACEMENT ######
 
 ### prevent bias in pixel vertex
@@ -81,36 +85,32 @@ process.pixelVertices.useBeamConstraint = False
 
 ### pixelTracks
 #---- new parameters ----
-process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
+process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06)
+process.pixelTracks.RegionFactoryPSet.RegionPSet.originHalfLength = cms.double(40.6)
 
 ### 0th step of iterative tracking
-#---- replaces ----
-process.newSeedFromTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
 #---- new parameters ----
-process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)  # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
-process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 40.6
 
 ### 2nd step of iterative tracking
-#---- replaces ----
-process.secTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
 #---- new parameters ----
-process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)  # was originHalfLength = 17.5; translated assuming sigmaZ ~ 3.8
-process.secTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
-
+process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)
+process.secTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 44.7
 
 ## Primary Vertex
 process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
 process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
-process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconHits = 6
 process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
-process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelHits = 1
-process.offlinePrimaryVerticesWithBS.TkClusParameters.zSeparation = 1
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelLayersWithHits = 2
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconLayersWithHits = 5
+process.offlinePrimaryVerticesWithBS.TkClusParameters.TkGapClusParameters.zSeparation = 1
 process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
 process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
-process.offlinePrimaryVertices.TkFilterParameters.minSiliconHits = 6
 process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
-process.offlinePrimaryVertices.TkFilterParameters.minPixelHits = 1
-process.offlinePrimaryVertices.TkClusParameters.zSeparation = 1
+process.offlinePrimaryVertices.TkFilterParameters.minPixelLayersWithHits = 2
+process.offlinePrimaryVertices.TkFilterParameters.minSiliconLayersWithHits = 5
+process.offlinePrimaryVertices.TkClusParameters.TkGapClusParameters.zSeparation = 1
 
 ## ECAL 
 process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 78, 142 ]
@@ -119,7 +119,7 @@ process.ecalRecHit.ChannelStatusToBeExcluded = [ 1, 2, 3, 4, 8, 9, 10, 11, 12, 1
 process.ecalPreshowerRecHit.ESBaseline = 0
 
 ##Preshower algo for data is different than for MC
-process.ecalPreshowerRecHit.ESRecoAlgo = cms.untracked.int32(1)
+process.ecalPreshowerRecHit.ESRecoAlgo = 1
 
 ## HCAL temporary fixes
 process.hfreco.firstSample  = 3
@@ -131,8 +131,6 @@ process.photons.minSCEtEndcap =5.
 process.photonCore.minSCEt = 5.
 process.conversionTrackCandidates.minSCEt = 5.
 process.conversions.minSCEt =5.
-process.trackerOnlyConversions.AllowTrackBC = False
-process.trackerOnlyConversions.AllowRightBC = False
 process.trackerOnlyConversions.rCut = 2.
 process.trackerOnlyConversions.vtxChi2 = 0.0005
 
