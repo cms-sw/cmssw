307c55336c5b9557759ca7beb00209a2d7c5abce
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-05-20 16:46:43 +0000

    added

---
+++ RecoLuminosity/LumiDB/test/matplotlibLumi.py
@@ -0,0 +1,65 @@
+import sys
+from numpy import arange,sin,pi
+
+batchonly=False
+def destroy(e) :
+    sys.exit()
+import matplotlib
+try:
+    matplotlib.use('TkAgg',warn=False)
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as CanvasBackend
+    from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+    import Tkinter as Tk
+    root=Tk.Tk()
+    root.wm_title("Embedding in TK")
+except ImportError:
+    print 'unable to import GUI backend, switch to batch only mode'
+    matplotlib.use('Agg',warn=False)
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as CanvasBackend
+    batchonly=True
+
+from matplotlib.figure import Figure
+ 
+def drawHTTPstring():
+    fig=Figure(figsize=(5,4),dpi=100)
+    a=fig.add_subplot(111)
+    t=arange(0.0,3.0,0.01)
+    s=sin(2*pi*t)
+    a.plot(t,s)
+    canvas=CanvasBackend(fig)    
+    cherrypy.response.headers['Content-Type']='image/png'
+    buf=StringIO()
+    canvas.print_png(buf)
+    return buf.getvalue()
+    
+def drawBatch():
+    fig=Figure(figsize=(5,4),dpi=100)
+    a=fig.add_subplot(111)
+    t=arange(0.0,3.0,0.01)
+    s=sin(2*pi*t)
+    a.plot(t,s)
+    canvas=CanvasBackend(fig)    
+    canvas.print_figure("a.png")
+    
+def drawInteractive():
+    if batchonly:
+        print 'interactive mode is not available for your setup, exit'
+        sys.exit()
+    fig=Figure(figsize=(5,4),dpi=100)
+    a=fig.add_subplot(111)
+    t=arange(0.0,3.0,0.01)
+    s=sin(2*pi*t)
+    a.plot(t,s)
+    canvas=CanvasBackend(fig,master=root)
+    canvas.show()
+    canvas.get_tk_widget().pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
+    toolbar=NavigationToolbar2TkAgg(canvas,root)
+    toolbar.update()
+    canvas._tkcanvas.pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
+    button = Tk.Button(master=root,text='Quit',command=sys.exit)
+    button.pack(side=Tk.BOTTOM)
+    Tk.mainloop()
+if __name__=='__main__':
+    drawBatch()
+    #drawInteractive()
+    #print drawHTTPstring()
