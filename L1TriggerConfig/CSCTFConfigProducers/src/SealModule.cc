ace5c966a3f210620ea3e2955d5c490df87fdea2
Gian Piero Di Giovanni <sha1-97d7914cdbb84beaaa6fa850086d502bb992ecda@cern.ch>
2010-05-21 13:04:50 +0000

    Analyzer to printout the Global Tag content for CSCTF configuration

--- L1TriggerConfig/CSCTFConfigProducers/src/SealModule.cc
+++ L1TriggerConfig/CSCTFConfigProducers/src/SealModule.cc
@@ -5,10 +5,11 @@
 #include <L1TriggerConfig/CSCTFConfigProducers/interface/CSCTFObjectKeysOnlineProd.h>
 #include <L1TriggerConfig/CSCTFConfigProducers/interface/CSCTFConfigOnlineProd.h>
 #include <L1TriggerConfig/CSCTFConfigProducers/interface/CSCTFAlignmentOnlineProd.h>
-
+#include <L1TriggerConfig/CSCTFConfigProducers/interface/L1MuCSCTFParametersTester.h>
 
 DEFINE_FWK_EVENTSETUP_MODULE(CSCTFConfigProducer);
 DEFINE_FWK_EVENTSETUP_MODULE(CSCTFObjectKeysOnlineProd);  
 DEFINE_FWK_EVENTSETUP_MODULE(CSCTFConfigOnlineProd);
 DEFINE_FWK_EVENTSETUP_MODULE(CSCTFAlignmentOnlineProd);
+DEFINE_FWK_MODULE(L1MuCSCTFParametersTester);
 
