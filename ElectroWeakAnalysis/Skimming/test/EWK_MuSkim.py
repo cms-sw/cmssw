90a59e37af050b87b2477979c07a1013767782b2
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2010-05-24 17:43:04 +0000

    switching to a HLT skim only

--- ElectroWeakAnalysis/Skimming/test/EWK_MuSkim.py
+++ ElectroWeakAnalysis/Skimming/test/EWK_MuSkim.py
@@ -10,15 +10,14 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 # source
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-    'file:/scratch1/cms/data/summer09/aodsim/zmumu/0016/A68B6BD1-FF83-DE11-B579-001E68A99420.root'
-#    'file:/scratch1/cms/data/summer09/aodsim/ppMuX/0010/9C519151-5883-DE11-8BC8-001AA0095119.root'
+    'file:/scratch2/users/fabozzi/8039A1DC-9A5B-DF11-A15E-001A6478706C.root'
     )
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
+process.GlobalTag.globaltag = cms.string('GR_R_35X_V6::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 # HLT filter
@@ -28,19 +27,11 @@ process.EWK_MuHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clo
 #process.EWK_MuHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT8E29")
 #process.EWK_MuHLTFilter.HLTPaths = ["HLT_Mu3", "HLT_DoubleMu3"]
 # Uncomment this to filter on 1E31 HLT menu
-process.EWK_MuHLTFilter.HLTPaths = ["HLT_Mu9", "HLT_DoubleMu3"]
-
-# Muon filter
-process.goodMuons = cms.EDFilter("CandViewSelector",
-  src = cms.InputTag("muons"),
-  cut = cms.string('isGlobalMuon = 1 & abs(eta) < 2.5 & pt > 0'),
-  filter = cms.bool(True)                                
-)
+process.EWK_MuHLTFilter.HLTPaths = ["HLT_L1Mu20", "HLT_L2Mu11", "HLT_Mu9", "HLT_DoubleMu3"]
 
 # Skim path
 process.EWK_MuSkimPath = cms.Path(
-  process.EWK_MuHLTFilter +
-  process.goodMuons
+  process.EWK_MuHLTFilter
 )
 
 # Output module configuration
@@ -64,7 +55,7 @@ process.EWK_MuSkimOutputModule = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('EWKMuSkim'),
         dataTier = cms.untracked.string('USER')
    ),
-   fileName = cms.untracked.string('file:testEWKMuSkim.root')
+   fileName = cms.untracked.string('EWK_MuSkim_SD_Mu.root')
 )
 
 process.outpath = cms.EndPath(process.EWK_MuSkimOutputModule)
