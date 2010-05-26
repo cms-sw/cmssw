f398b226b1835fc38b722f80bb3d091107af1d1e
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2010-05-26 16:04:30 +0000

    report name of file which failed its unit tests

--- FWCore/ParameterSet/test/runPythonTests.sh
+++ FWCore/ParameterSet/test/runPythonTests.sh
@@ -7,6 +7,6 @@ for file in ${CMSSW_BASE}/src/FWCore/ParameterSet/python/*.py
 do
   bn=`basename $file`
   if [ "$bn" != "__init__.py" ]; then
-     python "$file" || die 'unit tests for $file failed' $?
+     python "$file" || die "unit tests for $bn failed" $?
   fi
 done
