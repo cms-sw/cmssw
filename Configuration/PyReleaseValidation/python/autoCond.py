d2c278b95b5128ed55660066cd5c94be4d935658
Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch>
2010-05-27 09:21:28 +0000

    fix typo (missing ,)

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -4,6 +4,6 @@ autoCond = { 'mc'      : 'MC_37Y_V4::All',
              'startup' : 'START37_V4::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_37R_V4::All',
-             'com10'   : 'GR_R_37X_V4::All'
+             'com10'   : 'GR_R_37X_V4::All',
              'hltonline' : 'GR_R_37X_V4::All', # same as com10 until H tag available
              }
