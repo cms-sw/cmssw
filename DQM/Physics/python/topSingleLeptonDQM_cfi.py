872d3587c52a1da7579dca4ef6c2efe92b1c7f53
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-05-27 18:29:00 +0000

    added new selection instance for testing

--- DQM/Physics/python/topSingleLeptonDQM_cfi.py
+++ DQM/Physics/python/topSingleLeptonDQM_cfi.py
@@ -357,3 +357,88 @@ topElecPlusJetsOfflineDQM = cms.EDAnalyzer("TopSingleLeptonDQM",
     ),
   )
 )
+
+topSingleMuonLooseDQM = cms.EDAnalyzer("TopSingleLeptonDQM",
+  ## ------------------------------------------------------
+  ## SETUP
+  ##
+  ## configuration of the MonitoringEnsemble(s)
+  ## [mandatory] : optional PSets may be omitted
+  ##
+  setup = cms.PSet(
+    ## sub-directory to write the monitor histograms to
+    ## [mandatory] : should not be changed w/o explicit
+    ## communication to TopCom!
+    directory = cms.string("Physics/Top/TopSingleMuonLooseDQM/"),
+      ## [mandatory]
+    sources = cms.PSet(
+      muons = cms.InputTag("muons"),
+      elecs = cms.InputTag("gsfElectrons"),
+      jets  = cms.InputTag("ak5CaloJets"),
+      mets  = cms.VInputTag("met", "tcMet", "pfMet")
+    ),
+    ## [optional] : when omitted the verbosity level is set to STANDARD
+    monitoring = cms.PSet(
+      verbosity = cms.string("DEBUG")
+    ),
+    ## [optional] : when omitted all monitoring plots for the muon
+    ## will be filled w/o preselection                                           
+    muonExtras = cms.PSet(
+      isolation = cms.string("(isolationR03.sumPt+isolationR03.emEt+isolationR03.hadEt)/pt<0.2"),
+    ),
+    ## [optional] : when omitted all monitoring plots for the electron
+    ## will be filled w/o preselection
+    jetExtras = cms.PSet(
+      jetCorrector = cms.string("ak5CaloL2L3")
+    ),
+    ## [optional] : when omitted no mass window will be applied
+    ## for the W mass befor filling the event monitoring plots
+    massExtras = cms.PSet(
+      lowerEdge = cms.double( 70.),
+      upperEdge = cms.double(110.)
+    ),
+    ## [optional] : when omitted the monitoring plots for triggering
+    ## will be empty
+    triggerExtras = cms.PSet(
+      src   = cms.InputTag("TriggerResults","","HLT"),
+      paths = cms.vstring(['HLT_Mu9:HLT_QuadJet30',
+                           'HLT_Mu15:HLT_QuadJet30',
+                           'HLT_Ele15_SW_L1R:HLT_QuadJet30'])
+    )
+  ),
+  ## ------------------------------------------------------
+  ## PRESELECTION
+  ##
+  ## setup of the event preselection, which will not
+  ## be monitored
+  ## [mandatory] : but may be empty
+  ##
+  preselection = cms.PSet(
+    ## [optional] : when omitted no preselection is applied
+    trigger = cms.PSet(
+      src    = cms.InputTag("TriggerResults","","HLT"),
+      select = cms.vstring(['HLT_Mu9'])
+    ),
+    ## [optional] : when omitted no preselection is applied
+    vertex = cms.PSet(
+      src    = cms.InputTag("offlinePrimaryVertices"),
+      select = cms.string('abs(x)<1. & abs(y)<1. & abs(z)<20. & tracksSize>3 & !isFake')
+    )
+  ),
+  ## ------------------------------------------------------
+  ## SELECTION
+  ##
+  ## monitor histrograms are filled after each selection
+  ## step, the selection is applied in the order defined
+  ## by this vector
+  ## [mandatory] : may be empty or contain an arbitrary
+  ## number of PSets
+  selection = cms.VPSet(
+    cms.PSet(
+      label  = cms.string("muons:step0"),
+      src    = cms.InputTag("muons"),
+      select = cms.string("pt > 10 & abs(eta)<2.1"),
+      min    = cms.int32(1),
+    ),
+  )
+)
