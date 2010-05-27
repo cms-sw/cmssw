ae35d38451f953770b14b9290db0c1d268f5201a
Anton Anastassov <sha1-3db3eeb8464f657978ce5211c2d36ab42dec8d78@cern.ch>
2010-05-27 23:08:05 +0000

    Modified configuration to be used with the updated versions of ecal severity levels implemented in
    RecoLocalCalo/EcalRecAlgos >V00-11-02-02 (36x)
    RecoLocalCalo/EcalRecAlgos  >V00-12-10 (37x)
    
    This setting will not use the newly added cleanup method that
    uses timing.
    Need to change the parameter to turn on this cleanup.

--- RecoLocalCalo/CaloTowersCreator/python/calotowermaker_cfi.py
+++ RecoLocalCalo/CaloTowersCreator/python/calotowermaker_cfi.py
@@ -99,7 +99,7 @@ calotowermaker = cms.EDProducer("CaloTowersCreator",
 # 
     # acceptable severity level
     HcalAcceptSeverityLevel = cms.uint32(9),
-    EcalAcceptSeverityLevel = cms.uint32(1),
+    EcalAcceptSeverityLevel = cms.uint32(3),
        
     # use of recovered hits
     UseHcalRecoveredHits = cms.bool(True),
