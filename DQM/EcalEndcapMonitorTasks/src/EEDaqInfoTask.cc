24ef401bfdc1ef596e2a7eb1a0ba73e999689695
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-05-27 09:23:00 +0000

    add check for record key

--- DQM/EcalEndcapMonitorTasks/src/EEDaqInfoTask.cc
+++ DQM/EcalEndcapMonitorTasks/src/EEDaqInfoTask.cc
@@ -88,10 +88,15 @@ void EEDaqInfoTask::beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, 
     }
   }
 
+  if ( !iSetup.find( edm::eventsetup::EventSetupRecordKey::makeKey<EcalDAQTowerStatusRcd>() ) ) {
+    edm::LogWarning("EEDaqInfoTask") << "EcalDAQTowerStatus record not found";
+    return;
+  }
+
   edm::ESHandle<EcalDAQTowerStatus> pDAQStatus;
   iSetup.get<EcalDAQTowerStatusRcd>().get(pDAQStatus);
   if ( !pDAQStatus.isValid() ) {
-    edm::LogWarning("EBDaqInfoTask") << "EcalDAQTowerStatus record not valid";
+    edm::LogWarning("EEDaqInfoTask") << "EcalDAQTowerStatus record not valid";
     return;
   }
   const EcalDAQTowerStatus *daqStatus = pDAQStatus.product();
