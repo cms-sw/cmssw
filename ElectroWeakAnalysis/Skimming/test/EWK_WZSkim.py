084093292cf11a50264a05f32d81d6467f8e8cfb
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2010-05-27 14:53:52 +0000

    W Z skim, version 0.0

---
+++ ElectroWeakAnalysis/Skimming/test/EWK_WZSkim.py
@@ -0,0 +1,129 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("EWKWZSkim")
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options.FailPath = cms.untracked.vstring('ProductNotFound')
+
+# source
+process.source = cms.Source("PoolSource", 
+     fileNames = cms.untracked.vstring(
+    'file:/scratch2/users/fabozzi/spring10/wmn/24BF0D12-DF46-DF11-BA71-001D0968F2F6.root'
+    )
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('START36_V8::All')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+# HLT filter
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.EWK_MuHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.EWK_MuHLTFilter.HLTPaths = ["HLT_Mu9"]
+
+# Muon candidates filters 
+process.goodMuons = cms.EDFilter("MuonSelector",
+  src = cms.InputTag("muons"),
+  cut = cms.string('pt > 20 && abs(eta)<2.4 && isGlobalMuon = 1'),
+  filter = cms.bool(True)                                
+)
+
+# Z->mumu candidates
+process.dimuons = cms.EDProducer("CandViewShallowCloneCombiner",
+    checkCharge = cms.bool(True),
+    cut = cms.string('mass > 60'),
+    decay = cms.string("goodMuons@+ goodMuons@-")
+)
+
+# Z filters
+process.dimuonsFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("dimuons"),
+    minNumber = cms.uint32(1)
+)
+
+# WMuNu candidates
+process.load("ElectroWeakAnalysis.WMuNu.wmunusProducer_cfi")
+# WMuNu candidates selectors
+process.load("ElectroWeakAnalysis.WMuNu.WMuNuSelection_cff")
+process.selcorMet.JetTag = cms.untracked.InputTag("ak5CaloJets")
+process.seltcMet.JetTag = cms.untracked.InputTag("ak5CaloJets")
+process.selpfMet.JetTag = cms.untracked.InputTag("ak5CaloJets")
+process.selcorMet.TrigTag = cms.untracked.InputTag("TriggerResults::HLT")
+process.seltcMet.TrigTag = cms.untracked.InputTag("TriggerResults::HLT")
+process.selpfMet.TrigTag = cms.untracked.InputTag("TriggerResults::HLT")
+
+# W filters
+#
+#process.pfMetWMuNusFilter = cms.EDFilter("CandViewCountFilter",
+#    src = cms.InputTag("pfMetWMuNus"),
+#    minNumber = cms.uint32(1)
+#)
+#
+#process.tcMetWMuNusFilter = cms.EDFilter("CandViewCountFilter",
+#    src = cms.InputTag("tcMetWMuNus"),
+#    minNumber = cms.uint32(1)
+#)
+#
+#process.corMetWMuNusFilter = cms.EDFilter("CandViewCountFilter",
+#    src = cms.InputTag("corMetWMuNus"),
+#    minNumber = cms.uint32(1)
+#)
+
+
+# Skim paths
+process.EWK_dimuonsPath = cms.Path(
+    process.goodMuons *
+    process.dimuons *
+    process.dimuonsFilter
+    )
+
+process.EWK_corMetWMuNusPath = cms.Path(
+    process.corMetWMuNus *
+    process.selcorMet
+)
+
+process.EWK_tcMetWMuNusPath = cms.Path(
+    process.tcMetWMuNus *
+    process.seltcMet
+)
+
+process.EWK_pfMetWMuNusPath = cms.Path(
+    process.pfMetWMuNus *
+    process.selpfMet
+)
+
+# Output module configuration
+from Configuration.EventContent.EventContent_cff import *
+EWK_WZSkimEventContent = cms.PSet(
+    outputCommands = cms.untracked.vstring()
+)
+EWK_WZSkimEventContent.outputCommands.extend(AODEventContent.outputCommands)
+
+EWK_WZSkimEventSelection = cms.PSet(
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring(
+           'EWK_dimuonsPath',
+           'EWK_corMetWMuNusPath',
+           'EWK_tcMetWMuNusPath',
+           'EWK_pfMetWMuNusPath')
+    )
+)
+
+process.EWK_WZSkimOutputModule = cms.OutputModule("PoolOutputModule",
+    EWK_WZSkimEventContent,
+    EWK_WZSkimEventSelection,
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('EWKWZSkim'),
+        dataTier = cms.untracked.string('USER')
+   ),
+   fileName = cms.untracked.string('EWK_WZSkim_SD_Mu.root')
+)
+
+process.outpath = cms.EndPath(process.EWK_WZSkimOutputModule)
+
+
