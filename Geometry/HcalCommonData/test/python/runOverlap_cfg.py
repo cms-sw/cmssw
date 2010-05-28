a848d39ce3d8ee7df7a08d67f92137b0165ec751
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-05-28 18:52:17 +0000

    Additional file for fibre bundle

--- Geometry/HcalCommonData/test/python/runOverlap_cfg.py
+++ Geometry/HcalCommonData/test/python/runOverlap_cfg.py
@@ -28,34 +28,38 @@ process.Timing = cms.Service("Timing")
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
     moduleSeeds = cms.PSet(
+        generator = cms.untracked.uint32(456789),
         g4SimHits = cms.untracked.uint32(9876),
         VtxSmeared = cms.untracked.uint32(12345)
     ),
     sourceSeed = cms.untracked.uint32(98765)
 )
 
-process.source = cms.Source("FlatRandomEGunSource",
-    PGunParameters = cms.untracked.PSet(
-        PartID = cms.untracked.vint32(211),
-        MaxEta = cms.untracked.double(0.5655),
-        MaxPhi = cms.untracked.double(-0.1309),
-        MinEta = cms.untracked.double(0.5655),
-        MinE = cms.untracked.double(9.99),
-        MinPhi = cms.untracked.double(-0.1309),
-        MaxE = cms.untracked.double(10.01)
+process.source = cms.Source("EmptySource",
+    firstRun        = cms.untracked.uint32(1),
+    firstEvent      = cms.untracked.uint32(1)
+)
+
+process.generator = cms.EDProducer("FlatRandomEGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(211),
+        MinEta = cms.double(0.5655),
+        MaxEta = cms.double(0.5655),
+        MinPhi = cms.double(-0.1309),
+        MaxPhi = cms.double(-0.1309),
+        MinE   = cms.double(9.99),
+        MaxE   = cms.double(10.01)
     ),
     Verbosity = cms.untracked.int32(0),
-    AddAntiParticle = cms.untracked.bool(False),
-    firstRun = cms.untracked.uint32(1)
+    AddAntiParticle = cms.bool(False)
 )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
-process.p1 = cms.Path(process.g4SimHits)
+process.p1 = cms.Path(process.generator*process.g4SimHits)
 process.g4SimHits.UseMagneticField = False
 process.g4SimHits.Physics.DefaultCutValue = 1.
-process.g4SimHits.Generator.HepMCProductLabel = 'source'
 process.g4SimHits.Watchers = cms.VPSet(cms.PSet(
     Resolution = cms.untracked.int32(1000),
     type       = cms.string('CheckOverlap'),
