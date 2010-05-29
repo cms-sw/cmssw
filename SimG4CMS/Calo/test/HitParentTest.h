d6aa35bd339b2ecc254276af87094e773dd265ea
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-05-29 22:29:28 +0000

    Correct HitParentTest

--- SimG4CMS/Calo/test/HitParentTest.h
+++ SimG4CMS/Calo/test/HitParentTest.h
@@ -10,7 +10,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
