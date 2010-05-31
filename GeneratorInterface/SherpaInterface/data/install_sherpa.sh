18b0c67e6556f233e727634198c3d12a022ae74b
Markus Karl Merschmeyer <sha1-b8baadebb85be3a7f9818ee600a69f30b411b9d7@cern.ch>
2010-05-31 16:27:56 +0000

    COMIX-compatible scripts

--- GeneratorInterface/SherpaInterface/data/install_sherpa.sh
+++ GeneratorInterface/SherpaInterface/data/install_sherpa.sh
@@ -638,9 +638,11 @@ if [ "$HEPMC" = "TRUE" ]; then
 echo " <I> HepMC2 version "${HEPMC2VER}" installed in "${HEPMC2DIR}
 fi
 if [ "$LHAPDF" = "TRUE" ]; then
+echo "\n\n\n\n\n\n\n\n\n"
 echo " <I> LHAPDF version "${LHAPDFVER}" installed in "${LHAPDFDIR}
 echo " <I>  -> before using SHERPA please define"
 echo " <I>  -> export LHAPATH="${pdfdir}
+echo "\n\n\n\n\n\n\n\n\n"
 fi
 echo " <I> SHERPA version "${SHERPAVER}" installed in "${SHERPADIR}
 
