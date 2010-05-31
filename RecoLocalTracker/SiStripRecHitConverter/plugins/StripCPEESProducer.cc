f1924092b5b17f77f624e92bfb6258ae0659e584
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2010-05-31 14:49:51 +0000

    Access more dependent conditions

--- RecoLocalTracker/SiStripRecHitConverter/plugins/StripCPEESProducer.cc
+++ RecoLocalTracker/SiStripRecHitConverter/plugins/StripCPEESProducer.cc
@@ -3,6 +3,11 @@
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngle.h"
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngle2.h"
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEgeometric.h"
+#include "CondFormats/SiStripObjects/interface/SiStripConfObject.h"
+#include "CondFormats/SiStripObjects/interface/SiStripLatency.h"
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBadStrip.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
 StripCPEESProducer::StripCPEESProducer(const edm::ParameterSet & p) 
@@ -15,14 +20,14 @@ StripCPEESProducer::StripCPEESProducer(const edm::ParameterSet & p)
 boost::shared_ptr<StripClusterParameterEstimator> StripCPEESProducer::
 produce(const TkStripCPERecord & iRecord) 
 { 
-  edm::ESHandle<MagneticField> magfield;
-  iRecord.getRecord<IdealMagneticFieldRecord>().get(magfield );
-  
-  edm::ESHandle<TrackerGeometry> pDD;
-  iRecord.getRecord<TrackerDigiGeometryRecord>().get( pDD );
-  
-  edm::ESHandle<SiStripLorentzAngle> lorentzAngle;
-  iRecord.getRecord<SiStripLorentzAngleRcd>().get(lorentzAngle);
+  edm::ESHandle<TrackerGeometry> pDD;  iRecord.getRecord<TrackerDigiGeometryRecord>().get( pDD );
+  edm::ESHandle<MagneticField> magfield;  iRecord.getRecord<IdealMagneticFieldRecord>().get(magfield );
+  edm::ESHandle<SiStripLorentzAngle> lorentzAngle;  iRecord.getRecord<SiStripLorentzAngleRcd>().get(lorentzAngle);
+  edm::ESHandle<SiStripConfObject> confObj;  iRecord.getRecord<SiStripConfObjectRcd>().get(confObj);
+  edm::ESHandle<SiStripLatency> latency;  iRecord.getRecord<SiStripLatencyRcd>().get(latency);
+  edm::ESHandle<SiStripNoises> noise;  iRecord.getRecord<SiStripNoisesRcd>().get(noise);
+  edm::ESHandle<SiStripApvGain> gain;  iRecord.getRecord<SiStripApvGainRcd>().get(gain);
+  edm::ESHandle<SiStripBadStrip> bad;  iRecord.getRecord<SiStripBadChannelRcd>().get(bad);
 
   if(name_=="SimpleStripCPE") 
     cpe_ = boost::shared_ptr<StripClusterParameterEstimator>(new StripCPE(               pset_, magfield.product(), pDD.product(), lorentzAngle.product() ));  
