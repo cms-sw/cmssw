e9a46e6faeed2db1871ca7b423a7a198a204f60d
Burt Betchart <sha1-60bb7eb19bf777a37a7ee89dfcf353857811e301@cern.ch>
2010-05-31 14:20:22 +0000

    Additional records dependencies: latency,noise,gain,confObject

--- RecoLocalTracker/Records/interface/TkStripCPERecord.h
+++ RecoLocalTracker/Records/interface/TkStripCPERecord.h
@@ -6,11 +6,19 @@
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"                
-#include "CondFormats/DataRecord/interface/SiStripLorentzAngleRcd.h"
+#include "CondFormats/DataRecord/interface/SiStripCondDataRecords.h"
 #include "boost/mpl/vector.hpp"
 
 class  TkStripCPERecord: public edm::eventsetup::DependentRecordImplementation<TkStripCPERecord,
-  boost::mpl::vector<TrackerDigiGeometryRecord,IdealMagneticFieldRecord,SiStripLorentzAngleRcd> > {};
+  boost::mpl::vector<  TrackerDigiGeometryRecord,
+                       IdealMagneticFieldRecord,
+                       SiStripLorentzAngleRcd,
+                       SiStripConfObjectRcd,
+                       SiStripLatencyRcd,
+                       SiStripNoisesRcd,
+                       SiStripApvGainRcd,
+                       SiStripBadChannelRcd
+                       > > {};
 
 #endif 
 
