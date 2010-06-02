49549eebe39e97333023d980729ab546cdb82620
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2010-06-02 15:19:47 +0000

    New colinearity constraint

--- RecoVertex/KinematicFit/src/ColinearityKinematicConstraint.cc
+++ RecoVertex/KinematicFit/src/ColinearityKinematicConstraint.cc
@@ -31,9 +31,9 @@ AlgebraicVector  ColinearityKinematicConstraint::value(const vector<KinematicSta
   double pt2  = sqrt(p2(3)*p2(3)+p2(4)*p2(4));
 
   // H_phi:
-  res(1)  = (pt1 - p1vx) / p1vy - (pt2 - p2vx) / p2vy;
+  res(1)  = (p1vy * p2vx) - (p1vx * p2vy);
   // H_theta:
-  if (dimension == PhiTheta) res(2)  = p1(5)/pt1 - p2(5)/pt2;
+  if (dimension == PhiTheta) res(2)  = (p1(5)*p2vx) - (p1vx*p2(5));
 
   return res;
 }
@@ -71,23 +71,23 @@ AlgebraicMatrix ColinearityKinematicConstraint::parametersDerivative(const vecto
   // H_phi:
 
   //x1 and x2 derivatives: 1st and 8th elements
-  res(1,1) =   a_1*(pt1 - p1vx)/(p1vy*p1vy);
-  res(1,8) = - a_2*(pt2 - p2vx)/(p2vy*p2vy);
+  res(1,1) =  -a_1*p2vx;
+  res(1,8) =   a_2*p1vx;
 
   //y1 and y2 derivatives: 2nd and 9th elements:
-  res(1,2) = -a_1/p1vy;
-  res(1,9) =  a_2/p2vy;
+  res(1,2) = -a_1*p2vy;
+  res(1,9) =  a_2*p1vy;
 
   //z1 and z2 components: 3d and 10th elmnets stay 0:
   res(1,3)  = 0.; res(1,10) = 0.;
 
   //px1 and px2 components: 4th and 11th elements:
-  res(1,4)  = -(pt1 - p1vx)/(pt1*p1vy);
-  res(1,11) =  (pt2 - p2vx)/(pt2*p2vy);
+  res(1,4)  = -p2vy;
+  res(1,11) =  p1vy;
 
   //py1 and py2 components: 5th and 12 elements:
-  res(1,5)  =  (pt1*p1vx - pt1*pt1 + p1(4)*p1vy)/(pt1*p1vy*p1vy);
-  res(1,12) = -(pt2*p2vx - pt2*pt2 + p2(4)*p2vy)/(pt2*p2vy*p2vy);
+  res(1,5)  =  p2vx;
+  res(1,12) = -p1vx;
 
   //pz1 and pz2 components: 6th and 13 elements:
   res(1,6)  = 0.; res(1,13) = 0.;
@@ -100,21 +100,22 @@ AlgebraicMatrix ColinearityKinematicConstraint::parametersDerivative(const vecto
     res(2,1) =  0.; res(2,8) = 0.;
 
     //y1 and y2 derivatives: 2nd and 9th elements:
-    res(2,2) = 0.; res(2,9) = 0.;
+    res(2,2) = - a_1 *p2vz;
+    res(2,9) = a_1 * p1vz;
 
     //z1 and z2 components: 3d and 10th elmnets stay 0:
     res(2,3) = 0.; res(2,10) = 0.;
 
-    res(2,4)  = - p1(3)*p1vz / (pt1*pt1*pt1);
-    res(2,11) =   p2(3)*p2vz / (pt2*pt2*pt2);
+    res(2,4)  = - p2vz;
+    res(2,11) =   p1vz;
 
     //py1 and py2 components: 5th and 12 elements:
-    res(2,5)  = - p1(4)*p1vz / (pt1*pt1*pt1);
-    res(2,12) =   p2(4)*p2vz / (pt2*pt2*pt2);
+    res(2,5)  = 0;
+    res(2,12) = 0;
 
     //pz1 and pz2 components: 6th and 13 elements:
-    res(2,6)  =   1/pTot1;
-    res(2,13) = - 1/pTot2;
+    res(2,6)  =  p2vx;
+    res(2,13) = -p1vx;
     //mass components: 7th and 14th elements:
     res(2,7)  = 0.; res(2,14) = 0.;
   }
@@ -140,26 +141,30 @@ AlgebraicMatrix ColinearityKinematicConstraint::positionDerivative(const vector<
 
   double p1vx = p1(3) - a_1*(point.y() - p1(1));
   double p1vy = p1(4) + a_1*(point.x() - p1(0));
+  double p1vz = p1(5);
   double pt1 = sqrt(p1(3)*p1(3)+p1(4)*p1(4));
 
   double p2vx = p2(3) - a_2*(point.y() - p2(1));
   double p2vy = p2(4) + a_2*(point.x() - p2(0));
+  double p2vz = p2(5);
   double pt2  = sqrt(p2(3)*p2(3)+p2(4)*p2(4));
 
   // H_phi:
 
   // xv component
-  res(1,1) = -a_1 *(pt1 - p1vx) / (p1vy*p1vy) + a_2 *(pt2 - p2vx) / (p2vy*p2vy);
+  res(1,1) = - a_2 * p1vx + a_1 * p2vx;
 
   //yv component
-  res(1,2) = a_1/p1vy - a_2/p2vy;
+  res(1,2) = - a_2 * p1vy + a_1 * p2vy;
 
   //zv component
   res(1,3) = 0.;
 
   // H_theta: no correlation with vertex position
   if (dimension == PhiTheta) {
-    res(2,1) = 0.; res(2,2) = 0.; res(2,3) = 0.;
+    res(2,1) = 0.;
+    res(2,2) = -a_2 * p1vz + a_1 * p2vz; 
+    res(2,3) = 0.;
   }
 
   return res;
