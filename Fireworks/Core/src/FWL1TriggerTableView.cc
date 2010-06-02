de88590cae0ba75b4a1fa4a24b7136d26fa4e932
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-06-02 13:30:01 +0000

    Add technical triggers

--- Fireworks/Core/src/FWL1TriggerTableView.cc
+++ Fireworks/Core/src/FWL1TriggerTableView.cc
@@ -20,38 +20,38 @@ static const std::string kColumns = "columns";
 static const std::string kSortColumn = "sortColumn";
 static const std::string kDescendingSort = "descendingSort";
 
-FWL1TriggerTableView::FWL1TriggerTableView(TEveWindowSlot* parent, FWL1TriggerTableViewManager *manager)
-   : m_manager(manager),
-     m_tableManager(new FWL1TriggerTableViewTableManager(this)),
-     m_tableWidget(0),
-     m_currentColumn(-1)
+FWL1TriggerTableView::FWL1TriggerTableView( TEveWindowSlot* parent, FWL1TriggerTableViewManager *manager )
+   : m_manager( manager ),
+     m_tableManager( new FWL1TriggerTableViewTableManager( this ) ),
+     m_tableWidget( 0 ),
+     m_currentColumn( -1 )
 {
-   m_columns.push_back(Column("Algorithm Name"));
-   m_columns.push_back(Column("Result"));
-   m_columns.push_back(Column("Bit Number"));
-   m_eveWindow = parent->MakeFrame(0);
+	m_columns.push_back( Column( "Algorithm Name" ));
+	m_columns.push_back( Column( "Result" ) );
+	m_columns.push_back( Column( "Bit Number" ) );
+   m_eveWindow = parent->MakeFrame( 0 );
    TGCompositeFrame *frame = m_eveWindow->GetGUICompositeFrame();
 
-   m_vert = new TGVerticalFrame(frame);
-   frame->AddFrame(m_vert, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY));
+   m_vert = new TGVerticalFrame( frame );
+   frame->AddFrame( m_vert, new TGLayoutHints( kLHintsExpandX | kLHintsExpandY ));
 
-   m_tableWidget = new FWTableWidget(m_tableManager, m_vert);
-   resetColors(m_manager->colorManager());
-   m_tableWidget->SetHeaderBackgroundColor(gVirtualX->GetPixel(kWhite));
-   m_tableWidget->Connect("columnClicked(Int_t,Int_t,Int_t)", "FWL1TriggerTableView",
-                          this, "columnSelected(Int_t,Int_t,Int_t)");
-   m_vert->AddFrame(m_tableWidget, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY));
+   m_tableWidget = new FWTableWidget( m_tableManager, m_vert );
+   resetColors( m_manager->colorManager() );
+   m_tableWidget->SetHeaderBackgroundColor( gVirtualX->GetPixel( kWhite ));
+   m_tableWidget->Connect( "columnClicked(Int_t,Int_t,Int_t)", "FWL1TriggerTableView",
+								  this, "columnSelected(Int_t,Int_t,Int_t)");
+   m_vert->AddFrame( m_tableWidget, new TGLayoutHints( kLHintsExpandX | kLHintsExpandY ));
    dataChanged();
    frame->MapSubwindows();
    frame->Layout();
    frame->MapWindow();
 }
 
-FWL1TriggerTableView::~FWL1TriggerTableView(void)
+FWL1TriggerTableView::~FWL1TriggerTableView( void )
 {
    // take out composite frame and delete it directly (without the timeout)
    TGCompositeFrame *frame = m_eveWindow->GetGUICompositeFrame();
-   frame->RemoveFrame(m_vert);
+   frame->RemoveFrame( m_vert );
    delete m_vert;
 
    m_eveWindow->DestroyWindowAndSlot();
@@ -59,83 +59,99 @@ FWL1TriggerTableView::~FWL1TriggerTableView(void)
 }
 
 void
-FWL1TriggerTableView::setBackgroundColor(Color_t iColor)
+FWL1TriggerTableView::setBackgroundColor( Color_t iColor )
 {
-   m_tableWidget->SetBackgroundColor(gVirtualX->GetPixel(iColor));
+	m_tableWidget->SetBackgroundColor( gVirtualX->GetPixel( iColor ));
 }
 
-void FWL1TriggerTableView::resetColors (const FWColorManager &manager)
+void FWL1TriggerTableView::resetColors( const FWColorManager &manager )
 {
-   m_tableWidget->SetBackgroundColor(gVirtualX->GetPixel(manager.background()));
-   m_tableWidget->SetLineSeparatorColor(gVirtualX->GetPixel(manager.foreground()));
+   m_tableWidget->SetBackgroundColor( gVirtualX->GetPixel( manager.background()));
+   m_tableWidget->SetLineSeparatorColor( gVirtualX->GetPixel( manager.foreground()));
 }
 
 TGFrame*
-FWL1TriggerTableView::frame(void) const
+FWL1TriggerTableView::frame( void ) const
 {
    return 0;
 }
 
 const std::string&
-FWL1TriggerTableView::typeName(void) const
+FWL1TriggerTableView::typeName( void ) const
 {
    return staticTypeName();
 }
 
 void
-FWL1TriggerTableView::saveImageTo(const std::string& iName) const
+FWL1TriggerTableView::saveImageTo( const std::string& iName ) const
 {
-  fwLog(fwlog::kWarning) << "FWL1TriggerTableView::saveImageTo is not implemented." << std::endl;
+	fwLog( fwlog::kWarning ) << "FWL1TriggerTableView::saveImageTo is not implemented." << std::endl;
 }
 
-void FWL1TriggerTableView::dataChanged(void)
+void FWL1TriggerTableView::dataChanged( void )
 {
    m_columns.at(0).values.clear();
    m_columns.at(1).values.clear();
    m_columns.at(2).values.clear();
-   if(! m_manager->items().empty() && m_manager->items().front() != 0)
+   if(! m_manager->items().empty() && m_manager->items().front() != 0 )
    {
-      if(fwlite::Event* event = const_cast<fwlite::Event*>(m_manager->items().front()->getEvent()))
+		if( fwlite::Event* event = const_cast<fwlite::Event*>( m_manager->items().front()->getEvent()))
       {
-	 fwlite::Handle<L1GtTriggerMenuLite> triggerMenuLite;
-	 fwlite::Handle<L1GlobalTriggerReadoutRecord> triggerRecord;
+			fwlite::Handle<L1GtTriggerMenuLite> triggerMenuLite;
+			fwlite::Handle<L1GlobalTriggerReadoutRecord> triggerRecord;
 
-	 try
-	 {
-	    // FIXME: Replace magic strings with configurable ones
-	    triggerMenuLite.getByLabel(event->getRun(), "l1GtTriggerMenuLite", "", "");
-	    triggerRecord.getByLabel(*event, "gtDigis", "", "");
-	 }
-	 catch(cms::Exception&)
-	 {
-	    fwLog(fwlog::kWarning) << "FWL1TriggerTableView: no L1Trigger menu is available." << std::endl;
-	    m_tableManager->dataChanged();
-	    return;
-	 }
+			try
+			{
+				// FIXME: Replace magic strings with configurable ones
+				triggerMenuLite.getByLabel( event->getRun(), "l1GtTriggerMenuLite", "", "" );
+				triggerRecord.getByLabel( *event, "gtDigis", "", "" );
+			}
+			catch(cms::Exception&)
+			{
+				fwLog( fwlog::kWarning ) << "FWL1TriggerTableView: no L1Trigger menu is available." << std::endl;
+				m_tableManager->dataChanged();
+				return;
+			}
 	  
-	 if(triggerMenuLite.isValid() && triggerRecord.isValid())
-	 {
-	    const L1GtTriggerMenuLite::L1TriggerMap& algorithmMap = triggerMenuLite->gtAlgorithmMap();
+			if( triggerMenuLite.isValid() && triggerRecord.isValid() )
+			{
+				const L1GtTriggerMenuLite::L1TriggerMap& algorithmMap = triggerMenuLite->gtAlgorithmMap();
 
-	    const DecisionWord dWord = triggerRecord->decisionWord();
-	    for(L1GtTriggerMenuLite::CItL1Trig itTrig = algorithmMap.begin(), itTrigEnd = algorithmMap.end();
-		itTrig != itTrigEnd; ++itTrig)
-	    {
-	       const unsigned int bitNumber = itTrig->first;
-	       const std::string& aName = itTrig->second;
-	       int errorCode = 0;
-	       const bool result = triggerMenuLite->gtTriggerResult(aName, dWord, errorCode);
+				const DecisionWord dWord = triggerRecord->decisionWord();
+				for( L1GtTriggerMenuLite::CItL1Trig itTrig = algorithmMap.begin(), itTrigEnd = algorithmMap.end();
+					 itTrig != itTrigEnd; ++itTrig)
+				{
+					const unsigned int bitNumber = itTrig->first;
+					const std::string& aName = itTrig->second;
+					int errorCode = 0;
+					const bool result = triggerMenuLite->gtTriggerResult( aName, dWord, errorCode );
 
-	       m_columns.at(0).values.push_back(aName);
-	       m_columns.at(1).values.push_back(Form("%d",result));
-	       m_columns.at(2).values.push_back(Form("%d",bitNumber));
-	    }
-	 }
+					m_columns.at(0).values.push_back( aName );
+					m_columns.at(1).values.push_back( Form( "%d", result ));
+					m_columns.at(2).values.push_back( Form( "%d", bitNumber ));
+				}
+				const TechnicalTriggerWord ttWord = triggerRecord->technicalTriggerWord();
+				
+				int tBitNumber = 0;
+				int tBitResult = 0;
+				for( TechnicalTriggerWord::const_iterator tBitIt = ttWord.begin(), tBitEnd = ttWord.end(); 
+					 tBitIt != tBitEnd; ++tBitIt, ++tBitNumber )
+				{
+					if( *tBitIt )
+						tBitResult = 1;
+					else
+						tBitResult = 0;
+
+					m_columns.at(0).values.push_back( "TechTrigger" );
+					m_columns.at(1).values.push_back( Form( "%d", tBitResult ));
+					m_columns.at(2).values.push_back( Form( "%d", tBitNumber ));
+				}
+			}
          else
          {
-            fwLog(fwlog::kWarning) << "FWL1TriggerTableView: " <<
-               "L1GtTriggerMenuLite.isValid()=" << triggerMenuLite.isValid() << ", " <<
-               "L1GlobalTriggerReadoutRecord.isValid()=" << triggerRecord.isValid() << "." << std::endl;
+				fwLog( fwlog::kWarning ) << "FWL1TriggerTableView: " <<
+				"L1GtTriggerMenuLite.isValid()=" << triggerMenuLite.isValid() << ", " <<
+				"L1GlobalTriggerReadoutRecord.isValid()=" << triggerRecord.isValid() << "." << std::endl;
          }
       }
    }
