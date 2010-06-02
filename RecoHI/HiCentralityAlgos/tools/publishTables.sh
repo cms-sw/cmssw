d26f99b1d63c831a3ea23e17fd3b0de508137218
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-06-02 11:02:41 +0000

    updated cfg

--- RecoHI/HiCentralityAlgos/tools/publishTables.sh
+++ RecoHI/HiCentralityAlgos/tools/publishTables.sh
@@ -1,26 +1,53 @@
 
 datadir=$CMSSW_BASE/src/RecoHI/HiCentralityAlgos/data
 
-input=/home/yilmaz/analysis/jets/work0322/CentralityTables.root
+input=../data/CentralityTables.root
 output=$datadir/CentralityTables.db
 
-cmsRun makeDBFromTFile.py outputTag="HFhits40_MC_Hydjet4TeV_MC_3XY_V24_v0" inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="EEbcs40_MC_Hydjet4TeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="HFhits20_MC_Hydjet4TeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="EEbcs20_MC_Hydjet4TeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="HFhits10_MC_Hydjet4TeV_MC_3XY_V24_v0" inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="EEbcs10_MC_Hydjet4TeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="HFhits5_MC_Hydjet4TeV_MC_3XY_V24_v0" inputFile=$input outputFile=$output
-cmsRun makeDBFromTFile.py outputTag="EEbcs5_MC_Hydjet4TeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-
-#cmsRun makeDBFromTFile.py outputTag="HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="EEbcs40_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="HFhits20_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="EEbcs20_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="HFhits10_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="EEbcs10_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="HFhits5_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
-#cmsRun makeDBFromTFile.py outputTag="EEbcs5_MC_Hydjet2760GeV_MC_3XY_V24_v0"  inputFile=$input outputFile=$output
+for tag in \
+HFhits40_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+EEbcs40_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+HFhits20_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+EEbcs20_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+HFhits10_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+EEbcs10_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+HFhits5_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+EEbcs5_MC_Hydjet4TeV_MC_3XY_V24_v0 \
+HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs40_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits20_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs20_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits10_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs10_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits5_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs5_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs40_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits20_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs20_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits10_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs10_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits5_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+EEbcs5_MC_Hydjet2760GeV_MC_3XY_V24_v0 \
+HFhits40_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+EEbcs40_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+HFhits20_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+EEbcs20_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+HFhits10_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+EEbcs10_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+HFhits5_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+EEbcs5_MC_Hydjet2760TeV_MC_3XY_V24_NoZS_v0 \
+HFhits40_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+EEbcs40_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+HFhits20_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+EEbcs20_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+HFhits10_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+EEbcs10_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+HFhits5_MC_AMPT2760GeV_MC_3XY_V24_v0 \
+EEbcs5_MC_AMPT2760GeV_MC_3XY_V24_v0
+do
+cmsRun makeDBFromTFile.py outputTag=$tag inputFile=$input outputFile=$output
+done
 
 
 
