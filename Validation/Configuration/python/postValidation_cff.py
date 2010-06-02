422c1fa228f613cba2a26fd5da0f2f34fd8d108f
Jan Olzem <sha1-713996176e893ae17e542633137001ed8a2b989e@cern.ch>
2010-06-02 09:49:05 +0000

    adding calotowers and hcalrechits postprocessors

--- Validation/Configuration/python/postValidation_cff.py
+++ Validation/Configuration/python/postValidation_cff.py
@@ -3,7 +3,21 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoMuon.PostProcessor_cff import *
 from Validation.RecoTrack.PostProcessorTracker_cfi import *
 from Validation.MuonIsolation.PostProcessor_cff import *
+from Validation.CaloTowers.CaloTowersPostProcessor_cff import *
+from Validation.HcalRecHits.hcalRecHitsPostProcessor_cff import *
 
-postValidation = cms.Sequence(recoMuonPostProcessors+postProcessorTrack+MuIsoValPostProcessor)
 
-postValidation_fastsim = cms.Sequence(recoMuonPostProcessorsFastSim+postProcessorTrack+MuIsoValPostProcessor)
+postValidation = cms.Sequence(
+      recoMuonPostProcessors
+    + postProcessorTrack
+    + MuIsoValPostProcessor
+    + calotowersPostProcessor
+    + hcalrechitsPostProcessor
+)
+
+
+postValidation_fastsim = cms.Sequence(
+      recoMuonPostProcessorsFastSim
+    + postProcessorTrack
+    + MuIsoValPostProcessor
+)
