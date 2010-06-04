a74023723b934ab5ce672abb4f81f8ce17516dda
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-06-04 08:37:50 +0000

    Rename the association sequence as prevalidation

--- FastSimulation/Configuration/python/Validation_cff.py
+++ FastSimulation/Configuration/python/Validation_cff.py
@@ -3,9 +3,9 @@ from Validation.Generator.BasicGenValidation_cff import *
 from FastSimulation.Validation.globalValidation_cff import *
 from HLTriggerOffline.Common.HLTValidation_cff import *
 
-association = cms.Sequence(globalAssociation+hltassociation_fastsim)
-association_preprod = cms.Sequence(globalAssociation)
-association_prod = cms.Sequence(globalAssociation)
+prevalidation = cms.Sequence(globalAssociation+hltassociation_fastsim)
+prevalidation_preprod = cms.Sequence(globalAssociation)
+prevalidation_prod = cms.Sequence(globalAssociation)
 validation = cms.Sequence(basicGenTest_seq+globalValidation+hltvalidation_fastsim) 
 validation_preprod = cms.Sequence(basicGenTest_seq+globalValidation_preprod+hltvalidation_preprod_fastsim) 
 validation_prod = cms.Sequence(basicGenTest_seq+hltvalidation_prod) 
