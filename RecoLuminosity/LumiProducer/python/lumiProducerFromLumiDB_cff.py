d53dece4a8ea071c95e826f99530ed99e3c03cc2
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-06-08 12:36:39 +0000

    use frontier and lumiprod servlet as default

--- RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cff.py
+++ RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cff.py
@@ -1,3 +1,3 @@
-
 import FWCore.ParameterSet.Config as cms
 from RecoLuminosity.LumiProducer.lumiProducerFromLumiDB_cfi import *
+LumiDBService=cms.Service('DBService')
--- RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cfi.py
+++ RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cfi.py
@@ -1,6 +1,6 @@
 
 import FWCore.ParameterSet.Config as cms
 lumiProducer=cms.EDProducer("LumiProducer",
-                            connect=cms.string('oracle://cms_orcoff_prep/cms_lumi_dev_offline'),
+                            connect=cms.string('frontier://LumiProd/CMS_LUMI_PROD'),
                             lumiversion=cms.untracked.string('0001')
                             )
