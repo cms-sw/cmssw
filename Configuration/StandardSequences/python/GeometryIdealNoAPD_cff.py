b0dce1be632cc8543face6f256bea49e0eecc799
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-06-08 14:19:30 +0000

    Change default geometry to one with 3G version of APD

--- Configuration/StandardSequences/python/GeometryIdealNoAPD_cff.py
+++ Configuration/StandardSequences/python/GeometryIdealNoAPD_cff.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # Geometry master configuration
 #
 # Ideal geometry, needed for simulation
-from Geometry.CMSCommonData.cmsIdealGeometryAPD2XML_cfi import *
+from Geometry.CMSCommonData.cmsIdealGeometryNoAPDXML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi import *
 # Reconstruction geometry services
 #  Tracking Geometry
