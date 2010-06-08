057cd9f949e8e97e145613c37304525075a69370
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2010-06-08 19:55:51 +0000

    Turn on APD digitization & tune parameters

--- SimCalorimetry/EcalSimProducers/python/apdSimParameters_cff.py
+++ SimCalorimetry/EcalSimProducers/python/apdSimParameters_cff.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 apd_sim_parameters = cms.PSet(
-    apdAddToBarrel  = cms.bool(False),
+    apdAddToBarrel  = cms.bool(True),
     apdSeparateDigi = cms.bool(False),
-    apdSimToPELow   = cms.double(4.41e6),
-    apdSimToPEHigh  = cms.double(157.5e6),
-    apdTimeOffset   = cms.double(-10.0),
+    apdSimToPELow   = cms.double(3.5e6),
+    apdSimToPEHigh  = cms.double(126.e6),
+    apdTimeOffset   = cms.double(-15.0),
     apdDoPEStats    = cms.bool(True),
     apdDigiTag      = cms.string("APD"),
     apdShapeTstart  = cms.double( 74.5 ),
