7bb0c21e936a5a66473536e193df583b2b45b638
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-06-10 16:17:05 +0000

    Bug fix: namespace issue for standalone comparison functions

--- PhysicsTools/SelectorUtils/src/strbitset.cc
+++ PhysicsTools/SelectorUtils/src/strbitset.cc
@@ -1,21 +1,23 @@
 #include "PhysicsTools/SelectorUtils/interface/strbitset.h"
 
+namespace pat {
 
-pat::strbitset operator&(const pat::strbitset& l, const pat::strbitset& r) {
-  pat::strbitset ret = r;
-  ret &= l;
-  return ret;
+  strbitset operator&(const strbitset& l, const strbitset& r) {
+    strbitset ret = r;
+    ret &= l;
+    return ret;
+  }
+
+  strbitset operator|(const strbitset& l, const strbitset& r) {
+    strbitset ret = r;
+    ret |= l;
+    return ret;
+  }
+
+  strbitset operator^(const strbitset& l, const strbitset& r){
+    strbitset ret = r;
+    ret ^= l;
+    return ret;
+  }
+
 }
-
-pat::strbitset operator|(const pat::strbitset& l, const pat::strbitset& r) {
-  pat::strbitset ret = r;
-  ret |= l;
-  return ret;
-}
-
-pat::strbitset operator^(const pat::strbitset& l, const pat::strbitset& r){
-  pat::strbitset ret = r;
-  ret ^= l;
-  return ret;
-}
-
