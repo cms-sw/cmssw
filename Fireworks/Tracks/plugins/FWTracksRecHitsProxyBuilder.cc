f06eab0b55ddc15c9b9273efaafa5dd8cc2dbd10
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2010-06-10 14:25:03 +0000

    Fix TrackDets color/selection update.

--- Fireworks/Tracks/plugins/FWTracksRecHitsProxyBuilder.cc
+++ Fireworks/Tracks/plugins/FWTracksRecHitsProxyBuilder.cc
@@ -3,12 +3,21 @@
 //
 
 // user include files
+#include "TEveGeoShape.h"
+
+#include "Fireworks/Core/interface/DetIdToMatrix.h"
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Tracks/interface/TrackUtils.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
 
+#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+#include "DataFormats/MuonDetId/interface/DTChamberId.h"
+#include "DataFormats/TrackingRecHit/interface/RecHit2DLocalPos.h"
+#include "DataFormats/TrackingRecHit/interface/RecSegment.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHit.h"
 class FWTracksRecHitsProxyBuilder : public FWSimpleProxyBuilderTemplate<reco::Track>
 {
 public:
@@ -38,6 +47,8 @@ bool FWTracksRecHitsProxyBuilder::representsSubPart()
    return true;
 }
 
+
+
 class FWTracksModulesProxyBuilder : public FWSimpleProxyBuilderTemplate<reco::Track>
 {
 public:
@@ -55,10 +66,28 @@ private:
 };
 
 void
-FWTracksModulesProxyBuilder::build(const reco::Track& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext*) 
+FWTracksModulesProxyBuilder::build(const reco::Track& track, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext*) 
 {
-   if( iData.extra().isAvailable() )
-      fireworks::addModules(iData, item(), &oItemHolder, false);
+   if( track.extra().isAvailable() )
+   {
+      for( trackingRecHit_iterator recIt = track.recHitsBegin(), recItEnd = track.recHitsEnd();
+           recIt != recItEnd; ++recIt )
+      {
+         DetId detid = (*recIt)->geographicalId();
+         if ((*recIt)->isValid()) continue;
+         {
+            if (detid.det() ==  DetId::Muon)
+            {
+               if (detid.subdetId() == MuonSubdetId::DT)
+                  detid = DetId(DTChamberId(detid)); // get rid of layer bits
+            }
+
+            TEveGeoShape* shape = item()->getGeom()->getShape( detid );
+            setupAddElement(shape, &oItemHolder);
+         }
+
+      }
+   }
 }
 
 bool FWTracksModulesProxyBuilder::representsSubPart()
