2be2ca4f93538e21fe3f86933cfd4e447352dfca
Matteo Sani <sha1-b1a59c6d8d0f0b19695380739157bd7a38c2f52f@cern.ch>
2010-06-10 00:35:10 +0000

    Fixed correction parameter

--- RecoEgamma/EgammaTools/python/correctedElectronsProducer_cfi.py
+++ RecoEgamma/EgammaTools/python/correctedElectronsProducer_cfi.py
@@ -4,6 +4,6 @@ gsfElectrons = cms.EDProducer('CorrectedElectronsProducer',
                               electronCollection = cms.InputTag("gsfElectrons"),
                               scPositionCorrectionEBPlus  = cms.vdouble(0., 0., 0.),
                               scPositionCorrectionEBMinus = cms.vdouble(0., 0., 0.),
-                              scPositionCorrectionEEPlus  = cms.vdouble(0., 0., 0.),
-                              scPositionCorrectionEEMinus = cms.vdouble(0., 0., 0.)
+                              scPositionCorrectionEEPlus  = cms.vdouble(0.52, -0.81, 0.81),
+                              scPositionCorrectionEEMinus = cms.vdouble(-0.02, -0.81, -0.94)
                               )
