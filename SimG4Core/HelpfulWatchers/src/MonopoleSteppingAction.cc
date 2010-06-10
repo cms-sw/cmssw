0f8c1bc2910dad66d0e6c134dfb9b9ea50af49e3
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-06-10 07:19:33 +0000

    Correct bug

--- SimG4Core/HelpfulWatchers/src/MonopoleSteppingAction.cc
+++ SimG4Core/HelpfulWatchers/src/MonopoleSteppingAction.cc
@@ -132,7 +132,7 @@ void MonopoleSteppingAction::update(const G4Step* aStep) {
     double fac2 = pZ1*cInMByS*eT2;
     double fac1 = fac0*cInMByS*ts1*eT2 + fac2;
     double fact1 = (eT1/fac0)*std::sqrt(1+fac1*fac1)-std::sqrt(1+fac2*fac2);
-    double fact2 = (pT1/(magCharge*bZ)*(asinh(fac1+fac2)-asinh(fac2)));
+    double fact2 = (pT1/(magCharge*bZ)*(asinh(fac1)-asinh(fac2)));
     LogDebug("SimG4CoreWatcher") << "MonopoleSeppingAction: Factor eT = "
 				 << eT << " " << eT1 << " " << eT2
 				 << ", pT = " << pT << " " << pT1
