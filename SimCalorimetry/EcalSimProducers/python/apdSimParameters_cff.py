7c0b51a1e7b761875e80bae57f2b473f7c7ff29f
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2010-06-11 15:33:36 +0000

    Keep only APD digi but don't add to EB

--- SimCalorimetry/EcalSimProducers/python/apdSimParameters_cff.py
+++ SimCalorimetry/EcalSimProducers/python/apdSimParameters_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 apd_sim_parameters = cms.PSet(
-    apdAddToBarrel  = cms.bool(True),
-    apdSeparateDigi = cms.bool(False),
+    apdAddToBarrel  = cms.bool(False),
+    apdSeparateDigi = cms.bool(True),
     apdSimToPELow   = cms.double(3.5e6),
     apdSimToPEHigh  = cms.double(126.e6),
     apdTimeOffset   = cms.double(-15.0),
