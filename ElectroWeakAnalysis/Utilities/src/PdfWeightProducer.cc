9c563cb973d02e8b89bf0195fe504d5824c82370
Juan Alcaraz Maestre <sha1-00c52069b91662a716b700699899914afb62bf4e@cern.ch>
2010-06-13 16:30:03 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/Utilities/src/PdfWeightProducer.cc
+++ ElectroWeakAnalysis/Utilities/src/PdfWeightProducer.cc
@@ -27,12 +27,11 @@ class PdfWeightProducer : public edm::EDProducer {
       virtual void produce(edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
-      bool fixPOWHEG_;
+      std::string fixPOWHEG_;
       edm::InputTag genTag_;
       edm::InputTag pdfInfoTag_;
       std::vector<std::string> pdfSetNames_;
       std::vector<std::string> pdfShortNames_;
-      int powhegPdfIndex_;
 };
 
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
@@ -50,17 +49,18 @@ namespace LHAPDF {
 
 /////////////////////////////////////////////////////////////////////////////////////
 PdfWeightProducer::PdfWeightProducer(const edm::ParameterSet& pset) :
- fixPOWHEG_(pset.getUntrackedParameter<bool> ("FixPOWHEG", false)),
+ fixPOWHEG_(pset.getUntrackedParameter<std::string> ("FixPOWHEG", "")),
  genTag_(pset.getUntrackedParameter<edm::InputTag> ("GenTag", edm::InputTag("genParticles"))),
  pdfInfoTag_(pset.getUntrackedParameter<edm::InputTag> ("PdfInfoTag", edm::InputTag("generator"))),
  pdfSetNames_(pset.getUntrackedParameter<std::vector<std::string> > ("PdfSetNames"))
 {
+      if (fixPOWHEG_ != "") pdfSetNames_.insert(pdfSetNames_.begin(),fixPOWHEG_);
+
       if (pdfSetNames_.size()>3) {
             edm::LogWarning("") << pdfSetNames_.size() << " PDF sets requested on input. Using only the first 3 sets and ignoring the rest!!";
             pdfSetNames_.erase(pdfSetNames_.begin()+3,pdfSetNames_.end());
       }
 
-      powhegPdfIndex_ = -1;
       for (unsigned int k=0; k<pdfSetNames_.size(); k++) {
             size_t dot = pdfSetNames_[k].find_first_of('.');
             size_t underscore = pdfSetNames_[k].find_first_of('_');
@@ -70,14 +70,6 @@ PdfWeightProducer::PdfWeightProducer(const edm::ParameterSet& pset) :
                   pdfShortNames_.push_back(pdfSetNames_[k].substr(0,dot));
             }
             produces<std::vector<double> >(pdfShortNames_[k].data());
-            if (fixPOWHEG_ && pdfShortNames_[k]=="cteq66") {
-                  powhegPdfIndex_ = k+1;
-            }
-      }
-      if (fixPOWHEG_ && powhegPdfIndex_<0) {
-            edm::LogWarning("") << " You have requested to fix POWHEG PDFs, but a cteq66 set has not be found in the list";
-            edm::LogWarning("") << " We will use the first PDF set found in the list as reference !!";
-            powhegPdfIndex_ = 1;
       }
 } 
 
@@ -116,7 +108,7 @@ void PdfWeightProducer::produce(edm::Event& iEvent, const edm::EventSetup&) {
       double pdf2 = pdfstuff->pdf()->xPDF.second; 
 
       // Ad-hoc fix for POWHEG
-      if (fixPOWHEG_) {
+      if (fixPOWHEG_!="") {
             edm::Handle<reco::GenParticleCollection> genParticles;
             if (!iEvent.getByLabel(genTag_, genParticles)) {
                   edm::LogError("PDFWeightProducer") << ">>> genParticles  not found: " << genTag_.encode() << " !!!";
@@ -134,9 +126,9 @@ void PdfWeightProducer::produce(edm::Event& iEvent, const edm::EventSetup&) {
                   break;
             }
             Q = sqrt(mboson*mboson+Q*Q);
-            LHAPDF::usePDFMember(powhegPdfIndex_,0);
-            pdf1 = LHAPDF::xfx(powhegPdfIndex_, x1, Q, id1)/x1;
-            pdf2 = LHAPDF::xfx(powhegPdfIndex_, x2, Q, id2)/x2;
+            LHAPDF::usePDFMember(1,0);
+            pdf1 = LHAPDF::xfx(1, x1, Q, id1)/x1;
+            pdf2 = LHAPDF::xfx(1, x2, Q, id2)/x2;
       }
 
       // Put PDF weights in the event
