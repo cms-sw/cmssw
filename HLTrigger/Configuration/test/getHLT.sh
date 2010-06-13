cac5204879fb3648e9e3b9850c50a59be65cfe04
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-06-13 18:18:45 +0000

    add verbose flag and messages

--- HLTrigger/Configuration/test/getHLT.sh
+++ HLTrigger/Configuration/test/getHLT.sh
@@ -5,6 +5,15 @@ MASTER="/dev/CMSSW_3_6_2/HLT"         # no explicit version, take te most recent
 TARGET="/dev/CMSSW_3_6_2/\$TABLE"     # no explicit version, take te most recent 
 TABLES="8E29 1E31 GRun HIon"               # $TABLE in the above variable will be expanded to these TABLES
 
+# print extra messages ?
+VERBOSE=false
+
+[ "$1" == "-v" ] && { VERBOSE=true; shift; }
+
+function log() {
+  $VERBOSE && echo -e "$@"
+}
+
 function findHltScript() {
   local PACKAGE="HLTrigger/Configuration"
   local SCRIPT="$1"
@@ -26,15 +35,16 @@ GETCONTENT=$(findHltScript getEventContent.py)
 GETDATASETS=$(findHltScript getDatasets.py)
 
 function getConfigForCVS() {
-  # for things in CMSSW CVS
   local CONFIG="$1"
   local NAME="$2"
+  log "    dumping HLT cff for $NAME"
   $GETHLT --cff --mc $CONFIG $NAME
 }
 
 function getContentForCVS() {
   local CONFIG="$1"
 
+  log "    dumping EventContet"
   $GETCONTENT $CONFIG
   rm -f hltOutput*_cff.py*
 }
@@ -43,13 +53,15 @@ function getDatasetsForCVS() {
   local CONFIG="$1"
   local TARGET="$2"
 
+  log "    dumping Primary Dataset"
   $GETDATASETS $CONFIG $TARGET
 }
 
 function getConfigForOnline() {
-  # for things NOT in CMSSW CVS:
   local CONFIG="$1"
   local NAME="$2"
+
+  log "    dumping full HLT for $NAME"
   if [ "$NAME" == "8E29" ] || [ "$NAME" == "GRun" ]; then
     $GETHLT --full --offline --data $CONFIG $NAME --l1 L1Menu_Commissioning2010_v2
   elif [ "$NAME" == "1E31" ] || [ "$NAME" == "HIon" ]; then
@@ -75,6 +87,7 @@ done
 for TABLE in "GRun"; do
   getDatasetsForCVS $(eval echo $TARGET) HLTrigger_Datasets_cff.py
 done
+log "Done"
 ls -l HLT_*_cff.py HLTrigger_EventContent_cff.py HLTrigger_Datasets_cff.py
 mv -f HLT_*_cff.py HLTrigger_EventContent_cff.py HLTrigger_Datasets_cff.py ../python/
 echo
@@ -86,6 +99,7 @@ rm -f OnLine_HLT_*.py
 for TABLE in $TABLES; do
   getConfigForOnline $(eval echo $TARGET) $TABLE
 done
+log "Done"
 ls -l OnData_HLT_*.py
 ls -l OnLine_HLT_*.py
 echo
