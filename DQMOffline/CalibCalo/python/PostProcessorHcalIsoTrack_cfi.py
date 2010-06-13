d8e9b7b0d2d063ffbec3ccf2d7d49a148d2ba434
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2010-06-13 17:42:00 +0000

    bugfixes, several new important plots

--- DQMOffline/CalibCalo/python/PostProcessorHcalIsoTrack_cfi.py
+++ DQMOffline/CalibCalo/python/PostProcessorHcalIsoTrack_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 PostProcessorHcalIsoTrack = cms.EDAnalyzer("DQMHcalIsoTrackPostProcessor",
      subDir = cms.untracked.string("AlCaReco/HcalIsoTrack"),
-     saveToFile=cms.bool(True),
+     saveToFile=cms.bool(False),
      outputFile=cms.string("HarvestOutput.root")
 )
