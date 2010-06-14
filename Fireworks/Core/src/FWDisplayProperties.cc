730018d4addb5dc98494dfda3ef139c1552d4ad9
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2010-06-14 07:36:45 +0000

    Replace old `transparency = 0` default with the actual passed value.

--- Fireworks/Core/src/FWDisplayProperties.cc
+++ Fireworks/Core/src/FWDisplayProperties.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:
 //         Created:  Thu Jan  3 17:05:44 EST 2008
-// $Id: FWDisplayProperties.cc,v 1.6 2010/05/31 15:44:01 eulisse Exp $
+// $Id: FWDisplayProperties.cc,v 1.7 2010/06/03 13:38:32 eulisse Exp $
 //
 
 // system include files
@@ -24,8 +24,8 @@ const FWDisplayProperties FWDisplayProperties::defaultProperties(kWhite, true, 1
 FWDisplayProperties::FWDisplayProperties(const Color_t& iColor,
                                          bool isVisible,
                                          Int_t transparency) 
-: m_isVisible(isVisible),
-  m_transparency(0)
+   : m_isVisible(isVisible),
+     m_transparency(transparency)
 {
    setColor(iColor);
 }
