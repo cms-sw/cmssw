c86051199f629451d09d8ba13b8ffbce478528ae
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2010-06-14 10:28:03 +0000

    removed unused module

--- SimCalorimetry/EcalSelectiveReadoutProducers/src/SealModule.cc
+++ SimCalorimetry/EcalSelectiveReadoutProducers/src/SealModule.cc
@@ -8,11 +8,9 @@
 
 #include "SimCalorimetry/EcalSelectiveReadoutProducers/interface/EcalSelectiveReadoutProducer.h"
 #include "SimCalorimetry/EcalSelectiveReadoutProducers/interface/EcalSRCondTools.h"
-#include "SimCalorimetry/EcalSelectiveReadoutProducers/interface/EcalSRSimpleESSource.h"
+#include "CondFormats/EcalObjects/interface/EcalSRSettings.h"
 
 DEFINE_FWK_MODULE(EcalSelectiveReadoutProducer);
 
 DEFINE_FWK_MODULE(EcalSRCondTools);
-
-DEFINE_FWK_EVENTSETUP_SOURCE(EcalSRSimpleESSource);
 
