02652929c6ccc649548d6284f43d0cf68c098651
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-06-15 22:15:46 +0000

    change default parameter

--- HLTrigger/special/python/hltCSCActivityFilter_cfi.py
+++ HLTrigger/special/python/hltCSCActivityFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 hltCSCActivityFilter = cms.EDFilter( "HLTCSCActivityFilter",
     cscStripDigiTag = cms.InputTag("hltMuonCSCDigis","MuonCSCStripDigi"),
-    skipStationRing = cms.bool( False ),
+    skipStationRing = cms.bool( True ),
     skipStationNumber = cms.int32(1),
     skipRingNumber = cms.int32(4),
     applyfilter = cms.bool(True)
