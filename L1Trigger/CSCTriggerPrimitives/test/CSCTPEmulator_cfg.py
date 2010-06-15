a344f638812d7f54071a28e16ff6730cda02a758
jarvis <sha1-df264f6d1562bd8731dbc6928e1125559804b1ee@cern.ch>
2010-06-15 17:28:06 +0000

    updated lctreader for DQM

--- L1Trigger/CSCTriggerPrimitives/test/CSCTPEmulator_cfg.py
+++ L1Trigger/CSCTriggerPrimitives/test/CSCTPEmulator_cfg.py
@@ -24,21 +24,14 @@ sys.path.insert(0, os.path.join(os.environ['CMSSW_BASE'],
 #        int32 max_queue_depth = 5
 #    }
 
-#process.source = cms.Source("PoolSource",
+process.source = cms.Source("PoolSource",
 ##    fileNames = cms.untracked.vstring('file:/data0/slava/data/run58731/4C6067C2-B972-DD11-9672-000423D996B4.root')
-#     fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/data/Commissioning08/Cosmics/RAW/v1/000/069/912/007F93B1-18AD-DD11-AB6D-000423D991D4.root'),
-#     fileNames = cms.untracked.vstring(
-#       '/store/data/BeamCommissioning09/Cosmics/RAW/v1/000/122/909/F81EA88E-A5DB-DE11-AA71-00304879FBB2.root'
-#     )
-#     fileNames = cms.untracked.vstring('file:/data0/slava/data/run109562/FE316E49-047F-DE11-AC0C-001D09F231B0.root')
+     fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/cms/store/data/Run2010A/Cosmics/RAW/v1/000/137/079/12A5105A-C271-DF11-82D3-001D09F2527B.root')
 ##        untracked uint32 debugVebosity = 10
 ##        untracked bool   debugFlag     = false
 ###	untracked uint32 skipEvents    = 2370
-#)
-
-#process.load("DQM.CSCMonitorModule.csc_dqm_sourceclient_cfi")
-
-process.load("localrun_cfi")
+)
+#process.load("localrun_cfi")
 
 process.MessageLogger = cms.Service("MessageLogger",
     destinations = cms.untracked.vstring("debug"),
@@ -69,7 +62,8 @@ process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 process.load("Geometry.CSCGeometry.cscGeometry_cfi")
 
 process.load("Configuration/StandardSequences/FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'GR10_P_V5::All'
+process.GlobalTag.globaltag = 'MC_3XY_V21::All'
+process.GlobalTag.globaltag = 'GR_R_36X_V12::All'
 #process.GlobalTag.globaltag = 'CRAFT09_R_V9::All'
 #process.prefer("GlobalTag")
 
@@ -102,15 +96,8 @@ process.load("L1Trigger.CSCTriggerPrimitives.cscTriggerPrimitiveDigis_cfi")
 process.cscTriggerPrimitiveDigis.alctParam07.verbosity = 2
 process.cscTriggerPrimitiveDigis.clctParam07.verbosity = 2
 process.cscTriggerPrimitiveDigis.tmbParam.verbosity = 2
-process.cscTriggerPrimitiveDigis.tmbParam.mpcBlockMe1a = 0
 process.cscTriggerPrimitiveDigis.CSCComparatorDigiProducer = "muonCSCDigis:MuonCSCComparatorDigi"
 process.cscTriggerPrimitiveDigis.CSCWireDigiProducer = "muonCSCDigis:MuonCSCWireDigi"
-process.load("L1TriggerConfig.L1CSCTPConfigProducers.L1CSCTriggerPrimitivesConfig_cff")
-process.l1csctpconf.alctParamMTCC2.alctNplanesHitPretrig = 3
-process.l1csctpconf.alctParamMTCC2.alctNplanesHitAccelPretrig = 3
-process.l1csctpconf.clctParam.clctNplanesHitPretrig = 3
-process.l1csctpconf.clctParam.clctHitPersist = 4
-
 
 # CSC Trigger Primitives reader
 # =============================
@@ -134,6 +121,10 @@ process.out = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring("keep *", 
         "drop *_DaqSource_*_*")
 )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('TPEHists.root')
+                                   )
 
 # Scheduler path
 # ==============
