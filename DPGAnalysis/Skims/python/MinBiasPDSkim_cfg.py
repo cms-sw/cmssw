fc1bcfa379e28d4df823c104e902cae16084ab5e
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2010-06-16 10:08:03 +0000

    TPG skims enabled again

--- DPGAnalysis/Skims/python/MinBiasPDSkim_cfg.py
+++ DPGAnalysis/Skims/python/MinBiasPDSkim_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("SKIM")
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.30 $'),
+    version = cms.untracked.string('$Revision: 1.31 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/DPGAnalysis/Skims/python/MinBiasPDSkim_cfg.py,v $'),
     annotation = cms.untracked.string('Combined MinBias skim')
 )
@@ -371,61 +371,61 @@ process.outputvalskim = cms.OutputModule("PoolOutputModule",
 ###########################################################################
 ######################################TPG Performance SKIMS#####################################
 
-## process.load('DPGAnalysis/Skims/singleMuonSkim_cff')
-## process.load('DPGAnalysis/Skims/singleElectronSkim_cff')
-## process.load('DPGAnalysis/Skims/muonTagProbeFilters_cff')
-## process.load('DPGAnalysis/Skims/electronTagProbeFilters_cff')
-## process.load('DPGAnalysis/Skims/singlePhotonSkim_cff')
-## process.load('DPGAnalysis/Skims/jetSkim_cff')
-## process.load('DPGAnalysis/Skims/METSkim_cff')
-## process.load('DPGAnalysis/Skims/singlePfTauSkim_cff')
+process.load('DPGAnalysis/Skims/singleMuonSkim_cff')
+process.load('DPGAnalysis/Skims/singleElectronSkim_cff')
+process.load('DPGAnalysis/Skims/muonTagProbeFilters_cff')
+process.load('DPGAnalysis/Skims/electronTagProbeFilters_cff')
+process.load('DPGAnalysis/Skims/singlePhotonSkim_cff')
+process.load('DPGAnalysis/Skims/jetSkim_cff')
+process.load('DPGAnalysis/Skims/METSkim_cff')
+process.load('DPGAnalysis/Skims/singlePfTauSkim_cff')
 
-## #process.singleMuPt20SkimPath=cms.Path(process.singleMuPt20RecoQualitySeq)
-## #process.singleMuPt15SkimPath=cms.Path(process.singleMuPt15RecoQualitySeq)
-## #process.singleMuPt10SkimPath=cms.Path(process.singleMuPt10RecoQualitySeq)
-## process.singleMuPt5SkimPath=cms.Path(process.singleMuPt5RecoQualitySeq)
-## #process.singleElectronPt20SkimPath=cms.Path(process.singleElectronPt20RecoQualitySeq)
-## #process.singleElectronPt15SkimPath=cms.Path(process.singleElectronPt15RecoQualitySeq)
-## #process.singleElectronPt10SkimPath=cms.Path(process.singleElectronPt10RecoQualitySeq)
-## process.singleElectronPt5SkimPath=cms.Path(process.singleElectronPt5RecoQualitySeq)
-## #process.singlePhotonPt20SkimPath=cms.Path(process.singlePhotonPt20QualitySeq)
-## #process.singlePhotonPt15SkimPath=cms.Path(process.singlePhotonPt15QualitySeq)
-## #process.singlePhotonPt10SkimPath=cms.Path(process.singlePhotonPt10QualitySeq)
-## process.singlePhotonPt5SkimPath=cms.Path(process.singlePhotonPt5QualitySeq)
-## #process.muonZMMSkimPath=cms.Path(process.muonZMMRecoQualitySeq)
-## process.muonJPsiMMSkimPath=cms.Path(process.muonJPsiMMRecoQualitySeq)
-## #process.electronZEESkimPath=cms.Path(process.electronZEERecoQualitySeq)
-## process.jetSkimPath=cms.Path(process.jetRecoQualitySeq)
-## #process.METSkimPath=cms.Path(process.METQualitySeq)
-## process.singlePfTauPt15SkimPath=cms.Path(process.singlePfTauPt15QualitySeq) 
+#process.singleMuPt20SkimPath=cms.Path(process.singleMuPt20RecoQualitySeq)
+#process.singleMuPt15SkimPath=cms.Path(process.singleMuPt15RecoQualitySeq)
+#process.singleMuPt10SkimPath=cms.Path(process.singleMuPt10RecoQualitySeq)
+process.singleMuPt5SkimPath=cms.Path(process.singleMuPt5RecoQualitySeq)
+#process.singleElectronPt20SkimPath=cms.Path(process.singleElectronPt20RecoQualitySeq)
+#process.singleElectronPt15SkimPath=cms.Path(process.singleElectronPt15RecoQualitySeq)
+#process.singleElectronPt10SkimPath=cms.Path(process.singleElectronPt10RecoQualitySeq)
+process.singleElectronPt5SkimPath=cms.Path(process.singleElectronPt5RecoQualitySeq)
+#process.singlePhotonPt20SkimPath=cms.Path(process.singlePhotonPt20QualitySeq)
+#process.singlePhotonPt15SkimPath=cms.Path(process.singlePhotonPt15QualitySeq)
+#process.singlePhotonPt10SkimPath=cms.Path(process.singlePhotonPt10QualitySeq)
+process.singlePhotonPt5SkimPath=cms.Path(process.singlePhotonPt5QualitySeq)
+#process.muonZMMSkimPath=cms.Path(process.muonZMMRecoQualitySeq)
+process.muonJPsiMMSkimPath=cms.Path(process.muonJPsiMMRecoQualitySeq)
+#process.electronZEESkimPath=cms.Path(process.electronZEERecoQualitySeq)
+process.jetSkimPath=cms.Path(process.jetRecoQualitySeq)
+#process.METSkimPath=cms.Path(process.METQualitySeq)
+process.singlePfTauPt15SkimPath=cms.Path(process.singlePfTauPt15QualitySeq) 
 
-## process.outTPGSkim = cms.OutputModule("PoolOutputModule",
-##     outputCommands = process.FEVTHLTALLEventContent.outputCommands,
-##     fileName = cms.untracked.string("/tmp/azzi/TPGSkim.root"),
-##     dataset = cms.untracked.PSet(
-##       dataTier = cms.untracked.string('USER'),
-##       filterName = cms.untracked.string('TPGSkim')
-##     ),
-##     SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring(
-##                                                                  #'singleMuPt20SkimPath',
-##                                                                  #'singleMuPt15SkimPath',
-##                                                                  #'singleMuPt10SkimPath',
-##                                                                  'singleMuPt5SkimPath',
-##                                                                  #'singleElectronPt20SkimPath',
-##                                                                  #'singleElectronPt15SkimPath',
-##                                                                  #'singleElectronPt10SkimPath',
-##                                                                  'singleElectronPt5SkimPath',
-##                                                                  #'singlePhotonPt20SkimPath',
-##                                                                  #'singlePhotonPt15SkimPath',
-##                                                                  #'singlePhotonPt10SkimPath',
-##                                                                  'singlePhotonPt5SkimPath',
-##                                                                  #'muonZMMSkimPath',
-##                                                                  'muonJPsiMMSkimPath',
-##                                                                  #'electronZEESkimPath',
-##                                                                  'jetSkimPath',
-##                                                                  #'METSkimPath',
-##                                                                  'singlePfTauPt15SkimPath'))
-## )
+process.outTPGSkim = cms.OutputModule("PoolOutputModule",
+    outputCommands = process.FEVTHLTALLEventContent.outputCommands,
+    fileName = cms.untracked.string("/tmp/azzi/TPGSkim.root"),
+    dataset = cms.untracked.PSet(
+      dataTier = cms.untracked.string('USER'),
+      filterName = cms.untracked.string('TPGSkim')
+    ),
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring(
+                                                                 #'singleMuPt20SkimPath',
+                                                                 #'singleMuPt15SkimPath',
+                                                                 #'singleMuPt10SkimPath',
+                                                                 'singleMuPt5SkimPath',
+                                                                 #'singleElectronPt20SkimPath',
+                                                                 #'singleElectronPt15SkimPath',
+                                                                 #'singleElectronPt10SkimPath',
+                                                                 'singleElectronPt5SkimPath',
+                                                                 #'singlePhotonPt20SkimPath',
+                                                                 #'singlePhotonPt15SkimPath',
+                                                                 #'singlePhotonPt10SkimPath',
+                                                                 'singlePhotonPt5SkimPath',
+                                                                 #'muonZMMSkimPath',
+                                                                 'muonJPsiMMSkimPath',
+                                                                 #'electronZEESkimPath',
+                                                                 'jetSkimPath',
+                                                                 #'METSkimPath',
+                                                                 'singlePfTauPt15SkimPath'))
+)
 
 
 ###########################################################################
@@ -435,8 +435,7 @@ process.options = cms.untracked.PSet(
  wantSummary = cms.untracked.bool(True)
 )
 
-process.outpath = cms.EndPath(process.outputBeamHaloSkim+process.outputMuonDPGSkim+process.outHSCP+process.ecalrechitfilter_out+process.outputpfgskim3+process.outlogerr+process.outputvalskim)
-###+process.outTPGSkim)
+process.outpath = cms.EndPath(process.outputBeamHaloSkim+process.outputMuonDPGSkim+process.outHSCP+process.ecalrechitfilter_out+process.outputpfgskim3+process.outlogerr+process.outputvalskim+process.outTPGSkim)
 
 
 
