e3894dec5816afadf2635ff580b2d55f053695d3
Nikolaos Rompotis <sha1-013c00d56849712eaa9efb4912fb1ea8ac9bfbf7@cern.ch>
2010-06-16 08:36:44 +0000

    update with Paolo's restrictions

--- ElectroWeakAnalysis/ZEE/python/simpleEleIdSequence_cff.py
+++ ElectroWeakAnalysis/ZEE/python/simpleEleIdSequence_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from ElectroWeakAnalysis.WENu.simpleCutBasedElectronIDSpring10_cfi import *
+from ElectroWeakAnalysis.ZEE.simpleCutBasedElectronIDSpring10_cfi import *
 
 
 simpleEleId95relIso = simpleCutBasedElectronID.clone()
