2cf393a208aee7b87de93c082e96a4b5e80d0e22
Jean Fay <sha1-b378da5b1d9b5e4cebe96112455ca1cdbe7a50fb@cern.ch>
2010-06-16 14:51:27 +0000

    new ECAL barrel alignment record

---
+++ CondFormats/DataRecord/interface/EcalAlignmentEBRcd.h
@@ -0,0 +1,6 @@
+#ifndef ECALALIGNMENTEBRCD_H
+#define ECALALIGNMENTEBRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class EcalAlignmentEBRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalAlignmentEBRcd> {};
+#endif
