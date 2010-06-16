e0fde1e1023ccbb2da96fdc06c681318a86dd908
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2010-06-16 14:16:23 +0000

    changes for the 36x series

--- IORawData/CSCCommissioning/src/SealModule.cc
+++ IORawData/CSCCommissioning/src/SealModule.cc
@@ -1,10 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "CSCFileReader.h"
 #include <IORawData/DaqSource/interface/DaqReaderPluginFactory.h>
-DEFINE_SEAL_MODULE();
 DEFINE_EDM_PLUGIN(DaqReaderPluginFactory, CSCFileReader, "CSCFileReader");
 
 #include "CSCFileDumper.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_ANOTHER_FWK_MODULE(CSCFileDumper);
+DEFINE_FWK_MODULE(CSCFileDumper);
 
