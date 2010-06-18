ffafb0a5ad84f71e43c61486268213df43e1ad56
Radek Ofierzynski <sha1-0f46c6a3659d46e055c32a629b3c16db5dedb2e3@cern.ch>
2010-06-18 15:22:23 +0000

    set default for HF to reco 4ts

--- RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hf_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalHitReconstructor_hf_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hfreco = cms.EDProducer("HcalHitReconstructor",
                         correctionPhaseNS = cms.double(0.0),
                         digiLabel = cms.InputTag("hcalDigis"),
-                        samplesToAdd = cms.int32(1),
+                        samplesToAdd = cms.int32(4),
                         Subdetector = cms.string('HF'),
                         firstSample = cms.int32(3),
                         correctForPhaseContainment = cms.bool(False),
