6942c92506cd0d80e661485a7fa9ec241948523d
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-06-21 10:43:11 +0000

    swap concrete parameters to cff

--- RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cfi.py
+++ RecoLuminosity/LumiProducer/python/lumiProducerFromLumiDB_cfi.py
@@ -1,6 +1,6 @@
 
 import FWCore.ParameterSet.Config as cms
 lumiProducer=cms.EDProducer("LumiProducer",
-                            connect=cms.string('frontier://LumiProd/CMS_LUMI_PROD'),
-                            lumiversion=cms.untracked.string('0001')
+                            connect=cms.string(''),
+                            lumiversion=cms.untracked.string('')
                             )
