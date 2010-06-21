fced7136bd02c1fd22786d0bf29e293c5f5b48f7
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-06-21 12:55:12 +0000

    fix omport

--- RecoLuminosity/LumiProducer/python/lumiProducer_cff.py
+++ RecoLuminosity/LumiProducer/python/lumiProducer_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-from RecoLuminosity.LumiProducer.lumiProducerFromLumiDB_cfi import *
+from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
 LumiDBService=cms.Service('DBService')
 lumiProducer.connect='frontier://LumiProd/CMS_LUMI_PROD'
 lumiProducer.lumiversion='0001'
