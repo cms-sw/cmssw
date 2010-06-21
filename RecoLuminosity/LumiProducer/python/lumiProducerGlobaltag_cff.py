cf705ec8754e111e994851f8c7162c5d0437182d
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-06-21 10:33:28 +0000

    cleanup

--- RecoLuminosity/LumiProducer/python/lumiProducerGlobaltag_cff.py
+++
@@ -1,4 +0,0 @@
-
-import FWCore.ParameterSet.Config as cms
-from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
-from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
--- RecoLuminosity/LumiProducer/python/nonGlobalTagLumiProducerPrep_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from CondCore.DBCommon.CondDBSetup_cfi import *
-LumiESSource = cms.ESSource( "PoolDBESSource",
-  CondDBSetup,
-  connect = cms.string('oracle://cms_orcoff_prep/CMS_COND_RUN_INFO'),
-  BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService')
-)
-from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
