40d08a2cada2a708785c5ae9c02e4302cee2186a
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-06-22 15:42:06 +0000

    unprescale all paths

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -6,11 +6,11 @@ L1T='L1Menu_Commissioning2010_v2'
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-./getHLT.py --process TEST --full --offline --mc   --l1 $L1T $HLT TEST
+./getHLT.py --process TEST --full --offline --mc   --l1 $L1T --unprescale $HLT TEST
 mv OnLine_HLT_TEST.py offline_mc.py
-./getHLT.py --process TEST --full --offline --data --l1 $L1T $HLT TEST
+./getHLT.py --process TEST --full --offline --data --l1 $L1T --unprescale $HLT TEST
 mv OnData_HLT_TEST.py offline_data.py
-./getHLT.py --process TEST --full --online  --data --l1 $L1T $HLT TEST
+./getHLT.py --process TEST --full --online  --data --l1 $L1T --unprescale $HLT TEST
 mv OnData_HLT_TEST.py online_data.py
 
 {
