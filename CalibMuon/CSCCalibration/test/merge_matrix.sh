19fa3ad5492b4746091df9a231eba06065f3f181
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2010-06-22 11:51:21 +0000

    changed to a run directory instead of Summary directory

--- CalibMuon/CSCCalibration/test/merge_matrix.sh
+++ CalibMuon/CSCCalibration/test/merge_matrix.sh
@@ -1,4 +1,4 @@
-for i in /nfshome0/cscdqm/results/calib/Test_CFEB02/Summary.plots/*/*_DB_NoiseMatrix_fixed.dat
+for i in /data/dqm/calib/Test_CFEB02/run_00131361_Calib_CFEB_SCAPed.plots/*/*_DB_NoiseMatrix.dat
 do
-cat $i >> matrixSummary2008_09_02_fixed.dat
+cat $i >> matrixSummary2010_03_18_run131361.dat
 done
