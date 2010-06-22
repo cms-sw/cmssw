25cfc4160da542ef560c3c5570c046220f26f4c1
Jean Fay <sha1-b378da5b1d9b5e4cebe96112455ca1cdbe7a50fb@cern.ch>
2010-06-22 17:29:54 +0000

    useless : use Alignments class instead

--- CondFormats/DataRecord/interface/EcalAlignmentEBRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef ECALALIGNMENTEBRCD_H
-#define ECALALIGNMENTEBRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class EcalAlignmentEBRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalAlignmentEBRcd> {};
-#endif
