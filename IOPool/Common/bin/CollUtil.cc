487a95ae7358dcd2fb8f655166a6d2909adb6c88
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2010-06-24 21:41:11 +0000

    updated to work with IndexToFile

--- IOPool/Common/bin/CollUtil.cc
+++ IOPool/Common/bin/CollUtil.cc
@@ -4,8 +4,11 @@
 #include "DataFormats/Provenance/interface/FileID.h"
 #include "DataFormats/Provenance/interface/FileFormatVersion.h"
 #include "DataFormats/Provenance/interface/FileIndex.h"
+#include "DataFormats/Provenance/interface/IndexIntoFile.h"
+#include "DataFormats/Provenance/interface/EventAuxiliary.h"
 
 #include <iostream>
+#include <iomanip>
 
 #include "TFile.h" 
 #include "TList.h"
@@ -97,17 +100,8 @@ namespace edm {
     std::cout << "UUID: " << fid.fid() << std::endl;
   }
 
-  void printEventLists(TFile *tfl) {
+  static void preIndexIntoFilePrintEventLists(TFile* tfl, const FileFormatVersion& fileFormatVersion, TTree *metaDataTree) {
 
-    TTree *metaDataTree = dynamic_cast<TTree *>(tfl->Get(poolNames::metaDataTreeName().c_str()));
-
-    FileFormatVersion fileFormatVersion;
-    FileFormatVersion *fftPtr = &fileFormatVersion;
-    if (metaDataTree->FindBranch(poolNames::fileFormatVersionBranchName().c_str()) != 0) {
-      TBranch *fft = metaDataTree->GetBranch(poolNames::fileFormatVersionBranchName().c_str());
-      fft->SetAddress(&fftPtr);
-      fft->GetEntry(0);
-    }
     FileIndex fileIndex;
     FileIndex *findexPtr = &fileIndex;
     if (metaDataTree->FindBranch(poolNames::fileIndexBranchName().c_str()) != 0) {
@@ -143,4 +137,104 @@ namespace edm {
     }
     std::cout << "(Note that other factors can prevent fast copy from occurring)\n\n";
   }
+
+  static void postIndexIntoFilePrintEventLists(TFile* tfl, const FileFormatVersion& fileFormatVersion, TTree *metaDataTree) {
+
+    IndexIntoFile indexIntoFile;
+    IndexIntoFile *findexPtr = &indexIntoFile;
+    if (metaDataTree->FindBranch(poolNames::indexIntoFileBranchName().c_str()) != 0) {
+      TBranch *fndx = metaDataTree->GetBranch(poolNames::indexIntoFileBranchName().c_str());
+      fndx->SetAddress(&findexPtr);
+      fndx->GetEntry(0);
+    } else {
+      std::cout << "IndexIntoFile not found.  If this input file was created with release 1_8_0 or later\n"
+                   "this indicates a problem with the file.  This condition should be expected with\n"
+	"files created with earlier releases and printout of the event list will fail.\n";
+      return;
+    }
+    
+    //need to read event # from the EventAuxiliary branch
+    TTree* eventsTree = dynamic_cast<TTree*>(tfl->Get(edm::poolNames::eventTreeName().c_str()));
+    TBranch* eventAuxBranch=0;
+    assert(0!=eventsTree);
+    const char* const kEventAuxiliaryBranchName = "EventAuxiliary";
+    if(eventsTree->FindBranch(kEventAuxiliaryBranchName)!=0){
+      eventAuxBranch = eventsTree->GetBranch(kEventAuxiliaryBranchName);
+    } else {
+      std::cout <<"Failed to find "<<kEventAuxiliaryBranchName<<" branch in Events TTree.  Something is wrong with this file."<<std::endl;
+      return;
+    }
+
+    EventAuxiliary eventAuxiliary;
+    EventAuxiliary* eAPtr=&eventAuxiliary;
+    eventAuxBranch->SetAddress(&eAPtr);
+    std::cout << "\nPrinting IndexIntoFile contents.  This includes a list of all Runs, LuminosityBlocks\n"
+       << "and Events stored in the root file.\n\n";
+    std::cout << std::setw(15) << "Run"
+       << std::setw(15) << "Lumi"
+       << std::setw(15) << "Event"
+       << std::setw(15) << "TTree Entry"
+       << "\n";
+    
+    for(IndexIntoFile::IndexIntoFileItr it = indexIntoFile.begin(false), itEnd = indexIntoFile.end(false);
+    it != itEnd;
+    ++it) {
+      IndexIntoFile::EntryType t = it.getEntryType();
+      std::cout << std::setw(15)<<it.run()<< std::setw(15)<<it.lumi();
+      EventNumber_t eventNum = 0; 
+      std::string type;
+      switch(t) {
+        case IndexIntoFile::kRun:
+        type = "(Run)";
+        break;
+        case IndexIntoFile::kLumi:
+        type = "(Lumi)";
+        break;
+        case IndexIntoFile::kEvent:
+        eventAuxBranch->GetEntry(it.entry());
+        eventNum = eventAuxiliary.id().event();
+        break;
+        default:
+        break;
+      }
+      std::cout << std::setw(15)<<eventNum << std::setw(15) << it.entry()<<" "<<type<<std::endl;
+    }
+
+    std::cout << "\nFileFormatVersion = " << fileFormatVersion << ".  ";
+    if (fileFormatVersion.fastCopyPossible()) std::cout << "This version supports fast copy\n";
+    else std::cout << "This version does not support fast copy\n";
+
+    if (indexIntoFile.allEventsInEntryOrder(false)) {
+      std::cout << "Events are sorted such that fast copy is possible in the default mode\n";
+    }
+    else {
+      std::cout << "Events are sorted such that fast copy is NOT possible in the default mode\n";
+    }
+
+    if (indexIntoFile.allEventsInEntryOrder(false)) {
+      std::cout << "Events are sorted such that fast copy is possible in the \"noEventSort\" mode\n";
+    }
+    else {
+      std::cout << "Events are sorted such that fast copy is NOT possible in the \"noEventSort\" mode\n";
+    }
+    std::cout << "(Note that other factors can prevent fast copy from occurring)\n\n";
+  }
+  
+  void printEventLists(TFile *tfl) {
+    TTree *metaDataTree = dynamic_cast<TTree *>(tfl->Get(poolNames::metaDataTreeName().c_str()));
+
+    FileFormatVersion fileFormatVersion;
+    FileFormatVersion *fftPtr = &fileFormatVersion;
+    if (metaDataTree->FindBranch(poolNames::fileFormatVersionBranchName().c_str()) != 0) {
+      TBranch *fft = metaDataTree->GetBranch(poolNames::fileFormatVersionBranchName().c_str());
+      fft->SetAddress(&fftPtr);
+      fft->GetEntry(0);
+    }
+    if(fileFormatVersion.hasIndexIntoFile()) {
+      postIndexIntoFilePrintEventLists(tfl, fileFormatVersion, metaDataTree);      
+    } else {
+      preIndexIntoFilePrintEventLists(tfl, fileFormatVersion, metaDataTree);
+    }
+  }
+  
 }
