74fcf655b3620eebcb7f39b5737a535c0afab1d0
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-06-25 11:54:02 +0000

    fix a typo

--- Configuration/GlobalRuns/python/reco_TLR_37X.py
+++ Configuration/GlobalRuns/python/reco_TLR_37X.py
@@ -99,7 +99,7 @@ def customisePPData(process):
 def customisePPMC(process):
     process=customiseCommon(process)
     process.hfreco.firstSample=1
-    process.CSCHaloData.ExpectedBX = cms.int33(6)
+    process.CSCHaloData.ExpectedBX = cms.int32(6)
     
     return process
 
