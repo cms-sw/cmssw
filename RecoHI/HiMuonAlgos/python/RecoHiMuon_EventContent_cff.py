033b16f59336136df20ccf946f4d1a4c1c501082
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-06-29 15:31:43 +0000

    dummy extra event content file

---
+++ RecoHI/HiMuonAlgos/python/RecoHiMuon_EventContent_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+#Full Event content 
+RecoHiMuonFEVT = cms.PSet(
+    outputCommands = cms.untracked.vstring()
+    )
+
+#RECO content
+RecoHiMuonRECO = cms.PSet(
+    outputCommands = cms.untracked.vstring()
+    )
+
+#AOD content
+RecoHiMuonAOD = cms.PSet(
+    outputCommands = cms.untracked.vstring()
+    )
