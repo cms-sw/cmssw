3512c2121acc1ffefae282c53e85aa55cb4c1759
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2010-06-29 11:32:35 +0000

    remove CastorJet and CastorEgamma production

--- RecoLocalCalo/Castor/python/CastorJetEgammaReco_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-	
-CastorJetEgammaRecoAntiKt07 = cms.EDProducer('CastorJetEgammaProducer',
-	input = cms.untracked.string("CastorClusterRecoAntiKt07"),
-	fastsim = cms.untracked.bool(False) )	
-
