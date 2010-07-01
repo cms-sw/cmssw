dbc74a3db76281b589a6b0a9f7d66dcf792c9a84
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-07-01 15:36:37 +0000

    setup multiple paths that can be configured on the fly by ConfigBuilder

--- Configuration/StandardSequences/python/AlCaHarvesting_cff.py
+++ Configuration/StandardSequences/python/AlCaHarvesting_cff.py
@@ -5,19 +5,37 @@ from Calibration.TkAlCaRecoProducers.AlcaBeamSpotHarvester_cff import *
 from CondCore.DBCommon.CondDBCommon_cfi import CondDBCommon
 CondDBCommon.connect = "sqlite_file:promptCalibConditions.db"
 
-# FIXME: the toPut could be configured dinamically....
 PoolDBOutputService = cms.Service("PoolDBOutputService",
                                   CondDBCommon,
-                                  toPut = cms.VPSet(cms.PSet(record = cms.string('BeamSpotObjectsRcd'),
-                                                             tag = cms.string('BeamSpotObject_runbased'),
-                                                             timetype   = cms.untracked.string('runnumber'))
-                                                    ),
+                                  toPut = cms.VPSet(),
                                   loadBlobStreamer = cms.untracked.bool(False),
                                   #    timetype   = cms.untracked.string('lumiid')
                                   #    timetype   = cms.untracked.string('runnumber')
                                   )
 
-promptCalibHarvest = cms.Path(alcaBeamSpotHarvester)
+
+ALCAHARVESTBeamSpotByRun = alcaBeamSpotHarvester.clone()
+ALCAHARVESTBeamSpotByRun.AlcaBeamSpotHarvesterParameters.BeamSpotOutputBase = cms.untracked.string("runbased")
+ALCAHARVESTBeamSpotByRun.AlcaBeamSpotHarvesterParameters.outputRecordName = cms.untracked.string("BeamSpotObjectsRcdByRun")
+ALCAHARVESTBeamSpotByRun_dbOutput = cms.PSet(record = cms.string('BeamSpotObjectsRcdByRun'),
+                                             tag = cms.string('BeamSpotObject_ByRun'),
+                                             timetype   = cms.untracked.string('runnumber'))
+
+
+ALCAHARVESTBeamSpotByLumi = alcaBeamSpotHarvester.clone()
+ALCAHARVESTBeamSpotByLumi.AlcaBeamSpotHarvesterParameters.BeamSpotOutputBase = cms.untracked.string("lumibased")
+ALCAHARVESTBeamSpotByLumi.AlcaBeamSpotHarvesterParameters.outputRecordName = cms.untracked.string("BeamSpotObjectsRcdByLumi")
+ALCAHARVESTBeamSpotByLumi_dbOutput = cms.PSet(record = cms.string('BeamSpotObjectsRcdByLumi'),
+                                             tag = cms.string('BeamSpotObject_ByLumi'),
+                                             timetype   = cms.untracked.string('lumiid'))
+
+
+
+BeamSpotByRun = cms.Path(ALCAHARVESTBeamSpotByRun)
+BeamSpotByLumi = cms.Path(ALCAHARVESTBeamSpotByLumi)
+
+
+#promptCalibHarvest = cms.Path(alcaBeamSpotHarvester)
 
 
 
