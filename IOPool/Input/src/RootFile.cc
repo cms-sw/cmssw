74bbcaefd781aca55801894d1214645038e04ec1
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2010-07-02 16:29:14 +0000

    Restore head from previous commit

--- IOPool/Input/src/RootFile.cc
+++ IOPool/Input/src/RootFile.cc
@@ -919,7 +919,6 @@ namespace edm {
 
     // For now always fill these. This needs to be improved
     // to only fill these parts of the index when required
-    indexIntoFile_.fillRunOrLumiIndexes();
     fillEventNumbersInIndex();
     fillEventEntriesInIndex();
   }
