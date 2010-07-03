86d03f060ba779ddacb1d4209584a0c4ed80f017
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2010-07-03 19:15:47 +0000

    update usage of namespace

--- DataFormats/CastorReco/src/CastorCluster.cc
+++ DataFormats/CastorReco/src/CastorCluster.cc
@@ -1,10 +1,8 @@
 #include "DataFormats/CastorReco/interface/CastorCluster.h"
 
-using namespace reco;
-
-CastorCluster::CastorCluster(const double energy, const ROOT::Math::XYZPoint& position, const double emEnergy, const double hadEnergy, 
+reco::CastorCluster::CastorCluster(const double energy, const ROOT::Math::XYZPoint& position, const double emEnergy, const double hadEnergy, 
 			     const double fem, const double width, const double depth, const double fhot, const double sigmaz,
-			     const CastorTowerRefVector& usedTowers) {
+			     const reco::CastorTowerRefVector& usedTowers) {
   position_ = position;
   energy_ = energy;
   emEnergy_ = emEnergy;
@@ -14,11 +12,11 @@ CastorCluster::CastorCluster(const double energy, const ROOT::Math::XYZPoint& po
   depth_ = depth;
   fhot_ = fhot;
   sigmaz_ = sigmaz;
-  for(CastorTowerRefVector::const_iterator towerit  = usedTowers.begin(); towerit != usedTowers.end();++towerit) {
+  for(reco::CastorTowerRefVector::const_iterator towerit  = usedTowers.begin(); towerit != usedTowers.end();++towerit) {
     usedTowers_.push_back( (*towerit) );
   }
 }
 
-CastorCluster::~CastorCluster() {
+reco::CastorCluster::~CastorCluster() {
 
 }
---
+++ DataFormats/CastorReco/src/CastorJet.cc
@@ -0,0 +1,10 @@
+#include "DataFormats/CastorReco/interface/CastorJet.h"
+
+reco::CastorJet::CastorJet(const double energycal, const reco::CastorClusterRef& usedCluster) {
+  energycal_ = energycal;
+  usedCluster_ = usedCluster;
+}
+
+reco::CastorJet::~CastorJet() {
+
+}
