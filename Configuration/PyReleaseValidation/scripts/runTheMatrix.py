91364089059989d929d6cda06ef941419de901cf
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-07-05 16:24:20 +0000

    ops...remove debug output

--- Configuration/PyReleaseValidation/scripts/runTheMatrix.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix.py
@@ -135,7 +135,6 @@ class WorkFlowRunner(Thread):
             if self.wf.cmdStep3 and retStep2 == 0:
                 fullcmd = preamble
                 fullcmd += self.wf.cmdStep3
-                print "Command is: " + fullcmd
                 # FIXME: dirty hack for beam-spot dedicated relval
                 if not '4.6' in str(self.wf.numId):
                     fullcmd += ' --filein file:reco.root --fileout file:step3.root '
@@ -146,7 +145,6 @@ class WorkFlowRunner(Thread):
                 if self.wf.cmdStep4 and retStep3 == 0:
                     fullcmd = preamble
                     fullcmd += self.wf.cmdStep4
-                    print "Command is: " + fullcmd
                     # FIXME: dirty hack for beam-spot dedicated relval
                     if not '4.6' in str(self.wf.numId):
                         fullcmd += ' --filein file:step3.root '
