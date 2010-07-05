7930cd7fcf21ccbd7976356b8365a823eaae77d6
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2010-07-05 04:35:55 +0000

    fix dictionary definition

--- SimDataFormats/PileupSummaryInfo/src/classes.h
+++ SimDataFormats/PileupSummaryInfo/src/classes.h
@@ -6,7 +6,8 @@ namespace {
   struct dictionary {
     PileupSummaryInfo dummy0;
     std::vector<PileupSummaryInfo> dummy1;
-    edm::Wrapper<std::vector<PileupSummaryInfo> > dummy2;
+    edm::Wrapper<PileupSummaryInfo> dummy2;
+    edm::Wrapper<std::vector<PileupSummaryInfo> > dummy3;
   };
 }
 
