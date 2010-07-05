6e7b7672425c0f6d1487f1ba08f8f2978f9023e9
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2010-07-05 03:55:59 +0000

    New package containing Pileup summary object to be inserted into the Event

---
+++ SimDataFormats/PileupSummaryInfo/src/classes.h
@@ -0,0 +1,13 @@
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace { 
+  struct dictionary {
+    PileupSummaryInfo dummy0;
+    std::vector<PileupSummaryInfo> dummy1;
+    edm::Wrapper<std::vector<PileupSummaryInfo> > dummy2;
+  };
+}
+
+
