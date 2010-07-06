e7d62c2b1838b5d718bcd34c88ff7daedfba06f4
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-07-06 17:54:33 +0000

    Changed file in Jet Reco package

--- RecoLocalCalo/Castor/python/Castor_cff.py
+++ RecoLocalCalo/Castor/python/Castor_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoLocalCalo.Castor.CastorCellReco_cfi import *
 from RecoLocalCalo.Castor.CastorTowerReco_cfi import *
-from RecoJets.JetProducers.ak7BasicJets_cfi import *
+from RecoJets.JetProducers.ak7CastorJets_cfi import *
 from RecoJets.JetProducers.ak7CastorJetID_cfi import *
 
 CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*ak7BasicJets*ak7CastorJetID)
