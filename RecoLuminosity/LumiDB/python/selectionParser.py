29d0b4626573d600c0158222a34262e0d7617b0c
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-07-08 16:00:48 +0000

    add len(ls)==0 case

--- RecoLuminosity/LumiDB/python/selectionParser.py
+++ RecoLuminosity/LumiDB/python/selectionParser.py
@@ -7,8 +7,12 @@ class selectionParser(object):
         for k,v in strresult.items():
             expandedvalues=[]
             for w in v:
+                if len(w)==0:
+                    self.__result[int(k)]=expandedvalues
+                    self.__strresult[k]=[]
+                    continue
             ###weed out [10]-like stuff just in case they exist
-                if len(w)==1:
+                elif len(w)==1:
                     expandedvalues.append(w[0])
             ##weed out [10,10]-like stuff
                 elif len(w)==2 and w[0]==w[1]:
@@ -33,7 +37,7 @@ class selectionParser(object):
     def numls(self,run):
         return len(self.__result[run])
 if __name__ == "__main__":
-    s=selectionParser('{"1":[[3,45]],"2":[[4,8],[10,10]]}')
+    s=selectionParser('{"1":[[3,45]],"2":[[4,8],[10,10]],"3":[[]]}')
     print 'runs : ',s.runs()
     print 'full result : ',s.runsandls()
     print 'str result : ',s.runsandlsStr()
