1910e350bec96f193ddd9735e89688cf2bfddee5
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2010-07-08 17:52:21 +0000

    update to new global tag

--- PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -20,7 +20,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START36_V4::All')
+process.GlobalTag.globaltag = cms.string('START38_V6::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 ## Standard PAT Configuration File
