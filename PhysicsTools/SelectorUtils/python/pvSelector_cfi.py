623f41fb2a0186e05f126137441b759fd5919529
Kevin Patrick Lannon <sha1-98da13cf4627d3b3f63196eb5312c9bb6dc47cd9@cern.ch>
2010-07-08 02:10:35 +0000

    Changed ndof cut from 5 to 4 to match top group selection (v2)

--- PhysicsTools/SelectorUtils/python/pvSelector_cfi.py
+++ PhysicsTools/SelectorUtils/python/pvSelector_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 pvSelector = cms.PSet(
     pvSrc = cms.InputTag('offlinePrimaryVertices'),
-    minNdof = cms.double(5.0),
+    minNdof = cms.double(4.0),
     maxZ = cms.double(15.0),
     maxRho = cms.double(2.0)
     )
