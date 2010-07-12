b3facaf66925fa326a41f67f34660616d214aa32
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2010-07-12 15:46:25 +0000

    Gains from file

--- RecoTBCalo/HcalPlotter/python/hcal_tb10_cff.py
+++ RecoTBCalo/HcalPlotter/python/hcal_tb10_cff.py
@@ -14,13 +14,17 @@ hcales_ascii = cms.ESSource("HcalTextCalibrations",
     cms.PSet(
     object = cms.string('Pedestals'),
     file = cms.FileInPath('RecoTBCalo/HcalPlotter/data/ped_tb2010_000495.txt')
+    ),
+    cms.PSet(
+    object = cms.string('Gains'),
+    file = cms.FileInPath('RecoTBCalo/HcalPlotter/data/gain_tb2010_muon1.txt')
     )
     )
                                 )
 
 hcales_hardcode = cms.ESSource("HcalHardcodeCalibrations",
                                toGet = cms.untracked.vstring('PedestalWidths', 'LutMetadata',
-                                                             'Gains', 'GainWidths', 'LUTCorrs', 
+                                                             'GainWidths', 'LUTCorrs', 
                                                              'PFCorrs', 'QIEData',
                                                              'L1TriggerObjects','ZSThresholds','DcsValues',
                                                              'ChannelQuality','RespCorrs','TimeCorrs')
