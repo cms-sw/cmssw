d03af8d06909f335e86767e6c0a760ceb26ea34c
Lorenzo Uplegger <sha1-223a82789f8ecd9e56ff8ac23bb2cbd39fd43227@cern.ch>
2010-07-12 19:44:00 +0000

    Fixed a small thing in the megascript

--- RecoVertex/BeamSpotProducer/scripts/BeamSpotWorkflow.py
+++ RecoVertex/BeamSpotProducer/scripts/BeamSpotWorkflow.py
@@ -47,8 +47,8 @@ except:
     try:
         import simplejson as json
     except:
-        print "Please set a crab environment in order to get the proper JSON lib"
-        sys.exit(1)
+        error = "Please set a crab environment in order to get the proper JSON lib"
+        exit(error)
 
 #####################################################################################
 # General functions
@@ -158,7 +158,7 @@ def getListOfRunsAndLumiFromRR(lastRun=-1):
             time.sleep(5)
         if tries==10:
             error = "Run registry unaccessible.....exiting now"
-            sys.exit(error)
+            exit(error)
     
 
     listOfRuns=[]
