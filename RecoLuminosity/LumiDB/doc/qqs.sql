95b2035189c8bd6b17a0db7f7f6e5624b7435468
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-07-13 14:36:01 +0000

    add mapperq

--- RecoLuminosity/LumiDB/doc/qqs.sql
+++ RecoLuminosity/LumiDB/doc/qqs.sql
@@ -3,4 +3,6 @@ select cmslsnum,instlumi,numorbit,startorbit,beamstatus,beamenery from lumisumma
 select sum(instlumi) from lumisummary where runnum=:runnum and lumiversion=:lumiversion
 select cmslsnum,trgcount,deadtime,bitname,prescale from trg where runnum=:runnum and bitnum=0;
 select  cmslsnum,trgcount,deadtime,bitnum,prescale from trg where runnum=:runnum and bitname=:bitname;
+select  cmslsnum,trgcount,deadtime,bitnum,prescale from trg where runnum=:runnum
 select s.cmslsnum,d.bxlumivalue,d.bxlumierror,d.bxlumiquality,d.algoname from LUMIDETAIL d,LUMISUMMARY s where s.runnum=:runnumber and d.algoname=:algoname and s.lumisummary_id=d.lumisummary_id order by s.startorbit,s.cmslsnum
+select trghltmap.hltpathname,trghltmap.l1seed from cmsrunsummary,trghltmap where cmsrunsummary.runnum=:runnum and trghltmap.hltkey=cmsrunsummary.hltkey
