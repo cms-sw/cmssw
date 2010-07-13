d523c138c5169ae1b05a79cda974038a9ffc13cc
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2010-07-13 10:55:49 +0000

    Scripts and cfgs to run the statistical errors analysis using pseudoexperiments from Daniele

---
+++ MuonAnalysis/MomentumScaleCalibration/test/StatisticalErrors/TakeParameterFromBatch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+     echo "Error, usage is:"
+     echo "$0 N (dir)"
+     echo "Where"
+     echo "  - N is the number of the line at which the parameter to extract is"
+     echo "  - dir is the directory where to look for FitParameters_xxx.txt file"
+     echo "    (optional, default is the last created)"
+     exit
+fi
+
+if [ $# -eq 2 ]
+then
+    dir=$2
+else
+    dir=`\ls -d1rt StatErrors_* | tail -1`
+fi
+
+echo $dir
+
+echo Taking parameter at line $1
+
+if [ -f Values.txt ]
+then
+    rm Values.txt
+fi
+
+for file in $(ls ${dir}/FitParameters_*.txt)
+do
+    value=$(sed -n "${1}p" ${file} | awk '{print $9}' | awk -F+ '{print $1}')
+    #echo "dir = $dir, file = $file value = $value"
+    echo $value >> Values.txt
+done
