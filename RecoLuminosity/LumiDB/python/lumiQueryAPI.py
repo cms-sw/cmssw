abe8bb83566e145b4fa5fc661095619b85997168
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-07-13 18:04:39 +0000

    addq

--- RecoLuminosity/LumiDB/python/lumiQueryAPI.py
+++ RecoLuminosity/LumiDB/python/lumiQueryAPI.py
@@ -1,25 +1,90 @@
+import os
 import coral
+from RecoLuminosity.LumiDB import nameDealer,lumiTime
 '''
 This module defines lowlevel SQL query API for lumiDB 
 We do not like range queries so far because of performance of range scan. 
 The principle is to query by runnumber and per each coral queryhandle
 Try reuse db session/transaction and just renew query handle each time to reduce metadata queries.
 Avoid explicit order by, should be solved by asc index in the schema.
+Do not handle transaction in here.
+Do not do explicit del in here.
 '''
 def runsummaryByrun(queryHandle,runnum):
     '''
-    select fillnum,sequence,hltkey,starttime,stoptime from cmsrunsummary where runnum=:runnum
+    select fillnum,sequence,hltkey,to_char(starttime),to_char(stoptime) from cmsrunsummary where runnum=:runnum
     output: [fillnum,sequence,hltkey,starttime,stoptime]
     '''
-    queryHandle.add
-    pass
+    t=lumiTime.lumiTime()
+    result=[]
+    queryHandle.addToTableList(nameDealer.cmsrunsummaryTableName(),'runsummary')
+    queryCondition=coral.AttributeList()
+    queryCondition.extend('runnum','unsigned int')
+    queryCondition['runnum'].setData(int(runnum))
+    queryHandle.addToOutputList('runsummary.fillnum')
+    queryHandle.addToOutputList('runsummary.sequence')
+    queryHandle.addToOutputList('runsummary.hltkey')
+    queryHandle.addToOutputList('to_char(runsummary.starttime,\''+t.coraltimefm+'\')')
+    queryHandle.addToOutputList('to_char(runsummary.stoptime,\''+t.coraltimefm+'\')')
+    queryHandle.setCondition('runnum=:runnum',queryCondition)
+    queryResult=coral.AttributeList()
+    queryResult.extend('fillnum','unsigned int')
+    queryResult.extend('sequence','string')
+    queryResult.extend('hltkey','string')
+    queryResult.extend('starttime','string')
+    queryResult.extend('stoptime','string')
+    queryHandle.defineOutput(queryResult)
+    cursor=queryHandle.execute()
+    while cursor.next():
+        result.append(cursor.currentRow()['fillnum'].data())
+        result.append(cursor.currentRow()['sequence'].data())
+        result.append(cursor.currentRow()['hltkey'].data())
+        result.append(cursor.currentRow()['starttime'].data())
+        result.append(cursor.currentRow()['stoptime'].data())
+    if len(result)!=5:
+        print 'wrong runsummary result'
+        raise
+    return result
 
 def lumisummaryByrun(queryHandle,runnum,lumiversion):
     '''
     select cmslsnum,instlumi,numorbit,startorbit,beamstatus,beamenery from lumisummary where runnum=:runnum and lumiversion=:lumiversion
     output: {cmslsnum:[instlumi,numorbit,startorbit,beamstatus,beamenergy]}
     '''
-    pass
+    result={}
+    queryHandle.addToTableList(nameDealer.cmslumisummaryTableName(),'lumisummary')
+    queryCondition=coral.AttributeList()
+    queryCondition.extend('runnum','unsigned int')
+    queryCondition.extend('lumiversion','string')
+    
+    queryCondition['runnum'].setData(int(runnum))
+    queryCondition['lumiversion'].setData(lumiversion)
+    queryHandle.addToOutputList('lumisummary.cmslsnum')
+    queryHandle.addToOutputList('lumisummary.instlumi')
+    queryHandle.addToOutputList('lumisummary.numorbit')
+    queryHandle.addToOutputList('lumisummary.startorbit')
+    queryHandle.addToOutputList('lumisummary.beamstatus')
+    queryHandle.addToOutputList('lumisummary.beamenergy')
+    queryHandle.setCondition('runnum=:runnum and lumiversion=:lumiversion',queryCondition)
+    queryResult=coral.AttributeList()
+    queryResult.extend('cmslsnum','unsigned int')
+    queryResult.extend('instlumi','float')
+    queryResult.extend('numorbit','unsigned int')
+    queryResult.extend('startorbit','unsigned int')
+    queryResult.extend('beamstatus','string')
+    queryResult.extend('beamenergy','float')
+    queryHandle.defineOutput(queryResult)
+    cursor=queryHandle.execute()
+    while cursor.next():
+        cmslsnum=cursor.currentRow()['cmslsnum'].data()
+        instlumi=cursor.currentRow()['instlumi'].data()
+        numorbit=cursor.currentRow()['numorbit'].data()
+        startorbit=cursor.currentRow()['startorbit'].data()
+        beamstatus=cursor.currentRow()['beamstatus'].data()
+        beamenergy=cursor.currentRow()['beamenergy'].data()
+        if not result.has_key(cmslsnum):
+            result[cmslsnum]=[instlumi,numorbit,startorbit,beamstatus,beamenergy]
+    return result
 
 def lumisumByrun(queryHandle,runnum,lumiversion):
     '''
@@ -58,7 +123,7 @@ def trgAllbitsByrun(queryHandle,runnum):
     '''
     pass
 
-def hltBypathByrun(queryHandle,runnum.hltpath):
+def hltBypathByrun(queryHandle,runnum,hltpath):
     '''
     select cmslsnum,inputcount,acceptcount,prescale from hlt where runnum=:runnum and pathname=:hltpath
     output: {cmslsnum:[inputcount,acceptcount,prescale]}
@@ -94,3 +159,23 @@ def hlttrgMappingByrun(queryHandle,runnum):
     output: {hltpath:l1seed}
     '''
     pass
+
+if __name__=='__main__':
+    msg=coral.MessageStream('')
+    #msg.setMsgVerbosity(coral.message_Level_Debug)
+    msg.setMsgVerbosity(coral.message_Level_Error)
+    os.environ['CORAL_AUTH_PATH']='/afs/cern.ch/cms/DB/lumi'
+    svc = coral.ConnectionService()
+    connectstr='oracle://cms_orcoff_prod/cms_lumi_prod'
+    session=svc.connect(connectstr,accessMode=coral.access_ReadOnly)
+    session.typeConverter().setCppTypeForSqlType("unsigned int","NUMBER(10)")
+    session.typeConverter().setCppTypeForSqlType("unsigned long long","NUMBER(20)")
+    session.transaction().start(True)
+    schema=session.nominalSchema()
+    q=schema.newQuery()
+    runsummaryOut=runsummaryByrun(q,139400)
+    session.transaction().commit()  
+    del q
+    del session
+    del svc
+    print runsummaryOut
