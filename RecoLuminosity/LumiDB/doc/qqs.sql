97050f3ba00bf109907686b2f55c11fb9fc401ac
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-07-13 14:30:19 +0000

    added

---
+++ RecoLuminosity/LumiDB/doc/qqs.sql
@@ -0,0 +1,6 @@
+select fillnum,sequence,hltkey,startime,stoptime from cmsrunsummary where runnum=:runnum
+select cmslsnum,instlumi,numorbit,startorbit,beamstatus,beamenery from lumisummary where runnum=:runnum and lumiversion=:lumiversion
+select sum(instlumi) from lumisummary where runnum=:runnum and lumiversion=:lumiversion
+select cmslsnum,trgcount,deadtime,bitname,prescale from trg where runnum=:runnum and bitnum=0;
+select  cmslsnum,trgcount,deadtime,bitnum,prescale from trg where runnum=:runnum and bitname=:bitname;
+select s.cmslsnum,d.bxlumivalue,d.bxlumierror,d.bxlumiquality,d.algoname from LUMIDETAIL d,LUMISUMMARY s where s.runnum=:runnumber and d.algoname=:algoname and s.lumisummary_id=d.lumisummary_id order by s.startorbit,s.cmslsnum
