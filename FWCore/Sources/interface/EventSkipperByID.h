a92dfae4446f71f3f0d934f150bc79454ea28c12
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-07-16 22:59:10 +0000

    get rid of unneeded include

--- FWCore/Sources/interface/EventSkipperByID.h
+++ FWCore/Sources/interface/EventSkipperByID.h
@@ -3,12 +3,12 @@
 
 #include "DataFormats/Provenance/interface/EventID.h"
 #include "DataFormats/Provenance/interface/EventRange.h"
-#include "DataFormats/Provenance/interface/IndexIntoFile.h"
 #include "DataFormats/Provenance/interface/LuminosityBlockID.h"
 #include "DataFormats/Provenance/interface/LuminosityBlockRange.h"
 #include "DataFormats/Provenance/interface/RunID.h"
 #include "boost/shared_ptr.hpp"
-#include "memory"
+#include <memory>
+#include <vector>
 
 namespace edm {
   class ParameterSet;
