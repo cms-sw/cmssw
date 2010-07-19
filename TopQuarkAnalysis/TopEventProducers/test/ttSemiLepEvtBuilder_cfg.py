fa867ea53b67fa880331e901f44f7b91260d3e22
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2010-07-19 11:43:11 +0000

    updated GlobalTags to keep unit tests running in 38X

--- TopQuarkAnalysis/TopEventProducers/test/ttSemiLepEvtBuilder_cfg.py
+++ TopQuarkAnalysis/TopEventProducers/test/ttSemiLepEvtBuilder_cfg.py
@@ -33,7 +33,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 
 ## configure conditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START36_V4::All')
+process.GlobalTag.globaltag = cms.string('START38_V7::All')
 
 ## load magnetic field
 process.load("Configuration.StandardSequences.MagneticField_cff")
