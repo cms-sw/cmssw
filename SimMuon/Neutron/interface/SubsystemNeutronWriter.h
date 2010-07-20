fa5da5abbf826298f30f77f4c716ba9f03898e21
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-07-20 03:25:59 +0000

    Temporarily remove untagged changes and add missing header

--- SimMuon/Neutron/interface/SubsystemNeutronWriter.h
+++ SimMuon/Neutron/interface/SubsystemNeutronWriter.h
@@ -16,6 +16,7 @@
 #include <map>
 #include "SimDataFormats/TrackingHit/interface/PSimHitContainer.h"
 #include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "CLHEP/Random/RandFlat.h"
 
 
@@ -55,7 +56,7 @@ protected:
   void writeCluster(int chamberType, const edm::PSimHitContainer & cluster);
 
   /// helper to add time offsets and local det ID
-  void adjust(PSimHit & h, float timeOffset, float smearing);
+  void adjust(PSimHit & h, float timeOffset);
 
   /// updates the counter
   void updateCount(int chamberType);
