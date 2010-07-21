65dab30f270ecab69abb3155fef51b74583d39a5
Aram Avetisyan <sha1-7ff0e5566e0f225722b7be057c805d9777704bf9@cern.ch>
2010-07-21 14:15:55 +0000

    Removing files and limiting message logging

--- Validation/RecoB/test/validation_cfg.py
+++ Validation/RecoB/test/validation_cfg.py
@@ -9,11 +9,11 @@ process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 #keep the logging output to a nice level
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.load("DQMServices.Core.DQM_cfg")
 
 process.load("RecoBTag.Configuration.RecoBTag_cff")
-
 
 process.load("PhysicsTools.JetMCAlgos.CaloJetsMCFlavour_cfi")  
 
@@ -38,11 +38,6 @@ process.dqmSaver.saveByRun = cms.untracked.int32(-1)
 process.dqmSaver.saveAtJobEnd =cms.untracked.bool(True) 
 process.dqmSaver.forceRunNumber = cms.untracked.int32(1)
 process.PoolSource.fileNames = [
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/CAAA36CC-9841-DE11-A587-0019B9F730D2.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/B47CEC98-E641-DE11-9999-001D09F2437B.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/98E6DFEA-9941-DE11-B198-001D09F25438.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/74475B04-9B41-DE11-A6CB-001D09F24D8A.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/6A9F37C0-9B41-DE11-8334-001D09F28C1E.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/4CF9716F-9E41-DE11-A0BA-001D09F25438.root',
-       '/store/relval/CMSSW_3_1_0_pre7/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0004/18B38D57-9C41-DE11-9DD9-001D09F250AF.root' ]
+
+]
 
