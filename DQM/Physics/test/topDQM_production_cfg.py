085d9b36f4301a52fd448c03d115f7fde7adab2f
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-07-22 17:53:34 +0000

    production cfg for top high statistics workflow

---
+++ DQM/Physics/test/topDQM_production_cfg.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('TOPDQM')
+
+## imports of standard configurations
+process.load('DQMOffline.Configuration.DQMOffline_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+## global tag (needed for JEC)
+#process.GlobalTag.globaltag = 'GR10_P_V5::All' ## for data with CMSSW_3_6_1_patch4
+process.GlobalTag.globaltag = 'START38_V7::All' ## for CMSSW_3_8_0
+
+
+## input file(s) for testing
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+      'file:/afs/desy.de/user/r/rwolf/cms13/samples/847D00B0-608E-DF11-A37D-003048678FA0.root'
+    )
+)
+
+## number of events
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(-1)
+)
+
+## output
+process.output = cms.OutputModule("PoolOutputModule",
+  fileName       = cms.untracked.string('topDQM_production.root'),
+  outputCommands = cms.untracked.vstring('drop *_*_*_*','keep *_*_*_TOPDQM'),
+  splitLevel     = cms.untracked.int32(0),
+  dataset = cms.untracked.PSet(
+    dataTier   = cms.untracked.string(''),
+    filterName = cms.untracked.string('')
+  )
+)
+
+
+## load jet corrections
+process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
+process.prefer("ak5CaloL2L3")
+
+## check the event content
+process.content = cms.EDAnalyzer("EventContentAnalyzer")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.categories.append('TopSingleLeptonDQM'   )
+process.MessageLogger.cerr.TopSingleLeptonDQM    = cms.untracked.PSet(limit = cms.untracked.int32(1))
+process.MessageLogger.categories.append('TopDiLeptonOfflineDQM')
+process.MessageLogger.cerr.TopDiLeptonOfflineDQM = cms.untracked.PSet(limit = cms.untracked.int32(1))
+
+
+## path definitions
+process.p      = cms.Path(
+   #process.content *
+    process.topDiLeptonOfflineDQM +
+    process.topSingleLeptonDQM
+)
+
+process.endjob = cms.Path(
+    process.endOfProcess
+)
+
+process.fanout = cms.EndPath(
+    process.output
+)
+
+## schedule definition
+process.schedule = cms.Schedule(
+    process.p,
+    process.endjob,
+    process.fanout
+)
