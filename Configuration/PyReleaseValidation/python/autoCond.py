8db84883964a21178ea5fcb60745b307013ae1de
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-07-22 15:24:54 +0000

    Update GT to V8 for 38X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
-autoCond = { 'mc'      : 'MC_38Y_V7::All',
-             'startup' : 'START38_V7::All',
+autoCond = { 'mc'      : 'MC_38Y_V8::All',
+             'startup' : 'START38_V8::All',
              'craft08' : 'CRFT8_37R_V0::All',
-             'craft09' : 'CRFT9_38R_V7::All',
-             'com10'   : 'GR_R_38X_V7::All',
-             'hltonline' : 'GR_R_38X_V7::All' # same as com10 until H tag available
+             'craft09' : 'CRFT9_38R_V8::All',
+             'com10'   : 'GR_R_38X_V8::All',
+             'hltonline' : 'GR_R_38X_V8::All' # same as com10 until H tag available
              }
