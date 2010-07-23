dceb074ae9a23e46996f17a3732d7fbf3a0c7b4f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-07-23 22:22:07 +0000

    Really reset training after initial EventAuxiliary reads

--- IOPool/Input/src/RootFile.cc
+++ IOPool/Input/src/RootFile.cc
@@ -375,6 +375,9 @@ namespace edm {
     setIfFastClonable(remainingEvents, remainingLumis);
 
     setRefCoreStreamer(true);  // backward compatibility
+
+    // We are done with our initial reading of EventAuxiliary.
+    eventTree_.resetTraining();
   }
 
   RootFile::~RootFile() {
@@ -888,17 +891,15 @@ namespace edm {
     indexIntoFile_.fixIndexes(orderedProcessHistoryIDs_);
     indexIntoFile_.setNumberOfEvents(eventTree_.entries());
     indexIntoFile_.setEventFinder(boost::shared_ptr<IndexIntoFile::EventFinder>(new RootFileEventFinder(eventTree_)));
-    if (!noEventSort_) {
-      // We need event entries for sorting.
-      indexIntoFile_.fillEventEntries();
-    }
     // We fill the event numbers explicitly if we need to find events in closed files,
     // such as for secondary files (or secondary sources) or if duplicate checking across files.
     if (secondaryFile || (duplicateChecker_ && duplicateChecker_->checkingAllFiles())) {
       indexIntoFile_.fillEventNumbers();
     }
-    // We are done with our initial reading of EventAuxiliary.
-    eventTree_.resetTraining();
+    if (secondaryFile || !noEventSort_) {
+      // We need event entries for sorting or for secondary files or sources.
+      indexIntoFile_.fillEventEntries();
+    }
   }
 
   void
