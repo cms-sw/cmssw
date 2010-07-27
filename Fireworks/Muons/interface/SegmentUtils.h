29b7d4e86f80597efd8b3f96dd8aa36912906a5a
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-07-27 15:51:36 +0000

    Add line segment utility.

--- Fireworks/Muons/interface/SegmentUtils.h
+++ Fireworks/Muons/interface/SegmentUtils.h
@@ -1,16 +1,20 @@
 #ifndef FIREWORKS_MUONS_SEGMENTUTILS_H
 #define FIREWORKS_MUONS_SEGMENTUTILS_H
 
+class TGeoHMatrix;
+class TEveStraightLineSet;
+
 namespace fireworks
 {
-  void createSegment(int detector,               // DT,CSC, or RPC? 
-                     bool matchedSegment,        // Is the segment a MuonSegmentMatch?
-                     double segmentLength,       // Nominal length of the segment along chamber thickness
-                     double segmentLimit,        // Limit of the segment extent (i.e. stay inside chamber along a certain dimension) 
-                     double* segmentPosition,    // Segment position in local coordinates 
-                     double* segmentDirection,   // Segment direction in local coordinates
-                     double* segmentInnerPoint,  // Line set connect this point...
-                     double* segmentOuterPoint); // ...with this one
+  void addSegment( const TGeoHMatrix* matrix, TEveStraightLineSet& oSegmentSet,  double* localSegmentCenterPoint, double* localSegmentInnerPoint, double* localSegmentOuterPoint );
+  void createSegment( int detector,                // DT,CSC, or RPC? 
+		      bool matchedSegment,         // Is the segment a MuonSegmentMatch?
+		      double segmentLength,        // Nominal length of the segment along chamber thickness
+		      double segmentLimit,         // Limit of the segment extent (i.e. stay inside chamber along a certain dimension) 
+		      double* segmentPosition,     // Segment position in local coordinates 
+		      double* segmentDirection,    // Segment direction in local coordinates
+		      double* segmentInnerPoint,   // Line set connect this point...
+		      double* segmentOuterPoint ); // ...with this one
 }
 
 #endif
--- Fireworks/Muons/src/SegmentUtils.cc
+++ Fireworks/Muons/src/SegmentUtils.cc
@@ -2,6 +2,8 @@
 #include "Fireworks/Core/interface/fwLog.h"
 
 #include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
+#include "TEveStraightLineSet.h"
+#include "TGeoMatrix.h"
 
 #include <iostream>
 #include <cassert>
@@ -9,51 +11,79 @@
 
 namespace fireworks
 {
-  void createSegment(int detector, 
-                     bool matchedSegment,
-                     double segmentLength,
-                     double segmentLimit,
-                     double* segmentPosition,   
-                     double* segmentDirection,
-                     double* segmentInnerPoint, 
-                     double* segmentOuterPoint)
+  void
+  addSegment( const TGeoHMatrix* matrix, TEveStraightLineSet& oSegmentSet, double* localSegmentCenterPoint, double* localSegmentInnerPoint, double* localSegmentOuterPoint )
   {
-    if ( detector == MuonSubdetId::CSC )
+    double globalSegmentInnerPoint[3];
+    double globalSegmentCenterPoint[3];
+    double globalSegmentOuterPoint[3];
+
+    matrix->LocalToMaster( localSegmentInnerPoint, globalSegmentInnerPoint );
+    matrix->LocalToMaster( localSegmentCenterPoint, globalSegmentCenterPoint );
+    matrix->LocalToMaster( localSegmentOuterPoint, globalSegmentOuterPoint );
+
+    if( globalSegmentInnerPoint[1] * globalSegmentOuterPoint[1] > 0 )
     {
-      if ( matchedSegment )
+      oSegmentSet.AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
+			   globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );
+    }
+    else
+    {
+      if( fabs(globalSegmentInnerPoint[1]) > fabs(globalSegmentOuterPoint[1]) )
+	oSegmentSet.AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
+			     globalSegmentCenterPoint[0], globalSegmentCenterPoint[1], globalSegmentCenterPoint[2] );
+      else
+	oSegmentSet.AddLine( globalSegmentCenterPoint[0], globalSegmentCenterPoint[1], globalSegmentCenterPoint[2],
+			     globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );
+    }
+  }
+
+  // FIXME: This should provide limits for DT segments as well.
+  void createSegment( int detector, 
+		      bool matchedSegment,
+		      double segmentLength,
+		      double segmentLimit,
+		      double* segmentPosition,   
+		      double* segmentDirection,
+		      double* segmentInnerPoint, 
+		      double* segmentOuterPoint )
+  {
+    if( detector == MuonSubdetId::CSC )
+    {
+      if( matchedSegment )
       {
         segmentOuterPoint[0] = segmentPosition[0] + segmentLength*segmentDirection[0];
         segmentOuterPoint[1] = segmentPosition[1] + segmentLength*segmentDirection[1];
         segmentOuterPoint[2] = segmentLength;
 
-        if ( fabs(segmentOuterPoint[1]) > segmentLimit )
-          segmentOuterPoint[1] = segmentLimit*(segmentOuterPoint[1]/fabs(segmentOuterPoint[1]));
+        if( fabs(segmentOuterPoint[1]) > segmentLimit )
+          segmentOuterPoint[1] = segmentLimit * ( segmentOuterPoint[1] / fabs( segmentOuterPoint[1] ));
 
         segmentInnerPoint[0] = segmentPosition[0] - segmentLength*segmentDirection[0];
         segmentInnerPoint[1] = segmentPosition[1] - segmentLength*segmentDirection[1];
         segmentInnerPoint[2] = -segmentLength;
 
-        if ( fabs(segmentInnerPoint[1]) > segmentLimit )
-          segmentInnerPoint[1] = segmentLimit*(segmentInnerPoint[1]/fabs(segmentInnerPoint[1]));
+        if( fabs(segmentInnerPoint[1]) > segmentLimit )
+          segmentInnerPoint[1] = segmentLimit * ( segmentInnerPoint[1] / fabs( segmentInnerPoint[1] ));
 
         return;
       }
       
       else 
       {
-        segmentOuterPoint[0] = segmentPosition[0] + segmentDirection[0]*(segmentPosition[2]/segmentDirection[2]);
-        segmentOuterPoint[1] = segmentPosition[1] + segmentDirection[1]*(segmentPosition[2]/segmentDirection[2]);
+        segmentOuterPoint[0] = segmentPosition[0] + segmentDirection[0] * ( segmentPosition[2] / segmentDirection[2] );
+        segmentOuterPoint[1] = segmentPosition[1] + segmentDirection[1] * ( segmentPosition[2] / segmentDirection[2] );
         segmentOuterPoint[2] = segmentPosition[2];
 
-        if ( fabs(segmentOuterPoint[1]) > segmentLength )
-          segmentOuterPoint[1] = segmentLength*(segmentOuterPoint[1]/fabs(segmentOuterPoint[1]));
+        if( fabs( segmentOuterPoint[1] ) > segmentLength )
+          segmentOuterPoint[1] = segmentLength * ( segmentOuterPoint[1]/fabs( segmentOuterPoint[1] ));
 
-        segmentInnerPoint[0] = segmentPosition[0] - segmentDirection[0]*(segmentPosition[2]/segmentDirection[2]);
-        segmentInnerPoint[1] = segmentPosition[1] - segmentDirection[1]*(segmentPosition[2]/segmentDirection[2]);
+        segmentInnerPoint[0] = segmentPosition[0] - segmentDirection[0] * ( segmentPosition[2] / segmentDirection[2] );
+        segmentInnerPoint[1] = segmentPosition[1] - segmentDirection[1] * ( segmentPosition[2] / segmentDirection[2] );
         segmentInnerPoint[2] = -segmentPosition[2];
 
-        if ( fabs(segmentInnerPoint[1]) > segmentLength )
-          segmentInnerPoint[1] = segmentLength*(segmentInnerPoint[1]/fabs(segmentInnerPoint[1]));
+        if( fabs( segmentInnerPoint[1] ) > segmentLength )
+          segmentInnerPoint[1] = segmentLength * ( segmentInnerPoint[1] / fabs( segmentInnerPoint[1] ));
       
         return;
       }
@@ -61,7 +91,7 @@ namespace fireworks
     
     if ( detector == MuonSubdetId::DT )
     {
-      if ( matchedSegment )
+      if( matchedSegment )
       {
         segmentOuterPoint[0] = segmentPosition[0] + segmentLength*segmentDirection[0];
         segmentOuterPoint[1] = segmentPosition[1] + segmentLength*segmentDirection[1];
@@ -73,31 +103,38 @@ namespace fireworks
          
         return;
       }
-
       else
       {
-        double mag = sqrt(segmentDirection[0]*segmentDirection[0] + 
-                          segmentDirection[1]*segmentDirection[1] +
-                          segmentDirection[2]*segmentDirection[2]);
+        double mag = sqrt( segmentDirection[0] * segmentDirection[0] + 
+                           segmentDirection[1] * segmentDirection[1] +
+                           segmentDirection[2] * segmentDirection[2] );
       
-        double theta = atan2(sqrt(segmentDirection[0]*segmentDirection[0]
-                                  + segmentDirection[1]*segmentDirection[1]), segmentDirection[2]);
+        double theta = atan2( sqrt( segmentDirection[0] * segmentDirection[0]
+                                  + segmentDirection[1] * segmentDirection[1] ), segmentDirection[2] );
 
-        segmentLength /= cos(theta);
+        segmentLength /= cos( theta );
 
-        segmentInnerPoint[0] = segmentPosition[0] + (segmentDirection[0]/mag)*segmentLength;
-        segmentInnerPoint[1] = segmentPosition[1] + (segmentDirection[1]/mag)*segmentLength;
-        segmentInnerPoint[2] = segmentPosition[2] + (segmentDirection[2]/mag)*segmentLength;
+        segmentInnerPoint[0] = segmentPosition[0] + ( segmentDirection[0] / mag ) * segmentLength;
+        segmentInnerPoint[1] = segmentPosition[1] + ( segmentDirection[1] / mag ) * segmentLength;
+        segmentInnerPoint[2] = segmentPosition[2] + ( segmentDirection[2] / mag ) * segmentLength;
 
-        segmentOuterPoint[0] = segmentPosition[0] - (segmentDirection[0]/mag)*segmentLength;
-        segmentOuterPoint[1] = segmentPosition[1] - (segmentDirection[1]/mag)*segmentLength;
-        segmentOuterPoint[2] = segmentPosition[2] - (segmentDirection[2]/mag)*segmentLength;
+	std::cout << "segmentInnerPoint[0] = " << segmentInnerPoint[0]
+		  << ", segmentInnerPoint[1] = " << segmentInnerPoint[1]
+		  << ", segmentInnerPoint[2] = " << segmentInnerPoint[2] << std::endl;
+	
+        segmentOuterPoint[0] = segmentPosition[0] - ( segmentDirection[0] / mag ) * segmentLength;
+        segmentOuterPoint[1] = segmentPosition[1] - ( segmentDirection[1] / mag ) * segmentLength;
+        segmentOuterPoint[2] = segmentPosition[2] - ( segmentDirection[2] / mag ) * segmentLength;
+
+	std::cout << "segmentOuterPoint[0] = " << segmentOuterPoint[0]
+		  << ", segmentOuterPoint[1] = " << segmentOuterPoint[1]
+		  << ", segmentOuterPoint[2] = " << segmentOuterPoint[2] << std::endl;
       
         return;
       }  
     }
     
-    fwLog(fwlog::kWarning) <<" MuonSubdetId: "<< detector <<std::endl;
+    fwLog( fwlog::kWarning ) << "MuonSubdetId: " << detector << std::endl;
     return;
   }
   
