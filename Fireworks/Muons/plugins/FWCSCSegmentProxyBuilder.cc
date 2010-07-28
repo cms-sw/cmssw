1201593f48719338bd822c9d31c3a84539b42ef0
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-07-28 13:31:35 +0000

    Correct and simplify drawing of a segment. It fits into its chamber in any direction, even if the position is outside of the chamber.

--- Fireworks/Muons/plugins/FWCSCSegmentProxyBuilder.cc
+++ Fireworks/Muons/plugins/FWCSCSegmentProxyBuilder.cc
@@ -8,107 +8,97 @@
 //
 // Original Author:
 //         Created:  Sun Jan  6 23:57:00 EST 2008
-// $Id: FWCSCSegmentProxyBuilder.cc,v 1.12 2010/05/21 13:45:46 mccauley Exp $
+// $Id: FWCSCSegmentProxyBuilder.cc,v 1.13 2010/06/18 12:44:05 yana Exp $
 //
 
+#include "TEveGeoNode.h"
 #include "TEveStraightLineSet.h"
+#include "TGeoArb8.h"
 
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/DetIdToMatrix.h"
 #include "Fireworks/Core/interface/fwLog.h"
 
-#include "Fireworks/Muons/interface/CSCUtils.h"
-#include "Fireworks/Muons/interface/SegmentUtils.h"
-
 #include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
-#include "DataFormats/MuonDetId/interface/MuonSubdetId.h"
 
 class FWCSCSegmentProxyBuilder : public FWSimpleProxyBuilderTemplate<CSCSegment>
 {
 public:
-   FWCSCSegmentProxyBuilder() {}
-   virtual ~FWCSCSegmentProxyBuilder() {}
+  FWCSCSegmentProxyBuilder( void ) {}
+  virtual ~FWCSCSegmentProxyBuilder( void ) {}
   
-   REGISTER_PROXYBUILDER_METHODS();
+  REGISTER_PROXYBUILDER_METHODS();
 
 private:
-   FWCSCSegmentProxyBuilder(const FWCSCSegmentProxyBuilder&);   
-   const FWCSCSegmentProxyBuilder& operator=(const FWCSCSegmentProxyBuilder&);
+  FWCSCSegmentProxyBuilder( const FWCSCSegmentProxyBuilder& );   
+  const FWCSCSegmentProxyBuilder& operator=( const FWCSCSegmentProxyBuilder& );
 
-  void build(const CSCSegment& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext*);
+  void build( const CSCSegment& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* );
 };
 
 void
-FWCSCSegmentProxyBuilder::build(const CSCSegment& iData,           
-                                unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext*)
+FWCSCSegmentProxyBuilder::build( const CSCSegment& iData,           
+				 unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* )
 {
-  const TGeoHMatrix* matrix = item()->getGeom()->getMatrix(iData.cscDetId().rawId());
+  unsigned int rawid = iData.cscDetId().rawId();
+  const TGeoHMatrix* matrix = item()->getGeom()->getMatrix( rawid );
   
-  if ( ! matrix ) 
+  if( ! matrix ) 
   {
-    fwLog(fwlog::kError) << " failed to get geometry of CSC chamber with detid: " 
-                         << iData.cscDetId().rawId() <<std::endl;
+    fwLog(fwlog::kError) << "failed to get geometry of CSC chamber with rawid: " 
+                         << rawid << std::endl;
     return;
   }
   
+  TEveStraightLineSet* segmentSet = new TEveStraightLineSet();
+  // FIXME: This should be set elsewhere.
+  segmentSet->SetLineWidth( 3 );
+  setupAddElement( segmentSet, &oItemHolder );
+
   double length    = 0.0;
   double thickness = 0.0;
- 
-  fireworks::fillCSCChamberParameters(iData.cscDetId().station(), 
-                                      iData.cscDetId().ring(), 
-                                      length, thickness);
 
-  std::stringstream s;
-  s << "chamber" << iIndex;
-  
-  TEveStraightLineSet* segmentSet = new TEveStraightLineSet(s.str().c_str());
-  segmentSet->SetLineWidth(3);
-  setupAddElement(segmentSet, &oItemHolder);
-  
-  double localPosition[3] = 
+  TEveGeoShape* shape = item()->getGeom()->getShape( rawid );
+  if( shape ) 
   {
-    iData.localPosition().x(), iData.localPosition().y(), thickness*0.5
-  };
+    if( TGeoTrap* trap = dynamic_cast<TGeoTrap*>( shape->GetShape()))
+    {
+      length = trap->GetDz();
+      thickness = trap->GetH1();
+
+      LocalPoint pos = iData.localPosition();
+      LocalVector dir = iData.localDirection();   
+      LocalVector unit = dir.unit();
+    
+      double localPosition[3]     = {  pos.x(),  pos.y(),  pos.z() };
+      double localDirectionIn[3]  = {  dir.x(),  dir.y(),  dir.z() };
+      double localDirectionOut[3] = { -dir.x(), -dir.y(), -dir.z() };
   
-  length *= 0.5;
+      Double_t distIn = trap->DistFromInside( localPosition, localDirectionIn );
+      Double_t distOut = trap->DistFromInside( localPosition, localDirectionOut );
+      LocalVector vIn = unit * distIn;
+      LocalVector vOut = -unit * distOut;
+      double localSegmentInnerPoint[3] = { localPosition[0] + vIn.x(),
+					   localPosition[1] + vIn.y(),
+					   localPosition[2] + vIn.z() 
+      };
+      
+      double localSegmentOuterPoint[3] = { localPosition[0] + vOut.x(),
+					   localPosition[1] + vOut.y(),
+					   localPosition[2] + vOut.z() 
+      };
 
-  // If there is a bad reconstruction of the CSC segment one can have positions that
-  // lie outside the chamber. In this case just drawn the position but do not
-  // attempt to draw a line.
+      double globalSegmentInnerPoint[3];
+      double globalSegmentOuterPoint[3];
 
-  // Q: can we determine this from the shape? A: It would make sense, but doesn't work when 
-  // trying to determine from TGeoShape
+      matrix->LocalToMaster( localSegmentInnerPoint,  globalSegmentInnerPoint );
+      matrix->LocalToMaster( localSegmentOuterPoint,  globalSegmentOuterPoint );
 
-  if ( fabs(localPosition[1]) > length )
-  {
-    fwLog(fwlog::kWarning) <<" position of CSC segment "<< iIndex <<" lies outside the chamber; station: "
-                           << iData.cscDetId().station() <<"  ring: "<< iData.cscDetId().ring()
-                           << std::endl;  
-    return;
+      segmentSet->AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
+			   globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );
+    }
   }
-
-  double localDirection[3] =
-  {
-    iData.localDirection().x(), iData.localDirection().y(), iData.localDirection().z()
-  };
-  
-  double localSegmentInnerPoint[3];
-  double localSegmentOuterPoint[3];
- 
-  fireworks::createSegment(MuonSubdetId::CSC, false, 
-                           length, 0.0,
-                           localPosition, localDirection,
-                           localSegmentInnerPoint, localSegmentOuterPoint);
-
-  double globalSegmentInnerPoint[3];
-  double globalSegmentOuterPoint[3];
-
-  matrix->LocalToMaster(localSegmentInnerPoint,  globalSegmentInnerPoint);
-  matrix->LocalToMaster(localSegmentOuterPoint,  globalSegmentOuterPoint);
-  
-  segmentSet->AddLine(globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
-                      globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2]);
 }
 
 REGISTER_FWPROXYBUILDER( FWCSCSegmentProxyBuilder, CSCSegment, "CSC-segments", FWViewType::kAll3DBits | FWViewType::kRhoPhiBit | FWViewType::kRhoZBit );
