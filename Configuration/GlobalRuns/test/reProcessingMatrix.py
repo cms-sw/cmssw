ea6d4c83c89baab9edf7fdded1cd1e9356580947
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-07-28 14:04:00 +0000

    merge with updated version

--- Configuration/GlobalRuns/test/reProcessingMatrix.py
+++ Configuration/GlobalRuns/test/reProcessingMatrix.py
@@ -4,6 +4,7 @@ usage="--list"
 parser = optparse.OptionParser(usage)
 parser.add_option("--GT")
 parser.add_option("--era")
+parser.add_option("--release")
 (options,args)=parser.parse_args()
 
 def Era_8PDs():
@@ -32,22 +33,20 @@ def Era_1PDs():
     alcaMap['MinimumBias']='SiStripCalMinBias+SiStripCalZeroBias+TkAlMinBias+TkAlMuonIsolated+MuAlCalIsolatedMu+MuAlOverlaps+HcalCalIsoTrk+HcalCalDijets+DtCalib+EcalCalElectron'
     return alcaMap
             
-com='cmsDriver.py reco -s RAW2DIGI,L1Reco,RECO,DQM%s  --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent RECO,DQM --customise Configuration/GlobalRuns/reco_TLR_37X.py --cust_function customisePPData --no_exec --python_filename=rereco_%sCollision_37X.py --conditions %s'
+com='cmsDriver.py reco -s RAW2DIGI,L1Reco,RECO,DQM%s  --data --magField AutoFromDBCurrent --scenario pp --datatier RECO --eventcontent RECO,DQM --customise Configuration/GlobalRuns/reco_TLR_'+options.release+'.py --cust_function customisePPData --no_exec --python_filename=rereco_%sCollision_'+options.release+'.py --conditions %s'
 
 import os
-
-#GT='GR_R_37X_V5A::All'
 
 alcaMap=globals()["Era_%ss"%(options.era)]()
 for PD in alcaMap.keys():
     c=com
     if (PD==''):
         alca=''
-        spec=''
+        spec=options.era+'_'
         c=com+' --process reRECO'
     else:
         alca=',ALCA:%s'%(alcaMap[PD],)
-        spec=PD+"_"
+        spec=options.era+"_"+PD+"_"
     os.system(c%(alca,spec,options.GT))
 
 
