e858dbebb14caf99268f991961d04dccfaff4f9f
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-07-29 16:33:41 +0000

    added Array

---
+++ DataFormats/Math/interface/SSEArray.h
@@ -0,0 +1,61 @@
+#ifndef DataFormat_Math_SSEArray_H
+#define DataFormat_Math_SSEArray_H
+
+#if defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 4)
+#include <x86intrin.h>
+#define CMS_USE_SSE
+
+#else
+
+#ifdef __SSE2__
+#define CMS_USE_SSE
+
+#include <mmintrin.h>
+#include <emmintrin.h>
+#endif
+#ifdef __SSE3__
+#include <pmmintrin.h>
+#endif
+#ifdef __SSE4_1__
+#include <smmintrin.h>
+#endif
+
+#endif
+
+#include<cmath>
+
+namespace mathSSE {
+
+// "vertical array"
+  template<typename T, size_t S>
+  struct Sizes {
+  };
+
+  template<size_t S>
+  struct Sizes<float> {
+    typedef __m128 Vec;
+    static const size_t size = S;
+    static const size_t ssesize = (S+3)/4;
+    static const size_t arrsize = 4*ssesize;
+  };
+  
+  template<size_t S>
+  struct Sizes<double> {
+    typedef __m128d Vec;
+    static const size_t size = S;
+    static const size_t ssesize = (S+1)/2;
+    static const size_t arrsize = 2*ssesize;
+  };
+  
+  template<typename T, size_t S>
+  union Array {
+    typedef Sizes<S> Size;
+    typedef typename Size::Vec Vec;
+    Vec vec[SIZE::ssesize];
+    T __attribute__ ((aligned(16))) arr[SIZE::arrsize];
+  };
+
+
+}
+
+}
