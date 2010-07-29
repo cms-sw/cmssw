b2b4038c44d6a44fcc84f3211da7621b68747f2b
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-07-29 08:48:18 +0000

    seems working

--- DataFormats/Math/test/SSEMathFun_t.cpp
+++ DataFormats/Math/test/SSEMathFun_t.cpp
@@ -1,4 +1,6 @@
 #include "DataFormats/Math/interface/sse_mathfun.h"
+#include "DataFormats/Math/interface/SSEVec.h"
+#include<iostream>
 
 #ifdef  CMS_USE_SSE
 
@@ -6,19 +8,27 @@
 const size_t SIZE = 10;
 
 template<size_t S>
+struct VVECSIZE {
+  static const size_t ssesize = (S+3)/4;
+  static const size_t arrsize = 4*ssesize;
+};
+
+template<size_t S>
 union VVEC {
-  const size_t SSSESIZE = (S+3)/4;
-  const size_t AllignSIZE = 4*SSSESIZE;
-  __m128 vec[SSSESIZE];
-  float __attribute__ ((aligned(16))) arr[AllignSIZE];
+  typedef VVECSIZE<S> SIZE;
+  __m128 vec[SIZE::ssesize];
+  float __attribute__ ((aligned(16))) arr[SIZE::arrsize];
 };
 
 
+typedef VVEC<10> V10;
+static const size_t ssesize = V10::SIZE::ssesize;
+static const size_t arrsize = V10::SIZE::arrsize;
 
-void chi2(VVEC const & ampl, VVEC const & err2, float t, float sumAA, float&chi2, float&amp) {
+
+void compChi2(V10 const & ampl, V10 const & err2, float t, float sumAA, float& chi2, float& amp) {
   typedef float Scalar;
-  typedev  __m128 Vec;
-  Scalar const eps = Scalar(1e-6);
+  typedef  __m128 Vec;
   Scalar const denom =  Scalar(1)/Scalar(SIZE);
 
   Vec one = _mm_set1_ps(1);
@@ -28,8 +38,8 @@ void chi2(VVEC const & ampl, VVEC const & err2, float t, float sumAA, float&chi2
   Vec alpha = _mm_set1_ps(2);
   Vec overab = _mm_set1_ps(0.2);
 
-  VVEC index;
-  for(unsigned int it = 0; it < VVEC::AllignSIZE; it++){
+  V10 index;
+  for(unsigned int it = 0; it < arrsize; it++){
     index.arr[it]=it;
   }
 
@@ -37,12 +47,12 @@ void chi2(VVEC const & ampl, VVEC const & err2, float t, float sumAA, float&chi2
   Vec sumff =  _mm_setzero_ps();
 
 
-  for(unsigned int it = 0; it < VVEC::SSESIZE; it++){
+  for(unsigned int it = 0; it < ssesize; it++){
     Vec offset =  _mm_mul_ps(_mm_sub_ps(index.vec[it],tv),overab);
     Vec term1 =  _mm_add_ps(one,offset);
-    Vec cmp = _mm_cmp_gt(term1,eps);
+    Vec cmp = _mm_cmpgt_ps(term1,eps);
     
-    Vec f = exp_pf( _mm_sub_ps(_mm_mul_ps(alpha,log_pf(term1)),offset) );
+    Vec f = exp_ps( _mm_sub_ps(_mm_mul_ps(alpha,log_ps(term1)),offset) );
     //Vec f = _mm_or_ps(_mm_andnot_ps(cmp, _mm_setzero_ps()), _mm_and_ps(cmp, f));
     f = _mm_and_ps(cmp, f);
     Vec fe = _mm_mul_ps(f, err2.vec[it]);
@@ -55,39 +65,43 @@ void chi2(VVEC const & ampl, VVEC const & err2, float t, float sumAA, float&chi2
   sumff = _mm_hadd_ps(sumff,sumff);
   sumff = _mm_hadd_ps(sumff,sumff);
 
-
+  /*
   chi2 = sumAA;
   amp = 0;
-  if( sumff > 0 ){
-    amp = sumAf/sumff;
-    chi2 = sumAA - sumAf*amp;
+  if( sumff.arr[0] > 0 ){
+    amp = sumAf.arr[0]/sumff.arr[0];
+    chi2 = sumAA - sumAf.arr[0]*amp;
   }
   chi2 *=denom;
+  */
 }
 
 
 
 int main() {  
+  using mathSSE::Vec4F;
 
   Vec4F x(0.,-1.,1.,1000.);
   std::cout << x << std::endl;
   
-  Vec4F y.vec = exp_ps(x.vec);
+  Vec4F y; y.vec = exp_ps(x.vec);
   std::cout << y << std::endl;
 
-  Vec4F z.vec = log_ps(x.vec);
+  Vec4F z; z.vec = log_ps(x.vec);
   std::cout << z << std::endl;
 
-  Vec4F y.vec = log_ps(y.vec);
+  y.vec = log_ps(y.vec);
   std::cout << y << std::endl;
 
-  Vec4F z.vec = exp_ps(z.vec);
+  z.vec = exp_ps(z.vec);
   std::cout << z << std::endl;
 
-  Vec4F k(0.,-1.,1.,1.1e-3);
+  Vec4F k(0.1,-1.,1.1e-3,1.);
   Vec4F eps(1.e-3);
-  Vec4F cmp; cmp.vec = _mm_cmp_gt(k,eps.vec);
-  std::cout << k << std::endl;
+  Vec4F cmp; cmp.vec = _mm_cmpgt_ps(k.vec,eps.vec);
+  std::cout << cmp  << std::endl;
+  k.vec = _mm_and_ps(cmp.vec, k.vec  ); 
+  std::cout << k  << std::endl;
 
   return 0;
 
@@ -99,7 +113,4 @@ int main() {
 }
 #endif
 
-
-
-#endif
 
