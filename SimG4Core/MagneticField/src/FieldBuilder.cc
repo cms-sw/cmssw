812d0a2a0a97b5ca3d5d382da364b80cacd511e8
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-07-29 22:46:28 +0000

    Changes due to Monopole transportation

--- SimG4Core/MagneticField/src/FieldBuilder.cc
+++ SimG4Core/MagneticField/src/FieldBuilder.cc
@@ -9,6 +9,7 @@
 #include "SimG4Core/MagneticField/interface/FieldBuilder.h"
 #include "SimG4Core/MagneticField/interface/Field.h"
 #include "SimG4Core/MagneticField/interface/FieldStepper.h"
+#include "SimG4Core/MagneticField/interface/G4MonopoleEquation.hh"
 #include "SimG4Core/Notification/interface/SimG4Exception.h"
 
 /*
@@ -18,6 +19,7 @@
 */
 
 #include "G4Mag_UsualEqRhs.hh"
+#include "G4ClassicalRK4.hh"
 #include "G4PropagatorInField.hh"
 #include "G4FieldManager.hh"
 #include "G4TransportationManager.hh"
@@ -34,148 +36,113 @@ FieldBuilder::FieldBuilder(const MagneticField * f,
 			   const edm::ParameterSet & p) 
    : theField( new Field(f,p.getParameter<double>("delta"))), 
      theFieldEquation(new G4Mag_UsualEqRhs(theField.get())),
-     theTopVolume(0),
+     theTopVolume(0), fChordFinder(0), fChordFinderMonopole(0),
      fieldValue(0.), minStep(0.), dChord(0.), dOneStep(0.),
      dIntersection(0.), dIntersectionAndOneStep(0.), 
      maxLoopCount(0), minEpsilonStep(0.), maxEpsilonStep(0.), 
-     thePSet(p)
-{
-    theField->fieldEquation(theFieldEquation);
+     thePSet(p) {
+
+  theField->fieldEquation(theFieldEquation);
 }
 
-/*
-void FieldBuilder::readFieldParameters(DDLogicalPart lp,
-				       const std::string& keywordField)
-{
-    int tmp;
-    tmp = map_.toString(keywordField,lp,fieldType);
-    tmp = map_.toDouble("FieldValue",lp,fieldValue);
-    tmp = map_.toString("Stepper",lp,stepper);
-    tmp = map_.toDouble("MinStep",lp,minStep);
-    tmp = map_.toDouble("DeltaChord",lp,dChord);
-    tmp = map_.toDouble("DeltaOneStep",lp,dOneStep);
-    tmp = map_.toDouble("DeltaIntersection",lp,dIntersection);
-    tmp = map_.toDouble("DeltaIntersectionAndOneStep",lp,dIntersectionAndOneStep);
-    tmp = map_.toDouble("MaximumLoopCount",lp,maxLoopCount);
-    tmp = map_.toDouble("MinimumEpsilonStep",lp,minEpsilonStep);
-    tmp = map_.toDouble("MaximumEpsilonStep",lp,maxEpsilonStep);
-    LogDebug("MagneticField") << "FieldBuilder " << lp.name() << " " 
-			      << fieldType << " " << fieldValue << " " 
-			      << stepper << " " << minStep << " " << dChord 
-			      << " " << dOneStep << " " << dIntersection << " "
-			      << dIntersectionAndOneStep << " "
-			      << maxLoopCount << " " << minEpsilonStep << " " 
-			      << maxEpsilonStep;
-    return;
-}
-*/
 
-void FieldBuilder::build( G4FieldManager* fM, G4PropagatorInField* fP)
-{
+void FieldBuilder::build( G4FieldManager* fM, G4PropagatorInField* fP) {
     
-    edm::ParameterSet thePSetForGMFM =
-       thePSet.getParameter<edm::ParameterSet>("ConfGlobalMFM");
+  edm::ParameterSet thePSetForGMFM =
+    thePSet.getParameter<edm::ParameterSet>("ConfGlobalMFM");
 
-    std::string volName = thePSetForGMFM.getParameter< std::string >("Volume");
-
-    edm::ParameterSet volPSet =
-       thePSetForGMFM.getParameter< edm::ParameterSet >( volName );
+  std::string volName = thePSetForGMFM.getParameter< std::string >("Volume");
+  
+  edm::ParameterSet volPSet =
+    thePSetForGMFM.getParameter< edm::ParameterSet >( volName );
     
-    configureForVolume( volName, volPSet, fM, fP );
+  configureForVolume( volName, volPSet, fM, fP );
     
-    // configure( "MagneticFieldType", fM, fP ) ;
+  // configure( "MagneticFieldType", fM, fP ) ;
 
-    if ( thePSet.getParameter<bool>("UseLocalMagFieldManager") )
-    {
+  if ( thePSet.getParameter<bool>("UseLocalMagFieldManager") )  {
 
-       edm::ParameterSet defpset ;
-       edm::ParameterSet thePSetForLMFM = 
-          thePSet.getUntrackedParameter<edm::ParameterSet>("ConfLocalMFM", defpset);
-       //
-       // Patology !!! LocalFM requested but configuration not given ! 
-       // In principal, need to throw an exception
-       //
-       if ( thePSetForLMFM == defpset )
-       {
-	  std::cout << " Patology ! Local Mag.Field Manager requested but config not given !\n";
-	  return ;
-       }
-       
-       std::vector<std::string> ListOfVolumes = 
-          thePSetForLMFM.getParameter< std::vector<std::string> >("ListOfVolumes");
-	  
-       // creating Local Mag.Field Manager
-       for (unsigned int i = 0; i < ListOfVolumes.size(); ++ i )
-       {
-          volPSet = thePSetForLMFM.getParameter< edm::ParameterSet >(ListOfVolumes[i]);
-          G4FieldManager* fAltM = new G4FieldManager() ;
-	  configureForVolume( ListOfVolumes[i], volPSet, fAltM ) ;
-          //configureLocalFM( ListOfVolumes[i], fAltM ) ;
-          LocalFieldManager* fLM = new LocalFieldManager( theField.get(), fM, fAltM ) ;
-          fLM->SetVerbosity(thePSet.getUntrackedParameter<bool>("Verbosity",false));
-          theTopVolume->SetFieldManager( fLM, true ) ;
-       }
+    edm::ParameterSet defpset ;
+    edm::ParameterSet thePSetForLMFM = 
+      thePSet.getUntrackedParameter<edm::ParameterSet>("ConfLocalMFM", defpset);
+    //
+    // Patology !!! LocalFM requested but configuration not given ! 
+    // In principal, need to throw an exception
+    //
+    if ( thePSetForLMFM == defpset )  {
+      std::cout << " Patology ! Local Mag.Field Manager requested but config not given !\n";
+      return ;
     }
-    return ;
- 
-}
-
-/*
-void FieldBuilder::configure(const std::string& keywordField,
-			     G4FieldManager * fM,
-			     G4PropagatorInField * fP)
-{
-  G4LogicalVolumeToDDLogicalPartMap::Vector vec = map_.all(keywordField);
-  for (G4LogicalVolumeToDDLogicalPartMap::Vector::iterator tit = vec.begin();
-       tit != vec.end(); tit++) {
-    theTopVolume = (*tit).first;
-    readFieldParameters((*tit).second,keywordField);	
-    if (fM!=0) configureFieldManager(fM);
-    if (fP!=0) configurePropagatorInField(fP);	
+       
+    std::vector<std::string> ListOfVolumes = 
+      thePSetForLMFM.getParameter< std::vector<std::string> >("ListOfVolumes");
+	  
+    // creating Local Mag.Field Manager
+    for (unsigned int i = 0; i < ListOfVolumes.size(); ++ i )   {
+      volPSet = thePSetForLMFM.getParameter< edm::ParameterSet >(ListOfVolumes[i]);
+      G4FieldManager* fAltM = new G4FieldManager() ;
+      configureForVolume( ListOfVolumes[i], volPSet, fAltM ) ;
+      //configureLocalFM( ListOfVolumes[i], fAltM ) ;
+      LocalFieldManager* fLM = new LocalFieldManager( theField.get(), fM, fAltM ) ;
+      fLM->SetVerbosity(thePSet.getUntrackedParameter<bool>("Verbosity",false));
+      theTopVolume->SetFieldManager( fLM, true ) ;
+    }
   }
   return ;
+ 
 }
-*/
 
 void FieldBuilder::configureForVolume( const std::string& volName,
                                        edm::ParameterSet& volPSet,
 				       G4FieldManager * fM,
-				       G4PropagatorInField * fP )
-{
+				       G4PropagatorInField * fP ) {
 
-   G4LogicalVolumeStore* theStore = G4LogicalVolumeStore::GetInstance();
-   for (unsigned int i=0; i<(*theStore).size(); ++i )
-   {
-      std::string curVolName = ((*theStore)[i])->GetName();
-      if ( curVolName == volName )
-      {
-         theTopVolume = (*theStore)[i] ;
-      }
-   }
+  G4LogicalVolumeStore* theStore = G4LogicalVolumeStore::GetInstance();
+  for (unsigned int i=0; i<(*theStore).size(); ++i ) {
+    std::string curVolName = ((*theStore)[i])->GetName();
+    if ( curVolName == volName ) {
+      theTopVolume = (*theStore)[i] ;
+    }
+  }
 
-   fieldType     = volPSet.getParameter<std::string>("Type") ;
-   stepper       = volPSet.getParameter<std::string>("Stepper") ;
-   edm::ParameterSet stpPSet = 
-      volPSet.getParameter<edm::ParameterSet>(stepper) ;
-   minStep       = stpPSet.getParameter<double>("MinStep") ;
-   dChord        = stpPSet.getParameter<double>("DeltaChord") ;
-   dOneStep      = stpPSet.getParameter<double>("DeltaOneStep") ;
-   dIntersection = stpPSet.getParameter<double>("DeltaIntersection") ;
-   dIntersectionAndOneStep = stpPSet.getUntrackedParameter<double>("DeltaIntersectionAndOneStep",-1.);
-   maxLoopCount = stpPSet.getUntrackedParameter<double>("MaximumLoopCounts",1000);
-   minEpsilonStep = stpPSet.getUntrackedParameter<double>("MinimumEpsilonStep",0.00001);
-   maxEpsilonStep = stpPSet.getUntrackedParameter<double>("MaximumEpsilonStep",0.01);
+  fieldType     = volPSet.getParameter<std::string>("Type") ;
+  stepper       = volPSet.getParameter<std::string>("Stepper") ;
+  edm::ParameterSet stpPSet = 
+    volPSet.getParameter<edm::ParameterSet>(stepper) ;
+  minStep       = stpPSet.getParameter<double>("MinStep") ;
+  dChord        = stpPSet.getParameter<double>("DeltaChord") ;
+  dOneStep      = stpPSet.getParameter<double>("DeltaOneStep") ;
+  dIntersection = stpPSet.getParameter<double>("DeltaIntersection") ;
+  dIntersectionAndOneStep = stpPSet.getUntrackedParameter<double>("DeltaIntersectionAndOneStep",-1.);
+  maxLoopCount = stpPSet.getUntrackedParameter<double>("MaximumLoopCounts",1000);
+  minEpsilonStep = stpPSet.getUntrackedParameter<double>("MinimumEpsilonStep",0.00001);
+  maxEpsilonStep = stpPSet.getUntrackedParameter<double>("MaximumEpsilonStep",0.01);
    
-   if (fM!=0) configureFieldManager(fM);
-   if (fP!=0) configurePropagatorInField(fP);	
+  if (fM!=0) configureFieldManager(fM);
+  if (fP!=0) configurePropagatorInField(fP);	
 
-   return;
+  return;
 
 }
 
-void FieldBuilder::configureFieldManager(G4FieldManager * fM)
-{
-    if (fM==0) return;
+G4LogicalVolume * FieldBuilder::fieldTopVolume() { return theTopVolume; }
+
+void FieldBuilder::setStepperAndChordFinder(G4FieldManager * fM, int val) {
+
+  if (fM != 0) {
+    if (val == 0) {
+      if (fChordFinder != 0) fM->SetChordFinder(fChordFinder);
+    } else {
+      fChordFinder = fM->GetChordFinder();
+      if (fChordFinderMonopole != 0) fM->SetChordFinder(fChordFinderMonopole);
+    }
+  }
+}
+
+ 
+void FieldBuilder::configureFieldManager(G4FieldManager * fM) {
+
+  if (fM!=0) {
     fM->SetDetectorField(theField.get());
     FieldStepper * theStepper = new FieldStepper(theField->fieldEquation());
     theStepper->select(stepper);
@@ -185,18 +152,22 @@ void FieldBuilder::configureFieldManager(G4FieldManager * fM)
     fM->SetDeltaOneStep(dOneStep);
     fM->SetDeltaIntersection(dIntersection);
     if (dIntersectionAndOneStep != -1.) 
-	fM->SetAccuraciesWithDeltaOneStep(dIntersectionAndOneStep);
-    return;
+      fM->SetAccuraciesWithDeltaOneStep(dIntersectionAndOneStep);
+  }
+  if (fChordFinderMonopole == 0) {
+    G4MonopoleEquation* fMonopoleEquation = new G4MonopoleEquation(theField.get());
+    G4MagIntegratorStepper* theStepper = new G4ClassicalRK4(fMonopoleEquation);
+    fChordFinderMonopole = new G4ChordFinder(theField.get(),minStep,theStepper);
+    fChordFinderMonopole->SetDeltaChord(dChord);
+  }
 }
 
-void FieldBuilder::configurePropagatorInField(G4PropagatorInField * fP)
-{
-    if (fP==0) return;
-    fP->SetMaxLoopCount(int(maxLoopCount));
-    fP->SetMinimumEpsilonStep(minEpsilonStep);
-    fP->SetMaximumEpsilonStep(maxEpsilonStep);
-    fP->SetVerboseLevel(0);
-    return;
+void FieldBuilder::configurePropagatorInField(G4PropagatorInField * fP) {
+  if (fP==0) return;
+  fP->SetMaxLoopCount(int(maxLoopCount));
+  fP->SetMinimumEpsilonStep(minEpsilonStep);
+  fP->SetMaximumEpsilonStep(maxEpsilonStep);
+  fP->SetVerboseLevel(0);
+  return;
 }
 
-G4LogicalVolume * FieldBuilder::fieldTopVolume() { return theTopVolume; }
