d2d42e76030af1226870efe6936cbaaa99339986
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-08-02 15:46:54 +0000

    migrate to use hltGetConfiguration instead of ./getHlt.py

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -6,12 +6,10 @@ L1T='L1Menu_Commissioning2010_v3'
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-./getHLT.py --process TEST --full --offline --mc   --unprescale --l1 $L1T $HLT TEST --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG'
-mv OnLine_HLT_TEST.py offline_mc.py
-./getHLT.py --process TEST --full --offline --data --unprescale --l1 $L1T $HLT TEST
-mv OnData_HLT_TEST.py offline_data.py
-./getHLT.py --process TEST --full --online  --data --unprescale --l1 $L1T $HLT TEST
-mv OnData_HLT_TEST.py online_data.py
+mkdir -p x
+hltGetConfiguration $HLT --process TEST --full --offline --mc   --unprescale --l1 $L1T --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG' > x/offline_mc.py
+hltGetConfiguration $HLT --process TEST --full --offline --data --unprescale --l1 $L1T > x/offline_data.py
+hltGetConfiguration $HLT --process TEST --full --online  --data --unprescale --l1 $L1T > x/online_data.py
 
 {
   TABLE=$(echo $HLT | cut -d: -f2)
