443c76701f03c87b520129dec2704bea5db8ae10
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-08-02 15:47:24 +0000

    ops

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -6,10 +6,9 @@ L1T='L1Menu_Commissioning2010_v3'
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-mkdir -p x
-hltGetConfiguration $HLT --process TEST --full --offline --mc   --unprescale --l1 $L1T --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG' > x/offline_mc.py
-hltGetConfiguration $HLT --process TEST --full --offline --data --unprescale --l1 $L1T > x/offline_data.py
-hltGetConfiguration $HLT --process TEST --full --online  --data --unprescale --l1 $L1T > x/online_data.py
+hltGetConfiguration $HLT --process TEST --full --offline --mc   --unprescale --l1 $L1T --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG' > offline_mc.py
+hltGetConfiguration $HLT --process TEST --full --offline --data --unprescale --l1 $L1T > offline_data.py
+hltGetConfiguration $HLT --process TEST --full --online  --data --unprescale --l1 $L1T > online_data.py
 
 {
   TABLE=$(echo $HLT | cut -d: -f2)
