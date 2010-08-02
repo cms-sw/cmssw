a7e9befe8ad483d73964bf9503d63b2825e141c8
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-08-02 15:08:29 +0000

    update online menu, and use new --dataset option to set MC data

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-HLT='/online/collisions/2010/week30/HLT'
+HLT='/online/collisions/2010/week31/HLT'
 L1T='L1Menu_Commissioning2010_v3'
 
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-./getHLT.py --process TEST --full --offline --mc   --unprescale --l1 $L1T $HLT TEST
+./getHLT.py --process TEST --full --offline --mc   --unprescale --l1 $L1T $HLT TEST --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG'
 mv OnLine_HLT_TEST.py offline_mc.py
 ./getHLT.py --process TEST --full --offline --data --unprescale --l1 $L1T $HLT TEST
 mv OnData_HLT_TEST.py offline_data.py
