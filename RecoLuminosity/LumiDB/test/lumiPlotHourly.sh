373df2067013acbbf8ce1c723d0fccf67b637250
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-08-03 17:16:17 +0000

    fix default setup

--- RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
+++ RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 currendir=`pwd`
-workdir="/afs/cern.ch/user/x/xiezhen/w1/lumidist/CMSSW_3_7_0_pre3/src/RecoLuminosity/LumiDB"
+workdir="/build1/zx/cron/CMSSW_3_7_0_pre3"
 authdir="/afs/cern.ch/user/x/xiezhen"
 outdir="/afs/cern.ch/cms/lumi/www/plots/operation"
 logpath="/afs/cern.ch/cms/lumi/"
 dbConnectionString="oracle://cms_orcoff_prod/cms_lumi_prod"
 cd $workdir
-eval `scramv1 runtime -sh`
+eval `/afs/cern.ch/cms/sw/common/scramv1 runtime -sh`
 touch "$logpath/lumiPlotHourly.log"
 date >> "$logpath/lumiPlotHourly.log"
 cp  "$outdir/runlist.txt" "$outdir/runlist.txt.old"
 lumiPlotFiller.py -c $dbConnectionString -P $authdir createrunlist -o $outdir >> "$logpath/lumiPlotHourly.log"
 if `diff "$outdir/runlist.txt" "$outdir/runlist.txt.old" > /dev/null` ; then
-   echo 'no new runs, do nothing' 
-   lumiPlotFiller.py -c $dbConnectionString -P $authdir instperrun -o $outdir -i "$outdir/runlist.txt">> "$logpath/lumiPlotHourly.log"
+   echo 'no new runs, do nothing' >> "$logpath/lumiPlotHourly.log"
 else
    lumiPlotFiller.py -c $dbConnectionString -P $authdir instperrun -o $outdir -i "$outdir/runlist.txt">> "$logpath/lumiPlotHourly.log"
 fi   
