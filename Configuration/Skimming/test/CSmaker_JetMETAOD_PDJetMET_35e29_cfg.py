aa05c3b3a81c402b68cc3b7c56da990ebf5d3068
chiorbo <sha1-a37cc23738f7aa7c467ce16efb74e15501057467@cern.ch>
2010-08-04 10:13:40 +0000

    Correction in the names of the triggers, HLT_DiJetAve50U added

--- Configuration/Skimming/test/CSmaker_JetMETAOD_PDJetMET_35e29_cfg.py
+++ Configuration/Skimming/test/CSmaker_JetMETAOD_PDJetMET_35e29_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("makeSD")
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.2 $'),
+    version = cms.untracked.string('$Revision: 1.1 $'),
     annotation = cms.untracked.string('JetMETAOD central skim'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/Skimming/test/CSmaker_JetMETAOD_PDJetMET_35e29_cfg.py,v $')
 )
@@ -23,7 +23,22 @@ process.GlobalTag.globaltag = "GR10_P_V7::All"
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Commissioning10/MinimumBias/RAW-RECO/v8/000/132/601/F85204EE-EB40-DF11-8F71-001A64789D1C.root'
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/FC12B62D-D39E-DF11-A172-001617E30D40.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/F2C6D071-C59E-DF11-97FC-0030487CD184.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/C2ABC2C1-C49E-DF11-BB3B-0030487C90C4.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/B204921D-D89E-DF11-A164-0030487C6062.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/AA4601FB-CE9E-DF11-BF26-0030487A195C.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/A8535E5E-C79E-DF11-9C13-001D09F24493.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/94920AC0-CA9E-DF11-A18C-003048D2C174.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/6CF950BE-DD9E-DF11-A8CA-001D09F23F2A.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/64B83DAB-CF9E-DF11-9BAD-003048F110BE.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/60306759-E39E-DF11-9A9C-0030487A1884.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/427127BE-C89E-DF11-87E6-003048F117EA.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/4205F80E-C39E-DF11-960A-001D09F24303.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/3CDB7561-DC9E-DF11-8517-001D09F24664.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/2C3BE7B5-C69E-DF11-917D-001617C3B5F4.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/28F11C21-D89E-DF11-BD4E-0030487A18D8.root',
+        '/store/data/Run2010A/JetMET/RECO/v4/000/142/187/1E464E9C-DB9E-DF11-8FDC-001617DBD556.root'
         )
 )
 process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
@@ -32,8 +47,8 @@ import HLTrigger.HLTfilters.hltHighLevelDev_cfi
 
 ### JetMET AOD CS
 process.DiJetAve_1e29 = HLTrigger.HLTfilters.hltHighLevelDev_cfi.hltHighLevelDev.clone(andOr = True)
-process.DiJetAve_1e29.HLTPaths = ("HLT_DiJetAve15U_8E29","HLT_DiJetAve30U_8E29")
-process.DiJetAve_1e29.HLTPathsPrescales  = cms.vuint32(1,1)
+process.DiJetAve_1e29.HLTPaths = ("HLT_DiJetAve15U","HLT_DiJetAve30U","HLT_DiJetAve50U")
+process.DiJetAve_1e29.HLTPathsPrescales  = cms.vuint32(1,1,1)
 process.DiJetAve_1e29.HLTOverallPrescale = cms.uint32(1)
 process.DiJetAve_1e29.andOr = True
 
