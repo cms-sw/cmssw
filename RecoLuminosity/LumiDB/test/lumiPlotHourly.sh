01e3f87cfceae0c8ab3adc27a586435167b389e6
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-08-04 10:03:36 +0000

    remove diff check, always run

--- RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
+++ RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
@@ -12,10 +12,10 @@ touch "$logpath/lumiPlotHourly.log"
 date >> "$logpath/lumiPlotHourly.log"
 cp  "$outdir/runlist.txt" "$outdir/runlist.txt.old"
 lumiPlotFiller.py -c $dbConnectionString -P $authdir createrunlist -o $outdir >> "$logpath/lumiPlotHourly.log"
-if `diff "$outdir/runlist.txt" "$outdir/runlist.txt.old" > /dev/null` ; then
-   echo 'no new runs, do nothing' >> "$logpath/lumiPlotHourly.log"
-else
-   lumiPlotFiller.py -c $dbConnectionString -P $authdir instperrun -o $outdir -i "$outdir/runlist.txt">> "$logpath/lumiPlotHourly.log"
-fi   
+ lumiPlotFiller.py -c $dbConnectionString -P $authdir instperrun -o $outdir -i "$outdir/runlist.txt">> "$logpath/lumiPlotHourly.log"
+#if `diff "$outdir/runlist.txt" "$outdir/runlist.txt.old" > /dev/null` ; then
+#   echo 'no new runs, do nothing' >> "$logpath/lumiPlotHourly.log"
+#else
+#fi   
 date >> "$logpath/lumiPlotHourly.log"
 cd $currentdir
