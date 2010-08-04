531a0a05c3603aeffeec9a31537cb9977fd0cb93
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2010-08-04 09:10:27 +0000

    New, cleaned up version for 3.8.X

--- MuonAnalysis/MuonAssociators/python/muonHLTL1Match_cfi.py
+++ MuonAnalysis/MuonAssociators/python/muonHLTL1Match_cfi.py
@@ -1,13 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi import *
-from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi import *
-from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi import *
-from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-
-from math import pi
+from MuonAnalysis.MuonAssociators.muonL1Match_cfi import *
 
 muonHLTL1Match = cms.EDProducer("HLTL1MuonMatcher",
+    muonL1MatcherParameters,
+
+    # Reconstructed muons
     src = cms.InputTag("muons"),
 
     # L1 Muon collection, and preselection on that collection
@@ -19,18 +17,9 @@ muonHLTL1Match = cms.EDProducer("HLTL1MuonMatcher",
     filterIdsEnum  = cms.vstring( '*' ),
     filterIds      = cms.vint32( 0 ),
     filterLabels   = cms.vstring( '*' ),
-    pathNames      = cms.vstring( 'HLT_L1MuOpen' ),
-    collectionTags = cms.vstring( '*' ),
+    pathNames      = cms.vstring( '*' ),
+    collectionTags = cms.vstring( 'hltL1extraParticles' ),
     resolveAmbiguities    = cms.bool( True ), # if True, no more than one reco object can be matched to the same L1 object; precedence is given to the reco ones coming first in the list
-
-    # Choice of matching algorithm
-    useTrack = cms.string("tracker"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
-    useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra (not AOD)
-    useSimpleGeometry = cms.bool(True), # just use a cylinder plus two disks.
-
-    # Matching Criteria
-    maxDeltaR = cms.double(0.5),
-    l1PhiOffset = cms.double(1.25 * pi/180.), ## Offset to add to L1 phi before matching (according to L1 experts)
 
     # Fake filter lavels for the object propagated to the second muon station
     setPropLabel = cms.string("propagatedToM2"),
