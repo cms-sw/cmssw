7e5e585f3c33a57fbe26902249a1c8c187ab733c
Bruno Wittmer <sha1-a68e14ffc3191775a6893edb5ff9bc223a940403@cern.ch>
2010-08-04 14:42:11 +0000

    Added LaserAlignmentEventFilter for detecting Events with Laser Signal

--- Alignment/LaserAlignment/plugins/LaserAlignmentEventFilter.h
+++ Alignment/LaserAlignment/plugins/LaserAlignmentEventFilter.h
@@ -1,34 +1,83 @@
-// Purpose: filter only needed events for LAS
-// Original Author:  Adrian Perieanu
-//         Created:  11th of August
-
-// system include files
-#include <memory>
-#include <algorithm>
-
-// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDFilter.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-// class declaration
+class SiStripFedCabling;
+
 class LaserAlignmentEventFilter : public edm::EDFilter {
 
-public:
-  explicit LaserAlignmentEventFilter( const edm::ParameterSet& );
+ public:
+  explicit LaserAlignmentEventFilter(const edm::ParameterSet&);
   ~LaserAlignmentEventFilter();
-  
-private:
-  virtual void beginJob() ;
-  virtual bool filter( edm::Event&, const edm::EventSetup& );
-  virtual void endJob();
 
-  // container for cfg data
-  int runFirst;
-  int runLast;
-  int eventFirst;
-  int eventLast;
+ private:
+  // Map the std::vector<int> that is returned by edm::ParameterSet::getParameter() to internal representations (which are optimized for the Filter)
+  void set_las_fed_ids(const std::vector<int>& las_feds);
+  //void set_las_det_ids(const std::vector<int>& las_dets);
+  void set_las_signal_ids(const std::vector<int>& las_signal);
+  
+  virtual void beginRun(const edm::EventSetup&) ;
+  virtual bool filter(edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+  // Filter Settings
+  //bool fed_filter; // Discard unused FEDs
+  //bool det_id_filter; // Discard unused DetIds
+  bool signal_filter; // Check for LAS Signals
+
+  std::vector<uint16_t> las_fed_ids; // List of FEDs used by LAS
+  //std::vector<uint32_t> las_det_ids; // List of DetIds used by LAS
+  std::vector<uint32_t> las_signal_ids; // List of DetIds to probe for signal
+
+  uint16_t single_channel_thresh; // Signal threshold for a single channel
+  uint16_t channel_count_thresh; // Nr. of channels that have to contain signal for LAS event
+ 
+  // Debug output variables
+  std::vector<uint16_t> buffer_size;
+  std::vector<uint32_t> buffer_size_sum;
+  uint32_t LAS_event_count;
+
+  // Handling of FED Cabling
+  void updateCabling( const edm::EventSetup& );
+  const SiStripFedCabling* cabling;
+  uint32_t cacheId_;
+    
 };
+
+
+/////////////////////////////////////
+// Old Version of File from Adrian //
+/////////////////////////////////////
+/* // Purpose: filter only needed events for LAS */
+/* // Original Author:  Adrian Perieanu */
+/* //         Created:  11th of August */
+
+/* // system include files */
+/* #include <memory> */
+/* #include <algorithm> */
+
+/* // user include files */
+/* #include "FWCore/Framework/interface/Frameworkfwd.h" */
+/* #include "FWCore/Framework/interface/EDFilter.h" */
+/* #include "FWCore/Framework/interface/Event.h" */
+/* #include "FWCore/Framework/interface/MakerMacros.h" */
+/* #include "FWCore/ParameterSet/interface/ParameterSet.h" */
+
+/* // class declaration */
+/* class LaserAlignmentEventFilter : public edm::EDFilter { */
+
+/* public: */
+/*   explicit LaserAlignmentEventFilter( const edm::ParameterSet& ); */
+/*   ~LaserAlignmentEventFilter(); */
+  
+/* private: */
+/*   virtual void beginJob() ; */
+/*   virtual bool filter( edm::Event&, const edm::EventSetup& ); */
+/*   virtual void endJob(); */
+
+/*   // container for cfg data */
+/*   int runFirst; */
+/*   int runLast; */
+/*   int eventFirst; */
+/*   int eventLast; */
+/* }; */
 
---
+++ Alignment/LaserAlignment/test/LaserAlignmentEventFilter_test.py
@@ -0,0 +1,64 @@
+# EXAMPLE PYTHON CONFIGURATION FILE FOR COMMISSIONING CONVERTER
+# for global runs (multi-partition, zero suppressed)
+# editable options are marked with a #@@@
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process( "LaserAlignmentEventFilterTest" )
+
+## input files
+process.source = cms.Source( "PoolSource",
+  fileNames = cms.untracked.vstring(
+       '/store/data/Run2010A/TestEnables/RAW/v1/000/140/124/56E00D1B-308F-DF11-BE54-001D09F24691.root'              
+    ),
+
+
+    # LAS Runs, TgBoard Delays 32 - 42
+    #eventsToProcess = cms.untracked.VEventRange(
+    #'119407:672983000-119407:733700000'
+    #)
+
+    # LAS Run, TgBoard Delay 38 + 39
+    #eventsToProcess = cms.untracked.VEventRange(
+    #'119407:708316000-119407:715494000'
+    #)
+
+    #LAS Run, TgBoard Delay 39
+    #eventsToProcess = cms.untracked.VEventRange(
+    #'119407:713637500-119407:715494000'
+    #)
+
+    #LAS Run, TgBoard Delay 39
+    #eventsToProcess = cms.untracked.VEventRange(
+    #'119407:713500000-119407:715500000'
+    #)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32( 10 )
+)
+
+## message logger
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    destinations = cms.untracked.vstring( 'cout', 'cerr' ),
+    #destinations = cms.untracked.vstring( 'cerr' ),
+    debugModules = cms.untracked.vstring( 'LaserAlignmentEventFilter' ),
+    cerr = cms.untracked.PSet(
+    threshold = cms.untracked.string( 'ERROR' )
+    ),
+    cout = cms.untracked.PSet(
+    threshold = cms.untracked.string( 'INFO' )
+    )
+)
+
+
+process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
+#process.GlobalTag.globaltag = 'GR09_31X_V5P::All'
+process.GlobalTag.globaltag = cms.string('GR_R_37X_V6::All')
+
+
+process.load('Alignment.LaserAlignment.LaserAlignmentEventFilter_cfi')
+
+process.p = cms.Path( process.LaserAlignmentEventFilter )
+
