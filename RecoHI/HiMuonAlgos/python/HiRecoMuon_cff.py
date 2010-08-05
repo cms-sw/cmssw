dc6c1ca7dff1e480b88a5ae6107cf23089f2aa09
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-08-05 08:22:40 +0000

    revert calo muon change as it has little effect on timing

--- RecoHI/HiMuonAlgos/python/HiRecoMuon_cff.py
+++ RecoHI/HiMuonAlgos/python/HiRecoMuon_cff.py
@@ -15,6 +15,9 @@ globalMuons.TrackerCollectionLabel = hiTracks
 # replace with heavy ion jet label
 muons.JetExtractorPSet.JetCollectionLabel = cms.InputTag("iterativeConePu5CaloJets")
 
+# turn off calo muons for timing considerations?
+#muons.fillCaloCompatibility = cms.bool(False)
+
 # HI muon sequence (passed to RecoHI.Configuration.Reconstruction_HI_cff)
 muonRecoPbPb = cms.Sequence(muonreco_plus_isolation)
 
