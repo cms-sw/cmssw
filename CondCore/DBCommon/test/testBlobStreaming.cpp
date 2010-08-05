e3e80585e0529a4d65feb971a8a58ffceab7310d
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-08-05 12:44:11 +0000

    first test works

--- CondCore/DBCommon/test/testBlobStreaming.cpp
+++ CondCore/DBCommon/test/testBlobStreaming.cpp
@@ -8,29 +8,42 @@
 
 
 #include <iostream>
-#include <iomanim>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 
 int main() {
 
+  using namespace cond;
+
   BlobStreamingService streamer;
 
   // white box tests...
-  for (size_t i=0 i!=BlobStreamingService::m_idsize; ++i)
+  for (size_t i=0; i!=BlobStreamingService::nVariants; ++i)
     std::cout << i << " " << std::hex << BlobStreamingService::variantIds[i].first << "-" 
-	      << BlobStreamingService::variantIds[i].second << std::endl;
+	      << BlobStreamingService::variantIds[i].second << std::dec << std::endl;
   
   std::vector<unsigned char> crap(1024);
+  for(size_t i=0; i!=crap.size(); ++i)
+    crap[i] = i%256;
   crap[3]=5; crap[10]=123;
 
   BlobStreamingService::Variant id = BlobStreamingService::findVariant(&crap.front());
   std::cout << "shall be zero " << id << std::endl;
 
-  *reinterpret_cast<uuid*>(&crap.front()) = BlobStreamingService::variantIds[BlobStreamingService::COMPRESSED_TBUFFER];
+  *reinterpret_cast<BlobStreamingService::uuid*>(&crap.front()) = BlobStreamingService::variantIds[BlobStreamingService::COMPRESSED_TBUFFER];
   id = BlobStreamingService::findVariant(&crap.front());
   std::cout << "shall be one " << id << std::endl;
 
+  boost::shared_ptr<coral::Blob> blob1 = BlobStreamingService::compress(&crap.front(), crap.size());
+  unsigned long long s = *reinterpret_cast<unsigned long long*>((unsigned char*)(blob1->startingAddress())+ BlobStreamingService::m_idsize);
+  std::cout << "compressed " <<  blob1->size() << " " << s << std::endl;
+
+  boost::shared_ptr<coral::Blob> blob2 = BlobStreamingService::expand(*blob1);
+  std::cout << "expanded (shall be " << crap.size() <<  ") "<<  blob2->size() << std::endl;
+  std::vector<unsigned char> crap2(1024); 
+  ::memcpy(&crap2.front(),blob2->startingAddress(),blob2->size());
+  if (crap!=crap2) std::cout << "compress - expansion failed" << std::endl;
 
   return 0;
 
