5d14b39013f8aba47b80f487e1eb6fa3c12f3f45
Niklas Mohr <sha1-d7144b0376d6eeccdb71f9d0d9703e188f5084e0@cern.ch>
2010-08-06 15:28:38 +0000

    Use VarParsing for parameters

--- PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py
+++ PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py
@@ -11,7 +11,7 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 #-- Meta data to be logged in DBS ---------------------------------------------
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.29 $'),
+    version = cms.untracked.string('$Revision: 1.30 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/PhysicsTools/Configuration/test/SUSY_pattuple_cfg.py,v $'),
     annotation = cms.untracked.string('SUSY pattuple definition')
 )
@@ -24,38 +24,72 @@ process.MessageLogger.cerr.PATSummaryTables = cms.untracked.PSet(
     )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
+#-- VarParsing ----------------------------------------------------------------
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing ('standard')
+
+options.output = "SUSYPAT.root"
+options.maxEvents = 100
+#  for SusyCaf specifics
+options.register('GlobalTag', "", VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "GlobalTag to use")
+options.register('mcInfo', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "process MonteCarlo data")
+options.register('JetCorrections', 'Spring10', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "Jet corrections to use, default 'Spring10'")
+options.register('hltName', 'HLT', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "HLT menu to use for trigger matching")
+options.register('mcVersion', '', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "'35X' for samples from Spring10 production, not needed for new productions")
+options.register('jetTypes', '', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "Jet types that will be produced")
+options.register('hltSelection', '', VarParsing.VarParsing.multiplicity.list, VarParsing.VarParsing.varType.string, "hlTriggers (OR) used to filter events")
+
+#---parse user input
+options.parseArguments()
+options._tagOrder =[]
+
+jetList = []
+if options.jetTypes:
+    for jet in options.jetTypes: jetList.append(jet)
+else:
+    jetList = ['IC5Calo','AK5JPT']
+
 #-- Input Source --------------------------------------------------------------
-process.source.fileNames = [
-     '/store/relval/CMSSW_3_7_0_pre5/RelValProdTTbar/GEN-SIM-RECO/MC_37Y_V4-v1/0023/BA92C6D3-8863-DF11-B3AF-002618943939.root'
-     #'/store/relval/CMSSW_3_6_0_pre6/RelValProdTTbar/GEN-SIM-RECO/MC_36Y_V4-v1/0011/82DAA1BE-B344-DF11-A116-00304867C0C4.root'
-     #'/store/relval/CMSSW_3_7_0_pre5/MinimumBias/RECO/GR_R_37X_V4_RelVal_col_10-v1/0024/CAD8C3ED-8E63-DF11-B60D-00261894393B.root'
-     #'/store/mc/Spring10/MinBias/GEN-SIM-RECO/START3X_V25B_356ReReco-v1/0007/FE90A396-233C-DF11-8106-002618943898.root'
-     #'/store/data/Commissioning10/MinimumBias/RAW-RECO/Apr1Skim_GOODCOLL-v1/0140/E27B88D1-8040-DF11-B3FC-00261894391B.root'
-    ]
-process.maxEvents.input = 100
+if options.files:
+    process.source.fileNames = cms.untracked.vstring (options.files)
+#process.source.fileNames = [
+#     '/store/relval/CMSSW_3_7_0_pre5/RelValProdTTbar/GEN-SIM-RECO/MC_37Y_V4-v1/0023/BA92C6D3-8863-DF11-B3AF-002618943939.root'
+#    ]
+
+process.maxEvents.input = options.maxEvents
 # Due to problem in production of LM samples: same event number appears multiple times
 process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
 #-- Calibration tag -----------------------------------------------------------
-# Should match input file's tag
-process.GlobalTag.globaltag = 'GR_R_37X_V4::All'
+if options.GlobalTag:
+    process.GlobalTag.globaltag = options.Global
 
 ############################# START SUSYPAT specifics ####################################
 from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, getSUSY_pattuple_outputCommands
-#Apply SUSYPAT, parameters are: mcInfo, HLT menu, Jet energy corrections, mcVersion ('35x' for 35x samples, empty string for 36X samples),JetCollections
-addDefaultSUSYPAT(process,True,'HLT','Spring10','',['IC5Calo','AK5JPT']) 
+#Apply SUSYPAT, parameters are: mcInfo, HLT menu, Jet energy corrections, mcVersion ('35x' for 35x samples, empty string for later samples),JetCollections
+addDefaultSUSYPAT(process,options.mcInfo,options.hltName,options.JetCorrections,options.mcVersion,jetList)
 SUSY_pattuple_outputCommands = getSUSY_pattuple_outputCommands( process )
 ############################## END SUSYPAT specifics ####################################
 
+#-- HLT selection ------------------------------------------------------------
+import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
+if options.hltSelection:
+    process.hltFilter = hlt.hltHighLevel.clone(
+        HLTPaths = cms.vstring(options.hltSelection),
+        throw = False
+    )
+    process.susyPatDefaultSequence.replace(process.eventCountProducer, process.eventCountProducer * process.hltFilter)
 
 #-- Output module configuration -----------------------------------------------
-process.out.fileName = 'SUSYPAT.root'       # <-- CHANGE THIS TO SUIT YOUR NEEDS
+process.out.fileName = options.output
 
 # Custom settings
 process.out.splitLevel = cms.untracked.int32(99)  # Turn on split level (smaller files???)
 process.out.overrideInputFileSplitLevels = cms.untracked.bool(True)
 process.out.dropMetaData = cms.untracked.string('DROPPED')   # Get rid of metadata related to dropped collections
 process.out.outputCommands = cms.untracked.vstring('drop *', *SUSY_pattuple_outputCommands )
+if options.hltSelection:
+    process.out.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring("p"))
 
 #-- Execution path ------------------------------------------------------------
 # Full path
