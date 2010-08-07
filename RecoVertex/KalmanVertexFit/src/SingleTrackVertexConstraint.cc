3f7bd15fac1db25663ee2e27dd1881046a02a418
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-08-07 22:38:10 +0000

    Restore temporarily backed out changes

--- RecoVertex/KalmanVertexFit/src/SingleTrackVertexConstraint.cc
+++ RecoVertex/KalmanVertexFit/src/SingleTrackVertexConstraint.cc
@@ -54,7 +54,7 @@ SingleTrackVertexConstraint::BTFtuple SingleTrackVertexConstraint::constrain(
   vertex = vertexUpdator.add(vertex, vertexTrack);
   if (!vertex.isValid()) {
     return BTFtuple(false, TransientTrack(), 0.);
-  } else  if (!insideTrackerBounds(vertex.position())) {
+  } else  if (doTrackerBoundCheck_ && (!insideTrackerBounds(vertex.position()))) {
       LogDebug("RecoVertex/SingleTrackVertexConstraint") 
 	 << "Fitted position is out of tracker bounds.\n";
       return BTFtuple(false, TransientTrack(), 0.);
