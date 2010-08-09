bba49df3c60a61774f84dd960a2574601d226a7f
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2010-08-09 19:05:02 +0000

    initial commit for ConfDB parsing

---
+++ RecoMuon/TrackerSeedGenerator/plugins/L3TrackCombiner_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+L3TrackCombiner = cms.EDProducer(
+    "L3TrackCombiner",
+    labels = cms.VInputTag(
+    cms.InputTag("hltL3MuonsOIState"),
+    cms.InputTag("hltL3MuonsOIHit"),
+    )
+    )
