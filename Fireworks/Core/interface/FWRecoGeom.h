21757ab6e695c30085adea47f359b3b0772ad61d
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-08-11 09:28:58 +0000

    Separate geometry info structure.

---
+++ Fireworks/Core/interface/FWRecoGeom.h
@@ -0,0 +1,32 @@
+#ifndef CORE_FWRECO_GEOM_H
+# define CORE_FWRECO_GEOM_H
+
+# include <map>
+
+class FWRecoGeom
+{
+public:
+  FWRecoGeom( void ) {}
+  
+  virtual ~FWRecoGeom( void ) {}
+
+  struct Info
+  {
+    std::string name;
+    std::vector<float> points; // x1,y1,z1...x8,y8,z8
+    std::vector<float> topology;
+    Info( const std::string& iname )
+      : name( iname ),
+	points( 24, 0 ),
+	topology( 9, 0 )
+      {}
+    Info( void )
+      : points( 24, 0 ),
+	topology( 9, 0 )
+      {}
+  };
+  
+  typedef std::map<unsigned int, FWRecoGeom::Info> InfoMap;
+};
+
+#endif // CORE_FWRECO_GEOM_H
