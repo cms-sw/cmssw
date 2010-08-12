b37e8ef71b9b491e546972a52e9118c49693566b
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2010-08-12 09:24:50 +0000

    distinguish caloJets and pfJets

--- TopQuarkAnalysis/Examples/plugins/TopJetAnalyzer.cc
+++ TopQuarkAnalysis/Examples/plugins/TopJetAnalyzer.cc
@@ -40,41 +40,63 @@ TopJetAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& setup)
   if( jets->size()<1 || !verbose_ )
     return;
 
-  std::cout << "================================================================="
-            << "================================================================="
-            << std::endl;
-  std::cout << std::setw(5 ) << "jet :"
-            << std::setw(13) << "pt :"
-            << std::setw(13) << "eta :"
-            << std::setw(13) << "phi :"
-	    << std::setw(13) << "emf :"
-	    << std::setw(10) << "n90Hits :"
-	    << std::setw(13) << "fHPD :"
-            << std::setw(13) << "TCHE :"
-            << std::setw(13) << "TCHP :"
-            << std::setw(13) << "SSVHE :"
-            << std::setw(11) << "SSVHP" << std::endl;
-  std::cout << "-----------------------------------------------------------------"
-            << "-----------------------------------------------------------------"
-            << std::endl;
+  int lineWidth = 75;
+  if( jets->begin()->isCaloJet() )
+    lineWidth = 100;
+  else if( jets->begin()->isPFJet() )
+    lineWidth = 122;
+
+  std::cout << std::setfill('=') << std::setw(lineWidth) << "\n" << std::setfill(' ');
+  std::cout << std::setw( 5) << "jet :"
+            << std::setw(11) << "pt :"
+            << std::setw( 9) << "eta :"
+            << std::setw( 9) << "phi :"
+	    << std::setw(11) << "TCHE :"
+            << std::setw(11) << "TCHP :"
+            << std::setw( 9) << "SSVHE :"
+            << std::setw( 9) << "SSVHP :";
+  if( jets->begin()->isCaloJet() ) {
+    std::cout << std::setw( 8) << "emf :"
+	      << std::setw(10) << "n90Hits :"
+	      << std::setw( 7) << "fHPD";
+  }
+  if( jets->begin()->isPFJet() ) {
+    std::cout << std::setw(9) << "chf : "
+	      << std::setw(8) << "nhf : "
+	      << std::setw(8) << "cef : "
+	      << std::setw(8) << "nef : "
+	      << std::setw(6) << "nCh : "
+	      << std::setw(6) << "nConst";
+  }
+  std::cout << std::endl
+	    << std::setfill('-') << std::setw(lineWidth) << "\n" << std::setfill(' ');
   unsigned i=0;
   for(std::vector<pat::Jet>::const_iterator jet=jets->begin(); jet!=jets->end(); ++jet){
-    std::cout << std::setw(3 ) << i << " : "
-	      << std::setw(10) << jet->pt() << " : "
-	      << std::setw(10) << jet->eta() << " : "
-	      << std::setw(10) << jet->phi() << " : "
-              << std::setw(10) << jet->emEnergyFraction() << " : "
-              << std::setw(7 ) << jet->jetID().n90Hits << " : "
-              << std::setw(10) << jet->jetID().fHPD << " : "
-	      << std::setw(10) << jet->bDiscriminator("trackCountingHighEffBJetTags") << " : "
-	      << std::setw(10) << jet->bDiscriminator("trackCountingHighPurBJetTags") << " : "
-	      << std::setw(10) << jet->bDiscriminator("simpleSecondaryVertexHighEffBJetTags") << " : "
-	      << std::setw(10) << jet->bDiscriminator("simpleSecondaryVertexHighPurBJetTags") << std::endl;
+    std::cout << std::setw(3) << i << " : " << std::setprecision(3) << std::fixed
+	      << std::setw(8) << jet->pt() << " : "
+	      << std::setw(6) << jet->eta() << " : "
+	      << std::setw(6) << jet->phi() << " : "
+	      << std::setw(8) << jet->bDiscriminator("trackCountingHighEffBJetTags") << " : "
+	      << std::setw(8) << jet->bDiscriminator("trackCountingHighPurBJetTags") << " : "
+	      << std::setw(6) << jet->bDiscriminator("simpleSecondaryVertexHighEffBJetTags") << " : "
+	      << std::setw(6) << jet->bDiscriminator("simpleSecondaryVertexHighPurBJetTags") << " : ";
+    if( jet->isCaloJet() ) {
+      std::cout << std::setw(5) << jet->emEnergyFraction() << " : "
+		<< std::setw(7) << jet->jetID().n90Hits << " : "
+		<< std::setw(6) << jet->jetID().fHPD;
+    }
+    if( jet->isPFJet() ) {
+      std::cout << std::setw(5) << jet->chargedHadronEnergyFraction() << " : "
+		<< std::setw(5) << jet->neutralHadronEnergyFraction() << " : "
+		<< std::setw(5) << jet->chargedEmEnergyFraction() << " : "
+		<< std::setw(5) << jet->neutralEmEnergyFraction() << " : "
+		<< std::setw(3) << jet->chargedMultiplicity() << " : "
+		<< std::setw(6) << jet->nConstituents();
+    }
+    std::cout << std::endl;
     i++;
   }
-  std::cout << "================================================================="
-            << "================================================================="
-            << std::endl;
+  std::cout << std::setfill('=') << std::setw(lineWidth) << "\n" << std::setfill(' ');
 }
 
 void TopJetAnalyzer::beginJob()
