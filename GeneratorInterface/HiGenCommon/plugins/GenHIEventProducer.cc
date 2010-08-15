32bc1af70f83485a910092a7d1cab8d8a1c134e4
Dong Ho Moon <sha1-1ad0717712808f944549adad0408f5ef45185f60@cern.ch>
2010-08-15 19:33:09 +0000

    NchargedPtCut, NchargedPtCutMR, EtMR added

--- GeneratorInterface/HiGenCommon/plugins/GenHIEventProducer.cc
+++ GeneratorInterface/HiGenCommon/plugins/GenHIEventProducer.cc
@@ -5,15 +5,15 @@
 // 
 /**\class GenHIEventProducer GenHIEventProducer.cc yetkin/GenHIEventProducer/src/GenHIEventProducer.cc
 
- Description: <one line class summary>
+Description: <one line class summary>
 
- Implementation:
-     <Notes on implementation>
+Implementation:
+<Notes on implementation>
 */
 //
 // Original Author:  Yetkin Yilmaz
 //         Created:  Thu Aug 13 08:39:51 EDT 2009
-// $Id: GenHIEventProducer.cc,v 1.2 2010/05/04 17:15:10 yilmaz Exp $
+// $Id: GenHIEventProducer.cc,v 1.3 2010/08/12 16:36:07 yilmaz Exp $
 //
 //
 
@@ -46,14 +46,17 @@ using namespace std;
 //
 
 class GenHIEventProducer : public edm::EDProducer {
-   public:
-      explicit GenHIEventProducer(const edm::ParameterSet&);
-      ~GenHIEventProducer();
+    public:
+        explicit GenHIEventProducer(const edm::ParameterSet&);
+        ~GenHIEventProducer();
 
-   private:
-      virtual void produce(edm::Event&, const edm::EventSetup&);
-   std::vector<std::string> hepmcSrc_;
-   edm::ESHandle < ParticleDataTable > pdt;
+    private:
+        virtual void produce(edm::Event&, const edm::EventSetup&);
+        std::vector<std::string> hepmcSrc_;
+        edm::ESHandle < ParticleDataTable > pdt;
+
+        double ptCut_;
+        
 };
 
 //
@@ -70,16 +73,18 @@ class GenHIEventProducer : public edm::EDProducer {
 //
 GenHIEventProducer::GenHIEventProducer(const edm::ParameterSet& iConfig)
 {
-   produces<edm::GenHIEvent>();
-   hepmcSrc_ = iConfig.getParameter<std::vector<std::string> >("generators");
+    produces<edm::GenHIEvent>();
+    hepmcSrc_ = iConfig.getParameter<std::vector<std::string> >("generators");
+    ptCut_ = iConfig.getParameter<double> ("ptCut"); // ptCut added
+    //ptCut_ = iConfig.getParameter<double> >("ptCut"); // ptCut added
 }
 
 
 GenHIEventProducer::~GenHIEventProducer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+
+    // do anything here that needs to be done at desctruction time
+    // (e.g. close files, deallocate resources etc.)
 
 }
 
@@ -89,83 +94,113 @@ GenHIEventProducer::~GenHIEventProducer()
 //
 
 // ------------ method called to produce the data  ------------
-void
+    void
 GenHIEventProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+    using namespace edm;
 
-   if(!(pdt.isValid())) iSetup.getData(pdt);
+    if(!(pdt.isValid())) iSetup.getData(pdt);
 
-   double b = -1;
-   int npart = -1;
-   int ncoll = 0;
-   int nhard = 0;
-   double phi = 0;
+    double b = -1;
+    int npart = -1;
+    int ncoll = 0;
+    int nhard = 0;
+    double phi = 0;
 
-   int nCharged = 0;
-   int nChargedMR = 0;
-   double meanPt = 0;
-   double meanPtMR = 0;
+    int nCharged = 0;
+    int nChargedMR = 0;
+    int nChargedPtCut = 0; // NchargedPtCut bym
+    int nChargedPtCutMR = 0; // NchargedPtCutMR bym
 
-      for(size_t ihep = 0; ihep < hepmcSrc_.size(); ++ihep){
-	 Handle<edm::HepMCProduct> hepmc;
-	 iEvent.getByLabel(hepmcSrc_[ihep],hepmc);
+    double meanPt = 0;
+    double meanPtMR = 0;
+    double EtMR = 0; // Normalized of total energy bym
+    double TotEnergy = 0; // Total energy bym
 
-	 const HepMC::GenEvent* evt = hepmc->GetEvent();
-	 HepMC::GenEvent::particle_const_iterator begin = evt->particles_begin();
-	 HepMC::GenEvent::particle_const_iterator end = evt->particles_end();
-	 for(HepMC::GenEvent::particle_const_iterator it = begin; it != end; ++it){
-	    if((*it)->status() != 1) continue;
-	    int pdg_id = (*it)->pdg_id();
-	    const ParticleData * part = pdt->particle(pdg_id );
-	    int charge = static_cast<int>(part->charge());
+    for(size_t ihep = 0; ihep < hepmcSrc_.size(); ++ihep){
+        Handle<edm::HepMCProduct> hepmc;
+        iEvent.getByLabel(hepmcSrc_[ihep],hepmc);
 
-	    if(charge == 0) continue;
+        const HepMC::GenEvent* evt = hepmc->GetEvent();
+        HepMC::GenEvent::particle_const_iterator begin = evt->particles_begin();
+        HepMC::GenEvent::particle_const_iterator end = evt->particles_end();
+        for(HepMC::GenEvent::particle_const_iterator it = begin; it != end; ++it){
+            if((*it)->status() != 1) continue;
+            int pdg_id = (*it)->pdg_id();
+            const ParticleData * part = pdt->particle(pdg_id );
+            int charge = static_cast<int>(part->charge());
+
+            if(charge == 0) continue;
             float pt = (*it)->momentum().perp();
-	    float eta = (*it)->momentum().eta();
+            float eta = (*it)->momentum().eta();
+            float energy = (*it)->momentum().e(); // energy bym
+            //float energy = (*it)->momentum().energy(); // energy bym
             nCharged++;
-	    meanPt += pt;
-	    if(fabs(eta) > 0.5) continue;
-	    nChargedMR++;
+            meanPt += pt;
+            // Get the total energy bym
+            if(fabs(eta)<1.0){
+                //cout<<"energy : "<<energy<<endl;
+                TotEnergy += energy;
+                //cout<<"TotEnergy 1 : "<<TotEnergy<<endl;
+            }
+            if(pt>ptCut_){
+                nChargedPtCut++;
+                if(fabs(eta)<0.5){
+                    nChargedPtCutMR++;
+                }
+            }
+            // end bym
+
+            if(fabs(eta) > 0.5) continue;
+            nChargedMR++;
             meanPtMR += pt;
-	 }
+        }
 
-	 const HepMC::HeavyIon* hi = evt->heavy_ion();
+        const HepMC::HeavyIon* hi = evt->heavy_ion();
 
-	 if(hi){
-	    ncoll = ncoll + hi->Ncoll();
-	    nhard = nhard + hi->Ncoll_hard();
-	    int np = hi->Npart_proj() + hi->Npart_targ();
-	    if(np >= 0){
-	       npart = np;
-	       b = hi->impact_parameter();
-	       phi = hi->event_plane_angle();
-	    }
-	 }
-      }
+        if(hi){
+            ncoll = ncoll + hi->Ncoll();
+            nhard = nhard + hi->Ncoll_hard();
+            int np = hi->Npart_proj() + hi->Npart_targ();
+            if(np >= 0){
+                npart = np;
+                b = hi->impact_parameter();
+                phi = hi->event_plane_angle();
+            }
+        }
+    }
+    //cout<<"TotEnergy 2 : "<<TotEnergy<<endl;
+    // Get the normalized total energy bym
+    if(TotEnergy != 0){
+        EtMR = TotEnergy/2;
+        //cout<<"EtMR : "<<EtMR<<endl;
+    }
 
-      if(nChargedMR != 0){
-	 meanPtMR /= nChargedMR;
-      }
-      if(nCharged != 0){
-         meanPt /= nCharged;
-      }
+    if(nChargedMR != 0){
+        meanPtMR /= nChargedMR;
+    }
+    if(nCharged != 0){
+        meanPt /= nCharged;
+    }
 
-      std::auto_ptr<edm::GenHIEvent> pGenHI(new edm::GenHIEvent(b,
-								npart,
-								ncoll,
-								nhard,
-								phi, 
-								nCharged,
-								nChargedMR,
-								meanPt,
-								meanPtMR								
-								));
+    std::auto_ptr<edm::GenHIEvent> pGenHI(new edm::GenHIEvent(b,
+                npart,
+                ncoll,
+                nhard,
+                phi, 
+                nCharged,
+                nChargedMR,
+                meanPt,
+                meanPtMR,								
+                EtMR, // bym
+                nChargedPtCut, // bym
+                nChargedPtCutMR  // bym
+                ));
 
 
 
-      
-      iEvent.put(pGenHI);
+
+    iEvent.put(pGenHI);
 
 }
 
