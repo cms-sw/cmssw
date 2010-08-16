2350b2ed366556e593b766cb091f01538af543c5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-08-16 17:13:06 +0000

    faster math

--- RecoTracker/TkMSParametrization/src/LongitudinalBendingCorrection.cc
+++ RecoTracker/TkMSParametrization/src/LongitudinalBendingCorrection.cc
@@ -5,13 +5,22 @@
 #include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 
+namespace {
+  struct FieldAt0 {
+    FieldAt0(const edm::EventSetup& es) {
+      edm::ESHandle<MagneticField> pSetup;
+      es.get<IdealMagneticFieldRecord>().get(pSetup);
+      fieldInInvGev = 1.f/std::abs(pSetup->inTesla(GlobalPoint(0,0,0)).z()  *2.99792458e-3f);
+    }
+    float fieldInInvGev;
+  };
+}
+
 namespace  pixelrecoutilities {
   void LongitudinalBendingCorrection::init(float pt, const edm::EventSetup& es)
   {
-    edm::ESHandle<MagneticField> pSetup;
-    es.get<IdealMagneticFieldRecord>().get(pSetup);
-    float fieldInInvGev = 1.f/std::abs(pSetup->inTesla(GlobalPoint(0,0,0)).z()  *2.99792458e-3f);
-    theInvCurv =  pt*fieldInInvGev;
+    static  FieldAt0 fieldAt0(es);
+    theInvCurv =  pt*fieldAt0.fieldInInvGev;
     coeff = 1.f/(4.f*6.f*theInvCurv*theInvCurv);
   }
   
