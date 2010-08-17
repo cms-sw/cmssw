2db85bda380b57ca97884065b676c3ed25163e37
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-08-17 19:39:39 +0000

    new DB access

--- RecoHI/HiCentralityAlgos/test/testCentralityBinning_cfg.py
+++ RecoHI/HiCentralityAlgos/test/testCentralityBinning_cfg.py
@@ -3,20 +3,15 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TEST")
 
-#process.load("RecoHI.HiCentralityAlgos.HiTrivialCondRetriever_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'MC_38Y_V8::All'
 
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-
-process.CondDBCommon.connect = 'sqlite_file:/net/hisrv0001/home/yutingb/work/Centrality_3_7_0/src/RecoHI/HiCentralityAlgos/data/CentralityTables.db'
-
-process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-                                       process.CondDBCommon,
-                                       DumpStat=cms.untracked.bool(True),
-                                       toGet = cms.VPSet(cms.PSet(
-                                               record = cms.string('HeavyIonRcd'),
-                                               tag = cms.string('HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
-                                                )),
-                                      )
+process.GlobalTag.toGet = cms.VPSet(
+    cms.PSet(record = cms.string("HeavyIonRcd"),
+             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v0_mc"),
+             connect = cms.untracked.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS")
+             )
+    )
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(-1)
@@ -26,11 +21,12 @@ process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring("rfio:/castor/cern.ch/user/y/yilmaz/share/Hydjet_MinBias_Jets_runs1to20.root")
                             )
 
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
 process.analyze = cms.EDAnalyzer("AnalyzerWithCentrality")
 
 process.TFileService = cms.Service('TFileService',
                                    fileName = cms.string("histograms.root")
                                    )
 
-process.p = cms.Path(process.analyze)
+process.p = cms.Path(process.centralityBin*process.analyze)
 
