528e10660ed1d542c037ddce38f3522e2d3af22d
Dong Ho Moon <sha1-1ad0717712808f944549adad0408f5ef45185f60@cern.ch>
2010-08-17 15:24:56 +0000

    DoGetData added

--- GeneratorInterface/HiGenCommon/python/HeavyIon_cff.py
+++ GeneratorInterface/HiGenCommon/python/HeavyIon_cff.py
@@ -4,7 +4,10 @@ from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 heavyIon = cms.EDProducer("GenHIEventProducer",
                             doReco     = cms.bool(True),
                             doMC       = cms.bool(True),
-                            generators = cms.vstring("generator")
+                            generators = cms.vstring("generator"),
+
+                            ptCut      = cms.double(0),
+                            DoGetData  = cms.bool(False)
                           )
 
 
