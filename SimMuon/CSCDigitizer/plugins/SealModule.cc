d6fec76682f3fdc27ab1888fd232b853689e8569
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2010-08-19 20:55:38 +0000

    CSCDigiSuppressor is unneeded, because now EventFilter/CSCRawToDigi uses pretriggers for this

--- SimMuon/CSCDigitizer/plugins/SealModule.cc
+++ SimMuon/CSCDigitizer/plugins/SealModule.cc
@@ -3,10 +3,8 @@
 #include "SimMuon/CSCDigitizer/src/CSCDigiProducer.h"
 #include "SimMuon/CSCDigitizer/src/CSCDigiDump.h"
 #include "SimMuon/CSCDigitizer/src/CSCNeutronWriter.h"
-#include "SimMuon/CSCDigitizer/src/CSCDigiSuppressor.h"
 
 
 DEFINE_FWK_MODULE(CSCDigiProducer);
 DEFINE_FWK_MODULE(CSCDigiDump);
 DEFINE_FWK_MODULE(CSCNeutronWriter);
-DEFINE_FWK_MODULE(CSCDigiSuppressor);
