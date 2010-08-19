d82f8bd876a41552b685a348fe6eff590027330e
dfehling <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2010-08-19 09:19:36 +0000

    RecoLocalTracker/SiPixelRecHits/

--- RecoLocalTracker/SiPixelRecHits/python/PixelCPETemplateReco_cfi.py
+++ RecoLocalTracker/SiPixelRecHits/python/PixelCPETemplateReco_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 templates = cms.ESProducer("PixelCPETemplateRecoESProducer",
     ComponentName = cms.string('PixelCPETemplateReco'),
     #TanLorentzAnglePerTesla = cms.double(0.106),
-    speed = cms.int32(0),
+    speed = cms.int32(-2),
     #PixelErrorParametrization = cms.string('NOTcmsim'),
     Alpha2Order = cms.bool(True),
     UseClusterSplitter = cms.bool(False),
