c3a91027fe9fd427dc8c562b15b1500901b9f3bc
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-08-19 18:03:46 +0000

    fix vector filling

--- RecoPixelVertexing/PixelTrackFitting/src/RZLine.cc
+++ RecoPixelVertexing/PixelTrackFitting/src/RZLine.cc
@@ -2,27 +2,29 @@
 #include "CommonTools/Statistics/interface/LinearFit.h"
 
 using namespace std;
-template <class T> T sqr( T t) {return t*t;}
+template <class T> inline T sqr( T t) {return t*t;}
 
 RZLine::RZLine(const std::vector<float> & aR, 
-          const std::vector<float> & aZ, 
-          const std::vector<float> & aErrZ)
+	       const std::vector<float> & aZ, 
+	       const std::vector<float> & aErrZ)
   : r(aR), z(aZ), errZ(aErrZ)
 {}
 
-RZLine::RZLine(const vector<GlobalPoint> & points, const vector<GlobalError> & errors, 
-         const vector<bool> isBarrel)
+RZLine::RZLine(const vector<GlobalPoint> & points, 
+	       const vector<GlobalError> & errors, 
+	       const vector<bool> isBarrel) : r(points.size()), z(points.size()), errZ(points.size());
 {
   int nPoints = points.size();
   for (int i=0; i<nPoints; i++) {
     const GlobalPoint & p = points[i];
-    r.push_back(p.perp());
-    z.push_back(p.z());
+    r[i] = p.perp();
+    z[i] = p.z();
   }
+
   float simpleCot = ( z.back()-z.front() )/ (r.back() - r.front() );
   for (int i=0; i<nPoints; i++) {
-    if (isBarrel[i])  errZ.push_back( sqrt(errors[i].czz()) );
-    else errZ.push_back( sqrt(errors[i].rerr(points[i]) ) * simpleCot);
+    errZ[i] = (isBarrel[i]) ? std::sqrt(errors[i].czz()) :  
+      std::sqrt(errors[i].rerr(points[i]) ) * simpleCot;
   }
 }
 
@@ -34,7 +36,7 @@ void RZLine::fit(float & cotTheta, float & intercept,
 
 float RZLine::chi2(float cotTheta, float intercept) const
 {
-  float chi2 = 0.;
+  float chi2 = 0.f;
   int r_size = r.size();  
   for (int i=0; i< r_size; i++) chi2 += sqr( ((z[i]-intercept) - cotTheta*r[i]) / errZ[i]);
   return chi2;
