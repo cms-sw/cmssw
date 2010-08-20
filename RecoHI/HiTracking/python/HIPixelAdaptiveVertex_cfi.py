57cffaf3184309de40bb9d4a3bdac7002d65cf6d
Philip Daniel Allfrey <sha1-2c182dadb8985c5592236e90a4bbc08509a3c3fd@cern.ch>
2010-08-20 00:36:19 +0000

    update cfi with new parameters added to PrimaryVertexProducerAlgorithm V01-04-00

--- RecoHI/HiTracking/python/HIPixelAdaptiveVertex_cfi.py
+++ RecoHI/HiTracking/python/HIPixelAdaptiveVertex_cfi.py
@@ -8,12 +8,14 @@ hiPixelAdaptiveVertex = cms.EDProducer("PrimaryVertexProducer",
     algorithm = cms.string('AdaptiveVertexFitter'),
     minNdof  = cms.double(0.0),
     TkFilterParameters = cms.PSet(
+        algorithm = cms.string('filterWithThreshold'),
         maxNormalizedChi2 = cms.double(5.0),
         minSiliconLayersWithHits = cms.int32(0), ## >=0 (was 5 for generalTracks)
         minPixelLayersWithHits = cms.int32(2),   ## >=2 (was 2 for generalTracks)
         maxD0Significance = cms.double(3.0),     ## keep most primary tracks (was 5.0)
         minPt = cms.double(0.0),                 ## better for softish events
-        trackQuality = cms.string("any")        
+        trackQuality = cms.string("any"),
+        numTracksThreshold = cms.int32(2)
     ),
     beamSpotLabel = cms.InputTag("offlineBeamSpot"),
     # label of tracks to be used
