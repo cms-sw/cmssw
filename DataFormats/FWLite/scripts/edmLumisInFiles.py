913a77dd7da569b77ea98d0614df905cbb9cb582
Charles Plager <sha1-ca639ee8adf7ee0915fef7e2e829d2fa8d50ba78@cern.ch>
2010-08-20 18:53:39 +0000

    added --intLumi option to print out recorded and delivered integrated luminosity.  I don't yet know the units and have not yet run on 3.8 data file.

--- DataFormats/FWLite/scripts/edmLumisInFiles.py
+++ DataFormats/FWLite/scripts/edmLumisInFiles.py
@@ -6,25 +6,47 @@ import optparse
 
 if __name__ == '__main__':
     
-    parser = \optparse.OptionParser ("Usage: %prog [--options] edm1.root [edm2.root...]",
+    parser = optparse.OptionParser ("Usage: %prog [--options] edm1.root [edm2.root...]",
                                     description='Runs over input EDM files and prints out a list of contained lumi sections')
+    parser.add_option ('--intLumi', dest='intLumi', action='store_true',
+                       help='print out total recorded and delivered integrated luminosity')
     parser.add_option ('--output', dest='output', type='string',
-                       help='Save output to file OUTPUT')
+                       help='save lumi sections output to file OUTPUT')
     (options, args) = parser.parse_args()
     # put this here after parsing the arguments since ROOT likes to
     # grab command line arguments even when it shouldn't.
-    from DataFormats.FWLite import Lumis
+    from DataFormats.FWLite import Lumis, Handle
     if not args:
         raise RuntimeError, "Must provide at least one input file"
 
+    # do we want to get the luminosity summary?
+    if options.intLumi:
+        handle = Handle ('LumiSummary')
+        label  = ('lumiProducer')
+    else:
+        handle, lable = None, None
+
     runsLumisDict = {}
     lumis = Lumis (args)
+    delivered = recorded = 0
     for lum in lumis:
         runList = runsLumisDict.setdefault (lum.aux().run(), [])
         runList.append( lum.aux().id().luminosityBlock() )
+        # get the summary and keep track of the totals
+        if options.intLumi:
+            lum.getByLabel (label, handle)
+            summary = handle.product()
+            delivered += summary.avgInsDelLumi()
+            recorded  += summary.avgInsRecLumi()
 
+    # print out lumi sections in JSON format
     jsonList = LumiList (runsAndLumis = runsLumisDict)
     if options.output:
         jsonList.writeJSON (options.output)
     else:
         print jsonList
+
+    # print out integrated luminosity numbers if requested
+    if options.intLumi:
+        print "delivered %f (units?) recorded %s (units)" % \
+              (delivered, recorded)
