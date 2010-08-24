c949e1079a4606d0e73a4774d5eba8947237918e
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-08-24 14:35:19 +0000

    fix typo

--- RecoHI/HiEgammaAlgos/python/HiElectronSequence_cff.py
+++ RecoHI/HiEgammaAlgos/python/HiElectronSequence_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # creates the recoGsfTracks_electronGsfTracks__RECO = input GSF tracks
-from "TrackingTools.GsfTracking.GsfElectronTracking_cff" import *
+from TrackingTools.GsfTracking.GsfElectronTracking_cff import *
 ecalDrivenElectronSeeds.SeedConfiguration.initialSeeds = "hiPixelTrackSeeds"
 electronCkfTrackCandidates.src = "hiPixelTrackSeeds"
 
@@ -10,7 +10,7 @@ electronGsfTrackingHi = cms.Sequence(ecalDrivenElectronSeeds *
                                      electronGsfTracks)
 
 # run the supercluster(EE+EB)-GSF track association ==> output: recoGsfElectrons_gsfElectrons__RECO  
-from "RecoEgamma.EgammaElectronProducers.gsfElectronSequence_cff" import *
+from RecoEgamma.EgammaElectronProducers.gsfElectronSequence_cff import *
 gsfElectrons.ctfTracks     = cms.InputTag("hiGlobalPrimTracks")
 gsfElectronCores.ctfTracks = cms.InputTag("hiGlobalPrimTracks")
 
