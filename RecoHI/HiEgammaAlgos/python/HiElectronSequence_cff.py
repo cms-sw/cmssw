4724fad2dce43ba0bf86a068fd26a0cd325f8c3b
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-08-24 14:33:34 +0000

    add electron sequence

---
+++ RecoHI/HiEgammaAlgos/python/HiElectronSequence_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+# creates the recoGsfTracks_electronGsfTracks__RECO = input GSF tracks
+from "TrackingTools.GsfTracking.GsfElectronTracking_cff" import *
+ecalDrivenElectronSeeds.SeedConfiguration.initialSeeds = "hiPixelTrackSeeds"
+electronCkfTrackCandidates.src = "hiPixelTrackSeeds"
+
+electronGsfTrackingHi = cms.Sequence(ecalDrivenElectronSeeds *
+                                     electronCkfTrackCandidates *
+                                     electronGsfTracks)
+
+# run the supercluster(EE+EB)-GSF track association ==> output: recoGsfElectrons_gsfElectrons__RECO  
+from "RecoEgamma.EgammaElectronProducers.gsfElectronSequence_cff" import *
+gsfElectrons.ctfTracks     = cms.InputTag("hiGlobalPrimTracks")
+gsfElectronCores.ctfTracks = cms.InputTag("hiGlobalPrimTracks")
+
+hiElectronSequence = cms.Sequence(electronGsfTrackingHi * gsfElectronSequence)
