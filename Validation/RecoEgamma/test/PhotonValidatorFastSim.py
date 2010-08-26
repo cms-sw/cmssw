8563fe6f81940c89affc222ffedeffe70ee297c9
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2010-08-26 09:39:41 +0000

    This the renamed old PhotonValidatorFastSim_ZEE.py

---
+++ Validation/RecoEgamma/test/PhotonValidatorFastSim.py
@@ -0,0 +1,90 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TestPhotonValidator")
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi")
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+process.load("RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cfi")
+process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load("DQMServices.Components.MEtoEDMConverter_cfi")
+process.load("Validation.RecoEgamma.photonValidationSequence_cff")
+process.load("Validation.RecoEgamma.photonPostprocessing_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MC_38Y_V8::All'
+
+process.DQMStore = cms.Service("DQMStore");
+process.load("DQMServices.Components.DQMStoreStats_cfi")
+from DQMServices.Components.DQMStoreStats_cfi import *
+dqmStoreStats.runOnEndJob = cms.untracked.bool(True)
+
+
+
+process.maxEvents = cms.untracked.PSet(
+#input = cms.untracked.int32(10)
+)
+
+
+
+from Validation.RecoEgamma.photonValidationSequence_cff import *
+from Validation.RecoEgamma.photonPostprocessing_cfi import *
+
+photonValidation.OutputMEsInRootFile = True
+photonValidation.OutputFileName = 'PhotonValidationRelVal390pre2_SingleGammaPt10_FastSim.root'
+#photonValidation.OutputFileName = 'PhotonValidationRelVal390pre2_H130GGgluonfusion_FastSim.root'
+photonValidation.fastSim = True
+
+photonPostprocessing.batch = cms.bool(True)
+photonPostprocessing.InputFileName = photonValidation.OutputFileName
+
+process.source = cms.Source("PoolSource",
+noEventSort = cms.untracked.bool(True),
+duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            
+    fileNames = cms.untracked.vstring(
+'rfio:/castor/cern.ch/user/a/aperrott/Prove/SingleGammaPt10_cfi_py_GEN_FASTSIM_HLT_VALIDATION.root'
+
+#    'rfio:/castor/cern.ch/user/a/aperrott/Prove/H130GGgluonfusion_cfi_py_GEN_FASTSIM_HLT_VALIDATION.root'
+    
+
+
+    )
+     )
+
+
+photonPostprocessing.rBin = 48
+
+## For single gamma pt =10
+photonValidation.eMax  = 100
+photonValidation.etMax = 50
+photonValidation.etScale = 0.20
+photonPostprocessing.eMax  = 100
+photonPostprocessing.etMax = 50
+
+## For single gamma pt = 35
+#photonValidation.eMax  = 300
+#photonValidation.etMax = 50
+#photonValidation.etScale = 0.20
+#photonValidation.dCotCutOn = False
+#photonValidation.dCotCutValue = 0.15
+
+
+## For gam Jet and higgs
+#photonValidation.eMax  = 500
+#photonValidation.etMax = 500
+#photonPostprocessing.eMax  = 500
+#photonPostprocessing.etMax = 500
+
+
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring("keep *_MEtoEDMConverter_*_*"),
+    fileName = cms.untracked.string('pippo.root')
+)
+
+
+
+process.p1 = cms.Path(process.tpSelection*process.photonValidationSequence*process.photonPostprocessing*process.dqmStoreStats)
+process.schedule = cms.Schedule(process.p1)
