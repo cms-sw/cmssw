f8eec4d4e02156297d0ac5a68e952c1a929bf3bf
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-08-26 14:39:41 +0000

    Adding some speed improvements for WPlusJetsEventSelector, making use of selector index caching and adding an option for using shallow clones of jets that pass jet ID

--- PhysicsTools/SelectorUtils/interface/WPlusJetsEventSelector.h
+++ PhysicsTools/SelectorUtils/interface/WPlusJetsEventSelector.h
@@ -54,6 +54,7 @@ class WPlusJetsEventSelector : public EventSelector {
   edm::InputTag               muonTag_;
   edm::InputTag               electronTag_;
   edm::InputTag               jetTag_;
+  edm::InputTag               jetClonesTag_;
   edm::InputTag               metTag_;
   edm::InputTag               trigTag_;
 
@@ -103,7 +104,28 @@ class WPlusJetsEventSelector : public EventSelector {
 
   double metMin_;
 
+  bool useJetClones_;
+
   
+
+
+  index_type   inclusiveIndex_; 
+  index_type   triggerIndex_;   
+  index_type   pvIndex_;        
+  index_type   lep1Index_;      
+  index_type   lep2Index_;      
+  index_type   lep3Index_;      
+  index_type   lep4Index_;      
+  index_type   metIndex_;       
+  index_type   zvetoIndex_;     
+  index_type   conversionIndex_;
+  index_type   cosmicIndex_;    
+  index_type   jet1Index_;      
+  index_type   jet2Index_;      
+  index_type   jet3Index_;      
+  index_type   jet4Index_;      
+  index_type   jet5Index_;      
+
 };
 
 
