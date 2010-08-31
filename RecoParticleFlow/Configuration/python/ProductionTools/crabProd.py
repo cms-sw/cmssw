1ae629dbdd90f6f2ae1b696e58718502d191866b
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2010-08-31 11:05:45 +0000

    added crabProd script

---
+++ RecoParticleFlow/Configuration/python/ProductionTools/crabProd.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# Colin
+# interface to CRAB, a bit like multicrab
+
+import os, sys,  imp, re, pprint, string
+from optparse import OptionParser
+
+import castortools
+
+parser = OptionParser()
+parser.usage = ""
+parser.add_option("-n", "--negate", action="store_true",
+                  dest="negate",
+                  help="do not proceed",
+                  default=False)
+parser.add_option("-c", "--castorBaseDir", 
+                  dest="castorBaseDir",
+                  help="Base castor directory. Subdirectories will be created automatically for each prod",
+                  default="/castor/cern.ch/user/c/cbern/cmst3/Test")
+
+(options,args) = parser.parse_args()
+
+if len(args)!=1:
+    parser.print_help()
+    sys.exit(1)
+
+sampleName = args[0]
+
+print 'starting prod for sample:', sampleName
+
+# preparing castor dir
+
+cdir = options.castorBaseDir 
+cdir += sampleName
+rfmkdir = 'rfmkdir -p ' + cdir
+print rfmkdir
+os.system( rfmkdir )
+rfchmod = 'rfchmod 775 ' + cdir
+print rfchmod
+os.system( rfchmod )
+
+# making local crab directory
+ldir = '.' + sampleName
+
+mkdir = 'mkdir -p ' + ldir
+print mkdir
+os.system( mkdir )
+
+#cpcrab = 'cp crab.cfg %s/crab.cfg' % ldir
+#print cpcrab
+#os.system( cpcrab )
+
+#prepare the crab file
+newCrabPath = '%s/crab.cfg' % ldir
+print newCrabPath
+
+newCrab = open(newCrabPath,'w')
+oldCrab = open('crab.cfg','r')
+newPSet = ""
+
+patternDataSet = re.compile("\w*datasetpath")
+patternRemoteDir = re.compile('\w*user_remote_dir')
+patternPSet = re.compile('pset=(.*py)\w*')
+
+for line in oldCrab.readlines():
+    if patternDataSet.match( line ):
+#        print 'not writing ', line
+        continue
+    if patternRemoteDir.match( line ):
+#        print 'not writing ', line
+        continue
+    match = patternPSet.match( line )
+    if match != None:
+        pset  = match.group(1)
+        newPSet = ldir + "/" + match.group(1)
+        os.system('cp %s %s' % (pset, newPSet) )
+        
+    newCrab.write( line )
+
+newCrab.write('[CMSSW]\n')
+newCrab.write('datasetpath = '+sampleName+'\n')
+
+patStripOffCastor = re.compile('/castor/cern.ch/(user/.*)')
+match = patStripOffCastor.match( cdir )
+newCrab.write('[USER]\n')
+newCrab.write('user_remote_dir = %s\n' % match.group(1))
+
+print ''
+print 'SUMMARY'
+print cdir
+print ldir
+print newCrabPath
+print newPSet
+
+
