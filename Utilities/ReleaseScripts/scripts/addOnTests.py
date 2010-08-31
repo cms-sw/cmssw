c791be63c395a3b9dfb5318e1529195d527d9170
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2010-08-31 16:29:05 +0000

    removed the test which does not exist any more

--- Utilities/ReleaseScripts/scripts/addOnTests.py
+++ Utilities/ReleaseScripts/scripts/addOnTests.py
@@ -148,16 +148,13 @@ class StandardTester(object):
                 }
 
         hltTests = { 'hlt1' : ['cmsDriver.py TTbar_Tauola.cfi -s GEN,SIM,DIGI,L1,DIGI2RAW -n 10 --conditions auto:startup --relval 9000,50 --datatier "GEN-SIM-RAW" --eventcontent RAW --fileout file:RelVal_DigiL1Raw_8E29.root',
-                      'cmsRun ../HLTrigger/Configuration/test/OnLine_HLT_8E29.py',
                       'ln -s RelVal_DigiL1Raw_8E29.root RelVal_DigiL1Raw_GRun.root',
                       'cmsRun ../HLTrigger/Configuration/test/OnLine_HLT_GRun.py' ], 
 
                      'hlt2' : ['cmsDriver.py TTbar_Tauola.cfi -s GEN,SIM,DIGI,L1,DIGI2RAW -n 10 --conditions auto:mc --relval 9000,50 --datatier "GEN-SIM-RAW" --eventcontent RAW --fileout file:RelVal_DigiL1Raw_1E31.root',
-                      'cmsRun ../HLTrigger/Configuration/test/OnLine_HLT_1E31.py',
                       'ln -s RelVal_DigiL1Raw_1E31.root RelVal_DigiL1Raw_HIon.root',
                       'cmsRun ../HLTrigger/Configuration/test/OnLine_HLT_HIon.py'],
-                     'hlt3' : ['cmsRun ../HLTrigger/Configuration/test/OnData_HLT_8E29.py'],
-                     'hlt4' : ['cmsRun ../HLTrigger/Configuration/test/OnData_HLT_GRun.py']
+                     'hlt3' : ['cmsRun ../HLTrigger/Configuration/test/OnData_HLT_GRun.py']
                      }
 
     	commands={}
