88dd07e5a044fb156f7bd35ac80ed0872d6c1930
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2010-08-31 15:42:27 +0000

    and declare plugin

--- EventFilter/RawDataCollector/src/SealModule.cc
+++ EventFilter/RawDataCollector/src/SealModule.cc
@@ -2,8 +2,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "EventFilter/RawDataCollector/interface/RawDataCollectorModule.h"
+#include "EventFilter/RawDataCollector/src/RawDataCollectorByLabel.h"
 
 using namespace edm::serviceregistry;
 
-
 DEFINE_FWK_MODULE(RawDataCollectorModule);
+DEFINE_FWK_MODULE(RawDataCollectorByLabel);
+
