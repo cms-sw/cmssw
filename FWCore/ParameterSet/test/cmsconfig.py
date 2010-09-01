13342987a1b4e2820a9a38f9e429723b1ea55cef
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2010-09-01 16:20:13 +0000

    now also properly handles VESInputTag type

--- FWCore/ParameterSet/test/cmsconfig.py
+++ FWCore/ParameterSet/test/cmsconfig.py
@@ -1,6 +1,6 @@
 #------------------------------------------------------------
 #
-# $Id: cmsconfig.py,v 1.15 2006/08/26 00:54:35 rpw Exp $
+# $Id: cmsconfig.py,v 1.16 2006/08/31 16:47:14 lsexton Exp $
 #
 # cmsconfig: a class to provide convenient access to the Python form
 # of a parsed CMS configuration file.
@@ -77,7 +77,7 @@ class printable_parameter:
             self.trackedCode = "untracked " # trailing space is needed
 
         # We need special handling of some of the parameter types.
-        if self.type in ["vbool", "vint32", "vuint32", "vdouble", "vstring", "VInputTag"]:
+        if self.type in ["vbool", "vint32", "vuint32", "vdouble", "vstring", "VInputTag", "VESInputTag"]:   
             # TODO: Consider using cStringIO, if this is observed
             # to be a bottleneck. This may happen if many large
             # vectors are used in parameter sets.
