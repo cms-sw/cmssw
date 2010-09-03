d4f25f5f5307ed4ace3b73514bff004bcdcc869d
Eike Schlieckau <sha1-fc2fe05d7bf2eff84264ececb7f974ab67c284cc@cern.ch>
2010-09-03 16:49:11 +0000

    changed class of enum *Param* to parent class, added missing includes

--- TopQuarkAnalysis/TopKinFitter/interface/CovarianceMatrix.h
+++ TopQuarkAnalysis/TopKinFitter/interface/CovarianceMatrix.h
@@ -2,6 +2,11 @@
 #define CovarianceMatrix_h
 
 #include "DataFormats/PatCandidates/interface/PATObject.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+
 #include "TopQuarkAnalysis/TopObjectResolutions/interface/MET.h"
 #include "TopQuarkAnalysis/TopObjectResolutions/interface/Jet.h"
 #include "TopQuarkAnalysis/TopObjectResolutions/interface/Muon.h"
@@ -30,20 +35,20 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
   if(object.hasKinResolution())
     {
       switch(param){
-      case TtSemiLepKinFitter::kEtEtaPhi :
+      case TopKinFitter::kEtEtaPhi :
 	CovM3(0,0) = pow(object.resolEt(resolutionProvider) , 2);
-	if( dynamic_cast<const reco::MET*>(&object) )CovM3(1,1) = pow(9999., 2);
+	if( dynamic_cast<const reco::MET*>(&object) ) CovM3(1,1) = pow(9999., 2);
 	else CovM3(1,1) = pow(object.resolEta(resolutionProvider), 2);
 	CovM3(2,2) = pow(object.resolPhi(resolutionProvider), 2);
 	CovM = &CovM3;
 	break;
-      case TtSemiLepKinFitter::kEtThetaPhi :
+      case TopKinFitter::kEtThetaPhi :
 	CovM3(0,0) = pow(object.resolEt(resolutionProvider)   , 2);
 	CovM3(1,1) = pow(object.resolTheta(resolutionProvider), 2);
 	CovM3(2,2) = pow(object.resolPhi(resolutionProvider)  , 2);
 	CovM = &CovM3;
 	break;
-      case TtSemiLepKinFitter::kEMom :
+      case TopKinFitter::kEMom :
 	CovM4(0,0) = pow(1, 2);
 	CovM4(1,1) = pow(1, 2);
 	CovM4(2,2) = pow(1, 2);
@@ -60,7 +65,7 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
       if( dynamic_cast<const reco::Jet*>(&object) ) {
 	res::HelperJet jetRes;
 	switch(param){
-	case TtSemiLepKinFitter::kEMom :
+	case TopKinFitter::kEMom :
 	  if(resolutionProvider == "bjet") {
 	    CovM4(0,0) = pow(jetRes.a (pt, eta, res::HelperJet::kB  ), 2); 
 	    CovM4(1,1) = pow(jetRes.b (pt, eta, res::HelperJet::kB  ), 2); 
@@ -75,7 +80,7 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
 	  }
 	  CovM = &CovM4;
 	  break;
-	case TtSemiLepKinFitter::kEtEtaPhi : 
+	case TopKinFitter::kEtEtaPhi : 
 	  if(resolutionProvider == "bjet") {
 	    CovM3(0,0) = pow(jetRes.et (pt, eta, res::HelperJet::kB  ), 2); 
 	    CovM3(1,1) = pow(jetRes.eta(pt, eta, res::HelperJet::kB  ), 2); 
@@ -88,7 +93,7 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
 	  }
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtThetaPhi :
+	case TopKinFitter::kEtThetaPhi :
 	  if(resolutionProvider == "bjet") {
 	    CovM3(0,0) = pow(jetRes.et   (pt, eta, res::HelperJet::kB  ), 2); 
 	    CovM3(1,1) = pow(jetRes.theta(pt, eta, res::HelperJet::kB  ), 2); 
@@ -107,19 +112,19 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
       else if( dynamic_cast<const reco::GsfElectron*>(&object) ) {
 	res::HelperElectron elecRes;
 	switch(param){
-	case TtSemiLepKinFitter::kEMom :
+	case TopKinFitter::kEMom :
 	  CovM3(0,0) = pow(elecRes.a (pt, eta), 2);
 	  CovM3(1,1) = pow(elecRes.b (pt, eta), 2); 
 	  CovM3(2,2) = pow(elecRes.c (pt, eta), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtEtaPhi :
+	case TopKinFitter::kEtEtaPhi :
 	  CovM3(0,0) = pow(elecRes.et (pt, eta), 2);
 	  CovM3(1,1) = pow(elecRes.eta(pt, eta), 2); 
 	  CovM3(2,2) = pow(elecRes.phi(pt, eta), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtThetaPhi :
+	case TopKinFitter::kEtThetaPhi :
 	  CovM3(0,0) = pow(elecRes.et   (pt, eta), 2);
 	  CovM3(1,1) = pow(elecRes.theta(pt, eta), 2); 
 	  CovM3(2,2) = pow(elecRes.phi  (pt, eta), 2);
@@ -131,19 +136,19 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
       else if( dynamic_cast<const reco::Muon*>(&object) ) {
 	res::HelperMuon muonRes;
 	switch(param){
-	case TtSemiLepKinFitter::kEMom :
+	case TopKinFitter::kEMom :
 	  CovM3(0,0) = pow(muonRes.a (pt, eta), 2);
 	  CovM3(1,1) = pow(muonRes.b (pt, eta), 2); 
 	  CovM3(2,2) = pow(muonRes.c (pt, eta), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtEtaPhi :
+	case TopKinFitter::kEtEtaPhi :
 	  CovM3(0,0) = pow(muonRes.et (pt, eta), 2);
 	  CovM3(1,1) = pow(muonRes.eta(pt, eta), 2); 
 	  CovM3(2,2) = pow(muonRes.phi(pt, eta), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtThetaPhi :
+	case TopKinFitter::kEtThetaPhi :
 	  CovM3(0,0) = pow(muonRes.et   (pt, eta), 2);
 	  CovM3(1,1) = pow(muonRes.theta(pt, eta), 2); 
 	  CovM3(2,2) = pow(muonRes.phi  (pt, eta), 2);
@@ -155,19 +160,19 @@ TMatrixD CovarianceMatrix::setupMatrix(const pat::PATObject<ObjectType>& object,
       else if( dynamic_cast<const reco::MET*>(&object) ) {
 	res::HelperMET metRes;
 	switch(param){
-	case TtSemiLepKinFitter::kEMom :
+	case TopKinFitter::kEMom :
 	  CovM3(0,0) = pow(metRes.a(pt), 2);
 	  CovM3(1,1) = pow(metRes.b(pt), 2);
 	  CovM3(2,2) = pow(metRes.c(pt), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtEtaPhi :
+	case TopKinFitter::kEtEtaPhi :
 	  CovM3(0,0) = pow(metRes.et(pt), 2);
 	  CovM3(1,1) = pow(          9999., 2);
 	  CovM3(2,2) = pow(metRes.phi(pt), 2);
 	  CovM = &CovM3;
 	  break;
-	case TtSemiLepKinFitter::kEtThetaPhi :
+	case TopKinFitter::kEtThetaPhi :
 	  CovM3(0,0) = pow(metRes.et(pt), 2);
 	  CovM3(1,1) = pow(          9999., 2);
 	  CovM3(2,2) = pow(metRes.phi(pt), 2);
