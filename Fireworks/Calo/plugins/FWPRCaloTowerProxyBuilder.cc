2efd762db5d3f4d6283ebde723eb90720402a5d7
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-09-08 15:42:57 +0000

    Show CaloTowers in ISpy view.

---
+++ Fireworks/Calo/plugins/FWPRCaloTowerProxyBuilder.cc
@@ -0,0 +1,33 @@
+#include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
+#include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Core/interface/FWGeometry.h"
+#include "Fireworks/Calo/interface/CaloUtils.h"
+#include "DataFormats/CaloTowers/interface/CaloTower.h"
+
+class FWPRCaloTowerProxyBuilder : public FWSimpleProxyBuilderTemplate<CaloTower>
+{
+public:
+   FWPRCaloTowerProxyBuilder( void ) {}  
+   virtual ~FWPRCaloTowerProxyBuilder( void ) {}
+
+   REGISTER_PROXYBUILDER_METHODS();
+
+private:
+   FWPRCaloTowerProxyBuilder( const FWPRCaloTowerProxyBuilder& ); 			// stop default
+   const FWPRCaloTowerProxyBuilder& operator=( const FWPRCaloTowerProxyBuilder& ); 	// stop default
+
+   void build( const CaloTower& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* );
+};
+
+void
+FWPRCaloTowerProxyBuilder::build( const CaloTower& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* ) 
+{
+  const float* corners = item()->getGeom()->getCorners( iData.id().rawId() );
+  if( corners == 0 ) {
+    return;
+  }
+  
+  fireworks::drawEnergyTower3D( corners, iData.et(), &oItemHolder, this, false );
+}
+
+REGISTER_FWPROXYBUILDER( FWPRCaloTowerProxyBuilder, CaloTower, "CaloTower", FWViewType::kISpyBit );
