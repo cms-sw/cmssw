486ea49d65e49bab30b5280cc951ed4f81f2272f
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-09-10 05:58:05 +0000

    revert previous fix, and apply some cosmetic changes

--- DQMServices/Components/src/DQMFileSaver.cc
+++ DQMServices/Components/src/DQMFileSaver.cc
@@ -78,8 +78,6 @@ DQMFileSaver::saveForOffline(const std::string &workflow, int run, int lumi)
 	     (DQMStore::SaveReferenceTag) saveReference_,
 	     saveReferenceQMin_,
 	     fileUpdate_);
-    // from now on update newly created file
-    if (fileUpdate_=="RECREATE") fileUpdate_="UPDATE";
   }
   else // save EventInfo folders for luminosity sections
   {
@@ -466,7 +464,7 @@ DQMFileSaver::endRun(const edm::Run &, const edm::EventSetup &)
       saveForOnline(suffix, rewrite);
     }
     else if (convention_ == Offline)
-      saveForOffline(workflow_, irun_,0);
+      saveForOffline(workflow_, irun_, 0);
     else
       throw cms::Exception("DQMFileSaver")
 	<< "Internal error.  Can only save files in endRun()"
@@ -482,7 +480,7 @@ DQMFileSaver::endJob(void)
   if (saveAtJobEnd_)
   {
     if (convention_ == Offline && forceRunNumber_ > 0)
-      saveForOffline(workflow_, forceRunNumber_);
+      saveForOffline(workflow_, forceRunNumber_, 0);
     else
       throw cms::Exception("DQMFileSaver")
 	<< "Internal error.  Can only save files at the end of the"
