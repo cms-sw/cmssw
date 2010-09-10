740c506e5aa49de929474f6c7c867b9dde6d2666
Charles Plager <sha1-ca639ee8adf7ee0915fef7e2e829d2fa8d50ba78@cern.ch>
2010-09-10 19:14:13 +0000

    added maxSize command line parameter

--- PhysicsTools/Utilities/configuration/copyPickMerge_cfg.py
+++ PhysicsTools/Utilities/configuration/copyPickMerge_cfg.py
@@ -9,6 +9,11 @@ options.register ('eventsToProcess',
 				  VarParsing.multiplicity.list,
 				  VarParsing.varType.string,
 				  "Events to process")
+options.register ('maxSize',
+				  0,
+				  VarParsing.multiplicity.singleton,
+				  VarParsing.varType.int,
+				  "Maximum (suggested) file size (in Kb)")
 options.parseArguments()
 
 process = cms.Process("PickEvent")
@@ -26,9 +31,11 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 
-
 process.Out = cms.OutputModule("PoolOutputModule",
         fileName = cms.untracked.string (options.outputFile)
 )
+
+if options.maxSize:
+    process.Out.maxSize = cms.untracked.int32 (options.maxSize)
 
 process.end = cms.EndPath(process.Out)
