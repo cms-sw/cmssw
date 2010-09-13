4f663084a27972e73c7f240af6096387883bd970
Philippe Gras <sha1-94d441c233bc366b3c83a8acb025a2fe03fe3e01@cern.ch>
2010-09-13 20:03:50 +0000

    read config from global tag

--- SimCalorimetry/EcalSelectiveReadoutProducers/test/srCondRead_cfg.py
+++ SimCalorimetry/EcalSelectiveReadoutProducers/test/srCondRead_cfg.py
@@ -1,34 +1,38 @@
 import FWCore.ParameterSet.Config as cms
 
-#tag = 'EcalSRSettings_v00_lowlumi_mc'
-tag = 'EcalSRSettings_v00_beam10_mc'
+process = cms.Process("P")
 
-process = cms.Process("ProcessOne")
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.CondDBCommon.connect = 'sqlite_file:' + tag + '.db'
-process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
+#Conditions:
+#process.load("CalibCalorimetry.Configuration.Ecal_FakeConditions_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'MC_38Y_V10::All'
 
-process.MessageLogger = cms.Service("MessageLogger",
-                                      debugModules = cms.untracked.vstring('*'),
-                                      destinations = cms.untracked.vstring('cout')
-                                    )
-
+process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
-process.source = cms.Source("EmptySource")
 
-process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-                                      process.CondDBCommon,
-                                      toGet = cms.VPSet(cms.PSet(
-    record = cms.string('EcalSRSettingsRcd'),
-    tag = cms.string(tag)
-    )))
-
-
-process.readFromDB = cms.EDAnalyzer("EcalSRCondTools",
-    mode = cms.string("read")
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring('cout')
 )
 
-process.p = cms.Path(process.readFromDB)
+process.ecalSRCondTools = cms.EDAnalyzer("EcalSRCondTools",
+                                         mode = cms.string("read")
+)
 
+
+## ----------------------------------------------------------------------
+## To read SR configuration from an sqlite file:
+##
+## #tag = 'EcalSRSettings_v00_lowlumi_mc'
+## tag = 'EcalSRSettings_v00_beam10_mc'
+## 
+## process.GlobalTag.toGet = cms.VPSet(
+##       cms.PSet(record = cms.string("EcalSRSettingsRcd"),
+##                tag = cms.string(tag),
+##                connect = cms.untracked.string('sqlite_file:' + tag + '.db')
+##       )
+## )
+## ----------------------------------------------------------------------
+
+process.p1 = cms.Path(process.ecalSRCondTools)
