b2b9aa12e5d37c9e42a8bab87cbc2f342e1f5ef9
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-09-14 21:12:59 +0000

    Update GTs for 38X and 39X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
-autoCond = { 'mc'      : 'MC_38Y_V9::All',
-             'startup' : 'START38_V9::All',
+autoCond = { 'mc'      : 'MC_38Y_V11::All',
+             'startup' : 'START38_V11::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_38R_V9::All',
-             'com10'   : 'GR_R_38X_V9::All',
+             'com10'   : 'GR_R_38X_V13::All',
              'hltonline' : 'GR10_H_V8::All' # same as com10 until H tag available
              }
