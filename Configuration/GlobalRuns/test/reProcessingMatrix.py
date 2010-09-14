57496f9da9d1f3e18a3b4881cfc363f4dab34564
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-09-14 13:58:37 +0000

    add CS skims and more PD eras

--- Configuration/GlobalRuns/test/reProcessingMatrix.py
+++ Configuration/GlobalRuns/test/reProcessingMatrix.py
@@ -7,7 +7,28 @@ parser.add_option("--era")
 parser.add_option("--release")
 parser.add_option("--options",default="")
 parser.add_option("--output",default="RECO,DQM")
+parser.add_option("--fullPDs",default=False,action="store_true")
 (options,args)=parser.parse_args()
+
+
+def Era_12PDs():
+    alcaAndSkimMap={}
+    if options.fullPDs:
+        alcaAndSkimMap=Era_10PDs()
+        alcaAndSkimMap.pop('JetMET')
+    alcaAndSkimMap['Jet']=('HcalCalDijets','LogError','DiJet')
+    alcaAndSkimMap['METFwd']=('','LogError')
+    return alcaAndSkimMap
+    
+def Era_10PDs():
+    alcaAndSkimMap={}
+    if options.fullPDs:
+        alcaAndSkimMap=Era_8PDs()
+        alcaAndSkimMap.pop('JetMETTau')
+    alcaAndSkimMap['JetMET']=('HcalCalDijets','LogError','DiJet')
+    alcaAndSkimMap['BTau']=('HcalCalDijets','LogError','Tau')
+    alcaAndSkimMap['MuOnia']=('TkAlJpsiMuMu+TkAlUpsilonMuMu','')
+    return alcaAndSkimMap
 
 def Era_8PDs():
     alcaAndSkimMap={}
@@ -16,8 +37,8 @@ def Era_8PDs():
     alcaAndSkimMap['Commissioning']=('','DT+L1MuBit+RPC+CSCHLT+CSCAlone+MuonTrack+LogError')
     alcaAndSkimMap['MinimumBias']=('SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias+HcalCalIsoTrk','CSC+MuonTrack+HSCP+BeamBkg+ValSkim+TPG+LogError')
     alcaAndSkimMap['EG']=('EcalCalElectron','LogError')
-    alcaAndSkimMap['Mu']=('MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib','LogError')
-    alcaAndSkimMap['JetMETTau']=('HcalCalDijets','LogError')
+    alcaAndSkimMap['Mu']=('MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu','LogError')
+    alcaAndSkimMap['JetMETTau']=('HcalCalDijets','LogError','DiJet','Tau')
     #alcaAndSkimMap['AlCaP0']=('EcalCalPi0Calib+EcalCalEtaCalib','')
     #alcaAndSkimMap['AlCaPhiSymEcal']=('EcalCalPhiSym+DQM','')
     #alcaAndSkimMap['HcalNZS']=('HcalCalMinBias','')
@@ -51,9 +72,12 @@ skim='cmsDriver.py skim -s SKIM:%s --data --magField AutoFromDBCurrent --scenari
 import os
 
 alcaAndSkimMap=globals()["Era_%ss"%(options.era)]()
+print "list of PD to create reprocessing and skim configuration for:", alcaAndSkimMap.keys()
 for PD in alcaAndSkimMap.keys():
     alcaArg=alcaAndSkimMap[PD][0]
     skimArg=alcaAndSkimMap[PD][1]
+    print "PD:",PD,"alca=",alcaArg,"skim=",skimArg
+    
     c=com
     s=skim
     spec=options.era+'_'
