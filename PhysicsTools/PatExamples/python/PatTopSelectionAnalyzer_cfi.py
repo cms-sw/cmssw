00aa93d9fe0b4d8f7c4130c562bad26a314d755b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-09-14 13:32:29 +0000

    fixed typo

--- PhysicsTools/PatExamples/python/PatTopSelectionAnalyzer_cfi.py
+++ PhysicsTools/PatExamples/python/PatTopSelectionAnalyzer_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-analyzePatTopselection = cms.EDAnalyzer("PatTopSelectionAnalyzer",
+analyzePatTopSelection = cms.EDAnalyzer("PatTopSelectionAnalyzer",
     elecs = cms.untracked.InputTag("selectedPatElectrons"),
     muons = cms.untracked.InputTag("selectedPatMuons"),                                             
     jets  = cms.untracked.InputTag("selectedPatJets"),
