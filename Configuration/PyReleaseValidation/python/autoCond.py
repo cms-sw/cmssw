594fd898b333b0554f1354093a28fd14e6ffa2e5
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-09-15 16:16:03 +0000

    update HLT GlobalTag to GR10_H_V9::All

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -5,5 +5,5 @@ autoCond = { 'mc'      : 'MC_38Y_V11::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_38R_V9::All',
              'com10'   : 'GR_R_38X_V13::All',
-             'hltonline' : 'GR10_H_V8::All' # same as com10 until H tag available
+             'hltonline' : 'GR10_H_V9::All' # same as com10 until H tag available
              }
