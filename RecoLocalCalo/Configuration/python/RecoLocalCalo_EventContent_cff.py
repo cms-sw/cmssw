d7f588eec0706b29ac7c59a53bc933e94c969160
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-09-17 15:49:14 +0000

    modification to castor jet id and local reco event content configuration

--- RecoLocalCalo/Configuration/python/RecoLocalCalo_EventContent_cff.py
+++ RecoLocalCalo/Configuration/python/RecoLocalCalo_EventContent_cff.py
@@ -7,44 +7,35 @@ from RecoLocalCalo.Configuration.ecalLocalReco_EventContent_cff import *
 #FEVT
 RecoLocalCaloFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_hbhereco_*_*', 
-        'keep *_hbheprereco_*_*',
-        'keep *_hfreco_*_*', 
-        'keep *_horeco_*_*',
-        'keep HBHERecHitsSorted_hbherecoMB_*_*',
-        'keep HBHERecHitsSorted_hbheprerecoMB_*_*',
-        'keep HORecHitsSorted_horecoMB_*_*',
-        'keep HFRecHitsSorted_hfrecoMB_*_*',
-        'keep ZDCDataFramesSorted_*Digis_*_*',
-        'keep ZDCRecHitsSorted_*_*_*',
+                                           'keep *_hbheprereco_*_*',
+                                           'keep *_hfreco_*_*', 
+                                           'keep *_horeco_*_*',
+                                           'keep HBHERecHitsSorted_hbherecoMB_*_*',
+                                           'keep HBHERecHitsSorted_hbheprerecoMB_*_*',
+                                           'keep HORecHitsSorted_horecoMB_*_*',
+                                           'keep HFRecHitsSorted_hfrecoMB_*_*',
+                                           'keep ZDCDataFramesSorted_*Digis_*_*',
+                                           'keep ZDCRecHitsSorted_*_*_*',
                                            'keep *_castorreco_*_*',
-                                           'keep *_CastorTowerReco_*_*',
-                                           'keep *_CastorClusterRecoAntiKt07_*_*',
-                                           'keep *_CastorJetEgammaRecoAntiKt07_*_*'                                           
         )
 )
 #RECO content
 RecoLocalCaloRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_hbhereco_*_*', 
-        'keep *_hfreco_*_*', 
-        'keep *_horeco_*_*',
-        'keep HBHERecHitsSorted_hbherecoMB_*_*',
-        'keep HORecHitsSorted_horecoMB_*_*',
-        'keep HFRecHitsSorted_hfrecoMB_*_*',
-        'keep ZDCDataFramesSorted_*Digis_*_*',
-        'keep ZDCRecHitsSorted_*_*_*',
+                                           'keep *_hfreco_*_*', 
+                                           'keep *_horeco_*_*',
+                                           'keep HBHERecHitsSorted_hbherecoMB_*_*',
+                                           'keep HORecHitsSorted_horecoMB_*_*',
+                                           'keep HFRecHitsSorted_hfrecoMB_*_*',
+                                           'keep ZDCDataFramesSorted_*Digis_*_*',
+                                           'keep ZDCRecHitsSorted_*_*_*',
                                            'keep *_castorreco_*_*',
-                                           'keep *_CastorTowerReco_*_*',
-                                           'keep *_CastorClusterRecoAntiKt07_*_*',
-                                           'keep *_CastorJetEgammaRecoAntiKt07_*_*'                                           
         )
 )
 #AOD content
 RecoLocalCaloAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
     'keep *_castorreco_*_*',
-    'keep *_CastorTowerReco_*_*',
-    'keep *_CastorClusterRecoAntiKt07_*_*',
-    'keep *_CastorJetEgammaRecoAntiKt07_*_*'                                           
     )
 )
 RecoLocalCaloFEVT.outputCommands.extend(ecalLocalRecoFEVT.outputCommands)
