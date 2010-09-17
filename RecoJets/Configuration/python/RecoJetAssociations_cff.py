4e46d89eaf2d5a68e80b1c7ae570780a8ce40ee5
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-09-17 16:47:55 +0000

    track extrapolator moved to tracking sequence

--- RecoJets/Configuration/python/RecoJetAssociations_cff.py
+++ RecoJets/Configuration/python/RecoJetAssociations_cff.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoJetAssociations_cff.py,v 1.4 2009/09/02 03:07:18 srappocc Exp $
+# $Id: RecoJetAssociations_cff.py,v 1.5 2010/03/16 21:38:47 srappocc Exp $
 #
 # Associate reconstructed jets with other objects
 # keep IC5 association for backward compatibility for external use
 from RecoJets.JetAssociationProducers.ic5JetTracksAssociatorAtVertex_cfi import *
-from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
+#from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
 # standard associations
 from RecoJets.JetAssociationProducers.iterativeCone5JTA_cff import *
 from RecoJets.JetAssociationProducers.sisCone5JTA_cff import *
 from RecoJets.JetAssociationProducers.kt4JTA_cff import *
 from RecoJets.JetAssociationProducers.ak5JTA_cff import *
 from RecoJets.JetAssociationProducers.ak7JTA_cff import *
-recoJetAssociations = cms.Sequence(trackExtrapolator+ic5JetTracksAssociatorAtVertex+iterativeCone5JTA+kt4JTA+ak5JTA+ak7JTA)
+recoJetAssociations = cms.Sequence(ic5JetTracksAssociatorAtVertex+iterativeCone5JTA+kt4JTA+ak5JTA+ak7JTA)
 
