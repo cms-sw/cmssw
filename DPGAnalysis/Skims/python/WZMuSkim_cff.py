d3c1420d68d93975b964f7d3564c3503bdd46fb4
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-09-20 16:19:02 +0000

    Remove dependencies on EW software (cfg from Juan Alcaraz)

--- DPGAnalysis/Skims/python/WZMuSkim_cff.py
+++ DPGAnalysis/Skims/python/WZMuSkim_cff.py
@@ -1,72 +1,89 @@
 import FWCore.ParameterSet.Config as cms
 
-
-# HLT filter
+### HLT filter
 import copy
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 WZMuHLTFilter = copy.deepcopy(hltHighLevel)
-WZMuHLTFilter.HLTPaths = ["HLT_Mu9","HLT_Mu11"]
-WZMuHLTFilter.throw = False # don't throw on unknown path names
+WZMuHLTFilter.throw = cms.bool(False)
+WZMuHLTFilter.HLTPaths = ["HLT_Mu9","HLT_Mu11","HLT_Mu15"]
 
+### Z -> MuMu candidates
 
-# Muon candidates filters 
-qualityMuonFilter = cms.EDFilter("MuonSelector",
-                                 src = cms.InputTag("muons"),
-                                 cut = cms.string('pt > 20 && abs(eta)<2.4 && isGlobalMuon = 1 && isTrackerMuon = 1 && isolationR03().sumPt<3.0'),
-                                 filter = cms.bool(True)                                
-                                 )
-
-# dxy filter on good muons
-dxyMuonFilter = cms.EDFilter("MuonSelector",
-                             src = cms.InputTag("goodMuons"),
-                             cut = cms.string('abs(innerTrack().dxy)<1.0'),
+# Get muons of needed quality for Zs
+goodMuonsForZ = cms.EDFilter("MuonSelector",
+                             src = cms.InputTag("muons"),
+                             cut = cms.string('pt > 20 && abs(eta)<2.4 && isGlobalMuon = 1 && isTrackerMuon = 1 && isolationR03().sumPt<3.0 && abs(innerTrack().dxy)<1.0'),
                              filter = cms.bool(True)                                
                              )
 
-# Z->mumu candidates
-diMuonProducer = cms.EDProducer("CandViewShallowCloneCombiner",
-                                checkCharge = cms.bool(True),
-                                cut = cms.string('mass > 60'),
-                                decay = cms.string("dxyMuonFilter@+ dxyMuonFilter@-")
-                                )
+# build Z-> MuMu candidates
+dimuons = cms.EDProducer("CandViewShallowCloneCombiner",
+                         checkCharge = cms.bool(True),
+                         cut = cms.string('mass > 60'),
+                         decay = cms.string("goodMuonsForZ@+ goodMuonsForZ@-")
+                         )
 
-# Z filters
-diMuonFilter = cms.EDFilter("CandViewCountFilter",
-                            src = cms.InputTag("dimuons"),
-                            minNumber = cms.uint32(1)
-                            )
-
-# WMuNu candidates
-from ElectroWeakAnalysis.WMuNu.wmunusProducer_cfi import *
-# WMuNu candidates selectors
-from ElectroWeakAnalysis.WMuNu.WMuNuSelection_cff import *
-seltcMet.JetTag = cms.untracked.InputTag("ak5CaloJets")
-seltcMet.TrigTag = cms.untracked.InputTag("TriggerResults::HLT")
-seltcMet.IsCombinedIso = cms.untracked.bool(True)
-seltcMet.IsoCut03 = cms.untracked.double(0.15)
-
-selpfMet.JetTag = cms.untracked.InputTag("ak5CaloJets")
-selpfMet.TrigTag = cms.untracked.InputTag("TriggerResults::HLT")
-selpfMet.IsCombinedIso = cms.untracked.bool(True)
-selpfMet.IsoCut03 = cms.untracked.double(0.15)
-
-
-# define the sequences
-diMuonSelSeq = cms.Sequence(WZMuHLTFilter *
-                            qualityMuonFilter *
-                            dxyMuonFilter *
-                            diMuonProducer *
-                            diMuonFilter
-                            )
-
-tcMetWMuNuSeq = cms.Sequence(WZMuHLTFilter *
-                             tcMetWMuNus *
-                             seltcMet
+# Z filter
+dimuonsFilter = cms.EDFilter("CandViewCountFilter",
+                             src = cms.InputTag("dimuons"),
+                             minNumber = cms.uint32(1)
                              )
 
+# Z Skim sequence
+diMuonSelSeq = cms.Sequence(WZMuHLTFilter *
+                            goodMuonsForZ *
+                            dimuons *
+                            dimuonsFilter
+                            )
+
+### W -> MuNu candidates
+
+#Get muons of needed quality for Ws
+goodMuonsForW = cms.EDFilter("MuonViewRefSelector",
+                             src = cms.InputTag("muons"),
+                             cut = cms.string('isGlobalMuon=1 && isTrackerMuon=1 && abs(eta)<2.1 && abs(globalTrack().dxy)<0.2 && pt>20. && globalTrack().normalizedChi2<10 && globalTrack().hitPattern().numberOfValidTrackerHits>10 && globalTrack().hitPattern().numberOfValidMuonHits>0 && globalTrack().hitPattern().numberOfValidPixelHits>0 && numberOfMatches>1 && (isolationR03().sumPt+isolationR03().emEt+isolationR03().hadEt)<0.15*pt'),
+                             filter = cms.bool(True)
+                             )
+
+# build W->MuNu candidates using PF MET
+wmnPFCands = cms.EDProducer("CandViewShallowCloneCombiner",
+                            checkCharge = cms.bool(False),
+                            cut = cms.string('sqrt((daughter(0).pt+daughter(1).pt)*(daughter(0).pt+daughter(1).pt)-pt*pt)>50'),
+                            decay = cms.string("goodMuonsForW pfMet")
+                            )
+
+# W filter
+wmnPFFilter = cms.EDFilter("CandViewCountFilter",
+                           src = cms.InputTag("wmnPFCands"),
+                           minNumber = cms.uint32(1)
+                           )
+
+# build W->MuNu candidates using TC MET
+wmnTCCands = cms.EDProducer("CandViewShallowCloneCombiner",
+                            checkCharge = cms.bool(False),
+                            cut = cms.string('sqrt((daughter(0).pt+daughter(1).pt)*(daughter(0).pt+daughter(1).pt)-pt*pt)>50'),
+                            decay = cms.string("goodMuonsForW tcMet")
+                            )
+
+# W filter
+wmnTCFilter = cms.EDFilter("CandViewCountFilter",
+                           src = cms.InputTag("wmnTCCands"),
+                           minNumber = cms.uint32(1)
+                           )
+
+
+# W Skim sequences
 pfMetWMuNuSeq = cms.Sequence(WZMuHLTFilter *
-                             pfMetWMuNus *
-                             selpfMet
+                             goodMuonsForW *
+                             wmnPFCands *
+                             wmnPFFilter
+                             )
+
+
+tcMetWMuNuSeq = cms.Sequence(WZMuHLTFilter *
+                             goodMuonsForW *
+                             wmnTCCands *
+                             wmnTCFilter
                              )
 
 
