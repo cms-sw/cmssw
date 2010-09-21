bc801571e34ab935c520c2e1c9f91e356089c631
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-09-21 12:09:47 +0000

    explicit event content definition

--- RecoParticleFlow/Configuration/python/RecoParticleFlow_EventContent_cff.py
+++ RecoParticleFlow/Configuration/python/RecoParticleFlow_EventContent_cff.py
@@ -7,34 +7,78 @@ import FWCore.ParameterSet.Config as cms
 
 # Full Event content 
 RecoParticleFlowFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('drop CaloTowersSorted_towerMakerPF_*_*', 
-        'keep recoPFRecHits_*_Cleaned_*',
-        'keep recoPFClusters_*_*_*',
-        'keep recoPFBlocks_*_*_*', 
-        'keep recoPFCandidates_*_*_*',
-        'keep recoPFDisplacedVertexs_*_*_*', 
-        'keep *_pfElectronTranslator_*_*',
-        'keep *_trackerDrivenElectronSeeds_preid_*')
-)
+    outputCommands = cms.untracked.vstring(
+    'drop CaloTowersSorted_towerMakerPF_*_*',
+    #'keep recoPFRecHits_*_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFEM_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFHAD_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterPS_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitPS_Cleaned_*',
+    #'keep recoPFClusters_*_*_*',
+    'keep recoPFClusters_particleFlowClusterECAL_*',
+    'keep recoPFClusters_particleFlowClusterHCAL_*',
+    'keep recoPFClusters_particleFlowClusterHFEM_*',
+    'keep recoPFClusters_particleFlowClusterHFHAD_*',
+    'keep recoPFClusters_particleFlowClusterPS_*',
+    #'keep recoPFBlocks_*_*_*',
+    'keep recoPFBlocks_particleFlowBlock_*',
+    #'keep recoPFCandidates_*_*_*',
+    'keep recoPFCandidates_particleFlow_*',
+    #'keep recoPFDisplacedVertexs_*_*_*',
+    'keep recoPFDisplacedVertexs_particleFlowDisplacedVertex_*',
+    'keep *_pfElectronTranslator_*_*',
+    'keep *_trackerDrivenElectronSeeds_preid_*')
+    )
 # RECO content
 RecoParticleFlowRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('drop CaloTowersSorted_towerMakerPF_*_*', 
-        'keep recoPFRecHits_*_Cleaned_*',
-        'keep recoPFClusters_*_*_*', 
-        'keep recoPFBlocks_*_*_*',
-        'keep recoPFCandidates_*_*_*',
-        'keep recoPFDisplacedVertexs_*_*_*', 
-        'keep *_pfElectronTranslator_*_*',
-        'keep *_trackerDrivenElectronSeeds_preid_*')
+    outputCommands = cms.untracked.vstring(
+    'drop CaloTowersSorted_towerMakerPF_*_*', 
+    #'keep recoPFRecHits_*_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFEM_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFHAD_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterPS_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitPS_Cleaned_*',
+    #'keep recoPFClusters_*_*_*',
+    'keep recoPFClusters_particleFlowClusterECAL_*',
+    'keep recoPFClusters_particleFlowClusterHCAL_*',
+    'keep recoPFClusters_particleFlowClusterHFEM_*',
+    'keep recoPFClusters_particleFlowClusterHFHAD_*',
+    'keep recoPFClusters_particleFlowClusterPS_*',
+    #'keep recoPFBlocks_*_*_*',
+    'keep recoPFBlocks_particleFlowBlock_*',
+    #'keep recoPFCandidates_*_*_*',
+    'keep recoPFCandidates_particleFlow_*',
+    #'keep recoPFDisplacedVertexs_*_*_*',
+    'keep recoPFDisplacedVertexs_particleFlowDisplacedVertex_*',
+    'keep *_pfElectronTranslator_*_*',
+    'keep *_trackerDrivenElectronSeeds_preid_*')
 )    
     
 # AOD content
 RecoParticleFlowAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('drop CaloTowersSorted_towerMakerPF_*_*',
-        'drop *_pfElectronTranslator_*_*',
-        'keep recoPFRecHits_*_Cleaned_*',
-        'keep recoPFCandidates_*_*_*',
-        'keep recoCaloClusters_pfElectronTranslator_*_*',
-        'keep recoPreshowerClusters_pfElectronTranslator_*_*',
-        'keep recoSuperClusters_pfElectronTranslator_*_*')
+    outputCommands = cms.untracked.vstring(
+    'drop CaloTowersSorted_towerMakerPF_*_*',
+    'drop *_pfElectronTranslator_*_*',
+    #'keep recoPFRecHits_*_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFEM_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterHFHAD_Cleaned_*',
+    'keep recoPFRecHits_particleFlowClusterPS_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitECAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitHCAL_Cleaned_*',
+    'keep recoPFRecHits_particleFlowRecHitPS_Cleaned_*',
+    #'keep recoPFCandidates_*_*_*',
+    'keep recoPFCandidates_particleFlow_*',
+    'keep recoCaloClusters_pfElectronTranslator_*_*',
+    'keep recoPreshowerClusters_pfElectronTranslator_*_*',
+    'keep recoSuperClusters_pfElectronTranslator_*_*')
 )
