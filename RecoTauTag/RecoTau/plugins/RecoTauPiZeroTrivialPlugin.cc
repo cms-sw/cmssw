89edd66e6b401f0f6587b8ae695dd380891bd52d
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-09-22 15:29:21 +0000

    cleanup

--- RecoTauTag/RecoTau/plugins/RecoTauPiZeroTrivialPlugin.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauPiZeroTrivialPlugin.cc
@@ -1,11 +1,3 @@
-#include "RecoTauTag/RecoTau/interface/RecoTauPiZeroPlugins.h"
-#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
-#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
-#include "DataFormats/JetReco/interface/PFJet.h"
-#include "DataFormats/TauReco/interface/RecoTauPiZero.h"
-
-#include "RecoTauTag/RecoTau/interface/RecoTauCommonUtilities.h"
-
 /* 
  * RecoTauPiZeroTrivialPlugin
  *
@@ -19,38 +11,46 @@
  *
  */
 
-namespace reco {
-  namespace tau {
+#include "RecoTauTag/RecoTau/interface/RecoTauPiZeroPlugins.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+#include "DataFormats/TauReco/interface/RecoTauPiZero.h"
 
-    class RecoTauPiZeroTrivialPlugin : public RecoTauPiZeroBuilderPlugin
-    {
-      public:
-        explicit RecoTauPiZeroTrivialPlugin(const edm::ParameterSet& pset);
-        ~RecoTauPiZeroTrivialPlugin() {}
-        std::vector<RecoTauPiZero> operator()(const reco::PFJet& jet) const;
-    };
+#include "RecoTauTag/RecoTau/interface/RecoTauCommonUtilities.h"
 
-    RecoTauPiZeroTrivialPlugin::RecoTauPiZeroTrivialPlugin(const edm::ParameterSet& pset):RecoTauPiZeroBuilderPlugin(pset){}
+#include <boost/foreach.hpp>
 
-    std::vector<RecoTauPiZero> RecoTauPiZeroTrivialPlugin::operator()(const reco::PFJet& jet) const
-    {
-      typedef std::vector<reco::PFCandidatePtr> PFCandPtrs; 
-      typedef PFCandPtrs::iterator PFCandIter;
-      std::vector<RecoTauPiZero> output;
+namespace reco { namespace tau {
 
-      PFCandPtrs pfGammaCands = tau::pfGammas(jet);
+class RecoTauPiZeroTrivialPlugin : public RecoTauPiZeroBuilderPlugin {
+  public:
+    explicit RecoTauPiZeroTrivialPlugin(const edm::ParameterSet& pset);
+    ~RecoTauPiZeroTrivialPlugin() {}
+    std::vector<RecoTauPiZero> operator()(const reco::PFJet& jet) const;
+};
 
-      for(PFCandIter gamma = pfGammaCands.begin(); gamma != pfGammaCands.end(); ++gamma)
-      {
-        RecoTauPiZero piZero(0, (**gamma).p4(), (**gamma).vertex(), 22, 1000, true, name());
-        piZero.addDaughter(*gamma);
-        output.push_back(piZero);
-      }
-      return output;
-    }
+RecoTauPiZeroTrivialPlugin::RecoTauPiZeroTrivialPlugin(
+    const edm::ParameterSet& pset):RecoTauPiZeroBuilderPlugin(pset){}
 
+std::vector<RecoTauPiZero> RecoTauPiZeroTrivialPlugin::operator()(
+    const reco::PFJet& jet) const {
+  std::vector<PFCandidatePtr> pfGammaCands = tau::pfGammas(jet);
+  std::vector<RecoTauPiZero> output;
+  output.reserve(pfGammaCands.size());
+
+  BOOST_FOREACH(const PFCandidatePtr& gamma, pfGammaCands) {
+    RecoTauPiZero piZero(0, (*gamma).p4(), 
+        (*gamma).vertex(), 22, 1000, true, name());
+    piZero.addDaughter(gamma);
+    output.push_back(piZero);
   }
+  return output;
 }
 
+}} // end reco::tau 
+
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_EDM_PLUGIN(RecoTauPiZeroBuilderPluginFactory, reco::tau::RecoTauPiZeroTrivialPlugin, "RecoTauPiZeroTrivialPlugin");
+DEFINE_EDM_PLUGIN(RecoTauPiZeroBuilderPluginFactory, 
+    reco::tau::RecoTauPiZeroTrivialPlugin, 
+    "RecoTauPiZeroTrivialPlugin");
