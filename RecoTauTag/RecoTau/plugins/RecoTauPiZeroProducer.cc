2b38c78ac1174ada80c5bb78653adcd80e0d1959
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-09-22 15:52:52 +0000

    cleanup

--- RecoTauTag/RecoTau/plugins/RecoTauPiZeroProducer.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauPiZeroProducer.cc
@@ -1,3 +1,17 @@
+/*
+ * RecoTauPiZeroProducer
+ *
+ * Author: Evan K. Friis, UC Davis
+ *
+ * Associates reconstructed PiZeros to PFJets.  The PiZeros are built using one
+ * or more RecoTauBuilder plugins.  Any overlaps (PiZeros sharing constituents)
+ * are removed, with the best PiZero candidates taken.  The 'best' are defined
+ * via the input list of RecoTauPiZeroQualityPlugins, which form a
+ * lexicograpical ranking.
+ *
+ * $Id $
+ */ 
+
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -12,17 +26,17 @@
 #include "DataFormats/TauReco/interface/RecoTauPiZero.h"
 
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/foreach.hpp>
 
 #include <algorithm>
 
 
 namespace {
-  /* Class that checks if two PiZeros contain any of the same daughters */
-  class PiZeroOverlapChecker 
-  {
+  // Class that checks if two PiZeros contain any of the same daughters 
+  class PiZeroOverlapChecker {
     public:
-      bool operator()(const reco::RecoTauPiZero& a, const reco::RecoTauPiZero& b)
-      {
+      bool operator()(const reco::RecoTauPiZero& a, 
+          const reco::RecoTauPiZero& b) {
         typedef std::vector<reco::CandidatePtr> daughters;
         daughters aDaughters = a.daughterPtrVector();
         daughters bDaughters = b.daughterPtrVector();
@@ -37,47 +51,52 @@ namespace {
 
 using namespace reco;
 
-class RecoTauPiZeroProducer : public edm::EDProducer 
-{
+class RecoTauPiZeroProducer : public edm::EDProducer {
   public:
+    typedef reco::tau::RecoTauPiZeroBuilderPlugin Builder;
+    typedef reco::tau::RecoTauPiZeroQualityPlugin Ranker;
+
     explicit RecoTauPiZeroProducer(const edm::ParameterSet& pset);
     ~RecoTauPiZeroProducer(){};
     void produce(edm::Event& evt, const edm::EventSetup& es);
     void print(const std::vector<RecoTauPiZero>& piZeros, std::ostream& out);
 
   private:
-    typedef boost::ptr_vector<reco::tau::RecoTauPiZeroBuilderPlugin> builderList;
-    typedef boost::ptr_vector<reco::tau::RecoTauPiZeroQualityPlugin> rankerList;
-    typedef reco::tau::RecoTauLexicographicalRanking<rankerList, RecoTauPiZero> PiZeroPredicate;
+    typedef boost::ptr_vector<Builder> builderList;
+    typedef boost::ptr_vector<Ranker> rankerList;
+    typedef reco::tau::RecoTauLexicographicalRanking<rankerList, RecoTauPiZero> 
+      PiZeroPredicate;
     edm::InputTag jetSrc_;
     builderList builders_;
     rankerList rankers_;
     std::auto_ptr<PiZeroPredicate> predicate_;
 };
 
-RecoTauPiZeroProducer::RecoTauPiZeroProducer(const edm::ParameterSet& pset)
-{
+RecoTauPiZeroProducer::RecoTauPiZeroProducer(const edm::ParameterSet& pset) {
   jetSrc_ = pset.getParameter<edm::InputTag>("jetSrc");
 
   typedef std::vector<edm::ParameterSet> VPSet;
-
   // Get each of our PiZero builders
   const VPSet& builders = pset.getParameter<VPSet>("builders");
 
-  for(VPSet::const_iterator builderPSet = builders.begin(); builderPSet != builders.end(); ++builderPSet)
-  {
+  for(VPSet::const_iterator builderPSet = builders.begin(); 
+      builderPSet != builders.end(); ++builderPSet) {
     // Get plugin name
-    const std::string& pluginType = builderPSet->getParameter<std::string>("plugin");
+    const std::string& pluginType = 
+      builderPSet->getParameter<std::string>("plugin");
     // Build the plugin
-    builders_.push_back(RecoTauPiZeroBuilderPluginFactory::get()->create(pluginType, *builderPSet));
+    builders_.push_back(RecoTauPiZeroBuilderPluginFactory::get()->create(
+          pluginType, *builderPSet));
   }
 
   // Get each of our quality rankers
   const VPSet& rankers = pset.getParameter<VPSet>("ranking"); 
-  for(VPSet::const_iterator rankerPSet = rankers.begin(); rankerPSet != rankers.end(); ++rankerPSet)
-  {
-    const std::string& pluginType = rankerPSet->getParameter<std::string>("plugin");
-    rankers_.push_back(RecoTauPiZeroQualityPluginFactory::get()->create(pluginType, *rankerPSet));
+  for(VPSet::const_iterator rankerPSet = rankers.begin(); 
+      rankerPSet != rankers.end(); ++rankerPSet) {
+    const std::string& pluginType = 
+      rankerPSet->getParameter<std::string>("plugin");
+    rankers_.push_back(RecoTauPiZeroQualityPluginFactory::get()->create(
+          pluginType, *rankerPSet));
   }
 
   // Build the sorting predicate
@@ -92,21 +111,20 @@ void RecoTauPiZeroProducer::produce(edm::Event& evt, const edm::EventSetup& es)
   evt.getByLabel(jetSrc_, pfJets);
 
   // Make our association
-  std::auto_ptr<JetPiZeroAssociation> association(new JetPiZeroAssociation(PFJetRefProd(pfJets)));
+  std::auto_ptr<JetPiZeroAssociation> association(
+      new JetPiZeroAssociation(PFJetRefProd(pfJets)));
 
   size_t iJet = 0;
   // Loop over our jets
-  for(PFJetCollection::const_iterator jet = pfJets->begin(); jet != pfJets->end(); ++jet, ++iJet)
-  {
+  for(PFJetCollection::const_iterator jet = pfJets->begin(); 
+      jet != pfJets->end(); ++jet, ++iJet) {
     typedef std::vector<RecoTauPiZero> PiZeroVector;
     // Build our global list of RecoTauPiZero
     PiZeroVector dirtyPiZeros;
 
     // Compute the pi zeros from this jet for all the desired algorithms
-    for(builderList::const_iterator builder = builders_.begin(); builder != builders_.end(); ++builder)
-    {
-      PiZeroVector result((*builder)(*jet));
-      // Append to our result
+    BOOST_FOREACH(const Builder& builder, builders_) {
+      PiZeroVector result(builder(*jet));
       dirtyPiZeros.insert(dirtyPiZeros.end(), result.begin(), result.end());
     }
 
@@ -120,8 +138,8 @@ void RecoTauPiZeroProducer::produce(edm::Event& evt, const edm::EventSetup& es)
     //print(dirtyPiZeros, std::cout);
 
     // Now clean the list to ensure that no photon is counted twice
-    PiZeroVector cleanPiZeros = 
-      reco::tau::cleanOverlaps<PiZeroVector, PiZeroOverlapChecker>(dirtyPiZeros);
+    PiZeroVector cleanPiZeros = reco::tau::cleanOverlaps<PiZeroVector, 
+                 PiZeroOverlapChecker>(dirtyPiZeros);
 
     // Sort the clean pizeros by pt
     std::sort(cleanPiZeros.begin(), cleanPiZeros.end(), 
@@ -136,17 +154,20 @@ void RecoTauPiZeroProducer::produce(edm::Event& evt, const edm::EventSetup& es)
   evt.put(association);
 }
 
-void RecoTauPiZeroProducer::print(const std::vector<reco::RecoTauPiZero>& piZeros, std::ostream& out)
-{
+// Print some helpful information
+void RecoTauPiZeroProducer::print(
+    const std::vector<reco::RecoTauPiZero>& piZeros, std::ostream& out) {
   const unsigned int width = 25;
-  for(std::vector<reco::RecoTauPiZero>::const_iterator piZero = piZeros.begin(); piZero != piZeros.end(); ++piZero)
-  {
-    out << *piZero;
+  BOOST_FOREACH(const reco::RecoTauPiZero& piZero, piZeros) {
+    out << piZero;
     out << "* Rankers:" << std::endl;
-    for(rankerList::const_iterator ranker = rankers_.begin(); ranker != rankers_.end(); ++ranker)
+    for(rankerList::const_iterator ranker = rankers_.begin(); 
+        ranker != rankers_.end(); ++ranker)
     {
-      out << "* " << std::setiosflags(std::ios::left) << std::setw(width) << ranker->name();
-      out << " " << std::resetiosflags(std::ios::left) << std::setprecision(3) << (*ranker)(*piZero);
+      out << "* " << std::setiosflags(std::ios::left) 
+        << std::setw(width) << ranker->name()
+        << " " << std::resetiosflags(std::ios::left) 
+        << std::setprecision(3) << (*ranker)(piZero);
       out << std::endl;
     }
   }
