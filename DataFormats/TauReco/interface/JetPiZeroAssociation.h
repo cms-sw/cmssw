52174df3a14f948235b567936d60821861da4cbe
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-09-22 14:09:26 +0000

    Adding PiZero dataformat and required infrastructure

---
+++ DataFormats/TauReco/interface/JetPiZeroAssociation.h
@@ -0,0 +1,34 @@
+#ifndef DataFormats_TauReco_JetPiZeroAssociation_h
+#define DataFormats_TauReco_JetPiZeroAssociation_h
+
+#include "DataFormats/Common/interface/AssociationVector.h"
+#include "DataFormats/TauReco/interface/RecoTauPiZero.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+
+namespace reco {
+   // This base class improves the readability of the ROOT class name by hiding
+   // the template crap
+   typedef edm::AssociationVector<PFJetRefProd, std::vector<std::vector<RecoTauPiZero> > >
+     JetPiZeroAssociationBase;  
+
+   class JetPiZeroAssociation : public JetPiZeroAssociationBase {
+      public: 
+    JetPiZeroAssociation() :
+      JetPiZeroAssociationBase()
+      { }
+    
+    JetPiZeroAssociation(const reco::PFJetRefProd & ref) :
+      JetPiZeroAssociationBase(ref)
+      { }
+    
+    JetPiZeroAssociation(const JetPiZeroAssociationBase &v) :
+      JetPiZeroAssociationBase(v)
+      { }
+  };
+  
+  typedef JetPiZeroAssociation::value_type JetPiZeroAssociationPiZeros;  
+  typedef edm::Ref<JetPiZeroAssociation> JetPiZeroAssociationRef;  
+  typedef edm::RefProd<JetPiZeroAssociation> JetPiZeroAssociationRefProd;  
+  typedef edm::RefVector<JetPiZeroAssociation> JetPiZeroAssociationRefVector; 
+}
+#endif
