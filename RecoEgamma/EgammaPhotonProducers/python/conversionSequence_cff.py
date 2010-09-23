d4565f7c7777669aba081d255a8b976804f4aee3
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2010-09-23 17:34:11 +0000

    Move ckfTracksFromConversions in the new conversionTrackSequence

--- RecoEgamma/EgammaPhotonProducers/python/conversionSequence_cff.py
+++ RecoEgamma/EgammaPhotonProducers/python/conversionSequence_cff.py
@@ -7,5 +7,6 @@ from RecoEgamma.EgammaPhotonProducers.conversionTracks_cff import *
 # converted photon producer
 #from RecoEgamma.EgammaTools.PhotonConversionMVAComputer_cfi import *
 from RecoEgamma.EgammaPhotonProducers.conversions_cfi import *
-conversionSequence = cms.Sequence(ckfTracksFromConversions*conversions)
+#conversionSequence = cms.Sequence(ckfTracksFromConversions*conversions)
+conversionSequence = cms.Sequence(conversions)
 
