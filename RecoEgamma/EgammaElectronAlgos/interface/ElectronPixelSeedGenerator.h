a7283681be6db9496d9411bef38ca33095acd435
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2010-09-23 12:15:04 +0000

    remove deprecated files

--- RecoEgamma/EgammaElectronAlgos/interface/ElectronPixelSeedGenerator.h
+++
@@ -1,8 +0,0 @@
-#ifndef ElectronPixelSeedGenerator_H
-#define ElectronPixelSeedGenerator_H
-
-#error Obsolete, use ElectronSeedGenerator.h instead
-
-#endif // ElectronPixelSeedGenerator_H
-
-
--- RecoEgamma/EgammaElectronProducers/plugins/ElectronPixelSeedProducer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ElectronPixelSeedProducer_H
-#define ElectronPixelSeedProducer_H
-
-#error Obsolete, use ElectronSeedProducer.h instead
-
-#endif // ElectronPixelSeedProducer_H
-
-
