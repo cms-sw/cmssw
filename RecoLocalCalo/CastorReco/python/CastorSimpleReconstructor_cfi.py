622172102d5eab8844bd9808473434e8a20e1273
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2010-09-26 12:19:06 +0000

    update time slices and fix input

--- RecoLocalCalo/CastorReco/python/CastorSimpleReconstructor_cfi.py
+++ RecoLocalCalo/CastorReco/python/CastorSimpleReconstructor_cfi.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 castorreco = cms.EDProducer("CastorSimpleReconstructor",
     correctionPhaseNS = cms.double(0.0),
-    digiLabel = cms.InputTag("simCastorDigis"),
-    samplesToAdd = cms.int32(1),
+    digiLabel = cms.InputTag("castorDigis"),
+    samplesToAdd = cms.int32(2),
     Subdetector = cms.string('CASTOR'),
-    firstSample = cms.int32(3),
+    firstSample = cms.int32(4),
     correctForPhaseContainment = cms.bool(False),
     correctForTimeslew = cms.bool(False)
 )
