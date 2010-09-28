b58589971172262dcc6388a080c68b6ce053a6d7
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-09-28 09:33:23 +0000

    More fixes

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-HLT='/online/collisions/2010/week37/HLT'
+HLT='/online/collisions/2010/week38/HLT'
 L1T='L1Menu_Commissioning2010_v4'
 
 hltGetConfiguration $HLT --process TEST --full --offline --mc             --unprescale --dataset '/RelValTTbar/CMSSW_3_6_3-START36_V10-v1/GEN-SIM-DIGI-RAW-HLTDEBUG' > offline_mc.py
