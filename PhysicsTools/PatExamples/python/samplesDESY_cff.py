08e52dc27872ed223a522c326f87cd4fd3710e73
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-09-28 15:10:11 +0000

    added ttbar sample

--- PhysicsTools/PatExamples/python/samplesDESY_cff.py
+++ PhysicsTools/PatExamples/python/samplesDESY_cff.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+
+ttbarRECO = cms.untracked.vstring(
+     '/store/mc/Spring10/TTbarJets-madgraph/AODSIM/START3X_V26_S09-v1/0005/0210B899-9C46-DF11-A10F-003048C69294.root'
+    ,'/store/mc/Spring10/TTbarJets-madgraph/AODSIM/START3X_V26_S09-v1/0005/0C39D8AD-A846-DF11-8016-003048C692CA.root'
+)
+
 zjetsRECO = cms.untracked.vstring(
      '/store/mc/Spring10/ZJets-madgraph/AODSIM/START3X_V26_S09-v1/0013/00EFC4EA-3847-DF11-A194-003048D4DF80.root'
     ,'/store/mc/Spring10/ZJets-madgraph/AODSIM/START3X_V26_S09-v1/0013/0C096217-3A47-DF11-9E65-003048C692A4.root'
