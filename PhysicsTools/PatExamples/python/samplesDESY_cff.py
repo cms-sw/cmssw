dd4cd7b10178550dcdf71addb0fef2c5aeb50085
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-09-28 10:31:35 +0000

    get rid of obsolte comments

--- PhysicsTools/PatExamples/python/samplesDESY_cff.py
+++ PhysicsTools/PatExamples/python/samplesDESY_cff.py
@@ -1,10 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-## ----------------------------------------------------------------
-##
-##   9,991 Z+Jets events as defined on WorkBookPATExampleTopQuarks
-##
-## ----------------------------------------------------------------
 zjetsRECO = cms.untracked.vstring(
      '/store/mc/Spring10/ZJets-madgraph/AODSIM/START3X_V26_S09-v1/0013/00EFC4EA-3847-DF11-A194-003048D4DF80.root'
     ,'/store/mc/Spring10/ZJets-madgraph/AODSIM/START3X_V26_S09-v1/0013/0C096217-3A47-DF11-9E65-003048C692A4.root'
