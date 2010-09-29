6027c5ddd35776ccbcfd301e6bcf338aac6cfb03
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2010-09-29 16:40:33 +0000

    added digi sequence for Technical Triggers in HF.  (From Vasile)

--- SimCalorimetry/Configuration/python/hcalDigiSequence_cff.py
+++ SimCalorimetry/Configuration/python/hcalDigiSequence_cff.py
@@ -3,5 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from SimCalorimetry.HcalSimProducers.hcalUnsuppressedDigis_cfi import *
 from SimCalorimetry.HcalZeroSuppressionProducers.hcalDigisRealistic_cfi import *
 from SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff import *
-hcalDigiSequence = cms.Sequence(simHcalUnsuppressedDigis+simHcalTriggerPrimitiveDigis+simHcalDigis)
+from SimCalorimetry.HcalTrigPrimProducers.hcalTTPDigis_cfi import *
+
+hcalDigiSequence = cms.Sequence(simHcalUnsuppressedDigis
+                                +simHcalTriggerPrimitiveDigis
+                                +simHcalDigis
+                                *simHcalTTPDigis)
 
