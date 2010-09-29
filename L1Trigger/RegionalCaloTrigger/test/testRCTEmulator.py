bab05fb2413d2b70248b571105b3d66f04a84e3c
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2010-09-29 15:33:17 +0000

    Adding new test cfg

--- L1Trigger/RegionalCaloTrigger/test/testRCTEmulator.py
+++ L1Trigger/RegionalCaloTrigger/test/testRCTEmulator.py
@@ -6,64 +6,42 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-    'file:/scratch/bachtis/test.root'
-       
+        '/store/data/Run2010B/CommissioningNoBeam/RAW/v1/000/146/527/CA5D36A5-D0C7-DF11-A027-0019B9F4A1D7.root',
+#        '/store/data/Run2010B/CommissioningNoBeam/RAW/v1/000/146/529/08B9E39D-D7C7-DF11-9B6F-0030487CAF0E.root',
+        
     )
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(3000)
+    input = cms.untracked.int32(-1)
 )
 
 # standard includes
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "IDEAL_30X::All"
+process.GlobalTag.globaltag = 'GR10_P_V9::All'
 
 
 # unpack raw data
-process.load("Configuration.StandardSequences.RawToDigi_cff")
-
-# run trigger primitive generation on unpacked digis, then central L1
-process.load("L1Trigger.Configuration.CaloTriggerPrimitives_cff")
-
-process.simEcalTriggerPrimitiveDigis.Label = 'ecalDigis'
-process.simHcalTriggerPrimitiveDigis.inputLabel = 'hcalDigis'
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
 
 
-process.load("L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cff")
-process.load("L1Trigger.RegionalCaloTrigger.rctDigis_cfi")
+process.rctEmulDigis = cms.EDProducer("L1RCTProducer",
+                                  hcalDigis = cms.VInputTag(cms.InputTag("hcalTriggerPrimitiveDigis")),
+                                  useDebugTpgScales = cms.bool(True),
+                                  useEcal = cms.bool(True),
+                                  useHcal = cms.bool(True),
+                                  ecalDigis = cms.VInputTag(cms.InputTag("ecalTriggerPrimitiveDigis")),
+                                  BunchCrossings = cms.vint32(0)
+                              )
 
-process.rctDigis.ecalDigis =cms.VInputTag( cms.InputTag('simEcalTriggerPrimitiveDigis'))
-process.rctDigis.hcalDigis = cms.VInputTag(cms.InputTag('simHcalTriggerPrimitiveDigis'))
+process.rctEmulDigis.hcalDigis = cms.VInputTag(cms.InputTag("hcalDigis"))
+process.rctEmulDigis.ecalDigis = cms.VInputTag(cms.InputTag("ecalDigis:EcalTriggerPrimitives"))
 
-process.L1Analysis = cms.EDAnalyzer("L1RCTTestAnalyzer",
-    hcalDigisLabel = cms.InputTag("simHcalTriggerPrimitiveDigis"),
-    showEmCands = cms.untracked.bool(False),
-    ecalDigisLabel = cms.InputTag("simEcalTriggerPrimitiveDigis"),
-    rctDigisLabel = cms.InputTag("rctDigis"),
-    showRegionSums = cms.untracked.bool(False)
-)
-
-
-process.TFileService = cms.Service("TFileService",
-                                 fileName = cms.string("histo.root"),
-                                 closeFileFast = cms.untracked.bool(True)
-                             )
-
-
-
-# L1 configuration
-process.load('L1Trigger.Configuration.L1DummyConfig_cff')
-
-
-process.rctDigis.useCorrectionsLindsey = cms.bool(False)
 
 process.p = cms.Path(
-    process.ecalDigis
-    *process.hcalDigis
-    *process.rctDigis
-    *process.L1Analysis
+    process.RawToDigi*
+    process.rctEmulDigis
 )
 
 
