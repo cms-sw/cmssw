5cb05cb7a7bab986c7be2da839f12e9f3779b71b
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-09-29 14:00:29 +0000

    Add directions for sim hits. End a track at a last matched hit.

--- Fireworks/SimData/plugins/FWTrackingParticleProxyBuilder.cc
+++ Fireworks/SimData/plugins/FWTrackingParticleProxyBuilder.cc
@@ -38,7 +38,7 @@ FWTrackingParticleProxyBuilder::build( const TrackingParticle& iData, unsigned i
    TEveRecTrack t;
    t.fBeta = 1.0;
    t.fP = TEveVector( iData.px(), iData.py(), iData.pz() );
-   t.fV = TEveVector( iData.vx() * 0.01, iData.vy() * 0.01, iData.vz() * 0.01 );
+   t.fV = TEveVector( iData.vx(), iData.vy(), iData.vz() );
    t.fSign = iData.charge();
   
    TEveTrack* track = new TEveTrack(&t, context().getTrackPropagator());
@@ -51,17 +51,41 @@ FWTrackingParticleProxyBuilder::build( const TrackingParticle& iData, unsigned i
    const std::vector<PSimHit>& hits = iData.trackPSimHit();
 
    float local[3];
+   float localDir[3];
    float global[3];
-   for( std::vector<PSimHit>::const_iterator it = hits.begin(), end = hits.end(); it != end; ++it )
+   float globalDir[3];
+   std::vector<PSimHit>::const_iterator it = hits.begin();
+   std::vector<PSimHit>::const_iterator end = hits.end();
+   if( it != end )
    {
-     const PSimHit& phit = (*it);
-     local[0] = phit.localPosition().x();
-     local[1] = phit.localPosition().y();
-     local[2] = phit.localPosition().z();
-     geom->localToGlobal( phit.detUnitId(), local, global );
-     pointSet->SetNextPoint( global[0], global[1], global[2] );
-     track->AddPathMark( TEvePathMark( TEvePathMark::kDaughter, TEveVector( global[0], global[1], global[2] )));
+      unsigned int trackid = hits.begin()->trackId();
+
+      for( std::vector<PSimHit>::const_iterator it = hits.begin(), end = hits.end(); it != end; ++it )
+      {
+	 const PSimHit& phit = (*it);
+	 if( phit.trackId() != trackid )
+	 {
+	    trackid = phit.trackId();
+	    track->AddPathMark( TEvePathMark( TEvePathMark::kDecay, TEveVector( global[0], global[1], global[2] ),
+					      TEveVector( globalDir[0], globalDir[1], globalDir[2] )));
+	 }
+	 local[0] = phit.localPosition().x();
+	 local[1] = phit.localPosition().y();
+	 local[2] = phit.localPosition().z();
+	 localDir[0] = phit.momentumAtEntry().x();
+	 localDir[1] = phit.momentumAtEntry().y();
+	 localDir[2] = phit.momentumAtEntry().z();
+	 geom->localToGlobal( phit.detUnitId(), local, global );
+	 geom->localToGlobal( phit.detUnitId(), localDir, globalDir );
+	 pointSet->SetNextPoint( global[0], global[1], global[2] );
+	 track->AddPathMark( TEvePathMark( TEvePathMark::kReference/*kDaughter*/, TEveVector( global[0], global[1], global[2] ),
+					   TEveVector( globalDir[0], globalDir[1], globalDir[2] )));
+      }
+      if( hits.size() > 1 )
+	 track->AddPathMark( TEvePathMark( TEvePathMark::kDecay, TEveVector( global[0], global[1], global[2] ),
+					   TEveVector( globalDir[0], globalDir[1], globalDir[2] )));
    }
+   
    track->MakeTrack();
    setupAddElement( track, &oItemHolder );
 }
