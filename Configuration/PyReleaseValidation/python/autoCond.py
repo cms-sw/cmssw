c6122a361abf823eb329fcc72220466fee2cefff
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-09-29 16:27:03 +0000

    New GT for 39X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,10 +1,10 @@
 
 
 
-autoCond = { 'mc'      : 'MC_39Y_V0::All',
-             'startup' : 'START39_V0::All',
-             'craft08' : 'CRFT8_37R_V0::All',
-             'craft09' : 'CRFT9_39R_V0::All',
-             'com10'   : 'GR_R_39X_V0::All',
-             'hltonline' : 'GR_R_39X_V0::All' # same as com10 until H tag available
+autoCond = { 'mc'      : 'MC_39Y_V1::All',
+             'startup' : 'START39_V1::All',
+             'craft08' : 'CRFT8_37R_V1::All',
+             'craft09' : 'CRFT9_39R_V1::All',
+             'com10'   : 'GR_R_39X_V1::All',
+             'hltonline' : 'GR_R_39X_V1::All' # same as com10 until H tag available
              }
