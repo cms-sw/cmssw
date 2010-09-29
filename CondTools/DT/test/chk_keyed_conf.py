67f85812d936dae0d3744e4c4e502c4184e0d8fa
Paolo Ronchese <sha1-591dc25f43b7b0cdf7a5025ce38797ed3141c081@cern.ch>
2010-09-29 13:58:05 +0000

    minor change

--- CondTools/DT/test/chk_keyed_conf.py
+++ CondTools/DT/test/chk_keyed_conf.py
@@ -22,8 +22,7 @@ process.PoolDBESSource = cms.ESSource("PoolDBESSource",
     tag = cms.string('keyedConfBricks_V01')
     )
     ),
-    connect = cms.string('sqlite_file:userconf.db'),
-#    connect = cms.string('sqlite_file:testconf.db'),
+    connect = cms.string('sqlite_file:testconf.db'),
     DBParameters = cms.PSet(
         messageLevel = cms.untracked.int32(0),
         authenticationPath = cms.untracked.string('.')
