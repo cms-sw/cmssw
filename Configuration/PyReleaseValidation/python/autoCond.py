387bf545fd8057a8943f4f92011332c087d8393a
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-09-30 14:23:14 +0000

    Update GT for CRAFT09 data in 39X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -4,7 +4,7 @@
 autoCond = { 'mc'      : 'MC_39Y_V1::All',
              'startup' : 'START39_V1::All',
              'craft08' : 'CRFT8_37R_V1::All',
-             'craft09' : 'CRFT9_39R_V1::All',
+             'craft09' : 'CRFT9_39R_V2::All',
              'com10'   : 'GR_R_39X_V1::All',
              'hltonline' : 'GR_R_39X_V1::All' # same as com10 until H tag available
              }
