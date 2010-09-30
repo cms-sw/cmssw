a3aa5ea7777d7972caa9b4c0f1a3189c3a1c8477
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-09-30 16:35:54 +0000

    Revert to the old algorithm, but put a protection in FastSimulation/ShowerDevelopment that avoids too large energy enhancements

--- FastSimulation/Calorimetry/python/Calorimetry_cff.py
+++ FastSimulation/Calorimetry/python/Calorimetry_cff.py
@@ -40,8 +40,8 @@ FamosCalorimetryBlock = cms.PSet(
             HCAL_Sampling = cms.double(0.0035),
             # Watch out ! The following two values are defined wrt the electron shower simulation
             # There are not directly related to the detector properties
-#            HCAL_PiOverE = cms.double(0.2)
-            HCAL_PiOverE = cms.double(0.4)
+            HCAL_PiOverE = cms.double(0.2)
+#            HCAL_PiOverE = cms.double(0.4)
         ),
         UnfoldedMode = cms.untracked.bool(False),
         Debug = cms.untracked.bool(False),
