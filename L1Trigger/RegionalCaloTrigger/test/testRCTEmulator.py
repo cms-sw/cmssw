5977b5c38f81f0f41a6ab73a2b47362e8818092e
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2010-09-30 22:27:34 +0000

    *** empty log message ***

--- L1Trigger/RegionalCaloTrigger/test/testRCTEmulator.py
+++ L1Trigger/RegionalCaloTrigger/test/testRCTEmulator.py
@@ -3,11 +3,17 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("L1")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+#process.load("DQMServices.Core.DQM_cfg")
+process.DQMStore = cms.Service("DQMStore")
+
+
+
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Run2010B/CommissioningNoBeam/RAW/v1/000/146/527/CA5D36A5-D0C7-DF11-A027-0019B9F4A1D7.root',
-#        '/store/data/Run2010B/CommissioningNoBeam/RAW/v1/000/146/529/08B9E39D-D7C7-DF11-9B6F-0030487CAF0E.root',
+#    '/store/data/Run2010B/TestEnables/RAW/v1/000/146/150/98DEA325-BBC3-DF11-9EDD-0030487C90EE.root',
+    '/store/data/Run2010B/TestEnables/RAW/v1/000/146/153/049306C1-CEC3-DF11-BC34-0030487CD7C6.root',
+        
         
     )
 )
@@ -38,10 +44,31 @@ process.rctEmulDigis = cms.EDProducer("L1RCTProducer",
 process.rctEmulDigis.hcalDigis = cms.VInputTag(cms.InputTag("hcalDigis"))
 process.rctEmulDigis.ecalDigis = cms.VInputTag(cms.InputTag("ecalDigis:EcalTriggerPrimitives"))
 
+process.l1tderct = cms.EDAnalyzer("L1TdeRCT",
+                              rctSourceData = cms.InputTag("gctDigis"),
+                              HistFolder = cms.untracked.string('L1TEMU/L1TdeRCT/'),
+                              outputFile = cms.untracked.string('dqm.root'),
+                              verbose = cms.untracked.bool(False),
+                              DQMStore = cms.untracked.bool(True),
+                              singlechannelhistos = cms.untracked.bool(False),
+                              ecalTPGData = cms.InputTag("ecalDigis","EcalTriggerPrimitives"),
+                              rctSourceEmul = cms.InputTag("valRctDigis"),
+                              disableROOToutput = cms.untracked.bool(False),
+                              hcalTPGData = cms.InputTag("hcalDigis"),
+                              gtDigisLabel = cms.InputTag("gtDigis"),
+                              gtEGAlgoName = cms.string("L1_SingleEG1"),
+                              doubleThreshold = cms.int32(3),
+
+                          )
+
+
+process.l1tderct.rctSourceEmul = 'rctEmulDigis'
+
 
 process.p = cms.Path(
     process.RawToDigi*
-    process.rctEmulDigis
+    process.rctEmulDigis*
+    process.l1tderct
 )
 
 
