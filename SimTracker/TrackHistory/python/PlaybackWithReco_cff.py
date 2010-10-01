f8e131dd574b66c318176556393c4d8c3e86669b
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-10-01 14:15:29 +0000

    Small changes for syntax

--- SimTracker/TrackHistory/python/PlaybackWithReco_cff.py
+++ SimTracker/TrackHistory/python/PlaybackWithReco_cff.py
@@ -2,11 +2,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Import of standard configurations
-from Configuration.StandardSequences.Services_cff import *
 from FWCore.MessageService.MessageLogger_cfi import *
-from Configuration.StandardSequences.MixingNoPileUp_cff import *
-from Configuration.StandardSequences.GeometryPilot2_cff import *
-from Configuration.StandardSequences.MagneticField_38T_cff import *
 from Configuration.StandardSequences.RawToDigi_cff import *
 from Configuration.StandardSequences.Reconstruction_cff import *
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
@@ -14,6 +10,7 @@ from Configuration.EventContent.EventContent_cff import *
 from Configuration.StandardSequences.Digi_cff import *
 
 # Playback
+from Configuration.StandardSequences.Services_cff import *
 del RandomNumberGeneratorService.generator
 RandomNumberGeneratorService.restoreStateLabel = cms.untracked.string('randomEngineStateProducer')
 from SimGeneral.MixingModule.mixNoPU_cfi import *
