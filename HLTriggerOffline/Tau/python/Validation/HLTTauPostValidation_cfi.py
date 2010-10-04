95da7b91f60ed46e953d89c94b5e40fa6c1c2f8a
Joshua James Swanson <sha1-8881b0a90e2d698b47914ccde7f42725e686d8ab@cern.ch>
2010-10-04 10:00:42 +0000

    updated trigger paths

--- HLTriggerOffline/Tau/python/Validation/HLTTauPostValidation_cfi.py
+++ HLTriggerOffline/Tau/python/Validation/HLTTauPostValidation_cfi.py
@@ -3,41 +3,31 @@ import FWCore.ParameterSet.Config as cms
 HLTTauValPostAnalysis = cms.EDAnalyzer("HLTTauPostProcessor",
   Harvester = cms.PSet(
     L1Dirs                  = cms.vstring(
-    "HLT/TauRelVal/MC_8E29/L1",
-    "HLT/TauRelVal/MC_1E31/L1",
-    "HLT/TauRelVal/PF_8E29/L1",
-    "HLT/TauRelVal/PF_1E31/L1"
+    "HLT/TauRelVal/MC_6E31/L1",
+    "HLT/TauRelVal/PF_6E31/L1"
     ),
     caloDirs                = cms.vstring(
-    "HLT/TauRelVal/MC_8E29/L2",
-    "HLT/TauRelVal/MC_1E31/L2",
-    "HLT/TauRelVal/PF_8E29/L2",
-    "HLT/TauRelVal/PF_1E31/L2"
+    "HLT/TauRelVal/MC_6E31/L2",
+    "HLT/TauRelVal/PF_6E31/L2"
 
     ),
     trackDirs               = cms.vstring(
-    "HLT/TauRelVal/MC_1E31/L25",
-    "HLT/TauRelVal/MC_1E31/L3",
-    "HLT/TauRelVal/PF_1E31/L25",
-    "HLT/TauRelVal/PF_1E31/L3"
+    "HLT/TauRelVal/MC_6E31/L25",
+    "HLT/TauRelVal/MC_6E31/L3",
+    "HLT/TauRelVal/PF_6E31/L25",
+    "HLT/TauRelVal/PF_6E31/L3"
 
     ),
     pathDirs                = cms.vstring(
-    "HLT/TauRelVal/MC_8E29/DoubleTau",
-    "HLT/TauRelVal/MC_1E31/DoubleTau",
-    "HLT/TauRelVal/MC_8E29/SingleTau",
-    "HLT/TauRelVal/MC_1E31/SingleTau",
-    "HLT/TauRelVal/PF_8E29/DoubleTau",
-    "HLT/TauRelVal/PF_1E31/DoubleTau",
-    "HLT/TauRelVal/PF_8E29/SingleTau",
-    "HLT/TauRelVal/PF_1E31/SingleTau"
+    "HLT/TauRelVal/MC_6E31/DoubleTau",
+    "HLT/TauRelVal/MC_6E31/SingleTau",
+    "HLT/TauRelVal/PF_6E31/DoubleTau",
+    "HLT/TauRelVal/PF_6E31/SingleTau"
 
     ),
     pathSummaryDirs         = cms.vstring(
-    'HLT/TauRelVal/MC_8E29/Summary',
-    'HLT/TauRelVal/MC_1E31/Summary'
-    'HLT/TauRelVal/PF_8E29/Summary',
-    'HLT/TauRelVal/PF_1E31/Summary'
+    'HLT/TauRelVal/MC_6E31/Summary',
+    'HLT/TauRelVal/PF_6E31/Summary'
     )
   )
 )
--- HLTriggerOffline/Tau/python/Validation/HLTTauQualityTests_cff.py
+++ HLTriggerOffline/Tau/python/Validation/HLTTauQualityTests_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from HLTriggerOffline.Tau.Validation.HLTTauQualityTests_cfi import *
 
-hltTauRelvalQualityTests = cms.Sequence(hltTauQualityTesterForZ8E29)
+hltTauRelvalQualityTests = cms.Sequence(hltTauQualityTesterForZ6E31)
 
                                         
