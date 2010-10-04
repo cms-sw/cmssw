f67a908f42f809adfa5de83a78e360866b740d41
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-10-04 21:28:03 +0000

    and this one is for 39X compatibility

--- PhysicsTools/PatExamples/plugins/JetEnergyShift.cc
+++ PhysicsTools/PatExamples/plugins/JetEnergyShift.cc
@@ -1,5 +1,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 /**
    \class   JetEnergyShift JetEnergyShift.h "PhysicsTools/PatExamples/plugins/JetEnergyShift.h"
