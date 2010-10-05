02e500d04146311bc377754b9ed9ac15a34ec929
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-05 16:04:09 +0000

    Return ptrs instead of by value

--- RecoTauTag/RecoTau/interface/RecoTauPiZeroPlugins.h
+++ RecoTauTag/RecoTau/interface/RecoTauPiZeroPlugins.h
@@ -6,7 +6,7 @@
  *
  * Author: Evan K. Friis (UC Davis)
  *
- * Base classes for plugins that construct and rank RecoTauPiZero 
+ * Base classes for plugins that construct and rank RecoTauPiZero
  * objects from a jet.  The builder plugin has an abstract function
  * that takes a PFJet and returns a list of reconstructed photons in
  * the jet.
@@ -18,9 +18,10 @@
  * $Id $
  */
 
+#include <vector>
+#include <boost/ptr_container/ptr_vector.hpp>
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
 #include "RecoTauTag/RecoTau/interface/RecoTauPluginsCommon.h"
-#include <vector>
 
 namespace reco {
 // Forward declarations
@@ -30,24 +31,31 @@ namespace tau {
 
 class RecoTauPiZeroBuilderPlugin : public RecoTauNamedPlugin {
   public:
+    // Return a vector of pointers
+    typedef boost::ptr_vector<RecoTauPiZero> PiZeroVector;
+    // Storing the result in an auto ptr on function return allows
+    // allows us to safely release the ptr_vector in the virtual function
+    typedef std::auto_ptr<PiZeroVector> return_type;
     explicit RecoTauPiZeroBuilderPlugin(const edm::ParameterSet& pset):
-      RecoTauNamedPlugin(pset) {}
+        RecoTauNamedPlugin(pset) {}
     virtual ~RecoTauPiZeroBuilderPlugin() {}
     /// Build a collection of piZeros from objects in the input jet
-    virtual std::vector<RecoTauPiZero> operator()(const PFJet&) const = 0;
+    virtual return_type operator()(const PFJet&) const = 0;
 };
 
 class RecoTauPiZeroQualityPlugin : public RecoTauNamedPlugin {
   public:
     explicit RecoTauPiZeroQualityPlugin(const edm::ParameterSet& pset):
-      RecoTauNamedPlugin(pset) {}
+        RecoTauNamedPlugin(pset) {}
     virtual ~RecoTauPiZeroQualityPlugin() {}
     /// Return a number indicating the quality of this PiZero
     virtual double operator()(const RecoTauPiZero&) const = 0;
 };
-}} // end namespace reco::tau
+}}  // end namespace reco::tau
 
 #include "FWCore/PluginManager/interface/PluginFactory.h"
-typedef edmplugin::PluginFactory<reco::tau::RecoTauPiZeroQualityPlugin* (const edm::ParameterSet&)> RecoTauPiZeroQualityPluginFactory;
-typedef edmplugin::PluginFactory<reco::tau::RecoTauPiZeroBuilderPlugin* (const edm::ParameterSet&)> RecoTauPiZeroBuilderPluginFactory;
+typedef edmplugin::PluginFactory<reco::tau::RecoTauPiZeroQualityPlugin*
+(const edm::ParameterSet&)> RecoTauPiZeroQualityPluginFactory;
+typedef edmplugin::PluginFactory<reco::tau::RecoTauPiZeroBuilderPlugin*
+(const edm::ParameterSet&)> RecoTauPiZeroBuilderPluginFactory;
 #endif
