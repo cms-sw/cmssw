9c83459b7c80f18770f7ed4edb2850a59f442dc2
Dmytro Volyanskyy <sha1-ee228e6b0533b5d2566d9787bda6c58e75d066a4@cern.ch>
2010-10-06 14:39:47 +0000

    remove ME within the endJob

--- DQM/CastorMonitor/src/CastorMonitorModule.cc
+++ DQM/CastorMonitor/src/CastorMonitorModule.cc
@@ -11,7 +11,7 @@
 //**************************************************************//
 ////---- simple event filter which directs events to monitoring tasks: 
 ////---- access unpacked data from each event and pass them to monitoring tasks 
-////---- last revision: 05.03.2010 
+////---- last revision: 06.10.2010 
 
 //==================================================================//
 //======================= Constructor ==============================//
@@ -299,8 +299,9 @@ void CastorMonitorModule::endRun(const edm::Run& r, const edm::EventSetup& conte
 //========================== endJob ===============================//
 //=================================================================//
 void CastorMonitorModule::endJob(void) {
-  if ( meStatus_ ) meStatus_->Fill(2);
-
+ 
+  //if ( meStatus_ ) meStatus_->Fill(2);
+ 
   if(RecHitMon_!=NULL) RecHitMon_->done();
   if(DigiMon_!=NULL) DigiMon_->done();
   if(LedMon_!=NULL) LedMon_->done();
