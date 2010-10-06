c1110b96e07ccb3200cae509403d93e4cdc1abdd
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-10-06 09:45:09 +0000

    support @something in ALCA: step

---
+++ Configuration/PyReleaseValidation/python/autoAlca.py
@@ -0,0 +1,4 @@
+autoAlca = { 'Tier0' : 'SiStripCalZeroBias+TkAlMinBias+TkAlMuonIsolated+MuAlCalIsolatedMu+MuAlOverlaps+HcalCalIsoTrk+HcalCalDijets+SiStripCalMinBias+EcalCalElectron+DtCalib+TkAlJpsiMuMu+TkAlUpsilonMuMu+TkAlZMuMu',
+             'Mu' : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu',
+             'Electron' : 'EcalCalElectron'
+             }
