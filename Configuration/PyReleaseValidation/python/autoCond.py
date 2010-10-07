2216bab1d78d354006603e938fa48540561dac3b
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-10-07 13:25:59 +0000

    Update MC GTs for 39X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
 
-autoCond = { 'mc'      : 'MC_39Y_V1::All',
-             'startup' : 'START39_V1::All',
+autoCond = { 'mc'      : 'MC_39Y_V2::All',
+             'startup' : 'START39_V2::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
              'com10'   : 'GR_R_39X_V1::All',
