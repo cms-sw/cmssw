3970c2e4604ea8ec61a0d68ee0001ec52b8c67fd
Maria Cepeda Hermida <sha1-4a727c1fdf5a92d567ee4b2317070045307b41b3@cern.ch>
2010-10-07 09:16:37 +0000

    Changed trigger (due to preescale), plus modifications in the histograms (added number of PVs, removed most of the MuonID plots for redundancy)

--- DQM/Physics/python/ewkMuDQM_cfi.py
+++ DQM/Physics/python/ewkMuDQM_cfi.py
@@ -6,30 +6,31 @@ ewkMuDQM = cms.EDAnalyzer("EwkMuDQM",
       TrigTag = cms.untracked.InputTag("TriggerResults::HLT"),
       MuonTag = cms.untracked.InputTag("muons"),
       METTag = cms.untracked.InputTag("pfMet"),
-      METIncludesMuons = cms.untracked.bool(True),
-#      JetTag = cms.untracked.InputTag("antikt5CaloJets"),
-      JetTag = cms.untracked.InputTag("ak5CaloJets"),
-
+      JetTag = cms.untracked.InputTag("ak5PFJets"),
+      VertexTag= cms.untracked.InputTag("offlinePrimaryVertices"),
 
       # Main cuts ->
-      MuonTrig = cms.untracked.string("HLT_Mu9"),
+      MuonTrig = cms.untracked.vstring("HLT_Mu9","HLT_Mu11","HLT_Mu15"),
       UseTrackerPt = cms.untracked.bool(True),
       PtCut = cms.untracked.double(20.0),
       EtaCut = cms.untracked.double(2.1),
       IsRelativeIso = cms.untracked.bool(True),
       IsCombinedIso = cms.untracked.bool(True),
       IsoCut03 = cms.untracked.double(0.15),
-      MtMin = cms.untracked.double(40.0),
-      MtMax = cms.untracked.double(200.0),
+      MtMin = cms.untracked.double(50.0),
+      MtMax = cms.untracked.double(9999999),
       MetMin = cms.untracked.double(-999999.),
       MetMax = cms.untracked.double(999999.),
-      AcopCut = cms.untracked.double(2.),
+      AcopCut = cms.untracked.double(999.),
 
       # Muon quality cuts ->
-      DxyCut = cms.untracked.double(0.5),
-      NormalizedChi2Cut = cms.untracked.double(10.),
-      TrackerHitsCut = cms.untracked.int32(11),
+      DxyCut = cms.untracked.double(0.2), # dxy < 0.2 cm (cosmics)
+      NormalizedChi2Cut = cms.untracked.double(10.), # chi2/ndof < 10. 
+      TrackerHitsCut = cms.untracked.int32(11),  # Hits in inner track > 10
+      PixelHitsCut = cms.untracked.int32(1),  # Pixel Hits  > 0 
+      MuonHitsCut = cms.untracked.int32(1),  # Valid Muon Hits  > 0 
       IsAlsoTrackerMuon = cms.untracked.bool(True),
+      NMatchesCut = cms.untracked.int32(2),  # At least 2 Chambers matched with segments
 
       # To suppress Zmm ->
       PtThrForZ1 = cms.untracked.double(20.0),
