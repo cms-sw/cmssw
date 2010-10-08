49c0069f3966f42a6fdb5c674e1c3fe57b1522e2
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-10-08 15:03:49 +0000

    Update the matrix

--- Configuration/PyReleaseValidation/python/autoAlca.py
+++ Configuration/PyReleaseValidation/python/autoAlca.py
@@ -1,4 +1,12 @@
-autoAlca = { 'Tier0' : 'SiStripCalZeroBias+TkAlMinBias+TkAlMuonIsolated+MuAlCalIsolatedMu+MuAlOverlaps+HcalCalIsoTrk+HcalCalDijets+SiStripCalMinBias+EcalCalElectron+DtCalib+TkAlJpsiMuMu+TkAlUpsilonMuMu+TkAlZMuMu',
-             'Mu' : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu',
-             'Electron' : 'EcalCalElectron'
+autoAlca = { 'AllForPrompt'   : 'SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias+HcalCalIsoTrk+MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu+TkAlJPsiMuMu+TkAlUpsilonMuMu+EcalCalElectron',
+             'StreamExpress'  : 'SiStripCalZeroBias+TkAlMinBias+MuAlCalIsolatedMu+DtCalib',
+             'MinimumBias'    : 'SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias',
+             'Commissioning'  : 'HcalCalIsoTrk',
+             'Mu'             : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu',
+             'MuOnia'         : 'TkAlJPsiMuMu+TkAlUpsilonMuMu',
+             'Electron'       : 'EcalCalElectron',
+             'Cosmics'        : 'TkAlBeamHalo+TkAlCosmics0T+MuAlGlobalCosmics+MuAlCalIsolatedMu+HcalCalHOCosmics',
+             'AlCaP0'         : 'EcalCalPi0Calib+EcalCalEtaCalib',
+             'AlCaPhiSymEcal' : 'EcalCalPhiSym',
+             'HcalNZS'        : 'HcalCalMinBias'
              }
