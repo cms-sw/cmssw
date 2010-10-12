e727187f0707c234848adca8e8bc5e8670d4748d
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-10-12 14:49:53 +0000

    Updating to the new naming conventions for global tags

--- JetMETCorrections/Modules/test/uploadConstants.py
+++ JetMETCorrections/Modules/test/uploadConstants.py
@@ -13,6 +13,7 @@ p1 = re.compile(r'TAGNAME')
 p2 = re.compile(r'PRODNAME')
 
 #******************   definitions  **********************************
+jec_type    = 'JetCorrectorParametersCollection'
 ERA         = 'Spring10'
 ALGO_LIST   = ['IC5Calo','IC5PF',
                'AK5Calo','AK5PF','AK5JPT','AK5TRK',
@@ -27,11 +28,11 @@ files = []
 
 ### L2+L3 Corrections
 for aa in ALGO_LIST: #loop for jet algorithms
-    s1 = aa
+    s1 = jec_type + '_' + ERA + '_' + aa
     s2 = s1
     k1 = p1.sub( s1, fileContents )
     k2 = p2.sub( s2, k1 )
-    k2outfile = ERA + '_' + s1 + '.txt'
+    k2outfile = s1 + '.txt'
     print '--------------------------------------'
     print 'ORCOFF File for jet correction : ' + s1
     print 'Written to ' + k2outfile
@@ -41,6 +42,8 @@ for aa in ALGO_LIST: #loop for jet algorithms
     
 
 
-for file in files :
-    subprocess.call( ["./dropBoxOffline_test.sh", "JEC_"+ERA+".db", file])
+for ifile in files :
+    s = "./dropBoxOffline_test.sh JEC_"+ERA+".db " + ifile
+    print s
+    subprocess.call( ["./dropBoxOffline_test.sh", "JEC_"+ERA+".db", ifile])
   
