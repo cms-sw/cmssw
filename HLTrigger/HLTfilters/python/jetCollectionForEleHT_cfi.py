8ab2c9df7d90a75da74a4e53186d496807bcaef0
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-10-13 10:26:43 +0000

    simplify names

--- HLTrigger/HLTfilters/python/jetCollectionForEleHT_cfi.py
+++ HLTrigger/HLTfilters/python/jetCollectionForEleHT_cfi.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-
-hltMCJetCorJetIcone5HF07EleRemoved = cms.EDProducer('JetCollectionForEleHT',
-                                            HltElectronTag = cms.InputTag("hltL1NonIsoHLTNonIsoSingleElectronEt10HT70PixelMatchFilter"),
-                                            SourceJetTag = cms.InputTag("hltMCJetCorJetIcone5HF07"),
-                                            minDeltaR = cms.double(0.5)
+jetCollectionForEleHT = cms.EDProducer('JetCollectionForEleHT',
+    HltElectronTag = cms.InputTag("triggerFilterObjectWithRefs"),
+    SourceJetTag   = cms.InputTag("caloJetCollection"),
+    minDeltaR      = cms.double(0.5)
 )
