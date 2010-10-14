d72a934155c73736c9a1937ccb3e5d0d37e11d1c
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-14 15:58:56 +0000

    Add HPS selection discriminator

---
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByHPSSelection_cfi.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
+
+hpsSelectionDiscriminator = cms.EDProducer(
+    "PFRecoTauDiscriminationByHPSSelection",
+    PFTauProducer = cms.InputTag('combinatoricRecoTaus'), 
+    Prediscriminants = noPrediscriminants,
+    matchingCone = cms.double(0.1),
+    coneSizeFormula = cms.string("max(min(0.1, 2.8/et()),0.05)"),
+    decayModes = cms.VPSet(
+        cms.PSet(
+            nCharged = cms.uint32(1),
+            nPiZeros = cms.uint32(0),
+            # Always passes
+            minMass = cms.double(0),
+            maxMass = cms.double(1),
+        ),
+        cms.PSet(
+            nCharged = cms.uint32(1),
+            nPiZeros = cms.uint32(1),
+            minMass = cms.double(0.3),
+            maxMass = cms.double(1.3),
+        ),
+        cms.PSet(
+            nCharged = cms.uint32(1),
+            nPiZeros = cms.uint32(2),
+            minMass = cms.double(0.4),
+            maxMass = cms.double(1.2),
+        ),
+        cms.PSet(
+            nCharged = cms.uint32(3),
+            nPiZeros = cms.uint32(0),
+            minMass = cms.double(0.8),
+            maxMass = cms.double(1.5),
+        ),
+    )
+)
+
+
+
