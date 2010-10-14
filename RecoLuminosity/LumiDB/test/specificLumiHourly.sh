5577b9ac08912b8e914931b12036bb86ab620f36
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2010-10-14 13:56:24 +0000

    added

---
+++ RecoLuminosity/LumiDB/test/specificLumiHourly.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+currendir=`pwd`
+workdir="/build1/zx/cron/CMSSW_3_7_0_pre3"
+authdir="/afs/cern.ch/user/x/xiezhen"
+indir="/afs/cern.ch/cms/lumi/lhcfills"
+outdir="/afs/cern.ch/cms/lumi/specificlumi"
+logpath="/afs/cern.ch/cms/lumi/"
+dbConnectionString="oracle://cms_orcoff_prod/cms_lumi_prod"
+source /afs/cern.ch/cms/cmsset_default.sh;
+cd $workdir
+eval `scramv1 runtime -sh`
+touch "$logpath/specificLumiHourly.log"
+date >> "$logpath/specificLumiHourly.log"
+lumiPlotFiller.py -c $dbConnectionString -P $authdir -i $indir -o $outdir >> "$logpath/specificlumiHourly.log" ;
+sleep 1
+date >> "$logpath/specificLumiHourly.log"
+cd $currentdir
