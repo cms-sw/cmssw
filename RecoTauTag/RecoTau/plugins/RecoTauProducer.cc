f03376179c1700ad8dca1e5f22ca778c36981116
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-14 10:05:02 +0000

    Take generic view of candidates as input, and style cleanup

--- RecoTauTag/RecoTau/plugins/RecoTauProducer.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauProducer.cc
@@ -1,4 +1,5 @@
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/foreach.hpp>
 
 #include <algorithm>
 #include <functional>
@@ -10,13 +11,14 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "RecoTauTag/RecoTau/interface/RecoTauBuilderPlugins.h"
+#include "RecoTauTag/RecoTau/interface/RecoTauCommonUtilities.h"
 
 #include "DataFormats/JetReco/interface/PFJetCollection.h"
 #include "DataFormats/TauReco/interface/JetPiZeroAssociation.h"
 #include "DataFormats/TauReco/interface/PFTau.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
 
-class RecoTauProducer : public edm::EDProducer
-{
+class RecoTauProducer : public edm::EDProducer {
   public:
     typedef reco::tau::RecoTauBuilderPlugin Builder;
     typedef reco::tau::RecoTauModifierPlugin Modifier;
@@ -24,7 +26,7 @@ class RecoTauProducer : public edm::EDProducer
     typedef boost::ptr_vector<Modifier> ModifierList;
 
     explicit RecoTauProducer(const edm::ParameterSet& pset);
-    ~RecoTauProducer(){};
+    ~RecoTauProducer() {}
     void produce(edm::Event& evt, const edm::EventSetup& es);
 
   private:
@@ -34,51 +36,55 @@ class RecoTauProducer : public edm::EDProducer
     ModifierList modifiers_;
 };
 
-RecoTauProducer::RecoTauProducer(const edm::ParameterSet& pset)
-{
+RecoTauProducer::RecoTauProducer(const edm::ParameterSet& pset) {
   jetSrc_ = pset.getParameter<edm::InputTag>("jetSrc");
   piZeroSrc_ = pset.getParameter<edm::InputTag>("piZeroSrc");
 
   typedef std::vector<edm::ParameterSet> VPSet;
   // Get each of our tau builders
   const VPSet& builders = pset.getParameter<VPSet>("builders");
-  for(VPSet::const_iterator builderPSet = builders.begin();
+  for (VPSet::const_iterator builderPSet = builders.begin();
       builderPSet != builders.end(); ++builderPSet) {
     // Get plugin name
-    const std::string& pluginType = builderPSet->getParameter<std::string>("plugin");
+    const std::string& pluginType =
+        builderPSet->getParameter<std::string>("plugin");
     // Build the plugin
-    builders_.push_back(RecoTauBuilderPluginFactory::get()->create(pluginType, *builderPSet));
+    builders_.push_back(
+        RecoTauBuilderPluginFactory::get()->create(pluginType, *builderPSet));
   }
 
   const VPSet& modfiers = pset.getParameter<VPSet>("modifiers");
-  for(VPSet::const_iterator modfierPSet = modfiers.begin();
-      modfierPSet != modfiers.end(); ++modfierPSet)
-  {
+  for (VPSet::const_iterator modfierPSet = modfiers.begin();
+      modfierPSet != modfiers.end(); ++modfierPSet) {
     // Get plugin name
-    const std::string& pluginType = modfierPSet->getParameter<std::string>("plugin");
+    const std::string& pluginType =
+        modfierPSet->getParameter<std::string>("plugin");
     // Build the plugin
-    modifiers_.push_back(RecoTauModifierPluginFactory::get()->create(pluginType, *modfierPSet));
+    modifiers_.push_back(
+        RecoTauModifierPluginFactory::get()->create(pluginType, *modfierPSet));
   }
-
   produces<reco::PFTauCollection>();
 }
 
-void RecoTauProducer::produce(edm::Event& evt, const edm::EventSetup& es)
-{
-  // Get the jet input collection
-  edm::Handle<reco::PFJetCollection> pfJets;
-  evt.getByLabel(jetSrc_, pfJets);
+void RecoTauProducer::produce(edm::Event& evt, const edm::EventSetup& es) {
+  // Get the jet input collection via a view of Candidates
+  edm::Handle<reco::CandidateView> jetView;
+  evt.getByLabel(jetSrc_, jetView);
+
+  // Convert to a vector of PFJetRefs
+  reco::PFJetRefVector jets =
+      reco::tau::castView<reco::PFJetRefVector>(*jetView);
 
   // Get the pizero input collection
   edm::Handle<reco::JetPiZeroAssociation> piZeroAssoc;
   evt.getByLabel(piZeroSrc_, piZeroAssoc);
 
   // Update all our builders and modifiers with the event info
-  for(BuilderList::iterator builder = builders_.begin();
+  for (BuilderList::iterator builder = builders_.begin();
       builder != builders_.end(); ++builder) {
     builder->setup(evt, es);
   }
-  for(ModifierList::iterator modifier = modifiers_.begin();
+  for (ModifierList::iterator modifier = modifiers_.begin();
       modifier != modifiers_.end(); ++modifier) {
     modifier->setup(evt, es);
   }
@@ -86,37 +92,29 @@ void RecoTauProducer::produce(edm::Event& evt, const edm::EventSetup& es)
   // Create output collection
   std::auto_ptr<reco::PFTauCollection> output(new reco::PFTauCollection());
 
-
   // Loop over the jets and build the taus for each jet
-  const size_t nJets = pfJets->size();
-  for(size_t iJet = 0; iJet < nJets; ++iJet)
-  {
-    // Get a Ref to the PFJet
-    const reco::PFJetRef jetRef(pfJets, iJet);
-
+  BOOST_FOREACH(reco::PFJetRef jetRef, jets) {
     // Get the PiZeros associated with this jet
     const std::vector<reco::RecoTauPiZero>& piZeros = (*piZeroAssoc)[jetRef];
-
     // Loop over our builders and create the set of taus for this jet
-    for(BuilderList::const_iterator builder = builders_.begin();
-        builder != builders_.end(); ++builder)
-    {
+    for (BuilderList::const_iterator builder = builders_.begin();
+        builder != builders_.end(); ++builder) {
       std::vector<reco::PFTau> taus((*builder)(jetRef, piZeros));
+      // Grow the vector if necessary
+      output->reserve(output->size() + taus.size());
       // Ensure the jetRef is set correctly
       output->insert(output->end(), taus.begin(), taus.end());
     }
-
   }
 
   // Loop over the taus we have created and apply our modifiers to the taus
-  for(reco::PFTauCollection::iterator tau = output->begin();
+  for (reco::PFTauCollection::iterator tau = output->begin();
       tau != output->end(); ++tau) {
-    for(ModifierList::const_iterator modifier = modifiers_.begin();
+    for (ModifierList::const_iterator modifier = modifiers_.begin();
         modifier != modifiers_.end(); ++modifier) {
       (*modifier)(*tau);
     }
   }
-
   evt.put(output);
 }
 
