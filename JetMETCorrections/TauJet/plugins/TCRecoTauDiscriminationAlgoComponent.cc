74b249775898f9a04bcd8233fccee42baba3a4e5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-10-17 18:31:13 +0000

    Add needed namespace qualifiers

--- JetMETCorrections/TauJet/plugins/TCRecoTauDiscriminationAlgoComponent.cc
+++ JetMETCorrections/TauJet/plugins/TCRecoTauDiscriminationAlgoComponent.cc
@@ -8,20 +8,22 @@
 #include "RecoTauTag/RecoTau/interface/TauDiscriminationProducerBase.h"
 #include "RecoTauTag/RecoTau/interface/TCTauAlgorithm.h"
 
+using namespace reco;
+
 class TCRecoTauDiscriminationAlgoComponent : public CaloTauDiscriminationProducerBase {
     public:
-      	explicit TCRecoTauDiscriminationAlgoComponent(const ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
+      	explicit TCRecoTauDiscriminationAlgoComponent(const edm::ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
 		tcTauAlgorithm = new TCTauAlgorithm(iConfig);
       	}
       	~TCRecoTauDiscriminationAlgoComponent(){} 
       	double discriminate(const CaloTauRef& theCaloTauRef);
-	void beginEvent(const Event&, const EventSetup&);
+	void beginEvent(const edm::Event&, const edm::EventSetup&);
 
     private:
 	TCTauAlgorithm*  tcTauAlgorithm;
 };
 
-void TCRecoTauDiscriminationAlgoComponent::beginEvent(const Event& iEvent, const EventSetup& iSetup){
+void TCRecoTauDiscriminationAlgoComponent::beginEvent(const edm::Event& iEvent, const edm::EventSetup& iSetup){
 	tcTauAlgorithm->eventSetup(iEvent,iSetup);
 }
 
