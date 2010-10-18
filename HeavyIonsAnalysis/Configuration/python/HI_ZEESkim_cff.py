e0bedc2696eaa74339cf6ce56b4df86f43a48f24
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-10-18 12:29:24 +0000

    use reco::Photon instead of superclusters for ZEE skim

--- HeavyIonsAnalysis/Configuration/python/HI_ZEESkim_cff.py
+++ HeavyIonsAnalysis/Configuration/python/HI_ZEESkim_cff.py
@@ -7,43 +7,28 @@ hltPhotonHI.HLTPaths = ["HLT_HIPhoton15"]
 hltPhotonHI.throw = False
 hltPhotonHI.andOr = True
 
-# selection of good superclusters
-superClusterMerger =  cms.EDProducer("EgammaSuperClusterMerger",
-  src = cms.VInputTag(
-    cms.InputTag('correctedHybridSuperClusters'),
-    cms.InputTag('correctedMulti5x5SuperClustersWithPreshower')
-  )
+# photon selection
+goodPhotons = cms.EDFilter("PhotonSelector",
+    src = cms.InputTag("photons"),
+    cut = cms.string('et > 20 && hadronicOverEm < 0.1 && r9 > 0.8')
 )
 
-superClusterCands = cms.EDProducer("ConcreteEcalCandidateProducer",
-  src = cms.InputTag("superClusterMerger"),
-  particleType = cms.string('gamma')
-)
-
-goodSuperClusters = cms.EDFilter("CandViewRefSelector",
-  src = cms.InputTag("superClusterCands"),
-  cut = cms.string('et > 20.0'),
-  filter = cms.bool(True)
-)
-
-# select supercluster pairs around Z mass
-superClusterCombiner = cms.EDFilter("CandViewShallowCloneCombiner",
+# select pairs around Z mass
+photonCombiner = cms.EDFilter("CandViewShallowCloneCombiner",
   checkCharge = cms.bool(False),
   cut = cms.string('60 < mass < 120'),
-  decay = cms.string('goodSuperClusters goodSuperClusters')
+  decay = cms.string('goodPhotons goodPhotons')
 )
 
-superClusterPairCounter = cms.EDFilter("CandViewCountFilter",
-  src = cms.InputTag("superClusterCombiner"),
+photonPairCounter = cms.EDFilter("CandViewCountFilter",
+  src = cms.InputTag("photonCombiner"),
   minNumber = cms.uint32(1)
 )
 
 # Z->ee skim sequence
 zEESkimSequence = cms.Sequence(
     hltPhotonHI *
-    superClusterMerger *
-    superClusterCands *
-    goodSuperClusters *
-    superClusterCombiner *
-    superClusterPairCounter
+    goodPhotons *
+    photonCombiner *
+    photonPairCounter
     )
