7f5f575c510c62c039a29a90a2acabe5b7a7398f
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2010-10-20 12:32:04 +0000

    Add the module for track based conversion validation

--- Validation/RecoEgamma/src/SealModule.cc
+++ Validation/RecoEgamma/src/SealModule.cc
@@ -4,6 +4,8 @@
 
 #include "Validation/RecoEgamma/interface/EgammaObjects.h"
 #include "Validation/RecoEgamma/interface/PhotonValidator.h"
+#include "Validation/RecoEgamma/interface/TkConvValidator.h"
+#include "Validation/RecoEgamma/interface/ConversionPostprocessing.h"
 #include "Validation/RecoEgamma/interface/PhotonPostprocessing.h"
 #include "Validation/RecoEgamma/interface/ElectronMcSignalValidator.h"
 #include "Validation/RecoEgamma/interface/ElectronMcFakeValidator.h"
@@ -12,7 +14,9 @@
 
 DEFINE_FWK_MODULE(EgammaObjects);
 DEFINE_FWK_MODULE(PhotonValidator);
+DEFINE_FWK_MODULE(TkConvValidator);
 DEFINE_FWK_MODULE(PhotonPostprocessing);
+DEFINE_FWK_MODULE(ConversionPostprocessing);
 DEFINE_FWK_MODULE(ElectronMcSignalValidator);
 DEFINE_FWK_MODULE(ElectronMcFakeValidator);
 
