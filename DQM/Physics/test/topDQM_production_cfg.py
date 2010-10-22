5f0562d5adbe9170b9c8c566d756cd47d56f60f8
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-10-22 15:34:51 +0000

    droped unnecessary content to facilitate file handling, update of GTs and skims -- some cosmetics

--- DQM/Physics/test/topDQM_production_cfg.py
+++ DQM/Physics/test/topDQM_production_cfg.py
@@ -12,16 +12,24 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 ##
 ## For CMSSW_3_8_X MC use             ---> 'START38_V12::All'
 ## For Data (38X re-processing) use   ---> 'GR_R_38X_V13::All'
+## For Data (38X prompt reco) use     ---> 'GR10_P_V10::All'
 ##
 ## For more details have a look at: WGuideFrontierConditions
 ## --------------------------------------------------------------------
-process.GlobalTag.globaltag = 'START38_V12::All'
+##process.GlobalTag.globaltag = 'START38_V12::All'
 ##process.GlobalTag.globaltag = 'GR_R_38X_V13::All' 
+process.GlobalTag.globaltag   = 'GR10_P_V10::All'
 
 ## input file(s) for testing
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-     '/store/relval/CMSSW_3_8_4/RelValTTbar/GEN-SIM-RECO/START38_V12-v1/0024/1A650A81-83C2-DF11-B355-002618FDA287.root'
+     '/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/2ABE8934-E7D1-DF11-9985-003048678C9A.root'
+    #,'/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/740F4118-E3D1-DF11-90EF-001A92971B32.root'
+    #,'/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/80C7AB1A-E4D1-DF11-AFF2-00261894388D.root'
+    #,'/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/829A469A-E5D1-DF11-B64A-001A92810AC0.root'
+    #,'/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/D06EF8B3-E3D1-DF11-9F56-001A92971BA0.root'
+    #,'/store/relval/CMSSW_3_8_5/RelValTTbar/GEN-SIM-RECO/MC_38Y_V12-v1/0039/ECB3B1FA-E1D1-DF11-87E5-003048678AE2.root'
+    #'/store/relval/CMSSW_3_8_4/RelValTTbar/GEN-SIM-RECO/START38_V12-v1/0024/1A650A81-83C2-DF11-B355-002618FDA287.root'
     )
 )
 
@@ -39,7 +47,12 @@ process.load("DQM.Physics.topElectronID_cff")
 ## output
 process.output = cms.OutputModule("PoolOutputModule",
   fileName       = cms.untracked.string('topDQM_production.root'),
-  outputCommands = cms.untracked.vstring('drop *_*_*_*','keep *_*_*_TOPDQM'),
+  outputCommands = cms.untracked.vstring(
+    'drop *_*_*_*',
+    'keep *_*_*_TOPDQM',
+    'drop *_TriggerResults_*_TOPDQM',
+    'drop *_simpleEleId70cIso_*_TOPDQM',
+  ),
   splitLevel     = cms.untracked.int32(0),
   dataset = cms.untracked.PSet(
     dataTier   = cms.untracked.string(''),
