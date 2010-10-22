cc1e2fa84a3c9d9529ed85e6eb9fd714341a19a5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-10-22 12:03:55 +0000

    one more new algo

--- RecoVertex/KinematicFit/interface/ColinearityKinematicConstraintT.h
+++ RecoVertex/KinematicFit/interface/ColinearityKinematicConstraintT.h
@@ -104,19 +104,23 @@ ColinearityKinematicConstraintT<Dim>::value() const
   double p1vx = p1(3) - a_1*(point.y() - p1(1));
   double p1vy = p1(4) + a_1*(point.x() - p1(0));
   double pt1  = sqrt(p1(3)*p1(3)+p1(4)*p1(4));
-  double pTot1  = sqrt(p1(3)*p1(3)+p1(4)*p1(4)+p1(5)*p1(5));
-
+ 
   double p2vx = p2(3) - a_2*(point.y() - p2(1));
   double p2vy = p2(4) + a_2*(point.x() - p2(0));
   double pt2  = sqrt(p2(3)*p2(3)+p2(4)*p2(4));
-  double pTot2   = sqrt(p2(3)*p2(3)+p2(4)*p2(4)+p2(5)*p2(5));
+  
 
   // H_phi:
-  res(0)  = ((p1vx*p2vx) + (p1vy* p2vy))/(pt1*pt2) - 1 ;
+  res(0)  = atan2(p1vy,p1vx) - atan2(p2vy,p2vx);
+  if ( res(0) >  M_PI ) res(1) -= 2.0*M_PI;
+  if ( res(0) <= -M_PI ) res(1) += 2.0*M_PI;
   // H_theta:
-  if (Dim==colinearityKinematic::PhiTheta) 
-    res(1)  = ((pt1*pt2) + (p1(5)*p2(5)))/(pTot1*pTot2) - 1;
-
+  if (dimension == PhiTheta) {  
+    res(1)  = atan2(pt1,p1(5)) - atan2(pt2,p2(5));
+    if ( res(1) >  M_PI ) res(2) -= 2.0*M_PI;
+    if ( res(1) <= -M_PI ) res(2) += 2.0*M_PI;
+  }
+  
   return res;
 }
 
@@ -128,42 +132,44 @@ ColinearityKinematicConstraintT<Dim>::parametersDerivative() const
 
   double p1vx = p1(3) - a_1*(point.y() - p1(1));
   double p1vy = p1(4) + a_1*(point.x() - p1(0));
+  double k1 = 1.0/(p1vx*p1vx + p1vy*p1vy);
   double pt1 = sqrt(p1(3)*p1(3)+p1(4)*p1(4));
   double pTot1  = sqrt(p1(3)*p1(3)+p1(4)*p1(4)+p1(5)*p1(5));
 
   double p2vx = p2(3) - a_2*(point.y() - p2(1));
   double p2vy = p2(4) + a_2*(point.x() - p2(0));
+  double k2 = 1.0/(p2vx*p2vx + p2vy*p2vy);
   double pt2  = sqrt(p2(3)*p2(3)+p2(4)*p2(4));
   double pTot2   = sqrt(p2(3)*p2(3)+p2(4)*p2(4)+p2(5)*p2(5));
 
   // H_phi:
 
   //x1 and x2 derivatives: 1st and 8th elements
-  res(0,0) =  -a_1*p2vy/(pt1*pt2);
-  res(0,7) =  -a_2*p1vy/(pt1*pt2);
+  res(0,0) =  -k1*a_1*p1vx;
+  res(0,7) =   k2*a_2*p2vx;
 
   //y1 and y2 derivatives: 2nd and 9th elements:
-  res(0,1) = a_1*p2vx/(pt1*pt2);
-  res(0,8) = a_2*p1vx/(pt1*pt2);
+  res(0,1) = -k1*a_1*p1vy;
+  res(0,8) =  k2*a_2*p2vy;
 
   //z1 and z2 components: 3d and 10th elmnets stay 0:
   res(0,2)  = 0.; res(1,10) = 0.;
 
   //px1 and px2 components: 4th and 11th elements:
-  res(0,3)  = ( pt1*pt1*p2vx - p1(3)*(p1vy*p2vy + p1vx*p2vx))/(pt1*pt1*pt1*pt2);
-  res(0,10) = ( pt2*pt2*p1vx - p2(3)*(p1vy*p2vy + p1vx*p2vx))/(pt1*pt2*pt2*pt2);
+  res(0,3)  = -k1*p1vy;
+  res(0,10) =  k2*p2vy;
 
   //py1 and py2 components: 5th and 12 elements:
-  res(0,4)  = ( pt1*pt1*p2vy - p1(4)*(p1vy*p2vy + p1vx*p2vx))/(pt1*pt1*pt1*pt2);
-  res(0,11) = ( pt2*pt2*p1vy - p2(4)*(p1vy*p2vy + p1vx*p2vx))/(pt1*pt2*pt2*pt2);
+  res(0,5)  =  k1*p1vx;
+  res(0,11) = -k2*p2vx;
 
 
   //pz1 and pz2 components: 6th and 13 elements:
-  res(0,5)  = 0.; res(0,12) = 0.;
+  res(1,6)  = 0.; res(1,13) = 0.;
   //mass components: 7th and 14th elements:
-  res(0,6)  = 0.; res(0,13) = 0.;
+  res(1,7)  = 0.; res(1,14) = 0.;
 
-  if (Dim==colinearityKinematic::PhiTheta) {
+  if (dimension == PhiTheta)  {
     // H_theta:
     //x1 and x2 derivatives: 1st and 8th elements
     res(1,0) =  0.; res(1,7) = 0.;
@@ -174,17 +180,16 @@ ColinearityKinematicConstraintT<Dim>::parametersDerivative() const
     //z1 and z2 components: 3d and 10th elmnets stay 0:
     res(1,2) = 0.; res(1,9) = 0.;
 
-    //px1 and px2 components: 4th and 11 elements:
-    res(1,3)  = (p1(3)* p1(5)* (pt2* p1(5) - pt1* p2(5)))/(pt1 *pTot1*pTot1*pTot1*pTot2);
-    res(1,10) = (p2(3)* p2(5)* (pt1* p2(5) - pt2* p1(5)))/(pt2 *pTot1*pTot2*pTot2*pTot2);
+    res(1,3)  =   p1(5)*p1(3) / (pTot1*pTot1*pt1);
+    res(1,10) = - p2(5)*p2(3) / (pTot2*pTot2*pt2);
 
     //py1 and py2 components: 5th and 12 elements:
-    res(1,4)  = (p1(4)* p1(5)* (pt2* p1(5) - pt1* p2(5)))/(pt1 *pTot1*pTot1*pTot1*pTot2);
-    res(1,11) = (p2(4)* p2(5)* (pt1* p2(5) - pt2* p1(5)))/(pt2 *pTot1*pTot2*pTot2*pTot2);
+    res(1,4)  =   p1(5)*p1(4) / (pTot1*pTot1*pt1);
+    res(1,11) = - p2(5)*p2(4) / (pTot2*pTot2*pt2);
 
     //pz1 and pz2 components: 6th and 13 elements:
-    res(1,5)  = (pt1*pt1*p2(5) - p1(5)* pt1* pt2) / (pTot1*pTot1*pTot1*pTot2);
-    res(1,12) = (pt2*pt2*p1(5) - p2(5)* pt1* pt2) / (pTot1*pTot2*pTot2*pTot2);
+    res(1,5)  = - pt1/(pTot1*pTot1);
+    res(1,12) =   pt2/(pTot2*pTot2);
 
     //mass components: 7th and 14th elements:
     res(1,6)  = 0.; res(1,13) = 0.;
@@ -201,19 +206,19 @@ ColinearityKinematicConstraintT<Dim>::positionDerivative() const
 
   double p1vx = p1(3) - a_1*(point.y() - p1(1));
   double p1vy = p1(4) + a_1*(point.x() - p1(0));
-  double pt1 = sqrt(p1(3)*p1(3)+p1(4)*p1(4));
+  double k1 = 1.0/(p1vx*p1vx + p1vy*p1vy);
 
   double p2vx = p2(3) - a_2*(point.y() - p2(1));
   double p2vy = p2(4) + a_2*(point.x() - p2(0));
-  double pt2  = sqrt(p2(3)*p2(3)+p2(4)*p2(4));
-
+  double k2 = 1.0/(p2vx*p2vx + p2vy*p2vy);
+ 
   // H_phi:
 
   // xv component
-  res(0,0) = (a_2*p1vy + a_1*p2vy)/(pt1*pt2);
+  res(0,0) = k1*a_1*p1vx - k2*a_2*p2vx;
 
   //yv component
-  res(0,1) = -(a_2*p1vx + a_1*p2vx)/(pt1*pt2);
+  res(0,1) =  k1*a_1*p1vy - k2*a_2*p2vy;
 
   //zv component
   res(0,2) = 0.;
