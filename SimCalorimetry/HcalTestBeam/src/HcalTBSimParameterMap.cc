17ffcd11073839e41ed9b17fd05928e5f04e0eec
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2010-10-26 13:08:49 +0000

    To match the changes in HcalSimAlgo

--- SimCalorimetry/HcalTestBeam/src/HcalTBSimParameterMap.cc
+++ SimCalorimetry/HcalTestBeam/src/HcalTBSimParameterMap.cc
@@ -5,11 +5,11 @@
   
 
 HcalTBSimParameterMap::HcalTBSimParameterMap() :
-  theHBParameters(2000., 0.3305, 117, 5, 10, 5, 
+  theHBParameters(2000., std::vector<double>(16, 0.3305), 117, 5, 10, 5, 
 		  true, false, 1, std::vector<double>(16, 117.)),
-  theHEParameters(2000., 0.3305, 178, 5, 10, 5, 
+  theHEParameters(2000., std::vector<double>(16, 0.3305), 178, 5, 10, 5, 
 		  true, false, 16, std::vector<double>(16, 178.)),
-  theHOParameters(4000., 0.3065, 217, 5, 10, 5, 
+  theHOParameters(4000., std::vector<double>(16, 0.3065), 217, 5, 10, 5, 
 		  true, false, 1, std::vector<double>(16, 217.)) {}
 
 /*
@@ -21,32 +21,35 @@ HcalTBSimParameterMap::HcalTBSimParameterMap() :
 
 HcalTBSimParameterMap::HcalTBSimParameterMap(const edm::ParameterSet & p) :
   theHBParameters(p.getUntrackedParameter<double>("photomultiplierGainTBHB",2000.),
-		  p.getUntrackedParameter<double>("amplifierGainTBHB",0.3305),
+		  p.getUntrackedParameter<std::vector<double> >("photoelectronsToAnalogTBHB",std::vector<double>(16, 0.3305)),
 		  p.getUntrackedParameter<double>("samplingFactorTBHB",117),
 		  p.getUntrackedParameter<double>("timePhaseTBHB",5),
 		  p.getUntrackedParameter<int>("readoutFrameSizeTB",10),
 		  p.getUntrackedParameter<int>("binOfMaximumTBHB",5),
 		  p.getUntrackedParameter<bool>("doPhotostatisticsTB",true),
 		  p.getUntrackedParameter<bool>("syncPhaseTB",true),
-		  1, std::vector<double>(16, 117.)),
+		  p.getUntrackedParameter<int>("firstRingTBHB",1), 
+                  p.getUntrackedParameter<std::vector<double> >("samplingFactorsTBHB", std::vector<double>(16, 117.)) ),
   theHEParameters(p.getUntrackedParameter<double>("photomultiplierGainTBHE",2000.),
-		  p.getUntrackedParameter<double>("amplifierGainTBHE",0.3305),
+		  p.getUntrackedParameter<std::vector<double> >("photoelectronsToAnalogTBHE",std::vector<double>(16, 0.3305)),
 		  p.getUntrackedParameter<double>("samplingFactorTBHE",178),
 		  p.getUntrackedParameter<double>("timePhaseTBHE",5),
 		  p.getUntrackedParameter<int>("readoutFrameSizeTB",10),
 		  p.getUntrackedParameter<int>("binOfMaximumTBHE",5),
 		  p.getUntrackedParameter<bool>("doPhotostatisticsTB",true),
 		  p.getUntrackedParameter<bool>("syncPhaseTB",true),
-		  16, std::vector<double>(16, 178.)),
+		  p.getUntrackedParameter<int>("firstRingTBHE",16), 
+                  p.getUntrackedParameter<std::vector<double> >("samplingFactorsTBHE", std::vector<double>(16, 178.)) ),
   theHOParameters(p.getUntrackedParameter<double>("photomultiplierGainTBHE",4000.),
-		  p.getUntrackedParameter<double>("amplifierGainTBHO",.3065),
+		  p.getUntrackedParameter<std::vector<double> >("photoelectronsToAnalogTBHO",std::vector<double>(16, 3065)),
 		  p.getUntrackedParameter<double>("samplingFactorTBHO",217),
 		  p.getUntrackedParameter<double>("timePhaseTBHO",5),
 		  p.getUntrackedParameter<int>("readoutFrameSizeTB",10),
 		  p.getUntrackedParameter<int>("binOfMaximumTBHO",5),
 		  p.getUntrackedParameter<bool>("doPhotostatisticsTB",true),
 		  p.getUntrackedParameter<bool>("syncPhaseTB",true),
-		  1, std::vector<double>(16, 217.)) {}
+		  p.getUntrackedParameter<int>("firstRingTBHO",1), 
+                  p.getUntrackedParameter<std::vector<double> >("samplingFactorsTBHO", std::vector<double>(16, 217.)) ) {}
 
 
 const CaloSimParameters & HcalTBSimParameterMap::simParameters(const DetId & detId) const {
