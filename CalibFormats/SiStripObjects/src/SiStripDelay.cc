d491153258b47e0347c1738d181d8820ff9d3860
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2010-10-26 14:57:12 +0000

    Added SiStripDelay

---
+++ CalibFormats/SiStripObjects/src/SiStripDelay.cc
@@ -0,0 +1,127 @@
+// -*- C++ -*-
+//
+// Package:     SiStripObjects
+// Class  :     SiStripDelay
+// Implementation:
+//     <Notes on implementation>
+// Original Author:  M. De Mattia
+//         Created:  26/10/2010
+// $Id: SiStripDelay.cc,v 1.13 2010/04/27 08:06:06 demattia Exp $
+
+#include "FWCore/Utilities/interface/typelookup.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripDelay.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "CondFormats/SiStripObjects/interface/SiStripDetSummary.h"
+#include "CalibTracker/SiStripCommon/interface/SiStripDetInfoFileReader.h"
+#include <sstream>
+
+void SiStripDelay::fillNewDelay(const SiStripBaseDelay& baseDelay, const int sumSign,
+				const std::pair<std::string, std::string> & recordLabelPair)
+{
+  baseDelayVector_.push_back(&baseDelay);
+  sumSignVector_.push_back(sumSign);
+  recordLabelPair_.push_back(recordLabelPair);
+}
+
+float SiStripDelay::getDelay(const uint32_t detId) const
+{
+  boost::unordered_map<uint32_t, double>::const_iterator it = delays_.find(detId);
+  if( it != delays_.end() ) {
+    return it->second;
+  }
+  return 0.;
+}
+
+bool SiStripDelay::makeDelay()
+{
+  if( baseDelayVector_.empty() ) {
+    return false;
+  }
+  std::vector<const SiStripBaseDelay*>::const_iterator it = baseDelayVector_.begin();
+  // Check for consistent size in all baseDelays
+  if( baseDelayVector_.size() > 1 ) {
+    for( ; it != baseDelayVector_.end()-1; ++it ) {
+      if( (*it)->delaysSize() != (*(it+1))->delaysSize() ) {
+	std::cout << "makeDelay: Error, size of base delays is different!!" << std::endl;
+	return false;
+      }
+    }
+  }
+  
+//   // All checks done, fill the boost::unoredered_map with the first one (initialization)
+//   int sumSignIndex = 0;
+//   int sumSign = 0;
+//   std::vector<uint32_t>::const_iterator detIdIt;
+//   for( it = baseDelayVector_.begin(); it != baseDelayVector_.end(); ++it, ++sumSignIndex ) {
+//     std::vector<uint32_t> detIds;
+//     (*it)->detIds(detIds);
+//     sumSign = sumSignVector_[sumSignIndex];
+//     for( detIdIt = detIds.begin(); detIdIt != detIds.end(); ++detIdIt ) {
+//       // Check if is alread there so that we never rely on the default initialization
+//       boost::unordered_map<uint32_t, double>::iterator delayIt = delays_.find(*detIdIt);
+//       if( delayIt != delays_.end() ) {
+// 	std::cout << "second delay = " << (*it)->delay(*detIdIt)*sumSign << std::endl;
+// 	delays_[*detIdIt] += (*it)->delay(*detIdIt)*sumSign;
+// 	std::cout << "Full delay = " << delays_[*detIdIt] << std::endl;
+//       }
+//       else {
+// 	std::cout << "first delay = " << (*it)->delay(*detIdIt)*sumSign << std::endl;
+// 	delays_[*detIdIt] = (*it)->delay(*detIdIt)*sumSign;
+//       }
+//     }
+//   }
+
+  // All checks done, fill the boost::unoredered_map with the first one (initialization)
+  int sumSignIndex = 0;
+  int sumSign = sumSignVector_[sumSignIndex];
+  it = baseDelayVector_.begin();
+  std::vector<uint32_t> detIds;
+  (*it)->detIds(detIds);
+  std::vector<uint32_t>::const_iterator detIdIt = detIds.begin();
+  for( ; detIdIt != detIds.end(); ++detIdIt ) {
+    delays_[*detIdIt] = (*it)->delay(*detIdIt)*sumSign;
+  }
+  ++it;
+  ++sumSignIndex;
+  // Fill all the others
+  for( ; it != baseDelayVector_.end(); ++it, ++sumSignIndex ) {
+    std::vector<uint32_t> detIds;
+    (*it)->detIds(detIds);
+    detIdIt = detIds.begin();
+    sumSign = sumSignVector_[sumSignIndex];
+    for( ; detIdIt != detIds.end(); ++detIdIt ) {
+      // The same detIds should be in both maps, if not don't rely on the default initialization
+      boost::unordered_map<uint32_t, double>::iterator delayIt = delays_.find(*detIdIt);
+      if( delayIt != delays_.end() ) {
+	delays_[*detIdIt] += (*it)->delay(*detIdIt)*sumSign;
+      }
+      else {
+	std::cout << "makeDelay: Warning, detId = " << *detIdIt << " not present, summing to 0..." << std::cout;
+	std::cout << "This means that the two baseDelay tags have different detIds. PLEASE, CHECK THAT THIS IS EXPECTED." << std::cout;
+	delays_[*detIdIt] = (*it)->delay(*detIdIt)*sumSign;
+      }
+    }
+  }
+
+  return true;
+}
+
+void SiStripDelay::clear()
+{
+  baseDelayVector_.clear();
+  sumSignVector_.clear();
+  recordLabelPair_.clear();
+  delays_.clear();
+}
+
+void SiStripDelay::printDebug(std::stringstream & ss) const
+{
+  boost::unordered_map<uint32_t, double>::const_iterator it = delays_.begin();
+  for( ; it != delays_.end(); ++it ) {
+    ss << "detId = " << it->first << " delay = " << it->second << std::endl;
+  }
+}
+
+void SiStripDelay::printSummary(std::stringstream& ss) const
+{
+}
---
+++ CalibFormats/SiStripObjects/test/UnitTests/TestSiStripDelay.cc
@@ -0,0 +1,118 @@
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TextTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+
+#include <algorithm>
+#include <iterator>
+#include <boost/foreach.hpp>
+
+#define private public
+#define protected public
+#include "CalibFormats/SiStripObjects/interface/SiStripDelay.h"
+#undef private
+#undef protected
+
+#ifndef TestSiStripDelay_cc
+#define TestSiStripDelay_cc
+
+class TestSiStripDelay : public CppUnit::TestFixture {
+public:
+  TestSiStripDelay() {}
+  void setUp()
+  {
+    baseDelay1 = new SiStripBaseDelay;
+    baseDelaySign1 = 1;
+    baseDelay2 = new SiStripBaseDelay;
+    baseDelaySign2 = -1;
+    totalDelays = 15000;
+    // totalDelays = 1;
+    for( uint32_t i=0; i<totalDelays; ++i ) {
+      detIds.push_back(i);
+      coarseDelay.push_back(i/1000);
+      fineDelay.push_back(i/2000);
+      baseDelay1->put(i, i/1000, i/2000);
+      baseDelay2->put(i, i/2000, i/1000);
+      // coarseDelay.push_back(1);
+      // fineDelay.push_back(2);
+      // baseDelay1->put(i, 1, 2);
+      // baseDelay2->put(i, 2, 1);
+    }
+  }
+
+  void tearDown()
+  {
+    delete baseDelay1;
+    delete baseDelay2;
+  }
+
+  void testConstructor()
+  {
+    SiStripDelay delay(*baseDelay1, baseDelaySign1, std::make_pair("baseDelay1", "delay1"));
+    CPPUNIT_ASSERT( delay.getNumberOfTags() == 1 );
+    CPPUNIT_ASSERT( delay.getBaseDelay(0) == baseDelay1 );
+    CPPUNIT_ASSERT( delay.getTagSign(0) == 1 );
+    CPPUNIT_ASSERT( delay.getRcdName(0) == "baseDelay1" );
+    CPPUNIT_ASSERT( delay.getLabelName(0) == "delay1" );
+  }
+
+  void testFillNewDelay()
+  {
+    SiStripDelay delay(*baseDelay1, baseDelaySign1, std::make_pair("baseDelay1", "delay1"));
+    delay.fillNewDelay(*baseDelay2, baseDelaySign2, std::make_pair("baseDelay2", "delay2"));
+    CPPUNIT_ASSERT( delay.getNumberOfTags() == 2 );
+    CPPUNIT_ASSERT( delay.getBaseDelay(0) == baseDelay1 );
+    CPPUNIT_ASSERT( delay.getTagSign(0) == baseDelaySign1 );
+    CPPUNIT_ASSERT( delay.getRcdName(0) == "baseDelay1" );
+    CPPUNIT_ASSERT( delay.getLabelName(0) == "delay1" );
+    CPPUNIT_ASSERT( delay.getBaseDelay(1) == baseDelay2 );
+    CPPUNIT_ASSERT( delay.getTagSign(1) == baseDelaySign2 );
+    CPPUNIT_ASSERT( delay.getRcdName(1) == "baseDelay2" );
+    CPPUNIT_ASSERT( delay.getLabelName(1) == "delay2" );
+  }
+
+  void testMakeDelay()
+  {
+    SiStripDelay delay(*baseDelay1, baseDelaySign1, std::make_pair("baseDelay1", "delay1"));    
+    CPPUNIT_ASSERT(delay.makeDelay());
+    for( uint32_t i=0; i<totalDelays; ++i ) {
+      CPPUNIT_ASSERT( float(delay.getDelay(detIds[i])) == float(baseDelay1->delay(detIds[i])*baseDelaySign1) );
+    }
+    delay.fillNewDelay(*baseDelay2, baseDelaySign2, std::make_pair("baseDelay2", "delay2"));
+    CPPUNIT_ASSERT(delay.makeDelay());
+    for( uint32_t i=0; i<totalDelays; ++i ) {
+//       std::cout << "float(delay.getDelay("<<detIds[i]<<")) = " << float(delay.getDelay(detIds[i])) << std::endl;
+//       std::cout << "float(baseDelay1->delay("<<detIds[i]<<") = " << float(baseDelay1->delay(detIds[i])) << std::endl;
+//       std::cout << "float(baseDelay2->delay("<<detIds[i]<<") = " << float(baseDelay2->delay(detIds[i])) << std::endl;
+//       std::cout << "float(baseDelay1->delay("<<detIds[i]<<")*"<<baseDelaySign1<<" - baseDelay2->delay("<<detIds[i]<<"))*"<<baseDelaySign2<<" = " << float(baseDelay1->delay(detIds[i])*baseDelaySign1 + baseDelay2->delay(detIds[i])*baseDelaySign2) << std::endl;
+      CPPUNIT_ASSERT( float(delay.getDelay(detIds[i])) == float(baseDelay1->delay(detIds[i])*baseDelaySign1 + baseDelay2->delay(detIds[i])*baseDelaySign2) );
+    }
+  }
+
+  SiStripBaseDelay * baseDelay1;
+  int baseDelaySign1;
+  SiStripBaseDelay * baseDelay2;
+  int baseDelaySign2;
+  std::vector<uint32_t> detIds;
+  std::vector<uint16_t> coarseDelay;
+  std::vector<uint16_t> fineDelay;
+  uint32_t totalDelays;
+
+  // Declare and build the test suite
+  CPPUNIT_TEST_SUITE( TestSiStripDelay );
+  CPPUNIT_TEST( testConstructor );
+  CPPUNIT_TEST( testFillNewDelay );
+  CPPUNIT_TEST( testMakeDelay );
+  CPPUNIT_TEST_SUITE_END();
+};
+
+// Register the test suite in the registry.
+// This way we will have to only pass the registry to the runner
+// and it will contain all the registered test suites.
+CPPUNIT_TEST_SUITE_REGISTRATION( TestSiStripDelay );
+
+#endif
