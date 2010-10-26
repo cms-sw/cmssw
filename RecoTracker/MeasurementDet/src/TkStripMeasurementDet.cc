f36484916603c5826930f9e70a5b6a233517c9b1
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-10-26 14:57:41 +0000

    remove compilation warning

--- RecoTracker/MeasurementDet/src/TkStripMeasurementDet.cc
+++ RecoTracker/MeasurementDet/src/TkStripMeasurementDet.cc
@@ -18,10 +18,10 @@ TkStripMeasurementDet::TkStripMeasurementDet( const GeomDet* gdet,
 					      const StripClusterParameterEstimator* cpe,
 					      bool regional) : 
     MeasurementDet (gdet),
-    theCPE(cpe),
+    isRegional(regional),
     empty(true),
     activeThisEvent_(true), activeThisPeriod_(true),
-    isRegional(regional)
+    theCPE(cpe)
   {
     theStripGDU = dynamic_cast<const StripGeomDetUnit*>(gdet);
     if (theStripGDU == 0) {
