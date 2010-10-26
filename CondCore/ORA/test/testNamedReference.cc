97c6b410d403220af01fb532b525e35f22380fc0
Giacomo Govi <sha1-c6008bb91ca6bbc745928cb6f23a999d03f434c3@cern.ch>
2010-10-26 08:50:24 +0000

    added naming service selection functions

--- CondCore/ORA/test/testNamedReference.cc
+++ CondCore/ORA/test/testNamedReference.cc
@@ -51,7 +51,28 @@ int main(){
     db.connect( connStr );
     ora::ScopedTransaction trans1( db.transaction() );
     trans1.start( true );
+    contH0 = db.containerHandle( "Cont0" );
     contH1 = db.containerHandle( "Cont1" );
+    std::vector<std::string> contNames; 
+    if(!contH0.getNames( contNames )){
+      std::cout << "** No names found for container "<<contH0.name()<<std::endl;
+    } else {
+      std::cout << "** Found "<<contNames.size()<<" names for container "<<contH0.name()<<std::endl;
+      for( std::vector<std::string>::const_iterator iN = contNames.begin();
+           iN != contNames.end(); ++iN ){
+	std::cout << "** Name: "<<*iN<<std::endl;
+      }
+    }
+    std::vector<std::string> o0Names;
+    if(!db.getNamesForObject( ora::OId( contH0.id(),oid0 ), o0Names)){
+      std::cout << "** No names found for object cid="<<contH0.id()<<" oid="<<oid0<<std::endl;
+    } else {
+      std::cout << "** Found "<<o0Names.size()<<" names for object cid="<<contH0.id()<<" oid="<<oid0<<std::endl;
+      for( std::vector<std::string>::const_iterator iN = o0Names.begin();
+           iN != o0Names.end(); ++iN ){
+	std::cout << "** Name: "<<*iN<<std::endl;
+      }
+    }
     std::cout << "** First object..."<<std::endl;
     boost::shared_ptr<SH> sr1 = contH1.fetch<SH>( oid1);
     try{
@@ -63,6 +84,16 @@ int main(){
     } catch (ora::Exception& e ){
       std::cout <<"****1 Exception: "<<e.what()<<std::endl;
     }
+    std::vector<std::string> o1Names;
+    if(!db.getNamesForObject( ora::OId( contH1.id(),oid1 ), o1Names)){
+      std::cout << "** No names found for object cid="<<contH1.id()<<" oid="<<oid1<<std::endl;
+    } else {
+      std::cout << "** Found "<<o1Names.size()<<" names for object cid="<<contH1.id()<<" oid="<<oid2<<std::endl;
+      for( std::vector<std::string>::const_iterator iN = o1Names.begin();
+           iN != o1Names.end(); ++iN ){
+	std::cout << "** Name: "<<*iN<<std::endl;
+      }
+    }
     std::cout << "** Second object..."<<std::endl;
     boost::shared_ptr<SH> sr2 = contH1.fetch<SH>( oid2);
     try{
@@ -73,6 +104,16 @@ int main(){
       }
     } catch (ora::Exception& e ){
       std::cout <<"****2 Exception: "<<e.what()<<std::endl;
+    }
+    std::vector<std::string> o2Names;
+    if(!db.getNamesForObject( ora::OId( contH1.id(),oid2 ), o2Names)){
+      std::cout << "** No names found for object cid="<<contH1.id()<<" oid="<<oid2<<std::endl;
+    } else {
+      std::cout << "** Found "<<o2Names.size()<<" names for object cid="<<contH1.id()<<" oid="<<oid2<<std::endl;
+      for( std::vector<std::string>::const_iterator iN = o2Names.begin();
+           iN != o2Names.end(); ++iN ){
+	std::cout << "** Name: "<<*iN<<std::endl;
+      }
     }
     trans1.commit();
     trans1.start( false );
