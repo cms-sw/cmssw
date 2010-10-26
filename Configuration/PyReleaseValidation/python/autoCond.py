4f270cd1cbb31577ec2d244e858ca362a93f3e37
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-10-26 17:46:31 +0000

    Update the GTs for 39X and 310X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,10 +1,10 @@
 
 
 
-autoCond = { 'mc'      : 'MC_39Y_V2::All',
-             'startup' : 'START39_V2::All',
+autoCond = { 'mc'      : 'MC_39Y_V3::All',
+             'startup' : 'START39_V3::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
-             'com10'   : 'GR_R_39X_V1::All',
-             'hltonline' : 'GR_R_39X_V1::All' # same as com10 until H tag available
+             'com10'   : 'GR_R_39X_V2::All',
+             'hltonline' : 'GR_R_39X_V2::All' # same as com10 until H tag available
              }
