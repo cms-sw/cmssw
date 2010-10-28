7d3b42a18b7088a5aae0de14cd1d5b3a83463618
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-10-28 14:34:48 +0000

    test cff

--- RecoHI/HiCentralityAlgos/test/FrontierConditions_GlobalTag_Temp_cff.py
+++ RecoHI/HiCentralityAlgos/test/FrontierConditions_GlobalTag_Temp_cff.py
@@ -3,23 +3,23 @@ from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 
 GlobalTag.toGet = cms.VPSet(
     cms.PSet(record = cms.string("HeavyIonRcd"),
-             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v1_mc"),
-             connect = cms.untracked.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
-             label = cms.untracked.string("")
+             tag = cms.string("CentralityTable_PixelHits40_Hydjet2760GeV_v0_mc"),
+             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
+             label = cms.untracked.string("PixelHitsHydjet_2760GeV")
              ),
     cms.PSet(record = cms.string("HeavyIonRcd"),
-             tag = cms.string("CentralityTable_PixelHits40_AMPT2760GeV_v3_mc"),
-             connect = cms.untracked.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
+             tag = cms.string("CentralityTable_PixelHits40_AMPT2760GeV_v0_mc"),
+             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
              label = cms.untracked.string("PixelHitsAMPT_2760GeV")
              ),
     cms.PSet(record = cms.string("HeavyIonRcd"),
-             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v1_mc"),
-             connect = cms.untracked.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
-             label = cms.untracked.string("HFhits")
+             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v0_mc"),
+             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
+             label = cms.untracked.string("HFhitsHydjet_2760GeV")
              ),
     cms.PSet(record = cms.string("HeavyIonRcd"),
-             tag = cms.string("CentralityTable_HFhits40_AMPT2760GeV_v3_mc"),
-             connect = cms.untracked.string("frontier://FrontierPrep/CMS_COND_PHYSICSTOOLS"),
+             tag = cms.string("CentralityTable_HFhits40_AMPT2760GeV_v0_mc"),
+             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
              label = cms.untracked.string("HFhitsAMPT_2760GeV")
              )
     )
