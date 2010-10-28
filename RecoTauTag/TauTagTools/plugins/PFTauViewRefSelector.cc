3c207d74496411c8efe94a6fd6c05b97d225173e
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-28 13:01:53 +0000

    Adding PFTau ref selectors

---
+++ RecoTauTag/TauTagTools/plugins/PFTauViewRefSelector.cc
@@ -0,0 +1,35 @@
+/* \class CandViewRefSelector
+ *
+ * PFTau Selector based on a configurable cut.
+ * Reads a edm::View<PFTau> as input
+ * and saves a vector of references
+ * Usage:
+ *
+ * module selectedCands = PFTauViewRefSelector {
+ *   InputTag src = myCollection
+ *   string cut = "pt > 15.0"
+ * };
+ *
+ * \author: Luca Lista, INFN
+ * \modifications: Evan Friis, UC Davis
+ *
+ */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
+#include "CommonTools/UtilAlgos/interface/StringCutObjectSelector.h"
+#include "DataFormats/TauReco/interface/PFTau.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+
+typedef SingleObjectSelector<
+          edm::View<reco::PFTau>,
+          StringCutObjectSelector<reco::PFTau>
+       > PFTauViewRefSelector;
+
+DEFINE_FWK_MODULE(PFTauViewRefSelector);
+
+typedef SingleObjectSelector<
+          edm::View<reco::Jet>,
+          StringCutObjectSelector<reco::Jet, true>
+       > JetViewRefSelector;
+
+DEFINE_FWK_MODULE(JetViewRefSelector);
