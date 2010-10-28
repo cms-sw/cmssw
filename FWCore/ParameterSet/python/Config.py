6fbec318b906dac9c8c5a68c547dd16c4131418d
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-10-28 12:49:51 +0000

    allow FilteredStream content to be a string

--- FWCore/ParameterSet/python/Config.py
+++ FWCore/ParameterSet/python/Config.py
@@ -808,8 +808,8 @@ class FilteredStream(dict):
            raise ValueError("The needed parameters are: content, dataTier, name, paths, responsible, selectEvents")
 	if not isinstance(kw['name'],str):
            raise ValueError("name must be of type string")
-        if not isinstance(kw['content'], vstring):
-           raise ValueError("content must be of type vstring")
+        if not isinstance(kw['content'], vstring) and not isinstance(kw['content'],str):
+           raise ValueError("content must be of type vstring or string")
         if not isinstance(kw['dataTier'], string):
            raise ValueError("dataTier must be of type string")
         if not isinstance(kw['selectEvents'], PSet):
