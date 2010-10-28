9bd0e10bd903a1c6c900887a64cf28ab0f16161f
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2010-10-28 20:58:20 +0000

    Further unit test fix. Let the 2 unit tests that were passing at CERN continue to run, but they will only run there. They will not run anywhere else

---
+++ Utilities/StorageFactory/test/run_StorageFactoryTests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function die { echo Failure $1: status $2 ; exit $2 ; }
+
+pushd ${LOCAL_TMP_DIR}
+
+  domain=`hostname -d`
+  echo domain is $domain
+
+  echo $domain | grep cern
+
+  if [ $? = "1" ]
+  then
+    echo This test only works at cern. This machine is not at cern.
+    echo Skipping test.
+    exit 0
+  fi
+
+  ${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/test_StorageFactory_Rfio || die "test_StorageFactory_Rfio" $?
+  ${LOCAL_TOP_DIR}/test/${SCRAM_ARCH}/test_StorageFactory_Write || die "test_StorageFactory_Write" $?
+
+popd
+exit 0
