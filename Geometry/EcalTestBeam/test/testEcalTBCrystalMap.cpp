f6c5ac61ae91c460af7c709e570c8cf81c941485
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-10-28 09:24:39 +0000

    Fix unit test.

--- Geometry/EcalTestBeam/test/testEcalTBCrystalMap.cpp
+++ Geometry/EcalTestBeam/test/testEcalTBCrystalMap.cpp
@@ -6,10 +6,12 @@
 
 int main() {
 
-  const std::string MapFileName = "BarrelSM1CrystalCenterElectron120GeV.dat";
+  const char *workarea = getenv("CMSSW_BASE");
 
-  EcalTBCrystalMap theTestMap(MapFileName);
+  const std::string MapFileName = "/src/Geometry/EcalTestBeam/data/BarrelSM1CrystalCenterElectron120GeV.dat";
 
+  EcalTBCrystalMap theTestMap(workarea + MapFileName);
+  
   long nCrystal = 1700;
 
   for ( int i = 0 ; i < 10 ; ++i ) {
@@ -19,13 +21,13 @@ int main() {
     double thisPhi = 0.;
 
     theTestMap.findCrystalAngles(thisCrystal, thisEta, thisPhi);
-
+    
     std::cout << "Crystal number " << thisCrystal << " eta = " << thisEta << " phi = " << thisPhi << std::endl;
 
     int checkThisCrystal = theTestMap.CrystalIndex(thisEta, thisPhi);
-
+    
     std::cout << "(eta,phi) = " << thisEta << " , " << thisPhi << " corresponds to crystal n. = " << checkThisCrystal << std::endl;
-
+    
   }
 
   return 0;
