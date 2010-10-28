0350cae95359a68a43662c487db9387216b1d95c
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-28 12:49:49 +0000

    style

--- RecoTauTag/TauTagTools/plugins/TauDiscriminationByStringCut.cc
+++ RecoTauTag/TauTagTools/plugins/TauDiscriminationByStringCut.cc
@@ -1,5 +1,5 @@
 #include "RecoTauTag/RecoTau/interface/TauDiscriminationProducerBase.h"
-#include "CommonTools/Utils/interface/StringCutObjectSelector.h" 
+#include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 
 /* class TauDiscriminationByStringCut
  * created : Nov 9 2009
@@ -8,40 +8,38 @@
  */
 
 template<class TauType, class TauDiscriminator>
-class TauDiscriminationByStringCut : public TauDiscriminationProducerBase<TauType, TauDiscriminator> 
-{
+class TauDiscriminationByStringCut :
+    public TauDiscriminationProducerBase<TauType, TauDiscriminator> {
    public:
       explicit TauDiscriminationByStringCut(const edm::ParameterSet& iConfig)
-	: TauDiscriminationProducerBase<TauType, TauDiscriminator>(iConfig)
-      {   
-	cut_ = new StringCutObjectSelector<TauType>( iConfig.getParameter<std::string>("cut") );
-	cutFailValue_ = ( iConfig.exists("cutFailValue") ) ? iConfig.getParameter<double>("cutFailValue") : 0.;
-	cutPassValue_ = ( iConfig.exists("cutPassValue") ) ? iConfig.getParameter<double>("cutPassValue") : 1.;
-	this->prediscriminantFailValue_ = cutFailValue_;
+          : TauDiscriminationProducerBase<TauType, TauDiscriminator>(iConfig),
+          cut_(new StringCutObjectSelector<TauType>(
+            iConfig.getParameter<std::string>("cut"))) {
+        cutFailValue_ = ( iConfig.exists("cutFailValue") ) ?
+            iConfig.getParameter<double>("cutFailValue") : 0.;
+        cutPassValue_ = ( iConfig.exists("cutPassValue") ) ?
+            iConfig.getParameter<double>("cutPassValue") : 1.;
+        this->prediscriminantFailValue_ = cutFailValue_;
       }
 
-      ~TauDiscriminationByStringCut()
-      {
-        delete cut_;
-      }
+      typedef std::vector<TauType> TauCollection;
+      typedef edm::Ref<TauCollection> TauRef;
 
-      typedef std::vector<TauType> TauCollection; 
-      typedef edm::Ref<TauCollection> TauRef; 
-
-      double discriminate(const TauRef& tau)
-      {
-	return ( (*cut_)(*tau) ) ? cutPassValue_ : cutFailValue_; // StringCutObjectSelector::operator() returns true if tau passes cut
+      double discriminate(const TauRef& tau) {
+        // StringCutObjectSelector::operator() returns true if tau passes cut
+        return ( (*cut_)(*tau) ) ? cutPassValue_ : cutFailValue_;
       }
 
    private:
-      StringCutObjectSelector<TauType>* cut_;
+      std::auto_ptr<StringCutObjectSelector<TauType> > cut_;
       double cutFailValue_;
       double cutPassValue_;
 };
 
-typedef TauDiscriminationByStringCut<reco::PFTau, reco::PFTauDiscriminator> PFRecoTauDiscriminationByStringCut;
-typedef TauDiscriminationByStringCut<reco::CaloTau, reco::CaloTauDiscriminator> CaloRecoTauDiscriminationByStringCut;
+typedef TauDiscriminationByStringCut<reco::PFTau, reco::PFTauDiscriminator>
+  PFRecoTauDiscriminationByStringCut;
+typedef TauDiscriminationByStringCut<reco::CaloTau, reco::CaloTauDiscriminator>
+  CaloRecoTauDiscriminationByStringCut;
 
 DEFINE_FWK_MODULE(PFRecoTauDiscriminationByStringCut);
 DEFINE_FWK_MODULE(CaloRecoTauDiscriminationByStringCut);
-
