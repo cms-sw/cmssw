6ed393b9fa93f2776072181de1b05084c38da43b
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-28 13:31:52 +0000

    Remove obsolete stuff

--- RecoTauTag/Configuration/python/RecoTauTag_FakeConditions_cff.py
+++
@@ -1,3 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoTauTag.TauTagTools.TancConditions_cff import *
--- RecoTauTag/Configuration/python/RecoTauTag_FrontierConditions_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from RecoTauTag.TauTagTools.TancConditions_cff import *
-TauTagMVAComputerRecord.connect = cms.string('frontier://FrontierProd/CMS_COND_BTAU')
