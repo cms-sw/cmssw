99a0ced49f6bac71c8e7cd144afaa9f5608803da
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-28 13:23:05 +0000

    Adding tau truth matching sequences

---
+++ RecoTauTag/TauTagTools/python/TauTruthProduction_cfi.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.JetMCAlgos.TauGenJets_cfi import tauGenJets
+from PhysicsTools.HepMCCandAlgos.genParticles_cfi import genParticles
+
+trueHadronicTaus = cms.EDFilter(
+    "TauGenJetDecayModeSelector",
+    src = cms.InputTag("tauGenJets"),
+    select = cms.vstring(
+        'oneProng0Pi0', 'oneProng1Pi0', 'oneProng2Pi0', 'oneProngOther',
+        'threeProng0Pi0', 'threeProng1Pi0', 'threeProngOther', 'rare'),
+    filter = cms.bool(False)
+)
+
+trueCommonHadronicTaus = cms.EDFilter(
+    "TauGenJetDecayModeSelector",
+    src = cms.InputTag("tauGenJets"),
+    select = cms.vstring(
+        'oneProng0Pi0', 'oneProng1Pi0', 'oneProng2Pi0', 
+        'threeProng0Pi0', 'threeProng1Pi0'),
+    filter = cms.bool(False)
+)
+
+trueMuonicTaus = cms.EDFilter(
+    "TauGenJetDecayModeSelector",
+    src = cms.InputTag("tauGenJets"),
+    select = cms.vstring('muon'),
+    filter = cms.bool(False)
+)
+
+trueElecronicTaus = cms.EDFilter(
+    "TauGenJetDecayModeSelector",
+    src = cms.InputTag("tauGenJets"),
+    select = cms.vstring('electron'),
+    filter = cms.bool(False)
+)
+
+tauTruthSequence = cms.Sequence(
+    genParticles *
+    tauGenJets *
+    trueHadronicTaus *
+    trueMuonicTaus *
+    trueElecronicTaus)
