4a3c2e87c740babb48166c76943fbf97a09b3bda
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-10-28 13:28:15 +0000

    whitespace only

--- RecoTauTag/TauTagTools/plugins/PFTauSelectorDefinition.h
+++ RecoTauTag/TauTagTools/plugins/PFTauSelectorDefinition.h
@@ -18,33 +18,33 @@ struct PFTauSelectorDefinition {
   struct TDiscCutPair {
     edm::Handle<reco::PFTauDiscriminator> m_discHandle;
     double m_cut;
-  };    
+  };
   typedef std::vector<TDiscCutPair> TDiscCutPairVec;
-  
+
   PFTauSelectorDefinition ( const edm::ParameterSet & cfg ) {
-    discriminators_ = cfg.getParameter< std::vector<edm::ParameterSet> >( "discriminators" ); 
+    discriminators_ = cfg.getParameter< std::vector<edm::ParameterSet> >( "discriminators" );
     cut_ = ( cfg.exists("cut") ) ? new StringCutObjectSelector<reco::PFTau>( cfg.getParameter<std::string>( "cut" ) ) : 0;
   }
 
   ~PFTauSelectorDefinition () { delete cut_; }
-  
+
   const_iterator begin() const { return selected_.begin(); }
 
   const_iterator end() const { return selected_.end(); }
 
-  void select( const HandleToCollection & hc, 
+  void select( const HandleToCollection & hc,
 	       const edm::Event & e,
-	       const edm::EventSetup& s) 
+	       const edm::EventSetup& s)
   {
-           
+
     selected_.clear();
     assert( hc.isValid() ); // collection of PFTaus
-    
+
     // get discriminators and coresponding cuts from stored vpset
     static TDiscCutPairVec discriminators;
     discriminators.clear();
-    
-    for(std::vector< edm::ParameterSet >::iterator it = discriminators_.begin(); 
+
+    for(std::vector< edm::ParameterSet >::iterator it = discriminators_.begin();
         it != discriminators_.end();
         ++it)
     {
@@ -58,16 +58,16 @@ struct PFTauSelectorDefinition {
       assert( disc.m_discHandle->size() ==  hc->size());
       discriminators.push_back(disc);
     }
-      
+
     unsigned key=0;
     static bool passedAllCuts;
-    for( collection::const_iterator pftau = hc->begin(); 
+    for( collection::const_iterator pftau = hc->begin();
           pftau != hc->end();
-          ++pftau, ++key) 
+          ++pftau, ++key)
     {
       passedAllCuts = true;
       reco::PFTauRef pfTauRef(hc, key);
-      
+
       //iterate over all discriminators, check the cuts
       for (TDiscCutPairVec::iterator discIt = discriminators.begin();
            discIt!=discriminators.end();
@@ -78,7 +78,7 @@ struct PFTauSelectorDefinition {
       }
 
       if ( cut_ ) passedAllCuts &= (*cut_)(*pftau);
-         
+
       if(passedAllCuts)
         selected_.push_back( new reco::PFTau(*pftau) );
     } // end collection iteration
