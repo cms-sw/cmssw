7738d4d955c5aeb5fe5730bea3b173ab599c8fc1
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2010-10-29 11:52:39 +0000

    added record for tracker surface deformation alignment parameters

---
+++ CondFormats/AlignmentRecord/src/TrackerSurfaceDeformationRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     TrackerGeometry
+// Class  :     TrackerSurfaceDeformationRecord
+// 
+// Implementation:
+//     <Notes on implementation>
+//
+// Author:      Chris Jones
+// Created:     Wed May 11 16:28:46 EDT 2005
+// $Id: TrackerAlignmentRcd.cc,v 1.1 2007/10/18 07:31:49 fronga Exp $
+
+#include "CondFormats/AlignmentRecord/interface/TrackerSurfaceDeformationRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+EVENTSETUP_RECORD_REG(TrackerSurfaceDeformationRcd);
