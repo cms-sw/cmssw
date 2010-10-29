ebd90f84b8f3be74d6307ba81f45e0c7a6d4a5b7
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2010-10-29 10:09:35 +0000

    added DB storage for alignment surface deformation parameters

---
+++ CondFormats/Alignment/src/T_EventSetup_AlignmentSurfaceDeformation.cc
@@ -0,0 +1,13 @@
+// -*- C++ -*-
+// Author:      Andreas Mussgiller
+// Created:     2010/08/05
+// $Id: T_EventSetup_Alignment.cc,v 1.2 2010/02/20 20:55:32 wmtan Exp $
+//
+
+// system include files
+
+// user include files
+#include "CondFormats/Alignment/interface/AlignmentSurfaceDeformations.h"
+#include "FWCore/Utilities/interface/typelookup.h"
+
+TYPELOOKUP_DATA_REG(AlignmentSurfaceDeformations);
