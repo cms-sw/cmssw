13ea10d2c0e5ff3c9b0d648539f78cfe04eeb3cb
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2010-11-03 19:53:38 +0000

    Switch back to CERN input and adapt to new PAT trigger python tools.

--- PhysicsTools/PatExamples/test/producePatTrigger_cfg.py
+++ PhysicsTools/PatExamples/test/producePatTrigger_cfg.py
@@ -4,31 +4,16 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 ## ---
 ## Use proper input
 ## ---
-from PhysicsTools.PatExamples.samplesDESY_cff import *
+from PhysicsTools.PatExamples.samplesCERN_cff import *
 process.source.fileNames = zjetsRECO
-
-## ---
-## Adjust inputs if necessary
-## ---
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run36xOn35xInput
-run36xOn35xInput(process)
-
-## this might be needed when running on 383 rereco'ed data
-process.load("RecoJets.Configuration.GenJetParticles_cff")
-process.load("RecoJets.Configuration.RecoGenJets_cff")
-
-process.p0 = cms.Path(
-    process.genJetParticles *
-    process.ak5GenJets
-)
 
 ## ---
 ## Determine number of events to be processed
 ## ---
-process.maxEvents.input = 100
+process.maxEvents.input = 1000
 
 ## ---
-## Adaptations to the event content
+## Define the path
 ## ---
 process.p = cms.Path(
     process.patDefaultSequence
@@ -38,7 +23,9 @@ process.options.wantSummary = False # to suppress the long output at the end of 
 
 process.selectedPatMuons.cut = "isTrackerMuon==1 & isGlobalMuon==1 & innerTrack.numberOfValidHits>=11 & globalTrack.normalizedChi2<10.0  & globalTrack.hitPattern.numberOfValidMuonHits>0 & abs(dB)<0.02 & (trackIso+caloIso)/pt<0.05"
 
-# PAT trigger
+## ---
+## PAT trigger matching
+## --
 process.load( "PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff" )
 process.muonTriggerMatchHLTMuons = cms.EDProducer( "PATTriggerMatcherDRLessByR",
     src     = cms.InputTag( "cleanPatMuons" ),
@@ -54,10 +41,23 @@ process.muonTriggerMatchHLTMuons = cms.EDProducer( "PATTriggerMatcherDRLessByR",
     resolveAmbiguities    = cms.bool( True ),
     resolveByMatchQuality = cms.bool( True )
 )
-process.patTriggerMatcher += process.muonTriggerMatchHLTMuons
-process.patTriggerMatcher.remove( process.patTriggerMatcherElectron )
-process.patTriggerMatcher.remove( process.patTriggerMatcherMuon )
-process.patTriggerMatcher.remove( process.patTriggerMatcherTau )
-process.patTriggerEvent.patTriggerMatches = [ "muonTriggerMatchHLTMuons" ]
-from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
-switchOnTrigger( process )
+
+### ============
+### Python tools
+### ============
+### Attention: order matters
+
+## --
+## Switch to selected PAT objects in the main work flow
+## --
+from PhysicsTools.PatAlgos.tools.coreTools import removeCleaning
+removeCleaning( process )
+
+## --
+## Switch on PAT trigger
+## --
+from PhysicsTools.PatAlgos.tools.trigTools import *
+switchOnTrigger( process ) # This is optional and can be omitted.
+switchOnTriggerMatching( process, ['muonTriggerMatchHLTMuons'] )
+# Switch to selected PAT objects in the trigger matching
+removeCleaningFromTriggerMatching( process )
