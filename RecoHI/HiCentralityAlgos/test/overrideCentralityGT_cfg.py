57312b12717c608928dc4dc3054485d0b919584e
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2010-11-04 09:37:26 +0000

    example cfgs updated

--- RecoHI/HiCentralityAlgos/test/overrideCentralityGT_cfg.py
+++ RecoHI/HiCentralityAlgos/test/overrideCentralityGT_cfg.py
@@ -4,12 +4,20 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TEST")
 
 process.HeavyIonGlobalParameters = cms.PSet(
-    centralityVariable = cms.string("HFhits"),
+    centralityVariable = cms.string(""),
     nonDefaultGlauberModel = cms.string("AMPT_2760GeV")
     )
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = 'START39_V4HI::All'
+
+process.GlobalTag.toGet = cms.VPSet(
+    cms.PSet(record = cms.string("HeavyIonRcd"),
+             tag = cms.string("CentralityTable_HFhits40_Hydjet2760GeV_v1_mc"),
+             connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS")
+             )
+    )
+
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(-1)
