3450a66dc9d6b31df61e307c1b981ad6932a6bfa
Yongsun Kim <sha1-cf1309631de6d8b3179fc471ac6b6474eee4ad5d@cern.ch>
2010-11-04 13:58:55 +0000

    moved HiSuperClusterProducer to plugins

--- RecoHI/HiEgammaAlgos/src/SealModule.cc
+++ RecoHI/HiEgammaAlgos/src/SealModule.cc
@@ -1,7 +1,5 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoHI/HiEgammaAlgos/interface/HiSuperClusterProducer.h"
 #include "RecoHI/HiEgammaAlgos/interface/HiEgammaSCCorrectionMaker.h"
 
-DEFINE_FWK_MODULE(HiSuperClusterProducer);
 DEFINE_FWK_MODULE(HiEgammaSCCorrectionMaker);
