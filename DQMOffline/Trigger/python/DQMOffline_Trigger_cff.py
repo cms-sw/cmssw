a826e3ba8d074616d90f4560b27163618a48e8b6
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-11-04 21:28:34 +0000

    DQMOffline/Trigger/python/DQMOffline_Trigger_Client_cff.py

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
@@ -8,6 +8,8 @@ from DQM.L1TMonitor.L1TFED_cfi import *
 l1tgt.gtSource = 'gtDigis::'
 l1tgmt.gmtSource = 'gtDigis::'
 l1tdttf.dttpgSource = 'dttfDigis::'
+l1tdttf.gmtSource = 'gtDigis::'
+l1tdttf.online = cms.untracked.bool(False)
 l1tcsctf.statusProducer = 'csctfDigis::'
 l1tcsctf.lctProducer = 'csctfDigis::'
 l1tcsctf.trackProducer = 'csctfDigis::'
@@ -19,8 +21,6 @@ l1tgct.gctEnergySumsSource = 'gctDigis::'
 l1tgct.gctIsoEmSource = 'gctDigis:isoEm:'
 l1tgct.gctNonIsoEmSource = 'gctDigis:nonIsoEm:'
 l1trct.rctSource = 'gctDigis::'
-l1tdttf.gmtSource = 'gtDigis::'
-l1tdttf.online = cms.untracked.bool(False)
 
 
 import DQMServices.Components.DQMEnvironment_cfi
