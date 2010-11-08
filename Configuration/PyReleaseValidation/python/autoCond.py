4d3b7c177a5e727792ac74dd43c07985fbdfba69
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2010-11-08 15:52:42 +0000

    condition global tag for HIon Startup configurable via auto:starthi

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -6,5 +6,6 @@ autoCond = { 'mc'      : 'MC_39Y_V3::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
              'com10'   : 'GR_R_39X_V2::All',
-             'hltonline' : 'GR_R_39X_V2::All' # same as com10 until H tag available
+             'hltonline' : 'GR_R_39X_V2::All', # same as com10 until H tag available
+             'starthi' : 'START39_V4HI::All'
              }
