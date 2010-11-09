70b7908351b614a542aa8639778a6a51e300254c
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-11-09 23:29:16 +0000

    fix MC tag name

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,7 +1,7 @@
 
 
 
-autoCond = { 'mc'      : 'MC_39Y_V5:All',
+autoCond = { 'mc'      : 'MC_39Y_V5::All',
              'startup' : 'START39_V5::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
