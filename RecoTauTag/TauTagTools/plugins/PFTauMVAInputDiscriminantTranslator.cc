5cee3eb5111c87a48d5ffa3d201820c0ebf881ea
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-11-09 16:00:38 +0000

    Updating tau ntuple production from discriminants.  Currently disabled due to to strange PSET behaviorcvs up -A

--- RecoTauTag/TauTagTools/plugins/PFTauMVAInputDiscriminantTranslator.cc
+++ RecoTauTag/TauTagTools/plugins/PFTauMVAInputDiscriminantTranslator.cc
@@ -9,171 +9,121 @@
  *
  */
 
-// system include files
+#include <boost/shared_ptr.hpp>
+#include <boost/foreach.hpp>
+
 #include <memory>
-// user include files
+#include <string>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "RecoTauTag/TauTagTools/interface/DiscriminantList.h"
-#include "RecoTauTag/TauTagTools/interface/PFTauDiscriminantManager.h"
-#include "DataFormats/TauReco/interface/PFTauDecayModeAssociation.h"
+#include "PhysicsTools/MVAComputer/interface/Variable.h"
 #include "DataFormats/TauReco/interface/PFTauDiscriminator.h"
+#include "RecoTauTag/RecoTau/interface/RecoTauDiscriminantPlugins.h"
 
-using namespace PFTauDiscriminants;
 using namespace reco;
 
 class PFTauMVAInputDiscriminantTranslator : public edm::EDProducer {
-   public:
-      struct DiscriminantInfo {
-         PhysicsTools::AtomicId name;
-         std::string collName;
-         size_t index;
-         float defaultValue;
-      };
+  public:
+    struct DiscriminantInfo {
+      PhysicsTools::AtomicId name;
+      std::string collName;
+      size_t index;
+      float defaultValue;
+      boost::shared_ptr<reco::tau::RecoTauDiscriminantPlugin> plugin;
+    };
 
-      PFTauMVAInputDiscriminantTranslator(const edm::ParameterSet&);
-      void produce(edm::Event&, const edm::EventSetup&);
+    PFTauMVAInputDiscriminantTranslator(const edm::ParameterSet&);
+    void produce(edm::Event&, const edm::EventSetup&);
 
-   private:
-      edm::InputTag pfTauDMSource_;
-      edm::InputTag pfTauSource_;
-      std::vector<DiscriminantInfo> discriminators_;
-      PFTauDiscriminantManager  discriminantManager_;
-      DiscriminantList          myDiscriminants_;  
+  private:
+    edm::InputTag pfTauSource_;
+    std::vector<DiscriminantInfo> discriminators_;
 };
 
+PFTauMVAInputDiscriminantTranslator::PFTauMVAInputDiscriminantTranslator(
+    const edm::ParameterSet& pset) {
+  typedef std::vector<edm::ParameterSet> VPSet;
+  pfTauSource_ = pset.getParameter<edm::InputTag>("pfTauSource");
+  VPSet discriminants = pset.getParameter<VPSet>("discriminants");
 
-
-PFTauMVAInputDiscriminantTranslator::PFTauMVAInputDiscriminantTranslator(const edm::ParameterSet& pset)
-{
-   typedef std::vector<edm::ParameterSet> VPSet;
-   pfTauDMSource_ = pset.getParameter<edm::InputTag>("decayModeSource");
-   pfTauSource_ = pset.getParameter<edm::InputTag>("pfTauSource");
-
-   VPSet discriminants = pset.getParameter<VPSet>("discriminants");
-
-   for(VPSet::const_iterator iDisc = discriminants.begin(); 
-         iDisc != discriminants.end(); ++iDisc)
-   {
-      edm::ParameterSet discPSet = *iDisc;
-      // get discriminant name
-      std::string name = discPSet.getParameter<std::string>("name");
-      double defaultValue = (discPSet.exists("default")) ? discPSet.getParameter<double>("default") : 0.;
-      // check if we are getting multiple indices
-      bool requestMultiple = discPSet.exists("indices");
-      if(requestMultiple)
-      {
-         // make a discrimiantor for each desired index
-         std::vector<uint32_t> indices = discPSet.getParameter<std::vector<uint32_t> >("indices");
-         for(std::vector<uint32_t>::const_iterator index = indices.begin();
-               index != indices.end(); ++index)
-         {
-            DiscriminantInfo newDisc;
-            newDisc.name = name;
-            newDisc.index = *index;
-            newDisc.defaultValue = defaultValue;
-            // make a nice colleciton name
-            std::stringstream collectionName;
-            collectionName << name << *index;
-            newDisc.collName = collectionName.str();
-            discriminators_.push_back(newDisc);
-         }
-      } else 
-      { 
-         //single discriminant
-         DiscriminantInfo newDisc;
-         newDisc.name = name;
-         newDisc.collName = name;
-         newDisc.index = 0;
-         newDisc.defaultValue = defaultValue;
-         discriminators_.push_back(newDisc);
+  for(VPSet::const_iterator iDisc = discriminants.begin();
+      iDisc != discriminants.end(); ++iDisc) {
+    // WTF IS GOING ON HERE
+    std::string name = iDisc->getParameter<std::string>("name");
+    double defaultValue = (iDisc->exists("default")) ?
+        iDisc->getParameter<double>("default") : 0.;
+    // check if we are getting multiple indices
+    bool requestMultiple = iDisc->exists("indices");
+    if(requestMultiple) {
+      // make a discrimiantor for each desired index
+      std::vector<uint32_t> indices =
+          iDisc->getParameter<std::vector<uint32_t> >("indices");
+      for(std::vector<uint32_t>::const_iterator index = indices.begin();
+          index != indices.end(); ++index) {
+        DiscriminantInfo newDisc;
+        newDisc.name = name;
+        newDisc.index = *index;
+        newDisc.defaultValue = defaultValue;
+        // make a nice colleciton name
+        std::stringstream collectionName;
+        collectionName << name << *index;
+        newDisc.collName = collectionName.str();
+        // Build the plugin
+        edm::ParameterSet fakePSet;
+        newDisc.plugin.reset(
+            RecoTauDiscriminantPluginFactory::get()->create(
+                reco::tau::discPluginName(name), fakePSet));
+        discriminators_.push_back(newDisc);
       }
-   }
-   // register products
-   for(std::vector<DiscriminantInfo>::const_iterator iDisc = discriminators_.begin();
-         iDisc != discriminators_.end(); ++iDisc)
-   {
-      produces<PFTauDiscriminator>(iDisc->collName);
-   }
-
-   // Load discriminants into the manager
-   for(DiscriminantList::const_iterator aDiscriminant  = myDiscriminants_.begin();
-                                        aDiscriminant != myDiscriminants_.end();
-                                      ++aDiscriminant)
-   {
-      //load the discriminants into the discriminant manager
-      discriminantManager_.addDiscriminant(*aDiscriminant);
-   }
+    } else {
+      //single discriminant
+      DiscriminantInfo newDisc;
+      newDisc.name = name;
+      newDisc.collName = name;
+      newDisc.index = 0;
+      newDisc.defaultValue = defaultValue;
+      // Build the plugin
+      edm::ParameterSet fakePSet;
+      newDisc.plugin.reset(
+          RecoTauDiscriminantPluginFactory::get()->create(
+              reco::tau::discPluginName(name), fakePSet));
+      discriminators_.push_back(newDisc);
+    }
+  }
+  // register products
+  BOOST_FOREACH(const DiscriminantInfo& disc, discriminators_) {
+    produces<PFTauDiscriminator>(disc.collName);
+  }
 }
 
-void PFTauMVAInputDiscriminantTranslator::produce(edm::Event& evt, const edm::EventSetup& es)
-{
-   typedef std::vector<PhysicsTools::Variable::Value> VarList;
-   // Handle to get PFTaus to associated to
-   edm::Handle<PFTauCollection> pfTaus; 
-   evt.getByLabel(pfTauSource_, pfTaus);
+void PFTauMVAInputDiscriminantTranslator::produce(edm::Event& evt,
+                                                  const edm::EventSetup& es) {
+  // Handle to get PFTaus to associated to
+  edm::Handle<PFTauCollection> pfTaus;
+  evt.getByLabel(pfTauSource_, pfTaus);
 
-   // Handle to PFTauDecayModes for given taus
-   edm::Handle<PFTauDecayModeAssociation> pfTauDecayModes; 
-   evt.getByLabel(pfTauDMSource_, pfTauDecayModes);
-
-   // Make an MVA output object for each of the taus.  (Due to auto_ptr container issues
-   // we want to loop over the output discriminators separately)
-
-   size_t nTaus = pfTaus->size();
-   // holder for the the MVA inputs for each tau
-   std::vector<VarList> mvaVariablesForTau(nTaus);
-
-   // Setup Disc. mananger for this event
-   discriminantManager_.setEvent(evt, 1.0);
-
-   // Compute the variables for each tau
-   for(size_t iTau = 0; iTau < nTaus; ++iTau)
-   {
-      // get tau ref
-      PFTauRef tauRef(pfTaus, iTau);
-      // get associated PFTauDecayMode
-      const PFTauDecayMode& decayMode = (*pfTauDecayModes)[tauRef];
-      discriminantManager_.setTau(decayMode);
-      discriminantManager_.buildMVAComputerLink(mvaVariablesForTau[iTau]);
-   }
-
-   // Now produce the output collections for each discriminant
-   for(std::vector<DiscriminantInfo>::const_iterator iDisc = discriminators_.begin();
-         iDisc != discriminators_.end(); ++iDisc)
-   {
-      std::auto_ptr<PFTauDiscriminator> output(new PFTauDiscriminator(edm::RefProd<PFTauCollection>(pfTaus)));
-      for(size_t iTau = 0; iTau < nTaus; ++iTau)
-      {
-         // get computed variables for this tau
-         const VarList& computedMVADiscriminants = mvaVariablesForTau[iTau];
-         // We're looking to pull out the discriminant with the matching name and index
-         // get our desired index
-         double varForThisTau = iDisc->defaultValue;
-         size_t desired_index = iDisc->index + 1;
-         // keep count of what instance of this variable we are on
-         size_t instancesFound = 0;
-         for(VarList::const_iterator mvaVariable = computedMVADiscriminants.begin(); 
-               mvaVariable != computedMVADiscriminants.end(); ++mvaVariable)
-         {
-            // see if the name of this variable matches the desired one
-            if(mvaVariable->getName() == iDisc->name)
-               instancesFound++;
-            // check if this was the desired instance
-            if(desired_index == instancesFound)
-            {
-               varForThisTau = mvaVariable->getValue();
-               break;
-            }
-         }
-         output->setValue(iTau, varForThisTau);
+  BOOST_FOREACH(const DiscriminantInfo& disc, discriminators_) {
+    // output for this discriminator
+    std::auto_ptr<PFTauDiscriminator> output(
+        new PFTauDiscriminator(edm::RefProd<PFTauCollection>(pfTaus)));
+    // loop over taus
+    for(size_t itau = 0; itau < pfTaus->size(); ++itau) {
+      PFTauRef tauRef(pfTaus, itau);
+      // discriminator result
+      std::vector<double> result = (*disc.plugin)(tauRef);
+      // The desired index
+      double selected_result = disc.defaultValue;
+      if (result.size()-1 < disc.index) {
+        selected_result = result[disc.index];
       }
-      evt.put(output, iDisc->collName);
-   }
+      output->setValue(itau, selected_result);
+    }
+    evt.put(output, disc.collName);
+  }
 }
 
 DEFINE_FWK_MODULE(PFTauMVAInputDiscriminantTranslator);
