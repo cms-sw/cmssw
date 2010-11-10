0cadd2dab60c319ab19b6b6086cef393244cda9d
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-11-10 04:30:18 +0000

    apply isolation cuts and cleaning together

--- HeavyIonsAnalysis/Configuration/python/HI_PhotonSkim_cff.py
+++ HeavyIonsAnalysis/Configuration/python/HI_PhotonSkim_cff.py
@@ -13,21 +13,29 @@ goodPhotons = cms.EDFilter("PhotonSelector",
     cut = cms.string('et > 20 && hadronicOverEm < 0.1 && r9 > 0.8')
 )
 
-# supercluster cleaning sequence (output = cleanPhotons)
-from RecoHI.HiEgammaAlgos.HiEgamma_cff import *
-
 # leading photon E_T filter
 photonFilter = cms.EDFilter("EtMinPhotonCountFilter",
-    src = cms.InputTag("cleanPhotons"),
+    src = cms.InputTag("goodPhotons"),
     etMin = cms.double(40.0),
     minNumber = cms.uint32(1)
 )
 
+# supercluster cleaning sequence (output = cleanPhotons)
+from RecoHI.HiEgammaAlgos.HiEgamma_cff import *
+
+# photon selection of spike-cleaned collection
+goodCleanPhotons = goodPhotons.clone(src=cms.InputTag("cleanPhotons"))
+
+# leading E_T filter on cleaned collection
+cleanPhotonFilter = photonFilter.clone(src=cms.InputTag("goodCleanPhotons"))
+
 # photon skim sequence
 photonSkimSequence = cms.Sequence(hltPhotonHI
                                   * goodPhotons
-                                  * hiPhotonCleaningSequence
                                   * photonFilter
+                                  * hiPhotonCleaningSequence
+                                  * goodCleanPhotons
+                                  * cleanPhotonFilter
                                   )
 
 # two-photon E_T filter
@@ -41,7 +49,7 @@ twoPhotonFilter = cms.EDFilter("EtMinPhotonCountFilter",
 photonCombiner = cms.EDProducer("CandViewShallowCloneCombiner",
   checkCharge = cms.bool(False),
   cut = cms.string('60 < mass < 120'),
-  decay = cms.string('cleanPhotons cleanPhotons')
+  decay = cms.string('goodCleanPhotons goodCleanPhotons')
 )
 
 photonPairCounter = cms.EDFilter("CandViewCountFilter",
@@ -54,6 +62,7 @@ zEESkimSequence = cms.Sequence(hltPhotonHI
                                * goodPhotons
                                * twoPhotonFilter
                                * hiPhotonCleaningSequence
+                               * goodCleanPhotons
                                * photonCombiner
                                * photonPairCounter
                                )
