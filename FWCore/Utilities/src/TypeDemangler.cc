7a5167630217b6de16a62c9ca4a7f16dff05e543
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-11-13 03:38:13 +0000

    Move const before identifier to be consistent with REFLEX

--- FWCore/Utilities/src/TypeDemangler.cc
+++ FWCore/Utilities/src/TypeDemangler.cc
@@ -3,23 +3,23 @@
 #include "FWCore/Utilities/interface/Exception.h"
 
 /********************************************************************
-	TypeDemangler is used to demangle the mangled name provided by type_info into the type name.
+        TypeDemangler is used to demangle the mangled name provided by type_info into the type name.
 
-	The conventions used (e.g. const qualifiers before identifiers, no spaces after commas)
-	are chosen to match the type names that can be used by the plug-in manager to load data dictionaries.
-	It also strips comparators from (multi) maps or sets, and always strips allocators.
+        The conventions used (e.g. const qualifiers before identifiers, no spaces after commas)
+        are chosen to match the type names that can be used by the plug-in manager to load data dictionaries.
+        It also strips comparators from (multi) maps or sets, and always strips allocators.
 
-	This demangler is platform dependent.  This version works for gcc.
+        This demangler is platform dependent.  This version works for gcc.
 
-	Known limitations:
+        Known limitations:
 
-	0) It is platform dependent. See above.
+        0) It is platform dependent. See above.
 
-	1) It does not demangle function names, only type names.
+        1) It does not demangle function names, only type names.
 
-	2) If an enum value is used as a non-type template parameter, the demangled name cannot
-	be used successfully to load the dictionary.  This is because the enumerator value name
-	(used by Reflex) is not available in the mangled name (on this platform).
+        2) If an enum value is used as a non-type template parameter, the demangled name cannot
+        be used successfully to load the dictionary.  This is because the enumerator value name
+        (used by Reflex) is not available in the mangled name (on this platform).
 
 ********************************************************************/
 namespace {
@@ -41,7 +41,7 @@ namespace {
       int depth = 1;
       std::string::size_type inx = index + asize;
       while ((inx = demangledName.find_first_of(delimiters, inx)) != std::string::npos) {
-	if (demangledName[inx] == '<') {
+        if (demangledName[inx] == '<') {
           ++depth;
         } else {
           --depth;
@@ -57,6 +57,28 @@ namespace {
       }
     } 
   }
+
+  void
+  constBeforeIdentifier(std::string& demangledName) {
+    std::string const toBeMoved(" const");
+    std::string::size_type const asize = toBeMoved.size();
+    std::string::size_type index = std::string::npos;
+    while ((index = demangledName.find(toBeMoved)) != std::string::npos) {
+      demangledName.erase(index, asize);
+      int depth = 0;
+      for (std::string::size_type inx = index - 1; inx > 0; --inx) {
+        char const c = demangledName[inx];
+        if (c == '>') {
+          ++depth;
+        } else if (depth > 0) {
+          if (c == '<') --depth;
+        } else if (c == '<' || c == ',') {
+          demangledName.insert(inx + 1, "const ");
+          break;
+        }
+      }
+    }
+  }
 }
 
 namespace edm {
@@ -67,13 +89,17 @@ namespace edm {
     char* const null = 0;
     demangledName += abi::__cxa_demangle(name, null, nullSize, &status);
     if (status != 0) {
-       throw cms::Exception("Demangling error") << " '" << name << "'\n";
+      throw cms::Exception("Demangling error") << " '" << name << "'\n";
     } 
     // We must use the same conventions used by REFLEX.
     noSpaceAfterComma(demangledName);
+    // Strip default allocator
     std::string const allocator(",std::allocator<");
     removeParameter(demangledName, allocator);
+    // Strip default comparator
     std::string const comparator(",std::less<");
     removeParameter(demangledName, comparator);
+    // Put const qualifier before identifier.
+    constBeforeIdentifier(demangledName);
   }
 }
