3843bafe516fca5ae5d008c6fc24dc93272305d9
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-11-14 17:46:39 +0000

    Backing out Christians changes in 1.3 for now, these need to be properly optimized

--- RecoTauTag/RecoTau/python/PFRecoTauQualityCuts_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauQualityCuts_cfi.py
@@ -4,27 +4,27 @@ import FWCore.ParameterSet.Config as cms
 # different for the signal and isolation regions.  (Currently, only in Nhits)
 
 PFTauQualityCuts = cms.PSet(
-    signalQualityCuts = cms.PSet(
-        minTrackPt                   = cms.double(0.5),  # filter PFChargedHadrons below given pt
-        maxTrackChi2                 = cms.double(100.), # require track Chi2
-        maxTransverseImpactParameter = cms.double(0.1),  # w.r.t. PV
-        maxDeltaZ                    = cms.double(1.0),  # w.r.t. PV
-        minTrackPixelHits            = cms.uint32(0),    # pixel-only hits (note that these cuts are turned off, 
+      signalQualityCuts = cms.PSet(
+         minTrackPt                   = cms.double(0.5),  # filter PFChargedHadrons below given pt
+         maxTrackChi2                 = cms.double(100.), # require track Chi2
+         maxTransverseImpactParameter = cms.double(0.03), # w.r.t. PV
+         maxDeltaZ                    = cms.double(0.2),  # w.r.t. PV
+         minTrackPixelHits            = cms.uint32(0),    # pixel-only hits (note that these cuts are turned off, 
                                                           # the tracking cuts might be higher)
-        minTrackHits                 = cms.uint32(3),    # total track hits
-        minGammaEt                   = cms.double(0.5),  # filter PFgammas below given Pt
-        useTracksInsteadOfPFHadrons  = cms.bool(False),  # if true, use generalTracks, instead of PFChargedHadrons
-    ),
-    isolationQualityCuts = cms.PSet(
-        minTrackPt                   = cms.double(1.0), 
-        maxTrackChi2                 = cms.double(100.),
-        maxTransverseImpactParameter = cms.double(0.1),
-        maxDeltaZ                    = cms.double(1.0),
-        minTrackPixelHits            = cms.uint32(0),
-        minTrackHits                 = cms.uint32(5),
-        minGammaEt                   = cms.double(1.5),
-        useTracksInsteadOfPFHadrons  = cms.bool(False),
-    )
+         minTrackHits                 = cms.uint32(3),    # total track hits
+         minGammaEt                   = cms.double(0.5),  # filter PFgammas below given Pt
+         useTracksInsteadOfPFHadrons  = cms.bool(False),  # if true, use generalTracks, instead of PFChargedHadrons
+      ),
+      isolationQualityCuts = cms.PSet(
+         minTrackPt                   = cms.double(1.0), 
+         maxTrackChi2                 = cms.double(100.),
+         maxTransverseImpactParameter = cms.double(0.03),
+         maxDeltaZ                    = cms.double(0.2),
+         minTrackPixelHits            = cms.uint32(0),
+         minTrackHits                 = cms.uint32(8),
+         minGammaEt                   = cms.double(1.5),
+         useTracksInsteadOfPFHadrons  = cms.bool(False),
+      )
 )
 
 
