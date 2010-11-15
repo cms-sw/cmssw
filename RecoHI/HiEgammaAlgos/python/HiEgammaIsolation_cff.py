9dd712cf4866680044258f510d0e88471fb90a3f
Yongsun Kim <sha1-cf1309631de6d8b3179fc471ac6b6474eee4ad5d@cern.ch>
2010-11-15 16:30:49 +0000

    *** empty log message ***

--- RecoHI/HiEgammaAlgos/python/HiEgammaIsolation_cff.py
+++ RecoHI/HiEgammaAlgos/python/HiEgammaIsolation_cff.py
@@ -8,6 +8,10 @@ from RecoEgamma.PhotonIdentification.photonId_cff import *
 
 from RecoEcal.EgammaClusterProducers.reducedRecHitsSequence_cff import *
 
+interestingEcalDetIdEB.basicClustersLabel = cms.InputTag("islandBasicClusters","islandBarrelBasicClusters")
+interestingEcalDetIdEE.basicClustersLabel = cms.InputTag("islandBasicClusters","islandEndcapBasicClusters")
+
+
 gamIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = cms.InputTag("reducedEcalRecHitsEB")
 gamIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = cms.InputTag("reducedEcalRecHitsEE")
 
