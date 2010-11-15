6ea440ae0878b28e3a6f9b430193f7ff4db0c762
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-11-15 18:11:28 +0000

    *** empty log message ***

--- RecoEgamma/EgammaHLTProducers/python/hltElectronTrackIsol_cfi.py
+++ RecoEgamma/EgammaHLTProducers/python/hltElectronTrackIsol_cfi.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-hltEgammaElectronTrackIsolationProducers = cms.EDProducer( "EgammaHLTElectronTrackIsolationProducers",
-    egTrkIsoVetoConeSize = cms.double( 0.03 ),
-    trackProducer = cms.InputTag( "ctfWithMaterialTracks" ),
-    electronProducer = cms.InputTag( "pixelMatchElectronsForHLT" ),
-    egTrkIsoConeSize = cms.double( 0.3 ),
-    egTrkIsoRSpan = cms.double( 999999.0 ),
-    egTrkIsoPtMin = cms.double( 1.0 ),
-    egTrkIsoZSpan = cms.double( 0.15 ),
+hltEgammaElectronTrackIsolationProducers= cms.EDProducer( "EgammaHLTElectronTrackIsolationProducers",
+    electronProducer = cms.InputTag( "ctfWithMaterialTracks" ),
+    trackProducer = cms.InputTag( "pixelMatchElectronsForHLT" ),
     egCheckForOtherEleInCone = cms.untracked.bool( False ),
+    egTrkIsoPtMin = cms.double( 1.0 ),
+    egTrkIsoConeSize = cms.double( 0.3 ),
+    egTrkIsoZSpan = cms.double( 0.15 ),
+    egTrkIsoRSpan = cms.double( 999999.0 ),
+    egTrkIsoVetoConeSize = cms.double( 0.03 ),
     egTrkIsoStripBarrel = cms.double( 0.03 ),
     egTrkIsoStripEndcap = cms.double( 0.03 )
 )
