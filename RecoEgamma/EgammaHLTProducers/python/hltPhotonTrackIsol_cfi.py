1d8bb8ee1b4ebe86ca2b5da3bcc2449c8687a656
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-11-16 09:25:56 +0000

    add missing parameters

--- RecoEgamma/EgammaHLTProducers/python/hltPhotonTrackIsol_cfi.py
+++ RecoEgamma/EgammaHLTProducers/python/hltPhotonTrackIsol_cfi.py
@@ -1,13 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-hltPhotonTrackIsol = cms.EDProducer("EgammaHLTPhotonTrackIsolationProducersRegional",
-    recoEcalCandidateProducer = cms.InputTag("hltEgammaHcalIsolFilter"),
-    trackProducer = cms.InputTag("ctfWithMaterialTracks"),
-    countTracks = cms.bool(False),
-    egTrkIsoPtMin = cms.double(1.5),
-    egTrkIsoConeSize = cms.double(0.3),
-    egTrkIsoZSpan = cms.double(999999.0),
-    egTrkIsoRSpan = cms.double(999999.0),
-    egTrkIsoVetoConeSize = cms.double(0.0)
+hltEgammaPhotonTrackIsolationProducersRegional= cms.EDProducer( "EgammaHLTPhotonTrackIsolationProducersRegional",
+    recoEcalCandidateProducer = cms.InputTag( "hltEgammaHcalIsolFilter" ),
+    trackProducer = cms.InputTag( "ctfWithMaterialTracks" ),
+    countTracks = cms.bool( False ),
+    egTrkIsoPtMin = cms.double( 1.0 ),
+    egTrkIsoConeSize = cms.double( 0.29 ),
+    egTrkIsoZSpan = cms.double( 999999.0 ),
+    egTrkIsoRSpan = cms.double( 999999.0 ),
+    egTrkIsoVetoConeSize = cms.double( 0.06 ),
+    egTrkIsoStripBarrel = cms.double( 0.03 ),
+    egTrkIsoStripEndcap = cms.double( 0.03 )
 )
 
