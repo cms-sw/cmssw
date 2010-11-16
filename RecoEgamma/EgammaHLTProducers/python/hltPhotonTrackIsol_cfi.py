3b5edc7cfd01ef6974d038d58417b9d3dd0a071e
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-11-16 09:24:44 +0000

    clean up

--- RecoEgamma/EgammaHLTProducers/python/hltPhotonTrackIsol_cfi.py
+++ RecoEgamma/EgammaHLTProducers/python/hltPhotonTrackIsol_cfi.py
@@ -1,18 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-#
-# producer for hltPhotonTrackIsol
-#
 hltPhotonTrackIsol = cms.EDProducer("EgammaHLTPhotonTrackIsolationProducersRegional",
-    egTrkIsoVetoConeSize = cms.double(0.0),
-    trackProducer = cms.InputTag("ctfWithMaterialTracks"),
-    egTrkIsoConeSize = cms.double(0.3),
-    egTrkIsoRSpan = cms.double(999999.0),
     recoEcalCandidateProducer = cms.InputTag("hltEgammaHcalIsolFilter"),
-    #InputTag trackProducer       = hltSingleCtfWithMaterialTracks
+    trackProducer = cms.InputTag("ctfWithMaterialTracks"),
+    countTracks = cms.bool(False),
     egTrkIsoPtMin = cms.double(1.5),
+    egTrkIsoConeSize = cms.double(0.3),
     egTrkIsoZSpan = cms.double(999999.0),
-    countTracks = cms.bool(False)
+    egTrkIsoRSpan = cms.double(999999.0),
+    egTrkIsoVetoConeSize = cms.double(0.0)
 )
-
 
