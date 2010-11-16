406ee2554618f1858ac3ea625d91486536010046
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2010-11-16 08:57:01 +0000

    Moved here from EventFilter/Utilities since EvFFEDSelector doesn't depend on xdaq.

---
+++ EventFilter/FEDInterface/plugins/Module.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
+#include "EventFilter/Utilities/plugins/EvFFEDSelector.h"
+
+using namespace evf;
+
+DEFINE_FWK_MODULE(EvFFEDSelector);
