dfde6c19cfbb8da2110c5309f6a73a10d4096d80
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2010-11-17 15:52:10 +0000

    - add pixel(..) also taking track angles, - extend interface by method implemented in RectangularPixelTopology   * not yet isItBigPixelIn<X|Y> since that requires more changes (from Giovanni Petrucciani)

--- Geometry/CommonTopologies/interface/PixelTopology.h
+++ Geometry/CommonTopologies/interface/PixelTopology.h
@@ -3,10 +3,6 @@
 
 #include "Geometry/CommonTopologies/interface/Topology.h"
 
-
-class Topology;
-
-
 /**   
  * Interface for all pixel topologies  
  */  
@@ -23,11 +19,29 @@ class PixelTopology : public Topology {
   //  virtual int channel( const LocalPoint& p) const = 0;
   
   virtual std::pair<float,float> pixel( const LocalPoint& p) const = 0;
+
+  /// conversion taking also the angle from the track state
+  virtual std::pair<float,float> pixel( const LocalPoint& p, const Topology::LocalTrackAngles &ltp ) const { 
+    return pixel(p); 
+  }
   
   virtual std::pair<float,float> pitch() const = 0;
   virtual int nrows() const = 0;
   virtual int ncolumns() const = 0;
-  
+
+  virtual float localX(const float mpX) const = 0;
+  virtual float localY(const float mpY) const = 0;
+
+/*  GF: These two need to be added! */
+/*   virtual bool isItBigPixelInX(const int ixbin) const = 0;  */
+/*   virtual bool isItBigPixelInY(const int iybin) const = 0;  */
+  virtual bool containsBigPixelInX(const int& ixmin, const int& ixmax) const = 0;
+  virtual bool containsBigPixelInY(const int& iymin, const int& iymax) const = 0;
+
+  virtual bool isItEdgePixelInX (int ixbin) const = 0;
+  virtual bool isItEdgePixelInY (int iybin) const = 0;
+  virtual bool isItEdgePixel (int ixbin, int iybin) const = 0;
+
 };
 
 #endif
