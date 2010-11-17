345225ce216436369009e33f0342aa7e367fe301
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2010-11-17 17:15:35 +0000

    add HLTpath

--- DPGAnalysis/Skims/python/dtActivitySkim_cff.py
+++ DPGAnalysis/Skims/python/dtActivitySkim_cff.py
@@ -38,7 +38,7 @@ hltDTActivityFilter = HLTrigger.special.hltDTActivityFilter_cfi.hltDTActivityFil
 # this is for filtering on HLT path
 HLTDT =cms.EDFilter("HLTHighLevel",
      TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-     HLTPaths = cms.vstring('HLT_L1MuOpen','HLT_Activity_DT'),           # provide list of HLT paths (or patterns) you want
+     HLTPaths = cms.vstring('HLT_L1MuOpen','HLT_Activity_DT','HLT_Activity_DT_Tuned'),           # provide list of HLT paths (or patterns) you want
      eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
      andOr = cms.bool(True),             # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
      throw = cms.bool(False)    # throw exception on unknown path names
