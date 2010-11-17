8bca6696d81226e3f2f1e564e07a3e78fcca3651
Seth Cooper <sha1-32dd4c14096c6277ce1889fd6c006e25f1f45009@cern.ch>
2010-11-17 09:21:10 +0000

    Updating with dE/dx and more information.

--- SUSYBSMAnalysis/HSCP/interface/BetaCalculatorECAL.h
+++ SUSYBSMAnalysis/HSCP/interface/BetaCalculatorECAL.h
@@ -1,34 +1,68 @@
 // system include files
 #include <memory>
+#include <vector>
+#include <map>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 
 #include "DataFormats/Common/interface/Handle.h" 
-#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloTopology/interface/CaloTopology.h"
 
 #include "TrackingTools/TrackAssociator/interface/TrackDetectorAssociator.h"
 #include "TrackingTools/TrackAssociator/interface/TrackAssociatorParameters.h"
+#include "TrackingTools/TrackAssociator/interface/DetIdAssociator.h"
+#include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
 
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 #include "AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h"
 
 using namespace susybsm;
 
-class  BetaCalculatorECAL{
+class BetaCalculatorECAL {
+
    public:
-      BetaCalculatorECAL(const edm::ParameterSet& iConfig);
-      void  addInfoToCandidate(HSCParticle& candidate, edm::Handle<reco::TrackCollection>& tracks, edm::Event& iEvent, const edm::EventSetup& iSetup);
+       BetaCalculatorECAL(const edm::ParameterSet& iConfig);
+       void  addInfoToCandidate(HSCParticle& candidate, edm::Handle<reco::TrackCollection>& tracks, edm::Event& iEvent, const edm::EventSetup& iSetup);
 
    private:
-       TrackDetectorAssociator trackAssociator_; 
-       TrackAssociatorParameters parameters_; 
+       int getDetailedTrackLengthInXtals(std::map<int,GlobalPoint>& trackExitPositionMap,
+           std::map<int,float>& trackCrossedXtalMap,
+           double& totalLengthCurved,
+           GlobalPoint& internalPointCurved,
+           GlobalPoint& externalPointCurved,
+           const CaloGeometry* theGeometry,
+           const CaloTopology * theTopology,
+           const std::vector<SteppingHelixStateInfo>& neckLace);
+       std::vector<SteppingHelixStateInfo> calcEcalDeposit(const FreeTrajectoryState* tkInnerState,
+           const DetIdAssociator& associator);
+       void addStepToXtal(std::map<int,GlobalPoint>& trackExitPositionMap,
+           std::map<int,float>& trackCrossedXtalMap,
+           DetId aDetId,
+           float step,
+           GlobalPoint point,
+           const CaloSubdetectorGeometry* theSubdetGeometry);
+
+      // Data members
+      TrackDetectorAssociator trackAssociator_; 
+      TrackAssociatorParameters parameters_; 
+      edm::InputTag EBRecHitCollection_;
+      edm::InputTag EERecHitCollection_;
+
+      edm::ESHandle<DetIdAssociator> ecalDetIdAssociator_;
+      edm::ESHandle<MagneticField> bField_;
+      edm::ESHandle<CaloGeometry> theCaloGeometry_;
+
 };
 
 
