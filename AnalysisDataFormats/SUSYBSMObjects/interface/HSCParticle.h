daa0fb400e49a7e1a5f48ff3122418632494f69a
Seth Cooper <sha1-32dd4c14096c6277ce1889fd6c006e25f1f45009@cern.ch>
2010-11-17 09:16:44 +0000

    Adding additional ECAL data members.

--- AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h
+++ AnalysisDataFormats/SUSYBSMObjects/interface/HSCParticle.h
@@ -7,6 +7,7 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/DeDxData.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 #include <vector>
 
 #include "DataFormats/MuonReco/interface/MuonTimeExtra.h"
@@ -40,11 +41,45 @@ namespace susybsm {
  class CaloBetaMeasurement
   {
    public:
-     float hcalenergy, ecalenergy, hoenergy;
+     float hcalCrossedEnergy, ecalCrossedEnergy, hoCrossedEnergy;
+     float ecalDeDx;
      float ecal3by3dir, ecal5by5dir;
      float hcal3by3dir, hcal5by5dir;
-     float trkisodr;
-     float ecaltime, ecalbeta;
+     float trkIsoDr;
+     float ecalTime, ecalTimeError;
+     float ecalBeta, ecalBetaError;
+     float ecalInvBetaError;
+     int ecalCrysCrossed;
+     std::vector<float> ecalSwissCrossKs;
+     std::vector<float> ecalE1OverE9s;
+     std::vector<float> ecalTrackLengths;
+     std::vector<float> ecalEnergies;
+     std::vector<float> ecalTimes;
+     std::vector<float> ecalTimeErrors;
+     std::vector<float> ecalChi2s;
+     std::vector<float> ecalOutOfTimeChi2s;
+     std::vector<float> ecalOutOfTimeEnergies;
+     std::vector<DetId> ecalDetIds;
+     std::vector<GlobalPoint> ecalTrackExitPositions;
+
+     CaloBetaMeasurement()
+     {
+       hcalCrossedEnergy = -9999;
+       ecalCrossedEnergy = -9999;
+       hoCrossedEnergy = -9999;
+       ecalDeDx = -9999;
+       ecal3by3dir = -9999;
+       ecal5by5dir = -9999;
+       hcal3by3dir = -9999;
+       hcal5by5dir = -9999;
+       trkIsoDr = -9999;
+       ecalTime = -9999;
+       ecalTimeError = -9999;
+       ecalBeta = -9999;
+       ecalBetaError = -9999;
+       ecalInvBetaError = -9999;
+       ecalCrysCrossed = 0;
+     }
   };
 
  class HSCParticle 
