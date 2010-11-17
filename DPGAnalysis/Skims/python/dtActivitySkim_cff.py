c97289d9da81d7873d61734528763ff8aaa344da
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2010-11-17 16:46:54 +0000

    update the DT activity skim

--- DPGAnalysis/Skims/python/dtActivitySkim_cff.py
+++ DPGAnalysis/Skims/python/dtActivitySkim_cff.py
@@ -6,16 +6,33 @@ muonDTDigis = dtunpacker.clone()
 import HLTrigger.special.hltDTActivityFilter_cfi
 
 hltDTActivityFilter = HLTrigger.special.hltDTActivityFilter_cfi.hltDTActivityFilter.clone(
-    inputDCC         = cms.InputTag( "dttfDigis" ),   
-    inputDDU         = cms.InputTag( "muonDTDigis" ),   
-    inputDigis       = cms.InputTag( "muonDTDigis" ),   
-    processDCC       = cms.bool( False ),   
+   inputDCC         = cms.InputTag( "dttfDigis" ),
+    inputDDU         = cms.InputTag( "muonDTDigis" ),
+    inputRPC         = cms.InputTag( "hltGtDigis" ),
+    inputDigis       = cms.InputTag( "muonDTDigis" ),
+    processDCC       = cms.bool( False ),
     processDDU       = cms.bool( False ),
     processRPC       = cms.bool( False ),
-    processDigis     = cms.bool( True ),   
-    minChamberLayers = cms.int32( 6 ),
-    minDDUBX         = cms.int32( 9 ),
-    maxDDUBX         = cms.int32( 14 ),
+    processDigis     = cms.bool( True ),
+
+    maxDeltaPhi = cms.double( 1.0 ),
+    maxDeltaEta = cms.double( 0.3 ),
+
+    orTPG         = cms.bool( True ),
+    orRPC         = cms.bool( True ),
+    orDigi        = cms.bool( True ),
+
+    minChamberLayers = cms.int32( 5 ),
+    maxStation       = cms.int32( 3 ),
+    minTPGQual       = cms.int32( 2 ),   # 0-1=L 2-3=H 4=LL 5=HL 6=HH
+    minDDUBX         = cms.int32( 8 ),
+    maxDDUBX         = cms.int32( 13 ),
+    minDCCBX         = cms.int32( -1 ),
+    maxDCCBX         = cms.int32( 1 ),
+    minRPCBX         = cms.int32( -1 ),
+    maxRPCBX         = cms.int32( 1 ),
+    minActiveChambs  = cms.int32( 1 ),
+    activeSectors    = cms.vint32(1,2,3,4,5,6,7,8,9,10,11,12) 
 )
 
 # this is for filtering on HLT path
