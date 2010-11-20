368eaeedf2daa14007cff25918f2077bac2bc712
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2010-11-20 10:44:33 +0000

    *** empty log message ***

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
@@ -27,7 +27,6 @@ hcalminbiasHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
 )
 
 seqALCARECOHcalCalMinBias = cms.Sequence(hcalminbiasHLT*hcalDigiAlCaMB*gtDigisAlCaMB*hbherecoNoise*hfrecoNoise*hfrecoMBspecial*horecoNoise)
-seqALCARECOHcalCalMinBiasHI = cms.Sequence(hcalDigiAlCaMB*gtDigisAlCaMB*hbherecoNoise*hfrecoNoise*hfrecoMBspecial*horecoNoise)
 
 gtDigisAlCaMB.DaqGtInputTag = 'source'
 
