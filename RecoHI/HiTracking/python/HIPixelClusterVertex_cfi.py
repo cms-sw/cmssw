5f444a951b9d7b013f3cda10e490b78e2969ca65
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2010-11-20 00:25:06 +0000

    settings updated from experience with 2010 data

--- RecoHI/HiTracking/python/HIPixelClusterVertex_cfi.py
+++ RecoHI/HiTracking/python/HIPixelClusterVertex_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 hiPixelClusterVertex = cms.EDProducer("HIPixelClusterVtxProducer",
                                       pixelRecHits=cms.string("siPixelRecHits"),
-                                      minZ=cms.double(-20.0),
-                                      maxZ=cms.double(20.05),
+                                      minZ=cms.double(-30.0),
+                                      maxZ=cms.double(30.05),
                                       zStep=cms.double(0.1)
                                       )
 
