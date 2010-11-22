6c6aac289c248ac46c81f458574a2637dbeebb1a
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2010-11-22 10:44:42 +0000

    renaming of redudedRecHitsSequence to reducedEcalRecHitsSequence

--- Validation/Configuration/python/ECALHCAL.py
+++ Validation/Configuration/python/ECALHCAL.py
@@ -54,9 +54,9 @@ def customise(process):
     process.ecalRecHit.recoverEEFE = cms.bool(False)
 
 
-#    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalLocalRecoSequence * process.hbhereco * process.hfreco * process.horeco * (process.ecalClusters+process.caloTowersRec) * process.reducedRecHitsSequence )
+#    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalLocalRecoSequence * process.hbhereco * process.hfreco * process.horeco * (process.ecalClusters+process.caloTowersRec) * process.reducedEcalRecHitsSequence )
 
-    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalPacker * process.esDigiToRaw * process.hcalRawData * process.rawDataCollector * process.ecalDigis * process.ecalPreshowerDigis * process.hcalDigis * process.calolocalreco *(process.ecalClusters+process.caloTowersRec) * process.reducedRecHitsSequence )
+    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalPacker * process.esDigiToRaw * process.hcalRawData * process.rawDataCollector * process.ecalDigis * process.ecalPreshowerDigis * process.hcalDigis * process.calolocalreco *(process.ecalClusters+process.caloTowersRec) * process.reducedEcalRecHitsSequence )
 
     process.schedule.append(process.local_digireco)
 
