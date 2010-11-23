5978be03ec97d5186ca6d4d9842a2b6c30dca9cd
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2010-11-23 15:09:41 +0000

    updated HI globaltag

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -7,5 +7,5 @@ autoCond = { 'mc'      : 'MC_310_V0::All',
              'craft09' : 'CRFT9_39R_V2::All',
              'com10'   : 'GR_R_310_V0::All',
              'hltonline' : 'GR_R_310_V0::All', # same as com10 until H tag available
-             'starthi' : 'START39_V4HI::All'
+             'starthi' : 'START39_V7HI::All'
              }
