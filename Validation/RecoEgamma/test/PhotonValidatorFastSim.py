7c7b850a94f372c3bfa1681b01130414c3f9d86f
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2010-11-23 12:08:09 +0000

    Update the configuration

--- Validation/RecoEgamma/test/PhotonValidatorFastSim.py
+++ Validation/RecoEgamma/test/PhotonValidatorFastSim.py
@@ -12,6 +12,7 @@ process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 process.load("DQMServices.Components.MEtoEDMConverter_cfi")
 process.load("Validation.RecoEgamma.photonValidationSequence_cff")
 process.load("Validation.RecoEgamma.photonPostprocessing_cfi")
+process.load("Validation.RecoEgamma.conversionPostprocessing_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'START39_V3::All'
 
@@ -30,36 +31,39 @@ process.maxEvents = cms.untracked.PSet(
 
 from Validation.RecoEgamma.photonValidationSequence_cff import *
 from Validation.RecoEgamma.photonPostprocessing_cfi import *
+from Validation.RecoEgamma.conversionPostprocessing_cfi import *
 
-photonValidation.OutputMEsInRootFile = True
-photonValidation.OutputFileName = 'PhotonValidationRelVal3_10_0_pre2_H130GGgluonfusion_FastSim.root'
-
+photonValidation.OutputFileName = 'PhotonValidationRelVal392_H130GGgluonfusion_FastSim.root'
 photonValidation.fastSim = True
-
-photonPostprocessing.batch = cms.bool(True)
+photonPostprocessing.standalone = cms.bool(True)
 photonPostprocessing.InputFileName = photonValidation.OutputFileName
+photonPostprocessing.OuputFileName = photonValidation.OutputFileName
+
+#conversionPostprocessing.standalone = cms.bool(True)
+#conversionPostprocessing.InputFileName = tkConversionValidation.OutputFileName
+#conversionPostprocessing.OuputFileName = tkConversionValidation.OutputFileName
+
 
 process.source = cms.Source("PoolSource",
 noEventSort = cms.untracked.bool(True),
 duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
                             
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0058/168E0A6C-DFE2-DF11-8C1C-002618943896.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/E8EF2EBE-80E2-DF11-BD5D-00261894389A.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/DA005739-80E2-DF11-BE4C-003048678EE2.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/C64400A9-82E2-DF11-A6FA-002618943954.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/B6746E1D-80E2-DF11-B2BF-0026189438E4.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/A8BBA4BC-80E2-DF11-8859-0026189438D4.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/920921C2-80E2-DF11-BE5D-002618943860.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/680681C1-80E2-DF11-94E2-003048678F78.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/58165CC3-80E2-DF11-A3CB-0018F3D0965C.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/524D9A40-80E2-DF11-87BD-001A92810AAE.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/464F8C2B-80E2-DF11-AE25-00304867904E.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/42B4021D-80E2-DF11-A19D-001A92811702.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/1ECC5CBA-80E2-DF11-9EB8-00248C55CC62.root',
-        '/store/relval/CMSSW_3_10_0_pre2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0057/0699064D-80E2-DF11-8E58-0018F3D0965C.root'
 
- 
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0073/12994C21-A8E9-DF11-A178-001A928116C2.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/E49CB54D-11E8-DF11-A4EB-0026189438EB.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/DCAF69E2-12E8-DF11-94DA-0018F3D096BC.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/D0FF0A65-14E8-DF11-8D27-0026189438FF.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/B096A94D-11E8-DF11-8DD5-00248C0BE005.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/AA786CEF-11E8-DF11-A9C5-001A92971B88.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/A251DC53-12E8-DF11-B2DA-001A9281173A.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/86D9DFE9-11E8-DF11-86D3-001A92810ACE.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/865BC9EC-11E8-DF11-82B8-001A92971BB4.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/74B1C8EF-11E8-DF11-8894-001A9281170E.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/72194F4F-11E8-DF11-9667-002618943956.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/6820D04E-11E8-DF11-8A29-0026189437F8.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/4E1BE7EC-11E8-DF11-A03F-001A92811706.root',
+        '/store/relval/CMSSW_3_9_2/RelValH130GGgluonfusion/GEN-SIM-DIGI-RECO/START39_V3_FastSim-v1/0067/2C34F1E6-11E8-DF11-8BDD-003048678FF6.root'
 
     )
     )
@@ -97,5 +101,6 @@ process.FEVT = cms.OutputModule("PoolOutputModule",
 
 
 
-process.p1 = cms.Path(process.tpSelection*process.photonValidationSequence*process.photonPostprocessing*process.dqmStoreStats)
+#process.p1 = cms.Path(process.tpSelection*process.photonValidationSequence*process.photonPostprocessing*process.conversionPostprocessing*process.dqmStoreStats)
+process.p1 = cms.Path(process.tpSelection*process.photonValidation*process.photonPostprocessing*process.dqmStoreStats)
 process.schedule = cms.Schedule(process.p1)
