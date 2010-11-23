60d8550614078f61e9b712433ecc759f988f57b4
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-11-23 22:43:33 +0000

    Test for permitted filtering on end path

--- FWCore/Framework/test/run_trigbit.sh
+++ FWCore/Framework/test/run_trigbit.sh
@@ -8,11 +8,13 @@ F2=${LOCAL_TEST_DIR}/testBitsFail_cfg.py
 F3=${LOCAL_TEST_DIR}/testBitsMove_cfg.py
 F4=${LOCAL_TEST_DIR}/testBitsCount_cfg.py
 F5=${LOCAL_TEST_DIR}/testFilterIgnore_cfg.py
+F6=${LOCAL_TEST_DIR}/testFilterOnEndPath_cfg.py
 
 (cmsRun $F1 ) || die "Failure using $F1" $?
 (cmsRun $F2 ) || die "Failure using $F2" $?
 (cmsRun $F3 ) || die "Failure using $F3" $?
 (cmsRun $F4 ) || die "Failure using $F4" $?
 (cmsRun $F5 ) || die "Failure using $F5" $?
+(cmsRun $F6 ) || die "Failure using $F6" $?
 
 
