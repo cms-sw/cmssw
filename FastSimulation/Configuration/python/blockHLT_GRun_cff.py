4d1edab0001b61f6860ca76ad49f6a135185f752
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-11-24 22:28:54 +0000

    update HLT to /dev/CMSSW_3_10_0/pre6/GRun/V4

--- FastSimulation/Configuration/python/blockHLT_GRun_cff.py
+++ FastSimulation/Configuration/python/blockHLT_GRun_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_8_5/GRun/V42 (CMSSW_3_8_1_HLT25)
+# /dev/CMSSW_3_10_0/pre6/GRun/V4 (CMSSW_3_10_X_2010-11-23-0200_HL1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_8_5/GRun/V42')
+  tableName = cms.string('/dev/CMSSW_3_10_0/pre6/GRun/V4')
 )
 
 
