82b235ee166cef1e3bd4d774cf9bf1da85024531
Alexandr Zhokin <sha1-3934d39e6468375f9412692c75d9c3501c9552f4@cern.ch>
2010-11-26 15:48:02 +0000

    update for 394

--- RecoRomanPot/RecoFP420/test/data/runHecDigiCluTrack_cfg.py
+++ RecoRomanPot/RecoFP420/test/data/runHecDigiCluTrack_cfg.py
@@ -7,7 +7,12 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
 
-process.load("Configuration.StandardSequences.VtxSmearedGauss_cff")
+#process.load("Configuration.StandardSequences.VtxSmearedGauss_cff")
+# event vertex smearing - applies only once (internal check)
+# Note : all internal generators will always do (0,0,0) vertex
+#
+process.load("IOMC.EventVertexGenerators.VtxSmearedGauss_cfi")
+
 
 process.load("Geometry.FP420CommonData.FP420GeometryXML_cfi")
 
@@ -17,6 +22,15 @@ process.load("SimG4Core.Application.g4SimHits_cfi")
 process.load("SimTransport.HectorProducer.HectorTransport_cfi")
 process.transport = cms.Path(process.LHCTransport)
 
+process.LHCTransport.ZDCTransport = cms.bool(False) ## main flag to set transport for FP420
+
+process.LHCTransport.Hector.smearEnergy = cms.bool(True)
+#process.LHCTransport.Hector.sigmaEnergy    = cms.double(0.001)## GeV
+
+process.LHCTransport.Hector.smearAng    = cms.bool(True)
+#process.LHCTransport.Hector.sigmaSTX    = cms.double(0.01)## urad
+#process.LHCTransport.Hector.sigmaSTY    = cms.double(0.01)## urad
+
 #process.LHCTransport.ZDCTransport = cms.bool(False) ## main flag to set transport for FP420
 
 #process.LHCTransport.Hector.smearEnergy = cms.bool(True)
@@ -25,9 +39,15 @@ process.transport = cms.Path(process.LHCTransport)
 #process.LHCTransport.Hector.smearAng    = cms.bool(True)
 #process.LHCTransport.Hector.sigmaSTX    = cms.double(0.01)## urad
 #process.LHCTransport.Hector.sigmaSTY    = cms.double(0.01)## urad
+
+
+
 ## to be changed :
 ##process.LHCTransport.Hector.Beam1 = cms.string('SimTransport/HectorProducer/data/LHCB1IR5_7TeV.tfs'),
 ##process.LHCTransport.Hector.Beam2 = cms.string('SimTransport/HectorProducer/data/LHCB2IR5_7TeV.tfs'),
+
+
+
 
 process.load("SimGeneral.MixingModule.mixNoPU_cfi")
 
@@ -50,13 +70,11 @@ process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(1000)
 )
 process.source = cms.Source("PoolSource",
-#    fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/z/zhokin/fff/FP420development/data/Exhume_Hbb.root')
-   fileNames = cms.untracked.vstring('file:ExHuME_CEPHiggsTobb_14TeV_cff_py_GEN_20events.root')
+fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/z/zhokin/fff/FP420development/data/ExhumeHbb/V384/ExHuME_CEPHiggs200Tobb_14TeV_cff_py_GEN.root')
 )
-
 process.o1 = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring('drop *', 
         'keep edmHepMCProduct_*_*_*', 
@@ -67,8 +85,10 @@ process.o1 = cms.OutputModule("PoolOutputModule",
         'keep ClusterCollectionFP420_*_*_*', 
         'keep TrackCollectionFP420_*_*_*', 
         'keep RecoCollectionFP420_*_*_*'),
-    fileName = cms.untracked.string('HecExhume_Hbb_14TeV_20ev.root')
-)
+    fileName = cms.untracked.string('HecFP420_test.root')
+                              )
+#    fileName = cms.untracked.string('HecFP420_Hbb200_14TeV_vtx0HecSmear.root')
+#    fileName = cms.untracked.string('HecExhume_Hbb_14TeV_20ev.root')
 
 process.Timing = cms.Service("Timing")
 
@@ -96,5 +116,34 @@ process.FP420Digi.ApplyTofCut = cms.bool(False)
 #process.VtxSmeared.MaxY = 0.00000001
 #process.VtxSmeared.MinZ = -0.00000001
 #process.VtxSmeared.MaxZ = 0.00000001
+##   for VtxSmearedGauss:
+process.VtxSmeared.MeanX = cms.double(0.0)
+process.VtxSmeared.MeanY = cms.double(0.0)
+process.VtxSmeared.MeanZ = cms.double(0.01)
+process.VtxSmeared.SigmaX = cms.double(0.005)
+process.VtxSmeared.SigmaY = cms.double(0.005)
+process.VtxSmeared.SigmaZ = cms.double(9.0)
+#
+#process.FP420Digi.FedFP420Algorithm = 2 # was 1
+process.FP420Digi.FedFP420LowThreshold =4. # was 6.
+process.FP420Digi.FedFP420HighThreshold =4.5 # was 6.5
+#process.FP420Digi.NoFP420Noise = True # was False
+process.FP420Digi.AdcFP420Threshold =5.0 # was 6.0
+#process.FP420Digi.AddNoisyPixels = True # was True
+#process.FP420Digi.ApplyTofCut = True   # was True
+#process.FP420Digi.VerbosityLevel = -50
+#process.FP420Cluster.VerbosityLevel = 1
+process.FP420Cluster.ChannelFP420Threshold =5.0 # was 8.
+process.FP420Cluster.ClusterFP420Threshold =6.0 # was 9.
+process.FP420Cluster.SeedFP420Threshold =5.5 # was 8.5
+#process.FP420Cluster.MaxVoidsFP420InCluster = 1 # was 1
+#process.FP420Track.VerbosityLevel = -29
+#process.FP420Track.VerbosityLevel = -30
+#process.FP420Track.VerbosityLevel = 1
+process.FP420Track.chiCutY420 = 3. # means chi2Y (was=2)
+process.FP420Track.chiCutX420 = 3. # means chi2Y (was=50)
+#process.FP420Reco.VerbosityLevel = -49
+#process.FP420Reco.VtxFlagGenRec = 1
+
 
 
