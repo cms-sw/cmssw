53998259d62ed992f43085a187962e704d206a23
Manuel Zeise <sha1-6795779774ca59e47e7b8b82fb8b5f84bb8f5870@cern.ch>
2010-11-29 17:00:28 +0000

    use preselected dimuon candidates by default

--- TauAnalysis/MCEmbeddingTools/python/PFEmbeddingSource_cff.py
+++ TauAnalysis/MCEmbeddingTools/python/PFEmbeddingSource_cff.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import FWCore.ParameterSet.Config as cms
-
+import os
   
 from Configuration.Generator.PythiaUESettings_cfi import *
 
@@ -22,24 +22,31 @@ source = cms.Source("PoolSource",
         skipEvents = cms.untracked.uint32(0),
         fileNames = cms.untracked.vstring('file:/tmp/fruboes/Zmumu/patLayer1_fromAOD_PF2PAT_full.root')
 )
+if os.path.exists("/storage/6/zeise/temp/goldenZmumuEvents_RAW_RECO_9_1_EzB.root"):
+	source.fileNames=cms.untracked.vstring("file:/storage/6/zeise/temp/goldenZmumuEvents_RAW_RECO_9_1_EzB.root")
 
 filterEmptyEv = cms.EDFilter("EmptyEventsFilter",
     minEvents = cms.untracked.int32(1),
     target =  cms.untracked.int32(1) 
 )
 
-adaptedMuonsFromDiTauCands = cms.EDProducer("CompositePtrCandidateT1T2MEtAdapter",
-    diTau  = cms.untracked.InputTag("zMuMuCandsMuEta"),
-    pfCands = cms.untracked.InputTag("particleFlow","")
-)
+#adaptedMuonsFromDiTauCands = cms.EDProducer("CompositePtrCandidateT1T2MEtAdapter",
+#    diTau  = cms.untracked.InputTag("zMuMuCandsMuEta"),
+#    pfCands = cms.untracked.InputTag("particleFlow","")
+#)
+
+#inputColl = cms.InputTag("adaptedMuonsFromDiTauCands","zMusExtracted")
+inputColl = cms.InputTag("goldenZmumuCandidatesGe1IsoMuons")
 
 dimuonsGlobal = cms.EDProducer('ZmumuPFEmbedder',
     tracks = cms.InputTag("generalTracks"),
-    selectedMuons = cms.InputTag("adaptedMuonsFromDiTauCands","zMusExtracted"),
-    keepMuonTrack = cms.bool(False)
+    selectedMuons = inputColl,
+    keepMuonTrack = cms.bool(False),
+    useCombinedCandidate = cms.untracked.bool(True),
 )
 
 generator = newSource.clone()
-generator.src = cms.InputTag("adaptedMuonsFromDiTauCands","zMusExtracted")
+generator.src = inputColl
 
-ProductionFilterSequence = cms.Sequence(adaptedMuonsFromDiTauCands*dimuonsGlobal*generator*filterEmptyEv)
+#ProductionFilterSequence = cms.Sequence(adaptedMuonsFromDiTauCands*dimuonsGlobal*generator*filterEmptyEv)
+ProductionFilterSequence = cms.Sequence(dimuonsGlobal*generator*filterEmptyEv)
