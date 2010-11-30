a9082e6d4dad3c905b514819e93a77398d6aa6ea
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2010-11-30 22:32:21 +0000

    Use the standard release pixel datafile

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_stdgeom_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_stdgeom_cff.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 siPixelFakeGainOfflineESSource = cms.ESSource("SiPixelFakeGainOfflineESSource",
-        file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/stdgeom/PixelSkimmedGeometry_stdgeom.txt')
+        file = cms.FileInPath('CalibTracker/SiPixelESProducers/data/PixelSkimmedGeometry.txt')
         )
 es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFakeGainOfflineESSource")
 
 siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
-    file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/stdgeom/PixelSkimmedGeometry_stdgeom.txt')
+    file = cms.FileInPath('CalibTracker/SiPixelESProducers/data/PixelSkimmedGeometry.txt')
 )
 es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
 
