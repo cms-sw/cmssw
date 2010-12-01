e432fd57ef5187e7a76059c7badf1495bbc4d7d2
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-12-01 18:34:35 +0000

    for the time being commented instantiation of PrimaryVertexFiler to get things compiling

--- PhysicsTools/UtilAlgos/plugins/PrimaryVertexFilter.cc
+++ PhysicsTools/UtilAlgos/plugins/PrimaryVertexFilter.cc
@@ -1,7 +1,7 @@
 #include "PhysicsTools/UtilAlgos/interface/EDFilterWrapper.h"
 #include "PhysicsTools/SelectorUtils/interface/PVSelector.h"
 
-typedef edm::FilterWrapper<PVSelector> PrimaryVertexFilter;
+//typedef edm::FilterWrapper<PVSelector> PrimaryVertexFilter;
 
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(PrimaryVertexFilter);
+//#include "FWCore/Framework/interface/MakerMacros.h"
+//DEFINE_FWK_MODULE(PrimaryVertexFilter);
