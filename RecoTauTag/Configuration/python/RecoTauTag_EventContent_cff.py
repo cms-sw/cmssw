738ff9b0c9b36f93c2b3f594344987adf0800adb
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-12-02 10:57:26 +0000

    Update event content for new algorithms and dataformats

--- RecoTauTag/Configuration/python/RecoTauTag_EventContent_cff.py
+++ RecoTauTag/Configuration/python/RecoTauTag_EventContent_cff.py
@@ -1,50 +1,51 @@
 import FWCore.ParameterSet.Config as cms
 
-#Full Event content 
+#Full Event content
 RecoTauTagFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_pfRecoTauTagInfoProducer_*_*', 
-        'keep *_fixedConePFTauProducer*_*_*', 
-        'keep *_fixedConePFTauDiscrimination*_*_*', 
-        'keep *_hpsPFTauProducer*_*_*', 
-        'keep *_hpsPFTauDiscrimination*_*_*', 
-        'keep *_shrinkingConePFTauProducer*_*_*', 
-        'keep *_shrinkingConePFTauDecayModeProducer*_*_*', 
-        'keep *_shrinkingConePFTauDecayModeIndexProducer*_*_*', 
-        'keep *_shrinkingConePFTauDiscrimination*_*_*', 
+        'keep *_pfRecoTauTagInfoProducer_*_*',
+        'keep *_ak5PFJetsRecoTauPiZeros_*_*',
+        'keep *_hpsPFTauProducer*_*_*',
+        'keep *_hpsPFTauDiscrimination*_*_*',
+        'keep *_shrinkingConePFTauProducer*_*_*',
+        'keep *_shrinkingConePFTauDiscrimination*_*_*',
+        'keep *_hpsTancTaus_*_*',
+        'keep *_hpsTancTausDiscrimination*_*_*',
 	    'keep *_TCTauJetPlusTrackZSPCorJetAntiKt5_*_*',
-        'keep *_caloRecoTauTagInfoProducer_*_*', 
-        'keep recoCaloTaus_caloRecoTauProducer*_*_*', 
+        'keep *_caloRecoTauTagInfoProducer_*_*',
+        'keep recoCaloTaus_caloRecoTauProducer*_*_*',
         'keep *_caloRecoTauDiscrimination*_*_*')
 )
 #RECO content
 RecoTauTagRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_pfRecoTauTagInfoProducer_*_*', 
-        'keep *_fixedConePFTauProducer*_*_*', 
-        'keep *_fixedConePFTauDiscrimination*_*_*', 
-        'keep *_hpsPFTauProducer*_*_*', 
-        'keep *_hpsPFTauDiscrimination*_*_*', 
-        'keep *_shrinkingConePFTauProducer*_*_*', 
-        'keep *_shrinkingConePFTauDecayModeIndexProducer*_*_*', 
+        'keep *_pfRecoTauTagInfoProducer_*_*',
+        'keep *_ak5PFJetsRecoTauPiZeros_*_*',
+        'keep *_hpsPFTauProducer*_*_*',
+        'keep *_hpsPFTauDiscrimination*_*_*',
+        'keep *_shrinkingConePFTauProducer*_*_*',
         'keep *_shrinkingConePFTauDiscrimination*_*_*',
-        'keep *_TCTauJetPlusTrackZSPCorJetAntiKt5_*_*', 
-        'keep *_caloRecoTauTagInfoProducer_*_*', 
-        'keep recoCaloTaus_caloRecoTauProducer*_*_*', 
+        'keep *_hpsTancTaus_*_*',
+        'keep *_hpsTancTausDiscrimination*_*_*',
+        'keep *_TCTauJetPlusTrackZSPCorJetAntiKt5_*_*',
+        'keep *_caloRecoTauTagInfoProducer_*_*',
+        'keep recoCaloTaus_caloRecoTauProducer*_*_*',
         'keep *_caloRecoTauDiscrimination*_*_*')
 )
 #AOD content
 RecoTauTagAOD = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_pfRecoTauTagInfoProducer_*_*', 
-        'keep *_hpsPFTauProducer*_*_*', 
-        'keep *_hpsPFTauDiscrimination*_*_*', 
-        'keep *_shrinkingConePFTauProducer*_*_*', 
-        'keep *_shrinkingConePFTauDecayModeIndexProducer*_*_*', 
+        'keep *_pfRecoTauTagInfoProducer_*_*',
+        'keep *_ak5PFJetsRecoTauPiZeros_*_*',
+        'keep *_hpsPFTauProducer*_*_*',
+        'keep *_hpsPFTauDiscrimination*_*_*',
+        'keep *_shrinkingConePFTauProducer*_*_*',
         'keep *_shrinkingConePFTauDiscrimination*_*_*',
-	    'keep *_TCTauJetPlusTrackZSPCorJetAntiKt5_*_*', 
-        'keep *_caloRecoTauTagInfoProducer_*_*', 
-        'keep recoCaloTaus_caloRecoTauProducer*_*_*', 
+        'keep *_hpsTancTaus_*_*',
+        'keep *_hpsTancTausDiscrimination*_*_*',
+	    'keep *_TCTauJetPlusTrackZSPCorJetAntiKt5_*_*',
+        'keep *_caloRecoTauTagInfoProducer_*_*',
+        'keep recoCaloTaus_caloRecoTauProducer*_*_*',
         'keep *_caloRecoTauDiscrimination*_*_*')
 )
 
