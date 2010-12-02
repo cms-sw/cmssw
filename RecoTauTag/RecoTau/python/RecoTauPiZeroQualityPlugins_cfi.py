4727adbc5a35708b2c7d43503f6165fa87cc1aa4
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-12-02 16:30:43 +0000

    Use enum correctly

--- RecoTauTag/RecoTau/python/RecoTauPiZeroQualityPlugins_cfi.py
+++ RecoTauTag/RecoTau/python/RecoTauPiZeroQualityPlugins_cfi.py
@@ -9,9 +9,6 @@ Author: Evan K. Friis, UC Davis
 
 '''
 
-# Get Builder parameters
-import RecoTauTag.RecoTau.RecoTauPiZeroBuilderPlugins_cfi as builders
-
 DELTA_M_PIZERO = 'abs(mass() - 0.13579)'
 
 isInStrip = cms.PSet(
@@ -23,7 +20,7 @@ isInStrip = cms.PSet(
     # Mike pointed out the max value of the strip can be greater than what the
     # intial cuts are and still be consistent.  Until there is a good way to
     # deal with this just cut on the algo name.
-    selection = cms.string('algoIs(kStrips)'),
+    selection = cms.string('algoIs("kStrips")'),
     selectionPassFunction = cms.string(DELTA_M_PIZERO),
     selectionFailValue = cms.double(1000)
 )
