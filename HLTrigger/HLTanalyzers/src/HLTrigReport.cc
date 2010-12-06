646e75261fbff9bb4bfdfb7e93a486edc19d2055
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-12-06 11:40:44 +0000

    add missing clear()

--- HLTrigger/HLTanalyzers/src/HLTrigReport.cc
+++ HLTrigger/HLTanalyzers/src/HLTrigReport.cc
@@ -2,8 +2,8 @@
  *
  * See header file for documentation
  *
- *  $Date: 2010/12/03 17:28:26 $
- *  $Revision: 1.19 $
+ *  $Date: 2010/12/06 11:14:51 $
+ *  $Revision: 1.20 $
  *
  *  \author Martin Grunewald
  *
@@ -154,9 +154,6 @@ HLTrigReport::beginRun(edm::Run const & iRun, edm::EventSetup const& iSetup)
       }
 
       // fill the matrices of hlIndex_, hlAccTotDS_ and dsIndex_, dsAccTotS_
-      hlIndex_.clear();
-      hlAccTotDS_.clear();
-      dsAccTotS_.clear();
       if (not isCustomDatasets_) {
         datasetNames_    = hltConfig_.datasetNames();
         datasetContents_ = hltConfig_.datasetContents();
@@ -165,6 +162,8 @@ HLTrigReport::beginRun(edm::Run const & iRun, edm::EventSetup const& iSetup)
         streamNames_     = hltConfig_.streamNames();
         streamContents_  = hltConfig_.streamContents();
       }
+      hlIndex_.clear();
+      hlAccTotDS_.clear();
       for (unsigned int ds = 0; ds < datasetNames_.size(); ds++) {
         hlIndex_.push_back(vector<unsigned int>());
         hlAccTotDS_.push_back(vector<unsigned int>());
@@ -176,13 +175,17 @@ HLTrigReport::beginRun(edm::Run const & iRun, edm::EventSetup const& iSetup)
           }
         }
       }
+      dsIndex_.clear();
+      dsAccTotS_.clear();
       for (unsigned int s=0; s<streamNames_.size(); s++) {
         dsIndex_.push_back(vector<unsigned int>());
         dsAccTotS_.push_back(vector<unsigned int>());
         for (unsigned int ds=0; ds<streamContents_[s].size(); ds++) {
           unsigned int i = 0;
-          for (; i<datasetNames_.size(); i++) if (datasetNames_[i] == streamContents_[s][ds]) break;
-          if (i<datasetNames_.size() && hlIndex_[i].size()>0) { // report only datasets that have at least one path otherwise crash
+          for (; i<datasetNames_.size(); i++) if (datasetNames_[i] == streamContents_[s][ds]) 
+            break;
+          // report only datasets that have at least one path otherwise crash
+          if (i<datasetNames_.size() && hlIndex_[i].size()>0) {
             dsIndex_[s].push_back(i);
             dsAccTotS_[s].push_back(0);
           }
