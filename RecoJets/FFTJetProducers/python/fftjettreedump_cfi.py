4dacf1cc1528d88a6eb77b5be9546a4315480ebe
Igor Volobouev <sha1-0145642b110684c0a5e41962d213328ec89408e5@cern.ch>
2010-12-06 17:33:19 +0000

    initial FFTJet port

---
+++ RecoJets/FFTJetProducers/python/fftjettreedump_cfi.py
@@ -0,0 +1,56 @@
+# FFTJet clustering tree dumper configuration
+
+import os, errno
+import FWCore.ParameterSet.Config as cms
+
+from RecoJets.FFTJetProducers.fftjetcommon_cfi import *
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+
+# Output directory for the trees
+clustering_trees_outdir = "./ClusteringTrees"
+mkdir_p(clustering_trees_outdir)
+
+# Base name for the output files
+trees_basename = "clustree"
+
+# Configure the FFTJet tree dumper module
+fftjet_tree_dumper = cms.EDAnalyzer(
+    "FFTJetTreeDump",
+    #
+    # Label for the input clustering tree (either sparse or dense)
+    treeLabel = cms.InputTag("fftjetpatreco", "FFTJetPatternRecognition"),
+    #
+    # Prefix for the output trees
+    outputPrefix = cms.string(clustering_trees_outdir + '/' + trees_basename),
+    #
+    # Eta limit for the plot
+    etaMax = cms.double(fftjet_standard_eta_range),
+    #
+    # Is input tree data stored in single or double precision?
+    storeInSinglePrecision = cms.bool(fftjet_single_precision),
+    #
+    # The initial set of scales used by the pattern recognition stage.
+    # This is also the final set unless clustering tree construction
+    # is adaptive. Needed here for reading back non-adaptive trees.
+    InitialScales = fftjet_patreco_scales_50,
+    #
+    # Clustering tree distance functor
+    TreeDistanceCalculator = fftjet_fixed_bandwidth_distance,
+    #
+    # Which quantity will be mapped into OpenDX glyph size?
+    GlyphSize = cms.PSet(
+        Class = cms.string("ScaledMagnitude2")
+    ),
+    #
+    # Which quantity will be mapped into OpenDX glyph color?
+    GlyphColor = cms.PSet(
+        Class = cms.string("ScaledHessianDet")
+    )
+)
