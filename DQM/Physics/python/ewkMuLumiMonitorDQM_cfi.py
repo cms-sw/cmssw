48599b3be0fae3472170802427c49441eb367b60
Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch>
2010-12-06 15:05:31 +0000

    new trigger selection

--- DQM/Physics/python/ewkMuLumiMonitorDQM_cfi.py
+++ DQM/Physics/python/ewkMuLumiMonitorDQM_cfi.py
@@ -17,7 +17,7 @@ CombIsoCuts = cms.PSet(
 TrkIsoCuts = cms.PSet(
     IsRelativeIso = cms.untracked.bool(False),
     IsCombinedIso = cms.untracked.bool(False),
-    IsoCut03 = cms.untracked.double(4.0),   
+    IsoCut03 = cms.untracked.double(3.0),   
 #    deltaRTrk = cms.untracked.double(0.3),
     ptThreshold = cms.untracked.double("0.0"), 
  #   deltaRVetoTrk = cms.untracked.double("0.015"), 
@@ -28,7 +28,7 @@ TrkIsoCuts = cms.PSet(
 ewkMuLumiMonitorDQM = cms.EDAnalyzer(
     "EwkMuLumiMonitorDQM",
     # isolation cuts
-    CombIsoCuts,
+    TrkIsoCuts,
     # for Z/W
     muons= cms.untracked.InputTag("muons"),
     tracks=cms.untracked.InputTag("generalTracks"),
@@ -37,8 +37,8 @@ ewkMuLumiMonitorDQM = cms.EDAnalyzer(
     METIncludesMuons=cms.untracked.bool(True),
     TrigTag = cms.untracked.InputTag("TriggerResults::HLT"),
     triggerEvent = cms.untracked.InputTag( "hltTriggerSummaryAOD::HLT" ),  
-    hltPath = cms.untracked.string("HLT_Mu15_v1"),
-    L3FilterName= cms.untracked.string("hltSingleMu15L3Filtered15"),
+##    hltPath = cms.untracked.string("HLT_Mu11"),
+##    L3FilterName= cms.untracked.string("hltSingleMu15L3Filtered15"),
     maxDPtRel = cms.untracked.double( 1.0 ),
     maxDeltaR = cms.untracked.double( 0.2 ),
     ptMuCut = cms.untracked.double( 20.0 ),
