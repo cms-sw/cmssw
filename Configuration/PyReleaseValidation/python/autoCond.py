52bd2110e02eea0faeff400d399afb12ca4f08ba
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-12-06 16:11:22 +0000

    Update Gts for 310X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_310_V1::All',
-             'startup' : 'START310_V1::All',
+autoCond = { 'mc'      : 'MC_310_V2::All',
+             'startup' : 'START310_V2::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_310_V1::All',
              'com10'   : 'GR_R_310_V1::All',
              'hltonline' : 'GR_R_310_V1::All', # same as com10 until H tag available
-             'starthi' : 'START39_V7HI::All'
+             'starthi' : 'START310_V1HI::All'
              }
