28383b27809957111ff302f01979dc18d18fb065
Kevin.Klapoetke Klapoetke <sha1-00ae6b1fd57e2673af62ea5af6540d64f8b247c9@cern.ch>
2010-12-09 17:34:44 +0000

    update before new changes

--- RecoEgamma/EgammaHFProducers/src/HFClusterAlgo.cc
+++ RecoEgamma/EgammaHFProducers/src/HFClusterAlgo.cc
@@ -105,7 +105,7 @@ void HFClusterAlgo::clusterize(const HFRecHitCollection& hf,
          eshort*=m_correctionByEta[indexByEta(j2->id())];
       if (((elong-eshort)/(elong+eshort))>m_maximumSL) continue;
       if ((m_usePMTFlag)&&(j->flagField(4,1))) continue;
-      if ((m_usePulseFlag)&&(il->flagField(1,1))) continue;
+      if ((m_usePulseFlag)&&(j->flagField(1,1))) continue;
 
       HFCompleteHit ahit;
       double eta=geom.getPosition(j->id()).eta();
@@ -250,13 +250,13 @@ bool HFClusterAlgo::makeCluster(const HcalDetId& seedid,
 	// cut on "PMT HIT" flag
 	if ((il!=hf.end())&&(il->flagField(4,1))&&(m_usePMTFlag)) {//HFPET flag for lone/short doil->flagField(0,1)
 	  if (dp==0 && de==0) clusterOk=false; // somehow, the seed is hosed
-	  continue;
+	  continue;//continue to next hit, do not include this one in cluster
 	}
 
 	// cut on "Pulse shape HIT" flag
 	if ((il!=hf.end())&&(il->flagField(1,1))&&(m_usePulseFlag)) {//HF DIGI TIME flag
 	  if (dp==0 && de==0) clusterOk=false; // somehow, the seed is hosed
-	  continue;
+	  continue;//continue to next hit, do not include this one in cluster
 	}
        
 	if (e_long > m_minTowerEnergy && il!=hf.end()) {
