e90a0d91d418d54b185005a6d4d2f31502143f1f
Joseph Anthony Gartner <sha1-120ea671594459b01226f9faee5dbc168f706fa8@cern.ch>
2010-12-12 05:30:37 +0000

    added offline data emulator comparison module

--- L1Trigger/CSCTrackFinder/plugins/SealModule.cc
+++ L1Trigger/CSCTrackFinder/plugins/SealModule.cc
@@ -3,8 +3,9 @@
 #include "L1Trigger/CSCTrackFinder/plugins/CSCTFanalyzer.h"
 #include "L1Trigger/CSCTrackFinder/plugins/CSCTFTrackProducer.h"
 #include "L1Trigger/CSCTrackFinder/plugins/CSCTFCandidateProducer.h"
+#include "L1Trigger/CSCTrackFinder/plugins/CsctfDatEmu.h"
 
 DEFINE_FWK_MODULE(CSCTFTrackProducer);
 DEFINE_FWK_MODULE(CSCTFCandidateProducer);
 DEFINE_FWK_MODULE(CSCTFanalyzer);
-
+DEFINE_FWK_MODULE(CsctfDatEmu);
