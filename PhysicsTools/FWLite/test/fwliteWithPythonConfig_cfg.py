1817b6f3b36e5ddf4aec4e5aaafefb2bf096d78e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-12-14 00:14:10 +0000

    brought up to date

--- PhysicsTools/FWLite/test/fwliteWithPythonConfig_cfg.py
+++ PhysicsTools/FWLite/test/fwliteWithPythonConfig_cfg.py
@@ -2,8 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
-
-process.FWLiteParams = cms.PSet(
-    muonSrc = cms.InputTag('cleanPatMuons')
+process.MuonAnalyzer = cms.PSet(
+    ## common input for wrapped analyzers
+    fileNames   = cms.vstring('rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_8_6/RelValTTbar/GEN-SIM-RECO/START38_V13-v1/0065/2856A4C7-B5E7-DF11-BE1D-00304867BFA8.root'),  ## mandatory
+    outputFile  = cms.string('analyzeFWLiteHistograms.root'),## mandatory
+    maxEvents   = cms.int32(-1),                      ## optional
+    reportAfter = cms.uint32(10),                     ## optional
+    ## input specific for this analyzer
+    muons = cms.InputTag('muons')
 )
-
