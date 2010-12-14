258d0d9ca81f403a56d5be2878f2d8cb53079628
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-12-14 00:09:07 +0000

    brought up to date with ste of the art example -- this one is equivalent with the BasicAnalyzer examples

--- PhysicsTools/FWLite/bin/FWLiteWithPythonConfig.cc
+++ PhysicsTools/FWLite/bin/FWLiteWithPythonConfig.cc
@@ -7,7 +7,7 @@
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/FWLite/interface/AutoLibraryLoader.h"
 
-#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
 #include "PhysicsTools/FWLite/interface/TFileService.h"
 #include "FWCore/ParameterSet/interface/ProcessDesc.h"
 #include "FWCore/PythonParameterSet/interface/PythonProcessDesc.h"
@@ -34,69 +34,62 @@ int main(int argc, char* argv[])
 
   // Get the python configuration
   PythonProcessDesc builder(argv[1]);
-  edm::ParameterSet const& fwliteParameters = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("FWLiteParams");
+  edm::ParameterSet const& cfg = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("MuonAnalyzer");
 
-  // Now get each parameter
-  edm::InputTag muonLabel( fwliteParameters.getParameter<edm::InputTag>("muonSrc") );
+  // now get each parameter
+  std::vector<std::string> inputFiles_( cfg.getParameter<std::vector<std::string> >("fileNames") );
+  std::string outputFile_( cfg.getParameter<std::string  >("outputFile" ) );
+  unsigned int reportAfter_( cfg.getParameter<unsigned int>("reportAfter") );
+  int maxEvents_( cfg.getParameter<int>("maxEvents") );
+  edm::InputTag muons_ ( cfg.getParameter<edm::InputTag>("muons"  ) );
 
-  // Handle to the muon collection
-  edm::Handle<std::vector<pat::Muon> > muons;
-  
   // book a set of histograms
-  fwlite::TFileService fs = fwlite::TFileService("analyzePatBasics.root");
-  TFileDirectory theDir = fs.mkdir("analyzeBasicPat");
-  TH1F* muonPt_  = theDir.make<TH1F>("muonPt", "pt",    100,  0.,300.);
-  TH1F* muonEta_ = theDir.make<TH1F>("muonEta","eta",   100, -3.,  3.);
-  TH1F* muonPhi_ = theDir.make<TH1F>("muonPhi","phi",   100, -5.,  5.);  
+  fwlite::TFileService fs = fwlite::TFileService(outputFile_.c_str());
+  TFileDirectory dir = fs.mkdir("analyzeBasicPat");
+  TH1F* muonPt_  = dir.make<TH1F>("muonPt", "pt",    100,  0.,300.);
+  TH1F* muonEta_ = dir.make<TH1F>("muonEta","eta",   100, -3.,  3.);
+  TH1F* muonPhi_ = dir.make<TH1F>("muonPhi","phi",   100, -5.,  5.);  
   
-  // open input file (can be located on castor)
-  TFile* inFile = TFile::Open( "file:PATLayer1_Output.fromAOD_full.root" );
-
-  // ----------------------------------------------------------------------
-  // Second Part: 
-  //
-  //  * loop the events in the input file 
-  //  * receive the collections of interest via fwlite::Handle
-  //  * fill the histograms
-  //  * after the loop close the input file
-  // ----------------------------------------------------------------------
-
   // loop the events
-  unsigned int iEvent=0;
-  fwlite::Event ev(inFile);
-  for(ev.toBegin(); !ev.atEnd(); ++ev, ++iEvent){
-    edm::EventBase const & event = ev;
-
-    // break loop after end of file is reached 
-    // or after 1000 events have been processed
-    if( iEvent==1000 ) break;
-    
-    // simple event counter
-    if(iEvent>0 && iEvent%1==0){
-      std::cout << "  processing event: " << iEvent << std::endl;
+  int ievt=0;  
+  for(unsigned int iFile=0; iFile<inputFiles_.size(); ++iFile){
+    // open input file (can be located on castor)
+    TFile* inFile = TFile::Open(inputFiles_[iFile].c_str());
+    if( inFile ){
+      // ----------------------------------------------------------------------
+      // Second Part: 
+      //
+      //  * loop the events in the input file 
+      //  * receive the collections of interest via fwlite::Handle
+      //  * fill the histograms
+      //  * after the loop close the input file
+      // ----------------------------------------------------------------------
+      fwlite::Event ev(inFile);
+      for(ev.toBegin(); !ev.atEnd(); ++ev, ++ievt){
+	edm::EventBase const & event = ev;
+	// break loop if maximal number of events is reached 
+	if(maxEvents_>0 ? ievt+1>maxEvents_ : false) break;
+	// simple event counter
+	if(reportAfter_!=0 ? (ievt>0 && ievt%reportAfter_==0) : false) 
+	  std::cout << "  processing event: " << ievt << std::endl;
+	
+	// Handle to the muon collection
+	edm::Handle<std::vector<reco::Muon> > muons;
+	event.getByLabel(muons_, muons);
+	
+	// loop muon collection and fill histograms
+	for(unsigned i=0; i<muons->size(); ++i){
+	  muonPt_ ->Fill( (*muons)[i].pt()  );
+	  muonEta_->Fill( (*muons)[i].eta() );
+	  muonPhi_->Fill( (*muons)[i].phi() );
+	}
+      }  
+      // close input file
+      inFile->Close();
     }
-
-
-    event.getByLabel(muonLabel, muons);
-    
-    // loop muon collection and fill histograms
-    for(unsigned i=0; i<muons->size(); ++i){
-      muonPt_ ->Fill( (*muons)[i].pt()  );
-      muonEta_->Fill( (*muons)[i].eta() );
-      muonPhi_->Fill( (*muons)[i].phi() );
-    }
-  }  
-  // close input file
-  inFile->Close();
-
-  // ----------------------------------------------------------------------
-  // Third Part: 
-  //
-  //  * never forget to free the memory of objects you created
-  // ----------------------------------------------------------------------
-
-  // in this example there is nothing to do 
-  
-  // that's it!
+    // break loop if maximal number of events is reached:
+    // this has to be done twice to stop the file loop as well
+    if(maxEvents_>0 ? ievt+1>maxEvents_ : false) break;
+  }
   return 0;
 }
