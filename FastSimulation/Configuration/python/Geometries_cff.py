211ead636216545d4883d1bcc96139be07d47b51
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2010-12-15 19:34:24 +0000

    Take an ideal default geometry and define a new MC one

--- FastSimulation/Configuration/python/Geometries_cff.py
+++ FastSimulation/Configuration/python/Geometries_cff.py
@@ -50,11 +50,3 @@ from Geometry.CaloEventSetup.CaloTopology_cfi import *
 
 # The muon geometry left-over
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-
-
-from FastSimulation.Configuration.FamosSequences_cff import famosSimHits
-# Apply Tracker and Muon misalignment
-famosSimHits.ApplyAlignment = True
-misalignedTrackerGeometry.applyAlignment = True
-misalignedDTGeometry.applyAlignment = True
-misalignedCSCGeometry.applyAlignment = True
