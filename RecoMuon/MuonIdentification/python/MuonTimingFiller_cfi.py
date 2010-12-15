4ad28c8b11b2740e227b6cb6720328054e254ea6
Piotr Traczyk <sha1-f63c8ead76e5e4a129ecffbbd6f7786205ea2389@cern.ch>
2010-12-15 11:06:08 +0000

    Add constant offset for DT and change hit cut, tweak errors for combination

--- RecoMuon/MuonIdentification/python/MuonTimingFiller_cfi.py
+++ RecoMuon/MuonIdentification/python/MuonTimingFiller_cfi.py
@@ -9,8 +9,8 @@ TimingFillerBlock = cms.PSet(
     CSCTimingExtractorBlock,
     
     # Single hit time measurement precition in ns
-    ErrorDT  = cms.double(3.1),
-    ErrorCSC = cms.double(7.),
+    ErrorDT  = cms.double(6.0),
+    ErrorCSC = cms.double(7.4),
     
     # Ecal minimum energy cut
     EcalEnergyCut = cms.double(0.4),
