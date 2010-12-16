f0d26b03db05d5d63b13b93412267a5cfca236d6
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2010-12-16 18:00:54 +0000

    false default

--- RecoLocalMuon/RPCRecHit/python/rpcRecHitAli_cfi.py
+++ RecoLocalMuon/RPCRecHit/python/rpcRecHitAli_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 rpcRecHitAli = cms.EDProducer("RPCRecHitAli",
-  debug = cms.untracked.bool(True),
+  debug = cms.untracked.bool(False),
   rpcRecHits = cms.InputTag("rpcRecHits"),
   AliFileName = cms.untracked.string('../data/Alignment.dat')
 )
