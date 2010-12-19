90d026b7bf4214b5201f1a508dc9aa8d2df9f975
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-12-19 23:50:51 +0000

    more basic port from PatExamples

--- PhysicsTools/FWLite/bin/FWLiteWithSelectorUtils.cc
+++ PhysicsTools/FWLite/bin/FWLiteWithSelectorUtils.cc
@@ -1,9 +1,9 @@
-#include <memory>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <iostream>
+//#include <memory>
+//#include <string>
+//#include <vector>
+//#include <sstream>
+//#include <fstream>
+//#include <iostream>
 
 #include <TH1F.h>
 #include <TROOT.h>
@@ -24,85 +24,84 @@
 #include "PhysicsTools/SelectorUtils/interface/EventSelector.h"
 
 
+
 class WSelector : public EventSelector {
+
 public:
-  WSelector( edm::ParameterSet const & params ) :
+  /// constructor
+  WSelector(edm::ParameterSet const& params) :
     muonSrc_(params.getParameter<edm::InputTag>("muonSrc")),
     metSrc_ (params.getParameter<edm::InputTag>("metSrc")) 
   {
     double muonPtMin = params.getParameter<double>("muonPtMin");
-    double metMin = params.getParameter<double>("metMin");
+    double metMin    = params.getParameter<double>("metMin");
     push_back("Muon Pt", muonPtMin );
-    push_back("MET", metMin );
-    set("Muon Pt");
-    set("MET");
-
-    wMuon_ = 0;
-    met_ = 0;
-
- 
-    if ( params.exists("cutsToIgnore") )
+    push_back("MET"    , metMin    );
+    set("Muon Pt"); set("MET");
+    wMuon_ = 0; met_ = 0;
+    if ( params.exists("cutsToIgnore") ){
       setIgnoredCuts( params.getParameter<std::vector<std::string> >("cutsToIgnore") );
-
+    }
     retInternal_ = getBitTemplate();
-
-
   }
+  /// destructor
   virtual ~WSelector() {}
+  /// return muon candidate of W boson
+  pat::Muon const& wMuon() const { return *wMuon_;}
+  /// return MET of W boson
+  pat::MET  const& met()   const { return *met_;  }
 
-
-  // Here is where the selection occurs
+  /// here is where the selection occurs
   virtual bool operator()( edm::EventBase const & event, pat::strbitset & ret){
-
     ret.set(false);
-
     // Handle to the muon collection
     edm::Handle<std::vector<pat::Muon> > muons;    
     // Handle to the MET collection
     edm::Handle<std::vector<pat::MET> > met;
-    
-    // Get the objects from the event
+    // get the objects from the event
     bool gotMuons = event.getByLabel(muonSrc_, muons);
-    bool gotMET = event.getByLabel(metSrc_, met);
-
+    bool gotMET   = event.getByLabel(metSrc_, met   );
     // get the MET, require to be > minimum
-    if ( gotMET ) {
+    if( gotMET ){
       met_ = &met->at(0);
-      if ( met_->pt() > cut("MET",   double()) || ignoreCut("MET") ) 
+      if( met_->pt() > cut("MET", double()) || ignoreCut("MET") ) 
 	passCut(ret, "MET");
     }
-
     // get the highest pt muon, require to have pt > minimum
-    if ( gotMuons ) {
-      if ( !ignoreCut("Muon Pt") )  {
-	if ( muons->size() > 0 ) {
+    if( gotMuons ){
+      if( !ignoreCut("Muon Pt") ){
+	if( muons->size() > 0 ){
 	  wMuon_ = &muons->at(0);
-	  if ( wMuon_->pt() > cut("Muon Pt", double()) || ignoreCut("Muon Pt") ) 
+	  if( wMuon_->pt() > cut("Muon Pt", double()) || ignoreCut("Muon Pt") ) 
 	    passCut(ret, "Muon Pt");
 	}
-      } else {
+      } 
+      else{
 	passCut( ret, "Muon Pt");
       }
     }
-
     setIgnored(ret);
     return (bool)ret;
-    
   }
 
-  pat::Muon const & wMuon() const { return *wMuon_;}
-  pat::MET  const & met()   const { return *met_;}
-
 protected:
+  /// muon input
   edm::InputTag muonSrc_;
+  /// met input
   edm::InputTag metSrc_;
-
-  pat::Muon const * wMuon_;
-  pat::MET const *  met_;
+  /// muon candidate from W boson
+  pat::Muon const* wMuon_;
+  /// MET from W boson
+  pat::MET const* met_;
 };
+
 
 int main(int argc, char* argv[]) 
 {
+  // define what muon you are using; this is necessary as FWLite is not 
+  // capable of reading edm::Views
+  using pat::Muon;
+
   // ----------------------------------------------------------------------
   // First Part: 
   //
@@ -120,89 +119,82 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  // Get the python configuration
+  // get the python configuration
   PythonProcessDesc builder(argv[1]);
-  edm::ParameterSet const& fwliteParameters = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("FWLiteParams");
+  edm::ParameterSet const& cfg = builder.processDesc()->getProcessPSet()->getParameter<edm::ParameterSet>("WBosonAnalyzer");
   
-  // Now get the W selector
-  edm::ParameterSet wSelectorParams = fwliteParameters.getParameter<edm::ParameterSet>("wSelectorParams");
-  WSelector wSelector( wSelectorParams );
-  pat::strbitset wSelectorReturns = wSelector.getBitTemplate();
-  
+  // now get each parameter
+  int maxEvents_( cfg.getParameter<int>("maxEvents") );
+  unsigned int outputEvery_( cfg.getParameter<unsigned int>("outputEvery") );
+  std::string outputFile_( cfg.getParameter<std::string>("outputFile" ) );
+  std::vector<std::string> inputFiles_( cfg.getParameter<std::vector<std::string> >("fileNames") );
 
+  // initialize the W selector
+  edm::ParameterSet selection = cfg.getParameter<edm::ParameterSet>("selection");
+  WSelector wSelector( selection ); pat::strbitset wSelectorReturns = wSelector.getBitTemplate();
+  
   // book a set of histograms
-  fwlite::TFileService fs = fwlite::TFileService("analyzePatBasics.root");
+  fwlite::TFileService fs = fwlite::TFileService(outputFile_.c_str());
   TFileDirectory theDir = fs.mkdir("analyzeBasicPat");
   TH1F* muonPt_  = theDir.make<TH1F>("muonPt", "pt",    100,  0.,300.);
   TH1F* muonEta_ = theDir.make<TH1F>("muonEta","eta",   100, -3.,  3.);
   TH1F* muonPhi_ = theDir.make<TH1F>("muonPhi","phi",   100, -5.,  5.);  
-  
-  // open input file (can be located on castor)
-  TFile* inFile = TFile::Open( "patTuple.root" );
 
-  // ----------------------------------------------------------------------
-  // Second Part: 
-  //
-  //  * loop the events in the input file 
-  //  * receive the collections of interest via fwlite::Handle
-  //  * fill the histograms
-  //  * after the loop close the input file
-  // ----------------------------------------------------------------------
-
-  TStopwatch timer;
-  timer.Start();
-
-  unsigned int nEventsAnalyzed = 0;
+  // start a CPU timer
+  TStopwatch timer; timer.Start();
 
   // loop the events
-  unsigned int iEvent=0;
-  fwlite::Event ev(inFile);
-  for(ev.toBegin(); !ev.atEnd(); ++ev, ++iEvent){
-    edm::EventBase const & event = ev;
+  int ievt=0;  
+  unsigned int nEventsAnalyzed = 0;
+  for(unsigned int iFile=0; iFile<inputFiles_.size(); ++iFile){
+    // open input file (can be located on castor)
+    TFile* inFile = TFile::Open(inputFiles_[iFile].c_str());
+    if( inFile ){
+      // ----------------------------------------------------------------------
+      // Second Part: 
+      //
+      //  * loop the events in the input file 
+      //  * receive the collections of interest via fwlite::Handle
+      //  * fill the histograms
+      //  * after the loop close the input file
+      // ----------------------------------------------------------------------
+      fwlite::Event ev(inFile);
+      for(ev.toBegin(); !ev.atEnd(); ++ev, ++ievt){
+	edm::EventBase const & event = ev;
+	// break loop if maximal number of events is reached 
+	if(maxEvents_>0 ? ievt+1>maxEvents_ : false) break;
+	// simple event counter
+	if(outputEvery_!=0 ? (ievt>0 && ievt%outputEvery_==0) : false) 
+	  std::cout << "  processing event: " << ievt << std::endl;
     
-    if ( wSelector(event, wSelectorReturns ) ) {
-      
-
-      pat::Muon const & wMuon = wSelector.wMuon();
-
-      muonPt_ ->Fill( wMuon.pt()  );
-      muonEta_->Fill( wMuon.eta() );
-      muonPhi_->Fill( wMuon.phi() );
-      
-    } 
-
-
-    ++nEventsAnalyzed;
-  }  
-  // close input file
-  inFile->Close();
-
-
+	if ( wSelector(event, wSelectorReturns ) ) {
+	  pat::Muon const & wMuon = wSelector.wMuon();
+	  muonPt_ ->Fill( wMuon.pt()  );
+	  muonEta_->Fill( wMuon.eta() );
+	  muonPhi_->Fill( wMuon.phi() );
+	} 
+	++nEventsAnalyzed;
+      }  
+      // close input file
+      inFile->Close();
+    }
+    // break loop if maximal number of events is reached:
+    // this has to be done twice to stop the file loop as well
+    if(maxEvents_>0 ? ievt+1>maxEvents_ : false) break;
+  }
+  // stop CPU timer
   timer.Stop();
 
   // print selector
   wSelector.print(std::cout);
 
   // print some timing statistics
-  Double_t rtime = timer.RealTime();
-  Double_t ctime = timer.CpuTime();
+  double rtime = timer.RealTime();
+  double ctime = timer.CpuTime ();
+  // timing printouts
   printf("Analyzed events: %d \n",nEventsAnalyzed);
   printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
   printf("%4.2f events / RealTime second .\n", (double)nEventsAnalyzed/rtime);
   printf("%4.2f events / CpuTime second .\n", (double)nEventsAnalyzed/ctime);
-  
-
-
-
-
-  // ----------------------------------------------------------------------
-  // Third Part: 
-  //
-  //  * never forget to free the memory of objects you created
-  // ----------------------------------------------------------------------
-
-  // in this example there is nothing to do 
-  
-  // that's it!
   return 0;
 }
--- PhysicsTools/FWLite/test/fwliteWithSelectorUtils_cfg.py
+++ PhysicsTools/FWLite/test/fwliteWithSelectorUtils_cfg.py
@@ -2,49 +2,19 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
 
-#input stuff for Run/Lumi selection with the "JSON"-formatted files from the PVT group
-import FWCore.PythonUtilities.LumiList as LumiList
-
-
-# setup process
-process = cms.Process("FWLitePlots")
-
-# get JSON file correctly parced
-JSONfile = 'DCSTRONLY_132440-140388'
-myList = LumiList.LumiList (filename = JSONfile).getCMSSWString().split(',')
-
-
-# Set up the parameters for the calo jet analyzer
-process.jetStudies = cms.PSet(
-    # input parameter sets
-    jetSrc = cms.InputTag('selectedPatJets'),
-    pfJetSrc = cms.InputTag('selectedPatJetsAK5PF'),
-    metSrc = cms.InputTag('patMETs'),
-    pfMetSrc = cms.InputTag('patMETsPF'),
-    useCalo = cms.bool(True)
+process.WBosonAnalyzer = cms.PSet(
+    ## common input for wrapped analyzers
+    fileNames   = cms.vstring('file:patTuple.root'),  ## mandatory
+    outputFile  = cms.string('analyzeFWLiteHistograms.root'),## mandatory
+    maxEvents   = cms.int32(-1),                      ## optional
+    outputEvery = cms.uint32(10),                     ## optional
+    ## parameters for W boson selector
+    selection = cms.PSet(
+        muonSrc      = cms.InputTag('cleanPatMuons'),
+        metSrc       = cms.InputTag('patMETs'),
+        muonPtMin    = cms.double(20.0),
+        metMin       = cms.double(20.0),
+        cutsToIgnore = cms.vstring('MET')
+        )
 )
 
-# Set up the parameters for the PF jet analyzer
-process.pfJetStudies = process.jetStudies.clone( useCalo = cms.bool(False) )
-
-
-process.load('PhysicsTools.SelectorUtils.pfJetIDSelector_cfi')
-process.load('PhysicsTools.SelectorUtils.jetIDSelector_cfi')
-
-process.plotParameters = cms.PSet (
-    doTracks = cms.bool(False),
-    useMC = cms.bool(False)
-)
-
-
-process.inputs = cms.PSet (
-    fileNames = cms.vstring(
-        'reco_7TeV_380_pat.root'
-        ),
-    lumisToProcess = cms.untracked.VLuminosityBlockRange( myList )
-
-)
-
-process.outputs = cms.PSet (
-    outputName = cms.string('jetPlots.root')
-)
