afaf9244e6115aac52942a4134e1127b5925b680
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-12-19 11:15:41 +0000

    added commandline and Z mass histogram for CMSDAS

--- PhysicsTools/FWLite/bin/FWLiteHistograms.cc
+++ PhysicsTools/FWLite/bin/FWLiteHistograms.cc
@@ -16,6 +16,7 @@
 
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "PhysicsTools/FWLite/interface/TFileService.h"
+#include "PhysicsTools/FWLite/interface/CommandLineParser.h"
 
 
 int main(int argc, char* argv[]) 
@@ -32,22 +33,31 @@ int main(int argc, char* argv[])
   gSystem->Load( "libFWCoreFWLite" );
   AutoLibraryLoader::enable();
 
-  // book a set of histograms
-  fwlite::TFileService fs = fwlite::TFileService("analyzeFWLiteHistograms.root");
-  TFileDirectory dir = fs.mkdir("analyzeBasicPat");
-  TH1F* muonPt_  = dir.make<TH1F>("muonPt", "pt",    100,  0.,300.);
-  TH1F* muonEta_ = dir.make<TH1F>("muonEta","eta",   100, -3.,  3.);
-  TH1F* muonPhi_ = dir.make<TH1F>("muonPhi","phi",   100, -5.,  5.);  
+  // initialize command line parser
+  optutl::CommandLineParser parser ("Analyze FWLite Histograms");
 
-  // open input file (can be located on castor)
-  // please replace by an apropriate LFN
-  std::vector<std::string> inputFiles_;
-  inputFiles_.push_back("rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_8_6/RelValTTbar/GEN-SIM-RECO/START38_V13-v1/0065/2856A4C7-B5E7-DF11-BE1D-00304867BFA8.root");
+  // set defaults
+  parser.integerValue ("maxEvents"  ) = 1000;
+  parser.integerValue ("outputEvery") =   10;
+  parser.stringValue  ("outputFile" ) = "analyzeFWLiteHistograms.root";
+
+  // parse arguments
+  parser.parseArguments (argc, argv);
+  int maxEvents_ = parser.integerValue("maxEvents");
+  unsigned int outputEvery_ = parser.integerValue("outputEvery");
+  std::string outputFile_ = parser.stringValue("outputFile");
+  std::vector<std::string> inputFiles_ = parser.stringVector("inputFiles");
+
+  // book a set of histograms
+  fwlite::TFileService fs = fwlite::TFileService(outputFile_.c_str());
+  TFileDirectory dir = fs.mkdir("analyzeBasicPat");
+  TH1F* muonPt_  = dir.make<TH1F>("muonPt"  , "pt"  ,   100,   0., 300.);
+  TH1F* muonEta_ = dir.make<TH1F>("muonEta" , "eta" ,   100,  -3.,   3.);
+  TH1F* muonPhi_ = dir.make<TH1F>("muonPhi" , "phi" ,   100,  -5.,   5.);  
+  TH1F* mumuMass_= dir.make<TH1F>("mumuMass", "mass",    90,  30.,  120.);
 
   // loop the events
   int ievt=0;  
-  int maxEvents_=1000;
-  int reportAfter_=10;
   for(unsigned int iFile=0; iFile<inputFiles_.size(); ++iFile){
     // open input file (can be located on castor)
     TFile* inFile = TFile::Open(inputFiles_[iFile].c_str());
@@ -66,7 +76,7 @@ int main(int argc, char* argv[])
 	// break loop if maximal number of events is reached 
 	if(maxEvents_>0 ? ievt+1>maxEvents_ : false) break;
 	// simple event counter
-	if(reportAfter_!=0 ? (ievt>0 && ievt%reportAfter_==0) : false) 
+	if(outputEvery_!=0 ? (ievt>0 && ievt%outputEvery_==0) : false) 
 	  std::cout << "  processing event: " << ievt << std::endl;
 
 	// Handle to the muon collection
@@ -74,10 +84,21 @@ int main(int argc, char* argv[])
 	event.getByLabel(std::string("muons"), muons);
 	
 	// loop muon collection and fill histograms
-	for(unsigned i=0; i<muons->size(); ++i){
-	  muonPt_ ->Fill( (*muons)[i].pt()  );
-	  muonEta_->Fill( (*muons)[i].eta() );
-	  muonPhi_->Fill( (*muons)[i].phi() );
+	for(std::vector<reco::Muon>::const_iterator mu1=muons->begin(); mu1!=muons->end(); ++mu1){
+	  muonPt_ ->Fill( mu1->pt () );
+	  muonEta_->Fill( mu1->eta() );
+	  muonPhi_->Fill( mu1->phi() );	  
+	  if( mu1->pt()>20 && fabs(mu1->eta())<2.1 ){
+	    for(std::vector<reco::Muon>::const_iterator mu2=muons->begin(); mu2!=muons->end(); ++mu2){
+	      if(mu2>mu1){ // prevent double conting
+		if( mu1->charge()*mu2->charge()<0 ){ // check only muon pairs of unequal charge 
+		  if( mu2->pt()>20 && fabs(mu2->eta())<2.1 ){
+		    mumuMass_->Fill( (mu1->p4()+mu2->p4()).mass() );
+		  }
+		}
+	      }
+	    }
+	  }
 	}
       }  
       // close input file
