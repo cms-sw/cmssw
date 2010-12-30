579f53f09e2eddd57eddb1e6ea1d9e225c26888e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-12-30 19:10:16 +0000

    Add const qualifier to ActionTable and ProcessConfiguration pointers

--- FWCore/Framework/src/WorkerParams.h
+++ FWCore/Framework/src/WorkerParams.h
@@ -9,26 +9,25 @@ This struct is used to communication parameters into the worker factory.
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include <string>
 #include "boost/shared_ptr.hpp"
 
-namespace edm
-{
+#include <string>
+
+namespace edm {
   class ProcessConfiguration;
   class ProductRegistry;
   class ActionTable;
 
-  struct WorkerParams
-  {
-    WorkerParams(): 
+  struct WorkerParams {
+    WorkerParams() :
       procPset_(0), pset_(0), reg_(0), processConfiguration_(), actions_(0)
       {}
 
     WorkerParams(ParameterSet const& procPset,
-		 ParameterSet * pset,
+                 ParameterSet* pset,
                  ProductRegistry& reg,
-		 boost::shared_ptr<ProcessConfiguration> processConfiguration,
-		 ActionTable& actions) :
+                 boost::shared_ptr<ProcessConfiguration const> processConfiguration,
+                 ActionTable const& actions) :
       procPset_(&procPset),
       pset_(pset),
       reg_(&reg),
@@ -38,8 +37,8 @@ namespace edm
     ParameterSet const* procPset_;
     ParameterSet* pset_;
     ProductRegistry* reg_;
-    boost::shared_ptr<ProcessConfiguration> processConfiguration_;
-    ActionTable* actions_;
+    boost::shared_ptr<ProcessConfiguration const> processConfiguration_;
+    ActionTable const* actions_;
   };
 }
 
