14424967d258dd0cb771ddd8ab63515f90bbbe9c
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2011-01-03 12:26:21 +0000

    adjusted to moved header file

--- Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h
+++ Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h
@@ -5,7 +5,7 @@
 
 #include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
 #include "DataFormats/DetId/interface/DetId.h"
-#include "Geometry/TrackerTopology/interface/ProxyStripTopology.h"
+#include "Geometry/TrackerGeometryBuilder/interface/ProxyStripTopology.h"
 
 class StripGeomDetType;
 class StripTopology;
