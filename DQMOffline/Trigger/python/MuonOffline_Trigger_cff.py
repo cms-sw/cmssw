7496bedabd4268caba966452e976cb529e9979a4
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2011-01-03 21:13:03 +0000

    Overhaul of muon offline DQM

--- DQMOffline/Trigger/python/MuonOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/MuonOffline_Trigger_cff.py
@@ -1,13 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.Trigger.MuonTrigRateAnalyzer_cfi import *
-from DQMOffline.Trigger.TopTrigRateAnalyzer_cfi import *
-
-from DQMOffline.Trigger.TnPEfficiency_cff import *
-
+from DQMOffline.Trigger.HLTMuonOfflineAnalyzer_cff import *
 from DQMOffline.Trigger.topHLTDiMuonDQM_cfi import *
 
-muonFullOfflineDQM = cms.Sequence( offlineDQMMuonTrig
-                                   + topTrigOfflineDQM
-                                   + topHLTDiMuonAnalyzer
-                                   + TnPEfficiency)
+muonFullOfflineDQM = cms.Sequence(
+    hltMuonOfflineAnalyzers
+    + topHLTDiMuonAnalyzer
+)
