e97c66e1746ee6d684dca343d68787357c50d155
Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch>
2011-01-05 17:08:41 +0000

    Change a:      make_pair<X,Y>(x,y)   to:      make_pair(x,y)   Required for gcc45/c++0x.

--- PhysicsTools/PatAlgos/plugins/JetCorrFactorsProducer.cc
+++ PhysicsTools/PatAlgos/plugins/JetCorrFactorsProducer.cc
@@ -162,7 +162,7 @@ JetCorrFactorsProducer::produce(edm::Event& event, const edm::EventSetup& setup)
       // factors, which might be flavor dependent or not. In the default configuration 
       // the CorrectionFactor will look like this: 'Uncorrected': 1 ; 'L2Relative': x ; 
       // 'L3Absolute': x ; 'L5Flavor': v, x, y, z ; 'L7Parton': v, x, y, z 
-      jec.push_back(std::make_pair<std::string, std::vector<float> >((corrLevel->second[idx]).substr(0, (corrLevel->second[idx]).find("_")), factors));
+      jec.push_back(std::make_pair((corrLevel->second[idx]).substr(0, (corrLevel->second[idx]).find("_")), factors));
     }
     // create the actual object with the scale factors we want the valuemap to refer to
     // label_ corresponds to the label of the module instance
