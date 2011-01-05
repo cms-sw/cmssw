3f4a213a52df6deb8b60dd21a95e1298a20e1ef4
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-01-05 21:53:01 +0000

    Add constructor from ProductRegistry, and standardize style

--- FWCore/Framework/src/SignallingProductRegistry.cc
+++ FWCore/Framework/src/SignallingProductRegistry.cc
@@ -2,13 +2,12 @@
 //
 // Package:     Framework
 // Class  :     SignallingProductRegistry
-// 
+//
 // Implementation:
 //     <Notes on implementation>
 //
 // Original Author:  Chris Jones
 //         Created:  Fri Sep 23 16:52:50 CEST 2005
-// $Id: SignallingProductRegistry.cc,v 1.3 2006/06/24 01:46:34 wmtan Exp $
 //
 
 // system include files
@@ -23,38 +22,39 @@ using namespace edm;
 //
 namespace {
    struct StackGuard {
-      StackGuard(const std::string& iTypeName, std::map<std::string,unsigned int>& ioStack, bool iFromListener):
-      numType_(++ioStack[iTypeName]), itr_(ioStack.find(iTypeName)),fromListener_(iFromListener)
-      { if(iFromListener) {++(itr_->second);} }
+      StackGuard(std::string const& iTypeName, std::map<std::string, unsigned int>& ioStack, bool iFromListener) :
+          numType_(++ioStack[iTypeName]), itr_(ioStack.find(iTypeName)), fromListener_(iFromListener) {
+        if(iFromListener) {++(itr_->second);}
+      }
 
       ~StackGuard() {
          --(itr_->second);
-         if(fromListener_){
+         if(fromListener_) {
             --(itr_->second);
          }
       }
-      
+
       unsigned int numType_;
-      std::map<std::string,unsigned int>::iterator itr_;
+      std::map<std::string, unsigned int>::iterator itr_;
       bool fromListener_;
    };
 }
 
-void SignallingProductRegistry::addCalled(BranchDescription const& iProd, bool iFromListener)
-{
+void SignallingProductRegistry::addCalled(BranchDescription const& iProd, bool iFromListener) {
    StackGuard guard(iProd.className(), typeAddedStack_, iFromListener);
    if(guard.numType_ > 2) {
-      throw cms::Exception("CircularReference")
-      <<"Attempted to register the production of "<<iProd.className()
-      <<" from module "
-      <<iProd.moduleLabel()
-      <<" with product instance \""
-      <<iProd.productInstanceName()
-      <<"\"\n"
-      <<"However, this was in reaction to a registration of a production for the same type \n"
-      <<"from another module who was also listening to product registrations.\n"
-      <<"This can lead to circular Event::get* calls.\n"
-      <<"Please reconfigure job so it does not contain both of the modules.";
+     throw cms::Exception("CircularReference")
+       << "Attempted to register the production of "
+       << iProd.className()
+       << " from module "
+       << iProd.moduleLabel()
+       << " with product instance \""
+       << iProd.productInstanceName()
+       << "\"\n"
+       << "However, this was in reaction to a registration of a production for the same type \n"
+       << "from another module who was also listening to product registrations.\n"
+       << "This can lead to circular Event::get* calls.\n"
+       << "Please reconfigure job so it does not contain both of the modules.";
    }
    productAddedSignal_(iProd);
 }
--- FWCore/Framework/src/SignallingProductRegistry.h
+++ FWCore/Framework/src/SignallingProductRegistry.h
@@ -1,10 +1,10 @@
-#ifndef Framework_SignallingProductRegistry_h
-#define Framework_SignallingProductRegistry_h
+#ifndef FWCore_Framework_SignallingProductRegistry_h
+#define FWCore_Framework_SignallingProductRegistry_h
 // -*- C++ -*-
 //
 // Package:     Framework
 // Class  :     SignallingProductRegistry
-// 
+//
 /**\class SignallingProductRegistry SignallingProductRegistry.h FWCore/Framework/interface/SignallingProductRegistry.h
 
  Description: <one line class summary>
@@ -16,31 +16,28 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Fri Sep 23 16:47:10 CEST 2005
-// $Id: SignallingProductRegistry.h,v 1.5 2006/09/01 18:16:42 wmtan Exp $
 //
 
 // system include files
-#include "sigc++/signal.h"
 #include <map>
 #include <string>
+
+#include "boost/utility.hpp"
+#include "sigc++/signal.h"
 
 // user include files
 #include "DataFormats/Provenance/interface/ProductRegistry.h"
 
 // forward declarations
 namespace edm {
-   class SignallingProductRegistry : public ProductRegistry
-   {
+   class SignallingProductRegistry : public ProductRegistry, private boost::noncopyable {
 
    public:
       SignallingProductRegistry() : ProductRegistry(), productAddedSignal_(), typeAddedStack_() {}
+      explicit SignallingProductRegistry(ProductRegistry const& preg) : ProductRegistry(preg.productList(), false), productAddedSignal_(), typeAddedStack_() {}
       sigc::signal<void, BranchDescription const&> productAddedSignal_;
-      
+
    private:
-      SignallingProductRegistry(const SignallingProductRegistry&); // stop default
-
-      const SignallingProductRegistry& operator=(const SignallingProductRegistry&); // stop default
-
       virtual void addCalled(BranchDescription const&, bool);
       // ---------- member data --------------------------------
       std::map<std::string, unsigned int> typeAddedStack_;
