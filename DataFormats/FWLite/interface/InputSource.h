c63dbd11e34ad573c68f6567864d3bdb6b6facb5
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-01-09 13:25:47 +0000

    cleanup and a few comment lines added

--- DataFormats/FWLite/interface/InputSource.h
+++ DataFormats/FWLite/interface/InputSource.h
@@ -1,47 +1,55 @@
 #ifndef DataFormats_FWLite_interface_InputSource_h
 #define DataFormats_FWLite_interface_InputSource_h
 
-
-/**
-  \class    fwlite::InputSource "DataFormats/FWLite/interface/InputSource.h"
-  \brief    Helper class to handle FWLite file input sources
-
-  This is a very simple class to handle the appropriate python configuration
-  of input files in FWLite. 
-
-  \author   Salvatore Rappoccio
-*/
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include <vector>
 #include <string>
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+/**
+  \class    InputSource InputSource.h "DataFormats/FWLite/interface/InputSource.h"
+  \brief    Helper class to handle FWLite file input sources
+
+  This is a very simple class to handle the appropriate python configuration of 
+  input files in FWLite. 
+*/
 
 namespace fwlite {
+
   class InputSource {
+  
   public:
+    /// empty constructor
     InputSource() {
       throw cms::Exception("InvalidInput") << "Must specify a vstring fileNames" << std::endl;
     }
-    InputSource( edm::ParameterSet const & pset ) :
-      files_( pset.getParameter<edm::ParameterSet>("fwliteInput").getParameter<std::vector<std::string> >("fileNames") )
-	{
-	  if(pset.getParameter<edm::ParameterSet>("fwliteInput").exists("maxEvents")){ 
-	    maxEvents_=pset.getParameter<edm::ParameterSet>("fwliteInput").getParameter<int>("maxEvents"); 
-	  }
-	  if(pset.getParameter<edm::ParameterSet>("fwliteInput").exists("outputEvery")){ 
-	    reportAfter_=pset.getParameter<edm::ParameterSet>("fwliteInput").getParameter<unsigned int>("outputEvery"); 
-	  }
+    /// default constructor from parameter set
+    InputSource(const edm::ParameterSet& cfg) :
+      maxEvents_(-1), reportAfter_(10),
+      files_( cfg.getParameter<edm::ParameterSet>("fwliteInput").getParameter<std::vector<std::string> >("fileNames") )
+      {
+	// optional parameter
+	if( cfg.getParameter<edm::ParameterSet>("fwliteInput").existsAs<int>("maxEvents")){ 
+	  maxEvents_ = cfg.getParameter<edm::ParameterSet>("fwliteInput").getParameter<int>("maxEvents"); 
 	}
-
-    std::vector<std::string> const & files() const { return files_; }
-
-    int maxEvents() const { return maxEvents_; }
-    unsigned int reportAfter() const { return reportAfter_;}
-
+	// optional parameter
+	if( cfg.getParameter<edm::ParameterSet>("fwliteInput").existsAs<unsigned int>("outputEvery")){ 
+	  reportAfter_ = cfg.getParameter<edm::ParameterSet>("fwliteInput").getParameter<unsigned int>("outputEvery"); 
+	}
+      }
+      /// return vector of files_
+      const std::vector<std::string>& files() const { return files_; }
+      /// return maxEvetns_
+      int maxEvents() const { return maxEvents_; }
+      /// return reportAfter_
+      unsigned int reportAfter() const { return reportAfter_;}
+      
   protected:
-    std::vector<std::string>  files_;
-    int                       maxEvents_;
-    unsigned int              reportAfter_;
+      /// maximal number of events to loop
+      int maxEvents_;
+      /// report after N events
+      unsigned int reportAfter_;
+      /// vector of input files
+      std::vector<std::string> files_;
   };
 }
 
--- DataFormats/FWLite/interface/OutputFiles.h
+++ DataFormats/FWLite/interface/OutputFiles.h
@@ -1,37 +1,36 @@
 #ifndef DataFormats_FWLite_interface_OutputFiles_h
 #define DataFormats_FWLite_interface_OutputFiles_h
 
-
-/**
-  \class    fwlite::OutputFiles "DataFormats/FWLite/interface/OutputFiles.h"
-  \brief    Helper class to handle FWLite file output names
-
-  This is a very simple class to handle the appropriate python configuration
-  of output files in FWLite. 
-
-  \author   Salvatore Rappoccio
-*/
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include <vector>
 #include <string>
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+/**
+  \class    OutputFiles OutputFiles.h "DataFormats/FWLite/interface/OutputFiles.h"
+  \brief    Helper class to handle FWLite file output names
+
+  This is a very simple class to handle the appropriate python configuration of 
+  output files in FWLite. 
+*/
 
 namespace fwlite {
+
   class OutputFiles {
+  
   public:
+    /// empty constructor 
     OutputFiles() {
       throw cms::Exception("InvalidInput") << "Must specify a string fileName" << std::endl;
     }
-    OutputFiles( edm::ParameterSet const & pset ) :
-    file_( pset.getParameter<edm::ParameterSet>("fwliteOutput").getParameter<std::string>("fileName") )
-	{
-	}
-
-      std::string const & file() const { return file_; }
-
+    /// default constructor from parameter set
+    OutputFiles(const edm::ParameterSet& cfg) :
+      file_( cfg.getParameter<edm::ParameterSet>("fwliteOutput").getParameter<std::string>("fileName") ) {};
+    /// return output fuke name
+    std::string const & file() const { return file_; }
+      
   protected:
-      std::string file_;
-    
+    /// output file name
+    std::string file_;
   };
 }
 
