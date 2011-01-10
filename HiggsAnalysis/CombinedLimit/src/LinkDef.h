3ddc4d14f6b30427b551b5814a75ced4076cd5c0
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-01-10 09:26:26 +0000

    Proposal that verbosely debugs another proposal function

--- HiggsAnalysis/CombinedLimit/src/LinkDef.h
+++ HiggsAnalysis/CombinedLimit/src/LinkDef.h
@@ -1,7 +1,9 @@
 #include "HiggsAnalysis/CombinedLimit/interface/TestProposal.h"
+#include "HiggsAnalysis/CombinedLimit/interface/DebugProposal.h"
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class TestProposal;
+#pragma link C++ class DebugProposal;
 #endif
