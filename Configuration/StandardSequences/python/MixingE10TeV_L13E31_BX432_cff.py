591114f97b3b6b809f8a84c7864ab55a812dcff6
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2011-01-10 14:27:00 +0000

    moving Mixing configuration to Mixing.py

--- Configuration/StandardSequences/python/MixingE10TeV_L13E31_BX432_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimGeneral.MixingModule.mix_E10TeV_L13E31_BX432_cfi import *
-
--- Configuration/StandardSequences/python/MixingE7TeV_AVE_5_BX156_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimGeneral.MixingModule.mix_E7TeV_AVE_5_BX156_cfi import *
-
