f46eb8e8824ce4670b1f5d6c3fb286e9355f2ee9
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2011-01-11 09:20:56 +0000

    yet an other attempt to put the files in the head

---
+++ DataFormats/ParticleFlowCandidate/interface/PFCandidateElectronExtraFwd.h
@@ -0,0 +1,48 @@
+#ifndef ParticleFlowCandidate_PFCandidateElectronExtraFwd_h
+#define ParticleFlowCandidate_PFCandidateElectronExtraFwd_h
+#include <vector>
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefProd.h"
+#include "DataFormats/Common/interface/RefVector.h"
+#include "DataFormats/Common/interface/Ptr.h"
+#include "DataFormats/Common/interface/FwdRef.h"
+#include "DataFormats/Common/interface/FwdPtr.h"
+
+namespace reco {
+  class PFCandidateElectronExtra;
+
+    /// collection of PFCandidateElectronExtras
+  typedef std::vector<reco::PFCandidateElectronExtra> PFCandidateElectronExtraCollection;
+
+  /// iterator 
+  typedef PFCandidateElectronExtraCollection::const_iterator PFCandidateElectronExtraConstIterator;
+
+  /// iterator 
+  typedef PFCandidateElectronExtraCollection::iterator PFCandidateElectronExtraIterator;
+
+  /// persistent reference to a PFCandidateElectronExtra
+  typedef edm::Ref<PFCandidateElectronExtraCollection> PFCandidateElectronExtraRef;
+
+  /// persistent Ptr to a PFCandidateElectronExtra
+  typedef edm::Ptr<PFCandidateElectronExtra> PFCandidateElectronExtraPtr;
+
+  /// persistent reference to a PFCandidateElectronExtras collection
+  typedef edm::RefProd<PFCandidateElectronExtraCollection> PFCandidateElectronExtraRefProd;
+
+  /// vector of reference to GenParticleCandidate in the same collection
+  typedef edm::RefVector<PFCandidateElectronExtraCollection> PFCandidateElectronExtraRefVector;
+
+   /// persistent "forward" reference to a PFCandidateElectronExtra
+   typedef edm::FwdRef<PFCandidateElectronExtraCollection> PFCandidateElectronExtraFwdRef;
+ 
+   /// persistent FwdPtr to a PFCandidateElectronExtra
+   typedef edm::FwdPtr<PFCandidateElectronExtra> PFCandidateElectronExtraFwdPtr;
+ 
+   /// vector of "forward" reference
+   typedef std::vector<PFCandidateElectronExtraFwdRef> PFCandidateElectronExtraFwdRefVector;
+ 
+   /// vector of "forward" reference
+   typedef std::vector<PFCandidateElectronExtraFwdPtr> PFCandidateElectronExtraFwdPtrVector;
+
+}
+#endif
