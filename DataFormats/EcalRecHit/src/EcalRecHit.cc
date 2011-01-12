d0401e488ad0df398e09f57d29c9527b60aaca1b
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2011-01-12 13:22:59 +0000

    Removed the recoFlag/flagBit ambiguity, changed flagBit interfeace. recoFlag() function provided for backward compat

--- DataFormats/EcalRecHit/src/EcalRecHit.cc
+++ DataFormats/EcalRecHit/src/EcalRecHit.cc
@@ -18,10 +18,11 @@ EcalRecHit::EcalRecHit(const DetId& id, float energy, float time, uint32_t flags
 }
 
 bool EcalRecHit::isRecovered() const {
-        return (    recoFlag() == kLeadingEdgeRecovered 
-                 || recoFlag() == kNeighboursRecovered 
-                 || recoFlag() == kTowerRecovered 
-                 );
+
+  return (    checkFlag(kLeadingEdgeRecovered) || 
+	      checkFlag(kNeighboursRecovered)  ||
+	      checkFlag(kTowerRecovered) 
+	  );
 }
 
 float EcalRecHit::chi2() const
@@ -30,30 +31,7 @@ float EcalRecHit::chi2() const
         return (float)rawChi2 / (float)((1<<7)-1) * 64.;
 }
 
-float EcalRecHit::chi2Prob() const
-{
-        std::cerr << "Please retrieve the chi2 value instead of its probability.\n";
-        std::cerr << "Use the method EcalRecHit::chi2() for that purpose.\n";
-        std::cerr << "The chi2 is still in a commissioning phase and further\n";
-        std::cerr << "actions will be taken in the future. Thanks.\n";
-        assert(false);
-        uint32_t rawChi2Prob = 0x7F & (flags()>>4);
-        return (float)rawChi2Prob / (float)((1<<7)-1);
-}
 
-float EcalRecHit::outOfTimeChi2Prob() const
-{
-        std::cerr << "Please retrieve the chi2 value instead of its probability.\n";
-        std::cerr << "Use the method EcalRecHit::outOfTimeChi2() for that purpose.\n";
-        std::cerr << "The chi2 is still in a commissioning phase and further\n";
-        std::cerr << "actions will be taken in the future. Thanks.\n";
-        assert(false);
-        /*
-        uint32_t rawChi2Prob = 0x7F & (flags()>>24);
-        return (float)rawChi2Prob / (float)((1<<7)-1);
-        */
-        return -1; // will never get here
-}
 
 float EcalRecHit::outOfTimeChi2() const
 {
@@ -69,30 +47,9 @@ float EcalRecHit::outOfTimeEnergy() const
         return (float) significand*pow(10,exponent-5);
 }
 
-void EcalRecHit::setRecoFlag( uint32_t flag )
-{
-        setFlags( (~0xF & flags()) | (flag & 0xF) );
-}
 
-void EcalRecHit::setChi2Prob( float chi2Prob )
-{
-        /* not used - store the raw chi2 instead */
-        std::cerr << "Please store the chi2 value instead of its probability.\n";
-        std::cerr << "Use the method EcalRecHit::setChi2() for that purpose.\n";
-        std::cerr << "The chi2 is still in a commissioning phase and further\n";
-        std::cerr << "actions will be taken in the future. Thanks.\n";
-        assert(false);
-        /*
-        if ( chi2Prob < 0 || chi2Prob > 1 ) {
-                edm::LogWarning("EcalRecHit::setChi2Prob") << "chi2Prob outside limits [0, 1] : " << chi2Prob;
-        } else {
-                // use 7 bits
-                uint32_t rawChi2Prob = lround( chi2Prob * ((1<<7)-1) );
-                // shift by 4 bits (recoFlag)
-                setFlags( (~(0x7F<<4) & flags()) | ((rawChi2Prob & 0x7F)<<4) );
-        }
-        */
-}
+
+
 
 void EcalRecHit::setChi2( float chi2 )
 {
@@ -116,25 +73,7 @@ void EcalRecHit::setOutOfTimeEnergy( float energy )
         }
 }
 
-void EcalRecHit::setOutOfTimeChi2Prob( float chi2Prob )
-{
-        /* not used - store the raw chi2 instead */
-        std::cerr << "Please store the chi2 value instead of its probability.\n";
-        std::cerr << "Use the method EcalRecHit::setOutOfTimeChi2() for that purpose.\n";
-        std::cerr << "The chi2 is still in a commissioning phase and further\n";
-        std::cerr << "actions will be taken in the future. Thanks.\n";
-        assert(false);
-        /*
-        if ( chi2Prob < 0 || chi2Prob > 1 ) {
-                edm::LogWarning("EcalRecHit::setOutOfTimeChi2Prob") << "chi2Prob outside limits [0, 1] : " << chi2Prob;
-        } else {
-                // use 7 bits
-                uint32_t rawChi2Prob = lround( chi2Prob * ((1<<7)-1) );
-                // shift by 24 bits (recoFlag + chi2 + outOfTimeEnergy)
-                setFlags( (~(0x7F<<24) & flags()) | ((rawChi2Prob & 0x7F)<<24) );
-        }
-        */
-}
+
 
 
 void EcalRecHit::setOutOfTimeChi2( float chi2 )
@@ -191,6 +130,18 @@ bool EcalRecHit::isTimeErrorValid() const
         return true;
 }
 
+
+ /// DEPRECATED provided for temporary backward compatibility
+EcalRecHit::Flags EcalRecHit::recoFlag() const {
+  for (int i=0; i<kUnknown; ++i){
+    if (checkFlag(i)) return Flags(i);
+  }
+  // no flag assigned, assume good
+  return kGood;
+}
+
+
+
 std::ostream& operator<<(std::ostream& s, const EcalRecHit& hit) {
   if (hit.detid().det() == DetId::Ecal && hit.detid().subdetId() == EcalBarrel) 
     return s << EBDetId(hit.detid()) << ": " << hit.energy() << " GeV, " << hit.time() << " ns";
@@ -201,3 +152,5 @@ std::ostream& operator<<(std::ostream& s, const EcalRecHit& hit) {
   else
     return s << "EcalRecHit undefined subdetector" ;
 }
+
+
