6e3a5955d09d2c8c82a681838a313ffc930d3a7d
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2011-01-13 20:35:49 +0000

    Test AND write new keys from list of currently valid keys.

--- CondTools/L1Trigger/scripts/validate-l1Key.sh
+++ CondTools/L1Trigger/scripts/validate-l1Key.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# L1Trigger O2O - validate L1 key, called by cron job
+# L1Trigger O2O - validate TSC key, and write it to ORCON called by cron job on l1-o2o.cms
+
+# Run as l1emulator on machine with frontier squids enabled.  Check with `rpm -qa | grep squid`.
+# Should have a line with STABLE, e.g. squid-2.7.STABLE7-frontiercmshlt2
 
 pflag=0
 while getopts 'ph' OPTION
@@ -22,14 +25,14 @@ shift $(($OPTIND - 1))
 
 release=CMSSW_3_8_1_onlpatch4_ONLINE
 emulatorRelease=CMSSW_3_8_2
-version=008
+version=009
 
 #==============================================================================
-# Last file contains last time stamp, its key, and its validation status
+# File containing TSC keys that have been tested and written (+ status)
 #==============================================================================
 
-#lastFile=~zrwan/CMSSW_3_5_0/cronjob/last.txt
-lastFile=~popcondev/L1Job/${release}/validate-l1Key/last.txt
+# writtenFile=~zrwan/CMSSW_3_5_0/cronjob/writtenTscKeys.txt
+writtenFile=~popcondev/L1Job/${release}/validate-l1Key/writtenTscKeys.txt
 
 #==============================================================================
 # Summary file
@@ -44,37 +47,6 @@ summaryFile=/nfshome0/popcondev/L1Job/o2o.summary
 
 #logFile=~zrwan/CMSSW_3_5_0/cronjob/validate-l1Key-${version}.log
 logFile=/nfshome0/popcondev/L1Job/validate-l1Key-${version}.log
-
-#==============================================================================
-# Last ID validated, its L1 key, and its status are contained in the
-# file last.txt. If the validation was failed, the contents are e.g.
-# L1_20100904_125311_2508 TSC_20100904_002309_cosmics_BASE failed
-# Otherwise, the contents are e.g.
-# L1_20100904_125311_2508 TSC_20100904_002309_cosmics_BASE successful
-# Read in the last ID validated, its L1 key, and its status. If the
-# status was successful, assume all of the L1 keys for the previous IDs
-# were validated successfully already and move forward. Otherwise, do
-# nothing and exit, and when we manually check the summary file and the log
-# file, we will find out that we have a problem to solve.
-#==============================================================================
-
-last=`cat ${lastFile}`
-
-lastID=`echo ${last} | cut -f 1 -d ' '`
-lastTscKey=`echo ${last} | cut -f 2 -d ' '`
-lastStatus=`echo ${last} | cut -f 3 -d ' '`
-
-if [ $lastStatus == "failed" ]
-    then
-#    echo "Last validated key failed: ${lastID} ${lastTscKey}" > ${summaryFile}
-    exit
-fi
-
-#==============================================================================
-# In the case that last ID was validated as successful, we move
-# forward, look for the first of the next new L1 keys which has not been
-# validated yet.
-#==============================================================================
 
 #==============================================================================
 # Set up environment
@@ -94,28 +66,45 @@ else
 fi
 eval `scramv1 run -sh`
 
-#next=`~zrwan/CMSSW_3_5_0/cronjob/getNext.sh ${lastID}`
-next=`$CMSSW_BASE/src/CondTools/L1Trigger/scripts/getNextTscKeyByID.sh ${lastID}`
+#==============================================================================
+# Find next key to be tested and written
+#==============================================================================
 
-id=`echo $next | cut -f 1 -d ' '`
-tsc_key=`echo $next | cut -f 2 -d ' '`
-
-if [ -z $id ]
+if [ ! -f ${writtenFile} ]
     then
-#    echo "`date` : validate-l1Key.sh" > ${summaryFile}
-#    echo "No new key to be validated" >> ${summaryFile}
-#    echo "Last ID ${lastID} key ${lastTscKey}" >> ${summaryFile}
-    exit
+    touch ${writtenFile}
+fi
+
+#for KEY in `~zrwan/CMSSW_3_5_0/cronjob/getValidTscKeys.sh`
+for KEY in `$CMSSW_BASE/src/CondTools/L1Trigger/scripts/getValidTscKeys.sh`
+do
+  if [ -z "${tsc_key}" ]
+      then
+      last=`grep ${KEY} writtenTscKeys.txt`
+      if [ -n "${last}" ]
+	  then
+	  status=`echo ${last} | cut -f 2 -d ' '`
+          #  echo "Key ${KEY} already written and validated: status ${status}"
+      else
+          #  echo "Setting tsc_key ${KEY}"
+	  tsc_key=${KEY}
+      fi
+  fi
+done
+
+if [ -z "${tsc_key}" ]
+    then
+    # echo "No new keys to write and test." >> ${summaryFile}
+    exit 0
 fi
 
 #==============================================================================
-# Up to this point, last ID was validated as successful, and there
-# is a new ID with a new L1 key to be validated.
+# Start testing and writing new key
 #==============================================================================
 
 echo "`date` : validate-l1Key.sh" >> ${logFile}
-echo "id = ${id}" >> ${logFile}
 echo "tsc_key = ${tsc_key}" >> ${logFile}
+echo "O2O release ${release}$" >> ${logFile}
 
 #==============================================================================
 # 1. Copy conditions for a given TSC key from online database to a sqlite file
@@ -144,6 +133,7 @@ echo "" >> ${logFile}
 
 if [ ${o2ocode1} -eq 0 ]
     then
+    echo "Running emulator job in ${emulatorRelease}" >> ${logFile}
     cd /nfshome0/popcondev/L1Job/${emulatorRelease}/validate-l1Key
     ln -sf /nfshome0/popcondev/L1Job/${release}/validate-l1Key/l1config.db .
     ln -sf /nfshome0/popcondev/L1Job/${release}/validate-l1Key/Raw.root .
@@ -165,7 +155,7 @@ if [ ${o2ocode1} -eq 0 ]
 fi
 
 #==============================================================================
-# Clean up and exit
+# 4. Clean up
 #==============================================================================
 
 #rm -f ~zrwan/CMSSW_3_5_0/cronjob/l1config.db
@@ -173,7 +163,6 @@ rm -f /nfshome0/popcondev/L1Job/${release}/validate-l1Key/l1config.db
 
 o2ocode=`echo ${o2ocode1} + ${o2ocode2} | bc`
 
-echo "id = ${id}" >> ${logFile}
 echo "tsc_key = ${tsc_key}" >> ${logFile}
 echo "getConditions status ${o2ocode1}" >> ${logFile}
 echo "emulator status ${o2ocode2}" >> ${logFile}
@@ -181,24 +170,36 @@ echo "exit code ${o2ocode}" >> ${logFile}
 if [ ${o2ocode} -eq 0 ]
     then
     echo "L1-O2O-INFO: successful" >> ${logFile}
+    validationStatus="successful"
 else
     echo "L1-O2O-INFO: failed" >> ${logFile}
+    validationStatus="failed"
+    writeStatus="failed"
 fi
 echo "`date` : validate-l1Key.sh finished" >> ${logFile}
 
-tail -7 ${logFile} >> ${summaryFile}
+tail -6 ${logFile} >> ${summaryFile}
 echo "" >> ${logFile}
 
-cat ${lastFile} >> ${lastFile}.done
+#==============================================================================
+# 5. If key is validated, call O2O payload-writing script
+#==============================================================================
+
 if [ ${o2ocode} -eq 0 ]
     then
-    echo "${id} ${tsc_key} successful" > ${lastFile}
-    # standard output goes to email
-    #echo "${id} ${tsc_key} successful"
-else
-    echo "${id} ${tsc_key} failed" > ${lastFile}
-    # standard output goes to email
-    #echo "${id} ${tsc_key} failed"
+    /nfshome0/l1emulator/o2o/o2o-tscKey-slc5.sh ${tsc_key}
+    o2ocode3=$?
+
+    if [ ${o2ocode3} -eq 0 ]
+	then
+	writeStatus="successful"
+    else
+	writeStatus="failed"
+    fi
+
+    o2ocode=`echo ${o2ocode} + ${o2ocode3} | bc`
 fi
+
+echo "${tsc_key} ${validationStatus} ${writeStatus}" >> ${writtenFile}
 
 exit ${o2ocode}
