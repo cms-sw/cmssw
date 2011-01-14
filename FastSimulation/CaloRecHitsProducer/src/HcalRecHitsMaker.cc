ffdf6636dad11f848210393277eca88b1210b5ab
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2011-01-14 11:16:43 +0000

    HF noise to cope with 2TS Reco in FullSim

--- FastSimulation/CaloRecHitsProducer/src/HcalRecHitsMaker.cc
+++ FastSimulation/CaloRecHitsProducer/src/HcalRecHitsMaker.cc
@@ -694,7 +694,7 @@ double HcalRecHitsMaker::noiseInfCfromDB(const HcalDbService * conditions,const 
   double ssqq_4 = pedWidth->getSigma(3,3);
 
   // correction factors (hb,he,ho,hf)
-  static float corrfac[4]={1.39,1.32,1.53,5.41};
+  static float corrfac[4]={1.39,1.32,1.53,3.76};
 
   int sub   = detId.subdet();
   
