ae684fa5a0565ccdb0496b3512ede385d368b219
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-01-14 16:18:38 +0000

    Update GTs for 311X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_310_V3::All',
-             'startup' : 'START310_V3::All',
-             'craft08' : 'CRFT8_37R_V1::All',
-             'craft09' : 'CRFT9_310_V1::All',
-             'com10'   : 'GR_R_310_V2::All',
-             'hltonline' : 'GR_R_310_V2::All', # same as com10 until H tag available
-             'starthi' : 'START310_V1HI::All'
+autoCond = { 'mc'      : 'MC_311_V0::All',
+             'startup' : 'START311_V0::All',
+             'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
+             'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
+             'com10'   : 'GR_R_311_V0::All',
+             'hltonline' : 'GR_R_311_V0::All', # same as com10 until H tag available
+             'starthi' : 'START311_V0HI::All'
              }
