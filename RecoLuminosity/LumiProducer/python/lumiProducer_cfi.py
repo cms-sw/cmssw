d6f9373a9847dbff82c3b3e60450951d41059b6c
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2011-01-15 11:25:52 +0000

    set cachedefault size

--- RecoLuminosity/LumiProducer/python/lumiProducer_cfi.py
+++ RecoLuminosity/LumiProducer/python/lumiProducer_cfi.py
@@ -2,5 +2,6 @@
 import FWCore.ParameterSet.Config as cms
 lumiProducer=cms.EDProducer("LumiProducer",
                             connect=cms.string(''),
-                            lumiversion=cms.untracked.string('')
+                            lumiversion=cms.untracked.string(''),
+                            ncacheEntries=cms.untracked.uint32(5)
                             )
