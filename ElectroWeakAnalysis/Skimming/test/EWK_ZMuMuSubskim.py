a996f054f77f439edb6ead1c380106b77c37ca10
Francesco Fabozzi <sha1-e99353100965f6444b6939202d1c2c2146f860f4@cern.ch>
2011-01-18 16:21:36 +0000

    update for 3_9_X analysis

--- ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
+++ ElectroWeakAnalysis/Skimming/test/EWK_ZMuMuSubskim.py
@@ -8,14 +8,14 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 # source
 process.source = cms.Source("PoolSource", 
      fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_3_8_5/RelValZMM/GEN-SIM-RECO/START38_V12-v1/0041/1C1BBE0B-D2D2-DF11-BDA3-002618943852.root'
+    'file:3ADEF5CA-1511-E011-ADD2-0017A4770010.root'
     )
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('START38_V12::All')
+process.GlobalTag.globaltag = cms.string('START39_V8::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 ############
@@ -32,24 +32,24 @@ process.load("ElectroWeakAnalysis.Skimming.zMuMu_SubskimPathsWithMCTruth_cff")
 process.zMuMuSubskimOutputModule.outputCommands.extend(process.mcEventContent.outputCommands)
 ############
 
-process.zMuMuSubskimOutputModule.fileName = 'file:/tmp/fabozzi/testZMuMuSubskim.root'
+process.zMuMuSubskimOutputModule.fileName = 'file:testZMuMuSubskim.root'
 
 process.outpath = cms.EndPath(process.zMuMuSubskimOutputModule)
 
 ### Here set the HLT Path for trigger filtering and matching
-process.dimuonsHLTFilter.HLTPaths = ["HLT_Mu9", "HLT_Mu11", "HLT_DoubleMu3"]
-process.muonTriggerMatchHLTMuons.pathNames = cms.vstring( 'HLT_Mu11' )
-process.userDataMuons.hltPath = cms.string("HLT_Mu11")
-process.userDataDimuons.hltPath = cms.string("HLT_Mu11")
-process.userDataDimuonsOneTrack.hltPath = cms.string("HLT_Mu11")
+process.dimuonsHLTFilter.HLTPaths = ["HLT_Mu9", "HLT_Mu11", "HLT_Mu15_v1"]
+process.muonTriggerMatchHLTMuons.pathNames = cms.vstring( 'HLT_Mu15_v1' )
+process.userDataMuons.hltPath = cms.string("HLT_Mu15_v1")
+process.userDataDimuons.hltPath = cms.string("HLT_Mu15_v1")
+process.userDataDimuonsOneTrack.hltPath = cms.string("HLT_Mu15_v1")
 ############
 
-### 3_6_X reprocessed MC: to process REDIGI HLT tables uncomment the following
-#process.dimuonsHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","REDIGI36X")
-#process.patTrigger.processName = "REDIGI36X"
-#process.patTriggerEvent.processName = "REDIGI36X"
-#process.patTrigger.triggerResults = cms.InputTag( "TriggerResults::REDIGI36X" )
-#process.patTrigger.triggerEvent = cms.InputTag( "hltTriggerSummaryAOD::REDIGI36X" )
+### 3_9_X reprocessed MC: to process REDIGI HLT tables uncomment the following
+#process.dimuonsHLTFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","REDIGI39X")
+#process.patTrigger.processName = "REDIGI39X"
+#process.patTriggerEvent.processName = "REDIGI39X"
+#process.patTrigger.triggerResults = cms.InputTag( "TriggerResults::REDIGI39X" )
+#process.patTrigger.triggerEvent = cms.InputTag( "hltTriggerSummaryAOD::REDIGI39X" )
 
 
 
