2fe48ecbf31fbb97a184e15f9612cc185517a4f5
Camilo Andres Carrillo Montoya <sha1-9a8ae306c671cce603c2f323bae9412ba35af28e@cern.ch>
2011-01-19 13:25:21 +0000

    absolute path for dat file

--- RecoLocalMuon/RPCRecHit/python/rpcRecHitAli_cfi.py
+++ RecoLocalMuon/RPCRecHit/python/rpcRecHitAli_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 rpcRecHitAli = cms.EDProducer("RPCRecHitAli",
-  debug = cms.untracked.bool(False),
+  debug = cms.untracked.bool(True),
   rpcRecHits = cms.InputTag("rpcRecHits"),
-  AliFileName = cms.untracked.string('../data/Alignment.dat')
+  AliFileName = cms.untracked.string('/afs/cern.ch/user/c/carrillo/efficiency/CMSSW_3_8_1/src/RecoLocalMuon/RPCRecHit/data/Alignment.dat')
 )
