1ee3498849f9d6e7dfc36a8c2dc9846b8750b5b6
Chris Tully <sha1-282340fa34747b4e5c06e8de32a0f36f636155ed@cern.ch>
2011-01-19 21:34:31 +0000

    Added Photon R9 ID variable R9 = e3x3/E(supercluster)

---
+++ RecoEgamma/EgammaHLTProducers/interface/EgammaHLTR9IDProducer.h
@@ -0,0 +1,50 @@
+// -*- C++ -*-
+//
+// Package:    EgammaHLTProducers
+// Class:      EgammaHLTR9IDProducer
+// 
+/**\class EgammaHLTR9IDProducer EgammaHLTR9IDProducer.cc RecoEgamma/EgammaHLTProducers/interface/EgammaHLTR9IDProducer.h
+*/
+//
+// Original Author:  Roberto Covarelli (CERN)
+//         Created:  Tue Jun 13 14:48:33 CEST 2006
+// $Id: EgammaHLTR9Producer.h,v 1.2 2010/06/10 16:19:31 ghezzi Exp $
+//         modified by Chris Tully (Princeton)
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//
+// class declaration
+//
+
+class EgammaHLTR9IDProducer : public edm::EDProducer {
+   public:
+      explicit EgammaHLTR9IDProducer(const edm::ParameterSet&);
+      ~EgammaHLTR9IDProducer();
+
+
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+   private:
+      // ----------member data ---------------------------
+
+  edm::InputTag recoEcalCandidateProducer_;
+  edm::InputTag ecalRechitEBTag_;
+  edm::InputTag ecalRechitEETag_;
+  
+  edm::ParameterSet conf_;
+
+};
+
