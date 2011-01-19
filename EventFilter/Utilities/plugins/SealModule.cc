95045160b725d4f76ce07372dd9367e88e60c997
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2011-01-19 13:32:40 +0000

    added support for stream rates

--- EventFilter/Utilities/plugins/SealModule.cc
+++ EventFilter/Utilities/plugins/SealModule.cc
@@ -3,6 +3,7 @@
 #include "EventFilter/Utilities/interface/MicroStateService.h"
 #include "EventFilter/Utilities/interface/ModuleWebRegistry.h"
 #include "EventFilter/Utilities/interface/ServiceWebRegistry.h"
+#include "EventFilter/Utilities/interface/ShmOutputModuleRegistry.h"
 #include "EventFilter/Utilities/interface/TimeProfilerService.h"
 #include "EventFilter/Utilities/interface/Stepper.h"
 #include "EventFilter/Utilities/plugins/ExceptionGenerator.h"
@@ -15,10 +16,12 @@ using namespace evf;
 typedef edm::serviceregistry::AllArgsMaker<MicroStateService> MicroStateServiceMaker;
 typedef ParameterSetMaker<ModuleWebRegistry> maker1;
 typedef ParameterSetMaker<ServiceWebRegistry> maker2;
+typedef ParameterSetMaker<ShmOutputModuleRegistry> maker3;
 
 DEFINE_FWK_SERVICE_MAKER(MicroStateService, MicroStateServiceMaker);
 DEFINE_FWK_SERVICE_MAKER(ModuleWebRegistry,maker1);
 DEFINE_FWK_SERVICE_MAKER(ServiceWebRegistry,maker2);
+DEFINE_FWK_SERVICE_MAKER(ShmOutputModuleRegistry,maker3);
 DEFINE_FWK_SERVICE(TimeProfilerService);
 DEFINE_FWK_SERVICE(Stepper);
 DEFINE_FWK_MODULE(ExceptionGenerator);
