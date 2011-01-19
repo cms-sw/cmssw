405d32838b00f30a7e018ef4c0708baece8593d1
edwenger <sha1-1dd8aeb6c3bfbb7044c74c70dcb7a705e5cc548b@cern.ch>
2011-01-19 20:34:20 +0000

    updated PF cff without GSF tracks

--- RecoHI/Configuration/python/Reconstruction_hiPF_cff.py
+++ RecoHI/Configuration/python/Reconstruction_hiPF_cff.py
@@ -8,38 +8,27 @@ particleFlowClusterPS.thresh_Pt_Seed_Endcap = cms.double(99999.)
 particleFlowClusterHFEM.thresh_Pt_Seed_Endcap = cms.double(99999.)
 particleFlowClusterHFHAD.thresh_Pt_Seed_Endcap = cms.double(99999.)
 
-# run tracker-driven electron seeds with heavy-ion tracks
-from TrackingTools.GsfTracking.FwdAnalyticalPropagator_cfi import *
-from RecoParticleFlow.PFTracking.trackerDrivenElectronSeeds_cff import *
-trackerDrivenElectronSeeds.UseQuality = cms.bool(True)
-trackerDrivenElectronSeeds.TrackQuality = cms.string('highPurity')
-trackerDrivenElectronSeeds.TkColList = cms.VInputTag("hiSelectedTracks")
-trackerDrivenElectronSeeds.ProducePreId = cms.untracked.bool(False)
-trackerDrivenElectronSeeds.DisablePreId = cms.untracked.bool(True)
-
-# additions for 3_11_X
-from RecoParticleFlow.PFTracking.pfTrack_cfi import *  
-pfTrack.UseQuality = cms.bool(True) 
+from RecoParticleFlow.PFTracking.pfTrack_cfi import *   
+pfTrack.UseQuality = cms.bool(True)     
 pfTrack.TrackQuality = cms.string('highPurity')   
-pfTrack.TkColList = cms.VInputTag("hiSelectedTracks") 
+pfTrack.TkColList = cms.VInputTag("hiSelectedTracks")  
+pfTrack.GsfTracksInEvents = cms.bool(False)  
 
 # run a trimmed down PF sequence with heavy-ion vertex, no electrons, etc.
 from RecoParticleFlow.Configuration.RecoParticleFlow_cff import *
 particleFlowBlock.useConvBremPFRecTracks = cms.bool(False)
 particleFlowBlock.usePFatHLT = cms.bool(True)
 particleFlowBlock.useIterTracking = cms.bool(False)
-particleFlowBlock.useNuclear = cms.bool(False)
+particleFlowBlock.useNuclear = cms.bool(False)   
 particleFlow.vertexCollection = cms.InputTag("hiSelectedVertex")
 particleFlow.usePFElectrons = cms.bool(False)
-#particleFlowReco.remove(particleFlowTrack)
 particleFlowReco.remove(particleFlowTrackWithDisplacedVertex)
 particleFlowReco.remove(pfElectronTranslatorSequence)
 
 # define new high-level RECO sequence
 from RecoJets.Configuration.RecoPFJets_cff import *
 HiParticleFlowReco = cms.Sequence(particleFlowCluster
-                                  * trackerDrivenElectronSeeds
-                                  * pfTrack  
+                                  * pfTrack
                                   * particleFlowReco
                                   * recoPFJets)
 
