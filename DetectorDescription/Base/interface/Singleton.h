75499585ffaa887b1f40673023bc13ea241e7519
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2011-01-20 10:34:15 +0000

    Remove commented out code. Remove unneeded include.

--- DetectorDescription/Base/interface/Singleton.h
+++ DetectorDescription/Base/interface/Singleton.h
@@ -1,23 +1,18 @@
 #ifndef DDI_Singleton_h
 #define DDI_Singleton_h
-#include "DetectorDescription/Base/interface/Ptr.h"
 
 namespace DDI {
  template <class I> 
- class Singleton //: private I
+ class Singleton 
  {
  public:
    typedef I value_type;
-   //typedef I* pointer_type;
-   
-   //~Singleton() { delete instance_; }
-   
+
    static value_type & instance();
    
  private:  
    Singleton();
    Singleton& operator=(const Singleton &);
-   //static pointer_type instance_;
  };
 }
 #endif
