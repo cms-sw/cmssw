d33e1b4eb1b173b74b0112466f1d7b5fe3e553e9
Francisco Xavier Yumiceva Del Pozo <sha1-d7feb986bbb8c90e95e5ffd6abf676c1f06ed388@cern.ch>
2011-01-21 23:32:20 +0000

    update

--- RecoBTag/PerformanceDB/test/plotPerformanceDB.py
+++ RecoBTag/PerformanceDB/test/plotPerformanceDB.py
@@ -17,7 +17,7 @@ from ROOT import *
 def main():
 
     executable = "getbtagPerformance"
-    rootFile = "rootFile=performance_beff.root"
+    rootFile = "rootFile=performanceDB.root"
     payload  = "payload=SYSTEM8SSVHEM"
     OP = "SSVHEM"
     flavor   = "flavor=b"
@@ -45,18 +45,24 @@ def main():
     SFerrarray = array('d')
     effarray = array('d')
     efferrarray = array('d')
+    MCeffarray = array('d')
+    MCefferrarray = array('d')    
     SFarray_alleta = array('d')
     SFerrarray_alleta = array('d')
     effarray_alleta = array('d')
     efferrarray_alleta = array('d')
-                
+    MCeffarray_alleta = array('d')
+    MCefferrarray_alleta = array('d')
+        
 
     for ipt in xrange(0,ptNbins):
         SF_alleta = 0.
         SFerr_alleta = 0.
         Eff_alleta = 0.
         Efferr_alleta = 0.
-
+        MCEff_alleta = 0.
+        MCEfferr_alleta = 0.
+                
         apt = ptmin + ipt*ptbinwidth
         ptarray.append( apt )
         pterrarray.append( ptbinwidth*0.5 )
@@ -97,37 +103,55 @@ def main():
             effarray.append( aEff )
             efferrarray.append( aEfferr )
 
+            MCeffarray.append( aEff/aSF )
+            aMCEfferr = math.sqrt( (aSF*aSF*aEfferr*aEfferr + aEff*aEff*aSFerr*aSFerr)/(aSF*aSF*aSF*aSF) )
+            MCefferrarray.append( aMCEfferr )
+                        
             SF_alleta += aSF
             SFerr_alleta += aSF*aSF
             Eff_alleta += aEff
             Efferr_alleta += aEfferr*aEfferr
-            
+            MCEff_alleta += aEff/aSF
+            MCEfferr_alleta += aMCEfferr*aMCEfferr
+                                  
         SF_alleta = SF_alleta/etaNbins
         SFerr_alleta = math.sqrt(SFerr_alleta)/etaNbins
         Eff_alleta = Eff_alleta/etaNbins
         Efferr_alleta = math.sqrt(Efferr_alleta)/etaNbins
-
+        MCEff_alleta = MCEff_alleta/etaNbins
+        MCEfferr_alleta = math.sqrt(MCEfferr_alleta)/etaNbins
+                
         SFarray_alleta.append( SF_alleta )
         SFerrarray_alleta.append( SFerr_alleta )
         effarray_alleta.append( Eff_alleta )
         efferrarray_alleta.append( Efferr_alleta )
-        
+        MCeffarray_alleta.append( MCEff_alleta )
+        MCefferrarray_alleta.append( MCEfferr_alleta )
+                
     histogram = {}
     histogram["SF_jet_pt"] = TGraphErrors(len(SFarray_alleta), ptarray, SFarray_alleta, pterrarray, SFerrarray_alleta)
     histogram["Eff_jet_pt"] = TGraphErrors(len(effarray_alleta), ptarray, effarray_alleta, pterrarray, efferrarray_alleta)
+    histogram["MC_Eff_jet_pt"] = TGraphErrors(len(MCeffarray_alleta), ptarray, MCeffarray_alleta, pterrarray, MCefferrarray_alleta)
     
     cv1 = TCanvas("SF_jet_pt","SF_jet_pt",700,700)
     histogram["SF_jet_pt"].Draw("ap")
     histogram["SF_jet_pt"].SetName("SF_jet_pt")
-    histogram["SF_jet_pt"].SetXTitle("Jet p_{T} [GeV/c]")
-    histogram["SF_jet_pt"].SetXTitle("SF_{b}=#epsilon^{"+OP+"}_{data}/#epsilon^{"+OP+"}_{MC}")
+    histogram["SF_jet_pt"].GetHistogram().SetYTitle("Jet p_{T} [GeV/c]")
+    histogram["SF_jet_pt"].GetHistogram().SetXTitle("SF_{b}=#epsilon^{"+OP+"}_{data}/#epsilon^{"+OP+"}_{MC}")
     
     cv2 = TCanvas("Eff_jet_pt","Eff_jet_pt",700,700)
     histogram["Eff_jet_pt"].Draw("ap")
     histogram["Eff_jet_pt"].SetName("Eff_jet_pt")
-    histogram["Eff_jet_pt"].SetXTitle("Jet p_{T} [GeV/c]")
-    histogram["Eff_jet_pt"].SetXTitle("b-tag efficiency #epsilon^{"+OP+"}_{data}")
-    
+    histogram["Eff_jet_pt"].GetHistogram().SetYTitle("Jet p_{T} [GeV/c]")
+    histogram["Eff_jet_pt"].GetHistogram().SetXTitle("b-tag efficiency #epsilon^{"+OP+"}_{data}")
+
+    cv3 = TCanvas("MC_Eff_jet_pt","MC_Eff_jet_pt",700,700)
+    histogram["MC_Eff_jet_pt"].Draw("ap")
+    histogram["MC_Eff_jet_pt"].SetName("MC_Eff_jet_pt")
+    histogram["MC_Eff_jet_pt"].GetHistogram().SetYTitle("Jet p_{T} [GeV/c]")
+    histogram["MC_Eff_jet_pt"].GetHistogram().SetXTitle("b-tag efficiency #epsilon^{"+OP+"}_{MC}")
+
+                    
     outfilename = "plotPerformanceDB.root"
     outputroot = TFile( outfilename, "RECREATE")
         
