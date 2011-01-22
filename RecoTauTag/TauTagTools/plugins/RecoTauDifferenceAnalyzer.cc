2e267b369400c27e01caa9b516fe7700d8b1edd3
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-01-22 09:38:40 +0000

    add summary at endJob

--- RecoTauTag/TauTagTools/plugins/RecoTauDifferenceAnalyzer.cc
+++ RecoTauTag/TauTagTools/plugins/RecoTauDifferenceAnalyzer.cc
@@ -12,12 +12,20 @@ class RecoTauDifferenceAnalyzer : public edm::EDAnalyzer {
     explicit RecoTauDifferenceAnalyzer(const edm::ParameterSet& pset);
     virtual ~RecoTauDifferenceAnalyzer() {}
     virtual void analyze(const edm::Event& evt, const edm::EventSetup& es);
+    virtual void endJob();
   private:
     edm::InputTag src1_;
     edm::InputTag src2_;
     edm::InputTag disc1_;
     edm::InputTag disc2_;
     double maxDeltaR_;
+    size_t eventsExamined_;
+    size_t tausExamined_;
+    size_t differences_;
+    size_t passed1_;
+    size_t passed2_;
+    size_t allPassed1_;
+    size_t allPassed2_;
 };
 
 RecoTauDifferenceAnalyzer::RecoTauDifferenceAnalyzer(
@@ -26,10 +34,28 @@ RecoTauDifferenceAnalyzer::RecoTauDifferenceAnalyzer(
   src2_ = pset.getParameter<edm::InputTag>("src2");
   disc1_ = pset.getParameter<edm::InputTag>("disc1");
   disc2_ = pset.getParameter<edm::InputTag>("disc2");
+  eventsExamined_ = 0;
+  tausExamined_ = 0;
+  differences_ = 0;
+  passed1_ = 0;
+  passed2_ = 0;
+  allPassed2_ = 0;
+  allPassed1_ = 0;
+}
+
+namespace {
+  reco::PFJetRef getJetRef(const reco::PFTau& tau) {
+    if (tau.jetRef().isNonnull())
+      return tau.jetRef();
+    else if (tau.pfTauTagInfoRef()->pfjetRef().isNonnull())
+      return tau.pfTauTagInfoRef()->pfjetRef();
+    else throw cms::Exception("cant find jet ref");
+  }
 }
 
 void RecoTauDifferenceAnalyzer::analyze(const edm::Event& evt,
                                         const edm::EventSetup& es) {
+  eventsExamined_++;
   // Get taus
   edm::Handle<reco::PFTauCollection> taus1;
   evt.getByLabel(src1_, taus1);
@@ -44,13 +70,16 @@ void RecoTauDifferenceAnalyzer::analyze(const edm::Event& evt,
 
   // Loop over first collection
   for (size_t iTau1 = 0; iTau1 < taus1->size(); ++iTau1) {
+    tausExamined_++;
     reco::PFTauRef tau1(taus1, iTau1);
     // Find the best match in the other collection
     reco::PFTauRef bestMatch;
     double bestDeltaR = -1;
     for (size_t iTau2 = 0; iTau2 < taus2->size(); ++iTau2) {
       reco::PFTauRef tau2(taus2, iTau2);
-      double deltaRVal = deltaR(tau2->p4(), tau1->p4());
+      reco::PFJetRef jet1 = getJetRef(*tau1);
+      reco::PFJetRef jet2 = getJetRef(*tau2);
+      double deltaRVal = deltaR(jet2->p4(), jet1->p4());
       if (bestMatch.isNull() || deltaRVal < bestDeltaR) {
         bestMatch = tau2;
         bestDeltaR = deltaRVal;
@@ -59,7 +88,12 @@ void RecoTauDifferenceAnalyzer::analyze(const edm::Event& evt,
     // See what's up with the discriminators
     bool result1 = ((*disc1)[tau1] > 0.5);
     bool result2 = ((*disc2)[bestMatch] > 0.5);
+    allPassed1_ += result1;
+    allPassed2_ += result2;
     if (result1 ^ result2) {
+      passed1_ += result1;
+      passed2_ += result2;
+      differences_++;
       std::cout << "********* RecoTau difference detected! *************"
           << std::endl;
       std::cout << " Tau1 InputTag: " << src1_ << " result: " << result1
@@ -77,6 +111,20 @@ void RecoTauDifferenceAnalyzer::analyze(const edm::Event& evt,
     }
   }
 }
+
+void RecoTauDifferenceAnalyzer::endJob() {
+  std::cout <<  " RECO TAU DIFFERENCE SUMMARY: " << std::endl;
+  std::cout <<  " Examined " << tausExamined_ << " taus in "
+    << eventsExamined_ << " events." << std::endl;
+  std::cout << " There were " << differences_ << " differences." << std::endl;
+  std::cout << src1_ << "," << disc1_ << " had "
+    << allPassed1_ << " total passes and "
+    << passed1_ << " exclusive passes." << std::endl;
+  std::cout << src2_ << "," << disc2_ << " had "
+    << allPassed2_ << " total passes and "
+    << passed2_ << " exclusive passes." << std::endl;
+}
+
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(RecoTauDifferenceAnalyzer);
