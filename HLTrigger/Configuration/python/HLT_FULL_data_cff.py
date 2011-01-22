1f181501f1dcd82d8c4898702d24956dd1d75611
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-01-22 17:39:26 +0000

    *** empty log message ***

--- HLTrigger/Configuration/python/HLT_FULL_data_cff.py
+++ HLTrigger/Configuration/python/HLT_FULL_data_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_3_11_0/HLT/V6 (CMSSW_3_10_0_pre6_HLT4)
+# /dev/CMSSW_3_11_0/HLT/V8 (CMSSW_3_10_0_pre6_HLT4)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_11_0/HLT/V6')
+  tableName = cms.string('/dev/CMSSW_3_11_0/HLT/V8')
 )
 
 streams = cms.PSet( 
