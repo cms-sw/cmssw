15ec27d51bbdd36e744e151e6aa6c59413d27784
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-01-22 20:42:58 +0000

    test/getOnline.sh

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -3,7 +3,6 @@
 HLT='/dev/CMSSW_3_11_0/GRun'
 L1T='L1Menu_Collisions2010_v0'
 
-
 hltGetConfiguration $HLT --process TEST --full --offline --mc   --l1 $L1T --unprescale > offline_mc.py
 hltGetConfiguration $HLT --process TEST --full --offline --data --l1 $L1T --unprescale > offline_data.py
 hltGetConfiguration $HLT --process TEST --full --online  --data --l1 $L1T --unprescale > online_data.py
