f03d3a1ebd79c5de65fd399611122f32c1b73000
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2011-01-25 10:32:40 +0000

    Fix a bug reported by Coverity.

--- DetectorDescription/Core/interface/DDValue.h
+++ DetectorDescription/Core/interface/DDValue.h
@@ -15,7 +15,6 @@ class DDValue;
 class DDSpecifics;
 class DDLSpecPar;
 
-//std::ostream & operator<< ( std::ostream &, const DDValue &);
 /** A DDValue std::maps a std::vector of DDValuePair (std::string,double) to a name. Names of DDValues are stored
  transiently. Furthermore, an ID is assigned std::mapping to the name.
  If a particular DDValue is not used anymore, use DDValue::clear() to free the internally
@@ -23,50 +22,50 @@ class DDLSpecPar;
  the DDValuePair make sense, otherwise an exception will be thrown when trying to get access
  to these values via DDValue::doubles() or DDValue::operator[].
 */
-class DDValue //: public std::vector<DDValuePair>
+class DDValue
 {
   friend class DDSpecifics;
   friend class DDLSpecPar;
 public:
   //! create a unnamed emtpy value. One can assing a named DDValue to it.
-  DDValue() : id_(0), vecPair_(0) { }
+  DDValue( void ) : id_(0), vecPair_(0) { }
   
   //! create a named empty value
-  DDValue(const std::string &);
+  DDValue( const std::string & );
 
   //! create a named empty value
-  DDValue(const char *);
+  DDValue( const char * );
  
-  void init(const std::string &);
+  void init( const std::string & );
  
   //! creates a named DDValue initialized with a std::vector of values 
-  explicit DDValue(const std::string &, const std::vector<DDValuePair>&);
+  explicit DDValue( const std::string &, const std::vector<DDValuePair>& );
   
   //! creates a single double valued named DDValue. The corresponding std::string-value is an empty std::string
-  explicit DDValue(const std::string &, double);
+  explicit DDValue( const std::string &, double );
   
   /** creates a single std::string & numerical-valued named DDValue.  */
-  explicit DDValue(const std::string &, const std::string &, double);
+  explicit DDValue( const std::string &, const std::string &, double );
   
   /** creates a single std::string-valued named DDValue */
-  explicit DDValue(const std::string & name, const std::string & val);
+  explicit DDValue( const std::string & name, const std::string & val );
   
-  explicit DDValue(unsigned int);
+  explicit DDValue( unsigned int );
   
-  ~DDValue();
+  ~DDValue( void );
   
   //! returns the ID of the DDValue
-  unsigned int id() const { return id_; }
+  unsigned int id( void ) const { return id_; }
   
   //! converts a DDValue object into its ID
-  operator unsigned int() const { return id_; }
+  operator unsigned int( void ) const { return id_; }
   
   //! the name of the DDValue
-  const std::string & name() const { return names()[id_]; }
+  const std::string & name( void ) const { return names()[id_]; }
   
   /** access to the values stored in DDValue by an index. Note, that
    the index is not checked for bounds excess! */
-  DDValuePair operator[](unsigned int i) const; 
+  DDValuePair operator[]( unsigned int i ) const; 
    
   //! a reference to the std::string-valued values stored in the given instance of DDValue
   const std::vector<std::string> & strings() const { return vecPair_->second.first; }
@@ -80,23 +79,23 @@ public:
    return vecPair_ ? vecPair_->second.first.size() : 0 ; 
   } 
   
-  static void clear();
+  static void clear( void );
   
   //! set to true, if the double-values (method DDValue::doubles()) make sense
-  void setEvalState(bool newState); 
+  void setEvalState( bool newState ); 
   
   //! true, if values are numerical evaluated; else false. 
   /** in case of a 'true' return value, the method DDValue::doubles() and the operator
      DDValue::operator[] can be used */
-  bool isEvaluated() const;
+  bool isEvaluated( void ) const;
   
   //! Two DDValues are equal only if their id() is equal AND their values are equal
   /** If the DDValue::isEvalued() == true, the numerical representation is taken for comparison,
       else the std::string representation */
-  bool operator==(const DDValue & v) const;
+  bool operator==( const DDValue & v ) const;
   
   //! A DDValue a is smaller than a DDValue b if (a.id()<b.id()) OR (a.id()==b.id() and value(a)<value(b))
-  bool operator<(const DDValue &) const;
+  bool operator<( const DDValue & ) const;
   
 private:  
   typedef std::pair<bool, std::pair<std::vector<std::string>, std::vector<double> > >vecpair_type;
