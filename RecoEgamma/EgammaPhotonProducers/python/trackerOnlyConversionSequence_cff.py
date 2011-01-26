078a232804511cf4ed5cfa1203832327c3fad783
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2011-01-26 17:04:53 +0000

    Renaming to allConversionSequence_cff after the renaming of the producer

--- RecoEgamma/EgammaPhotonProducers/python/trackerOnlyConversionSequence_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-#
-#
-# Tracker only conversion producer
-from RecoEgamma.EgammaPhotonProducers.trackerOnlyConversions_cfi import *
-trackerOnlyConversionSequence = cms.Sequence(trackerOnlyConversions)
-
-
