fe072886eb709cb827136b24997b982403e01099
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2011-01-27 20:14:50 +0000

    Change trackerOnly Conversion with allConversions

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -4,14 +4,14 @@ from RecoEgamma.EgammaElectronProducers.electronSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.photonSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.conversionSequence_cff import *
 from RecoEgamma.EgammaPhotonProducers.conversionTrackSequence_cff import *
-from RecoEgamma.EgammaPhotonProducers.trackerOnlyConversionSequence_cff import *
+from RecoEgamma.EgammaPhotonProducers.allConversionSequence_cff import *
 from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
 from RecoEgamma.EgammaIsolationAlgos.interestingEgammaIsoDetIdsSequence_cff import *
 from RecoEgamma.PhotonIdentification.photonId_cff import *
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 from RecoEgamma.EgammaHFProducers.hfEMClusteringSequence_cff import *
 
-egammarecoGlobal = cms.Sequence(conversionTrackSequence*trackerOnlyConversionSequence)
+egammarecoGlobal = cms.Sequence(conversionTrackSequence*allConversionSequence)
 egammareco = cms.Sequence(electronSequence*conversionSequence*photonSequence)
 egammarecoFull = cms.Sequence(egammareco*interestingEgammaIsoDetIds*photonIDSequence*eIdSequence*hfEMClusteringSequence)
 egammarecoWithID = cms.Sequence(egammareco*photonIDSequence*eIdSequence)
