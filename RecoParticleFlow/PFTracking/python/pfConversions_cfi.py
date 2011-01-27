5c783a79bdeb82ee7660da9814d4adec5a6c011e
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2011-01-27 20:18:37 +0000

    Replace the collection ame trackerOnlyConversions with allConversions

--- RecoParticleFlow/PFTracking/python/pfConversions_cfi.py
+++ RecoParticleFlow/PFTracking/python/pfConversions_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 #  Author: M. Gouzevitch base on N. Marinelli work
 #
 pfConversions = cms.EDProducer("PFConversionProducer", 
-    conversionCollection = cms.InputTag("trackerOnlyConversions", "")
+    conversionCollection = cms.InputTag("allConversions", "")
 )
 
 
