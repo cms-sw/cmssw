9648a0b8d7de6d9e865d8a88702ff5993a806004
Darin Acosta <sha1-91766f943a4c5bc2483d919a8bbd6cc4b04bee4f@cern.ch>
2011-02-01 00:44:34 +0000

    add offset for ME11a

--- L1Trigger/CSCTrackFinder/src/CSCSectorReceiverMiniLUT.cc
+++ L1Trigger/CSCTrackFinder/src/CSCSectorReceiverMiniLUT.cc
@@ -85,6 +85,8 @@ global_eta_data CSCSectorReceiverMiniLUT::calcGlobalEtaMEMini(unsigned short end
                       log(gbl_eta_params[endcap-1][sector-1][station-1][subsector-1][lclPhi][cscid-1][2] + WG));
           eta_min = gbl_eta_bounds[endcap-1][sector-1][station-1][subsector-1][lclPhi][cscid-1][0];
           eta_max = gbl_eta_bounds[endcap-1][sector-1][station-1][subsector-1][lclPhi][cscid-1][1];
+	  // add offset to ME11a
+	  if ( (cscid < 4)&&(lclPhi == 3) ) eta_temp += 2;  
 	}
       else
         {
