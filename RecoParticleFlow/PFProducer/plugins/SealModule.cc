c54b9c6a027a0119bfb5b04c0e48d7c2816a7dbb
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2011-02-02 18:21:35 +0000

    fixed BuildFile

--- RecoParticleFlow/PFProducer/plugins/SealModule.cc
+++ RecoParticleFlow/PFProducer/plugins/SealModule.cc
@@ -7,12 +7,10 @@
 #include "RecoParticleFlow/PFProducer/plugins/PFProducer.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFElectronTranslator.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFBlockProducer.h"
-#include "RecoParticleFlow/PFProducer/plugins/PFBlockElementSuperClusterProducer.h"
 #include "RecoParticleFlow/PFProducer/plugins/EFilter.h"
 
 
 DEFINE_FWK_MODULE(PFProducer);
 DEFINE_FWK_MODULE(PFElectronTranslator);
 DEFINE_FWK_MODULE(PFBlockProducer);
-DEFINE_FWK_MODULE(PFBlockElementSuperClusterProducer);
 DEFINE_FWK_MODULE(EFilter);
