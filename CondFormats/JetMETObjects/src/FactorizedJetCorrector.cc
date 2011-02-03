9d14675d72e2eb76b825a6812820898c58e896db
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2011-02-03 11:53:59 +0000

    Turned-off the interpolation between eta bins

--- CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc
+++ CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc
@@ -356,8 +356,8 @@ std::vector<float> FactorizedJetCorrector::getSubCorrections()
     { 
       vx = fillVector(mBinTypes[i]);
       vy = fillVector(mParTypes[i]);
-      if (mLevels[i]==kL2 || mLevels[i]==kL6)
-        mCorrectors[i]->setInterpolation(true); 
+      //if (mLevels[i]==kL2 || mLevels[i]==kL6)
+        //mCorrectors[i]->setInterpolation(true); 
       scale = mCorrectors[i]->correction(vx,vy); 	
       if (mLevels[i]==kL6 && mAddLepToJet) scale *= 1.0 + getLepPt() / mJetPt;
       factor*=scale; 
