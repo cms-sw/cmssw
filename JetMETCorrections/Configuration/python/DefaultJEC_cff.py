e233261f59d3b3b35cdb8a0a7380f274e70597a0
Konstantinos Kousouris <sha1-42e49b3afeb813a917bbe72cd14687394eae1311@cern.ch>
2011-02-03 11:57:16 +0000

    Added the L1Fastjet services

--- JetMETCorrections/Configuration/python/DefaultJEC_cff.py
+++ JetMETCorrections/Configuration/python/DefaultJEC_cff.py
@@ -28,11 +28,23 @@ kt4CaloJetsL1L2L3 = ak5CaloJetsL2L3.clone(src = 'kt4CaloJets', correctors = ['kt
 kt6CaloJetsL1L2L3 = ak5CaloJetsL2L3.clone(src = 'kt6CaloJets', correctors = ['kt6CaloL1L2L3'])
 ic5CaloJetsL1L2L3 = ak5CaloJetsL2L3.clone(src = 'iterativeCone5CaloJets', correctors = ['ic5CaloL1L2L3'])
 
+ak5CaloJetsL1FastL2L3 = ak5CaloJetsL2L3.clone(src = 'ak5CaloJets', correctors = ['ak5CaloL1FastL2L3'])
+ak7CaloJetsL1FastL2L3 = ak5CaloJetsL2L3.clone(src = 'ak7CaloJets', correctors = ['ak7CaloL1FastL2L3'])
+kt4CaloJetsL1FastL2L3 = ak5CaloJetsL2L3.clone(src = 'kt4CaloJets', correctors = ['kt4CaloL1FastL2L3'])
+kt6CaloJetsL1FastL2L3 = ak5CaloJetsL2L3.clone(src = 'kt6CaloJets', correctors = ['kt6CaloL1FastL2L3'])
+ic5CaloJetsL1FastL2L3 = ak5CaloJetsL2L3.clone(src = 'iterativeCone5CaloJets', correctors = ['ic5CaloL1FastL2L3'])
+
 ak5CaloJetsL1L2L3Residual = ak5CaloJetsL2L3.clone(src = 'ak5CaloJets', correctors = ['ak5CaloL1L2L3Residual'])
 ak7CaloJetsL1L2L3Residual = ak5CaloJetsL2L3.clone(src = 'ak7CaloJets', correctors = ['ak7CaloL1L2L3Residual'])
 kt4CaloJetsL1L2L3Residual = ak5CaloJetsL2L3.clone(src = 'kt4CaloJets', correctors = ['kt4CaloL1L2L3Residual'])
 kt6CaloJetsL1L2L3Residual = ak5CaloJetsL2L3.clone(src = 'kt6CaloJets', correctors = ['kt6CaloL1L2L3Residual'])
 ic5CaloJetsL1L2L3Residual = ak5CaloJetsL2L3.clone(src = 'iterativeCone5CaloJets', correctors = ['ic5CaloL1L2L3Residual'])
+
+ak5CaloJetsL1FastL2L3Residual = ak5CaloJetsL2L3.clone(src = 'ak5CaloJets', correctors = ['ak5CaloL1FastL2L3Residual'])
+ak7CaloJetsL1FastL2L3Residual = ak5CaloJetsL2L3.clone(src = 'ak7CaloJets', correctors = ['ak7CaloL1FastL2L3Residual'])
+kt4CaloJetsL1FastL2L3Residual = ak5CaloJetsL2L3.clone(src = 'kt4CaloJets', correctors = ['kt4CaloL1FastL2L3Residual'])
+kt6CaloJetsL1FastL2L3Residual = ak5CaloJetsL2L3.clone(src = 'kt6CaloJets', correctors = ['kt6CaloL1FastL2L3Residual'])
+ic5CaloJetsL1FastL2L3Residual = ak5CaloJetsL2L3.clone(src = 'iterativeCone5CaloJets', correctors = ['ic5CaloL1FastL2L3Residual'])
 
 ##------------------  PF JETS -----------------------------------
 ak5PFJetsL2L3   = cms.EDProducer('PFJetCorrectionProducer',
@@ -57,11 +69,23 @@ kt4PFJetsL1L2L3 = ak5PFJetsL2L3.clone(src = 'kt4PFJets', correctors = ['kt4PFL1L
 kt6PFJetsL1L2L3 = ak5PFJetsL2L3.clone(src = 'kt6PFJets', correctors = ['kt6PFL1L2L3'])
 ic5PFJetsL1L2L3 = ak5PFJetsL2L3.clone(src = 'iterativeCone5PFJets', correctors = ['ic5PFL1L2L3'])
 
+ak5PFJetsL1FastL2L3 = ak5PFJetsL2L3.clone(src = 'ak5PFJets', correctors = ['ak5PFL1FastL2L3'])
+ak7PFJetsL1FastL2L3 = ak5PFJetsL2L3.clone(src = 'ak7PFJets', correctors = ['ak7PFL1FastL2L3'])
+kt4PFJetsL1FastL2L3 = ak5PFJetsL2L3.clone(src = 'kt4PFJets', correctors = ['kt4PFL1FastL2L3'])
+kt6PFJetsL1FastL2L3 = ak5PFJetsL2L3.clone(src = 'kt6PFJets', correctors = ['kt6PFL1FastL2L3'])
+ic5PFJetsL1FastL2L3 = ak5PFJetsL2L3.clone(src = 'iterativeCone5PFJets', correctors = ['ic5PFL1FastL2L3'])
+
 ak5PFJetsL1L2L3Residual = ak5PFJetsL2L3.clone(src = 'ak5PFJets', correctors = ['ak5PFL1L2L3Residual'])
 ak7PFJetsL1L2L3Residual = ak5PFJetsL2L3.clone(src = 'ak7PFJets', correctors = ['ak7PFL1L2L3Residual'])
 kt4PFJetsL1L2L3Residual = ak5PFJetsL2L3.clone(src = 'kt4PFJets', correctors = ['kt4PFL1L2L3Residual'])
 kt6PFJetsL1L2L3Residual = ak5PFJetsL2L3.clone(src = 'kt6PFJets', correctors = ['kt6PFL1L2L3Residual'])
 ic5PFJetsL1L2L3Residual = ak5PFJetsL2L3.clone(src = 'iterativeCone5PFJets', correctors = ['ic5PFL1L2L3Residual'])
+
+ak5PFJetsL1FastL2L3Residual = ak5PFJetsL2L3.clone(src = 'ak5PFJets', correctors = ['ak5PFL1FastL2L3Residual'])
+ak7PFJetsL1FastL2L3Residual = ak5PFJetsL2L3.clone(src = 'ak7PFJets', correctors = ['ak7PFL1FastL2L3Residual'])
+kt4PFJetsL1FastL2L3Residual = ak5PFJetsL2L3.clone(src = 'kt4PFJets', correctors = ['kt4PFL1FastL2L3Residual'])
+kt6PFJetsL1FastL2L3Residual = ak5PFJetsL2L3.clone(src = 'kt6PFJets', correctors = ['kt6PFL1FastL2L3Residual'])
+ic5PFJetsL1FastL2L3Residual = ak5PFJetsL2L3.clone(src = 'iterativeCone5PFJets', correctors = ['ic5PFL1FastL2L3Residual'])
 
 ##------------------  JPT JETS ----------------------------------
 ak5JPTJetsL2L3   = cms.EDProducer('JPTJetCorrectionProducer',
@@ -70,8 +94,10 @@ ak5JPTJetsL2L3   = cms.EDProducer('JPTJetCorrectionProducer',
     )
 
 ak5JPTJetsL1L2L3 = ak5JPTJetsL2L3.clone(correctors = ['ak5JPTL1L2L3'])
+ak5JPTJetsL1FastL2L3 = ak5JPTJetsL2L3.clone(correctors = ['ak5JPTL1FastL2L3'])
 ak5JPTJetsL2L3Residual = ak5JPTJetsL2L3.clone(correctors = ['ak5JPTL2L3Residual'])
 ak5JPTJetsL1L2L3Residual = ak5JPTJetsL2L3.clone(correctors = ['ak5JPTL1L2L3Residual'])
+ak5JPTJetsL1FastL2L3Residual = ak5JPTJetsL2L3.clone(correctors = ['ak5JPTL1FastL2L3Residual'])
 
 ##------------------  TRK JETS ----------------------------------
 ak5TrackJetsL2L3   = cms.EDProducer('TrackJetCorrectionProducer',
