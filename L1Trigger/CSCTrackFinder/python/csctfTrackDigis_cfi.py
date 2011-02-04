3cad256aa712358b27d5bb782e50438c047a70cf
Justin Hugon <sha1-508125a75ede1393167c23522662573ff579fa72@cern.ch>
2011-02-04 18:34:47 +0000

    Changed to PtMethod=24

--- L1Trigger/CSCTrackFinder/python/csctfTrackDigis_cfi.py
+++ L1Trigger/CSCTrackFinder/python/csctfTrackDigis_cfi.py
@@ -18,7 +18,7 @@ csctfTrackDigis = cms.EDProducer("CSCTFTrackProducer",
 		PTLUT = cms.PSet(
 			LowQualityFlag = cms.untracked.uint32(4),
 			ReadPtLUT = cms.bool(False),
-			PtMethod = cms.untracked.uint32(21)
+			PtMethod = cms.untracked.uint32(24)
 		),
 		
 		# Operational mode control
