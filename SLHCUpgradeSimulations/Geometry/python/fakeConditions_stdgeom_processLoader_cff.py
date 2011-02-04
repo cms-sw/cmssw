a61129becb8bfcb84ced6e47ad95ed3a2081b4a9
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2011-02-04 18:53:18 +0000

    Only needed for Gen-Sim steps, removing PU

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_stdgeom_processLoader_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_stdgeom_processLoader_cff.py
@@ -2,7 +2,6 @@
 import FWCore.ParameterSet.Config as cms
 
 def customise(process):
-     process.load('SLHCUpgradeSimulations.Geometry.mixLowLumPU_stdgeom_cff')
      process.load("SLHCUpgradeSimulations.Geometry.fakeConditions_stdgeom_cff")
      return (process)
 
