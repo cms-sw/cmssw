3009032fdbbb419c5fb665a31ef11e78a8d94a7b
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2011-02-07 14:31:09 +0000

    fix for cosmic TP and SP skims

--- DPGAnalysis/Skims/python/Skims_DPG_cff.py
+++ DPGAnalysis/Skims/python/Skims_DPG_cff.py
@@ -150,11 +150,29 @@ SKIMStreamHSCP = cms.FilteredStream(
 #####################
 
 from DPGAnalysis.Skims.cosmicSPSkim_cff import *
-cosmicSPSkimPath = cms.Path( cosmicSPSkim )
+
+cosmicMuonsBarrelOnlyPath = cms.Path(cosmicMuonsBarrelOnlySequence)
+cosmicMuonsPath = cms.Path(cosmicMuonsSequence)
+cosmicMuons1LegPath = cms.Path(cosmicMuons1LegSequence)
+globalCosmicMuonsBarrelOnlyPath = cms.Path(globalCosmicMuonsBarrelOnlySequence)
+cosmictrackfinderP5Path = cms.Path(cosmictrackfinderP5Sequence)
+globalCosmicMuonsPath = cms.Path(globalCosmicMuonsSequence)
+rsWithMaterialTracksP5Path = cms.Path(rsWithMaterialTracksP5Sequence)
+globalCosmicMuons1LegPath = cms.Path(globalCosmicMuons1LegSequence)
+rsWithMaterialTracksP5Path = cms.Path(rsWithMaterialTracksP5Sequence) 
+
 SKIMStreamCosmicSP = cms.FilteredStream(
         responsible = '',
         name = 'CosmicSP',
-        paths = (cosmicSPSkimPath),
+        paths = (cosmicMuonsBarrelOnlyPath,
+                 cosmicMuonsPath,
+                 cosmicMuons1LegPath,
+                 globalCosmicMuonsBarrelOnlyPath,
+                 cosmictrackfinderP5Path,
+                 globalCosmicMuonsPath,
+                 rsWithMaterialTracksP5Path,
+                 globalCosmicMuons1LegPath,
+                 rsWithMaterialTracksP5Path),
         content = skimContent.outputCommands,
         selectEvents = cms.untracked.PSet(),
         dataTier = cms.untracked.string('RAW-RECO')
@@ -163,11 +181,33 @@ SKIMStreamCosmicSP = cms.FilteredStream(
 #####################
 
 from DPGAnalysis.Skims.cosmicTPSkim_cff import *
-cosmicTPSkimPath = cms.Path( cosmicTPSkim )
+
+cosmicMuonsBarrelOnlyTkPath = cms.Path(cosmicMuonsEndCapsOnlyTkSequence)
+cosmicMuonsEndCapsOnlyTkPath = cms.Path(cosmicMuonsTkSequence)
+cosmicMuonsTkPath = cms.Path(cosmicMuons1LegTkSequence)
+cosmicMuons1LegTkPath = cms.Path(cosmicMuons1LegTkSequence)
+globalCosmicMuonsBarrelOnlyTkPath = cms.Path(globalCosmicMuonsBarrelOnlyTkSequence)
+globalCosmicMuonsEndCapsOnlyTkPath = cms.Path(globalCosmicMuonsEndCapsOnlyTkSequence)
+globalCosmicMuonsTkPath = cms.Path(globalCosmicMuonsTkSequence)
+globalCosmicMuons1LegTkPath = cms.Path(globalCosmicMuons1LegTkSequence)
+cosmictrackfinderP5TkCntPath = cms.Path(cosmictrackfinderP5TkCntSequence)
+ctfWithMaterialTracksP5TkCntPath = cms.Path(ctfWithMaterialTracksP5TkCntSequence)
+rsWithMaterialTracksP5TkCntPath = cms.Path(rsWithMaterialTracksP5TkCntSequence)
+
 SKIMStreamCosmicTP = cms.FilteredStream(
         responsible = '',
         name = 'CosmicTP',
-        paths = (cosmicTPSkimPath),
+        paths = (cosmicMuonsBarrelOnlyTkPath, 
+                 cosmicMuonsEndCapsOnlyTkPath, 
+                 cosmicMuonsTkPath,
+                 cosmicMuons1LegTkPath,
+                 globalCosmicMuonsBarrelOnlyTkPath,
+                 globalCosmicMuonsEndCapsOnlyTkPath,
+                 globalCosmicMuonsTkPath,
+                 globalCosmicMuons1LegTkPath,
+                 cosmictrackfinderP5TkCntPath,
+                 ctfWithMaterialTracksP5TkCntPath,
+                 rsWithMaterialTracksP5TkCntPath),
         content = skimContent.outputCommands,
         selectEvents = cms.untracked.PSet(),
         dataTier = cms.untracked.string('RAW-RECO')
