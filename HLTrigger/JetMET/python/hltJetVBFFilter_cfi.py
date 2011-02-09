316776e300ceb2ca05ecf43e2fb8228e7974d889
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-02-09 06:38:58 +0000

    Remove recently added *CleandedVBF* filter in favor of modified *VBF* filter.

--- HLTrigger/JetMET/python/hltJetVBFFilter_cfi.py
+++ HLTrigger/JetMET/python/hltJetVBFFilter_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 hltJetVBFFilter = cms.EDFilter("HLTJetVBFFilter",
-    minDeltaEta = cms.double(4.2),
-    minEt = cms.double(40.0),
+    minDeltaEta = cms.double(2.0),
+    minEtLow = cms.double(20.0),
+    minEtHigh = cms.double(20.0),                               
     saveTag = cms.untracked.bool( False ),
     inputTag = cms.InputTag("iterativeCone5CaloJets")
 )
