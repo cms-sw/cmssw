672027c2e65da2f03feb5b92b368138c7916c298
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-02-10 21:53:13 +0000

    Update GTs for 42X

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
 
-autoCond = { 'mc'      : 'MC_311_V1::All',
-             'startup' : 'START311_V1::All',
+autoCond = { 'mc'      : 'MC_42_V0::All',
+             'startup' : 'START42_V0::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
              'com10'   : 'GR_R_311_V1::All',
