9f34058c5a9f9cb7ce2386465c9a82294cea5c6d
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2011-02-10 19:41:04 +0000

    define common RelValTTBar input file centrally for all test cfgs

--- TopQuarkAnalysis/TopJetCombination/test/ttSemiLepJetCombMVATrainTreeSaver_cfg.py
+++ TopQuarkAnalysis/TopJetCombination/test/ttSemiLepJetCombMVATrainTreeSaver_cfg.py
@@ -11,15 +11,16 @@ process.MessageLogger.cerr.TtSemiLepJetCombMVATrainer = cms.untracked.PSet(
 )
 
 ## define input
+from TopQuarkAnalysis.TopEventProducers.tqafInputFiles_cff import relValTTbar
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_3_8_2/RelValTTbar/GEN-SIM-RECO/MC_38Y_V9-v1/0018/E8B5D618-96AF-DF11-835A-003048679070.root'
-    )
+    fileNames = cms.untracked.vstring(relValTTbar)
 )
+
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
+
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(False)
