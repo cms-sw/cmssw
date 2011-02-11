cdf471f4d7b529038399a6a33aff61e24d13de47
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2011-02-11 17:03:42 +0000

    fix the customize for new HCAL configs

--- RecoParticleFlow/Configuration/python/pFlowHCALCalibration.py
+++ RecoParticleFlow/Configuration/python/pFlowHCALCalibration.py
@@ -1,11 +1,11 @@
 
 def pFlowHCALCalibration(process):
-    if hasattr(process,'hcalSimParameters'):
-        process.hcalSimParameters.hb.samplingFactors = cms.vdouble(125.44, 125.54, 125.32, 125.13, 124.46,
+    if hasattr(process,'simHcalUnsuppressedDigis'):
+        process.simHcalUnsuppressedDigis.hb.samplingFactors = cms.vdouble(125.44, 125.54, 125.32, 125.13, 124.46,
                                                   125.01, 125.22, 125.48, 124.45, 125.9,
                                                   125.83, 127.01, 126.82, 129.73, 131.83,
                                                   143.52)
-        process.hcalSimParameters.he.samplingFactors = cms.vdouble(210.55, 197.93, 186.12, 189.64, 189.63,
+        process.simHcalUnsuppressedDigis.he.samplingFactors = cms.vdouble(210.55, 197.93, 186.12, 189.64, 189.63,
                                                                    190.28, 189.61, 189.6, 190.12, 191.22,
                                                                    190.9, 193.06, 188.42, 188.42)
         
@@ -14,3 +14,4 @@ def pFlowHCALCalibration(process):
         process.particleFlowRecHitHCAL.HCAL_Calib = False
 
     return (process)
+
