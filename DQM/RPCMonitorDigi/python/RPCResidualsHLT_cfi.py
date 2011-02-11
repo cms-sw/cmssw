a8b08a8af0b4f4332d41456dbdc41af06abdf09a
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2011-02-11 16:21:55 +0000

    fix for HLT

--- DQM/RPCMonitorDigi/python/RPCResidualsHLT_cfi.py
+++ DQM/RPCMonitorDigi/python/RPCResidualsHLT_cfi.py
@@ -17,12 +17,15 @@ rpcEfficiencyHLT = cms.EDAnalyzer("RPCEfficiency",
     MaxD = cms.untracked.double(80.0),
     MaxDrb4 = cms.untracked.double(150.0),
 
+
 #    cscSegments = cms.untracked.string('cscSegments'),
 #    dt4DSegments = cms.untracked.string('dt4DSegments'),
 
     cscSegments = cms.untracked.string('hltCscSegments'),
     dt4DSegments = cms.untracked.string('hltDt4DSegments'),
     RecHitLabel = cms.InputTag("hltRpcRecHits"),
+
+
 
     folderPath = cms.untracked.string('HLT/HLTMonMuon/RPC/'),
 
