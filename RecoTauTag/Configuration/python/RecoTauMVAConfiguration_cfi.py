9f1b71cfdab9a3524cc72b4c21e80fce10df35a1
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-02-14 12:58:10 +0000

    Add non-MVA running, global tag version of MVA dependency

--- RecoTauTag/Configuration/python/RecoTauMVAConfiguration_cfi.py
+++ RecoTauTag/Configuration/python/RecoTauMVAConfiguration_cfi.py
@@ -1,14 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 # Flag to disable/enable MVA dependent tau sequences
-recoTauEnableMVA = True
+recoTauEnableMVA = False
 
 # Temporary code to load database if CMSSW version < 3_11
 # In 3_11 this has entered the global tag.
-from RecoTauTag.TauTagTools.TancConditions_cff import TauTagMVAComputerRecord
-TauTagMVAComputerRecord.connect = cms.string(
-   'sqlite_fip:RecoTauTag/RecoTau/data/hpstanc.db'
-)
-TauTagMVAComputerRecord.toGet[0].tag = cms.string('Tanc')
-# Don't conflict with TaNC global tag
-TauTagMVAComputerRecord.appendToDataLabel = cms.string('hpstanc')
+TauTagMVAComputerRecord = None
+#from RecoTauTag.TauTagTools.TancConditions_cff import TauTagMVAComputerRecord
+#TauTagMVAComputerRecord.connect = cms.string(
+   #'sqlite_fip:RecoTauTag/RecoTau/data/hpstanc.db'
+#)
+#TauTagMVAComputerRecord.toGet[0].tag = cms.string('Tanc')
+## Don't conflict with TaNC global tag
+#TauTagMVAComputerRecord.appendToDataLabel = cms.string('hpstanc')
