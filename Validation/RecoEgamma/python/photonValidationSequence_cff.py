c89a3a96711ae80e558c8f70e50210f79e4499b0
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2011-02-15 20:46:53 +0000

    remove two silly lines

--- Validation/RecoEgamma/python/photonValidationSequence_cff.py
+++ Validation/RecoEgamma/python/photonValidationSequence_cff.py
@@ -14,8 +14,6 @@ TrackAssociatorByHits.Quality_SimToReco = 0.5
 photonValidation.minPhoEtCut = 10
 photonValidation.eMax  = 500
 photonValidation.etMax = 250
-eMax = photonValidation.etMax
-etMax = photonValidation.etMax 
 ## same for all
 photonValidation.convTrackMinPtCut = 1.
 photonValidation.useTP = True
