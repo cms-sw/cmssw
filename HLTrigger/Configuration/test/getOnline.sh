7d0c65d05cd010661397d99e908b0fe38abad2c4
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-02-15 12:34:15 +0000

    Fix for new syntax

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 HLT='/dev/CMSSW_3_11_1/GRun'
-L1T='L1Menu_Collisions2011_v0a'
+L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v0a_mc'
 
 hltGetConfiguration $HLT --process TEST --full --offline --mc   --l1 $L1T --unprescale > offline_mc.py
 hltGetConfiguration $HLT --process TEST --full --offline --data --l1 $L1T --unprescale > offline_data.py
