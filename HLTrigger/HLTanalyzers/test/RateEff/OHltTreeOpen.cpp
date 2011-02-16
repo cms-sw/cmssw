cb1438db7bff8f0736f1e6bfee6ee2d9b59b7b48
Arnaud Gay <sha1-ffc0f44b10535426b6e80e730966fc0636a397a8@cern.ch>
2011-02-16 15:57:49 +0000

    Added OpenHLT_Ele10_CaloIdL_TrkIdVL_CaloIsoVL_TrkIsoVL_HT220_v1

--- HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
+++ HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
@@ -5316,8 +5316,6 @@ void OHltTree::CheckOpenHlt(
    //to be removed
    else if (menu->GetTriggerName(it).CompareTo("OpenHLT_Ele10_longname_HT140U") == 0)
    {
-      //Good one? ...HT220U?
-      //  else if (menu->GetTriggerName(it).CompareTo("OpenHLT_Ele10_CaloIdL_TrkIdVL_CaloIsoVL_TrkIsoVL_HT140U") == 0) {  
       if (map_L1BitOfStandardHLTPath.find(menu->GetTriggerName(it))->second==1)
       {
          if ( (OpenHlt1ElectronSamHarperPassed(10., 0, // ET, L1isolation 
@@ -5340,6 +5338,39 @@ void OHltTree::CheckOpenHlt(
                0.15,
                0.10 // Dphi barrel, Dphi endcap 
          )>=1) && (OpenHltSumHTPassed(140, 20, 3)>=1))
+         {
+            if (prescaleResponse(menu, cfg, rcounter, it))
+            {
+               triggerBit[it] = true;
+            }
+         }
+      }
+   }
+
+   else if (menu->GetTriggerName(it).CompareTo("OpenHLT_Ele10_CaloIdL_TrkIdVL_CaloIsoVL_TrkIsoVL_HT220_v1") == 0)
+     {
+       if (map_L1BitOfStandardHLTPath.find(menu->GetTriggerName(it))->second==1)
+	 {
+	   if ( (OpenHlt1ElectronSamHarperPassed(10., 0, // ET, L1isolation 
+               999.,
+               999., // Track iso barrel, Track iso endcap 
+               0.2,
+               0.2, // Track/pT iso barrel, Track/pT iso endcap 
+               0.2,
+               0.2, // H/ET iso barrel, H/ET iso endcap 
+               0.2,
+               0.2, // E/ET iso barrel, E/ET iso endcap 
+               0.15,
+               0.10, // H/E barrel, H/E endcap 
+               0.014,
+               0.035, // cluster shape barrel, cluster shape endcap 
+               0.98,
+               1.0, // R9 barrel, R9 endcap 
+               0.01,
+               0.01, // Deta barrel, Deta endcap 
+               0.15,
+               0.10 // Dphi barrel, Dphi endcap 
+         )>=1) && (OpenHltSumCorHTPassed(220, 30)>=1))
          {
             if (prescaleResponse(menu, cfg, rcounter, it))
             {
