34ccc508cbc4ca03838fb16ec5a20c54c464085e
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-02-16 16:12:06 +0000

    whitespace

--- RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByFlightPathSignificance.cc
+++ RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByFlightPathSignificance.cc
@@ -21,92 +21,95 @@ using namespace reco;
 using namespace std;
 using namespace edm;
 
-class PFRecoTauDiscriminationByFlightPathSignificance : public PFTauDiscriminationProducerBase  {
-    public:
-	explicit PFRecoTauDiscriminationByFlightPathSignificance(const ParameterSet& iConfig):PFTauDiscriminationProducerBase(iConfig),
-                                                                               qualityCuts_(iConfig.getParameter<ParameterSet>("qualityCuts")){  // retrieve quality cuts
-		flightPathSig		= iConfig.getParameter<double>("flightPathSig");
-		withPVError		= iConfig.getParameter<bool>("UsePVerror");
+class PFRecoTauDiscriminationByFlightPathSignificance
+: public PFTauDiscriminationProducerBase  {
+  public:
+    explicit PFRecoTauDiscriminationByFlightPathSignificance(const ParameterSet& iConfig):PFTauDiscriminationProducerBase(iConfig),
+    qualityCuts_(iConfig.getParameter<ParameterSet>("qualityCuts")){  // retrieve quality cuts
+      flightPathSig		= iConfig.getParameter<double>("flightPathSig");
+      withPVError		= iConfig.getParameter<bool>("UsePVerror");
 
-		PVProducer		= iConfig.getParameter<edm::InputTag>("PVProducer");
+      PVProducer		= iConfig.getParameter<edm::InputTag>("PVProducer");
 
-		booleanOutput 		= iConfig.getParameter<bool>("BooleanOutput");
-	}
+      booleanOutput 		= iConfig.getParameter<bool>("BooleanOutput");
+    }
 
-      	~PFRecoTauDiscriminationByFlightPathSignificance(){}
+    ~PFRecoTauDiscriminationByFlightPathSignificance(){}
 
-	void beginEvent(const edm::Event&, const edm::EventSetup&);
-	double discriminate(const reco::PFTauRef&);
+    void beginEvent(const edm::Event&, const edm::EventSetup&);
+    double discriminate(const reco::PFTauRef&);
 
-    private:
-	double threeProngFlightPathSig(const PFTauRef&);
-	double vertexSignificance(reco::Vertex&,reco::Vertex&,GlobalVector&);
+  private:
+    double threeProngFlightPathSig(const PFTauRef&);
+    double vertexSignificance(reco::Vertex&,reco::Vertex&,GlobalVector&);
 
-	PFTauQualityCutWrapper qualityCuts_;
+    PFTauQualityCutWrapper qualityCuts_;
 
-	double flightPathSig;
-	bool withPVError;
+    double flightPathSig;
+    bool withPVError;
 
-	reco::Vertex primaryVertex;
-	const TransientTrackBuilder* transientTrackBuilder;
-	edm::InputTag PVProducer;
+    reco::Vertex primaryVertex;
+    const TransientTrackBuilder* transientTrackBuilder;
+    edm::InputTag PVProducer;
 
-	bool booleanOutput;
+    bool booleanOutput;
 };
 
 void PFRecoTauDiscriminationByFlightPathSignificance::beginEvent(const Event& iEvent, const EventSetup& iSetup){
 
-//Primary vertex
-	edm::Handle<edm::View<reco::Vertex> > vertexHandle;
-	iEvent.getByLabel(PVProducer, vertexHandle);
-        const edm::View<reco::Vertex>& vertexCollection(*vertexHandle);
+  //Primary vertex
+  edm::Handle<edm::View<reco::Vertex> > vertexHandle;
+  iEvent.getByLabel(PVProducer, vertexHandle);
+  const edm::View<reco::Vertex>& vertexCollection(*vertexHandle);
 
-        primaryVertex = *(vertexCollection.begin());
+  primaryVertex = *(vertexCollection.begin());
 
-	// Transient Tracks
-	edm::ESHandle<TransientTrackBuilder> builder;
-        iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",builder);
-        transientTrackBuilder = builder.product();
+  // Transient Tracks
+  edm::ESHandle<TransientTrackBuilder> builder;
+  iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",builder);
+  transientTrackBuilder = builder.product();
 
 }
 
 double PFRecoTauDiscriminationByFlightPathSignificance::discriminate(const PFTauRef& tau){
 
-	if(booleanOutput) return ( threeProngFlightPathSig(tau) > flightPathSig ? 1. : 0. );
-	return threeProngFlightPathSig(tau);
+  if(booleanOutput) return ( threeProngFlightPathSig(tau) > flightPathSig ? 1. : 0. );
+  return threeProngFlightPathSig(tau);
 }
 
-double PFRecoTauDiscriminationByFlightPathSignificance::threeProngFlightPathSig(const PFTauRef& tau){
-	double flightPathSignificance = 0;
+double PFRecoTauDiscriminationByFlightPathSignificance::threeProngFlightPathSig(
+    const PFTauRef& tau){
+  double flightPathSignificance = 0;
 
-//Secondary vertex
-	const PFCandidateRefVector pfSignalCandidates = tau->signalPFChargedHadrCands();
-	vector<TransientTrack> transientTracks;
-	RefVector<PFCandidateCollection>::const_iterator iTrack;
-        for(iTrack = pfSignalCandidates.begin(); iTrack!= pfSignalCandidates.end(); iTrack++){
-		const PFCandidate& pfCand = *(iTrack->get());
-		if(pfCand.trackRef().isNonnull()){
-                  const TransientTrack transientTrack = transientTrackBuilder->build(pfCand.trackRef());
-                  transientTracks.push_back(transientTrack);
-                }
-	}
-        if(transientTracks.size() > 1){
-                KalmanVertexFitter kvf(true);
-                TransientVertex tv = kvf.vertex(transientTracks);
+  //Secondary vertex
+  const PFCandidateRefVector pfSignalCandidates = tau->signalPFChargedHadrCands();
+  vector<TransientTrack> transientTracks;
+  RefVector<PFCandidateCollection>::const_iterator iTrack;
+  for(iTrack = pfSignalCandidates.begin(); iTrack!= pfSignalCandidates.end(); iTrack++){
+    const PFCandidate& pfCand = *(iTrack->get());
+    if(pfCand.trackRef().isNonnull()){
+      const TransientTrack transientTrack = transientTrackBuilder->build(pfCand.trackRef());
+      transientTracks.push_back(transientTrack);
+    }
+  }
+  if(transientTracks.size() > 1){
+    KalmanVertexFitter kvf(true);
+    TransientVertex tv = kvf.vertex(transientTracks);
 
-                if(tv.isValid()){
-			GlobalVector tauDir(tau->px(),
-                                            tau->py(),
-                                            tau->pz());
-			Vertex secVer = tv;
-			flightPathSignificance = vertexSignificance(primaryVertex,secVer,tauDir);
-                }
-        }
-	return flightPathSignificance;
+    if(tv.isValid()){
+      GlobalVector tauDir(tau->px(),
+          tau->py(),
+          tau->pz());
+      Vertex secVer = tv;
+      flightPathSignificance = vertexSignificance(primaryVertex,secVer,tauDir);
+    }
+  }
+  return flightPathSignificance;
 }
 
-double PFRecoTauDiscriminationByFlightPathSignificance::vertexSignificance(reco::Vertex& pv, Vertex& sv,GlobalVector& direction){
-	return SecondaryVertex::computeDist3d(pv,sv,direction,withPVError).significance();
+double PFRecoTauDiscriminationByFlightPathSignificance::vertexSignificance(
+    reco::Vertex& pv, Vertex& sv,GlobalVector& direction){
+  return SecondaryVertex::computeDist3d(pv,sv,direction,withPVError).significance();
 }
 
 DEFINE_FWK_MODULE(PFRecoTauDiscriminationByFlightPathSignificance);
