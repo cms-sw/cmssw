12f90c8a610ca5da1a25a68dc0927502767849ab
Arnaud Gay <sha1-ffc0f44b10535426b6e80e730966fc0636a397a8@cern.ch>
2011-02-17 10:43:51 +0000

    Added OpenHLT_Mu3_Ele8_CaloIdT_TrkIdVL_HT160_v1 and fixed the latest addition :)

--- HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
+++ HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
@@ -6413,7 +6413,7 @@ void OHltTree::CheckOpenHlt(
          {
             if (OpenHlt1MuonPassed(3., 3., 3., 2., 0)>=1 && OpenHltSumCorHTPassed(
                   100,
-                  30,)>0 && OpenHlt1ElectronSamHarperPassed(8., 0, // ET, L1isolation
+                  30)>0 && OpenHlt1ElectronSamHarperPassed(8., 0, // ET, L1isolation
                   999.,
                   999., // Track iso barrel, Track iso endcap
                   999.,
@@ -6426,6 +6426,41 @@ void OHltTree::CheckOpenHlt(
                   0.10, // H/E barrel, H/E endcap
                   0.014,
                   0.035, // cluster shape barrel, cluster shape endcap
+                  0.98,
+                  1.0, // R9 barrel, R9 endcap
+                  0.01,
+                  0.01, // Deta barrel, Deta endcap
+                  0.15,
+                  0.10 // Dphi barrel, Dphi endcap
+                  )>=1)
+            {
+               triggerBit[it] = true;
+            }
+         }
+      }
+   }
+
+   else if (menu->GetTriggerName(it).CompareTo("OpenHLT_Mu3_Ele8_CaloIdT_TrkIdVL_HT160_v1") == 0)
+   {
+      if (map_L1BitOfStandardHLTPath.find(menu->GetTriggerName(it))->second==1)
+      {
+         if (prescaleResponse(menu, cfg, rcounter, it))
+         {
+            if (OpenHlt1MuonPassed(3., 3., 3., 2., 0)>=1 && OpenHltSumCorHTPassed(
+                  100,
+                  30)>0 && OpenHlt1ElectronSamHarperPassed(8., 0, // ET, L1isolation
+                  999.,
+                  999., // Track iso barrel, Track iso endcap
+                  999.,
+                  999., // Track/pT iso barrel, Track/pT iso endcap
+                  999.,
+                  999., // H/ET iso barrel, H/ET iso endcap
+                  999.,
+                  999., // E/ET iso barrel, E/ET iso endcap
+                  0.10,
+                  0.075, // H/E barrel, H/E endcap 
+                  0.011,
+                  0.031, // cluster shape barrel, cluster shape endcap 
                   0.98,
                   1.0, // R9 barrel, R9 endcap
                   0.01,
