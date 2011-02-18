cbc6c10004661004d33b3c02a72d6f6ef34e8fd9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-02-18 21:43:46 +0000

    Must now pop services from the ParameterSet

--- FWCore/ParameterSet/interface/ProcessDesc.h
+++ FWCore/ParameterSet/interface/ProcessDesc.h
@@ -1,31 +1,26 @@
-#ifndef ParameterSet_ProcessDesc_h
-#define ParameterSet_ProcessDesc_h
+#ifndef FWCore_ParameterSet_ProcessDesc_h
+#define FWCore_ParameterSet_ProcessDesc_h
 
 #include "boost/shared_ptr.hpp"
 #include <string>
 #include <vector>
 
 namespace edm {
-  
+
   class ParameterSet;
 
   class ProcessDesc {
 
   public:
-    explicit ProcessDesc(ParameterSet const& pset);
+    explicit ProcessDesc(ParameterSet& pset);
 
     /// construct from the configuration language string
     explicit ProcessDesc(std::string const& config);
 
     ~ProcessDesc();
 
-    /// get the ParameterSet that describes the process
+    /// get the parameter set
     boost::shared_ptr<ParameterSet> getProcessPSet() const;
-
-    /// get the dependencies for this module
-    /** the return string is a list of comma-separated
-      * names of the modules on which modulename depends*/
-    std::string  getDependencies(std::string const& modulename);
 
     /// get the descriptions of the services
     boost::shared_ptr<std::vector<ParameterSet> > getServicesPSets() const;
@@ -35,14 +30,14 @@ namespace edm {
     void addService(std::string const& service);
     /// add a service if it's not already there
     void addDefaultService(std::string const& service);
-    /// add some defaults services, and some forced
+    /// add a service and replace it if it's already there
+    void addForcedService(std::string const& service);
+    /// add some default services and forced services
     void addServices(std::vector<std::string> const& defaultServices,
-                     std::vector<std::string> const& forcedServices);
+                     std::vector<std::string> const& forcedServices = std::vector<std::string>());
 
     std::string dump() const;
   private:
-
-    //Path and sequence information
     boost::shared_ptr<ParameterSet> pset_;
     boost::shared_ptr<std::vector<ParameterSet> > services_;
   };
--- FWCore/ParameterSet/src/ProcessDesc.cc
+++ FWCore/ParameterSet/src/ProcessDesc.cc
@@ -1,97 +1,86 @@
-/**
-   \file
-   Implementation of calss ProcessDesc
 
-   \author Stefano ARGIRO
-   \date 17 Jun 2005
-*/
-
-
-#include "FWCore/ParameterSet/interface/ProcessDesc.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ProcessDesc.h"
 #include "FWCore/Utilities/interface/EDMException.h"
-#include <iostream>
 
-namespace edm
-{
+namespace edm {
+  ProcessDesc::ProcessDesc(ParameterSet& pset) :
+      pset_(new ParameterSet(pset)), services_(new std::vector<ParameterSet>) {
+    std::auto_ptr<ParameterSet> services = pset.popParameterSet(std::string("services"));
+    std::vector<std::string> serviceNames;
+    services->getParameterSetNames(serviceNames);
+    for(std::vector<std::string>::const_iterator it = serviceNames.begin(), itEnd = serviceNames.end();
+        it != itEnd; ++it) {
+      services_->push_back(services->getUntrackedParameter<ParameterSet>(*it));
+    }
+  }
 
-  ProcessDesc::ProcessDesc(ParameterSet const& pset)
-  : pset_(new ParameterSet(pset)), services_(new std::vector<ParameterSet>())
-  {
+  ProcessDesc::ProcessDesc(std::string const& config) :
+      pset_(new ParameterSet),
+      services_(new std::vector<ParameterSet>()) {
+    throw Exception(errors::Configuration,"Old config strings no longer accepted");
   }
 
   ProcessDesc::~ProcessDesc() {
   }
 
-  ProcessDesc::ProcessDesc(std::string const& config)
-  : pset_(new ParameterSet),
-    services_(new std::vector<ParameterSet>()) {
-    throw edm::Exception(errors::Configuration,"Old config strings no longer accepted");
-  }
-
-  boost::shared_ptr<edm::ParameterSet>  
+  boost::shared_ptr<ParameterSet>
   ProcessDesc::getProcessPSet() const {
     return pset_;
   }
 
-  boost::shared_ptr<std::vector<ParameterSet> > 
+  boost::shared_ptr<std::vector<ParameterSet> >
   ProcessDesc::getServicesPSets() const {
     return services_;
   }
 
-  
   void ProcessDesc::addService(ParameterSet& pset) {
     services_->push_back(pset);
   }
 
-
   void ProcessDesc::addService(std::string const& service) {
     ParameterSet newpset;
-    newpset.addParameter<std::string>("@service_type",service);
+    newpset.addParameter<std::string>("@service_type", service);
     addService(newpset);
   }
 
   void ProcessDesc::addDefaultService(std::string const& service) {
     typedef std::vector<edm::ParameterSet>::iterator Iter;
     for(Iter it = services_->begin(), itEnd = services_->end(); it != itEnd; ++it) {
-        std::string name = it->getParameter<std::string>("@service_type");
-
-        if (name == service) {
-          // If the service is already there move it to the end so
-          // it will be created before all the others already there
-          // This means we use the order from the default services list
-          // and the parameters from the configuration file
-          while (true) {
-            Iter iterNext = it + 1;
-            if (iterNext == itEnd) return;
-            iter_swap(it, iterNext);
-            ++it;
-          }
-        }
+      std::string name = it->getParameter<std::string>("@service_type");
+      if (name == service) {
+        // Use the configured service.  Don't add a default.
+        return;
+      }
     }
     addService(service);
   }
 
+  void ProcessDesc::addForcedService(std::string const& service) {
+    typedef std::vector<edm::ParameterSet>::iterator Iter;
+    for(Iter it = services_->begin(), itEnd = services_->end(); it != itEnd; ++it) {
+      std::string name = it->getParameter<std::string>("@service_type");
+      if (name == service) {
+        // Remove the configured service before adding the default.
+        services_->erase(it);
+        break;
+      }
+    }
+    addService(service);
+  }
 
   void ProcessDesc::addServices(std::vector<std::string> const& defaultServices,
                                 std::vector<std::string> const& forcedServices) {
-    // Add the forced and default services to services_.
-    // In services_, we want the default services first, then the forced
-    // services, then the services from the configuration.  It is efficient
-    // and convenient to add them in reverse order.  Then after we are done
-    // adding, we reverse the std::vector again to get the desired order.
-    std::reverse(services_->begin(), services_->end());
-    for(std::vector<std::string>::const_reverse_iterator j = forcedServices.rbegin(),
-                                            jEnd = forcedServices.rend();
-         j != jEnd; ++j) {
-      addService(*j);
+    // Add the forced services to services_.
+    for(std::vector<std::string>::const_iterator i = forcedServices.begin(), iEnd = forcedServices.end();
+         i != iEnd; ++i) {
+      addForcedService(*i);
     }
-    for(std::vector<std::string>::const_reverse_iterator i = defaultServices.rbegin(),
-                                            iEnd = defaultServices.rend();
+    // Add the default services to services_.
+    for(std::vector<std::string>::const_iterator i = defaultServices.begin(), iEnd = defaultServices.end();
          i != iEnd; ++i) {
       addDefaultService(*i);
     }
-    std::reverse(services_->begin(), services_->end());
   }
 
   std::string ProcessDesc::dump() const {
@@ -101,6 +90,4 @@ namespace edm
     }
     return out;
   }
-
-
 } // namespace edm
