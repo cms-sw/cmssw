603e65eced7232ce56db23234e90e4b4bda04f91
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2011-02-18 09:41:38 +0000

    optimised the evaluation of quality test for S/N

--- DQM/SiStripMonitorClient/src/SiStripQualityChecker.cc
+++ DQM/SiStripMonitorClient/src/SiStripQualityChecker.cc
@@ -380,20 +380,29 @@ void SiStripQualityChecker::fillSubDetStatus(DQMStore* dqm_store,
 	 it != meVec.end(); it++) {
       MonitorElement * me = (*it);
       if (!me) continue;
-      if (me->getQReports().size() == 0) continue;
+      std::vector<QReport *> reports = me->getQReports();
+
+      if (reports.size() == 0) continue;
       std::string name = me->getName();
       
       if( name.find("Summary_ClusterStoNCorr__OnTrack") != std::string::npos){
 	int istat =  SiStripUtility::getMEStatus((*it)); 
-	if (me->getEntries() > 100 && istat == dqm::qstatus::ERROR) ston_stat = 0;
+        if (reports[0]->getQTresult() == -1) {
+	  std::cout << name << " : entries " << me->getEntries() << std::endl;
+	  ston_stat =-1;
+        } else {
+          if (istat == dqm::qstatus::ERROR) ston_stat = 0;
+          else if (istat == dqm::qstatus::STATUS_OK) ston_stat = 1;
+        }
+	//	if (me->getEntries() > 100 && istat == dqm::qstatus::ERROR) ston_stat = 0;
       }
     }
     if (ndet > 0) {
       float eff_fac = 1 - (errdet*1.0/ndet);
       fillStatusHistogram(SToNReportMap,        xbin, ybin, ston_stat);
       fillStatusHistogram(DetFractionReportMap, xbin, ybin, eff_fac);
-      if (ston_stat > 0) fillStatusHistogram(SummaryReportMap, xbin, ybin, eff_fac);
-      else               fillStatusHistogram(SummaryReportMap, xbin, ybin, 0.0);
+      if (ston_stat < 0) fillStatusHistogram(SummaryReportMap, xbin, ybin, ston_stat);
+      else       fillStatusHistogram(SummaryReportMap, xbin, ybin, ston_stat*eff_fac);
 
       tot_ndet      += ndet;
       tot_errdet    += errdet;
