3db7c16568816867561feb7d20caf0e9bfa41599
Stilianos Kesisoglou <sha1-3f19527a89677f9d8b930352bda00f72f573c29b@cern.ch>
2011-02-18 08:45:37 +0000

    for CMSSW_3_9_X (tested with CMSSW_3_9_7)

--- ElectroWeakAnalysis/ZEE/test/pat_ZeePlotsFromSkimmed.py
+++ ElectroWeakAnalysis/ZEE/test/pat_ZeePlotsFromSkimmed.py
@@ -181,6 +181,16 @@ else:
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+
+#   Global Tag:
+#
+process.GlobalTag.globaltag = cms.string('GR_R_39X_V5::All')
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
 
 #   Z Selection that you prefer
 #
