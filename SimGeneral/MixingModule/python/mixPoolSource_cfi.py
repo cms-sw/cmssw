503ee6b04fc2681c2902db2566609705a8eaf48c
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2011-02-20 03:47:14 +0000

    add new mixPoolSource_cfi to centrally specify input files

---
+++ SimGeneral/MixingModule/python/mixPoolSource_cfi.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+FileNames = cms.untracked.vstring('/store/relval/CMSSW_3_1_0_pre11/RelValMinBias/GEN-SIM-DIGI-RA\
+W-HLTDEBUG/MC_31X_V1-v1/0000/ECDB1818-A964-DE11-9B4B-001D09F24934.root',
+                                  '/store/relval/CMSSW_3_1_0_pre11/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V1-v1/0000/D245A5BB-4C64-DE11-9F79-001D09F248F8.root')
--- SimGeneral/MixingModule/python/mix_E8TeV_AVE_16_BX_50ns_cfi.py
+++ SimGeneral/MixingModule/python/mix_E8TeV_AVE_16_BX_50ns_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 # configuration to model pileup for initial physics phase
-from SimGeneral.MixingModule.mixObjects_cfi import *
+from SimGeneral.MixingModule.mixObjects_cfi import * 
+from SimGeneral.MixingModule.mixPoolSource_cfi import * 
+
 mix = cms.EDProducer("MixingModule",
     LabelPlayback = cms.string(''),
     maxBunch = cms.int32(2),
@@ -21,12 +23,7 @@ mix = cms.EDProducer("MixingModule",
         seed = cms.int32(1234567),
         type = cms.string('poisson'),
 	sequential = cms.untracked.bool(False),
-        fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/18890F4C-FD99-DD11-BFF9-000423D996C8.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/1A423C16-6099-DD11-9320-000423D9853C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/5EDA8A7F-5D99-DD11-B1CD-001617C3B706.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/600D1E6A-5F99-DD11-A7D5-000423D9890C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/68ECAE92-5F99-DD11-ACAB-000423D98E6C.root',
-        '/store/relval/CMSSW_2_1_10/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V7_v2/0000/8802D325-5E99-DD11-B858-000423D98A44.root')
+        fileNames = FileNames 
     ),
     mixObjects = cms.PSet(
         mixCH = cms.PSet(
