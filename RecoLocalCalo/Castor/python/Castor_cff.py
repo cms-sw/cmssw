c90c909dcf84c56f9cfab5cb03a662b32a2fe2d9
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2011-02-21 20:41:25 +0000

    use rechits

--- RecoLocalCalo/Castor/python/Castor_cff.py
+++ RecoLocalCalo/Castor/python/Castor_cff.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoLocalCalo.Castor.CastorCellReco_cfi import *
 from RecoLocalCalo.Castor.CastorTowerReco_cfi import *
 from RecoJets.JetProducers.ak7CastorJets_cfi import *
 from RecoJets.JetProducers.ak7CastorJetID_cfi import *
 
-CastorFullReco = cms.Sequence(CastorCellReco*CastorTowerReco*ak7BasicJets*ak7CastorJetID)
+CastorFullReco = cms.Sequence(CastorTowerReco*ak7BasicJets*ak7CastorJetID)
 
