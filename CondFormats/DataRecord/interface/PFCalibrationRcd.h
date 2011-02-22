bdbd016807598f8ebd9688f1e4ff1f74040fc6c2
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-02-22 10:55:58 +0000

    Add a rcd for PF calibrations

---
+++ CondFormats/DataRecord/interface/PFCalibrationRcd.h
@@ -0,0 +1,8 @@
+#ifndef Reco_Records_PFCalibration_h
+#define Reco_Records_PFCalibration_h
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class PFCalibrationRcd : public edm::eventsetup::EventSetupRecordImplementation<PFCalibrationRcd> {};
+
+#endif
