c220224e1c03792a521a5720466e10035d60e708
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2011-02-22 20:25:53 +0000

    define a pfTrackingGlobalReco sequence equivalent to particleFlowTrackWithDisplacedVertex

--- RecoParticleFlow/PFTracking/python/particleFlowTrack_cff.py
+++ RecoParticleFlow/PFTracking/python/particleFlowTrack_cff.py
@@ -4,9 +4,9 @@ from RecoParticleFlow.PFTracking.pfTrack_cfi import *
 from RecoParticleFlow.PFTracking.pfNuclear_cfi import *
 from RecoParticleFlow.PFTracking.pfV0_cfi import *
 from RecoParticleFlow.PFTracking.pfTrackElec_cfi import *
-
+from RecoParticleFlow.PFTracking.particleFlowTrackWithDisplacedVertex_cff import *
 
 particleFlowTrack = cms.Sequence(pfTrack*pfTrackElec)
 particleFlowTrackWithNuclear = cms.Sequence(pfTrack*pfTrackElec*pfNuclear)
 particleFlowTrackWithV0 = cms.Sequence(pfTrack*pfTrackElec*pfV0)
-
+pfTrackingGlobalReco = cms.Sequence(particleFlowTrackWithDisplacedVertex)
