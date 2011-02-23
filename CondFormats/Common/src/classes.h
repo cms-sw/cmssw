85373e632b579cd8ac2a91fcadb5ee9d239e1e74
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-02-23 17:04:35 +0000

    Add the dictionary for the DropBoxMetadata class

--- CondFormats/Common/src/classes.h
+++ CondFormats/Common/src/classes.h
@@ -7,6 +7,9 @@
 #include "CondFormats/Common/interface/IOVKeysDescription.h"
 #include "CondFormats/Common/interface/ConfObject.h"
 
+#include "CondFormats/Common/interface/DropBoxMetadata.h"
+
+
 #include <vector>
 
 namespace {
@@ -17,6 +20,14 @@ namespace {
     struct Dummy {
          std::map<unsigned long long,unsigned long long> dummyForTests;
          std::map<unsigned long long,unsigned long long>::value_type dummyForTest2;
+
+      DropBoxMetadata::Parameters aparam;
+      std::pair<std::string, DropBoxMetadata::Parameters> apair1;
+      std::pair<const std::basic_string<char>, DropBoxMetadata::Parameters> apair2;
+
+      std::map<std::string, DropBoxMetadata::Parameters> amap1;
+      std::map<const std::basic_string<char>, DropBoxMetadata::Parameters> amap2;
+
     };
 
   }
