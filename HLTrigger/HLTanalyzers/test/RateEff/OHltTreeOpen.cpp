19dc9dd3f914406c94feb755480471dbc7599ba5
Arnaud Gay <sha1-ffc0f44b10535426b6e80e730966fc0636a397a8@cern.ch>
2011-02-23 14:45:58 +0000

    Added OpenHLT_Mu8_Photon20_CaloIdVT_IsoT_v1.

--- HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
+++ HLTrigger/HLTanalyzers/test/RateEff/OHltTreeOpen.cpp
@@ -5868,6 +5868,41 @@ void OHltTree::CheckOpenHlt(
    /*********************/
 
    /*Muon-photon cross-triggers*/
+
+   else if (triggerName.CompareTo("OpenHLT_Mu8_Photon20_CaloIdVT_IsoT_v1") == 0)
+   {
+      if (map_L1BitOfStandardHLTPath.find(triggerName)->second==1)
+      {
+         if (prescaleResponse(menu, cfg, rcounter, it))
+         {
+            if (OpenHlt1MuonPassed(0., 4., 8., 2., 0)>=1
+                  && OpenHlt1PhotonSamHarperPassed(20., 0, // ET, L1isolation
+                        999.,
+                        999., // Track iso barrel, Track iso endcap
+                        3.0,
+                        3.0, // Track/pT iso barrel, Track/pT iso endcap
+                        3.0,
+                        3.0, // H/ET iso barrel, H/ET iso endcap
+                        5.0,
+                        5.0, // E/ET iso barrel, E/ET iso endcap
+                        0.05,
+                        0.05, // H/E barrel, H/E endcap  
+                        0.011,
+                        0.031, // cluster shape barrel, cluster shape endcap  
+                        0.98,
+                        999., // R9 barrel, R9 endcap
+                        999.,
+                        999., // Deta barrel, Deta endcap
+                        999.,
+                        999. // Dphi barrel, Dphi endcap
+                  )>=1)
+            {
+               triggerBit[it] = true;
+            }
+         }
+      }
+   }
+
    else if (triggerName.CompareTo("OpenHLT_Mu15_Photon20_CaloIdL_v1") == 0)
    {
       if (map_L1BitOfStandardHLTPath.find(triggerName)->second==1)
