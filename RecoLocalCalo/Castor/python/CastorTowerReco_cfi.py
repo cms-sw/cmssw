9a46fecb59c14c676d1743c718af0875c9864123
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2011-02-24 09:32:56 +0000

    use rechits as input and add timecut

--- RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
+++ RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 	
 CastorTowerReco = cms.EDProducer('CastorTowerProducer',
-	inputprocess = cms.untracked.string("CastorCellReco"),
-	towercut = cms.untracked.double(1.) )
+	inputprocess = cms.untracked.string("castorreco"),
+	towercut = cms.untracked.double(1.),
+	mintime = cms.untracked.double(15.),
+	maxtime = cms.untracked.double(35.) )
 
