4372880647f482a21a92481eb4f70d447acc5688
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2011-02-24 09:14:45 +0000

    change file includes

--- RecoParticleFlow/Configuration/python/RecoParticleFlow_cff.py
+++ RecoParticleFlow/Configuration/python/RecoParticleFlow_cff.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 
-#from RecoParticleFlow.PFTracking.particleFlowTrack_cff import *
-from RecoParticleFlow.PFTracking.particleFlowTrackWithDisplacedVertex_cff import *
+from RecoParticleFlow.PFTracking.particleFlowTrack_cff import *
+#from RecoParticleFlow.PFTracking.particleFlowTrackWithDisplacedVertex_cff import *
 
 from RecoParticleFlow.PFProducer.particleFlowSimParticle_cff import *
 from RecoParticleFlow.PFProducer.particleFlowBlock_cff import *
