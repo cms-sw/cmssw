ed32cbc509a202b66bfe30d64032c78d6dd1d125
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2011-02-24 14:57:54 +0000

    Missing parentheses?

--- RecoParticleFlow/Benchmark/src/PFJetBenchmark.cc
+++ RecoParticleFlow/Benchmark/src/PFJetBenchmark.cc
@@ -277,7 +277,7 @@ void PFJetBenchmark::process(const reco::PFJetCollection& pfJets, const reco::Ge
     if(!truth) continue;   
     double deltaR = algo_->deltaR(&pfj, truth);
     // check deltaR is small enough
-    if(deltaR < deltaRMax_ || abs(rec_eta)>2.5 && deltaR < 0.2 || deltaRMax_ == -1.0 ) {//start case deltaR < deltaRMax
+    if(deltaR < deltaRMax_ || (abs(rec_eta)>2.5 && deltaR < 0.2) || deltaRMax_ == -1.0 ) {//start case deltaR < deltaRMax
 
       // generate histograms comparing the reco and truth candidate (truth = closest in delta-R) 
       // get the quantities to place on the denominator and/or divide by
