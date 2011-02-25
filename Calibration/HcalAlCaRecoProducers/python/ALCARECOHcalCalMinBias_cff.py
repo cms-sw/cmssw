925151f85ba2974509e0657bc6ec9e5077bcfbc8
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2011-02-25 08:53:48 +0000

    *** empty log message ***

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalMinBias_cff.py
@@ -55,4 +55,8 @@ hbherecoNoise.dropZSmarkedPassed = cms.bool(False)
 hfrecoNoise.dropZSmarkedPassed = cms.bool(False)
 horecoNoise.dropZSmarkedPassed = cms.bool(False)
 hfrecoMBspecial.dropZSmarkedPassed = cms.bool(False)
+hbherecoNoise.tsFromDB = False
+hfrecoNoise.tsFromDB = False
+hfrecoMBspecial.tsFromDB = False
+horecoNoise.tsFromDB = False
 
