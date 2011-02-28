3fe074df2e1c73ad94673137d91dd321c3d3a8de
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2011-02-28 13:19:22 +0000

    *** empty log message ***

--- Validation/RecoEgamma/test/ElectronRedoFromRaw_cfg.py
+++ Validation/RecoEgamma/test/ElectronRedoFromRaw_cfg.py
@@ -25,36 +25,10 @@ process.RECOSIMoutput.outputCommands = cms.untracked.vstring('drop *',
   'keep *_gsfElectrons_*_*'
 )
 
+#from Configuration.PyReleaseValidation.autoCond import autoCond
+#process.GlobalTag.globaltag = autoCond['mc']
 process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']+'::All'
 
-#import sys
-#import os
-#import electronDbsDiscovery
-#import FWCore.ParameterSet.Config as cms
-#
-#from TrackingTools.Configuration.TrackingTools_cff import *
-#from RecoEgamma.Configuration.RecoEgamma_cff import *
-#
-#process = cms.Process("electrons")
-#
-#process.load("Configuration.StandardSequences.Services_cff")
-#process.load("Configuration.StandardSequences.Geometry_cff")
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-#process.load("FWCore.MessageService.MessageLogger_cfi")
-#
-#process.load("Configuration.StandardSequences.RawToDigi_cff")
-#process.load("Configuration.StandardSequences.Reconstruction_cff")
-#
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.load("Configuration.EventContent.EventContent_cff")
-#
-#process.source = cms.Source ("PoolSource",
-#    fileNames = cms.untracked.vstring(),
-#    secondaryFileNames = cms.untracked.vstring(),
-#)
-#
-#process.source.fileNames.extend(electronDbsDiscovery.search())
-#process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 #
 #process.out = cms.OutputModule("PoolOutputModule",
 #    outputCommands = cms.untracked.vstring('drop *', 
@@ -75,15 +49,4 @@ process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']+'::All'
 #
 #process.gsfElectrons.ctfTracksCheck = cms.bool(False)
 #
-#process.mylocalreco =  cms.Sequence(process.trackerlocalreco*process.calolocalreco)
-#process.myglobalreco = cms.Sequence(process.offlineBeamSpot+process.recopixelvertexing*process.ckftracks+process.ecalClusters+process.caloTowersRec*process.vertexreco*egammarecoGlobal*process.particleFlowCluster)
-#process.myelectronseeding = cms.Sequence(process.trackerDrivenElectronSeeds*process.ecalDrivenElectronSeeds*process.electronMergedSeeds)
-#process.myelectrontracking = cms.Sequence(process.electronCkfTrackCandidates*process.electronGsfTracks)
-#
-#process.p = cms.Path(process.RawToDigi*process.mylocalreco*process.myglobalreco*process.myelectronseeding*process.myelectrontracking*process.particleFlowReco*process.pfElectronTranslator*process.gsfElectronSequence)
-#
-#process.outpath = cms.EndPath(process.out)
-#process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']+'::All'
-#
-
 
