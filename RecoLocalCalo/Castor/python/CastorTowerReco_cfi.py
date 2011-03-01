8116b1c2a339c76509fe89d50a7fe43804b88c99
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2011-03-01 14:25:07 +0000

    loosen up time cut for now until pulseshape in MC is properly implemented

--- RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
+++ RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
@@ -4,6 +4,6 @@ import FWCore.ParameterSet.Config as cms
 CastorTowerReco = cms.EDProducer('CastorTowerProducer',
 	inputprocess = cms.untracked.string("castorreco"),
 	towercut = cms.untracked.double(1.),
-	mintime = cms.untracked.double(15.),
-	maxtime = cms.untracked.double(35.) )
+	mintime = cms.untracked.double(-99999.), 
+	maxtime = cms.untracked.double(99999.) )
 
