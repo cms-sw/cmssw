dc536d7094bac7650f7ee3b268d96223ef7e775c
Sebastien Viret <sha1-a6cea6e353e5220dd4cdea2aa93b0d8e568d8d13@cern.ch>
2011-03-01 14:02:55 +0000

    Adding new TrackerHaloFilter

---
+++ HLTrigger/special/python/hltTrackerHaloFilter_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+hltTrackerHaloFilter = cms.EDFilter( "HLTTrackerHaloFilter",
+   inputTag           = cms.InputTag( "hltSiStripClusters" ),
+   saveTag            = cms.untracked.bool( False ),
+   MaxClustersTECp    = cms.int32(50),
+   MaxClustersTECm    = cms.int32(50),
+   SignalAccumulation = cms.int32(5),
+   MaxClustersTEC     = cms.int32(60),
+   MaxAccus           = cms.int32(4),
+   FastProcessing     = cms.int32(1)
+)
+
+
