7cc4970a863136ad50e0a13b626bed76a429fa13
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-01 12:53:48 +0000

    Add feature to make combinatoric combinations of strps

--- RecoTauTag/RecoTau/plugins/RecoTauPiZeroStripPlugin.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauPiZeroStripPlugin.cc
@@ -26,8 +26,20 @@
 
 #include "RecoTauTag/RecoTau/interface/RecoTauCommonUtilities.h"
 #include "RecoTauTag/RecoTau/interface/RecoTauQualityCuts.h"
+#include "RecoTauTag/RecoTau/interface/CombinatoricGenerator.h"
 
 namespace reco { namespace tau {
+
+namespace {
+// Apply a hypothesis on the mass of the strips.
+math::XYZTLorentzVector applyMassConstraint(
+    const math::XYZTLorentzVector& vec,double mass) {
+  double factor = sqrt(vec.energy()*vec.energy()-mass*mass)/vec.P();
+  return math::XYZTLorentzVector(
+      vec.px()*factor,vec.py()*factor,vec.pz()*factor,vec.energy());
+}
+}
+
 
 class RecoTauPiZeroStripPlugin : public RecoTauPiZeroBuilderPlugin {
   public:
@@ -47,6 +59,11 @@ class RecoTauPiZeroStripPlugin : public RecoTauPiZeroBuilderPlugin {
     double etaAssociationDistance_;//eta Clustering Association Distance
     double phiAssociationDistance_;//phi Clustering Association Distance
 
+    // Parameters for build strip combinations
+    bool combineStrips_;
+    int maxStrips_;
+    double combinatoricStripMassHypo_;
+
     AddFourMomenta p4Builder_;
 };
 
@@ -61,6 +78,12 @@ RecoTauPiZeroStripPlugin::RecoTauPiZeroStripPlugin(
       "stripEtaAssociationDistance");
   phiAssociationDistance_ = pset.getParameter<double>(
       "stripPhiAssociationDistance");
+  combineStrips_ = pset.getParameter<bool>("makeCombinatoricStrips");
+  if (combineStrips_) {
+    maxStrips_ = pset.getParameter<int>("maxInputStrips");
+    combinatoricStripMassHypo_ =
+      pset.getParameter<double>("stripMassWhenCombining");
+  }
 }
 
 // Update the primary vertex
@@ -116,8 +139,61 @@ RecoTauPiZeroStripPlugin::return_type RecoTauPiZeroStripPlugin::operator()(
         ++stripCand;
       }
     }
+    // Update the vertex
+    if (strip->daughterPtr(0).isNonnull())
+      strip->setVertex(strip->daughterPtr(0)->vertex());
     output.push_back(strip);
   }
+
+  // Check if we want to combine our strips
+  if (combineStrips_ && output.size() > 1) {
+    PiZeroVector stripCombinations;
+    // Sort the output by descending pt
+    output.sort(output.begin(), output.end(),
+        boost::bind(&RecoTauPiZero::pt, _1) >
+        boost::bind(&RecoTauPiZero::pt, _2));
+    // Get the end of interesting set of strips to try and combine
+    PiZeroVector::const_iterator end_iter = takeNElements(
+        output.begin(), output.end(), maxStrips_);
+
+    // Look at all the combinations
+    for (PiZeroVector::const_iterator first = output.begin();
+        first != end_iter-1; ++first) {
+      for (PiZeroVector::const_iterator second = first+1;
+          second != end_iter; ++second) {
+        Candidate::LorentzVector firstP4 = first->p4();
+        Candidate::LorentzVector secondP4 = second->p4();
+        // If we assume a certain mass for each strip apply it here.
+        firstP4 = applyMassConstraint(firstP4, combinatoricStripMassHypo_);
+        secondP4 = applyMassConstraint(secondP4, combinatoricStripMassHypo_);
+        Candidate::LorentzVector totalP4 = firstP4 + secondP4;
+        // Make our new combined strip
+        std::auto_ptr<RecoTauPiZero> combinedStrips(
+            new RecoTauPiZero(0, totalP4,
+              Candidate::Point(0, 0, 0),
+              111, 10001, true, RecoTauPiZero::kCombinatoricStrips));
+
+        // Now loop over the strip members
+        BOOST_FOREACH(const RecoTauPiZero::daughters::value_type& gamma,
+            first->daughterPtrVector()) {
+          combinedStrips->addDaughter(gamma);
+        }
+        BOOST_FOREACH(const RecoTauPiZero::daughters::value_type& gamma,
+            second->daughterPtrVector()) {
+          combinedStrips->addDaughter(gamma);
+        }
+        // Update the vertex
+        if (combinedStrips->daughterPtr(0).isNonnull())
+          combinedStrips->setVertex(combinedStrips->daughterPtr(0)->vertex());
+        // Add to our collection of combined strips
+        stripCombinations.push_back(combinedStrips);
+      }
+    }
+    // When done doing all the combinations, add the combined strips to the
+    // output.
+    output.transfer(output.end(), stripCombinations);
+  }
+
   return output.release();
 }
 }} // end namespace reco::tau
