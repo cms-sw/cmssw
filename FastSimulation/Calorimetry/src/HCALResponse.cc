8853a46b05fdab2ed4d86aed1c44d9919f9a5721
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2011-03-02 09:10:52 +0000

    updating HB/HE scale for FullSim HCAL energy scale change in 4_2_0_pre6

--- FastSimulation/Calorimetry/src/HCALResponse.cc
+++ FastSimulation/Calorimetry/src/HCALResponse.cc
@@ -84,8 +84,8 @@ HCALResponse::HCALResponse(const edm::ParameterSet& pset,
 
   // additional tuning factor to correct the response in the barrel
   //                              1     2     3     5     9    15    20    30 
-  double barrelCorrection[15] = {1.0, 0.97, 0.96, 0.96, 0.96, 0.96, 0.96, 0.97,     0.97, 0.97, 0.97, 0.97, 0.96, 0.96, 0.96};  
-  double endcapCorrection[15] = {1.06, 1.06, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05,    1.04, 1.04, 1.04, 1.04, 1.04, 1.04, 1.04};
+  double barrelCorrection[15] = {1.02, 1.01, 1.02, 1.02, 1.02, 1.02, 1.02, 1.03,     1.03, 1.03, 1.03, 1.03, 1.03, 1.03, 1.03};  
+  double endcapCorrection[15] = {1.16, 1.16, 1.15, 1.15, 1.15, 1.15, 1.15, 1.15,    1.14, 1.14, 1.14, 1.14, 1.14, 1.14, 1.14};
   //                             1-15 >>> 20   30    50   100   150   225
   double forwardCorrection[15] = {1.13, 1.13, 1.13, 1.12, 1.11, 1.10,
                                         1.08, 1.07, 1.08, 1.10, 1.12, 1.12, 
