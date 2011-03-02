703febc9a74d6bc9660940e3f5521b6e0afbd7b1
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2011-03-02 21:38:49 +0000

    *** empty log message ***

--- DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
+++ DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
@@ -4,16 +4,13 @@ muonFlag = True
 
 # report summary
 from DQM.RPCMonitorClient.RPCEventSummary_cfi import *
-rpcEventSummary.MinimumRPCEvents = cms.untracked.int32(1)
+
 from DQM.RPCMonitorClient.RPCDqmClient_cfi import *
-rpcdqmclient.MinimumRPCEvents = cms.untracked.int32(1)
-rpcdqmMuonclient.MinimumRPCEvents = cms.untracked.int32(1)
+
 
 from DQM.RPCMonitorClient.RPCRecHitProbabilityClient_cfi import *
 
 from  DQM.RPCMonitorClient.RPCChamberQuality_cfi import *
-rpcChamberQuality.MinimumRPCEvents = cms.untracked.int32(1)
-rpcMuonChamberQuality.MinimumRPCEvents = cms.untracked.int32(1)
 
                                    
 from  DQM.RPCMonitorClient.RPCEfficiencySecondStep_cfi import *
@@ -34,5 +31,5 @@ from DQM.RPCMonitorClient.RPCDcsInfoClient_cfi import *
 if (muonFlag):
     rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto)
 else:
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcEventSummary*rpcEfficiencyShiftHisto)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto)
 
