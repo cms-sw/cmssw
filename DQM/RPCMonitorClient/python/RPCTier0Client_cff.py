573ad6ae7c49230e544fe8d695b501ee593475a7
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2011-03-02 21:46:02 +0000

    bug fix

--- DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
+++ DQM/RPCMonitorClient/python/RPCTier0Client_cff.py
@@ -29,7 +29,7 @@ qTesterRPC = cms.EDAnalyzer("QualityTester",
 from DQM.RPCMonitorClient.RPCDcsInfoClient_cfi import *
 
 if (muonFlag):
-    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto)
+    rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcdqmMuonclient*rpcrechitprobabilityclient*rpcChamberQuality*rpcMuonChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto)
 else:
     rpcTier0Client = cms.Sequence(qTesterRPC*rpcdqmclient*rpcChamberQuality*rpcDcsInfoClient*rpcefficiencysecond*rpcEventSummary*rpcEfficiencyShiftHisto)
 
