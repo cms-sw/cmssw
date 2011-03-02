1a41408c950bbb35ccdb8d143540e5dfda37a3ac
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-02 20:16:07 +0000

    Make workflow more modular

---
+++ RecoTauTag/TauTagTools/test/training/train_cfg.py
@@ -0,0 +1,103 @@
+'''
+
+TaNC MVA trainer
+
+Author: Evan K. Friis (UC Davis)
+
+'''
+
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+import os
+import sys
+
+options = VarParsing.VarParsing ('analysis')
+
+# Register options
+options.register(
+    'xml', '',
+    VarParsing.VarParsing.multiplicity.singleton,
+    VarParsing.VarParsing.varType.string,
+    "XML file with MVA configuration")
+
+options.parseArguments()
+
+# Map the XML file name to a nice computer name
+_computer_name = os.path.basename(os.path.splitext(options.xml)[0])
+print _computer_name
+
+process = cms.Process("TrainMVA")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# Input files
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(options.inputFiles),
+    skipBadFiles = cms.untracked.bool(True),
+)
+
+#######################################################
+# Database BS
+#######################################################
+
+process.PoolDBOutputService = cms.Service(
+    "PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet( messageLevel = cms.untracked.int32(0) ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:%s' % options.outputFile.replace('.root','')),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('TauTagMVAComputerRcd'),
+        tag = cms.string('Train')
+    ))
+)
+
+process.MVATrainerSave = cms.EDAnalyzer(
+    "TauMVATrainerSave",
+    toPut = cms.vstring(_computer_name),
+    toCopy = cms.vstring()
+)
+
+setattr(process.MVATrainerSave, _computer_name,
+        cms.string(_computer_name + ".mva"))
+
+process.looper = cms.Looper(
+    "TauMVATrainerLooper",
+    trainers = cms.VPSet(cms.PSet(
+        calibrationRecord = cms.string(_computer_name),
+        saveState = cms.untracked.bool(True),
+        trainDescription = cms.untracked.string(options.xml),
+        loadState = cms.untracked.bool(False),
+        doMonitoring = cms.bool(True),
+    ))
+)
+
+###############################################################################
+# Define signal and background paths Each path only gets run if the appropriate
+# colleciton is present.  This allows separation of signal and background
+# events.
+###############################################################################
+
+# Finally, pass our selected sig/bkg taus to the MVA trainer
+from RecoTauTag.RecoTau.RecoTauDiscriminantConfiguration import \
+        discriminantConfiguration
+
+process.trainer = cms.EDAnalyzer(
+    "RecoTauMVATrainer",
+    signalSrc = cms.InputTag("signalTaus"),
+    backgroundSrc = cms.InputTag("backgroundTaus"),
+    computerName = cms.string(_computer_name),
+    dbLabel = cms.string("trainer"),
+    discriminantOptions = discriminantConfiguration
+)
+
+process.trainPath = cms.Path(process.trainer)
+
+process.outpath = cms.EndPath(process.MVATrainerSave)
+
+process.schedule = cms.Schedule(
+    process.trainPath,
+    process.outpath
+)
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
