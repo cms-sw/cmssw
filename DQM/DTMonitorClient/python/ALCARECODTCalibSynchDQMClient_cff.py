16bdd508d6b525bd59c2c305628b1772d7450461
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2011-03-03 09:06:16 +0000

    fix to base folder name in cff

--- DQM/DTMonitorClient/python/ALCARECODTCalibSynchDQMClient_cff.py
+++ DQM/DTMonitorClient/python/ALCARECODTCalibSynchDQMClient_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from DQM.DTMonitorClient.dtResolutionAnalysisTest_cfi import *
 dtResolutionAnalysisTestAlca = dtResolutionAnalysisTest.clone() 
-dtResolutionAnalysisTestAlca.topHistoFolder = 'DtCalibSynch/01-Calibration'
+dtResolutionAnalysisTestAlca.topHistoFolder = 'AlCaReco/DtCalibSynch/01-Calibration'
 
 ALCARECODTCalibSynchDQMClient = cms.Sequence(dtResolutionAnalysisTestAlca)
 
