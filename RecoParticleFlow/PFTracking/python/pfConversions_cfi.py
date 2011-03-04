adb0424850abe6e8523148f5831cbb8f45640d94
Daniele Benedetti <sha1-b78b7786b135946381de7085dafdd1b4e3149539@cern.ch>
2011-03-04 16:25:29 +0000

    restore vs 1.5 PFConversionProducer

--- RecoParticleFlow/PFTracking/python/pfConversions_cfi.py
+++ RecoParticleFlow/PFTracking/python/pfConversions_cfi.py
@@ -4,8 +4,7 @@ import FWCore.ParameterSet.Config as cms
 #  Author: M. Gouzevitch base on N. Marinelli work
 #
 pfConversions = cms.EDProducer("PFConversionProducer", 
-    conversionCollection = cms.InputTag("allConversions", ""),
-    PrimaryVertexLabel = cms.InputTag("offlinePrimaryVertices"),  
+    conversionCollection = cms.InputTag("allConversions", "")
 )
 
 
