b2d6c3482b7758a01ce1a2cc4c37aab76d505a77
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2011-03-07 16:05:15 +0000

    First committ

---
+++ FastSimulation/ParticleFlow/plugins/SealModules.cc
@@ -0,0 +1,7 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+
+#include "FastSimulation/ParicleFlow/plugins/FSPFProducer.h"
+
+DEFINE_FWK_MODULE(FSPFProducer);
