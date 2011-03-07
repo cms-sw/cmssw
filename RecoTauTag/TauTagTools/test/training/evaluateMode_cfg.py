733582d50228005fa95979cc59866ca44ef00eb4
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-07 19:57:45 +0000

    new workflow

--- RecoTauTag/TauTagTools/test/training/evaluateMode_cfg.py
+++ RecoTauTag/TauTagTools/test/training/evaluateMode_cfg.py
@@ -14,7 +14,6 @@ Author: Evan K. Friis (UC Davis)
 import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.VarParsing as VarParsing
 import sys
-import os
 import RecoTauTag.TauTagTools.RecoTauCommonJetSelections_cfi as common
 
 options = VarParsing.VarParsing ('analysis')
@@ -47,58 +46,25 @@ options.register(
 
 options.parseArguments()
 
-_SIGNAL = False
-if options.signal == 1:
-    _SIGNAL = True
 
 if options.tracks < 0 or options.pizeros < 0:
     print "You must specify the [tracks] and [pizeros] arguments."
     sys.exit(1)
 # Make a nice tuple of the decay mode
 _decay_mode = (options.tracks, options.pizeros)
-
-# We need to turn off the track based quality cuts, since we don't save them
-# in the skim.
-#from RecoTauTag.RecoTau.PFRecoTauQualityCuts_cfi import PFTauQualityCuts
-#PFTauQualityCuts.signalQualityCuts = cms.PSet(
-    #minTrackPt = PFTauQualityCuts.signalQualityCuts.minTrackPt,
-    #minGammaEt = PFTauQualityCuts.signalQualityCuts.minGammaEt,
-#)
-#PFTauQualityCuts.isolationQualityCuts = cms.PSet(
-    #minTrackPt = PFTauQualityCuts.isolationQualityCuts.minTrackPt,
-    #minGammaEt = PFTauQualityCuts.isolationQualityCuts.minGammaEt,
-#)
-
-# Build a combinatoric tau producer that only builds the desired decay modes
-from RecoTauTag.RecoTau.RecoTauCombinatoricProducer_cfi \
-        import _combinatoricTauConfig
-
-selectedDecayModes = cms.VPSet()
-for decayMode in _combinatoricTauConfig.decayModes:
-    decay_mode_tuple = (decayMode.nCharged.value(), decayMode.nPiZeros.value())
-    if decay_mode_tuple == _decay_mode:
-        print "Decay mode:", decay_mode_tuple
-        print " tau prod. max tracks:", decayMode.maxTracks
-        print " tau prod. max pizeros:", decayMode.maxPiZeros
-        selectedDecayModes.append(decayMode)
-        break
-
-# Only build the selected decay modes
-#_combinatoricTauConfig.decayModes = selectedDecayModes
+decay_mode_map = {
+    (1, 0): 0,
+    (1, 1): 1,
+    (1, 2): 2,
+    (3, 0): 10
+}
+_decay_mode_name = decay_mode_map[_decay_mode]
 
 process = cms.Process("Eval")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 
 # DQM store, PDT sources etc
-process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 process.load("Configuration.StandardSequences.Services_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-# Shit, is this okay?
-from Configuration.PyReleaseValidation.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond['mc']
-
 
 # Setup output file
 process.TFileService = cms.Service(
@@ -115,111 +81,69 @@ process.source = cms.Source(
 # Load PiZero algorithm
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
 
-decay_mode_translator = {
-    (1, 0) : 'oneProng0Pi0',
-    (1, 1) : 'oneProng1Pi0',
-    (1, 2) : 'oneProng2Pi0',
-    (3, 0) : 'threeProng0Pi0',
-    (3, 1) : 'threeProng1Pi0',
-}
-
 _KIN_CUT = "pt > 10 & abs(eta) < 2.5"
+_TYPE_LABEL = options.signal==1 and "Signal" or "Background"
 
-# For signal, select jets that match a hadronic decaymode
-process.kinematicSignalJets = cms.EDFilter(
-    "CandViewRefSelector",
-    src = cms.InputTag("preselectedSignalJets"),
-    cut = cms.string(_KIN_CUT),
-)
-process.signalSpecific = cms.Sequence(process.kinematicSignalJets)
-
-# Reselect our signal jets, using only those matched to this decay mode
-process.signalJetsDMTruthMatching = cms.EDProducer(
-    "GenJetMatcher",
-    src = cms.InputTag("kinematicSignalJets"),
-    matched = cms.InputTag("selectedTrueHadronicTaus"),
-    mcPdgId     = cms.vint32(),                      # n/a
-    mcStatus    = cms.vint32(),                      # n/a
-    checkCharge = cms.bool(False),
-    maxDeltaR   = cms.double(0.15),
-    maxDPtRel   = cms.double(3.0),
-    # Forbid two RECO objects to match to the same GEN object
-    resolveAmbiguities    = cms.bool(True),
-    resolveByMatchQuality = cms.bool(True),
-)
-process.signalSpecific += process.signalJetsDMTruthMatching
-
-process.inputJetRefs = cms.EDFilter(
-    "CandViewGenJetMatchRefSelector",
-    src = cms.InputTag("kinematicSignalJets"),
-    matching = cms.InputTag("signalJetsDMTruthMatching"),
-    # Don't keep events with no matches
-    filter = cms.bool(True)
-)
-
-process.kinematicBackgroundJets = cms.EDFilter(
-    "CandViewRefSelector",
-    src = cms.InputTag("preselectedBackgroundJets"),
+process.selectedBaseTaus = cms.EDFilter(
+    "PFTauViewRefSelector",
+    src = cms.InputTag("hpsTancTaus" + _TYPE_LABEL),
     cut = cms.string(_KIN_CUT)
 )
-process.backgroundSpecific = cms.Sequence(process.kinematicBackgroundJets)
-
-# Figure out how to deal with our events - are they signal or background?
-if _SIGNAL:
-    process.specific = process.signalSpecific
-else:
-    process.specific = process.backgroundSpecific
-    process.inputJetRefs = cms.EDFilter(
-        "CandViewRefSelector",
-        src = cms.InputTag("kinematicBackgroundJets"),
-        cut = cms.string("pt > 0"), # take everything
-        filter = cms.bool(False)
-    )
-
-# Convert to PFJetRefs, instead of candidate
-process.castedInputJets = cms.EDProducer(
-    "PFJetRefsCastFromCandView",
-    src = cms.InputTag("inputJetRefs"),
-)
-
-# Cast the jet refs to real PFJets
-process.inputJets = cms.EDProducer(
-    "PFJetCopyProducer",
-    src = cms.InputTag("castedInputJets"),
+process.selectedDecayModeTaus = cms.EDFilter(
+    "PFTauViewRefSelector",
+    src = cms.InputTag(
+        ("selectedHpsTancTrainTausDecayMode%i" % _decay_mode_name) + _TYPE_LABEL),
+    cut = cms.string(_KIN_CUT)
 )
 
 process.main = cms.Sequence(
-    process.specific*process.inputJetRefs*
-    process.castedInputJets*process.inputJets)
+    process.selectedBaseTaus*
+    process.selectedDecayModeTaus)
 
 # Plot the input jets to use in weighting the transformation
 process.plotInputJets = cms.EDAnalyzer(
     "CandViewHistoAnalyzer",
-    src = cms.InputTag("inputJets"),
-    histograms = common.jet_histograms
+    src = cms.InputTag("selectedBaseTaus"),
+    histograms = common.tau_histograms
 )
 process.main += process.plotInputJets
 
 # Build our taus
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
-process.recoTauAK5PFJets08Region.src = cms.InputTag("inputJets")
-process.ak5PFJetsRecoTauPiZeros.jetSrc = "inputJets"
-process.combinatoricRecoTaus.jetSrc = "inputJets"
-process.combinatoricRecoTaus.piZeroSrc = cms.InputTag("ak5PFJetsRecoTauPiZeros")
-process.combinatoricRecoTaus.jetRegionSrc = cms.InputTag("recoTauAK5PFJets08Region")
-process.combinatoricRecoTaus.outputSelection = cms.string(
-        'mass() < 3 & isolationPFChargedHadrCandsPtSum() < 7')
 
-#process.combinatoricRecoTaus.builders = cms.VPSet(_combinatoricTauConfig)
-# Add PFTauTagInfo workaround
-process.main += process.ak5PFJetsRecoTauPiZeros
-process.main += process.recoTauCommonSequence
-process.main += process.combinatoricRecoTaus
-process.main += process.hpsTancTauSequence
-print process.hpsTancTauSequence.remove(
-    process.hpsTancTausDiscriminationAgainstElectron)
-print process.hpsTancTauSequence.remove(
-    process.hpsTancTausDiscriminationAgainstMuon)
+# Dont compute MVAs for ones that don't matter.  We make a discriminator that
+# selects taus taht should have an MVA value computed for them.
+from RecoTauTag.RecoTau.TauDiscriminatorTools import noPrediscriminants
+process.selectedTausDiscriminator = cms.EDProducer(
+    "PFRecoTauDiscriminationByStringCut",
+    PFTauProducer = cms.InputTag("hpsTancTaus" + _TYPE_LABEL),
+    cut = cms.string(_KIN_CUT + " && decayMode == %i" % _decay_mode_name),
+    Prediscriminants = noPrediscriminants,
+)
+
+process.hpsTancTausDiscriminationByTancRaw.PFTauProducer = cms.InputTag(
+    "hpsTancTaus" + _TYPE_LABEL)
+# Make sure we first require the correct decay mode
+process.hpsTancTausDiscriminationByTancRaw.Prediscriminants = cms.PSet(
+    BooleanOperator = cms.string("and"),
+    relevanceCut = cms.PSet(
+        cut = cms.double(0.5),
+        Producer = cms.InputTag("selectedTausDiscriminator")
+    )
+)
+process.hpsTancTausDiscriminationByTancRaw.remapOutput = False
+
+
+# Setup the flight path discriminators.  In the training case, these come from
+# two different places
+process.hpsTancTausDiscriminationByTancRaw.discriminantOptions.\
+        FlightPathSignificance.discSrc = cms.VInputTag(
+    "hpsTancTausDiscriminationByFlightPathSignal",
+    "hpsTancTausDiscriminationByFlightPathBackground",
+)
+
+process.main += process.selectedTausDiscriminator
+process.main += process.hpsTancTausDiscriminationByTancRaw
 
 ################################################################################
 ##         Prepare new TaNC discriminator                                    ###
@@ -230,10 +154,6 @@ process.load("RecoTauTag.TauTagTools.TancConditions_cff")
 process.TauTagMVAComputerRecord.connect = cms.string(
     'sqlite:%s' % options.db)
 
-#process.es_prefer_tanc = cms.ESPrefer("PoolDBESSource",
-#                                      "TauTagMVAComputerRecord")
-#
-
 process.TauTagMVAComputerRecord.toGet[0].tag = cms.string('Tanc')
 process.TauTagMVAComputerRecord.appendToDataLabel = cms.string("hpstanc")
 #mvaLabel = os.path.splitext(os.path.basename(options.db))[0]
@@ -243,20 +163,9 @@ process.TauTagMVAComputerRecord.appendToDataLabel = cms.string("hpstanc")
 ##         Plot the output of the "best" Taus                                ###
 ################################################################################
 
-# Only plot those that match this decay mode
-process.matchingDecayMode = cms.EDFilter(
-    "PFTauViewRefSelector",
-    src = cms.InputTag("hpsTancTaus"),
-    cut = cms.string("signalPFChargedHadrCands().size() = %i &"
-                     "signalPiZeroCandidates().size() = %i" %
-                     (options.tracks, options.pizeros)),
-    filter = cms.bool(False),
-)
-process.main += process.matchingDecayMode
-
 process.cleanTauPlots = cms.EDAnalyzer(
     "RecoTauPlotDiscriminator",
-    src = cms.InputTag("matchingDecayMode"),
+    src = cms.InputTag("selectedDecayModeTaus"),
     discriminators = cms.VInputTag(
         cms.InputTag("hpsTancTausDiscriminationByTancRaw")
     ),
@@ -266,12 +175,5 @@ process.cleanTauPlots = cms.EDAnalyzer(
 )
 
 process.main += process.cleanTauPlots
-
-process.dirtyTauPlots = process.cleanTauPlots.clone(
-    src = cms.InputTag("combinatoricRecoTaus"),
-    discriminators = cms.VInputTag(
-        cms.InputTag("combinatoricRecoTausDiscriminationByTanc"))
-)
-process.main += process.dirtyTauPlots
 
 process.path = cms.Path(process.main)
