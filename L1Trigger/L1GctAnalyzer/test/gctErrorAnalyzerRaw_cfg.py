0c4f3aa4035c7b9085a9b3c253c6fdcb91a93b57
Georgia Karapostoli <sha1-f11bf99b04a805318f50d29a11dbfa38bec6f562@cern.ch>
2011-03-07 15:55:29 +0000

    updates for Lab sys specific configuration

--- L1Trigger/L1GctAnalyzer/test/gctErrorAnalyzerRaw_cfg.py
+++ L1Trigger/L1GctAnalyzer/test/gctErrorAnalyzerRaw_cfg.py
@@ -3,10 +3,11 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("GCTAnalyzerTest")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger = cms.Service ("MessageLogger", 
-	destinations = cms.untracked.vstring( "detailedInfo.txt" ),
-	threshold = cms.untracked.string ( 'WARNING' )
-)
+process.MessageLogger.cerr.INFO.limit = cms.untracked.int32(100000)
+#process.MessageLogger = cms.Service ("MessageLogger", 
+#	destinations = cms.untracked.vstring( "detailedInfo.txt" ),
+#	threshold = cms.untracked.string ( 'WARNING' )
+#)
 
 process.source = cms.Source ( "EmptySource" )
 
@@ -14,9 +15,9 @@ process.source = cms.Source ( "EmptySource" )
 process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 ( 3563 ) ) #use 3563 for whole orbit
 
 # raw data
-process.gctRaw = cms.OutputModule( "TextToRaw",
-  filename = cms.untracked.string("slinkOutput.txt"),
-  GctFedId = cms.untracked.int32 ( 745 ),
+process.gctRaw = cms.EDProducer( "TextToRaw",
+  filename = cms.untracked.string("patternCaptureOrbit_hwtest__2011_02_18__10h01m55s_HfInc.txt"),
+   GctFedId = cms.untracked.int32 ( 745 ),
   FileEventOffset = cms.untracked.int32 ( 0 )
 )
 
@@ -33,13 +34,25 @@ process.l1GctHwDigis.unpackInternEm = cms.untracked.bool ( True )
 process.l1GctHwDigis.unpackInternJets = cms.untracked.bool ( True )
 
 # GCT emulator
-process.load('L1Trigger.Configuration.L1StartupConfig_cff')
+process.load('L1Trigger.L1GctAnalyzer.gctPatternTestLabConfig_cff')
+#process.load('L1TriggerConfig.GctConfigProducers.l1GctConfig_cfi')
+#process.load('L1Trigger.Configuration.L1StartupConfig_cff')
 import L1Trigger.GlobalCaloTrigger.gctDigis_cfi
 process.valGctDigis = L1Trigger.GlobalCaloTrigger.gctDigis_cfi.gctDigis.clone()
 process.valGctDigis.writeInternalData = cms.bool(True)
 process.valGctDigis.inputLabel = cms.InputTag( "l1GctHwDigis" )
 process.valGctDigis.preSamples = cms.uint32(0)
 process.valGctDigis.postSamples = cms.uint32(0)
+
+process.valGctDigis.hardwareTest = cms.bool(True) ## specific to "Lab" configuration
+process.valGctDigis.jetLeafMask = cms.untracked.uint32(0x1f) ## specific to "Lab" configuration
+
+
+## HF exclusion
+#process.L1GctConfigProducers.MEtEtaMask = cms.uint32(0x3C000F)
+#process.L1GctConfigProducers.TEtEtaMask = cms.uint32(0x3C000F)
+#process.L1GctConfigProducers.MHtEtaMask = cms.uint32(0x3E001F)
+#process.L1GctConfigProducers.HtEtaMask = cms.uint32(0x3E001F)
 
 # my analyzer
 process.TFileService = cms.Service("TFileService",
@@ -50,7 +63,11 @@ process.TFileService = cms.Service("TFileService",
 process.load('L1Trigger.L1GctAnalyzer.gctErrorAnalyzer_cfi')
 process.gctErrorAnalyzer.doEmuMBx = cms.untracked.bool(True)
 process.gctErrorAnalyzer.doGCTMBx = cms.untracked.bool(True)
-process.gctErrorAnalyzer.doExtraMissingHTDebug = cms.untracked.bool(True)
+process.gctErrorAnalyzer.doExtraMissingHTDebug = cms.untracked.bool(False)
+process.gctErrorAnalyzer.useSys = cms.untracked.string("Lab") ## specific to "Lab" configuration
+
+process.load("L1TriggerConfig.GctConfigProducers.l1GctConfigDump_cfi")
+ 
 
 process.defaultPath = cms.Sequence ( 
 # Text to Raw
@@ -60,7 +77,9 @@ process.l1GctHwDigis *
 # Emulator
 process.valGctDigis *
 # GCTErrorAnalyzer
-process.gctErrorAnalyzer
+process.gctErrorAnalyzer #*
+
+#process.l1GctConfigDump
 )
 
 process.p = cms.Path(process.defaultPath)
