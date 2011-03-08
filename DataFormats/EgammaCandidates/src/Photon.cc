cf5d3a6e3da1edb3d93ed6309bba3320f984210e
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2011-03-08 17:54:09 +0000

    bug fix in copy constructor

--- DataFormats/EgammaCandidates/src/Photon.cc
+++ DataFormats/EgammaCandidates/src/Photon.cc
@@ -1,44 +1,45 @@
-// $Id: Photon.cc,v 1.21 2010/11/15 21:31:48 dlange Exp $
+// $Id: Photon.cc,v 1.22 2011/02/25 22:13:38 dlange Exp $
 #include "DataFormats/EgammaCandidates/interface/Photon.h"
-#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h" 
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
 
 using namespace reco;
 
-Photon::Photon( const LorentzVector & p4, 
+Photon::Photon( const LorentzVector & p4,
 		Point caloPos,
-                const PhotonCoreRef & core,
-		const Point & vtx) : 
-    RecoCandidate( 0, p4, vtx, 22 ), 
+    const PhotonCoreRef & core,
+		const Point & vtx) :
+    RecoCandidate( 0, p4, vtx, 22 ),
     caloPosition_( caloPos ),
     photonCore_(core),
     pixelSeed_(false)
-{ }
+ {}
 
 
-Photon::Photon( const Photon& rhs ) : 
+Photon::Photon( const Photon& rhs ) :
   RecoCandidate(rhs),
-  caloPosition_(rhs.caloPosition_), 
+  caloPosition_(rhs.caloPosition_),
   photonCore_ ( rhs.photonCore_),
   pixelSeed_  ( rhs.pixelSeed_ ),
   fiducialFlagBlock_ ( rhs.fiducialFlagBlock_ ),
   isolationR04_ ( rhs.isolationR04_),
   isolationR03_ ( rhs.isolationR03_),
-  showerShapeBlock_ ( rhs.showerShapeBlock_)
-{}
+  showerShapeBlock_ ( rhs.showerShapeBlock_),
+  pfIsolation_ ( rhs.pfIsolation_ )
+ {}
 
 
- 
+
 
 Photon::~Photon() { }
 
-Photon * Photon::clone() const { 
-  return new Photon( * this ); 
+Photon * Photon::clone() const {
+  return new Photon( * this );
 }
 
 
 bool Photon::overlap( const Candidate & c ) const {
   const RecoCandidate * o = dynamic_cast<const RecoCandidate *>( & c );
-  return ( o != 0 && 
+  return ( o != 0 &&
 	   ( checkOverlap( superCluster(), o->superCluster() ) )
 	   );
   return false;
