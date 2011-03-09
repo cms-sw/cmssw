5292a7e7b06237d7d8ca0ff3c6160af4e71d54bb
Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch>
2011-03-09 10:16:00 +0000

    Replaced BooleanOutput, invMassMin, invMassMax by cms.PSet select

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByInvMass_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByInvMass_cfi.py
@@ -7,8 +7,9 @@ pfRecoTauDiscriminationByInvMass = cms.EDProducer("PFRecoTauDiscriminationByInvM
     PFTauProducer       = cms.InputTag('pfRecoTauProducer'), #tau collection to discriminate
 
     Prediscriminants    = requireLeadTrack,
-    BooleanOutput	= cms.bool(True),
 
-    invMassMin		= cms.double(0.0),   # used only if threeProngSelection == true
-    invMassMax          = cms.double(1.4),   # used only if threeProngSelection == true
+    select = cms.PSet(
+        min = cms.double(0.0),
+        max = cms.double(1.4),
+    )
 )
