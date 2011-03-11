a41a97d80cb2c60895062d2b5f17d1991fb3f18b
Joshua James Swanson <sha1-8881b0a90e2d698b47914ccde7f42725e686d8ab@cern.ch>
2011-03-11 09:37:52 +0000

    Updated to 5E32 Menu

--- HLTriggerOffline/Tau/python/Validation/HLTTauValidation_cff.py
+++ HLTriggerOffline/Tau/python/Validation/HLTTauValidation_cff.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from HLTriggerOffline.Tau.Validation.HLTTauReferences_cfi import *
 #from HLTriggerOffline.Tau.Validation.HLTTauValidation_8E29_cfi import *
 #from HLTriggerOffline.Tau.Validation.HLTTauValidation_1E31_cfi import *
-from HLTriggerOffline.Tau.Validation.HLTTauValidation_6E31_cfi import *
-from HLTriggerOffline.Tau.Validation.HLTTauValidation_2E32_cfi import *
+#from HLTriggerOffline.Tau.Validation.HLTTauValidation_6E31_cfi import *
+from HLTriggerOffline.Tau.Validation.HLTTauValidation_5E32_cfi import *
 
-HLTTauVal    = cms.Sequence(hltTauRef+hltTauValIdeal2E32+hltTauValIdeal6E31)
+HLTTauVal    = cms.Sequence(hltTauRef+hltTauValIdeal5E32)
 
 # for FS, hltTauRef Producers go into separate "prevalidation" sequence
 # (this fixes the "no EDProducer in EndPath" problem)
-HLTTauValFS  = cms.Sequence(hltTauValIdeal2E32+hltTauValIdeal6E31)
+HLTTauValFS  = cms.Sequence(hltTauValIdeal5E32)
 
 
 
