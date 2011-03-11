70b5d8d4112bc2f0fb777c556d3aa18398cd910c
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2011-03-11 12:36:53 +0000

    add Cascade monitoring

--- DQM/HLTEvF/interface/HLTMuonDQMSource.h
+++ DQM/HLTEvF/interface/HLTMuonDQMSource.h
@@ -5,8 +5,8 @@
  * *
  *  DQM Test Client
  *
- *  $Date: 2009/10/15 11:31:28 $
- *  $Revision: 1.19 $
+ *  $Date: 2010/04/27 23:26:25 $
+ *  $Revision: 1.20 $
  *  \author  M. Vander Donckt CERN
  *   
  */
@@ -35,20 +35,20 @@ protected:
   void beginJob();
 
   /// BeginRun
-  void beginRun(const edm::Run& r, const edm::EventSetup& c);
+  void beginRun(const edm::Run& run, const edm::EventSetup& eventSetup);
 
   /// Fake Analyze
-  void analyze(const edm::Event& e, const edm::EventSetup& c) ;
+  void analyze(const edm::Event& event, const edm::EventSetup& eventSetup) ;
 
-  void beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
-                            const edm::EventSetup& context) ;
+  void beginLuminosityBlock(const edm::LuminosityBlock& lumiBlock, 
+                            const edm::EventSetup& eventSetup) ;
 
   /// DQM Client Diagnostic
-  void endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
-                          const edm::EventSetup& c);
+  void endLuminosityBlock(const edm::LuminosityBlock& lumiBlock, 
+                          const edm::EventSetup& eventSetup);
 
   /// EndRun
-  void endRun(const edm::Run& r, const edm::EventSetup& c);
+  void endRun(const edm::Run& run, const edm::EventSetup& eventSetup);
 
   /// Endjob
   void endJob();
@@ -64,6 +64,7 @@ private:
   int nTrig_;		/// mutriggered events
   int prescaleEvt_;     ///every n events
   double coneSize_;
+  int nl3muon, nl3muonOIState, nl3muonOIHit, nl3muonIOHit;
   edm::InputTag l2collectionTag_;
   edm::InputTag l2isolationTag_;
   edm::InputTag l2seedscollectionTag_;
@@ -78,6 +79,23 @@ private:
   std::vector<std::string> theHLTCollectionLevel;
   std::string striggers_[20];
  
+  edm::InputTag l3seedscollectionTagOIState_;
+  edm::InputTag l3seedscollectionTagOIHit_;
+  edm::InputTag l3seedscollectionTagIOHit_;
+
+  edm::InputTag l3trkfindingOIState_;
+  edm::InputTag l3trkfindingOIHit_;
+  edm::InputTag l3trkfindingIOHit_;
+
+  edm::InputTag l3trkOIState_;
+  edm::InputTag l3trkOIHit_;
+  edm::InputTag l3trkIOHit_;
+  edm::InputTag l3tktrk_;
+
+  edm::InputTag l3muons_;
+  edm::InputTag l3muonsOIState_;
+  edm::InputTag l3muonsOIHit_;
+  edm::InputTag l3muonsIOHit_;
   // ----------member data ---------------------------
   bool verbose_;
   static const int NTRIG = 20;
@@ -111,9 +129,12 @@ private:
   MonitorElement * hnHits[NTRIG][4];
   MonitorElement * hnValidHits[NTRIG];
   MonitorElement * hnTkValidHits[NTRIG];
+  MonitorElement * hnPixelValidHits[NTRIG];
+  MonitorElement * hnStripValidHits[NTRIG];
   MonitorElement * hnMuValidHits[NTRIG];
   MonitorElement * hdimumass[NTRIG][2];
-  MonitorElement * hiso[NTRIG][2];
+  MonitorElement * hisoL2[NTRIG];
+  MonitorElement * hisoL3[NTRIG];
   MonitorElement * hl1quality[NTRIG];
   MonitorElement * hptfrac[NTRIG][2];
   MonitorElement * hetafrac[NTRIG][2];
@@ -155,11 +176,105 @@ private:
   MonitorElement * htowerdRL2[NTRIG];
   MonitorElement * htowerdRL3[NTRIG];
   MonitorElement * hL2muonIsoDR[NTRIG];
+  MonitorElement * hL3muonIsoDR[NTRIG];
+
   float XMIN; float XMAX;
 
   TH1D *_hpt1[NTRIG][2], *_hpt2[NTRIG][2];
   TH1D *_heta1[NTRIG][2], *_heta2[NTRIG][2];
   TH1D *_hphi1[NTRIG][2], *_hphi2[NTRIG][2];
+
+  MonitorElement * hd0_OIState[NTRIG][5];
+  MonitorElement * hd0_OIHit[NTRIG][5];
+  MonitorElement * hd0_IOHit[NTRIG][5];
+  MonitorElement * hdz_OIState[NTRIG][5];
+  MonitorElement * hdz_OIHit[NTRIG][5];
+  MonitorElement * hdz_IOHit[NTRIG][5];
+  MonitorElement * hdr_OIState[NTRIG][5];
+  MonitorElement * hdr_OIHit[NTRIG][5];
+  MonitorElement * hdr_IOHit[NTRIG][5];
+
+  MonitorElement * hNMu_comp[NTRIG][5];
+  MonitorElement * hNMu_trk_comp[NTRIG][5];
+  MonitorElement * hNMu_l3seed_comp[NTRIG][5];
+
+// OIState,OIHit,IOHit
+  MonitorElement * hNMu_OIState[NTRIG][5];
+  MonitorElement * hNMu_OIHit[NTRIG][5];
+  MonitorElement * hNMu_IOHit[NTRIG][5];
+  MonitorElement * hpt_OIState[NTRIG][5];
+  MonitorElement * hpt_OIHit[NTRIG][5];
+  MonitorElement * hpt_IOHit[NTRIG][5];
+  MonitorElement * heta_OIState[NTRIG][5];
+  MonitorElement * heta_OIHit[NTRIG][5];
+  MonitorElement * heta_IOHit[NTRIG][5];
+  MonitorElement * hphi_OIState[NTRIG][5];
+  MonitorElement * hphi_OIHit[NTRIG][5];
+  MonitorElement * hphi_IOHit[NTRIG][5];
+  MonitorElement * hpteta_OIState[NTRIG][5];
+  MonitorElement * hpteta_OIHit[NTRIG][5];
+  MonitorElement * hpteta_IOHit[NTRIG][5];
+  MonitorElement * hetaphi_OIState[NTRIG][5];
+  MonitorElement * hetaphi_OIHit[NTRIG][5];
+  MonitorElement * hetaphi_IOHit[NTRIG][5];
+  MonitorElement * hptphi_OIState[NTRIG][5];
+  MonitorElement * hptphi_OIHit[NTRIG][5];
+  MonitorElement * hptphi_IOHit[NTRIG][5];
+  MonitorElement * hcharge_OIState[NTRIG][5];
+  MonitorElement * hcharge_OIHit[NTRIG][5];
+  MonitorElement * hcharge_IOHit[NTRIG][5];
+// tracker
+  MonitorElement * hNMu_trk[NTRIG][5];
+  MonitorElement * hcharge_trk[NTRIG][5];
+  MonitorElement * hpt_trk[NTRIG][5];
+  MonitorElement * heta_trk[NTRIG][5];
+  MonitorElement * hphi_trk[NTRIG][5];
+  MonitorElement * hpteta_trk[NTRIG][5];
+  MonitorElement * hptphi_trk[NTRIG][5];
+  MonitorElement * hetaphi_trk[NTRIG][5];
+  MonitorElement * hd0_trk[NTRIG][5];
+  MonitorElement * hdz_trk[NTRIG][5];
+  MonitorElement * hdr_trk[NTRIG][5];
+  MonitorElement * hNMu_trk_OIState[NTRIG][5];
+  MonitorElement * hcharge_trk_OIState[NTRIG][5];
+  MonitorElement * hpt_trk_OIState[NTRIG][5];
+  MonitorElement * heta_trk_OIState[NTRIG][5];
+  MonitorElement * hphi_trk_OIState[NTRIG][5];
+  MonitorElement * hpteta_trk_OIState[NTRIG][5];
+  MonitorElement * hptphi_trk_OIState[NTRIG][5];
+  MonitorElement * hetaphi_trk_OIState[NTRIG][5];
+  MonitorElement * hd0_trk_OIState[NTRIG][5];
+  MonitorElement * hdz_trk_OIState[NTRIG][5];
+  MonitorElement * hdr_trk_OIState[NTRIG][5];
+  MonitorElement * hNMu_trk_OIHit[NTRIG][5];
+  MonitorElement * hcharge_trk_OIHit[NTRIG][5];
+  MonitorElement * hpt_trk_OIHit[NTRIG][5];
+  MonitorElement * heta_trk_OIHit[NTRIG][5];
+  MonitorElement * hphi_trk_OIHit[NTRIG][5];
+  MonitorElement * hpteta_trk_OIHit[NTRIG][5];
+  MonitorElement * hptphi_trk_OIHit[NTRIG][5];
+  MonitorElement * hetaphi_trk_OIHit[NTRIG][5];
+  MonitorElement * hd0_trk_OIHit[NTRIG][5];
+  MonitorElement * hdz_trk_OIHit[NTRIG][5];
+  MonitorElement * hdr_trk_OIHit[NTRIG][5];
+  MonitorElement * hNMu_trk_IOHit[NTRIG][5];
+  MonitorElement * hcharge_trk_IOHit[NTRIG][5];
+  MonitorElement * hpt_trk_IOHit[NTRIG][5];
+  MonitorElement * heta_trk_IOHit[NTRIG][5];
+  MonitorElement * hphi_trk_IOHit[NTRIG][5];
+  MonitorElement * hpteta_trk_IOHit[NTRIG][5];
+  MonitorElement * hptphi_trk_IOHit[NTRIG][5];
+  MonitorElement * hetaphi_trk_IOHit[NTRIG][5];
+  MonitorElement * hd0_trk_IOHit[NTRIG][5];
+  MonitorElement * hdz_trk_IOHit[NTRIG][5];
+  MonitorElement * hdr_trk_IOHit[NTRIG][5];
+
+  MonitorElement * hptres_L3L3trk[NTRIG][5];
+  MonitorElement * hetares_L3L3trk[NTRIG][5];
+  MonitorElement * hphires_L3L3trk[NTRIG][5];
+  MonitorElement * hptrelres_L3L3trk[NTRIG][5];
+  MonitorElement * hetarelres_L3L3trk[NTRIG][5];
+  MonitorElement * hphirelres_L3L3trk[NTRIG][5];
 };
 
 #endif
