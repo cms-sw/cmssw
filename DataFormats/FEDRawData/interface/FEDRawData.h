e2932d024473a6e654dd8174b371264dd309781e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-03-12 14:22:47 +0000

    added cstddef

--- DataFormats/FEDRawData/interface/FEDRawData.h
+++ DataFormats/FEDRawData/interface/FEDRawData.h
@@ -8,14 +8,16 @@
  *  lenght of the data is a multiple of the S-Link64 word lenght (8 byte).
  *  The FED data should include the standard FED header and trailer.
  *
- *  $Date: 2005/10/06 18:25:22 $
- *  $Revision: 1.5 $
+ *  $Date: 2005/10/18 13:28:12 $
+ *  $Revision: 1.6 $
  *  \author G. Bruno - CERN, EP Division
  *  \author S. Argiro - CERN and INFN - 
  *                      Refactoring and Modifications to fit into CMSSW
  */   
 
+
 #include <vector>
+#include <cstddef>
 
 class FEDRawData {
 
