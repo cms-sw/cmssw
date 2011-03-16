1ac510e948d48aa34d0e59590bf8d8b1ac7f684c
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-03-16 15:56:53 +0000

    more tests

--- DataFormats/Math/test/MulSymMatrix_t.cpp
+++ DataFormats/Math/test/MulSymMatrix_t.cpp
@@ -3,6 +3,24 @@
 
 typedef unsigned int IndexType;
 //typedef unsigned long long IndexType;
+
+
+namespace ROOT { 
+
+  namespace Math { 
+
+    template <class T,  IndexType D1,  IndexType D2, class R>
+    inline SMatrix<T,D1,D1,MatRepSym<T,D1> > Similarity(const SMatrix<T,D1,D2,R>& lhs, const SMatrix<T,D2,D2,MatRepStd<T,D2> >& rhs) {
+      SMatrix<T,D1,D2, MatRepStd<T,D1,D2> > tmp = lhs * rhs;
+      typedef  SMatrix<T,D1,D1,MatRepSym<T,D1> > SMatrixSym; 
+      SMatrixSym mret; 
+      AssignSym::Evaluate(mret,  tmp * Transpose(lhs)  ); 
+      return mret; 
+    }
+
+  }
+}
+
 template<typename T, IndexType N>
 inline void
 mult(ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepSym<T,N> > & a, 
@@ -73,6 +91,11 @@ similarity(ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepStd<T,N> > & b,
 	b(i,j) += u(i,k)*s[k];
     }
   */
+
+ for (IndexType i=0; i!=N-1; ++i) 
+    for (IndexType j=i+1; j!=N; ++j)
+      b(i,j)=b(j,i);
+ 
 }
 
 // U(k,i) * A(k,l) * U(l,j)
@@ -86,6 +109,11 @@ similarityT(ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepStd<T,N> > & b,
       for (IndexType k=0; k!=N; ++k) 
 	for (IndexType l=0; l!=N; ++l) 
 	  b(i,j) += u(k,i)*a(k,l)*u(l,j);
+
+  for (IndexType i=0; i!=N-1; ++i) 
+    for (IndexType j=i+1; j!=N; ++j)
+      b(i,j)=b(j,i);
+
 }
 
 template<typename M1, typename M2>
@@ -98,7 +126,14 @@ double eps(M1 const & m1, M2 const & m2) {
   return ret;
 }
 
-
+template<typename M>
+bool isSym(M const & m) {
+  IndexType N = M::kRows;
+  for (IndexType i=0; i!=N; ++i) 
+    for (IndexType j=0; j<=i; ++j)
+      if (m(i,j)!=m(j,i)) return false;
+  return true;
+}
 
 #include<iostream>
 #include "FWCore/Utilities/interface/HRRealTime.h"
@@ -111,9 +146,14 @@ namespace {
   template<typename T, IndexType N>
   inline void
   fillRandom(ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepStd<T,N> > & a) {
-    for (IndexType k=0; k!=N; ++k)
-      for (IndexType j=0; j!=N; ++j)
+    for (IndexType k=0; k!=N; ++k) {
+      a(k,k) =  std::abs(rgen(eng));
+      for (IndexType j=0; j<k; ++j)
 	a(k,j) =  rgen(eng);
+    }
+    for (IndexType i=0; i!=N-1; ++i) 
+      for (IndexType j=i+1; j!=N; ++j)
+	a(i,j) = - a(j,i);
   }
 }
 
@@ -121,44 +161,65 @@ template<typename T, IndexType N>
 void go( edm::HRTimeType & s1, edm::HRTimeType & s2,  
 	 edm::HRTimeType & s3,  edm::HRTimeType & s4,  
 	 edm::HRTimeType & s5,
-	 edm::HRTimeType & s6,  edm::HRTimeType & s7,
+	 edm::HRTimeType & s6,  edm::HRTimeType & s7,  edm::HRTimeType & s8,
 	 bool print) {
   typedef ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepStd<T,N> > Matrix;
   typedef ROOT::Math::SMatrix<T,N,N,ROOT::Math::MatRepSym<T,N> > SymMatrix;
 
   ROOT::Math::SMatrixIdentity id;
 
-  SymMatrix is(id);
-  Matrix im(id);
+  Matrix im1(id);
+  Matrix im2(id);
+  /*
+  fillRandom(im1);
+  im1 = im1*ROOT::Math::Transpose(im1);
+  im1 *=im1;
+
+  Matrix im2; fillRandom(im2);
+  im2 = im2*ROOT::Math::Transpose(im2);
+  im2 *=im2;
+  */
+
+  SymMatrix is1; ROOT::Math::AssignSym::Evaluate(is1, im1);
+  SymMatrix is2; ROOT::Math::AssignSym::Evaluate(is2, im2);
 
   Matrix j1;  fillRandom(j1);
   Matrix j2;  fillRandom(j2);
 
   SymMatrix rh;
   s6 = edm::hrRealTime();
-  rh =  ROOT::Math::SimilarityT(j1, is);
+  rh =  ROOT::Math::Similarity(j1, is1);
   s6 =  edm::hrRealTime() -s6;
 
-  Matrix mlh;
+  SymMatrix rh2;
   s7 = edm::hrRealTime();
-  similarityT(mlh,j2,im);
+  rh2 =  ROOT::Math::Similarity(j1, im1);
   s7 =  edm::hrRealTime() -s7;
 
-  SymMatrix lh = ROOT::Math::SimilarityT(j2, is);
 
-  Matrix mrh = rh;
+  Matrix mrh;
+  s8 = edm::hrRealTime();
+  similarity(mrh,j1,im1);
+  s8 =  edm::hrRealTime() -s8;
 
 
+ 
+  SymMatrix lh = ROOT::Math::SimilarityT(j2, is1);
+
+  Matrix mlh;  similarityT(mlh,j2,im1);
+
+
+ 
 
 
   SymMatrix a; 
   s1 = edm::hrRealTime();
-  mult(a,rh,lh);
+  mult(a,lh,rh);
   s1 =  edm::hrRealTime() -s1;
 
   SymMatrix b;
   s2 = edm::hrRealTime();
-  ROOT::Math::AssignSym::Evaluate(b, rh*lh);
+  ROOT::Math::AssignSym::Evaluate(b, lh*rh);
   s2 =  edm::hrRealTime() -s2;
 
   Matrix m0 = b;
@@ -166,17 +227,17 @@ void go( edm::HRTimeType & s1, edm::HRTimeType & s2,
 
   Matrix m;
   s3 = edm::hrRealTime();
-  m = mrh*mlh;
+  m = mlh*mrh;
   s3 =  edm::hrRealTime() -s3;
 
   SymMatrix sm;
   s4 = edm::hrRealTime();
-  ROOT::Math::AssignSym::Evaluate(sm,mrh*mlh);
+  ROOT::Math::AssignSym::Evaluate(sm,mlh*mrh);
   s4 =  edm::hrRealTime() -s4;
 
   Matrix m2;
   s5 = edm::hrRealTime();
-  mult(m2,mrh,mlh);
+  mult(m2,mlh,mrh);
   s5 =  edm::hrRealTime() -s5;
 
 
@@ -184,8 +245,15 @@ void go( edm::HRTimeType & s1, edm::HRTimeType & s2,
   SymMatrix smm2; ROOT::Math::AssignSym::Evaluate(smm2,m2);
 
   if (print) {
-    std::cout << "eps as   " << eps(rh,mrh) << std::endl;
-    std::cout << "eps sim  " << eps(lh,mlh) << std::endl;
+    if (!isSym(im1))  std::cout << " im is not sym" << std::endl;
+    if (!isSym(mrh)) std::cout << " rh is not sym" << std::endl;
+    if (!isSym(mlh)) std::cout << " lh is not sym" << std::endl;
+    if (!isSym(m)) std::cout << " m is not sym" << std::endl;
+    if (!isSym(m2)) std::cout << "m2 is not sym" << std::endl;
+
+    std::cout << "eps sim  " << eps(rh,mrh) << std::endl;
+    std::cout << "eps sim  " << eps(rh,rh2) << std::endl;
+    std::cout << "eps simT " << eps(lh,mlh) << std::endl;
     std::cout << "eps s m  " << eps(m,b) << std::endl;
     std::cout << "eps s sm " << eps(m2,b) << std::endl;
 
@@ -206,33 +274,35 @@ void go( edm::HRTimeType & s1, edm::HRTimeType & s2,
     std::cout << "mat as sym " << s4 << std::endl;
     std::cout << "mat mult   " << s5 << std::endl;
     std::cout << "sym  sim   " << s6 << std::endl;
-    std::cout << "loop sim   " << s7 << std::endl;
+    std::cout << "std  sim   " << s7 << std::endl;
+    std::cout << "loop sim   " << s8 << std::endl;
   
   }
 
 }
  
 int main() {
-  edm::HRTimeType s1=0, s2=0, s3=0, s4=0, s5=0, s6=0, s7=0;
-  go<double,3>(s1,s2,s3,s4,s5,s6,s7, true);
+  edm::HRTimeType s1=0, s2=0, s3=0, s4=0, s5=0, s6=0, s7=0, s8=0;
+  go<double,3>(s1,s2,s3,s4,s5,s6,s7,s8, true);
  
 
- go<double,15>(s1,s2,s3,s4,s5,s6,s7, true);
+  go<double,15>(s1,s2,s3,s4,s5,s6,s7,s8, true);
 
   edm::HRTimeType t1=0; edm::HRTimeType t2=0;  edm::HRTimeType t3=0; edm::HRTimeType t4=0; edm::HRTimeType t5=0;
-  edm::HRTimeType t6=0; edm::HRTimeType t7=0;
+  edm::HRTimeType t6=0; edm::HRTimeType t7=0;  edm::HRTimeType t8=0;
 
   for (int  i=0; i!=50000; ++i) {
-    go<double,15>(s1,s2,s3,s4,s5,s6,s7, false);
-    t1+=s1; t2+=s2; t3+=s3; t4+=s4; t5+=s5;  t6+=s6; t7+=s7;
+    go<double,15>(s1,s2,s3,s4,s5,s6,s7,s8, false);
+    t1+=s1; t2+=s2; t3+=s3; t4+=s4; t5+=s5;  t6+=s6; t7+=s7; t8+=s8;
   }
   std::cout << "sym mult   " << t1/50000 << std::endl;
   std::cout << "sym    *   " << t2/50000 << std::endl;
   std::cout << "mat    *   " << t3/50000 << std::endl;
   std::cout << "mat as sym " << t4/50000 << std::endl;
   std::cout << "mat mult   " << t5/50000 << std::endl;
-    std::cout << "sym  sim " << t6/50000 << std::endl;
-    std::cout << "loop sim " << t7/50000 << std::endl;
+  std::cout << "sym  sim " << t6/50000 << std::endl;
+  std::cout << "std  sim " << t7/50000 << std::endl;
+  std::cout << "loop sim " << t8/50000 << std::endl;
  
   return 0;
 }
