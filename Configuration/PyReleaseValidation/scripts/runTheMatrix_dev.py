dd339bb1241b85ce8e44431c373ccd5ea871e0aa
Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch>
2011-03-16 18:27:32 +0000

    set default for step2 to be 100 events, step1 still 10

--- Configuration/PyReleaseValidation/scripts/runTheMatrix_dev.py
+++ Configuration/PyReleaseValidation/scripts/runTheMatrix_dev.py
@@ -227,15 +227,16 @@ class WorkFlow(object):
         self.numId  = num.strip()
         self.nameId = nameID
         self.cmdStep1 = self.check(cmd1)
-        self.cmdStep2 = self.check(cmd2)
-        self.cmdStep3 = self.check(cmd3)
-        self.cmdStep4 = self.check(cmd4)
+        self.cmdStep2 = self.check(cmd2, 100)
+        self.cmdStep3 = self.check(cmd3, 100)
+        self.cmdStep4 = self.check(cmd4, 100)
 
         # run on real data requested:
         self.input = inputInfo
+
         return
 
-    def check(self, cmd=None):
+    def check(self, cmd=None, nEvtDefault=10):
         if not cmd : return None
 
         # raw data are treated differently ...
@@ -245,7 +246,7 @@ class WorkFlow(object):
         reN = re.compile('\s*-n\s*\d+\s*')
         newCmd = reN.sub(' -n 10 ', cmd)
         if not reN.match(newCmd) : # -n not specified, add it:
-            newCmd += ' -n 10 '
+            newCmd += ' -n '+str(nEvtDefault)+' '
 
         return newCmd
 
