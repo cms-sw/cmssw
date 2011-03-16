0cab78941b7b7ec4cabcf0d3fc2e8062b97508e8
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2011-03-16 13:03:53 +0000

    Reduce verbosity at warning level for L1 algos in 2011A data, until reason is identified.

--- PhysicsTools/PatAlgos/plugins/PATTriggerProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATTriggerProducer.cc
@@ -1,5 +1,5 @@
 //
-// $Id: PATTriggerProducer.cc,v 1.24 2010/11/27 15:16:20 vadler Exp $
+// $Id: PATTriggerProducer.cc,v 1.25 2011/02/22 18:29:50 vadler Exp $
 //
 
 
@@ -807,9 +807,9 @@ void PATTriggerProducer::produce( Event& iEvent, const EventSetup& iSetup )
         }
 //         if ( ( l1ObjectMap->algoGtlResult() != decisionBeforeMask ) && ( decisionBeforeMask == true || prescale == 1 ) ) {
         if ( ( l1ObjectMap->algoGtlResult() != decisionBeforeMask ) && ( decisionBeforeMask == true ) ) { // FIXME: understand the difference for un-prescaled algos 118, 119, 123
-          LogWarning( "l1ObjectMap" ) << "L1 physics algorithm '" << algoName << "' with different decisions in\n"
-                                      << "L1GlobalTriggerObjectMapRecord (GTL result)        : " << l1ObjectMap->algoGtlResult() << "\n"
-                                      << "L1GlobalTriggerReadoutRecord (decision before mask): " << decisionBeforeMask;
+          LogInfo( "l1ObjectMap" ) << "L1 physics algorithm '" << algoName << "' with different decisions in\n"
+                                   << "L1GlobalTriggerObjectMapRecord (GTL result)        : " << l1ObjectMap->algoGtlResult() << "\n"
+                                   << "L1GlobalTriggerReadoutRecord (decision before mask): " << decisionBeforeMask;
         }
         triggerAlgo.setGtlResult( l1ObjectMap->algoGtlResult() );
         // conditions in algorithm
