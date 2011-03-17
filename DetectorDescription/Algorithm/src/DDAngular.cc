34a4d3bda869b3645088701c8c880f994190506b
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2011-03-17 15:31:57 +0000

    Fix rotation of the solid.

--- DetectorDescription/Algorithm/src/DDAngular.cc
+++ DetectorDescription/Algorithm/src/DDAngular.cc
@@ -28,6 +28,8 @@ DDAngular::initialize( const DDNumericArguments & nArgs,
   m_radius      = nArgs["Radius"];
   m_center      = vArgs["Center"];
   m_rotateSolid = vArgs["RotateSolid"];
+  m_idNameSpace = DDCurrentNamespace::ns();
+  m_childName   = sArgs["ChildName"]; 
 
   m_solidRot   = DDRotationMatrix();
   
@@ -57,8 +59,6 @@ DDAngular::initialize( const DDNumericArguments & nArgs,
   //======= collect data concerning the rotation of the solid 
   typedef parE_type::mapped_type::size_type sz_type;
   sz_type sz = m_rotateSolid.size();
-  m_rotateSolid.clear();
-  m_rotateSolid.resize( sz );
   if( sz%3 )
   {
     LogDebug( "DDAlgorithm" ) << "\trotateSolid must occur 3*n times (defining n subsequent rotations)\n"
@@ -72,16 +72,12 @@ DDAngular::initialize( const DDNumericArguments & nArgs,
 				<< "\t  currently it is " << m_rotateSolid[i]/deg 
 				<< "*deg in rotateSolid[" << double(i) << "]!\n";
     }
-    
     DDAxisAngle temp( fUnitVector( m_rotateSolid[i], m_rotateSolid[i + 1] ),
 		      m_rotateSolid[i + 2] );
     LogDebug( "DDAlgorithm" ) << "  rotsolid[" << i <<  "] axis=" << temp.Axis() << " rot.angle=" << temp.Angle()/deg;
     m_solidRot = temp * m_solidRot;			  
   }
-  
-  m_idNameSpace = DDCurrentNamespace::ns();
-  m_childName   = sArgs["ChildName"]; 
-  
+
   DDName parentName = parent().name();
   LogDebug( "DDAlgorithm" ) << "DDAngular debug: Parent " << parentName 
 			    << "\tChild " << m_childName << " NameSpace "
@@ -95,6 +91,7 @@ DDAngular::execute( DDCompactView& cpv )
   double theta  = 90.*CLHEP::deg;
   int    copy   = m_startCopyNo;
   double phi    = m_startAngle;
+
   for( int i = 0; i < m_n; ++i )
   {
     double phix = phi;
@@ -102,21 +99,18 @@ DDAngular::execute( DDCompactView& cpv )
     double phideg = phix / CLHEP::deg;
 
     DDRotation rotation;
-
-    if( phideg != 0 )
+    
+    std::string rotstr = DDSplit( m_childName ).first + dbl_to_string( phideg * 10.);
+    rotation = DDRotation( DDName( rotstr, m_idNameSpace ));
+    if( !rotation )
     {
-      std::string rotstr = DDSplit( m_childName ).first + dbl_to_string( phideg * 10.);
-      rotation = DDRotation( DDName( rotstr, m_idNameSpace ));
-      if( !rotation )
-      {
-	LogDebug( "DDAlgorithm" ) << "DDAngular test: Creating a new "
-				  << "rotation: " << rotstr << "\t90., " 
-				  << phix / CLHEP::deg << ", 90.," 
-				  << phiy / CLHEP::deg << ", 0, 0";
+      LogDebug( "DDAlgorithm" ) << "DDAngular test: Creating a new "
+				<< "rotation: " << rotstr << "\t90., " 
+				<< phix / CLHEP::deg << ", 90.," 
+				<< phiy / CLHEP::deg << ", 0, 0";
 	
- 	rotation = DDrot( DDName( rotstr, m_idNameSpace), new DDRotationMatrix( *DDcreateRotationMatrix( theta, phix, theta, phiy,
-													 0., 0. ) * m_solidRot )); 
-      }
+      rotation = DDrot( DDName( rotstr, m_idNameSpace), new DDRotationMatrix(( *DDcreateRotationMatrix( theta, phix, theta, phiy,
+													  0., 0. ) * m_solidRot ))); 
     }
       
     double xpos = m_radius * cos( phi ) + m_center[0];
@@ -127,7 +121,7 @@ DDAngular::execute( DDCompactView& cpv )
     cpv.position( DDName( m_childName, m_idNameSpace ), mother, copy, tran, rotation );
     LogDebug( "DDAlgorithm" ) << "DDAngular test " << m_childName << " number " 
 			      << copy << " positioned in " << mother << " at "
-			      << tran << " with " << rotation;
+			      << tran << " with " << rotation << "\n";
     copy += m_incrCopyNo;
     phi  += m_delta;
   }
