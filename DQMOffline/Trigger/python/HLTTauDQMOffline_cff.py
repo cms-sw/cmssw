49c460efef699541243c7d341d3d39289a20da8c
Joshua James Swanson <sha1-8881b0a90e2d698b47914ccde7f42725e686d8ab@cern.ch>
2011-03-18 15:15:21 +0000

    Update to 5E32

--- DQMOffline/Trigger/python/HLTTauDQMOffline_cff.py
+++ DQMOffline/Trigger/python/HLTTauDQMOffline_cff.py
@@ -7,8 +7,6 @@ from DQMOffline.Trigger.HLTTauCertifier_cfi import *
 
 HLTTauDQMOffline = cms.Sequence(TauRefProducer+
                                 hltTauOfflineMonitor_PFTaus
-                                +hltTauOfflineMonitor_Photons
-                                +hltTauOfflineMonitor_HPD
                                 +hltTauOfflineMonitor_Inclusive
                                 )
 
--- DQMOffline/Trigger/python/HLTTauPostProcessor_cfi.py
+++ DQMOffline/Trigger/python/HLTTauPostProcessor_cfi.py
@@ -4,31 +4,22 @@ HLTTauPostAnalysis = cms.EDAnalyzer("HLTTauPostProcessor",
   Harvester = cms.PSet(
     L1Dirs                  = cms.vstring(
     "HLT/TauOffline/Inclusive/L1",
-    "HLT/TauOffline/PFTaus/L1",
-    "HLT/TauOffline/Photons/L1",
-    "HLT/TauOffline/HPD/L1"
+    "HLT/TauOffline/PFTaus/L1"
     ),
     caloDirs                = cms.vstring(
-    "HLT/TauOffline/Inclusive/L2",
-    "HLT/TauOffline/PFTaus/L2",
-    "HLT/TauOffline/Photons/L2",
-    "HLT/TauOffline/HPD/L2"
+
     ),
     trackDirs               = cms.vstring(
 
     ),
     pathDirs                = cms.vstring(
     "HLT/TauOffline/PFTaus/DoubleLooseIsoTau",
-    "HLT/TauOffline/Photons/DoubleLooseIsoTau",
-    "HLT/TauOffline/HPD/DoubleLooseIsoTau",
     "HLT/TauOffline/PFTaus/SingleLooseIsoTau",
-    "HLT/TauOffline/Photons/SingleLooseIsoTau",
-    "HLT/TauOffline/HPD/SingleLooseIsoTau"
+    "HLT/TauOffline/PFTaus/EleTau",
+    "HLT/TauOffline/PFTaus/MuTau",
     ),
     pathSummaryDirs         = cms.vstring(
     "HLT/TauOffline/PFTaus/Summary",
-    "HLT/TauOffline/Photons/Summary",
-    "HLT/TauOffline/HPD/Summary"
     )
   )
 )
