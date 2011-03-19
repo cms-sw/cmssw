0d761f7d7f69ef2e933da04a4f9665751f802466
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2011-03-19 10:54:22 +0000

    add new btag module

--- DQM/HLTEvF/python/HLTMonitor_cff.py
+++ DQM/HLTEvF/python/HLTMonitor_cff.py
@@ -13,7 +13,7 @@ from DQM.HLTEvF.HLTMonJetMETDQMSource_cff import *
 # muon
 from DQM.HLTEvF.HLTMonMuonDQM_cff import *
 from DQM.HLTEvF.HLTLocalRecoDT_cff import *
-#from DQM.HLTEvF.HLTLocalRecoRPC_cff import *
+from DQM.HLTEvF.HLTLocalRecoRPC_cff import *
 from DQM.HLTEvF.HLTLocalRecoSiStrip_cff import *
 from DQM.HLTEvF.HLTLocalRecoSiPixel_cff import *
 # onia
@@ -24,3 +24,4 @@ from DQM.HLTEvF.HLTMonAlCaPhySim_cff import *
 from DQM.HLTEvF.HLTMonHcalIsoTrack_cff import *
 # btag
 from DQM.HLTEvF.hltMonBTag_cff import *
+from DQM.HLTEvF.HLTMonSimpleBTag_cff import *
