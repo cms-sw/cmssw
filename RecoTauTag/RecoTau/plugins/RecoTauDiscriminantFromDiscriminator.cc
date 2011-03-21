3e053df5fd989e285586a5cd14c8b43a26d1ab5f
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-21 11:10:29 +0000

    Allow the plugin to capture and clean corrupt inputs from track impact parameter calculation

--- RecoTauTag/RecoTau/plugins/RecoTauDiscriminantFromDiscriminator.cc
+++ RecoTauTag/RecoTau/plugins/RecoTauDiscriminantFromDiscriminator.cc
@@ -23,12 +23,22 @@ class RecoTauDiscriminantFromDiscriminator : public RecoTauDiscriminantPlugin{
     void beginEvent();
     std::vector<double> operator()(const reco::PFTauRef& tau) const;
   private:
+    bool takeAbs_;
+    double min_;
+    double max_;
     typedef std::pair<edm::InputTag, edm::Handle<reco::PFTauDiscriminator> > DiscInfo;
     std::vector<DiscInfo> discriminators_;
 };
 
 RecoTauDiscriminantFromDiscriminator::RecoTauDiscriminantFromDiscriminator(
     const edm::ParameterSet& pset):RecoTauDiscriminantPlugin(pset) {
+  takeAbs_ = pset.existsAs<bool>("takeAbs") ? 
+    pset.getParameter<bool>("takeAbs") : false;
+  min_ = pset.existsAs<double>("minValue") ? 
+    pset.getParameter<double>("minValue") : -1*std::numeric_limits<double>::max();
+  max_ = pset.existsAs<double>("maxValue") ? 
+    pset.getParameter<double>("maxValue") : std::numeric_limits<double>::max();
+
   if (pset.existsAs<edm::InputTag>("discSrc")) {
     discriminators_.push_back(std::make_pair(
           pset.getParameter<edm::InputTag>("discSrc"),
@@ -55,29 +65,39 @@ void RecoTauDiscriminantFromDiscriminator::beginEvent() {
 std::vector<double> RecoTauDiscriminantFromDiscriminator::operator()(
     const reco::PFTauRef& tau) const {
   edm::ProductID tauProdId = tau.id();
+  double result = -999;
+  bool foundGoodDiscriminator = false;
   for (size_t i = 0; i < discriminators_.size(); ++i) {
     // Check if the discriminator actually exists
     if (!discriminators_[i].second.isValid())
       continue;
     const reco::PFTauDiscriminator& disc = *(discriminators_[i].second);
-    if (tauProdId == disc.keyProduct().id())
-      return std::vector<double>(1, (disc)[tau]);
-  }
-  // Can only reach this point if not appropriate discriminator is defined for
-  // the passed tau.
-  std::stringstream error;
-  error << "Couldn't find a PFTauDiscriminator usable with given tau."
-    << std::endl << " Input tau has product id: " << tau.id() << std::endl;
-  for (size_t i = 0; i < discriminators_.size(); ++i ) {
-    error << "disc: " << discriminators_[i].first;
-    error << " isValid: " << discriminators_[i].second.isValid();
-    if (discriminators_[i].second.isValid()) {
-      error << " product: " << discriminators_[i].second->keyProduct().id();
+    if (tauProdId == disc.keyProduct().id()) {
+      foundGoodDiscriminator = true;
+      result = (disc)[tau];
+      break;
     }
-    error << std::endl;
   }
-  edm::LogError("BadDiscriminatorConfiguration") << error.str();
-  return std::vector<double>(1,-999);
+  // In case no discriminator is found.
+  if (!foundGoodDiscriminator) {
+    std::stringstream error;
+    error << "Couldn't find a PFTauDiscriminator usable with given tau."
+      << std::endl << " Input tau has product id: " << tau.id() << std::endl;
+    for (size_t i = 0; i < discriminators_.size(); ++i ) {
+      error << "disc: " << discriminators_[i].first;
+      error << " isValid: " << discriminators_[i].second.isValid();
+      if (discriminators_[i].second.isValid()) {
+        error << " product: " << discriminators_[i].second->keyProduct().id();
+      }
+      error << std::endl;
+    }
+    edm::LogError("BadDiscriminatorConfiguration") << error.str();
+  }
+  if (result < min_)
+    result = min_;
+  if (result > max_)
+    result = max_;
+  return std::vector<double>(1, result);
 }
 
 }} // end namespace reco::tau
