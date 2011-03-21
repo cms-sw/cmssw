613ba07d45e8e34daa6992a9c19848241ab46da6
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-03-21 11:26:19 +0000

    More reasonable number of toys Keep fork to a maximum of 2 in fast test suite (otherwise when running multiple jobs at a time you can kill a machine)

--- HiggsAnalysis/CombinedLimit/test/validation/test_HN.py
+++ HiggsAnalysis/CombinedLimit/test/validation/test_HN.py
@@ -24,10 +24,10 @@ suite += [ (M, '*', MultiOptionTest("HWW_TestStats", "hww4ch-1fb-B-mH140.txt", M
 ### Test the p-values 
 for R in [ 'CLs', 'CLsplusb' ]:
     suite += [ (M, 'fast', MultiOptionTest("Counting_pValues_%s" % R, "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M,
-                            "--singlePoint 5 --fork 4 -T 500 --clsAcc=0.01  --rule=%s" % R,
+                            "--singlePoint 5 --fork 4 --clsAcc=0.01  --rule=%s" % R,
                             {'Atlas':'--testStat=Atlas', 'LEP':'--testStat=LEP', 'TEV':'--testStat=TEV'})) ]
     suite += [ (M, 'full', MultiOptionTest("Counting_pValues_%s" % R, "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M,
-                            "--singlePoint 5 --fork 4 -T 500 --clsAcc=0.001 --rule=%s" % R,
+                            "--singlePoint 5 --fork 4 --clsAcc=0.001 --rule=%s" % R,
                             {'Atlas':'--testStat=Atlas', 'LEP':'--testStat=LEP', 'TEV':'--testStat=TEV'})) ]
 
 for X in [ "Atlas", "LEP", "TEV" ]:
@@ -36,18 +36,22 @@ for X in [ "Atlas", "LEP", "TEV" ]:
         suite += [ (M, 'full', SingleDatacardTest("HWW_pValues_%s"%X, "hww4ch-1fb-B-mH140.txt", M, 
                         "--singlePoint 2 --fork 6 -T 250 --clsAcc=1 --testStat=%s"%X )) ]
 
-### Test the limits
-options="--fork 4 -T 50 -H ProfileLikelihood"
-suite += [ (M, 'fast', SingleDatacardTest("Counting", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, options)) ]
-suite += [ (M, 'fast', SingleDatacardTest("HWW",      "hww4ch-1fb-B-mH140.txt",                         M, options)) ]
 
-suite += [ (M, 'full', MultiDatacardTest("Counting",  datacardGlob("simple-counting/counting-B5p5-Obs[16]*-S*[Uy].txt"), M, options)) ]
+### Test the limits
+optionsFast="--fork 2 -H ProfileLikelihood"
+optionsFull="--fork 6 -H ProfileLikelihood --rAbsAcc=0.05 --rRelAcc=0.02"
+suite += [ (M, 'fast', MultiDatacardTest("Counting", [ "simple-counting/counting-B5p5-Obs6-Syst30B.txt",
+                                                       "simple-counting/counting-B5p5-Obs1-Syst30B.txt",
+                                                       "simple-counting/counting-B5p5-Obs11-Syst30B.txt"], M, optionsFast)) ]
+suite += [ (M, 'fast', SingleDatacardTest("HWW", "hww4ch-1fb-B-mH140.txt",  M, optionsFast)) ]
+
+suite += [ (M, 'full', MultiDatacardTest("Counting",  datacardGlob("simple-counting/counting-B5p5-Obs[16]*-S*[Uy].txt"), M, optionsFast)) ]
 for d in datacardGlob("hww4ch-1fb-B*mH*.txt"):
     n = re.sub("hww4ch-1fb-(.*mH..0).txt","\\1",d)
-    suite += [ (M, 'full',  SingleDatacardTest('HWW_'+n, d, M, options)) ]
+    suite += [ (M, 'full',  SingleDatacardTest('HWW_'+n, d, M, optionsFull)) ]
 
 ### Custom suite for just the ATLAS test statistics
 suite += [ (M, 'atlas', SingleDatacardTest("TestStat_Atlas", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5 --onlyTestStat --testStat=Atlas")) ]
 suite += [ (M, 'atlas', SingleDatacardTest("TestStat_LEP",   "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5 --onlyTestStat --testStat=LEP"  )) ]
-suite += [ (M, 'atlas', SingleDatacardTest("pValue_Atlas",   "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5                --testStat=Atlas --fork 4 -T 500")) ]
-suite += [ (M, 'atlas', SingleDatacardTest("pValue_LEP",     "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5                --testStat=LEP   --fork 4 -T 500")) ]
+suite += [ (M, 'atlas', SingleDatacardTest("pValue_Atlas",   "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5                --testStat=Atlas --fork 4 ")) ]
+suite += [ (M, 'atlas', SingleDatacardTest("pValue_LEP",     "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, "--singlePoint 5                --testStat=LEP   --fork 4 ")) ]
