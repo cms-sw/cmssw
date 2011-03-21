5bc9cb1ef84e7ad25111ec661ca1949d57d43ed8
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2011-03-21 19:53:45 +0000

    Removed previous fix; concensus is that it can hide other problems

--- DQMOffline/Trigger/plugins/DQMGenericTnPClient.cc
+++ DQMOffline/Trigger/plugins/DQMGenericTnPClient.cc
@@ -184,8 +184,6 @@ DQMGenericTnPClient::~DQMGenericTnPClient(){
 
 void DQMGenericTnPClient::findAllSubdirectories (std::string dir, std::set<std::string> * myList, TString pattern = "") {
 
-  if (!dqmStore->dirExists(dir)) return;
-
   TPRegexp nonPerlWildcard("\\w\\*|^\\*");
 
   if (pattern != "") {
