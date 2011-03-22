6fee0e9f0940d9bc585b45d4187ae868bc51dc0f
Daniel Sherman Riley <sha1-159aefc003aa44e4c9724c4c97b8e9a2524e86c8@cern.ch>
2011-03-22 17:39:32 +0000

    convert to using ProofLite, make runnable within the standard test suite environment

--- FWCore/TFWLiteSelector/test/proof_thing_sel.C
+++ FWCore/TFWLiteSelector/test/proof_thing_sel.C
@@ -1,24 +1,34 @@
 {
-  //Setup the connect to the proof server
-  gROOT->Proof( "lnx7108.lns.cornell.edu" );
-  
-  //Need this to allow ROOT to be able to use a ThingsTSelector
   gSystem->Load("libFWCoreFWLite");
   AutoLibraryLoader::enable();
-  //Have to load library manually since Proof does not use the 
+  
+  if (gSystem->Getenv("tmpdir")) {
+    gEnv->SetValue("Proof.Sandbox", "$tmpdir/proof");
+  }
+
+  //Setup the proof server
+  TProof *gProof=TProof::Open( "" );
+
+  // This makes sure the TSelector library and dictionary are properly
+  // installed in the remote PROOF servers
+
+  // This works, but results in an annoying error message from 'cp',
+  // something not right with the how the macro is sent?
+  //gProof->Exec( ".x proof_remote.C" );
+
+  // So inline it...
+  gProof->Exec("gSystem->Load(\"libFWCoreFWLite\"); "
+               "AutoLibraryLoader::enable(); "
+  // Have to load library manually since Proof does not use the 
   // mechanism used by TFile to find class dictionaries and therefore
   // the AutoLibraryLoader can not help
-  gSystem->Load("libFWCoreTFWLiteSelectorTest");
+               "gSystem->Load(\"libFWCoreTFWLiteSelectorTest\");");
 
-  //This makes sure the TSelector library and dictionary are properly
-  // installed in the remote PROOF servers
-  gProof->Exec( ".x proof_remote.C" );
-
-  //This creates the 'data set' which defines what files we need to process
+  // This creates the 'data set' which defines what files we need to process
   // NOTE: the files given must be accessible by the remote systems
   TDSet c( "TTree", "Events");
-  c.Add("/home/gregor/cms/CMSSW_0_7_2/src/FWCore/TFWLiteSelector/test/test.root");
+  c.Add("$CMSSW_BASE/test.root");
   
   //This makes the actual processing happen
-  c.Process( "ThingsTSelector" );
+  c.Process( "tfwliteselectortest::ThingsTSelector" );
 }
