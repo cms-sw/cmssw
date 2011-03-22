d239c43fcf7772eb7230ba615248aa0e79d55629
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-03-22 10:51:56 +0000

    fix clhep

--- FastSimulation/EgammaElectronAlgos/src/FastElectronSeedGenerator.cc
+++ FastSimulation/EgammaElectronAlgos/src/FastElectronSeedGenerator.cc
@@ -422,7 +422,7 @@ bool FastElectronSeedGenerator::prepareElTrackSeed(ConstRecHitPointer innerhit,
   FreeTrajectoryState fts = helix.stateAtVertex();
 
   // Give infinite errors to start the fit (no pattern recognition here).
-  AlgebraicSymMatrix errorMatrix(5,1);
+  AlgebraicSymMatrix55 errorMatrix= AlgebraicMatrixID();
   fts.setCurvilinearError(errorMatrix*100.);
 
    TrajectoryStateOnSurface propagatedState = thePropagator->propagate(fts,innerhit->det()->surface()) ;
