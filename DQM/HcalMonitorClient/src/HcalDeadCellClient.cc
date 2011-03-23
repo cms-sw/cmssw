5dea146002eb7dbeb7a66963bc7f3320156a7931
Artur Apresyan <sha1-67a4fa6b8737bf2346ce241c850492ffcfe29190@cern.ch>
2011-03-23 03:25:54 +0000

    minor fix to remove debug message

--- DQM/HcalMonitorClient/src/HcalDeadCellClient.cc
+++ DQM/HcalMonitorClient/src/HcalDeadCellClient.cc
@@ -11,8 +11,8 @@
 /*
  * \file HcalDeadCellClient.cc
  * 
- * $Date: 2010/11/17 19:17:42 $
- * $Revision: 1.72 $
+ * $Date: 2011/03/21 23:02:03 $
+ * $Revision: 1.73 $
  * \author J. Temple
  * \brief Dead Cell Client class
  */
@@ -46,7 +46,9 @@ HcalDeadCellClient::HcalDeadCellClient(std::string myname, const edm::ParameterS
 						   ps.getUntrackedParameter<double>("minerrorrate",0.25));
   minevents_    = ps.getUntrackedParameter<int>("DeadCell_minevents",
 						ps.getUntrackedParameter<int>("minevents",1000));
+
   excludeHOring2_backup_=ps.getUntrackedParameter<bool>("excludeHOring2_backup",false);  // this is used only if excludeHOring2 value from Dead Cell task can't be read
+
   ProblemCellsByDepth=0;
   ProblemCells=0;
 
@@ -85,15 +87,13 @@ void HcalDeadCellClient::calculateProblems()
       if (debug_>2) 
 	std::cout <<"Could not read excludeFromHOring2 from HcalMonitorTasks; using value from cfg file:  "<<excludeFromHOring2<<std::endl;
     }
-
+  
   // Don't fill histograms if nothing from Hcal is present
   if (HBpresent_!=1)
     {
       temp_present=dqmStore_->get(prefixME_+"HcalInfo/HBpresent");
       if (temp_present!=0)
         HBpresent_=temp_present->getIntValue();
-      else
-	std::cout <<" COULD NOT GET TEMP_PRESENT"<<std::endl;
     }
   if (HEpresent_!=1)
     {
@@ -208,6 +208,7 @@ void HcalDeadCellClient::calculateProblems()
 	      // If cell is never-present in all runs, then problemvalue = event
 	      if (DigiPresentByDepth[d]!=0 && DigiPresentByDepth[d]->GetBinContent(eta+1,phi+1)==0) 
 		problemvalue=totalevents;
+
 	      // Rec Hit presence test
 	      else if (RecHitsPresentByDepth[d]!=0)
 		{
@@ -223,7 +224,9 @@ void HcalDeadCellClient::calculateProblems()
 		  if (RecentMissingRecHitsByDepth[d]!=0)
 		    problemvalue+=RecentMissingRecHitsByDepth[d]->GetBinContent(eta+1,phi+1);
 		}
+	      
 	      if (problemvalue==0) continue;
+	      	      	      
 	      problemvalue/=totalevents; // problem value is a rate; should be between 0 and 1
 	      problemvalue = std::min(1.,problemvalue);
 	      
@@ -246,7 +249,6 @@ void HcalDeadCellClient::calculateProblems()
 		}
 	      ProblemCellsByDepth->depth[d]->setBinContent(eta+1,phi+1,problemvalue);
 	      if (ProblemCells!=0) ProblemCells->Fill(ieta+zside,phi+1,problemvalue);
-	      
 	    } // loop on phi
 	} // loop on eta
     } // loop on depth
