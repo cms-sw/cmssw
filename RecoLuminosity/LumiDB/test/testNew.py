6bc38d6e25479bcfd2e17fd7b4a2679831ab847f
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2011-03-23 18:40:36 +0000

    tested guessnorm

--- RecoLuminosity/LumiDB/test/testNew.py
+++ RecoLuminosity/LumiDB/test/testNew.py
@@ -10,7 +10,14 @@ if __name__=='__main__':
     session=svc.openSession(isReadOnly=True,cpp2sqltype=[('unsigned int','NUMBER(10)'),('unsigned long long','NUMBER(20)')])
     session.transaction().start(True)
     schema=session.nominalSchema()
-    runs=dataDML.runList(schema,fillnum=1616,runmin=160403,runmax=160957)
+    runs=dataDML.runList(schema,fillnum=1616,runmin=160403,runmax=160957,nominalEnergy=3500.0)
+    print runs
+    myrun=runs[0]
+    runsummary=dataDML.runsummary(schema,myrun)
+    print runsummary
+    normid=dataDML.guessnormIdByContext(schema,'PROTPHYS',3500)
+    normval=dataDML.luminormById(schema,normid)[2]
+    print 'norm in use ',normval
     session.transaction().commit()
     del session
-    print runs
+    
