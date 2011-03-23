d5e9a002040cb004dd1584259d78ebd1a128531d
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2011-03-23 12:25:40 +0000

    update trigger paths

--- DPGAnalysis/Skims/python/HSCPSkim_cff.py
+++ DPGAnalysis/Skims/python/HSCPSkim_cff.py
@@ -1,7 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 hltstoppedhscp = cms.EDFilter("HLTHighLevel",
                                       TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-                                      HLTPaths = cms.vstring("HLT_StoppedHSCP*"), # provide list of HLT paths (or patterns) you want
+                                      HLTPaths = cms.vstring(
+    "HLT_StoppedHSCP*",
+    "HLT_JetE*_NoBptx_*",
+    "HLT_JetE*_NoBptx_NoHalo_*",
+    "HLT_JetE*_NoBptx3BX_NoHalo_*"), # provide list of HLT paths (or patterns) you want
                                       eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
                                       andOr = cms.bool(True),             # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
                                       throw = cms.bool(False)    # throw exception on unknown path names
