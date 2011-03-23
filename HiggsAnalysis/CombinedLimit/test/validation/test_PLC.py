bc9124a63bf8c348886a93dc17a217df081bb272
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-03-23 20:05:51 +0000

    More reference numbers, and add asymm uncertainties to the suite

--- HiggsAnalysis/CombinedLimit/test/validation/test_PLC.py
+++ HiggsAnalysis/CombinedLimit/test/validation/test_PLC.py
@@ -5,6 +5,9 @@ M='ProfileLikelihood'
 suite += [ (M, '*', MultiDatacardTest("Counting",     datacardGlob("simple-counting/counting-B5p5-Obs[16]*.txt"),M,""))]
 suite += [ (M, '*', MultiDatacardTest("Counting_Sig", datacardGlob("simple-counting/counting-B5p5-Obs[12][10]-S*.txt"),M,"--signif"))]
 
+suite += [ (M, '*', MultiDatacardTest("Asymm",      datacardGlob("asymm/counting-B5p5-Obs6*txt"),  M, "")) ]
+suite += [ (M, '*', MultiDatacardTest("Asymm_Sig",  datacardGlob("asymm/counting-B5p5-Obs20*txt"), M, "--signif")) ]
+
 suite += [ (M, '*', MultiDatacardTest("Gamma",     datacardGlob("gammas/counting-*.txt"), M, "")) ]
 suite += [ (M, '*', MultiDatacardTest("Gamma_Sig", [ "gammas/counting-S1-Sideband1-alpha0p3-Obs2.txt",
                                                      "gammas/counting-S1-Sideband2-alpha0p3-Obs6.txt",
