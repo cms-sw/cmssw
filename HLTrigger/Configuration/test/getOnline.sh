41431c61420c6b6b933afbf5ce03bfeaa6cb8991
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-03-24 12:50:16 +0000

    *** empty log message ***

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-HLT='/online/collisions/2011/5e32/v4.2/HLT'
-L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v0a_mc'
+HLT='/online/collisions/2011/5e32/v6.0/HLT'
+L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v1_mc,sqlite_file:/afs/cern.ch/user/g/ghete/public/L1Menu/L1Menu_Collisions2011_v1/sqlFile/L1Menu_Collisions2011_v1_mc.db'
 
-hltGetConfiguration $HLT --process TEST --full --offline --mc   --unprescale > offline_mc.py
-hltGetConfiguration $HLT --process TEST --full --offline --data --unprescale > offline_data.py
-hltGetConfiguration $HLT --process TEST --full --online  --data --unprescale > online_data.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T --full --offline --mc   --unprescale > offline_mc.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T --full --offline --data --unprescale > offline_data.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T --full --online  --data --unprescale > online_data.py
 
 {
   TABLE=$(echo $HLT | cut -d: -f2)
