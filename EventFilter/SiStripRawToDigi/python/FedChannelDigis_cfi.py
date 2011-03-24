8dd172ac52f23a67368d99c6284b788bcc37d445
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2011-03-24 14:08:25 +0000

    Added ErrorThreshold parameter

--- EventFilter/SiStripRawToDigi/python/FedChannelDigis_cfi.py
+++ EventFilter/SiStripRawToDigi/python/FedChannelDigis_cfi.py
@@ -14,5 +14,6 @@ FedChannelDigis = cms.EDProducer("SiStripRawToDigiModule",
     #Quiet             = cms.untracked.bool(False)
     UnpackCommonModeValues = cms.bool(False),
     DoAllCorruptBufferChecks = cms.bool(False),
+    ErrorThreshold = cms.uint32(7174)
 )
 
