8320d90413af956232ccb3dd2c5c24b10573fbec
Chiara Ilaria Rovelli <sha1-156f87976672d5574743371779e65c95d251b39f@cern.ch>
2011-03-25 18:27:47 +0000

    update the mc GT

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
 
-autoCond = { 'mc'      : 'MC_42_V7::All',
-             'startup' : 'START42_V7::All',
+autoCond = { 'mc'      : 'MC_42_V8::All',
+             'startup' : 'START42_V8::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
              'com10'   : 'GR_R_42_V7::All',
