8cd24d1fe564fd49dd3178abce238025e5bcb3f6
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2011-04-01 15:12:43 +0000

    2010 version

--- Configuration/Skimming/python/PDWG_HSCP_SD_cff.py
+++ Configuration/Skimming/python/PDWG_HSCP_SD_cff.py
@@ -4,10 +4,10 @@ import FWCore.ParameterSet.Config as cms
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 HSCPSD = hlt.triggerResultsFilter.clone()
 HSCPSD.triggerConditions = cms.vstring(
-#    "HLT_StoppedHSCP*",
-    "HLT_JetE*_NoBPTX_*",
-    "HLT_JetE*_NoBPTX_NoHalo_*",
-    "HLT_JetE*_NoBPTX3BX_NoHalo_*")
+    "HLT_StoppedHSCP*",
+#    "HLT_JetE*_NoBPTX_*",
+#    "HLT_JetE*_NoBPTX_NoHalo_*",
+#    "HLT_JetE*_NoBPTX3BX_NoHalo_*")
 HSCPSD.hltResults = cms.InputTag( "TriggerResults", "", "HLT" )
 HSCPSD.l1tResults = cms.InputTag("")
 HSCPSD.throw = cms.bool( False )
