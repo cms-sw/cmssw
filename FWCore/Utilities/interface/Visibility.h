ca06a165e9e2e1a3e1040d132cad0fc0f4babfd9
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-04-05 15:25:37 +0000

    add check of compiler

--- FWCore/Utilities/interface/Visibility.h
+++ FWCore/Utilities/interface/Visibility.h
@@ -1,10 +1,17 @@
 #ifndef VISIBILITY_MACROS_H
 #define VISIBILITY_MACROS_H
 
+#if defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 5)
 #define dso_export    __attribute__ ((visibility ("default")))
 #define dso_hidden    __attribute__ ((visibility ("hidden")) )
 #define dso_internal  __attribute__ ((visibility ("internal")))
 #define dso_protected __attribute__ ((visibility ("protected")))
+#else
+#define dso_export  
+#define dso_hidden   
+#define dso_internal 
+#define dso_protected
+#endif
 
 
 #endif // VISIBILITY
