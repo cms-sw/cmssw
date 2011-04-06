83ca76ba85630683109abda85c1f628fe5551011
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2011-04-06 11:38:39 +0000

    update example cfg for rechitcorrector

--- RecoLocalCalo/Castor/test/castor_cfg.py
+++ RecoLocalCalo/Castor/test/castor_cfg.py
@@ -4,6 +4,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("CastorProducts")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+# specify the correct database tags which contain the updated gains and channelquality flags
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 process.CastorDbProducer = cms.ESProducer("CastorDbProducer")
 process.es_pool = cms.ESSource(
@@ -43,42 +44,36 @@ process.es_pool = cms.ESSource(
            )
    )
 )
-
-#process.es_pool2 = cms.ESSource("PoolDBESSource",
-#     process.CondDBSetup,
-#     timetype = cms.string('runnumber'),
-#    connect = cms.string('sqlite_file:testExample.db'),
-#	authenticationMethod = cms.untracked.uint32(0),
-#	toGet = cms.VPSet(
-#		cms.PSet(
-#			record = cms.string('CastorChannelQualityRcd'),
-#			tag = cms.string('castor_channelstatus_v1.0_test')
-#		)
-#	)
-#)
+# end of Db configuration
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
+
 process.source = cms.Source("PoolSource",
 duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
-    fileNames = 
-cms.untracked.vstring('file:../../../data_RAW2DIGI_L1Reco_RECO.root')
+    fileNames = cms.untracked.vstring(
+    	'file:../../../../../CMSSW_4_2_0_pre5/src/data_RAW2DIGI_L1Reco_RECO.root' # choose your input file here
+	)
 )
 
+# load CASTOR default reco chain (from towers on)
 process.load('RecoLocalCalo.Castor.Castor_cff')
 
+# construct the module which executes the RechitCorrector
 process.rechitcorrector = cms.EDProducer("RecHitCorrector",
-	rechitLabel = cms.InputTag("castorreco","","RERECO"),
-	revertFactor = cms.double(62.5)
+	rechitLabel = cms.InputTag("castorreco","","RECO"), # choose the original RecHit collection
+	revertFactor = cms.double(62.5) # this is the factor to go back to the original fC: 1/0.016
 )
+
+# tell to the CastorTower reconstruction that he should use the new corrected rechits
+process.CastorTowerReco.inputprocess = "rechitcorrector"
 
 process.MyOutputModule = cms.OutputModule("PoolOutputModule",
-    #outputCommands = cms.untracked.vstring('keep recoGenParticles*_*_*_*', 'keep *_castorreco_*_*', 'keep *_Castor*Reco*_*_CastorProducts'),
-    #outputCommands = cms.untracked.vstring('keep *_Castor*Reco*_*_CastorProducts','drop *_CastorFastjetReco*_*_CastorProducts','drop *_CastorTowerCandidateReco*_*_CastorProducts'),
-    fileName = cms.untracked.string('rechitcorrector_output.root')
+    fileName = cms.untracked.string('rechitcorrector_output.root') # choose your output file
 )
 
+# execute the rechitcorrector and afterwards do the reco chain again (towers -> jets)
 process.producer = cms.Path(process.rechitcorrector*process.CastorFullReco)
 process.end = cms.EndPath(process.MyOutputModule)
 
