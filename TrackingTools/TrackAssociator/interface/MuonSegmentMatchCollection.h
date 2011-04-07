dd0b45b683d93dc22385c3049d91b256dc283d98
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-04-07 08:16:57 +0000

    moved to plugin

--- TrackingTools/TrackAssociator/interface/MuonSegmentMatchCollection.h
+++
@@ -1,9 +0,0 @@
-#ifndef TrackAssociator_TAMuonSegmentMatchCollection_h
-#define TrackAssociator_TAMuonSegmentMatchCollection_h
-
-#include <vector>
-#include "TrackingTools/TrackAssociator/interface/TAMuonSegmentMatch.h"
-
-typedef std::vector<TAMuonSegmentMatch> TAMuonSegmentMatchCollection;
-
-#endif
--- TrackingTools/TrackAssociator/plugins/modules.cc
+++ TrackingTools/TrackAssociator/plugins/modules.cc
@@ -2,17 +2,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 
-#include "TrackingTools/TrackAssociator/interface/DetIdAssociatorFactory.h"
-#include "TrackingTools/TrackAssociator/interface/EcalDetIdAssociator.h"
-#include "TrackingTools/TrackAssociator/interface/HcalDetIdAssociator.h"
-#include "TrackingTools/TrackAssociator/interface/HODetIdAssociator.h"
-#include "TrackingTools/TrackAssociator/interface/CaloDetIdAssociator.h"
-#include "TrackingTools/TrackAssociator/interface/MuonDetIdAssociator.h"
-#include "TrackingTools/TrackAssociator/interface/PreshowerDetIdAssociator.h"
+#include "TrackingTools/TrackAssociator/interface/DetIdAssociator.h"
 
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, EcalDetIdAssociator, "EcalDetIdAssociator");
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, HcalDetIdAssociator, "HcalDetIdAssociator");
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, HODetIdAssociator, "HODetIdAssociator");
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, CaloDetIdAssociator, "CaloDetIdAssociator");
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, MuonDetIdAssociator, "MuonDetIdAssociator");
-DEFINE_EDM_PLUGIN(DetIdAssociatorFactory, PreshowerDetIdAssociator, "PreshowerDetIdAssociator");
+#include "FWCore/Utilities/interface/typelookup.h"
+TYPELOOKUP_DATA_REG(DetIdAssociator);
