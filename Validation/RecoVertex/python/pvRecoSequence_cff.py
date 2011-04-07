5169d0077fb17dc28501837aa73c9ced0bf158ef
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2011-04-07 07:30:00 +0000

    cff modified to be independent from the actual PV official reco configuration

--- Validation/RecoVertex/python/pvRecoSequence_cff.py
+++ Validation/RecoVertex/python/pvRecoSequence_cff.py
@@ -4,12 +4,47 @@ from Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff import 
 from Configuration.StandardSequences.Geometry_cff import *
 from Configuration.StandardSequences.Reconstruction_cff import *
 
-from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import *
+#from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import *
 
-offlinePrimaryVerticesD0s5 = offlinePrimaryVertices.clone()
+# 2010-like PV reconstruction 
+
+offlinePrimaryVerticesGAP = cms.EDProducer("PrimaryVertexProducer",
+                                            PVSelParameters = cms.PSet(
+            maxDistanceToBeam = cms.double(2) ## 0.1
+                ),
+                                            verbose = cms.untracked.bool(False),
+                                            algorithm = cms.string('AdaptiveVertexFitter'),
+                                            minNdof  = cms.double(0.0),
+                                            TkFilterParameters = cms.PSet(
+            algorithm=cms.string('filter'),
+                    maxNormalizedChi2 = cms.double(20.0),     #
+                    minSiliconLayersWithHits = cms.int32(5), # >= 5
+                    minPixelLayersWithHits = cms.int32(2),   # >= 2
+                    maxD0Significance = cms.double(100.0),     # keep most primary tracks
+                    minPt = cms.double(0.0),                 # better for softish events
+                    trackQuality = cms.string("any")
+                ),
+                                            beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+                                            # label of tracks to be used
+                                            TrackLabel = cms.InputTag("generalTracks"),
+                                            useBeamConstraint = cms.bool(False),
+
+
+                                            # clustering
+                                            TkClusParameters = cms.PSet(
+            algorithm   = cms.string('gap'),
+                    TkGapClusParameters = cms.PSet(
+                zSeparation = cms.double(0.2)
+                        )
+                )
+                                        )
+
+
+
+offlinePrimaryVerticesD0s5 = offlinePrimaryVerticesGAP.clone()
 offlinePrimaryVerticesD0s5.TkFilterParameters.maxD0Significance = cms.double(5)
 
-offlinePrimaryVerticesD0s51mm = offlinePrimaryVertices.clone()
+offlinePrimaryVerticesD0s51mm = offlinePrimaryVerticesGAP.clone()
 offlinePrimaryVerticesD0s51mm.TkFilterParameters.maxD0Significance = cms.double(5)
 offlinePrimaryVerticesD0s51mm.TkClusParameters.TkGapClusParameters.zSeparation = cms.double(0.1) 
 
@@ -67,5 +102,5 @@ offlinePrimaryVerticesDA100umV8.TkClusParameters.TkDAClusParameters.coolingFacto
 offlinePrimaryVerticesDA100umV8.TkClusParameters.TkDAClusParameters.Tmin = cms.double(4.)
 
 
-seqPVReco = cms.Sequence(offlinePrimaryVerticesD0s5 + offlinePrimaryVerticesD0s51mm +
+seqPVReco = cms.Sequence(offlinePrimaryVerticesGAP + offlinePrimaryVerticesD0s5 + offlinePrimaryVerticesD0s51mm +
                          offlinePrimaryVerticesDA100um + offlinePrimaryVerticesDA100umV7 + offlinePrimaryVerticesDA100umV8 )
