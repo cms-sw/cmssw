4ea757bd8cf024ddb2cd39d0b23acb93718eb0d0
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-04-09 10:17:14 +0000

    added missing useClosestPV configuration parameter

--- RecoTauTag/RecoTau/python/RecoTauFixedConeProducer_cfi.py
+++ RecoTauTag/RecoTau/python/RecoTauFixedConeProducer_cfi.py
@@ -14,6 +14,7 @@ See PFT-08-001 for a description of the algorithm.
 _fixedConeRecoTausConfig = cms.PSet(
     name = cms.string("fixedCone"),
     primaryVertexSrc = cms.InputTag("offlinePrimaryVertices"),
+    useClosestPV = cms.bool(True),
     qualityCuts = PFTauQualityCuts.signalQualityCuts,
     # If true, consider PFLeptons (e/mu) as charged hadrons.
     usePFLeptons = cms.bool(True),
