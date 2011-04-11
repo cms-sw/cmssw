773240c448896fd0fd22e793ca4ab36ea9ccd496
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-04-11 18:34:54 +0000

    upda

--- Configuration/PyReleaseValidation/python/autoCond.py
+++ Configuration/PyReleaseValidation/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_42_V9::All',
-             'startup' : 'START42_V9::All',
+autoCond = { 'mc'      : 'MC_42_V10::All',
+             'startup' : 'START42_V10::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_42_V8::All',
-             'hltonline' : 'GR_R_42_V8::All', # same as com10 until H tag available
+             'com10'   : 'GR_R_42_V10::All',
+             'hltonline' : 'GR_R_42_V10::All', # same as com10 until H tag available
              'starthi' : 'STARTHI42_V8::All'
              }
