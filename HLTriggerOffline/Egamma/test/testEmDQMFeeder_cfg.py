f1e0222ea6471b1fb8dbe52399d97d19e6a190d5
Thomas Reis <sha1-5047472e5ab9ba2787d73271e47b3cb9a1574555@cern.ch>
2011-04-12 11:42:29 +0000

    - changed process name of postprocessor path to ppost so that it not overwrites the path for the analyzer

--- HLTriggerOffline/Egamma/test/testEmDQMFeeder_cfg.py
+++ HLTriggerOffline/Egamma/test/testEmDQMFeeder_cfg.py
@@ -4,14 +4,14 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) ) # only one event needed to extract trigger information (-1 for all events)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #'rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_2_0_pre7/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START42_V6-v2/0033/BAA28B05-224F-E011-92F7-0026189438D3.root'
+        'rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_2_0_pre7/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START42_V6-v2/0033/BAA28B05-224F-E011-92F7-0026189438D3.root'
         # 'rfio:/castor/cern.ch/cms/store/relval/CMSSW_3_11_3/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START311_V2-v2/0007/A8FACACC-2F4F-E011-B535-002618943906.root'
 
-        '/store/relval/CMSSW_3_11_1_hltpatch1/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/L1HLTST311_V1_ECALSelectiveReadOut-v1/0041/4A702B9C-9C54-E011-BADB-003048678ED2.root',
+        #'/store/relval/CMSSW_3_11_1_hltpatch1/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/L1HLTST311_V1_ECALSelectiveReadOut-v1/0041/4A702B9C-9C54-E011-BADB-003048678ED2.root',
         #'rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_1_3/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START311_V2-v1/0037/D27F9CA1-C851-E011-92BC-003048679266.root'
         #'rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_2_0_pre5/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START42_V3-v1/0009/1289680D-5D3C-E011-83B4-002618943894.root'
         #'rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_2_0_pre6/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/START42_V4-v1/0020/78B8F51E-5245-E011-BA20-003048679294.root'
@@ -21,38 +21,31 @@ process.source = cms.Source("PoolSource",
 )
 
 process.demo = cms.EDAnalyzer('EmDQMFeeder',
-    processname = cms.string("HLT"),
-                              
-
-    triggerobject = cms.InputTag("hltTriggerSummaryRAW","","HLT"),
-
-    # genEtMin = cms.untracked.double(0),
-
-    reqNum = cms.uint32(1),
-    pdgGen = cms.int32(11),
-    genEtaAcc = cms.double(2.5),
-    genEtAcc = cms.double(2.0),
-    PtMax = cms.untracked.double(100.0),
-
-    cutcollection = cms.InputTag("fiducialWenu"),
-    cutnum = cms.int32(1),
-
-)
+                              processname = cms.string("HLT"),
+                              triggerobject = cms.InputTag("hltTriggerSummaryRAW","","HLT"),
+                              # genEtMin = cms.untracked.double(0),
+                              #reqNum = cms.uint32(1),
+                              #pdgGen = cms.int32(11),
+                              genEtaAcc = cms.double(2.5),
+                              genEtAcc = cms.double(2.0),
+                              PtMax = cms.untracked.double(100.0),
+                              #cutcollection = cms.InputTag("fiducialWenu"),
+                              #cutnum = cms.int32(1),
+                             )
 
 process.load("HLTriggerOffline.Egamma.EgammaValidation_cff")
 
 process.p = cms.Path(
-
-    # require generated particles in fiducial volume
-    process.egammaSelectors *     
-
-    process.demo)
+                     # require generated particles in fiducial volume
+                     process.egammaSelectors *     
+                     process.demo
+                    )
 
 #----------------------------------------
 process.post=cms.EDAnalyzer("EmDQMPostProcessor",
                             subDir = cms.untracked.string("HLT/HLTEgammaValidation"),
                             dataSet = cms.untracked.string("unknown"),
-    )
+                           )
 
 
 
@@ -68,4 +61,4 @@ process.dqmSaver.convention = 'Online'
 process.dqmSaver.saveByRun = 1
 process.dqmSaver.saveAtJobEnd = True
 
-process.p = cms.EndPath(process.post+process.dqmSaver)
+process.ppost = cms.EndPath(process.post+process.dqmSaver)
