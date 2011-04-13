8f502dffe3b1375fb8c68b0bfe1cb819253445fe
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2011-04-13 10:30:16 +0000

    add test_file for parameterset special cases

---
+++ FWCore/GuiBrowsers/test/test_parameterssets.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("copy_events")
+
+# source
+process.source = cms.Source("PoolSource", 
+     fileNames = cms.untracked.vstring('file:/opt/user/hinzmann/QCDDiJet_Pt50to80_Summer09_RECO_3_1_X.root')
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+# Output module configuration
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('QCDDiJet_Pt50to80_Summer09_RECO_3_1_X_10events.root'),
+)
+process.outpath = cms.EndPath(process.out)
+
+process.module1=cms.EDAnalyzer("Module1",
+vinputtag1 = cms.untracked.VInputTag(cms.InputTag("module2"))
+)
+process.module2=cms.EDAnalyzer("Module2")
+process.p=cms.Path(process.module1+~process.module2)
