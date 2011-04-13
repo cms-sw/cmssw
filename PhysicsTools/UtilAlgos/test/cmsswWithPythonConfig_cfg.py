c71a68079f4380e1441e1b34dcc2af0f665c25e7
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-04-13 16:58:47 +0000

    reduced number of events plus update on input file in preparation of unit tests to be introduced

--- PhysicsTools/UtilAlgos/test/cmsswWithPythonConfig_cfg.py
+++ PhysicsTools/UtilAlgos/test/cmsswWithPythonConfig_cfg.py
@@ -4,14 +4,16 @@ process = cms.Process("Test")
 
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_4_1_3/RelValTTbar/GEN-SIM-RECO/START311_V2-v1/0037/648B6AA5-C751-E011-8208-001A928116C6.root'
+    "/store/relval/CMSSW_4_2_0/RelValZTT/GEN-SIM-RECO/START42_V9-v1/0054/107DB9B4-7D5E-E011-91E9-001A92810AEA.root"
   )
 )
+### Maximal Number of Events
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.MessageLogger = cms.Service("MessageLogger")
 
 process.muonAnalyzer = cms.EDAnalyzer("WrappedEDMuonAnalyzer",
-  muons     = cms.InputTag("muons"),                                             
+  muons = cms.InputTag("muons"),                                             
 )
 
 process.TFileService = cms.Service("TFileService",
