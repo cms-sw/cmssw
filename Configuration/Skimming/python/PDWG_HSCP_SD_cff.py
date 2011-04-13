ac8833387c1f6c756a976c3139f304b72a7322fa
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2011-04-13 21:58:03 +0000

    fix python error

--- Configuration/Skimming/python/PDWG_HSCP_SD_cff.py
+++ Configuration/Skimming/python/PDWG_HSCP_SD_cff.py
@@ -5,10 +5,10 @@ import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
 HSCPSD = hlt.hltHighLevel.clone()
 HSCPSD.TriggerResultsTag = cms.InputTag( "TriggerResults", "", "HLT" )
 HSCPSD.HLTPaths = cms.vstring(
-    "HLT_StoppedHSCP*")
-    "HLT_JetE*_NoBPTX_*",
-    "HLT_JetE*_NoBPTX_NoHalo_*",
-    "HLT_JetE*_NoBPTX3BX_NoHalo_*")
+    "HLT_StoppedHSCP*",
+    "HLT_JetE*_NoBPTX_*")
+#    "HLT_JetE*_NoBPTX_NoHalo_*",
+#    "HLT_JetE*_NoBPTX3BX_NoHalo_*")
 HSCPSD.andOr = cms.bool( True )
 HSCPSD.throw = cms.bool( False )
 
