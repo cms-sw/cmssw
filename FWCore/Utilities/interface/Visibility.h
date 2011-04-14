8f18138c91d975a648e06982a079822bc9db3ff6
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-04-14 14:08:20 +0000

    make it available also in gcc45

--- FWCore/Utilities/interface/Visibility.h
+++ FWCore/Utilities/interface/Visibility.h
@@ -1,7 +1,7 @@
 #ifndef VISIBILITY_MACROS_H
 #define VISIBILITY_MACROS_H
 
-#if defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 5)
+#if defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 3)
 #define dso_export    __attribute__ ((visibility ("default")))
 #define dso_hidden    __attribute__ ((visibility ("hidden")) )
 #define dso_internal  __attribute__ ((visibility ("internal")))
