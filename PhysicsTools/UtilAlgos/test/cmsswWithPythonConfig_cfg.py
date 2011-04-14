8148d64665c5a108f5ec7b3e13125ac0c48129a4
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-04-14 10:52:20 +0000

    cleanup of documentation and module structures

--- PhysicsTools/UtilAlgos/test/cmsswWithPythonConfig_cfg.py
+++ PhysicsTools/UtilAlgos/test/cmsswWithPythonConfig_cfg.py
@@ -7,11 +7,17 @@ process.source = cms.Source("PoolSource",
     "/store/relval/CMSSW_4_2_0/RelValZTT/GEN-SIM-RECO/START42_V9-v1/0054/107DB9B4-7D5E-E011-91E9-001A92810AEA.root"
   )
 )
-### Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.MessageLogger = cms.Service("MessageLogger")
 
+## ---
+## This is an example of the use of the BasicAnalyzer concept used to exploit C++ classes to do anaysis
+## in full framework or FWLite using the same class. You can find the implementation of this module in
+## PhysicsTools/UtilAlgos/plugins/WrappedEDMuonAnlyzer. You can find the EDAnalyzerWrapper.h class in
+## PhysicsTools/UtilAlgos/interface/EDAnalyzerWrapper.h. You can find the implementation of the
+## BasicMuonAnalyzer class in PhysicsTools/UtilAlgos/interface/BasicMuonAnlyzer.h. You will also find
+## back the input parameters to the module.
 process.muonAnalyzer = cms.EDAnalyzer("WrappedEDMuonAnalyzer",
   muons = cms.InputTag("muons"),                                             
 )
--- PhysicsTools/UtilAlgos/test/testPrimaryVertexFilter_cfg.py
+++ PhysicsTools/UtilAlgos/test/testPrimaryVertexFilter_cfg.py
@@ -7,11 +7,17 @@ process.source = cms.Source("PoolSource",
     "/store/relval/CMSSW_4_2_0/RelValZTT/GEN-SIM-RECO/START42_V9-v1/0054/107DB9B4-7D5E-E011-91E9-001A92810AEA.root"
   )
 )
-## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.MessageLogger = cms.Service("MessageLogger")
 
+## ---
+## This is an example of the use of the EDFilterWrapper to exploit C++ selector classes as defined in the
+## PhysicsTools/SelectorUtils package wrapping them into an EDFilter. You can find the implementation of
+## the EDFilter in PhysicsTools/UtilAlgos/plugins/PrimaryVertexFilter.cc. You can find the EDfilterWrapper
+## class in PhysicsTools/UtilAlgos/interface/EDFilterWrapper.h. The class that has been exploited here is 
+## the PVSelector class of the PhysicsTools/SelectorUtils package, which makes implicit use of the
+## PVObjectSelector class in the same package.You cna find the input parameters there. 
 process.primaryVertexFilter = cms.EDFilter("PrimaryVertexFilter",
   pvSrc   = cms.InputTag("offlinePrimaryVertices"),
   minNdof = cms.double( 4 ),
