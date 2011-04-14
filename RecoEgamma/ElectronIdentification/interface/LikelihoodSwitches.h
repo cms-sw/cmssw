943c08e9215be4b59ce98116961aa3aac99ce807
Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch>
2011-04-14 23:02:26 +0000

    by default use: - 1/E - 1/P and do not use H/E (pileup sensitivity)

--- RecoEgamma/ElectronIdentification/interface/LikelihoodSwitches.h
+++ RecoEgamma/ElectronIdentification/interface/LikelihoodSwitches.h
@@ -5,14 +5,16 @@ struct LikelihoodSwitches {
 
   LikelihoodSwitches () :
     m_useEoverP (false) ,
+    m_useOneOverEMinusOneOverP (true) ,
     m_useDeltaEta (true) ,
     m_useDeltaPhi (true) ,
-    m_useHoverE (true) ,
+    m_useHoverE (false) ,
     m_useFBrem (true) ,
     m_useSigmaEtaEta (true) ,
     m_useSigmaPhiPhi (true) {} ;
 
   bool m_useEoverP ;
+  bool m_useOneOverEMinusOneOverP ;
   bool m_useDeltaEta ;
   bool m_useDeltaPhi ;
   bool m_useHoverE ;
