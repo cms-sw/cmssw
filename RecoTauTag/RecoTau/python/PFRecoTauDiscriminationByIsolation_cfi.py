7787574a1bc0ea33d5556a897674572f87a9d320
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-04-15 15:34:35 +0000

    Enable delta-beta corrections

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByIsolation_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByIsolation_cfi.py
@@ -3,29 +3,36 @@ import FWCore.ParameterSet.Config as cms
 from RecoTauTag.RecoTau.PFRecoTauQualityCuts_cfi import PFTauQualityCuts
 from RecoTauTag.RecoTau.TauDiscriminatorTools import requireLeadTrack
 
-pfRecoTauDiscriminationByIsolation = cms.EDProducer("PFRecoTauDiscriminationByIsolation",
+pfRecoTauDiscriminationByIsolation = cms.EDProducer(
+    "PFRecoTauDiscriminationByIsolation",
     PFTauProducer = cms.InputTag('pfRecoTauProducer'), #tau collection to discriminate
 
     # Require leading pion ensures that:
-    #  1) these is at least one track above threshold (0.5 GeV) in the signal cone
-    #  2) a track in the signal cone has pT > 5 GeV
+    # 1) these is at least one track above threshold (0.5 GeV) in the signal cone
+    # 2) a track in the signal cone has pT > 5 GeV
     Prediscriminants = requireLeadTrack,
 
-    # Select which collections to use for isolation.  You can select one or both
-    ApplyDiscriminationByECALIsolation    = cms.bool(True),  # use PFGammas when isolating
-    ApplyDiscriminationByTrackerIsolation = cms.bool(True),  # use PFChargedHadr when isolating
+    # Select which collections to use for isolation. You can select one or both
+    ApplyDiscriminationByECALIsolation = cms.bool(True), # use PFGammas when isolating
+    ApplyDiscriminationByTrackerIsolation = cms.bool(True), # use PFChargedHadr when isolating
 
-    applyOccupancyCut                     = cms.bool(True),  # apply a cut on number of isolation objects
-    maximumOccupancy                      = cms.uint32(0),   # no tracks > 1 GeV or gammas > 1.5 GeV allowed
+    applyOccupancyCut = cms.bool(True), # apply a cut on number of isolation objects
+    maximumOccupancy = cms.uint32(0), # no tracks > 1 GeV or gammas > 1.5 GeV allowed
 
-    applySumPtCut                         = cms.bool(False), # apply a cut on the sum Pt of the isolation objects
-    maximumSumPtCut                       = cms.double(6.0),
+    applySumPtCut = cms.bool(False), # apply a cut on the sum Pt of the isolation objects
+    maximumSumPtCut = cms.double(6.0),
 
-    applyRelativeSumPtCut                 = cms.bool(False), # apply a cut on IsoPt/TotalPt
-    relativeSumPtCut                      = cms.double(0.0),
+    applyRelativeSumPtCut = cms.bool(False), # apply a cut on IsoPt/TotalPt
+    relativeSumPtCut = cms.double(0.0),
 
-    qualityCuts                           = PFTauQualityCuts,# set the standard quality cuts
+    qualityCuts = PFTauQualityCuts,# set the standard quality cuts
+
+    # Delta-Beta corrections to remove Pileup
+    applyDeltaBetaCorrection = cms.bool(True),
+    particleFlowSrc = cms.InputTag("particleFlow"),
+    # This must correspond to the cone size of the algorithm which built the
+    # tau. (or if customOuterCone option is used, the custom cone size)
+    isoConeSizeForDeltaBeta = cms.double(0.5),
+    # The expected neutral contribution if the charged contribution is 1.0
+    deltaBetaFactor = cms.double(0.38),
 )
-
-
-
