8f4f84d8c66640850a242544bf865ea2cca17a69
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-04-15 14:00:26 +0000

    hide all

--- RecoTracker/MeasurementDet/src/MeasurementTracker.cc
+++ RecoTracker/MeasurementDet/src/MeasurementTracker.cc
@@ -1,9 +1,12 @@
 #include "RecoTracker/MeasurementDet/interface/MeasurementTracker.h"
 
+MeasurementTracker::~MeasurementTracker(){}
+
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/Utilities/interface/typelookup.h"
+
 
 TYPELOOKUP_DATA_REG(MeasurementTracker);
