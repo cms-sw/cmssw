6331ba2aaae76aecc7e1a02724cf7b5ba2c6d0ff
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-04-15 08:55:20 +0000

    removed old and deprecated code for jet validation

--- Validation/RecoJets/plugins/Balancing_fwd.h
+++
@@ -1,7 +0,0 @@
-#include "Validation/RecoJets/plugins/Balance.h"
-#include "Validation/RecoJets/plugins/Comparison.h"
-
-#include "Validation/RecoJets/plugins/PhotonQualifier.h"
-#include "Validation/RecoJets/plugins/CaloJetQualifier.h"
-
-typedef Comparison<reco::PhotonCollection, PhotonQualifier, reco::CaloJetCollection, CaloJetQualifier, Balance> PhotonCaloJetBalance;
