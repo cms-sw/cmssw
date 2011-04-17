d5483bd95a52da651f83dd5856116ca8c509e067
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2011-04-17 08:33:12 +0000

    Check if reference is Nonull as suggested by M.Gruenwald

--- HLTriggerOffline/Muon/src/HLTMuonValidator.cc
+++ HLTriggerOffline/Muon/src/HLTMuonValidator.cc
@@ -1,6 +1,6 @@
 /** \file HLTMuonValidator.cc
- *  $Date: 2010/11/04 12:56:22 $
- *  $Revision: 1.23 $
+ *  $Date: 2011/02/22 16:11:21 $
+ *  $Revision: 1.24 $
  */
 
 
@@ -308,7 +308,8 @@ HLTMuonValidator::analyzePath(const Event & iEvent,
   }
   else for (size_t i = 0; i < nStepsHlt; i++)
     for (size_t j = 0; j < refsHlt[i].size(); j++)
-      candsHlt[i].push_back(& * refsHlt[i][j]);
+       if (refsHlt[i][j].isNonnull()) candsHlt[i].push_back(& * refsHlt[i][j]);
+      
 
   // Add trigger objects to the MatchStructs
   findMatches(matches, candsL1, candsHlt);
@@ -400,12 +401,14 @@ HLTMuonValidator::findMatches(
     double bestDeltaR = cutsDr_[0];
     size_t bestMatch = kNull;
     for (it = indicesL1.begin(); it != indicesL1.end(); it++) {
+     if (candsL1[*it].isNonnull()) {
       double dR = deltaR(cand->eta(), cand->phi(),
                          candsL1[*it]->eta(), candsL1[*it]->phi());
       if (dR < bestDeltaR) {
         bestMatch = *it;
         bestDeltaR = dR;
       }
+     }
       // TrajectoryStateOnSurface propagated;
       // float dR = 999., dPhi = 999.;
       // bool isValid = l1Matcher_.match(* cand, * candsL1[*it], 
