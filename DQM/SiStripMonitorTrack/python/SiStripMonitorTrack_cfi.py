77281771029668494ad12daa6dd34e5491b369ba
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2011-04-18 17:09:41 +0000

    optimization of the range of some histograms without changing the number of bins

--- DQM/SiStripMonitorTrack/python/SiStripMonitorTrack_cfi.py
+++ DQM/SiStripMonitorTrack/python/SiStripMonitorTrack_cfi.py
@@ -41,7 +41,7 @@ SiStripMonitorTrack = cms.EDAnalyzer(
 
     TH1nClustersOff = cms.PSet( Nbinx = cms.int32(100),
                              xmin  = cms.double(-0.5),
-                             xmax  = cms.double(999.5)
+                             xmax  = cms.double(1999.5)
                              ),
     
     TH1ClusterCharge = cms.PSet( Nbinx = cms.int32(100),
