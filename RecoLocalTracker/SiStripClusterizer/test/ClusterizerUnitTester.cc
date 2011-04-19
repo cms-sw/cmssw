cb279c8468855f6ab6a3e9ed5d5e0381e8140ee0
Rebeca Gonzalez Suarez <sha1-288f8676bd34b27ee22458ebd2ae32a0707dc8d5@cern.ch>
2011-04-19 08:27:36 +0000

    Removed use of the geographicalId() of the Strip clusters

--- RecoLocalTracker/SiStripClusterizer/test/ClusterizerUnitTester.cc
+++ RecoLocalTracker/SiStripClusterizer/test/ClusterizerUnitTester.cc
@@ -128,8 +128,7 @@ clusterDetSetsIdentical(const edmNew::DetSet<SiStripCluster>& L, const edmNew::D
 bool ClusterizerUnitTester::
 clustersIdentical(const SiStripCluster& L, const SiStripCluster& R) {
   return
-    L.geographicalId() == R.geographicalId()
-    && L.firstStrip() == R.firstStrip() 
+    L.firstStrip() == R.firstStrip() 
     && L.amplitudes().size() == R.amplitudes().size()
     && inner_product(L.amplitudes().begin(), L.amplitudes().end(), R.amplitudes().begin(), 
 		     bool(true), std::logical_and<bool>(), std::equal_to<uint16_t>() );
--- RecoLocalTracker/SiStripClusterizer/test/CompareClusters.cc
+++ RecoLocalTracker/SiStripClusterizer/test/CompareClusters.cc
@@ -118,8 +118,7 @@ identicalDetSet(const edmNew::DetSet<SiStripCluster>& L, const edmNew::DetSet<Si
 bool CompareClusters::
 identicalClusters(const SiStripCluster& L, const SiStripCluster& R) {
   return
-    L.geographicalId() == R.geographicalId()
-    && L.firstStrip() == R.firstStrip() 
+    L.firstStrip() == R.firstStrip() 
     && L.amplitudes().size() == R.amplitudes().size()
     && inner_product(L.amplitudes().begin(), L.amplitudes().end(), R.amplitudes().begin(), 
 		     bool(true), std::logical_and<bool>(), std::equal_to<uint16_t>() );
