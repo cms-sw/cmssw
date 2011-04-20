a715b8df5c099b2ee82a4566c22d73acfe232a3a
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2011-04-20 08:38:52 +0000

    FINAL 2010 skim config for 421rereco

--- Configuration/Skimming/python/autoSkim.py
+++ Configuration/Skimming/python/autoSkim.py
@@ -2,14 +2,14 @@ autoSkim = {
     'MinimumBias':'MuonTrack+BeamBkg+ValSkim+LogError+HSCPSD',
     'ZeroBias':'LogError',
     'Commissioning':'MuonDPG+LogError',
-    'Cosmics':'CSC+CosmicSP+LogError',
-    'Mu' : 'WZMu+WMu+ZMu+LogError',    
-    'EG':'WZEG+WElectron+ZElectron+LogError',
-    'Electron':'WZEG+WElectron+ZElectron+LogError',
-    'Photon':'WZEG+WElectron+ZElectron+LogError',
+    'Cosmics':'CosmicSP+LogError',
+    'Mu' : 'WMu+ZMu+HighMET+LogError',    
+    'EG':'WElectron+ZElectron+HighMET+LogError',
+    'Electron':'WElectron+ZElectron+HighMET+LogError',
+    'Photon':'WElectron+ZElectron+HighMET+LogError',
     'JetMETTau':'LogError+DiJet+Tau',
-    'JetMET':'LogError+DiJet',
+    'JetMET':'HighMET+LogError+DiJet',
     'BTau':'LogError+Tau',
-    'Jet':'LogError+DiJet',
-    'METFwd':'LogError'
+    'Jet':'HighMET+LogError+DiJet',
+    'METFwd':'HighMET+LogError'
     }
