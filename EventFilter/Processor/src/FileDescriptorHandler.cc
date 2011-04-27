e27d01606443029bb4371f6990b4d0a652a49bcf
Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch>
2011-04-27 15:51:16 +0000

    fix problem with filedescriptor close for sockets

--- EventFilter/Processor/src/FileDescriptorHandler.cc
+++ EventFilter/Processor/src/FileDescriptorHandler.cc
@@ -8,7 +8,9 @@
 #include <dirent.h>
 
 #include <stdlib.h>
+#include <unistd.h>
 
+#include <vector>
 
 namespace evf{
 
@@ -22,16 +24,29 @@ FileDescriptorHandler::FileDescriptorHandler(){
   DIR *dir = opendir(ost.str().c_str());
   dirent *de; 
   struct stat buf;
+  std::vector<int> oldfds;
+  std::vector<int> newfds;
+
   while((de = readdir(dir))!=0){
     char *name = de->d_name;    
     std::string path = ost.str()+name;
     stat(path.c_str(),&buf);
     if(S_ISSOCK(buf.st_mode)){
       int fd = atoi(name);
-      close(fd);
+      oldfds.push_back(fd);
+      int newfd = dup(fd);
+      if(newfd>0) newfds.push_back(newfd);
+      else std::cout <<"couldn't duplicate old fd " << fd << std::endl;
     }
   }
   closedir(dir);
+  for(unsigned int i = 0; i < oldfds.size(); i++){
+    close(oldfds[i]);
+    int newfd = dup2(newfds[i],oldfds[i]);
+    if(newfd!=oldfds[i]) std::cout <<"couldn't duplicate new fd to old " 
+				   << oldfds[i] << std::endl;
+    close(newfds[i]);
+  }
 }
 
 }
