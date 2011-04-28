b766ed1d0e1fcb491db4d19261270e9d9bff4fb5
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2011-04-28 19:29:37 +0000

    Updating instructions and checks for Jec_V1

--- JetMETCorrections/Modules/test/JetCorrectorOnTheFly_cfg.py
+++ JetMETCorrections/Modules/test/JetCorrectorOnTheFly_cfg.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("myprocess")
+process.TFileService=cms.Service("TFileService",fileName=cms.string('JECplots.root'))
 ##-------------------- Communicate with the DB -----------------------
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -10,11 +11,9 @@ process.GlobalTag.globaltag = 'START38_V14::All'
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.ak5CaloL1Offset.useCondDB = False
 process.ak5PFL1Offset.useCondDB = False
-process.ak5JPTL1Offset.useCondDB = False
-process.ak5L1JPTOffset.offsetService = 'ak5CaloL1Offset'
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
 )
 #############   Define the source file ###############
 process.source = cms.Source("PoolSource",
@@ -23,22 +22,16 @@ process.source = cms.Source("PoolSource",
 process.TFileService=cms.Service("TFileService",fileName=cms.string('plots.root'))
 ##-------------------- User analyzer  --------------------------------
 process.testCalo  = cms.EDAnalyzer('CaloJetCorrectorOnTheFly',
-    JetCorrectionService     = cms.string('ak5CaloL1Offset'),
+    JetCorrectionService     = cms.string('ak5CaloL1L2L3Residual'),
     JetCollectionName        = cms.string('ak5CaloJets'),
     MinRawJetPt              = cms.double(10),
-    Debug                    = cms.bool(False)
+    Debug                    = cms.bool(True)
 )
 process.testPF  = cms.EDAnalyzer('PFJetCorrectorOnTheFly',
     JetCorrectionService     = cms.string('ak5PFL1L2L3Residual'),
     JetCollectionName        = cms.string('ak5PFJets'),
     MinRawJetPt              = cms.double(7),
-    Debug                    = cms.bool(False)
-)
-process.testJPT  = cms.EDAnalyzer('JPTJetCorrectorOnTheFly',
-    JetCorrectionService     = cms.string('ak5L1JPTOffset'),
-    JetCollectionName        = cms.string('JetPlusTrackZSPCorJetAntiKt5'),
-    MinRawJetPt              = cms.double(15),
     Debug                    = cms.bool(True)
 )
-process.p = cms.Path(process.testCalo * process.testPF * process.testJPT)
+process.p = cms.Path(process.testCalo * process.testPF)
 
