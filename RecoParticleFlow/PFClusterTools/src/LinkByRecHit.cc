ec6846b5c6ca8ccc84a46dd140cf78bdee85a9ca
Rishi Gautam Patel <sha1-e65bb9bd7753fe5cc17f9bad497a1c07a01f7e34@cern.ch>
2011-04-30 18:14:50 +0000

    Fix for using PFRootEvent with imported Tracks

--- RecoParticleFlow/PFClusterTools/src/LinkByRecHit.cc
+++ RecoParticleFlow/PFClusterTools/src/LinkByRecHit.cc
@@ -7,7 +7,7 @@
 //#define PFLOW_DEBUG
 
 double 
-LinkByRecHit::testTrackAndClusterByRecHit ( const reco::PFRecTrack& track, 
+LinkByRecHit::testTrackAndClusterByRecHit ( const reco::PFRecTrack& track_tmp, 
 					    const reco::PFCluster&  cluster,
 					    bool isBrem,
 					    bool debug)  {
@@ -16,6 +16,16 @@ LinkByRecHit::testTrackAndClusterByRecHit ( const reco::PFRecTrack& track,
   if( debug ) 
     std::cout<<"entering test link by rechit function"<<std::endl;
 #endif
+  
+  reco::PFRecTrack track(track_tmp);
+  const reco::PFTrajectoryPoint& atECAL_tmp
+    = track_tmp.extrapolatedPoint( reco::PFTrajectoryPoint::ECALShowerMax );
+  if(std::abs(atECAL_tmp.positionREP().Eta())<1E-9 &&
+     std::abs(atECAL_tmp.positionREP().Phi())<1E-9 &&
+     atECAL_tmp.positionREP().R()<1E-9) {
+    // needs to be recomputed 
+    track.calculatePositionREP();
+  }
 
   //cluster position
   double clustereta  = cluster.positionREP().Eta();
