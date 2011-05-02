93e24858f8fe729ccaa16f1de934784b057e266e
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2011-05-02 16:09:31 +0000

    class to build links to a generic tracker track collection, made of from several ROI tracks

---
+++ RecoMuon/MuonIdentification/plugins/MuonLinksProducerForHLT.cc
@@ -0,0 +1,72 @@
+/** \class MuonLinksProducerForHLT
+ *
+ *  $Date: $
+ *  $Revision:$
+ *  \author R. Bellan - UCSB <riccardo.bellan@cern.ch>
+ */
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+//#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonTrackLinks.h"
+#include "RecoMuon/MuonIdentification/plugins/MuonLinksProducerForHLT.h"
+
+//#include <algorithm>
+
+MuonLinksProducerForHLT::MuonLinksProducerForHLT(const edm::ParameterSet& iConfig)
+{
+   produces<reco::MuonTrackLinksCollection>();
+   theLinkCollectionInInput = iConfig.getParameter<edm::InputTag>("LinkCollection");
+   theInclusiveTrackCollectionInInput = iConfig.getParameter<edm::InputTag>("InclusiveTrackerTrackCollection");
+}
+
+MuonLinksProducerForHLT::~MuonLinksProducerForHLT()
+{
+}
+
+void MuonLinksProducerForHLT::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   std::auto_ptr<reco::MuonTrackLinksCollection> output(new reco::MuonTrackLinksCollection());
+
+   edm::Handle<reco::MuonTrackLinksCollection> links; 
+   iEvent.getByLabel(theLinkCollectionInInput, links);
+
+   edm::Handle<reco::TrackCollection> incTracks; 
+   iEvent.getByLabel(theInclusiveTrackCollectionInInput, incTracks);
+
+   unsigned int trackIndex = 0;
+   for(reco::MuonTrackLinksCollection::const_iterator link = links->begin(); 
+       link != links->end(); ++link){
+     bool found = false;
+     for(reco::TrackCollection::const_iterator track = incTracks->begin();
+	 track != incTracks->end(); ++track, ++trackIndex){      
+       if(found) continue;
+       if(track->momentum() == link->trackerTrack()->momentum()){
+	 output->push_back(reco::MuonTrackLinks(reco::TrackRef(incTracks,trackIndex), 
+						link->standAloneTrack(), 
+						link->globalTrack() ) );
+	 found = true;
+       }
+     }
+     if (!found) 
+       output->push_back(reco::MuonTrackLinks(link->trackerTrack(), 
+					      link->standAloneTrack(), 
+					      link->globalTrack() ) );
+   }  
+   iEvent.put( output );
+}
