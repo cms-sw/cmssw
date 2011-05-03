9ac87c97ea09195dedcc40e7d6cb7f27f82578e6
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2011-05-03 14:59:30 +0000

    first batch of adding 2011 skims: DiPhoton, DoublePhoton HWWMuMu, HWWElEl, HWWElMu

--- Configuration/Skimming/python/autoSkim.py
+++ Configuration/Skimming/python/autoSkim.py
@@ -6,10 +6,16 @@ autoSkim = {
     'Mu' : 'WMu+ZMu+HighMET+LogError',    
     'EG':'WElectron+ZElectron+HighMET+LogError',
     'Electron':'WElectron+ZElectron+HighMET+LogError',
-    'Photon':'WElectron+ZElectron+HighMET+LogError',
+    'Photon':'WElectron+ZElectron+HighMET+LogError+DiPhoton+DoublePhoton',
     'JetMETTau':'LogError+DiJet+Tau',
     'JetMET':'HighMET+LogError+DiJet',
     'BTau':'LogError+Tau',
     'Jet':'HighMET+LogError+DiJet',
-    'METFwd':'HighMET+LogError'
+    'METFwd':'HighMET+LogError',
+
+    'SingleMu' : 'WMu+ZMu+HighMET+LogError+HWWMuMu',
+    'DoubleMu' : 'WMu+ZMu+HighMET+LogError+HWWMuMu',
+    'DoubleElectron' : 'LogError+HWWElEl',
+    'MuEG' : 'LogError+HWWElMu',
+    
     }
