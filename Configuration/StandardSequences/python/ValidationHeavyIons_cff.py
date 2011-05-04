eb3fc44a1419bf0c4144e954cbead32fa1931a67
Marco Rovere <sha1-46e684da570041e629f20e4154a1c32d32598aaf@cern.ch>
2011-05-04 15:20:25 +0000

    Final fix for the "Filters/Producers on EndPath" problem in the HI validation sequence by moving all filters/producers to the prevalidation path.

--- Configuration/StandardSequences/python/ValidationHeavyIons_cff.py
+++ Configuration/StandardSequences/python/ValidationHeavyIons_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.Configuration.ValidationHI_cff import *
 
-prevalidation = cms.Sequence(cms.SequencePlaceholder("mix"))
+prevalidation = cms.Sequence(cms.SequencePlaceholder("mix") * globalPrevalidationHI * hltPrevalidationHI )
+
 validation = cms.Sequence(validationHI)
 
