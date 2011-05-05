47eddf447ac154cb33a4bcef7b8cb871f68efec7
Andrew York <sha1-3a5714195ac460c33b89c67d050e2d8afc5fe72c@cern.ch>
2011-05-05 16:58:53 +0000

    added assignment of detId to type 25 and 29 errors

--- EventFilter/SiPixelRawToDigi/src/ErrorChecker.cc
+++ EventFilter/SiPixelRawToDigi/src/ErrorChecker.cc
@@ -205,7 +205,7 @@ void ErrorChecker::conversionError(int fedId, const SiPixelFrameConverter* conve
   };
 }
 
-// this function finds the detId for an error word, which cannot be processed in word2digi
+// this function finds the detId for an error word that cannot be processed in word2digi
 uint32_t ErrorChecker::errorDetId(const SiPixelFrameConverter* converter, 
     int errorType, const Word32 & word) const
 {
@@ -214,7 +214,7 @@ uint32_t ErrorChecker::errorDetId(const SiPixelFrameConverter* converter,
   ElectronicIndex cabling;
 
   switch (errorType) {
-    case  30 : case  31: case  36: {
+    case  25 : case  30 : case  31 : case  36 : {
       // set dummy values for cabling just to get detId from link if in Barrel
       cabling.dcol = 0;
       cabling.pxid = 2;
@@ -224,9 +224,41 @@ uint32_t ErrorChecker::errorDetId(const SiPixelFrameConverter* converter,
       DetectorIndex detIdx;
       int status = converter->toDetector(cabling, detIdx);
       if (status) break;
-      
       if(DetId(detIdx.rawId).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) return detIdx.rawId;
       break;
+    }
+    case  29 : {
+      int chanNmbr = 0;
+      const int DB0_shift = 0;
+      const int DB1_shift = DB0_shift + 1;
+      const int DB2_shift = DB1_shift + 1;
+      const int DB3_shift = DB2_shift + 1;
+      const int DB4_shift = DB3_shift + 1;
+      const uint32_t DataBit_mask = ~(~uint32_t(0) << 1);
+
+      int CH1 = (word >> DB0_shift) & DataBit_mask;
+      int CH2 = (word >> DB1_shift) & DataBit_mask;
+      int CH3 = (word >> DB2_shift) & DataBit_mask;
+      int CH4 = (word >> DB3_shift) & DataBit_mask;
+      int CH5 = (word >> DB4_shift) & DataBit_mask;
+      int BLOCK_bits      = 3;
+      int BLOCK_shift     = 8;
+      uint32_t BLOCK_mask = ~(~uint32_t(0) << BLOCK_bits);
+      int BLOCK = (word >> BLOCK_shift) & BLOCK_mask;
+      int localCH = 1*CH1+2*CH2+3*CH3+4*CH4+5*CH5;
+      if (BLOCK%2==0) chanNmbr=(BLOCK/2)*9+localCH;
+      else chanNmbr = ((BLOCK-1)/2)*9+4+localCH;
+      if ((chanNmbr<1)||(chanNmbr>36)) break;  // signifies unexpected result
+      
+      // set dummy values for cabling just to get detId from link if in Barrel
+      cabling.dcol = 0;
+      cabling.pxid = 2;
+      cabling.roc  = 1;
+      cabling.link = chanNmbr;  
+      DetectorIndex detIdx;
+      int status = converter->toDetector(cabling, detIdx);
+      if (status) break;
+      if(DetId(detIdx.rawId).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) return detIdx.rawId;
     }
     case  37 : case  38: {
       cabling.dcol = 0;
