a264868e20228dece9718274538231196b261966
Tae Jeong Kim <sha1-32b43f0d0700c58cd86e2e757255aa5f208dff55@cern.ch>
2011-05-09 13:21:36 +0000

    add for new RPCDQM object for RPCPVT project

---
+++ CondFormats/DataRecord/src/RPCDQMObjectRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCDQMObjectRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      
+// Created:     Mon Feb  7 17:00:06 CET 2011
+// $Id: RPCDQMObjectRcd.cc,v 1.1 2011/02/07 16:41:23 tjkim Exp $
+
+#include "CondFormats/DataRecord/interface/RPCDQMObjectRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(RPCDQMObjectRcd);
