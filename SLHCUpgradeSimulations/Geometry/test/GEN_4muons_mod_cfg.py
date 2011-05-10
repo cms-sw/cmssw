a210ba7482ef71b122794c40830cff30855e1a31
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2011-05-10 22:42:34 +0000

    Standardize the Naming Convention

--- SLHCUpgradeSimulations/Geometry/test/GEN_4muons_mod_cfg.py
+++ SLHCUpgradeSimulations/Geometry/test/GEN_4muons_mod_cfg.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.303 
 # Source: /cvs/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: SLHCUpgradeSimulations/Configuration/python/FourMuPt_1_50_cfi.py -s GEN,SIM -n 10 --conditions auto:mc --eventcontent FEVTDEBUG --beamspot Gauss --slhc R39F16 --no_exec --python_filename RECO_4muons_cfg.py
+# with command line options: SLHCUpgradeSimulations/Configuration/python/FourMuPt_1_50_cfi.py -s GEN,SIM --conditions DESIGN42_V10::All --eventcontent FEVTDEBUG --beamspot Gauss --slhc Phase1_R39F16 --datatier GEN-SIM --python_filename GEN_4muons_mod_cfg.py --no_exec
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('SIM')
@@ -13,7 +13,7 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('SLHCUpgradeSimulations.Geometry.PhaseI_cmsSimIdealGeometryXML_R39F16_cff')
+process.load('SLHCUpgradeSimulations.Geometry.Phase1_R39F16_cmsSimIdealGeometryXML_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
 process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
@@ -21,25 +21,23 @@ process.load('GeneratorInterface.Core.genFilterSummary_cff')
 process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load('SLHCUpgradeSimulations.Geometry.fakeConditions_Phase1_cff')
 process.load('SLHCUpgradeSimulations.Geometry.fakeConditions_Phase1_R39F16_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(1)
 )
 
 # Input source
 process.source = cms.Source("EmptySource")
 
 process.options = cms.untracked.PSet(
-  wantSummary = cms.untracked.bool(True)
+
 )
-process.Timing =  cms.Service("Timing")
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
-    annotation = cms.untracked.string('SLHCUpgradeSimulations/Configuration/python/FourMuPt_1_50_cfi.py nevts:10'),
+    version = cms.untracked.string('$Revision: 1.303 $'),
+    annotation = cms.untracked.string('SLHCUpgradeSimulations/Configuration/python/FourMuPt_1_50_cfi.py nevts:1'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 
@@ -63,22 +61,23 @@ process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
 
 # Other statements
 process.GlobalTag.globaltag = 'DESIGN42_V10::All'
+
 process.generator = cms.EDProducer("FlatRandomPtGunProducer",
     PGunParameters = cms.PSet(
         MaxPt = cms.double(50.0),
         MinPt = cms.double(0.9),
-        PartID = cms.vint32(-13,-13),
+        PartID = cms.vint32(-13, -13),
         MaxEta = cms.double(2.5),
         MaxPhi = cms.double(3.14159265359),
         MinEta = cms.double(-2.5),
-        MinPhi = cms.double(-3.14159265359) ## in radians
+        MinPhi = cms.double(-3.14159265359)
     ),
-    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
-
+    Verbosity = cms.untracked.int32(0),
     psethack = cms.string('Four mu pt 1 to 50'),
     AddAntiParticle = cms.bool(True),
     firstRun = cms.untracked.uint32(1)
 )
+
 
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
@@ -88,10 +87,7 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
 
 # Schedule definition
-#process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
-process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
-
-# special treatment in case of production filter sequence
-for path in process.paths: 
-    getattr(process,path)._seq = process.generator*getattr(process,path)._seq
-
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
