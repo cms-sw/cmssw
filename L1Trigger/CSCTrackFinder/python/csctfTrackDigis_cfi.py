d688c4830329abe1762b60c2f135347732bc6e97
Justin Hugon <sha1-508125a75ede1393167c23522662573ff579fa72@cern.ch>
2011-05-11 09:23:59 +0000

    updated for new PT LUT 28

--- L1Trigger/CSCTrackFinder/python/csctfTrackDigis_cfi.py
+++ L1Trigger/CSCTrackFinder/python/csctfTrackDigis_cfi.py
@@ -18,7 +18,7 @@ csctfTrackDigis = cms.EDProducer("CSCTFTrackProducer",
 		PTLUT = cms.PSet(
 			LowQualityFlag = cms.untracked.uint32(4),
 			ReadPtLUT = cms.bool(False),
-			PtMethod = cms.untracked.uint32(26)
+			PtMethod = cms.untracked.uint32(28)
 		),
 		
 		# Operational mode control
