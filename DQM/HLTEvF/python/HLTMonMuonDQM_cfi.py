7ffe94fc04f71c4c63d2fbc084fa28d26ec1d65c
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2011-05-13 13:39:10 +0000

    update paths for 1E33

--- DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
+++ DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
@@ -11,27 +11,27 @@ hltMonMuDQM = cms.EDAnalyzer("HLTMuonDQMSource",
     	# L1 muon
 	cms.PSet(
 		directoryName = cms.string('L1PassThrough'),
-		triggerBits = cms.vstring('HLT_L1SingleMu10_v1','HLT_L1SingleMu20_v1','HLT_L1SingleMuOpen_DT_v1','HLT_L1SingleMuOpen_v1')
+		triggerBits = cms.vstring('HLT_L1SingleMu10_v2','HLT_L1SingleMu20_v2','HLT_L1SingleMuOpen_DT_v2','HLT_L1SingleMuOpen_v2')
 	),
     	# L2 muon
 	cms.PSet(
 		directoryName = cms.string('L2PassThrough'),
-		triggerBits = cms.vstring('HLT_L2Mu10_v2','HLT_L2Mu20_v2')
+		triggerBits = cms.vstring('HLT_L2Mu10_v3','HLT_L2Mu20_v3')
 	),
     	# L3 muon
 	cms.PSet(
 		directoryName = cms.string('L3Triggers'),
-		triggerBits = cms.vstring('HLT_Mu20_v2','HLT_Mu24_v2','HLT_Mu30_v2','HLT_Mu3_v4','HLT_Mu5_v4','HLT_Mu8_v2','HLT_IsoMu12_v2','HLT_IsoMu15_v6','HLT_IsoMu17_v6','HLT_IsoMu24_v2','HLT_IsoMu30_v2')
+		triggerBits = cms.vstring('HLT_Mu20_v3','HLT_Mu24_v3','HLT_Mu30_v3', 'HLT_Mu40_v1', 'HLT_Mu3_v5','HLT_Mu5_v5','HLT_Mu8_v3','HLT_IsoMu12_v3','HLT_IsoMu15_v7','HLT_IsoMu17_v7','HLT_IsoMu24_v3','HLT_IsoMu30_v3')
 	),
     	# DoubleMu
 	cms.PSet(
 		directoryName = cms.string('DoubleMu'),
-		triggerBits = cms.vstring('HLT_L1DoubleMu0_v1','HLT_L2DoubleMu0_v2','HLT_DoubleMu3_v4','HLT_DoubleMu6_v2','HLT_DoubleMu7_v2')
+		triggerBits = cms.vstring('HLT_L1DoubleMu0_v2','HLT_L2DoubleMu0_v4','HLT_DoubleMu3_v5','HLT_DoubleMu6_v3','HLT_DoubleMu7_v3')
 	),
 	# No tracker
 	cms.PSet(
 		directoryName = cms.string('Cosmics'),
-		triggerBits = cms.vstring('HLT_L2DoubleMu35_NoVertex_v1')
+		triggerBits = cms.vstring('HLT_L2DoubleMu23_NoVertex_v3', 'HLT_L1SingleMuOpen_AntiBPTX_v2')
 	)
     	# JetStream
 	#cms.PSet(
