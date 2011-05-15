28aa524728fa534e92b6529a4e66b4420a95e174
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2011-05-15 19:25:38 +0000

    Removing obsolete files.

--- TopQuarkAnalysis/Configuration/python/patRefSel_goodVertex_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DPGAnalysis.Skims.goodvertexSkim_cff import primaryVertexFilter
-goodVertex = primaryVertexFilter.clone( vertexCollection = cms.InputTag( 'offlinePrimaryVertices' )
-                                      , minimumNDOF      =  4
-                                      , maxAbsZ          = 24.
-                                      , maxd0            =  2.
-                                      )
