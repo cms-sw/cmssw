6f7a36f3a464f0237823e5296bcc6620f42f90be
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2011-05-16 13:32:19 +0000

    Adapt FastSim to /dev/CMSSW_4_2_0/GRun/V70

--- HLTrigger/Configuration/python/Tools/confdb.py
+++ HLTrigger/Configuration/python/Tools/confdb.py
@@ -30,6 +30,7 @@ class HLTProcess(object):
     "HLT_L1SingleMuOpen_AntiBPTX_v*",
     "HLT_JetE30_NoBPTX*_v*",
     "HLT_JetE50_NoBPTX*_v*",
+    "HLT_JetE50_NoBPTX3BX_NoHalo_v*",
   
   # TODO: paths not supported by FastSim, but for which a recovery should be attempted
     "HLT_Mu3_Track3_Jpsi_v*",
