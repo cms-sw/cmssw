4f307bf7418b882f01ed6155bdf625fb582cdcf7
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2011-05-18 18:26:09 +0000

    New HBHENoiseFilter config.

--- TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
+++ TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
@@ -1,6 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
+# s. https://hypernews.cern.ch/HyperNews/CMS/get/JetMET/1196.html
+HBHENoiseFilter.minIsolatedNoiseSumE        = 999999.
+HBHENoiseFilter.minNumIsolatedNoiseChannels = 999999
+HBHENoiseFilter.minIsolatedNoiseSumEt       = 999999.
 
 from TopQuarkAnalysis.Configuration.patRefSel_eventCleaning_cfi import scrapingFilter
 
