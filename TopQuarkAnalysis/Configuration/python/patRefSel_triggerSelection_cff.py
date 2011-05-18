db70bf21c2a3eabd0a2394630b6a1c319c18e368
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2011-05-18 18:26:47 +0000

    Take the L1 out, can only cause harm with the GlobalTag.

--- TopQuarkAnalysis/Configuration/python/patRefSel_triggerSelection_cff.py
+++ TopQuarkAnalysis/Configuration/python/patRefSel_triggerSelection_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-l1Tag  = cms.InputTag( 'gtDigis' )
+l1Tag  = cms.InputTag( '' ) # skip L1 results, since conflicts with the GlobalTag can occur
 hltTag = cms.InputTag( 'TriggerResults::HLT' )
 
 from HLTrigger.special.hltPhysicsDeclared_cfi import *
