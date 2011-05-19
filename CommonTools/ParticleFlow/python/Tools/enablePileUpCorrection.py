8f8af58e7ebe3bef50a8da54f9ba2db3021c5a97
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2011-05-19 07:31:20 +0000

    bug fix in Sals recipe. improved the replaces so that they dont break the PF2PAT sequences

--- CommonTools/ParticleFlow/python/Tools/enablePileUpCorrection.py
+++ CommonTools/ParticleFlow/python/Tools/enablePileUpCorrection.py
@@ -7,8 +7,10 @@ def enablePileUpCorrectionInPF2PAT( process, postfix, sequence='PF2PAT' ):
 
     # pile up subtraction
     getattr(process,"pfNoPileUp"+postfix).enable = True 
+    getattr(process,"pfPileUp"+postfix).Enable = True 
     getattr(process,"pfPileUp"+postfix).checkClosestZVertex = False 
-    getattr(process,"pfJets"+postfix).Vertices = cms.InputTag('goodOfflinePrimaryVertices')
+    # crashes ... waiting for Sal
+    getattr(process,"pfPileUp"+postfix).Vertices = 'goodOfflinePrimaryVertices'
 
     getattr(process,"pfJets"+postfix).doAreaFastjet = True
     getattr(process,"pfJets"+postfix).doRhoFastjet = False
@@ -22,7 +24,17 @@ def enablePileUpCorrectionInPF2PAT( process, postfix, sequence='PF2PAT' ):
                                                             voronoiRfact = cms.double(0.9)
                                                             )
              )
-    getattr(process,sequence+postfix).replace( getattr(process,"pfNoElectron"+postfix), getattr(process,"pfNoElectron"+postfix)*getattr(process,"kt6PFJets"+postfix) )
+
+    # adding goodOfflinePrimaryVertices before pfPileUp
+    process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')   
+    getattr(process, 'pfNoPileUpSequence'+postfix).replace( getattr(process,"pfPileUp"+postfix),
+                                                            process.goodOfflinePrimaryVertices +
+                                                            getattr(process,"pfPileUp"+postfix) )
+
+    # adding kt6PFJets
+    getattr(process,'pfJetSequence'+postfix).replace( getattr(process,"pfJets"+postfix),
+                                                      getattr(process,"kt6PFJets"+postfix) +
+                                                      getattr(process,"pfJets"+postfix) )
 
 def enablePileUpCorrectionInPAT( process, postfix, sequence ):
     # PAT specific stuff:
