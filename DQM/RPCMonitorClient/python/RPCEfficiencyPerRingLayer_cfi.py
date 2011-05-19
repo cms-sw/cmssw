f775be0f8350d3750c7eed585e67949abbe08a17
Cesare Calabria <sha1-03e633e2c1ed6a61e67c2721ac7bd66746a2297c@cern.ch>
2011-05-19 12:37:03 +0000

    *** empty log message ***

---
+++ DQM/RPCMonitorClient/python/RPCEfficiencyPerRingLayer_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+rpcEfficiencyPerRingLayer = cms.EDAnalyzer("RPCEfficiencyPerRingLayer",
+
+   GlobalFolder = cms.untracked.string('RPC/RPCEfficiency/'),
+   SaveFile = cms.untracked.bool(False),
+   NameFile = cms.untracked.string('RPCEfficiency.root')
+
+)
