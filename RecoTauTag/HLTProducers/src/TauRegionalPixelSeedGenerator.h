8cc8e8a71e44cd7436ffc070970a982f66dcf59c
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2011-05-20 11:00:28 +0000

    possibility of passing the measurement tracker name and ondemand configuration to the region

--- RecoTauTag/HLTProducers/src/TauRegionalPixelSeedGenerator.h
+++ RecoTauTag/HLTProducers/src/TauRegionalPixelSeedGenerator.h
@@ -44,6 +44,14 @@ class TauRegionalPixelSeedGenerator : public TrackingRegionProducer {
       m_deltaPhi     = regionPSet.getParameter<double>("deltaPhiRegion");
       m_jetSrc       = regionPSet.getParameter<edm::InputTag>("JetSrc");
       m_vertexSrc    = regionPSet.getParameter<edm::InputTag>("vertexSrc");
+      m_measurementTracker ="";
+      m_howToUseMeasurementTracker=0;
+      if (regionPSet.exists("measurementTrackerName")){
+	m_measurementTracker = regionPSet.getParameter<std::string>("measurementTrackerName");
+	if (regionPSet.exists("howToUseMeasurementTracker")){
+	  m_howToUseMeasurementTracker = regionPSet.getParameter<double>("howToUseMeasurementTracker");
+	}
+      }
     }
   
     virtual ~TauRegionalPixelSeedGenerator() {}
@@ -88,7 +96,10 @@ class TauRegionalPixelSeedGenerator : public TrackingRegionProducer {
                                                                                                deltaRho,
                                                                                                deltaZVertex,
                                                                                                m_deltaEta,
-                                                                                               m_deltaPhi );
+                                                                                               m_deltaPhi,
+											       m_howToUseMeasurementTracker,
+											       true,
+											       m_measurementTracker);
           result.push_back(etaphiRegion);
       }
 
@@ -105,6 +116,8 @@ class TauRegionalPixelSeedGenerator : public TrackingRegionProducer {
   float m_deltaPhi;
   edm::InputTag m_jetSrc;
   edm::InputTag m_vertexSrc;
+  std::string m_measurementTracker;
+  double m_howToUseMeasurementTracker;
 };
 
 #endif
