a846e1ae514f4bcc227af228deb4936a2a6c2e56
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-05-22 23:08:19 +0000

    Clang fixes.
    * Add explicit cstdio header for fscanf.

--- RecoLocalCalo/EcalDeadChannelRecoveryProducers/src/EcalDeadChannelRecoveryProducers.cc
+++ RecoLocalCalo/EcalDeadChannelRecoveryProducers/src/EcalDeadChannelRecoveryProducers.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Georgios Daskalakis
 //         Created:  Thu Apr 12 17:01:03 CEST 2007
-// $Id: EcalDeadChannelRecoveryProducers.cc,v 1.5 2009/12/14 22:23:56 wmtan Exp $
+// $Id: EcalDeadChannelRecoveryProducers.cc,v 1.6 2010/08/06 20:24:55 wmtan Exp $
 //
 //
 
@@ -38,6 +38,8 @@
 
 
 #include <string>
+#include <cstdio>
+
 using namespace cms;
 using namespace std;
 
