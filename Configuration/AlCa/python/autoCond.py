4444f336521f8eb66b143777d817e73d97b7d598
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-05-24 18:38:40 +0000

    Update GTs for 43X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_43_V2::All',
-             'startup' : 'START43_V2::All',
+autoCond = { 'mc'      : 'MC_43_V3::All',
+             'startup' : 'START43_V3::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_43_V2::All',
-             'hltonline' : 'GR_R_43_V2::All', # same as com10 until H tag available
+             'com10'   : 'GR_R_43_V3::All',
+             'hltonline' : 'GR_R_43_V3::All', # same as com10 until H tag available
              'starthi' : 'STARTHI43_V0::All'
              }
