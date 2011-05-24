375f227ca9c8014a162881abae3d2f259815a3cc
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2011-05-24 18:25:07 +0000

    Add correct header file

---
+++ PhysicsTools/Utilities/interface/LumiReWeighting.h
@@ -0,0 +1,64 @@
+#ifndef PhysicsTools_Utilities_interface_LumiReWeighting_h
+#define PhysicsTools_Utilities_interface_LumiReWeighting_h
+
+
+/**
+  \class    LumiReWeighting LumiReWeighting.h "PhysicsTools/Utilities/interface/LumiReWeighting.h"
+  \brief    Class to provide lumi weighting for analyzers to weight "flat-to-N" MC samples to data
+
+  This class will trivially take two histograms:
+  1. The generated "flat-to-N" distributions from a given processing
+  2. A histogram generated from the "estimatePileup" macro here:
+
+  https://twiki.cern.ch/twiki/bin/view/CMS/LumiCalc#How_to_use_script_estimatePileup
+
+  \author Salvatore Rappoccio
+*/
+
+#include "TH1.h"
+#include "TFile.h"
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventPrincipal.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Selector.h"
+#include "DataFormats/Provenance/interface/Provenance.h"
+#include "DataFormats/Provenance/interface/ProductID.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+
+namespace edm {
+  class LumiReWeighting {
+  public:
+    LumiReWeighting( std::string generatedFile,
+		     std::string dataFile,
+		     std::string histName1,
+		     std::string histName2);
+    
+    LumiReWeighting( std::vector< float > MC_distr, std::vector< float > Lumi_distr);
+
+
+    double weight( int npv ) const;
+
+    double weight( const edm::Event &e ) const;
+
+
+    
+  protected:
+
+    std::string generatedFileName_;
+    std::string dataFileName_;
+    std::string histName1_;
+    std::string histName2_;
+    boost::shared_ptr<TFile>     generatedFile_;
+    boost::shared_ptr<TFile>     dataFile_;
+    boost::shared_ptr<TH1F>      weights_;
+  };
+}
+
+
+
+#endif
