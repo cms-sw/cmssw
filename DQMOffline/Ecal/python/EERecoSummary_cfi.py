99615961c64a62e00b248cdf8daba1a8011d5bd0
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2011-05-26 15:06:44 +0000

    update for changes in Ecal basic clusters

--- DQMOffline/Ecal/python/EERecoSummary_cfi.py
+++ DQMOffline/Ecal/python/EERecoSummary_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 ecalEndcapRecoSummary = cms.EDAnalyzer("EERecoSummary",
     prefixME = cms.untracked.string('EcalEndcap'),    
     superClusterCollection_EE = cms.InputTag("correctedMulti5x5SuperClustersWithPreshower"),
-    basicClusterCollection_EE = cms.InputTag("multi5x5BasicClusters","multi5x5EndcapBasicClusters"),
+    basicClusterCollection_EE = cms.InputTag("multi5x5SuperClusters","multi5x5EndcapBasicClusters"),
     recHitCollection_EE       = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
     redRecHitCollection_EE    = cms.InputTag("reducedEcalRecHitsEE"),
                                     
