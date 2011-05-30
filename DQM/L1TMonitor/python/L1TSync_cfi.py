2227cfb1b6a34ac6f8fd9bfdc75d9f9de9500a55
Joao Arnauth Pela <sha1-88ae29f575b1865d9b0cc50c33c978ba48e7e4d3@cern.ch>
2011-05-30 14:06:24 +0000

    Setting OMDS paths for online

--- DQM/L1TMonitor/python/L1TSync_cfi.py
+++ DQM/L1TMonitor/python/L1TSync_cfi.py
@@ -26,12 +26,12 @@ l1tSyncMonitor = cms.EDAnalyzer("L1TSync",
   inputTagtEvmSource      = cms.InputTag("gtEvmDigis","","DQM"),
 
   # Online
-  #oracleDB   = cms.string("oracle://CMS_OMDS_LB/CMS_TRG_R"),
-  #pathCondDB = cms.string("/nfshome0/centraltspro/secure/"),                
+  oracleDB   = cms.string("oracle://CMS_OMDS_LB/CMS_TRG_R"),
+  pathCondDB = cms.string("/nfshome0/centraltspro/secure/"),                
 
   # Offline
-  oracleDB   = cms.string("oracle://cms_orcoff_prod/CMS_COND_31X_L1T"), # For offline
-  pathCondDB = cms.string("/afs/cern.ch/cms/DB/conddb"),                
+  #oracleDB   = cms.string("oracle://cms_orcoff_prod/CMS_COND_31X_L1T"), # For offline
+  #pathCondDB = cms.string("/afs/cern.ch/cms/DB/conddb"),                
 
   # Index for the prescale set to be used 
   # as reference
