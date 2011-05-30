b7c93243fa99c589b699b38f96249abb63cd7905
Antonio Vilela Pereira <sha1-99b95d901683f11ac96ebd76fd2bbc9bee4e1aa0@cern.ch>
2011-05-30 18:47:42 +0000

    add ALCARECO DQM sequence for Cosmics

---
+++ DQM/DTMonitorModule/python/ALCARECODTCalibSynchCosmicsDQM_cff.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+from DQMOffline.CalibMuon.dtPreCalibrationTask_cfi import *
+from DQM.DTMonitorModule.dtResolutionTask_cfi import *
+from DQM.DTMonitorModule.dtTriggerSynchTask_cfi import *
+
+dtPreCalibrationTaskAlcaCosmics = dtPreCalibTask.clone()
+dtPreCalibrationTaskAlcaCosmics.SaveFile = False
+dtPreCalibrationTaskAlcaCosmics.folderName = 'AlCaReco/DtCalibSynchCosmics/01-Calibration'
+
+dtAlcaResolutionMonitorCosmics = dtResolutionAnalysisMonitor.clone()
+dtAlcaResolutionMonitorCosmics.topHistoFolder = "AlCaReco/DtCalibSynchCosmics/01-Calibration"
+ 
+#dtTriggerSynchMonitorCosmics = dtTriggerSynchMonitor.clone()
+#dtTriggerSynchMonitorCosmics.baseDir = 'AlCaReco/DtCalibSynchCosmics/02-Synchronization'             
+#dtTriggerSynchMonitorCosmics.SEGInputTag = 'dt4DSegments'             
+#dtTriggerSynchMonitorCosmics.rangeWithinBX  = False
+#dtTriggerSynchMonitorCosmics.nBXHigh        = 3
+#dtTriggerSynchMonitorCosmics.nBXLow         = -2
+
+from DQM.HLTEvF.HLTMonBitSummary_cfi import hltMonBitSummary
+from CalibMuon.DTCalibration.ALCARECODtCalibCosmics_cff import ALCARECODtCalibCosmicsHLTFilter
+ALCARECODtCalibCosmicsHLTDQM = hltMonBitSummary.clone(
+    directory = 'AlCaReco/DtCalibSynchCosmics/HLTSummary',
+    histLabel = 'DtCalibSynchCosmics',
+    HLTPaths = ["HLT_.*Mu.*"],
+    eventSetupPathsKey =  ALCARECODtCalibCosmicsHLTFilter.eventSetupPathsKey.value()
+)
+
+ALCARECODTCalibSynchCosmicsDQM = cms.Sequence( dtPreCalibrationTaskAlcaCosmics +
+                                               dtAlcaResolutionMonitorCosmics + 
+                                               ALCARECODtCalibCosmicsHLTDQM )
