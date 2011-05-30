4f2ceab64311a7da91bc6630d74a079df71630a2
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-05-30 17:31:55 +0000

    Remove ommented out code

--- HiggsAnalysis/CombinedLimit/src/ProfiledLikelihoodRatioTestStat.cc
+++ HiggsAnalysis/CombinedLimit/src/ProfiledLikelihoodRatioTestStat.cc
@@ -13,24 +13,14 @@ Double_t ProfiledLikelihoodRatioTestStat::Evaluate(RooAbsData& data, RooArgSet& 
     *paramsNull_ = nuisances_;
     *paramsNull_ = snapNull_;
     *paramsNull_ = nullPOI;
-/*
-    if (nullPOI.getSize()) {
-        RooRealVar *var = dynamic_cast<RooRealVar *>(paramsNull_->find(nullPOI.first()->GetName()));
-        if (var) var->setConstant(true);
-    }
-*/
+
     pdfNull_->fitTo(data, RooFit::Constrain(nuisances_), RooFit::Hesse(0), RooFit::PrintLevel(-1), RooFit::PrintEvalErrors(-1));
     double nullNLL = pdfNull_->getVal();
 
     *paramsAlt_ = *entry;
     *paramsAlt_ = nuisances_;
     *paramsAlt_ = snapAlt_;
-/*
-    if (snapAlt_.getSize()) {
-        RooRealVar *var = dynamic_cast<RooRealVar *>(paramsAlt_->find(snapAlt_.first()->GetName()));
-        if (var) var->setConstant(true);
-    }
-*/
+
     pdfAlt_->fitTo(data, RooFit::Constrain(nuisances_), RooFit::Hesse(0), RooFit::PrintLevel(-1), RooFit::PrintEvalErrors(-1));
 
     double altNLL = pdfAlt_->getVal();
