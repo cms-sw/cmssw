32c38008ebf9966af961cd8f8ec947164bbbac8b
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2011-05-31 17:41:18 +0000

    added TimeMaps

--- RecoMuon/MuonIdentification/plugins/MuonProducer.cc
+++ RecoMuon/MuonIdentification/plugins/MuonProducer.cc
@@ -1,9 +1,9 @@
 /** \class MuonProducer
- *  No description available.
+ *  See header file.
  *
- *  $Date: 2010/03/25 14:08:49 $
- *  $Revision: 1.2 $
- *  \author R. Bellan - INFN Torino <riccardo.bellan@cern.ch>
+ *  $Date: 2011/05/31 14:47:01 $
+ *  $Revision: 1.3 $
+ *  \author R. Bellan - UCSB <riccardo.bellan@cern.ch>
  */
 
 #include "RecoMuon/MuonIdentification/plugins/MuonProducer.h"
@@ -16,6 +16,8 @@
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/MuonReco/interface/MuonTrackLinks.h"
 #include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/MuonTimeExtra.h"
+#include "DataFormats/MuonReco/interface/MuonTimeExtraMap.h"
 
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
@@ -43,10 +45,10 @@ MuonProducer::MuonProducer(const edm::ParameterSet& pSet):debug_(pSet.getUntrack
   //  setAlias(pSet.getParameter<std::string>("@module_label"));
   //  produces<reco::MuonCollection>().setBranchAlias(theAlias + "s");
   produces<reco::MuonCollection>();
-//   produces<reco::MuonTimeExtraMap>("combined");
-//   produces<reco::MuonTimeExtraMap>("dt");
-//   produces<reco::MuonTimeExtraMap>("csc");
-
+  produces<reco::MuonTimeExtraMap>("combined");
+  produces<reco::MuonTimeExtraMap>("dt");
+  produces<reco::MuonTimeExtraMap>("csc");
+  
 //  if (fillIsolation_ && writeIsoDeposits_){
 //      trackDepositName_ = iConfig.getParameter<std::string>("trackDepositName");
 //      produces<reco::IsoDepositMap>(trackDepositName_);
@@ -74,15 +76,43 @@ void MuonProducer::produce(edm::Event& event, const edm::EventSetup& eventSetup)
 
    // the muon collection, it will be loaded in the event
    std::auto_ptr<reco::MuonCollection> outputMuons(new reco::MuonCollection());
-   // FIXME: need to update the asso map too!!!!!!
-   
 
    edm::Handle<reco::MuonCollection> inputMuons; 
    event.getByLabel(theMuonsCollectionLabel, inputMuons);
-
+   
    edm::Handle<reco::PFCandidateCollection> pfCandidates; 
    event.getByLabel(thePFCandLabel, pfCandidates);
 
+
+   // FIXME! Add map between old and new muons! (useful for the following PF step)
+
+   // FIXME: need to update the asso map too!!!!!!
+   
+   // Fill timing information
+   std::auto_ptr<reco::MuonTimeExtraMap> muonTimeMap(new reco::MuonTimeExtraMap());
+   reco::MuonTimeExtraMap::Filler filler(*muonTimeMap);
+   std::auto_ptr<reco::MuonTimeExtraMap> muonTimeMapDT(new reco::MuonTimeExtraMap());
+   reco::MuonTimeExtraMap::Filler fillerDT(*muonTimeMapDT);
+   std::auto_ptr<reco::MuonTimeExtraMap> muonTimeMapCSC(new reco::MuonTimeExtraMap());
+   reco::MuonTimeExtraMap::Filler fillerCSC(*muonTimeMapCSC);
+
+
+   edm::Handle<reco::MuonTimeExtraMap> timeMapCmb;
+   edm::Handle<reco::MuonTimeExtraMap> timeMapDT;
+   edm::Handle<reco::MuonTimeExtraMap> timeMapCSC;
+   
+   int nMuons=inputMuons->size();
+
+   std::vector<reco::MuonTimeExtra> dtTimeColl(nMuons);
+   std::vector<reco::MuonTimeExtra> cscTimeColl(nMuons);
+   std::vector<reco::MuonTimeExtra> combinedTimeColl(nMuons);
+
+
+   event.getByLabel(theMuonsCollectionLabel.label(),"combined",timeMapCmb);
+   event.getByLabel(theMuonsCollectionLabel.label(),"dt",timeMapDT);
+   event.getByLabel(theMuonsCollectionLabel.label(),"csc",timeMapCSC);
+   
+   
 
    if(inputMuons->empty()) {
      // FIXME! Needs to check that all the default variables (the new one) are properly set
@@ -109,13 +139,14 @@ void MuonProducer::produce(edm::Event& event, const edm::EventSetup& eventSetup)
 
 
    reco::MuonRef::key_type muIndex = 0;
+   unsigned int i = 0;
    foreach(const reco::Muon &inMuon, *inputMuons){
-	  
+     
      reco::MuonRef muRef(inputMuons, muIndex++);
-
+     
      // Copy the muon 
      reco::Muon outMuon = inMuon;
-
+     
      // search for the corresponding pf candidate
      MuToPFMap::iterator iter =  muToPFMap.find(muRef);
      if(iter != muToPFMap.end()){
@@ -127,22 +158,45 @@ void MuonProducer::produce(edm::Event& event, const edm::EventSetup& eventSetup)
 	    << " PF p4: " << outMuon.pfP4() << endl;
      }
      
-
+     
      dout << "MuonRef: " << muRef.id() << " " << muRef.key() 
 	  << " Is it PF? " << outMuon.isPFMuon() << endl;
-   
+     
      dout << "GLB "  << outMuon.isGlobalMuon()
 	  << " TM "  << outMuon.isTrackerMuon()
 	  << " STA " << outMuon.isStandAloneMuon() 
 	  << " p4 "  << outMuon.p4() << endl;
 
-       // FIXME: Fill iso quantities too!
+     
+     // Fill timing information
+     
+     
+     combinedTimeColl[i] = (*timeMapCmb)[muRef];
+     dtTimeColl[i] = (*timeMapDT)[muRef];
+     cscTimeColl[i] = (*timeMapCSC)[muRef];
+          
 
+
+     // FIXME: Fill iso quantities too!
+    
 
        
-     outputMuons->push_back(outMuon);    
+     outputMuons->push_back(outMuon); 
+     ++i;
    }
 
    dout << "Number of Muons in the new muon collection: " << outputMuons->size() << endl;
-   event.put(outputMuons);
+   edm::OrphanHandle<reco::MuonCollection> muonHandle = event.put(outputMuons);
+
+   filler.insert(muonHandle, combinedTimeColl.begin(), combinedTimeColl.end());
+   filler.fill();
+   fillerDT.insert(muonHandle, dtTimeColl.begin(), dtTimeColl.end());
+   fillerDT.fill();
+   fillerCSC.insert(muonHandle, cscTimeColl.begin(), cscTimeColl.end());
+   fillerCSC.fill();
+
+   event.put(muonTimeMap,"combined");
+   event.put(muonTimeMapDT,"dt");
+   event.put(muonTimeMapCSC,"csc");
+   
 }
