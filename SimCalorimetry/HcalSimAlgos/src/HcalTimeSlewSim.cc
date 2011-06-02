ce2d130433983f49cdfe74d89a8914b46e3a4dec
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-06-02 00:36:57 +0000

    New, more straightforward than HcalHitCorrection, implementation of time delay for small signals

---
+++ SimCalorimetry/HcalSimAlgos/src/HcalTimeSlewSim.cc
@@ -0,0 +1,67 @@
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalTimeSlewSim.h"
+#include "SimCalorimetry/CaloSimAlgos/interface/CaloSimParameters.h"
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameters.h"
+#include "CalibCalorimetry/HcalAlgos/interface/HcalTimeSlew.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalZDCDetId.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+HcalTimeSlewSim::HcalTimeSlewSim(const CaloVSimParameterMap * parameterMap)
+  : theParameterMap(parameterMap),theRandGaussQ(0) 
+{
+}
+
+
+
+double HcalTimeSlewSim::charge(const CaloSamples & samples) const
+{
+  double totalCharge = 0.;
+  for(int i = 0; i < 4; ++i) {
+    int bin = i + samples.presamples();
+    if(bin < samples.size()) {
+      totalCharge += samples[bin];
+    }
+  }
+  return totalCharge;
+}
+
+
+void HcalTimeSlewSim::delay(CaloSamples & samples) const 
+{
+  // HO goes slow, HF shouldn't be used at all
+  //ZDC not used for the moment
+
+  DetId detId(samples.id());
+  if(detId.det()==DetId::Calo && detId.subdetId()==HcalZDCDetId::SubdetectorId) return;
+  HcalDetId hcalDetId(detId);
+
+  if(hcalDetId.subdet() == HcalBarrel || hcalDetId.subdet() == HcalEndcap || hcalDetId.subdet() == HcalOuter ) {
+
+    HcalTimeSlew::BiasSetting biasSetting = (hcalDetId.subdet() == HcalOuter) ?
+      HcalTimeSlew::Slow :
+      HcalTimeSlew::Medium;
+
+    double totalCharge = charge(samples);
+    double delay = HcalTimeSlew::delay(totalCharge, biasSetting);
+    // now, the smearing
+    const HcalSimParameters& params=static_cast<const HcalSimParameters&>(theParameterMap->simParameters(detId));
+    if (params.doTimeSmear() && theRandGaussQ!=0) {
+      double rms=params.timeSmearRMS(totalCharge);
+      double smearns=theRandGaussQ->fire()*rms;
+
+      LogDebug("HcalTimeSlewSim") << "TimeSmear charge " << totalCharge << " rms " << rms << " delay " << delay << " smearns " << smearns;
+      delay+=smearns;
+    }
+
+    samples.offsetTime(delay);
+  }
+}
+
+
+void HcalTimeSlewSim::setRandomEngine(CLHEP::HepRandomEngine & engine) {
+  if (theRandGaussQ==0) {
+    theRandGaussQ=new CLHEP::RandGaussQ(engine);
+  }
+}
