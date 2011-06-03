79ae98d2488f003731d853c73c090c431eff895b
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2011-06-03 19:17:00 +0000

    Update to current Geometry code conventions

--- SimDataFormats/EcalTestBeam/interface/HodoscopeDetId.h
+++ SimDataFormats/EcalTestBeam/interface/HodoscopeDetId.h
@@ -5,42 +5,41 @@
 #include <cmath>
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
 
 /** \class HodoscopeDetId
  *  Hodoscope fiber identifier class for the ECAL TBH4 setup
  *
  *
- *  $Id$
+ *  $Id: HodoscopeDetId.h,v 1.1 2007/03/16 19:33:23 fabiocos Exp $
  */
 
+// bkh June 2011: must be a calo detid type that is recognized by
+//                CaloGenericDetId for use of its denseIndex() fcn.
+//                Hence choose CaloTowerDetId to inherit from.
 
-class HodoscopeDetId : public DetId {
- public:
-  /** Constructor of a null id */
-  HodoscopeDetId();
-  /** Constructor from a raw value */
-  HodoscopeDetId(uint32_t rawid);
-  /** Constructor from crystal ieta and iphi 
-      or from SM# and crystal# */
-  HodoscopeDetId(int indexPlane, int indexFibr);
-  /** Constructor from a generic cell id */
-  HodoscopeDetId(const DetId& id);
-  /** Assignment operator from cell id */
-  HodoscopeDetId& operator=(const DetId& id);
+class HodoscopeDetId : public CaloTowerDetId 
+{
+   public:
 
-  /// get the subdetector
-  EcalSubdetector subdet() const { return EcalSubdetector(subdetId()); }
+      HodoscopeDetId();
+      HodoscopeDetId( uint32_t rawid ) ;
+      HodoscopeDetId( int iPlane, int iFibr ) ;
+      HodoscopeDetId( const DetId& id ) ;
 
-  int planeId() const { return id_&0x3 ; }
+      int planeId() const ;
 
-  int fibrId() const { return (id_>>2)&0x3F ; }
+      int fibrId() const ;
 
-  /// range constants
-  static const int MIN_PLANE = 0;
-  static const int MAX_PLANE = 3;
-  static const int MIN_FIBR = 0;
-  static const int MAX_FIBR = 63;
-    
+      static bool validDetId( int iPlane , int iFibr ) ;
+
+      /// range constants
+
+      static const int MIN_PLANE =  0 ;
+      static const int MAX_PLANE =  3 ;
+      static const int MIN_FIBR  =  0 ;
+      static const int MAX_FIBR  = 63 ;
+
 };
 
 std::ostream& operator<<(std::ostream& s,const HodoscopeDetId& id);
--- SimDataFormats/EcalTestBeam/src/HodoscopeDetId.cc
+++ SimDataFormats/EcalTestBeam/src/HodoscopeDetId.cc
@@ -2,42 +2,61 @@
 #include "FWCore/Utilities/interface/Exception.h"
 
 #include <iostream>
-HodoscopeDetId::HodoscopeDetId() : DetId() {
-}
-  
-HodoscopeDetId::HodoscopeDetId(uint32_t rawid) : DetId(rawid) {
-}
-  
-// use the LaserPnDiode as sub-detector to avoid to create a new one
-
-HodoscopeDetId::HodoscopeDetId(int indexPlane, int indexFibr) 
-  : DetId(Ecal,EcalLaserPnDiode)
+HodoscopeDetId::HodoscopeDetId() : 
+   CaloTowerDetId() 
 {
-  int iPlane = indexPlane;
-  int iFibr = indexFibr;
-  if (iPlane < MIN_PLANE || iPlane > MAX_PLANE ||
-      iFibr < MIN_FIBR || iFibr > MAX_FIBR) {
-    throw cms::Exception("InvalidDetId") << "HodoscopeDetId:  Cannot create object.  Indexes out of bounds.";
-  }
-  id_ |= ((iPlane&0x3) | ((iFibr&0x3F)<<2)) ;
+}
+
+HodoscopeDetId::HodoscopeDetId( uint32_t rawid ) : 
+   CaloTowerDetId( rawid ) 
+{
+}
+
+int 
+HodoscopeDetId::planeId() const 
+{
+   return ieta() ; 
+}
+
+int 
+HodoscopeDetId::fibrId() const 
+{ 
+   return iphi() ; 
+}
+
+HodoscopeDetId::HodoscopeDetId( int iPlane ,
+				int iFibr    ) 
+   : CaloTowerDetId( iPlane, iFibr )
+{
+   if( !validDetId( iPlane, iFibr ) )
+   {
+      throw cms::Exception("InvalidDetId") 
+	 << "HodoscopeDetId:  Cannot create object.  Indices out of bounds.";
+   }
 }
   
-HodoscopeDetId::HodoscopeDetId(const DetId& gen) {
- if (!gen.null() && ( gen.det()!=Ecal || gen.subdetId()!=EcalLaserPnDiode )) {
-    throw cms::Exception("InvalidDetId");
-  }
-  id_=gen.rawId();
+HodoscopeDetId::HodoscopeDetId( const DetId& gen ) :
+   CaloTowerDetId( gen )
+{
+   if( !validDetId( planeId(), fibrId() ) )
+   {
+      throw cms::Exception("InvalidDetId") 
+	 << "HodoscopeDetId:  Cannot create object.  Indices out of bounds.";
+   }
 }
-  
-HodoscopeDetId& HodoscopeDetId::operator=(const DetId& gen) {
-  if (!gen.null() && ( gen.det()!=Ecal || gen.subdetId()!=EcalLaserPnDiode )) {
-    throw cms::Exception("InvalidDetId");
-  }
-  id_=gen.rawId();
-  return *this;
+
+bool 
+HodoscopeDetId::validDetId( int iPlane ,
+			    int iFibr   ) 
+{
+   return !( iPlane < MIN_PLANE || 
+	     iPlane > MAX_PLANE ||
+	     iFibr  < MIN_FIBR  ||
+	     iFibr  > MAX_FIBR     ) ;
 }
-  
-std::ostream& operator<<(std::ostream& s,const HodoscopeDetId& id) {
-  return s << "(Plane " << id.planeId() << ", fiber " << id.fibrId() << ')';
+
+std::ostream& operator<<(std::ostream& s,const HodoscopeDetId& id) 
+{
+   return s << "(Plane " << id.planeId() << ", fiber " << id.fibrId() << ')';
 }
   
