33d5b3af62db802ae8f34beac4168ce84c07521a
Antonio Vilela Pereira <sha1-99b95d901683f11ac96ebd76fd2bbc9bee4e1aa0@cern.ch>
2011-06-03 18:12:20 +0000

    update to ALCARECO config.

--- CalibMuon/DTCalibration/python/ALCARECODtCalibCosmics_cff.py
+++ CalibMuon/DTCalibration/python/ALCARECODtCalibCosmics_cff.py
@@ -7,6 +7,8 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 ALCARECODtCalibCosmicsHLTFilter = copy.deepcopy(hltHighLevel)
 ALCARECODtCalibCosmicsHLTFilter.throw = False ## dont throw on unknown path names
 ALCARECODtCalibCosmicsHLTFilter.eventSetupPathsKey = 'MuAlcaDtCalibCosmics'
+#ALCARECODtCalibCosmicsHLTFilter.HLTPaths = ['HLT_L1SingleMuOpen_AntiBPTX_v*','HLT_L1TrackerCosmics_v*']
+#ALCARECODtCalibCosmicsHLTFilter.eventSetupPathsKey = ''
 
 #import RecoLocalMuon.DTSegment.dt4DSegments_CombPatternReco4D_LinearDriftFromDB_cfi as dt4DSegmentsCfiRef
 #dt4DSegmentsNoWire = dt4DSegmentsCfiRef.dt4DSegments.clone()
