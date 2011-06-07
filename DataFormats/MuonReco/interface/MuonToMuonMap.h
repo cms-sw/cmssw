e5b903b6e73fd911f0f320769dde90685ce391e9
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2011-06-07 15:21:13 +0000

    map to the two muon collection. Usefull for PF step

---
+++ DataFormats/MuonReco/interface/MuonToMuonMap.h
@@ -0,0 +1,13 @@
+#ifndef MuonReco_MuonToMuonMap_h
+#define MuonReco_MuonToMuonMap_h
+
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+
+namespace reco {
+
+typedef edm::ValueMap<reco::MuonRef> MuonToMuonMap;
+
+}
+
+#endif
