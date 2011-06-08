68b826a4fe95751b46664cced9d5a4bfb786e644
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2011-06-08 13:05:15 +0000

    change input collection

--- RecoEcal/EgammaClusterProducers/python/multi5x5PreshowerClusterShape_cfi.py
+++ RecoEcal/EgammaClusterProducers/python/multi5x5PreshowerClusterShape_cfi.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: multi5x5PreshowerClusterShape.cfi,v 1.1 2008/04/29 15:27:38 dlevans Exp $
+# $Id: multi5x5PreshowerClusterShape_cfi.py,v 1.1 2008/05/22 11:13:25 hegner Exp $
 # Preshower cluster producer
 multi5x5PreshowerClusterShape = cms.EDProducer("PreshowerClusterShapeProducer",
     preshStripEnergyCut = cms.double(0.0),
     # building multi5x5Preshower clusters
     preshRecHitProducer = cms.InputTag("ecalPreshowerRecHit","EcalRecHitsES"),
     preshPi0Nstrip = cms.int32(5),
-    endcapSClusterProducer = cms.InputTag("multi5x5SuperClusters","multi5x5EndcapSuperClusters"),
+    endcapSClusterProducer = cms.InputTag('correctedMulti5x5SuperClustersWithPreshower'),
     #    string corrPhoProducer = "correctedPhotons"
     #    string correctedPhotonCollection   = ""
     PreshowerClusterShapeCollectionX = cms.string('multi5x5PreshowerXClustersShape'),
