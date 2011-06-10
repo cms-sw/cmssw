ccf64c743e4169957ed452597c3278ee36d03515
Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch>
2011-06-10 08:15:55 +0000

    remove ic5 JPT jets from default jet reconstruction

--- RecoJets/Configuration/python/RecoJPTJets_cff.py
+++ RecoJets/Configuration/python/RecoJPTJets_cff.py
@@ -1,4 +1,4 @@
-# $Id: RecoJPTJets_cff.py,v 1.2 2010/03/09 03:30:46 srappocc Exp $
+# $Id: RecoJPTJets_cff.py,v 1.3 2010/03/21 19:05:55 dlange Exp $
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
 
 #define jetPlusTrackZSPCorJet sequences
@@ -6,4 +6,4 @@ jetPlusTrackZSPCorJetIcone5   = cms.Sequence(JetPlusTrackCorrectionsIcone5)
 jetPlusTrackZSPCorJetSiscone5 = cms.Sequence(JetPlusTrackCorrectionsSisCone5)
 jetPlusTrackZSPCorJetAntiKt5  = cms.Sequence(JetPlusTrackCorrectionsAntiKt5)
  
-recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetIcone5*jetPlusTrackZSPCorJetAntiKt5)
+recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetAntiKt5)
