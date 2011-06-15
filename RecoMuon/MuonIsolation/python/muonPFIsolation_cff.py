638b3948969be4a1e899bc345b939ff792c5008e
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2011-06-15 15:23:03 +0000

    first part moved in CommonTools/ParticleFlow

--- RecoMuon/MuonIsolation/python/muonPFIsolation_cff.py
+++ RecoMuon/MuonIsolation/python/muonPFIsolation_cff.py
@@ -1,46 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-#Import sequence frokm ParticleFlow
-from CommonTools.ParticleFlow.ParticleSelectors.pfCandsForIsolation_cff import *
-
-
-#Create some additional collection (NEEDS TO BE DISCUSSED WITH E/G friends to not double create) 
-
-
-#Create the PU candidates
-pfPileUpCandidates = cms.EDProducer(
-         "TPPFCandidatesOnPFCandidates",
-         enable =  cms.bool( True ),
-         verbose = cms.untracked.bool( False ),
-         name = cms.untracked.string("pileUpCandidates"),
-         topCollection = cms.InputTag("pfNoPileUp"),
-         bottomCollection = cms.InputTag("particleFlowTmp"),
-)
-
-
-#Take the PU charged particles
-pfPUChargedCandidates = cms.EDFilter("PdgIdPFCandidateSelector",
-                                          src = cms.InputTag("pfPileUpCandidates"),
-                                          pdgId = cms.vint32(211,-211,321,-321,999211,2212,-2212,11,-11,13,-13)
-)
-                                                                                                                        
-
-#Create All Charged Particles
-pfAllChargedCandidates = cms.EDFilter("PdgIdPFCandidateSelector",
-                              src = cms.InputTag("pfNoPileUp"),
-                              pdgId = cms.vint32(211,-211,321,-321,999211,2212,-2212,11,-11,13,-13)
-                          )
-
-
-muonPrePFIsolationSequence =  cms.Sequence(pfCandsForIsolationSequence+
-                                           pfPileUpCandidates+
-                                           pfPUChargedCandidates+
-                                           pfAllChargedCandidates
-)                                         
-
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
 from CommonTools.ParticleFlow.Isolation.tools_cfi import *
 
 
