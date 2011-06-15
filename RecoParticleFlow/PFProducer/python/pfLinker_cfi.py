fbd992e2002323ab9ded463e6fb6bc52389988f1
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2011-06-15 11:52:23 +0000

    linker update

--- RecoParticleFlow/PFProducer/python/pfLinker_cfi.py
+++ RecoParticleFlow/PFProducer/python/pfLinker_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 pfLinker = cms.EDProducer("PFLinker",
-                          PFCandidate = cms.InputTag("particleFlow"),
+                          PFCandidates = cms.VInputTag(cms.InputTag("particleFlow")),
                           GsfElectrons = cms.InputTag("gsfElectrons"),
                           Photons = cms.InputTag("pfPhotonTranslator:pfphot"),
                           Muons = cms.InputTag("muons","muons1stStep2muonsMap"),
@@ -9,4 +9,5 @@ pfLinker = cms.EDProducer("PFLinker",
                           FillMuonRefs = cms.bool(True),
                           OutputPF = cms.string(""),
                           ValueMapElectrons = cms.string("electrons"),                              
-                          ValueMapPhotons = cms.string("photons"))
+                          ValueMapPhotons = cms.string("photons"),
+                          ValueMapMerged = cms.string("all"))
