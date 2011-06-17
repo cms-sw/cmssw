b34f96ca741d9a97af0eb38236f9d4dae5e2b009
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2011-06-17 16:03:45 +0000

    added stuff for Maxime, temporary

--- CommonTools/ParticleFlow/test/PFAOD_cfg.py
+++ CommonTools/ParticleFlow/test/PFAOD_cfg.py
@@ -24,6 +24,36 @@ process.out = cms.OutputModule(
 process.load("CommonTools.ParticleFlow.PF2PAT_EventContent_cff")
 process.out.outputCommands.extend( process.prunedAODForPF2PATEventContent.outputCommands )
 
+# additional stuff for Maxime: 
+process.out.outputCommands.extend(
+    [
+      'keep GenEventInfoProduct_*_*_*',
+      'keep *_ak5GenJets_*_*',
+      'keep *_ak5CaloJets_*_*',
+      'keep *_ak5JetID_*_*',
+      'keep *_ak5JetExtender_*_*',
+      'keep *_ak7GenJets_*_*',
+      'keep *_ak7CaloJets_*_*',
+      'keep *_ak7JetID_*_*',
+      'keep *_ak7JetExtender_*_*',
+      #------- PFJet collections --------
+      'keep *_kt6PFJets_rho_*',
+      'keep *_kt6PFJets_sigma_*',
+      'keep *_ak5PFJets_*_*',        'keep *_ak7PFJets_*_*',
+      #------- Trigger collections ------
+      'keep edmTriggerResults_TriggerResults_*_*',
+      'keep *_hltTriggerSummaryAOD_*_*',
+      'keep L1GlobalTriggerObjectMapRecord_*_*_*',
+      'keep L1GlobalTriggerReadoutRecord_*_*_*',
+      #------- Various collections ------
+      'keep *_EventAuxilary_*_*',
+      'keep *_offlinePrimaryVertices_*_*',
+      'keep *_offlinePrimaryVerticesWithBS_*_*',
+      #------- MET collections ----------
+      'keep *_met_*_*',
+      'keep *_pfMet_*_*'
+    ])
+
 process.endpath = cms.EndPath(
     process.out
     )
