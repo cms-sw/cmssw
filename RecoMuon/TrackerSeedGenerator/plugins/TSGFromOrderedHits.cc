fff23d8363a296e54616cf0d26efd4b2c336e378
Marcin Konecki <sha1-3dd4037a88237b8e967681abdc78f7c551f4f0d0@cern.ch>
2011-06-17 12:16:01 +0000

    In case of change of run reinitialise seed generator.

--- RecoMuon/TrackerSeedGenerator/plugins/TSGFromOrderedHits.cc
+++ RecoMuon/TrackerSeedGenerator/plugins/TSGFromOrderedHits.cc
@@ -4,26 +4,26 @@
 #include "RecoTracker/TkTrackingRegions/interface/OrderedHitsGeneratorFactory.h"
 #include "RecoTracker/TkTrackingRegions/interface/OrderedHitsGenerator.h"
 #include "RecoTracker/TkSeedGenerator/interface/SeedGeneratorFromRegionHits.h"
-#include "RecoTracker/TkSeedGenerator/interface/SeedCreatorFactory.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "DataFormats/Provenance/interface/RunID.h"
+
 
 TSGFromOrderedHits::TSGFromOrderedHits(const edm::ParameterSet &pset)
-  : theConfig(pset), theGenerator(0)
+  : theLastRun(0), theConfig(pset), theGenerator(0)
 {
- 
+	init();
+}
+void TSGFromOrderedHits::init()
+{
   edm::ParameterSet hitsfactoryPSet =
       theConfig.getParameter<edm::ParameterSet>("OrderedHitsFactoryPSet");
   std::string hitsfactoryName = hitsfactoryPSet.getParameter<std::string>("ComponentName");
   OrderedHitsGenerator*  hitsGenerator =
         OrderedHitsGeneratorFactory::get()->create( hitsfactoryName, hitsfactoryPSet);
 
-  // FIXME??
-  edm::ParameterSet creatorPSet;
-  creatorPSet.addParameter<std::string>("propagator","PropagatorWithMaterial");
-  theGenerator = new SeedGeneratorFromRegionHits(hitsGenerator, 0, 
-						 SeedCreatorFactory::get()->create("SeedFromConsecutiveHitsCreator", creatorPSet)
-						 );
-
-
+  if (theGenerator) delete theGenerator;
+  theGenerator = new SeedGeneratorFromRegionHits( hitsGenerator, theConfig);
 }
 
 TSGFromOrderedHits::~TSGFromOrderedHits()
@@ -34,5 +34,7 @@ TSGFromOrderedHits::~TSGFromOrderedHits()
 void TSGFromOrderedHits::run(TrajectorySeedCollection &seeds, 
       const edm::Event &ev, const edm::EventSetup &es, const TrackingRegion& region)
 {
+  edm::RunNumber_t thisRun = ev.run();
+  if (thisRun != theLastRun) { theLastRun = thisRun; init(); }
   theGenerator->run( seeds, region, ev, es);
 }
