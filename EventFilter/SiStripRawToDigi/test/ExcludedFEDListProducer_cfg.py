23bb38fbea71b5df2d7e6a5602414ab77b63ef3f
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2011-06-19 19:16:51 +0000

    Producer for the list of detIds excluded from DAQ in the HLT

---
+++ EventFilter/SiStripRawToDigi/test/ExcludedFEDListProducer_cfg.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("OWNPARTICLES")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        '/store/data/Run2010B/MinimumBias/RAW/v1/000/147/926/F4FCCEDB-3CD7-DF11-B220-001D09F28EC1.root'
+    )
+)
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'GR_R_42_V14::All'
+
+process.myProducerLabel = cms.EDProducer(
+    'SiStripExcludedFEDListProducer',
+    ProductLabel = cms.InputTag("source")
+)
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('myOutputFile.root')
+)
+
+  
+process.p = cms.Path(process.myProducerLabel)
+
+process.e = cms.EndPath(process.out)
