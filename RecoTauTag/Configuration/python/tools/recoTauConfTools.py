5ce3dde2fc054ac36c4e583d9d2e873046f7d737
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-06-20 11:48:05 +0000

    Correctly parse IB releases

--- RecoTauTag/Configuration/python/tools/recoTauConfTools.py
+++ RecoTauTag/Configuration/python/tools/recoTauConfTools.py
@@ -10,6 +10,22 @@ Author: Evan K. Friis, UC Davis
 
 '''
 
+def represents_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
 def cmssw_version():
     version_str = os.environ['CMSSW_VERSION'].replace('CMSSW_', '').split('_')
-    return (int(version_str[0]), int(version_str[1]), int(version_str[2]))
+    major_version = int(version_str[0])
+    minor_version = int(version_str[1])
+    subminor_version = None
+    # Correctly deal with IB releases, where the subminor version is X. Return it 
+    # as a string, which is always larger than integers.
+    if represents_int(version_str[2]):
+        subminor_version = int(version_str[2])
+    else:
+        subminor_version = version_str[2]
+    return (version_str[0], version_str[1], version_str[2])
