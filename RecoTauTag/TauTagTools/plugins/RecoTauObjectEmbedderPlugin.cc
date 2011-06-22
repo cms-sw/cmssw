b6d5694606dae0db179f236fa01761dff844bde1
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-06-22 14:16:18 +0000

    Set dummy lorentz vector if no match

--- RecoTauTag/TauTagTools/plugins/RecoTauObjectEmbedderPlugin.cc
+++ RecoTauTag/TauTagTools/plugins/RecoTauObjectEmbedderPlugin.cc
@@ -88,6 +88,7 @@ void RecoTauObjectEmbedder<T>::operator()(PFTau& tau) const {
         );
   } else {
     tau.setbremsRecoveryEOverPLead(-10);
+    tau.setalternatLorentzVect(reco::Candidate::LorentzVector());
   }
 }
 }}  // end namespace reco::tau
