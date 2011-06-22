edaab3817ad7abf795506f0035058429429de472
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-06-22 23:03:20 +0000

    fix for nofloat

--- HiggsAnalysis/CombinedLimit/scripts/combineCards.py
+++ HiggsAnalysis/CombinedLimit/scripts/combineCards.py
@@ -33,7 +33,7 @@ for ich,fname in enumerate(args):
             expline.append("%.4f" % e)
             keyline.append((bout, p, DC.isSignal[p]))
     # systematics
-    for (lsyst,pdf,pdfargs,errline) in DC.systs:
+    for (lsyst,nofloat,pdf,pdfargs,errline) in DC.systs:
         systeffect = {}
         if pdf == "param":
             if paramSysts.has_key(lsyst):
@@ -52,7 +52,7 @@ for ich,fname in enumerate(args):
                 if len(r) > cmax: cmax = len(r) # get max col length, as it's more tricky do do it later with a map
                 systeffect[bout][p] = r
         if systlines.has_key(lsyst):
-            (otherpdf, otherargs, othereffect) = systlines[lsyst]
+            (otherpdf, otherargs, othereffect, othernofloat) = systlines[lsyst]
             if otherpdf != pdf: 
                 if (pdf == "lnN" and otherpdf.startswith("shape")):
                     if systlines[lsyst][0][-1] != '?': systlines[lsyst][0] += '?'
@@ -69,7 +69,7 @@ for ich,fname in enumerate(args):
                 for b,v in systeffect.items(): othereffect[b] = v;
         else:
             pdfargs = [ str(x) for x in pdfargs ]
-            systlines[lsyst] = [pdf,pdfargs,systeffect]
+            systlines[lsyst] = [pdf,pdfargs,systeffect,nofloat]
     # put shapes, if available
     if len(DC.shapeMap):
         for b in DC.bins:
@@ -133,7 +133,7 @@ for (b,p,s) in keyline:
         if p not in backgrounds: backgrounds.append(p)
         pidline.append(1+backgrounds.index(p))
 cmax = max([cmax]+[max(len(p),len(b)) for p,b,s in keyline]+[len(e) for e in expline])
-hmax = max([10] + [len("%-12s  %s %s" % (l,p,a)) for l,(p,a,e) in systlines.items()])
+hmax = max([10] + [len("%-12s[nofloat]  %s %s" % (l,p,a)) for l,(p,a,e,nf) in systlines.items()])
 cfmt  = "%-"+str(cmax)+"s"; hfmt = "%-"+str(hmax)+"s  ";
 print hfmt % "bin",     "  ".join([cfmt % p for p,b,s in keyline])
 print hfmt % "process", "  ".join([cfmt % b for p,b,s in keyline])
@@ -144,13 +144,14 @@ print "-" * 130
 
 sysnamesSorted = systlines.keys(); sysnamesSorted.sort()
 for name in sysnamesSorted:
-    (pdf,pdfargs,effect) = systlines[name]
+    (pdf,pdfargs,effect,nofloat) = systlines[name]
+    if nofloat: name += "[nofloat]"
     systline = []
     for b,p,s in keyline:
         try:
             systline.append(effect[b][p])
         except KeyError:
             systline.append("-");
-    print hfmt % ("%-12s   %s  %s" % (name, pdf, " ".join(pdfargs))), "  ".join([cfmt % x for x in systline])
+    print hfmt % ("%-21s   %s  %s" % (name, pdf, " ".join(pdfargs))), "  ".join([cfmt % x for x in systline])
 for (pname, pargs) in paramSysts.items():
     print "%-12s  param  %s" %  (pname, " ".join(pargs))
