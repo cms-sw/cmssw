b3b21c7c80e4f92eb45bc1af195479395f6df481
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-06-23 21:52:14 +0000

    Obsolete files

--- FWCore/Framework/interface/NoDelayedReader.h
+++ FWCore/Framework/interface/NoDelayedReader.h
@@ -1,18 +1,1 @@
-#ifndef FWCore_Framework_NoDelayedReader_h
-#define FWCore_Framework_NoDelayedReader_h
-
-/*----------------------------------------------------------------------
-----------------------------------------------------------------------*/
-
-#include <memory>
-#include "FWCore/Framework/interface/DelayedReader.h"
-
-namespace edm {
-  class NoDelayedReader : public DelayedReader {
-  public:
-    virtual ~NoDelayedReader();
-  private:
-    virtual WrapperHolder getProduct_(BranchKey const& k, WrapperInterfaceBase const* interface, EDProductGetter const* ep) const;
-  };
-}
-#endif
+#error // obsolete
