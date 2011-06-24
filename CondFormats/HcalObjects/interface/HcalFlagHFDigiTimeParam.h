2800d6491a07a4d99d6f3ec0c88d00ba1c82674a
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2011-06-24 22:09:37 +0000

    Added HcalFlagHFDigiTimeParam db objects

---
+++ CondFormats/HcalObjects/interface/HcalFlagHFDigiTimeParam.h
@@ -0,0 +1,56 @@
+#ifndef GUARD_HcalFlagHFDigiTimeParam
+#define GUARD_HcalFlagHFDigiTimeParam
+
+/**
+\class HcalFlagHFDigiTimeParam
+\author Jeff Temple
+
+DB object to store HF DigiTime flag parameters
+Flag parameters stored are:
+
+ HFDigiTimeParam needs the following arguments:
+ *  HFdigiflagFirstSample, HFdigiflagSamplesToAdd, HFdigiflagExpectedPeak -- values used to set the digi sections used for forming TS(peak)/NTS ratio
+
+ *  HFdigiflagMinEthreshold -- minimum energy threshold needed before a rechit may be considered for flagging
+ 
+ *  HFdigiflagCoefficients:  vector of floats that define cut contour.  Current contour is [0]-exp([1]+[2]*E), but I think we want this to be a vector, so that we can allow for [0]-exp([1]+[2]*E+[3]*E*E), or [0], etc.
+
+**/
+
+#include <boost/cstdint.hpp>
+#include <vector>
+
+class HcalFlagHFDigiTimeParam{
+
+  // Default constructor sets parameters according to 6-TS digis
+  // (Is there a way to pass in default vector arguments directly, rather than by using push_back?) 
+ public:HcalFlagHFDigiTimeParam():mId(0),mHFdigiflagFirstSample(1),mHFdigiflagSamplesToAdd(3),mHFdigiflagExpectedPeak(2),mHFdigiflagMinEthreshold(40) {
+    mHFdigiflagCoefficients.clear();
+    mHFdigiflagCoefficients.push_back(0.93);
+    mHFdigiflagCoefficients.push_back(-0.38275);
+    mHFdigiflagCoefficients.push_back(-0.012667);
+  }
+  
+  // Set parameters based on input
+  HcalFlagHFDigiTimeParam(unsigned long fId, unsigned int fFirstSample, unsigned int fSamplesToAdd, unsigned int fExpectedPeak, float fminEThreshold, std::vector<float> fcoef): mId(fId), mHFdigiflagFirstSample(fFirstSample), mHFdigiflagSamplesToAdd(fSamplesToAdd), mHFdigiflagExpectedPeak(fExpectedPeak), mHFdigiflagCoefficients(fcoef){}
+    
+    uint32_t rawId () const {return mId;}
+  
+
+    // Return flag parameter values
+    uint32_t HFdigiflagFirstSample()   const  {return mHFdigiflagFirstSample;}
+    uint32_t HFdigiflagSamplesToAdd()  const  {return mHFdigiflagSamplesToAdd;}
+    uint32_t HFdigiflagExpectedPeak()  const  {return mHFdigiflagExpectedPeak;}
+    float    HFdigiflagMinEThreshold() const  {return mHFdigiflagMinEthreshold;}
+    std::vector<float> HFdigiflagCoefficients()  const {return mHFdigiflagCoefficients;}
+
+ private:
+    uint32_t mId; // detector ID
+    uint32_t mHFdigiflagFirstSample;         // first sample used in NTS calculation
+    uint32_t mHFdigiflagSamplesToAdd;        // # of sampels to use in NTS calculation
+    uint32_t mHFdigiflagExpectedPeak;        // expected peak position; used for calculating TS(peak)
+    float    mHFdigiflagMinEthreshold;       // minimum energy for flagged rechit
+    std::vector<float> mHFdigiflagCoefficients; // coefficients used to parameterize TS(peak)/NTS threshold:  [0]-exp([1]+[2]*E+....)
+};
+
+#endif
