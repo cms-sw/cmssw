7dd7e5c7d11a6cf7970ef0ae4a9d84364629ca62
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2011-06-30 17:59:10 +0000

    Updating to Jec11_V3

--- JetMETCorrections/Modules/test/testAllIOVs.py
+++ JetMETCorrections/Modules/test/testAllIOVs.py
@@ -3,23 +3,23 @@
 import subprocess
 
 iovs = [
-    'JetCorrectorParametersCollection_Jec11_V2_AK5Calo',
-    'JetCorrectorParametersCollection_Jec11_V2_AK5PF',
-    'JetCorrectorParametersCollection_Jec11_V2_AK5PFchs',    
-    'JetCorrectorParametersCollection_Jec11_V2_AK5JPT',
-    'JetCorrectorParametersCollection_Jec11_V2_AK5TRK',
-    'JetCorrectorParametersCollection_Jec11_V2_AK7Calo',
-    'JetCorrectorParametersCollection_Jec11_V2_AK7PF',
-    'JetCorrectorParametersCollection_Jec11_V2_AK7JPT',
-    'JetCorrectorParametersCollection_Jec11_V2_KT4Calo',
-    'JetCorrectorParametersCollection_Jec11_V2_KT4PF',
-    'JetCorrectorParametersCollection_Jec11_V2_KT6Calo',
-    'JetCorrectorParametersCollection_Jec11_V2_KT6PF',
-    'JetCorrectorParametersCollection_Jec11_V2_IC5Calo',
-    'JetCorrectorParametersCollection_Jec11_V2_IC5PF',
+    'JetCorrectorParametersCollection_Jec11_V3_AK5Calo',
+    'JetCorrectorParametersCollection_Jec11_V3_AK5PF',
+    'JetCorrectorParametersCollection_Jec11_V3_AK5PFchs',    
+    'JetCorrectorParametersCollection_Jec11_V3_AK5JPT',
+    'JetCorrectorParametersCollection_Jec11_V3_AK5TRK',
+    'JetCorrectorParametersCollection_Jec11_V3_AK7Calo',
+    'JetCorrectorParametersCollection_Jec11_V3_AK7PF',
+    'JetCorrectorParametersCollection_Jec11_V3_AK7JPT',
+    'JetCorrectorParametersCollection_Jec11_V3_KT4Calo',
+    'JetCorrectorParametersCollection_Jec11_V3_KT4PF',
+    'JetCorrectorParametersCollection_Jec11_V3_KT6Calo',
+    'JetCorrectorParametersCollection_Jec11_V3_KT6PF',
+    'JetCorrectorParametersCollection_Jec11_V3_IC5Calo',
+    'JetCorrectorParametersCollection_Jec11_V3_IC5PF',
     
     ]
 
 for iov in iovs :
-    s = 'cmscond_list_iov -c sqlite_file:Jec11_V2.db -t ' + iov
+    s = 'cmscond_list_iov -c sqlite_file:Jec11_V3.db -t ' + iov
     subprocess.call( [s, ""], shell=True )    
