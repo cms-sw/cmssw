2363c86528fca790ba190642396fbf7ac0732d84
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-07-02 09:59:15 +0000

    Fixing jets put into filter object

--- HLTrigger/JetMET/src/HLTJetCollectionsFilter.cc
+++ HLTrigger/JetMET/src/HLTJetCollectionsFilter.cc
@@ -30,19 +30,13 @@ typedef std::vector<edm::RefVector<std::vector<reco::CaloJet>,reco::CaloJet,edm:
 // constructors and destructor
 //
 HLTJetCollectionsFilter::HLTJetCollectionsFilter(const edm::ParameterSet& iConfig):
-inputTag_(iConfig.getParameter< edm::InputTag > ("inputTag")),
-saveTags_(iConfig.getParameter<bool>("saveTags")),
-minJetPt_(iConfig.getParameter<double> ("MinJetPt")),
-   maxAbsJetEta_(iConfig.getParameter<double> ("MaxAbsJetEta")),
-   minNJets_(iConfig.getParameter<unsigned int> ("MinNJets"))
+  inputTag_(iConfig.getParameter< edm::InputTag > ("inputTag")),
+  saveTags_(iConfig.getParameter<bool>("saveTags")),
+  minJetPt_(iConfig.getParameter<double> ("MinJetPt")),
+  maxAbsJetEta_(iConfig.getParameter<double> ("MaxAbsJetEta")),
+  minNJets_(iConfig.getParameter<unsigned int> ("MinNJets"))
 {
-   // inputJetCollsTag_ = iConfig.getParameter< edm::InputTag > ("inputJetCollsTag");
-   //saveTags_     = iConfig.getParameter<bool>("saveTags");
-   //minJetPt_(iConfig.getParameter<double> ("MinJetPt")),
-//   maxAbsJetEta_(iConfig.getParameter<double> ("MaxAbsJetEta")),
-//   minNJets_(iConfig.getParameter<unsigned int> ("MinNJets")),
-   //register your products
-   produces<trigger::TriggerFilterObjectWithRefs>();
+  produces<trigger::TriggerFilterObjectWithRefs>();
 }
 
 HLTJetCollectionsFilter::~HLTJetCollectionsFilter(){}
@@ -67,48 +61,50 @@ HLTJetCollectionsFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetu
   using namespace reco;
   using namespace trigger;
   // The filter object
-  auto_ptr<trigger::TriggerFilterObjectWithRefs> 
-    filterobject (new trigger::TriggerFilterObjectWithRefs(path(),module()));
+  auto_ptr < trigger::TriggerFilterObjectWithRefs > filterobject(new trigger::TriggerFilterObjectWithRefs(path(), module()));
   if (saveTags_) filterobject->addCollectionTag(inputTag_);
 
-  Handle<JetCollectionVector> theCaloJetCollectionsHandle;
-  iEvent.getByLabel(inputTag_,theCaloJetCollectionsHandle);
+  Handle < JetCollectionVector > theCaloJetCollectionsHandle;
+  iEvent.getByLabel(inputTag_, theCaloJetCollectionsHandle);
   const JetCollectionVector & theCaloJetCollections = *theCaloJetCollectionsHandle;
+
   // filter decision
   bool accept(false);
-  for(unsigned int collection = 0; collection < theCaloJetCollections.size(); ++ collection){
-      unsigned int numberOfGoodJets(0);
-    const reco::CaloJetRefVector & refVector =  theCaloJetCollections[collection];
-//cout << theCaloJetCollections[collection]->size();
-     if(refVector.size() < minNJets_)
-         continue;
+  std::vector < Ref<CaloJetCollection> > goodJetRefs;
 
-     Ref<CaloJetCollection> ref;
-     reco::CaloJetRefVector::const_iterator jet ( refVector.begin() );
+  for (unsigned int collection = 0; collection < theCaloJetCollections.size(); ++collection) {
+    unsigned int numberOfGoodJets(0);
+    const reco::CaloJetRefVector & refVector = theCaloJetCollections[collection];
 
-     for (; jet != refVector.end(); jet++) {
-    reco::CaloJetRef jetRef(*jet);
-            if (jetRef->pt() >= minJetPt_ && fabs(jetRef->eta()) <= maxAbsJetEta_) {
-                numberOfGoodJets++;
-                ref = Ref<CaloJetCollection> (refVector, distance(refVector.begin(), jet));
-                filterobject->addObject(TriggerJet, ref);
-            }
-        }
-//     for (unsigned int jetIndex = 0; jetIndex < theCollection.size(); ++jetIndex) {
-//         if (theCaloJetCollection[jetIndex].pt() > minJetPt_ && std::abs(theCaloJetCollection[jetIndex].eta()) < maxAbsJetEta_)
-//                numberOfGoodJets++;
-//        }
+    if (refVector.size() < minNJets_) continue;
 
-     if(numberOfGoodJets >= minNJets_){
+    //empty the good jets collection
+    goodJetRefs.clear();
 
-         accept = true;
-         break;
-     }
+    Ref < CaloJetCollection > ref;
+    reco::CaloJetRefVector::const_iterator jet(refVector.begin());
+    for (; jet != refVector.end(); jet++) {
+      reco::CaloJetRef jetRef(*jet);      
+      if (jetRef->pt() >= minJetPt_ && fabs(jetRef->eta()) <= maxAbsJetEta_) {
+	numberOfGoodJets++;
+	ref = Ref<CaloJetCollection> (refVector, distance(refVector.begin(), jet));
+	goodJetRefs.push_back(ref);	
+      }
+    }
+    if (numberOfGoodJets >= minNJets_) {
+      accept = true;
+      break;
+    }
+  }
 
+  //fill the filter object
+  for (unsigned int refIndex = 0; refIndex < goodJetRefs.size(); ++refIndex) {
+    filterobject->addObject(TriggerJet, goodJetRefs.at(refIndex));
   }
 
   // put filter object into the Event
   iEvent.put(filterobject);
   
   return accept;
+
 }
