611d5a5e8984ba5f6513cc0652a0ba068841f6b5
Igor Volobouev <sha1-0145642b110684c0a5e41962d213328ec89408e5@cern.ch>
2011-07-05 10:21:17 +0000

    updated dictionary

--- RecoJets/FFTJetAlgorithms/src/classes.h
+++ RecoJets/FFTJetAlgorithms/src/classes.h
@@ -1,6 +1,8 @@
 #ifndef RecoJets_FFTJetAlgorithms_classes_h
 #define RecoJets_FFTJetAlgorithms_classes_h
 
+#include <utility>
+
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "RecoJets/FFTJetAlgorithms/interface/DiscretizedEnergyFlow.h"
 
@@ -8,6 +10,8 @@ namespace {
   struct dictionary {
     fftjetcms::DiscretizedEnergyFlow dflow;
     edm::Wrapper<fftjetcms::DiscretizedEnergyFlow> wr_dflow;
+    std::pair<double,double> ddpair;
+    edm::Wrapper<std::pair<double,double> > wr_ddpair;
   };
 }
 
