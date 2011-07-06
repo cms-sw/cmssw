b9f8349b613132d7bb0f236c9fe997410c0c7cd8
Maria Cepeda Hermida <sha1-4a727c1fdf5a92d567ee4b2317070045307b41b3@cern.ch>
2011-07-06 13:30:45 +0000

    *** empty log message ***

--- ElectroWeakAnalysis/WMuNu/test/WMuNuValidator.py
+++ ElectroWeakAnalysis/WMuNu/test/WMuNuValidator.py
@@ -7,13 +7,7 @@ process.maxEvents = cms.untracked.PSet(
       #input = cms.untracked.int32(100)
 )
 process.source = cms.Source("PoolSource",
-<<<<<<< WMuNuValidator.py
-       fileNames = cms.untracked.vstring(
-"rfio:/castor/cern.ch/cms/store/relval/CMSSW_4_3_0_pre1/RelValWM/GEN-SIM-RECO/MC_42_V7-v1/0048/6AA13071-0859-E011-A86A-0018F3D09688.root"
-)
-=======
        fileNames = cms.untracked.vstring('/store/data/Run2011A/SingleMu/AOD/PromptReco-v4/000/167/786/401AE9B7-F8A1-E011-93CB-003048F1C832.root')
->>>>>>> 1.8
 )
 
 # Debug/info printouts
@@ -30,7 +24,7 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 process.load("ElectroWeakAnalysis.WMuNu.wmunusValidation_cfi") #load validation sequence (for WMunu & ZMuMu)
 #process.load("UserCode.MCepeda.GoodData2010_cfi")
-process.wmnVal_pfMet.MuonTrig = cms.untracked.vstring("HLT_Mu9","HLT_Mu11","HLT_Mu15_v")
+process.wmnVal_pfMet.MuonTrig = cms.untracked.vstring("HLT_IsoMu17_v","HLT_Mu30_v")
 
 
 # Output
