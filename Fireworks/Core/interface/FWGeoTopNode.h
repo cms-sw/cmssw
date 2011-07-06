4b075b34165cc224826adec6b69f8b4134cb4b29
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2011-07-06 18:47:00 +0000

    Fix compilation error.

--- Fireworks/Core/interface/FWGeoTopNode.h
+++ Fireworks/Core/interface/FWGeoTopNode.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Matevz Tadel, Alja Mrak Tadel
 //         Created:  Thu Jun 23 01:25:00 CEST 2011
-// $Id: FWGeoTopNode.h,v 1.5 2011/07/01 23:33:53 amraktad Exp $
+// $Id: FWGeoTopNode.h,v 1.6 2011/07/04 20:11:25 amraktad Exp $
 //
 
 #include "Fireworks/Core/interface/FWGeometryTableManager.h"
@@ -44,7 +44,7 @@ private:
 
    void setupBuffMtx(TBuffer3D& buff, const TGeoHMatrix& mat);
 
-   void paintChildNodesRecurse(FWGeometryTableManager::Entries_i pIt, TGeoHMatrix& mtx);
+   void paintChildNodesRecurse(FWGeometryTableManager::Entries_i pIt, const TGeoHMatrix& mtx);
    void  paintShape(FWGeometryTableManager::NodeInfo& nodeInfo, const TGeoHMatrix& nm);
    FWGeometryBrowser       *m_geoBrowser;
 
