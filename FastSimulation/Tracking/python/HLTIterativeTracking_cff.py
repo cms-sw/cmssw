db5ee4f8c6627af30f49a9d2a370d7af1fd79f4f
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2011-07-06 10:50:58 +0000

    Implementation of iterative tracking in the HLT: first attempt

--- FastSimulation/Tracking/python/HLTIterativeTracking_cff.py
+++ FastSimulation/Tracking/python/HLTIterativeTracking_cff.py
@@ -1,20 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
-from FastSimulation.Tracking.PixelTracksProducer_cff import *
-from FastSimulation.Tracking.PixelVerticesProducer_cff import *
-from FastSimulation.Tracking.IterativeFirstTracking_cff import *
-from FastSimulation.Tracking.IterativeSecondTracking_cff import *
-from FastSimulation.Tracking.IterativeThirdTracking_cff import *
-from FastSimulation.Tracking.IterativeFourthTracking_cff import *
-from FastSimulation.Tracking.IterativeFifthTracking_cff import *
-from FastSimulation.Tracking.GeneralTracks_cfi import *
-from TrackingTools.TrackFitters.TrackFitters_cff import *
-from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
-HLTIterativeTracking = cms.Sequence(pixelTracking+pixelVertexing
-                                    +iterativeFirstTracking
-                                    +iterativeSecondTracking
-                                    +iterativeThirdTracking
-                                    +iterativeFourthTracking
-                                    +generalTracks
-                                    +trackExtrapolator
+#from FastSimulation.Tracking.PixelTracksProducer_cff import *
+#from FastSimulation.Tracking.PixelVerticesProducer_cff import *
+#from FastSimulation.Tracking.IterativeFirstTracking_cff import *
+#from FastSimulation.Tracking.IterativeSecondTracking_cff import *
+#from FastSimulation.Tracking.IterativeThirdTracking_cff import *
+#from FastSimulation.Tracking.IterativeFourthTracking_cff import *
+####from FastSimulation.Tracking.IterativeFifthTracking_cff import *
+from FastSimulation.Tracking.HLTGeneralTracks_cfi import *
+#from TrackingTools.TrackFitters.TrackFitters_cff import *
+#from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
+
+hltIter4Merged = HLTgeneralTracks.clone()
+hltPFJetCtfWithMaterialTracks = HLTgeneralTracks.clone()
+
+
+HLTIterativeTracking = cms.Sequence(hltIter4Merged
+                                    +hltPFJetCtfWithMaterialTracks
                                     )
