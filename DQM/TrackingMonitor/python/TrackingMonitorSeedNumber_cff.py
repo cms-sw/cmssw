1d2b769b646d5b05f79942dc3b5065960354deab
Francesco Fiori <sha1-435ce20379a72cd458f38343bf9e16494d8dbb7c@cern.ch>
2011-07-07 13:56:21 +0000

    New cff to Monitor Seed multiplicity Vs Cluster Multiplicity

---
+++ DQM/TrackingMonitor/python/TrackingMonitorSeedNumber_cff.py
@@ -0,0 +1,90 @@
+import FWCore.ParameterSet.Config as cms
+
+#-------------------------------------------------
+# Tracking Monitor 
+#-------------------------------------------------
+from DQM.TrackingMonitor.TrackingMonitor_cfi import *
+
+# properties
+TrackMon.OutputMEsInRootFile 	= cms.bool(True)
+TrackMon.OutputFileName         = cms.string('TrackingMonitorSeedMultiplicity.root')
+TrackMon.MeasurementState       = cms.string('ImpactPoint')
+TrackMon.FolderName    = cms.string('Tracking/GenTk')
+TrackMon.AlgoName      = cms.string('Seed')
+
+# out of the box
+# ---------------------------------------------------------------------------#
+
+# Step0 
+TrackMonStep0 = TrackMon.clone()
+TrackMonStep0.SeedProducer  = cms.InputTag("newSeedFromTriplets")
+TrackMonStep0.doSeedNumberHisto = cms.bool(True)
+TrackMonStep0.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep0.doSeedPTHisto  = cms.bool(True)
+TrackMonStep0.TkSeedSizeBin = cms.int32(200)
+TrackMonStep0.TkSeedSizeMax = cms.double(1500)                         
+TrackMonStep0.TkSeedSizeMin = cms.double(0)
+
+TrackMonStep1 = TrackMon.clone()
+TrackMonStep1.SeedProducer  = cms.InputTag("newSeedFromPairs")
+TrackMonStep1.doSeedNumberHisto = cms.bool(True)
+TrackMonStep1.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep1.doSeedPTHisto  = cms.bool(True)
+TrackMonStep1.TkSeedSizeBin = cms.int32(200)
+TrackMonStep1.TkSeedSizeMax = cms.double(100.e3)                         
+TrackMonStep1.TkSeedSizeMin = cms.double(0)
+
+
+TrackMonStep2 = TrackMon.clone()
+TrackMonStep2.SeedProducer  = cms.InputTag("secTriplets")
+TrackMonStep2.doSeedNumberHisto = cms.bool(True)
+TrackMonStep2.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep2.doSeedPTHisto  = cms.bool(True)
+TrackMonStep2.TkSeedSizeBin = cms.int32(200)
+TrackMonStep2.TkSeedSizeMax = cms.double(30.e3)                         
+TrackMonStep2.TkSeedSizeMin = cms.double(0)
+
+TrackMonStep3 = TrackMon.clone()
+TrackMonStep3.SeedProducer  = cms.InputTag("thTriplets")
+TrackMonStep3.doSeedNumberHisto = cms.bool(True)
+TrackMonStep3.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep3.doSeedPTHisto  = cms.bool(True)
+TrackMonStep3.TkSeedSizeBin = cms.int32(200)
+TrackMonStep3.TkSeedSizeMax = cms.double(100.e3)                         
+TrackMonStep3.TkSeedSizeMin = cms.double(0)
+
+TrackMonStep4 = TrackMon.clone()
+TrackMonStep4.SeedProducer  = cms.InputTag("fourthPLSeeds")
+TrackMonStep4.doSeedNumberHisto = cms.bool(True)
+TrackMonStep4.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep4.doSeedPTHisto  = cms.bool(True)
+TrackMonStep4.TkSeedSizeBin = cms.int32(200)
+TrackMonStep4.TkSeedSizeMax = cms.double(100.e3)                         
+TrackMonStep4.TkSeedSizeMin = cms.double(0)
+
+
+TrackMonStep5 = TrackMon.clone()
+TrackMonStep5.SeedProducer  = cms.InputTag("fifthSeeds")
+TrackMonStep5.doSeedNumberHisto = cms.bool(True)
+TrackMonStep5.doSeedVsClusterHisto = cms.bool(True)
+TrackMonStep5.doSeedPTHisto  = cms.bool(True)
+TrackMonStep5.TkSeedSizeBin = cms.int32(200)
+TrackMonStep5.TkSeedSizeMax = cms.double(30.e3)                         
+TrackMonStep5.TkSeedSizeMin = cms.double(0)
+
+# out of the box
+trkmonootb = cms.Sequence(
+      TrackMonStep0
+    * TrackMonStep1
+    * TrackMonStep2
+    * TrackMonStep4
+    * TrackMonStep5 
+)
+
+
+
+# all paths
+trkmon = cms.Sequence(
+      trkmonootb
+)
+
---
+++ DQM/TrackingMonitor/test/TrackingMonitor_SeedMonitor_cfg.py
@@ -0,0 +1,102 @@
+import FWCore.ParameterSet.Config as cms
+
+#-------------------------------------------------
+# DQMServices
+#-------------------------------------------------
+process = cms.Process("DQMTrackMon")
+
+#-------------------------------------------------
+# message logger
+#-------------------------------------------------
+process.MessageLogger = cms.Service("MessageLogger",
+     default = cms.untracked.PSet( limit = cms.untracked.int32(500) )
+)
+
+#-------------------------------------------------
+# Source  
+#-------------------------------------------------
+process.source = cms.Source("PoolSource",
+#fileNames = cms.untracked.vstring('/store/data/Run2011A/Jet/RAW/v1/000/166/950/6EA38368-6696-E011-ACD3-0030487C8CB8.root')   
+fileNames = cms.untracked.vstring(
+    '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/10CF910B-E057-E011-9B13-000423D9A2AE.root',
+	'/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/FA2A2CFC-EB57-E011-BF91-00304879EE3E.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/F8C8D3A7-D957-E011-85A4-003048F11CF0.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/DAACC74C-DF57-E011-A45D-001D09F2B30B.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/9A449B92-D757-E011-8FC7-003048F1183E.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/92FD48CA-F557-E011-959E-003048F1C832.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/888BEB25-0A58-E011-98B3-003048F024DC.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/7C7651A9-EC57-E011-B60D-00304879FBB2.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/6AF8632D-F057-E011-BD34-001D09F2516D.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/60819326-1658-E011-BB11-003048F1C420.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/560220D8-FC57-E011-9986-003048F024DC.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/4CB4C0AC-F357-E011-B294-003048F118E0.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/42CB3662-ED57-E011-A068-001617E30F58.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/32029E2D-E957-E011-8CAE-001D09F251CC.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/283A353B-E457-E011-AB13-0030487C8CBE.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/24196B2D-F057-E011-BB16-001D09F23D1D.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/20143BE5-E957-E011-AAF3-00304879EE3E.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/10CF910B-E057-E011-9B13-000423D9A2AE.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/312/08357011-F357-E011-9076-000423D98B6C.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/311/F46E958A-E357-E011-9FB1-0030487A18F2.root',
+        '/store/data/Run2011A/Jet/RECO/PromptReco-v1/000/161/311/D63AC4DF-CF57-E011-942A-001D09F25460.root')
+)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
+
+
+#-------------------------------------------------
+# MAGNETIC FIELD & RECO
+#-------------------------------------------------
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR_R_43_V3::All"
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load("RecoTracker.Configuration.RecoTracker_cff")
+process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+
+#-------------------------------------------------
+# Tracking Monitor 
+#-------------------------------------------------
+process.load("DQM.TrackingMonitor.TrackingMonitorSeedNumber_cff")
+
+
+#-------------------------------------------------
+
+# DQM Store 
+#-------------------------------------------------
+
+
+process.DQMStore = cms.Service("DQMStore",
+    referenceFileName = cms.untracked.string(''),
+    verbose = cms.untracked.int32(0)
+)
+
+#-------------------------------------------------
+# Paths 
+#-------------------------------------------------
+
+
+process.preTracking = cms.Path(process.siStripMatchedRecHits*process.siPixelRecHits)
+process.tracking    = cms.Path(process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
+
+#process.tracking    = cms.Path(process.ckftracks)
+
+# tracking monitor paths
+process.trackmon    = cms.Path(process.trkmon)
+#process.trackmon  = cms.Path( process.TrackMon )
+# end path
+process.outP    = cms.OutputModule("AsciiOutputModule")
+process.ep      = cms.EndPath(process.outP)
+
+process.trkmon = cms.Schedule(
+#process.digis ,	
+      process.preTracking,
+     process.tracking
+    , process.trackmon 
+    , process.ep
+)
+
+
