5eb2cac69e0d43e9545c9ffc50e938992cb81f01
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-07-07 11:39:58 +0000

    Fix GTs for 44X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_44_V0::All',
-             'startup' : 'START44_V0::All',
+autoCond = { 'mc'      : 'MC_44_V1::All',
+             'startup' : 'START44_V1::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_44_V0::All',
-             'hltonline' : 'GR_R_44_V0::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI44_V0::All'
+             'com10'   : 'GR_R_44_V1::All',
+             'hltonline' : 'GR_R_44_V1::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI44_V1::All'
              }
