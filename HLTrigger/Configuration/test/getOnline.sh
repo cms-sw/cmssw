bfe33224946e9a201c61fa3216330cc10f67b27b
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-07-11 16:23:26 +0000

    /online/collisions/2011/2e33/v1.0/HLT/V2

--- HLTrigger/Configuration/test/getOnline.sh
+++ HLTrigger/Configuration/test/getOnline.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-HLT='/online/collisions/2011/1.4e33/v1.0/HLT'
-L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v1_mc,frontier://FrontierProd/CMS_COND_31X_L1T'
+HLT='/online/collisions/2011/2e33/v1.0/HLT/V2'
+L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v4_mc,sqlite_file:/afs/cern.ch/user/g/ghete/public/L1Menu/L1Menu_Collisions2011_v4/sqlFile/L1Menu_Collisions2011_v4_mc.db'
 
 hltGetConfiguration $HLT --process TEST --globaltag auto:startup   --full --offline --mc   --unprescale > offline_mc.py
 hltGetConfiguration $HLT --process TEST                            --full --offline --data --unprescale > offline_data.py
 hltGetConfiguration $HLT --process TEST                            --full --online  --data --unprescale > online_data.py
+
+hltGetConfiguration $HLT --cff --offline --mc   > ../python/HLT_GRun_cff.py
+hltGetConfiguration $HLT --cff --offline --data > ../python/HLT_GRun_data_cff.py
 
 {
   TABLE=$(echo $HLT | cut -d: -f2)
