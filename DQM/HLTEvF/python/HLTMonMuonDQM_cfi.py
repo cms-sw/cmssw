d699a10964050add27295217d8efd3dc986e16ff
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2011-07-11 09:41:43 +0000

    menu for 1.4E33 V4

--- DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
+++ DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
@@ -11,32 +11,32 @@ hltMonMuDQM = cms.EDAnalyzer("HLTMuonDQMSource",
     	# L1 muon
 	cms.PSet(
 		directoryName = cms.string('L1PassThrough'),
-		triggerBits = cms.vstring('HLT_L1SingleMu10_v2','HLT_L1SingleMu20_v2','HLT_L1SingleMuOpen_DT_v2','HLT_L1SingleMuOpen_v2')
+		triggerBits = cms.vstring('HLT_L1SingleMu10_v4','HLT_L1SingleMu20_v4','HLT_L1SingleMuOpen_DT_v4','HLT_L1SingleMuOpen_v4')
 	),
     	# L2 muon
 	cms.PSet(
 		directoryName = cms.string('L2PassThrough'),
-		triggerBits = cms.vstring('HLT_L2Mu10_v3','HLT_L2Mu20_v3')
+		triggerBits = cms.vstring('HLT_L2Mu10_v5','HLT_L2Mu20_v5')
 	),
     	# L3 muon no iso
 	cms.PSet(
 		directoryName = cms.string('L3Triggers'),
-		triggerBits = cms.vstring('HLT_Mu20_v3','HLT_Mu24_v3','HLT_Mu30_v3', 'HLT_Mu40_v1', 'HLT_Mu3_v5','HLT_Mu5_v5','HLT_Mu8_v3')
+		triggerBits = cms.vstring('HLT_Mu20_v5','HLT_Mu24_v5','HLT_Mu30_v5', 'HLT_Mu40_v3', 'HLT_Mu3_v7','HLT_Mu5_v7','HLT_Mu8_v5')
 	),
     	# L3 muon with iso
 	cms.PSet(
 		directoryName = cms.string('L3Triggers_ISO'),
-		triggerBits = cms.vstring('HLT_IsoMu12_v5','HLT_IsoMu15_v9','HLT_IsoMu17_v9','HLT_IsoMu24_v5','HLT_IsoMu30_v5')
+		triggerBits = cms.vstring('HLT_IsoMu12_v7','HLT_IsoMu15_v11','HLT_IsoMu17_v11','HLT_IsoMu24_v7','HLT_IsoMu30_v7')
 	),
     	# DoubleMu
 	cms.PSet(
 		directoryName = cms.string('DoubleMu'),
-		triggerBits = cms.vstring('HLT_L1DoubleMu0_v2','HLT_L2DoubleMu0_v4','HLT_DoubleMu3_v5','HLT_DoubleMu6_v3','HLT_DoubleMu7_v3','HLT_Mu13_Mu8_v2','HLT_Mu17_Mu8_v2')
+		triggerBits = cms.vstring('HLT_L1DoubleMu0_v4','HLT_L2DoubleMu0_v6','HLT_DoubleMu3_v7','HLT_DoubleMu6_v5','HLT_DoubleMu7_v5','HLT_Mu13_Mu8_v4','HLT_Mu17_Mu8_v4')
 	),
 	# No tracker
 	cms.PSet(
 		directoryName = cms.string('Cosmics'),
-		triggerBits = cms.vstring('HLT_L2DoubleMu23_NoVertex_v3', 'HLT_L1SingleMuOpen_AntiBPTX_v2')
+		triggerBits = cms.vstring('HLT_L2DoubleMu23_NoVertex_v5', 'HLT_L1SingleMuOpen_AntiBPTX_v3')
 	)
     	# JetStream
 	#cms.PSet(
