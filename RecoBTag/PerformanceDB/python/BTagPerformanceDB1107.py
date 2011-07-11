bcc14505d252144c567db773c4ed2a325d1c5745
Matthew Alexander Chadwick <sha1-e33946dc488abe9ad93585dbc2d45f8e698a5fb8@cern.ch>
2011-07-11 22:08:15 +0000

    2011A data measurements

---
+++ RecoBTag/PerformanceDB/python/BTagPerformanceDB1107.py
@@ -0,0 +1,2 @@
+from RecoBTag.PerformanceDB.measure.Btag_mistag110711 import *
+from RecoBTag.PerformanceDB.measure.Btag_btag110711 import *
---
+++ RecoBTag/PerformanceDB/python/PoolBTagPerformanceDB1107.py
@@ -0,0 +1,3 @@
+from RecoBTag.PerformanceDB.measure.Pool_mistag110711 import *
+from RecoBTag.PerformanceDB.measure.Pool_btag110711 import *
+
