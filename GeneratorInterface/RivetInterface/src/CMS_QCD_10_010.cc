0956be9ed1fe33365c756e6d9fdd624b62e7c495
Albert Knutsson <sha1-d7d4c6d578df489ec46a88d0d29628b8121ea6d4@cern.ch>
2011-07-12 09:03:07 +0000

    Updated info files. Added CMS_2011_S8884919 (QCD-10-004).

--- GeneratorInterface/RivetInterface/src/CMS_QCD_10_010.cc
+++ GeneratorInterface/RivetInterface/src/CMS_QCD_10_010.cc
@@ -1,4 +1,4 @@
-// -*- C++ -*
+// -*- C++ -*-
 
 // Author: Mohammed Zakaria (mzakaria@ufl.edu)
 
@@ -8,255 +8,220 @@
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/ChargedFinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
- #include "Rivet/Projections/Beam.hh"
+#include "Rivet/Projections/Beam.hh"
 
 using namespace std;
-int  j = 0 ;
-int jj = 0 , jjj = 0;
+int  _j = 0 ;
+int _jj = 0 , _jjj = 0;
 
-double Nch_tot_7TeV_pT3 = 0;
-double Nch_tot_7TeV_pT20 = 0;
-double Nch_tot_09TeV_pT3 = 0;
+double _nch_tot_7TeV_pT3 = 0;
+double _nch_tot_7TeV_pT20 = 0;
+double _nch_tot_09TeV_pT3 = 0;
 
 namespace Rivet {
 
+  //RIVET for the UE for p-p collisions at 7TeV and 0.9TeV. 
 
   class CMS_QCD_10_010 : public Analysis {
-  public:
+    public:
 
-    /// @name Constructors etc.
-    //@{
+      /// @name Constructors etc.
+      //@{
 
-    /// Constructor
-    CMS_QCD_10_010()
-      : Analysis("CMS_QCD_10_010")
-    {
-      setNeedsCrossSection(true);
-    }
+      /// Constructor
+      CMS_QCD_10_010()
+        : Analysis("CMS_QCD_10_010")
+      {
+        setNeedsCrossSection(true);
+      }
 
-    //@}
-
-
-  public:
-
-    /// @name Analysis methods
-    //@{
-
-    /// Book histograms and initialise projections before the run
-    void init() {
-
-      /// Initialise and register projections
-      const ChargedFinalState cfs(-2.0, 2.0, 500*MeV);
-      addProjection(cfs, "CFS");
-
-//AK
-      const ChargedFinalState cfsforjet(-2.5, 2.5, 500*MeV);
-      addProjection(cfsforjet, "CFSforjet");
+      //@}
 
 
-//AK       const FastJets jetpro(FinalState(), FastJets::SISCONE, 0.5);
-      const FastJets jetpro(cfsforjet, FastJets::SISCONE, 0.5);
-      addProjection(jetpro, "Jets");
-      addProjection(Beam(), "Beam");
+    public:
 
-      /// Book histograms rivet can read it directly from aida file (of Data). 
-    _profile_Nch_pT_7TeV = bookProfile1D(1, 1, 1);
-    _Profile_SumpT_pT_7TeV = bookProfile1D(2, 1, 1);   
-    _hist_Nch_09TeV = bookProfile1D(3, 1, 1);
-    _hist_Sum_09TeV = bookProfile1D(4, 1, 1);
-    _Dist_Nch_7TeV_pT3 = bookHistogram1D(5, 1, 1); //7TeV pT>3GeV
-    _Dist_Sum_7TeV_pT3 = bookHistogram1D(6, 1, 1);
-    _Dist_pT_7TeV_pT3 = bookHistogram1D(7, 1, 1);
-    _Dist_Nch_7TeV_pT20 = bookHistogram1D(8, 1, 1);
-    _Dist_Sum_7TeV_pT20 = bookHistogram1D(9,1,1); 
-    _Dist_pT_7TeV_pT20 = bookHistogram1D(10, 1, 1);
-    _Dist_Nch_09TeV_pT3 = bookHistogram1D(11, 1, 1);
-    _Dist_Sum_09TeV_pT3 = bookHistogram1D(12 , 1, 1); 
-    _Dist_pT_09TeV_pT3 = bookHistogram1D(13, 1, 1);
-/// _hist_dsigma_dpTjet1 = bookHistogram1D("dsigma_dpTjet1",(50, 0, 50));
-    }
+      /// @name Analysis methods
+      //@{
+
+      /// Book histograms and initialise projections before the run
+      void init() {
+
+        /// Initialise and register projections
+        const ChargedFinalState cfs(-2.0, 2.0, 500*MeV);
+        addProjection(cfs, "CFS");
+
+        const ChargedFinalState cfsforjet(-2.5, 2.5, 500*MeV); // tracks accepted are with pT > 0.5 GeV
+        addProjection(cfsforjet, "CFSforjet");
 
 
-    /// Perform the per-event analysis
-    void analyze(const Event& event) {
-      const double weight = event.weight();
+        const FastJets jetpro(cfsforjet, FastJets::SISCONE, 0.5);    
+        addProjection(jetpro, "Jets");
+        addProjection(Beam(), "Beam");
 
-      // Require at least one jet in the event with pT >= 20 GeV
-      Jets jets = applyProjection<FastJets>(event, "Jets").jetsByPt(0.5*GeV);
-      if (jets.size() < 1) vetoEvent;
+        /// Book histograms rivet can read it directly from aida file (of Data). 
 
-      FourMomentum p_lead = jets[0].momentum();
-      const double philead = p_lead.phi();
-      //      const double etalead = p_lead.eta();
-      const double pTlead  = p_lead.perp();
-      //AK  MSG_INFO("Leading track: pT = " << pTlead << ", eta = " << etalead << ", phi = " << philead);
-      // These are the charged particles (tracks) with pT > 500 MeV defined above
-      ParticleVector particles =
+        _hist_profile_Nch_pT_7TeV = bookProfile1D(1, 1, 1); //Profile plot for No. of Charged particles vs. pT max for 7 TeV.
+        _hist_profile_SumpT_pT_7TeV = bookProfile1D(2, 1, 1);   //Profile plot Trans. Momentum sum vs. pT max for 7 TeV.
+        _hist_profile_Nch_09TeV = bookProfile1D(3, 1, 1);
+        _hist_profile_Sum_09TeV = bookProfile1D(4, 1, 1);
+        _hist_dist_Nch_7TeV_pT3 = bookHistogram1D(5, 1, 1); 
+        _hist_dist_Sum_7TeV_pT3 = bookHistogram1D(6, 1, 1);
+        _hist_dist_pT_7TeV_pT3 = bookHistogram1D(7, 1, 1);
+        _hist_dist_Nch_7TeV_pT20 = bookHistogram1D(8, 1, 1);
+        _hist_dist_Sum_7TeV_pT20 = bookHistogram1D(9,1,1); 
+        _hist_dist_pT_7TeV_pT20 = bookHistogram1D(10, 1, 1); 
+        _hist_dist_Nch_09TeV_pT3 = bookHistogram1D(11, 1, 1); // No. of trans. charged particles Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
+        _hist_dist_Sum_09TeV_pT3 = bookHistogram1D(12 , 1, 1); // No. of trans. momentum sum Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
+        _hist_dist_pT_09TeV_pT3 = bookHistogram1D(13, 1, 1); // Trans. momentum Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
+      }
+
+
+      /// Perform the per-event analysis
+      void analyze(const Event& event) {
+        const double weight = event.weight();
+
+
+        Jets jets = applyProjection<FastJets>(event, "Jets").jetsByPt(1.0*GeV); // we require our jets to have pT > 1 GeV.
+        if (jets.size() < 1) vetoEvent;
+
+        FourMomentum _p_lead = jets[0].momentum();
+        const double _philead = _p_lead.phi();
+        const double _etalead = _p_lead.eta();
+        const double _pTlead  = _p_lead.perp();
+
+        ParticleVector particles =
           applyProjection<ChargedFinalState>(event, "CFS").particlesByPt();
 
+        double _nTransverse(0.0), _ptSumTransverse(0.0), _pT, _nch_average ;
+        //		int  _i = 0;
+        foreach (const Particle& p, particles) {
 
-      double nTransverse(0.0), ptSumTransverse(0.0), pT, Nch_average(0) ;
-	int  i = 0;
-      foreach (const Particle& p, particles) {
- //AK  MSG_INFO( "pTlead =  " << pTlead);
+          double _dphi = fabs(deltaPhi(_philead, p.momentum().phi()));
+          double _eta = fabs(p.momentum().eta());
+          if (((PI/3.0 < _dphi) && (_dphi < 2*PI/3.0))&&(_eta < 2)) {
+            _nTransverse += 1.0;
 
-        double dphi=fabs(deltaPhi(philead, p.momentum().phi()));
- //AK  MSG_INFO( "dphi =  " << dphi*180/PI);
-       double eta = fabs(p.momentum().eta());
- //AK  MSG_INFO( "eta = " << eta); 
-        if (((PI/3.0 < dphi) && (dphi < 2*PI/3.0))&&(eta < 2)) {
-//AK  MSG_INFO( "<passes> dphi =  " << dphi*180/PI );       
-   nTransverse += 1.0;
-//AK  MSG_INFO( "nTransverse =  " << nTransverse );       
+            _ptSumTransverse += p.momentum().perp();
+            _pT = p.momentum().perp();
 
-          ptSumTransverse += p.momentum().perp();
-// //AK  MSG_INFO( "pT =  " << p.momentum().perp());
-pT = p.momentum().perp();
-if((fuzzyEquals(sqrtS(), 7*TeV))&&(pTlead > 3*GeV))
-_Dist_pT_7TeV_pT3->fill(pT/GeV);
-if((fuzzyEquals(sqrtS(), 7*TeV))&&(pTlead > 20*GeV))
-_Dist_pT_7TeV_pT20->fill(pT/GeV);
-if((fuzzyEquals(sqrtS(), 900*GeV))&&(pTlead > 3*GeV))
-_Dist_pT_09TeV_pT3->fill(pT/GeV);
+            if ( (fuzzyEquals(sqrtS(), 7*TeV)) && (_pTlead > 20*GeV) )
+              _hist_dist_pT_7TeV_pT20 -> fill(_pT/GeV);
+
+            if ( (fuzzyEquals(sqrtS(), 7*TeV)) && (_pTlead > 3*GeV) )
+              _hist_dist_pT_7TeV_pT3 -> fill(_pT/GeV);
+
+            if ( (fuzzyEquals(sqrtS(), 900*GeV)) && (_pTlead > 3*GeV) )
+              _hist_dist_pT_09TeV_pT3 -> fill(_pT/GeV);
+
+
+          }
+        }
+
+
+        if (fuzzyEquals(sqrtS(), 7*TeV)) 
+        {
+          _hist_profile_Nch_pT_7TeV -> fill
+            (_pTlead/GeV, _nTransverse / ((8 * PI/3)));
+
+          _hist_profile_SumpT_pT_7TeV -> fill
+            (_pTlead/GeV , _ptSumTransverse / ((GeV * (8 * PI/3))) , weight);
+          if(_pTlead > 3*GeV)
+          {
+            _hist_dist_Nch_7TeV_pT3 -> fill(_nTransverse);
+            _nch_average = _nch_average + _nTransverse;
+            _nch_tot_7TeV_pT3 = _nch_tot_7TeV_pT3 + _nTransverse;
+            // _i = _i + 1;
+            _j += 1 ;
+            _hist_dist_Sum_7TeV_pT3 -> fill(_ptSumTransverse / GeV);
+          }
+          if(_pTlead > 20*GeV)
+          {
+
+            _hist_dist_Nch_7TeV_pT20 -> fill(_nTransverse);
+            _nch_tot_7TeV_pT20 = _nch_tot_7TeV_pT20 + _nTransverse;
+            _jj += 1 ;
+
+            _hist_dist_Sum_7TeV_pT20 -> fill(_ptSumTransverse / GeV);
+          } 
+        }
+        else if (fuzzyEquals(sqrtS() , 900*GeV))
+        {
+          _hist_profile_Nch_09TeV -> fill(_pTlead / GeV, _nTransverse / ((8 * PI/3)), weight);
+          _hist_profile_Sum_09TeV -> fill(_pTlead / GeV, _ptSumTransverse / ((GeV * (8 * PI/3))), weight);
+
+
+          if(_pTlead > 3*GeV)
+          {
+            _hist_dist_Nch_09TeV_pT3 -> fill(_nTransverse);
+            _nch_tot_09TeV_pT3 = _nch_tot_09TeV_pT3 + _nTransverse;
+            _jjj += 1;  //counts how many events 
+
+            _hist_dist_Sum_09TeV_pT3 -> fill(_ptSumTransverse/GeV);
+          }
         }
       }
 
 
-//      if (nTransverse > 0)
- if (fuzzyEquals(sqrtS(), 7*TeV)) 
-{
-        _profile_Nch_pT_7TeV->fill
-//            (pTlead/GeV, ptSumTransverse/GeV/nTransverse, weight);
-            (pTlead/GeV, nTransverse/((8*PI/3)));
- //AK  MSG_INFO( "filling Nch Profile 7TeV, with ptlead = " << pTlead );
-//AK  MSG_INFO("Nch = " << nTransverse );
 
-	_Profile_SumpT_pT_7TeV->fill
-		(pTlead/GeV, ptSumTransverse/((GeV*(8*PI/3))), weight);
-if(pTlead > 3*GeV)
-{
-_Dist_Nch_7TeV_pT3->fill(nTransverse);
-Nch_average = Nch_average + nTransverse;
-Nch_tot_7TeV_pT3 = Nch_tot_7TeV_pT3 + nTransverse;
-i = i + 1;
-j = j + 1;
- //     //AK  MSG_INFO("Nch_average  = " << Nch_average << " i = "  << i);
-/////	//AK  MSG_INFO("Nch_7TeV_pT3 = " << nTransverse << " Nch_tot_7TeV_pT3  = " << Nch_tot_7TeV_pT3 << " j = "  << j);
-_Dist_Sum_7TeV_pT3->fill(ptSumTransverse/GeV);
-} //       //AK  MSG_INFO("This is a Distribution");
+      /// Normalise histograms etc., after the run
+      void finalize() {
+        scale(_hist_dist_Nch_7TeV_pT3 , 1/integral(_hist_dist_Nch_7TeV_pT3 ));
+        scale(_hist_dist_Sum_7TeV_pT3 , 1/integral(_hist_dist_Sum_7TeV_pT3 ));
+        scale(_hist_dist_Nch_7TeV_pT20 , 1/integral(_hist_dist_Nch_7TeV_pT20 ));
+        scale(_hist_dist_Sum_7TeV_pT20 , 1/integral(_hist_dist_Sum_7TeV_pT20 ));
+        scale(_hist_dist_Nch_09TeV_pT3 , 1/integral(_hist_dist_Nch_09TeV_pT3 ));
+        scale(_hist_dist_Sum_09TeV_pT3 , 1/integral(_hist_dist_Sum_09TeV_pT3 ));
 
-if(pTlead > 20*GeV)
-{
+        double  _nch_avg_7TeV_pT3 = 0;
+        double  _nch_avg_7TeV_pT20 = 0;
+        double _nch_avg_09TeV_pT3 = 0;
+        _nch_avg_7TeV_pT3 = (_nch_tot_7TeV_pT3 / _j) ;
+        _nch_avg_7TeV_pT20 = (_nch_tot_7TeV_pT20 / _jj) ;
+        _nch_avg_09TeV_pT3 = (_nch_tot_09TeV_pT3 / _jjj) ;
 
-_Dist_Nch_7TeV_pT20->fill(nTransverse);
-Nch_tot_7TeV_pT20 = Nch_tot_7TeV_pT20 + nTransverse;
-jj = jj + 1;
-////AK  MSG_INFO("Nch_7TeV_pT20 = " << nTransverse << " Nch_tot_7TeV_pT20  = " << Nch_tot_7TeV_pT20 << " jj = "  << jj);
+        scale(_hist_dist_pT_7TeV_pT20, _nch_avg_7TeV_pT20 / integral(_hist_dist_pT_7TeV_pT20) ); //normalizes pT Distribution so that area = avg Nch
+        scale(_hist_dist_pT_7TeV_pT3, _nch_avg_7TeV_pT3 / integral(_hist_dist_pT_7TeV_pT3) ); // normalizes Nch Distribution so that area = avg Nch
+        scale(_hist_dist_pT_09TeV_pT3, _nch_avg_09TeV_pT3 / integral(_hist_dist_pT_09TeV_pT3) ); // normalizes SumpT distribtuion so that area = avg Nch
 
-_Dist_Sum_7TeV_pT20->fill(ptSumTransverse/GeV);
-} 
+
       }
-else if (fuzzyEquals(sqrtS(), 900*GeV))
-{
-	_hist_Nch_09TeV->fill(pTlead/GeV, nTransverse/((8*PI/3)), weight);
-        _hist_Sum_09TeV->fill(pTlead/GeV, ptSumTransverse/((GeV*(8*PI/3))), weight);
 
- //     _hist_dsigma_dpTjet1->fill(pTlead/GeV, weight);
-
-if(pTlead > 3*GeV)
-{
-_Dist_Nch_09TeV_pT3->fill(nTransverse);
-Nch_tot_09TeV_pT3 = Nch_tot_09TeV_pT3 + nTransverse;
-jjj = jjj + 1;  //counts how many events 
-////AK  MSG_INFO("Nch_09TeV_pT3 = " << nTransverse << " Nch_tot_09TeV_pT3  = " << Nch_tot_09TeV_pT3 << " jjj = "  << jjj);
-
-_Dist_Sum_09TeV_pT3->fill(ptSumTransverse/GeV);
-}
-}
-    }
+      //@}
 
 
+    private:
 
-    /// Normalise histograms etc., after the run
-    void finalize() {
-//int nxBins = _Dist_Nch_09TeV_pT3.size();
-//for(int x = 0 ; x < 200 ; x++)
-//{
-//   _Dist_Nch_09TeV_pT3.numEvents();
+      /// @name Histograms
+      //@{
 
+      AIDA::IProfile1D * _hist_profile_Nch_pT_7TeV;
+      AIDA::IProfile1D * _hist_profile_SumpT_pT_7TeV;
+      AIDA::IProfile1D * _hist_profile_Nch_09TeV;
+      AIDA::IProfile1D * _hist_profile_Sum_09TeV;
+      AIDA::IHistogram1D * _hist_dist_Nch_7TeV_pT3 ;
+      AIDA::IHistogram1D * _hist_dist_Sum_7TeV_pT3; 
+      AIDA::IHistogram1D * _hist_dist_pT_7TeV_pT3;
+      AIDA::IHistogram1D * _hist_dist_Nch_7TeV_pT20;
+      AIDA::IHistogram1D * _hist_dist_Sum_7TeV_pT20;
+      AIDA::IHistogram1D * _hist_dist_pT_7TeV_pT20;
+      AIDA::IHistogram1D * _hist_dist_Nch_09TeV_pT3;
+      AIDA::IHistogram1D * _hist_dist_Sum_09TeV_pT3;
+      AIDA::IHistogram1D * _hist_dist_pT_09TeV_pT3;
+      //@}
 
-//if(_Dist_Nch_09TeV_pT3 != 0)
-//{///_Dist_Nch_09TeV_pT3.empty();
-//int i =  _Dist_Nch_09TeV_pT3->binHeight(x);
-//      //AK  MSG_INFO("Bin_Value  = " << _Dist_Nch_09TeV_pT3->binHeight(x) << " "  << x);
-//}
-//}
-//      scale(_hist_ptavg_transverse, 3/8*PI);
- /////AK  MSG_INFO( "Area beneath the histogram Nch =  " << integral(_Dist_Nch_7TeV_pT3));
-// //AK  MSG_INFO( "Average of the histogram Nch =  " << _Dist_Nch_7TeV_pT3.getMean());
+    public:
+      string experiment()         const { return "CMS"; }
+      string year()               const { return "2011"; }
+      string spiresId()           const { return "None"; }
+      string collider()           const { return "LHC"; }
+      string summary()            const { return "None"; }
+      string description()        const { return "None"; }
+      string runInfo()            const { return "None"; }
+      string status()             const { return "UNVALIDATED"; }
+      vector<string> authors()    const { return vector<string>(); }
+      vector<string> references() const { return vector<string>(); }
+      vector<std::string> todos() const { return vector<string>(); }
 
-
-scale(_Dist_Nch_7TeV_pT3, 1/integral(_Dist_Nch_7TeV_pT3 ));
-scale(_Dist_Sum_7TeV_pT3, 1/integral(_Dist_Sum_7TeV_pT3 ));
-scale(_Dist_Nch_7TeV_pT20, 1/integral(_Dist_Nch_7TeV_pT20 ));
-scale(_Dist_Sum_7TeV_pT20, 1/integral(_Dist_Sum_7TeV_pT20 ));
-scale(_Dist_Nch_09TeV_pT3, 1/integral(_Dist_Nch_09TeV_pT3 ));
-scale(_Dist_Sum_09TeV_pT3, 1/integral(_Dist_Sum_09TeV_pT3 ));
- 
-double  Nch_avg_7TeV_pT3 = 0;
-double  Nch_avg_7TeV_pT20 = 0;
-double Nch_avg_09TeV_pT3 = 0;
-Nch_avg_7TeV_pT3 = (Nch_tot_7TeV_pT3 / j) ;
-//	//AK  MSG_INFO("Nch_avg_7TeV_pT3 = " << Nch_avg_7TeV_pT3);
-Nch_avg_7TeV_pT20 = (Nch_tot_7TeV_pT20 / jj) ;
-//	//AK  MSG_INFO("Nch_avg_7TeV_pT20 = " << Nch_avg_7TeV_pT20);
-Nch_avg_09TeV_pT3 = (Nch_tot_09TeV_pT3 / jjj) ;
-//	//AK  MSG_INFO("Nch_avg_09TeV_pT3 = " << Nch_avg_09TeV_pT3);
-
-scale(_Dist_pT_7TeV_pT20, Nch_avg_7TeV_pT20/integral(_Dist_pT_7TeV_pT20) ); //normalizes pT Distribution so that area = avg Nch
-scale(_Dist_pT_7TeV_pT3, Nch_avg_7TeV_pT3/integral(_Dist_pT_7TeV_pT3) );
-scale(_Dist_pT_09TeV_pT3, Nch_avg_09TeV_pT3/integral(_Dist_pT_09TeV_pT3) );
-
-
-   }
-
-    //@}
-
-
-  private:
-
-    /// @name Histograms
-    //@{
-
-AIDA::IProfile1D * _profile_Nch_pT_7TeV;
-AIDA::IProfile1D * _Profile_SumpT_pT_7TeV;
-AIDA::IProfile1D * _hist_Nch_09TeV;
-AIDA::IProfile1D * _hist_Sum_09TeV;
-AIDA::IHistogram1D * _Dist_Nch_7TeV_pT3 ;
-AIDA::IHistogram1D * _Dist_Sum_7TeV_pT3; 
-AIDA::IHistogram1D * _Dist_pT_7TeV_pT3;
-AIDA::IHistogram1D * _Dist_Nch_7TeV_pT20;
-AIDA::IHistogram1D * _Dist_Sum_7TeV_pT20;
-AIDA::IHistogram1D * _Dist_pT_7TeV_pT20;
-AIDA::IHistogram1D * _Dist_Nch_09TeV_pT3;
-AIDA::IHistogram1D * _Dist_Sum_09TeV_pT3;
-AIDA::IHistogram1D * _Dist_pT_09TeV_pT3;
-//    AIDA::IHistogram1D * _hist_dsigma_dpTjet1;
-    //@}
-
-  public:
-    string experiment()         const { return "CMS"; }
-    string year()               const { return "2011"; }
-    string spiresId()           const { return "None"; }
-    string collider()           const { return "None"; }
-    string summary()            const { return "None"; }
-    string description()        const { return "None"; }
-    string runInfo()            const { return "None"; }
-    string status()             const { return "UNVALIDATED"; }
-    vector<string> authors()    const { return vector<string>(); }
-    vector<string> references() const { return vector<string>(); }
-    vector<std::string> todos() const { return vector<string>(); }
-    
 
   };
 
@@ -267,4 +232,6 @@ AIDA::IHistogram1D * _Dist_pT_09TeV_pT3;
 
 
 }
+
+
 
