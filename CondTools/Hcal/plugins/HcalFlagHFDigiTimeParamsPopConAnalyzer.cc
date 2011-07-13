1008c2c286a7133f041af269d5f41c1dba6f831e
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2011-07-13 16:20:30 +0000

    Added HFDigiTime PopCon analyzer

---
+++ CondTools/Hcal/plugins/HcalFlagHFDigiTimeParamsPopConAnalyzer.cc
@@ -0,0 +1,42 @@
+#include "CondCore/PopCon/interface/PopConAnalyzer.h"
+#include "CondTools/Hcal/interface/HcalFlagHFDigiTimeParamsHandler.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+//typedef popcon::PopConAnalyzer<HcalFlagHFDigiTimeParamsHandler> HcalFlagHFDigiTimeParamsPopConAnalyzer;
+
+class HcalFlagHFDigiTimeParamsPopConAnalyzer: public popcon::PopConAnalyzer<HcalFlagHFDigiTimeParamsHandler>
+{
+public:
+  typedef HcalFlagHFDigiTimeParamsHandler SourceHandler;
+
+  HcalFlagHFDigiTimeParamsPopConAnalyzer(const edm::ParameterSet& pset): 
+    popcon::PopConAnalyzer<HcalFlagHFDigiTimeParamsHandler>(pset),
+    m_populator(pset),
+    m_source(pset.getParameter<edm::ParameterSet>("Source")) {}
+
+private:
+  virtual void endJob() 
+  {
+    m_source.initObject(myDBObject);
+    write();
+  }
+
+  virtual void analyze(const edm::Event& ev, const edm::EventSetup& esetup)
+  {
+    //Using ES to get the data:
+
+    edm::ESHandle<HcalFlagHFDigiTimeParams> objecthandle;
+    esetup.get<HcalFlagHFDigiTimeParamsRcd>().get(objecthandle);
+    myDBObject = new HcalFlagHFDigiTimeParams(*objecthandle.product() );
+  }
+
+  void write() { m_populator.write(m_source); }
+
+private:
+  popcon::PopCon m_populator;
+  SourceHandler m_source;
+
+  HcalFlagHFDigiTimeParams* myDBObject;
+};
+
+DEFINE_FWK_MODULE(HcalFlagHFDigiTimeParamsPopConAnalyzer);
