5cf316561619d0fb9d17f9d19f29a01fc89d0a2a
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-07-14 16:02:55 +0000

    *** empty log message ***

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,8 +1,8 @@
 
 
 
-autoCond = { 'mc'      : 'MC_44_V1::All',
-             'startup' : 'START44_V1::All',
+autoCond = { 'mc'      : 'MC_44_V2::All',
+             'startup' : 'START44_V2::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
              'com10'   : 'GR_R_44_V1::All',
