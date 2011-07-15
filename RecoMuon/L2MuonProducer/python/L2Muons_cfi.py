fdb9a4776a4f5359a27183230712c90578e8249e
Daniele Trocino <sha1-89d8c3587cf9958f0ef4069e1db7c791d2c3970d@cern.ch>
2011-07-15 14:46:19 +0000

    updated

--- RecoMuon/L2MuonProducer/python/L2Muons_cfi.py
+++ RecoMuon/L2MuonProducer/python/L2Muons_cfi.py
@@ -7,6 +7,15 @@ L2Muons = cms.EDProducer("L2MuonProducer",
     MuonTrackLoaderForSTA,
     MuonServiceProxy,
     InputObjects = cms.InputTag("L2MuonSeeds"),
+    DoSeedRefit = cms.bool(False),
+    SeedTransformerParameters = cms.PSet( 
+        Fitter = cms.string("hltESPKFFittingSmootherForL2Muon"),
+        MuonRecHitBuilder = cms.string("hltESPMuonTransientTrackingRecHitBuilder"),
+        NMinRecHits = cms.uint32(2),
+        UseSubRecHits = cms.bool(False),
+        Propagator = cms.string("hltESPFastSteppingHelixPropagatorAny"),
+        RescaleError = cms.double(100.0)
+    ),
     L2TrajBuilderParameters = cms.PSet(
         DoRefit = cms.bool(False),
         FilterParameters = cms.PSet(
@@ -15,10 +24,12 @@ L2Muons = cms.EDProducer("L2MuonProducer",
             DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
             MaxChi2 = cms.double(1000.0),
             MuonTrajectoryUpdatorParameters = cms.PSet(
-                MaxChi2 = cms.double(1000.0),
+                MaxChi2 = cms.double(25.0),
                 RescaleError = cms.bool(False),
                 RescaleErrorFactor = cms.double(100.0),
-                Granularity = cms.int32(0)
+                Granularity = cms.int32(0),
+                ExcludeRPCFromFit = cms.bool(False),
+                UseInvalidHits = cms.bool(True),
             ),
             EnableRPCMeasurement = cms.bool(True),
             CSCRecSegmentLabel = cms.InputTag("cscSegments"),
@@ -39,13 +50,15 @@ L2Muons = cms.EDProducer("L2MuonProducer",
             BWSeedType = cms.string('fromGenerator'),
             FitDirection = cms.string('outsideIn'),
             DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
-            MaxChi2 = cms.double(25.0),
+            MaxChi2 = cms.double(100.0),
             MuonTrajectoryUpdatorParameters = cms.PSet(
                 MaxChi2 = cms.double(25.0),
                 RescaleError = cms.bool(False),
                 RescaleErrorFactor = cms.double(100.0),
-                Granularity = cms.int32(2)
-            ),
+                Granularity = cms.int32(2),
+                ExcludeRPCFromFit = cms.bool( False ),
+                UseInvalidHits = cms.bool( True ),
+           ),
             EnableRPCMeasurement = cms.bool(True),
             CSCRecSegmentLabel = cms.InputTag("cscSegments"),
             EnableDTMeasurement = cms.bool(True),
@@ -53,10 +66,6 @@ L2Muons = cms.EDProducer("L2MuonProducer",
             Propagator = cms.string('SteppingHelixPropagatorL2Any'),
             EnableCSCMeasurement = cms.bool(True)
         ),
-        RefitterParameters = cms.PSet(
-            FitterName = cms.string('KFFitterSmootherSTA'),
-            Option = cms.int32(1)
-        )
     )
 )
 
