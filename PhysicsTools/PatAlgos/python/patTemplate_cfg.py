ed6b081127e47b4d693f27e9b31b54f91b26c9fd
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2011-07-18 16:23:54 +0000

    update to more up to date relval file

--- PhysicsTools/PatAlgos/python/patTemplate_cfg.py
+++ PhysicsTools/PatAlgos/python/patTemplate_cfg.py
@@ -12,9 +12,9 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_4_2_0_pre8'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_4_2_5'
                         , relVal        = 'RelValTTbar'
-                        , globalTag     = 'START42_V7'
+                        , globalTag     = 'START42_V12'
                         , numberOfFiles = 1
                         )
     )
