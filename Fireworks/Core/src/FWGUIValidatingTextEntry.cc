fc40110b32d4af6f27abd65a8b492d653e81d963
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2011-07-20 04:58:37 +0000

    Set TGListBox entry selected when the entry is selected from keyboard. This is a workaround for a ROOT GUI feature, where list box entry is set selected only on mouse click event.

--- Fireworks/Core/src/FWGUIValidatingTextEntry.cc
+++ Fireworks/Core/src/FWGUIValidatingTextEntry.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Fri Aug 22 18:13:39 EDT 2008
-// $Id: FWGUIValidatingTextEntry.cc,v 1.6 2008/12/04 20:38:26 chrjones Exp $
+// $Id: FWGUIValidatingTextEntry.cc,v 1.7 2009/01/23 21:35:43 amraktad Exp $
 //
 
 // system include files
@@ -110,7 +110,7 @@ FWGUIValidatingTextEntry::ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2)
 }
 
 void
-FWGUIValidatingTextEntry::keyPressedInPopup(TGFrame*, UInt_t keysym, UInt_t mask)
+FWGUIValidatingTextEntry::keyPressedInPopup(TGFrame* f, UInt_t keysym, UInt_t mask)
 {
    switch(keysym) {
       case kKey_Tab:
@@ -122,12 +122,16 @@ FWGUIValidatingTextEntry::keyPressedInPopup(TGFrame*, UInt_t keysym, UInt_t mask
          RequestFocus();
          //NOTE: If chosen from the keyboard, m_list->GetSelected() does not work, however
          // m_list->GetSelectedEntries does work
-         TList selected;
-         m_list->GetSelectedEntries(&selected);
-         assert(selected.GetEntries() == 1);
-         const TGLBEntry* entry = dynamic_cast<TGLBEntry*> (selected.First());
-         assert(0!=entry);
-         insertTextOption(m_options[entry->EntryId()].second);
+
+         //AMT NOTE: TGListEntry does not select entry on key return event, it has to be selected here.
+         //          Code stolen from TGComboBox::KeyPressed
+
+         const TGLBEntry* entry = dynamic_cast<TGLBEntry*> (f);
+         if (entry)
+         {
+            insertTextOption(m_options[entry->EntryId()].second);
+            m_list->Selected(entry->EntryId());
+         }
          hideOptions();
          break;
    }
@@ -210,7 +214,6 @@ FWGUIValidatingTextEntry::insertTextOption(const std::string& iOption)
    long pos = GetCursorPosition();
    InsertText(iOption.c_str(), pos);
    SetCursorPosition(pos + iOption.size());
-
 }
 
 //
