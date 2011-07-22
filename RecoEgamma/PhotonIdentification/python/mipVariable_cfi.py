7707f0bb4ed00e1c2cfbf762c78e93c47baa2c21
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2011-07-22 00:09:14 +0000

    First commit from Sushil

---
+++ RecoEgamma/PhotonIdentification/python/mipVariable_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+
+mipVariable = cms.PSet(
+    #required inputs
+    ComponentName = cms.string('mipVariable'),
+
+    barrelEcalRecHitCollection = cms.InputTag('ecalRecHit:EcalRecHitsEB'),
+    endcapEcalRecHitCollection = cms.InputTag('ecalRecHit:EcalRecHitsEE'),
+
+    #some of the variables use to get mipHalo bool
+    # These values could be improved with further study
+    YRangeFit          = cms.double(180.0),
+    XRangeFit          = cms.double(7.0),
+    ResidualWidth      = cms.double(0.23),
+    HaloDiscThreshold  = cms.double(70.0)
+
+)
+
+
