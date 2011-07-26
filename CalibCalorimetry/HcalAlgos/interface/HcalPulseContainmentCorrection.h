907bc50fc418c4e8d60858cde46e0d9c57742d62
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-07-26 21:07:56 +0000

    Factor the Algo stuff into HcalPUlseContainmentAlgo

--- CalibCalorimetry/HcalAlgos/interface/HcalPulseContainmentCorrection.h
+++ CalibCalorimetry/HcalAlgos/interface/HcalPulseContainmentCorrection.h
@@ -2,14 +2,15 @@
 #define CALIBCALORIMETRY_HCALALGOS_HCALPULSECONTAINMENTCORRECTION_H 1
 
 #include <map>
+#include "CalibCalorimetry/HcalAlgos/interface/HcalPulseShape.h"
 
 /** \class HcalPulseContainmentCorrection
   *
   * Amplitude correction for pulse containment in time.
   * Currently only for HPD pulse shape.
   *  
-  * $Date: $
-  * $Revision: $
+  * $Date: 2006/11/08 16:53:17 $
+  * $Revision: 1.1 $
   * \author P. Dudero - Minnesota
   */
 class HcalPulseContainmentCorrection {
@@ -17,7 +18,10 @@ public:
   HcalPulseContainmentCorrection(int num_samples,
                                  float fixedphase_ns,
                                  float max_fracerror);
-
+  HcalPulseContainmentCorrection(const HcalPulseShape * shape,
+                                 int num_samples,
+                                 float fixedphase_ns,
+                                 float max_fracerror);
   double getCorrection(double fc_ampl) const;
   double fractionContained(double fc_ampl) const { return 1.0/this->getCorrection(fc_ampl); }
 
