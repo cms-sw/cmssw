29daceb42c7f0b8c9e9a2a02a0879ee56fa07aef
Josh Bendavid <sha1-10bff5a2fc8b453c5b7cea095bb60481d86f739f@cern.ch>
2011-07-29 13:12:38 +0000

    update scripts

--- Validation/RecoEgamma/test/ElectronConversionRejection.py
+++ Validation/RecoEgamma/test/ElectronConversionRejection.py
@@ -22,11 +22,11 @@ process.eleConvRejectionValidation.OutputFileName = 'ElectronConversionRejection
 process.source = cms.Source("PoolSource",
                             
     fileNames = cms.untracked.vstring(
-        #'/store/relval/CMSSW_4_4_0_pre5/RelValSingleGammaPt35/GEN-SIM-RECO/START44_V2-v1/0018/D0CE4726-80B3-E011-AC32-0030486790BA.root',
+        '/store/relval/CMSSW_4_4_0_pre5/RelValSingleGammaPt35/GEN-SIM-RECO/START44_V2-v1/0018/D0CE4726-80B3-E011-AC32-0030486790BA.root',
 
-        '/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0022/12E5D087-A6B3-E011-9A28-003048679266.root',
-        '/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0018/E04D64F4-39B2-E011-B8DD-002618943976.root',
-        '/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0018/4AC726F3-3AB2-E011-9DAD-002354EF3BCE.root',
+        #'/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0022/12E5D087-A6B3-E011-9A28-003048679266.root',
+        #'/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0018/E04D64F4-39B2-E011-B8DD-002618943976.root',
+        #'/store/relval/CMSSW_4_4_0_pre5/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START44_V2-v1/0018/4AC726F3-3AB2-E011-9DAD-002354EF3BCE.root',
 
 
      ),
