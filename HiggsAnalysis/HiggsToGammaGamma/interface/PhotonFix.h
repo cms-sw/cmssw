1d87707db252f109f83ebe73f132d725f4dbcb45
Matthew William Kenzie <sha1-1f5a1c751a31ca0ee48177eb1c9c783e13e5fcd1@cern.ch>
2011-07-29 17:25:51 +0000

    Updates for electrons studies

--- HiggsAnalysis/HiggsToGammaGamma/interface/PhotonFix.h
+++ HiggsAnalysis/HiggsToGammaGamma/interface/PhotonFix.h
@@ -25,9 +25,7 @@
   
   The string gives the reco version used. Valid strings are 
   "3_8", "3_11", "4_2" and "Nominal", where the latter gives no correction 
-  to the energy and a nominal resolution value. There is also "4_2e" which 
-  provides corrections for electrons which are reconstructed as photons (to
-  aid with testing the performance of these corrections in data).
+  to the energy and a nominal resolution value.
 
   Make objects using
     PhotonFix a(energy,eta,phi,r9);
