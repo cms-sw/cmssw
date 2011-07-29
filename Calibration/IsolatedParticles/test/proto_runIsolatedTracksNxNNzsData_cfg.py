3295843702fb958498f619b7c01d2f9a6fb704d0
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2011-07-29 22:45:13 +0000

    Update the test configuration

--- Calibration/IsolatedParticles/test/proto_runIsolatedTracksNxNNzsData_cfg.py
+++ Calibration/IsolatedParticles/test/proto_runIsolatedTracksNxNNzsData_cfg.py
@@ -2,62 +2,29 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("L1SKIM")
 
-process.load("Calibration.IsolatedParticles.isolatedTracksNxN_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100000
-#process.MessageLogger.categories.append('L1GtTrigReport')
-#process.MessageLogger.categories.append('HLTrigReport')
 
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 ####################### configure pool source #############################
 process.source = cms.Source("PoolSource",
  fileNames =cms.untracked.vstring(
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8EDA42C-9BC6-DF11-8CAD-00261894385A.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8D7F0A3-98C6-DF11-9642-0018F3D0965A.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8D23680-94C6-DF11-91AC-0018F3D096DC.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8C6D89F-98C6-DF11-92B3-002618943810.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8BF2E5A-94C6-DF11-9A0C-002618943927.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F89C8FD4-9AC6-DF11-A55B-001A92971B88.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F895BB8B-9BC6-DF11-BE04-001A92971B68.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F888160F-93C6-DF11-B46F-0018F3D09686.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F87A30AD-93C6-DF11-B063-0018F3D096DC.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F823F82F-9BC6-DF11-97A7-0026189438F3.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F8146F8C-9AC6-DF11-B576-0018F3D09710.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F6FF8D8D-9BC6-DF11-863A-0018F3D09620.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F6E06F83-97C6-DF11-8C54-001A92971BD6.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F6B89D88-9BC6-DF11-930D-003048678EE2.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F67DE8AE-97C6-DF11-A08E-0018F3D09706.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/F6788CAE-99C6-DF11-9D50-001BFCDBD19E.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FEE9AE8E-9BC6-DF11-95A4-00261894393C.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FEC5A32D-9AC6-DF11-82E5-001A92971BD6.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FE9446FA-97C6-DF11-98ED-003048679080.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FE73FD57-9FC6-DF11-9657-0018F3D096DA.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FE285975-94C6-DF11-AB0F-0018F3D096D2.root',
-    '/store/data/Run2010A/HcalNZS/RECO/Sep17ReReco_v2/0019/FE0C5E28-99C6-DF11-8605-00261894385A.root'
-
-##########run2010B Dec22Rereco
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F8172904-4528-E011-BF6A-001A92971B7E.root',
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F6D9B64A-4C28-E011-B75C-0018F3D096C8.root',
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F6CB08D7-4928-E011-94D6-001BFCDBD1BE.root',
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F6C84402-4A28-E011-B98E-00261894389E.root',
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F6AB82C5-4728-E011-AC61-001A92810AD2.root',
-#    '/store/data/Run2010B/HcalNZS/RECO/Dec22ReReco_v1/0156/F68CFD2D-4228-E011-8237-001A92971B7E.root'#    "/store/data/Run2010A/HcalNZS/RECO/v4/000/139/103/2634CE90-0385-DF11-B9B3-0030487CD17C.root"
+    '/store/data/Run2010A/MinimumBias/RECO/Apr21ReReco-v1/0000/08275F4A-5270-E011-9DC3-003048635E02.root'
     ),
  skipEvents = cms.untracked.uint32(0)
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 ##################### digi-2-raw plus L1 emulation #########################
 process.load("Configuration.StandardSequences.Services_cff")
 process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
-#process.load('TrackingTools/TrackAssociator/DetIdAssociatorESProducer_cff')
 
 #################### Conditions and L1 menu ################################
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'GR_R_311_V2::All'   # used for May27 ReReco
+process.GlobalTag.globaltag = 'GR_R_42_V13::All'
 
 ############ Skim the events according to the L1 seeds  ####################
 
@@ -75,36 +42,9 @@ process.load("HLTrigger.HLTfilters.hltLevel1Activity_cfi")
 process.hltLevel1Activity.L1GtReadoutRecordTag  = cms.InputTag('gtDigis')
 
 ######################## Configure Analyzer ###############################
-#process.isotracksL1 = cms.EDAnalyzer("IsolatedTracksNxN",
-#                                   DoMC                  = cms.untracked.bool(False),
-#                                   WriteAllTracks        = cms.untracked.bool(False),
-#                                     Verbosity             = cms.untracked.int32( 1 ),
-#                                   PVTracksPtMin         = cms.untracked.double( 0.200 ),
-#                                   DebugTracks           = cms.untracked.int32(0),
-#                                   PrintTrkHitPattern    = cms.untracked.bool(True),
-#                                   minTrackP             = cms.untracked.double( 1.0 ),
-#                                   maxTrackEta           = cms.untracked.double( 2.6 ),
-#                                   DebugL1Info           = cms.untracked.bool(False),
-#                                   L1TriggerAlgoInfo     = cms.untracked.bool(False),
-#                                   L1extraTauJetSource   = cms.InputTag("l1extraParticles", "Tau"),
-#                                   L1extraCenJetSource   = cms.InputTag("l1extraParticles", "Central"),
-#                                   L1extraFwdJetSource   = cms.InputTag("l1extraParticles", "Forward"),
-#                                   L1extraMuonSource     = cms.InputTag("l1extraParticles"),
-#                                   L1extraIsoEmSource    = cms.InputTag("l1extraParticles","Isolated"),
-#                                   L1extraNonIsoEmSource = cms.InputTag("l1extraParticles","NonIsolated"),
-#                                   L1GTReadoutRcdSource  = cms.InputTag("gtDigis"),
-#                                   L1GTObjectMapRcdSource= cms.InputTag("hltL1GtObjectMap"),
-#                                   JetSource             = cms.InputTag("iterativeCone5CaloJets"),
-#                                   JetExtender           = cms.InputTag("iterativeCone5JetExtender"),
-#                                     HBHERecHitSource      = cms.InputTag("hbherecoMB"),
-#                                   maxNearTrackPT        = cms.untracked.double( 1.0 ),
-#                                   TimeMinCutECAL        = cms.untracked.double(-500.0),
-#                                   TimeMaxCutECAL        = cms.untracked.double(500.0),
-#                                   TimeMinCutHCAL        = cms.untracked.double(-500.0),
-#                                   TimeMaxCutHCAL        = cms.untracked.double(500.0),
-#                                   DebugEcalSimInfo      = cms.untracked.int32(2),
-#                                  )
 
+process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
+process.load("Calibration.IsolatedParticles.isolatedTracksNxN_cfi")
 process.isolatedTracksNxN.Verbosity = cms.untracked.int32( 0 )
 process.isolatedTracksNxN.HBHERecHitSource = cms.InputTag("hbhereco")
 process.isolatedTracksNxN.L1TriggerAlgoInfo = True
@@ -156,6 +96,5 @@ process.l1GtTrigReport.PrintVerbosity = 1
 
 #### by Benedikt
 process.p1 = cms.Path(process.primaryVertexFilter * process.hltLevel1GTSeed * process.noScraping * process.skimL1Seeds       *process.isolatedTracksNxN * process.isolatedTracksNxN_NZS)
-#process.p1 = cms.Path( process.hltLevel1GTSeed * process.noScraping  *process.isolatedTracksNxN * process.isolatedTracksNxN_NZS)
 process.e = cms.EndPath(process.l1GtTrigReport + process.hltTrigReport)
-#process.e = cms.EndPath(process.hltTrigReport)
+
