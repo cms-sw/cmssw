59096642be27c4fe5f457b1d43f4bd066583e62f
ewidl <sha1-3c65bb8ad5ab20d6db077c9cd74c65f6de0120f6@cern.ch>
2011-08-01 09:16:18 +0000

    added association collection for track parameters constraint

--- TrackingTools/PatternTools/interface/TrackConstraintAssociation.h
+++ TrackingTools/PatternTools/interface/TrackConstraintAssociation.h
@@ -5,6 +5,7 @@
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/GlobalError.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 
 //typedef std::pair<double,double> MomentumConstraint;
 struct MomentumConstraint {
@@ -17,17 +18,24 @@ struct MomentumConstraint {
 
 typedef std::pair<GlobalPoint,GlobalError> VertexConstraint;
 
+typedef TrajectoryStateOnSurface TrackParamConstraint;
+
 typedef edm::AssociationMap<edm::OneToOne<reco::TrackCollection,std::vector<MomentumConstraint> > > TrackMomConstraintAssociationCollection;
 typedef TrackMomConstraintAssociationCollection::value_type TrackMomConstraintAssociation;
 typedef edm::Ref<TrackMomConstraintAssociationCollection> TrackMomConstraintAssociationRef;
 typedef edm::RefProd<TrackMomConstraintAssociationCollection> TrackMomConstraintAssociationRefProd;
 typedef edm::RefVector<TrackMomConstraintAssociationCollection> TrackMomConstraintAssociationRefVector;
 
-
 typedef edm::AssociationMap<edm::OneToOne<reco::TrackCollection,std::vector<VertexConstraint> > > TrackVtxConstraintAssociationCollection;
 typedef TrackVtxConstraintAssociationCollection::value_type TrackVtxConstraintAssociation;
 typedef edm::Ref<TrackVtxConstraintAssociationCollection> TrackVtxConstraintAssociationRef;
 typedef edm::RefProd<TrackVtxConstraintAssociationCollection> TrackVtxConstraintAssociationRefProd;
 typedef edm::RefVector<TrackVtxConstraintAssociationCollection> TrackVtxConstraintAssociationRefVector;
+
+typedef edm::AssociationMap<edm::OneToOne<reco::TrackCollection,std::vector<TrackParamConstraint> > > TrackParamConstraintAssociationCollection;
+typedef TrackParamConstraintAssociationCollection::value_type TrackParamConstraintAssociation;
+typedef edm::Ref<TrackParamConstraintAssociationCollection> TrackParamConstraintAssociationRef;
+typedef edm::RefProd<TrackParamConstraintAssociationCollection> TrackParamConstraintAssociationRefProd;
+typedef edm::RefVector<TrackParamConstraintAssociationCollection> TrackParamConstraintAssociationRefVector;
 
 #endif
