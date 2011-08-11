ff26f8a3d3e39e049d1e594e210394e5d180d68c
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2011-08-11 17:44:29 +0000

    new code

--- DQM/HLTEvF/plugins/OccupancyPlotter.cc
+++ DQM/HLTEvF/plugins/OccupancyPlotter.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jason Michael Slaunwhite,512 1-008,+41227670494,
 //         Created:  Fri Aug  5 10:34:47 CEST 2011
-// $Id$
+// $Id: OccupancyPlotter.cc,v 1.1 2011/08/05 09:19:38 slaunwhj Exp $
 //
 //
 
@@ -50,7 +50,8 @@
 
 using namespace edm;
 using namespace trigger;
-
+using std::vector;
+using std::string;
 
 
 class OccupancyPlotter : public edm::EDAnalyzer {
@@ -81,6 +82,10 @@ class OccupancyPlotter : public edm::EDAnalyzer {
   DQMStore * dbe;
 
   MonitorElement* testME;
+
+  HLTConfigProvider hltConfig_;
+
+  vector<std::string> triggersInSingleMuPD;
   
 };
 
@@ -129,7 +134,7 @@ void
 OccupancyPlotter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
-
+   using std::string;
 
    if (debugPrint) std::cout << "Inside analyze" << std::endl;
 
@@ -137,6 +142,86 @@ OccupancyPlotter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
      testME->Fill(1);
 
    }
+
+
+   // Access Trigger Results
+   edm::Handle<edm::TriggerResults> triggerResults;
+   iEvent.getByLabel(InputTag("TriggerResults","", "HLT"), triggerResults);
+   if (!triggerResults.isValid()) return;
+
+   if (debugPrint) std::cout << "Found triggerResults" << std::endl;
+
+
+   edm::Handle<trigger::TriggerEvent>         aodTriggerEvent;   
+   iEvent.getByLabel(InputTag("hltTriggerSummaryAOD", "", "HLT"), aodTriggerEvent);
+   
+   if ( !aodTriggerEvent.isValid() ) { 
+     if (debugPrint) std::cout << "No AOD trigger summary found! Returning..."; 
+     return; 
+   }
+
+   const TriggerObjectCollection objects = aodTriggerEvent->getObjects();
+
+   for (unsigned int iPath = 0; iPath < triggersInSingleMuPD.size(); iPath++) {
+
+     
+     std::string pathName = triggersInSingleMuPD[iPath];
+
+     std::cout << "Looking at path " << pathName << std::endl;
+     
+     unsigned int index = hltConfig_.triggerIndex(pathName);
+
+     if (index < triggerResults->size()) {
+       if(triggerResults->accept(index)){
+
+         std::cout << "We fired path " << pathName << std::endl;
+
+         // look up module labels for this path
+
+         vector<std::string> modulesThisPath = hltConfig_.moduleLabels(pathName);
+
+         if (debugPrint) std::cout << "Looping over module labels " << std::endl;
+
+         // loop backward through module names
+         for ( int iModule = (modulesThisPath.size()-1); iModule >= 0; iModule--) {
+
+           if (debugPrint) std::cout << "Module name is " << modulesThisPath[iModule] << std::endl;
+
+           // check to see if you have savetags information
+           if (hltConfig_.saveTags(modulesThisPath[iModule])){
+
+             if (debugPrint) std::cout << "This module " << modulesThisPath[iModule] <<" is a saveTags module of type " << hltConfig_.moduleType(modulesThisPath[iModule]) << std::endl;
+
+             InputTag moduleWhoseResultsWeWant(modulesThisPath[iModule], "", "HLT");
+
+             unsigned int indexOfModuleInAodTriggerEvent = aodTriggerEvent->filterIndex(moduleWhoseResultsWeWant);
+
+             if ( indexOfModuleInAodTriggerEvent < aodTriggerEvent->sizeFilters() ) {
+               const Keys &keys = aodTriggerEvent->filterKeys( indexOfModuleInAodTriggerEvent );
+               if (debugPrint) std::cout << "Got Keys for index " << indexOfModuleInAodTriggerEvent <<", size of keys is " << keys.size() << std::endl;
+               
+               for ( size_t iKey = 0; iKey < keys.size(); iKey++ ){
+                 TriggerObject foundObject = objects[keys[iKey]];
+                 if (debugPrint) std::cout << "This object has (pt, eta, phi) = "
+                                           << foundObject.pt()
+                                           << ", " << foundObject.eta() << ", "
+                                           << foundObject.phi() << std::endl;
+               }// end for each key               
+             }// end if filter in aodTriggerEvent
+
+
+             // OK, we found the last module. No need to look at the others.
+             // get out of the loop
+
+             break;
+           }// end if saveTags
+         }//end for each module    
+       }// end if accept
+     }// end if index in triggerResults
+   }// end for each path in PD
+
+
+   
 
 }
 
@@ -169,8 +254,32 @@ OccupancyPlotter::endJob()
 
 // ------------ method called when starting to processes a run  ------------
 void 
-OccupancyPlotter::beginRun(edm::Run const&, edm::EventSetup const&)
+OccupancyPlotter::beginRun(edm::Run const& iRun, edm::EventSetup const& iSetup)
 {
+
+  bool changed = true;
+  if (hltConfig_.init(iRun, iSetup, "HLT", changed)) {
+    if(debugPrint)
+      std::cout << "HLT config with process name " 
+                << "HLT" << " successfully extracted" << std::endl;
+  } else {
+    if(debugPrint)
+      std::cout << "Warning, didn't find process HLT" << std::endl;
+  }
+
+  vector<string> datasetNames =  hltConfig_.streamContent("A") ;
+  for (unsigned int i=0;i<datasetNames.size();i++) {
+    if (debugPrint) std::cout << "This is dataset " << datasetNames[i] <<std::endl;
+    vector<string> datasetPaths = hltConfig_.datasetContent(datasetNames[i]);
+    if (datasetNames[i] == "SingleMu"){      
+      if (debugPrint) std::cout <<"Found SingleMu PD" << std::endl;
+      triggersInSingleMuPD = datasetPaths;
+    }    
+
+  }// end of loop over dataset names
+
+  
+  
 }
 
 // ------------ method called when ending the processing of a run  ------------
