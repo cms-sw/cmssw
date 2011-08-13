32a1928ac419ae28c0542345de198d1ba52f3676
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-08-13 19:42:47 +0000

    Move dictionaries to fix circular package dependency

--- DataFormats/TrackReco/src/classes.h
+++ DataFormats/TrackReco/src/classes.h
@@ -4,6 +4,7 @@
 #include "Math/Cartesian3D.h" 
 #include "Math/Polar3D.h" 
 #include "Math/CylindricalEta3D.h" 
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h" 
 #include "DataFormats/TrackReco/interface/TrackExtra.h"
 #include "DataFormats/TrackReco/interface/TrackExtraFwd.h" 
@@ -87,5 +88,9 @@ namespace {
 
     edm::ValueMap<reco::TrackRefVector> vm_trv;
     edm::Wrapper<edm::ValueMap<reco::TrackRefVector> > w_vm_trv;
+
+    edm::helpers::KeyVal<edm::RefProd<std::vector<reco::Track> >,edm::RefProd<std::vector<TrajectorySeed> > > x1;
+    edm::AssociationMap<edm::OneToMany<std::vector<reco::Track>,std::vector<TrajectorySeed>,unsigned int> > x2;
+    edm::Wrapper<edm::AssociationMap<edm::OneToMany<std::vector<reco::Track>,std::vector<TrajectorySeed>,unsigned int> > > x3;
   };
 }
