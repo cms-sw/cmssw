aa754ea6eb85e05947150ac3ee44b819ebc70e31
Loic Quertenmont <sha1-87ca56b44ac7447a9c679691ad6ed79ac661d179@cern.ch>
2011-08-16 12:41:45 +0000

    Remove warning because directories already rexists

--- SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_PlotFunction.h
+++ SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_PlotFunction.h
@@ -37,14 +37,17 @@ TObject* GetObjectFromPath(TDirectory* Container, TDirectory* File, std::string 
 
 
 void MakeDirectories(std::string path){
-   size_t pos = 0;
+/*   size_t pos = 0;
    
    while(pos!=std::string::npos){
       pos = path.find("/",pos+1);
       if(pos!=std::string::npos){
-         system( (std::string("mkdir ") + path.substr(0,pos)).c_str());
+         system( (std::string("mkdir -p ") + path.substr(0,pos)).c_str());
       }
    }
+*/
+
+   system( (std::string("mkdir -p ") + path).c_str());
 }
 
 void SaveCanvas(TCanvas* c, std::string path, std::string name, bool OnlyPPNG=false){
