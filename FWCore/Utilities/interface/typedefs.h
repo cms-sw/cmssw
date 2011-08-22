53fee705182de0b2077a7ee5631cca47b37d0b0d
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-08-22 21:07:53 +0000

    Add 16 and 8 bit typedefs.

--- FWCore/Utilities/interface/typedefs.h
+++ FWCore/Utilities/interface/typedefs.h
@@ -5,8 +5,12 @@
 // To support class versioning for schema evolution, the typedef must resolve
 // to the same built-in C++ type on all supported platforms.
 // int64_t, uint64_t cannot be used, because they resolve to long on some platforms and long long on others.
-// For consistency, we don't use int32_t or uint32_t, either.
+// For consistency, we don't use int32_t, uint32_t, int16_t, uint16_t, int8_t or uint8_t, either.
 
+typedef char cms_int8_t;
+typedef unsigned char cms_uint8_t;
+typedef short cms_int16_t;
+typedef unsigned short cms_uint16_t;
 typedef int cms_int32_t;
 typedef unsigned int cms_uint32_t;
 typedef long long cms_int64_t;
