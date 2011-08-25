27110d2288ec5183e17f078b5324bddec892702e
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2011-08-25 14:26:07 +0000

    New macros for run numbers

--- CalibTracker/SiStripDCS/test/WebMonitoring/Alarm.py
+++ CalibTracker/SiStripDCS/test/WebMonitoring/Alarm.py
@@ -2,6 +2,13 @@
 
 import time
 import os
+import sys
+
+def sendMail(message):
+    os.system("echo \""+message+"\" > mail.txt")
+    os.system("mail -s \"DCS WARNING\" \"marco.de.mattia@cern.ch\" < \"mail.txt\"")
+    os.system("mail -s \"DCS WARNING\" \"gabriele.benelli@cern.ch\" < \"mail.txt\"")
+
 
 def runTheAlarm(file, type):
 
@@ -36,22 +43,51 @@ def runTheAlarm(file, type):
                             # print "modulesOn =", modulesOn
                             # 10 minutes = 600 seconds
                             if timeDiff > 600:
-                                print "WARNING: Bad IOV for", type, "lasting for =", timeDiff, "seconds and ending at", time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(IOVtime))
-                                os.system("echo \"WARNING: Bad IOV for "+type+" lasting for = "+str(timeDiff)+" seconds and ending at "+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(IOVtime))+"\" > mail.txt")
-                                os.system("mail -s \"DCS WARNING\" \"marco.de.mattia@cern.ch\" < \"mail.txt\"")
-                                os.system("mail -s \"DCS WARNING\" \"gabriele.benelli@cern.ch\" < \"mail.txt\"")
+                                message = "WARNING: Bad IOV for "+type+" lasting for = "+str(timeDiff)+" seconds and ending at "+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(IOVtime))
+                                print message
+                                sendMail(message)
                                 timeDiff = 0
                                 previousIOV = IOVtime
-    if timeDiff > 300:
+    if timeDiff > 600:
         print "WARNING: timeDiff =", timeDiff
         os.system("echo \"WARNING: Bad IOV for "+type+" lasting for = "+str(timeDiff)+" seconds and ending at "+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(IOVtime))+"\" > mail.txt")
         os.system("mail -s \"DCS WARNING\" \"marco.de.mattia@cern.ch\" < \"mail.txt\"")
         os.system("mail -s \"DCS WARNING\" \"gabriele.benelli@cern.ch\" < \"mail.txt\"")
 
 
-fileHV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_hv.js")
-fileLV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_hv.js")
+def checkHVGreaterThanLV(fileHV, fileLV):
+    currentTime = time.time()
 
+    arrayHV = fileHV.read().split("]")
+    arrayLV = fileLV.read().split("]")
+    if len(arrayHV) == len(arrayLV):
+        totalNum = len(arrayHV)
+        print "Comparing", totalNum, "IOVs"
+        for i in range(totalNum):
+            if( arrayLV[i].find(", [") != -1 ):
+                IOVtime = int(arrayHV[i].split("[")[1].split(",")[0])/1000
+                # 7200 are two hours in seconds = 2*60*60
+                # if (currentTime - IOVtime) > 0:
+                if (currentTime - IOVtime) < 7200:
+                    numHVon = int(arrayHV[i].split("[")[1].split(",")[1])
+                    numLVon = int(arrayLV[i].split("[")[1].split(",")[1])
+                    if( numHVon > numLVon ):
+                        message = "WARNING: for IOV"+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(IOVtime))+" numHVon("+str(numHVon)+") > numLVon("+str(numLVon)+")"
+                        print message
+                        sendMail(message)
+    else:
+        print "Error: HV and LV json files have different IOVs!"
+        sys.exit()
+
+
+fileHV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_hv.js")
 runTheAlarm(fileHV, "HV")
+
+fileLV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_lv.js")
 runTheAlarm(fileLV, "LV")
+
+# Reopen the files that have already been looped on the previous lines
+fileHV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_hv.js")
+fileLV = open("/afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/oneMonth_lv.js")
+checkHVGreaterThanLV(fileHV, fileLV)
 
---
+++ CalibTracker/SiStripDCS/test/WebMonitoring/cleanLogFilesDCS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /afs/cern.ch/cms/tracker/sistrcalib/DCSTrend/cronlog
+
+rm -f *.log
