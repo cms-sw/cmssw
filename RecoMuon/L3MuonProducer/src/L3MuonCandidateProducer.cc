522531519ef621710eca4fd6a0c721a3b43272e7
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-08-26 11:04:54 +0000

    clean up, and add LogError message for null trackrefs

--- RecoMuon/L3MuonProducer/src/L3MuonCandidateProducer.cc
+++ RecoMuon/L3MuonProducer/src/L3MuonCandidateProducer.cc
@@ -1,7 +1,7 @@
 /**  \class L3MuonCandidateProducer
- * 
+ *
  *   Intermediate step in the L3 muons selection.
- *   This class takes the L3 muons (which are reco::Tracks) 
+ *   This class takes the L3 muons (which are reco::Tracks)
  *   and creates the correspondent reco::RecoChargedCandidate.
  *
  *   Riccardo's comment:
@@ -63,7 +63,7 @@ L3MuonCandidateProducer::L3MuonCandidateProducer(const ParameterSet& parameterSe
     theType=InnerTrack;
   else if (muon_track_for_momentum == std::string("Standalone"))
     theType=OuterTrack;
-  else if (muon_track_for_momentum == std::string("Global")) 
+  else if (muon_track_for_momentum == std::string("Global"))
     theType=CombinedTrack;
   else {
     LogError("Muon|RecoMuon|L3MuonCandidateProducer")<<"invalid value for MuonPtOption, please choose among 'Tracker', 'Standalone', 'Global'";
@@ -72,7 +72,7 @@ L3MuonCandidateProducer::L3MuonCandidateProducer(const ParameterSet& parameterSe
 
   produces<RecoChargedCandidateCollection>();
 }
-  
+
 /// destructor
 L3MuonCandidateProducer::~L3MuonCandidateProducer(){
   LogTrace("Muon|RecoMuon|L3MuonCandidateProducer")<<" L3MuonCandidateProducer destructor called";
@@ -82,45 +82,52 @@ L3MuonCandidateProducer::~L3MuonCandidateProducer(){
 /// reconstruct muons
 void L3MuonCandidateProducer::produce(Event& event, const EventSetup& eventSetup){
   const string metname = "Muon|RecoMuon|L3MuonCandidateProducer";
-  
+
   // Take the L3 container
   LogTrace(metname)<<" Taking the L3/GLB muons: "<<theL3CollectionLabel.label();
-  Handle<TrackCollection> tracks; 
+  Handle<TrackCollection> tracks;
   event.getByLabel(theL3CollectionLabel,tracks);
 
-  edm::Handle<reco::MuonTrackLinksCollection> links; 
+  edm::Handle<reco::MuonTrackLinksCollection> links;
   if (theUseLinks)
     event.getByLabel(theL3LinksLabel, links);
 
   // Create a RecoChargedCandidate collection
   LogTrace(metname)<<" Creating the RecoChargedCandidate collection";
-  auto_ptr<RecoChargedCandidateCollection> candidates( new RecoChargedCandidateCollection()); 
-  LogDebug(metname) << " size = " << tracks->size() ;  
+  auto_ptr<RecoChargedCandidateCollection> candidates( new RecoChargedCandidateCollection());
+  LogDebug(metname) << " size = " << tracks->size();
   for (unsigned int i=0; i<tracks->size(); i++) {
     TrackRef inRef(tracks,i);
     TrackRef tkRef = TrackRef();
 
     if (theUseLinks) {
       for(reco::MuonTrackLinksCollection::const_iterator link = links->begin();
-          link != links->end(); ++link){ LogDebug(metname) << " i = " << i ;  
+          link != links->end(); ++link){ LogDebug(metname) << " i = " << i;
 
-        if(!link->trackerTrack().isNull()) LogTrace(metname) << " link tk pt " << link->trackerTrack()->pt();
-        if(!link->standAloneTrack().isNull()) LogTrace(metname) << " sta pt " << link->standAloneTrack()->pt();
-        if(!link->globalTrack().isNull()) LogTrace(metname) << " global pt " << link->globalTrack()->pt();
-        if(!inRef.isNull()) LogTrace(metname) << " inRef pt " << inRef->pt() ;
+        if (not link->trackerTrack().isNull()) LogTrace(metname) << " link tk pt " << link->trackerTrack()->pt();
+        if (not link->standAloneTrack().isNull()) LogTrace(metname) << " sta pt " << link->standAloneTrack()->pt();
+        if (not link->globalTrack().isNull()) LogTrace(metname) << " global pt " << link->globalTrack()->pt();
+        if (not inRef.isNull()) LogTrace(metname) << " inRef pt " << inRef->pt();
 
-        double dR = (!link->globalTrack().isNull()) ? deltaR(inRef->eta(),inRef->phi(),link->globalTrack()->eta(),link->globalTrack()->phi()) : 999.;
-        if(not link->globalTrack().isNull() and dR < 0.02 and abs(inRef->pt() - link->globalTrack()->pt())/inRef->pt() < 0.001) {
+        if (link->globalTrack().isNull()) {
+          edm::LogError("Muon|RecoMuon|L3MuonCandidateProducer") << "null reference to global track";
+          // skip this candidate
+          continue;
+        }
+
+        float dR  = deltaR(inRef->eta(),inRef->phi(),link->globalTrack()->eta(),link->globalTrack()->phi());
+        float dPt = abs(inRef->pt() - link->globalTrack()->pt())/inRef->pt();
+        if (dR < 0.02 and dPt < 0.001) {
           LogTrace(metname) << " *** pt matches *** ";
           switch(theType) {
-          case InnerTrack:    tkRef = link->trackerTrack();    break;
-          case OuterTrack:    tkRef = link->standAloneTrack(); break;
-          case CombinedTrack: tkRef = link->globalTrack();     break;
-          default:            tkRef = link->globalTrack();     break;
+            case InnerTrack:    tkRef = link->trackerTrack();    break;
+            case OuterTrack:    tkRef = link->standAloneTrack(); break;
+            case CombinedTrack: tkRef = link->globalTrack();     break;
+            default:            tkRef = link->globalTrack();     break;
           }
         }
       }
-      if(tkRef.isNull()) {
+      if (tkRef.isNull()) {
         LogDebug(metname) << "tkRef is NULL";
         tkRef = inRef;
       }
@@ -128,25 +135,25 @@ void L3MuonCandidateProducer::produce(Event& event, const EventSetup& eventSetup
       // theUseLinks is false
       tkRef = inRef;
     }
-    LogDebug(metname) << "tkRef Used For Momentum pt " << tkRef->pt() << " inRef from the input collection pt " << inRef->pt(); 
+    LogDebug(metname) << "tkRef Used For Momentum pt " << tkRef->pt() << " inRef from the input collection pt " << inRef->pt();
 
     Particle::Charge q = tkRef->charge();
     Particle::LorentzVector p4(tkRef->px(), tkRef->py(), tkRef->pz(), tkRef->p());
     Particle::Point vtx(tkRef->vx(),tkRef->vy(), tkRef->vz());
-    
+
     int pid = 13;
     if(abs(q)==1) pid = q < 0 ? 13 : -13;
     else LogWarning(metname) << "L3MuonCandidate has charge = "<<q;
-    RecoChargedCandidate cand(q, p4, vtx, pid); 
+    RecoChargedCandidate cand(q, p4, vtx, pid);
 
-    //set the inRef as the RecoChargedCandidate ref so that the isolation maps 
+    //set the inRef as the RecoChargedCandidate ref so that the isolation maps
     //work in downstream filters
     cand.setTrack(inRef);
     candidates->push_back(cand);
   }
-  
+
   event.put(candidates);
- 
+
   LogTrace(metname)<<" Event loaded"
                    <<"================================";
 }
