76fe352a75239794b3f12a3985ea78e3a11ec41f
Christopher Farrell <sha1-8ff4c3e4c5e5d4f1b2a4a4e825bd74e83b42ab48@cern.ch>
2011-09-06 20:18:39 +0000

    Update to 2.1 fb-1

--- SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Samples.h
+++ SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Samples.h
@@ -63,7 +63,7 @@
 #define SID_DS500     57
 
 int                  RunningPeriods = 2;
-double               IntegratedLuminosity = 1912; //1947; //1631; //976.204518023; //705.273820; //342.603275; //204.160928; //191.04;
+double               IntegratedLuminosity = 2080; //1912; //1947; //1631; //976.204518023; //705.273820; //342.603275; //204.160928; //191.04;
 double               IntegratedLuminosityBeforeTriggerChange = 353.494; // Total luminosity taken before RPC L1 trigger change (went into effect on run 165970)
 float                Event_Weight = 1;
 int                  MaxEntry = -1;
@@ -224,6 +224,7 @@ void GetInputFiles(std::vector<std::string>& inputFiles, std::string SampleName,
      inputFiles.push_back(BaseDirectory + "Data_RunA_172791_172802.root");
      inputFiles.push_back(BaseDirectory + "Data_RunA_172803_172900.root");
      inputFiles.push_back(BaseDirectory + "Data_RunA_172901_173243.root");
+     inputFiles.push_back(BaseDirectory + "Data_RunA_173244_173692.root");
    }else if(SampleName.find("MC_",0)<std::string::npos){
      inputFiles.push_back(BaseDirectory + SampleName + ".root");
    }else{
