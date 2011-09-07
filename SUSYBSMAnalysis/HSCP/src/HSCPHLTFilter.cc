2c6dbee87e94134b408fda3eff45c5bbd6a67611
Christopher Farrell <sha1-8ff4c3e4c5e5d4f1b2a4a4e825bd74e83b42ab48@cern.ch>
2011-09-07 22:57:50 +0000

    Remove stray backslash

--- SUSYBSMAnalysis/HSCP/src/HSCPHLTFilter.cc
+++ SUSYBSMAnalysis/HSCP/src/HSCPHLTFilter.cc
@@ -1,4 +1,4 @@
-\#include <memory>
+#include <memory>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/EDFilter.h"
