73e58a88509d49681c5c87cf4ba6546e6fb068aa
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-09-08 07:50:48 +0000

    replaced builder.strips by builder.modStrips (to disable track quality cuts for PFElectrons in strips)

--- RecoTauTag/RecoTau/python/RecoTauPiZeroProducer_cfi.py
+++ RecoTauTag/RecoTau/python/RecoTauPiZeroProducer_cfi.py
@@ -10,10 +10,10 @@ ak5PFJetsRecoTauGreedyPiZeros = cms.EDProducer(
     massHypothesis = cms.double(0.136),
     outputSelection = cms.string('pt > 1.5'),
     builders = cms.VPSet(
-        builders.comboStrips,
+        builders.comboStrips
     ),
     ranking = cms.VPSet(
-        ranking.greedy,
+        ranking.greedy
     ),
 )
 
@@ -25,12 +25,13 @@ ak5PFJetsRecoTauPiZeros = cms.EDProducer(
     outputSelection = cms.string('pt > 1.5'),
     builders = cms.VPSet(
         builders.combinatoricPhotonPairs,
-        builders.strips,
+        #builders.strips
+        builders.modStrips 
     ),
     ranking = cms.VPSet(
         ranking.nearPiZeroMassBarrel, # Prefer pi zeros +- 0.05 GeV correct mass
         ranking.nearPiZeroMassEndcap,
-        ranking.isInStrip,      # Allow incorrect masses if in strip
+        ranking.isInStrip             # Allow incorrect masses if in strip
     ),
 )
 
@@ -42,7 +43,7 @@ ak5PFJetsLegacyTaNCPiZeros = cms.EDProducer(
     outputSelection = cms.string('pt > 1.5'),
     builders = cms.VPSet(
         builders.allSinglePhotons,
-        builders.combinatoricPhotonPairs,
+        builders.combinatoricPhotonPairs
     ),
     ranking = cms.VPSet(
         ranking.legacyPFTauDecayModeSelection
@@ -56,10 +57,11 @@ ak5PFJetsLegacyHPSPiZeros = cms.EDProducer(
     massHypothesis = cms.double(0.136),
     outputSelection = cms.string('pt > 0'),
     builders = cms.VPSet(
-        builders.strips,
+        #builders.strips
+        builders.modStrips 
     ),
     ranking = cms.VPSet(
         ranking.isInStrip
-    ),
+    )
 )
 
