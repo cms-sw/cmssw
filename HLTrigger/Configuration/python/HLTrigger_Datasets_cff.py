df60ac4c5f4c7292c0ebaad237f3fd3efdbadd0e
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-09-09 06:41:51 +0000

    Remove new BeamGas paths until new L1 menu is available

--- HLTrigger/Configuration/python/HLTrigger_Datasets_cff.py
+++ HLTrigger/Configuration/python/HLTrigger_Datasets_cff.py
@@ -1,4 +1,4 @@
-# /dev/CMSSW_4_2_0/GRun/V229
+# /dev/CMSSW_4_2_0/GRun/V230
 
 import FWCore.ParameterSet.Config as cms
 
@@ -18,8 +18,7 @@ streamA_datasetCommissioning_selector.hltResults = cms.InputTag('TriggerResults'
 streamA_datasetCommissioning_selector.l1tResults = cms.InputTag('')
 streamA_datasetCommissioning_selector.throw      = cms.bool(False)
 streamA_datasetCommissioning_selector.triggerConditions = cms.vstring('HLT_Activity_Ecal_SC7_v8', 
-    'HLT_BeamGas_HF_Beam1_v1', 
-    'HLT_BeamGas_HF_Beam2_v1', 
+    'HLT_BeamGas_BSC_v5', 
     'HLT_BeamGas_HF_v6', 
     'HLT_IsoTrackHB_v7', 
     'HLT_IsoTrackHE_v8', 
--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_4_2_0/HIon/V229 (CMSSW_4_2_0_HLT23)
+# /dev/CMSSW_4_2_0/HIon/V230 (CMSSW_4_2_0_HLT23)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_4_2_0/HIon/V229')
+  tableName = cms.string('/dev/CMSSW_4_2_0/HIon/V230')
 )
 
 process.streams = cms.PSet( 
