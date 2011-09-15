79a1ab97997cc62be0eb84b99ca7aad728312453
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2011-09-15 11:01:48 +0000

    pass by reference instead of value in writeXML method. On top of revision 1.13

--- Alignment/MuonAlignment/interface/MuonAlignment.h
+++ Alignment/MuonAlignment/interface/MuonAlignment.h
@@ -9,6 +9,7 @@
  *  \author Andre Sznajder - UERJ(Brazil)
  */
 
+#include <vector>
 #include <map>
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -32,18 +33,18 @@ class MuonAlignment {
       AlignableNavigator* getAlignableNavigator() { return theAlignableNavigator; }
 
 
-      void moveAlignableLocalCoord( DetId& , align::Scalars& , align::Scalars& );
-      void moveAlignableGlobalCoord( DetId& , align::Scalars& , align::Scalars& );
+      void moveAlignableLocalCoord( DetId& , std::vector<float>& , std::vector<float>& );
+      void moveAlignableGlobalCoord( DetId& , std::vector<float>& , std::vector<float>& );
 
-      void recursiveList(align::Alignables alignables, align::Alignables &theList);
-      void recursiveMap(align::Alignables alignables, std::map<align::ID, Alignable*> &theMap);
-      void recursiveStructureMap(align::Alignables alignables, std::map<std::pair<align::StructureType, align::ID>, Alignable*> &theMap);
+      void recursiveList(std::vector<Alignable*> alignables, std::vector<Alignable*> &theList);
+      void recursiveMap(std::vector<Alignable*> alignables, std::map<align::ID, Alignable*> &theMap);
+      void recursiveStructureMap(std::vector<Alignable*> alignables, std::map<std::pair<align::StructureType, align::ID>, Alignable*> &theMap);
 
       void copyAlignmentToSurvey(double shiftErr, double angleErr);
       void fillGapsInSurvey(double shiftErr, double angleErr);
       void copySurveyToAlignment();
 
-      void writeXML(const edm::ParameterSet iConfig, const edm::EventSetup &iSetup);
+      void writeXML(const edm::ParameterSet &iConfig, const edm::EventSetup &iSetup);
 
       void saveDTSurveyToDB();
       void saveCSCSurveyToDB();
@@ -63,13 +64,15 @@ class MuonAlignment {
       std::string theDTSurveyRecordName, theDTSurveyErrorRecordName;
       std::string theCSCSurveyRecordName, theCSCSurveyErrorRecordName;
  
-      align::Scalars displacements;
+      std::vector<float> displacements;
 
-      align::Scalars rotations;
+      std::vector<float> rotations;
 
       AlignableMuon* theAlignableMuon;
 
       AlignableNavigator* theAlignableNavigator;
+
+
 };
 
 #endif //MuonAlignment_H
