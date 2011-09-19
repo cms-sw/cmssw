c868a1069e7d7b9d609d1474b6754023141db45e
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2011-09-19 11:44:50 +0000

    fixed coverity UNINIT_CTOR defect

--- Alignment/CommonAlignment/interface/MisalignmentScenarioBuilder.h
+++ Alignment/CommonAlignment/interface/MisalignmentScenarioBuilder.h
@@ -3,8 +3,8 @@
 
 /// \class MisalignmentScenarioBuilder
 ///
-/// $Date: 2009/03/15 17:10:46 $
-/// $Revision: 1.8 $
+/// $Date: 2009/03/20 17:28:51 $
+/// $Revision: 1.9 $
 ///
 /// $Author: flucke $
 /// \author Frederic Ronga - CERN-PH-CMG
@@ -24,7 +24,8 @@ class MisalignmentScenarioBuilder
 public:
  
   /// Default constructor
-  MisalignmentScenarioBuilder( ) {};
+  MisalignmentScenarioBuilder( ) :
+    theModifierCounter(0) {};
 
   /// Destructor
   virtual ~MisalignmentScenarioBuilder() {};
