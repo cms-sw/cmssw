35c88876ef5e345d264280ff78cb7379a5d58c5d
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-09-19 14:24:34 +0000

    Add missing boost/version.hpp header.
    * Not sure what happened since last time, maybe some header which was
      implicitly including it got cleaned up?

--- FWCore/PluginManager/src/CacheParser.cc
+++ FWCore/PluginManager/src/CacheParser.cc
@@ -8,12 +8,13 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Wed Apr  4 14:30:46 EDT 2007
-// $Id: CacheParser.cc,v 1.4 2008/11/11 11:54:43 elmer Exp $
+// $Id: CacheParser.cc,v 1.5 2011/08/24 12:28:53 eulisse Exp $
 //
 
 // system include files
 #include <algorithm>
 #include <limits>
+#include "boost/version.hpp"
 
 // user include files
 #include "FWCore/PluginManager/interface/CacheParser.h"
