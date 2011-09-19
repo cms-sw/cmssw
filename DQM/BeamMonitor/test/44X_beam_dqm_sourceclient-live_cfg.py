3c64c7be8bdc3fefd11880df32acf5ece43afa70
Sushil Chauhan <sha1-dc853c26adfd296df98de885cec220b751df39de@cern.ch>
2011-09-19 14:04:21 +0000

    Common for PP and HI for 44X

--- DQM/BeamMonitor/test/44X_beam_dqm_sourceclient-live_cfg.py
+++ DQM/BeamMonitor/test/44X_beam_dqm_sourceclient-live_cfg.py
@@ -54,7 +54,7 @@ process.dqmBeamMonitorBx.resetEveryNLumi = 30
 #it will pick all triggers which has these strings in theri name
 process.dqmBeamMonitor.jetTrigger  = cms.untracked.vstring("HLT_ZeroBias_v",
                                                            "HLT_Jet300_v",
-                                                           "HLT_QuadJet60_v")
+                                                           "HLT_QuadJet70_v")
 
 process.dqmBeamMonitor.hltResults = cms.InputTag("TriggerResults","","HLT")
 
@@ -187,22 +187,144 @@ process.dqmTKStatus = cms.EDAnalyzer("TKStatus",
 #--------------------------
 # Path
 #--------------------------
-
-
+from DQM.Integration.test.environment_cfi import runType, runTypes
+                               
 process.dqmcommon = cms.Sequence(process.dqmEnv
                                 *process.dqmSaver)
 
 process.monitor = cms.Sequence(process.dqmBeamMonitor)
 
-
 #process.monitor_pixelless = cms.Sequence(process.dqmBeamMonitor_pixelless
-#                                          *process.dqmEnvPixelLess)
+#                                        *process.dqmEnvPixelLess)
 
-process.p = cms.Path(process.scalersRawToDigi
-                    *process.dqmTKStatus
-                    *process.hltTriggerTypeFilter
-                    *process.dqmcommon
-                    *process.tracking_FirstStep
-                    *process.offlinePrimaryVertices
-                    *process.monitor)
+if (runType == runTypes.pp_run):
 
+    print "Running pp"
+    process.p = cms.Path(process.scalersRawToDigi
+                         *process.dqmTKStatus
+                         *process.hltTriggerTypeFilter
+                         *process.dqmcommon
+                         *process.tracking_FirstStep
+                         *process.offlinePrimaryVertices
+                         *process.monitor)
+
+
+
+#--------------------------------------------------
+# Heavy Ion Specific Fed Raw Data Collection Label
+#--------------------------------------------------
+
+if (runType == runTypes.hi_run):
+    process.castorDigis.InputLabel = cms.InputTag("rawDataRepacker")
+    process.csctfDigis.producer = cms.InputTag("rawDataRepacker")
+    process.dttfDigis.DTTF_FED_Source = cms.InputTag("rawDataRepacker")
+    process.ecalDigis.InputLabel = cms.InputTag("rawDataRepacker")
+    process.ecalPreshowerDigis.sourceTag = cms.InputTag("rawDataRepacker")
+    process.gctDigis.inputLabel = cms.InputTag("rawDataRepacker")
+    process.gtDigis.DaqGtInputTag = cms.InputTag("rawDataRepacker")
+    process.gtEvmDigis.EvmGtInputTag = cms.InputTag("rawDataRepacker")
+    process.hcalDigis.InputLabel = cms.InputTag("rawDataRepacker")
+    process.muonCSCDigis.InputObjects = cms.InputTag("rawDataRepacker")
+    process.muonDTDigis.inputLabel = cms.InputTag("rawDataRepacker")
+    process.muonRPCDigis.InputLabel = cms.InputTag("rawDataRepacker")
+    process.scalersRawToDigi.scalersInputTag = cms.InputTag("rawDataRepacker")
+
+#----------------------------
+# Event Source
+#-----------------------------
+    process.load("DQM.Integration.test.inputsource_cfi")
+    process.EventStreamHttpReader.SelectEvents =  cms.untracked.PSet(
+     SelectEvents = cms.vstring(
+        'HLT_HI*'
+     )
+    )
+
+
+    process.dqmBeamMonitor.resetEveryNLumi = 5
+    process.dqmBeamMonitor.resetPVEveryNLumi = 5
+
+# HI only has one bunch
+    process.dqmBeamMonitorBx.fitEveryNLumi = 50
+    process.dqmBeamMonitorBx.resetEveryNLumi = 50
+
+
+#TriggerName for selecting pv for DIP publication, NO wildcard needed here
+#it will pick all triggers which has these strings in theri name
+    process.dqmBeamMonitor.jetTrigger  = cms.untracked.vstring("HLT_HI*"
+                                                               )
+
+    process.dqmBeamMonitor.hltResults = cms.InputTag("TriggerResults","","HLT")
+
+
+## Load Heavy Ion Sequence
+    process.load("Configuration.StandardSequences.ReconstructionHeavyIons_cff") ## HI sequences
+
+# Select events based on the pixel cluster multiplicity
+    import  HLTrigger.special.hltPixelActivityFilter_cfi
+    process.multFilter = HLTrigger.special.hltPixelActivityFilter_cfi.hltPixelActivityFilter.clone(
+      inputTag  = cms.InputTag('siPixelClusters'),
+      minClusters = cms.uint32(150),
+      maxClusters = cms.uint32(50000)
+      )
+
+    process.filter_step = cms.Sequence( process.siPixelDigis
+                                   *process.siPixelClusters
+                                   #*process.multFilter
+                                  )
+
+    process.HIRecoForDQM = cms.Sequence( process.siPixelDigis
+                                    *process.siPixelClusters
+                                    *process.siPixelRecHits
+                                    *process.offlineBeamSpot
+                                    *process.hiPixelVertices
+                                    *process.hiPixel3PrimTracks
+                                   )
+
+# use HI pixel tracking and vertexing
+    process.dqmBeamMonitor.BeamFitter.TrackCollection = cms.untracked.InputTag('hiPixel3PrimTracks')
+    process.dqmBeamMonitorBx.BeamFitter.TrackCollection = cms.untracked.InputTag('hiPixel3PrimTracks')
+    process.dqmBeamMonitor.primaryVertex = cms.untracked.InputTag('hiSelectedVertex')
+    process.dqmBeamMonitor.PVFitter.VertexCollection = cms.untracked.InputTag('hiSelectedVertex')
+
+# Beamspot DQM options
+    process.dqmBeamMonitor.OnlineMode = True                  ## in MC the LS are not ordered??
+    process.dqmBeamMonitor.BeamFitter.MinimumTotalLayers = 3   ## using pixel triplets
+    process.dqmBeamMonitorBx.BeamFitter.MinimumTotalLayers = 3   ## using pixel triplets
+
+# make pixel vertexing less sensitive to incorrect beamspot
+    process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.originRadius = 0.2
+    process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.fixedError = 0.5
+    process.hiSelectedProtoTracks.maxD0Significance = 100
+    process.hiPixelAdaptiveVertex.TkFilterParameters.maxD0Significance = 100
+    process.hiPixelAdaptiveVertex.useBeamConstraint = False
+    process.hiPixelAdaptiveVertex.PVSelParameters.maxDistanceToBeam = 1.0
+
+# Lower for HI
+    process.dqmBeamMonitor.PVFitter.minNrVerticesForFit = 20
+    process.dqmBeamMonitorBx.PVFitter.minNrVerticesForFit = 20
+
+
+#--------------------------
+# Scheduling
+#--------------------------
+#   process.phystrigger = cms.Sequence(process.hltTriggerTypeFilter
+#                                      *process.gtDigis
+#                                      *process.hltLevel1GTSeed)
+
+
+    process.dqmcommon = cms.Sequence(process.dqmEnv
+                                   *process.dqmSaver)
+
+
+    process.monitor = cms.Sequence(process.dqmBeamMonitor
+                                  #+process.dqmBeamMonitorBx
+                                 )
+    
+    print "Running HI"
+    process.hi = cms.Path(process.scalersRawToDigi
+                        *process.dqmTKStatus
+                        *process.hltTriggerTypeFilter
+                        *process.filter_step
+                        *process.HIRecoForDQM
+                        *process.dqmcommon
+                        *process.monitor)
