25c978c887cdcead571ebc11717e97d72585c0c4
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2011-09-19 20:52:47 +0000

    Changed the definition of track selections, hiSelected now corresponds to the old hiGoodTightSelection from UserCode/edwenger/HiTrkEffAnalyzer

--- RecoHI/HiTracking/python/HISelectedTracks_cfi.py
+++ RecoHI/HiTracking/python/HISelectedTracks_cfi.py
@@ -1,51 +1,69 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoTracker.FinalTrackSelectors.selectHighPurity_cfi import selectHighPurity
+hiGoodTracks = cms.EDProducer("HiAnalyticalTrackSelector",
 
-#loose
-hiTracksWithLooseQuality = selectHighPurity.clone(
-    src = "hiGlobalPrimTracks",
-    keepAllTracks = False,
-    
-    vertices = "hiSelectedVertex",
-    vertexCut = "ndof>=0&((chi2==0.0)|(chi2prob(chi2,ndof)>=0.01))",
-    copyTrajectories = True,             # needed by TrackClusterRemover
-    
-    qualityBit = 'loose',                # set to '' if you don't want to set the bit
+                                  src = cms.InputTag("hiSelectedTracks"),
+                                  keepAllTracks = cms.bool(False), ## if set to true tracks failing this filter are kept in the output
+                                  beamspot = cms.InputTag("offlineBeamSpot"),
 
-    chi2n_par = 1.6,                     # normalizedChi2 < nLayers * chi2n_par
-    res_par = [0.003, 0.001],            # residual parameterization (re-check in HI)
-    d0_par1 = [9999, 1],                 # parameterized nomd0E
-    dz_par1 = [9999, 1],
-    d0_par2 = [300.0, 0.3],              # d0E from tk.d0Error
-    dz_par2 = [300.0, 0.3],
+                                  vertices = cms.InputTag("hiSelectedVertex"),
+                                  vtxNumber = cms.int32(0),
+                                  vtxTracks = cms.uint32(0), ## at least 3 tracks
+                                  vtxChi2Prob = cms.double(0.0), ## at least 1% chi2nprobability (if it has a chi2)
 
-    minNumberLayers = 3,
-    minNumber3DLayers = 3,
-    maxNumberLostLayers = 999
-)
+                                  copyTrajectories = cms.untracked.bool(True),
+                                  copyExtras = cms.untracked.bool(False), ## set to false on AOD
+                                  qualityBit = cms.string('highPurity'), ## set to '' or comment out if you don't want to set the bit
 
-#tight
-hiTracksWithTightQuality = hiTracksWithLooseQuality.clone(
-    src = "hiTracksWithLooseQuality",
-    keepAllTracks = False,
-    qualityBit = 'tight',
-    chi2n_par = 0.7,   
-    d0_par2 = [30.0, 0.3],
-    dz_par2 = [30.0, 0.3],
-    minNumberLayers = 5,
-)
+                                  # parameters for cutting on pterror/pt and number of valid hits
+                                  min_relpterr = cms.double(0.05),
+                                  min_nhits = cms.uint32(12),
 
-#highPurity
-hiSelectedTracks = hiTracksWithLooseQuality.clone(
-    src = "hiTracksWithTightQuality",
-    keepAllTracks = False,
-    qualityBit = 'highPurity',
-    chi2n_par = 0.4,    
-    d0_par2 = [5.0, 0.3],               
-    dz_par2 = [30.0, 0.3],
-    minNumberLayers = 7,
-)
+                                  # parameters for adapted optimal cuts on chi2 and primary vertex compatibility
+                                  chi2n_par = cms.double(99999.), # already applied in hiSelectedTracks
+                                  res_par = cms.vdouble(99999., 99999.),
+                                  d0_par1 = cms.vdouble(99999., 0.0),
+                                  dz_par1 = cms.vdouble(99999., 0.0),
+                                  d0_par2 = cms.vdouble(3, 0.0),
+                                  dz_par2 = cms.vdouble(3, 0.0),
+                                  # Boolean indicating if adapted primary vertex compatibility cuts are to be applied.
+                                  applyAdaptedPVCuts = cms.bool(True),
+
+                                  # Impact parameter absolute cuts.
+                                  max_d0 = cms.double(1000),
+                                  max_z0 = cms.double(1000),
+
+                                  # Cuts on numbers of layers with hits/3D hits/lost hits.
+                                  minNumberLayers = cms.uint32(0),
+                                  minNumber3DLayers = cms.uint32(0),
+                                  maxNumberLostLayers = cms.uint32(99999)
+                              )
+
+hiSelectedTracks = hiGoodTracks.clone(src = cms.InputTag("hiGlobalPrimTracks"),
+                                                                              qualityBit = cms.string('highPurity'),
+                                                                              min_nhits = cms.uint32(13),
+                                                                              chi2n_par = cms.double(0.15))
+
+hiTracksWithLooseQuality = hiGoodTracks.clone(src = cms.InputTag("hiGlobalPrimTracks"),
+                                                                              qualityBit = cms.string('loose'),
+                                                                              min_nhits = cms.uint32(12),
+                                                                              chi2n_par = cms.double(0.15),
+                                                                              d0_par2 = cms.vdouble(5.0, 0.0),
+                                                                              dz_par2 = cms.vdouble(5.0, 0.0))
+
+hiTracksWithTightQuality = hiGoodTracks.clone(src = cms.InputTag("hiGlobalPrimTracks"),
+                                                                        qualityBit = cms.string('tight'),
+                                                                        min_relpterr = cms.double(0.06),
+                                                                        min_nhits = cms.uint32(13),
+                                                                        chi2n_par = cms.double(0.15),
+                                                                        d0_par2 =cms.vdouble(999.0, 0.0),
+                                                                        dz_par2 = cms.vdouble(999.0, 0.0))
+
+
+hiGoodTracksSelection = cms.Sequence(hiGoodTracks)
+hiTracksWithLooseQualitySelection = cms.Sequence(hiTracksWithLooseQuality)
+hiSelectedTracksSelection = cms.Sequence(hiSelectedTracks)
+hiTracksWithTightQualitySelection = cms.Sequence(hiTracksWithTightQuality)
 
 #complete sequence
 hiTracksWithQuality = cms.Sequence(hiTracksWithLooseQuality
