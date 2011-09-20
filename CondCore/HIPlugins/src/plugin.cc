54717e06123541f91ef796a5087676eede70a734
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2011-09-20 14:18:30 +0000

    this

--- CondCore/HIPlugins/src/plugin.cc
+++ CondCore/HIPlugins/src/plugin.cc
@@ -1,7 +1,8 @@
 #include "CondCore/PluginSystem/interface/registration_macros.h"
 #include "CondFormats/DataRecord/interface/HeavyIonRcd.h"
+#include "CondFormats/DataRecord/interface/HeavyIonRPRcd.h"
 #include "CondFormats/HIObjects/interface/CentralityTable.h"
 #include "CondFormats/HIObjects/interface/RPFlatParams.h"
 
-REGISTER_PLUGIN(HeavyIonRcd,RPFlatParams);
+REGISTER_PLUGIN(HeavyIonRPRcd,RPFlatParams);
 REGISTER_PLUGIN(HeavyIonRcd,CentralityTable);
