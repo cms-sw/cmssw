289bad0037aebbceb6b9ec3417eaec116b7ad58d
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-09-21 10:36:23 +0000

    Fix coverity issue.
    https://cmscov.cern.ch/sourcebrowser.htm?projectId=10004#mergedDefectId=13459&streamDefectId=54790&defectInstanceId=87645&fileInstanceId=199997

--- DQM/DTMonitorModule/src/ROSDebugUtility.h
+++ DQM/DTMonitorModule/src/ROSDebugUtility.h
@@ -49,7 +49,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
   double ResetCount = ROSDebug_BcntResCnt*0.0000891;
  
   if (ResetCount_unfolded_comp <= (ResetCount)) {
-  cont = cont;  
    ResetCount_unfolded = ResetCount + cycle*cont;
    }
   else { cont = cont + 1;
@@ -62,7 +61,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 2){
   double ResetCount2 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp2 <= (ResetCount2)) {
-  cont2 = cont2;  
    ResetCount_unfolded = ResetCount2 + cycle*cont2;
    }
   else { cont2 = cont2 + 1;
@@ -75,7 +73,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 3){
   double ResetCount3 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp3 <= (ResetCount3)) {
-  cont3 = cont3;  
    ResetCount_unfolded = ResetCount3 + cycle*cont3;
    }
   else { cont3 = cont3 + 1;
@@ -89,7 +86,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 4){
   double ResetCount4 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp4 <= (ResetCount4)) {
-  cont4 = cont4;  
    ResetCount_unfolded = ResetCount4 + cycle*cont4;
    }
   else { cont4 = cont4 + 1;
@@ -102,7 +98,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 5){
   double ResetCount5 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp5 <= (ResetCount5)) {
-  cont5 = cont5;  
    ResetCount_unfolded = ResetCount5 + cycle*cont5;
    }
   else { cont5 = cont5 + 1;
@@ -115,7 +110,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 6){
   double ResetCount6 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp6 <= (ResetCount6)) {
-  cont6 = cont6;  
    ResetCount_unfolded = ResetCount6 + cycle*cont6;
    }
   else { cont6 = cont6 + 1;
@@ -128,7 +122,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 7){
   double ResetCount7 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp7 <= (ResetCount7)) {
-  cont7 = cont7;  
    ResetCount_unfolded = ResetCount7 + cycle*cont7;
    }
   else { cont7 = cont7 + 1;
@@ -141,7 +134,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 8){
   double ResetCount8 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp8 <= (ResetCount8)) {
-  cont8 = cont8;  
    ResetCount_unfolded = ResetCount8 + cycle*cont8;
    }
   else { cont8 = cont8 + 1;
@@ -154,7 +146,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 9){
   double ResetCount9 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp9 <= (ResetCount9)) {
-  cont9 = cont9;  
    ResetCount_unfolded = ResetCount9 + cycle*cont9;
    }
   else { cont9 = cont9 + 1;
@@ -167,7 +158,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 10){
   double ResetCount10 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp10 <= (ResetCount10)) {
-  cont10 = cont10;  
    ResetCount_unfolded = ResetCount10 + cycle*cont10;
    }
   else { cont10 = cont10 + 1;
@@ -180,7 +170,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 11){
   double ResetCount11 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp11 <= (ResetCount11)) {
-  cont11 = cont11;  
    ResetCount_unfolded = ResetCount11 + cycle*cont11;
    }
   else { cont11 = cont11 + 1;
@@ -193,7 +182,6 @@ inline void ROSWords_t(double& ResetCount_unfolded,int ROS_number,int ROSDebug_B
  else if (ROS_number == 12){
   double ResetCount12 = ROSDebug_BcntResCnt*0.0000891;
   if (ResetCount_unfolded_comp12 <= (ResetCount12)) {
-  cont12 = cont12;  
    ResetCount_unfolded = ResetCount12 + cycle*cont12;
    }
   else { cont12 = cont12 + 1;
