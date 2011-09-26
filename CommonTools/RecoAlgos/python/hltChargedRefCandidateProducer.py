609c267da71a9043234e902313476c37bdb7fc8d
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-09-26 13:12:27 +0000

    sorry, wrong file name

--- CommonTools/RecoAlgos/python/hltChargedRefCandidateProducer.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltChargedRefCandidateProducer = cms.EDProducer("ChargedRefCandidateProducer",
-    src          = cms.InputTag('tracks'),
-    particleType = cms.string('pi+')
-)
