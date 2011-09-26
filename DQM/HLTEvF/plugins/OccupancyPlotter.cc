0b087af4ff14a3070e7f0e199ac7825e4fe48dc2
Andrew Brinkerhoff <sha1-c06a4ae8f8340a5e909e71e7f3dfef4f3aebb0b4@cern.ch>
2011-09-26 10:15:04 +0000

    Fixed Jet spike problem (HLTLevel1GTSeed)

--- DQM/HLTEvF/plugins/OccupancyPlotter.cc
+++ DQM/HLTEvF/plugins/OccupancyPlotter.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jason Michael Slaunwhite,512 1-008,`+41227670494,
 //         Created:  Fri Aug  5 10:34:47 CEST 2011
-// $Id: OccupancyPlotter.cc,v 1.4 2011/08/16 10:29:04 abrinke1 Exp $
+// $Id: OccupancyPlotter.cc,v 1.6 2011/09/21 13:54:48 abrinke1 Exp $
 //
 //
 
@@ -204,7 +204,7 @@ OccupancyPlotter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
              if (debugPrint) std::cout << "For path " << pathName << " this module " << modulesThisPath[iModule] <<" is a saveTags module of type " << hltConfig_.moduleType(modulesThisPath[iModule]) << std::endl;
 
-	     //if (hltConfig_.moduleType(modulesThisPath[iModule]) == "HLTLevel1GTSeed") break;
+	     if (hltConfig_.moduleType(modulesThisPath[iModule]) == "HLTLevel1GTSeed") break;
 
              InputTag moduleWhoseResultsWeWant(modulesThisPath[iModule], "", "HLT");
 
@@ -350,9 +350,13 @@ Double_t EtaMax = 2.610;
 Double_t eta_bins[] = {-2.610,-2.349,-2.088,-1.827,-1.740,-1.653,-1.566,-1.479,-1.392,-1.305,-1.044,-0.783,-0.522,-0.261,0,0.261,0.522,0.783,1.044,1.305,1.392,1.479,1.566,1.653,1.740,1.827,2.088,2.349,2.610}; //Has narrower bins in Barrel/Endcap border region
 Double_t phi_bins[] = {-TMath::Pi(),-3*TMath::Pi()/4,-TMath::Pi()/2,-TMath::Pi()/4,0,TMath::Pi()/4,TMath::Pi()/2,3*TMath::Pi()/4,TMath::Pi()};
 
-TH2F * hist_EtaVsPhi = new TH2F(h_name.c_str(),h_title.c_str(),numBinsEta,eta_bins,numBinsPhi,phi_bins);
-TH1F * hist_1dEta = new TH1F(h_name_1dEta.c_str(),h_title_1dEta.c_str(),numBinsEtaFine,-EtaMax,EtaMax);
-TH1F * hist_1dPhi = new TH1F(h_name_1dPhi.c_str(),h_title_1dPhi.c_str(),numBinsPhiFine,-EtaMax,EtaMax);
+ TH2F * hist_EtaVsPhi = new TH2F(h_name.c_str(),h_title.c_str(),numBinsEta,eta_bins,numBinsPhi,phi_bins);
+ TH1F * hist_1dEta = new TH1F(h_name_1dEta.c_str(),h_title_1dEta.c_str(),numBinsEtaFine,-EtaMax,EtaMax);
+ TH1F * hist_1dPhi = new TH1F(h_name_1dPhi.c_str(),h_title_1dPhi.c_str(),numBinsPhiFine,-EtaMax,EtaMax);
+
+ hist_EtaVsPhi->SetMinimum(0);
+ hist_1dEta->SetMinimum(0);
+ hist_1dPhi->SetMinimum(0);
 
 MonitorElement * ME_EtaVsPhi = dbe->book2D(h_name.c_str(),hist_EtaVsPhi);
 MonitorElement * ME_1dEta = dbe->book1D(h_name_1dEta.c_str(),hist_1dEta);
@@ -415,12 +419,6 @@ if (label != "SingleMu" && label != "SingleElectron" && label != "Jet") {
   TH1F * hist_1dEtaPath = ME_1dEtaPath->getTH1F();
   TH1F * hist_1dPhiPath = ME_1dPhiPath->getTH1F();
 
-  hist_2d->SetMinimum(0);
-  hist_1dEta->SetMinimum(0);
-  hist_1dPhi->SetMinimum(0);
-  hist_1dEtaPath->SetMinimum(0);
-  hist_1dPhiPath->SetMinimum(0);
-  
   if (debugPrint) std::cout << "TH2F *" << std::endl;
 
  int i=2;
