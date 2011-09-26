075df022af2d262b9c52700f6e797c8ce7c390d0
Matevz Tadel <sha1-3c3651c44304837c4421d46b5b430a5900d92bbd@cern.ch>
2011-09-26 23:17:23 +0000

    In getEveShape() put det-id in the name of TEveGeoShape.

--- Fireworks/Core/src/FWGeometry.cc
+++ Fireworks/Core/src/FWGeometry.cc
@@ -265,7 +265,7 @@ FWGeometry::getEveShape( unsigned int id  ) const
 			   info.translation[0], info.translation[1], info.translation[2], 1.
       };
       TEveGeoManagerHolder gmgr( TEveGeoShape::GetGeoMangeur());
-      TEveGeoShape* shape = new TEveGeoShape;
+      TEveGeoShape* shape = new TEveGeoShape(TString::Format("RecoGeom Id=%u", id));
       TGeoShape* geoShape = getShape( info );
       shape->SetShape( geoShape );
       // Set transformation matrix from a column-major array
