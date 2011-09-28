081bd1affbca445f38bb56ddbfe089bb4f2b2533
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2011-09-28 15:04:48 +0000

    Produce TGeo version of Reco geometry.

---
+++ Fireworks/Geometry/python/dumpFWTGeoRecoGeometry_cfg.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DUMP")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['mc']
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+process.add_(cms.ESProducer("FWTGeoRecoGeometryESProducer"))
+
+#Adding Timing service:
+process.Timing = cms.Service("Timing")
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+    )
+
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+    )
+process.dump = cms.EDAnalyzer("DumpFWTGeoRecoGeometry",
+                              level = cms.untracked.int32(1)
+                              )
+
+process.p = cms.Path(process.dump)
