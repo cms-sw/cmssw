d2b8180aee438631d41ed94ac05ab5588f19c7d9
Stephen James Sanders <sha1-416c80580f6ba2ea6c936b800fa533612aecc26c@cern.ch>
2011-09-29 15:43:07 +0000

    remove no longer used code elements related to sub-event analysis

--- RecoHI/HiEvtPlaneAlgos/src/EvtPlaneProducer.cc
+++ RecoHI/HiEvtPlaneAlgos/src/EvtPlaneProducer.cc
@@ -13,7 +13,7 @@ Implementation:
 //
 // Original Author:  Sergey Petrushanko
 //         Created:  Fri Jul 11 10:05:00 2008
-// $Id: EvtPlaneProducer.cc,v 1.9 2011/09/20 18:07:31 ssanders Exp $
+// $Id: EvtPlaneProducer.cc,v 1.11 2011/09/24 14:02:09 ssanders Exp $
 //
 //
 #define TRACKCOLLECTION 1
@@ -61,15 +61,6 @@ Implementation:
 
 #include <cstdlib>
 #include "TROOT.h"
-//#include "TFile.h"
-#include "TH1.h"
-#include "TH2D.h"
-#include "TH2F.h"
-#include "TTree.h"
-#include "TH1I.h"
-#include "TF1.h"
-#include "TRandom3.h"
-#include "TRandom3.h"
 
 using std::rand;
 
@@ -98,16 +89,7 @@ private:
       etamax2=etamaxval2;
       sumsin=0;
       sumcos=0;
-      sumsin_sub1 = 0;
-      sumsin_sub2 = 0;
-      sumcos_sub1 = 0;
-      sumcos_sub2 = 0;
       order = (double) orderval;
-      ran = new TRandom3();
-      ranSubEvnt = false;
-      etaSubEvnt = true;
-      etaGap = 0.4;
-      nB = 4;
     }
     ~GenPlane(){;}
     void addParticle(double w, double s, double c, double eta) {
@@ -120,101 +102,24 @@ private:
 	//For calorimetors, w is an energy that needs to be subsequently 
 	//converted if an  multiplicity if needed
 	mult+=w;
-	if(ranSubEvnt) {
-	  if(ran->Uniform(0.,1.) < 0.5) {
-	    sumsin_sub1+=w*s;
-	    sumcos_sub1+=w*c;
-	    mult_sub1+=w;
-	  } else {
-	    sumsin_sub2+=w*s;
-	    sumcos_sub2+=w*c;
-	    mult_sub2+=w;
-	  }
-	}
-	if(etaSubEvnt) {
-	  int isub = FindSubEvent(eta);
-	  if(isub == 0) {
-	    sumsin_sub1+=w*s;
-	    sumcos_sub1+=w*c;
-	    mult_sub1+=w;
-	  } else if (isub == 1) {
-	    sumsin_sub2+=w*s;
-	    sumcos_sub2+=w*c;
-	    mult_sub2+=w;
-	  }
-	}
       }
-    }
-    Int_t FindSubEvent(Double_t eta) {
-      Double_t shift = 0;
-      Double_t DellRange = etamax2-etamin2+etamax1-etamin1;
-      Double_t DellBin = DellRange/nB; 
-      Double_t reta = eta-etamin1;
-      if (etamax2>etamin2 && eta> etamax1) {
-	if(eta>etamax1 && eta<etamin2) return -1;
-	shift = etamin2-etamax1;
-	reta-=shift;
-      }
-      if(reta < 0 || reta>DellRange) return -1;
-      Int_t b = (Int_t ) (reta/DellBin);
-      if(reta-b*DellBin>DellBin-etaGap/2. && 
-	 b!=nB-1 && etamax1-eta>etaGap/2.) {
-	return -1;
-      }
-      if(reta-b*DellBin<etaGap/2. && b!=0) {
-	if(etamax2!=etamin2 && eta-etamin2>etaGap/2.) {
-	  return -1;
-	}
-      }
-      double db = b;
-      int iretval = -1;
-      if(etamin2 == etamax2 || eta<etamax1 || 
-	 nB==2 ||(Int_t) (fmod(db/2.,2.e0)+0.1) ==1) {
-	iretval =  (Int_t) (fmod(db,2.e0)+0.1);
-      } else {
-	iretval = (Int_t) (fmod(db+1.,2.e0)+0.1);
-      }
-      return iretval;
     }
     
-    double getAngle(int sub, double &ang, double &sv, double &cv, double &Q, int &retmult){
+    double getAngle(double &ang, double &sv, double &cv, double &Q, int &retmult){
       ang = -10;
       sv = 0;
       cv = 0;
-      Q = 0;
       retmult = 0;
-      if(sub == 0) {
-	sv = sumsin;
-	cv = sumcos;
-	retmult = mult;
-      } else if (sub == 1) {
-	sv = sumsin_sub1;
-	cv = sumcos_sub1;
-	retmult = mult_sub1;
-      } else if (sub == 2) {
-	sv = sumsin_sub2;
-	cv = sumcos_sub2;
-	retmult = mult_sub2;
-      }
-
-      Q = sv*sv+cv*cv;
-      if(Q>0) {
-	ang = atan2(sv,cv)/order;
-	Q = sqrt(Q);
-      }
+      sv = sumsin;
+      cv = sumcos;
+      retmult = mult;
  
       return ang;
     }
     void reset() {
       sumsin=0;
       sumcos=0;
-      sumsin_sub1 = 0;
-      sumsin_sub2 = 0;
-      sumcos_sub1 = 0;
-      sumcos_sub2 = 0;
       mult = 0;
-      mult_sub1 = 0;
-      mult_sub2 = 0;
     }
   private:
     string epname;
@@ -225,19 +130,8 @@ private:
     double etamax2;
     double sumsin;
     double sumcos;
-    double sumsin_sub1;
-    double sumsin_sub2;
-    double sumcos_sub1;
-    double sumcos_sub2;
     int mult;
-    int mult_sub1;
-    int mult_sub2;
-    TRandom3 * ran;
     double order;
-    bool etaSubEvnt;
-    bool ranSubEvnt;
-    double etaGap;
-    int nB;
   };
   
 
@@ -263,9 +157,6 @@ private:
   double biggap_;
   double dzerr_;
   double chi2_;
-  TRandom3 * ran;
-  TH1D * etSpec;
-  TH1D * ptSpec;
 };
 
 //
@@ -283,14 +174,9 @@ private:
 EvtPlaneProducer::EvtPlaneProducer(const edm::ParameterSet& iConfig)
 {
   
-  int k=100;
-  int iseed=k;
   
-  srand(iseed);
-  ran = new TRandom3();
   //register your products
   
-  genSubEvt_ = iConfig.getUntrackedParameter<bool>("genSubEvt",false);
   useECAL_ = iConfig.getUntrackedParameter<bool>("useECAL",true);
   useHCAL_ = iConfig.getUntrackedParameter<bool>("useHCAL",true);
   useTrack_ = iConfig.getUntrackedParameter<bool>("useTrack",true);
@@ -301,58 +187,13 @@ EvtPlaneProducer::EvtPlaneProducer(const edm::ParameterSet& iConfig)
   maxpt_ = iConfig.getUntrackedParameter<double>("maxpt_",2.5);
   minvtx_ = iConfig.getUntrackedParameter<double>("minvtx_",-50.);
   maxvtx_ = iConfig.getUntrackedParameter<double>("maxvtx_",50.);
-  biggap_ = iConfig.getUntrackedParameter<double>("biggap_",0.3);
   dzerr_ = iConfig.getUntrackedParameter<double>("dzerr_",14.);
   chi2_  = iConfig.getUntrackedParameter<double>("chi2_",80.);
 
-  //TFileDirectory subdir = fs->mkdir("EvtPlaneProducer");
-  //etSpec = subdir.make<TH1D>("etSpec","etSpec",840,-20,400);
-  //ptSpec = subdir.make<TH1D>("ptSpec","ptSpec",200,0,50);
-  //cout<<"////////////////////////  EvtPlaneProducer /////////////////"<<endl;
-  //if(genSubEvt_) 
-//     cout<<"genSubEvt_ true"<<endl;
-//   else
-//     cout<<"genSubEvt_ false"<<endl;
-//   if(useECAL_) 
-//     cout<<"useECAL_ true"<<endl;
-//   else
-//     cout<<"useECAL_ false"<<endl;
-//   if(useHCAL_) 
-//     cout<<"useHCAL_ true"<<endl;
-//   else
-//     cout<<"useHCAL_ false"<<endl;
-//   if(useTrack_) 
-//     cout<<"useTrack_ true"<<endl;
-//   else
-//     cout<<"useTrack_ false"<<endl;
-//   if(useTrackPtWeight_) 
-//     cout<<"useTrackPtWeight_ true"<<endl;
-//   else
-//     cout<<"useTrackPtWeight_ false"<<endl;
-
 
   produces<reco::EvtPlaneCollection>("recoLevel");
-  Double_t g1low = 0;
-  Double_t g1high = 0;
-  Double_t g2low = 0;
-  Double_t g2high = 0;
-  if(biggap_>0) {
-    Double_t ernge = (4.-2.*biggap_)/3.;
-    g1low = -2. + ernge;
-    g1high = g1low+biggap_;
-    g2low = g1high+ernge;
-    g2high = 2.-ernge;
-  }
   for(int i = 0; i<NumEPNames; i++ ) {
-    //if(i==EPTracksPosEtaBigGap&&biggap_>0){
-      //rp[i] = new GenPlane(EPNames[i],g2high,2.,0,0,EPOrder[i]);
-    //} else if (i==EPTracksNegEtaBigGap&&biggap_>0) {
-    //  rp[i] = new GenPlane(EPNames[i],-2.,g1low,0,0,EPOrder[i]);
-    //} else if (i==EvtPlaneFromTracksMidEta&&biggap_>0) {
-    //  rp[i] = new GenPlane(EPNames[i],g1high,g2low,0,0,EPOrder[i]);
-    //} else {
       rp[i] = new GenPlane(EPNames[i],EPEtaMin1[i],EPEtaMax1[i],EPEtaMin2[i],EPEtaMax2[i],EPOrder[i]);
-      //}
   }
 }
 
@@ -431,7 +272,6 @@ EvtPlaneProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 	
 	if(tower_energyet<minet_) continue;
 	if(tower_energyet>maxet_) continue;;
-	//etSpec->Fill(tower_energyet);
 
 	rp[etEcal    ]->addParticle (tower_energyet_e, s1,    s2,    tower_eta);
 	rp[etEcalP   ]->addParticle (tower_energyet_e, s1,    s2,    tower_eta);
@@ -557,12 +397,6 @@ EvtPlaneProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 	  }
 	  if(track_pt<minpt_) continue;
 	  if(track_pt>maxpt_) continue;
-	  //ptSpec->Fill(track_pt);
-	  //	  if(track_charge > 0 ){
-	  //	    rp[EvtPlaneFromTracksPosCh]->addParticle(w,s,c,track_eta);
-	  //	  } else if(track_charge < 0){
-	  //   rp[EvtPlaneFromTracksNegCh]->addParticle(w,s,c,track_eta);
-	  // }
 	  rp[EvtPlaneFromTracksMidEta]->addParticle(w,s,c,track_eta);
 	  rp[EvtPTracksPosEtaGap]->addParticle(w,s,c,track_eta);
 	  rp[EvtPTracksNegEtaGap]->addParticle(w,s,c,track_eta);
@@ -597,14 +431,8 @@ EvtPlaneProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   int mult = 0;
 
   for(int i = 0; i<NumEPNames; i++) {
-    rp[i]->getAngle(0,ang,sv,cv,Q,mult);
+    rp[i]->getAngle(ang,sv,cv,Q,mult);
     ep[i] = new EvtPlane(ang,sv,cv,EPNames[i].data());
-    if(genSubEvt_) {
-      rp[i]->getAngle(1,ang,sv,cv,Q,mult);
-      ep1[i] = new EvtPlane(ang, sv, cv, Form("%s_sub1",EPNames[i].data()));
-      rp[i]->getAngle(2,ang,sv,cv,Q,mult);
-      ep2[i] = new EvtPlane(ang, sv, cv, Form("%s_sub2",EPNames[i].data()));
-    }
   }
   if(useTrack_) {
     for(int i = 0; i<9; i++) {
