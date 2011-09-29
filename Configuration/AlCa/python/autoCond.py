0425fefdc193e5af21f914edd03353f9991f4832
Stephanie Beauceron <sha1-c117a3d4e0f055ce820584107fcb2695f540f4e1@cern.ch>
2011-09-29 13:38:31 +0000

    Update GlobalTag

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_50_V0::All',
-             'startup' : 'START50_V0::All',
+autoCond = { 'mc'      : 'MC_50_V1::All',
+             'startup' : 'START50_V1::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_44_V4::All',
-             'hltonline' : 'GR_R_44_V4::All', # same as com10 until H tag available
+             'com10'   : 'GR_R_44_V6::All',
+             'hltonline' : 'GR_R_44_V6::All', # same as com10 until H tag available
              'starthi' : 'STARTHI44_V4::All'
              }
