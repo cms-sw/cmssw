dac1207c8b4be749fe649ae8946a52f4b2c38910
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2011-09-30 18:08:37 +0000

    clean up variables

--- RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatcalib_cfi.py
+++ RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatcalib_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 hiEvtPlaneFlatCalib = cms.EDAnalyzer('HiEvtPlaneFlatCalib',
-                                genFlatPsi_ = cms.untracked.bool(True),
-                                genSubEvt_  = cms.untracked.bool(False)
-)
+                                     genFlatPsi_ = cms.untracked.bool(True)
+                                     )
--- RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatproducer_cfi.py
+++ RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatproducer_cfi.py
@@ -3,7 +3,5 @@ import FWCore.ParameterSet.Config as cms
 hiEvtPlaneFlat = cms.EDProducer('HiEvtPlaneFlatProducer',
                                 vtxCollection_=cms.InputTag("hiSelectedVertex"),
                                 inputPlanes_ = cms.InputTag("hiEvtPlane","recoLevel"),
-                                centrality_  = cms.InputTag("centralityBin"),
-                                genFlatPsi_ = cms.untracked.bool(True),
-                                genSubEvt_  = cms.untracked.bool(False)
+                                centrality_  = cms.InputTag("centralityBin")
                                 )
