ad1dbbd8e652d4e152cd83e192aa498ae8b11db4
Mantas Stankevicius <sha1-f5d6afd40b8ebb24f8d4024232016bd32d210fa8@cern.ch>
2011-10-07 11:37:44 +0000

    clean up before launching scripts

--- Documentation/ReferenceManualScripts/doxygen/script_launcher.sh
+++ Documentation/ReferenceManualScripts/doxygen/script_launcher.sh
@@ -3,7 +3,25 @@
 set CMSSW_xyz = $1	# CMSSW version
 set SCRIPTS = $2	# full path to utils folder i.e. /data/refman/CMSSW_4_3_0/src/Documentation/ReferenceManualScripts/doxygen/utils
 set LOCALTOP = $3	# full path to buildarea i.e. /data/refman/CMSSW_4_3_0/
-set ASSOCIATION = $SCRIPTS/Association-V6.txt
+
+# CLEANUP
+if (-e cfgfile.conf ) then
+	rm -f cfgfile.conf
+endif
+
+if (-e $LOCALTOP/doc/html/splittedTree ) then
+	rm -Rf $LOCALTOP/doc/html/splittedTree
+endif
+
+if (-e $LOCALTOP/$CMSSW_xyz.index) then
+	rm -f $LOCALTOP/$CMSSW_xyz.index
+endif
+
+if (-e $LOCALTOP/doc/html/ReferenceManual.html) then
+	rm $LOCALTOP/doc/html/ReferenceManual.html
+endif
+
+# END CLEANUP
 
 # Generate configfiles
 time python $SCRIPTS/configfiles/configfiles.py $LOCALTOP
@@ -12,6 +30,7 @@ time python $SCRIPTS/configfiles/configfiles.py $LOCALTOP
 time python $SCRIPTS/linker/linker.py $LOCALTOP
 
 #Generationg tree view
+
 mkdir $LOCALTOP/doc/html/splittedTree
 cp -R $SCRIPTS/jquery/ $LOCALTOP/doc/html/splittedTree
 
@@ -24,9 +43,5 @@ cp $SCRIPTS/other/ReferenceManual.html $LOCALTOP/doc/html
 time python $SCRIPTS/splitter/splitter.py $LOCALTOP /doc/html/namespaces.html namespaceList_ 
 time python $SCRIPTS/splitter/splitter.py $LOCALTOP /doc/html/configfiles.html configfilesList_ 
 time python $SCRIPTS/splitter/splitter.py $LOCALTOP /doc/html/annotated.html annotatedList_ 
-
-if (-e $LOCALTOP/$CMSSW_xyz.index) then
-	rm $LOCALTOP/$CMSSW_xyz.index
-endif
 
 find $LOCALTOP/doc/html/ -name "*.html" ! \( -name "*dir_*" -o -name "*globals_*" -o -name "*namespacemembers_*" -o -name "*functions_*" \) -print | sort > $LOCALTOP/$CMSSW_xyz.index
