bd8bf63fee071ded2d94fa9df7f806c174cdae1b
Julie Malcles <sha1-2597814f27f9a62af47364ec73303bc025115982@cern.ch>
2011-10-07 15:00:21 +0000

    updates

--- CalibCalorimetry/EcalLaserAnalyzer/test/MusEcal/generatePrim.pl
+++ CalibCalorimetry/EcalLaserAnalyzer/test/MusEcal/generatePrim.pl
@@ -5,6 +5,10 @@ use Term::ANSIColor;
 use Cwd;
 
 $cfgFile     = @ARGV[0];
+$ecalPart    = @ARGV[1];
+
+print " config: $cfgFile \n";
+print " ecalPart: $ecalPart \n";
 
 do "/nfshome0/ecallaser/config/readconfig.pl";
 readconfig(${cfgFile});
@@ -12,19 +16,26 @@ readconfig(${cfgFile});
 ${MON_MUSECAL_DIR}=~ s/\s+//;
 ${MON_OUTPUT_DIR}=~ s/\s+//;
 ${LMF_LASER_PERIOD}=~ s/\s+//;
+
+print "MON_MUSECAL_DIR  : ${MON_MUSECAL_DIR} \n";
+print "MON_OUTPUT_DIR: ${MON_OUTPUT_DIR} \n";
+print "LMF_LASER_PERIOD: ${LMF_LASER_PERIOD} \n";
+
 while( 1 ) 
 {   
     
     generate();
-    sleep 60;
+    sleep 10;
 }
 
 
 sub generate{
     
-    my $command1 = "${MON_MUSECAL_DIR}/generatePrim.sh ${cfgFile} > ${MON_OUTPUT_DIR}/${LMF_LASER_PERIOD}/log/generatePrimSh.log";
+    my $command1 = "${MON_MUSECAL_DIR}/generatePrim.sh ${cfgFile} ${ecalPart} >> ${MON_OUTPUT_DIR}/${LMF_LASER_PERIOD}/log/generatePrimSh$ecalPart.log";
     system $command1;	
     
+#    my $command2 = "${MON_MUSECAL_DIR}/generateCLS.sh ${cfgFile} >> ${MON_OUTPUT_DIR}/${LMF_LASER_PERIOD}/log/generateCLSSh.log";
+#    system $command2;	
 }
 
 exit;
