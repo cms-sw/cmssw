9220ae44875add44d4f925b9ba993da8ff7e0ccc
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2011-10-08 19:44:11 +0000

    Use correct input tags for RECO/AOD input, just as in CommonTools/ParticleFlow/python/PF2PAT_cff.py.

--- PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from CommonTools.ParticleFlow.ParticleSelectors.pfSortByType_cff import *
 from CommonTools.ParticleFlow.pfNoPileUp_cff import *
 
+pfPileUp.PFCandidates       = 'particleFlow'
+pfNoPileUp.bottomCollection = 'particleFlow'
 
 patPFCandidateIsoDepositSelection = cms.Sequence(
     pfNoPileUpSequence *
