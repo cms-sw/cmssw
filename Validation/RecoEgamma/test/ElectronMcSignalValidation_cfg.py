634ee2da3f4b51038fb4773635255f8e81bcfed3
David Chamont <sha1-cc2cbd4cf572176bc632dcdb55d943ce51d4e6af@cern.ch>
2011-10-10 07:57:12 +0000

    add some stuff for redoing data in startup conditions

--- Validation/RecoEgamma/test/ElectronMcSignalValidation_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcSignalValidation_cfg.py
@@ -27,15 +27,20 @@ process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond['mc']
+#from Configuration.AlCa.autoCond import autoCond
+#process.GlobalTag.globaltag = autoCond['mc']
+process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']+'::All'
+
+# FOR DATA REDONE FROM RAW, ONE MUST HIDE IsoFromDeps
+# CONFIGURATION
 
 process.load("Validation.RecoEgamma.electronIsoFromDeps_cff")
 process.load("Validation.RecoEgamma.ElectronMcSignalValidator_cfi")
 
-process.electronMcSignalValidator.OutputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
-#process.electronMcSignalValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
+#process.electronMcSignalValidator.OutputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
+process.electronMcSignalValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
 
+#process.p = cms.Path(process.electronMcSignalValidator*process.dqmStoreStats)
 process.p = cms.Path(process.electronIsoFromDeps*process.electronMcSignalValidator*process.dqmStoreStats)
 
 
