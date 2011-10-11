0a82b623fb0c949e01cc526618ecbb2d2e13161f
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2011-10-11 19:57:32 +0000

    include ttFullLepEvtBuilder_cfg.py again

--- TopQuarkAnalysis/TopEventProducers/test/runtests.sh
+++ TopQuarkAnalysis/TopEventProducers/test/runtests.sh
@@ -12,8 +12,7 @@ cmsRun ${LOCAL_TEST_DIR}/ttDecaySelection_cfg.py || die 'Failure using ttDecaySe
 
 cmsRun ${LOCAL_TEST_DIR}/ttFullHadEvtBuilder_cfg.py || die 'Failure using ttFullHadEvtBuilder_cfg.py' $?
 
-## commented until the reasons for remaining crashes are resolved
-#cmsRun ${LOCAL_TEST_DIR}/ttFullLepEvtBuilder_cfg.py || die 'Failure using ttFullLepEvtBuilder_cfg.py' $?
+cmsRun ${LOCAL_TEST_DIR}/ttFullLepEvtBuilder_cfg.py || die 'Failure using ttFullLepEvtBuilder_cfg.py' $?
 
 cmsRun ${LOCAL_TEST_DIR}/ttSemiLepEvtBuilder_cfg.py || die 'Failure using ttSemiLepEvtBuilder_cfg.py' $?
 
