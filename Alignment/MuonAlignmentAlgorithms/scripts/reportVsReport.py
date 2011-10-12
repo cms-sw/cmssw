1c79f389e4e8465abcc9420747605ffa67bd46e3
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2011-10-12 23:33:47 +0000

    added options

--- Alignment/MuonAlignmentAlgorithms/scripts/reportVsReport.py
+++ Alignment/MuonAlignmentAlgorithms/scripts/reportVsReport.py
@@ -1,49 +1,100 @@
 #! /usr/bin/env python
 
-import sys
+import sys, optparse
 
-execfile("plotscripts.py")
+copyargs = sys.argv[:]
 
-#ROOT.gROOT.SetBatch(1)
+for i in range(len(copyargs)):
+  if copyargs[i] == "":             copyargs[i] = "\"\""
+  if copyargs[i].find(" ") != -1:   copyargs[i] = "\"%s\"" % copyargs[i]
+commandline = " ".join(copyargs)
+print commandline
 
-cargs = sys.argv[:]
+prog = sys.argv[0]
 
-if len(cargs) != 5 :
-  print "Draws delta corrections from report1 (x axis) vs. from report2 (y axis)."
-  print "usage: ./reportVsReport.py label selector report1.py report2.py"
-  print "where selector is one of ALL, DT, CSC, CSCE1, CSCE2"
-  print "The label will be included into the filename as reportVsReport_label_selector.png"
-  sys.exit()
+usage = """Usage:
+%(prog)s [options] reportX.py reportY.py
 
+Draws a scatterplot of delta corrections from reportX vs. from reportY.
+""" % vars()
+
+parser = optparse.OptionParser(usage)
+parser.add_option("-o", "--output",
+                  help="plots' file name. If not give, an automatic file name would be given as reportVsReport_label_selection.png",
+                  type="string",
+                  default="",
+                  dest="filename")
+parser.add_option("-l", "--label",
+                  help="label for an automatic filename",
+                  type="string",
+                  default="",
+                  dest="label")
+parser.add_option("-s", "--selection",
+                   help="is one of the following: ALL, DT, CSC, CSCE1, CSCE2",
+                   type="string",
+                   default="ALL",
+                   dest="selection")
+parser.add_option("-x", "--xlabel",
+                   help="prefix to add to plots' X axis",
+                   type="string",
+                   default="None",
+                   dest="xlabel")
+parser.add_option("-y", "--ylabel",
+                   help="prefix to add to plots' Y axis",
+                   type="string",
+                   default="None",
+                   dest="ylabel")
+parser.add_option("-w", "--which",
+                   help="binary mask for which variables to draw, defauls is 110011",
+                   type="string",
+                   default="110011",
+                   dest="which")
+                   
+options, args = parser.parse_args(sys.argv[1:])
+
+if len(args)!=2:   print usage; sys.exit()
+
+
+### definitions of selectors:
 
 def DT(dt, wheel, station, sector): return dt == "DT"
 
-def CSC(csc, endcap, station, ring, chamber): return csc == "CSC"
+def CSC(csc, endcap, station, ring, chamber): 
+  if csc != "CSC": return False
+  # skip the duplicated ME1/a
+  if station==1 and ring==4: return False
+  # skip non-instrumented ME4/2's:
+  if station==4 and ring==2 and ( (endcap==1 and (chamber<9 or chamber >13)) or endcap==2 ) : return False
+  return True
 
-def CSCE1(csc, endcap, station, ring, chamber): return csc == "CSC" and endcap==1
+def CSCE1(csc, endcap, station, ring, chamber): return CSC(csc, endcap, station, ring, chamber) and endcap==1
 
-def CSCE2(csc, endcap, station, ring, chamber): return csc == "CSC" and endcap==2
+def CSCE2(csc, endcap, station, ring, chamber): return CSC(csc, endcap, station, ring, chamber) and endcap==2
 
 
-label = cargs[1]
+### main part
 
-selection = cargs[2]
+execfile("plotscripts.py")
+
+ROOT.gROOT.SetBatch(1)
+
+selection = options.selection
 if selection == 'ALL': selection = None
 
-reportfile1 = cargs[3]
-reportfile2 = cargs[4]
-execfile(reportfile1)
-r1 = reports
-execfile(reportfile2)
-r2 = reports
+execfile(args[0])
+rx = reports
+execfile(args[1])
+ry = reports
 
-c1 = ROOT.TCanvas("c1","c1",760,800)
+if options.which.count('1')>4: c1 = ROOT.TCanvas("c1","c1",1000,800)
+else: c1 = ROOT.TCanvas("c1","c1",760,800)
 
-ranges = "window=25"
-#if selection=="DT": ranges = "windows=[25.,5,1.,1.,5.,5.]"
-
-eval("reportsDiff(r1, r2, %s, selection=%s, bins=2001, canvas=c1)" % (ranges, selection))
-print "reportsDiff(r1, r2, %s, selection=%s, bins=2001)" % (ranges, selection)
+print "corrections2D(reportsX=rx, reportsY=ry, selection=%s, pre_title_x='%s', pre_title_y='%s', which='%s' )" % ( 
+      selection, options.xlabel, options.ylabel, options.which )
+eval( "corrections2D(reportsX=rx, reportsY=ry, selection=%s, pre_title_x='%s', pre_title_y='%s', which='%s', canvas=c1 )" % ( 
+      selection, options.xlabel, options.ylabel, options.which) )
 
 c1.Update()
-c1.Print("reportVsReport_%s_%s.png" % (label, cargs[2]))
+if len(options.filename)>0: filename = options.filename
+else: filename = "reportVsReport_"+options.label+"_"+options.selection+".png"
+c1.Print(filename)
