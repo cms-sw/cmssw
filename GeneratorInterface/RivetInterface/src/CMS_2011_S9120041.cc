d6464897cb2298540fda4b709a502a7a64096b63
Albert Knutsson <sha1-d7d4c6d578df489ec46a88d0d29628b8121ea6d4@cern.ch>
2011-10-13 14:38:03 +0000

    CMS_QCD_10_010 renamed to CMS_2011_S9120041. cc, plot, info, aida updated for this analysis.

--- GeneratorInterface/RivetInterface/src/CMS_2011_S9120041.cc
+++ GeneratorInterface/RivetInterface/src/CMS_2011_S9120041.cc
@@ -1,7 +1,5 @@
 // -*- C++ -*-
 
-// Author: Mohammed Zakaria (mzakaria@ufl.edu)
-
 #include "Rivet/Analysis.hh"
 #include "Rivet/RivetAIDA.hh"
 #include "Rivet/Tools/Logging.hh"
@@ -11,34 +9,24 @@
 #include "Rivet/Projections/Beam.hh"
 
 using namespace std;
-int  _j = 0 ;
-int _jj = 0 , _jjj = 0;
-
-double _nch_tot_7TeV_pT3 = 0;
-double _nch_tot_7TeV_pT20 = 0;
-double _nch_tot_09TeV_pT3 = 0;
 
 namespace Rivet {
 
-  //RIVET for the UE for p-p collisions at 7TeV and 0.9TeV. 
-
-  class CMS_QCD_10_010 : public Analysis {
+  class CMS_2011_S9120041 : public Analysis {
     public:
 
 
       /// Constructor
-      CMS_QCD_10_010()
-        : Analysis("CMS_QCD_10_010")
+      CMS_2011_S9120041()
+        : Analysis("CMS_2011_S9120041")
       {
         setNeedsCrossSection(false);
       }
 
     public:
 
-      /// Book histograms and initialise projections before the run
       void init() {
 
-        /// Initialise and register projections
         const ChargedFinalState cfs(-2.0, 2.0, 500*MeV);
         addProjection(cfs, "CFS");
 
@@ -49,8 +37,6 @@ namespace Rivet {
         const FastJets jetpro(cfsforjet, FastJets::SISCONE, 0.5);    
         addProjection(jetpro, "Jets");
         addProjection(Beam(), "Beam");
-
-        /// Book histograms rivet can read it directly from aida file (of Data). 
 
         _hist_profile_Nch_pT_7TeV = bookProfile1D(1, 1, 1); //Profile plot for No. of Charged particles vs. pT max for 7 TeV.
         _hist_profile_SumpT_pT_7TeV = bookProfile1D(2, 1, 1);   //Profile plot Trans. Momentum sum vs. pT max for 7 TeV.
@@ -65,6 +51,14 @@ namespace Rivet {
         _hist_dist_Nch_09TeV_pT3 = bookHistogram1D(11, 1, 1); // No. of trans. charged particles Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
         _hist_dist_Sum_09TeV_pT3 = bookHistogram1D(12 , 1, 1); // No. of trans. momentum sum Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
         _hist_dist_pT_09TeV_pT3 = bookHistogram1D(13, 1, 1); // Trans. momentum Distribution for sqrt(s) = 0.9TeV, pT max > 3GeV.
+
+	_j = 0;
+	_jj = 0; 
+	_jjj = 0;
+
+	_nch_tot_7TeV_pT3 = 0;
+	_nch_tot_7TeV_pT20 = 0;
+	_nch_tot_09TeV_pT3 = 0;
       }
 
 
@@ -75,7 +69,7 @@ namespace Rivet {
 
         Jets jets = applyProjection<FastJets>(event, "Jets").jetsByPt(1.0*GeV); // we require our jets to have pT > 1 GeV.
         if (jets.size() < 1) vetoEvent;
-
+	
         FourMomentum _p_lead = jets[0].momentum();
         const double _philead = _p_lead.phi();
         const double _pTlead  = _p_lead.perp();
@@ -84,7 +78,6 @@ namespace Rivet {
           applyProjection<ChargedFinalState>(event, "CFS").particlesByPt();
 
         double _nTransverse(0.0), _ptSumTransverse(0.0), _pT, _nch_average = 0 ;
-        //		int  _i = 0;
         foreach (const Particle& p, particles) {
 
           double _dphi = fabs(deltaPhi(_philead, p.momentum().phi()));
@@ -121,8 +114,7 @@ namespace Rivet {
             _hist_dist_Nch_7TeV_pT3 -> fill(_nTransverse, weight);
             _nch_average = _nch_average + _nTransverse;
             _nch_tot_7TeV_pT3 = _nch_tot_7TeV_pT3 + _nTransverse;
-            // _i = _i + 1;
-            _j += 1 ;
+            _j += weight ;
             _hist_dist_Sum_7TeV_pT3 -> fill(_ptSumTransverse / GeV, weight);
           }
           if(_pTlead > 20*GeV)
@@ -130,7 +122,7 @@ namespace Rivet {
 
             _hist_dist_Nch_7TeV_pT20 -> fill(_nTransverse, weight);
             _nch_tot_7TeV_pT20 = _nch_tot_7TeV_pT20 + _nTransverse;
-            _jj += 1 ;
+            _jj += weight;
 
             _hist_dist_Sum_7TeV_pT20 -> fill(_ptSumTransverse / GeV, weight);
           } 
@@ -145,7 +137,7 @@ namespace Rivet {
           {
             _hist_dist_Nch_09TeV_pT3 -> fill(_nTransverse, weight);
             _nch_tot_09TeV_pT3 = _nch_tot_09TeV_pT3 + _nTransverse;
-            _jjj += 1;  //counts how many events 
+            _jjj += weight;  //counts how many events 
 
             _hist_dist_Sum_09TeV_pT3 -> fill(_ptSumTransverse/GeV, weight);
           }
@@ -174,12 +166,19 @@ namespace Rivet {
         scale(_hist_dist_pT_7TeV_pT3, _nch_avg_7TeV_pT3 / integral(_hist_dist_pT_7TeV_pT3) ); // normalizes Nch Distribution so that area = avg Nch
         scale(_hist_dist_pT_09TeV_pT3, _nch_avg_09TeV_pT3 / integral(_hist_dist_pT_09TeV_pT3) ); // normalizes SumpT distribtuion so that area = avg Nch
 
-
       }
 
 
 
     private:
+
+      int  _j;
+      int _jj; 
+      int _jjj;
+      
+      double _nch_tot_7TeV_pT3;
+      double _nch_tot_7TeV_pT20;
+      double _nch_tot_09TeV_pT3;
 
       AIDA::IProfile1D * _hist_profile_Nch_pT_7TeV;
       AIDA::IProfile1D * _hist_profile_SumpT_pT_7TeV;
@@ -196,26 +195,12 @@ namespace Rivet {
       AIDA::IHistogram1D * _hist_dist_pT_09TeV_pT3;
       //@}
 
-    public:
-      string experiment()         const { return "CMS"; }
-      string year()               const { return "2011"; }
-      string spiresId()           const { return "None"; }
-      string collider()           const { return "LHC"; }
-      string summary()            const { return "None"; }
-      string description()        const { return "None"; }
-      string runInfo()            const { return "None"; }
-      string status()             const { return "UNVALIDATED"; }
-      vector<string> authors()    const { return vector<string>(); }
-      vector<string> references() const { return vector<string>(); }
-      vector<std::string> todos() const { return vector<string>(); }
-
-
   };
 
 
 
   // This global object acts as a hook for the plugin system
-   AnalysisBuilder<CMS_QCD_10_010> plugin_CMS_QCD_10_010;
+   AnalysisBuilder<CMS_2011_S9120041> plugin_CMS_2011_S9120041;
 
 
 }
