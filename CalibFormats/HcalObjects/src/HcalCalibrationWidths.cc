4ebbfba74f64d00bb6f45bb46c2fc74048687fba
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-10-13 09:44:45 +0000

    Change backward loop to forward one to avoid warnings.

--- CalibFormats/HcalObjects/src/HcalCalibrationWidths.cc
+++ CalibFormats/HcalObjects/src/HcalCalibrationWidths.cc
@@ -1,16 +1,17 @@
 #include "CalibFormats/HcalObjects/interface/HcalCalibrationWidths.h"
+#include <cstdlib>
 
 /** \class HcalCalibrationWidths
     
     Container for retrieving uncertainties of calibration constants for HCAL
    $Author: ratnikov
-   $Date: 2005/08/02 01:31:24 $
-   $Revision: 1.1 $
+   $Date: 2005/10/04 18:05:54 $
+   $Revision: 1.2 $
 */
 
 HcalCalibrationWidths::HcalCalibrationWidths (const float fGain [4], const float fPedestal [4]) {
-  int iCap = 4;
-  while (--iCap >= 0) {
+  for (size_t iCap = 0; iCap < 4; ++iCap)
+  {
     mGain [iCap] = fGain [iCap];
     mPedestal [iCap] = fPedestal [iCap];
   }
--- CalibFormats/HcalObjects/src/HcalCalibrations.cc
+++ CalibFormats/HcalObjects/src/HcalCalibrations.cc
@@ -1,18 +1,18 @@
 #include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+#include <cstdlib>
 
 /** \class HcalCalibrations
     
     Container for retrieved calibration constants for HCAL
    $Author: ratnikov
-   $Date: 2009/05/06 22:22:44 $
-   $Revision: 1.5 $
+   $Date: 2009/05/19 16:06:08 $
+   $Revision: 1.6 $
 */
 
 HcalCalibrations::HcalCalibrations (const float fGain [4], const float fPedestal [4], 
 				    const float fRespCorr, const float fTimeCorr, 
 				    const float fLUTCorr ) {
-  int iCap = 4;
-  while (--iCap >= 0) {
+  for (size_t iCap = 0; iCap < 4; ++iCap) {
     mRespCorrGain [iCap] = fGain [iCap] * fRespCorr;
     mPedestal [iCap] = fPedestal [iCap];
   }
