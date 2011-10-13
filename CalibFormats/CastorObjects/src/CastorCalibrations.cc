5711801972bd964ec29cb127c24b59cb1835670b
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-10-13 09:37:52 +0000

    Use forward loop rarther than backward one.

--- CalibFormats/CastorObjects/src/CastorCalibrations.cc
+++ CalibFormats/CastorObjects/src/CastorCalibrations.cc
@@ -1,16 +1,17 @@
 #include "CalibFormats/CastorObjects/interface/CastorCalibrations.h"
+#include <cstdlib>
 
 /** \class CastorCalibrations
     
     Container for retrieved calibration constants for Castor
    $Author: katsas
-   $Date: 2008/02/15 15:31:59 $
+   $Date: 2008/03/04 10:01:27 $
    $Revision: 1.1 $
 */
 
 CastorCalibrations::CastorCalibrations (const float fGain [4], const float fPedestal [4]) {
-  int iCap = 4;
-  while (--iCap >= 0) {
+  for (size_t iCap = 0; iCap < 4; ++iCap)
+  {
     mGain [iCap] = fGain [iCap];
     mPedestal [iCap] = fPedestal [iCap];
   }
