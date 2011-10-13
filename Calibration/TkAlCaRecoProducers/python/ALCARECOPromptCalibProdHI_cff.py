38b0c20860a5f2143a570d7559bade9e61d8488f
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-10-13 15:51:37 +0000

    Fix the vertex selection and add the highPurity bit

--- Calibration/TkAlCaRecoProducers/python/ALCARECOPromptCalibProdHI_cff.py
+++ Calibration/TkAlCaRecoProducers/python/ALCARECOPromptCalibProdHI_cff.py
@@ -4,3 +4,5 @@ from Calibration.TkAlCaRecoProducers.ALCARECOPromptCalibProd_cff import *
 # adapt the names for the HI AlcaRecos
 ALCARECOTkAlMinBiasFilterForBS.HLTPaths = ['pathALCARECOTkAlMinBiasHI']
 alcaBeamSpotProducer.BeamFitter.TrackCollection = 'ALCARECOTkAlMinBiasHI'
+alcaBeamSpotProducer.BeamFitter.TrackQuality = cms.untracked.vstring('highPurity')
+alcaBeamSpotProducer.PVFitter.VertexCollection = 'hiSelectedVertex'
