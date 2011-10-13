9eb8ab924f8885fcbdf1a638f104f4ef28ff9e71
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2011-10-13 15:52:25 +0000

    Set the tolerance in TotalKinematicsFilter at 500 MeV, as suggested by S. Mrenna

--- GeneratorInterface/GenFilters/python/TotalKinematicsFilter_cfi.py
+++ GeneratorInterface/GenFilters/python/TotalKinematicsFilter_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 totalKinematicsFilter = cms.EDFilter('TotalKinematicsFilter',
   src             = cms.InputTag("genParticles"),
-  tolerance       = cms.double(0.1),
+  tolerance       = cms.double(0.5),
   verbose         = cms.untracked.bool(False)                                   
 )
