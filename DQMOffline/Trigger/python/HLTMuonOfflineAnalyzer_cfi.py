a20dadf4a576f92f075b1336d386915dbae1f17b
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2011-10-19 19:56:43 +0000

    Expanded regular expressions and revised binning

--- DQMOffline/Trigger/python/HLTMuonOfflineAnalyzer_cfi.py
+++ DQMOffline/Trigger/python/HLTMuonOfflineAnalyzer_cfi.py
@@ -10,7 +10,9 @@ hltMuonOfflineAnalyzer = cms.EDAnalyzer("HLTMuonOfflineAnalyzer",
 
     ## HLT paths passing any one of these regular expressions will be included
     hltPathsToCheck = cms.vstring(
-        "HLT_(HI)?(L[12])?(Single)?(Double)?(Iso)?Mu[0-9]*(Open)?(_NoVertex)?(_Core)?(_v[0-9]*)?$",
+        "HLT_.*Mu[^l]", 
+        "HLT_.*Jpsi", 
+                                            #"HLT_(HI)?(L[12])?(Single)?(Double)?(Iso)?Mu[0-9]*(Open)?(_NoVertex)?(_Core)?(_v[0-9]*)?$",
     ),
 
     ## All input tags are specified in this pset for convenience
@@ -28,11 +30,14 @@ hltMuonOfflineAnalyzer = cms.EDAnalyzer("HLTMuonOfflineAnalyzer",
         phi        = cms.untracked.vdouble( 20,  -3.14,   3.14),
         z0         = cms.untracked.vdouble( 10, -15.00,  15.00),
         d0         = cms.untracked.vdouble( 10,  -0.50,   0.50),
-        zMass      = cms.untracked.vdouble(100,  65.00, 115.00),
+        zMass      = cms.untracked.vdouble( 50,  65.00, 115.00),
+        jpsiMass   = cms.untracked.vdouble( 60,   0.00,   6.00),
         charge     = cms.untracked.vdouble(  2,  -2.00,   2.00),
-        resolution = cms.untracked.vdouble( 20,  -0.15,   0.15),
-        deltaR     = cms.untracked.vdouble( 20,   0.00,   0.10),
+        deltaR     = cms.untracked.vdouble( 20,   0.00,   0.05),
         phiCoarse  = cms.untracked.vdouble( 10,  -3.14,   3.14),
+        resolutionRel = cms.untracked.vdouble( 20,  -0.15,   0.15),
+        resolutionEta = cms.untracked.vdouble( 20,  -0.01,   0.01),
+        resolutionPhi = cms.untracked.vdouble( 20,  -0.01,   0.01),
         ## parameters for variable-width plots
         etaCoarse = cms.untracked.vdouble(-2.4, -2.1, -1.6, -1.2, -0.8, 0.0,
                                            0.8,  1.2,  1.6,  2.1,  2.4),
