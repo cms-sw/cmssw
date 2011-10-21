65cc9d74f44a41be9a36b1187cd96c7e41c937dc
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2011-10-21 09:50:50 +0000

    revert to the previous FastSim GlobalPixel seeds

--- FastSimulation/Tracking/python/GlobalPixelSeedProducerForElectrons_cff.py
+++ FastSimulation/Tracking/python/GlobalPixelSeedProducerForElectrons_cff.py
@@ -9,9 +9,8 @@ globalPixelSeedsForElectrons.secondHitSubDetectorNumber = [2]
 globalPixelSeedsForElectrons.secondHitSubDetectors = [1, 2]
 globalPixelSeedsForElectrons.seedingAlgo = ['GlobalPixel']
 globalPixelSeedsForElectrons.zVertexConstraint = cms.vdouble(0.5)
-globalPixelSeedsForElectrons.originRadius = cms.vdouble(0.2)
+globalPixelSeedsForElectrons.originRadius = cms.vdouble(0.02)
 globalPixelSeedsForElectrons.originpTMin = cms.vdouble(1.5)
-globalPixelSeedsForElectrons.originHalfLength = cms.vdouble(15.)
 
 globalPixelSeedsForPhotons = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.clone()
 globalPixelSeedsForPhotons.firstHitSubDetectorNumber = [2]
@@ -22,4 +21,3 @@ globalPixelSeedsForPhotons.seedingAlgo = ['GlobalPixel']
 globalPixelSeedsForPhotons.zVertexConstraint = cms.vdouble(-1)
 globalPixelSeedsForPhotons.originRadius = cms.vdouble(0.02)
 globalPixelSeedsForPhotons.originpTMin = cms.vdouble(1.5)
-globalPixelSeedsForPhotons.originHalfLength = cms.vdouble(15.)
