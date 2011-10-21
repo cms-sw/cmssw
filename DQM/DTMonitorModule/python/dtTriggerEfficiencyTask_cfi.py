1dcdce573dd431ed05f4e0ba8eb0d823dc058e59
Antonio Branca <sha1-7f8a4b1f618923258b126d0204553ccfd91737d2@cern.ch>
2011-10-21 18:17:46 +0000

    Improved best segment selection

--- DQM/DTMonitorModule/python/dtTriggerEfficiencyTask_cfi.py
+++ DQM/DTMonitorModule/python/dtTriggerEfficiencyTask_cfi.py
@@ -15,17 +15,11 @@ dtTriggerEfficiencyMonitor = cms.EDAnalyzer("DTTriggerEfficiencyTask",
     processDCC = cms.untracked.bool(True), # if true enables DCC data analysis
     minBXDDU = cms.untracked.int32(7),  # min BX for DDU eff computation
     maxBXDDU = cms.untracked.int32(15), # max BX for DDU eff computation
-<<<<<<< dtTriggerEfficiencyTask_cfi.py
 
     nMinHitsPhi = cms.untracked.int32(5),
     phiAccRange = cms.untracked.double(30.),
 
     detailedAnalysis = cms.untracked.bool(False), #if true enables detailed analysis plots
-=======
-    nMinHitsPhi = cms.untracked.int32(5),
-    phiAccRange = cms.untracked.double(30.),
-    detailedAnalysis = cms.untracked.bool(False) #if true enables detailed analysis plots
->>>>>>> 1.2
 )
 
 
