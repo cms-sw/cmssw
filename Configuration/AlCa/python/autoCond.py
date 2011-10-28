9dd8206cdfbe1f6415173994e51ec52de078f758
Stephanie Beauceron <sha1-c117a3d4e0f055ce820584107fcb2695f540f4e1@cern.ch>
2011-10-28 14:27:51 +0000

    New GT for Heavy Ion

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -5,5 +5,5 @@ autoCond = { 'mc'      : 'MC_50_V3::All',
              'startup' : 'START50_V3::All',
              'com10'   : 'GR_R_50_V1::All',
              'hltonline' : 'GR_R_50_V1::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI50_V3::All'
+             'starthi' : 'STARTHI50_V4::All'
              }
