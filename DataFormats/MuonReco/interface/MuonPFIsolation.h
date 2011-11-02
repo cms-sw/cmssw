44224491ffbf6760a3bcf8d5ffecda1957b0180b
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2011-11-02 06:43:20 +0000

    add sums of neutral hadrons and photons with a higher threshold (value is defined by reconstruction algo)

--- DataFormats/MuonReco/interface/MuonPFIsolation.h
+++ DataFormats/MuonReco/interface/MuonPFIsolation.h
@@ -8,9 +8,12 @@ namespace reco {
      float sumChargedParticlePt; //!< sum-pt of charged Particles(inludes e/mu) 
      float sumNeutralHadronEt;  //!< sum pt of neutral hadrons
      float sumPhotonEt;  //!< sum pt of PF photons
+     float sumNeutralHadronEtHighThreshold;  //!< sum pt of neutral hadrons with a higher threshold
+     float sumPhotonEtHighThreshold;  //!< sum pt of PF photons with a higher threshold
      float sumPUPt;  //!< sum pt of charged Particles not from PV  (for Pu corrections)
      MuonPFIsolation():
-       sumChargedHadronPt(0),sumChargedParticlePt(0),sumNeutralHadronEt(0),sumPhotonEt(0),sumPUPt(0) {};
+       sumChargedHadronPt(0),sumChargedParticlePt(0),sumNeutralHadronEt(0),sumPhotonEt(0),
+       sumNeutralHadronEtHighThreshold(0), sumPhotonEtHighThreshold(0), sumPUPt(0) {};
    };
 
 
