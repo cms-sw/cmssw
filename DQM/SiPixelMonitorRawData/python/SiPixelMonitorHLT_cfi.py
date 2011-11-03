8f8282bbaf0dcdf87fd80f2a47d835dccfde92fd
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-11-03 14:54:21 +0000

    Use rawDataCollector instead of source for raw data

--- DQM/SiPixelMonitorRawData/python/SiPixelMonitorHLT_cfi.py
+++ DQM/SiPixelMonitorRawData/python/SiPixelMonitorHLT_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 SiPixelHLTSource = cms.EDAnalyzer("SiPixelHLTSource",
-    RawInput = cms.InputTag("source"),
+    RawInput = cms.InputTag("rawDataCollector"),
     ErrorInput = cms.InputTag("siPixelDigis"),
     outputFile = cms.string('Pixel_DQM_HLT.root'),
     saveFile = cms.untracked.bool(False),
