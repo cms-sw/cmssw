76edfef26b2c72cf067a05fb4ebff5404ab79e59
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2011-11-07 11:10:23 +0000

    RPC Noise file overlooked

---
+++ CondFormats/DataRecord/src/RPCNoiseStripsObjectRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCNoiseStripsObjectRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      
+// Created:     Wed Nov  2 17:37:55 CET 2011
+// $Id: RPCNoiseStripsObjectRcd.cc,v 1.2 2011/11/07 08:09:34 mmaggi Exp $
+
+#include "CondFormats/DataRecord/interface/RPCNoiseStripsObjectRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(RPCNoiseStripsObjectRcd);
