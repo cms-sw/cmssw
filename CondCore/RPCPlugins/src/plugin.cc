fc069807c29b7b0725b66b8f7ef6470aca34f631
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2011-11-07 08:05:16 +0000

    clean up

--- CondCore/RPCPlugins/src/plugin.cc
+++ CondCore/RPCPlugins/src/plugin.cc
@@ -12,21 +12,20 @@
 #include "CondFormats/DataRecord/interface/RPCReadOutMappingRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCEMap.h"
 #include "CondFormats/DataRecord/interface/RPCEMapRcd.h"
+#include "CondFormats/RPCObjects/interface/RPCNoiseObject.h"
+#include "CondFormats/DataRecord/interface/RPCNoiseObjectRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCStripNoises.h"
 #include "CondFormats/DataRecord/interface/RPCStripNoisesRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCClusterSize.h"
 #include "CondFormats/DataRecord/interface/RPCClusterSizeRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCDQMObject.h"
 #include "CondFormats/DataRecord/interface/RPCDQMObjectRcd.h"
-#include "CondFormats/RPCObjects/interface/RPCNoiseObject.h"
-#include "CondFormats/DataRecord/interface/RPCNoiseObjectRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCMaskedStrips.h"
 #include "CondFormats/DataRecord/interface/RPCMaskedStripsRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCDeadStrips.h"
 #include "CondFormats/DataRecord/interface/RPCDeadStripsRcd.h"
 #include "CondFormats/RPCObjects/interface/L1RPCHwConfig.h"
 #include "CondFormats/DataRecord/interface/L1RPCHwConfigRcd.h"
-
 #include "CondFormats/DataRecord/interface/RPCObGasMixRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCObGasMix.h"
 #include "CondFormats/DataRecord/interface/RPCObGasHumRcd.h"
@@ -37,10 +36,8 @@
 #include "CondFormats/RPCObjects/interface/RPCObCond.h"
 #include "CondFormats/DataRecord/interface/RPCObUXCRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCObUXC.h"
-
 #include "CondFormats/DataRecord/interface/RPCObPVSSmapRcd.h"
 #include "CondFormats/RPCObjects/interface/RPCObPVSSmap.h"
-
 #include "CondFormats/DataRecord/interface/RBCBoardSpecsRcd.h"
 #include "CondFormats/DataRecord/interface/TTUBoardSpecsRcd.h"
 #include "CondFormats/RPCObjects/interface/RBCBoardSpecs.h"
@@ -48,10 +45,10 @@
 
 REGISTER_PLUGIN(RPCReadOutMappingRcd,RPCReadOutMapping);
 REGISTER_PLUGIN(RPCEMapRcd,RPCEMap);
+REGISTER_PLUGIN(RPCNoiseObjectRcd,RPCNoiseObject);
 REGISTER_PLUGIN(RPCStripNoisesRcd,RPCStripNoises);
-REGISTER_PLUGIN(RPCClusterSizeRcd,RPCClusterSize); //new plugin 21.XII.2009 for RPC cluster size chamber by chamber 
-REGISTER_PLUGIN(RPCDQMObjectRcd,RPCDQMObject); //new plugin for RPC DQMPVT 
-REGISTER_PLUGIN(RPCNoiseObjectRcd,RPCNoiseObject); //new plugin for RPC DQMPVT 
+REGISTER_PLUGIN(RPCClusterSizeRcd,RPCClusterSize); 
+REGISTER_PLUGIN(RPCDQMObjectRcd,RPCDQMObject); 
 REGISTER_PLUGIN(L1RPCHwConfigRcd,L1RPCHwConfig);
 REGISTER_PLUGIN(RPCObGasRcd,RPCObGas);
 REGISTER_PLUGIN(RPCObImonRcd,RPCObImon);
