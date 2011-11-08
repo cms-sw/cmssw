1144a5fa91f5d7ddde2d0e319026d4df354c921c
Christopher Farrell <sha1-8ff4c3e4c5e5d4f1b2a4a4e825bd74e83b42ab48@cern.ch>
2011-11-08 03:46:40 +0000

    Correct luminosity

--- SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Samples.h
+++ SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Samples.h
@@ -90,7 +90,7 @@
 
 
 int                  RunningPeriods = 2;
-double               IntegratedLuminosity = 4694; //3168; //2410;//2125; //2080; //1912; //1947; //1631; //976.204518023; //705.273820; //342.603275; //204.160928; //191.04;
+double               IntegratedLuminosity = 4679; //3168; //2410;//2125; //2080; //1912; //1947; //1631; //976.204518023; //705.273820; //342.603275; //204.160928; //191.04;
 double               IntegratedLuminosityBeforeTriggerChange = 355.227; //353.494; // Total luminosity taken before RPC L1 trigger change (went into effect on run 165970)
 float                Event_Weight = 1;
 int                  MaxEntry = -1;
