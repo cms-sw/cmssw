409e084739a674bb36ca7bb914e276f5c3ca59d5
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2011-11-08 14:31:32 +0000

    keeping a reference to the old file (for backward-compatibility reasons); will give a printout that tells the user to migrate to another cff

---
+++ TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py
@@ -0,0 +1,4 @@
+print "The file TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py is outdated."
+print "Please migrate to a more recent one (to be found in the same directory)."
+
+from TopQuarkAnalysis.TopObjectResolutions.stringResolutions_etEtaPhi_Spring10_cff import *
