bd14a0bcbc9a7dcf89369d305be90ec98367afa2
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2011-11-08 17:56:18 +0000

    muon DQM for HI run 2011

--- DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
+++ DQM/HLTEvF/python/HLTMonMuonDQM_cfi.py
@@ -6,43 +6,46 @@ hltMonMuDQM = cms.EDAnalyzer("HLTMuonDQMSource",
     MonitorDaemon = cms.untracked.bool(True),
     reqNum = cms.uint32(1),
     DaqMonitorBEInterface = cms.untracked.bool(True),
-    TrigResultInput = cms.InputTag('TriggerResults','','HLTonline'),
+    l3MuonSeedTag = cms.untracked.InputTag("hltHIL3TrajectorySeed"),
+    l3MuonTag = cms.untracked.InputTag("hltHIL3MuonCandidates"),
+    l3MuonSeedTagOIState = cms.untracked.InputTag("hltL3TrajSeedOIState"),
+    l3MuonSeedTagOIHit = cms.untracked.InputTag("hltHIL3TrajSeedOIHit"),
+    l3MuonTrkFindingOIState = cms.untracked.InputTag("hltHIL3TrackCandidateFromL2OIState"),
+    l3MuonTrkFindingOIHit = cms.untracked.InputTag("hltHIL3TrackCandidateFromL2OIHit"),
+    l3MuonTrkOIState = cms.untracked.InputTag("hltHIL3TkTracksFromL2OIState"),
+    l3MuonTrkOIHit = cms.untracked.InputTag("hltHIL3TkTracksFromL2OIHit"),
+    l3MuonTrk = cms.untracked.InputTag("hltHIL3TkTracksFromL2"),
+    l3muons = cms.untracked.InputTag("hltHIL3Muons"),
+    l3muonsOIState = cms.untracked.InputTag("hltHIL3MuonsOIState"),
+    l3muonsOIHit = cms.untracked.InputTag("hltHIL3MuonsOIHit"),
+    TrigResultInput = cms.InputTag('TriggerResults','','HLT'),
     filters = cms.VPSet(
-    	# L1 muon
-	cms.PSet(
-		directoryName = cms.string('L1PassThrough'),
-		triggerBits = cms.vstring('HLT_L1SingleMu10_v4','HLT_L1SingleMu20_v4','HLT_L1SingleMuOpen_DT_v4','HLT_L1SingleMuOpen_v4')
-	),
-    	# L2 muon
-	cms.PSet(
-		directoryName = cms.string('L2PassThrough'),
-		triggerBits = cms.vstring('HLT_L2Mu10_v5','HLT_L2Mu20_v5')
-	),
-    	# L3 muon no iso
-	cms.PSet(
-		directoryName = cms.string('L3Triggers'),
-		triggerBits = cms.vstring('HLT_Mu20_v5','HLT_Mu24_v5','HLT_Mu30_v5', 'HLT_Mu40_v3', 'HLT_Mu3_v7','HLT_Mu5_v7','HLT_Mu8_v5')
-	),
-    	# L3 muon with iso
-	cms.PSet(
-		directoryName = cms.string('L3Triggers_ISO'),
-		triggerBits = cms.vstring('HLT_IsoMu12_v7','HLT_IsoMu15_v11','HLT_IsoMu17_v11','HLT_IsoMu24_v7','HLT_IsoMu30_v7')
-	),
-    	# DoubleMu
-	cms.PSet(
-		directoryName = cms.string('DoubleMu'),
-		triggerBits = cms.vstring('HLT_L1DoubleMu0_v4','HLT_L2DoubleMu0_v6','HLT_DoubleMu3_v7','HLT_DoubleMu6_v5','HLT_DoubleMu7_v5','HLT_Mu13_Mu8_v4','HLT_Mu17_Mu8_v4')
-	),
-	# No tracker
-	cms.PSet(
-		directoryName = cms.string('Cosmics'),
-		triggerBits = cms.vstring('HLT_L2DoubleMu23_NoVertex_v5', 'HLT_L1SingleMuOpen_AntiBPTX_v3')
-	)
-    	# JetStream
-	#cms.PSet(
-	#	directoryName = cms.string('JetStream'),
-	#	triggerBits = cms.vstring('HLT_Jet15U', 'HLT_Jet30U')
-	#)
+      # Settings for Heavy Ion
+      # HI L1 Muons
+      cms.PSet(
+        directoryName = cms.string('HIL1PassThrough'),
+        triggerBits = cms.vstring('HLT_HIL1DoubleMuOpen_v1', 'HLT_HIL1DoubleMu0_HighQ_v1')
+      ),
+      # HI L2 Muons
+      cms.PSet(
+        directoryName = cms.string('HIL2PassThrough'),
+        triggerBits = cms.vstring('HLT_HIL2Mu3_NHitQ_v1', 'HLT_HIL2Mu7_v1', 'HLT_HIL2Mu15_v1', 'HLT_HIL2DoubleMu0_L1HighQL2NHitQ_v1', 'HLT_HIL2DoubleMu3_v1')
+      ),
+      # HI L3 Muons
+      cms.PSet(
+        directoryName = cms.string('HIL3PassThrough'),
+        triggerBits = cms.vstring('HLT_HIL3Mu3', 'HLT_HIL3DoubleMuOpen_v1', 'HLT_HIL3DoubleMuOpen_Mgt2_OS_NoCowboy_v1')
+      ),
+      # HI Single Mu
+      cms.PSet(
+        directoryName = cms.string('HISingleMu'),
+        triggerBits = cms.vstring('HLT_HIL2Mu3_NHitQ_v1', 'HLT_HIL2Mu7_v1', 'HLT_HIL2Mu15_v1', 'HLT_HIL3Mu3')
+      ),
+      # HI Double Mu
+      cms.PSet(
+        directoryName = cms.string('HIDoubleMu'),
+        triggerBits = cms.vstring('HLT_HIL1DoubleMu0_HighQ_v1', 'HLT_HIL2DoubleMu0_L1HighQL2NHitQ_v1', 'HLT_HIL2DoubleMu3_v1', 'HLT_HIL3DoubleMuOpen_v1', 'HLT_HIL3DoubleMuOpen_Mgt2_OS_NoCowboy_v1')
+      ),
     ),
     disableROOToutput = cms.untracked.bool(True)
 )
