0107bfe13f9871fed3fb98cd70b6ac6dd167580c
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2011-11-11 21:27:59 +0000

    new v

--- RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
+++ RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 currendir=`pwd`
-workdir="/build1/zx/cron/CMSSW_3_7_0_pre3"
+workdir="/afs/cern.ch/user/x/xiezhen/w1/luminewschema/CMSSW_4_2_3"
 authdir="/afs/cern.ch/user/x/xiezhen"
-outdir="/afs/cern.ch/cms/lumi/www/plots/operation"
-logpath="/afs/cern.ch/cms/lumi/"
+outdir="/afs/cern.ch/user/x/xiezhen/w1/luminewschema/CMSSW_4_2_3"
+logpath="."
 dbConnectionString="oracle://cms_orcoff_prod/cms_lumi_prod"
+amodetag="IONPHYS"
+begTime='11/11/11 00:00:00'
+normStr="hi7TeV"
 source /afs/cern.ch/cms/cmsset_default.sh;
 cd $workdir
 eval `scramv1 runtime -sh`
-touch "$logpath/lumiPlotHourly.log"
-date >> "$logpath/lumiPlotHourly.log"
-cp  "$outdir/runlist.txt" "$outdir/runlist.txt.old"
-lumiPlotFiller.py -c $dbConnectionString -P $authdir createrunlist -o $outdir >> "$logpath/lumiPlotHourly.log" ;
-sleep 1
-lumiPlotFiller.py -c $dbConnectionString -P $authdir instperrun -o $outdir -i "$outdir/runlist.txt">> "$logpath/lumiPlotHourly.log"
-#if `diff "$outdir/runlist.txt" "$outdir/runlist.txt.old" > /dev/null` ; then
-#   echo 'no new runs, do nothing' >> "$logpath/lumiPlotHourly.log"
-#else
-#fi   
-date >> "$logpath/lumiPlotHourly.log"
+touch "$logpath/lumiPlotCron.log"
+date >> "$logpath/lumiPlotCron.log"
+
+outfile="$outdir/totallumivstime-hi-2011"
+infile="$outdir/totallumivstime-hi-2011.csv"
+touch $infile
+echo "lumiSumPlot.py -c $dbConnectionString -P $authdir -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime $begTime -inplot $outfile -outplot $outfile time" >> "$logpath/lumiPlotCron.log"
+echo 0
+lumiSumPlot.py -c $dbConnectionString -P $authdir  -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime "$begTime" -inplot "$infile" -outplot "$outfile" time >> "$logpath/lumiPlotCron.log"
+
+outfile="$outdir/lumiperday-hi-2011"
+infile="$outdir/lumiperday-hi-2011.csv"
+touch $infile
+echo "lumiSumPlot.py -c $dbConnectionString -P $authdir -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime $begTime -inplot $infile -outplot $outfile  perday ">>  "$logpath/lumiPlotCron.log"
+echo 1
+lumiSumPlot.py -c $dbConnectionString -P $authdir -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime "$begTime" -inplot $infile -outplot $outfile perday >> "$logpath/lumiPlotCron.log"
+echo 2
+outfile="$outdir/lumipeak-hi-2011"
+infile="$outdir/lumipeak-hi-2011.csv"
+touch $infile
+echo "lumiSumPlot.py -c $dbConnectionString -P $authdir -o $outdir -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime $begTime -inplot $infile -outplot $outfile instpeakperday">> "$logpath/lumiPlotCron.log"
+echo 3
+lumiSumPlot.py -c $dbConnectionString -P $authdir -o $outdir -norm $normStr -b stable -beamenergy 3500 -beamfluctuation 0.2 -amodetag $amodetag -beginTime "$begTime" -inplot $infile -outplot $outfile instpeakperday >> "$logpath/lumiPlotCron.log"
+
+date >> "$logpath/lumiPlotCron.log"
 cd $currentdir
