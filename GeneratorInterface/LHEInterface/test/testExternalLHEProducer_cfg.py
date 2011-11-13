80aed3d7c6cb6e13df715ddac55e032ea1a0af46
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2011-11-13 22:45:03 +0000

    Set the number of events per luminosity block in order to split files

--- GeneratorInterface/LHEInterface/test/testExternalLHEProducer_cfg.py
+++ GeneratorInterface/LHEInterface/test/testExternalLHEProducer_cfg.py
@@ -5,10 +5,11 @@ process = cms.Process("ScriptExample")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.source = cms.Source("EmptySource",
-    firstLuminosityBlock = cms.untracked.uint32(23456)                            
+    firstLuminosityBlock = cms.untracked.uint32(23456),
+    numberEventsInLuminosityBlock = cms.untracked.uint32(10)
 )                            
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.load("GeneratorInterface/LHEInterface/ExternalLHEProducer_cfi")
 
