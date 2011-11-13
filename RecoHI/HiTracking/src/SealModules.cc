c33a4f14de0812aafd671105de2363cf3d47a5b5
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2011-11-13 13:17:15 +0000

    Add track-calo compatibility selector, remove track quality selector (instead using pp version)

--- RecoHI/HiTracking/src/SealModules.cc
+++ RecoHI/HiTracking/src/SealModules.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoHI/HiTracking/interface/HiAnalyticalTrackSelector.h"
+#include "RecoHI/HiTracking/interface/HICaloCompatibleTrackSelector.h"
 
-using reco::modules::HiAnalyticalTrackSelector;
+using reco::modules::HICaloCompatibleTrackSelector;
 
-DEFINE_FWK_MODULE(HiAnalyticalTrackSelector); 
+DEFINE_FWK_MODULE(HICaloCompatibleTrackSelector); 
