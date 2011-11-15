432e9b212cd655c2461e8f757d4589eb46bc7711
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2011-11-15 02:37:09 +0000

    Update minbias files for 5_0 series

--- SimGeneral/MixingModule/python/mixPoolSource_cfi.py
+++ SimGeneral/MixingModule/python/mixPoolSource_cfi.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 #FileNames = cms.untracked.vstring('dataset:/RelValMinBias/CMSSW_4_2_0_pre4-START42_V1-v1/GEN-SIM-DIGI-RAW-HLTDEBUG')
 
-FileNames = cms.untracked.vstring('/store/relval/CMSSW_4_2_0_pre4/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_42_V1-v1/0001/4C824492-2639-E011-9506-001A928116F0.root',
-        '/store/relval/CMSSW_4_2_0_pre4/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_42_V1-v1/0000/9ED6309B-C238-E011-A1D6-003048678ADA.root',
-        '/store/relval/CMSSW_4_2_0_pre4/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_42_V1-v1/0000/487A3591-CA38-E011-92A8-00248C0BE013.root',
-        '/store/relval/CMSSW_4_2_0_pre4/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_42_V1-v1/0000/0E5A4D31-BD38-E011-93B7-0026189437F2.root')
+FileNames = cms.untracked.vstring('/store/relval/CMSSW_5_0_0_pre2/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START50_V2-v1/0000/1632EC65-DBF2-E011-9F71-0018F3D09658.root',
+       '/store/relval/CMSSW_5_0_0_pre2/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START50_V2-v1/0002/242948B4-0DF3-E011-B762-001A928116C6.root',
+       '/store/relval/CMSSW_5_0_0_pre2/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START50_V2-v1/0002/566DA3FE-DCF3-E011-BB34-003048678B26.root')
                         
