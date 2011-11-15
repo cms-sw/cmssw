457fa09f6c6d9af86dec572da8b0ebffd9e79bd7
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2011-11-15 16:41:18 +0000

    Remove obsolete modules. This finishes the migration from ESSource to ESProducer for modules which read from the EventSetup. Previous changes removed all dependence on them.

--- JetMETCorrections/Modules/src/SealModule.cc
+++ JetMETCorrections/Modules/src/SealModule.cc
@@ -5,10 +5,8 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "JetMETCorrections/Objects/interface/JetCorrector.h"
 #include "JetMETCorrections/Objects/interface/JetCorrectionsRecord.h"
-#include "JetMETCorrections/Modules/interface/JetCorrectionService.h"
 #include "JetMETCorrections/Modules/interface/JetCorrectionESProducer.h"
 #include "JetMETCorrections/Modules/interface/JetCorrectionESSource.h"
-#include "JetMETCorrections/Modules/interface/JetCorrectionServiceChain.h"
 #include "JetMETCorrections/Modules/interface/JetCorrectionESChain.h"
 #include "JetMETCorrections/Modules/interface/JetCorrectionProducer.h"
 #include "JetMETCorrections/Algorithms/interface/LXXXCorrector.h"
@@ -42,30 +40,24 @@ DEFINE_FWK_MODULE(TrackJetCorrectionProducer);
 typedef JetCorrectionProducer<GenJet> GenJetCorrectionProducer;
 DEFINE_FWK_MODULE(GenJetCorrectionProducer);
 
-DEFINE_FWK_EVENTSETUP_SOURCE(JetCorrectionServiceChain);
 DEFINE_FWK_EVENTSETUP_MODULE(JetCorrectionESChain);
 
 //--------------- Generic LX corrections --------------------
-DEFINE_JET_CORRECTION_SERVICE (LXXXCorrector, LXXXCorrectionService);
 DEFINE_JET_CORRECTION_ESSOURCE (LXXXCorrector, LXXXCorrectionESSource);
 DEFINE_JET_CORRECTION_ESPRODUCER (LXXXCorrector, LXXXCorrectionESProducer);
 
 //--------------- L1 Offset subtraction corrections ---------
-DEFINE_JET_CORRECTION_SERVICE (L1OffsetCorrector, L1OffsetCorrectionService);
 DEFINE_JET_CORRECTION_ESSOURCE (L1OffsetCorrector, L1OffsetCorrectionESSource);
 DEFINE_JET_CORRECTION_ESPRODUCER (L1OffsetCorrector, L1OffsetCorrectionESProducer);
 
 //--------------- L1 Offset subtraction corrections ---------
-DEFINE_JET_CORRECTION_SERVICE (L1JPTOffsetCorrector, L1JPTOffsetCorrectionService);
 DEFINE_JET_CORRECTION_ESSOURCE (L1JPTOffsetCorrector, L1JPTOffsetCorrectionESSource);
 DEFINE_JET_CORRECTION_ESPRODUCER (L1JPTOffsetCorrector, L1JPTOffsetCorrectionESProducer);
 
 //--------------- L1 fastjet UE&PU subtraction corrections --
-DEFINE_JET_CORRECTION_SERVICE (L1FastjetCorrector, L1FastjetCorrectionService);
 DEFINE_JET_CORRECTION_ESSOURCE (L1FastjetCorrector, L1FastjetCorrectionESSource);
 DEFINE_JET_CORRECTION_ESPRODUCER (L1FastjetCorrector, L1FastjetCorrectionESProducer);
 
 //---------------  L6 SLB corrections -----------------------
-DEFINE_JET_CORRECTION_SERVICE (L6SLBCorrector, L6SLBCorrectionService);
 DEFINE_JET_CORRECTION_ESSOURCE (L6SLBCorrector, L6SLBCorrectionESSource);
 DEFINE_JET_CORRECTION_ESPRODUCER (L6SLBCorrector, L6SLBCorrectionESProducer);
