c364bd463cd5744f896bc79fe06693e198d93b96
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2011-11-15 22:38:08 +0000

    the DIGIPU1 step2 action does not need to drop schema since step1 is never from input

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -358,7 +358,6 @@ step2Defaults = { 'cfg'           : 'step2',
                   }
 #for reading back from a file, we have to drop objects whose schema may have changed
 step2DropSchemaNoPU ={'--customise' : 'Configuration/StandardSequences/DigiToRecoNoPU.customise',}
-step2DropSchemaPU ={'--customise' : 'Configuration/StandardSequences/DigiToRecoPU.customise',}
 
 step2 = {}
 
@@ -367,7 +366,7 @@ step2['DIGI']=merge([step2Defaults,step2DropSchemaNoPU])
 #step2['DIGI2']=merge([stCond,step2Defaults,step2DropSchemaNoPU])
 step2['DIGICOS']=merge([{'--scenario':'cosmics','--eventcontent':'FEVTDEBUG','--datatier':'GEN-SIM-DIGI-RAW'},stCond,step2Defaults,step2DropSchemaNoPU])
 
-step2['DIGIPU1']=merge([step2Defaults,PU1,step2DropSchemaPU])
+step2['DIGIPU1']=merge([step2Defaults,PU1])
 
 step2['DIGIHI']=merge([{'--inputCommands':'"keep *","drop *_simEcalPreshowerDigis_*_*"','-n':10},hiDefaults,step2Defaults])
 
