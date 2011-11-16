ea2c27426370e448f52c0f6ee3c5e5fa41fe9d61
Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch>
2011-11-16 14:54:02 +0000

    add new class for clibrating ES dead regions

---
+++ CondFormats/DataRecord/interface/ESMissingEnergyCalibrationRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_ESMissingEnergyCalibration_H
+#define CondFormats_DataRecord_ESMissingEnergyCalibration_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class ESMissingEnergyCalibrationRcd : public edm::eventsetup::EventSetupRecordImplementation<ESMissingEnergyCalibrationRcd> {};
+#endif
