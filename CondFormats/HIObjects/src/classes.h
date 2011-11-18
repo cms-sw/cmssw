470a750b9911e9e619afb6ef7e06fc33400ed038
Salvatore Di Guida <sha1-744703a2e531bf35de4c8c886b1940546bb70d35@cern.ch>
2011-11-18 18:40:51 +0000

    fixed mapping for RPFlatParams: now using a blob

--- CondFormats/HIObjects/src/classes.h
+++ CondFormats/HIObjects/src/classes.h
@@ -3,8 +3,10 @@
 
 #include <vector>
 
-namespace { namespace {
-std::vector<CentralityTable::CBin> dummy;
- std::vector<RPFlatParams::EP> yummy;
-}}
+namespace {  
+  struct dictionary{
+    std::vector<CentralityTable::CBin> dummy;
+    std::vector<RPFlatParams::EP> yummy;
+  };
+}
 
