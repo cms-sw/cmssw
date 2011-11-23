bab13cbbcaf55ac0fe682d4f923555f9fdc39aa4
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2011-11-23 14:46:31 +0000

    Shuichi Kunori's update to pack more info ("toward 2012")

--- CondFormats/HcalObjects/interface/HcalMCParam.h
+++ CondFormats/HcalObjects/interface/HcalMCParam.h
@@ -16,22 +16,37 @@ POOL object to store MC information
 // 2 - SiPMs shape (HO, possibly also in HB/HE)
 // 3 - HF Shape
 // 4 - ZDC shape 
-
-
+// 
+// change in definition  28.Oct.2011  sk
+// mParam1 is now packed word.
+//   pulseShapeID                              [0,500]          9 bits  (use this as phot0 detetor ID as well)
+//   syncPhase = cms.bool(True),               bool             1 bit   (use this for QPLL unlocked channel)
+//   binOfMaximum = cms.int32(5)               [1-10]           4 bits
+//   timePhase = cms.double(5.0),              [-30.0,30.0]     8 bits  (0.25ns step)
+//   timeSmearing = cms.bool(False)            bool             1 bit  
+//   packingScheme                                              4 bits
 class HcalMCParam {
  public:
-  HcalMCParam():mId(0), mSignalShape(0) {}
+  HcalMCParam():mId(0), mParam1(0) {}
 
-  HcalMCParam(unsigned long fId, unsigned int fSignalShape):
-    mId(fId), mSignalShape(fSignalShape) {}
+  HcalMCParam(unsigned long fId, unsigned int fParam1):
+    mId(fId), mParam1(fParam1) {}
 
   uint32_t rawId () const {return mId;}
 
-  unsigned int signalShape() const {return mSignalShape;}
+  unsigned int param1() const {return mParam1;}
+  unsigned int signalShape() const {return mParam1&0x1FF;}
+  bool         syncPhase() const {return (mParam1>>9)&0x1;}
+  unsigned int binOfMaximum() const {return (mParam1>>10)&0xF;}
+  float        timePhase() const {return ((mParam1>>14)&0xFF)/4.0-32.0;}
+  bool         timeSmearing() const {return (mParam1>>22)&0x1;}
+  unsigned int packingScheme() const {return (mParam1>>27)&0xF;}
+
 
  private:
+
   uint32_t mId;
-  uint32_t mSignalShape;
+  uint32_t mParam1;
 };
 
 #endif
