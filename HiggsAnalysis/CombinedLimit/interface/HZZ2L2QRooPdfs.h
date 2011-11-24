82186a017daa5d4913de83e926d39fc1d7741d79
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-11-24 11:36:16 +0000

    Pdfs from Alessio

--- HiggsAnalysis/CombinedLimit/interface/HZZ2L2QRooPdfs.h
+++ HiggsAnalysis/CombinedLimit/interface/HZZ2L2QRooPdfs.h
@@ -122,4 +122,38 @@ private:
   ClassDef(RooRelBW,1)
 };
  
+
+class Triangle : public RooAbsPdf {
+public:
+  Triangle();
+  Triangle(const char *name, const char *title,                
+	   RooAbsReal& _m,
+	   RooAbsReal& _start,
+	   RooAbsReal& _turn,
+	   RooAbsReal& _stop
+	   );	
+  
+  Triangle(const Triangle& other, const char* name = 0);
+  virtual TObject* clone(const char* newname) const { 
+    return new Triangle(*this,newname); }
+
+  inline virtual ~Triangle() { }
+  Int_t getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* rangeName=0) const ;
+  Double_t analyticalIntegral(Int_t code, const char* rangeName=0) const ;
+
+protected:
+
+  RooRealProxy m;
+  RooRealProxy start;
+  RooRealProxy turn;
+  RooRealProxy stop;
+  
+  Double_t evaluate() const;
+
+private:
+  
+  ClassDef(Triangle,1)
+};
+
+
 #endif
--- HiggsAnalysis/CombinedLimit/src/HZZ2L2QRooPdfs.cc
+++ HiggsAnalysis/CombinedLimit/src/HZZ2L2QRooPdfs.cc
@@ -191,3 +191,67 @@ double RooCB::evaluate() const
    return pow(x*x,n)/((x*x-mean*mean)*(x*x-mean*mean)+pow(x*x/(mean*mean),2*n)*mean*mean*width*width);
  } 
 
+
+ClassImp(Triangle)
+
+  Triangle::Triangle(){}
+
+Triangle::Triangle(const char *name, const char *title,                
+		   RooAbsReal& _m,
+		   RooAbsReal& _start,
+		   RooAbsReal& _turn,
+		   RooAbsReal& _stop
+		   ):
+  RooAbsPdf(name, title),
+  m("m", "Dependent", this, _m),
+  start("start","start",this,_start),
+  turn("turn","turn",this,_turn),
+  stop("stop","stop",this,_stop)
+{
+}
+
+Triangle::Triangle(const Triangle& other, const char* name) :
+  RooAbsPdf(other, name), m("m", this, other.m),start("start", this, other.start), turn("turn", this, other.turn), stop("stop", this, other.stop)
+{
+}
+
+Double_t Triangle::evaluate() const 
+{
+
+  //std::cout << m << " "<<1.+(start-m)/turn << " " << 1+(turn-m)/stop << std::endl;
+  if(m<turn  && m > turn+start)
+    return 1.+(turn-m)/start;
+  if(m>=turn && m < turn+stop)
+    return 1.+(turn-m)/stop;
+  
+  return 0;
+}
+
+
+Int_t Triangle::getAnalyticalIntegral(RooArgSet& allVars, RooArgSet& analVars, const char* range) const 
+{
+  if (matchArgs(allVars,analVars,m)) return 1;
+  return 0;
+}
+
+Double_t Triangle::analyticalIntegral(Int_t code, const char* rangeName) const 
+{
+
+  // WARNING, ASSSUMES TURN TO BE IN INTERVAL
+  assert(code==1) ;
+  //whole triangle
+  Double_t sumleft = sqrt(1+ (turn+start)*(turn+start) ) ;
+  Double_t sumright= sqrt(1+ (turn+stop)*(turn+stop) );
+
+
+  if(m.min() < turn+start)// correct for left missing bit
+    sumleft -= sumleft*(m.min()-(turn+start))/fabs(start);
+
+
+  if(m.max() > turn+stop)// correct for right missing bit
+    sumright -= sumright*(turn+stop -m.max())/fabs(stop);
+
+  
+
+  return sumleft+sumright;    
+}
