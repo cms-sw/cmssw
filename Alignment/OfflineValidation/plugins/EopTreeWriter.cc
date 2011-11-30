cd3789391a0dfa865fe779c85af7205403cd2675
Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch>
2011-11-30 07:45:28 +0000

    - made getDistInCM function a member function - made several global variables member variables - moved remaining parts in EopTreeWriter.h to EopTreeWriter.cc (EopTreeWriter.h is now obsolete) - cleanup of gcc461 warnings - removal of forbidden using directives

--- Alignment/OfflineValidation/plugins/EopTreeWriter.cc
+++ Alignment/OfflineValidation/plugins/EopTreeWriter.cc
@@ -13,11 +13,57 @@
 //
 // Original Author:  Holger Enderle
 //         Created:  Thu Dec  4 11:22:48 CET 2008
-// $Id$
+// $Id: EopTreeWriter.cc,v 1.1 2011/11/10 13:18:06 henderle Exp $
 //
 //
 
-#include "EopTreeWriter.h"
+// framework include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+// user include files
+#include <DataFormats/TrackReco/interface/Track.h>
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include <TMath.h>
+#include <TH1.h>
+#include "TTree.h"
+
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/Utils/interface/TFileDirectory.h"
+
+#include "TrackingTools/TrackAssociator/interface/TrackDetectorAssociator.h"
+#include "TrackingTools/TrackAssociator/interface/TrackAssociatorParameters.h"
+#include "TrackingTools/GeomPropagators/interface/PropagationExceptions.h"
+#include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerFwd.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
+#include "RecoParticleFlow/PFRootEvent/interface/JetRecoTypes.h"
+#include "RecoParticleFlow/PFRootEvent/interface/JetMaker.h"
+#include "RecoParticleFlow/PFRootEvent/interface/ProtoJet.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+
+#include "DataFormats/HcalIsolatedTrack/interface/IsolatedPixelTrackCandidateFwd.h"
+#include "DataFormats/HcalIsolatedTrack/interface/IsolatedPixelTrackCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoCaloTowerCandidate.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/DTRecHit/interface/DTRecHitCollection.h"
+
+#include "Alignment/OfflineValidation/interface/EopVariables.h"
 
 //
 // class decleration
@@ -34,9 +80,16 @@ class EopTreeWriter : public edm::EDAnalyzer {
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
+      double getDistInCM(double eta1, double phi1, double eta2, double phi2);
+
       // ----------member data ---------------------------
       edm::InputTag src_;
 
+      edm::Service<TFileService> fs_;
+      TTree *tree_;
+      EopVariables *treeMemPtr_;
+      TrackDetectorAssociator trackAssociator_;
+      TrackAssociatorParameters parameters_;
 };
 
 //
@@ -59,9 +112,9 @@ EopTreeWriter::EopTreeWriter(const edm::ParameterSet& iConfig) :
    edm::ParameterSet parameters = iConfig.getParameter<edm::ParameterSet>("TrackAssociatorParameters");
    parameters_.loadParameters( parameters );
 
-   tree = fs->make<TTree>("EopTree","EopTree");
-   treeMemPtr = new EopVariables;
-   tree->Branch("EopVariables", &treeMemPtr); // address of pointer!
+   tree_ = fs_->make<TTree>("EopTree","EopTree");
+   treeMemPtr_ = new EopVariables;
+   tree_->Branch("EopVariables", &treeMemPtr_); // address of pointer!
 }
 
 
@@ -223,23 +276,18 @@ EopTreeWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	   
 	 }
 
-       treeMemPtr->fillVariables(track->charge(), track->innerOk(), track->outerRadius(),
-				 track->numberOfValidHits(), track->numberOfLostHits(),
-				 track->chi2(), track->normalizedChi2(),
-				 track->p(), track->pt(), track->ptError(), 
-				 track->theta(), track->eta(), track->phi(),
-				 trackemc1, trackemc3, trackemc5,
-				 trackhac1, trackhac3, trackhac5,
-				 maxPNearby, dist, EnergyIn, EnergyOut);
+       treeMemPtr_->fillVariables(track->charge(), track->innerOk(), track->outerRadius(),
+				  track->numberOfValidHits(), track->numberOfLostHits(),
+				  track->chi2(), track->normalizedChi2(),
+				  track->p(), track->pt(), track->ptError(), 
+				  track->theta(), track->eta(), track->phi(),
+				  trackemc1, trackemc3, trackemc5,
+				  trackhac1, trackhac3, trackhac5,
+				  maxPNearby, dist, EnergyIn, EnergyOut);
        
-       tree->Fill();
+       tree_->Fill();
        }
    }
-
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
 }
 
 
@@ -253,8 +301,33 @@ EopTreeWriter::beginJob()
 void 
 EopTreeWriter::endJob() {
 
-  delete treeMemPtr; treeMemPtr = 0;
+  delete treeMemPtr_; treeMemPtr_ = 0;
 
+}
+
+double 
+EopTreeWriter::getDistInCM(double eta1, double phi1, double eta2, double phi2)
+{
+  double deltaPhi=phi1-phi2;
+  while(deltaPhi >   TMath::Pi())deltaPhi-=2*TMath::Pi();
+  while(deltaPhi <= -TMath::Pi())deltaPhi+=2*TMath::Pi();
+  double dR;
+  // double Rec;
+  double theta1=2*atan(exp(-eta1));
+  double theta2=2*atan(exp(-eta2));
+  double cotantheta1;
+  if(cos(theta1)==0)cotantheta1=0;
+  else cotantheta1=1/tan(theta1);
+  double cotantheta2;
+  if(cos(theta2)==0)cotantheta2=0;
+  else cotantheta2=1/tan(theta2);
+  // if (fabs(eta1)<1.479) Rec=129; //radius of ECAL barrel
+  // else Rec=317; //distance from IP to ECAL endcap
+  //|vect| times tg of acos(scalar product)
+  // dR=fabs((Rec/sin(theta1))*tan(acos(sin(theta1)*sin(theta2)*(sin(phi1)*sin(phi2)+cos(phi1)*cos(phi2))+cos(theta1)*cos(theta2))));
+  if(fabs(eta1)<1.479)dR=129*sqrt((cotantheta1-cotantheta2)*(cotantheta1-cotantheta2)+deltaPhi*deltaPhi);
+  else dR=317*sqrt(tan(theta1)*tan(theta1)+tan(theta2)*tan(theta2)-2*tan(theta1)*tan(theta2)*cos(deltaPhi));
+  return dR;
 }
 
 //define this as a plug-in
