bf7a44f127c1d468ea0767a84bed34f03511c7b4
Kevin Burkett <sha1-d3b62d5921247fe185d98f6705fe0f56064ef84b@cern.ch>
2011-12-01 08:39:36 +0000

    Add centered parameters based on 2011 beam size

--- IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
@@ -143,6 +143,19 @@ Realistic2p76TeV2011CollisionVtxSmearingParameters = cms.PSet(
     Y0 = cms.double(0.3929),
     Z0 = cms.double(0.4145)
 )
+# 7 TeV centered collisions with parameters for 2011
+# normalized emittance 2.5 microns, transverse beam size is 32 microns
+Centered7TeV2011CollisionVtxSmearingParameters = cms.PSet(
+    Phi = cms.double(0.0),
+    BetaStar = cms.double(150.0),
+    Emittance = cms.double(0.67e-07),
+    Alpha = cms.double(0.0),
+    SigmaZ = cms.double(5.22),
+    TimeOffset = cms.double(0.0),
+    X0 = cms.double(0.0),
+    Y0 = cms.double(0.0),
+    Z0 = cms.double(0.0)
+)
 # 8 TeV realistic collisions, transverse beam width size is 26 microns
 Realistic8TeVCollisionVtxSmearingParameters = cms.PSet(
     Phi = cms.double(0.0),
