a5feb5c4468fb9148c37c77175db0ad384d663f0
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-12-03 00:52:23 +0000

    update GTs for 50X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_50_V5::All',
-             'startup' : 'START50_V5::All',
-             'com10'   : 'GR_R_50_V3::All',
-             'hltonline' : 'GR_R_50_V3::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI50_V6::All'
+autoCond = { 'mc'      : 'MC_50_V6::All',
+             'startup' : 'START50_V6::All',
+             'com10'   : 'GR_R_50_V4::All',
+             'hltonline' : 'GR_R_50_V4::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI50_V7::All'
              }
