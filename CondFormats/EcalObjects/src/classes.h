60669527ae6b138c1248b4e8dfbeb7f0eb9f1eff
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-12-05 18:06:54 +0000

    Move std dictionaries to DataFormats/StdDictionaries

--- CondFormats/EcalObjects/src/classes.h
+++ CondFormats/EcalObjects/src/classes.h
@@ -153,8 +153,8 @@ namespace{
     EcalTPGCrystalStatus tpgCrystalStatus;
 
     EcalSRSettings ecalSRSettings;
-    std::vector<std::vector<short> > ecalSRSettings_srpMasksFromConfig;
-    std::vector<std::vector<float> > ecalSRSettings_dccNormalizedWeights_0;
+    //    std::vector<std::vector<short> > ecalSRSettings_srpMasksFromConfig;
+    //    std::vector<std::vector<float> > ecalSRSettings_dccNormalizedWeights_0;
     //    std::vector<float> ecalSRSettings_dccNormalizedWeights_1;
     //    float ecalSRSettings_dccNormalizedWeights_elt_2;
     
