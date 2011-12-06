4fc4f4429fceab11a0d99321bfd143cd24428730
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2011-12-06 10:02:50 +0000

    bin number in evt content

--- RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
+++ RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 RecoHiCentralityFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*',
+                                           'keep *_centralityBin_*_*',
+                                           )
     )
 
 RecoHiCentralityRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*'
+                                           'keep *_centralityBin_*_*',                                           
+                                           )
     )
 
 RecoHiCentralityAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*'
+                                           'keep *_centralityBin_*_*',                                           
+                                           )
     )
