c3e88cb455e7cae989d4ab12b9db5b8948888a7d
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-12-06 18:46:28 +0000

    add FastTimerServiceClient

--- HLTrigger/Timer/python/FastTimerService_cff.py
+++ HLTrigger/Timer/python/FastTimerService_cff.py
@@ -2,16 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 # FastTimerService
 from HLTrigger.Timer.FastTimerService_cfi import *
+from HLTrigger.Timer.fastTimerServiceClient_cfi import *
 
 # DQM file saver
 dqmFileSaver = cms.EDAnalyzer( "DQMFileSaver",
-  convention        = cms.untracked.string( "Offline" ),
-  workflow          = cms.untracked.string( "/HLT/FastTimerService/All" ),
-  dirName           = cms.untracked.string( "." ),
-  saveByRun         = cms.untracked.int32(  1 ),
-  saveByLumiSection = cms.untracked.int32( -1 ),
-  saveAtJobEnd      = cms.untracked.bool( False ),
- #forceRunNumber    = cms.untracked.int32( 999999 )
+#   producer          = cms.untracked.string('DQM'),
+#   version           = cms.untracked.int32(1),
+#   referenceRequireStatus = cms.untracked.int32(100),
+#   runIsComplete     = cms.untracked.bool(False),
+#   referenceHandling = cms.untracked.string('all'),
+    convention        = cms.untracked.string( "Offline" ),
+    workflow          = cms.untracked.string( "/HLT/FastTimerService/All" ),
+    dirName           = cms.untracked.string( "." ),
+    saveByRun         = cms.untracked.int32(1),
+    saveByLumiSection = cms.untracked.int32(-1),
+    saveByEvent       = cms.untracked.int32(-1),
+    saveByTime        = cms.untracked.int32(-1),
+    saveByMinute      = cms.untracked.int32(-1),
+    saveAtJobEnd      = cms.untracked.bool(False),
+    forceRunNumber    = cms.untracked.int32(-1),
 )
 
-DQMFileSaverOutput = cms.EndPath( dqmFileSaver )
+DQMFileSaverOutput = cms.EndPath( fastTimerServiceClient + dqmFileSaver )
