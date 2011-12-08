0557d601238d68114102ea697c3cf76a6f65056e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-12-08 10:01:45 +0000

    devirtualize TrajectoryState

--- TrackingTools/GsfTools/interface/BasicMultiTrajectoryState.h
+++ TrackingTools/GsfTools/interface/BasicMultiTrajectoryState.h
@@ -7,6 +7,14 @@
 #include "FWCore/Utilities/interface/Exception.h"
 #include "TrackingTools/GsfTools/interface/MultiTrajectoryStateCombiner.h"
 
+/** Class which combines a set of components of a Gaussian mixture
+ *  into a single component. Given all the components of a mixture, it
+ *  calculates the mean and covariance matrix of the entire mixture.
+ *  This combiner class can also be used in the process of transforming a
+ *  Gaussian mixture into another Gaussian mixture with a smaller number
+ *  of components. The relevant formulas can be found in
+ *  R. Fruhwirth, Computer Physics Communications 100 (1997), 1.
+ */
 class BasicMultiTrajectoryState : public BasicTrajectoryState {
 
   typedef TrajectoryStateOnSurface        TSOS;  
