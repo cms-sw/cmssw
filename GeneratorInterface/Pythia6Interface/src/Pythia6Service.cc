3f1a3e0c5fe100576ecca03b6d6c58dac8574308
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-12-08 16:30:39 +0000

    Properly fix txgive.
    * Actually pass buf to fortran, not the original string.
    * Check for parameter size.
    * Terminate with a \n, than with a 0, to make sure both read and C are happy.
    * Pad with ' ' just in case the string is also not used with read.

--- GeneratorInterface/Pythia6Interface/src/Pythia6Service.cc
+++ GeneratorInterface/Pythia6Interface/src/Pythia6Service.cc
@@ -209,18 +209,28 @@ void Pythia6Service::setCSAParams()
    char buf[514];
    
    txgive_init_();
-   
    for(std::vector<std::string>::const_iterator iter = fParamCSA.begin();
 	                                        iter != fParamCSA.end(); ++iter)
    {
-      if ( iter->length() <= 0 ) continue;
-      strcpy( buf, iter->c_str() );
-      if ( buf[iter->length()-1] != '\n' )
+      // Null pad the string should not be needed because it uses
+      // read, which will look for \n, but just in case...
+      for (size_t i = 0; i < 515; ++i)
+        buf[i] = ' ';
+      // Skip empty parameters.
+      if (iter->length() <= 0)
+        continue;
+      // Limit the size of the string to something which fits the buffer.
+      size_t maxSize = iter->length() > 512 ? 512 : iter->length();
+      strncpy(buf, iter->c_str(), maxSize);
+      // Add extra \n if missing, otherwise "read" continues reading. 
+      if (buf[maxSize-1] != '\n')
       {
-         buf[iter->length()] = '\n';
-	 buf[iter->length()+1] = 0;
-      }      
-      txgive_( iter->c_str(), iter->length() );
+         buf[maxSize] = '\n';
+         // Null terminate in case the string is passed back to C.
+         // Not sure that is actually needed.
+	 buf[maxSize + 1] = 0;
+      }
+      txgive_(buf, iter->length() );
    }   
    
    return ;
