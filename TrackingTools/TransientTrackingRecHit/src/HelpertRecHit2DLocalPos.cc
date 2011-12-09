83a67170cd40b343a0e195150959f446c21e74a8
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-12-09 09:51:31 +0000

    remove global align error

--- TrackingTools/TransientTrackingRecHit/src/HelpertRecHit2DLocalPos.cc
+++ TrackingTools/TransientTrackingRecHit/src/HelpertRecHit2DLocalPos.cc
@@ -4,30 +4,31 @@
 #include "DataFormats/TrackingRecHit/interface/AlignmentPositionError.h"
 
 void
-HelpertRecHit2DLocalPos::updateWithAPE(LocalError& le, const GeomDet& det) {
-    LocalError lape = det.localAlignmentError();
-    if (lape.valid())
-      le = LocalError(le.xx()+lape.xx(),
-		      le.xy()+lape.xy(),
-		      le.yy()+lape.yy());
-}
+HelpertRecHit2DLocalPos::updateWithAPE(LocalError& le, const GeomDet& det) 
+{
+  LocalError lape = det.localAlignmentError();
+  if (lape.valid())
+    le = LocalError(le.xx()+lape.xx(),
+		    le.xy()+lape.xy(),
+		    le.yy()+lape.yy()
+		    );
 }
 
 AlgebraicSymMatrix HelpertRecHit2DLocalPos::parError( const LocalError& le,
 						      const GeomDet& det)
 {
   AlgebraicSymMatrix m(2);
-    LocalError lape = det.localAlignmentError();
-    if (lape.valid()) {
-      m[0][0] = le.xx()+lape.xx();
-      m[0][1] = le.xy()+lape.xy();
-      m[1][1] = le.yy()+lape.yy();
-    } else {
-      m[0][0] = le.xx();
-      m[0][1] = le.xy();
-      m[1][1] = le.yy();
-    };
-    return m;
+  LocalError lape = det.localAlignmentError();
+  if (lape.valid()) {
+    m[0][0] = le.xx()+lape.xx();
+    m[0][1] = le.xy()+lape.xy();
+    m[1][1] = le.yy()+lape.yy();
+  } else {
+    m[0][0] = le.xx();
+    m[0][1] = le.xy();
+    m[1][1] = le.yy();
+  };
+  return m;
 }
 
 void
@@ -35,14 +36,14 @@ HelpertRecHit2DLocalPos::getKfComponents(KfComponentsHolder & holder,
 					 const TrackingRecHit &hit2dLocalPos,
 					 const GeomDet& det)
 {
-    hit2dLocalPos.getKfComponents(holder);
-    LocalError lape = det.localAlignmentError();
-    if (lape.valid()) {
-      AlgebraicSymMatrix22 &m = holder.errors<2>();
-      m(0, 0) += lape.xx();
-      m(0, 1) += lape.xy();
-      m(1, 1) += lape.yy();
-    }
+  hit2dLocalPos.getKfComponents(holder);
+  LocalError lape = det.localAlignmentError();
+  if (lape.valid()) {
+    AlgebraicSymMatrix22 &m = holder.errors<2>();
+    m(0, 0) += lape.xx();
+    m(0, 1) += lape.xy();
+    m(1, 1) += lape.yy();
+  }
 }
 
 void
@@ -50,10 +51,10 @@ HelpertRecHit2DLocalPos::getKfComponents(KfComponentsHolder & holder,
 					 const SiStripRecHit1D& hit1D,
 					 const GeomDet& det)
 {
-    hit1D.getKfComponents(holder);
-    LocalError lape =det.localAlignmentError();
-    if (lape.valid()) {
-      AlgebraicSymMatrix11 &m = holder.errors<1>();
-      m(0, 0) += lape.xx();
-    }
+  hit1D.getKfComponents(holder);
+  LocalError lape =det.localAlignmentError();
+  if (lape.valid()) {
+    AlgebraicSymMatrix11 &m = holder.errors<1>();
+    m(0, 0) += lape.xx();
+  }
 }
