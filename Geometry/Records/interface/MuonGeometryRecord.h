6c7854a64e9ae21ebc9ba8b2bc1428c8d1dcd818
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2011-12-10 03:15:16 +0000

    Added an explicit dependency on DTRecoGeometryRcd. Formerly it was implicitly dependent through MuonNumberingRecord.

--- Geometry/Records/interface/MuonGeometryRecord.h
+++ Geometry/Records/interface/MuonGeometryRecord.h
@@ -4,8 +4,8 @@
 /** \class MuonGeometryRecord
  *  The Muon DetUnit geometry.
  *
- *  $Date: 2008/02/19 13:54:58 $
- *  $Revision: 1.5 $
+ *  $Date: 2009/03/16 17:06:53 $
+ *  $Revision: 1.6 $
  *  \author N. Amapane - CERN
  */
 
@@ -14,6 +14,7 @@
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/Records/interface/MuonNumberingRecord.h"
 #include "Geometry/Records/interface/RPCRecoGeometryRcd.h"
+#include "Geometry/Records/interface/DTRecoGeometryRcd.h"
 #include "boost/mpl/vector.hpp"
 #include "CondFormats/AlignmentRecord/interface/DTAlignmentRcd.h"
 #include "CondFormats/AlignmentRecord/interface/DTAlignmentErrorRcd.h"
@@ -21,7 +22,7 @@
 #include "CondFormats/AlignmentRecord/interface/CSCAlignmentErrorRcd.h"
 #include "CondFormats/AlignmentRecord/interface/GlobalPositionRcd.h"
 
-class MuonGeometryRecord : public edm::eventsetup::DependentRecordImplementation<MuonGeometryRecord,boost::mpl::vector<IdealGeometryRecord, MuonNumberingRecord, DTAlignmentRcd, DTAlignmentErrorRcd, CSCAlignmentRcd, CSCAlignmentErrorRcd, GlobalPositionRcd, RPCRecoGeometryRcd> > {};
+class MuonGeometryRecord : public edm::eventsetup::DependentRecordImplementation<MuonGeometryRecord,boost::mpl::vector<IdealGeometryRecord, MuonNumberingRecord, DTAlignmentRcd, DTAlignmentErrorRcd, CSCAlignmentRcd, CSCAlignmentErrorRcd, GlobalPositionRcd, RPCRecoGeometryRcd, DTRecoGeometryRcd> > {};
 
 #endif
 
