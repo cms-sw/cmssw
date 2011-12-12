3157cea537f0e6a3c1b6147a1b6668cfbb9da5cd
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2011-12-12 13:31:44 +0000

    rollback changes on run selection

--- RecoLuminosity/LumiDB/python/lumiCorrections.py
+++ RecoLuminosity/LumiDB/python/lumiCorrections.py
@@ -131,8 +131,102 @@ def correctionsForRange(schema,inputRange):
             if runnum not in runs or result.has_key(runnum):
                 continue
             fillnum=cursor.currentRow()['fillnum'].data()
+            constfactor=1.141
             afterglow=1.0
             nonlinear=0.076
+            nonlinearPerBX=0.0
+            ncollidingbunches=0
+            if cursor.currentRow()['ncollidingbunches']:
+                ncollidingbunches=cursor.currentRow()['ncollidingbunches'].data()
+            fillscheme=''
+            if cursor.currentRow()['fillscheme']:
+                fillscheme=cursor.currentRow()['fillscheme'].data()
+            if fillscheme and len(fillscheme)!=0:
+                afterglow=afterglowByFillscheme(fillscheme,afterglows)
+            if ncollidingbunches and ncollidingbunches!=0:
+                nonlinearPerBX=float(1)/float(ncollidingbunches)
+            nonlinear=nonlinearPerBX*nonlinear
+            result[runnum]=(constfactor,afterglow,nonlinear)
+    except :
+        del qHandle
+        raise
+    del qHandle
+    for run in runs:
+        if run not in result.keys():
+            result[run]=(1.0,1.0,0.0) #those have no fillscheme 2011 runs
+    return result
+
+def correctionsForRangeV2(schema,inputRange):
+    '''
+    decide on the corrections to apply in the input range
+    select fillschemepattern,correctionfactor from fillscheme; 
+       [(fillschemepattern,afterglow),...]
+    select fillnum,runnum,fillscheme,ncollidingbunches,egev from cmsrunsummary where amodetag='PROTPYHS' and egev>3000
+        {runnum: (fillnum,fillscheme,ncollidingbunches),...}
+    output:
+        {runnum:(constantfactor,afterglowfactor,nonlinearfactor)}
+    '''
+    runs=[]
+    result={}
+    if isinstance(inputRange,str):
+        runs.append(int(inputRange))
+    else:
+        runs=inputRange
+    for r in runs:
+        if r<150008 :
+            result[r]=(1.0,1.0,0.0)
+    afterglows=[]
+    constfactor=1.141
+    s=nameDealer.fillschemeTableName()
+    r=nameDealer.cmsrunsummaryTableName()
+    qHandle=schema.newQuery()
+    try:
+        qHandle.addToTableList(s)
+        qResult=coral.AttributeList()
+        qResult.extend('FILLSCHEMEPATTERN','string')
+        qResult.extend('CORRECTIONFACTOR','float')
+        qHandle.defineOutput(qResult)
+        qHandle.addToOutputList('FILLSCHEMEPATTERN')
+        qHandle.addToOutputList('CORRECTIONFACTOR')
+        cursor=qHandle.execute()
+        while cursor.next():
+            fillschemePattern=cursor.currentRow()['FILLSCHEMEPATTERN'].data()
+            afterglowfac=cursor.currentRow()['CORRECTIONFACTOR'].data()
+            afterglows.append((fillschemePattern,afterglowfac))
+    except :
+        del qHandle
+        raise
+    del qHandle
+    qHandle=schema.newQuery()
+    try:
+        qHandle.addToTableList(r)
+        qHandle.addToOutputList('FILLNUM', 'fillnum')
+        qHandle.addToOutputList('RUNNUM', 'runnum')
+        qHandle.addToOutputList('FILLSCHEME','fillscheme')
+        qHandle.addToOutputList('NCOLLIDINGBUNCHES','ncollidingbunches')
+        qResult=coral.AttributeList()
+        qResult.extend('fillnum','unsigned int')
+        qResult.extend('runnum','unsigned int')
+        qResult.extend('fillscheme','string')
+        qResult.extend('ncollidingbunches','unsigned int')
+        qConditionStr='AMODETAG=:amodetag AND EGEV>=:egev'
+        qCondition=coral.AttributeList()
+        qCondition.extend('amodetag','string')
+        qCondition.extend('egev','unsigned int')
+        qCondition['amodetag'].setData('PROTPHYS')
+        qCondition['egev'].setData(3000)
+        qHandle.defineOutput(qResult)
+        qHandle.setCondition(qConditionStr,qCondition)
+        cursor=qHandle.execute()
+        while cursor.next():
+            runnum=cursor.currentRow()['runnum'].data()
+            #print 'runnum ',runnum 
+            if runnum not in runs or result.has_key(runnum):
+                continue
+            fillnum=cursor.currentRow()['fillnum'].data()
+            afterglow=1.0
+            nonlinear_1=0.063
+            nonlinear_2=-0.0037
             nonlinearPerBX=0.0
             ncollidingbunches=0
             if cursor.currentRow()['ncollidingbunches']:
