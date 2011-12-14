c7ef64830dae3e0e65a6ac8d874fa1057152ca19
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2011-12-14 18:11:12 +0000

    update GTs for 44X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_50_V7::All',
-             'startup' : 'START50_V7::All',
-             'com10'   : 'GR_R_50_V5::All',
-             'hltonline' : 'GR_R_50_V5::All', # same as com10 until H tag available
+autoCond = { 'mc'      : 'MC_50_V8::All',
+             'startup' : 'START50_V8::All',
+             'com10'   : 'GR_R_50_V6::All',
+             'hltonline' : 'GR_R_50_V6::All', # same as com10 until H tag available
              'starthi' : 'STARTHI50_V8::All'
              }
