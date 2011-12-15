452ea571f2f0be0f819c47c3cee451ad7e9e9523
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-12-15 11:25:01 +0000

    Allow counting upcrossings at zero

--- HiggsAnalysis/CombinedLimit/test/leeFromUpcrossings.py
+++ HiggsAnalysis/CombinedLimit/test/leeFromUpcrossings.py
@@ -11,6 +11,7 @@ argv.remove( '-b-' )
 from optparse import OptionParser
 parser = OptionParser(usage="usage: %prog [options] bands.root graph [min max]")
 parser.add_option("-l", "--level", dest="level", default=1, type="float",  help="Level to count upcrossings against")
+parser.add_option("-f", "--fit",   dest="fit",   default=None, type="string",  help="Name of the graph of the best fit signal strength, to count upcrossings at zero")
 (options, args) = parser.parse_args()
 if len(args) not in [2,4]:
     parser.print_usage()
@@ -20,25 +21,38 @@ file = ROOT.TFile(args[0]);
 if file == None: raise RuntimeError, "Cannot open %s" % args[0]
 
 graph = file.Get(args[1]);
-if graph == None: raise RuntimeError, "Cannot open %s" % args[0]
+if graph == None: raise RuntimeError, "Cannot find %s in %s" % (args[1], args[0])
 
+graphUp = graph
+if options.level == 0:
+    if options.fit == None: raise RuntimeError, "Must specify a the graph of fitted signal strength to count upcrossings at zero"
+    graphUp = file.Get(options.fit)
+    if graphUp == None: raise RuntimeError, "Cannot find %s in %s" % (options.fit, args[0])
+    
 (mhmin, mhmax) = (0, 999)
 if len(args) == 4: 
     mhmin = float(args[2])
     mhmax = float(args[3])
 
-n = graph.GetN()
-pvals = [ graph.GetY()[i] for i in range(n) if (graph.GetX()[i] >= mhmin and graph.GetX()[i] <= mhmax)]
+n = graph.GetN(); n2 = graphUp.GetN();
+pvals  = [ graph.GetY()[i] for i in range(n) if (graph.GetX()[i] >= mhmin and graph.GetX()[i] <= mhmax)]
+valsUp = [ graphUp.GetY()[i] for i in range(n) if (graphUp.GetX()[i] >= mhmin and graphUp.GetX()[i] <= mhmax)]
 #print "Selected ",len(pvals)," p-values."
 
-pupcross = ROOT.ROOT.Math.normal_cdf_c(options.level, 1.0)
-upcrossings = 0; pmin = pvals[-1];
-for i in range(len(pvals)-1):
-    if pvals[i] > pupcross and pvals[i+1] <= pupcross: 
-        upcrossings += 1
-    if pvals[i] < pmin: 
-        pmin = pvals[i]
+pmin = min(pvals);
 zmax = ROOT.ROOT.Math.normal_quantile_c(pmin, 1.0)
+
+upcrossings = 0; 
+if options.level > 0:
+    pupcross = ROOT.ROOT.Math.normal_cdf_c(options.level, 1.0)
+    for i in range(len(pvals)-1):
+        if pvals[i] > pupcross and pvals[i+1] <= pupcross: 
+            upcrossings += 1
+else:
+    pupcross = 0.5
+    for i in range(len(valsUp)-1):
+        if valsUp[i] <= options.level and valsUp[i+1] > options.level: 
+            upcrossings += 1
 
 expfactor = exp(-0.5*(zmax**2 - options.level**2))
 pglobal     = pmin + expfactor * upcrossings 
