520846884335ee5ff253c2c7e0c44e95d2ed80a6
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-12-16 17:07:19 +0000

    cleaned up

--- RecoPixelVertexing/PixelLowPtUtilities/interface/LowPtClusterShapeSeedComparitor.h
+++ RecoPixelVertexing/PixelLowPtUtilities/interface/LowPtClusterShapeSeedComparitor.h
@@ -4,15 +4,6 @@
 
 #include "RecoTracker/TkSeedingLayers/interface/SeedComparitor.h"
 
-#include "DataFormats/GeometryVector/interface/GlobalTag.h"
-#include "DataFormats/GeometryVector/interface/Vector2DBase.h"
-typedef Vector2DBase<float,GlobalTag> Global2DVector;
-
-//#include "DataFormats/GeometryVector/interface/LocalVector.h"
-#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
-#include "DataFormats/GeometryVector/interface/GlobalVector.h"
-
-#include <vector>
 
 namespace edm { class ParameterSet; class EventSetup; }
 
