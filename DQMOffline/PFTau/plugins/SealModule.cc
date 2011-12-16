c5ae7a6dc64298c146062b16c2d820ffda79c595
Lars Perchalla <sha1-33d4055c8560ac1d30535bc0038854133dd249fa@cern.ch>
2011-12-16 09:56:37 +0000

    deprecate PFTauDQM

--- DQMOffline/PFTau/plugins/SealModule.cc
+++ DQMOffline/PFTau/plugins/SealModule.cc
@@ -6,11 +6,9 @@
 #include "DQMOffline/PFTau/plugins/PFCandidateManagerAnalyzer.h"
 #include "DQMOffline/PFTau/plugins/MatchMETBenchmarkAnalyzer.h"
 #include "DQMOffline/PFTau/plugins/METBenchmarkAnalyzer.h"
-#include "DQMOffline/PFTau/plugins/PFTauDQM.h"
 
 DEFINE_FWK_MODULE (PFCandidateBenchmarkAnalyzer) ;
 DEFINE_FWK_MODULE (CandidateBenchmarkAnalyzer) ;
 DEFINE_FWK_MODULE (PFCandidateManagerAnalyzer) ;
 DEFINE_FWK_MODULE (METBenchmarkAnalyzer) ;
 DEFINE_FWK_MODULE (MatchMETBenchmarkAnalyzer) ;
-DEFINE_FWK_MODULE (PFTauDQM) ;
