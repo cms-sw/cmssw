00bfd1f8776e8ea7f7ce41c96edb5262c3e56b2a
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2011-12-19 14:41:20 +0000

    Add a private assignment operator.

--- DetectorDescription/RegressionTest/interface/DDHtmlFormatter.h
+++ DetectorDescription/RegressionTest/interface/DDHtmlFormatter.h
@@ -1,7 +1,6 @@
 #ifndef DDHtmlFormatter_h
 #define DDHtmlFormatter_h
 
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -11,7 +10,6 @@
 
 #include "DetectorDescription/RegressionTest/interface/DDErrorDetection.h"
 
-
 class DDHtmlFormatter;
 
 std::ostream & operator<<(std::ostream & o, const DDHtmlFormatter & f);
@@ -20,41 +18,44 @@ class DDHtmlFormatter
 {
 public:
  
- typedef std::map<std::string,std::set<std::string> > ns_type;
+  typedef std::map<std::string,std::set<std::string> > ns_type;
  
- explicit DDHtmlFormatter() { }
- DDHtmlFormatter(const DDHtmlFormatter & f) : os_(f.os_.str()) { }
+  explicit DDHtmlFormatter() { }
+  DDHtmlFormatter(const DDHtmlFormatter & f) : os_(f.os_.str()) { }
  
- DDHtmlFormatter header(const std::string & text, const std::string & style="../../style.css");
- DDHtmlFormatter footer();
+  DDHtmlFormatter header(const std::string & text, const std::string & style="../../style.css");
+  DDHtmlFormatter footer();
  
- DDHtmlFormatter br() { pre(); os_ << "<br>" << std::endl; return *this; }
- DDHtmlFormatter p(const std::string & content) { pre(); os_ << "<p>" << std::endl << content << std::endl << "</p>" << std::endl; return *this; }
+  DDHtmlFormatter br() { pre(); os_ << "<br>" << std::endl; return *this; }
+  DDHtmlFormatter p(const std::string & content) { pre(); os_ << "<p>" << std::endl << content << std::endl << "</p>" << std::endl; return *this; }
  
- DDHtmlFormatter ul() { pre(); os_ << "<ul>" << std::endl; return *this;}
- DDHtmlFormatter li(const std::string & content) { pre(); os_ << "<li>" << content << "</li>" << std::endl; return *this;}
- DDHtmlFormatter ulEnd() { pre(); os_ << "</ul>" << std::endl; return *this;}
+  DDHtmlFormatter ul() { pre(); os_ << "<ul>" << std::endl; return *this;}
+  DDHtmlFormatter li(const std::string & content) { pre(); os_ << "<li>" << content << "</li>" << std::endl; return *this;}
+  DDHtmlFormatter ulEnd() { pre(); os_ << "</ul>" << std::endl; return *this;}
  
- DDHtmlFormatter h1(const std::string & content) { pre(); os_ << "<h1>" << content << "</h1>" << std::endl; return *this;}
- DDHtmlFormatter h2(const std::string & content) { pre(); os_ << "<h2>" << content << "</h2>" << std::endl; return *this;}
- DDHtmlFormatter h3(const std::string & content) { pre(); os_ << "<h3>" << content << "</h3>" << std::endl; return *this;}
+  DDHtmlFormatter h1(const std::string & content) { pre(); os_ << "<h1>" << content << "</h1>" << std::endl; return *this;}
+  DDHtmlFormatter h2(const std::string & content) { pre(); os_ << "<h2>" << content << "</h2>" << std::endl; return *this;}
+  DDHtmlFormatter h3(const std::string & content) { pre(); os_ << "<h3>" << content << "</h3>" << std::endl; return *this;}
  
- DDHtmlFormatter link(const std::string & url, const std::string & text, const std::string & target="_self");
- std::string           lnk(const std::string & url, const std::string & text, const std::string & target="_self");
+  DDHtmlFormatter link(const std::string & url, const std::string & text, const std::string & target="_self");
+  std::string           lnk(const std::string & url, const std::string & text, const std::string & target="_self");
  
- DDHtmlFormatter table(int border=0){ pre(); os_ << "<table border=\"" << border << "\">" << std::endl; return *this;}
- DDHtmlFormatter tableEnd() { pre(); os_ << "</table>" << std::endl; return *this;}
- DDHtmlFormatter tr() { pre(); os_ << " <tr>" << std::endl; return *this;}
- DDHtmlFormatter trEnd() { pre(); os_ << " </tr>" << std::endl; return *this;}
- DDHtmlFormatter td(const std::string & content) { pre(); os_ << "  <td>" << content << std::endl << "  </td>" << std::endl; return *this;}
+  DDHtmlFormatter table(int border=0){ pre(); os_ << "<table border=\"" << border << "\">" << std::endl; return *this;}
+  DDHtmlFormatter tableEnd() { pre(); os_ << "</table>" << std::endl; return *this;}
+  DDHtmlFormatter tr() { pre(); os_ << " <tr>" << std::endl; return *this;}
+  DDHtmlFormatter trEnd() { pre(); os_ << " </tr>" << std::endl; return *this;}
+  DDHtmlFormatter td(const std::string & content) { pre(); os_ << "  <td>" << content << std::endl << "  </td>" << std::endl; return *this;}
  
- DDHtmlFormatter color(int red, int green, int blue){return *this;};
+  DDHtmlFormatter color(int red, int green, int blue){return *this;};
  
- void pre() { os_.str(""); }
+  void pre() { os_.str(""); }
  
-// std::string operator<<(std::string o) { o << os_; }
+  // std::string operator<<(std::string o) { o << os_; }
   mutable std::stringstream os_;
- 
+
+private:
+
+  DDHtmlFormatter& operator= ( const DDHtmlFormatter& );
 };
 
 
@@ -63,26 +64,26 @@ public:
 
 
 /** 
- Generates HTML for DD-namespaces 
+    Generates HTML for DD-namespaces 
 */
 class DDNsGenerator 
 {
  
 public:
- DDNsGenerator(std::ostream & os, 
-              const std::string & title, 
-              const std::string & target, 
-	       const ns_type & n, 
-	       const std::string & text="") 
- : os_(os), title_(title), text_(text), target_(target), n_(n){ }
+  DDNsGenerator(std::ostream & os, 
+		const std::string & title, 
+		const std::string & target, 
+		const ns_type & n, 
+		const std::string & text="") 
+    : os_(os), title_(title), text_(text), target_(target), n_(n){ }
  
- void doit();
+  void doit();
 
 private:
 
- std::ostream & os_;  
- std::string title_, text_, target_; 
- const ns_type & n_;
+  std::ostream & os_;  
+  std::string title_, text_, target_; 
+  const ns_type & n_;
 }; 
 
 class DDFrameGenerator
@@ -91,12 +92,12 @@ public:
   DDFrameGenerator(std::ostream & os,
                    const std::string & title,
                    const std::string & n1 = "_ns", //frame names
-		     const std::string & n2 = "_list",
-		     const std::string & n3 = "_details",
-		     const std::string & u1 = "ns.html", //url to be displayed in each frame
-		     const std::string & u2 = "list.html",
-		     const std::string & u3 = "details.html")
-   : t_(title), n1_(n1), n2_(n2), n3_(n3), u1_(u1), u2_(u2), u3_(u3), os_(os) { }
+		   const std::string & n2 = "_list",
+		   const std::string & n3 = "_details",
+		   const std::string & u1 = "ns.html", //url to be displayed in each frame
+		   const std::string & u2 = "list.html",
+		   const std::string & u3 = "details.html")
+    : t_(title), n1_(n1), n2_(n2), n3_(n3), u1_(u1), u2_(u2), u3_(u3), os_(os) { }
    
   void doit();   
 
