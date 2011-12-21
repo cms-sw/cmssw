f0b7456410b18230b79431c97eb99cb26bac64ad
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2011-12-21 08:39:04 +0000

    Drop set but unused variable.

--- L1Trigger/DTTrackFinder/src/L1MuDTSecProcMap.cc
+++ L1Trigger/DTTrackFinder/src/L1MuDTSecProcMap.cc
@@ -5,8 +5,8 @@
 //   Description: Sector Processor container 
 //
 //
-//   $Date: 2008/10/13 07:44:43 $
-//   $Revision: 1.3 $
+//   $Date: 2008/10/17 12:41:48 $
+//   $Revision: 1.4 $
 //
 //   Author :
 //   N. Neumeister             CERN EP
@@ -84,7 +84,7 @@ L1MuDTSectorProcessor* L1MuDTSecProcMap::sp(const L1MuDTSecProcId& id ) const {
 //
 void L1MuDTSecProcMap::insert(const L1MuDTSecProcId& id, L1MuDTSectorProcessor* sp)  { 
 
-  SPmap::const_iterator it = m_map.find(id);
+  //SPmap::const_iterator it = m_map.find(id);
   //  if ( it != m_map.end() ) 
     //    cerr << "Error: More than one Sector Processor with same identifier" 
     //         << endl;    
