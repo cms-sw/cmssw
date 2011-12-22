8bea603b1e42b02b1e65405be0d9620a1f9ecbdc
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2011-12-22 20:10:49 +0000

    removed leak

--- Alignment/CommonAlignmentMonitor/plugins/TrackerToMuonPropagator.cc
+++ Alignment/CommonAlignmentMonitor/plugins/TrackerToMuonPropagator.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Jim Pivarski
 //         Created:  Wed Dec 12 13:31:55 CST 2007
-// $Id: TrackerToMuonPropagator.cc,v 1.3 2008/05/06 10:47:05 pivarski Exp $
+// $Id: TrackerToMuonPropagator.cc,v 1.4 2010/01/04 15:36:54 mussgill Exp $
 //
 //
 
@@ -150,7 +150,7 @@ TrackerToMuonPropagator::produce(edm::Event& iEvent, const edm::EventSetup& iSet
    iSetup.get<GlobalTrackingGeometryRecord>().get(globalGeometry);
 
    // Create these factories once per event
-   TrajectoryStateTransform transformer;
+   
    MuonTransientTrackingRecHitBuilder muonTransBuilder;
 
    // Create a collection of Trajectories, to put in the Event
@@ -231,8 +231,8 @@ TrackerToMuonPropagator::produce(edm::Event& iEvent, const edm::EventSetup& iSet
 	 
       // if it has any successful extrapolations, make them into a Trajectory
       if (muonHits.size() > 0) {
-	 PTrajectoryStateOnDet *PTraj = transformer.persistentState(tracker_tsos, outerDetId.rawId());
-	 TrajectorySeed trajectorySeed(*PTraj, muonHits, alongMomentum);
+	 PTrajectoryStateOnDet const & PTraj = trajectoryStateTransform::persistentState(tracker_tsos, outerDetId.rawId());
+	 TrajectorySeed trajectorySeed(PTraj, muonHits, alongMomentum);
 	 Trajectory trajectory(trajectorySeed, alongMomentum);
 
 	 for (unsigned int i = 0;  i < muonHits.size();  i++) {
