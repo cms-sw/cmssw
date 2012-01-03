3832e92f8675794785271db8de3ffa6dbc531e01
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-01-03 18:14:28 +0000

    maps of vector removed, test added

--- RecoPixelVertexing/PixelLowPtUtilities/test/ClusterShapeHitFilter_t.cpp
+++ RecoPixelVertexing/PixelLowPtUtilities/test/ClusterShapeHitFilter_t.cpp
@@ -9,8 +9,8 @@ int main() {
 
   ClusterShapeHitFilter filter(nullptr,nullptr,nullptr,nullptr);
 
-  const float out 10e12;
-  const float eps 0.01;
+  const float out = 10e12;
+  const float eps = 0.01;
   std::cout << "dump strip limits" << std::endl;
   for (int i=0; i!=StripKeys::N+1; i++) {
     assert(!filter.stripLimits[i].isInside(out));
@@ -26,7 +26,7 @@ int main() {
     std::cout << std::endl;
   }
 
-  const std::pair<float,float> out1(out,out), out2(-out,-out)
+  const std::pair<float,float> out1(out,out), out2(-out,-out);
   std::cout << "\ndump pixel limits" << std::endl;
   for (int i=0; i!=PixelKeys::N+1; i++) {
     assert(!filter.pixelLimits[i].isInside(out1));
@@ -34,8 +34,8 @@ int main() {
     std::cout << i << ": ";
     float const * p = filter.pixelLimits[i].data[0][0];
     if (p[1]<1.e9) {
-      assert(filter.stripLimits[i].isInside(std::pair<float,float>(p[0]+eps,p[3]-eps)));
-      assert(filter.stripLimits[i].isInside(std::pair<float,float>(p[5]-eps,p[6]+eps)));
+      assert(filter.pixelLimits[i].isInside(std::pair<float,float>(p[0]+eps,p[3]-eps)));
+      assert(filter.pixelLimits[i].isInside(std::pair<float,float>(p[5]-eps,p[6]+eps)));
     }
     for (int j=0;j!=8; ++j)
       std::cout << p[j] << ", ";
