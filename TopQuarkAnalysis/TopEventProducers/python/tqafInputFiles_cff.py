756799865f45e65571eaac244461c5c77301c3b1
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-01-04 09:02:25 +0000

    Use CMSSW_5_0_0 rather than the CMSSW_5_0_0_pre6 since it seems the relvals are not registered in DAS anymore.

--- TopQuarkAnalysis/TopEventProducers/python/tqafInputFiles_cff.py
+++ TopQuarkAnalysis/TopEventProducers/python/tqafInputFiles_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
-relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_5_0_0_pre6',
+relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_5_0_0',
                                    relVal        = 'RelValProdTTbar',
                                    globalTag     = 'START50_V5',
                                    dataTier      = 'AODSIM',
