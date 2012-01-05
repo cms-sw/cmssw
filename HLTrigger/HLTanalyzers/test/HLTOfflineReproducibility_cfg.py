5bc8cd5ad8afe65835dc0dc3c86e954050765e0c
Juliette Alimena <sha1-8ce8f4c1f397c2d37216e2f844f3f59da506a691@cern.ch>
2012-01-05 10:38:21 +0000

    added GlobalTag info

--- HLTrigger/HLTanalyzers/test/HLTOfflineReproducibility_cfg.py
+++ HLTrigger/HLTanalyzers/test/HLTOfflineReproducibility_cfg.py
@@ -8,13 +8,18 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
-#INPUTFILE="input.root"
 INPUTFILE="rfio:/castor/cern.ch/user/j/jalimena/177139/HT/out_177139_HT_0.root"
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(INPUTFILE)
                             )
 
 process.load("HLTrigger.HLTanalyzers.hltOfflineReproducibility_cfi")
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi')
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = autoCond['hltonline']
+process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'
+process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
 
 OUTPUTFILE="./HLTOfflineReproducibility_177139HT_0.root"
 process.TFileService = cms.Service("TFileService",
