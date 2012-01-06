f9bfe0f10981f68a256abc7f36e6cd113677f6b7
Gavril Giurgiu <sha1-f6994d9f32e89a2dbb5f0337c9e5f8a2f598a299@cern.ch>
2012-01-06 16:20:24 +0000

    Add strip template reco parameters

--- RecoLocalTracker/SiStripRecHitConverter/python/StripCPEfromTrackAngle_cfi.py
+++ RecoLocalTracker/SiStripRecHitConverter/python/StripCPEfromTrackAngle_cfi.py
@@ -1,7 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 StripCPEfromTrackAngleESProducer = cms.ESProducer("StripCPEESProducer",
-                                                  ComponentName = cms.string('StripCPEfromTrackAngle')
-)
+                                                  ComponentName = cms.string('StripCPEfromTrackAngle'),
+                                                  UseTemplateReco            = cms.bool(False),
+                                                  TemplateRecoSpeed          = cms.int32(0),
+                                                  UseStripSplitClusterErrors = cms.bool(False)
+                                                  )
 
 
