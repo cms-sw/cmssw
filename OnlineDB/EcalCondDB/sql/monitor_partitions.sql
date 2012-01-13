9ece50bbe38c192f69a05c048f19cd8829be4475
Giovanni Organtini <sha1-b952f33b7475f865564a85bee68138ba94fc77ba@cern.ch>
2012-01-13 08:46:14 +0000

    Added informations on final table

--- OnlineDB/EcalCondDB/sql/monitor_partitions.sql
+++ OnlineDB/EcalCondDB/sql/monitor_partitions.sql
@@ -11,14 +11,37 @@ COLUMN max_extent                           HEADING 'Max|Extents'
 COLUMN pct_increase       FORMAT 999        HEADING '%|Inc'
 COLUMN partition_position FORMAT 9999       HEADING 'Part|Nmbr'
 COLUMN high_value         FORMAT a8         HEADING 'High|Value'
+COLUMN column_name        FORMAT a15        HEADING 'key'
+
+CREATE TABLE TEMP_TABLE (
+	TABLE_NAME VARCHAR2(35),
+	MAX INT
+)
+/
+
+SPOOL TEMP.sql
+set head off
+set echo off
+set feed off
+SELECT DISTINCT 'INSERT INTO TEMP_TABLE (TABLE_NAME, MAX) SELECT ''' || 
+	TABLE_NAME || ''' TABLE_NAME, COALESCE(MAX(' || 
+	COLUMN_NAME || '),0) MAX FROM ' || 
+	TABLE_NAME || ';' STATEMENT FROM USER_TAB_PARTITIONS P JOIN
+        USER_PART_KEY_COLUMNS C ON P.TABLE_NAME = C.NAME; 
+SPOOL OFF
+@TEMP
+
 SET LINES 230
-START title132 'Table Partition File Storage'
-SELECT
-     table_name,
+SET HEAD ON
+SET FEED ON
+SELECT 
+     p.table_name,
      tablespace_name,
      partition_name,
      partition_position,
+     column_name, 
      high_value,
+     t.max, 
      pct_free,
      pct_used,
      ini_trans,
@@ -27,6 +50,13 @@ SELECT
      next_extent,
      max_extent,
      pct_increase
-FROM USER_tab_partitions
+FROM USER_tab_partitions p JOIN USER_part_key_columns c ON 
+     p.TABLE_NAME = c.NAME JOIN TEMP_TABLE t ON p.TABLE_NAME =
+     t.TABLE_NAME
 ORDER BY table_name, partition_position
 /
+
+drop table TEMP_TABLE
+/
+
+
