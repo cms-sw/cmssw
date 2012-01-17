90bba6798bf76f43f327b0a526ef622ed172d4d4
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2012-01-17 11:51:50 +0000

    add the mixing configuration in DB

--- CondFormats/RunInfo/src/classes.h
+++ CondFormats/RunInfo/src/classes.h
@@ -2,10 +2,13 @@
 #include "CondFormats/RunInfo/interface/RunSummary.h"
 #include "CondFormats/RunInfo/interface/RunInfo.h"
 #include "CondFormats/RunInfo/interface/L1TriggerScaler.h"
+#include "CondFormats/RunInfo/interface/MixingModuleConfig.h"
 namespace {
   struct dictionary {
     std::vector<runinfo_test::RunNumber::Item>::iterator tmp0;
     std::vector<L1TriggerScaler::Lumi>::iterator tmp1;
     //  std::vector<RunSummary::Summary>::iterator tmp2;
+    MixingModuleConfig mmc;
+    MixingInputConfig mic;
   };
 }
