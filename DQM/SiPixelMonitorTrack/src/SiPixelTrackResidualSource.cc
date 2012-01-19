9a4467587020430a8bd51b905b3d0e5d83d22b73
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2012-01-19 11:26:26 +0000

    added missing line about TrajectoryStateTransform

--- DQM/SiPixelMonitorTrack/src/SiPixelTrackResidualSource.cc
+++ DQM/SiPixelMonitorTrack/src/SiPixelTrackResidualSource.cc
@@ -10,7 +10,7 @@
 // Original Author: Shan-Huei Chuang
 //         Created: Fri Mar 23 18:41:42 CET 2007
 //         Updated by Lukas Wehrli (plots for clusters on/off track added)
-// $Id: SiPixelTrackResidualSource.cc,v 1.20 2011/01/10 00:26:34 dlange Exp $
+// $Id: SiPixelTrackResidualSource.cc,v 1.21 2011/12/23 08:13:32 innocent Exp $
 
 
 #include <iostream>
@@ -530,8 +530,9 @@ void SiPixelTrackResidualSource::analyze(const edm::Event& iEvent, const edm::Ev
 
   for (TrackCandidateCollection::const_iterator tc = trackCandidateCollection->begin(); 
        tc!=trackCandidateCollection->end(); ++tc) {
+    TrajectoryStateTransform transformer;
     PTrajectoryStateOnDet tcPTSoD = tc->trajectoryStateOnDet();
-    TrajectoryStateOnSurface tcTSoS = trajectoryStateTransform::transientState(tcPTSoD, &(theTrackerGeometry->idToDet(tcPTSoD.detId())->surface()), 
+    TrajectoryStateOnSurface tcTSoS = transformer.transientState(tcPTSoD, &(theTrackerGeometry->idToDet(tcPTSoD.detId())->surface()), 
 						                 theMagneticField);
     const TrajectorySeed& tcSeed = tc->seed();
 
