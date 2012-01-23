5175af77a901ad4c46bf8b1ad5bc00f96bfac33c
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-01-23 15:49:43 +0000

    Add missing cfi files for plugins used in HLT

---
+++ TrackingTools/GsfTools/python/hltCloseComponentsMergerESProducer5D_cfi.py
@@ -0,0 +1,5 @@
+hltCloseComponentsMergerESProducer5D = cms.ESProducer("CloseComponentsMergerESProducer5D",
+    ComponentName = cms.string('hltESPCloseComponentsMerger5D'),
+    MaxComponents = cms.int32(12),
+    DistanceMeasure = cms.string('hltESPKullbackLeiblerDistance5D')
+)
---
+++ TrackingTools/GsfTools/python/hltDistanceBetweenComponentsESProducer5D_cfi.py
@@ -0,0 +1,4 @@
+hltDistanceBetweenComponentsESProducer5D = cms.ESProducer("DistanceBetweenComponentsESProducer5D",
+    ComponentName = cms.string('hltESPKullbackLeiblerDistance5D'),
+    DistanceMeasure = cms.string('KullbackLeibler')
+)
