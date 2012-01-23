f9015bd232a3326747e26c61ba7fadfa8d642d62
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-01-23 23:06:32 +0000

    Undo last delta, which was applied by mistake

--- IOPool/Input/test/poolsource_multiprocess_gen_file_cfg.py
+++ IOPool/Input/test/poolsource_multiprocess_gen_file_cfg.py
@@ -14,8 +14,8 @@ process.source = cms.Source("EmptySource",
     firstRun = cms.untracked.uint32(100),
     numberEventsInRun = cms.untracked.uint32(5),
     numberEventsInLuminosityBlock = cms.untracked.uint32(2),
-    firstTime = cms.untracked.uint32(1000),
-    timeBetweenEvents = cms.untracked.uint32(10)
+    firstTime = cms.untracked.uint64(1000),
+    timeBetweenEvents = cms.untracked.uint64(10)
 )
 
 process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string("multiprocess_test.root"))
--- IOPool/Input/test/poolsource_multiprocess_gen_file_oneRun_cfg.py
+++ IOPool/Input/test/poolsource_multiprocess_gen_file_oneRun_cfg.py
@@ -13,8 +13,8 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("EmptySource",
     firstRun = cms.untracked.uint32(100),
     numberEventsInRun = cms.untracked.uint32(500),
-    firstTime = cms.untracked.uint32(1000),
-    timeBetweenEvents = cms.untracked.uint32(10)
+    firstTime = cms.untracked.uint64(1000),
+    timeBetweenEvents = cms.untracked.uint64(10)
 )
 
 process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string("multiprocess_oneRun_test.root"))
