664ddc502eee76ab9d786942ca6f10dd05f9a4a8
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2012-01-24 19:28:50 +0000

    sync back the latest dev: same as rev 1.5

--- SimGeneral/MixingModule/python/mixPoolSource_cfi.py
+++ SimGeneral/MixingModule/python/mixPoolSource_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-#FileNames = cms.untracked.vstring('dataset:/RelValMinBias/CMSSW_4_4_2_patch10-START42_V1-v1/GEN-SIM-DIGI-RAW-HLTDEBUG')
+#FileNames = cms.untracked.vstring('dataset:/RelValMinBias/CMSSW_4_2_0_pre4-START42_V1-v1/GEN-SIM-DIGI-RAW-HLTDEBUG')
 
-FileNames =  cms.untracked.vstring('/store/relval/CMSSW_4_4_0/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START44_V5-v2/0044/9AA7696D-D3E5-E011-937C-003048678FF6.root',
-       '/store/relval/CMSSW_4_4_0/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START44_V5-v2/0044/F065CC04-D4E5-E011-BFB3-0026189438BA.root',
-       '/store/relval/CMSSW_4_4_0/RelValMinBias/GEN-SIM-DIGI-RAW-HLTDEBUG/START44_V5-v2/0044/FA7E5F05-D7E5-E011-8A3F-002618943800.root')
+FileNames = cms.untracked.vstring('/store/relval/CMSSW_5_0_0_pre6/RelValProdMinBias/GEN-SIM-RAW/START50_V5-v1/0195/1AD9E627-7316-E111-B3A5-001A9281173C.root',
+                             '/store/relval/CMSSW_5_0_0_pre6/RelValProdMinBias/GEN-SIM-RAW/START50_V5-v1/0196/0477EED1-7516-E111-B834-0018F3D0962E.root',
+                             '/store/relval/CMSSW_5_0_0_pre6/RelValProdMinBias/GEN-SIM-RAW/START50_V5-v1/0197/3E1F5CF3-DB16-E111-A7C7-001A928116CE.root')
