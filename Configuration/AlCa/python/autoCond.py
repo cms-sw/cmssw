00597865c9a4e36bad7f9d1baa25ff32e29e9b9e
Chiara Ilaria Rovelli <sha1-156f87976672d5574743371779e65c95d251b39f@cern.ch>
2012-01-24 13:43:22 +0000

    new GTs for data and MC

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_50_V9::All',
-             'startup' : 'START50_V9::All',
-             'com10'   : 'GR_R_50_V7::All',
-             'hltonline' : 'GR_R_50_V7::All', # same as com10 until H tag available
+autoCond = { 'mc'      : 'MC_50_V13::All',
+             'startup' : 'START50_V13::All',
+             'com10'   : 'GR_R_50_V10::All',
+             'hltonline' : 'GR_R_50_V10::All', # same as com10 until H tag available
              'starthi' : 'STARTHI50_V10::All'
              }
