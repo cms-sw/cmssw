9377cc0a4b02dbdb08c1dfd4773e775046bd94d7
Tae Jeong Kim <sha1-32b43f0d0700c58cd86e2e757255aa5f208dff55@cern.ch>
2012-01-25 19:36:02 +0000

    change the particle-flow collection name to particleFlow as default particleFlowTmp only works in Reco sequence

--- PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
@@ -3,6 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from CommonTools.ParticleFlow.ParticleSelectors.pfSortByType_cff import *
 from CommonTools.ParticleFlow.pfNoPileUpIso_cff  import *
 
+pfPileUpIso.PFCandidates = 'particleFlow'
+pfNoPileUpIso.bottomCollection='particleFlow'
+
 patPFCandidateIsoDepositSelection = cms.Sequence(
        pfNoPileUpIsoSequence +
        pfSortByTypeSequence
