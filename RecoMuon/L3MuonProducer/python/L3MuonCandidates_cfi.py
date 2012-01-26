a507e8e230cd2daaff1ac648f9e19a29e2f28542
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-01-26 12:07:35 +0000

    Add missing parametrs for HLT

--- RecoMuon/L3MuonProducer/python/L3MuonCandidates_cfi.py
+++ RecoMuon/L3MuonProducer/python/L3MuonCandidates_cfi.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 L3MuonCandidates = cms.EDProducer("L3MuonCandidateProducer",
-    InputObjects = cms.InputTag("L3Muons")
+    InputObjects = cms.InputTag("L3Muons"),
+    InputLinksObjects = cms.InputTag(""),
+    MuonPtOption = cms.string("Global")
 )
-
-
-
