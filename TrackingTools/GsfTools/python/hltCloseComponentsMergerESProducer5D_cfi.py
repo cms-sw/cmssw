64eb3270b2fb2ba56e42a974240ac748f51edca8
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-01-27 10:49:39 +0000

    Fix of cfi files

--- TrackingTools/GsfTools/python/hltCloseComponentsMergerESProducer5D_cfi.py
+++ TrackingTools/GsfTools/python/hltCloseComponentsMergerESProducer5D_cfi.py
@@ -1,3 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
 hltCloseComponentsMergerESProducer5D = cms.ESProducer("CloseComponentsMergerESProducer5D",
     ComponentName = cms.string('hltESPCloseComponentsMerger5D'),
     MaxComponents = cms.int32(12),
--- TrackingTools/GsfTools/python/hltDistanceBetweenComponentsESProducer5D_cfi.py
+++ TrackingTools/GsfTools/python/hltDistanceBetweenComponentsESProducer5D_cfi.py
@@ -1,3 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
 hltDistanceBetweenComponentsESProducer5D = cms.ESProducer("DistanceBetweenComponentsESProducer5D",
     ComponentName = cms.string('hltESPKullbackLeiblerDistance5D'),
     DistanceMeasure = cms.string('KullbackLeibler')
