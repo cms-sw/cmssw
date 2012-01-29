adabd304a260c353ed3fabf07a9a49ddb5ccb366
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2012-01-29 15:00:40 +0000

    HI Iterative tracking sequence

---
+++ RecoHI/HiTracking/python/hiIterTracking_cff.py
@@ -0,0 +1,11 @@
+from RecoHI.HiTracking.hiSecondPixelTripletStep_cff import *
+from RecoHI.HiTracking.hiMixedTripletStep_cff import *
+from RecoHI.HiTracking.hiPixelPairStep_cff import *
+from RecoHI.HiTracking.MergeTrackCollectionsHI_cff import *
+
+hiIterTracking = cms.Sequence(
+    hiSecondPixelTripletStep
+    *hiMixedTripletStep
+    *hiPixelPairStep
+    *hiGeneralTracks
+)
