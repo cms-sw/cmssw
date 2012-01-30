231c4b2e303e4c918090540d46712eea70cfe076
Chiara Ilaria Rovelli <sha1-156f87976672d5574743371779e65c95d251b39f@cern.ch>
2012-01-30 09:20:44 +0000

    update to MC HI GT for 50X serie

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -5,5 +5,5 @@ autoCond = { 'mc'      : 'MC_50_V13::All',
              'startup' : 'START50_V13::All',
              'com10'   : 'GR_R_50_V11::All',
              'hltonline' : 'GR_R_50_V11::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI50_V10::All'
+             'starthi' : 'STARTHI50_V13::All'
              }
