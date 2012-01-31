c7edc0e1741b95f04daa65fa8e3d70f3af1fc9dd
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2012-01-31 20:55:04 +0000

    Reflected eta subtraction

--- RecoHI/HiJetAlgos/plugins/plugins.cc
+++ RecoHI/HiJetAlgos/plugins/plugins.cc
@@ -2,6 +2,8 @@
 #include "RecoHI/HiJetAlgos/interface/MultipleAlgoIterator.h"
 #include "RecoHI/HiJetAlgos/interface/JetOffsetCorrector.h"
 #include "RecoHI/HiJetAlgos/interface/ParametrizedSubtractor.h"
+#include "RecoHI/HiJetAlgos/interface/ReflectedIterator.h"
+
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ModuleFactory.h"
@@ -10,6 +12,7 @@
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,JetOffsetCorrector,"JetOffsetCorrector");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,MultipleAlgoIterator,"MultipleAlgoIterator");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ParametrizedSubtractor,"ParametrizedSubtractor");
+DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ReflectedIterator,"ReflectedIterator");
 
 
 
