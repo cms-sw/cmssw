1c16e5da4885448ede005ff46caf307f148bb47d
Phat Srimanobhas <sha1-6706f3f9508602eabb6b9686f7dc9bb750600853@cern.ch>
2012-01-31 11:53:36 +0000

    1st PFEFs filter config file

---
+++ HLTrigger/JetMET/python/hltPFEnergyFractionsFilter_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+hltPFEnergyFractionsFilter = cms.EDFilter("HLTPFEnergyFractionsFilter",
+    inputPFJetTag   = cms.InputTag( "hltAntiKT5PFJets" ),
+    inputCaloJetTag = cms.InputTag( "hltAntiKT5ConvPFJets" ),
+    saveTags = cms.bool( False ),
+    nJet     = cms.uint32(1),
+    min_CEEF = cms.double( -99. ),
+    max_CEEF = cms.double( 99. ),
+    min_CHEF = cms.double( -99. ),
+    max_CHEF = cms.double( 99. ),
+    min_NEEF = cms.double( -99. ),
+    max_NEEF = cms.double( 99. ),
+    min_NHEF = cms.double( -99. ),
+    max_NHEF = cms.double( 99. )
+)
+
