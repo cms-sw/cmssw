ee769d013b178b92bacc5baf8506730464bbc614
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2012-02-07 16:06:39 +0000

    add installation utility

---
+++ TauAnalysis/MCEmbeddingTools/test/rhEmbedder/install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+wd=`pwd`
+targetDir=$CMSSW_BASE/src/
+cp DetAssoc.patch $targetDir
+cd $targetDir
+addpkg TrackingTools/TrackAssociator
+patch -p0 -i DetAssoc.patch
+
+scramv1 b -j 12
