2a75b4426c6231ff7ef3035f75912455acf5e483
Ernest Aguilo Chivite <sha1-442618130c9378a8db4a60b192e812931505d3c7@cern.ch>
2012-02-08 23:21:45 +0000

    forgot the _cfi.py's

--- HLTrigger/btau/python/hltmmkkFilter_cfi.py
+++ HLTrigger/btau/python/hltmmkkFilter_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hltmmkkFilter = cms.EDFilter("HLTmmkkFilter",
     MinCosinePointingAngle = cms.double(0.9),
     MinLxySignificance = cms.double(3.0),
+    MinD0Significance = cms.double(0.0),
     MinPt = cms.double(3.0),
     MaxEta = cms.double(2.5),
     ThirdTrackMass = cms.double(0.106),
