812f31a44b35352683ad5bdd259c8809a4f6ef66
Hongxuan Liu <sha1-b70f13a6c621c4c85f2bad0c1693be1e66e48d4f@cern.ch>
2012-02-08 07:14:06 +0000

    temporary solution for hcal laser filter...

--- RecoMET/METFilters/python/hcalLaserEventFilter_cfi.py
+++ RecoMET/METFilters/python/hcalLaserEventFilter_cfi.py
@@ -1,19 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 try:
-    from badEvents import badEvents
+    from hcalLaserBadEvents_2011 import badEvents
 except:
     print "<hcalLaserEventFilter_cfi> WARNING!  No explicit list 'badEvents.py' containing bad HCAL laser run/events was found!"
     badEvents=[]
 
 hcalLaserEventFilter = cms.EDFilter("HcalLaserEventFilter",
-                                    vetoByRunEventNumber=cms.untracked.bool(False),
+                                    vetoByRunEventNumber=cms.untracked.bool(True),
                                     # Add each bad event as run #, event # in this list
                                     BadRunEventNumbers=cms.untracked.vuint32(badEvents
                                                                              # badEvents file contains 218 bad events, as of 28 Nov. 2011
 
     ),
-                                    vetoByHBHEOccupancy=cms.untracked.bool(True),
+                                    vetoByHBHEOccupancy=cms.untracked.bool(False),
                                     minOccupiedHBHE=cms.untracked.uint32(5000), # minimum number of HBHErechits that must be present for HBHEOccupancy filter to remove event
                                     
                                     debug = cms.untracked.bool(False),
