4b108e99d82d5ba70af9c854be129b85a942a7d2
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-02-09 09:26:06 +0000

    Add PFJet variants of CaloJet plugins for HLT (incl. cfi files)

---
+++ CommonTools/RecoAlgos/plugins/EtMinPFJetSelector.cc
@@ -0,0 +1,18 @@
+/* \class EtMinCaloJetSelector
+ *
+ * selects calo-jet above a minumum Et cut
+ *
+ * \author: Luca Lista, INFN
+ *
+ */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CommonTools/UtilAlgos/interface/EtMinSelector.h"
+#include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+
+ typedef SingleObjectSelector<
+           reco::PFJetCollection, 
+           EtMinSelector
+         > EtMinPFJetSelector;
+
+DEFINE_FWK_MODULE( EtMinPFJetSelector );
