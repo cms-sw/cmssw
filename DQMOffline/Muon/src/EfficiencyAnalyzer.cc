c1dccf75c6c5e5c4914ee5caae466465d7296b45
Alicia Calderon Tazon <sha1-20ab221e5492c4f4230acf63e8589e0f442d026b@cern.ch>
2012-02-10 12:33:42 +0000

    bugs corrected

--- DQMOffline/Muon/src/EfficiencyAnalyzer.cc
+++ DQMOffline/Muon/src/EfficiencyAnalyzer.cc
@@ -117,20 +117,12 @@ void EfficiencyAnalyzer::analyze(const edm::Event & iEvent,const edm::EventSetup
 
   // Loop on muon collection
   TLorentzVector Mu1, Mu2;
-<<<<<<< EfficiencyAnalyzer.cc
-
 
   bool isMB = false;
   bool isME = false;
-=======
-
-  bool isMB = false;
-  bool isME = false;
->>>>>>> 1.3
 
   for (reco::MuonCollection::const_iterator recoMu1 = muons->begin(); recoMu1!=muons->end(); ++recoMu1) {
 
-<<<<<<< EfficiencyAnalyzer.cc
     LogTrace(metname)<<"[EfficiencyAnalyzer] loop over first muons" << endl;
 
     //--- Define combined isolation
@@ -215,106 +207,3 @@ void EfficiencyAnalyzer::analyze(const edm::Event & iEvent,const edm::EventSetup
 
 
 
-
-
-/*
-    // Loop on second muons to fill invariant mass plots
-    for (reco::MuonCollection::const_iterator recoMu2 = recoMu1; recoMu2!=muons->end(); ++recoMu2){ 
-      LogTrace(metname)<<"[EfficiencyAnalyzer] loop over 2nd muon"<<endl;
-      if (recoMu1==recoMu2) continue;
-      
-      // Global-Global Muon
-      if (!recoMu1->isGlobalMuon()) continue;
-      if (!recoMu2->isGlobalMuon()) continue;
-      
-      reco::TrackRef recoCombinedGlbTrack1 = recoMu1->combinedMuon();
-      reco::TrackRef recoCombinedGlbTrack2 = recoMu2->combinedMuon();
-=======
-    LogTrace(metname)<<"[EfficiencyAnalyzer] loop over first muons" << endl;
-
-    //--- Define combined isolation
-    reco::MuonIsolation Iso_muon = recoMu1->isolationR03();
-    float combIso = (Iso_muon.emEt + Iso_muon.hadEt + Iso_muon.sumPt);  
-    
-    //--- Is Global Muon 
-    if (!recoMu1->isGlobalMuon()) continue;
-
-      // get the track combinig the information from both the Tracker and the Spectrometer
-      reco::TrackRef recoCombinedGlbTrack1 = recoMu1->combinedMuon();    
-      float muPt1 = recoCombinedGlbTrack1->pt();
->>>>>>> 1.3
-      Mu1.SetPxPyPzE(recoCombinedGlbTrack1->px(), recoCombinedGlbTrack1->py(),recoCombinedGlbTrack1->pz(), recoCombinedGlbTrack1->p());
-      
-
-      //--- Define if it is a tight muon
-      if (recoMu1->isGlobalMuon() && recoMu1->isTrackerMuon() && recoMu1->combinedMuon()->normalizedChi2()<10. 
-	  && recoMu1->combinedMuon()->hitPattern().numberOfValidMuonHits()>0 && fabs(recoMu1->combinedMuon()->dxy(beamSpot.position()))<0.2 && recoMu1->combinedMuon()->hitPattern().numberOfValidPixelHits()>0 && recoMu1->numberOfMatches() > 1) {
-
-	//-- is isolated muon
-	if (muPt1 > 15  && (combIso/muPt1) < 0.1 ) {
-
-
-	  for (reco::MuonCollection::const_iterator recoMu2 = muons->begin(); recoMu2!=muons->end(); ++recoMu2){ 
-	  
-	    LogTrace(metname)<<"[EfficiencyAnalyzer] loop over second muon" <<endl;
-	  
-	    if (recoMu2 == recoMu1) continue;
-
-	    
-	    if (recoMu2->eta() < 1.479 )  isMB = true;
-	    if (recoMu2->eta() >= 1.479 ) isME = true;
-
-
-	    //--> should we apply track quality cuts??? 
-	    Mu2.SetPxPyPzE(recoMu2->px(), recoMu2->py(), recoMu2->pz(), recoMu2->p());
-
-	    if (!recoMu2->isTrackerMuon()) continue;
-
-	    if ( recoMu2->pt() < 5 ) continue;
-
-	    if ( (recoMu1->charge())*(recoMu2->charge()) > 0 ) continue; 
-
-	    float Minv = (Mu1+Mu2).M();
-	    
-	    if ( Minv < 70 ||  Minv > 110 ) continue; 
-
-	    h_allProbes_pt->Fill(recoMu2->pt());
-	    h_allProbes_eta->Fill(recoMu2->eta());
-	    h_allProbes_phi->Fill(recoMu2->phi());
-
-
-	    if (isMB) h_allProbes_barrel_pt->Fill(recoMu2->pt());
-	    if (isME) h_allProbes_endcap_pt->Fill(recoMu2->pt());
-	    if(recoMu2->pt() > 20 ) h_allProbes_hp_eta->Fill(recoMu2->eta());
-
-
-	    test_TightMu_Minv->Fill(Minv);
-
-	 
-	    // Probes passing the tight muon criteria 
-
-	    if (recoMu2->isGlobalMuon() && recoMu2->isTrackerMuon() && recoMu2->combinedMuon()->normalizedChi2()<10. && recoMu2->combinedMuon()->hitPattern().numberOfValidMuonHits()>0 && fabs(recoMu2->combinedMuon()->dxy(beamSpot.position()))<0.2 && recoMu2->combinedMuon()->hitPattern().numberOfValidPixelHits()>0 && recoMu2->numberOfMatches() > 1) { 
-		 
-		 h_passProbes_TightMu_pt->Fill(recoMu2->pt());
-		 h_passProbes_TightMu_eta->Fill(recoMu2->eta());
-		 h_passProbes_TightMu_phi->Fill(recoMu2->phi());
-
-		 if (isMB) h_passProbes_TightMu_barrel_pt->Fill(recoMu2->pt());
-		 if (isME) h_passProbes_TightMu_endcap_pt->Fill(recoMu2->pt());
-		 if( recoMu2->pt() > 20 ) h_passProbes_TightMu_hp_eta->Fill(recoMu2->eta());
-       
-	    }
-	  }
-	      
-	}
-      }
-  }
-}
-
-<<<<<<< EfficiencyAnalyzer.cc
-// }
-*/
-=======
-
-
->>>>>>> 1.3
