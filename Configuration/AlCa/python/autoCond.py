963bc0cb208981383a2098789dc6419c7531c653
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-02-12 12:13:23 +0000

    new offline GT

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -3,7 +3,7 @@
 
 autoCond = { 'mc'      : 'MC_52_V1::All',
              'startup' : 'START52_V1::All',
-             'com10'   : 'GR_R_52_V1::All',
-             'hltonline' : 'GR_R_52_V1::All', # same as com10 until H tag available
+             'com10'   : 'GR_R_52_V2::All',
+             'hltonline' : 'GR_R_52_V2::All', # same as com10 until H tag available
              'starthi' : 'STARTHI52_V1::All'
              }
