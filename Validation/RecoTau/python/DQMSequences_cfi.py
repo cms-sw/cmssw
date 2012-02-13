9da45b5bc5fb1e823945be7328401bd821b36624
Mauro Verzetti <sha1-a72272ea14fe5ac21105bbc72f1ae068f99bb5e2@cern.ch>
2012-02-13 15:20:48 +0000

    removing unused exports

--- Validation/RecoTau/python/DQMSequences_cfi.py
+++ Validation/RecoTau/python/DQMSequences_cfi.py
@@ -1,12 +1,6 @@
-from Validation.RecoTau.dataTypes.ValidateTausOnQCD_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealData_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealElectronsData_cff import *
 from Validation.RecoTau.dataTypes.ValidateTausOnRealMuonsData_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZEEFastSim_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZEE_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZMM_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZTTFastSim_cff import *
-from Validation.RecoTau.dataTypes.ValidateTausOnZTT_cff import *
 
 pfTauRunDQMValidation = cms.Sequence(
     TauValNumeratorAndDenominatorRealData+
@@ -26,6 +20,24 @@ runTauEff = cms.Sequence(
     efficienciesRealMuonsData+
     normalizePlotsRealMuonsData
     )
+
+runTauEffSingleMu = cms.Sequence(
+        efficienciesRealMuonsData+
+            efficienciesRealData+
+            normalizePlotsRealMuonsData
+            )
+
+runTauEffJet = cms.Sequence(
+        TauEfficienciesRealData
+            )
+
+runTauEffMutiJet = runTauEffJet
+
+runTauEffSingleE = cms.Sequence(
+        produceDenominatorRealElectronsData
+            )
+runTauEffTausPlusE = runTauEffSingleE
+
 ##Full sequences, including normalizations
 ## TauEfficienciesRealData+
 ## TauEfficienciesRealElectronsData+
