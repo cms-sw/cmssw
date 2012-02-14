3a354406f81e510867171db3ba2ca1ea260c4a0b
Juliette Alimena <sha1-8ce8f4c1f397c2d37216e2f844f3f59da506a691@cern.ch>
2012-02-14 13:43:12 +0000

    sr #126250 DoubleMu_Aco path done

--- HLTrigger/Configuration/python/HLTrigger_Datasets_cff.py
+++ HLTrigger/Configuration/python/HLTrigger_Datasets_cff.py
@@ -1,4 +1,4 @@
-# /dev/CMSSW_5_1_0/HIon/V30
+# /dev/CMSSW_5_1_0/HIon/V31
 
 import FWCore.ParameterSet.Config as cms
 
--- HLTrigger/Configuration/test/OnData_HLT_GRun.py
+++ HLTrigger/Configuration/test/OnData_HLT_GRun.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_5_1_0/GRun/V30 (CMSSW_5_2_0_pre4_HLT9)
+# /dev/CMSSW_5_1_0/GRun/V31 (CMSSW_5_2_0_pre4_HLT9)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_1_0/GRun/V30')
+  tableName = cms.string('/dev/CMSSW_5_1_0/GRun/V31')
 )
 
 process.streams = cms.PSet( 
