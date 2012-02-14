5577e1794b163475998374ccd01398a5ac09322a
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2012-02-14 08:44:27 +0000

    added option to leave out HF

--- RecoHI/HiJetAlgos/python/ParticleTowerProducer_cff.py
+++ RecoHI/HiJetAlgos/python/ParticleTowerProducer_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 particleTowerProducer = cms.EDProducer('ParticleTowerProducer',
-                                          src    = cms.InputTag('particleFlowTmp')
-                                          )
+                                       src    = cms.InputTag('particleFlowTmp'),
+                                       useHF = cms.untracked.bool(False)
+                                       )
 
