76182de92b2cf7615ac4820ae759f311944caae6
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-02-15 11:05:16 +0000

    Back to V00-03-00.

--- RecoHI/HiEvtPlaneAlgos/python/RecoHiEvtPlane_EventContent_cff.py
+++ RecoHI/HiEvtPlaneAlgos/python/RecoHiEvtPlane_EventContent_cff.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
 RecoHiEvtPlaneFEVT = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*')
+    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*',
+                                           'keep *_hiEvtPlaneFlat_*_*',
+                                           )
     )
 
 RecoHiEvtPlaneRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*')
+    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*',
+                                           'keep *_hiEvtPlaneFlat_*_*',                                           
+                                           )
     )
 
 RecoHiEvtPlaneAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*')
+    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*',
+                                           'keep *_hiEvtPlaneFlat_*_*',                                           
+                                           )
     )
