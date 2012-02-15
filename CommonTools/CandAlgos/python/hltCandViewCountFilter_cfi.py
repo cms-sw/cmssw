048f9b869d6b62e663325f785146a15710fc9875
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-02-15 09:38:38 +0000

    Fix typo

--- CommonTools/CandAlgos/python/hltCandViewCountFilter_cfi.py
+++ CommonTools/CandAlgos/python/hltCandViewCountFilter_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 hltCandViewCountFilter = cms.EDFilter("CandViewCountFilter",
-   src    = cms.InputTag("hltCollection"),
-   minNumber = cms.unit32(0)
+   src       = cms.InputTag("hltCollection"),
+   minNumber = cms.uint32(0)
 )
