52ddd7e503fd22f8ebb6a5489f61394eb54ebaa9
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2012-02-18 09:10:19 +0000

    Change default to useHF=true

--- RecoHI/HiJetAlgos/python/ParticleTowerProducer_cff.py
+++ RecoHI/HiJetAlgos/python/ParticleTowerProducer_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 
 particleTowerProducer = cms.EDProducer('ParticleTowerProducer',
                                        src    = cms.InputTag('particleFlowTmp'),
-                                       useHF = cms.untracked.bool(False)
+                                       useHF = cms.untracked.bool(True)
                                        )
 
