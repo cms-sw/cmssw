68656444409d64c233ffeca360f99741ac3911da
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-02-20 16:57:32 +0000

    New GTs for 52X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_50_V15A::All',
-             'startup' : 'START50_V15A::All',
-             'com10'   : 'GR_R_50_V12::All',
-             'hltonline' : 'GR_R_50_V12::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI50_V15::All'
+autoCond = { 'mc'      : 'MC_52_V2::All',
+             'startup' : 'START52_V2::All',
+             'com10'   : 'GR_R_52_V3::All',
+             'hltonline' : 'GR_R_52_V3::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI52_V2::All'
              }
