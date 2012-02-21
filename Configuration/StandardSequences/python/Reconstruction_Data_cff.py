84f873b747c3512e51727a4c9f5c339c71b8cfd8
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2012-02-21 10:01:09 +0000

    use the new hcal modifier method

--- Configuration/StandardSequences/python/Reconstruction_Data_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_Data_cff.py
@@ -5,8 +5,10 @@ from Configuration.StandardSequences.Reconstruction_cff import *
 particleFlowRecHitHCAL.LongShortFibre_Cut = 30.
 particleFlowRecHitHCAL.ApplyPulseDPG = True
 
-hcalRecAlgos.SeverityLevels[3].RecHitFlags.remove("HFDigiTime")
-hcalRecAlgos.SeverityLevels[4].RecHitFlags.append("HFDigiTime")
+import RecoLocalCalo.HcalRecAlgos.RemoveAddSevLevel as HcalRemoveAddSevLevel
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFDigiTime",11)
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHEFlatNoise",12)
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHESpikeNoise",12)
 
 CSCHaloData.ExpectedBX = cms.int32(3)
 
