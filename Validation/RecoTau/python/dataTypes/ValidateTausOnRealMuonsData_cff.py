edc18478f29798748b28b35a6d4a01fc7283e432
Riccardo Manzoni <sha1-36fd93e3eb4fc1ea4592b1bad6463bcf5262b0e4@cern.ch>
2012-02-22 21:22:01 +0000

    Zmumu tag and probe stuff added

--- Validation/RecoTau/python/dataTypes/ValidateTausOnRealMuonsData_cff.py
+++ Validation/RecoTau/python/dataTypes/ValidateTausOnRealMuonsData_cff.py
@@ -5,17 +5,56 @@ import copy
 from RecoJets.Configuration.RecoPFJets_cff import *
 import PhysicsTools.PatAlgos.tools.helpers as helpers
 
-kinematicSelectedTauValDenominatorRealMuonsData = cms.EDFilter( ##FIXME: this should be a filter
-   "TauValMuonSelector", #"GenJetSelector"
-   src = cms.InputTag("muons"),
-   cut = cms.string(kinematicSelectedTauValDenominatorCut.value()+' && isGlobalMuon && isTrackerMuon && isPFIsolationValid && (pfIsolationR04.sumChargedParticlePt + max(pfIsolationR04.sumPhotonEt + pfIsolationR04.sumNeutralHadronEt - 0.0729*pfIsolationR04.sumPUPt,0.0) )/pt < 0.25'),#cms.string('pt > 5. && abs(eta) < 2.5'), #Defined: Validation.RecoTau.RecoTauValidation_cfi 
-   filter = cms.bool(False)
-)
+selectedMuons = cms.EDFilter("MuonSelector",
+    src = cms.InputTag('muons'),
+    cut = cms.string("pt > 20.0 && abs(eta) < 2.1 && isGlobalMuon = 1 && isTrackerMuon = 1 && abs(innerTrack().dxy) < 2.0 && abs(innerTrack().dz) < 24."),
+    filter = cms.bool(False)
+	)
+
+selectedMuonsIso = cms.EDFilter("MuonSelector",
+    src = cms.InputTag('selectedMuons'),
+    cut = cms.string('(isolationR03().emEt + isolationR03().hadEt + isolationR03().sumPt)/pt < 0.25'),
+    filter = cms.bool(False)
+	)    
+
+from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
+
+goodTracks = cms.EDFilter("TrackSelector",
+    src = cms.InputTag("generalTracks"), 
+    cut = cms.string("pt > 5 && abs(eta) < 2.5 && abs(dxy) < 2.0 && abs(dz) < 24."),
+    #cut = cms.string("pt > 0.5 "),
+    filter = cms.bool(False)
+	)
+
+trackCands  = cms.EDProducer("ConcreteChargedCandidateProducer", 
+    src  = cms.InputTag("goodTracks"),      
+    particleType = cms.string("mu+")     # this is needed to define a mass
+	)
+
+ZmmCandMuonTrack = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay = cms.string("selectedMuonsIso@+ trackCands@-"), # it takes opposite sign collection, no matter if +- or -+
+    cut   = cms.string("60 < mass < 120")
+	)
+
+BestZ = cms.EDProducer("BestMassZArbitrationProducer", # returns the Z with mass closest to 91.18 GeV
+	ZCandidateCollection = cms.InputTag("ZmmCandMuonTrack")
+	)
+
+ZLegs  = cms.EDProducer("CollectionFromZLegProducer", 
+    ZCandidateCollection  = cms.InputTag("BestZ"),      
+	)
+
+# kinematicSelectedTauValDenominatorRealMuonsData = cms.EDFilter( ##FIXME: this should be a filter
+#    "TauValMuonSelector", #"GenJetSelector"
+#    src = cms.InputTag("muons"),
+#    cut = cms.string(kinematicSelectedTauValDenominatorCut.value()+' && isGlobalMuon && isTrackerMuon && isPFIsolationValid && (pfIsolationR04.sumChargedParticlePt + max(pfIsolationR04.sumPhotonEt + pfIsolationR04.sumNeutralHadronEt - 0.0729*pfIsolationR04.sumPUPt,0.0) )/pt < 0.25'),#cms.string('pt > 5. && abs(eta) < 2.5'), #Defined: Validation.RecoTau.RecoTauValidation_cfi 
+#    filter = cms.bool(False)
+# )
 
 procAttributes = dir(proc) #Takes a snapshot of what there in the process
 helpers.cloneProcessingSnippet( proc, proc.TauValNumeratorAndDenominator, 'RealMuonsData') #clones the sequence inside the process with RealMuonsData postfix
 helpers.cloneProcessingSnippet( proc, proc.TauEfficiencies, 'RealMuonsData') #clones the sequence inside the process with RealMuonsData postfix
-helpers.massSearchReplaceAnyInputTag(proc.TauValNumeratorAndDenominatorRealMuonsData, 'kinematicSelectedTauValDenominator', 'kinematicSelectedTauValDenominatorRealMuonsData') #sets the correct input tag
+helpers.massSearchReplaceAnyInputTag(proc.TauValNumeratorAndDenominatorRealMuonsData, 'kinematicSelectedTauValDenominator',  cms.InputTag("ZLegs","theProbeLeg")) #sets the correct input tag
 
 #adds to TauValNumeratorAndDenominator modules in the sequence RealMuonsData to the extention name
 zttLabeler = lambda module : SetValidationExtention(module, 'RealMuonsData')
@@ -32,9 +71,22 @@ newProcAttributes = filter( lambda x: (x not in procAttributes) and (x.find('Rea
 for newAttr in newProcAttributes:
     locals()[newAttr] = getattr(proc,newAttr)
 
-produceDenominatorRealMuonsData = cms.Sequence(
-      kinematicSelectedTauValDenominatorRealMuonsData
-      )
+produceDenominatorRealMuonsData = cms.Sequence( ( ( selectedMuons * selectedMuonsIso ) +
+                                                ( goodTracks * trackCands ) ) *
+						                        ZmmCandMuonTrack *
+						                        BestZ *
+						                        ZLegs 
+						                      )
+
+#################################    
+# produceDenominatorRealMuonsData = cms.Sequence(
+#       kinematicSelectedTauValDenominatorRealMuonsData +
+#       #goodMuons *
+#       goodTracks +
+#       (tagMuons + probeMuons)*
+#       ZCandidates
+#       #trackCands *
+#       )
 
 produceDenominator = produceDenominatorRealMuonsData
 
