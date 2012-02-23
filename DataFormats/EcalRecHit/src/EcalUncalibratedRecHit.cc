877bb9b0721823c0a8155fa71484376cb5172f0c
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-02-23 18:54:16 +0000

    initialize all values

--- DataFormats/EcalRecHit/src/EcalUncalibratedRecHit.cc
+++ DataFormats/EcalRecHit/src/EcalUncalibratedRecHit.cc
@@ -3,11 +3,11 @@
 #include <math.h>
 
 EcalUncalibratedRecHit::EcalUncalibratedRecHit() :
-     amplitude_(0.), pedestal_(0.), jitter_(0.), chi2_(10000.), flags_(0), aux_(0) { }
+     amplitude_(0.), pedestal_(0.), jitter_(0.), chi2_(10000.), OOTamplitude_(0.), OOTchi2_(10000.), flags_(0), aux_(0) { }
 
 EcalUncalibratedRecHit::EcalUncalibratedRecHit(const DetId& id, float ampl, float ped,
                           float jit, float chi2, uint32_t flags, uint32_t aux) :
-     amplitude_(ampl), pedestal_(ped), jitter_(jit), chi2_(chi2), flags_(flags), aux_(aux), id_(id) { }
+     amplitude_(ampl), pedestal_(ped), jitter_(jit), chi2_(chi2), OOTamplitude_(0.), OOTchi2_(10000.), flags_(flags), aux_(aux), id_(id) { }
 
 EcalUncalibratedRecHit::~EcalUncalibratedRecHit() {
 }
