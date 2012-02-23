cb7e759742408a26321ccf2ceacdca1bcc53c83f
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2012-02-23 16:28:04 +0000

    Fixing mistake in filtered jet configuration in subjet colleciton name

--- RecoJets/JetProducers/python/ak5PFJetsFiltered_cfi.py
+++ RecoJets/JetProducers/python/ak5PFJetsFiltered_cfi.py
@@ -9,13 +9,16 @@ ak5PFJetsFiltered = ak5PFJets.clone(
     nFilt = cms.int32(3),
     rFilt = cms.double(0.3),
     useExplicitGhosts = cms.bool(True),
-    writeCompound = cms.bool(True)
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
     )
 
 
 ak5PFJetsMassDropFiltered = ak5PFJetsFiltered.clone(
     useMassDropTagger = cms.bool(True),
     muCut = cms.double(0.667),
-    yCut = cms.double(0.08)
+    yCut = cms.double(0.08),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
     )
 
