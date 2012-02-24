5d2dcff3a4dca23adfa8c1f1a4d4f20707f935a9
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2012-02-24 16:16:22 +0000

    Not to miss the problem...

--- PhysicsTools/PatAlgos/test/patTuple_addJets_cfg.py
+++ PhysicsTools/PatAlgos/test/patTuple_addJets_cfg.py
@@ -22,7 +22,7 @@ from PhysicsTools.PatAlgos.tools.jetTools import *
 #                 jetCorrLabel = ('AK5JPT', cms.vstring(['L1Offset', 'L1JPTOffset', 'L2Relative', 'L3Absolute'])),
 #                 doType1MET   = False,
 #                 doL1Cleaning = False,
-#                 doL1Counters = True,                 
+#                 doL1Counters = True,
 #                 genJetCollection = cms.InputTag("ak5GenJets"),
 #                 doJetID      = True,
 #                 jetIdLabel   = "ak5"
@@ -35,7 +35,7 @@ addJetCollection(process,cms.InputTag('ak7CaloJets'),
                  doBTagging   = False,
                  jetCorrLabel = ('AK7Calo', cms.vstring(['L1Offset', 'L2Relative', 'L3Absolute'])),
                  doType1MET   = True,
-                 doL1Cleaning = True,                 
+                 doL1Cleaning = True,
                  doL1Counters = False,
                  genJetCollection=cms.InputTag("ak7GenJets"),
                  doJetID      = True,
@@ -49,7 +49,7 @@ addJetCollection(process,cms.InputTag('kt4CaloJets'),
                  doBTagging   = True,
                  jetCorrLabel = ('KT4Calo', cms.vstring(['L2Relative', 'L3Absolute'])),
                  doType1MET   = True,
-                 doL1Cleaning = True,                 
+                 doL1Cleaning = True,
                  doL1Counters = False,
                  genJetCollection=cms.InputTag("kt4GenJets"),
                  doJetID      = True,
@@ -63,7 +63,7 @@ addJetCollection(process,cms.InputTag('kt4CaloJets'),
 #                 doBTagging   = False,
 #                 jetCorrLabel = ('KT6Calo', cms.vstring(['L2Relative', 'L3Absolute'])),
 #                 doType1MET   = False,
-#                 doL1Cleaning = True,                 
+#                 doL1Cleaning = True,
 #                 doL1Counters = False,
 #                 genJetCollection=cms.InputTag("kt6GenJets"),
 #                 doJetID      = True,
@@ -74,7 +74,7 @@ addJetCollection(process,cms.InputTag('kt4CaloJets'),
 switchJetCollection(process,cms.InputTag('ak5PFJets'),
                  doJTA        = True,
                  doBTagging   = True,
-                 jetCorrLabel = ('AK5PF', cms.vstring(['L1Offset', 'L2Relative', 'L3Absolute'])),
+                 jetCorrLabel = ('AK5PF', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute'])),
                  doType1MET   = True,
                  genJetCollection=cms.InputTag("ak5GenJets"),
                  doJetID      = True
@@ -98,11 +98,11 @@ process.p = cms.Path(
 #    '/store/relval/CMSSW_3_8_6/RelValTTbar/GEN-SIM-RECO/START38_V13-v1/0065/F438C4C4-BCE7-DF11-BC6B-002618943885.root'
 #   ]                                     ##  (e.g. 'file:AOD.root')
 #                                         ##
-process.maxEvents.input = 10              ##  (e.g. -1 to run on all events)
+process.maxEvents.input = 100             ##  (e.g. -1 to run on all events)
 #                                         ##
 #   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
 #                                         ##
 #   process.out.fileName = ...            ##  (e.g. 'myTuple.root')
 #                                         ##
-process.options.wantSummary = False       ##  (to suppress the long output at the end of the job)    
+process.options.wantSummary = False       ##  (to suppress the long output at the end of the job)
 
