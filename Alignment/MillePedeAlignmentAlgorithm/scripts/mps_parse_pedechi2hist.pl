abb741834c5fc428117533c28bf9409bd04f9c49
Joerg Behr <sha1-6fc71cbfef80ca4fef4a27bd037b04a2b7c5d6f8@cern.ch>
2012-02-24 16:12:51 +0000

    bug fix regarding the plotting tool for the <chi2/ndf> plot

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_parse_pedechi2hist.pl
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_parse_pedechi2hist.pl
@@ -2,11 +2,11 @@
 
 # Author: Joerg Behr
 #
-# This script reads the histogram file produced by pede and it extracts the plot showing the average chi2/ndf per Mille binary number.
-# After reading the MPS library, for which the file name has to be provided as first argument, an output file (name chi2pedehis.txt) is produced
-# where the first column corresponds to the associated name, the second column corresponds to Mille binary number, and the last column
-# is <chi2/ndf>. As second argument this scripts expects the file name of the pede histogram file (usually millepede.his). The last argument
-# represents the location of the python config which is used by CMSSW.
+# This script reads the histogram file produced by Pede and it extracts the plot showing the average chi2/ndf per Mille binary number.
+# After reading the MPS library, for which the file name has to be provided as first argument, an output file called chi2pedehis.txt is produced
+# where the first column corresponds to the associated name, the second column corresponds to the Mille binary number, and the last column
+# is equal to <chi2/ndf>. As second argument this scripts expects the file name of the Pede histogram file -- usually millepede.his. The last argument
+# represents the location of the Python config which was used by CMSSW.
 #
 # Standalone usage:
 # mps_parse_pedechi2hist.pl mps.db millepede.his $RUNDIR/alignment_merge.py
@@ -22,30 +22,30 @@ my $pyconfig = $ARGV[2];
 
 if(!defined $ARGV[0] || !defined $ARGV[1] || !defined $ARGV[2])
   {
-    print "The location of the mps.db file, the path to the jobData directory and the python config for CMSSW has to be specified.\n";
-    exit(-1);
+    print "The location of the mps.db and Pede histogram file, and the Python config for CMSSW have to be specified.\n";
+    exit 0;
   }
 
 unless(-e "$db")
   {
     print "Could not find mps.db file: $db\n";
-    exit(-1);
+    exit 0;
   }
 
 unless(-e "$pedehis")
   {
     print "Could not find pedehis file: $pedehis\n";
-    exit(-1);
+    exit 0;
   }
 
 unless(-e "$pyconfig")
   {
-    print "Could not find python config file: $pyconfig\n";
-    exit(-1);
+    print "Could not find Python config file: $pyconfig\n";
+    exit 0;
   }
 
 
-open IN, "< $db" || die "error when opening mps.db file: $!";
+open IN, "< $db" || die "error while opening mps.db file: $!";
 my @adb = <IN>;
 close IN;
 
@@ -67,7 +67,7 @@ foreach (@adb)
     if($nrow != 13)
       {
         print "There seems to be a problem reading the file mps.db. The number of fields per row is $nrow but it should be equal to 13.\n";
-        exit(-1);
+        exit 0;
       }
     next if(!defined $row[0] || !defined $row[1]);
     next if($row[0] =~ /\D/g);
@@ -77,6 +77,7 @@ foreach (@adb)
     next if($jobid =~ /\D/g);
     next if($jobid != $row[0]);
     my $name = pop @row;
+
     chomp $name;
     next if(!defined $name);
     next if($name eq "");
@@ -87,43 +88,33 @@ foreach (@adb)
 
 if(@names == 0)
   {
-    print "Names were not associated to Mille binaries (Use mps_setup.pl -N blabla ... for that purpose). Chi2ndf plot is not produced.\n";
+    print "Names were not associated to Mille binaries (Use mps_setup.pl -N blabla ... for that purpose). Chi2ndf plot is not produced. exit.\n";
     exit(-1);
   }
 else
   {
     #now try to guess the used binaries. extract the file number of those
-    open INPY, "< $pyconfig" || die "error when opening alignment_merge.py: $!";
+    open INPY, "< $pyconfig" || die "error while opening alignment_merge.py: $!\n";
     my @pycontent = <INPY>;
     close INPY;
     chomp @pycontent;
 
-    my %usedbinaries;
-    my $checkincreasing = 0;
+    my @usedbinaries;
     foreach (@pycontent)
       {
         next unless(/milleBinary/i);
         if(my ($i) = /milleBinary(.+?)\.dat/i)
           {
-            $usedbinaries{int scalar $i} = 1;
-            if(int scalar $i > $checkincreasing)
-              {
-                $checkincreasing = int scalar $i;
-              }
-            else
-              {
-                print "Error: inconsistent Mille binary list. exit.\n";
-                exit(-1);
-              }
+            push @usedbinaries, int scalar $i;
           }
       }
     #read pede hists
-    open INPEDEHIS, "< $pedehis" || die "error when opening pedehis file: $!";
+    open INPEDEHIS, "< $pedehis" || die "error while opening pedehis file: $!\n";
     my @ph = <INPEDEHIS>;
     close INPEDEHIS;
     chomp @ph;
     my $foundchi2start = 0;
-    my %hisdata;
+    my @hisdata;
     for(my $i =0; $i < @ph; $i++)
       {
         my $line = $ph[$i];
@@ -137,37 +128,47 @@ else
         if($foundchi2start)
           {
             my @tmp = split " ", $line;
-            $hisdata{int scalar $tmp[0]} = $tmp[1];
+            $hisdata[int scalar $tmp[0] - 1] = $tmp[1];
           }
       }
+    if(scalar @hisdata != scalar @usedbinaries)
+      {
+        print "The number of used binaries is " . scalar @usedbinaries . " whereas in contrast, however, the <chi2/ndf> histogram in Pede has " . scalar @hisdata . " bins (Pede version >= rev92 might help if #bins < #binaries). exit.\n";
+        exit 0;
+      }
+
     my $outname = "chi2pedehis.txt";
     unlink "$outname" if(-e "$outname");
-    open OUTPUT, "> $outname" || die "error when opening file for writing: $!";
-    #for(my $i = 1; $i<= $lastid; $i++)
-    foreach my $i (keys %usedbinaries)
-      {
-        my $found = 0;
-        foreach my $j (@ids)
-          {
-            $found = 1 if($i == $j);
-          }
-        if(!$found)
-          {
-            print "Problem parsing the mps.db file. Could not find job $i. Maybe only a fraction of binaries was named?\n";
-            exit(-1);
-          }
-        else
-          {
-            #everything seems to be ok so far ...
-            my $tmp1 = $names[$i-1];
-            my $tmp2 =  $ids[$i-1];
-            #it could be that someone has assigned names only to a fraction of all binaries. Thus, check whether $tmp1 and $tmp2 are defined.
-            my $t = int scalar $tmp2;
-            if(defined $hisdata{$t} && defined $tmp1 && defined $tmp2)
-              {
-                print OUTPUT "$tmp1 $tmp2 $hisdata{$t}\n";
-              }
-          }
-      }
+    open OUTPUT, "> $outname" || die "error while opening file for writing: $!\n";
+
+    for(my $i = 1; $i <= @usedbinaries; $i++)
+     {
+       my $found = 0;
+       my $name;
+       my $index = -1;
+       foreach my $j (@ids)
+         {
+           if($usedbinaries[$i-1] == $j)
+             {
+               $found = 1;
+               $name  = $names[$j-1];
+               $index = $j;
+               last;
+             }
+         }
+       if(!$found)
+         {
+           print "Problem parsing the mps.db file. Could not find job $i. Maybe only a fraction of binaries was named?\n";
+           exit 0;
+         }
+       else
+         {
+           my $tmp2 =  $ids[$index-1];
+           if(defined $hisdata[$i-1] && defined $name && defined $tmp2)
+             {
+               print OUTPUT "$name $tmp2 " . $hisdata[$i-1] . "\n";
+             }
+         }
+     }
     close OUTPUT;
   }
