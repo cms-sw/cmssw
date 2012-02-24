616931333cf93a512fea1cdbc6d456cc4ed4861b
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2012-02-24 11:03:45 +0000

    replacing START50_V5 by START50_V8

--- TopQuarkAnalysis/TopEventProducers/python/tqafInputFiles_cff.py
+++ TopQuarkAnalysis/TopEventProducers/python/tqafInputFiles_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_5_0_0',
                                    relVal        = 'RelValProdTTbar',
-                                   globalTag     = 'START50_V5',
+                                   globalTag     = 'START50_V8',
                                    dataTier      = 'AODSIM',
                                    maxVersions   = 1
                                    )
