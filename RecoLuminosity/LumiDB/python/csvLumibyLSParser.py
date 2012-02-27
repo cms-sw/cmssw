fe611567a49ae9650e2a24f75b98529c6afd7a26
Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch>
2012-02-27 16:17:31 +0000

    small fix for lumibyls format with HLT conditions

--- RecoLuminosity/LumiDB/python/csvLumibyLSParser.py
+++ RecoLuminosity/LumiDB/python/csvLumibyLSParser.py
@@ -1,3 +1,6 @@
+# Note: this is specifically to parse a .csv file generated from a command like
+# pixelLumiCalc.py lumibyls -i json_DCSONLY_pp.txt --hltpath "HLT_Photon75_CaloIdVL_IsoL*" -o myHLTtest.out
+# format: Run,LS,HLTpath,L1bit,HLTpresc,L1presc,Recorded(/ub),Effective(/ub)
 import csv
 import re
 def is_intstr(s):
@@ -21,9 +24,7 @@ class csvLumibyLSParser(object):
         for row in csvReader:
             field0=str(row[0]).strip()
             try:
-                field1=str(row[1]).strip()
-                fieldsplit=re.split(':',field1)
-                lsstring = fieldsplit[0]
+                lsstring=str(row[1]).strip()
             except Exception,e:
                 lsstring='1' # for list with run number only, fake lsnum
             if not is_intstr(field0) or not  is_intstr(lsstring):
@@ -42,7 +43,7 @@ class csvLumibyLSParser(object):
                     oldRun = runnumber
 
             try:
-                delivered, recorded = float( row[5] ), float( row[6] )
+                delivered, recorded = float( row[6] ), float( row[7] )
             except:
                 print 'Record not parsed, Run = %d, LS = %d' % (runnumber, lsnumber)                
 
