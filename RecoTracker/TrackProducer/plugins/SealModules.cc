aa4019f8b3c6e2f85c33adbfdf89f92e8b1ba283
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2012-02-27 18:55:55 +0000

    add module to save light hit collection from highpurity tracks

--- RecoTracker/TrackProducer/plugins/SealModules.cc
+++ RecoTracker/TrackProducer/plugins/SealModules.cc
@@ -4,9 +4,11 @@
 #include "RecoTracker/TrackProducer/plugins/TrackRefitter.h"
 #include "RecoTracker/TrackProducer/plugins/GsfTrackProducer.h"
 #include "RecoTracker/TrackProducer/plugins/GsfTrackRefitter.h"
+#include "RecoTracker/TrackProducer/plugins/ExtraFromSeeds.h"
 
 // 
 DEFINE_FWK_MODULE(TrackProducer);
 DEFINE_FWK_MODULE(TrackRefitter);
 DEFINE_FWK_MODULE(GsfTrackProducer);
 DEFINE_FWK_MODULE(GsfTrackRefitter);
+DEFINE_FWK_MODULE(ExtraFromSeeds);
