576c5a5cc7cff76147652bdeb40fe59f047d9239
Jared Sturdy <sha1-db53c096984bc383f729541321a2e7ec9133743e@cern.ch>
2012-02-27 13:20:59 +0000

    fixed jetTools.py when sequence is improperly modified

--- PhysicsTools/PatAlgos/python/tools/jetTools.py
+++ PhysicsTools/PatAlgos/python/tools/jetTools.py
@@ -829,9 +829,6 @@ class SwitchJetCollection(ConfigToolBase):
 
                     ## add MET corrections to sequence
                     getattr(process, 'patMETs'+ postfix).metSource = cms.InputTag('pfType1CorrectedMet'+postfix)
-                    getattr(process,'patDefaultSequence'+postfix).replace(getattr(process,'patJetCorrFactors'+postfix),
-                                                                          getattr(process,'kt6PFJets'+postfix)
-                                                                          *getattr(process,'patJetCorrFactors'+postfix))
                     getattr(process,'producePFMETCorrections'+postfix).remove(getattr(process,'kt6PFJets'))#,            getattr(process,'kt6PFJets'            +postfix))
                     getattr(process,'producePFMETCorrections'+postfix).remove(getattr(process,'ak5PFJets'))#,            getattr(process,'ak5PFJets'            +postfix))
                     getattr(process,'producePFMETCorrections'+postfix).replace(getattr(process,'pfCandsNotInJet'),      getattr(process,'pfCandsNotInJet'      +postfix))
@@ -844,6 +841,10 @@ class SwitchJetCollection(ConfigToolBase):
                     getattr(process,"patDefaultSequence"+postfix).replace( getattr(process,'patMETs'+postfix),
                                                                            getattr(process,'producePFMETCorrections'+postfix)
                                                                            *getattr(process,'patMETs'+postfix) )
+
+            getattr(process,'patDefaultSequence'+postfix).replace(getattr(process,'patJetCorrFactors'+postfix),
+                                                                  getattr(process,'kt6PFJets'+postfix)
+                                                                  *getattr(process,'patJetCorrFactors'+postfix))
         else:
             ## remove the jetCorrFactors from the std sequence
             process.patJetMETCorrections.remove(process.patJetCorrFactors)
