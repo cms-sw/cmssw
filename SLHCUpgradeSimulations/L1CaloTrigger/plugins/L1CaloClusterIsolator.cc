557933b6875d6fd3ff54ce6ffb5fb82d74390aa4
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2012-02-28 14:19:45 +0000

    Whitespace only

--- SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1CaloClusterIsolator.cc
+++ SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1CaloClusterIsolator.cc
@@ -7,14 +7,14 @@
 class L1CaloClusterIsolator:public L1CaloAlgoBase < l1slhc::L1CaloClusterCollection , l1slhc::L1CaloClusterCollection >
 {
   public:
-	L1CaloClusterIsolator( const edm::ParameterSet & );
-	 ~L1CaloClusterIsolator(  );
+    L1CaloClusterIsolator( const edm::ParameterSet & );
+    ~L1CaloClusterIsolator(  );
 
-	void algorithm( const int &, const int & );
-//	void initialize(  );
+    void algorithm( const int &, const int & );
+    //	void initialize(  );
 
   private:
-	bool isoLookupTable( const int& clusters, const int& aCoeffA, const int& aCoeffB, const int& E );
+    bool isoLookupTable( const int& clusters, const int& aCoeffA, const int& aCoeffB, const int& E );
 
 };
 
@@ -25,12 +25,12 @@ class L1CaloClusterIsolator:public L1CaloAlgoBase < l1slhc::L1CaloClusterCollect
 
 
 L1CaloClusterIsolator::L1CaloClusterIsolator( const edm::ParameterSet & aConfig ):
-L1CaloAlgoBase < l1slhc::L1CaloClusterCollection , l1slhc::L1CaloClusterCollection > ( aConfig )
+  L1CaloAlgoBase < l1slhc::L1CaloClusterCollection , l1slhc::L1CaloClusterCollection > ( aConfig )
 {
-// mPhiOffset = 0; 
-mEtaOffset = -1;
-//mPhiIncrement = 1; 
-//mEtaIncrement = 1;
+  // mPhiOffset = 0; 
+  mEtaOffset = -1;
+  //mPhiIncrement = 1; 
+  //mEtaIncrement = 1;
 }
 
 
@@ -39,69 +39,69 @@ L1CaloClusterIsolator::~L1CaloClusterIsolator(  )
 }
 
 /*
-void L1CaloClusterIsolator::initialize(  )
-{
-}
-*/
+   void L1CaloClusterIsolator::initialize(  )
+   {
+   }
+   */
 
 void L1CaloClusterIsolator::algorithm( const int &aEta, const int &aPhi )
 {
 
-		// Look if there is a cluster here and if the cluster is central (not pruned)
+  // Look if there is a cluster here and if the cluster is central (not pruned)
 
-			l1slhc::L1CaloClusterCollection::const_iterator lClusterItr = fetch( aEta, aPhi );
-			if ( lClusterItr != mInputCollection->end(  ) )
-			{
+  l1slhc::L1CaloClusterCollection::const_iterator lClusterItr = fetch( aEta, aPhi );
+  if ( lClusterItr != mInputCollection->end(  ) )
+  {
 
-				if ( lClusterItr->isCentral(  ) )
-				{
-					int lEgammaClusterCount= 0;
-					int lTauClusterCount = 0;
-					l1slhc::L1CaloCluster lIsolatedCluster( *lClusterItr );
+    if ( lClusterItr->isCentral(  ) )
+    {
+      int lEgammaClusterCount= 0;
+      int lTauClusterCount = 0;
+      l1slhc::L1CaloCluster lIsolatedCluster( *lClusterItr );
 
-					// There is a cluster here:Calculate isoDeposits
-					for ( int lPhi = aPhi - mCaloTriggerSetup->nIsoTowers(  ); lPhi <= aPhi + mCaloTriggerSetup->nIsoTowers(  ) + 1; ++lPhi )
-					{
-						for ( int lEta = aEta - mCaloTriggerSetup->nIsoTowers(  ); lEta <= aEta + mCaloTriggerSetup->nIsoTowers(  ) + 1; ++lEta )
-						{
-							if ( !( lEta == aEta && lPhi == aPhi ) )
-							{
-								// If neighbor exists
-								l1slhc::L1CaloClusterCollection::const_iterator lNeighbourItr = fetch( lEta, lPhi );
-								if ( lNeighbourItr != mInputCollection->end(  ) )
-								{
-									if ( lNeighbourItr->E(  ) >= mCaloTriggerSetup->isoThr(0) )
-									{
-										lEgammaClusterCount++;
-									}
-									if ( lNeighbourItr->E(  ) >= mCaloTriggerSetup->isoThr(1) )
-									{
-										lTauClusterCount++;
-									}
-								}
-							}
-						}
-					}
-
-
-					lIsolatedCluster.setIsoClusters( lEgammaClusterCount, lTauClusterCount );
+      // There is a cluster here:Calculate isoDeposits
+      for ( int lPhi = aPhi - mCaloTriggerSetup->nIsoTowers(  ); lPhi <= aPhi + mCaloTriggerSetup->nIsoTowers(  ) + 1; ++lPhi )
+      {
+        for ( int lEta = aEta - mCaloTriggerSetup->nIsoTowers(  ); lEta <= aEta + mCaloTriggerSetup->nIsoTowers(  ) + 1; ++lEta )
+        {
+          if ( !( lEta == aEta && lPhi == aPhi ) )
+          {
+            // If neighbor exists
+            l1slhc::L1CaloClusterCollection::const_iterator lNeighbourItr = fetch( lEta, lPhi );
+            if ( lNeighbourItr != mInputCollection->end(  ) )
+            {
+              if ( lNeighbourItr->E(  ) >= mCaloTriggerSetup->isoThr(0) )
+              {
+                lEgammaClusterCount++;
+              }
+              if ( lNeighbourItr->E(  ) >= mCaloTriggerSetup->isoThr(1) )
+              {
+                lTauClusterCount++;
+              }
+            }
+          }
+        }
+      }
 
 
-					// Calculate Bits Tau isolation / electron Isolation
-					if ( isoLookupTable( lEgammaClusterCount, mCaloTriggerSetup->isolationE(0), mCaloTriggerSetup->isolationE(1), lIsolatedCluster.E(  ) ) )
-					{
-						lIsolatedCluster.setIsoEG( true );
-					}
+      lIsolatedCluster.setIsoClusters( lEgammaClusterCount, lTauClusterCount );
 
-					// Add the LUT inputs 
 
-					if ( isoLookupTable( lTauClusterCount, mCaloTriggerSetup->isolationT(0), mCaloTriggerSetup->isolationT(1), lIsolatedCluster.E(  ) ) )
-					{
-						lIsolatedCluster.setIsoTau( true );
-					}
-					mOutputCollection->insert( lIsolatedCluster.iEta(  ) , lIsolatedCluster.iPhi() , lIsolatedCluster );
-				}
-			}
+      // Calculate Bits Tau isolation / electron Isolation
+      if ( isoLookupTable( lEgammaClusterCount, mCaloTriggerSetup->isolationE(0), mCaloTriggerSetup->isolationE(1), lIsolatedCluster.E(  ) ) )
+      {
+        lIsolatedCluster.setIsoEG( true );
+      }
+
+      // Add the LUT inputs 
+
+      if ( isoLookupTable( lTauClusterCount, mCaloTriggerSetup->isolationT(0), mCaloTriggerSetup->isolationT(1), lIsolatedCluster.E(  ) ) )
+      {
+        lIsolatedCluster.setIsoTau( true );
+      }
+      mOutputCollection->insert( lIsolatedCluster.iEta(  ) , lIsolatedCluster.iPhi() , lIsolatedCluster );
+    }
+  }
 
 
 }
@@ -110,14 +110,14 @@ void L1CaloClusterIsolator::algorithm( const int &aEta, const int &aPhi )
 
 bool L1CaloClusterIsolator::isoLookupTable( const int& aClusters, const int& aCoeffA, const int& aCoeffB, const int& aE )	// takes as input the # Clusters the isolation coefficients
 {
-	if( aE < 0 ) return false;
-	if( aE >= 160 ) return true;
+  if( aE < 0 ) return false;
+  if( aE >= 160 ) return true;
 
-	int lRegime = (16*(aE/16))+8;
+  int lRegime = (16*(aE/16))+8;
 
-	int lThresh = aCoeffA + int( double( aCoeffB * lRegime ) / 1000. );
+  int lThresh = aCoeffA + int( double( aCoeffB * lRegime ) / 1000. );
 
-	return ( aClusters <= lThresh );
+  return ( aClusters <= lThresh );
 }
 
 
--- SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1CaloClusterProducer.cc
+++ SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1CaloClusterProducer.cc
@@ -10,15 +10,15 @@
 class L1CaloClusterProducer:public L1CaloAlgoBase < l1slhc::L1CaloTowerCollection, l1slhc::L1CaloClusterCollection >
 {
   public:
-	L1CaloClusterProducer( const edm::ParameterSet & );
-	 ~L1CaloClusterProducer(  );
+    L1CaloClusterProducer( const edm::ParameterSet & );
+    ~L1CaloClusterProducer(  );
 
-	void initialize(  );
+    void initialize(  );
 
-	void algorithm( const int &, const int & );
+    void algorithm( const int &, const int & );
 
   private:
-	int mElectronThrA, mElectronThrB;
+    int mElectronThrA, mElectronThrB;
 
 };
 
@@ -29,7 +29,7 @@ class L1CaloClusterProducer:public L1CaloAlgoBase < l1slhc::L1CaloTowerCollectio
 
 
 L1CaloClusterProducer::L1CaloClusterProducer( const edm::ParameterSet & aConfig ):
-L1CaloAlgoBase < l1slhc::L1CaloTowerCollection, l1slhc::L1CaloClusterCollection > ( aConfig )
+  L1CaloAlgoBase < l1slhc::L1CaloTowerCollection, l1slhc::L1CaloClusterCollection > ( aConfig )
 {
 }
 
@@ -41,83 +41,83 @@ L1CaloClusterProducer::~L1CaloClusterProducer(  )
 
 void L1CaloClusterProducer::initialize(  )
 {
-	// these values are constant within an event, so do not recalculate them every time...
-	mElectronThrA = int ( double ( mCaloTriggerSetup->electronThr( 2 ) ) / 10. );
-	mElectronThrB = mCaloTriggerSetup->electronThr( 0 ) + ( mElectronThrA * mCaloTriggerSetup->electronThr( 1 ) );
+  // these values are constant within an event, so do not recalculate them every time...
+  mElectronThrA = int ( double ( mCaloTriggerSetup->electronThr( 2 ) ) / 10. );
+  mElectronThrB = mCaloTriggerSetup->electronThr( 0 ) + ( mElectronThrA * mCaloTriggerSetup->electronThr( 1 ) );
 }
 
 
 void L1CaloClusterProducer::algorithm( const int &aEta, const int &aPhi )
 {
-	int lClusterIndex = mCaloTriggerSetup->getBin( aEta, aPhi );
-	std::pair < int, int >lClusterEtaPhi = mCaloTriggerSetup->getTowerEtaPhi( lClusterIndex );
+  int lClusterIndex = mCaloTriggerSetup->getBin( aEta, aPhi );
+  std::pair < int, int >lClusterEtaPhi = mCaloTriggerSetup->getTowerEtaPhi( lClusterIndex );
 
-	l1slhc::L1CaloCluster lCaloCluster( lClusterEtaPhi.first, lClusterEtaPhi.second );
+  l1slhc::L1CaloCluster lCaloCluster( lClusterEtaPhi.first, lClusterEtaPhi.second );
 
-	bool lFineGrain = false;
-	int lClusterEcalE = 0;
-	int lClusterTotalE = 0;
-	int lLeadTower = 0;
+  bool lFineGrain = false;
+  int lClusterEcalE = 0;
+  int lClusterTotalE = 0;
+  int lLeadTower = 0;
 
-	for ( int lTowerEta = aEta; lTowerEta <= aEta + 1; ++lTowerEta )
-	{
-		for ( int lTowerPhi = aPhi; lTowerPhi <= aPhi + 1; ++lTowerPhi )
-		{
+  for ( int lTowerEta = aEta; lTowerEta <= aEta + 1; ++lTowerEta )
+  {
+    for ( int lTowerPhi = aPhi; lTowerPhi <= aPhi + 1; ++lTowerPhi )
+    {
 
-			// ---------- new way ------------
-			l1slhc::L1CaloTowerCollection::const_iterator lTowerItr = fetch( lTowerEta, lTowerPhi );
-			if ( lTowerItr != mInputCollection->end(  ) )
-			{
-				int lTowerTotalE = lTowerItr->E(  ) + lTowerItr->H(  );
-				// this is no longer done in filling map, so do it here
-				if ( lTowerTotalE > 0 )
-				{
-					// Skip over fine grain bit calculation if desired
-					if ( mCaloTriggerSetup->fineGrainPass(  ) == 1 )
-					{
-						lFineGrain = false;
-					}
-					else
-					{
-						lFineGrain = lFineGrain || lTowerItr->EcalFG(  );
-					}
+      // ---------- new way ------------
+      l1slhc::L1CaloTowerCollection::const_iterator lTowerItr = fetch( lTowerEta, lTowerPhi );
+      if ( lTowerItr != mInputCollection->end(  ) )
+      {
+        int lTowerTotalE = lTowerItr->E(  ) + lTowerItr->H(  );
+        // this is no longer done in filling map, so do it here
+        if ( lTowerTotalE > 0 )
+        {
+          // Skip over fine grain bit calculation if desired
+          if ( mCaloTriggerSetup->fineGrainPass(  ) == 1 )
+          {
+            lFineGrain = false;
+          }
+          else
+          {
+            lFineGrain = lFineGrain || lTowerItr->EcalFG(  );
+          }
 
-					lClusterTotalE += lTowerTotalE;
-					lClusterEcalE += lTowerItr->E(  );
+          lClusterTotalE += lTowerTotalE;
+          lClusterEcalE += lTowerItr->E(  );
 
-					l1slhc::L1CaloTowerRef lRef( mInputCollection, lTowerItr - mInputCollection->begin(  ) );
-					lCaloCluster.addConstituent( lRef );
+          l1slhc::L1CaloTowerRef lRef( mInputCollection, lTowerItr - mInputCollection->begin(  ) );
+          lCaloCluster.addConstituent( lRef );
 
-					if ( lTowerTotalE > lLeadTower )
-						lLeadTower = lTowerTotalE;
-				}
-			}
-			// ---------- new way ------------
+          if ( lTowerTotalE > lLeadTower )
+            lLeadTower = lTowerTotalE;
+        }
+      }
+      // ---------- new way ------------
 
-		}
-	}
+    }
+  }
 
-	// we only register the Cluster into the event if this condition is satisfied....
-	if ( lCaloCluster.E(  ) > 0 )
-	{
-		// Calculate Electron Cut and Save it in the Cluster
-		int lElectronValue = ( int )( 100. * ( ( double )lClusterEcalE ) / ( ( double )lClusterTotalE ) );
-		lCaloCluster.setEGammaValue( lElectronValue );
+  // we only register the Cluster into the event if this condition is satisfied....
+  if ( lCaloCluster.E(  ) > 0 )
+  {
+    // Calculate Electron Cut and Save it in the Cluster
+    int lElectronValue = ( int )( 100. * ( ( double )lClusterEcalE ) / ( ( double )lClusterTotalE ) );
+    lCaloCluster.setEGammaValue( lElectronValue );
 
-		lCaloCluster.setLeadTower( lLeadTower >= mCaloTriggerSetup->seedTowerThr(  ) );
+    lCaloCluster.setLeadTower( lLeadTower >= mCaloTriggerSetup->seedTowerThr(  ) );
 
-		// Electron Bit Decision
-		bool lLowPtElectron = lCaloCluster.E(  ) <= mCaloTriggerSetup->electronThr( 1 ) && lElectronValue > mCaloTriggerSetup->electronThr( 0 );
+    // Electron Bit Decision
+    bool lLowPtElectron = lCaloCluster.E(  ) <= mCaloTriggerSetup->electronThr( 1 ) && lElectronValue > mCaloTriggerSetup->electronThr( 0 );
 
-		bool lHighPtElectron = lCaloCluster.E(  ) > mCaloTriggerSetup->electronThr( 1 ) && lElectronValue > ( mElectronThrB - ( mElectronThrA * lCaloCluster.E(  ) ) );
+    bool lHighPtElectron = lCaloCluster.E(  ) > mCaloTriggerSetup->electronThr( 1 ) && lElectronValue > ( mElectronThrB - ( mElectronThrA * lCaloCluster.E(  ) ) );
 
-		lCaloCluster.setEGamma( lLowPtElectron || lHighPtElectron );
+    lCaloCluster.setEGamma( lLowPtElectron || lHighPtElectron );
 
-		// FineGrain bit
-		lCaloCluster.setFg( lFineGrain );
+    // FineGrain bit
+    lCaloCluster.setFg( lFineGrain );
 
-		mOutputCollection->insert( lClusterEtaPhi.first, lClusterEtaPhi.second , lCaloCluster );
-	}
+    mOutputCollection->insert( lClusterEtaPhi.first, lClusterEtaPhi.second , lCaloCluster );
+  }
 
 }
 
