e0a6fa4249d2b81c1c2415127751308875c0edb9
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2012-02-29 10:43:17 +0000

    Modified structure to reflect the content of the T0 AlCaRecoMatrix

--- Configuration/AlCa/python/autoAlca.py
+++ Configuration/AlCa/python/autoAlca.py
@@ -1,20 +1,37 @@
-autoAlca = { 'AllForPrompt'   : 'SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias+HcalCalIsoTrk+MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu+TkAlJpsiMuMu+TkAlUpsilonMuMu+EcalCalElectron',
-             'StreamExpress'  : 'SiStripCalZeroBias+TkAlMinBias+MuAlCalIsolatedMu+DtCalib',
-             'MinimumBias'    : 'SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias+HcalCalIsoTrk',
-             'Commissioning'  : 'HcalCalIsoTrk',
-             'Mu'             : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib+TkAlZMuMu',
-             'SingleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib',
-             'DoubleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
-             'MuOnia'         : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
-             'Electron'       : 'EcalCalElectron',
-             'SingleElectron' : 'EcalCalElectron',
-             'DoubleElectron' : 'EcalCalElectron',
-             'EG'             : 'EcalCalElectron',
-             'Cosmics'        : 'TkAlBeamHalo+TkAlCosmics0T+MuAlGlobalCosmics+MuAlCalIsolatedMu+HcalCalHOCosmics',
-             'AlCaP0'         : 'EcalCalPi0Calib+EcalCalEtaCalib',
-             'AlCaPhiSymEcal' : 'EcalCalPhiSym',
-             'AlCaPhiSym'     : 'EcalCalPhiSym',
-             'HcalNZS'        : 'HcalCalMinBias',
-             'ExpressCosmics' : 'SiStripCalZeroBias, MuAlCalIsolatedMu',
-             'QCD'            : 'MuAlCalIsolatedMu+DtCalib+EcalCalPi0Calib+EcalCalEtaCalib+HcalCalIsoTrk+HcalCalDijets'
-             }
+AlCaRecoMatrix = {'ExpressCosmics' : 'SiStripCalZeroBias',
+                  'StreamExpress'  : 'SiStripCalZeroBias+TkAlMinBias+MuAlCalIsolatedMu+DtCalib',
+                  'MinimumBias'    : 'SiStripCalMinBias+TkAlMinBias',
+                  'Commissioning'  : 'HcalCalIsoTrk',
+                  'SingleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib',
+                  'DoubleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
+                  'MuOnia'         : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
+                  'SingleElectron' : 'EcalCalElectron',
+                  'DoubleElectron' : 'EcalCalElectron',
+                  'Cosmics'        : 'TkAlCosmics0T+MuAlGlobalCosmics+HcalCalHOCosmics+DtCalibCosmics',
+                  'AlCaP0'         : 'EcalCalPi0Calib+EcalCalEtaCalib',
+                  'AlCaPhiSym'     : 'EcalCalPhiSym',
+                  'HcalNZS'        : 'HcalCalMinBias'
+                  # This is in the AlCaRecoMatrix, but no RelVals are produced
+                  # 'TestEnablesTracker' : 'TkAlLAS'
+                  }
+
+def buildList(pdList, matrix):
+    """Takes a list of primary datasets (PDs) and the AlCaRecoMatrix (a dictinary) and returns a string with all the AlCaRecos for the selected PDs separated by the '+' character without duplicates."""
+    alCaRecoList = []
+    for pd in pdList:
+        alCaRecoList.append(matrix[pd])
+    # remove duplicates converting to a set
+    alCaRecoList = set(alCaRecoList)
+    stringList = ''
+    for alCaReco in alCaRecoList:
+        if stringList == '':
+            stringList += alCaReco
+        else:
+            stringList += '+'+alCaReco
+    return stringList
+
+# Update the lists anytime a new PD is added to the matrix
+autoAlca = { 'allForPrompt'         : buildList(['MinimumBias', 'Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'SingleElectron', 'DoubleElectron', 'AlCaP0', 'AlCaPhiSym', 'HcalNZS'], AlCaRecoMatrix),
+             'allForExpress'        : buildList(['StreamExpress'], AlCaRecoMatrix),
+             'allForPromptCosmics'  : buildList(['Cosmics'], AlCaRecoMatrix),
+             'allForExpressCosmics' : buildList(['ExpressCosmics'], AlCaRecoMatrix) }
