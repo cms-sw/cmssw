afbf1baf7d2acae32067512162450c742647eca6
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2012-03-01 16:46:50 +0000

    Add patches needed for muon reco

--- TauAnalysis/MCEmbeddingTools/test/rhEmbedder/install.sh
+++ TauAnalysis/MCEmbeddingTools/test/rhEmbedder/install.sh
@@ -2,8 +2,15 @@
 
 wd=`pwd`
 targetDir=$CMSSW_BASE/src/
+
+
 cp DetAssoc.patch $targetDir
+cp MuonRecoPatch.patch  $targetDir
+
 cd $targetDir
+addpkg RecoMuon/GlobalTrackFinder
+patch -p0 -i  MuonRecoPatch.patch
+
 addpkg TrackingTools/TrackAssociator
 patch -p0 -i DetAssoc.patch
 
