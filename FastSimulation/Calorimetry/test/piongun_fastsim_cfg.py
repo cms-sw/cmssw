1bff22da5e1835c30f0c9f44986413c4f2c1b117
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-03-01 10:52:56 +0000

    removed unnecessary lines

--- FastSimulation/Calorimetry/test/piongun_fastsim_cfg.py
+++ FastSimulation/Calorimetry/test/piongun_fastsim_cfg.py
@@ -36,12 +36,6 @@ process.options = cms.untracked.PSet(
 
 )
 
-# Production Info
-process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
-    annotation = cms.untracked.string('SingleMuPt10_cfi.py nevts:10'),
-    name = cms.untracked.string('PyReleaseValidation')
-)
 
 # Output definition
 
