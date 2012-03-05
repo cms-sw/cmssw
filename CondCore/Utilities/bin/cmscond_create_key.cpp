268a64a0386d648bdd799b6f1397533f5da4ee5a
Giacomo Govi <sha1-c6008bb91ca6bbc745928cb6f23a999d03f434c3@cern.ch>
2012-03-05 14:37:39 +0000

    extended tool for exporting

--- CondCore/Utilities/bin/cmscond_create_key.cpp
+++ CondCore/Utilities/bin/cmscond_create_key.cpp
@@ -20,12 +20,9 @@ namespace cond {
 
 cond::CreateKeyUtilities::CreateKeyUtilities():Utilities("cmscond_create_key"){
   addOption<std::string>("create","c","creating from input file data");
-  addOption<bool>("generate","g","generate a new key for every service");
+  addOption<size_t>("generate","g","generate a new key for every service");
   addOption<std::string>("read","r","read data from input file");
-  addOption<std::string>("encode","e","only encode the input data");
-  addOption<std::string>("decode","d","only decode the input data");
-  addOption<std::string>("key","k","the encoding key");
-  addOption<std::string>("file","f","the input/output file");
+  addOption<std::string>("clone","k","clone from the input key");
 }
 
 cond::CreateKeyUtilities::~CreateKeyUtilities(){
@@ -34,12 +31,13 @@ cond::CreateKeyUtilities::~CreateKeyUtilities(){
 int cond::CreateKeyUtilities::execute(){
   std::string inFile("");
   if( hasOptionValue("create") ) inFile = getOptionValue<std::string>("create");
-  bool generate = hasOptionValue("generate");
+  size_t keySize = 0;
+  if( hasOptionValue("generate") ) keySize = getOptionValue<size_t>("generate");
   
   if(!inFile.empty()){
     DecodingKey key;
     key.init( DecodingKey::FILE_NAME, Auth::COND_KEY, false );
-    key.createFromInputFile( inFile, generate );
+    key.createFromInputFile( inFile, keySize );
     if( hasDebug() ) key.list( std::cout );
     key.flush();
     return 0;
@@ -53,43 +51,13 @@ int cond::CreateKeyUtilities::execute(){
     return 0;
   }
 
-  std::string input("");
-  std::string key("");
-  if( hasOptionValue("key") ) key = getOptionValue<std::string>("key");
-  std::string fileOut("");
-  bool outFile = hasOptionValue("file");
-  if( outFile ) fileOut = getOptionValue<std::string>("file");
-  bool encode = hasOptionValue("encode");
-  bool decode = hasOptionValue("decode");
-  if( encode ){
-    input = getOptionValue<std::string>("encode");
-    Cipher cipher( key );
-    if( outFile ) {
-      std::ofstream fout(fileOut);
-      cipher.bencrypt( input, fout );
-      fout.close();
-    } else {
-      std::cout<< cipher.encrypt( input )<<std::endl;
-    }
-    return 0; 
+  if( hasOptionValue("clone") ) {
+    std::string inputKeyFile = getOptionValue<std::string>("clone");
+    DecodingKey inputKey;
+    inputKey.init( inputKeyFile, Auth::COND_KEY );
+    
   }
-  if( decode ){
-    input = getOptionValue<std::string>("decode");
-    Cipher cipher( key );
-    std::string decoded("");
-    if( outFile ) {
-      std::ifstream fin( fileOut );
-      decoded = cipher.bdecrypt( fin );
-      fin.close();
-      std::cout <<"Done. s="<<decoded<<std::endl;
-    } else {
-      decoded = cipher.decrypt( input );
-    }
-    std::cout<< decoded<<std::endl;
-    return 0; 
-  }
-  
-  
+
   return 1;
 }
 
--- CondCore/Utilities/bin/cmscond_export_database.cpp
+++ CondCore/Utilities/bin/cmscond_export_database.cpp
@@ -7,58 +7,79 @@
 #include "CondCore/IOVService/interface/IOVNames.h"
 #include "CondCore/IOVService/interface/IOVSchemaUtility.h"
 #include "CondCore/IOVService/interface/IOVEditor.h"
+//
+#include "RelationalAccess/ISchema.h"
+#include "RelationalAccess/ITable.h"
+#include "RelationalAccess/ITableDataEditor.h"
+#include "RelationalAccess/IQuery.h"
+#include "RelationalAccess/ICursor.h"
+#include "CoralBase/Attribute.h"
+#include "CoralBase/AttributeList.h"
 
 namespace cond {
   class ExportAccountUtilities : public Utilities {
     public:
-      ExportAccountUtilities();
-      ~ExportAccountUtilities();
-      int execute();
+    ExportAccountUtilities();
+    ~ExportAccountUtilities();
+    int exportTags( const std::vector<std::string>& tagList );
+    bool getTagList( const std::string& globalTag, const std::string& accountName, std::vector<std::string>& tagList );
+    int execute();
+    DbSession m_sourceDb;
+    DbSession m_destDb;
+
   };
+
+  std::string getAccountName( const std::string& connectionString ){
+    std::string ret("");
+    size_t lastSlash = connectionString.find_last_of('/');
+    if( lastSlash != std::string::npos ){
+      ret = connectionString.substr( lastSlash+1 );
+    }
+    return ret;
+  }
 }
 
-cond::ExportAccountUtilities::ExportAccountUtilities():Utilities("cmscond_export_database"){
+cond::ExportAccountUtilities::ExportAccountUtilities():
+  Utilities("cmscond_export_database"),
+  m_sourceDb(),
+  m_destDb(){
   addAuthenticationOptions();
   addConnectOption("sourceConnect","s","source connection string(required)");
   addConnectOption("destConnect","d","destionation connection string(required)");
+  addOption<std::string>("globalTag","t","only export the tags referenced in the specified GT");
+  addConnectOption("gtConnect","g","source connection string(required)");
   addOption<size_t>("bunchSize","n","iterate with bunches of specific size (optional)");
   addOption<bool>("check","c","check the iov elements imported.");
+  addOption<bool>("verbose","v","verbose printout.");
 }
 
 cond::ExportAccountUtilities::~ExportAccountUtilities(){
 }
 
-int cond::ExportAccountUtilities::execute(){
-
-  cond::DbSession sourceDb = openDbSession("sourceConnect", true);
-  cond::DbSession destDb = openDbSession("destConnect");
+int cond::ExportAccountUtilities::exportTags( const std::vector<std::string>& inputTags ){
   size_t bunchSize = 1;
   if(hasOptionValue("bunchSize")) bunchSize = getOptionValue<size_t>("bunchSize"); 
+  bool check = hasOptionValue("check");
 
-  // listing tag in source
-  sourceDb.transaction().start(true);
-  cond::MetaData  sourceMetadata(sourceDb);
-  std::vector<std::string> inputTags;
-  sourceMetadata.listAllTags( inputTags );
-  std::cout << inputTags.size() <<" tags found in the source database."<<std::endl;
+  MetaData sourceMetadata( m_sourceDb );
   for( std::vector<std::string>::const_iterator iT = inputTags.begin(); iT!= inputTags.end(); ++iT ){
     std::string sourceIovToken=sourceMetadata.getToken( *iT );
       
-    cond::IOVProxy sourceIov( sourceDb );
+    cond::IOVProxy sourceIov( m_sourceDb );
     sourceIov.load( sourceIovToken );
     cond::TimeType  sourceIovType = sourceIov.timetype();
     std::set<std::string> const& sourceClasses = sourceIov.payloadClasses();
     int sourceSz = sourceIov.size();
-    std::cout <<"---> Exporting tag \""<<*iT<<"\" ("<<sourceSz<<" iov elements)"<<std::endl;
+    std::cout <<"---> Exporting tag \""<<*iT<<"\" ("<<sourceSz<<" iov element(s))"<<std::endl;
 
-    cond::DbScopedTransaction transaction(destDb);
+    cond::DbScopedTransaction transaction(m_destDb);
     transaction.start(false);
 
-    cond::IOVEditor destIov( destDb );
+    cond::IOVEditor destIov( m_destDb );
     destIov.createIOVContainerIfNecessary();
-    destDb.storage().lockContainer( IOVNames::container() );
+    m_destDb.storage().lockContainer( IOVNames::container() );
 
-    cond::MetaData  destMetadata( destDb );
+    cond::MetaData  destMetadata( m_destDb );
     if( destMetadata.hasTag( *iT ) ){
       std::cout <<"ERROR: Tag \""<<*iT<<"\" already exists in destination database."<<std::endl;
       throw std::runtime_error("Destination account is not clean.");
@@ -90,13 +111,12 @@ int cond::ExportAccountUtilities::execute(){
     std::cout <<">> Tag \""<<*iT<<"\": "<<totalImported<<" element(s) exported."<<std::endl;
     transaction.commit();
 
-    bool check = hasOptionValue("check");
     if( check ){
       std::cout<<">> Checking exported iov..."<<std::endl;
       ::sleep(1);
-      destDb.transaction().start(true);
+      m_destDb.transaction().start(true);
       std::string dTok = destMetadata.getToken( *iT );
-      cond::IOVProxy dIov( destDb );
+      cond::IOVProxy dIov( m_destDb );
       dIov.load( dTok );
       cond::TimeType  dIovType = sourceIov.timetype();
       if( dIovType != sourceIovType ){
@@ -117,7 +137,7 @@ int cond::ExportAccountUtilities::execute(){
       cond::IOVProxy::const_iterator iEnd = dIov.end();
       bool ok = true;
       for( cond::IOVProxy::const_iterator iEl = dIov.begin(); iEl != iEnd; ++iEl ){
-	ora::Object payload = destDb.getObject(iEl->token());
+	ora::Object payload = m_destDb.getObject(iEl->token());
         bool ok = true;
 	if( payload.address() == 0 ){
 	  std::cout <<"ERROR: payload retrieved for token \""<<iEl->token()<<"\" cannot be loaded."<<std::endl;
@@ -130,14 +150,124 @@ int cond::ExportAccountUtilities::execute(){
         payload.destruct();
 	if(!ok) break;
       }
-      destDb.transaction().commit();
+      m_destDb.transaction().commit();
       if(!ok) return -1;
       std::cout << ">> All of the tests passed. Tag \""<<*iT<<"\" successfully exported."<<std::endl; 
     }
   }
-
-  sourceDb.transaction().commit();
   return 0;
+}
+
+bool cond::ExportAccountUtilities::getTagList( const std::string& globalTag, 
+					       const std::string& accountName, 
+					       std::vector<std::string>& tagList ){
+  if(hasDebug() || hasOptionValue("verbose")) std::cout <<">> Account name=\""<<accountName<<"\""<<std::endl;
+  DbSession gtSession =  openDbSession("gtConnect",true);
+  gtSession.transaction().start(true);
+  coral::ISchema& schema = gtSession.nominalSchema();
+  std::string gtTable("TAGTREE_TABLE_");
+  gtTable += globalTag;
+  if( !schema.existsTable( gtTable ) ){
+    std::cout <<"ERROR: The specified Global Tag \"" << globalTag <<"\" has not been found in the database." <<std::endl;
+    return false;
+  }
+  std::auto_ptr<coral::IQuery> query( schema.newQuery() );
+  query->addToTableList("TAGINVENTORY_TABLE","TAGS");
+  query->addToTableList( gtTable,"GT");
+  query->addToOutputList( "TAGS.tagname" );
+  coral::AttributeList condData;
+  condData.extend<std::string>( "PFNHINT" );
+  std::string condDataToBind("%");
+  condDataToBind += accountName;
+  condData[ "PFNHINT" ].data<std::string>() =  condDataToBind;
+  std::string condition("TAGS.tagid = GT.tagid");
+  condition += " AND ";
+  condition += "TAGS.pfn LIKE :PFNHINT";
+  //condition += "TAGS.pfn LIKE " + condDataToBind;
+  coral::AttributeList qresult;
+  qresult.extend<std::string>("TAGS.tagname");
+  query->defineOutput(qresult);
+  query->setCondition( condition, condData );
+  coral::ICursor& cursor = query->execute();
+  while( cursor.next() ) {
+    const coral::AttributeList& row = cursor.currentRow();
+    tagList.push_back( row["TAGS.tagname"].data<std::string>() );
+  }
+  cursor.close();
+  gtSession.transaction().commit();
+  return true;
+}
+
+int cond::ExportAccountUtilities::execute(){
+
+  int ret = 0;
+  m_sourceDb = openDbSession("sourceConnect", true);
+  m_destDb = openDbSession("destConnect");
+  std::string sourceConnect = getOptionValue<std::string>( "sourceConnect" );
+  std::string destConnect = getOptionValue<std::string>( "destConnect" );
+  bool verbose = hasOptionValue("verbose");
+
+  // listing tag in source
+  m_sourceDb.transaction().start(true);
+  std::vector<std::string> allTags;
+  MetaData sourceMetadata( m_sourceDb );
+  sourceMetadata.listAllTags( allTags );
+  size_t ntags = allTags.size();
+  std::vector<std::string> gtTags;
+  std::vector<std::string>* tags = 0;
+
+  std::string gt("");
+  if( hasOptionValue("globalTag") ) {
+    gt = getOptionValue<std::string>("globalTag");
+    std::string accountName = getAccountName( sourceConnect );
+    if( accountName.empty() ){
+	std::cout <<"ERROR: cannot resolve account name from connection string \""<<sourceConnect<<"\""<<std::endl;
+	return -1;      
+    }
+    bool gtok = getTagList( gt, accountName, gtTags );
+    if(!gtok){
+      return -1;
+    } 
+    for( std::vector<std::string>::const_iterator igT = gtTags.begin();
+	 igT != gtTags.end(); ++igT ){
+      bool found = false;
+      for( std::vector<std::string>::const_iterator iaT = allTags.begin();
+	 iaT != allTags.end(); ++iaT ){
+	if( *igT == *iaT ) found = true;
+      }
+      if(!found) {
+	std::cout <<"ERROR: the tag \""<<*igT<<"\" referenced in the Global Tag \""<<gt<<"\" has not been found in the database \""<< sourceConnect<<"\""<<std::endl;
+	return -1;
+      }
+    }
+    ntags = gtTags.size();
+    std::cout << ">> Global Tag \"" << gt << "\" references "<<ntags << " tag(s) in database \""<<sourceConnect<<"\""<<std::endl;
+    tags = &gtTags;
+  } else {
+    std::cout << ">> "<<allTags.size() <<" tag(s) found in the database \""<<sourceConnect<<"\""<<std::endl;
+    tags = &allTags;
+  }
+  if ( verbose ){
+    for ( std::vector<std::string>::const_iterator iT = tags->begin();
+	  iT != tags->end(); ++iT ){
+      std::cout <<"    "<<*iT<<std::endl;
+    }
+  }
+  char k;
+  std::cout << std::endl;
+  std::cout << ">> Confirm the export of "<<ntags<<" tag(s)? (Y/N)";
+  std::cin >> k;
+  if( k=='Y' || k=='y' ){
+    int nexp = exportTags( *tags );
+    if(nexp<0) {
+      return -1;
+    }
+  } else {
+    ntags = 0;
+  }
+  std::cout <<">> "<<ntags<<" tag(s) exported to \""<<destConnect<<"\""<<std::endl;
+  m_sourceDb.transaction().commit();
+  return ret;
 }
 
 int main( int argc, char** argv ){
