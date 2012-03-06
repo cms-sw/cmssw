49c31a12ec7fd33e2f31982a6799111d74099776
Christopher Farrell <sha1-8ff4c3e4c5e5d4f1b2a4a4e825bd74e83b42ab48@cern.ch>
2012-03-06 19:28:42 +0000

    Make loading files from dcache work

--- SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/LaunchOnCondor.py
+++ SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/LaunchOnCondor.py
@@ -123,6 +123,7 @@ def CreateTheShellFile(argv):
 	        shell_file.write('   gSystem->Load("libDataFormatsVertexReco.so");\n')
 	        shell_file.write('   gSystem->Load("libDataFormatsHepMCCandidate.so");\n')
                 shell_file.write('   gSystem->Load("libPhysicsToolsUtilities.so");\n')
+                shell_file.write('   gSystem->Load("libdcap.so");\n')
                 shell_file.write('   .x %s+' % argv[1] + function_argument + '\n')
 	        shell_file.write('   .q\n')
 	        shell_file.write('EOF\n\n')
