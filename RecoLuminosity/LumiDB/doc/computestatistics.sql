3e36af5773c4fd033d254edd9180c06c1f280edc
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-03-06 10:04:31 +0000

    added

---
+++ RecoLuminosity/LumiDB/doc/computestatistics.sql
@@ -0,0 +1,4 @@
+exec dbms_stats.gather_table_stats('','TRGDATA',no_invalidate=>false);
+exec dbms_stats.gather_table_stats('','LSTRG',no_invalidate=>false);
+exec dbms_stats.gather_table_stats('','LUMISUMMARYV2',no_invalidate=>false);
+exec dbms_stats.gather_table_stats('','CMSRUNSUMMARY',no_invalidate=>false);
