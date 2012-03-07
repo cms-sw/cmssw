9fed18b28f9ccc7fcb39157525a744d55b0e7874
William Badgett Jr <sha1-0342ea9cbf4e6781ff6fc4596018aec98179df76@cern.ch>
2012-03-07 19:29:05 +0000

    Rename spare_ field to actual data content name pileupRMS_

--- DataFormats/Scalers/interface/LumiScalers.h
+++ DataFormats/Scalers/interface/LumiScalers.h
@@ -93,7 +93,7 @@ class LumiScalers
   unsigned int numOrbits() const           { return(numOrbits_);}
 
   float pileup() const                     { return(pileup_);}
-  float spare() const                      { return(spare_);}
+  float pileupRMS() const                  { return(pileupRMS_);}
 
   /// equality operator
   int operator==(const LumiScalers& e) const { return false; }
@@ -146,7 +146,7 @@ protected:
   unsigned int numOrbits_;
 
   float pileup_;
-  float spare_;
+  float pileupRMS_;
 };
 
 
