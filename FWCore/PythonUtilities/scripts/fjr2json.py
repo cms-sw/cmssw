790410d647a5c65dcb13d78c4a94bcc6e9a62a15
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2012-03-07 21:30:47 +0000

    Allow JSON-like version of lumis in CRAB XML files

--- FWCore/PythonUtilities/scripts/fjr2json.py
+++ FWCore/PythonUtilities/scripts/fjr2json.py
@@ -3,12 +3,14 @@
 from FWCore.PythonUtilities.XML2Python import xml2obj
 from FWCore.PythonUtilities.LumiList   import LumiList
 from pprint import pprint
+
+import ast
 import optparse
 import sys
 
 
 if __name__ == '__main__':
-    
+
     parser = optparse.OptionParser ("Usage: %prog [--options] job1.fjr [job2.fjr...]")
     parser.add_option ('--output', dest='output', type='string',
                        help='Save output to file OUTPUT')
@@ -24,26 +26,36 @@ if __name__ == '__main__':
             print "'%s' is not an framework job report.  Skipping." % fjr
             continue
         for inputFile in obj.InputFile:
-            try:
-                runList = inputFile.Runs.Run
+            try: # Regular XML version, assume only one of these
+                runObjects = inputFile.Runs.Run
+                for run in runObjects:
+                    runNumber = int (run.ID)
+                    runList = runsLumisDict.setdefault (runNumber, [])
+                    for lumiPiece in run.LumiSection:
+                        lumi = int (lumiPiece.ID)
+                        runList.append (lumi)
             except:
-                try:
-                    print "'%s' in '%s' contains no runs.  Skipping." % \
-                          (inputFile.PFN, fjr)
+                try: # JSON-like version in CRAB XML files, runObjects is usually a list
+                    if isinstance(inputFile.Runs, basestring):
+                        runObjects = [inputFile.Runs]
+                    else:
+                        runObjects = inputFile.Runs
+
+                    for runObject in runObjects:
+                        try:
+                            runs = ast.literal_eval(runObject)
+                            for (run, lumis) in runs.iteritems():
+                                runList = runsLumisDict.setdefault (int(run), [])
+                                runList.extend(lumis)
+                        except ValueError: # Old style handled above
+                            pass
                 except:
-                    print "Some run in '%s' contains no runs.  Skipping." % \
-                          fjr
+                    print "Run missing in '%s'.  Skipping." % fjr
                 continue
-            for run in runList:
-                runNumber = int (run.ID)
-                runList = runsLumisDict.setdefault (runNumber, [])
-                for lumiPiece in run.LumiSection:
-                    lumi = int (lumiPiece.ID)
-                    runList.append (lumi)
 
     jsonList = LumiList (runsAndLumis = runsLumisDict)
     if options.output:
         jsonList.writeJSON (options.output)
     else:
         print jsonList
-                
+
