0942ad23ae67cd22da870c18c2e00d83665b77d4
Markus Seidel <sha1-e9c7c1f66faf0db812f95e610f4050dafdd0d825@cern.ch>
2012-03-07 13:43:39 +0000

    First load KinFitter electron module, then replace InputTag

--- TopQuarkAnalysis/TopEventProducers/python/sequences/ttSemiLepEvtBuilder_cff.py
+++ TopQuarkAnalysis/TopEventProducers/python/sequences/ttSemiLepEvtBuilder_cff.py
@@ -80,8 +80,8 @@ def setForAllTtSemiLepHypotheses(process, attribute, value):
 
 ## use electrons instead of muons for the hypotheses
 def useElectronsForAllTtSemiLepHypotheses(process, elecLabel = "selectedPatElectrons"):
-    ## replace lepton InputTags in all modules
-    setForAllTtSemiLepHypotheses(process, "leps", elecLabel)
     ## use correct KinFitter module
     import TopQuarkAnalysis.TopKinFitter.TtSemiLepKinFitProducer_Electrons_cfi
     process.kinFitTtSemiLepEventHypothesis = TopQuarkAnalysis.TopKinFitter.TtSemiLepKinFitProducer_Electrons_cfi.kinFitTtSemiLepEvent.clone()
+    ## replace lepton InputTags in all modules
+    setForAllTtSemiLepHypotheses(process, "leps", elecLabel)
