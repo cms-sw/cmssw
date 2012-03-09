38fbf510319b089ed6d9d7faa762d66343ad12bf
Halil Gamsizkan <sha1-b9ce56aa9f6e1e182c1d16a6e7a697e06a6c9eee@cern.ch>
2012-03-09 08:22:18 +0000

    histograms cleanup

--- DQM/HLTEvF/python/FourVectorHLTOnline_cff.py
+++ DQM/HLTEvF/python/FourVectorHLTOnline_cff.py
@@ -4,5 +4,5 @@ from DQM.HLTEvF.FourVectorHLTOnline_cfi import *
 
 from DQM.HLTEvF.OccupancyPlotter_cfi import *
 
-hlt4vector = cms.Path(hltResultsOn * onlineOccPlot)
-
+#hlt4vector = cms.Path(hltResultsOn * onlineOccPlot)
+hlt4vector = cms.Path(onlineOccPlot)
