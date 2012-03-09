33fc7f794e375c19500a18a31ff3fcfa8bbd9d12
Danilo Piparo <sha1-e2231637d47316c544de5e218528626d57de224e@cern.ch>
2012-03-09 08:36:50 +0000

    o added the forgotten plugins

---
+++ DQM/DataScouting/plugins/SealModule.cc
@@ -0,0 +1,5 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DQM/DataScouting/plugins/ScoutingTestAnalyzer.h"
+
+DEFINE_FWK_MODULE(ScoutingTestAnalyzer);
