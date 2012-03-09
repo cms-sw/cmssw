2627e2f2e02200e1754f59b3ae599f3a6d3c80b2
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-03-09 16:10:35 +0000

    Allow hltRegionaPixelTrack to be used

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
@@ -1,47 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-#Primary Vertex
-from FastSimulation.Tracking.PixelVerticesProducer_cff import *
-# (Not-so) Regional Tracking
-from FastSimulation.Tracking.GlobalPixelTracking_cff import *
-
-# L3 pixel-seeded tracks for Single Tau Collection (pT>1GeV/c)
-hltL3TauCtfWithMaterialTracks = cms.EDProducer("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates")),
-    ptMin = cms.untracked.double(1.0)
-
+import FastSimulation.Tracking.HLTPixelTracksProducer_cfi
+hltRegionalPixelTracks = FastSimulation.Tracking.HLTPixelTracksProducer_cfi.hltPixelTracks.clone()
+hltRegionalPixelTracks.FilterPSet.ptMin = 0.1
+hltRegionalPixelTracks.RegionFactoryPSet.ComponentName = "L3MumuTrackingRegion"
+hltRegionalPixelTracks.RegionFactoryPSet.RegionPSet = cms.PSet(
+    originRadius = cms.double( 1.0 ),
+    ptMin = cms.double( 0.5 ),
+    originHalfLength = cms.double( 15.0 ),
+    vertexZDefault = cms.double( 0.0 ),
+    vertexSrc = cms.string( "hltDisplacedmumuVtxProducerTauTo2Mu" ),
+    deltaEtaRegion = cms.double( 0.5 ),
+    deltaPhiRegion = cms.double( 0.5 ),
+    TrkSrc = cms.InputTag( "hltL3Muons" ),
+    UseVtxTks = cms.bool( False )
 )
-hltL3TauCtfWithMaterialHighPtTracks = cms.EDProducer("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates")),
-    ptMin = cms.untracked.double(1.0)
-)
-hltL25TauCtfWithMaterialTracks = cms.EDProducer("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates")),
-    ptMin = cms.untracked.double(1.0)
-)
-hltL3TauSingleTrack15CtfWithMaterialTracks = cms.EDProducer("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates")),
-    ptMin = cms.untracked.double(0.9)
-)
-hltPFJetCtfWithMaterialTracks = cms.EDProducer("FastTrackMerger",
-    SaveTracksOnly = cms.untracked.bool(True),
-    TrackProducers = cms.VInputTag(cms.InputTag("globalPixelWithMaterialTracks"),
-                                   cms.InputTag("globalPixelTrackCandidates")),
-    ptMin = cms.untracked.double(0.2)
-)
-
-#--- Fastsim sequences replacing modules in tau paths ---#
-#hltL3TauCkfTrackCandidates = cms.Sequence(globalPixelTracking)
-HLTL3TauTrackReconstructionSequence = cms.Sequence(globalPixelTracking + hltL3TauCtfWithMaterialTracks)
-HLTL3TauHighPtTrackReconstructionSequence = cms.Sequence(globalPixelTracking + hltL3TauCtfWithMaterialHighPtTracks)
-HLTL25TauTrackReconstructionSequence = cms.Sequence(globalPixelTracking + hltL25TauCtfWithMaterialTracks)
-HLTL3TauSingleTrack15ReconstructionSequence = cms.Sequence(globalPixelTracking + hltL3TauSingleTrack15CtfWithMaterialTracks)
-HLTTrackReconstructionForJets  = cms.Sequence(globalPixelTracking + hltPFJetCtfWithMaterialTracks)
--- FastSimulation/HighLevelTrigger/python/HLTFastReco_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastReco_cff.py
@@ -10,7 +10,7 @@ from FastSimulation.Tracking.GlobalPixelTracking_cff import *
 from FastSimulation.HighLevelTrigger.HLTFastRecoForJetMET_cff import *
 from FastSimulation.HighLevelTrigger.HLTFastRecoForEgamma_cff import *
 from FastSimulation.HighLevelTrigger.HLTFastRecoForMuon_cff import *
-#from FastSimulation.HighLevelTrigger.HLTFastRecoForTau_cff import *  # IT IS NOT NEEDED ANY MORE IN 44X
+from FastSimulation.HighLevelTrigger.HLTFastRecoForTau_cff import *
 from FastSimulation.HighLevelTrigger.HLTFastRecoForB_cff import *
 #from FastSimulation.HighLevelTrigger.HLTFastRecoForPF_cff import *   # IT IS NOT NEEDED ANY MORE IN 44X
 from FastSimulation.HighLevelTrigger.HLTFastRecoForXchannel_cff import *
