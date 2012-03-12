24343352b890fa187f668de7067b023cce879172
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-03-12 16:57:23 +0000

    Allow paths with HLTHBHENoise in the menu

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForJetMET_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastRecoForJetMET_cff.py
@@ -1,4 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
+# Provide dummy calonoise sequences
+import FastSimulation.HighLevelTrigger.DummyModule_cfi
+HLTHBHENoiseSequence = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+HLTHBHENoiseCleanerSequence = FastSimulation.HighLevelTrigger.DummyModule_cfi.dummyModule.clone()
+
 
 
