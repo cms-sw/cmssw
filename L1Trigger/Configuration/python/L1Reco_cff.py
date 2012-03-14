8a9cf34624e57d1b9be7a468e95692720efa2681
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2012-03-14 13:26:35 +0000

    Add l1L1GtObjectMap (convert L1GlobalTriggerObjectMapRecord to L1GlobalTriggerObjectMaps) to L1Reco sequence and keep it at RECO and AOD level in L1 trigger event content.

--- L1Trigger/Configuration/python/L1Reco_cff.py
+++ L1Trigger/Configuration/python/L1Reco_cff.py
@@ -4,6 +4,7 @@ import FWCore.ParameterSet.Config as cms
 #     L1Extra (all BxInEvent)
 #     L1GtTriggerMenuLite
 #     l1GtRecord - requires functional L1 O2O
+#     l1L1GtObjectMap - L1 trigger object maps, now produced with the convertor
 #     
 # V.M. Ghete 2009-07-11
 
@@ -22,9 +23,13 @@ from EventFilter.L1GlobalTriggerRawToDigi.l1GtTriggerMenuLite_cfi import *
 import EventFilter.L1GlobalTriggerRawToDigi.conditionDumperInEdm_cfi
 conditionsInEdm = EventFilter.L1GlobalTriggerRawToDigi.conditionDumperInEdm_cfi.conditionDumperInEdm.clone()
 
+# l1L1GtObjectMap
+from L1Trigger.GlobalTrigger.convertObjectMapRecord_cfi import *
+l1L1GtObjectMap = convertObjectMapRecord.clone()
+
 # sequences
 
 L1Reco_L1Extra = cms.Sequence(l1extraParticles)
 L1Reco_L1Extra_L1GtRecord = cms.Sequence(l1extraParticles+l1GtRecord)
 #
-L1Reco = cms.Sequence(l1extraParticles+l1GtTriggerMenuLite+conditionsInEdm)
+L1Reco = cms.Sequence(l1extraParticles+l1GtTriggerMenuLite+conditionsInEdm+l1L1GtObjectMap)
