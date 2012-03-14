a44b23d5f80065fcd0c49e8d59129ba5388f1f3f
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-03-14 12:44:26 +0000

    updated HI

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_52_V4::All',
-             'startup' : 'START52_V4::All',
-             'com10'   : 'GR_R_52_V4::All',
-             'hltonline' : 'GR_R_52_V4::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI52_V3::All'
+autoCond = { 'mc'      : 'MC_50_V16A::All',
+             'startup' : 'START50_V16A::All',
+             'com10'   : 'GR_R_50_V13::All',
+             'hltonline' : 'GR_R_50_V13::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI50_V16::All'
              }
