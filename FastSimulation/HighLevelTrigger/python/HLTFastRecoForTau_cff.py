250e34f080f3d6d02ed35771b9a0824538365890
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-03-15 22:33:07 +0000

    First fixes for HLT_Tau2Mu_ItTrack_v1, not yet conclusives...

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
@@ -31,3 +31,24 @@ hltRegPixelTracks.RegionFactoryPSet.RegionPSet = cms.PSet(
     TrkSrc = cms.InputTag( "hltL3Muons" ),
     UseVtxTks = cms.bool( False )
 )
+
+
+
+# CKFTrackCandidateMaker
+import FastSimulation.Tracking.TrackCandidateProducer_cfi
+
+hltTau3MuCkfTrackCandidates = FastSimulation.Tracking.TrackCandidateProducer_cfi.trackCandidateProducer.clone()
+hltTau3MuCkfTrackCandidates.SeedProducer = cms.InputTag("hltTau3MuPixelSeedsFromPixelTracks")
+hltTau3MuCkfTrackCandidates.TrackProducers = []
+hltTau3MuCkfTrackCandidates.SeedCleaning = True
+hltTau3MuCkfTrackCandidates.SplitHits = False
+
+# CTF track fit with material
+import RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cfi
+
+hltTau3MuCtfWithMaterialTracks = RecoTracker.TrackProducer.CTFFinalFitWithMaterial_cfi.ctfWithMaterialTracks.clone()
+hltTau3MuCtfWithMaterialTracks.src = 'hltTau3MuCkfTrackCandidates'
+hltTau3MuCtfWithMaterialTracks.TTRHBuilder = 'WithoutRefit'
+hltTau3MuCtfWithMaterialTracks.Fitter = 'KFFittingSmoother'
+hltTau3MuCtfWithMaterialTracks.Propagator = 'PropagatorWithMaterial'
+
