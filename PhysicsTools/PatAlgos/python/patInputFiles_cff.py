4f81ef99b3120a303aac5ea66fe03cb0cd406e09
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2012-03-15 19:02:43 +0000

    Rather use production release RelVals.

--- PhysicsTools/PatAlgos/python/patInputFiles_cff.py
+++ PhysicsTools/PatAlgos/python/patInputFiles_cff.py
@@ -2,28 +2,28 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 
 filesRelValProdTTbarAODSIM = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START52_V2'
+                        , globalTag     = 'START52_V4A'
                         , dataTier      = 'AODSIM'
-                        , maxVersions   = 3
+                        , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesRelValProdTTbarGENSIMRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START52_V2'
+                        , globalTag     = 'START52_V4A'
                         , dataTier      = 'GEN-SIM-RECO'
                         , maxVersions   = 3
                         , numberOfFiles = 1
                         )
     )
 filesSingleMuRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
                         , relVal        = 'SingleMu'
                         , dataTier      = 'RECO'
-                        , globalTag     = 'GR_R_52_V3_RelVal_mu2011B'
+                        , globalTag     = 'GR_R_52_V4_RelVal_mu2011B'
                         , maxVersions   = 3
                         , numberOfFiles = 1
                         )
