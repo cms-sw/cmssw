bc70b9e2eda172a12cf29b48de428186d1e4ca4e
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-03-15 21:44:29 +0000

    Re-define also hltRegPixelTracks

--- FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
+++ FastSimulation/HighLevelTrigger/python/HLTFastRecoForTau_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 import FastSimulation.Tracking.HLTPixelTracksProducer_cfi
+
 hltRegionalPixelTracks = FastSimulation.Tracking.HLTPixelTracksProducer_cfi.hltPixelTracks.clone()
 hltRegionalPixelTracks.FilterPSet.ptMin = 0.1
 hltRegionalPixelTracks.RegionFactoryPSet.ComponentName = "L3MumuTrackingRegion"
@@ -10,6 +11,21 @@ hltRegionalPixelTracks.RegionFactoryPSet.RegionPSet = cms.PSet(
     originHalfLength = cms.double( 15.0 ),
     vertexZDefault = cms.double( 0.0 ),
     vertexSrc = cms.string( "hltDisplacedmumuVtxProducerTauTo2Mu" ),
+    deltaEtaRegion = cms.double( 0.5 ),
+    deltaPhiRegion = cms.double( 0.5 ),
+    TrkSrc = cms.InputTag( "hltL3Muons" ),
+    UseVtxTks = cms.bool( False )
+)
+
+hltRegPixelTracks = FastSimulation.Tracking.HLTPixelTracksProducer_cfi.hltPixelTracks.clone()
+hltRegPixelTracks.FilterPSet.ptMin = 0.1
+hltRegPixelTracks.RegionFactoryPSet.ComponentName = "L3MumuTrackingRegion"
+hltRegPixelTracks.RegionFactoryPSet.RegionPSet = cms.PSet(
+    originRadius = cms.double( 1.0 ),
+    ptMin = cms.double( 0.5 ),
+    originHalfLength = cms.double( 15.0 ),
+    vertexZDefault = cms.double( 0.0 ),
+    vertexSrc = cms.string( "hltDisplacedmumuVtxProducerDoubleMuTau2Mu" ),
     deltaEtaRegion = cms.double( 0.5 ),
     deltaPhiRegion = cms.double( 0.5 ),
     TrkSrc = cms.InputTag( "hltL3Muons" ),
