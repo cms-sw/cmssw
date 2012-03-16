44be4f25c17d0eafd97d2799b9844de65c084904
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-03-16 21:34:30 +0000

    add testDIPLumiProducer

---
+++ RecoLuminosity/LumiProducer/test/testDIPLumiProd_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("dbsourcetest")
+
+import FWCore.Framework.test.cmsExceptionsFatalOption_cff
+process.options = cms.untracked.PSet(
+#  wantSummary = cms.untracked.bool(True),
+  Rethrow = FWCore.Framework.test.cmsExceptionsFatalOption_cff.Rethrow
+)
+process.maxEvents = cms.untracked.PSet(
+  input = cms.untracked.int32(-1)
+)
+#process.source = cms.Source("EmptySource",
+#        numberEventsInRun = cms.untracked.uint32(10),
+#        firstRun = cms.untracked.uint32(1),
+#        numberEventsInLuminosityBlock = cms.untracked.uint32(1),
+#        firstLuminosityBlock = cms.untracked.uint32(1)
+#)
+process.source= cms.Source("PoolSource",
+             processingMode=cms.untracked.string('RunsAndLumis'),        
+             fileNames=cms.untracked.vstring(
+             'file:/data/cmsdata/009F3522-D604-E111-A08D-003048F1183E.root')
+)
+process.DIPLumiProducer=cms.ESSource("DIPLumiProducer")
+process.prod = cms.EDAnalyzer("TestDIPLumiProducer")
+
+process.p = cms.Path(process.prod)
+
