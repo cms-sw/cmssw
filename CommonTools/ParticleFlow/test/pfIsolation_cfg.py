67b1c1eda0a4d70dcc779bc3526adc8ab7123d0d
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2012-03-19 18:06:47 +0000

    added example

--- CommonTools/ParticleFlow/test/pfIsolation_cfg.py
+++ CommonTools/ParticleFlow/test/pfIsolation_cfg.py
@@ -5,13 +5,16 @@ process = cms.Process("PFISO")
 
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
  
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_5_1_2/RelValZMM/GEN-SIM-RECO/START50_V15A-v1/0247/301EC619-0062-E111-8A3F-00261894387B.root'
-    
+'/store/relval/CMSSW_5_1_2/RelValZEE/GEN-SIM-RECO/PU_START50_V15A-v1/0003/C61C71DC-8061-E111-AAEB-0025B32036D2.root',
+'/store/relval/CMSSW_5_1_2/RelValZEE/GEN-SIM-RECO/PU_START50_V15A-v1/0003/C49AC4E5-7F61-E111-8FB2-003048F1C836.root',
+'/store/relval/CMSSW_5_1_2/RelValZEE/GEN-SIM-RECO/PU_START50_V15A-v1/0003/C27CBF93-7E61-E111-AB30-003048F024C2.root',
+'/store/relval/CMSSW_5_1_2/RelValZEE/GEN-SIM-RECO/PU_START50_V15A-v1/0003/B46B6A67-8D61-E111-82C0-003048F118AA.root',
+'/store/relval/CMSSW_5_1_2/RelValZEE/GEN-SIM-RECO/PU_START50_V15A-v1/0003/36B93F53-7F61-E111-AA27-002481E0D524.root'
     ))
    
 
@@ -26,12 +29,29 @@ from CommonTools.ParticleFlow.Tools.pfIsolation import setupPFElectronIso, setup
 process.eleIsoSequence = setupPFElectronIso(process, 'gsfElectrons')
 process.muIsoSequence = setupPFMuonIso(process, 'muons')
 
+process.TFileService = cms.Service("TFileService", fileName = cms.string("histo.root") )
+
+process.elePFIsoReader = cms.EDAnalyzer("PFIsoReaderDemo",
+                                        Electrons = cms.InputTag('gsfElectrons'),
+                                        IsoDepElectron = cms.VInputTag(cms.InputTag('elPFIsoDepositChargedPFIso'),
+                                                                       cms.InputTag('elPFIsoDepositGammaPFIso'),
+                                                                       cms.InputTag('elPFIsoDepositNeutralPFIso')),
+                                        IsoValElectronPF = cms.VInputTag(cms.InputTag('elPFIsoValueCharged03PFIdPFIso'),
+                                                                         cms.InputTag('elPFIsoValueGamma03PFIdPFIso'),
+                                                                         cms.InputTag('elPFIsoValueNeutral03PFIdPFIso')),
+                                        IsoValElectronNoPF = cms.VInputTag(cms.InputTag('elPFIsoValueCharged03NoPFIdPFIso'),
+                                                                           cms.InputTag('elPFIsoValueGamma03NoPFIdPFIso'),
+                                                                           cms.InputTag('elPFIsoValueNeutral03NoPFIdPFIso'))
+                                        )
+
 process.p = cms.Path(
     # process.pfNoPileUpSequence +
     process.pfParticleSelectionSequence + 
     process.eleIsoSequence + 
-    process.muIsoSequence
+    process.muIsoSequence+
+    process.elePFIsoReader
     )
+
 
 # output ------------------------------------------------------------
 
@@ -41,7 +61,7 @@ process.out = cms.OutputModule("PoolOutputModule",
 )
 
 process.outpath = cms.EndPath(
-    process.out 
+#    process.out 
     )
 
 
