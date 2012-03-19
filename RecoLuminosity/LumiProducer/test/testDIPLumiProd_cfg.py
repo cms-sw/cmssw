f543b9972a287ee89549f071ec4a7fc2031c40d5
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-03-19 10:44:21 +0000

    db test ok

--- RecoLuminosity/LumiProducer/test/testDIPLumiProd_cfg.py
+++ RecoLuminosity/LumiProducer/test/testDIPLumiProd_cfg.py
@@ -16,12 +16,17 @@ process.maxEvents = cms.untracked.PSet(
 #        numberEventsInLuminosityBlock = cms.untracked.uint32(1),
 #        firstLuminosityBlock = cms.untracked.uint32(1)
 #)
-process.source= cms.Source("PoolSource",
-             processingMode=cms.untracked.string('RunsAndLumis'),        
-             fileNames=cms.untracked.vstring(
-             'file:/data/cmsdata/009F3522-D604-E111-A08D-003048F1183E.root')
+process.DBService=cms.Service('DBService',
+        authPath= cms.untracked.string('/data/cmsdata')       
 )
-process.DIPLumiProducer=cms.ESSource("DIPLumiProducer")
+process.source= cms.Source("PoolSource",
+        processingMode=cms.untracked.string('RunsAndLumis'),        
+        fileNames=cms.untracked.vstring(
+        'file:/data/cmsdata/009F3522-D604-E111-A08D-003048F1183E.root')
+)
+process.DIPLumiProducer=cms.ESSource("DIPLumiProducer",
+        connect=cms.string('oracle://cms_orcoff_prod/cms_runtime_logger')
+)
 process.prod = cms.EDAnalyzer("TestDIPLumiProducer")
 
 process.p = cms.Path(process.prod)
