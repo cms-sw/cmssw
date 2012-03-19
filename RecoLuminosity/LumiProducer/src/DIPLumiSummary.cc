6f83b89547452dd5bf9ffaa70aa499e77de5679e
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-03-19 15:29:51 +0000

    fix printer

--- RecoLuminosity/LumiProducer/src/DIPLumiSummary.cc
+++ RecoLuminosity/LumiProducer/src/DIPLumiSummary.cc
@@ -8,15 +8,27 @@ DIPLumiSummary::instDelLumi() const{
   return m_instlumi;
 }
 float 
-DIPLumiSummary::intgDelLumi()const{
-  return m_dellumi;
+DIPLumiSummary::intgDelLumiByLS()const{
+  return m_instlumi*23.31;
 }
 float 
-DIPLumiSummary::intgRecLumi()const{
-  return m_reclumi;
+DIPLumiSummary::intgRecLumiByLS()const{
+  if(m_totdellumi<=0.0){return 0.0;}
+  return m_instlumi*23.31*(m_totreclumi/m_totdellumi);
+}
+float 
+DIPLumiSummary::intgDelLumiSinceRun()const{
+  return m_totdellumi*23.31;
+}
+float 
+DIPLumiSummary::intgRecLumiSinceRun()const{
+  return m_totreclumi*23.31;
 }
 float 
 DIPLumiSummary::deadtimefraction() const{
+  if(m_totreclumi!=0){
+    m_deadfrac=1.0-(m_totreclumi/m_totdellumi);
+  }
   return m_deadfrac;
 }
 int 
@@ -24,12 +36,16 @@ DIPLumiSummary::cmsalive()const{
   return m_cmsalive;
 }
 std::ostream& operator<<(std::ostream& s, const DIPLumiSummary& diplumiSummary) {
-  s << "\nDumping DIPLumiSummary\n\n";
-  s << std::setw(15) << "  instDelLumi = " << diplumiSummary.instDelLumi();
-  s << std::setw(15) << "  intgDelLumi = " << diplumiSummary.intgDelLumi();
-  s << std::setw(15) << "  intgRecLumi = " << diplumiSummary.intgRecLumi();
-  s << std::setw(15) << "  deadtimefraction = " << diplumiSummary.deadtimefraction();
-  s << std::setw(15) << "  cmsalive = " << diplumiSummary.cmsalive();
+  std::cout.setf(std::ios::fixed,std::ios::floatfield);
+  std::cout.setf(std::ios::showpoint);
+  s << "\nDumping DIPLumiSummary (/ub)\n\n";
+  s << std::setw(20) << "instDelLumi = " << std::setprecision(3) << diplumiSummary.instDelLumi();
+  s << std::setw(20) << "intgDelLumiByLS = " << std::setprecision(3) << diplumiSummary.intgDelLumiByLS();
+  s << std::setw(20) << "intgRecLumiByLS = " << std::setprecision(3) << diplumiSummary.intgRecLumiByLS();
+  s << std::setw(25) << "intgDelLumiSinceRun = " << std::setprecision(3) << diplumiSummary.intgDelLumiSinceRun();
+  s << std::setw(25) << "intgRecLumiSinceRun = " << std::setprecision(3)<< diplumiSummary.intgRecLumiSinceRun();
+  s << std::setw(20) << "deadtimefraction = " << std::setprecision(3) << diplumiSummary.deadtimefraction();
+  s << std::setw(15) << "cmsalive = " << diplumiSummary.cmsalive();
   s << "\n";
   return s<<"\n";
 }
