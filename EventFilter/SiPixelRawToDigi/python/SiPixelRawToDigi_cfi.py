8403be0299f87098fa51c50c603e8634311adf14
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2012-03-19 06:53:56 +0000

    For proper use with ConfDB, keep single instance of configuration with empty Regions PSet in this file

--- EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
+++ EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
@@ -10,25 +10,8 @@ siPixelDigis = cms.EDProducer("SiPixelRawToDigi",
 ## ErrorList: list of error codes used by tracking to invalidate modules
     ErrorList = cms.vint32(29),
 ## UserErrorList: list of error codes used by Pixel experts for investigation
-    UserErrorList = cms.vint32(40)
-)
-
-
-## regional seeded unpacking for specialized HLT paths 
-siPixelDigisRegional = cms.EDProducer( "SiPixelRawToDigi",
-    Timing = cms.untracked.bool( False ),
-    IncludeErrors = cms.bool( False ),
-    InputLabel = cms.InputTag( "rawDataCollector" ),
-    CheckPixelOrder = cms.bool( False ),
-    UseQualityInfo = cms.bool( False ),
-    UseCablingTree = cms.untracked.bool( True ),
-    ErrorList = cms.vint32( ),
-    UserErrorList = cms.vint32( ),
-    Regions = cms.PSet(
-        inputs = cms.VInputTag( "hltL2EtCutDoublePFIsoTau45Trk5" ),
-        deltaPhi = cms.vdouble( 0.5 ),
-        maxZ = cms.vdouble( 24. ),
-        beamSpot = cms.InputTag( "hltOnlineBeamSpot" )
-    )
+    UserErrorList = cms.vint32(40),
+## Empty Regions PSet means complete unpacking
+    Regions = cms.PSet( ) 
 )
 
