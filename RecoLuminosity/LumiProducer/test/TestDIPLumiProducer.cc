d942d3ec4b1fc6e9aa8edafa76ab376c1fd489d5
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-03-21 18:49:45 +0000

    test all product delivery

--- RecoLuminosity/LumiProducer/test/TestDIPLumiProducer.cc
+++ RecoLuminosity/LumiProducer/test/TestDIPLumiProducer.cc
@@ -9,6 +9,7 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/EventSetupRecord.h"
 #include "RecoLuminosity/LumiProducer/interface/DIPLumiSummary.h"
+#include "RecoLuminosity/LumiProducer/interface/DIPLumiDetail.h"
 #include "RecoLuminosity/LumiProducer/interface/DIPLumiSummaryRcd.h"
 
 #include <iostream>
@@ -52,8 +53,7 @@ void TestDIPLumiProducer::analyze(edm::Event const& e,edm::EventSetup const&)
 // -----------------------------------------------------------------
 
 void TestDIPLumiProducer::endLuminosityBlock(edm::LuminosityBlock const& lumiBlock, EventSetup const& es){
-  std::cout <<" I AM IN RUN NUMBER "<<lumiBlock.run() <<std::endl;
-  std::cout <<" ---LS NUMBER "<< lumiBlock.luminosityBlock()<<std::endl;
+  std::cout <<" I AM IN RUN NUMBER "<<lumiBlock.run() <<" LS NUMBER "<< lumiBlock.luminosityBlock()<<std::endl;
   edm::eventsetup::EventSetupRecordKey recordKey(edm::eventsetup::EventSetupRecordKey::TypeTag::findType("DIPLumiSummaryRcd"));
   if( recordKey.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()) {
     std::cout <<"Record \"DIPLumiSummaryRcd"<<"\" does not exist "<<std::endl;
@@ -62,10 +62,25 @@ void TestDIPLumiProducer::endLuminosityBlock(edm::LuminosityBlock const& lumiBlo
     edm::ESHandle<DIPLumiSummary> datahandle;
     unsigned long long cache_id=es.get<DIPLumiSummaryRcd>().cacheIdentifier();
     std::cout<<cache_id<<std::endl;
-    es.get<DIPLumiSummaryRcd>().get(datahandle);
+    es.getData(datahandle);
     if(datahandle.isValid()){
       const DIPLumiSummary* mydata=datahandle.product();
       std::cout<<*mydata<<std::endl;
+    }else{
+      std::cout<<"no valid data found"<<std::endl;
+    }
+  }catch(const edm::eventsetup::NoRecordException<DIPLumiSummaryRcd>& er){
+    std::cout<<"no data found"<<std::endl;
+  }
+  std::cout<<"looking at detail"<<std::endl;
+  try{
+    edm::ESHandle<DIPLumiDetail> pDetail;
+    unsigned long long cache_id=es.get<DIPLumiSummaryRcd>().cacheIdentifier();
+    std::cout<<cache_id<<std::endl;
+    if(pDetail.isValid()){
+      es.getData(pDetail);
+      const DIPLumiDetail* ddata=pDetail.product();
+      std::cout<<*ddata<<std::endl;
     }else{
       std::cout<<"no valid data found"<<std::endl;
     }
