21b57ed6b49d7c773463aef4f206676487a27c94
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-03-21 16:12:53 +0000

    correct collection

--- FastSimulation/Configuration/python/mixFastSimObjects_cfi.py
+++ FastSimulation/Configuration/python/mixFastSimObjects_cfi.py
@@ -29,7 +29,7 @@ mixSimVertices = cms.PSet(
     type = cms.string('SimVertex')
 )
 mixHepMCProducts = cms.PSet(
-    input = cms.VInputTag(cms.InputTag("famosSimHits")),
+    input = cms.VInputTag(cms.InputTag("generator")),
     type = cms.string('HepMCProduct')
 )
 
