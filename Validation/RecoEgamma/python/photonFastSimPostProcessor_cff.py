a7ea0f9879839313a6140b18037f34976f2f4bcd
Nancy Marinelli <sha1-ef5da52cfaf56298cecd1a4c085ed43aaf8901ee@cern.ch>
2012-03-22 15:25:03 +0000

    New sequence to be used for FastSim validation. The need of a new sequence is necessary because plots related to ecal seeded conversions (not implemented in fastsim) need to be switched off

---
+++ Validation/RecoEgamma/python/photonFastSimPostProcessor_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.RecoEgamma.photonPostprocessing_cfi import *
+photonPostprocessing.batch = cms.bool(False)
+photonPostprocessing.standalone = cms.bool(False)
+photonPostprocessing.isRunCentrally = cms.bool(True)
+
+from Validation.RecoEgamma.conversionPostprocessing_cfi import *
+conversionPostprocessing.batch = cms.bool(False)
+conversionPostprocessing.standalone = cms.bool(False)
+conversionPostprocessing.fastSim = cms.bool(True)
+
+photonPostProcessor = cms.Sequence(photonPostprocessing*conversionPostprocessing)
