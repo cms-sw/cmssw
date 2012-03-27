d2300109f9c1c382de1f5f7e336008f46f7bc2a0
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2012-03-27 11:00:42 +0000

    Added the new HLT GT

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -4,6 +4,6 @@
 autoCond = { 'mc'      : 'MC_52_V5::All',
              'startup' : 'START52_V5::All',
              'com10'   : 'GR_R_52_V7::All',
-             'hltonline' : 'GR_R_52_V7::All', # same as com10 until H tag available
+             'hltonline' : 'GR_H_V28::All', # same as com10 until H tag available
              'starthi' : 'STARTHI52_V6::All'
              }
