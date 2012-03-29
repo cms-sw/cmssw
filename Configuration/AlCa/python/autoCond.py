d86982bda0c19e2f3b95a37615114ff03e724501
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-03-29 10:24:58 +0000

    add overrides for the offline connection string and pfnPrefix

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,8 @@
-
-
-
-autoCond = { 'mc'      : 'MC_52_V5::All',
-             'startup' : 'START52_V5::All',
-             'com10'   : 'GR_R_52_V7::All',
-             'hltonline' : 'GR_H_V28::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI52_V6::All'
-             }
+autoCond = { 
+    'mc'        : 'MC_52_V5::All',
+    'startup'   : 'START52_V5::All',
+    'com10'     : 'GR_R_52_V7::All',
+     # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
+    'hltonline' : 'GR_H_V28::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
+    'starthi'   : 'STARTHI52_V6::All'
+}
