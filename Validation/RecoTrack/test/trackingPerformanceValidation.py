3be538ff4206f8ba337ca5d4ce99a037d1c4d5e5
Adish Vartak <sha1-97eaff72678caf6cf86d14826829db661927c6fa@cern.ch>
2012-03-30 21:03:40 +0000

    Another small fix to trackingPerformanceValidation.py

--- Validation/RecoTrack/test/trackingPerformanceValidation.py
+++ Validation/RecoTrack/test/trackingPerformanceValidation.py
@@ -11,10 +11,10 @@ import copy
 
 
 ### Reference release
-RefRelease='CMSSW_5_2_0'
+RefRelease='CMSSW_5_2_1'
 
 ### Relval release (set if different from $CMSSW_VERSION)
-NewRelease='CMSSW_5_2_1'
+NewRelease='CMSSW_5_2_2'
 
 ### sample list 
 
@@ -47,7 +47,7 @@ Version='v1'
 # Global tags
 StartupTag='START52_V4'
 
-RefStartupTag='START52_V4A'
+RefStartupTag='START52_V4'
 
 ### Track algorithm name and quality. Can be a list.
 Algos= ['ootb', 'iter0', 'iter1','iter2','iter3','iter4','iter5','iter6']
@@ -121,8 +121,8 @@ def replace(map, filein, fileout):
 def do_validation(samples, GlobalTag, trackquality, trackalgorithm, PileUp, sampleType, dofastfull):
     global Sequence, Version, RefSelection, RefRepository, NewSelection, NewRepository, defaultNevents, Events, castorHarvestedFilesDirectory
     global cfg, macro, Tracksname
-    tracks_map = { 'ootb':'general_AssociatorByHitsRecoDenom','iter0':'cutsRecoZero_AssociatorByHitsRecoDenom','iter1':'cutsRecoFirst_AssociatorByHitsRecoDenom','iter2':'cutsRecoSecond_AssociatorByHitsRecoDenom','iter3':'cutsRecoThird_AssociatorByHitsRecoDenom','iter4':'cutsRecoFourth_AssociatorByHitsRecoDenom','iter5':'cutsRecoFifth_AssociatorByHitsRecoDenom','iter6':'cutsRecoSixth_AssociatorByHitsRecoDenom'}
-    tracks_map_hp = { 'ootb':'cutsRecoHp_AssociatorByHitsRecoDenom','iter0':'cutsRecoZeroHp_AssociatorByHitsRecoDenom','iter1':'cutsRecoFirstHp_AssociatorByHitsRecoDenom','iter2':'cutsRecoSecondHp_AssociatorByHitsRecoDenom','iter3':'cutsRecoThirdHp_AssociatorByHitsRecoDenom','iter4':'cutsRecoFourthHp_AssociatorByHitsRecoDenom','iter5':'cutsRecoFifthHp_AssociatorByHitsRecoDenom','iter6':'cutsRecoSixthHp_AssociatorByHitsRecoDenom'}
+    tracks_map = { 'ootb':'general_AssociatorByHits','iter0':'cutsRecoZero_AssociatorByHits','iter1':'cutsRecoFirst_AssociatorByHits','iter2':'cutsRecoSecond_AssociatorByHits','iter3':'cutsRecoThird_AssociatorByHits','iter4':'cutsRecoFourth_AssociatorByHits','iter5':'cutsRecoFifth_AssociatorByHits','iter6':'cutsRecoSixth_AssociatorByHits'}
+    tracks_map_hp = { 'ootb':'cutsRecoHp_AssociatorByHits','iter0':'cutsRecoZeroHp_AssociatorByHits','iter1':'cutsRecoFirstHp_AssociatorByHits','iter2':'cutsRecoSecondHp_AssociatorByHits','iter3':'cutsRecoThirdHp_AssociatorByHits','iter4':'cutsRecoFourthHp_AssociatorByHits','iter5':'cutsRecoFifthHp_AssociatorByHits','iter6':'cutsRecoSixthHp_AssociatorByHits'}
     if(trackalgorithm=='iter0' or trackalgorithm=='ootb'):
         mineff='0.5'
         maxeff='1.025'
@@ -387,7 +387,7 @@ for algo in Algos:
             RefSelection+='_ootb'
         do_validation(pileupstartupsamples, StartupTag, quality , algo, PileUp, sampleType, dofastfull)
 
-        NewRepository = copy.copy(NewRepositoryBase) + '/fastim'
+        NewRepository = copy.copy(NewRepositoryBase) + '/fastsim'
         PileUp = 'noPU'
         sampleType = 'FastSim'
         RefSelection=RefStartupTag+'_'+PileUp
