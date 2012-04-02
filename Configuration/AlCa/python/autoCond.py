26e15b06b77c8d0149f78820b82af4dbbf1c7c00
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2012-04-02 16:52:44 +0000

    New GTs for 52X MC fixing L1 and updating strips channel status

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,6 +1,6 @@
 autoCond = { 
-    'mc'        : 'MC_52_V8::All',
-    'startup'   : 'START52_V8::All',
+    'mc'        : 'MC_52_V9::All',
+    'startup'   : 'START52_V9::All',
     'com10'     : 'GR_R_52_V7::All',
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
     'hltonline' : 'GR_H_V29::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
