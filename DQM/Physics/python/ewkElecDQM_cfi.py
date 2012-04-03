a91cd6bbc2c320cb87a8e85bdcf6ba71fa0284a8
Valentina Gori <sha1-a8f920a5fd6daebfdd62d1c5060b9dfff859ca57@cern.ch>
2012-04-03 17:03:34 +0000

    trigger names modified

--- DQM/Physics/python/ewkElecDQM_cfi.py
+++ DQM/Physics/python/ewkElecDQM_cfi.py
@@ -13,7 +13,8 @@ ewkElecDQM = cms.EDAnalyzer("EwkElecDQM",
 
       # Main cuts ->
 #      MuonTrig = cms.untracked.string("HLT_Mu9"),
-      ElecTrig = cms.untracked.vstring("HLT_Ele10_LW_L1R","HLT_Ele10_SW_L1R","HLT_Photon15_L1R","HLT_L1SingleEG5","HLT_L1SingleEG8"), 
+       ElecTrig = cms.untracked.vstring("HLT_Ele","HLT_DoubleEle","HLT_DoublePhoton","HLT_Photon","HLT_L1SingleEG"),
+#      ElecTrig = cms.untracked.vstring("*"), 
 #      UseTrackerPt = cms.untracked.bool(True), # ??
 #      PtCut = cms.untracked.double(25.0),
       PtCut = cms.untracked.double(10.0),
