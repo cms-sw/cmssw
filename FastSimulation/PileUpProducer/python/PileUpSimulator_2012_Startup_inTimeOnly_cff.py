c07ce44643f00d644c05c9f438105ef31850d9b4
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-04-03 08:41:18 +0000

    use 8 TeV minbias

--- FastSimulation/PileUpProducer/python/PileUpSimulator_2012_Startup_inTimeOnly_cff.py
+++ FastSimulation/PileUpProducer/python/PileUpSimulator_2012_Startup_inTimeOnly_cff.py
@@ -1,5 +1,5 @@
 from FastSimulation.Configuration.RandomServiceInitialization_cff import *
-from FastSimulation.PileUpProducer.PileUpSimulator7TeV_cfi import *
+from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import *
 from FastSimulation.Configuration.FamosSequences_cff import famosPileUp
 famosPileUp.PileUpSimulator = PileUpSimulatorBlock.PileUpSimulator
 famosPileUp.PileUpSimulator.usePoisson = False
