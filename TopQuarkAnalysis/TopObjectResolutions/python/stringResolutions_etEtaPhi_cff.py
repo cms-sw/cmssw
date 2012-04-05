34328daa510525e2b3aa427b0a6dbb50f902b700
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2012-04-05 11:06:50 +0000

    Make printout a WARNING

--- TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py
+++ TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py
@@ -1,4 +1,4 @@
-print "The file TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py is outdated."
-print "Please migrate to a more recent one (to be found in the same directory)."
+print "*** WARNING: The file TopQuarkAnalysis/TopObjectResolutions/python/stringResolutions_etEtaPhi_cff.py is outdated."
+print "***          Please migrate to a more recent one (possibly found in the same directory)."
 
 from TopQuarkAnalysis.TopObjectResolutions.stringResolutions_etEtaPhi_Spring10_cff import *
