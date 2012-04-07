3b09af12744d15ee6850698eecf8d3c54cd890be
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-04-07 09:43:41 +0000

    forgotten file

---
+++ DQM/SiStripMonitorClient/bin/listbadmodule.h
@@ -0,0 +1,5 @@
+#ifndef DQM_SiStripMonitorClient_listbadmodule_h
+#define DQM_SiStripMonitorClient_listbadmodule_h
+#include <string>
+void listbadmodule(std::string filename);
+#endif // DQM_SiStripMonitorClient_listbadmodule_h
