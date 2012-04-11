f8d84c1db21ca48df4c4a76ff86967813ba27c72
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2012-04-11 11:58:03 +0000

    removed the TtDecaySelection module to reduce code duplication and potential confusion; the full functionality is now in TopSkimming's TopDecayChannelFilter, which takes a GenParticleCollection but alternatively also a TtGenEvent as input; TtDecaySelection_cfi and ttGenEventFilters_cff are still kept functional and ensure backward-compatibility but print a warning messages

--- TopQuarkAnalysis/TopEventProducers/python/producers/TtDecaySelection_cfi.py
+++ TopQuarkAnalysis/TopEventProducers/python/producers/TtDecaySelection_cfi.py
@@ -1,40 +1,7 @@
-import FWCore.ParameterSet.Config as cms
+print "*** Warning: TopQuarkAnalysis/TopEventProducers/python/producers/TtDecaySelection_cfi.py will be deprecated"
+print "*** Please use TopQuarkAnalysis/TopSkimming/python/TtDecayChannelFilter_cfi.py instead"
+print "*** (you can still use 'genEvent' instead of 'genParticles' as input)"
 
-#
-# module to perform a selection of specific top
-# deecays based on the genEvt
-#
-ttDecaySelection = cms.EDFilter("TtDecaySelection",
-    ## input source for decay channel selection
-    src    = cms.InputTag("genEvt"),
-    ## invert the selection choice                                
-    invert = cms.bool(False),
-
-    ## allow given lepton in corresponding decay
-    ## branch for a given decay channel selection;
-    ## all leptons to 'False' corresponds to the
-    ## full hadronic decay channel
-    allowedTopDecays = cms.PSet(
-      decayBranchA = cms.PSet(
-        electron = cms.bool(False),
-        muon     = cms.bool(False),
-        tau      = cms.bool(False)
-      ),
-      decayBranchB= cms.PSet(
-        electron = cms.bool(False),
-        muon     = cms.bool(False),
-        tau      = cms.bool(False)
-      )
-    ),
-
-    ## add a restriction to the decay channel of taus
-    ## by redefining the following ParameterSet in
-    ## your cfg file; the following restrictions are
-    ## available:
-    restrictTauDecays = cms.PSet(
-    #  leptonic   = cms.bool(False),
-    #  oneProng   = cms.bool(False),
-    #  threeProng = cms.bool(False)
-    )
-)
-
+import TopQuarkAnalysis.TopSkimming.TtDecayChannelFilter_cfi
+ttDecaySelection = TopQuarkAnalysis.TopSkimming.TtDecayChannelFilter_cfi.ttDecayChannelFilter.clone()
+ttDecaySelection.src = "genEvt"
