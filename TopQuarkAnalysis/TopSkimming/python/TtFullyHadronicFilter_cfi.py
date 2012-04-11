c775d2565d51f98e2b8d8fe3a33f616f2e768ede
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2012-04-11 11:33:53 +0000

    add a central cfi for the TtDecayChannelFilter as the one-and-only place where parameters are defined and default values set; add a cff in which ttFullHadronicFilter, ttSemiLeptonicFilter and ttFullLeptonicFilter are defined; the old cfi's are obsolete but still functional: they import the corresponding module from the new cff (and print a warning)

--- TopQuarkAnalysis/TopSkimming/python/TtFullyHadronicFilter_cfi.py
+++ TopQuarkAnalysis/TopSkimming/python/TtFullyHadronicFilter_cfi.py
@@ -1,37 +1,6 @@
-import FWCore.ParameterSet.Config as cms
+print "*** Warning: TopQuarkAnalysis/TopSkimming/python/TtFullyHadronicFilter_cfi.py will be deprecated"
+print "*** Please use TopQuarkAnalysis/TopSkimming/python/TtDecayChannelFilter_cfi.py or"
+print "*** TopQuarkAnalysis/TopSkimming/python/ttDecayChannelFilter_cff.py instead"
 
-ttFullyHadronicFilter = cms.EDFilter("TtDecayChannelFilter",
-    ## input source for decay channel selection
-    src = cms.InputTag("genParticles"),
-    ## invert the selection choice                                
-    invert = cms.bool(False),
-
-    ## allow given lepton in corresponding decay
-    ## branch for a given decay channel selection;
-    ## all leptons to 'False' corresponds to the
-    ## full hadronic decay channel
-    allowedTopDecays = cms.PSet(
-      decayBranchA = cms.PSet(
-        electron = cms.bool(False),
-        muon     = cms.bool(False),
-        tau      = cms.bool(False)
-      ),
-      decayBranchB= cms.PSet(
-        electron = cms.bool(False),
-        muon     = cms.bool(False),
-        tau      = cms.bool(False)
-      )
-    ),
-
-    ## add a restriction to the decay channel of taus
-    ## by redefining the following ParameterSet in
-    ## your cfg file; the following restrictions are
-    ## available:
-    restrictTauDecays = cms.PSet(
-    #  leptonic   = cms.bool(False),
-    #  oneProng   = cms.bool(False),
-    #  threeProng = cms.bool(False)
-    )
-)
-
-
+from TopQuarkAnalysis.TopSkimming.ttDecayChannelFilter_cff import ttFullHadronicFilter
+ttFullyHadronicFilter = ttFullHadronicFilter
