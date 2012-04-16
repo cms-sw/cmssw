c3bde7017b6cc52452c242a5d9bdfd1c3a2880ae
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2012-04-16 14:19:11 +0000

    proper importing with new mixing placeholder scheme

--- FastSimulation/PileUpProducer/python/PileUpSimulator_2012_Startup_inTimeOnly_cff.py
+++ FastSimulation/PileUpProducer/python/PileUpSimulator_2012_Startup_inTimeOnly_cff.py
@@ -1,8 +1,10 @@
-from IOMC.RandomEngine.IOMC_cff import *
-from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import *
-from FastSimulation.Configuration.FamosSequences_cff import famosPileUp
-famosPileUp.PileUpSimulator = PileUpSimulatorBlock.PileUpSimulator
+from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import PileUpSimulatorBlock as block8TeV
+from FastSimulation.Configuration.MixingFamos_cff import *
+
+#define the PU scenario itself
+famosPileUp.PileUpSimulator = block8TeV.PileUpSimulator
 famosPileUp.PileUpSimulator.usePoisson = False
+
 famosPileUp.PileUpSimulator.probFunctionVariable = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59)
 famosPileUp.PileUpSimulator.probValue = (
     2.344E-05,
