db40d0072f6e87baef0426433295a109ed55541c
Chiara Ilaria Rovelli <sha1-156f87976672d5574743371779e65c95d251b39f@cern.ch>
2012-04-17 17:17:13 +0000

    first GTs for 60X

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,10 +1,10 @@
 autoCond = { 
-    'mc'        : 'MC_52_V9::All',
-    'startup'   : 'START52_V9::All',
-    'com10'     : 'GR_R_52_V7::All',
+    'mc'        : 'MC_60_V0::All',
+    'startup'   : 'START60_V0::All',
+    'com10'     : 'GR_R_60_V0::All',
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
-    'hltonline' : 'GR_H_V29::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
+    'hltonline' : 'GR_R_60_V0::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
     'hltonline11' : ('GR_H_V29::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
                      'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
-    'starthi'   : 'STARTHI52_V8::All'
+    'starthi'   : 'STARTHI60_V0::All'
 }
