a659208f9147258eadfeb44ca662217c717a07c0
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-04-18 09:45:04 +0000

    new version from online

--- RecoLuminosity/LumiProducer/interface/LumiCorrector.h
+++ RecoLuminosity/LumiProducer/interface/LumiCorrector.h
@@ -2,22 +2,28 @@
 #define LUMICORRECTOR_HH
 
 #include <map>
-
+#include <string>
 class LumiCorrector {
 
-  public:
-    LumiCorrector();
-    LumiCorrector(float norm=7.13,float punorm=6.37,float alpha1=0.063,float alpha2=-0.0037);
-    ~LumiCorrector(){}
-
-    float AfterglowFactor(int nBXs);
-    float TotalCorrectionFactor(float TotLumi_noNorm, int nBXs);
-
-    float Norm_;
-    float PUNorm_;
-    float Alpha1_;
-    float Alpha2_;
-    std::map<int,float> AfterglowMap_;
+ public:
+  LumiCorrector();
+  ~LumiCorrector(){}
+  void setNormForAlgo(const std::string& algo,float value);
+  void setCoefficient(const std::string& name,float value);
+  float getNormForAlgo(const std::string& algo)const;
+  float getCoefficient(const std::string& name)const;
+  float AfterglowFactor(int nBXs);
+  float TotalNormOcc1(float TotLumi_noNorm, int nBXs);
+  float TotalNormOcc2(float TotLumi_noNorm, int nBXs);
+  float TotalNormET(float TotLumi_noNorm, int nBXs);
+ private:
+  float Occ1Norm_;
+  float Occ2Norm_;
+  float ETNorm_;
+  float PUNorm_;
+  float Alpha1_;
+  float Alpha2_;
+  std::map<int,float> AfterglowMap_;
 };
 
 #endif
