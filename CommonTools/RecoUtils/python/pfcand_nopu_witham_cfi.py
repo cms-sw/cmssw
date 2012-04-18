6c3f3517d7594bb57135cce8e239945eb40d01ed
Matthias Geisler <sha1-ca4f0a71d5bbadf63f826def6c188008b2bb8317@cern.ch>
2012-04-18 15:12:53 +0000

    Changed to use of the PFCand association map

--- CommonTools/RecoUtils/python/pfcand_nopu_witham_cfi.py
+++ CommonTools/RecoUtils/python/pfcand_nopu_witham_cfi.py
@@ -1,22 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 FirstVertexPFCandidates = cms.EDProducer('PFCand_NoPU_WithAM',		
-	 
-	  #Set the Input Collections
-          PFCandidateCollection = cms.InputTag('particleFlow'),
-          VertexCollection = cms.InputTag('offlinePrimaryVertices'),
 		  
 	  #Set the Input Association Map
-          VertexTrackAssociationMap = cms.InputTag('Tracks2Vertex'),
-		  	    
-	  #Configuration for the reassociation of gamma conversion particles
-          ConversionsCollection = cms.InputTag('allConversions'),
-	   
-	  #Configuration for the reassociation of particles from V0 decays
-          V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
-          V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
-	   
-	  #Configuration for the reassociation of particles from nuclear interactions
-          NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+          VertexPFCandAssociationMap = cms.InputTag('PFCandAssoMap'),
 )
 
