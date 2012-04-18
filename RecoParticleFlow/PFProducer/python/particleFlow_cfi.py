9fe96e74faeba515fde0b0ac0e9b9075d87b5a33
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-04-18 10:32:08 +0000

    push back our long term plan back to the head: enable HO

--- RecoParticleFlow/PFProducer/python/particleFlow_cfi.py
+++ RecoParticleFlow/PFProducer/python/particleFlow_cfi.py
@@ -21,7 +21,7 @@ particleFlowTmp = cms.EDProducer("PFProducer",
     debug = cms.untracked.bool(False),
 
     # Use HO clusters in PF hadron reconstruction
-    useHO = cms.bool(False),                                 
+    useHO = cms.bool(True),                                 
 
     # Use electron identification in PFAlgo
     usePFElectrons = cms.bool(True),
