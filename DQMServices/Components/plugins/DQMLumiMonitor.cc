eac94f125e0106760e1ed5a4a7c6a3bbcebdc3b5
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2012-04-20 06:49:08 +0000

    Updating DQMLumiMonitor to access Luminosity information from LumiSummary and Lumidetails of the Luminosity block instead of ESProducer. We need to include the EDProducer in the execution sequence which keeps the information

--- DQMServices/Components/plugins/DQMLumiMonitor.cc
+++ DQMServices/Components/plugins/DQMLumiMonitor.cc
@@ -2,8 +2,8 @@
  * \file DQMLumiMonitor.cc
  * \author S. Dutta
  * Last Update:
- * $Date: 2012/04/11 07:56:56 $
- * $Revision: 1.3 $
+ * $Date: 2012/04/13 14:10:24 $
+ * $Revision: 1.4 $
  * $Author: dutta $
  *
  * Description: Pixel Luminosity Monitoring 
@@ -14,11 +14,10 @@
 #include "FWCore/Framework/interface/LuminosityBlock.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
-#include "RecoLuminosity/LumiProducer/interface/DIPLuminosityRcd.h" 
-#include "RecoLuminosity/LumiProducer/interface/DIPLumiSummary.h" 
+#include "DataFormats/Luminosity/interface/LumiDetails.h"
+#include "DataFormats/Luminosity/interface/LumiSummary.h"
 #include "TPRegexp.h"
 
 
@@ -110,27 +109,29 @@ void DQMLumiMonitor::analyze(edm::Event const& iEvent, edm::EventSetup const& iS
 
 void DQMLumiMonitor::endLuminosityBlock(edm::LuminosityBlock const& lumiBlock, edm::EventSetup const& eSetup){
   std::cout <<" Run Number "<<lumiBlock.run() <<" Lumi Section Numnber "<< lumiBlock.luminosityBlock()<<std::endl;
+
   nLumi_ = lumiBlock.luminosityBlock();
-  edm::eventsetup::EventSetupRecordKey recordKey(edm::eventsetup::EventSetupRecordKey::TypeTag::findType(lumiRecordName_));
-  
-  if( recordKey.type() == edm::eventsetup::EventSetupRecordKey::TypeTag()) {
-    std::cout <<"Record \"DIPLuminosityRcd"<<"\" does not exist "<<std::endl;
-    return;
-  }
-  edm::ESHandle<DIPLumiSummary> datahandle;
-  eSetup.getData(datahandle);
-  if(datahandle.isValid()){
-    const DIPLumiSummary* dipLumiSummary_=datahandle.product();
-    if (!dipLumiSummary_->isNull()) {
-      intLumi_ = dipLumiSummary_->intgDelLumiByLS();
-      edm::LogInfo("PixelLumiMonotor") <<" Luminosity in this Lumi Section " << intLumi_ ;
-      intLumiVsLSME_->Fill(nLumi_, intLumi_);
-    }else{  
-      edm::LogError("PixelLumiMonotor") << "Empty data found !";
-    } 
+
+  // Access Lumi Summary
+  edm::Handle<LumiSummary> lumiSummary_;
+  lumiBlock.getByLabel(lumiRecordName_, lumiSummary_);
+  if(lumiSummary_->isValid()){
+    intLumi_ = lumiSummary_->intgDelLumi();
+    edm::LogInfo("PixelLumiMonotor") <<" Luminosity in this Lumi Section " << intLumi_ ;
+    intLumiVsLSME_->Fill(nLumi_, intLumi_);
   } else {
     edm::LogError("PixelLumiMonotor") << "No valid data found!";
   }
+  /*
+  // Access Lumi Details
+  Handle<LumiDetails> lumiDetails;
+  lumiBlock.getByLabel("expressLumiProducer", lumiDetails);
+  if(lumiDetails->isValid()){
+    std::cout<<"valid detail"<<std::endl;
+  }else{
+    std::cout << "no valid lumi detail data" <<std::endl;
+  }  */
+  
 }
 
 void DQMLumiMonitor::endRun(edm::Run const& iRun, edm::EventSetup const& iSetup) {
