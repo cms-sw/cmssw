1c9043bb191bdcd6ddc0c4b990a6fbad1d71e188
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2012-04-21 00:57:19 +0000

    Fix example of displaying geometry with cmsRun.

--- Fireworks/Geometry/python/displayGeom_cfg.py
+++ Fireworks/Geometry/python/displayGeom_cfg.py
@@ -1,18 +1,34 @@
+#################################################
+#
+# Please run the script with cmsRun:
+# 
+# cmsRun displayGeom_cfg.py
+#
+#################################################
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("DISPLAY")
+
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
+
+
 
 process.source = cms.Source("EmptySource")
 
-process.add_( cms.ESProducer("TGeoMgrFromDdd") )
+process.EveService = cms.Service("EveService")
 
-process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(1)
-        )
-process.dump = cms.EDAnalyzer("DisplayGeom",
-                                verbose = cms.untracked.bool(False),
-                                level = cms.untracked.int32(11)
-                            )
+### Extractor of geometry needed to display it in Eve.
+### Required for "DummyEvelyser".
+process.add_( cms.ESProducer(
+        "TGeoMgrFromDdd",
+        verbose = cms.untracked.bool(False),
+        level   = cms.untracked.int32(8)
+))
+
+process.dump = cms.EDAnalyzer(
+    "DisplayGeom"
+
+)
 
 process.p = cms.Path(process.dump)
