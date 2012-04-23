1e31978d1941a104960d1874e7c845f540c531f4
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2012-04-23 13:59:51 +0000

    remove kt6 recalculation, to be consistent with JetMET recommendations and to optimize speed and since this is done by PAT in 52X

--- CommonTools/ParticleFlow/python/Tools/enablePileUpCorrection.py
+++ CommonTools/ParticleFlow/python/Tools/enablePileUpCorrection.py
@@ -14,21 +14,6 @@ def enablePileUpCorrectionInPF2PAT( process, postfix, sequence='PF2PAT'):
     getattr(process,"pfJets"+postfix).doAreaFastjet = True
     getattr(process,"pfJets"+postfix).doRhoFastjet = False
         
-    from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
-    
-    setattr( process, 'kt6PFJets'+postfix, kt4PFJets.clone( rParam = cms.double(0.6),
-                                                            src = cms.InputTag('pfNoElectron'+postfix),
-                                                            doAreaFastjet = cms.bool(True),
-                                                            doRhoFastjet = cms.bool(True)
-                                                            # voronoiRfact = cms.double(0.9)
-                                                            )
-             )
-
-    # adding kt6PFJets
-    getattr(process,'pfJetSequence'+postfix).replace( getattr(process,"pfJets"+postfix),
-                                                      getattr(process,"kt6PFJets"+postfix) +
-                                                      getattr(process,"pfJets"+postfix) )
-        
     # adding goodOfflinePrimaryVertices before pfPileUp
     process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')   
     getattr(process, 'pfNoPileUpSequence'+postfix).replace( getattr(process,"pfPileUp"+postfix),
@@ -39,7 +24,8 @@ def enablePileUpCorrectionInPAT( process, postfix, sequence ):
     # PAT specific stuff:
 
     jetCorrFactors = getattr(process,"patJetCorrFactors"+postfix)
-    jetCorrFactors.rho = cms.InputTag("kt6PFJets"+postfix, "rho")
+    # using non-pileup-charged-hadron-substracted kt6PFJets consistently with JetMET recommendation
+    jetCorrFactors.rho = cms.InputTag("kt6PFJets", "rho")
 
 
 def enablePileUpCorrection( process, postfix, sequence='patPF2PATSequence'):
