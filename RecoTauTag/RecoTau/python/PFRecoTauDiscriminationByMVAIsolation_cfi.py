95cad6649377d918dd468a9c0e14c50ca7e8616a
Matthew Chan <sha1-88a3f655540d481775c944e2d27613d911c20175@cern.ch>
2012-04-23 13:04:56 +0000

    Initial commit of ring-based tau isolation (MVA isolation)

---
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByMVAIsolation_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+from RecoTauTag.RecoTau.TauDiscriminatorTools import requireLeadTrack
+
+pfRecoTauDiscriminationByMVAIsolation = cms.EDProducer(
+    "PFRecoTauDiscriminationByMVAIsolation",
+    PFTauProducer = cms.InputTag('pfRecoTauProducer'),
+    rhoProducer = cms.InputTag('kt6PFJetsForRhoComputationVoronoi','rho'),
+    Prediscriminants = requireLeadTrack,
+    gbrfFilePath = cms.FileInPath('RecoTauTag/RecoTau/data/gbrfTauIso.root')
+    )
