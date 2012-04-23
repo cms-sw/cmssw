a49e25a88ff1eabd334e4612cf3e46c1edf037db
Roberval Walsh Bastos Rangel <sha1-c53865588b959fce0b759d40c710029984d01f4f@cern.ch>
2012-04-23 16:50:58 +0000

    BTV skim for efficiencies and ZHbb analysis

---
+++ DPGAnalysis/Skims/python/ZHbbSkim_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+#Trigger bit requirement
+import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
+ZHbbSkim = hlt.hltHighLevel.clone()
+ZHbbSkim.TriggerResultsTag = cms.InputTag( "TriggerResults", "", "HLT" )
+ZHbbSkim.HLTPaths = cms.vstring(
+    "HLT_L1ETM40_v*",
+    "HLT_DiCentralPFJet30_PFMHT80_v*" )
+ZHbbSkim.andOr = cms.bool( True )
+ZHbbSkim.throw = cms.bool( False )
+
