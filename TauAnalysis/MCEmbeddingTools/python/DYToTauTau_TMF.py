52f2e3af0817cff8e4cb7280b530f1d766d494c1
Tomasz Maciej Frueboes <sha1-2994ee7f07815af3dae4763f2de9aa5185898879@cern.ch>
2012-04-24 12:40:01 +0000

    Add missing params

--- TauAnalysis/MCEmbeddingTools/python/DYToTauTau_TMF.py
+++ TauAnalysis/MCEmbeddingTools/python/DYToTauTau_TMF.py
@@ -5,7 +5,9 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.GenProduction.DYToTauTau_M_20_TuneZ2_7TeV_pythia6_tauola_cff import generator
 
 tfFilter = cms.EDFilter("DYGenFilter",
-  code = cms.untracked.int32(15)
+  code = cms.untracked.int32(15),
+  etaMax = cms.untracked.double(2.5),
+  ptMin  = cms.untracked.double(15)
 )
 
 
