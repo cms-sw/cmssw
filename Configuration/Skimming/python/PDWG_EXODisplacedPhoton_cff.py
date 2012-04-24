d82601b09b40bf7bcb92ae6eecf2f4ae939162c5
Michael Sigamani <sha1-baf67b3063ac3d707170746a1bac0711fe781273@cern.ch>
2012-04-24 15:17:11 +0000

    EXODisplacedPhoton update

--- Configuration/Skimming/python/PDWG_EXODisplacedPhoton_cff.py
+++ Configuration/Skimming/python/PDWG_EXODisplacedPhoton_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 #Trigger bit requirement
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 EXODisplacedPhoton = hlt.triggerResultsFilter.clone()
-EXODisplacedPhoton.triggerConditions = cms.vstring('HLT_DisplacedPhoton65_CaloIdVL_IsoL_PFMET25_v*',)
+EXODisplacedPhoton.triggerConditions = cms.vstring('HLT_DisplacedPhoton65*',)
 EXODisplacedPhoton.hltResults = cms.InputTag( "TriggerResults", "", "HLT" )
 EXODisplacedPhoton.l1tResults = cms.InputTag("")
 #EXODisplacedPhoton.throw = cms.bool( False )
