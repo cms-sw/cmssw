6eb65ef055abe05ff61cc6f43ae241c274007489
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-04-25 13:09:35 +0000

    new beamspot

--- FastSimulation/Configuration/python/CommonInputsFake_cff.py
+++ FastSimulation/Configuration/python/CommonInputsFake_cff.py
@@ -3,9 +3,17 @@ import FWCore.ParameterSet.Config as cms
 from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
 
 # Primary vertex smearing.
-fastsimPrimaryVertex = 'Realistic8TeV2012'
 from IOMC.EventVertexGenerators.VtxSmearedGauss_cfi import *
-
+fastsimPrimaryVertex = 'Realistic8TeV2012' # this is the single place in FastSim where the beamspot scenario is chosen; this choice is propagated to famosSimHits and famosPileUp; in the future, more modularity will be possible when the famosPileUp module will be deprecated
+if(fastsimPrimaryVertex=='Realistic8TeV'):
+    from FastSimulation.Event.Realistic8TeVCollisionVertexGenerator_cfi import *
+elif(fastsimPrimaryVertex=='Realistic8TeV2012'):
+    from FastSimulation.Event.Realistic8TeV2012CollisionVertexGenerator_cfi import *
+elif(fastsimPrimaryVertex=='Realistic7TeV2011'):
+    from FastSimulation.Event.Realistic7TeV2011CollisionVertexGenerator_cfi import *
+else: # by default, the currently recommended one
+    from FastSimulation.Event.Realistic8TeV2012CollisionVertexGenerator_cfi import *
+         
 # The Geometries
 from FastSimulation.Configuration.Geometries_cff import *
 
