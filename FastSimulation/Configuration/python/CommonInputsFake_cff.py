80551b65fc493ec4d19c866a44052964857ff587
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-04-25 13:13:27 +0000

    avoid duplication between CommonInputs and CommonInputsFake

--- FastSimulation/Configuration/python/CommonInputsFake_cff.py
+++ FastSimulation/Configuration/python/CommonInputsFake_cff.py
@@ -1,24 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
-
-# Primary vertex smearing.
-from IOMC.EventVertexGenerators.VtxSmearedGauss_cfi import *
-fastsimPrimaryVertex = 'Realistic8TeV2012' # this is the single place in FastSim where the beamspot scenario is chosen; this choice is propagated to famosSimHits and famosPileUp; in the future, more modularity will be possible when the famosPileUp module will be deprecated
-if(fastsimPrimaryVertex=='Realistic8TeV'):
-    from FastSimulation.Event.Realistic8TeVCollisionVertexGenerator_cfi import *
-elif(fastsimPrimaryVertex=='Realistic8TeV2012'):
-    from FastSimulation.Event.Realistic8TeV2012CollisionVertexGenerator_cfi import *
-elif(fastsimPrimaryVertex=='Realistic7TeV2011'):
-    from FastSimulation.Event.Realistic7TeV2011CollisionVertexGenerator_cfi import *
-else: # by default, the currently recommended one
-    from FastSimulation.Event.Realistic8TeV2012CollisionVertexGenerator_cfi import *
-         
-# The Geometries
-from FastSimulation.Configuration.Geometries_cff import *
-
-#The Magnetic Field ESProducer's 
-from FastSimulation.ParticlePropagator.MagneticFieldMapESProducer_cfi import *
+from FastSimulation.Configuration.CommonInputs_cff.py import *
 
 # The muon digi calibration
 from CalibMuon.Configuration.DT_FakeConditions_cff import *
