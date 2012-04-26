ca38f0ceb842a145a3b24797c4ecf397aa2c6b23
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2012-04-26 16:26:12 +0000

    Add more useful functions for unpacking of QDC data

--- RecoTBCalo/HcalTBObjectUnpacker/interface/HcalTBQADCUnpacker.h
+++ RecoTBCalo/HcalTBObjectUnpacker/interface/HcalTBQADCUnpacker.h
@@ -9,6 +9,10 @@ public:
   HcalTBQADCUnpacker();
   void unpack(const FEDRawData& raw,
 	      HcalTBBeamCounters& beamadc, bool is04_=true) const;
+  void unpackRaw(const FEDRawData& raw, std::vector<uint16_t>& values, bool is04_=true) const;
+  void unpackWithGains(const FEDRawData& raw, std::vector<double>& values, bool is04_=true) const;
+
+  void setCalib(int chan, double ped, double gain);
   void setCalib(const std::vector<std::vector<std::string> >& calibLines_);
 private:
   bool isTB04_;
