f97de234c3596e14dc7075e6f5f8f108ce7e3322
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2012-04-26 07:47:06 +0000

    new framework

--- DQMOffline/Ecal/python/ecal_dqm_client_offline_cosmic_cff.py
+++ DQMOffline/Ecal/python/ecal_dqm_client_offline_cosmic_cff.py
@@ -1,7 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
+from DQM.EcalBarrelMonitorClient.EcalMonitorClient_cfi import *
+
+# placeholder
 from DQM.EcalBarrelMonitorClient.EcalBarrelMonitorClient_cfi import *
 from DQM.EcalEndcapMonitorClient.EcalEndcapMonitorClient_cfi import *
+
+from DQM.EcalCommon.EcalDQMBinningService_cfi import *
 
 dqmQTestEB = cms.EDAnalyzer("QualityTester",
 #    reportThreshold = cms.untracked.string('red'),
@@ -25,26 +30,16 @@ dqmQTestEE = cms.EDAnalyzer("QualityTester",
     verboseQT = cms.untracked.bool(False)
 )
 
-eb_dqm_client_offline = cms.Sequence(
-    ecalBarrelMonitorClient *
-    dqmQTestEB
-    )
-
-ee_dqm_client_offline = cms.Sequence(
-    ecalEndcapMonitorClient *
-    dqmQTestEE
-    )
-
 ecal_dqm_client_offline = cms.Sequence(
-    eb_dqm_client_offline *
-    ee_dqm_client_offline
-    )
+    ecalMonitorClient
+)
 
-ecalBarrelMonitorClient.location = 'P5'
-ecalBarrelMonitorClient.verbose = False
-ecalBarrelMonitorClient.enabledClients = ['Integrity', 'StatusFlags', 'Occupancy', 'PedestalOnline', 'Cosmic', 'Cluster', 'TriggerTower', 'Summary']
-
-ecalEndcapMonitorClient.location = 'P5'
-ecalEndcapMonitorClient.verbose = False
-ecalEndcapMonitorClient.enabledClients = ['Integrity', 'StatusFlags', 'Occupancy', 'PedestalOnline', 'Cosmic', 'Cluster', 'TriggerTower', 'Summary']
+ecalMonitorClient.clients = cms.untracked.vstring(
+    "IntegrityClient",
+    "OccupancyClient",
+    "PresampleClient",
+    "RawDataClient",
+    "TimingClient",
+    "SummaryClient"
+)
 
