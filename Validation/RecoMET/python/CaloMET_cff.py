2a97ff269e96d23eb09c18d25f04e8636e9e0905
Samantha Kaushalya Hewamanage <sha1-f566ca6ecd5cb4f745ec52d3255758d360a64a51@cern.ch>
2012-04-26 15:14:17 +0000

    added a sequence to analyse only caloMet. This is to reduce thE load on DQM server by removing unwanted stuff.

--- Validation/RecoMET/python/CaloMET_cff.py
+++ Validation/RecoMET/python/CaloMET_cff.py
@@ -5,7 +5,11 @@ import FWCore.ParameterSet.Config as cms
 # Date: 03.04.2008
 #
 # Fill validation histograms for MET. Assumes met, metNoHF, metOpt, metOptNoHF are in the event
+# Added met only sequence. Removing the unsed met collections to reduce the load
+# on DQM server. - Samantha Hewamanage (samantha@cern.ch) 04-26-2012
+
 from Validation.RecoMET.CaloMET_cfi import *
 analyzeCaloMETNoHO = cms.Sequence(metAnalyzer*metNoHFAnalyzer*metOptAnalyzer*metOptNoHFAnalyzer)
 analyzeCaloMETHO = cms.Sequence(metHOAnalyzer*metNoHFHOAnalyzer*metOptHOAnalyzer*metOptNoHFHOAnalyzer)
 analyzeCaloMET = cms.Sequence(analyzeCaloMETNoHO*analyzeCaloMETHO)
+analyzeCaloMETonly = cms.Sequence(metAnalyzer)
