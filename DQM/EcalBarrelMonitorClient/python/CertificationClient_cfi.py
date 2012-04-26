0e0f1acd9ca4c8c802e60298be709cdba6a94e63
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2012-04-26 09:45:18 +0000

    certification

---
+++ DQM/EcalBarrelMonitorClient/python/CertificationClient_cfi.py
@@ -0,0 +1,11 @@
+certificationClient = dict(
+)
+
+certificationClientPaths = dict(
+    CertificationMap = "EventInfo/CertificationSummaryMap",
+    CertificationContents = "EventInfo/CertificationContents/",
+    CertificationSummary = "EventInfo/CertificationSummary",
+    ReportSummaryMap = "EventInfo/reportSummaryMap",
+    ReportSummaryContents = "EventInfo/reportSummaryContents/",
+    ReportSummary = "EventInfo/reportSummary"
+)
--- DQM/EcalEndcapMonitorTasks/python/EEDaqInfoTask_cfi.py
+++ DQM/EcalEndcapMonitorTasks/python/EEDaqInfoTask_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-ecalEndcapDaqInfoTask = cms.EDAnalyzer("EEDaqInfoTask",
-    prefixME = cms.untracked.string('EcalEndcap'),
-    enableCleanup = cms.untracked.bool(False),
-    mergeRuns = cms.untracked.bool(False),
-)
+# ecalEndcapDaqInfoTask = cms.EDAnalyzer("EEDaqInfoTask",
+#     prefixME = cms.untracked.string('EcalEndcap'),
+#     enableCleanup = cms.untracked.bool(False),
+#     mergeRuns = cms.untracked.bool(False),
+# )
+
+ecalEndcapDaqInfoTask = cms.Sequence()
