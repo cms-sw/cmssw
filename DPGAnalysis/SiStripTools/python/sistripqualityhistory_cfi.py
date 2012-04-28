64eedf65b1e2c9223b57e1a04ad3ed2affaecee8
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-04-28 19:42:16 +0000

    added histogram to minitor the number of bad modules per event

--- DPGAnalysis/SiStripTools/python/sistripqualityhistory_cfi.py
+++ DPGAnalysis/SiStripTools/python/sistripqualityhistory_cfi.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 ssqhistory = cms.EDAnalyzer("SiStripQualityHistory",
-                                 granularityMode=cms.untracked.uint32(2),  # 1 = Module , 2= Fiber , 3= APV 
-                                 monitoredSiStripQuality = cms.VPSet(
+                            eventProcessing = cms.bool(False),
+                            granularityMode=cms.untracked.uint32(2),  # 1 = Module , 2= Fiber , 3= APV 
+                            monitoredSiStripQuality = cms.VPSet(
 #    cms.PSet( name = cms.string("Cabling"), ssqLabel = cms.string("onlyCabling")),  # name= used in histos, ssqLabel= label of SiStripQuality object
 #    cms.PSet( name = cms.string("RunInfo"), ssqLabel = cms.string("CablingRunInfo")),
 #    cms.PSet( name = cms.string("BadChannel"), ssqLabel = cms.string("BadChannel")),
