d2bef4066a179674d662d2d6bcd772694f704d97
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-05-01 11:35:34 +0000

    removed kt6PFJets and ak5PFJets modules (not needed anymore since rho and jet area computations are enabled per default)

--- JetMETCorrections/Type1MET/python/pfMETCorrections_cff.py
+++ JetMETCorrections/Type1MET/python/pfMETCorrections_cff.py
@@ -4,23 +4,6 @@ import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
 
 #--------------------------------------------------------------------------------
-# produce rho parameters needed for L1FastJet corrections
-from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
-kt6PFJets = kt4PFJets.clone(
-    src = cms.InputTag('particleFlow'),
-    rParam = cms.double(0.6),
-    doRhoFastjet = cms.bool(True),
-    Rho_EtaMax = cms.double(2.5)
-)
-
-# CV: need to rerun 'ak5PFJets' module with jet area computation enabled,
-#     since it has not been enabled per default in CMSSW_4_2_x
-#    (if the jet area of 'ak5PFJets' is zero, the L1FastjetCorrector::correction function always returns 1.0)
-from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
-ak5PFJets.doAreaFastjet = cms.bool(True)
-#--------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------
 # select PFCandidates ("unclustered energy") not within jets
 # for Type 2 MET correction
 from CommonTools.ParticleFlow.TopProjectors.pfNoJet_cfi import pfNoJet
@@ -104,9 +87,7 @@ pfType1p2CorrectedMet = cms.EDProducer("CorrectedPFMETProducer",
 #--------------------------------------------------------------------------------
 # define sequence to run all modules
 producePFMETCorrections = cms.Sequence(
-    kt6PFJets
-   * ak5PFJets
-   * pfCandsNotInJet
+    pfCandsNotInJet
    * pfJetMETcorr
    * pfCandMETcorr
    * pfchsMETcorr
