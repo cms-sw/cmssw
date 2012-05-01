6e3816b7205e325c05dbbd143e4f8b1da87ba088
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2012-05-01 13:42:11 +0000

    restoring old standard sequence

--- DQM/EcalEndcapMonitorTasks/python/EEDcsInfoTask_cfi.py
+++ DQM/EcalEndcapMonitorTasks/python/EEDcsInfoTask_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-# ecalEndcapDcsInfoTask = cms.EDAnalyzer("EEDcsInfoTask",
-#     prefixME = cms.untracked.string('EcalEndcap'),
-#     enableCleanup = cms.untracked.bool(False),
-#     mergeRuns = cms.untracked.bool(False)
-# )
+ecalEndcapDcsInfoTask = cms.EDAnalyzer("EEDcsInfoTask",
+    prefixME = cms.untracked.string('EcalEndcap'),
+    enableCleanup = cms.untracked.bool(False),
+    mergeRuns = cms.untracked.bool(False)
+)
 
-ecalEndcapDcsInfoTask = cms.Sequence()
+# ecalEndcapDcsInfoTask = cms.Sequence()
