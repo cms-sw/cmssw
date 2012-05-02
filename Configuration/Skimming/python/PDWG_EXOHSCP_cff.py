184d6ee7bfa87e171d2b673b00c51fdc21516bea
Loic Quertenmont <sha1-87ca56b44ac7447a9c679691ad6ed79ac661d179@cern.ch>
2012-05-02 08:00:07 +0000

    adding one missing comma

--- Configuration/Skimming/python/PDWG_EXOHSCP_cff.py
+++ Configuration/Skimming/python/PDWG_EXOHSCP_cff.py
@@ -16,7 +16,8 @@ HSCPTrigger.HLTPaths = [
             "HLT_HT650_*",
             "HLT_MET80_*",
             "HLT_PFMET*",
-            "HLT_L2Mu70_eta2p1_PFMET65", "HLT_L2Mu80_eta2p1_PFMET70"
+            "HLT_L2Mu70_eta2p1_PFMET65", 
+            "HLT_L2Mu80_eta2p1_PFMET70",
             "HLT_*L2Mu*_NoBPTX*"
 		]
 hltSeq = cms.Sequence(HSCPTrigger)  
