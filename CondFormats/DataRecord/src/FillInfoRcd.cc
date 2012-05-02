088b3212068759163a2d15dd81cf944767e906d4
Salvatore Di Guida <sha1-744703a2e531bf35de4c8c886b1940546bb70d35@cern.ch>
2012-05-02 19:22:50 +0000

    added Record for FillInfo Condition Object

---
+++ CondFormats/DataRecord/src/FillInfoRcd.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     FillInfoRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      
+// Created:     Tue Apr 10 19:38:43 CEST 2012
+// $Id$
+
+#include "CondFormats/DataRecord/interface/FillInfoRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(FillInfoRcd);
