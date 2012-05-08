56e1b13b5c746e778165f3d7754a126b11995ca5
Francesco Santanastasio <sha1-89406cb43660773ab322fafa675b9f4e8b627eb0@cern.ch>
2012-05-08 16:44:40 +0000

    removed summary output by default

--- Configuration/Skimming/test/createSkimConfigForEachPDs.py
+++ Configuration/Skimming/test/createSkimConfigForEachPDs.py
@@ -15,9 +15,13 @@ os.system( 'mkdir %s' %(testdirname) )
 for k in autoSkim:
    #    print k
    #print ('cmsDriver.py skims -s SKIM:@%s --data --no_exec --dbs \"find file,file.parent where dataset=/%s/Run2012A-PromptReco-v1/RECO and run=190705\" -n 100 --conditions auto:com10 --python_filename=skim_%s.py' %(k,k,k))
-   os.system('cmsDriver.py skims -s SKIM:@%s --data --no_exec --dbs \"find file,file.parent where dataset=/%s/Run2012A-PromptReco-v1/RECO and run=190705\" -n 100 --conditions auto:com10 --python_filename=skim_%s.py' %(k,k,k))
+#   os.system('cmsDriver.py skims -s SKIM:@%s --data --no_exec --dbs \"find file,file.parent where dataset=/%s/Run2012A-PromptReco-v1/RECO and run=190705\" -n 100 --conditions auto:com10 --python_filename=skim_%s.py' %(k,k,k))
+   os.system('cmsDriver.py skims -s SKIM:@%s --data --no_exec --dbs \"find file,file.parent where dataset=/%s/Run2012A-PromptReco-v1/RECO and run=191277\" -n 100 --conditions auto:com10 --python_filename=skim_%s.py' %(k,k,k))
    os.system('mkdir -p %s/%s' %(testdirname,k))
-   os.system('echo \"process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )\" >> skim_%s.py' %(k))
+######################################################
+# uncomment below if you want to get a summary from your test
+######################################################
+#   os.system('echo \"process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )\" >> skim_%s.py' %(k))
    os.system('cp skim_%s.py %s/%s' %(k,testdirname,k))
    os.system('mv skim_%s.py %s' %(k,tier1dirname))
 ######################################################
