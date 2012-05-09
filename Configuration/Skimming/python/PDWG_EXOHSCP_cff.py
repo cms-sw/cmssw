7dd13349e4b921650c22544d4c7e6eccd1b979eb
Loic Quertenmont <sha1-87ca56b44ac7447a9c679691ad6ed79ac661d179@cern.ch>
2012-05-09 15:54:57 +0000

    changes for 7E33

--- Configuration/Skimming/python/PDWG_EXOHSCP_cff.py
+++ Configuration/Skimming/python/PDWG_EXOHSCP_cff.py
@@ -18,7 +18,8 @@ HSCPTrigger.HLTPaths = [
             "HLT_PFMET*",
             "HLT_L2Mu70_eta2p1_PFMET65", 
             "HLT_L2Mu80_eta2p1_PFMET70",
-            "HLT_*L2Mu*_NoBPTX*"
+            "HLT_*L2Mu*_NoBPTX*",
+            "HLT_L2Mu70_eta2p1_PFMET*",
 		]
 hltSeq = cms.Sequence(HSCPTrigger)  
 
