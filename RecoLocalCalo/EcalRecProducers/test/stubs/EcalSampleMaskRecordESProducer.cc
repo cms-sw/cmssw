b2bdaaa8105e97bf202567cb8f7e2de353955b6f
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2012-05-10 08:23:23 +0000

    Added EcalSampleMask Record ESproducer for testing

---
+++ RecoLocalCalo/EcalRecProducers/test/stubs/EcalSampleMaskRecordESProducer.cc
@@ -0,0 +1,71 @@
+#include "boost/shared_ptr.hpp"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CondFormats/DataRecord/interface/EcalSampleMaskRcd.h"
+#include "CondFormats/EcalObjects/interface/EcalSampleMask.h"
+
+/**
+   ESProducer to fill the EcalSampleMaskRecord
+   starting from EcalChannelStatus information
+   
+
+   \author Stefano Argiro
+   \version $Id: EcalNextToDeadChannelESProducer.cc,v 1.1 2011/05/19 14:38:28 argiro Exp $
+   \date 9 May 2012
+*/
+
+
+class EcalSampleMaskRecordESProducer : public edm::ESProducer {
+  
+public:
+  EcalSampleMaskRecordESProducer(const edm::ParameterSet& iConfig);
+  
+  typedef boost::shared_ptr<EcalSampleMask> ReturnType;
+  
+  ReturnType produce(const EcalSampleMaskRcd& iRecord);
+  
+  unsigned int maskeb_;
+  unsigned int maskee_;
+
+};
+
+
+EcalSampleMaskRecordESProducer::EcalSampleMaskRecordESProducer(const edm::ParameterSet& iConfig){
+  //the following line is needed to tell the framework what
+  // data is being produced
+  setWhatProduced(this);
+
+  maskeb_ = iConfig.getParameter<unsigned int>("maskeb");
+  maskee_ = iConfig.getParameter<unsigned int>("maskee");
+
+}
+
+
+
+EcalSampleMaskRecordESProducer::ReturnType
+EcalSampleMaskRecordESProducer::produce(const EcalSampleMaskRcd& iRecord){
+
+  ReturnType returnRcd (new EcalSampleMask(maskeb_,maskee_));
+
+  return returnRcd ;
+
+}
+
+
+//define this as a plug-in
+DEFINE_FWK_EVENTSETUP_MODULE(EcalSampleMaskRecordESProducer);
+
+
+
+
+
+
+
+
+// Configure (x)emacs for this file ...
+// Local Variables:
+// mode:c++
+// compile-command: "cd .. ; scram b"
+// End:
