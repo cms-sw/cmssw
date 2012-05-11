f2fe1b799a61f449630cd4324c7912d122c6c2df
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-05-11 06:01:08 +0000

    Add missing virtual destructor.

--- L1Trigger/RPCTechnicalTrigger/interface/RBCConfiguration.h
+++ L1Trigger/RPCTechnicalTrigger/interface/RBCConfiguration.h
@@ -1,4 +1,4 @@
-// $Id: $
+// $Id: RBCConfiguration.h,v 1.1 2009/01/30 15:42:47 aosorio Exp $
 #ifndef INTERFACE_RBCCONFIGURATION_H 
 #define INTERFACE_RBCCONFIGURATION_H 1
 
@@ -20,7 +20,7 @@
 
 class RBCConfiguration {
 public: 
-  
+  virtual ~RBCConfiguration() {}
   virtual bool initialise()=0;
 
   virtual void preprocess(RBCInput &)=0;
