c1ff3ddbcfaeaf8275e1db0060b62ad3b4a55aed
Matthias Geisler <sha1-ca4f0a71d5bbadf63f826def6c188008b2bb8317@cern.ch>
2012-05-14 09:01:48 +0000

    New input for ignoring missing collection

--- CommonTools/RecoUtils/python/pfcand_assomap_cfi.py
+++ CommonTools/RecoUtils/python/pfcand_assomap_cfi.py
@@ -17,6 +17,9 @@ PFCandAssoMap = cms.EDProducer('PFCand_AssoMap',
           V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
 	   
 	  #Configuration for the reassociation of particles from nuclear interactions
-          NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+          NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),			    
+	   	   
+	  #What to do if the dipl vertex coll can't be found
+          ignoreMissingCollection = cms.bool(False),
 )
 
