52ec700aab7b1709aeb2e16acfd3535b7d679586
Joerg Behr <sha1-6fc71cbfef80ca4fef4a27bd037b04a2b7c5d6f8@cern.ch>
2012-05-14 12:33:10 +0000

    disable/enable jobs using config names

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_enablejob.pl
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_enablejob.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+# Author: Joerg Behr
 #This script enables jobs with the given job numbers.
 
 BEGIN {
@@ -6,16 +7,53 @@ use File::Basename;
 unshift(@INC, dirname($0)."/mpslib");
 }
 use Mpslib;
+use warnings;
 read_db();
 
 my @enabledjobs;
 my $count = 0;
-foreach my $arg  (@ARGV) {
-  $count++;
-  push @enabledjobs, $arg;
-}
+my $confname = "";
 
-if($count==0)
+while (@ARGV) {
+   my $arg = shift(@ARGV);
+    if ($arg =~ /-N/g) {
+      $confname = $arg;
+      $confname =~ s/-N//; # Strips away the "-N"
+      if (length($confname) == 0) {
+        $confname = shift(@ARGV);
+       }
+      $confname =~ s/\s//g;
+      if($confname =~ /\:/)
+        {
+          $confname =~ s/\://g;
+          print "colons were removed in configuration name because they are not allowed: $confname\n";
+        }
+    }
+   else
+     {
+       $count++;
+       push @enabledjobs, $arg;
+     }
+ }
+
+if($confname ne "")
+  {
+    print "Enable jobs: ${confname}.\n";
+    for (my $i=0; $i<@JOBID; ++$i) {
+      my $status = $JOBSTATUS[$i];
+      unless($JOBDIR[$i] =~ /jobm/)
+        {
+          my $name = $JOBSP3[$i];
+          if($name eq $confname)
+            {
+               my $status = $JOBSTATUS[$i];
+               $status =~ s/DISABLED//;
+               $JOBSTATUS[$i] = $status;
+             }
+        }
+    }
+  }
+elsif($count==0)
   {
     for (my $i=0; $i<@JOBID; ++$i) {
       my $status = $JOBSTATUS[$i];
