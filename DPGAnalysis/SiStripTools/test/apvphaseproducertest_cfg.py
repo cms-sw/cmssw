08fd3be5ad5dda9085a8763ea4e9672760e6084a
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-05-14 15:25:55 +0000

    added new paths to monitor in details cosmics run cluster multiplicity

--- DPGAnalysis/SiStripTools/test/apvphaseproducertest_cfg.py
+++ DPGAnalysis/SiStripTools/test/apvphaseproducertest_cfg.py
@@ -1,8 +1,22 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
 
 from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
 
 process = cms.Process("apvphaseTest")
+
+#prepare options
+
+options = VarParsing.VarParsing("analysis")
+
+options.register ('globalTag',
+                  "DONOTEXIST::All",
+                  VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  "GlobalTag")
+
+options.parseArguments()
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True),
@@ -48,11 +62,13 @@ process.MessageLogger.suppressWarning.append("onlineBeamSpot")
 process.MessageLogger.suppressInfo.append("newTracksFromV0")
 process.MessageLogger.suppressInfo.append("newTracksFromOtobV0")
 
+process.MessageLogger.suppressWarning.append("consecutiveHEs")
+
 
 #------------------------------------------------------------------
 
 #process.MessageLogger.cout.threshold = cms.untracked.string("DEBUG")
-#process.MessageLogger.debugModules = cms.untracked.vstring("APVPhases")
+#process.MessageLogger.debugModules = cms.untracked.vstring("eventtimedistribution")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -111,6 +127,71 @@ process.isoTriplets5000 = cms.EDFilter('EventWithHistoryEDFilter',
                                   )
                             )
 
+# Filter for no noisy bins
+
+process.load("DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_HugeEvents_AlCaReco_cfi")
+process.load("DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_firstpeak_AlCaReco_cfi")
+process.load("DPGAnalysis.SiStripTools.filters.Potential_TIBTEC_FrameHeaderEvents_additionalpeak_AlCaReco_cfi")
+
+process.latencyPlusOne = cms.EDFilter('EventWithHistoryEDFilter',
+                                      commonConfiguration = cms.untracked.PSet(historyProduct = cms.untracked.InputTag("consecutiveHEs"),
+                                                                               APVPhaseLabel  = cms.untracked.string("APVPhases"),
+                                                                               ),
+                                      filterConfigurations = cms.untracked.VPSet(cms.PSet(apvModes = cms.untracked.vint32(47),
+                                                                                          dbxRangeLtcyAware = cms.untracked.vint32(1,1)
+                                                                                          )
+                                                                                 )
+                                      )
+
+process.PotentialNewHugeEvents = cms.EDFilter('EventWithHistoryEDFilter',
+                                              commonConfiguration = cms.untracked.PSet(historyProduct = cms.untracked.InputTag("consecutiveHEs"),
+                                                                                       APVPhaseLabel  = cms.untracked.string("APVPhases"),
+                                                                                       ),
+                                              filterConfigurations = cms.untracked.VPSet(cms.PSet(apvModes = cms.untracked.vint32(37),
+                                                                                                  partitionName = cms.untracked.string("Any"),
+                                                                                                  absBXInCycleRangeLtcyAware = cms.untracked.vint32(12,12)
+                                                                                                  )
+                                                                                         )
+                                              )
+
+process.PotentialAdditionalHugeEvents = cms.EDFilter('EventWithHistoryEDFilter',
+                                                     commonConfiguration = cms.untracked.PSet(historyProduct = cms.untracked.InputTag("consecutiveHEs"),
+                                                                                              APVPhaseLabel  = cms.untracked.string("APVPhases"),
+                                                                                              ),
+                                                     filterConfigurations = cms.untracked.VPSet(cms.PSet(apvModes = cms.untracked.vint32(47),
+                                                                                                  partitionName = cms.untracked.string("Any"),
+                                                                                                  absBXInCycleRangeLtcyAware = cms.untracked.vint32(9,10)
+                                                                                                  )
+                                                                                         )
+                                              )
+
+
+process.seqNoNoisyBins = cms.Sequence(~process.PotentialTIBTECHugeEvents
+                                      + ~process.PotentialTIBTECFrameHeaderEventsFPeak
+                                      + ~process.PotentialTIBTECFrameHeaderEventsAdditionalPeak
+                                      + ~process.latencyPlusOne
+                                      + ~process.PotentialNewHugeEvents
+                                      + ~process.PotentialAdditionalHugeEvents
+                                      )
+
+
+# ------------------ Filters for peak and deco
+
+process.DecoEvents = cms.EDFilter('EventWithHistoryEDFilter',
+                                  commonConfiguration = cms.untracked.PSet(historyProduct = cms.untracked.InputTag("consecutiveHEs"),
+                                                                           APVPhaseLabel  = cms.untracked.string("APVPhases"),
+                                                                           ),
+                                  filterConfigurations = cms.untracked.VPSet(cms.PSet(apvModes = cms.untracked.vint32(37))
+                                                                             )
+                                  )
+process.PeakEvents = cms.EDFilter('EventWithHistoryEDFilter',
+                                  commonConfiguration = cms.untracked.PSet(historyProduct = cms.untracked.InputTag("consecutiveHEs"),
+                                                                           APVPhaseLabel  = cms.untracked.string("APVPhases"),
+                                                                           ),
+                                  filterConfigurations = cms.untracked.VPSet(cms.PSet(apvModes = cms.untracked.vint32(47))
+                                                                             )
+                                  )
+
 
 process.load("DPGAnalysis.SiStripTools.apvcyclephasemonitor_cfi")
 #process.apvcyclephasemonitorEC02011 = process.apvcyclephasemonitor.clone(apvCyclePhaseCollection = cms.InputTag("APVPhasesEC02011"))
@@ -119,13 +200,53 @@ process.load("DPGAnalysis.SiStripTools.apvcyclephasemonitor_cfi")
 #process.apvcyclephasemonitor2010 = process.apvcyclephasemonitor.clone(apvCyclePhaseCollection = cms.InputTag("APVPhases2010"))
 
 process.load("DPGAnalysis.SiStripTools.eventtimedistribution_cfi")
+process.eventtimedistribution.wantEWHDepthHisto = cms.untracked.bool(True)
+process.eventtimedistranydcs = process.eventtimedistribution.clone()
+process.eventtimedistrdeco = process.eventtimedistribution.clone()
+process.eventtimedistrpeak = process.eventtimedistribution.clone()
 process.eventtimedistrIsoPairs5000 = process.eventtimedistribution.clone()
 process.eventtimedistrIsoTriplets5000 = process.eventtimedistribution.clone()
+process.eventtimedistrnonoisybins = process.eventtimedistribution.clone()
+process.eventtimedistrnonoisybinsdeco = process.eventtimedistribution.clone()
+process.eventtimedistrnonoisybinspeak = process.eventtimedistribution.clone()
 #process.evttimedistrwithnophase = process.eventtimedistribution.clone(phasePartition = cms.untracked.string("None"))
 #process.evttimedistrwithphaseEC02011 = process.eventtimedistribution.clone(apvPhaseCollection = cms.InputTag("APVPhasesEC02011"))
 #process.evttimedistrwithphaseEC02010 = process.eventtimedistribution.clone(apvPhaseCollection = cms.InputTag("APVPhasesEC02010"))
 #process.evttimedistrwithphase2011 = process.eventtimedistribution.clone(apvPhaseCollection = cms.InputTag("APVPhases2011"))
 #process.evttimedistrwithphase2010 = process.eventtimedistribution.clone(apvPhaseCollection = cms.InputTag("APVPhases2010"))
+
+# bad component analysis
+
+import CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi
+
+process.ssqoffbadstrips = CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi.siStripQualityESProducer.clone()
+process.ssqoffbadstrips.appendToDataLabel = cms.string("offlineBadStrips")
+process.ssqoffbadstrips.ListOfRecordToMerge=cms.VPSet(
+ cms.PSet(record=cms.string('SiStripBadFiberRcd'),tag=cms.string(''))
+) 
+
+process.ssqdcsbadmodules = CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi.siStripQualityESProducer.clone()
+process.ssqdcsbadmodules.appendToDataLabel = cms.string("dcsBadModules")
+process.ssqdcsbadmodules.ListOfRecordToMerge=cms.VPSet(
+ cms.PSet(record=cms.string('SiStripDetVOffRcd'),tag=cms.string(''))
+) 
+
+
+process.load("DPGAnalysis.SiStripTools.sistripqualityhistory_cfi")
+process.ssqDCShistory = process.ssqhistory.clone()
+
+process.ssqhistory.maxLSBeforeRebin = cms.untracked.uint32(10)
+process.ssqhistory.startingLSFraction = cms.untracked.uint32(1)
+process.ssqhistory.granularityMode=cms.untracked.uint32(3)
+process.ssqhistory.monitoredSiStripQuality = cms.VPSet(
+    cms.PSet( name = cms.string("OfflineBadStrips"), ssqLabel = cms.string("offlineBadStrips"))
+    )# name= used in histos, ssqLabel= label of SiStripQuality object
+
+process.ssqDCShistory.granularityMode=cms.untracked.uint32(0)
+process.ssqDCShistory.monitoredSiStripQuality = cms.VPSet(
+    cms.PSet( name = cms.string("DCSBadModules"), ssqLabel = cms.string("dcsBadModules"))
+    )# name= used in histos, ssqLabel= label of SiStripQuality object
+
 
 
 # cluster analysis
@@ -135,21 +256,37 @@ process.load("DPGAnalysis.SiStripTools.sistripclustermultiplicityprod_cfi")
 process.load("DPGAnalysis.SiStripTools.ssclusmultinvestigator_cfi")
 process.ssclusmultinvestigator.scaleFactor = cms.untracked.int32(2)
 process.ssclusmultinvestigator.numberOfBins = cms.untracked.int32(1000)
+process.ssclusmultinvestdeco = process.ssclusmultinvestigator.clone()
+process.ssclusmultinvestpeak = process.ssclusmultinvestigator.clone()
 process.ssclusmultinvestIsoPairs5000 = process.ssclusmultinvestigator.clone()
 process.ssclusmultinvestIsoTriplets5000 = process.ssclusmultinvestigator.clone()
+process.ssclusmultinvestnonoisybins = process.ssclusmultinvestigator.clone()
+process.ssclusmultinvestnonoisybinsdeco = process.ssclusmultinvestigator.clone()
+process.ssclusmultinvestnonoisybinspeak = process.ssclusmultinvestigator.clone()
 
 
 process.load("DPGAnalysis.SiStripTools.ssclusmulttimecorrelations_cfi")
 process.ssclusmulttimecorrelations.historyProduct = cms.InputTag("consecutiveHEs")
 process.ssclusmulttimecorrelations.scaleFactors = cms.untracked.vint32(2)
 process.ssclusmulttimecorrelations.numberOfBins = cms.untracked.int32(1000)
-process.ssclusmulttimecorrelations.runHisto = cms.untracked.bool(False)
+process.ssclusmulttimecorrelations.runHisto = cms.untracked.bool(True)
 process.ssclusmulttimecorrelations.dbx3Histo = cms.untracked.bool(True)
 process.ssclusmulttimecorrelations.dbx3Histo3D = cms.untracked.bool(True)
+process.ssclusmulttimecorrpeak = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrpeak.runHisto = cms.untracked.bool(False)
+process.ssclusmulttimecorrdeco = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrdeco.runHisto = cms.untracked.bool(False)
+process.ssclusmulttimecorrnonoisybins = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrnonoisybinspeak = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrnonoisybinspeak.runHisto = cms.untracked.bool(False)
+process.ssclusmulttimecorrnonoisybinsdeco = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrnonoisybinsdeco.runHisto = cms.untracked.bool(False)
 process.ssclusmulttimecorrIsoPairs5000 = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrIsoPairs5000.runHisto = cms.untracked.bool(False)
 process.ssclusmulttimecorrIsoPairs5000.dbx3Histo = cms.untracked.bool(False)
 process.ssclusmulttimecorrIsoPairs5000.dbx3Histo3D = cms.untracked.bool(False)
 process.ssclusmulttimecorrIsoTriplets5000 = process.ssclusmulttimecorrelations.clone()
+process.ssclusmulttimecorrIsoTriplets5000.runHisto = cms.untracked.bool(False)
 #process.ssclusmulttimecorrphaseEC02011 = process.ssclusmulttimecorrelations.clone(apvPhaseCollection = cms.InputTag("APVPhasesEC02011"))
 #process.ssclusmulttimecorrphaseEC02010 = process.ssclusmulttimecorrelations.clone(apvPhaseCollection = cms.InputTag("APVPhasesEC02010"))
 #process.ssclusmulttimecorrphase2011 = process.ssclusmulttimecorrelations.clone(apvPhaseCollection = cms.InputTag("APVPhases2011"))
@@ -173,13 +310,25 @@ process.TFileService = cms.Service('TFileService',
                                    fileName = cms.string('apvcyclephaseTest.root')
                                    )
 
+#----GlobalTag ------------------------
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = options.globalTag
+
 #
+
+process.panydcs = cms.Path(process.consecutiveHEs
+                           + process.APVPhases
+                           + process.ssqDCShistory
+                           + process.eventtimedistranydcs
+                           )
 
 process.p0 = cms.Path(process.consecutiveHEs
                       + process.APVPhases
 #                      + process.APVPhasesEC02011 + process.APVPhasesEC02010 + process.APVPhases2011 + process.APVPhases2010
                       + process.dcsstatus
                       + process.apvcyclephasemonitor
+                      + process.ssqhistory
 #                      + process.apvcyclephasemonitorEC02011 + process.apvcyclephasemonitorEC02010
 #                      + process.apvcyclephasemonitor2011 + process.apvcyclephasemonitor2010
                       + process.eventtimedistribution
@@ -192,6 +341,24 @@ process.p0 = cms.Path(process.consecutiveHEs
 #                      + process.ssclusmulttimecorrphaseEC02011 + process.ssclusmulttimecorrphaseEC02010
 #                      + process.ssclusmulttimecorrphase2011 + process.ssclusmulttimecorrphase2010)
                       )
+
+process.ppeak = cms.Path(process.consecutiveHEs
+                         + process.APVPhases
+                         + process.dcsstatus
+                         + process.PeakEvents
+                         + process.eventtimedistrpeak
+                         + process.ssclustermultprod + process.ssclusmultinvestpeak
+                         + process.ssclusmulttimecorrpeak
+                         )
+
+process.pdeco = cms.Path(process.consecutiveHEs
+                         + process.APVPhases
+                         + process.dcsstatus
+                         + process.DecoEvents
+                         + process.eventtimedistrdeco
+                         + process.ssclustermultprod + process.ssclusmultinvestdeco
+                         + process.ssclusmulttimecorrdeco
+                         )
 
 process.pIsoPairs5000 = cms.Path(process.consecutiveHEs
                                  + process.APVPhases
@@ -212,4 +379,36 @@ process.pIsoTriplets5000 = cms.Path(process.consecutiveHEs
                                     + process.ssclusmultinvestIsoTriplets5000
                                     + process.ssclusmulttimecorrIsoTriplets5000
                                     )
+
+process.pnonoisybins = cms.Path(process.consecutiveHEs
+                                + process.APVPhases
+                                + process.dcsstatus
+                                + process.seqNoNoisyBins
+                                + process.ssclustermultprod 
+                                + process.eventtimedistrnonoisybins
+                                + process.ssclusmultinvestnonoisybins
+                                + process.ssclusmulttimecorrnonoisybins
+                                )
+
+process.pnonoisybinspeak = cms.Path(process.consecutiveHEs
+                                + process.APVPhases
+                                + process.dcsstatus
+                                + process.PeakEvents    
+                                + process.seqNoNoisyBins
+                                + process.ssclustermultprod 
+                                + process.eventtimedistrnonoisybinspeak
+                                + process.ssclusmultinvestnonoisybinspeak
+                                + process.ssclusmulttimecorrnonoisybinspeak
+                                )
+
+process.pnonoisybinsdeco = cms.Path(process.consecutiveHEs
+                                + process.APVPhases
+                                + process.dcsstatus
+                                + process.DecoEvents    
+                                + process.seqNoNoisyBins
+                                + process.ssclustermultprod 
+                                + process.eventtimedistrnonoisybinsdeco
+                                + process.ssclusmultinvestnonoisybinsdeco
+                                + process.ssclusmulttimecorrnonoisybinsdeco
+                                )
 
