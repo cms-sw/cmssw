54265c545cb1db5fd20c4c31515b640e82567c8b
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-05-15 10:14:10 +0000

    Add missing virtual destructor. This seems to be a genuine problem.

--- RecoTracker/TkTrackingRegions/interface/HitRZCompatibility.h
+++ RecoTracker/TkTrackingRegions/interface/HitRZCompatibility.h
@@ -6,6 +6,7 @@
 
 class HitRZCompatibility {
 public:
+  virtual ~HitRZCompatibility() {}
   typedef PixelRecoRange<float> Range;
   virtual bool operator() (const float & r, const float & z) const = 0;
   virtual Range range(const float & rORz) const = 0; 
