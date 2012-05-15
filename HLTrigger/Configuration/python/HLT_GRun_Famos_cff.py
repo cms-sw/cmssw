82f3e419a238477eaa888c095e6bf1ce04421c71
Juliette Alimena <sha1-8ce8f4c1f397c2d37216e2f844f3f59da506a691@cern.ch>
2012-05-15 08:58:36 +0000

    removed unassigned paths in DQM stream

--- HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,11 +1,11 @@
-# /online/collisions/2012/7e33/v2.2/HLT/V3 (CMSSW_5_2_5_HLT5)
+# /online/collisions/2012/7e33/v2.2/HLT/V5 (CMSSW_5_2_5_HLT5)
 
 import FWCore.ParameterSet.Config as cms
 from FastSimulation.HighLevelTrigger.HLTSetup_cff import *
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/online/collisions/2012/7e33/v2.2/HLT/V3')
+  tableName = cms.string('/online/collisions/2012/7e33/v2.2/HLT/V5')
 )
 
 hltESSBTagRecord = cms.ESSource( "EmptyESSource",
