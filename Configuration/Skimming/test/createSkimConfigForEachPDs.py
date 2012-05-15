093edf3ad9105e44b145a241f280f0b7eba2ca4b
Francesco Santanastasio <sha1-89406cb43660773ab322fafa675b9f4e8b627eb0@cern.ch>
2012-05-15 12:13:16 +0000

    do not execute by default

--- Configuration/Skimming/test/createSkimConfigForEachPDs.py
+++ Configuration/Skimming/test/createSkimConfigForEachPDs.py
@@ -34,7 +34,7 @@ for k in autoSkim:
    ######################################################
    # uncomment below if you want to get a summary from your test
    ######################################################
-   os.system('echo \"process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )\" >> skim_%s.py' %(k))
+   #os.system('echo \"process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )\" >> skim_%s.py' %(k))
 
    os.system('cp skim_%s.py %s/%s' %(k,testdirname,k))
    os.system('mv skim_%s.py %s' %(k,tier1dirname))
@@ -42,4 +42,4 @@ for k in autoSkim:
    ######################################################
    # uncomment below if you want to run a test on all the skims
    ######################################################
-   os.system('cd %s/%s ; cmsRun skim_%s.py > skim_%s.txt 2>&1' %(testdirname,k,k,k) )
+   #os.system('cd %s/%s ; cmsRun skim_%s.py > skim_%s.txt 2>&1' %(testdirname,k,k,k) )
