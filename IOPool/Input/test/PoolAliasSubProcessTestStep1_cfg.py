3f02f6f58cec81c7e2fa54bc14e7210d03916ae9
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-05-16 17:22:23 +0000

    Tests for new EDAlias feature

---
+++ IOPool/Input/test/PoolAliasSubProcessTestStep1_cfg.py
@@ -0,0 +1,69 @@
+# Configuration file for PrePoolInputTest 
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TESTPROD")
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(11)
+)
+process.Thing = cms.EDProducer("ThingProducer",
+    debugLevel = cms.untracked.int32(1)
+)
+
+process.source = cms.Source("EmptySource",
+    firstLuminosityBlock = cms.untracked.uint32(6),
+    numberEventsInLuminosityBlock = cms.untracked.uint32(3),
+    firstRun = cms.untracked.uint32(561),
+    numberEventsInRun = cms.untracked.uint32(7)
+)
+
+
+process.OtherThing = cms.EDProducer("OtherThingProducer",
+    thingLabel = cms.untracked.string("AltThing"),
+    debugLevel = cms.untracked.int32(1)
+)
+
+process.AltThing = cms.EDAlias(
+    Thing = cms.VPSet(
+      cms.PSet(type = cms.string('edmtestThings'),
+               fromProductInstance = cms.string('*'),
+               toProductInstance = cms.string('*'))
+    )
+)
+
+process.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring("keep *", "drop *_Thing_*_TESTPROD"),
+    fileName = cms.untracked.string('step1.root')
+)
+
+
+process.p = cms.Path(process.Thing*process.OtherThing)
+process.ep = cms.EndPath(process.output)
+
+analysisProcess = cms.Process("TESTANALYSIS")
+
+process.subProcess = cms.SubProcess(analysisProcess,
+   # Optional SelectEvents parameter can go here.
+   #outputCommands = cms.untracked.vstring("drop *")
+   outputCommands = cms.untracked.vstring("keep *", "drop *_Thing_*_TESTPROD")
+)
+
+# Configuration file for PoolInputTest
+
+analysisProcess.Analysis = cms.EDAnalyzer("OtherThingAnalyzer",
+    debugLevel = cms.untracked.int32(1)
+)
+
+
+analysisProcess.output = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring("keep *", "drop *_Thing_*_TESTPROD"),
+    fileName = cms.untracked.string('step2.root')
+)
+
+analysisProcess.p = cms.Path(analysisProcess.Analysis)
+analysisProcess.ep = cms.EndPath(analysisProcess.output)
+
+
+
