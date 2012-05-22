68ca3cdfc1b6587a3a95c29ff47d65f2d44e0917
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2012-05-22 10:03:17 +0000

    use KinFit hypothesis instead of MVA; run on skimmed PAT tuple and add/adjust several settings for tutorial with 5_2_5

--- TopQuarkAnalysis/Examples/test/analyzeTopHypotheses_cfg.py
+++ TopQuarkAnalysis/Examples/test/analyzeTopHypotheses_cfg.py
@@ -4,16 +4,20 @@ process = cms.Process("TEST")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
 process.MessageLogger.categories.append('TtSemiLeptonicEvent')
-process.MessageLogger.cerr.TtSemiLeptonicEvent = cms.untracked.PSet(
-    limit = cms.untracked.int32(-1)
-)
+process.MessageLogger.categories.append('TtSemiLepKinFitter')
 
 ## define input
-from TopQuarkAnalysis.TopEventProducers.tqafInputFiles_cff import relValTTbar
+#from TopQuarkAnalysis.TopEventProducers.tqafInputFiles_cff import relValTTbar
+#process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring(relValTTbar)
+#)
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(relValTTbar)
+    fileNames = cms.untracked.vstring(
+    ['rfio:///castor/cern.ch/user/s/snaumann/test/Spring12/TTJets_TuneZ2star_8TeV-madgraph-tauola_AODSIM_PU_S7_START52_V5-v1/PAT_muJets_1.root',
+     'rfio:///castor/cern.ch/user/s/snaumann/test/Spring12/TTJets_TuneZ2star_8TeV-madgraph-tauola_AODSIM_PU_S7_START52_V5-v1/PAT_muJets_2.root',
+     'rfio:///castor/cern.ch/user/s/snaumann/test/Spring12/TTJets_TuneZ2star_8TeV-madgraph-tauola_AODSIM_PU_S7_START52_V5-v1/PAT_muJets_3.root']
+    )
 )
 
 ## define maximal number of events to loop over
@@ -23,36 +27,46 @@ process.maxEvents = cms.untracked.PSet(
 
 ## configure process options
 process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(False)
+    wantSummary = cms.untracked.bool(True)
 )
 
 ## configure geometry & conditions
-#process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 from Configuration.AlCa.autoCond import autoCond
 process.GlobalTag.globaltag = autoCond['mc']
 
-## std sequence for pat
+## use genParticles to select only muon+jets events
+process.load("TopQuarkAnalysis.TopSkimming.ttDecayChannelFilters_cff")
+process.ttSemiLeptonicFilter.allowedTopDecays.decayBranchA.electron = False
+
+## standard PAT sequence
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 ## sequences for ttGenEvent and TtSemiLeptonicEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff")
 ## enable additional per-event printout from the TtSemiLeptonicEvent
-#process.ttSemiLepEvent.verbosity = 1
-## change maximum number of jets taken into account per event (default: 4)
-#process.ttSemiLepEvent.maxNJets = 5
-## change jet-parton matching algorithm
-#process.ttSemiLepJetPartonMatch.algorithm = "unambiguousOnly"
+process.ttSemiLepEvent.verbosity = 1
 
 ## choose which hypotheses to produce
-from TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff import addTtSemiLepHypotheses
-addTtSemiLepHypotheses(process,
-                       ["kMaxSumPtWMass", "kMVADisc"]
-                       )
+from TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff import *
+addTtSemiLepHypotheses(process, ["kMaxSumPtWMass", "kKinFit"])
+## change some common parameters
+#setForAllTtSemiLepHypotheses(process, "maxNJets", 5)
+#setForAllTtSemiLepHypotheses(process, "neutrinoSolutionType", 2)
+## use b-tagging for hypotheses (neglected for GenMatch)
+#setForAllTtSemiLepHypotheses(process, "useBTagging", True)
+#setForAllTtSemiLepHypotheses(process, "bTagAlgorithm", "combinedSecondaryVertexBJetTags")
+#setForAllTtSemiLepHypotheses(process, "minBDiscBJets"    , 0.679)
+#setForAllTtSemiLepHypotheses(process, "maxBDiscLightJets", 0.679)
+
+## change jet-parton matching algorithm
+#process.ttSemiLepJetPartonMatch.algorithm = "unambiguousOnly"
+## change constraints used in kinematic fit
+#process.kinFitTtSemiLepEventHypothesis.constraints = [1,2,6]
 
 ## load HypothesisAnalyzer
 process.load("TopQuarkAnalysis.Examples.HypothesisAnalyzer_cff")
@@ -63,7 +77,8 @@ process.TFileService = cms.Service("TFileService",
 )
 
 ## end path   
-process.path = cms.Path(process.patDefaultSequence *
+process.path = cms.Path(process.ttSemiLeptonicFilter *
+                        #process.patDefaultSequence *
                         process.makeGenEvt *
                         process.makeTtSemiLepEvent *
                         process.analyzeHypotheses)
