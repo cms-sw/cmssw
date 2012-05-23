0ae9fa5b1b3096dfacc6880ee12e81fe45ac1f22
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-05-23 15:28:45 +0000

    cleanup from unnecessary imports

--- PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
@@ -1,7 +1,8 @@
 #add negative secondary vertex tagger
 
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
-from RecoBTau.JetTagComputer.jetTagRecord_cfi import *
-from RecoBTag.SecondaryVertex.secondaryVertexNegativeTagInfos_cfi import *
 from RecoBTag.SecondaryVertex.simpleSecondaryVertex2TrkES_cfi import *
-from RecoBTag.SecondaryVertex.simpleSecondaryVertexNegativeBJetTags_cfi import *
+#from RecoBTag.SecondaryVertex.secondaryVertexNegativeTagInfos_cfi import *
+#from RecoBTag.SecondaryVertex.simpleSecondaryVertexNegativeBJetTags_cfi import *
+
+
