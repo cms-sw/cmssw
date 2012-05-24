fb2b9f48c1e422bdf10986196fb9d3ca06d24549
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-05-24 21:46:03 +0000

    overwite L1 menu

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,10 +1,10 @@
 autoCond = { 
-    'mc'        : 'MC_52_V10::All',
-    'startup'   : 'START52_V10::All',
-    'com10'     : 'GR_R_52_V7::All', # This should always be the GR_R GT
+    'mc'        : 'MC_53_V6::All',
+    'startup'   : 'START53_V6::All',
+    'com10'     : 'GR_R_53_V2::All', # This should always be the GR_R GT
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
-    'hltonline' : 'GR_H_V29::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
-    'hltonline11' : ('GR_H_V29::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
-                     'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
-    'starthi'   : 'STARTHI52_V8::All'
+    'hltonline' : 'GR_R_53_V2::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
+    'hltonline11' : ('GR_R_53_V2::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
+                     'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
+    'starthi'   : 'STARTHI53_V4::All'
 }
