252af4daecc84887cc04533b5a922922a5dea5c3
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-05-25 09:20:11 +0000

    Added comments

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,3 +1,9 @@
+# Remarks:
+# - 'com10' must always be the GR_R GT
+# - 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
+# - 'hltonline11' is the same as above, but it runs over 2011 data. To run the HLT the workflow needs to also rereco the L1 hence the L1 menu must be forced explicitly. The one for MC is used and must
+# be kept updated.
+
 autoCond = { 
     'mc'        : 'MC_52_V10::All',
     'startup'   : 'START52_V10::All',
