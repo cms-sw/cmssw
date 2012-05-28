856e9614e7da1f57142b3f49d549c273d863eb25
Hongxuan Liu <sha1-b70f13a6c621c4c85f2bad0c1693be1e66e48d4f@cern.ch>
2012-05-28 22:03:55 +0000

    Add in eeBadSc filter

---
+++ RecoMET/METFilters/python/eeBadScFilter_cfi.py
@@ -0,0 +1,23 @@
+
+import FWCore.ParameterSet.Config as cms
+
+eeBadScFilter = cms.EDFilter(
+  "EEBadScFilter",
+  # the EE rechit collection found in AOD
+  EERecHitSource = cms.InputTag('reducedEcalRecHitsEE'),
+  # minimum rechit energy used for rechit flag check 
+  EminHit        = cms.double(1000.),
+  # minimum transverse energy of the 5x5 array for each bad SC 
+  EtminSC        = cms.double(3000.),
+  # size of the crystal array (default = 5 -> 5x5 array)
+  SCsize         = cms.int32(5),
+  # minimum number of hits above EminHit with !kGood flags
+  nBadHitsSC     = cms.int32(2),
+  #coordinates of the crystals in the centre of each bad supercrystal
+  # packed into a single integer in the form  iz,ix,iy
+  #   for instance -1023023 ->  ix=23, iy=23, iz=-1
+  badscEE        = cms.vint32(-1023023,1048098),
+  taggingMode = cms.bool(False),
+  #prints debug info for each supercrystal if set to true
+  debug = cms.bool(False),
+)
