ca5086be8d8222e0ed9d67f3993a453f47674729
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2012-05-30 08:43:28 +0000

    sync back latest dev plus cleanup for using namespace

--- FastSimulation/ParticleDecay/interface/PythiaDecays.h
+++ FastSimulation/ParticleDecay/interface/PythiaDecays.h
@@ -8,11 +8,11 @@
 #include <HepMC/GenParticle.h> 
 #include <Pythia.h>
 #include <HepMCInterface.h>
-#include "FWCore/Utilities/interface/RandomNumberGenerator.h"
-#include "GeneratorInterface/Core/interface/RNDMEngineAccess.h"
+// #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
+// #include "GeneratorInterface/Core/interface/RNDMEngineAccess.h"
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenRunInfoProduct.h"
-#include "GeneratorInterface/ExternalDecays/interface/ExternalDecayDriver.h"
+// #include "GeneratorInterface/ExternalDecays/interface/ExternalDecayDriver.h"
 
 class ParticlePropagator;
 class Pythia6jets;
@@ -42,7 +42,7 @@ class PythiaDecays
   gen::Pythia6Service *pyservice;
   Pythia6jets* pyjets;
   // for Pythia8:
-  std::auto_ptr<Pythia8::Pythia>   pythia;
+  // ---> no need std::auto_ptr<Pythia8::Pythia>   pythia;
   std::auto_ptr<Pythia8::Pythia>   decayer; 
 
 };
