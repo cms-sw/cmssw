3f0d31a90d37a55fd3f6cafa2b20c07d16ae15bb
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2012-06-05 08:44:42 +0000

    Add new parameters for a DB version.

--- Geometry/TrackerNumberingBuilder/python/trackerNumberingGeometryDB_cfi.py
+++ Geometry/TrackerNumberingBuilder/python/trackerNumberingGeometryDB_cfi.py
@@ -4,7 +4,9 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the Tracker Numbering.
 #
 TrackerGeometricDetESModule = cms.ESProducer("TrackerGeometricDetESModule",
-    fromDDD = cms.bool(False)
+    fromDDD = cms.bool(False),
+    layerNumberPXB = cms.uint32(16),
+    totalBlade = cms.uint32(24)
 )
 
 
