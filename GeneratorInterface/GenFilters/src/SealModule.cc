baa950d972a165e3d1df92b07e4c02aa52307e0f
Nicolas Pierre Chanon <sha1-dc1081d72f70055f90623ceb19c685a609c9cac9@cern.ch>
2012-06-05 08:57:26 +0000

    Adding Zto2lFilter in SealModule

--- GeneratorInterface/GenFilters/src/SealModule.cc
+++ GeneratorInterface/GenFilters/src/SealModule.cc
@@ -38,6 +38,7 @@
 #include "GeneratorInterface/GenFilters/interface/HerwigMaxPtPartonFilter.h"
 #include "GeneratorInterface/GenFilters/interface/TwoVBGenFilter.h"
 #include "GeneratorInterface/GenFilters/interface/TotalKinematicsFilter.h"
+#include "GeneratorInterface/GenFilters/interface/Zto2lFilter.h"
 
 
   using cms::BHFilter;
@@ -79,6 +80,8 @@
   DEFINE_FWK_MODULE(HerwigMaxPtPartonFilter);
   DEFINE_FWK_MODULE(TwoVBGenFilter);
   DEFINE_FWK_MODULE(TotalKinematicsFilter);
+  DEFINE_FWK_MODULE(Zto2lFilter);
+
 
 
 
