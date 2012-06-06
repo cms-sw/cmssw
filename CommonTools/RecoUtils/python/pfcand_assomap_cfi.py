f8f60d2c05edeec1a3ebc8797bcd736f405f59a6
Matthias Geisler <sha1-ca4f0a71d5bbadf63f826def6c188008b2bb8317@cern.ch>
2012-06-06 09:05:13 +0000

    Update to use the beam spot and changed to standalone running

--- CommonTools/RecoUtils/python/pfcand_assomap_cfi.py
+++ CommonTools/RecoUtils/python/pfcand_assomap_cfi.py
@@ -5,9 +5,6 @@ PFCandAssoMap = cms.EDProducer('PFCand_AssoMap',
 	  #Set the Input Collections
           PFCandidateCollection = cms.InputTag('particleFlow'),
           VertexCollection = cms.InputTag('offlinePrimaryVertices'),
-		  
-	  #Set the Input Association Map
-          VertexTrackAssociationMap = cms.InputTag('Tracks2Vertex'),
 		  	    
 	  #Configuration for the reassociation of gamma conversion particles
           ConversionsCollection = cms.InputTag('allConversions'),
@@ -17,9 +14,17 @@ PFCandAssoMap = cms.EDProducer('PFCand_AssoMap',
           V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
 	   
 	  #Configuration for the reassociation of particles from nuclear interactions
-          NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),			    
+          NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+	   	   
+	  #Configuration to check if a secondary is compatible with the BeamSpot
+	  #True for best performance in jet-pt response
+	  UseBeamSpotCompatibility = cms.untracked.bool(True),
+	  BeamSpot = cms.InputTag('offlineBeamSpot'),
+		  
+	  #Configuration for the final association
+          VertexAssClosest = cms.untracked.bool(True),			    
 	   	   
 	  #What to do if the dipl vertex coll can't be found
-          ignoreMissingCollection = cms.bool(False),
+          ignoreMissingCollection = cms.bool(True),	
+		  
 )
-
