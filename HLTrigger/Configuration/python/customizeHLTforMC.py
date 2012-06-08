8e6f248a98b95ff7b70ebbeddd9f46625b7f698c
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-06-08 14:36:58 +0000

    turn into a customise fragment

--- HLTrigger/Configuration/python/customizeHLTforMC.py
+++ HLTrigger/Configuration/python/customizeHLTforMC.py
@@ -1,19 +1,27 @@
-# CSCHaloDataProducer
-# not used at HLT - process.CSCHaloData.ExpectedBX = cms.int32(6)
+def customize(process):
+"""adapt the HLT to run on MC, instead of data
+see Configuration/StandardSequences/Reconstruction_Data_cff.py
+which does the opposite, for RECO"""
 
-# EcalUncalibRecHitProducer
-# not used at HLT - process.ecalGlobalUncalibRecHit.doEBtimeCorrection = cms.bool(False)
-# not used at HLT - process.ecalGlobalUncalibRecHit.doEEtimeCorrection = cms.bool(False)
+  # CSCHaloDataProducer - not used at HLT
+  #if 'CSCHaloData' in process.__dict__():
+  #  process.CSCHaloData.ExpectedBX = cms.int32(6)
 
-# HcalRecAlgoESProducer
-if hcalRecAlgos in process.dict():
-  import RecoLocalCalo.HcalRecAlgos.RemoveAddSevLevel as HcalRemoveAddSevLevel
-  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HFDigiTime",     8)
-  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HBHEFlatNoise",  8)
-  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HBHESpikeNoise", 8)
+  # EcalUncalibRecHitProducer - not used at HLT
+  #if 'ecalGlobalUncalibRecHit' in process.__dict__():
+  #  process.ecalGlobalUncalibRecHit.doEBtimeCorrection = cms.bool(False)
+  #  process.ecalGlobalUncalibRecHit.doEEtimeCorrection = cms.bool(False)
 
-# PFRecHitProducerHCAL
-if hltParticleFlowRecHitHCAL in process.dict():
-  process.hltParticleFlowRecHitHCAL.ApplyPulseDPG      = cms.bool(False)
-  process.hltParticleFlowRecHitHCAL.LongShortFibre_Cut = cms.double(1000000000.0)
+  # HcalRecAlgoESProducer - these flags are not used at HLT (they should stay set to the default value for both data and MC)
+  #if 'hcalRecAlgos' in process.__dict__():
+  #  import RecoLocalCalo.HcalRecAlgos.RemoveAddSevLevel as HcalRemoveAddSevLevel
+  #  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HFDigiTime",     8)
+  #  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HBHEFlatNoise",  8)
+  #  HcalRemoveAddSevLevel.AddFlag(process.hcalRecAlgos, "HBHESpikeNoise", 8)
 
+  # PFRecHitProducerHCAL
+  if 'hltParticleFlowRecHitHCAL' in process.__dict__():
+    process.hltParticleFlowRecHitHCAL.ApplyPulseDPG      = cms.bool(False)
+    process.hltParticleFlowRecHitHCAL.LongShortFibre_Cut = cms.double(1000000000.0)
+
+  return process
