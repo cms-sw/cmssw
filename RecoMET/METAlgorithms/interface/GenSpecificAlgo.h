ac26652646bda9cfc061ce170dabaa4b32610283
Tai Sakuma <sha1-83aadde34f3854f55faaed54e7ebb52fa93fe00c@cern.ch>
2012-06-10 21:54:11 +0000

    cleaned up

--- RecoMET/METAlgorithms/interface/GenSpecificAlgo.h
+++ RecoMET/METAlgorithms/interface/GenSpecificAlgo.h
@@ -1,24 +1,46 @@
+// -*- C++ -*-
+//
+// Package:    METAlgorithms
+// Class:      GenSpecificAlgo
+// 
+/**\class GenSpecificAlgo GenSpecificAlgo.h RecoMET/METAlgorithms/interface/GenSpecificAlgo.h
+
+ Description: Adds generator level HEPMC specific information to MET
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Authors:  R. Cavanaugh (taken from F.Ratnikov, UMd)
+//          Created:  June 6, 2006
+// $Id: PFSpecificAlgo.h,v 1.6 2012/06/10 16:37:16 sakuma Exp $
+//
+//
 #ifndef METProducers_GenMETInfo_h
 #define METProducers_GenMETInfo_h
 
-/// Adds generator level HEPMC specific information to MET base class
-/// Author: R. Cavanaugh (taken from F.Ratnikov, UMd)
-/// 6 June, 2006
-
+//____________________________________________________________________________||
 #include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/CommonMETData.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "DataFormats/Math/interface/Point3D.h"
-#include "DataFormats/METReco/interface/CommonMETData.h"
+#include "DataFormats/METReco/interface/SpecificGenMETData.h"
 
-class GenSpecificAlgo 
+//____________________________________________________________________________||
+class GenSpecificAlgo
 {
- public:
+
+public:
+  reco::GenMET addInfo(edm::Handle<edm::View<reco::Candidate> > particles, CommonMETData *met, double globalThreshold, bool onlyFiducial = false, bool usePt = false);
+
+private:
   typedef math::XYZTLorentzVector LorentzVector;
   typedef math::XYZPoint Point;
-  typedef std::vector <const reco::Candidate*> ParticleCollection;
-  /// Make GenMET. Assumes MET is made from MCCandidates
-  //reco::GenMET addInfo(const reco::CandidateCollection *particles, CommonMETData met);
-  reco::GenMET addInfo(edm::Handle<edm::View<reco::Candidate> > particles, CommonMETData *met, double globalThreshold, bool onlyFiducial=false, bool usePt=false);
+
+  void fillCommonMETData(CommonMETData *met, edm::Handle<edm::View<reco::Candidate> >& particles, double globalThreshold, bool onlyFiducial, bool usePt);
+  SpecificGenMETData mkSpecificGenMETData(edm::Handle<edm::View<reco::Candidate> >& particles, bool usePt);
+
 };
 
-#endif
+//____________________________________________________________________________||
+#endif // METProducers_GenMETInfo_h
