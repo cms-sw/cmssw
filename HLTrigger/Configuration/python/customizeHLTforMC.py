4f24ac5b51202bb40802cb6e78f00b5f561c28d5
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-06-14 08:48:55 +0000

    rename customisation function to customizeHLTforMC

--- HLTrigger/Configuration/python/customizeHLTforMC.py
+++ HLTrigger/Configuration/python/customizeHLTforMC.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-def customize(process):
+def customizeHLTforMC(process):
   """adapt the HLT to run on MC, instead of data
   see Configuration/StandardSequences/Reconstruction_Data_cff.py
   which does the opposite, for RECO"""
