54941841990fdc9a9c0a93d1c6cae0b994453722
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2012-06-14 14:37:40 +0000

    Implementing policy that new parameters affecting HLT should be optional. Extract parameters in a separate configuration file.

---
+++ Geometry/TrackerNumberingBuilder/python/pixelGeometryConstants_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+pixelGeometryConstants = cms.PSet(
+    layerNumberPXB = cms.uint32(16),
+    totalBlade = cms.uint32(24)
+)
---
+++ Geometry/TrackerNumberingBuilder/python/pixelSLHCGeometryConstants_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+pixelGeometryConstants = cms.PSet(
+    layerNumberPXB = cms.uint32(18),
+    totalBlade = cms.uint32(56)
+)
