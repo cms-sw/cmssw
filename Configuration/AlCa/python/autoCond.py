b56516e7d86707b20a1ae7bd66c17554b85941d8
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-06-15 12:48:32 +0000

    New B-tag probabilities in GR_R and updated MC GTs with new L1 menu and jet energy corrections

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,10 +1,9 @@
 autoCond = { 
-    'mc'        : 'MC_60_V1::All',
-    'startup'   : 'START60_V1::All',
-    'com10'     : 'GR_R_60_V1::All', # This should always be the GR_R GT
+    'mc'        : 'MC_53_V7::All',
+    'startup'   : 'START53_V7::All',
+    'com10'     : 'GR_R_53_V2B::All',
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
-    'hltonline' : 'GR_R_60_V1::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
-    'hltonline11' : ('GR_R_60_V1::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
-                     'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
-    'starthi'   : 'STARTHI60_V1::All'
+    'hltonline' : 'GR_R_53_V2B::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
+    'hltonline11' : ('GR_R_53_V2B::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/','L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
+    'starthi'   : 'STARTHI53_V5::All'
 }
