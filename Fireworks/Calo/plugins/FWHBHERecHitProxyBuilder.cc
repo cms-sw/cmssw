a3e72383e6634bd3f813c125d5f88dd3d80f7888
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2012-06-16 04:54:14 +0000

    Use base class FWCaloRecHitDigitSetProxyBuilder with implemented scaling.

--- Fireworks/Calo/plugins/FWHBHERecHitProxyBuilder.cc
+++ Fireworks/Calo/plugins/FWHBHERecHitProxyBuilder.cc
@@ -1,151 +1,60 @@
-#include "TEveCompound.h"
-#include "TEveBoxSet.h"
-
-#include "Fireworks/Core/interface/FWDigitSetProxyBuilder.h"
-#include "Fireworks/Core/interface/FWEventItem.h"
-#include "Fireworks/Core/interface/FWGeometry.h"
-#include "Fireworks/Core/interface/BuilderUtils.h"
-#include "Fireworks/Core/interface/FWViewEnergyScale.h"
-#include "DataFormats/HcalRecHit/interface/HBHERecHit.h"
+#include "Fireworks/Calo/plugins/FWCaloRecHitDigitSetProxyBuilder.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 
-class FWHBHERecHitProxyBuilder : public FWDigitSetProxyBuilder
+class FWHBHERecHitProxyBuilder : public FWCaloRecHitDigitSetProxyBuilder
 {
 public:
-   FWHBHERecHitProxyBuilder( void )
-    {}
-  
-   virtual ~FWHBHERecHitProxyBuilder( void ) 
-    {}
-
-   virtual bool havePerViewProduct(FWViewType::EType) const { return true; }
-   virtual void scaleProduct(TEveElementList* parent, FWViewType::EType, const FWViewContext* vc);
+   FWHBHERecHitProxyBuilder( void ) { invertBox(true); }
+   virtual ~FWHBHERecHitProxyBuilder( void ) {}
 
    REGISTER_PROXYBUILDER_METHODS();
 
 private:
-   virtual void build( const FWEventItem* iItem, TEveElementList* product, const FWViewContext* );
-
    FWHBHERecHitProxyBuilder( const FWHBHERecHitProxyBuilder& );
    const FWHBHERecHitProxyBuilder& operator=( const FWHBHERecHitProxyBuilder& );
 };
 
-namespace
-{
+REGISTER_FWPROXYBUILDER( FWHBHERecHitProxyBuilder, HBHERecHitCollection, "HBHE RecHit", FWViewType::kISpyBit );
 
-float  scaleFactor(const FWViewContext* vc)
-{
-   // printf("scale face %f \n", vc->getEnergyScale()->getScaleFactor3D());
-   return vc->getEnergyScale()->getScaleFactor3D()/20;
-}
 
-void viewContextBoxScale( const float* corners, float scale, bool plotEt, std::vector<float>& scaledCorners, bool invert)
-{
-   TEveVector  center;
-   for( unsigned int i = 0; i < 24; i += 3 )
-   {	 
-      center[0] += corners[i];
-      center[1] += corners[i + 1];
-      center[2] += corners[i + 2];
-   }
-   center *= 1.f/8.f;
+// AMT: Refelct box. Previously used energyScaledBox3DCorners()
 
-   if (plotEt)
-   {
-      scale *= center.Perp()/center.Mag();
-   }
-
-   // Coordinates for a scaled version of the original box
-   for( unsigned int i = 0; i < 24; i += 3 )
-   {	
-      scaledCorners[i] = center[0] + ( corners[i] - center[0] ) * scale;
-      scaledCorners[i + 1] = center[1] + ( corners[i + 1] - center[1] ) * scale;
-      scaledCorners[i + 2] = center[2] + ( corners[i + 2] - center[2] ) * scale;
-   }
-      
-   if( invert )
-      fireworks::invertBox( scaledCorners );
-}
-}
-
-void
-FWHBHERecHitProxyBuilder::scaleProduct(TEveElementList* parent, FWViewType::EType type, const FWViewContext* vc)
-{
-
-   const HBHERecHitCollection* collection = 0;
-   item()->get( collection );
-   if (! collection)
-      return;
-
-   int index = 0;
-   std::vector<float> scaledCorners(24);
-
-   float scale = scaleFactor(vc);
-
-   for (std::vector<HBHERecHit>::const_iterator it = collection->begin() ; it != collection->end(); ++it, ++index)
-   {
-      const float* corners = item()->getGeom()->getCorners((*it).detid());
-      if (corners == 0) 
-         continue;
-      FWDigitSetProxyBuilder::BFreeBox_t* b = (FWDigitSetProxyBuilder::BFreeBox_t*)getBoxSet()->GetPlex()->Atom(index);
-
-      /*          
-      printf("--------------------scale product \n");
-      for (int i = 0; i < 8 ; ++i)
-         printf("[%f %f %f ]\n",b->fVertices[i][0], b->fVertices[i][1],b->fVertices[i][2] );
-      
-      */
-
-      viewContextBoxScale(corners, (*it).energy()*scale, vc->getEnergyScale()->getPlotEt(), scaledCorners, true);
-      /*
-        printf("after \n");
-        for (int i = 0; i < 8 ; ++i)
-        printf("[%f %f %f ]\n",b->fVertices[i][0], b->fVertices[i][1],b->fVertices[i][2] );        
-      */
-      memcpy(b->fVertices, &scaledCorners[0], sizeof(b->fVertices));
-   }
-   getBoxSet()->ElementChanged();
-}
-
-//______________________________________________________________________________
-
+/*
 void
 FWHBHERecHitProxyBuilder::build( const FWEventItem* iItem, TEveElementList* product, const FWViewContext* vc)
 {
+   m_plotEt = vc->getEnergyScale()->getPlotEt();
+
    const HBHERecHitCollection* collection = 0;
    iItem->get( collection );
 
    if( 0 == collection )
+   {
       return;
-
+   }
    std::vector<HBHERecHit>::const_iterator it = collection->begin();
    std::vector<HBHERecHit>::const_iterator itEnd = collection->end();
    std::vector<float> scaledCorners(24);
 
+   for( ; it != itEnd; ++it )
+   {
+      if(( *it ).energy() > m_maxEnergy )
+         m_maxEnergy = ( *it ).energy();
+   }
+
    TEveBoxSet* boxSet = addBoxSetToProduct(product);
    int index = 0;
-
-   float scale = scaleFactor(vc);
-
-   TEveVector centre;
    for (std::vector<HBHERecHit>::const_iterator it = collection->begin() ; it != collection->end(); ++it)
    {  
       const float* corners = context().getGeom()->getCorners((*it).detid());
       if (corners)
       {
-         for( unsigned int i = 0; i < 24; i += 3 )
-         {	 
-            centre[0] += corners[i];
-            centre[1] += corners[i + 1];
-            centre[2] += corners[i + 2];
-         }
-         centre.Normalize();
-         context().voteMaxEtAndEnergy( centre.Perp() *it->energy() ,it->energy());
-         viewContextBoxScale(corners, (*it).energy()*scale, vc->getEnergyScale()->getPlotEt(), scaledCorners, true);
+         if (m_plotEt)
+            fireworks::etScaledBox3DCorners(corners, (*it).energy(), m_maxEnergy, scaledCorners, true);
+         else
+            fireworks::energyScaledBox3DCorners(corners, (*it).energy() / m_maxEnergy, scaledCorners, true);
       }
-
       addBox(boxSet, &scaledCorners[0], iItem->modelInfo(index++).displayProperties());
    }
 }
-
-REGISTER_FWPROXYBUILDER( FWHBHERecHitProxyBuilder, HBHERecHitCollection, "HBHE RecHit", FWViewType::kISpyBit );
+*/
