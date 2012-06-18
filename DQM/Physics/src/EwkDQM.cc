99140188dfddf84674d1d6e9832c769eacd19c7c
Valentina Gori <sha1-a8f920a5fd6daebfdd62d1c5060b9dfff859ca57@cern.ch>
2012-06-18 16:52:41 +0000

    CaloJets substituted by PFJets; jet cleaning implemented; leading jet multiplicity plots correctly filled; leading jet pseudo-rapidity distribution filled

--- DQM/Physics/src/EwkDQM.cc
+++ DQM/Physics/src/EwkDQM.cc
@@ -1,8 +1,8 @@
 /*
  *  See header file for a description of this class.
  *
- *  $Date: 2011/12/21 14:24:21 $
- *  $Revision: 1.17 $
+ *  $Date: 2012/01/11 13:53:28 $
+ *  $Revision: 1.18 $
  *  \author Michael B. Anderson, University of Wisconsin-Madison
  *  \author Will Parker, University of Wisconsin-Madison
  */
@@ -26,7 +26,8 @@
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
 #include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/JetReco/interface/CaloJet.h"
+//#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/PFJet.h"  // voglio usare i Particle Flow Jets, non i Calo Jets
 #include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
 #include "DataFormats/METReco/interface/CaloMETFwd.h"
@@ -49,20 +50,27 @@ using namespace reco;
 
 
 
+// EwkDQM::EwkDQM(const ParameterSet& parameters) {
 EwkDQM::EwkDQM(const ParameterSet& parameters) {
+  //  eJetMin_     = parameters.getUntrackedParameter<double>("EJetMin", 999999.);
+  eJetMin_     = parameters.getUntrackedParameter<double>("EJetMin", 15.);
+  // faccio questa prova: inizializzo eJetMin_ a 15, come dopo
+
+  // riguardare questa sintassi 
   // Get parameters from configuration file
   theElecTriggerPathToPass    = parameters.getParameter<string>("elecTriggerPathToPass");
   theMuonTriggerPathToPass    = parameters.getParameter<string>("muonTriggerPathToPass");
   theTriggerResultsCollection = parameters.getParameter<InputTag>("triggerResultsCollection");
   theMuonCollectionLabel      = parameters.getParameter<InputTag>("muonCollection");
   theElectronCollectionLabel  = parameters.getParameter<InputTag>("electronCollection");
-  theCaloJetCollectionLabel   = parameters.getParameter<InputTag>("caloJetCollection");
+  //  theCaloJetCollectionLabel   = parameters.getParameter<InputTag>("caloJetCollection");
+  thePFJetCollectionLabel   = parameters.getParameter<InputTag>("PFJetCollection");
   theCaloMETCollectionLabel   = parameters.getParameter<InputTag>("caloMETCollection");
   
   // just to initialize
   isValidHltConfig_ = false;
 
-  // coverity says..
+  // coverity says.. (cos'e` questo?? che variabili sono???)
   h_e1_et = 0;
   h_e1_eta = 0;
   h_e1_phi = 0;
@@ -74,6 +82,10 @@ EwkDQM::EwkDQM(const ParameterSet& parameters) {
   h_jet2_et = 0;
   h_jet_count = 0;
   h_jet_et = 0;
+
+  h_jet_pt = 0;   // prova
+
+  h_jet_eta = 0;  // aggiunto il 23 maggio 
   h_m1_eta = 0;
   h_m1_phi = 0;
   h_m1_pt = 0;
@@ -92,6 +104,7 @@ EwkDQM::EwkDQM(const ParameterSet& parameters) {
   h_vertex_numTrks = 0;
   h_vertex_number = 0;
   h_vertex_sumTrks = 0;
+
   
   theDbe = Service<DQMStore>().operator->();
 
@@ -118,9 +131,20 @@ void EwkDQM::beginJob() {
   h_vertex_d0    = theDbe->book1D("h_vertex_d0"   , "Event Vertex d0"                        , 20,  0.0,   0.05);
   h_mumu_invMass = theDbe->book1D("h_mumu_invMass", "#mu#mu Invariant Mass;InvMass (GeV)"    , 20, 40.0, 140.0 );
   h_ee_invMass   = theDbe->book1D("h_ee_invMass",   "ee Invariant Mass;InvMass (Gev)"        , 20, 40.0, 140.0 );
-  h_jet_et       = theDbe->book1D("h_jet_et",       "Jet with highest E_{T} (from "+theCaloJetCollectionLabel.label()+");E_{T}(1^{st} jet) (GeV)",    20, 0., 200.0);
-  h_jet2_et      = theDbe->book1D("h_jet2_et",      "Jet with 2^{nd} highest E_{T} (from "+theCaloJetCollectionLabel.label()+");E_{T}(2^{nd} jet) (GeV)",    20, 0., 200.0);
-  h_jet_count    = theDbe->book1D("h_jet_count",    "Number of "+theCaloJetCollectionLabel.label()+" (E_{T} > 15 GeV);Number of Jets", 8, -0.5, 7.5);
+  
+  //  h_jet_et       = theDbe->book1D("h_jet_et",       "Jet with highest E_{T} (from "+theCaloJetCollectionLabel.label()+");E_{T}(1^{st} jet) (GeV)",    20, 0., 200.0);
+  h_jet_et       = theDbe->book1D("h_jet_et",       "Leading jet E_{T} (from "+thePFJetCollectionLabel.label()+");E_{T}(1^{st} jet) (GeV)",    20, 0., 200.0);
+
+  h_jet_pt       = theDbe->book1D("h_jet_pt",      "Leading jet p_{T} (from "+thePFJetCollectionLabel.label()+");p_{T}(1^{st} jet) (GeV/c)",    20, 0., 200.0); 
+
+  h_jet_eta      = theDbe->book1D("h_jet_eta",      "Leading jet #eta (from "+thePFJetCollectionLabel.label()+"); #eta (1^{st} jet)",    20, -10., 10.0);
+
+  //  h_jet2_et      = theDbe->book1D("h_jet2_et",      "Jet with 2^{nd} highest E_{T} (from "+theCaloJetCollectionLabel.label()+");E_{T}(2^{nd} jet) (GeV)",    20, 0., 200.0);
+  h_jet2_et      = theDbe->book1D("h_jet2_et",      "2^{nd} leading jet E_{T} (from "+thePFJetCollectionLabel.label()+");E_{T}(2^{nd} jet) (GeV)",    20, 0., 200.0);
+
+  //  h_jet_count    = theDbe->book1D("h_jet_count",    "Number of "+theCaloJetCollectionLabel.label()+" (E_{T} > 15 GeV);Number of Jets", 8, -0.5, 7.5);
+  h_jet_count    = theDbe->book1D("h_jet_count",    "Number of "+thePFJetCollectionLabel.label()+" (E_{T} > 15 GeV);Number of Jets", 8, -0.5, 7.5);
+
   h_e1_et        = theDbe->book1D("h_e1_et",  "E_{T} of Leading Electron;E_{T} (GeV)"        , 20,  0.0 , 100.0);
   h_e2_et        = theDbe->book1D("h_e2_et",  "E_{T} of Second Electron;E_{T} (GeV)"         , 20,  0.0 , 100.0);
   h_e1_eta       = theDbe->book1D("h_e1_eta", "#eta of Leading Electron;#eta"                , 20, -4.0 , 4.0);
@@ -164,7 +188,7 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
   // short-circuit if hlt problems
   if( ! isValidHltConfig_ ) return;
 
-
+  // non mi e` chiaro come faccia a "ciclare" sugli eventi
   LogTrace(logTraceName)<<"Analysis of event # ";
   // Did it pass certain HLT path?
   Handle<TriggerResults> HLTresults;
@@ -303,47 +327,112 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
     mm_invMass = pair.M();
   }
   ////////////////////////////////////////////////////////////////////////////////  
-
-
+  
+  
   ////////////////////////////////////////////////////////////////////////////////
-  // Find the highest et jet
-  Handle<CaloJetCollection> caloJetCollection;
-  iEvent.getByLabel (theCaloJetCollectionLabel,caloJetCollection);
-  if ( !caloJetCollection.isValid() ) return;
+  // Find the highest et jet			
+    
+  //  Handle<CaloJetCollection> caloJetCollection;
+  Handle<PFJetCollection> PFJetCollection;
+  //  iEvent.getByLabel (theCaloJetCollectionLabel,caloJetCollection);
+  iEvent.getByLabel (thePFJetCollectionLabel,PFJetCollection);
+  //  if ( !caloJetCollection.isValid() ) return;
+  if ( !PFJetCollection.isValid() ) return;
+  
+  unsigned int muonCollectionSize = muonCollection->size();
+  //unsigned int jetCollectionSize = jetCollection->size();
+  unsigned int PFJetCollectionSize = PFJetCollection->size();
+  int jet_count = 0; int LEADJET=-1;  double max_pt=0;
+  //  eJetMin_     (cfg.getUntrackedParameter<double>("EJetMin", 999999.))
+  
 
+  
+  
+  
   float jet_et    = -8.0;
-  //  float jet_eta   = -8.0; // UNUSED
-  //  float jet_phi   = -8.0; // UNUSED
-  int   jet_count = 0;
+  float jet_pt    = -8.0;  // prova
+  float jet_eta   = -80.0; // now USED
+  float jet_phi   = -8.0; // not USED
   float jet2_et   = -9.0;
-  //  float jet2_eta  = -9.0; // UNUSED
-  //  float jet2_phi  = -9.0; // UNUSED
-  for (CaloJetCollection::const_iterator i_calojet = caloJetCollection->begin(); i_calojet != caloJetCollection->end(); i_calojet++) {
+  float jet2_eta  = -9.0; // now USED
+  float jet2_phi  = -9.0; // not USED
+  //  for (CaloJetCollection::const_iterator i_calojet = caloJetCollection->begin(); i_calojet != caloJetCollection->end(); i_calojet++) {
+  //  for (PFJetCollection::const_iterator i_pfjet = PFJetCollection->begin(); i_pfjet != PFJetCollection->end(); i_pfjet++) {
 
-    float jet_current_et = i_calojet->et();
 
-    // if it overlaps with electron, it is not a jet
-    if ( electron_et>0.0 && fabs(i_calojet->eta()-electron_eta ) < 0.2 && calcDeltaPhi(i_calojet->phi(), electron_phi ) < 0.2) continue;
-    if ( electron2_et>0.0&& fabs(i_calojet->eta()-electron2_eta) < 0.2 && calcDeltaPhi(i_calojet->phi(), electron2_phi) < 0.2) continue;
 
-    // if it has too low Et, throw away
-    if (jet_current_et < 15) continue;
 
-    jet_count++;
-    if (jet_current_et > jet_et) {
-      jet2_et  = jet_et;  // 2nd highest jet get's et from current highest
-      //      jet2_eta = jet_eta; // UNUSED
-      //      jet2_phi = jet_phi; // UNUSED
-      jet_et   = i_calojet->et(); // current highest jet gets et from the new highest
-      //      jet_eta  = i_calojet->eta(); // UNUSED
-      //      jet_phi  = i_calojet->phi(); // UNUSED
-    } else if (jet_current_et > jet2_et) {
-      jet2_et  = i_calojet->et();
-      //      jet2_eta = i_calojet->eta(); // UNUSED
-      //      jet2_phi = i_calojet->phi(); // UNUSED
+   //  float jet_current_et = i_calojet->et();
+   //  float jet_current_et = i_pfjet->et();            // e` identico a jet.et()
+
+
+
+    //    jet_count++;
+
+
+
+
+    // cleaning: va messo prima del riempimento dell'istogramma // This is in order to use PFJets
+    for (unsigned int i=0; i<PFJetCollectionSize; i++) {
+      const Jet& jet = PFJetCollection->at(i);
+      // la classe "jet" viene definita qui!!!
+      double minDistance=99999;
+      for (unsigned int j=0; j<muonCollectionSize; j++) {
+	const Muon& mu = muonCollection->at(j);
+	double distance = sqrt( (mu.eta()-jet.eta())*(mu.eta()-jet.eta()) +(mu.phi()-jet.phi())*(mu.phi()-jet.phi()) );      
+	if (minDistance>distance) minDistance=distance;
+      }
+      if (minDistance<0.3) continue; // 0.3 is the isolation cone around the muon 
+      // se la distanza muone-cono del jet e` minore di 0.3, passo avanti e non conteggio il mio jet
+       
+      // If it overlaps with ELECTRON, it is not a jet
+      if ( electron_et>0.0 && fabs(jet.eta()-electron_eta ) < 0.2 && calcDeltaPhi(jet.phi(), electron_phi ) < 0.2) continue;
+      if ( electron2_et>0.0&& fabs(jet.eta()-electron2_eta) < 0.2 && calcDeltaPhi(jet.phi(), electron2_phi) < 0.2) continue;
+      
+      // provo a cambiare la parte degli elettroni in modo simmetrico alla parte per i muoni
+
+      // ...
+      // ...
+
+      
+      // if it has too low Et, throw away
+      if (jet.et() < 15) continue;
+      jet_count ++;
+
+      // ovvero: incrementa jet_count se:
+      //   - non c'e un muone entro 0.3 di distanza dal cono del jet;
+      //   - se il jet non si sovrappone ad un elettrone;
+      //   - se l'energia trasversa e` maggiore della soglia impostata (15?)
+      
+      if(jet.et()>max_pt) { LEADJET=i; max_pt=jet.et();} 
+      // se l'energia del jet e` maggiore di max_pt, diventa "i" l'indice del jet piu` energetico e max_pt la sua energia 
+        
+  
+    // riguardare questo!!!
+    // fino ad ora, jet_et era inizializzato a -8.0
+    if (jet.et() > jet_et) {
+      jet2_et  = jet_et;  // 2nd highest jet gets et from current highest // perche` prende l'energia del primo jet??
+      jet2_eta = jet_eta; // now USED   
+      jet2_phi = jet_phi; // now USED
+      //      jet_et   = i_calojet->et(); // current highest jet gets et from the new highest
+      jet_et   = jet.et(); // current highest jet gets et from the new highest
+      // ah, ok! lo riaggiorna solo dopo!
+      jet_pt = jet.pt();           // e` il pT del leading jet
+      jet_eta  = jet.eta(); // now USED
+      jet_phi  = jet.phi(); // now USED
+    } 
+        else if (jet.et() > jet2_et) {
+          //      jet2_et  = i_calojet->et();
+          jet2_et  = jet.et();
+          //      jet2_eta = i_calojet->eta(); // UNUSED
+          //      jet2_phi = i_calojet->phi(); // UNUSED
+          jet2_eta = jet.eta(); // now USED
+          jet2_phi = jet.phi(); // now USED
+        }
+    // questo elseif funziona
+    
     }
-  }
-  ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -368,7 +457,7 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
   if (mm_invMass > 0.0) {
     h_mumu_invMass->Fill(mm_invMass);
     fill_m1 = true;
-    fill_m2 = true;
+    fill_m2 = true;    h_jet2_et  ->Fill(jet2_et);
   }
 
   // Was W->e nu found?
@@ -389,10 +478,23 @@ void EwkDQM::analyze(const Event& iEvent, const EventSetup& iSetup) {
     fill_met = true;
   }
 
-  if (jet_et>0.0) {
+
+
+  if (jet_et>-10.0) {
     h_jet_et   ->Fill(jet_et);
     h_jet_count->Fill(jet_count);
+    h_jet2_et  ->Fill(jet2_et);
   }
+
+  if (jet_pt>0.) {
+    h_jet_pt   ->Fill(jet_pt);
+  }
+
+  if (jet_eta>-50.) {
+    h_jet_eta  ->Fill(jet_eta); 
+  }
+  
+
 
   if (fill_e1 || fill_m1) {
     h_vertex_number->Fill(vertex_number);
