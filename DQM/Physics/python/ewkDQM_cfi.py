906e7cc322ae1e57cb39159d3cb553858b5df40f
Valentina Gori <sha1-a8f920a5fd6daebfdd62d1c5060b9dfff859ca57@cern.ch>
2012-06-19 14:41:27 +0000

    CaloJets substituted by PFJets and different trigger paths

--- DQM/Physics/python/ewkDQM_cfi.py
+++ DQM/Physics/python/ewkDQM_cfi.py
@@ -2,16 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 # DQM monitor module for EWK
 ewkDQM = cms.EDAnalyzer("EwkDQM",
-            elecTriggerPathToPass    = cms.string("HLT_Ele"),
-            muonTriggerPathToPass    = cms.string("HLT_Mu"),
-            #eleTrigPathNames = cms.untracked.vstring("HLT_Ele","HLT_DoubleEle"),
-            #muTrigPathNames = cms.untracked.vstring("HLT_Mu","HLT_DoubleMu","HLT_IsoMu"),
-            triggerResultsCollection = cms.InputTag("TriggerResults","","HLT"),
+
+            elecTriggerPathToPass    = cms.vstring("HLT_Ele", "HLT_DoubleEle", "HLT_DoublePhoton"),
+            muonTriggerPathToPass    = cms.vstring("HLT_Mu", "HLT_IsoMu", "HLT_DoubleMu"),
+            triggerResultsCollection = cms.InputTag("TriggerResults", "", "HLT"),
+
             muonCollection           = cms.InputTag("muons"),
             electronCollection       = cms.InputTag("gsfElectrons"),
             PFJetCollection          = cms.InputTag("ak5PFJets"),
             caloMETCollection        = cms.InputTag("corMetGlobalMuons"),
             #genParticleCollection    = cms.InputTag("genParticles")
-#           caloJetCollection        = cms.InputTag("L2L3CorJetSC5Calo"),
             EJetMin = cms.untracked.double(15.0)
 )
