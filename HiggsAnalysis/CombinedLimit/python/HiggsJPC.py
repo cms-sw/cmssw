a6d8b0d0abc6f45902cdf893cb404a7ba43bab6d
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2012-06-20 23:45:25 +0000

    Assorted fixes

--- HiggsAnalysis/CombinedLimit/python/HiggsJPC.py
+++ HiggsAnalysis/CombinedLimit/python/HiggsJPC.py
@@ -8,6 +8,9 @@ class TwoHypotesisHiggs(PhysicsModel):
         self.muAsPOI    = False
         self.muFloating = False
         self.altSignal  = "ALT"
+    def setModelBuilder(self, modelBuilder):
+        PhysicsModel.setModelBuilder(self, modelBuilder)
+        self.modelBuilder.doModelBOnly = False
     def getYieldScale(self,bin,process):
         "Split in production and decay, and call getHiggsSignalYieldScale; return 1 for backgrounds "
         if not self.DC.isSignal[process]: return 1
@@ -34,13 +37,18 @@ class TwoHypotesisHiggs(PhysicsModel):
         self.modelBuilder.doVar("x[0,0,1]");
         poi = "x"
         if self.muFloating: 
-            self.modelBuilder.doVar("r[1,0,10]");
+            self.modelBuilder.doVar("r[1,0,4]");
             if self.muAsPOI: poi += ",r"
             self.modelBuilder.factory_("expr::r_times_not_x(\"@0*(1-@1)\", r, x)")
             self.modelBuilder.factory_("expr::r_times_x(\"@0*@1\", r, x)")
+            #self.modelBuilder.factory_("expr::r_times_not_x(\"@0*(1-0.9999*@1)\", r, x)")
+            #self.modelBuilder.factory_("expr::r_times_x(\"@0*(0.0001+0.9999*@1)\", r, x)")
             self.sigNorms = { True:'r_times_x', False:'r_times_not_x' }
         else:
-            self.modelBuilder.factory_("expr::not_x(\"(1-@1)\", x)")
+            #self.modelBuilder.factory_("expr::not_x(\"(1.000 - 0.999*@0)\", x)")
+            #self.modelBuilder.factory_("expr::yes_x(\"(0.001 + 0.999*@0)\", x)")
+            #self.sigNorms = { True:'yes_x', False:'not_x' }
+            self.modelBuilder.factory_("expr::not_x(\"(1-@0)\", x)")
             self.sigNorms = { True:'x', False:'not_x' }
         if self.modelBuilder.out.var("MH"):
             if len(self.mHRange):
