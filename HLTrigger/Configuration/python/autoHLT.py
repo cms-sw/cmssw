259e9cfa086e6664a86b1fef1ce51bf556a893d2
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-06-22 12:17:08 +0000

    *** empty log message ***

--- HLTrigger/Configuration/python/autoHLT.py
+++ HLTrigger/Configuration/python/autoHLT.py
@@ -1,6 +1,6 @@
 autoHLT = {
-  'relval'     : '7e33v2',   # change to 7e33v3 when ready
-  'relvalD'    : '7e33v2',   # for data if different
+  'relval'     : 'GRun',   # change to 7e33v3 when ready
+  'relvalD'    : 'GRun',   # for data if different
   'production' : '5e33v4',
   'test'       : 'GRun'
 }
