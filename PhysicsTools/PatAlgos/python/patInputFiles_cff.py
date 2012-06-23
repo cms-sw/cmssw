2bb6e471a73dec9f96825626a917cf3d101bae3d
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2012-06-23 19:57:52 +0000

    Update input RelVals.

--- PhysicsTools/PatAlgos/python/patInputFiles_cff.py
+++ PhysicsTools/PatAlgos/python/patInputFiles_cff.py
@@ -2,28 +2,28 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 
 filesRelValProdTTbarAODSIM = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_0_0_pre7'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START52_V4A'
+                        , globalTag     = 'START53_V6'
                         , dataTier      = 'AODSIM'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesRelValProdTTbarGENSIMRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_0_0_pre7'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START52_V4A'
+                        , globalTag     = 'START53_V6'
                         , dataTier      = 'GEN-SIM-RECO'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesSingleMuRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_5_2_0'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_0_0_pre7'
                         , relVal        = 'SingleMu'
                         , dataTier      = 'RECO'
-                        , globalTag     = 'GR_R_52_V4_RelVal_mu2011B'
+                        , globalTag     = 'GR_R_53_V2_RelVal_mu2011B'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
