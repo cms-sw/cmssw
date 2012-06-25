ce84b030448596abbcda2454645a0caf278a1dee
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-06-25 14:07:56 +0000

    fix for L1 menu replacement

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -4,6 +4,6 @@ autoCond = {
     'com10'     : 'GR_R_53_V4::All',
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
     'hltonline' : 'GR_R_53_V4::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
-    'hltonline11' : ('GR_R_53_V4::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/','L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
+    'hltonline11' : ('GR_R_53_V4::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/','L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
     'starthi'   : 'STARTHI53_V6::All'
 }
