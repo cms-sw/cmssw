15ad4e5257b2a56b70f389335a7a2e60488b66cb
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2012-06-25 15:12:09 +0000

    Merging with SLHC development - HcalReLabel is needed to produce HCAL geometry payloads.

--- Geometry/HcalEventSetup/python/HcalGeometryDBWriter_cfi.py
+++ Geometry/HcalEventSetup/python/HcalGeometryDBWriter_cfi.py
@@ -1,11 +1,14 @@
 import FWCore.ParameterSet.Config as cms
+from Geometry.HcalEventSetup.HcalRelabel_cfi import HcalReLabel
 
 HcalHardcodeGeometryEP = cms.ESProducer( "HcalHardcodeGeometryEP" ,
-                                         appendToDataLabel = cms.string("_master")
+                                         appendToDataLabel = cms.string("_master"),
+                                         HcalReLabel = HcalReLabel
                                          )
 
 HcalGeometryToDBEP = cms.ESProducer( "HcalGeometryToDBEP" ,
                                      applyAlignment = cms.bool(False) ,
-                                     appendToDataLabel = cms.string("_toDB")
+                                     appendToDataLabel = cms.string("_toDB"),
+                                     HcalReLabel = HcalReLabel
                                      )
 
