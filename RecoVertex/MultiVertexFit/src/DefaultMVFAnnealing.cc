bff125d6c85722cfb4dd1eef41af527af8abbe6e
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-07-01 19:38:24 +0000

    Remove ancient, unused code.

--- RecoVertex/MultiVertexFit/src/DefaultMVFAnnealing.cc
+++ RecoVertex/MultiVertexFit/src/DefaultMVFAnnealing.cc
@@ -1,57 +1,16 @@
 #include "RecoVertex/MultiVertexFit/interface/DefaultMVFAnnealing.h"
 
-namespace {
-  bool useOrcarcs()
-  {
-    static bool ret = false; /* SimpleConfigurable<bool>
+/* Old (ORCA) defaults:
+
       (false,"DefaultMVFAnnealing:UseOrcarc").value();
-      */
-    return ret;
-  }
-
-  double defaultCutoff ( double cutoff )
-  {
-    if ( useOrcarcs() )
-    {
-      return 3.0;
-      /*
-      return SimpleConfigurable<double>
         (3.0,"DefaultMVFAnnealing:Cutoff").value();
-        */
-    };
-    return cutoff;
-  }
-
-  double defaultT ( double T )
-  {
-    if ( useOrcarcs() )
-    {
-      return 1.0;
-      /*
-      return SimpleConfigurable<double>
         (1.0,"DefaultMVFAnnealing:Tini").value();
-        */
-    };
-    return T;
-  }
-
-  double defaultRatio ( double ratio )
-  {
-    if ( useOrcarcs() )
-    {
-      return 0.5;
-      /*
-      return SimpleConfigurable<double>
         (0.5,"DefaultMVFAnnealing:Ratio").value();
-        */
-    };
-    return ratio;
-  }
-}
+*/
 
 DefaultMVFAnnealing::DefaultMVFAnnealing (
      const double cutoff, const double T, const double ratio ) :
-  GeometricAnnealing ( defaultCutoff ( cutoff ), 
-                       defaultT ( T ) , 
-                       defaultRatio ( ratio ) )
+  GeometricAnnealing (cutoff, 
+                      T, 
+                      ratio)
 {}
