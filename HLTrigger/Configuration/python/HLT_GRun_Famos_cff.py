9294b4e8fca16c9c8a9b44f8023f50517ebb3c3b
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-07-03 14:23:33 +0000

    /online/collisions/2012/7e33/v3.2/HLT

--- HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_5_2_1/GRun/V171 (CMSSW_5_2_6)
+# /online/collisions/2012/7e33/v3.2/HLT/V2 (CMSSW_5_2_6)
 
 import FWCore.ParameterSet.Config as cms
 from FastSimulation.HighLevelTrigger.HLTSetup_cff import *
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_2_1/GRun/V171')
+  tableName = cms.string('/online/collisions/2012/7e33/v3.2/HLT/V2')
 )
 
 hltESSBTagRecord = cms.ESSource( "EmptyESSource",
