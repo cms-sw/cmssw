9fb14a88c4e0da6608f03027ba381b8c6387d4ad
Adrien Caudron <sha1-8f0d6c43c2a59dd7afae5302c75364feb095f4d0@cern.ch>
2012-07-03 14:09:00 +0000

    add for JEC sequences

--- DQMOffline/RecoB/python/bTagSequences_cff.py
+++ DQMOffline/RecoB/python/bTagSequences_cff.py
@@ -43,3 +43,19 @@ goodOfflinePrimaryVertices = cms.EDFilter(
 #to select events passing a scpecific trigger
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 bTagHLT  = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = ["HLT_PFJet40_v*"])
+
+#for the  use of JEC, could change with time : be careful if recommandations change for the correctors
+#define you sequence like  process.JECAlgo = cms.Sequence(process.ak5PFJetsJEC * process.PFJetsFilter)
+JetCut=cms.string("neutralHadronEnergyFraction < 0.99 && neutralEmEnergyFraction < 0.99 && nConstituents > 1 && chargedHadronEnergyFraction > 0.0 && chargedMultiplicity > 0.0 && chargedEmEnergyFraction < 0.99")
+
+from JetMETCorrections.Configuration.DefaultJEC_cff import *
+ak5PFJetsJEC = ak5PFJetsL2L3.clone(
+        src = 'ak5PFJets',
+        correctors = ['ak5PFL2L3']
+        )
+
+PFJetsFilter = cms.EDFilter("PFJetSelector",
+                            src = cms.InputTag("ak5PFJetsJEC"),
+                            cut = JetCut,
+                            filter = cms.bool(True)
+                            )
