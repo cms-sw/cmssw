21055fa00141824f6bc2126c235d15e869667615
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2012-07-04 15:49:43 +0000

    including fix for local cont. corr. in the release

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,9 +1,9 @@
 autoCond = { 
-    'mc'        : 'MC_53_V8::All',
-    'startup'   : 'START53_V8::All',
+    'mc'        : 'MC_53_V9::All',
+    'startup'   : 'START53_V9::All',
     'com10'     : 'GR_R_53_V4::All',
      # 'hltonline' should be the same as same as 'com10' until a compatible GR_H tag is available, then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
     'hltonline' : 'GR_R_53_V4::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/',
     'hltonline11' : ('GR_R_53_V4::All,frontier://FrontierProd/CMS_COND_31X_GLOBALTAG,frontier://FrontierProd/','L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T'),
-    'starthi'   : 'STARTHI53_V6::All'
+    'starthi'   : 'STARTHI53_V7::All'
 }
