a8a9f388f0f21e4f384df4149099a5b5a8a08a27
Daniel Duggan <sha1-a38f4466a54a4858eaba6132046a9c87013d90e8@cern.ch>
2012-07-06 11:43:32 +0000

    updated script to reduce runs shown on X-axis for png files

--- DQM/SiStripHistoricInfoClient/test/NewHDQM/test/makeRecoErrRatios.py
+++ DQM/SiStripHistoricInfoClient/test/NewHDQM/test/makeRecoErrRatios.py
@@ -1,27 +1,51 @@
 from ROOT import *
+import sys
 
 top = []
 bot = []
-file = TFile("recoErrors.root","UPDATE")
-#file  = TFile.Open("recoErrors.root")
+##I need to simply read in the appropriate web directory.
+##From here I can open the root file...
+dir = sys.argv[1]
+if dir[-1] != '/': dir += '/'
+file = TFile(dir+"RECOError_plots_all_plots.root", "UPDATE")
+print dir+ "RECOError_plots_all_plots.root"
 for key in file.GetListOfKeys():
+    print  key.GetName()
     if key.GetClassName() != "TH1F": continue
     if "denominator" in key.GetName(): bot.append(key.GetName())
     else: top.append(key.GetName())
-for t in top:
-    if len(bot) != 1: continue
-    if "Ratio" in t: continue
-    ##Load Denominator histo
-    bkey = file.GetListOfKeys().FindObject(bot[0])
-    bplot = bkey.ReadObj();
-    ##Load Numerator histo
-    tkey = file.GetListOfKeys().FindObject(t)
-    tplot = tkey.ReadObj();
-    ##Make ratio histo
-    ratio = tplot.Clone(t.split("_")[1]+"_Ratio")
-    ratio.SetTitle("Fraction of Errors in "+t.split("_")[1])
-    ratio.GetYaxis().SetTitle(t.split("_")[1])
-    ratio.Divide(bplot)
-##Write it out
-file.Write()
+##Load Denominator histo
+bkey = file.GetListOfKeys().FindObject(bot[0])
+bplot = bkey.ReadObj();
+if len(bot) ==1 and bplot.GetEntries() > 0:
+    for t in top:
+        if len(bot) != 1: continue
+        if "Ratio" in t: continue
+        ##Load Numerator histo
+        tkey = file.GetListOfKeys().FindObject(t)
+        tplot = tkey.ReadObj();
+        if not tplot.GetSumOfWeights() > 0.0: continue
+        ##Make new TCanvas to print out ratios
+        C=TCanvas("C","C",600,600)
+        ##Clone and setup ratio histo
+        ratio = tplot.Clone(t.split("_")[1]+"_Ratio")
+        ratio.SetName(t.split("_")[1]+"_Ratio")
+        ratio.SetTitle("Fraction of Errors in "+t.split("_")[1])
+        ##Make ratio, then print + save plots
+        ratio.Divide(bplot)
+        if ratio.Integral(int((ratio.GetNbinsX()*3)/4), ratio.GetNbinsX())/ratio.Integral(1, ratio.GetNbinsX()) > 0.90:
+            C.SetFrameLineWidth(10)
+            C.SetFrameLineColor(2)
+        print ratio.Integral(int((ratio.GetNbinsX()*3)/4), ratio.GetNbinsX())/ratio.Integral(1, ratio.GetNbinsX())
+        ##Now temporarily change the bins shown on X axis...
+        for i in range(1,pfTrackElec_Ratio->GetNbinsX()+1):
+            if i%int(145*0.03) !=  0: ratio.GetXaxis().SetBinLabel(i,"")
+        C.cd()
+        ratio.Draw()
+        C.SaveAs(dir+t.split("_")[1]+"_Ratio.png")
+        C.Close()
+        ##Now change bin labels back
+        for i in range(1,pfTrackElec_Ratio->GetNbinsX()+1): ratio.GetXaxis().SetBinLabel(i,bplot.GetXaxis().GetBinLabel(i))
+        file.cd()
+        ratio.Write()
 file.Close()
