52c454c77b0e298922b117b96e30d25086fc8b36
Adrien Caudron <sha1-8f0d6c43c2a59dd7afae5302c75364feb095f4d0@cern.ch>
2012-07-07 22:26:49 +0000

    sequence for data

--- Validation/RecoB/python/bTagAnalysis_firststep_cfi.py
+++ Validation/RecoB/python/bTagAnalysis_firststep_cfi.py
@@ -4,7 +4,13 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoB.bTagAnalysis_cfi import *
 
 bTagValidationFirstStep = bTagValidation.clone()
-
 bTagValidationFirstStep.finalizePlots = False
 bTagValidationFirstStep.finalizeOnly = False
-#
+
+
+from DQMOffline.RecoB.bTagAnalysisData_cfi import *
+
+bTagValidationFirstStepData = bTagAnalysis.clone()
+bTagValidationFirstStepData.finalizePlots = False
+bTagValidationFirstStepData.finalizeOnly = False
+
--- Validation/RecoB/python/bTagAnalysis_harvesting_cfi.py
+++ Validation/RecoB/python/bTagAnalysis_harvesting_cfi.py
@@ -4,7 +4,12 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoB.bTagAnalysis_cfi import *
 
 bTagValidationHarvest = bTagValidation.clone()
-
 bTagValidationHarvest.finalizePlots = True
 bTagValidationHarvest.finalizeOnly = True
-#
+
+
+from DQMOffline.RecoB.bTagAnalysisData_cfi import *
+
+bTagValidationHarvestData = bTagAnalysis.clone()
+bTagValidationHarvestData.finalizePlots = True
+bTagValidationHarvestData.finalizeOnly = True
