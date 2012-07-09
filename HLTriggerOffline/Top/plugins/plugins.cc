a5cb29a1b140b7f902a313a097e34aa84e3c0e5d
Abideh Jafari <sha1-b1dd6ed6fa47c29d651e4802a069539a71beee87@cern.ch>
2012-07-09 16:17:04 +0000

    for further use in Event monitoring

---
+++ HLTriggerOffline/Top/plugins/plugins.cc
@@ -0,0 +1,8 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "HLTriggerOffline/Top/interface/TopValidation.h"
+
+
+DEFINE_FWK_MODULE(TopValidation);
+//DEFINE_FWK_MODULE(Calculate);
