95cffcb622fb1dade3afdff86283a48befeec1fd
Matthew Hsing Hung Walker <sha1-2c1f16c1e845fbc1186c3b95fdda0af3180f1a61@cern.ch>
2012-07-09 09:13:35 +0000

    made some changes to defaults for Cosmics fitting

--- RecoTracker/TrackProducer/python/TrackRefitterP5_cfi.py
+++ RecoTracker/TrackProducer/python/TrackRefitterP5_cfi.py
@@ -4,7 +4,7 @@ import RecoTracker.TrackProducer.TrackRefitter_cfi
 TrackRefitterP5  = RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone(
     src = cms.InputTag("ctfWithMaterialTracksP5"),
     Fitter = cms.string('FittingSmootherRKP5'),
-    TTRHBuilder = cms.string('WithAngleAndTemplate'),
+    #TTRHBuilder = cms.string('WithAngleAndTemplate'),
     AlgorithmName = cms.string('ctf')
 )
 
