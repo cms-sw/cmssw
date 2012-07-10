2d2252bf6ade9ab3e85be8a2d74285bb5c62a839
Hans Jozef H Van Haevermaet <sha1-529be9e651b4fe8c7cd7bf6388f36e3905523124@cern.ch>
2012-07-10 14:53:49 +0000

    update towercut value

--- RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
+++ RecoLocalCalo/Castor/python/CastorTowerReco_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 	
 CastorTowerReco = cms.EDProducer('CastorTowerProducer',
 	inputprocess = cms.untracked.string("castorreco"),
-	towercut = cms.untracked.double(1.),
+	towercut = cms.untracked.double(0.65), # 4*sigma cut per channel in GeV
 	mintime = cms.untracked.double(-99999.), 
 	maxtime = cms.untracked.double(99999.) )
 
