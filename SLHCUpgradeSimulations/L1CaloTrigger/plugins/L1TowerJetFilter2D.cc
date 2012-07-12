7e53037b5a28e43d1fc62dee3af109af359365cf
Robyn Elizabeth Lucas <sha1-50a5c5d4db958b6d2aae3d7834fc100bfb31fa95@cern.ch>
2012-07-12 09:32:03 +0000

    Fix bug so only have one set of jets in the output collection

--- SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1TowerJetFilter2D.cc
+++ SLHCUpgradeSimulations/L1CaloTrigger/plugins/L1TowerJetFilter2D.cc
@@ -95,12 +95,12 @@ void L1TowerJetFilter2D::initialize(  )
 {
 }
 */
-
+int DO_ONCE = 1;
 void L1TowerJetFilter2D::algorithm( const int &aEta, const int &aPhi )
 {
 
-  if ( mComparisonDirection == phi && aPhi != mCaloTriggerSetup->phiMin() ) return;
-  else if ( mComparisonDirection == eta && aEta != mCaloTriggerSetup->etaMin() ) return;
+    if ( mComparisonDirection == phi && aPhi != mCaloTriggerSetup->phiMin() ) return;
+    else if ( mComparisonDirection == eta && aEta != mCaloTriggerSetup->etaMin() ) return;
 
 //---------------------------------------------------------------------------------------------------
 //  When we call fetch, we use the Wisconsin coordinate system
@@ -108,101 +108,102 @@ void L1TowerJetFilter2D::algorithm( const int &aEta, const int &aPhi )
 // ie aEta 4->60, aPhi 4->75
 //---------------------------------------------------------------------------------------------------
 
-  //declare a vector of structs to pass to algorithm 
-
-  //for just one value of eta/phi depending on mComparisonDirection
-  std::vector< JetWrapper2D > lJetWrapper2DVector;
-
-  //Want all of the input collection so we can veto properly.
-  //Its now the same for both directions
-
-  for(  int lPhi = mCaloTriggerSetup->phiMin() ; lPhi <= mCaloTriggerSetup->phiMax() ; ++lPhi ){
-     for(  int lEta = mCaloTriggerSetup->etaMin() ; lEta <= mCaloTriggerSetup->etaMax() ; ++lEta ){
-
-      l1slhc::L1TowerJetCollection::const_iterator lIt = fetch( lEta, lPhi);
- 
-      if( lIt != mInputCollection->end(  ) ){
-        lJetWrapper2DVector.push_back( JetWrapper2D( *lIt , mComparisonDirection ) ); 		
-      }
- 
-    }
-  }
-  lJetWrapper2DVector.resize(4096); //resize to nearest power of 2 to 72*56
-
-
-//--------------------------------------------------------------------------------------------------
-//  When we call iEta() and iPhi() on the Jets, we use the sensible integer coordinate system
-//  ie Eta runs from -28-28 with no 0, and Phi runs from 1-72
-// This system is used everywhere beyond this point
-//--------------------------------------------------------------------------------------------------
-
- // std::cout << "Sorting Jets produced by " << sourceName() << std::endl;
-/*
-  for( std::vector<JetWrapper2D>::iterator lIt =lJetWrapper2DVector.begin(); lIt != lJetWrapper2DVector.end(); ++lIt){
-    if( (*lIt).mJet )	{
-   //   std::cout << "Before sort, (eta, phi) = " << (*lIt).mJet->iEta() << " " << (*lIt).mJet->iPhi() <<"  energy " << (*lIt).mJet->E() << " and asym = " << (*lIt).mJet->AsymPhi() <<std::endl;	
-    }
-  }*/
-
-  // sort jets around eta/phi by energy
-  std::vector<JetWrapper2D>::iterator lStart( lJetWrapper2DVector.begin() );
-  std::vector<JetWrapper2D>::iterator lEnd( lJetWrapper2DVector.end() );	
-  BitonicSort< JetWrapper2D >( down , lStart , lEnd );
-
-  //Filter the jets with vetoes
-  std::deque< std::pair<int, int> > lVetos; 
-  int lCounter(0);
-  for( std::vector<JetWrapper2D>::iterator lIt =lJetWrapper2DVector.begin(); lIt != lJetWrapper2DVector.end(); ++lIt){
-
-    if( (*lIt).mJet ){ //if jet exists	
-      int lJetsize =  (*lIt).mJet->JetSize() ;
-      bool lVetoed( false );
-
-      for( std::deque< std::pair<int, int> >::iterator lIt2 = lVetos.begin() ; lIt2 != lVetos.end() ; ++ lIt2 ){
-
-        if ( (*lIt).mJet->iEta() == (*lIt2).first && (*lIt).mJet->iPhi()  == (*lIt2).second ){ 
-          //if jet is already vetoed break
-          lVetoed = true;
-          break;
+    //declare a vector of structs to pass to algorithm 
+  
+    //for just one value of eta/phi depending on mComparisonDirection
+    std::vector< JetWrapper2D > lJetWrapper2DVector;
+  
+    //Want all of the input collection so we can veto properly.
+    //Its now the same for both directions
+  
+    for(  int lPhi = mCaloTriggerSetup->phiMin() ; lPhi <= mCaloTriggerSetup->phiMax() ; ++lPhi ){
+      for(  int lEta = mCaloTriggerSetup->etaMin() ; lEta <= mCaloTriggerSetup->etaMax() ; ++lEta ){
+  
+        l1slhc::L1TowerJetCollection::const_iterator lIt = fetch( lEta, lPhi);
+  
+        if( lIt != mInputCollection->end(  ) ){
+          lJetWrapper2DVector.push_back( JetWrapper2D( *lIt , mComparisonDirection ) ); 		
         }
+  
       }
-
-      if( !lVetoed ){	//if jet not vetoed then add to collection and create vetoes around it
-      //std::cout << "Added jet to the output collection = (" << (*lIt).mJet->iEta() << " , " << (*lIt).mJet->iPhi() <<"), energy = " << (*lIt).mJet->E() << " and asym = " << (*lIt).mJet->AsymPhi() <<std::endl;	
-        
-        mOutputCollection->insert( (*lIt).mJet->iEta() , (*lIt).mJet->iPhi() , *((*lIt).mJet)  );
-        lCounter++;
-        for( int i = -lJetsize +1 ; i != lJetsize ; ++i ){
-
-          int lPhi( (*lIt).mJet->iPhi() + i );
-          if( lPhi >  72 ) lPhi -=  72;
-          if( lPhi <  1 ) lPhi +=  72;
-          std::pair<int,int> veto;
-          veto.second = lPhi;
-          //for each phi, want to eradicate all the etas along it
-          for( int j = -lJetsize +1 ; j != lJetsize ; ++j ){
-            int lEta( (*lIt).mJet->iEta() + j );
-
-            // no eta=0 in this coordinate system: need to allow for this
-            if(lEta == 0 && j<0) lEta = (*lIt).mJet->iEta() - lJetsize;
-            if(lEta == 0 && j>0) lEta = (*lIt).mJet->iEta() + lJetsize;
-
-            veto.first = lEta;
-            lVetos.push_back( veto );
+    }
+    lJetWrapper2DVector.resize(4096); //resize to nearest power of 2 to 72*56
+  
+  
+  //--------------------------------------------------------------------------------------------------
+  //  When we call iEta() and iPhi() on the Jets, we use the sensible integer coordinate system
+  //  ie Eta runs from -28-28 with no 0, and Phi runs from 1-72
+  // This system is used everywhere beyond this point
+  //--------------------------------------------------------------------------------------------------
+  
+    //std::cout << "Sorting Jets produced by " << sourceName() << std::endl;
+  
+    for( std::vector<JetWrapper2D>::iterator lIt =lJetWrapper2DVector.begin(); lIt != lJetWrapper2DVector.end(); ++lIt){
+      if( (*lIt).mJet )	{
+        //std::cout << "Before sort, (eta, phi) = " << (*lIt).mJet->iEta() << " " << (*lIt).mJet->iPhi() <<"  energy " << (*lIt).mJet->E() << " and asym = " << (*lIt).mJet->AsymPhi() <<std::endl;	
+      }
+    }
+  
+    // sort jets around eta/phi by energy
+    std::vector<JetWrapper2D>::iterator lStart( lJetWrapper2DVector.begin() );
+    std::vector<JetWrapper2D>::iterator lEnd( lJetWrapper2DVector.end() );	
+    BitonicSort< JetWrapper2D >( down , lStart , lEnd );
+  
+    //Filter the jets with vetoes
+    std::deque< std::pair<int, int> > lVetos; 
+    int lCounter(0);
+    for( std::vector<JetWrapper2D>::iterator lIt =lJetWrapper2DVector.begin(); lIt != lJetWrapper2DVector.end(); ++lIt){
+  
+      if( (*lIt).mJet ){ //if jet exists	
+        int lJetsize =  (*lIt).mJet->JetSize() ;
+        bool lVetoed( false );
+  
+        for( std::deque< std::pair<int, int> >::iterator lIt2 = lVetos.begin() ; lIt2 != lVetos.end() ; ++ lIt2 ){
+  
+          if ( (*lIt).mJet->iEta() == (*lIt2).first && (*lIt).mJet->iPhi()  == (*lIt2).second ){ 
+            //if jet is already vetoed break
+            lVetoed = true;
+            break;
           }
         }
-        if( lCounter > mNumOfOutputJets ) break;  //only N jets per ring/strip
-      }
-    }//jet exists
-  }
+  
+        if( !lVetoed && (DO_ONCE % 57)==0 ){	//if jet not vetoed then add to collection and create vetoes around it
+        //std::cout << "Added jet to the output collection = (" << (*lIt).mJet->iEta() << " , " << (*lIt).mJet->iPhi() <<"), energy = " << (*lIt).mJet->E() << " and asym = " << (*lIt).mJet->AsymPhi() <<std::endl;	
+          
+          mOutputCollection->insert( (*lIt).mJet->iEta() , (*lIt).mJet->iPhi() , *((*lIt).mJet)  );
+          lCounter++;
+          for( int i = -lJetsize +1 ; i != lJetsize ; ++i ){
+  
+            int lPhi( (*lIt).mJet->iPhi() + i );
+            if( lPhi >  72 ) lPhi -=  72;
+            if( lPhi <  1 ) lPhi +=  72;
+            std::pair<int,int> veto;
+            veto.second = lPhi;
+            //for each phi, want to eradicate all the etas along it
+            for( int j = -lJetsize +1 ; j != lJetsize ; ++j ){
+              int lEta( (*lIt).mJet->iEta() + j );
+  
+              // no eta=0 in this coordinate system: need to allow for this
+              if(lEta == 0 && j<0) lEta = (*lIt).mJet->iEta() - lJetsize;
+              if(lEta == 0 && j>0) lEta = (*lIt).mJet->iEta() + lJetsize;
+  
+              veto.first = lEta;
+              lVetos.push_back( veto );
+            }
+          }
+          if( lCounter > mNumOfOutputJets ) break;  //only N jets per ring/strip
+        }
+      }//jet exists
+    }
 
+    //only want to add jets to the output collection ONCE
 
-  
-  
-  
+  //std::cout<<"iteration through function: "<<DO_ONCE<<std::endl;
+    
+  ++DO_ONCE;
   
 }
-
+ 
 DEFINE_EDM_PLUGIN (edm::MakerPluginFactory,edm::WorkerMaker<L1TowerJetFilter2D>,"L1TowerJetFilter2D");
 DEFINE_FWK_PSET_DESC_FILLER(L1TowerJetFilter2D);
 
