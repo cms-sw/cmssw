75b784115212b8ff3067a667a95398439f5d8af2
Tomas Hreus <sha1-3b7f95655a4528fda85af562e681c07c81f0445e@cern.ch>
2012-07-19 13:21:14 +0000

    update paths to APVShot and APVShotFinder

--- DQM/SiStripMonitorDigi/interface/SiStripMonitorDigi.h
+++ DQM/SiStripMonitorDigi/interface/SiStripMonitorDigi.h
@@ -8,7 +8,7 @@
 */
 // Original Author:  dkcira
 //         Created:  Sat Feb  4 20:49:51 CET 2006
-// $Id: SiStripMonitorDigi.h,v 1.29 2011/10/27 19:49:19 fiori Exp $
+// $Id: SiStripMonitorDigi.h,v 1.30 2012/07/13 15:32:52 threus Exp $
 #include <memory>
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/LuminosityBlock.h"
@@ -18,8 +18,8 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "DQM/SiStripCommon/interface/TkHistoMap.h"
-#include "DPGAnalysis/SiStripTools/interface/APVShotFinder.h"
-#include "DPGAnalysis/SiStripTools/interface/APVShot.h"
+#include "DQM/SiStripCommon/interface/APVShotFinder.h"
+#include "DQM/SiStripCommon/interface/APVShot.h"
 
 class DQMStore;
 class SiStripDCSStatus;
