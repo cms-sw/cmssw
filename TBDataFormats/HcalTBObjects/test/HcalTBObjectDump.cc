ae03f60faef7cd57f55433fef692ad300f832364
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2012-07-20 20:59:13 +0000

    Remove unneeded #include of header soon to be deleted

--- TBDataFormats/HcalTBObjects/test/HcalTBObjectDump.cc
+++ TBDataFormats/HcalTBObjects/test/HcalTBObjectDump.cc
@@ -7,7 +7,6 @@
 #include "TBDataFormats/HcalTBObjects/interface/HcalTBRunData.h"
 #include "TBDataFormats/HcalTBObjects/interface/HcalTBEventPosition.h"
 #include "TBDataFormats/HcalTBObjects/interface/HcalTBTiming.h"
-#include "FWCore/Framework/interface/Selector.h"
 #include <iostream>
 
 using namespace std;
@@ -16,8 +15,8 @@ namespace cms {
 
   /** \class HcalTBObjectDump
       
-  $Date: 2007/04/10 23:11:20 $
-  $Revision: 1.6 $
+  $Date: 2010/02/25 00:34:37 $
+  $Revision: 1.7 $
   \author J. Mans - Minnesota
   */
   class HcalTBObjectDump : public edm::EDAnalyzer {
