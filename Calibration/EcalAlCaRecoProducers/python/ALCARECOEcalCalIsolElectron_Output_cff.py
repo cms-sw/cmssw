a4be5ee15a5ed9c18c14f65d88a6a727dfcc5e6c
Shervin Nourbakhsh <sha1-425501efa29961317dfc55b3ef88a3db4a2f33ab@cern.ch>
2012-07-23 16:27:57 +0000

    updated output commands

--- Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
+++ Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
@@ -1,34 +1,49 @@
 import FWCore.ParameterSet.Config as cms
 
-OutALCARECOEcalCalElectron = cms.PSet(
+
+
+# output block for alcastream Electron
+OutALCARECOEcalCalElectron_specific = cms.untracked.vstring(
+    'drop *EcalRecHit*_ecalRecHit_*_*',
+    'drop *EcalrecHit*_*ecalPreshowerRecHit*_*EcalRecHitsES*_*',
+    'drop *EcalRecHit*_reducedEcalRecHitsE*_*_*',
+    'keep *EcalRecHit*_alCaIsolatedElectrons_*_*',
+    'keep *EcalRecHit*_reducedEcalRecHitsES_alCaRecHitsES_*'
+)
+
+OutALCARECOEcalCalElectron_noDrop = cms.PSet(
+    # put this if you have a filter
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('pathALCARECOEcalCalElectron')
     ),
-    outputCommands = cms.untracked.vstring(
-    "drop *_*_*_*",
-    "keep *_pfMet_*_*",
-    "keep *_kt6PFJetsForRhoCorrection_rho_*",
-    "keep *_kt6PFJets_rho_*",
-    "keep *_offlinePrimaryVerticesWithBS_*_*",
-    "keep *BeamSpot_offlineBeamSpot_*_*",
-    "keep *_allConversions_*_*",
-    "keep *_conversions_*_*",
-    "keep *GsfTrack*_*_*_*",
-    "keep *_generator_*_*",
+    outputCommands = cms.untracked.vstring( 
+    'keep *_pfMet_*_*',
+    'keep *_kt6PFJetsForRhoCorrection_rho_*',
+    'keep *_kt6PFJets_rho_*',
+    'keep *_offlinePrimaryVerticesWithBS_*_*',
+    'keep *BeamSpot_offlineBeamSpot_*_*',
+    'keep *_allConversions_*_*',
+    'keep *_conversions_*_*',
+    'keep *GsfTrack*_*_*_*',
+    'keep *_generator_*_*',
     'keep *_addPileupInfo_*_*',
     'keep *_genParticles_*_*',
+    'keep recoGsfElectron*_gsfElectron*_*_*',
     'keep recoCaloClusters_*_*_*',
     'keep recoSuperClusters_*_*_*',
     'keep recoPreshowerCluster*_*_*_*',
-    'drop reco*Clusters_hfEMClusters_*_RECO',
-    'drop reco*Clusters_*Translator_*_RECO',
-    'keep reco*Clusters_pfElectronTranslator_*_*',
-    'keep recoGsfElectron*_*_*_*',
-    'drop recoGsfElectron*_uncleaned*_*_*',
-    'keep *EcalRecHit*_alCaIsolatedElectrons_*_*',#+processName
-    'keep *EcalRecHit*_reducedEcalRecHitsES_*_*',
-    'drop *EcalRecHit*_ecalRecHit_*_*',
-    'drop *EcalrecHit*_*ecalPreshowerRecHit*_*EcalRecHitsES*_*'
+    'drop reco*Clusters_hfEMClusters_*_*',
+    'drop reco*Clusters_pfPhotonTranslator_*_*',
+    'drop *_*Unclean*_*_*',
+    'drop *_*unclean*_*_*',
+    'drop *_*_*Unclean*_*',
+    'drop *_*_*unclean*_*'       
     )
 )
 
+OutALCARECOEcalCalElectron_noDrop.outputCommands+=OutALCARECOEcalCalElectron_specific
+
+
+import copy
+OutALCARECOEcalCalElectron=copy.deepcopy(OutALCARECOEcalCalElectron_noDrop)
+OutALCARECOEcalCalElectron.outputCommands.insert(0, "drop *")
