f09156beb9374a27e0235266d7f4d42f5e533758
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-07-24 17:04:17 +0000

    Find must still be public

--- DataFormats/Common/interface/AssociationMap.h
+++ DataFormats/Common/interface/AssociationMap.h
@@ -6,7 +6,7 @@
  *
  * \author Luca Lista, INFN
  *
- * $Id: AssociationMap.h,v 1.42 2011/11/16 22:33:15 wmtan Exp $
+ * $Id: AssociationMap.h,v 1.43 2012/07/24 04:56:32 wmtan Exp $
  *
  */
 #include "DataFormats/Common/interface/CMS_CLASS_VERSION.h"
@@ -132,10 +132,8 @@ namespace edm {
      /// post insert action
     void post_insert() { Tag::sort(map_); }
 
-    //Used by ROOT storage
-    CMS_CLASS_VERSION(10)
-
-    private:
+    // Find should be private!  However, generated reflex dictionaries do not compile
+    // if Find is private.  
     /// find helper
     struct Find :
       public std::binary_function<const self&, size_type, const value_type *> {
@@ -146,6 +144,10 @@ namespace edm {
       }
     };
 
+    //Used by ROOT storage
+    CMS_CLASS_VERSION(10)
+
+  private:
     /// reference set
     ref_type ref_;
     /// index map
