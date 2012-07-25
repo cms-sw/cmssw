c81a258237688fe3d252ae31604e1d1b5ea19f00
Grigory Safronov <sha1-6b5ae81a08744f20f03dac79ab370e2ec5b18f30@cern.ch>
2012-07-25 11:52:24 +0000

    *** empty log message ***

--- DQMOffline/CalibCalo/python/MonitorAlCaHcalPhisym_cfi.py
+++ DQMOffline/CalibCalo/python/MonitorAlCaHcalPhisym_cfi.py
@@ -5,19 +5,19 @@
 import FWCore.ParameterSet.Config as cms
 
 #
-# $Id: MonitorAlCaHcalPhisym_cfi.py,v 1.4 2009/09/23 16:05:56 kodolova Exp $
+# $Id: MonitorAlCaHcalPhisym_cfi.py,v 1.5 2009/11/16 12:31:38 kodolova Exp $
 #
 # \author Stefano Argiro
 #
 HcalPhiSymMon = cms.EDAnalyzer("DQMHcalPhiSymAlCaReco",
     # product to monitor
-    hbheInputMB = cms.InputTag("hbherecoMB"),
-    hoInputMB = cms.InputTag("horecoMB"),
-    hfInputMB = cms.InputTag("hfrecoMB"),
+    hbheInputMB = cms.InputTag("hbheprereco"),
+    hoInputMB = cms.InputTag("horeco"),
+    hfInputMB = cms.InputTag("hfreco"),
     hbheInputNoise = cms.InputTag("hbherecoNoise"),
     hoInputNoise = cms.InputTag("horecoNoise"),
     hfInputNoise = cms.InputTag("hfrecoNoise"),
-    rawInputLabel=cms.InputTag("source"),
+    rawInputLabel=cms.InputTag("rawDataCollector"),
     period = cms.uint32(4096),                               
     # File to save 
     SaveToFile = cms.untracked.bool(False),
