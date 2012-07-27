3c97cfc1f51b594e0afb4c1780b6c69638bc6b62
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-07-27 10:36:46 +0000

    add all methods

--- RecoLuminosity/LumiProducer/interface/LumiCorrectionParam.h
+++ RecoLuminosity/LumiProducer/interface/LumiCorrectionParam.h
@@ -7,21 +7,59 @@
 class LumiCorrectionParam {
  public:
   enum LumiType{HF,PIXEL};
-  
   /// default constructor
   LumiCorrectionParam();
   explicit LumiCorrectionParam(LumiType lumitype);
   /// destructor
   ~LumiCorrectionParam(){}
   
-  ///set ncollidingbunches
-  void setNBX(unsigned int nbx);
+  /*
+    getters are used by users
+    standard user just needs to apply the final correction
+    other methods are for inspection or advanced correction customisation
+  */
+  ///get the final correction factor
+  float getCorrection()const;
   ///get ncollidingbunches
   unsigned int ncollidingbunches()const;
-  ///
+  ///get current normtag
+  std::string normtag()const;
+  ///get correction function name
+  std::string corrFunc()const;
+  ///get correction coefficients
+  std::map<const std::string,float>::const_iterator nonlinearCoeff()const;
+  ///get afterglow threshold/value
+  std::vector< std::pair<unsigned int,float> >::const_iterator afterglows()const;
+  ///on which amodetag this correction definition should be applied for
+  ///information only
+  std::string amodetag()const;
+  ///on which single beam egev this correction definition should be applied for
+  ///information only
+  unsigned int beamegev()const;
+
+  /*
+    setters 
+  */  
+  ///set ncollidingbunches
+  void setNBX(unsigned int nbx);  
+  ///set current normtag
+  void setNormtag(const std::string& normtag);
+  ///set correction function
+  void setcorrFunc(const std::string& corrfunc);
+  ///set nonlinear constants
+  void setnonlinearCoeff(std::map<const std::string,float>& coeffmap);
+  ///set afterglow thresholds
+  void setafterglows(std::vector< std::pair<unsigned int,float> >& afterglows);
+  
  private :
   LumiType m_lumitype;
   unsigned int m_ncollidingbx;
+  std::string m_normtag;
+  std::string m_corrfunc;
+  std::map<const std::string,float> m_coeffmap;
+  std::vector< std::pair<unsigned int,float> > m_afterglows;
+  std::string m_amodetag;
+  float m_beamegev;
 }; 
 
 std::ostream& operator<<(std::ostream& s, const LumiCorrectionParam& lumicorr);
--- RecoLuminosity/LumiProducer/src/LumiCorrectionParam.cc
+++ RecoLuminosity/LumiProducer/src/LumiCorrectionParam.cc
@@ -1,16 +1,62 @@
 #include "RecoLuminosity/LumiProducer/interface/LumiCorrectionParam.h"
 #include <iomanip>
 #include <ostream>
-LumiCorrectionParam::LumiCorrectionParam():m_lumitype(LumiCorrectionParam::HF),m_ncollidingbx(0){}
-LumiCorrectionParam::LumiCorrectionParam(LumiCorrectionParam::LumiType lumitype):m_lumitype(lumitype),m_ncollidingbx(0){}
-void
-LumiCorrectionParam::setNBX(unsigned int nbx){
-  m_ncollidingbx=nbx;
+LumiCorrectionParam::LumiCorrectionParam():m_lumitype(LumiCorrectionParam::HF),m_ncollidingbx(0),m_normtag(""),m_corrfunc(""),m_amodetag("PROTPHYS"),m_beamegev(0.){}
+LumiCorrectionParam::LumiCorrectionParam(LumiCorrectionParam::LumiType lumitype):m_lumitype(lumitype),m_ncollidingbx(0),m_normtag(""),m_corrfunc(""),m_amodetag("PROTPHYS"),m_beamegev(0.){}
+
+float 
+LumiCorrectionParam::getCorrection()const{
+  return 1.0;
 }
 unsigned int 
 LumiCorrectionParam::ncollidingbunches()const{
   return m_ncollidingbx;
 }
+std::string 
+LumiCorrectionParam::normtag()const{
+  return m_normtag;
+}
+std::string 
+LumiCorrectionParam::corrFunc()const{
+  return m_corrfunc;
+}
+std::map<const std::string,float>::const_iterator 
+LumiCorrectionParam::nonlinearCoeff()const{
+  return m_coeffmap.begin();
+}
+std::vector< std::pair<unsigned int,float> >::const_iterator 
+LumiCorrectionParam::afterglows()const{
+  return m_afterglows.begin();
+}
+std::string 
+LumiCorrectionParam::amodetag()const{
+  return m_amodetag;
+}
+unsigned int 
+LumiCorrectionParam::beamegev()const{
+  return m_beamegev;
+}
+void 
+LumiCorrectionParam::setNBX(unsigned int nbx){
+  m_ncollidingbx=nbx;
+}
+void 
+LumiCorrectionParam::setNormtag(const std::string& normtag){
+  m_normtag=normtag;
+}
+void 
+LumiCorrectionParam::setcorrFunc(const std::string& corrfunc){
+  m_corrfunc=corrfunc;
+}
+void 
+LumiCorrectionParam::setnonlinearCoeff(std::map<const std::string,float>& coeffmap){
+  m_coeffmap=coeffmap;
+}
+void 
+LumiCorrectionParam::setafterglows(std::vector< std::pair<unsigned int,float> >& afterglows){
+  m_afterglows=afterglows;
+}
+
 std::ostream& operator<<(std::ostream& s, LumiCorrectionParam const& lumiparam){
   s<<"\n LumiCorrectionParam\n";
   s << std::setw(12) << "ncollidingbx " << lumiparam.ncollidingbunches() << "\n";
