fa81383842c633548394c6c77a9ab2f1ee7ca081
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2012-07-31 22:32:18 +0000

    Fix clang safety report - change static local member to const static.

--- Fireworks/Core/src/FWGUIManager.cc
+++ Fireworks/Core/src/FWGUIManager.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Feb 11 11:06:40 EST 2008
-// $Id: FWGUIManager.cc,v 1.250.2.3 2012/02/18 01:58:26 matevz Exp $
+// $Id: FWGUIManager.cc,v 1.251 2012/02/22 03:45:59 amraktad Exp $
 
 
 //
@@ -782,7 +782,8 @@ FWGUIManager::getCurrentEvent() const
 bool
 FWGUIManager::promptForConfigurationFile(std::string &result, enum EFileDialogMode mode)
 {
-   static const char* kFileTypes[] = {"Fireworks Configuration files","*.fwc",
+   
+   const static char* kFileTypes[] = {"Fireworks Configuration files","*.fwc",
                                        "All Files","*",
                                        0,0};
 
