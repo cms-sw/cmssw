1779f7b8a6b3ee20f97112788ce5bc1a2d711fc5
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-08-01 10:46:37 +0000

    HLT specific configuration

---
+++ HLTrigger/Timer/python/hltFastTimerService_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+# FastTimerService
+FastTimerService = cms.Service( 'FastTimerService',
+    useRealTimeClock        = cms.untracked.bool( True ),
+    enableTimingPaths       = cms.untracked.bool( True ),
+    enableTimingModules     = cms.untracked.bool( True ),
+    enableTimingSummary     = cms.untracked.bool( False ),
+    skipFirstPath           = cms.untracked.bool( False ), 
+    enableDQM               = cms.untracked.bool( True ),
+    enableDQMbyPathActive   = cms.untracked.bool( False ),
+    enableDQMbyPathTotal    = cms.untracked.bool( True ),
+    enableDQMbyPathOverhead = cms.untracked.bool( False ),
+    enableDQMbyPathDetails  = cms.untracked.bool( False ),
+    enableDQMbyPathCounters = cms.untracked.bool( False ),
+    enableDQMbyModule       = cms.untracked.bool( False ),
+    enableDQMbyLumi         = cms.untracked.bool( False ),
+    dqmTimeRange            = cms.untracked.double( 1000.  ),
+    dqmTimeResolution       = cms.untracked.double(    5.  ),
+    dqmPathTimeRange        = cms.untracked.double(  100.  ),
+    dqmPathTimeResolution   = cms.untracked.double(    0.5 ),
+    dqmModuleTimeRange      = cms.untracked.double(   40.  ),
+    dqmModuleTimeResolution = cms.untracked.double(    0.2 ),
+    dqmPath                 = cms.untracked.string( "HLT/TimerService" )
+)
