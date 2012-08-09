5eb866ae57e3c9118fd83595c04e33431886317e
Pablo Martinez Ruiz Del Arbol <sha1-15dc4660aa7959f8f641d1f9d99c061d1a122e5d@cern.ch>
2012-08-09 07:30:10 +0000

    New development adding more histograms to layout. Updated list of triggers.

--- DQMOffline/Trigger/python/HLTMuonOfflineAnalyzer_cff.py
+++ DQMOffline/Trigger/python/HLTMuonOfflineAnalyzer_cff.py
@@ -16,38 +16,61 @@ trackerMuonParams = cms.PSet(
     hltCuts  = cms.untracked.string("abs(eta) < 2.4"),
 )
 
-vbtfMuonParams = cms.PSet(
+
+tightMuonParams = cms.PSet(
     d0Cut = cms.untracked.double(0.2),
-    z0Cut = cms.untracked.double(25.0),
+    z0Cut = cms.untracked.double(0.5),
     recoCuts = cms.untracked.string(' && '.join([
-        "isGlobalMuon && isTrackerMuon",
+        "isGlobalMuon && isPFMuon && isTrackerMuon",
         "abs(eta) < 2.4",
         "innerTrack.hitPattern.numberOfValidPixelHits > 0",
-        "innerTrack.hitPattern.numberOfValidTrackerHits > 10",
+        "innerTrack.hitPattern.trackerLayersWithMeasurement > 5",
+        "(pfIsolationR04().sumChargedHadronPt + max(pfIsolationR04().sumNeutralHadronEt + pfIsolationR04().sumPhotonEt - pfIsolationR04().sumPUPt/2,0.0))/pt < 0.12", 
         "globalTrack.hitPattern.numberOfValidMuonHits > 0",
         "globalTrack.normalizedChi2 < 10",
-        "numberOfMatches > 1",
+        "numberOfMatches > 1"
         ])),
     hltCuts  = cms.untracked.string("abs(eta) < 2.4"),
 )
 
+
+
+looseMuonParams = cms.PSet(
+    d0Cut = cms.untracked.double(50),
+    z0Cut = cms.untracked.double(100),
+    recoCuts = cms.untracked.string(' && '.join([
+        "isPFMuon && (isTrackerMuon || isGlobalMuon)",
+        "(pfIsolationR04().sumChargedHadronPt + max(pfIsolationR04().sumNeutralHadronEt + pfIsolationR04().sumPhotonEt - pfIsolationR04().sumPUPt/2,0.0))/pt < 0.20"
+        ])),
+    hltCuts  = cms.untracked.string("abs(eta) < 2.4"),
+)
+
+
 globalAnalyzer = hltMuonOfflineAnalyzer.clone()
 globalAnalyzer.destination = "HLT/Muon/DistributionsGlobal"
 globalAnalyzer.targetParams = globalMuonParams
-globalAnalyzer.probeParams = cms.PSet()
+#globalAnalyzer.probeParams = cms.PSet()
 
 trackerAnalyzer = hltMuonOfflineAnalyzer.clone()
 trackerAnalyzer.destination = "HLT/Muon/DistributionsTracker"
 trackerAnalyzer.targetParams = trackerMuonParams
-trackerAnalyzer.probeParams = cms.PSet()
+#trackerAnalyzer.probeParams = cms.PSet()
 
-vbtfAnalyzer = hltMuonOfflineAnalyzer.clone()
-vbtfAnalyzer.destination = "HLT/Muon/DistributionsVbtf"
-vbtfAnalyzer.targetParams = vbtfMuonParams
-vbtfAnalyzer.probeParams = vbtfMuonParams
+tightAnalyzer = hltMuonOfflineAnalyzer.clone()
+tightAnalyzer.destination = "HLT/Muon/DistributionsTight"
+tightAnalyzer.targetParams = tightMuonParams
+#tightAnalyzer.probeParams = cms.PSet() 
+
+looseAnalyzer = hltMuonOfflineAnalyzer.clone()
+looseAnalyzer.destination = "HLT/Muon/DistributionsLoose"
+looseAnalyzer.targetParams = looseMuonParams
+#tightAnalyzer.probeParams = cms.PSet() 
+
+
 
 hltMuonOfflineAnalyzers = cms.Sequence(
     globalAnalyzer *
     trackerAnalyzer *
-    vbtfAnalyzer
+    tightAnalyzer *
+    looseAnalyzer
 )
