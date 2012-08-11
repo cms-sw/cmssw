2b5f24e10e98d72a7c29c3ba9cf4c3981cf986bf
Jaime Arturo Gomez <sha1-7724e0e940e6c5c8be734dfad93dd92f59c00b98@cern.ch>
2012-08-11 22:25:08 +0000

    Updating the ZDCSimHitStudy code to show more meaningful plots

--- Validation/HcalHits/interface/ZdcSimHitStudy.h
+++ Validation/HcalHits/interface/ZdcSimHitStudy.h
@@ -1,9 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Package:    ZdcSimHitStudy
+// Class:      ZdcSimHitStudy
+//
+/*
+ Description: 
+              This code has been developed to be a check for the ZDC sim. In 2009, it was found that the ZDC Simulation was unrealistic and needed repair. The aim of this code is to show the user the input and output of a ZDC MinBias simulation.
+
+ Implementation:
+      First a MinBias simulation should be run, it could be pythia,hijin,or hydjet. This will output a .root file which should have information about recoGenParticles, hcalunsuppresseddigis, and g4SimHits_ZDCHits. Use this .root file as the input into the cfg.py which is found in the main directory of this package. This output will be another .root file which is meant to be viewed in a TBrowser.
+
+*/
+//
+// Original Author: Jaime Gomez (U. of Maryland) with SIGNIFICANT assistance of Dr. Jefferey Temple (U. of Maryland) 
+// Adapted from: E. Garcia-Solis' (CSU) original code    
+//
+//         Created:  Summer 2012
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 #ifndef SimG4CMS_ZdcSimHitStudy_H
 #define SimG4CMS_ZdcSimHitStudy_H
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -19,11 +39,17 @@
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <map>
 #include <string>
+#include <memory>
 
 class ZdcSimHitStudy: public edm::EDAnalyzer{
 public:
@@ -35,6 +61,8 @@ protected:
 
   void beginJob ();
   void endJob   ();
+  void endRun(const edm::Run& run, const edm::EventSetup& c);
+
   void analyze  (const edm::Event& e, const edm::EventSetup& c);
   void analyzeHits  (std::vector<PCaloHit> &);
   int FillHitValHist (int side,int section,int channel,double energy,double time);
@@ -43,6 +71,13 @@ private:
   double enetotEmN, enetotHadN,enetotN;  
   double enetotEmP,enetotHadP,enetotP;
   double enetot;
+
+  /////////////////////////////////////////
+  //#   Below all the monitoring elements #
+  //# are simply the plots "code names"  #
+  //# they will be filled in the .cc file #
+  /////////////////////////////////////////
+
 
 
   std::string    g4Label, zdcHits, outFile_;
@@ -67,6 +102,53 @@ private:
   MonitorElement *meZdcEneTHadP1_,*meZdcEneTHadP2_,*meZdcEneTHadP3_,*meZdcEneTHadP4_;
   MonitorElement *meZdcEneHadPTot_,*meZdcEneEmPTot_, *meZdcEnePTot_;
   MonitorElement *meZdcCorEEmPEHadP_,*meZdcCorEtotNEtotP_,*meZdcEneTot_;
+
+///////////////////New Plots/////////////
+
+////////GenParticle Plots///////
+ MonitorElement* genpart_Pi0F;
+ MonitorElement* genpart_Pi0F_energydist;
+ MonitorElement* genpart_Pi0B;
+ MonitorElement* genpart_Pi0B_energydist;
+ MonitorElement* genpart_NeutF;
+ MonitorElement* genpart_NeutF_energydist;
+ MonitorElement* genpart_NeutB;
+ MonitorElement* genpart_NeutB_energydist;
+ MonitorElement* genpart_GammaF;
+ MonitorElement* genpart_GammaF_energydist;
+ MonitorElement* genpart_GammaB;
+ MonitorElement* genpart_GammaB_energydist;
+/////////////////////////////////
+
+/////////ZDC Digi Plots////////
+ MonitorElement* meZdcfCPHAD;
+ MonitorElement* meZdcfCPTOT;
+ MonitorElement* meZdcfCNHAD;
+ MonitorElement* meZdcfCNTOT;
+ MonitorElement* meZdcfCPEMvHAD;
+ MonitorElement* meZdcfCNEMvHAD;
+ MonitorElement* meZdcPEM1fCvsTS;
+ MonitorElement* meZdcPEM2fCvsTS;
+ MonitorElement* meZdcPEM3fCvsTS;
+ MonitorElement* meZdcPEM4fCvsTS;
+ MonitorElement* meZdcPEM5fCvsTS;
+ MonitorElement* meZdcPHAD1fCvsTS;
+ MonitorElement* meZdcPHAD2fCvsTS;
+ MonitorElement* meZdcPHAD3fCvsTS;
+ MonitorElement* meZdcPHAD4fCvsTS;
+ MonitorElement* meZdcNEM1fCvsTS;
+ MonitorElement* meZdcNEM2fCvsTS;
+ MonitorElement* meZdcNEM3fCvsTS;
+ MonitorElement* meZdcNEM4fCvsTS;
+ MonitorElement* meZdcNEM5fCvsTS;
+ MonitorElement* meZdcNHAD1fCvsTS;
+ MonitorElement* meZdcNHAD2fCvsTS;
+ MonitorElement* meZdcNHAD3fCvsTS;
+ MonitorElement* meZdcNHAD4fCvsTS;
+////////////////////////////////////////
+
+
+
 };
 
 #endif
