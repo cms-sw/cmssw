7c9d30f85e93682bd6a8a2dd114a5db37fe1cff4
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2012-08-17 15:33:37 +0000

    Remove obsolete module not used anymore. Immediate motivation was to get rid of the usage of getByType function whose support is being dropped from the Framework.

--- SimCalorimetry/EcalTrigPrimProducers/plugins/SealModules.cc
+++ SimCalorimetry/EcalTrigPrimProducers/plugins/SealModules.cc
@@ -5,7 +5,6 @@
 
 #include "EcalTrigPrimProducer.h"
 #include "EcalTrigPrimAnalyzer.h"
-#include "EcalTrigPrimAnalyzerMIPs.h"
 #include "EcalTPInputAnalyzer.h"
 #include "EcalTPCondAnalyzer.h"
 #include "EcalTrigPrimESProducer.h"
@@ -15,7 +14,6 @@
 DEFINE_FWK_MODULE(EcalTrigPrimProducer);
 DEFINE_FWK_MODULE(EcalTPInputAnalyzer);
 DEFINE_FWK_MODULE(EcalTrigPrimAnalyzer);
-DEFINE_FWK_MODULE(EcalTrigPrimAnalyzerMIPs);
 DEFINE_FWK_MODULE(EcalTPCondAnalyzer);
 DEFINE_FWK_EVENTSETUP_MODULE(EcalTrigPrimESProducer);
 DEFINE_FWK_EVENTSETUP_MODULE(EcalTrigPrimSpikeESProducer);
