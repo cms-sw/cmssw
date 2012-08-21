22f983d1f3de852dd49653175664a466498c99ec
Tristan Arnoldus Du Pree <sha1-a4437f5ff2816724e41eeb2d079b0bbdcee75cc6@cern.ch>
2012-08-21 18:48:11 +0000

    tried to implement to produce a single DQM file from multiple events. Problem: dont know how to specify multiple events as input for DQM producer

--- DQM/TrackerCommon/test/processMultipleEventsFromASingleRun.py
+++ DQM/TrackerCommon/test/processMultipleEventsFromASingleRun.py
@@ -17,7 +17,24 @@ for run in runs:
     print "dir2 = ", dir2
     events = glob.glob(dir2)
     print "in run ", run ,"... the events are: ", events
-    if len(events)>1 : print "MULTIPLE EVENTS IN ONE RUN... MERGE THEM!!!"
-    else             : print "DONT PANIC, NOTHING!"
-
-
+    if len(events)<2 : print "DONT PANIC, JUST ONE EVENT IN THIS RUN!"
+    files = ""
+    for event in events:
+        files+=" file:"+event+"/recoDQM_RAW2DIGI_RECO_DQM.root"
+    print "files = ", files    
+    if len(events)>1 : 
+        print "MULTIPLE EVENTS IN ONE RUN... MERGE THEM!!!"
+        newdir = run+"/MergedEvents"
+        f1 = subprocess.Popen("mkdir "+newdir, shell=True)
+        f1.wait()
+        
+        ### creating the configuration file to process DQM output file
+        command2 = "cmsDriver.py offlineDQM -s HARVESTING:dqmHarvesting --conditions auto:com10 --data --filein "+files+" --scenario pp  --no_exec --python_filename=offlineDQM.py --dirout="+newdir
+        f3 = subprocess.Popen(command2, shell=True)
+        f3.wait()
+        
+        ### produce DQM file
+        f4 = subprocess.Popen("cmsRun -e offlineDQM.py", shell=True)
+        f4.wait()
+        
+                                                    
