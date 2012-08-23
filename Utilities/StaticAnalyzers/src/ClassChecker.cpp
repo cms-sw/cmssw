921faf5336078e7667405f45287a5ca2aa1058d0
Patrick Gartung <sha1-33476957b06a1bc61e5124243258241c72d1e027@cern.ch>
2012-08-23 18:50:36 +0000

    Retry but using checkPostStmt with CXXMemberCallExpr

--- Utilities/StaticAnalyzers/src/ClassChecker.cpp
+++ Utilities/StaticAnalyzers/src/ClassChecker.cpp
@@ -1,49 +1,106 @@
+// ClassChecker.cpp by Patrick Gartung (gartung@fnal.gov)
+//
+// Objectives of this checker
+//
+// For each special function of a class (produce, beginrun, endrun, beginlumi, endlumi)
+//
+//	1) indentify member data being modified
+//		built-in types reseting values
+//		calling non-const member function object if member data is an object
+//	2) for each non-const member functions of self called
+//		do 1) above
+//	3) for each non member function (external) passed in a member object
+//		complain if arguement passed in by non-const ref
+//		pass by value OK
+//		pass by const ref & pointer OK
+//
+//
+
+
 #include "ClassChecker.h"
 
 namespace clangcms {
 
-void ClassChecker::checkASTDecl(const clang::CXXRecordDecl *D,
+void ClassCheckerDecl::checkASTDecl(const clang::CXXRecordDecl *D,
                     clang::ento::AnalysisManager &Mgr,
                     clang::ento::BugReporter &BR) const
 {
-	if ( D->isRecord() )
-	{
-	    clang::ento::PathDiagnosticLocation DLoc =
-	    clang::ento::PathDiagnosticLocation::createBegin(D, BR.getSourceManager());
+	return; /* Don't do anything since this is the wrong approach */
+	if ( D->hasDefinition() )
+	{	    
 
-	    if ( ! m_exception.reportClass( DLoc, BR ) )
+	    clang::CXXRecordDecl *CD = D->getDefinition();
+	    clang::ento::PathDiagnosticLocation PLoc =
+    	    clang::ento::PathDiagnosticLocation::createBegin(CD, BR.getSourceManager());
+
+	    if ( ! m_exception.reportGeneral( PLoc, BR ) )
 		return; 
-
 	    std::string buf;
 	    llvm::raw_string_ostream os(buf);
-	    os << "Declaration of Class  " << *D << " .";
-	    BR.EmitBasicReport(D, "Class Checker","ThreadSafety",os.str(), DLoc);
+	    os << "Declaration of Class  " << *CD << " .";
+//	    BR.EmitBasicReport(CD, "Class Method Checker","ThreadSafety",os.str(), DLoc);
 
   		for (clang::CXXRecordDecl::method_iterator
-         	I = D->method_begin(), E = D->method_end(); I != E; ++I)  {
-			if ( (*I).hasBody() ) {
-	    		std::string buf;
-	    		llvm::raw_string_ostream os(buf);
+         	I = CD->method_begin(), E = CD->method_end(); I != E; ++I)  {
+			if ( I->isThisDeclarationADefinition() ) {
+				std::string buf;
+	    			llvm::raw_string_ostream os(buf);
+      				os << "Declaration of Method  " << *I << " in Class "<< *CD<<".";
+//				BR.EmitBasicReport( CD, "Class Method Checker","ThreadSafety",os.str(), DLoc);
 
-      			os << "Declaration of Method  " << *I << " in Class "<<*D<<" .";
-			BR.EmitBasicReport( D, "Class Checker","ThreadSafety",os.str(), DLoc);
-    			}
-		}
+// 		This loop keeps causing EOF crashes in clang
+//
+//				clang::Stmt * S = I->getBody();
+//				for (clang::Stmt::const_child_iterator
+//					c = S->child_begin(), e=S->child_end();c !=e; ++c) {						
+//						if ( llvm::isa<clang::CallExpr>(*c) ) {
+//							const clang::CallExpr * ce = llvm::cast<clang::CallExpr>(*c);
+//							clang::ento::PathDiagnosticLocation DLoc =
+//   	    						clang::ento::PathDiagnosticLocation::createBegin
+//							(ce->getCalleeDecl(), BR.getSourceManager());
+// 							if ( ! m_exception.reportClass( DLoc, BR ) )
+//								return; 
+//     							std::string buf;
+//	    						llvm::raw_string_ostream os(buf);
+//							os << "Declaration of CallExpr in Method" << *CD << " .";
+//							BR.EmitBasicReport(D, "Class Checker","ThreadSafety",os.str(), DLoc);
+//						}
+//				} 
+
+    			} /* end declaration is definition */
+		} /* end method loop */
+
 
   		for (clang::CXXRecordDecl::field_iterator
          	I = D->field_begin(), E = D->field_end(); I != E; ++I)  {
 	    		std::string buf;
 	    		llvm::raw_string_ostream os(buf);
       			os << "Declaration of Field  " << *I << " in Class "<<*D<<" .";
-			BR.EmitBasicReport(D, "Class Checker","ThreadSafety",os.str(), DLoc);
-    			}
+//			BR.EmitBasicReport(D, "Class Field Checker","ThreadSafety",os.str(), DLoc);
+    			} 
 
-	
-  		return;
+	} /* has definition */
+} /* end class */
+
+
+
+void ClassCheckerCall::checkPostStmt(const clang::CXXMemberCallExpr *CE,
+		clang::ento::CheckerContext &C) const 
+{
+
+	if (clang::ento::ExplodedNode *errorNode = C.generateSink()) {
+		if (!BT)
+			BT.reset(new clang::ento::BugType("Class Call Checker", "ThreadSafety"));
+		clang::ento::BugReport *R = new clang::ento::BugReport(*BT, "CXXMemberCallExpression", errorNode);
+		R->addRange(CE->getSourceRange());
+	   	if ( ! m_exception.reportConstCast( *R, C ) )
+			return;
+		C.EmitReport(R);
 	}
 
 }
 
 
-}
+
+} /* end namespace */
 
--- Utilities/StaticAnalyzers/src/ClassChecker.h
+++ Utilities/StaticAnalyzers/src/ClassChecker.h
@@ -2,6 +2,7 @@
 #define Utilities_StaticAnalyzers_MemberChecker_h
 #include <clang/AST/DeclCXX.h>
 #include <clang/AST/StmtVisitor.h>
+#include <clang/AST/ExprCXX.h>
 #include <clang/StaticAnalyzer/Core/Checker.h>
 #include <clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h>
 #include <clang/StaticAnalyzer/Core/BugReporter/BugType.h>
@@ -9,16 +10,30 @@
 #include "CmsException.h"
 
 namespace clangcms {
-class ClassChecker : public clang::ento::Checker< clang::ento::check::ASTDecl< clang::CXXRecordDecl> > {
+class ClassCheckerDecl : public clang::ento::Checker< clang::ento::check::ASTDecl< clang::CXXRecordDecl> > {
   mutable clang::OwningPtr< clang::ento::BuiltinBug> BT;
 
 public:
   void checkASTDecl(const clang::CXXRecordDecl *D,
                       clang::ento::AnalysisManager &Mgr,
                       clang::ento::BugReporter &BR) const;
+
 private:
   CmsException m_exception;
 };  
-}
 
+
+class ClassCheckerCall : public clang::ento::Checker< clang::ento::check::PostStmt< clang::CXXMemberCallExpr> > {
+  mutable clang::OwningPtr<clang::ento::BugType> BT;
+
+public:
+  void checkPostStmt(const clang::CXXMemberCallExpr *CE,
+		clang::ento::CheckerContext &C) const;
+
+
+private:
+  CmsException m_exception;
+
+};
+}
 #endif
