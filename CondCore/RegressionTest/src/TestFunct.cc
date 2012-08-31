47ee375b03b0cb560edcae40f4ab5ad5825b8f57
Giacomo Govi <sha1-c6008bb91ca6bbc745928cb6f23a999d03f434c3@cern.ch>
2012-08-31 17:35:06 +0000

    fix cvs error

--- CondCore/RegressionTest/src/TestFunct.cc
+++ CondCore/RegressionTest/src/TestFunct.cc
@@ -2,9 +2,6 @@
 #include "CondCore/IOVService/interface/IOVEditor.h"
 #include "CondCore/IOVService/interface/IOVProxy.h"
 
-#include "RelationalAccess/ITableSchemaEditor.h"
-#include "CoralBase/Blob.h"
-
 //typedef TestPayloadClass Payload;
 typedef RegressionTestPayload Payload;
 
@@ -53,7 +50,7 @@ bool TestFunct::Read (std::string mappingName)
 		coral::ITable& mytable=s.nominalSchema().tableHandle("TEST_METADATA");
 		std::auto_ptr< coral::IQuery > query(mytable.newQuery());
 		coral::AttributeList BindVariableList;
-		std::string condition="NAME =: NAME";
+		std::string condition="NAME =:NAME";
 		BindVariableList.extend("NAME",typeid(std::string));
 		BindVariableList["NAME"].data<std::string>()=mappingName;
 		query->setCondition( condition, BindVariableList );
@@ -64,7 +61,7 @@ bool TestFunct::Read (std::string mappingName)
 			refSeed=row[ "SEED" ].data<int>();
 		}
 		std::string readToken = metadata.getToken(mappingName);
-		pool::Ref<Payload> readRef0 = s.getTypedObject<Payload>( readToken );  //v3	
+		boost::shared_ptr<Payload> readRef0 = s.getTypedObject<Payload>( readToken ); //v4	
 		std::cout << "Object with id="<<readToken<<" has been read"<<std::endl;
 		Payload tp = *readRef0;
 		Payload tp2(refSeed);
@@ -91,15 +88,14 @@ bool TestFunct::ReadWithIOV(std::string mappingName,
         bool ret = false;
  	try {
  		trans.start(true);
-
  		std::string iovToken = metadata.getToken(mappingName);
- 		cond::IOVProxy iov(s,iovToken, false, false );
+ 		cond::IOVProxy iov(s,iovToken);
  		cond::IOVProxy::const_iterator iPayload = iov.find( validity );
  		if( iPayload == iov.end() ){
  		  std::cout << "ERROR: no payload found in IOV for run="<<validity<<std::endl;
- 		  return 1;
+ 		  return false;
  		}
- 		pool::Ref<Payload> readRef0 = s.getTypedObject<Payload>( iPayload->token() ); //v4	
+ 		boost::shared_ptr<Payload> readRef0 = s.getTypedObject<Payload>( iPayload->token() ); //v4	
  		std::cout << "Object with id="<<iPayload->token()<<" has been read"<<std::endl;
  		Payload tp = *readRef0;
  		Payload tp2(seed);
@@ -139,59 +135,25 @@ bool TestFunct::ReadAll()
 	}
 	return ret;
 }
-
-/**
-namespace TestFuncImpl {
-  // workaround to make possible the streaming of std::bitset
-  static const char* BITSET_COLNAME = "DARRAYPA_M_BITSET";
-  pool::Ref<Payload> initWriting( cond::DbSession& s ){
-    cond::DbScopedTransaction trans(s);
-    pool::Ref<Payload> ret;
-    trans.start();
-    coral::ISchema& schema = s.nominalSchema();
-    if(!schema.existsTable("POOL_RSS_DB")){
-      ret = s.storeObject( new Payload(1), "cont1" );
-      trans.commit();
-      trans.start();
-      coral::ITableSchemaEditor& mainPayloadTableSchema = schema.tableHandle("REGRESSIONTESTPAYLOA").schemaEditor();
-      mainPayloadTableSchema.insertColumn(BITSET_COLNAME,coral::AttributeSpecification::typeNameForId( typeid(coral::Blob) ));
-      coral::ITableDataEditor& mappingTable = schema.tableHandle("POOL_OR_MAPPING_ELEMENTS").dataEditor();
-      coral::AttributeList inputData;
-      inputData.extend<std::string>( "ELEMENT_TYPE");
-      inputData.extend<std::string>( "COLUMN_NAME");
-      inputData.extend<std::string>( "VARIABLE_NAME");
-      inputData[ "ELEMENT_TYPE" ].data<std::string>() = "Blob";
-      inputData[ "COLUMN_NAME" ].data<std::string>() = BITSET_COLNAME;
-      inputData[ "VARIABLE_NAME" ].data<std::string>() = "ArrayPayload::m_bitset";
-      std::string setClause = "ELEMENT_TYPE =:ELEMENT_TYPE, COLUMN_NAME=:COLUMN_NAME";
-      std::string whereClause = "VARIABLE_NAME =:VARIABLE_NAME";
-      mappingTable.updateRows( setClause,whereClause, inputData  );
-    }
-    trans.commit();
-    return ret;
-  }
-}
-**/
-
 bool TestFunct::Write (std::string mappingName, int payloadID)
 {
-		cond::DbScopedTransaction trans(s);
-	   cond::MetaData  metadata(s);
-	   std::string tok0("");
+        cond::DbScopedTransaction trans(s);
+	cond::MetaData  metadata(s);
+	std::string tok0("");
 	try 
 	{
-	  //pool::Ref<Payload> dummy = TestFuncImpl::initWriting( s );
 	    trans.start();
-		coral::ITable& mytable=s.nominalSchema().tableHandle("TEST_METADATA");
-		coral::AttributeList rowBuffer;
-		coral::ITableDataEditor& dataEditor = mytable.dataEditor();
-		dataEditor.rowBuffer( rowBuffer );
-		rowBuffer["NAME"].data<std::string>()=mappingName;
-		rowBuffer["SEED"].data<int>()=payloadID;
-		rowBuffer["RUN"].data<int>()=-1;
-		dataEditor.insertRow( rowBuffer );	
-	        pool::Ref<Payload> myRef0 = s.storeObject(new Payload(payloadID), "cont1"); //v3
-		tok0 = myRef0.toString(); //v3
+	    coral::ITable& mytable=s.nominalSchema().tableHandle("TEST_METADATA");
+	    coral::AttributeList rowBuffer;
+	    coral::ITableDataEditor& dataEditor = mytable.dataEditor();
+	    dataEditor.rowBuffer( rowBuffer );
+	    rowBuffer["NAME"].data<std::string>()=mappingName;
+	    rowBuffer["SEED"].data<int>()=payloadID;
+	    rowBuffer["RUN"].data<int>()=-1;
+	    dataEditor.insertRow( rowBuffer );		
+	    s.createDatabase();
+	    boost::shared_ptr<Payload> myRef0(new Payload(payloadID)); //v4
+	    tok0 = s.storeObject( myRef0.get(),"cont1"); //v4
 	    metadata.addMapping(mappingName, tok0);
 	    std::cout << "Stored object with id = "<<tok0<<std::endl;
 	    trans.commit();
@@ -211,7 +173,6 @@ bool TestFunct::WriteWithIOV(std::string mappingName,
    cond::MetaData  metadata(s);
    std::string tok0("");
    try {
-     //pool::Ref<Payload> dummy = TestFuncImpl::initWriting( s );
      cond::IOVEditor iov(s);
      trans.start();
      if( updateTestMetadata ){
@@ -222,10 +183,11 @@ bool TestFunct::WriteWithIOV(std::string mappingName,
        rowBuffer["NAME"].data<std::string>()=mappingName;
        rowBuffer["SEED"].data<int>()=payloadID;
        rowBuffer["RUN"].data<int>()= runValidity;
-       dataEditor.insertRow( rowBuffer );
-     }		
-     pool::Ref<Payload> myRef0 = s.storeObject(new Payload(payloadID), "cont1"); //v3
-     std::string payloadTok = myRef0.toString(); 
+       dataEditor.insertRow( rowBuffer );		
+     }
+     s.createDatabase();
+     boost::shared_ptr<Payload> myRef0(new Payload(payloadID)); //v4
+     std::string payloadTok = s.storeObject( myRef0.get(),"cont1"); 
      iov.create( cond::runnumber );
      iov.append( runValidity, payloadTok );
      metadata.addMapping(mappingName, iov.token());
@@ -236,27 +198,26 @@ bool TestFunct::WriteWithIOV(std::string mappingName,
        return false;
      }
    return true;    
+   
 }
 
 bool TestFunct::CreateMetaTable ()
 {
 	cond::DbScopedTransaction trans(s);
-	cond::MetaDataSchemaUtility metaUt(s);
 	try
 	{
 		trans.start();
-		metaUt.create();
 		coral::ISchema& schema=s.nominalSchema();
 		coral::TableDescription description;
 		description.setName("TEST_METADATA");
 		description.insertColumn(  "NAME", coral::AttributeSpecification::typeNameForId( typeid(std::string)) );
 		description.insertColumn( "SEED", coral::AttributeSpecification::typeNameForId( typeid(int)) );
-		description.insertColumn( "RUN", coral::AttributeSpecification::typeNameForId( typeid(int)) );
+                description.insertColumn( "RUN", coral::AttributeSpecification::typeNameForId( typeid(int)) );
 		std::vector<std::string> cols;
 		cols.push_back( "NAME" );
 		description.setPrimaryKey(cols);
 		description.setNotNullConstraint("SEED");
-		description.setNotNullConstraint("RUN");
+                description.setNotNullConstraint("RUN");
 		coral::ITable& table=schema.createTable(description);
 		table.privilegeManager().grantToPublic( coral::ITablePrivilegeManager::Select);
 		std::cout<<"Table created"<<std::endl;
