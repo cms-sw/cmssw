58e0a02e167827a19fe1fa62aff241d5497bb1c1
Justin Pilot <sha1-9ec486b145bd592fce6262ad8bfba846cc93855e@cern.ch>
2012-09-03 18:09:13 +0000

    Adding interfaces to additional top tag algorithms

--- RecoJets/JetProducers/plugins/CATopJetProducer.h
+++ RecoJets/JetProducers/plugins/CATopJetProducer.h
@@ -63,6 +63,13 @@
 
 #include "RecoJets/JetProducers/plugins/FastjetJetProducer.h"
 #include "RecoJets/JetAlgorithms/interface/CATopJetAlgorithm.h"
+#include "CATopJetProducer.h"
+#include "CMSTopTagger.hh"
+#include "HEPTopTaggerWrapper.hh"
+
+#include <fastjet/tools/RestFrameNSubjettinessTagger.hh>
+#include "fastjet/SISConePlugin.hh"
+
 
 namespace cms
 {
@@ -79,7 +86,26 @@ namespace cms
     virtual void runAlgorithm( edm::Event& iEvent, const edm::EventSetup& iSetup );
 
   private:
-    CATopJetAlgorithm        alg_;         /// The algorithm to do the work
+    std::auto_ptr<CATopJetAlgorithm>        legacyCMSTopTagger_;         /// The algorithm to do the work
+    std::auto_ptr<fastjet::CMSTopTagger>     fjCMSTopTagger_;    // The FastJet implementation of the CMS tagger
+    std::auto_ptr<fastjet::HEPTopTagger>     fjHEPTopTagger_;
+    std::auto_ptr<fastjet::JHTopTagger>     fjJHUTopTagger_;
+    std::auto_ptr<fastjet::RestFrameNSubjettinessTagger>   fjNSUBTagger_;
+
+
+
+    int tagAlgo_;
+    double ptMin_;
+    double centralEtaCut_;
+    bool verbose_;
+    enum tagalgos {
+	CA_TOPTAGGER,
+	FJ_CMS_TOPTAG,
+	FJ_HEP_TOPTAG,
+	FJ_JHU_TOPTAG,
+	FJ_NSUB_TAG
+    };
+
 
   };
 
