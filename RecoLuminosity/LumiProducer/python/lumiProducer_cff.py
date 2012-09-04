52e84610471f367a9c05956cafbdfd11fa3357b4
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2012-09-04 14:48:35 +0000

    remove default lumiversion

--- RecoLuminosity/LumiProducer/python/lumiProducer_cff.py
+++ RecoLuminosity/LumiProducer/python/lumiProducer_cff.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
 LumiDBService=cms.Service('DBService')
 lumiProducer.connect='frontier://LumiProd/CMS_LUMI_PROD'
-lumiProducer.lumiversion='0001'
+lumiProducer.lumiversion=''
 
