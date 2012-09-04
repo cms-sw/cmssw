56381c97a373a9820b1cd2817b883d665bebb7bd
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-09-04 10:14:59 +0000

    add external counter

--- DataFormats/TrackReco/test/testHitPattern.cpp
+++ DataFormats/TrackReco/test/testHitPattern.cpp
@@ -9,7 +9,13 @@ int main() {
   int nhit;
   auto unpack =[&hit,&nhit](uint32_t pattern) {
     hit[nhit++]= 255&&(pattern>>3);
-  };
+    // buuble sort
+    if (nhit>1)
+    for (auto h=hit+nhit-1; h!=hit+1; --h) {
+      if ( (*(h-1)) <= (*h)) break;
+      std::swap(*(h-1),(*h));
+    }
+  }
 
   reco::HitPattern hp;
 
