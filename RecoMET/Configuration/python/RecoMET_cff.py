b57642a7590199d513d800a098bb0fc7b14d0386
Tai Sakuma <sha1-83aadde34f3854f55faaed54e7ebb52fa93fe00c@cern.ch>
2012-09-04 20:44:36 +0000

    deleted commented out lines. cleaned up comments

--- RecoMET/Configuration/python/RecoMET_cff.py
+++ RecoMET/Configuration/python/RecoMET_cff.py
@@ -1,23 +1,12 @@
 import FWCore.ParameterSet.Config as cms
+# $Id$
 
-# Name:   RecoMET.cff
-# Original Author: R.Cavanaugh
-# Date:   05.11.2006
-# Notes:  CaloMET.cfi assumes that a product with label "caloTowers" is
-#         already written into the event.
-# Modification by F. Ratnikov and R. Remington
-# Date: 10/21/08 
-# Addition of MET significance by F.Blekman
-# Date: 10/23/08
-# Addition of HCAL noise by JP Chou
-# Date:  3/26/09
-
+##____________________________________________________________________________||
 from RecoJets.Configuration.CaloTowersES_cfi import *
 from RecoMET.METProducers.CaloTowersOpt_cfi import *
 from RecoMET.METProducers.CaloMET_cfi import *
 from RecoMET.METProducers.HTMET_cfi import *
 from RecoMET.METProducers.CaloMETSignif_cfi import *
-#from RecoMET.METProducers.TCMET_cfi import *
 from RecoMET.METProducers.hcalnoiseinfoproducer_cfi import *
 from RecoMET.METProducers.MuonMETValueMapProducer_cff import *
 from RecoMET.METProducers.MuonTCMETValueMapProducer_cff import *
@@ -25,6 +14,7 @@ from RecoMET.METProducers.MetMuonCorrections_cff import *
 from RecoMET.Configuration.RecoMET_BeamHaloId_cff import *
 from RecoMET.Configuration.RecoTCMET_cff import *
 
+##____________________________________________________________________________||
 metreco = cms.Sequence(
         met+
         metNoHF+
@@ -36,8 +26,6 @@ metreco = cms.Sequence(
         calotoweroptmakerWithHO+
         metOptHO+
         metOptNoHFHO+
-#        htMetSC5+
-#        htMetSC7+
         htMetKT4+
         htMetKT6+
         htMetIC5+
@@ -50,7 +38,7 @@ metreco = cms.Sequence(
         BeamHaloId
         )
 
+##____________________________________________________________________________||
 metrecoPlusHCALNoise = cms.Sequence( metreco + hcalnoise )
 
-
-
+##____________________________________________________________________________||
