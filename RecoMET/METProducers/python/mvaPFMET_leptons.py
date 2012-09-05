507326d2d94e6027705299e41f025efcf3e18b06
Philip Coleman Harris <sha1-dd4ce72ed302de3d017f8bf6c5b43ad5d0f6400d@cern.ch>
2012-09-05 09:36:48 +0000

    fixed tau isolation

--- RecoMET/METProducers/python/mvaPFMET_leptons.py
+++ RecoMET/METProducers/python/mvaPFMET_leptons.py
@@ -43,22 +43,58 @@ isoelectrons = cms.EDFilter(
         filter = cms.bool(False)
         )
 
+from RecoJets.Configuration.RecoPFJets_cff import kt6PFJets as dummy
+kt6PFJetsForRhoComputationVoronoi = dummy.clone(
+        doRhoFastjet = True,
+            voronoiRfact = 0.9
+        )
+
+from RecoTauTag.RecoTau.PFRecoTauDiscriminationByHPSSelection_cfi import hpsSelectionDiscriminator
+hpsPFTauDiscriminationByDecayModeFinding = hpsSelectionDiscriminator.clone(
+        PFTauProducer = cms.InputTag('hpsPFTauProducer')
+            )
+
+from RecoTauTag.RecoTau.TauDiscriminatorTools import requireLeadTrack
+# Define decay mode prediscriminant
+requireDecayMode = cms.PSet(
+        BooleanOperator = cms.string("and"),
+            decayMode = cms.PSet(
+            Producer = cms.InputTag('hpsPFTauDiscriminationByDecayModeFinding'),
+                    cut = cms.double(0.5)
+                )
+        )
+
+hpsPFTauDiscriminationByMVAIsolation = cms.EDProducer(
+    "PFRecoTauDiscriminationByMVAIsolation",
+            PFTauProducer = cms.InputTag('hpsPFTauProducer'),
+            rhoProducer = cms.InputTag('kt6PFJetsForRhoComputationVoronoi','rho'),
+            Prediscriminants = requireDecayMode.clone(),
+            gbrfFilePath = cms.FileInPath('RecoTauTag/RecoTau/data/gbrfTauIso.root'),
+            returnMVA = cms.bool(False),
+            mvaMin = cms.double(0.7),
+            )
+
 isotaus = cms.EDFilter(
     "PFTauSelector",
     src = cms.InputTag('hpsPFTauProducer'),
+    BooleanOperator = cms.string("and"),
     discriminators = cms.VPSet(
     cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByDecayModeFinding"),       selectionCut=cms.double(0.5)),
-    cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByVLooseIsolation"),        selectionCut=cms.double(0.5)),
+    cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByMVAIsolation"),           selectionCut=cms.double(0.5)),
     cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByLooseElectronRejection"), selectionCut=cms.double(0.5)),
     cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByLooseMuonRejection"),     selectionCut=cms.double(0.5)) 
     ),
-    cut = cms.string("abs(eta) < 2.3 && pt > 19.0"),
+    cut = cms.string("abs(eta) < 2.3 && pt > 19.0 "),
     filter = cms.bool(False)
     )
 
 isomuonseq     = cms.Sequence(isomuons)
 isoelectronseq = cms.Sequence(isoelectrons)
-isotauseq      = cms.Sequence(isotaus)
+isotauseq      = cms.Sequence(
+     kt6PFJetsForRhoComputationVoronoi*
+     hpsPFTauDiscriminationByMVAIsolation*
+     isotaus
+    )
 
 leptonSelection = cms.PSet(
     SelectEvents = cms.untracked.PSet(
