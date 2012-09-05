3de6597e43f7dc3e2dbdc984cc3610755fbd4fb1
Loic Quertenmont <sha1-87ca56b44ac7447a9c679691ad6ed79ac661d179@cern.ch>
2012-09-05 07:36:53 +0000

    correct integrated lumi

--- SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Global.h
+++ SUSYBSMAnalysis/HSCP/test/ICHEP_Analysis/Analysis_Global.h
@@ -41,7 +41,7 @@ double               IntegratedLuminosityBeforeTriggerChange = 409.91;
 #else
 double               SQRTS          = 8;
 int                  RunningPeriods = 1;
-double               IntegratedLuminosity = 5105;
+double               IntegratedLuminosity = 8318;
 double               IntegratedLuminosityBeforeTriggerChange = 0;
 #endif
 
