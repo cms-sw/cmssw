d3607ce83b68afae8e7a3fa80a910cef4e65c8a9
Alja Mrak Tadel <sha1-a7060483da9297f8a5e32b46919e3b15dd1653ae@cern.ch>
2012-09-08 06:13:57 +0000

    Init pset member in PSetData constructor.

--- Fireworks/FWInterface/src/FWPSetTableManager.h
+++ Fireworks/FWInterface/src/FWPSetTableManager.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  
 //         Created:  Mon Feb 28 17:06:50 CET 2011
-// $Id: FWPSetTableManager.h,v 1.9 2012/04/06 19:51:38 amraktad Exp $
+// $Id: FWPSetTableManager.h,v 1.10 2012/09/08 04:47:56 matevz Exp $
 //
 
 // system include files
@@ -61,7 +61,7 @@ public:
          matches(false),
          childMatches(false),
 
-         editable(false) {}
+       editable(false) , pset(0){}
 
       std::string label;
       std::string value;
