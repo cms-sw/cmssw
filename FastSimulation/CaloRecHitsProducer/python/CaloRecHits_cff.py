00a8ce31672a363cb027ed5f92d02d657be4172b
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-09-11 15:45:49 +0000

    separate digi and local-reco sequences

--- FastSimulation/CaloRecHitsProducer/python/CaloRecHits_cff.py
+++ FastSimulation/CaloRecHitsProducer/python/CaloRecHits_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-CaloMode = 0
+CaloMode = 1
 # 0: custom local reco bypassing digis, ECAL and HCAL
 # 1: as 0, but full digi + std local reco in ECAL
 # 2: as 0, but full digi + std local reco in HCAL
@@ -18,19 +18,22 @@ if(CaloMode==1):
     from FastSimulation.CaloRecHitsProducer.EcalDigisPlusRecHits_cff import *
     from FastSimulation.CaloRecHitsProducer.HcalRecHitsCustom_cff import *
 
-    caloRecHits = cms.Sequence(ecalDigisPlusRecHitSequence*ecalPreshowerRecHit*hbhereco*horeco*hfreco)
+    caloDigis = cms.Sequence(ecalDigisSequence)
+    caloRecHits = cms.Sequence(ecalRecHitSequence*ecalPreshowerRecHit*hbhereco*horeco*hfreco)
     
 if(CaloMode==2):
 
     from FastSimulation.CaloRecHitsProducer.EcalRecHitsCustom_cff import *
     from FastSimulation.CaloRecHitsProducer.HcalDigisPlusRecHits_cff import *
 
-    caloRecHits = cms.Sequence(ecalRecHit*ecalPreshowerRecHit*hcalDigisPlusRecHitsSequence)
+    caloDigis = cms.Sequence(hcalDigisSequence)
+    caloRecHits = cms.Sequence(ecalRecHit*ecalPreshowerRecHit*hcalRecHitSequence)
 
 if(CaloMode==3):
 
     from FastSimulation.CaloRecHitsProducer.EcalDigisPlusRecHits_cff import *
     from FastSimulation.CaloRecHitsProducer.HcalDigisPlusRecHits_cff import *
 
-    caloRecHits = cms.Sequence(ecalDigisPlusRecHitSequence*ecalPreshowerRecHit*hcalDigisPlusRecHitsSequence)
+    caloDigis = cms.Sequence(ecalDigisSequence*hcalDigisSequence)
+    caloRecHits = cms.Sequence(ecalRecHitSequence*ecalPreshowerRecHit*hcalRecHitSequence)
 
