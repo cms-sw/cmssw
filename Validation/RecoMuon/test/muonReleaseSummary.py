1302d6cf34d72876523f91004a0554459d91cb11
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2012-09-11 09:44:32 +0000

    Changed again the name of the PU FastSim RelVal's in the GUI..

--- Validation/RecoMuon/test/muonReleaseSummary.py
+++ Validation/RecoMuon/test/muonReleaseSummary.py
@@ -5,8 +5,8 @@ import sys
 import fileinput
 import string
 
-NewRelease='CMSSW_6_0_0_pre1'
-RefRelease='CMSSW_5_2_2'
+NewRelease='CMSSW_6_1_0_pre1'
+RefRelease='CMSSW_6_0_0'
 #NewRelease='Summer09'
 #RefRelease='Summer09_pre1'
 
@@ -26,7 +26,6 @@ if (NewCondition=='MC'):
         samples= ['RelValSingleMuPt10','RelValSingleMuPt100','RelValTTbar']
 elif (NewCondition=='STARTUP'):
     samples= ['RelValSingleMuPt10','RelValSingleMuPt100','RelValSingleMuPt1000','RelValTTbar','RelValZMM','RelValJpsiMM']
-#    samples= ['RelValSingleMuPt10','RelValSingleMuPt100','RelValSingleMuPt1000','RelValTTbar', 'RelValJpsiMM']
     if (NewFastSim|RefFastSim):
         samples= ['RelValSingleMuPt10','RelValSingleMuPt100','RelValTTbar']
 elif (NewCondition=='PILEUP'):
@@ -49,18 +48,18 @@ GetRefsFrom='GUI'
 
 #DqmGuiNewRepository = 'https://cmsweb.cern.ch/dqm/dev/data/browse/Development/RelVal/CMSSW_4_2_x/'
 #DqmGuiNewRepository = 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/RelVal/CMSSW_4_3_x/'
-DqmGuiNewRepository = 'https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/RelVal/CMSSW_6_0_x/'
+DqmGuiNewRepository = 'https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/RelVal/CMSSW_6_1_x/'
 #DqmGuiRefRepository = 'https://cmsweb.cern.ch/dqm/dev/data/browse/Development/RelVal/CMSSW_4_2_x/'
 #DqmGuiRefRepository = 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/RelVal/CMSSW_4_3_x/'
-DqmGuiRefRepository = 'https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/RelVal/CMSSW_5_2_x/'
+DqmGuiRefRepository = 'https://cmsweb.cern.ch/dqm/relval/data/browse/ROOT/RelVal/CMSSW_6_0_x/'
 CastorRepository = '/castor/cern.ch/user/a/aperrott/ValidationRecoMuon'
 
 # These are only needed if you copy any root file from the DQM GUI:
-NewLabel='START52_V4'
+NewLabel='START60_V4'
 if (NewCondition=='MC'):
     NewLabel='MC_52_V1'
 #RefLabel='START50_V13'
-RefLabel='START52_V4'
+RefLabel='START60_V4'
 if (RefCondition=='MC'):
     RefLabel='MC_52_V1'
 
@@ -109,14 +108,16 @@ if (NewCondition=='PILEUP'):
     if (NewFastSim):
 #        NewLabel=NewLabel+'_PU_FlatDist10_2011EarlyData_50ns'
 #        NewLabel='PU_'+NewLabel+'_PU_FlatDist10_2011EarlyData_50ns'
-        NewLabel='PU_'+NewLabel+'_PU_2012_Startup_inTimeOnly'
+#        NewLabel='PU_'+NewLabel+'_PU_2012_Startup_inTimeOnly'
+        NewLabel='PU_'+NewLabel
     else:
         NewLabel='PU_'+NewLabel   # What about sticking on some kind of default ? 
 #        NewLabel=NewLabel+'_PU'
 if (RefCondition=='PILEUP'):
     if (RefFastSim):
 #        RefLabel=RefLabel+'_PU_FlatDist10_2011EarlyData_50ns'
-        RefLabel='PU_'+RefLabel+'_PU_2012_Startup_inTimeOnly'
+#        RefLabel='PU_'+RefLabel+'_PU_2012_Startup_inTimeOnly'
+        RefLabel='PU_'+RefLabel
     else:
         RefLabel='PU_'+RefLabel
 #        RefLabel=RefLabel+'_PU'
