47ae4a1a7d6db485902790365dd336f716d64202
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-09-17 17:26:36 +0000

    MVA MET code + config files moved to JetMETCorrections/METPUSubtraction

--- RecoMET/METProducers/src/SealModule.cc
+++ RecoMET/METProducers/src/SealModule.cc
@@ -9,9 +9,6 @@
 #include "RecoMET/METProducers/interface/EcalHaloDataProducer.h" 
 #include "RecoMET/METProducers/interface/GlobalHaloDataProducer.h" 
 #include "RecoMET/METProducers/interface/PFCandidatesForTrackMETProducer.h" 
-#include "RecoMET/METProducers/interface/PFMETProducerMVA.h" 
-#include "RecoMET/METProducers/interface/PFMETProducerMVA2.h" 
-#include "RecoMET/METProducers/interface/PFMETProducerMVAData.h" 
 
 using cms::METProducer;
 using reco::BeamHaloSummaryProducer;
@@ -20,9 +17,6 @@ using reco::HcalHaloDataProducer;
 using reco::EcalHaloDataProducer;
 using reco::GlobalHaloDataProducer;
 using reco::PFCandidatesForTrackMETProducer;
-using reco::PFMETProducerMVA;
-using reco::PFMETProducerMVA2;
-using reco::PFMETProducerMVAData;
 
 DEFINE_FWK_MODULE(METProducer);
 DEFINE_FWK_MODULE(BeamHaloSummaryProducer);
@@ -31,10 +25,6 @@ DEFINE_FWK_MODULE(HcalHaloDataProducer);
 DEFINE_FWK_MODULE(EcalHaloDataProducer);
 DEFINE_FWK_MODULE(GlobalHaloDataProducer);
 DEFINE_FWK_MODULE(PFCandidatesForTrackMETProducer);
-DEFINE_FWK_MODULE(PFMETProducerMVA);
-DEFINE_FWK_MODULE(PFMETProducerMVA2);
-DEFINE_FWK_MODULE(PFMETProducerMVAData);
-
 
 #include "RecoMET/METProducers/interface/MuonMET.h"
 using cms::MuonMET;
