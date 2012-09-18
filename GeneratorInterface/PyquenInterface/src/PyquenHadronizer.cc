61f63583bf54502466fd55117d2994d6be40cf79
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2012-09-18 17:19:35 +0000

    option to have p on one side

--- GeneratorInterface/PyquenInterface/src/PyquenHadronizer.cc
+++ GeneratorInterface/PyquenInterface/src/PyquenHadronizer.cc
@@ -2,7 +2,7 @@
  *
  * Generates PYQUEN HepMC events
  *
- * $Id: PyquenHadronizer.cc,v 1.12 2011/02/17 20:53:49 yarba Exp $
+ * $Id: PyquenHadronizer.cc,v 1.13 2011/03/02 10:35:17 yilmaz Exp $
 */
 
 #include <iostream>
@@ -46,6 +46,7 @@ doquench_(pset.getParameter<bool>("doQuench")),
 doradiativeenloss_(pset.getParameter<bool>("doRadiativeEnLoss")),
 docollisionalenloss_(pset.getParameter<bool>("doCollisionalEnLoss")),
 doIsospin_(pset.getParameter<bool>("doIsospin")),
+   protonSide_(pset.getUntrackedParameter<int>("protonSide",0)),
 embedding_(pset.getParameter<bool>("embeddingMode")),
 evtPlane_(0),
 nquarkflavor_(pset.getParameter<int>("qgpNumQuarkFlavor")),
@@ -140,7 +141,13 @@ bool PyquenHadronizer::generatePartonsAndHadronize()
    // generate single partonic PYTHIA jet event
    
    // Take into account whether it's a nn or pp or pn interaction
-   if(doIsospin_) call_pyinit("CMS", nucleon(), nucleon(), comenergy);
+   if(doIsospin_){
+     string projN = "p";
+     string targN = "p";
+     if(protonSide_ == 2) projN = nucleon();
+     if(protonSide_ == 1) targN = nucleon();
+     call_pyinit("CMS", projN.data(), targN.data(), comenergy);
+   }
    call_pyevnt();
    
    // call PYQUEN to apply parton rescattering and energy loss 
