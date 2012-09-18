660b6549cf748be0cffcac15cc7af3e3e6286baf
Philip Coleman Harris <sha1-dd4ce72ed302de3d017f8bf6c5b43ad5d0f6400d@cern.ch>
2012-09-18 01:22:34 +0000

    Adding Pileup Jet Id

---
+++ RecoJets/JetProducers/src/PileupJetIdentifier.cc
@@ -0,0 +1,26 @@
+#include "RecoJets/JetProducers/interface/PileupJetIdentifier.h"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+
+// ------------------------------------------------------------------------------------------
+StoredPileupJetIdentifier::StoredPileupJetIdentifier() 
+{
+}
+
+// ------------------------------------------------------------------------------------------
+StoredPileupJetIdentifier::~StoredPileupJetIdentifier() 
+{
+}
+
+// ------------------------------------------------------------------------------------------
+PileupJetIdentifier::PileupJetIdentifier() 
+{
+}
+
+// ------------------------------------------------------------------------------------------
+PileupJetIdentifier::~PileupJetIdentifier() 
+{
+}
