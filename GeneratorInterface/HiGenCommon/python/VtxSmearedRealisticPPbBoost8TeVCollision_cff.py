9880cca6cfed2c17fc7e86e651c62e32432efd1d
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2012-09-18 08:54:45 +0000

    Vesrion modified according to L. Xu instructions

--- GeneratorInterface/HiGenCommon/python/VtxSmearedRealisticPPbBoost8TeVCollision_cff.py
+++ GeneratorInterface/HiGenCommon/python/VtxSmearedRealisticPPbBoost8TeVCollision_cff.py
@@ -1,16 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
-VtxSmeared = cms.EDProducer("BetafuncEvtVtxGenerator",
-    VtxSmearedCommon,
+Realistic5TeVCollisionPPbBoostVtxSmearingParameters= cms.PSet(
     Phi = cms.double(0.0),
-    BetaStar = cms.double(150.0),
-    Emittance = cms.double(0.90e-07),
+    BetaStar = cms.double(1100.0),
+    Emittance = cms.double(0.5e-07),
     Alpha = cms.double(0.0),
-    SigmaZ = cms.double(6.26),
+    SigmaZ = cms.double(6.889),
     TimeOffset = cms.double(0.0),
-    X0 = cms.double(0.244),
-    Y0 = cms.double(0.393),
-    Z0 = cms.double(0.41),
-    Beta=cms.double(0.434)
+    X0 = cms.double(0.080989),
+    Y0 = cms.double(0.0693616),
+    Z0 = cms.double(-0.259745),
+    Beta=cms.double(-0.434)
 )
