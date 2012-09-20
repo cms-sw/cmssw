ae8062f370eb62e9bbd4f9123ba45f9c7231ed90
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2012-09-20 15:30:14 +0000

    Delete section of dead and old code that could never be executed because it is after a return statement. I suspect the entire class and the things that use it could be deleted ... The deleted code contained a getByType call and this is part of the effort to remove getByType from CMSSW.

--- DQM/CastorMonitor/src/CastorMonitorSelector.cc
+++ DQM/CastorMonitor/src/CastorMonitorSelector.cc
@@ -21,29 +21,4 @@ void CastorMonitorSelector::processEvent(const edm::Event& e){
   m_eventMask = m_eventMask|DO_CASTOR_PED_CALIBMON; 
 
   return;
-
-  edm::Handle<HcalTBTriggerData> triggerD; ////-- where is CastorTBTriggerData ????
-  e.getByType(triggerD);
-  if (!triggerD.isValid()) {
-    m_runNum=-1; 
-    ////---- If there is no trigger data, just activate everything
-    //m_eventMask = m_eventMask|DO_CASTOR_PED_CALIBMON|DO_CASTOR_LED_CALIBMON|DO_CASTOR_LASER_CALIBMON;
-    m_eventMask = m_eventMask|DO_CASTOR_PED_CALIBMON;
-    return; 
-  }
-  const HcalTBTriggerData trigger = *triggerD;
-  
-  m_runNum = trigger.runNumber();
-
-  ////--- check trigger contents
-  //if (trigger.wasBeamTrigger())             { m_triggerMask |= 0x01; m_eventMask = m_eventMask|CASTOR_BEAM_TRIGGER;}
-  if (trigger.wasOutSpillPedestalTrigger()) { m_triggerMask |= 0x02; m_eventMask = m_eventMask|DO_CASTOR_PED_CALIBMON;}
-  if (trigger.wasInSpillPedestalTrigger())  { m_triggerMask |= 0x04; m_eventMask = m_eventMask|DO_CASTOR_PED_CALIBMON;}
-  // if (trigger.wasLEDTrigger())              { m_triggerMask |= 0x08; m_eventMask = m_eventMask|DO_CASTOR_LED_CALIBMON;}
-  // if (trigger.wasLaserTrigger())            { m_triggerMask |= 0x10; m_eventMask = m_eventMask|DO_CASTOR_LASER_CALIBMON;}
-
-  if(m_eventMask&DO_CASTOR_PED_CALIBMON) m_eventMask = m_eventMask^DO_CASTOR_RECHITMON;
-
-  return;
-
 }
