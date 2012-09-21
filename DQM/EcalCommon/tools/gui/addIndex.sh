40877d1b4c07889391ce488a11c97a0970c0c75f
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2012-09-21 09:59:46 +0000

    bugfix

--- DQM/EcalCommon/tools/gui/addIndex.sh
+++ DQM/EcalCommon/tools/gui/addIndex.sh
@@ -8,7 +8,7 @@ fi
 guidir=/data/ecalod-disk01/dqm-gui
 datadir=/data/ecalod-disk01/dqm-data
 
-ix=$guidir/state/dqmgui/ecal/ix
+ix=$guidir/state/dqmgui/online/ix
 tmp=$datadir/tmp/closed
 dest=$datadir/root
 
@@ -18,6 +18,5 @@ cd $guidir
 
 for file in $(ls --color=never $tmp/DQM* 2> /dev/null); do
     echo "Adding index for "$(echo $file | sed 's|^.*/\([^\/]*\)$|\1|')
-    visDQMIndex add $ix $file
-    mv $file $dest/
+    visDQMIndex add $ix $file && mv $file $dest/
 done
