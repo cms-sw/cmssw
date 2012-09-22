4d6a0b58533bfcd6780df73f1c20ba8e52689b81
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-09-22 08:09:45 +0000

    use new diff method in topology

--- Geometry/TrackerGeometryBuilder/interface/ProxyStripTopology.h
+++ Geometry/TrackerGeometryBuilder/interface/ProxyStripTopology.h
@@ -23,8 +23,8 @@
 ///
 ///  \author    : Andreas Mussgiller
 ///  date       : November 2010
-///  $Revision: 1.4 $
-///  $Date: 2012/04/26 17:28:55 $
+///  $Revision: 1.5 $
+///  $Date: 2012/09/19 15:47:48 $
 ///  (last update by $Author: innocent $)
 
 #include "DataFormats/GeometryCommonDetAlgo/interface/DeepCopyPointerByClone.h"
@@ -73,6 +73,10 @@ public:
   virtual float strip( const LocalPoint& lp) const { return specificTopology().strip(lp);}
   /// conversion taking also the track state (LocalTrajectoryParameters)
   virtual float strip( const LocalPoint& lp, const Topology::LocalTrackAngles &dir ) const;
+
+  virtual float coveredStrips(const LocalPoint& lp1, const LocalPoint& lp2)  const {
+    return specificTopology().coveredStrips(lp1,lp2);
+  }
 
   virtual float pitch() const { return specificTopology().pitch(); }
   virtual float localPitch( const LocalPoint& lp) const { return specificTopology().localPitch(lp);}
