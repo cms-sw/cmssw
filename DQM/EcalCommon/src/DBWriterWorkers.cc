6f2d92697812f5687092c98987980bffcb2c86e1
Yutaro Iiyama <sha1-b63dc01a642d380a16da36b4e834787802efdeb4@cern.ch>
2012-09-24 22:16:05 +0000

    bugfix

--- DQM/EcalCommon/src/DBWriterWorkers.cc
+++ DQM/EcalCommon/src/DBWriterWorkers.cc
@@ -1114,6 +1114,8 @@ namespace ecaldqm {
             samples[i] += shapeME->getBinContent(id, i + 1);
         }
 
+	if(nChannels == 0) continue;
+
         for(int i(0); i < 10; ++i)
           samples[i] /= nChannels;
 
