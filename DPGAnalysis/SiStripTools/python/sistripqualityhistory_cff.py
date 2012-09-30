47fe9eb5eca7864702bb6a6f6f1f714c54d5bd96
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-09-30 13:48:20 +0000

    updated cfg fragment to monitor bad components, added cfg and crab cfg to produce the results

--- DPGAnalysis/SiStripTools/python/sistripqualityhistory_cff.py
+++ DPGAnalysis/SiStripTools/python/sistripqualityhistory_cff.py
@@ -23,14 +23,25 @@ ssqbadch.ListOfRecordToMerge=cms.VPSet(
  cms.PSet(record=cms.string('SiStripBadChannelRcd'),tag=cms.string(''))
 ) 
 
+ssqdcs = CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi.siStripQualityESProducer.clone()
+ssqdcs.appendToDataLabel = cms.string("dcsBadModules")
+ssqdcs.ListOfRecordToMerge=cms.VPSet(
+ cms.PSet(record=cms.string('SiStripDetCablingRcd'),tag=cms.string('')),
+ cms.PSet(record=cms.string('RunInfoRcd'),tag=cms.string('')),
+ cms.PSet(record=cms.string('SiStripBadChannelRcd'),tag=cms.string('')),
+ cms.PSet(record=cms.string('SiStripDetVOffRcd'),tag=cms.string(''))
+)
+
 ssqbadfiber = CalibTracker.SiStripESProducers.SiStripQualityESProducer_cfi.siStripQualityESProducer.clone()
 ssqbadfiber.appendToDataLabel = cms.string("BadFiber")
 ssqbadfiber.ListOfRecordToMerge=cms.VPSet(
  cms.PSet(record=cms.string('SiStripDetCablingRcd'),tag=cms.string('')),
  cms.PSet(record=cms.string('RunInfoRcd'),tag=cms.string('')),
  cms.PSet(record=cms.string('SiStripBadChannelRcd'),tag=cms.string('')),
+ cms.PSet(record=cms.string('SiStripDetVOffRcd'),tag=cms.string('')),
  cms.PSet(record=cms.string('SiStripBadFiberRcd'),tag=cms.string(''))
 )
+
 
 
 from DPGAnalysis.SiStripTools.sistripqualityhistory_cfi import *
@@ -39,5 +50,6 @@ ssqhistory.monitoredSiStripQuality = cms.VPSet(
     cms.PSet( name = cms.string("Cabling"), ssqLabel = cms.string("onlyCabling")),
     cms.PSet( name = cms.string("RunInfo"), ssqLabel = cms.string("CablingRunInfo")),
     cms.PSet( name = cms.string("BadChannel"), ssqLabel = cms.string("BadChannel")),
+    cms.PSet( name = cms.string("DCS"), ssqLabel = cms.string("dcsBadModules")),
     cms.PSet( name = cms.string("BadFiber"), ssqLabel = cms.string("BadFiber"))
    )
