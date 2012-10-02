5b637899c40d864e770598235d72dd208fa1eb9c
Abideh Jafari <sha1-b1dd6ed6fa47c29d651e4802a069539a71beee87@cern.ch>
2012-10-02 16:32:53 +0000

    instead of badly-named modules

--- HLTriggerOffline/Top/python/topHLTDQM_cff.py
+++ HLTriggerOffline/Top/python/topHLTDQM_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from HLTriggerOffline.Top.topDiLeptonOfflineDQM_cfi import *
+from HLTriggerOffline.Top.topDiLeptonHLTEventDQM_cfi import *
 from HLTriggerOffline.Top.topSingleLeptonHLTEventDQM_cfi import *
 from HLTriggerOffline.Top.singletopHLTEventDQM_cfi import *
 from HLTriggerOffline.Top.topvalidation_cfi import *
