94487b7f275e7aea738cd49cad45a09e4745b48b
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-02 15:25:20 +0000

    cfg for test

---
+++ TrackPropagation/RungeKutta/test/rkTest_cfg.py
@@ -0,0 +1,50 @@
+#
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MAGNETICFIELDTEST")
+
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# Example configuration for the magnetic field
+
+# Uncomment ONE of the following:
+
+### Uniform field
+#process.load("Configuration.StandardSequences.MagneticField_0T_cff")
+#process.localUniform.ZFieldInTesla = 3.8
+
+
+### Full field map, static configuration for each field value
+#process.load("Configuration.StandardSequences.MagneticField_20T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_30T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_35T_cff")
+
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+
+
+process.MessageLogger = cms.Service("MessageLogger",
+    categories   = cms.untracked.vstring("MagneticField"),
+    destinations = cms.untracked.vstring("cout"),
+    cout = cms.untracked.PSet(  
+    noLineBreaks = cms.untracked.bool(True),
+    threshold = cms.untracked.string("WARNING"),
+    WARNING = cms.untracked.PSet(
+      limit = cms.untracked.int32(0)
+    ),
+    MagneticField = cms.untracked.PSet(
+     limit = cms.untracked.int32(10000000)
+    )
+  )
+)
+
+process.testField  = cms.EDAnalyzer("RKTest")
+process.p1 = cms.Path(process.testField)
+
