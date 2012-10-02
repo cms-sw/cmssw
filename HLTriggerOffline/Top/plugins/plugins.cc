d3ce4aed060c544b0bd71ae271d85bbf1b0466f6
Abideh Jafari <sha1-b1dd6ed6fa47c29d651e4802a069539a71beee87@cern.ch>
2012-10-02 16:04:21 +0000

    instead of badly-named modules

--- HLTriggerOffline/Top/plugins/plugins.cc
+++ HLTriggerOffline/Top/plugins/plugins.cc
@@ -1,11 +1,11 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "HLTriggerOffline/Top/interface/TopValidation.h"
-#include "DQM/Physics/src/TopDiLeptonOfflineDQM.h"
-#include "DQM/Physics/src/TopSingleLeptonDQM.h"
+#include "HLTriggerOffline/Top/src/TopHLTDiLeptonOfflineDQM.h"
+#include "HLTriggerOffline/Top/src/TopHLTSingleLeptonDQM.h"
 #include "HLTriggerOffline/Top/interface/HLTEfficiencyCalculator.h"
 
 DEFINE_FWK_MODULE(TopValidation);
-//DEFINE_FWK_MODULE(TopDiLeptonOfflineDQM);
-//DEFINE_FWK_MODULE(TopSingleLeptonDQM);
+DEFINE_FWK_MODULE(TopHLTDiLeptonOfflineDQM);
+DEFINE_FWK_MODULE(TopHLTSingleLeptonDQM);
 DEFINE_FWK_MODULE(HLTEffCalculator);
