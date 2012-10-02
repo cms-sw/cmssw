c3d0f0a45c3c19176efb48dc8487e204bdf9759f
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2012-10-02 19:06:37 +0000

    Running JME-recommended selection on primary vertices for PF CHS

--- CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
+++ CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
@@ -1,10 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
-
-goodOfflinePrimaryVertices = cms.EDFilter(
-    "PrimaryVertexObjectFilter",
-    filterParams = pvSelector.clone( minNdof = cms.double(4.0), maxZ = cms.double(24.0) ),
-    src=cms.InputTag('offlinePrimaryVertices')
-    )
+goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
+   src = cms.InputTag("offlinePrimaryVertices"),
+   cut = cms.string("!isFake && ndof >= 4.0 && abs(z) <= 24.0 && abs(position.Rho) <= 2.0"),
+   filter = cms.bool(True)
+)
 
