3e1f9b21023545a8fca7ab9b84aef1f0367d07b7
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2012-10-03 15:29:23 +0000

    update 53X

--- python/electronRegressionEnergyProducer_cfi.py
+++ python/electronRegressionEnergyProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 eleRegressionEnergy = cms.EDProducer("RegressionEnergyPatElectronProducer",
                                    debug = cms.untracked.bool(False),
-                                   inputPatElectronsTag = cms.InputTag('selectedPatElectrons'),
+                                   inputPatElectronsTag = cms.InputTag('cleanPatElectrons'),
                                    regressionInputFile = cms.string("EGamma/EGammaAnalysisTools/data/eleEnergyRegWeights_V1.root"),
                                    energyRegressionType = cms.uint32(1),
                                    rhoCollection = cms.InputTag('kt6PFJets:rho'),
