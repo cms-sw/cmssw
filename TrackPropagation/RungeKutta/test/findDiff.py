4504acf39325f2f3ffebe936acf21f9640816fd8
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-03 11:29:03 +0000

    a script to parse test output

--- TrackPropagation/RungeKutta/test/findDiff.py
+++ TrackPropagation/RungeKutta/test/findDiff.py
@@ -6,9 +6,13 @@ if len(sys.argv) !=3 :
 mm=0.
 n=0
 ave=0
+tot=0
+ln=0
+ml=0
 file1 = open(sys.argv[1])
 file2 = open(sys.argv[2])
 for l1 in file1.readlines() :
+    ln+=1
     l2 = file2.readline()
     a1 =l1.split('(')
     a2 = l2.split('(')
@@ -21,12 +25,23 @@ for l1 in file1.readlines() :
     f1 = [float(i) for i in p1]
     f2 = [float(i) for i in p2]
     diff = []
+    tot+=1
     for i in range(len(f1)):
         diff.append(f1[i]-f2[i])
     for x in diff:
-        mm = max(mm,abs(x))
+        if (abs(x)>mm):
+            mm=x
+            ml=ln
         if (x!=0) :
             n+=1
             ave+=abs(x)
-print mm, n, ave/n
-               
+print tot, mm, n, ave/n
+#
+ml = ml-5
+file1.seek(0)
+file2.seek(0)
+l1 = file1.readlines()
+l2 = file2.readlines()
+for i in range(10) :
+    print l1[ml+i].rstrip('\n')
+    print l2[ml+i].rstrip('\n')
