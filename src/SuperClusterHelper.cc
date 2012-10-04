bc016579c96af21420eba9d2e13cfa8390644abd
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2012-10-04 07:13:19 +0000

    bug fix

--- src/SuperClusterHelper.cc
+++ src/SuperClusterHelper.cc
@@ -17,7 +17,16 @@ SuperClusterHelper::SuperClusterHelper(const reco::GsfElectron * electron, const
 SuperClusterHelper::SuperClusterHelper(const pat::Electron * electron, const EcalRecHitCollection * rechits, const CaloTopology * topo, const CaloGeometry * geom) {
   theElectron_  = (const reco::GsfElectron*)electron;
   rechits_ = rechits ;
+//  for(unsigned ir=0; ir<rechits_->size();++ir) {
+//    std::cout << "RecHit " << (*rechits_)[ir].id().rawId() << " " << (*rechits_)[ir] << std::endl;
+//  }
   seedCluster_ = & (*(electron->seed()));
+//  std::vector< std::pair<DetId, float> >::const_iterator it=seedCluster_->hitsAndFractions().begin();
+//  std::vector< std::pair<DetId, float> >::const_iterator itend=seedCluster_->hitsAndFractions().end();
+//  for( ; it!=itend ; ++it) {
+//    DetId id=it->first;
+//    std::cout << " Basic cluster " << id.rawId() << std::endl;
+//  }
   topology_ = topo;
   geometry_ = geom;
   barrel_ = electron->isEB();
@@ -34,6 +43,7 @@ void SuperClusterHelper::computeLocalCovariances() {
     if (!isnan(vCov_[2])) spp_ = sqrt (vCov_[2]);
     
     if (theElectron_->sigmaIetaIeta()*spp_ > 0) {
+      std::cout << "Computing sep "<< vCov_[1] << std::endl;
       sep_ = vCov_[1] / theElectron_->sigmaIetaIeta()*spp_;
     } else if (vCov_[1] > 0) {
       sep_ = 1.0;
@@ -50,11 +60,17 @@ float SuperClusterHelper::spp() {
 
 float SuperClusterHelper::sep() {
   computeLocalCovariances();
+  std::cout << " SEP " << sep_ << std::endl;
   return sep_;
 }
 
 void SuperClusterHelper::localCoordinates() {
+  if (localCoordinatesComputed_) return;
+
   if (barrel_) {
     local_.localCoordsEB(*seedCluster_, *geometry_, etaCrySeed_ , phiCrySeed_ ,ietaSeed_ , iphiSeed_ , thetaTilt_ , phiTilt_);
+  } else {
+    local_.localCoordsEE(*seedCluster_, *geometry_, etaCrySeed_ , phiCrySeed_ ,ietaSeed_ , iphiSeed_ , thetaTilt_ , phiTilt_);
   }
+    localCoordinatesComputed_ = true;
 }
