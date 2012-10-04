c7c90d41ef03b2c075a8e75dce69d35f25cbe4d3
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2012-10-04 07:27:05 +0000

    bug fix

--- interface/SuperClusterHelper.h
+++ interface/SuperClusterHelper.h
@@ -6,7 +6,6 @@
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "DataFormats/EgammaReco/interface/BasicCluster.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
-#include "RecoEcal/EgammaCoreTools/interface/EcalClusterTools.h"
 #include "RecoEgamma/EgammaTools/interface/EcalClusterLocal.h"
 #include "Geometry/CaloTopology/interface/CaloSubdetectorTopology.h"
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
@@ -28,8 +27,8 @@ class SuperClusterHelper {
   float sigmaIetaIeta() const {return theElectron_->sigmaIetaIeta();}
   float preshowerEnergy() const {return theElectron_->superCluster()->preshowerEnergy();}
   float preshowerEnergyOverRaw() const {return theElectron_->superCluster()->preshowerEnergy()/theElectron_->superCluster()->rawEnergy();}
-  float e3x3()      const {return EcalClusterTools::e3x3(*seedCluster_,rechits_,topology_); }
-  float e5x5()      const {return EcalClusterTools::e5x5(*seedCluster_,rechits_,topology_); }  
+  float e3x3()      const { return EcalClusterTools::e3x3(*seedCluster_,rechits_,topology_); }
+  float e5x5()      const { return EcalClusterTools::e5x5(*seedCluster_,rechits_,topology_); }  
   float eMax()      const { return EcalClusterTools::eMax(*seedCluster_,rechits_); }
   float e2nd()      const { return EcalClusterTools::e2nd(*seedCluster_,rechits_); }
   float eTop()      const { return EcalClusterTools::eTop(*seedCluster_,rechits_,topology_); }
@@ -41,16 +40,19 @@ class SuperClusterHelper {
   float e2x5Bottom()const { return EcalClusterTools::e2x5Bottom(*seedCluster_,rechits_,topology_); }
   float e2x5Left()  const { return EcalClusterTools::e2x5Left(*seedCluster_,rechits_,topology_); }
   float e2x5Right() const { return EcalClusterTools::e2x5Right(*seedCluster_,rechits_,topology_); } 
-  float r9()        const { return e3x3()/theElectron_->superCluster()->rawEnergy();}
+  float r9()        const { //std::cout << " E3x3 " << e3x3() << " raw "  << theElectron_->superCluster()->rawEnergy() << std::endl;
+ return e3x3()/theElectron_->superCluster()->rawEnergy();}
   float spp();
   float sep();
   float seedEta()   const { return seedCluster_->eta(); }
   float seedPhi()   const { return seedCluster_->phi(); }
   float seedEnergy()const { return seedCluster_->energy();}
-  int ietaSeed() const { return ietaSeed_;}
-  int iphiSeed() const { return iphiSeed_;}
-  float etaCrySeed() const { return etaCrySeed_;}
-  float phiCrySeed() const { return phiCrySeed_;} 
+  int ietaSeed()  { localCoordinates(); return ietaSeed_;}
+  int iphiSeed()  { localCoordinates(); return iphiSeed_;}
+  float etaCrySeed()  { localCoordinates(); return etaCrySeed_;}
+  float phiCrySeed()  { localCoordinates(); return phiCrySeed_;} 
+  float thetaTilt() { localCoordinates(); return thetaTilt_;}
+  float phiTilt() {localCoordinates(); return phiTilt_;}
 
  private:
   const reco::GsfElectron * theElectron_;
@@ -69,8 +71,8 @@ class SuperClusterHelper {
   float sep_;
   /// local coordinates
   bool localCoordinatesComputed_;
-  int ietaSeed_;
-  int iphiSeed_;
+  int ietaSeed_; // ix in the endcaps
+  int iphiSeed_; // iy in the endcaps
   float etaCrySeed_;
   float phiCrySeed_;
   float thetaTilt_;
