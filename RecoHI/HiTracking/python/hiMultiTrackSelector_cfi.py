5d8101df8e8202f5b7c7f82fa0be76e14bef5e35
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2012-10-08 12:14:11 +0000

    Add new parameters to the track selectors, use default values

--- RecoHI/HiTracking/python/hiMultiTrackSelector_cfi.py
+++ RecoHI/HiTracking/python/hiMultiTrackSelector_cfi.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 hiLooseMTS = cms.PSet(
     preFilterName=cms.string(''),
     name= cms.string('hiTrkLoose'),
-    
+
    # vertex selection
     vtxNumber = cms.int32(-1),
     vertexCut = cms.string(''),
@@ -12,8 +12,8 @@ hiLooseMTS = cms.PSet(
     #untracked bool copyTrajectories = true // when doing retracking before
     copyTrajectories = cms.untracked.bool(True),
     copyExtras = cms.untracked.bool(True), ## set to false on AOD
-    qualityBit = cms.string('loose'), ## set to '' or comment out if you dont want to set the 
-    
+    qualityBit = cms.string('loose'), ## set to '' or comment out if you dont want to set the
+
     chi2n_par = cms.double(9999),                     # version with 1D hits modification
     chi2n_no1Dmod_par = cms.double(0.3),                     # normalizedChi2 < nLayers * chi2n_par
     res_par = cms.vdouble(99999., 99999.),            # residual parameterization (re-check in HI)
@@ -23,22 +23,26 @@ hiLooseMTS = cms.PSet(
     dz_par2 = cms.vdouble(8.0, 0.0),
     # Boolean indicating if adapted primary vertex compatibility cuts are to be applied.
     applyAdaptedPVCuts = cms.bool(True),
-    
+
     # Impact parameter absolute cuts.
     max_z0 = cms.double(1000),
     max_d0 = cms.double(1000),
     nSigmaZ = cms.double(9999.),
 
-   # Cuts on numbers of layers with hits/3D hits/lost hits. 
+   # Cuts on numbers of layers with hits/3D hits/lost hits.
     minNumberLayers = cms.uint32(0),
     minNumber3DLayers = cms.uint32(0),
     maxNumberLostLayers = cms.uint32(999),
     minHitsToBypassChecks = cms.uint32(20),
+    max_minMissHitOutOrIn = cms.int32(99),
+    max_lostHitFraction = cms.double(1.0),
+    min_eta = cms.double(-9999.),
+    max_eta = cms.double(9999.) ,
 
     # Absolute cuts in case of no PV. If yes, please define also max_d0NoPV and max_z0NoPV
     applyAbsCutsIfNoPV = cms.bool(False),
     keepAllTracks= cms.bool(False),
-    
+
     # parameters for cutting on pterror/pt and number of valid hits
     max_relpterr = cms.double(0.1),
     min_nhits = cms.uint32(10)
@@ -47,10 +51,10 @@ hiLooseMTS = cms.PSet(
 hiTightMTS=hiLooseMTS.clone(
     preFilterName='hiTrkLoose',
     min_nhits = cms.uint32(12),
-    max_relpterr = cms.double(0.075),    
+    max_relpterr = cms.double(0.075),
     d0_par2 = cms.vdouble(5.0, 0.0),
     dz_par2 = cms.vdouble(5.0, 0.0),
-    chi2n_no1Dmod_par = cms.double(0.25),    
+    chi2n_no1Dmod_par = cms.double(0.25),
     name= cms.string('hiTrkTight'),
     qualityBit = cms.string('tight'), ## set to '' or comment out if you dont want to set the bit
     keepAllTracks= cms.bool(True)
@@ -60,10 +64,10 @@ hiHighpurityMTS= hiTightMTS.clone(
     name= cms.string('hiTrkHighPurity'),
     preFilterName='hiTrkTight',
     min_nhits = cms.uint32(13),
-    max_relpterr = cms.double(0.05),    
+    max_relpterr = cms.double(0.05),
     d0_par2 = [3.0, 0.0],
     dz_par2 = [3.0, 0.0],
-    chi2n_no1Dmod_par = cms.double(0.15),    
+    chi2n_no1Dmod_par = cms.double(0.15),
     qualityBit = cms.string('highPurity') ## set to '' or comment out if you dont want to set the bit
     )
 
