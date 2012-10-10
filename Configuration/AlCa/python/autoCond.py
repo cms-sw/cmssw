e359f2e0cb62681de46e0244593ebedb01075705
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-10-10 11:10:56 +0000

    use L1 scenarios

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -90,116 +90,67 @@ conditions_L1_HIRun2011 = (
 
 
 # dedicated GlobalTags for MC production with the frozen HLT menus
-autoCond['startup_5E33v4'] = ( autoCond['startup'], ) + (
-                            # L1 menu 2012 v0
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # old L1 CSCTF configuration
-                            'L1MuCSCPtLut_key-10_mc,L1MuCSCPtLutRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used up to Run2012B
-                            'L1MuDTTFParameters_dttf11_TSC_09_17_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # old L1 GCT configuration, without jet seed threshold
-                            'L1GctJetFinderParams_GCTPhysics_2011_09_01_B_mc,L1GctJetFinderParamsRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1HfRingEtScale_GCTPhysics_2011_09_01_B_mc,L1HfRingEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1HtMissScale_GCTPhysics_2011_09_01_B_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1JetEtScale_GCTPhysics_2011_09_01_B_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['startup_7E33v2'] = ( autoCond['startup'], ) + (
-                            # L1 menu 2012 v1
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used up to Run2012B
-                            'L1MuDTTFParameters_dttf11_TSC_09_17_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # old L1 GCT configuration, without jet seed threshold
-                            'L1GctJetFinderParams_GCTPhysics_2011_09_01_B_mc,L1GctJetFinderParamsRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1HfRingEtScale_GCTPhysics_2011_09_01_B_mc,L1HfRingEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1HtMissScale_GCTPhysics_2011_09_01_B_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            'L1JetEtScale_GCTPhysics_2011_09_01_B_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['startup_7E33v3'] = ( autoCond['startup'],
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used since Run2012C
-                            'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['startup_7E33v4'] = ( autoCond['startup'], ) + (
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used since Run2012C
-                            'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['startup_8E33v1'] = ( autoCond['startup'], ) + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used since Run2012C
-                            'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['startup_GRun'] = ( autoCond['startup'], ) + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                            # L1 DTTF settings used since Run2012C
-                            'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['starthi_HIon'] = ( autoCond['starthi'], ) + (
-                            # L1 heavy ions menu 2011 v0
-                            'L1GtTriggerMenu_L1Menu_CollisionsHeavyIons2011_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
+autoCond['startup_5E33v4']   = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012A
+
+autoCond['startup_7E33v2']   = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012B
+
+autoCond['startup_7E33v3']   = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012C
+
+autoCond['startup_7E33v4']   = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012C
+
+autoCond['startup_8E33v1']   = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012D
+
+autoCond['startup_GRun']     = ( autoCond['startup'], ) \
+                             + conditions_L1_Run2012D
+
+autoCond['starthi_HIon']     = ( autoCond['starthi'], ) \
+                             + conditions_L1_HIRun2011
 
 # dedicated GlobalTags for running the frozen HLT menus on data
-autoCond['hltonline_5E33v4'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v0
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_7E33v2'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v1
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_7E33v3'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_7E33v4'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_8E33v1'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_GRun'] = autoCond['hltonline'] + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['hltonline_HIon'] = autoCond['hltonline'] + (
-                            # L1 heavy ions menu 2011 v0
-                            'L1GtTriggerMenu_L1Menu_CollisionsHeavyIons2011_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
+autoCond['hltonline_5E33v4'] = autoCond['hltonline'] \
+                             + conditions_L1_Run2012A
+
+autoCond['hltonline_7E33v2'] = autoCond['hltonline'] \
+                             + conditions_L1_Run2012B
+
+autoCond['hltonline_7E33v3'] = autoCond['hltonline'] \
+                             + conditions_L1_Run2012C
+
+autoCond['hltonline_7E33v4'] = autoCond['hltonline'] \
+                             + conditions_L1_Run2012C
+
+autoCond['hltonline_8E33v1'] = autoCond['hltonline'] \
+                             + conditions_L1_Run2012D
+
+autoCond['hltonline_GRun']   = autoCond['hltonline'] \
+                             + conditions_L1_Run2012D
+
+autoCond['hltonline_HIon']   = autoCond['hltonline'] \
+                             + conditions_L1_HIRun2011
 
 # dedicated GlobalTags for running RECO and the frozen HLT menus on data
-autoCond['com10_5E33v4'] = autoCond['com10'] + (
-                            # L1 menu 2012 v0
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_7E33v2'] = autoCond['com10'] + (
-                            # L1 menu 2012 v1
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_7E33v3'] = autoCond['com10'] + (
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_7E33v4'] = autoCond['com10'] + (
-                            # L1 menu 2012 v2
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_8E33v1'] = autoCond['com10'] + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_GRun'] = autoCond['com10'] + (
-                            # L1 menu 2012 v3
-                            'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
-autoCond['com10_HIon'] = autoCond['com10'] + (
-                            # L1 heavy ions menu 2011 v0
-                            'L1GtTriggerMenu_L1Menu_CollisionsHeavyIons2011_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-                          )
+autoCond['com10_5E33v4']     = autoCond['com10'] \
+                             + conditions_L1_Run2012A
 
+autoCond['com10_7E33v2']     = autoCond['com10'] \
+                             + conditions_L1_Run2012B
+
+autoCond['com10_7E33v3']     = autoCond['com10'] \
+                             + conditions_L1_Run2012C
+
+autoCond['com10_7E33v4']     = autoCond['com10'] \
+                             + conditions_L1_Run2012C
+
+autoCond['com10_8E33v1']     = autoCond['com10'] \
+                             + conditions_L1_Run2012D
+
+autoCond['com10_GRun']       = autoCond['com10'] \
+                             + conditions_L1_Run2012D
+
+autoCond['com10_HIon']       = autoCond['com10'] \
+                             + conditions_L1_HIRun2011
