4a6e72b6e8637ae2b85bfa38f215c9061925c531
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2012-10-10 10:55:15 +0000

    *** empty log message ***

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -21,7 +21,7 @@ autoCond = {
 }
 
 # dedicated GlobalTags for MC production with the frozen HLT menus
-autoCond['startup_5E33v4'] = ( autoCond['startup'],
+autoCond['startup_5E33v4'] = ( autoCond['startup'], ) + (
                             # L1 menu 2012 v0
                             'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             # old L1 CSCTF configuration
@@ -34,7 +34,7 @@ autoCond['startup_5E33v4'] = ( autoCond['startup'],
                             'L1HtMissScale_GCTPhysics_2011_09_01_B_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             'L1JetEtScale_GCTPhysics_2011_09_01_B_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
-autoCond['startup_7E33v2'] = ( autoCond['startup'],
+autoCond['startup_7E33v2'] = ( autoCond['startup'], ) + (
                             # L1 menu 2012 v1
                             'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             # L1 DTTF settings used up to Run2012B
@@ -51,25 +51,25 @@ autoCond['startup_7E33v3'] = ( autoCond['startup'],
                             # L1 DTTF settings used since Run2012C
                             'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
-autoCond['startup_7E33v4'] = ( autoCond['startup'],
+autoCond['startup_7E33v4'] = ( autoCond['startup'], ) + (
                             # L1 menu 2012 v2
                             'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             # L1 DTTF settings used since Run2012C
                             'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
-autoCond['startup_8E33v1'] = ( autoCond['startup'],
+autoCond['startup_8E33v1'] = ( autoCond['startup'], ) + (
                             # L1 menu 2012 v3
                             'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             # L1 DTTF settings used since Run2012C
                             'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
-autoCond['startup_GRun'] = ( autoCond['startup'],
+autoCond['startup_GRun'] = ( autoCond['startup'], ) + (
                             # L1 menu 2012 v3
                             'L1GtTriggerMenu_L1Menu_Collisions2012_v3_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                             # L1 DTTF settings used since Run2012C
                             'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
-autoCond['starthi_HIon'] = ( autoCond['starthi'],
+autoCond['starthi_HIon'] = ( autoCond['starthi'], ) + (
                             # L1 heavy ions menu 2011 v0
                             'L1GtTriggerMenu_L1Menu_CollisionsHeavyIons2011_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
                           )
