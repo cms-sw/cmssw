0b5dbf7e631005b8972039704533d6edf73c5301
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-10-12 05:40:47 +0000

    Regernate subtables after version bump

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_5_2_6/PIon/V43 (CMSSW_5_2_7_HLT2)
+# /dev/CMSSW_5_2_6/PIon/V44 (CMSSW_5_2_7_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_2_6/PIon/V43')
+  tableName = cms.string('/dev/CMSSW_5_2_6/PIon/V44')
 )
 
 streams = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_5_2_6/PIon/V43 (CMSSW_5_2_7_HLT2)
+# /dev/CMSSW_5_2_6/PIon/V44 (CMSSW_5_2_7_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_2_6/PIon/V43')
+  tableName = cms.string('/dev/CMSSW_5_2_6/PIon/V44')
 )
 
 process.streams = cms.PSet( 
