da613fb96202a3e0f49d6ec06af66e9a5b771198
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-10-13 16:00:16 +0000

    use common Type-0 correction parameters for reco::PFMET and pat::PFMET

--- PhysicsTools/PatUtils/python/patPFMETCorrections_cff.py
+++ PhysicsTools/PatUtils/python/patPFMETCorrections_cff.py
@@ -51,15 +51,7 @@ patPFJetMETtype2Corr = patPFJetMETtype1p2Corr.clone(
 #--------------------------------------------------------------------------------
 # produce Type 0 MET corrections
 from JetMETCorrections.Type1MET.pfMETCorrectionType0_cfi import *
-patPFMETtype0Corr = pfMETcorrType0.clone(
-    correction = cms.PSet(
-        formula = cms.string("-([0] + [1]*x)*(1.0 + TMath::Erf(-[2]*TMath::Power(x, [3])))"),
-        par0 = cms.double(0.),
-        par1 = cms.double(-0.710135),
-        par2 = cms.double(0.0870503),
-        par3 = cms.double(0.621243)          
-    )
-)
+patPFMETtype0Corr = pfMETcorrType0.clone()
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
