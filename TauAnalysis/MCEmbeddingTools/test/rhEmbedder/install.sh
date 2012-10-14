ff4b2cfa0eef150c12e0b65c25e77d92dafbd7a8
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-10-14 12:31:35 +0000

    improved tabbing

--- TauAnalysis/MCEmbeddingTools/test/rhEmbedder/install.sh
+++ TauAnalysis/MCEmbeddingTools/test/rhEmbedder/install.sh
@@ -3,13 +3,12 @@
 wd=`pwd`
 targetDir=$CMSSW_BASE/src/
 
-
 cp DetAssoc.patch $targetDir
-cp MuonRecoPatch.patch  $targetDir
+cp MuonRecoPatch.patch $targetDir
 
 cd $targetDir
 addpkg RecoMuon/GlobalTrackFinder
-patch -p0 -i  MuonRecoPatch.patch
+patch -p0 -i MuonRecoPatch.patch
 
 addpkg TrackingTools/TrackAssociator
 patch -p0 -i DetAssoc.patch
