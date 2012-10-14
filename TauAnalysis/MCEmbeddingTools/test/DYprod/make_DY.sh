d0e023276fa4d96dd7434aad98ad586cb2c1293b
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-10-14 12:30:41 +0000

    - switch to 8 TeV - added L1Reco sequence needed for mixing of L1Extra objects

--- TauAnalysis/MCEmbeddingTools/test/DYprod/make_DY.sh
+++ TauAnalysis/MCEmbeddingTools/test/DYprod/make_DY.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#CONDITIONS=FrontierConditions_GlobalTag,START42_V13::All
-#CONDITIONS=FrontierConditions_GlobalTag,START52_V5::All
 CONDITIONS=FrontierConditions_GlobalTag,START53_V7A::All
 
 # mix_E7TeV_Ave25_50ns_PoissonOOTPU_cfi.py -> E7TeV_Ave25_50ns_PoissonOOTPU
@@ -9,36 +7,8 @@ CONDITIONS=FrontierConditions_GlobalTag,START53_V7A::All
 #PU=mix_E7TeV_Fall2011ReDigi_prelim_50ns_PoissonOOT_cfi
 PU=NoPileUp
 
-cd $CMSSW_BASE/src/
-
-f1=Configuration/GenProduction/python/DYToMuMu_M_20_TuneZ2_7TeV_pythia6_cff.py
-if [ ! -f $f1 ]; then
-  cvs co -r 1.3 $f1
-fi
-
-f2=Configuration/GenProduction/python/DYToTauTau_M_20_TuneZ2_7TeV_pythia6_tauola_cff.py
-if [ ! -f $f2 ]; then
-  cvs co -r 1.1 $f2
-fi
-scramv1 b
-
-cd -
-
-
-#
-# cmsDriver.py  Configuration/GenProduction/python/DYToMuMu_M_20_TuneZ2_7TeV_pythia6_cff \
-#       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO \
-#       --no_exec \
-#       --pileup=$PU \
-#       --conditions=${CONDITIONS} \
-#       --fileout=ZmumuTF.root  \
-#       --python_filename=DYmumuTF.py \
-#       -n 10
-
-#exit
-
 cmsDriver.py  TauAnalysis/MCEmbeddingTools/python/DYToMuMu_TMF \
-       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO \
+       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,L1Reco,RECO \
        --no_exec \
        --pileup=$PU \
        --conditions=${CONDITIONS} \
@@ -49,7 +19,7 @@ cmsDriver.py  TauAnalysis/MCEmbeddingTools/python/DYToMuMu_TMF \
 exit
 
 cmsDriver.py  TauAnalysis/MCEmbeddingTools/python/DYToTauTau_TMF \
-       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO \
+       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,L1Reco,RECO \
        --no_exec \
        --pileup=$PU \
        --conditions=${CONDITIONS} \
