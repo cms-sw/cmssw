828923cc02af34ae62e96739dc38a128408cf56c
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-10-14 12:29:31 +0000

    added L1Reco sequence (needed for mixing of L1 trigger directives)

--- TauAnalysis/MCEmbeddingTools/test/pfEmbedderFullSim/make_config_all.sh
+++ TauAnalysis/MCEmbeddingTools/test/pfEmbedderFullSim/make_config_all.sh
@@ -4,14 +4,14 @@
 #CONDITIONS=FrontierConditions_GlobalTag,START38_V12::All
 #CONDITIONS=FrontierConditions_GlobalTag,START311_V2::All
 #CONDITIONS=FrontierConditions_GlobalTag,START42_V13::All
-CONDITIONS=FrontierConditions_GlobalTag,START52_V5::All
+CONDITIONS=FrontierConditions_GlobalTag,START53_V7A::All
 
 cmsDriver.py TauAnalysis/MCEmbeddingTools/python/PFEmbeddingSource_cff \
-       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT:GRun,RAW2DIGI,RECO \
+       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT:GRun,RAW2DIGI,L1Reco,RECO \
        --no_exec \
        --conditions=${CONDITIONS} \
        --fileout=embedded.root  \
-       --python_filename=embed.py \
+       --python_filename=embed2.py \
        --customise=TauAnalysis/MCEmbeddingTools/pf_01_customizeAll.py \
        -n 10
 
--- TauAnalysis/MCEmbeddingTools/test/rhEmbedder/make_config.sh
+++ TauAnalysis/MCEmbeddingTools/test/rhEmbedder/make_config.sh
@@ -7,7 +7,7 @@
 CONDITIONS=FrontierConditions_GlobalTag,START53_V7A::All
 
 cmsDriver.py TauAnalysis/MCEmbeddingTools/python/PFEmbeddingSource_cff \
-       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT:GRun,RAW2DIGI,RECO \
+       -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT:GRun,RAW2DIGI,L1Reco,RECO \
        --no_exec \
        --conditions=${CONDITIONS} \
        --fileout=embedded.root  \
