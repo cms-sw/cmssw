5f26e8ddf985c770a0810fb643297a2e2447d091
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-14 10:11:43 +0000

    remove umbigous templates

--- RecoTauTag/TauTagTools/interface/Ellipse.h
+++ RecoTauTag/TauTagTools/interface/Ellipse.h
@@ -5,21 +5,11 @@
 
 namespace reco{
   
-  inline double deltaEta2(double eta1, double eta2){
-    double deltaEta = eta1 - eta2;
-    return deltaEta*deltaEta;
-  }
-  
-  template <typename T1, typename T2>
-  inline double deltaEta2(const T1& t1, const T2& t2){
-    return deltaEta2(t1.eta(), t2.eta());
-  }
-  
   template <typename T1, typename T2>
   inline double ellipse (const T1& t1, const T2& t2, double rPhi, double rEta){
-    double dEta2 = deltaEta2(t1.eta(), t2.eta());
+    double dEta = t1.eta()-t2.eta();
     double dPhi = deltaPhi(t1.phi(), t2.phi());
-    double distance = dEta2/(rEta*rEta) + (dPhi*dPhi)/(rPhi*rPhi);
+    double distance = (dEta*dEta)/(rEta*rEta) + (dPhi*dPhi)/(rPhi*rPhi);
     return distance;
   }
 
