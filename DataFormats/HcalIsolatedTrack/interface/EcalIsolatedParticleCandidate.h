f9251302f77be4bdb35c7c179d92292123d717be
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-14 09:03:13 +0000

    fixed sort of

--- DataFormats/HcalIsolatedTrack/interface/EcalIsolatedParticleCandidate.h
+++ DataFormats/HcalIsolatedTrack/interface/EcalIsolatedParticleCandidate.h
@@ -19,50 +19,42 @@ namespace reco {
     // default constructor
     EcalIsolatedParticleCandidate() : LeafCandidate() { }
       // constructor from a tau jet
-      EcalIsolatedParticleCandidate(const l1extra::L1JetParticleRef& l1tau, double etatau, double phitau,  double enIn, double enOut, int nhitIn, int nhitOut): 
-	LeafCandidate( 0, LorentzVector() ),
-	l1tau_(l1tau), eta_(etatau),phi_(phitau),enIn_(enIn), enOut_(enOut), nhitIn_(nhitIn), nhitOut_(nhitOut){}
-	
-	//constructor with null candidate
-	EcalIsolatedParticleCandidate(double etatau, double phitau,  double enIn, double enOut, int nhitIn, int nhitOut):
-	  LeafCandidate( 0, LorentzVector() ), eta_(etatau),phi_(phitau),enIn_(enIn), enOut_(enOut), nhitIn_(nhitIn), nhitOut_(nhitOut) {} 
-	  /// destructor
-	virtual ~EcalIsolatedParticleCandidate();
-	/// returns a clone of the candidate
-	virtual EcalIsolatedParticleCandidate * clone() const;
-	
-	/// reference to a tau jet
-	virtual l1extra::L1JetParticleRef l1TauJet() const;
-	
-	/// tau jet eta
-	double eta() const {return eta_; }
-	
-	/// tau jet phi
-	double phi() const {return phi_; }
+    EcalIsolatedParticleCandidate(const l1extra::L1JetParticleRef& l1tau, double etatau, double phitau,  double enIn, double enOut, int nhitIn, int nhitOut): 
+      LeafCandidate( 0, PtEtaPhiMass(0.,etatau, phitau, 0.) )
+      ,l1tau_(l1tau), enIn_(enIn), enOut_(enOut), nhitIn_(nhitIn), nhitOut_(nhitOut){}
+    
+    //constructor with null candidate
+    EcalIsolatedParticleCandidate(double etatau, double phitau,  double enIn, double enOut, int nhitIn, int nhitOut):
+      LeafCandidate( 0, PtEtaPhiMass(0.,etatau, phitau, 0.) )
+      ,enIn_(enIn), enOut_(enOut), nhitIn_(nhitIn), nhitOut_(nhitOut) {} 
 
-        /// ECAL energy in the inner cone around tau jet
-	double energyIn() const {return enIn_; }
-	
-	/// ECAL energy in the outer cone around tau jet
-	double energyOut() const {return enOut_;}
-
-	/// number of ECAL hits in the inner cone around tau jet
-	int nHitIn() const {return nhitIn_;}
-
-	/// number of ECAL hits in the outer cone around tau jet
-	int nHitOut() const {return nhitOut_;}
-	
-	/// set reference to l1 tau jet
-	void setL1TauJet( const l1extra::L1JetParticleRef & l1tau ) { l1tau_ = l1tau; }
-	
-
+    /// destructor
+    virtual ~EcalIsolatedParticleCandidate();
+    /// returns a clone of the candidate
+    virtual EcalIsolatedParticleCandidate * clone() const;
+    
+    /// reference to a tau jet
+    virtual l1extra::L1JetParticleRef l1TauJet() const;
+    
+    /// ECAL energy in the inner cone around tau jet
+    double energyIn() const {return enIn_; }
+    
+    /// ECAL energy in the outer cone around tau jet
+    double energyOut() const {return enOut_;}
+    
+    /// number of ECAL hits in the inner cone around tau jet
+    int nHitIn() const {return nhitIn_;}
+    
+    /// number of ECAL hits in the outer cone around tau jet
+    int nHitOut() const {return nhitOut_;}
+    
+    /// set reference to l1 tau jet
+    void setL1TauJet( const l1extra::L1JetParticleRef & l1tau ) { l1tau_ = l1tau; }
+    
+    
   private:
     /// reference to a L1 tau jet
     l1extra::L1JetParticleRef l1tau_;
-    /// eta of L1 tau jet
-    double eta_;
-    /// phi of L1 tau jet
-    double phi_;
     /// energy in inner cone around L1 tau jet
     double enIn_;
     /// energy in outer cone around L1 tau jet
