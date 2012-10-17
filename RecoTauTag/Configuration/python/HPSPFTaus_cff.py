8726f9a5ce9dfb52ad97460cad2e7da7865f5d48
Pavel Jez <sha1-5412db20736c393637675ccc5f0b9ea7f3a21499@cern.ch>
2012-10-17 22:42:01 +0000

    introduce strip Pt cut for leading candidate and remove matching cone cut

--- RecoTauTag/Configuration/python/HPSPFTaus_cff.py
+++ RecoTauTag/Configuration/python/HPSPFTaus_cff.py
@@ -536,8 +536,18 @@ hpsPFTauProducerSansRefs = cms.EDProducer(
     cleaners = cms.VPSet(
         # Prefer taus that dont' have charge == 3
         cleaners.unitCharge,
+         # Ignore taus reconstructed in pi0 decay modes in which the highest Pt ("leading") pi0 has pt below 2.5 GeV
+         # (in order to make decay mode reconstruction less sensitive to pile-up)
+         # NOTE: strips are sorted by decreasing pt
+        cms.PSet(
+            name = cms.string("leadStripPtLt2_5"),
+            plugin = cms.string("RecoTauStringCleanerPlugin"),
+            selection = cms.string("signalPiZeroCandidates().size() = 0 | signalPiZeroCandidates()[0].pt > 2.5"),
+            selectionPassFunction = cms.string("0"),
+            selectionFailValue = cms.double(1e3)
+            ),
         # Prefer taus that are within DR<0.1 of the jet axis
-        cleaners.matchingConeCut,
+#        cleaners.matchingConeCut,
         # Prefer taus that pass HPS selections
         cms.PSet(
             name = cms.string("HPS_Select"),
