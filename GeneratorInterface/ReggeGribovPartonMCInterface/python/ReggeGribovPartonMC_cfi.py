b456fbba595972e34cc5564a7e43e699415e2d27
Colin Baus <sha1-295d49880a4056d88a747bd11fbf9bda9cb756dd@cern.ch>
2012-10-18 22:28:22 +0000

    added config file

---
+++ GeneratorInterface/ReggeGribovPartonMCInterface/python/ReggeGribovPartonMC_cfi.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+process.generator = cms.EDFilter("ReggeGribovPartonMCGeneratorFilter",
+                    beammomentum = cms.double(4000),
+                    targetmomentum = cms.double(-4000),
+                    beamid = cms.int32(1),
+                    targetid = cms.int32(1),
+                    model = cms.int32(0),
+                    paramFileName = cms.string("Configuration/Generator/data/ReggeGribovPartonMC.param")
+                    )
+
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /local/reps/CMSSW/CMSSW/GeneratorInterface/ReggeGribovPartonMCInterface/python/ReggeGribovPartonMC_cfi.py,v $'),
+    annotation = cms.untracked.string('ReggeGribovMC generator')
+    )
+
+
+
+
+
