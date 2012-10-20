0bf21a4e7a755be570dfe2df8cda2acb5f03a042
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-20 16:35:23 +0000

    fix long standing typo

--- FWCore/Utilities/interface/Visibility.h
+++ FWCore/Utilities/interface/Visibility.h
@@ -4,7 +4,7 @@
 
 #if GCC_PREREQUISITE(4,4,0)
 #define dso_export    __attribute__ ((visibility ("default")))
-#define dso_hidden    __attribute__ ((visibility ("hidden") )
+#define dso_hidden    __attribute__ ((visibility ("hidden")))
 #define dso_internal  __attribute__ ((visibility ("hidden")))
 #define dso_protected __attribute__ ((visibility ("protected")))
 #else
