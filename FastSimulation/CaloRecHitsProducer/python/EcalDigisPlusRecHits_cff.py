26f22af5490c58c662cb14f1e8322afaf407c64f
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2012-10-22 10:50:13 +0000

    cleaning up unnecessary lines

--- FastSimulation/CaloRecHitsProducer/python/EcalDigisPlusRecHits_cff.py
+++ FastSimulation/CaloRecHitsProducer/python/EcalDigisPlusRecHits_cff.py
@@ -1,29 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-#To include the ECAL RecHit containment corrections (the famous 0.97 factor)
-#from SimCalorimetry.EcalSimProducers.ecalNotContainmentSim_cff import *
-
-#from SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff import *
-#from SimCalorimetry.HcalSimProducers.hcalSimParameters_cfi import *
-
-# This includes is needed for the ECAL digis
-from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
-
-# unsuppressed digis simulation - fast preshower
-from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
-
 # ECAL Trigger Primitives (needed by SRP)
 from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_cff import *
 # Selective Readout Processor producer
-#import SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi 
-#ecalDigis = SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi.simEcalDigis.clone()
 from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
 
 # RCT (Regional Calorimeter Trigger) emulator import 
 import L1Trigger.RegionalCaloTrigger.rctDigis_cfi
-## simRctDigisECAL = L1Trigger.RegionalCaloTrigger.rctDigis_cfi.rctDigis.clone()  
-## simRctDigisECAL.ecalDigis = cms.VInputTag( cms.InputTag( 'simEcalTriggerPrimitiveDigis' ) ) 
-## simRctDigisECAL.useHcal = cms.bool(False)
 simRctDigis = L1Trigger.RegionalCaloTrigger.rctDigis_cfi.rctDigis.clone()  
 simRctDigis.ecalDigis = cms.VInputTag( cms.InputTag( 'simEcalTriggerPrimitiveDigis' ) ) 
 simRctDigis.useHcal = cms.bool(False)
@@ -32,8 +15,6 @@ simRctDigis.useHcal = cms.bool(False)
 from RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
 from RecoLocalCalo.EcalRecProducers.ecalDetIdToBeRecovered_cfi import *
-#simEcalUnsuppressedDigis.hitsProducer = cms.string('famosSimHits')
-#ecal_digi_parameters.hitsProducer = cms.string('famosSimHits')
 
 import EventFilter.EcalDigiToRaw.ecalDigiToRaw_cfi
 ecalPacker = EventFilter.EcalDigiToRaw.ecalDigiToRaw_cfi.ecaldigitorawzerosup.clone()
@@ -48,7 +29,6 @@ from EventFilter.RawDataCollector.rawDataCollector_cfi import *
 import EventFilter.EcalRawToDigi.EcalUnpackerData_cfi
 ecalDigis = EventFilter.EcalRawToDigi.EcalUnpackerData_cfi.ecalEBunpacker.clone()
 ecalDigis.InputLabel = 'rawDataCollector'
-#ecalDigis.InputLabel = 'source'
 
 ##### THIS IS JUST TEMPORARY, THE ECAL PRESHOWER MUST EVENTUALLY BE MOVED TO REAL DIGITIZER TOO
 ecalPreshowerRecHit =  cms.EDProducer("CaloRecHitsProducer",
@@ -64,12 +44,7 @@ ecalPreshowerRecHit =  cms.EDProducer("CaloRecHitsProducer",
                                                        MixedSimHits = cms.InputTag("mix","famosSimHitsEcalHitsES"))))
 #####
 
-
-## ecalDigisPlusRecHitSequence = cms.Sequence(simEcalTriggerPrimitiveDigis*simEcalDigis* # Digi
-## 			  simRctDigis*							           # L1Simulation
-##                           ecalPacker*rawDataCollector*ecalDigis*                                   #Digi2raw raw2digi
-## 			  ecalGlobalUncalibRecHit*ecalDetIdToBeRecovered*ecalRecHit)	           # Reconstruction	
-			  
+		  
 ecalDigisSequence = cms.Sequence(simEcalTriggerPrimitiveDigis*simEcalDigis* # Digi
 			  simRctDigis*							           # L1Simulation
                           ecalPacker*rawDataCollector*ecalDigis)	
