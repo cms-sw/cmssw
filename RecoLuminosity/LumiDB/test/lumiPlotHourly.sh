76ba556e733283b524169c6001b03785f19573ff
Jeroen Hegeman <sha1-3eda86c1f2739f8c5b2991525ee3d7f19516f60c@cern.ch>
2012-10-22 09:25:03 +0000

    New version of lumiPlotHourly.sh to go with create_public_lumi_plots.py.

--- RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
+++ RecoLuminosity/LumiDB/test/lumiPlotHourly.sh
@@ -1,62 +1,48 @@
 #!/bin/sh
-currendir=`pwd`
-sarch="slc5_amd64_gcc462"
-export SCRAM_ARCH="$sarch"
 workdir="/afs/cern.ch/user/l/lumipro/scratch0/exec/CMSSW_5_3_2"
 authdir="/afs/cern.ch/user/l/lumipro"
 macrodir="$workdir/src/RecoLuminosity/LumiDB/plotdata"
 outdir="/afs/cern.ch/cms/lumi/www/publicplots"
 logpath="/afs/cern.ch/cms/lumi"
-logfile="lumiPlot-2012pp.log"
-beamenergy=4000
-dbConnectionString="oracle://cms_orcon_adg/cms_lumi_prod"
-amodetag="PROTPHYS"
-begTime='04/04/12 00:00:00'
+
+if [[ $# -lt 1 ]]; then
+  echo "ERROR Need at least a config file name in order to do something"
+  exit 1
+fi
+
+if [[ $# -gt 2 ]]; then
+  echo "ERROR Can only handle one config file name and an optional --ignore-cache flag"
+  exit 1
+fi
+
+# Figure out the configuration file to use.
+cfgfile=$macrodir/$1
+logfile=${1/cfg/log}
+if [[ ! -f $cfgfile ]]; then
+  echo "ERROR File $cfgfile does not exist"
+  exit 1
+fi
+
+# See if we are being asked to clean the cache.
+IGNORE_CACHE_FLAG=""
+if [[ $# -eq 2 ]]; then
+  if [ "$2" != "--ignore-cache" ]; then
+    echo "WARNING: Don't understand the '$2' flag --> ignoring it"
+  else
+    echo "WARNING: Ignoring and rebuilding the lumiCalc cache"
+    IGNORE_CACHE_FLAG="--ignore-cache"
+  fi
+fi
+
 source /afs/cern.ch/cms/cmsset_default.sh;
 cd $workdir
 eval `scramv1 runtime -sh`
 
-touch "$logpath/$logfile"
+touch $logpath/$logfile
 
-date >> "$logpath/$logfile"
+date >> $logpath/$logfile
 
-outfile="$outdir/totallumivstime-pp-2012"
-infile="$outdir/totallumivstime-pp-2012.csv"
+cd $outdir
+python -u $macrodir/create_public_lumi_plots.py $cfgfile ${IGNORE_CACHE_FLAG} >> $logpath/$logfile
 
-export TNS_ADMIN=/afs/cern.ch/cms/lumi/DB
-  
-touch $infile
-echo "lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin $begTime --outplotdata $outfile --inplotdata $infile --lastpointfromdb --without-png time" >> "$logpath/$logfile"
-
-lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin "$begTime" --outplotdata "$outfile"  --inplotdata $infile --lastpointfromdb --without-png time >> "$logpath/$logfile"
-
-date >> "$logpath/$logfile"
-
-outfile="$outdir/lumiperday-pp-2012"
-infile="$outdir/lumiperday-pp-2012.csv"
-touch $infile
-
-echo "lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin $begTime --outplotdata $outfile  --inplotdata $infile --lastpointfromdb --without-png perday">>  "$logpath/$logfile"
-
-lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin "$begTime" --outplotdata $outfile  --inplotdata $infile --lastpointfromdb --without-png perday >> "$logpath/$logfile"
-
-date >> "$logpath/$logfile"
-
-outfile="$outdir/lumipeak-pp-2012"
-infile="$outdir/lumipeak-pp-2012.csv"
-
-touch $infile
-
-echo "lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin $begTime --outplotdata $outfile  --inplotdata $infile --lastpointfromdb --without-png instpeakperday">> "$logpath/$logfile"
-
-lumiPlot.py -c $dbConnectionString -P $authdir -b stable --beamenergy $beamenergy --beamfluctuation 0.15 --amodetag $amodetag --begin "$begTime" --outplotdata $outfile  --inplotdata $infile --lastpointfromdb --without-png instpeakperday >> "$logpath/$logfile"
-
-cd $macrodir
-echo "root -b -q create_public_lumi_plots.C">>"$logpath/$logfile"
-root -b -q create_public_lumi_plots.C >> "$logpath/$logfile"
-/bin/cp int_*.png $outdir
-/bin/rm int_*.png
-/bin/cp peak_lumi*.png $outdir
-/bin/rm peak_lumi*.png
-cd $currentdir
-date >> "$logpath/$logfile"
+date >> $logpath/$logfile
