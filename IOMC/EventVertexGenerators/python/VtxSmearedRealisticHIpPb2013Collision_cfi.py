a298d8890aab8f05bd25eb6c30b11251b5ba8b16
Kevin Burkett <sha1-d3b62d5921247fe185d98f6705fe0f56064ef84b@cern.ch>
2012-10-23 21:58:50 +0000

    new parameters for 2013 HI collisions

---
+++ IOMC/EventVertexGenerators/python/VtxSmearedRealisticHIpPb2013Collision_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
+VtxSmeared = cms.EDProducer("BetafuncEvtVtxGenerator",
+    RealisticHIpPb2013CollisionVtxSmearingParameters,
+    VtxSmearedCommon
+)
+
+
+
