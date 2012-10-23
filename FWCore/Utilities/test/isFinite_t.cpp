de0b01a2043725a2d4a5b51565a79519a0a4ce7e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-10-23 15:26:15 +0000

    added for long double

--- FWCore/Utilities/test/isFinite_t.cpp
+++ FWCore/Utilities/test/isFinite_t.cpp
@@ -7,7 +7,7 @@
 
 int main(int n, const char**) {
   using namespace edm;
-  
+  typedef long double LD;
   
   double zero = atof("0");
   
@@ -30,10 +30,14 @@ int main(int n, const char**) {
   assert(!isNotFinite(float(-3.14)) );
   assert(isNotFinite(std::sqrt(-double(n))) );
   assert(isNotFinite(std::sqrt(-float(n))) );
-  assert(isNotFinite(1./zero) );
+  assert(isNotFinite(-1.f/float(zero)) );
   assert(isNotFinite(float(1.)/float(zero)) );
   assert(isNotFinite(-1./zero) );
   assert(isNotFinite(-1.f/float(zero)) );
+
+  assert(!isNotFinite(LD(3.14)) );
+  assert(isNotFinite(-1.f/float(zero)) );
+  assert(isNotFinite(std::sqrt(-LD(n))) );
 
   return 0;
 }
