77c51f3d9dc851ae41e846860e3ff4f369b6136d
Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch>
2012-10-24 12:31:07 +0000

    doc

--- python/electronRegressionEnergyProducer_cfi.py
+++ python/electronRegressionEnergyProducer_cfi.py
@@ -7,14 +7,16 @@ eleRegressionEnergy = cms.EDProducer("RegressionEnergyPatElectronProducer",
                                    inputCollectionType = cms.uint32(1),
                                      # useRecHitCollections ( should be set to true if GsfElectrons or if the RecHits have not been embedded into the PATElectron
                                    useRecHitCollections = cms.bool(False),
-                                     # produce ValueMaps. Should be true for GsfElectrons otherwise this producer doest nothing
+                                     # produce ValueMaps. Should be true for GsfElectrons otherwise this producer doest nothing. Keep it to false for PAT
                                    produceValueMaps = cms.bool(False),
                                    regressionInputFile = cms.string("EGamma/EGammaAnalysisTools/data/eleEnergyRegWeights_V1.root"),
                                    energyRegressionType = cms.uint32(1),
                                    rhoCollection = cms.InputTag('kt6PFJets:rho:RECO'),
-                                   vertexCollection = cms.InputTag('offlinePrimaryVertices'),  
+                                   vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+                                     # Not used if inputCollectionType is set to 1                                         
                                    nameEnergyReg = cms.string("eneRegForGsfEle"),
                                    nameEnergyErrorReg = cms.string("eneErrorRegForGsfEle"),
+                                     # Used only if useRecHitCollections is set to true 
                                    recHitCollectionEB = cms.InputTag('reducedEcalRecHitsEB'),
                                    recHitCollectionEE = cms.InputTag('reducedEcalRecHitsEE')
                                    )
