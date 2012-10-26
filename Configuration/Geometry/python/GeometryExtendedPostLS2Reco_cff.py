4e6410b4aeec713029308ab44d285074ce05ced1
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2012-10-26 06:14:09 +0000

    Make sure all SLHC Hcal constants are customized.

--- Configuration/Geometry/python/GeometryExtendedPostLS2Reco_cff.py
+++ Configuration/Geometry/python/GeometryExtendedPostLS2Reco_cff.py
@@ -2,27 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for transient ECAL alignement
 from Configuration.Geometry.GeometryExtendedPostLS2_cff import *
-
-# Reconstruction geometry services
-#  Tracking Geometry
-from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
-
-#Tracker
-from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
-
-#Muon
-from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
-from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-
-#  Alignment
-from Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff import *
-from Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff import *
-from Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff import *
-
-#  Calorimeters
-from Geometry.CaloEventSetup.CaloTopology_cfi import *
-from Geometry.CaloEventSetup.CaloGeometry_cff import *
-from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
-from Geometry.EcalMapping.EcalMapping_cfi import *
-from Geometry.EcalMapping.EcalMappingRecord_cfi import *
-
+from Configuration.Geometry.GeometryReco_cff import *
