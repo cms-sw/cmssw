6c823948ba2038ebd85c48fe5eba66a834030b67
Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch>
2012-10-27 17:00:35 +0000

    Updated parameter to match what Kevin added to IOMC

--- GeneratorInterface/HiGenCommon/python/VtxSmearedRealisticPPbBoost8TeVCollision_cff.py
+++ GeneratorInterface/HiGenCommon/python/VtxSmearedRealisticPPbBoost8TeVCollision_cff.py
@@ -3,15 +3,16 @@ import FWCore.ParameterSet.Config as cms
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 
 Realistic5TeVCollisionPPbBoostVtxSmearingParameters= cms.PSet(
+
     Phi = cms.double(0.0),
-    BetaStar = cms.double(1100.0),
-    Emittance = cms.double(0.5e-07),
+    BetaStar = cms.double(80.0),
+    Emittance = cms.double(6.25e-07),
     Alpha = cms.double(0.0),
-    SigmaZ = cms.double(6.889),
+    SigmaZ = cms.double(8.0),
     TimeOffset = cms.double(0.0),
-    X0 = cms.double(0.228489),
-    Y0 = cms.double(0.4475616),
-    Z0 = cms.double(0.224955),
+    X0 = cms.double(0.2440),
+    Y0 = cms.double(0.3929),
+    Z0 = cms.double(0.4145)
     Beta=cms.double(-0.434)
 )
 
