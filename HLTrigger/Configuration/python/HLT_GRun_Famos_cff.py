df5b633277e8a98ef074a667eef95bb7e13100bf
Juliette Alimena <sha1-8ce8f4c1f397c2d37216e2f844f3f59da506a691@cern.ch>
2012-10-29 16:23:08 +0000

    sr #133325: pPb muon paths

--- HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,11 +1,11 @@
-# /online/collisions/2012/8e33/v2.0/HLT/V5 (CMSSW_5_2_7_HLT2)
+# /dev/CMSSW_5_2_6/GRun/V51 (CMSSW_5_2_7_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 from FastSimulation.HighLevelTrigger.HLTSetup_cff import *
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/online/collisions/2012/8e33/v2.0/HLT/V5')
+  tableName = cms.string('/dev/CMSSW_5_2_6/GRun/V51')
 )
 
 hltESSBTagRecord = cms.ESSource( "EmptyESSource",
--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_5_2_6/HIon/V50 (CMSSW_5_2_7_HLT2)
+# /dev/CMSSW_5_2_6/HIon/V51 (CMSSW_5_2_7_HLT2)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_2_6/HIon/V50')
+  tableName = cms.string('/dev/CMSSW_5_2_6/HIon/V51')
 )
 
 streams = cms.PSet( 
