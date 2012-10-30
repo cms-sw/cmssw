d10c8579a3cff6599eacf1a7daa098fe0874df15
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-10-30 21:34:42 +0000

    script modified to get the DQM root file name as input. Return code different from zero means a problem

--- DQM/SiStripMonitorClient/bin/check_runcomplete.h
+++ DQM/SiStripMonitorClient/bin/check_runcomplete.h
@@ -1,7 +1,6 @@
 #ifndef DQM_SiStripMonitorClient_check_runcomplete_h
 #define DQM_SiStripMonitorClient_check_runcomplete_h
 #include <string>
-int  read_runflag        ( int,std::string);
-int  get_filename        ( int,std::string,std::string&);
-void check_runcomplete ( int run , std::string repro_type );
+int  read_runflag        (std::string);
+int   check_runcomplete (std::string );
 #endif // DQM_SiStripMonitorClient_check_runcomplete_h
