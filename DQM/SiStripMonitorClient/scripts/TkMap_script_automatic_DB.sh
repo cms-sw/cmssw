860de44c57787826a07fdb1701f115e276c5d125
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-10-30 22:14:10 +0000

    commented lines removed, DQM root file check can stop the process, working directory moved into the CMSSW working area, added script to get the GlobalTag name, results of SiStripQualityStatisics fully integrated, use of html index file templates

--- DQM/SiStripMonitorClient/scripts/TkMap_script_automatic_DB.sh
+++ DQM/SiStripMonitorClient/scripts/TkMap_script_automatic_DB.sh
@@ -2,11 +2,12 @@
 
 export PATH=/afs/cern.ch/cms/common:${PATH}
 if [[ "$#" == "0" ]]; then
-    echo "usage: 'TkMap_script_automatic.sh Cosmics|MinimumBias|StreamExpress runNumber1 runNumber2...'";
+    echo "usage: 'TkMap_script_automatic.sh Cosmics|MinimumBias|StreamExpress|StreamExpressCosmics runNumber1 runNumber2...'";
     exit 1;
 fi
 
-export WORKINGDIR=/afs/cern.ch/user/c/cctrack/scratch0/TKMap
+export WORKINGDIR=${CMSSW_BASE}/src
+#export WORKINGDIR=/afs/cern.ch/user/c/cctrack/scratch0/TKMap/AndreaTests
 
 #echo " Moving to CMSSW release"
 #cd /afs/cern.ch/user/c/cctrack/scratch0/TKMap/CMSSW_4_2_3/src
@@ -24,26 +25,27 @@ do
     nnn=`echo $Run_numb | awk '{print substr($0,0,4)}'` 
     curl -k --cert /data/users/cctrkdata/current/auth/proxy/proxy.cert --key /data/users/cctrkdata/current/auth/proxy/proxy.cert -X GET 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/Run2012/'${1}'/000'${nnn}'xx/' > index.html
 #    wget --no-check-certificate 'https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/HIRun2011/'${1}'/000'${nnn}'xx/' -O index.html
-    dqmFileName=`cat index.html | grep ${Run_numb} | grep "_DQM.root" | sed 's/.*>\(.*\)<\/a.*/\1/' `
+    dqmFileNames=`cat index.html | grep ${Run_numb} | grep "_DQM.root" | sed 's/.*>\(.*\)<\/a.*/\1/' `
+    dqmFileName=`expr "$dqmFileNames" : '\(DQM[A-Za-z0-9_/.\-]*root\)'`
     echo ' dqmFileName = ['$dqmFileName']'
     curl -k --cert /data/users/cctrkdata/current/auth/proxy/proxy.cert --key /data/users/cctrkdata/current/auth/proxy/proxy.cert -X GET https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/Run2012/${1}/000${nnn}xx/${dqmFileName} > /tmp/${dqmFileName}
     checkFile=`ls /tmp/${dqmFileName} | grep ${Run_numb}`
 
 ##check if the full run is completely saved (Info/Run summary/ProvInfo/ runIsComplete flag == 1? 
 ##if not, throw a warning
-# root macro to be moved in the CMSSW package
 
-#    root.exe -l -b -q   "check_runcomplete.C+( $Run_numb , \"${1}\")"
+    file_path="/tmp/"
 
-    check_runcomplete $Run_numb  ${1}
+#    check_runcomplete $Run_numb  ${1}
+    check_runcomplete ${file_path}/$dqmFileName
+    if [ $? -ne  0 ]; then continue; fi
 
-    echo Process $Run_numb
+    echo Process ${file_path}/$dqmFileName
 
     cd /tmp
 
     nnn=`echo $Run_numb | awk '{print substr($0,0,4)}'` 
   
-file_path="/tmp/"
 
     echo ' dqmFileName = ['$dqmFileName']'
 
@@ -59,23 +61,37 @@ file_path="/tmp/"
     [ -e $Run_numb/$1 ] || mkdir $Run_numb/$1;
     echo "Run ${Run_numb}"
 
-#    ##Build the cfg files:
-#    [ -e SiStripDQM_OfflineTkMap_cfg_$Run_numb.py ] && rm SiStripDQM_OfflineTkMap_cfg_$Run_numb.py
-#    cat SiStripDQM_OfflineTkMap_Template_cfg_DB.py | sed -e "s@RunNum@$Run_numb@g" | sed -e "s@DQMfile_to_process.root@${file_path}/$dqmFileName@g" > SiStripDQM_OfflineTkMap_cfg_$Run_numb.py  
-#
+    cd $Run_numb/$1
+    echo `pwd`
+    rm -f *.png
+    rm -f *.xml
+    rm -f *.log
+    rm -f *.txt
+    rm -f *.html
+
+# Determine the GlobalTag name used to process the data and the DQM
+
+    GLOBALTAG=`getGTscript.sh $dqmFileName $Run_numb` 
+#    GLOBALTAG="GR_P_V40::All"
+    echo "The GlobalTag is $GLOBALTAG"
+    if [[ "${GLOBALTAG}" == "" ]]
+    then
+       GLOBALTAG="GR_P_V42::All"
+    fi
+
     echo " Creating the TrackerMap.... "
 
 
-#    cmsRun SiStripDQM_OfflineTkMap_cfg_${Run_numb}.py &> logfile_${Run_numb}.txt
- 
-    cmsRun ${CMSSW_BASE}/src/DQM/SiStripMonitorClient/test/SiStripDQM_OfflineTkMap_Template_cfg_DB.py print globalTag=GR_P_V32::All runNumber=${Run_numb} dqmFile=${file_path}/$dqmFileName
+    cmsRun ${CMSSW_BASE}/src/DQM/SiStripMonitorClient/test/SiStripDQM_OfflineTkMap_Template_cfg_DB.py print globalTag=${GLOBALTAG} runNumber=${Run_numb} dqmFile=${file_path}/$dqmFileName  # update GlobalTag
 
-    mv *.png $Run_numb/$1
+#    mv *.png $Run_numb/$1
+#    mv *.xml $Run_numb/$1
+#    mv PCLBadComponents.log $Run_numb/$1
 
-    if [ "${1}" == "Cosmics" ]; then 
-	cat /afs/cern.ch/user/c/cctrack/scratch0/TKMap/scripts/index_template_TKMap_cosmics.html | sed -e "s@RunNumber@$Run_numb@g" > $Run_numb/$1/index.html
+    if [ "${1}" == "Cosmics" ]; then  # should I add StreamExpressCosmics too
+	cat ${CMSSW_BASE}/src/DQM/SiStripMonitorClient/data/index_template_TKMap_cosmics.html | sed -e "s@RunNumber@$Run_numb@g" > index.html
     else
-	cat /afs/cern.ch/user/c/cctrack/scratch0/TKMap/scripts/index_template_TKMap.html | sed -e "s@RunNumber@$Run_numb@g" > $Run_numb/$1/index.html
+	cat ${CMSSW_BASE}/src/DQM/SiStripMonitorClient/data/index_template_TKMap.html | sed -e "s@RunNumber@$Run_numb@g" > index.html
     fi
     
     echo " Check TrackerMap on $Run_numb/$1 folder"
@@ -84,36 +100,34 @@ file_path="/tmp/"
 
 ## Producing the list of bad modules
     echo " Creating the list of bad modules "
-#    root.exe -l -b -q   "listbadmodule.C+(\"${file_path}/$dqmFileName\")"
     
-    listbadmodule ${file_path}/$dqmFileName
+    listbadmodule ${file_path}/$dqmFileName PCLBadComponents.log
 
-    mv QualityTest*txt $Run_numb/$1
+#    mv QualityTest*txt $Run_numb/$1
 
 ## Producing the run certification by lumisection
     echo " Creating the lumisection certification:"
-##    root.exe -l -b -q   "ls_cert.C+(0.95,0.95,\"${file_path}/$dqmFileName\")"
 
-    ls_cert 0.95 0.95 ${file_path}/$dqmFileName
-
-    mv Certification_run_* $Run_numb/$1
+    if [ "${1}" == "MinimumBias" -o "${1}" == "StreamExpress" ]; then	
+	ls_cert 0.95 0.95 ${file_path}/$dqmFileName
+#	mv Certification_run_* $Run_numb/$1
+    fi
 
 ## Producing the PrimaryVertex/BeamSpot quality test by LS..
     if [ "${1}" == "MinimumBias" -o "${1}" == "Jet" ]; then	
 	echo " Creating the BeamSpot Calibration certification summary:"
-##	root.exe -l -b -q   "lsbs_cert.C+(\"${file_path}/$dqmFileName\")"
 
 	lsbs_cert  ${file_path}/$dqmFileName
 
-	mv Certification_BS_run_* $Run_numb/$1
+#	mv Certification_BS_run_* $Run_numb/$1
     fi
 ## .. and harvest the bad beamspot LS with automatic emailing (if in period and if bad LS found)
-    bs_bad_ls_harvester $Run_numb/$1 $Run_numb
+#    bs_bad_ls_harvester $Run_numb/$1 $Run_numb
+    bs_bad_ls_harvester . $Run_numb
 
 ## Producing the Module difference for ExpressStream
     if [ "${1}" == "StreamExpress" -o "${1}" == "StreamExpressCosmics" ]; then	
 	echo " Creating the Module Status Difference summary:"
-##	root.exe -l -b -q   "modulediff.C+( $Run_numb , \"${1}\")"
 
 #	modulediff $Run_numb ${1}
 
@@ -129,17 +143,16 @@ file_path="/tmp/"
 #    dest=AndreaTests
 
 #    ssh cmstacuser@cmstac05 "mkdir -p /storage/data2/SiStrip/event_display/Data2011/${dest}/${nnn}/${Run_numb} 2> /dev/null"
- mkdir -p /data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb} 2> /dev/null
+ mkdir -p /data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb}/$1 2> /dev/null
+     rm -f *.xml
+     rm -f *svg
 
 #    scp -r ${Run_numb}/$1 cmstacuser@cmstac05:/storage/data2/SiStrip/event_display/Data2011/${dest}/${nnn}/${Run_numb}/
 #    cp -r ${Run_numb}/$1 /data/users/event_display/Data2011/${dest}/${nnn}/${Run_numb}/
-    ssh cctrack@vocms01 "mkdir -p /data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb} 2> /dev/null"
-    scp -r ${Run_numb}/$1 cctrack@vocms01:/data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb}/
+     ssh cctrack@vocms01 "mkdir -p /data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb}/$1 2> /dev/null"
+#     scp -r ${Run_numb}/$1 cctrack@vocms01:/data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb}/
+     scp -r * cctrack@vocms01:/data/users/event_display/Data2012/${dest}/${nnn}/${Run_numb}/$1
 
-    rm -f *svg
-    rm -rf ${Run_numb}
-    rm -f SiStripDQM_OfflineTkMap_cfg_$Run_numb.py
-    rm -f logfile_${Run_numb}.txt
-    rm ${file_path}/$dqmFileName
+     rm ${file_path}/$dqmFileName
 
 done
