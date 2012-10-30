83c8b7754bbdbb5621662c283bad0770ebcc7ed3
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2012-10-30 17:46:30 +0000

    fix example configuration

--- GeneratorInterface/LHEInterface/python/ExternalLHEProducer_cfi.py
+++ GeneratorInterface/LHEInterface/python/ExternalLHEProducer_cfi.py
@@ -2,7 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
     scriptName = cms.FileInPath("GeneratorInterface/LHEInterface/data/run_madgraph_gridpack.sh"),
-    outputFile = cms.string("events_final.lhe"),
+    outputFile = cms.string("W1Jet_7TeV_madgraph_final.lhe"),
+    numberOfParameters = cms.uint32(10),
     args = cms.vstring('slc5_ia32_gcc434/madgraph/V5_1.3.27/test','W1Jet_7TeV_madgraph','false','true','wjets','5','20','false','0','99'),
     nEvents = cms.uint32(100)                                    
 )
