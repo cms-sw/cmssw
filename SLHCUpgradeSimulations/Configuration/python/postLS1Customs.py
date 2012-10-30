9d51c987bfae0e6faf4fb39d2d1ed9721603940d
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2012-10-30 14:06:50 +0000

    get crossing frame and extra output commands

--- SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
@@ -2,9 +2,31 @@
 import FWCore.ParameterSet.Config as cms
 
 from muonCustoms import customise_csc_geom_cond_digi
-from customise_mixing import customise_NoCrossing
 
 def digiCustoms(process):
-#    process=customise_NoCrossing(process)
+    #turn off the crossing frame
+    process.mix.mixObjects.mixSH.crossingFrames = cms.untracked.vstring(
+        'BSCHits',
+        'FP420SI',
+        'MuonCSCHits',
+        'MuonDTHits',
+        'MuonRPCHits',
+        'TotemHitsRP',
+        'TotemHitsT1',
+        'TotemHitsT2Gem')
+    process.mix.mixObjects.mixCH.crossingFrames = cms.untracked.vstring('')
+    process.mix.mixObjects.mixTracks.makeCrossingFrame = cms.untracked.bool(False)
+    process.mix.mixObjects.mixVertices.makeCrossingFrame = cms.untracked.bool(False)
+    process.mix.mixObjects.mixHepMC.makeCrossingFrame = cms.untracked.bool(False)
+    process.digitisation_step.remove(process.simSiStripDigiSimLink)
+    process.digitisation_step.remove(process.mergedtruth)
+   
+    #deal with csc
     process=customise_csc_geom_cond_digi(process)
+
+    #extend the event content
+    process.FEVTDEBUGoutput.outputCommands.append( 'keep *_simMuonCSCDigis_*_*')
+    process.FEVTDEBUGoutput.outputCommands.append( 'keep *_simHcalUnsuppressedDigis_*_*')
+                                                   
+    
     return process
