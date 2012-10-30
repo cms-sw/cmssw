2f57137ef061696150fac08a865391d72a32709c
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2012-10-30 20:36:46 +0000

    Correctly applying PU subtraction for the EI

--- CommonTools/ParticleFlow/python/EITopPAG_cff.py
+++ CommonTools/ParticleFlow/python/EITopPAG_cff.py
@@ -5,6 +5,9 @@ from CommonTools.ParticleFlow.pfJets_cff import *
 from CommonTools.ParticleFlow.pfTaus_cff import *
 from CommonTools.ParticleFlow.ParticleSelectors.pfMuonsFromVertex_cfi import pfMuonsFromVertex
 from CommonTools.ParticleFlow.ParticleSelectors.pfElectronsFromVertex_cfi import pfElectronsFromVertex
+from CommonTools.ParticleFlow.pfNoPileUp_cff  import *
+from CommonTools.ParticleFlow.pfNoPileUpJME_cff  import *
+
 
 # sequential top projection cleaning
 from CommonTools.ParticleFlow.TopProjectors.pfNoMuon_cfi import * 
@@ -13,75 +16,68 @@ from CommonTools.ParticleFlow.TopProjectors.pfNoJet_cfi import *
 from CommonTools.ParticleFlow.TopProjectors.pfNoTau_cfi import *
 
 
+#### PU Again... need to do this twice because the "linking" stage of PF reco ####
+#### condenses information into the new "particleFlow" collection.            ####
+
+
+pfPileUpEI = pfPileUp.clone( PFCandidates = cms.InputTag('particleFlowPtrs') )
+pfNoPileUpEI = pfNoPileUp.clone( bottomCollection = cms.InputTag('particleFlowPtrs'),
+                                 topCollection = cms.InputTag('pfPileUpEI') )
+
+pfPileUpJMEEI = pfPileUpJME.clone( PFCandidates = cms.InputTag('particleFlowPtrs') )
+pfNoPileUpJMEEI = pfNoPileUpJME.clone( bottomCollection = cms.InputTag('particleFlowPtrs'),
+                                       topCollection = cms.InputTag('pfPileUpJMEEI') )
+
+
+#### Muons ####
 
 pfAllMuonsEI = cms.EDFilter(
     "PFCandidateFwdPtrCollectionStringFilter",
-    src = cms.InputTag("particleFlow"),
+    src = cms.InputTag("pfNoPileUpEI"),
     cut = cms.string("abs(pdgId())==13"
         ),
     makeClones = cms.bool(True)
 )
 
-
-pfMuonsNoPUEI = cms.EDProducer(
-    "TPPFCandidatesOnPFCandidates",
-    enable =  cms.bool( True ),
-    verbose = cms.untracked.bool( False ),
-    name = cms.untracked.string("muonsNoPUEI"),
-    topCollection = cms.InputTag("pfPileUp"),
-    bottomCollection = cms.InputTag("pfAllMuonsEI")
-)
-
-pfMuonsNoPUEIClones = cms.EDProducer("PFCandidateFromFwdPtrProducer",
-                                     src=cms.InputTag('pfMuonsNoPUEI')
-    )
-
-
-pfMuonsFromVertexEI = pfMuonsFromVertex.clone( src = cms.InputTag('pfMuonsNoPUEIClones') )
+pfMuonsFromVertexEI = pfMuonsFromVertex.clone( src = cms.InputTag('pfAllMuonsEI') )
 
 pfIsolatedMuonsEI = cms.EDFilter(
     "PFCandidateFwdPtrCollectionStringFilter",
-    src = cms.InputTag("pfAllMuonsEI"),
+    src = cms.InputTag("pfMuonsFromVertexEI"),
     cut = cms.string("pt > 5 & muonRef.isAvailable() & "\
-                     "muonRef.pfIsolationR03().sumChargedHadronPt + "\
-                     "muonRef.pfIsolationR03().sumNeutralHadronEt + "\
-                     "muonRef.pfIsolationR03().sumPhotonEt "\
+                     "muonRef.pfIsolationR04().sumChargedHadronPt + "\
+                     "muonRef.pfIsolationR04().sumNeutralHadronEt + "\
+                     "muonRef.pfIsolationR04().sumPhotonEt "\
                      " < 0.15 * pt "
         ),
     makeClones = cms.bool(True)
 )
 
+pfNoMuon.topCollection    = 'pfIsolatedMuonsEI'
+pfNoMuon.bottomCollection = 'pfNoPileUpEI'
+
+
+pfNoMuonJME.topCollection    = 'pfIsolatedMuonsEI'
+pfNoMuonJME.bottomCollection = 'pfNoPileUpJMEEI'
+
+
+
+#### Electrons ####
 
 pfAllElectronsEI = cms.EDFilter(
     "PFCandidateFwdPtrCollectionStringFilter",
-    src = cms.InputTag("particleFlow"),
+    src = cms.InputTag("pfNoMuon"),
     cut = cms.string("abs(pdgId())==11"
         ),
     makeClones = cms.bool(True)
 )
 
-
-pfElectronsNoPUEI = cms.EDProducer(
-    "TPPFCandidatesOnPFCandidates",
-    enable =  cms.bool( True ),
-    verbose = cms.untracked.bool( False ),
-    name = cms.untracked.string("muonsNoPUEI"),
-    topCollection = cms.InputTag("pfPileUp"),
-    bottomCollection = cms.InputTag("pfAllElectronsEI")
-)
-
-
-pfElectronsNoPUEIClones = cms.EDProducer("PFCandidateFromFwdPtrProducer",
-                                     src=cms.InputTag('pfElectronsNoPUEI')
-    )
-
-
-pfElectronsFromVertexEI = pfElectronsFromVertex.clone( src = cms.InputTag('pfElectronsNoPUEIClones') )
+pfElectronsFromVertexEI = pfElectronsFromVertex.clone( src = cms.InputTag('pfAllElectronsEI') )
 
 
 pfIsolatedElectronsEI = cms.EDFilter(
     "PFCandidateFwdPtrCollectionStringFilter",
-    src = cms.InputTag("particleFlow"),
+    src = cms.InputTag("pfElectronsFromVertexEI"),
     cut = cms.string(" pt > 5 & gsfElectronRef.isAvailable() & gsfTrackRef.trackerExpectedHitsInner.numberOfLostHits<2 & "\
                      "gsfElectronRef.pfIsolationVariables().chargedHadronIso + "\
                      "gsfElectronRef.pfIsolationVariables().neutralHadronIso + "\
@@ -92,35 +88,32 @@ pfIsolatedElectronsEI = cms.EDFilter(
 )
 
 
-pfNoMuon.topCollection    = 'pfIsolatedMuonsEI'
-pfNoMuon.bottomCollection = 'pfNoPileUp'
-
 pfNoElectron.topCollection    = 'pfIsolatedElectronsEI'
 pfNoElectron.bottomCollection = 'pfNoMuon'
-
-pfNoMuonJME.topCollection    = 'pfIsolatedMuonsEI'
-pfNoMuonJME.bottomCollection = 'pfNoPileUpJME'
 
 pfNoElectronJME.topCollection    = 'pfIsolatedElectronsEI'
 pfNoElectronJME.bottomCollection = 'pfNoMuonJME'
 
+
+#### Jets ####
 pfNoJet.topCollection = 'pfJetsPtrs'
 pfNoJet.bottomCollection = 'pfNoElectronJME'
 
+#### Taus ####
 pfNoTau.topCollection = 'pfTausPtrs'
 pfNoTau.bottomCollection = 'pfJetsPtrs'
 
 EITopPAG = cms.Sequence(
+    pfPileUpEI +
+    pfPileUpJMEEI +
+    pfNoPileUpEI +
+    pfNoPileUpJMEEI + 
     pfAllMuonsEI +
-    pfMuonsNoPUEI +
-    pfMuonsNoPUEIClones +
     pfMuonsFromVertexEI + 
     pfIsolatedMuonsEI +
     pfNoMuon +
     pfNoMuonJME +
     pfAllElectronsEI +
-    pfElectronsNoPUEI +
-    pfElectronsNoPUEIClones +
     pfElectronsFromVertexEI + 
     pfIsolatedElectronsEI +    
     pfNoElectron +
