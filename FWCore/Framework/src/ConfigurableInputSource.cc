55f7cd1b8f54359af6b73d5e5b60420eb3c317d5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-10-31 22:44:11 +0000

    Don't access principal cache directly if not necessary

--- FWCore/Framework/src/ConfigurableInputSource.cc
+++ FWCore/Framework/src/ConfigurableInputSource.cc
@@ -66,7 +66,7 @@ namespace edm {
   ConfigurableInputSource::readEvent_(EventPrincipal& eventPrincipal) {
     assert(eventCached() || processingMode() != RunsLumisAndEvents);
     resetEventCached();
-    return eventPrincipalCache();
+    return &eventPrincipal;
   }
 
   void
