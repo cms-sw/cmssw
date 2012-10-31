30b7f1c2eab98761659ae7feb3cae5dfff5c881d
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-10-31 16:26:54 +0000

    backup copy before clan-up

--- TauAnalysis/MCEmbeddingTools/python/rerunParticleFlow.py
+++ TauAnalysis/MCEmbeddingTools/python/rerunParticleFlow.py
@@ -7,7 +7,31 @@ from Configuration.EventContent.EventContent_cff import RECOEventContent
 
 import re
 
-def updateInputTags(object, inputProcess):
+def loadIfNecessary(process, configFile, module_or_sequenceName):
+    if not hasattr(process, module_or_sequenceName):
+        process.load(configFile)
+
+from FWCore.ParameterSet.Modules import _Module
+class seqVisitorGetModuleNames():
+    def __init__(self):
+        self.modulesNames = []
+
+    def giveNext(self):
+	return self.nextInChain
+    def givePrev(self):
+	return self.prevInChain
+      
+    def enter(self, visitee):
+        if isinstance(visitee, _Module):
+            self.modulesNames.append(visitee.label())
+	      	
+    def leave(self, visitee):
+        pass
+
+    def getModuleNames(self):
+        return self.modulesNames
+
+def updateInputTags(process, object, inputProcess):
     if isinstance(object, cms.InputTag):
         #print "InputTag: %s" % object
         keepStatement_regex = r"keep [a-zA-Z0-9*]+_(?P<label>[a-zA-Z0-9*]+)_[a-zA-Z0-9*]+_[a-zA-Z0-9*]+"
@@ -19,6 +43,14 @@ def updateInputTags(object, inputProcess):
                 label = keepStatement_match.group('label')
                 if label == object.getModuleLabel():
                     isStoredInRECO = True
+        isInRerunParticleFlowSequence = False
+        if hasattr(process, "rerunParticleFlowSequence"):
+            sequenceVisitor = seqVisitorGetModuleNames()
+            getattr(process, "rerunParticleFlowSequence").visit(sequenceVisitor)
+            moduleNames = sequenceVisitor.getModuleNames()
+            for moduleName in moduleNames:
+                if moduleName == object.getModuleLabel():
+                    isInRerunParticleFlowSequence = True
         if isStoredInRECO:
             if object.getProcessName() != inputProcess:
                 #print "InputTag: %s --> updating processName = %s" % (object, inputProcess)
@@ -26,16 +58,17 @@ def updateInputTags(object, inputProcess):
     elif isinstance(object, cms.PSet):
         for attrName in dir(object):
             attr = getattr(object, attrName)
-            updateInputTags(attr, inputProcess)
+            updateInputTags(process, attr, inputProcess)
     elif isinstance(object, cms.VPSet):
         for pset in object:
             for attrName in dir(pset):
                 attr = getattr(pset, attrName)
-                updateInputTags(attr, inputProcess)
+                updateInputTags(process, attr, inputProcess)
 
 from FWCore.ParameterSet.Modules import _Module
-class SequenceVisitor():
-    def __init__(self, inputProcess):
+class seqVisitorUpdateInputTags():
+    def __init__(self, process, inputProcess):
+        self.process = process
         self.inputProcess = inputProcess
 
     def giveNext(self):
@@ -46,81 +79,90 @@ class SequenceVisitor():
     def enter(self, visitee):
 	if isinstance(visitee, cms.Sequence):
             #print "Sequence: %s" %  visitee.label()
-            sequenceVisitor = SequenceVisitor(self.inputProcess)
+            sequenceVisitor = SequenceVisitor(self.process, self.inputProcess)
             visitee.visit(sequenceVisitor)
         elif isinstance(visitee, _Module):
             #print "Module: %s" %  visitee.label()
             for attrName in dir(visitee):
                 attr = getattr(visitee, attrName)
-                updateInputTags(attr, self.inputProcess)
+                updateInputTags(self.process, attr, self.inputProcess)
 	      	
     def leave(self, visitee):
         pass
 
 def rerunParticleFlow(process, inputProcess):
 
-    #print "<rerunParticleFlow>:"
-
-    # load conditions needed to run particle-flow sequence
-    process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
-    process.load("RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi")
-
-    process.rerunParticleFlowSequence = cms.Sequence()
-
-    # produce objects needed as input of particle-flow algorithm
-    process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
-    process.load("CalibTracker.SiPixelESProducers.SiPixelTemplateDBObjectESProducer_cfi")
-    process.UpdaterService = cms.Service("UpdaterService")
-    # CV: first part of muon reconstruction (input to particle-flow algorithm)
-    if not hasattr(process, "muonreco"):
-        process.load("RecoMuon.Configuration.RecoMuon_cff")
-    process.rerunParticleFlowSequence += process.muonreco
-    process.rerunParticleFlowSequence += process.muIsolation
-    process.rerunParticleFlowSequence += process.muonSelectionTypeSequence
-    process.load("RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cfi")
-    process.rerunParticleFlowSequence += process.cosmicsMuonIdSequence
-
-    if not hasattr(process, "particleFlowCluster"):
-        process.load('RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff')
-    process.rerunParticleFlowSequence += process.particleFlowCluster
-    if not hasattr(process, "pfTrackingGlobalReco"):
-        process.load('RecoParticleFlow.PFTracking.particleFlowTrack_cff')
-    process.rerunParticleFlowSequence += process.pfTrackingGlobalReco
-
-    if not hasattr(process, "gsfEcalDrivenElectronSequence"):
-        process.load("RecoEgamma.EgammaElectronProducers.gsfElectronSequence_cff")
-        process.load("RecoEcal.EgammaCoreTools.EcalNextToDeadChannelESProducer_cff")
-    process.rerunParticleFlowSequence += process.gsfEcalDrivenElectronSequence
-
-    # reconstruct PFCandidates
-    if not hasattr(process, "particleFlowReco"):
-        process.load('RecoParticleFlow.Configuration.RecoParticleFlow_cff')
-    process.rerunParticleFlowSequence += process.particleFlowReco
-
-    # CV: second part of muon reconstruction (needs particle-flow output in order to compute muon isolation)
-    if not hasattr(process, "muonPFIsolationSequence"):
-        process.load("RecoMuon.MuonIsolation.muonPFIsolationValues_cff")
-    process.rerunParticleFlowSequence += process.muonPFIsolationSequence
-    process.rerunParticleFlowSequence += process.muonshighlevelreco
-
-    # CV: compute MET corrections
-    if not hasattr(process, "muonMETValueMapProducer"):
-        process.load("RecoMET.METProducers.MuonMETValueMapProducer_cff")
-    process.rerunParticleFlowSequence += process.muonMETValueMapProducer
-    if not hasattr(process, "muonTCMETValueMapProducer"):
-        process.load("RecoMET.METProducers.MuonTCMETValueMapProducer_cff")
-    process.rerunParticleFlowSequence += process.muonTCMETValueMapProducer
-    
-    # build final particleFlow collection
-    process.rerunParticleFlowSequence += process.particleFlowLinks
+    # configuration for re-running particle-flow sequence taken from
+    #  RecoParticleFlow/Configuration/test/RecoToDisplay_cfg.py
+    loadIfNecessary(process, "RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi", "siPixelRecHits")
+    loadIfNecessary(process, "RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi", "siStripMatchedRecHits")
+    loadIfNecessary(process, "RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff", "particleFlowCluster")
+    loadIfNecessary(process, "RecoEcal.Configuration.RecoEcal_cff.py", "ecalClusters")
+    ##loadIfNecessary(process, "RecoPixelVertexing.Configuration.RecoPixelVertexing_cff", "recopixelvertexing")
+    loadIfNecessary(process, "RecoTracker.Configuration.RecoTracker_cff", "ckftracks")
+    loadIfNecessary(process, "RecoMuon.Configuration.RecoMuonPPonly_cff", "muonrecoComplete")
+    loadIfNecessary(process, "RecoEgamma.Configuration.RecoEgamma_cff", "egammaGlobalReco")
+    loadIfNecessary(process, "RecoParticleFlow.PFTracking.particleFlowTrack_cff", "pfTrackingGlobalReco")
+    loadIfNecessary(process, "RecoEgamma.Configuration.RecoEgamma_cff", "egammaHighLevelRecoPrePF")
+    loadIfNecessary(process, "RecoParticleFlow.Configuration.RecoParticleFlow_cff", "particleFlowReco")
+    loadIfNecessary(process, "RecoEgamma.Configuration.RecoEgamma_cff", "egammaHighLevelRecoPostPF")
+    loadIfNecessary(process, "RecoMuon.Configuration.RecoMuon_cff", "muonshighlevelreco")
+    loadIfNecessary(process, "RecoParticleFlow.Configuration.RecoParticleFlow_cff", "particleFlowLinks")
+    process.printEventContent = cms.EDAnalyzer("EventContentAnalyzer")
+    process.rerunParticleFlowSequence = cms.Sequence(
+        process.siPixelRecHits
+       + process.siStripMatchedRecHits
+       + process.particleFlowCluster
+       + process.ecalClusters
+       ##+ process.recopixelvertexing
+       + process.ckftracks
+       + process.muonrecoComplete
+       + process.egammaGlobalReco
+       + process.pfTrackingGlobalReco
+       + process.egammaHighLevelRecoPrePF
+       + process.particleFlowReco
+       + process.egammaHighLevelRecoPostPF
+       + process.muonshighlevelreco
+       + process.printEventContent
+       + process.particleFlowLinks
+    )
 
     configtools.cloneProcessingSnippet(process, process.rerunParticleFlowSequence, "ForPFMuonCleaning")
     rerunParticleFlowSequenceForPFMuonCleaning = getattr(process, "rerunParticleFlowSequenceForPFMuonCleaning")
 
     # CV: update input tags to run on inputProcess
-    sequenceVisitor = SequenceVisitor(inputProcess)
+    sequenceVisitor = seqVisitorUpdateInputTags(process, inputProcess)
     process.rerunParticleFlowSequenceForPFMuonCleaning.visit(sequenceVisitor)
+
+    # CV: update moduleLabel passed as string parameter to Multi5x5SuperClusterProducer
+    process.multi5x5SuperClustersCleanedForPFMuonCleaning.barrelClusterProducer = cms.string('multi5x5BasicClustersForPFMuonCleaning')
+    process.multi5x5SuperClustersCleanedForPFMuonCleaning.endcapClusterProducer = cms.string('multi5x5BasicClustersCleanedForPFMuonCleaning')
+    process.multi5x5SuperClustersUncleanedForPFMuonCleaning.barrelClusterProducer = cms.string('multi5x5BasicClustersForPFMuonCleaning')
+    process.multi5x5SuperClustersUncleanedForPFMuonCleaning.endcapClusterProducer = cms.string('multi5x5BasicClustersCleanedForPFMuonCleaning')
     # CV: update instanceLabel dynamically composed from moduleLabel by MuonProducer
     process.particleFlowForPFMuonCleaning.Muons = cms.InputTag("muonsForPFMuonCleaning", "muons1stStepForPFMuonCleaning2muonsForPFMuonCleaninsMap")
+    # CV: special handling of modules the configuration of which has been overwritten
+    #     in order to mix collections of objects reconstructed and Z -> mu+ mu- event with simulated tau decay products
+##     if hasattr(process, "trackerDrivenElectronSeeds"):
+##         moduleType = getattr(process, "trackerDrivenElectronSeeds").type_()
+##         if moduleType == "ElectronSeedTrackRefUpdater":
+##             if hasattr(process, "trackerDrivenElectronSeedsORG"):
+##                 trackerDrivenElectronSeedsForPFMuonCleaning = getattr(process, "trackerDrivenElectronSeedsORG").clone(
+##                     PFPSClusterLabel = cms.InputTag("particleFlowClusterPSForPFMuonCleaning"),
+##                     PFHcalClusterLabel = cms.InputTag("particleFlowClusterHCALForPFMuonCleaning"),
+##                     PFEcalClusterLabel = cms.InputTag("particleFlowClusterECALForPFMuonCleaning"),
+##                     TkColList = cms.VInputTag(cms.InputTag("generalTracks"))
+##                 )                
+##             else:
+##                 raise ValueError ("Failed to find module of type = '%s' replacing 'trackerDrivenElectronSeeds' module !!")
+##         elif moduleType == "GoodSeedProducer":
+##             pass
+##         else:
+##             raise ValueError ("Module of name = 'trackerDrivenElectronSeeds' is of invalid type = %s !!" % moduleType)
+##     else:
+##         raise ValueError ("No module of name = 'trackerDrivenElectronSeeds' defined !!")
+
+    process.convLayerPairs.FPix.HitProducer = cms.string('siPixelRecHitsBLAH')
+    process.convLayerPairs.BPix.HitProducer = cms.string('siPixelRecHitsBLAH')
             
     return rerunParticleFlowSequenceForPFMuonCleaning
