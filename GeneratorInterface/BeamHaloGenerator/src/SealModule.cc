c3f39e088cde769f69cba094677dd083f21085e8
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-01 19:11:14 +0000

    Remove unused source.  It is now a producer.

--- GeneratorInterface/BeamHaloGenerator/src/SealModule.cc
+++ GeneratorInterface/BeamHaloGenerator/src/SealModule.cc
@@ -1,12 +1,6 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-#include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "GeneratorInterface/BeamHaloGenerator/interface/BeamHaloSource.h"
 #include "GeneratorInterface/BeamHaloGenerator/interface/BeamHaloProducer.h"
 
-using edm::BeamHaloSource;
 using edm::BeamHaloProducer;
 
-DEFINE_FWK_INPUT_SOURCE(BeamHaloSource);
 DEFINE_FWK_MODULE(BeamHaloProducer);
