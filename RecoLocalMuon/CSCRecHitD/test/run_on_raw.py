8b748a6e82adb4b7c7dc926410cd70a944486ad3
Tim Cox <sha1-4c91a11922f74e8a5f3338bf3414bab331f90be6@cern.ch>
2012-11-01 15:37:53 +0000

    dump some events

---
+++ RecoLocalMuon/CSCRecHitD/test/run_on_raw.py
@@ -0,0 +1,55 @@
+## Dump  10  events in CSC rechit builder - Tim Cox - 01.11.2012
+## This version runs in 610pre4 on a real data RelVal RAW sample.
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+
+process.load("Configuration/StandardSequences/Geometry_cff")
+process.load("Configuration/StandardSequences/MagneticField_cff")
+process.load("Configuration/StandardSequences/FrontierConditions_GlobalTag_cff")
+process.load("Configuration/StandardSequences/RawToDigi_Data_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+# --- MATCH GT TO RELEASE AND DATA SAMPLE
+
+process.GlobalTag.globaltag = 'GR_R_61_V1::All'
+
+# --- NUMBER OF EVENTS --- 
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.options   = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound') )
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.source    = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_6_1_0_pre4-GR_P_V42_RelVal_mu2012C/SingleMu/RAW/v1/00000/F6AA1146-4F15-E211-8EE1-001A92810AD8.root'
+    )
+)
+
+# --- ACTIVATE LogTrace IN CSCRecHitD BUT NEED TO COMPILE IT WITH scram b -j8 USER_CXXFLAGS="-DEDM_ML_DEBUG"
+# LogTrace output goes to cout; all other output to "junk.log"
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.categories.append("CSCRecHit")
+# module label is something like "muonCSCDigis"...
+process.MessageLogger.debugModules = cms.untracked.vstring("*")
+process.MessageLogger.destinations = cms.untracked.vstring("cout","junk")
+process.MessageLogger.cout = cms.untracked.PSet(
+    threshold = cms.untracked.string("DEBUG"),
+    default   = cms.untracked.PSet( limit = cms.untracked.int32(0)  ),
+    FwkReport = cms.untracked.PSet( limit = cms.untracked.int32(-1) ),
+    CSCRecHit = cms.untracked.PSet( limit = cms.untracked.int32(-1) )
+)
+
+#process.p = cms.Path(process.muonCSCDigis * process.csc2DRecHits * process.cscSegments * process.cscValidation)
+
+# Path and EndPath def
+process.unpack = cms.Path(process.muonCSCDigis)
+process.reco = cms.Path(process.csc2DRecHits)
+process.endjob = cms.EndPath(process.endOfProcess)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.unpack, process.reco, process.endjob)
+
