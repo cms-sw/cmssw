c4422fd2a1fbe2423c47786c21a34372936a4032
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-01 19:30:22 +0000

    Obsolete source, obsolete package

--- IOMC/NtupleConverter/src/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "IOMC/NtupleConverter/interface/H2RootNtplSource.h"
-
-  
-  using edm::H2RootNtplSource;
-  DEFINE_FWK_INPUT_SOURCE(H2RootNtplSource);
