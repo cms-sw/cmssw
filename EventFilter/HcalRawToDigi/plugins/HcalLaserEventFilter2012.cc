30d9965a6b56865c82d0bf10d47a5696ed612d6b
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2012-11-07 09:24:49 +0000

    Read event file in filter and speedup of search

--- EventFilter/HcalRawToDigi/plugins/HcalLaserEventFilter2012.cc
+++ EventFilter/HcalRawToDigi/plugins/HcalLaserEventFilter2012.cc
@@ -39,7 +39,7 @@
 
 #include "DataFormats/Provenance/interface/EventID.h"
 #include "DataFormats/Provenance/interface/RunID.h"
-
+#include "zlib.h"
 
 //
 // class declaration
@@ -62,7 +62,13 @@ private:
   virtual bool beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
   virtual bool endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
 
+  void readEventListFile(const std::string & eventFileName);
+  void addEventString(const std::string & eventString);
+
  // ----------member data ---------------------------
+  typedef std::vector< std::string > strVec;
+  typedef std::vector< std::string >::iterator strVecI;
+  
   std::vector< std::string > EventList_;  // vector of strings representing bad events, with each string in "run:LS:event" format
   bool verbose_;  // if set to true, then the run:LS:event for any event failing the cut will be printed out
   std::string prefix_;  // prefix will be printed before any event if verbose mode is true, in order to make searching for events easier
@@ -84,6 +90,8 @@ private:
 //
 // static data member definitions
 //
+using namespace std;
+#define CHUNK 16384
 
 //
 // constructors and destructor
@@ -99,56 +107,111 @@ HcalLaserEventFilter2012::HcalLaserEventFilter2012(const edm::ParameterSet& ps)
     outfile_.open("badHcalLaserList_eventfilter.txt");
   forceFilterTrue_=ps.getUntrackedParameter<bool>("forceFilterTrue",false);
 
-  std::vector<std::string> dummylist;
-  dummylist.clear();
-  std::vector<std::string> templist;
-  templist=ps.getUntrackedParameter<std::vector<std::string> >("EventList",dummylist); // get list of events
-  
-  // Loop through list of bad events, and if run is in allowed range, add bad event to EventList
-  for (unsigned int i=0;i<templist.size();++i)
-    {
-      int run=0;
-      unsigned int ls=0;
-      unsigned int event=0;
-      // Check that event list object is in correct form
-      size_t found = templist[i].find(":");  // find first colon
-      if (found!=std::string::npos)
-	run=atoi((templist[i].substr(0,found)).c_str());  // convert to run
-      else
-	{
-	  edm::LogError("HcalLaserHFFilter2012")<<"  Unable to parse Event list input '"<<templist[i]<<"' for run number!";
-	  continue;
-	}
-      size_t found2 = templist[i].find(":",found+1);  // find second colon
-      if (found2!=std::string::npos)
-	{
-	  /// Some event numbers are less than 0?  \JetHT\Run2012C-v1\RAW:201278:2145:-2130281065  -- due to events being dumped out as ints, not uints!
-	  ls=atoi((templist[i].substr(found+1,(found2-found-1))).c_str());  // convert to ls
-	  event=atoi((templist[i].substr(found2+1)).c_str()); // convert to event
-	  /// Some event numbers are less than 0?  \JetHT\Run2012C-v1\RAW:201278:2145:-2130281065
-	  if (ls==0 || event==0) edm::LogWarning("HcalLaserHFFilter2012")<<"  Strange lumi, event numbers for input '"<<templist[i]<<"'";
-	}
-      else
-	{
-	  edm::LogError("HcalLaserHFFilter2012")<<"Unable to parse Event list input '"<<templist[i]<<"' for run number!";
-	  continue;
-	}
-      // If necessary, check that run is within allowed range
-      if (minrun_>-1 && run<minrun_)
-	{
-	  if (verbose_)  edm::LogInfo("HcalLaserHFFilter2012") <<"Skipping Event list input '"<<templist[i]<<"' because it is less than minimum run # "<<minrun_;
-	  continue;
-	}
-      if (maxrun_>-1 && run>maxrun_)
-	{
-	  if (verbose_) edm::LogInfo("HcalLaserHFFilter2012") <<"Skipping Event list input '"<<templist[i]<<"' because it is greater than maximum run # "<<maxrun_;
-	  continue;
-	}
-      // Now add event to Event List
-      EventList_.push_back(templist[i]);
-    }
+  string eventFileName=ps.getParameter<string>("eventFileName");
+  if (verbose_) edm::LogInfo("HcalLaserHFFilter2012") << "HCAL laser event list from file "<<eventFileName;
+  readEventListFile(eventFileName);
+  std::sort(EventList_.begin(), EventList_.end());
   if (verbose_) edm::LogInfo("HcalLaserHFFilter2012")<<" A total of "<<EventList_.size()<<" listed HCAL laser events found in given run range";
+}
 
+void HcalLaserEventFilter2012::addEventString(const string & eventString)
+{
+  // Loop through list of bad events, and if run is in allowed range, add bad event to EventList
+  int run=0;
+  unsigned int ls=0;
+  unsigned int event=0;
+  // Check that event list object is in correct form
+  size_t found = eventString.find(":");  // find first colon
+  if (found!=std::string::npos)
+    run=atoi((eventString.substr(0,found)).c_str());  // convert to run
+  else
+    {
+      edm::LogError("HcalLaserHFFilter2012")<<"  Unable to parse Event list input '"<<eventString<<"' for run number!";
+      return;
+    }
+  size_t found2 = eventString.find(":",found+1);  // find second colon
+  if (found2!=std::string::npos)
+    {
+      /// Some event numbers are less than 0?  \JetHT\Run2012C-v1\RAW:201278:2145:-2130281065  -- due to events being dumped out as ints, not uints!
+      ls=atoi((eventString.substr(found+1,(found2-found-1))).c_str());  // convert to ls
+      event=atoi((eventString.substr(found2+1)).c_str()); // convert to event
+      /// Some event numbers are less than 0?  \JetHT\Run2012C-v1\RAW:201278:2145:-2130281065
+      if (ls==0 || event==0) edm::LogWarning("HcalLaserHFFilter2012")<<"  Strange lumi, event numbers for input '"<<eventString<<"'";
+    }
+  else
+    {
+      edm::LogError("HcalLaserHFFilter2012")<<"Unable to parse Event list input '"<<eventString<<"' for run number!";
+      return;
+    }
+  // If necessary, check that run is within allowed range
+  if (minrun_>-1 && run<minrun_)
+    {
+      if (verbose_)  edm::LogInfo("HcalLaserHFFilter2012") <<"Skipping Event list input '"<<eventString<<"' because it is less than minimum run # "<<minrun_;
+      return;
+    }
+  if (maxrun_>-1 && run>maxrun_)
+    {
+      if (verbose_) edm::LogInfo("HcalLaserHFFilter2012") <<"Skipping Event list input '"<<eventString<<"' because it is greater than maximum run # "<<maxrun_;
+      return;
+    }
+  // Now add event to Event List
+  EventList_.push_back(eventString);
+}
+
+#define LENGTH 0x2000
+
+void HcalLaserEventFilter2012::readEventListFile(const string & eventFileName)
+{
+  gzFile  file = gzopen (eventFileName.c_str(), "r");
+  if (! file) {
+    edm::LogError("HcalLaserHFFilter2012")<<"  Unable to open event list file "<<eventFileName;
+    return;
+  }
+  string b2;
+  int err;                    
+  int bytes_read;
+  char buffer[LENGTH];
+  unsigned int i;
+  char * pch;
+
+  while (1) {
+    bytes_read = gzread (file, buffer, LENGTH - 1);
+    buffer[bytes_read] = '\0';
+    i=0;
+    pch = strtok (buffer,"\n");
+    if (buffer[0] == '\n' ) {
+      addEventString(b2);
+      ++i;
+    } else b2+=pch;
+  
+    while (pch != NULL)
+    {
+      i+=strlen(pch)+1;
+      if (i>b2.size()) b2= pch;
+      if (i==(LENGTH-1)) {
+	 if ((buffer[LENGTH-2] == '\n' )|| (buffer[LENGTH-2] == '\0' )){
+          addEventString(b2);
+          b2="";
+	}
+      } else if (i<LENGTH) {
+	addEventString(b2);
+      } 
+      pch = strtok (NULL, "\n");
+    }
+    if (bytes_read < LENGTH - 1) {
+      if (gzeof (file)) break;
+        else {
+          const char * error_string;
+          error_string = gzerror (file, & err);
+          if (err) {
+	    edm::LogError("HcalLaserHFFilter2012")<<"Error while reading gzipped file:  "<<error_string;
+            return;
+          }
+        }
+    }
+  }
+  gzclose (file);
+  return;
 }
 
 
@@ -179,8 +242,8 @@ HcalLaserEventFilter2012::filter(edm::Event& iEvent, const edm::EventSetup& iSet
   thisevent<<run<<":"<<iEvent.luminosityBlock()<<":"<<iEvent.id().event();
 
   // Event not found in bad list; it is a good event
-  if (std::find(EventList_.begin(), EventList_.end(), thisevent.str())==EventList_.end())
-    return true;
+  strVecI it = std::lower_bound(EventList_.begin(), EventList_.end(), thisevent.str());
+  if (it == EventList_.end() || thisevent.str() < *it) return true;
   // Otherwise, this is a bad event
   // if verbose, dump out event info
   // Dump out via cout, or via LogInfo?  For now, use cout
