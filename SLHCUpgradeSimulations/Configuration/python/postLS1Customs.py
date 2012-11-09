42dfefbe77de5650b689a8aa6105b4dca28dc252
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2012-11-09 14:12:23 +0000

    take one on reco customs

--- SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 from muonCustoms import customise_csc_geom_cond_digi
 
-def digiCustoms(process):
+def turnOffXFrame(process):
     #turn off the crossing frame
     process.mix.mixObjects.mixSH.crossingFrames = cms.untracked.vstring(
         'BSCHits',
@@ -20,20 +20,44 @@ def digiCustoms(process):
     process.mix.mixObjects.mixHepMC.makeCrossingFrame = cms.untracked.bool(False)
     process.digitisation_step.remove(process.simSiStripDigiSimLink)
     process.digitisation_step.remove(process.mergedtruth)
-   
+    return process
+    
+def digiCustoms(process):
+    process=turnOffXFrame(process)
+    
     #deal with csc
     process=customise_csc_geom_cond_digi(process)
 
     #extend the event content
     if hasattr(process,'FEVTDEBUGoutput'):
         process.FEVTDEBUGoutput.outputCommands.append( 'keep *_simMuonCSCDigis_*_*')
+        process.FEVTDEBUGoutput.outputCommands.append( 'keep *_simMuonRPCDigis_*_*')
         process.FEVTDEBUGoutput.outputCommands.append( 'keep *_simHcalUnsuppressedDigis_*_*')
     if hasattr(process,'GENRAWoutput'):
         print 'ok'
         process.GENRAWoutput.outputCommands.append( 'keep *_simMuonCSCDigis_*_*')
+        process.GENRAWoutput.outputCommands.append( 'keep *_simMuonRPCDigis_*_*')
         process.GENRAWoutput.outputCommands.append( 'keep *_simHcalUnsuppressedDigis_*_*')
-                                                   
 
     process.digi2raw_step.remove(process.cscpacker)
+
+    return process
+
+def recoCustoms(process):
+
+    # ME1/1A is  u n g a n g e d  Post-LS1
+
+    process.CSCGeometryESModule.useGangedStripsInME1a = False
+
+    # Turn off some flags for CSCRecHitD that are turned ON in default config
+
+    process.csc2DRecHits.readBadChannels = cms.bool(False)
+    process.csc2DRecHits.CSCUseTimingCorrections = cms.bool(False)
+    process.csc2DRecHits.CSCUseGasGainCorrection = cms.bool(False)
+
+    # Switch input for CSCRecHitD to  s i m u l a t e d  digis
+
+    process.csc2DRecHits.wireDigiTag  = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi")
+    process.csc2DRecHits.stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi")
 
     return process
