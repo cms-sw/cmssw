1ba6333a9e4d996b9960c54a9f8fae462cac54a0
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-11-11 10:35:19 +0000

    if we use this in RECO, it has to be not a filter; make a clone or override this for analysis if really necessary

--- CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
+++ CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
    src = cms.InputTag("offlinePrimaryVertices"),
    cut = cms.string("!isFake && ndof >= 4.0 && abs(z) <= 24.0 && abs(position.Rho) <= 2.0"),
-   filter = cms.bool(True)
+   filter = cms.bool(False)
 )
 
