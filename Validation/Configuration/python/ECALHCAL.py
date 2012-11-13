2aa9c4484d0c2a134593c22c6c070bfd22800201
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-11-13 19:49:53 +0000

    ecalClusters now includes PF clustering, use non-PF sequence here

--- Validation/Configuration/python/ECALHCAL.py
+++ Validation/Configuration/python/ECALHCAL.py
@@ -57,7 +57,7 @@ def customise(process):
 
 #    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalLocalRecoSequence * process.hbhereco * process.hfreco * process.horeco * (process.ecalClusters+process.caloTowersRec) * process.reducedEcalRecHitsSequence )
 
-    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalPacker * process.esDigiToRaw * process.hcalRawData * process.rawDataCollector * process.ecalDigis * process.ecalPreshowerDigis * process.hcalDigis * process.calolocalreco *(process.ecalClusters+process.caloTowersRec) * process.reducedEcalRecHitsSequence )
+    process.local_digireco = cms.Path(process.mix * process.calDigi * process.ecalPacker * process.esDigiToRaw * process.hcalRawData * process.rawDataCollector * process.ecalDigis * process.ecalPreshowerDigis * process.hcalDigis * process.calolocalreco *(process.ecalClustersNoPFBox+process.caloTowersRec) * process.reducedEcalRecHitsSequence )
 
     process.schedule.append(process.local_digireco)
 
