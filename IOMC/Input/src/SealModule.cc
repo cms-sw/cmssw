8bce7be3cad486c1c4311a681c9774b8a31af0f4
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-13 22:52:27 +0000

    Remove unnecessary #includes

--- IOMC/Input/src/SealModule.cc
+++ IOMC/Input/src/SealModule.cc
@@ -1,10 +1,8 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
 #include "IOMC/Input/interface/MCFileSource.h"
 
 
-  using edm::MCFileSource; 
+using edm::MCFileSource; 
   
-  DEFINE_FWK_INPUT_SOURCE(MCFileSource);
+DEFINE_FWK_INPUT_SOURCE(MCFileSource);
 
