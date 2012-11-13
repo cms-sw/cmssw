3ad8a9338df5c3e9a5c2bb7714d7c203e4f750bd
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2012-11-13 16:29:17 +0000

    Have test read a RAW data file rather than MC and force system to do a slow copy to guarantee serialization compatibility

--- Utilities/ReleaseScripts/scripts/read312RV_cfg.py
+++ Utilities/ReleaseScripts/scripts/read312RV_cfg.py
@@ -1,17 +1,15 @@
 
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Rec")
+process = cms.Process("READTEST")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
 process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-                  'rfio:/castor/cern.ch/cms/store/relval/compatibility_ref/CMSSW_3_1_2/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/MC_31X_V3-v1/0007/DAD33187-9078-DE11-9ABE-0019B9F709A4.root'
+                 '/store/data/Run2010A/MinimumBias/RAW/v1/000/140/386/02FD0631-A092-DF11-9E12-001D09F290CE.root'
     )
 )
 
@@ -19,16 +17,11 @@ process.options = cms.untracked.PSet( Rethrow = cms.untracked.vstring('ProductNo
 
 # output module
 #
-process.load("Configuration.EventContent.EventContentCosmics_cff")
 
 process.out = cms.OutputModule("PoolOutputModule",
-    process.FEVTEventContent,
-    fileName = cms.untracked.string('merge.root')
+                               fastCloning=cms.untracked.bool(False),
+                               fileName = cms.untracked.string('merge.root')
 )
-
-# process.source.inputCommands = cms.untracked.vstring(
-#          'keep *',
-#          'drop *_particleFlowBlock_*_*' )
 
 process.outpath = cms.EndPath(process.out)
 
