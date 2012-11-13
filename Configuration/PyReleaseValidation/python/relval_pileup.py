5300faa792acea56964efe48dfe215fea2e7cfcd
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2012-11-13 12:04:05 +0000

    add the Ztautau to pileup set

--- Configuration/PyReleaseValidation/python/relval_pileup.py
+++ Configuration/PyReleaseValidation/python/relval_pileup.py
@@ -14,7 +14,7 @@ workflows[201]=['',['ZmumuJets_Pt_20_300','DIGIPU1','RECOPU1']]
 workflows[202]=['',['TTbar','DIGIPU1','RECOPU1']]
 workflows[203]=['',['H130GGgluonfusion','DIGIPU1','RECOPU1']]
 workflows[204]=['',['QQH1352T_Tauola','DIGIPU1','RECOPU1']]
-
+workflows[205]=['',['ZTT','DIGIPU1','RECOPU1']]
 
 #heavy ions tests
 workflows[300]=['Pyquen_GammaJet_pt20_2760GeV',['HydjetQ_MinBias_2760GeVINPUT','Pyquen_GammaJet_pt20_2760GeV','DIGIHISt3','RECOHISt4']]
