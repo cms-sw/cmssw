ac8e70a659d15f255943aa8ab160ca38a9790c85
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-14 23:46:28 +0000

    Remove unused source CosMuoGenSource

--- GeneratorInterface/CosmicMuonGenerator/src/SealModule.cc
+++ GeneratorInterface/CosmicMuonGenerator/src/SealModule.cc
@@ -1,14 +1,9 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "GeneratorInterface/CosmicMuonGenerator/interface/CosMuoGenSource.h"
 #include "GeneratorInterface/CosmicMuonGenerator/interface/CosMuoGenProducer.h"
 #include "GeneratorInterface/CosmicMuonGenerator/interface/CosMuoGenEDFilter.h"
 
-using edm::CosMuoGenSource;
 using edm::CosMuoGenProducer;
 using edm::CosMuoGenEDFilter;
 
-DEFINE_FWK_INPUT_SOURCE(CosMuoGenSource);
 DEFINE_FWK_MODULE(CosMuoGenProducer);
 DEFINE_FWK_MODULE(CosMuoGenEDFilter);
