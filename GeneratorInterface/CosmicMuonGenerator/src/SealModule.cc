2da9711697572087f5b06e5498f1c0fc340423a0
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-14 23:59:00 +0000

    Restore the EDFilter

--- GeneratorInterface/CosmicMuonGenerator/src/SealModule.cc
+++ GeneratorInterface/CosmicMuonGenerator/src/SealModule.cc
@@ -1,6 +1,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "GeneratorInterface/CosmicMuonGenerator/interface/CosMuoGenProducer.h"
+#include "GeneratorInterface/CosmicMuonGenerator/interface/CosMuoGenEDFilter.h"
 
 using edm::CosMuoGenProducer;
+using edm::CosMuoGenEDFilter;
 
 DEFINE_FWK_MODULE(CosMuoGenProducer);
+DEFINE_FWK_MODULE(CosMuoGenEDFilter);
