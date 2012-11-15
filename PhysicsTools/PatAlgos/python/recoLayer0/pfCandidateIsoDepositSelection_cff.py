e4673c9110b4c249210c9ea4b888b879eec87388
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-11-15 16:33:06 +0000

    use Ptrs with pf{No,}PileUpIso modules/projectors

--- PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from CommonTools.ParticleFlow.ParticleSelectors.pfSortByType_cff import *
 from CommonTools.ParticleFlow.pfNoPileUpIso_cff  import *
 
-pfPileUpIso.PFCandidates = 'particleFlow'
-pfNoPileUpIso.bottomCollection='particleFlow'
+pfPileUpIso.PFCandidates = 'particleFlowPtrs'
+pfNoPileUpIso.bottomCollection='particleFlowPtrs'
 
 patPFCandidateIsoDepositSelection = cms.Sequence(
        pfNoPileUpIsoSequence +
