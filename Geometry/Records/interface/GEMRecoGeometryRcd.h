a388382385f6dd843aa937a5ef9eae67d7e4add8
Marcello Maggi <sha1-f5c5b039b8392b25aa577ef29c9af39f83b1c981@cern.ch>
2012-11-15 22:57:49 +0000

    Add GEM Geometry records

---
+++ Geometry/Records/interface/GEMRecoGeometryRcd.h
@@ -0,0 +1,6 @@
+#ifndef Geometry_GEMRecoGeometryRcd_H
+#define Geometry_GEMRecoGeometryRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class GEMRecoGeometryRcd : public edm::eventsetup::EventSetupRecordImplementation<GEMRecoGeometryRcd> {};
+#endif
