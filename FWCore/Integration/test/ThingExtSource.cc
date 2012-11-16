c12505444b9cd940595f36ebfc126409e3d633e5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-16 20:51:11 +0000

    Restore test for input source from files

---
+++ FWCore/Integration/test/ThingExtSource.cc
@@ -0,0 +1,97 @@
+#include "FWCore/Integration/test/ThingExtSource.h"
+#include "DataFormats/TestObjects/interface/ThingCollection.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+
+namespace edmtest {
+  ThingExtSource::ThingExtSource(edm::ParameterSet const& pset, edm::InputSourceDescription const& desc) :
+    ProducerSourceFromFiles(pset, desc, true), alg_() {
+    produces<ThingCollection>();
+    produces<ThingCollection, edm::InLumi>("beginLumi");
+    produces<ThingCollection, edm::InLumi>("endLumi");
+    produces<ThingCollection, edm::InRun>("beginRun");
+    produces<ThingCollection, edm::InRun>("endRun");
+  }
+
+  // Virtual destructor needed.
+  ThingExtSource::~ThingExtSource() { }  
+
+  // Functions that gets called by framework every event
+  bool ThingExtSource::produce(edm::Event& e) {
+
+    // Fake running out of data for an external input source.
+    if (event() > 2) return false;
+
+    // Step A: Get Inputs 
+
+    // Step B: Create empty output 
+    std::auto_ptr<ThingCollection> result(new ThingCollection);  //Empty
+
+    // Step C: Invoke the algorithm, passing in inputs (NONE) and getting back outputs.
+    alg_.run(*result);
+
+    // Step D: Put outputs into event
+    e.put(result);
+
+    return true;
+  }
+
+  // Functions that gets called by framework every luminosity block
+  void ThingExtSource::beginLuminosityBlock(edm::LuminosityBlock& lb) {
+    // Step A: Get Inputs 
+
+    // Step B: Create empty output 
+    std::auto_ptr<ThingCollection> result(new ThingCollection);  //Empty
+
+    // Step C: Invoke the algorithm, passing in inputs (NONE) and getting back outputs.
+    alg_.run(*result);
+
+    // Step D: Put outputs into lumi block
+    lb.put(result, "beginLumi");
+  }
+
+  void ThingExtSource::endLuminosityBlock(edm::LuminosityBlock& lb) {
+    // Step A: Get Inputs 
+
+    // Step B: Create empty output 
+    std::auto_ptr<ThingCollection> result(new ThingCollection);  //Empty
+
+    // Step C: Invoke the algorithm, passing in inputs (NONE) and getting back outputs.
+    alg_.run(*result);
+
+    // Step D: Put outputs into lumi block
+    lb.put(result, "endLumi");
+  }
+
+  // Functions that gets called by framework every run
+  void ThingExtSource::beginRun(edm::Run& r) {
+    // Step A: Get Inputs 
+
+    // Step B: Create empty output 
+    std::auto_ptr<ThingCollection> result(new ThingCollection);  //Empty
+
+    // Step C: Invoke the algorithm, passing in inputs (NONE) and getting back outputs.
+    alg_.run(*result);
+
+    // Step D: Put outputs into event
+    r.put(result, "beginRun");
+  }
+
+  void ThingExtSource::endRun(edm::Run& r) {
+    // Step A: Get Inputs 
+
+    // Step B: Create empty output 
+    std::auto_ptr<ThingCollection> result(new ThingCollection);  //Empty
+
+    // Step C: Invoke the algorithm, passing in inputs (NONE) and getting back outputs.
+    alg_.run(*result);
+
+    // Step D: Put outputs into event
+    r.put(result, "endRun");
+  }
+
+}
+using edmtest::ThingExtSource;
+DEFINE_FWK_INPUT_SOURCE(ThingExtSource);
