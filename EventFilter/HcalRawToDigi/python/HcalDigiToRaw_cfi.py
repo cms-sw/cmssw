d667e40088813e2f5dfb5adab0c9c4d2cc70a07a
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2012-11-16 15:21:41 +0000

    adding TPs to rawData

--- EventFilter/HcalRawToDigi/python/HcalDigiToRaw_cfi.py
+++ EventFilter/HcalRawToDigi/python/HcalDigiToRaw_cfi.py
@@ -4,7 +4,8 @@ hcalRawData = cms.EDProducer("HcalDigiToRaw",
     HBHE = cms.untracked.InputTag("simHcalDigis"),
     HF = cms.untracked.InputTag("simHcalDigis"),
     HO = cms.untracked.InputTag("simHcalDigis"),
-    ZDC = cms.untracked.InputTag("simHcalUnsuppressedDigis")                       
+    ZDC = cms.untracked.InputTag("simHcalUnsuppressedDigis"),
+    TRIG =  cms.untracked.InputTag("simHcalTriggerPrimitiveDigis")
 )
 
 
