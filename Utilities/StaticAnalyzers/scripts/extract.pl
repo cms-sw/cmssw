770f4ed313746250d36f861ca9794571667f3e90
Patrick Gartung <sha1-33476957b06a1bc61e5124243258241c72d1e027@cern.ch>
2012-11-16 20:54:24 +0000

    added extract.pl script to extract class names from nested, comma-delimited template arguments

---
+++ Utilities/StaticAnalyzers/scripts/extract.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use Text::Balanced qw/:ALL/;
+use strict;
+use warnings;
+use Data::Dumper;
+use Scalar::Util 'blessed';
+my @queue =<>;
+
+while  (@queue){
+my $string=shift @queue;
+$string =~ s/\&gt\;/\>/g;
+$string =~ s/\&lt\;/\</g;
+my @fields = extract_multiple($string,
+	[
+	{ B => sub { extract_bracketed($_[0],'<>','[^<>]++') } },
+	]);
+
+#print "\nshift: $string\n";
+#print Dumper(\@fields);
+foreach (@fields) {	
+	if ( defined blessed($_) )
+		{
+		my $temp = $$_;
+		$temp =~ s/<//;
+		$temp =~ s/>//;
+#		print "unshift: $temp\n";
+		unshift (@queue, $temp); 
+		}
+	else
+		{$,="\n"; print split (',', $_), "\n" }; 
+	}
+}
