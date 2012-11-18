8e147e5edea2c3ee17efe0a6e4d3f553986975c1
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2012-11-18 06:17:47 +0000

    rename a few process name

--- Validation/HcalHits/python/SimHitsPostProcessor_cff.py
+++ Validation/HcalHits/python/SimHitsPostProcessor_cff.py
@@ -2,4 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.HcalHits.hcalSimHitsClient_cfi import *
 
-SimHitsPostProcessor = cms.Sequence(hcalsimhitsClient)
+hcalSimHitsPostProcessor = cms.Sequence(hcalsimhitsClient)
--- Validation/HcalHits/python/SimHitsValidationSequence_cff.py
+++ Validation/HcalHits/python/SimHitsValidationSequence_cff.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Validation.HcalHits.SimHitsValidationHcal_cfi import *
 import Validation.HcalHits.SimHitsValidationHcal_cfi
+
 AllSimHitsValidation = Validation.HcalHits.SimHitsValidationHcal_cfi.simHitsValidationHcal.clone()
-SimHitsValidationSequence = cms.Sequence(AllSimHitsValidation)
+
+hcalSimHitsValidationSequence = cms.Sequence(AllSimHitsValidation)
