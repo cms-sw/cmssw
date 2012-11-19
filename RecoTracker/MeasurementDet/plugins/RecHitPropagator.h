aa6354a88c116663ec5adb722bc53705b17c1c94
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-11-19 09:53:59 +0000

    cleanup

--- RecoTracker/MeasurementDet/plugins/RecHitPropagator.h
+++ RecoTracker/MeasurementDet/plugins/RecHitPropagator.h
@@ -27,6 +27,7 @@ TrajectoryStateOnSurface fastProp(const TrajectoryStateOnSurface& ts, const Plan
   LocalVector ldir = tPlane.toLocal(gdir);  // fast prop!
   LocalPoint lPos = tPlane.toLocal( ts.globalPosition());
   LocalPoint projectedPos = lPos - ldir * delta/ldir.z();
+  // we can also patch it up as only the position-errors are used...
   GlobalTrajectoryParameters gp(tPlane.toGlobal(projectedPos),gdir,ts.charge(), &ts.globalParameters().magneticField());
   if (ts.hasError())
     return TrajectoryStateOnSurface(gp,ts.curvilinearError(),tPlane);
