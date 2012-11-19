caa088fba524e5ce0bd167e3ae701e8f9fc47b11
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-11-19 21:01:33 +0000

    Remove include of obsolete header

--- L1Trigger/GlobalMuonTrigger/src/SealModule.cc
+++ L1Trigger/GlobalMuonTrigger/src/SealModule.cc
@@ -1,10 +1,7 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "L1Trigger/GlobalMuonTrigger/src/L1MuGMTHWFileReader.h"
 #include "L1Trigger/GlobalMuonTrigger/interface/L1MuGlobalMuonTrigger.h"
 
-
-  
-  DEFINE_FWK_INPUT_SOURCE(L1MuGMTHWFileReader);
-  DEFINE_FWK_MODULE(L1MuGlobalMuonTrigger);
+DEFINE_FWK_INPUT_SOURCE(L1MuGMTHWFileReader);
+DEFINE_FWK_MODULE(L1MuGlobalMuonTrigger);
