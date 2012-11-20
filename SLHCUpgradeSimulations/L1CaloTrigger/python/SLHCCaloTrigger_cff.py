f49d4ee85a512a93cc8f3b715567ac32f9c6a070
Robyn Elizabeth Lucas <sha1-50a5c5d4db958b6d2aae3d7834fc100bfb31fa95@cern.ch>
2012-11-20 13:27:35 +0000

    Added jet filters

--- SLHCUpgradeSimulations/L1CaloTrigger/python/SLHCCaloTrigger_cff.py
+++ SLHCUpgradeSimulations/L1CaloTrigger/python/SLHCCaloTrigger_cff.py
@@ -3,15 +3,16 @@ import FWCore.ParameterSet.Config as cms
 from SLHCUpgradeSimulations.L1CaloTrigger.SLHCCaloTrigger_cfi import *
 
 SLHCCaloTrigger  = cms.Sequence(L1CaloTowerProducer+
-							#	L1RingSubtractionProducer+
+                                #	L1RingSubtractionProducer+
                                 L1CaloRegionProducer+
                                 L1CaloClusterProducer+
                                 L1CaloClusterFilter+
                                 L1CaloClusterIsolator+
                                 L1CaloJetProducer+
                                 L1CaloJetFilter+
-                                L1CaloJetExpander+
-								L1TowerJetProducer+
+                                L1TowerJetProducer+
+                                L1TowerJetFilter1D+
+                                L1TowerJetFilter2D+
                                 rawSLHCL1ExtraParticles+
                                 SLHCL1ExtraParticles+
                                 l1extraParticlesCalibrated
