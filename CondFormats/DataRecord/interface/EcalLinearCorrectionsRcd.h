5aab1befbcea0581dd3e96fc14ef3999f39ca812
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2012-11-21 16:59:38 +0000

    added EcalLinearCorrectionsRcd

---
+++ CondFormats/DataRecord/interface/EcalLinearCorrectionsRcd.h
@@ -0,0 +1,6 @@
+#ifndef CondFormats_DataRecord_EcalLinearCorrectionsRcd_H
+#define CondFormats_DataRecord_EcalLinearCorrectionsRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class  EcalLinearCorrectionsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalLinearCorrectionsRcd> {};
+#endif
---
+++ CondFormats/DataRecord/src/EcalLinearCorrectionsRcd.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/DataRecord/interface/EcalLinearCorrectionsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(EcalLinearCorrectionsRcd);
