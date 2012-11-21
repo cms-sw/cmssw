9bffc2afc193f150d55f9c87f1c9787ea3a95482
Matthias Geisler <sha1-ca4f0a71d5bbadf63f826def6c188008b2bb8317@cern.ch>
2012-11-21 09:42:35 +0000

    Updated to multiple associations and to different types of associations

--- CommonTools/RecoUtils/interface/PF_PU_AssoMap.h
+++ CommonTools/RecoUtils/interface/PF_PU_AssoMap.h
@@ -1,7 +1,11 @@
 #ifndef PF_PU_AssoMap_h
 #define PF_PU_AssoMap_h
 
-
+// -*- C++ -*-
+//
+// Package:    PF_PU_AssoMap
+// Class:      PF_PU_AssoMap
+// 
 /**\class PF_PU_AssoMap PF_PU_AssoMap.cc CommonTools/RecoUtils/plugins/PF_PU_AssoMap.cc
 
  Description: Produces a map with association between tracks and their particular most probable vertex with a quality of this association
@@ -9,13 +13,23 @@
 */
 //
 // Original Author:  Matthias Geisler,32 4-B20,+41227676487,
+// $Id$
+//
 //
 
-#include <string>
 
+// system include files
+#include <memory>
+
+// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "FWCore/Utilities/interface/InputTag.h"
 
 #include "CommonTools/RecoUtils/interface/PF_PU_AssoMapAlgos.h"
@@ -24,7 +38,7 @@
 // class declaration
 //
 
-class PF_PU_AssoMap : public edm::EDProducer, private PF_PU_AssoMapAlgos {
+class PF_PU_AssoMap : public edm::EDProducer, public PF_PU_AssoMapAlgos {
    public:
       explicit PF_PU_AssoMap(const edm::ParameterSet&);
       ~PF_PU_AssoMap();
@@ -36,8 +50,9 @@ class PF_PU_AssoMap : public edm::EDProducer, private PF_PU_AssoMapAlgos {
 
       // ----------member data ---------------------------
 
-      edm::InputTag input_TrackCollection_;
+      edm::InputTag input_AssociationType_;
 
+      edm::InputTag input_TrackCollection_;
 };
 
 
