e0f8f701790ae88aa0a7e2d47175c4ada249f504
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2012-11-21 17:01:39 +0000

    first insert

---
+++ CondTools/Ecal/interface/EcalLinearCorrectionsXMLTranslator.h
@@ -0,0 +1,24 @@
+/**
+   Translates a EcalLinearCorrections record to XML
+   and vice versa   
+
+   \author Stefano ARGIRO
+   \version $Id: EcalLinearCorrectionsXMLTranslator.h,v 1.3 2009/06/30 16:15:16 argiro Exp $
+   \date 20 Jun 2008
+*/
+
+#ifndef __EcalLinearCorrectionsXMLTranslator_h_
+#define __EcalLinearCorrectionsXMLTranslator_h_
+
+#include "CondTools/Ecal/interface/EcalFloatCondObjectContainerXMLTranslator.h"
+
+typedef EcalFloatCondObjectContainerXMLTranslator  EcalLinearCorrectionsXMLTranslator;
+
+
+#endif // __EcalLinearCorrectionsXMLTranslator_h_
+
+// Configure (x)emacs for this file ...
+// Local Variables:
+// mode:c++
+// compile-command: "cd ..; scram b"
+// End:
