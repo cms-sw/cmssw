4f20f515e7705bb5ff0a0178ff234a5d78bad2f3
Eike Schlieckau <sha1-fc2fe05d7bf2eff84264ececb7f974ab67c284cc@cern.ch>
2012-11-21 09:50:13 +0000

    added function to clone the ttFullHadEvent including all hypotheses and the needed renaming of modules, inputtags etc.; removal of kGenMatch now removes the genMatch of all (cloned) ttFullHadEvents

--- TopQuarkAnalysis/TopEventProducers/python/sequences/ttFullHadEvtBuilder_cff.py
+++ TopQuarkAnalysis/TopEventProducers/python/sequences/ttFullHadEvtBuilder_cff.py
@@ -12,9 +12,10 @@ from TopQuarkAnalysis.TopEventProducers.sequences.ttFullHadEvtHypotheses_cff imp
 from TopQuarkAnalysis.TopEventProducers.producers.TtFullHadEvtBuilder_cfi import *
 
 ## make ttFullHadEvent
-makeTtFullHadEvent = cms.Sequence(makeTtFullHadHypotheses *
-                                  ttFullHadEvent
-                                  )
+makeTtFullHadEventBase = cms.Sequence(makeTtFullHadHypotheses *
+                                      ttFullHadEvent
+                                      )
+makeTtFullHadEvent = cms.Sequence(makeTtFullHadEventBase)
 
 
 ################################################################################
@@ -28,32 +29,56 @@ def addTtFullHadHypotheses(process,
                            ):
 
     ## edit list of input hypotheses for the TtFullHadEventBuilder
-    labels =  getattr(process.ttFullHadEvent, "hypotheses")
-    for obj in range(len(names)):
+    labels = getattr(process.ttFullHadEvent, "hypotheses")
+    for obj in names:
         ## create correct label from HypoClassKey string (stripping the leading "k")
         ## e.g. kKinFit -> ttFullHadHypKinFit
-        label = "ttFullHadHyp" + names[obj][1:]
+        label = "ttFullHadHyp" + obj[1:]
         ## add it to the list
         labels.append(label)
     process.ttFullHadEvent.hypotheses = labels
 
     ## include hypotheses in the standard sequence
     sequence = getattr(process, "makeTtFullHadHypotheses")
-    for obj in range(len(names)):
+    for obj in names:
         ## create correct label from HypoClassKey string (stripping the leading "k")
         ## e.g. kKinFit -> makeHypothesis_kinFit
-        if names[obj][1:4] == "MVA":
-            label = "makeHypothesis_" + names[obj][1:4].lower() + names[obj][4:]
+        if obj[1:4] == "MVA":
+            label = "makeHypothesis_" + obj[1:4].lower() + obj[4:]
         else:
-            label = "makeHypothesis_" + names[obj][1:2].lower() + names[obj][2:]
+            label = "makeHypothesis_" + obj[1:2].lower() + obj[2:]
         ## add it to the sequence
         sequence += getattr(process, label)
 
+
+## clone ttFullHadEvent
+def cloneTtFullHadEvent(process
+                        ):
+
+    ## search highest already existing clone of ttFullHadEvent
+    ## to get the needed index for the new ttFullHadEvent
+    i=2
+    while ("ttFullHadEvent"+str(i) in process.producerNames()):
+        i = i+1
+    ## clone the ttFullHadEvent including all hypotheses
+    from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
+    process.makeTtFullHadEvent += cloneProcessingSnippet(process, process.makeTtFullHadEventBase, str(i))
+        
 
 ## remove genMatch hypothesis from the process
 def removeTtFullHadHypGenMatch(process):
     process.makeTtFullHadHypotheses.remove(process.makeHypothesis_genMatch)
     process.ttFullHadEvent.hypotheses.remove("ttFullHadHypGenMatch")
+    ## remove genMatch hypothesis for all clones of the ttFullHadEvent
+    i=2
+    while ("ttFullHadEvent"+str(i) in process.producerNames()):
+        getattr(process, "makeTtFullHadHypotheses"+str(i)).remove(getattr(process, "makeHypothesis_genMatch"+str(i)))
+        getattr(process, "ttFullHadEvent"+str(i)).hypotheses.remove(cms.InputTag("ttFullHadHypGenMatch"+str(i)))
+        ## remove full ttFullHadEvent sequence if
+        ## kGenMatch was the only included hypothesis
+        if len(getattr(process, "ttFullHadEvent"+str(i)).hypotheses) == 0:
+            process.makeTtFullHadEvent.remove(getattr(process,'makeTtFullHadEventBase'+str(i)))
+        i = i+1
 
 
 ## set a specific attribute for all hypotheses to a given value
@@ -63,7 +88,7 @@ def setForAllTtFullHadHypotheses(process, attribute, value):
                "ttFullHadHypGenMatch",
                "kinFitTtFullHadEventHypothesis",
                "ttFullHadHypKinFit"]
-    for obj in range(len(modules)):
-        object = getattr(process, modules[obj])
+    for obj in modules:
+        object = getattr(process, obj)
         if hasattr(object, attribute):
             setattr(object, attribute, value)
