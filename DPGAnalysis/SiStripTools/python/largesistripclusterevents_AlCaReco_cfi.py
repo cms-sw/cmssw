8a6bf2f5f46f125136ce5406391d48a46934176b
Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch>
2012-11-22 11:47:55 +0000

    new plugin used to select events based on sistrip cluster multiplicity for ALCARECO production

--- DPGAnalysis/SiStripTools/python/largesistripclusterevents_AlCaReco_cfi.py
+++ DPGAnalysis/SiStripTools/python/largesistripclusterevents_AlCaReco_cfi.py
@@ -1,7 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-LargeSiStripClusterEvents = cms.EDFilter('LargeSiStripClusterEvents',
-                                         collectionName    = cms.InputTag("calZeroBiasClusters"),
-                                         absoluteThreshold = cms.untracked.int32(150000),
-                                         moduleThreshold   = cms.untracked.int32(20)
-)	
+LargeSiStripClusterEvents = cms.EDFilter('BySiStripClusterMultiplicityEventFilter',
+                                             multiplicityConfig = cms.PSet(
+                                                                  collectionName = cms.InputTag("calZeroBiasClusters"),
+                                                                  moduleThreshold = cms.untracked.int32(20),
+                                                                  useQuality = cms.untracked.bool(False),
+                                                                  qualityLabel = cms.untracked.string("")
+                                                                  ),
+                                             cut = cms.string("mult > 150000")
+                                             )
+	
