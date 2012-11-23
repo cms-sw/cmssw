1c832a20674c05e35e1c50905501bfe739cbe862
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-11-23 11:53:47 +0000

    less inclusive wildcards; remove vertices (if needed, will have to be defined somewhere else; currently the EI sequence doesn't even use any specially selected vertices

--- CommonTools/ParticleFlow/python/EITopPAG_EventContent_cff.py
+++ CommonTools/ParticleFlow/python/EITopPAG_EventContent_cff.py
@@ -5,15 +5,14 @@ import FWCore.ParameterSet.Config as cms
 EITopPAGEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
     # isolated electrons and muons
-    'keep *_pfIsolatedElectronsEI*_*_*',
-    'keep *_pfIsolatedMuonsEI*_*_*',
+    'keep *_pfIsolatedElectronsEI_*_*',
+    'keep *_pfIsolatedMuonsEI_*_*',
     # jets
     'keep recoPFJets_pfJets_*_*',
     # taus 
     'keep recoPFTaus_pfTaus_*_*',
-    'keep recoPFTauDiscriminator_*_*_*',
-    'keep *_*fflinePrimaryVertices*_*_*',
+    'keep recoPFTauDiscriminator_pfTausDiscrimination*_*_*',
     # MET
-    'keep *_pfMET*_*_*'
+    'keep *_pfMetEI_*_*'
     )
 )
