c8c610095b12d8929f8f706f1a5af824c7a175c6
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-11-26 15:05:56 +0000

    adding list/dictionary of available and supported btag discriminators and btagInfos

--- PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/bTagging_cff.py
@@ -6,3 +6,31 @@ from RecoBTag.SecondaryVertex.simpleSecondaryVertex2TrkES_cfi import *
 #from RecoBTag.SecondaryVertex.simpleSecondaryVertexNegativeBJetTags_cfi import *
 
 
+## list of all available btagInfos
+supportedBtagInfos = [
+    'None',
+    'impactParameterTagInfos',
+    'secondaryVertexTagInfos',
+    'softMuonTagInfos',
+    'secondaryVertexNegativeTagInfos',
+    ]
+
+## dictionary with all available btag discriminators and the btagInfos that they require
+supportedBtagDiscr = {
+    'None'                                         : [],
+    'jetBProbabilityBJetTags'                      : ['impactParameterTagInfos'],
+    'jetProbabilityBJetTags'                       : ['impactParameterTagInfos'],
+    'trackCountingHighPurBJetTags'                 : ['impactParameterTagInfos'],
+    'trackCountingHighEffBJetTags'                 : ['impactParameterTagInfos'],
+    'simpleSecondaryVertexHighEffBJetTags'         : ['secondaryVertexTagInfos'],
+    'simpleSecondaryVertexHighPurBJetTags'         : ['secondaryVertexTagInfos'],
+    'combinedSecondaryVertexBJetTags'              : ['impactParameterTagInfos', 'secondaryVertexTagInfos'],
+    'combinedSecondaryVertexMVABJetTags'           : ['impactParameterTagInfos', 'secondaryVertexTagInfos'],
+    'softMuonBJetTags'                             : ['softMuonTagInfos'],
+    'softMuonByPtBJetTags'                         : ['softMuonTagInfos'],
+    'softMuonByIP3dBJetTags'                       : ['softMuonTagInfos'],
+    'simpleSecondaryVertexNegativeHighEffBJetTags' : ['secondaryVertexNegativeTagInfos'],
+    'simpleSecondaryVertexNegativeHighPurBJetTags' : ['secondaryVertexNegativeTagInfos'],
+    'negativeTrackCountingHighEffJetTags'          : ['impactParameterTagInfos'],
+    'negativeTrackCountingHighPurJetTags'          : ['impactParameterTagInfos'],
+    }
