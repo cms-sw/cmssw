15a444ca15ab69b0b11a63f7acb8b81c0eb656ff
Pavel Jez <sha1-5412db20736c393637675ccc5f0b9ea7f3a21499@cern.ch>
2012-11-28 22:11:49 +0000

    restoring HEAD

--- PhysicsTools/PatAlgos/python/recoLayer0/tauDiscriminators_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/tauDiscriminators_cff.py
@@ -1,12 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-try:
-    from RecoTauTag.Configuration.updateHPSPFTaus_cff import *
-    patHPSPFTauDiscriminationUpdate = cms.Sequence(updateHPSPFTaus)
-except ImportError:
-    patHPSPFTauDiscriminationUpdate = cms.Sequence()
+from RecoTauTag.Configuration.updateHPSPFTaus_cff import *
 
 patFixedConePFTauDiscrimination = cms.Sequence()
+
+patHPSPFTauDiscrimination = cms.Sequence(updateHPSPFTaus)
 
 patShrinkingConePFTauDiscrimination = cms.Sequence()
 
