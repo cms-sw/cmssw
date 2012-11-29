c66e4f7cfdb3ac53ae77af92531d8b503b9ff6bb
Anna Zsigmond <sha1-6e42027d3c49a348a482f91b089438b797e31dcc@cern.ch>
2012-11-29 09:19:03 +0000

    added pACentrality module

--- RecoHI/HiCentralityAlgos/python/HiCentrality_cfi.py
+++ RecoHI/HiCentralityAlgos/python/HiCentrality_cfi.py
@@ -33,6 +33,47 @@ hiCentrality = cms.EDFilter("reco::CentralityProducer",
                             trackEtaCut = cms.double(2),
                             trackPtCut = cms.double(1),
                             midRapidityRange = cms.double(1),
+			    hfEtaCut = cms.double(4),
+                            
+                            UseQuality = cms.bool(True),
+                            TrackQuality = cms.string('highPurity')
+                            
+                            )
+
+pACentrality = cms.EDFilter("reco::CentralityProducer",
+
+                            doFilter = cms.bool(False),
+                            
+                            produceHFhits = cms.bool(True),
+                            produceHFtowers = cms.bool(True),
+                            produceEcalhits = cms.bool(True),
+                            produceBasicClusters = cms.bool(True),
+                            produceZDChits = cms.bool(True),
+                            produceETmidRapidity = cms.bool(True),
+                            producePixelhits = cms.bool(True),
+                            produceTracks = cms.bool(True),
+                            producePixelTracks = cms.bool(True),
+                            reUseCentrality = cms.bool(False),
+
+                            srcHFhits = cms.InputTag("hfreco"),
+                            srcTowers = cms.InputTag("towerMaker"),
+                            srcEBhits = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+                            srcEEhits = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+                            srcVertex = cms.InputTag("offlinePrimaryVertices"),
+                            srcBasicClustersEB = cms.InputTag("hybridSuperClusters","hybridBarrelBasicClusters"),
+                            srcBasicClustersEE = cms.InputTag("multi5x5SuperClusters","multi5x5EndcapBasicClusters"),
+                            srcZDChits = cms.InputTag("zdcreco"),
+                            srcPixelhits = cms.InputTag("siPixelRecHits"),
+                            srcTracks = cms.InputTag("generalTracks"),
+                            srcReUse = cms.InputTag("pACentrality"),
+                            srcPixelTracks = cms.InputTag("pixelTracks"),
+                            
+                            doPixelCut = cms.bool(True),
+                            pixelBarrelOnly = cms.bool(False),
+                            trackEtaCut = cms.double(2),
+                            trackPtCut = cms.double(0.4),
+                            midRapidityRange = cms.double(1),
+			    hfEtaCut = cms.double(4), #hf above the absolute value of this cut is used
                             
                             UseQuality = cms.bool(True),
                             TrackQuality = cms.string('highPurity')
