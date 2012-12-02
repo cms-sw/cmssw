f93023eb88d41c37b92d4a5ed2461d7d541a98aa
Kevin Stenson <sha1-a1e9d2d7408f6d1ada06b4098eb954f682f9b807@cern.ch>
2012-12-02 18:58:39 +0000

    new merging preselection

--- RecoTracker/FinalTrackSelectors/python/MergeTrackCollections_cff.py
+++ RecoTracker/FinalTrackSelectors/python/MergeTrackCollections_cff.py
@@ -1,37 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-import RecoTracker.FinalTrackSelectors.trackListMerger_cfi
-preDuplicateMergingGeneralTracks = RecoTracker.FinalTrackSelectors.trackListMerger_cfi.trackListMerger.clone(
-    TrackProducers = (cms.InputTag('initialStepTracks'),
-                      cms.InputTag('lowPtTripletStepTracks'),
-                      cms.InputTag('pixelPairStepTracks'),
-                      cms.InputTag('detachedTripletStepTracks'),
-                      cms.InputTag('mixedTripletStepTracks'),
-                      cms.InputTag('pixelLessStepTracks'),
-                      cms.InputTag('tobTecStepTracks')),
-    hasSelector=cms.vint32(1,1,1,1,1,1,1),
-    indivShareFrac=cms.vdouble(1.0,0.16,0.19,0.13,0.11,0.11,0.09),
-    selectedTrackQuals = cms.VInputTag(cms.InputTag("initialStepSelector","initialStep"),
-                                       cms.InputTag("lowPtTripletStepSelector","lowPtTripletStep"),
-                                       cms.InputTag("pixelPairStepSelector","pixelPairStep"),
-                                       cms.InputTag("detachedTripletStep"),
-                                       cms.InputTag("mixedTripletStep"),
-                                       cms.InputTag("pixelLessStepSelector","pixelLessStep"),
-                                       cms.InputTag("tobTecStepSelector","tobTecStep")
-                                       ),
-    setsToMerge = cms.VPSet( cms.PSet( tLists=cms.vint32(0,1,2,3,4,5,6), pQual=cms.bool(True) )
-                             ),
-    copyExtras = True,
-    makeReKeyedSeeds = cms.untracked.bool(False)
-    )
 
 import RecoTracker.FinalTrackSelectors.DuplicateTrackMerger_cfi
 
 duplicateTrackCandidates = RecoTracker.FinalTrackSelectors.DuplicateTrackMerger_cfi.duplicateTrackMerger.clone(
     source=cms.InputTag("preDuplicateMergingGeneralTracks"),
-    minDeltaR3d = cms.double(-4.0),
-    minBDTG = cms.double(-0.1),
-    minpT = cms.double(0.2),
     useInnermostState  = cms.bool(True),
     ttrhBuilderName    = cms.string("WithAngleAndTemplate")
     )
@@ -43,14 +16,12 @@ mergedDuplicateTracks = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProduce
 
 generalTracks = RecoTracker.FinalTrackSelectors.DuplicateTrackMerger_cfi.duplicateListMerger.clone(
     originalSource = cms.InputTag("preDuplicateMergingGeneralTracks"),
-    diffHitsCut = cms.int32(5),
     mergedSource = cms.InputTag("mergedDuplicateTracks"),
     candidateSource = cms.InputTag("duplicateTrackCandidates","candidateMap")
     )
 
 
 generalTracksSequence = cms.Sequence(
-    preDuplicateMergingGeneralTracks*
     duplicateTrackCandidates*
     mergedDuplicateTracks*
     generalTracks
