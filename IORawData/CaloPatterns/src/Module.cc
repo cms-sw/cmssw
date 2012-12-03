bd9a4394cb8d8e5aa3ab181035006080c428e547
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-12-03 20:28:15 +0000

    Remove unnecessary #includes

--- IORawData/CaloPatterns/src/Module.cc
+++ IORawData/CaloPatterns/src/Module.cc
@@ -1,7 +1,6 @@
 #include "IORawData/CaloPatterns/src/HcalPatternSource.h"
 #include "IORawData/CaloPatterns/src/HtrXmlPattern.h"
 
-#include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_FWK_MODULE(HcalPatternSource);
