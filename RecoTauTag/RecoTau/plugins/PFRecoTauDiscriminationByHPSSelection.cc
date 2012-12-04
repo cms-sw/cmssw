fea17cdfd7ab42a38ece4db14800997263a5096a
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-12-04 10:17:16 +0000

    added option to use tau mass window which increases with tauPt

--- RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByHPSSelection.cc
+++ RecoTauTag/RecoTau/plugins/PFRecoTauDiscriminationByHPSSelection.cc
@@ -19,22 +19,26 @@ math::XYZTLorentzVector applyMassConstraint(
 class PFRecoTauDiscriminationByHPSSelection
   : public PFTauDiscriminationProducerBase {
   public:
-    explicit PFRecoTauDiscriminationByHPSSelection(
-        const edm::ParameterSet& pset);
-
-    ~PFRecoTauDiscriminationByHPSSelection() {}
+    explicit PFRecoTauDiscriminationByHPSSelection(const edm::ParameterSet&);
+    ~PFRecoTauDiscriminationByHPSSelection();
     double discriminate(const reco::PFTauRef&);
 
   private:
-    struct DecayModeCuts {
+    typedef StringObjectFunction<reco::PFTau> TauFunc;
+
+    struct DecayModeCuts 
+    {
+      DecayModeCuts()
+	: maxMass_(0)
+      {}
+      ~DecayModeCuts() {} // CV: maxMass object gets deleted by PFRecoTauDiscriminationByHPSSelection destructor
       double minMass_;
-      double maxMass_;
+      TauFunc* maxMass_;
       double minPi0Mass_;
       double maxPi0Mass_;
       double assumeStripMass_;
     };
 
-    typedef StringObjectFunction<reco::PFTau> TauFunc;
     typedef std::pair<unsigned int, unsigned int> IntPair;
     typedef std::pair<double, double> DoublePair;
     typedef std::map<IntPair, DecayModeCuts> DecayModeCutMap;
@@ -45,9 +49,10 @@ class PFRecoTauDiscriminationByHPSSelection
     double minPt_;
 };
 
-PFRecoTauDiscriminationByHPSSelection::PFRecoTauDiscriminationByHPSSelection(
-    const edm::ParameterSet& pset):PFTauDiscriminationProducerBase(pset),
-    signalConeFun_(pset.getParameter<std::string>("coneSizeFormula")) {
+PFRecoTauDiscriminationByHPSSelection::PFRecoTauDiscriminationByHPSSelection(const edm::ParameterSet& pset)
+  : PFTauDiscriminationProducerBase(pset),
+    signalConeFun_(pset.getParameter<std::string>("coneSizeFormula")) 
+{
   // Get the matchign cut
   matchingCone_ = pset.getParameter<double>("matchingCone");
   minPt_ = pset.getParameter<double>("minTauPt");
@@ -58,13 +63,13 @@ PFRecoTauDiscriminationByHPSSelection::PFRecoTauDiscriminationByHPSSelection(
     // The mass window(s)
     DecayModeCuts cuts;
     cuts.minMass_ = dm.getParameter<double>("minMass");
-    cuts.maxMass_ = dm.getParameter<double>("maxMass");
+    cuts.maxMass_ = new TauFunc(dm.getParameter<std::string>("maxMass"));
     if (dm.exists("minPi0Mass")) {
       cuts.minPi0Mass_ = dm.getParameter<double>("minPi0Mass");
       cuts.maxPi0Mass_ = dm.getParameter<double>("maxPi0Mass");
     } else {
-      cuts.minPi0Mass_ = -1.0;
-      cuts.maxPi0Mass_ = 1e9;
+      cuts.minPi0Mass_ = -1.e3;
+      cuts.maxPi0Mass_ = 1.e9;
     }
     if (dm.exists("assumeStripMass")) {
       cuts.assumeStripMass_ = dm.getParameter<double>("assumeStripMass");
@@ -78,6 +83,14 @@ PFRecoTauDiscriminationByHPSSelection::PFRecoTauDiscriminationByHPSSelection(
                 dm.getParameter<uint32_t>("nPiZeros")),
             cuts
           ));
+  }
+}
+
+PFRecoTauDiscriminationByHPSSelection::~PFRecoTauDiscriminationByHPSSelection()
+{
+  for ( DecayModeCutMap::iterator it = decayModeCuts_.begin();
+	it != decayModeCuts_.end(); ++it ) {
+    delete it->second.maxMass_;
   }
 }
 
@@ -98,7 +111,7 @@ PFRecoTauDiscriminationByHPSSelection::discriminate(const reco::PFTauRef& tau)
   if (massWindowIter == decayModeCuts_.end()) {
     return 0.0;
   }
-  const DecayModeCuts &massWindow = massWindowIter->second;
+  const DecayModeCuts& massWindow = massWindowIter->second;
 
   math::XYZTLorentzVector tauP4 = tau->p4();
   //std::cout << "tau: Pt = " << tauP4.pt() << ", eta = " << tauP4.eta() << ", phi = " << tauP4.phi() << ", mass = " << tauP4.mass() << std::endl;
@@ -125,7 +138,8 @@ PFRecoTauDiscriminationByHPSSelection::discriminate(const reco::PFTauRef& tau)
   //std::cout << "strips: Pt = " << stripsP4.pt() << ", eta = " << stripsP4.eta() << ", phi = " << stripsP4.phi() << ", mass = " << stripsP4.mass() << std::endl;
 
   // Check if tau fails mass cut
-  if (tauP4.M() > massWindow.maxMass_ || tauP4.M() < massWindow.minMass_) {
+  double maxMass_value = (*massWindow.maxMass_)(*tau);
+  if (tauP4.M() > maxMass_value || tauP4.M() < massWindow.minMass_) {
     return 0.0;
   }
 
