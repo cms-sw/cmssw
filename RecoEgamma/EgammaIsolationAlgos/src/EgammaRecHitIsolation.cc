d9f196e05cbc272ca9307d16d016bc2ee987bbae
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2012-12-04 14:59:07 +0000

    speed up isolation

--- RecoEgamma/EgammaIsolationAlgos/src/EgammaRecHitIsolation.cc
+++ RecoEgamma/EgammaIsolationAlgos/src/EgammaRecHitIsolation.cc
@@ -214,10 +214,12 @@ double EgammaRecHitIsolation::getSum_(const reco::SuperCluster* sc, bool returnE
 	  if(useNumCrystals_) {
 	    if( fabs(etaclus) < 1.479 ) {  // Barrel num crystals, crystal width = 0.0174
 	      if ( fabs(etaDiff) < 0.0174*etaSlice_) continue;  
-	      if ( sqrt(etaDiff*etaDiff + phiDiff*phiDiff) < 0.0174*intRadius_) continue; 
+	      // if ( sqrt(etaDiff*etaDiff + phiDiff*phiDiff) < 0.0174*intRadius_) continue; 
+	      if ((etaDiff*etaDiff + phiDiff*phiDiff) < 0.00030276*r2) continue
 	    } else {                       // Endcap num crystals, crystal width = 0.00864*fabs(sinh(eta))
 	      if ( fabs(etaDiff) < 0.00864*fabs(sinh(eta))*etaSlice_) continue;  
-	      if ( sqrt(etaDiff*etaDiff + phiDiff*phiDiff) < 0.00864*fabs(sinh(eta))*intRadius_) continue; 
+	      // if ( sqrt(etaDiff*etaDiff + phiDiff*phiDiff) < 0.00864*fabs(sinh(eta))*intRadius_) continue; 
+	      if ((etaDiff*etaDiff + phiDiff*phiDiff) < (0.000037325*(cosh(2*eta)-1)*r2)) continue;
 	    }
 	  } else {
 	    if ( fabs(etaDiff) < etaSlice_) continue;  // jurassic strip cut
