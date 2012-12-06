d07ada2ad19bc1161e1b209ff4711763180ad788
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2012-12-06 13:23:28 +0000

    revert back to the version with HCAL filters

--- Configuration/StandardSequences/python/Reconstruction_Data_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_Data_cff.py
@@ -5,10 +5,10 @@ from Configuration.StandardSequences.Reconstruction_cff import *
 particleFlowRecHitHCAL.LongShortFibre_Cut = 30.
 particleFlowRecHitHCAL.ApplyPulseDPG = True
 
-#import RecoLocalCalo.HcalRecAlgos.RemoveAddSevLevel as HcalRemoveAddSevLevel
-#HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFDigiTime",11)
-#HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHEFlatNoise",12)
-#HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHESpikeNoise",12)
+import RecoLocalCalo.HcalRecAlgos.RemoveAddSevLevel as HcalRemoveAddSevLevel
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFDigiTime",11)
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHEFlatNoise",12)
+HcalRemoveAddSevLevel.AddFlag(hcalRecAlgos,"HBHESpikeNoise",12)
 
 CSCHaloData.ExpectedBX = cms.int32(3)
 
