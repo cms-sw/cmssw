528be87a623f06b93179a037091147d4e366d210
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2012-12-06 14:59:47 +0000

    Use `edm::isNotFinite` in place of `std::isnan`.
    * Required when running `-Ofast` builds.

--- RecoTracker/SpecialSeedGenerators/src/SimpleCosmicBONSeeder.cc
+++ RecoTracker/SpecialSeedGenerators/src/SimpleCosmicBONSeeder.cc
@@ -8,6 +8,7 @@
 #include "RecoTracker/SpecialSeedGenerators/interface/SimpleCosmicBONSeeder.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2D.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+#include "FWCore/Utilities/interface/isFinite.h"
 typedef TransientTrackingRecHit::ConstRecHitPointer SeedingHit;
 
 #include <numeric>
@@ -472,7 +473,7 @@ bool SimpleCosmicBONSeeder::seeds(TrajectorySeedCollection &output, const edm::E
         float        ch = pq.second; 
         float Mom = sqrt( gv.x()*gv.x() + gv.y()*gv.y() + gv.z()*gv.z() ); 
 
-        if(Mom > 10000 || isnan(Mom))  { 
+        if(Mom > 10000 || edm::isNotFinite(Mom))  { 
             if (seedVerbosity_ > 1)
                 std::cout << "Processing triplet " << it << ": fail for momentum." << std::endl; 
             continue;
