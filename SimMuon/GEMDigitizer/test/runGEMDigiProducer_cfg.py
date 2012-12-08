6bd0b3e0ca314de0de532cbe38199589a2dccbe2
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2012-12-08 01:30:47 +0000

    example of running the GEMDigiProducer

---
+++ SimMuon/GEMDigitizer/test/runGEMDigiProducer_cfg.py
@@ -0,0 +1,93 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("GEMDIGI")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+#process.Timing = cms.Service("Timing")
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMXML_cfi')
+process.load('Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi')
+process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
+process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
+process.load('Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometryDB_cff')
+process.load('Geometry.DTGeometryBuilder.idealForDigiDtGeometryDB_cff')
+process.load('Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff')
+process.load('Geometry.GEMGeometry.gemGeometry_cfi')
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'POSTLS161_V12::All'
+#process.GlobalTag.globaltag = 'DESIGN60_V5::All'
+
+
+# GEM digitizer
+process.load('SimMuon.GEMDigitizer.muonGEMDigis_cfi')
+
+# customization of the process.pdigi sequence to add the GEM digitizer 
+from SimMuon.GEMDigitizer.customizeGEMDigi import *
+#process = customize_digi_addGEM(process)  # run all detectors digi
+process = customize_digi_addGEM_muon_only(process) # only muon+GEM digi
+#process = customize_digi_addGEM_gem_only(process)  # only GEM digi
+
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:out_sim.root'
+    )
+)
+
+
+process.output = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string( 
+        'out_digi.root'
+    ),
+    outputCommands = cms.untracked.vstring(
+        'keep  *_*_*_*',
+        # drop all CF stuff
+        'drop *_mix_*_*',
+        # drop tracker simhits
+        'drop PSimHits_*_Tracker*_*',
+        # drop calorimetry stuff
+        'drop PCaloHits_*_*_*',
+        # clean up simhits from other detectors
+        'drop PSimHits_*_Totem*_*',
+        'drop PSimHits_*_FP420*_*',
+        'drop PSimHits_*_BSC*_*',
+        # drop some not useful muon digis and links
+        'drop *_*_MuonCSCStripDigi_*',
+        'drop *_*_MuonCSCStripDigiSimLinks_*',
+        #'drop *SimLink*_*_*_*',
+        'drop *RandomEngineStates_*_*_*',
+        'drop *_randomEngineStateProducer_*_*'
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('digi_step')
+    )
+)
+
+
+
+#process.contentAna = cms.EDAnalyzer("EventContentAnalyzer")
+
+
+process.digi_step    = cms.Path(process.pdigi)
+process.endjob_step  = cms.Path(process.endOfProcess)
+process.out_step     = cms.EndPath(process.output)
+
+
+process.schedule = cms.Schedule(
+    process.digi_step,
+    process.endjob_step,
+    process.out_step
+)
+
