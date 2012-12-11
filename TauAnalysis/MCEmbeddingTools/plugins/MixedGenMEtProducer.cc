d0b75cd9cde6e680f53b2ec542230a70c529ca15
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-12-11 18:36:40 +0000

    added debug output (disabled per default)

--- TauAnalysis/MCEmbeddingTools/plugins/MixedGenMEtProducer.cc
+++ TauAnalysis/MCEmbeddingTools/plugins/MixedGenMEtProducer.cc
@@ -22,19 +22,29 @@ namespace
 {
   void sumNeutrinoP4s(const reco::GenParticleCollection& genParticles, reco::Candidate::LorentzVector& sumNeutrinoP4, double& sumEt)
   {
+    int idx = 0;
     for ( reco::GenParticleCollection::const_iterator genParticle = genParticles.begin();
 	  genParticle != genParticles.end(); ++genParticle ) {
+      //std::cout << "genParticle #" << idx << ": Pt = " << genParticle->pt() << ", eta = " << genParticle->eta() << ", phi = " << genParticle->phi() << ","
+      //	  << " pdgId = " << genParticle->pdgId() << ", status = " << genParticle->status() << std::endl;
       if ( genParticle->status() == 1 ) {
 	int absPdgId = TMath::Abs(genParticle->pdgId());
 	if ( absPdgId == 12 || absPdgId == 14 || absPdgId == 16 ) sumNeutrinoP4 += genParticle->p4();
 	else sumEt += genParticle->et();
       }
+      ++idx;
     }
+    //std::cout << "sumNeutrinos: Pt = " << sumNeutrinoP4.pt() << " (Px = " << sumNeutrinoP4.px() << ", Py = " << sumNeutrinoP4.py() << ")" << std::endl; 
+    //std::cout << "sumEt = " << sumEt << std::endl;
   }
 }
 
 void MixedGenMEtProducer::produce(edm::Event& evt, const edm::EventSetup& es)
 {
+  //std::cout << "<MixedGenMEtProducer::produce>:" << std::endl;
+  //std::cout << " srcGenParticles1 = " << srcGenParticles1_ << std::endl;
+  //std::cout << " srcGenParticles2 = " << srcGenParticles2_ << std::endl;
+
   edm::Handle<reco::GenParticleCollection> genParticles1;
   evt.getByLabel(srcGenParticles1_, genParticles1);
 
@@ -43,7 +53,9 @@ void MixedGenMEtProducer::produce(edm::Event& evt, const edm::EventSetup& es)
 
   reco::Candidate::LorentzVector genMEtP4;
   double genSumEt = 0.;
+  //std::cout << "processing original Z->mumu event:" << std::endl;
   sumNeutrinoP4s(*genParticles1, genMEtP4, genSumEt);
+  //std::cout << "processing embedded event:" << std::endl;
   sumNeutrinoP4s(*genParticles2, genMEtP4, genSumEt);
 
   typedef edm::View<reco::Candidate> CandidateView;
