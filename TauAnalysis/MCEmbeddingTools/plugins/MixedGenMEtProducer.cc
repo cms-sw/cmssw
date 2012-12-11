86870599204e540d5b783f5aa036d1abd9451b2f
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-12-11 18:37:29 +0000

    clean-up (removed debug-output)

--- TauAnalysis/MCEmbeddingTools/plugins/MixedGenMEtProducer.cc
+++ TauAnalysis/MCEmbeddingTools/plugins/MixedGenMEtProducer.cc
@@ -25,8 +25,6 @@ namespace
     int idx = 0;
     for ( reco::GenParticleCollection::const_iterator genParticle = genParticles.begin();
 	  genParticle != genParticles.end(); ++genParticle ) {
-      //std::cout << "genParticle #" << idx << ": Pt = " << genParticle->pt() << ", eta = " << genParticle->eta() << ", phi = " << genParticle->phi() << ","
-      //	  << " pdgId = " << genParticle->pdgId() << ", status = " << genParticle->status() << std::endl;
       if ( genParticle->status() == 1 ) {
 	int absPdgId = TMath::Abs(genParticle->pdgId());
 	if ( absPdgId == 12 || absPdgId == 14 || absPdgId == 16 ) sumNeutrinoP4 += genParticle->p4();
@@ -34,17 +32,11 @@ namespace
       }
       ++idx;
     }
-    //std::cout << "sumNeutrinos: Pt = " << sumNeutrinoP4.pt() << " (Px = " << sumNeutrinoP4.px() << ", Py = " << sumNeutrinoP4.py() << ")" << std::endl; 
-    //std::cout << "sumEt = " << sumEt << std::endl;
   }
 }
 
 void MixedGenMEtProducer::produce(edm::Event& evt, const edm::EventSetup& es)
 {
-  //std::cout << "<MixedGenMEtProducer::produce>:" << std::endl;
-  //std::cout << " srcGenParticles1 = " << srcGenParticles1_ << std::endl;
-  //std::cout << " srcGenParticles2 = " << srcGenParticles2_ << std::endl;
-
   edm::Handle<reco::GenParticleCollection> genParticles1;
   evt.getByLabel(srcGenParticles1_, genParticles1);
 
@@ -53,9 +45,7 @@ void MixedGenMEtProducer::produce(edm::Event& evt, const edm::EventSetup& es)
 
   reco::Candidate::LorentzVector genMEtP4;
   double genSumEt = 0.;
-  //std::cout << "processing original Z->mumu event:" << std::endl;
   sumNeutrinoP4s(*genParticles1, genMEtP4, genSumEt);
-  //std::cout << "processing embedded event:" << std::endl;
   sumNeutrinoP4s(*genParticles2, genMEtP4, genSumEt);
 
   typedef edm::View<reco::Candidate> CandidateView;
