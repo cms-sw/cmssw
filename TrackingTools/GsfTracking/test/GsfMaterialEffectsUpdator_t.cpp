9d1d1e6674398457ecb4a7e2cf7a8ae47b4184f0
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2012-12-26 10:48:19 +0000

    fix test

--- TrackingTools/GsfTracking/test/GsfMaterialEffectsUpdator_t.cpp
+++ TrackingTools/GsfTracking/test/GsfMaterialEffectsUpdator_t.cpp
@@ -77,7 +77,7 @@ int main(int argc, char * arg[]) {
 
   Surface::PositionType pos( 0., 0., 0.);
 
-  BoundPlane::BoundPlanePointer plane = BoundPlane::build(pos,rot, RectangularPlaneBounds(1.,1.,1));
+  Plane::PlanePointer plane = Plane::build(pos,rot, new RectangularPlaneBounds(1.,1.,1));
   plane->setMediumProperties(MediumProperties(0.1,0.3));
   M5T const m; 
 
