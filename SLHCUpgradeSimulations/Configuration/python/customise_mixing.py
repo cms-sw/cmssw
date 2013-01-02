e2d1c3c8d67ba237c7604d344594dbe598872df0
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2013-01-02 00:06:17 +0000

    Fix No PU

--- SLHCUpgradeSimulations/Configuration/python/customise_mixing.py
+++ SLHCUpgradeSimulations/Configuration/python/customise_mixing.py
@@ -1,8 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
+def customise_pixelMixing(process):
+    process.mix.digitizers.pixel.MissCalibrate = False
+    process.mix.digitizers.pixel.LorentzAngle_DB = False
+    process.mix.digitizers.pixel.killModules = False
+    process.mix.digitizers.pixel.useDB = False
+    process.mix.digitizers.pixel.DeadModules_DB = False
+    process.mix.digitizers.pixel.NumPixelBarrel = cms.int32(4)
+    process.mix.digitizers.pixel.NumPixelEndcap = cms.int32(3)
+    process.mix.digitizers.pixel.AddPixelInefficiency = -1
+    process.mix.digitizers.pixel.ThresholdInElectrons_FPix = cms.double(2000.0)
+    process.mix.digitizers.pixel.ThresholdInElectrons_BPix = cms.double(2000.0)
+    process.mix.digitizers.pixel.ThresholdInElectrons_BPix_L1 = cms.double(2000.0)
+    process.mix.digitizers.pixel.thePixelColEfficiency_BPix4 = cms.double(0.999)
+    process.mix.digitizers.pixel.thePixelEfficiency_BPix4 = cms.double(0.999)
+    process.mix.digitizers.pixel.thePixelChipEfficiency_BPix4 = cms.double(0.999)
+    return (process)
+
 # Remove the Crossing Frames to save memory
 def customise_NoCrossing(process):
-    process.load('SLHCUpgradeSimulations.Geometry.mixLowLumPU_Phase1_R30F12_cff')
+    process=customise_pixelMixing(process)
     process.mix.mixObjects.mixSH.crossingFrames = cms.untracked.vstring(
         'BSCHits',
         'FP420SI',
@@ -18,9 +35,15 @@ def customise_NoCrossing(process):
     process.mix.mixObjects.mixHepMC.makeCrossingFrame = cms.untracked.bool(False)
     process.digitisation_step.remove(process.simSiStripDigiSimLink)
     process.digitisation_step.remove(process.mergedtruth)
-    process.mix.digitizers.pixel.thePixelColEfficiency_BPix4 = cms.double(0.999)
-    process.mix.digitizers.pixel.thePixelEfficiency_BPix4 = cms.double(0.999)
-    process.mix.digitizers.pixel.thePixelChipEfficiency_BPix4 = cms.double(0.999)
+    return (process)
 
+def customise_pixelMixing_PU(process):
+    process.load('SLHCUpgradeSimulations.Geometry.mixLowLumPU_Phase1_R30F12_cff')
+    process=customise_pixelMixing(process)
+    return (process)
+
+def customise_NoCrossing_PU(process):
+    process.load('SLHCUpgradeSimulations.Geometry.mixLowLumPU_Phase1_R30F12_cff')
+    process=customise_NoCrossing(process)
     return (process)
 
