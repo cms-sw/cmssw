cbd3151d30222a662a8a5653be32e0cbe4ea7ac5
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2013-01-09 17:29:28 +0000

    avoid inheritance from TMatrixD

--- DataFormats/METReco/interface/PFMEtSignCovMatrix.h
+++ DataFormats/METReco/interface/PFMEtSignCovMatrix.h
@@ -11,7 +11,7 @@
  *
  * \version $Revision: 1.1 $
  *
- * $Id: PFMEtSignCovMatrix.h,v 1.1 2012/02/13 13:56:46 veelken Exp $
+ * $Id: PFMEtSignCovMatrix.h,v 1.1 2012/09/16 19:53:56 pharris Exp $
  *
  */
 
@@ -19,27 +19,84 @@
 
 #include <TMatrixD.h>
 
-class PFMEtSignCovMatrix : public TMatrixD
+class PFMEtSignCovMatrix
 {
  public:
   /// constructor 
   explicit PFMEtSignCovMatrix() 
-    : TMatrixD(2,2)
-  {}
-  explicit PFMEtSignCovMatrix(const PFMEtSignCovMatrix& bluePrint) 
-    : TMatrixD(bluePrint)
-  {}  
-  explicit PFMEtSignCovMatrix(const TMatrixD& bluePrint) 
-    : TMatrixD(bluePrint)
   {
-    if ( !(GetNrows() == 2 && GetNcols() == 2) )
+    covXX_ = 0.;
+    covXY_ = 0.;
+    covYY_ = 0.;
+  }
+  explicit PFMEtSignCovMatrix(const PFMEtSignCovMatrix& bluePrint) 
+  {
+    covXX_ = bluePrint.covXX_;
+    covXY_ = bluePrint.covXY_;
+    covYY_ = bluePrint.covYY_;
+  }  
+  /// convert TMatrixD to PFMEtSignCovMatrix 
+  explicit PFMEtSignCovMatrix(const TMatrixD& bluePrint) 
+  {
+    if ( !(bluePrint.GetNrows() == 2 && bluePrint.GetNcols() == 2) )
       throw cms::Exception("PFMEtSignCovMatrix")
 	<< "Matrix must have 2x2 rows and colums !!\n";
+    covXX_ = bluePrint(0,0);
+    covXY_ = bluePrint(0,1);
+    covYY_ = bluePrint(1,1);
   }
 
   /// destructor
   ~PFMEtSignCovMatrix() {}
-  ClassDef(PFMEtSignCovMatrix, 1);
+
+  /// get value of element(row, column)
+  double operator()(int row, int column) const
+  {
+    checkRange(row, column);
+    if      ( row == 0 && column == 0 ) return covXX_;
+    else if ( row == 0 && column == 1 ) return covXY_;
+    else if ( row == 1 && column == 0 ) return covXY_;
+    else if ( row == 1 && column == 1 ) return covYY_;
+    else assert(0);
+  }
+
+  /// set value of element(row, column)
+  double& operator()(int row, int column)
+  {
+    checkRange(row, column);
+    if      ( row == 0 && column == 0 ) return covXX_;
+    else if ( row == 0 && column == 1 ) return covXY_;
+    else if ( row == 1 && column == 0 ) return covXY_;
+    else if ( row == 1 && column == 1 ) return covYY_;
+    else assert(0);
+  }
+
+  /// convert PFMEtSignCovMatrix to TMatrixD
+  operator TMatrixD() const
+  {
+    TMatrixD matrix(2,2);
+    matrix(0,0) = covXX_;
+    matrix(0,1) = covXY_;
+    matrix(1,0) = covXY_;
+    matrix(1,1) = covYY_;
+    return matrix;
+  }
+
+ protected:
+  void checkRange(int row, int column) const
+  {
+    if ( !(row >= 0 && row <= 1) )
+      throw cms::Exception("PFMEtSignCovMatrix")
+	<< "Invalid row = " << row << ", expected range = 0..1 !!\n";
+    if ( !(column >= 0 && column <= 1) )
+      throw cms::Exception("PFMEtSignCovMatrix")
+	<< "Invalid column = " << column << ", expected range = 0..1 !!\n";
+  }  
+
+ private:
+  double covXX_;
+  double covXY_;
+  double covYY_;
 };
 
 #endif
