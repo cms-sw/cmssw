0bc785648ad2259292cad72cd465164e4a8cab7b
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2013-01-09 04:17:11 +0000

    remove TrajectoryCleanerMerger

--- TrackingTools/TrajectoryCleaning/plugins/modules.cc
+++ TrackingTools/TrajectoryCleaning/plugins/modules.cc
@@ -8,8 +8,6 @@
 
 #include "TrackingTools/TrajectoryCleaning/interface/TrajectoryCleanerBySharedHits.h"
 #include "TrackingTools/TrajectoryCleaning/interface/TrajectoryCleanerBySharedSeeds.h"
-#include "TrackingTools/TrajectoryCleaning/interface/TrajectoryCleanerMerger.h"
 
 DEFINE_EDM_PLUGIN(TrajectoryCleanerFactory, TrajectoryCleanerBySharedHits, "TrajectoryCleanerBySharedHits");
 DEFINE_EDM_PLUGIN(TrajectoryCleanerFactory, TrajectoryCleanerBySharedSeeds, "TrajectoryCleanerBySharedSeeds");
-DEFINE_EDM_PLUGIN(TrajectoryCleanerFactory, TrajectoryCleanerMerger, "TrajectoryCleanerMerger");
