409ee2aaf726024a252864cb77f88a744437c78f
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2013-01-12 12:07:46 +0000

    sr #135006: pPb: Menu adjustment requests

--- HLTrigger/Configuration/python/HLT_GRun_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_5_2_6/GRun/V92 (CMSSW_5_2_8)
+# /dev/CMSSW_5_2_6/GRun/V93 (CMSSW_5_2_8)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_5_2_6/GRun/V92')
+  tableName = cms.string('/dev/CMSSW_5_2_6/GRun/V93')
 )
 
 streams = cms.PSet( 
