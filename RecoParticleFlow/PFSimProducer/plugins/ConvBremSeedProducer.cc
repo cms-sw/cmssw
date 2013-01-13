82fe74ee5f39efb8c9fb539d4bc6dad882b0cb09
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2013-01-13 11:57:21 +0000

    fix use of FastHelix

--- RecoParticleFlow/PFSimProducer/plugins/ConvBremSeedProducer.cc
+++ RecoParticleFlow/PFSimProducer/plugins/ConvBremSeedProducer.cc
@@ -262,6 +262,10 @@ ConvBremSeedProducer::produce(Event& iEvent, const EventSetup& iSetup)
     
 
     //OUTPUT COLLECTION
+    edm::ESHandle<MagneticField> bfield;
+    iSetup.get<IdealMagneticFieldRecord>().get(bfield);
+    float nomField = bfield->nominalValue();
+ 
 
     TransientTrackingRecHit::ConstRecHitContainer glob_hits;
     OwnVector<TrackingRecHit> loc_hits;
@@ -294,13 +298,13 @@ ConvBremSeedProducer::produce(Event& iEvent, const EventSetup& iSetup)
 	    bool tak3=isGsfTrack(gsfRecHits,&(*it3));  
 	    
 
-	    FastHelix helix(gp3, gp2, gp1,iSetup);
-	    GlobalVector gv=helix.stateAtVertex().parameters().momentum();
+	    FastHelix helix(gp3, gp2, gp1,nomField,bfield.get());
+	    GlobalVector gv=helix.stateAtVertex().momentum();
 	    GlobalVector gv_corr(gv.x(),gv.y(),gv.perp()*sineta_brem);
 	    float ene= sqrt(gv_corr.mag2()+(pfmass*pfmass));
 
-	    GlobalPoint gp=helix.stateAtVertex().parameters().position();
-	    float ch=helix.stateAtVertex().parameters().charge();
+	    GlobalPoint gp=helix.stateAtVertex().position();
+	    float ch=helix.stateAtVertex().charge();
 
 
 
