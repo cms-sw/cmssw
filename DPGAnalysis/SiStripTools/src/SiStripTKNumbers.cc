392d8d864d46f9d6bcfe26762cc162441e61583a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2013-01-14 20:13:53 +0000

    Remove some unnecessary #includes

--- DPGAnalysis/SiStripTools/src/SiStripTKNumbers.cc
+++ DPGAnalysis/SiStripTools/src/SiStripTKNumbers.cc
@@ -2,9 +2,6 @@
 
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
-#include "DataFormats/SiStripDetId/interface/TIBDetId.h"
-#include "DataFormats/SiStripDetId/interface/TIDDetId.h"
-#include "DataFormats/SiStripDetId/interface/TOBDetId.h"
 #include "DataFormats/SiStripDetId/interface/TECDetId.h"
 #include "DPGAnalysis/SiStripTools/interface/SiStripTKNumbers.h"
 
