5ae0bb87334ea1a3717f978fd122fba8f6a4e0ba
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2013-01-14 15:03:02 +0000

    adjust log dir

--- RecoLuminosity/LumiDB/test/resultdiff.sh
+++ RecoLuminosity/LumiDB/test/resultdiff.sh
@@ -4,16 +4,17 @@ export VO_CMS_SW_DIR="/nfshome0/cmssw2";
 export SCRAM_ARCH=slc5_amd64_gcc434
 source /nfshome0/cmssw2/cmsset_default.sh;
 export PATH="/nfshome0/cmssw2/scripts:${PATH}";
-
 workdir="/nfshome0/hcallumipro/LumiDBUtil/exec/CMSSW_4_2_3/"
+lumilogpath="/home/lumidb/log"
+pwd=`pwd`
+
 cd $workdir
 eval `scramv1 runtime -sh`
 export TNS_ADMIN=/home/lumidb
 
 macrodir="$workdir/src/RecoLuminosity/LumiDB/test"
 outdir="/home/lumidb/lumibylsresult/2012"
-pwd=`pwd`
 cd $outdir
-python $macrodir/lumibylsdiff.py > lumibylsdiff.out
+python $macrodir/lumibylsdiff.py > "$lumilogpath/lumibylsdiff.out"
 cd $pwd 
 
