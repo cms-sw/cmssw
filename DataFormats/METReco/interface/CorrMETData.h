a644a68b6b2f1e4baaee6688a4ad75bb238febf6
Tai Sakuma <sha1-83aadde34f3854f55faaed54e7ebb52fa93fe00c@cern.ch>
2013-01-15 06:26:51 +0000

    added operators

--- DataFormats/METReco/interface/CorrMETData.h
+++ DataFormats/METReco/interface/CorrMETData.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: CorrMETData.h,v 1.5 2012/09/10 18:21:56 sakuma Exp $
+// $Id: CorrMETData.h,v 1.6 2012/11/12 23:47:28 sakuma Exp $
 #ifndef METRECO_CORR_MET_DATA_H
 #define METRECO_CORR_MET_DATA_H
 
@@ -9,7 +9,7 @@
 /// 
 /// CorrMETData represents a MET correction term.
 /// 
-/// \author Michael Schmitt, Richard Cavanaugh The University of Florida
+/// \author Michael Schmitt, Richard Cavanaugh The University of Florida; Tai Sakuma, Texas A&M University
 
 //____________________________________________________________________________||
 struct CorrMETData
@@ -24,6 +24,33 @@ struct CorrMETData
   CorrMETData() : mex(0.0), mey(0.0), sumet(0.0), significance(0.0) { }
 
   CorrMETData(const CorrMETData& corr) : mex(corr.mex), mey(corr.mey), sumet(corr.sumet), significance(corr.significance) { }
+
+  CorrMETData& operator+=(const CorrMETData& rhs)
+  {
+    mex += rhs.mex;	 
+    mey += rhs.mey;	 
+    sumet += rhs.sumet;	 
+    significance += rhs.significance;
+    return *this;
+  }
+
+  friend CorrMETData operator+(const CorrMETData& lhs, const CorrMETData& rhs)
+  {
+    CorrMETData ret(lhs);
+    ret += rhs;
+    return ret;
+  }
+
+  friend CorrMETData operator*(const double& lhs, const CorrMETData& rhs)
+  {
+    CorrMETData ret;
+    ret.mex = lhs*rhs.mex;
+    ret.mey = lhs*rhs.mey;
+    ret.sumet = lhs*rhs.sumet;
+    ret.significance = lhs*rhs.significance;
+    return ret;
+  }
+
 };
 
 //____________________________________________________________________________||
