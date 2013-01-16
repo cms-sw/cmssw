1b44a0e6913a280a33b669ac5670446ef8a92a9d
Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch>
2013-01-16 06:52:52 +0000

    tighten the cut

--- DPGAnalysis/Skims/python/HighLumiFilter_cfi.py
+++ DPGAnalysis/Skims/python/HighLumiFilter_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
  
 highLumiFilter = cms.EDFilter("HighLumiFilter",
                               lumiTag = cms.InputTag('lumiProducer'),
-                              minLumi = cms.double(1.0)
+                              minLumi = cms.double(1.08)
                               )
