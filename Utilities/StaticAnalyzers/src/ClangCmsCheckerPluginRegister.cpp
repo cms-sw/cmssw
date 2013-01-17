e68264dde05a070545f539dfddd6dc4f6852971a
Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132@cern.ch>
2013-01-17 11:22:19 +0000

    registered new cms.CodeRules.UsingNamespace and cms.CodeRules.CatchAll checkers , moved FiniteMathChecker to be in cms category instead of fastmath

--- Utilities/StaticAnalyzers/src/ClangCmsCheckerPluginRegister.cpp
+++ Utilities/StaticAnalyzers/src/ClangCmsCheckerPluginRegister.cpp
@@ -13,7 +13,8 @@
 #include "MutableMemberChecker.h"
 #include "ClassChecker.h"
 #include "FiniteMathChecker.h"
-
+#include "CatchAll.h"
+#include "UsingNamespace.h"
 
 #include <clang/StaticAnalyzer/Core/CheckerRegistry.h>
 
@@ -29,7 +30,9 @@ void clang_registerCheckers ( clang::ento::CheckerRegistry &registry)
 	registry.addChecker< clangcms::MutableMemberChecker>( "threadsafety.MutableMember", "Checks for members with the mutable keyword which might not be thread-safe" );
 	registry.addChecker< clangcms::GlobalStaticChecker>( "threadsafety.GlobalStatic", "Checks for global non-const statics which might not be thread-safe" );
 	registry.addChecker< clangcms::ClassCheckerRDecl>( "threadsafety.Class", "Reports classes " );
-	registry.addChecker< clangcms::FiniteMathChecker>( "fastmath.NonFiniteMath", "Reports usage of isnan and isinf." );
+	registry.addChecker< clangcms::FiniteMathChecker>( "cms.NonFiniteMath", "Reports usage of isnan and isinf." );
+	registry.addChecker< clangcms::UsingNamespace>( "cms.CodeRules.UsingNamespace", "Checks for 'using namespace' or 'using std::' in header files" );
+	registry.addChecker< clangcms::CatchAll>( "cms.CodeRules.CatchAll", "Checks for 'catch(...)' in source files" );
 }
 
 extern "C"
