e358d5f6d4c7fed08df9ec7a8a97e00b58cf5b3c
Anna Zsigmond <sha1-6e42027d3c49a348a482f91b089438b797e31dcc@cern.ch>
2013-01-18 16:48:21 +0000

    centrality tables for Hijing

--- HeavyIonsAnalysis/Configuration/python/CommonFunctions_cff.py
+++ HeavyIonsAnalysis/Configuration/python/CommonFunctions_cff.py
@@ -224,12 +224,12 @@ def overrideGlobalTag(process):
                 cms.PSet(record = cms.string("HeavyIonRcd"),
                                           tag = cms.string("CentralityTable_Tracks100_HijingPA_v538x01_mc"),
                                           connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
-                                          label = cms.untracked.string("Tracks")
+                                          label = cms.untracked.string("TracksHijing")
                                           ),
                 cms.PSet(record = cms.string("HeavyIonRcd"),
                                           tag = cms.string("CentralityTable_HFplus100_Hijing_v538x01_mc"),
                                           connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_PHYSICSTOOLS"),
-                                          label = cms.untracked.string("HFtowersPlusTrunc")
+                                          label = cms.untracked.string("HFtowersPlusTruncHijing")
                                           ),
 
         ])
