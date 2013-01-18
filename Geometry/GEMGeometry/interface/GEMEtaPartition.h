4e58bc184b3b61aecaf48c0223049d182aa76484
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2013-01-18 04:31:27 +0000

    addition of GEM-CSC trigger pads which are groups of strips, and  in geometrical sense are nothing more then alternative more coarse strips

--- Geometry/GEMGeometry/interface/GEMEtaPartition.h
+++ Geometry/GEMGeometry/interface/GEMEtaPartition.h
@@ -9,35 +9,66 @@
 class StripTopology;
 class GEMEtaPartitionSpecs;
 //class GEMChamber;
-class GEMEtaPartition : public GeomDetUnit{
 
- public:
+class GEMEtaPartition : public GeomDetUnit
+{
+public:
   
   GEMEtaPartition(GEMDetId id, BoundPlane::BoundPlanePointer bp, GEMEtaPartitionSpecs* rrs);
   ~GEMEtaPartition();
+
   const GEMEtaPartitionSpecs* specs() const;
   GEMDetId id() const;
+
   const Topology& topology() const;
   const StripTopology& specificTopology() const;
+
+  const Topology& padTopology() const;
+  const StripTopology& specificPadTopology() const;
+
   const GeomDetType& type() const; 
  
   /// Return the chamber this roll belongs to 
   //const GEMChamber* chamber() const;
-  
+ 
+  // strip-related methods:
+
   int nstrips() const;
 
   LocalPoint  centreOfStrip(int strip) const;
   LocalPoint  centreOfStrip(float strip) const;
   LocalError  localError(float strip) const;
 
+  // [0..nstrips)
   float strip(const LocalPoint& lp) const;
   float pitch() const;
-  float localPitch(const LocalPoint& lp) const; 
+  float localPitch(const LocalPoint& lp) const;
+ 
+
+  // pad-related methods:
   
- private:
+  int npads() const;
+
+  LocalPoint  centreOfPad(int pad) const;
+  LocalPoint  centreOfPad(float pad) const;
+
+  // [0..npads)
+  float pad(const LocalPoint& lp) const;
+  float padPitch() const;
+  float localPadPitch(const LocalPoint& lp) const;
+
+
+  // relations between strips and pads:
+  
+  // [0..npads)
+  float padOfStrip(int strip) const;
+  int firstStripInPad(int pad) const;
+  int lastStripInPad(int pad) const;
+
+private:
+
   //  void setChamber(const GEMChamber* ch);
 
- private:
   GEMDetId _id;
   GEMEtaPartitionSpecs* _rrs;
   //  const GEMChamber* theCh; // NOT owned
