e188901c39b9e537c9c0123418c6eb7a97669eab
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2013-01-18 04:42:33 +0000

    added a producer and a test-reader for GEMCSCPadDigis

---
+++ SimMuon/GEMDigitizer/src/GEMCSCPadDigiProducer.cc
@@ -0,0 +1,82 @@
+#include "SimMuon/GEMDigitizer/src/GEMCSCPadDigiProducer.h"
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+#include "Geometry/GEMGeometry/interface/GEMGeometry.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <string>
+#include <map>
+#include <vector>
+
+
+GEMCSCPadDigiProducer::GEMCSCPadDigiProducer(const edm::ParameterSet& ps)
+: geometry_(nullptr)
+{
+  produces<GEMCSCPadDigiCollection>();
+
+  input_ = ps.getParameter<edm::InputTag>("InputCollection");
+}
+
+
+GEMCSCPadDigiProducer::~GEMCSCPadDigiProducer()
+{}
+
+
+void GEMCSCPadDigiProducer::beginRun( edm::Run& r, const edm::EventSetup& eventSetup)
+{
+  edm::ESHandle<GEMGeometry> hGeom;
+  eventSetup.get<MuonGeometryRecord>().get( hGeom );
+  geometry_ = &*hGeom;
+}
+
+
+void GEMCSCPadDigiProducer::produce(edm::Event& e, const edm::EventSetup& eventSetup)
+{
+  edm::Handle<GEMDigiCollection> hdigis;
+  e.getByLabel(input_, hdigis);
+
+  // Create empty output
+  std::auto_ptr<GEMCSCPadDigiCollection> pPads(new GEMCSCPadDigiCollection());
+
+  // build the pads
+  buildPads(*(hdigis.product()), *pPads);
+
+  // store them in the event
+  e.put(pPads);
+}
+
+
+void GEMCSCPadDigiProducer::buildPads(const GEMDigiCollection &det_digis, GEMCSCPadDigiCollection &out_pads)
+{
+  auto etaPartitions = geometry_->etaPartitions();
+  for(auto p: etaPartitions)
+  {
+    // set of <pad, bx> pairs, sorted first by pad then by bx
+    std::set<std::pair<unsigned int, int> > proto_pads;
+  
+    // walk over digis in this partition, 
+    // and stuff them into a set of unique pads (equivalent of OR operation)
+    auto digis = det_digis.get(p->id());
+    for (auto d = digis.first; d != digis.second; ++d)
+    {
+      unsigned int pad_num = 1 + static_cast<unsigned int>( p->padOfStrip(d->strip()) );
+      auto pad = std::make_pair(pad_num, d->bx());
+      proto_pads.insert(pad);
+    }
+  
+    // in the future, do some dead-time handling
+    // emulateDeadTime(proto_pads)
+  
+    // fill the output collections
+    for (auto & d: proto_pads)
+    {
+      GEMCSCPadDigi pad_digi(d.first, d.second);
+      out_pads.insertDigi(p->id(), pad_digi);
+    }
+  }
+}
+
---
+++ SimMuon/GEMDigitizer/test/GEMCSCPadDigiReader.cc
@@ -0,0 +1,128 @@
+#ifndef SimMuon_GEMCSCPadDigiReader_h
+#define SimMuon_GEMCSCPadDigiReader_h
+
+/** \class GEMDigiReader
+ *  Dumps GEM-CSC trigger pad digis 
+ *  
+ *  $Id: GEMDigiReader.cc,v 1.1 2012/12/08 01:31:36 khotilov Exp $
+ *  \authors: Vadim Khotilovich
+ */
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+
+#include "Geometry/GEMGeometry/interface/GEMGeometry.h"
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+#include "DataFormats/MuonDetId/interface/GEMDetId.h"
+#include "DataFormats/GEMDigi/interface/GEMDigiCollection.h"
+#include "DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include <map>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+#include "DataFormats/Common/interface/DetSet.h"
+
+using namespace std;
+
+
+class GEMCSCPadDigiReader: public edm::EDAnalyzer
+{
+public:
+
+  explicit GEMCSCPadDigiReader(const edm::ParameterSet& pset);
+  
+  virtual ~GEMCSCPadDigiReader(){}
+  
+  void analyze(const edm::Event &, const edm::EventSetup&); 
+  
+private:
+
+  string label_pads_;
+  string label_digis_;
+};
+
+
+
+GEMCSCPadDigiReader::GEMCSCPadDigiReader(const edm::ParameterSet& pset)
+{
+  label_pads_ = pset.getUntrackedParameter<string>("labelPads", "simMuonGEMCSCPadDigis");
+  label_digis_ = pset.getUntrackedParameter<string>("labelDigis", "simMuonGEMDigis");
+}
+
+
+void GEMCSCPadDigiReader::analyze(const edm::Event & event, const edm::EventSetup& eventSetup)
+{
+  cout << "--- Run: " << event.id().run() << " Event: " << event.id().event() << endl;
+
+  edm::Handle<GEMCSCPadDigiCollection> pads;
+  event.getByLabel(label_pads_, pads);
+
+  edm::Handle<GEMDigiCollection> digis;
+  event.getByLabel(label_digis_, digis);
+
+  edm::ESHandle<GEMGeometry> pDD;
+  eventSetup.get<MuonGeometryRecord>().get( pDD );
+ 
+  /* 
+  size_t digi_dets_size = std::distance(digis->begin(), digis->end());
+  size_t pads_dets_size = std::distance(pads->begin(), pads->end());
+  cout<<"#dets with digis = "<<digi_dets_size<<"  with pad digis = "<<pads_dets_size<<"   "
+      <<( (digi_dets_size == pads_dets_size) ? "GOOD" : "BAD")<<endl;
+  */
+
+  GEMCSCPadDigiCollection::DigiRangeIterator detUnitIt;
+  for (detUnitIt = pads->begin();	detUnitIt != pads->end(); ++detUnitIt)
+  {
+    const GEMDetId& id = (*detUnitIt).first;
+    const GEMEtaPartition* roll = pDD->etaPartition(id);
+
+    //if(id.rawId() != 637567293) continue;
+
+    // GEMDetId print-out
+    cout<<"--------------"<<endl;
+    //cout<<"id: "<<id.rawId()<<" #strips "<<roll->nstrips()<<"  #pads "<<roll->npads()<<endl;
+
+    // retrieve this DetUnit's digis
+    std::map< std::pair<unsigned int, int>, // #pad, BX
+              std::vector<int>              // digi strip numbers
+      > digi_map;
+    auto digis_in_det = digis->get(id);
+    for (auto d = digis_in_det.first; d != digis_in_det.second; ++d)
+    {
+      unsigned int pad_num = 1 + static_cast<unsigned int>( roll->padOfStrip(d->strip()) );
+      digi_map[std::make_pair(pad_num, d->bx())].push_back(d->strip());
+    }
+
+    // loop over pads of this DetUnit and print stuff
+    auto pads_range = (*detUnitIt).second;
+    for (auto padIt = pads_range.first; padIt != pads_range.second; ++padIt)
+    {
+      cout<<id <<" paddigi(pad,bx) "<<*padIt<<endl;
+      if (padIt->pad() < 1 || padIt->pad() > roll->npads() )
+      {
+        cout <<" XXXXXXXXXXXXX Problem! "<<id<<" has pad digi with too large pad# = "<<padIt->pad()<<endl;
+      }
+
+      auto strips = digi_map[std::make_pair(padIt->pad(), padIt->bx())];
+      cout<<" has "<<strips.size()<<" strip digis at strips ";
+      copy(strips.begin(), strips.end(), ostream_iterator<unsigned int>(cout, " "));
+      cout<<endl;
+    }
+
+
+  }// for layers
+
+  cout<<"--------------"<<endl;
+}
+
+
+
+#endif
+#include <FWCore/Framework/interface/MakerMacros.h>
+DEFINE_FWK_MODULE(GEMCSCPadDigiReader);
+
