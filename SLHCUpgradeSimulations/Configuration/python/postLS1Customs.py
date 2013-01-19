f3c6d0cd2f699dfedc8902fed05a2afc493461db
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2013-01-19 16:15:44 +0000

    remove duplicated mixing changes

--- SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
@@ -3,24 +3,6 @@ import FWCore.ParameterSet.Config as cms
 
 from muonCustoms import customise_csc_geom_cond_digi
 
-def turnOffXFrame(process):
-    #turn off the crossing frame
-    process.mix.mixObjects.mixSH.crossingFrames = cms.untracked.vstring(
-        'BSCHits',
-        'FP420SI',
-        'MuonCSCHits',
-        'MuonDTHits',
-        'MuonRPCHits',
-        'TotemHitsRP',
-        'TotemHitsT1',
-        'TotemHitsT2Gem')
-    process.mix.mixObjects.mixCH.crossingFrames = cms.untracked.vstring('')
-    process.mix.mixObjects.mixTracks.makeCrossingFrame = cms.untracked.bool(False)
-    process.mix.mixObjects.mixVertices.makeCrossingFrame = cms.untracked.bool(False)
-    process.mix.mixObjects.mixHepMC.makeCrossingFrame = cms.untracked.bool(False)
-    process.digitisation_step.remove(process.simSiStripDigiSimLink)
-    process.digitisation_step.remove(process.mergedtruth)
-    return process
 
 def digiEventContent(process):
     #extend the event content
