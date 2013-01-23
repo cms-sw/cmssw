172644bad88a38f67b658f2854e75c9a6cd0e631
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2013-01-23 17:20:32 +0000

    add timestamptodatetimeUTC

--- RecoLuminosity/LumiDB/python/lumiTime.py
+++ RecoLuminosity/LumiDB/python/lumiTime.py
@@ -1,4 +1,4 @@
-import os,sys,time,calendar
+import os,sys,time,calendar,pytz
 from datetime import datetime,timedelta
 
 class lumiTime(object):
@@ -9,6 +9,9 @@ class lumiTime(object):
         self.bunchspace_us=0.02495 #in microseconds
         self.bunchspace_s=24.95e-09 #in seconds
         
+    def timestampTodatetimeUTC(ts):
+        return datetime.datetime.fromtimestamp(ts,tz=pytz.utc)
+    
     def LSDuration(self,norbits):
         return timedelta(microseconds=(self.nbx*norbits*self.bunchspace_us))
     
