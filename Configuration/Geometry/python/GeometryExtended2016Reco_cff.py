edc3a0ac5591e12353b40a4a623125f8622ea194
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2013-01-27 08:37:52 +0000

    Adding new geometry scenarios.

---
+++ Configuration/Geometry/python/GeometryExtended2016Reco_cff.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+# Ideal geometry, needed for transient ECAL alignement
+from Configuration.Geometry.GeometryExtended2016_cff import *
+from Configuration.Geometry.GeometryReco_cff import *
