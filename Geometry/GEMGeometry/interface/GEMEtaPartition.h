acf53dbc91250b2e305e7eaa315a9bd605698f55
Vadim Khotilovich <sha1-5697aef6c9e4fa922c99cf9168bcbd872fa0606b@cern.ch>
2013-01-27 13:51:55 +0000

    more documentation, cleanup

--- Geometry/GEMGeometry/interface/GEMEtaPartition.h
+++ Geometry/GEMGeometry/interface/GEMEtaPartition.h
@@ -17,8 +17,8 @@ public:
   GEMEtaPartition(GEMDetId id, BoundPlane::BoundPlanePointer bp, GEMEtaPartitionSpecs* rrs);
   ~GEMEtaPartition();
 
-  const GEMEtaPartitionSpecs* specs() const;
-  GEMDetId id() const;
+  const GEMEtaPartitionSpecs* specs() const { return specs_; }
+  GEMDetId id() const { return id_; }
 
   const Topology& topology() const;
   const StripTopology& specificTopology() const;
@@ -33,45 +33,65 @@ public:
  
   // strip-related methods:
 
+  /// number of readout strips in partition
   int nstrips() const;
 
+  /// returns center of strip position for INTEGER strip number
+  /// that has a value range of [1, nstrip]
   LocalPoint  centreOfStrip(int strip) const;
+
+  /// returns center of strip position for FRACTIONAL strip number
+  /// that has a value range of [0., nstrip]
   LocalPoint  centreOfStrip(float strip) const;
   LocalError  localError(float strip) const;
 
-  // [0..nstrips)
+  /// returns fractional strip number [0..nstrips] for a LocalPoint
+  /// E.g., if local point hit strip #2, the fractional strip number would be
+  /// somewhere in the (1., 2] interval
   float strip(const LocalPoint& lp) const;
+
   float pitch() const;
   float localPitch(const LocalPoint& lp) const;
  
 
-  // pad-related methods:
+  // GEM-CSC pad-related methods:
   
+  /// number of GEM-CSC trigger readout pads in partition
   int npads() const;
 
+  /// returns center of pad position for INTEGER pad number
+  /// that has a value range of [1, npads]
   LocalPoint  centreOfPad(int pad) const;
+
+  /// returns center of pad position for FRACTIONAL pad number
+  /// that has a value range of [0., npads]
   LocalPoint  centreOfPad(float pad) const;
 
-  // [0..npads)
+  /// returns FRACTIONAL pad number [0.,npads] for a point
   float pad(const LocalPoint& lp) const;
+
+  /// pad pitch in a center
   float padPitch() const;
+  /// pad pitch at a particular point
   float localPadPitch(const LocalPoint& lp) const;
 
 
   // relations between strips and pads:
   
-  // [0..npads)
+  /// returns FRACTIONAL pad number [0.,npads] for an integer strip [1,nstrip]
   float padOfStrip(int strip) const;
+
+  /// returns first strip (INT number [1,nstrip]) for pad (an integer [1,npads])
   int firstStripInPad(int pad) const;
+
+  /// returns last strip (INT number [1,nstrip]) for pad (an integer [1,npads])
   int lastStripInPad(int pad) const;
 
 private:
 
-  //  void setChamber(const GEMChamber* ch);
-
-  GEMDetId _id;
-  GEMEtaPartitionSpecs* _rrs;
-  //  const GEMChamber* theCh; // NOT owned
+  GEMDetId id_;
+  GEMEtaPartitionSpecs* specs_;
 };
 
 #endif
+
