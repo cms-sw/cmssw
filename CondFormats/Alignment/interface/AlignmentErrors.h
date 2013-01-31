e75a98153454b72dad57dec7a334a7f88c487555
Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch>
2013-01-31 13:22:12 +0000

    add method to clean the underlying vector (no data format change)

--- CondFormats/Alignment/interface/AlignmentErrors.h
+++ CondFormats/Alignment/interface/AlignmentErrors.h
@@ -11,6 +11,9 @@ public:
   virtual ~AlignmentErrors(){}
   /// Test of empty vector without having to look into internals:
   inline bool empty() const { return m_alignError.empty();}
+  /// Clear vector without having to look into internals:
+  inline void clear() {m_alignError.clear();}
+
   std::vector<AlignTransformError> m_alignError;
 };
 #endif // AlignmentErrors_H
--- CondFormats/Alignment/interface/Alignments.h
+++ CondFormats/Alignment/interface/Alignments.h
@@ -11,6 +11,9 @@ public:
   virtual ~Alignments(){}
   /// Test of empty vector without having to look into internals:
   inline bool empty() const { return m_align.empty();}
+  /// Clear vector without having to look into internals:
+  inline void clear() {m_align.clear();}
+
   std::vector<AlignTransform> m_align;
 };
 #endif // Alignments_H
