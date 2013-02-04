096f7c5f90a52de931f8ab3990f119784295a295
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2013-02-04 15:55:59 +0000

    adopt api  chances

--- RecoLuminosity/LumiDB/scripts/pixelLumiCalc.py
+++ RecoLuminosity/LumiDB/scripts/pixelLumiCalc.py
@@ -7,8 +7,6 @@
 ########################################################################
 
 import os,sys,time
-import coral
-
 from RecoLuminosity.LumiDB import sessionManager,lumiTime,inputFilesetParser,csvSelectionParser,selectionParser,csvReporter,argparse,CommonUtil,lumiCalcAPI,revisionDML,normDML,lumiReport,lumiCorrections,RegexValidator
 
 def parseInputFiles(inputfilename):
@@ -99,7 +97,7 @@ if __name__ == '__main__':
                         help='max run start time (mm/dd/yy hh:mm:ss),max fill or max run'
                         )
     parser.add_argument('--minBiasXsec',dest='minbiasxsec',action='store',
-                        default=71300.0,
+                        default=69300.0,
                         type=float,
                         required=False,
                         help='minbias cross-section in ub'
@@ -163,7 +161,36 @@ if __name__ == '__main__':
     reqfillmax=None
     reqtimemax=None
     timeFilter=[None,None]
-    
+    iresults=[]
+    reqTrg=False
+    reqHlt=False
+    if options.action=='overview' or options.action=='lumibyls':
+        reqTrg=True
+    if options.action=='recorded':
+        reqTrg=True
+        reqHlt=True
+    if options.runnumber:
+        reqrunmax=options.runnumber
+        reqrunmin=options.runnumber
+    if options.fillnum:
+        reqfillmin=options.fillnum
+        reqfillmax=options.fillnum
+
+    if options.begin:
+        (reqrunmin,reqfillmin,reqtimemin)=CommonUtil.parseTime(options.begin)
+        if reqtimemin:
+            lute=lumiTime.lumiTime()
+            reqtimeminT=lute.StrToDatetime(reqtimemin,customfm='%m/%d/%y %H:%M:%S')
+            timeFilter[0]=reqtimeminT
+    if options.end:
+        (reqrunmax,reqfillmax,reqtimemax)=CommonUtil.parseTime(options.end)
+        if reqtimemax:
+            lute=lumiTime.lumiTime()
+            reqtimemaxT=lute.StrToDatetime(reqtimemax,customfm='%m/%d/%y %H:%M:%S')
+            timeFilter[1]=reqtimemaxT
+    ##############################################################
+    # check working environment
+    ##############################################################            
     workingversion='UNKNOWN'
     updateversion='NONE'
     thiscmmd=sys.argv[0]
@@ -187,23 +214,9 @@ if __name__ == '__main__':
     #############################################################
     #pre-check option compatibility
     #############################################################
-
     if options.action=='recorded':
         if not options.hltpath:
-            raise RuntimeError('argument --hltpath pathname is required for recorded action')
-    if options.begin:
-        (reqrunmin,reqfillmin,reqtimemin)=CommonUtil.parseTime(options.begin)
-        if reqtimemin:
-            lute=lumiTime.lumiTime()
-            reqtimeminT=lute.StrToDatetime(reqtimemin,customfm='%m/%d/%y %H:%M:%S')
-            timeFilter[0]=reqtimeminT
-    if options.end:
-        (reqrunmax,reqfillmax,reqtimemax)=CommonUtil.parseTime(options.end)
-        if reqtimemax:
-            lute=lumiTime.lumiTime()
-            reqtimemaxT=lute.StrToDatetime(reqtimemax,customfm='%m/%d/%y %H:%M:%S')
-            timeFilter[1]=reqtimemaxT
-    
+            raise RuntimeError('argument --hltpath pathname is required for recorded action')                
     svc=sessionManager.sessionManager(options.connect,
                                       authpath=options.authpath,
                                       siteconfpath=options.siteconfpath,
@@ -213,49 +226,56 @@ if __name__ == '__main__':
     ##############################################################
     # check run/ls list
     ##############################################################
-
     irunlsdict={}
-    rruns=[]
-    iresults=[]
-
+    rruns=[]    
     session.transaction().start(True)
-    if options.runnumber: # if runnumber specified, do not go through other run selection criteria
-        irunlsdict[options.runnumber]=None
-        rruns=irunlsdict.keys()
-    else:  
-        runlist=lumiCalcAPI.runList(session.nominalSchema(),options.fillnum,runmin=reqrunmin,runmax=reqrunmax,fillmin=reqfillmin,fillmax=reqfillmax,startT=reqtimemin,stopT=reqtimemax,l1keyPattern=None,hltkeyPattern=None,amodetag=None,nominalEnergy=None,energyFlut=None,requiretrg=False,requirehlt=False,lumitype='PIXEL')
-        if options.inputfile:
-            (irunlsdict,iresults)=parseInputFiles(options.inputfile)
-            rruns=[val for val in runlist if val in irunlsdict.keys()]
-            for selectedrun in irunlsdict.keys():#if there's further filter on the runlist,clean input dict
-                if selectedrun not in rruns:
-                    del irunlsdict[selectedrun]
-        else:
-            for run in runlist:
-                irunlsdict[run]=None
-            rruns=irunlsdict.keys()
-
-    ###############################################################
+    filerunlist=None
+    if options.inputfile:
+        (irunlsdict,iresults)=parseInputFiles(options.inputfile)
+        filerunlist=irunlsdict.keys()
+    ##############################################################
     # check datatag
-    ###############################################################
+    # #############################################################
     datatagname=options.datatag
     if not datatagname:
-        (datatagid,datatagname)=revisionDML.currentDataTag(session.nominalSchema(),lumitype='PIXEL')
-        dataidmap=revisionDML.dataIdsByTagId(session.nominalSchema(),datatagid,runlist=rruns,lumitype='PIXEL',withcomment=False)
-        #{run:(lumidataid,trgdataid,hltdataid,())}
+        (datatagid,datatagname)=revisionDML.currentDataTag(session.nominalSchema())
     else:
-        dataidmap=revisionDML.dataIdsByTagName(session.nominalSchema(),datatagname,runlist=rruns,lumitype='PIXEL',withcomment=False)
-        #{run:(lumidataid,trgdataid,hltdataid,())}
-    #
+        datatagid=revisionDML.getDataTagId(session.nominalSchema(),datatagname)
+
+    dataidmap=lumiCalcAPI.runList(session.nominalSchema(),datatagid,runmin=reqrunmin,runmax=reqrunmax,fillmin=reqfillmin,fillmax=reqfillmax,startT=reqtimemin,stopT=reqtimemax,l1keyPattern=None,hltkeyPattern=None,amodetag=None,nominalEnergy=None,energyFlut=None,requiretrg=reqTrg,requirehlt=reqHlt,preselectedruns=filerunlist,lumitype='PIXEL')
+    if not dataidmap:
+        print '[INFO] No qualified run found, do nothing'
+        sys.exit(14)
+    rruns=[]
+    for irun,(lid,tid,hid) in dataidmap.items():
+        if not lid:
+            print '[INFO] No qualified lumi data found for run, ',irun
+        if reqTrg and not tid:
+            print '[INFO] No qualified trg data found for run ',irun
+            continue
+        if reqHlt and not hid:
+            print '[INFO] No qualified hlt data found for run ',irun
+            continue
+        rruns.append(irun)
+    if not irunlsdict: #no file
+        irunlsdict=dict(zip(rruns,[None]*len(rruns)))
+    else:
+        for selectedrun in irunlsdict.keys():#if there's further filter on the runlist,clean input dict
+            if selectedrun not in rruns:
+                del irunlsdict[selectedrun]
+    if not irunlsdict:
+        print '[INFO] No qualified run found, do nothing'
+        sys.exit(13)
+    ###############################################################
     # check normtag and get norm values if required
-    #
+    ###############################################################
     normname='NONE'
     normid=0
     normvalueDict={}
     if not options.withoutNorm:
         normname=options.normtag
         if not normname:
-            normmap=normDML.normIdByType(session.nominalSchema(),lumitype='PIXEL',defaultonly=True)
+            normmap=normDML.normIdByType(session.nominalSchema(),lumitype='HF',defaultonly=True)
             if len(normmap):
                 normname=normmap.keys()[0]
                 normid=normmap[normname]
@@ -265,15 +285,14 @@ if __name__ == '__main__':
             raise RuntimeError('[ERROR] cannot resolve norm/correction')
             sys.exit(12)
         normvalueDict=normDML.normValueById(session.nominalSchema(),normid) #{since:[corrector(0),{paramname:paramvalue}(1),amodetag(2),egev(3),comment(4)]}
-    lumiReport.toScreenHeader(thiscmmd,datatagname,normname,workingversion,updateversion,'PIXEL',toFile=options.headerfile)
     session.transaction().commit()
-    if not dataidmap:
-        print '[INFO] No qualified data found, do nothing'
-        sys.exit(13)
-    
-    session.transaction().start(True)
+    lumiReport.toScreenHeader(thiscmmd,datatagname,normname,workingversion,updateversion,'PIXEL',toFile=options.headerfile)
 
-    GrunsummaryData=lumiCalcAPI.runsummaryMap(session.nominalSchema(),irunlsdict)
+    ##################
+    # ls level       #
+    ##################
+    session.transaction().start(True)
+    GrunsummaryData=lumiCalcAPI.runsummaryMap(session.nominalSchema(),irunlsdict,dataidmap,lumitype='PIXEL')
     if options.action == 'overview':
        result=lumiCalcAPI.lumiForIds(session.nominalSchema(),irunlsdict,dataidmap,runsummaryMap=GrunsummaryData,beamstatusfilter=None,timeFilter=timeFilter,normmap=normvalueDict,lumitype='PIXEL')
        lumiReport.toScreenOverview(result,iresults,options.scalefactor,irunlsdict=irunlsdict,noWarning=options.nowarning,toFile=options.outputfile)
