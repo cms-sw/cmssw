2b062169d3a34f97326758cda05a166f23a69d32
Adrien Caudron <sha1-8f0d6c43c2a59dd7afae5302c75364feb095f4d0@cern.ch>
2013-02-13 13:29:10 +0000

    bug fix due to segfault in vertexPlots method : int i was defined 2 times and was going out of range (>1)

--- DQMOffline/RecoB/plugins/PrimaryVertexMonitor.cc
+++ DQMOffline/RecoB/plugins/PrimaryVertexMonitor.cc
@@ -142,6 +142,7 @@ void PrimaryVertexMonitor::analyze(const edm::Event& iEvent, const edm::EventSet
 void PrimaryVertexMonitor::vertexPlots(const Vertex & v, const BeamSpot& beamSpot, int i)
 {
 
+    if (i < 0 || i > 2) return;
     if (!v.isValid()) type[i]->Fill(2.);
     else if (v.isFake()) type[i]->Fill(1.);
     else type[i]->Fill(0.);
@@ -178,12 +179,12 @@ void PrimaryVertexMonitor::vertexPlots(const Vertex & v, const BeamSpot& beamSpo
       nans[i]->Fill(3.,edm::isNotFinite(v.position().z())*1.);
 
       int index = 3;
-      for (int i = 0; i != 3; i++) {
-	for (int j = i; j != 3; j++) {
+      for (int k = 0; k != 3; k++) {
+	for (int j = k; j != 3; j++) {
 	  index++;
-	  nans[i]->Fill(index*1., edm::isNotFinite(v.covariance(i, j))*1.);
+	  nans[i]->Fill(index*1., edm::isNotFinite(v.covariance(k, j))*1.);
 	  // in addition, diagonal element must be positive
-	  if (j == i && v.covariance(i, j) < 0) {
+	  if (j == k && v.covariance(k, j) < 0) {
 	    nans[i]->Fill(index*1., 1.);
 	  }
 	}
