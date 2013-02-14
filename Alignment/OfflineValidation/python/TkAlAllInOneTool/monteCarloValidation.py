36272298a5a13f9dd0e0ad922d589c1b1312745a
Gregor Hellwig <sha1-0fd33106e71082062498fd3bb4060f6add1c2210@cern.ch>
2013-02-14 14:38:10 +0000

    Prepared `jobmode:crab` for `MonteCarloValidation`.

--- Alignment/OfflineValidation/python/TkAlAllInOneTool/monteCarloValidation.py
+++ Alignment/OfflineValidation/python/TkAlAllInOneTool/monteCarloValidation.py
@@ -1,80 +1,69 @@
 import os
 import configTemplates
-from genericValidation import GenericValidation
+import globalDictionaries
+from dataset import Dataset
+from genericValidation import GenericValidationMC
 from helperFunctions import replaceByMap
 from TkAlExceptions import AllInOneError
 
 
-class MonteCarloValidation(GenericValidation):
+class MonteCarloValidation(GenericValidationMC):
     def __init__(self, valName, alignment, config):
-        GenericValidation.__init__(self, valName, alignment, config)
-        defaults = {
-            "jobmode":self.jobmode
-            }
+        GenericValidationMC.__init__(self, valName, alignment, config,
+                                       "mcValidate")
         mandatories = [ "relvalsample", "maxevents" ]
         if not config.has_section( "mcValidate:"+self.name ):
             mcValidate = config.getResultingSection( "general",
-                                                     defaultDict = defaults,
                                                      demandPars = mandatories )
         else:
             mcValidate = config.getResultingSection( "mcValidate:"+self.name, 
-                                                     defaultDict = defaults,
                                                      demandPars = mandatories )
         self.general.update( mcValidate )
-        self.jobmode = self.general["jobmode"]
+        if self.general["relvalsample"] not in globalDictionaries.usedDatasets:
+            globalDictionaries.usedDatasets[self.general["relvalsample"]] = Dataset(
+                self.general["relvalsample"] )
+        self.dataset = globalDictionaries.usedDatasets[self.general["relvalsample"]]
+        self.general["dataset"] = self.general["relvalsample"]
+
 
     def createConfiguration(self, path ):
-        cfgName = "TkAlMcValidation.%s.%s_cfg.py"%( self.name,
-                                                    self.alignmentToValidate.name )
+        cfgName = "TkAlMcValidate.%s.%s_cfg.py"%(self.name,
+                                                   self.alignmentToValidate.name)
         repMap = self.getRepMap()
-        repMap.update({
-                "outputFile": replaceByMap( (".oO[workdir]Oo./McValidation_"
-                                             + self.name +
-                                             "_.oO[name]Oo..root"),
-                                            repMap )
-                })
-        repMap["outputFile"] = os.path.expandvars( repMap["outputFile"] )
-        repMap["outputFile"] = os.path.abspath( repMap["outputFile"] )
-        cfgs = {cfgName:replaceByMap( configTemplates.mcValidateTemplate, repMap)}
-        self.filesToCompare[ GenericValidation.defaultReferenceName ] = repMap["outputFile"]
-        GenericValidation.createConfiguration(self, cfgs, path)
+        cfgs = {cfgName:replaceByMap(configTemplates.mcValidateTemplate, repMap)}
+        self.filesToCompare[GenericValidationMC.defaultReferenceName] = \
+            repMap["outputFile"]
+        GenericValidationMC.createConfiguration(self, cfgs, path)
 
     def createScript(self, path):
-        scriptName = "TkAlMcValidate.%s.%s.sh"%( self.name,
-                                                 self.alignmentToValidate.name )
+        scriptName = "TkAlMcValidate.%s.%s.sh"%(self.name,
+                                                self.alignmentToValidate.name)
         repMap = self.getRepMap()
         repMap["CommandLine"]=""
         for cfg in self.configFiles:
-            repMap["CommandLine"]+= repMap["CommandLineTemplate"]%{"cfgFile":cfg,
-                                                  "postProcess":""
-                                                  }
+            repMap["CommandLine"] += \
+                repMap["CommandLineTemplate"]%{"cfgFile":cfg, "postProcess":"" }
 
-        scripts = {scriptName: replaceByMap( configTemplates.scriptTemplate, repMap ) }
-        return GenericValidation.createScript(self, scripts, path)
+        scripts = {scriptName: replaceByMap(configTemplates.scriptTemplate,
+                                            repMap)}
+        return GenericValidationMC.createScript(self, scripts, path)
 
-    def createCrabCfg( self, path,
-                       crabCfgBaseName = "TkAlMcValidate"  ):
-        crabCfgName = "crab.%s.%s.%s.cfg"%( crabCfgBaseName, self.name,
-                                            self.alignmentToValidate.name )
-        repMap = self.getRepMap()
-        repMap["script"] = "dummy_script.sh"
-        repMap["crabOutputDir"] = os.path.basename( path )
-        repMap["crabWorkingDir"] = crabCfgName.split( '.cfg' )[0]
-        self.crabWorkingDir = repMap["crabWorkingDir"]
-        repMap["numberOfJobs"] = self.general["parallelJobs"]
-        repMap["cfgFile"] = self.configFiles[0]
-        repMap["queue"] = self.jobmode.split( ',' )[1].split( '-q' )[1]
-        crabCfg = {crabCfgName: replaceByMap( configTemplates.crabCfgTemplate,
-                                              repMap ) }
-        return GenericValidation.createCrabCfg( self, crabCfg, path )
+    def createCrabCfg(self, path, crabCfgBaseName = "TkAlMcValidate"):
+        return GenericValidationMC.createCrabCfg(self, path, crabCfgBaseName)
 
     def getRepMap( self, alignment = None ):
-        repMap = GenericValidation.getRepMap(self, alignment)
-        repMap.update({ 
+        repMap = GenericValidationMC.getRepMap(self, alignment)
+        repMap.update({
+            "outputFile": replaceByMap((".oO[workdir]Oo./McValidation_"
+                                        + self.name +
+                                        "_.oO[name]Oo..root"), repMap ),
             "nEvents": self.general["maxevents"],
             # Keep the following parameters for backward compatibility
             "RelValSample": self.general["relvalsample"]
-
-            })
+                })
+        repMap["outputFile"] = os.path.expandvars( repMap["outputFile"] )
+        repMap["outputFile"] = os.path.abspath( repMap["outputFile"] )
+        if self.jobmode.split( ',' )[0] == "crab":
+            repMap["outputFile"] = os.path.basename( repMap["outputFile"] )
         return repMap
 
