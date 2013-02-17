f962607ab9c7cf901ec692362a6d5d28435c9ec8
khotilov <>
2013-02-17 15:46:40 +0000

    switches for separate delta and efficiency ntuples

--- GEMValidation/python/GEMCSCAnalyzer_cfi.py
+++ GEMValidation/python/GEMCSCAnalyzer_cfi.py
@@ -5,6 +5,8 @@ GEMCSCAnalyzer = cms.EDAnalyzer("GEMCSCAnalyzer",
     verbose = cms.untracked.int32(0),
     simInputLabel = cms.untracked.string("g4SimHits"),
     minPt = cms.untracked.double(4.5),
+    ntupleTrackChamberDelta = cms.untracked.bool(True),
+    ntupleTrackEff = cms.untracked.bool(True),
     simTrackMatching = cms.PSet(
         # SimHit matching:
         verboseSimHit = cms.untracked.int32(0),
