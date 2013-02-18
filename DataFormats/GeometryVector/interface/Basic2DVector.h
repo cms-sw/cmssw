ce272e3301c649cb26ff98182ca1698a78d17cbd
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2013-02-18 11:04:18 +0000

    add support for extended vectors

--- DataFormats/GeometryVector/interface/Basic2DVector.h
+++ DataFormats/GeometryVector/interface/Basic2DVector.h
@@ -1,14 +1,21 @@
 #ifndef GeometryVector_Basic2DVector_h
 #define GeometryVector_Basic2DVector_h
 
-#if (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 4)) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 7)) || defined(__clang__)
+#define USE_EXTVEC
+#elif (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 4)) 
 #define USE_SSEVECT
 #endif
 
-#if defined(USE_SSEVECT) && ! ( defined(__REFLEX__) || defined(__CINT__) )
-#include "DataFormats/GeometryVector/interface/newBasic2DVector.h"
-#else
+
+
+
+#if ( defined(__REFLEX__) || defined(__CINT__) )
 #include "DataFormats/GeometryVector/interface/oldBasic2DVector.h"
+#elif defined(USE_EXTVECT)       
+#include "DataFormats/GeometryVector/interface/extBasic2DVector.h"
+#elif defined(USE_SSEVECT)
+#include "DataFormats/GeometryVector/interface/sseBasic2DVector.h"
 #endif
 
 #endif // GeometryVector_Basic2DVector_h
--- DataFormats/GeometryVector/interface/Basic3DVector.h
+++ DataFormats/GeometryVector/interface/Basic3DVector.h
@@ -1,16 +1,22 @@
 #ifndef GeometryVector_Basic3DVector_h
 #define GeometryVector_Basic3DVector_h
 
-#if (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 4)) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 7)) || defined(__clang__)
+#define USE_EXTVEC
+#elif (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 4)) 
 #define USE_SSEVECT
 #endif
 
-#if defined(USE_SSEVECT) && ! ( defined(__REFLEX__) || defined(__CINT__) )
-#include "DataFormats/GeometryVector/interface/newBasic3DVector.h"
-#else
+
+
+
+#if ( defined(__REFLEX__) || defined(__CINT__) )
 #include "DataFormats/GeometryVector/interface/oldBasic3DVector.h"
+#elif defined(USE_EXTVECT)       
+#include "DataFormats/GeometryVector/interface/extBasic3DVector.h"
+#elif defined(USE_SSEVECT)
+#include "DataFormats/GeometryVector/interface/sseBasic3DVector.h"
 #endif
 
 #endif // GeometryVector_Basic3DVector_h
-
 
