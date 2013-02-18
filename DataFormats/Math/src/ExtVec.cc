678287e8aad5c97a5567f9b7d9596c3ec2ad26cb
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2013-02-18 17:15:47 +0000

    move output to .cc

--- DataFormats/Math/src/ExtVec.cc
+++ DataFormats/Math/src/ExtVec.cc
@@ -1,6 +1,5 @@
-#include "DataFormats/Math/interface/SSEVec.h"
-#include "DataFormats/Math/interface/SSERot.h"
-using namespace mathSSE;
+#if (defined(__GNUC__) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 7)) || defined(__clang__)
+#include "DataFormats/Math/interface/ExtVec.h"
 
 #include <ostream>
 std::ostream & operator<<(std::ostream & out,  Vec4F const & v) {
@@ -16,11 +15,11 @@ std::ostream & operator<<(std::ostream & out,  Vec2D const & v) {
   return out << '(' << v[0] <<", " << v[1] <<')';
 }
 
-std::ostream & operator<<(std::ostream & out, As3D<float> const & v) {
+std::ostream & operator<<(std::ostream & out, ::As3D<Vec4F> const & v) {
   return out << '(' << v.v[0] <<", " << v.v[1] <<", "<< v.v[2] <<')';
 }
 
-std::ostream & operator<<(std::ostream & out, As3D<double> const & v) {
+std::ostream & operator<<(std::ostream & out, ::As3D<Vec4D> const & v) {
   return out << '(' << v.v[0] <<", " << v.v[1] <<", "<< v.v[2] <<')';
 }
 
@@ -39,4 +38,4 @@ std::ostream & operator<<(std::ostream & out, Rot2F const & r){
 std::ostream & operator<<(std::ostream & out, Rot2D const & r){
   return out << r.axis[0] << '\n' << r.axis[1];
 }
-
+#endif
