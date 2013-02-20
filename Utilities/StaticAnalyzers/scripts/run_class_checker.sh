cc46238059b6264206f8998caf14cc5c82f23950
Patrick Gartung <sha1-33476957b06a1bc61e5124243258241c72d1e027@cern.ch>
2013-02-20 18:22:39 +0000

    update class checker script

--- Utilities/StaticAnalyzers/scripts/run_class_checker.sh
+++ Utilities/StaticAnalyzers/scripts/run_class_checker.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+export SCRAM_ARCH=slc5_amd64_gcc472
+cd ${CMSSW_BASE}
 eval `scram runtime -sh`
-for file in `cmsglimpse -l -F src/classes.h$ include`;do dir=`dirname $file`;echo \#include \<$file\> >${CMSSW_BASE}/src/$dir/classes_def.cc ; done
-export USER_LLVM_CHECKERS="-disable-checker unix -disable-checker threadsafety -disable-checker core -disable-checker security -disable-checker deadcode -disable-checker cms -enable-checker optional.ClassDumperCT -enable-checker optional.ClassDumperFT"
 ulimit -m 2000000
 ulimit -v 2000000
-scram b -j $1 checker 2>&1 | tee /tmp/classdumper.log
+ulimit -t 3600
+for file in `cmsglimpse -l -F src/classes.h$ include`;do dir=`dirname $file`;echo \#include \<$file\> >${CMSSW_BASE}/src/$dir/classes_def.cc ; done
+export USER_LLVM_CHECKERS="-disable-checker unix -disable-checker threadsafety -disable-checker core -disable-checker security -disable-checker deadcode -disable-checker cms -enable-checker optional.ClassDumperCT -enable-checker optional.ClassDumperFT"
+scram b -k -j $1 checker 2>&1 | tee /tmp/classdumper.log
+mv /tmp/classes.txt /tmp/classes.txt.unsorted
+sort /tmp/classes.txt.unsorted >/tmp/classes.txt
 export USER_LLVM_CHECKERS="-disable-checker unix -disable-checker threadsafety -disable-checker core -disable-checker security -disable-checker deadcode -disable-checker cms -enable-checker optional.ClassChecker"
-scram b -j $1 checker 2>&1 | tee /tmp/classchecker.log
+scram b -k -j $1 checker 2>&1 | tee /tmp/classchecker.log
