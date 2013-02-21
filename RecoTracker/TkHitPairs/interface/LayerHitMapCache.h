cc605b03be44ca33f494c1945e81ddcaa224e2e5
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2013-02-21 10:16:34 +0000

    try to be clever

--- RecoTracker/TkHitPairs/interface/LayerHitMapCache.h
+++ RecoTracker/TkHitPairs/interface/LayerHitMapCache.h
@@ -15,53 +15,46 @@ class LayerHitMapCache {
 
 private:
 
-  template <class KeyType, class ValueType> class SimpleCache {
+  class SimpleCache {
   public:
-    SimpleCache(int initSize) { reserve(initSize); }
-    virtual ~SimpleCache() { clear(); }
-    void reserve(int size) { theContainer.reserve(size); }
-    const ValueType*  get(const KeyType & key) {
-      for (ConstItr it = theContainer.begin(); it != theContainer.end(); it++) {
-        if ( it->first == key) return it->second;
-      }
-      return 0;
-    }
+    using ValueType = RecHitsSortedInPhi;
+    using KeyType = int;
+    SimpleCache(unsigned int initSize) : theContainer(initSize, nullptr){}
+    ~SimpleCache() { clear(); }
+    void resize(int size) { theContainer.resize(size,nullptr); }
+    const ValueType*  get(KeyType key) { return theContainer[key];}
     /// add object to cache. It is caller responsibility to check that object is not yet there.
-    void add(const KeyType & key, const ValueType * value) {
-      theContainer.push_back( std::make_pair(key,value));
+    void add(KeyType key, const ValueType * value) {
+      if (key>=int(theContainer.size())) resize(key+1);
+      theContainer[key]=value;
     }
     /// emptify cache, delete values associated to Key
-    virtual void clear() {
-      for (ConstItr i=theContainer.begin(); i!= theContainer.end(); i++) { delete i->second; }
-      theContainer.clear();
+    void clear() {
+      for ( auto & v : theContainer)  { delete v; v=nullptr;}
     }
-  protected:
-    typedef std::pair< KeyType, const ValueType * > KeyValuePair;
-    std::vector< KeyValuePair > theContainer;
-    typedef typename std::vector< KeyValuePair >::const_iterator ConstItr;
+  private:
+    std::vector< const ValueType *> theContainer;
   private:
     SimpleCache(const SimpleCache &) { }
   };
 
 private:
-  typedef const DetLayer * LayerRegionKey;
-  typedef SimpleCache<LayerRegionKey, RecHitsSortedInPhi> Cache;
- public:
-  LayerHitMapCache(int initSize=50) { theCache = new Cache(initSize); }
+  typedef SimpleCache Cache;
+public:
+  LayerHitMapCache(unsigned int initSize=50) : theCache(initSize) { }
 
-  ~LayerHitMapCache() { delete theCache; }
-
-  void clear() { theCache->clear(); }
-
-  const RecHitsSortedInPhi & operator()(
-      const ctfseeding::SeedingLayer * layer, const TrackingRegion & region, 
-      const edm::Event & iEvent, const edm::EventSetup & iSetup) {
-    LayerRegionKey key(layer->detLayer());
-    const RecHitsSortedInPhi * lhm = theCache->get(key);
-    if (lhm==0) {
+  void clear() { theCache.clear(); }
+  
+  const RecHitsSortedInPhi & 
+  operator()(const ctfseeding::SeedingLayer * layer, const TrackingRegion & region, 
+	     const edm::Event & iEvent, const edm::EventSetup & iSetup) {
+    int key = layer->detLayer()->seqNum();
+    assert (key>=0);
+    const RecHitsSortedInPhi * lhm = theCache.get(key);
+    if (lhm==nullptr) {
       lhm=new RecHitsSortedInPhi (region.hits(iEvent,iSetup,layer));
       LogDebug("LayerHitMapCache")<<" I got"<< lhm->all().second-lhm->all().first<<" hits in the cache for: "<<layer->detLayer();
-      theCache->add( key, lhm); 
+      theCache.add( key, lhm); 
     }
     else{
       LogDebug("LayerHitMapCache")<<" I got"<< lhm->all().second-lhm->all().first<<" hits FROM THE cache for: "<<layer->detLayer();
@@ -69,11 +62,9 @@ private:
     return *lhm;
   }
 
-public:
-  LayerHitMapCache(const LayerHitMapCache &) { }
 
 private:
-  Cache * theCache; 
+  Cache theCache; 
 };
 
 #endif
