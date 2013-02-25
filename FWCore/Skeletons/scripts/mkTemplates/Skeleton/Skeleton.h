53fdc63fb720f45e5fc642662fb0f9dc7d39a4b1
Valentin Y Kuznetsov <sha1-2e4e43c3894d54a6c1d0fa422e76e2bed321960f@cern.ch>
2013-02-25 15:48:21 +0000

    New Skeleton templates

---
+++ FWCore/Skeletons/scripts/mkTemplates/Skeleton/Skeleton.h
@@ -0,0 +1,51 @@
+#ifndef __subsys_____pkgname_____class___h
+#define __subsys_____pkgname_____class___h
+// -*- C++ -*-
+//
+// Package:     __pkgname__
+// Class  :     __class__
+// 
+/**\class __class__ __class__.h "__incdir____class__.h"
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  __author__
+//         Created:  __date__
+// __rcsid__
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+
+class __class__
+{
+
+   public:
+      __class__();
+      virtual ~__class__();
+
+      // ---------- const member functions ---------------------
+
+      // ---------- static member functions --------------------
+
+      // ---------- member functions ---------------------------
+
+   private:
+      __class__(const __class__&); // stop default
+
+      const __class__& operator=(const __class__&); // stop default
+
+      // ---------- member data --------------------------------
+
+};
+
+
+#endif
