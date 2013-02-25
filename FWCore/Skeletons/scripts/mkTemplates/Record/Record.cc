8c056faa303014795c8437794548e7d490dd5d44
Valentin Y Kuznetsov <sha1-2e4e43c3894d54a6c1d0fa422e76e2bed321960f@cern.ch>
2013-02-25 15:40:45 +0000

    New templates

---
+++ FWCore/Skeletons/scripts/mkTemplates/Record/Record.cc
@@ -0,0 +1,16 @@
+// -*- C++ -*-
+//
+// Package:     __class__
+// Class  :     __class__
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      __author__
+// Created:     __date__
+// __rcsid__
+
+#include "__subsys__/__class__/interface/__class__.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(__class__);
---
+++ FWCore/Skeletons/scripts/mkTemplates/Record/Record.h
@@ -0,0 +1,26 @@
+#ifndef __class_____class___h
+#define __class_____class___h
+// -*- C++ -*-
+//
+// Package:     __class__
+// Class  :     __class__
+// 
+/**\class __class__ __class__.h __author__/CMSSW/__pkgname__/__class__.h
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      __author__
+// Created:     __date__
+// __rcsid__
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class __class__ : public edm::eventsetup::EventSetupRecordImplementation<__class__> {};
+
+#endif
