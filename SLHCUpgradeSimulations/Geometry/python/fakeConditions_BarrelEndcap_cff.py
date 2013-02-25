68d3219e953f58a395439debca1cceda3a586fd3
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2013-02-25 12:45:09 +0000

    nor more test directory

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_BarrelEndcap_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_BarrelEndcap_cff.py
@@ -2,13 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 siPixelFakeGainOfflineESSource = cms.ESSource("SiPixelFakeGainOfflineESSource",
         file = 
-cms.FileInPath('SLHCUpgradeSimulations/Geometry/test/EmptyPixelSkimmedGeometry.txt')
+cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/BarrelEndcap/EmptyPixelSkimmedGeometry.txt')
         )
 es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFakeGainOfflineESSource")
 
 siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
         file = 
-cms.FileInPath('SLHCUpgradeSimulations/Geometry/test/PixelSkimmedGeometry.txt')
+cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseII/BarrelEndcap/PixelSkimmedGeometry.txt')
         )
 es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
 
