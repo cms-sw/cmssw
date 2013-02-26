27151dd97cc2c3506cd40891b4812eb49f388123
Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch>
2013-02-26 16:52:48 +0000

    Use new API for beginRun.

--- L1Trigger/DTTrigger/interface/DTTrigProd.h
+++ L1Trigger/DTTrigger/interface/DTTrigProd.h
@@ -4,8 +4,8 @@
  *     Main EDProducer for the DTTPG
  *
  *
- *   $Date: 2009/11/02 14:18:30 $
- *   $Revision: 1.7 $
+ *   $Date: 2011/03/28 13:47:50 $
+ *   $Revision: 1.8 $
  *
  *   \author C. Battilana
  *
@@ -37,7 +37,7 @@ public:
 
   //! Create Trigger Units before starting event processing
   //void beginJob(const edm::EventSetup & iEventSetup);
-  void beginRun(edm::Run& iRun, const edm::EventSetup& iEventSetup);
+  void beginRun(edm::Run const& iRun, const edm::EventSetup& iEventSetup) override;
   
   //! Producer: process every event and generates trigger data
   void produce(edm::Event & iEvent, const edm::EventSetup& iEventSetup);
