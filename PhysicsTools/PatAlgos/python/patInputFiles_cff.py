e91ede190af5e1a77abadc4a123c243cb1e88e41
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2013-03-05 15:50:21 +0000

    Updated input RelVals.

--- PhysicsTools/PatAlgos/python/patInputFiles_cff.py
+++ PhysicsTools/PatAlgos/python/patInputFiles_cff.py
@@ -2,37 +2,37 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
 
 filesRelValProdTTbarAODSIM = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_1'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START61_V5'
+                        , globalTag     = 'START61_V11'
                         , dataTier      = 'AODSIM'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesRelValProdTTbarGENSIMRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_1'
                         , relVal        = 'RelValProdTTbar'
-                        , globalTag     = 'START61_V5'
+                        , globalTag     = 'START61_V11'
                         , dataTier      = 'GEN-SIM-RECO'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesRelValTTbarPileUpGENSIMRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_1'
                         , relVal        = 'RelValTTbar'
-                        , globalTag     = 'PU_START61_V5'
+                        , globalTag     = 'PU_START61_V11'
                         , dataTier      = 'GEN-SIM-RECO'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
     )
 filesSingleMuRECO = cms.untracked.vstring(
-    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_0_pre6'
+    pickRelValInputFiles( cmsswVersion  = 'CMSSW_6_1_1'
                         , relVal        = 'SingleMu'
                         , dataTier      = 'RECO'
-                        , globalTag     = 'GR_R_61_V3_RelVal_mu2012A'
+                        , globalTag     = 'GR_R_61_V6_RelVal_mu2012C'
                         , maxVersions   = 1
                         , numberOfFiles = 1
                         )
