b5dd17d84bcb850fcf1d180e363a263249d03290
Francesca Cavallari <sha1-cf24814f6426e55741f66a9979d2192918cf784d@cern.ch>
2013-03-11 09:35:03 +0000

    removed permanently unnecessary files

--- CondFormats/DataRecord/interface/EcalConstantTermsRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef CondFormats_DataRecord_EcalConstantTermsRcd_H
-#define CondFormats_DataRecord_EcalConstantTermsRcd_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class EcalConstantTermsRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalConstantTermsRcd> {};
-#endif
--- CondFormats/DataRecord/interface/EcalHFNoiseRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef ECALHFNOISERCD_H
-#define ECALHFNOISERCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class EcalHFNoiseRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalHFNoiseRcd> {};
-#endif
