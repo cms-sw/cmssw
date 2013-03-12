3d6f7bc2b65a924d348dd949b0a56306f8946086
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2013-03-12 15:58:00 +0000

    Prepend the name block8TeV with an underscore so it does not get attached to the Process in the python configuration. It is just a temporary and the way it is used will cause a check that will soon be added to the Framework to fail.

--- FastSimulation/PileUpProducer/python/PileUpSimulator_8TeV_cff.py
+++ FastSimulation/PileUpProducer/python/PileUpSimulator_8TeV_cff.py
@@ -1,7 +1,7 @@
-from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import PileUpSimulatorBlock as block8TeV
+from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import PileUpSimulatorBlock as _block8TeV
 from FastSimulation.Configuration.MixingFamos_cff import *
 
 #define the PU scenario itself
-famosPileUp.PileUpSimulator = block8TeV.PileUpSimulator
+famosPileUp.PileUpSimulator = _block8TeV.PileUpSimulator
 
 famosPileUp.PileUpSimulator.averageNumber = 0.000000
