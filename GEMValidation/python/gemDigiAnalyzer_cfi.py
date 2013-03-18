089f49f7effd06b56f5f7bc9a89f5107144b99a5
dildick <>
2013-03-18 08:42:10 +0000

    deleting old files

--- GEMValidation/python/gemDigiAnalyzer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-gemDigiAnalyzer = cms.EDAnalyzer("GEMDigiAnalyzer",
-    verbosoty = cms.untracked.int32(1),
-    inputTagRPC = cms.untracked.InputTag("simMuonRPCDigis"),
-    inputTagGEM = cms.untracked.InputTag("simMuonGEMDigis")
-)
-
--- GEMValidation/python/gemSimHitAnalyzer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-gemSimHitAnalyzer = cms.EDAnalyzer("GEMSimHitAnalyzer",
-                                   minTrackPt = cms.double(10.0),
-)
