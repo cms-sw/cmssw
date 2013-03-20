153e6132c2d759af2b5de7e66e2ed495dbb5510a
Tai Sakuma <sha1-83aadde34f3854f55faaed54e7ebb52fa93fe00c@cern.ch>
2013-03-20 19:17:34 +0000

    turned off MET significance calculation

--- RecoMET/METProducers/python/trackMET_cfi.py
+++ RecoMET/METProducers/python/trackMET_cfi.py
@@ -1,22 +1,24 @@
 import FWCore.ParameterSet.Config as cms
+# $Id: RecoMET_cff.py,v 1.19 2012/11/06 02:33:52 sakuma Exp $
 
+##____________________________________________________________________________||
+pfCandidatesForTrackMet = cms.EDProducer(
+    "PFCandidatesForTrackMETProducer",
+    PFCollectionLabel = cms.InputTag("particleFlow"),
+    PVCollectionLabel = cms.InputTag("offlinePrimaryVertices"),
+    dzCut = cms.double(0.2),
+    neutralEtThreshold = cms.double(-1.0)
+    )
 
-pfCandidatesForTrackMet = cms.EDProducer("PFCandidatesForTrackMETProducer",
-                                         PFCollectionLabel = cms.InputTag("particleFlow"),
-                                         PVCollectionLabel = cms.InputTag("offlinePrimaryVertices"),
-                                         dzCut = cms.double(0.2),
-                                         neutralEtThreshold = cms.double(-1.0)
-                                         )
+##____________________________________________________________________________||
+trackMet = cms.EDProducer(
+    "METProducer",
+    src = cms.InputTag("pfCandidatesForTrackMet"),
+    METType = cms.string('PFMET'),
+    alias = cms.string('PFMET'),
+    globalThreshold = cms.double(0.0),
+    InputType = cms.string('PFCandidateCollection'),
+    calculateSignificance = cms.bool(False),
+    )
 
-from RecoMET.METProducers.METSigParams_cfi import *
-trackMet = cms.EDProducer("METProducer",
-                          METSignificance_params,
-                          src = cms.InputTag("pfCandidatesForTrackMet"),
-                          METType = cms.string('PFMET'),
-                          alias = cms.string('PFMET'),
-                          noHF = cms.bool(False),
-                          globalThreshold = cms.double(0.0),
-                          InputType = cms.string('PFCandidateCollection'),
-                          calculateSignificance = cms.bool(True),
-                          jets = cms.InputTag("ak5PFJets") #used for significance calculation
-                          )
+##____________________________________________________________________________||
