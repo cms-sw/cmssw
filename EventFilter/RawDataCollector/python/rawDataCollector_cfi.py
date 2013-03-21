49f6364ac7cb592c71560353090acf24456e349a
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2013-03-21 16:02:56 +0000

    Use RawDataCollectByLabel everywhere and remove RawDataCollectorModule. RawDataCollectorModule will not work in unscheduled mode (which is required for the threaded Framework) and the other module will work in scheduled and unscheduled mode.

--- EventFilter/RawDataCollector/python/rawDataCollector_cfi.py
+++ EventFilter/RawDataCollector/python/rawDataCollector_cfi.py
@@ -1,11 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-# Default is to merge all FEDRawDataCollections, regardless of origin
-# It is possible to merge collections from the current process only
-# This switch is needed in case a FEDRawDataCollection is already 
-# present but is not wanted 
-rawDataCollector = cms.EDProducer("RawDataCollectorModule",
-    currentProcessOnly = cms.bool(True)
+rawDataCollector = cms.EDProducer("RawDataCollectorByLabel",
+    verbose = cms.untracked.int32(0),     # 0 = quiet, 1 = collection list, 2 = FED list
+    RawCollectionList = cms.VInputTag( cms.InputTag('SiStripDigiToRaw'),
+                                       cms.InputTag('castorRawData'),
+                                       cms.InputTag('cscpacker', 'CSCRawData'),
+                                       cms.InputTag('csctfpacker', 'CSCTFRawData'),
+                                       cms.InputTag('dtpacker'),
+                                       cms.InputTag('dttfpacker'),
+                                       cms.InputTag('ecalPacker'),
+                                       cms.InputTag('esDigiToRaw'),
+                                       cms.InputTag('gctDigiToRaw'),
+                                       cms.InputTag('hcalRawData'),
+                                       cms.InputTag('l1GtEvmPack'),
+                                       cms.InputTag('l1GtPack'),
+                                       cms.InputTag('rpcpacker'),
+                                       cms.InputTag('siPixelRawData')
+    ),
 )
-
 
--- EventFilter/RawDataCollector/src/SealModule.cc
+++ EventFilter/RawDataCollector/src/SealModule.cc
@@ -1,11 +1,9 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-#include "EventFilter/RawDataCollector/interface/RawDataCollectorModule.h"
 #include "EventFilter/RawDataCollector/src/RawDataCollectorByLabel.h"
 
 using namespace edm::serviceregistry;
 
-DEFINE_FWK_MODULE(RawDataCollectorModule);
 DEFINE_FWK_MODULE(RawDataCollectorByLabel);
 
