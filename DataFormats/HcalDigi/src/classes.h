3f3446cbb2ce4e591c56b46746be3c927839a05c
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2013-03-27 14:53:32 +0000

    Introducing HcalUpgradeQIESamples used by HcalUpgradeDataFrame

--- DataFormats/HcalDigi/src/classes.h
+++ DataFormats/HcalDigi/src/classes.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include "DataFormats/Common/interface/SortedCollection.h"
 #include "DataFormats/HcalDigi/interface/HcalQIESample.h"
+#include "DataFormats/HcalDigi/interface/HcalUpgradeQIESample.h"
 #include "DataFormats/HcalDigi/interface/HBHEDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HFDataFrame.h"
 #include "DataFormats/HcalDigi/interface/HODataFrame.h"
@@ -15,6 +16,7 @@
 namespace {
   struct dictionary {
     std::vector<HcalQIESample> vQIE_;
+    std::vector<HcalUpgradeQIESample> vUQIE_;
     std::vector<HcalTriggerPrimitiveSample> vTPS_;
     
     edm::SortedCollection<HBHEDataFrame> vHBHE_;
