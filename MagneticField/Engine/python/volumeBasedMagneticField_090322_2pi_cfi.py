0cfd323e9cba42d36625147c9077054062e9524a
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2013-03-30 21:15:39 +0000

    table spec in cfg

--- MagneticField/Engine/python/volumeBasedMagneticField_090322_2pi_cfi.py
+++ MagneticField/Engine/python/volumeBasedMagneticField_090322_2pi_cfi.py
@@ -38,7 +38,52 @@ VolumeBasedMagneticFieldESProducer = cms.ESProducer("VolumeBasedMagneticFieldESP
     cacheLastVolume = cms.untracked.bool(True),
     overrideMasterSector = cms.bool(False),
     scalingVolumes = cms.vint32(),
-    scalingFactors = cms.vdouble()
+    scalingFactors = cms.vdouble(),
+
+    gridFiles = cms.VPSet(
+        cms.PSet( # Default tables, replicate sector 1
+            volumes   = cms.string('1-312'),
+            sectors   = cms.string('0') ,
+            master    = cms.int32(1),
+            path      = cms.string('grid.[v].bin'),
+        ),
+
+        cms.PSet( # Specific volumes in Barrel, sector 3
+            volumes   = cms.string('176-186,231-241,286-296'),
+            sectors   = cms.string('3') ,
+            master    = cms.int32(3),
+            path      = cms.string('S3/grid.[v].bin'),
+        ),
+
+        cms.PSet( # Specific volumes in Barrel, sector 4
+            volumes   = cms.string('176-186,231-241,286-296'),
+            sectors   = cms.string('4') ,
+            master    = cms.int32(4),
+            path      = cms.string('S4/grid.[v].bin'),
+        ),
+
+        cms.PSet(  # Specific volumes in Barrel and endcaps, sector 9
+            volumes   = cms.string('14,15,20,21,24-27,32,33,40,41,48,49,56,57,62,63,70,71,286-296'),
+            sectors   = cms.string('9') ,
+            master    = cms.int32(9),
+            path      = cms.string('S9/grid.[v].bin'),
+        ),
+
+        cms.PSet(  # Specific volumes in Barrel and endcaps, sector 10
+            volumes   = cms.string('14,15,20,21,24-27,32,33,40,41,48,49,56,57,62,63,70,71,286-296'),
+            sectors   = cms.string('10') ,
+            master    = cms.int32(10),
+            path      = cms.string('S10/grid.[v].bin'),
+        ),
+                                                        
+        cms.PSet( # Specific volumes in Barrel and endcaps, sector 11
+            volumes   = cms.string('14,15,20,21,24-27,32,33,40,41,48,49,56,57,62,63,70,71,286-296'),
+            sectors   = cms.string('11') ,
+            master    = cms.int32(11),
+            path      = cms.string('S11/grid.[v].bin'),
+        ),
+    )
+                                                    
 )
 
 
