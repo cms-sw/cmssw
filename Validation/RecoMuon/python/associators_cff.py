2633e4d454687fd30ffab81189faf2135a8f3bff
Andrea Perrotta <sha1-53ee1c4bcae388a998de3c3ad14e27529ab5dcac@cern.ch>
2013-04-03 10:17:39 +0000

    replace TrackAssociatorByHits with quickTrackAssociatorByHits

--- Validation/RecoMuon/python/associators_cff.py
+++ Validation/RecoMuon/python/associators_cff.py
@@ -5,15 +5,16 @@ from Validation.RecoMuon.selectors_cff import *
 
 #TrackAssociation
 from SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi import *
-import SimTracker.TrackAssociation.TrackAssociatorByHits_cfi
+import SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi
 import SimTracker.TrackAssociation.TrackAssociatorByPosition_cfi
 
-TrackAssociatorByHits = SimTracker.TrackAssociation.TrackAssociatorByHits_cfi.TrackAssociatorByHits.clone()
+TrackAssociatorByHits = SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi.quickTrackAssociatorByHits.clone()
+TrackAssociatorByHits.ComponentName = 'TrackAssociatorByHits'
 
-OnlineTrackAssociatorByHits = SimTracker.TrackAssociation.TrackAssociatorByHits_cfi.TrackAssociatorByHits.clone()
+OnlineTrackAssociatorByHits = SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi.quickTrackAssociatorByHits.clone()
 OnlineTrackAssociatorByHits.ComponentName = 'OnlineTrackAssociatorByHits'
-OnlineTrackAssociatorByHits.UseGrouped = False
-OnlineTrackAssociatorByHits.UseSplitting = False
+#OnlineTrackAssociatorByHits.UseGrouped = False
+#OnlineTrackAssociatorByHits.UseSplitting = False
 OnlineTrackAssociatorByHits.ThreeHitTracksAreSpecial = False
 
 TrackAssociatorByPosDeltaR = SimTracker.TrackAssociation.TrackAssociatorByPosition_cfi.TrackAssociatorByPosition.clone()
