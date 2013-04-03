43a0e4b14bb22b00171a15802dffa853fe53cc12
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2013-04-03 16:45:46 +0000

    PileupSummaryInfo object created also for the new mixing

--- FastSimulation/Configuration/python/MixingHitsAndTracks_cff.py
+++ FastSimulation/Configuration/python/MixingHitsAndTracks_cff.py
@@ -1,8 +1,14 @@
 #from FastSimulation.Configuration.mixNoPU_cfi import * 
 
+# PileupSummaryInfo is taken from mixHits because this is the one that can handle OOT pileup
+from SimGeneral.PileupInformation.AddPileupSummary_cfi import *
+addPileupInfo.PileupMixingLabel = 'mixSimCaloHits'
+addPileupInfo.simHitLabel = 'g4SimHits'
+
 from FastSimulation.Configuration.mixHitsWithPU_cfi import *
 mixHits = cms.Sequence(
-    mixSimCaloHits
+    mixSimCaloHits+
+    addPileupInfo
     )
     
 from FastSimulation.Configuration.mixTracksWithPU_cfi import *
