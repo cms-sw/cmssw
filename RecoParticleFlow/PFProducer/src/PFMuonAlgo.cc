fbc2411fdfdac424523a65ef6734f4060f9dd974
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2013-04-03 18:00:20 +0000

    Small tunings and improvements on the PF post cleaning configuration based on studies done in data after backporting to 53X

--- RecoParticleFlow/PFProducer/src/PFMuonAlgo.cc
+++ RecoParticleFlow/PFProducer/src/PFMuonAlgo.cc
@@ -814,8 +814,8 @@ void PFMuonAlgo::estimateEventQuantities(const reco::PFCandidateCollection* pfc)
     sumet_+=i->pt();
 
     if (vertices_->size()>0 && vertices_->at(0).isValid()&& !vertices_->at(0).isFake()) {
-      //If charged and from PV
-      if( i->charge() !=0 && i->trackRef().isNonnull() && vertices_->size()>0&& i->trackRef()->dz(vertices_->at(0).position())<dzPV_) {
+      //If charged and from PV or muon
+      if( (i->charge() !=0 && i->trackRef().isNonnull() && vertices_->size()>0&& i->trackRef()->dz(vertices_->at(0).position())<dzPV_)||(abs(i->pdgId())==13)) {
 	METXCh+=i->px();
 	METYCh+=i->py();
       }
@@ -827,7 +827,7 @@ void PFMuonAlgo::estimateEventQuantities(const reco::PFCandidateCollection* pfc)
       //Neutral
       else if( !(i->charge() !=0 && i->trackRef().isNonnull())) {
 	METXNeut+=i->px();
-      METYNeut+=i->py();
+	METYNeut+=i->py();
       }
     } //else if we dont have a vertex make standard PFMET
     else {
@@ -994,7 +994,7 @@ void PFMuonAlgo::addMissingMuons(edm::Handle<reco::MuonCollection> muons, reco::
   
     std::vector<reco::Muon::MuonTrackTypePair> tracks  = goodMuonTracks(muonRef,true);
     //If there is more than 1 track choice  try to change the track 
-    if(tracks.size()>1) {
+    if(tracks.size()>0) {
 
     //Find tracks that change dramatically MET or Pt
     std::vector<reco::Muon::MuonTrackTypePair> tracksThatChangeMET = tracksPointingAtMET(tracks);
