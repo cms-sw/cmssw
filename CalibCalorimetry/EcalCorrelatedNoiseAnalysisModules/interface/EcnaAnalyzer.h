13e3e3d9cf618eee630fe1dd3fa6422fedf7be77
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2013-04-05 20:17:20 +0000

    Pass TString by const reference, not by value.

--- CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/interface/EcnaAnalyzer.h
+++ CalibCalorimetry/EcalCorrelatedNoiseAnalysisModules/interface/EcnaAnalyzer.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Bernard Fabbro
 //         Created:  Fri Jun  2 10:27:01 CEST 2006
-// $Id: EcnaAnalyzer.h,v 1.1 2010/04/09 08:57:35 fabbro Exp $
+// $Id: EcnaAnalyzer.h,v 1.2 2011/03/21 14:57:41 fabbro Exp $
 //
 //
 
@@ -138,7 +138,7 @@ class EcnaAnalyzer : public edm::EDAnalyzer {
   Int_t   gainvalue(const Int_t&);
   void    CheckMsg(const Int_t&, const Int_t&);
   void    CheckMsg(const Int_t&);
-  Bool_t  AnalysisOutcome(const TString);
+  Bool_t  AnalysisOutcome(const TString&);
 
  private:
 
