1866f47bc5306ef6243c34f8a954a2e56680507f
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2013-04-09 10:54:31 +0000

    Adding untracked parameters explicitely

--- HLTrigger/HLTanalyzers/python/hlTrigReport_cfi.py
+++ HLTrigger/HLTanalyzers/python/hlTrigReport_cfi.py
@@ -2,6 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 #
 hlTrigReport = cms.EDAnalyzer("HLTrigReport",
-    HLTriggerResults = cms.InputTag("TriggerResults","","HLT")
+    HLTriggerResults = cms.InputTag("TriggerResults","","HLT"),
+    reportBy         = cms.untracked.string("job"),
+    resetBy          = cms.untracked.string("never"),
+    serviceBy        = cms.untracked.string("never")
 )
 
