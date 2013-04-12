4074c540f60a516af0f6cf976f486b0652aedc4d
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2013-04-12 08:16:18 +0000

    new scenarios for upgrades

---
+++ FastSimulation/PileUpProducer/python/mix_E8TeV_AVE_70_BX_inTimeOnly.py
@@ -0,0 +1,17 @@
+from FastSimulation.PileUpProducer.PileUpSimulator8TeV_cfi import *
+
+from FastSimulation.Configuration.CommonInputs_cff import *
+
+if (MixingMode==2):
+    # mix at SIM level:
+    from FastSimulation.Configuration.MixingHitsAndTracks_cff import *
+    mixSimCaloHits.input.nbPileupEvents.averageNumber = cms.double(70.0) 
+    mixSimCaloHits.input.type = cms.string('poisson')
+    mixRecoTracks.input.nbPileupEvents.averageNumber = cms.double(70.0) 
+    mixRecoTracks.input.type = cms.string('poisson')
+else:
+    # mix at GEN level:
+    from FastSimulation.Configuration.MixingFull_cff import *
+    mixGenPU.input.nbPileupEvents.averageNumber = cms.double(70.0) 
+    mixGenPU.input.type = cms.string('poisson')
+
