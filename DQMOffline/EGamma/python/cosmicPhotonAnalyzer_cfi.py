ae3bf2fc940f313829e12482c37981c6c404af6c
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2013-04-16 17:54:22 +0000

    Avoid potential future problems by cloning a module instead of giving the same global object a second name and changing its parameter values. Note the temporary name starts with underscore so it is not propagated up to the top level process by load or import *. This should not change the behavior of cosmicPhotonAnalysis, and if it changes the behavior of anything else it probably means there already is an undiscovered problem.

--- DQMOffline/EGamma/python/cosmicPhotonAnalyzer_cfi.py
+++ DQMOffline/EGamma/python/cosmicPhotonAnalyzer_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.EGamma.photonAnalyzer_cfi import photonAnalysis as cosmicPhotonAnalysis 
+from DQMOffline.EGamma.photonAnalyzer_cfi import photonAnalysis as _photonAnalysis 
+cosmicPhotonAnalysis = _photonAnalysis.clone()
 
 cosmicPhotonAnalysis.minPhoEtCut = cms.double(0.0)
 cosmicPhotonAnalysis.eMax = cms.double(3.0)
