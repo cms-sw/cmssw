0829323ec4c91d719553cc05c9d0449e58c7ded7
Nicolas Pierre Chanon <sha1-dc1081d72f70055f90623ceb19c685a609c9cac9@cern.ch>
2013-04-17 10:08:40 +0000

    fix message log and edmInputTag

--- RecoEcal/EgammaClusterProducers/src/RecHitFilter.cc
+++ RecoEcal/EgammaClusterProducers/src/RecHitFilter.cc
@@ -1,9 +1,9 @@
 /** \class RecHitFilter
  **   simple filter of EcalRecHits
  **
- **  $Id: RecHitFilter.cc,v 1.2 2007/03/08 19:11:10 futyand Exp $
- **  $Date: 2007/03/08 19:11:10 $
- **  $Revision: 1.2 $
+ **  $Id: RecHitFilter.cc,v 1.3 2013/04/09 14:30:49 chanon Exp $
+ **  $Date: 2013/04/09 14:30:49 $
+ **  $Revision: 1.3 $
  **  \author Shahram Rahatlou, University of Rome & INFN, May 2006
  **
  ***/
@@ -17,12 +17,12 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 // Reconstruction Classes
 #include "DataFormats/EcalRecHit/interface/EcalRecHit.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 #include "DataFormats/EcalDetId/interface/EBDetId.h"
-
 
 // Class header file
 #include "RecoEcal/EgammaClusterProducers/interface/RecHitFilter.h"
@@ -34,9 +34,9 @@ RecHitFilter::RecHitFilter(const edm::ParameterSet& ps)
   noiseEnergyThreshold_       = ps.getParameter<double>("noiseEnergyThreshold");
   //noiseEtThreshold_       = ps.getParameter<double>("noiseEtThreshold");
   noiseChi2Threshold_       = ps.getParameter<double>("noiseChi2Threshold");
-  hitProducer_          = ps.getParameter<std::string>("hitProducer");
-  hitCollection_        = ps.getParameter<std::string>("hitCollection");
-  reducedHitCollection_ = ps.getParameter<std::string>("reducedHitCollection");
+  hitProducer_          = ps.getParameter<edm::InputTag>("hitProducer");
+  hitCollection_        = ps.getParameter<edm::InputTag>("hitCollection");
+  reducedHitCollection_ = ps.getParameter<edm::InputTag>("reducedHitCollection");
 
   produces< EcalRecHitCollection >(reducedHitCollection_);
 }
@@ -54,7 +54,7 @@ void RecHitFilter::produce(edm::Event& evt, const edm::EventSetup& es)
   evt.getByLabel(hitProducer_, hitCollection_, rhcHandle);
   if (!(rhcHandle.isValid())) 
     {
-      std::cout << "could not get a handle on the EcalRecHitCollection!" << std::endl;
+      edm::LogError << "could not get a handle on the EcalRecHitCollection!" << std::endl;
       return;
     }
   const EcalRecHitCollection* hit_collection = rhcHandle.product();
@@ -74,7 +74,7 @@ void RecHitFilter::produce(edm::Event& evt, const edm::EventSetup& es)
     }
    
   }
-  std::cout << "total # hits: " << nTot << "  #hits with E > " << noiseEnergyThreshold_ << " GeV  and  chi2 < " <<  noiseChi2Threshold_ << " : " << nRed << std::endl;
+  edm::LogTrace << "total # hits: " << nTot << "  #hits with E > " << noiseEnergyThreshold_ << " GeV  and  chi2 < " <<  noiseChi2Threshold_ << " : " << nRed << std::endl;
 
   evt.put(redCollection, reducedHitCollection_);
 
