cf929083180e679b56bef0927187c0d3f1cfda80
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2013-04-19 07:36:50 +0000

    For upgrade simulation studies

--- CalibFormats/HcalObjects/interface/HcalCoderDb.h
+++ CalibFormats/HcalObjects/interface/HcalCoderDb.h
@@ -1,16 +1,14 @@
 #ifndef HCAL_CODER_DB_H
 #define HCAL_CODER_DB_H
 
-#include "CalibFormats/HcalObjects/interface/HcalChannelCoder.h"
-#include "CalibFormats/HcalObjects/interface/QieShape.h"
 #include "CalibFormats/HcalObjects/interface/HcalCoder.h"
 
 /** \class HcalCoderDb
     
     coder which uses DB services to convert to fC
     $Author: ratnikov
-    $Date: 2006/09/25 21:59:23 $
-    $Revision: 1.5 $
+    $Date: 2013/04/15 12:24:12 $
+    $Revision: 1.6 $
 */
 
 class HcalQIECoder;
@@ -25,17 +23,19 @@ public:
   virtual void adc2fC(const HFDataFrame& df, CaloSamples& lf) const;
   virtual void adc2fC(const ZDCDataFrame& df, CaloSamples& lf) const;
   virtual void adc2fC(const HcalCalibDataFrame& df, CaloSamples& lf) const;
+  virtual void adc2fC(const HcalUpgradeDataFrame& df, CaloSamples& lf) const;
+
   virtual void fC2adc(const CaloSamples& clf, HBHEDataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, HFDataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, HODataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, ZDCDataFrame& df, int fCapIdOffset) const;
   virtual void fC2adc(const CaloSamples& clf, HcalCalibDataFrame& df, int fCapIdOffset) const;
-  virtual void adc2fC(const HcalUpgradeDataFrame& df, CaloSamples& lf) const { }
-  virtual void fC2adc(const CaloSamples& clf, HcalUpgradeDataFrame& df, int fCapIdOffset) const { }
+  virtual void fC2adc(const CaloSamples& clf, HcalUpgradeDataFrame& df, int fCapIdOffset) const;
 
 private:
   template <class Digi> void adc2fC_ (const Digi& df, CaloSamples& clf) const;
   template <class Digi> void fC2adc_ (const CaloSamples& clf, Digi& df, int fCapIdOffset) const;
+  template <class Digi> void fCUpgrade2adc_ (const CaloSamples& clf, Digi& df, int fCapIdOffset) const;
 
   const HcalQIECoder* mCoder;
   const HcalQIEShape* mShape;
