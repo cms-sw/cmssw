20b1898abfeeea9915825bc6a6342e413235864d
dildick <>
2013-04-19 08:09:28 +0000

    Moving the validation script to bin/

--- GEMValidation/test/produceDigiValidationPlots_cfg.py
+++
@@ -1,9 +0,0 @@
-import os
-import FWCore.ParameterSet.Config as cms
-
-process = cms.PSet()
-process.verbose = cms.uint32( 1 )
-process.inputFile = cms.string('gem_digi_ana.MuonGunPt40Evt10k-8Part.root')
-process.targetDir = cms.string('/afs/cern.ch/user/d/dildick/www/GEMValidation/DigiL1CSC/8Part/MuonGunPt40Evt10kPU0/')
-process.ext = cms.string(".png")
-process.npads = cms.uint32( 24 )
