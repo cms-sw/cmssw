115cab68b68ae24529b6f86ae0220acf5f067a9c
Nicholas Wardle <sha1-066e6c0b134809cea872f24c1feeee084b77b70c@cern.ch>
2013-04-22 11:26:12 +0000

    higher dimension of models

--- HiggsAnalysis/CombinedLimit/test/scanScalefactorsFromWS.py
+++ HiggsAnalysis/CombinedLimit/test/scanScalefactorsFromWS.py
@@ -10,16 +10,22 @@ from optparse import OptionParser
 parser = OptionParser(usage="usage: %prog [options] file \nrun with --help to get list of options")
 parser.add_option("-M","--Model",dest="model",default="CvCf",type='str',help="Name of model used in model builder")
 parser.add_option("-m","--mh",dest="mh",default=125.8,type='float',help="Lightest Higgs Mass")
+parser.add_option("-e","--energydependance",dest="energydependant",action='store_true')
+parser.add_option("-s","--step",dest="stepsize",type='float',default=0.05)
+parser.add_option("","--slice",dest="sliceval",type='str', default = "")
 (options,args)=parser.parse_args()
 
 # Can do full matrix of prod*decay
-energies = ["7TeV"]
+energies = ["7TeV","8TeV"]
 production_channels 	= ["ggH","qqH","WH","ZH","VH","ttH"]
 decay_channels 		= ["hgg","hzz","hww","htt","hbb"]
 decay_modes 		= ["hgg","hvv","hff"]
 
 # Stepping of parameters in the model 
-step = 0.05
+step = options.stepsize
+
+# A way to keep x,y,z parameters for slice plots
+config ={}
 
 # Coloured plots
 def set_palette(ncontours=999):
@@ -62,14 +68,16 @@ def fillGrid(func,graph,txtfile,tree,c_vals):
 
 	  mu = func.getVal()
 
-	  if nparams == 2 : 
+	  if nparams == 2: 
 	 	graph.SetPoint(point,val[0],val[1],mu)
+		point+=1
+	  elif abs(config["sliceval"] - val[config["fixparameter"]]) < 0.001 and nparams < 4:
+	 	graph.SetPoint(point,val[config["xparameter"]],val[config["yparameter"]],mu)
 		point+=1
 
 	  txtfile.write("%2.4f\n"%(mu))
 	  parameter_vals["mu"][0]=mu
 	  tree.Fill()
-	  point += 1
 
 def fillOutputs(func,graph,txtfile,tree):
 
@@ -90,11 +98,23 @@ def fillOutputs(func,graph,txtfile,tree):
 
 def makePlot(name,tgraph):
 
-	c = ROOT.TCanvas("c","c",600,600)
 
+	#if options.sliceval:
 	it = params.createIterator()
-	xparam = it.Next()
-	yparam = it.Next()
+	if "xparameter_name" not in config.keys(): 
+	  for pcounter in range(nparams):
+		p = it.Next()
+		#if options.sliceval:
+			 #if pcounter == config["fixparameter"]: config["fixparameter_name"] = p.GetName()
+  		if pcounter == config["xparameter"] : config["xparameter_name"] = p.GetName()
+  		if pcounter == config["yparameter"] : config["yparameter_name"] = p.GetName()
+
+	xparam = params.find(config["xparameter_name"])
+	yparam = params.find(config["yparameter_name"])
+
+	if options.sliceval : zparam = params.find(config["fixparameter_name"])
+	
+	c = ROOT.TCanvas("c","c",600,600)
 
 	minxval = xparam.getMin()
 	maxxval = xparam.getMax()
@@ -103,6 +123,7 @@ def makePlot(name,tgraph):
 
 	thist = tgraph.GetHistogram()
 
+	if options.sliceval: name+="_%s_%.3f"%(zparam.GetName(),config["sliceval"])
 	thist.SetTitle(name)
 	thist.GetXaxis().SetTitle(xparam.GetName())
 	thist.GetYaxis().SetTitle(yparam.GetName())
@@ -132,12 +153,20 @@ def makePlot(name,tgraph):
 	#c.SetLogz()
 	#c.SaveAs("%s_logscale.pdf"%(name))
 
-def produceScan(modelname,extname,proddecay,work):
+def produceScan(modelname,extname,proddecaystring,work,energy=""):
 
 	# Get the appropriate mapping 
+	proddecay = proddecaystring+"_%s"%energy
 	name = "%s_%s_%s"%(modelname,extname,proddecay)
 	func = work.function(name)
-	if not func: return 
+	if not func: 
+		# mostly modesl will NOT be energy dependant
+	        #proddecay+="_%s"%energy	
+		proddecay = proddecaystring
+		name = "%s_%s_%s"%(modelname,extname,proddecay)
+		func = work.function(name)
+	if not func: #Give up!
+		return 
 
 	# Produce a plot of the scaling parameters
 	tgraph = ROOT.TGraph2D()
@@ -161,7 +190,7 @@ def produceScan(modelname,extname,proddecay,work):
 	fillOutputs(func,tgraph,txtfile,tr)
 
 	# make a plot 
-	if nparams == 2: makePlot("%s_%s"%(modelname,proddecay),tgraph)
+	if nparams == 2 or (options.sliceval and nparams <4): makePlot("%s_%s"%(modelname,proddecay),tgraph)
 	else: print "Skipping plots (nparams != 2)"
 	
 	# Write The Tree:
@@ -192,13 +221,41 @@ params.Print()
 
 parameter_vals	= {"mu":array.array('d',[0])}
 
-# create a dictionary object which containts name, empty array and ref to parm
+# create a dictionary object which containts name, empty array
 iter = params.createIterator()
+pcounter = 0
+index_x = 0
+index_y = 1
+index_z = 2
+
+doslice = False
+if options.sliceval: 
+	myFixed,sliceval = options.sliceval.split(",")
+	sliceval = float(sliceval)
+	doslice = True
+	config["sliceval"]=sliceval
+	config["fixparameter_name"]=myFixed
+
 while 1: 
   p = iter.Next()
   if p == None: break
   name = p.GetName()
+  if doslice and name == myFixed:
+	print "putting z index as ", pcounter 
+	index_z = pcounter
+	index_x = (pcounter+1)%3
+	index_y = (pcounter+2)%3
   parameter_vals[name] = array.array('d',[0])
+  pcounter+=1
+
+# config will say which index is x and y (and z)
+
+config["xparameter"]=index_x
+config["yparameter"]=index_y
+config["fixparameter"]=index_z
+
+
+print config
 
 #for p in params: print p.GetName()
 # Output file for ROOT TTrees
@@ -213,13 +270,16 @@ for decay in decay_modes:
 	produceScan(options.model,ext,decay,work)
 
 # Full Matrix sigma*BR available in Model:
-#for e in energies:
 for prod in production_channels:
    for decay in decay_channels:
-
+	
 	ext = "XSBRscal"
 	proddecay = "%s_%s"%(prod,decay)
-	produceScan(options.model,ext,proddecay,work)
+	if options.energydependant: 
+		for e in energies:
+			produceScan(options.model,ext,proddecay,work,energy=e)
+	else: 
+			produceScan(options.model,ext,proddecay,work)
 
 	
 
