885c2926a037b42446157b381410e9c08682e79b
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2013-04-22 17:16:53 +0000

    latest recipes for aging

--- SLHCUpgradeSimulations/Configuration/python/aging.py
+++ SLHCUpgradeSimulations/Configuration/python/aging.py
@@ -10,8 +10,12 @@ def customise_aging_300(process):
 
     if hasattr(process,'g4SimHits'):
         process.g4SimHits.HCalSD.DelivLuminosity = cms.double(300)  # integrated lumi in fb-1
-        process.g4SimHits.HCalSD.Darkening       = cms.untracked.bool(True)
-               
+        process.g4SimHits.HCalSD.HEDarkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HFDarkening       = cms.untracked.bool(True)
+        #these lines need to be further activiated by tuning on 'complete' aging for ecal 
+        process.g4SimHits.ECalSD.InstLuminosity = cms.double(1.0E34)
+        process.g4SimHits.ECalSD.DelivLuminosity = cms.double(300.)
+
 
     return process
 
@@ -26,7 +30,11 @@ def customise_aging_500(process):
 
     if hasattr(process,'g4SimHits'):
         process.g4SimHits.HCalSD.DelivLuminosity = cms.double(500)  # integrated lumi in fb-1
-        process.g4SimHits.HCalSD.Darkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HEDarkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HFDarkening       = cms.untracked.bool(True)
+        #these lines need to be further activiated by tuning on 'complete' aging for ecal 
+        process.g4SimHits.ECalSD.InstLuminosity = cms.double(1.0E34)
+        process.g4SimHits.ECalSD.DelivLuminosity = cms.double(500.)
 
     return process
 
@@ -40,7 +48,11 @@ def customise_aging_1000(process):
 
     if hasattr(process,'g4SimHits'):
         process.g4SimHits.HCalSD.DelivLuminosity = cms.double(3000)  # integrated lumi in fb-1
-        process.g4SimHits.HCalSD.Darkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HEDarkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HFDarkening       = cms.untracked.bool(True)
+        #these lines need to be further activiated by tuning on 'complete' aging for ecal 
+        process.g4SimHits.ECalSD.InstLuminosity = cms.double(1.0E34)
+        process.g4SimHits.ECalSD.DelivLuminosity = cms.double(1000.)
 
     return process
 
@@ -54,7 +66,27 @@ def customise_aging_3000(process):
 
     if hasattr(process,'g4SimHits'):
         process.g4SimHits.HCalSD.DelivLuminosity = cms.double(3000)  # integrated lumi in fb-1
-        process.g4SimHits.HCalSD.Darkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HEDarkening       = cms.untracked.bool(True)
+        process.g4SimHits.HCalSD.HFDarkening       = cms.untracked.bool(True)
+        #these lines need to be further activiated by tuning on 'complete' aging for ecal 
+        process.g4SimHits.ECalSD.InstLuminosity = cms.double(1.0E34)
+        process.g4SimHits.ECalSD.DelivLuminosity = cms.double(3000.)
 
     return process
     
+def ecal_complete_aging(proess):
+    if hasattr(process,'g4SimHits'):
+        process.g4SimHits.ECalSD.AgeingWithSlopeLY = cms.untracked.bool(True)
+    if hasattr(process,ecal_digi_parameters):    
+        process.ecal_digi_parameters.UseLCcorrection = cms.untracked.bool(False)
+    return process
+
+def turn_off_HE_aging(process):
+    if hasattr(process,'g4SimHits'):
+        process.g4SimHits.HHCalSD.HEDarkening       = cms.untracked.bool(False)
+    return process
+
+def turn_off_HF_aging(process):
+    if hasattr(process,'g4SimHits'):
+        process.g4SimHits.HCalSD.HFDarkening       = cms.untracked.bool(False)
+    return process
