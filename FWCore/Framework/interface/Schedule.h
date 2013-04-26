93f895fddf946c4ee7aae23815a1b6b95f6a2099
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2013-04-26 18:14:51 +0000

    Move UnscheduledCallProducer into its own file

--- FWCore/Framework/interface/Schedule.h
+++ FWCore/Framework/interface/Schedule.h
@@ -64,7 +64,7 @@
 #include "FWCore/Framework/interface/ExceptionHelpers.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/OccurrenceTraits.h"
-#include "FWCore/Framework/interface/UnscheduledHandler.h"
+#include "FWCore/Framework/interface/UnscheduledCallProducer.h"
 #include "FWCore/Framework/src/Path.h"
 #include "FWCore/Framework/src/RunStopwatch.h"
 #include "FWCore/Framework/src/Worker.h"
@@ -348,85 +348,6 @@ namespace edm {
   private:
     typename T::MyPrincipal&   ep;
     EventSetup const& es;
-  };
-
-  class UnscheduledCallProducer : public UnscheduledHandler {
-  public:
-    UnscheduledCallProducer() : UnscheduledHandler(), labelToWorkers_() {}
-    void addWorker(Worker* aWorker) {
-      assert(0 != aWorker);
-      labelToWorkers_[aWorker->description().moduleLabel()] = aWorker;
-    }
-
-    template <typename T>
-    void runNow(typename T::MyPrincipal& p, EventSetup const& es) {
-      //do nothing for event since we will run when requested
-      if(!T::isEvent_) {
-        for(std::map<std::string, Worker*>::iterator it = labelToWorkers_.begin(), itEnd=labelToWorkers_.end();
-            it != itEnd;
-            ++it) {
-          CPUTimer timer;
-          try {
-            it->second->doWork<T>(p, es, nullptr, &timer);
-          }
-          catch (cms::Exception & ex) {
-	    std::ostringstream ost;
-            if (T::isEvent_) {
-              ost << "Calling event method";
-            }
-            else if (T::begin_ && T::branchType_ == InRun) {
-              ost << "Calling beginRun";
-            }
-            else if (T::begin_ && T::branchType_ == InLumi) {
-              ost << "Calling beginLuminosityBlock";
-            }
-            else if (!T::begin_ && T::branchType_ == InLumi) {
-              ost << "Calling endLuminosityBlock";
-            }
-            else if (!T::begin_ && T::branchType_ == InRun) {
-              ost << "Calling endRun";
-            }
-            else {
-              // It should be impossible to get here ...
-              ost << "Calling unknown function";
-            }
-            ost << " for unscheduled module " << it->second->description().moduleName()
-                << "/'" << it->second->description().moduleLabel() << "'";
-            ex.addContext(ost.str());
-            ost.str("");
-            ost << "Processing " << p.id();
-            ex.addContext(ost.str());
-            throw;
-          }
-        }
-      }
-    }
-
-  private:
-    virtual bool tryToFillImpl(std::string const& moduleLabel,
-                               EventPrincipal& event,
-                               EventSetup const& eventSetup,
-                               CurrentProcessingContext const* iContext) {
-      std::map<std::string, Worker*>::const_iterator itFound =
-        labelToWorkers_.find(moduleLabel);
-      if(itFound != labelToWorkers_.end()) {
-        CPUTimer timer;
-        try {
-          itFound->second->doWork<OccurrenceTraits<EventPrincipal, BranchActionBegin> >(event, eventSetup, iContext, &timer);
-        }
-        catch (cms::Exception & ex) {
-	  std::ostringstream ost;
-          ost << "Calling produce method for unscheduled module " 
-              <<  itFound->second->description().moduleName() << "/'"
-              << itFound->second->description().moduleLabel() << "'";
-          ex.addContext(ost.str());
-          throw;
-        }
-        return true;
-      }
-      return false;
-    }
-    std::map<std::string, Worker*> labelToWorkers_;
   };
 
   void
