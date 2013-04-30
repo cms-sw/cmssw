763c6875d74733f316c0c1dc27a12f89704442e8
Dinko Ferencek <sha1-29555ec4b6c6892b86e99f356655c621a998e9d0@cern.ch>
2013-04-30 02:43:49 +0000

    added import of additional negative and positive taggers

--- RecoBTag/ImpactParameter/python/impactParameter_cff.py
+++ RecoBTag/ImpactParameter/python/impactParameter_cff.py
@@ -8,13 +8,13 @@ from RecoBTau.JetTagComputer.jetTagRecord_cfi import *
 # MVA
 from RecoBTag.ImpactParameter.impactParameterMVAComputer_cfi import *
 from RecoBTag.ImpactParameter.impactParameterMVABJetTags_cfi import *
-#B Jet Prob
+# Jet BProb
 from RecoBTag.ImpactParameter.jetBProbabilityComputer_cfi import *
 from RecoBTag.ImpactParameter.jetBProbabilityBJetTags_cfi import *
-#Jet Prob
+# Jet Prob
 from RecoBTag.ImpactParameter.jetProbabilityComputer_cfi import *
 from RecoBTag.ImpactParameter.jetProbabilityBJetTags_cfi import *
-# HighEff
+# High Eff
 from RecoBTag.ImpactParameter.trackCounting3D2ndComputer_cfi import *
 from RecoBTag.ImpactParameter.trackCountingHighEffBJetTags_cfi import *
 # High Purity
@@ -23,8 +23,26 @@ from RecoBTag.ImpactParameter.trackCountingHighPurBJetTags_cfi import *
 
 # Negative Tags
 
-from RecoBTag.ImpactParameter.negativeTrackCountingHighEffJetTags_cfi import *
-from RecoBTag.ImpactParameter.negativeTrackCountingHighPurJetTags_cfi import *
+# Jet BProb
+from RecoBTag.ImpactParameter.negativeOnlyJetBProbabilityComputer_cfi import *
+from RecoBTag.ImpactParameter.negativeOnlyJetBProbabilityJetTags_cfi import *
+# Jet Prob
+from RecoBTag.ImpactParameter.negativeOnlyJetProbabilityComputer_cfi import *
+from RecoBTag.ImpactParameter.negativeOnlyJetProbabilityJetTags_cfi import *
+# High Eff
 from RecoBTag.ImpactParameter.negativeTrackCounting3D2ndComputer_cfi import *
+from RecoBTag.ImpactParameter.negativeTrackCountingHighEffJetTags_cfi import *
+# High Purity
 from RecoBTag.ImpactParameter.negativeTrackCounting3D3rdComputer_cfi import *
+from RecoBTag.ImpactParameter.negativeTrackCountingHighPurJetTags_cfi import *
+
+# Positive Tags
+
+# Jet BProb
+from RecoBTag.ImpactParameter.positiveOnlyJetBProbabilityComputer_cfi import *
+from RecoBTag.ImpactParameter.positiveOnlyJetBProbabilityJetTags_cfi import *
+# Jet Prob
+from RecoBTag.ImpactParameter.positiveOnlyJetProbabilityComputer_cfi import *
+from RecoBTag.ImpactParameter.positiveOnlyJetProbabilityJetTags_cfi import *
+
 
