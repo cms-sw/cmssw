899deb309b8515d6b4c708054f36a7d59b6739da
Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch>
2013-04-30 11:58:25 +0000

    3 ged wf

--- Configuration/PyReleaseValidation/python/relval_ged.py
+++ Configuration/PyReleaseValidation/python/relval_ged.py
@@ -11,7 +11,9 @@ workflows[4001] = ['', ['TTbar','DIGIPU1','RECOPUDBG']]
 workflows[4002] = ['', ['ZEE','DIGI','RECO']]
 workflows[4003] = ['', ['ZMM','DIGI','RECO']]
 
-#workflows[4004] = ['QCD_FlatPt_15_3000', ['QCD_FlatPt_15_3000HS','DIGI','RECO']]
-#workflows[4005] = ['QCD_FlatPt_15_3000', ['QCD_FlatPt_15_3000HS','DIGIPU1','RECOPU1']]
+workflows[4004] = ['QCD_FlatPt_15_3000', ['QCD_FlatPt_15_3000HS','DIGI','RECO']]
+workflows[4005] = ['QCD_FlatPt_15_3000', ['QCD_FlatPt_15_3000HS','DIGIPU1','RECOPU1']]
 
-    
+workflows[4006] = ['', ['SingleElectronFlatPt1To100','DIGIPU1','RECOPU1']]
+workflows[4007] = ['',['QCD_Pt_30_80_BCtoE_8TeV','DIGIPU1','RECOPUDBG']]
+workflows[4008] = ['',['QCD_Pt_80_170_BCtoE_8TeV','DIGIPU1','RECOPUDBG']]
