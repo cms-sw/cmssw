65c954f4f8d22d8e6e8b3e3f2acdeec26ba535b3
Pal Hidas <sha1-f822214d57e65e8300149b247c2b1f4ae61e200e@cern.ch>
2013-04-30 19:50:46 +0000

    phase1 geometry

--- CondTools/SiPixel/test/SiPixelCondObjAllPayloadsReader_cfg.py
+++ CondTools/SiPixel/test/SiPixelCondObjAllPayloadsReader_cfg.py
@@ -5,9 +5,11 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load("Geometry.TrackerSimData.trackerSimGeometryXML_cfi")
 
-process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
-
-process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+# phase1
+#process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+#process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+process.load('Configuration.Geometry.GeometryExtendedPhaseIPixelReco_cff')
+process.load('Configuration.Geometry.GeometryExtendedPhaseIPixel_cff')
 
 process.load("CondTools.SiPixel.SiPixelGainCalibrationService_cfi")
 
@@ -16,7 +18,11 @@ process.TFileService = cms.Service("TFileService",
                                    )
 
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.CondDBCommon.connect = 'sqlite_file:prova.db'
+#process.CondDBCommon.connect = 'sqlite_file:prova_all_1440.db'
+#process.CondDBCommon.connect = 'sqlite_file:prova_all_1856.db'
+#process.CondDBCommon.connect = 'sqlite_file:prova_forhlt_1856.db'
+#process.CondDBCommon.connect = 'sqlite_file:prova_offline_1856.db'
+process.CondDBCommon.connect = 'sqlite_file:gain_empty_612_slhc1_offline.db'
 process.CondDBCommon.DBParameters.messageLevel = 2
 process.CondDBCommon.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
 
@@ -39,24 +45,29 @@ process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
 process.PoolDBESSource = cms.ESSource("PoolDBESSource",
     process.CondDBCommon,
     BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-    toGet = cms.VPSet(cms.PSet(
-        record = cms.string('SiPixelGainCalibrationRcd'),
-        tag = cms.string('GainCalibTestFull')
-    ), 
-        cms.PSet(
-            record = cms.string('SiPixelGainCalibrationForHLTRcd'),
-            tag = cms.string('GainCalibTestHLT')
-        ), 
+    toGet = cms.VPSet(
+##        cms.PSet(
+##            record = cms.string('SiPixelGainCalibrationRcd'),
+##            tag = cms.string('GainCalibTestFull')
+##        ), 
+#        cms.PSet(
+#            record = cms.string('SiPixelGainCalibrationForHLTRcd'),
+#            tag = cms.string('SiPixelGainCalibration_TBuffer_hlt_const')
+#            #tag = cms.string('GainCalibTestHLT')
+#        ), 
         cms.PSet(
             record = cms.string('SiPixelGainCalibrationOfflineRcd'),
-            tag = cms.string('GainCalibTestOffline')
-        ))
+            tag = cms.string('SiPixelGainCalibration_TBuffer_const')
+            #tag = cms.string('GainCalibTestOffline')
+        ),
+    )
 )
 
 process.prefer("PoolDBESSource")
 process.SiPixelCondObjAllPayloadsReader = cms.EDAnalyzer("SiPixelCondObjAllPayloadsReader",
     process.SiPixelGainCalibrationServiceParameters,
-    payloadType = cms.string('HLT')
+    #payloadType = cms.string('HLT')
+    payloadType = cms.string('Offline')
 )
 
 #process.print = cms.OutputModule("AsciiOutputModule")
