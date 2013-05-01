6432f54d41991cd14c72a2de2c2ac2dcc336f2cd
Andrea Giammanco <sha1-dd5ad124e0a6e1fb76a56e400ebd3457c2d4a27c@cern.ch>
2013-05-01 18:11:43 +0000

    activate HCAL digitizer

--- FastSimulation/Configuration/python/CommonInputs_cff.py
+++ FastSimulation/Configuration/python/CommonInputs_cff.py
@@ -34,7 +34,7 @@ from RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi import *
 # 2: as 0, but full digi + std local reco in HCAL
 # 3: full digi + std local reco in ECAL and HCAL; planned to become default soon
 
-CaloMode = 1
+CaloMode = 3
 
 # This flag is to switch between GEN-level and SIM/RECO-level pileup mixing
 # 1: GEN-level <---- DEFAULT
