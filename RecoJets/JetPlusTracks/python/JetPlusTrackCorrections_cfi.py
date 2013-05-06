a7007de86a65a9dd1593304fdc3e0c15db933065
Olga Kodolova <sha1-ea692fd16269a55c9895f16ae2b67a003b603c27@cern.ch>
2013-05-06 17:26:01 +0000

    New txt files for efficiency/response

--- RecoJets/JetPlusTracks/python/JetPlusTrackCorrections_cfi.py
+++ RecoJets/JetPlusTracks/python/JetPlusTrackCorrections_cfi.py
@@ -43,9 +43,9 @@ JPTZSPCorrectorICone5 = cms.PSet(
     DzVertexCut     = cms.double(0.2),
     
     # Response and efficiency maps
-    ResponseMap   = cms.string("CondFormats/JetMETObjects/data/CMSSW_3110_response.txt"),
-    EfficiencyMap = cms.string("CondFormats/JetMETObjects/data/CMSSW_362_TrackNonEff.txt"),
-    LeakageMap    = cms.string("CondFormats/JetMETObjects/data/CMSSW_362_TrackLeakage.txt"),
+    ResponseMap   = cms.string("CondFormats/JetMETObjects/data/CMSSW_538_response.txt"),
+    EfficiencyMap = cms.string("CondFormats/JetMETObjects/data/CMSSW_538_TrackNonEff.txt"),
+    LeakageMap    = cms.string("CondFormats/JetMETObjects/data/CMSSW_538_TrackLeakage.txt"),
 
     # Jet-related
     MaxJetEta = cms.double(3.0)
