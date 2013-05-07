2f1a2a80c2598793431e63bca844e0fd8c3b51f1
abenagli <>
2013-05-07 13:43:01 +0000

    first import

---
+++ Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalUncalIsolElectron_cff.py
@@ -0,0 +1,15 @@
+import FWCore.ParameterSet.Config as cms
+
+
+
+from Calibration.EcalAlCaRecoProducers.ALCARECOEcalCalIsolElectron_cff import *
+from Configuration.StandardSequences.RawToDigi_Data_cff import *
+from RecoLocalCalo.Configuration.RecoLocalCalo_cff import *
+
+ecalUncalibRecHitSequence = cms.Sequence(ecalGlobalUncalibRecHit * ecalDetIdToBeRecovered)
+uncalibRecHitSeq          = cms.Sequence((ecalDigis+ecalPreshowerDigis) * ecalUncalibRecHitSequence)
+
+seqALCARECOEcalUncalElectron = cms.Sequence( uncalibRecHitSeq )
+
+seqALCARECOEcalUncalZElectron = cms.Sequence( tagGsfSeq * seqALCARECOEcalUncalElectron)
+seqALCARECOEcalUncalWElectron = cms.Sequence( WSkimSeq  * seqALCARECOEcalUncalElectron) 
