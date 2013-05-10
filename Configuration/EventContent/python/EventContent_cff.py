fe40d8c659ed3ddd77e4d12430654901cabd94d4
Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch>
2013-05-10 09:19:35 +0000

    Fix for the REGEN event content

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -28,7 +28,7 @@ import FWCore.ParameterSet.Config as cms
 #
 #  FEVT (RAW+RECO), FEVTSIM (RAWSIM+RECOSIM), FEVTDEBUG (FEVTSIM+ALL_SIM_INFO), FEVTDEBUGHLT (FEVTDEBUG+HLTDEBUG)
 #
-#  $Id: EventContent_cff.py,v 1.53 2013/04/30 16:48:40 cerminar Exp $
+#  $Id: EventContent_cff.py,v 1.54 2013/05/01 15:44:29 mikeh Exp $
 #
 #
 #
@@ -712,6 +712,7 @@ REPACKRAWEventContent.outputCommands.extend(['drop FEDRawDataCollection_source_*
 #from modules in Configuration.StandardSequence.Generator_cff fixGenInfo
 REGENEventContent = cms.PSet(
     inputCommands=cms.untracked.vstring(
+      'keep *',
       'drop *_genParticles_*_*',
       'drop *_genParticlesForJets_*_*',
       'drop *_kt4GenJets_*_*',
