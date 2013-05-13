7b4ac36cac03ca4695ea6e19c49a3f39469ca731
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2013-05-13 11:01:57 +0000

    merging customs into one

---
+++ SLHCUpgradeSimulations/Configuration/python/combinedCustoms.py
@@ -0,0 +1,36 @@
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1
+from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE import customise as customiseBE
+from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import customise as customiseLB6PS
+from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import customise as customiseLB4LPS_2L2S
+from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE import l1EventContent as customise_ev_BE
+from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import l1EventContent as customise_ev_LB6PS
+from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import l1EventContent as customise_ev_LB4LPS_2L2S
+from SLHCUpgradeSimulations.Configuration.customise_mixing import customise_NoCrossing
+
+def cust_phase2_BE(process):
+    process=customisePostLS1(process)
+    process=customiseBE(process)
+    process=customise_ev_BE(process)
+    return process
+
+def cust_phase2_LB6PS(process):
+    process=customisePostLS1(process)
+    process=customiseLB6PS(process)
+    process=customise_ev_LB6PS(process)
+    return process
+
+def cust_phase2_LB4LPS_2L2S(process):
+    process=customisePostLS1(process)
+    process=customiseLB4LPS_2L2S(process)
+    process=customise_ev_LB4LPS_2L2S(process)
+    return process
+
+def noCrossing(process):
+    process=customise_NoCrossing(process)
+    return process
+
+
+
+
+
+    
