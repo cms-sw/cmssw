ad4cc66f50618e31ba0bf5ac07fcddcbdc09669f
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2013-05-13 16:40:29 +0000

    New unit test for EDAlias where we make an Ref from a product in a process later than the one where the EDAlias was produced. In response to a reported problem.

--- IOPool/Input/test/TestPoolInput.sh
+++ IOPool/Input/test/TestPoolInput.sh
@@ -43,6 +43,8 @@ cmsRun --parameter-set ${LOCAL_TEST_DIR}/PoolAliasTestStep1_cfg.py || die 'Failu
 
 cmsRun --parameter-set ${LOCAL_TEST_DIR}/PoolAliasTestStep2_cfg.py || die 'Failure using PoolAliasTestStep2_cfg.py' $?
 
+cmsRun --parameter-set ${LOCAL_TEST_DIR}/PoolAliasTestStep2A_cfg.py || die 'Failure using PoolAliasTestStep2A_cfg.py' $?
+
 cmsRun --parameter-set ${LOCAL_TEST_DIR}/PoolAliasSubProcessTestStep1_cfg.py || die 'Failure using PoolAliasSubProcessTestStep1_cfg.py' $?
 
 cmsRun --parameter-set ${LOCAL_TEST_DIR}/PoolAliasSubProcessTestStep2_cfg.py || die 'Failure using PoolAliasSubProcessTestStep2_cfg.py' $?
