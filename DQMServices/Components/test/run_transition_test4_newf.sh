6f6947aae5b8aa0beeed44ee592b2ca8dbf0c37f
Federico De Guio <sha1-209454cda4d3952545b45c4a26e8c7cc35979b24@cern.ch>
2013-05-17 13:48:47 +0000

    simple script to perform transition tests: first import

---
+++ DQMServices/Components/test/run_transition_test4_newf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function die { echo Failure $1: status $2 ; exit $2 ; }
+
+export LOCAL_TEST_DIR=$CMSSW_BASE/src/DQMServices/Components/python/test/
+echo ${LOCAL_TEST_DIR}
+
+for bookIn in BR
+do
+  #NEW file4
+  testConfig=create_file4_cfg.py
+  rm -f dqm_file4.root
+  echo ${testConfig} ------------------------------------------------------------
+  cmsRun ${LOCAL_TEST_DIR}/${testConfig} $bookIn  || die "cmsRun ${testConfig}" $?
+
+  #check file4
+  testConfig=check_file4.py 
+  echo ${testConfig} ------------------------------------------------------------
+  python ${LOCAL_TEST_DIR}/${testConfig} || die "cmsRun ${testConfig}" $?
+
+  #HARVEST SINGLE FILES 
+  testConfig=harv_file4_cfg.py
+  rm -f DQM_V0001_R00000000?__Test__File4__DQM.root
+  echo ${testConfig} ------------------------------------------------------------
+  cmsRun ${LOCAL_TEST_DIR}/${testConfig} || die "cmsRun ${testConfig}" $?
+
+  #CHECK HARVESTED FILE
+  echo COMPARING with reference ------------------------------------------------------------
+  compare_using_files.py -C -s b2b -t 0.999999 DQM_V0001_R000000001__Test__File4__DQM.root /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/DQM_V0001_R000000001__Test__File1_oldf_REFERENCE__DQM.root 
+  compare_using_files.py -C -s b2b -t 0.999999 DQM_V0001_R000000002__Test__File4__DQM.root /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/DQM_V0001_R000000002__Test__File3_REFERENCE__DQM.root 
+done
+
+exit 0
