8830f37130254275ca0e63f6c844105512cfd894
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2013-05-17 13:22:32 +0000

    Checking cfi files for py correctnes within FW cms py

---
+++ HLTrigger/Configuration/test/add/hltCheckCfis.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+import os
+
+dir = os.environ['CMSSW_RELEASE_BASE']+"/python"
+
+def check(dir) :
+    for root, dirs, files in os.walk(dir,followlinks=True):
+        for f in files:
+            if f[-1] != 'c' and f.find("_cfi.py") != -1:
+#               print root[len(dir)+1:], f
+                p = cms.Process("DUMMY1")
+                try:
+                    p.load(root[len(dir)+1:]+"/"+f[:-3])
+                except ImportError,e:
+                    print root[len(dir)+1:], f
+                    print "ImportError:",e
+                except RuntimeError, e:
+                    print root[len(dir)+1:], f
+                    print "RuntimeError:", e
+                except ValueError,e:
+                    print root[len(dir)+1:], f
+                    print "ValueError:",e
+                except NameError, e:
+                    print root[len(dir)+1:], f
+                    print "NameError:",e
+                except TypeError, e:
+                    print root[len(dir)+1:], f
+                    print "NameError:",e
+
+print "START"
+check(dir)
+print "STOP"
