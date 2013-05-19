ab2ad6133f0482e22f573a21fd2ba5eb30f404d7
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2013-05-19 16:14:25 +0000

    z-matching in window2013 + a table-based algorithm (Seb's cuts for BE)

--- SLHCUpgradeSimulations/L1TrackTrigger/python/HitMatchingAlgorithmRegister_cfi.py
+++ SLHCUpgradeSimulations/L1TrackTrigger/python/HitMatchingAlgorithmRegister_cfi.py
@@ -38,8 +38,28 @@ HitMatchingAlgorithm_window2012_PixelDigi_ = cms.ESProducer("HitMatchingAlgorith
 
 # Window2013 hit matching algorithm
 HitMatchingAlgorithm_window2013_PixelDigi_ = cms.ESProducer("HitMatchingAlgorithm_window2013_PixelDigi_",
-    minPtThreshold = cms.double(2.0),
+   minPtThreshold = cms.double(2.0),
+   zMatching = cms.bool(True),
 )
+
+# Tab2013 hit matching algorithm
+HitMatchingAlgorithm_tab2013_PixelDigi_ = cms.ESProducer("HitMatchingAlgorithm_tab2013_PixelDigi_",
+   zMatching = cms.bool(True),
+   BarrelCut = cms.vdouble( 0, 2.5, 2.5, 3.0, 4.5, 5.5, 7.0 ), #Use 0 as dummy to have direct access using DetId to the correct element
+   EndcapCutSet = cms.VPSet(
+     cms.PSet( EndcapCut = cms.vdouble( 0 ) ), #Use 0 as dummy to have direct access using DetId to the correct element
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.5, 1.5, 2.0, 2.0, 2.5, 3.0, 3.0, 3.5, 2.5, 3.0, 3.0, 4.5, 5.5, 5.5 ) ), #D1
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.0, 1.5, 2.0, 2.0, 2.5, 2.5, 3.0, 3.5, 2.0, 3.0, 3.0, 4.0, 4.0, 5.0 ) ), #D2 ...
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.0, 1.0, 1.5, 2.0, 2.0, 2.5, 3.0, 3.0, 2.0, 2.0, 3.0, 3.0, 4.0, 4.5 ) ),
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.0, 1.0, 1.5, 1.5, 2.0, 2.0, 2.5, 2.5, 3.5, 2.0, 2.5, 3.0, 3.0, 4.0 ) ),
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.0, 1.5, 1.5, 2.0, 2.0, 2.5, 2.5, 3.0, 4.0, 2.0, 2.5, 3.0, 3.5 ) ),
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.5, 1.5, 1.5, 2.0, 2.0, 2.5, 3.0, 3.5, 2.0, 2.5, 3.0, 3.0 ) ),
+     cms.PSet( EndcapCut = cms.vdouble( 0, 1.0, 1.5, 2.0, 2.0, 2.5, 2.5, 3.0, 4.0, 2.0, 2.5, 3.0 ) ), # missing rings are not taken into account in numbering, so everything
+                                                                                                      # always starts from 1 to N, with increasing r
+   )
+
+)
+
 
 # Thresholds hit matching algorithm
 
