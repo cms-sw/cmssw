8b6e3607e3750a680a2f1bbd3201c998bb000f05
Antonio Vilela Pereira <sha1-99b95d901683f11ac96ebd76fd2bbc9bee4e1aa0@cern.ch>
2013-05-20 18:02:37 +0000

    update

--- GeneratorInterface/PomwigInterface/test/analyzeSDDijets_cfg.py
+++ GeneratorInterface/PomwigInterface/test/analyzeSDDijets_cfg.py
@@ -7,7 +7,7 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring("file:POMWIG_SingleDiffractiveDijetsPlus_10TeV_Pt_30_cff_py_GEN.root")
+    fileNames = cms.untracked.vstring("file:POMWIG_SingleDiffractiveDijetsPlus_8TeV_Pt_30_cff_py_GEN.root")
 )
 
 #process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -41,8 +41,8 @@ process.genParticlesVisible = cms.EDFilter("GenJetParticleRefSelector",
 
 process.SDDijets = cms.EDAnalyzer("SDDijetsAnalyzer",
 	GenParticleTag = cms.InputTag("genParticles"),
-	GenJetTag = cms.InputTag("sisCone5GenJets"),
-        EBeam = cms.double(5000.0),
+	GenJetTag = cms.InputTag("ak5GenJets"),
+        EBeam = cms.double(4000.0),
 	debug = cms.untracked.bool(True)
 )
 
