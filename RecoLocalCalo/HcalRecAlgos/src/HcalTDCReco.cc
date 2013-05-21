0c1e3010fa82ca73b9e1a5221596d0e99746f791
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2013-05-21 13:05:50 +0000

    Corresponds to HcalTDC in V04-01-14   SimCalorimetry/HcalSimAlgos

--- RecoLocalCalo/HcalRecAlgos/src/HcalTDCReco.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HcalTDCReco.cc
@@ -11,21 +11,38 @@ void HcalTDCReco::reconstruct(const HcalUpgradeDataFrame & digi,
   int n = digi.size();
   double risingTime = -999.;
   double fallingTime = -999.;
-  int signalBX = 4;              // NB: HARDWIRED !!!
-  // start at bunch crossing 3
-  for(int i=3; i < n; ++i)
+  int signalBX = 5;              // NB: HARDWIRED !!!
+  int nbins = 50; // as in HcalTDCParameters.h (SimCalorimetry/HcalSimAlgos)
+  // start at bunch crossing 3 by default
+  for(int i = 3; i < n; ++i)
   {
     unsigned tdc = digi.tdc(i);
+
+    /* 
     unsigned rising = tdc & 0x7F;
-    // altered packing to tdcBins*2=7
     unsigned falling = (tdc >> 7) & 0x7F;
-    // only set the first time
-    if(risingTime < -998. && rising != 64 && rising != 65) {
-      risingTime = rising*25./64. + (i-signalBX)*25.;
+    */
+    // temporary "unpacking" instead, which directly corresponds to 
+    // SimCalorimetry/HcalSimAlgos/src/HcalTDC.cc  (nibs = 50...)
+    // packedTDC = TDC_RisingEdge + (tdcBins*2) * TDC_FallingEdge;
+    unsigned rising  =  tdc%100; 
+    unsigned falling =  tdc/100;
+
+    // only set the first time, avoiding "special" codes 
+    if(risingTime < -998. && rising != 62 && rising != 63) {
+      risingTime = rising*25./nbins + (i-signalBX)*25.;
     }
-    if(fallingTime < -998. && falling != 64 && falling != 65) {
-      fallingTime = falling*25./64. + (i-signalBX)*25.;
+    if(fallingTime < -998. && falling != 62 && falling != 63) {
+      fallingTime = falling*25./nbins + (i-signalBX)*25.;
     }    
+
+    /*
+    std::cout << " digi.tdc[" << i << "] = " << tdc 
+	      << "  rising = " << rising << "  falling = " << falling
+	      << "   Rt = " << risingTime
+	      << "   Ft = " << fallingTime
+	      << std::endl;
+    */
   }
   recHit = HBHERecHit(recHit.id(), recHit.energy(), risingTime, fallingTime);
 }
