72088c74562d12a30c6d3f05405ebe592ec33f19
AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch>
2013-05-28 01:52:17 +0000

    New track selection GBRForest in GTs

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -1,17 +1,17 @@
 autoCond = { 
     # GlobalTag for MC production with perfectly aligned and calibrated detector
-    'mc'                :   'PRE_MC62_V6::All',
+    'mc'                :   'PRE_MC62_V7::All',
     # GlobalTag for MC production with realistic alignment and calibrations
-    'startup'           :   'PRE_ST62_V6::All',
+    'startup'           :   'PRE_ST62_V7::All',
     # GlobalTag for MC production of Heavy Ions events with realistic alignment and calibrations
-    'starthi'           :   'PRE_SH62_V11::All',
+    'starthi'           :   'PRE_SH62_V13::All',
     # GlobalTag for MC production of p-Pb events with realistic alignment and calibrations
-    'startpa'           :   'PRE_SH62_V12::All',
+    'startpa'           :   'PRE_SH62_V14::All',
     # GlobalTag for data reprocessing: this should always be the GR_R tag
-    'com10'             :   'PRE_62_V6::All',
+    'com10'             :   'PRE_62_V7::All',
     # GlobalTag for running HLT on recent data: this should be the GR_P (prompt reco) global tag until a compatible GR_H tag is available, 
     # then it should point to the GR_H tag and override the connection string and pfnPrefix for use offline
-    'hltonline'         :   'PRE_P62_V6::All',
+    'hltonline'         :   'PRE_P62_V7::All',
     # GlobalTag for POSTLS1 upgrade studies:
     'upgradePLS1'       :   'PRE_PO62_V6::All',
     'upgrade2017'       :   'DES17_61_V5::All',
