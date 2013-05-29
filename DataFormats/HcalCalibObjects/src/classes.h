dcc8e746a569aca069c9378fc00a00fc6c44bffb
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2013-05-29 06:21:31 +0000

    adding HE darkening

--- DataFormats/HcalCalibObjects/src/classes.h
+++ DataFormats/HcalCalibObjects/src/classes.h
@@ -1,9 +1,11 @@
 #include "DataFormats/HcalCalibObjects/interface/HOCalibVariables.h"
+#include "DataFormats/HcalCalibObjects/interface/HEDarkening.h"
 // #include "DataFormats/HOCalibHit/interface/HOCalibVariableCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   struct dictionary {
+   HEDarkening                                  hed;       
    HOCalibVariables                             rv1;
    std::vector<HOCalibVariables>                v1;
    edm::Wrapper<std::vector<HOCalibVariables> > wc1;
