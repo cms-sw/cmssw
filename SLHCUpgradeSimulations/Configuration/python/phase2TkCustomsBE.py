d95d0b62088b384362eccd79d77a82b43cd16176
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2013-05-30 15:20:04 +0000

    Still working on it

--- SLHCUpgradeSimulations/Configuration/python/phase2TkCustomsBE.py
+++ SLHCUpgradeSimulations/Configuration/python/phase2TkCustomsBE.py
@@ -101,6 +101,22 @@ def customise_Reco(process,pileup):
     process.convClusters.oldClusterRemovalInfo=cms.InputTag("mixedTripletStepClusters")
     process.convClusters.trajectories=cms.InputTag("mixedTripletStepTracks")
     process.convClusters.overrideTrkQuals= cms.InputTag("mixedTripletStep")
+    process.pixellayertriplets.layerList = cms.vstring( 'BPix1+BPix2+BPix3',
+                                                        'BPix2+BPix3+BPix4',
+                                                        'BPix1+BPix3+BPix4',
+                                                        'BPix1+BPix2+BPix4',
+                                                        'BPix2+BPix3+FPix1_pos',
+                                                        'BPix2+BPix3+FPix1_neg',
+                                                        'BPix1+BPix2+FPix1_pos',
+                                                        'BPix1+BPix2+FPix1_neg',
+                                                        'BPix2+FPix1_pos+FPix2_pos',
+                                                        'BPix2+FPix1_neg+FPix2_neg',
+                                                        'BPix1+FPix1_pos+FPix2_pos',
+                                                        'BPix1+FPix1_neg+FPix2_neg',
+                                                        'FPix1_pos+FPix2_pos+FPix3_pos',
+                                                        'FPix1_neg+FPix2_neg+FPix3_neg' )
+    process.MeasurementTracker.UsePixelROCQualityDB = cms.bool(False)
+    process.MeasurementTracker.UsePixelModuleQualityDB = cms.bool(False)
     process.mixedTripletStepSeedLayersA.layerList = cms.vstring('BPix1+BPix2+BPix3', 
         'BPix1+BPix2+FPix1_pos', 
         'BPix1+BPix2+FPix1_neg', 
