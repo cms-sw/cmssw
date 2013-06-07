b9ed49bd01ab9d3ea2a66ee4582995edf3d2ce21
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2013-06-07 11:00:50 +0000

    moving HFRecalibration from CalibCalorimetry/HcalPlugins

--- DataFormats/HcalCalibObjects/src/classes.h
+++ DataFormats/HcalCalibObjects/src/classes.h
@@ -1,11 +1,13 @@
 #include "DataFormats/HcalCalibObjects/interface/HOCalibVariables.h"
 #include "DataFormats/HcalCalibObjects/interface/HEDarkening.h"
+#include "DataFormats/HcalCalibObjects/interface/HFRecalibration.h"
 // #include "DataFormats/HOCalibHit/interface/HOCalibVariableCollection.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   struct dictionary {
    HEDarkening                                  hed;       
+   HFRecalibration                              hfr;       
    HOCalibVariables                             rv1;
    std::vector<HOCalibVariables>                v1;
    edm::Wrapper<std::vector<HOCalibVariables> > wc1;
