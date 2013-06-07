f8b2f5f8d791872059ace26a230764b8046b5962
Nicholas Wardle <sha1-066e6c0b134809cea872f24c1feeee084b77b70c@cern.ch>
2013-06-07 13:16:49 +0000

    added -o (name) and -O (options) to options

--- HiggsAnalysis/CombinedLimit/test/makeAsymptoticGrid.py
+++ HiggsAnalysis/CombinedLimit/test/makeAsymptoticGrid.py
@@ -9,11 +9,14 @@ parser.add_option("-m","--mass",dest="mass",default=120.,type='float')
 parser.add_option("-n","--npoints",dest="npoints",default=20,type='int')
 parser.add_option("-r","--range",dest="range",nargs=2,type='float',default=(0.,2.))
 parser.add_option("","--nCPU",dest="nCPU",type='int',default=1)
+parser.add_option("-o","",dest="outputname",default="")
+parser.add_option("-O","",dest="options",default="")
 
 (options,args)=parser.parse_args()
 
 ws=options.workspace
 mass=options.mass
+outputname=options.outputname
 print options.range
 step=(options.range[1]-options.range[0])/options.npoints
 points=numpy.arange(options.range[0],options.range[1]+step,step)
@@ -21,7 +24,7 @@ points=numpy.arange(options.range[0],options.range[1]+step,step)
 workingDir=os.getcwd()
 wslocation=workingDir+"/"+ws
 
-jobname =  "limitgrid_%.1f"%(mass)
+jobname =  "limitgrid_%.1f%s"%(mass,outputname)
 f = open("%s.sh"%(jobname),"w")
 
 # Create job script
@@ -39,12 +42,12 @@ f.write("cp -p %s . \n" % (wslocation) )
 # Loop over number of points, and add wait after npoints/nCPU jobs
 ext = " & " if options.nCPU>1 else " " 
 for i,po in enumerate(points):
-  f.write("combine -M Asymptotic %s -m %.1f --singlePoint %f -n %.1f %s \n" % (ws,mass,po,po,ext) )
+  f.write("combine -M Asymptotic %s -m %.1f --singlePoint %f -n %.1f %s %s \n" % (ws,mass,po,po,ext,options.options) )
   if (i>0 and i%options.nCPU==0): f.write("wait\n")
 print i
 if options.nCPU>i: f.write("wait\n")
-f.write("hadd -f grid_%.1f.root higgsCombine* \n"%(mass))
-f.write("cp -f grid_%.1f.root %s \n"%(mass,workingDir))
+f.write("hadd -f grid_%.1f%s.root higgsCombine* \n"%(mass,outputname))
+f.write("cp -f grid_%.1f%s.root %s \n"%(mass,outputname,workingDir))
 f.write("echo 'DONE' \n")
  
 # make it executable
