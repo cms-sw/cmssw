1408db8c16331d9ae9da633317727302271f9a54
Andrius Juodagalvis <sha1-abaf34c54281481efaf19310050c074dc84ff65e@cern.ch>
2013-06-08 10:33:32 +0000

    clean-up of old EWK & QCD modules

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from DQM.Physics.bphysicsOniaDQM_cfi import *
 from DQM.Physics.ewkMuDQM_cfi import *
 from DQM.Physics.ewkElecDQM_cfi import *
-from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
-from DQM.Physics.qcdPhotonsDQM_cfi import *
+#from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
+#from DQM.Physics.qcdPhotonsDQM_cfi import *
 from DQM.Physics.topDiLeptonOfflineDQM_cfi import *
 from DQM.Physics.topSingleLeptonDQM_cfi import *
 from DQM.Physics.topSingleLeptonDQM_PU_cfi import *
 from DQM.Physics.singleTopDQM_cfi import *
 from DQM.Physics.ExoticaDQM_cfi import *
-from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
+#from DQM.Physics.ewkMuLumiMonitorDQM_cfi import * # duplicate!
 from DQM.Physics.susyDQM_cfi import *
 from DQM.Physics.HiggsDQM_cfi import *
 from JetMETCorrections.Configuration.JetCorrectionProducersAllAlgos_cff import *
@@ -25,8 +25,8 @@ from JetMETCorrections.Configuration.JetCorrectionProducersAllAlgos_cff import *
 dqmPhysics = cms.Sequence( bphysicsOniaDQM 
                            *ewkMuDQM
                            *ewkElecDQM
-                           *ewkMuLumiMonitorDQM
-                           *qcdPhotonsDQM
+#                           *ewkMuLumiMonitorDQM
+#                           *qcdPhotonsDQM
 			   *topSingleMuonMediumDQM
                            *topSingleElectronMediumDQM	
                            *DiMuonDQM
@@ -45,6 +45,6 @@ dqmPhysics = cms.Sequence( bphysicsOniaDQM
 bphysicsOniaDQMHI = bphysicsOniaDQM.clone(vertex=cms.InputTag("hiSelectedVertex"))
 dqmPhysicsHI = cms.Sequence(bphysicsOniaDQMHI)
 
-from DQM.Physics.qcdPhotonsCosmicDQM_cff import *
+#from DQM.Physics.qcdPhotonsCosmicDQM_cff import *
 dqmPhysicsCosmics = cms.Sequence(dqmPhysics)
-dqmPhysicsCosmics.replace(qcdPhotonsDQM, qcdPhotonsCosmicDQM)
+#dqmPhysicsCosmics.replace(qcdPhotonsDQM, qcdPhotonsCosmicDQM)
