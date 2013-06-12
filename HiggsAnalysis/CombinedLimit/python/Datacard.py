23808feebe0fe7f4b1d9985be685a8cc31d2421e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2013-06-12 23:29:30 +0000

    added two more functinos to obtain histogram names for shapes and uncertainties. Addec option to resolve {CHANNEL} and {PROCESS} as given in the shapes line or not

--- HiggsAnalysis/CombinedLimit/python/Datacard.py
+++ HiggsAnalysis/CombinedLimit/python/Datacard.py
@@ -92,24 +92,44 @@ class Datacard():
         """
         return self.barcode(bin, proc, 0)
 
-    def path_to_shape(self, bin, proc) :
+    def path_to_shape(self, bin, proc, resolve=True) :
         """
         Return the path to the histogram within the root input file for shape analyses for a given bin and process. If
-        there is no such path an empty string will be returned. The path will be returned as obtained from the datacard
-        with the exception that the keywords $CHANNEL correp. to bin and $PROCESS corresp. to proc are replaced by their
-        corresponding real values. Other potential keywords remain unchanged.
+        there is no such path an empty string will be returned. The path will be returned as obtained from the datacard.
+        If the option resolve is True the keywords $CHANNEL correp. to bin and $PROCESS corresp. to proc are replaced
+        by their corresponding real values. Other potential keywords remain unchanged.
         """
-        return self.barcode(bin, proc, 1).replace('$CHANNEL',bin).replace('$PROCESS',proc)
+        return self.barcode(bin, proc, 1).replace('$CHANNEL',bin).replace('$PROCESS',proc) if resolve else self.barcode(bin, proc, 1)
 
-    def path_to_uncert(self, bin, proc) :
+    def shape(self, bin, proc, resolve) :
+        """
+        This is a safe way to determine the histogram name of the shape template for a given bin and proc.
+        """
+        path =  self.path_to_shape(bin, proc, resolve)
+        if '/' in path :
+            return path[path.rfind('/')+1:]
+        else :
+            return path
+        
+    def path_to_uncert(self, bin, proc, resolve=True) :
         """
         Return the path to the uncertainty histograms within the root input file for shape analyses for a given bin and
         process. If there is no such path an empty string will be returned. The path will be returned as obtained from
-        the datacard with the exception that the keywords $CHANNEL correp. to bin and $PROCESS corresp. to proc are
+        the datacard. If option resolve is True the keywords $CHANNEL correp. to bin and $PROCESS corresp. to proc are
         replaced by their corresponding real values. Other potential keywords remain unchanged.
         """
-        return self.barcode(bin, proc, 2).replace('$CHANNEL',bin).replace('$PROCESS',proc)
+        return self.barcode(bin, proc, 2).replace('$CHANNEL',bin).replace('$PROCESS',proc) if resolve else self.barcode(bin, proc, 2)
 
+    def uncert(self, bin, proc, resolve) :
+        """
+        This is a safe way to determine the histogram name of the uncert template for a given bin and proc.
+        """
+        path =  self.path_to_uncert(bin, proc, contact)
+        if '/' in path :
+            return path[path.rfind('/')+1:]
+        else :
+            return path
+        
     def obs(self, bin) :
         """
         Return the number of observed events for a given bin.
