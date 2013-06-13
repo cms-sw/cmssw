8f0b760f0c9333a25e83a0b5f735df8957840db9
Vadim Khotilovich <khotilovich@gmail.com>
2013-06-13 16:08:12 -0500

    first compiling version of FastGE21CSCProducer

---
+++ SimMuL1/python/FastGE21CSCProducer_cfi.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
+
+SimTrackMatching.usedChamberTypesCSC = cms.untracked.vint32( 5, )
+SimTrackMatching.minBXCSCComp = 0
+SimTrackMatching.maxBXCSCComp = 16
+SimTrackMatching.minBXCSCWire = 0
+SimTrackMatching.maxBXCSCWire = 16
+SimTrackMatching.minBXCLCT = 0
+SimTrackMatching.maxBXCLCT = 16
+SimTrackMatching.minBXALCT = 0
+SimTrackMatching.maxBXALCT = 16
+SimTrackMatching.minBXLCT = 0
+SimTrackMatching.maxBXLCT = 16
+
+
+FastGE21CSCProducer = cms.EDAnalyzer("FastGE21CSCProducer",
+    verbose = cms.untracked.int32(0),
+    simInputLabel = cms.untracked.string("g4SimHits"),
+    lctInput = cms.untracked.InputTag("simCscTriggerPrimitiveDigis", "MPCSORTED"),
+    productInstanceName = cms.untracked.string("FastGE21"),
+    minPt = cms.untracked.double(4.5),
+    zOddGE21 = cms.untracked.double(780.),
+    zEvenGE21 = cms.untracked.double(775.),
+    createNtuple = cms.untracked.bool(False),
+    simTrackMatching = SimTrackMatching
+)
