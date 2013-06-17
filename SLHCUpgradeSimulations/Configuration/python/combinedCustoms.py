002d2daa9f440793599aca25144ff7a2ffb7cf1d
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2013-06-17 22:40:50 +0000

    adding BarrelEndCap5D

--- SLHCUpgradeSimulations/Configuration/python/combinedCustoms.py
+++ SLHCUpgradeSimulations/Configuration/python/combinedCustoms.py
@@ -1,9 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1
 from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE import customise as customiseBE
+from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE5D import customise as customiseBE5D
 from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import customise as customiseLB6PS
 from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_4LPS_2L2S import customise as customiseLB4LPS_2L2S
 from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE import l1EventContent as customise_ev_BE
+from SLHCUpgradeSimulations.Configuration.phase2TkCustomsBE5D import l1EventContent as customise_ev_BE5D
 from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_6PS import l1EventContent as customise_ev_LB6PS
 from SLHCUpgradeSimulations.Configuration.phase2TkCustoms_LB_4LPS_2L2S import l1EventContent as customise_ev_LB4LPS_2L2S
 from SLHCUpgradeSimulations.Configuration.customise_mixing import customise_NoCrossing
@@ -11,6 +13,13 @@ from SLHCUpgradeSimulations.Configuration.phase1TkCustoms import customise as cu
 from SLHCUpgradeSimulations.Configuration.HCalCustoms import customise_HcalPhase1, customise_HcalPhase0
 
 import SLHCUpgradeSimulations.Configuration.aging as aging
+
+def cust_phase2_BE5D(process):
+    process=customisePostLS1(process)
+    process=customise_HcalPhase1(process)
+    process=customiseBE5D(process)
+    process=customise_ev_BE5D(process)
+    return process
 
 def cust_phase2_BE(process):
     process=customisePostLS1(process)
