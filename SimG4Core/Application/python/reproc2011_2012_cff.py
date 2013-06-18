ea962fe30dda7d38172014d4be1020c5f69e3356
Vladimir Ivantchenko <sha1-05628bbcf33eb7e0ec319f684b9c387a833d2578@cern.ch>
2013-06-18 17:19:08 +0000

    minor cleanup

--- SimG4Core/Application/python/reproc2011_2012_cff.py
+++ SimG4Core/Application/python/reproc2011_2012_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-def customiseGeant4(process):
+def customiseG4(process):
 
     process.g4SimHits.Physics.type = cms.string('SimG4Core/Physics/QGSP_FTFP_BERT_EML_New')
 
