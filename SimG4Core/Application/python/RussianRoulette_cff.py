07c78a09b53bdea1d057bc812846afa89bb9d514
Vladimir Ivantchenko <sha1-05628bbcf33eb7e0ec319f684b9c387a833d2578@cern.ch>
2013-06-20 11:29:27 +0000

    restore the head

--- SimG4Core/Application/python/RussianRoulette_cff.py
+++ SimG4Core/Application/python/RussianRoulette_cff.py
@@ -3,11 +3,11 @@ import FWCore.ParameterSet.Config as cms
 def customise(process):
 
     # advanced Geant4 physics
-    process.g4SimHits.Physics.type = cms.string('SimG4Core/Physics/QGSP_FTFP_BERT_EML_New')
+    #process.g4SimHits.Physics.type = cms.string('SimG4Core/Physics/QGSP_FTFP_BERT_EML_New')
 
     # extended geometric acceptance (full CASTOR acceptance)
-    process.g4SimHits.Generator.MinEtaCut = cms.double(-6.7)
-    process.g4SimHits.Generator.MaxEtaCut = cms.double(6.7)
+    #process.g4SimHits.Generator.MinEtaCut = cms.double(-6.7)
+    #process.g4SimHits.Generator.MaxEtaCut = cms.double(6.7)
 
     # Russian roulette parameters
     process.g4SimHits.Physics.RusRoGammaEnergyLimit = cms.double(5.)
