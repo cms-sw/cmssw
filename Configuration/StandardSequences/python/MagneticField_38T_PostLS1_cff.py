af11eb66373bfe8235f929f56ef41d6117e4945a
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2013-06-22 03:37:24 +0000

    postls1 mag field

---
+++ Configuration/StandardSequences/python/MagneticField_38T_PostLS1_cff.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+# This cfi contains everything needed to use the VolumeBased magnetic
+# field engine.
+#Default is version 85l
+from MagneticField.Engine.volumeBasedMagneticField_120812_largeYE4_cfi import *
+
+
