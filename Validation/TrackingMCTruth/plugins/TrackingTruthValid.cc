052876bdde7ec2cab3a8e1c69b784cddf67fbbf9
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2013-06-24 14:43:31 +0000

    Merge changes for the new TrackingParticle, see branch NewTPfor620 for details

--- Validation/TrackingMCTruth/plugins/TrackingTruthValid.cc
+++ Validation/TrackingMCTruth/plugins/TrackingTruthValid.cc
@@ -12,7 +12,7 @@
 
 #include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
 #include "SimDataFormats/CrossingFrame/interface/MixCollection.h"
-#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
 
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
@@ -126,11 +126,17 @@ void TrackingTruthValid::analyze(const edm::Event& event, const edm::EventSetup&
     meTPEta->Fill(t->momentum().eta());
 
     meTPPhi->Fill(t->momentum().Phi());
+#warning "This file has been modified just to get it to compile without any regard as to whether it still functions as intended"
+#ifdef REMOVED_JUST_TO_GET_IT_TO_COMPILE__THIS_CODE_NEEDS_TO_BE_CHECKED
     std::vector<PSimHit> trackerPSimHit( t->trackPSimHit(DetId::Tracker) );
-    meTPAllHits->Fill(trackerPSimHit.size());
+#endif
+    meTPAllHits->Fill(t->numberOfTrackerHits());
     //get the process of the first hit
+#warning "This file has been modified just to get it to compile without any regard as to whether it still functions as intended"
+#ifdef REMOVED_JUST_TO_GET_IT_TO_COMPILE__THIS_CODE_NEEDS_TO_BE_CHECKED
     if(trackerPSimHit.size() !=0) meTPProc->Fill( trackerPSimHit.front().processType());
-    meTPMatchedHits->Fill(t->matchedHit());
+#endif
+    meTPMatchedHits->Fill(t->numberOfTrackerLayers());
     meTPVtxX->Fill(t->vx());
     meTPVtxY->Fill(t->vy());
     meTPVtxZ->Fill(t->vz());
