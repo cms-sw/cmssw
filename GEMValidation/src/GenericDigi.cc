4b40b402e4131ae397d5c12480dd2d35fadaca1f
Vadim Khotilovich <khotilovich@gmail.com>
2013-06-26 15:07:49 -0500

    added GEM-LCT dphi field to GenericDigi

--- GEMValidation/src/GenericDigi.cc
+++ GEMValidation/src/GenericDigi.cc
@@ -23,10 +23,11 @@ std::ostream & operator<<(std::ostream & o, const matching::Digi& d)
   int q = digi_quality(d);
   int p = digi_pattern(d);
   int wg = digi_wg(d);
+  float dphi = digi_wg(d);
 
   if (t == CSC_CLCT) o<<id<<" t: CLCT s "<<ch<<" bx "<<bx<<" q "<<q<<" p "<<p;
   else if (t == CSC_ALCT) o<<id<<" t: ALCT wg "<<ch<<" bx "<<bx<<" q "<<q;
-  else if (t == CSC_LCT) o<<id<<" t: LCT s "<<ch<<" wg "<<wg<<" bx "<<bx<<" q "<<q<<" p "<<p;
+  else if (t == CSC_LCT) o<<id<<" t: LCT s "<<ch<<" wg "<<wg<<" bx "<<bx<<" q "<<q<<" p "<<p<<" dphi "<<dphi;
   else o<<id<<" t: "<<t<<" ch "<<ch<<" bx "<<bx;
 
   return o;
--- GEMValidation/src/GenericDigi.h
+++ GEMValidation/src/GenericDigi.h
@@ -27,7 +27,7 @@ namespace matching {
 typedef enum {INVALID=0, GEM_STRIP, GEM_PAD, GEM_COPAD, CSC_STRIP, CSC_WIRE, CSC_CLCT, CSC_ALCT, CSC_LCT} DigiType;
 
 // digi info keeper: <detid, channel, bx, type, quality, bend, WireGroup>
-typedef std::tuple<unsigned int, int, int, DigiType, int, int, int> Digi;
+typedef std::tuple<unsigned int, int, int, DigiType, int, int, int, float> Digi;
 
 // digi collection
 typedef std::vector<Digi> DigiContainer;
@@ -38,6 +38,7 @@ inline Digi make_digi(unsigned int id, int ch, int bx, DigiType t) { return std:
 inline Digi make_digi(unsigned int id, int ch, int bx, DigiType t, int q) { return std::make_tuple(id, ch, bx, t, q, 0, 0); }
 inline Digi make_digi(unsigned int id, int ch, int bx, DigiType t, int q, int pat) { return std::make_tuple(id, ch, bx, t, q, pat, 0); }
 inline Digi make_digi(unsigned int id, int ch, int bx, DigiType t, int q, int pat, int wg) { return std::make_tuple(id, ch, bx, t, q, pat, wg); }
+inline Digi make_digi(unsigned int id, int ch, int bx, DigiType t, int q, int pat, int wg, float dphi) { return std::make_tuple(id, ch, bx, t, q, pat, wg, dphi); }
 
 // digi accessors
 inline bool is_valid(const Digi& d) {return std::get<0>(d) != INVALID; }
@@ -49,9 +50,15 @@ inline DigiType digi_type(const Digi& d) { return std::get<3>(d); }
 inline int digi_quality(const Digi& d) { return std::get<4>(d); }
 inline int digi_pattern(const Digi& d) { return std::get<5>(d); }
 
-// can access and also modify the WG value with this one
+// can access and also modify the WG value by reference with this one
 int& digi_wg(Digi& d);
+// only read for const digi
 int digi_wg(const Digi& d);
+
+// can access and also modify the dphi value by reference with this one
+int& digi_dphi(Digi& d) { return std::get<7>(d); }
+// only read for const digi
+int digi_dphi(const Digi& d) { return std::get<7>(d); }
 
 }
 
