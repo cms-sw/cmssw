7cfa1cca09172bbe7eb136e1fe4acbe871991343
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2013-06-26 11:40:55 +0000

    a simpler dumper

---
+++ RecoLocalCalo/EcalRecProducers/test/stubs/URHDump.cc
@@ -0,0 +1,51 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+
+
+
+class URHDump : public edm::EDAnalyzer {
+   public:
+  explicit URHDump(const edm::ParameterSet&);
+
+private:
+  virtual void beginJob(){}
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob(){}
+
+private:
+
+  edm::InputTag EBURecHitCollection_;
+  edm::InputTag EEURecHitCollection_;
+  edm::Handle<EcalUncalibratedRecHitCollection> EBURecHits_;
+  edm::Handle<EcalUncalibratedRecHitCollection> EEURecHits_;
+
+};
+
+
+URHDump::URHDump(const edm::ParameterSet&iConfig) {
+  EBURecHitCollection_ = iConfig.getParameter<edm::InputTag>("EBURecHitCollection");
+  EEURecHitCollection_ = iConfig.getParameter<edm::InputTag>("EEURecHitCollection");
+}
+
+void URHDump::analyze(const edm::Event& ev, const edm::EventSetup&){
+
+  ev.getByLabel(EBURecHitCollection_, EBURecHits_);
+  ev.getByLabel(EEURecHitCollection_, EEURecHits_);
+
+  for (auto const & h : (*EBURecHits_))
+    std::cout << h.id() << " "
+	      << std::endl;
+
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(URHDump);
