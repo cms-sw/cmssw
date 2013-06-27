80e282ed7388f67327f3b110ccb82b50265b56aa
Chris Jones <chrisdjones15@gmail.com>
2013-06-27 15:45:23 -0500

    New id classes needed for the threaded framework

---
+++ FWCore/Utilities/interface/RunIndex.h
@@ -0,0 +1,57 @@
+#ifndef FWCore_Utilities_RunIndex_h
+#define FWCore_Utilities_RunIndex_h
+// -*- C++ -*-
+//
+// Package:     FWCore/Utilities
+// Class  :     RunIndex
+// 
+/**\class RunIndex RunIndex.h "FWCore/Utilities/interface/RunIndex.h"
+
+ Description: Identifies a 'slot' being used to hold an active Run
+
+ Usage:
+    Various APIs used this to access per Run information.
+ It is important to realize that the same RunIndex may be used to refer
+ to different Runs over the lifetime of a job. An RunIndex will only get
+ a new Run after the previous Run using that index has finished being used.
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Fri, 26 Apr 2013 19:38:56 GMT
+// $Id$
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+namespace edm {
+  class RunIndex
+  {
+    
+  public:
+    ~RunIndex() = default;
+    
+    // ---------- const member functions ---------------------
+    bool operator==(const RunIndex& iIndex) const {
+      return value() == iIndex.value();
+    }
+    unsigned int value() const { return value_;}
+    
+    
+  private:
+    explicit RunIndex(unsigned int iIndex) : value_(iIndex) {}
+
+    RunIndex() = delete;
+    RunIndex(const RunIndex&) = delete; // stop default
+    const RunIndex& operator=(const RunIndex&) = delete; // stop default
+    
+    // ---------- member data --------------------------------
+    
+  };
+}
+
+
+#endif
---
+++ FWCore/Utilities/interface/StreamID.h
@@ -0,0 +1,53 @@
+#ifndef FWCore_Utilities_StreamID_h
+#define FWCore_Utilities_StreamID_h
+// -*- C++ -*-
+//
+// Package:     FWCore/Utilities
+// Class  :     StreamID
+// 
+/**\class StreamID StreamID.h "FWCore/Utilities/interface/StreamID.h"
+
+ Description: Identifies an edm stream
+
+ Usage:
+    Various APIs use this type to allow access to per stream information.
+
+*/
+//
+// Original Author:  Chris Jones
+//         Created:  Fri, 26 Apr 2013 19:37:37 GMT
+// $Id$
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+namespace edm {
+  class StreamID
+  {
+    
+  public:
+    ~StreamID() = default;
+    
+    bool operator==(const StreamID& iID) const {
+      return iID.value_ == value_;
+    }
+
+    unsigned int value() const { return value_; }
+    
+  private:
+    explicit StreamID(unsigned int iValue) : value_(iValue) {}
+    
+    StreamID() = delete;
+    StreamID(const StreamID&) = delete; // stop default
+    const StreamID& operator=(const StreamID&) - delete; // stop default
+    
+    // ---------- member data --------------------------------
+    unsigned int value_;
+  };
+}
+
+
+#endif
