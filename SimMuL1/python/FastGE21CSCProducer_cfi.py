ab0265b71a34cbccedcc259fdd76aea0e6b6139a
Vadim Khotilovich <khotilovich@gmail.com>
2013-07-02 09:51:03 -0500

    safer python include handling plus more...
    use the 6cm larger then ME1 lever arm distance
    use 2x deltaPhi GE21 resolution for smearing
    fix a typo in parameter

--- SimMuL1/python/FastGE21CSCProducer_cfi.py
+++ SimMuL1/python/FastGE21CSCProducer_cfi.py
@@ -2,21 +2,23 @@ import FWCore.ParameterSet.Config as cms
 
 from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
 
-SimTrackMatching.useCSCChamberTypes = cms.untracked.vint32( 2, 5 )
-SimTrackMatching.minBXCSCComp = 0
-SimTrackMatching.maxBXCSCComp = 16
-SimTrackMatching.minBXCSCWire = 0
-SimTrackMatching.maxBXCSCWire = 16
-SimTrackMatching.minBXCLCT = 0
-SimTrackMatching.maxBXCLCT = 16
-SimTrackMatching.minBXALCT = 0
-SimTrackMatching.maxBXALCT = 16
-SimTrackMatching.minBXLCT = 0
-SimTrackMatching.maxBXLCT = 16
+stm = SimTrackMatching.clone()
+
+stm.useCSCChamberTypes = cms.untracked.vint32( 2, 5 )
+stm.minBXCSCComp = 0
+stm.maxBXCSCComp = 16
+stm.minBXCSCWire = 0
+stm.maxBXCSCWire = 16
+stm.minBXCLCT = 0
+stm.maxBXCLCT = 16
+stm.minBXALCT = 0
+stm.maxBXALCT = 16
+stm.minBXLCT = 0
+stm.maxBXLCT = 16
 # turn off all matchers except SimHitMatcher
-SimTrackMatching.gemDigiInput = cms.untracked.InputTag("")
-SimTrackMatching.cscWireDigiInput = cms.untracked.InputTag("")
-SimTrackMatching.cscLCTInput = cms.untracked.InputTag("")
+stm.gemDigiInput = cms.untracked.InputTag("")
+stm.cscWireDigiInput = cms.untracked.InputTag("")
+stm.cscLCTInput = cms.untracked.InputTag("")
  
 
 
@@ -24,17 +26,19 @@ FastGE21CSCProducer = cms.EDProducer("FastGE21CSCProducer",
     verbose = cms.untracked.int32(0),
     simInputLabel = cms.untracked.string("g4SimHits"),
     lctInput = cms.untracked.InputTag("simCscTriggerPrimitiveDigis", "MPCSORTED"),
-    productInstanceName = cms.untracked.string("FastGE21"),
+    productInstanceName = cms.untracked.string("FastGEM"),
     minPt = cms.untracked.double(4.5),
     minEta = cms.untracked.double(1.55),
     maxEta = cms.untracked.double(2.4),
-    usePropagatedDPhi_ = cms.untracked.bool(True),
+    usePropagatedDPhi = cms.untracked.bool(True),
     # index-to-chamber-type: 0:dummy, 1:ME1/a, 2:ME1/b, 3:ME1/2, 4:ME1/3, 5: ME2/1, ...
-    zOddGE21 = cms.vdouble(-1., -1., 569.7, -1., -1., 798.3, -1., -1., -1., -1., -1.),
-    zEvenGE21 = cms.vdouble(-1., -1., 567.6, -1., -1., 796.2, -1., -1., -1., -1., -1.),
+    #zOddGE21 = cms.vdouble(-1., -1., 569.7, -1., -1., 798.3, -1., -1., -1., -1., -1.), # comparable to ME11
+    #zEvenGE21 = cms.vdouble(-1., -1., 567.6, -1., -1., 796.2, -1., -1., -1., -1., -1.),# comparable to ME11
+    zOddGE21 = cms.vdouble(-1., -1., 569.7, -1., -1., 792.3, -1., -1., -1., -1., -1.), # + 6cm lever arm
+    zEvenGE21 = cms.vdouble(-1., -1., 567.6, -1., -1., 790.2, -1., -1., -1., -1., -1.),# + 6cm lever arm
     # half-strip phi-pitch flat smearing
     phiSmearCSC = cms.vdouble(-1., -1., 0.00148, -1., -1., 0.00233, -1., -1., -1., -1., -1.),
     # trigger pad phi-pitch flat smearing
-    phiSmearGEM = cms.vdouble(-1., -1., 0.00190, -1., -1., 0.00190, -1., -1., -1., -1., -1.),
-    simTrackMatching = SimTrackMatching
+    phiSmearGEM = cms.vdouble(-1., -1., 0.00190, -1., -1., 2*0.00190, -1., -1., -1., -1., -1.),
+    simTrackMatching = stm
 )
