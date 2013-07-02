ee8fc690ba0e7fee8bf6def06261e88133498f34
Vadim Khotilovich <khotilovich@gmail.com>
2013-07-02 09:57:50 -0500

    extra parameter for a list of stationsToUse in FastGE21CSCProducer
    make the handling of python includes safer

--- GEMValidation/python/GEMCSCAnalyzer_cfi.py
+++ GEMValidation/python/GEMCSCAnalyzer_cfi.py
@@ -2,11 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
 
+stm = SimTrackMatching.clone()
+
 GEMCSCAnalyzer = cms.EDAnalyzer("GEMCSCAnalyzer",
     verbose = cms.untracked.int32(0),
     simInputLabel = cms.untracked.string("g4SimHits"),
     minPt = cms.untracked.double(4.5),
     ntupleTrackChamberDelta = cms.untracked.bool(True),
     ntupleTrackEff = cms.untracked.bool(True),
-    simTrackMatching = SimTrackMatching
+    stationsToUse = cms.vint32(1,),
+    simTrackMatching = stm
 )
