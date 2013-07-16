86e3ed4db53ecbd5f162fddc8b955da842486f82
David Mendez <david.mendez@cern.ch>
2013-07-16 14:36:56 +0200

    Removed copyright declarations of DQMOffline and DQMServices.
    Signed-off-by: David Mendez <david.mendez@cern.ch>

--- DQMServices/XdaqCollector/interface/Exception.h
+++ DQMServices/XdaqCollector/interface/Exception.h
@@ -2,7 +2,6 @@
 
 /*************************************************************************
  * XDAQ Components for Distributed Data Acquisition                      *
- * Copyright (C) 2000-2004, CERN.			                 *
  * All rights reserved.                                                  *
  * Authors: J. Gutleber and L. Orsini					 *
  *                                                                       *
