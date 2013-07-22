c07aa6a60bc5556b540b8f4cb89445b1c67fbe26
ivanakurecic@gmail.com <ivanakurecic@gmail.com>
2013-07-22 15:32:32 +0200

    Added particle energy & parent type conditions.

--- TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
+++ TimeEventStudies/TimeAnalyzer/python/CfiFile_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 TimeAnalysis = cms.EDAnalyzer('TimeAnalyzer',
                               acceptedParticleTypes = cms.vint32( 11,-11 ),
-                              lowestenergy=cms.double( 10.0 ) 
+                              lowestenergy=cms.double( 10.0 ),
+			      acceptedParentTypes = cms.vint32(23), 
                               )
