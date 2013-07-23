afad524ab2a348f8fe4929de44f85bf6ff0f9dec
Sven Dildick <sven.dildick@cern.ch>
2013-07-23 11:36:27 +0200

    Adding library of samples for GEM-CSC trigger studies

---
+++ SimMuL1/python/GEMCSCTriggerSamplesLib.py
@@ -0,0 +1,123 @@
+## library of input files for the GEM-CSC trigger study
+
+files = {}
+
+files['_pat2'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/NuGun_PU100_L1_pat2/']
+files['_pat6'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/NuGun_PU100_L1_pat6/']
+files['_pat8'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/NuGun_PU100_L1_pat8/']
+
+files['_gem_pat2'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_pat2/']
+files['_gem_pat6'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_pat6/']
+files['_gem_pat8'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_pat8/']
+
+files['_def_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/khotilov/GEN-SIM-NeutrinoGun_v3/GEM_NeutrinoGun_110K_pu100_DIGI_L1/2d6d66b06450f6f4eca385bc78cbc39a/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/khotilov/GEN-SIM-NeutrinoGun_v3/GEM_NeutrinoGun_pu100_DIGI_L1/2d6d66b06450f6f4eca385bc78cbc39a/']
+
+files['_def_pat2_other'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/khotilov/MuomGUN_SIM_Pt2-50_100k/MuonGun_DIGI_L1_Pt2-50_100k/29891ddb18281fff4c42a6e5f5d4bc55/']
+
+files['_def_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_def_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_def_pat8/']
+
+files['_gem98_pt05_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt05_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt05_pat2_128K/']
+files['_gem98_pt06_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt06_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt06_pat2_128K/']
+files['_gem98_pt10_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt10_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt10_pat2_128K/']
+files['_gem98_pt15_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt15_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt15_pat2_128K/']
+files['_gem98_pt20_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt20_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt20_pat2_128K/']
+files['_gem98_pt30_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt30_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt30_pat2_128K/']
+files['_gem98_pt40_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt40_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt40_pat2_128K/']
+
+files['_gem95_pt05_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt05_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt05_pat2_128K/']
+files['_gem95_pt10_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt10_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt10_pat2_128K/']
+files['_gem95_pt15_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt15_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt15_pat2_128K/']
+files['_gem95_pt20_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt20_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt20_pat2_128K/']
+files['_gem95_pt30_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt30_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt30_pat2_128K/']
+files['_gem95_pt40_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt40_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt40_pat2_128K/']
+
+files['_gem98_pt05_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt05_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt05_pat8_128K/']
+files['_gem98_pt06_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt06_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt06_pat8_128K/']
+files['_gem98_pt10_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt10_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt10_pat8_128K/']
+files['_gem98_pt15_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt15_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt15_pat8_128K/']
+files['_gem98_pt20_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt20_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt20_pat8_128K/']
+files['_gem98_pt30_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt30_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt30_pat8_128K/']
+files['_gem98_pt40_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt40_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_NuGun_PU100_L1_pt40_pat8_128K/']
+
+files['_gem95_pt05_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt05_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt05_pat8_128K/']
+files['_gem95_pt10_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt10_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt10_pat8_128K/']
+files['_gem95_pt15_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt15_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt15_pat8_128K/']
+files['_gem95_pt20_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt20_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt20_pat8_128K/']
+files['_gem95_pt30_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt30_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt30_pat8_128K/']
+files['_gem95_pt40_pat8'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt40_pat8_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_NuGun_PU100_L1_pt40_pat8_128K/']
+
+files['_gem98_pt10_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_MuGun_PU0_L1_pt10_pat2/']
+files['_gem98_pt15_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_MuGun_PU0_L1_pt15_pat2/']
+files['_gem98_pt20_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_MuGun_PU0_L1_pt20_pat2/']
+files['_gem98_pt30_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_MuGun_PU0_L1_pt30_pat2/']
+files['_gem98_pt40_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM98_MuGun_PU0_L1_pt40_pat2/']
+
+files['_gem95_pt10_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_MuGun_PU0_L1_pt10_pat2/']
+files['_gem95_pt20_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_MuGun_PU0_L1_pt20_pat2/']
+files['_gem95_pt30_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_MuGun_PU0_L1_pt30_pat2/']
+files['_gem95_pt40_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM95_MuGun_PU0_L1_pt40_pat2/']
+
+files['_ntup_pt40_pat2'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/yasser1/yasser/muonGun_50k_pT40_lpcgem/MuomGunPt40L1CSC50k_digi/82325e40d6202e6fec2dd983c477f3ca/']
+
+files['_gem_dphi_pat2_PU0'] = ['/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_MuGun_PU0_L1_dphi_pat2/']
+files['_gem_dphi_pat2_PU100'] = [
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_dphi_pat2_110K/',
+    '/pnfs/cms/WAX/11/store/user/lpcgem/khotilov/GEM_NuGun_PU100_L1_dphi_pat2_128K/']
