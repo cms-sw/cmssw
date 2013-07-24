763e68491324e39427d3bbe32d0ec013d0f371d3
David <David.Lange@cern.ch>
2013-07-24 23:04:45 +0200

    remove init

--- SLHCUpgradeSimulations/Configuration/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/SLHCUpgradeSimulations/Configuration/',1)[0])+'/cfipython/slc5_amd64_gcc472/SLHCUpgradeSimulations/Configuration')
