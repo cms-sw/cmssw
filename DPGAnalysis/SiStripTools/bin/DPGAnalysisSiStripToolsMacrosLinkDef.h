6ab3638781660a9970aedb67b52a69abd47d58ad
Andrea <andrea.venturi@cern.ch>
2013-07-29 10:59:22 +0200

    Added new macros. <export> tag removed from bin BuildFile

--- DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
+++ DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
@@ -1,5 +1,8 @@
 #include "OccupancyPlotMacros.h"
+#include "MultiplicityPlotMacros.h"
 #ifdef __CINT__
 #pragma link off all functions;
 #pragma link C++ function PlotOccupancyMap;
+#pragma link C++ function AverageRunMultiplicity;
+#pragma link C++ function PlotPixelMultVtxPos;
 #endif
