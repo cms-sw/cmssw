9c51e6d1e3852b172d57143fd0bc6d59efa360a5
Giuseppe <giuseppe.cerati@cern.ch>
2013-07-29 12:28:43 +0200

    first commit

---
+++ SimGeneral/TrackingAnalysis/src/classes.h
@@ -0,0 +1,26 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
+#include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/TrackerRecHit2D/interface/OmniClusterRef.h"
+
+#include "DataFormats/Common/interface/AssociationMap.h"
+namespace {
+  struct dictionary {
+    edm::AssociationMap<edm::OneToMany<std::vector<SimTrack>, std::vector<OmniClusterRef>, unsigned int > > dummy01;
+    edm::Wrapper<edm::AssociationMap<edm::OneToMany<std::vector<SimTrack>, std::vector<OmniClusterRef>, unsigned int > > > dummy02;
+    edm::helpers::KeyVal<edm::RefProd<std::vector<SimTrack> >, edm::RefProd<std::vector<OmniClusterRef> > > dummy03;
+    edm::Wrapper<edm::helpers::KeyVal<edm::RefProd<std::vector<SimTrack> >, edm::RefProd<std::vector<OmniClusterRef> > > > dummy04;
+    std::vector<OmniClusterRef> dummy05;
+    edm::Wrapper<std::vector<OmniClusterRef> > dummy06;
+    std::pair<OmniClusterRef, TrackingParticleRef> dummy13;
+    edm::Wrapper<std::pair<OmniClusterRef, TrackingParticleRef> > dummy14;
+    std::vector<std::pair<OmniClusterRef, TrackingParticleRef> > dummy07;
+    edm::Wrapper<std::vector<std::pair<OmniClusterRef, TrackingParticleRef> > > dummy08;
+    std::map<TrackingParticleRef, std::vector<OmniClusterRef> > dummy09;
+    edm::Wrapper<std::map<TrackingParticleRef, std::vector<OmniClusterRef> > > dummy10;
+    std::map<OmniClusterRef, std::vector<TrackingParticleRef> > dummy11;
+    edm::Wrapper<std::map<OmniClusterRef, std::vector<TrackingParticleRef> > > dummy12;
+  };
+}
