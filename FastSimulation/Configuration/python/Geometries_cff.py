08deafe845d06c459cecd3a4eeadea4991f5e943
Ianna Osborne <ianna.osborne@cern.ch>
2013-08-05 10:42:54 +0200

    Make sure fast sim geometry configuration uses the same label as defined in standard sequences. Revert changes to standard workflow to allow current fast sim working as expected.

--- FastSimulation/Configuration/python/Geometries_cff.py
+++ FastSimulation/Configuration/python/Geometries_cff.py
@@ -6,20 +6,16 @@ import FWCore.ParameterSet.Config as cms
 
 # To use the "full" CMS geometry, comment the prevous line, and uncomment the following one:
 #####from Configuration.StandardSequences.Geometry_cff import *
-#from Configuration.StandardSequences.GeometryDB_cff import *
-from Configuration.Geometry.GeometryExtended2017Reco_cff import *
+from Configuration.StandardSequences.GeometryDB_cff import *
 
 
-#TrackerDigiGeometryESModule.applyAlignement = False
 # The tracker geometry left-over (for aligned/misaligned geometry)
 # The goemetry used for reconstruction must not be misaligned.
-trackerSLHCGeometry.applyAlignment = cms.bool(False)
+trackerGeometryDB.applyAlignment = False
 # Create a misaligned geometry for simulation
-#misalignedTrackerGeometry = Geometry.TrackerGeometryBuilder.trackerGeometry_cfi.trackerGeometry.clone()
-#Try to change the name of the file.
-misalignedTrackerGeometry = Geometry.TrackerGeometryBuilder.trackerSLHCGeometry_cfi.trackerSLHCGeometry.clone()
+misalignedTrackerGeometry = Geometry.TrackerGeometryBuilder.trackerGeometryDB_cfi.trackerGeometryDB.clone()
 # The misalignment is not applied by default
-misalignedTrackerGeometry.applyAlignment = cms.bool(False)
+misalignedTrackerGeometry.applyAlignment = False
 # Label of the produced TrackerGeometry:
 misalignedTrackerGeometry.appendToDataLabel = 'MisAligned'
 
@@ -27,7 +23,7 @@ misalignedTrackerGeometry.appendToDataLabel = 'MisAligned'
 # The geometry used for reconstruction must not be misaligned.
 DTGeometryESModule.applyAlignment = False
 # Create a misaligned geometry for simulation
-misalignedDTGeometry = Geometry.DTGeometryBuilder.dtGeometry_cfi.DTGeometryESModule.clone()
+misalignedDTGeometry = Geometry.DTGeometryBuilder.dtGeometryDB_cfi.DTGeometryESModule.clone()
 # The misalignment is not applied by default
 misalignedDTGeometry.applyAlignment = False
 # Label of the produced DTGeometry:
@@ -37,7 +33,7 @@ misalignedDTGeometry.appendToDataLabel = 'MisAligned'
 # The geometry used for reconstruction must not be misaligned.
 CSCGeometryESModule.applyAlignment = False
 # Create a misaligned geometry for simulation
-misalignedCSCGeometry = Geometry.CSCGeometryBuilder.cscGeometry_cfi.CSCGeometryESModule.clone()
+misalignedCSCGeometry = Geometry.CSCGeometryBuilder.cscGeometryDB_cfi.CSCGeometryESModule.clone()
 # The misalignment is not applied by default
 misalignedCSCGeometry.applyAlignment = False
 # Label of the produced CSCGeometry:
