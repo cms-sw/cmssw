47085622c614ebaeb2c1b488059f005fc9135877
Vladimir <Vladimir.Ivantchenko@cern.ch>
2013-08-05 20:02:59 +0200

    fixed compillation warnings

--- SimTracker/TrackHistory/src/TrackQuality.cc
+++ SimTracker/TrackHistory/src/TrackQuality.cc
@@ -68,7 +68,7 @@ struct MatchedHit
                simTrackId == other.simTrackId;
     }
 };
-
+  /*
 static bool operator < (const MatchedHit &hit, DetId detId)
 {
     return hit.detId < detId;
@@ -77,6 +77,7 @@ static bool operator < (DetId detId, const MatchedHit &hit)
 {
     return detId < hit.detId;
 }
+  */
 }
 
 typedef std::pair<TrackQuality::Layer::SubDet, short int> DetLayer;
