c65c92d5a9d2c3b62f5099a936b9354940019428
ivanakurecic@gmail.com <ivanakurecic@gmail.com>
2013-08-05 17:06:35 +0200

    Correct relr processing.

--- SimulationTOF/SimulationSimple/programs/TOFftheta.cc
+++ SimulationTOF/SimulationSimple/programs/TOFftheta.cc
@@ -5,6 +5,7 @@
 #include "TMath.h"
 #include "TCanvas.h"
 #include "TLegend.h"
+#include "TH1F.h"
 
 #define B 3.8
 #define BarrelR 1.3
@@ -15,44 +16,49 @@ using namespace TMath;
 
 Double_t delta_t( Double_t *eta, Double_t *par )
 {
-    Double_t m=par[2]/Sqrt(1-Power(par[0],2));
-    Double_t Tfinal=ACos(1-0.5*Power(BarrelR*par[3]*B*(1+exp(-2*(*eta)))/(m*(3.0E8)*par[0]*2*exp(-(*eta))),2));
-    Double_t neutral_t=BarrelR*(1+exp(-2*(*eta)))/(2*exp(-(*eta))*(3.0E8)*par[0]);
-    Double_t neutral_t_e=(Zhalflength-par[1])*(1+exp(-2*(*eta)))/(par[0]*(3.0E8)*(1-exp(-2*(*eta))));
-    Double_t value=Tfinal*m/(par[3]*B)-neutral_t;
-    Double_t Tz1=(Zhalflength-par[1])*par[3]*B*(1+exp(-2*(*eta)))/(m*(3.0E8)*par[0]*(1-exp(-2*(*eta))));
+    Double_t value=42.0;Double_t tmomentum=par[0];Double_t zcoord=par[1];Double_t natcharge=par[2];Double_t invmass=par[3];Double_t theta=2*ATan(Exp(-(*eta)));
+    Double_t relr=tmomentum*(1.0E9)/(C()*natcharge*B);
+    Double_t Tfinal=ACos(1-0.5*Power(BarrelR/relr,2));
+    Double_t neutral_t=BarrelR/(Sin(theta)*C());
+    Double_t neutral_t_e=(Zhalflength-zcoord)/(C()*Cos(theta));
+    Double_t tdiff=Tfinal*relr/(C()*Sin(theta))-neutral_t;
+    Double_t Tz1=(Zhalflength-zcoord)*natcharge*1.60217657*B*Tan(theta)/(tmomentum*5.344286);
     if((*eta)>=0.0 && Abs(Tfinal)>Abs(Tz1)) {Tfinal=Tz1;
-	if(neutral_t>neutral_t_e) neutral_t=neutral_t_e;
-	value=Tfinal*m/(par[3]*B)-neutral_t;
+        if(neutral_t>neutral_t_e) neutral_t=neutral_t_e;
+        tdiff=Tfinal*relr/(C()*Sin(theta))-neutral_t;
     }
-    if((*eta)<0.0 && Abs(Tfinal)>Abs(Tz1)) {Tfinal=Tz1;
-	if(neutral_t>neutral_t_e) neutral_t=neutral_t_e;
-	value=Tfinal*m/(par[3]*B)-neutral_t;
-    }    
+/*  if((*eta)<0.0 && Abs(Tfinal)>Abs(Tz1)) {Tfinal=Tz1;
+        if(neutral_t>neutral_t_e) neutral_t=neutral_t_e;
+        tdiff=Tfinal*relr/(C()*Sin(theta))-neutral_t;
+    }*/
+    if(par[4]==0.0) value=tdiff;
+    if(par[4]==1.0) value=neutral_t;
+    if(par[4]==2.0) value=Tfinal*relr/(C()*Sin(theta));
     return value;
 }
+
 
 int main()
 {
     TFile *outfile=new TFile("1.root", "RECREATE");
     TCanvas *c1=new TCanvas("c1","asdfaksdfj",600,400);
-    TLegend *legend=new TLegend(0.4,0.6,0.89,0.89);
-    TF1 *f = new TF1( "deltat",delta_t, -3.,3.,4);
-    f->SetParameters(0.9999999999999,0.0,1.9E-31,1.6E-19);
+    TLegend *legend=new TLegend(0.6,0.7,0.89,0.89);
+    TF1 *f = new TF1( "deltat",delta_t, 0.1,3.,5);
+    f->SetParameters(5.0,0.0,1.0,9.1E-31,0.0);f->SetLineColor(42);
     f->Draw();
     f->Write();
     c1->Update();
-    TF1 *f2 = new TF1( "deltat2", delta_t, -3., 3., 4 );
-    f2->SetParameters(0.99999999,0.0,1.9E-31,1.6E-19);
+    TF1 *f2 = new TF1( "deltat2", delta_t, 0.1, 3., 5 );
+    f2->SetParameters(10.0,0.0,1.,9.1E-31,0.0);f2->SetLineColor(13);
     f2->Draw("SAME");
     f2->Write();
-    TF1 *f3 = new TF1( "deltat3", delta_t, -3., 3., 4 );
-    f3->SetParameters(0.99999999,0.0,1.9E-31,1.6E-19);
+    TF1 *f3 = new TF1( "deltat3", delta_t, 0.1, 3., 5 );
+    f3->SetParameters(50.0,0.0,1.,9.1E-31,0.0);
     f3->Draw("SAME");
     f3->Write();
-    legend->AddEntry(f,"Vz=0.","l");
-    legend->AddEntry(f2,"Vz=1.","l");
-    legend->AddEntry(f3,"Vz=2.","l");
+    legend->AddEntry(f,"p=5GeV","l");
+    legend->AddEntry(f2,"p=10GeV","l");
+    legend->AddEntry(f3,"p=50GeV","l");
     legend->Draw();
     c1->Update();
     c1->Write();
