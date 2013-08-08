71f0cf84be5617a2bb8d02a304e9cfbd3a7b8c0f
Mia <mia.tosi@cern.ch>
2013-08-08 15:19:50 +0200

    add Strip monitoring @HLT

--- DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
+++ DQMOffline/Trigger/python/DQMOffline_Trigger_cff.py
@@ -59,6 +59,78 @@ TrackerCollisionTrackMonHLT = DQM.TrackingMonitor.TrackerCollisionTrackingMonito
 TrackerCollisionTrackMonHLT.FolderName    = 'HLT/Tracking'
 TrackerCollisionTrackMonHLT.TrackProducer    = 'hltPixelTracks'
 
+#### TEST track collection
+import DQM.TrackingMonitor.TrackerCollisionTrackingMonitor_cfi
+TrackerCollisionTrackMonHLT = DQM.TrackingMonitor.TrackerCollisionTrackingMonitor_cfi.TrackerCollisionTrackMon.clone()
+TrackerCollisionTrackMonHLT.FolderName    = 'HLT/Tracking/iter4'
+TrackerCollisionTrackMonHLT.TrackProducer = 'hltIter4Merged'
+
+
+# SiStripCluster converter
+import EventFilter.SiStripRawToDigi.SiStripRawToClustersHLTdsvbuilder_cff
+HLTsiStripClusters = EventFilter.SiStripRawToDigi.SiStripRawToClustersHLTdsvbuilder_cff.siStripClusters.clone()
+HLTsiStripClusters.SiStripLazyGetter = cms.InputTag("hltSiStripRawToClustersFacility")
+HLTsiStripClusters.SiStripRefGetter  = cms.InputTag("hltSiStripClusters")
+
+# SiStripCluster monitoring
+import DQM.SiStripMonitorCluster.SiStripMonitorCluster_cfi
+HLTSiStripMonitorCluster = DQM.SiStripMonitorCluster.SiStripMonitorCluster_cfi.SiStripMonitorCluster.clone()
+HLTSiStripMonitorCluster.ClusterProducerStrip = cms.InputTag("HLTsiStripClusters")
+HLTSiStripMonitorCluster.ClusterProducerPix   = cms.InputTag("hltSiPixelClusters")
+HLTSiStripMonitorCluster.TopFolderName        = cms.string("HLT/SiStrip")
+HLTSiStripMonitorCluster.Mod_On = False
+HLTSiStripMonitorCluster.TH1TotalNumberOfClusters.subdetswitchon   = True
+HLTSiStripMonitorCluster.TProfClustersApvCycle.subdetswitchon      = False
+HLTSiStripMonitorCluster.TProfTotalNumberOfClusters.subdetswitchon = True
+HLTSiStripMonitorCluster.TH2CStripVsCpixel.globalswitchon       = True
+HLTSiStripMonitorCluster.TH1MultiplicityRegions.globalswitchon  = True
+HLTSiStripMonitorCluster.TH1MainDiagonalPosition.globalswitchon = True
+HLTSiStripMonitorCluster.TH1StripNoise2ApvCycle.globalswitchon  = False
+HLTSiStripMonitorCluster.TH1StripNoise3ApvCycle.globalswitchon  = False
+HLTSiStripMonitorCluster.ClusterHisto = True
+HLTSiStripMonitorCluster.BPTXfilter = cms.PSet(
+        andOr         = cms.bool( False ),
+            dbLabel       = cms.string("SiStripDQMTrigger"),
+            l1Algorithms = cms.vstring( 'L1Tech_BPTX_plus_AND_minus.v0', 'L1_ZeroBias' ),
+            andOrL1       = cms.bool( True ),
+            errorReplyL1  = cms.bool( True ),
+            l1BeforeMask  = cms.bool( True ) # specifies, if the L1 algorithm decision should be read as before (true) or after (false) masking is applied.
+        )
+HLTSiStripMonitorCluster.PixelDCSfilter = cms.PSet(
+        andOr         = cms.bool( False ),
+            dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+            dcsPartitions = cms.vint32 ( 28, 29),
+            andOrDcs      = cms.bool( False ),
+            errorReplyDcs = cms.bool( True ),
+        )
+HLTSiStripMonitorCluster.StripDCSfilter = cms.PSet(
+        andOr         = cms.bool( False ),
+            dcsInputTag   = cms.InputTag( "scalersRawToDigi" ),
+            dcsPartitions = cms.vint32 ( 24, 25, 26, 27 ),
+            andOrDcs      = cms.bool( False ),
+            errorReplyDcs = cms.bool( True ),
+        )
+
+import DQM.SiStripMonitorTrack.SiStripMonitorTrack_cfi
+HLTSiStripMonitorTrack = DQM.SiStripMonitorTrack.SiStripMonitorTrack_cfi.SiStripMonitorTrack.clone()
+HLTSiStripMonitorTrack.TrackProducer     = 'hltIter4Merged'
+HLTSiStripMonitorTrack.TrackLabel        = ''
+HLTSiStripMonitorTrack.TrajectoryInEvent = cms.bool(False)
+HLTSiStripMonitorTrack.AlgoName          = cms.string("HLT")
+HLTSiStripMonitorTrack.RawDigis_On       = cms.bool(False)
+HLTSiStripMonitorTrack.Cluster_src       = cms.InputTag('HLTsiStripClusters')
+HLTSiStripMonitorTrack.Trend_On          = cms.bool(True)
+HLTSiStripMonitorTrack.TopFolderName     = cms.string('HLT/SiStrip')
+
+offlineHLTtrackerSource = cms.Sequence(
+#    HLTSiPixelDigiSource *
+#    HLTSiPixelClusterSource *
+    HLTsiStripClusters *
+    HLTSiStripMonitorCluster *
+    HLTSiStripMonitorTrack
+)    
+
+
 import DQMServices.Components.DQMEnvironment_cfi
 dqmEnvHLT= DQMServices.Components.DQMEnvironment_cfi.dqmEnv.clone()
 dqmEnvHLT.subSystemFolder = 'HLT'
@@ -77,6 +149,8 @@ offlineHLTSource = cms.Sequence(
     #TnPEfficiency *
     hltInclusiveVBFSource *
     TrackerCollisionTrackMonHLT *
+#### tracker monitor @ HLT
+    offlineHLTtrackerSource *
     dqmEnvHLT)
 
 #triggerOfflineDQMSource =  cms.Sequence(onlineHLTSource*offlineHLTSource)
