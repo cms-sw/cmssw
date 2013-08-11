f30baff93bf9b061433fd7ce8c13495c2127dc47
Sven Dildick <sven.dildick@cern.ch>
2013-08-11 17:43:40 +0200

    port to latest CMSSW SLHC instructions

--- GEMValidation/test/runGEMDigiAnalyzer_cfg.py
+++ GEMValidation/test/runGEMDigiAnalyzer_cfg.py
@@ -1,19 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("GEMANA")
+process = cms.Process("GEMDIGIANA")
 
+## Standard sequence
+process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2019_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-#process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMXML_cfi')
-process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMr08v01XML_cfi')
-#process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMr10v01XML_cfi')
-process.load('Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi')
-process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
-process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
-process.load('Geometry.GEMGeometry.gemGeometry_cfi')
-process.load('Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometryDB_cff')
-process.load('Geometry.DTGeometryBuilder.idealForDigiDtGeometryDB_cff')
-process.load('Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff')
+## TrackingComponentsRecord required for matchers
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi')
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi')
+
+## global tag for 2019 upgrade studies
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
 
 # the analyzer configuration
 process.load('GEMCode.GEMValidation.GEMDigiAnalyzer_cfi')
@@ -23,30 +26,17 @@ process.GEMDigiAnalyzer.simTrackMatching.cscCLCTInput = ""
 process.GEMDigiAnalyzer.simTrackMatching.cscALCTInput = ""
 process.GEMDigiAnalyzer.simTrackMatching.cscLCTInput = ""
 
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'POSTLS161_V12::All'
-
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring("file:out_digi.root")
+)
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string("gem_digi_ana.root")
 )
 
-#dir_pt40 = '/pnfs/cms/WAX/11/store/user/lpcgem/yasser1/yasser/muonGun_50k_pT40_lpcgem/MuomGunPt40L1CSC50k_digi/82325e40d6202e6fec2dd983c477f3ca/'
-#inputDir = dir_pt40
-
-import os
-
-#ls = os.listdir(inputDir)
-
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-    #[inputDir[16:] + x for x in ls if x.endswith('root')]
-    'file:out_digi.root'
-    )
-)
-
-process.p    = cms.Path(process.GEMDigiAnalyzer)
+process.p = cms.Path(process.GEMDigiAnalyzer)
 
