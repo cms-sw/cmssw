4ad54a7fa22445f15d7f0702a9baa1f43ab6bc82
Junghwan John Goh <jhgoh@cern.ch>
2013-08-12 09:46:34 +0200

    cleanup

--- GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
+++ GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 generator = cms.EDProducer("GenParticles2HepMCConverter",
-    lheEvent = cms.InputTag("source"),
     genParticles = cms.InputTag("genParticles"),
-#    genRunInfo   = cms.InputTag("generator"),
     genEventInfo = cms.InputTag("generator"),
 )
