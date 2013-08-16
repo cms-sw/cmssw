63442ed8cb5bb4d4711d819e682e04cf46389adc
Andrea <andrea.giammanco@cern.ch>
2013-08-16 17:09:38 +0200

    Towards the new mixing scheme

--- FastSimulation/Configuration/python/MixingHitsAndTracks_cff.py
+++ FastSimulation/Configuration/python/MixingHitsAndTracks_cff.py
@@ -1,18 +1,25 @@
 #from FastSimulation.Configuration.mixNoPU_cfi import * 
 
-# PileupSummaryInfo is taken from mixHits because this is the one that can handle OOT pileup
 from SimGeneral.PileupInformation.AddPileupSummary_cfi import *
-addPileupInfo.PileupMixingLabel = 'mixSimCaloHits'
+#addPileupInfo.PileupMixingLabel = 'mixSimCaloHits'
+addPileupInfo.PileupMixingLabel = 'mix'
 addPileupInfo.simHitLabel = 'g4SimHits'
 
-from FastSimulation.Configuration.mixHitsWithPU_cfi import *
-mixHits = cms.Sequence(
-    mixSimCaloHits+
+from FastSimulation.Configuration.mixHitsAndTracksWithPU_cfi import *
+mixHitsAndTracks = cms.Sequence(
+    mix+
     addPileupInfo
     )
     
-from FastSimulation.Configuration.mixTracksWithPU_cfi import *
-mixTracks = cms.Sequence(
-    mixRecoTracks
-    )
+
+#from FastSimulation.Configuration.mixHitsWithPU_cfi import *
+#mixHits = cms.Sequence(
+#    mixSimCaloHits+
+#    addPileupInfo
+#    )
+    
+#from FastSimulation.Configuration.mixTracksWithPU_cfi import *
+#mixTracks = cms.Sequence(
+#    mixRecoTracks
+#    )
     
