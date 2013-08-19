b339fcacae27d3e8b5f43b4985ac7036bbf401ed
Matti Kortelainen <matti.kortelainen@cern.ch>
2013-08-19 14:34:11 +0200

    Remove partly the recursion in recSearch()
    The recursion in cases only one child node will be accessed can be
    easily transformed to iteration, which is somewhat faster. When both
    children need to be accessed, it is probably better to keep the
    recursion, as some kind of stack would be needed anyway.

--- RecoPixelVertexing/PixelTriplets/plugins/KDTreeLinkerAlgo.h
+++ RecoPixelVertexing/PixelTriplets/plugins/KDTreeLinkerAlgo.h
@@ -148,31 +148,47 @@ KDTreeLinkerAlgo<DATA>::recSearch(int	current,
   assert (!(((nodePool_.left[current] < 0) && (nodePool_.right[current] >= 0)) ||
             ((nodePool_.left[current] >= 0) && (nodePool_.right[current] < 0))));
   */
-  int right = nodePool_.right[current];
+  while(true) {
+    int right = nodePool_.right[current];
+    if(nodePool_.isLeaf(right)) {
+      float dimCurr = nodePool_.median[current];
 
-  if (nodePool_.isLeaf(right)) {
-    float dimCurr = nodePool_.median[current];
-
-    // If point inside the rectangle/area
-    // Use intentionally bit-wise & instead of logical && for better
-    // performance. It is faster to always do all comparisons than to
-    // allow use of branches to not do some if any of the first ones
-    // is false.
-    if((dimCurr >= dimCurrMin) & (dimCurr <= dimCurrMax)) {
-      float dimOther = nodePool_.dimOther[current];
-      if((dimOther >= dimOtherMin) & (dimOther <= dimOtherMax)) {
-        closestNeighbour->push_back(nodePool_.data[current]);
+      // If point inside the rectangle/area
+      // Use intentionally bit-wise & instead of logical && for better
+      // performance. It is faster to always do all comparisons than to
+      // allow use of branches to not do some if any of the first ones
+      // is false.
+      if((dimCurr >= dimCurrMin) & (dimCurr <= dimCurrMax)) {
+        float dimOther = nodePool_.dimOther[current];
+        if((dimOther >= dimOtherMin) & (dimOther <= dimOtherMax)) {
+          closestNeighbour->push_back(nodePool_.data[current]);
+        }
       }
+      break;
     }
-  } else {
-    float median = nodePool_.median[current];
+    else {
+      float median = nodePool_.median[current];
 
-    if(dimCurrMin <= median) {
-      int left = current+1;
-      recSearch(left, dimOtherMin, dimOtherMax, dimCurrMin, dimCurrMax);
-    }
-    if(dimCurrMax > median) {
-      recSearch(right, dimOtherMin, dimOtherMax, dimCurrMin, dimCurrMax);
+      bool goLeft = (dimCurrMin <= median);
+      bool goRight = (dimCurrMax > median);
+
+      std::swap(dimCurrMin, dimOtherMin);
+      std::swap(dimCurrMax, dimOtherMax);
+      if(goLeft & goRight) {
+        int left = current+1;
+        recSearch(left, dimCurrMin, dimCurrMax, dimOtherMin, dimOtherMax);
+        // continue with right
+        current = right;
+      }
+      else if(goLeft) {
+        ++current;
+      }
+      else if(goRight) {
+        current = right;
+      }
+      else {
+        break;
+      }
     }
   }
 }
