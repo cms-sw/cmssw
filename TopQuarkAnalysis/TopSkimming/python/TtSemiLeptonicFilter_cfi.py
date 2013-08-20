e82f96a19efdba8e0ed0c8e7c8d1f33b28ef6cba
Volker Adler <volker.adler@cern.ch>
2013-08-20 23:44:01 +0200

    Fix typo in Python path.

--- TopQuarkAnalysis/TopSkimming/python/TtSemiLeptonicFilter_cfi.py
+++ TopQuarkAnalysis/TopSkimming/python/TtSemiLeptonicFilter_cfi.py
@@ -1,5 +1,5 @@
 print "*** Warning: TopQuarkAnalysis/TopSkimming/python/TtSemiLeptonicFilter_cfi.py will be deprecated"
 print "*** Please use TopQuarkAnalysis/TopSkimming/python/TtDecayChannelFilter_cfi.py or"
-print "*** TopQuarkAnalysis/TopSkimming/python/ttDecayChannelFilter_cff.py instead"
+print "*** TopQuarkAnalysis/TopSkimming/python/ttDecayChannelFilters_cff.py instead"
 
-from TopQuarkAnalysis.TopSkimming.ttDecayChannelFilter_cff import ttSemiLeptonicFilter
+from TopQuarkAnalysis.TopSkimming.ttDecayChannelFilters_cff import ttSemiLeptonicFilter
