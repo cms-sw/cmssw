54f36f5405a7073fd5d1cf309a7cfacdc9e1553b
Chris Jones <chrisdjones15@gmail.com>
2013-08-22 13:11:18 -0500

    Code cleanup
    Removed unnecessary includes and source code.

--- FWCore/Framework/src/GlobalSchedule.cc
+++ FWCore/Framework/src/GlobalSchedule.cc
@@ -1,81 +1,20 @@
 #include "FWCore/Framework/src/GlobalSchedule.h"
+#include "FWCore/Framework/src/WorkerMaker.h"
 
-#include "DataFormats/Provenance/interface/BranchIDListHelper.h"
 #include "DataFormats/Provenance/interface/ProcessConfiguration.h"
 #include "DataFormats/Provenance/interface/ProductRegistry.h"
-#include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/Framework/interface/OutputModuleDescription.h"
-#include "FWCore/Framework/interface/TriggerNamesService.h"
-#include "FWCore/Framework/interface/TriggerReport.h"
-#include "FWCore/Framework/src/Factory.h"
-#include "FWCore/Framework/interface/OutputModule.h"
-#include "FWCore/Framework/src/OutputModuleCommunicator.h"
-#include "FWCore/Framework/src/TriggerResultInserter.h"
-#include "FWCore/Framework/src/WorkerInPath.h"
-#include "FWCore/Framework/src/WorkerMaker.h"
-#include "FWCore/Framework/src/WorkerT.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ParameterSet/interface/FillProductRegistryTransients.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "FWCore/ParameterSet/interface/Registry.h"
 #include "FWCore/Utilities/interface/Algorithms.h"
-#include "FWCore/Utilities/interface/ConvertException.h"
-#include "FWCore/Utilities/interface/ExceptionCollector.h"
-#include "FWCore/Utilities/interface/DictionaryTools.h"
-
-#include "boost/bind.hpp"
-#include "boost/ref.hpp"
+#include "FWCore/Utilities/interface/Exception.h"
 
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <functional>
-#include <iomanip>
-#include <list>
 #include <map>
-#include <exception>
 
 namespace edm {
-  namespace {
-
-    // Function template to transform each element in the input range to
-    // a value placed into the output range. The supplied function
-    // should take a const_reference to the 'input', and write to a
-    // reference to the 'output'.
-    template <typename InputIterator, typename ForwardIterator, typename Func>
-    void
-    transform_into(InputIterator begin, InputIterator end,
-                   ForwardIterator out, Func func) {
-      for (; begin != end; ++begin, ++out) func(*begin, *out);
-    }
-
-    // Function template that takes a sequence 'from', a sequence
-    // 'to', and a callable object 'func'. It and applies
-    // transform_into to fill the 'to' sequence with the values
-    // calcuated by the callable object, taking care to fill the
-    // outupt only if all calls succeed.
-    template <typename FROM, typename TO, typename FUNC>
-    void
-    fill_summary(FROM const& from, TO& to, FUNC func) {
-      TO temp(from.size());
-      transform_into(from.begin(), from.end(), temp.begin(), func);
-      to.swap(temp);
-    }
-
-    // -----------------------------
-
-    bool binary_search_string(std::vector<std::string> const& v, std::string const& s) {
-      return std::binary_search(v.begin(), v.end(), s);
-    }
-  }
-
-  // -----------------------------
-
-  typedef std::vector<std::string> vstring;
-
-  // -----------------------------
-
   GlobalSchedule::GlobalSchedule(boost::shared_ptr<ModuleRegistry> modReg,
                                  std::vector<std::string> const& iModulesToUse,
                                  ParameterSet& proc_pset,
