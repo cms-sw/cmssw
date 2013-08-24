6b0bf11df9d25279a420321a78f0d6879354165d
Alja <amraktadel@ucsd.edu>
2013-08-24 08:18:34 +0200

    Merge from CVS HEAD.

--- Fireworks/Electrons/plugins/FWConvTrackHitsDetailView.h
+++ Fireworks/Electrons/plugins/FWConvTrackHitsDetailView.h
@@ -3,13 +3,17 @@
 // Package:     Tracks
 // Class  :     FWConvTrackHitsDetailView
 
+#ifndef Fireworks_Electrons_FWConversionDetailView_h
+#define Fireworks_Electrons_FWConversionDetailView_h
+
 #include "Rtypes.h"
 
 #include "Fireworks/Core/interface/FWDetailViewGL.h"
 #include "Fireworks/Core/interface/CSGActionSupervisor.h"
 
 class TGLEmbeddedViewer;
-class FWIntValueListener;
+class TGTextButton;
+
 namespace reco {
    class Conversion;
 }
@@ -22,41 +26,47 @@ public:
    virtual ~FWConvTrackHitsDetailView();
 
    void pickCameraCenter();
-   void transparencyChanged(int);
-   void rnrLabels();
+   void rnrLabels();   
+   void rnrModules();
+   void rnrHits();
 
-   virtual void setBackgroundColor(Color_t);
-
-protected:
-   TEveElementList*    m_modules;
-   TEveElementList*    m_moduleLabels;
-   TEveElementList*    m_hits;
-   TGSlider*           m_slider;
-   FWIntValueListener* m_sliderListener;
-  
+   void camera1Callback();
+   void camera2Callback();
+   void camera3Callback();
+   void switchProjection();
+      
 private:
    FWConvTrackHitsDetailView(const FWConvTrackHitsDetailView&); // stop default
    const FWConvTrackHitsDetailView& operator=(const FWConvTrackHitsDetailView&); // stop default
-
+   
    void build (const FWModelId &id, const reco::Conversion*);
    void setTextInfo (const FWModelId &id, const reco::Conversion*);
-   void makeLegend( void );
-
+   
    void
    addTrackerHits3D( std::vector<TVector3> &points,
-		     class TEveElementList *tList,
-		     Color_t color, int size );
-
+                    class TEveElementList *tList,
+                    Color_t color, int size );
+   
    void
    addHits( const reco::Track& track,
-	    const FWEventItem* iItem,
-	    TEveElement* trkList,
-	    bool addNearbyHits );
+           const FWEventItem* iItem,
+           TEveElement* trkList,
+           bool addNearbyHits );
    void
    addModules( const reco::Track& track,
-	       const FWEventItem* iItem,
-	       TEveElement* trkList,
-	       bool addLostHits );
-  
-   TLegend             *m_legend;
+              const FWEventItem* iItem,
+              TEveElement* trkList,
+              bool addLostHits );
+   
+   
+   TEveElementList*    m_modules;
+   TEveElementList*    m_moduleLabels;
+   TEveElementList*    m_hits;
+   TEveElement*          m_calo3D;
+   
+   TLegend*                m_legend;
+   bool                       m_orthographic;
+   CSGAction*            m_camTypeAction;
 };
+
+#endif
