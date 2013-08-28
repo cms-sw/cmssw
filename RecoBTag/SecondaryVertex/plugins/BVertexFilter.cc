0d1fb1bf683e1f3b56b9d3c1236fbf104ef9ce3d
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2013-08-28 03:36:12 +0200

    Updates corresponding to the CVS tag change V01-08-03 --> V01-11-02 of RecoBTag/SecondaryVertex
    - added retrained CSV (CSVV1) and double SV taggers

--- RecoBTag/SecondaryVertex/plugins/BVertexFilter.cc
+++ RecoBTag/SecondaryVertex/plugins/BVertexFilter.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Andrea RIZZI
 //         Created:  Mon Dec  7 18:02:10 CET 2009
-// $Id: BVertexFilter.cc,v 1.4 2010/02/28 20:10:01 wmtan Exp $
+// $Id: BVertexFilter.cc,v 1.2 2012/10/06 21:17:41 alschmid Exp $
 //
 //
 
@@ -45,8 +45,8 @@ class BVertexFilter : public edm::EDFilter {
 
    private:
       virtual bool filter(edm::Event&, const edm::EventSetup&);
-      edm::EDGetTokenT<reco::VertexCollection> token_primaryVertex;
-      edm::EDGetTokenT<reco::VertexCollection> token_secondaryVertex;
+      edm::InputTag                           primaryVertexCollection;
+      edm::InputTag                           secondaryVertexCollection;
       reco::VertexFilter                      svFilter;
       bool                                    useVertexKinematicAsJetAxis;
       int                                     minVertices;
@@ -54,13 +54,13 @@ class BVertexFilter : public edm::EDFilter {
 
 
 BVertexFilter::BVertexFilter(const edm::ParameterSet& params):
+      primaryVertexCollection(params.getParameter<edm::InputTag>("primaryVertices")),
+      secondaryVertexCollection(params.getParameter<edm::InputTag>("secondaryVertices")),
       svFilter(params.getParameter<edm::ParameterSet>("vertexFilter")),
       useVertexKinematicAsJetAxis(params.getParameter<bool>("useVertexKinematicAsJetAxis")),
       minVertices(params.getParameter<int>("minVertices"))
 
 {
-	token_primaryVertex = consumes<reco::VertexCollection>(params.getParameter<edm::InputTag>("primaryVertices"));
-	token_secondaryVertex = consumes<reco::VertexCollection>(params.getParameter<edm::InputTag>("secondaryVertices"));
         produces<reco::VertexCollection>();
 
 }
@@ -75,28 +75,31 @@ BVertexFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
  int count = 0; 
  edm::Handle<reco::VertexCollection> pvHandle; 
- iEvent.getByToken(token_primaryVertex, pvHandle);
+ iEvent.getByLabel(primaryVertexCollection,pvHandle);
  edm::Handle<reco::VertexCollection> svHandle; 
- iEvent.getByToken(token_secondaryVertex, svHandle);
- const reco::Vertex & primary = (*pvHandle.product())[0];
- const reco::VertexCollection & vertices = *svHandle.product();
+ iEvent.getByLabel(secondaryVertexCollection,svHandle);
  std::auto_ptr<reco::VertexCollection> recoVertices(new reco::VertexCollection);
+ if(pvHandle->size()!=0) {
+   const reco::Vertex & primary = (*pvHandle.product())[0];
+   const reco::VertexCollection & vertices = *svHandle.product();
 
- if(! primary.isFake()) 
- {
-   for(reco::VertexCollection::const_iterator it=vertices.begin() ; it!=vertices.end() ; ++it)
-    {
-          GlobalVector axis(0,0,0);
-          if(useVertexKinematicAsJetAxis) axis = GlobalVector(it->p4().X(),it->p4().Y(),it->p4().Z());
-          if(svFilter(primary,reco::SecondaryVertex(primary,*it,axis,true),axis))  {
-                count++;
-                recoVertices->push_back(*it);
+
+   if(! primary.isFake()) 
+     {
+       for(reco::VertexCollection::const_iterator it=vertices.begin() ; it!=vertices.end() ; ++it)
+	 {
+	   GlobalVector axis(0,0,0);
+	   if(useVertexKinematicAsJetAxis) axis = GlobalVector(it->p4().X(),it->p4().Y(),it->p4().Z());
+	   if(svFilter(primary,reco::SecondaryVertex(primary,*it,axis,true),axis))  {
+	     count++;
+	     recoVertices->push_back(*it);
            }
-   }
+	 }
+     }
  }
-   iEvent.put(recoVertices);
-
-   return(count >= minVertices);
+ iEvent.put(recoVertices);
+ 
+ return(count >= minVertices);
 }
 
 
