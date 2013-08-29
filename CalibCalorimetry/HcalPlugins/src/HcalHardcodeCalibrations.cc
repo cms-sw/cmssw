169af69d0373edff155718529dfc4e6dbe2ee89b
David <David.Lange@cern.ch>
2013-08-29 22:56:18 +0200

    big porting

--- CalibCalorimetry/HcalPlugins/src/HcalHardcodeCalibrations.cc
+++ CalibCalorimetry/HcalPlugins/src/HcalHardcodeCalibrations.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 // Original Author:  Fedor Ratnikov
-// $Id: HcalHardcodeCalibrations.cc,v 1.43 2013/04/23 16:06:26 abdullin Exp $
+// $Id: HcalHardcodeCalibrations.cc,v 1.44 2013/06/03 17:44:52 abdullin Exp $
 //
 //
 
@@ -117,7 +117,7 @@ HcalHardcodeCalibrations::HcalHardcodeCalibrations ( const edm::ParameterSet& iC
   else  switchGainWidthsForTrigPrims = false;
        
 
-  // HE recalibration preparation
+  // HE and HF recalibration preparation
   iLumi = 0.;
   if ( iConfig.exists("iLumi") )
     iLumi=iConfig.getParameter<double>("iLumi");
@@ -125,7 +125,10 @@ HcalHardcodeCalibrations::HcalHardcodeCalibrations ( const edm::ParameterSet& iC
   if( iLumi > 0.0 ) {
     bool he_recalib = iConfig.getParameter<bool>("HERecalibration");
     bool hf_recalib = iConfig.getParameter<bool>("HFRecalibration");
-    if(he_recalib)  he_recalibration = new HERecalibration(iLumi);
+    if(he_recalib) {
+      double cutoff = iConfig.getParameter<double>("HEreCalibCutoff"); 
+      he_recalibration = new HERecalibration(iLumi,cutoff);
+    }
     if(hf_recalib)  hf_recalibration = new HFRecalibration();
     
     //     std::cout << " HcalHardcodeCalibrations:  iLumi = " <<  iLumi << std::endl;
