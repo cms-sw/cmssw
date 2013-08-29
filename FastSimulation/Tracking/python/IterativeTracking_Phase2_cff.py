028303181880ffd38e9bc68e8597ee28e2745a78
David <David.Lange@cern.ch>
2013-08-29 23:51:41 +0200

    port from slhc8p1

---
+++ FastSimulation/Tracking/python/IterativeTracking_Phase2_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from FastSimulation.Tracking.PixelTracksProducer_cff import *
+from FastSimulation.Tracking.PixelVerticesProducer_cff import *
+from TrackingTools.TrackFitters.TrackFitters_cff import *
+from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
+
+from FastSimulation.Tracking.IterativeInitialStep_Phase2_cff import *
+from FastSimulation.Tracking.GeneralTracks_Phase2_cfi import *
+
+iterativeTracking = cms.Sequence(pixelTracking+pixelVertexing
+                                 +iterativeInitialStep
+                                 +generalTracks
+                                 +trackExtrapolator)
+
+
