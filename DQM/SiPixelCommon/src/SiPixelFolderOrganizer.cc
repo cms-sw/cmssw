30dc3c5db0dda1044ff71d09cbb0d8f3d3cd19a7
David <David.Lange@cern.ch>
2013-08-29 22:56:22 +0200

    big porting

--- DQM/SiPixelCommon/src/SiPixelFolderOrganizer.cc
+++ DQM/SiPixelCommon/src/SiPixelFolderOrganizer.cc
@@ -7,7 +7,9 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
 #include "DataFormats/SiPixelDetId/interface/PixelBarrelName.h"
+#include "DataFormats/SiPixelDetId/interface/PixelBarrelNameUpgrade.h"
 #include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
+#include "DataFormats/SiPixelDetId/interface/PixelEndcapNameUpgrade.h"
 #include <sstream>
 #include <cstdio>
 
@@ -21,7 +23,7 @@ SiPixelFolderOrganizer::SiPixelFolderOrganizer() :
 
 SiPixelFolderOrganizer::~SiPixelFolderOrganizer() {}
 
-bool SiPixelFolderOrganizer::setModuleFolder(const uint32_t& rawdetid, int type) {
+bool SiPixelFolderOrganizer::setModuleFolder(const uint32_t& rawdetid, int type, bool isUpgrade) {
 
   bool flag = false;
 
@@ -34,6 +36,7 @@ bool SiPixelFolderOrganizer::setModuleFolder(const uint32_t& rawdetid, int type)
    ///
    else if(DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) {
 
+     if (!isUpgrade) {
      //for endcap types there is nothing to do: 
      if(type>3 && type!=7) return true;
 
@@ -66,12 +69,48 @@ bool SiPixelFolderOrganizer::setModuleFolder(const uint32_t& rawdetid, int type)
      
      dbe_->setCurrentFolder(sfolder.str().c_str());
      flag = true;
+     } else if (isUpgrade) {
+       //for endcap types there is nothing to do: 
+       if(type>3 && type!=7) return true;
+       
+       std::string subDetectorFolder = "Barrel";
+       PixelBarrelNameUpgrade::Shell DBshell = PixelBarrelNameUpgrade(DetId(rawdetid)).shell();
+       int DBlayer  = PixelBarrelNameUpgrade(DetId(rawdetid)).layerName();
+       int DBladder = PixelBarrelNameUpgrade(DetId(rawdetid)).ladderName();
+       int DBmodule = PixelBarrelNameUpgrade(DetId(rawdetid)).moduleName();
+       
+       char slayer[80];  sprintf(slayer, "Layer_%i",   DBlayer);
+       char sladder[80]; sprintf(sladder,"Ladder_%02i",DBladder);
+       char smodule[80]; sprintf(smodule,"Module_%i",  DBmodule);
+       
+       std::ostringstream sfolder;
+       
+       sfolder << rootFolder << "/" << subDetectorFolder; 
+       if(type<4){
+       sfolder << "/Shell_" <<DBshell
+               << "/" << slayer;
    } 
+       if(type<2){
+         sfolder << "/" << sladder;
+         if ( PixelBarrelNameUpgrade(DetId(rawdetid)).isHalfModule() ) sfolder <<"H"; 
+         else sfolder <<"F";
+       }
+       if(type==0) sfolder << "/" <<smodule;
+       //if(type==3) sfolder << "/all_" << smodule;
+       
+       //std::cout<<"set barrel folder: "<<rawdetid<<" : "<<sfolder.str().c_str()<<std::endl;
+       
+       dbe_->setCurrentFolder(sfolder.str().c_str());
+       flag = true;
+     }//endif(isUpgrade)
+   } 
+   
    ///
    /// Pixel Endcap
    ///
    else if(DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelEndcap)) {
 
+     if (!isUpgrade) {
      //for barrel types there is nothing to do: 
      if(type>0 && type < 4) return true;
 
@@ -106,6 +145,42 @@ bool SiPixelFolderOrganizer::setModuleFolder(const uint32_t& rawdetid, int type)
       dbe_->setCurrentFolder(sfolder.str().c_str());
       flag = true;
 
+     } else if (isUpgrade) {
+       //for barrel types there is nothing to do: 
+       if(type>0 && type < 4) return true;
+       
+       std::string subDetectorFolder = "Endcap";
+       PixelEndcapNameUpgrade::HalfCylinder side = PixelEndcapNameUpgrade(DetId(rawdetid)).halfCylinder();
+        int disk   = PixelEndcapNameUpgrade(DetId(rawdetid)).diskName();
+        int blade  = PixelEndcapNameUpgrade(DetId(rawdetid)).bladeName();
+        int panel  = PixelEndcapNameUpgrade(DetId(rawdetid)).pannelName();
+        int module = PixelEndcapNameUpgrade(DetId(rawdetid)).plaquetteName();
+        
+        
+        char sdisk[80];  sprintf(sdisk,  "Disk_%i",disk);
+        char sblade[80]; sprintf(sblade, "Blade_%02i",blade);
+        char spanel[80]; sprintf(spanel, "Panel_%i",panel);
+        char smodule[80];sprintf(smodule,"Module_%i",module);
+        
+        std::ostringstream sfolder;
+        
+        sfolder <<rootFolder <<"/" << subDetectorFolder << 
+          "/HalfCylinder_" << side << "/" << sdisk; 
+        if(type==0 || type ==4){
+          sfolder << "/" << sblade; 
+        }
+        if(type==0){
+          sfolder << "/" << spanel << "/" << smodule;
+        }
+//        if(type==6){
+//          sfolder << "/" << spanel << "_all_" << smodule;
+//        }
+       
+       //std::cout<<"set endcap folder: "<<rawdetid<<" : "<<sfolder.str().c_str()<<std::endl;
+       
+       dbe_->setCurrentFolder(sfolder.str().c_str());
+       flag = true;
+     }//endifendcap&&isUpgrade
    } else throw cms::Exception("LogicError")
      << "[SiPixelFolderOrganizer::setModuleFolder] Not a Pixel detector DetId ";
    
@@ -127,13 +202,13 @@ bool SiPixelFolderOrganizer::setFedFolder(const uint32_t FedId) {
 }
 
 void SiPixelFolderOrganizer::getModuleFolder(const uint32_t& rawdetid, 
-                                             std::string& path) {
+                                             std::string& path,
+                                             bool isUpgrade) {
 
   path = rootFolder;
-  //std::cout<<"rawdetis: "<<rawdetid<<" , path= "<<path<<std::endl;
   if(rawdetid == 0) {
     return;
-  }else if(DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) {
+  }else if( (DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) && (!isUpgrade) ) {
     std::string subDetectorFolder = "Barrel";
     PixelBarrelName::Shell DBshell = PixelBarrelName(DetId(rawdetid)).shell();
     int DBlayer  = PixelBarrelName(DetId(rawdetid)).layerName();
@@ -158,13 +233,58 @@ void SiPixelFolderOrganizer::getModuleFolder(const uint32_t& rawdetid,
     //else path = path + "F";
     //path = path + "/" + smodule;
 
-  }else if(DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelEndcap)) {
+  }else if( (DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelBarrel)) && (isUpgrade) ) {
+    std::string subDetectorFolder = "Barrel";
+    PixelBarrelNameUpgrade::Shell DBshell = PixelBarrelNameUpgrade(DetId(rawdetid)).shell();
+    int DBlayer  = PixelBarrelNameUpgrade(DetId(rawdetid)).layerName();
+    int DBladder = PixelBarrelNameUpgrade(DetId(rawdetid)).ladderName();
+    int DBmodule = PixelBarrelNameUpgrade(DetId(rawdetid)).moduleName();
+    
+    //char sshell[80];  sprintf(sshell, "Shell_%i",   DBshell);
+    char slayer[80];  sprintf(slayer, "Layer_%i",   DBlayer);
+    char sladder[80]; sprintf(sladder,"Ladder_%02i",DBladder);
+    char smodule[80]; sprintf(smodule,"Module_%i",  DBmodule);
+    
+    std::ostringstream sfolder;
+    sfolder << rootFolder << "/" << subDetectorFolder << "/Shell_" <<DBshell << "/" << slayer << "/" << sladder;
+    if ( PixelBarrelNameUpgrade(DetId(rawdetid)).isHalfModule() ) sfolder <<"H"; 
+    else sfolder <<"F";
+    sfolder << "/" <<smodule;
+    path = sfolder.str().c_str();
+   
+    //path = path + "/" + subDetectorFolder + "/" + sshell + "/" + slayer + "/" + sladder;
+    //if(PixelBarrelNameUpgrade(DetId(rawdetid)).isHalfModule() )
+    //  path = path + "H"; 
+    //else path = path + "F";
+    //path = path + "/" + smodule;
+
+  } else if( (DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelEndcap)) && (!isUpgrade) ) {
     std::string subDetectorFolder = "Endcap";
     PixelEndcapName::HalfCylinder side = PixelEndcapName(DetId(rawdetid)).halfCylinder();
     int disk   = PixelEndcapName(DetId(rawdetid)).diskName();
     int blade  = PixelEndcapName(DetId(rawdetid)).bladeName();
     int panel  = PixelEndcapName(DetId(rawdetid)).pannelName();
     int module = PixelEndcapName(DetId(rawdetid)).plaquetteName();
+
+    //char shc[80];  sprintf(shc,  "HalfCylinder_%i",side);
+    char sdisk[80];  sprintf(sdisk,  "Disk_%i",disk);
+    char sblade[80]; sprintf(sblade, "Blade_%02i",blade);
+    char spanel[80]; sprintf(spanel, "Panel_%i",panel);
+    char smodule[80];sprintf(smodule,"Module_%i",module);
+
+    std::ostringstream sfolder;
+    sfolder <<rootFolder <<"/" << subDetectorFolder << "/HalfCylinder_" << side << "/" << sdisk << "/" << sblade << "/" << spanel << "/" << smodule;
+    path = sfolder.str().c_str();
+    
+    //path = path + "/" + subDetectorFolder + "/" + shc + "/" + sdisk + "/" + sblade + "/" + spanel + "/" + smodule;
+
+  } else if( (DetId(rawdetid).subdetId() == static_cast<int>(PixelSubdetector::PixelEndcap)) && (isUpgrade) ) {
+    std::string subDetectorFolder = "Endcap";
+    PixelEndcapNameUpgrade::HalfCylinder side = PixelEndcapNameUpgrade(DetId(rawdetid)).halfCylinder();
+    int disk   = PixelEndcapNameUpgrade(DetId(rawdetid)).diskName();
+    int blade  = PixelEndcapNameUpgrade(DetId(rawdetid)).bladeName();
+    int panel  = PixelEndcapNameUpgrade(DetId(rawdetid)).pannelName();
+    int module = PixelEndcapNameUpgrade(DetId(rawdetid)).plaquetteName();
 
     //char shc[80];  sprintf(shc,  "HalfCylinder_%i",side);
     char sdisk[80];  sprintf(sdisk,  "Disk_%i",disk);
