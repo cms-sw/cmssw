a96beaeb4af8abecc455eb8fe821e0dad82dea7d
David <David.Lange@cern.ch>
2013-08-29 22:56:25 +0200

    big porting

--- DQM/SiPixelMonitorClient/src/SiPixelDataQuality.cc
+++ DQM/SiPixelMonitorClient/src/SiPixelDataQuality.cc
@@ -21,7 +21,9 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/GeometrySurface/interface/Surface.h"
 #include "DataFormats/SiPixelDetId/interface/PixelBarrelName.h"
+#include "DataFormats/SiPixelDetId/interface/PixelBarrelNameUpgrade.h"
 #include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
+#include "DataFormats/SiPixelDetId/interface/PixelEndcapNameUpgrade.h"
 #include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
 
 #include "CondFormats/SiPixelObjects/interface/DetectorIndex.h"
