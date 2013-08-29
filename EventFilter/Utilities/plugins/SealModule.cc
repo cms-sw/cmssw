4229414c18b751bbc4174f662e498cba58fdc2e5
Emilio <Emilio.Meschi@cern.ch>
2013-08-29 16:52:34 +0200

    Fix remaining compilation problems wrt 70X

--- EventFilter/Utilities/plugins/SealModule.cc
+++ EventFilter/Utilities/plugins/SealModule.cc
@@ -1,10 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 //#include "EventFilter/Utilities/interface/MicroStateService.h"
-#include "EventFilter/Utilities/interface/ModuleWebRegistry.h"
-#include "EventFilter/Utilities/interface/ServiceWebRegistry.h"
-#include "EventFilter/Utilities/interface/TimeProfilerService.h"
-#include "EventFilter/Utilities/interface/Stepper.h"
+//#include "EventFilter/Utilities/interface/TimeProfilerService.h"
 #include "EventFilter/Utilities/plugins/ExceptionGenerator.h"
 #include "EventFilter/Utilities/plugins/EvFRecordInserter.h"
 #include "EventFilter/Utilities/plugins/EvFRecordUnpacker.h"
@@ -22,19 +19,14 @@ using namespace evf;
 
 //typedef edm::serviceregistry::AllArgsMaker<MicroStateService> MicroStateServiceMaker;
 typedef edm::serviceregistry::AllArgsMaker<MicroStateService, FastMonitoringService> FastMonitoringServiceMaker;
-typedef ParameterSetMaker<ModuleWebRegistry> maker1;
-typedef ParameterSetMaker<ServiceWebRegistry> maker2;
 
 typedef RawEventOutputModuleForBU<RawEventFileWriterForBU> RawStreamFileWriterForBU;
 typedef RawEventOutputModuleForBU<MTRawEventFileWriterForBU> MTRawStreamFileWriterForBU;
 typedef RecoEventOutputModuleForFU<RecoEventWriterForFU> Stream;
 
 //DEFINE_FWK_SERVICE_MAKER(MicroStateService, MicroStateServiceMaker);
-DEFINE_FWK_SERVICE_MAKER(ModuleWebRegistry,maker1);
-DEFINE_FWK_SERVICE_MAKER(ServiceWebRegistry,maker2);
+
 DEFINE_FWK_SERVICE_MAKER(FastMonitoringService, FastMonitoringServiceMaker);
-DEFINE_FWK_SERVICE(TimeProfilerService);
-DEFINE_FWK_SERVICE(Stepper);
 DEFINE_FWK_SERVICE(EvFBuildingThrottle);
 DEFINE_FWK_SERVICE(EvFDaqDirector);
 DEFINE_FWK_MODULE(ExceptionGenerator);
