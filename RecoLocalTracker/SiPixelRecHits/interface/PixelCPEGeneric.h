4270091e928d42bc13dc1ed2bd515b4e51eb57b3
David <David.Lange@cern.ch>
2013-08-29 22:57:01 +0200

    big porting

--- RecoLocalTracker/SiPixelRecHits/interface/PixelCPEGeneric.h
+++ RecoLocalTracker/SiPixelRecHits/interface/PixelCPEGeneric.h
@@ -115,6 +115,8 @@ class PixelCPEGeneric : public PixelCPEBase
   bool TruncatePixelCharge_;
   bool IrradiationBiasCorrection_;
   bool isUpgrade_;
+  int Phase2BPixStart_;
+  int Phase2FPixStart_;
 
   double EdgeClusterErrorX_;
   double EdgeClusterErrorY_;
