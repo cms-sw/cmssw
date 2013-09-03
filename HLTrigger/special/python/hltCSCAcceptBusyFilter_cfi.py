deffbf73f4f0dbab70837817792ec7367d4d1668
Andrea <perrotta@cern.ch>
2013-09-03 14:46:40 +0200

    Fixes for fillDescriptions and removal of unneded configs

--- HLTrigger/special/python/hltCSCAcceptBusyFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltCSCAcceptBusyFilter = cms.EDFilter("HLTCSCAcceptBusyFilter",
-       cscrechitsTag        = cms.InputTag( "hltCsc2DRecHits" ),
-       invert               = cms.bool(True),
-       maxRecHitsPerChamber = cms.uint32(200),
-       saveTags = cms.bool( False )
-)
