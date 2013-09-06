b11c13a4356c04969c9a8e6820f0fff4b3d77917
inugent <inugent@uvic.ca>
2013-09-05 16:13:47 -1000

    Update DrellYanValidation.cc

--- Validation/EventGenerator/plugins/DrellYanValidation.cc
+++ Validation/EventGenerator/plugins/DrellYanValidation.cc
@@ -2,8 +2,8 @@
  *  
  *  Class to fill dqm monitor elements from existing EDM file
  *
- *  $Date: 2011/12/29 10:53:11 $
- *  $Revision: 1.5 $
+ *  $Date: 2011/01/24 17:41:34 $
+ *  $Revision: 1.3 $
  */
  
 #include "Validation/EventGenerator/interface/DrellYanValidation.h"
