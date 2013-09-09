4aedb30008de56e36bf8c3173bc4aef033a17b56
Alexander Gude <gude@physics.umn.edu>
2013-09-09 14:29:05 -0500

    Fix inheritance problem from std::exception
    std::exception defines `virtual ~Exception() throw()`, whereas the
    hcal::exception::Exception was trying to overwrite this with `virtual
    ~Exception {}`. This lead to a "looser throw specifier" error. As the
    derived class has no reason to redefine this method, it had been
    removed.

--- CaloOnlineTools/HcalOnlineDb/interface/Exception.hh
+++ CaloOnlineTools/HcalOnlineDb/interface/Exception.hh
@@ -32,7 +32,6 @@ namespace hcal {
         Exception( const std::string& message ): message_(message) { }
 
         virtual const char* what () const throw() { return message_.c_str(); }
-        virtual ~Exception() { }
 
       private:
         std::string message_;
