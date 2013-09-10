68f5faa91f8641acff871c7a4e37673f0a434de4
Tim Cox <Timothy.Cox@cern.ch>
2013-09-10 17:17:32 +0200

    add consumes interface

--- EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h
+++ EventFilter/CSCRawToDigi/src/CSCDigiToPattern.h
@@ -6,13 +6,14 @@
  */
 
 #include <iostream>
+#include <FWCore/Framework/interface/ConsumesCollector.h>
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-class CSCCorrelatedLCTDigi;
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
 
 class CSCDigiToPattern : public edm::EDAnalyzer {
 public:
@@ -22,6 +23,7 @@ public:
   //virtual void endJob();
 private:
   // variables persistent across events should be declared here.
+  edm::EDGetTokenT<CSCCorrelatedLCTDigiCollection> d_token;
   //
 };
 
