bdbbd71684df9893260a68d06f98821945f7a46f
Tim Cox <Timothy.Cox@cern.ch>
2013-09-10 17:16:15 +0200

    add consumes interface

--- EventFilter/CSCRawToDigi/plugins/CSCDigiToRawModule.cc
+++ EventFilter/CSCRawToDigi/plugins/CSCDigiToRawModule.cc
@@ -1,43 +1,33 @@
 /** \file
- *
- *  $Date: 2010/04/23 23:03:03 $
- *  $Revision: 1.11 $
  *  \author A. Tumanov - Rice
  */
 
 #include "EventFilter/CSCRawToDigi/src/CSCDigiToRawModule.h"
 #include "EventFilter/CSCRawToDigi/src/CSCDigiToRaw.h"
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCStripDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCWireDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCComparatorDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCALCTDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCCLCTDigiCollection.h"
-#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "CondFormats/DataRecord/interface/CSCChamberMapRcd.h"
 
-
-
-using namespace edm;
-using namespace std;
-
 CSCDigiToRawModule::CSCDigiToRawModule(const edm::ParameterSet & pset): 
-  packer(new CSCDigiToRaw(pset)),
-  theStripDigiTag(pset.getParameter<edm::InputTag>("stripDigiTag")),
-  theWireDigiTag(pset.getParameter<edm::InputTag>("wireDigiTag")),
-  theComparatorDigiTag(pset.getParameter<edm::InputTag>("comparatorDigiTag")),
-  theALCTDigiTag(pset.getParameter<edm::InputTag>("alctDigiTag")),
-  theCLCTDigiTag(pset.getParameter<edm::InputTag>("clctDigiTag")),
-  thePreTriggerTag(pset.getParameter<edm::InputTag>("preTriggerTag")),
-  theCorrelatedLCTDigiTag(pset.getParameter<edm::InputTag>("correlatedLCTDigiTag"))
+  packer(new CSCDigiToRaw(pset))
 {
   //theStrip = pset.getUntrackedParameter<string>("DigiCreator", "cscunpacker");
+
+  wd_token = consumes<CSCWireDigiCollection>( pset.getParameter<edm::InputTag>("wireDigiTag") );
+  sd_token = consumes<CSCStripDigiCollection>( pset.getParameter<edm::InputTag>("stripDigiTag") );
+  cd_token = consumes<CSCComparatorDigiCollection>( pset.getParameter<edm::InputTag>("comparatorDigiTag") );
+  pr_token = consumes<CSCCLCTPreTriggerCollection>( pset.getParameter<edm::InputTag>("preTriggerTag") );
+  al_token = consumes<CSCALCTDigiCollection>( pset.getParameter<edm::InputTag>("alctDigiTag") );
+  cl_token = consumes<CSCCLCTDigiCollection>( pset.getParameter<edm::InputTag>("clctDigiTag") );
+  co_token = consumes<CSCCorrelatedLCTDigiCollection>( pset.getParameter<edm::InputTag>("correlatedLCTDigiTag") );
+
   produces<FEDRawDataCollection>("CSCRawData"); 
+
 }
 
 
@@ -46,31 +36,30 @@ CSCDigiToRawModule::~CSCDigiToRawModule(){
 }
 
 
-void CSCDigiToRawModule::produce(Event & e, const EventSetup& c){
+void CSCDigiToRawModule::produce( edm::Event & e, const edm::EventSetup& c ){
   ///reverse mapping for packer
   edm::ESHandle<CSCChamberMap> hcham;
   c.get<CSCChamberMapRcd>().get(hcham); 
   const CSCChamberMap* theMapping = hcham.product();
 
+  std::auto_ptr<FEDRawDataCollection> fed_buffers(new FEDRawDataCollection);
 
-  auto_ptr<FEDRawDataCollection> fed_buffers(new FEDRawDataCollection);
   // Take digis from the event
-  Handle<CSCStripDigiCollection> stripDigis;
-  e.getByLabel(theStripDigiTag, stripDigis);
-  Handle<CSCWireDigiCollection> wireDigis;
-  e.getByLabel(theWireDigiTag, wireDigis);
-  Handle<CSCComparatorDigiCollection> comparatorDigis;
-  e.getByLabel(theComparatorDigiTag, comparatorDigis);
-  Handle<CSCALCTDigiCollection> alctDigis;
-  e.getByLabel(theALCTDigiTag, alctDigis);
-  Handle<CSCCLCTDigiCollection> clctDigis;
-  e.getByLabel(theCLCTDigiTag, clctDigis);
-  Handle<CSCCLCTPreTriggerCollection> preTriggers;
-  e.getByLabel(thePreTriggerTag, preTriggers);
-  Handle<CSCCorrelatedLCTDigiCollection> correlatedLCTDigis;
-  e.getByLabel(theCorrelatedLCTDigiTag, correlatedLCTDigis);
+  edm::Handle<CSCWireDigiCollection> wireDigis;
+  edm::Handle<CSCStripDigiCollection> stripDigis;
+  edm::Handle<CSCComparatorDigiCollection> comparatorDigis;
+  edm::Handle<CSCALCTDigiCollection> alctDigis;
+  edm::Handle<CSCCLCTDigiCollection> clctDigis;
+  edm::Handle<CSCCLCTPreTriggerCollection> preTriggers;
+  edm::Handle<CSCCorrelatedLCTDigiCollection> correlatedLCTDigis;
 
-
+  e.getByToken( wd_token, wireDigis );
+  e.getByToken( sd_token, stripDigis );
+  e.getByToken( cd_token, comparatorDigis );
+  e.getByToken( al_token, alctDigis );
+  e.getByToken( cl_token, clctDigis );
+  e.getByToken( pr_token, preTriggers );
+  e.getByToken( co_token, correlatedLCTDigis );
 
   // Create the packed data
   packer->createFedBuffers(*stripDigis, *wireDigis, *comparatorDigis, 
