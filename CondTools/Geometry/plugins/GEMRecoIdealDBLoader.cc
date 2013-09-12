f83b83c3443f83971ea89b1ecf103c1a3006d62a
Ianna Osborne <ianna.osborne@cern.ch>
2013-09-12 11:26:23 +0200

    Clean up

--- CondTools/Geometry/plugins/GEMRecoIdealDBLoader.cc
+++ CondTools/Geometry/plugins/GEMRecoIdealDBLoader.cc
@@ -1,60 +1,75 @@
-#include "GEMRecoIdealDBLoader.h"
-
-#include "Geometry/GEMGeometryBuilder/src/GEMGeometryParsFromDD.h"
-
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/ESTransientHandle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
 #include "CondFormats/GeometryObjects/interface/RecoIdealGeometry.h"
-#include "Geometry/Records/interface/GEMRecoGeometryRcd.h"
-#include "Geometry/Records/interface/MuonGeometryRecord.h"
-
 #include "DetectorDescription/Core/interface/DDCompactView.h"
-
-#include "Geometry/Records/interface/MuonNumberingRecord.h"
+#include "Geometry/GEMGeometryBuilder/src/GEMGeometryParsFromDD.h"
 #include "Geometry/MuonNumbering/interface/MuonDDDConstants.h"
+#include "Geometry/Records/interface/GEMRecoGeometryRcd.h"
+#include "Geometry/Records/interface/MuonNumberingRecord.h"
 
 #include <iostream>
 
 using namespace std;
 
-GEMRecoIdealDBLoader::GEMRecoIdealDBLoader(const edm::ParameterSet& iConfig) : label_()
+class GEMRecoIdealDBLoader : public edm::EDAnalyzer
 {
-  std::cout<<"GEMRecoIdealDBLoader::GEMRecoIdealDBLoader"<<std::endl;
+public:
+  
+  explicit GEMRecoIdealDBLoader( const edm::ParameterSet& );
+  ~GEMRecoIdealDBLoader( void );
+  
+  virtual void beginRun( const edm::Run&, edm::EventSetup const& );
+  virtual void analyze( const edm::Event&, const edm::EventSetup& ) {}
+  virtual void endJob( void ) {};
+};
+
+GEMRecoIdealDBLoader::GEMRecoIdealDBLoader( const edm::ParameterSet& )
+{
+  std::cout << "GEMRecoIdealDBLoader::GEMRecoIdealDBLoader" << std::endl;
 }
 
 GEMRecoIdealDBLoader::~GEMRecoIdealDBLoader()
 {
-  std::cout<<"GEMRecoIdealDBLoader::~GEMRecoIdealDBLoader"<<std::endl;
+  std::cout << "GEMRecoIdealDBLoader::~GEMRecoIdealDBLoader" << std::endl;
 }
 
 void
-GEMRecoIdealDBLoader::beginRun( const edm::Run&, edm::EventSetup const& es) 
+GEMRecoIdealDBLoader::beginRun( const edm::Run&, edm::EventSetup const& es ) 
 {
-  RecoIdealGeometry* rig = new RecoIdealGeometry;
   edm::Service<cond::service::PoolDBOutputService> mydbservice;
-  if( !mydbservice.isAvailable() ){
-    edm::LogError("GEMRecoIdealDBLoader")<<"PoolDBOutputService unavailable";
+  if( !mydbservice.isAvailable())
+  {
+    edm::LogError( "GEMRecoIdealDBLoader" ) << "PoolDBOutputService unavailable";
     return;
   }
 
-  edm::ESTransientHandle<DDCompactView> pDD;
-  edm::ESHandle<MuonDDDConstants> pMNDC;
-  es.get<IdealGeometryRecord>().get(label_, pDD );
-  es.get<MuonNumberingRecord>().get( pMNDC );
+  if( mydbservice->isNewTagRequest( "GEMRecoGeometryRcd" ))
+  {
+    edm::ESTransientHandle<DDCompactView> pDD;
+    edm::ESHandle<MuonDDDConstants> pMNDC;
+    es.get<IdealGeometryRecord>().get( pDD );
+    es.get<MuonNumberingRecord>().get( pMNDC );
 
-  const DDCompactView& cpv = *pDD;
-  GEMGeometryParsFromDD rpcpd;
+    const DDCompactView& cpv = *pDD;
+    GEMGeometryParsFromDD rpcpd;
 
-  rpcpd.build( &cpv, *pMNDC, *rig );
+    RecoIdealGeometry* rig = new RecoIdealGeometry;
+    rpcpd.build( &cpv, *pMNDC, *rig );
 
-  if ( mydbservice->isNewTagRequest("GEMRecoGeometryRcd") ) {
-    mydbservice->createNewIOV<RecoIdealGeometry>(rig
-                                                 , mydbservice->beginOfTime()
-                                                 , mydbservice->endOfTime()
-                                                 , "GEMRecoGeometryRcd");
-  } else {
-    edm::LogError("GEMRecoIdealDBLoader")<<"GEMRecoGeometryRcd Tag is already present.";
+    mydbservice->createNewIOV<RecoIdealGeometry>( rig,
+						  mydbservice->beginOfTime(),                                                 
+						  mydbservice->endOfTime(),
+						  "GEMRecoGeometryRcd" );
+  }
+  else
+  {
+    edm::LogError( "GEMRecoIdealDBLoader" ) << "GEMRecoGeometryRcd Tag is already present";
   }
 }
+
+DEFINE_FWK_MODULE( GEMRecoIdealDBLoader );
