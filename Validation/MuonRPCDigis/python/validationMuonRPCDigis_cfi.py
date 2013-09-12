0bfa49b1afbe30a4514c2f3687b1146e3544ca59
mileva <roumyana.mileva.hadjiiska@cern.ch>
2013-09-12 02:11:38 +0200

    fixed config

--- Validation/MuonRPCDigis/python/validationMuonRPCDigis_cfi.py
+++ Validation/MuonRPCDigis/python/validationMuonRPCDigis_cfi.py
@@ -7,7 +7,7 @@ validationMuonRPCDigis = cms.EDAnalyzer("RPCDigiValid",
     # Tag for Digis event data retrieval
     rpcDigiTag = cms.untracked.InputTag("simMuonRPCDigis"),
     # Tag for simulated hits event data retrieval
-    simHitTag = cms.untracked.InputTag("4SimHits", "MuonRPCHits"),
+    simHitTag = cms.untracked.InputTag("g4SimHits", "MuonRPCHits"),
 
     # Name of the root file which will contain the histos
     outputFile = cms.untracked.string('output.root')
