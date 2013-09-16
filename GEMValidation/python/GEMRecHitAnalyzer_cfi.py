f904ca5f8d094fe5cc81823f60d3f1341ea372c7
Cesare <cesare.calabria@ba.infn.it>
2013-09-16 12:05:47 +0200

    several plots added

--- GEMValidation/python/GEMRecHitAnalyzer_cfi.py
+++ GEMValidation/python/GEMRecHitAnalyzer_cfi.py
@@ -1,6 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
+
 GEMRecHitAnalyzer = cms.EDAnalyzer("GEMRecHitAnalyzer",
-   verbose = cms.untracked.int32(0),
-##    simInputLabel = cms.untracked.string("g4SimHits")
+   verbose = cms.untracked.int32(5),
+    inputTagGEM = cms.untracked.InputTag("simMuonGEMDigis"),
+    simInputLabel = cms.untracked.string("g4SimHits"),
+    minPt = cms.untracked.double(5.),
+    simTrackMatching = SimTrackMatching
 )
