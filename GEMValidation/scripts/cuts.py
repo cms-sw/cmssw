28f437947a1bc799e654cd6ca0b0c88345537c35
Cesare <cesare.calabria@ba.infn.it>
2013-09-16 12:08:20 +0200

    recHit cuts added

--- GEMValidation/scripts/cuts.py
+++ GEMValidation/scripts/cuts.py
@@ -15,12 +15,11 @@ ok_gL1dg = TCut("gem_dg_layer1 > 0")
 ok_gL2dg = TCut("gem_dg_layer2 > 0")
 ok_gL1pad = TCut("gem_pad_layer1 > 0")
 ok_gL2pad = TCut("gem_pad_layer2 > 0")
+ok_gL1rh = TCut("gem_rh_layer1 > 0")
+ok_gL2rh = TCut("gem_rh_layer2 > 0")
 
 ok_trk_gL1sh = TCut("has_gem_sh_l1 > 0")
 ok_trk_gL2sh = TCut("has_gem_sh_l2 > 0")
 
 ok_lx_odd =  TCut("TMath::Abs(TMath::ASin(gem_lx_odd/gem_trk_rho)) < 5*TMath::Pi()/180.")
 ok_lx_even = TCut("TMath::Abs(TMath::ASin(gem_lx_even/gem_trk_rho)) < 5*TMath::Pi()/180.")
-
-
-    
