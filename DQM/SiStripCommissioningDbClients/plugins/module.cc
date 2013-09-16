9e555bcfb3fb293e72cb1609c60b793a8d8fb578
Dan Duggan <duggan@cern.ch>
2013-09-16 15:20:05 +0200

    adding back DbClients now that tkonlinesw is back in

---
+++ DQM/SiStripCommissioningDbClients/plugins/module.cc
@@ -0,0 +1,6 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+
+#include "DQM/SiStripCommissioningDbClients/plugins/SiStripCommissioningOfflineDbClient.h"
+DEFINE_FWK_MODULE(SiStripCommissioningOfflineDbClient);
+
