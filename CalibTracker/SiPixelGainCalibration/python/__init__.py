efce68982d68b4faa94b7293f98f2d159a1ccbbf
jluetic <jelena.luetic@cern.ch>
2013-09-18 19:58:59 +0200

    Error fix

--- CalibTracker/SiPixelGainCalibration/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/CalibTracker/SiPixelGainCalibration/',1)[0])+'/cfipython/slc6_amd64_gcc472/CalibTracker/SiPixelGainCalibration')
--- CalibTracker/SiPixelTools/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/CalibTracker/SiPixelTools/',1)[0])+'/cfipython/slc6_amd64_gcc472/CalibTracker/SiPixelTools')
