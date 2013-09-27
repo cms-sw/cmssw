75d4ac1152ab75d91971b24252b6e2565f847128
Michael Mulhearn <mulhearn@cern.ch>
2013-09-27 20:33:40 +0200

    all data objects compile

---
+++ CondFormats/DataRecord/src/L1TYellowParamsRcd.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/DataRecord/interface/L1TYellowParamsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(L1TYellowParamsRcd);
---
+++ CondFormats/L1TYellow/src/classes.h
@@ -0,0 +1,9 @@
+#include "CondFormats/L1TYellow/interface/L1TYellowParams.h"
+//#include "CondFormats/DataRecord/L1TYellowParamsRcd.h"
+
+namespace {
+  struct dictionary {
+    //L1TYellowParams dummy;
+    //L1TYellowParamsRcd dummyb;
+  };
+}
