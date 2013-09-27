9f2e81dd6baac28f686e30d29132c0669adc71e2
Michael Mulhearn <mulhearn@cern.ch>
2013-09-27 20:35:09 +0200

    modified missed in previous commit

--- DataFormats/L1TYellow/src/classes.h
+++ DataFormats/L1TYellow/src/classes.h
@@ -1,11 +1,14 @@
 
 #include <vector>
+#include "DataFormats/L1TYellow/interface/L1TYellowDigi.h"
 #include "DataFormats/L1TYellow/interface/L1TYellowOutput.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   struct dictionary {
-    L1TYellowOutputCollection dummy;
-    edm::Wrapper<L1TYellowOutputCollection> w_dummy;
+    L1TYellowDigiCollection dummya;
+    edm::Wrapper<L1TYellowDigiCollection> w_dummya;
+    L1TYellowOutputCollection dummyb;
+    edm::Wrapper<L1TYellowOutputCollection> w_dummyb;
   };
 }
--- L1Trigger/L1TYellow/src/L1TYellowConfigESProducer.cc
+++ L1Trigger/L1TYellow/src/L1TYellowConfigESProducer.cc
@@ -1,13 +1,13 @@
 // -*- C++ -*-
 //
 // Package:    L1Trigger/L1TYellow
-// Class:      L1TYellowConfigESProducer
+// Class:      L1TYellowParamsESProducer
 // 
-/**\class L1TYellowConfigESProducer L1TYellowConfigESProducer.cc L1Trigger/L1TYellow/src/L1TYellowConfigESProducer.cc
+/**\class L1TYellowParamsESProducer L1TYellowParamsESProducer.cc L1Trigger/L1TYellow/src/L1TYellowParamsESProducer.cc
 
  Description:  This is part of the fictitious Yellow trigger emulation for demonstration purposes.
 
- This uses the parameters set in a config file to fill the ConfData/L1TYellow/L1TYellowConfig object.  
+ This uses the parameters set in a config file to fill the ConfData/L1TYellow/L1TYellowParams object.  
  Other modules can retreive this object in same manner whether it was filled from config file or 
  the conditions database.
 
@@ -18,124 +18,51 @@
 
 // system include files
 #include <memory>
+#include "boost/shared_ptr.hpp"
 
 // user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
 
 #include "CondFormats/L1TYellow/interface/L1TYellowParams.h"
+#include "CondFormats/DataRecord/interface/L1TYellowParamsRcd.h"
 
-using namespace std;
-//using namespace l1t;
+// forward declarations
 
-//
-// class declaration
-//
-
-class L1TYellowConfigESProducer : public edm::ESProducer {
+class L1TYellowParamsESProducer : public edm::ESProducer {
 public:
-  explicit L1TYellowConfigESProducer(const edm::ParameterSet&);
-  ~L1TYellowConfigESProducer();
-  
-private:
-  std::auto_ptr<L1TYellowParams> produce(const FooRecord&);
+  L1TYellowParamsESProducer(const edm::ParameterSet&);
+  ~L1TYellowParamsESProducer();
 
-  
+  typedef boost::shared_ptr<L1TYellowParams> ReturnType;
+
+  ReturnType produce(const L1TYellowParamsRcd&);
+private:
+  // ----------member data ---------------------------
+  L1TYellowParams  m_params ;
 };
 
 
-//using namespace l1t;
-
-//
-// constants, enums and typedefs
-//
-
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
-L1TYellowConfigESProducer::L1TYellowConfigESProducer(const edm::ParameterSet& iConfig)
+L1TYellowParamsESProducer::L1TYellowParamsESProducer(const edm::ParameterSet& iConfig)
 {
-  //register your products
-  produces<L1TYellowDigiCollection>();
+  setWhatProduced(this, "L1TYellowParamsESProducer");
 }
-
-
-L1TYellowConfigESProducer::~L1TYellowConfigESProducer()
+L1TYellowParamsESProducer::~L1TYellowParamsESProducer()
 {
 }
 
-
-//
-// member functions
-//
-
-// ------------ method called to produce the data  ------------
-void
-L1TYellowConfigESProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+L1TYellowParamsESProducer::ReturnType
+L1TYellowParamsESProducer::produce(const L1TYellowParamsRcd& iRecord)
 {
-  cout << "L1TYellowConfigESProducer::produce function called...\n";
+  using namespace edm::es;
+  boost::shared_ptr<L1TYellowParams> pL1TYellowParams ;
 
-  std::auto_ptr<L1TYellowDigiCollection> outColl (new L1TYellowDigiCollection);
-  L1TYellowDigi iout;
-
-  iout.setRawData(20);
-  outColl->push_back(iout);
-
-  iEvent.put(outColl); 
+  pL1TYellowParams = boost::shared_ptr< L1TYellowParams >(new L1TYellowParams( m_params ));
+  return pL1TYellowParams;
 }
 
-// ------------ method called once each job just before starting event loop  ------------
-void 
-L1TYellowConfigESProducer::beginJob()
-{
-}
+DEFINE_FWK_EVENTSETUP_MODULE(L1TYellowParamsESProducer);
 
-// ------------ method called once each job just after ending the event loop  ------------
-void 
-L1TYellowConfigESProducer::endJob() {
-}
 
-// ------------ method called when starting to processes a run  ------------
-void 
-L1TYellowConfigESProducer::beginRun(edm::Run&, edm::EventSetup const&)
-{
-
-}
-
-// ------------ method called when ending the processing of a run  ------------
-void 
-L1TYellowConfigESProducer::endRun(edm::Run&, edm::EventSetup const&)
-{
-}
-
-// ------------ method called when starting to processes a luminosity block  ------------
-void 
-L1TYellowConfigESProducer::beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
-{
-}
-
-// ------------ method called when ending the processing of a luminosity block  ------------
-void 
-L1TYellowConfigESProducer::endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
-{
-}
-
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void
-L1TYellowConfigESProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
-}
-
-//define this as a plug-in
-DEFINE_FWK_MODULE(L1TYellowConfigESProducer);
