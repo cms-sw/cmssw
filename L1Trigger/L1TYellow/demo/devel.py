0926d67c6aa77870068d414160fc6c18aa237c68
Michael Mulhearn <mulhearn@cern.ch>
2013-09-30 13:15:01 +0200

    put demo into repo

---
+++ L1Trigger/L1TYellow/demo/devel.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('L1TEMULATION')
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('L1Trigger/L1TYellow/upgrade_emulation_cfi')
+process.load('L1Trigger/L1TYellow/l1tyellow_params_cfi')
+
+process.prefer("yellowParams")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('file:test.root')
+)
+
+process.options = cms.untracked.PSet(
+)
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS1', '')
+
+process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
+process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
+
+process.p1 = cms.Path(
+#    process.digiStep*
+#    process.dumpED*
+#    process.dumpES
+    )
+
+process.schedule = cms.Schedule(
+    process.p1
+    )
+
+# Spit out filter efficiency at the end.
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
