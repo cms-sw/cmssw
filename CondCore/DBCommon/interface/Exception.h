c4cf24ec035caf37ca023b9c49cd66714de11653
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-10-01 14:55:19 +0200

    Fix misc static analyzer issues.

--- CondCore/DBCommon/interface/Exception.h
+++ CondCore/DBCommon/interface/Exception.h
@@ -31,6 +31,6 @@ namespace cond{
     virtual ~TransactionException() throw(){}
   };
   
-  void throwException( std::string const& message, std::string const& methodName );
+  void throwException( std::string const& message, std::string const& methodName )__attribute__ ((noreturn));
 }
 #endif
