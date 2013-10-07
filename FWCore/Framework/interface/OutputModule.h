4828ba433996bff7bc4a52692eba7195b59373ac
Chris Jones <chrisdjones15@gmail.com>
2013-10-07 15:28:44 -0500

    Made legacy modules thread safe
    Now uses a std::mutex to guarantee a module instance is only processing one event at a time. Also uses a edm::SharedResourcesAcquirer to get the legacy resource and therefore guarantee only one legacy module is running at a time.

--- FWCore/Framework/interface/OutputModule.h
+++ FWCore/Framework/interface/OutputModule.h
@@ -21,6 +21,8 @@ output stream.
 #include "FWCore/Framework/interface/ProductSelector.h"
 #include "FWCore/Framework/interface/EDConsumerBase.h"
 #include "FWCore/Framework/interface/getAllTriggerNames.h"
+#include "FWCore/Framework/interface/SharedResourcesAcquirer.h"
+
 #include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
 
 #include <array>
@@ -28,6 +30,7 @@ output stream.
 #include <vector>
 #include <map>
 #include <atomic>
+#include <mutex>
 
 namespace edm {
 
@@ -161,6 +164,9 @@ namespace edm {
     BranchParents branchParents_;
 
     BranchChildren branchChildren_;
+
+    SharedResourcesAcquirer resourceAcquirer_;
+    std::mutex mutex_;
 
     //------------------------------------------------------------------
     // private member functions
