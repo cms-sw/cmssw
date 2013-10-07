26ba0daea267f5e49d0c228870a1ef8d243aad96
Chris Jones <chrisdjones15@gmail.com>
2013-10-07 15:28:44 -0500

    Made legacy modules thread safe
    Now uses a std::mutex to guarantee a module instance is only processing one event at a time. Also uses a edm::SharedResourcesAcquirer to get the legacy resource and therefore guarantee only one legacy module is running at a time.

--- FWCore/Framework/src/OutputModule.cc
+++ FWCore/Framework/src/OutputModule.cc
@@ -19,6 +19,8 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/DebugMacros.h"
 
+#include "SharedResourcesRegistry.h"
+
 #include <cassert>
 #include <iostream>
 
@@ -58,6 +60,9 @@ namespace edm {
                                                     process_name_,
                                                     getAllTriggerNames(),
                                                     selectors_);
+    SharedResourcesRegistry::instance()->registerSharedResource(
+                                                                SharedResourcesRegistry::kLegacyModuleResourceName);
+
   }
 
   void OutputModule::configure(OutputModuleDescription const& desc) {
@@ -155,6 +160,9 @@ namespace edm {
   OutputModule::~OutputModule() { }
 
   void OutputModule::doBeginJob() {
+    std::vector<std::string> res = {SharedResourcesRegistry::kLegacyModuleResourceName};
+    resourceAcquirer_ = SharedResourcesRegistry::instance()->createAcquirer(res);
+
     this->beginJob();
   }
 
@@ -177,13 +185,20 @@ namespace edm {
 
     FDEBUG(2) << "writeEvent called\n";
 
-    if(!wantAllEvents_) {
-      if(!selectors_.wantEvent(ep, mcc)) {
-        return true;
+    {
+      std::lock_guard<std::mutex> guard(mutex_);
+      if(!wantAllEvents_) {
+        if(!selectors_.wantEvent(ep, mcc)) {
+          return true;
+        }
       }
+      
+      {
+        std::lock_guard<SharedResourcesAcquirer> guardAcq(resourceAcquirer_);
+        write(ep, mcc);
+      }
+      updateBranchParents(ep);
     }
-    write(ep, mcc);
-    updateBranchParents(ep);
     if(remainingEvents_ > 0) {
       --remainingEvents_;
     }
