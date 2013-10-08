2eae30ecc60cd08acaa42042cb95d0b9387c4957
Lindsey Gray <lindsey.gray@gmail.com>
2013-10-08 15:53:47 +0200

    validated

--- GeneratorInterface/LHEInterface/test/testReader_cfg.py
+++ GeneratorInterface/LHEInterface/test/testReader_cfg.py
@@ -7,7 +7,7 @@ process.source = cms.Source("LHESource",
 	fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/l/lgray/work/public/CMSSW_7_0_0_pre2/src/2.0.0beta4/VBF_HAA_reweight/Events/VBF_HAA_reweight4/unweighted_events.lhe')
 )
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.configurationMetadata = cms.untracked.PSet(
 	version = cms.untracked.string('alpha'),
@@ -23,5 +23,10 @@ process.LHE = cms.OutputModule("PoolOutputModule",
 	dataset = cms.untracked.PSet(dataTier = cms.untracked.string('LHE')),
 	fileName = cms.untracked.string('lhe.root')
 )
+
+#process.lhedump = cms.EDAnalyzer("DummyLHEAnalyzer",
+#                                 src = cms.InputTag("source")
+#                                 )
+
 
 process.outpath = cms.EndPath(process.LHE)
