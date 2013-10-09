fb6ee860647d74774f81139467ec806ae4b15183
Lindsey Gray <lindsey.gray@gmail.com>
2013-10-09 10:18:21 +0200

    remove spurious cout

--- RecoEgamma/EgammaPhotonProducers/src/GEDPhotonCoreProducer.cc
+++ RecoEgamma/EgammaPhotonProducers/src/GEDPhotonCoreProducer.cc
@@ -119,7 +119,7 @@ void GEDPhotonCoreProducer::produce(edm::Event &theEvent, const edm::EventSetup&
   SingleLeg_p->assign(outputOneLegConversionCollection.begin(),outputOneLegConversionCollection.end()); 
   const edm::OrphanHandle<reco::ConversionCollection> singleLegConvOrhpHandle = theEvent.put(SingleLeg_p,PFConversionCollection_);
 
-  std::cout <<  "  GEDPhotonCoreProducer::produce orphanHandle to single legs " <<  singleLegConvOrhpHandle->size() << std::endl;
+  //std::cout <<  "  GEDPhotonCoreProducer::produce orphanHandle to single legs " <<  singleLegConvOrhpHandle->size() << std::endl;
   //std::cout <<  "  GEDPhotonCoreProducer::produce photon size " <<  outputPhotonCoreCollection.size() << std::endl;
 
   
