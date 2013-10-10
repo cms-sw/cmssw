88f0e2af7ba1098b5adc71d9b9ba54f4e60a2349
Martin Grunewald <Martin.Grunewald@cern.ch>
2013-10-10 14:15:11 +0200

    MeasurementTrackerAndHLT

--- RecoTracker/MeasurementDet/python/MaskedMeasurementTrackerEventProducer_cfi.py
+++ RecoTracker/MeasurementDet/python/MaskedMeasurementTrackerEventProducer_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 MaskedMeasurementTrackerEvent = cms.EDProducer("MaskedMeasurementTrackerEventProducer",
     src = cms.InputTag("MeasurementTrackerEvent"),
     OnDemand = cms.bool(False),
-    skipClusters = cms.InputTag(""),
+    clustersToSkip = cms.InputTag(""),
 )
