e4b50f5da4f23c8417ee4ca737ba3cd0074e18e0
Mia <mia.tosi@cern.ch>
2013-10-11 18:18:42 +0200

    synch TkMap script in official CMSSW_53x

---
+++ DQM/SiStripMonitorClient/scripts/getGTfromDQMFile.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from math import *
+from ROOT import TFile, TObject, TTree
+from array import array
+from ROOT import gDirectory
+import sys
+import os
+
+DQMfile = sys.argv[1]
+RunNumber = sys.argv[2]
+globalTagVar = sys.argv[3]
+
+if not os.path.isfile(DQMfile):
+#    print "Error: file", DQMfile, "not found, exit" 
+    sys.exit(0)
+
+
+
+thefile = TFile( DQMfile )
+globalTagDir = 'DQMData/Run ' + RunNumber + '/Info/Run summary/CMSSWInfo'
+
+if not gDirectory.GetDirectory( globalTagDir ):
+#    print "Warning: globalTag not found in DQM file"
+    sys.exit(0)    
+
+keys = gDirectory.GetDirectory( globalTagDir ).GetListOfKeys()
+
+key = keys[0]
+globalTag = ''
+while key:
+    obj = key.ReadObj()
+    if globalTagVar in obj.GetName():
+        globalTag = obj.GetName()[len("<"+globalTagVar+">s="):-len("</"+globalTagVar+">")]
+        break
+    key = keys.After(key)
+
+if len(globalTag) > 1:
+    print globalTag
