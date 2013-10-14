b79e0f7d2148b479e9202c748e018f48670a5faf
Laura <Laura.Borrello@cern.ch>
2013-10-14 18:20:15 +0200

    Add plots for PFlowDQM

--- Validation/Configuration/python/globalValidation_cff.py
+++ Validation/Configuration/python/globalValidation_cff.py
@@ -31,6 +31,8 @@ from Validation.RecoEgamma.egammaValidation_cff import *
 from Validation.RecoParticleFlow.PFJetValidation_cff  import *
 from Validation.RecoParticleFlow.PFMETValidation_cff import *
 from Validation.RecoParticleFlow.PFMuonValidation_cff import *
+from Validation.RecoParticleFlow.PFElectronValidation_cff import *
+from Validation.RecoParticleFlow.PFJetResValidation_cff import *
 from Validation.RPCRecHits.rpcRecHitValidation_cfi import *
 from Validation.DTRecHits.DTRecHitQuality_cfi import *
 from Validation.RecoTau.DQMMCValidation_cfi import *
@@ -78,6 +80,8 @@ globalValidation = cms.Sequence(   trackerHitsValidation
                                  + egammaValidation
                                  + pfJetValidationSequence
                                  + pfMETValidationSequence
+                                 + pfElectronValidationSequence
+                                 + pfJetResValidationSequence
                                  + pfMuonValidationSequence
                                  + rpcRecHitValidation_step
 				 + dtLocalRecoValidation_no2D
