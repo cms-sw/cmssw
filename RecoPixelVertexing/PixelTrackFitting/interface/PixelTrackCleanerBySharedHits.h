1e93f3ecc752b4530611478e83e8146c5505943e
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2013-10-17 16:50:19 +0200

    remove local variables and functions from class

--- RecoPixelVertexing/PixelTrackFitting/interface/PixelTrackCleanerBySharedHits.h
+++ RecoPixelVertexing/PixelTrackFitting/interface/PixelTrackCleanerBySharedHits.h
@@ -24,12 +24,6 @@ public:
   typedef pixeltrackfitting::TracksWithRecHits TracksWithRecHits;
   virtual TracksWithRecHits cleanTracks(const TracksWithRecHits & tracksWithRecHits, const TrackerTopology *tTopo);
 
-private:
-
-  void cleanTrack();
-  bool recHitsAreEqual(const TrackingRecHit *recHit1, const TrackingRecHit *recHit2);
-
-  std::vector<bool> trackOk;
 
 };
 
