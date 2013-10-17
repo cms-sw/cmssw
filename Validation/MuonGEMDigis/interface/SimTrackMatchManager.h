55b06c36394d6e862b7074366c501669aace6b46
Geonmo <ry840901@gmail.com>
2013-10-18 00:52:02 +0900

    Remove dummy and legacy code about CSC.

--- Validation/MuonGEMDigis/interface/SimTrackMatchManager.h
+++ Validation/MuonGEMDigis/interface/SimTrackMatchManager.h
@@ -15,8 +15,6 @@
 #include "Validation/MuonGEMDigis/interface/BaseMatcher.h"
 #include "Validation/MuonGEMDigis/interface/SimHitMatcher.h"
 #include "Validation/MuonGEMDigis/interface/GEMDigiMatcher.h"
-#include "Validation/MuonGEMDigis/interface/CSCDigiMatcher.h"
-#include "Validation/MuonGEMDigis/interface/CSCStubMatcher.h"
 #include "Validation/MuonGEMDigis/interface/GEMRecHitMatcher.h"
 
 class SimTrackMatchManager
@@ -30,16 +28,12 @@ public:
 
   const SimHitMatcher& simhits() const {return simhits_;}
   const GEMDigiMatcher& gemDigis() const {return gem_digis_;}
-  const CSCDigiMatcher& cscDigis() const {return csc_digis_;}
-  const CSCStubMatcher& cscStubs() const {return stubs_;}
   const GEMRecHitMatcher& gemRecHits() const {return gem_rechits_;}
   
 private:
 
   SimHitMatcher simhits_;
   GEMDigiMatcher gem_digis_;
-  CSCDigiMatcher csc_digis_;
-  CSCStubMatcher stubs_;
   GEMRecHitMatcher gem_rechits_;
 };
 
--- Validation/MuonGEMDigis/src/SimTrackMatchManager.cc
+++ Validation/MuonGEMDigis/src/SimTrackMatchManager.cc
@@ -4,8 +4,6 @@ SimTrackMatchManager::SimTrackMatchManager(const SimTrack& t, const SimVertex& v
       const edm::ParameterSet& ps, const edm::Event& ev, const edm::EventSetup& es)
 : simhits_(t, v, ps, ev, es)
 , gem_digis_(simhits_)
-, csc_digis_(simhits_)
-, stubs_(simhits_, csc_digis_)
 , gem_rechits_(simhits_)
 {}
 
