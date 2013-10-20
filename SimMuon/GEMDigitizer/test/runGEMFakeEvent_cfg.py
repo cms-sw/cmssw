54a1413fa48eb2b74ffb3ab9a5c7451802e0915d
Sven Dildick <sven.dildick@cern.ch>
2013-10-20 21:53:08 +0200

    Align with 2019 configuration

--- SimMuon/GEMDigitizer/test/runGEMFakeEvent_cfg.py
+++ SimMuon/GEMDigitizer/test/runGEMFakeEvent_cfg.py
@@ -2,29 +2,26 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Fake")
 
+process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-
-process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMXML_cfi')
-process.load('Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi')
-process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
-process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
-process.load('Geometry.GEMGeometry.gemGeometry_cfi')
-process.load('Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometryDB_cff')
-process.load('Geometry.DTGeometryBuilder.idealForDigiDtGeometryDB_cff')
-process.load('Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff')
-
-
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#process.GlobalTag.globaltag = 'POSTLS161_V12::All'
-
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2019_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
-
 
 process.source = cms.Source("EmptySource")
 
 process.faker = cms.EDProducer("GEMFakeEvent")
 
-
-process.p    = cms.Path(process.faker)
+process.p = cms.Path(process.faker)
 
