4a1eb3993c6f2c685bc1c7e0dd169ace3c7427be
inugent <you@somedomain.com>
2013-10-21 22:46:07 +0200

    First compiling ExternalDecays

--- GeneratorInterface/ExternalDecays/interface/DecayRandomEngine.h
+++
@@ -1,7 +0,0 @@
-#ifndef gen_ExternalDecays_DecayRandomEngine_h
-#define gen_ExternalDecays_DecayRandomEngine_h
-
-#include "CLHEP/Random/RandomEngine.h"
-extern CLHEP::HepRandomEngine* decayRandomEngine;
-
-#endif
---
+++ GeneratorInterface/ExternalDecays/src/ExternalDecayDriver.cc
@@ -0,0 +1,162 @@
+#include "GeneratorInterface/ExternalDecays/interface/ExternalDecayDriver.h"
+
+#include "GeneratorInterface/EvtGenInterface/interface/EvtGenFactory.h"
+#include "GeneratorInterface/EvtGenInterface/interface/EvtGenInterfaceBase.h"
+#include "GeneratorInterface/TauolaInterface/interface/TauolaFactory.h"
+#include "GeneratorInterface/TauolaInterface/interface/TauolaInterfaceBase.h"
+#include "GeneratorInterface/PhotosInterface/interface/PhotosFactory.h"
+#include "GeneratorInterface/PhotosInterface/interface/PhotosInterfaceBase.h"
+
+#include "GeneratorInterface/ExternalDecays/interface/DecayRandomEngine.h"
+
+#include "HepMC/GenEvent.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/RandomNumberGenerator.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+using namespace gen;
+using namespace edm;
+
+CLHEP::HepRandomEngine* decayRandomEngine;
+
+ExternalDecayDriver::ExternalDecayDriver( const ParameterSet& pset )
+   : fIsInitialized(false),
+     fTauolaInterface(0),
+     fEvtGenInterface(0),
+     fPhotosInterface(0)
+{
+    
+    std::vector<std::string> extGenNames =
+       pset.getParameter< std::vector<std::string> >("parameterSets");
+
+    Service<RandomNumberGenerator> rng;
+    if(!rng.isAvailable()) {
+       throw cms::Exception("Configuration")
+       << "The RandomNumberProducer module requires the RandomNumberGeneratorService\n"
+          "which appears to be absent.  Please add that service to your configuration\n"
+          "or remove the modules that require it." << std::endl;
+    } 
+    decayRandomEngine = &rng->getEngine();   
+    
+    for (unsigned int ip=0; ip<extGenNames.size(); ++ip )
+    {
+      std::string curSet = extGenNames[ip];
+      if ( curSet == "EvtGen" )
+      {
+	fEvtGenInterface = (EvtGenInterfaceBase*)(EvtGenFactory::get()->create("EvtGenFactory", pset.getUntrackedParameter< ParameterSet >(curSet)));
+      }
+      else if ( curSet == "Tauola" )
+      {
+	fTauolaInterface = (TauolaInterfaceBase*)(EvtGenFactory::get()->create("TauolaFactory", pset.getUntrackedParameter< ParameterSet >(curSet)));
+	fTauolaInterface->SetDecayRandomEngine(decayRandomEngine);
+      }
+      else if ( curSet == "Photos" )
+      {
+	if ( !fPhotosInterface ){
+	  fPhotosInterface = (PhotosInterfaceBase*)(EvtGenFactory::get()->create("PhotosFactory", pset.getUntrackedParameter< ParameterSet >(curSet)));
+	  fPhotosInterface->SetDecayRandomEngine(decayRandomEngine);
+	}
+      }
+
+    }
+
+}
+
+ExternalDecayDriver::~ExternalDecayDriver()
+{
+   if ( fEvtGenInterface ) delete fEvtGenInterface;
+   if ( fTauolaInterface ) delete fTauolaInterface;
+   if ( fPhotosInterface ) delete fPhotosInterface;
+}
+
+HepMC::GenEvent* ExternalDecayDriver::decay( HepMC::GenEvent* evt )
+{
+   
+   if ( !fIsInitialized ) return evt;
+   
+   if ( fEvtGenInterface )
+   {  
+      evt = fEvtGenInterface->decay( evt ); 
+      if ( !evt ) return 0;
+   }
+
+   if ( fTauolaInterface ) 
+   {
+      evt = fTauolaInterface->decay( evt ); 
+      if ( !evt ) return 0;
+   }
+   
+
+   if ( fPhotosInterface )
+   {
+      evt = fPhotosInterface->apply( evt );
+      if ( !evt ) return 0;
+   }
+
+         
+   return evt;
+}
+
+void ExternalDecayDriver::init( const edm::EventSetup& es )
+{
+
+   if ( fIsInitialized ) return;
+   
+   if ( fTauolaInterface ) 
+   {
+      fTauolaInterface->init( es );
+      for ( std::vector<int>::const_iterator i=fTauolaInterface->operatesOnParticles().begin();
+            i!=fTauolaInterface->operatesOnParticles().end(); i++ ) 
+               fPDGs.push_back( *i );
+   }
+   
+   if ( fEvtGenInterface ) 
+   {
+      fEvtGenInterface->init();
+      for ( std::vector<int>::const_iterator i=fEvtGenInterface->operatesOnParticles().begin();
+            i!=fEvtGenInterface->operatesOnParticles().end(); i++ )
+               fPDGs.push_back( *i );
+   }
+   
+
+   if ( fPhotosInterface )
+   {
+      fPhotosInterface->init();
+/*   will fix shortly, for future tauola++
+      if ( fPhotosInterface )
+      {
+         for ( unsigned int iss=0; iss<fPhotosInterface->specialSettings().size(); iss++ )
+         {
+            fSpecialSettings.push_back( fPhotosInterface->specialSettings()[iss]; )
+         }
+      }
+*/
+   }
+   
+   // now do special settings
+
+   // This is TEMPORARY THING, until we switch to tauola++ !!!
+   
+   if ( fPhotosInterface )
+   {
+      fSpecialSettings.push_back( "QED-brem-off:all" );
+   }
+   if ( fTauolaInterface )
+   {
+      // override !
+      fSpecialSettings.clear();
+      fSpecialSettings.push_back( "QED-brem-off:15" );
+   }
+   
+   fIsInitialized = true;
+   
+   return;
+}
+
+void ExternalDecayDriver::statistics() const
+{
+   if ( fTauolaInterface ) fTauolaInterface->statistics();
+   // similar for EvtGen and/or Photos, if needs be
+   return;
+}
