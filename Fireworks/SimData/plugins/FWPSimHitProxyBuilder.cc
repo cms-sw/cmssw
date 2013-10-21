10a9160857108c882d03475ca1a71e9ee21e7aad
Nicola Amapane <nicola.amapane@cern.ch>
2013-10-22 00:44:46 +0200

    Cherry pick changes in DTSegments vis. from nicola's branch.

--- Fireworks/SimData/plugins/FWPSimHitProxyBuilder.cc
+++ Fireworks/SimData/plugins/FWPSimHitProxyBuilder.cc
@@ -11,6 +11,7 @@
 #include "Fireworks/Core/interface/FWGeometry.h"
 #include "Fireworks/Core/interface/fwLog.h"
 #include "SimDataFormats/TrackingHit/interface/PSimHit.h"
+#include <DataFormats/MuonDetId/interface/DTWireId.h>
 
 #include "TEvePointSet.h"
 
@@ -20,6 +21,8 @@ public:
    FWPSimHitProxyBuilder( void ) {} 
    virtual ~FWPSimHitProxyBuilder( void ) {}
 
+   virtual bool haveSingleProduct() const override { return false; }
+
    REGISTER_PROXYBUILDER_METHODS();
 
 private:
@@ -28,11 +31,11 @@ private:
    // Disable default assignment operator
    const FWPSimHitProxyBuilder& operator=( const FWPSimHitProxyBuilder& );
 
-   void build( const PSimHit& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* );
+   void buildViewType( const PSimHit& iData, unsigned int iIndex, TEveElement& oItemHolder, FWViewType::EType type, const FWViewContext* ) override;
 };
 
 void
-FWPSimHitProxyBuilder::build( const PSimHit& iData, unsigned int iIndex, TEveElement& oItemHolder, const FWViewContext* )
+FWPSimHitProxyBuilder::buildViewType( const PSimHit& iData, unsigned int iIndex, TEveElement& oItemHolder, FWViewType::EType type, const FWViewContext* )
 {
    TEvePointSet* pointSet = new TEvePointSet;
    setupAddElement( pointSet, &oItemHolder );
@@ -48,6 +51,26 @@ FWPSimHitProxyBuilder::build( const PSimHit& iData, unsigned int iIndex, TEveEle
    
    float local[3] = { iData.localPosition().x(), iData.localPosition().y(), iData.localPosition().z() };
    float global[3];
+
+   // Specialized for DT simhits
+   DetId id(rawid);
+   if (id.det()==DetId::Muon && id.subdetId()==1) {   
+     DTWireId wId(rawid);
+     rawid = wId.layerId().rawId(); // DT simhits are in the RF of the DTLayer, but their ID is the id of the wire!
+     if (abs(iData.particleType())!=13){
+       pointSet->SetMarkerStyle(26); // Draw non-muon simhits (e.g. delta rays) with a different marker
+     } 
+     if (type == FWViewType::kRhoZ) { // 
+       // In RhoZ view, draw hits at the middle of the layer in the global Z coordinate, 
+       // otherwise they won't align with 1D rechits, for which only one coordinate is known.
+       if (wId.superLayer()==2) {
+	 local[1]=0;
+       } else {
+	 local[0]=0;
+       }
+     }
+   }
+
    geom->localToGlobal( rawid, local, global );
    pointSet->SetNextPoint( global[0], global[1], global[2] );
 }
