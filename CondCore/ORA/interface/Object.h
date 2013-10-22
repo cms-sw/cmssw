59f919f2df1eace43eea365b84e710c1cd4ec486
wmtan <wmtan@fnal.gov>
2013-10-22 13:38:51 -0500

    remove use of Reflex in header

--- CondCore/ORA/interface/Object.h
+++ CondCore/ORA/interface/Object.h
@@ -1,31 +1,32 @@
 #ifndef INCLUDE_ORA_OBJECT_H
 #define INCLUDE_ORA_OBJECT_H
 
+#include <string>
 //
 #include <typeinfo>
 // externals
-#include "Reflex/Type.h"
+//#include "Reflex/Type.h"
 
 namespace ora {
   
   class Object {
     public:
     Object();
-    Object( const void* ptr, const Reflex::Type& type );
+//    Object( const void* ptr, const Reflex::Type& type );
     Object( const Object& rhs);
     virtual ~Object();
     Object& operator=( const Object& rhs);
     bool operator==( const Object& rhs) const;
     bool operator!=( const Object& rhs) const;
     void* address() const;
-    const Reflex::Type& type() const;
+//    const Reflex::Type& type() const;
     std::string typeName() const;
     void* cast( const std::type_info& asType ) const;
     template <typename T> T* cast() const;
     void destruct();
     private:
     void* m_ptr;
-    Reflex::Type m_type;
+//    Reflex::Type m_type;
   };
 }
 
