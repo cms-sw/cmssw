3530e7d81e276576c502df5bad8e6a69290ca3d5
Nancy <nancy.marinelli@cern.ch>
2013-10-22 11:51:09 +0200

    Modify so that the GEDPhotonProducer is cloned and can be used to produce tmpGedPhotons starting from gedPhotonCore and gedPhotons starting from tmpGedPhotons

--- RecoEgamma/EgammaPhotonProducers/python/gedPhotonSequence_cff.py
+++ RecoEgamma/EgammaPhotonProducers/python/gedPhotonSequence_cff.py
@@ -7,6 +7,18 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaPhotonProducers.gedPhotonCore_cfi import *
 from RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi import *
 
+import RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi 
 
-gedPhotonSequence = cms.Sequence(gedPhotonCore+gedPhotons)
+tmpGedPhotons = RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi.gedPhotons.clone()
+tmpGedPhotons.photonProducer = cms.InputTag("gedPhotonCore")
+tmpGedPhotons.outputPhotonCollection = cms.string("")
+gedPhotonSequenceTmp = cms.Sequence(gedPhotonCore+tmpGedPhotons)
+
+
+gedPhotons = RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi.gedPhotons.clone()
+gedPhotons.photonProducer = cms.InputTag("tmpGedPhotons")
+gedPhotons.outputPhotonCollection = cms.string("")
+gedPhotonSequence    = cms.Sequence(gedPhotons)
+
+
 
