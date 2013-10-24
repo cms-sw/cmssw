be6c86857eceebc2ab4cc1c8a0055f6b5cbcf85a
wmtan <wmtan@fnal.gov>
2013-10-23 22:22:29 -0500

    Workarond to make dictionary compile

--- Validation/RecoParticleFlow/src/classes.h
+++ Validation/RecoParticleFlow/src/classes.h
@@ -1,6 +1,8 @@
 #include "Validation/RecoParticleFlow/interface/TH2Analyzer.h"
 #include "Validation/RecoParticleFlow/interface/NicePlot.h"
 #include "Validation/RecoParticleFlow/interface/Comparator.h"
+#include "TH2.h"
+#include "TH2D.h"
 
 namespace {
   namespace {
