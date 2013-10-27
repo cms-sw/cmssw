dc0cc5f32040fae0e799ee1f5e531c696ebf369b
Roumyana <roumyana.mileva.hadjiiska@cern.ch>
2013-10-27 12:34:07 +0100

    Simple digitization model, neutron and gamma background, realistic cluster size.

--- SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
+++ SimMuon/GEMDigitizer/python/muonGEMDigis_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# Module to create simulated GEM digis.
+# Module to create simulated GEM digis and analyze them in flight.
 simMuonGEMDigis = cms.EDProducer("GEMDigiProducer",
     signalPropagationSpeed = cms.double(0.66),
     cosmics = cms.bool(False),
@@ -10,12 +10,22 @@ simMuonGEMDigis = cms.EDProducer("GEMDigiProducer",
     timeCalibrationOffset = cms.double(19.9),
     averageClusterSize = cms.double(1.5),
     averageEfficiency = cms.double(0.98),
-    averageNoiseRate = cms.double(200.0), ## [Hz/cm^2]
+    averageNoiseRate = cms.double(0.005),#intrinsic noise
     numberOfStripsPerPartition = cms.int32(384),
-    bxwidth = cms.double(25.0),
+    bxwidth = cms.int32(25),
     minBunch = cms.int32(-5), ## in terms of 25 ns
     maxBunch = cms.int32(3),
     inputCollection = cms.string('g4SimHitsMuonGEMHits'),
-    digiModelString = cms.string('Trivial'),
-    digitizeOnlyMuons = cms.bool(False)                             
+    digiModelString = cms.string('Simple'),#might be Trivial, Simple or SimpleAnalyzeInFlight
+    digitizeOnlyMuons = cms.bool(False),# digitize all the particles or only muons
+    cutElecMomentum = cms.double(0.01),# cut those non muon particles with the momentum < than cutElecMomentum [GeV/c] to avoid the time issues
+    cutForCls = cms.int32(3),#minimal distance [#strips] between the centres of the fired clusters of the muon and the secondary particle
+    neutronGammaRoll1 = cms.double(76.),# combined effective rate in Hz/cm^2 for the first eta partition                     
+    neutronGammaRoll2 = cms.double(62.),                    
+    neutronGammaRoll3 = cms.double(52.),                     
+    neutronGammaRoll4 = cms.double(45.),                    
+    neutronGammaRoll5 = cms.double(39.),                     
+    neutronGammaRoll6 = cms.double(30.),                  
+    neutronGammaRoll7 = cms.double(23.),                    
+    neutronGammaRoll8 = cms.double(18.)                    
 )
