0479f6c394c767f885dbcedb04b4b2f682dc9db0
wmtan <wmtan@fnal.gov>
2013-10-29 17:09:00 +0100

    Remove workaround for genreflex bug that has been fixed

--- Validation/RecoParticleFlow/src/classes.h
+++ Validation/RecoParticleFlow/src/classes.h
@@ -1,8 +1,6 @@
 #include "Validation/RecoParticleFlow/interface/TH2Analyzer.h"
 #include "Validation/RecoParticleFlow/interface/NicePlot.h"
 #include "Validation/RecoParticleFlow/interface/Comparator.h"
-#include "TH2.h"  // WORKAROUND for genreflex bug
-#include "TH2D.h" // WORKAROUND for genreflex bug
 
 namespace {
   namespace {
