e10a5422ef7d827bb359750f53ea9f3919f491f5
Patrick Gartung <gartung@fnal.gov>
2013-10-30 23:38:24 -0500

    remove isDataClass check for StaticLocalChecker

--- Utilities/StaticAnalyzers/src/StaticLocalChecker.cpp
+++ Utilities/StaticAnalyzers/src/StaticLocalChecker.cpp
@@ -27,7 +27,7 @@ void StaticLocalChecker::checkASTDecl(const clang::VarDecl *D,
 	    std::string vname = D->getCanonicalDecl()->getQualifiedNameAsString();
 	    unsigned found = vname.find_last_of("::");
 	    std::string cname = vname.substr(0,found);
-	    if ( ! support::isDataClass( cname) ) return;
+//	    if ( ! support::isDataClass( cname) ) return;
 	    if ( support::isSafeClassName( vname ) ) return;
 
 	    std::string buf;
