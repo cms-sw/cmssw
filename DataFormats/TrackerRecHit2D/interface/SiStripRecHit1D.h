172a55ab79b2069ebf19ece3c4c9ef0e46f6214e
Valentin Kuznetsov <vkuznet@gmail.com>
2013-11-01 09:59:36 -0400

    Thread safe changes

--- DataFormats/TrackerRecHit2D/interface/SiStripRecHit1D.h
+++ DataFormats/TrackerRecHit2D/interface/SiStripRecHit1D.h
@@ -1,35 +1,42 @@
 #ifndef SiStripRecHit1D_H
 #define SiStripRecHit1D_H
 
-
+#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
+#include <atomic>
+#endif
 
 #include "DataFormats/TrackerRecHit2D/interface/TrackerSingleRecHit.h"
 
 #include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
 
-class SiStripRecHit1D GCC11_FINAL : public TrackerSingleRecHit { 
+class SiStripRecHit1D GCC11_FINAL : public TrackerSingleRecHit {
 public:
 
- 
-  SiStripRecHit1D(): sigmaPitch_(-1.){}
-  
-  
+  SiStripRecHit1D();
+  // swap function
+  void swap(SiStripRecHit1D& other);
+  // copy ctor
+  SiStripRecHit1D(const SiStripRecHit1D& src);
+  // copy assignment operator
+  SiStripRecHit1D& operator=(const SiStripRecHit1D& rhs);
+  // move ctor
+#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
+  SiStripRecHit1D(SiStripRecHit1D&& other);
+#endif
+
   typedef OmniClusterRef::ClusterStripRef         ClusterRef;
   typedef OmniClusterRef::ClusterRegionalRef ClusterRegionalRef;
 
-
   SiStripRecHit1D( const LocalPoint& p, const LocalError& e,
-		   const DetId& id, 
-		   OmniClusterRef const&  clus) : TrackerSingleRecHit(p,e,id,clus), sigmaPitch_(-1.){}
-
+		   const DetId& id,
+		   OmniClusterRef const&  clus);
   SiStripRecHit1D( const LocalPoint& p, const LocalError& e,
-		   const DetId& id, 
-		   ClusterRef const&  clus) : TrackerSingleRecHit(p,e,id,clus), sigmaPitch_(-1.){}
-
+		   const DetId& id,
+		   ClusterRef const&  clus);
   SiStripRecHit1D( const LocalPoint& p, const LocalError& e,
-		   const DetId& id, 
-		   ClusterRegionalRef const& clus) : TrackerSingleRecHit(p,e,id,clus), sigmaPitch_(-1.){}
-  
+		   const DetId& id,
+		   ClusterRegionalRef const& clus);
+
   /// method to facilitate the convesion from 2D to 1D hits
   SiStripRecHit1D(const SiStripRecHit2D*);
 
@@ -38,19 +45,23 @@ public:
 
 
   virtual SiStripRecHit1D * clone() const {return new SiStripRecHit1D( * this); }
-  
+
 
   virtual int dimension() const {return 1;}
   virtual void getKfComponents( KfComponentsHolder & holder ) const {getKfComponents1D(holder);}
 
- 
-  double sigmaPitch() const { return sigmaPitch_;}
-  void setSigmaPitch(double sigmap) const { sigmaPitch_=sigmap;}
+
+  double sigmaPitch() const;
+  void setSigmaPitch(double sigmap) const;
 
 private:
- 
+
  /// cache for the matcher....
-  mutable double sigmaPitch_;  // transient.... 
+#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
+  mutable std::atomic<double> sigmaPitch_;  // transient....
+#else
+  mutable double sigmaPitch_;  // transient....
+#endif
 };
 
 #endif
--- DataFormats/TrackerRecHit2D/src/SiStripRecHit2D.cc
+++ DataFormats/TrackerRecHit2D/src/SiStripRecHit2D.cc
@@ -1,1 +1,69 @@
 #include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+
+// ctor
+SiStripRecHit2D::SiStripRecHit2D()
+{
+    sigmaPitch_.store(-1., std::memory_order_release);
+}
+
+// copy ctor
+SiStripRecHit2D::SiStripRecHit2D(const SiStripRecHit2D& src)
+{
+    src.sigmaPitch_.exchange(
+            sigmaPitch_.exchange(src.sigmaPitch_.load(std::memory_order_acquire), std::memory_order_acq_rel),
+            std::memory_order_acq_rel);
+}
+// copy assingment operator
+SiStripRecHit2D& SiStripRecHit2D::operator=(const SiStripRecHit2D& rhs)
+{
+    SiStripRecHit2D temp(rhs);
+    temp.swap(*this);
+    return *this;
+}
+// public swap function
+void SiStripRecHit2D::swap(SiStripRecHit2D& other) {
+    other.sigmaPitch_.exchange(
+            sigmaPitch_.exchange(other.sigmaPitch_.load(std::memory_order_acquire), std::memory_order_acq_rel),
+            std::memory_order_acq_rel);
+}
+// move constructor
+SiStripRecHit2D::SiStripRecHit2D(SiStripRecHit2D&& other)
+    : SiStripRecHit2D() {
+    other.swap(*this);
+}
+
+SiStripRecHit2D::SiStripRecHit2D(const DetId& id,
+      OmniClusterRef const& clus) : TrackerSingleRecHit(id, clus)
+{
+  sigmaPitch_.store(-1, std::memory_order_release);
+}
+
+SiStripRecHit2D::SiStripRecHit2D( const LocalPoint& pos, const LocalError& err,
+       const DetId& id,
+       OmniClusterRef const& clus) : TrackerSingleRecHit(pos,err,id, clus)
+{
+  sigmaPitch_.store(-1, std::memory_order_release);
+}
+
+SiStripRecHit2D::SiStripRecHit2D( const LocalPoint& pos, const LocalError& err,
+       const DetId& id,
+       ClusterRef const& clus) : TrackerSingleRecHit(pos,err,id, clus)
+{
+  sigmaPitch_.store(-1, std::memory_order_release);
+}
+
+SiStripRecHit2D::SiStripRecHit2D(const LocalPoint& pos, const LocalError& err,
+      const DetId& id,
+      ClusterRegionalRef const& clus) : TrackerSingleRecHit(pos,err,id, clus)
+{
+  sigmaPitch_.store(-1, std::memory_order_release);
+}
+
+double SiStripRecHit2D::sigmaPitch() const
+{
+    return sigmaPitch_.load(std::memory_order_acquire);
+}
+void SiStripRecHit2D::setSigmaPitch(double sigmap) const
+{
+    sigmaPitch_.store(sigmap, std::memory_order_release);
+}
