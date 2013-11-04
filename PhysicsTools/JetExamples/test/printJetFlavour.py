f8a2a2860e7b3b84e9c10d20d7430b695afe5ff2
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2013-11-04 02:39:37 +0100

    new jet flavour definition

--- PhysicsTools/JetExamples/test/printJetFlavour.py
+++ PhysicsTools/JetExamples/test/printJetFlavour.py
@@ -1,6 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("testJET")
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
@@ -9,25 +14,26 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(5)
 )
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/relval/2008/5/20/RelVal-RelValTTbar-1211209682-FakeConditions-2nd/0000/08765709-5826-DD11-9CE8-000423D94700.root')
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_5_3_11_patch3/RelValTTbar/GEN-SIM-RECO/PU_START53_LV2_09Jul2013-v1/00000/A26AB2F9-41E9-E211-806F-002590593920.root')
 )
 
-process.printTree = cms.EDFilter("ParticleListDrawer",
+process.printTree = cms.EDAnalyzer("ParticleListDrawer",
     src = cms.InputTag("genParticles"),
     maxEventsToPrint  = cms.untracked.int32(1)
 )
 
-process.myPartons = cms.EDFilter("PartonSelector",
+process.myPartons = cms.EDProducer("PartonSelector",
+    src = cms.InputTag("genParticles"),
     withLeptons = cms.bool(False)
 )
 
-process.flavourByRef = cms.EDFilter("JetPartonMatcher",
-    jets = cms.InputTag("iterativeCone5CaloJets"),
+process.flavourByRef = cms.EDProducer("JetPartonMatcher",
+    jets = cms.InputTag("ak5PFJets"),
     coneSizeToAssociate = cms.double(0.3),
     partons = cms.InputTag("myPartons")
 )
 
-process.flavourByVal = cms.EDFilter("JetFlavourIdentifier",
+process.flavourByVal = cms.EDProducer("JetFlavourIdentifier",
     srcByReference = cms.InputTag("flavourByRef"),
     physicsDefinition = cms.bool(False)
 )
@@ -38,10 +44,8 @@ process.printEvent = cms.EDAnalyzer("printJetFlavour",
     srcByValue = cms.InputTag("flavourByVal")
 )
 
-process.printEventNumber = cms.OutputModule("AsciiOutputModule")
-
 process.p = cms.Path(process.printTree*process.myPartons*process.flavourByRef*process.flavourByVal*process.printEvent)
-process.outpath = cms.EndPath(process.printEventNumber)
+
 process.MessageLogger.destinations = cms.untracked.vstring('cout','cerr')
 #process.MessageLogger.cout = cms.PSet(
 #    threshold = cms.untracked.string('ERROR')
