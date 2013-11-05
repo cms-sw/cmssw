64316e87f2aa95921bb611af46ef1ec04978ae61
Christopher Jones <chrisdjones15@gmail.com>
2013-11-05 14:38:50 -0600

    Made PFCandidate's const functions thread-safe
    elementsInBlocks is actually a cache calculated from other members in the class. Changed the transient member to be a std::atomic<*> and then set the value in a thread-safe manner.
    In addition, fixed some const casts caused by using a C style cast.

--- DataFormats/ParticleFlowCandidate/src/PFCandidate.cc
+++ DataFormats/ParticleFlowCandidate/src/PFCandidate.cc
@@ -29,6 +29,7 @@ const float PFCandidate::bigMva_ = -999.;
 
 
 PFCandidate::PFCandidate() : 
+  elementsInBlocks_(nullptr),
   ecalERatio_(1.),
   hcalERatio_(1.),
   hoERatio_(1.),
@@ -67,7 +68,8 @@ PFCandidate::PFCandidate( Charge charge,
 			  const LorentzVector & p4, 
 			  ParticleType partId ) : 
   
-  CompositeCandidate(charge, p4), 
+  CompositeCandidate(charge, p4),
+  elementsInBlocks_(nullptr),
   ecalERatio_(1.),
   hcalERatio_(1.),
   hoERatio_(1.),
@@ -119,7 +121,76 @@ PFCandidate::PFCandidate( Charge charge,
   setPdgId( translateTypeToPdgId( partId ) );
 }
 
-PFCandidate::~PFCandidate() {}
+PFCandidate::PFCandidate( PFCandidate const& iOther) : 
+  elementsInBlocks_(nullptr),
+  blocksStorage_(iOther.blocksStorage_),
+  elementsStorage_(iOther.elementsStorage_),
+  sourcePtr_(iOther.sourcePtr_),
+  muonTrackType_(iOther.muonTrackType_),
+  ecalERatio_(iOther.ecalERatio_),
+  hcalERatio_(iOther.hcalERatio_),
+  hoERatio_(iOther.hoERatio_),
+  rawEcalEnergy_(iOther.rawEcalEnergy_),
+  rawHcalEnergy_(iOther.rawHcalEnergy_),
+  rawHoEnergy_(iOther.rawHoEnergy_),
+  ps1Energy_(iOther.ps1Energy_),
+  ps2Energy_(iOther.ps2Energy_),
+  flags_(iOther.flags_), 
+  deltaP_(iOther.deltaP_), 
+  vertexType_(iOther.vertexType_),
+  mva_e_pi_(iOther.mva_e_pi_),
+  mva_e_mu_(iOther.mva_e_mu_),
+  mva_pi_mu_(iOther.mva_pi_mu_),
+  mva_nothing_gamma_(iOther.mva_nothing_gamma_),
+  mva_nothing_nh_(iOther.mva_nothing_nh_),
+  mva_gamma_nh_(iOther.mva_gamma_nh_),
+  positionAtECALEntrance_(iOther.positionAtECALEntrance_),
+  getter_(iOther.getter_),
+  storedRefsBitPattern_(iOther.storedRefsBitPattern_),
+  refsInfo_(iOther.refsInfo_),
+  refsCollectionCache_(iOther.refsCollectionCache_)
+{}
+
+PFCandidate& PFCandidate::operator=(PFCandidate const& iOther) {
+  PFCandidate tmp(iOther);
+  swap(tmp);
+  return *this;
+}
+
+PFCandidate::~PFCandidate() {
+  delete elementsInBlocks_.load(std::memory_order_acquire);
+}
+
+void
+PFCandidate::swap( PFCandidate & iOther) {
+  elementsInBlocks_.store( iOther.elementsInBlocks_.exchange(elementsInBlocks_.load(std::memory_order_acquire), std::memory_order_acq_rel), std::memory_order_release);
+  std::swap(blocksStorage_,iOther.blocksStorage_);
+  std::swap(elementsStorage_,iOther.elementsStorage_);
+  std::swap(sourcePtr_,iOther.sourcePtr_);
+  std::swap(muonTrackType_,iOther.muonTrackType_);
+  std::swap(ecalERatio_,iOther.ecalERatio_);
+  std::swap(hcalERatio_,iOther.hcalERatio_);
+  std::swap(hoERatio_,iOther.hoERatio_);
+  std::swap(rawEcalEnergy_,iOther.rawEcalEnergy_);
+  std::swap(rawHcalEnergy_,iOther.rawHcalEnergy_);
+  std::swap(rawHoEnergy_,iOther.rawHoEnergy_);
+  std::swap(ps1Energy_,iOther.ps1Energy_);
+  std::swap(ps2Energy_,iOther.ps2Energy_);
+  std::swap(flags_,iOther.flags_); 
+  std::swap(deltaP_,iOther.deltaP_); 
+  std::swap(vertexType_,iOther.vertexType_);
+  std::swap(mva_e_pi_,iOther.mva_e_pi_);
+  std::swap(mva_e_mu_,iOther.mva_e_mu_);
+  std::swap(mva_pi_mu_,iOther.mva_pi_mu_);
+  std::swap(mva_nothing_gamma_,iOther.mva_nothing_gamma_);
+  std::swap(mva_nothing_nh_,iOther.mva_nothing_nh_);
+  std::swap(mva_gamma_nh_,iOther.mva_gamma_nh_);
+  std::swap(positionAtECALEntrance_,iOther.positionAtECALEntrance_);
+  std::swap(getter_,iOther.getter_);
+  std::swap(storedRefsBitPattern_,iOther.storedRefsBitPattern_);
+  std::swap(refsInfo_,iOther.refsInfo_);
+  std::swap(refsCollectionCache_,iOther.refsCollectionCache_);
+}
 
 PFCandidate * PFCandidate::clone() const {
   return new PFCandidate( * this );
@@ -133,6 +204,8 @@ void PFCandidate::addElementInBlock( const reco::PFBlockRef& blockref,
     blocksStorage_ =Blocks(blockref.id());
   blocksStorage_.push_back(blockref);
   elementsStorage_.push_back(elementIndex);
+  auto ptr = elementsInBlocks_.exchange(nullptr);
+  delete ptr;
 }
 
 
@@ -291,16 +364,16 @@ void PFCandidate::storeRefInfo(unsigned int iMask,
       refsInfo_.insert(refsInfo_.begin()+index, bitPackRefInfo(iCore,iKey));
       if (iGetter==0)
 	refsCollectionCache_.insert(refsCollectionCache_.begin()+index,
-				    (void*)iCore.productPtr());
+				    static_cast<void const*>(iCore.productPtr()));
       else
 	refsCollectionCache_.insert(refsCollectionCache_.begin()+index,0);
     } else {
       assert(refsInfo_.size()>index);
       *(refsInfo_.begin()+index)=bitPackRefInfo(iCore,iKey);
       if (iGetter==0)
-	*(refsCollectionCache_.begin()+index)=(void*)iCore.productPtr();
+	*(refsCollectionCache_.begin()+index)=static_cast<void const*>(iCore.productPtr());
       else
-	*(refsCollectionCache_.begin()+index)=0;
+	*(refsCollectionCache_.begin()+index)=nullptr;
 
     }
     storedRefsBitPattern_ |= iBit;
@@ -589,4 +662,20 @@ const math::XYZPoint & PFCandidate::vertex() const {
     break;
   }
   return vertex_;
+}
+
+const PFCandidate::ElementsInBlocks& 
+PFCandidate::elementsInBlocks() const { 
+      
+  if (nullptr == elementsInBlocks_.load(std::memory_order_acquire))
+    {
+      std::unique_ptr<ElementsInBlocks> temp( new ElementsInBlocks(blocksStorage_.size()));
+      for(unsigned int icopy=0;icopy!=blocksStorage_.size();++icopy)
+	(*temp)[icopy]=std::make_pair(blocksStorage_[icopy],elementsStorage_[icopy]);
+      ElementsInBlocks* expected=nullptr;
+      if(elementsInBlocks_.compare_exchange_strong(expected,temp.get(),std::memory_order_acq_rel)) {
+	temp.release();
+      }
+    }
+  return *(elementsInBlocks_.load(std::memory_order_acquire));
 }
