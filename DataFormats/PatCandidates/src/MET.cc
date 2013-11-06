50a422ad69e623959869e94efe175d88f8184e8a
Christopher Jones <chrisdjones15@gmail.com>
2013-11-06 16:00:29 -0600

    Made caching thread-safe in pat::MET
    Used std::atomic<*> to manage a thread safe cache of information. This dropped a transient data member so classes_def_objects.xml file had to be changed.

--- DataFormats/PatCandidates/src/MET.cc
+++ DataFormats/PatCandidates/src/MET.cc
@@ -8,12 +8,12 @@ using namespace pat;
 
 
 /// default constructor
-MET::MET(): uncorInfo_(0) {
+MET::MET(): uncorInfo_(nullptr) {
 }
 
 
 /// constructor from reco::MET
-MET::MET(const reco::MET & aMET) : PATObject<reco::MET>(aMET), uncorInfo_(0) {
+MET::MET(const reco::MET & aMET) : PATObject<reco::MET>(aMET), uncorInfo_(nullptr) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(&aMET);
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
     const reco::PFMET * pf = dynamic_cast<const reco::PFMET *>(&aMET);
@@ -22,7 +22,7 @@ MET::MET(const reco::MET & aMET) : PATObject<reco::MET>(aMET), uncorInfo_(0) {
 
 
 /// constructor from ref to reco::MET
-MET::MET(const edm::RefToBase<reco::MET> & aMETRef) : PATObject<reco::MET>(aMETRef), uncorInfo_(0) {
+MET::MET(const edm::RefToBase<reco::MET> & aMETRef) : PATObject<reco::MET>(aMETRef), uncorInfo_(nullptr) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(aMETRef.get());
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
     const reco::PFMET * pf = dynamic_cast<const reco::PFMET *>(aMETRef.get());
@@ -30,18 +30,50 @@ MET::MET(const edm::RefToBase<reco::MET> & aMETRef) : PATObject<reco::MET>(aMETR
 }
 
 /// constructor from ref to reco::MET
-MET::MET(const edm::Ptr<reco::MET> & aMETRef) : PATObject<reco::MET>(aMETRef), uncorInfo_(0) {
+MET::MET(const edm::Ptr<reco::MET> & aMETRef) : PATObject<reco::MET>(aMETRef), uncorInfo_(nullptr) {
     const reco::CaloMET * calo = dynamic_cast<const reco::CaloMET *>(aMETRef.get());
     if (calo != 0) caloMET_.push_back(calo->getSpecific());
     const reco::PFMET * pf = dynamic_cast<const reco::PFMET *>(aMETRef.get());
     if (pf != 0) pfMET_.push_back(pf->getSpecific());
 }
 
+/// copy constructor
+MET::MET(MET const& iOther):
+PATObject<reco::MET>(iOther),
+genMET_(iOther.genMET_),
+caloMET_(iOther.caloMET_),
+pfMET_(iOther.pfMET_),
+uncorInfo_(nullptr)
+{
+   auto tmp = iOther.uncorInfo_.load(std::memory_order_acquire);
+   if(tmp != nullptr) {
+      //Only thread-safe to read iOther.nCorrections_ if iOther.uncorInfo_ != nullptr
+      nCorrections_ = iOther.nCorrections_;
+      uncorInfo_.store( new std::vector<UncorInfo>{*tmp},std::memory_order_release);
+   }
+}
 
 /// destructor
 MET::~MET() {
+   delete uncorInfo_.load(std::memory_order_acquire);
 }
 
+MET& MET::operator=(MET const& iOther) {
+   PATObject<reco::MET>::operator=(iOther);
+   genMET_ = iOther.genMET_;
+   caloMET_ =iOther.caloMET_;
+   pfMET_ =iOther.pfMET_;
+   auto tmp = iOther.uncorInfo_.load(std::memory_order_acquire);
+   if(tmp != nullptr) {
+      //Only thread-safe to read iOther.nCorrections_ if iOther.uncorInfo_ != nullptr
+      nCorrections_ = iOther.nCorrections_;
+      delete uncorInfo_.exchange( new std::vector<UncorInfo>{*tmp},std::memory_order_acq_rel);
+   } else {
+      nCorrections_ = 0;
+      delete uncorInfo_.exchange(nullptr, std::memory_order_acq_rel);
+   }
+   return *this;
+}
 
 /// return the generated MET from neutrinos
 const reco::GenMET * MET::genMET() const {
@@ -59,81 +91,90 @@ unsigned int MET::nCorrections() const { checkUncor_(); return nCorrections_; }
 
 float MET::corEx(UncorrectionType ix) const {
   if (ix == uncorrNONE) return 0;
-  checkUncor_(); return uncorInfo_[ix].corEx;
+  checkUncor_(); return (*uncorInfo_.load(std::memory_order_acquire))[ix].corEx;
 }
 float MET::corEy(UncorrectionType ix) const {
   if (ix == uncorrNONE) return 0;
-  checkUncor_(); return uncorInfo_[ix].corEy;
+  checkUncor_(); return (*uncorInfo_.load(std::memory_order_acquire))[ix].corEy;
 }
 float MET::corSumEt(UncorrectionType ix) const {
   if (ix == uncorrNONE) return 0;
-  checkUncor_(); return uncorInfo_[ix].corSumEt;
+  checkUncor_(); return (*uncorInfo_.load(std::memory_order_acquire))[ix].corSumEt;
 }
 float MET::uncorrectedPt(UncorrectionType ix) const {
   if (ix == uncorrNONE) return pt();
-  checkUncor_(); return uncorInfo_[ix].pt;
+  checkUncor_(); return (*uncorInfo_.load(std::memory_order_acquire))[ix].pt;
 }
 float MET::uncorrectedPhi(UncorrectionType ix) const {
   if (ix == uncorrNONE) return phi();
-  checkUncor_(); return uncorInfo_[ix].phi;
+  checkUncor_(); return (*uncorInfo_.load(std::memory_order_acquire))[ix].phi;
 }
 
 
 //! check and set transients
 void MET::checkUncor_() const {
-  if (uncorInfo_.size() == uncorrMAXN && oldPt_ == pt() ) return;
+  if (uncorInfo_.load(std::memory_order_acquire)!=nullptr ) return;
 
-  oldPt_ = pt();
-  std::vector<CorrMETData> corrs(mEtCorr());
-  nCorrections_ = corrs.size();
+  const std::vector<CorrMETData>& corrs(mEtCorr());
+  const auto nCorrectionsTmp = corrs.size();
+  
+  std::unique_ptr<std::vector<UncorInfo>> uncorInfoTmpPtr{ new std::vector<UncorInfo>{uncorrMAXN} };
+  auto& uncorInfoTmp = *uncorInfoTmpPtr;
 
-  uncorInfo_.resize(uncorrMAXN);
   UncorrectionType ix;
 
   //! ugly
   //! ALL
   ix = uncorrALL;
-  uncorInfo_[ix] = UncorInfo();
-  for (unsigned int iC=0; iC < nCorrections_; ++iC){
-    uncorInfo_[ix].corEx +=    corrs[iC].mex;
-    uncorInfo_[ix].corEy +=    corrs[iC].mey;
-    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+  uncorInfoTmp[ix] = UncorInfo();
+  for (unsigned int iC=0; iC < nCorrectionsTmp; ++iC){
+    uncorInfoTmp[ix].corEx +=    corrs[iC].mex;
+    uncorInfoTmp[ix].corEy +=    corrs[iC].mey;
+    uncorInfoTmp[ix].corSumEt += corrs[iC].sumet;
   }
-  setPtPhi_(uncorInfo_[ix]);
+  setPtPhi_(uncorInfoTmp[ix]);
 
   //! JES
   ix = uncorrJES;
-  uncorInfo_[ix] = UncorInfo();
-  if (nCorrections_ >=1 ){
+  uncorInfoTmp[ix] = UncorInfo();
+  if (nCorrectionsTmp >=1 ){
     unsigned int iC = 0;
-    uncorInfo_[ix].corEx +=    corrs[iC].mex;
-    uncorInfo_[ix].corEy +=    corrs[iC].mey;
-    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+    uncorInfoTmp[ix].corEx +=    corrs[iC].mex;
+    uncorInfoTmp[ix].corEy +=    corrs[iC].mey;
+    uncorInfoTmp[ix].corSumEt += corrs[iC].sumet;
   }
-  setPtPhi_(uncorInfo_[ix]);
+  setPtPhi_(uncorInfoTmp[ix]);
 
   //! MUON
   ix = uncorrMUON;
-  uncorInfo_[ix] = UncorInfo();
-  if (nCorrections_ >=2 ){
+  uncorInfoTmp[ix] = UncorInfo();
+  if (nCorrectionsTmp >=2 ){
     unsigned int iC = 1;
-    uncorInfo_[ix].corEx +=    corrs[iC].mex;
-    uncorInfo_[ix].corEy +=    corrs[iC].mey;
-    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+    uncorInfoTmp[ix].corEx +=    corrs[iC].mex;
+    uncorInfoTmp[ix].corEy +=    corrs[iC].mey;
+    uncorInfoTmp[ix].corSumEt += corrs[iC].sumet;
   }
-  setPtPhi_(uncorInfo_[ix]);
+  setPtPhi_(uncorInfoTmp[ix]);
 
   //! TAU
   ix = uncorrTAU;
-  uncorInfo_[ix] = UncorInfo();
-  if (nCorrections_ >=3 ){
+  uncorInfoTmp[ix] = UncorInfo();
+  if (nCorrectionsTmp >=3 ){
     unsigned int iC = 2;
-    uncorInfo_[ix].corEx +=    corrs[iC].mex;
-    uncorInfo_[ix].corEy +=    corrs[iC].mey;
-    uncorInfo_[ix].corSumEt += corrs[iC].sumet;
+    uncorInfoTmp[ix].corEx +=    corrs[iC].mex;
+    uncorInfoTmp[ix].corEy +=    corrs[iC].mey;
+    uncorInfoTmp[ix].corSumEt += corrs[iC].sumet;
   }
-  setPtPhi_(uncorInfo_[ix]);
+  setPtPhi_(uncorInfoTmp[ix]);
 
+  //The compare_exchange_strong guarantees that the new value of nCorrections_ will be seen by other
+  // threads
+  nCorrections_ = nCorrectionsTmp;
+  
+  std::vector<UncorInfo>* expected=nullptr;
+  if(uncorInfo_.compare_exchange_strong(expected,uncorInfoTmpPtr.get(),std::memory_order_acq_rel)) {
+     uncorInfoTmpPtr.release();
+  }
 }
 
 void MET::setPtPhi_(UncorInfo& uci) const {
