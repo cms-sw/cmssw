559a30034022a41257eedd3792a2e5a14511eae2
Geonmo <ry840901@gmail.com>
2013-11-07 17:56:11 +0900

    Modify test file to use cfi

--- Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
+++ Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
@@ -1,17 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMServices.Components.DQMEnvironment_cfi import *
 
-from DQMServices.Examples.test.ConverterTester_cfi import *
-
-
-DQMStore = cms.Service("DQMStore")
-
-dqmSaver.convention = 'Offline'
-dqmSaver.workflow = '/MuonGEMDigisV/Workflow/DIGI'
 
 from Validation.MuonGEMDigis.simTrackMatching_cfi import SimTrackMatching
-
 gemDigiValidation = cms.EDAnalyzer('MuonGEMDigis',
 	outputFile = cms.string('valid.root'),
 	stripLabel= cms.InputTag('simMuonGEMDigis'),
