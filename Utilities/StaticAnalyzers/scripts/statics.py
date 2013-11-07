ead2d8f74b5891557a05a08956cae7b5d991ced4
Patrick Gartung <gartung@fnal.gov>
2013-11-07 17:29:57 -0600

    use dictionary to save the call stack instead of string

--- Utilities/StaticAnalyzers/scripts/statics.py
+++ Utilities/StaticAnalyzers/scripts/statics.py
@@ -8,8 +8,9 @@ keyword = re.compile("calls|overrides|variable")
 paths = re.compile(".*?\s*src/([A-Z].*?/[A-z].*?)(/.*?):(.*?):(.*?)")
 from collections import defaultdict
 
-gets = defaultdict(list)
-calls = defaultdict(list)
+gets = defaultdict(set)
+callby = defaultdict(set)
+calls = defaultdict(set)
 
 f = open('db.txt')
 
@@ -17,38 +18,39 @@ for line in f :
 	fields = line.split("'")
 	if keyword.search(line) :
 		if fields[2] == ' calls function ' :
-			if fields[1] not in calls[fields[3]]:
-				calls[fields[3]].append(fields[1])
+			if fields[1] not in callby[fields[3]]:
+				callby[fields[3]].add(fields[1])
+			if fields[3] not in calls[fields[1]]:
+				calls[fields[1]].add(fields[3])
 		if fields[2] == ' overrides function ' :
-			if fields[3] not in calls[fields[1]] and not edmns.search(fields[3]) :
-				calls[fields[1]].append(fields[3])		
+			if fields[3] not in callby[fields[1]] and not edmns.search(fields[3]) :
+				callby[fields[1]].add(fields[3])
 		if fields[2] == ' static variable ' :
 				if fields[1] not in gets[fields[3]]:
-					gets[fields[3]].append(fields[1])
+					gets[fields[3]].add(fields[1])
 
 f.close()
 
-def callstack(str):
-	for call in calls[str]:
+def stackup(str):
+	for call in callby[str]:
 		if call not in stack:
 			stack.add(call)
-			callstack(call)
+			stackup(call)
 	return
 
-funcs=[]
+funcs=defaultdict(list)
 
 for key in gets: 
 	for get in gets[key]:
 		func = get+" # "+key
 		stack = set()
-		callstack(get)
-		for item in sorted(stack):
-			func += " # "+item
-		funcs.append(func)
+		stackup(get)
+		funcs[func].append(sorted(stack))
 
-for func in sorted(set(funcs)):
-	fields = func.split("#")
-	for field in fields:
-		if topfunc.search(field):
-			print fields[0]+ " # "+ fields[1]+ " # " + field
+
+for func in sorted(funcs.keys()):
+	for fields in funcs[func]:
+		for field in fields:
+			if topfunc.search(field) :
+				print func + " # " + field
 		
