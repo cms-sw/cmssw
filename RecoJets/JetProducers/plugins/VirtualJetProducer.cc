045b68844b8741f70cf64a711e7d706ab16806fb
Christian <christian.veelken@cern.ch>
2013-11-08 11:05:30 +0100

    merge with updates from CMSSW_6_2_X branch

--- RecoJets/JetProducers/plugins/VirtualJetProducer.cc
+++ RecoJets/JetProducers/plugins/VirtualJetProducer.cc
@@ -17,6 +17,7 @@
 #include "FWCore/Utilities/interface/Exception.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Utilities/interface/isFinite.h"
 
 #include "DataFormats/Common/interface/View.h"
 #include "DataFormats/Common/interface/Handle.h"
@@ -31,8 +32,7 @@
 #include "DataFormats/Candidate/interface/CandidateFwd.h"
 #include "DataFormats/Candidate/interface/LeafCandidate.h"
 #include "DataFormats/Math/interface/deltaR.h"
-
-//#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 
 #include "fastjet/SISConePlugin.hh"
 #include "fastjet/CMSIterativeConePlugin.hh"
@@ -336,9 +336,24 @@ void VirtualJetProducer::produce(edm::Event& iEvent,const edm::EventSetup& iSetu
   
   // get inputs and convert them to the fastjet format (fastjet::PeudoJet)
   edm::Handle<reco::CandidateView> inputsHandle;
-  iEvent.getByLabel(src_,inputsHandle);
-  for (size_t i = 0; i < inputsHandle->size(); ++i) {
-    inputs_.push_back(inputsHandle->ptrAt(i));
+  
+  edm::Handle< std::vector<edm::FwdPtr<reco::PFCandidate> > > pfinputsHandleAsFwdPtr; 
+  
+  bool isView = iEvent.getByLabel(src_,inputsHandle);
+  if ( isView ) {
+    for (size_t i = 0; i < inputsHandle->size(); ++i) {
+      inputs_.push_back(inputsHandle->ptrAt(i));
+    }
+  } else {
+    iEvent.getByLabel(src_,pfinputsHandleAsFwdPtr);
+    for (size_t i = 0; i < pfinputsHandleAsFwdPtr->size(); ++i) {
+      if ( (*pfinputsHandleAsFwdPtr)[i].ptr().isAvailable() ) {
+	inputs_.push_back( (*pfinputsHandleAsFwdPtr)[i].ptr() );
+      }
+      else if ( (*pfinputsHandleAsFwdPtr)[i].backPtr().isAvailable() ) {
+	inputs_.push_back( (*pfinputsHandleAsFwdPtr)[i].backPtr() );
+      }
+    }
   }
   LogDebug("VirtualJetProducer") << "Got inputs\n";
   
@@ -398,7 +413,7 @@ void VirtualJetProducer::inputTowers( )
     inEnd = inputs_.end(), i = inBegin;
   for (; i != inEnd; ++i ) {
     reco::CandidatePtr input = *i;
-    if (std::isnan(input->pt()))           continue;
+    if (edm::isNotFinite(input->pt()))           continue;
     if (input->et()    <inputEtMin_)  continue;
     if (input->energy()<inputEMin_)   continue;
     if (isAnomalousTower(input))      continue;
@@ -576,7 +591,7 @@ void VirtualJetProducer::writeJets( edm::Event & iEvent, edm::EventSetup const& 
 	}
       */
       clusterSequenceWithArea->get_median_rho_and_sigma(*fjRangeDef_,false,*rho,*sigma,mean_area);
-      if((*rho < 0)|| (std::isnan(*rho))) {
+      if((*rho < 0)|| (edm::isNotFinite(*rho))) {
 	edm::LogError("BadRho") << "rho value is " << *rho << " area:" << mean_area << " and n_empty_jets: " << clusterSequenceWithArea->n_empty_jets(*fjRangeDef_) << " with range " << fjRangeDef_->description()
 				<<". Setting rho to rezo.";
 	*rho = 0;
