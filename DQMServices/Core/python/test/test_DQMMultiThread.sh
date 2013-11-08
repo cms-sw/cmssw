5a9f11564a546508b95cb4463068ed6fedb1cb88
Marco Rovere <marco.rovere@cern.ch>
2013-11-08 09:32:29 +0100

    Improve test script

--- DQMServices/Core/python/test/test_DQMMultiThread.sh
+++ DQMServices/Core/python/test/test_DQMMultiThread.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+print_abort() {
+  val=$1;shift
+  echo "Test failed with exit code ${val}"
+  exit ${val}
+}
+
 clean_files() {
   echo 'Cleaning up files'
   rm -fr *.root
@@ -10,16 +16,18 @@ clean_files() {
 run_step1_edm() {
   echo 'Running first step for MEtoEDM'
   cmsRun dqm_testMultiThread_MEtoEDM_cfg.py &> /dev/null
-  if [ $? != 0 ]; then
-    exit $?
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    print_abort ${ret}
   fi
 }
 
 run_step2_edm() {
   echo 'Running Harvesting for MEtoEDM'
   cmsRun dqm_testMultiThread_HarvestingEDMtoME.py &> /dev/null
-  if [ $? != 0 ]; then
-    exit $?
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    print_abort ${ret}
   fi
 }
 
@@ -27,8 +35,9 @@ check_files() {
 filter=$1;shift
  for f in $(ls ${filter}); do
    python check_harvesting.py ${f}
-   if [ $? != 0 ]; then
-     exit $?
+   ret=$?
+   if [ ${ret} -ne 0 ]; then
+     print_abort ${ret}
    fi
  done
 }
@@ -36,16 +45,18 @@ filter=$1;shift
 run_step1_dqmio() {
   echo 'Running first step for DQMIO'
   cmsRun dqm_testMultiThread_DQMIO_cfg.py &> /dev/null
-  if [ $? != 0 ]; then
-    exit $?
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    print_abort ${ret}
   fi
 }
 
 run_step2_dqmio() {
   echo 'Running Harvesting for DQMIO'
   cmsRun dqm_testMultiThread_HarvestingDQMIO.py &> /dev/null
-  if [ $? != 0 ]; then
-    exit $?
+  ret=$?
+  if [ ${ret} -ne 0 ]; then
+    print_abort ${ret}
   fi
 }
 
@@ -57,4 +68,4 @@ check_files "*EDM_Harvesting.root"
 run_step1_dqmio
 run_step2_dqmio
 check_files "*DQMIO_Harvesting.root"
-
+echo 'Test Successful'
