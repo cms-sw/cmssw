4f19e8fe7a797333acac1e42cce4ad68f585b60c
Taejeong <monttj@gmail.com>
2013-11-09 18:28:43 +0100

    switch to PFJet and PFMet as default for standard sequence and add b-tagging sequence for new soft lepton b-tag development and remove obsolete soft lepton tags

--- PhysicsTools/PatAlgos/python/producersLayer1/jetProducer_cff.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/jetProducer_cff.py
@@ -6,6 +6,14 @@ from PhysicsTools.PatAlgos.recoLayer0.jetMETCorrections_cff import *
 from PhysicsTools.PatAlgos.recoLayer0.bTagging_cff import *
 #from PhysicsTools.PatAlgos.recoLayer0.jetID_cff import *
 
+# b tagging 
+jetSource = 'ak5PFJets'
+from RecoJets.JetAssociationProducers.ak5JTA_cff import *
+ak5JetTracksAssociatorAtVertex.jets = jetSource
+from RecoBTag.Configuration.RecoBTag_cff import * # btagging sequence
+softPFMuonsTagInfos.jets = jetSource
+softPFElectronsTagInfos.jets = jetSource
+
 # add PAT specifics
 from PhysicsTools.PatAlgos.mcMatchLayer0.jetFlavourId_cff import *
 from PhysicsTools.PatAlgos.mcMatchLayer0.jetMatch_cfi import *
@@ -15,6 +23,8 @@ from PhysicsTools.PatAlgos.producersLayer1.jetProducer_cfi import *
 
 makePatJets = cms.Sequence(
     # reco pre-production
+    ak5JetTracksAssociatorAtVertex *
+    btagging * 
     patJetCorrections *
     patJetCharge *
    #secondaryVertexNegativeTagInfos *
