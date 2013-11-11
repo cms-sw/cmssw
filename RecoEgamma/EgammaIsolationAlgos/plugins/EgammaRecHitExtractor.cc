517188b5fed1b66dae1d5459c1ba6831f75cdd8e
Volker Adler <volker.adler@cern.ch>
2013-11-11 22:33:51 +0100

    Dependencies, part I.

--- RecoEgamma/EgammaIsolationAlgos/plugins/EgammaRecHitExtractor.cc
+++ RecoEgamma/EgammaIsolationAlgos/plugins/EgammaRecHitExtractor.cc
@@ -39,14 +39,16 @@ using namespace std;
 using namespace egammaisolation;
 using namespace reco::isodeposit;
 
-EgammaRecHitExtractor::EgammaRecHitExtractor(const edm::ParameterSet& par) : 
+EgammaRecHitExtractor::EgammaRecHitExtractor(const edm::ParameterSet& par, edm::ConsumesCollector & iC) :
     etMin_(par.getParameter<double>("etMin")),
     energyMin_(par.getParameter<double>("energyMin")),
     extRadius_(par.getParameter<double>("extRadius")),
     intRadius_(par.getParameter<double>("intRadius")),
     intStrip_(par.getParameter<double>("intStrip")),
-    barrelEcalHitsTag_(par.getParameter<edm::InputTag>("barrelEcalHits")), 
+    barrelEcalHitsTag_(par.getParameter<edm::InputTag>("barrelEcalHits")),
     endcapEcalHitsTag_(par.getParameter<edm::InputTag>("endcapEcalHits")),
+    barrelEcalHitsToken_(iC.consumes<EcalRecHitCollection>(barrelEcalHitsTag_)),
+    endcapEcalHitsToken_(iC.consumes<EcalRecHitCollection>(endcapEcalHitsTag_)),
     fakeNegativeDeposit_(par.getParameter<bool>("subtractSuperClusterEnergy")),
     tryBoth_(par.getParameter<bool>("tryBoth")),
     vetoClustered_(par.getParameter<bool>("vetoClustered")),
@@ -54,35 +56,35 @@ EgammaRecHitExtractor::EgammaRecHitExtractor(const edm::ParameterSet& par) :
     //severityLevelCut_(par.getParameter<int>("severityLevelCut"))
     //severityRecHitThreshold_(par.getParameter<double>("severityRecHitThreshold")),
     //spIdString_(par.getParameter<std::string>("spikeIdString")),
-    //spIdThreshold_(par.getParameter<double>("spikeIdThreshold")), 
-{ 
-  
-  const std::vector<std::string> flagnamesEB = 
+    //spIdThreshold_(par.getParameter<double>("spikeIdThreshold")),
+{
+
+  const std::vector<std::string> flagnamesEB =
     par.getParameter<std::vector<std::string> >("RecHitFlagToBeExcludedEB");
-  
+
   const std::vector<std::string> flagnamesEE =
     par.getParameter<std::vector<std::string> >("RecHitFlagToBeExcludedEE");
-  
-  flagsexclEB_= 
+
+  flagsexclEB_=
     StringToEnumValue<EcalRecHit::Flags>(flagnamesEB);
-  
+
   flagsexclEE_=
     StringToEnumValue<EcalRecHit::Flags>(flagnamesEE);
-  
-  const std::vector<std::string> severitynamesEB = 
+
+  const std::vector<std::string> severitynamesEB =
     par.getParameter<std::vector<std::string> >("RecHitSeverityToBeExcludedEB");
-  
-  severitiesexclEB_= 
+
+  severitiesexclEB_=
     StringToEnumValue<EcalSeverityLevel::SeverityLevel>(severitynamesEB);
-  
-  const std::vector<std::string> severitynamesEE = 
+
+  const std::vector<std::string> severitynamesEE =
     par.getParameter<std::vector<std::string> >("RecHitSeverityToBeExcludedEE");
-  
-  severitiesexclEE_= 
+
+  severitiesexclEE_=
     StringToEnumValue<EcalSeverityLevel::SeverityLevel>(severitynamesEE);
-   
+
   if ((intRadius_ != 0.0) && (fakeNegativeDeposit_)) {
-    throw cms::Exception("Configuration Error") << "EgammaRecHitExtractor: " << 
+    throw cms::Exception("Configuration Error") << "EgammaRecHitExtractor: " <<
       "If you use 'subtractSuperClusterEnergy', you *must* set 'intRadius' to ZERO; it does not make sense, otherwise.";
   }
   std::string isoVariable = par.getParameter<std::string>("isolationVariable");
@@ -91,7 +93,7 @@ EgammaRecHitExtractor::EgammaRecHitExtractor(const edm::ParameterSet& par) :
   } else if (isoVariable == "energy") {
     useEt_ = false;
   } else {
-    throw cms::Exception("Configuration Error") << "EgammaRecHitExtractor: isolationVariable '" << isoVariable << "' not known. " 
+    throw cms::Exception("Configuration Error") << "EgammaRecHitExtractor: isolationVariable '" << isoVariable << "' not known. "
 						<< " Supported values are 'et', 'energy'. ";
   }
   if (endcapEcalHitsTag_.encode() ==  barrelEcalHitsTag_.encode()) {
@@ -103,86 +105,86 @@ EgammaRecHitExtractor::EgammaRecHitExtractor(const edm::ParameterSet& par) :
   }
 }
 
-EgammaRecHitExtractor::~EgammaRecHitExtractor() 
+EgammaRecHitExtractor::~EgammaRecHitExtractor()
 {}
 
-reco::IsoDeposit EgammaRecHitExtractor::deposit(const edm::Event & iEvent, 
+reco::IsoDeposit EgammaRecHitExtractor::deposit(const edm::Event & iEvent,
 						const edm::EventSetup & iSetup, const reco::Candidate &emObject ) const {
   edm::ESHandle<CaloGeometry> pG;
   iSetup.get<CaloGeometryRecord>().get(pG);
-  
+
   //Get the channel status from the db
   //edm::ESHandle<EcalChannelStatus> chStatus;
   //iSetup.get<EcalChannelStatusRcd>().get(chStatus);
-  
+
   edm::ESHandle<EcalSeverityLevelAlgo> sevlv;
   iSetup.get<EcalSeverityLevelAlgoRcd>().get(sevlv);
   const EcalSeverityLevelAlgo* sevLevel = sevlv.product();
-    
-  const CaloGeometry* caloGeom = pG.product(); 
+
+  const CaloGeometry* caloGeom = pG.product();
   const CaloSubdetectorGeometry* barrelgeom = caloGeom->getSubdetectorGeometry(DetId::Ecal,EcalBarrel);
   const CaloSubdetectorGeometry* endcapgeom = caloGeom->getSubdetectorGeometry(DetId::Ecal,EcalEndcap);
-  
+
   static std::string metname = "EgammaIsolationAlgos|EgammaRecHitExtractor";
-  
+
   std::auto_ptr<const CaloRecHitMetaCollectionV> barrelRecHits(0), endcapRecHits(0);
-  
-  //Get barrel ECAL RecHits 
+
+  //Get barrel ECAL RecHits
   edm::Handle<EcalRecHitCollection> barrelEcalRecHitsH;
-  iEvent.getByLabel(barrelEcalHitsTag_, barrelEcalRecHitsH);
-  
-  //Get endcap ECAL RecHits 
+  iEvent.getByToken(barrelEcalHitsToken_, barrelEcalRecHitsH);
+
+  //Get endcap ECAL RecHits
   edm::Handle<EcalRecHitCollection> endcapEcalRecHitsH;
-  iEvent.getByLabel(endcapEcalHitsTag_, endcapEcalRecHitsH);
-  
+  iEvent.getByToken(endcapEcalHitsToken_, endcapEcalRecHitsH);
+
   //define isodeposit starting from candidate
   reco::SuperClusterRef sc = emObject.get<reco::SuperClusterRef>();
   math::XYZPoint caloPosition = sc->position();
-  
+
   Direction candDir(caloPosition.eta(), caloPosition.phi());
   reco::IsoDeposit deposit( candDir );
-  deposit.setVeto( reco::IsoDeposit::Veto(candDir, intRadius_) ); 
+  deposit.setVeto( reco::IsoDeposit::Veto(candDir, intRadius_) );
   double sinTheta = sin(2*atan(exp(-sc->eta())));
   deposit.addCandEnergy(sc->energy() * (useEt_ ? sinTheta : 1.0)) ;
-  
+
   // subtract supercluster if desired
   double fakeEnergy = -sc->rawEnergy();
   if (fakeNegativeDeposit_) {
     deposit.addDeposit(candDir, fakeEnergy * (useEt_ ?  sinTheta : 1.0)); // not exactly clean...
   }
-  
+
   // fill rechits
   bool inBarrel = sameTag_ || ( abs(sc->eta()) < 1.479 ); //check for barrel. If only one collection is used, use barrel
   if (inBarrel || tryBoth_) {
     collect(deposit, sc, barrelgeom, caloGeom, *barrelEcalRecHitsH, sevLevel, true);
-  } 
+  }
 
   if ((!inBarrel) || tryBoth_) {
     collect(deposit, sc, endcapgeom, caloGeom, *endcapEcalRecHitsH, sevLevel, false);
   }
-  
+
   return deposit;
 }
 
-void EgammaRecHitExtractor::collect(reco::IsoDeposit &deposit, 
-                                    const reco::SuperClusterRef& sc, const CaloSubdetectorGeometry* subdet, 
+void EgammaRecHitExtractor::collect(reco::IsoDeposit &deposit,
+                                    const reco::SuperClusterRef& sc, const CaloSubdetectorGeometry* subdet,
                                     const CaloGeometry* caloGeom,
-                                    const EcalRecHitCollection &hits, 
+                                    const EcalRecHitCollection &hits,
                                     //const EcalChannelStatus* chStatus,
-                                    const EcalSeverityLevelAlgo* sevLevel, 
+                                    const EcalSeverityLevelAlgo* sevLevel,
                                     bool barrel) const {
-  
+
   GlobalPoint caloPosition(sc->position().x(), sc->position().y() , sc->position().z());
   CaloSubdetectorGeometry::DetIdSet chosen = subdet->getCells(caloPosition,extRadius_);
   EcalRecHitCollection::const_iterator j=hits.end();
   double caloeta=caloPosition.eta();
   double calophi=caloPosition.phi();
   double r2 = intRadius_*intRadius_;
-  
+
   std::vector< std::pair<DetId, float> >::const_iterator rhIt;
-  
-  
-  for (CaloSubdetectorGeometry::DetIdSet::const_iterator i = chosen.begin(), end = chosen.end() ; i != end;  ++i)  {  
+
+
+  for (CaloSubdetectorGeometry::DetIdSet::const_iterator i = chosen.begin(), end = chosen.end() ; i != end;  ++i)  {
     j = hits.find(*i);
     if (j != hits.end()) {
       const  GlobalPoint & position = caloGeom->getPosition(*i);
@@ -191,10 +193,10 @@ void EgammaRecHitExtractor::collect(reco::IsoDeposit &deposit,
       double energy = j->energy();
       double et = energy*position.perp()/position.mag();
       double phiDiff= reco::deltaPhi(phi,calophi);
-      
+
       //check if we are supposed to veto clustered and then do so
       if(vetoClustered_) {
-	
+
 	//Loop over basic clusters:
 	bool isClustered = false;
 	for(    reco::CaloCluster_iterator bcIt = sc->clustersBegin();bcIt != sc->clustersEnd(); ++bcIt) {
@@ -204,10 +206,10 @@ void EgammaRecHitExtractor::collect(reco::IsoDeposit &deposit,
 	  }
 	  if( isClustered ) break;
 	} //end loop over basic clusters
-	
+
 	if(isClustered) continue;
       }  //end if removeClustered
-      
+
       std::vector<int>::const_iterator sit;
       int severityFlag = sevLevel->severityLevel(j->detid(), hits);
       if (barrel) {
@@ -219,39 +221,39 @@ void EgammaRecHitExtractor::collect(reco::IsoDeposit &deposit,
 	if (sit != severitiesexclEE_.end())
 	  continue;
       }
-      
+
       std::vector<int>::const_iterator vit;
       if (barrel) {
-	// new rechit flag checks 
+	// new rechit flag checks
 	//vit = std::find(flagsexclEB_.begin(), flagsexclEB_.end(), ((EcalRecHit*)(&*j))->recoFlag());
 	//if (vit != flagsexclEB_.end())
 	//  continue;
 	if (!((EcalRecHit*)(&*j))->checkFlag(EcalRecHit::kGood)) {
-	  if (((EcalRecHit*)(&*j))->checkFlags(flagsexclEB_)) {                
+	  if (((EcalRecHit*)(&*j))->checkFlags(flagsexclEB_)) {
 	    continue;
 	  }
 	}
       } else {
-	// new rechit flag checks 
+	// new rechit flag checks
 	//vit = std::find(flagsexclEE_.begin(), flagsexclEE_.end(), ((EcalRecHit*)(&*j))->recoFlag());
 	//if (vit != flagsexclEE_.end())
 	//  continue;
 	if (!((EcalRecHit*)(&*j))->checkFlag(EcalRecHit::kGood)) {
-	  if (((EcalRecHit*)(&*j))->checkFlags(flagsexclEE_)) {                
+	  if (((EcalRecHit*)(&*j))->checkFlags(flagsexclEE_)) {
 	    continue;
 	  }
 	}
       }
-      
-      if(et > etMin_ 
+
+      if(et > etMin_
 	 && energy > energyMin_  //Changed to fabs - then changed back to energy
-	 && fabs(eta-caloeta) > intStrip_ 
+	 && fabs(eta-caloeta) > intStrip_
 	 && (eta-caloeta)*(eta-caloeta) + phiDiff*phiDiff >r2 ) {
-	
+
 	deposit.addDeposit( Direction(eta, phi), (useEt_ ? et : energy));
       }
     }
   }
-} 
+}
 
 
