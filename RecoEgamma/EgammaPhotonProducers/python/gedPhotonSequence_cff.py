7ba8a91516c2aaf30730a8763a5a50e8e3ab2f7f
Nancy <nancy.marinelli@cern.ch>
2013-11-11 19:53:41 +0100

    Drive the two uses of the same producer with a switch instead of the name of the producer

--- RecoEgamma/EgammaPhotonProducers/python/gedPhotonSequence_cff.py
+++ RecoEgamma/EgammaPhotonProducers/python/gedPhotonSequence_cff.py
@@ -12,12 +12,14 @@ import RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi
 tmpGedPhotons = RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi.gedPhotons.clone()
 tmpGedPhotons.photonProducer = cms.InputTag("gedPhotonCore")
 tmpGedPhotons.outputPhotonCollection = cms.string("")
+tmpGedPhotons.reconstructionStep = cms.string("tmp")
 gedPhotonSequenceTmp = cms.Sequence(gedPhotonCore+tmpGedPhotons)
 
 
 gedPhotons = RecoEgamma.EgammaPhotonProducers.gedPhotons_cfi.gedPhotons.clone()
 gedPhotons.photonProducer = cms.InputTag("tmpGedPhotons")
 gedPhotons.outputPhotonCollection = cms.string("")
+gedPhotons.reconstructionStep = cms.string("final")
 gedPhotonSequence    = cms.Sequence(gedPhotons)
 
 
