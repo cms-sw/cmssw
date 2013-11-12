3615e3a759f9680dd635673d72f824b0cd193304
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-11-12 11:20:29 +0100

    Fix statics in CondFormats/EcalObjects.
    - Make dummy elements thread_local. More correct solution is probably simply to
      throw.
    - In case of the const API, simply change it to return by value, since Item is
      in any case basically a int.
    - Add default constructor for a few classes.

--- CondFormats/EcalObjects/interface/EcalCondObjectContainer.h
+++ CondFormats/EcalObjects/interface/EcalCondObjectContainer.h
@@ -106,9 +106,10 @@ class EcalCondObjectContainer {
                 // add coherent operator++, not needed now -- FIXME
 
                 inline
-                Item & operator[]( uint32_t rawId ) {
+                Item & operator[]( uint32_t rawId ) 
+#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
+                {
                         DetId id(rawId);
-                        static Item dummy;
                         switch (id.subdetId()) {
                                 case EcalBarrel :
                                         { 
@@ -122,14 +123,17 @@ class EcalCondObjectContainer {
                                         break;
                                 default:
                                         // FIXME (add throw)
+                                        thread_local static Item dummy;
                                         return dummy;
                         }
                 }
+#else
+;
+#endif
                 
                 inline
-                Item const & operator[]( uint32_t rawId ) const {
+                Item operator[]( uint32_t rawId ) const {
                         DetId id(rawId);
-                        static Item dummy;
                         switch (id.subdetId()) {
                                 case EcalBarrel :
                                         { 
@@ -143,11 +147,13 @@ class EcalCondObjectContainer {
                                         break;
                                 default:
                                         // FIXME (add throw)
-                                        return dummy;
+                                        // sizeof(Item) <= sizeof(int64_t) for all Items.
+                                        return Item();
                         }
                 }
                 
         private:
+                
                 EcalContainer< EBDetId, Item > eb_;
                 EcalContainer< EEDetId, Item > ee_;
 };
