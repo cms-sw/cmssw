5f42ab1e9bc65b89452371c6ac5f26cdc754ed71
Volker Adler <volker.adler@cern.ch>
2013-11-13 20:17:01 +0100

    Forward porting MVA eelctron ID from 62X, part IIa.

--- RecoEgamma/EgammaTools/interface/EcalClusterLocal.h
+++ RecoEgamma/EgammaTools/interface/EcalClusterLocal.h
@@ -4,9 +4,6 @@
 /** \class EcalClusterLocal
   *  Function to compute local coordinates of Ecal clusters
   *  (adapted from RecoEcal/EgammaCoreTools/plugins/EcalClusterLocal)
-  *  $Id: EcalClusterLocal.h
-  *  $Date:
-  *  $Revision:
   *  \author Josh Bendavid, MIT, 2011
   */
 
@@ -16,20 +13,20 @@
 #include "DataFormats/CaloRecHit/interface/CaloClusterFwd.h"
 //#include "DataFormats/EgammaReco/interface/BasicClusterFwd.h"
 
-class CaloGeometry; 
+class CaloGeometry;
 
 class EcalClusterLocal {
         public:
           EcalClusterLocal();
           ~EcalClusterLocal();
-          
+
           void localCoordsEB( const reco::CaloCluster &bclus, const edm::EventSetup &es, float &etacry, float &phicry, int &ieta, int &iphi, float &thetatilt, float &phitilt) const;
           void localCoordsEE( const reco::CaloCluster &bclus, const edm::EventSetup &es, float &xcry, float &ycry, int &ix, int &iy, float &thetatilt, float &phitilt) const;
-	  
+
 	  void localCoordsEB( const reco::CaloCluster &bclus, const CaloGeometry & geom, float &etacry, float &phicry, int &ieta, int &iphi, float &thetatilt, float &phitilt) const;
           void localCoordsEE( const reco::CaloCluster &bclus, const CaloGeometry & geom, float &xcry, float &ycry, int &ix, int &iy, float &thetatilt, float &phitilt) const;
 
-		 
+
 };
 
 #endif
