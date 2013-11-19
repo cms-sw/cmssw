24f42fa9d670e81c9093276dc39156b42cc7c2ff
Carlo Battilana <carlo.battilana@gmail.com>
2013-11-19 12:06:18 +0100

    bugfix: now pointing to correct L1Ntuple environment

--- macros/initL1Analysis.C
+++ macros/initL1Analysis.C
@@ -5,25 +5,29 @@
 
   std::cout << "Macros & ntuple producer paths setting ..." <<std::endl;
   std::string tmp=gEnv->GetValue("Unix.*.Root.MacroPath","");
-  tmp=tmp+std::string(":$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros:$CAF_TRIGGER");
+  //tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros:$CAF_TRIGGER");
+  tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros");
   gEnv->SetValue("Unix.*.Root.MacroPath",tmp.c_str());
   tmp=gROOT->GetMacroPath();
-  tmp=tmp+std::string(":$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros:$CAF_TRIGGER");
+  //tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros:$CAF_TRIGGER");
+  tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros");
  
   // L1 rates 
-  tmp=tmp+std::string(":$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates:$CAF_TRIGGER");	
-  tmp=tmp+std::string(":$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates/toolbox:$CAF_TRIGGER");
+  //tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates:$CAF_TRIGGER");	
+  //tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates/toolbox:$CAF_TRIGGER");
+  tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates");	
+  tmp=tmp+std::string(":$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates/toolbox");
   
   gROOT->SetMacroPath(tmp.c_str());
-  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/UserCode/L1TriggerDPG/interface");
-  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros");
-  gROOT->ProcessLine(".include $CMSSW_BASE/src/UserCode/L1TriggerDPG/interface");
-  gROOT->ProcessLine(".include $CMSSW_BASE/src/UserCode/L1TriggerDPG/macros"); 
+  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/interface");
+  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros");
+  gROOT->ProcessLine(".include $CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/interface");
+  gROOT->ProcessLine(".include $CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros"); 
 
-  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates");
-  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates/toolbox");
-  gROOT->ProcessLine(".include $CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates");
-  gROOT->ProcessLine(".include $CMSSW_BASE/src/UserCode/L1TriggerDPG/macros/L1Rates/toolbox");
+  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates");
+  gSystem->AddIncludePath(" -I$CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates/toolbox");
+  gROOT->ProcessLine(".include $CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates");
+  gROOT->ProcessLine(".include $CMSSW_BASE/src/L1TriggerDPG/L1Ntuples/macros/L1Rates/toolbox");
 
   std::cout << "L1Ntuple library loading ..." <<std::endl;
   gROOT->ProcessLine(".L L1Ntuple.C+");
