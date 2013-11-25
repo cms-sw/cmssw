a2c4af7ddd650fff9f10df4530617211831bf84e
nadjieh <ajafari@cern.ch>
2013-11-25 13:32:51 +0100

    rm eleID request, dilepton modules are ready!

--- DQM/Physics/python/DQMPhysics_cff.py
+++ DQM/Physics/python/DQMPhysics_cff.py
@@ -6,13 +6,13 @@ from DQM.Physics.ewkElecDQM_cfi import *
 from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
 from DQM.Physics.qcdPhotonsDQM_cfi import *
 from DQM.Physics.topSingleLeptonDQM_cfi import *
+from DQM.Physics.topDiLeptonOfflineDQM_cfi import *
 from DQM.Physics.topSingleLeptonDQM_PU_cfi import *
 from DQM.Physics.ewkMuLumiMonitorDQM_cfi import *
 from DQM.Physics.susyDQM_cfi import *
 from DQM.Physics.HiggsDQM_cfi import *
 from DQM.Physics.ExoticaDQM_cfi import *
 from DQM.Physics.B2GDQM_cfi import *
-
 from JetMETCorrections.Configuration.JetCorrectionProducersAllAlgos_cff import *
 
 
@@ -23,6 +23,9 @@ dqmPhysics = cms.Sequence( bphysicsOniaDQM
                            *qcdPhotonsDQM
 			   *topSingleMuonMediumDQM
                            *topSingleElectronMediumDQM	
+			   *DiMuonDQM
+			   *DiElectronDQM
+			   *ElecMuonDQM
                            *susyDQM
                            *HiggsDQM
                            *ExoticaDQM
