1e1e78c73f47dc804b996ba92b9eeb2cfdf45634
Sven Dildick <sven.dildick@cern.ch>
2013-11-28 15:03:03 +0100

    GEMSuperChamber inherits from GeomDet

--- Geometry/GEMGeometry/src/GEMSuperChamber.cc
+++ Geometry/GEMGeometry/src/GEMSuperChamber.cc
@@ -1,24 +1,20 @@
 #include "Geometry/GEMGeometry/interface/GEMSuperChamber.h"
 #include "Geometry/GEMGeometry/interface/GEMChamber.h"
 
-GEMSuperChamber::GEMSuperChamber() 
+GEMSuperChamber::GEMSuperChamber(GEMDetId id, const ReferenceCountingPointer<BoundPlane> & plane) :
+  GeomDet(plane), detId_(id)
 {
-}
-
-GEMSuperChamber::GEMSuperChamber(GEMDetId ch1, GEMDetId ch2) 
-{
-  if ( ch1.region() ==  ch2.region() && ch1.ring() == ch2.ring() &&
-       ch1.station() == ch2.station() && ch1.chamber() == ch2.chamber() &&
-       ch1.roll() == ch2.roll() )
-  {
-    detIds_.clear();
-    detIds_.push_back(ch1);
-    detIds_.push_back(ch2);
-  }
+  setDetId(id);
 }
 
 GEMSuperChamber::~GEMSuperChamber() 
 {
+}
+
+GEMDetId
+GEMSuperChamber::id() const
+{
+  return detId_;
 }
 
 const std::vector<GEMDetId>&
@@ -30,18 +26,25 @@ GEMSuperChamber::ids() const
 bool 
 GEMSuperChamber::operator==(const GEMSuperChamber& sch) const 
 {
-  return ( this->ids().at(0) == sch.ids().at(0) &&
-	   this->ids().at(1) == sch.ids().at(1) );
+  return this->id()==sch.id();
 }
 
 void 
-GEMSuperChamber::add(GEMChamber* rl) 
+GEMSuperChamber::add(GEMChamber* ch) 
 {
-  //const GEMDetId chId(rl->id());
-  if (std::find(chambers_.begin(), chambers_.end(), rl) == chambers_.end()){
-    //  detIds_.push_back(chId);
-    chambers_.push_back(rl);
-  }
+  chambers_.push_back(ch);
+}
+
+std::vector<const GeomDet*>
+GEMSuperChamber::components() const 
+{
+  return std::vector<const GeomDet*>(chambers_.begin(), chambers_.end());
+}
+
+const GeomDet* 
+GEMSuperChamber::component(DetId id) const 
+{
+  return chamber(GEMDetId(id.rawId()));
 }
 
 const std::vector<const GEMChamber*>& 
@@ -59,19 +62,16 @@ GEMSuperChamber::nChambers() const
 const GEMChamber* 
 GEMSuperChamber::chamber(GEMDetId id) const
 {
- // not in this super chamber! 
- if (id.chamber()!=detIds_.at(0).chamber() &&
-     id.chamber()!=detIds_.at(1).chamber()) return 0;
- 
+  if (id.chamber()!=detId_.chamber()) return 0; // not in this super chamber!
   return chamber(id.layer());
 }
 
 const GEMChamber* 
 GEMSuperChamber::chamber(int isl) const 
 {
-  for (auto detId : detIds_){
-    if (detId.layer()==isl) 
-      return dynamic_cast<const GEMChamber*>(chambers_.at(isl-1));
+  for (auto ch : chambers_){
+    if (ch->id().layer()==isl) 
+      return ch;
   }
   return 0;
 }
