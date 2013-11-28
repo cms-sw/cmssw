5c04c81f7afc9819b772a453bb155fa7c355eb7a
Marco Rovere <marco.rovere@cern.ch>
2013-11-28 17:49:09 +0100

    Fix consumes for Validation/RecoHI

--- Validation/RecoHI/plugins/HiBasicGenTest.cc
+++ Validation/RecoHI/plugins/HiBasicGenTest.cc
@@ -18,6 +18,8 @@ HiBasicGenTest::HiBasicGenTest(const edm::ParameterSet& iPSet)
 {
   dbe = 0;
   dbe = edm::Service<DQMStore>().operator->();
+  generatorToken_ = consumes<edm::HepMCProduct> (
+      iPSet.getParameter<edm::InputTag>("generatorLabel"));
 }
 
 HiBasicGenTest::~HiBasicGenTest() {}
@@ -27,7 +29,7 @@ void HiBasicGenTest::beginJob()
   if(dbe){
     ///Setting the DQM top directories
     dbe->setCurrentFolder("Generator/Particles");
-    
+
     ///Booking the ME's
     for(int ibin=0; ibin<3; ibin++) {
       dnchdeta[ibin] = dbe->book1D(Form("dnchdeta%d",ibin), ";#eta;dN^{ch}/d#eta", 100, -6.0, 6.0);
@@ -45,7 +47,7 @@ void HiBasicGenTest::beginJob()
     dbe->tag(rp->getFullname(),13);
 
 
- }
+  }
   return;
 }
 
@@ -63,19 +65,19 @@ void HiBasicGenTest::beginRun(const edm::Run& iRun,const edm::EventSetup& iSetup
 void HiBasicGenTest::endRun(const edm::Run& iRun,const edm::EventSetup& iSetup){return;}
 
 void HiBasicGenTest::analyze(const edm::Event& iEvent,const edm::EventSetup& iSetup)
-{ 
+{
 
   Handle<HepMCProduct> mc;
-  iEvent.getByLabel("generator",mc);
+  iEvent.getByToken(generatorToken_, mc);
   const HepMC::GenEvent *evt = mc->GetEvent();
   const HepMC::HeavyIon *hi = evt->heavy_ion();
-  
+
   double ip = hi->impact_parameter();
   double phi0 = hi->event_plane_angle();
 
   // fill reaction plane distribution
   rp->Fill(phi0);
-  
+
   // if the event is in one of the centrality bins of interest fill hists
   int cbin=-1;
   if(ip < 5.045) cbin=0;
@@ -90,7 +92,7 @@ void HiBasicGenTest::analyze(const edm::Event& iEvent,const edm::EventSetup& iSe
   HepMC::GenEvent::particle_const_iterator begin = evt->particles_begin();
   HepMC::GenEvent::particle_const_iterator end = evt->particles_end();
   for(HepMC::GenEvent::particle_const_iterator it = begin; it != end; ++it){
-    
+
     // only fill hists for status=1 particles
     if((*it)->status() != 1) continue;
 
@@ -99,21 +101,21 @@ void HiBasicGenTest::analyze(const edm::Event& iEvent,const edm::EventSetup& iSe
     const ParticleData * part = pdt->particle(pdg_id);
     int charge = static_cast<int>(part->charge());
     if(charge==0) continue;
-    
+
     float eta = (*it)->momentum().eta();
     float phi = (*it)->momentum().phi();
     float pt = (*it)->momentum().perp();
-    
+
     dnchdeta[cbin]->Fill(eta);
     dnchdpt[cbin]->Fill(pt);
-    
+
     double pi = TMath::Pi();
     double p = phi-phi0;
     if(p > pi) p = p - 2*pi;
     if(p < -1*pi) p = p + 2*pi;
     dnchdphi[cbin]->Fill(p);
 
-  } 
+  }
 
   return;
 
