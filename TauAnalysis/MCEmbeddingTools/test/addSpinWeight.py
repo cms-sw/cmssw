c346429e1c6361f3d37b8daf83bd54f95cc20804
Volker Adler <volker.adler@cern.ch>
2013-11-29 10:15:16 +0100

    Change INSTRUCTIONS file so that it can be run out of the box
    Just doing `source INSTRUCTIONS'

--- TauAnalysis/MCEmbeddingTools/test/addSpinWeight.py
+++ TauAnalysis/MCEmbeddingTools/test/addSpinWeight.py
@@ -11,7 +11,7 @@ process.maxEvents = cms.untracked.PSet(
 
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:../emb/embedded_RH.root')
+    fileNames = cms.untracked.vstring('file:embedded.root')
 )
 
 # Set up random number generator
