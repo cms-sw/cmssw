5283980e6d1d886f7c6fd9e6da5a7c8c4d455f0a
Cesare <cesare.calabria@ba.infn.it>
2013-11-30 19:00:09 +0100

    first commit of the validation code (DQM style)

---
+++ DQMAnalyzer/DQMAnalyzerSTEP2/python/CfiFile_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+DQMGEMSecondStep = cms.EDAnalyzer('DQMAnalyzerSTEP2',
+  	GlobalFolder = cms.untracked.string("GEMBasicPlots/"),
+  	LocalFolder = cms.untracked.vstring("5GeV","10GeV","50GeV","100GeV","200GeV","500GeV","1000GeV"),
+  	SaveFile  = cms.untracked.bool(True),
+  	NameFile  = cms.untracked.string("GEMPlots.root")
+)
