416b19e404cd3e5a2f396dd9020742cb5f581b46
Nicola <nicola.pozzobon@cern.ch>
2013-12-04 21:34:56 +0100

    testing DetSetVector

--- SimTracker/TrackTriggerAssociation/python/TTTrackAssociation_cfi.py
+++ SimTracker/TrackTriggerAssociation/python/TTTrackAssociation_cfi.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 TTTrackAssociatorFromPixelDigis = cms.EDProducer("TTTrackAssociator_PixelDigi_",
-    TTTracks = cms.VInputTag( cms.InputTag("TTTracksFromPixelDigisTracklet", "TrackletBasedL1Tracks"), ),
+    TTTracks = cms.VInputTag( #cms.InputTag("TTTracksFromPixelDigisTracklet", "TrackletBasedL1Tracks"),
+                              cms.InputTag("TTTracksFromPixelDigisAM", "AML1Tracks"),
+    ),
     TTClusterTruth = cms.InputTag("TTClusterAssociatorFromPixelDigis", "ClusterAccepted"),
     TTStubTruth = cms.InputTag("TTStubAssociatorFromPixelDigis", "StubAccepted"),
 )
