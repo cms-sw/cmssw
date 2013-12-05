0d682bdc67efd2113bafce709269ecc2c248d926
Jim <jim.brooke@cern.ch>
2013-12-05 18:24:09 +0000

    change BXVector to std::vector

--- L1Trigger/L1TCalorimeter/interface/CaloStage1TauAlgorithmFirmware.h
+++ L1Trigger/L1TCalorimeter/interface/CaloStage1TauAlgorithmFirmware.h
@@ -23,8 +23,8 @@ namespace l1t {
   public:
     CaloStage1TauAlgorithm1(const CaloMainProcessorParams & dbPars);
     virtual ~CaloStage1TauAlgorithmFirmware1();
-    virtual void processEvent(const BXVector<Tower> & towers,
-			      BXVector<Cluster> & clusters);
+    virtual void processEvent(const std::vector<Tower> & towers,
+			      std::vector<Cluster> & clusters);
   private:
     CaloParams const & m_params;
   };
