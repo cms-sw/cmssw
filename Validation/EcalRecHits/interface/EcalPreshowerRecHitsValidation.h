8776fcd4a6bbd28ee969c2fbd421253ac0ec6464
Laura <Laura.Borrello@cern.ch>
2013-12-05 11:29:44 +0100

    fix name for consumes

--- Validation/EcalRecHits/interface/EcalPreshowerRecHitsValidation.h
+++ Validation/EcalRecHits/interface/EcalPreshowerRecHitsValidation.h
@@ -58,9 +58,9 @@ class EcalPreshowerRecHitsValidation: public edm::EDAnalyzer{
   
   DQMStore* dbe_;
 
-  edm::EDGetTokenT<EEUncalibratedRecHitCollection> EEuncalibrechitCollection_;
-  edm::EDGetTokenT<EERecHitCollection> EErechitCollection_;
-  edm::EDGetTokenT<ESRecHitCollection> ESrechitCollection_;
+  edm::EDGetTokenT<EEUncalibratedRecHitCollection> EEuncalibrechitCollection_token_;
+  edm::EDGetTokenT<EERecHitCollection> EErechitCollection_token_;
+  edm::EDGetTokenT<ESRecHitCollection> ESrechitCollection_token_;
   
   MonitorElement* meESRecHitsEnergy_;    
   MonitorElement* meESRecHitsEnergy_zp1st_;
