c564be7445157afb6d98bd1207c5269c5d4af3ae
R. Alex Barbieri <luck@mit.edu>
2013-12-09 16:19:23 -0500

    fussing with LorentzVector

--- L1Trigger/L1TCalorimeter/src/firmware/CaloStage1JetAlgorithmImpHI.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/CaloStage1JetAlgorithmImpHI.cc
@@ -12,6 +12,8 @@
 // Taken from UCT code. Might not be appropriate. Refers to legacy L1 objects.
 #include "DataFormats/L1CaloTrigger/interface/L1CaloRegionDetId.h"
 
+#include "DataFormats/Candidate/interface/LeafCandidate.h"
+
 using namespace std;
 using namespace l1t;
 
