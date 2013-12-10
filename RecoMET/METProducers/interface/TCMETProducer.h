216dcd3d94767640eb17d1c97d4c8a4a922d52a1
Tai Sakuma <tai.sakuma@gmail.com>
2013-12-09 22:07:06 -0600

    add TCMETProducer, extracted from METProducer

---
+++ RecoMET/METProducers/interface/TCMETProducer.h
@@ -0,0 +1,52 @@
+// -*- C++ -*-
+//
+// Package:    METProducers
+// Class:      TCMETProducer
+//
+/**\class TCMETProducer
+
+ Description: An EDProducer for CaloMET
+
+ Implementation:
+
+*/
+//
+//
+//
+
+//____________________________________________________________________________||
+#ifndef TCMETProducer_h
+#define TCMETProducer_h
+
+//____________________________________________________________________________||
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "RecoMET/METAlgorithms/interface/TCMETAlgo.h"
+
+//____________________________________________________________________________||
+namespace cms
+{
+  class TCMETProducer: public edm::EDProducer
+    {
+    public:
+      explicit TCMETProducer(const edm::ParameterSet&);
+      virtual ~TCMETProducer() { }
+      virtual void produce(edm::Event&, const edm::EventSetup&) override;
+
+    private:
+
+      edm::EDGetTokenT<edm::View<reco::Candidate> > inputToken_;
+
+      TCMETAlgo tcMetAlgo_;
+
+    };
+}
+
+//____________________________________________________________________________||
+#endif // TCMETProducer_h
