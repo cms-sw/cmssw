4233d4dac39b63e48cab4f20f66bad11ed0400a1
Volker Adler <volker.adler@cern.ch>
2013-12-11 16:53:15 +0100

    Clean-up.

--- PhysicsTools/SelectorUtils/src/classes.h
+++ PhysicsTools/SelectorUtils/src/classes.h
@@ -1,15 +1,13 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 
-//Add includes for your classes here
 #include "PhysicsTools/SelectorUtils/interface/strbitset.h"
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "PhysicsTools/SelectorUtils/interface/JetIDSelectionFunctor.h"
-#include "PhysicsTools/SelectorUtils/interface/EventSelector.h"
-#include "PhysicsTools/SelectorUtils/interface/ElectronVPlusJetsIDSelectionFunctor.h"
-#include "PhysicsTools/SelectorUtils/interface/MuonVPlusJetsIDSelectionFunctor.h"
-#include "PhysicsTools/SelectorUtils/interface/PFJetIDSelectionFunctor.h"
-#include "PhysicsTools/SelectorUtils/interface/PVSelector.h"
-#include "PhysicsTools/SelectorUtils/interface/RunLumiSelector.h"
+// #include "PhysicsTools/SelectorUtils/interface/JetIDSelectionFunctor.h"
+// #include "PhysicsTools/SelectorUtils/interface/EventSelector.h"
+// #include "PhysicsTools/SelectorUtils/interface/ElectronVPlusJetsIDSelectionFunctor.h"
+// #include "PhysicsTools/SelectorUtils/interface/MuonVPlusJetsIDSelectionFunctor.h"
+// #include "PhysicsTools/SelectorUtils/interface/PFJetIDSelectionFunctor.h"
+// #include "PhysicsTools/SelectorUtils/interface/PVSelector.h"
+// #include "PhysicsTools/SelectorUtils/interface/RunLumiSelector.h"
 
 
 namespace PhysicsTools_SelectorUtils {
@@ -19,7 +17,7 @@ namespace PhysicsTools_SelectorUtils {
     edm::Wrapper<pat::strbitset> wstrbitset;
     std::vector< pat::strbitset> vstrbitset;
     edm::Wrapper< std::vector< pat::strbitset> > wvstrbitset;
-    
+
   };
 
 }
