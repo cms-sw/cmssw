35bd0681b27e7ed935c3a9af465e341aae151e5b
Giuseppe <giuseppe.cerati@cern.ch>
2013-12-16 13:50:48 +0100

    adding missing files

---
+++ TrackingTools/MaterialEffects/python/MaterialPropagatorParabolicMf_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from TrackingTools.MaterialEffects.MaterialPropagator_cfi import MaterialPropagator
+MaterialPropagatorParabolicMF = MaterialPropagator.clone(
+    SimpleMagneticField = cms.string('ParabolicMf'),
+    ComponentName = cms.string('PropagatorWithMaterialParabolicMf')
+)
+
+from TrackingTools.MaterialEffects.OppositeMaterialPropagator_cfi import OppositeMaterialPropagator
+OppositeMaterialPropagatorParabolicMF = OppositeMaterialPropagator.clone(
+    SimpleMagneticField = cms.string('ParabolicMf'),
+    ComponentName = cms.string('PropagatorWithMaterialParabolicMfOpposite')
+)
+
+from MagneticField.ParametrizedEngine.ParabolicParametrizedField_cfi import ParametrizedMagneticFieldProducer
+ParametrizedMagneticFieldProducerForTracking = ParametrizedMagneticFieldProducer.clone(label = "ParabolicMf")
