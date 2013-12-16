370949b5f5f373c6862a350b7623d4f7540f2542
Volker Adler <volker.adler@cern.ch>
2013-12-16 15:47:40 +0100

    Do not use CHS for MET corrections.

--- PhysicsTools/PatAlgos/python/recoLayer0/metCorrections_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/metCorrections_cff.py
@@ -5,9 +5,5 @@ from JetMETCorrections.Type1MET.pfMETCorrections_cff import *
 from JetMETCorrections.Type1MET.caloMETCorrections_cff import *
 #from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
 
-pfJetMETcorrCHS          = pfJetMETcorr.clone( src = cms.InputTag( "ak5PFJetsCHS" ) )
-pfType1CorrectedMetCHS   = pfType1CorrectedMet.clone( srcType1Corrections = cms.VInputTag( cms.InputTag( "pfJetMETcorrCHS", "type1" ) ) )
-pfType1p2CorrectedMetCHS = pfType1p2CorrectedMet.clone( srcType1Corrections = cms.VInputTag( cms.InputTag( "pfJetMETcorrCHS", "type1" ) ) )
-
 ## for scheduled mode
 patMETCorrections = cms.Sequence(produceCaloMETCorrections+producePFMETCorrections)
