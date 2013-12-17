160ee6f257b1edd37a1e6b84c85fa34658175cc3
Giuseppe <giuseppe.cerati@cern.ch>
2013-12-17 19:36:18 +0100

    add parabolic field for track building

--- Configuration/StandardSequences/python/MagneticField_cff.py
+++ Configuration/StandardSequences/python/MagneticField_cff.py
@@ -4,3 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # Master configuration for the magnetic field
 # old mapping with 4T
 from Configuration.StandardSequences.MagneticField_38T_cff import *
+
+# Parabolic parametrized magnetic field used for track building
+from MagneticField.ParametrizedEngine.ParabolicParametrizedField_cfi import ParametrizedMagneticFieldProducer as ParabolicParametrizedMagneticFieldProducer
+ParabolicParametrizedMagneticFieldProducer.label = "ParabolicMf"
