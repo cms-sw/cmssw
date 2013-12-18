1ca9856d532000f59d27c96fd23ec0d1d89e77c0
Yutaro <yutaro.iiyama@cern.ch>
2013-12-18 01:51:39 +0100

    Updating the default cluster collections to PF

--- DQM/EcalEndcapMonitorTasks/python/EEClusterTask_cfi.py
+++ DQM/EcalEndcapMonitorTasks/python/EEClusterTask_cfi.py
@@ -5,8 +5,8 @@ ecalEndcapClusterTask = cms.EDAnalyzer("EEClusterTask",
     enableCleanup = cms.untracked.bool(False),
     mergeRuns = cms.untracked.bool(False),    
     EcalRawDataCollection = cms.InputTag("ecalEBunpacker"),
-    BasicClusterCollection = cms.InputTag("multi5x5SuperClusters","multi5x5EndcapBasicClusters"),
-    SuperClusterCollection = cms.InputTag("multi5x5SuperClusters","multi5x5EndcapSuperClusters"),
+    BasicClusterCollection = cms.InputTag("particleFlowClusterECAL"),
+    SuperClusterCollection = cms.InputTag("particleFlowSuperClusterECAL", "particleFlowSuperClusterECALEndcapWithPreshower"),
     EcalRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE")
 )
 
