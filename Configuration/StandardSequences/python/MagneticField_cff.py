7e3ec421d9a12223c87cbc00ddc483ce35601782
Giuseppe <giuseppe.cerati@cern.ch>
2013-12-18 16:06:33 +0100

    remove parabolic MF

--- Configuration/StandardSequences/python/MagneticField_cff.py
+++ Configuration/StandardSequences/python/MagneticField_cff.py
@@ -4,7 +4,3 @@ import FWCore.ParameterSet.Config as cms
 # Master configuration for the magnetic field
 # old mapping with 4T
 from Configuration.StandardSequences.MagneticField_38T_cff import *
-
-# Parabolic parametrized magnetic field used for track building
-from MagneticField.ParametrizedEngine.ParabolicParametrizedField_cfi import ParametrizedMagneticFieldProducer as ParabolicParametrizedMagneticFieldProducer
-ParabolicParametrizedMagneticFieldProducer.label = "ParabolicMf"
