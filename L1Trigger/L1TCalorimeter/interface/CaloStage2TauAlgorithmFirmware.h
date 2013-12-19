b7b44bdca4d18e626b1dbe47dc1cfe5268a588d3
Jim <jim.brooke@cern.ch>
2013-12-19 15:52:20 +0000

    add missing algorithms and producer

---
+++ L1Trigger/L1TCalorimeter/interface/CaloStage2TauAlgorithmFirmware.h
@@ -0,0 +1,35 @@
+///
+/// Description: Firmware headers
+///
+/// Implementation:
+///    Concrete firmware implementations
+///
+/// \author: Jim Brooke - University of Bristol
+///
+
+//
+//
+
+#ifndef CaloStage2TauAlgorithmFirmware_H
+#define CaloStage2TauAlgorithmFirmware_H
+
+#include "L1Trigger/L1TCalorimeter/interface/CaloStage2TauAlgorithm.h"
+//#include "CondFormats/L1TCalorimeter/interface/CaloParams.h"
+
+namespace l1t {
+
+  // Imp1 is for v1 and v2
+  class CaloStage2TauAlgorithmFirmwareImp1 : public CaloStage2TauAlgorithm {
+  public:
+    CaloStage2TauAlgorithmImp1(); //const CaloMainProcessorParams & dbPars);
+    virtual ~CaloStage2TauAlgorithmFirmware1();
+    virtual void processEvent(const std::vector<Cluster> & clusters,
+			      std::vector<Tau> & taus);
+    
+  private:
+    //    CaloParams const & m_params;
+  };
+  
+}
+
+#endif
