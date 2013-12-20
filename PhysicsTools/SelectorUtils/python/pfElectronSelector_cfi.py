c451b3b5e8f6433ea3cd4079f11c9061e6292c6c
Salvatore Rappoccio <rappoccio@gmail.com>
2013-12-20 15:47:51 -0600

    Adding a few updates for reference selection from top PAG

--- PhysicsTools/SelectorUtils/python/pfElectronSelector_cfi.py
+++ PhysicsTools/SelectorUtils/python/pfElectronSelector_cfi.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 #Electron Selector
 pfElectronSelector = cms.PSet(
-    version = cms.string('SPRING11'),
-    MVA = cms.double(-0.01),
+    version = cms.string('TOPPAG'),
+    Fiducial = cms.bool(True),
     MaxMissingHits = cms.int32(1),
     D0 = cms.double(0.02),
-    electronIDused = cms.string('eidTightMC'),
     ConversionRejection = cms.bool(True),
     PFIso = cms.double(0.1),
+    MVA = cms.double(0.5),
     cutsToIgnore = cms.vstring()
     )
