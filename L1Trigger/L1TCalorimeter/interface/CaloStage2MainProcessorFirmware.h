02049ca3f8dafe371936c9aa8b7157af67e70853
Jim <jim.brooke@cern.ch>
2013-12-21 13:28:15 +0000

    fix compilation errors

--- L1Trigger/L1TCalorimeter/interface/CaloStage2MainProcessorFirmware.h
+++ L1Trigger/L1TCalorimeter/interface/CaloStage2MainProcessorFirmware.h
@@ -22,7 +22,7 @@
 #include "L1Trigger/L1TCalorimeter/interface/CaloStage2JetSumAlgorithm.h"
 
 //#include "CondFormats/L1TObjects/interface/CaloParams.h"
-
+#include "CondFormats/L1TObjects/interface/FirmwareVersion.h"
 
 
 namespace l1t {
@@ -30,7 +30,7 @@ namespace l1t {
   // first iteration
   class CaloStage2MainProcessorFirmwareImp1 : public CaloStage2MainProcessor {
   public:
-    CaloStage2MainProcessorFirmwareImp1(); //const CaloParams & dbPars);
+    CaloStage2MainProcessorFirmwareImp1(const FirmwareVersion & fwv ); //const CaloParams & dbPars);
     virtual ~CaloStage2MainProcessorFirmwareImp1();
     virtual void processEvent(const std::vector<l1t::CaloTower> &,
 			      std::vector<l1t::EGamma> & egammas,
@@ -40,7 +40,8 @@ namespace l1t {
   private:
     
     //    CaloParams const & m_params;
-    
+    FirmwareVersion const & m_fwv;
+ 
     CaloStage2ClusterAlgorithm* m_clusterAlgo;
     CaloStage2EGammaAlgorithm* m_egAlgo;
     CaloStage2TauAlgorithm* m_tauAlgo;
