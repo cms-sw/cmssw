a4289fcbf8c764fed9549b8ccf6e1a99dfc73ee9
Andrea <perrotta@cern.ch>
2013-12-22 15:11:41 +0100

    restore PVClusterComparer.cc as it is in CMSSW_7_0_0_pre11

--- RecoPixelVertexing/PixelVertexFinding/src/PVClusterComparer.cc
+++ RecoPixelVertexing/PixelVertexFinding/src/PVClusterComparer.cc
@@ -8,8 +8,8 @@ double PVClusterComparer::pTSquaredSum(const PVCluster &v) const {
   double sum=0;
   for (unsigned int i=0; i<v.tracks().size(); ++i) {
     double pt = v.tracks()[i]->pt();
-    if (pt > 0.1 && v.tracks()[i]->normalizedChi2()<20) { // Don't count tracks below 0.1 GeV
-      if (pt > 20.0) pt = 20.0;
+    if (pt > 2.5) { // Don't count tracks below 2.5 GeV
+      if (pt > 10.0) pt = 10.0;
       sum += pt*pt;
     }
   }
@@ -19,8 +19,8 @@ double PVClusterComparer::pTSquaredSum(const reco::Vertex &v) const {
   double sum=0;
   for (reco::Vertex::trackRef_iterator i=v.tracks_begin(); i!=v.tracks_end(); ++i) {
     double pt = (*i)->pt();
-    if (pt > 0.1 && (*i)->normalizedChi2()<20) { // Don't count tracks below 0.1 GeV
-      if (pt > 20.0) pt = 20.0;
+    if (pt > 2.5) { // Don't count tracks below 2.5 GeV
+      if (pt > 10.0) pt = 10.0;
       sum += pt*pt;
     }
   }
@@ -33,4 +33,3 @@ bool PVClusterComparer::operator() (const PVCluster &v1, const PVCluster &v2) co
 bool PVClusterComparer::operator() (const reco::Vertex &v1, const reco::Vertex &v2) const {
   return ( pTSquaredSum(v1) > pTSquaredSum(v2) );
 }
-
