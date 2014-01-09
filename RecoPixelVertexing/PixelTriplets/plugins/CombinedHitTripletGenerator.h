2b61caa6f4615a1df917b1c5725ceb79d7655980
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-01-09 10:54:30 +0100

    Migrate CombinedHitTripletGenerator to SeedingLayerSetsHits

--- RecoPixelVertexing/PixelTriplets/plugins/CombinedHitTripletGenerator.h
+++ RecoPixelVertexing/PixelTriplets/plugins/CombinedHitTripletGenerator.h
@@ -7,13 +7,13 @@
  */ 
 
 #include <vector>
+#include <memory>
 #include "RecoPixelVertexing/PixelTriplets/interface/HitTripletGenerator.h"
 #include "RecoTracker/TkHitPairs/interface/LayerHitMapCache.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 class TrackingRegion;
 class HitTripletGeneratorFromPairAndLayers;
-namespace ctfseeding { class SeedingLayer;}
 
 namespace edm { class Event; }
 namespace edm { class EventSetup; }
@@ -33,14 +33,10 @@ public:
       const edm::Event & ev,  const edm::EventSetup& es);
 
 private:
-  void init(const edm::ParameterSet & cfg, const edm::EventSetup& es);
+  edm::InputTag theSeedingLayerSrc;
 
-  mutable bool initialised;
-
-  edm::ParameterSet         theConfig;
   LayerCacheType            theLayerCache;
 
-  typedef std::vector<HitTripletGeneratorFromPairAndLayers* > GeneratorContainer;
-  GeneratorContainer        theGenerators;
+  std::unique_ptr<HitTripletGeneratorFromPairAndLayers> theGenerator;
 };
 #endif
