75cfe0db746f8180c4a3b79a1797aae386e30f32
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-01-09 08:37:34 +0100

    Migrate HitPairGeneratorFromLayerPair to SeedingLayerSetsHits

--- RecoTracker/ConversionSeedGenerators/interface/CombinedHitPairGeneratorForPhotonConversion.h
+++ RecoTracker/ConversionSeedGenerators/interface/CombinedHitPairGeneratorForPhotonConversion.h
@@ -2,6 +2,7 @@
 #define CombinedHitPairGeneratorForPhotonConversion_H
 
 #include <vector>
+#include <memory>
 #include "RecoTracker/TkHitPairs/interface/HitPairGenerator.h"
 #include "RecoTracker/TkHitPairs/interface/LayerHitMapCache.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -10,12 +11,7 @@
 class TrackingRegion;
 class OrderedHitPairs;
 class HitPairGeneratorFromLayerPairForPhotonConversion;
-namespace ctfseeding { class SeedingLayer;}
 namespace edm { class Event; class EventSetup; }
-
-#include "FWCore/Framework/interface/ESWatcher.h"
-#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
-#include "RecoTracker/TkSeedingLayers/interface/SeedingLayerSets.h"
 
 #include "RecoTracker/ConversionSeedGenerators/interface/ConversionRegion.h"
 
@@ -31,15 +27,14 @@ public:
   CombinedHitPairGeneratorForPhotonConversion(const edm::ParameterSet & cfg);
   virtual ~CombinedHitPairGeneratorForPhotonConversion();
 
-  void  add(const ctfseeding::SeedingLayer & inner, 
-            const ctfseeding::SeedingLayer & outer);
+  void setSeedingLayers(SeedingLayerSetsHits::SeedingLayerSet layers) override;
 
   /// form base class
   virtual void hitPairs(const TrackingRegion&, OrderedHitPairs&, const edm::Event&, const edm::EventSetup&){};
 
   /// from base class
   virtual CombinedHitPairGeneratorForPhotonConversion * clone() const 
-    { return new CombinedHitPairGeneratorForPhotonConversion(theConfig); } 
+    { return new CombinedHitPairGeneratorForPhotonConversion(*this); }
 
   /*Added to the original class*/
   const OrderedHitPairs & run(
@@ -55,21 +50,13 @@ public:
   /*------------------------*/
 private:
   CombinedHitPairGeneratorForPhotonConversion(const CombinedHitPairGeneratorForPhotonConversion & cb); 
-  void init(const ctfseeding::SeedingLayerSets & layerSets);
-  void init(const edm::ParameterSet & cfg, const edm::EventSetup& es);
-  void cleanup();
 
-
-  mutable bool initialised;
-  edm::ParameterSet theConfig;
+  edm::InputTag theSeedingLayerSrc;
   uint32_t maxHitPairsPerTrackAndGenerator;
 
   LayerCacheType   theLayerCache;
 
-  edm::ESWatcher<TrackerDigiGeometryRecord> theESWatcher;
-
-  typedef std::vector<HitPairGeneratorFromLayerPairForPhotonConversion *>   Container;
-  Container        theGenerators;
+  std::unique_ptr<HitPairGeneratorFromLayerPairForPhotonConversion> theGenerator;
 
   OrderedHitPairs thePairs;
 
