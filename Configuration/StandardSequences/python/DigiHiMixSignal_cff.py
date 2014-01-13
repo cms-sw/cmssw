4949a6c1a353dd08d3bb99fce093ed4510bac94b
Yetkin <yetkin.yilmaz@cern.ch>
2014-01-13 15:35:47 +0100

    use only one HepMCProduct when keeping signal-only

--- Configuration/StandardSequences/python/DigiHiMixSignal_cff.py
+++ Configuration/StandardSequences/python/DigiHiMixSignal_cff.py
@@ -3,5 +3,10 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.DigiHiMix_cff import *
 
 mergedtruth.HepMCDataLabels = ['hiSignal']
+mergedtruth.useMultipleHepMCLabels = False
+
+
+
+
 
 
