d41d9efa7102aeab88c9c1a01727f1cbd5592089
Igor <igor.i.katkov@gmail.com>
2014-01-15 17:26:35 +0100

    pmt gain, photoelectronsToAnalog, set to 4.24 by 20 to work around saturation in PbPb simulation; this assumes also a corresponding check of conditions

--- SimCalorimetry/CastorSim/python/castordigi_cfi.py
+++ SimCalorimetry/CastorSim/python/castordigi_cfi.py
@@ -9,7 +9,7 @@ simCastorDigis = cms.EDProducer("CastorDigiProducer",
         samplingFactor = cms.double(16.75), ## pe/GeV
 
         doPhotoStatistics = cms.bool(True),
-        photoelectronsToAnalog = cms.double(0.848),
+        photoelectronsToAnalog = cms.double(0.212), #pPb: 0.848 = 4.24/5 => PbPb: 4.24/20 = 0.212
         simHitToPhotoelectrons = cms.double(1000.0),
         syncPhase = cms.bool(True),
         timePhase = cms.double(-4.0)
