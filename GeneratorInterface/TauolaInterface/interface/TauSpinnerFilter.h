77d5e1b953c712a7fe1bdd72185f7c271597ff7a
inugent <you@somedomain.com>
2014-01-15 20:39:12 +0100

    Addingplugin setup for External in 7_0_X

---
+++ GeneratorInterface/TauolaInterface/interface/TauSpinnerFilter.h
@@ -0,0 +1,39 @@
+#ifndef Gen_TauolaInterface_TauSpinnerFilter_H
+#define Gen_TauolaInterface_TauSpinnerFilter_H
+
+// I. M. Nugent
+// Filter on TauSpinner polarization weights to make unweighted polarized MC 
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+
+#include "CLHEP/Random/RandomEngine.h"
+
+//
+// class declaration
+//
+
+class TauSpinnerFilter : public edm::EDFilter {
+ public:
+  TauSpinnerFilter(const edm::ParameterSet&);
+  ~TauSpinnerFilter(){};
+
+  virtual bool filter(edm::Event& e, edm::EventSetup const& es);
+
+ private:
+  CLHEP::HepRandomEngine* decayRandomEngine;
+  double ntaus_;
+};
+
+#endif
