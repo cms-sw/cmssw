133c7216afdfd8304d583afe0e41a592d22fd9e2
Andrea <perrotta@cern.ch>
2014-01-15 15:51:40 +0100

    fix FindPeakFastPV.h

--- RecoPixelVertexing/PixelVertexFinding/interface/FindPeakFastPV.h
+++ RecoPixelVertexing/PixelVertexFinding/interface/FindPeakFastPV.h
@@ -5,43 +5,42 @@
  * Use only values with *zWeights*>*m_weightCut*. 
  * Look near *oldVertex* within *m_zClusterSearchArea*.
  *
- *  $Date: 2013/12/17 17:33:24 $
- *  $Revision: 1.0 $
  *  \author Silvio Donato (SNS)
  */
 
 
-float FindPeakFastPV(  std::vector<float> &zProjections, std::vector<float> &zWeights, float oldVertex, const float m_zClusterWidth, const float m_zClusterSearchArea, const float m_weightCut){
-float centerWMax= oldVertex;
-if( m_zClusterWidth > 0 && m_zClusterSearchArea >0 )
+float FindPeakFastPV(const std::vector<float> &zProjections, const std::vector<float> &zWeights, const float oldVertex, const float m_zClusterWidth, const float m_zClusterSearchArea, const float m_weightCut) {
+float centerWMax = oldVertex;
+if( m_zClusterWidth > 0 && m_zClusterSearchArea >0 ) 
 {
-  std::vector<float>::iterator itCenter = zProjections.begin();
-  std::vector<float>::iterator itLeftSide = zProjections.begin();
-  std::vector<float>::iterator itRightSide = zProjections.begin();
-  float zClusterWidth = m_zClusterWidth/2.0; //take half zCluster width 
+  std::vector<float>::const_iterator itCenter = zProjections.begin();
+  std::vector<float>::const_iterator itLeftSide = zProjections.begin();
+  std::vector<float>::const_iterator itRightSide = zProjections.begin();
+  const float zClusterWidth = m_zClusterWidth*0.5; //take half zCluster width 
+  const float zLowerBound = oldVertex - zClusterWidth - m_zClusterSearchArea;
+  const float zUpperBound = oldVertex + zClusterWidth + m_zClusterSearchArea;
   float maxW=0;
-  for(;itCenter!=zProjections.end(); itCenter++)
-  {
+  for(;itCenter!=zProjections.end(); itCenter++)  {
+    //loop only on the zProjections within oldVertex +/- (zClusterWidth + m_zClusterSearchArea)
+    if( (*itCenter < zLowerBound) || (*itCenter > zUpperBound) ) continue; 
 
-    if( (*itCenter < (oldVertex - zClusterWidth - m_zClusterSearchArea  ) ) || (*itCenter > (oldVertex + zClusterWidth + m_zClusterSearchArea ) )) continue; //loop only on the zProjections within oldVertex +/- (zClusterWidth + m_zClusterSearchArea)
     while(itLeftSide != zProjections.end() && (*itCenter - *itLeftSide) > zClusterWidth  ) itLeftSide++;
     while(itRightSide != zProjections.end() && (*itRightSide - *itCenter) < zClusterWidth  ) itRightSide++;
     float nWeighted = 0;
     float centerW= 0;
     
-    for(std::vector<float>::iterator ii = itLeftSide; ii != itRightSide; ii++) 
-    {//loop inside the peak and calculate its weight
-     if(zWeights[ii-zProjections.begin()]<m_weightCut) continue;
-     nWeighted+=zWeights[ii-zProjections.begin()]; 
-     centerW+=(*ii)*zWeights[ii-zProjections.begin()]; 
+    for(std::vector<float>::const_iterator ii = itLeftSide; ii != itRightSide; ii++) {
+      //loop inside the peak and calculate its weight
+      if(zWeights[ii-zProjections.begin()]<m_weightCut) continue;
+      nWeighted+=zWeights[ii-zProjections.begin()]; 
+      centerW+=(*ii)*zWeights[ii-zProjections.begin()]; 
     }
     centerW/=nWeighted; //calculate the weighted peak center
-    if(nWeighted > maxW)
-    {
-       maxW=nWeighted;
-       centerWMax=centerW;	       
+    if(nWeighted > maxW) {
+      maxW=nWeighted;
+      centerWMax=centerW;	       
     }
-  }  
+  }
 }
 return centerWMax;
 }
