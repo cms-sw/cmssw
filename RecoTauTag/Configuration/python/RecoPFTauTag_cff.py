978be2d468393bf75fbce4a3ee784108e4d0b6da
Pavel Jez <pavel.jez@cern.ch>
2014-01-15 16:56:03 +0100

    edit RecoTauJetRegionProducer to match filename

--- RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
+++ RecoTauTag/Configuration/python/RecoPFTauTag_cff.py
@@ -17,8 +17,10 @@ import FWCore.ParameterSet.Config as cms
 # Collection PFCandidates from a DR=0.8 cone about the jet axis and make new
 # faux jets with this collection
 from RecoTauTag.RecoTau.RecoTauJetRegionProducer_cfi import \
-      recoTauAK5PFJets08Region
-recoTauAK5PFJets08Region.src = cms.InputTag("ak5PFJets")
+      RecoTauJetRegionProducer
+recoTauAK5PFJets08Region=RecoTauJetRegionProducer.clone(
+    src = cms.InputTag("ak5PFJets")
+)
 
 
 
--- RecoTauTag/RecoTau/python/RecoTauJetRegionProducer_cfi.py
+++ RecoTauTag/RecoTau/python/RecoTauJetRegionProducer_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-recoTauAK5PFJets08Region = cms.EDProducer(
+RecoTauJetRegionProducer = cms.EDProducer(
         "RecoTauJetRegionProducer",
             deltaR = cms.double(0.8),
             src = cms.InputTag("ak5PFJets"),
