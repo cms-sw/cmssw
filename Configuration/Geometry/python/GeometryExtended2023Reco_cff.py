2da5c39eec77619bca06d482668f0b0dd0807eee
Marcello <marcello.maggi@cern.ch>
2014-01-17 00:08:25 +0100

    back port to synch with PR1902

--- Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
@@ -15,7 +15,6 @@ from Geometry.TrackerNumberingBuilder.trackerTopologyConstants_cfi import *
 #Muon
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-from Geometry.GEMGeometry.gemGeometry_cfi import *
 
 #  Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerSLHCGeometry_cff import *
@@ -25,7 +24,22 @@ trackerSLHCGeometry.applyAlignment = cms.bool(False)
 
 #  Calorimeters
 from Geometry.CaloEventSetup.CaloTopology_cfi import *
-from Geometry.CaloEventSetup.CaloGeometry_cff import *
+
+from Geometry.CaloEventSetup.CaloGeometryBuilder_cfi import *
+
+CaloGeometryBuilder = cms.ESProducer("CaloGeometryBuilder",
+    SelectedCalos = cms.vstring('HCAL'          , 
+                                'ZDC'           ,
+                                'CASTOR'        ,
+                                'EcalBarrel'    , 
+                                'TOWER'           )
+)
+
+from Geometry.EcalAlgo.EcalBarrelGeometry_cfi import *
+from Geometry.HcalEventSetup.HcalGeometry_cfi import *
+from Geometry.HcalEventSetup.CaloTowerGeometry_cfi import *
+from Geometry.HcalEventSetup.HcalTopology_cfi import *
+from Geometry.ForwardGeometry.ForwardGeometry_cfi import *
 from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
 from Geometry.EcalMapping.EcalMapping_cfi import *
 from Geometry.EcalMapping.EcalMappingRecord_cfi import *
