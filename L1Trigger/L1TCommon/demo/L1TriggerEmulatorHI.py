40a1ec9289149044b8504f454b4950083ea5bb9f
R. Alex Barbieri <luck@mit.edu>
2014-01-17 12:39:01 -0500

    run on gctDigis for now, to get real results. TODO: fix rctDigis

--- L1Trigger/L1TCommon/demo/L1TriggerEmulatorHI.py
+++ L1Trigger/L1TCommon/demo/L1TriggerEmulatorHI.py
@@ -21,12 +21,13 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("PoolSource",
     secondaryFileNames = cms.untracked.vstring(),
-    fileNames = cms.untracked.vstring("file:/d101/icali/ROOTFiles/HIHIghPtRAW/181530/A8D6061E-030D-E111-A482-BCAEC532971A.root")
+    #fileNames = cms.untracked.vstring("file:/d101/icali/ROOTFiles/HIHIghPtRAW/181530/A8D6061E-030D-E111-A482-BCAEC532971A.root")
 	#fileNames = cms.untracked.vstring("/store/relval/CMSSW_7_0_0_pre8/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/START70_V1-v1/00000/262AA156-744A-E311-9829-002618943945.root")
     #fileNames = cms.untracked.vstring("/store/RelVal/CMSSW_7_0_0_pre4/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/PRE_ST62_V8-v1/00000/22610530-FC24-E311-AF35-003048FFD7C2.root")
     #fileNames = cms.untracked.vstring("/store/relval/CMSSW_7_0_0_pre4/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/PRE_ST62_V8-v1/00000/22610530-FC24-E311-AF35-003048FFD7C2.root")
     #fileNames = cms.untracked.vstring("file:22610530-FC24-E311-AF35-003048FFD7C2.root")
     #fileNames = cms.untracked.vstring("file:test.root")
+    fileNames = cms.untracked.vstring("file:/mnt/hadoop/cms/store/user/icali/HIHighPt/HIHIHighPt_RAW_Skim_HLT_HIFullTrack14/4d786c9deacb28bba8fe5ed87e99b9e4/SD_HIFullTrack14_975_1_SZU.root")
     )
 
 
@@ -35,7 +36,7 @@ process.output = cms.OutputModule(
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = cms.untracked.vstring('keep *'),
-    fileName = cms.untracked.string('demo_output.root'),
+    fileName = cms.untracked.string('demo_output_HI.root'),
     dataset = cms.untracked.PSet(
     filterName = cms.untracked.string(''),
     dataTier = cms.untracked.string('')
@@ -50,25 +51,25 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS1', '')
 process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
 process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
 
-process.rctDigis = cms.EDProducer("L1RCTProducer",
-    hcalDigis = cms.VInputTag(cms.InputTag("hcalTriggerPrimitiveDigis")),
-    useEcal = cms.bool(True),
-    useHcal = cms.bool(True),
-    ecalDigis = cms.VInputTag(cms.InputTag("ecalTriggerPrimitiveDigis")),
-    BunchCrossings = cms.vint32(0),
-    getFedsFromOmds = cms.bool(False),
-#    getFedsFromOmds = cms.bool(True), # ONLY for online DQM!
-    queryDelayInLS = cms.uint32(10),
-    queryIntervalInLS = cms.uint32(100)#,
-)
+# process.rctDigis = cms.EDProducer("L1RCTProducer",
+#     hcalDigis = cms.VInputTag(cms.InputTag("hcalTriggerPrimitiveDigis")),
+#     useEcal = cms.bool(True),
+#     useHcal = cms.bool(True),
+#     ecalDigis = cms.VInputTag(cms.InputTag("ecalTriggerPrimitiveDigis")),
+#     BunchCrossings = cms.vint32(0),
+#     getFedsFromOmds = cms.bool(False),
+# #    getFedsFromOmds = cms.bool(True), # ONLY for online DQM!
+#     queryDelayInLS = cms.uint32(10),
+#     queryIntervalInLS = cms.uint32(100)#,
+# )
 
 process.RCTConverter = cms.EDProducer(
     "l1t::L1TCaloRCTToUpgradeConverter",
-    regionTag = cms.InputTag("rctDigis"),
-    emTag = cms.InputTag("rctDigis"),
-    preSamples = cms.uint32(0),
-    postSamples = cms.uint32(0))
-	
+    regionTag = cms.InputTag("gctDigis"),
+    emTag = cms.InputTag("gctDigis"),
+    preSamples = cms.uint32(1),
+    postSamples = cms.uint32(1))
+
 
 process.caloTowers = cms.EDProducer("l1t::L1TCaloTowerProducer")
 process.caloStage1 = cms.EDProducer(
@@ -84,12 +85,12 @@ process.GCTConverter=cms.EDProducer("l1t::L1TCaloUpgrateToGCTConverter",
 
 
 process.digiStep = cms.Sequence(
-        process.rctDigis
-		*process.RCTConverter
-#        *process.caloTowers
-        *process.caloStage1
-		*process.GCTConverter
-        )
+    process.gctDigis
+    *process.RCTConverter
+    #        *process.caloTowers
+    *process.caloStage1
+    *process.GCTConverter
+)
 
 
 process.p1 = cms.Path(
