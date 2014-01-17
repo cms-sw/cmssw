b2adfa70eb82ca558ff516c868263c4c10d24429
R. Alex Barbieri <luck@mit.edu>
2014-01-17 12:16:42 -0500

    fix off-by-one error

--- L1Trigger/L1TCalorimeter/plugins/L1TCaloUpgrateToGCTConverter.cc
+++ L1Trigger/L1TCalorimeter/plugins/L1TCaloUpgrateToGCTConverter.cc
@@ -5,12 +5,12 @@
 l1t::L1TCaloUpgrateToGCTConverter::L1TCaloUpgrateToGCTConverter(const ParameterSet& iConfig)
   {
 
-    
+
     produces<L1GctEmCandCollection>("isoEm");
     produces<L1GctEmCandCollection>("nonIsoEm");
     produces<L1GctJetCandCollection>("cenJets");
     produces<L1GctJetCandCollection>("forJets");
-    produces<L1GctJetCandCollection>("tauJets");  
+    produces<L1GctJetCandCollection>("tauJets");
     produces<L1GctInternJetDataCollection>();
     produces<L1GctEtTotalCollection>();
     produces<L1GctEtHadCollection>();
@@ -20,14 +20,14 @@ l1t::L1TCaloUpgrateToGCTConverter::L1TCaloUpgrateToGCTConverter(const ParameterS
     produces<L1GctInternHtMissCollection>();
     produces<L1GctHFBitCountsCollection>();
     produces<L1GctHFRingEtSumsCollection>();
-      
+
     // register what you consume and keep token for later access:
     EGammaToken_ = consumes<L1TEGammaCollection>(iConfig.getParameter<InputTag>("InputCollection"));
     TauToken_ = consumes<L1TTauCollection>(iConfig.getParameter<InputTag>("InputCollection"));
     JetToken_ = consumes<L1TJetCollection>(iConfig.getParameter<InputTag>("InputCollection"));
     EtSumToken_ = consumes<L1TEtSumCollection>(iConfig.getParameter<InputTag>("InputCollection"));
 
-   
+
   }
 
 
@@ -54,34 +54,34 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
 
   Handle<L1TJetCollection> Jet;
   e.getByToken(JetToken_,Jet);
-    
+
   Handle<L1TEtSumCollection> EtSum;
   e.getByToken(EtSumToken_,EtSum);
-  
-    
+
+
     // create the em and jet collections
     std::auto_ptr<L1GctEmCandCollection> isoEmResult(new L1GctEmCandCollection( ) );
     std::auto_ptr<L1GctEmCandCollection> nonIsoEmResult(new L1GctEmCandCollection( ) );
     std::auto_ptr<L1GctJetCandCollection> cenJetResult(new L1GctJetCandCollection( ) );
     std::auto_ptr<L1GctJetCandCollection> forJetResult(new L1GctJetCandCollection( ) );
     std::auto_ptr<L1GctJetCandCollection> tauJetResult(new L1GctJetCandCollection( ) );
-    
+
     // create the energy sum digis
     std::auto_ptr<L1GctEtTotalCollection> etTotResult (new L1GctEtTotalCollection( ) );
     std::auto_ptr<L1GctEtHadCollection>   etHadResult (new L1GctEtHadCollection  ( ) );
     std::auto_ptr<L1GctEtMissCollection>  etMissResult(new L1GctEtMissCollection ( ) );
     std::auto_ptr<L1GctHtMissCollection>  htMissResult(new L1GctHtMissCollection ( ) );
-    
+
     // create the Hf sums digis
     std::auto_ptr<L1GctHFBitCountsCollection>  hfBitCountResult (new L1GctHFBitCountsCollection ( ) );
     std::auto_ptr<L1GctHFRingEtSumsCollection> hfRingEtSumResult(new L1GctHFRingEtSumsCollection( ) );
-    
+
     // create internal data collections
     std::auto_ptr<L1GctInternJetDataCollection> internalJetResult   (new L1GctInternJetDataCollection( ));
     std::auto_ptr<L1GctInternEtSumCollection>   internalEtSumResult (new L1GctInternEtSumCollection  ( ));
     std::auto_ptr<L1GctInternHtMissCollection>  internalHtMissResult(new L1GctInternHtMissCollection ( ));
-   
-    
+
+
     //Finding the BX range for each input collection
     int firstBxEGamma = EGamma->getFirstBX();
     int lastBxEGamma = EGamma->getLastBX();
@@ -91,17 +91,17 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
     int lastBxJet = Jet->getLastBX();
     int firstBxEtSum = EtSum->getFirstBX();
     int lastBxEtSum = EtSum->getLastBX();
-   
-    
+
+
     //Looping over EGamma BXVector
-    for(int itBX=firstBxEGamma; itBX!=lastBxEGamma; ++itBX){
-        cout << "Doing Egamma bx " << itBX << endl;
-		
+    for(int itBX=firstBxEGamma; itBX!=lastBxEGamma+1; ++itBX){
+      //cout << "Doing Egamma bx " << itBX << endl;
+
         //looping over EGamma elments with a specific BX
         L1TEGammaCollection::const_iterator itEGamma = EGamma->begin(itBX);
         for(; itEGamma != EGamma->end(itBX); ++itEGamma){
             bool iso = itEGamma->hwIso();
-            
+
             L1GctEmCand EmCand(itEGamma->hwPt(), itEGamma->hwPhi(), itEGamma->hwEta(), iso, 0, 0, itBX);             //L1GctEmCand(unsigned rank, unsigned phi, unsigned eta, bool iso, uint16_t block, uint16_t index, int16_t bx);
 
             if(iso) isoEmResult->push_back(EmCand);
@@ -111,33 +111,33 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
         }
 
     }
-    
+
      //Looping over Tau BXVector
-    for(int itBX=firstBxTau; itBX!=lastBxTau; ++itBX){
-        
-        
+    for(int itBX=firstBxTau; itBX!=lastBxTau+1; ++itBX){
+
+
         //looping over Tau elments with a specific BX
         L1TTauCollection::const_iterator itTau = Tau->begin(itBX);
         for(; itTau != Tau->end(itBX); ++itTau){
-            
+
             bool forward= (itTau->hwEta() < 4 || itTau->hwEta() > 17);
             L1GctJetCand TauCand(itTau->hwPt(), itTau->hwPhi(), itTau->hwEta(), true, forward,0, 0, itBX);     //L1GctJetCand(unsigned rank, unsigned phi, unsigned eta, bool isTau, bool isFor, uint16_t block, uint16_t index, int16_t bx);
              tauJetResult->push_back(TauCand);
-            
+
         }
-        
-        
+
+
     }
 
-    
+
      //Looping over Jet BXVector
-    for(int itBX=firstBxJet; itBX!=lastBxJet; ++itBX){
-       
-        
+    for(int itBX=firstBxJet; itBX!=lastBxJet+1; ++itBX){
+
+
         //looping over Jet elments with a specific BX
         L1TJetCollection::const_iterator itJet = Jet->begin(itBX);
         for(; itJet != Jet->end(itBX); ++itJet){
-            
+
             bool forward=(itJet->hwEta() < 4 || itJet->hwEta() > 17);
             L1GctJetCand JetCand(itJet->hwPt(), itJet->hwPhi(), itJet->hwEta(), false, forward,0, 0, itBX);     //L1GctJetCand(unsigned rank, unsigned phi, unsigned eta, bool isTau, bool isFor, uint16_t block, uint16_t index, int16_t bx);
             if(forward) forJetResult->push_back(JetCand);
@@ -145,17 +145,17 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
         }
     }
 
-    
-    
+
+
      //Looping over EtSum BXVector
-    for(int itBX=firstBxEtSum; itBX!=lastBxEtSum; ++itBX){
+    for(int itBX=firstBxEtSum; itBX!=lastBxEtSum+1; ++itBX){
         L1TEtSumCollection::const_iterator itEtSum = EtSum->begin(itBX);
     }
 
-    
-    
-    
-    
+
+
+
+
     //*isoEmResult =
     //this->ConvertToNonIsoEmCand(EGamma);
   //  DataFormatter.ConvertToNonIsoEmCand(*EGamma, nonIsoEmResult);
@@ -169,12 +169,12 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
   // DataFormatter.ConvertToHtMiss(EtSum,htMissResult);
   // DataFormatter.ConvertToHFBitCounts(EtSum,hfBitCountResult);
   // DataFormatter.ConvertToHFRingEtSums(EtSum, hfRingEtSumResult);
-    
+
   //  DataFormatter.ConvertToIntJet(Jet, internalJetResult);
   //  DataFormatter.ConvertToIntEtSum(EtSum,internalEtSumResult);
   //  DataFormatter.ConvertToIntHtMiss(EtSum,internalHtMissResult);
 
-    
+
 
   e.put(isoEmResult,"isoEm");
   e.put(nonIsoEmResult,"nonIsoEm");
@@ -191,8 +191,8 @@ l1t::L1TCaloUpgrateToGCTConverter::produce(Event& e, const EventSetup& es)
   e.put(internalJetResult);
   e.put(internalEtSumResult);
   e.put(internalHtMissResult);
-    
-    
+
+
 }
 
 // ------------ method called once each job just before starting event loop ------------
@@ -229,7 +229,3 @@ l1t::L1TCaloUpgrateToGCTConverter::fillDescriptions(ConfigurationDescriptions& d
   desc.setUnknown();
   descriptions.addDefault(desc);
 }
-
-
-
-
