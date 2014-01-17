a4e67e4a32c3d34bdada3c61725c1e87d906a945
Giuseppe <giuseppe.cerati@cern.ch>
2014-01-17 16:25:52 +0100

    update list of layers and add cfi

--- RecoTracker/IterativeTracking/python/PixelLessTripletStep_cff.py
+++ RecoTracker/IterativeTracking/python/PixelLessTripletStep_cff.py
@@ -11,21 +11,25 @@ pixelLessStepSeedLayers = cms.ESProducer("SeedingLayersESProducer",
     #TIB+TID
     'TIB1+TIB2+MTID1_pos','TIB1+TIB2+MTID1_neg',
     #TID
-    'TID1_pos+TID2_pos+MTID3_pos','TID1_neg+TID2_neg+MTID3_neg',#ring 3 (mono)
     'TID1_pos+TID2_pos+TID3_pos','TID1_neg+TID2_neg+TID3_neg',#ring 1-2 (matched)
+    'TID1_pos+TID2_pos+MTID3_pos','TID1_neg+TID2_neg+MTID3_neg',#ring 3 (mono)
+    'TID1_pos+TID2_pos+MTEC1_pos','TID1_neg+TID2_neg+MTEC1_neg',
     #TID+TEC RING 1-3
     'TID2_pos+TID3_pos+TEC1_pos','TID2_neg+TID3_neg+TEC1_neg',#ring 1-2 (matched)
-    'TID3_pos+TEC1_pos+TEC2_pos','TID3_neg+TEC1_neg+TEC2_neg',
     'TID2_pos+TID3_pos+MTEC1_pos','TID2_neg+TID3_neg+MTEC1_neg',#ring 3 (mono)
-    'TID3_pos+TEC1_pos+MTEC2_pos','TID3_neg+TEC1_neg+MTEC2_neg',
     #TEC RING 1-3
-    'TEC1_pos+TEC2_pos+TEC3_pos','TEC1_neg+TEC2_neg+TEC3_neg',
-    'TEC2_pos+TEC3_pos+TEC4_pos','TEC2_neg+TEC3_neg+TEC4_neg',
-    'TEC2_pos+TEC3_pos+TEC5_pos','TEC2_neg+TEC3_neg+TEC5_neg',
-    'TEC2_pos+TEC3_pos+TEC6_pos','TEC2_neg+TEC3_neg+TEC6_neg',
-    'TEC3_pos+TEC4_pos+TEC5_pos','TEC3_neg+TEC4_neg+TEC5_neg',
-    'TEC3_pos+TEC5_pos+TEC6_pos','TEC3_neg+TEC5_neg+TEC6_neg',
-    'TEC4_pos+TEC5_pos+TEC6_pos','TEC4_neg+TEC5_neg+TEC6_neg'
+    'TEC1_pos+TEC2_pos+TEC3_pos', 'TEC1_neg+TEC2_neg+TEC3_neg',
+    'TEC1_pos+TEC2_pos+MTEC3_pos','TEC1_neg+TEC2_neg+MTEC3_neg',
+    'TEC1_pos+TEC2_pos+TEC4_pos', 'TEC1_neg+TEC2_neg+TEC4_neg',
+    'TEC1_pos+TEC2_pos+MTEC4_pos','TEC1_neg+TEC2_neg+MTEC4_neg',
+    'TEC2_pos+TEC3_pos+TEC4_pos', 'TEC2_neg+TEC3_neg+TEC4_neg',
+    'TEC2_pos+TEC3_pos+MTEC4_pos','TEC2_neg+TEC3_neg+MTEC4_neg',
+    'TEC2_pos+TEC3_pos+TEC5_pos', 'TEC2_neg+TEC3_neg+TEC5_neg',
+    'TEC2_pos+TEC3_pos+TEC6_pos', 'TEC2_neg+TEC3_neg+TEC6_neg',
+    'TEC3_pos+TEC4_pos+TEC5_pos', 'TEC3_neg+TEC4_neg+TEC5_neg',
+    'TEC3_pos+TEC4_pos+MTEC5_pos','TEC3_neg+TEC4_neg+MTEC5_neg',
+    'TEC3_pos+TEC5_pos+TEC6_pos', 'TEC3_neg+TEC5_neg+TEC6_neg',
+    'TEC4_pos+TEC5_pos+TEC6_pos', 'TEC4_neg+TEC5_neg+TEC6_neg'    
     ),
     TIB = cms.PSet(
          TTRHBuilder    = cms.string('WithTrackAngle'),
