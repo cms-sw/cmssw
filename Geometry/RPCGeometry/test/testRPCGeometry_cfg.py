7e848c687184060ed00db7cfeef2d6ac5e95e758
Sven Dildick <sven.dildick@cern.ch>
2014-01-17 05:35:31 +0100

    2023HGCalMuon as default scenario

--- Geometry/RPCGeometry/test/testRPCGeometry_cfg.py
+++ Geometry/RPCGeometry/test/testRPCGeometry_cfg.py
@@ -1,13 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-process.load('Configuration.Geometry.GeometryExtended_cff')
+process.load('Configuration.Geometry.GeometryExtended2023HGCalMuon_cff')
+process.load('Configuration.Geometry.GeometryExtended2023HGCalMuonReco_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
-process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
 
-from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond['mc']
+process.load('FWCore.MessageLogger.MessageLogger_cfi')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
