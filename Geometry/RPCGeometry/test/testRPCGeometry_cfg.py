42f2a086d67692752055b482ec7c45519a54684d
Piet <piet.verwilligen@cern.ch>
2014-01-19 18:46:14 +0100

    loading right Extended2023RPCEtaUpscope geometry

--- Geometry/RPCGeometry/test/testRPCGeometry_cfg.py
+++ Geometry/RPCGeometry/test/testRPCGeometry_cfg.py
@@ -1,13 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
-process.load('Configuration.Geometry.GeometryExtended_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
-process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
 
-from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond['mc']
+# process.load('Configuration.Geometry.GeometryExtended_cff')
+# process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+# process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
+# process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
+
+# from Configuration.AlCa.autoCond import autoCond
+# process.GlobalTag.globaltag = autoCond['mc']
+
+process.load("Configuration.Geometry.GeometryExtended2023RPCEtaUpscope_cff")
+process.load('Configuration.Geometry.GeometryExtended2023RPCUpscopeReco_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+#process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
+#process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
