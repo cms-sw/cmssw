826c3bc7e8072fdad08c80a20457d9ecaf860f4a
michael <michael.northup@cern.ch>
2014-01-21 06:18:35 +0100

    Added CaloStage1SingleTrackHI class and implementation

---
+++ L1Trigger/L1TCalorimeter/src/firmware/CaloStage1SingleTrackHI.h
@@ -0,0 +1,22 @@
+#ifndef CaloStage1SingleTrackHI_h
+#define CaloStage1SingleTrackHI_h
+
+#include "L1Trigger/L1TCalorimeter/interface/CaloStage1TauAlgorithm.h"
+
+namespace l1t {
+  
+  class CaloStage1SingleTrackHI : CaloStage1TauAlgorithm { 
+  public:
+    CaloStage1SingleTrackHI();
+    virtual ~CaloStage1SingleTrackHI();
+    virtual void processEvent(/*const std::vector<l1t::CaloStage1Cluster> & clusters,*/
+			      const std::vector<l1t::CaloEmCand> & clusters,	
+                              const std::vector<l1t::CaloRegion> & regions,
+                              std::vector<l1t::Tau> & taus);    
+  
+  private:
+    double regionLSB_;
+ }; 
+   
+}
+#endif 
