29d5f33fb093a7bf16a278280d97e48592bb9b53
Chris <neu@cern.ch>
2014-01-21 19:53:49 -0600

    simple example

---
+++ Geometry/HGCalCommonData/plugins/DDShashlikSupermodule.h
@@ -0,0 +1,42 @@
+#ifndef HGCalCommonData_DDShashlikSupermodule_h
+#define HGCalCommonData_DDShashlikSupermodule_h
+
+#include <string>
+#include <vector>
+#include "DetectorDescription/Base/interface/DDTypes.h"
+#include "DetectorDescription/Algorithm/interface/DDAlgorithm.h"
+
+class DDShashlikSupermodule : public DDAlgorithm
+{
+public:
+  DDShashlikSupermodule( void ); 
+  virtual ~DDShashlikSupermodule( void );
+  
+  void initialize( const DDNumericArguments & nArgs,
+		   const DDVectorArguments & vArgs,
+		   const DDMapArguments & mArgs,
+		   const DDStringArguments & sArgs,
+		   const DDStringVectorArguments & vsArgs );
+
+  void execute( DDCompactView& cpv );
+
+private:
+
+  int           m_nRow;         // nRows in a supermodule
+  int           m_nCol;         // nCols in a supermodule
+  double        m_taperAngle;   // taper angle of individual Shashlik module
+
+
+  double        m_startAngle;   // Start angle 
+  double        m_stepAngle;    // Step  angle
+  int           m_invert;       // Inverted or forward
+  double        m_rPos;         // Radial position of center
+  double        m_zoffset;      // Offset in z
+  int           m_n;            // Mumber of copies
+  int           m_startCopyNo;  // Start copy Number
+  int           m_incrCopyNo;   // Increment copy Number
+  std::string   m_childName;    // Children name
+  std::string   m_idNameSpace;  // Namespace of this and ALL sub-parts
+};
+
+#endif
