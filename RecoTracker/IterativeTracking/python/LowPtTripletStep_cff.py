96ce3f0e51c7751a8004af9db864678f5bdf0f27
Giuseppe <giuseppe.cerati@cern.ch>
2014-01-23 11:42:24 +0100

    fix bugs here and there leading to higher fake rate for loose tracks

--- RecoTracker/IterativeTracking/python/LowPtTripletStep_cff.py
+++ RecoTracker/IterativeTracking/python/LowPtTripletStep_cff.py
@@ -120,7 +120,9 @@ lowPtTripletStepSelector = RecoTracker.FinalTrackSelectors.multiTrackSelector_cf
     GBRForestLabel = cms.string('MVASelectorIter1'),
     trackSelectors= cms.VPSet(
         RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.looseMTS.clone(
+        #RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.highpurityMTS.clone(
             name = 'lowPtTripletStepLoose',
+            #preFilterName = '',
             ), #end of pset
         RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.tightMTS.clone(
             name = 'lowPtTripletStepTight',
