a6a0efa8fef97e45f275483a76ca9ad313dc0313
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2014-01-23 19:35:46 +0100

    dileptonic ttbar

--- HLTriggerOffline/Top/python/topHLTValidation_cff.py
+++ HLTriggerOffline/Top/python/topHLTValidation_cff.py
@@ -1,15 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-#from HLTriggerOffline.Top.topDiLeptonHLTEventValidation_cfi import *
+from HLTriggerOffline.Top.topDiLeptonHLTEventValidation_cfi import *
 from HLTriggerOffline.Top.topSingleLeptonHLTEventValidation_cfi import *
 from HLTriggerOffline.Top.singletopHLTEventValidation_cfi import *
 
 topHLTriggerValidation = cms.Sequence(  
-#        DiMuonHLTValidation
-#        *DiElectronHLTValidation
-#        *ElecMuonHLTValidation
-#        *
-        topSingleMuonHLTValidation
+        DiMuonHLTValidation
+        *DiElectronHLTValidation
+        *ElecMuonHLTValidation
+        *topSingleMuonHLTValidation
         *topSingleElectronHLTValidation
         *SingleTopSingleMuonHLTValidation
         *SingleTopSingleElectronHLTValidation	
