cc7e3a347c3c195614cdd852302d0591e09deda6
Sven Dildick <sven.dildick@cern.ch>
2014-01-24 18:57:52 +0100

    Fix invalid syntax

--- SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
+++ SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
@@ -7,7 +7,7 @@ def customise(process):
         process=customise_RawToDigi(process)
     if hasattr(process,'reconstruction'):
         process=customise_Reco(process)
-   if hasattr(process,'L1simulation_step'):
+    if hasattr(process,'L1simulation_step'):
        process=customise_L1Emulator(process)
     if hasattr(process,'digitisation_step'):
         process=customise_Digi(process)
