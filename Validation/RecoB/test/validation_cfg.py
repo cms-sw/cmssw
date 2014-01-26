8bc06d3182654d0a06488244256b664f34b44cfd
Adrien <adrien.caudron@cern.ch>
2014-01-26 12:25:47 +0100

    Update b-tag validation cfg to use CHS jets by default.

--- Validation/RecoB/test/validation_cfg.py
+++ Validation/RecoB/test/validation_cfg.py
@@ -7,7 +7,7 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 options = VarParsing.VarParsing ('analysis')
 
 options.register ('jets',
-                  "ak5PF", # default value, allowed : "ak5PF", "ak5PFJEC", ak5PFJECL1", "ak5PFnoPU"
+                  "ak5PFCHS", # default value, allowed : "ak5PF", "ak5PFCHS"
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string,  
                   "jet collection to use")
@@ -17,7 +17,7 @@ options.parseArguments()
 whichJets  = options.jets 
 useTrigger = False
 runOnMC    = True
-tag =  'START60_V1::All'
+tag =  'START70_V4::All'
 
 ###prints###
 print "jet collcetion asked : ", whichJets
@@ -46,36 +46,15 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMOffline.RecoB.bTagSequences_cff")
 #process.bTagHLT.HLTPaths = ["HLT_PFJet80_v*"] #uncomment this line if you want to use different trigger
 
-if whichJets=="ak5PFnoPU":
-    process.out = cms.OutputModule("PoolOutputModule",
-                                   outputCommands = cms.untracked.vstring('drop *'),
-                                   fileName = cms.untracked.string('EmptyFile.root')
-                                   )
-    process.load("PhysicsTools.PatAlgos.patSequences_cff")
-    from PhysicsTools.PatAlgos.tools.pfTools import *
-    postfix="PF2PAT"
-    usePF2PAT(process,runPF2PAT=True, jetAlgo="AK5", runOnMC=runOnMC, postfix=postfix)
-    applyPostfix(process,"patJetCorrFactors",postfix).payload = cms.string('AK5PFchs')
-    process.pfPileUpPF2PAT.Vertices = cms.InputTag('goodOfflinePrimaryVertices')
-    process.pfPileUpPF2PAT.checkClosestZVertex = cms.bool(False)
-    from DQMOffline.RecoB.bTagSequences_cff import JetCut
-    process.selectedPatJetsPF2PAT.cut = JetCut
-    process.JECAlgo = cms.Sequence( getattr(process,"patPF2PATSequence"+postfix) )
-    newjetID=cms.InputTag("selectedPatJetsPF2PAT")
-elif whichJets=="ak5PFJEC" or whichJets=="ak5PFJECL1":
-    if whichJets=="ak5PFJECL1":
-        if not runOnMC : process.ak5PFJetsJEC.correctors = ['ak5PFL1FastL2L3Residual']
-        else : process.ak5PFJetsJEC.correctors = ['ak5PFL1FastL2L3']
-        process.PFJetsFilter.src = cms.InputTag("ak5PFJetsJEC")
-    process.JECAlgo = cms.Sequence(process.ak5PFJetsJEC * process.PFJetsFilter)
-    newjetID=cms.InputTag("PFJetsFilter")
-
-if not whichJets=="ak5PF":
-    process.myak5JetTracksAssociatorAtVertex.jets = newjetID
-    process.softPFMuonsTagInfos.jets             = newjetID
-    process.softPFElectronsTagInfos.jets          = newjetID
-    process.AK5byRef.jets                         = newjetID
-
+process.JECAlgo = cms.Sequence(process.ak5JetsJEC * process.PFJetsFilter)
+newjetID=cms.InputTag("PFJetsFilter")
+process.myak5JetTracksAssociatorAtVertex.jets = newjetID
+process.softPFMuonsTagInfos.jets              = newjetID
+process.softPFElectronsTagInfos.jets          = newjetID
+process.AK5byRef.jets                         = newjetID
+if whichJets=="ak5PF":
+    process.ak5JetsJEC.src = 'ak5PFJets'
+    process.ak5JetsJEC.correctors = ['ak5PFL1L2L3']
 ###
 print "inputTag : ", process.myak5JetTracksAssociatorAtVertex.jets
 ###
@@ -84,10 +63,10 @@ if runOnMC:
     process.load("Validation.RecoB.bTagAnalysis_cfi")
     process.bTagValidation.jetMCSrc = 'AK5byValAlgo'
     process.bTagValidation.allHistograms = True 
-    #process.bTagValidation.fastMC = True
     process.bTagValidation.applyPtHatWeight = False
     process.bTagValidation.flavPlots = "allbcl" #if contains "noall" plots for all jets not booked, if contains "dusg" all histograms booked, default : all, b, c, udsg, ni
-else:
+else :
+    process.ak5JetsJEC.correctors[0] += 'Residual'
     process.load("DQMOffline.RecoB.bTagAnalysisData_cfi")
 
 process.maxEvents = cms.untracked.PSet(
@@ -97,10 +76,7 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring()
 )
 
-if whichJets=="ak5PF":
-    process.jetSequences = cms.Sequence(process.goodOfflinePrimaryVertices * process.btagSequence)
-else:
-    process.jetSequences = cms.Sequence(process.goodOfflinePrimaryVertices * process.JECAlgo * process.btagSequence)
+process.jetSequences = cms.Sequence(process.goodOfflinePrimaryVertices * process.JECAlgo * process.btagSequence)
 
 if runOnMC:
     process.dqmSeq = cms.Sequence(process.flavourSeq * process.bTagValidation * process.dqmSaver)
