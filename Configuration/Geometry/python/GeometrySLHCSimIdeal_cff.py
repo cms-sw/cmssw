dfeeabcd841cbc0dec6f0cdef649a0580e45fc0f
Sunanda <sunanda.banerjee@cern.ch>
2014-01-26 08:41:09 +0100

    Update the geometry cff files to initiate a producer

--- Configuration/Geometry/python/GeometrySLHCSimIdeal_cff.py
+++ Configuration/Geometry/python/GeometrySLHCSimIdeal_cff.py
@@ -3,3 +3,4 @@ import FWCore.ParameterSet.Config as cms
 # Ideal geometry, needed for simulation
 from SLHCUpgradeSimulations.Geometry.Phase1_R30F12_cmsSimIdealGeometryXML_cff import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingSLHCGeometry_cfi import *
+from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
