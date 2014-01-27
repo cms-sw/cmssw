b19d57d7b0db7610edb36158a3a243c93ec43438
Chris <neu@cern.ch>
2014-01-27 16:11:28 -0600

    improved translations, rotations unchanged

--- Geometry/HGCalCommonData/plugins/DDShashlikSupermodule.cc
+++ Geometry/HGCalCommonData/plugins/DDShashlikSupermodule.cc
@@ -43,19 +43,46 @@ DDShashlikSupermodule::initialize(const DDNumericArguments & nArgs,
 void
 DDShashlikSupermodule::execute( DDCompactView& cpv )
 {
-    double phi    = m_startAngle - 2* m_stepAngle;
-    double xphi   = m_startAngle - 2* m_stepAngle;
     int    copyNo = m_startCopyNo;
-    double phideg = 0.0;
-    double phidegX = 0.0;
-    double theta  = 90.*CLHEP::deg;
-    double offsetX = - 2 * m_zoffset;
-    double offsetY = -2 * m_zoffset;
 
-    double thetaX = theta;
+
+    // Variables pertaining to rotations
+    // 
+    //
+    double phi    = m_startAngle - 2* m_stepAngle; //counting starts from edge, each module is 1*stepAngle from center, edge is 2 mods away
+    double xphi   = m_startAngle - 2* m_stepAngle; //counting starts from edge, each module is 1*stepAngle from center, edge is 2 mods away
+    double phideg = 0.0;
+    double xphideg = 0.0;
+    double theta  = 90.*CLHEP::deg;
+
+    // these do not change module-to-module. 
+    //  retaining initial values since they seem harmless
+    // are these choices or necessary consequences of the parent/child frame orientations?
     double phiX = 0.0;
     double phiY = theta;
     double phiZ = 3*theta;
+
+
+    // Variables pertaining to translations
+    // 
+    //
+    // define single-module translation in xyz center-to-center (c2c), using law of sines
+    double offsetXc2c = (sin(m_stepAngle)*(0.5*m_zoffset/sin(0.5*m_stepAngle)))*cos(0.5*m_stepAngle);
+    double offsetYc2c = (sin(m_stepAngle)*(0.5*m_zoffset/sin(0.5*m_stepAngle)))*cos(0.5*m_stepAngle);
+    double offsetZc2c = (sin(m_stepAngle)*(0.5*m_zoffset/sin(0.5*m_stepAngle)))*sin(0.5*m_stepAngle);
+ 
+    //  starting point for two-translation scheme
+    double offsetX  = -2.0 * offsetXc2c;
+    double offsetZ1 = -2.0 * offsetZc2c;
+
+    double offsetY  = -2.0 * offsetYc2c;
+    double offsetZ2 = -2.0 * offsetZc2c;
+
+
+//     edm::LogInfo("HGCalGeom") << "*****************";
+//     edm::LogInfo("HGCalGeom") << "*****************";
+//     edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule::execute: m_startAngle = " << m_startAngle/CLHEP::deg << " deg, m_stepAngle = " << m_stepAngle/CLHEP::deg << " deg, m_zoffset = " << m_zoffset; 
+//     edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule::execute: c2c = (" << offsetXc2c << "," << offsetYc2c << "," << offsetZc2c << ")"; 
 
     for( int iy = 0; iy < 5; ++iy )
     {
@@ -63,7 +90,7 @@ DDShashlikSupermodule::execute( DDCompactView& cpv )
       for( int ix = 0; ix < 5; ++ix )
       {
 	phideg = phi / CLHEP::deg;
-	phidegX = xphi / CLHEP::deg;
+	xphideg = xphi / CLHEP::deg;
 
 	DDRotation rotation;
 	std::string rotstr( "NULL" );
@@ -73,125 +100,98 @@ DDShashlikSupermodule::execute( DDCompactView& cpv )
 	rotstr = "R"; 
 	rotstrX = "RX"; 
 	rotstr  += dbl_to_string( phideg * 10.);
-	rotstrX  += dbl_to_string( phidegX * 10.);
+	rotstrX  += dbl_to_string( xphideg * 10.);
 	rotation = DDRotation( DDName( rotstr + rotstrX ));
 	if( !rotation )
 	{
+
+// 	  edm::LogInfo("HGCalGeom") << "Module " << copyNo << ": first: (" 
+// 				    << theta/CLHEP::deg << ", " 
+// 				    << phiX/CLHEP::deg << ", "
+// 				    << (theta+phi)/CLHEP::deg << ", " 
+// 				    << phiY/CLHEP::deg << ", "
+// 				    << -1.0*phi/CLHEP::deg << ", " 
+// 				    << phiZ/CLHEP::deg << ")";
+// 	  edm::LogInfo("HGCalGeom") << "Module " << copyNo << ": second: (" 
+// 				    << (theta+xphi)/CLHEP::deg << ", " 
+// 				    << 0.0/CLHEP::deg << ", "
+// 				    << 90. << ", " 
+// 				    << 90. << ", "
+// 				    << xphi/CLHEP::deg << ", " 
+// 				    << 0.0/CLHEP::deg << ")"; 
+
+
+	  // why is phi rot in z in second matrix not phiZ instead of 0.0?
  	  rotation = DDrot( DDName( rotstr + rotstrX, m_idNameSpace ),
-			    new DDRotationMatrix( *DDcreateRotationMatrix( thetaX, phiX, theta + phi, phiY, -phi, phiZ )
-						  * ( *DDcreateRotationMatrix( theta + xphi, 0, 90.*CLHEP::deg, 90.*CLHEP::deg, xphi, 0 ))));
+			    new DDRotationMatrix( *DDcreateRotationMatrix( theta, phiX, theta + phi, phiY, -phi, phiZ )
+						  * ( *DDcreateRotationMatrix( theta + xphi, phiX, 90.*CLHEP::deg, 90.*CLHEP::deg, xphi, 0.0 ))));
 	}
 
-	DDTranslation tran( offsetX, offsetY, 0.0 );
+	// Translation
+	//
+	// we will do two translations for each module, one in xz and one in yz
+	
+// 	edm::LogInfo("HGCalGeom") << "Module " << copyNo << ":tran1 ("
+// 				  << offsetX << ","
+// 				  << 0.0 << ","
+// 				  << offsetZ1 << ")";
+// 	edm::LogInfo("HGCalGeom") << "Module " << copyNo << ":tran2 ("
+// 				  << 0.0 << ","
+// 				  << offsetY << ","
+// 				  << offsetZ2 << ")";
+	
+// 	edm::LogInfo("HGCalGeom") << "Module " << copyNo << ":tran1+2 ("
+// 				  << offsetX << ","
+// 				  << offsetY << ","
+// 				  << offsetZ1+offsetZ2 << ")";
+
+			  
+	DDTranslation tran1( offsetX, 0.0, offsetZ1 );
+ 	DDTranslation tran2( 0.0, offsetY, offsetZ2 );
     
 	DDName parentName = parent().name(); 
-	cpv.position( DDName( m_childName ), parentName, copyNo, tran, rotation );
+	cpv.position( DDName( m_childName ), parentName, copyNo, (tran1+tran2), rotation );
+
+	// these are hard-coded for now
+	//
+	//
+	if(copyNo%5 <= 2 && copyNo%5 > 0)
+	  offsetZ1 += offsetZc2c;
+	else 
+	  offsetZ1 -= offsetZc2c;
+
 
 	copyNo += m_incrCopyNo;
-	offsetX += m_zoffset;
+	offsetX += offsetXc2c;
+
+	  
+
 	xphi += m_stepAngle;
       }
+
       xphi    = - 2* m_stepAngle;
-      offsetX = - 2 * m_zoffset;      
-      offsetY += m_zoffset;
       phi    += m_stepAngle;
+ 
+
+      offsetX = - 2.0 * offsetXc2c;      
+      offsetY += offsetYc2c;
+      offsetZ1 = -2.0 * offsetZc2c;
+
+
+      // these magic numbers are hard-coded for now
+      //
+      //
+      if(copyNo == 6)
+	offsetZ2 += offsetZc2c;
+      if(copyNo == 11)
+	offsetZ2 += offsetZc2c;
+      if(copyNo == 16)
+	offsetZ2 -= offsetZc2c;
+      if(copyNo == 21)
+	offsetZ2 -= offsetZc2c;
+
+
     }
-
-  //   // Brute force
-
-  //   // x24 x23 x22 x21 x20    ^ y
-  //   // x19 x18 x17 x16 x15    |
-  //   // x14 x13 x12 x11 x10    |
-  //   //  x9  x8  x7  x6 x5     -----> z
-  //   //  x4  x3  x2  x1 x0
-
-  //   //ccn: change these to access widthFront and widthBack and Thickness
-  //   double PI = 4.0 * atan(1.0);
-  //   double TWOPI = 2.0*PI;
-  //   long double MODLENGTH = 113.5;
-  //   long double MODINNERSIDE = 14.0;
-  //   long double MODMIDSIDE = 14.28;
-  //   long double MODOUTERSIDE = 14.56;
-
-  //   long double taper_angle = atan((MODOUTERSIDE-MODINNERSIDE)*0.5 / MODLENGTH);
-  //   long double taper_angle_deg = taper_angle / CLHEP::deg;
-  //  edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule:: taper_angle = " << taper_angle << " rad (" << taper_angle_deg << " deg)"; 
-
-  //   long double f2f_rotation = 2.0*taper_angle;
-  //   //long double f2f_rotation = taper_angle;
-  //   long double f2f_rotation_deg = f2f_rotation / CLHEP::deg;
-  //   edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule:: f2f_rotation = " << f2f_rotation << " rad (" << f2f_rotation_deg << " deg)"; 
-
-  //   long double f2f_angle = TWOPI - (PI + 2.0*taper_angle);
-  //   long double f2f_angle_deg = f2f_angle / CLHEP::deg;
-  //   edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule:: f2f_angle = " << f2f_angle << " rad (" << f2f_angle_deg << " deg)"; 
-
-  //   // law of sines to get distance from face-to-face
-  //   long double f2f_dist = sin(f2f_angle)*(0.5*MODINNERSIDE/sin(taper_angle));
-  //   edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule:: f2f_dist = " << f2f_dist << " mm";
-
-  //   long double c2c_dist = sin(f2f_angle)*(0.5*MODMIDSIDE/sin(taper_angle));
-  //   edm::LogInfo("HGCalGeom") << "DDShashlikSuperModule:: c2c_dist = " << c2c_dist << " mm";
-
-
-  //   // module 12
-  //   //
-  //   phi = 0.0;
-  //   phideg = phi / CLHEP::deg;
-    
-  //   DDRotation rotation;
-  //   std::string rotstr( "NULL" );
-
-  //   // Check if we've already created the rotation matrix
-  //   rotstr = "R"; 
-  //   rotstr  += dbl_to_string( phideg );
-  //   rotation = DDRotation( DDName( rotstr ));
-  //   if( !rotation )
-  //     {
-  //       double thetax = 90.0;
-  //       double phix   = m_invert == 0 ? ( 90.0 + phideg ) : ( -90.0 + phideg );
-  //       double thetay = m_invert == 0 ? 0.0 : 180.0;
-  //       double phiz   = phideg;
-  //       rotation = DDrot( DDName( rotstr ), thetax*CLHEP::deg, 
-  // 			phix*CLHEP::deg, thetay*CLHEP::deg, 0*CLHEP::deg,
-  // 			thetax*CLHEP::deg, phiz*CLHEP::deg);
-  //     }
-
-  //   // ccn: m_rPos = 0.0 and m_zoffset = 0.0 here, so these thre args are each 0.0
-  //   DDTranslation tran( (m_rPos * cos( phideg*CLHEP::deg)), (m_rPos * sin( phideg*CLHEP::deg )), m_zoffset );
-    
-  //   DDName parentName = parent().name(); 
-  //   cpv.position( DDName( m_childName ), parentName, 12, tran, rotation );
-
-
-  
-  //   // module 17
-  //   //
-  //   phideg = f2f_rotation_deg;    
-  //   //phideg = -2.0;
-
-  //   // Check if we've already created the rotation matrix
-  //   rotstr = "R"; 
-  //   rotstr  += dbl_to_string( phideg );
-  //   rotation = DDRotation( DDName( rotstr ));
-  //   if( !rotation )
-  //     {
-  //       double thetax = 90.0;
-  //       double phix   = m_invert == 0 ? ( 90.0 + phideg ) : ( -90.0 + phideg );
-  //       double thetay = m_invert == 0 ? 0.0 : 180.0;
-  //       double phiz   = phideg;
-  //       rotation = DDrot( DDName( rotstr ), thetax*CLHEP::deg, 
-  // 			phix*CLHEP::deg, thetay*CLHEP::deg, 0*CLHEP::deg,
-  // 			thetax*CLHEP::deg, phiz*CLHEP::deg);
-  //     }
-  
-  //   tran = DDTranslation( (-1.0*c2c_dist*sin(taper_angle) + m_rPos * cos( phideg*CLHEP::deg)), (c2c_dist*cos(taper_angle) + m_rPos * sin( phideg*CLHEP::deg )), (m_zoffset) );
-
-  //   //tran = DDTranslation( (-1.0*c2c_dist*sin(taper_angle)), (c2c_dist*cos(taper_angle)),0.0 );
-
-  //   parentName = parent().name(); 
-  //   cpv.position( DDName( m_childName ), parentName, 17, tran, rotation );
-  
 
 }
 
