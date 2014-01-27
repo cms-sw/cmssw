c7bd4595c355ed782e536696ff02611e81baf7ec
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-01-27 15:51:00 +0100

    Migrate SeedingLayerSetsHits and SeedingLayersEDProducer to the new interface of SeedingLayerSetsBuilder

--- RecoTracker/TkSeedingLayers/plugins/SeedingLayersEDProducer.cc
+++ RecoTracker/TkSeedingLayers/plugins/SeedingLayersEDProducer.cc
@@ -19,7 +19,6 @@ public:
 
 private:
   SeedingLayerSetsBuilder builder_;
-  ctfseeding::SeedingLayerSets cachedLayerSets_;
 };
 
 SeedingLayersEDProducer::SeedingLayersEDProducer(const edm::ParameterSet& iConfig):
@@ -31,26 +30,16 @@ SeedingLayersEDProducer::~SeedingLayersEDProducer() {}
 
 void SeedingLayersEDProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   if(builder_.check(iSetup)) {
-    cachedLayerSets_ = builder_.layers(iSetup);
-  }
-
-  // Ensure all SeedingLayers objects have same number of SeedingLayer's
-  unsigned int nlayers = cachedLayerSets_[0].size();
-  for(size_t i=0; i<cachedLayerSets_.size(); ++i) {
-    if(nlayers != cachedLayerSets_[i].size())
-      throw cms::Exception("Configuration") << "Assuming all SeedingLayers to have same number of layers, Layers " << i << " has " << cachedLayerSets_[i].size() << " while 0th has " << nlayers;
+    builder_.updateEventSetup(iSetup);
   }
 
   // Get hits
-  std::auto_ptr<SeedingLayerSetsHits> prod(new SeedingLayerSetsHits(nlayers));
-  for(const ctfseeding::SeedingLayers& layers: cachedLayerSets_) {
-    for(const ctfseeding::SeedingLayer& layer: layers) {
-      std::pair<unsigned int, bool> index = prod->insertLayer(layer.name(), layer.detLayer());
-      if(index.second) {
-        // layer was really inserted, we have to pass also the hits
-        prod->insertLayerHits(index.first, layer.hits(iEvent, iSetup));
-      }
-    }
+  std::auto_ptr<SeedingLayerSetsHits> prod(new SeedingLayerSetsHits(builder_.numberOfLayersInSet(),
+                                                                    &builder_.layerSetIndices(),
+                                                                    &builder_.layerNames(),
+                                                                    builder_.layerDets()));
+  for(unsigned short i=0; i<builder_.numberOfLayers(); ++i) {
+    prod->setHits(i, builder_.hits(iEvent, iSetup, i));
   }
   //prod->print();
 
