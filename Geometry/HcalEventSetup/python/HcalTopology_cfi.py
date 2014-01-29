db91d14a4f5414e2e076889d87b164d90db5f577
Ianna Osborne <ianna.osborne@cern.ch>
2014-01-29 11:15:09 +0100

    Add dependent numbering record to HcalTopology configuration.

--- Geometry/HcalEventSetup/python/HcalTopology_cfi.py
+++ Geometry/HcalEventSetup/python/HcalTopology_cfi.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from Geometry.HcalCommonData.hcalRecNumberingInitialization_cfi import *
+
 import Geometry.HcalEventSetup.hcalTopologyIdeal_cfi
 
 hcalTopologyIdeal = Geometry.HcalEventSetup.hcalTopologyIdeal_cfi.hcalTopologyIdeal.clone()
