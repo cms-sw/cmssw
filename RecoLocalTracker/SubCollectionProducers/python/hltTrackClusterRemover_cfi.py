fd57470668058fa575341b63977a8e1aba88307e
Mia <mia.tosi@cern.ch>
2014-01-29 11:46:23 +0100

    synchronize HLTTrackClusterRemover to the offline version w/ SiStripClusterChargeCut

--- RecoLocalTracker/SubCollectionProducers/python/hltTrackClusterRemover_cfi.py
+++ RecoLocalTracker/SubCollectionProducers/python/hltTrackClusterRemover_cfi.py
@@ -1,11 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 hltTrackClusterRemover = cms.EDProducer( "HLTTrackClusterRemover",
-   trajectories = cms.InputTag( "hltPFlowTrackSelectionHighPurity" ),
-   doStrip = cms.bool( True ),
-   doPixel = cms.bool( True ),
-   stripClusters = cms.InputTag( "hltSiStripRawToClustersFacility" ),
-   pixelClusters = cms.InputTag( "hltSiPixelClusters" ),
+   trajectories          = cms.InputTag( "hltPFlowTrackSelectionHighPurity" ),
+   doStrip               = cms.bool( True ),
+   doPixel               = cms.bool( True ),
+   stripClusters         = cms.InputTag( "hltSiStripRawToClustersFacility" ),
+   pixelClusters         = cms.InputTag( "hltSiPixelClusters" ),
    oldClusterRemovalInfo = cms.InputTag( "" ),
-   Common = cms.PSet(  maxChi2 = cms.double( 9.0 ) )
+   Common = cms.PSet(
+      maxChi2            = cms.double( 9.0 ),
+      minGoodStripCharge = cms.double(60.0)
+   ),
+   doStripChargeCheck = cms.bool(True),
 )
