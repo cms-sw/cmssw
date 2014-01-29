81e761e8d333319f704052852de12adb759e4e61
Martin Grunewald <Martin.Grunewald@cern.ch>
2014-01-29 12:02:16 +0100

    More cleanup

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -24,52 +24,6 @@ aliases = {
     'BASEGT' : 'BASE1_V1::All|BASE2_V1::All'
 }
 
-# L1 configuration used during Run2012A
-conditions_L1_Run2012A = (
-    # L1 GT menu 2012 v0, used during Run2012A
-    'L1GtTriggerMenu_L1Menu_Collisions2012_v0_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 GCT configuration without jet seed threshold, used up to Run2012B
-    'L1GctJetFinderParams_GCTPhysics_2011_09_01_B_mc,L1GctJetFinderParamsRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HfRingEtScale_GCTPhysics_2011_09_01_B_mc,L1HfRingEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HtMissScale_GCTPhysics_2011_09_01_B_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1JetEtScale_GCTPhysics_2011_09_01_B_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 CSCTF configuration used up to Run2012A
-    'L1MuCSCPtLut_key-10_mc,L1MuCSCPtLutRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 DTTF settings used up to Run2012B
-    'L1MuDTTFParameters_dttf11_TSC_09_17_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-)
-
-# L1 configuration used during Run2012B
-conditions_L1_Run2012B = (
-    # L1 GT menu 2012 v1, used during Run2012B
-    'L1GtTriggerMenu_L1Menu_Collisions2012_v1a_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 GCT configuration without jet seed threshold, used up to Run2012B
-    'L1GctJetFinderParams_GCTPhysics_2011_09_01_B_mc,L1GctJetFinderParamsRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HfRingEtScale_GCTPhysics_2011_09_01_B_mc,L1HfRingEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HtMissScale_GCTPhysics_2011_09_01_B_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1JetEtScale_GCTPhysics_2011_09_01_B_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 CSCTF configuration used since Run2012B
-    'L1MuCSCPtLut_key-11_mc,L1MuCSCPtLutRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 DTTF settings used up to Run2012B
-    'L1MuDTTFParameters_dttf11_TSC_09_17_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-)
-
-# L1 configuration used during Run2012C
-conditions_L1_Run2012C = (
-    # L1 GT menu 2012 v2, used during Run2012C
-    'L1GtTriggerMenu_L1Menu_Collisions2012_v2_mc,L1GtTriggerMenuRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 GCT configuration with 5 GeV jet seed threshold, used since Run2012C
-    'L1GctJetFinderParams_GCTPhysics_2012_04_27_JetSeedThresh5GeV_mc,L1GctJetFinderParamsRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HfRingEtScale_GCTPhysics_2012_04_27_JetSeedThresh5GeV_mc,L1HfRingEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1HtMissScale_GCTPhysics_2012_04_27_JetSeedThresh5GeV_mc,L1HtMissScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    'L1JetEtScale_GCTPhysics_2012_04_27_JetSeedThresh5GeV_mc,L1JetEtScaleRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 CSCTF configuration used since Run2012B
-    'L1MuCSCPtLut_key-11_mc,L1MuCSCPtLutRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-    # L1 DTTF settings used since Run2012C
-    'L1MuDTTFParameters_dttf12_TSC_03_csc_col_mc,L1MuDTTFParametersRcd,frontier://FrontierProd/CMS_COND_31X_L1T',
-)
-
-
 # L1 configuration used during Run2012D
 conditions_L1_Run2012D = (
     # L1 GT menu 2012 v3, used during Run2012D
@@ -126,8 +80,8 @@ autoCond['starthi_HIon']     = ( autoCond['starthi'], ) \
                              + conditions_L1_HIRun2011 \
                              + conditions_HLT_JECs
 
-autoCond['startup_PIon']     = ( autoCond['startpa'], )
-
+autoCond['startup_PIon']     = ( autoCond['startpa'], ) \
+                             + conditions_L1_PARun2013
 
 # dedicated GlobalTags for running the frozen HLT menus on data
 autoCond['hltonline_8E33v2'] = ( autoCond['hltonline'], ) \
