178ad0edff069230c0dd9300a3056e4e84d5e310
Ianna Osborne <ianna.osborne@cern.ch>
2014-01-31 11:51:43 +0100

    Place Shahslik in full geometry.

--- Geometry/HGCalCommonData/plugins/module.cc
+++ Geometry/HGCalCommonData/plugins/module.cc
@@ -1,6 +1,8 @@
 #include "Geometry/HGCalCommonData/plugins/DDShashlikModule.h"
 #include "Geometry/HGCalCommonData/plugins/DDShashlikSupermodule.h"
+#include "Geometry/HGCalCommonData/plugins/DDShashlikEndcap.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDShashlikModule, "shashlik:DDShashlikModule");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDShashlikSupermodule, "shashlik:DDShashlikSupermodule");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDShashlikEndcap, "shashlik:DDShashlikEndcap");
