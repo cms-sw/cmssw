d919e2bcb8be5d4dbeee000913dbd6b08490e5ce
Sven Dildick <sven.dildick@cern.ch>
2014-02-02 23:07:39 +0100

    add GEMDigi dataformat

--- DataFormats/GEMDigi/src/classes.h
+++ DataFormats/GEMDigi/src/classes.h
@@ -4,6 +4,9 @@
 #include <DataFormats/GEMDigi/interface/GEMCSCPadDigi.h>
 #include <DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h>
 
+#include <DataFormats/ME0Digi/interface/ME0Digi.h>
+#include <DataFormats/ME0Digi/interface/ME0DigiCollection.h>
+
 #include <DataFormats/Common/interface/Wrapper.h>
 #include <vector>
 
@@ -21,5 +24,11 @@ namespace{
     std::vector<std::vector<GEMCSCPadDigi> >  vvgc;
     GEMCSCPadDigiCollection gccol;
     edm::Wrapper<GEMCSCPadDigiCollection> wgc;
+
+    ME0Digi g;
+    std::vector<ME0Digi>  vg;
+    std::vector<std::vector<ME0Digi> >  vvg;
+    ME0DigiCollection gcol;
+    edm::Wrapper<ME0DigiCollection> wg;
   };
 }
