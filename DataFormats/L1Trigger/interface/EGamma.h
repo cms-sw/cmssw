83091431074cb994f48466e13421b74a1f493383
Sam Harper <sam.j.harper@gmail.com>
2014-02-03 18:18:28 +0100

    allowing EGamma to be created by coping a L1Candidate

--- DataFormats/L1Trigger/interface/EGamma.h
+++ DataFormats/L1Trigger/interface/EGamma.h
@@ -21,7 +21,9 @@ namespace l1t {
 	    int phi=0,
 	    int qual=0,
 	    int iso=0);
-    
+    EGamma(const L1Candidate& rhs):L1Candidate(rhs){} //this is okay given that EGamma currently has no additional data members 
+                                                      //but need to add a check for rhs being an EGamma if this changes
+
     ~EGamma();		
 
     
