190a59c73f4c4bd450beda803b8ea62bae141552
Piet <piet.verwilligen@cern.ch>
2014-02-03 17:57:22 +0100

    load extended2023 geom

--- Geometry/RPCGeometry/test/rpcgeo.py
+++ Geometry/RPCGeometry/test/rpcgeo.py
@@ -5,9 +5,10 @@ process = cms.Process("Demo")
 # process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
 # process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
-process.load("Configuration.Geometry.GeometryExtended2023RPCUpscope_2p4_64_cff")
-process.load('Configuration.Geometry.GeometryExtended2023RPCUpscopeReco_2p4_64_cff')
-
+process.load("Configuration.Geometry.GeometryExtended2023_cff")
+process.load("Configuration.Geometry.GeometryExtended2023Reco_cff")
+# process.load("Configuration.Geometry.GeometryExtended2023RPCUpscope_2p4_192_cff")
+# process.load('Configuration.Geometry.GeometryExtended2023RPCUpscopeReco_2p4_192_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
 #process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
