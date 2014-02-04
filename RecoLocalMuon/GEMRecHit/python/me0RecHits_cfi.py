bab8b024bc2d45b9c80d1d2e815982342cee6154
Marcello <marcello.maggi@cern.ch>
2014-02-04 09:26:54 +0100

    me0 rechit py fragment

---
+++ RecoLocalMuon/GEMRecHit/python/me0RecHits_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+me0RecHits = cms.EDProducer("ME0RecHitProducer",
+    recAlgoConfig = cms.PSet(
+
+    ),
+    recAlgo = cms.string('ME0RecHitStandardAlgo'),
+    me0DigiLabel = cms.InputTag("muonME0Digis"),
+)
+
+
