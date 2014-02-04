1bbe956e186c1b9ef379c335ced446bc681ca9a3
Marcello <marcello.maggi@cern.ch>
2014-02-04 09:25:12 +0100

    add ME0Digis

--- DataFormats/GEMDigi/src/classes.h
+++ DataFormats/GEMDigi/src/classes.h
@@ -1,11 +1,10 @@
 #include <DataFormats/GEMDigi/interface/GEMDigi.h>
 #include <DataFormats/GEMDigi/interface/GEMDigiCollection.h>
+#include <DataFormats/GEMDigi/interface/ME0DigiPreReco.h>
+#include <DataFormats/GEMDigi/interface/ME0DigiPreRecoCollection.h>
 
 #include <DataFormats/GEMDigi/interface/GEMCSCPadDigi.h>
 #include <DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h>
-
-#include <DataFormats/GEMDigi/interface/ME0Digi.h>
-#include <DataFormats/GEMDigi/interface/ME0DigiCollection.h>
 
 #include <DataFormats/Common/interface/Wrapper.h>
 #include <vector>
@@ -25,10 +24,11 @@ namespace{
     GEMCSCPadDigiCollection gccol;
     edm::Wrapper<GEMCSCPadDigiCollection> wgc;
 
-    ME0Digi m;
-    std::vector<ME0Digi>  vm;
-    std::vector<std::vector<ME0Digi> >  vvm;
-    ME0DigiCollection mcol;
-    edm::Wrapper<ME0DigiCollection> wm;
+    ME0DigiPreReco m;
+    std::vector<ME0DigiPreReco>  vm;
+    std::vector<std::vector<ME0DigiPreReco> >  vvm;
+    ME0DigiPreRecoCollection mcol;
+    edm::Wrapper<ME0DigiPreRecoCollection> wm;
+
   };
 }
