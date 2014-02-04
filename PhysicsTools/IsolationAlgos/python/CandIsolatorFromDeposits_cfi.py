58b4da822bb349f216c475cfa4d949f601ca2e01
sara <sara.fiorendi@cern.ch>
2014-02-04 18:29:29 +0100

    modify input parameters

--- PhysicsTools/IsolationAlgos/python/CandIsolatorFromDeposits_cfi.py
+++ PhysicsTools/IsolationAlgos/python/CandIsolatorFromDeposits_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-CandIsolatorFromDeposits = cms.EDProducer("CandIsolatorFromDeposits",
+hltMuPFSumDRIsoValueCharged03 = cms.EDProducer("CandIsolatorFromDeposits",
      deposits = cms.VPSet(
              cms.PSet(
-             src = cms.InputTag("hltMuPFIsoDepositCharged"),
-             deltaR = cms.double(0.3),
+             src = cms.InputTag("ISODEPOSIT_COLLECTION"), # input collection of type reco::IsoDepositMap
+             deltaR = cms.double(99999.), #cone size
              weight = cms.string('1'),
              vetos = cms.vstring('0.0001','Threshold(0.0)'),
              skipDefaultVeto = cms.bool(True),
