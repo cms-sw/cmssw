e40db6ab1f5d896891edb9f30c4370fbe010a009
Marcello <marcello.maggi@cern.ch>
2014-02-04 09:22:40 +0100

    remove ME0 explicit subpackages

--- SimMuon/ME0Digitizer/src/ME0DigiModelFactory.cc
+++
@@ -1,4 +0,0 @@
-#include "FWCore/PluginManager/interface/PluginFactory.h"
-
-#include "SimMuon/ME0Digitizer/interface/ME0DigiModelFactory.h"
-EDM_REGISTER_PLUGINFACTORY(ME0DigiModelFactory, "ME0DigiModelFactory");
