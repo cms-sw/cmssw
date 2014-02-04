081e2db7f1059dcc3c8af4a519a2cffc7b64f2f1
Bill <wtford@colorado.edu>
2014-02-04 01:32:57 +0100

    Force (via bool) simHit matching without CF pointers from digiSimLinks

--- SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h
+++ SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h
@@ -74,13 +74,15 @@ class TrackerHitAssociator {
   //  std::vector<SimHitIdpr> associateSimpleRecHit(const SiStripRecHit2D * simplerechit);
   void associateSimpleRecHit(const SiStripRecHit2D * simplerechit,std::vector<SimHitIdpr> & simhitid);
   void associateSiStripRecHit1D(const SiStripRecHit1D * simplerechit,std::vector<SimHitIdpr> & simhitid);
+  /*
   void associateSimpleRecHitCluster(const SiStripCluster* clust,
 				    const uint32_t& detID,
 				    std::vector<SimHitIdpr>& theSimtrackid,
-				    std::vector<PSimHit>& simhit);
+				    std::vector<PSimHit>& clusterSimHits);
   void associateSimpleRecHitCluster(const SiStripCluster* clust,
 				    const uint32_t& detID,
-				    std::vector<PSimHit>& simhit);
+				    std::vector<PSimHit>& clusterSimHits);
+  */
   void associateSimpleRecHitCluster(const SiStripCluster* clust,
 				    const uint32_t& detID,
 				    std::vector<SimHitIdpr>& simtrackid);
@@ -98,6 +100,7 @@ class TrackerHitAssociator {
   typedef std::map<unsigned int, std::vector<PSimHit> > simhit_map;
   typedef simhit_map::iterator simhit_map_iterator;
   simhit_map SimHitMap;
+  simhit_map SimHitSubdetMap;
   std::vector<PSimHit> thePixelHits;
  
  private:
@@ -108,7 +111,7 @@ class TrackerHitAssociator {
   //ADDED NOW AS A PRIVATE MEMBER
   edm::Handle<CrossingFrame<PSimHit> > cf_simhit;
   std::vector<const CrossingFrame<PSimHit> *> cf_simhitvec;
-  MixCollection<PSimHit>  TrackerHits;
+  /* MixCollection<PSimHit>  TrackerHits; */
 
   edm::Handle< edm::DetSetVector<StripDigiSimLink> >  stripdigisimlink;
   edm::Handle< edm::DetSetVector<PixelDigiSimLink> >  pixeldigisimlink;
@@ -121,6 +124,7 @@ class TrackerHitAssociator {
   bool StripHits;
   
   bool doPixel_, doStrip_, doTrackAssoc_;
+  bool useCFpos_;
   
 };  
 
