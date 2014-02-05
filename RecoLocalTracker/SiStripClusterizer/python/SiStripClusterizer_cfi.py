81fdcb4c0bbab197b4cbac638d0fdd95fdcae137
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-02-05 11:02:18 +0100

    back to standard production config

--- RecoLocalTracker/SiStripClusterizer/python/SiStripClusterizer_cfi.py
+++ RecoLocalTracker/SiStripClusterizer/python/SiStripClusterizer_cfi.py
@@ -1,15 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-# from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_RealData_cfi import *
+from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_RealData_cfi import *
 
-from RecoLocalTracker.SiStripClusterizer.DefaultClusterizer_cff import *
-from RecoLocalTracker.SiStripZeroSuppression.DefaultAlgorithms_cff import *
-siStripClusters = cms.EDProducer("SiStripClusterizerFromRaw",
-                                                onDemand = cms.bool(True),
-                                                Clusterizer = DefaultClusterizer,
-                                                Algorithms = DefaultAlgorithms,
-                                                DoAPVEmulatorCheck = cms.bool(False),
-                                                ProductLabel = cms.InputTag('rawDataCollector')
-                                                )
+# from RecoLocalTracker.SiStripClusterizer.SiStripClusterizerOnDemand_cfi import *
 
 
