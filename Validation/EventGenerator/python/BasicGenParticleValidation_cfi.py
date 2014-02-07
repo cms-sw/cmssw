ed54cc5bbaf5e149d686386064624b449ffe449b
Salvatore <rappoccio@gmail.com>
2014-02-07 18:07:37 +0100

    Reverting to old AK5 until relvals are produced for AK4

--- Validation/EventGenerator/python/BasicGenParticleValidation_cfi.py
+++ Validation/EventGenerator/python/BasicGenParticleValidation_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 basicGenParticleValidation = cms.EDAnalyzer("BasicGenParticleValidation",
     hepmcCollection = cms.InputTag("generator",""),
     genparticleCollection = cms.InputTag("genParticles",""),
-    genjetsCollection = cms.InputTag("ak4GenJets",""),
+    genjetsCollection = cms.InputTag("ak5GenJets",""),
     matchingPrecision = cms.double(0.001),
     verbosity = cms.untracked.uint32(0),
     UseWeightFromHepMC = cms.bool(True)
--- Validation/EventGenerator/python/MBUEandQCDValidation_cfi.py
+++ Validation/EventGenerator/python/MBUEandQCDValidation_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 mbueAndqcdValidation = cms.EDAnalyzer("MBUEandQCDValidation",
     hepmcCollection = cms.InputTag("generator",""),
     genChjetsCollection = cms.InputTag("chargedak4GenJets",""),
-    genjetsCollection = cms.InputTag("ak4GenJets",""),
+    genjetsCollection = cms.InputTag("ak5GenJets",""),
     verbosity = cms.untracked.uint32(0),
     UseWeightFromHepMC = cms.bool(True)
 )
