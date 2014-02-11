b510e8ce7e41223c6aaa1519b6b7014881ed558e
Mia <mia.tosi@cern.ch>
2014-02-11 20:42:00 +0100

    adjust config files and make them back-compatible and make new parameters available (above all for confDB)

--- RecoTracker/TkSeedGenerator/python/SeedFromConsecutiveHitsStraightLineCreator_cfi.py
+++ RecoTracker/TkSeedGenerator/python/SeedFromConsecutiveHitsStraightLineCreator_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 SeedFromConsecutiveHitsStraightLineCreator = cms.PSet(
   ComponentName = cms.string('SeedFromConsecutiveHitsStraightLineCreator'),
-  propagator = cms.string('PropagatorWithMaterialParabolicMf'),
+  propagator = cms.string('PropagatorWithMaterial'),
+#  propagator = cms.string('PropagatorWithMaterialParabolicMf'),
   OriginTransverseErrorMultiplier = cms.double(1.0),
   MinOneOverPtError = cms.double(1.0)
 )
