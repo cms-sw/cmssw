fda567eb1334fba5f8efcf4b14d21c9581f07c4b
John <jstupak@fnal.gov>
2014-02-12 11:53:06 -0600

    updated to new/fast Qjet version

--- RecoJets/JetAlgorithms/interface/Qjets.h
+++ RecoJets/JetAlgorithms/interface/Qjets.h
@@ -19,30 +19,33 @@ struct jet_distance{
 class JetDistanceCompare{
  public:
   JetDistanceCompare(){};
-  bool operator() (const jet_distance& lhs, const jet_distance&rhs) const;
+  bool operator() (const jet_distance& lhs, const jet_distance&rhs) const {return lhs.dij > rhs.dij;};
 };
 
 class Qjets{
  private:
-  unsigned int _seed;
-  double _zcut,  _dcut_fctr, _exp_min, _exp_max, _rigidity,  _dcut;
   bool _rand_seed_set;
-  vector <int> _merged_jets;
-  list <jet_distance> _distances;
+  unsigned int _seed;
+  double _zcut, _dcut, _dcut_fctr, _exp_min, _exp_max, _rigidity, _truncation_fctr;
+  map<int,bool> _merged_jets;
+  priority_queue <jet_distance, vector<jet_distance>, JetDistanceCompare> _distances;
 
-  double d_ij(const fastjet::PseudoJet& v1, const fastjet::PseudoJet& v2);
+  double d_ij(const fastjet::PseudoJet& v1, const fastjet::PseudoJet& v2) const; 
   void ComputeDCut(fastjet::ClusterSequence & cs);
 
   double Rand();
   bool Prune(jet_distance& jd,fastjet::ClusterSequence & cs);
-  bool JetsUnmerged(jet_distance& jd);
-  bool JetUnmerged(int num);
-  void ComputeNewDistanceMeasures(fastjet::ClusterSequence & cs, int new_jet);
+  bool JetsUnmerged(const jet_distance& jd) const;
+  bool JetUnmerged(int num) const;
+  void ComputeNewDistanceMeasures(fastjet::ClusterSequence & cs, unsigned int new_jet);
   void ComputeAllDistances(const vector<fastjet::PseudoJet>& inp);  
   double ComputeMinimumDistance();
   double ComputeNormalization(double dmin);
+  jet_distance GetNextDistance();
+  bool Same(const jet_distance& lhs, const jet_distance& rhs);
+
  public:
-  Qjets(double zcut, double dcut_fctr, double exp_min, double exp_max, double rigidity);
+  Qjets(double zcut, double dcut_fctr, double exp_min, double exp_max, double rigidity, double truncation_fctr);
   void Cluster(fastjet::ClusterSequence & cs);
   void SetRandSeed(unsigned int seed); /* In case you want reproducible behavior */
 };
--- RecoJets/JetAlgorithms/src/QjetsPlugin.cc
+++ RecoJets/JetAlgorithms/src/QjetsPlugin.cc
@@ -1,9 +1,15 @@
 #include "RecoJets/JetAlgorithms/interface/QjetsPlugin.h"
 
-QjetsPlugin::QjetsPlugin(double zcut, double dcut_fctr, double exp_min, double exp_max, double rigidity)
-  : _zcut(zcut), _dcut_fctr(dcut_fctr), _exp_min(exp_min), _exp_max(exp_max), _rigidity(rigidity), _rand_seed_set(false)
+QjetsPlugin::QjetsPlugin(double zcut, double dcut_fctr, double exp_min, double exp_max, double rigidity, double truncation_fctr)
+  : _rand_seed_set(false),
+    _zcut(zcut), 
+    _dcut_fctr(dcut_fctr), 
+    _exp_min(exp_min), 
+    _exp_max(exp_max),
+    _rigidity(rigidity), 
+    _truncation_fctr(truncation_fctr)
 {
-}
+} 
 
 void QjetsPlugin::SetRandSeed(unsigned int seed){
   _rand_seed_set = true;
@@ -20,9 +26,8 @@ string QjetsPlugin::description() const{
 }
 
 void QjetsPlugin::run_clustering(fastjet::ClusterSequence & cs) const{
-  Qjets qjets(_zcut, _dcut_fctr, _exp_min, _exp_max, _rigidity);
+  Qjets qjets(_zcut, _dcut_fctr, _exp_min, _exp_max, _rigidity, _truncation_fctr);
   if(_rand_seed_set)
     qjets.SetRandSeed(_seed);
-  
   qjets.Cluster(cs);
 }
