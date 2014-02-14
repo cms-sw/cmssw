2a69deda4fed80148679755decf8dc089bf515a4
Chris <neu@cern.ch>
2014-02-14 15:04:10 -0600

    code for building simple shashlik endcaps

--- Geometry/HGCalCommonData/plugins/DDShashlikEndcap.cc
+++ Geometry/HGCalCommonData/plugins/DDShashlikEndcap.cc
@@ -44,9 +44,11 @@ DDShashlikEndcap::initialize(const DDNumericArguments & nArgs,
 			    << "\tParent " << parent().name();
 }
 
-void
-DDShashlikEndcap::execute( DDCompactView& cpv )
-{
+void 
+DDShashlikEndcap::createQuarter( DDCompactView& cpv, int xQuadrant, int yQuadrant) {
+
+  edm::LogInfo("HGCalGeom") << "DDShashlikEndcap::createQuarter test";
+
   int copyNo = m_startCopyNo;
   double phi = m_startAngle;
   double tiltAngle = m_tiltAngle;
@@ -56,16 +58,24 @@ DDShashlikEndcap::execute( DDCompactView& cpv )
   double phiX = 0.0;
   double phiY = theta;
   double phiZ = 3*theta; 
-  double offsetX = m_rMin;
-  double offsetY = m_xyoffset;
-  double offsetZ = m_zoffset;
   double angle = 0.0*CLHEP::deg;
-  double offsetLimitX = m_rMax;
-  double offsetLimitY = m_rMax;
-    
-  for( int iy = 0; offsetY < offsetLimitY; ++iy )
+  double transX = m_rMin + m_xyoffset/2.0;
+  double transY = m_xyoffset/2.0;
+  double transZ = m_zoffset;
+  double transLimitX = m_rMax;
+  double transLimitY = m_rMax;
+
+  if(xQuadrant==-1 && yQuadrant==1)
+    copyNo += 376;
+  if(xQuadrant==1 && yQuadrant==-1)
+    copyNo += 752;
+  if(xQuadrant==-1 && yQuadrant==-1)
+    copyNo += 1128;
+
+
+  for( int iy = 0; transY < transLimitY; ++iy )
   {
-    for( int ix = 0; offsetX < offsetLimitX; ++ix )
+    for( int ix = 0; transX < transLimitX; ++ix )
     {
       DDRotation rotation;
       std::string rotstr( "NULL" );
@@ -86,15 +96,27 @@ DDShashlikEndcap::execute( DDCompactView& cpv )
 			  new DDRotationMatrix( *DDcreateRotationMatrix( theta, phiX, theta + phi, phiY, -phi, phiZ )
 						* ( *DDcreateRotationMatrix( theta + xphi, phiX, 90.*CLHEP::deg, 90.*CLHEP::deg, xphi, 0.0 ))));
       }
-      
-      DDTranslation tran( offsetX, offsetY, offsetZ );
+
+      // Translation
+      //
+    //   edm::LogInfo("HGCalGeom") << "Module " << copyNo << ", quadrant (" 
+// 				<< xQuadrant << "," << yQuadrant
+// 				<< "), (ix,iy)=("
+// 				<< ix << "," << iy
+// 				<< "): transX=" << xQuadrant*transX 
+// 				<< ", transY=" << yQuadrant*transY
+// 				<< ", transZ=" << transZ;
+
+   
+      DDTranslation tran( xQuadrant*transX, yQuadrant*transY, transZ );
 	
       DDName parentName = parent().name(); 
+
       cpv.position( DDName( m_childName ), parentName, copyNo, tran, rotation );
       
-      offsetX += m_xyoffset;
-      if (sqrt(offsetX*offsetX + offsetY*offsetY) >= m_rMax)
-	offsetLimitX = offsetX;
+      transX += m_xyoffset;
+      if (sqrt(transX*transX + transY*transY) >= m_rMax)
+	transLimitX = transX;
 	
       copyNo += m_incrCopyNo;
       xphi += tiltAngle;     
@@ -103,16 +125,120 @@ DDShashlikEndcap::execute( DDCompactView& cpv )
     if( iy < 3 )
     {
       angle = m_startAngle + m_stepAngle * iy;
-      offsetX = m_rMin * cos( angle );
-      ( offsetX < 0.0 ) ? offsetX = 0.0 : offsetX;
+      transX = (m_rMin + m_xyoffset/2.0) * cos( angle );
+      ( transX < 0.0 ) ? transX = m_xyoffset/2.0 : transX;
     }
     else
-      offsetX = 0.0 ;
+      transX = m_xyoffset/2.0;
 
-    offsetY += m_xyoffset;
+    transY += m_xyoffset;
     xphi = 0.;
     yphi += tiltAngle;
     
   }
+
+
+}
+
+
+void
+DDShashlikEndcap::execute( DDCompactView& cpv )
+{
+
+
+
+  
+  for(int i=0; i<2; i++){
+    int yquad = (int)pow((-1.0),i);
+
+    for(int j=0; j<2; j++){
+ 
+      int xquad = (int)pow((-1.0),j);
+      createQuarter(cpv, xquad, yquad);
+
+    }
+  }
+ 
+
+  //createQuarter(cpv, 1, 1);
+
+
+//   int copyNo = m_startCopyNo;
+//   double phi = m_startAngle;
+//   double tiltAngle = m_tiltAngle;
+//   double xphi = 0.;
+//   double yphi = 0.;
+//   double theta  = 90.*CLHEP::deg;
+//   double phiX = 0.0;
+//   double phiY = theta;
+//   double phiZ = 3*theta; 
+//   double offsetX = m_rMin;
+//   double offsetY = m_xyoffset;
+//   double offsetZ = m_zoffset;
+//   double angle = 0.0*CLHEP::deg;
+//   double offsetLimitX = m_rMax;
+//   double offsetLimitY = m_rMax;
+    
+//   for( int iy = 0; offsetY < offsetLimitY; ++iy )
+//   {
+//     for( int ix = 0; offsetX < offsetLimitX; ++ix )
+//     {
+//       DDRotation rotation;
+//       std::string rotstr( "NULL" );
+//       std::string rotstrX( "XNULL" );
+//       std::string rotstrY( "YNULL" );
+
+//       // Check if we've already created the rotation matrix
+//       rotstr = "R"; 
+//       rotstrX = "RX"; 
+//       rotstrY = "RY"; 
+//       rotstr  += dbl_to_string( phi * 10.);
+//       rotstrX  += dbl_to_string( xphi * 10.);
+//       rotstrY  += dbl_to_string( yphi * 10.);
+//       rotation = DDRotation( DDName( rotstr + rotstrX + rotstrY ));
+//       if( !rotation )
+//       {
+// 	rotation = DDrot( DDName( rotstr + rotstrX + rotstrY, m_idNameSpace ),
+// 			  new DDRotationMatrix( *DDcreateRotationMatrix( theta, phiX, theta + phi, phiY, -phi, phiZ )
+// 						* ( *DDcreateRotationMatrix( theta + xphi, phiX, 90.*CLHEP::deg, 90.*CLHEP::deg, xphi, 0.0 ))));
+//       }
+
+//       // Translation
+//       //
+//       edm::LogInfo("HGCalGeom") << "Module " << copyNo << ", (ix,iy)=("
+// 				<< ix << "," << iy
+// 				<< "): offsetX=" << offsetX 
+// 				<< ", offsetY=" << offsetY
+// 				<< ", offsetZ=" << offsetZ;
+      
+//       DDTranslation tran( offsetX, offsetY, offsetZ );
+	
+//       DDName parentName = parent().name(); 
+//       cpv.position( DDName( m_childName ), parentName, copyNo, tran, rotation );
+      
+//       offsetX += m_xyoffset;
+//       if (sqrt(offsetX*offsetX + offsetY*offsetY) >= m_rMax)
+// 	offsetLimitX = offsetX;
+	
+//       copyNo += m_incrCopyNo;
+//       xphi += tiltAngle;     
+//     }
+
+//     if( iy < 3 )
+//     {
+//       angle = m_startAngle + m_stepAngle * iy;
+//       offsetX = m_rMin * cos( angle );
+//       ( offsetX < 0.0 ) ? offsetX = 0.0 : offsetX;
+//     }
+//     else
+//       offsetX = 0.0 ;
+
+//     offsetY += m_xyoffset;
+//     xphi = 0.;
+//     yphi += tiltAngle;
+    
+//   }
+
+
 }
 
