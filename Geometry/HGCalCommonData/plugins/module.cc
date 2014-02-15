a10c90d2c96069fd897a94c9a5177e51590ed321
Sunanda <sunanda.banerjee@cern.ch>
2014-02-15 06:10:47 +0100

    Parametrized definition of HGCAL Geometry

--- Geometry/HGCalCommonData/plugins/module.cc
+++ Geometry/HGCalCommonData/plugins/module.cc
@@ -1,6 +1,10 @@
+#include "Geometry/HGCalCommonData/plugins/DDHGCalEEAlgo.h"
+#include "Geometry/HGCalCommonData/plugins/DDHGCalHEAlgo.h"
 #include "Geometry/HGCalCommonData/plugins/DDShashlikModule.h"
 #include "Geometry/HGCalCommonData/plugins/DDShashlikSupermodule.h"
 #include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
 
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalEEAlgo, "hgcal:DDHGCalEEAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalHEAlgo, "hgcal:DDHGCalHEAlgo");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDShashlikModule, "shashlik:DDShashlikModule");
 DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDShashlikSupermodule, "shashlik:DDShashlikSupermodule");
