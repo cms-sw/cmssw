25c93398b35d9de6a8ffe398856e0a7024d2ec7f
Michail <bachtis@cern.ch>
2014-02-16 21:48:56 +0100

    cleaning old code

--- RecoParticleFlow/PFClusterProducer/plugins/SealModule.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/SealModule.cc
@@ -2,24 +2,10 @@
 
 #include "RecoParticleFlow/PFClusterProducer/plugins/PFClusterProducer.h"
 #include "RecoParticleFlow/PFClusterProducer/plugins/PFHCALSuperClusterProducer.h"
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducerECAL.h"
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducerHCAL.h"
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFHCALDualTimeRecHitProducer.h"
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducerHO.h"
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducerPS.h"
-
-
-
 
 
 DEFINE_FWK_MODULE(PFClusterProducer);
 DEFINE_FWK_MODULE(PFHCALSuperClusterProducer);
-DEFINE_FWK_MODULE(PFRecHitProducerECAL);
-DEFINE_FWK_MODULE(PFRecHitProducerHCAL);
-DEFINE_FWK_MODULE(PFHCALDualTimeRecHitProducer);
-DEFINE_FWK_MODULE(PFRecHitProducerHO);
-DEFINE_FWK_MODULE(PFRecHitProducerPS);
-
 
 
 #include "RecoParticleFlow/PFClusterProducer/interface/PFRecHitNavigatorBase.h"
@@ -71,8 +57,8 @@ DEFINE_EDM_PLUGIN(PFRecHitFactory, PFPSRecHitCreator, "PFPSRecHitCreator");
 
 
 
-#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducerNew.h"
+#include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducer.h"
 #include "RecoParticleFlow/PFClusterProducer/plugins/PFCTRecHitProducer.h"
 
-DEFINE_FWK_MODULE(PFRecHitProducerNew);
+DEFINE_FWK_MODULE(PFRecHitProducer);
 DEFINE_FWK_MODULE(PFCTRecHitProducer);
