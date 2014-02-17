b306bbd4c2263e287963cb14bce9137780719609
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2014-02-17 17:52:50 +0100

    add new performance measurement tool (CondCore/Utilities/bin/conddb_test_gt_perf.cpp) with related updates
    move to use tbb parallel_for_each instead of boost threadpool

--- CondCore/Utilities/interface/CondDBImport.h
+++ CondCore/Utilities/interface/CondDBImport.h
@@ -12,7 +12,7 @@ namespace cond {
     cond::Hash import( const std::string& inputTypeName, const void* inputPtr, Session& destination, bool checkEqual = true );
 
     std::pair<std::string,boost::shared_ptr<void> > fetch( const cond::Hash& payloadId, Session& session );
-
+    std::pair<std::string, boost::shared_ptr<void> > fetchOne( const std::string &payloadTypeName, const cond::Binary &data, boost::shared_ptr<void> payloadPtr );
   }
 
 }
