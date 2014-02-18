3668a0a7a543b84261e5f7c7bb979071732711f3
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-02-18 07:20:04 +0100

    Remove inheritance of MultiHitGenerator from MultiHitGeneratorFromPairAndLayers
    MultiHitGeneratorFromPairAndLayers does not really benefit from the
    base class, and the user (CombinedMultiHitGenerator) use it as the
    base class type for dynamic polymorphism. Now we can also remove some
    unneeded methods that are pure virtual in the base class.

--- RecoTracker/TkSeedGenerator/src/MultiHitGenerator.cc
+++ RecoTracker/TkSeedGenerator/src/MultiHitGenerator.cc
@@ -5,11 +5,9 @@
 const OrderedMultiHits & MultiHitGenerator::run(
     const TrackingRegion& region, const edm::Event & ev, const edm::EventSetup& es)
 {
-  // std::cout << "MultiHitGenerator cache b " << cache.size() << std::endl;
   theHitSets.clear(); // called multiple time for the same seed collection
   theHitSets.reserve(localRA.upper());
   hitSets(region, theHitSets, ev, es);
-  // std::cout << "MultiHitGenerator cache	a " << cache.size() << std::endl;
   theHitSets.shrink_to_fit();
   localRA.update(theHitSets.size());
   return theHitSets;
@@ -18,8 +16,5 @@ const OrderedMultiHits & MultiHitGenerator::run(
 void MultiHitGenerator::clear() 
 {
   theHitSets.clear(); theHitSets.shrink_to_fit();
-  //std::cout << "MultiHitGenerator " << typeid(*this).name()
-  //           <<" cache c " << cache.size() << ' ' << cache.capacity() << std::endl;
-  cache.clear();
 }
 
