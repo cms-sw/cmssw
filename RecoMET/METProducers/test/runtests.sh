f17ab67e2d1ebef4c0ea695976bda885d0616a3c
Tai Sakuma <tai.sakuma@gmail.com>
2014-02-18 20:40:24 -0600

    add test for CaloMET

--- RecoMET/METProducers/test/runtests.sh
+++ RecoMET/METProducers/test/runtests.sh
@@ -5,3 +5,4 @@ function die { echo $1: status $2 ;  exit $2; }
 
 ##____________________________________________________________________________||
 cmsRun ${LOCAL_TEST_DIR}/recoMET_pfMet_cfg.py || die 'Failure using recoMET_pfMet_cfg.py' $?
+cmsRun ${LOCAL_TEST_DIR}/recoMET_caloMet_cfg.py || die 'Failure using recoMET_caloMet_cfg.py' $?
