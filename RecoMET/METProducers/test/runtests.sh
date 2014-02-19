fc040e8265a0d54394e5b4216332eeccae07f68c
Tai Sakuma <tai.sakuma@gmail.com>
2014-02-18 18:31:33 -0600

    add test for pfMET

--- RecoMET/METProducers/test/runtests.sh
+++ RecoMET/METProducers/test/runtests.sh
@@ -4,3 +4,4 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 ##____________________________________________________________________________||
+cmsRun ${LOCAL_TEST_DIR}/recoMET_pfMet_cfg.py || die 'Failure using recoMET_pfMet_cfg.py' $?
