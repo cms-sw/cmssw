dfb17183d2fec5a8b67eba554cc0d23c2e3563e9
Tai Sakuma <tai.sakuma@gmail.com>
2014-02-18 21:21:14 -0600

    add test for GenMET

--- RecoMET/METProducers/test/runtests.sh
+++ RecoMET/METProducers/test/runtests.sh
@@ -7,3 +7,4 @@ function die { echo $1: status $2 ;  exit $2; }
 cmsRun ${LOCAL_TEST_DIR}/recoMET_pfMet_cfg.py || die 'Failure using recoMET_pfMet_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/recoMET_caloMet_cfg.py || die 'Failure using recoMET_caloMet_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/recoMET_tcMet_cfg.py || die 'Failure using recoMET_tcMet_cfg.py' $?
+cmsRun ${LOCAL_TEST_DIR}/recoMET_genMet_cfg.py || die 'Failure using recoMET_genMet_cfg.py' $?
