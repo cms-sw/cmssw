1177e4e4d7c5ede4ddfef72841b55717c551608b
acimmino <anna.cimmino@cern.ch>
2014-02-20 10:26:50 +0100

    chenges for threaded cmssw

--- DQM/RPCMonitorClient/python/RPCDCSSummary_cfi.py
+++ DQM/RPCMonitorClient/python/RPCDCSSummary_cfi.py
@@ -2,6 +2,4 @@ import FWCore.ParameterSet.Config as cms
 
 rpcDCSSummary = cms.EDAnalyzer("RPCDCSSummary", 
                                NumberOfEndcapDisks  = cms.untracked.int32(4),
-                               MinimumRPCFEDId  = cms.untracked.int32(790),
-                               MaximumRPCFEDId  = cms.untracked.int32(792)
                                )
--- DQM/RPCMonitorClient/python/RPCDaqInfo_cfi.py
+++ DQM/RPCMonitorClient/python/RPCDaqInfo_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 rpcDaqInfo = cms.EDAnalyzer("RPCDaqInfo",
-                            NumberOfEndcapDisks  = cms.untracked.int32(4),
-                            MinimumRPCFEDId  = cms.untracked.int32(790),
-                            MaximumRPCFEDId  = cms.untracked.int32(792)
+                            NumberOfEndcapDisks  = cms.untracked.int32(4)
                             )
 
