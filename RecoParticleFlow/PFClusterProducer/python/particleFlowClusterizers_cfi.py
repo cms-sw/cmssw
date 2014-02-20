376e6844b4c36faed9f9a86f5539b2993aca18b0
Lindsey Gray <lindsey.gray@gmail.com>
2014-02-20 10:48:36 +0100

    normalize fraction calculations to rechit thresholds. In the case of the seeds it's not necessary.

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterizers_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterizers_cfi.py
@@ -81,7 +81,15 @@ pfClusterizer_ECAL = cms.PSet(
     showerSigma = cms.double(1.5),
     stoppingTolerance = cms.double(1e-8),
     maxIterations = cms.uint32(50),
-    excludeOtherSeeds = cms.bool(True)
+    excludeOtherSeeds = cms.bool(True),
+    recHitEnergyNorms = cms.VPSet(
+                            cms.PSet( detector = cms.string("ECAL_BARREL"),
+                                      recHitEnergyNorm = cms.double(0.08)
+                                      ),
+                            cms.PSet( detector = cms.string("ECAL_ENDCAP"),
+                                      recHitEnergyNorm = cms.double(0.3)
+                                      )
+                            )
     )
 
 pfClusterizer_HCAL = cms.PSet(
@@ -93,7 +101,15 @@ pfClusterizer_HCAL = cms.PSet(
     showerSigma = cms.double(10.0),
     stoppingTolerance = cms.double(1e-8),
     maxIterations = cms.uint32(50),
-    excludeOtherSeeds = cms.bool(True)
+    excludeOtherSeeds = cms.bool(True),
+    recHitEnergyNorms = cms.VPSet(
+                            cms.PSet( detector = cms.string("HCAL_BARREL1"),
+                                      recHitEnergyNorm = cms.double(0.8)
+                                      ),
+                            cms.PSet( detector = cms.string("HCAL_ENDCAP"),
+                                      recHitEnergyNorm = cms.double(0.8)
+                                      )
+                            )
     )
 
 pfClusterizer_HO = cms.PSet(
@@ -105,7 +121,15 @@ pfClusterizer_HO = cms.PSet(
     showerSigma = cms.double(10.0),
     stoppingTolerance = cms.double(1e-8),
     maxIterations = cms.uint32(50),
-    excludeOtherSeeds = cms.bool(True)
+    excludeOtherSeeds = cms.bool(True),
+    recHitEnergyNorms = cms.VPSet(
+                            cms.PSet( detector = cms.string("HCAL_BARREL2_RING0"),
+                                      recHitEnergyNorm = cms.double(0.5)
+                                      ),
+                            cms.PSet( detector = cms.string("HCAL_BARREL2_RING1"),
+                                      recHitEnergyNorm = cms.double(1.0)
+                                      )
+                            )
     )
 
 pfClusterizer_HF = cms.PSet(
@@ -117,7 +141,15 @@ pfClusterizer_HF = cms.PSet(
     showerSigma = cms.double(10.0),
     stoppingTolerance = cms.double(1e-8),
     maxIterations = cms.uint32(50),
-    excludeOtherSeeds = cms.bool(True)
+    excludeOtherSeeds = cms.bool(True),
+    recHitEnergyNorms = cms.VPSet(
+                            cms.PSet( detector = cms.string("HF_EM"),
+                                      recHitEnergyNorm = cms.double(0.8)
+                                      ),
+                            cms.PSet( detector = cms.string("HF_HAD"),
+                                      recHitEnergyNorm = cms.double(0.8)
+                                      )
+                            )
     )
 
 pfClusterizer_PS = cms.PSet(
@@ -128,5 +160,13 @@ pfClusterizer_PS = cms.PSet(
     showerSigma = cms.double(0.2),
     stoppingTolerance = cms.double(1e-8),
     maxIterations = cms.uint32(50),
-    excludeOtherSeeds = cms.bool(True)
+    excludeOtherSeeds = cms.bool(True),    
+    recHitEnergyNorms = cms.VPSet(
+                            cms.PSet( detector = cms.string("PS1"),
+                                      recHitEnergyNorm = cms.double(6e-5)
+                                      ),
+                            cms.PSet( detector = cms.string("PS2"),
+                                      recHitEnergyNorm = cms.double(6e-5)
+                                      )
+                            )
     )
--- RecoParticleFlow/PFClusterProducer/src/Basic2DGenericPFlowClusterizer.h
+++ RecoParticleFlow/PFClusterProducer/src/Basic2DGenericPFlowClusterizer.h
@@ -4,37 +4,13 @@
 #include "RecoParticleFlow/PFClusterProducer/interface/PFClusterBuilderBase.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecHitFraction.h"
 
+#include <unordered_map>
+
 class Basic2DGenericPFlowClusterizer : public PFClusterBuilderBase {
   typedef Basic2DGenericPFlowClusterizer B2DGPF;
  public:
-  Basic2DGenericPFlowClusterizer(const edm::ParameterSet& conf) :
-    PFClusterBuilderBase(conf),
-    _maxIterations(conf.getParameter<unsigned>("maxIterations")),
-    _stoppingTolerance(conf.getParameter<double>("stoppingTolerance")),
-    _showerSigma(conf.getParameter<double>("showerSigma")),
-    _excludeOtherSeeds(conf.getParameter<bool>("excludeOtherSeeds")) { 
-    _allCellsPosCalc.reset(NULL);
-    if( conf.exists("allCellsPositionCalc") ) {
-      const edm::ParameterSet& acConf = 
-	conf.getParameterSet("allCellsPositionCalc");
-      const std::string& algoac = 
-	acConf.getParameter<std::string>("algoName");
-      PosCalc* accalc = 
-	PFCPositionCalculatorFactory::get()->create(algoac, acConf);
-      _allCellsPosCalc.reset(accalc);
-    }
-    // if necessary a third pos calc for convergence testing
-    _convergencePosCalc.reset(NULL);
-    if( conf.exists("positionCalcForConvergence") ) {
-      const edm::ParameterSet& convConf = 
-	conf.getParameterSet("positionCalcForConvergence");
-      const std::string& algoconv = 
-	convConf.getParameter<std::string>("algoName");
-      PosCalc* convcalc = 
-	PFCPositionCalculatorFactory::get()->create(algoconv, convConf);
-      _convergencePosCalc.reset(convcalc);
-    }
-  }
+  Basic2DGenericPFlowClusterizer(const edm::ParameterSet& conf);
+    
   virtual ~Basic2DGenericPFlowClusterizer() {}
   Basic2DGenericPFlowClusterizer(const B2DGPF&) = delete;
   B2DGPF& operator=(const B2DGPF&) = delete;
@@ -54,6 +30,8 @@ class Basic2DGenericPFlowClusterizer : public PFClusterBuilderBase {
   const double _stoppingTolerance;
   const double _showerSigma;
   const bool _excludeOtherSeeds;
+  const std::unordered_map<std::string,int> _layerMap;
+  std::unordered_map<int,double> _recHitEnergyNorms;
   std::unique_ptr<PFCPositionCalculatorBase> _allCellsPosCalc;
   std::unique_ptr<PFCPositionCalculatorBase> _convergencePosCalc;
   
