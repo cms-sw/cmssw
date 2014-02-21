2e6815fe2f20e9711a7548887eef855a167d038f
acimmino <anna.cimmino@cern.ch>
2014-02-21 10:44:46 +0100

    fixed bug

--- DQM/RPCMonitorClient/src/RPCEfficiencySecond.cc
+++ DQM/RPCMonitorClient/src/RPCEfficiencySecond.cc
@@ -6,14 +6,12 @@ Original Author:Camilo Carrillo
 #include "DQM/RPCMonitorDigi/interface/utils.h"
 #include "DQM/RPCMonitorClient/interface/RPCEfficiencySecond.h"
 #include "DQM/RPCMonitorDigi/interface/RPCBookFolderStructure.h"
-
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 #include <Geometry/RPCGeometry/interface/RPCGeometry.h>
 #include <Geometry/RPCGeometry/interface/RPCGeomServ.h>
 #include <Geometry/CommonDetUnit/interface/GeomDet.h>
@@ -157,12 +155,18 @@ void RPCEfficiencySecond::beginRun(const edm::Run&, const edm::EventSetup& iSetu
     MeTitle<<"Occupancy Distribution for Station "<<d<<" Ring 3";
     OcGregR3D[index]= dbe->book1D(MeName.str(),MeTitle.str(),36,0.5,36.5);
 
-
     MeName.str("");
     MeName<<"ExGregDistroR2D"<<d;
     MeTitle.str("");
     MeTitle<<"Expected Distribution for Station "<<d<<" Ring 2";
     ExGregR2D[index]= dbe->book1D(MeName.str(),MeTitle.str(),36,0.5,36.5);
+
+
+    MeName.str("");
+    MeName<<"ExGregDistroR3D"<<d;
+    MeTitle.str("");
+    MeTitle<<"Expected Distribution for Station "<<d<<" Ring 3";
+    ExGregR3D[index]= dbe->book1D(MeName.str(),MeTitle.str(),36,0.5,36.5);
 
     index++;
   }
@@ -205,13 +209,15 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
   }
    
   int indexDisk[10];
-  for(int j=0;j<(numberOfDisks_*2);j++){
+  for(int j=0;j<10;j++){
     indexDisk[j]=0;
   }
    
   
-  for(TrackingGeometry::DetContainer::const_iterator it=rpcGeo->dets().begin();it<rpcGeo->dets().end();it++){
+  for(TrackingGeometry::DetContainer::const_iterator it=rpcGeo->dets().begin(); it!=rpcGeo->dets().end();it++){
    
+    
+
     if(dynamic_cast< RPCChamber* >( *it ) != 0 ){
       RPCChamber* ch = dynamic_cast< RPCChamber* >( *it ); 
       std::vector< const RPCRoll*> roles = (ch->rolls());
@@ -220,7 +226,7 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	RPCGeomServ rpcsrv(rpcId);
 	//	std::string nameRoll = rpcsrv.name();
 	std::string camera = rpcsrv.name();
-
+	
 	//Breaking down the geometry
 	int region = rpcId.region();
 	int wheel = rpcId.ring();  int ring = rpcId.ring();  
@@ -228,27 +234,27 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	int station = rpcId.station();
 	int geolayer = rpcId.layer(); 
 	  
-
+	
 	if(region==0){//Barrel
-
+	  
 	  std::stringstream meIdRPC,  meIdDT; //,  bxDistroId;	  
 	  
 	  meIdRPC<<folderPath<<"MuonSegEff/RPCDataOccupancyFromDT_"<<rpcId.rawId();
 	  meIdDT<<folderPath<<"MuonSegEff/ExpectedOccupancyFromDT_"<<rpcId.rawId();
-
+	  
 	  histoRPC = dbe->get(meIdRPC.str());
 	  histoDT = dbe->get(meIdDT.str());
-	 
+	     
 	  int NumberWithOutPrediction=0;
 	  double p = 0.;
 	  double o = 0.;
-	  float ef = 0.;
-	  float er = 0.;
-	  float buffef = 0.;
-	  float buffer = 0.;
-	  float sumbuffef = 0.;
-	  float sumbuffer = 0.;
-	  float averageeff = 0.;
+	  double ef = 0.;
+	  double er = 0.;
+	  double buffef = 0.;
+	  double buffer = 0.;
+	  double sumbuffef = 0.;
+	  double sumbuffer = 0.;
+	  double averageeff = 0.;
 	  int NumberStripsPointed = 0;
 	 
 	  
@@ -257,11 +263,10 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	    for(int i=1;i<=int((*r)->nstrips());++i){
 	  
 	      if(histoDT->getBinContent(i)!=0){
-		
 		LogDebug("rpcefficiencysecond")<<"Inside the If";
 		
-		buffef = float(histoRPC->getBinContent(i))/float(histoDT->getBinContent(i));
-		buffer = sqrt(buffef*(1.-buffef)/float(histoDT->getBinContent(i)));
+		buffef = double(histoRPC->getBinContent(i))/double(histoDT->getBinContent(i));
+		buffer = sqrt(buffef*(1.-buffef)/double(histoDT->getBinContent(i)));
 		
 		sumbuffef=sumbuffef+buffef;
 		sumbuffer = sumbuffer + buffer*buffer;
@@ -271,24 +276,24 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	      }
 	      LogDebug("rpcefficiencysecond")<<"Strip="<<i<<" RPC="<<histoRPC->getBinContent(i)<<" DT="<<histoDT->getBinContent(i)<<" buffef="<<buffef<<" buffer="<<buffer<<" sumbuffef="<<sumbuffef<<" sumbuffer="<<sumbuffer<<" NumberStripsPointed="<<NumberStripsPointed<<" NumberWithOutPrediction"<<NumberWithOutPrediction;
 	    }
-	    
+
 	    p=histoDT->getTH1F()->Integral();
 	    o=histoRPC->getTH1F()->Integral();
-	    
+
 	    if(NumberStripsPointed!=0){
-	      averageeff = (sumbuffef/float(NumberStripsPointed))*100.;
-	      //averageerr = sqrt(sumbuffer/float(NumberStripsPointed))*100.;
+	      averageeff = (sumbuffef/double(NumberStripsPointed))*100.;
+	      //averageerr = sqrt(sumbuffer/double(NumberStripsPointed))*100.;
 	    }
-	    
+
 	  } //////////////////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	  
 	  //	  int Ring = rpcId.ring();
 	  
 	  if(p!=0){
-	    ef = float(o)/float(p); 
-	    er = sqrt(ef*(1.-ef)/float(p));
+	    ef = double(o)/double(p); 
+	    er = sqrt(ef*(1.-ef)/double(p));
 	  }
-	    
+	  
 	  ef=ef*100;
 	  er=er*100;
 
@@ -296,7 +301,7 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	   //Filling azimutal Wheel Histograms
 		  
 	  int layer = 0;
-	  	    
+	  
 	    if(station==1&&geolayer==1) layer = 1;
 	    else if(station==1&&geolayer==2) layer = 2;
 	    else if(station==2&&geolayer==1) layer = 3;
@@ -308,11 +313,7 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	    ExsectorEffW[wheel+2]->Fill(sector,p); OcsectorEffW[wheel+2]->Fill(sector,o); 
 	    ExpLayerW[wheel+2]->Fill(layer, p); ObsLayerW[wheel+2]->Fill(layer, o);
 	   
-	    
-	    
-	    
-	  
-	    
+	    	    
 	    
 	    LogDebug("rpcefficiencysecond")<<"Pigi "<<camera<<" "<<rpcsrv.shortname()<<" "<<(*r)->id();
 	    
@@ -326,8 +327,8 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	    
 	      WheelSummary[wheel+2]->setBinContent(xBin,yBin,averageeff);
 	    }
- 	  
-
+	    
+	    
 	    EffDistroW[wheel+2]->Fill(averageeff);
 	    indexWheel[wheel+2]++;
 	    EffGlobW[wheel+2]->setBinContent(indexWheel[wheel+2],ef);  
@@ -337,131 +338,120 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
 	}else{//EndCap
 
 	  
-	    std::stringstream meIdRPC,meIdCSC; //, bxDistroId;
-	    std::string      meIdPRO;
-	    
-	    
-	    meIdRPC<<folderPath<<"MuonSegEff/RPCDataOccupancyFromCSC_"<<rpcId.rawId();
-	    meIdCSC<<folderPath<<"MuonSegEff/ExpectedOccupancyFromCSC_"<<rpcId.rawId();
-	    
-	    meIdPRO = "Profile_"+ rpcsrv.name();
-	    
-	    histoRPC= dbe->get(meIdRPC.str());
-	    histoCSC= dbe->get(meIdCSC.str());
-	    //BXDistribution = dbe->get(bxDistroId.str());
-	    
-	    int NumberWithOutPrediction=0;
-	    double p = 0;
-	    double o = 0;
-	    float ef =0;
-	    float er =0;
-	    float buffef = 0;
-	    float buffer = 0;
-	    float sumbuffef = 0;
-	    float sumbuffer = 0;
-	    float averageeff = 0;
-	    int NumberStripsPointed = 0;
-	    
-	    
-	    if(histoRPC && histoCSC) {
-	      LogDebug("rpcefficiencysecond")<<rpcsrv.name();
-	      
-	      for(int i=1;i<=int((*r)->nstrips());++i){
-		if(histoCSC->getBinContent(i)!=0){
-		  LogDebug("rpcefficiencysecond")<<"Inside the If";
-		  buffef = float(histoRPC->getBinContent(i))/float(histoCSC->getBinContent(i));
-		  //	meMap[meIdPRO]->setBinContent(i,buffef); 
-		  buffer = sqrt(buffef*(1.-buffef)/float(histoCSC->getBinContent(i)));
-		  //	meMap[meIdPRO]->setBinError(i,buffer);
-		  sumbuffef=sumbuffef+buffef;
-		  sumbuffer = sumbuffer + buffer*buffer;
-		  NumberStripsPointed++;
-		}else{
-		  NumberWithOutPrediction++;
-		}
-		
-		LogDebug("rpcefficiencysecond")<<"Strip="<<i<<" RPC="<<histoRPC->getBinContent(i)<<" CSC="<<histoCSC->getBinContent(i)<<" buffef="<<buffef<<" buffer="<<buffer<<" sumbuffef="<<sumbuffef<<" sumbuffer="<<sumbuffer<<" NumberStripsPointed="<<NumberStripsPointed<<" NumberWithOutPrediction"<<NumberWithOutPrediction;
-	      }
-
-	      p=histoCSC->getTH1F()->Integral();
-	      o=histoRPC->getTH1F()->Integral();
-	      
-	      if(NumberStripsPointed!=0){
-		averageeff = (sumbuffef/float(NumberStripsPointed))*100.;
-	      }
-	      
-	    }
-	    
-	    int Disk = station*region;
-	    int station = station;
-	    
-	    if(p!=0){
-	      ef = float(o)/float(p); 
-	      er = sqrt(ef*(1.-ef)/float(p));
-	    }
-	    
-	    ef=ef*100;
-	    er=er*100;
-	    
-	    
-	    //Filling azimutal GregHistograms
-	    
-	    if(region==1){ //Positive Endcap
-	      
-	      if(ring==2){ ExGregR2D[Disk+numberOfDisks_-1]->Fill(rpcsrv.segment(),p);OcGregR2D[Disk+numberOfDisks_-1]->Fill(rpcsrv.segment(),o);} 
-	      else if(ring==3){ ExGregR3D[Disk+numberOfDisks_-1]->Fill(rpcsrv.segment(),p);OcGregR3D[Disk+numberOfDisks_-1]->Fill(rpcsrv.segment(),o);}
-	   	      
-	      if(p > 100){ //We need at least 100 predictions to fill the summary plot
-		RPCGeomServ RPCServ((*r)->id());
-		int xBin = RPCServ.segment();
-		int yBin= (ring-1)*3-(*r)->id().roll()+1;
-		
-		DiskSummary[Disk+numberOfDisks_-1]->setBinContent(xBin, yBin, averageeff);
-		
-	      }
-	      
-	      
-	      EffDistroD[Disk+numberOfDisks_-1]->Fill(averageeff);
-	      indexDisk[Disk+numberOfDisks_-1]++;  
-	      EffGlobD[Disk+numberOfDisks_-1]->setBinContent(indexDisk[Disk+numberOfDisks_-1],ef);  
-	      EffGlobD[Disk+numberOfDisks_-1]->setBinError(indexDisk[Disk+numberOfDisks_-1],er);  
-	      EffGlobD[Disk+numberOfDisks_-1]->setBinLabel(indexDisk[Disk+numberOfDisks_-1],camera,1);  
-	      
-	      
-	      
-	    
-	    }else if(region==-1){//Negative Encap
+	  std::stringstream meIdRPC,meIdCSC; //, bxDistroId;
+	  std::string      meIdPRO;
 	  
-	      if(ring==2){ ExGregR2D[Disk+numberOfDisks_]->Fill(rpcsrv.segment(),p);OcGregR2D[Disk+numberOfDisks_]->Fill(rpcsrv.segment(),o);} 
-	      else if(ring==3){ ExGregR3D[Disk+numberOfDisks_]->Fill(rpcsrv.segment(),p);OcGregR3D[Disk+numberOfDisks_]->Fill(rpcsrv.segment(),o);}
-	   	      
-	      if(p > 100){ //We need at least 100 predictions to fill the summary plot
-		RPCGeomServ RPCServ((*r)->id());
-		int xBin = RPCServ.segment();
-		int yBin= (ring-1)*3-(*r)->id().roll()+1;
-		
-		DiskSummary[Disk+numberOfDisks_]->setBinContent(xBin, yBin, averageeff);
-		
+	  
+	  meIdRPC<<folderPath<<"MuonSegEff/RPCDataOccupancyFromCSC_"<<rpcId.rawId();
+	  meIdCSC<<folderPath<<"MuonSegEff/ExpectedOccupancyFromCSC_"<<rpcId.rawId();
+	  
+	  meIdPRO = "Profile_"+ rpcsrv.name();
+	  
+	  histoRPC= dbe->get(meIdRPC.str());
+	  histoCSC= dbe->get(meIdCSC.str());
+	  //BXDistribution = dbe->get(bxDistroId.str());
+	  
+	  int NumberWithOutPrediction=0;
+	  double p = 0;
+	  double o = 0;
+	  double ef =0;
+	  double er =0;
+	  double buffef = 0;
+	  double buffer = 0;
+	  double sumbuffef = 0;
+	  double sumbuffer = 0;
+	  double averageeff = 0;
+	  int NumberStripsPointed = 0;
+	    
+	  
+	  if(histoRPC && histoCSC) {
+	    LogDebug("rpcefficiencysecond")<<rpcsrv.name();
+	    
+	    for(int i=1;i<=int((*r)->nstrips());++i){
+	      if(histoCSC->getBinContent(i)!=0){
+		LogDebug("rpcefficiencysecond")<<"Inside the If";
+		buffef = double(histoRPC->getBinContent(i))/double(histoCSC->getBinContent(i));  
+		//	meMap[meIdPRO]->setBinContent(i,buffef); 
+		buffer = sqrt(buffef*(1.-buffef)/double(histoCSC->getBinContent(i)));
+		//	meMap[meIdPRO]->setBinError(i,buffer);
+		sumbuffef=sumbuffef+buffef;   
+		sumbuffer = sumbuffer + buffer*buffer;
+		NumberStripsPointed++;
+	      }else{
+		NumberWithOutPrediction++;
 	      }
 	      
-	      
-	      EffDistroD[Disk+numberOfDisks_]->Fill(averageeff);
-	      indexDisk[Disk+numberOfDisks_]++;  
-	      EffGlobD[Disk+numberOfDisks_]->setBinContent(indexDisk[Disk+numberOfDisks_],ef);  
-	      EffGlobD[Disk+numberOfDisks_]->setBinError(indexDisk[Disk+numberOfDisks_],er);  
-	      EffGlobD[Disk+numberOfDisks_]->setBinLabel(indexDisk[Disk+numberOfDisks_],camera,1);
+	      LogDebug("rpcefficiencysecond")<<"Strip="<<i<<" RPC="<<histoRPC->getBinContent(i)<<" CSC="<<histoCSC->getBinContent(i)<<" buffef="<<buffef<<" buffer="<<buffer<<" sumbuffef="<<sumbuffef<<" sumbuffer="<<sumbuffer<<" NumberStripsPointed="<<NumberStripsPointed<<" NumberWithOutPrediction"<<NumberWithOutPrediction;
 	    }
+	    
+	    p=histoCSC->getTH1F()->Integral();
+	    o=histoRPC->getTH1F()->Integral();
+	    
+	    if(NumberStripsPointed!=0){
+	      averageeff = (sumbuffef/double(NumberStripsPointed))*100.;
+	    }
+	    
+	  }
+	  
+	  int Disk = station*region;
+
+	  if(Disk > numberOfDisks_ || Disk<-numberOfDisks_){continue;} //remove strange disk numbers!!
+	  
+	  int dIndex = -1;
+	  if(region == -1){
+	    dIndex = Disk+numberOfDisks_;
+	  }else if(region == 1){
+	    dIndex = Disk+numberOfDisks_-1;
+	  }
+
+	  if( dIndex<0 || dIndex>= numberOfDisks_*2){continue;} //extra check on disk numering
+	  
+	  if(p!=0){
+	    ef = double(o)/double(p); 
+	    er = sqrt(ef*(1.-ef)/double(p));
+	  }
+	  
+	  ef=ef*100;
+	  er=er*100;
+	  
+	  if(ring==2){ 
+	    if (ExGregR2D[dIndex] && OcGregR2D[dIndex] ){
+	      ExGregR2D[dIndex]->Fill(rpcsrv.segment(),p);
+	      OcGregR2D[dIndex]->Fill(rpcsrv.segment(),o);
+	    }
+	  } else if(ring==3){
+	    ExGregR3D[dIndex]->Fill(rpcsrv.segment(),p);
+	    OcGregR3D[dIndex]->Fill(rpcsrv.segment(),o);
+	  }
+	      
+	  if(p > 100){ //We need at least 100 predictions to fill the summary plot
+	    RPCGeomServ RPCServ((*r)->id());
+	    int xBin = RPCServ.segment();
+	    int yBin= (ring-1)*3-(*r)->id().roll()+1;
+	    
+	    DiskSummary[dIndex]->setBinContent(xBin, yBin, averageeff);
+	  }
+	  
+	    
+	  if (  EffDistroD[dIndex]){EffDistroD[dIndex]->Fill(averageeff);}
+	  indexDisk[dIndex]++;     
+	  if ( EffGlobD[dIndex]){
+	    EffGlobD[dIndex]->setBinContent(indexDisk[dIndex],ef);     
+	    EffGlobD[dIndex]->setBinError(indexDisk[dIndex],er);  
+	    EffGlobD[dIndex]->setBinLabel(indexDisk[dIndex],camera,1);     
+	    
 	  }
 	}
       }
+    }
   }
   
-    float eff,N,err;
-    int k;
-    
-    for (int d = 0; d<(numberOfDisks_*2); d++){
-      for(k=1;k<=36;k++){
-
+  double eff,N,err;
+  int k;
+     
+  for (int d = 0; d<(numberOfDisks_*2); d++){
+    for(k=1;k<=36;k++){
+         
       err=0; eff=0; N=ExGregR2D[d]->getBinContent(k);
       if(N!=0.){ eff = OcGregR2D[d]->getBinContent(k)/N; err=sqrt(eff*(1-eff)/N);}
       GregR2D[d]->setBinContent(k,eff); GregR2D[d]->setBinError(k,err);
@@ -469,38 +459,37 @@ void RPCEfficiencySecond::endRun(const edm::Run& r, const edm::EventSetup& iSetu
       err=0; eff=0; N=ExGregR3D[d]->getBinContent(k);
       if(N!=0.){ eff = OcGregR3D[d]->getBinContent(k)/N; err=sqrt(eff*(1-eff)/N);}
       GregR3D[d]->setBinContent(k,eff); GregR3D[d]->setBinError(k,err);
-      }
     }
-
-    
-    for (int w =-2; w<=2; w++){
-      for(k=1;k<=12;k++){
-	err=0; eff=0; N=ExsectorEffW[w+2]->getBinContent(k);
-	if(N!=0.){ eff = OcsectorEffW[w+2]->getBinContent(k)/N;err=sqrt(eff*(1-eff)/N);}
-	sectorEffW[w+2]->setBinContent(k,eff); sectorEffW[w+2]->setBinError(k,err);
-      }
+  }
+     
+  
+  for (int w =-2; w<=2; w++){
+    for(k=1;k<=12;k++){
+      err=0; eff=0; N=ExsectorEffW[w+2]->getBinContent(k);
+      if(N!=0.){ eff = OcsectorEffW[w+2]->getBinContent(k)/N;err=sqrt(eff*(1-eff)/N);}
+      sectorEffW[w+2]->setBinContent(k,eff); sectorEffW[w+2]->setBinError(k,err);
     }
-
-    
-    //Ranges for Both
-    //Barrel
-    
-    for (int w=-2; w<=2; w++){
-	EffGlobW[w+2]->setAxisRange(-4.,100.,2);
-	EffGlobW[w+2]->setAxisTitle("%",2);
-    }
-    
-    for (int d=0; d<(numberOfDisks_*2); d++){
-	EffGlobD[d]->setAxisRange(-4.,100.,2);
-	EffGlobD[d]->setAxisTitle("%",2);
-    }
-      
-      
-    if(SaveFile){
-      std::cout<<"Saving RootFile"<<std::endl;
-      dbe->save(NameFile);
-    }
-   
+  }
+     
+  
+  //Ranges for Both
+  //Barrel
+     
+  for (int w=-2; w<=2; w++){
+    EffGlobW[w+2]->setAxisRange(-4.,100.,2);
+    EffGlobW[w+2]->setAxisTitle("%",2);
+  }   
+  
+  for (int d=0; d<(numberOfDisks_*2); d++){
+    EffGlobD[d]->setAxisRange(-4.,100.,2);
+    EffGlobD[d]->setAxisTitle("%",2);
+  }
+  
+     
+  if(SaveFile){
+    dbe->save(NameFile);
+  }
+  
 }
 
 void RPCEfficiencySecond::endJob(){}
