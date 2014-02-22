c163aae63e55fd58909e69248f61679194b6d049
wmtan <wmtan@fnal.gov>
2014-02-22 23:47:48 +0100

    Don't fail on harmless warnings

--- FWCore/Services/src/InitRootHandlers.cc
+++ FWCore/Services/src/InitRootHandlers.cc
@@ -110,6 +110,8 @@ namespace {
           (el_message.find("already in TClassTable") != std::string::npos) ||
           (el_message.find("matrix not positive definite") != std::string::npos) ||
           (el_message.find("not a TStreamerInfo object") != std::string::npos) ||
+          (el_message.find("nbins is <=0") != std::string::npos) ||
+          (el_message.find("Problems declaring payload") != std::string::npos) ||
           (el_location.find("Fit") != std::string::npos) ||
           (el_location.find("TDecompChol::Solve") != std::string::npos) ||
           (el_location.find("THistPainter::PaintInit") != std::string::npos) ||
