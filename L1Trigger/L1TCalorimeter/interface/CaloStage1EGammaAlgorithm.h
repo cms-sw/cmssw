fc41691dbbb2925d3d1415a4004b2dfb391c99da
Kalanand <kalanand@gmail.com>
2014-02-22 18:11:30 -0600

    initial commit of CaloStage1EGammaAlgorithm

--- L1Trigger/L1TCalorimeter/interface/CaloStage1EGammaAlgorithm.h
+++ L1Trigger/L1TCalorimeter/interface/CaloStage1EGammaAlgorithm.h
@@ -5,7 +5,8 @@
 ///
 /// Implementation:
 ///
-/// \author: Jim Brooke - University of Bristol
+/// \authors: Jim Brooke - University of Bristol
+///           Kalanand Mishra - Fermilab 
 ///
 
 //
@@ -13,22 +14,36 @@
 #ifndef CaloStage1EGammaAlgorithm_h
 #define CaloStage1EGammaAlgorithm_h
 
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "DataFormats/L1TCalorimeter/interface/CaloStage1Cluster.h"
 #include "DataFormats/L1TCalorimeter/interface/CaloRegion.h"
 
 #include "DataFormats/L1Trigger/interface/EGamma.h"
 
+#include "L1Trigger/L1TCalorimeter/interface/L1GObject.h"
+#include "DataFormats/L1TCalorimeter/interface/CaloEmCand.h"
+
 #include <vector>
+
 
 namespace l1t {
     
   class CaloStage1EGammaAlgorithm { 
   public:
     virtual void processEvent(const std::vector<l1t::CaloStage1Cluster> & clusters,
-			      const std::veector<l1t::CaloRegion> & regions,
-			      std::vector<l1t::EGamma> & egammas) = 0;    
+			      const std::vector<l1t::CaloRegion> & regions,
+			      std::vector<l1t::EGamma> & egammas, 
+			      std::list<L1GObject> & rlxEGList, 
+			      std::list<L1GObject> & isoEGList) = 0;    
 
     virtual ~CaloStage1EGammaAlgorithm(){};
+
+
+    unsigned int egtSeed;
+
+    double puLevel;
+    double relativeIsolationCut;
+    double relativeJetIsolationCut;
   }; 
   
 } 
