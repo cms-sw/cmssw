e14e96fcd3ebf035e04269c1c394ec8477e7f200
Pavel Jez <pavel.jez@cern.ch>
2014-02-26 14:08:09 +0100

    added jet filtering

--- RecoTauTag/RecoTau/python/PFRecoTauPFJetInputs_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauPFJetInputs_cfi.py
@@ -1,7 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
+from CommonTools.RecoAlgos.pfJetSelector_cfi import pfJetSelector
+
+tauInputJets = pfJetSelector.clone(
+    src = cms.InputTag("ak5PFJets"),
+    filter = cms.bool( True ),
+    cut = cms.string ( "pt > 20 && abs(eta) < 2.5")
+)
+
 PFRecoTauPFJetInputs = cms.PSet (
-    inputJetCollection = cms.InputTag("ak5PFJets"),
+    inputJetCollection = cms.InputTag("tauInputJets"),
     jetConeSize = cms.double(0.5),
     isolationConeSize = cms.double(0.5)
 )
