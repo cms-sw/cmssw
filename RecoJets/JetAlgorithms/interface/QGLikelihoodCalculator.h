ab91139212f5a81f03f91b3ad3b3e5675a6f0dd6
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-02-28 00:37:00 +0100

    Simplify bin functions

--- RecoJets/JetAlgorithms/interface/QGLikelihoodCalculator.h
+++ RecoJets/JetAlgorithms/interface/QGLikelihoodCalculator.h
@@ -27,11 +27,13 @@ class QGLikelihoodCalculator{
   float likelihoodProduct( float nCharged, float nNeutral, float ptD, float rmsCand, TH1F* h1_nCharged, TH1F* h1_nNeutral, TH1F* h1_ptD, TH1F* h1_rmsCand);
 
  private:
-  TFile* histoFile_;
-  std::map<std::string,TH1F*> plots_;
+  TFile* histoFile;
+  std::map<int , TH1F*> plots;
   unsigned int nPtBins_;
   unsigned int nRhoBins_;
 
+  std::vector<int> RhoBins;
+  std::vector<int> PtBins;
 };
 
 
