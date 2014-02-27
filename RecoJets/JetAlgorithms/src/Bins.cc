f32866bf5e9774891a3adc690bf146b0890b5a00
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-02-28 00:37:00 +0100

    Simplify bin functions

--- RecoJets/JetAlgorithms/src/Bins.cc
+++ RecoJets/JetAlgorithms/src/Bins.cc
@@ -1,56 +1,32 @@
 #include "RecoJets/JetAlgorithms/interface/Bins.h"
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-using namespace std;
-
-int Bins::getBins(double  *bins,int nBins,double MinBin,double MaxBin,bool log){
-  double incr;
-  if(log){
-    incr=TMath::Power(MaxBin/MinBin,1.0/double(nBins));
-    bins[0]=MinBin;
-    bins[nBins]=MaxBin;
-    for(int i=1;i<nBins;i++) bins[i]=bins[i-1]*incr;
-  } else {
-    incr=(MaxBin-MinBin)/nBins;
-    bins[0]=MinBin;
-    bins[nBins+1]=MaxBin;
-    for(int i=1; i<nBins+1;i++) bins[i]=bins[i-1]+incr;
-  }
-  return 0;
+bool Bins::getBin(std::vector<int>& bins, double value, int& low, int& up){
+  if(value <= bins.front() || value >= bins.back()) return false;
+  std::vector<int>::iterator binUp = bins.begin() + 1;
+  while(value > *binUp) ++binUp;
+  low = *(binUp-1);
+  up = *(binUp);
+  return true;
 }
 
-
-int Bins::getBin(int nBins,double  bins[],double value,double *x0,double *x1){
-  int R=0;
-  if(value <bins[0])return -1;
-  if(value >bins[nBins]){
-    *x0 = bins[nBins-1];
-    *x1 = bins[nBins];
-    return nBins-1;
-  }
-
-  for(R=0;R<nBins;R++){
-    if(bins[R]>value)break;	
-  }
-  R--;
-  if(x0) *x0=bins[R];
-  if(x1) *x1=bins[R+1];
-  return R;	
+int Bins::getBinNumber(std::vector<int>& bins, double value){
+  if(value <= bins.front() || value >= bins.back()) return -1;
+  std::vector<int>::iterator binUp = bins.begin() + 1;
+  while(value > *binUp) ++binUp;
+  return binUp - bins.begin() - 1;
 }
 
-void Bins::getBins_int( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax, bool plotLog){
-  double Lower_exact;
-  int nBins = nBins_total-1;
-  const double dx = (plotLog) ? pow((xmax / xmin), (1. / (double)nBins)) : ((xmax - xmin) / (double)nBins);
-  Lower[0] = xmin;
-  Lower_exact = Lower[0];
-  for(int i = 1; i != nBins; ++i){
-    if(plotLog){
-      Lower_exact *= dx;
-      Lower[i] = TMath::Ceil(Lower_exact);
-    } else {
-      Lower[i] = TMath::Ceil(Lower[i-1] + dx);
-    }
+void Bins::getBins_int(std::vector<int>& bins, int nBins, double xmin, double xmax, bool log){
+  const double dx = (log ? std::pow((xmax/xmin), (1./(double)nBins)) : ((xmax - xmin)/(double)nBins));
+  bins.push_back((int)xmin);
+  double binEdge = xmin;
+  for(int i = 1; i < nBins; ++i){
+    if(log) binEdge *= dx;
+    else binEdge += (int)dx;
+    bins.push_back((int)(binEdge + 1));
   }
-  Lower[nBins] = xmax;
+  bins.push_back((int)xmax);
 }
--- RecoJets/JetAlgorithms/src/QGLikelihoodCalculator.cc
+++ RecoJets/JetAlgorithms/src/QGLikelihoodCalculator.cc
@@ -18,10 +18,12 @@ QGLikelihoodCalculator::QGLikelihoodCalculator( TString dataDir, bool chs){
 
   TString histoFileName = "ReducedHisto_2012.root";
   if(chs) histoFileName = "ReducedHisto_2012_CHS.root";
-  histoFile_ = TFile::Open(TString(edm::FileInPath(dataDir + histoFileName).fullPath()));
+  histoFile = TFile::Open(TString(edm::FileInPath(dataDir + histoFileName).fullPath()));
 
-  nPtBins_ = 21;
-  nRhoBins_ = 45 ;
+  Bins::getBins_int(RhoBins, Bins::nRhoBins, Bins::Rho0, Bins::Rho1, false);
+  Bins::getBins_int(PtBins, Bins::nPtBins, Bins::Pt0, Bins::Pt1, true);
+  PtBins.push_back(4000);
+
 }
 
 
@@ -47,33 +49,26 @@ float QGLikelihoodCalculator::computeQGLikelihood2012( float pt, float eta, floa
   vars.push_back(ptD_QC);
   vars.push_back(-log(axis2_QC)); //-log
 
-  double RhoBins[100];
-  double PtBins[100];
+  std::vector<int> RhoBins;
+  std::vector<int> PtBins;
 
-  Bins::getBins_int(Bins::nPtBins+1,PtBins,Bins::Pt0,Bins::Pt1,true);
-  PtBins[Bins::nPtBins+1]=Bins::PtLastExtend;
-  Bins::getBins_int(Bins::nRhoBins+1,RhoBins,Bins::Rho0,Bins::Rho1,false);
+  Bins::getBins_int(RhoBins, Bins::nRhoBins, Bins::Rho0, Bins::Rho1, false);
+  Bins::getBins_int(PtBins, Bins::nPtBins, Bins::Pt0, Bins::Pt1, true);
+  PtBins.push_back(4000);
 
-  double ptMin=0.;
-  double ptMax=0;
-  double rhoMin=0.;
-  double rhoMax=0;
-
-  if(Bins::getBin(Bins::nPtBins,PtBins,pt,&ptMin,&ptMax) <0 ) return -1;
-  if(Bins::getBin(Bins::nRhoBins,RhoBins,rho,&rhoMin,&rhoMax) <0 ) return -1;
+  int ptMin=0, ptMax = 0, rhoMin = 0, rhoMax = 0;
+  if(!Bins::getBin(PtBins, pt, ptMin, ptMax)) return -1;
+  if(!Bins::getBin(RhoBins, rho, rhoMin, rhoMax)) return -1;
 
   float Q=1;
   float G=1;
   char histoName[1023];
-  ptMin=ceil(ptMin);
-  ptMax=ceil(ptMax);
-  rhoMin=floor(rhoMin);
-  rhoMax=floor(rhoMax);
+
 
   for(unsigned int i=0;i<vars.size();i++){
     //get Histo
     float Qi(1),Gi(1),mQ,mG;
-    sprintf( histoName, "rhoBins_pt%.0f_%.0f/%s_quark_pt%.0f_%.0f_rho%.0f", ptMin, ptMax,varName[i].c_str(), ptMin, ptMax, rhoMin);
+    sprintf( histoName, "rhoBins_pt%d_%d/%s_quark_pt%d_%d_rho%d", ptMin, ptMax,varName[i].c_str(), ptMin, ptMax, rhoMin);
     if( plots_[histoName] == nullptr ) { //first time 
       plots_[histoName]=(TH1F*)histoFile_->Get(histoName); 
       if( plots_[histoName]->GetEntries()<50 ) plots_[histoName]->Rebin(5); // try to make it more stable
@@ -85,13 +80,13 @@ float QGLikelihoodCalculator::computeQGLikelihood2012( float pt, float eta, floa
     Qi=plots_[histoName]->GetBinContent(plots_[histoName]->FindBin(vars[i]));
     mQ=plots_[histoName]->GetMean();
 	
-    sprintf( histoName, "rhoBins_pt%.0f_%.0f/%s_gluon_pt%.0f_%.0f_rho%.0f", ptMin, ptMax,varName[i].c_str(), ptMin, ptMax, rhoMin);
+    sprintf( histoName, "rhoBins_pt%d_%d/%s_gluon_pt%d_%d_rho%d", ptMin, ptMax,varName[i].c_str(), ptMin, ptMax, rhoMin);
     if( plots_[histoName] == nullptr ) { // first time
       plots_[histoName]=(TH1F*)histoFile_->Get(histoName);
       if( plots_[histoName]->GetEntries()<50 ) plots_[histoName]->Rebin(5); // try to make it more stable
       else if( plots_[histoName]->GetEntries()<500 ) plots_[histoName]->Rebin(2); // try to make it more stable
     }
-    if( plots_[histoName] == NULL ) fprintf(stderr,"Histo %s does not exists\n",histoName); //DEBUG
+    if( plots_[histoName] == nullptr) fprintf(stderr,"Histo %s does not exists\n",histoName); //DEBUG
     plots_[ histoName]->Scale(1./plots_[histoName]->Integral("width")); 
 
     Gi=plots_[histoName]->GetBinContent(plots_[histoName]->FindBin(vars[i]));
