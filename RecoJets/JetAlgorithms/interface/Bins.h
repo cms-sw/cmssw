8bf8e74a767e21dcfdb9d915c8844f24402475d5
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-02-28 00:37:00 +0100

    Simplify bin functions

--- RecoJets/JetAlgorithms/interface/Bins.h
+++ RecoJets/JetAlgorithms/interface/Bins.h
@@ -1,23 +1,19 @@
 #ifndef BINS_H
 #define BINS_H
-
-#include "TMath.h"
-
+#include <vector>
 
 class Bins{
-public:
-const static int nRhoBins=40; //18
-const static int nPtBins=20; //20
-const static int Pt0=20;  //20
-const static int Pt1=2000; //1000
-const static int Rho0=0;  //2
-const static int Rho1=40; //20
-const static int PtLastExtend=4000; //3500
+  public:
+  const static int nRhoBins=40;
+  const static int nPtBins=20;
+  const static int Pt0=20;
+  const static int Pt1=2000;
+  const static int Rho0=0;
+  const static int Rho1=40;
+  const static int PtLastExtend=4000;
 
-static int getBins(double  *bins,int nBins,double MinBin=15.0,double MaxBin=1000.,bool log=false);
-static int getBin(int nBins,double  *bins,double value,double*x0=0,double*x1=0);
-static void getBins_int( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax, bool plotLog=true); 
-
+  static bool getBin(std::vector<int>& bins, double value, int& low, int& up);
+  static void getBins_int(std::vector<int>& bins, int nBins, double xmin, double xmax, bool plotLog=true);
 };
 
 
