d04f3ccfccbda592508fa76e0e1baa5bfab15c45
federico de guio <federico.de.guio@cern.ch>
2014-02-27 10:36:05 +0100

    driver3

--- DQMServices/Components/test/driver3a.sh
+++ DQMServices/Components/test/driver3a.sh
@@ -7,7 +7,7 @@ numev=3
 step=a
 DQMSEQUENCE=DQM
 
-cmsDriver.py SingleMuPt10.cfi -s GEN,SIM,DIGI:pdigi_valid,L1,DIGI2RAW,HLT:GRun -n ${numev} --eventcontent FEVTDEBUGHLT --conditions auto:startup_GRun --mc --no_exec --python_filename=test_${tnum}_${step}_1.py
+cmsDriver.py SingleMuPt10.cfi -s GEN,SIM,DIGI:pdigi_valid,L1,DIGI2RAW,HLT:GRun -n ${numev} --eventcontent FEVTDEBUGHLT,DQM --datatier RECO,DQMROOT --conditions auto:startup_GRun --mc --no_exec --python_filename=test_${tnum}_${step}_1.py
 
 cmsRun -e test_${tnum}_${step}_1.py >& p3.0.log
 
@@ -17,25 +17,25 @@ fi
 
 mv FrameworkJobReport{,_3_${step}_1}.xml
 
-#cmsDriver.py step2_MC1_1 -s RAW2DIGI,RECO,VALIDATION:validation_preprod -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.1.log 
+#cmsDriver.py step2_MC1_1 -s RAW2DIGI,RECO,VALIDATION:validation_preprod -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM,DQM --datatier RECO,DQMROOT --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.1.log 
 
 #if [ $? -ne 0 ]; then
 #	return 1
 #fi
 
-#cmsDriver.py step2_MC1_2 -s RAW2DIGI,RECO,${DQMSEQUENCE} -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.2.log 
+#cmsDriver.py step2_MC1_2 -s RAW2DIGI,RECO,${DQMSEQUENCE} -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM,DQM --datatier RECO,DQMROOT --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.2.log 
 
 #if [ $? -ne 0 ]; then
 #	return 1
 #fi
 
-#cmsDriver.py step2_MC1_3 -s RAW2DIGI,RECO,VALIDATION:validation_preprod,${DQMSEQUENCE} -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.3.log 
+#cmsDriver.py step2_MC1_3 -s RAW2DIGI,RECO,VALIDATION:validation_preprod,${DQMSEQUENCE} -n 3 --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM,DQM --datatier RECO,DQMROOT --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py >& p3.3.log 
 
 #if [ $? -ne 0 ]; then
 #	return 1
 #fi
 
-cmsDriver.py step2_MC1_4 -s RAW2DIGI,RECO,VALIDATION,${DQMSEQUENCE} -n ${numev} --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py 
+cmsDriver.py step2_MC1_4 -s RAW2DIGI,RECO,VALIDATION,${DQMSEQUENCE} -n ${numev} --filein file:SingleMuPt10_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root --eventcontent RECOSIM,DQM --datatier RECO,DQMROOT --conditions auto:mc --mc --customise DQMServices/Components/test/customRecoSim.py 
 #mv memory.out smemory_p.3.4.log
 #mv checkMem.log p${tnum}.4.log
 
