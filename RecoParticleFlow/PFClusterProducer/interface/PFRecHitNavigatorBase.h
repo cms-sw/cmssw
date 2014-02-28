da18d93899e7701236754ecd6dc328150d663a53
Lindsey Gray <lindsey.gray@gmail.com>
2014-02-28 19:01:48 +0100

    improve PF RecHit producer timing, massive gains for larger rechit collections

--- RecoParticleFlow/PFClusterProducer/interface/PFRecHitNavigatorBase.h
+++ RecoParticleFlow/PFClusterProducer/interface/PFRecHitNavigatorBase.h
@@ -34,12 +34,16 @@ class PFRecHitNavigatorBase {
  protected:
 
   void associateNeighbour(const DetId& id, reco::PFRecHit& hit,std::auto_ptr<reco::PFRecHitCollection>& hits,edm::RefProd<reco::PFRecHitCollection>& refProd,short eta, short phi,short depth) {
-    for( unsigned int i=0;i<hits->size();++i) 
-      if (hits->at(i).detId()==id.rawId()) {
-	reco::PFRecHitRef ref(refProd,i);
-	hit.addNeighbour(eta,phi,depth,ref);
-	break;
-      }
+    const reco::PFRecHit temp(id,PFLayer::NONE,0.0,math::XYZPoint(0,0,0),math::XYZVector(0,0,0),std::vector<math::XYZPoint>());
+    auto found_hit = std::lower_bound(hits->begin(),hits->end(),
+				      temp,
+				      [](const reco::PFRecHit& a, 
+					 const reco::PFRecHit& b){
+					return a.detId() < b.detId();
+				      });
+    if( found_hit != hits->end() && found_hit->detId() == id.rawId() ) {
+      hit.addNeighbour(eta,phi,depth,reco::PFRecHitRef(refProd,std::distance(hits->begin(),found_hit)));
+    }    
   }
 
 
