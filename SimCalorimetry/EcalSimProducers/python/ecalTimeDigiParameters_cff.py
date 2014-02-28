cbc88ccb29e4b7f4c7cc5cef1292f92c3e7f4710
Paolo Meridiani <paolo.meridiani@cern.ch>
2014-02-28 14:45:09 +0100

    fixing the EB/EE inversion

--- SimCalorimetry/EcalSimProducers/python/ecalTimeDigiParameters_cff.py
+++ SimCalorimetry/EcalSimProducers/python/ecalTimeDigiParameters_cff.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 ecal_time_digi_parameters = cms.PSet(
     hitsProducer     = cms.string('g4SimHits'),
-    EEtimeDigiCollection = cms.string('EBTimeDigi'),
-    EBtimeDigiCollection = cms.string('EETimeDigi'),
+    EBtimeDigiCollection = cms.string('EBTimeDigi'),
+    EEtimeDigiCollection = cms.string('EETimeDigi'),
     timeLayerBarrel = cms.int32(7),
     timeLayerEndcap = cms.int32(3)
     )
