22fa3d2debc6f653b05155843022da9ec8ad374e
Claudio Caputo <cld.cpt@gmail.com>
2014-03-03 17:51:54 +0100

    clean up

--- Validation/MuonGEMRecHits/python/CfiFile_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-RecHitAnalyzer = cms.EDAnalyzer("RecHitAnalyzer",
-                      debug = cms.untracked.bool(True),
-                      folderPath = cms.untracked.string('GEMBasicPlots/'),
-                      EffSaveRootFile = cms.untracked.bool(True),
-                      EffRootFileName = cms.untracked.string('GEMRecHit_ME.root')
-)
