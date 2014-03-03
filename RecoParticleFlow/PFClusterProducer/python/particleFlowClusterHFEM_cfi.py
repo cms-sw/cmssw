b6be100fe2a87291557c82367379aa3b9bdc0799
Lindsey Gray <lindsey.gray@gmail.com>
2014-03-03 13:58:44 +0100

    factor config into each cluster type

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHFEM_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHFEM_cfi.py
@@ -1,24 +1,88 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterCleaners_cfi \
-     import *
+#### PF CLUSTER HFEM ####
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterSeeders_cfi import *
+#cleaning
+_spikeAndDoubleSpikeCleaner_HFEM = cms.PSet(
+    algoName = cms.string("SpikeAndDoubleSpikeCleaner"),    
+    cleaningByDetector = cms.VPSet(
+       cms.PSet( detector = cms.string("HF_EM"),
+                 #single spike
+                 singleSpikeThresh = cms.double(80.0),
+                 minS4S1_a = cms.double(0.11), #constant term
+                 minS4S1_b = cms.double(-0.19), #log pt scaling
+                 #double spike
+                 doubleSpikeThresh = cms.double(1e9),
+                 doubleSpikeS6S2 = cms.double(-1.0),
+                 energyThresholdModifier = cms.double(1.0), ## aka "tighterE"
+                 fractionThresholdModifier = cms.double(1.0) ## aka "tighterF"
+                 )
+       )
+    )
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterizers_cfi import *
+#seeding
+_localMaxSeeds_HFEM = cms.PSet(
+    algoName = cms.string("LocalMaximum2DSeedFinder"),
+    thresholdsByDetector = cms.VPSet(
+    cms.PSet( detector = cms.string("HF_EM"),
+              seedingThreshold = cms.double(1.4),
+              seedingThresholdPt = cms.double(0.0)
+              )
+    ),
+    nNeighbours = cms.uint32(0)
+)
 
-from RecoParticleFlow.PFClusterProducer.\
-     particleFlowClusterPositionCalculators_cfi import *
+#topo clusters
+_topoClusterizer_HFEM = cms.PSet(
+    algoName = cms.string("Basic2DGenericTopoClusterizer"),
+    thresholdsByDetector = cms.VPSet(
+    cms.PSet( detector = cms.string("HF_EM"),
+              gatheringThreshold = cms.double(0.8),
+              gatheringThresholdPt = cms.double(0.0)
+              )
+    ),
+    useCornerCells = cms.bool(False)
+)
 
-from RecoParticleFlow.PFClusterProducer.\
-     particleFlowClusterEnergyCorrectors_cfi import *
+#position calc
+_positionCalcHFEM_cross_nodepth = cms.PSet(
+    algoName = cms.string("Basic2DGenericPFlowPositionCalc"),
+    ##
+    minFractionInCalc = cms.double(1e-9),
+    posCalcNCrystals = cms.int32(5),
+    logWeightDenominator = cms.double(0.8), # same as gathering threshold
+    minAllowedNormalization = cms.double(1e-9)    
+)
+
+_positionCalcHFEM_all_nodepth = _positionCalcHFEM_cross_nodepth.clone(
+    posCalcNCrystals = cms.int32(-1)
+    )
+
+#pf clusters
+_pfClusterizer_HFEM = cms.PSet(
+    algoName = cms.string("Basic2DGenericPFlowClusterizer"),
+    #pf clustering parameters
+    minFractionToKeep = cms.double(1e-7),
+    positionCalc = _positionCalcHFEM_cross_nodepth,
+    allCellsPositionCalc = _positionCalcHFEM_all_nodepth,
+    showerSigma = cms.double(10.0),
+    stoppingTolerance = cms.double(1e-8),
+    maxIterations = cms.uint32(50),
+    excludeOtherSeeds = cms.bool(True),
+    minFracTot = cms.double(1e-20), ## numerical stabilization
+    recHitEnergyNorms = cms.VPSet(
+    cms.PSet( detector = cms.string("HF_EM"),
+              recHitEnergyNorm = cms.double(0.8)
+              )
+    )
+)
 
 particleFlowClusterHFEM = cms.EDProducer(
     "PFClusterProducer",
     recHitsSource = cms.InputTag("particleFlowRecHitHCAL:HFEM"),
-    recHitCleaners = cms.VPSet(spikeAndDoubleSpikeCleaner_HFEM),
-    seedFinder = localMaxSeeds_HF,
-    initialClusteringStep = topoClusterizer_HF,
-    pfClusterBuilder = pfClusterizer_HF
+    recHitCleaners = cms.VPSet(_spikeAndDoubleSpikeCleaner_HFEM),
+    seedFinder = _localMaxSeeds_HFEM,
+    initialClusteringStep = _topoClusterizer_HFEM,
+    pfClusterBuilder = _pfClusterizer_HFEM
     )
 
--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterPS_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterPS_cfi.py
@@ -1,24 +1,79 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterCleaners_cfi \
-     import *
+#### PF CLUSTER PRESHOWER ####
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterSeeders_cfi import *
+#cleaning
 
-from RecoParticleFlow.PFClusterProducer.particleFlowClusterizers_cfi import *
+#seeding
+_localMaxSeeds_PS = cms.PSet(
+    algoName = cms.string("LocalMaximum2DSeedFinder"),
+    ### seed finding parameters    
+    thresholdsByDetector = cms.VPSet(
+    cms.PSet( detector = cms.string("PS1"),
+              seedingThreshold = cms.double(1.2e-4),
+              seedingThresholdPt = cms.double(0.0)
+              ),
+    cms.PSet( detector = cms.string("PS2"),
+              seedingThreshold = cms.double(1.2e-4),
+              seedingThresholdPt = cms.double(0.0)
+              )
+    ),
+    nNeighbours = cms.uint32(4)
+)
 
-from RecoParticleFlow.PFClusterProducer.\
-     particleFlowClusterPositionCalculators_cfi import *
+#topo clusters
+_topoClusterizer_PS = cms.PSet(
+    algoName = cms.string("Basic2DGenericTopoClusterizer"),
+    thresholdsByDetector = cms.VPSet(
+    cms.PSet( detector = cms.string("PS1"),
+              gatheringThreshold = cms.double(6e-5),
+              gatheringThresholdPt = cms.double(0.0)
+              ),
+    cms.PSet( detector = cms.string("PS2"),
+              gatheringThreshold = cms.double(6e-5),
+              gatheringThresholdPt = cms.double(0.0)
+              )
+    ),    
+    useCornerCells = cms.bool(False)
+)
 
-from RecoParticleFlow.PFClusterProducer.\
-     particleFlowClusterEnergyCorrectors_cfi import *
+#position calc
+_positionCalcPS_all_nodepth = cms.PSet(
+    algoName = cms.string("Basic2DGenericPFlowPositionCalc"),
+    ##
+    minFractionInCalc = cms.double(1e-9),
+    posCalcNCrystals = cms.int32(-1),
+    logWeightDenominator = cms.double(6e-5), # same as gathering threshold
+    minAllowedNormalization = cms.double(1e-9)
+)
+
+#pf clustering
+_pfClusterizer_PS = cms.PSet(
+    algoName = cms.string("Basic2DGenericPFlowClusterizer"),
+    #pf clustering parameters
+    minFractionToKeep = cms.double(1e-7),
+    positionCalc = _positionCalcPS_all_nodepth,
+    showerSigma = cms.double(0.3),
+    stoppingTolerance = cms.double(1e-8),
+    maxIterations = cms.uint32(50),
+    excludeOtherSeeds = cms.bool(True),
+    minFracTot = cms.double(1e-20), ## numerical stabilization
+    recHitEnergyNorms = cms.VPSet(
+    cms.PSet( detector = cms.string("PS1"),
+              recHitEnergyNorm = cms.double(6e-5)
+              ),
+    cms.PSet( detector = cms.string("PS2"),
+              recHitEnergyNorm = cms.double(6e-5)
+              )
+    )
+)
 
 particleFlowClusterPS = cms.EDProducer(
     "PFClusterProducer",
     recHitsSource = cms.InputTag("particleFlowRecHitPS"),
     recHitCleaners = cms.VPSet(),
-    seedFinder = localMaxSeeds_PS,
-    initialClusteringStep = topoClusterizer_PS,
-    pfClusterBuilder = pfClusterizer_PS,
+    seedFinder = _localMaxSeeds_PS,
+    initialClusteringStep = _topoClusterizer_PS,
+    pfClusterBuilder = _pfClusterizer_PS,
     )
 
