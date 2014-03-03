909a8d636f3fe0cdd767767a8f410bb2d464406a
Paolo Meridiani <paolo.meridiani@cern.ch>
2014-03-03 09:34:10 +0100

    working reco. Missing z vertex correction

--- RecoLocalCalo/EcalRecProducers/python/ecalDetailedTimeRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalDetailedTimeRecHit_cfi.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 ecalDetailedTimeRecHit = cms.EDProducer("EcalDetailedTimeRecHitProducer",
-    EERecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
-    EBRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
-    EBDetailedTimeRecHitCollection = cms.string('EcalRecHitsEB'),
-    EEDetailedTimeRecHitCollection = cms.string('EcalRecHitsEE')
+                                        EERecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+                                        EBRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+                                        EETimeDigiCollection = cms.InputTag("mix","EETimeDigi"),
+                                        EBTimeDigiCollection = cms.InputTag("mix","EBTimeDigi"),
+                                        EBDetailedTimeRecHitCollection = cms.string('EcalRecHitsEB'),
+                                        EEDetailedTimeRecHitCollection = cms.string('EcalRecHitsEE')
 )
