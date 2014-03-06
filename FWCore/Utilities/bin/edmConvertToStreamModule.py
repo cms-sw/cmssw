0bbc688a49f2f964222074662ccdfc9beed2f63d
Christopher Jones <chrisdjones15@gmail.com>
2014-03-06 15:49:27 -0600

    Improved reporting of problems and handling large number of modules

--- FWCore/Utilities/bin/edmConvertToStreamModule.py
+++ FWCore/Utilities/bin/edmConvertToStreamModule.py
@@ -3,28 +3,60 @@
 import subprocess
 import shutil
 import sys
+import re
 
 kProducer = 0
 kFilter = 1
 
-def find_file_for_module(name):
+def find_all_module_classes():
+  s = set()
+  found = subprocess.check_output(["git","grep", "class *[A-Za-z0-9_<>]* *: *public "])
+  s.update(found.split("\n"))
+
+  ret = dict()
+  for l in s:
+    parts = l.split(":")
+    if len(parts)>2:
+      file = parts[0]
+      name = parts[1]
+      name = name[name.find("class")+5:].strip()
+      ret.setdefault(name,[]).append((":".join(parts[2:]), file))
+  
+  return ret
+
+def print_lines(lines):
+  for l in lines:
+    if len(l)>0:
+      print "  ",l
+
+def find_file_for_module(name,all_modules):
+  if name in all_modules:
+    info = all_modules[name]
+    if len(info) != 1:
+      print "ERROR: more than one declaration for '"+name+"'\n"
+      for inherits,file in info:
+        print "  ",file, inherits
+      return (None,None)
+    inherits,file = info[0]
+    if -1 != inherits.find("edm::EDProducer"):
+      type = kProducer
+    elif -1 != inherits.find("edm::EDFilter"):
+      type = kFilter
+    else:
+      print "ERROR: class '"+name+"' does not directly inherit from EDProducer or EDFilter\n  "+inherits
+      return (None,None)
+    return (file,type)
+  print "ERROR: did not find a standard class declaration for '"+name+"'"
   try:
-    found = subprocess.check_output(["git","grep", "class *"+name+" *: *public .*edm::EDProducer"])
-    type = kProducer
+    found = subprocess.check_output(["git","grep", "class *"+name+" *:"])
+    print_lines( found.split("\n") )
   except:
     try:
-      found = subprocess.check_output(["git","grep", "class *"+name+" *: *public .*edm::EDFilter"])
-      type = kFilter
+      found = subprocess.check_output(["git","grep", "typedef *.* "+name])
+      print_lines( found.split("\n") )
     except:
-      print "ERROR: Unable to find class declaration for "+name
-      exit(1)
-  lfound = found.split("\n")
-  if len(lfound) > 2:
-    print "ERROR: found more than one declaration of "+name
-    print lfound
-    exit(1)
-  s = lfound[0]
-  return (s[:s.find(":")],type)
+      pass
+  return (None,None)
   
 def checkout_package(fileName):
   c = fileName.split("/")
@@ -51,14 +83,17 @@ def edit_file(fileName,moduleType,moduleName):
   
   lookingForChanges = True
   for l in fOld.readlines():
+    addedInclude = False
     if lookingForChanges:
       if -1 != l.find("#include"):
         if moduleType == kProducer:
           if -1 != l.find("FWCore/Framework/interface/EDProducer.h"):
             l='#include "FWCore/Framework/interface/stream/EDProducer.h"\n'
+            addedInclude = True
         elif moduleType == kFilter:
           if -1 != l.find("FWCore/Framework/interface/EDFilter.h"):
             l = '#include "FWCore/Framework/interface/stream/EDFilter.h"\n'
+            addedInclude = True
       elif -1 != l.find("class"):
         if -1 != l.find(moduleName):
           if moduleType == kProducer:
@@ -69,6 +104,14 @@ def edit_file(fileName,moduleType,moduleName):
             if -1 != l.find("edm::EDFilter"):
               l=l.replace("edm::EDFilter","edm::stream::EDFilter<>")
     fNew.write(l)
+    if -1 != l.find(" beginJob("):
+      print " WARNING: beginJob found but not supported by stream"
+      print "  ",l
+    if -1 != l.find(" endJob("):
+      print " WARNING: endJob found but not supported by stream"
+      print "  ",l
+  if not addedInclude:
+    print " WARNING: did not write include into "+fileName
   fNew.close()
   fOld.close()
   shutil.move(fileName,fileName+"_OLD")
@@ -76,8 +119,12 @@ def edit_file(fileName,moduleType,moduleName):
 
 modules = sys.argv[1:]
 
+print "getting info"
+all_mods_info= find_all_module_classes()
+
+
 for m in modules:
-  f,t = find_file_for_module(m)
+  f,t = find_file_for_module(m,all_mods_info)
   if f:
     checkout_package(f)
     edit_file(f,t,m)
