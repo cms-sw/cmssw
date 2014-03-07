930e967b6bb5ffa756b03ecaa2888f7a6524acf8
Sven Dildick <sven.dildick@cern.ch>
2014-03-07 13:15:18 +0100

    Distinguish fast from full RECO

--- SLHCUpgradeSimulations/Configuration/python/me0Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/me0Customs.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-def customise(process):
+def customise(process,isFast):
     if hasattr(process,'digitisation_step'):
         process=customise_Digi(process)
     if hasattr(process,'L1simulation_step'):
@@ -10,14 +10,15 @@ def customise(process):
     if hasattr(process,'RawToDigi'):
         process=customise_RawToDigi(process)
     if hasattr(process,'reconstruction'):
-        process=customise_Reco(process)
+        process=customise_RecoFull(process)
+    if hasattr(process,'famosWithEverything'):
+        process=customise_RecoFast(process)
     if hasattr(process,'dqmoffline_step'):
         process=customise_DQM(process)
     if hasattr(process,'dqmHarvesting'):
         process=customise_harvesting(process)
     if hasattr(process,'validation_step'):
         process=customise_Validation(process)
-
     return process
 
 def customise_Digi(process):
@@ -45,16 +46,36 @@ def customise_DigiToRaw(process):
 def customise_RawToDigi(process):
     return process
 
-def customise_Reco(process):
-    ## local 
+def customise_LocalReco(process):
     process.load('RecoLocalMuon.GEMRecHit.me0RecHits_cfi')
     process.load('RecoLocalMuon.GEMRecHit.me0Segments_cfi')
+    
     process.me0RecHits.me0DigiLabel = cms.InputTag("simMuonME0Digis")
     process.me0Segments.me0RecHitLabel = cms.InputTag("me0RecHits")
+    
     process.muonlocalreco += process.me0RecHits
     process.muonlocalreco += process.me0Segments
+    
+    process=outputCustoms(process)
+    return process
 
-    ## global
+def customise_GlobalRecoFast(process):
+    process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi")
+    process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi")
+    process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
+
+    process.load('FastSimulation.Muons.me0SegmentProducer_cfi')
+    process.load('RecoMuon.MuonIdentification.me0SegmentMatcher_cfi')
+    process.load('RecoMuon.MuonIdentification.me0MuonConverter_cfi')
+
+    process.reconstructionWithFamos += process.me0SegmentProducer
+    process.reconstructionWithFamos += process.me0SegmentMatcher
+    process.reconstructionWithFamos += process.me0MuonConverter
+    
+    process=outputCustoms(process)
+    return process
+    
+def customise_GlobalRecoFull(process):
     process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi")
     process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi")
     process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
@@ -66,15 +87,25 @@ def customise_Reco(process):
     process.reconstruction += process.me0SegmentProducer
     process.reconstruction += process.me0SegmentMatcher
     process.reconstruction += process.me0MuonConverter
-
+    
     process=outputCustoms(process)
     return process
+
+def customise_RecoFast(process):
+    customise_LocalReco(process)
+    customise_GlobalRecoFast(process)
+    return proces
+
+def customise_RecoFull(process):
+    customise_LocalReco(process)
+    customise_GlobalRecoFull(process)
+    return proces
 
 def customise_DQM(process):
     return process
 
 def customise_harvesting(process):
-    return (process)
+    return process
 
 def customise_Validation(process):
     return process
