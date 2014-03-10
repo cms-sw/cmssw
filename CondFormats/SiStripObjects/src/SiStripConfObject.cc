903a1be5220f93565dbcc8dc7ce7bcbc307622a7
Andrea Venturi <andrea.venturi@cern.ch>
2014-03-10 15:23:45 +0100

    Added the possibility to store vectors of integers and strings in SiStripConfObject

--- CondFormats/SiStripObjects/src/SiStripConfObject.cc
+++ CondFormats/SiStripObjects/src/SiStripConfObject.cc
@@ -1,5 +1,98 @@
 #include "CondFormats/SiStripObjects/interface/SiStripConfObject.h"
 
+template <>
+bool SiStripConfObject::put<std::vector<int> >( const std::string & name, const std::vector<int> & inputValue )
+{
+  std::stringstream ss;
+  for(std::vector<int>::const_iterator elem=inputValue.begin();elem!=inputValue.end();++elem) {
+    ss << *elem << " ";
+  }
+  if( parameters.insert(std::make_pair(name, ss.str())).second ) return true;
+  return false;
+}
+
+template <>
+bool SiStripConfObject::update<std::vector<int> >( const std::string & name, const std::vector<int> & inputValue )
+{
+  parMap::iterator it = parameters.find(name);
+  if (it == parameters.end()) {
+    std::cout << "WARNING in SiStripConfObject::update: parameter " << name << " not found, "
+	      << "so cannot be updated to the vector of int of size'" << inputValue.size() << "'." << std::endl;
+    return false;
+  } else {
+    std::stringstream ss;
+    for(std::vector<int>::const_iterator elem=inputValue.begin();elem!=inputValue.end();++elem) {
+      ss << *elem << " ";
+    }
+    it->second = ss.str();
+    return true;
+  }
+}
+
+template <>
+std::vector<int> SiStripConfObject::get<std::vector<int> >( const std::string & name ) const
+{
+  std::vector<int> returnValue;
+  parMap::const_iterator it = parameters.find(name);
+  std::stringstream ss;
+  if( it != parameters.end() ) {
+    ss << it->second;
+    int elem;
+    while(ss >> elem) returnValue.push_back(elem);
+  }
+  else {
+    std::cout << "WARNING: parameter " << name << " not found. Returning default value" << std::endl;
+  }
+  return returnValue;
+}
+
+template <>
+bool SiStripConfObject::put<std::vector<std::string> >( const std::string & name, const std::vector<std::string> & inputValue )
+{
+  std::stringstream ss;
+  for(std::vector<std::string>::const_iterator elem=inputValue.begin();elem!=inputValue.end();++elem) {
+    ss << *elem << " ";
+  }
+  if( parameters.insert(std::make_pair(name, ss.str())).second ) return true;
+  return false;
+}
+
+template <>
+bool SiStripConfObject::update<std::vector<std::string> >( const std::string & name, const std::vector<std::string> & inputValue )
+{
+  parMap::iterator it = parameters.find(name);
+  if (it == parameters.end()) {
+    std::cout << "WARNING in SiStripConfObject::update: parameter " << name << " not found, "
+	      << "so cannot be updated to the vector of std::string of size'" << inputValue.size() << "'." << std::endl;
+    return false;
+  } else {
+    std::stringstream ss;
+    for(std::vector<std::string>::const_iterator elem=inputValue.begin();elem!=inputValue.end();++elem) {
+      ss << *elem << " ";
+    }
+    it->second = ss.str();
+    return true;
+  }
+}
+
+template <>
+std::vector<std::string> SiStripConfObject::get<std::vector<std::string> >( const std::string & name ) const
+{
+  std::vector<std::string> returnValue;
+  parMap::const_iterator it = parameters.find(name);
+  std::stringstream ss;
+  if( it != parameters.end() ) {
+    ss << it->second;
+    std::string elem;
+    while(ss >> elem) returnValue.push_back(elem);
+  }
+  else {
+    std::cout << "WARNING: parameter " << name << " not found. Returning default value" << std::endl;
+  }
+  return returnValue;
+}
+
+
 void SiStripConfObject::printSummary(std::stringstream & ss) const
 {
   parMap::const_iterator it = parameters.begin();
