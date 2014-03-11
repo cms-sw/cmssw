ebfdd077c464e522d1ae1066938951f442ece4f3
Davide <davide.pagano@cern.ch>
2014-03-11 13:29:14 +0100

    dyt2.0

--- RecoMuon/L3MuonProducer/python/L3MuonCandidates_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-L3MuonCandidates = cms.EDProducer("L3MuonCandidateProducer",
-    InputObjects = cms.InputTag("L3Muons"),
-    InputLinksObjects = cms.InputTag(""),
-    MuonPtOption = cms.string("Global")
-)
--- RecoMuon/L3MuonProducer/python/L3TkMuonProducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltL3MuonsNoVtx = cms.EDProducer( "L3TkMuonProducer",
-    InputObjects = cms.InputTag( "hltL3TkTracksFromL2NoVtx" )
-)
-
