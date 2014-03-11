5074cbda6517a9613fbc7ef367fa4892b5be8b90
Lindsey Gray <lindsey.gray@gmail.com>
2014-03-11 10:03:51 +0100

    factorize python config for confDB

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoParticleFlow.PFClusterProducer.particleFlowClusterECALUncorrected_cfi import *
 from RecoParticleFlow.PFClusterProducer.particleFlowClusterECAL_cfi import *
 
 particleFlowClusterECALSequence = cms.Sequence(
