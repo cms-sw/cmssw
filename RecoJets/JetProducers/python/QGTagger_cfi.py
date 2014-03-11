325473c0eea0721d6e5fa974746d81885dff6279
John <jstupak@fnal.gov>
2014-03-10 23:12:27 -0500

    bugfix, added toolbox config, switched to fixedGridRho, added puJetId weights

--- RecoJets/JetProducers/python/QGTagger_cfi.py
+++ RecoJets/JetProducers/python/QGTagger_cfi.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 #needed for Likelihood tagger
-from RecoJets.JetProducers.kt4PFJets_cfi import *
-kt6PFJetsIsoQG = kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True )
-kt6PFJetsIsoQG.Rho_EtaMax = cms.double(2.5)
+#from RecoJets.JetProducers.kt4PFJets_cfi import *
+#kt6PFJetsIsoQG = kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True )
+#kt6PFJetsIsoQG.Rho_EtaMax = cms.double(2.5)
 
 QGTagger = cms.EDProducer('QGTagger',
-  srcRhoIso     = cms.InputTag('kt6PFJetsIsoQG','rho'),
+  srcRhoIso     = cms.InputTag('fixedGridRhoFastjetAll'),
   jec		= cms.string(''),
   dataDir	= cms.string('CondFormats/JetMETObjects/data/'),
   useCHS	= cms.bool(True)
