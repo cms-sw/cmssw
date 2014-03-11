4bf88c3545c3816140c0445ac234899702cc9b5c
Christopher Jones <chrisdjones15@gmail.com>
2014-03-11 13:51:02 -0500

    Fixed thread-safety issues with ElectronLikelihood
    Fixed const correctness problems with object directly and indirectly
    held by ElectronLikelihood. Then enforced that only const functions
    of the direct and indirect objects are called from const member functions
    of ElectronLikelihood. This guarantees thread-safety.

--- RecoEgamma/ElectronIdentification/interface/LikelihoodPdfProduct.h
+++ RecoEgamma/ElectronIdentification/interface/LikelihoodPdfProduct.h
@@ -27,11 +27,11 @@ class LikelihoodPdfProduct {
   void setSplitFrac(const char* specname, const char* catName, float frac=1.0);
 
   //! get the likelihood ratio p(a priori) * L(specName) / L_tot
-  float getRatio(const char* specName, const std::vector<float>& measurements, std::string);
+  float getRatio(const char* specName, const std::vector<float>& measurements, const std::string&) const;
 
  private:
 
-  float getSpeciesProb(const char* specName, const std::vector<float>& measurements, std::string gsfClass);
+  float getSpeciesProb(const char* specName, const std::vector<float>& measurements, const std::string& gsfClass) const;
   std::string _name;
   const ElectronLikelihoodCalibration *_calibration;
   std::vector<LikelihoodSpecies*> _specList;
