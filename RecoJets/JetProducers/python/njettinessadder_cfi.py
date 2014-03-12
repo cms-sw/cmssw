c97f422c83f2a92b58203298509d8f01d0986dff
John <jstupak@fnal.gov>
2014-03-12 01:14:21 -0500

    removed accidental duplicate

--- RecoJets/JetProducers/python/njettinessadder_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-Njettiness = cms.EDProducer("NjettinessAdder",
-                            src=cms.InputTag("ak5PFJetsCHS"),
-                            cone=cms.double(0.5)
-                            )
