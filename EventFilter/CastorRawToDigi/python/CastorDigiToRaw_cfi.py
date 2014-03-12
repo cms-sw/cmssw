b248cf60db7e4b8320e21d26cd407b8975dff307
Michael <mikeh@fnal.gov>
2014-03-12 02:35:45 +0100

    update inputTags

--- EventFilter/CastorRawToDigi/python/CastorDigiToRaw_cfi.py
+++ EventFilter/CastorRawToDigi/python/CastorDigiToRaw_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 castorRawData = cms.EDProducer("CastorDigiToRaw",
-    CASTOR = cms.untracked.InputTag("simCastorDigis"),
+    CASTOR = cms.InputTag("simCastorDigis"),
 )
