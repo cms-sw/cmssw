b23167bc720c20a231e12a0169ea2f995707a27c
Sven Dildick <sven.dildick@cern.ch>
2014-03-13 15:51:38 +0100

    Create rpcCustoms.py

---
+++ SLHCUpgradeSimulations/Configuration/python/rpcCustoms.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+def customise(process):
+    if hasattr(process,'digitisation_step'):
+        process=customise_Digi(process)
+    if hasattr(process,'L1simulation_step'):
+       process=customise_L1Emulator(process)
+    if hasattr(process,'DigiToRaw'):
+        process=customise_DigiToRaw(process)
+    if hasattr(process,'RawToDigi'):
+        process=customise_RawToDigi(process)
+    if hasattr(process,'reconstruction'):
+        process=customise_Reco(process)
+    if hasattr(process,'dqmoffline_step'):
+        process=customise_DQM(process)
+    if hasattr(process,'dqmHarvesting'):
+        process=customise_harvesting(process)
+    if hasattr(process,'validation_step'):
+        process=customise_Validation(process)
+    return process
+
+def customise_Digi(process):
+    return process
+
+def customise_L1Emulator(process):
+    return process
+
+def customise_DigiToRaw(process):
+    return process
+
+def customise_RawToDigi(process):
+    return process
+
+def customise_Reco(process):
+    return process
+
+def customise_DQM(process):
+    return process
+
+def customise_Validation(process):
+    return process
+
+def customise_harvesting(process):
+    return (process)
