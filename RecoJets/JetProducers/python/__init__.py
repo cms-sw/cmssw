9359a6f270b72e5defbe70df8f84693072512f42
Devdatta <devdatta.majumder@cern.ch>
2014-03-14 16:21:10 +0100

    Deleting unnecessary files

--- RecoJets/JetProducers/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/RecoJets/JetProducers/',1)[0])+'/cfipython/slc5_amd64_gcc461/RecoJets/JetProducers')
