c1a24fb8ae80d4683b8b251d5f1a22c66ec48436
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2014-03-14 14:08:03 -0500

    switching to status=71 partons in Pythia8 which are equivalent to status=2 partons in Pythia6

--- PhysicsTools/JetMCAlgos/src/Pythia8PartonSelector.cc
+++ PhysicsTools/JetMCAlgos/src/Pythia8PartonSelector.cc
@@ -1,6 +1,6 @@
 
 /**
- * This is a Pythia8-specific parton selector that selects all status==43, 44, 51, 52, and 62 partons. An explanation of
+ * This is a Pythia8-specific parton selector that selects all status==71 partons. An explanation of
  * the particle status codes returned by Pythia8 can be found in Pythia8 online manual
  * (http://home.thep.lu.se/~torbjorn/pythia81html/ParticleProperties.html).
  */
@@ -25,9 +25,9 @@ Pythia8PartonSelector::run(const edm::Handle<reco::GenParticleCollection> & part
    for(reco::GenParticleCollection::const_iterator it = particles->begin(); it != particles->end(); ++it)
    {
      int status = it->status();
-     if( !(status==43 || status==44 || status==51 || status==52 || status==62) ) continue; // only accept status==43, 44, 51, 52, and 62 particles
-     if( it->numberOfDaughters()==0 ) continue;                                            // skip particle if it has no daughters (likely a documentation line)
-     if( !CandMCTagUtils::isParton( *it ) ) continue;                                      // skip particle if not a parton
+     if( !(status==71) ) continue;                     // only accept status==71 particles
+     if( it->numberOfDaughters()==0 ) continue;        // skip particle if it has no daughters (likely a documentation line)
+     if( !CandMCTagUtils::isParton( *it ) ) continue;  // skip particle if not a parton
 
      partons->push_back( reco::GenParticleRef( particles, it - particles->begin() ) );
    }
