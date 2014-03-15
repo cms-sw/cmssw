124a6b630aa72cc0f4347dc99c5679c0f8c6fc0a
Sven Dildick <sven.dildick@cern.ch>
2014-03-15 04:18:21 -0500

    update GEM digitizer snippet

--- SimMuL1/test/runCustom2023MuL1Trigger_cfg.py
+++ SimMuL1/test/runCustom2023MuL1Trigger_cfg.py
@@ -58,16 +58,7 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 
-#process.Timing = cms.Service("Timing")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
-# customization of the process.pdigi sequence to add the GEM digitizer
-process.load('SimMuon.GEMDigitizer.muonGEMDigis_cfi')
-process.load('SimMuon.GEMDigitizer.muonGEMCSCPadDigis_cfi')
-from SimMuon.GEMDigitizer.customizeGEMDigi import *
-#process = customize_digi_addGEM(process)  # run all detectors digi
-process = customize_digi_addGEM_muon_only(process) # only muon+GEM digi
-#process = customize_digi_addGEM_gem_only(process)  # only GEM digi
 
 ## GEM geometry customization
 from Geometry.GEMGeometry.gemGeometryCustoms import custom_GE11_6partitions_v1
@@ -76,6 +67,10 @@ process = custom_GE11_6partitions_v1(process)
 ## upgrade CSC geometry 
 from SLHCUpgradeSimulations.Configuration.muonCustoms import unganged_me1a_geometry
 process = unganged_me1a_geometry(process)
+
+## GEM digitizer
+from SimMuon.GEMDigitizer.customizeGEMDigi import customize_digi_addGEM_muon_only
+process = customize_digi_addGEM_muon_only(process)
 
 ## upgrade CSC digitizer
 from SLHCUpgradeSimulations.Configuration.muonCustoms import digitizer_timing_pre3_median
@@ -123,7 +118,6 @@ process.source = cms.Source("PoolSource",
   duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
   inputCommands = cms.untracked.vstring('keep  *_*_*_*'),
   fileNames = cms.untracked.vstring('file:out_sim.root')
-#  fileNames = cms.untracked.vstring('/eos/store/user/SingleMuPt2-50Fwdv2_1M/jdimasva-SingleMuPt2-50Fwdv2_100K_DIGI_PU140-7444237097ec40e1cd737724f1a85642/USER')
 )
 
 ## input
