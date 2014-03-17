96638d30a3da4badff9fa6400f42e1ae03b99417
Andrea <andrea.rizzi@cern.ch>
2014-03-17 14:17:13 +0100

    Cleanup before inclusion in reco: make direct fitter configurable, replace clusterScale and densityFactor with single distanceRatio variable

--- RecoVertex/AdaptiveVertexFinder/test/sequenceTest.py
+++ RecoVertex/AdaptiveVertexFinder/test/sequenceTest.py
@@ -4,13 +4,13 @@ process = cms.Process('RECO2')
 
 # import of standard configurations
 
-process.load('Configuration/StandardSequences/GeometryExtended_cff')
+process.load('Configuration/StandardSequences/Geometry_cff')
 process.load('Configuration/StandardSequences/MagneticField_38T_cff')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
 process.load('Configuration/EventContent/EventContent_cff')
 
-process.load('RecoVertex/AdaptiveVertexFinder/inclusiveVertexing_cff')
+#process.load('RecoVertex/AdaptiveVertexFinder/inclusiveVertexing_cff')
 
 
 process.options = cms.untracked.PSet(
@@ -22,7 +22,7 @@ process.source = cms.Source("PoolSource",
     skipEvents = cms.untracked.uint32(51), 
     fileNames = cms.untracked.vstring( 
 #'file:/data1/arizzi/CMSSW_3_5_6/src/MCQCD80120START_C07EAA09-2D2C-DF11-87E7-002618943886.root'
-'/store/relval/CMSSW_3_5_4/RelValQCD_Pt_3000_3500/GEN-SIM-RECO/MC_3XY_V24-v1/0003/561FC9E7-9A2B-DF11-9CE2-001A92971B12.root'
+'/store/relval/CMSSW_7_0_0/RelValTTbar_13/GEN-SIM-RECO/PU50ns_POSTLS170_V4-v2/00000/265B9219-FF98-E311-BF4A-02163E00EA95.root'
     )
 )
 
@@ -32,7 +32,7 @@ process.FEVT = cms.OutputModule("PoolOutputModule",
 )
 
 # Other statements
-process.GlobalTag.globaltag = 'MC_3XY_V21::All'
+process.GlobalTag.globaltag = 'POSTLS162_V1::All'
 
 
 process.p = cms.Path(process.inclusiveVertexing)
