8dd56a2d1ce071935be7ea01276c33aba9bdb6b9
Florian Beaudette <Florian.Beaudette@cern.ch>
2014-03-17 11:38:12 +0100

    more updates for the status

--- RecoEgamma/EgammaElectronProducers/python/pfBasedElectronIso_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/pfBasedElectronIso_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from CommonTools.ParticleFlow.pfParticleSelection_cff import *
 from RecoEgamma.EgammaElectronProducers.electronPFIsolationDeposits_cff import *
 from RecoEgamma.EgammaElectronProducers.electronPFIsolationValues_cff import *
-from RecoEgamma.EgammaElectronProducers.pfIsolationFiller_cfi import *
+from RecoEgamma.EgammaElectronProducers.gedGsfElectronFinalizer_cfi import *
 
 # The following should be removed up to  <--- when moving to GED only
 pfBasedElectronIsoSequence = cms.Sequence(
