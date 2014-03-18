9186e946b8b6171a1a8b5ef4280e10ff9d0d19cc
Sven Dildick <sven.dildick@cern.ch>
2014-03-18 16:05:08 +0100

    update keep/drop collections

--- GEMValidation/python/sampleProductionCustoms.py
+++ GEMValidation/python/sampleProductionCustoms.py
@@ -1,30 +1,34 @@
 import FWCore.ParameterSet.Config as cms
 
 def dropNonMuonCollections(process):
-    process.FEVTDEBUGoutput.outputCommands = cms.untracked.vstring(
-        'keep  *_*_*_*',
-        # drop tracker simhits
-        'drop PSimHits_*_Tracker*_*',
-        # drop calorimetry stuff
-        'drop PCaloHits_*_*_*',
-        # clean up simhits from other detectors
-        'drop PSimHits_*_Totem*_*',
-        'drop PSimHits_*_FP420*_*',
-        'drop PSimHits_*_BSC*_*',
-        'drop *RandomEngineStates_*_*_*',
-        'drop *_randomEngineStateProducer_*_*'
-    )
+    alist=['AODSIM','RECOSIM','FEVTSIM','FEVTDEBUG','FEVTDEBUGHLT','RECODEBUG','RAWRECOSIMHLT','RAWRECODEBUGHLT']
+    for a in alist:
+        b=a+'output'
+        if hasattr(process,b):
+            getattr(process,b).outputCommands.append('keep  *_*_*_*')
+            # drop tracker simhits
+            getattr(process,b).outputCommands.append('drop PSimHits_*_Tracker*_*')
+            # drop calorimetry stuff
+            getattr(process,b).outputCommands.append('drop PCaloHits_*_*_*')
+            # clean up simhits from other detectors
+            getattr(process,b).outputCommands.append('drop PSimHits_*_Totem*_*')
+            getattr(process,b).outputCommands.append('drop PSimHits_*_FP420*_*')
+            getattr(process,b).outputCommands.append('drop PSimHits_*_BSC*_*')
+            getattr(process,b).outputCommands.append('drop *RandomEngineStates_*_*_*')
+            getattr(process,b).outputCommands.append('drop *_randomEngineStateProducer_*_*')
     return process
 
 def keepMuonTrackVertexCollections(process):
-    process.FEVTDEBUGoutput.outputCommands = cms.untracked.vstring(
-        'drop *_*_*_*',
-        'keep SimTracks_*_*_*',
-        'keep SimVertexs_*_*_*',
-        'keep *_*_MuonCSCHits_*',
-        'keep *_*_MuonRPCHits_*',
-        'keep *_*_MuonDTHits_*',
-        'keep *_*_MuonGEMHits_*',
-        'keep *_*_MuonME0Hits_*'
-    )
+    alist=['AODSIM','RECOSIM','FEVTSIM','FEVTDEBUG','FEVTDEBUGHLT','RECODEBUG','RAWRECOSIMHLT','RAWRECODEBUGHLT']
+    for a in alist:
+        b=a+'output'
+        if hasattr(process,b):
+            getattr(process,b).outputCommands.append('drop *_*_*_*')
+            getattr(process,b).outputCommands.append('keep SimTracks_*_*_*')
+            getattr(process,b).outputCommands.append('keep SimVertexs_*_*_*')
+            getattr(process,b).outputCommands.append('keep *_*_MuonCSCHits_*')
+            getattr(process,b).outputCommands.append('keep *_*_MuonRPCHits_*')
+            getattr(process,b).outputCommands.append('keep *_*_MuonDTHits_*')
+            getattr(process,b).outputCommands.append('keep *_*_MuonGEMHits_*')
+            getattr(process,b).outputCommands.append('keep *_*_MuonME0Hits_*')
     return process
