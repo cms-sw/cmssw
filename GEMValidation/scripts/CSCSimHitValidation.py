f3a9130d0bb2f796a2e26226320e7272f15126cd
Sven Dildick <sven.dildick@cern.ch>
2014-03-19 12:38:46 +0100

    CSC occupancy plots

--- GEMValidation/scripts/CSCSimHitValidation.py
+++ GEMValidation/scripts/CSCSimHitValidation.py
@@ -1,5 +1,3 @@
-import sys
-
 from ROOT import *
 
 from cuts import *
@@ -13,9 +11,156 @@ import ROOT
 ROOT.gROOT.SetBatch(1)
 
 #_______________________________________________________________________________
-def cscSimHitOccupancyXY():
-  pass
+def cscSimHitOccupancyXY(plotter,i):
   
+  ## per station
+  draw_occ(plotter.targetDir, "sh_csc_xy_st1" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station1;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st1,plotter.sel[i]), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st2" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station2;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st2,plotter.sel[i]), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st3" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station3;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st3,plotter.sel[i]), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st4" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station4;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st4,plotter.sel[i]), "COLZ")
+
+  ## per station even/odd
+  draw_occ(plotter.targetDir, "sh_csc_xy_st1_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station1;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st1,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st2_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station2;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st2,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st3_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station3;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st3,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st4_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station4;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st4,plotter.sel[i],even), "COLZ")
+
+  draw_occ(plotter.targetDir, "sh_csc_xy_st1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station1;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station2;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st3_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station3;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st3,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_st4_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: station4;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(st4,plotter.sel[i],odd), "COLZ")
+
+  ## per endcap, station, even/odd
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],odd), "COLZ")
+
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],odd), "COLZ")
+
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st3_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st3,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st3_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st3,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st3_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st3,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st3_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st3,plotter.sel[i],odd), "COLZ")
+
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st4_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st4,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st4_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st4,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st4_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st4,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st4_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st4,plotter.sel[i],odd), "COLZ")
+
+  ## per endcap, station, l1 even/odd
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_l1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer1, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_l2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_l1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2 ,layer1, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_l2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, layer2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],odd), "COLZ")
+  
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_l1_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer1, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st2_l2_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer2, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st2,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_l1_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2 ,layer1, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st2_l2_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, layer2, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st2,plotter.sel[i],even), "COLZ")
+
+
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_l1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer1, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_l2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_l1_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2 ,layer1, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],odd), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_l2_odd" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, layer2, Odd;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],odd), "COLZ")
+  
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_l1_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer1, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec2_st1_l2_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region-1, station2, layer2, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec2,st1,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_l1_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2 ,layer1, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],even), "COLZ")
+  draw_occ(plotter.targetDir, "sh_csc_xy_ec1_st1_l2_even" + plotter.suff[i], plotter.ext, plotter.treeCSCSimHits,
+           plotter.pre[i] + " SimHit occupancy: region1, station2, layer2, Even;globalX [cm];globalY [cm]",
+           "h_", "(100,-700,700,100,-700,700)", "globalX:globalY", AND(ec1,st1,plotter.sel[i],even), "COLZ")
+
 #_______________________________________________________________________________
 def cscSimHitOccupancyRZ():
   pass
