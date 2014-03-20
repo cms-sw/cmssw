e808d12f850efaf8e27a5add4f7d7c4a4fd874d9
Laura <Laura.Borrello@cern.ch>
2014-03-20 15:43:33 +0100

    Adding a new input source for online DQM

--- IOPool/Streamer/plugins/Module.cc
+++ IOPool/Streamer/plugins/Module.cc
@@ -8,18 +8,23 @@
 //new module to read events from Streamer files
 #include "IOPool/Streamer/src/StreamerFileReader.h"
 
+#include "IOPool/Streamer/src/DQMStreamerReader.h"
+
 #include "IOPool/Streamer/interface/FRDEventFileWriter.h"
 #include "IOPool/Streamer/interface/FRDEventOutputModule.h"
 
 typedef edm::StreamerOutputModule<edm::StreamerFileWriter> EventStreamFileWriter;
 typedef edm::StreamerFileReader NewEventStreamFileReader;
+typedef edm::DQMStreamerReader NewDQMStreamerFileReader;
 typedef FRDEventOutputModule<FRDEventFileWriter> FRDStreamFileWriter;
 typedef FRDEventOutputModule<FRDEventFileWriter> ErrorStreamFileWriter;
 
 using edm::StreamerFileReader;
 using edm::StreamerFileWriter;
+using edm::DQMStreamerReader;
 
 DEFINE_FWK_INPUT_SOURCE(NewEventStreamFileReader);
+DEFINE_FWK_INPUT_SOURCE(NewDQMStreamerFileReader);
 
 DEFINE_FWK_MODULE(EventStreamFileWriter);
 
