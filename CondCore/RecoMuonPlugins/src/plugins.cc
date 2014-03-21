8c04cea3ea67b99bc0a6868a1ca54d278b02ec3e
Davide <davide.pagano@cern.ch>
2014-03-21 15:06:23 +0100

    new dyt version

--- CondCore/RecoMuonPlugins/src/plugins.cc
+++ CondCore/RecoMuonPlugins/src/plugins.cc
@@ -1,7 +1,10 @@
 #include "CondCore/ESSources/interface/registration_macros.h"
 #include "CondFormats/RecoMuonObjects/interface/MuScleFitDBobject.h"
 #include "CondFormats/DataRecord/interface/MuScleFitDBobjectRcd.h"
-
-
 REGISTER_PLUGIN(MuScleFitDBobjectRcd,MuScleFitDBobject);
+
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondFormats/RecoMuonObjects/interface/DYTThrObject.h"
+#include "CondFormats/DataRecord/interface/DYTThrObjectRcd.h"
+REGISTER_PLUGIN(DYTThrObjectRcd,DYTThrObject);
 
