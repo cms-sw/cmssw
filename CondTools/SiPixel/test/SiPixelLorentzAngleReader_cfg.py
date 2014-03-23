5ec32288715095924c43fb5b269824500c118c64
Pal Hidas <hidas.pal@wigner.mta.hu>
2014-03-23 12:10:15 +0100

    moving SiPixelLorentzAngleReader

--- CondTools/SiPixel/test/SiPixelLorentzAngleReader_cfg.py
+++ CondTools/SiPixel/test/SiPixelLorentzAngleReader_cfg.py
@@ -5,10 +5,10 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 process.source = cms.Source("EmptySource",
-    lastRun = cms.untracked.uint32(1),
-    timetype = cms.string('runnumber'),
-    firstRun = cms.untracked.uint32(1),
-    interval = cms.uint32(1)
+#    lastRun = cms.untracked.uint32(1),
+#    timetype = cms.string('runnumber'),
+#    interval = cms.uint32(1),
+    firstRun = cms.untracked.uint32(1)
 )
 
 
@@ -27,6 +27,9 @@ process.MessageLogger = cms.Service("MessageLogger",
 process.Timing = cms.Service("Timing")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = "GR_R_70_V1::All"
+process.GlobalTag.globaltag = 'FT_R_53_V21::All'
+
 process.load("Configuration.StandardSequences.GeometryIdeal_cff")
 
 process.QualityReader = cms.ESSource("PoolDBESSource",
@@ -38,14 +41,17 @@ process.QualityReader = cms.ESSource("PoolDBESSource",
     toGet = cms.VPSet(
 		cms.PSet(
 			record = cms.string("SiPixelLorentzAngleRcd"),
-			tag = cms.string("trivial_LorentzAngle")
+			#tag = cms.string("trivial_LorentzAngle")
+			tag = cms.string("SiPixelLorentzAngle_v01")
 		),
-		cms.PSet(
-			record = cms.string("SiPixelLorentzAngleSimRcd"),
-			tag = cms.string("trivial_LorentzAngle_Sim")
-		)
+#		cms.PSet(
+#			record = cms.string("SiPixelLorentzAngleSimRcd"),
+#			tag = cms.string("trivial_LorentzAngle_Sim")
+#		)
 	),
-    connect = cms.string('sqlite_file:SiPixelLorentzAngle.db')
+    #connect = cms.string('sqlite_file:SiPixelLorentzAngle.db')
+    #connect = cms.string('sqlite_file:prova_LA_2012_IOV7.db')
+    connect = cms.string('sqlite_file:/afs/cern.ch/user/h/hidaspal/public/tracker/SiPixelLorentzAngle/pixelLorentzAngle_original_private_production_stuff/mytest/prova_LA_2012_IOV7.db')
 )
 
 process.es_prefer_QualityReader = cms.ESPrefer("PoolDBESSource","QualityReader")
@@ -62,5 +68,7 @@ process.LorentzAngleSimReader = cms.EDAnalyzer("SiPixelLorentzAngleReader",
 )
 
 
-process.p = cms.Path(process.LorentzAngleReader*process.LorentzAngleSimReader)
+#process.p = cms.Path(process.LorentzAngleReader*process.LorentzAngleSimReader)
+process.p = cms.Path(process.LorentzAngleReader)
+
 
