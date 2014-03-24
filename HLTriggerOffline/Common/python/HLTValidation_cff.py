ee766d15e3d21a92d578f9fd2aec5a88225a3988
Thiago <Thiago.Tomei@cern.ch>
2014-03-24 14:52:56 +0100

    Making our sequences known to the main validation sequences

--- HLTriggerOffline/Common/python/HLTValidation_cff.py
+++ HLTriggerOffline/Common/python/HLTValidation_cff.py
@@ -1,38 +1,42 @@
 from HLTriggerOffline.Muon.HLTMuonVal_cff import *
 from HLTriggerOffline.Tau.Validation.HLTTauValidation_cff import *
 from HLTriggerOffline.Egamma.EgammaValidationAutoConf_cff import *
-from HLTriggerOffline.Top.topvalidation_cfi import *
 from HLTriggerOffline.Common.FourVectorHLTriggerOffline_cff import *
 from HLTriggerOffline.HeavyFlavor.heavyFlavorValidationSequence_cff import *
 from HLTriggerOffline.JetMET.Validation.HLTJetMETValidation_cff import *
 #from HLTriggerOffline.special.hltAlCaVal_cff import *
 from HLTriggerOffline.SUSYBSM.SusyExoValidation_cff import *
 from HLTriggerOffline.Higgs.HiggsValidation_cff import *
-
+from HLTriggerOffline.Top.topHLTValidation_cff import *
+from HLTriggerOffline.Exotica.ExoticaValidation_cff import *
 
 # offline dqm:
 # from DQMOffline.Trigger.DQMOffline_Trigger_cff.py import *
 from DQMOffline.Trigger.HLTTauDQMOffline_cff import *
 from DQMOffline.Trigger.EgHLTOfflineSource_cfi import *
+from DQMOffline.Trigger.topHLTOfflineDQM_cff import *
 #from DQMOffline.Trigger.MuonTrigRateAnalyzer_cfi import *
 # online dqm:
 from DQMOffline.Trigger.HLTMonTau_cfi import *
-
+ 
 # additional producer sequence prior to hltvalidation
 # to evacuate producers/filters from the EndPath
-hltassociation = cms.Sequence( egammaSelectors )
+hltassociation = cms.Sequence( egammaSelectors
+                               +ExoticaValidationProdSeq )
 
 
 hltvalidation = cms.Sequence(
     HLTMuonVal
     +HLTTauVal
     +egammaValidationSequence
-    +HLTTopVal
+    +topHLTriggerOfflineDQM
+    +topHLTriggerValidation
     +HLTFourVector
     +heavyFlavorValidationSequence
     +HLTJetMETValSeq
     +HLTSusyExoValSeq
     +HiggsValidationSequence
+    +ExoticaValidationSequence
     )
 
 
@@ -48,7 +52,8 @@ hltvalidation_fastsim = cms.Sequence(
      HLTMuonVal_FastSim
     +HLTTauValFS
     +egammaValidationSequenceFS
-    +HLTTopVal
+    +topHLTriggerOfflineDQM
+    +topHLTriggerValidation
     +HLTFourVector
     +heavyFlavorValidationSequence
     +HLTJetMETValSeq
@@ -59,7 +64,6 @@ hltvalidation_fastsim = cms.Sequence(
 
 hltvalidation_preprod = cms.Sequence(
   HLTTauVal
-  +HLTTopVal
   +HLTFourVector
   +heavyFlavorValidationSequence
   +HLTSusyExoValSeq
@@ -68,7 +72,6 @@ hltvalidation_preprod = cms.Sequence(
 
 hltvalidation_preprod_fastsim = cms.Sequence(
  HLTTauVal
- +HLTTopVal
  +HLTFourVector
  +heavyFlavorValidationSequence
  +HLTSusyExoValSeq_FastSim
