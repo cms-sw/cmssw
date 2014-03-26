6b8acc5d0f1efacd09014de8cfeac971d6726bd1
Sunanda <sunanda.banerjee@cern.ch>
2014-03-26 20:13:13 +0100

    Add topology class for Shashlik

---
+++ Geometry/CaloEventSetup/python/ShashlikTopology_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# This cfi should be included to build the Shashlik Topology
+#
+ShashlikTopologyBuilder = cms.ESProducer("ShashlikTopologyBuilder")
+
+
