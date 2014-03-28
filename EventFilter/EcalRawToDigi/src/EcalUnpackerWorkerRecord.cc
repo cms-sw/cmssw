6472ca63a6b5338d7b1941b2e5308d7292914a9a
Dmitrijus Bugelskis <dmitrijus.bugelskis@cern.ch>
2014-03-28 18:29:11 +0100

    Preparation for EcalUnpackerWorker removal.

--- EventFilter/EcalRawToDigi/src/EcalUnpackerWorkerRecord.cc
+++
@@ -1,4 +0,0 @@
-#include "EventFilter/EcalRawToDigi/interface/EcalUnpackerWorkerRecord.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
- 
-EVENTSETUP_RECORD_REG(EcalUnpackerWorkerRecord);
