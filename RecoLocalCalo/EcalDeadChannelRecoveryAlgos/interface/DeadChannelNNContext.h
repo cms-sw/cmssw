c55695bbfa76110f0b4af54d44d82783218851c0
Dmitrijus Bugelskis <dmitrijus.bugelskis@cern.ch>
2014-03-28 03:28:47 +0100

    Neural network is now loaded from a file.

--- RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/DeadChannelNNContext.h
+++ RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/DeadChannelNNContext.h
@@ -1,36 +1,55 @@
 #ifndef RecoLocalCalo_EcalDeadChannelRecoveryAlgos_DeadChannelNNContext_H
 #define RecoLocalCalo_EcalDeadChannelRecoveryAlgos_DeadChannelNNContext_H
 
-#include "RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/xyNNEB.h"
-#include "RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/xyNNEE.h"
+#include <TTree.h>
+#include <TMultiLayerPerceptron.h>
 
 #include <functional>
 
 class DeadChannelNNContext {
-    public:
-		DeadChannelNNContext();
-		~DeadChannelNNContext();
+ public:
+  DeadChannelNNContext();
+  ~DeadChannelNNContext();
 
-        enum NetworkID { 
-          ccEB = 0, ccEE,
-          rrEB, rrEE,
-          llEB, llEE,
-                     
-          uuEB, uuEE,
-          ddEB, ddEE,
-          ruEB, ruEE,
-                     
-          rdEB, rdEE,
-          luEB, luEE,
-          ldEB, ldEE,
-        };
+  enum NetworkID {
+    ccEB = 0,
+    ccEE,
+    rrEB,
+    rrEE,
+    llEB,
+    llEE,
 
-        double value(NetworkID method, int index, double in0, double in1, double in2, double in3, double in4, double in5, double in6, double in7);
+    uuEB,
+    uuEE,
+    ddEB,
+    ddEE,
+    ruEB,
+    ruEE,
 
-    private:
-		void load();
-        typedef std::function<double(double[8])> NNFunc3x3;
-        NNFunc3x3 implementation[18];
+    rdEB,
+    rdEE,
+    luEB,
+    luEE,
+    ldEB,
+    ldEE,
+
+    lastID
+  };
+
+  double value(NetworkID method, int index, double in0, double in1, double in2,
+               double in3, double in4, double in5, double in6, double in7);
+
+ private:
+  void load();
+  void load_file(NetworkID id, std::string fn);
+
+  struct MultiLayerPerceptronContext {
+    Double_t tmp[9];
+    TTree *tree;
+    TMultiLayerPerceptron *mlp;
+  };
+
+  MultiLayerPerceptronContext ctx[lastID];
 };
 
 #endif
--- RecoLocalCalo/EcalDeadChannelRecoveryAlgos/src/DeadChannelNNContext.cc
+++ RecoLocalCalo/EcalDeadChannelRecoveryAlgos/src/DeadChannelNNContext.cc
@@ -1,61 +1,77 @@
 #include "RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/DeadChannelNNContext.h"
+#include "FWCore/ParameterSet/interface/FileInPath.h"
 
-#include "RecoLocalCalo/EcalDeadChannelRecoveryAlgos/interface/xyNNEB.h"
+#include <iostream>
 
 DeadChannelNNContext::DeadChannelNNContext() {
-	this->load();
+  for (int id = 0; id < NetworkID::lastID; ++id) {
+    ctx[id].mlp = NULL;
+  }
 }
 
-DeadChannelNNContext::~DeadChannelNNContext() {}
+DeadChannelNNContext::~DeadChannelNNContext() {
+  for (int id = 0; id < NetworkID::lastID; ++id) {
+    if (ctx[id].mlp) {
+      // @TODO segfaults for an uknown reason
+      // delete ctx[id].mlp;
+      // delete ctx[id].tree;
+    }
+  }
+}
+
+void DeadChannelNNContext::load_file(NetworkID id, std::string fn) {
+  std::string path = edm::FileInPath(fn).fullPath();
+
+  TTree *t = new TTree("t", "dummy MLP tree");
+  t->SetDirectory(0);
+
+  t->Branch("z1", &(ctx[id].tmp[0]), "z1/D");
+  t->Branch("z2", &(ctx[id].tmp[1]), "z2/D");
+  t->Branch("z3", &(ctx[id].tmp[2]), "z3/D");
+  t->Branch("z4", &(ctx[id].tmp[3]), "z4/D");
+  t->Branch("z5", &(ctx[id].tmp[4]), "z5/D");
+  t->Branch("z6", &(ctx[id].tmp[5]), "z6/D");
+  t->Branch("z7", &(ctx[id].tmp[6]), "z7/D");
+  t->Branch("z8", &(ctx[id].tmp[7]), "z8/D");
+  t->Branch("zf", &(ctx[id].tmp[8]), "zf/D");
+
+  ctx[id].tree = t;
+  ctx[id].mlp =
+      new TMultiLayerPerceptron("@z1,@z2,@z3,@z4,@z5,@z6,@z7,@z8:10:5:zf", t);
+  ctx[id].mlp->LoadWeights(path.c_str());
+}
 
 void DeadChannelNNContext::load() {
-	ccNNEB* ccNNObjEB = new ccNNEB();
-	rrNNEB* rrNNObjEB = new rrNNEB();
-	llNNEB* llNNObjEB = new llNNEB();
-	uuNNEB* uuNNObjEB = new uuNNEB();
-	ddNNEB* ddNNObjEB = new ddNNEB();
-	ruNNEB* ruNNObjEB = new ruNNEB();
-	rdNNEB* rdNNObjEB = new rdNNEB();
-	luNNEB* luNNObjEB = new luNNEB();
-	ldNNEB* ldNNObjEB = new ldNNEB();
+  std::string p = "RecoLocalCalo/EcalDeadChannelRecoveryAlgos/data/NNWeights/";
 
-	ccNNEE* ccNNObjEE = new ccNNEE();
-	rrNNEE* rrNNObjEE = new rrNNEE();
-	llNNEE* llNNObjEE = new llNNEE();
-	uuNNEE* uuNNObjEE = new uuNNEE();
-	ddNNEE* ddNNObjEE = new ddNNEE();
-	ruNNEE* ruNNObjEE = new ruNNEE();
-	rdNNEE* rdNNObjEE = new rdNNEE();
-	luNNEE* luNNObjEE = new luNNEE();
-	ldNNEE* ldNNObjEE = new ldNNEE();
+  this->load_file(NetworkID::ccEB, p + "EB_ccNNWeights.txt");
+  this->load_file(NetworkID::rrEB, p + "EB_rrNNWeights.txt");
+  this->load_file(NetworkID::llEB, p + "EB_llNNWeights.txt");
+  this->load_file(NetworkID::uuEB, p + "EB_uuNNWeights.txt");
+  this->load_file(NetworkID::ddEB, p + "EB_ddNNWeights.txt");
+  this->load_file(NetworkID::ruEB, p + "EB_ruNNWeights.txt");
+  this->load_file(NetworkID::rdEB, p + "EB_rdNNWeights.txt");
+  this->load_file(NetworkID::luEB, p + "EB_luNNWeights.txt");
+  this->load_file(NetworkID::ldEB, p + "EB_ldNNWeights.txt");
 
-	implementation[NetworkID::ccEB] = [=](double x[8]) -> double { return ccNNObjEB->Value(0, x); };
-	implementation[NetworkID::rrEB] = [=](double x[8]) -> double { return rrNNObjEB->Value(0, x); };
-	implementation[NetworkID::llEB] = [=](double x[8]) -> double { return llNNObjEB->Value(0, x); };
-	implementation[NetworkID::uuEB] = [=](double x[8]) -> double { return uuNNObjEB->Value(0, x); };
-	implementation[NetworkID::ddEB] = [=](double x[8]) -> double { return ddNNObjEB->Value(0, x); };
-	implementation[NetworkID::ruEB] = [=](double x[8]) -> double { return ruNNObjEB->Value(0, x); };
-	implementation[NetworkID::rdEB] = [=](double x[8]) -> double { return rdNNObjEB->Value(0, x); };
-	implementation[NetworkID::luEB] = [=](double x[8]) -> double { return luNNObjEB->Value(0, x); };
-	implementation[NetworkID::ldEB] = [=](double x[8]) -> double { return ldNNObjEB->Value(0, x); };
-
-	implementation[NetworkID::ccEE] = [=](double x[8]) -> double { return ccNNObjEE->Value(0, x); };
-	implementation[NetworkID::rrEE] = [=](double x[8]) -> double { return rrNNObjEE->Value(0, x); };
-	implementation[NetworkID::llEE] = [=](double x[8]) -> double { return llNNObjEE->Value(0, x); };
-	implementation[NetworkID::uuEE] = [=](double x[8]) -> double { return uuNNObjEE->Value(0, x); };
-	implementation[NetworkID::ddEE] = [=](double x[8]) -> double { return ddNNObjEE->Value(0, x); };
-	implementation[NetworkID::ruEE] = [=](double x[8]) -> double { return ruNNObjEE->Value(0, x); };
-	implementation[NetworkID::rdEE] = [=](double x[8]) -> double { return rdNNObjEE->Value(0, x); };
-	implementation[NetworkID::luEE] = [=](double x[8]) -> double { return luNNObjEE->Value(0, x); };
-	implementation[NetworkID::ldEE] = [=](double x[8]) -> double { return ldNNObjEE->Value(0, x); };
-
+  this->load_file(NetworkID::ccEE, p + "EE_ccNNWeights.txt");
+  this->load_file(NetworkID::rrEE, p + "EE_rrNNWeights.txt");
+  this->load_file(NetworkID::llEE, p + "EE_llNNWeights.txt");
+  this->load_file(NetworkID::uuEE, p + "EE_uuNNWeights.txt");
+  this->load_file(NetworkID::ddEE, p + "EE_ddNNWeights.txt");
+  this->load_file(NetworkID::ruEE, p + "EE_ruNNWeights.txt");
+  this->load_file(NetworkID::rdEE, p + "EE_rdNNWeights.txt");
+  this->load_file(NetworkID::luEE, p + "EE_luNNWeights.txt");
+  this->load_file(NetworkID::ldEE, p + "EE_ldNNWeights.txt");
 }
 
-double DeadChannelNNContext::value(NetworkID method, int index, double in0, double in1, double in2, 
-  double in3, double in4, double in5, double in6, double in7) {
+double DeadChannelNNContext::value(NetworkID method, int index, double in0,
+                                   double in1, double in2, double in3,
+                                   double in4, double in5, double in6,
+                                   double in7) {
 
-  double vCC[8] = {in0, in1, in2, in3, in4, in5, in6, in7};
-  return implementation[method](vCC);
+  if (!ctx[method].mlp) this->load();
+
+  double vCC[8] = { in0, in1, in2, in3, in4, in5, in6, in7 };
+  return ctx[method].mlp->Evaluate(0, vCC);
 }
-
-
