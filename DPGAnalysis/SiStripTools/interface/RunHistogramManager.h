00ecbceb8c63853ea8b7bf56f326564ec32fd5d2
Andrea Venturi <andrea.venturi@cern.ch>
2014-03-28 18:06:17 +0100

    Two bug fixes: consumes for InRun object and class with a constructor which builds another object of the same class

--- DPGAnalysis/SiStripTools/interface/RunHistogramManager.h
+++ DPGAnalysis/SiStripTools/interface/RunHistogramManager.h
@@ -218,7 +218,7 @@ class RunHistogramManager {
 
  public:
 
-  RunHistogramManager(edm::ConsumesCollector&& iC, const bool fillHistograms=false) {RunHistogramManager(iC, fillHistograms);}
+  RunHistogramManager(edm::ConsumesCollector&& iC, const bool fillHistograms=false);
   RunHistogramManager(edm::ConsumesCollector& iC, const bool fillHistograms=false);
   ~RunHistogramManager();
 
