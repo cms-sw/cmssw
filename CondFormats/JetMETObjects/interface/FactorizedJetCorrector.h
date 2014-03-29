637b68eba8689c3546d736e2f720484136216661
Christopher Jones <cdj@fnal.gov>
2014-03-29 15:39:04 +0100

    Refactored FactorizedJetCorrector
    Refactored the internals of FactorizedJetCorrector into two seperate
    classes, one which does the calculation and another which olds the
    list of variable values. This was needed since FactorizedJetCorrector
    was being used by JetCorrectors in the EventSetup and the changing of member
    data is a threading problem. Now the JetCorrectors only hold onto the
    FactorizedJetCorrectorCalculator and create the temporary variable value
    holders as needed.

--- CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h
+++ CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <string>
 #include "TLorentzVector.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrectorCalculator.h"
 
 class SimpleJetCorrector;
 class JetCorrectorParameters;
@@ -21,7 +22,7 @@ class FactorizedJetCorrector
     FactorizedJetCorrector();
     FactorizedJetCorrector(const std::string& fLevels, const std::string& fTags, const std::string& fOptions="");
     FactorizedJetCorrector(const std::vector<JetCorrectorParameters>& fParameters);
-    ~FactorizedJetCorrector();
+
     void setNPV		(int   fNPV);
     void setJetEta      (float fEta);
     void setJetPt       (float fPt); 
@@ -44,50 +45,8 @@ class FactorizedJetCorrector
   //---- Member Functions ----  
     FactorizedJetCorrector(const FactorizedJetCorrector&);
     FactorizedJetCorrector& operator= (const FactorizedJetCorrector&);
-    float getLepPt()    const;
-    float getRelLepPt() const;
-    float getPtRel()    const;
-    std::string parseOption(const std::string& ss, const std::string& type);
-    std::string removeSpaces(const std::string& ss);
-    std::vector<std::string> parseLevels(const std::string& ss);
-    void initCorrectors(const std::string& fLevels, const std::string& fFiles, const std::string& fOptions);
-    void checkConsistency(const std::vector<std::string>& fLevels, const std::vector<std::string>& fTags);
-    std::vector<float> fillVector(const std::vector<VarTypes>& fVarTypes);
-    std::vector<VarTypes> mapping(const std::vector<std::string>& fNames);
     //---- Member Data ---------
-    int   mNPV;
-    float mJetE;
-    float mJetEta;
-    float mJetPt;
-    float mJetPhi;
-    float mJetEMF; 
-    float mJetA;
-    float mRho;
-    float mJPTrawE;
-    float mJPTrawEt;
-    float mJPTrawPt;
-    float mJPTrawEta; 
-    float mJPTrawOff;
-    float mLepPx;
-    float mLepPy;
-    float mLepPz;
-    bool  mAddLepToJet;
-    bool  mIsNPVset;
-    bool  mIsJetEset;
-    bool  mIsJetPtset;
-    bool  mIsJetPhiset;
-    bool  mIsJetEtaset;
-    bool  mIsJetEMFset; 
-    bool  mIsJetAset;
-    bool  mIsRhoset;
-    bool  mIsJPTrawP4set;
-    bool  mIsJPTrawOFFset;
-    bool  mIsLepPxset;
-    bool  mIsLepPyset;
-    bool  mIsLepPzset;
-    bool  mIsAddLepToJetset;
-    std::vector<LevelTypes> mLevels;
-    std::vector<std::vector<VarTypes> > mParTypes,mBinTypes; 
-    std::vector<SimpleJetCorrector*> mCorrectors;
+    FactorizedJetCorrectorCalculator::VariableValues mValues;
+    FactorizedJetCorrectorCalculator mCalc;
 };
 #endif
