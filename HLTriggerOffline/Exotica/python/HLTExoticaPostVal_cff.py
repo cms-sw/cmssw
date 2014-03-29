2efb86e41a13936abaa8ee8dc9874eb6312656e4
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-03-29 17:29:19 +0100

    Revert "Take 2 of Exotica Trigger Validation"
    This reverts commit 6bb62fe869986e669c2c9abb4d2f7adee751df7d.

--- HLTriggerOffline/Exotica/python/HLTExoticaPostVal_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HLTriggerOffline.Exotica.hltExoticaPostProcessors_cff import *
-
-HLTExoticaPostVal = cms.Sequence( 
-		hltExoticaPostProcessors
-		)
-
