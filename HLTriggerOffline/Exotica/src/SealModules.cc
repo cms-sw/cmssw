be8b496f25d0618ca8e7e0fca84d17e93b24d4bc
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-03-29 17:29:19 +0100

    Revert "Take 2 of Exotica Trigger Validation"
    This reverts commit 6bb62fe869986e669c2c9abb4d2f7adee751df7d.

--- HLTriggerOffline/Exotica/src/SealModules.cc
+++
@@ -1,7 +0,0 @@
-
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "HLTriggerOffline/Exotica/interface/HLTExoticaValidator.h"
-
-DEFINE_FWK_MODULE(HLTExoticaValidator);
