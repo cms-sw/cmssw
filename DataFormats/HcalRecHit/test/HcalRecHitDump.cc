4a7ade65fa8fd522ee24caec665e1a98a17f2bc0
Igor <i.volobouev@ttu.eu>
2014-03-30 21:28:24 +0200

    preparing HBHERecHit class for introducing out-of-time pileup corrections

--- DataFormats/HcalRecHit/test/HcalRecHitDump.cc
+++ DataFormats/HcalRecHit/test/HcalRecHitDump.cc
@@ -2,9 +2,14 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/GetterOfProducts.h"
 #include "FWCore/Framework/interface/ProcessMatch.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/HcalRecHit/interface/HcalSourcePositionData.h"
+
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -15,27 +20,40 @@ namespace cms {
       
   \author J. Mans - Minnesota
   */
-  class HcalRecHitDump : public edm::EDAnalyzer {
+  class HcalRecHitDump : public edm::EDAnalyzer
+  {
   public:
     explicit HcalRecHitDump(edm::ParameterSet const& conf);
     virtual void analyze(edm::Event const& e, edm::EventSetup const& c);
+
   private:
     edm::GetterOfProducts<HcalSourcePositionData> getHcalSourcePositionData_;
+
+    string hbhePrefix_;
+    string hoPrefix_;
+    string hfPrefix_;
   };
 
   HcalRecHitDump::HcalRecHitDump(edm::ParameterSet const& conf) :
-    getHcalSourcePositionData_(edm::ProcessMatch("*"), this) {
+    getHcalSourcePositionData_(edm::ProcessMatch("*"), this),
+    hbhePrefix_(conf.getUntrackedParameter<string>("hbhePrefix", "")),
+    hoPrefix_(conf.getUntrackedParameter<string>("hoPrefix", "")),
+    hfPrefix_(conf.getUntrackedParameter<string>("hfPrefix", ""))
+  {
     callWhenNewProductsRegistered(getHcalSourcePositionData_);
   }
-  
-  template<typename COLL> void analyzeT(edm::Event const& e, const char * name=0) {
+
+  template<typename COLL>
+  static void analyzeT(edm::Event const& e, const char* name=0, const char* prefix=0)
+  {
+    const string marker(prefix ? prefix : "");
     try {
-      std::vector<edm::Handle<COLL> > colls;
+      vector<edm::Handle<COLL> > colls;
       e.getManyByType(colls);
       typename std::vector<edm::Handle<COLL> >::iterator i;
       for (i=colls.begin(); i!=colls.end(); i++) {
         for (typename COLL::const_iterator j=(*i)->begin(); j!=(*i)->end(); j++)
-          cout << *j << std::endl;
+          cout << marker << *j << endl;
       }
     } catch (...) {
       if(name) cout << "No " << name << " RecHits." << endl;
@@ -43,9 +61,9 @@ namespace cms {
   }
 
   void HcalRecHitDump::analyze(edm::Event const& e, edm::EventSetup const& c) {
-    analyzeT<HBHERecHitCollection>(e, "HB/HE"); 
-    analyzeT<HFRecHitCollection>(e, "HF");
-    analyzeT<HORecHitCollection>(e, "HO");
+    analyzeT<HBHERecHitCollection>(e, "HB/HE", hbhePrefix_.c_str()); 
+    analyzeT<HFRecHitCollection>(e, "HF", hfPrefix_.c_str());
+    analyzeT<HORecHitCollection>(e, "HO", hoPrefix_.c_str());
     analyzeT<HcalCalibRecHitCollection>(e);
     analyzeT<ZDCRecHitCollection>(e);
     analyzeT<CastorRecHitCollection>(e);
@@ -53,7 +71,7 @@ namespace cms {
     std::vector<edm::Handle<HcalSourcePositionData> > handles;
     getHcalSourcePositionData_.fillHandles(e, handles);
     for (auto const& spd : handles){
-      cout << *spd << std::endl;
+      cout << *spd << endl;
     }
     cout << endl;    
   }
