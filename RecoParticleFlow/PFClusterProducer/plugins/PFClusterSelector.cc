35014ea7f854e11b6d4e85b6e0abcc24fe8b4288
Jan Steggemann <jan.steggemann@cern.ch>
2014-03-31 14:38:25 +0200

    Fix issue that prevents endcap clusters to pass the cluster selection

--- RecoParticleFlow/PFClusterProducer/plugins/PFClusterSelector.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/PFClusterSelector.cc
@@ -29,29 +29,36 @@ void PFClusterSelector::produce(edm::Event& iEvent,
   iEvent.getByToken(clusters_,clusters);
   std::auto_ptr<reco::PFClusterCollection> out(new reco::PFClusterCollection);
 
-  for(unsigned int i=0;i<clusters->size();++i) {
-    double energy = clusters->at(i).energy();
-    double time = clusters->at(i).time();
+  const std::vector<double>* timingCutsLow = NULL;
+  const std::vector<double>* timingCutsHigh = NULL;
+  for(const auto& cluster : *clusters ) {    
+    const double energy = cluster.energy();
+    const double time = cluster.time();    
 
-    if (clusters->at(i).layer() == PFLayer::ECAL_BARREL ||
-	clusters->at(i).layer() == PFLayer::HCAL_BARREL1 ||
-	clusters->at(i).layer() == PFLayer::HCAL_BARREL2) {
-
-      for(unsigned int j=0;j<energyRanges_.size();++j) {
-	if (j==0 ) {
-	  if ( time >timingCutsLow_[j] &&  time <timingCutsHigh_[j] && energy<energyRanges_.at(0))
-	    out->push_back(clusters->at(i));
-	}
-	else if (j==(energyRanges_.size()-1) ) {
-	  if ( time >timingCutsLow_[j] &&  time <timingCutsHigh_[j] && energy>=energyRanges_.at(j))
-	    out->push_back(clusters->at(i));
-	}
-	else {
-	  if ( energy>energyRanges_[j-1] && energy < energyRanges_[j]&& time >timingCutsLow_[j] &&  time <timingCutsHigh_[j] )
-	    out->push_back(clusters->at(i));
-	}
-      }
-	
+    switch(cluster.layer()) {
+    case PFLayer::ECAL_BARREL:
+    case PFLayer::HCAL_BARREL1:
+    case PFLayer::HCAL_BARREL2:
+      timingCutsLow = &timingCutsLowBarrel_;
+      timingCutsHigh = &timingCutsHighBarrel_;
+      break;
+    case PFLayer::ECAL_ENDCAP:
+    case PFLayer::HCAL_ENDCAP:
+      timingCutsLow = &timingCutsLowEndcap_;
+      timingCutsHigh = &timingCutsHighEndcap_;
+      break;
+    default:      
+      continue;
+    }    
+    auto e_bin = std::lower_bound(energyRanges_.begin(),energyRanges_.end(),energy);
+    // Returns iterator to the first value that is *greater* than the value
+    // we are comparing to. 
+    // The timing cuts are indexed by the bin high edge so we just need the 
+    // distance between this bin timing cut vectors are padded to avoid 
+    // overflows.
+    const unsigned idx = std::distance(energyRanges_.begin(),e_bin);
+    if( time > (*timingCutsLow)[idx] && time < (*timingCutsHigh)[idx] ) {
+      out->push_back(cluster);
     }
     else 
       {
