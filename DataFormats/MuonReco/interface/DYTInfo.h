2830e8b30e4dadf4c95466d0532c9fcf7fef3f20
Davide <davide.pagano@cern.ch>
2014-04-01 12:08:26 +0200

    cleaning

--- DataFormats/MuonReco/interface/DYTInfo.h
+++ DataFormats/MuonReco/interface/DYTInfo.h
@@ -13,28 +13,28 @@ namespace reco {
     /// Constructor - Destructor
     DYTInfo();
     ~DYTInfo();
-    
+
     /// copy from another DYTInfo
-    void CopyFrom(DYTInfo);
+    void CopyFrom(const DYTInfo&);
     
     /// number of stations used by DYT
-    int  NStUsed()  { return NStUsed_; };
-    void setNStUsed(  int NStUsed ) { NStUsed_=NStUsed; };
+    const int  NStUsed() const { return NStUsed_; };
+    void setNStUsed( int NStUsed ) { NStUsed_=NStUsed; };
        
     /// estimator values for all station
-    std::vector<double> DYTEstimators()  { return DYTEstimators_; };
-    void setDYTEstimators( std::map<int, double> dytEstMap ) {
+    const std::vector<double> DYTEstimators() const { return DYTEstimators_; };
+    void setDYTEstimators( std::map<int, double> &dytEstMap ) {
       DYTEstimators_.clear();
       for (int st = 1; st <= 4; st++) {
 	if (dytEstMap.count(st) > 0) DYTEstimators_.push_back(dytEstMap[st]);
         else DYTEstimators_.push_back(-1);
       }
     };
-    void setDYTEstimators( std::vector<double> EstValues ) { DYTEstimators_ = EstValues; }
+    void setDYTEstimators( const std::vector<double> &EstValues ) { DYTEstimators_ = EstValues; }
     
     /// number of segments tested per muon station
-    std::vector<bool> UsedStations()  { return UsedStations_; };
-    void setUsedStations( std::map<int, bool> ustMap ) {
+    const std::vector<bool> UsedStations() const { return UsedStations_; };
+    void setUsedStations( std::map<int, bool> &ustMap ) {
       UsedStations_.clear();
       for (int st = 1; st <= 4; st++) 
         UsedStations_.push_back(ustMap[st]);
@@ -42,22 +42,22 @@ namespace reco {
     void setUsedStations( std::vector<bool> ustVal ) { UsedStations_ = ustVal; };
 
     /// DetId vector of chamber with valid estimator
-    std::vector<DetId> IdChambers()  { return IdChambers_; };
-    void setIdChambers( std::map<int, DetId> IdChambersMap ) {
+    const std::vector<DetId> IdChambers() const { return IdChambers_; };
+    void setIdChambers( std::map<int, DetId> &IdChambersMap ) {
       IdChambers_.clear();
       for (int st = 1; st <= 4; st++)
         IdChambers_.push_back(IdChambersMap[st]);
     };
-    void setIdChambers( std::vector<DetId> IdChambersVal ) { IdChambers_ = IdChambersVal; };
+    void setIdChambers( const std::vector<DetId> &IdChambersVal ) { IdChambers_ = IdChambersVal; };
     
     /// vector of thresholds                                                                                                                                                        
-    std::vector<double> Thresholds()  { return Thresholds_; };
-    void setThresholds( std::map<int, double> ThresholdsMap ) {
+    const std::vector<double> Thresholds() const { return Thresholds_; };
+    void setThresholds( std::map<int, double> &ThresholdsMap ) {
       Thresholds_.clear();
       for (int st = 1; st <= 4; st++)
 	Thresholds_.push_back(ThresholdsMap[st]);
     };
-    void setThresholds( std::vector<double> ThresholdsVal ) { Thresholds_ = ThresholdsVal; };
+    void setThresholds( const std::vector<double> &ThresholdsVal ) { Thresholds_ = ThresholdsVal; };
     
   private:
     
