098761b16748c3a0f166ea0088b92feff25e6fa6
Carlo Battilana <carlo.battilana@gmail.com>
2014-04-02 18:26:25 +0200

    Migrated the L2MuonIsolatorProducer to consumes. In order to do so the MuonIsolatorFactory (from RecoMuon/MuonIsolation) as well as the MuIsoBaseIsolator base class were updated to deal with ConsumesCollector.

--- RecoMuon/MuonIsolation/interface/MuonIsolatorFactory.h
+++ RecoMuon/MuonIsolation/interface/MuonIsolatorFactory.h
@@ -4,7 +4,8 @@
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 #include "RecoMuon/MuonIsolation/interface/MuIsoBaseIsolator.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 
-typedef edmplugin::PluginFactory<muonisolation::MuIsoBaseIsolator* (const edm::ParameterSet&) >  MuonIsolatorFactory;
+typedef edmplugin::PluginFactory<muonisolation::MuIsoBaseIsolator* (const edm::ParameterSet&, edm::ConsumesCollector && iC) >  MuonIsolatorFactory;
 
 #endif
