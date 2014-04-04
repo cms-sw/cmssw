e2b752981cf0517c45481f02ab130d2d64a4e348
Andrea <perrotta@cern.ch>
2014-04-04 17:37:43 +0200

    Again...

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_7_1_0/PIon/V22 (CMSSW_7_1_0_pre5_HLT1)
+# /dev/CMSSW_7_1_0/PIon/V23 (CMSSW_7_1_0_pre5_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_1_0/PIon/V22')
+  tableName = cms.string('/dev/CMSSW_7_1_0/PIon/V23')
 )
 
 streams = cms.PSet( 
