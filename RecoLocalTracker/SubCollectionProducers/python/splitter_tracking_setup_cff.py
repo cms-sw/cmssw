3f4c91aff86635ff54b106b0804f74540a275774
Silvia <Silvia.Taroni@cern.ch>
2014-04-04 16:32:42 +0200

    splitter setup as function

--- RecoLocalTracker/SubCollectionProducers/python/splitter_tracking_setup_cff.py
+++ RecoLocalTracker/SubCollectionProducers/python/splitter_tracking_setup_cff.py
@@ -160,6 +160,3 @@ def customizeTracking(newpixclusters, newstripclusters, newpixrechits, newstripr
     convLayerPairs.TID2.stereoRecHits = cms.InputTag(newstriprechits,"stereoRecHit")
     convLayerPairs.TID3.stereoRecHits = cms.InputTag(newstriprechits,"stereoRecHit")
     convLayerPairs.TEC.stereoRecHits  = cms.InputTag(newstriprechits,"stereoRecHit")
-
-
-
