56cf1bfab0697d169b880e59eee5062a0f0e4880
Andrea <perrotta@cern.ch>
2014-04-04 14:00:23 +0200

    Bug fix for (a few) ECal sequences in the HLT menus

--- HLTrigger/Configuration/test/OnData_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnData_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_1_0/HIon/V20 (CMSSW_7_1_0_pre5_HLT1)
+# /dev/CMSSW_7_1_0/HIon/V21 (CMSSW_7_1_0_pre5_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_1_0/HIon/V20')
+  tableName = cms.string('/dev/CMSSW_7_1_0/HIon/V21')
 )
 
 process.streams = cms.PSet( 
