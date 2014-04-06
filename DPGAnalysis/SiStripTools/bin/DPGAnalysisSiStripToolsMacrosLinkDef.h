8cf73396b023579abebd23d6648a97fa4417d22a
Andrea Venturi <andrea.venturi@cern.ch>
2014-04-06 10:22:07 +0200

    New macro to print phase 2 occupancy plots

--- DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
+++ DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
@@ -2,4 +2,6 @@
 #ifdef __CINT__
 #pragma link off all functions;
 #pragma link C++ function PlotOccupancyMap;
+#pragma link C++ function PlotOccupancyMapPhase2;
+#pragma link C++ function printFrame;
 #endif
--- DPGAnalysis/SiStripTools/bin/OccupancyPlotMacros.h
+++ DPGAnalysis/SiStripTools/bin/OccupancyPlotMacros.h
@@ -1,4 +1,9 @@
 class TFile;
+class TCanvas;
+class TH1D;
+class TText;
 
 void PlotOccupancyMap(TFile* ff, const char* module, const float min, const float max, const float mmin, const float mmax, const int color);
+void PlotOccupancyMapPhase2(TFile* ff, const char* module, const float min, const float max, const float mmin, const float mmax, const int color);
+void printFrame(TCanvas* c, TH1D* h, TText* t, const int frame, const int min, const int max);
 
