e5eee67ce02c78d95b84ae28212e2e6acc48df54
Andrea Venturi <andrea.venturi@cern.ch>
2014-04-06 10:22:07 +0200

    New macro to print phase 2 occupancy plots
    Conflicts:
    	DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
    	DPGAnalysis/SiStripTools/bin/OccupancyPlotMacros.cc
    	DPGAnalysis/SiStripTools/bin/OccupancyPlotMacros.h

--- DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
+++ DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
@@ -7,6 +7,8 @@
 #ifdef __CINT__
 #pragma link off all functions;
 #pragma link C++ function PlotOccupancyMap;
+#pragma link C++ function PlotOccupancyMapPhase2;
+#pragma link C++ function printFrame;
 #pragma link C++ function combinedOccupancy;
 #pragma link C++ function AverageRunMultiplicity;
 #pragma link C++ function PlotPixelMultVtxPos;
