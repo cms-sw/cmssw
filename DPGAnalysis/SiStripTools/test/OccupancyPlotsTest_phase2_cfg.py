e184eaaac76792a22bb3d444d399cb5b6f37a7c3
Andrea Venturi <andrea.venturi@cern.ch>
2014-04-07 12:32:50 +0200

    Tuning of the message logger options

--- DPGAnalysis/SiStripTools/test/OccupancyPlotsTest_phase2_cfg.py
+++ DPGAnalysis/SiStripTools/test/OccupancyPlotsTest_phase2_cfg.py
@@ -37,10 +37,13 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cout.placeholder = cms.untracked.bool(False)
 process.MessageLogger.cout.threshold = cms.untracked.string("INFO")
 process.MessageLogger.cout.default = cms.untracked.PSet(
-    limit = cms.untracked.int32(10000000)
+    limit = cms.untracked.int32(0)
+    )
+process.MessageLogger.cout.FwkSummary = cms.untracked.PSet(
+    limit = cms.untracked.int32(100000000)
     )
 process.MessageLogger.cout.FwkReport = cms.untracked.PSet(
-    reportEvery = cms.untracked.int32(10000)
+    reportEvery = cms.untracked.int32(1000)
     )
 
 process.MessageLogger.cerr.placeholder = cms.untracked.bool(False)
