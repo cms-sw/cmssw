e913e9199823b8d7be8f4b660dfe3218f94b5562
Ianna Osborne <ianna.osborne@cern.ch>
2014-04-07 14:53:50 +0200

    Extended2015 is the default scenario from now on.

--- CondTools/Geometry/test/writehelpers/createPayloads.sh
+++ CondTools/Geometry/test/writehelpers/createPayloads.sh
@@ -14,9 +14,9 @@ sed -i {s/TagXX/${mytag}/g} *.txt
 sed -i {s/TagXX/${mytag}/g} splitDatabase.sh
 
 # First read in the little XML files and create the
-# large XML file for the Extended scenario.
+# large XML file for the Extended2015 scenario.
 # Input cff                    Output file
-# GeometryExtended_cff         geSingleBigFile.xml
+# GeometryExtended2015_cff         geSingleBigFile.xml
 cmsRun geometryxmlwriter.py
 
 # Now convert the content of the large XML file into
@@ -34,66 +34,10 @@ cmsRun geometrywriter.py
 # files
 #
 # Input cff                    Output file
-# GeometryExtendedGFlash_cff   gegSingleBigFile.xml
 # GeometryIdeal_cff            giSingleBigFile.xml
-# GeometryExtendedX0Min_cff    gexminSingleBigFile.xml
-# GeometryExtendedX0Max_cff    gexmaxSingleBigFile.xml
-# GeometryExtendedLiMin_cff    geliminSingleBigFile.xml
-# GeometryExtendedLiMax_cff    gelimaxSingleBigFile.xml
 #
-sed -i '{s/Extended/ExtendedGFlash/g}' geometryxmlwriter.py
-sed -i '{s/\/ge/\/geg/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/ExtendedGFlash/Ideal/g}' geometryxmlwriter.py
-sed -i '{s/\/geg/\/gi/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Ideal/ExtendedX0Min/g}' geometryxmlwriter.py
-sed -i '{s/\/gi/\/gexmin/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/X0Min/X0Max/g}' geometryxmlwriter.py
-sed -i '{s/\/gexmin/\/gexmax/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/X0Max/LiMin/g}' geometryxmlwriter.py
-sed -i '{s/\/gexmax/\/gelimin/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/LiMin/LiMax/g}' geometryxmlwriter.py
-sed -i '{s/\/gelimin/\/gelimax/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/LiMax/PhaseIBeamPipe/g}' geometryxmlwriter.py
-sed -i '{s/\/gelimax/\/gepibp/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/PhaseIBeamPipe/PostLS1/g}' geometryxmlwriter.py
-sed -i '{s/\/gepibp/\/gepostls1/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/PostLS1/PostLS2/g}' geometryxmlwriter.py
-sed -i '{s/\/gepostls1/\/gepostls2/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-
-sed -i '{s/PostLS2/Flat10Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/gepostls2/\/geflat10percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Flat10Percent/Flat20Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/geflat10percent/\/geflat20percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Flat20Percent/Flat30Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/geflat20percent/\/geflat30percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Flat30Percent/Flat10Services30Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/geflat30percent/\/geflat10services30percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Flat10Services30Percent/Flat20Services30Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/geflat10services30percent/\/geflat20services30percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-sed -i '{s/Flat20Services30Percent/Services30Percent/g}' geometryxmlwriter.py
-sed -i '{s/\/geflat20services30percent/\/geservices30percent/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-
-sed -i '{s/Services30Percent/HFLibraryNoCastor/g}' geometryxmlwriter.py
-sed -i '{s/\/geservices30percent/\/gehflibrarynocastor/g}' geometryxmlwriter.py
-cmsRun geometryxmlwriter.py
-
-sed -i '{s/HFLibraryNoCastor/ZeroMaterial/g}' geometryxmlwriter.py
-sed -i '{s/\/gehflibrarynocastor/\/zeromaterial/g}' geometryxmlwriter.py
+sed -i '{s/Extended2015/Ideal/g}' geometryxmlwriter.py
+sed -i '{s/\/ge/\/gi/g}' geometryxmlwriter.py
 cmsRun geometryxmlwriter.py
 
 # Read the one big XML file and output a record to the
@@ -103,66 +47,11 @@ cmsRun geometryxmlwriter.py
 # tag
 #
 # Input file                Output tag
-# gegSingleBigFile.xml      XMLFILE_Geometry_${mytag}_ExtendedGFlash_mc
+# gegSingleBigFile.xml      XMLFILE_Geometry_${mytag}_Extended2015GFlash_mc
 # giSingleBigFile.xml       XMLFILE_Geometry_${mytag}_Ideal_mc
-# gexminSingleBigFile.xml   XMLFILE_Geometry_${mytag}_ExtendedX0Min_mc
-# gexmaxSingleBigFile.xml   XMLFILE_Geometry_${mytag}_ExtendedX0Max_mc
-# geliminSingleBigFile.xml  XMLFILE_Geometry_${mytag}_ExtendedLiMin_mc
-# gelimaxSingleBigFile.xml  XMLFILE_Geometry_${mytag}_ExtendedLiMax_mc
 #
-sed -i '{s/Extended/ExtendedGFlash/g}' xmlgeometrywriter.py
-sed -i '{s/\/ge/\/geg/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/ExtendedGFlash/Ideal/g}' xmlgeometrywriter.py
-sed -i '{s/\/geg/\/gi/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Ideal/ExtendedX0Min/g}' xmlgeometrywriter.py
-sed -i '{s/\/gi/\/gexmin/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/X0Min/X0Max/g}' xmlgeometrywriter.py
-sed -i '{s/\/gexmin/\/gexmax/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/X0Max/LiMin/g}' xmlgeometrywriter.py
-sed -i '{s/\/gexmax/\/gelimin/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/LiMin/LiMax/g}' xmlgeometrywriter.py
-sed -i '{s/\/gelimin/\/gelimax/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/LiMax/PhaseIBeamPipe/g}' xmlgeometrywriter.py
-sed -i '{s/\/gelimax/\/gepibp/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/PhaseIBeamPipe/PostLS1/g}' xmlgeometrywriter.py
-sed -i '{s/\/gepibp/\/gepostls1/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/PostLS1/PostLS2/g}' xmlgeometrywriter.py
-sed -i '{s/\/gepostls1/\/gepostls2/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-
-sed -i '{s/PostLS2/Flat10Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/gepostls2/\/geflat10percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Flat10Percent/Flat20Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/geflat10percent/\/geflat20percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Flat20Percent/Flat30Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/geflat20percent/\/geflat30percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Flat30Percent/Flat10Services30Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/geflat30percent/\/geflat10services30percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Flat10Services30Percent/Flat20Services30Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/geflat10services30percent/\/geflat20services30percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-sed -i '{s/Flat20Services30Percent/Services30Percent/g}' xmlgeometrywriter.py
-sed -i '{s/\/geflat20services30percent/\/geservices30percent/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-
-sed -i '{s/Services30Percent/HFLibraryNoCastor/g}' xmlgeometrywriter.py
-sed -i '{s/\/geservices30percent/\/gehflibrarynocastor/g}' xmlgeometrywriter.py
-cmsRun xmlgeometrywriter.py
-
-sed -i '{s/HFLibraryNoCastor/ZeroMaterial/g}' xmlgeometrywriter.py
-sed -i '{s/\/gehflibrarynocastor/\/zeromaterial/g}' xmlgeometrywriter.py
+sed -i '{s/Extended2015/Ideal/g}' xmlgeometrywriter.py
+sed -i '{s/\/ge/\/gi/g}' xmlgeometrywriter.py
 cmsRun xmlgeometrywriter.py
 
 # All the database objects were written into one database
