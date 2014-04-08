dd43dee100744e86b24686c05b531715e652da14
R. Alex Barbieri <luck@mit.edu>
2014-04-08 14:51:54 -0400

    make sure that the original loop avoids making central jets at the edge

--- L1Trigger/L1TCalorimeter/src/JetFinderMethods.cc
+++ L1Trigger/L1TCalorimeter/src/JetFinderMethods.cc
@@ -140,7 +140,7 @@ namespace l1t {
 	}
 
 	//first iteration, eta cut defines forward
-	const bool forward = (jetEta < 4 || jetEta > 17);
+	const bool forward = (jetEta <= 4 || jetEta >= 17);
 	int jetQual = 0;
 	if(forward)
 	  jetQual |= 0x2;
@@ -171,108 +171,108 @@ namespace l1t {
       unsigned int nNeighbors = 0;
       for(std::vector<CaloRegion>::const_iterator neighbor = regions->begin(); neighbor != regions->end(); neighbor++) {
 
-	double neighborET = neighbor->hwPt(); //regionPhysicalEt(*neighbor);
-	if(deltaGctPhi(*region, *neighbor) == 1 &&
-	   (region->hwEta()    ) == neighbor->hwEta()) {
-	  neighborN_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	else if(deltaGctPhi(*region, *neighbor) == -1 &&
-		(region->hwEta()    ) == neighbor->hwEta()) {
-	  neighborS_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	else if(deltaGctPhi(*region, *neighbor) == 0 &&
-		(region->hwEta() + 1) == neighbor->hwEta()) {
-	  neighborE_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	// else if(deltaGctPhi(*region, *neighbor) == 0 &&
-	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
-	//   neighborW_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
-	else if(deltaGctPhi(*region, *neighbor) == 1 &&
-		(region->hwEta() + 1) == neighbor->hwEta()) {
-	  neighborNE_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	// else if(deltaGctPhi(*region, *neighbor) == -1 &&
-	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
-	//   neighborSW_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
-	// else if(deltaGctPhi(*region, *neighbor) == 1 &&
-	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
-	//   neighborNW_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
-	else if(deltaGctPhi(*region, *neighbor) == -1 &&
-		(region->hwEta() + 1) == neighbor->hwEta()) {
-	  neighborSE_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
+    	double neighborET = neighbor->hwPt(); //regionPhysicalEt(*neighbor);
+    	if(deltaGctPhi(*region, *neighbor) == 1 &&
+    	   (region->hwEta()    ) == neighbor->hwEta()) {
+    	  neighborN_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    		(region->hwEta()    ) == neighbor->hwEta()) {
+    	  neighborS_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	else if(deltaGctPhi(*region, *neighbor) == 0 &&
+    		(region->hwEta() + 1) == neighbor->hwEta()) {
+    	  neighborE_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	// else if(deltaGctPhi(*region, *neighbor) == 0 &&
+    	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
+    	//   neighborW_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
+    	else if(deltaGctPhi(*region, *neighbor) == 1 &&
+    		(region->hwEta() + 1) == neighbor->hwEta()) {
+    	  neighborNE_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	// else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
+    	//   neighborSW_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
+    	// else if(deltaGctPhi(*region, *neighbor) == 1 &&
+    	// 	(region->hwEta() - 1) == neighbor->hwEta()) {
+    	//   neighborNW_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
+    	else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    		(region->hwEta() + 1) == neighbor->hwEta()) {
+    	  neighborSE_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
       }
       if(regionET > neighborN_et &&
-	 //regionET > neighborNW_et &&
-	 //regionET > neighborW_et &&
-	 //regionET > neighborSW_et &&
-	 regionET >= neighborNE_et &&
-	 regionET >= neighborE_et &&
-	 regionET >= neighborSE_et &&
-	 regionET >= neighborS_et) {
-	unsigned int jetET = regionET +
-	  neighborN_et + neighborS_et + neighborE_et + /*neighborW_et +*/
-	  neighborNE_et + /*neighborSW_et +*/ neighborSE_et;// + neighborNW_et;
-	/*
-	  int jetPhi = region->hwPhi() * 4 +
-	  ( - 2 * (neighborS_et + neighborSE_et + neighborSW_et)
-	  + 2 * (neighborN_et + neighborNE_et + neighborNW_et) ) / jetET;
-	  if(jetPhi < 0) {
+    	 //regionET > neighborNW_et &&
+    	 //regionET > neighborW_et &&
+    	 //regionET > neighborSW_et &&
+    	 regionET >= neighborNE_et &&
+    	 regionET >= neighborE_et &&
+    	 regionET >= neighborSE_et &&
+    	 regionET >= neighborS_et) {
+    	unsigned int jetET = regionET +
+    	  neighborN_et + neighborS_et + neighborE_et + /*neighborW_et +*/
+    	  neighborNE_et + /*neighborSW_et +*/ neighborSE_et;// + neighborNW_et;
+    	/*
+    	  int jetPhi = region->hwPhi() * 4 +
+    	  ( - 2 * (neighborS_et + neighborSE_et + neighborSW_et)
+    	  + 2 * (neighborN_et + neighborNE_et + neighborNW_et) ) / jetET;
+    	  if(jetPhi < 0) {
 
-	  }
-	  else if(jetPhi >= ((int) N_JET_PHI)) {
-	  jetPhi -= N_JET_PHI;
-	  }
-	  int jetEta = region->hwEta() * 4 +
-	  ( - 2 * (neighborW_et + neighborNW_et + neighborSW_et)
-	  + 2 * (neighborE_et + neighborNE_et + neighborSE_et) ) / jetET;
-	  if(jetEta < 0) jetEta = 0;
-	  if(jetEta >= ((int) N_JET_ETA)) jetEta = N_JET_ETA - 1;
-	*/
-	// Temporarily use the region granularity -- we will try to improve as above when code is debugged
-	int jetPhi = region->hwPhi();
-	int jetEta = region->hwEta();
+    	  }
+    	  else if(jetPhi >= ((int) N_JET_PHI)) {
+    	  jetPhi -= N_JET_PHI;
+    	  }
+    	  int jetEta = region->hwEta() * 4 +
+    	  ( - 2 * (neighborW_et + neighborNW_et + neighborSW_et)
+    	  + 2 * (neighborE_et + neighborNE_et + neighborSE_et) ) / jetET;
+    	  if(jetEta < 0) jetEta = 0;
+    	  if(jetEta >= ((int) N_JET_ETA)) jetEta = N_JET_ETA - 1;
+    	*/
+    	// Temporarily use the region granularity -- we will try to improve as above when code is debugged
+    	int jetPhi = region->hwPhi();
+    	int jetEta = region->hwEta();
 
-	bool neighborCheck = (nNeighbors == 5);
-	// On the eta edge we only expect 5 neighbors
-	// if (!neighborCheck && (jetEta == 0 || jetEta == 21) && nNeighbors == 5)
-	//   neighborCheck = true;
+    	bool neighborCheck = (nNeighbors == 5);
+    	// On the eta edge we only expect 5 neighbors
+    	// if (!neighborCheck && (jetEta == 0 || jetEta == 21) && nNeighbors == 5)
+    	//   neighborCheck = true;
 
-	if (!neighborCheck) {
-	  std::cout << "phi: " << jetPhi << " eta: " << jetEta << " n: " << nNeighbors << std::endl;
-	  assert(false);
-	}
+    	if (!neighborCheck) {
+    	  std::cout << "phi: " << jetPhi << " eta: " << jetEta << " n: " << nNeighbors << std::endl;
+    	  assert(false);
+    	}
 
-	const bool forward = false; //by definition
-	int jetQual = 0;
-	if(forward)
-	  jetQual |= 0x2;
+    	const bool forward = false; //by definition
+    	int jetQual = 0;
+    	if(forward)
+    	  jetQual |= 0x2;
 
-	ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > *jetLorentz =
-	  new ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >();
-	l1t::Jet theJet(*jetLorentz, jetET, jetEta, jetPhi, jetQual);
-	//l1t::Jet theJet(0, jetET, jetEta, jetPhi);
+    	ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > *jetLorentz =
+    	  new ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >();
+    	l1t::Jet theJet(*jetLorentz, jetET, jetEta, jetPhi, jetQual);
+    	//l1t::Jet theJet(0, jetET, jetEta, jetPhi);
 
-	jets->push_back(theJet);
+    	jets->push_back(theJet);
       }
     }
 
@@ -293,111 +293,111 @@ namespace l1t {
       unsigned int nNeighbors = 0;
       for(std::vector<CaloRegion>::const_iterator neighbor = regions->begin(); neighbor != regions->end(); neighbor++) {
 
-	double neighborET = neighbor->hwPt(); //regionPhysicalEt(*neighbor);
-	if(deltaGctPhi(*region, *neighbor) == 1 &&
-	   (region->hwEta()    ) == neighbor->hwEta()) {
-	  neighborN_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	else if(deltaGctPhi(*region, *neighbor) == -1 &&
-		(region->hwEta()    ) == neighbor->hwEta()) {
-	  neighborS_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	// else if(deltaGctPhi(*region, *neighbor) == 0 &&
-	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
-	//   neighborE_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
-	else if(deltaGctPhi(*region, *neighbor) == 0 &&
-		(region->hwEta() - 1) == neighbor->hwEta()) {
-	  neighborW_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	// else if(deltaGctPhi(*region, *neighbor) == 1 &&
-	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
-	//   neighborNE_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
-	else if(deltaGctPhi(*region, *neighbor) == -1 &&
-		(region->hwEta() - 1) == neighbor->hwEta()) {
-	  neighborSW_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	else if(deltaGctPhi(*region, *neighbor) == 1 &&
-		(region->hwEta() - 1) == neighbor->hwEta()) {
-	  neighborNW_et = neighborET;
-	  nNeighbors++;
-	  continue;
-	}
-	// else if(deltaGctPhi(*region, *neighbor) == -1 &&
-	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
-	//   neighborSE_et = neighborET;
-	//   nNeighbors++;
-	//   continue;
-	// }
+    	double neighborET = neighbor->hwPt(); //regionPhysicalEt(*neighbor);
+    	if(deltaGctPhi(*region, *neighbor) == 1 &&
+    	   (region->hwEta()    ) == neighbor->hwEta()) {
+    	  neighborN_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    		(region->hwEta()    ) == neighbor->hwEta()) {
+    	  neighborS_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	// else if(deltaGctPhi(*region, *neighbor) == 0 &&
+    	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
+    	//   neighborE_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
+    	else if(deltaGctPhi(*region, *neighbor) == 0 &&
+    		(region->hwEta() - 1) == neighbor->hwEta()) {
+    	  neighborW_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	// else if(deltaGctPhi(*region, *neighbor) == 1 &&
+    	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
+    	//   neighborNE_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
+    	else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    		(region->hwEta() - 1) == neighbor->hwEta()) {
+    	  neighborSW_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	else if(deltaGctPhi(*region, *neighbor) == 1 &&
+    		(region->hwEta() - 1) == neighbor->hwEta()) {
+    	  neighborNW_et = neighborET;
+    	  nNeighbors++;
+    	  continue;
+    	}
+    	// else if(deltaGctPhi(*region, *neighbor) == -1 &&
+    	// 	(region->hwEta() + 1) == neighbor->hwEta()) {
+    	//   neighborSE_et = neighborET;
+    	//   nNeighbors++;
+    	//   continue;
+    	// }
       }
       if(//regionET > neighborN_et &&
-	 regionET > neighborNW_et &&
-	 regionET > neighborW_et &&
-	 regionET > neighborSW_et &&
-	 //regionET >= neighborNE_et &&
-	 //regionET >= neighborE_et &&
-	 //regionET >= neighborSE_et &&
-	 regionET >= neighborS_et) {
-	unsigned int jetET = regionET +
-	  neighborN_et + neighborS_et + /*neighborE_et + */neighborW_et +
-	  /*neighborNE_et +*/ neighborSW_et + /*neighborSE_et + */neighborNW_et;
-	/*
-	  int jetPhi = region->hwPhi() * 4 +
-	  ( - 2 * (neighborS_et + neighborSE_et + neighborSW_et)
-	  + 2 * (neighborN_et + neighborNE_et + neighborNW_et) ) / jetET;
-	  if(jetPhi < 0) {
+    	 regionET > neighborNW_et &&
+    	 regionET > neighborW_et &&
+    	 regionET > neighborSW_et &&
+    	 //regionET >= neighborNE_et &&
+    	 //regionET >= neighborE_et &&
+    	 //regionET >= neighborSE_et &&
+    	 regionET >= neighborS_et) {
+    	unsigned int jetET = regionET +
+    	  neighborN_et + neighborS_et + /*neighborE_et + */neighborW_et +
+    	  /*neighborNE_et +*/ neighborSW_et + /*neighborSE_et + */neighborNW_et;
+    	/*
+    	  int jetPhi = region->hwPhi() * 4 +
+    	  ( - 2 * (neighborS_et + neighborSE_et + neighborSW_et)
+    	  + 2 * (neighborN_et + neighborNE_et + neighborNW_et) ) / jetET;
+    	  if(jetPhi < 0) {
 
-	  }
-	  else if(jetPhi >= ((int) N_JET_PHI)) {
-	  jetPhi -= N_JET_PHI;
-	  }
-	  int jetEta = region->hwEta() * 4 +
-	  ( - 2 * (neighborW_et + neighborNW_et + neighborSW_et)
-	  + 2 * (neighborE_et + neighborNE_et + neighborSE_et) ) / jetET;
-	  if(jetEta < 0) jetEta = 0;
-	  if(jetEta >= ((int) N_JET_ETA)) jetEta = N_JET_ETA - 1;
-	*/
-	// Temporarily use the region granularity -- we will try to improve as above when code is debugged
-	int jetPhi = region->hwPhi();
-	int jetEta = region->hwEta();
+    	  }
+    	  else if(jetPhi >= ((int) N_JET_PHI)) {
+    	  jetPhi -= N_JET_PHI;
+    	  }
+    	  int jetEta = region->hwEta() * 4 +
+    	  ( - 2 * (neighborW_et + neighborNW_et + neighborSW_et)
+    	  + 2 * (neighborE_et + neighborNE_et + neighborSE_et) ) / jetET;
+    	  if(jetEta < 0) jetEta = 0;
+    	  if(jetEta >= ((int) N_JET_ETA)) jetEta = N_JET_ETA - 1;
+    	*/
+    	// Temporarily use the region granularity -- we will try to improve as above when code is debugged
+    	int jetPhi = region->hwPhi();
+    	int jetEta = region->hwEta();
 
-	bool neighborCheck = (nNeighbors == 5);
-	// On the eta edge we only expect 5 neighbors
-	// if (!neighborCheck && (jetEta == 0 || jetEta == 21) && nNeighbors == 5)
-	//   neighborCheck = true;
+    	bool neighborCheck = (nNeighbors == 5);
+    	// On the eta edge we only expect 5 neighbors
+    	// if (!neighborCheck && (jetEta == 0 || jetEta == 21) && nNeighbors == 5)
+    	//   neighborCheck = true;
 
-	if (!neighborCheck) {
-	  std::cout << "phi: " << jetPhi << " eta: " << jetEta << " n: " << nNeighbors << std::endl;
-	  assert(false);
-	}
+    	if (!neighborCheck) {
+    	  std::cout << "phi: " << jetPhi << " eta: " << jetEta << " n: " << nNeighbors << std::endl;
+    	  assert(false);
+    	}
 
-	const bool forward = false; //by definition
-	int jetQual = 0;
-	if(forward)
-	  jetQual |= 0x2;
+    	const bool forward = false; //by definition
+    	int jetQual = 0;
+    	if(forward)
+    	  jetQual |= 0x2;
 
-	ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > *jetLorentz =
-	  new ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >();
-	l1t::Jet theJet(*jetLorentz, jetET, jetEta, jetPhi, jetQual);
-	//l1t::Jet theJet(0, jetET, jetEta, jetPhi);
+    	ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > *jetLorentz =
+    	  new ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> >();
+    	l1t::Jet theJet(*jetLorentz, jetET, jetEta, jetPhi, jetQual);
+    	//l1t::Jet theJet(0, jetET, jetEta, jetPhi);
 
-	jets->push_back(theJet);
+    	jets->push_back(theJet);
       }
     }
-
+ 
     //the jets should be sorted, highest pT first.
     // do not truncate the jet list, GT converter handles that
     std::sort(jets->begin(), jets->end(), compareJets);
