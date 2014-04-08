80d8e395eae466c56077f6c3c59dde68f387010a
Andrea <andrea.rizzi@cern.ch>
2014-04-08 15:09:08 +0200

    EI config based on CommonTools/ParticleFlow/python/EITopPAG_cff.py

--- PhysicsTools/PatAlgos/test/miniAOD/example_ei.py
+++ PhysicsTools/PatAlgos/test/miniAOD/example_ei.py
@@ -10,21 +10,37 @@ from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
 from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
 from RecoMET.METProducers.PFMET_cfi import pfMet
 
-
-
-process.pfNoPU = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
+#select isolated collections
 process.selectedMuons = cms.EDFilter("CandPtrSelector", src = cms.InputTag("slimmedMuons"), cut = cms.string('''abs(eta)<2.5 && pt>10. &&
-    (pfIsolationR04().sumChargedHadronPt) / pt < 0.10 && 
+   (pfIsolationR04().sumChargedHadronPt+
+    max(0.,pfIsolationR04().sumNeutralHadronEt+
+    pfIsolationR04().sumPhotonEt-
+    0.50*pfIsolationR04().sumPUPt))/pt < 0.20 && 
     (isPFMuon && (isGlobalMuon || isTrackerMuon) )'''))
-process.pfNoMuon =  cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfNoPU"), veto = cms.InputTag("selectedMuons"))
+process.selectedElectrons = cms.EDFilter("CandPtrSelector", src = cms.InputTag("slimmedElectrons"), cut = cms.string('''abs(eta)<2.5 && pt>20. &&
+    gsfTrack.isAvailable() &&
+    gsfTrack.trackerExpectedHitsInner.numberOfLostHits<2 &&
+    (pfIsolationVariables().sumChargedHadronPt+
+    max(0.,pfIsolationVariables().sumNeutralHadronEt+
+    pfIsolationVariables().sumPhotonEt-
+    0.5*pfIsolationVariables().sumPUPt))/pt < 0.15'''))
+
+#do projections
+process.pfCHS = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
+process.pfNoMuonCHS =  cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfCHS"), veto = cms.InputTag("selectedMuons"))
+process.pfNoElectronsCHS = cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfNoMuonCHS"), veto =  cms.InputTag("selectedElectrons"))
+
+process.pfNoMuon =  cms.EDProducer("CandPtrProjector", src = cms.InputTag("packedPFCandidates"), veto = cms.InputTag("selectedMuons"))
+process.pfNoElectrons = cms.EDProducer("CandPtrProjector", src = cms.InputTag("pfNoMuon"), veto =  cms.InputTag("selectedElectrons"))
 
 
-process.ak5PFJets = ak5PFJets.clone(src = 'packedPFCandidates', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
-process.ak5PFJetsCHS = ak5PFJets.clone(src = 'pfNoMuon', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
+
+process.ak5PFJets = ak5PFJets.clone(src = 'pfNoElectrons', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
+process.ak5PFJetsCHS = ak5PFJets.clone(src = 'pfNoElectronsCHS', doAreaFastjet = True) # no idea while doArea is false by default, but it's True in RECO so we have to set it
 process.ak5GenJets = ak5GenJets.clone(src = 'packedGenParticles')
-process.pfMet = pfMet.clone(src = "packedPFCandidates")
-process.pfMet.calculateSignificance = False # this can't be easily implemented on packed PF candidates at the moment
 
+
+# The following is make patJets, but EI is done with the above
 process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
 process.load("Configuration.EventContent.EventContent_cff")
 process.load('Configuration.StandardSequences.Geometry_cff')
@@ -33,29 +49,43 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = 'START70_V6::All'
 
 
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+addJetCollection(
+   process,
+   postfix   = "",
+   labelName = 'AK5PFCHS',
+   jetSource = cms.InputTag('ak5PFJetsCHS'),
+   trackSource = cms.InputTag('unpackedTracksAndVertices'), 
+   pvSource = cms.InputTag('unpackedTracksAndVertices'), 
+   jetCorrections = ('AK5PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'Type-2'),
+   btagDiscriminators = [      'combinedSecondaryVertexBJetTags'     ]
+   )
+addJetCollection(
+   process,
+   postfix   = "",
+   labelName = 'AK5PF',
+   jetSource = cms.InputTag('ak5PFJets'),
+   trackSource = cms.InputTag('unpackedTracksAndVertices'),
+   pvSource = cms.InputTag('unpackedTracksAndVertices'), 
+   jetCorrections = ('AK5PF', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'Type-2'),
+   btagDiscriminators = [      'combinedSecondaryVertexBJetTags'     ]
+   )
 
-process.patJets.addJetCharge   = False
-process.patJets.addBTagInfo    = True
-process.patJets.getJetMCFlavour = False
-process.patJets.addAssociatedTracks = False
-process.patJetPartonMatch.matched = "prunedGenParticles"
-process.patJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
-process.patMETs.addGenMET = False # There's no point in recalculating this, and we can't remake it since we don't have genParticles beyond |eta|=5
+#adjust MC matching
+process.patJetPartonMatchPatJetsAK5PFCHS.matched = "prunedGenParticles"
+process.patJetPartonMatchPatJetsAK5PF.matched = "prunedGenParticles"
+process.patJetPartons.src = "prunedGenParticles"
+process.patJetPartons.skipFirstN = cms.uint32(0) # do not skip first 6 particles, we already pruned some!
+process.patJetPartons.acceptNoDaughters = cms.bool(True) # as we drop intermediate stuff, we need to accept quarks with no siblings
 
-process.load('RecoBTag.Configuration.RecoBTag_cff')
-process.load('RecoJets.Configuration.RecoJetAssociations_cff')
+#adjust PV
+process.patJetCorrFactorsPatJetsAK5PFCHS.primaryVertices = "offlineSlimmedPrimaryVertices"
+process.patJetCorrFactorsPatJetsAK5PF.primaryVertices = "offlineSlimmedPrimaryVertices"
 
+#recreate tracks and pv for btagging
 process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
-
-process.ak5JetTracksAssociatorAtVertexPF.jets = cms.InputTag("ak5PFJetsCHS")
-process.ak5JetTracksAssociatorAtVertexPF.tracks = cms.InputTag("unpackedTracksAndVertices")
-process.impactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
 process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
 
-
-process.p = cms.Path(
-    process.patJets + process.patMETs
-)
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
@@ -65,7 +95,7 @@ process.options.allowUnscheduled = cms.untracked.bool(True)
 
 process.OUT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('test.root'),
-    outputCommands = cms.untracked.vstring(['drop *','keep patJets_patJets_*_*','keep *_*_*_PAT','keep *_selectedMuons_*_*','keep *_pfNoPU_*_*','keep *_pfNoMuon_*_*'])
+    outputCommands = cms.untracked.vstring(['drop *','keep patJets_patJetsAK5PF_*_*','keep patJets_patJetsAK5PFCHS_*_*','keep *_*_*_PAT'])
 )
 process.endpath= cms.EndPath(process.OUT)
 
