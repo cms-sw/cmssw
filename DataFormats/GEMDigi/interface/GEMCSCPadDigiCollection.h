945656760fa67bab382836e3c4c0c0160627d3dc
Sven Dildick <sven.dildick@cern.ch>
2014-04-10 07:44:47 +0200

    clean-up

--- DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h
+++ DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h
@@ -3,8 +3,6 @@
 /** \class GEMCSCPadDigiCollection
  *  
  *  \author Vadim Khotilovich
- *  \version $Id: GEMCSCPadDigiCollection.h,v 1.1 2013/01/18 04:21:50 khotilov Exp $
- *  \date 21 Apr 2005
  */
 
 #include <DataFormats/MuonDetId/interface/GEMDetId.h>
--- DataFormats/GEMDigi/interface/GEMDigiCollection.h
+++ DataFormats/GEMDigi/interface/GEMDigiCollection.h
@@ -3,8 +3,6 @@
 /** \class GEMDigiCollection
  *  
  *  \author Vadim Khotilovich
- *  \version $Id: GEMDigiCollection.h,v 1.1 2012/12/08 01:45:22 khotilov Exp $
- *  \date 21 Apr 2005
  */
 
 #include <DataFormats/MuonDetId/interface/GEMDetId.h>
