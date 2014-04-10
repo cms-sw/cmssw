910d0620594895142d67e00cdc8cf0e1a4a1d8bf
Remi Mommsen <remigius.mommsen@cern.ch>
2014-04-10 15:48:14 +0200

    Modernize the DAQ1 error stream source, enable reading of all FRD versions, and rename it to lessen confusion when it will be used as input source for reading BU files.

---
+++ EventFilter/Utilities/plugins/FRDStreamSource.h
@@ -0,0 +1,51 @@
+#ifndef EventFilter_Utilities_FRDStreamSource_h
+#define EventFilter_Utilities_FRDStreamSource_h
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+
+#include "FWCore/Sources/interface/ProducerSourceFromFiles.h"
+
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/Provenance/interface/Timestamp.h"
+
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <fstream>
+
+
+class FRDStreamSource : public edm::ProducerSourceFromFiles {
+
+public:
+  // construction/destruction
+  FRDStreamSource(edm::ParameterSet const& pset,
+                  edm::InputSourceDescription const& desc);
+  virtual ~FRDStreamSource() {};
+
+private:
+  // member functions
+  virtual bool setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& theTime);
+  virtual void produce(edm::Event& e);
+
+  void beginRun(edm::Run&) {}
+  void endRun(edm::Run&) {}
+  void beginLuminosityBlock(edm::LuminosityBlock&) {}
+  void endLuminosityBlock(edm::LuminosityBlock&) {}
+
+  bool openFile(const std::string& fileName);
+
+
+private:
+  // member data
+  std::vector<std::string>::const_iterator itFileName_;
+  std::ifstream fin_;
+  std::auto_ptr<FEDRawDataCollection> rawData_;
+  std::vector<char> buffer_;
+  const bool verifyAdler32_;
+};
+
+#endif // EventFilter_Utilities_FRDStreamSource_h
