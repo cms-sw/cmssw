42efb63aa6673012b4243120f3cc5fdf560fe9a0
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2014-04-15 19:38:22 +0200

    update to add serialisation for new DYTThrObject class

--- CondFormats/RecoMuonObjects/src/Serialization.cc
+++ CondFormats/RecoMuonObjects/src/Serialization.cc
@@ -9,6 +9,21 @@
 #include "CondFormats/Serialization/interface/Instantiate.h"
 
 template <class Archive>
+void DYTThrObject::serialize(Archive & ar, const unsigned int)
+{
+    ar & BOOST_SERIALIZATION_NVP(thrsVec);
+}
+COND_SERIALIZATION_INSTANTIATE(DYTThrObject);
+
+template <class Archive>
+void DYTThrObject::DytThrStruct::serialize(Archive & ar, const unsigned int)
+{
+    ar & BOOST_SERIALIZATION_NVP(id);
+    ar & BOOST_SERIALIZATION_NVP(thr);
+}
+COND_SERIALIZATION_INSTANTIATE(DYTThrObject::DytThrStruct);
+
+template <class Archive>
 void MuScleFitDBobject::serialize(Archive & ar, const unsigned int)
 {
     ar & BOOST_SERIALIZATION_NVP(identifiers);
