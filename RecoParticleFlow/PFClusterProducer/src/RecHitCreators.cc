f600160b746938bf74814a73c0de3fbeb4da9501
Michail <bachtis@cern.ch>
2014-04-15 15:08:33 +0200

    Adding HBHE creator so that we do not loop twice for HE and HB

--- RecoParticleFlow/PFClusterProducer/src/RecHitCreators.cc
+++ RecoParticleFlow/PFClusterProducer/src/RecHitCreators.cc
@@ -3,6 +3,7 @@
 #include "RecoParticleFlow/PFClusterProducer/interface/PFRecHitCreatorBase.h"
 #include "RecoParticleFlow/PFClusterProducer/interface/PFEcalRecHitCreator.h"
 #include "RecoParticleFlow/PFClusterProducer/interface/PFHcalRecHitCreator.h"
+#include "RecoParticleFlow/PFClusterProducer/interface/PFHBHERecHitCreator.h"
 #include "RecoParticleFlow/PFClusterProducer/interface/PFPSRecHitCreator.h"
 
 
@@ -11,6 +12,7 @@ DEFINE_EDM_PLUGIN(PFRecHitFactory, PFEERecHitCreator, "PFEERecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFEBRecHitCreator, "PFEBRecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHERecHitCreator, "PFHERecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHBRecHitCreator, "PFHBRecHitCreator");
+DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHBHERecHitCreator, "PFHBHERecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHORecHitCreator, "PFHORecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHFEMRecHitCreator, "PFHFEMRecHitCreator");
 DEFINE_EDM_PLUGIN(PFRecHitFactory, PFHFHADRecHitCreator, "PFHFHADRecHitCreator");
