6cc4c4538fbbd6eb1448f3f125e734960a3a3666
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-04-16 10:35:58 +0200

    make CPE thread safe, remove unsused attributes and methods, made FastCPE dummy as non implementable in the way it was

--- FastSimulation/TrackingRecHitProducer/src/FastPixelCPE.cc
+++ FastSimulation/TrackingRecHitProducer/src/FastPixelCPE.cc
@@ -7,30 +7,3 @@
 // Services
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "boost/multi_array.hpp"
-
-#include <iostream>
-#include <map>
-#include <memory>
-using namespace std;
-
-//-----------------------------------------------------------------------------
-//!  The constructor.
-//-----------------------------------------------------------------------------
-
-
-PixelClusterParameterEstimator::ReturnType FastPixelCPE::getParameters(const SiPixelCluster & cluster, const GeomDetUnit &det) const
-{
-  
-  std::map<std::pair<unsigned int, std::pair<int,int> >, std::pair<LocalPoint,LocalError> >::const_iterator pixel_link = 
-    pos_err_map.find(std::make_pair(det.geographicalId().rawId(),std::make_pair(cluster.minPixelRow(),cluster.minPixelCol())));
-  if (pixel_link != pos_err_map.end()) {
-    std::pair<LocalPoint,LocalError> pos_err_info = pixel_link->second;
-    return std::make_tuple(pos_err_info.first, pos_err_info.second, SiPixelRecHitQuality::QualWordType());
-  }
-  throw cms::Exception("FastPixelCPE") << "Cluster not filled.";
-}
-
-void FastPixelCPE::enterLocalParameters(unsigned int id, std::pair<int,int> &row_col, const std::pair<LocalPoint,LocalError>& pos_err_info) {
-  pos_err_map.insert(std::make_pair(std::make_pair(id,row_col), pos_err_info));
-}
--- RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h
+++ RecoLocalTracker/ClusterParameterEstimator/interface/StripClusterParameterEstimator.h
@@ -27,6 +27,7 @@ class StripClusterParameterEstimator
  public:
   typedef std::pair<LocalPoint,LocalError>  LocalValues;
   typedef std::vector<LocalValues> VLocalValues;
+
   virtual LocalValues localParameters( const SiStripCluster&,const GeomDetUnit&) const {
       return std::make_pair(LocalPoint(), LocalError());
   }
@@ -47,65 +48,13 @@ class StripClusterParameterEstimator
     return vlp;
   }
 
-  virtual std::unique_ptr<StripClusterParameterEstimator> clone() const {
-    return std::unique_ptr<StripClusterParameterEstimator>(new StripClusterParameterEstimator(*this));
-  }
+  
+  // used by Validation....
+  virtual LocalVector driftDirection(const StripGeomDetUnit* ) const =0;
 
   virtual ~StripClusterParameterEstimator(){}
   
-  //methods needed by FastSim
-  virtual void enterLocalParameters(unsigned int id, std::pair<int,int>
-				    &row_col, LocalValues pos_err_info) {}
-  virtual void enterLocalParameters(uint32_t id, uint16_t firstStrip,
-				    LocalValues pos_err_info) {}
-  virtual void clearParameters() {}
 
-  typedef std::pair<MeasurementPoint,MeasurementError>  MeasurementValues;
-  virtual LocalVector driftDirection(const StripGeomDetUnit* det)const {
-    return LocalVector();
-  }
-
-  //
-  // methods to get directly the measurements
-  //
-
-  virtual MeasurementValues measurementParameters( const SiStripCluster&,const GeomDetUnit&) const
-  {
-    throw cms::Exception("Not implemented")
-      << "StripClusterParameterEstimator::measurementParameters not yet implemented"<< std::endl;
-  }
-
-  virtual MeasurementValues measurementParameters( const SiStripCluster& cluster,
-                                                   const GeomDetUnit& gd,
-                                                   const LocalTrajectoryParameters & ltp) const
-  {
-    throw cms::Exception("Not implemented") << "StripClusterParameterEstimator::measurementParameters not yet implemented"<<
- std::endl;
-  }
-
-
-  float templateProbability() const
-  {
-    return stripCPEtemplateProbability_;
-  }
-
-  int templateQbin() const
-  {
-    return stripCPEtemplateQbin_;
-  }
-
-  void templateProbability( float stp )
-  {
-    stripCPEtemplateProbability_ = stp;
-  }
-
-  void templateQbin( int stqb )
-  {
-    stripCPEtemplateQbin_ = stqb;
-  }
-
-  mutable float stripCPEtemplateProbability_;
-  mutable int stripCPEtemplateQbin_;
 
 };
 
