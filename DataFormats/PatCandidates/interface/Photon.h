95dc535d20868f16e06939741e6b651339c4d600
Giovanni <gpetruc@gmail.com>
2014-04-16 12:29:48 +0200

    thread-safe relinking of pat photon cluster, as it was done for electrons

--- DataFormats/PatCandidates/interface/Photon.h
+++ DataFormats/PatCandidates/interface/Photon.h
@@ -24,6 +24,7 @@
 #include "DataFormats/PatCandidates/interface/Isolation.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/Common/interface/AtomicPtrCache.h"
 
 
 // Define typedefs for convenience
@@ -241,7 +242,9 @@ namespace pat {
 
       // ---- for content embedding ----
       bool embeddedSuperCluster_;
-      mutable std::vector<reco::SuperCluster> superCluster_;
+      std::vector<reco::SuperCluster> superCluster_;
+      /// Place to temporarily store the electron's supercluster after relinking the seed to it
+      edm::AtomicPtrCache<std::vector<reco::SuperCluster> > superClusterRelinked_;
       /// Place to store electron's basic clusters internally 
       std::vector<reco::CaloCluster> basicClusters_;
       /// Place to store electron's preshower clusters internally      
