bb25622bbcfe2005612193cd2c355f29e5aeaecf
Cesare <cesare.calabria@ba.infn.it>
2014-04-20 04:39:40 +0200

    me0 validation fixes

--- Validation/MuonME0Hits/python/MuonME0Hits_cfi.py
+++ Validation/MuonME0Hits/python/MuonME0Hits_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from Validation.MuonME0Hits.simTrackMatching_cfi import SimTrackMatching
 
 me0HitsValidation = cms.EDAnalyzer("MuonME0Hits",
-                                   debug = cms.untracked.bool(True),
+                                   debug = cms.untracked.bool(False),
                                    folderPath = cms.untracked.string('MuonME0HitsV/ME0HitTask'),
                                    EffSaveRootFile = cms.untracked.bool(False),
                                    EffRootFileName = cms.untracked.string('ME0Hits_ME.root'),
--- Validation/MuonME0RecHits/python/MuonME0RecHits_cfi.py
+++ Validation/MuonME0RecHits/python/MuonME0RecHits_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from Validation.MuonME0RecHits.simTrackMatching_cfi import SimTrackMatching
 
 me0RecHitsValidation = cms.EDAnalyzer("MuonME0RecHits",
-                                   debug = cms.untracked.bool(True),
+                                   debug = cms.untracked.bool(False),
                                    folderPath = cms.untracked.string('MuonME0RecHitsV/ME0RecHitTask'),
                                    EffSaveRootFile = cms.untracked.bool(False),
                                    EffRootFileName = cms.untracked.string('ME0RecHits_ME.root'),
