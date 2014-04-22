c6a9c6f3c5eb6b6d3a39f7b8e99f44b31964a838
Tai Sakuma <tai.sakuma@gmail.com>
2014-04-22 12:07:59 -0500

    change comment lines in SpecificCaloMETData

--- DataFormats/METReco/interface/SpecificCaloMETData.h
+++ DataFormats/METReco/interface/SpecificCaloMETData.h
@@ -1,22 +1,28 @@
+// -*- C++ -*-
+
+// Package:    METReco
+// Class:      SpecificCaloMETData
+//
+/** \class SpecificCaloMETData
+
+    SpecificCaloMETData represents MET made from CaloTowers Provide
+    energy contributions from different subdetectors in addition to
+    generic MET parameters
+
+*/
+//
+// Authors:    R. Cavanaugh, UFL
+//
+
+//____________________________________________________________________________||
 #ifndef METReco_SpecificCaloMETData_h
 #define METReco_SpecificCaloMETData_h
 
-/** \class SpecificCaloMETData
- *
- * \short MET made from CaloTowers
- *
- * SpecificCaloMETData represents MET made from CaloTowers
- * Provide energy contributions from different subdetectors
- * in addition to generic MET parameters
- *
- * \author    R. Cavanaugh, UFL
- *
- ************************************************************/
-
+//____________________________________________________________________________||
 struct SpecificCaloMETData
 {
-  double MaxEtInEmTowers;         // Maximum ET in EM towers
-  double MaxEtInHadTowers;        // Maximum ET in HCAL towers
+  double MaxEtInEmTowers;    // Maximum ET in EM towers
+  double MaxEtInHadTowers;   // Maximum ET in HCAL towers
   double HadEtInHO;          // Hadronic ET fraction in HO
   double HadEtInHB;          // Hadronic ET in HB
   double HadEtInHF;          // Hadronic ET in HF
@@ -26,13 +32,15 @@ struct SpecificCaloMETData
   double EmEtInHF;           // Em ET in HF
   double EtFractionHadronic; // Hadronic ET fraction
   double EtFractionEm;       // Em ET fraction
-  double METSignificance;       // Em ET fraction
-  double CaloMETInpHF;         // CaloMET in HF+ 
-  double CaloMETInmHF;         // CaloMET in HF- 
-  double CaloSETInpHF;         // CaloSET in HF+ 
-  double CaloSETInmHF;         // CaloSET in HF- 
-  double CaloMETPhiInpHF;         // CaloMET-phi in HF+ 
-  double CaloMETPhiInmHF;         // CaloMET-phi in HF- 
+  double METSignificance;    // Em ET fraction
+  double CaloMETInpHF;       // CaloMET in HF+
+  double CaloMETInmHF;       // CaloMET in HF-
+  double CaloSETInpHF;       // CaloSET in HF+
+  double CaloSETInmHF;       // CaloSET in HF-
+  double CaloMETPhiInpHF;    // CaloMET-phi in HF+
+  double CaloMETPhiInmHF;    // CaloMET-phi in HF-
 
-}; //public : struct SpecificCaloMETData
-#endif
+};
+
+//____________________________________________________________________________||
+#endif // METReco_SpecificCaloMETData_h
