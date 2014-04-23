2c16c07b949ebe8f02a2599efaf23404266f8b1a
Martin Grunewald <Martin.Grunewald@cern.ch>
2014-04-23 07:17:16 +0200

    TauConeSize and HLT Script Fixes

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_1_0/HIon/V28 (CMSSW_7_1_0_pre6_HLT1)
+# /dev/CMSSW_7_1_0/HIon/V29 (CMSSW_7_1_0_pre6_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_1_0/HIon/V28')
+  tableName = cms.string('/dev/CMSSW_7_1_0/HIon/V29')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
