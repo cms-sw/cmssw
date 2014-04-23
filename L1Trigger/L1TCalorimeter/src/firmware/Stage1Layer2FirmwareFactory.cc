983ea9d9ea8830f497784c750de82cf542c366d2
R. Alex Barbieri <luck@mit.edu>
2014-04-23 12:42:41 -0400

    remove references to FirmwareVersion condFormat

--- L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2FirmwareFactory.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2FirmwareFactory.cc
@@ -24,16 +24,16 @@ namespace l1t {
 
   Stage1Layer2FirmwareFactory::ReturnType
   // Stage1Layer2FirmwareFactory::create(const FirmwareVersion & m_fwv /*,const CaloParams & dbPars*/){
-  Stage1Layer2FirmwareFactory::create(const FirmwareVersion & m_fwv ,CaloParams* dbPars){
+  Stage1Layer2FirmwareFactory::create(const int m_fwv ,CaloParams* dbPars){
     ReturnType p;
-    unsigned fwv = m_fwv.firmwareVersion();
+    //unsigned fwv = m_fwv.firmwareVersion();
     //unsigned fwv = 1;
 
     // It is up to developers to choose when a new concrete firmware
     // implementation is needed. In this example, Imp1 handles FW
     // versions 1 and 2, while Imp2 handles FW version 3.
 
-    switch (fwv){
+    switch (m_fwv){
     case 1:
       p = ReturnType(new Stage1Layer2MainProcessorFirmwareImp1(m_fwv, dbPars));
       break;
@@ -42,7 +42,7 @@ namespace l1t {
       break;
     default:
       // Invalid Firmware, log an error:
-      LogError("l1t|stage 1 jets") << "Invalid firmware version requested: " << fwv << "\n";
+      LogError("l1t|stage 1 jets") << "Invalid firmware version requested: " << m_fwv << "\n";
       break;
     }
 
