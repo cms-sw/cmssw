eff652a499af17e6403324fb8906d95dc88a29bd
Michael Mulhearn <mulhearn@cern.ch>
2014-04-25 10:00:51 +0200

    rebase l1t consumes migration

--- L1Trigger/DTTrackFinder/plugins/DTTrackFinder.cc
+++ L1Trigger/DTTrackFinder/plugins/DTTrackFinder.cc
@@ -35,7 +35,7 @@ DTTrackFinder::DTTrackFinder(const edm::ParameterSet & pset) {
   produces<L1MuDTTrackContainer>("DTTF");
   produces<vector<L1MuRegionalCand> >("DT");
 
-  setup1 = new L1MuDTTFSetup(pset);
+  setup1 = new L1MuDTTFSetup(pset,consumesCollector());
   usesResource("DTTrackFinder");
 }
 
