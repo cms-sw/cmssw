9bbacdd55be8e08f989e17ec763cf98817a81f59
Andrea Venturi <andrea.venturi@cern.ch>
2014-04-27 20:18:28 +0200

    Added postfix to output files and modified output directory

--- DQM/SiStripMonitorSummary/scripts/makeModulePlots.sh
+++ DQM/SiStripMonitorSummary/scripts/makeModulePlots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-outputroot=/tmp/SiStripModules.root
+outputroot=/tmp/SiStripModules_$9.root
 pedmon=False
 noisemon=False
 if [ $(( $3 & 1 )) -eq 1 ]; then
@@ -11,4 +11,4 @@ if [ $(( $3 & 2 )) -eq 2 ]; then
 fi
 cmsRun $CMSSW_BASE/src/DQM/SiStripMonitorSummary/test/DBReader_conddbmonitoring_singlemodule_cfg.py print logDestination=cout outputRootFile=$outputroot moduleList_load=$2 globalTag=$4 connectionString=$5 tagName=$6 recordName=$7 runNumber=$1 PedestalMon=$pedmon NoiseMon=$noisemon gainNorm=$8 
 
-makeModulePlots $outputroot $2 $3 "/tmp/venturia/"
+makeModulePlots $outputroot $2 $3 "/tmp/" $9
