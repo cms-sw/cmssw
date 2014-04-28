a4a67a91864ca8851e7167f2e8eb9703f2e973ab
David <David.Lange@cern.ch>
2014-04-28 12:50:56 +0200

    cmsdriver changes for pat

--- Configuration/Applications/python/cmsDriverOptions.py
+++ Configuration/Applications/python/cmsDriverOptions.py
@@ -97,7 +97,8 @@ def OptionsFromItems(items):
                  "DATAMIX":"DIGI",
                  "DIGI2RAW":"DATAMIX",
                  "HARVESTING":"RECO",
-                 "ALCAHARVEST":"RECO"}
+                 "ALCAHARVEST":"RECO",
+                 "PAT":"RECO"}
 
     trimmedEvtType=options.evt_type.split('/')[-1]
 
