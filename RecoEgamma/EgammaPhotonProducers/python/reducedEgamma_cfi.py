6ec7aa04731c2eafd2c5090145d3469d2d2212de
Josh Bendavid <Josh.Bendavid@cern.ch>
2014-04-29 16:38:52 +0200

    add additional flexibility in relinking and restore previous mini aod behaviour of keeping supercluster and seed cluster for all output electrons and photons

--- RecoEgamma/EgammaPhotonProducers/python/reducedEgamma_cfi.py
+++ RecoEgamma/EgammaPhotonProducers/python/reducedEgamma_cfi.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
 reducedEgamma = cms.EDProducer("ReducedEGProducer",
-  keepPhotons = cms.string("pt > 14 && hadTowOverEm()<0.15"),
-  relinkPhotons = cms.string("(r9()>0.8 || chargedHadronIso()<20 || chargedHadronIso()<0.3*pt())"),
-  keepGsfElectrons = cms.string(""),
-  relinkGsfElectrons = cms.string("pt>5"),
+  keepPhotons = cms.string("pt > 14 && hadTowOverEm()<0.15"), #keep in output
+  slimRelinkPhotons = cms.string("pt > 14 && hadTowOverEm()<0.15"), #keep only slimmed SuperCluster plus seed cluster
+  relinkPhotons = cms.string("(r9()>0.8 || chargedHadronIso()<20 || chargedHadronIso()<0.3*pt())"), #keep all associated clusters/rechits/conversions
+  keepGsfElectrons = cms.string(""), #keep in output
+  slimRelinkGsfElectrons = cms.string(""), #keep only slimmed SuperCluster plus seed cluster
+  relinkGsfElectrons = cms.string("pt>5"), #keep all associated clusters/rechits/conversions
   photons = cms.InputTag("gedPhotons"),
   gsfElectrons = cms.InputTag("gedGsfElectrons"),
   conversions = cms.InputTag("allConversions"),
