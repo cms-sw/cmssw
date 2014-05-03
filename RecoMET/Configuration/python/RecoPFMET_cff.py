ca9a91510564bfe14c403d7791033edf64324b79
Tai Sakuma <tai.sakuma@gmail.com>
2014-05-03 17:39:36 -0500

    add pfChMet to cms.Sequence recoPFMET

--- RecoMET/Configuration/python/RecoPFMET_cff.py
+++ RecoMET/Configuration/python/RecoPFMET_cff.py
@@ -2,8 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 ##____________________________________________________________________________||
 from RecoMET.METProducers.PFMET_cfi import *
+from RecoMET.METProducers.pfChMet_cfi import *
 
 ##____________________________________________________________________________||
-recoPFMET = cms.Sequence( pfMet )
+recoPFMET = cms.Sequence(pfMet + particleFlowForChargedMET + pfChMet)
 
 ##____________________________________________________________________________||
