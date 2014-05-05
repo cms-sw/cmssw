c213007a6630d969c916c6af52319cd5dc56c3b9
Giovanni <gpetruc@gmail.com>
2014-05-05 19:11:32 +0200

    MiniAOD in cmsDriver for real (calling miniAOD_tools) Depends on #3545 (queued) and #3543 (already merged) Tested: 1) cmsDriver.py miniAOD_data-prod -s PAT -n 100 --data --filein /store/relval/CMSSW_7_1_0_pre6/JetHT/RECO/PRE_R_71_V2_RelVal_jet2012C-v1/00000/2CBD40F5-E2C7-E311-8206-003048678AC0.root --eventcontent MINIAOD --runUnscheduled --conditions GR_R_70_V1::All --no_exec 2) cmsDriver.py miniAOD-prod -s PAT -n 100 --mc --filein /store/relval/CMSSW_7_1_0_pre6/RelValTTbar_13/GEN-SIM-RECO/PRE_LS171_V5-v1/00000/064AD022-51C7-E311-9714-003048679228.root --eventcontent MINIAODSIM --runUnscheduled --conditions auto:startup --no_exec

--- PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
+++ PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
@@ -87,7 +87,7 @@ def miniAOD_customizeCommon(process):
     process.patJets.addTagInfos = cms.bool(True)
     #
     from PhysicsTools.PatAlgos.tools.trigTools import switchOnTriggerStandAlone
-    switchOnTriggerStandAlone( process )
+    switchOnTriggerStandAlone( process, outputModule = '' )
     process.patTrigger.packTriggerPathNames = cms.bool(True)
     #
     # apply type I/type I + II PFMEt corrections to pat::MET object
@@ -120,5 +120,14 @@ def miniAOD_customizeOutput(out):
 
 def miniAOD_customizeData(process):
     from PhysicsTools.PatAlgos.tools.coreTools import runOnData
-    runOnData( process )
+    runOnData( process, outputModules = [] )
 
+def miniAOD_customizeAllData(process):
+    miniAOD_customizeCommon(process)
+    miniAOD_customizeData(process)
+    return process
+
+def miniAOD_customizeAllMC(process):
+    miniAOD_customizeCommon(process)
+    miniAOD_customizeMC(process)
+    return process
