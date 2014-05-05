7f6a54f8987b3fcead0bec56dc05882ada1b01f0
Sven Dildick <sven.dildick@cern.ch>
2014-05-05 09:53:56 -0500

    already defined in ME11 customizations

--- SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
+++ SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
@@ -53,14 +53,12 @@ def customise_L1Emulator2019(process, ptdphi):
 def customise_L1Emulator2023(process, ptdphi):
     from L1Trigger.CSCTriggerPrimitives.cscTriggerPrimitiveDigisPostLS3_cfi import cscTriggerPrimitiveDigisPostLS3
     process.simCscTriggerPrimitiveDigis = cscTriggerPrimitiveDigisPostLS3
-
     ## ME21 has its own SLHC processors
     process.simCscTriggerPrimitiveDigis.alctSLHCME21 = process.simCscTriggerPrimitiveDigis.alctSLHC.clone()
     process.simCscTriggerPrimitiveDigis.clctSLHCME21 = process.simCscTriggerPrimitiveDigis.clctSLHC.clone()
     process.simCscTriggerPrimitiveDigis.alctSLHCME21.alctNplanesHitPattern = 3
     process.simCscTriggerPrimitiveDigis.clctSLHCME21.clctNplanesHitPattern = 3
     process.simCscTriggerPrimitiveDigis.clctSLHCME21.clctPidThreshPretrig = 2
-    process.simCscTriggerPrimitiveDigis.clctParam07.clctPidThreshPretrig = 2
     return process
 
 def customise_DigiToRaw(process):
