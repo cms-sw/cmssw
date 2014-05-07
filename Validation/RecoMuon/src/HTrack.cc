65d5f4e30b4ddce49a7e2dfa4c7b591bd4d7b83d
rocio vilar <rocio.vilar.cortabitarte@cern.ch>
2014-05-07 21:08:09 +0200

    multithreaded DQM

--- Validation/RecoMuon/src/HTrack.cc
+++ Validation/RecoMuon/src/HTrack.cc
@@ -4,7 +4,7 @@
 #include "TrackingTools/TrajectoryState/interface/FreeTrajectoryState.h"
 #include "SimDataFormats/Track/interface/SimTrack.h"
 
-#include "DQMServices/Core/interface/DQMStore.h"
+//#include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
