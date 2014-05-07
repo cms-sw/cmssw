873143025123cc3f9619d55bd6d49c4a101d4564
Patrick Gartung <gartung@fnal.gov>
2014-05-07 16:15:39 -0500

    Remove the filter to only check data classes

--- Utilities/StaticAnalyzers/src/MutableMemberChecker.cpp
+++ Utilities/StaticAnalyzers/src/MutableMemberChecker.cpp
@@ -26,7 +26,6 @@ void MutableMemberChecker::checkASTDecl(const clang::FieldDecl *D,
 	    if ( ! m_exception.reportMutableMember( t, DLoc, BR ) )
 		return;
 	    if ( support::isSafeClassName( t.getAsString() ) ) return;
-	    if ( ! support::isDataClass( D->getParent()->getQualifiedNameAsString() ) ) return;
 	    std::string buf;
 	    llvm::raw_string_ostream os(buf);
 	    os << "Mutable member'" <<t.getAsString()<<" "<<*D << "' in class '"<<D->getParent()->getQualifiedNameAsString()<<"', might be thread-unsafe when accessing via a const handle.";
