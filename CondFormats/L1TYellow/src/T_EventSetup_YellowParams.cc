7312640bc035cb223211a999286272f3df70fef0
Michael Mulhearn <mulhearn@cern.ch>
2014-05-07 04:20:52 +0200

    remove demo code L1TYellow

--- CondFormats/L1TYellow/src/T_EventSetup_YellowParams.cc
+++
@@ -1,4 +0,0 @@
-#include "CondFormats/L1TYellow/interface/YellowParams.h"
-#include "FWCore/Utilities/interface/typelookup.h"
-
-TYPELOOKUP_DATA_REG(l1t::YellowParams);
--- CondFormats/L1TYellow/src/classes.h
+++
@@ -1,9 +0,0 @@
-#include "CondFormats/L1TYellow/interface/YellowParams.h"
-//#include "CondFormats/DataRecord/L1TYellowParamsRcd.h"
-
-namespace {
-  struct dictionary {
-    //l1t::YellowParams dummy;
-    //L1TYellowParamsRcd dummyb;
-  };
-}
