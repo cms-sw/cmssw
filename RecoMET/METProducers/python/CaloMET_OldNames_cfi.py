6c5549e196b7ce9128c02671240946d894f91457
Tai Sakuma <tai.sakuma@gmail.com>
2014-05-10 10:35:47 -0500

    move the definitions of CaloMETs from CaloMET_cfi.py to CaloMET_OldNames_cfi.py

---
+++ RecoMET/METProducers/python/CaloMET_OldNames_cfi.py
@@ -0,0 +1,64 @@
+import FWCore.ParameterSet.Config as cms
+
+##____________________________________________________________________________||
+met = cms.EDProducer(
+    "CaloMETProducer",
+    src = cms.InputTag("towerMaker"),
+    alias = cms.string('RawCaloMET'),
+    noHF = cms.bool(False),
+    globalThreshold = cms.double(0.3),
+    calculateSignificance = cms.bool(False)
+    )
+
+##____________________________________________________________________________||
+metHO = met.clone()
+metHO.src = "towerMakerWithHO"
+metHO.alias = 'RawCaloMETHO'
+
+##____________________________________________________________________________||
+metNoHF = cms.EDProducer(
+    "CaloMETProducer",
+    src = cms.InputTag("towerMaker"),
+    alias = cms.string('RawCaloMETNoHF'),
+    noHF = cms.bool(True),
+    globalThreshold = cms.double(0.3),
+    calculateSignificance = cms.bool(False)
+)
+
+##____________________________________________________________________________||
+metNoHFHO = metNoHF.clone()
+metNoHFHO.src = "towerMakerWithHO"
+metNoHFHO.alias = 'RawCaloMETNoHFHO'
+
+##____________________________________________________________________________||
+metOpt = cms.EDProducer(
+    "CaloMETProducer",
+    src = cms.InputTag("calotoweroptmaker"),
+    alias = cms.string('RawCaloMETOpt'),
+    noHF = cms.bool(False),
+    globalThreshold = cms.double(0.0),
+    calculateSignificance = cms.bool(False)
+    )
+
+##____________________________________________________________________________||
+metOptHO = metOpt.clone()
+metOptHO.src = "calotoweroptmakerWithHO"
+metOptHO.alias = 'RawCaloMETOptHO'
+
+##____________________________________________________________________________||
+metOptNoHF = cms.EDProducer(
+    "CaloMETProducer",
+    src = cms.InputTag("calotoweroptmaker"),
+    alias = cms.string('RawCaloMETOptNoHF'),
+    noHF = cms.bool(True),
+    globalThreshold = cms.double(0.0),
+    calculateSignificance = cms.bool(False)
+    )
+
+##____________________________________________________________________________||
+metOptNoHFHO = metOptNoHF.clone()
+metOptNoHFHO.src = "calotoweroptmakerWithHO"
+metOptNoHFHO.alias = 'RawCaloMETOptNoHFHO'
+
+##____________________________________________________________________________||
+
