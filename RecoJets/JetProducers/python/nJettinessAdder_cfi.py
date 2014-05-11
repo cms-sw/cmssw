1fe24f176b49bd0e011959d2986f13c289e73430
John <jstupak@fnal.gov>
2014-05-11 23:20:36 +0200

    more AK5 -> AK4

--- RecoJets/JetProducers/python/nJettinessAdder_cfi.py
+++ RecoJets/JetProducers/python/nJettinessAdder_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 Njettiness = cms.EDProducer("NjettinessAdder",
-                            src=cms.InputTag("ak5PFJetsCHS"),
-                            cone=cms.double(0.5)
+                            src=cms.InputTag("ca8PFJetsCHS"),
+                            cone=cms.double(0.8)
                             )
