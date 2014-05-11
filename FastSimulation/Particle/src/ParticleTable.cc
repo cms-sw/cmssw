326e071b869c1af408327f70f303eec8075e6bda
lgray <lindsey.gray@gmail.com>
2014-05-11 13:59:21 -0500

    revert rechit producers to old style producers because of HcalChannelQuality

--- FastSimulation/Particle/src/ParticleTable.cc
+++ FastSimulation/Particle/src/ParticleTable.cc
@@ -1,6 +1,6 @@
 #include "FastSimulation/Particle/interface/ParticleTable.h"
 
-std::atomic<ParticleTable*> ParticleTable::myself; 
+std::atomic<ParticleTable*> ParticleTable::myself(NULL); 
 
 std::mutex ParticleTable::_mutex;
 
@@ -11,9 +11,9 @@ ParticleTable::instance(const HepPDT::ParticleDataTable* pdt) {
   if( tmp == NULL ) {
     std::lock_guard<std::mutex> lock(_mutex);
     tmp = myself.load(std::memory_order_relaxed);
-    if( tmp == NULL || pdt ) { // load a new singleton if given data table
+    if( tmp == NULL && pdt ) { 
+      // && means we load the singleton the first time we get non-null pdt
       // since we have acquired a lock here this is thread safe
-      if( tmp ) delete tmp;
       tmp = new ParticleTable(pdt);
       std::atomic_thread_fence(std::memory_order_release);
       myself.store(tmp);
