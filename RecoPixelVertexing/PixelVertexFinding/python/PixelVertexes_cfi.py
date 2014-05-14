b33d345c8640123a518d9fa4d86fcba9fb79dcc9
Mia <mia.tosi@cern.ch>
2014-05-14 23:17:42 +0200

    removed commented lines

--- RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cfi.py
+++ RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cfi.py
@@ -17,12 +17,6 @@ pixelVertices = cms.EDProducer("PixelVertexProducer",
     PVcomparer = cms.PSet(
        refToPSet_ = cms.string('pvClusterComparer')
     )
-#  PVcomparer = cms.PSet(
-#      track_pt_min   = cms.double(     1.0),
-#      track_pt_max   = cms.double(    10.0), # SD: 20.
-#      track_chi2_max = cms.double(999999. ), # SD: 20
-#      track_prob_min = cms.double(    -1. ), # RM: 0.001
-#   )
 )
 
 
