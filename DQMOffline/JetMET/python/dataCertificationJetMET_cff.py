3b534f4a536f7217a6563a94574df6bb692795e9
federico de guio <federico.de.guio@cern.ch>
2014-05-14 16:06:18 +0200

    fix use of DQMStore in DQMEDAnalyzer

--- DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
+++ DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
@@ -1,9 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMOffline.JetMET.jetMETDQMOfflineClient_cfi import *
 from DQMOffline.JetMET.dataCertificationJetMET_cfi import *
 
-dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET + dataCertificationJetMET)
-
-#dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET + dataCertificationJetMET)
-#dataCertificationJetMETSequence = cms.Sequence(jetMETDQMOfflineClient + qTesterJet + qTesterMET )
+dataCertificationJetMETSequence = cms.Sequence(qTesterJet + qTesterMET + dataCertificationJetMET)
