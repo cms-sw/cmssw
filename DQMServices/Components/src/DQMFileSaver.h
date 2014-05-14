cf48400997ba6dc102e331ccfece729e8e130385
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2014-05-14 13:17:17 +0200

    Make Convention and FileFormat enums public (in order to be used by static functions in the source file).

--- DQMServices/Components/src/DQMFileSaver.h
+++ DQMServices/Components/src/DQMFileSaver.h
@@ -22,17 +22,7 @@ protected:
   virtual void endJob(void);
   virtual void postForkReacquireResources(unsigned int childIndex, unsigned int numberOfChildren);
 
-private:
-  void saveForOfflinePB(const std::string &workflow, int run);
-  void saveForOffline(const std::string &workflow, int run, int lumi);
-  void saveForOnlinePB(const std::string &suffix);
-  void saveForOnline(const std::string &suffix, const std::string &rewrite);
-  void saveForFilterUnitPB(int run, int lumi);
-  void saveForFilterUnit(const std::string& rewrite, int run, int lumi);
-  void saveJobReport(const std::string &filename);
-
-  void saveJson(int run, int lumi, const std::string& fn, const std::string& data_fn);
-
+public:
   enum Convention
   {
     Online,
@@ -45,6 +35,16 @@ private:
     ROOT,
     PB
   };
+
+private:
+  void saveForOfflinePB(const std::string &workflow, int run);
+  void saveForOffline(const std::string &workflow, int run, int lumi);
+  void saveForOnlinePB(const std::string &suffix);
+  void saveForOnline(const std::string &suffix, const std::string &rewrite);
+  void saveForFilterUnitPB(int run, int lumi);
+  void saveForFilterUnit(const std::string& rewrite, int run, int lumi);
+  void saveJobReport(const std::string &filename);
+  void saveJson(int run, int lumi, const std::string& fn, const std::string& data_fn);
 
   Convention	convention_;
   FileFormat    fileFormat_;
