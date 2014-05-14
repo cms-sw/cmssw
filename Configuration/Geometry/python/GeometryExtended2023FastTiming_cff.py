52d76f6ec14d166626caf297b96546c7b6eb4052
Sunanda <sunanda.banerjee@cern.ch>
2014-05-14 17:56:07 +0200

    Complete the scenario with Fast Time after Yanas changes

--- Configuration/Geometry/python/GeometryExtended2023FastTiming_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023FastTiming_cff.py
@@ -6,3 +6,5 @@ import FWCore.ParameterSet.Config as cms
 from Geometry.CMSCommonData.cmsExtendedGeometry2023FastTimingXML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingSLHCGeometry_cfi import *
 from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
+from Geometry.HGCalCommonData.fastTimeNumberingInitialization_cfi import *
+
