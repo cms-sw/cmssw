d2543fec4254dfe91006a262663d14265a4a707b
Mia <mia.tosi@cern.ch>
2014-05-14 16:03:53 +0200

    add also track_pt_min parameter (consistency check added, in case PV producer and PV comparer have different threshold a LogWarning is raise

--- RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cfi.py
+++ RecoPixelVertexing/PixelVertexFinding/python/PixelVertexes_cfi.py
@@ -13,10 +13,14 @@ pixelVertices = cms.EDProducer("PixelVertexProducer",
     Finder = cms.string('DivisiveVertexFinder'),
     PtMin = cms.double(1.0),
     PVcomparer = cms.PSet(
-        track_pt_max   = cms.double(    10.0), # SD: 20.
-        track_chi2_max = cms.double(999999. ), # SD: 20
-        track_prob_min = cms.double(    -1. ), # RM: 0.001
+       refToPSet_ = cms.string('pvClusterComparer')
     )
+#  PVcomparer = cms.PSet(
+#      track_pt_min   = cms.double(     1.0),
+#      track_pt_max   = cms.double(    10.0), # SD: 20.
+#      track_chi2_max = cms.double(999999. ), # SD: 20
+#      track_prob_min = cms.double(    -1. ), # RM: 0.001
+#   )
 )
 
 
