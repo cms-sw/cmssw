152cf9533c2d5fba7353d275debf852d62ad9bbe
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2014-05-14 11:08:23 +0200

    removing jetCorrector

--- DQMOffline/Trigger/python/topSingleLeptonHLTEventDQM_cfi.py
+++ DQMOffline/Trigger/python/topSingleLeptonHLTEventDQM_cfi.py
@@ -54,7 +54,7 @@ topSingleLeptonHLTOfflineDQM = cms.EDAnalyzer("TopSingleLeptonHLTOfflineDQM",
     jetExtras = cms.PSet(
       ## when omitted monitor plots for pt will be filled from uncorrected
       ## jets                                            
-      jetCorrector = cms.string("ak4PFCHSL2L3"),
+      #jetCorrector = cms.string("ak4PFCHSL2L3"),
       ## when omitted no extra selection will be applied on jets before
       ## filling the monitor histograms; if jetCorrector is present the
       ## selection will be applied to corrected jets
@@ -163,7 +163,7 @@ topSingleMuonHLTOfflineDQM = cms.EDAnalyzer("TopSingleLeptonHLTOfflineDQM",
     jetExtras = cms.PSet(
       ## when omitted monitor plots for pt will be filled from uncorrected
       ## jets
-      jetCorrector = cms.string("ak4PFCHSL2L3"),
+      #jetCorrector = cms.string("ak4PFCHSL2L3"),
       ## when omitted no extra selection will be applied on jets before
       ## filling the monitor histograms; if jetCorrector is present the
       ## selection will be applied to corrected jets                                                
@@ -263,7 +263,7 @@ topSingleMuonHLTOfflineDQM = cms.EDAnalyzer("TopSingleLeptonHLTOfflineDQM",
 #      label  = cms.string("jets/pf:step5"),
       label  = cms.string("jets/pf:step2"),
       src  = cms.InputTag("ak4PFJetsCHS"),
-      jetCorrector = cms.string("ak4PFCHSL2L3"),
+      #jetCorrector = cms.string("ak4PFCHSL2L3"),
       select = cms.string("pt>20 & abs(eta)<2.5"),
       min = cms.int32(4),                                                
     ),
@@ -315,7 +315,7 @@ topSingleElectronHLTOfflineDQM = cms.EDAnalyzer("TopSingleLeptonHLTOfflineDQM",
     jetExtras = cms.PSet(
       ## when omitted monitor plots for pt will be filled from uncorrected
       ## jets
-      jetCorrector = cms.string("ak4PFCHSL2L3"),
+      #jetCorrector = cms.string("ak4PFCHSL2L3"),
       ## when omitted no extra selection will be applied on jets before
       ## filling the monitor histograms; if jetCorrector is present the
       ## selection will be applied to corrected jets 
@@ -416,7 +416,7 @@ topSingleElectronHLTOfflineDQM = cms.EDAnalyzer("TopSingleLeptonHLTOfflineDQM",
 #      label  = cms.string("jets/pf:step5"),
       label  = cms.string("jets/pf:step2"),
       src  = cms.InputTag("ak4PFJetsCHS"),
-      jetCorrector = cms.string("ak4PFCHSL2L3"),
+      #jetCorrector = cms.string("ak4PFCHSL2L3"),
       select = cms.string("pt>20 & abs(eta)<2.5"),
       min = cms.int32(4),                                                
     ),
