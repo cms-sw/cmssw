bbb0f777d1df1fb030fd6b54454eda6b455cc931
Sven Dildick <sven.dildick@cern.ch>
2014-05-15 15:31:19 +0200

    update geometry customs

--- Geometry/GEMGeometry/python/gemGeometryCustoms.py
+++ Geometry/GEMGeometry/python/gemGeometryCustoms.py
@@ -19,15 +19,20 @@ def custom_GE11_9and10partitions_v1(process):
     ## This is the default version
     return process
 
-def custom_GE11_9and10partitions_v2(process):
-    ## This is still in debug phase - use with caution!!!
+def custom_GE11_10partitions_v1(process):
+    mynum = process.XMLIdealGeometryESSource.geomXMLFiles.index('Geometry/MuonCommonData/data/v4/gem11.xml')
+    process.XMLIdealGeometryESSource.geomXMLFiles.remove('Geometry/MuonCommonData/data/v4/gem11.xml')
+    process.XMLIdealGeometryESSource.geomXMLFiles.insert(mynum,'Geometry/MuonCommonData/data/v3/gem11.xml')
+    return process
+
+def custom_GE11_8and8partitions_v1(process):
     mynum = process.XMLIdealGeometryESSource.geomXMLFiles.index('Geometry/MuonCommonData/data/v4/gem11.xml')
     process.XMLIdealGeometryESSource.geomXMLFiles.remove('Geometry/MuonCommonData/data/v4/gem11.xml')
     process.XMLIdealGeometryESSource.geomXMLFiles.insert(mynum,'Geometry/MuonCommonData/data/v6/gem11.xml')
     return process
 
-def custom_GE11_10partitions_v1(process):
+def custom_GE11_8and8partitions_v2(process):
     mynum = process.XMLIdealGeometryESSource.geomXMLFiles.index('Geometry/MuonCommonData/data/v4/gem11.xml')
     process.XMLIdealGeometryESSource.geomXMLFiles.remove('Geometry/MuonCommonData/data/v4/gem11.xml')
-    process.XMLIdealGeometryESSource.geomXMLFiles.insert(mynum,'Geometry/MuonCommonData/data/v3/gem11.xml')
+    process.XMLIdealGeometryESSource.geomXMLFiles.insert(mynum,'Geometry/MuonCommonData/data/v7/gem11.xml')
     return process
