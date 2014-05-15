a51336e088fc216c18cf96a617630ea045f0f2fe
federico de guio <federico.de.guio@cern.ch>
2014-05-15 15:31:46 +0200

    make the ibooker available in the endLuminosityBlock

--- DQMServices/Core/interface/DQMEDHarvester.h
+++ DQMServices/Core/interface/DQMEDHarvester.h
@@ -24,7 +24,7 @@ public:
   virtual void endRun(edm::Run const&, edm::EventSetup const&) {};
   virtual void endLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const&) final;
   virtual void endJob() final;
-  virtual void dqmEndLuminosityBlock(DQMStore::IGetter &, edm::LuminosityBlock const &, edm::EventSetup const&) {};
+  virtual void dqmEndLuminosityBlock(DQMStore::IBooker &, DQMStore::IGetter &, edm::LuminosityBlock const &, edm::EventSetup const&) {};
   virtual void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) = 0;
 
 private:
