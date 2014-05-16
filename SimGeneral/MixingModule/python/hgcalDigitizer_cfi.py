bf179208b3c85566270bb87fce05019f5b631750
Pedro Silva <psilva@cern.ch>
2014-05-16 15:45:06 +0200

    Adding realistic thresholds for LSB and setting noise to 0 at this point Adding gain bit to HGCSample and giving 15 bit to ADC Adding more functions to set/get specific bitsets in an sample

--- SimGeneral/MixingModule/python/hgcalDigitizer_cfi.py
+++ SimGeneral/MixingModule/python/hgcalDigitizer_cfi.py
@@ -7,19 +7,20 @@ hgceeDigitizer = cms.PSet( accumulatorType   = cms.string("HGCDigiProducer"),
                            bxTime            = cms.untracked.int32(25),
                            doTrivialDigis    = cms.untracked.bool(True),
                            makeDigiSimLinks  = cms.untracked.bool(False),
-                           digiCfg = cms.untracked.PSet( lsbInMeV   = cms.untracked.double(10),
-                                                         noiseInMeV = cms.untracked.double(10)
+                           digiCfg = cms.untracked.PSet( lsbInMeV   = cms.untracked.double(12.0),
+                                                         noiseInMeV = cms.untracked.double(0)
                                                          )
                            )
 
 hgchefrontDigitizer = hgceeDigitizer.clone()
 hgchefrontDigitizer.hitCollection  = cms.untracked.string("HGCHitsHEfront")
 hgchefrontDigitizer.digiCollection = cms.untracked.string("HGCDigisHEfront")
+hgchefrontDigitizer.digiCfg.lsbInMev = cms.untracked.double(17.6)
 
 hgchebackDigitizer = hgceeDigitizer.clone()
 hgchebackDigitizer.hitCollection = cms.untracked.string("HGCHitsHEback")
 hgchebackDigitizer.digiCollection = cms.untracked.string("HGCDigisHEback")
-                   
+hgchebackDigitizer.digiCfg.lsbInMev = cms.untracked.double(310.8)
 
 
 
