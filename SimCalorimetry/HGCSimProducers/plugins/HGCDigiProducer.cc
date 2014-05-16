0e98d312d92d3e5478dbb3e13bf05e8554218c5f
Pedro Silva <psilva@cern.ch>
2014-05-16 15:16:28 +0200

    Moving from "Sim" DetId to final DetId including sub-detector information

--- SimCalorimetry/HGCSimProducers/plugins/HGCDigiProducer.cc
+++ SimCalorimetry/HGCSimProducers/plugins/HGCDigiProducer.cc
@@ -3,14 +3,16 @@
 #include "SimCalorimetry/HGCSimProducers/plugins/HGCDigiProducer.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 
+//
 HGCDigiProducer::HGCDigiProducer(edm::ParameterSet const& pset, edm::EDProducer& mixMod) :
   DigiAccumulatorMixMod(),
-  theDigitizer_(pset) {
+  theDigitizer_(pset), 
+  numberingScheme_(0) 
+{
   if( theDigitizer_.producesEEDigis()     )
     mixMod.produces<HGCEEDigiCollection>(theDigitizer_.digiCollection());
   if( theDigitizer_.producesHEfrontDigis() || theDigitizer_.producesHEbackDigis() )
     mixMod.produces<HGCHEDigiCollection>(theDigitizer_.digiCollection());
-  
 }
 
 //
