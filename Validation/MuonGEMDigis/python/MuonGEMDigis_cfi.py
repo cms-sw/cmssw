e7efefebf60d9944659b71a9c755f3f7e20fcc28
Geonmo <ry840901@gmail.com>
2014-05-22 17:42:16 +0900

    Change Range of ZR plots.

--- Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
+++ Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
@@ -10,8 +10,8 @@ gemDigiValidation = cms.EDAnalyzer('MuonGEMDigis',
 	maxEta = cms.untracked.double(2.45),
 	minEta = cms.untracked.double(1.55), 
 	PlotBinInfo = cms.PSet(
-			nBinGlobalZR = cms.untracked.vdouble(200,200,200,110,140,210), 
-			RangeGlobalZR = cms.untracked.vdouble(564,572,786,794,794,802,130,240,190,330,120,330), 
+			nBinGlobalZR = cms.untracked.vdouble(200,200,200,150,180,250), 
+			RangeGlobalZR = cms.untracked.vdouble(564,572,786,794,794,802,110,260,170,350,100,350), 
   ),
 	simTrackMatching = cms.PSet(
             verboseSimHit = cms.untracked.int32(0),
