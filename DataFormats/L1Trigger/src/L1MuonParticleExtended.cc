b90889fc5d01c2d2197a7ec82e600cae6f348474
Slava Krutelyov <slava77@cern.ch>
2014-05-23 14:42:40 -0500

    L1MuonParticleExtended to add more handles to details of standalone L1 muon inputs

---
+++ DataFormats/L1Trigger/src/L1MuonParticleExtended.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     L1Trigger
+// Class  :     L1MuonParticleExtended
+// 
+//
+
+// system include files
+
+// user include files
+#include "DataFormats/L1Trigger/interface/L1MuonParticleExtended.h"
+
+using namespace l1extra ;
+
+
