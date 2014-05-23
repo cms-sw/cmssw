8aa23f18f1a723f60c687b7de1856c2ba1d42f41
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2014-05-23 15:57:39 +0200

    move default to use new production db

--- Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
+++ Configuration/StandardSequences/python/FrontierConditions_GlobalTag_cfi.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from CondCore.DBCommon.CondDBSetup_cfi import *
 
-# gtDbName = 'oracle://cms_orcoff_prep/CMS_CONDITIONS'
-gtDbName = 'frontier://FrontierPrep/CMS_CONDITIONS'
+gtDbName = 'frontier://FrontierProd/CMS_CONDITIONS'
 print "CondDB set up to read the Global Tag from : ", gtDbName
 
 GlobalTag = cms.ESSource( "PoolDBESSource",
