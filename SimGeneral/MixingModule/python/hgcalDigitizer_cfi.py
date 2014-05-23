156db6a99c2e1009e369d49dc2843ae2f2a779e6
Pedro Silva <psilva@cern.ch>
2014-05-23 12:26:34 +0200

    fixing names

--- SimGeneral/MixingModule/python/hgcalDigitizer_cfi.py
+++ SimGeneral/MixingModule/python/hgcalDigitizer_cfi.py
@@ -7,20 +7,22 @@ hgceeDigitizer = cms.PSet( accumulatorType   = cms.string("HGCDigiProducer"),
                            bxTime            = cms.untracked.int32(25),
                            doTrivialDigis    = cms.untracked.bool(True),
                            makeDigiSimLinks  = cms.untracked.bool(False),
-                           digiCfg = cms.untracked.PSet( lsbInMeV   = cms.untracked.double(12.0),
-                                                         noiseInMeV = cms.untracked.double(0)
+                           digiCfg = cms.untracked.PSet( lsbInKeV   = cms.untracked.double(12.0),
+                                                         noiseInKeV = cms.untracked.double(12.0)
                                                          )
                            )
 
 hgchefrontDigitizer = hgceeDigitizer.clone()
 hgchefrontDigitizer.hitCollection  = cms.untracked.string("HGCHitsHEfront")
 hgchefrontDigitizer.digiCollection = cms.untracked.string("HGCDigisHEfront")
-hgchefrontDigitizer.digiCfg.lsbInMev = cms.untracked.double(17.6)
+hgchefrontDigitizer.digiCfg.lsbInKeV = cms.untracked.double(17.6)
+hgchefrontDigitizer.digiCfg.noiseInKeV = cms.untracked.double(17.6)
 
 hgchebackDigitizer = hgceeDigitizer.clone()
 hgchebackDigitizer.hitCollection = cms.untracked.string("HGCHitsHEback")
 hgchebackDigitizer.digiCollection = cms.untracked.string("HGCDigisHEback")
-hgchebackDigitizer.digiCfg.lsbInMev = cms.untracked.double(310.8)
+hgchebackDigitizer.digiCfg.lsbInKeV = cms.untracked.double(310.8)
+hgchebackDigitizer.digiCfg.noiseInKeV = cms.untracked.double(310.8)
 
 
 
