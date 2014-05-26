ed50211d7015fa4ed0395ab70397e902268b9ef0
Tai Sakuma <tai.sakuma@gmail.com>
2014-05-27 00:52:12 +0200

    define corrCaloMetType1 in correctionTermsCaloMet_cff instead of cloning caloJetMETcorr

--- JetMETCorrections/Type1MET/python/correctionTermsCaloMet_cff.py
+++ JetMETCorrections/Type1MET/python/correctionTermsCaloMet_cff.py
@@ -4,7 +4,16 @@ import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Type1MET.caloMETCorrections_cff import *
 
 ##____________________________________________________________________________||
-corrCaloMetType1 = caloJetMETcorr.clone()
+corrCaloMetType1 = cms.EDProducer(
+    "CaloJetMETcorrInputProducer",
+    src = cms.InputTag('ak4CaloJets'),
+    jetCorrLabel = cms.string("ak4CaloL2L3"), # NOTE: use "ak4CaloL2L3" for MC / "ak4CaloL2L3Residual" for Data
+    jetCorrEtaMax = cms.double(9.9),
+    type1JetPtThreshold = cms.double(20.0),
+    skipEM = cms.bool(True),
+    skipEMfractionThreshold = cms.double(0.90),
+    srcMET = cms.InputTag('corMetGlobalMuons')
+)
 
 ##____________________________________________________________________________||
 corrCaloMetType2 = cms.EDProducer(
