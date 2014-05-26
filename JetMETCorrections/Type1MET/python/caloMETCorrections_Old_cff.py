4a771616e746b0a4978b4f8cc9866a3e830d53b5
Tai Sakuma <tai.sakuma@gmail.com>
2014-05-27 01:04:07 +0200

    move aloJetMETcorr and muonCaloMETcorr from caloMETCorrections_cff to caloMETCorrections_Old_cff

--- JetMETCorrections/Type1MET/python/caloMETCorrections_Old_cff.py
+++ JetMETCorrections/Type1MET/python/caloMETCorrections_Old_cff.py
@@ -1,3 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+##____________________________________________________________________________||
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
+
+##____________________________________________________________________________||
+caloJetMETcorr = cms.EDProducer("CaloJetMETcorrInputProducer",
+    src = cms.InputTag('ak4CaloJets'),
+    jetCorrLabel = cms.string("ak4CaloL2L3"), # NOTE: use "ak4CaloL2L3" for MC / "ak4CaloL2L3Residual" for Data
+    jetCorrEtaMax = cms.double(9.9),
+    type1JetPtThreshold = cms.double(20.0),
+    skipEM = cms.bool(True),
+    skipEMfractionThreshold = cms.double(0.90),
+    srcMET = cms.InputTag('corMetGlobalMuons')
+)
+
+##____________________________________________________________________________||
+muonCaloMETcorr = cms.EDProducer("MuonMETcorrInputProducer",
+    src = cms.InputTag('muons'),
+    srcMuonCorrections = cms.InputTag('muonMETValueMapProducer', 'muCorrData')
+)
 
 ##____________________________________________________________________________||
 caloType1CorrectedMet = cms.EDProducer("CorrectedCaloMETProducer",
