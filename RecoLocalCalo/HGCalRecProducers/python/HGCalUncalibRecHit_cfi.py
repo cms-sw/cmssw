1ffd84155d844ce4c0a5e8cb4c614f22c5bce08e
vandreev11 <Valeri.Andreev@cern.ch>
2014-05-28 12:20:19 +0200

    HGCAL RecHits configuration correction

--- RecoLocalCalo/HGCalRecProducers/python/HGCalUncalibRecHit_cfi.py
+++ RecoLocalCalo/HGCalRecProducers/python/HGCalUncalibRecHit_cfi.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 # HGCAL producer of rechits starting from digis
 HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
-    HGCEEdigiCollection = cms.InputTag("HGCalDigis","HGCEEDigis"),
+    HGCEEdigiCollection = cms.InputTag("mix","HGCDigisEE"),
     HGCEEhitCollection = cms.string('HGCEEUncalibRecHits'),
-    HGCHEFdigiCollection = cms.InputTag("HGCalDigis","HGCHEFDigis"),
+    HGCHEFdigiCollection = cms.InputTag("mix","HGCDigisHEfront"),
     HGCHEFhitCollection = cms.string('HGCHEFUncalibRecHits'),
-    HGCHEBdigiCollection = cms.InputTag("HGCalDigis","HGCHEBDigis"),
+    HGCHEBdigiCollection = cms.InputTag("mix","HGCDigisHEback"),
     HGCHEBhitCollection = cms.string('HGCHEBUncalibRecHits'),                                    
     algo = cms.string("HGCalUncalibRecHitWorkerWeights")
 )
