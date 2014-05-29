1fe01360c5a275baa78f543d99babc6d00b325db
Andrea Venturi <andrea.venturi@cern.ch>
2014-05-29 22:21:07 +0200

    CmsTrackerDiskBuilder split into present and Phase1 based on number of blades
    Conflicts:
    	Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.cc
    	Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.h

--- Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.cc
+++ Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.cc
@@ -11,67 +11,6 @@
 
 using namespace std;
 
-CmsTrackerDiskBuilder::CmsTrackerDiskBuilder( unsigned int totalBlade )
-  : m_totalBlade( totalBlade )
-{}
-
-bool
-PhiSort( const GeometricDet* Panel1, const GeometricDet* Panel2 )
-{
-  return( Panel1->phi() < Panel2->phi());
-}
-
-void
-CmsTrackerDiskBuilder::PhiPosNegSplit_innerOuter( std::vector< GeometricDet const *>::iterator begin,
-						  std::vector< GeometricDet const *>::iterator end )
-{
-  // first sort in phi, lowest first (-pi to +pi)
-  std::sort( begin, end, PhiSort );
-
-  // now put positive phi (in order) ahead of negative phi as in std geometry
-  std::vector<const GeometricDet*> theCompsPosNeg;
-  theCompsPosNeg.empty();
-  theCompsPosNeg.clear();
-  // also find the average radius (used to split inner and outer disk panels)
-  double theRmin = (**begin).rho();
-  double theRmax = theRmin;
-  for(vector<const GeometricDet*>::const_iterator it=begin;
-      it!=end;it++){
-    if((**it).phi() >= 0) theCompsPosNeg.push_back(*it);
-    theRmin = std::min( theRmin, (**it).rho());
-    theRmax = std::max( theRmax, (**it).rho());
-  }
-  for(vector<const GeometricDet*>::const_iterator it=begin;
-      it!=end;it++){
-    if((**it).phi() < 0) theCompsPosNeg.push_back(*it);
-  }
-
-  // now put inner disk panels first
-  double radius_split = 0.5 * (theRmin + theRmax);
-  if(theRmax < 150.) radius_split = 100.;
-  std::vector<const GeometricDet*> theCompsInnerOuter;
-  theCompsInnerOuter.empty();
-  theCompsInnerOuter.clear();
-  unsigned int num_inner = 0;
-  for(vector<const GeometricDet*>::const_iterator it=theCompsPosNeg.begin();
-      it!=theCompsPosNeg.end();it++){
-    if((**it).rho() <= radius_split) {
-      theCompsInnerOuter.push_back(*it);
-      num_inner++;
-    }
-  }
-
-  for(vector<const GeometricDet*>::const_iterator it=theCompsPosNeg.begin();
-      it!=theCompsPosNeg.end();it++){
-    if((**it).rho() > radius_split) theCompsInnerOuter.push_back(*it);
-  }
-  //  std::cout << "num of inner = " << num_inner << " with radius less than " << radius_split << std::endl;
-  // now shift outer by one
-
-  std::rotate(theCompsInnerOuter.begin()+num_inner,theCompsInnerOuter.begin()+num_inner+1,theCompsInnerOuter.end());
-  std::copy(theCompsInnerOuter.begin(), theCompsInnerOuter.end(), begin);
-}
-
 void
 CmsTrackerDiskBuilder::buildComponent( DDFilteredView& fv, GeometricDet* g, std::string s )
 {
@@ -99,10 +38,7 @@ CmsTrackerDiskBuilder::sortNS( DDFilteredView& fv, GeometricDet* det )
   switch( det->components().front()->type())
   {
   case GeometricDet::panel:
-    if( m_totalBlade == 24 )
-      TrackerStablePhiSort( comp.begin(), comp.end(), ExtractPhi());
-    else
-      PhiPosNegSplit_innerOuter( comp.begin(), comp.end());
+    TrackerStablePhiSort( comp.begin(), comp.end(), ExtractPhi());
     break;
   default:
     edm::LogError( "CmsTrackerDiskBuilder" ) << "ERROR - wrong SubDet to sort..... " << det->components().front()->type();
@@ -113,8 +49,6 @@ CmsTrackerDiskBuilder::sortNS( DDFilteredView& fv, GeometricDet* det )
 
   uint32_t totalblade = comp.size()/2;
   //  std::cout << "pixel_disk " << pixel_disk << endl; 
-  if( totalblade != m_totalBlade && totalblade != 34 )
-    edm::LogError( "CmsTrackerDiskBuilder" ) << "ERROR, The Total Number of Blade in one disk is " << totalblade << "; configured " << m_totalBlade;
 
   zminpanels.reserve( totalblade );
   zmaxpanels.reserve( totalblade );
--- Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.h
+++ Geometry/TrackerNumberingBuilder/plugins/CmsTrackerDiskBuilder.h
@@ -10,16 +10,10 @@
  */
 class CmsTrackerDiskBuilder : public CmsTrackerLevelBuilder
 {
-public:
-  CmsTrackerDiskBuilder( unsigned int totalBlade );
   
 private:
   virtual void sortNS( DDFilteredView& , GeometricDet* );
   virtual void buildComponent( DDFilteredView& , GeometricDet*, std::string );
-  
-  void PhiPosNegSplit_innerOuter( std::vector< GeometricDet const *>::iterator begin,
-				  std::vector< GeometricDet const *>::iterator end );
-  unsigned int m_totalBlade;
 };
 
 #endif
