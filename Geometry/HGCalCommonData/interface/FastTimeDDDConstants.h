48006cb84bca6b81eb0c7fa65d825a5ba4a4f8a0
Sunanda <sunanda.banerjee@cern.ch>
2014-05-30 17:50:18 +0200

    Clean up the code

--- Geometry/HGCalCommonData/interface/FastTimeDDDConstants.h
+++ Geometry/HGCalCommonData/interface/FastTimeDDDConstants.h
@@ -4,7 +4,7 @@
 /** \class FastTimeDDDConstants
  *
  * this class reads the constant section of
- * the shashlik-numbering xml-file
+ * the numbering xml-file for fast timer device
  *  
  *  $Date: 2014/03/20 00:06:50 $
  * \author Sunanda Banerjee, SINP <sunanda.banerjee@cern.ch>
@@ -24,7 +24,6 @@ class FastTimeDDDConstants {
 
 public:
 
-  FastTimeDDDConstants();
   FastTimeDDDConstants( const DDCompactView& cpv );
   ~FastTimeDDDConstants();
 
@@ -33,19 +32,17 @@ public:
   std::pair<int,int>  getXY(int copy)           const;
   std::pair<int,int>  getXY(double x, double y) const;
   int                 getCells()                const {return 4*nCells;}
-  void                initialize(const DDCompactView& cpv);
   bool                isValidXY(int ix, int iy) const;
   bool                isValidCell(int copy)     const;
   int                 quadrant(int ix, int iy)  const;
   int                 quadrant(int copy)        const;
        
 private:
-  void                checkInitialized() const;
+  void                initialize(const DDCompactView& cpv);
   void                loadSpecPars(const DDFilteredView& fv);
   std::vector<double> getDDDArray(const std::string &, 
                                   const DDsvalues_type &) const;
 
-  bool                tobeInitialized;
   int                 nCells, nCols, nRows, cellType;
   double              rIn, rOut, cellSize;
   std::vector<int>    firstY, lastY, firstCell, lastCell;
