69000b82868688ea152303a6050761c2956a6f28
Jim <jim.brooke@cern.ch>
2014-06-05 16:44:51 +0200

    include unpacker

--- EventFilter/L1TRawToDigi/test/test_MP7BufferDumpToRaw.py
+++ EventFilter/L1TRawToDigi/test/test_MP7BufferDumpToRaw.py
@@ -9,18 +9,10 @@ process = cms.Process('L1')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.Geometry.GeometryDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('Configuration.StandardSequences.Generator_cff')
-process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
-process.load('GeneratorInterface.Core.genFilterSummary_cff')
-process.load('Configuration.StandardSequences.SimIdeal_cff')
-process.load('Configuration.StandardSequences.Digi_cff')
 process.load('Configuration.StandardSequences.SimL1Emulator_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -45,19 +37,20 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Output definition
 
-process.output = cms.OutputModule("PoolOutputModule",
+process.output = cms.OutputModule(
+    "PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = cms.untracked.vstring("keep *",
 					   "drop *_mix_*_*"),
-    fileName = cms.untracked.string('SingleElectronPt10_cfi_py_GEN_SIM_DIGI_L1.root'),
+    fileName = cms.untracked.string('L1T_EDM.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('')
-    ),
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('generation_step')
     )
+#    SelectEvents = cms.untracked.PSet(
+#        SelectEvents = cms.vstring('generation_step')
+#    )
 )
 
 # Additional output definition
@@ -65,27 +58,6 @@ process.output = cms.OutputModule("PoolOutputModule",
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
-
-# convert buffer dump to raw
-process.load('EventFilter.L1TRawToDigi.mp7BufferDumpToRaw_cfi')
-
-# dump raw data
-process.dumpRaw = cms.EDAnalyzer( "DumpFEDRawDataProduct",
-  label = cms.untracked.string("mp7BufferDumpToRaw"),
-  feds = cms.untracked.vint32 ( 1 ),
-  dumpPayload = cms.untracked.bool ( True )
-)
-
-
-
-# upgrade calo stage 2
-#process.load('L1Trigger.L1TCalorimeter.L1TCaloStage2_cff')
-#process.caloStage2Layer1Digis.ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis")
-#process.caloStage2Layer1Digis.hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis")
-	    
-
-process.load('L1Trigger.L1TCalorimeter.l1tStage2CaloAnalyzer_cfi')
-
 
 # enable debug message logging for our modules
 process.MessageLogger = cms.Service(
@@ -98,8 +70,10 @@ process.MessageLogger = cms.Service(
 	threshold  = cms.untracked.string('DEBUG') 
     ),
     debugModules = cms.untracked.vstring(
-	'l1tCaloStage2TowerDigis',
-	'l1tCaloStage2Digis'
+        'mp7BufferDumpToRaw',
+        'l1tDigis',
+	'caloStage2TowerDigis',
+	'caloStage2Digis'
     )
 )
 
@@ -107,8 +81,46 @@ process.MessageLogger = cms.Service(
 process.load("CommonTools.UtilAlgos.TFileService_cfi")
 process.TFileService.fileName = cms.string('l1t.root')
 
+
+# user stuff
+
+# convert buffer dump to raw
+process.load('EventFilter.L1TRawToDigi.mp7BufferDumpToRaw_cfi')
+
+# dump raw data
+process.dumpRaw = cms.EDAnalyzer( 
+    "DumpFEDRawDataProduct",
+    label = cms.untracked.string("mp7BufferDumpToRaw"),
+    feds = cms.untracked.vint32 ( 1 ),
+    dumpPayload = cms.untracked.bool ( True )
+)
+
+# raw to digi
+import EventFilter.L1TRawToDigi.l1tRawToDigi_cfi
+process.l1tDigis = EventFilter.L1TRawToDigi.l1tRawToDigi_cfi.l1tRawToDigi.clone()
+process.l1tDigis.InputLabel = cms.InputTag("mp7BufferDumpToRaw")
+
+# upgrade calo stage 2
+#process.load('L1Trigger.L1TCalorimeter.L1TCaloStage2_cff')
+#process.caloStage2Layer1Digis.ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis")
+#process.caloStage2Layer1Digis.hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis")
+
+process.load('L1Trigger.L1TCalorimeter.l1tStage2CaloAnalyzer_cfi')
+process.l1tStage2CaloAnalyzer.towerToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.towerPreCompressionToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.clusterToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.egToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.tauToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.jetToken = cms.InputTag("l1tDigis")
+process.l1tStage2CaloAnalyzer.etSumToken = cms.InputTag("l1tDigis")
+
 # Path and EndPath definitions
 process.path = cms.Path(
     process.mp7BufferDumpToRaw
     +process.dumpRaw
+    +process.l1tDigis
 )
+
+#process.out = cms.EndPath(
+#process.output
+#)
