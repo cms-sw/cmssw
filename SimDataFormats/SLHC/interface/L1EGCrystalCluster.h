c563eabe1d088d15df6504f3420d6c9a03c19dca
Nick Smith <ncsmith6@wisc.edu>
2014-06-06 12:50:22 -0500

    Updated crystal level EG cluster producer to use dynamic hovere and isolation cuts. Also, refactored the L1EGCrystalCluster class to be more in line with the style of l1extra::L1EmParticle()

--- SimDataFormats/SLHC/interface/L1EGCrystalCluster.h
+++ SimDataFormats/SLHC/interface/L1EGCrystalCluster.h
@@ -2,27 +2,28 @@
 #define L1EGammaCrystalsCluster_h
 
 #include <vector>
+#include "DataFormats/Candidate/interface/LeafCandidate.h"
 
 namespace l1slhc
 {
 
-  class L1EGCrystalCluster {
-  public:
-    float et ;
-    float eta ;
-    float phi ;
-    float e ;
-    float x ;
-    float y ;
-    float z ;
-    float hovere ;
-
-    float ECALiso ;
-    float ECALetPUcorr;
-
-	int ieta;
-	int iphi;
+  class L1EGCrystalCluster : public reco::LeafCandidate {
+    public:
+      L1EGCrystalCluster() : LeafCandidate(), hovere_(0.), iso_(0.), PUcorrPt_(0.) {};
+      L1EGCrystalCluster(const PolarLorentzVector& p4, float hovere, float iso, float PUcorrPt = 0.) : LeafCandidate(0, p4), hovere_(hovere), iso_(iso), PUcorrPt_(PUcorrPt) {};
+      L1EGCrystalCluster(const LorentzVector& p4, float hovere, float iso, float PUcorrPt = 0.) : LeafCandidate(0, p4), hovere_(hovere), iso_(iso), PUcorrPt_(PUcorrPt) {};
+      virtual ~L1EGCrystalCluster() {};
+      inline float hovere() const { return hovere_; };
+      inline float isolation() const { return iso_; };
+      inline float PUcorrPt() const { return PUcorrPt_; };
     
+    private:
+      // HCal energy in region behind cluster (for size, look in producer) / ECal energy in cluster
+      float hovere_;
+      // ECal isolation (for outer window size, again look in producer)
+      float iso_;
+      // Pileup-corrected energy deposit, not studied carefully yet, don't use
+      float PUcorrPt_;
   };
   
   
