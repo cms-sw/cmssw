abb3230a120d0705ef0ce0f2be1d76966112f525
Sven Dildick <sven.dildick@cern.ch>
2014-06-10 22:26:16 +0200

    use geometry builder module in GEMGeometryBuilder

--- Configuration/Geometry/python/GeometryExtended2023HGCalMuon4EtaReco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023HGCalMuon4EtaReco_cff.py
@@ -15,8 +15,8 @@ from Geometry.TrackerNumberingBuilder.trackerTopologyConstants_cfi import *
 #Muon
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-from Geometry.GEMGeometry.gemGeometry_cfi import *
-from Geometry.GEMGeometry.me0Geometry_cfi import *
+from Geometry.GEMGeometryBuilder.gemGeometry_cfi import *
+from Geometry.GEMGeometryBuilder.me0Geometry_cfi import *
 
 #  Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerSLHCGeometry_cff import *
