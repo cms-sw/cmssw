36204f0248d83b0a0537fe8f5bcc85d483473f52
Michail <bachtis@cern.ch>
2014-06-10 15:23:37 +0200

    Adding new Thresholds for HO

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHBHE_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHBHE_cfi.py
@@ -1,21 +1,16 @@
 import FWCore.ParameterSet.Config as cms
-particleFlowRecHitHO = cms.EDProducer("PFRecHitProducer",
+particleFlowRecHitHBHEHO = cms.EDProducer("PFRecHitProducer",
     navigator = cms.PSet(
         name = cms.string("PFRecHitHCALNavigator")
     ),
     producers = cms.VPSet(
            cms.PSet(
-             name = cms.string("PFHORecHitCreator"),
-             src  = cms.InputTag("horeco",""),
+             name = cms.string("PFHBHERecHitCreator"),
+             src  = cms.InputTag("hbhereco",""),
              qualityTests = cms.VPSet(
-#                  cms.PSet(
-#                  name = cms.string("PFRecHitQTestHOThreshold"),
-#                  threshold_ring0 = cms.double(0.4),
-#                  threshold_ring12 = cms.double(1.0)
-#                  ),
                   cms.PSet(
                   name = cms.string("PFRecHitQTestThreshold"),
-                  threshold = cms.double(0.05) # new threshold for SiPM HO
+                  threshold = cms.double(0.4)
                   ),
                   cms.PSet(
                       name = cms.string("PFRecHitQTestHCALChannel"),
@@ -23,8 +18,11 @@ particleFlowRecHitHO = cms.EDProducer("PFRecHitProducer",
                       cleaningThresholds = cms.vdouble(0.0),
                       flags              = cms.vstring('Standard')
                   )
+                  
+
              )
-           )
+           ),
+           
     )
 
 )
