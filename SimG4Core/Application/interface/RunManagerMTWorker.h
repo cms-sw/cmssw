288fe08216cb0d2ffbe4e91c6569345865620c7f
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-06-11 18:02:48 +0200

    Add skeletons for produce() and generateEvent() to RunManagerMTWorker

--- SimG4Core/Application/interface/RunManagerMTWorker.h
+++ SimG4Core/Application/interface/RunManagerMTWorker.h
@@ -1,22 +1,37 @@
 #ifndef SimG4Core_Application_RunManagerMTWorker_H
 #define SimG4Core_Application_RunManagerMTWorker_H
 
+#include "FWCore/Utilities/interface/InputTag.h"
+
 #include "SimG4Core/Generators/interface/Generator.h"
 
 #include <memory>
 
 namespace edm {
   class ParameterSet;
+  class Event;
+  class EventSetup;
 }
 class Generator;
+
+class G4Event;
+class G4SimEvent;
 
 class RunManagerMTWorker {
 public:
   RunManagerMTWorker(const edm::ParameterSet& iConfig);
   ~RunManagerMTWorker();
 
+  void produce(const edm::Event& inpevt, const edm::EventSetup& es);
+
 private:
+  G4Event *generateEvent(const edm::Event& inpevt);
+
   Generator m_generator;
+  std::string m_InTag;
+
+  std::unique_ptr<G4Event> m_currentEvent;
+  std::unique_ptr<G4SimEvent> m_simEvent;
 };
 
 #endif
--- SimG4Core/Application/src/RunManagerMTWorker.cc
+++ SimG4Core/Application/src/RunManagerMTWorker.cc
@@ -1,9 +1,39 @@
 #include "SimG4Core/Application/interface/RunManagerMTWorker.h"
+#include "SimG4Core/Application/interface/G4SimEvent.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+
+#include "G4Event.hh"
 
 RunManagerMTWorker::RunManagerMTWorker(const edm::ParameterSet& iConfig):
-  m_generator(iConfig.getParameter<edm::ParameterSet>("Generator"))
+  m_generator(iConfig.getParameter<edm::ParameterSet>("Generator")),
+  m_InTag(iConfig.getParameter<edm::ParameterSet>("Generator").getParameter<std::string>("HepMCProductLabel"))
 {}
 
 RunManagerMTWorker::~RunManagerMTWorker() {}
+
+void RunManagerMTWorker::produce(const edm::Event& inpevt, const edm::EventSetup& es) {
+  m_currentEvent.reset(generateEvent(inpevt));
+}
+
+G4Event * RunManagerMTWorker::generateEvent(const edm::Event& inpevt) {
+  m_currentEvent.reset();
+  m_simEvent.reset();
+
+  G4Event * evt = new G4Event(inpevt.id().event());
+  edm::Handle<edm::HepMCProduct> HepMCEvt;
+
+  inpevt.getByLabel(m_InTag, HepMCEvt);
+
+  m_generator.setGenEvent(HepMCEvt->GetEvent());
+
+  // STUFF MISSING
+
+  return evt;
+}
+
