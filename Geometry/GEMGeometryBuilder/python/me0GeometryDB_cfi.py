6bb0362f501521c8579309c68c8db64ed62f670d
Sven Dildick <sven.dildick@cern.ch>
2014-06-11 17:12:49 +0200

    remove compatibiltyWith11 from config

--- Geometry/GEMGeometryBuilder/python/me0GeometryDB_cfi.py
+++ Geometry/GEMGeometryBuilder/python/me0GeometryDB_cfi.py
@@ -4,6 +4,5 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the ME0 geometry model.
 #
 ME0GeometryESModule = cms.ESProducer("ME0GeometryESModule",
-    compatibiltyWith11 = cms.untracked.bool(True),
     useDDD = cms.untracked.bool(False)
 )
