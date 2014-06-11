ffeec43b4bbb50a47665e9a5e7f615c406ad4124
erica brondolin <erica.brondolin@cern.ch>
2014-06-11 13:02:40 +0200

    cleaning up TrackingTools folder

--- TrackingTools/KalmanUpdators/src/KFUpdator.cc
+++ TrackingTools/KalmanUpdators/src/KFUpdator.cc
@@ -113,7 +113,6 @@ TrajectoryStateOnSurface KFUpdator::update(const TrajectoryStateOnSurface& tsos,
 
   MeasurementExtractor me(tsos);
 
-  typedef typename AlgebraicROOTObject<D,5>::Matrix MatD5;
   AlgebraicVector5 x = tsos.localParameters().vector();
   const AlgebraicSymMatrix55 &C = (tsos.localError().matrix());
   // Measurement matrix
