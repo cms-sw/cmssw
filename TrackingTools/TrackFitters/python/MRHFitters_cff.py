62b28f1625ba005970f90d45e2d901b3d81461b1
erica brondolin <erica.brondolin@cern.ch>
2014-06-12 11:49:18 +0200

    Add MRHChi2 in TrackingComponentsRecord

--- TrackingTools/TrackFitters/python/MRHFitters_cff.py
+++ TrackingTools/TrackFitters/python/MRHFitters_cff.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+import TrackingTools.KalmanUpdators.MRHChi2MeasurementEstimatorESProducer_cfi
+
 import TrackingTools.TrackFitters.KFTrajectoryFitterESProducer_cfi
 MRHTrajectoryFitter = TrackingTools.TrackFitters.KFTrajectoryFitterESProducer_cfi.KFTrajectoryFitter.clone(
     ComponentName = 'MRHFitter',
