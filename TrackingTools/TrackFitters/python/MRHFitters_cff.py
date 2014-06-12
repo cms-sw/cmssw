7da9ea9b7f6346acb574a4322e7a4f134ec515e7
erica brondolin <erica.brondolin@cern.ch>
2014-06-12 16:44:35 +0200

    Fix MRHChi2 in TrackingComponentsRecord

--- TrackingTools/TrackFitters/python/MRHFitters_cff.py
+++ TrackingTools/TrackFitters/python/MRHFitters_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-import TrackingTools.KalmanUpdators.MRHChi2MeasurementEstimatorESProducer_cfi
+from TrackingTools.KalmanUpdators.MRHChi2MeasurementEstimatorESProducer_cfi import *
 
 import TrackingTools.TrackFitters.KFTrajectoryFitterESProducer_cfi
 MRHTrajectoryFitter = TrackingTools.TrackFitters.KFTrajectoryFitterESProducer_cfi.KFTrajectoryFitter.clone(
