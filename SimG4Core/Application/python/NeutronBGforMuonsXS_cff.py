466ecda52fed20bc693dbd8dfb7e3bcb3c442115
Vladimir <Vladimir.Ivantchenko@cern.ch>
2014-06-16 15:09:06 +0200

    improved comments

--- SimG4Core/Application/python/NeutronBGforMuonsXS_cff.py
+++ SimG4Core/Application/python/NeutronBGforMuonsXS_cff.py
@@ -5,7 +5,7 @@ def customise(process):
     # fragment allowing to simulate neutron background in muon system
 
   if hasattr(process,'g4SimHits'):
-    # time window 10 millisecond
+    # time window 100 millisecond
     process.common_maximum_time.MaxTrackTime = cms.double(100000000.0)
     process.common_maximum_time.DeadRegions = cms.vstring()
     # Physics List XS
