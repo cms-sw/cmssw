7653503a37d12e5ebfc2e67581915de2b09c605e
Pavel Jez <pavel.jez@cern.ch>
2014-06-16 12:35:12 +0200

    Reverting to default PFBRECO config

--- CommonTools/ParticleFlow/python/PFBRECO_cff.py
+++ CommonTools/ParticleFlow/python/PFBRECO_cff.py
@@ -10,7 +10,7 @@ from CommonTools.ParticleFlow.pfJets_cff import *
 from CommonTools.ParticleFlow.pfTaus_cff import *
 
 #delta beta weighting
-from CommonTools.ParticleFlow.deltaBetaWeights_cff  import *
+#from CommonTools.ParticleFlow.deltaBetaWeights_cff  import *
 
 # sequential top projection cleaning
 from CommonTools.ParticleFlow.TopProjectors.pfNoMuon_cfi import * 
@@ -39,7 +39,7 @@ PFBRECO = cms.Sequence(
     pfNoPileUpSequence +
     pfNoPileUpJMESequence +
     pfParticleSelectionSequence + 
-    pfDeltaBetaWeightingSequence +
+#    pfDeltaBetaWeightingSequence +
     pfPhotonSequence +
     pfMuonSequence + 
     pfNoMuon +
