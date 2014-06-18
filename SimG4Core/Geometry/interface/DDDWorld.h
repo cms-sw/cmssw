5c3436722672e6b818e778b6fcec4caf23c937ac
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-06-18 14:54:20 +0200

    Make DDDWorld::SetAsWorld to static
    In order to use it from RunManagerMTWorker

--- SimG4Core/Geometry/interface/DDDWorld.h
+++ SimG4Core/Geometry/interface/DDDWorld.h
@@ -13,11 +13,10 @@ public:
   DDDWorld(const DDCompactView*, G4LogicalVolumeToDDLogicalPartMap &,
 	   SensitiveDetectorCatalog &, bool check=false);
   ~DDDWorld();
-  void SetAsWorld(G4VPhysicalVolume * pv);
+  static void SetAsWorld(G4VPhysicalVolume * pv);
   const G4VPhysicalVolume * GetWorldVolume() const { return m_world; }
 
 private:
-  DDG4Builder * theBuilder;
   G4VPhysicalVolume * m_world;
 };
 
