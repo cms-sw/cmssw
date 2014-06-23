48c530bb4ac1cc738a7b0b9689daf10cf2df98b7
Arnaud <arnaud.chiron@cern.ch>
2014-06-23 17:12:29 +0200

    Multithread Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.cc

--- Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.cc
+++ Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.cc
@@ -10,38 +10,119 @@ ElectronMcSignalPostValidator::ElectronMcSignalPostValidator( const edm::Paramet
 ElectronMcSignalPostValidator::~ElectronMcSignalPostValidator()
  {}
 
-void ElectronMcSignalPostValidator::book()
- { setBookIndex(-1) ; }
-
-void ElectronMcSignalPostValidator::finalize()
+//void ElectronMcSignalPostValidator::book()
+void ElectronMcSignalPostValidator::bookHistograms( DQMStore::IBooker & iBooker, edm::Run const &, edm::EventSetup const & )
  {
+  setBookIndex(-1) ; 
   setBookPrefix("h_ele") ;
 
   edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "efficiency calculation" ;
-  bookH1andDivide("etaEff","mc_Eta_matched","mc_Eta","#eta","Efficiency","");
-  bookH1andDivide("zEff","mc_Z_matched","mc_Z","z (cm)","Efficiency","");
-  bookH1andDivide("absetaEff","mc_AbsEta_matched","mc_AbsEta","|#eta|","Efficiency");
-  bookH1andDivide("ptEff","mc_Pt_matched","mc_Pt","p_{T} (GeV/c)","Efficiency");
-  bookH1andDivide("phiEff","mc_Phi_matched","mc_Phi","#phi (rad)","Efficiency");
-  bookH2andDivide("ptEtaEff","mc_PtEta_matched","mc_PtEta","#eta","p_{T} (GeV/c)");
+  bookH1andDivide(iBooker, "etaEff","mc_Eta_matched","mc_Eta","#eta","Efficiency","");
+  bookH1andDivide(iBooker, "zEff","mc_Z_matched","mc_Z","z (cm)","Efficiency","");
+  bookH1andDivide(iBooker, "absetaEff","mc_AbsEta_matched","mc_AbsEta","|#eta|","Efficiency");
+  bookH1andDivide(iBooker, "ptEff","mc_Pt_matched","mc_Pt","p_{T} (GeV/c)","Efficiency");
+  bookH1andDivide(iBooker, "phiEff","mc_Phi_matched","mc_Phi","#phi (rad)","Efficiency");
+  bookH2andDivide(iBooker, "ptEtaEff","mc_PtEta_matched","mc_PtEta","#eta","p_{T} (GeV/c)");
 
   edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "q-misid calculation" ;
-  bookH1andDivide("etaQmisid","mc_Eta_matched_qmisid","mc_Eta","#eta","q misId","");
-  bookH1andDivide("zQmisid","mc_Z_matched_qmisid","mc_Z","z (cm)","q misId","");
-  bookH1andDivide("absetaQmisid","mc_AbsEta_matched_qmisid","mc_AbsEta","|#eta|","q misId");
-  bookH1andDivide("ptQmisid","mc_Pt_matched_qmisid","mc_Pt","p_{T} (GeV/c)","q misId");
+  bookH1andDivide(iBooker, "etaQmisid","mc_Eta_matched_qmisid","mc_Eta","#eta","q misId","");
+  bookH1andDivide(iBooker, "zQmisid","mc_Z_matched_qmisid","mc_Z","z (cm)","q misId","");
+  bookH1andDivide(iBooker, "absetaQmisid","mc_AbsEta_matched_qmisid","mc_AbsEta","|#eta|","q misId");
+  bookH1andDivide(iBooker, "ptQmisid","mc_Pt_matched_qmisid","mc_Pt","p_{T} (GeV/c)","q misId");
 
   edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "all reco electrons" ;
-  bookH1andDivide("etaEff_all","vertexEta_all","h_mc_Eta","#eta","N_{rec}/N_{gen}","");
-  bookH1andDivide("ptEff_all","vertexPt_all","h_mc_Pt","p_{T} (GeV/c)","N_{rec}/N_{gen}","");
+  bookH1andDivide(iBooker, "etaEff_all","vertexEta_all","h_mc_Eta","#eta","N_{rec}/N_{gen}","");
+  bookH1andDivide(iBooker, "ptEff_all","vertexPt_all","h_mc_Pt","p_{T} (GeV/c)","N_{rec}/N_{gen}","");
 
   edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "classes" ;
-  bookH1andDivide("eta_goldenFrac","eta_golden","h_ele_eta","|#eta|","Fraction of electrons","fraction of golden electrons vs eta");
-  bookH1andDivide("eta_bbremFrac" ,"eta_bbrem","h_ele_eta","|#eta|","Fraction of electrons","fraction of big brem electrons vs eta");
-  bookH1andDivide("eta_showerFrac","eta_shower","h_ele_eta","|#eta|","Fraction of electrons","fraction of showering electrons vs eta");
+  bookH1andDivide(iBooker, "eta_goldenFrac","eta_golden","h_ele_eta","|#eta|","Fraction of electrons","fraction of golden electrons vs eta");
+  bookH1andDivide(iBooker, "eta_bbremFrac" ,"eta_bbrem","h_ele_eta","|#eta|","Fraction of electrons","fraction of big brem electrons vs eta");
+  bookH1andDivide(iBooker, "eta_showerFrac","eta_shower","h_ele_eta","|#eta|","Fraction of electrons","fraction of showering electrons vs eta");
 
   // fbrem
-  MonitorElement * p1_ele_fbremVsEta_mean = get("fbremvsEtamean") ;
+  MonitorElement * p1_ele_fbremVsEta_mean = ElectronDqmAnalyzerBase::get("fbremvsEtamean") ;
+  TAxis * etaAxis = p1_ele_fbremVsEta_mean->getTProfile()->GetXaxis() ;
+  h1_ele_xOverX0VsEta = ElectronDqmAnalyzerBase::bookH1withSumw2(iBooker, "xOverx0VsEta","mean X/X_0 vs eta",etaAxis->GetNbins(),etaAxis->GetXmin(),etaAxis->GetXmax());
+  std::cout << "McSignalPost bookHistograms h1_ele_xOverX0VsEta : " << h1_ele_xOverX0VsEta->getTH1F()->GetXaxis()->GetNbins()+1 << " Nbins" << std::endl;
+  for (int ibin=1;ibin<etaAxis->GetNbins()+1;ibin++) {
+    double xOverX0 = 0.; 
+	//std::cout << "boucle finalize" << ibin << std::endl;
+    if (p1_ele_fbremVsEta_mean->getBinContent(ibin)>0.)
+     { xOverX0 = -log(p1_ele_fbremVsEta_mean->getBinContent(ibin)) ; }
+    h1_ele_xOverX0VsEta->setBinContent(ibin,xOverX0) ;
+  }/**/
+
+  // profiles from 2D histos
+  ElectronDqmAnalyzerBase::profileX(iBooker,"PoPtrueVsEta","mean ele momentum / gen momentum vs eta","#eta","<P/P_{gen}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"PoPtrueVsPhi","mean ele momentum / gen momentum vs phi","#phi (rad)","<P/P_{gen}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EoEtruePfVsEg","mean pflow sc energy / true energy vs e/g sc energy","E/E_{gen} (e/g)","<E/E_{gen}> (pflow)") ;
+  ElectronDqmAnalyzerBase::profileY(iBooker,"EoEtruePfVsEg","mean e/g sc energy / true energy vs pflow sc energy","E/E_{gen} (pflow)","<E/E_{gen}> (eg)") ;
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EtaMnEtaTrueVsEta","mean ele eta - gen eta vs eta","#eta","<#eta_{rec} - #eta_{gen}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EtaMnEtaTrueVsPhi","mean ele eta - gen eta vs phi","#phi (rad)","<#eta_{rec} - #eta_{gen}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"PhiMnPhiTrueVsEta","mean ele phi - gen phi vs eta","#eta","<#phi_{rec} - #phi_{gen}> (rad)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"PhiMnPhiTrueVsPhi","mean ele phi - gen phi vs phi","#phi (rad)","");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"vertexPtVsEta","mean ele transverse momentum vs eta","#eta","<p_{T}> (GeV/c)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"vertexPtVsPhi","mean ele transverse momentum vs phi","#phi (rad)","<p_{T}> (GeV/c)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EoPVsEta","mean ele E/p vs eta","#eta","<E/P_{vertex}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EoPVsPhi","mean ele E/p vs phi","#phi (rad)","<E/P_{vertex}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EoPoutVsEta","mean ele E/pout vs eta","#eta","<E_{seed}/P_{out}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EoPoutVsPhi","mean ele E/pout vs phi","#phi (rad)","<E_{seed}/P_{out}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EeleOPoutVsEta","mean ele Eele/pout vs eta","#eta","<E_{ele}/P_{out}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"EeleOPoutVsPhi","mean ele Eele/pout vs phi","#phi (rad)","<E_{ele}/P_{out}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"HoEVsEta","mean ele H/E vs eta","#eta","<H/E>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"HoEVsPhi","mean ele H/E vs phi","#phi (rad)","<H/E>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"chi2VsEta","mean ele track chi2 vs eta","#eta","<#Chi^{2}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"chi2VsPhi","mean ele track chi2 vs phi","#phi (rad)","<#Chi^{2}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"ambiguousTracksVsEta","mean ele # ambiguous tracks  vs eta","#eta","<N_{ambiguous}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"foundHitsVsEta","mean ele track # found hits vs eta","#eta","<N_{hits}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"foundHitsVsPhi","mean ele track # found hits vs phi","#phi (rad)","<N_{hits}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"lostHitsVsEta","mean ele track # lost hits vs eta","#eta","<N_{hits}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"lostHitsVsPhi","mean ele track # lost hits vs phi","#phi (rad)","<N_{hits}>");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"vertexTIPVsEta","mean tip (wrt gen vtx) vs eta","#eta","<TIP> (cm)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"vertexTIPVsPhi","mean tip (wrt gen vtx) vs phi","#phi","<TIP> (cm)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"vertexTIPVsPt","mean tip (wrt gen vtx) vs phi","p_{T} (GeV/c)","<TIP> (cm)");
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDphi2_VsEta","mean ele seed dphi 2nd layer vs eta","#eta","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDphi2_VsPt","mean ele seed dphi 2nd layer vs pt","p_{T} (GeV/c)","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDrz2_VsEta","mean ele seed dr(dz) 2nd layer vs eta","#eta","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDrz2_VsPt","mean ele seed dr(dz) 2nd layer vs pt","p_{T} (GeV/c)","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDphi2Pos_VsEta","mean ele seed dphi 2nd layer positron vs eta","#eta","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDphi2Pos_VsPt","mean ele seed dphi 2nd layer positron vs pt","p_{T} (GeV/c)","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDrz2Pos_VsEta","mean ele seed dr(dz) 2nd layer positron vs eta","#eta","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
+  ElectronDqmAnalyzerBase::profileX(iBooker,"seedDrz2Pos_VsPt","mean ele seed dr(dz) 2nd layer positron vs pt","p_{T} (GeV/c)","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
+
+ }
+
+void ElectronMcSignalPostValidator::finalize()
+ {
+    // --- AC ---
+  std::cout << "McSignalPost appel finalize" << std::endl; // to be removed
+//  setBookPrefix("h_ele") ;
+
+/*  edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "efficiency calculation" ;
+  bookH1andDivide(iBooker, "etaEff","mc_Eta_matched","mc_Eta","#eta","Efficiency","");
+  bookH1andDivide(iBooker, "zEff","mc_Z_matched","mc_Z","z (cm)","Efficiency","");
+  bookH1andDivide(iBooker, "absetaEff","mc_AbsEta_matched","mc_AbsEta","|#eta|","Efficiency");
+  bookH1andDivide(iBooker, "ptEff","mc_Pt_matched","mc_Pt","p_{T} (GeV/c)","Efficiency");
+  bookH1andDivide(iBooker, "phiEff","mc_Phi_matched","mc_Phi","#phi (rad)","Efficiency");
+  bookH2andDivide(iBooker, "ptEtaEff","mc_PtEta_matched","mc_PtEta","#eta","p_{T} (GeV/c)");
+
+  edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "q-misid calculation" ;
+  bookH1andDivide(iBooker, "etaQmisid","mc_Eta_matched_qmisid","mc_Eta","#eta","q misId","");
+  bookH1andDivide(iBooker, "zQmisid","mc_Z_matched_qmisid","mc_Z","z (cm)","q misId","");
+  bookH1andDivide(iBooker, "absetaQmisid","mc_AbsEta_matched_qmisid","mc_AbsEta","|#eta|","q misId");
+  bookH1andDivide(iBooker, "ptQmisid","mc_Pt_matched_qmisid","mc_Pt","p_{T} (GeV/c)","q misId");
+
+  edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "all reco electrons" ;
+  bookH1andDivide(iBooker, "etaEff_all","vertexEta_all","h_mc_Eta","#eta","N_{rec}/N_{gen}","");
+  bookH1andDivide(iBooker, "ptEff_all","vertexPt_all","h_mc_Pt","p_{T} (GeV/c)","N_{rec}/N_{gen}","");
+
+  edm::LogInfo("ElectronMcSignalPostValidator::finalize") << "classes" ;
+  bookH1andDivide(iBooker, "eta_goldenFrac","eta_golden","h_ele_eta","|#eta|","Fraction of electrons","fraction of golden electrons vs eta");
+  bookH1andDivide(iBooker, "eta_bbremFrac" ,"eta_bbrem","h_ele_eta","|#eta|","Fraction of electrons","fraction of big brem electrons vs eta");
+  bookH1andDivide(iBooker, "eta_showerFrac","eta_shower","h_ele_eta","|#eta|","Fraction of electrons","fraction of showering electrons vs eta");*/
+
+  // fbrem
+/*  MonitorElement * p1_ele_fbremVsEta_mean = get("fbremvsEtamean") ;
   TAxis * etaAxis = p1_ele_fbremVsEta_mean->getTProfile()->GetXaxis() ;
   MonitorElement * h1_ele_xOverX0VsEta = bookH1withSumw2("xOverx0VsEta","mean X/X_0 vs eta",etaAxis->GetNbins(),etaAxis->GetXmin(),etaAxis->GetXmax());
   for (int ibin=1;ibin<etaAxis->GetNbins()+1;ibin++) {
@@ -49,12 +130,12 @@ void ElectronMcSignalPostValidator::finalize()
     if (p1_ele_fbremVsEta_mean->getBinContent(ibin)>0.)
      { xOverX0 = -log(p1_ele_fbremVsEta_mean->getBinContent(ibin)) ; }
     h1_ele_xOverX0VsEta->setBinContent(ibin,xOverX0) ;
-  }
+  }*/
 
   // profiles from 2D histos
-  profileX("PoPtrueVsEta","mean ele momentum / gen momentum vs eta","#eta","<P/P_{gen}>");
-  profileX("PoPtrueVsPhi","mean ele momentum / gen momentum vs phi","#phi (rad)","<P/P_{gen}>");
-  profileX("EoEtruePfVsEg","mean pflow sc energy / true energy vs e/g sc energy","E/E_{gen} (e/g)","<E/E_{gen}> (pflow)") ;
+/*  profileX(iBooker,"PoPtrueVsEta","mean ele momentum / gen momentum vs eta","#eta","<P/P_{gen}>");
+  profileX(iBooker,"PoPtrueVsPhi","mean ele momentum / gen momentum vs phi","#phi (rad)","<P/P_{gen}>");
+  profileX(iBooker,"EoEtruePfVsEg","mean pflow sc energy / true energy vs e/g sc energy","E/E_{gen} (e/g)","<E/E_{gen}> (pflow)") ;
   profileY("EoEtruePfVsEg","mean e/g sc energy / true energy vs pflow sc energy","E/E_{gen} (pflow)","<E/E_{gen}> (eg)") ;
   profileX("EtaMnEtaTrueVsEta","mean ele eta - gen eta vs eta","#eta","<#eta_{rec} - #eta_{gen}>");
   profileX("EtaMnEtaTrueVsPhi","mean ele eta - gen eta vs phi","#phi (rad)","<#eta_{rec} - #eta_{gen}>");
@@ -87,7 +168,7 @@ void ElectronMcSignalPostValidator::finalize()
   profileX("seedDphi2Pos_VsEta","mean ele seed dphi 2nd layer positron vs eta","#eta","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
   profileX("seedDphi2Pos_VsPt","mean ele seed dphi 2nd layer positron vs pt","p_{T} (GeV/c)","<#phi_{pred} - #phi_{hit}, 2nd layer> (rad)",-0.004,0.004);
   profileX("seedDrz2Pos_VsEta","mean ele seed dr(dz) 2nd layer positron vs eta","#eta","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
-  profileX("seedDrz2Pos_VsPt","mean ele seed dr(dz) 2nd layer positron vs pt","p_{T} (GeV/c)","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);
+  profileX("seedDrz2Pos_VsPt","mean ele seed dr(dz) 2nd layer positron vs pt","p_{T} (GeV/c)","<r(z)_{pred} - r(z)_{hit}, 2nd layer> (cm)",-0.15,0.15);*/
 
 //  // investigation
 //  TH2F * h2 = get("PoPtrueVsEta")->getTH2F() ;
@@ -128,8 +209,11 @@ void ElectronMcSignalPostValidator::finalize()
 //  std::cout<<"H2   nu      : "<<nu<<std::endl ;
 //  std::cout<<"H2   no      : "<<no<<std::endl ;
 //  std::cout<<"H2   outside : "<<nb<<std::endl ;
-//  std::cout<<"PFX  entries : "<<h2->ProfileX()->GetEntries()<<std::endl ;
-//  std::cout<<"PFX effective entries : "<<h2->ProfileX()->GetEffectiveEntries()<<std::endl ;
+//  std::cout<<"PFX  entries : "<<h2->ElectronDqmAnalyzerBase::profileX(iBooker,)->GetEntries()<<std::endl ;
+//  std::cout<<"PFX effective entries : "<<h2->ElectronDqmAnalyzerBase::profileX(iBooker,)->GetEffectiveEntries()<<std::endl ;
+
+  std::cout << "McSignalPost fin appel finalize" << std::endl; // to be removed
+
 }
 
 
