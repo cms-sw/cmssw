941cdc35d3acb25e8021e7aee898a6ae85729532
Arnaud <arnaud.chiron@cern.ch>
2014-06-23 17:18:26 +0200

    Multithread ElectronTagProbeAnalyzer.h

--- DQMOffline/EGamma/plugins/ElectronTagProbeAnalyzer.h
+++ DQMOffline/EGamma/plugins/ElectronTagProbeAnalyzer.h
@@ -27,7 +27,7 @@ class ElectronTagProbeAnalyzer : public ElectronDqmAnalyzerBase
     explicit ElectronTagProbeAnalyzer(const edm::ParameterSet& conf);
     virtual ~ElectronTagProbeAnalyzer();
 
-    virtual void book() ;
+    virtual void bookHistograms( DQMStore::IBooker &, edm::Run const &, edm::EventSetup const & ) ;
     virtual void analyze( const edm::Event & e, const edm::EventSetup & c) ;
 
   private:
