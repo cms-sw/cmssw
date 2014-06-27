ec7c5a34340754028e5a6d7f04c88c185aaefdcb
Christopher <christopher.cowden@cern.ch>
2014-06-27 17:11:39 +0200

    migration 1 to multi-threaded Hcal DQM

--- Validation/HcalDigis/interface/Digi2Raw2Digi.h
+++ Validation/HcalDigis/interface/Digi2Raw2Digi.h
@@ -5,7 +5,7 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
 
@@ -13,13 +13,18 @@
 
 #include <map>
 
-class Digi2Raw2Digi : public edm::EDAnalyzer {
+class Digi2Raw2Digi : public DQMEDAnalyzer {
 public:
   explicit Digi2Raw2Digi(const edm::ParameterSet&);
   ~Digi2Raw2Digi();
+
+  virtual void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const & );
+
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
-  template<class Digi>  void compare(const edm::Event&, const edm::EventSetup&, const edm::EDGetTokenT<edm::SortedCollection<Digi> >& tok1, const edm::EDGetTokenT<edm::SortedCollection<Digi> >& tok2);  virtual void beginJob() ;
-  virtual void endJob() ;
+
+  template<class Digi>  void compare(const edm::Event&, const edm::EventSetup&, const edm::EDGetTokenT<edm::SortedCollection<Digi> >& tok1, const edm::EDGetTokenT<edm::SortedCollection<Digi> >& tok2);  
+
+  //virtual void endJob() ;
 
  private:
 
@@ -36,7 +41,6 @@ public:
   edm::EDGetTokenT<edm::SortedCollection<ZDCDataFrame> > tok_zdc2_;
 
   std::string outputFile_;
-  DQMStore* dbe_;
 
   MonitorElement* meStatus;
 
