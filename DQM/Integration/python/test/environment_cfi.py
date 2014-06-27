beb757d08c89f276ab51615c73e4c514e2935b6a
Laura Borrello <laura.borrello@cern.ch>
2014-06-27 17:52:21 +0200

    removing the runkey definition which is moved to input or file source

--- DQM/Integration/python/test/environment_cfi.py
+++ DQM/Integration/python/test/environment_cfi.py
@@ -21,30 +21,3 @@ dqmSaver.saveByLumiSection = -1
 dqmSaver.saveByRun = 1
 dqmSaver.saveAtJobEnd = False
 
-#RunType, and Runkey selection from RCMS
-import sys
-from FWCore.ParameterSet.VarParsing import VarParsing
-from dqmPythonTypes import *
-
-runParameters = VarParsing ('analysis')
-runParameters.register ('runtype',
-  'pp_run',
-  VarParsing.multiplicity.singleton,
-  VarParsing.varType.string,
-  "Type of Run in CMS")
-
-runParameters.register ('runkey',
-  'cosmic_run',
-  VarParsing.multiplicity.singleton,
-  VarParsing.varType.string,
-  "Run Keys of CMS")
-
-# Fix to allow scram to compile
-#if len(sys.argv) > 1:
-#  runParameters.parseArguments()
-
-runType = RunType(['pp_run','cosmic_run','hi_run','hpu_run'])
-if not runParameters.runkey.strip():
-  runParameters.runkey = 'cosmic_run'
-
-runType.setRunType(runParameters.runkey.strip())
