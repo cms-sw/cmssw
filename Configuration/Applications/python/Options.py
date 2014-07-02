11ea9c4a2125b0fb9a5cf8947ca902f2bda4a075
wmtan <wmtan@fnal.gov>
2014-07-02 18:28:16 +0200

    Use V2 of conditions DB by default

--- Configuration/Applications/python/Options.py
+++ Configuration/Applications/python/Options.py
@@ -37,7 +37,7 @@ parser.add_option("--conditions",
 parser.add_option("--useCondDBv1",
                   help="use conditions DB V1",
                   action="store_true",
-                  default=True,
+                  default=False,
                   dest="useCondDBv1")
 
 parser.add_option("--eventcontent",
