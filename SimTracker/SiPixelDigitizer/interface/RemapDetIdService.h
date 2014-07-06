0994b89bb48a9239ab7c36be4308ab9e7648fc43
Mark Grimes <mark.grimes@cern.ch>
2014-07-06 11:29:19 +0200

    Switched to using a persistent remapped collection

--- SimTracker/SiPixelDigitizer/interface/RemapDetIdService.h
+++ SimTracker/SiPixelDigitizer/interface/RemapDetIdService.h
@@ -4,13 +4,16 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <memory>
 
 #include <DataFormats/Common/interface/Handle.h> // Template so can't forward declare
+#include <DataFormats/Provenance/interface/EventID.h>
 
 //
 // Forward declarations
 //
 class PSimHit;
+class PileUpEventPrincipal;
 namespace edm
 {
 	class ActivityRegistry;
@@ -33,6 +36,25 @@ namespace simtracker
 		 * This code should be removed as soon as the ability to read the SLHC11 files is no longer
 		 * required.
 		 *
+		 * Consumers that want a PSimHit collection with the DetIds remapped call the getByLabel
+		 * proxy methods. This gets the collection from the event and checks whether it should be
+		 * remapped. Things that are checked are whether this service was configured to remap a
+		 * collection with that InputTag, and what version of CMSSW the requested collection was
+		 * made with. If the collection was made with one of the ones listed in the config
+		 * parameters (i.e. SLHC11 or SLHC12) then a new collection is created copying the SimHits
+		 * but changing the DetId according to the mapping in the file specified in the config.
+		 * Hence it is safe to call the method for collections that don't need to be remapped.
+		 *
+		 * Some consumers require the remapped collection to be persistent for the duration of
+		 * its processing. E.g. TrackingTruthAccumulator takes pointers to the SimHits in the
+		 * collection. The remapped collection will remain valid until the next call for the
+		 * same InputTag. **This is in no way thread safe**. 6_2_0_SLHCX is not threaded anyway,
+		 * and this code should be taken out long before we move to the threaded framework.
+		 *
+		 * N.B. The returned handle always has the provinence information of the original handle,
+		 * regardless of whether it was remapped or not. This is probably not a good idea but I
+		 * don't see much of an alternative.
+		 *
 		 * @author Mark Grimes (mark.grimes@bristol.ac.uk)
 		 * @date 05/Jul/2014
 		 */
@@ -41,6 +63,11 @@ namespace simtracker
 		public:
 			RemapDetIdService( const edm::ParameterSet& parameterSet, edm::ActivityRegistry& activityRegister );
 			~RemapDetIdService();
+
+			/** @brief Gets the collection and remaps the DetIds if required. Safe to call on all collections. */
+			bool getByLabel( const edm::Event& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle );
+			/** @brief Gets the collection and remaps the DetIds if required. Safe to call on all collections. */
+			bool getByLabel( const PileUpEventPrincipal& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle );
 
 			/** @brief Checks the provinence and returns the version of CMSSW that the given collection was created in */
 			template<class T> std::string cmsswVersionForProduct( const edm::Handle<T>& handle );
@@ -55,9 +82,11 @@ namespace simtracker
 			 */
 			void remapCollection( const edm::Handle<std::vector<PSimHit> >& handle, std::vector<PSimHit>& returnValue );
 		private:
+			template<class T> bool getByLabel_( T& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle );
 			std::map<uint32_t,uint32_t> detIdMap_;
 			std::vector<edm::InputTag> inputCollectionNames_;
 			std::vector<std::string> cmsswVersionsToRemap_;
+			std::vector< std::pair<edm::InputTag, std::unique_ptr<std::vector<PSimHit> > > > remappedCollections_;
 		};
 
 	} // end of namespace services
--- SimTracker/SiPixelDigitizer/src/RemapDetIdService.cc
+++ SimTracker/SiPixelDigitizer/src/RemapDetIdService.cc
@@ -4,6 +4,8 @@
 #include <fstream>
 
 #include <FWCore/ServiceRegistry/interface/ServiceMaker.h> // Required for DEFINE_FWK_SERVICE
+#include <FWCore/Framework/interface/Event.h>
+#include <SimGeneral/MixingModule/interface/PileUpEventPrincipal.h>
 #include <SimDataFormats/TrackingHit/interface/PSimHit.h>
 #include <DataFormats/Provenance/interface/ProcessHistoryRegistry.h>
 #include <DataFormats/Provenance/interface/Provenance.h>
@@ -16,6 +18,9 @@ namespace simtracker { namespace services { DEFINE_FWK_SERVICE( RemapDetIdServic
 simtracker::services::RemapDetIdService::RemapDetIdService( const edm::ParameterSet& parameterSet, edm::ActivityRegistry& activityRegister )
 {
 	inputCollectionNames_=parameterSet.getParameter<std::vector<edm::InputTag> >( "inputCollections" );
+	// Put in a null pointer to a collection for each of the InputTags
+	for( const auto& tag : inputCollectionNames_ ) remappedCollections_.push_back( std::make_pair(tag,std::unique_ptr<std::vector<PSimHit> >()) );
+
 	cmsswVersionsToRemap_=parameterSet.getParameter<std::vector<std::string> >( "versionsToRemap" );
 
 	edm::FileInPath remapFilename=parameterSet.getParameter<edm::FileInPath>( "mapFilename" );
@@ -34,6 +39,91 @@ simtracker::services::RemapDetIdService::RemapDetIdService( const edm::Parameter
 
 simtracker::services::RemapDetIdService::~RemapDetIdService()
 {
+}
+
+template<class T> bool simtracker::services::RemapDetIdService::getByLabel_( T& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle )
+{
+	edm::Handle<std::vector<PSimHit> > hInputCollection;
+
+	if( !event.getByLabel( inputTag, hInputCollection ) )
+	{
+		handle=hInputCollection;
+		return false;
+	}
+
+	// See if I should remap this collection
+	std::vector<PSimHit>* pRemappedCollection=nullptr;
+	for( auto& tagCollectionPointerPair : remappedCollections_ )
+	{
+		const auto& configuredTag=tagCollectionPointerPair.first;
+		if( inputTag==configuredTag )
+		{
+			// The service has been configured to remap this collection. Also need to check
+			// which version of CMSSW was used to create it.
+			std::string cmsswVersion=cmsswVersionForProduct( hInputCollection );
+			// Use "find" rather than absolute equality in case the string contains
+			// e.g. "_patch3". I only want to remap the collection if it was made with
+			// a version of CMSSW before SLHC13.
+			if( cmsswVersion.find("CMSSW_6_2_0_SLHC11")!=std::string::npos || cmsswVersion.find("CMSSW_6_2_0_SLHC12")!=std::string::npos )
+			{
+				// This collection should be remapped. I'll reset the collection and
+				// set pRemappedCollection to the new one so that I know later to remap
+				auto& collectionPointer=tagCollectionPointerPair.second;
+				collectionPointer.reset( new std::vector<PSimHit>() );
+				pRemappedCollection=collectionPointer.get();
+			}
+		} // end of "if inputTag matches one set in the configuration"
+	} // end of loop over InputTags
+
+	// If I don't need to perform the remapping then pRemappedCollection will be null
+	if( pRemappedCollection==nullptr )
+	{
+		handle=hInputCollection;
+		return true;
+	}
+
+	edm::LogInfo("RemapDetIdService") << "Remapping the DetIds for collection " << inputTag;
+
+	for( const auto& simHit : *hInputCollection )
+	{
+		uint32_t oldDetId=simHit.detUnitId();
+
+		std::map<uint32_t,uint32_t>::const_iterator iFindResult=detIdMap_.find( oldDetId );
+		if( iFindResult!=detIdMap_.end() ) // If the DetId is in the last of mappings
+		{
+			uint32_t newDetId=iFindResult->second;
+
+			LogDebug("RemapDetIdService") << "    Modifying " << oldDetId << " to " << newDetId;
+			PSimHit modifiedSimHit( simHit.entryPoint(), simHit.exitPoint(),
+					simHit.pabs(), simHit.tof(), simHit.energyLoss(),
+					simHit.particleType(), newDetId, simHit.trackId(),
+					simHit.thetaAtEntry(), simHit.phiAtEntry(), simHit.processType() );
+
+			modifiedSimHit.setEventId( simHit.eventId() );
+
+			pRemappedCollection->push_back( std::move(modifiedSimHit) );
+		}
+		else // the DetId does not need to be remapped
+		{
+			LogDebug("RemapDetIdService") << "    Leaving " << oldDetId << " alone";
+			pRemappedCollection->push_back( simHit );
+		}
+
+	} // end of loop over simhits
+
+	handle=edm::Handle<std::vector<PSimHit> >( pRemappedCollection, hInputCollection.provenance() );
+	return true;
+
+}
+
+bool simtracker::services::RemapDetIdService::getByLabel( const edm::Event& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle )
+{
+	return getByLabel_( event, inputTag, handle );
+}
+
+bool simtracker::services::RemapDetIdService::getByLabel( const PileUpEventPrincipal& event, const edm::InputTag& inputTag, edm::Handle<std::vector<PSimHit> >& handle )
+{
+	return getByLabel_( event, inputTag, handle );
 }
 
 template<class T> std::string simtracker::services::RemapDetIdService::cmsswVersionForProduct( const edm::Handle<T>& handle )
