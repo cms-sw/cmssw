2d2e90f5255b18fbf453d8ad18b3a2673d121c31
Matthias <mkomm@cern.ch>
2014-07-09 17:26:09 +0200

    added B layer configuration from FullSim

--- FastSimulation/Tracking/python/IterativeMixedTripletStep_cff.py
+++ FastSimulation/Tracking/python/IterativeMixedTripletStep_cff.py
@@ -31,11 +31,9 @@ iterativeMixedTripletStepSeeds.newSyntax = True
 #                                            'BPix1+BPix2+FPix1_neg',
 #                                            'BPix1+FPix1_pos+FPix2_pos',
 #                                            'BPix1+FPix1_neg+FPix2_neg']
-from RecoTracker.IterativeTracking.MixedTripletStep_cff import mixedTripletStepSeedLayersA
-iterativeMixedTripletStepSeeds.layerList = mixedTripletStepSeedLayersA.layerList
-
-print "list A"
-print mixedTripletStepSeedLayersA.layerList
+from RecoTracker.IterativeTracking.MixedTripletStep_cff import mixedTripletStepSeedLayersA,mixedTripletStepSeedLayersB
+# combine both (A&B); Note: in FullSim, different cuts are applied for A & B seeds; in FastSim cuts are tuned (no need to corresponded to FullSim values)
+iterativeMixedTripletStepSeeds.layerList = mixedTripletStepSeedLayersA.layerList+mixedTripletStepSeedLayersB.layerList
 
 # candidate producer
 #from FastSimulation.Tracking.IterativeThirdCandidateProducer_cff import *
