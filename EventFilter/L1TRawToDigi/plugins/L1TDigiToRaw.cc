40c297e512b78976110c7b073c008e69c4af7b33
Matthias Wolf <matthias.wolf@cern.ch>
2014-07-10 16:52:10 -0500

    Move the plugin headers into the source files.

--- EventFilter/L1TRawToDigi/plugins/L1TDigiToRaw.cc
+++ EventFilter/L1TRawToDigi/plugins/L1TDigiToRaw.cc
@@ -1,13 +1,74 @@
+// -*- C++ -*-
+//
+// Package:    EventFilter/L1TRawToDigi
+// Class:      L1TDigiToRaw
+// 
+/**\class L1TDigiToRaw L1TDigiToRaw.cc EventFilter/L1TRawToDigi/plugins/L1TDigiToRaw.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Matthias Wolf
+//         Created:  Mon, 10 Feb 2014 14:29:40 GMT
+//
+//
+
+// system include files
+#include <iomanip>
+#include <memory>
+
+// user include files
 #include "DataFormats/FEDRawData/interface/FEDNumbering.h"
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
 #include "DataFormats/FEDRawData/interface/FEDHeader.h"
 #include "DataFormats/FEDRawData/interface/FEDTrailer.h"
 
-#include "EventFilter/L1TRawToDigi/interface/L1TDigiToRaw.h"
-
 #include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Utilities/interface/CRC16.h"
 
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "EventFilter/L1TRawToDigi/interface/PackerFactory.h"
+
+namespace l1t {
+   class L1TDigiToRaw : public edm::one::EDProducer<edm::one::SharedResources, edm::one::WatchRuns, edm::one::WatchLuminosityBlocks> {
+      public:
+         explicit L1TDigiToRaw(const edm::ParameterSet&);
+         ~L1TDigiToRaw();
+
+         static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+         using edm::one::EDProducer<edm::one::SharedResources, edm::one::WatchRuns, edm::one::WatchLuminosityBlocks>::consumes;
+
+      private:
+         virtual void beginJob() override;
+         virtual void produce(edm::Event&, const edm::EventSetup&) override;
+         virtual void endJob() override;
+
+         virtual void beginRun(edm::Run const&, edm::EventSetup const&) override {};
+         virtual void endRun(edm::Run const&, edm::EventSetup const&) override {};
+         virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override {};
+         virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override {};
+
+         // ----------member data ---------------------------
+         // FIXME is actually fixed by the firmware version
+         static const unsigned MAX_BLOCKS = 256;
+
+         edm::InputTag inputLabel_;
+         int fedId_;
+         unsigned fwId_;
+
+         PackerList packers_;
+   };
 #include <iomanip>
 
 namespace l1t {
