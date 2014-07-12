26f22948979b5cbdf70fc35fba155b9cbfae2314
Jason <jason.lee@cern.ch>
2014-07-13 02:59:27 +0900

    fireworks-debugging me0

--- Fireworks/Muons/plugins/FWME0SegmentProxyBuilder.cc
+++ Fireworks/Muons/plugins/FWME0SegmentProxyBuilder.cc
@@ -1,6 +1,7 @@
 #include "TEveGeoNode.h"
 #include "TEveStraightLineSet.h"
 #include "TGeoArb8.h"
+#include "TEvePointSet.h"
 
 #include "Fireworks/Core/interface/FWSimpleProxyBuilderTemplate.h"
 #include "Fireworks/Core/interface/FWEventItem.h"
@@ -40,41 +41,67 @@ FWME0SegmentProxyBuilder::build( const ME0Segment& iData,
   
   TEveStraightLineSet* segmentSet = new TEveStraightLineSet();
   // FIXME: This should be set elsewhere.
-  segmentSet->SetLineWidth( 3 );
+  segmentSet->SetLineWidth( 5 );
   setupAddElement( segmentSet, &oItemHolder );
 
   TEveGeoShape* shape = item()->getGeom()->getEveShape( rawid );
-  if( TGeoTrap* trap = dynamic_cast<TGeoTrap*>( shape->GetShape())) // Trapezoidal
+  if( shape ) 
   {
-     LocalPoint pos = iData.localPosition();
-     LocalVector dir = iData.localDirection();   
-     LocalVector unit = dir.unit();
+    if( TGeoBBox* box = dynamic_cast<TGeoBBox*>( shape->GetShape()))
+    {
+      LocalPoint pos = iData.localPosition();
+      LocalVector dir = iData.localDirection();   
+      LocalVector unit = dir.unit();
     
-     Double_t localPosition[3]     = {  pos.x(),  pos.y(),  pos.z() };
-     Double_t localDirectionIn[3]  = {  dir.x(),  dir.y(),  dir.z() };
-     Double_t localDirectionOut[3] = { -dir.x(), -dir.y(), -dir.z() };
-  
-     float distIn = trap->DistFromInside( localPosition, localDirectionIn );
-     float distOut = trap->DistFromInside( localPosition, localDirectionOut );
-     LocalVector vIn = unit * distIn;
-     LocalVector vOut = -unit * distOut;
-     float localSegmentInnerPoint[3] = { static_cast<float>(localPosition[0] + vIn.x()),
-					 static_cast<float>(localPosition[1] + vIn.y()),
-					 static_cast<float>(localPosition[2] + vIn.z()) 
-     };
+      double localPosition[3]     = {  pos.x(),  pos.y(),  pos.z() };
+      double localDirectionIn[3]  = {  dir.x(),  dir.y(),  dir.z() };
+      double localDirectionOut[3] = { -dir.x(), -dir.y(), -dir.z() };
+
+      Double_t distIn = box->DistFromInside( localPosition, localDirectionIn );
+      Double_t distOut = box->DistFromInside( localPosition, localDirectionOut );
+      LocalVector vIn = unit * distIn;
+      LocalVector vOut = -unit * distOut;
+      float localSegmentInnerPoint[3] = { static_cast<float>(localPosition[0] + vIn.x()),
+					  static_cast<float>(localPosition[1] + vIn.y()),
+					  static_cast<float>(localPosition[2] + vIn.z()) 
+      };
       
-     float localSegmentOuterPoint[3] = { static_cast<float>(localPosition[0] + vOut.x()),
-					 static_cast<float>(localPosition[1] + vOut.y()),
-					 static_cast<float>(localPosition[2] + vOut.z()) 
-     };
+      float localSegmentOuterPoint[3] = { static_cast<float>(localPosition[0] + vOut.x()),
+					  static_cast<float>(localPosition[1] + vOut.y()),
+					  static_cast<float>(localPosition[2] + vOut.z()) 
+      };
+                                   
+      float globalSegmentInnerPoint[3];
+      float globalSegmentOuterPoint[3];
 
-     float globalSegmentInnerPoint[3];
-     float globalSegmentOuterPoint[3];
-     
-     geom->localToGlobal( rawid, localSegmentInnerPoint,  globalSegmentInnerPoint, localSegmentOuterPoint,  globalSegmentOuterPoint );
+      geom->localToGlobal( rawid, localSegmentInnerPoint,  globalSegmentInnerPoint, localSegmentOuterPoint,  globalSegmentOuterPoint );
 
-     segmentSet->AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
-			  globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );	
+
+      std::cout << "rawid "<<   rawid <<std::endl;
+      std::cout << "debugging:: localSegmentInnerPoint "<<   localSegmentInnerPoint[0]<<", "<< localSegmentInnerPoint[1]<<", " << localSegmentInnerPoint[2] <<std::endl;
+      std::cout << "debugging:: localSegmentOuterPoint "<<   localSegmentOuterPoint[0]<<", "<< localSegmentOuterPoint[1]<<", " << localSegmentOuterPoint[2] <<std::endl;
+      std::cout << "debugging:: globalSegmentInnerPoint "<<   globalSegmentInnerPoint[0]<<", "<< globalSegmentInnerPoint[1]<<", " << globalSegmentInnerPoint[2] <<std::endl;
+
+      segmentSet->AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
+			   globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );
+
+      
+      // Draw hits included in the segment
+      TEvePointSet* pointSet = new TEvePointSet;
+      // FIXME: This should be set elsewhere.
+      pointSet->SetMarkerSize(0.5);
+      pointSet->SetMarkerColor(1);
+      setupAddElement( pointSet, &oItemHolder );
+      auto recHits = iData.specificRecHits();
+      for (auto rh = recHits.begin(); rh!= recHits.end(); rh++){
+	auto me0id = rh->me0Id();
+	LocalPoint hpos = rh->localPosition();
+	float hitLocalPos[3]= {hpos.x(), hpos.y(), hpos.z()};
+	float hitGlobalPoint[3];
+	geom->localToGlobal(me0id, hitLocalPos, hitGlobalPoint);
+	pointSet->SetNextPoint(hitGlobalPoint[0], hitGlobalPoint[1], hitGlobalPoint[2]);
+      }
+    }
   }
 }
 
