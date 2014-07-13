f13d2bd37bf22e9653352bdb884e97a9bdc0e53d
Lindsey Gray <lindsey.gray@gmail.com>
2014-07-13 18:36:38 +0200

    insert egmGsfElectronIDs into standard reconstruction sequences

--- RecoEgamma/Configuration/python/RecoEgamma_cff.py
+++ RecoEgamma/Configuration/python/RecoEgamma_cff.py
@@ -14,6 +14,8 @@ from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 from RecoEgamma.EgammaHFProducers.hfEMClusteringSequence_cff import *
 from TrackingTools.Configuration.TrackingTools_cff import *
 
+from EgammaAnalysis.ElectronTools.egmGsfElectronIDs_cff import *
+
 #importing new gedGsfElectronSequence :
 #from RecoEgamma.EgammaElectronProducers.gedGsfElectronSequence_cff import *
 from RecoEgamma.EgammaElectronProducers.pfBasedElectronIso_cff import *
@@ -25,11 +27,11 @@ egammaHighLevelRecoPrePF = cms.Sequence(gsfEcalDrivenElectronSequence*uncleanedO
 #egammaHighLevelRecoPostPF = cms.Sequence(gsfElectronMergingSequence*interestingEgammaIsoDetIds*photonIDSequence*eIdSequence*hfEMClusteringSequence)
 #adding new gedGsfElectronSequence and gedPhotonSequence :
 #egammaHighLevelRecoPostPF = cms.Sequence(gsfElectronMergingSequence*gedGsfElectronSequence*interestingEgammaIsoDetIds*gedPhotonSequence*photonIDSequence*eIdSequence*hfEMClusteringSequence)
-egammaHighLevelRecoPostPF = cms.Sequence(interestingEgammaIsoDetIds*photonIDSequence*photonIDSequenceGED*eIdSequence*hfEMClusteringSequence)
+egammaHighLevelRecoPostPF = cms.Sequence(interestingEgammaIsoDetIds*photonIDSequence*photonIDSequenceGED*eIdSequence*egmGsfElectronIDs*hfEMClusteringSequence)
 
 
-egammarecoFull = cms.Sequence(egammareco*interestingEgammaIsoDetIds*photonIDSequence*eIdSequence*hfEMClusteringSequence)
-egammarecoWithID = cms.Sequence(egammareco*photonIDSequence*eIdSequence)
+egammarecoFull = cms.Sequence(egammareco*interestingEgammaIsoDetIds*photonIDSequence*eIdSequence*egmGsfElectronIDs*hfEMClusteringSequence)
+egammarecoWithID = cms.Sequence(egammareco*photonIDSequence*eIdSequence*egmGsfElectronIDs)
 egammareco_woConvPhotons = cms.Sequence(electronSequence*photonSequence)
 egammareco_withIsolation = cms.Sequence(egammareco*egammaIsolationSequence)
 egammareco_withIsolation_woConvPhotons = cms.Sequence(egammareco_woConvPhotons*egammaIsolationSequence)
