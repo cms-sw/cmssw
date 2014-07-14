cda9d7000860bbda88f783a1b48730de7799a328
Lindsey Gray <lindsey.gray@gmail.com>
2014-07-14 19:21:12 +0200

    add in cuts that consume collections

---
+++ EgammaAnalysis/ElectronTools/plugins/cuts/GsfEleDxyCut.cc
@@ -0,0 +1,54 @@
+#include "PhysicsTools/SelectorUtils/interface/CutApplicatorWithEventContentBase.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+
+class GsfEleDxyCut : public CutApplicatorWithEventContentBase {
+public:
+  GsfEleDxyCut(const edm::ParameterSet& c);
+  
+  result_type operator()(const reco::GsfElectron&) const override final;
+
+  void setConsumes(edm::ConsumesCollector&) override final;
+  void getEventContent(const edm::EventBase&) override final;
+
+  CandidateType candidateType() const override final { 
+    return ELECTRON; 
+  }
+
+private:  
+  const double _dxyCutValue;
+  edm::Handle<reco::VertexCollection> _vtxs;
+};
+
+DEFINE_EDM_PLUGIN(CutApplicatorFactory,
+		  GsfEleDxyCut,
+		  "GsfEleDxyCut");
+
+GsfEleDxyCut::GsfEleDxyCut(const edm::ParameterSet& c) :
+  CutApplicatorWithEventContentBase(c),
+  _dxyCutValue(c.getParameter<double>("dxyCutValue")){
+  edm::InputTag vertextag = c.getParameter<edm::InputTag>("vertexSrc");
+  contentTags_.emplace("vertices",vertextag);
+}
+
+void GsfEleDxyCut::setConsumes(edm::ConsumesCollector& cc) {
+  auto vtcs = 
+    cc.consumes<reco::VertexCollection>(contentTags_["vertices"]);
+  contentTokens_.emplace("vertices",vtcs);
+}
+
+void GsfEleDxyCut::getEventContent(const edm::EventBase& ev) {    
+  ev.getByLabel(contentTags_["vertices"],_vtxs);
+}
+
+CutApplicatorBase::result_type 
+GsfEleDxyCut::
+operator()(const reco::GsfElectron& cand) const{  
+  const reco::VertexCollection& vtxs = *_vtxs;
+  const double dxy = ( vtxs.size() ? 
+		       cand.gsfTrack()->dxy(vtxs[0].position()) : 
+		       cand.gsfTrack()->dxy() );
+  return dxy < _dxyCutValue;
+}
