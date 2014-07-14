6e64d654ce5e70851fcdf44f40eb6a784d19607d
Lindsey Gray <lindsey.gray@gmail.com>
2014-07-14 16:42:59 +0200

    adding electron cuts

---
+++ EgammaAnalysis/ElectronTools/plugins/cuts/GsfEleHadronicOverEMCut.cc
@@ -0,0 +1,29 @@
+#include "PhysicsTools/SelectorUtils/interface/CutApplicatorBase.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+
+class GsfEleHadronicOverEMCut : public CutApplicatorBase {
+public:
+  GsfEleHadronicOverEMCut(const edm::ParameterSet& c) :
+    CutApplicatorBase(c),
+    _hadronicOverEMCutValue(c.getParameter<double>("hadronicOverEMCutValue")) {
+  }
+  
+  result_type operator()(const reco::GsfElectron&) const override final;
+
+  CandidateType candidateType() const override final { 
+    return ELECTRON; 
+  }
+
+private:
+  const float _hadronicOverEMCutValue;  
+};
+
+DEFINE_EDM_PLUGIN(CutApplicatorFactory,
+		  GsfEleHadronicOverEMCut,
+		  "GsfEleHadronicOverEMCut");
+
+CutApplicatorBase::result_type 
+GsfEleHadronicOverEMCut::
+operator()(const reco::GsfElectron& cand) const{ 
+  return cand.hadronicOverEm() < _hadronicOverEMCutValue;
+}
