1e86532a0d385f21e057568cc8f50088c1106b9b
Lindsey Gray <lindsey.gray@gmail.com>
2014-07-16 07:35:04 +0200

    finishing off isolation cone

---
+++ PhysicsTools/IsolationAlgos/src/CITKIsolationConeDefinitionBase.cc
@@ -0,0 +1,4 @@
+#include "PhysicsTools/IsolationAlgos/interface/CITKIsolationSumCalculator.h"
+
+EDM_REGISTER_PLUGINFACTORY(CITKIsolationConeDefinitionFactory,
+			   "CITKIsolationConeDefinitionFactory");
