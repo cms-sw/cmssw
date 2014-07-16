87967255065618ae555601f45425ed55ee3e6b40
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2014-07-16 12:00:41 -0700

    don't reset colorsat the end.

--- Fireworks/Geometry/src/FWTGeoRecoGeometryESProducer.cc
+++ Fireworks/Geometry/src/FWTGeoRecoGeometryESProducer.cc
@@ -146,7 +146,7 @@ FWTGeoRecoGeometryESProducer::produce( const FWTGeoRecoGeometryRecord& record )
 
   addCaloGeometry();
   
-  geom->CloseGeometry();
+
   geom->DefaultColors();
 
   m_nameToShape.clear();
