12b631b5be321f3e6d3f5e1ea90e4965d21adcc4
matthieu <matthieu.marionneau@cern.ch>
2014-07-17 15:16:59 +0200

    fix miniAOD configuration for the met uncertainty tool, v3

--- PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
+++ PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
@@ -85,7 +85,7 @@ def miniAOD_customizeCommon(process):
     #
     # apply type I/type I + II PFMEt corrections to pat::MET object
     # and estimate systematic uncertainties on MET
-
+    from PhysicsTools.PatUtils.tools.runType1PFMEtUncertainties import runType1PFMEtUncertainties
     addJetCollection(process, postfix   = "ForMetUnc", labelName = 'AK5PF', jetSource = cms.InputTag('ak5PFJets'), jetCorrections = ('AK5PF', ['L1FastJet', 'L2Relative', 'L3Absolute'], ''))
     runType1PFMEtUncertainties(process,addToPatDefaultSequence=False,jetCollection="selectedPatJetsAK5PFForMetUnc", outputModule=None)
 
