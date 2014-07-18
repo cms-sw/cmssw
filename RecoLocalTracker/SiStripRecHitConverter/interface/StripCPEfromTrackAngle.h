e95c37c295c228c5f8c7a50e06e61a0432913b74
Nick McColl <nickmccoll@gmail.com>
2014-07-18 13:48:50 +0200

    Code improvements from Vincenzo

--- RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngle.h
+++ RecoLocalTracker/SiStripRecHitConverter/interface/StripCPEfromTrackAngle.h
@@ -1,6 +1,7 @@
 #ifndef RecoLocalTracker_SiStripRecHitConverter_StripCPEfromTrackAngle_H
 #define RecoLocalTracker_SiStripRecHitConverter_StripCPEfromTrackAngle_H
 
+#include <map>
 #include "RecoLocalTracker/SiStripRecHitConverter/interface/StripCPE.h"
 
 class StripCPEfromTrackAngle : public StripCPE 
@@ -10,24 +11,18 @@ class StripCPEfromTrackAngle : public StripCPE
   using StripCPE::localParameters;
   
   //Error parameterization, low cluster width function
-  const double LC_P0;
-  const double LC_P1;
-  const double LC_P2;
+  const float LC_P0;
+  const float LC_P1;
+  const float LC_P2;
 
   //High cluster width is broken down by sub-det
-  const double TIB_P0;
-  const double TIB_P1;
-  const double TOB_P0;
-  const double TOB_P1;
-  const double TID_P0;
-  const double TID_P1;
-  const double TEC_P0;
-  const double TEC_P1;
+  std::map<SiStripDetId::SubDetector, float> HC_P0;
+  std::map<SiStripDetId::SubDetector, float> HC_P1;
   
   StripClusterParameterEstimator::LocalValues
   localParameters( const SiStripCluster&, const GeomDetUnit&, const LocalTrajectoryParameters&) const;
   
-  float stripErrorSquared(const unsigned N, const float uProj, const int& loc ) const ;
+  float stripErrorSquared(const unsigned N, const float uProj, const SiStripDetId::SubDetector loc ) const ;
 
   StripCPEfromTrackAngle( edm::ParameterSet & conf, 
 			  const MagneticField& mag, 
@@ -39,15 +34,16 @@ class StripCPEfromTrackAngle : public StripCPE
   : StripCPE(conf, mag, geom, lorentz, backPlaneCorrection, confObj, latency ),
     LC_P0 (conf.getParameter<double>("LC_P0" )),
     LC_P1 (conf.getParameter<double>("LC_P1" )),
-    LC_P2 (conf.getParameter<double>("LC_P2" )),
-    TIB_P0(conf.getParameter<double>("TIB_P0")),
-    TIB_P1(conf.getParameter<double>("TIB_P1")),
-    TOB_P0(conf.getParameter<double>("TOB_P0")),
-    TOB_P1(conf.getParameter<double>("TOB_P1")),
-    TID_P0(conf.getParameter<double>("TID_P0")),
-    TID_P1(conf.getParameter<double>("TID_P1")),
-    TEC_P0(conf.getParameter<double>("TEC_P0")),
-    TEC_P1(conf.getParameter<double>("TEC_P1"))
-    {}  
+    LC_P2 (conf.getParameter<double>("LC_P2" ))
+  {
+    HC_P0.emplace(SiStripDetId::TIB,conf.getParameter<double>("TIB_P0"));
+    HC_P0.emplace(SiStripDetId::TOB,conf.getParameter<double>("TOB_P0"));
+    HC_P0.emplace(SiStripDetId::TID,conf.getParameter<double>("TID_P0"));
+    HC_P0.emplace(SiStripDetId::TEC,conf.getParameter<double>("TEC_P0"));
+    HC_P1.emplace(SiStripDetId::TIB,conf.getParameter<double>("TIB_P1"));
+    HC_P1.emplace(SiStripDetId::TOB,conf.getParameter<double>("TOB_P1"));
+    HC_P1.emplace(SiStripDetId::TID,conf.getParameter<double>("TID_P1"));
+    HC_P1.emplace(SiStripDetId::TEC,conf.getParameter<double>("TEC_P1"));
+  }
 };
 #endif
--- RecoLocalTracker/SiStripRecHitConverter/src/StripCPEfromTrackAngle.cc
+++ RecoLocalTracker/SiStripRecHitConverter/src/StripCPEfromTrackAngle.cc
@@ -3,24 +3,16 @@
 
 #include "vdt/vdtMath.h"
 
-float StripCPEfromTrackAngle::stripErrorSquared(const unsigned N, const float uProj, const int& loc ) const {
+float StripCPEfromTrackAngle::stripErrorSquared(const unsigned N, const float uProj, const SiStripDetId::SubDetector loc ) const {
+  if( loc == SiStripDetId::UNKNOWN)
+    throw cms::Exception("StripCPEfromTrackAngle::stripErrorSquared", "Incompatible sub-detector.");
+
   float uerr = 0;
   if(N <= 4)
     uerr = LC_P0*uProj*vdt::fast_expf(-uProj*LC_P1)+LC_P2;
-  else
-    switch(loc){
-    case SiStripDetId::TEC :
-      uerr = TEC_P0+uProj*TEC_P1; break;
-    case SiStripDetId::TID :
-      uerr = TID_P0+uProj*TID_P1; break;
-    case SiStripDetId::TOB :
-      uerr = TOB_P0+uProj*TOB_P1; break;
-    case SiStripDetId::TIB :
-      uerr = TIB_P0+uProj*TIB_P1; break;
-    default:
-      throw cms::Exception("StripCPEfromTrackAngle::stripErrorSquared", "Incompatible sub-detector.");
-      break;
-    }
+  else{
+    uerr = HC_P0.at(loc)+uProj*HC_P1.at(loc);
+  }
   return uerr*uerr;
 }
 
@@ -38,7 +30,7 @@ localParameters( const SiStripCluster& cluster, const GeomDetUnit& det, const Lo
 
   const unsigned N = cluster.amplitudes().size();
   const float fullProjection = p.coveredStrips( track+p.drift, ltp.position());
-  const float uerr2 = stripErrorSquared( N, std::abs(fullProjection),ssdid.subdetId() );
+  const float uerr2 = stripErrorSquared( N, std::abs(fullProjection),ssdid.subDetector() );
   const float strip = cluster.barycenter() -  0.5f*(1.f-p.backplanecorrection) * fullProjection
     + 0.5f*p.coveredStrips(track, ltp.position());
   
