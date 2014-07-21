fe2a1a56fd731a9852b351b7c1d80cdad8a8ead6
Bill Ford <wtford@colorado.edu>
2014-07-21 16:24:17 -0600

    Make associateHitbySimTrack a tracked param via existsAs

--- SimTracker/TrackerHitAssociation/src/TrackerHitAssociator.cc
+++ SimTracker/TrackerHitAssociation/src/TrackerHitAssociator.cc
@@ -26,7 +26,8 @@ using namespace edm;
 TrackerHitAssociator::TrackerHitAssociator(const edm::Event& e)  : 
   doPixel_( true ),
   doStrip_( true ), 
-  doTrackAssoc_( false ) {
+  doTrackAssoc_( false ),
+  assocHitbySimTrack_( false ) {
   //
   // Take by default all tracker SimHits
   //
@@ -56,8 +57,8 @@ TrackerHitAssociator::TrackerHitAssociator(const edm::Event& e)  :
 TrackerHitAssociator::TrackerHitAssociator(const edm::Event& e, const edm::ParameterSet& conf)  : 
   doPixel_( conf.getParameter<bool>("associatePixel") ),
   doStrip_( conf.getParameter<bool>("associateStrip") ),
-  doTrackAssoc_( conf.getParameter<bool>("associateRecoTracks") ),
-  assocHitbySimTrack_( conf.getUntrackedParameter<bool>("associateHitbySimTrack", false) ) {
+  doTrackAssoc_( conf.getParameter<bool>("associateRecoTracks") ) {
+  assocHitbySimTrack_ = conf.existsAs<bool>("associateHitbySimTrack") ? conf.getParameter<bool>("associateHitbySimTrack") : false;
   
   //if track association there is no need to access the input collections
   if(!doTrackAssoc_) {
@@ -186,8 +187,6 @@ std::vector<PSimHit> TrackerHitAssociator::associateHit(const TrackingRecHit & t
 	
       for(size_t i=0; i<simtrackid.size();i++) {
 	if(simHitid == simtrackid[i].first && simHiteid == simtrackid[i].second) {
-//      In replay mode the MixingModule doesn't seem to save the eventId.event(); test on bunchCrossing only.
-// 	if(simHitid == simtrackid[i].first && simHiteid.bunchCrossing() == simtrackid[i].second.bunchCrossing()) {
 // 	    	cout << "Associator ---> ID" << ihit.trackId() << " Simhit x= " << ihit.localPosition().x() 
 //                   << " y= " <<  ihit.localPosition().y() << " z= " <<  ihit.localPosition().x() << endl;
 	  // std::cout << " matches";
