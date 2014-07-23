dd090695d9da72e5086def8942560f35167b5a1f
ndaci <daci.nadir@gmail.com>
2014-07-23 18:02:52 +0200

    Fix call to input files.

--- HLTriggerOffline/Exotica/test/hltExoticaValidator_cfg.py
+++ HLTriggerOffline/Exotica/test/hltExoticaValidator_cfg.py
@@ -19,46 +19,56 @@ for i in range(0,len(sys.argv)):
 print "Using myinput="+myinput
 
 if   myinput=="ZEE" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/0A69FE2A-DAFD-E311-9FA2-00261894391C.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/4ACF05B1-ABFD-E311-BC8C-0026189438BC.root')
 
 elif myinput=="ZMM" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZMM_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/12E10345-F9FD-E311-9694-0026189438AE.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZMM_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/B25222BF-F9FD-E311-A217-003048FFCBA4.root')
 
 elif myinput=="ZpEE" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpEE_2250_13TeV_Tauola/GEN-SIM-RECO/POSTLS172_V1-v1/00000/343EDE59-F9FD-E311-898D-0025905A612A.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpEE_2250_13TeV_Tauola/GEN-SIM-RECO/POSTLS172_V1-v1/00000/E2A8B37B-FDFD-E311-8018-002618943849.root')
 
 elif myinput=="ZpMM" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpMM_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/80CB6E2E-0BFE-E311-B644-0025905A4964.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpMM_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/E0547611-00FE-E311-88AB-0025905A48F0.root')
 
+elif myinput=="PhotonJets" :
+    myfileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_7_2_0_pre1/RelValPhotonJets_Pt_10_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/6AD79C3F-F9FD-E311-AD0C-0025905A610A.root',
+        '/store/relval/CMSSW_7_2_0_pre1/RelValPhotonJets_Pt_10_13/GEN-SIM-RECO/POSTLS172_V1-v1/00000/F8CBE44F-F6FD-E311-8F8C-0025905964BC.root')
+    
 elif myinput=="ZEE_f" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/0C68F3E2-D300-E411-A2E1-003048FFCC0A.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/6A11D18F-D800-E411-A121-0026189438C4.root')
 
 elif myinput=="ZMM_f" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZMM_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/2200CE0D-C600-E411-BF5B-0025905A60B4.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZMM_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/8860B293-DB00-E411-A736-0025905A60D6.root')
 
 elif myinput=="ZpEE_f" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpMM_f" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpMM_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/88067900-C600-E411-B0D4-003048FFD76E.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZpMM_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/BA282D59-C900-E411-B969-0025905A6064.root')
 
+elif myinput=="PhotonJets_f" :
+    myfileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_7_2_0_pre1/RelValPhotonJets_Pt_10_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/90422F57-D900-E411-944B-002590593872.root',
+        '/store/relval/CMSSW_7_2_0_pre1/RelValPhotonJets_Pt_10_13/GEN-SIM-RECO/POSTLS172_V1_frozenHLT-v1/00000/E0DD9115-BE00-E411-8C70-00261894382D.root')
+
 # PU40 25ns
 elif   myinput=="ZEE_PU25ns" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1-v1/00000/00E2C10F-E6FD-E311-ACE3-0025905A60DA.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1-v1/00000/62F42186-91FD-E311-92E8-0025905A60EE.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1-v1/00000/78D08417-8DFD-E311-B022-0026189438D9.root',
@@ -66,16 +76,16 @@ elif   myinput=="ZEE_PU25ns" :
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1-v1/00000/E468EFE3-E7FD-E311-A9FF-0025905A6088.root')
 
 elif myinput=="ZMM_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpEE_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpMM_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZEE_f_PU25ns" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1_frozenHLT-v1/00000/0C0AFEDA-B600-E411-B890-0025905A6094.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1_frozenHLT-v1/00000/684B8352-CC00-E411-B019-0026189438B3.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1_frozenHLT-v1/00000/A872BF63-B800-E411-AC92-003048FFCBA8.root',
@@ -83,17 +93,17 @@ elif myinput=="ZEE_f_PU25ns" :
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU25ns_POSTLS172_V1_frozenHLT-v1/00000/F2B7DB47-B500-E411-88B3-0025905A613C.root')
     
 elif myinput=="ZMM_f_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpEE_f_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpMM_f_PU25ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 ## PU40 50ns
 elif myinput=="ZEE_PU50ns" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2-v1/00000/0213DA84-8AFD-E311-A2FB-0026189438FF.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2-v1/00000/10BE77F0-86FD-E311-A7F6-0026189438DF.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2-v1/00000/3410BEEC-E6FD-E311-9D13-0025905A60F4.root',
@@ -101,16 +111,16 @@ elif myinput=="ZEE_PU50ns" :
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2-v1/00000/C2BECB0F-89FD-E311-83DE-0025905B8610.root')
 
 elif myinput=="ZMM_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpEE_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpMM_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZEE_f_PU50ns" :
-    fileNames = cms.untracked.vstring(
+    myfileNames = cms.untracked.vstring(
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2_frozenHLT-v1/00000/02D5E5C6-BD00-E411-B421-00259059391E.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2_frozenHLT-v1/00000/2817C10B-D600-E411-B17C-002618943939.root',
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2_frozenHLT-v1/00000/547D27A4-C700-E411-8686-0025905A60EE.root',
@@ -119,14 +129,16 @@ elif myinput=="ZEE_f_PU50ns" :
         '/store/relval/CMSSW_7_2_0_pre1/RelValZEE_13/GEN-SIM-RECO/PU50ns_POSTLS172_V2_frozenHLT-v1/00000/FABFF406-C300-E411-AE96-002618943800.root')
 
 elif myinput=="ZMM_f_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpEE_f_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
 elif myinput=="ZpMM_f_PU50ns" :
-    fileNames = cms.untracked.vstring()
+    myfileNames = cms.untracked.vstring()
 
+print "### Files : "
+print myfileNames
 
 
 ##############################################################################
