688eeda24eba583d5f5ab7f27f69264d613e18b1
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2014-07-23 18:26:22 +0200

    Modified parameterTurnOn for low Pt muons

---
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaLowPtDimuon_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+LowPtDimuonPSet = cms.PSet(
+    hltPathsToCheck = cms.vstring(
+        "HLT_Mu17_Mu8_v",
+        "HLT_Mu17_TkMu8_v",
+        ),
+    recMuonLabel  = cms.InputTag("muons"),
+    # -- Analysis specific cuts
+    minCandidates = cms.uint32(2),
+    # -- Analysis specific binnings
+    #parametersTurnOn = cms.vdouble( 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
+    #                                60, 70, 80, 100, 120, 140, 160, 180, 200
+    parametersTurnOn = cms.vdouble( 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
+                                    60, 70, 80, 100
+                                   ),
+    )
