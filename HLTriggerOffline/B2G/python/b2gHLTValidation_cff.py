0f611db2b7e0ac73f99c6fec5a612b9d4ce3f162
Salvatore Rappoccio <rappoccio@gmail.com>
2014-07-23 15:17:41 -0500

    Fixing B2G DQM module sequence to point to the correct module

--- HLTriggerOffline/B2G/python/b2gHLTValidation_cff.py
+++ HLTriggerOffline/B2G/python/b2gHLTValidation_cff.py
@@ -1,11 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-from HLTriggerOffline.B2G.topDiLeptonHLTEventValidation_cfi import *
-from HLTriggerOffline.B2G.topSingleLeptonHLTEventValidation_cfi import *
-from HLTriggerOffline.B2G.singletopHLTEventValidation_cfi import *
+from HLTriggerOffline.B2G.b2gSingleLeptonHLTEventValidation_cfi import *
+
 
 b2gHLTriggerValidation = cms.Sequence(  
-    topSingleMuonHLTValidation
-    *topSingleElectronHLTValidation
+    b2gSingleMuonHLTValidation*
+    b2gSingleElectronHLTValidation
     )
 
