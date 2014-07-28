f83814e574dda2b28342a2efb31a5c1df48d2f9a
matthieu <matthieu.marionneau@cern.ch>
2014-07-28 13:45:57 +0200

    update pileupjetid configuration

--- RecoJets/JetProducers/python/PileupJetID_cfi.py
+++ RecoJets/JetProducers/python/PileupJetID_cfi.py
@@ -2,25 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoJets.JetProducers.PileupJetIDParams_cfi import * 
 
-_stdalgos_4x = cms.VPSet(full,   cutbased,PhilV1)
+#_stdalgos_4x = cms.VPSet(full,   cutbased,PhilV1)
 _stdalgos_5x = cms.VPSet(full_5x,cutbased,PhilV1)
 
-_chsalgos_4x = cms.VPSet(full,   cutbased)
+#_chsalgos_4x = cms.VPSet(full,   cutbased)
 _chsalgos_5x = cms.VPSet(full_5x_chs,cutbased)
 _chsalgos = _chsalgos_5x
 
-import os
-try:
-    cmssw_version = os.environ["CMSSW_VERSION"].replace("CMSSW_","")
-except:
-    cmssw_version = "5_X"
+#import os
+#try:
+#    cmssw_version = os.environ["CMSSW_VERSION"].replace("CMSSW_","")
+#except:
+#    cmssw_version = "5_X"
 
-if cmssw_version.startswith("4"):
-    _stdalgos    = _stdalgos_4x
-    _chsalgos    = _chsalgos_4x
-else:
-    _stdalgos    = _stdalgos_5x
-    _chsalgos    = _chsalgos_5x
+#if cmssw_version.startswith("4"):
+#    _stdalgos    = _stdalgos_4x
+#    _chsalgos    = _chsalgos_4x
+#else:
+_stdalgos    = _stdalgos_5x
+_chsalgos    = _chsalgos_5x
 
 pileupJetIdProducer = cms.EDProducer('PileupJetIdProducer',
                          produceJetIds = cms.bool(True),
