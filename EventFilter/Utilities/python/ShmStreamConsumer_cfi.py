85978ad9dacee961dc069b7dcf1685bfd04a14ad
Srecko Morovic <srecko.morovic@cern.ch>
2014-07-28 17:37:55 +0200

    cms.Service --> cms.OutputModule

--- EventFilter/Utilities/python/ShmStreamConsumer_cfi.py
+++ EventFilter/Utilities/python/ShmStreamConsumer_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-process.ShmStreamConsumer = cms.Service("ShmStreamConsumer",
+shmStreamConsumer = cms.OutputModule("ShmStreamConsumer",
     fileName = cms.untracked.string(""),
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring(),
         outputCommands = cms.untracked.vstring()
     )
 )
-
