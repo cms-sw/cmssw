0712b314475c91d10097b93873554963362bed2c
Leonardo Cristella <leonardo.cristella@cern.ch>
2014-07-29 20:38:42 +0200

    multi-thread safe porting

--- DQMOffline/PFTau/plugins/PFCandidateDQMAnalyzer.cc
+++ DQMOffline/PFTau/plugins/PFCandidateDQMAnalyzer.cc
@@ -29,25 +29,39 @@ PFCandidateDQMAnalyzer::PFCandidateDQMAnalyzer(const edm::ParameterSet& paramete
   
   myCand_ = consumes< edm::View<reco::Candidate> >(inputLabel_);
   myMatchedCand_ = consumes< edm::View<reco::Candidate> >(matchLabel_);
-}
-//
-// -- BeginJob
-//
-void PFCandidateDQMAnalyzer::beginJob() {
 
-  Benchmark::DQM_ = edm::Service<DQMStore>().operator->();
-  // part of the following could be put in the base class
-  std::string path = "ParticleFlow/" + benchmarkLabel_;
-  Benchmark::DQM_->setCurrentFolder(path.c_str());
-  edm::LogInfo("PFCandidateDQMAnalyzer") << " PFCandidateDQMAnalyzer::beginJob " << "Histogram Folder path set to "<< path;
-  pfCandidateMonitor_.setup(pSet_);  
+
+  std::string folder = benchmarkLabel_ ;
+
+  subsystemname_ = "ParticleFlow" ;
+  eventInfoFolder_ = subsystemname_ + "/" + folder ;
+
   nBadEvents_ = 0;
+
 }
+
+
+//
+// -- BookHistograms
+//
+void PFCandidateDQMAnalyzer::bookHistograms(DQMStore::IBooker & ibooker,
+					    edm::Run const & /* iRun */,
+					    edm::EventSetup const & /* iSetup */ )
+{
+  ibooker.setCurrentFolder(eventInfoFolder_) ;
+
+  edm::LogInfo("PFCandidateDQMAnalyzer") << " PFCandidateDQMAnalyzer::beginJob " << "Histogram Folder path set to " << eventInfoFolder_;
+  
+  //pfCandidateMonitor_.setup(pSet_);
+  pfCandidateMonitor_.setup(ibooker, pSet_);
+}
+
 //
 // -- Analyze
 //
 void PFCandidateDQMAnalyzer::analyze(edm::Event const& iEvent, 
-				      edm::EventSetup const& iSetup) {
+				     edm::EventSetup const& iSetup) {
+//void PFCandidateDQMAnalyzer::analyze(DQMStore::IBooker & ibooker, edm::Event const& iEvent, edm::EventSetup const& iSetup) {
   
   edm::Handle< edm::View<reco::Candidate> > candCollection;
   edm::Handle< edm::View<reco::Candidate> > matchedCandCollection;
@@ -64,39 +78,51 @@ void PFCandidateDQMAnalyzer::analyze(edm::Event const& iEvent,
   if (candCollection.isValid() && matchedCandCollection.isValid()) {
     //pfCandidateMonitor_.fill( *candCollection, *matchedCandCollection, minRes, maxRes);
     pfCandidateMonitor_.fill( *candCollection, *matchedCandCollection, minRes, maxRes, pSet_);
-
+    
+    /*
     edm::ParameterSet skimPS = pSet_.getParameter<edm::ParameterSet>("SkimParameter");
     if ( (skimPS.getParameter<bool>("switchOn")) &&  
          (nBadEvents_ <= skimPS.getParameter<int32_t>("maximumNumberToBeStored")) ) {
       if ( minRes < skimPS.getParameter<double>("lowerCutOffOnResolution")) {
 	nBadEvents_++; 
-	storeBadEvents(iEvent,minRes);
+	//storeBadEvents(iEvent, minRes);
+	storeBadEvents(ibooker, iEvent, minRes);
       }	else if (maxRes > skimPS.getParameter<double>("upperCutOffOnResolution")) {
 	nBadEvents_++;
-	storeBadEvents(iEvent,maxRes);
+	//storeBadEvents(iEvent, maxRes);
+	storeBadEvents(ibooker, iEvent, minRes);
       }
     }
-
+    */
   }
 }
-void PFCandidateDQMAnalyzer::storeBadEvents(edm::Event const& iEvent, float& val) {
+
+//void PFCandidateDQMAnalyzer::storeBadEvents(edm::Event const& iEvent, float& val) {
+void PFCandidateDQMAnalyzer::storeBadEvents(DQMStore::IBooker & ibooker, edm::Event const& iEvent, float& val) {
   unsigned int runNb  = iEvent.id().run();
   unsigned int evtNb  = iEvent.id().event();
   unsigned int lumiNb = iEvent.id().luminosityBlock();
   
   std::string path = "ParticleFlow/" + benchmarkLabel_ + "/BadEvents";
-  Benchmark::DQM_->setCurrentFolder(path.c_str());
+  ibooker.setCurrentFolder(eventInfoFolder_) ;
   std::ostringstream eventid_str;
   eventid_str << runNb << "_"<< evtNb << "_" << lumiNb;
-  MonitorElement* me = Benchmark::DQM_->get(path + "/" + eventid_str.str());
+
+  //MonitorElement* me = Benchmark::DQM_->get(path + "/" + eventid_str.str());
+  /*  
+  MonitorElement* me = ibooker.get(path + "/" + eventid_str.str());
   if (me) me->Reset();
-  else me = Benchmark::DQM_->bookFloat(eventid_str.str());
-  me->Fill(val);  
+  else {
+    //me = Benchmark::DQM_->bookFloat(eventid_str.str());
+    me = ibooker.bookFloat(eventid_str.str());
+  }  
+  me->Fill(val); 
+  */
+  MonitorElement* me = ibooker.bookFloat(eventid_str.str());
+  me->Fill(val); 
+
 }
-//
-// -- EndJob
-// 
-void PFCandidateDQMAnalyzer::endJob() {
-}
+
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE (PFCandidateDQMAnalyzer) ;
