61f1d623951c993df33f302a02173bc1ad05246d
ndaci <daci.nadir@gmail.com>
2014-07-30 18:38:24 +0200

    Update thresholds.

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaHighPtPhoton_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaHighPtPhoton_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 HighPtPhotonPSet = cms.PSet(
     hltPathsToCheck = cms.vstring(
-        "HLT_Photon80_v",  # Run2 proposal
+        "HLT_Photon155_v",  # Run2 proposal
         "HLT_Photon135_v"  # Run1 (frozenHLT)
         ),
     recPhotonLabel  = cms.InputTag("gedPhotons"),
