e85b9a4ac52449b69191c8edcbfe3f9b9b841618
ndaci <daci.nadir@gmail.com>
2014-07-30 18:38:52 +0200

    Indent

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaJetNoBptx_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaJetNoBptx_cff.py
@@ -5,8 +5,10 @@ JetNoBptxPSet = cms.PSet(
         "HLT_JetE50_NoBPTX3BX_NoHalo_v" # Run2 proposal AND Run1 (frozenHLT)
         ),
     recCaloJetLabel    = cms.InputTag("ak5CaloJets"),
+
     # -- Analysis specific cuts
     minCandidates = cms.uint32(1),
+
     # -- Analysis specific binnings
     parametersTurnOn = cms.vdouble( 0, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,
                                     160, 170, 180, 190, 200,
