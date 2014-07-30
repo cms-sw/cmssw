763c67508491bc941cac958c70ac4f4687912ca6
Arnaud <arnaud.chiron@cern.ch>
2014-07-30 11:16:00 +0200

    adding forgotten legends

--- Validation/RecoEgamma/plugins/ElectronMcSignalValidator.cc
+++ Validation/RecoEgamma/plugins/ElectronMcSignalValidator.cc
@@ -705,12 +705,12 @@ void ElectronMcSignalValidator::book()
   h2_ele_PhiMnPhiTrueVsEta = bookH2("PhiMnPhiTrueVsEta","ele momentum  phi - gen  phi vs eta",eta2D_nbin,eta_min,eta_max,dphi_nbin/2,dphi_min,dphi_max);
   h2_ele_PhiMnPhiTrueVsPhi = bookH2("PhiMnPhiTrueVsPhi","ele momentum  phi - gen  phi vs phi",phi2D_nbin,phi_min,phi_max,dphi_nbin/2,dphi_min,dphi_max);
   h2_ele_PhiMnPhiTrueVsPt = bookH2("PhiMnPhiTrueVsPt","ele momentum  phi - gen  phi vs pt",pt2D_nbin,0.,pt_max,dphi_nbin/2,dphi_min,dphi_max);
-  h1_ele_ecalEnergyError = bookH1withSumw2("ecalEnergyError","",error_nbin,0,enerror_max);
-  h1_ele_ecalEnergyError_barrel = bookH1withSumw2("ecalEnergyError_barrel","",30,0,30);
-  h1_ele_ecalEnergyError_endcaps = bookH1withSumw2("ecalEnergyError_endcaps","",error_nbin,0,enerror_max);
-  h1_ele_combinedP4Error = bookH1withSumw2("combinedP4Error","",error_nbin,0,enerror_max);
-  h1_ele_combinedP4Error_barrel = bookH1withSumw2("combinedP4Error_barrel","",30,0,30);
-  h1_ele_combinedP4Error_endcaps = bookH1withSumw2("combinedP4Error_endcaps","",error_nbin,0,enerror_max);
+  h1_ele_ecalEnergyError = bookH1withSumw2("ecalEnergyError","Regression estimate of the ECAL energy error",error_nbin,0,enerror_max);
+  h1_ele_ecalEnergyError_barrel = bookH1withSumw2("ecalEnergyError_barrel","Regression estimate of the ECAL energy error - barrel",30,0,30);
+  h1_ele_ecalEnergyError_endcaps = bookH1withSumw2("ecalEnergyError_endcaps","Regression estimate of the ECAL energy error - endcaps",error_nbin,0,enerror_max);
+  h1_ele_combinedP4Error = bookH1withSumw2("combinedP4Error","Estimated error on the combined momentum",error_nbin,0,enerror_max);
+  h1_ele_combinedP4Error_barrel = bookH1withSumw2("combinedP4Error_barrel","Estimated error on the combined momentum - barrel",30,0,30);
+  h1_ele_combinedP4Error_endcaps = bookH1withSumw2("combinedP4Error_endcaps","Estimated error on the combined momentum - endcaps",error_nbin,0,enerror_max);
 
   // matched electron, superclusters
   setBookPrefix("h_scl") ;
