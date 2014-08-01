20f0357a75600f086f31a787a978620572760a6e
Michail <bachtis@cern.ch>
2014-08-01 20:07:28 +0200

    Reverting HO to the old setup for compatibility

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHO_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHO_cfi.py
@@ -8,15 +8,15 @@ particleFlowRecHitHO = cms.EDProducer("PFRecHitProducer",
              name = cms.string("PFHORecHitCreator"),
              src  = cms.InputTag("horeco",""),
              qualityTests = cms.VPSet(
-#                  cms.PSet(
-#                  name = cms.string("PFRecHitQTestHOThreshold"),
-#                  threshold_ring0 = cms.double(0.4),
-#                  threshold_ring12 = cms.double(1.0)
-#                  ),
                   cms.PSet(
-                  name = cms.string("PFRecHitQTestThreshold"),
-                  threshold = cms.double(0.05) # new threshold for SiPM HO
+                    name = cms.string("PFRecHitQTestHOThreshold"),
+                    threshold_ring0 = cms.double(0.4),
+                    threshold_ring12 = cms.double(1.0)
                   ),
+#                  cms.PSet(
+#                  name = cms.string("PFRecHitQTestThreshold"),
+#                  threshold = cms.double(0.05) # new threshold for SiPM HO
+#                  ),
                   cms.PSet(
                       name = cms.string("PFRecHitQTestHCALChannel"),
                       maxSeverities      = cms.vint32(11),
