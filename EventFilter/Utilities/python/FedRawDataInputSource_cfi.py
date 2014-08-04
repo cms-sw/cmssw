b74030f9d50246b64ecb9727b3881f07261e479a
Srecko Morovic <srecko.morovic@cern.ch>
2014-08-04 15:56:43 +0200

    *read 32 bit event ID from GT/EVM or emulator FED *single-buffer mode fix for case when header does not fit

--- EventFilter/Utilities/python/FedRawDataInputSource_cfi.py
+++ EventFilter/Utilities/python/FedRawDataInputSource_cfi.py
@@ -2,9 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 source = cms.Source("FedRawDataInputSource",
     getLSFromFilename = cms.untracked.bool(True),
-    eventChunkSize = cms.untracked.uint32(128),
-    eventChunkBlock = cms.untracked.uint32(128),
-    numBuffers = cms.untracked.uint32(1),
+    eventChunkSize = cms.untracked.uint32(32),
+    eventChunkBlock = cms.untracked.uint32(32),
+    numBuffers = cms.untracked.uint32(2),
     verifyAdler32 = cms.untracked.bool(True)
     )
 
