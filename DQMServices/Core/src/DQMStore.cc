be059a9bef714c6e7dd80e7fd877011aa562f93b
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2014-08-05 09:59:35 +0200

    In DQMStore, save files encoded with ProtocolBuffer by explicitly setting permissions to 0666.

--- DQMServices/Core/src/DQMStore.cc
+++ DQMServices/Core/src/DQMStore.cc
@@ -2459,8 +2459,10 @@ void DQMStore::savePB(const std::string &filename,
   }
 
   int filedescriptor = ::open(filename.c_str(),
-			      O_WRONLY | O_CREAT | O_TRUNC,
-			      S_IRUSR | S_IWUSR);
+                              O_WRONLY | O_CREAT | O_TRUNC,
+                              S_IRUSR | S_IWUSR |
+                              S_IRGRP | S_IWGRP |
+                              S_IROTH | S_IWOTH);
   FileOutputStream file_stream(filedescriptor);
   GzipOutputStream::Options options;
   options.format = GzipOutputStream::GZIP;
