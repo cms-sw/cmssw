7c653bd5f855d147c567bd33ccc4f043f2c4201c
Yetkin <yetkin.yilmaz@cern.ch>
2014-08-06 15:54:19 +0200

    UE db object in progress

---
+++ CondFormats/HIObjects/interface/UETable.h
@@ -0,0 +1,8 @@
+#ifndef __UETable_h__
+#define __UETable_h__
+
+#include "CondFormats/Serialization/interface/Serializable.h"
+
+typedef std::vector<std::vector<std::vector<std::vector<std::vector<float> > > > > UETable;
+
+#endif
