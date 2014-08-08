1c1c6a6cd0cf0f7a84479283eac6b2202326a3e1
Giovanni <gpetruc@gmail.com>
2014-08-08 22:26:45 +0200

    JetMETCorrections/Type1MET: CorrectedPFMETProducer: carry over significance to corrected MET (https://hypernews.cern.ch/HyperNews/CMS/get/csa14/11.html)

--- JetMETCorrections/Type1MET/plugins/CorrectedPFMETProducer.cc
+++ JetMETCorrections/Type1MET/plugins/CorrectedPFMETProducer.cc
@@ -28,10 +28,12 @@ namespace CorrectedMETProducer_namespace
 
     reco::PFMET operator()(const reco::PFMET& rawMEt, const CorrMETData& correction) const
     {
-      return reco::PFMET(rawMEt.getSpecific(), 
+      reco::PFMET ret(rawMEt.getSpecific(), 
 			 correctedSumEt(rawMEt, correction), 
 			 correctedP4(rawMEt, correction), 
 			 rawMEt.vertex());
+      ret.setSignificanceMatrix(rawMEt.getSignificanceMatrix()); 
+      return ret;
     }
   };
 }
