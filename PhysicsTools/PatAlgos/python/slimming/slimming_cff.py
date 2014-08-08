563c7725b80c4868cdcc09483c03bebab43888ea
Giovanni <gpetruc@gmail.com>
2014-08-08 22:56:24 +0200

    MINIAOD event content: remove explicit process name from keep statements (in particular, the process names for l1 information were wrong, so the objects were not kept)

--- PhysicsTools/PatAlgos/python/slimming/slimming_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimming_cff.py
@@ -46,13 +46,13 @@ MicroEventContent = cms.PSet(
 
         'keep double_fixedGridRho*__*', 
 
-        'keep *_selectedPatTrigger_*_PAT',
-        'keep patPackedTriggerPrescales_patTrigger__PAT',
-        'keep *_l1extraParticles_*_HLT',
-        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_HLT',
+        'keep *_selectedPatTrigger_*_*',
+        'keep patPackedTriggerPrescales_patTrigger__*',
+        'keep *_l1extraParticles_*_*',
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
         'keep *_TriggerResults_*_HLT',
         'keep *_TriggerResults_*_PAT', # for MET filters
-	'keep patPackedCandidates_lostTracks_*_PAT',
+	'keep patPackedCandidates_lostTracks_*_*',
         'keep HcalNoiseSummary_hcalnoise__*',
     )
 )
