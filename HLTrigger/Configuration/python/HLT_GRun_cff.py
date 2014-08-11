728ca621a1baaa6c53169af7ef9b921944d5b8b8
Andrea <perrotta@cern.ch>
2014-08-12 00:01:35 +0200

    Fix two parameters in two Egamma filters in di-photon paths for HIG

--- HLTrigger/Configuration/python/HLT_GRun_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_7_1_1/GRun/V81 (CMSSW_7_1_6)
+# /dev/CMSSW_7_1_1/GRun/V82 (CMSSW_7_1_6)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_1_1/GRun/V81')
+  tableName = cms.string('/dev/CMSSW_7_1_1/GRun/V82')
 )
 
 HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
