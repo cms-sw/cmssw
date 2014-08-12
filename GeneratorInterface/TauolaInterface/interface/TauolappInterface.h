30b1758576e9af866e48a7d017dff5bdd9fce1aa
inugent <you@somedomain.com>
2014-08-12 18:46:21 +0200

    adding patch to correctly propogate decay length through decay

--- GeneratorInterface/TauolaInterface/interface/TauolappInterface.h
+++ GeneratorInterface/TauolaInterface/interface/TauolappInterface.h
@@ -47,7 +47,9 @@ namespace gen {
       void selectDecayByMDTAU();
       int selectLeptonic();
       int selectHadronic();
-      
+      bool isLastTauInChain(const HepMC::GenParticle* tau);
+      void setLifeTimeInDecays(HepMC::GenParticle* p,double vx, double vy, double vz, double vt);
+
       //
       static CLHEP::HepRandomEngine*           fRandomEngine;            
       std::vector<int>                         fPDGs;
