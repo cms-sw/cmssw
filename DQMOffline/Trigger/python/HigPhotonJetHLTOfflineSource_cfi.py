d492005a294a038922c53b17fc8b9b432a92b0ab
Xin Shi <shi210@purdue.edu>
2014-08-14 10:20:11 -0400

    clean up

--- DQMOffline/Trigger/python/HigPhotonJetHLTOfflineSource_cfi.py
+++ DQMOffline/Trigger/python/HigPhotonJetHLTOfflineSource_cfi.py
@@ -6,7 +6,6 @@ higPhotonJetHLTOfflineSource = cms.EDAnalyzer(
     hltProcessName = cms.string("HLT"),
     # HLT paths passing any one of these regular expressions will be included
     hltPathsToCheck = cms.vstring(
-        #"HLT_Photon135_v", # standard test 
         "HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_PFMET40_v",
         "HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_PFMET40_v",
         "HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_PFMET40_v",
@@ -28,29 +27,12 @@ higPhotonJetHLTOfflineSource = cms.EDAnalyzer(
         "HLT_Photon250_NoHE_VBF_v",
         "HLT_Photon300_NoHE_VBF_v",
     ),
-
-    ## Location of plots in DQM
-    dirname = cms.untracked.string("HLT/xshi"), 
-
-    verbose = cms.untracked.bool(True),
+    # Location of plots in DQM
+    dirname = cms.untracked.string("HLT/Higgs/PhotonJet"), 
+    verbose = cms.untracked.bool(False),
+    triggerResultsToken = cms.InputTag("TriggerResults"),
     caloJetsToken = cms.InputTag("ak4CaloJets"),
     pvToken = cms.InputTag("offlinePrimaryVertices"),
     photonsToken = cms.InputTag("gedPhotons"),
     pfMetToken = cms.InputTag("pfMet"),
-    
-    ## All input tags are specified in this pset for convenience
-    # inputTags = cms.PSet(
-    #     recoMuon       = cms.InputTag("muons"),
-    #     beamSpot       = cms.InputTag("offlineBeamSpot"),
-    #     offlinePVs     = cms.InputTag("offlinePrimaryVertices"),
-    #     triggerSummary = cms.InputTag("hltTriggerSummaryAOD"),
-    #     triggerResults = cms.InputTag("TriggerResults")
-    # ),
-    ## Both 1D and 2D plots use the binnings defined here
-    # binParams = cms.untracked.PSet(
-    #     ## parameters for fixed-width plots
-    #     NVertex    = cms.untracked.vdouble( 20,  1,   50),
-    # ),
-
-    
 )
