1f0d38eaf93eb431bf7f3d40576e24459c1d41f8
Lindsey Gray <lindsey.gray@gmail.com>
2014-08-18 18:41:06 +0200

    working on code clean up and rerouting of python stuff

--- EgammaAnalysis/ElectronTools/interface/VersionedGsfElectronSelector.h
+++ EgammaAnalysis/ElectronTools/interface/VersionedGsfElectronSelector.h
@@ -11,32 +11,6 @@
 
 #include <iostream>
 
-class VersionedGsfElectronSelector : public VersionedSelector<reco::GsfElectronRef> {
- public:
- VersionedGsfElectronSelector() : 
-  VersionedSelector<reco::GsfElectronRef>(),
-    initialized_(false) {}
-  
- VersionedGsfElectronSelector( const edm::ParameterSet& parameters );
-  
- void initialize( const edm::ParameterSet& parameters );
-
-  // Allow for multiple definitions of the cuts.
- bool operator()(const reco::GsfElectronRef&,pat::strbitset&); 
-#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
- bool operator()(const reco::GsfElectronRef&,
-		 edm::EventBase const&,
-		 pat::strbitset&) override final;
-#else
- bool operator()(const reco::GsfElectronRef&,
-		 edm::EventBase const&,
-		 pat::strbitset&);
-#endif
-
- using VersionedSelector<reco::GsfElectronRef>::operator();
- 
- private:
- bool initialized_; 
-};
+typedef VersionedSelector<reco::GsfElectronRef> VersionedGsfElectronSelector;
 
 #endif
