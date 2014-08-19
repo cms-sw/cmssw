91ca5420926f99bd23b3875e494a4daaac9154bb
Lindsey Gray <lindsey.gray@gmail.com>
2014-08-19 13:31:21 +0200

    finished making nice templates of things, work on common producer

--- EgammaAnalysis/ElectronTools/interface/VersionedGsfElectronSelector.h
+++ EgammaAnalysis/ElectronTools/interface/VersionedGsfElectronSelector.h
@@ -3,13 +3,7 @@
 
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
 #include "PhysicsTools/SelectorUtils/interface/VersionedSelector.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include <iostream>
 
 typedef VersionedSelector<reco::GsfElectronRef> VersionedGsfElectronSelector;
 
--- EgammaAnalysis/ElectronTools/interface/VersionedPatElectronSelector.h
+++ EgammaAnalysis/ElectronTools/interface/VersionedPatElectronSelector.h
@@ -2,13 +2,7 @@
 #define EgammaAnalysis_ElectronTools_interface_VersionedPatElectronSelector_h
 
 #include "DataFormats/PatCandidates/interface/Electron.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
 #include "PhysicsTools/SelectorUtils/interface/VersionedSelector.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include <iostream>
 
 typedef VersionedSelector<pat::ElectronRef> VersionedPatElectronSelector;
 
