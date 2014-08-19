d8a86cb699838eff29805bae5f00ee0d23a97ae6
Ilya Kravchenko <ikrav@unl.edu>
2014-08-19 13:16:38 +0200

    Improvements to validation scripts

--- TestElectronID/ElectronIDAnalyzer/test/runEleIDTest_CSA14_sc2.py
+++ TestElectronID/ElectronIDAnalyzer/test/runEleIDTest_CSA14_sc2.py
@@ -14,15 +14,15 @@ process.load("Configuration.StandardSequences.Reconstruction_cff")
 #    for CSA14 scenario 1: global tag is 'POSTLS170_V6::All'
 #    for CSA14 scenario 2: global tag is 'PLS170_V7AN1::All'
 #  as a rule, find the global tag in the DAS under the Configs for given dataset
-process.GlobalTag.globaltag = 'POSTLS170_V6::All'
+process.GlobalTag.globaltag = 'PLS170_V7AN1::All'
 
-process.load("TestElectronID/ElectronIDAnalyzer/DYJetsToLL_Sc1_AODSIM")
+process.load("TestElectronID/ElectronIDAnalyzer/DYJetsToLL_Sc2_AODSIM")
 
 process.load('EgammaAnalysis/ElectronTools/egmGsfElectronIDs_cff')
 
 process.wp1 = cms.EDAnalyzer('ElectronIDAnalyzer',
                              electrons = cms.InputTag("gedGsfElectrons"),
-                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-veto"),
+                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-PU20bx25-V0-standalone-veto"),
                              vertices = cms.InputTag("offlinePrimaryVertices"),
                              genparticles = cms.InputTag("genParticles"),
                              convcollection = cms.InputTag("conversions"),
@@ -32,7 +32,7 @@ process.wp1 = cms.EDAnalyzer('ElectronIDAnalyzer',
 
 process.wp2 = cms.EDAnalyzer('ElectronIDAnalyzer',
                              electrons = cms.InputTag("gedGsfElectrons"),
-                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-loose"),
+                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-PU20bx25-V0-standalone-loose"),
                              vertices = cms.InputTag("offlinePrimaryVertices"),
                              genparticles = cms.InputTag("genParticles"),
                              convcollection = cms.InputTag("conversions"),
@@ -42,7 +42,7 @@ process.wp2 = cms.EDAnalyzer('ElectronIDAnalyzer',
 
 process.wp3 = cms.EDAnalyzer('ElectronIDAnalyzer',
                              electrons = cms.InputTag("gedGsfElectrons"),
-                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-medium"),
+                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-PU20bx25-V0-standalone-medium"),
                              vertices = cms.InputTag("offlinePrimaryVertices"),
                              genparticles = cms.InputTag("genParticles"),
                              convcollection = cms.InputTag("conversions"),
@@ -52,7 +52,7 @@ process.wp3 = cms.EDAnalyzer('ElectronIDAnalyzer',
 
 process.wp4 = cms.EDAnalyzer('ElectronIDAnalyzer',
                              electrons = cms.InputTag("gedGsfElectrons"),
-                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-tight"),
+                             electronIDs = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-PU20bx25-V0-standalone-tight"),
                              vertices = cms.InputTag("offlinePrimaryVertices"),
                              genparticles = cms.InputTag("genParticles"),
                              convcollection = cms.InputTag("conversions"),
@@ -61,7 +61,7 @@ process.wp4 = cms.EDAnalyzer('ElectronIDAnalyzer',
                              )
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('electron_ntuple_sc1.root')
+                                   fileName = cms.string('electron_ntuple_sc2.root')
                                    )
 
 process.p = cms.Path(process.egmGsfElectronIDSequence * process.wp1 * process.wp2 * process.wp3 * process.wp4)
