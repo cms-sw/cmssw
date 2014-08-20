898c44081465543bd940736a79ace1191612ab0f
Lindsey Gray <lindsey.gray@gmail.com>
2014-08-20 15:28:14 +0200

    prepare VID python interface for users and MiniAOD customization

--- EgammaAnalysis/ElectronTools/test/id_validation/runEleIDTest_CSA14_sc1.py
+++ EgammaAnalysis/ElectronTools/test/id_validation/runEleIDTest_CSA14_sc1.py
@@ -9,7 +9,7 @@ process.load("Configuration.StandardSequences.Services_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
+#process.load("Configuration.StandardSequences.Reconstruction_cff")
 # NOTE: the pick the right global tag!
 #    for CSA14 scenario 1: global tag is 'POSTLS170_V6::All'
 #    for CSA14 scenario 2: global tag is 'PLS170_V7AN1::All'
@@ -18,7 +18,19 @@ process.GlobalTag.globaltag = 'POSTLS170_V6::All'
 
 process.load("EgammaAnalysis/ElectronTools/Validation/DYJetsToLL_Sc1_AODSIM")
 
-process.load('EgammaAnalysis/ElectronTools/egmGsfElectronIDs_cff')
+process.load('PhysicsTools/PatAlgos/producersLayer1/electronProducer_cff')
+
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer
+switchOnVIDElectronIdProducer(process)
+# define which IDs we want to produce
+my_id_modules = ['EgammaAnalysis.ElectronTools.Identification.cutBasedElectronID_CSA14_50ns_V1_cff',
+                 'EgammaAnalysis.ElectronTools.Identification.cutBasedElectronID_CSA14_PU20bx25_V0_cff',
+                 'EgammaAnalysis.ElectronTools.Identification.heepElectronID_HEEPV50_CSA14_25ns_cff',
+                 'EgammaAnalysis.ElectronTools.Identification.heepElectronID_HEEPV50_CSA14_startup_cff']
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
 process.electronIDValueMapProducer.ebReducedRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
 process.electronIDValueMapProducer.eeReducedRecHitCollection = cms.InputTag('reducedEcalRecHitsEE')
@@ -68,5 +80,5 @@ process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string('electron_ntuple_sc1.root')
                                    )
 
-process.p = cms.Path(process.egmGsfElectronIDSequence * process.wp1 * process.wp2 * process.wp3 * process.wp4)
+process.p = cms.Path(process.egmGsfElectronIDSequence * process.wp1 * process.wp2 * process.wp3 * process.wp4 * process.makePatElectrons)
 
