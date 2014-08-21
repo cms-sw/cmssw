2384026f7ece20abc4403e5d5d09198738d0d6ff
Mia <mia.tosi@cern.ch>
2014-08-21 17:29:04 +0200

    fix tracking client in cosmic configuration
    (cherry picked from commit dfac7f46c9277533410e32479f1139b48843e518)

--- DQM/TrackingMonitorClient/python/TrackingClientConfig_Tier0_Cosmic_cff.py
+++ DQM/TrackingMonitorClient/python/TrackingClientConfig_Tier0_Cosmic_cff.py
@@ -2,27 +2,48 @@ import FWCore.ParameterSet.Config as cms
 
 #  TrackingOfflineDQM (for Tier0 Harvesting Step) ####
 trackingOfflineAnalyser = cms.EDAnalyzer("TrackingOfflineDQM",
-    GlobalStatusFilling      = cms.untracked.int32(2),
-    UsedWithEDMtoMEConverter = cms.untracked.bool(True),
-    TrackRatePSet            = cms.PSet(
-        Name     = cms.string("NumberOfTracks_"),
-        LowerCut = cms.double(0.0),
-        UpperCut = cms.double(100.0),
+    GlobalStatusFilling        = cms.untracked.int32(2),
+    UsedWithEDMtoMEConverter   = cms.untracked.bool(True),
+    TopFolderName              = cms.untracked.string("Tracking"),                                     
+    TrackingGlobalQualityPSets = cms.VPSet(
+         cms.PSet(
+             QT         = cms.string("Rate"),
+             dir        = cms.string("TrackParameters/highPurityTracks/pt_1/GeneralProperties"),
+             name       = cms.string("NumberOfGoodTracks_"),
+         ),
+         cms.PSet(
+             QT         = cms.string("Chi2"),
+             dir        = cms.string("TrackParameters/highPurityTracks/pt_1/GeneralProperties"),
+             name       = cms.string("GoodTrackChi2oNDF_"),
+         ),
+         cms.PSet(
+             QT         = cms.string("RecHits"),
+             dir        = cms.string("TrackParameters/highPurityTracks/pt_1/HitProperties"),
+             name       = cms.string("GoodTrackNumberOfRecHitsPerTrack_"),
+         ),
     ),
-    TrackChi2PSet            = cms.PSet(
-        Name     = cms.string("Chi2oNDF_"),
-        LowerCut = cms.double(0.0),
-        UpperCut = cms.double(25.0),
-    ),
-    TrackHitPSet            = cms.PSet(
-        Name     = cms.string("NumberOfRecHitsPerTrack_"),
-        LowerCut = cms.double(3.0),
-        UpperCut = cms.double(35.0),
-    ),
-    GoodTrackFractionPSet   = cms.PSet(
-        Name     = cms.string("FractionOfGoodTracks_"),
-        LowerCut = cms.double(-1.),
-        UpperCut = cms.double(1.),
+    TrackingLSQualityPSets = cms.VPSet(
+         cms.PSet(
+             QT         = cms.string("Rate"),
+             LSdir      = cms.string("TrackParameters/highPurityTracks/pt_1/GeneralProperties/LSanalysis"),
+             LSname     = cms.string("NumberOfGoodTracks_lumiFlag_"),
+             LSlowerCut = cms.double( -1.0 ),
+             LSupperCut = cms.double(  1.0 )    
+         ),
+         cms.PSet(
+             QT         = cms.string("Chi2"),
+             LSdir      = cms.string("TrackParameters/highPurityTracks/pt_1/GeneralProperties/LSanalysis"),
+             LSname     = cms.string("GoodTrackChi2oNDF_lumiFlag_"),
+             LSlowerCut = cms.double(  0.0 ),
+             LSupperCut = cms.double( 25.0 )
+         ),
+         cms.PSet(
+             QT         = cms.string("RecHits"),
+             LSdir      = cms.string("TrackParameters/highPurityTracks/pt_1/GeneralProperties/LSanalysis"),
+             LSname     = cms.string("GoodTrackNumberOfRecHitsPerTrack_lumiFlag_"),
+             LSlowerCut = cms.double(  3.0 ),
+             LSupperCut = cms.double( 35.0 )
+         ),
     )
 )
 
