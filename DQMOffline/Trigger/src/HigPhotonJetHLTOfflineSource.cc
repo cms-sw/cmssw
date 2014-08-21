6cd8467ce5ac68d28462991d9b3b73c7bdbb49ff
Xin Shi <xshi@xshi.org>
2014-08-21 10:51:36 -0400

    fixed scale problem

--- DQMOffline/Trigger/src/HigPhotonJetHLTOfflineSource.cc
+++ DQMOffline/Trigger/src/HigPhotonJetHLTOfflineSource.cc
@@ -60,6 +60,7 @@ private:
   virtual void analyze(const edm::Event &, const edm::EventSetup &) override;
   virtual void endRun(const edm::Run &, const edm::EventSetup &) override;
   virtual void endJob();
+  bool isMonitoredTriggerAccepted(const edm::TriggerNames, const edm::Handle<edm::TriggerResults>); 
 
   // Input from Configuration File
   edm::ParameterSet pset_;
@@ -151,8 +152,11 @@ HigPhotonJetHLTOfflineSource::bookHistograms(DQMStore::IBooker & iBooker,
   delphijetmet_ = iBooker.book1D("delphijetmet", "#Delta#phi(PFJet, MET);#Delta#phi(Jet,MET)", 100, 0, 4); 
   invmassjj_ = iBooker.book1D("invmassjj", "Inv mass two leading jets;M_{jj}[GeV]", 100, 0, 2000); 
   deletajj_ = iBooker.book1D("deletajj", "#Delta#eta(jj);|#Delta#eta_{jj}|", 100, 0, 6); 
-  triggers_ = iBooker.book1D("triggers", "Triggers", 20, 0, 20); 
-  trigvsnvtx_ = iBooker.book2D("trigvsnvtx", "Trigger vs. # vertices;N_{vertices};Trigger", 100, 0, 100, 20, 0, 20); 
+  // triggers_ = iBooker.book1D("triggers", "Triggers", 20, 0, 20);
+  triggers_ = iBooker.book1D("triggers", "Triggers", hltPathsToCheck_.size(), 0, hltPathsToCheck_.size());
+
+  // trigvsnvtx_ = iBooker.book2D("trigvsnvtx", "Trigger vs. # vertices;N_{vertices};Trigger", 100, 0, 100, 20, 0, 20); 
+  trigvsnvtx_ = iBooker.book2D("trigvsnvtx", "Trigger vs. # vertices;N_{vertices};Trigger", 100, 0, 100, hltPathsToCheck_.size(), 0, hltPathsToCheck_.size()); 
 }
 
 
@@ -164,7 +168,7 @@ HigPhotonJetHLTOfflineSource::analyze(const edm::Event& iEvent,
   evtsrun_++; 
 
   // Throw out this event if it doesn't pass any of the mornitored trigger.
-  bool triggered = false; 
+  //  bool triggered = false; 
   edm::Handle<edm::TriggerResults> triggerResults;
   iEvent.getByToken(triggerResultsToken_, triggerResults);
   
@@ -185,23 +189,46 @@ HigPhotonJetHLTOfflineSource::analyze(const edm::Event& iEvent,
     triggers_->setBinLabel(i+1, hltPathsToCheck_[i]); 
   }
 
-  // Check how many HLT triggers are in triggerResults
-  const edm::TriggerNames triggerNames = iEvent.triggerNames(*triggerResults);
+  // Check whether contains monitored trigger and accepted
+  const edm::TriggerNames triggerNames = iEvent.triggerNames(*triggerResults); 
+  bool triggered = isMonitoredTriggerAccepted(triggerNames, triggerResults); 
+  // const edm::TriggerNames triggerNames = iEvent.triggerNames(*triggerResults);
+  // for (unsigned int itrig = 0; itrig < triggerResults->size(); itrig++){
+  //   // Only consider the triggered case. 
+  //   if ( triggerAccept_ && ( (*triggerResults)[itrig].accept() != 1) ) continue; 
+  //   std::string triggername = triggerNames.triggerName(itrig);
+  //   for (size_t i = 0; i < hltPathsToCheck_.size(); i++) {
+  //     if ( triggername.find(hltPathsToCheck_[i]) != std::string::npos) {
+  // 	triggered = true;
+  // 	triggers_->Fill(i);
+  // 	trigvsnvtx_->Fill(vertices->size(), i);
+  // 	std::cout << "test >>> evt run: " << evtsrun_ <<
+  // 	  ", nvtx: " << vertices->size() << " , trig bit: " << i << std::endl; 
+  //     }
+  //   }
+  // }
+
+  if (!triggered) return; 
+
+  if (evtsrun_ > 10) return;
+  std::cout << "test >>> after return : " << evtsrun_ << std::endl; 
+
+  // Fill trigger info
   for (unsigned int itrig = 0; itrig < triggerResults->size(); itrig++){
     // Only consider the triggered case. 
-    if ( triggerAccept_ && ( (*triggerResults)[itrig].accept() != 1) ) continue; 
+    // if ( triggerAccept_ && ( (*triggerResults)[itrig].accept() != 1) ) continue; 
     std::string triggername = triggerNames.triggerName(itrig);
     for (size_t i = 0; i < hltPathsToCheck_.size(); i++) {
       if ( triggername.find(hltPathsToCheck_[i]) != std::string::npos) {
-	triggered = true;
-	triggers_->Fill(i);
-	trigvsnvtx_->Fill(vertices->size(), i); 
+  	triggers_->Fill(i);
+  	trigvsnvtx_->Fill(vertices->size(), i);
+  	std::cout << "test >>> evt run: " << evtsrun_ <<
+  	  ", nvtx: " << vertices->size() << " , trig bit: " << i << std::endl; 
       }
     }
   }
 
-  if (!triggered) return; 
-
+  // Fill Nvtx 
   nvertices_->Fill(vertices->size());
 
   // PF MET
@@ -292,13 +319,16 @@ void
 HigPhotonJetHLTOfflineSource::endRun(const edm::Run & iRun, 
 				     const edm::EventSetup& iSetup)
 {
-  if (verbose_) {
-    std::cout << "xshi:: endRun total number of events: " << evtsrun_ << std::endl;
-  }
-
   // Normalize to the total number of events in the run
   TH2F* h = trigvsnvtx_->getTH2F();
-  h->Scale( evtsrun_ );
+  double norm = evtsrun_*hltPathsToCheck_.size()/h->Integral(); 
+  h->Scale(norm);
+  if (verbose_) {
+    std::cout << "xshi:: endRun total number of events: " << evtsrun_
+	      << ", integral = " << h->Integral()
+	      << ", norm = " << norm << std::endl;
+  }
+
   
 }
 
@@ -309,7 +339,25 @@ HigPhotonJetHLTOfflineSource::endJob()
  
 }
 
+bool
+HigPhotonJetHLTOfflineSource::isMonitoredTriggerAccepted(const edm::TriggerNames triggerNames,
+							 const edm::Handle<edm::TriggerResults> triggerResults )
+{
+  // const edm::TriggerNames triggerNames = iEvent.triggerNames(*triggerResults);
+  for (unsigned int itrig = 0; itrig < triggerResults->size(); itrig++){
+    // Only consider the triggered case.
+    if ( triggerAccept_ && ( (*triggerResults)[itrig].accept() != 1) ) continue; 
+    // if ( (*triggerResults)[itrig].accept() != 1) continue; 
+    std::string triggername = triggerNames.triggerName(itrig);
+    for (size_t i = 0; i < hltPathsToCheck_.size(); i++) {
+      if ( triggername.find(hltPathsToCheck_[i]) != std::string::npos) {
+  	return true;
+      }
+    }
+  }
 
+  return false; 
+}
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(HigPhotonJetHLTOfflineSource);
