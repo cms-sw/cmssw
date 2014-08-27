fe60b3c53b550f164dcf0b102cd7899c54cfc417
Loic <Loic.Quertenmont@gmail.com>
2014-08-27 15:11:29 +0200

    update header location

--- SUSYBSMAnalysis/HSCP/plugins/HSCPValidator.cc
+++ SUSYBSMAnalysis/HSCP/plugins/HSCPValidator.cc
@@ -68,7 +68,7 @@
 #include "SimDataFormats/Vertex/interface/SimVertex.h"
 #include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
-#include "SUSYBSMAnalysis/HSCP/interface/HSCPValidator.h"
+#include "SUSYBSMAnalysis/HSCP/plugins/HSCPValidator.h"
 #include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 #include "TrackingTools/GeomPropagators/interface/Propagator.h"
