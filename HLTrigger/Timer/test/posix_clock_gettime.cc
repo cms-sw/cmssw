e7aae6bc34fdb3bd62b1ad2ac31d38e86023bf45
Andrea Bocci <fwyzard@gmail.com>
2014-08-28 13:25:43 +0200

    chrono: add timers based on CLOCK_REALTIME_COARSE and CLOCK_MONOTONIC_COARSE

--- HLTrigger/Timer/test/posix_clock_gettime.cc
+++ HLTrigger/Timer/test/posix_clock_gettime.cc
@@ -7,9 +7,19 @@ const bool clock_gettime_realtime::is_available = (sysconf(_SC_TIMERS) > 0);
 #endif // HAVE_POSIX_CLOCK_REALTIME
 
 
+#ifdef HAVE_POSIX_CLOCK_REALTIME_COARSE
+const bool clock_gettime_realtime_coarse::is_available = true;
+#endif // HAVE_POSIX_CLOCK_REALTIME_COARSE
+
+
 #ifdef HAVE_POSIX_CLOCK_MONOTONIC
 const bool clock_gettime_monotonic::is_available = (sysconf(_SC_MONOTONIC_CLOCK) > 0);
 #endif // HAVE_POSIX_CLOCK_MONOTONIC
+
+
+#ifdef HAVE_POSIX_CLOCK_MONOTONIC_COARSE
+const bool clock_gettime_monotonic_coarse::is_available = true;
+#endif // HAVE_POSIX_CLOCK_MONOTONIC_COARSE
 
 
 #ifdef HAVE_POSIX_CLOCK_MONOTONIC_RAW
