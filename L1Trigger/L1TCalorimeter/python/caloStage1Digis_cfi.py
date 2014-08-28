0f9e5aadef4cb5a1361a4785566ed11ed2b28b30
Inga Bucinskaite <ingabu@fnal.gov>
2014-08-28 17:30:50 -0500

    Added separate egRelativeJetIsolationCuts for eg in barrel or endcap as requested by Maria et al.

--- L1Trigger/L1TCalorimeter/python/caloStage1Digis_cfi.py
+++ L1Trigger/L1TCalorimeter/python/caloStage1Digis_cfi.py
@@ -5,7 +5,8 @@ caloStage1Digis = cms.EDProducer(
     CaloRegions = cms.InputTag("rctUpgradeFormatDigis"),
     CaloEmCands = cms.InputTag("rctUpgradeFormatDigis"),
     FirmwareVersion = cms.uint32(2),  ## 1=HI algo, 2= pp algo
-    egRelativeJetIsolationCut = cms.double(0.5), ## eg isolation cut
+    egRelativeJetIsolationBarrelCut = cms.double(0.3), ## eg isolation cut, 0.3 for loose, 0.2 for tight
+    egRelativeJetIsolationEndcapCut = cms.double(0.5), ## eg isolation cut, 0.5 for loose, 0.4 for tight
     tauRelativeJetIsolationCut = cms.double(1.) ## tau isolation cut
 )
 
