f957306bca5d9885f01d9927bed5cd7a56d7d35f
Sven Dildick <sven.dildick@cern.ch>
2014-09-03 05:24:46 +0200

    Port ME0DigiPreReco to 72X

---
+++ DataFormats/GEMDigi/interface/ME0DigiPreRecoCollection.h
@@ -0,0 +1,15 @@
+#ifndef GEMDigi_ME0DigiPreRecoCollection_h
+#define GEMDigi_ME0DigiPreRecoCollection_h
+/** \class ME0DigiPreRecoCollection
+ *  
+ *  \author Marcello Maggi
+ */
+
+#include <DataFormats/MuonDetId/interface/ME0DetId.h>
+#include <DataFormats/GEMDigi/interface/ME0DigiPreReco.h>
+#include <DataFormats/MuonData/interface/MuonDigiCollection.h>
+
+typedef MuonDigiCollection<ME0DetId, ME0DigiPreReco> ME0DigiPreRecoCollection;
+
+#endif
+
