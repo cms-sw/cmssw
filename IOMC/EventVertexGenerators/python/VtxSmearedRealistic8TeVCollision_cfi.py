bbc3cb976e217d3baf1ff5fdcaba42ab899b5d7e
lveldere <lveldere@users.noreply.github.com>
2014-09-03 15:25:52 +0200

    Update VtxSmearedRealistic8TeVCollision_cfi.py
    taken into account Vladimir's comment

--- IOMC/EventVertexGenerators/python/VtxSmearedRealistic8TeVCollision_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedRealistic8TeVCollision_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import Realistic8TeVCollisionVtxSmearingParameters,VtxSmearedCommon
+from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 VtxSmeared = cms.EDProducer("BetafuncEvtVtxGenerator",
     Realistic8TeVCollisionVtxSmearingParameters,
     VtxSmearedCommon
