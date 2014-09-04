8a14d87bb8523dc717912d3a29a08cedfc80bc16
Josh Bendavid <Josh.Bendavid@cern.ch>
2014-09-05 01:14:50 +0200

    fix collection names in validation sequence

--- Validation/EcalRecHits/python/ecalRecHitsValidation_cfi.py
+++ Validation/EcalRecHits/python/ecalRecHitsValidation_cfi.py
@@ -3,11 +3,11 @@ import FWCore.ParameterSet.Config as cms
 ecalRecHitsValidation = cms.EDAnalyzer("EcalRecHitsValidation",
     hitsProducer = cms.string('g4SimHits'),
     outputFile = cms.untracked.string(''),
-    EEuncalibrechitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEE"),
+    EEuncalibrechitCollection = cms.InputTag("ecalMultiFitUncalibRecHit","EcalUncalibRecHitsEE"),
     verbose = cms.untracked.bool(False),
     EErechitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
     ESrechitCollection = cms.InputTag("ecalPreshowerRecHit","EcalRecHitsES"),
-    EBuncalibrechitCollection = cms.InputTag("ecalGlobalUncalibRecHit","EcalUncalibRecHitsEB"),
+    EBuncalibrechitCollection = cms.InputTag("ecalMultiFitUncalibRecHit","EcalUncalibRecHitsEB"),
     EBrechitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
     moduleLabelMC = cms.string('generator')
 )
