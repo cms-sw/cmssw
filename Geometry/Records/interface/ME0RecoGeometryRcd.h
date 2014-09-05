8111bba5545524737b2c52dd238486f8ae92b18c
Sven Dildick <sven.dildick@cern.ch>
2014-09-05 21:13:14 +0200

    Clean-up include guards

--- Geometry/Records/interface/ME0RecoGeometryRcd.h
+++ Geometry/Records/interface/ME0RecoGeometryRcd.h
@@ -1,5 +1,5 @@
-#ifndef Geometry_ME0RecoGeometryRcd_H
-#define Geometry_ME0RecoGeometryRcd_H
+#ifndef Geometry_Records_ME0RecoGeometryRcd_H
+#define Geometry_Records_ME0RecoGeometryRcd_H
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 class ME0RecoGeometryRcd : public edm::eventsetup::EventSetupRecordImplementation<ME0RecoGeometryRcd> {};
