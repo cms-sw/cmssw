a54efe148c74e509b36fbee20ff78bb2fb2d251c
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-09-05 13:06:01 +0200

    add the time profiler service

--- RecoLocalCalo/EcalRecProducers/test/testMultipleEcalRecoLocal_cfg.py
+++ RecoLocalCalo/EcalRecProducers/test/testMultipleEcalRecoLocal_cfg.py
@@ -12,12 +12,12 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 
-process.GlobalTag.globaltag = 'START72_V1::All'
+process.GlobalTag.globaltag = 'POSTLS172_V1::All'
 process.GlobalTag.toGet = cms.VPSet(
     cms.PSet(record = cms.string("GeometryFileRcd"),
              tag = cms.string("XMLFILE_Geometry_2015_72YV2_Extended2015ZeroMaterial_mc"),
              connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_GEOMETRY_000"),
-             label = cms.untracked.string("Extended2015ZeroMaterial")
+#             label = cms.untracked.string("Extended2015ZeroMaterial")
              ),
     cms.PSet(record = cms.string("EcalTBWeightsRcd"),
              tag = cms.string("EcalTBWeights_3p5_time_mc"),
@@ -33,12 +33,15 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1
 # start from RAW format for more flexibility
 process.raw2digi_step = cms.Sequence(process.RawToDigi)
 
-# get uncalibrechits with weights method
-import RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi
-process.ecalWeightsUncalibRecHit = RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi.ecalWeightUncalibRecHit.clone()
-# get uncalib rechits from multifit method
+# get uncalibrechits with global method / time from ratio
+import RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi
+process.ecalGlobalUncalibRecHit = RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi.ecalGlobalUncalibRecHit.clone()
+# get uncalib rechits from multifit method / time from ratio
 import RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi
 process.ecalMultiFitUncalibRecHit =  RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi.ecalMultiFitUncalibRecHit.clone()
+# get uncalib rechits from multifit method / time from weights
+process.ecalMultiFit2UncalibRecHit =  RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi.ecalMultiFitUncalibRecHit.clone()
+process.ecalMultiFit2UncalibRecHit.timealgo = cms.string("WeightsMethod")
 
 # get the recovered digis
 if isMC:
@@ -53,11 +56,11 @@ if isMC:
     process.ecalRecHit.eeFEToBeRecovered = ''
 
 
-process.ecalRecHitWeights = process.ecalRecHit.clone()
-process.ecalRecHitWeights.EBuncalibRecHitCollection = 'ecalWeightsUncalibRecHit:EcalUncalibRecHitsEB'
-process.ecalRecHitWeights.EEuncalibRecHitCollection = 'ecalWeightsUncalibRecHit:EcalUncalibRecHitsEE'
-process.ecalRecHitWeights.EBrechitCollection = 'EcalRecHitsWeightsEB'
-process.ecalRecHitWeights.EErechitCollection = 'EcalRecHitsWeightsEE'
+process.ecalRecHitGlobal = process.ecalRecHit.clone()
+process.ecalRecHitGlobal.EBuncalibRecHitCollection = 'ecalGlobalUncalibRecHit:EcalUncalibRecHitsEB'
+process.ecalRecHitGlobal.EEuncalibRecHitCollection = 'ecalGlobalUncalibRecHit:EcalUncalibRecHitsEE'
+process.ecalRecHitGlobal.EBrechitCollection = 'EcalRecHitsGlobalEB'
+process.ecalRecHitGlobal.EErechitCollection = 'EcalRecHitsGlobalEE'
 
 process.ecalRecHitMultiFit = process.ecalRecHit.clone()
 process.ecalRecHitMultiFit.EBuncalibRecHitCollection = 'ecalMultiFitUncalibRecHit:EcalUncalibRecHitsEB'
@@ -65,9 +68,27 @@ process.ecalRecHitMultiFit.EEuncalibRecHitCollection = 'ecalMultiFitUncalibRecHi
 process.ecalRecHitMultiFit.EBrechitCollection = 'EcalRecHitsMultiFitEB'
 process.ecalRecHitMultiFit.EErechitCollection = 'EcalRecHitsMultiFitEE'
 
-process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(100) )
+process.ecalRecHitMultiFit2 = process.ecalRecHit.clone()
+process.ecalRecHitMultiFit2.EBuncalibRecHitCollection = 'ecalMultiFit2UncalibRecHit:EcalUncalibRecHitsEB'
+process.ecalRecHitMultiFit2.EEuncalibRecHitCollection = 'ecalMultiFit2UncalibRecHit:EcalUncalibRecHitsEE'
+process.ecalRecHitMultiFit2.EBrechitCollection = 'EcalRecHitsMultiFit2EB'
+process.ecalRecHitMultiFit2.EErechitCollection = 'EcalRecHitsMultiFit2EE'
+
+process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(1000) )
+path = '/store/group/comm_ecal/localreco/cmssw_720p4/photongun_pu25_ave40/'
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('/store/group/comm_ecal/localreco/cmssw_720p4/photongun_pu25_ave40/photongun_pu25_ave40_lsf_750.root')) 
+                            duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+                            fileNames = cms.untracked.vstring(path+'photongun_pu25_ave40_lsfext2_990.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_991.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_992.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_993.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_994.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_995.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_996.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_997.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_998.root',
+                                                              path+'photongun_pu25_ave40_lsfext2_999.root'
+                                                              ))
 
 
 process.out = cms.OutputModule("PoolOutputModule",
@@ -77,15 +98,18 @@ process.out = cms.OutputModule("PoolOutputModule",
                                                                       'keep *_offlineBeamSpot_*_*',
                                                                       'keep *_addPileupInfo_*_*'
                                                                       ),
-                               fileName = cms.untracked.string('reco2.root')
+                               fileName = cms.untracked.string('reco2_pu40.root')
                                )
 
 
-process.ecalAmplitudeReco = cms.Sequence( process.ecalWeightsUncalibRecHit *
-                                          process.ecalMultiFitUncalibRecHit )
+process.ecalAmplitudeReco = cms.Sequence( process.ecalGlobalUncalibRecHit *
+                                          process.ecalMultiFitUncalibRecHit *
+                                          process.ecalMultiFit2UncalibRecHit
+                                          )
 
-process.ecalRecHitsReco = cms.Sequence( process.ecalRecHitWeights *
-                                        process.ecalRecHitMultiFit )
+process.ecalRecHitsReco = cms.Sequence( process.ecalRecHitGlobal *
+                                        process.ecalRecHitMultiFit *
+                                        process.ecalRecHitMultiFit2 )
 
 process.ecalTestRecoLocal = cms.Sequence( process.raw2digi_step *
                                           process.ecalAmplitudeReco *
@@ -97,3 +121,71 @@ process.p = cms.Path(process.ecalTestRecoLocal)
 process.outpath = cms.EndPath(process.out)
 
 
+#########################
+#    Time Profiling     #
+#########################
+
+#https://twiki.cern.ch/twiki/bin/viewauth/CMS/FastTimerService
+process.MessageLogger.categories.append('FastReport')
+process.MessageLogger.cerr.FastReport = cms.untracked.PSet( limit = cms.untracked.int32( 10000000 ) )
+
+# remove any instance of the FastTimerService
+if 'FastTimerService' in process.__dict__:
+    del process.FastTimerService
+    
+# instrument the menu with the FastTimerService
+process.load( "HLTrigger.Timer.FastTimerService_cfi" )
+
+# this is currently ignored in 7.x, and alway uses the real tim clock
+process.FastTimerService.useRealTimeClock         = True
+
+# enable specific features
+process.FastTimerService.enableTimingPaths        = True
+process.FastTimerService.enableTimingModules      = True
+process.FastTimerService.enableTimingExclusive    = True
+
+# print a text summary at the end of the job
+process.FastTimerService.enableTimingSummary      = True
+
+# skip the first path (useful for HLT timing studies to disregard the time spent loading event and conditions data)
+process.FastTimerService.skipFirstPath            = False
+
+# enable per-event DQM plots
+process.FastTimerService.enableDQM                = True
+
+# enable per-path DQM plots
+process.FastTimerService.enableDQMbyPathActive    = True
+process.FastTimerService.enableDQMbyPathTotal     = True
+process.FastTimerService.enableDQMbyPathOverhead  = True
+process.FastTimerService.enableDQMbyPathDetails   = True
+process.FastTimerService.enableDQMbyPathCounters  = True
+process.FastTimerService.enableDQMbyPathExclusive = True
+
+# enable per-module DQM plots
+process.FastTimerService.enableDQMbyModule        = True
+process.FastTimerService.enableDQMbyModuleType    = True
+        
+# enable per-event DQM sumary plots
+process.FastTimerService.enableDQMSummary         = True
+
+# enable per-event DQM plots by lumisection
+process.FastTimerService.enableDQMbyLumiSection   = True
+process.FastTimerService.dqmLumiSectionsRange     = 2500    # lumisections (23.31 s)
+
+# set the time resolution of the DQM plots
+process.FastTimerService.dqmTimeRange             = 1000.   # ms
+process.FastTimerService.dqmTimeResolution        =    5.   # ms
+process.FastTimerService.dqmPathTimeRange         =  100.   # ms
+process.FastTimerService.dqmPathTimeResolution    =    0.5  # ms
+process.FastTimerService.dqmModuleTimeRange       = 1000.   # ms
+process.FastTimerService.dqmModuleTimeResolution  =    0.5  # ms
+
+# set the base DQM folder for the plots
+process.FastTimerService.dqmPath                  = "HLT/TimerService"
+process.FastTimerService.enableDQMbyProcesses     = True
+
+# save the DQM plots in the DQMIO format
+process.dqmOutput = cms.OutputModule("DQMRootOutputModule",
+                                     fileName = cms.untracked.string("DQM_pu40.root")
+                                     )
+process.FastTimerOutput = cms.EndPath( process.dqmOutput )
