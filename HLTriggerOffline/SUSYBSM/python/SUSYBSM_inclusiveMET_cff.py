af038806062cfbf679e9a73304e5c58b72396d62
jgran <jgran@physics.ucsb.edu>
2014-09-05 10:02:33 -0700

    removed testing-specific config

--- HLTriggerOffline/SUSYBSM/python/SUSYBSM_inclusiveMET_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SUSYBSM_inclusiveMET_cff.py
@@ -1,17 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 SUSY_HLT_InclusiveMET = cms.EDAnalyzer("SUSY_HLT_InclusiveHT",
-  trigSummary = cms.InputTag("hltTriggerSummaryAOD",'', 'HLTX'), #to use with test sample
-  #trigSummary = cms.InputTag("hltTriggerSummaryAOD"),
+  trigSummary = cms.InputTag("hltTriggerSummaryAOD"),
   pfMETCollection = cms.InputTag("pfMet"),
   caloMETCollection = cms.InputTag("met"),
   pfJetCollection = cms.InputTag("ak4PFJetsCHS"),
   caloJetCollection = cms.InputTag("ak4CaloJets"),
-  TriggerResults = cms.InputTag('TriggerResults','','HLTX'), #to use with test sample
-  #TriggerResults = cms.InputTag('TriggerResults','','HLT'),
+  TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   TriggerPath = cms.string('HLT_PFMET170_NoiseCleaned_v1'),
-  TriggerFilter = cms.InputTag('hltPFMET170Filter', '', 'HLTX'), #the last filter in the path, to use with test sample
-  #TriggerFilter = cms.InputTag('hltPFHT350', '', 'HLT'), #the last filter in the path
+  TriggerFilter = cms.InputTag('hltPFHT350', '', 'HLT'), #the last filter in the path
   PtThrJet = cms.untracked.double(40.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
