6e44aeccb0a4f5394dc893a570dbe0e7b4d90394
jgran <jgran@physics.ucsb.edu>
2014-09-05 21:47:04 -0700

    restored HLTSusyExoValSeq_FastSim sequence

--- HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
@@ -1,7 +1,9 @@
+from HLTriggerOffline.SUSYBSM.SUSYBSM_triggerValidation_fastSim_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_HT_MET_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_MET_BTAG_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_inclusiveHT_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_inclusiveMET_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_GenericClient_cfi import *
 
+HLTSusyExoValSeq_FastSim = cms.Sequence(HLTSusyExoValFastSim)
 HLTSusyExoValSeq = cms.Sequence(SUSY_HLT_HT_MET+SUSY_HLT_InclusiveHT+SUSY_HLT_InclusiveMET+SUSY_HLT_MET_BTAG+SUSYBSM_GenericClient)
