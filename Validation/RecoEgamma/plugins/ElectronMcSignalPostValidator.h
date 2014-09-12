7ca0e4bf2d2e72b1bab0c9a5edc40f08366e7b86
Arnaud <arnaud.chiron@cern.ch>
2014-09-12 15:09:22 +0200

    MultiThread modifications

--- Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.h
+++ Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.h
@@ -2,15 +2,21 @@
 #ifndef Validation_RecoEgamma_ElectronMcSignalPostValidator_h
 #define Validation_RecoEgamma_ElectronMcSignalPostValidator_h
 
-#include "DQMOffline/EGamma/interface/ElectronDqmAnalyzerBase.h"
+#include "DQMOffline/EGamma/interface/ElectronDqmHarvesterBase.h" 
 
-class ElectronMcSignalPostValidator : public ElectronDqmAnalyzerBase
+class ElectronMcSignalPostValidator : public ElectronDqmHarvesterBase
  {
   public:
     explicit ElectronMcSignalPostValidator( const edm::ParameterSet & conf ) ;
     virtual ~ElectronMcSignalPostValidator() ;
     virtual void book() ;
-    virtual void finalize() ;
+    virtual void finalize2() ;
+    virtual void finalize( DQMStore::IBooker & iBooker ) ; // , DQMStore::IGetter & iGetter
+
+  private:
+    // histos
+    MonitorElement *h1_ele_xOverX0VsEta ;
+	
  } ;
 
 #endif
