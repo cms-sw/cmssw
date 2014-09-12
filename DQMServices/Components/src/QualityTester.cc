89872900a5c9c3d3ee117c135574a82997910104
Mia <mia.tosi@cern.ch>
2014-09-12 14:08:55 +0200

    revert QualityTester
    (cherry picked from commit 19c2f169c81c22739d94cb50b1c9e772c80040a6)

--- DQMServices/Components/src/QualityTester.cc
+++ DQMServices/Components/src/QualityTester.cc
@@ -126,18 +126,16 @@ void QualityTester::performTests(void)
       {
         const std::string &alarmType = itr->first;
         const std::vector<std::string> &msgs = itr->second;
-	for (std::vector<std::string>::const_iterator msg = msgs.begin();
-	     msg != msgs.end(); ++msg)
         if ((reportThreshold == "black")
 	    || (reportThreshold == "orange" && (alarmType == "orange" || alarmType == "red"))
 	    || (reportThreshold == "red" && alarmType == "red"))
-	{
-	  std::cout << std::endl;
-	  std::cout << "Error Type: " << alarmType << std::endl;
-          for (std::vector<std::string>::const_iterator msg = msgs.begin();
-	       msg != msgs.end(); ++msg)
-	    std::cout << *msg << std::endl;
-        }
+	  {
+	    std::cout << std::endl;
+	    std::cout << "Error Type: " << alarmType << std::endl;
+	    for (std::vector<std::string>::const_iterator msg = msgs.begin();
+		 msg != msgs.end(); ++msg)
+	      std::cout << *msg << std::endl;
+	  }
       }
       std::cout << std::endl;
     }
