f864f741c684131555885485d75a44ddf8887d69
Nadir Daci <daci.nadir@gmail.com>
2014-09-15 18:15:43 +0200

    Fix dimuon thresholds.

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaLowPtDimuon_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaLowPtDimuon_cff.py
@@ -2,9 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 LowPtDimuonPSet = cms.PSet(
     hltPathsToCheck = cms.vstring(
+        "HLT_Mu30_TkMu11_v" # Run2
         "HLT_Mu17_Mu8_v",   # Run2 & Run1
-        "HLT_Mu17_TkMu8_v", # Run2 & Run1
-        "HLT_Mu22_TkMu8_v" # Run1
+        "HLT_Mu17_TkMu8_v", # Run1
+        "HLT_Mu22_TkMu8_v"  # Run1
         ),
     recMuonLabel  = cms.InputTag("muons"),
     # -- Analysis specific cuts
