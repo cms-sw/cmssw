111b7a60117bab27fd12de3de008261f1b92e2c8
Nadir Daci <daci.nadir@gmail.com>
2014-09-16 17:43:00 +0200

    test

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaHighPtDimuon_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaHighPtDimuon_cff.py
@@ -5,7 +5,7 @@ HighPtDimuonPSet = cms.PSet(
         "HLT_Mu30_TkMu11_v" # Run2
         "HLT_Mu17_Mu8_v",   # Run2 & Run1
         "HLT_Mu17_TkMu8_v", # Run1
-        "HLT_Mu22_TkMu8_v"  # Run1
+        "HLT_Mu22_TkMu8_v"  # Run1 test
         ),
     recMuonLabel  = cms.InputTag("muons"),
     # -- Analysis specific cuts
