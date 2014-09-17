a22d24178b0339e4d2169ab8e93886584093981c
Arnaud <arnaud.chiron@cern.ch>
2014-09-17 17:14:21 +0200

    multithreading, EfficiencyFlag & statOverflowFlag

--- Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.h
+++ Validation/RecoEgamma/plugins/ElectronMcSignalPostValidator.h
@@ -10,7 +10,7 @@ class ElectronMcSignalPostValidator : public ElectronDqmHarvesterBase
     explicit ElectronMcSignalPostValidator( const edm::ParameterSet & conf ) ;
     virtual ~ElectronMcSignalPostValidator() ;
     virtual void book() ;
-    virtual void finalize( DQMStore::IBooker & iBooker ) ; // , DQMStore::IGetter & iGetter
+    virtual void finalize( DQMStore::IBooker & iBooker ) ; // , DQMStore::IGetter & iGetter, const edm::Event& e, const edm::EventSetup & c
 
   private:
     // histos limits and binning
