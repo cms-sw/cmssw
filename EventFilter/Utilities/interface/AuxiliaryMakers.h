975272f96ee5361b22e6359cc19a3394155a002b
Emilio <Emilio.Meschi@cern.ch>
2014-09-18 19:46:06 +0200

    tcds_beta

---
+++ EventFilter/Utilities/interface/AuxiliaryMakers.h
@@ -0,0 +1,14 @@
+#ifndef EVF_UTILITIES_AUXILIARYMAKERS_H
+#define EVF_UTILITIES_AUXILIARYMAKERS_H
+
+#include "DataFormats/Provenance/interface/EventAuxiliary.h"
+#include "EventFilter/FEDInterface/interface/FED1024.h"
+
+namespace evf{
+  namespace evtn{
+    edm::EventAuxiliary makeEventAuxiliary(TCDSRecord *record, 
+					   unsigned int runNumber,
+					   std::string const &processGUID);
+  }
+}
+#endif
---
+++ EventFilter/Utilities/src/AuxiliaryMakers.cc
@@ -0,0 +1,26 @@
+#include "EventFilter/Utilities/interface/AuxiliaryMakers.h"
+namespace evf{
+  namespace evtn{
+
+      edm::EventAuxiliary makeEventAuxiliary(TCDSRecord *record, 
+					     unsigned int runNumber,
+					     std::string const &processGUID){
+	edm::EventID eventId(runNumber, // check that runnumber from record is consistent
+			     record->getHeader().getData().header.lumiSection,
+			     record->getHeader().getData().header.eventNumber);
+	
+	uint64_t gpsh = record->getBST().getBST().gpstimehigh;
+	uint32_t gpsl = record->getBST().getBST().gpstimelow;
+	edm::Timestamp tstamp = edm::Timestamp(static_cast<edm::TimeValue_t> ((gpsh << 32) + gpsl));      
+	return edm::EventAuxiliary(eventId,
+				   processGUID,
+				   tstamp,
+				   true,
+				   (edm::EventAuxiliary::ExperimentType)record->getHistory().history().hist[0].eventtype,
+				   (int)record->getHeader().getData().header.bcid,
+				   edm::EventAuxiliary::invalidStoreNumber,
+				   record->getHeader().getData().header.orbitLow);
+      }
+  }
+}
+
