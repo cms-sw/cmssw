8816ccdb3f99f4957fee772d1efd43378db226c5
Arnaud <arnaud.chiron@cern.ch>
2014-09-19 09:53:31 +0200

    setEfficiencyFlag setStatOverflowFlag

--- Validation/RecoEgamma/python/ElectronMcFakePostValidator_cfi.py
+++ Validation/RecoEgamma/python/ElectronMcFakePostValidator_cfi.py
@@ -1,6 +1,10 @@
 
 import FWCore.ParameterSet.Config as cms
 
+electronMcFakeHistosCfg = cms.PSet(
+  EfficiencyFlag = cms.bool(False),StatOverflowFlag = cms.bool(True)
+)
+
 electronMcFakePostValidator = cms.EDAnalyzer("ElectronMcFakePostValidator",
 
   Verbosity = cms.untracked.int32(0),
@@ -10,6 +14,7 @@ electronMcFakePostValidator = cms.EDAnalyzer("ElectronMcFakePostValidator",
   InputFolderName = cms.string("EgammaV/ElectronMcFakeValidator"),
   OutputFolderName = cms.string("EgammaV/ElectronMcFakeValidator"),
     
+  histosCfg = cms.PSet(electronMcFakeHistosCfg)
 )
 
 
