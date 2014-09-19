44f8eb4ef0886624eedda63b473a29dad2e69f19
Mia <mia.tosi@cern.ch>
2014-09-19 13:51:15 +0200

    fix configuration of strip CPE (needed for confDB)

---
+++ RecoLocalTracker/SiStripRecHitConverter/python/StripCPEESProducer_cfi.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+stripCPEESProducer = cms.ESProducer("StripCPEESProducer",
+                                    ComponentName = cms.string('SimpleStripCPE'),
+                                    ComponentType = cms.string('SimpleStripCPE'),
+                                    parameters    = cms.PSet("parameters")
+)
--- RecoLocalTracker/SiStripRecHitConverter/python/StripCPEfromTemplate_cfi.py
+++ RecoLocalTracker/SiStripRecHitConverter/python/StripCPEfromTemplate_cfi.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-StripCPEfromTemplateESProducer = cms.ESProducer("StripCPEESProducer",
-                                                  ComponentName = cms.string('StripCPEfromTemplate'),
-                                                  ComponentType = cms.string('StripCPEfromTemplate'),
-                                                  UseTemplateReco            = cms.bool(False),
-                                                  TemplateRecoSpeed          = cms.int32(0),
-                                                  UseStripSplitClusterErrors = cms.bool(False)
-                                                  )
+from RecoLocalTracker.SiStripRecHitConverter.StripCPEESProducer_cfi import *
+StripCPEfromTemplateESProducer = stripCPEESProducer.clone()
+StripCPEfromTemplateESProducerComponentName = cms.string('StripCPEfromTemplate')
+StripCPEfromTemplateESProducer.ComponentType = cms.string('StripCPEfromTemplate')
+StripCPEfromTemplateESProducer.parameters = cms.PSet(
+   UseTemplateReco            = cms.bool(False),
+   TemplateRecoSpeed          = cms.int32(0),
+   UseStripSplitClusterErrors = cms.bool(False)
+)
 
 
