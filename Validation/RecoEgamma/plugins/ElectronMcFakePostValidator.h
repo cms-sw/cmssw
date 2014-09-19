6dc21784221b49570a9292e5b42c7b77f806bbfd
Arnaud <arnaud.chiron@cern.ch>
2014-09-19 09:51:26 +0200

    multiThreading

--- Validation/RecoEgamma/plugins/ElectronMcFakePostValidator.h
+++ Validation/RecoEgamma/plugins/ElectronMcFakePostValidator.h
@@ -2,15 +2,24 @@
 #ifndef Validation_RecoEgamma_ElectronMcFakePostValidator_h
 #define Validation_RecoEgamma_ElectronMcFakePostValidator_h
 
-#include "DQMOffline/EGamma/interface/ElectronDqmAnalyzerBase.h"
+#include "DQMOffline/EGamma/interface/ElectronDqmHarvesterBase.h"
 
-class ElectronMcFakePostValidator : public ElectronDqmAnalyzerBase
+class ElectronMcFakePostValidator : public ElectronDqmHarvesterBase
  {
   public:
-    explicit ElectronMcFakePostValidator( const edm::ParameterSet & conf ) ;
+    explicit ElectronMcFakePostValidator( const edm::ParameterSet & conf ) ; 
     virtual ~ElectronMcFakePostValidator() ;
     virtual void book() ;
-    virtual void finalize() ;
+    virtual void finalize( DQMStore::IBooker & iBooker ) ; // , DQMStore::IGetter & iGetter
+
+  private:
+    // histos limits and binning
+
+    bool set_EfficiencyFlag ; bool set_StatOverflowFlag ;
+
+    // histos
+    MonitorElement *h1_ele_xOverX0VsEta ;
+	
  } ;
 
 #endif
