5c226c7e1f84f91369960fec2e3f9e6052c57785
Mia <mia.tosi@cern.ch>
2014-09-20 11:27:45 +0200

    fix error shown in relval 101.0 step1

--- RecoLocalTracker/SiStripRecHitConverter/python/StripCPEESProducer_cfi.py
+++ RecoLocalTracker/SiStripRecHitConverter/python/StripCPEESProducer_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 stripCPEESProducer = cms.ESProducer("StripCPEESProducer",
-                                    ComponentName = cms.string('SimpleStripCPE'),
+                                    ComponentName = cms.string('stripCPE'),
                                     ComponentType = cms.string('SimpleStripCPE'),
                                     parameters    = cms.PSet()
 )
