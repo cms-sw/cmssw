f03a5d1b972888955c8f199f497c99eae8d093d8
Lindsey Gray <lindsey.gray@gmail.com>
2014-09-24 12:42:23 +0200

    tuning up clustering based on initial geometry fixes
    Conflicts:
    	RecoParticleFlow/PFClusterProducer/test/run_display.sh

--- RecoParticleFlow/PFClusterProducer/test/run_display.sh
+++ RecoParticleFlow/PFClusterProducer/test/run_display.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cmsShow -c ${CMSSW_BASE}/src/RecoParticleFlow/PFClusterProducer/test/hgcal_rechits.fwc -g /home/lgray/xHGCAL/cmsRecoGeom1-HGCAL.root --sim-geom-file /home/lgray/xHGCAL/cmsSimGeom-14-HGCAL.root /home/lgray/CMSSW_6_2_0_SLHC16/src/matrix_tests/simple_jet_gun/step3.root
+cmsShow -c ${CMSSW_BASE}/src/RecoParticleFlow/PFClusterProducer/test/hgcal_rechits.fwc -g ~/work/public/xHGCAL/cmsRecoGeom1-HGCAL.root --sim-geom-file ~/work/public/xHGCAL/cmsSimGeom-14-HGCAL.root ${CMSSW_BASE}/src/matrix_tests/14801_FourMuPt1_200+FourMuPt_1_200_Extended2023HGCalV4_GenSimFull+DigiFull_Extended2023HGCalV4+RecoFull_Extended2023HGCalV4+HARVESTFull_Extended2023HGCalV4/step3.root
+
 
 #12202_SinglePionPt35+SinglePionPt35_Extended2023HGCalMuon_GenSimFull+DigiFull_Extended2023HGCalMuon+RecoFull_Extended2023HGCalMuon+HARVESTFull_Extended2023HGCalMuon
 
