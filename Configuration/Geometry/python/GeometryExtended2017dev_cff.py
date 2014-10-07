067990592d398e157e684e2788ace36bd4edd7b7
Gaelle <Gaelle.Boudoul@cern.ch>
2014-10-07 13:54:24 +0200

    2017dev + 2023Muondev scenario

--- Configuration/Geometry/python/GeometryExtended2017dev_cff.py
+++ Configuration/Geometry/python/GeometryExtended2017dev_cff.py
@@ -4,6 +4,6 @@ import FWCore.ParameterSet.Config as cms
 # Geometry master configuration
 #
 # Ideal geometry, needed for simulation
-from Geometry.CMSCommonData.cmsExtendedGeometry2017XML_cfi import *
+from Geometry.CMSCommonData.cmsExtendedGeometry2017devXML_cfi import *
 from Geometry.TrackerNumberingBuilder.trackerNumberingSLHCGeometry_cfi import *
 from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
