58fa591fe48a4e9c3f38d34ac9cd56d2f0dee539
Adrien <adrien.caudron@cern.ch>
2014-10-08 18:59:39 +0200

    Several fixes in the configuration and update of some example config files. Identical to PR 5740 in 72x.

--- Validation/RecoB/test/GridValidation/validation_Harvest.py
+++ Validation/RecoB/test/GridValidation/validation_Harvest.py
@@ -14,12 +14,10 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load("DQMServices.Core.DQM_cfg")
 
-process.load("RecoBTag.Configuration.RecoBTag_cff")
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
-process.source = cms.Source("PoolSource",
+process.source = cms.Source("DQMRootSource",
     fileNames = cms.untracked.vstring()
 )
 
@@ -29,8 +27,7 @@ if runOnMC:
 else:
     process.dqmSeq = cms.Sequence(process.bTagValidationHarvestData * process.dqmSaver)
 
-process.load("DQMServices.Components.EDMtoMEConverter_cfi")
-process.plots = cms.Path(process.EDMtoMEConverter * process.dqmSeq)
+process.plots = cms.Path(process.dqmSeq)
 
 process.dqmEnv.subSystemFolder = 'BTAG'
 process.dqmSaver.producer = 'DQM'
@@ -40,17 +37,7 @@ process.dqmSaver.saveByRun = cms.untracked.int32(-1)
 process.dqmSaver.saveAtJobEnd =cms.untracked.bool(True) 
 process.dqmSaver.forceRunNumber = cms.untracked.int32(1)
 
-process.PoolSource.fileNames = [
-    'file:MEtoEDMConverter_1.root',
-    'file:MEtoEDMConverter_2.root',
-    'file:MEtoEDMConverter_3.root',
-    'file:MEtoEDMConverter_4.root',
-    'file:MEtoEDMConverter_5.root',
-    'file:MEtoEDMConverter_6.root',
-    'file:MEtoEDMConverter_7.root',
-    'file:MEtoEDMConverter_8.root',
-    'file:MEtoEDMConverter_9.root',
-    'file:MEtoEDMConverter_10.root',
-    'file:MEtoEDMConverter_11.root'
+process.DQMRootSource.fileNames = [
+
 ]
 
