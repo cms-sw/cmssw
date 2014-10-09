b396e6ee99d765f3153e75aff868d936e139f155
Ye Chen <ye.chen@cern.ch>
2014-10-09 10:58:01 +0800

    Update EtaBToUpsilon1SUpsilon1S_forSTEAM_13TeV_cfi.py
    update filter efficiency and remove the pythia 8 setting.

--- Configuration/Generator/python/EtaBToUpsilon1SUpsilon1S_forSTEAM_13TeV_cfi.py
+++ Configuration/Generator/python/EtaBToUpsilon1SUpsilon1S_forSTEAM_13TeV_cfi.py
@@ -7,6 +7,7 @@ maxEventsToPrint = cms.untracked.int32(3),
 pythiaPylistVerbosity = cms.untracked.int32(1),
 displayPythiaCards = cms.untracked.bool(False),
 comEnergy = cms.double(13000.0),
+filterEfficiency = cms.untracked.double(0.64),
 PythiaParameters = cms.PSet(
 pythiaUESettingsBlock,
 pythiaEtab = cms.vstring(
@@ -15,10 +16,10 @@ pythiaEtab = cms.vstring(
 'MDME(1521,1)=0',
 'KFDP(1520,1)=553',
 'KFDP(1520,2)=553',
-'553:ALLOFF',
-'553:ONIFMATCH 13 13',
-'333:ALLOFF',
-'333:ONIFMATCH 321 321',
+#553:ALLOFF',
+#'553:ONIFMATCH 13 13',
+#'333:ALLOFF',
+#'333:ONIFMATCH 321 321',
 'PMAS(C10551,1)=20.0'),
 # This is a vector of ParameterSet names to be read, in this order
 parameterSets = cms.vstring(
