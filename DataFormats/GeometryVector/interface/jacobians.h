4c25b0f04be0f73f0464ecd56db87e808d66140a
Andrea <andrea.rizzi@cern.ch>
2014-10-09 11:56:40 +0200

    Move jacobians for curv2cart and viceversa into GeometryVector for usage in DF

---
+++ DataFormats/GeometryVector/interface/jacobians.h
@@ -0,0 +1,13 @@
+#ifndef GeomVector_jacobians_
+#define GeomVector_jacobians_
+
+
+#include "DataFormats/Math/interface/AlgebraicROOTObjects.h"
+#include "DataFormats/GeometryVector/interface/GlobalVector.h"
+namespace jacobians {
+
+AlgebraicMatrix65 jacobianCurvilinearToCartesian(const GlobalVector  & momentum, int charge) ;
+AlgebraicMatrix56 jacobianCartesianToCurvilinear(const GlobalVector  & momentum, int charge) ;
+
+}
+#endif
