99f3940cdebfb859d49cdff08dd757db517f6b1a
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-10-10 12:28:03 +0200

    Revert "RPC DQM migration to DQMEDHarvester"

--- DQM/RPCMonitorClient/interface/RPCDataCertification.h
+++ DQM/RPCMonitorClient/interface/RPCDataCertification.h
@@ -1,40 +1,42 @@
 #ifndef RPCMonitorClient_RPCDataCertification_H
 #define RPCMonitorClient_RPCDataCertification_H
 
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "DQMServices/Core/interface/MonitorElement.h"
-#include "DQMServices/Core/interface/DQMStore.h"
-#include "DQMServices/Core/interface/DQMEDHarvester.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
 
+#include <map>
 
-class RPCDataCertification : public  DQMEDHarvester{
+class DQMStore;
+class MonitorElement;
+
+class RPCDataCertification : public edm::EDAnalyzer {
 public:
-
   /// Constructor
   RPCDataCertification(const edm::ParameterSet& pset);
 
   /// Destructor
   virtual ~RPCDataCertification();
 
+  // Operations
+
 protected:
-  void beginJob();
-  void dqmEndLuminosityBlock(DQMStore::IBooker &, DQMStore::IGetter &, edm::LuminosityBlock const &, edm::EventSetup const&); //performed in the endLumi
-  void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override; //performed in the endJob
-
-
+  
 private:
-  void myBooker(DQMStore::IBooker &);
-  void checkFED(edm::EventSetup const& );
-
-  MonitorElement* CertMap_;
-  MonitorElement* totalCertFraction;
+  virtual void beginJob();
+  virtual void beginRun(const edm::Run& r, const edm::EventSetup& setup);
+  virtual void beginLuminosityBlock(const edm::LuminosityBlock& lumi, const  edm::EventSetup& setup);
+  virtual void analyze(const edm::Event& event, const edm::EventSetup& setup);
+  virtual void endLuminosityBlock(const edm::LuminosityBlock& lumi, const  edm::EventSetup& setup);
+  virtual void endJob() ;
+  
+  DQMStore *theDbe;  
+  MonitorElement*    CertMap_;
+  MonitorElement*  totalCertFraction;
   MonitorElement* certWheelFractions[5];
   MonitorElement* certDiskFractions[10];
  std::pair<int, int> FEDRange_;
   int numberOfDisks_;  
   int NumberOfFeds_;
-  bool init_, offlineDQM_ ;
-  double defaultValue_;
 
 };
 
