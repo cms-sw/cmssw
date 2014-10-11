08789fcf81cf5c9af30790a71df08fc4659014be
Alberto <a.escalante.del.valle@cern.ch>
2014-10-11 18:53:21 +0200

    Added more histograms to Exo DQMmodule in folders DiPhotons and MonoPhotons

--- DQM/Physics/src/ExoticaDQM.h
+++ DQM/Physics/src/ExoticaDQM.h
@@ -35,6 +35,10 @@
 // ParticleFlow
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 
+// Vertex
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
 // EGamma
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
@@ -103,27 +107,16 @@ protected:
   virtual void analyzeDiJets(edm::Event const& e);
   virtual void analyzeDiMuons(edm::Event const& e);
   virtual void analyzeDiElectrons(edm::Event const& e);
+  virtual void analyzeDiPhotons(edm::Event const& e);
 
   //Mono Searches
   virtual void analyzeMonoJets(edm::Event const& e);
   virtual void analyzeMonoMuons(edm::Event const& e);
   virtual void analyzeMonoElectrons(edm::Event const& e);
+  virtual void analyzeMonoPhotons(edm::Event const& e);
 
-  //Other... Phat stuff.
   virtual void analyzeMultiJetsTrigger(edm::Event const& e);
-  virtual void analyzeLongLived(edm::Event const& e);
   virtual void analyzeLongLivedTrigger(edm::Event const& e);
-  virtual void analyzeEventInterpretation(edm::Event const& e, edm::EventSetup const& eSetup);
-
-  //
-  //virtual void analyzeTopLike(edm::Event const& e);
-  //virtual void analyzeTopLikeTrigger(edm::Event const& e);
-  //
-  //virtual void analyzeLeptonJet(edm::Event const& e);
-  //virtual void analyzeLeptonJetTrigger(edm::Event const& e);
-  //
-  //virtual void analyzeNonHadronic(edm::Event const& e);
-  //virtual void analyzeNonHadronicTrigger(edm::Event const& e);
 
 private:
 
@@ -146,55 +139,36 @@ private:
   std::vector<std::string>  theTriggerForLongLivedList;
   edm::Handle<edm::TriggerResults> triggerResults_;
 
+  //Vertex
+  edm::EDGetTokenT<reco::VertexCollection> VertexToken_;
+  edm::Handle<reco::VertexCollection> VertexCollection_;
 
   // Electrons
   edm::EDGetTokenT<reco::GsfElectronCollection> ElectronToken_;
   edm::Handle<reco::GsfElectronCollection> ElectronCollection_;
-  //
-  edm::EDGetTokenT<reco::PFCandidateCollection> PFElectronTokenEI_;
-  edm::Handle<reco::PFCandidateCollection> pfElectronCollectionEI_;
-  reco::PFCandidateCollection pfelectronsEI;
-
 
   // Muons
   edm::EDGetTokenT<reco::MuonCollection> MuonToken_;
   edm::Handle<reco::MuonCollection> MuonCollection_;
-  //
-  edm::EDGetTokenT<reco::PFCandidateCollection> PFMuonTokenEI_;
-  edm::Handle<reco::PFCandidateCollection> pfMuonCollectionEI_;
-  reco::PFCandidateCollection pfmuonsEI;
-
-
-  // Taus
-  edm::EDGetTokenT<reco::CaloTauCollection> TauToken_;
-  edm::Handle<reco::CaloTauCollection> TauCollection_;
-  //
-  edm::InputTag PFTauLabelEI_;
-  edm::Handle<reco::PFTauCollection> pfTauCollectionEI_;
-
 
   // Photons
   edm::EDGetTokenT<reco::PhotonCollection> PhotonToken_;
   edm::Handle<reco::PhotonCollection> PhotonCollection_;
-  //
-  edm::InputTag PFPhotonLabelEI_;
-  edm::Handle<reco::PFCandidateCollection> pfPhotonCollectionEI_;
-  reco::PFCandidateCollection pfphotons;
-
 
   // Jets
   edm::EDGetTokenT<reco::CaloJetCollection> CaloJetToken_;
   edm::Handle<reco::CaloJetCollection> caloJetCollection_;
   reco::CaloJetCollection calojets;
-  //
+  // Nominal Jets
   edm::EDGetTokenT<reco::PFJetCollection> PFJetToken_;
   edm::Handle<reco::PFJetCollection> pfJetCollection_;
   reco::PFJetCollection pfjets;
-  //
-  edm::EDGetTokenT<reco::PFJetCollection> PFJetTokenEI_;
-  edm::Handle<reco::PFJetCollection> pfJetCollectionEI_;
-  reco::PFJetCollection pfjetsEI;
 
+  //All Other Jets
+  std::vector<edm::EDGetTokenT<reco::PFJetCollection> > DiJetPFJetToken_;
+  std::vector<edm::InputTag> DiJetPFJetCollection_;
+  edm::Handle<reco::PFJetCollection> DiJetpfJetCollection_;
+  reco::PFJetCollection DiJetpfjets;
 
   // MET
   edm::EDGetTokenT<reco::CaloMETCollection> CaloMETToken_;
@@ -202,9 +176,6 @@ private:
   //
   edm::EDGetTokenT<reco::PFMETCollection> PFMETToken_;
   edm::Handle<reco::PFMETCollection> pfMETCollection_;
-  //
-  edm::EDGetTokenT<reco::PFMETCollection> PFMETTokenEI_;
-  edm::Handle<reco::PFMETCollection> pfMETCollectionEI_;
 
   // ECAL RECHITS
   edm::EDGetTokenT<EBRecHitCollection> ecalBarrelRecHitToken_; // reducedEcalRecHitsEB
@@ -226,6 +197,8 @@ private:
   double PFJetPt[2];
   double PFJetEta[2];
   double PFJetPhi[2];
+  double PFJetRapidity[2];
+  double PFJetMass[2];
   double PFJetNHEF[2];
   double PFJetCHEF[2];
   double PFJetNEMF[2];
@@ -249,26 +222,43 @@ private:
   double ElectronPhi[2];
   double ElectronCharge[2];
 
+  // Photon
+  //
+  double PhotonEnergy[2];
+  double PhotonPt[2];
+  double PhotonEt[2];
+  double PhotonEta[2];
+  double PhotonEtaSc[2];
+  double PhotonPhi[2];
+  double PhotonHoverE[2];
+  double PhotonSigmaIetaIeta[2];
+  double PhotonTrkSumPtSolidConeDR03[2];
+  double PhotonE1x5E5x5[2];
+  double PhotonE2x5E5x5[2];
+
   ///////////////////////////
   // Histograms
   ///////////////////////////
   // Histograms - Dijet
-  //
-  MonitorElement* dijet_PFJet1_pt;
-  MonitorElement* dijet_PFJet1_eta;
-  MonitorElement* dijet_PFJet1_phi;
-  MonitorElement* dijet_PFJet2_pt;
-  MonitorElement* dijet_PFJet2_eta;
-  MonitorElement* dijet_PFJet2_phi;
-  MonitorElement* dijet_deltaPhiPFJet1PFJet2;
-  MonitorElement* dijet_deltaEtaPFJet1PFJet2;
-  MonitorElement* dijet_deltaRPFJet1PFJet2;
-  MonitorElement* dijet_invMassPFJet1PFJet2;
-  MonitorElement* dijet_PFchef;
-  MonitorElement* dijet_PFnhef;
-  MonitorElement* dijet_PFcemf;
-  MonitorElement* dijet_PFnemf;
-  MonitorElement* dijet_PFJetMulti;
+  std::vector<MonitorElement*> dijet_PFJet1_pt;
+  std::vector<MonitorElement*> dijet_PFJet1_eta;
+  std::vector<MonitorElement*> dijet_PFJet1_phi;
+  std::vector<MonitorElement*> dijet_PFJet1_rapidity;
+  std::vector<MonitorElement*> dijet_PFJet1_mass;
+  std::vector<MonitorElement*> dijet_PFJet2_pt;
+  std::vector<MonitorElement*> dijet_PFJet2_eta;
+  std::vector<MonitorElement*> dijet_PFJet2_phi;
+  std::vector<MonitorElement*> dijet_PFJet2_rapidity;
+  std::vector<MonitorElement*> dijet_PFJet2_mass;
+  std::vector<MonitorElement*> dijet_deltaPhiPFJet1PFJet2;
+  std::vector<MonitorElement*> dijet_deltaEtaPFJet1PFJet2;
+  std::vector<MonitorElement*> dijet_deltaRPFJet1PFJet2;
+  std::vector<MonitorElement*> dijet_invMassPFJet1PFJet2;
+  std::vector<MonitorElement*> dijet_PFchef;
+  std::vector<MonitorElement*> dijet_PFnhef;
+  std::vector<MonitorElement*> dijet_PFcemf;
+  std::vector<MonitorElement*> dijet_PFnemf;
+  std::vector<MonitorElement*> dijet_PFJetMulti;
   //
   double dijet_PFJet1_pt_cut_;
   double dijet_PFJet2_pt_cut_;
@@ -286,7 +276,7 @@ private:
   MonitorElement* dimuon_Charge;
   MonitorElement* dimuon_deltaEtaMuon1Muon2;
   MonitorElement* dimuon_deltaPhiMuon1Muon2;
-  MonitorElement* dimuon_deltaRMuon1Muon2; 
+  MonitorElement* dimuon_deltaRMuon1Muon2;
   MonitorElement* dimuon_invMassMuon1Muon2;
   MonitorElement* dimuon_MuonMulti;
   //
@@ -306,13 +296,58 @@ private:
   MonitorElement* dielectron_Charge;
   MonitorElement* dielectron_deltaEtaElectron1Electron2;
   MonitorElement* dielectron_deltaPhiElectron1Electron2;
-  MonitorElement* dielectron_deltaRElectron1Electron2; 
+  MonitorElement* dielectron_deltaRElectron1Electron2;
   MonitorElement* dielectron_invMassElectron1Electron2;
   MonitorElement* dielectron_ElectronMulti;
   //
   double dielectron_Electron1_pt_cut_;
   double dielectron_Electron2_pt_cut_;
   int    dielectron_countElectron_;
+
+  ///////////////////////////
+  // Histograms - DiPhoton
+  //
+  MonitorElement* diphoton_Photon1_pt;
+  MonitorElement* diphoton_Photon1_energy;
+  MonitorElement* diphoton_Photon1_et;
+  MonitorElement* diphoton_Photon1_eta;
+  MonitorElement* diphoton_Photon1_etasc;
+  MonitorElement* diphoton_Photon1_phi;
+  MonitorElement* diphoton_Photon1_hovere_eb;
+  MonitorElement* diphoton_Photon1_hovere_ee;
+  MonitorElement* diphoton_Photon1_sigmaietaieta_eb;
+  MonitorElement* diphoton_Photon1_sigmaietaieta_ee;
+  MonitorElement* diphoton_Photon1_trksumptsolidconedr03_eb;
+  MonitorElement* diphoton_Photon1_trksumptsolidconedr03_ee;
+  MonitorElement* diphoton_Photon1_e1x5e5x5_eb;
+  MonitorElement* diphoton_Photon1_e1x5e5x5_ee;
+  MonitorElement* diphoton_Photon1_e2x5e5x5_eb;
+  MonitorElement* diphoton_Photon1_e2x5e5x5_ee;
+  MonitorElement* diphoton_Photon2_pt;
+  MonitorElement* diphoton_Photon2_energy;
+  MonitorElement* diphoton_Photon2_et;
+  MonitorElement* diphoton_Photon2_eta;
+  MonitorElement* diphoton_Photon2_etasc;
+  MonitorElement* diphoton_Photon2_phi;
+  MonitorElement* diphoton_Photon2_hovere_eb;
+  MonitorElement* diphoton_Photon2_hovere_ee;
+  MonitorElement* diphoton_Photon2_sigmaietaieta_eb;
+  MonitorElement* diphoton_Photon2_sigmaietaieta_ee;
+  MonitorElement* diphoton_Photon2_trksumptsolidconedr03_eb;
+  MonitorElement* diphoton_Photon2_trksumptsolidconedr03_ee;
+  MonitorElement* diphoton_Photon2_e1x5e5x5_eb;
+  MonitorElement* diphoton_Photon2_e1x5e5x5_ee;
+  MonitorElement* diphoton_Photon2_e2x5e5x5_eb;
+  MonitorElement* diphoton_Photon2_e2x5e5x5_ee;
+  MonitorElement* diphoton_deltaEtaPhoton1Photon2;
+  MonitorElement* diphoton_deltaPhiPhoton1Photon2;
+  MonitorElement* diphoton_deltaRPhoton1Photon2;
+  MonitorElement* diphoton_invMassPhoton1Photon2;
+  MonitorElement* diphoton_PhotonMulti;
+  //
+  double diphoton_Photon1_pt_cut_;
+  double diphoton_Photon2_pt_cut_;
+  int    diphoton_countPhoton_;
 
   ///////////////////////////
   // Histograms - MonoJet
@@ -370,46 +405,32 @@ private:
   double monoelectron_Electron_met_cut_;
   int    monoelectron_countElectron_;
 
-
-
-  /////////// Phat Stuff /////////
-  // Cuts - Long Lived
+  ///////////////////////////
+  // Histograms - DiPhoton
   //
-  double CaloJetPx[2];
-  double CaloJetPy[2];
-  double CaloJetPt[2];
-  double CaloJetEta[2];
-  double CaloJetPhi[2];
-  double CaloJetEMF[2];
-  double CaloJetfHPD[2];
-  double CaloJetn90[2];
+  MonitorElement* monophoton_Photon_pt;
+  MonitorElement* monophoton_Photon_energy;
+  MonitorElement* monophoton_Photon_et;
+  MonitorElement* monophoton_Photon_eta;
+  MonitorElement* monophoton_Photon_etasc;
+  MonitorElement* monophoton_Photon_phi;
+  MonitorElement* monophoton_Photon_hovere;
+  MonitorElement* monophoton_Photon_sigmaietaieta;
+  MonitorElement* monophoton_Photon_trksumptsolidconedr03;
+  MonitorElement* monophoton_Photon_e1x5e5x5;
+  MonitorElement* monophoton_Photon_e2x5e5x5;
+  MonitorElement* monophoton_PFMet;
+  MonitorElement* monophoton_PhotonPtOverPFMet;
+  MonitorElement* monophoton_PFMet_phi;
+  MonitorElement* monophoton_deltaPhiPhotonPFMet;
+  MonitorElement* monophoton_PhotonMulti;
   //
-
-  // Cuts - EI
-  //
-  double PFJetEIPx;
-  double PFJetEIPy;
-  double PFJetEIPt;
-  double PFJetEIEta;
-  double PFJetEIPhi;
-  double PFJetEINHEF;
-  double PFJetEICHEF;
-  double PFJetEINEMF;
-  double PFJetEICEMF;
+  double monophoton_Photon_pt_cut_;
+  double monophoton_Photon_met_cut_;
+  int    monophoton_countPhoton_;
 
   // Histograms - MultiJets Trigger
   //
-  // Histograms - LongLived
-  //
-  MonitorElement* ll_gammajet_sMajMajPhot;
-  MonitorElement* ll_gammajet_sMinMinPhot;
-  // Histograms - LongLived Trigger
-  //
-  // Histograms - EIComparison
-  MonitorElement* ei_pfjet1_pt;
-  MonitorElement* ei_pfmet_pt;
-  MonitorElement* ei_pfmuon_pt;
-  MonitorElement* ei_pfelectron_pt;
 
 };
 
