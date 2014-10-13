b062bb8ca286b86dac3e38ec68751e269f85e18c
Matthias Wolf <matthias.wolf@cern.ch>
2014-10-13 13:06:22 +0200

    Change python configs to match pset usage.

--- EventFilter/L1TRawToDigi/python/l1tDigiToRaw_cfi.py
+++ EventFilter/L1TRawToDigi/python/l1tDigiToRaw_cfi.py
@@ -2,14 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 l1tDigiToRaw = cms.EDProducer(
     "l1t::L1TDigiToRaw",
-    Packers = cms.vstring([
-        # "l1t::CaloTowerPackerFactory",
-        "l1t::EGammaPackerFactory",
-        "l1t::EtSumPackerFactory",
-        "l1t::JetPackerFactory",
-        "l1t::TauPackerFactory"
-        ]),
-    InputLabel = cms.InputTag("caloStage1FinalDigis"),
-    FedId = cms.int32(100),
+    InputLabel = cms.InputTag("caloStage2FinalDigis"),
+    FedId = cms.int32(1),
     FWId = cms.uint32(1)
 )
---
+++ EventFilter/L1TRawToDigi/test/test_DigiToRawToDigi.py
@@ -0,0 +1,76 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SingleElectronPt10_cfi.py -s GEN,SIM,DIGI,L1 --pileup=NoPileUp --geometry DB --conditions=auto:startup -n 1 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DIGI2RAW2DIGI')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.PoolSource("EmptySource")
+
+
+# Output definition
+process.output = cms.OutputModule(
+    "PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = cms.untracked.vstring("keep *",
+        "drop *_mix_*_*"),
+    fileName = cms.untracked.string('L1T_PACK_EDM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+
+# enable debug message logging for our modules
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    destinations   = cms.untracked.vstring(
+	'detailedInfo',
+	'critical'
+    ),
+    detailedInfo   = cms.untracked.PSet(
+	threshold  = cms.untracked.string('DEBUG') 
+    ),
+    debugModules = cms.untracked.vstring(
+        'l1tDigis',
+    )
+)
+
+
+# user stuff
+process.load("EventFilter.L1TDigiToRaw.l1tDigiToRaw_cfi.py")
+process.load("EventFilter.L1TDigiToRaw.l1tRawToDigi_cfi.py")
+
+# Path and EndPath definitions
+process.path = cms.Path(
+    process.l1tDigiToRaw
+    +process.l1tRawToDigi
+)
+
+process.out = cms.EndPath(
+    process.output
+)
