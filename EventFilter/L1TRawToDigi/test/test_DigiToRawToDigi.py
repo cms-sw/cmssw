5522a0180d3a216410ea72b28fbfd803642485da
Matthias Wolf <matthias.wolf@cern.ch>
2014-10-13 13:08:00 +0200

    Some testing config changes.

--- EventFilter/L1TRawToDigi/test/test_DigiToRawToDigi.py
+++ EventFilter/L1TRawToDigi/test/test_DigiToRawToDigi.py
@@ -22,7 +22,10 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 # Input source
-process.source = cms.PoolSource("EmptySource")
+process.source = cms.Source(
+        "PoolSource",
+        fileNames = cms.untracked.vstring("file:L1T_EDM.root")
+)
 
 
 # Output definition
@@ -49,26 +52,50 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
 process.MessageLogger = cms.Service(
     "MessageLogger",
     destinations   = cms.untracked.vstring(
-	'detailedInfo',
-	'critical'
+        'detailedInfo',
+        'critical'
     ),
     detailedInfo   = cms.untracked.PSet(
-	threshold  = cms.untracked.string('DEBUG') 
+        threshold  = cms.untracked.string('DEBUG')
     ),
     debugModules = cms.untracked.vstring(
-        'l1tDigis',
+        'l1tDigiToRaw', 'l1tRawToDigi'
     )
 )
 
 
+# plotting
+process.load("CommonTools.UtilAlgos.TFileService_cfi")
+process.TFileService.fileName = cms.string('l1t.root')
+
+import L1Trigger.L1TCalorimeter.l1tStage2CaloAnalyzer_cfi
+process.packPlots = L1Trigger.L1TCalorimeter.l1tStage2CaloAnalyzer_cfi.l1tStage2CaloAnalyzer.clone()
+process.packPlots.towerToken = cms.InputTag("l1tRawToDigi")
+process.packPlots.clusterToken = cms.InputTag("None")
+process.packPlots.egToken = cms.InputTag("l1tRawToDigi")
+process.packPlots.tauToken = cms.InputTag("l1tRawToDigi")
+process.packPlots.jetToken = cms.InputTag("l1tRawToDigi")
+process.packPlots.etSumToken = cms.InputTag("l1tRawToDigi")
+
+# plots from emulator
+process.simPlots = L1Trigger.L1TCalorimeter.l1tStage2CaloAnalyzer_cfi.l1tStage2CaloAnalyzer.clone()
+process.simPlots.towerToken = cms.InputTag("caloStage2Digis")
+process.simPlots.clusterToken = cms.InputTag("None")
+process.simPlots.egToken = cms.InputTag("caloStage2Digis")
+process.simPlots.tauToken = cms.InputTag("caloStage2Digis")
+process.simPlots.jetToken = cms.InputTag("caloStage2Digis")
+process.simPlots.etSumToken = cms.InputTag("caloStage2Digis")
+
 # user stuff
-process.load("EventFilter.L1TDigiToRaw.l1tDigiToRaw_cfi.py")
-process.load("EventFilter.L1TDigiToRaw.l1tRawToDigi_cfi.py")
+process.load("EventFilter.L1TRawToDigi.l1tDigiToRaw_cfi")
+process.load("EventFilter.L1TRawToDigi.l1tRawToDigi_cfi")
 
 # Path and EndPath definitions
 process.path = cms.Path(
     process.l1tDigiToRaw
     +process.l1tRawToDigi
+    +process.simPlots
+    +process.packPlots
 )
 
 process.out = cms.EndPath(
