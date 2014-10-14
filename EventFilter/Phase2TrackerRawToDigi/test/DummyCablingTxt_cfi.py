eda7a550624b44bd09e93d6abb85922cf1ea3628
jerome <jdefaver@gmail.com>
2014-10-14 09:01:29 +0200

    add module type to dummy cabling

--- EventFilter/Phase2TrackerRawToDigi/test/DummyCablingTxt_cfi.py
+++ EventFilter/Phase2TrackerRawToDigi/test/DummyCablingTxt_cfi.py
@@ -7,22 +7,26 @@ def make_vpset_fromfile(filename):
         fedid = 0
         channel = 0
         for line in f:
-            psets.append(cms.PSet(
-                moduleType=cms.string("2S"),
-                detid=cms.uint32(int(line)),
-                gbtid=cms.uint32(0),
-                fedid=cms.uint32(fedid),
-                fedch=cms.uint32(channel),
-                powerGroup=cms.uint32(0),
-                coolingLoop=cms.uint32(0))
-                )
-            channel += 1
-            if channel >= 72:
-                channel = 0
-                fedid += 1
+            line = line.split()
+            detid = int(line[0])
+            layer = int(line[1])
+            if(layer > 4):
+                type = (layer > 7) and "2S" or "PS"
+                psets.append(cms.PSet(
+                    moduleType=cms.string(type),
+                    detid=cms.uint32(detid),
+                    gbtid=cms.uint32(0),
+                    fedid=cms.uint32(fedid),
+                    fedch=cms.uint32(channel),
+                    powerGroup=cms.uint32(0),
+                    coolingLoop=cms.uint32(0))
+                    )
+                channel += 1
+                if channel == 72:
+                    channel = 0
+                    fedid += 1
     return psets
 
-# my_psets = make_vpset_fromfile('/afs/cern.ch/user/f/favereau/detids_tracker.txt')
-my_psets = make_vpset_fromfile('/afs/cern.ch/user/f/favereau/detids.txt')
+my_psets = make_vpset_fromfile('/afs/cern.ch/user/f/favereau/detids_layers.txt')
 
 Phase2TrackerCabling = cms.ESSource("Phase2TrackerCablingCfgESSource", modules = cms.VPSet( *my_psets ))
