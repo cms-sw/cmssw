234250a67cb5851d1fa8ba6867f1828bf4a6be74
Arnaud <arnaud.chiron@cern.ch>
2014-10-15 15:00:30 +0200

    MT

--- Validation/RecoEgamma/plugins/ElectronWebGetter.h
+++ Validation/RecoEgamma/plugins/ElectronWebGetter.h
@@ -2,14 +2,14 @@
 #ifndef Validation_RecoEgamma_ElectronWget_h
 #define Validation_RecoEgamma_ElectronWget_h
 
-#include "DQMOffline/EGamma/interface/ElectronDqmAnalyzerBase.h"
+#include "DQMOffline/EGamma/interface/ElectronDqmHarvesterBase.h"
 
-class ElectronWebGetter : public ElectronDqmAnalyzerBase
+class ElectronWebGetter : public ElectronDqmHarvesterBase
  {
   public:
     explicit ElectronWebGetter( const edm::ParameterSet & conf ) ;
     virtual ~ElectronWebGetter() ;
-    virtual void finalize() ;
+    virtual void finalize(DQMStore::IGetter & iGetter) ;
  } ;
 
 #endif
