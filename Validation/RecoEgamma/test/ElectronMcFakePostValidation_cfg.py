6029d14fedffd1fe65c4b685b1551130aa74c14f
Arnaud <arnaud.chiron@cern.ch>
2014-10-15 15:02:02 +0200

    MT temp

--- Validation/RecoEgamma/test/ElectronMcFakePostValidation_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcFakePostValidation_cfg.py
@@ -15,16 +15,23 @@ process.source = cms.Source("EmptySource")
 
 process.load("Validation.RecoEgamma.ElectronMcFakePostValidator_cfi")
 
-process.electronMcFakePostValidator.InputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
+t1 = os.environ['TEST_HISTOS_FILE'].split('.')
+process.electronMcFakePostValidator.InputFile = 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO.root'
+#process.electronMcFakePostValidator.InputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
 process.electronMcFakePostValidator.OutputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
+process.electronMcFakePostValidator.InputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
+process.electronMcFakePostValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
 
-#process.load("Configuration.StandardSequences.EDMtoMEAtJobEnd_cff")
-#process.dqmSaver.workflow = os.environ['DQM_WORKFLOW']
-#process.dqmsave_step = cms.Path(process.DQMSaver)
+process.load("Configuration.StandardSequences.EDMtoMEAtJobEnd_cff")
+process.dqmSaver.workflow = '/electronHistos/' + t1[1] + '/RECO'
+process.dqmsave_step = cms.Path(process.DQMSaver)
 #
 #process.p = cms.Path(process.electronMcFakePostValidator*process.dqmStoreStats*process.DQMSaver)
 process.p = cms.Path(process.electronMcFakePostValidator*process.dqmStoreStats)
 
-
+# Schedule
+process.schedule = cms.Schedule(process.p,
+                                process.dqmsave_step,
+)
 
 
