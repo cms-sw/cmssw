9872d07c4e86cf1b26b8a7131576e8e741e2cbef
Arnaud <arnaud.chiron@cern.ch>
2014-10-15 15:02:26 +0200

    MT

--- Validation/RecoEgamma/test/ElectronMcFakeValidation_gedGsfElectrons_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcFakeValidation_gedGsfElectrons_cfg.py
@@ -25,19 +25,48 @@ process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load("Configuration.StandardSequences.EDMtoMEAtJobEnd_cff") # new voir si on garde
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
+# load DQM
+process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
 from Configuration.AlCa.autoCond import autoCond
-process.GlobalTag.globaltag = autoCond[os.environ['TEST_GLOBAL_AUTOCOND']]
+#process.GlobalTag.globaltag = autoCond[os.environ['TEST_GLOBAL_AUTOCOND']]
 # next line is for old releases, prior to 7XX
-#process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']+'::All'
+process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']#+'::All'
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')  #for MC
+#print "globaltag : ", os.environ['TEST_GLOBAL_TAG'] # sort POSTLS172_V3
+
+
+# FOR DATA REDONE FROM RAW, ONE MUST HIDE IsoFromDeps
+# CONFIGURATION
 
 process.load("Validation.RecoEgamma.electronIsoFromDeps_cff")
 process.load("Validation.RecoEgamma.ElectronMcFakeValidator_gedGsfElectrons_cfi")
 
+# DQM
+process.dqmSaver.saveAtJobEnd = True
+#process.dqmSaver.convention = 'Offline'
+#process.dqmSaver.referenceHandling = cms.untracked.string('all')
+process.dqmsave_step = cms.Path(process.DQMSaver)
+#process.edmtome_step = cms.Path(process.EDMtoME) # new voir si on garde
+#process.DQMoutput_step = cms.EndPath( process.DQMoutput )
+
 process.electronMcFakeValidator.OutputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
+#process.electronMcSignalValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
 
-process.p = cms.Path(process.electronIsoFromDeps*process.electronMcFakeValidator*process.dqmStoreStats)
-#process.p = cms.Path(process.electronMcFakeValidator*process.dqmStoreStats)
+t1 = os.environ['TEST_HISTOS_FILE'].split('.')
+process.dqmSaver.workflow = '/electronHistos/' + t1[1] + '/RECO'
 
+process.p = cms.Path(process.electronMcFakeValidator*process.dqmStoreStats)
+#process.p = cms.Path(process.electronIsoFromDeps*process.electronMcFakeValidator*process.dqmStoreStats)
+
+# Schedule
+#process.schedule = cms.Schedule(process.edmtome_step, process.p,
+process.schedule = cms.Schedule(process.p,
+                                process.dqmsave_step,
+#                                process.DQMoutput_step
+)
 
