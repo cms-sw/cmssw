6e5c012bf47820848b419f79f7b2d22bca4b63c0
Andrea Venturi <andrea.venturi@cern.ch>
2014-10-15 18:25:49 +0200

    Debug log messages added

--- Geometry/TrackerNumberingBuilder/plugins/CmsTrackerLevelBuilder.cc
+++ Geometry/TrackerNumberingBuilder/plugins/CmsTrackerLevelBuilder.cc
@@ -2,12 +2,19 @@
 #include "DetectorDescription/Core/interface/DDFilteredView.h"
 #include "Geometry/TrackerNumberingBuilder/interface/GeometricDet.h"
 #include "Geometry/TrackerNumberingBuilder/plugins/ExtractStringFromDDD.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 
 void CmsTrackerLevelBuilder::build (
 				    DDFilteredView& fv, 
 				    GeometricDet* tracker,
 				    std::string attribute){
+
+  LogDebug("GeometricDetBuilding");
+  LogTrace("GeometricDetBuilding") << "GeometricDet name and type: " << tracker->name() << " " << tracker->type() << std::endl;
+  LogTrace("GeometricDetBuilding") << "Attribute string: " << attribute << std::endl;
+  LogTrace("GeometricDetBuilding") << "Filtered View logical parts: " << fv.logicalPart().ddname().name() << std::endl;
+  LogTrace("GeometricDetBuilding") << "Extracted string: " << ExtractStringFromDDD::getString(attribute,&fv) << std::endl;
 
  bool doLayers = fv.firstChild(); // descend to the first Layer  
 
