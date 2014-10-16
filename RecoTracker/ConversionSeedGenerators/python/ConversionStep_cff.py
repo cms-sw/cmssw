f51172085c5bd9c3c1c2b1631e8289848fd2afbb
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-10-16 09:03:30 +0200

    avoid regression in Conversions

--- RecoTracker/ConversionSeedGenerators/python/ConversionStep_cff.py
+++ RecoTracker/ConversionSeedGenerators/python/ConversionStep_cff.py
@@ -222,7 +222,7 @@ convStepChi2Est = RecoTracker.MeasurementDet.Chi2ChargeMeasurementEstimatorESPro
     ComponentName = cms.string('convStepChi2Est'),
     nSigma = cms.double(3.0),
     MaxChi2 = cms.double(30.0),
-    minGoodStripCharge = cms.double(2069)
+    minGoodStripCharge = cms.double(-2069.)
 )
 
 
