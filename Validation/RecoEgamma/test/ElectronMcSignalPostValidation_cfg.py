70193cf5140202fc78e7dad78bc6bda85078379c
Arnaud <arnaud.chiron@cern.ch>
2014-10-17 17:22:16 +0200

    python script

--- Validation/RecoEgamma/test/ElectronMcSignalPostValidation_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcSignalPostValidation_cfg.py
@@ -15,7 +15,7 @@ localFileInput = 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO.root
 localFileInput2 = 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO2.root' # temp
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 
-process.source = cms.Source("EmptySource")
+process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring("file:" + localFileInput))
 #process.source = cms.Source("DQMRootSource", fileNames = cms.untracked.vstring("file:" + localFileInput))
 
 process.load("Validation.RecoEgamma.ElectronMcSignalPostValidator_cfi")
@@ -24,14 +24,13 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("Configuration.StandardSequences.EDMtoMEAtJobEnd_cff")
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
-)
-
 process.electronMcSignalPostValidator.InputFile = localFileInput
-process.electronMcSignalPostValidator.OutputFile = localFileInput2 # temporaire
-#process.electronMcSignalPostValidator.InputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
-#process.electronMcSignalPostValidator.OutputFile = cms.string(os.environ['TEST_HISTOS_FILE'])
+process.electronMcSignalPostValidator.OutputFile = localFileInput2 # temp
+
+#process.electronMcSignalPostValidator.InputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
+#process.electronMcSignalPostValidator.OutputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
+process.electronMcSignalPostValidator.InputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
+process.electronMcSignalPostValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
 
 process.dqmSaver.workflow = '/electronHistos/' + t1[1] + '/RECO3'
 #
@@ -39,9 +38,6 @@ process.dqmsave_step = cms.Path(process.DQMSaver)
 #process.p = cms.Path(process.electronMcSignalPostValidator*process.dqmStoreStats*process.DQMSaver)
 #process.p = cms.Path(process.electronMcSignalPostValidator*process.dqmStoreStats)
 process.p = cms.Path(process.electronMcSignalPostValidator)
-
-process.electronMcSignalPostValidator.InputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
-process.electronMcSignalPostValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
 
 # Schedule
 process.schedule = cms.Schedule(process.p,
