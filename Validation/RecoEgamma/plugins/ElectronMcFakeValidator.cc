9042b737705d50745bcf7cea06e39b2206c54243
Arnaud <arnaud.chiron@cern.ch>
2014-10-17 17:18:46 +0200

    MultiThreading without DQMStore

--- Validation/RecoEgamma/plugins/ElectronMcFakeValidator.cc
+++ Validation/RecoEgamma/plugins/ElectronMcFakeValidator.cc
@@ -470,18 +470,10 @@ ElectronMcFakeValidator::ElectronMcFakeValidator( const edm::ParameterSet & conf
 
 void ElectronMcFakeValidator::bookHistograms( DQMStore::IBooker & iBooker, edm::Run const &, edm::EventSetup const & )
  {
-//  store_ = edm::Service<DQMStore>().operator->() ;
-//  if (!store_)
-//   { edm::LogError("ElectronDqmAnalyzerBase::prepareStore")<<"No DQMStore found !" ; }
-//  store_->setVerbose(verbosity_) ;
-  std::cout << "ElectronMcFakeValidator::bookHistograms inputFile_ : " << inputFile_ << std::endl;
-/*  if (inputFile_!="")
-   { store_->open(inputFile_) ; }*/
   iBooker.setCurrentFolder(outputInternalPath_) ;
 
   setBookIndex(-1) ;
   setBookPrefix("h") ;
-//  std::cout << "appel setBookStatOverflowFlag : " << set_StatOverflowFlag << std::endl;
   setBookStatOverflowFlag( set_StatOverflowFlag ) ;
   
   // matching object type
