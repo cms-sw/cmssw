5b638cafe4866febc1fdf77bd07790912ee7cc6b
Salavat <abdullin@mail.cern.ch>
2014-10-20 08:29:40 +0200

    HCAL NZS customization for Digitization

--- SLHCUpgradeSimulations/Configuration/python/hcalNZS.py
+++ SLHCUpgradeSimulations/Configuration/python/hcalNZS.py
@@ -2,10 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 def customise_hcalNZS(process):
     if hasattr(process,'simHcalDigis'):
-        process.simHcalDigis.useConfigZSvalues=cms.int32(1)
-        process.simHcalDigis.HBlevel=cms.int32(-999)
-        process.simHcalDigis.HElevel=cms.int32(-999)
-        process.simHcalDigis.HOlevel=cms.int32(-999)
-        process.simHcalDigis.HFlevel=cms.int32(-999)
+        process.simHcalDigis.markAndPass = cms.bool(True)
 
     return process
