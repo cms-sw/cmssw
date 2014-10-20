80b3bca798f563d0a9813a98e85f7c98ea3b440b
Andrea <andrea.rizzi@cern.ch>
2014-10-20 18:11:32 +0200

    removing trivial inits because they do nothing

--- PhysicsTools/Heppy/python/analyzers/objects/__init__.py
+++
@@ -1,8 +0,0 @@
-from PhysicsTools.Heppy.analyzers.objects.GeneratorAnalyzer import *
-from PhysicsTools.Heppy.analyzers.objects.JetAnalyzer import *
-from PhysicsTools.Heppy.analyzers.objects.LeptonAnalyzer import *
-from PhysicsTools.Heppy.analyzers.objects.PhotonAnalyzer import *
-from PhysicsTools.Heppy.analyzers.objects.TauAnalyzer import *
-from PhysicsTools.Heppy.analyzers.objects.VertexAnalyzer import *
-
-from PhysicsTools.Heppy.analyzers.objects.autophobj import *
