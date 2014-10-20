d276dc35b0a657b0e635fd331aeb975fa01efc30
Andrea <andrea.rizzi@cern.ch>
2014-10-20 10:48:05 +0200

    newest version of Tree(numpy) from Giovanni repo?

--- PhysicsTools/HeppyCore/python/statistics/tree.py
+++ PhysicsTools/HeppyCore/python/statistics/tree.py
@@ -1,14 +1,9 @@
-# Copyright (C) 2014 Giovanni Petrucciani 
-# https://github.com/cbernet/heppy/blob/master/LICENSE
-
 import numpy
 from ROOT import TTree
+import ROOT
 
 class Tree(object):
-    """ROOT tree adaptor, based on numpy arrays
-    #TODO do I have a better version of this code somewhere?
-    """
-
+    
     def __init__(self, name, title, defaultFloatType="D", defaultIntType="I"):
         self.vars = {}
         self.vecvars = {}
@@ -17,40 +12,38 @@ class Tree(object):
         self.vecdefaults = {}
         self.defaultFloatType = defaultFloatType
         self.defaultIntType = defaultIntType
+        self.fillers = {}
 
     def setDefaultFloatType(self, defaultFloatType):
         self.defaultFloatType = defaultFloatType
 
-    def setDefaultIntType(self, defaultFloatType):
+    def setDefaultIntType(self, defaultIntType):
         self.defaultIntType = defaultIntType
-
+        
     def copyStructure(self, tree):
         for branch in tree.GetListOfBranches():
-            name = branch.GetName()
+            name = branch.GetName() 
             typeName = branch.GetListOfLeaves()[0].GetTypeName()
             type = float
             if typeName == 'Int_t':
                 type = int
-            self.var(name, type)
-
-    def branch_(self, selfmap, varName, type, len, postfix="",
-                storageType="default", title=None):
-        """Backend function used to create scalar and vector branches.
+            self.var(name, type)            
+    
+    def branch_(self, selfmap, varName, type, len, postfix="", storageType="default", title=None):
+        """Backend function used to create scalar and vector branches. 
            Users should call "var" and "vector", not this function directly."""
-        if storageType == "default":
-            storageType = self.defaultIntType if type is int \
-                          else self.defaultFloatType
+        if storageType == "default": 
+            storageType = self.defaultIntType if type is int else self.defaultFloatType
         if type is float  :
-            if storageType == "F":
+            if storageType == "F": 
                 selfmap[varName]=numpy.zeros(len,numpy.float32)
                 self.tree.Branch(varName,selfmap[varName],varName+postfix+'/F')
             elif storageType == "D":
                 selfmap[varName]=numpy.zeros(len,numpy.float64)
                 self.tree.Branch(varName,selfmap[varName],varName+postfix+'/D')
             else:
-                raise RuntimeError, 'Unknown storage type %s for branch %s'\
-                      % (storageType, varName)
-        elif type is int:
+                raise RuntimeError, 'Unknown storage type %s for branch %s' % (storageType, varName)
+        elif type is int: 
             dtypes = {
                 "i" : numpy.uint32,
                 "s" : numpy.uint16,
@@ -61,59 +54,89 @@ class Tree(object):
                 "B" : numpy.int8,
                 "L" : numpy.int64,
             }
-            if storageType not in dtypes:
-                raise RuntimeError, 'Unknown storage type %s for branch %s'\
-                      % (storageType, varName)
+            if storageType not in dtypes: 
+                raise RuntimeError, 'Unknown storage type %s for branch %s' % (storageType, varName)
             selfmap[varName]=numpy.zeros(len,dtypes[storageType])
             self.tree.Branch(varName,selfmap[varName],varName+postfix+'/I')
         else:
-            raise RuntimeError, 'Unknown type %s for branch %s'\
-                  % (type, varName)
+            raise RuntimeError, 'Unknown type %s for branch %s' % (type, varName)
         if title:
             self.tree.GetBranch(varName).SetTitle(title)
 
-    def var(self, varName,type=float, default=-99,
-            title=None, storageType="default" ):
-        self.branch_(self.vars, varName, type, 1,
-                     title=title, storageType=storageType)
+    def var(self, varName,type=float, default=-99, title=None, storageType="default", filler=None ):
+        if type in [int, float]:
+            self.branch_(self.vars, varName, type, 1, title=title, storageType=storageType)
+            self.defaults[varName] = default
+        elif __builtins__['type'](type) == str:
+            # create a value, looking up the type from ROOT and calling the default constructor
+            self.vars[varName] = getattr(ROOT,type)()
+            if type in [ "TLorentzVector" ]: # custom streamer classes
+                self.tree.Branch(varName+".", type, self.vars[varName], 8000,-1)
+            else:
+                self.tree.Branch(varName+".", type, self.vars[varName])
+            if filler is None:
+                raise RuntimeError, "Error: when brancing with an object, filler should be set to a function that takes as argument an object instance and a value, and set the instance to the value (as otherwise python assignment of objects changes the address as well)"
+            self.fillers[varName] = filler
+        else:
+            raise RuntimeError, 'Unknown type %s for branch %s: it is not int, float or a string' % (type, varName)
         self.defaults[varName] = default
 
-    def vector(self, varName, lenvar, maxlen=None,
-               type=float, default=-99, title=None, storageType="default" ):
-        """either lenvar is a string, and maxlen an int (variable size array),
-        or lenvar is an int and maxlen is not specified (fixed array)"""
-        if __builtins__['type'](lenvar) == int:
-            #Mike need the __builtins__ since 'type' is a variable here :-/
-            #TODO colin that's not true, get rid of this.
-            self.branch_(self.vecvars, varName, type, lenvar, postfix="[%d]"\
-                         % lenvar, title=title, storageType=storageType)
-        else:
-            if maxlen == None: RuntimeError,\
-               'You must specify a maxlen if making a dynamic array';
-            self.branch_(self.vecvars, varName, type, maxlen, postfix="[%s]"\
-                         % lenvar, title=title, storageType=storageType)
+    def vector(self, varName, lenvar, maxlen=None, type=float, default=-99, title=None, storageType="default", filler=None ):
+        """either lenvar is a string, and maxlen an int (variable size array), or lenvar is an int and maxlen is not specified (fixed array)"""
+        if type in [int, float]:
+            if __builtins__['type'](lenvar) == int:  # need the __builtins__ since 'type' is a variable here :-/
+                self.branch_(self.vecvars, varName, type, lenvar, postfix="[%d]" % lenvar, title=title, storageType=storageType)
+            else:
+                if maxlen == None: RuntimeError, 'You must specify a maxlen if making a dynamic array';
+                self.branch_(self.vecvars, varName, type, maxlen, postfix="[%s]" % lenvar, title=title, storageType=storageType)
+        elif __builtins__['type'](type) == str:
+            self.vecvars[varName] = ROOT.TClonesArray(type,(lenvar if __builtins__['type'](lenvar) == int else maxlen))
+            if type in [ "TLorentzVector" ]: # custom streamer classes
+                self.tree.Branch(varName+".", self.vecvars[varName], 32000, -1)
+            else:
+                self.tree.Branch(varName+".", self.vecvars[varName])
+            if filler is None:
+                raise RuntimeError, "Error: when brancing with an object, filler should be set to a function that takes as argument an object instance and a value, and set the instance to the value (as otherwise python assignment of objects changes the address as well)"
+            self.fillers[varName] = filler
         self.vecdefaults[varName] = default
 
     def reset(self):
         for name,value in self.vars.iteritems():
-            value[0]=self.defaults[name]
+            if name in self.fillers:
+                self.fillers[name](value, self.defaults[name])
+            else:
+                value[0]=self.defaults[name]
         for name,value in self.vecvars.iteritems():
-            value.fill(self.vecdefaults[name])
-
+            if isinstance(value, numpy.ndarray):
+                value.fill(self.vecdefaults[name])
+            else:
+                if isinstance(value, ROOT.TObject) and value.ClassName() == "TClonesArray":
+                    value.ExpandCreateFast(0)
+            
     def fill(self, varName, value ):
-        self.vars[varName][0]=value
+        if isinstance(self.vars[varName], numpy.ndarray):
+            self.vars[varName][0]=value
+        else:
+            self.fillers[varName](self.vars[varName],value)
 
     def vfill(self, varName, values ):
         a = self.vecvars[varName]
-        for (i,v) in enumerate(values):
-            a[i]=v
+        if isinstance(a, numpy.ndarray):
+            for (i,v) in enumerate(values):
+                a[i]=v
+        else:
+            if isinstance(a, ROOT.TObject) and a.ClassName() == "TClonesArray":
+                a.ExpandCreateFast(len(values))
+            fillit = self.fillers[varName]
+            for (i,v) in enumerate(values):
+                fillit(a[i],v)
 
 if __name__=='__main__':
-
+    
     from ROOT import TFile
 
-    f = TFile('Tree.root','RECREATE')
-    t = Tree('Colin', 'Another test tree')
+    f = TFile('TreeNumpy.root','RECREATE')
+    t = TreeNumpy('Colin', 'Another test tree')
     t.var('a')
     t.var('b')
 
