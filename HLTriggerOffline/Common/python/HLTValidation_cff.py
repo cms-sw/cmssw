05f220955fe3c84484e45027b8077222b6b6f728
Silvio <silvio.donato@cern.ch>
2014-10-20 19:07:16 +0200

    Btag

--- HLTriggerOffline/Common/python/HLTValidation_cff.py
+++ HLTriggerOffline/Common/python/HLTValidation_cff.py
@@ -11,6 +11,7 @@ from HLTriggerOffline.SUSYBSM.SusyExoValidation_cff import *
 from HLTriggerOffline.Higgs.HiggsValidation_cff import *
 from HLTriggerOffline.Top.topHLTValidation_cff import *
 from HLTriggerOffline.B2G.b2gHLTValidation_cff import *
+from HLTriggerOffline.Btag.HltBtagValidation_cff import *
 from HLTriggerOffline.Exotica.ExoticaValidation_cff import *
 
 # offline dqm:
@@ -43,6 +44,7 @@ hltvalidation = cms.Sequence(
     +HiggsValidationSequence
     +ExoticaValidationSequence
     +b2gHLTriggerValidation
+    +hltbtagValidationSequence
     )
 
 
@@ -67,6 +69,7 @@ hltvalidation_fastsim = cms.Sequence(
     +HLTSusyExoValSeq_FastSim
     +HiggsValidationSequence
     +b2gHLTriggerValidation
+    +hltbtagValidationSequence
     )
 
 hltvalidation_preprod = cms.Sequence(
