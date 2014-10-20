cf7e30e31a31a8f679f0bf7e745a9a4fd7e00d7d
Andrea <andrea.rizzi@cern.ch>
2014-10-20 15:03:06 +0200

    implement init.py and some renaming

--- PhysicsTools/Heppy/python/analyzers/core/SkimAnalyzerCount.py
+++ PhysicsTools/Heppy/python/analyzers/core/SkimAnalyzerCount.py
@@ -7,22 +7,22 @@ from PhysicsTools.HeppyCore.statistics.counter import Counter, Counters
 
 from DataFormats.FWLite import Events, Handle,Lumis
 
-class skimAnalyzerCount( Analyzer ):
+class SkimAnalyzerCount( Analyzer ):
     #---------------------------------------------
     # TO FINDS THE INITIAL EVENTS BEFORE THE SKIM
     #---------------------------------------------
     
     def __init__(self, cfg_ana, cfg_comp, looperName):
-        super(skimAnalyzerCount, self).__init__(cfg_ana, cfg_comp, looperName)
+        super(SkimAnalyzerCount, self).__init__(cfg_ana, cfg_comp, looperName)
         self.useLumiBlocks = self.cfg_ana.useLumiBlocks if (hasattr(self.cfg_ana,'useLumiBlocks')) else True
  
     def declareHandles(self):
-        super(skimAnalyzerCount, self).declareHandles()
+        super(SkimAnalyzerCount, self).declareHandles()
         self.counterHandle = Handle("edm::MergeableCounter")
         self.mchandles['GenInfo'] = AutoHandle( ('generator','',''), 'GenEventInfoProduct' )
         
     def beginLoop(self):
-        super(skimAnalyzerCount,self).beginLoop()
+        super(SkimAnalyzerCount,self).beginLoop()
 
         self.counters.addCounter('SkimReport')
         self.count = self.counters.counter('SkimReport')
--- PhysicsTools/Heppy/python/analyzers/objects/__init__.py
+++ PhysicsTools/Heppy/python/analyzers/objects/__init__.py
@@ -1,8 +1,8 @@
-from PhysicsTools.Heppy.analyzer.core GeneratorAnalyzer import *
-from PhysicsTools.Heppy.analyzer.core JetAnalyzer import *
-from PhysicsTools.Heppy.analyzer.core LeptonAnalyzer import *
-from PhysicsTools.Heppy.analyzer.core PhotonAnalyzer import *
-from PhysicsTools.Heppy.analyzer.core TauAnalyzer import *
-from PhysicsTools.Heppy.analyzer.core VertexAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.GeneratorAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.JetAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.LeptonAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.PhotonAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.TauAnalyzer import *
+from PhysicsTools.Heppy.analyzer.core.VertexAnalyzer import *
 
-from PhysicsTools.Heppy.analyzer.core autofilltypes import *
+from PhysicsTools.Heppy.analyzer.core.autofilltypes import *
