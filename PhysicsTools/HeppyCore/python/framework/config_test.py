f62af3b83a52908706d1e08f1fbf3de1aa7cfc92
Colin <colin.bernet@cern.ch>
2014-10-20 11:57:56 +0200

    added unit test for the config module

---
+++ PhysicsTools/HeppyCore/python/framework/config_test.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+import shutil
+
+from PhysicsTools.HeppyCore.framework.config import *
+
+class ConfigTestCase(unittest.TestCase):
+
+    def test_analyzer(self):
+        class Ana1(object):
+            pass
+        ana1 = Analyzer(
+            Ana1,
+            toto = '1',
+            tata = 'a'
+            )
+        self.assertTrue( '/' not in ana1.name )
+
+    def test_MCComponent(self):
+        DYJets = MCComponent(
+            name = 'DYJets',
+            files ='blah_mc.root',
+            xSection = 3048.,
+            nGenEvents = 34915945,
+            triggers = ['HLT_MC'],
+            vertexWeight = 1.,
+            effCorrFactor = 1 )
+        self.assertTrue(True)
+
+    def test_config(self):
+        class Ana1(object):
+            pass
+        ana1 = Analyzer(
+            Ana1,
+            toto = '1',
+            tata = 'a'
+            )
+        comp1 = Component( 
+            'comp1',
+            files='*.root',
+            triggers='HLT_stuff'
+            )
+        from PhysicsTools.HeppyCore.framework.chain import Chain as Events
+        config = Config( components = [comp1],
+                         sequence = [ana1], 
+                         events_class = Events )
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
