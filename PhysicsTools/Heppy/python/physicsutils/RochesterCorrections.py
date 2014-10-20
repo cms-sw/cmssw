4d01bdd0ea3a3696e51eb36c440aa10b7d8243a5
Andrea <andrea.rizzi@cern.ch>
2014-10-20 17:52:28 +0200

    fix wrong import

--- PhysicsTools/Heppy/python/physicsutils/RochesterCorrections.py
+++ PhysicsTools/Heppy/python/physicsutils/RochesterCorrections.py
@@ -1,7 +1,7 @@
 import copy
 
 from ROOT import RochCor, RochCor2012, TLorentzVector
-from CMGTools.RootTools.utils.cmsswRelease import isNewerThan
+from PhysicsTools.Heppy.utils.cmsswRelease import isNewerThan
 
 is2012 = isNewerThan('CMSSW_5_2_X')
 
