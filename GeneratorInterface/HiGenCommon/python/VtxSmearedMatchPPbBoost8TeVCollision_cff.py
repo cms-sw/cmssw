275a2b13e2ffa4bf6d4a817c369a26885cef1bff
Matthew <Matthew.Nguyen@cern.ch>
2014-10-21 18:11:20 +0200

    updated pPb vertex smearing names, added reverse boost, and customise to switch vertex smearing and production filters (take 2)

--- GeneratorInterface/HiGenCommon/python/VtxSmearedMatchPPbBoost8TeVCollision_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
- 
-from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
-VtxSmeared = cms.EDProducer("MixBoostEvtVtxGenerator",
-                            signalLabel = cms.InputTag("hiSignal"), 
-                            heavyIonLabel = cms.InputTag("generator"),
-                            Beta=cms.double(-0.434)                          
-                            )
