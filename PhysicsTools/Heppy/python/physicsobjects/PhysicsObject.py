2960bbf7047a135d1ee6fdb4d1a5be17f56c2035
Colin <colin.bernet@cern.ch>
2014-10-22 15:10:43 +0200

    clean up in PhysicsObjects.py

--- PhysicsTools/Heppy/python/physicsobjects/PhysicsObject.py
+++ PhysicsTools/Heppy/python/physicsobjects/PhysicsObject.py
@@ -1,15 +1,8 @@
 import copy
 from PhysicsTools.Heppy.physicsobjects.Particle import Particle
 
-#COLIN should make a module for lorentz vectors (and conversions)
-#instanciating template
-from ROOT import Math
-PtEtaPhiE4DLV = Math.PtEtaPhiE4D(float)
-PtEtaPhiM4DLV = Math.PtEtaPhiM4D(float)
-
-
 class PhysicsObject(Particle):
-    '''Extends the cmg::PhysicsObject functionalities.'''
+    '''Wrapper to particle-like C++ objects.'''
 
     def __init__(self, physObj):
         self.physObj = physObj
@@ -17,7 +10,6 @@ class PhysicsObject(Particle):
 
     def __copy__(self):
         '''Very dirty trick, the physObj is deepcopied...'''
-        # print 'call copy', self
         physObj = copy.deepcopy( self.physObj )
         newone = type(self)(physObj)
         newone.__dict__.update(self.__dict__)
@@ -28,29 +20,10 @@ class PhysicsObject(Particle):
         p4 = self.physObj.p4()
         p4 *= scale 
         self.physObj.setP4( p4 )  
-##         p4 = self.physObj.polarP4()
-##         sp4 = PtEtaPhiE4DLV(
-##             p4.Pt()*scale,
-##             p4.Eta(),
-##             p4.Phi(),
-##             p4.E()
-##             )
-##         sp4.SetE( sp4.E()*scale )
-##         ptepmsp4 = PtEtaPhiM4DLV(
-##             sp4.Pt(),
-##             sp4.Eta(),
-##             sp4.Phi(),
-##             sp4.M()
-##             )
-##         self.physObj.setP4( p4.__class__(
-##             sp4.Pt(),
-##             sp4.Eta(),
-##             sp4.Phi(),
-##             sp4.M()
-##             ) )
         
         
     def __getattr__(self,name):
-        '''all accessors  from cmg::DiTau are transferred to this class.'''
+        '''Makes all attributes and methods of the wrapped physObj
+        directly available.'''
         return getattr(self.physObj, name)
 
