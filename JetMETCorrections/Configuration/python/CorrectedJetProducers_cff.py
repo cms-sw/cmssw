71182448700f13770857f3e82375db9a139e31a3
Volker Adler <volker.adler@cern.ch>
2014-10-22 21:55:07 +0200

    Remove unwanted JetCorrectors again.

--- JetMETCorrections/Configuration/python/CorrectedJetProducers_cff.py
+++ JetMETCorrections/Configuration/python/CorrectedJetProducers_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.JetCorrectors_cff import *
 
-# L1 Correction Producers
+# L1 Corrected Jet Producers
 ak4CaloJetsL1 = cms.EDProducer(
     'CorrectedCaloJetProducer',
     src        = cms.InputTag('ak4CaloJets'),
@@ -30,49 +30,31 @@ ak4TrackJetsL1 = cms.EDProducer(
     correctors = cms.VInputTag('ak4TrackL1FastjetCorrector')
     )
 
-
-
-# L2 Correction Producers
+# L2 Corrected Jet Producers
 ak4CaloJetsL2 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL2RelativeCorrector'])
 ak4PFJetsL2 = ak4PFJetsL1.clone(correctors = ['ak4PFL2RelativeCorrector'])
 ak4PFCHSJetsL2 = ak4PFCHSJetsL1.clone(correctors = ['ak4PFCHSL2RelativeCorrector'])
 ak4JPTJetsL2 = ak4JPTJetsL1.clone(correctors = ['ak4JPTL2RelativeCorrector'])
 ak4TrackJetsL2 = ak4TrackJetsL1.clone(correctors = ['ak4TrackL2RelativeCorrector'])
 
-# L2L3 Correction Producers
+# L2L3 Corrected Jet Producers
 ak4CaloJetsL2L3 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL2L3Corrector'])
 ak4PFJetsL2L3 = ak4PFJetsL1.clone(correctors = ['ak4PFL2L3Corrector'])
 ak4PFCHSJetsL2L3 = ak4PFCHSJetsL1.clone(correctors = ['ak4PFCHSL2L3Corrector'])
 ak4JPTJetsL2L3 = ak4JPTJetsL1.clone(correctors = ['ak4JPTL2L3Corrector'])
 ak4TrackJetsL2L3 = ak4TrackJetsL1.clone(correctors = ['ak4TrackL2L3Corrector'])
 
-# L1L2L3 Correction Producers
+# L1L2L3 Corrected Jet Producers
 ak4CaloJetsL1L2L3 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL1L2L3Corrector'])
 ak4PFJetsL1L2L3 = ak4PFJetsL1.clone(correctors = ['ak4PFL1L2L3Corrector'])
 ak4PFCHSJetsL1L2L3 = ak4PFCHSJetsL1.clone(correctors = ['ak4PFCHSL1L2L3Corrector'])
 ak4JPTJetsL1L2L3 = ak4JPTJetsL1.clone(correctors = ['ak4JPTL1L2L3Corrector'])
-ak4TrackJetsL1L2L3 = ak4TrackJetsL1.clone(correctors = ['ak4TrackL1L2L3Corrector'])
 
-# L1FastL2L3 Correction Producers
-ak4CaloJetsL1FastL2L3 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL1FastL2L3Corrector'])
-ak4PFJetsL1FastL2L3 = ak4PFJetsL1.clone(correctors = ['ak4PFL1FastL2L3Corrector'])
-ak4PFCHSJetsL1FastL2L3 = ak4PFCHSJetsL1.clone(correctors = ['ak4PFCHSL1FastL2L3Corrector'])
-ak4JPTJetsL1FastL2L3 = ak4JPTJetsL1.clone(correctors = ['ak4JPTL1FastL2L3Corrector'])
-ak4TrackJetsL1FastL2L3 = ak4TrackJetsL1.clone(correctors = ['ak4TrackL1FastL2L3Corrector'])
-
-# L1FastL2L3Residual Correction Producers
-ak4CaloJetsL1FastL2L3Residual = ak4CaloJetsL1.clone(correctors = ['ak4CaloL1FastL2L3ResidualCorrector'])
-ak4PFJetsL1FastL2L3Residual = ak4PFJetsL1.clone(correctors = ['ak4PFL1FastL2L3ResidualCorrector'])
-ak4PFCHSJetsL1FastL2L3Residual = ak4PFCHSJetsL1.clone(correctors = ['ak4PFCHSL1FastL2L3ResidualCorrector'])
-ak4JPTJetsL1FastL2L3Residual = ak4JPTJetsL1.clone(correctors = ['ak4JPTL1FastL2L3ResidualCorrector'])
-ak4TrackJetsL1FastL2L3Residual = ak4TrackJetsL1.clone(correctors = ['ak4TrackL1FastL2L3ResidualCorrector'])
-
-# L2L3L6 CORRECTION PRODUCERS
+# L2L3L6 Corrected Jet Producers
 ak4CaloJetsL2L3L6 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL2L3L6Corrector'])
 ak4PFJetsL2L3L6 = ak4PFJetsL1.clone(correctors = ['ak4PFL2L3L6Corrector'])
 
-
-# L1L2L3L6 CORRECTION PRODUCERS
+# L1L2L3L6 Corrected Jet Producers
 ak4CaloJetsL1L2L3L6 = ak4CaloJetsL1.clone(correctors = ['ak4CaloL1L2L3L6Corrector'])
 ak4PFJetsL1L2L3L6 = ak4PFJetsL1.clone(correctors = ['ak4PFL1L2L3L6Corrector'])
 
