814fe2a33aa29d6990dded7b84fefc343a170cb6
Matteo <matteosan1@gmail.com>
2014-10-22 10:16:02 +0200

    fix fillDescription error and reduce duplicated code

--- HLTrigger/HLTfilters/src/HLTDoubletDZ.cc
+++ HLTrigger/HLTfilters/src/HLTDoubletDZ.cc
@@ -33,6 +33,63 @@ HLTDoubletDZ<T1,T2>::HLTDoubletDZ(const edm::ParameterSet& iConfig) :
   inputTag2_(iConfig.template getParameter<edm::InputTag>("inputTag2")),
   inputToken1_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag1_)),
   inputToken2_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag2_)), 
+  //electronToken_ (consumes<reco::ElectronCollection>(iConfig.template getParameter<edm::InputTag>("electronTag"))),
+  triggerType1_(iConfig.template getParameter<int>("triggerType1")),
+  triggerType2_(iConfig.template getParameter<int>("triggerType2")),
+  minDR_ (iConfig.template getParameter<double>("MinDR")),
+  maxDZ_ (iConfig.template getParameter<double>("MaxDZ")),
+  min_N_    (iConfig.template getParameter<int>("MinN")),
+  checkSC_  (iConfig.template getParameter<bool>("checkSC")),
+  same_     (inputTag1_.encode()==inputTag2_.encode())      // same collections to be compared?
+{}
+
+template<>
+HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoEcalCandidate>::HLTDoubletDZ(const edm::ParameterSet& iConfig) : 
+  HLTFilter(iConfig),
+  originTag1_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag1")),
+  originTag2_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag2")),
+  inputTag1_(iConfig.template getParameter<edm::InputTag>("inputTag1")),
+  inputTag2_(iConfig.template getParameter<edm::InputTag>("inputTag2")),
+  inputToken1_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag1_)),
+  inputToken2_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag2_)), 
+  electronToken_ (consumes<reco::ElectronCollection>(iConfig.template getParameter<edm::InputTag>("electronTag"))),
+  triggerType1_(iConfig.template getParameter<int>("triggerType1")),
+  triggerType2_(iConfig.template getParameter<int>("triggerType2")),
+  minDR_ (iConfig.template getParameter<double>("MinDR")),
+  maxDZ_ (iConfig.template getParameter<double>("MaxDZ")),
+  min_N_    (iConfig.template getParameter<int>("MinN")),
+  checkSC_  (iConfig.template getParameter<bool>("checkSC")),
+  same_     (inputTag1_.encode()==inputTag2_.encode())      // same collections to be compared?
+{}
+
+template<>
+HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoEcalCandidate>::HLTDoubletDZ(const edm::ParameterSet& iConfig) : 
+  HLTFilter(iConfig),
+  originTag1_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag1")),
+  originTag2_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag2")),
+  inputTag1_(iConfig.template getParameter<edm::InputTag>("inputTag1")),
+  inputTag2_(iConfig.template getParameter<edm::InputTag>("inputTag2")),
+  inputToken1_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag1_)),
+  inputToken2_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag2_)), 
+  electronToken_ (consumes<reco::ElectronCollection>(iConfig.template getParameter<edm::InputTag>("electronTag"))),
+  triggerType1_(iConfig.template getParameter<int>("triggerType1")),
+  triggerType2_(iConfig.template getParameter<int>("triggerType2")),
+  minDR_ (iConfig.template getParameter<double>("MinDR")),
+  maxDZ_ (iConfig.template getParameter<double>("MaxDZ")),
+  min_N_    (iConfig.template getParameter<int>("MinN")),
+  checkSC_  (iConfig.template getParameter<bool>("checkSC")),
+  same_     (inputTag1_.encode()==inputTag2_.encode())      // same collections to be compared?
+{}
+
+template<>
+HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoChargedCandidate>::HLTDoubletDZ(const edm::ParameterSet& iConfig) : 
+  HLTFilter(iConfig),
+  originTag1_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag1")),
+  originTag2_(iConfig.template getParameter<std::vector<edm::InputTag> >("originTag2")),
+  inputTag1_(iConfig.template getParameter<edm::InputTag>("inputTag1")),
+  inputTag2_(iConfig.template getParameter<edm::InputTag>("inputTag2")),
+  inputToken1_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag1_)),
+  inputToken2_(consumes<trigger::TriggerFilterObjectWithRefs>(inputTag2_)), 
   electronToken_ (consumes<reco::ElectronCollection>(iConfig.template getParameter<edm::InputTag>("electronTag"))),
   triggerType1_(iConfig.template getParameter<int>("triggerType1")),
   triggerType2_(iConfig.template getParameter<int>("triggerType2")),
@@ -183,6 +240,93 @@ HLTDoubletDZ<T1, T2>::getCollections(edm::Event& iEvent, std::vector<T1Ref>& col
     return false;
 }
 
+template<typename T1, typename T2>
+bool 
+HLTDoubletDZ<T1,T2>::computeDZ(edm::Event& iEvent, T1Ref& r1, T2Ref& r2) const
+{
+
+  const reco::Candidate& candidate1(*r1);
+  const reco::Candidate& candidate2(*r2);
+  if ( reco::deltaR(candidate1, candidate2) < minDR_ ) 
+    return false;
+  if ( std::abs(candidate1.vz()-candidate2.vz()) > maxDZ_ ) 
+    return false;
+
+  return true;
+}
+
+template<>
+bool 
+HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoChargedCandidate>::computeDZ(edm::Event& iEvent, T1Ref& r1, T2Ref& r2) const
+{
+  edm::Handle<reco::ElectronCollection> electronHandle_;
+  iEvent.getByToken(electronToken_, electronHandle_);
+  if (!electronHandle_.isValid()) 
+    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
+
+  if ( reco::deltaR(*r1, *r2) < minDR_ ) 
+    return false;
+  reco::Electron e1;
+  for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
+    if (eleIt->superCluster() == r1->superCluster())
+      e1 = *(eleIt);
+  }
+  
+  const reco::Candidate& candidate2(*r2);	
+  if ( std::abs(e1.vz()-candidate2.vz()) > maxDZ_ ) 
+    return false;
+
+  return true;
+}
+
+template<>
+bool 
+HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoEcalCandidate>::computeDZ(edm::Event& iEvent, T1Ref& r1, T2Ref& r2) const
+{
+  edm::Handle<reco::ElectronCollection> electronHandle_;
+  iEvent.getByToken(electronToken_, electronHandle_);
+  if (!electronHandle_.isValid()) 
+    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
+
+  if ( reco::deltaR(*r1, *r2) < minDR_ ) 
+    return false;
+  reco::Electron e2;
+  for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
+    if (eleIt->superCluster() == r2->superCluster())
+      e2 = *(eleIt);
+  }
+  
+  const reco::Candidate& candidate1(*r1);	
+  if ( std::abs(e2.vz()-candidate1.vz()) > maxDZ_ ) 
+    return false;
+
+  return true;
+}
+
+template<>
+bool 
+HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoEcalCandidate>::computeDZ(edm::Event& iEvent, T1Ref& r1, T2Ref& r2) const
+{
+  edm::Handle<reco::ElectronCollection> electronHandle_;
+  iEvent.getByToken(electronToken_, electronHandle_);
+  if (!electronHandle_.isValid()) 
+    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
+
+  if ( reco::deltaR(*r1, *r2) < minDR_ ) 
+    return false;
+  reco::Electron e1, e2;
+  for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
+    if (eleIt->superCluster() == r2->superCluster())
+      e2 = *(eleIt);
+    if (eleIt->superCluster() == r1->superCluster())
+      e1 = *(eleIt);
+  }
+  
+  if ( std::abs(e2.vz()-e1.vz()) > maxDZ_ ) 
+    return false;
+
+  return true;
+}
 
 // ------------ method called to produce the data  ------------
 template<typename T1, typename T2>
@@ -205,7 +349,7 @@ HLTDoubletDZ<T1,T2>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup
     
     for (unsigned int i1=0; i1!=coll1.size(); i1++) {
       r1=coll1[i1];
-      const reco::Candidate& candidate1(*r1);
+      //const reco::Candidate& candidate1(*r1);
       unsigned int I(0);
       if (same_) {I=i1+1;}
       for (unsigned int i2=I; i2!=coll2.size(); i2++) {
@@ -216,10 +360,9 @@ HLTDoubletDZ<T1,T2>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup
 	  }
 	}
 	
-	const reco::Candidate& candidate2(*r2);
-	if ( reco::deltaR(candidate1, candidate2) < minDR_ ) continue;
-	if ( std::abs(candidate1.vz()-candidate2.vz()) > maxDZ_ ) continue;
-	
+	if (!computeDZ(iEvent, r1, r2))
+	  continue;
+	  	
 	n++;
 	filterproduct.addObject(triggerType1_,r1);
 	filterproduct.addObject(triggerType2_,r2);
@@ -232,177 +375,12 @@ HLTDoubletDZ<T1,T2>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup
   return accept;
 }
 
-
-template<>
-bool
-HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoChargedCandidate>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup, trigger::TriggerFilterObjectWithRefs & filterproduct) const
-{
- 
-  edm::Handle<reco::ElectronCollection> electronHandle_;
-  iEvent.getByToken(electronToken_, electronHandle_);
-  if (!electronHandle_.isValid()) 
-    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
-     
-  bool accept(false);
-  
-  std::vector<T1Ref> coll1;
-  std::vector<T2Ref> coll2;
-  
-  if (getCollections(iEvent, coll1, coll2, filterproduct)) { 
-    int n(0);
-    reco::RecoEcalCandidateRef r1;
-    T2Ref r2;
-
-    for (unsigned int i1=0; i1!=coll1.size(); i1++) {
-      r1 = coll1[i1];
-      unsigned int I(0);
-      if (same_) {I=i1+1;}
-      for (unsigned int i2=I; i2!=coll2.size(); i2++) {
-	r2=coll2[i2];
-	if (checkSC_) {
-	  if (r1->superCluster().isNonnull() && r2->superCluster().isNonnull()) {
-	    if (r1->superCluster() == r2->superCluster()) continue;
-	  }
-	}
-	
-	if ( reco::deltaR(*r1, *r2) < minDR_ ) continue;
-	reco::Electron e1;
-	for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
-	  if (eleIt->superCluster() == r1->superCluster())
-	    e1 = *(eleIt);
-	}
-
-	const reco::Candidate& candidate2(*r2);	
-	if ( std::abs(e1.vz()-candidate2.vz()) > maxDZ_ ) continue;
-	
-	n++;
-	filterproduct.addObject(triggerType1_,r1);
-	filterproduct.addObject(triggerType2_,r2);
-      }
-    }
-  
-    accept = accept || (n>=min_N_);
-  }
-  
-  return accept;
-}
-
-template<>
-bool
-HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoEcalCandidate>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup, trigger::TriggerFilterObjectWithRefs & filterproduct) const
-{
- 
-  edm::Handle<reco::ElectronCollection> electronHandle_;
-  iEvent.getByToken(electronToken_, electronHandle_);
-  if (!electronHandle_.isValid()) 
-    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
-     
-  bool accept(false);
-  
-  std::vector<T1Ref> coll1;
-  std::vector<T2Ref> coll2;
-  
-  if (getCollections(iEvent, coll1, coll2, filterproduct)) { 
-    int n(0);
-    T1Ref r1;
-    reco::RecoEcalCandidateRef r2;
-
-    for (unsigned int i1=0; i1!=coll1.size(); i1++) {
-      r1 = coll1[i1];
-      unsigned int I(0);
-      if (same_) {I=i1+1;}
-      for (unsigned int i2=I; i2!=coll2.size(); i2++) {
-	r2=coll2[i2];
-	if (checkSC_) {
-	  if (r1->superCluster().isNonnull() && r2->superCluster().isNonnull()) {
-	    if (r1->superCluster() == r2->superCluster()) continue;
-	  }
-	}
-	
-	if ( reco::deltaR(*r1, *r2) < minDR_ ) continue;
-	reco::Electron e2;
-	for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
-	  if (eleIt->superCluster() == r2->superCluster())
-	    e2 = *(eleIt);
-	}
-
-	const reco::Candidate& candidate1(*r1);	
-	if ( std::abs(e2.vz()-candidate1.vz()) > maxDZ_ ) continue;
-	
-	n++;
-	filterproduct.addObject(triggerType1_,r1);
-	filterproduct.addObject(triggerType2_,r2);
-      }
-    }
-  
-    accept = accept || (n>=min_N_);
-  }
-  
-  return accept;
-}
-
-template<>
-bool
-HLTDoubletDZ<reco::RecoEcalCandidate, reco::RecoEcalCandidate>::hltFilter(edm::Event& iEvent, const edm::EventSetup& iSetup, trigger::TriggerFilterObjectWithRefs & filterproduct) const
-{
- 
-  edm::Handle<reco::ElectronCollection> electronHandle_;
-  iEvent.getByToken(electronToken_, electronHandle_);
-  if (!electronHandle_.isValid()) 
-    edm::LogError("HLTDoubletDZ") << "HLTDoubletDZ: Electron Handle not valid.";
-     
-  bool accept(false);
-  
-  std::vector<T1Ref> coll1;
-  std::vector<T2Ref> coll2;
-  
-  if (getCollections(iEvent, coll1, coll2, filterproduct)) { 
-
-    int n(0);
-    reco::RecoEcalCandidateRef r1;
-    reco::RecoEcalCandidateRef r2;
-    
-    for (unsigned int i1=0; i1!=coll1.size(); i1++) {
-      r1 = coll1[i1];
-      unsigned int I(0);
-      if (same_) {I=i1+1;}
-      for (unsigned int i2=I; i2!=coll2.size(); i2++) {
-	r2=coll2[i2];
-	if (checkSC_) {
-	  if (r1->superCluster().isNonnull() && r2->superCluster().isNonnull()) {
-	    if (r1->superCluster() == r2->superCluster()) continue;
-	  }
-	}
-	
-	if ( reco::deltaR(*r1, *r2) < minDR_ ) continue;
-	reco::Electron e1, e2;
-	for(reco::ElectronCollection::const_iterator eleIt = electronHandle_->begin(); eleIt != electronHandle_->end(); eleIt++) {
-	  if (eleIt->superCluster() == r1->superCluster())
-	    e1 = *(eleIt);
-	  if (eleIt->superCluster() == r2->superCluster())
-	    e2 = *(eleIt);
-	}
-	
-	if ( std::abs(e1.vz()-e2.vz()) > maxDZ_ ) continue;
-	
-	n++;
-	filterproduct.addObject(triggerType1_,r1);
-	filterproduct.addObject(triggerType2_,r2);
-      }
-    }
-  
-    accept = accept || (n>=min_N_);
-  }
-  
-  return accept;
-}
-
 typedef HLTDoubletDZ<reco::Electron            , reco::Electron>             HLT2ElectronElectronDZ;
 typedef HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoChargedCandidate> HLT2MuonMuonDZ;
 typedef HLTDoubletDZ<reco::Electron            , reco::RecoChargedCandidate> HLT2ElectronMuonDZ;
 typedef HLTDoubletDZ<reco::RecoEcalCandidate   , reco::RecoEcalCandidate>    HLT2PhotonPhotonDZ;
-typedef HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoEcalCandidate>    HLT2PhotonMuonDZ;
-typedef HLTDoubletDZ<reco::RecoEcalCandidate   , reco::RecoChargedCandidate> HLT2MuonPhotonDZ;
+typedef HLTDoubletDZ<reco::RecoChargedCandidate, reco::RecoEcalCandidate>    HLT2MuonPhotonDZ;
+typedef HLTDoubletDZ<reco::RecoEcalCandidate   , reco::RecoChargedCandidate> HLT2PhotonMuonDZ;
 
 DEFINE_FWK_MODULE(HLT2ElectronElectronDZ);
 DEFINE_FWK_MODULE(HLT2MuonMuonDZ);
