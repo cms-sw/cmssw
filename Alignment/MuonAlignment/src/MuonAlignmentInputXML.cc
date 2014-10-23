0a0360faf67b9f27048d35e2f9a001ae4cfbbed6
Alexey <asvyatkovskiy@gmail.com>
2014-10-23 21:41:36 +0200

    Accident reverted change, putting back

--- Alignment/MuonAlignment/src/MuonAlignmentInputXML.cc
+++ Alignment/MuonAlignment/src/MuonAlignmentInputXML.cc
@@ -8,11 +8,12 @@
 //
 // Original Author:  Jim Pivarski
 //         Created:  Mon Mar 10 16:37:40 CDT 2008
-// $Id: MuonAlignmentInputXML.cc,v 1.17 2011/06/07 19:38:24 khotilov Exp $
+//$Id: MuonAlignmentInputXML.cc,v 1.16 2011/03/22 09:49:50 innocent Exp $
 //
 
 // system include files
 #include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Concurrency/interface/Xerces.h"
 
 // Xerces include files
 #include "xercesc/parsers/XercesDOMParser.hpp"
@@ -224,7 +225,7 @@ void MuonAlignmentInputXML::recursiveGetId(std::map<unsigned int, Alignable*> &a
    }
 }
 
-void MuonAlignmentInputXML::fillAliToIdeal(std::map<Alignable*, Alignable*> &alitoideal, const align::Alignables alignables, const align::Alignables ideals) const {
+void MuonAlignmentInputXML::fillAliToIdeal(std::map<Alignable*, Alignable*> &alitoideal, const align::Alignables& alignables, const align::Alignables& ideals) const {
    align::Alignables::const_iterator alignable = alignables.begin();
    align::Alignables::const_iterator ideal = ideals.begin();
 
@@ -257,7 +258,7 @@ AlignableMuon *MuonAlignmentInputXML::newAlignableMuon(const edm::EventSetup& iS
    recursiveGetId(ideal_alignableNavigator, ideal_alignableMuon->CSCEndcaps());
 
    try {
-      XMLPlatformUtils::Initialize();
+      cms::concurrency::xercesInitialize();
    }
    catch (const XMLException &toCatch) {
       throw cms::Exception("XMLException") << "Xerces XML parser threw an exception on initialization." << std::endl;
@@ -431,7 +432,7 @@ AlignableMuon *MuonAlignmentInputXML::newAlignableMuon(const edm::EventSetup& iS
    delete parser;
    delete errHandler;
 
-   XMLPlatformUtils::Terminate();
+   cms::concurrency::xercesTerminate();
 
    delete ideal_alignableMuon;
    return alignableMuon;
