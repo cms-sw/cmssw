35edd9211e7934eae421fffc0552e8fdd577ea0d
Patrick Gartung <gartung@fnal.gov>
2014-10-23 15:14:29 -0500

    Update TrunCastChecker to look at ImplicitCastExpr when the child is a BinaryOperator with at least one side of type EventNumber_t/

--- Utilities/StaticAnalyzers/src/TrunCastChecker.cpp
+++ Utilities/StaticAnalyzers/src/TrunCastChecker.cpp
@@ -1,6 +1,7 @@
 #include <clang/AST/Attr.h>
 #include <clang/AST/DeclCXX.h>
 #include <clang/AST/ExprCXX.h>
+#include <clang/AST/Expr.h>
 #include <clang/AST/Decl.h>
 #include <clang/AST/DeclTemplate.h>
 #include <clang/AST/StmtVisitor.h>
@@ -39,30 +40,51 @@ bool VisitImplicitCastExpr( ImplicitCastExpr *CE);
 
 bool ICEVisitor::VisitImplicitCastExpr( ImplicitCastExpr *CE )  
 {
+ 	const char *sfile=BR.getSourceManager().getPresumedLoc(CE->getExprLoc()).getFilename();
+	std::string sname(sfile);
+	if ( ! support::isInterestingLocation(sname) || ! support::isCmsLocalFile(sfile) ) return true;	
 	BR.getContext();
   	llvm::SmallString<100> buf;
   	llvm::raw_svector_ostream os(buf);
-	const Expr * SE = CE->getSubExprAsWritten();	
-	std::string sname = SE->getType().getAsString();
-	if (!(sname=="EventNumber_t")) return true;
+	const Expr * SE = CE->getSubExprAsWritten();
+	std::string sename = SE->getType().getAsString();
+	std::string evn = "EventNumber_t";
+	std::string lname;
+	std::string rname;
 	const clang::Expr *E = CE->getSubExpr();
+	if ( BinaryOperator::classof(SE) ) {
+		const BinaryOperator * BO = dyn_cast<BinaryOperator>(SE);
+		lname = BO->getLHS()->getType().getAsString();
+		rname = BO->getRHS()->getType().getAsString();
+	}
+	if (!(sename==evn || lname==evn || rname==evn )) return true;	
 	clang::QualType OrigTy = BR.getContext().getCanonicalType(E->getType());
 	clang::QualType ToTy = BR.getContext().getCanonicalType(CE->getType());
-	if (!OrigTy->isIntegerType()) return true;
-	if (!(ToTy->isIntegerType()||ToTy->isFloatingType())) return true;
-	uint64_t size_otype = BR.getContext().getTypeSize(OrigTy);
-	uint64_t size_ttype = BR.getContext().getTypeSize(ToTy);
+	if (!(ToTy->isIntegerType()||ToTy->isFloatingType()) ) return true;
+	if ( ToTy->isBooleanType() ) return true;
+	CharUnits size_otype = BR.getContext().getTypeSizeInChars(OrigTy);
+	CharUnits size_ttype = BR.getContext().getTypeSizeInChars(ToTy);
 	std::string oname = OrigTy.getAsString();
 	std::string tname = ToTy.getAsString();
-
-	if ( size_ttype < size_otype || ToTy->isFloatingType() ) {
-		os <<"Size of cast-to type, "<<tname<<", is smaller than cast-from type, "<<oname<<", and may result in truncation"; 
+	if ( ToTy->isFloatingType() ) {
+		os <<"Cast-to type, "<<tname<<". Cast-from type, "<<oname; 
 		clang::ento::PathDiagnosticLocation CELoc = clang::ento::PathDiagnosticLocation::createBegin(CE, BR.getSourceManager(),AC);
- 		BugType * BT = new BugType(CheckName(), "implicit cast size truncates","CMS code rules");
+ 		BugType * BT = new BugType(CheckName(), "implicit cast of int type to float type","CMS code rules");
 		BugReport * R = new BugReport(*BT,os.str(),CELoc);
 		R->addRange(CE->getSourceRange());
 		BR.emitReport(R);
 		return true;
+		} 
+	else {
+		if (  (size_otype > size_ttype) || ( size_otype == size_ttype && ToTy->getTypeClass() != OrigTy->getTypeClass() ) ) {
+			os <<"Cast-to type, "<<tname<<". Cast-from type, "<<oname<<". Cast may result in truncation."; 
+			clang::ento::PathDiagnosticLocation CELoc = clang::ento::PathDiagnosticLocation::createBegin(CE, BR.getSourceManager(),AC);
+ 			BugType * BT = new BugType(CheckName(), "implicit cast of int type to different int type could truncate","CMS code rules");
+			BugReport * R = new BugReport(*BT,os.str(),CELoc);
+			R->addRange(CE->getSourceRange());
+			BR.emitReport(R);
+			return true;
+		} 
 	}
 	return true;
 }
