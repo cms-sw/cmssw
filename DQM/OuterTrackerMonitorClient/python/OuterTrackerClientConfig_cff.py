54c21b94638471a87aa1dc92232fa4c231360c4e
Isis Van Parijs <isis.vanparijs@gmail.com>
2014-10-24 15:02:46 +0200

    getting started with stubs

--- DQM/OuterTrackerMonitorClient/python/OuterTrackerClientConfig_cff.py
+++ DQM/OuterTrackerMonitorClient/python/OuterTrackerClientConfig_cff.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQM.OuterTrackerMonitorClient.OuterTrackerMonitorClusterClient_cfi import *
+from DQM.OuterTrackerMonitorClient.OuterTrackerMonitorStubClient_cfi import *
 
-OuterTrackerClient = cms.Sequence(OuterTrackerMonitorClusterClient)
+OuterTrackerClient = cms.Sequence(OuterTrackerMonitorClusterClient *
+				  OuterTrackerMonitorStubClient)
 
