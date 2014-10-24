cfc82ecd850fc556145a3610c2d05d98cb3f147e
Martin Grunewald <Martin.Grunewald@cern.ch>
2014-10-24 17:36:04 +0200

    HLT saveTags fix in filter config

--- HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_2_1/GRun/V9 (CMSSW_7_2_0)
+# /dev/CMSSW_7_2_1/GRun/V10 (CMSSW_7_2_0)
 
 import FWCore.ParameterSet.Config as cms
 from FastSimulation.HighLevelTrigger.HLTSetup_cff import *
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_2_1/GRun/V9')
+  tableName = cms.string('/dev/CMSSW_7_2_1/GRun/V10')
 )
 
 HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
@@ -7015,7 +7015,7 @@ hltPreEle17Ele12Ele10CaloIdTrackId = cms.EDFilter( "HLTPrescaler",
 hltEGL1EG12EG7EG5Filter = cms.EDFilter( "HLTEgammaL1MatchFilterRegional",
     doIsolated = cms.bool( False ),
     endcap_end = cms.double( 2.65 ),
-    saveTags = cms.bool( True ),
+    saveTags = cms.bool( False ),
     region_eta_size_ecap = cms.double( 1.0 ),
     barrel_end = cms.double( 1.4791 ),
     l1IsolatedTag = cms.InputTag( 'l1extraParticles','Isolated' ),
