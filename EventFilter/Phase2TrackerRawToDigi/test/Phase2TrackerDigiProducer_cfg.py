9052dae3dcc1ec5626565fb80cf30a7505c0712c
jerome <jdefaver@gmail.com>
2014-10-24 09:37:32 +0200

    new configs with manual cabling

--- EventFilter/Phase2TrackerRawToDigi/test/Phase2TrackerDigiProducer_cfg.py
+++ EventFilter/Phase2TrackerRawToDigi/test/Phase2TrackerDigiProducer_cfg.py
@@ -4,31 +4,36 @@ import sys
 process = cms.Process("RawToDigi")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+# process.MessageLogger = cms.Service("MessageLogger",
+#        destinations   = cms.untracked.vstring('detailedInfo', 'critical' ),
+#        detailedInfo   = cms.untracked.PSet( threshold  = cms.untracked.string('DEBUG') ),
+#        debugModules = cms.untracked.vstring( 'Phase2TrackerDigitestproducer', 'Phase2TrackerFEDBuffer' )
+# )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
-# Input source
-process.source = cms.Source("NewEventStreamFileReader",
+
+process.source = cms.Source("PoolSource",
+# process.source = cms.Source("NewEventStreamFileReader",
     fileNames = cms.untracked.vstring( 'file:'+sys.argv[-1])
 )
 
+
 #process.load('CalibTracker.SiStripESProducers.fake.Phase2TrackerConfigurableCablingESSource_cfi')
-process.load('TestbeamCabling_cfi')
+#process.load('TestbeamCabling_cfi')
+process.load('DummyCablingTxt_cfi')
 process.load('EventFilter.Phase2TrackerRawToDigi.Phase2TrackerDigiProducer_cfi')
+process.Phase2TrackerDigitestproducer.ProductLabel = cms.InputTag("Phase2TrackerDigiToRawProducer")
 # process.load('EventFilter.Phase2TrackerRawToDigi.Phase2TrackerCommissioningDigiProducer_cfi')
 
-process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('myOutputFile.root')
-)
-
-# process.output = cms.OutputModule(
-#     "PoolOutputModule",
-#     fileName = cms.untracked.string('output.root'),
-#     outputCommands = cms.untracked.vstring(
-#       'drop *',
-#       'keep *_*_*_DigiToRawToDigi'
-#       )
-#     )
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('rawtodigi.root'),
+    outputCommands = cms.untracked.vstring(
+      'drop *',
+      'keep *_Phase2TrackerDigitestproducer_*_*'
+      )
+    )
   
 process.p = cms.Path(process.Phase2TrackerDigitestproducer)
 # process.p = cms.Path(process.Phase2TrackerDigitestproducer*process.Phase2TrackerDigiCondDataproducer)
--- EventFilter/Phase2TrackerRawToDigi/test/Phase2TrackerDigiToRawProducer_cfg.py
+++ EventFilter/Phase2TrackerRawToDigi/test/Phase2TrackerDigiToRawProducer_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("DigiToRaw")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 # Input source
 process.source = cms.Source("PoolSource",
@@ -15,14 +15,15 @@ process.source = cms.Source("PoolSource",
 process.load('Configuration.Geometry.GeometryExtendedPhase2TkBEReco_cff')
 process.load('DummyCablingTxt_cfi')
 process.load('EventFilter.Phase2TrackerRawToDigi.Phase2TrackerDigiToRawProducer_cfi')
+process.load("Geometry.TrackerGeometryBuilder.StackedTrackerGeometry_cfi")
 
 process.out = cms.OutputModule(
     "PoolOutputModule",
     fileName = cms.untracked.string('digi2raw.root'),
     outputCommands = cms.untracked.vstring(
       'drop *',
-#      'keep *_Phase2TrackerDigiToRawProducer_*_*',
-      'keep *_siPixelClusters_*_*'
+      'keep *_Phase2TrackerDigiToRawProducer_*_*'
+#      'keep *_siPixelClusters_*_*'
       )
     )
 
