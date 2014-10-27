6158ee5903e9f63bc01361f5667810648952a4e8
Arnaud <arnaud.chiron@cern.ch>
2014-10-27 13:36:25 +0100

    last version

--- Validation/RecoEgamma/test/ElectronMcFakeValidation_gedGsfElectrons_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcFakeValidation_gedGsfElectrons_cfg.py
@@ -16,7 +16,6 @@ localFileInput = 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO.root
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(),secondaryFileNames = cms.untracked.vstring())
-#process.source = cms.Source("DQMRootSource",fileNames = cms.untracked.vstring())
 process.source.fileNames.extend(dd.search())
 
 process.load('Configuration.StandardSequences.Services_cff')
