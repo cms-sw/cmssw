2de195a3e736eb3d096b96c22ca2619b1746335f
Isabelle <isa.dbruyn@gmail.com>
2014-10-27 15:02:05 +0100

    cleaned up OuterTrackerMonitorClusterClient includes

--- DQM/OuterTrackerMonitorCluster/python/OuterTrackerMonitorCluster_cfi.py
+++ DQM/OuterTrackerMonitorCluster/python/OuterTrackerMonitorCluster_cfi.py
@@ -20,8 +20,8 @@ OuterTrackerMonitorCluster = cms.EDAnalyzer('OuterTrackerMonitorCluster',
     
 # Cluster Width vs. I/O sensor
     TH2TTCluster_Width = cms.PSet(
-        Nbinsx = cms.int32(10),
-        xmax = cms.double(9.5),                      
+        Nbinsx = cms.int32(7),
+        xmax = cms.double(6.5),                      
         xmin = cms.double(-0.5),
         Nbinsy = cms.int32(2),
         ymax = cms.double(1.5),                      
