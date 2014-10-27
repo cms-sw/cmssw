a604c12145caa2a1ef187cbe194fce1380f0cb69
pgunnell <paolo.gunnellini@cern.ch>
2014-10-27 11:52:47 +0100

    Update Pythia8CUETP8M1Settings_cfi.py

--- Configuration/Generator/python/Pythia8CUETP8M1Settings_cfi.py
+++ Configuration/Generator/python/Pythia8CUETP8M1Settings_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-pythia8CUEP8M1SettingsBlock = cms.PSet(
-    pythia8CUEP8M1Settings = cms.vstring(
+pythia8CUETP8M1SettingsBlock = cms.PSet(
+    pythia8CUETP8M1Settings = cms.vstring(
         'Tune:pp 14',
         'Tune:ee 7',
         'MultipleInteractions:pT0Ref=2.4024',
