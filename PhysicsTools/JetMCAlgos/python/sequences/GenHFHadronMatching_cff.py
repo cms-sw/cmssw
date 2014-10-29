2336ad04f0a1974cc1ddc723447d8e169871e4cf
Nazar Bartosik <nazar.bartosik@desy.de>
2014-10-29 14:19:19 +0100

    Clean up of the code. Removed sequences and switched to unscheduled mode in the test configuration. Added AOD and miniAOD input files accessible from CERN to the test configuration.

--- PhysicsTools/JetMCAlgos/python/sequences/GenHFHadronMatching_cff.py
+++ PhysicsTools/JetMCAlgos/python/sequences/GenHFHadronMatching_cff.py
@@ -1,15 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-# Supplies PDG ID to real name resolution of MC particles
-from SimGeneral.HepPDTESSource.pythiapdt_cfi import *
-
-# Ghost particle collection
-from PhysicsTools.JetMCAlgos.HadronAndPartonSelector_cfi import selectedHadronsAndPartons
-selectedHadronsAndPartons = selectedHadronsAndPartons.clone()
-
 # Flavour info: jet collection with all associated ghosts
 from PhysicsTools.JetMCAlgos.AK5PFJetsMCFlavourInfos_cfi import ak5JetFlavourInfos
-genJetFlavourInfos = ak5JetFlavourInfos.clone(
+genJetFlavourPlusLeptonInfos = ak5JetFlavourInfos.clone(
     leptons = cms.InputTag("selectedHadronsAndPartons","leptons")
 )
 
@@ -18,31 +11,15 @@ from PhysicsTools.JetMCAlgos.GenHFHadronMatcher_cfi import matchGenHFHadron
 
 
 # Configuration for matching B-hadrons ================================================================
-matchGenBHadron = matchGenHFHadron.clone()
-matchGenBHadron.flavour = 5
-genBHadronMatchingSequence = cms.Sequence(
-    selectedHadronsAndPartons * 
-    genJetFlavourInfos * 
-    matchGenBHadron 
+matchGenBHadron = matchGenHFHadron.clone(
+    jetFlavourInfos = cms.InputTag("genJetFlavourPlusLeptonInfos"),
+    flavour = 5
 )
 
 
 # Configuration for matching C-hadrons =================================================================
-matchGenCHadron = matchGenHFHadron.clone()
-matchGenCHadron.flavour = 4
-genCHadronMatchingSequence = cms.Sequence(
-    selectedHadronsAndPartons * 
-    genJetFlavourInfos * 
-    matchGenCHadron 
+matchGenCHadron = matchGenHFHadron.clone(
+    jetFlavourInfos = cms.InputTag("genJetFlavourPlusLeptonInfos"),
+    flavour = 4
 )
-
-
-# Configuration for matching B- and C-hadrons ==========================================================
-genBCHadronMatchingSequence = cms.Sequence(
-    selectedHadronsAndPartons * 
-    genJetFlavourInfos * 
-    matchGenBHadron *
-    matchGenCHadron
-)
-
 
