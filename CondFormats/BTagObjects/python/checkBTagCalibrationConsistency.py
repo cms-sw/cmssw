008f87ab54ce5feb714fef18dc1587f199719007
Heiner Tholen <heiner.tholen@cern.ch>
2014-10-29 18:06:44 +0100

    consistency checker logic done

--- CondFormats/BTagObjects/python/checkBTagCalibrationConsistency.py
+++ CondFormats/BTagObjects/python/checkBTagCalibrationConsistency.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 import ROOT
 ROOT.gSystem.Load('libCondFormatsBTagObjects')
@@ -5,18 +6,28 @@ ROOT.gSystem.Load('libCondFormatsBTagObjects')
 
 def get_csv_data():
     return [
-        "0, comb, central, 0, 0, 1, 0, 1, 0, 999, \"2*x\" \n",
-        "0, comb, central, 0, 0, 1, 1, 2, 0, 999, \"2*x\" \n",
-        "0, comb, central, 0, 1, 2, 0, 1, 0, 999, \"-2*x\" \n",
-        "0, comb, central, 0, 1, 2, 1, 2, 0, 999, \"-2*x\" \n",
-        "3, comb, central, 0, 0, 1, 0, 1, 2, 3, \"2*x\" \n",
-        "3, comb, central, 0, -1, 0, 0, 1, 2, 3, \"-2*x\" \n",
+        "0, comb, central, 0, 0, 2.4, 20, 100, 0, 999, \"2*x\" \n",
+        "0, comb, central, 0, 0, 2.4, 100, 1000, 0, 999, \"2*x\" \n",
+        "0, ttbar, central, 0, 0, 2.4, 20, 1000, 0, 999, \"2*x\" \n",
     ]
+
+
+ETA_MIN = -2.4
+ETA_MAX = 2.4
+PT_MIN = 20.
+PT_MAX = 1000.
+DISCR_MIN = 0.
+DISCR_MAX = 999.
 
 
 class DataLoader(object):
     def __init__(self, csv_data):
+
+        # list of entries
         ens = list(ROOT.BTagEntry(l) for l in csv_data)
+        self.entries = ens
+
+        # sets of fixed data
         self.ops = set(e.params.operatingPoint for e in ens)
         self.flavs = set(e.params.jetFlavor for e in ens)
         self.meass = set(e.params.measurementType for e in ens)
@@ -24,7 +35,48 @@ class DataLoader(object):
         self.etas = set((e.params.etaMin, e.params.etaMax) for e in ens)
         self.pts = set((e.params.ptMin, e.params.ptMax) for e in ens)
         self.discrs = set((e.params.discrMin, e.params.discrMax) for e in ens)
-        self.entries = ens
+
+        self.full_eta_mode = any(eta_min < 0. for eta_min, _ in self.etas)
+
+        # test points for variable data (using bound +- epsilon)
+        eps = 1e-4
+        eta_min = ETA_MIN if self.full_eta_mode else 0.
+        self.eta_test_points = list(itertools.ifilter(
+            lambda x: eta_min < x < ETA_MAX,
+            itertools.chain(
+                (a + eps for a, _ in self.etas),
+                (a - eps for a, _ in self.etas),
+                (b + eps for _, b in self.etas),
+                (b - eps for _, b in self.etas),
+                (eta_min + eps, ETA_MAX - eps),
+            )
+        ))
+        self.pt_test_points = list(itertools.ifilter(
+            lambda x: PT_MIN < x < PT_MAX,
+            itertools.chain(
+                (a + eps for a, _ in self.pts),
+                (a - eps for a, _ in self.pts),
+                (b + eps for _, b in self.pts),
+                (b - eps for _, b in self.pts),
+                (PT_MIN + eps, PT_MAX - eps),
+            )
+        ))
+        self.discr_test_points = list(itertools.ifilter(
+            lambda x: DISCR_MIN < x < DISCR_MAX,
+            itertools.chain(
+                (a + eps for a, _ in self.discrs),
+                (a - eps for a, _ in self.discrs),
+                (b + eps for _, b in self.discrs),
+                (b - eps for _, b in self.discrs),
+                (DISCR_MIN + eps, DISCR_MAX - eps),
+            )
+        ))
+        # use sets
+        self.eta_test_points = set(round(f, 5) for f in self.eta_test_points)
+        self.pt_test_points = set(round(f, 5) for f in self.pt_test_points)
+        self.discr_test_points = set(round(f, 5) for f in self.discr_test_points)
+
+
 
         print "\nFound operating points (need at least 0, 1, 2):"
         print self.ops
@@ -39,16 +91,26 @@ class DataLoader(object):
               "also 'up_SYS'/'down_SYS' compatibility is checked):"
         print self.syss
 
-        print "\nFound eta ranges: (need everything covered from -2.4 or 0. " \
-              "up to 2.4):"
+        print "\nFound eta ranges: (need everything covered from %g or 0. " \
+              "up to %g):" % (ETA_MIN, ETA_MAX)
         print self.etas
 
-        print "\nFound pt ranges: (need everything covered from 20. to 1000.):"
+        print "\nFound pt ranges: (need everything covered from %g " \
+              "to %g):" % (PT_MIN, PT_MAX)
         print self.pts
 
         print "\nFound discr ranges: (only needed for operatingPoint==3, " \
-              "covered from 0. to 999.):"
+              "covered from %g to %g):" % (DISCR_MIN, DISCR_MAX)
         print self.discrs
+
+        print "\nTest points for eta:"
+        print self.eta_test_points
+
+        print "\nTest points for pt:"
+        print self.pt_test_points
+
+        print "\nTest points for discr:"
+        print self.discr_test_points
         print ""
 data = None
 
@@ -80,9 +142,78 @@ class BtagCalibConsistencyChecker(unittest.TestCase):
             elif "down" in sys:
                 self.assertIn(sys.replace("down", "up"), data.syss)
 
+    def test_eta_ranges(self):
+        for a, b in data.etas:
+            self.assertLess(a, b)
+            self.assertGreater(a, ETA_MIN - 1e-7)
+            self.assertLess(b, ETA_MAX + 1e-7)
+
+    def test_pt_ranges(self):
+        for a, b in data.pts:
+            self.assertLess(a, b)
+            self.assertGreater(a, PT_MIN - 1e-7)
+            self.assertLess(b, PT_MAX + 1e-7)
+
+    def test_discr_ranges(self):
+        for a, b in data.discrs:
+            self.assertLess(a, b)
+            self.assertGreater(a, DISCR_MIN - 1e-7)
+            self.assertLess(b, DISCR_MAX + 1e-7)
+
+    def test_coverage(self):
+        for op in data.ops:
+            for meas in data.meass:
+                for sys in data.syss:
+                    for flav in data.flavs:
+                        self._check_coverage(op, meas, sys, flav)
+
+    def _check_coverage(self, op, meas, sys, flav):
+        print "Checking coverage (op meas sys flav):", op, meas, sys, flav
+        ens = filter(
+            lambda e:
+            e.params.operatingPoint == op and
+            e.params.measurementType == meas and
+            e.params.sysType == sys and
+            e.params.jetFlavor == flav,
+            data.entries
+        )
+        for eta in data.eta_test_points:
+            for pt in data.pt_test_points:
+                tmp_eta_pt = filter(
+                    lambda e:
+                    e.params.etaMin < eta < e.params.etaMax and
+                    e.params.ptMin < pt < e.params.ptMax,
+                    ens
+                )
+                if op == 3:
+                    for discr in data.discr_test_points:
+                        tmp_eta_pt_discr = filter(
+                            lambda e:
+                            e.params.discrMin < discr < e.params.discrMax,
+                            tmp_eta_pt
+                        )
+                        size = len(tmp_eta_pt_discr)
+                        self.assertGreater(
+                            size, 0, "Region not covered: "
+                            "eta=%f, pt=%f, discr=%f)" % (eta, pt, discr)
+                        )
+                        self.assertLess(
+                            size, 2, "Region covered %d times: "
+                            "eta=%f, pt=%f, discr=%f)" % (size, eta, pt, discr)
+                        )
+                else:
+                    size = len(tmp_eta_pt)
+                    self.assertGreater(
+                        size, 0, "Region not covered: "
+                        "eta=%f, pt=%f)" % (eta, pt)
+                    )
+                    self.assertLess(
+                        size, 2, "Region covered %d times: "
+                        "eta=%f, pt=%f)" % (size, eta, pt)
+                    )
+
 
 if __name__ == '__main__':
-    global data
     data = DataLoader(get_csv_data())
     unittest.main()
 
