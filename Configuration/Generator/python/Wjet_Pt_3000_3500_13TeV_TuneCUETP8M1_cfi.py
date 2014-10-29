b9efeb0b3fe9167666d6c510cdc26570e3da4a79
inugent <you@somedomain.com>
2014-10-29 17:42:37 +0100

    more files...

--- Configuration/Generator/python/Wjet_Pt_3000_3500_13TeV_TuneCUETP8M1_cfi.py
+++ Configuration/Generator/python/Wjet_Pt_3000_3500_13TeV_TuneCUETP8M1_cfi.py
@@ -1,34 +1,31 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         comEnergy = cms.double(13000.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'WeakBosonAndParton:qqbar2Wg = on',
+            'WeakBosonAndParton:qqbar2Wg = on',
+            '24:onMode = off',
+            '24:onIfAny = 11 12',
+            '24:onIfAny = 13 14',
+            '24:onIfAny = 15 16',
+            '24:PhaseSpace:pTHatMin = 3000.',
+            '24:PhaseSpace:pTHatMax = 3500.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+ProductionFilterSequence = cms.Sequence(generator)
 
-from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
-    pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(1.0),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    crossSection = cms.untracked.double(0.000000313),
-    comEnergy = cms.double(13000.0),
-    maxEventsToPrint = cms.untracked.int32(0),
-    PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL        = 0    !User defined processes', 
-            'MSUB(16)     = 1    !qW production', 
-            'MSUB(31)     = 1    !gW production', 
-            'CKIN(3)     = 3000.    !minimum pthat for hard interaction', 
-            'CKIN(4)     = 3500.    !maximum pthat for hard interaction', 
-            'MDME(190,1) = 0    !W decay into dbar u', 
-            'MDME(191,1) = 0    !W decay into dbar c', 
-            'MDME(192,1) = 0    !W decay into dbar t', 
-            'MDME(194,1) = 0    !W decay into sbar u', 
-            'MDME(195,1) = 0    !W decay into sbar c', 
-            'MDME(196,1) = 0    !W decay into sbar t', 
-            'MDME(198,1) = 0    !W decay into bbar u', 
-            'MDME(199,1) = 0    !W decay into bbar c', 
-            'MDME(200,1) = 0    !W decay into bbar t', 
-            'MDME(206,1) = 1    !W decay into e+ nu_e', 
-            'MDME(207,1) = 1    !W decay into mu+ nu_mu', 
-            'MDME(208,1) = 1    !W decay into tau+ nu_tau'),
-        # This is a vector of ParameterSet names to be read, in this order
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters')
-    )
-)
--- Configuration/Generator/python/ZMM_13TeV_TuneCUETP8M1_cfi.py
+++ Configuration/Generator/python/ZMM_13TeV_TuneCUETP8M1_cfi.py
@@ -1,35 +1,29 @@
 import FWCore.ParameterSet.Config as cms
-
-from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(0),
-    pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(1.0),
-    comEnergy = cms.double(13000.0),
-    PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL         = 11 ', 
-            'MDME( 174,1) = 0    !Z decay into d dbar', 
-            'MDME( 175,1) = 0    !Z decay into u ubar', 
-            'MDME( 176,1) = 0    !Z decay into s sbar', 
-            'MDME( 177,1) = 0    !Z decay into c cbar', 
-            'MDME( 178,1) = 0    !Z decay into b bbar', 
-            'MDME( 179,1) = 0    !Z decay into t tbar', 
-            'MDME( 182,1) = 0    !Z decay into e- e+', 
-            'MDME( 183,1) = 0    !Z decay into nu_e nu_ebar', 
-            'MDME( 184,1) = 1    !Z decay into mu- mu+', 
-            'MDME( 185,1) = 0    !Z decay into nu_mu nu_mubar', 
-            'MDME( 186,1) = 0    !Z decay into tau- tau+', 
-            'MDME( 187,1) = 0    !Z decay into nu_tau nu_taubar', 
-            'CKIN( 1)     = 40.  !(D=2. GeV)', 
-            'CKIN( 2)     = -1.  !(D=-1. GeV)'),
-        # This is a vector of ParameterSet names to be read, in this order
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters')
-    )
-)
-
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         crossSection = cms.untracked.double(425.6),
+                         comEnergy = cms.double(13000.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'WeakSingleBoson:ffbar2gmZ = on',
+            '23:onMode = off',
+            '23:onIfAny = 13',
+            '23:PhaseSpace:pTHatMin = 40.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
 mumugenfilter = cms.EDFilter("MCParticlePairFilter",
     Status = cms.untracked.vint32(1, 1),
     MinPt = cms.untracked.vdouble(2.5, 2.5),
