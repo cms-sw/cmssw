8242b36d60fd34b6c6c88ce4ded4596b14ba1f50
cmsbuild <cmsbuild@cern.ch>
2014-10-30 23:33:06 +0100

    Merge pull request #6012 from cms-analysis-tools/CMSSW_7_3_X_AT_genHFHadronOrigin
    GenHFHadronMatcher using the official JetFlavour tool for hadron-jet association

--- PhysicsTools/JetMCAlgos/python/GenJetParticles_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from PhysicsTools.JetMCAlgos.GenJetParticles_cfi import *
-
-
-genParticlesForJetsNoNuPlusNoHadron = genParticlesForJetsPlusNoHadron.clone()
-genParticlesForJetsNoNuPlusNoHadron.ignoreParticleIDs += cms.vuint32( 12,14,16)
