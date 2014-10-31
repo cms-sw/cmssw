99a8c9257f2bf50fb104ee63b337f23a49888114
inugent <you@somedomain.com>
2014-10-31 15:09:15 +0100

    adding H200ZZ4L ...

--- Configuration/Generator/python/PhotonJet_Pt_10_13TeV_TuneCUETP8M1_cfi.py
+++ Configuration/Generator/python/PhotonJet_Pt_10_13TeV_TuneCUETP8M1_cfi.py
@@ -1,18 +1,23 @@
 import FWCore.ParameterSet.Config as cms
-
-from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(0),
-    pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(1.0),
-    comEnergy = cms.double(13000.0),
-    PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL=10              ! Pythia Photon+Jet processes', 
-            'CKIN(3)=10.          ! minimum pt hat for hard interactions'),
-        # This is a vector of ParameterSet names to be read, in this order
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters')
-    )
-)
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+source = cms.Source("EmptySource")
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         comEnergy = cms.double(13000.0),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'PromptPhoton:all = on',
+            'PhaseSpace:pTHatMin = 10.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
