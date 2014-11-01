b24a3eaa1209acf54a7c2c2cdd3fb6706f1cd260
Matthias Wolf <matthias.wolf@cern.ch>
2014-11-01 11:24:23 +0100

    Templatize and add egamma, tau, and forward jet packing.

--- EventFilter/L1TRawToDigi/src/implementations_stage1/CaloTokens.cc
+++ EventFilter/L1TRawToDigi/src/implementations_stage1/CaloTokens.cc
@@ -9,12 +9,13 @@ namespace l1t {
       CaloTokens::CaloTokens(const edm::ParameterSet& cfg, edm::ConsumesCollector& cc) : PackerTokens(cfg, cc)
       {
          auto tag = cfg.getParameter<edm::InputTag>("InputLabel");
+         auto tautag = cfg.getParameter<edm::InputTag>("TauInputLabel");
 
          towerToken_ = cc.consumes<CaloTowerBxCollection>(tag);
          egammaToken_ = cc.consumes<EGammaBxCollection>(tag);
          etSumToken_ = cc.consumes<EtSumBxCollection>(tag);
          jetToken_ = cc.consumes<JetBxCollection>(tag);
-         tauToken_ = cc.consumes<TauBxCollection>(tag);
+         tauToken_ = cc.consumes<TauBxCollection>(tautag);
       }
    }
 }
