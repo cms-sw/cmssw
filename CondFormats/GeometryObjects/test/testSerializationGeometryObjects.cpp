ddf977f1be5dfb5d7a0e031698fec4f07a281181
Ianna Osborne <ianna.osborne@cern.ch>
2014-11-03 17:21:36 +0100

    Add new objects

--- CondFormats/GeometryObjects/test/testSerializationGeometryObjects.cpp
+++ CondFormats/GeometryObjects/test/testSerializationGeometryObjects.cpp
@@ -14,6 +14,12 @@ int main()
     testSerialization<std::vector<PGeometricDet::Item>>();
     testSerialization<std::vector<PGeometricDetExtra::Item>>();
     testSerialization<PTrackerParameters>();
+    testSerialization<PTrackerParameters::PxbItem>();
+    testSerialization<PTrackerParameters::PxfItem>();
+    testSerialization<PTrackerParameters::TECItem>();
+    testSerialization<PTrackerParameters::TIBItem>();
+    testSerialization<PTrackerParameters::TIDItem>();
+    testSerialization<PTrackerParameters::TOBItem>();
 
     return 0;
 }
