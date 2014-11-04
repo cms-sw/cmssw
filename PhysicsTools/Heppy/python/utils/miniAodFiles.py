98c4f39f0974dd65f189d527e3cacaca1ed7e7bb
Colin <colin.bernet@cern.ch>
2014-11-04 13:01:02 +0100

    mini aod test files for CMSSW_7_3_X integration builds

--- PhysicsTools/Heppy/python/utils/miniAodFiles.py
+++ PhysicsTools/Heppy/python/utils/miniAodFiles.py
@@ -3,15 +3,20 @@ from PhysicsTools.Heppy.utils.cmsswRelease import cmsswRelease, releaseNumber
 def miniAodFiles():
     relnum = releaseNumber(cmsswRelease())
     files = []
-    if relnum[:2]==(7,1):
+    big, medium = relnum[:2] # e.g. 7, 3 for CMSSW_7_3_X
+    if (big,medium)==(7,1):
         files = [
             '/store/relval/CMSSW_7_1_10_patch2/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_71_V1-v1/00000/3E4EC015-AF53-E411-8889-0025905A6076.root',
                  '/store/relval/CMSSW_7_1_10_patch2/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_71_V1-v1/00000/C483E714-AF53-E411-9B9A-0025905B855E.root'
             ]
-    elif relnum[:2]==(7,0):
+    elif (big,medium)==(7,0):
         files = [
             '/store/relval/CMSSW_7_0_9_patch3/RelValZMM_13/MINIAODSIM/PU25ns_PLS170_V7AN2-v1/00000/E0D7EDE4-0660-E411-B655-02163E00EF88.root',
              '/store/relval/CMSSW_7_0_9_patch3/RelValZMM_13/MINIAODSIM/PU25ns_PLS170_V7AN2-v1/00000/F0F88B6E-905F-E411-8080-0025904B0FBE.root'
+            ]
+    elif (big,medium)==(7,3):
+        files = [
+            '/store/relval/CMSSW_7_3_0_pre1/RelValZMM_13/MINIAODSIM/PU25ns_PRE_LS172_V15-v1/00000/582D0582-355F-E411-9F30-02163E006D72.root'
             ]
     else:
         raise ValueError('no mini AOD file defined for release '+cmsswRelease())
