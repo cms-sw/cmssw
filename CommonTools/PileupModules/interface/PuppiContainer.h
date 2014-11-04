a42470431f1af5c1619222f23caf110bcc89adb9
Salvatore Rappoccio <rappoccio@gmail.com>
2014-11-04 10:25:37 -0600

    Implementing Puppi with code review suggestions

--- CommonTools/PileupModules/interface/PuppiContainer.h
+++ CommonTools/PileupModules/interface/PuppiContainer.h
@@ -2,12 +2,9 @@
 #define COMMONTOOLS_PUPPI_PUPPICONTAINER_H_
 
 #include "CommonTools/PileupModules/interface/PuppiAlgo.h"
-#include "CommonTools/PileupModules/interface/RecoObj.hh"
+#include "CommonTools/PileupModules/interface/RecoObj.h"
 #include "fastjet/internal/base.hh"
 #include "fastjet/PseudoJet.hh"
-
-using namespace std;
-using namespace fastjet;
 
 //FASTJET_BEGIN_NAMESPACE      // defined in fastjet/internal/base.hh
 
@@ -17,22 +14,22 @@ public:
     PuppiContainer(const edm::ParameterSet &iConfig);
     ~PuppiContainer(); 
     void initialize(const std::vector<RecoObj> &iRecoObjects);
-    std::vector<fastjet::PseudoJet> pfParticles(){ return fPFParticles; }    
-    std::vector<fastjet::PseudoJet> pvParticles(){ return fChargedPV; }        
-    const std::vector<double> puppiWeights();
-    std::vector<fastjet::PseudoJet> puppiParticles() { return fPupParticles;}
+    std::vector<fastjet::PseudoJet> const & pfParticles() const { return fPFParticles; }    
+    std::vector<fastjet::PseudoJet> const & pvParticles() const { return fChargedPV; }        
+    std::vector<double> const & puppiWeights() ;
+    std::vector<fastjet::PseudoJet> const & puppiParticles() const { return fPupParticles;}
 
 protected:
-    double  goodVar      (PseudoJet &iPart,std::vector<PseudoJet> &iParts, int iOpt,double iRCone);
-    void    getRMSAvg    (int iOpt,std::vector<fastjet::PseudoJet> &iConstits,std::vector<fastjet::PseudoJet> &iParticles,std::vector<fastjet::PseudoJet> &iChargeParticles);
+    double  goodVar      (fastjet::PseudoJet const &iPart,std::vector<fastjet::PseudoJet> const &iParts, int iOpt,double iRCone);
+    void    getRMSAvg    (int iOpt,std::vector<fastjet::PseudoJet> const &iConstits,std::vector<fastjet::PseudoJet> const &iParticles,std::vector<fastjet::PseudoJet> const &iChargeParticles);
     double  getChi2FromdZ(double iDZ);
-    int     getPuppiId   (const float &iPt,const float &iEta);
-    double  var_within_R (int iId, const vector<PseudoJet> & particles, const PseudoJet& centre, double R);  
+    int     getPuppiId   ( float iPt, float iEta);
+    double  var_within_R (int iId, const std::vector<fastjet::PseudoJet> & particles, const fastjet::PseudoJet& centre, double R);  
     
     std::vector<RecoObj>   fRecoParticles;
-    std::vector<PseudoJet> fPFParticles;
-    std::vector<PseudoJet> fChargedPV;
-    std::vector<PseudoJet> fPupParticles;
+    std::vector<fastjet::PseudoJet> fPFParticles;
+    std::vector<fastjet::PseudoJet> fChargedPV;
+    std::vector<fastjet::PseudoJet> fPupParticles;
     std::vector<double>    fWeights;
     std::vector<double>    fVals;
     bool   fApplyCHS;
