bdbb8d3a793f706362ebd06ec06f55ef66f76d4f
Isabelle <isa.dbruyn@gmail.com>
2014-11-05 14:14:05 +0100

    load StackedTrackerGeometry in OuterTrackerMonitorClient/OuterTrackerSourceConfig_cff.py

--- DQM/OuterTrackerMonitorClient/python/OuterTrackerSourceConfig_cff.py
+++ DQM/OuterTrackerMonitorClient/python/OuterTrackerSourceConfig_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from Geometry.TrackerGeometryBuilder.StackedTrackerGeometry_cfi import *
 from DQM.OuterTrackerMonitorCluster.OuterTrackerMonitorCluster_cfi import *
 from DQM.OuterTrackerMonitorStub.OuterTrackerMonitorStub_cfi import *
 
