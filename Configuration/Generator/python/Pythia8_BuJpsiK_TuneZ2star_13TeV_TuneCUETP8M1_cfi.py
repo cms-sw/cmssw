ea4de4ac7a92e9282c2b8ee0bd8b5c2814dd07a7
inugent <you@somedomain.com>
2014-11-06 14:21:26 +0100

    updated onia relval fragments

--- Configuration/Generator/python/Pythia8_BuJpsiK_TuneZ2star_13TeV_TuneCUETP8M1_cfi.py
+++ Configuration/Generator/python/Pythia8_BuJpsiK_TuneZ2star_13TeV_TuneCUETP8M1_cfi.py
@@ -5,8 +5,8 @@ from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
 source = cms.Source("EmptySource")
 generator = cms.EDFilter("Pythia8GeneratorFilter",
                          comEnergy = cms.double(13000.0),
-                         crossSection = cms.untracked.double(54000000000),
-                         filterEfficiency = cms.untracked.double(3.0e-4),
+                         crossSection = cms.untracked.double(2978915.),
+                         filterEfficiency = cms.untracked.double(1.59e-4),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
                          maxEventsToPrint = cms.untracked.int32(0),
                          pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -15,19 +15,20 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
             use_default_decay = cms.untracked.bool(False),
-            user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/Bs_mumu.dec'),
-            list_forced_decays = cms.vstring('MyB_s0','Myanti-B_s0'),
+            user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/Bu_JpsiK.dec'),
+            list_forced_decays = cms.vstring('MyB+','MyB-'),
             operates_on_particles = cms.vint32()
             ),
         parameterSets = cms.vstring('EvtGen130')
         ),
+                         #ExternalDecays = cms.PSet(
                          #EvtGen = cms.untracked.PSet(
-                         #operates_on_particles = cms.vint32( 0 ), # 0 (zero) means default list (hardcoded)
+                         #operates_on_particles = cms.vint32( 0 ), 
                          #use_default_decay = cms.untracked.bool(False),
                          #decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
                          #particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
-                         #user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Bs_mumu.dec'),
-                         #list_forced_decays = cms.vstring('MyB_s0','Myanti-B_s0'),
+                         #user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/Bu_JpsiK.dec'),
+                         #list_forced_decays = cms.vstring('MyB+','MyB-'),
                          #),
                          #parameterSets = cms.vstring('EvtGen')
                          #),
@@ -44,26 +45,36 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
 
 generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 
-MuMuFilter = cms.EDFilter("MCParticlePairFilter",
-                          Status = cms.untracked.vint32(1, 1),
-                          MinPt = cms.untracked.vdouble(3., 3.),
-                          MaxEta = cms.untracked.vdouble(2.5, 2.5),
-                          MinEta = cms.untracked.vdouble(-2.5, -2.5),
-                          ParticleCharge = cms.untracked.int32(-1),
-                          #MaxInvMass = cms.untracked.double(5.5),
-                          #MinInvMass = cms.untracked.double(5.0),
-                          ParticleID1 = cms.untracked.vint32(13),
-                          ParticleID2 = cms.untracked.vint32(13)
-                          )
+bfilter = cms.EDFilter(
+        "PythiaFilter",
+        MaxEta = cms.untracked.double(9999.),
+        MinEta = cms.untracked.double(-9999.),
+        ParticleID = cms.untracked.int32(521)
+        )
 
-# -- Require Muon from Bs
-MuFilter = cms.EDFilter("PythiaFilter",
-                        Status = cms.untracked.int32(1),
-                        MotherID = cms.untracked.int32(531),
-                        MinPt = cms.untracked.double(3.),
-                        ParticleID = cms.untracked.int32(13),
-                        MaxEta = cms.untracked.double(2.5),
-                        MinEta = cms.untracked.double(-2.5)
-                        )
+jpsifilter = cms.EDFilter(
+        "PythiaDauVFilter",
+	verbose         = cms.untracked.int32(0), 
+	NumberDaughters = cms.untracked.int32(2), 
+	MotherID        = cms.untracked.int32(521),  
+	ParticleID      = cms.untracked.int32(443),  
+        DaughterIDs     = cms.untracked.vint32(13, -13),
+	MinPt           = cms.untracked.vdouble(3.5, 3.5), 
+	MinEta          = cms.untracked.vdouble(-2.5, -2.5), 
+	MaxEta          = cms.untracked.vdouble( 2.5,  2.5)
+        )
 
-ProductionFilterSequence = cms.Sequence(generator*MuMuFilter*MuFilter)
+kfilter = cms.EDFilter(
+        "PythiaDauVFilter",
+	verbose         = cms.untracked.int32(0), 
+	NumberDaughters = cms.untracked.int32(2), 
+	MotherID        = cms.untracked.int32(0),  
+	ParticleID      = cms.untracked.int32(521),  
+        DaughterIDs     = cms.untracked.vint32(443, 321),
+	MinPt           = cms.untracked.vdouble(0., 0.4), 
+	MinEta          = cms.untracked.vdouble(-99., -2.5), 
+	MaxEta          = cms.untracked.vdouble(99.,   2.5)
+        )
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*jpsifilter*kfilter)
+
--- Configuration/Generator/python/UpsMM_8TeV_TuneCUETP8M1_cfi.py
+++ Configuration/Generator/python/UpsMM_8TeV_TuneCUETP8M1_cfi.py
@@ -1,74 +1,57 @@
+# fragment from https://github.com/alberto-sanchez/my-genproductions/blob/master/py8_UpsilonMM_FSR_13TeV_cfi.py
+# Updated to CUEP8M1 tune by Ian M. Nugent
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 import FWCore.ParameterSet.Config as cms
-
-from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
-generator = cms.EDFilter("Pythia6GeneratorFilter",
-    pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(0.141),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    crossSection = cms.untracked.double(9090000.0),
-    comEnergy = cms.double(8000.0),
-    maxEventsToPrint = cms.untracked.int32(0),
-    PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(0.0757),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(8000.0),
+                         crossSection = cms.untracked.double(13775390),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
         processParameters = cms.vstring(
-		'MSEL=62          ! Quarkonia NRQCD bb',
-		'CKIN(3)=20.       ! Min pthard',
-		'CKIN(4)=-1.      ! Max pthard', 
-		'MDME(1034,1)=0   ! 0.025200    e- e+',
-		'MDME(1035,1)=1   ! 0.024800    mu- mu+',                   
-		'MDME(1036,1)=0   ! 0.026700    tau- tau+',
-		'MDME(1037,1)=0   ! 0.015000    d dbar',
-		'MDME(1038,1)=0   ! 0.045000    u ubar',
-		'MDME(1039,1)=0   ! 0.015000    s sbar',
-		'MDME(1040,1)=0   ! 0.045000    c cbar',
-		'MDME(1041,1)=0   ! 0.774300    g g g',
-		'MDME(1042,1)=0   ! 0.029000    gamma g',
-		'MSTP(142)=2      ! turns on the PYEVWT Pt re-weighting routine',
-		'PARJ(13)=0.750   ! probability that a c or b meson has S=1',
-		'PARJ(14)=0.162   ! probability that a meson with S=0 is produced with L=1, J=1',
-		'PARJ(15)=0.018   ! probability that a meson with S=1 is produced with L=1, J=0',
-		'PARJ(16)=0.054   ! probability that a meson with S=1 is produced with L=1, J=1',
-		'MSTP(145)=0      !  choice of polarization',
-		'MSTP(146)=0      ! choice of polarization frame ONLY when mstp(145)=1',
-		'MSTP(147)=0      ! particular helicity or density matrix component when mstp(145)=1',
-		'MSTP(148)=1      ! possibility to allow for final-state shower evolution, extreme case!',
-		'MSTP(149)=1      ! if mstp(148)=1, it determines the kinematics of the QQ~3S1(8)->QQ~3S1(8)+g branching',
-		'PARP(141)=1.16   ! New values for COM matrix elements',
-		'PARP(142)=0.0119 ! New values for COM matrix elements', 
-		'PARP(143)=0.01   ! New values for COM matrix elements', 
-		'PARP(144)=0.01   ! New values for COM matrix elements', 
-		'PARP(145)=0.05   ! New values for COM matrix elements',  
-		'PARP(146)=9.28   ! New values for COM matrix elements', 
-		'PARP(147)=0.15   ! New values for COM matrix elements', 
-		'PARP(148)=0.02   ! New values for COM matrix elements', 
-		'PARP(149)=0.02   ! New values for COM matrix elements', 
-		'PARP(150)=0.09   ! New values for COM matrix elements'
-	    ),
-        # This is a vector of ParameterSet names to be read, in this order
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters', 
-            'CSAParameters'),
-        CSAParameters = cms.vstring('CSAMODE = 6     ! cross-section reweighted quarkonia')
-    )
-)
+            'Bottomonium:all = on', # Quarkonia, MSEL=62
+            'ParticleDecays:allowPhotonRadiation = on', # Turn on QED FSR
+            'StringFlav:mesonBvector = 3.000', # relative production vector/pseudoscalar for charm mesons - needs work
+            'StringFlav:mesonBL1S1J0 = 0.072', # relative scalar production (L=1,S=1,J=0)/pseudoscalar for charm mesons
+            'StringFlav:mesonBL1S0J1 = 3.000', # relative pseudovector production (L=1,S=0,J=1)/pseudoscalar for charm mesons
+            'StringFlav:mesonBL1S1J1 = 0.216', # relative pseudovector production (L=1,S=1,J=1)/pseudoscalar for charm mesons
+            'StringFlav:mesonBL1S1J2 = 0.000', # relative tensor production (L=1,S=1,J=2)/pseudoscalar for charm mesons
+            '553:onMode = off', # Turn off Upsilon decays
+            '553:onIfMatch = 13 -13' # just let Upsilon -> mu+ mu-
+            ),
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
 
 oniafilter = cms.EDFilter("PythiaFilter",
-    Status = cms.untracked.int32(2),
-    MaxEta = cms.untracked.double(1000.0),
-    MinEta = cms.untracked.double(-1000.0),
-    MinPt = cms.untracked.double(0.0),
-    ParticleID = cms.untracked.int32(553)
+                          Status = cms.untracked.int32(2),
+                          MaxEta = cms.untracked.double(1000.0),
+                          MinEta = cms.untracked.double(-1000.0),
+                          MinPt = cms.untracked.double(0.0),
+                          ParticleID = cms.untracked.int32(553)
 )
 
 mumugenfilter = cms.EDFilter("MCParticlePairFilter",
-    Status = cms.untracked.vint32(1, 1),
-    MinPt = cms.untracked.vdouble(0.5, 0.5),
-    MinP = cms.untracked.vdouble(2.7, 2.7),
-    MaxEta = cms.untracked.vdouble(2.5, 2.5),
-    MinEta = cms.untracked.vdouble(-2.5, -2.5),
-    ParticleCharge = cms.untracked.int32(-1),
-    ParticleID1 = cms.untracked.vint32(13),
-    ParticleID2 = cms.untracked.vint32(13)
-)
+                             Status = cms.untracked.vint32(1, 1),
+                             MinPt = cms.untracked.vdouble(0.5, 0.5),
+                             MinP = cms.untracked.vdouble(2.7, 2.7),
+                             MaxEta = cms.untracked.vdouble(2.5, 2.5),
+                             MinEta = cms.untracked.vdouble(-2.5, -2.5),
+                             MinInvMass = cms.untracked.double(5.0),
+                             MaxInvMass = cms.untracked.double(20.0),
+                             ParticleCharge = cms.untracked.int32(-1),
+                             ParticleID1 = cms.untracked.vint32(13),
+                             ParticleID2 = cms.untracked.vint32(13)
+                             )
 
 ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumugenfilter)
+
+
