fac46649e9e2ce3daede6af9d782ec292474b4e3
Tomasz <fruboes@cern.ch>
2014-11-06 17:03:38 +0100

    Define plots in cfi

--- DQMOffline/Trigger/python/FSQHLTOfflineSource_cfi.py
+++ DQMOffline/Trigger/python/FSQHLTOfflineSource_cfi.py
@@ -1,78 +1,92 @@
 import FWCore.ParameterSet.Config as cms
+import math
 
-
+# see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePhysicsCutParser
 def getPTAveVPSet():
-    ret=cms.VPSet(
-        cms.PSet(
-            handlerType = cms.string("FromHLT"),
-            partialPathName = cms.string("HLT_DiPFJetAve30_HFJEC_"),
-            partialFilterName  = cms.string("ForHFJECBase"),
-            dqmhistolabel  = cms.string("hltcalo"),
-            mainDQMDirname = cms.untracked.string(fsqdirname),
-            singleObjectsPreselection = cms.string("abs(eta)<1.4 || abs(eta) > 2.7 "),
-            combinedObjectSelection =  cms.string("1==1"),
-            combinedObjectSortCriteria = cms.string("at(0).pt"),
-            combinedObjectDimension = cms.int32(1),
-            drawables =  cms.VPSet(
-                cms.PSet (
-                    name = cms.string("pt"),
-                    expression = cms.string("at(0).pt"),
-                    bins = cms.int32(100),
-                    min = cms.double(0),
-                    max = cms.double(100)
+    ret=cms.VPSet()
+    # note: always give integer values (!)
+    thresholds = [30, 60, 80, 100, 160, 220, 300]
+    for t in thresholds:
+            partialPathName = "HLT_DiPFJetAve"+ str(t) +"_HFJEC_"
+            ptBinLow  = t/2
+            ptBinHigh = max(100, t*2)
+            ptBins = min(100, ptBinHigh-ptBinLow)
+            hltCalo =  cms.PSet(
+                handlerType = cms.string("FromHLT"),
+                partialPathName = cms.string(partialPathName),
+                partialFilterName  = cms.string("ForHFJECBase"),
+                dqmhistolabel  = cms.string("hltcalo"),
+                mainDQMDirname = cms.untracked.string(fsqdirname),
+                singleObjectsPreselection = cms.string("abs(eta)<1.4 || abs(eta) > 2.7 "),
+                combinedObjectSelection =  cms.string("1==1"),
+                combinedObjectSortCriteria = cms.string("at(0).pt"),
+                combinedObjectDimension = cms.int32(1),
+                drawables =  cms.VPSet(
+                    cms.PSet (name = cms.string("pt"), expression = cms.string("at(0).pt"), bins = cms.int32(ptBins), min = cms.double(ptBinLow), max = cms.double(ptBinHigh)),
+                    cms.PSet (name = cms.string("eta"), expression = cms.string("at(0).eta"), bins = cms.int32(104), min = cms.double(-5.2), max = cms.double(5.2))
                 )
             )
-        ),
+            ret.append(hltCalo)
 
-
-
-
-        cms.PSet(
-            handlerType = cms.string("FromHLT"),
-            partialPathName = cms.string("HLT_DiPFJetAve60_HFJEC_"),
-            partialFilterName  = cms.string("ForHFJECBase"),
-            dqmhistolabel  = cms.string("hltcalo"),
-            mainDQMDirname = cms.untracked.string(fsqdirname),
-            singleObjectsPreselection = cms.string("abs(eta)<1.4 || abs(eta) > 2.7"),
-            combinedObjectSelection =  cms.string("1==1"),
-            combinedObjectSortCriteria = cms.string("at(0).pt"),
-            combinedObjectDimension = cms.int32(1),
-            drawables =  cms.VPSet(
-                cms.PSet (
-                    name = cms.string("pt"),
-                    expression = cms.string("at(0).pt"),
-                    bins = cms.int32(100),
-                    min = cms.double(0),
-                    max = cms.double(100)
+            l1 =  cms.PSet(
+                handlerType = cms.string("FromHLT"),
+                partialPathName = cms.string(partialPathName),
+                partialFilterName  = cms.string("hltL1"),
+                dqmhistolabel  = cms.string("l1"),
+                mainDQMDirname = cms.untracked.string(fsqdirname),
+                singleObjectsPreselection = cms.string("1==1"),
+                combinedObjectSelection =  cms.string("1==1"),
+                combinedObjectSortCriteria = cms.string("at(0).pt"),
+                combinedObjectDimension = cms.int32(1),
+                drawables =  cms.VPSet(
+                    cms.PSet (name = cms.string("pt"), expression = cms.string("at(0).pt"), bins = cms.int32(256/4), min = cms.double(0), max = cms.double(256)),
+                    cms.PSet (name = cms.string("eta"), expression = cms.string("at(0).eta"), bins = cms.int32(104/4), min = cms.double(-5.2), max = cms.double(5.2))
                 )
             )
-        ),
-        # vector<reco::GenJet>                "ak5GenJets"                ""          "SIM"          recoGenJets_ak5GenJets__SIM
-        cms.PSet(
-            handlerType = cms.string("FromRecoCandidate"),
-            inputCol = cms.InputTag("ak4GenJets"),
-            partialPathName = cms.string("HLT_DiPFJetAve30_HFJEC_"),
-            partialFilterName  = cms.string("dummy"),
-            dqmhistolabel  = cms.string("ak4gen"),
-            mainDQMDirname = cms.untracked.string(fsqdirname),
-            singleObjectsPreselection = cms.string("abs(eta)< 5.2 && pt > 20"),
-            combinedObjectSelection =  cms.string("1==1"),
-            combinedObjectSortCriteria = cms.string("at(0).pt"),
-            combinedObjectDimension = cms.int32(1),
-            drawables =  cms.VPSet(
-                cms.PSet (
-                    name = cms.string("pt"),
-                    expression = cms.string("at(0).pt"),
-                    bins = cms.int32(100),
-                    min = cms.double(0),
-                    max = cms.double(100)
+            ret.append(l1)
+
+            hltPFSingle  =  cms.PSet(
+                handlerType = cms.string("FromHLT"),
+                partialPathName = cms.string(partialPathName),
+                partialFilterName  = cms.string("hltDiPFJetAve"),
+                dqmhistolabel  = cms.string("hltpfsingle"),
+                mainDQMDirname = cms.untracked.string(fsqdirname),
+                singleObjectsPreselection = cms.string("abs(eta)<1.4 || abs(eta) > 2.7 "),
+                combinedObjectSelection =  cms.string("1==1"),
+                combinedObjectSortCriteria = cms.string("at(0).pt"),
+                combinedObjectDimension = cms.int32(1),
+                drawables =  cms.VPSet(
+                    cms.PSet (name = cms.string("pt"), expression = cms.string("at(0).pt"), bins = cms.int32(ptBins), min = cms.double(ptBinLow), max = cms.double(ptBinHigh)),
+                    cms.PSet (name = cms.string("eta"), expression = cms.string("at(0).eta"), bins = cms.int32(104), min = cms.double(-5.2), max = cms.double(5.2))
                 )
             )
-        ),
-    )
+            ret.append(hltPFSingle)
+
+
+            hltPFtopology  =  cms.PSet(
+                handlerType = cms.string("FromHLT"),
+                partialPathName = cms.string(partialPathName),
+                partialFilterName  = cms.string("hltDiPFJetAve"),
+                dqmhistolabel  = cms.string("hltpftopology"),
+                mainDQMDirname = cms.untracked.string(fsqdirname),
+                singleObjectsPreselection = cms.string("abs(eta)<1.4 || abs(eta) > 2.7 "),
+                combinedObjectSelection =  cms.string("abs(at(0).eta())< 1.4 && abs(at(1).eta()) > 2.7 "),
+                combinedObjectSortCriteria = cms.string("(at(0).pt+at(1).pt)/2"),
+                combinedObjectDimension = cms.int32(2),
+                drawables =  cms.VPSet(
+                    cms.PSet (name = cms.string("deltaEta"), expression = cms.string("abs(at(0).eta-at(1).eta)"), bins = cms.int32(70), min = cms.double(0), max = cms.double(7)),
+                    cms.PSet (name = cms.string("deltaPhi"), expression = cms.string("abs(deltaPhi(at(0).phi, at(1).phi))"), bins = cms.int32(100), min = cms.double(0), max = cms.double(3.2))
+                )
+            )
+            ret.append(hltPFtopology)
+
+
+
     return ret
+
+
 fsqdirname = "HLT/FSQ/"
-ttt = cms.EDAnalyzer("FSQDiJetAve",
+fsqHLTOfflineSource = cms.EDAnalyzer("FSQDiJetAve",
     dirname = cms.untracked.string("HLT/FSQ/DiJETAve/"),
     triggerSummaryLabel = cms.InputTag("hltTriggerSummaryAOD","","TTT"),
     triggerResultsLabel = cms.InputTag("TriggerResults","","TTT"),
