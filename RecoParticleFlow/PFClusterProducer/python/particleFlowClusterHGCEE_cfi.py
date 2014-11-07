c2420cf3981470fa2de57d36a53edcd2744a01af
Lindsey Gray <lindsey.gray@gmail.com>
2014-11-07 18:09:33 +0000

    add shower axis to PFCluster for HGC

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHGCEE_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHGCEE_cfi.py
@@ -9,37 +9,12 @@ _localmaxseeds_HGCEE = cms.PSet(
     algoName = cms.string("LocalMaximumSeedFinder"),
     thresholdsByDetector = cms.VPSet(
     cms.PSet( detector = cms.string("HGC_ECAL"),
-              seedingThreshold = cms.double(1e-6*1.75*55.1), #seeding threshold converted to GeV from keV
+              #seeding threshold converted to GeV from keV
+              seedingThreshold = cms.double(1e-6*1.75*55.1),
               seedingThresholdPt = cms.double(0.0)
               )
     ),
     nNeighbours = cms.int32(8)
-)
-
-#for arbor this is more a pre-clustering step to find little clusters
-_arborTopoClusterizer_HGCEE = cms.PSet(
-    algoName = cms.string("IntraLayerClusteringAlgorithm"),    
-    IntraLayerMaxDistance = cms.double( 19.0 ), # hit separation in mm
-    ShouldSplitClusterInSingleCaloHitClusters = cms.bool(False), # splitsmall clusters
-    MaximumSizeForClusterSplitting = cms.uint32( 3 ), #largest of small clusters to split
-    thresholdsByDetector = cms.VPSet( )
-)
-_simplePosCalcHGCEE =  cms.PSet(
-    algoName = cms.string("SimplePositionCalc"),
-    minFractionInCalc = cms.double(0.0)
-)
-
-#the real arbor clusterizer
-_manqiArborClusterizer_HGCEE = cms.PSet(
-    algoName = cms.string("SimpleArborClusterizer"), 
-    # use basic pad sizes in HGCEE
-    cellSize = cms.double(15.0),
-    layerThickness = cms.double(25.0),
-    distSeedForMerge = cms.double(20.0),
-    killNoiseClusters = cms.bool(True),
-    maxNoiseClusterSize = cms.uint32(3),
-    allowSameLayerSeedMerge = cms.bool(False),
-    thresholdsByDetector = cms.VPSet( )
 )
 
 _positionCalcHGCEE_onelayer = cms.PSet(
@@ -51,24 +26,21 @@ _positionCalcHGCEE_onelayer = cms.PSet(
     minAllowedNormalization = cms.double(1e-9)
     )
 
+_positionCalcHGCEE_pca = cms.PSet(
+    algoName = cms.string("3DClusterPCACalculator"),
+    ##
+    minFractionInCalc = cms.double(1e-9),
+    posCalcNCrystals = cms.int32(-1),
+    logWeightDenominator = cms.double(1e-6*0.25*55.1), # use ADC value 0.25*MIP
+    minAllowedNormalization = cms.double(1e-9)
+    )
+
 _fromScratchHGCClusterizer_HGCEE = cms.PSet(
     algoName = cms.string("HGCClusterizer"), 
     thresholdsByDetector = cms.VPSet( ),
     positionCalcInLayer = _positionCalcHGCEE_onelayer,
+    clusterPCACalculator3D = _positionCalcHGCEE_pca,
     moliereRadii = cms.PSet( HGC_ECAL = cms.double(2.9)  ) #cm
-)
-
-_arborClusterizer_HGCEE = cms.PSet(
-    algoName = cms.string("ArborConnectorClusteringAlgorithm"), 
-    # these are taken from the settings for Fine Granularity in ArborPFA
-    MaximumForwardDistanceForConnection = cms.double(60.0), #in mm
-    MaximumTransverseDistanceForConnection = cms.double(40.0), #in mm
-    AllowForwardConnectionForIsolatedObjects = cms.bool(False),
-    ShouldUseIsolatedObjects = cms.bool(True),
-    MaximumNumberOfKeptConnectors = cms.uint32(5),
-    OrderParameterAnglePower = cms.double(1.0),
-    OrderParameterDistancePower = cms.double(0.5),
-    minFractionToKeep = cms.double(1e-7)
 )
 
 #weights for layers from P.Silva (24 October 2014)
