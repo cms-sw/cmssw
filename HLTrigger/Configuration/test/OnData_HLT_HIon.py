1412429838c4399ea5b679d4801ada6fc060310d
Andrea <perrotta@cern.ch>
2014-11-13 13:56:47 +0100

    Fixes in tau paths because of the modified L2Muon filters, CMSHLT-172

--- HLTrigger/Configuration/test/OnData_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnData_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_2_1/HIon/V51 (CMSSW_7_2_1_patch2)
+# /dev/CMSSW_7_2_1/HIon/V52 (CMSSW_7_2_1_patch2)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_2_1/HIon/V51')
+  tableName = cms.string('/dev/CMSSW_7_2_1/HIon/V52')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
