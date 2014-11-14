4435bc15b7f6ef8448835fcf72067aa1284ab4cb
Tapio <tapio.lampen@cern.ch>
2014-11-14 19:25:38 +0100

    Changes required by GeometryDB_cff

--- Alignment/OfflineValidation/python/TkAlAllInOneTool/geometryComparisonTemplates.py
+++ Alignment/OfflineValidation/python/TkAlAllInOneTool/geometryComparisonTemplates.py
@@ -10,8 +10,8 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = ".oO[GlobalTag]Oo." 
 
 process.load("Configuration.Geometry.GeometryDB_cff")
-process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
-process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+#process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+#process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
 
 #process.load("Alignment.CommonAlignmentProducer.GlobalPosition_Frontier_cff")
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
@@ -52,9 +52,9 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = ".oO[GlobalTag]Oo." 
 process.load("Configuration.Geometry.GeometryDB_cff")
 
-process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+#process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
 
-process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+#process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
 
 #process.load("Alignment.CommonAlignmentProducer.GlobalPosition_Frontier_cff")
 # the input .GlobalPosition_Frontier_cff is providing the frontier://FrontierProd/CMS_COND_31X_ALIGNMENT in the release which does not provide the ideal geometry
