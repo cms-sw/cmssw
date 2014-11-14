25e5e5e8d6878715d28b1de9347dbc15cc8ef576
David Lange <David.Lange@cern.ch>
2014-11-14 10:32:38 +0100

    Revert "enable running on MiniAOD, switch to new JetCorrector"

--- Validation/RecoJets/python/JetValidation_cff.py
+++ Validation/RecoJets/python/JetValidation_cff.py
@@ -10,19 +10,29 @@ from RecoJets.Configuration.RecoJetAssociations_cff import *
 
 from Validation.RecoJets.JetValidation_cfi import *
 
-from JetMETCorrections.Configuration.JetCorrectors_cff import ak4CaloL2L3CorrectorChain,ak4CaloL2L3Corrector,ak4CaloL3AbsoluteCorrector,ak4CaloL2RelativeCorrector
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import ak4CaloL2L3,ak4CaloL2Relative,ak4CaloL3Absolute
+newAk4CaloL2L3 = ak4CaloL2L3.clone()
 
-from JetMETCorrections.Configuration.JetCorrectors_cff import ak4PFL1FastL2L3CorrectorChain,ak4PFL1FastL2L3Corrector,ak4PFL3AbsoluteCorrector,ak4PFL2RelativeCorrector,ak4PFL1FastjetCorrector
+#from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import ak7CaloL2L3,ak7CaloL2Relative,ak7CaloL3Absolute
+#newAk7CaloL2L3 = ak7CaloL2L3.clone()
 
-from JetMETCorrections.Configuration.JetCorrectors_cff import ak4PFCHSL1FastL2L3CorrectorChain,ak4PFCHSL1FastL2L3Corrector,ak4PFCHSL3AbsoluteCorrector,ak4PFCHSL2RelativeCorrector,ak4PFCHSL1FastjetCorrector
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import ak4PFL1FastL2L3,ak4PFL1Fastjet,ak4PFL2Relative,ak4PFL3Absolute
+newAk4PFL1FastL2L3 = ak4PFL1FastL2L3.clone()
+
+#from JetMETCorrections.Configuration.JetCorrectionServices_cff import ak4JPTL1FastL2L3,ak4JPTL1Fastjet,ak4JPTL2Relative,ak4JPTL3Absolute
+#newAk4JPTL1FastL2L3 = ak4JPTL1FastL2L3.clone()
+
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import ak4PFCHSL1FastL2L3,ak4PFCHSL1Fastjet,ak4PFCHSL2Relative,ak4PFCHSL3Absolute
 
 JetValidation = cms.Sequence(
-    ak4CaloL2L3CorrectorChain*
+#    JetAnalyzerKt6PF*
+#    JetAnalyzerKt6Calo*
     JetAnalyzerAk4Calo*
-    ak4PFL1FastL2L3CorrectorChain*
+#    JetAnalyzerAk7Calo*
     JetAnalyzerAk4PF*
-    ak4PFCHSL1FastL2L3CorrectorChain*
+#    JetAnalyzerAk4JPT*
     JetAnalyzerAk4PFCHS
+#    JetAnalyzerAk8PF*
+#    JetAnalyzerAk8PFCHS*
+#    *JetAnalyzerCA8PFCHS
     )
-
-JetValidationMiniAOD=cms.Sequence(JetAnalyzerAk4PFCHSMiniAOD)
