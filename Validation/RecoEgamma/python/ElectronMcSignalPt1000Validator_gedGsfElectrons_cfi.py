60119a5ff33ca5826ea137978e8cb43b8704016b
Arnaud <arnaud.chiron@cern.ch>
2014-11-18 15:10:39 +0100

    analyze Pt1000 for MultiThreading

--- Validation/RecoEgamma/python/ElectronMcSignalPt1000Validator_gedGsfElectrons_cfi.py
+++ Validation/RecoEgamma/python/ElectronMcSignalPt1000Validator_gedGsfElectrons_cfi.py
@@ -22,13 +22,13 @@ electronMcSignalHistosCfg = cms.PSet(
   Nbinhoe = cms.int32(100), Hoemin = cms.double(0.0), Hoemax = cms.double(0.5),
   Nbinpoptrue = cms.int32(75), Poptruemin = cms.double(0.0), Poptruemax = cms.double(1.5),
   Nbinerror = cms.int32(30), Energyerrormax = cms.double(150.0),
-  EfficiencyFlag = cms.bool(False), StatOverflowFlag = cms.bool(True)
+  EfficiencyFlag = cms.bool(False), StatOverflowFlag = cms.bool(False)
 )
 
 electronMcSignalValidator = cms.EDAnalyzer("ElectronMcSignalValidator",
 
   Verbosity = cms.untracked.int32(0),
-  FinalStep = cms.string("AtRunEnd"),
+  FinalStep = cms.string("AtJobEnd"),
   InputFile = cms.string(""),
   OutputFile = cms.string(""),
   InputFolderName = cms.string("EgammaV/ElectronMcSignalValidator"),
