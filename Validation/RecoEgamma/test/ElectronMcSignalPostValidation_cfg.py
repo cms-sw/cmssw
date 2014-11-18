3ff4ecef2935fdb2c4062787263df7e7618d6b29
Arnaud <arnaud.chiron@cern.ch>
2014-11-18 15:13:57 +0100

    finalize signal cfg for MultiThreading

--- Validation/RecoEgamma/test/ElectronMcSignalPostValidation_cfg.py
+++ Validation/RecoEgamma/test/ElectronMcSignalPostValidation_cfg.py
@@ -8,49 +8,42 @@ process = cms.Process("electronPostValidation")
 process.DQMStore = cms.Service("DQMStore")
 process.load("Validation.RecoEgamma.ElectronMcSignalPostValidator_cfi")
 process.load("DQMServices.Components.DQMStoreStats_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 # load DQM
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
-#process.load('Configuration.StandardSequences.EDMtoMEAtRunEnd_cff') # NEW
 process.load("Configuration.StandardSequences.EDMtoMEAtJobEnd_cff")
+# import DQMStore service
+process.load('DQMOffline.Configuration.DQMOffline_cff')
 
+# actually read in the DQM root file
+process.load("DQMServices.Components.DQMFileReader_cfi")
 
 from DQMServices.Components.DQMStoreStats_cfi import *
 dqmStoreStats.runOnEndJob = cms.untracked.bool(True)
 
-t1 = os.environ['TEST_HISTOS_FILE'].split('.')
-localFileInput = 'DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root' # 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO.root'
-localFileInput2 = 'DQM_V0001_R000000001__electronHistos__' + t1[1] + '__RECO2.root' # temp
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 
-#process.electronMcSignalPostValidator.InputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
-#process.electronMcSignalPostValidator.OutputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
-process.electronMcSignalPostValidator.InputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
-process.electronMcSignalPostValidator.OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator")
-
+t1 = os.environ['TEST_HISTOS_FILE'].split('.')
+localFileInput = os.environ['TEST_HISTOS_FILE'].replace(".root", "_a.root") #
 # Source
-process.source = cms.Source("EmptySource")
-#process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring("file:" + localFileInput))
-#process.source = cms.Source("DQMRootSource", fileNames = cms.untracked.vstring(localFileInput))
+process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring("file:" + localFileInput),
+secondaryFileNames = cms.untracked.vstring(),)
+
+process.electronMcSignalPostValidator.InputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
+process.electronMcSignalPostValidator.OutputFolderName = cms.string("EgammaV/ElectronMcSignalValidator")
 
 from Configuration.AlCa.autoCond import autoCond
 process.GlobalTag.globaltag = os.environ['TEST_GLOBAL_TAG']#+'::All'
 
-print "localFileInput : input :", localFileInput
-print "localFileInput2 : ouput : ", localFileInput2
-process.electronMcSignalPostValidator.InputFile = localFileInput
-process.electronMcSignalPostValidator.OutputFile = localFileInput2 # temp
-
 process.dqmSaver.workflow = '/electronHistos/' + t1[1] + '/RECO3'
-#
 process.dqmsave_step = cms.Path(process.DQMSaver)
 
-#process.p = cms.Path(process.electronMcSignalPostValidator*process.dqmStoreStats)
-process.p = cms.Path(process.electronMcSignalPostValidator)
+process.p = cms.Path(process.EDMtoME * process.electronMcSignalPostValidator * process.dqmStoreStats)
 
 # Schedule
-process.schedule = cms.Schedule(process.p,
+process.schedule = cms.Schedule(
+                                process.p,
                                 process.dqmsave_step,
 )
-
