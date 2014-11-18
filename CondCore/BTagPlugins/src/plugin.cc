1a2ec55dfce245b2cfb791a1716f8b548a47c9e7
Heiner Tholen <heiner.tholen@cern.ch>
2014-11-18 13:46:37 +0100

    moving CondCore/BTagPlugins code into CondCoreBTauPlugins

--- CondCore/BTagPlugins/src/plugin.cc
+++
@@ -1,6 +0,0 @@
-#include "CondCore/ESSources/interface/registration_macros.h"
-
-#include "CondFormats/BTagObjects/interface/BTagCalibration.h"
-#include "CondFormats/DataRecord/interface/BTagCalibrationRcd.h"
-
-REGISTER_PLUGIN(BTagCalibrationRcd, BTagCalibration);
