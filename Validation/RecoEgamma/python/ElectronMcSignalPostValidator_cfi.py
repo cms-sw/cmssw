a5c85cd79aef008506135ee12e59ca70458eae54
Arnaud <arnaud.chiron@cern.ch>
2014-11-18 15:10:01 +0100

    analyze signal for MultiThreading

--- Validation/RecoEgamma/python/ElectronMcSignalPostValidator_cfi.py
+++ Validation/RecoEgamma/python/ElectronMcSignalPostValidator_cfi.py
@@ -2,20 +2,17 @@
 import FWCore.ParameterSet.Config as cms
 
 electronMcSignalHistosCfg = cms.PSet(
-  EfficiencyFlag = cms.bool(False), StatOverflowFlag = cms.bool(True)
+  EfficiencyFlag = cms.bool(False), StatOverflowFlag = cms.bool(False)
 )
 
 electronMcSignalPostValidator = cms.EDAnalyzer("ElectronMcSignalPostValidator",
 
   Verbosity = cms.untracked.int32(0),
-#  FinalStep = cms.string("AtRunEnd"),
   FinalStep = cms.string("AtJobEnd"),
   InputFile = cms.string(""),
   OutputFile = cms.string(""),
   InputFolderName = cms.string("EgammaV/ElectronMcSignalValidator"),
   OutputFolderName = cms.string("EgammaV/ElectronMcSignalValidator"),
-#  InputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator"),
-#  OutputFolderName = cms.string("Run 1/EgammaV/Run summary/ElectronMcSignalValidator"),
     
   histosCfg = cms.PSet(electronMcSignalHistosCfg)
 )
