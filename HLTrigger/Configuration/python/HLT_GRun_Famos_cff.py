23cdf51995f2c350e5e0a498a31c04aae02f108c
Martin Grunewald <Martin.Grunewald@cern.ch>
2014-11-18 08:32:29 +0100

    HLT: Add extra parameter to L1Extra producer

--- HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_2_1/GRun/V63 (CMSSW_7_2_1_patch2)
+# /dev/CMSSW_7_2_1/GRun/V64 (CMSSW_7_2_1_patch2_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 from FastSimulation.HighLevelTrigger.HLTSetup_cff import *
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_2_1/GRun/V63')
+  tableName = cms.string('/dev/CMSSW_7_2_1/GRun/V64')
 )
 
 HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/test/OnMc_HLT_FULL.py
+++ HLTrigger/Configuration/test/OnMc_HLT_FULL.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_2_1/HLT/V116 (CMSSW_7_2_1_patch2)
+# /dev/CMSSW_7_2_1/HLT/V117 (CMSSW_7_2_1_patch2_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFULL" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_2_1/HLT/V116')
+  tableName = cms.string('/dev/CMSSW_7_2_1/HLT/V117')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
@@ -2417,6 +2417,7 @@ process.hltL1GtObjectMap = cms.EDProducer( "L1GlobalTrigger",
 process.hltL1extraParticles = cms.EDProducer( "L1ExtraParticlesProd",
     tauJetSource = cms.InputTag( 'hltGctDigis','tauJets' ),
     etHadSource = cms.InputTag( "hltGctDigis" ),
+    isoTauJetSource = cms.InputTag( 'hltGctDigis','isoTauJets' ),
     etTotalSource = cms.InputTag( "hltGctDigis" ),
     centralBxOnly = cms.bool( True ),
     centralJetSource = cms.InputTag( 'hltGctDigis','cenJets' ),
