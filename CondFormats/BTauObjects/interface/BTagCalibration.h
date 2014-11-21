f9edec2452505e2f3763563a12cbf0257062bdaa
wmtan <wmtan@fnal.gov>
2014-11-21 16:43:50 +0100

    Add missing std:: qualifiers

--- CondFormats/BTauObjects/interface/BTagCalibration.h
+++ CondFormats/BTauObjects/interface/BTagCalibration.h
@@ -37,9 +37,9 @@ public:
   void addEntry(const BTagEntry &entry);
   const std::vector<BTagEntry>& getEntries(const BTagEntry::Parameters &par) const;
 
-  void readCSV(istream &s);
+  void readCSV(std::istream &s);
   void readCSV(const std::string &s);
-  void makeCSV(ostream &s) const;
+  void makeCSV(std::ostream &s) const;
   std::string makeCSV() const;
 
 protected:
