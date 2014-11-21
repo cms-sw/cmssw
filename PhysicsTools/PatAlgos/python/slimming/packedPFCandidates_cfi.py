06d1d66b049a143dec40242eba3ba596b172ce9b
Volker Adler <volker.adler@cern.ch>
2014-11-21 20:08:14 +0100

    Use proper pile-up modules for packed PFCandidates.

--- PhysicsTools/PatAlgos/python/slimming/packedPFCandidates_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/packedPFCandidates_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 packedPFCandidates = cms.EDProducer("PATPackedCandidateProducer",
     inputCollection = cms.InputTag("particleFlow"),
     inputCollectionFromPVLoose = cms.InputTag("pfNoPileUpJME"),
-    inputCollectionFromPVTight = cms.InputTag("pfNoPileUp"),
+    inputCollectionFromPVTight = cms.InputTag("pfNoPileUpPFBRECO"),
     inputVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
     originalVertices = cms.InputTag("offlinePrimaryVertices"),
     originalTracks = cms.InputTag("generalTracks"),
-    minPtForTrackProperties = cms.double(0.95)	
+    minPtForTrackProperties = cms.double(0.95)
 )
