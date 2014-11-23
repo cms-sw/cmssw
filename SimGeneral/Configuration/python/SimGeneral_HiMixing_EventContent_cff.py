649bad8290bead0ccffaefeb7d96359f106aede4
Yetkin <yetkin.yilmaz@cern.ch>
2014-11-23 18:34:49 +0100

    event content cleaned from old hiSignal and updated to keep playback from gen-level mixing

--- SimGeneral/Configuration/python/SimGeneral_HiMixing_EventContent_cff.py
+++ SimGeneral/Configuration/python/SimGeneral_HiMixing_EventContent_cff.py
@@ -2,15 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 HiMixRAW = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_hiSignal_*_*',
-        'keep *_hiSignalG4SimHits_*_*',
+        'keep *_mixGen_*_*',
         'keep *_hiGenParticles_*_*'
     )
 )
 
 HiMixRECO = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep *_hiSignal_*_*',
+        'keep *_mixGen_*_*',
 	'keep *_hiGenParticles_*_*'
     )
 )
