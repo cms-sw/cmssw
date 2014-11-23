8596d1fa7066f654a8ae24a43b3657305ef68ef5
Yetkin <yetkin.yilmaz@cern.ch>
2014-11-23 18:02:16 +0100

    missing comma

--- GeneratorInterface/HiGenCommon/python/VtxSmearedMatch5TeVPPbBoostReversed_cff.py
+++ GeneratorInterface/HiGenCommon/python/VtxSmearedMatch5TeVPPbBoostReversed_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 VtxSmeared = cms.EDProducer("MixBoostEvtVtxGenerator",
                             useCF = cms.untracked.bool(True),
-                            mixLabel = cms.InputTag("mixGen","generator")
+                            mixLabel = cms.InputTag("mixGen","generator"),
                             Beta=cms.double(0.434)                          
                             )
