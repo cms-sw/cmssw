3849181b8cf6e2de3b008900cd1514f0b8fb2f30
Yetkin <yetkin.yilmaz@cern.ch>
2014-11-23 18:08:04 +0100

    signal label added back

--- GeneratorInterface/HiGenCommon/python/VtxSmearedMatch5TeVPPbBoost_cff.py
+++ GeneratorInterface/HiGenCommon/python/VtxSmearedMatch5TeVPPbBoost_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 VtxSmeared = cms.EDProducer("MixBoostEvtVtxGenerator",
                             useCF = cms.untracked.bool(True),
+                            signalLabel = cms.InputTag("generator"),
                             mixLabel = cms.InputTag("mixGen","generator"),
                             Beta=cms.double(-0.434)
                             )
--- GeneratorInterface/HiGenCommon/python/VtxSmearedMatchHI_cff.py
+++ GeneratorInterface/HiGenCommon/python/VtxSmearedMatchHI_cff.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 VtxSmeared = cms.EDProducer("MixEvtVtxGenerator",
                             useCF = cms.untracked.bool(True),
+                            signalLabel = cms.InputTag("generator"),
                             mixLabel = cms.InputTag("mixGen","generator")
                             )
