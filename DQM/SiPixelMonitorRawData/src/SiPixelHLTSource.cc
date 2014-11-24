8ef9924893ae243f16879b02b493c80d08b09005
Marton <marton.bartok@cern.ch>
2014-11-24 10:58:25 +0100

    SiPixelRawData

--- DQM/SiPixelMonitorRawData/src/SiPixelHLTSource.cc
+++ DQM/SiPixelMonitorRawData/src/SiPixelHLTSource.cc
@@ -52,10 +52,10 @@ SiPixelHLTSource::SiPixelHLTSource(const edm::ParameterSet& iConfig) :
   errin_( consumes<edm::DetSetVector<SiPixelRawDataError> >( conf_.getParameter<edm::InputTag>( "ErrorInput" ) ) ),
   saveFile( conf_.getUntrackedParameter<bool>("saveFile",false) ),
   slowDown( conf_.getUntrackedParameter<bool>("slowDown",false) ),
-  dirName_( conf_.getUntrackedParameter<std::string>("DirName","Pixel/FEDIntegrity/") )
 {
   firstRun = true;
   LogInfo ("PixelDQM") << "SiPixelHLTSource::SiPixelHLTSource: Got DQM BackEnd interface"<<endl;
+  topFolderName_ = conf_.getParameter<std::string>("TopFolderName");
 }
 
 
@@ -153,7 +153,7 @@ void SiPixelHLTSource::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 void SiPixelHLTSource::bookMEs(DQMStore::IBooker & iBooker){
 
   iBooker.cd();
-  iBooker.setCurrentFolder(dirName_);
+  iBooker.setCurrentFolder(topFolderName_+"/FEDIntegrity/");
 
   std::string rawhid;
   std::string errhid;
