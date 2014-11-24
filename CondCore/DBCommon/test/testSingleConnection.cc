ce8eb41bd13adbd7756ed0fadf843d7132a88e35
wmtan <wmtan@fnal.gov>
2014-11-24 23:46:47 +0100

    Fail deliberately to avoid infinite loop

--- CondCore/DBCommon/test/testSingleConnection.cc
+++ CondCore/DBCommon/test/testSingleConnection.cc
@@ -17,6 +17,7 @@ void wait ( int seconds )
   while (clock() < endwait) {}
 }
 int main(){
+  assert(0); //Fail deliberately to avoid invfinite loop.
   edmplugin::PluginManager::Config config;
   edmplugin::PluginManager::configure(edmplugin::standard::config());
   cond::DbConnection connection;
