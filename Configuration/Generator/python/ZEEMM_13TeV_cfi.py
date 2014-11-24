1fe1535f8845ecdf402c0d0cde4eb425b03db42d
Quan Wang <BetterWang@gmail.com>
2014-11-24 23:36:34 +0100

    [+] update

--- Configuration/Generator/python/ZEEMM_13TeV_cfi.py
+++ Configuration/Generator/python/ZEEMM_13TeV_cfi.py
@@ -30,14 +30,4 @@ generator = cms.EDFilter("Pythia6GeneratorFilter",
     )
 )
 
-mumugenfilter = cms.EDFilter("MCParticlePairFilter",
-    Status = cms.untracked.vint32(1, 1),
-    MinPt = cms.untracked.vdouble(2.5, 2.5),
-    MaxEta = cms.untracked.vdouble(2.5, 2.5),
-    MinEta = cms.untracked.vdouble(-2.5, -2.5),
-    ParticleCharge = cms.untracked.int32(-1),
-    ParticleID1 = cms.untracked.vint32(13),
-    ParticleID2 = cms.untracked.vint32(13)
-)
-
 ProductionFilterSequence = cms.Sequence(generator)
