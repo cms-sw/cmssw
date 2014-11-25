fe566c6532b399c147fadde865ebbb42d548aa32
David <dnash@cern.ch>
2014-11-25 16:46:00 +0100

    Included python config renaming that was missing

--- RecoMuon/MuonIdentification/python/me0MuonConverter_cfi.py
+++ RecoMuon/MuonIdentification/python/me0MuonConverter_cfi.py
@@ -3,4 +3,4 @@ import FWCore.ParameterSet.Config as cms
 #process = cms.Process("ME0MuonConversion")
 #process.me0MuonConverter = cms.EDProducer("ME0MuonConverter")
 
-me0MuonConverter = cms.EDProducer("ME0MuonConverter")
+me0MuonConverting = cms.EDProducer("ME0MuonConverter")
