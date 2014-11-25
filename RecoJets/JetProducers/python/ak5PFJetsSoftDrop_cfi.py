50f6bccb865f362522c9cbffd39a57fee2f9f152
Andreas Hinzmann <hinzmann@cern.ch>
2014-11-25 12:38:48 +0100

    update softdrop default

--- RecoJets/JetProducers/python/ak5PFJetsSoftDrop_cfi.py
+++ RecoJets/JetProducers/python/ak5PFJetsSoftDrop_cfi.py
@@ -6,7 +6,7 @@ from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
 
 ak5PFJetsSoftDrop = ak5PFJets.clone(
     useSoftDrop = cms.bool(True),
-    zcut = cms.double(0.2),
+    zcut = cms.double(0.1),
     beta = cms.double(0.0),
     useExplicitGhosts = cms.bool(True)
     )
