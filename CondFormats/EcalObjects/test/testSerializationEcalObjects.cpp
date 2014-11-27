1451a57d99a71257809edbeee208623f3723efdd
Pierre Depasse <depasse@cern.ch>
2014-11-27 12:20:06 +0100

    SamplesCorrelation

--- CondFormats/EcalObjects/test/testSerializationEcalObjects.cpp
+++ CondFormats/EcalObjects/test/testSerializationEcalObjects.cpp
@@ -121,6 +121,7 @@ int main()
     testSerialization<EcalWeightSet>();
     testSerialization<EcalWeightXtalGroups>();
     testSerialization<EcalXtalGroupId>();
+    testSerialization<EcalSamplesCorrelation>();
     testSerialization<std::map<std::pair<EcalXtalGroupId, EcalTBWeights::EcalTDCId>, EcalWeightSet>>();
     testSerialization<std::map<uint32_t, EcalTPGFineGrainConstEB>>();
     testSerialization<std::map<uint32_t, EcalTPGFineGrainStripEE::Item>>();
