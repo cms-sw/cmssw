e26fa92cef4ad2b8123f0e4d0f9ac6c357374369
Volker Adler <volker.adler@cern.ch>
2014-11-27 18:38:44 +0100

    Enable EA (rho) PU isolation corrections.

--- TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets.py
+++ TopQuarkAnalysis/Configuration/python/patRefSel_refMuJets.py
@@ -15,23 +15,29 @@ triggerSelectionMC    = 'HLT_*' # not recommended
 ### Muon selection
 
 # Minimal selection for all muons, also basis for signal and veto muons
+# Muon ID ("loose")
 muonCut  =     'isPFMuon'                                                                      # general reconstruction property
 muonCut += ' && (isGlobalMuon || isTrackerMuon)'                                               # general reconstruction property
+# Kinematics
 muonCut += ' && pt > 10.'                                                                      # transverse momentum
 muonCut += ' && abs(eta) < 2.5'                                                                # pseudo-rapisity range
+# (Relative) isolation
 muonCut += ' && (chargedHadronIso+neutralHadronIso+photonIso-0.5*puChargedHadronIso)/pt < 0.2' # relative isolation w/ Delta beta corrections (factor 0.5)
 
 # Signal muon selection on top of 'muonCut'
+# Muon ID ("tight")
 signalMuonCut  =     'isPFMuon'                                                                               # general reconstruction property
 signalMuonCut += ' && isGlobalMuon'                                                                           # general reconstruction property
-signalMuonCut += ' && pt > 26.'                                                                               # transverse momentum
-signalMuonCut += ' && abs(eta) < 2.1'                                                                         # pseudo-rapisity range
 signalMuonCut += ' && globalTrack.normalizedChi2 < 10.'                                                       # muon ID: 'isGlobalMuonPromptTight'
 signalMuonCut += ' && track.hitPattern.trackerLayersWithMeasurement > 5'                                      # muon ID: 'isGlobalMuonPromptTight'
 signalMuonCut += ' && globalTrack.hitPattern.numberOfValidMuonHits > 0'                                       # muon ID: 'isGlobalMuonPromptTight'
 signalMuonCut += ' && abs(dB) < 0.2'                                                                          # 2-dim impact parameter with respect to beam spot (s. "PAT muon configuration" above)
 signalMuonCut += ' && innerTrack.hitPattern.numberOfValidPixelHits > 0'                                       # tracker reconstruction
 signalMuonCut += ' && numberOfMatchedStations > 1'                                                            # muon chamber reconstruction
+# Kinematics
+signalMuonCut += ' && pt > 26.'                                                                               # transverse momentum
+signalMuonCut += ' && abs(eta) < 2.1'                                                                         # pseudo-rapisity range
+# (Relative) isolation
 signalMuonCut += ' && (chargedHadronIso+max(0.,neutralHadronIso+photonIso-0.5*puChargedHadronIso))/pt < 0.12' # relative isolation w/ Delta beta corrections (factor 0.5)
 
 muonVertexMaxDZ = 0.5 # DeltaZ between muon vertex and PV
@@ -39,13 +45,15 @@ muonVertexMaxDZ = 0.5 # DeltaZ between muon vertex and PV
 ### Jet selection
 
 # Signal jet selection
-jetCut  =     'abs(eta) < 2.5'                                        # pseudo-rapisity range
-jetCut += ' && numberOfDaughters > 1'                                 # PF jet ID:
+# Jet ID
+jetCut  =     'numberOfDaughters > 1'                                 # PF jet ID:
 jetCut += ' && neutralHadronEnergyFraction < 0.99'                    # PF jet ID:
 jetCut += ' && neutralEmEnergyFraction < 0.99'                        # PF jet ID:
 jetCut += ' && (chargedEmEnergyFraction < 0.99 || abs(eta) >= 2.4)'   # PF jet ID:
 jetCut += ' && (chargedHadronEnergyFraction > 0. || abs(eta) >= 2.4)' # PF jet ID:
 jetCut += ' && (chargedMultiplicity > 0 || abs(eta) >= 2.4)'          # PF jet ID:
+# Kinematics
+jetCut  +' && abs(eta) < 2.5'                                        # pseudo-rapisity range
 # varying jet pt thresholds
 veryLooseJetCut = 'pt > 30.' # transverse momentum (all jets)
 looseJetCut     = 'pt > 30.' # transverse momentum (3rd jet, optional)
@@ -56,10 +64,13 @@ veryTightJetCut = 'pt > 30.' # transverse momentum (leading jets)
 
 # Minimal selection for veto electrons
 # ... using GsfElectron kinematics
-electronGsfCut  =     'ecalDrivenMomentum.pt > 20.'                                                           # transverse energy
-electronGsfCut += ' && abs(ecalDrivenMomentum.eta) < 2.5'                                                     # pseudo-rapisity range
-electronGsfCut += ' && electronID("cutBasedElectronID-CSA14-50ns-V1-standalone-veto")'                        # electrons ID
-electronGsfCut += ' && (chargedHadronIso+max(0.,neutralHadronIso+photonIso-0.5*puChargedHadronIso))/et < 0.2' # relative isolation with Delta beta corrections
+# Electron ID
+electronGsfCut  =     'electronID("cutBasedElectronID-CSA14-50ns-V1-standalone-veto")'                                                  # electrons ID
+# Kinematics
+electronGsfCut += ' && ecalDrivenMomentum.pt > 20.'                                                                                     # transverse energy
+electronGsfCut += ' && abs(ecalDrivenMomentum.eta) < 2.5'                                                                               # pseudo-rapisity range
+# (Relative) isolation
+electronGsfCut += ' && (chargedHadronIso+max(0.,neutralHadronIso+photonIso-1.0*userIsolation("User1Iso")))/ecalDrivenMomentum.pt < 0.2' # relative isolation with Delta beta corrections
 # ... using re-calibrated (with regression energy) kinematics
 electronCalibCut = electronGsfCut.replace( 'ecalDrivenMomentum.', '' )
 electronCut = electronGsfCut
