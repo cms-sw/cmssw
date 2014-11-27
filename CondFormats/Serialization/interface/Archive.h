2917cbeb245d771e27a515e6c9f371f9694f07fe
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2014-11-27 20:09:09 +0100

    add capability to dump payloads, tags, gts in XML format. First version with dynamically compiled code

--- CondFormats/Serialization/interface/Archive.h
+++ CondFormats/Serialization/interface/Archive.h
@@ -3,6 +3,12 @@
 // Missing in EOS' portable archive
 #include <cassert>
 
+#include "boost/archive/xml_iarchive.hpp"
+#include "boost/archive/xml_oarchive.hpp"
+
+#include "boost/archive/xml_wiarchive.hpp"
+#include "boost/archive/xml_woarchive.hpp"
+
 #include "CondFormats/Serialization/interface/eos/portable_iarchive.hpp"
 #include "CondFormats/Serialization/interface/eos/portable_oarchive.hpp"
 
@@ -11,6 +17,12 @@ namespace serialization {
 
   typedef eos::portable_iarchive InputArchive;
   typedef eos::portable_oarchive OutputArchive;
+
+  typedef boost::archive::xml_iarchive InputArchiveXML;
+  typedef boost::archive::xml_oarchive OutputArchiveXML;
+
+  typedef boost::archive::xml_wiarchive InputArchiveXMLw;
+  typedef boost::archive::xml_woarchive OutputArchiveXMLw;
 
 }
 }
