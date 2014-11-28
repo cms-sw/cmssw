740f6eb48a4af7651f19f21431bb102e1d487537
Colin <colin.bernet@cern.ch>
2014-11-28 16:00:36 +0100

    mini aod test files for 72X

--- PhysicsTools/Heppy/python/utils/miniAodFiles.py
+++ PhysicsTools/Heppy/python/utils/miniAodFiles.py
@@ -4,15 +4,20 @@ def miniAodFiles():
     relnum = releaseNumber(cmsswRelease())
     files = []
     big, medium = relnum[:2] # e.g. 7, 3 for CMSSW_7_3_X
-    if (big,medium)==(7,1):
+    if (big, medium)==(7,2):
+        files = [
+            '/store/relval/CMSSW_7_2_2_patch1/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_72_V1-v1/00000/5C64D172-8D73-E411-9C77-0025905A60AA.root',
+            '/store/relval/CMSSW_7_2_2_patch1/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_72_V1-v1/00000/6CA2B9CC-8973-E411-8920-002618943904.root'
+            ]
+    elif (big,medium)==(7,1):
         files = [
             '/store/relval/CMSSW_7_1_10_patch2/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_71_V1-v1/00000/3E4EC015-AF53-E411-8889-0025905A6076.root',
-                 '/store/relval/CMSSW_7_1_10_patch2/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_71_V1-v1/00000/C483E714-AF53-E411-9B9A-0025905B855E.root'
+            '/store/relval/CMSSW_7_1_10_patch2/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_71_V1-v1/00000/C483E714-AF53-E411-9B9A-0025905B855E.root'
             ]
     elif (big,medium)==(7,0):
         files = [
             '/store/relval/CMSSW_7_0_9_patch3/RelValZMM_13/MINIAODSIM/PU25ns_PLS170_V7AN2-v1/00000/E0D7EDE4-0660-E411-B655-02163E00EF88.root',
-             '/store/relval/CMSSW_7_0_9_patch3/RelValZMM_13/MINIAODSIM/PU25ns_PLS170_V7AN2-v1/00000/F0F88B6E-905F-E411-8080-0025904B0FBE.root'
+            '/store/relval/CMSSW_7_0_9_patch3/RelValZMM_13/MINIAODSIM/PU25ns_PLS170_V7AN2-v1/00000/F0F88B6E-905F-E411-8080-0025904B0FBE.root'
             ]
     elif (big,medium)==(7,3):
         files = [
