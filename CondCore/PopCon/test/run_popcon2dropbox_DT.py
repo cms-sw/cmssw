8ff3f774c89f8f6d3483deb8493a6b78111d37ca
Giacomo Govi <giacomo.govi@cern.ch>
2014-11-28 10:26:27 +0100

    Several fixes for popcon2dropbox - tested with RunInfo system

--- CondCore/PopCon/test/run_popcon2dropbox_DT.py
+++ CondCore/PopCon/test/run_popcon2dropbox_DT.py
@@ -5,7 +5,7 @@
 __author__ = 'Giacomo Govi'
 
 import sys
-import popcon2dropbox
+from CondCore.Utilities import popcon2dropbox
 
 cmssw_dir = '/data/cmssw'
 release_dir = '/nfshome0/popcondev/popcon2dropbox'
@@ -13,10 +13,11 @@ release = 'CMSSW_7_2_0_pre6'
 scram_arch = 'slc5_amd64_gcc481'
 job_file = 'popcon2dropbox_job.py'
 log_file = 'popcon.log'
+dbox_backend = 'offline' 
 
 def main():
-    print popcon2dropbox.runO2O( cmssw_dir, release_dir, release, scram_arch, job_file, log_file )
-    popcon2dropbox.upload_to_dropbox()
+    print popcon2dropbox.runO2O( cmssw_dir, release_dir, release, scram_arch, job_file, log_file, *sys.argv[1:] )
+    popcon2dropbox.upload_to_dropbox( dbox_backend )
 
 if __name__ == '__main__':
     sys.exit(main())
