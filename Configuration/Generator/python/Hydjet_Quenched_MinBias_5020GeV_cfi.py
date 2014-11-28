26cb88effad2e777b6dad11a949502eea440d5a0
wouf <wouf@users.noreply.github.com>
2014-11-28 17:18:10 +0300

    Update Hydjet_Quenched_MinBias_5020GeV_cfi.py

--- Configuration/Generator/python/Hydjet_Quenched_MinBias_5020GeV_cfi.py
+++ Configuration/Generator/python/Hydjet_Quenched_MinBias_5020GeV_cfi.py
@@ -8,19 +8,19 @@ generator = cms.EDFilter("HydjetGeneratorFilter",
 			hydjetParameters,
 			hydjetMode = cms.string('kHydroQJets'),
 			PythiaParameters = cms.PSet(pyquenPythiaDefaultBlock,
-			# Quarkonia and Weak Bosons added back upon dilepton group's request.
-			parameterSets = cms.vstring('pythiaUESettings',
-						'hydjetPythiaDefault',
-						'myParameters',
-						'pythiaJets',
-						'pythiaPromptPhotons',
-						'pythiaZjets',
-						'pythiaBottomoniumNRQCD',
-						'pythiaCharmoniumNRQCD',
-						'pythiaQuarkoniaSettings',
-						'pythiaWeakBosons'
-						)
-			),
+						# Quarkonia and Weak Bosons added back upon dilepton group's request.
+						parameterSets = cms.vstring('pythiaUESettings',
+									'hydjetPythiaDefault',
+									'myParameters',
+									'pythiaJets',
+									'pythiaPromptPhotons',
+									'pythiaZjets',
+									'pythiaBottomoniumNRQCD',
+									'pythiaCharmoniumNRQCD',
+									'pythiaQuarkoniaSettings',
+									'pythiaWeakBosons'
+									)
+						),
 			cFlag = cms.int32(1),
 			bMin = cms.double(0),
 			bMax = cms.double(30),
