8a73c83f76b80c1e4997d9ff3752efd6bd199765
Taejeong <monttj@gmail.com>
2014-12-03 12:28:58 +0100

    CITK (new isolation framework) for muon

---
+++ PhysicsTools/PatAlgos/test/patTuple_isoval_cfg.py
@@ -0,0 +1,37 @@
+## import skeleton process
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+## switch to uncheduled mode
+process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.Tracer = cms.Service("Tracer")
+
+process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
+process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff")
+
+process.load("RecoMuon.MuonIsolation.muonPFIsolationCitk_cff")
+
+## ------------------------------------------------------
+#  In addition you usually want to change the following
+#  parameters:
+## ------------------------------------------------------
+#
+#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+#                                         ##
+from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM
+process.source.fileNames = filesRelValProdTTbarAODSIM
+#                                         ##
+process.maxEvents.input = 2000
+#                                         ##
+#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#                                         ##
+process.out.fileName = 'PAT_isoval.root'
+#                                         ##
+#   process.options.wantSummary = False   ##  (to suppress the long output at the end of the job)
+
+process.out.outputCommands = ['keep *_muonPFNoPileUpIsolation_*_*',
+                               'keep *_muonPFPileUpIsolation_*_*',
+                               'keep *_muPFIsoValue*_*_*',
+                               'keep recoMuons_muons_*_*',
+                               'keep *_patMuons_*_*',
+                              ]
+
+
