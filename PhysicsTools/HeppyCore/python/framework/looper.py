7e542cd93e4af7cd9c432cc00ef99d294131ba87
Andrea <andrea.rizzi@cern.ch>
2014-12-05 10:14:56 +0100

    fix heppy_batch to pickle only the components and add Pisa batch system

--- PhysicsTools/HeppyCore/python/framework/looper.py
+++ PhysicsTools/HeppyCore/python/framework/looper.py
@@ -189,15 +189,25 @@ if __name__ == '__main__':
     import pickle
     import sys
     import os
+    if len(sys.argv) == 2 :
+	    cfgFileName = sys.argv[1]
+	    pckfile = open( cfgFileName, 'r' )
+	    config = pickle.load( pckfile )
+	    comp = config.components[0]
+	    events_class = config.events_class
+    elif len(sys.argv) == 3 :
+            cfgFileName = sys.argv[1]
+	    file = open( cfgFileName, 'r' )
+	    cfg = imp.load_source( 'cfg', cfgFileName, file)
+            compFileName = sys.argv[2]
+            pckfile = open( compFileName, 'r' )
+            comp = pickle.load( pckfile )
+    	    cfg.config.components=comp
+            events_class = cfg.config.events_class
 
-    cfgFileName = sys.argv[1]
-    pckfile = open( cfgFileName, 'r' )
-    config = pickle.load( pckfile )
-    comp = config.components[0]
-    events_class = config.events_class
     looper = Looper( 'Loop', comp,
-                     config.sequence,
-                     config.services,
+                     cfg.config.sequence,
+                     cfg.config.services,
                      events_class, 
                      nPrint = 5)
     looper.loop()
--- PhysicsTools/HeppyCore/scripts/heppy_batch.py
+++ PhysicsTools/HeppyCore/scripts/heppy_batch.py
@@ -32,6 +32,36 @@ def split(comps):
     return splitComps
 
 
+def batchScriptPISA( index, remoteDir=''):
+   '''prepare the LSF version of the batch script, to run on LSF'''
+   script = """#!/bin/bash
+#BSUB -q cms
+echo 'PWD:'
+pwd
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+echo 'environment:'
+echo
+env
+# ulimit -v 3000000 # NO
+echo 'copying job dir to worker'
+###cd $CMSSW_BASE/src
+eval `scramv1 ru -sh`
+# cd $LS_SUBCWD
+# eval `scramv1 ru -sh`
+##cd -
+##cp -rf $LS_SUBCWD .
+ls
+echo `find . -type d | grep /`
+echo 'running'
+python $CMSSW_BASE/src/PhysicsTools/HeppyCore/python/framework/looper.py pycfg.py config.pck
+echo
+echo 'sending the job directory back'
+cp -r Loop/* $LS_SUBCWD 
+"""
+   return script
+
+
 def batchScriptCERN( index, remoteDir=''):
    '''prepare the LSF version of the batch script, to run on LSF'''
    script = """#!/bin/bash
@@ -182,14 +212,16 @@ class MyBatchManager( BatchManager ):
            scriptFile.write( batchScriptPSI ( value, jobDir, storeDir ) ) # storeDir not implemented at the moment
        elif mode == 'LOCAL':
            scriptFile.write( batchScriptLocal( storeDir, value) )  # watch out arguments are swapped (although not used)
+       elif mode == 'PISA' :
+	   scriptFile.write( batchScriptPISA( storeDir, value) ) 	
        scriptFile.close()
        os.system('chmod +x %s' % scriptFileName)
        
        shutil.copyfile(cfgFileName, jobDir+'/pycfg.py')
-       jobConfig = copy.deepcopy(config)
-       jobConfig.components = [ components[value] ]
+#      jobConfig = copy.deepcopy(config)
+#      jobConfig.components = [ components[value] ]
        cfgFile = open(jobDir+'/config.pck','w')
-       pickle.dump( jobConfig, cfgFile )
+       pickle.dump(  components[value] , cfgFile )
        # pickle.dump( cfo, cfgFile )
        cfgFile.close()
 
