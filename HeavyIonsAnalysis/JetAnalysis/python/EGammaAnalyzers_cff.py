36151c1ffa20ce23b7cbef267e370c0e6c26d764
Doga <dgulhan@cern.ch>
2014-12-05 18:41:53 +0100

    forgot to add this for track input change for photons

--- HeavyIonsAnalysis/JetAnalysis/python/EGammaAnalyzers_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/EGammaAnalyzers_cff.py
@@ -4,6 +4,7 @@ from HeavyIonsAnalysis.PhotonAnalysis.MultiPhotonAnalyzer_cfi import multiPhoton
 multiPhotonAnalyzer.GenParticleProducer = cms.InputTag("hiGenParticles")
 multiPhotonAnalyzer.PhotonProducer = cms.InputTag("selectedPatPhotons")
 multiPhotonAnalyzer.VertexProducer = cms.InputTag("hiSelectedVertex")
+multiPhotonAnalyzer.TrackProducer = cms.InputTag("hiGeneralTracks")
 multiPhotonAnalyzer.OutputFile = cms.string('mpa.root')
 multiPhotonAnalyzer.doStoreCompCone = cms.untracked.bool(False)
 multiPhotonAnalyzer.doStoreJets = cms.untracked.bool(False)
@@ -12,23 +13,25 @@ multiPhotonAnalyzer.gsfElectronCollection = cms.untracked.InputTag("ecalDrivenGs
 multiPhotonAnalyzer.GammaEtaMax = cms.untracked.double(100)
 multiPhotonAnalyzer.GammaPtMin = cms.untracked.double(10)
 
-from HeavyIonsAnalysis.HiTrkEffAnalyzer.TrackSelections_cff import hiGoodTracks
+# from HeavyIonsAnalysis.HiTrkEffAnalyzer.TrackSelections_cff import hiGoodTracks
 from PhysicsTools.PatAlgos.patHeavyIonSequences_cff import photonMatch, patPhotons, selectedPatPhotons
 
-hiGoodTracks.src = cms.InputTag("hiGeneralTracks")
+# hiGoodTracks.src = cms.InputTag("hiGeneralTracks")
 photonMatch.matched = cms.InputTag("hiGenParticles")
 patPhotons.addPhotonID = cms.bool(False)
 
 from RecoHI.HiEgammaAlgos.HiEgamma_cff import hiPhotonSequence
 
-photonStep_withReco = cms.Sequence(hiGoodTracks *
+photonStep_withReco = cms.Sequence(
+                                   # hiGoodTracks *
                                    hiPhotonSequence * # need to re-run to get track info
                                    photonMatch *
                                    patPhotons *
                                    selectedPatPhotons *
                                    multiPhotonAnalyzer)
 
-photonStep = cms.Sequence(hiGoodTracks *
+photonStep = cms.Sequence(
+                          # hiGoodTracks *
                           photonMatch *
                           patPhotons *
                           selectedPatPhotons *
