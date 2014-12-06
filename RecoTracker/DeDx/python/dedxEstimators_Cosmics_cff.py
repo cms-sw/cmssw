bc665e7b5714be6b483eac0ec2412ff923c5cf7a
Loic <Loic.Quertenmont@gmail.com>
2014-12-06 17:39:02 +0100

    drop RS

--- RecoTracker/DeDx/python/dedxEstimators_Cosmics_cff.py
+++ RecoTracker/DeDx/python/dedxEstimators_Cosmics_cff.py
@@ -19,23 +19,6 @@ dedxDiscrimASmiCTF                                      = dedxDiscrimASmi.clone(
 dedxDiscrimASmiCTF.tracks                               = cms.InputTag("ctfWithMaterialTracksP5")
 dedxDiscrimASmiCTF.trajectoryTrackAssociation           = cms.InputTag("ctfWithMaterialTracksP5")
 
-###RS
-dedxHitInfoRS                                      = dedxHitInfo.clone()
-dedxHitInfoRS.tracks                               = cms.InputTag("rsWithMaterialTracksP5")
-dedxHitInfoRS.trajectoryTrackAssociation           = cms.InputTag("rsWithMaterialTracksP5")
-
-dedxTruncated40RS                                  = dedxTruncated40.clone()
-dedxTruncated40RS.tracks                           = cms.InputTag("rsWithMaterialTracksP5")
-dedxTruncated40RS.trajectoryTrackAssociation       = cms.InputTag("rsWithMaterialTracksP5")
-
-dedxHarmonic2RS                                    = dedxHarmonic2.clone()
-dedxHarmonic2RS.tracks                             = cms.InputTag("rsWithMaterialTracksP5")
-dedxHarmonic2RS.trajectoryTrackAssociation         = cms.InputTag("rsWithMaterialTracksP5")
-
-dedxDiscrimASmiRS                                       = dedxDiscrimASmi.clone()
-dedxDiscrimASmiRS.tracks                                = cms.InputTag("rsWithMaterialTracksP5")
-dedxDiscrimASmiRS.trajectoryTrackAssociation            = cms.InputTag("rsWithMaterialTracksP5")
-
 #TF
 dedxHitInfoCosmicTF                                = dedxHitInfo.clone()
 dedxHitInfoCosmicTF.tracks                         = cms.InputTag("cosmictrackfinderP5")
@@ -72,11 +55,8 @@ dedxHarmonic2CTFP5LHC.trajectoryTrackAssociation   = cms.InputTag("ctfWithMateri
 
 
 doAlldEdXEstimatorsCTF      = cms.Sequence(dedxTruncated40CTF      + dedxHitInfoCTF      + dedxHarmonic2CTF)
-doAlldEdXEstimatorsRS       = cms.Sequence(dedxTruncated40RS       + dedxHitInfoRS       + dedxHarmonic2RS)
 doAlldEdXEstimatorsCosmicTF = cms.Sequence(dedxTruncated40CosmicTF + dedxHitInfoCosmicTF + dedxHarmonic2CosmicTF)
 doAlldEdXEstimatorsCTFP5LHC = cms.Sequence(dedxTruncated40CTFP5LHC + dedxHitInfoCTFP5LHC + dedxHarmonic2CTFP5LHC)
 
-# (SK) keep rstracks commented out in case of resurrection
-#doAllCosmicdEdXEstimators = cms.Sequence( doAlldEdXEstimatorsCTF + doAlldEdXEstimatorsRS + doAlldEdXEstimatorsCosmicTF + doAlldEdXEstimatorsCTFP5LHC)
 doAllCosmicdEdXEstimators = cms.Sequence( doAlldEdXEstimatorsCTF + doAlldEdXEstimatorsCosmicTF + doAlldEdXEstimatorsCTFP5LHC)
 
