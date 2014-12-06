e304c64544e3ff3d585344bc409f976fb3af7a0b
R. Alex Barbieri <luck@mit.edu>
2014-12-06 01:54:09 +0100

    disable tracking efficiency

--- HeavyIonsAnalysis/JetAnalysis/python/TrkAnalyzers_MC_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/TrkAnalyzers_MC_cff.py
@@ -1,17 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 from HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_cff import *
-from HeavyIonsAnalysis.JetAnalysis.TrkEfficiency_cff import *
+#from HeavyIonsAnalysis.JetAnalysis.TrkEfficiency_cff import *
 from SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi import *
 TrackAssociatorByChi2ESProducer.chi2cut = cms.double(10.0)
 
 anaTrack.doSimVertex = True
-anaTrack.doSimTrack = True
+#anaTrack.doSimTrack = True
 anaTrack.fillSimTrack = cms.untracked.bool(True)
 anaTrack.simTrackPtMin = 0.4
 
 pixelTrack.doSimVertex = True
-pixelTrack.doSimTrack = True
+#pixelTrack.doSimTrack = True
 pixelTrack.simTrackPtMin = 0.4
 pixelTrack.fillSimTrack = cms.untracked.bool(True)
 
@@ -28,8 +28,8 @@ anaTrack.tpEffSrc = cms.untracked.InputTag("cutsTPForEff")
 pixelTrack.tpFakeSrc = cms.untracked.InputTag("cutsTPForFak")
 pixelTrack.tpEffSrc = cms.untracked.InputTag("cutsTPForEff")
 
-cutsTPForFak.ptMin = 0.4
-cutsTPForEff.ptMin = 0.4
+#cutsTPForFak.ptMin = 0.4
+#cutsTPForEff.ptMin = 0.4
 
 ppTrack.doSimVertex = True
 ppTrack.doSimTrack = True
