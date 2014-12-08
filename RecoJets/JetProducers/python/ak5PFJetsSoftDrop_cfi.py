266b640f4a95fa97556a6cfea7c530c7e893ebe9
Andreas Hinzmann <hinzmann@cern.ch>
2014-12-08 10:34:25 +0100

    resolve merge conflict

--- RecoJets/JetProducers/python/ak5PFJetsSoftDrop_cfi.py
+++ RecoJets/JetProducers/python/ak5PFJetsSoftDrop_cfi.py
@@ -8,11 +8,7 @@ ak5PFJetsSoftDrop = ak5PFJets.clone(
     useSoftDrop = cms.bool(True),
     zcut = cms.double(0.1),
     beta = cms.double(0.0),
-<<<<<<< HEAD
     useExplicitGhosts = cms.bool(True),
-=======
-    useExplicitGhosts = cms.bool(True)
->>>>>>> 1e04e02... add subjets to ak5 softdrop
     writeCompound = cms.bool(True),
     jetCollInstanceName=cms.string("SubJets")
     )
