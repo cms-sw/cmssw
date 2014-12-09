ef7ae4df1c2da51f06220495034ff8774b2a9edc
Andreas Hinzmann <hinzmann@cern.ch>
2014-12-09 13:05:55 +0100

    keep ak4GenJets for a pre-release until new relvals for PAT are available

--- RecoJets/Configuration/python/GenJetParticles_cff.py
+++ RecoJets/Configuration/python/GenJetParticles_cff.py
@@ -30,4 +30,4 @@ genParticlesForJetsNoNu.ignoreParticleIDs += cms.vuint32( 12,14,16)
 genParticlesForJetsNoMuNoNu = genParticlesForJets.clone()
 genParticlesForJetsNoMuNoNu.ignoreParticleIDs += cms.vuint32( 12,13,14,16)
 
-genJetParticles = cms.Sequence(genParticlesForJetsNoNu)
+genJetParticles = cms.Sequence(genParticlesForJets+genParticlesForJetsNoNu)
