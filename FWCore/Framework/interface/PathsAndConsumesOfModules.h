3d846b2c704ee8f9af8405a45020e36caf35dc94
W. David Dagenhart <wdd@fnal.gov>
2014-12-09 09:23:26 -0600

    Add service callback with path and consumes info
    Add new callback that services can register for
    that will deliver information about the paths,
    end paths, and modules on them. It will also deliver
    information about the dependencies between modules
    in the current process based on the consumes information.
    It also makes available the information in the
    consumes declarations. Also includes a new configuration
    option to the Tracer that will dump this information
    and the Tracer code is an example of how a service
    accesses this information. New and expanded unit
    tests included.

---
+++ FWCore/Framework/interface/PathsAndConsumesOfModules.h
@@ -0,0 +1,72 @@
+#ifndef FWCore_Framework_PathsAndConsumesOfModules_h
+#define FWCore_Framework_PathsAndConsumesOfModules_h
+
+/**\class edm::PathsAndConsumesOfModules
+
+ Description: See comments in the base class
+
+ Usage:
+
+*/
+//
+// Original Author: W. David Dagenhart
+//         Created: 11/5/2014
+
+#include "FWCore/ServiceRegistry/interface/ConsumesInfo.h"
+#include "FWCore/ServiceRegistry/interface/PathsAndConsumesOfModulesBase.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace edm {
+
+  class ModuleDescription;
+  class ProductRegistry;
+  class Schedule;
+
+  class PathsAndConsumesOfModules : public PathsAndConsumesOfModulesBase {
+  public:
+
+    virtual ~PathsAndConsumesOfModules();
+
+    void initialize(Schedule const*, std::shared_ptr<ProductRegistry const>);
+
+  private:
+
+    virtual std::vector<std::string> const& doPaths() const override { return paths_; }
+    virtual std::vector<std::string> const& doEndPaths() const override { return endPaths_; }
+
+    virtual std::vector<ModuleDescription const*> const& doAllModules() const override { return allModuleDescriptions_; }
+    virtual ModuleDescription const* doModuleDescription(unsigned int moduleID) const override;
+
+    virtual std::vector<ModuleDescription const*> const& doModulesOnPath(unsigned int pathIndex) const override;
+    virtual std::vector<ModuleDescription const*> const& doModulesOnEndPath(unsigned int endPathIndex) const override;
+    virtual std::vector<ModuleDescription const*> const& doModulesWhoseProductsAreConsumedBy(unsigned int moduleID) const override;
+
+    virtual std::vector<ConsumesInfo> doConsumesInfo(unsigned int moduleID) const override;
+
+    unsigned int moduleIndex(unsigned int moduleID) const;
+
+    // data members
+
+    std::vector<std::string> paths_;
+    std::vector<std::string> endPaths_;
+
+    std::vector<ModuleDescription const*> allModuleDescriptions_;
+
+    std::vector<std::vector<ModuleDescription const*> > modulesOnPaths_;
+    std::vector<std::vector<ModuleDescription const*> > modulesOnEndPaths_;
+
+    // Gives a translation from the module ID to the index into the
+    // following data member
+    std::vector<std::pair<unsigned int, unsigned int> > moduleIDToIndex_;
+
+    std::vector<std::vector<ModuleDescription const*> > modulesWhoseProductsAreConsumedBy_;
+
+    Schedule const* schedule_;
+    std::shared_ptr<ProductRegistry const> preg_;
+  };
+}
+#endif
