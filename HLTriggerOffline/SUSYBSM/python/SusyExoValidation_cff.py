185bd64a6e6ed7dd55b4c8b7c3bfc7733ebbe416
aelwood <adam.elwood09@imperial.ac.uk>
2014-12-11 12:03:48 +0100

    Brought the SUSYBSM_postProcessor_cff.py and SusyExoValidation_cff.py files in line with the 74X version

--- HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
@@ -5,6 +5,14 @@ from HLTriggerOffline.SUSYBSM.SUSYBSM_inclusiveMET_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_MET_MUON_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_inclusiveHT_aux350_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_inclusiveHT_aux600_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Mu_HT_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Mu_HT_MET_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Mu_HT_BTag_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Mu_HT_Control_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Ele_HT_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Ele_HT_MET_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Ele_HT_BTag_SingleLepton_cff import *
+from HLTriggerOffline.SUSYBSM.SUSYBSM_Ele_HT_Control_SingleLepton_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_MET_MUON_ER_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_MET_HT_MUON_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_MET_HT_MUON_ER_cff import *
@@ -27,6 +35,14 @@ HLTSusyExoValSeq = cms.Sequence(SUSY_HLT_HT_MET +
                                 SUSY_HLT_MET_MUON +
                                 SUSY_HLT_InclusiveHT_aux350 + 
                                 SUSY_HLT_InclusiveHT_aux600 +
+                                SUSY_HLT_Mu_HT_SingleLepton +
+                                SUSY_HLT_Mu_HT_MET_SingleLepton +
+                                SUSY_HLT_Mu_HT_BTag_SingleLepton +
+                                SUSY_HLT_Mu_HT_Control_SingleLepton +
+                                SUSY_HLT_Ele_HT_SingleLepton +
+                                SUSY_HLT_Ele_HT_MET_SingleLepton +
+                                SUSY_HLT_Ele_HT_BTag_SingleLepton +
+                                SUSY_HLT_Ele_HT_Control_SingleLepton +
                                 SUSY_HLT_MET_MUON_ER +
                                 SUSY_HLT_MET_HT_MUON +
                                 SUSY_HLT_MET_HT_MUON_ER +
@@ -58,6 +74,14 @@ HLTSusyExoValSeq_FastSim = cms.Sequence(SUSY_HLT_HT_MET_FASTSIM +
                                         SUSY_HLT_InclusiveMET_FASTSIM + 
                                         SUSY_HLT_MET_BTAG_FASTSIM +
                                         SUSY_HLT_MET_MUON_FASTSIM +
+                                        SUSY_HLT_Mu_HT_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Mu_HT_MET_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Mu_HT_BTag_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Mu_HT_Control_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Ele_HT_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Ele_HT_MET_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Ele_HT_BTag_SingleLepton_FASTSIM +
+                                        SUSY_HLT_Ele_HT_Control_SingleLepton_FASTSIM +
                                         SUSY_HLT_InclusiveHT_aux350_FASTSIM + 
                                         SUSY_HLT_InclusiveHT_aux600_FASTSIM +
                                         SUSY_HLT_MET_MUON_ER_FASTSIM +
