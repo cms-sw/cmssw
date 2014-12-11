b601d2b5a8df19bd515744bb5da6164ede1b5879
Martin Grunewald <Martin.Grunewald@cern.ch>
2014-12-11 13:23:25 +0100

    Fix HLTPrescaler label

--- HLTrigger/Configuration/test/OnData_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnData_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_3_0/PIon/V8 (CMSSW_7_3_0)
+# /dev/CMSSW_7_3_0/PIon/V9 (CMSSW_7_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V8')
+  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V9')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
