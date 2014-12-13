0efeb6e9014c12235a8dda3a8a63e2404ae30445
R. Alex Barbieri <luck@mit.edu>
2014-12-13 19:25:15 +0100

    fix photon, ignore pf clusters for now

--- HeavyIonsAnalysis/JetAnalysis/python/EGammaAnalyzers_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/EGammaAnalyzers_cff.py
@@ -3,7 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from HeavyIonsAnalysis.PhotonAnalysis.MultiPhotonAnalyzer_cfi import multiPhotonAnalyzer
 multiPhotonAnalyzer.GenParticleProducer = cms.InputTag("hiGenParticles")
 multiPhotonAnalyzer.PhotonProducer = cms.InputTag("selectedPatPhotons")
-multiPhotonAnalyzer.PhotonProducer = cms.InputTag("photons")
 multiPhotonAnalyzer.VertexProducer = cms.InputTag("hiSelectedVertex")
 multiPhotonAnalyzer.TrackProducer = cms.InputTag("hiGeneralTracks")
 multiPhotonAnalyzer.OutputFile = cms.string('mpa.root')
@@ -14,10 +13,8 @@ multiPhotonAnalyzer.gsfElectronCollection = cms.untracked.InputTag("ecalDrivenGs
 multiPhotonAnalyzer.GammaEtaMax = cms.untracked.double(100)
 multiPhotonAnalyzer.GammaPtMin = cms.untracked.double(10)
 
-# from HeavyIonsAnalysis.HiTrkEffAnalyzer.TrackSelections_cff import hiGoodTracks
 from PhysicsTools.PatAlgos.patHeavyIonSequences_cff import photonMatch, patPhotons, selectedPatPhotons
 
-# hiGoodTracks.src = cms.InputTag("hiGeneralTracks")
 photonMatch.matched = cms.InputTag("hiGenParticles")
 photonMatch.src = cms.InputTag("photons")
 
@@ -25,12 +22,12 @@ patPhotons.addPhotonID = cms.bool(False)
 patPhotons.photonSource = cms.InputTag("photons")
 patPhotons.reducedBarrelRecHitCollection = cms.InputTag("ecalRecHit:EcalRecHitsEB")
 patPhotons.reducedEndcapRecHitCollection = cms.InputTag("ecalRecHit:EcalRecHitsEE")
-patPhotons.isoDeposits = cms.VInputTag()
+patPhotons.isoDeposits = cms.PSet()
+patPhotons.isolationValues = cms.PSet()
 
 from RecoHI.HiEgammaAlgos.HiEgamma_cff import hiPhotonSequence
 
 photonStep_withReco = cms.Sequence(
-                                   # hiGoodTracks *
                                    hiPhotonSequence * # need to re-run to get track info
                                    photonMatch *
                                    patPhotons *
@@ -38,7 +35,6 @@ photonStep_withReco = cms.Sequence(
                                    multiPhotonAnalyzer)
 
 photonStep = cms.Sequence(
-                          # hiGoodTracks *
                           photonMatch *
                           patPhotons *
                           selectedPatPhotons *
