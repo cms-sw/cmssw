aa98b4357068e6554f6ada39776f881640e6ba3b
mariarosaria <dalfonso@cern.ch>
2014-12-15 15:22:20 +0100

    Heppy IsoTrack: updated class and added Analyzer

--- PhysicsTools/Heppy/python/analyzers/objects/__init__.py
+++ PhysicsTools/Heppy/python/analyzers/objects/__init__.py
@@ -4,4 +4,5 @@ from PhysicsTools.Heppy.analyzers.objects.LeptonAnalyzer import LeptonAnalyzer
 from PhysicsTools.Heppy.analyzers.objects.METAnalyzer import METAnalyzer
 from PhysicsTools.Heppy.analyzers.objects.PhotonAnalyzer import PhotonAnalyzer
 from PhysicsTools.Heppy.analyzers.objects.TauAnalyzer import TauAnalyzer
+from PhysicsTools.Heppy.analyzers.objects.IsoTrackAnalyzer import IsoTrackAnalyzer
 from PhysicsTools.Heppy.analyzers.objects.VertexAnalyzer import VertexAnalyzer
