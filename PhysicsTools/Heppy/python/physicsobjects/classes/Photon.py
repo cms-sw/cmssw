5ce8ff15b4d1ca890c2cb513f8be75a4015a284c
Giovanni <gpetruc@gmail.com>
2014-12-15 10:02:01 +0100

    Heppy Photon: full5x5_hadTowOverEm not available in 7.2.X

--- PhysicsTools/Heppy/python/physicsobjects/classes/Photon.py
+++ PhysicsTools/Heppy/python/physicsobjects/classes/Photon.py
@@ -5,7 +5,10 @@ class Photon(PhysicsObject ):
     '''                                                                                                                                                                                                                                                                return object from the photon 
     '''
     def hOVERe(self):
-        return self.physObj.full5x5_hadTowOverEm()
+#        return self.physObj.full5x5_hadTowOverEm()
+        hadTowDepth1O = self.physObj.hadTowDepth1OverEm() * (self.physObj.superCluster().energy()/self.physObj.full5x5_e5x5() if self.physObj.full5x5_e5x5() else 1)
+        hadTowDepth2O = self.physObj.hadTowDepth2OverEm() * (self.physObj.superCluster().energy()/self.physObj.full5x5_e5x5() if self.physObj.full5x5_e5x5() else 1)
+        return hadTowDepth1O + hadTowDepth2O
 
     def r9(self):
         return self.physObj.full5x5_r9()
