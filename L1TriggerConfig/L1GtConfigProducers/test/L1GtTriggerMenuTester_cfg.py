687c15a08f37c897d03667499085c4ec4e6f5e2c
Vasile Mihai Ghete <Vasile.Mihai.Ghete@cern.ch>
2014-12-15 12:38:00 +0100

    Added menu L1Menu_Collisions2015_25ns_v2.

--- L1TriggerConfig/L1GtConfigProducers/test/L1GtTriggerMenuTester_cfg.py
+++ L1TriggerConfig/L1GtConfigProducers/test/L1GtTriggerMenuTester_cfg.py
@@ -33,7 +33,7 @@ process = cms.Process(processName)
 
 process.load("L1TriggerConfig.L1GtConfigProducers.l1GtTriggerMenuTester_cfi")
 process.l1GtTriggerMenuTester.OverwriteHtmlFile = True
-process.l1GtTriggerMenuTester.HtmlFile = "L1Menu_Collisions2015_25ns_v1_L1T_Scales_20101224_Imp0_0x102f.html"
+process.l1GtTriggerMenuTester.HtmlFile = "L1Menu_Collisions2015_25ns_v2_L1T_Scales_20141121_Imp0_0x1030.html"
 process.l1GtTriggerMenuTester.UseHltMenu = useHltMenuOption
 process.l1GtTriggerMenuTester.HltProcessName = processName
 #process.l1GtTriggerMenuTester.NoThrowIncompatibleMenu = False
