bc39e34590f2f6c7c8e806962afdd7120944b21d
mariarosaria <dalfonso@cern.ch>
2014-12-16 10:51:25 +0100

    Heppy: removed redundant HemisphereViaKt

--- PhysicsTools/Heppy/src/classes.h
+++ PhysicsTools/Heppy/src/classes.h
@@ -9,7 +9,6 @@
 #include "PhysicsTools/Heppy/interface/Davismt2.h"
 #include "PhysicsTools/Heppy/interface/mt2w_bisect.h"
 #include "PhysicsTools/Heppy/interface/Hemisphere.h"
-#include "PhysicsTools/Heppy/interface/HemisphereViaKt.h"
 #include "PhysicsTools/Heppy/interface/AlphaT.h"
 #include "PhysicsTools/Heppy/interface/ReclusterJets.h"
 #include "EgammaAnalysis/ElectronTools/interface/SimpleElectron.h"
@@ -25,8 +24,7 @@ namespace {
     heppy::TriggerBitChecker checker;
     heppy::CMGMuonCleanerBySegmentsAlgo cmgMuonCleanerBySegmentsAlgo;
     heppy::EGammaMvaEleEstimatorFWLite egMVA;
-    heppy::Hemisphere hemisphere_;
-    heppy::HemisphereViaKt hemisphereViaKt_;
+    heppy::Hemisphere hemisphere(std::vector<float> px, std::vector<float> py, std::vector<float> pz, std::vector<float> E, int hemi_seed, int hemi_association);
     heppy::Davismt2 mt2;
     heppy::mt2w_bisect::mt2w mt2wlept;
     heppy::AlphaT alphaT;
