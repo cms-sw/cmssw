8dda6ba852ae11d34b941f51e1e010276365fa35
Gaelle <Gaelle.Boudoul@cern.ch>
2014-12-16 22:40:55 +0100

    NoExtPix scenario

---
+++ Configuration/Geometry/python/GeometryExtended2023SHCalNoTaperNoExtPix_cff.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+from Geometry.CMSCommonData.cmsExtendedGeometry2023SHCalNoTaperXML_cfi import *
+from Geometry.TrackerNumberingBuilder.trackerNumberingSLHCGeometry_cfi import *
+from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
+from Geometry.HGCalCommonData.shashlikNumberingInitialization_cfi import *
