48340492cbed751a8f3e504cd10822206a7172af
Giovanni <gpetruc@gmail.com>
2014-12-16 18:14:35 +0100

    use all.py instead of __init__.py to imoprt everything so that pepople loading only one are not slowed down

--- PhysicsTools/Heppy/python/analyzers/core/__init__.py
+++ PhysicsTools/Heppy/python/analyzers/core/__init__.py
@@ -1,8 +1,1 @@
-from PhysicsTools.Heppy.analyzers.core.AutoFillTreeProducer import AutoFillTreeProducer
-from PhysicsTools.Heppy.analyzers.core.EventSelector import EventSelector
-from PhysicsTools.Heppy.analyzers.core.JSONAnalyzer import JSONAnalyzer
-from PhysicsTools.Heppy.analyzers.core.PileUpAnalyzer import PileUpAnalyzer
-from PhysicsTools.Heppy.analyzers.core.SkimAnalyzerCount import SkimAnalyzerCount
-from PhysicsTools.Heppy.analyzers.core.TreeAnalyzerNumpy import TreeAnalyzerNumpy
-from PhysicsTools.Heppy.analyzers.core.TriggerBitAnalyzer import TriggerBitAnalyzer
-from PhysicsTools.Heppy.analyzers.core.TriggerBitFilter import TriggerBitFilter
+# empty
