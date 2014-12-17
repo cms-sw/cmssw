8e3adf4d72f8bd1a587704172d27dde71aaee795
Gaelle <Gaelle.Boudoul@cern.ch>
2014-12-17 05:03:37 +0100

    NoExtPix scenario

--- Configuration/Geometry/python/GeometryExtended2023SHCalNoTaperNoExtPixReco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023SHCalNoTaperNoExtPixReco_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for transient ECAL alignement
-from Configuration.Geometry.GeometryExtended2023SHCalNoTaper_cff import *
+from Configuration.Geometry.GeometryExtended2023SHCalNoTaperNoExtPix_cff import *
 
 # Reconstruction geometry services
 #  Tracking Geometry
