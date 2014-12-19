a620697653571818670a50017d3640c61f95e03d
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2014-12-19 10:25:27 -0800

    Move new candidate tower PBs to 73.

---
+++ Fireworks/Candidates/interface/FWCandidateTowerSliceSelector.h
@@ -0,0 +1,16 @@
+#ifndef Fireworks_Calo_FWCandTowerSliceSelector_h
+#define Fireworks_Calo_FWCandTowerSliceSelector_h
+
+#include "Fireworks/Calo/interface/FWHistSliceSelector.h"
+
+class FWCandidateTowerSliceSelector : public FWHistSliceSelector
+{
+public:
+   FWCandidateTowerSliceSelector(TH2F* h, const FWEventItem* i);
+   virtual ~FWCandidateTowerSliceSelector();
+
+ protected:
+   virtual void getItemEntryEtaPhi(int itemIdx, float& eta, float& phi) const;
+};
+
+#endif
---
+++ Fireworks/ParticleFlow/plugins/FWPFCandidateTowerProxyBuilder.cc
@@ -0,0 +1,93 @@
+// -*- C++ -*-
+//
+// Package:     Calo
+// Class  :     FWCaloTowerProxyBuilderBase
+//
+// Implementation:
+//     <Notes on implementation>
+//
+// Original Author:  Chris Jones
+//         Created:  Wed Dec  3 11:28:28 EST 2008
+//
+
+// system includes
+#include <math.h>
+
+// user includes
+#include "TEveCaloData.h"
+#include "TEveCalo.h"
+#include "TH2F.h"
+
+#include "Fireworks/Core/interface/Context.h"
+#include "Fireworks/Core/interface/FWEventItem.h"
+#include "Fireworks/Core/interface/FWModelChangeManager.h"
+#include "Fireworks/Candidates/interface/FWCandidateTowerSliceSelector.h"
+
+#include "Fireworks/Core/interface/fw3dlego_xbins.h"
+#include "Fireworks/ParticleFlow/plugins/FWPFCandidateTowerProxyBuilder.h"
+
+#include "DataFormats/CaloTowers/interface/CaloTower.h"
+
+
+
+//
+// constructors , dectructors
+//
+FWPFCandidateTowerProxyBuilder::FWPFCandidateTowerProxyBuilder():
+m_towers(0)
+{
+}
+
+FWPFCandidateTowerProxyBuilder::~FWPFCandidateTowerProxyBuilder()
+{
+}
+
+//
+// member functions
+//
+
+
+
+void
+FWPFCandidateTowerProxyBuilder::build(const FWEventItem* iItem, TEveElementList* el, const FWViewContext* ctx)
+{
+   m_towers=0;
+   if (iItem)
+   {
+      iItem->get(m_towers);
+      FWCaloDataProxyBuilderBase::build(iItem, el, ctx);
+   }
+}
+
+
+FWHistSliceSelector*
+FWPFCandidateTowerProxyBuilder::instantiateSliceSelector()
+{
+   FWCandidateTowerSliceSelector* ss = new FWCandidateTowerSliceSelector(m_hist, item());
+   return ss;
+}
+
+void
+FWPFCandidateTowerProxyBuilder::fillCaloData()
+{
+    m_hist->Reset();
+
+    if (m_towers)
+    {
+        if(item()->defaultDisplayProperties().isVisible()) {
+            // assert(item()->size() >= m_towers->size());
+            unsigned int index=0;
+            for( reco::PFCandidateConstIterator tower = m_towers->begin(); tower != m_towers->end(); ++tower,++index) {
+                const FWEventItem::ModelInfo& info = item()->modelInfo(index);
+                if(info.displayProperties().isVisible()) {
+                   addEntryToTEveCaloData(tower->eta(), tower->phi(), tower->pt(), info.isSelected());
+                }
+            }
+        }
+    }
+}
+
+REGISTER_FWPROXYBUILDER(FWECalPFCandidateProxyBuilder, reco::PFCandidateCollection,"PFECal",FWViewType::k3DBit|FWViewType::kAllRPZBits|FWViewType::kAllLegoBits);
+REGISTER_FWPROXYBUILDER(FWHCalPFCandidateProxyBuilder, reco::PFCandidateCollection,"PFHCal",FWViewType::k3DBit|FWViewType::kAllRPZBits|FWViewType::kAllLegoBits );
+
+
