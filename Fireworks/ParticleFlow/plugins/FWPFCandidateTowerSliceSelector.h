a3a50295fd36f2329f652d5291321c8c45d8f1fc
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2014-12-23 13:31:35 -0800

    Correction in accessing Et values.

---
+++ Fireworks/ParticleFlow/plugins/FWPFCandidateTowerSliceSelector.h
@@ -0,0 +1,16 @@
+#ifndef Fireworks_Calo_FWPFCandTowerSliceSelector_h
+#define Fireworks_Calo_FWPFCandTowerSliceSelector_h
+
+#include "Fireworks/Calo/interface/FWHistSliceSelector.h"
+
+class FWPFCandidateTowerSliceSelector : public FWHistSliceSelector
+{
+public:
+   FWPFCandidateTowerSliceSelector(TH2F* h, const FWEventItem* i);
+   virtual ~FWPFCandidateTowerSliceSelector();
+
+ protected:
+   virtual void getItemEntryEtaPhi(int itemIdx, float& eta, float& phi) const;
+};
+
+#endif
