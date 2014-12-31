7bc7450707958cf5e8b767f752b1f70ff07b6b87
Andrea <perrotta@cern.ch>
2014-12-31 10:30:01 +0100

    Put back AK4 correctors to AK8 jet modules, until the AK8 correctors will get available in all GT's

--- HLTrigger/Configuration/test/OnData_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnData_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_3_0/HIon/V14 (CMSSW_7_3_0)
+# /dev/CMSSW_7_3_0/HIon/V15 (CMSSW_7_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/HIon/V14')
+  tableName = cms.string('/dev/CMSSW_7_3_0/HIon/V15')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/test/OnMc_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnMc_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_3_0/PIon/V14 (CMSSW_7_3_0)
+# /dev/CMSSW_7_3_0/PIon/V15 (CMSSW_7_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V14')
+  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V15')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
