144ddff7f858b25b3e76dddd5af618fc7c5f7bf3
Mark Grimes <mark.grimes@cern.ch>
2015-01-06 15:46:07 +0100

    Remove the Extended2023SHCalNoTaperNoTrk geometry

--- Configuration/Geometry/python/GeometryExtended2023SHCalNoTaperNoTrk_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Geometry.CMSCommonData.cmsExtendedGeometry2023SHCalNoTaperXMLNoTrk_cfi import *
-#from Geometry.TrackerNumberingBuilder.trackerNumberingSLHCGeometry_cfi import *
-from Geometry.HcalCommonData.hcalSimNumberingInitialization_cfi import *
-from Geometry.HGCalCommonData.shashlikNumberingInitialization_cfi import *
