9709c94989032059a31ab32dfa73187e55ea2f18
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-01-06 13:24:29 +0100

    Add plot for generated vertex position

--- Validation/RecoVertex/python/plotting/vertexPlots.py
+++ Validation/RecoVertex/python/plotting/vertexPlots.py
@@ -4,7 +4,7 @@ import Validation.RecoTrack.plotting.validation as validation
 _maxPU = 80
 _maxVtx = 60
 _maxEff = 1.025
-_maxFake = 0.2
+_maxFake = [0.2, 0.5, 1.025]
 
 _common = {"xlabel": "Pileup interactions", "xmax": _maxPU, "ymax": _maxVtx}
 _recovsgen = PlotGroup("recovsgen", [
@@ -23,6 +23,15 @@ _effandfake = PlotGroup("effandfake", [
     Plot("effic_vs_Pt2", xtitle="Sum p_{T}^{2}    ", ytitle="Efficiency vs. sum p_{T}^{2}", xlog=True, ymax=_maxEff),
     Plot("fakerate_vs_Pt2", xtitle="Sum p_{T}^{2}    ", ytitle="Fake rate vs. sum p_{T}^{2}", xlog=True, ymax=_maxFake),
 ])
+_common = {"drawStyle": "HIST"}
+_genpos = PlotGroup("genpos", [
+    Plot("GenAllV_X", xtitle="Gen AllV pos x", ytitle="N", **_common),
+    Plot("GenPV_X", xtitle="Gen PV pos x", ytitle="N", **_common),
+    Plot("GenAllV_Y", xtitle="Gen AllV pos y", ytitle="N", **_common),
+    Plot("GenPV_Y", xtitle="Gen PV pos y", ytitle="N", **_common),
+    Plot("GenAllV_Z", xtitle="Gen AllV pos z", ytitle="N", **_common),
+    Plot("GenPV_Z", xtitle="Gen PV pos z", ytitle="N", **_common),
+])
 
 plotter = Plotter([
     "DQMData/Run 1/Vertexing/Run summary/PrimaryVertex",
@@ -33,6 +42,14 @@ plotter = Plotter([
     _recovsgen,
     _effandfake,
 ])
+plotterGen = Plotter([
+    "DQMData/Run 1/Vertexing/Run summary/PrimaryVertex",
+    "DQMData/Vertexing/PrimaryVertex",
+    "DQMData/Run 1/Vertexing/Run summary/PrimaryVertexV",
+    "DQMData/Vertexing/PrimaryVertexV",
+],[
+     _genpos
+])
 
 class VertexValidation(validation.Validation):
     def _init__(self, *args, **kwargs):
@@ -42,4 +59,6 @@ class VertexValidation(validation.Validation):
         return algo
 
     def _getSelectionName(self, quality, algo):
+        if algo is None:
+            return ""
         return "_"+algo
