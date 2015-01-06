c3629596e06a54e7a18e2c9d367620fda5b26d4e
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-06 23:02:16 +0100

    Used to write v1 which includes QGL_AK4PF, QGL_AK4PFchs, QGL_AK4PF_antib, QGL_AK4PFchs_antib

--- JetMETCorrections/Modules/test/QGLikelihoodDBWriter_cfg.py
+++ JetMETCorrections/Modules/test/QGLikelihoodDBWriter_cfg.py
@@ -19,15 +19,25 @@ process.source = cms.Source('EmptySource')
 process.PoolDBOutputService = cms.Service('PoolDBOutputService',
    process.CondDBCommon,
    toPut = cms.VPSet(
-#      cms.PSet(
-#         record = cms.string('QGL_AK4PF'),
-#         tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PF'),
-#         label  = cms.string('QGL_AK4PF')
-#      ),
+      cms.PSet(
+         record = cms.string('QGL_AK4PF'),
+         tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PF'),
+         label  = cms.string('QGL_AK4PF')
+      ),
       cms.PSet(
          record = cms.string('QGL_AK4PFchs'),
          tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PFchs'),
          label  = cms.string('QGL_AK4PFchs')
+      ),
+      cms.PSet(
+         record = cms.string('QGL_AK4PF_antib'),
+         tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PF_antib'),
+         label  = cms.string('QGL_AK4PF_antib')
+      ),
+      cms.PSet(
+         record = cms.string('QGL_AK4PFchs_antib'),
+         tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PFchs_antib'),
+         label  = cms.string('QGL_AK4PFchs_antib')
       ),
 # ONLY AFTER FIRST DATA
 #     cms.PSet(
@@ -43,15 +53,24 @@ process.PoolDBOutputService = cms.Service('PoolDBOutputService',
    )
 )
 
-srcDirectory = 'temp/'
-#process.dbWriterAK4PF = cms.EDAnalyzer('QGLikelihoodDBWriter',
-#   src    = cms.string(srcDirectory + 'pdfQG_AK4_13TeV.root'),
-#   payload= cms.string('QGL_AK4PF')
-#) 
+# The ROOT files of v1 are at /afs/cern.ch/user/t/tomc/public/qgTagger/QGLikelihoodDBFiles/QGL_v1
+srcDirectory = 'JetMETCorrections/Modules/test/' + qgDatabaseVersion + '/'
+process.dbWriterAK4PF = cms.EDAnalyzer('QGLikelihoodDBWriter',
+   src    = cms.string(srcDirectory + 'pdfQG_AK4_13TeV_' + qgDatabaseVersion + '.root'),
+   payload= cms.string('QGL_AK4PF')
+)
 process.dbWriterAK4PFchs = cms.EDAnalyzer('QGLikelihoodDBWriter',
-   src    = cms.string(srcDirectory + 'pdfQG_AK4chs_13TeV_smallEtaBinning_newTest.root'),
+   src    = cms.string(srcDirectory + 'pdfQG_AK4chs_13TeV_' + qgDatabaseVersion + '.root'),
    payload= cms.string('QGL_AK4PFchs')
-) 
+)
+process.dbWriterAK4PF_antib = cms.EDAnalyzer('QGLikelihoodDBWriter',
+   src    = cms.string(srcDirectory + 'pdfQG_AK4_antib_13TeV_' + qgDatabaseVersion + '.root'),
+   payload= cms.string('QGL_AK4PF_antib')
+)
+process.dbWriterAK4PFchs_antib = cms.EDAnalyzer('QGLikelihoodDBWriter',
+   src    = cms.string(srcDirectory + 'pdfQG_AK4chs_antib_13TeV_' + qgDatabaseVersion + '.root'),
+   payload= cms.string('QGL_AK4PFchs_antib')
+)
 # ONLY AFTER FIRST DATA:
 #process.dbWriterSystPythia = cms.EDAnalyzer('QGLikelihoodSystematicsDBWriter',
 #   src    = cms.string(srcDirectory + 'SystDatabase.txt'),
@@ -63,4 +82,4 @@ process.dbWriterAK4PFchs = cms.EDAnalyzer('QGLikelihoodDBWriter',
 #)
  
 
-process.p = cms.Path(process.dbWriterAK4PFchs)
+process.p = cms.Path(process.dbWriterAK4PF * process.dbWriterAK4PFchs * process.dbWriterAK4PF_antib * process.dbWriterAK4PFchs_antib)
