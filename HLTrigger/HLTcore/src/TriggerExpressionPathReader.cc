64112ddeeb2e25b54c121ed84a0548c536c246e8
Andrea Bocci <fwyzard@gmail.com>
2015-01-07 02:35:44 +0100

    TriggerResultFilters: accept arbitrary Path names
      - accept arbitrary Path names
      - rename the internal classes for consistency
      - update test files for the new functionality
      - prevent segfault if an L1 trigger is used in the expression without
        passing the L1 results collection

--- HLTrigger/HLTcore/src/TriggerExpressionPathReader.cc
+++ HLTrigger/HLTcore/src/TriggerExpressionPathReader.cc
@@ -6,13 +6,13 @@
 #include "FWCore/Utilities/interface/RegexMatch.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
-#include "HLTrigger/HLTcore/interface/TriggerExpressionHLTReader.h"
+#include "HLTrigger/HLTcore/interface/TriggerExpressionPathReader.h"
 #include "HLTrigger/HLTcore/interface/TriggerExpressionData.h"
 
 namespace triggerExpression {
 
 // define the result of the module from the HLT reults
-bool HLTReader::operator()(const Data & data) const {
+bool PathReader::operator()(const Data & data) const {
   if (not data.hasHLT())
     return false;
 
@@ -20,11 +20,11 @@ bool HLTReader::operator()(const Data & data) const {
   BOOST_FOREACH(const value_type & trigger, m_triggers)
     if (data.hltResults().accept(trigger.second))
       return true;
-  
+
   return false;
 }
 
-void HLTReader::dump(std::ostream & out) const {
+void PathReader::dump(std::ostream & out) const {
   if (m_triggers.size() == 0) {
     out << "FALSE";
   } else if (m_triggers.size() == 1) {
@@ -38,7 +38,7 @@ void HLTReader::dump(std::ostream & out) const {
 }
 
 // (re)initialize the module
-void HLTReader::init(const Data & data) {
+void PathReader::init(const Data & data) {
   // clear the previous configuration
   m_triggers.clear();
 
