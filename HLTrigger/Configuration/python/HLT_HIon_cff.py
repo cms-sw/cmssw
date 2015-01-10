f9eb65ec1c789009fa7f2c6316245530b8711e00
Andrea <perrotta@cern.ch>
2015-01-10 14:59:22 +0100

    First HLT menu compatible with L1Menu_Collisions2015_25ns_v2

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_7_3_0/HIon/V17 (CMSSW_7_3_0_HLT1)
+# /dev/CMSSW_7_3_0/HIon/V23 (CMSSW_7_3_0_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/HIon/V17')
+  tableName = cms.string('/dev/CMSSW_7_3_0/HIon/V23')
 )
 
 HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_7_3_0/PIon/V17 (CMSSW_7_3_0_HLT1)
+# /dev/CMSSW_7_3_0/PIon/V23 (CMSSW_7_3_0_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V17')
+  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V23')
 )
 
 HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
