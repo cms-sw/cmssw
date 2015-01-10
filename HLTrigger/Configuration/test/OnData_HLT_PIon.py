1eab9ca51f598cb42702d43a5c2bd6c1aafb1bdf
Andrea <perrotta@cern.ch>
2015-01-10 13:23:45 +0100

    Move the HLT menu to a version with seeds compliant with GlobalTriggerMenu_L1Menu_Collisions2015_25ns_v2

--- HLTrigger/Configuration/test/OnData_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnData_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_3_0/PIon/V17 (CMSSW_7_3_0_HLT1)
+# /dev/CMSSW_7_3_0/PIon/V23 (CMSSW_7_3_0_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V17')
+  tableName = cms.string('/dev/CMSSW_7_3_0/PIon/V23')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
