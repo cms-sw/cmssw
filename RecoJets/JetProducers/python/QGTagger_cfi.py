1c999149f4313057f0792e8d8ec5716eceef0be2
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-14 16:14:24 +0100

    Add default arguments for srcJets and jetsLabel, include QGLikelihoodRcd's for AK4PF_antib and AK4PFchs_antib

--- RecoJets/JetProducers/python/QGTagger_cfi.py
+++ RecoJets/JetProducers/python/QGTagger_cfi.py
@@ -6,29 +6,21 @@ qgDatabaseVersion = 'v1'
 from CondCore.DBCommon.CondDBSetup_cfi import *
 QGPoolDBESSource = cms.ESSource("PoolDBESSource",
       CondDBSetup,
-      toGet = cms.VPSet(
-        cms.PSet(
-            record = cms.string('QGLikelihoodRcd'),
-            tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PFchs'),
-            label  = cms.untracked.string('QGL_AK4PFchs')
-        ),
-        cms.PSet(
-            record = cms.string('QGLikelihoodRcd'),
-            tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PF'),
-            label  = cms.untracked.string('QGL_AK4PF')
-        ),
-      ),
+      toGet = cms.VPSet(),
       connect = cms.string('frontier://FrontierProd/CMS_COND_PAT_000'),
 )
 
+for type in ['AK4PF','AK4PFchs','AK4PF_antib','AK4PFchs_antib']:
+  QGPoolDBESSource.toGet.extend(cms.VPSet(cms.PSet(
+    record = cms.string('QGLikelihoodRcd'),
+    tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_'+type),
+    label  = cms.untracked.string('QGL_'+type)
+  )))
+
 QGTagger = cms.EDProducer('QGTagger',
+  srcJets		= cms.InputTag('ak4PFJetsCHS'),
+  jetsLabel		= cms.string('QGL_AK4PFchs'),
   srcRho 		= cms.InputTag('fixedGridRhoFastjetAll'),		
   srcVertexCollection	= cms.InputTag('offlinePrimaryVerticesWithBS'),
-  useQualityCuts	= cms.bool(False)
-)
-
-QGTaggerMiniAOD = cms.EDProducer('QGTagger',
-  srcRho 		= cms.InputTag('fixedGridRhoFastjetAll'),		
-  srcVertexCollection	= cms.InputTag(''),
   useQualityCuts	= cms.bool(False)
 )
