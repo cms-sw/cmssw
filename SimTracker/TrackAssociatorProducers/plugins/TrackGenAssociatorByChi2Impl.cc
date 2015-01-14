417f6e0041ff31b6a386455859c8c7dd4e9c618b
Christopher Jones <chrisdjones15@gmail.com>
2015-01-14 17:25:25 -0600

    Do not call edm::RefVector<>::product in Associators
    The call to edm::RefVector<>::product is being deprecated. Instead
    of getting the full container, we now loop over the RefVector and
    fill a new container. This (argueably) better matches the API of
    the function call anyway.
    This may lead to differences is results because we might be looping
    over a restricted set of entries. If this is a problem then the calling
    module should pass in the full container and not a RefVector.

--- SimTracker/TrackAssociatorProducers/plugins/TrackGenAssociatorByChi2Impl.cc
+++ SimTracker/TrackAssociatorProducers/plugins/TrackGenAssociatorByChi2Impl.cc
@@ -25,7 +25,10 @@ RecoToGenCollection TrackGenAssociatorByChi2Impl::associateRecoToGen(const edm::
   RecoToGenCollection  outputCollection;
 
   GenParticleCollection tPC;
-  if (tPCH.size()!=0)  tPC = *tPCH.product();
+  tPC.reserve(tPCH.size());
+  for(auto const& ref: tPCH) {
+    tPC.push_back(*ref);
+  }
 
   int tindex=0;
   for (RefToBaseVector<reco::Track>::const_iterator rt=tC.begin(); rt!=tC.end(); rt++, tindex++){
@@ -78,7 +81,10 @@ GenToRecoCollection TrackGenAssociatorByChi2Impl::associateGenToReco(const edm::
   GenToRecoCollection  outputCollection;
 
   GenParticleCollection tPC;
-  if (tPCH.size()!=0)  tPC = *tPCH.product();
+  tPC.reserve(tPCH.size());
+  for(auto const& ref: tPCH) {
+    tPC.push_back(*ref);
+  }
 
   int tpindex =0;
   for (GenParticleCollection::const_iterator tp=tPC.begin(); tp!=tPC.end(); tp++, ++tpindex){
