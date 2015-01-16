9a6ad4e8574d44c5907dc3cad9bc907bbf24b7b4
W. David Dagenhart <wdd@fnal.gov>
2015-01-16 15:56:20 -0600

    Avoid some unnecessary memory allocations
    In RefVectorIterator::operator->() avoid
    some unnecessary calls to new that allocate
    memory by returning by value a proxy to
    a Ref instead of an auto_ptr to a Ref.

--- DataFormats/Common/test/RefVector_t.cpp
+++ DataFormats/Common/test/RefVector_t.cpp
@@ -1,5 +1,4 @@
-#include <map>
-#include <string>
+#include <vector>
 
 #include "Utilities/Testing/interface/CppUnit_testdriver.icpp"
 #include "cppunit/extensions/HelperMacros.h"
@@ -10,7 +9,7 @@
 class TestRefVector: public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(TestRefVector);
-  CPPUNIT_TEST(map_string_to_double);
+  CPPUNIT_TEST(testIteration);
   CPPUNIT_TEST_SUITE_END();
 
  public:
@@ -19,30 +18,47 @@ class TestRefVector: public CppUnit::TestFixture
   void setUp() {}
   void tearDown() {}
 
-  void map_string_to_double();
+  void testIteration();
 
  private:
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestRefVector);
 
-void TestRefVector::map_string_to_double()
+void TestRefVector::testIteration()
 {
-  typedef std::map<std::string, double> product_t;
-  typedef edm::Ref<product_t>           ref_t;
-  typedef edm::RefVector<product_t>     refvec_t;
+  typedef std::vector<double> product_t;
+  typedef edm::Ref<product_t> ref_t;
+  typedef edm::RefVector<product_t> refvec_t;
   
   product_t  product;
-  product["one"]  = 1.0;
-  product["half"] = 0.5;
-  product["two"]  = 2.0;
-  CPPUNIT_ASSERT(product.size() == 3);
-
-  ref_t    ref;
+  product.push_back(1.0);
+  product.push_back(0.5);
+  product.push_back(2.0);
 
   refvec_t  refvec;
   CPPUNIT_ASSERT(refvec.size() == 0);
   CPPUNIT_ASSERT(refvec.empty());
 
+  ref_t    ref0(edm::ProductID(1, 1), &product[0], 0, &product);
+  refvec.push_back(ref0);
 
+  ref_t    ref1(edm::ProductID(1, 1), &product[1], 1, &product);
+  refvec.push_back(ref1);
+
+  ref_t    ref2(edm::ProductID(1, 1), &product[2], 2, &product);
+  refvec.push_back(ref2);
+
+  auto iter = refvec.begin();
+
+  CPPUNIT_ASSERT(iter->id() == edm::ProductID(1,1) && iter->key() == 0 && *(iter->get()) == 1.0);
+  ++iter;
+
+  CPPUNIT_ASSERT(iter->id() == edm::ProductID(1,1) && iter->key() == 1 && *(iter->get()) == 0.5);
+  ++iter;
+
+  CPPUNIT_ASSERT(iter->id() == edm::ProductID(1,1) && iter->key() == 2 && *(iter->get()) == 2.0);
+  ++iter;
+
+  CPPUNIT_ASSERT(iter == refvec.end());
 }
