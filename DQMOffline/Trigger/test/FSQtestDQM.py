a8453437732d30ce8cbe1f9a3154656956505f13
Tomasz Fruboes <fruboes@cern.ch>
2015-01-19 14:38:02 +0100

    Use DQMOffline_Trigger_cff for imports

--- DQMOffline/Trigger/test/FSQtestDQM.py
+++ DQMOffline/Trigger/test/FSQtestDQM.py
@@ -19,7 +19,8 @@ process.dqmEnv.subSystemFolder = 'HLT'
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
-process.GlobalTag.globaltag = 'MCRUN2_72_V1::All'
+#process.GlobalTag.globaltag = 'MCRUN2_72_V1::All'
+process.GlobalTag.globaltag = 'PHYS14_25_V2::All'
 
 #f='/nfs/dust/cms/user/fruboest/2014.11.HLTJec721p1/CMSSW_7_2_1_patch1/src/outputFULL.root'
 #f='/nfs/dust/cms/user/fruboest/2014.11.HLTJec721p1/CMSSW_7_2_1_patch1/src/outputFULL_big.root'
@@ -28,6 +29,7 @@ process.GlobalTag.globaltag = 'MCRUN2_72_V1::All'
 #f='/nfs/dust/cms/user/fruboest/2014.11.HLTJec721p1/CMSSW_7_2_1_patch1/src/outputFULL_big.root'
 f='./fromMax/events.root'
 #f='fromTomasz/events_hlt_singletrack_v3.root'
+f='/afs/cern.ch/work/m/mazarkin/public/forTomasz/events.root'
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         'file:'+f
@@ -39,6 +41,7 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-200)
 )
 
+'''
 process.load("DQMOffline.Trigger.FSQHLTOfflineSource_cfi")
 process.load("DQMOffline.Trigger.FSQHLTOfflineClient_cfi")
 
@@ -46,13 +49,22 @@ process.load('DQMServices.Components.DQMFileSaver_cfi')
 process.dqmSaver.workflow = "/HLT/FSQ/All"
 
 
-#process.p = cms.Path(process.fsqHLTOfflineSource*process.fsqClient *process.dqmEnv*process.dqmSaver)
-process.p = cms.Path(process.fsqHLTOfflineSourceSequence*process.fsqClient *process.dqmEnv*process.dqmSaver)
+process.p = cms.Path(process.fsqHLTOfflineSource*process.fsqClient *process.dqmEnv*process.dqmSaver)
+#process.p = cms.Path(process.fsqHLTOfflineSourceSequence*process.fsqClient *process.dqmEnv*process.dqmSaver)
 #process.MessageLogger.threshold = cms.untracked.string( "INFO" )
 #process.MessageLogger.categories.append("FSQDiJetAve")
+'''
+process.load("DQMOffline.Trigger.DQMOffline_Trigger_cff")
+process.load("DQMOffline.Trigger.FSQHLTOfflineClient_cfi")
+process.load('DQMServices.Components.DQMFileSaver_cfi')
+process.dqmSaver.workflow = "/HLT/FSQ/All"
+process.p = cms.Path(process.fsqHLTOfflineSourceSequence*process.fsqClient *process.dqmEnv*process.dqmSaver)
+#'''
+
+
+
 
 # TODO
 # - apply jet callibration to offline jets
 # - Fix efficiency histos - add a check, that both reference and tested path simultaneusly
 #    went beyond the hlt prescale module
-
