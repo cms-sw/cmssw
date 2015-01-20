563928bf2b46ee73fbd500aaa9631fe3b7c91411
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2015-01-20 18:04:37 +0100

    fixing the calo towers candidate

--- DQMOffline/JetMET/python/jetAnalyzer_cff.py
+++ DQMOffline/JetMET/python/jetAnalyzer_cff.py
@@ -10,6 +10,25 @@ jetDQMAnalyzerSequence = cms.Sequence(jetDQMAnalyzerAk4CaloUncleaned*jetDQMAnaly
 jetDQMAnalyzerSequenceCosmics = cms.Sequence(jetDQMAnalyzerAk4CaloUncleaned)
 
 jetDQMAnalyzerSequenceHI = cms.Sequence(jetDQMAnalyzerIC5CaloHIUncleaned
+                                        * jetDQMAnalyzerAkPU3Calo
+                                        * jetDQMAnalyzerAkPU4Calo
+                                        * jetDQMAnalyzerAkPU5Calo
+                                        
+                                        * jetDQMAnalyzerAkPU3PF
+                                        * jetDQMAnalyzerAkPU4PF
+                                        * jetDQMAnalyzerAkPU5PF
+
+                                        * jetDQMAnalyzerAkVs2Calo	   
+                                        * jetDQMAnalyzerAkVs3Calo	   
+                                        * jetDQMAnalyzerAkVs4Calo	   
+                                        * jetDQMAnalyzerAkVs5Calo	   
+                                        #* jetDQMAnalyzerAkVs6Calo
+                                        #* jetDQMAnalyzerAkVs7Calo
+                                        
+                                        #* jetDQMAnalyzerAkVs2PF
                                         * jetDQMAnalyzerAkVs3PF
-                                        * jetDQMAnalyzerAk4PFCleaned
+                                        * jetDQMAnalyzerAkVs4PF	   
+                                        * jetDQMAnalyzerAkVs5PF
+                                        #* jetDQMAnalyzerAkVs6PF	   
+                                        #* jetDQMAnalyzerAkVs7PF
 )
