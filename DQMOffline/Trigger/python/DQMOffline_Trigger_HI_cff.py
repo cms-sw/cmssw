3423e020a8e484ab8a29654fe9b99742b4156b22
Tomasz Fruboes <fruboes@cern.ch>
2015-01-20 13:31:17 +0100

    Proper fix for HI/FSQ problem

--- DQMOffline/Trigger/python/DQMOffline_Trigger_HI_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DQMOffline.Trigger.DQMOffline_Trigger_cff import *
-
-# fix FSQ DQM - required products to calculate JEC not avaliable in HI workflow
-#   this turns off JEC calculation
-fsqHLTOfflineSourceSequence = cms.Sequence(fsqHLTOfflineSource)
-from DQMOffline.Trigger.FSQHLTOfflineSource_cfi import getFSQHI
-fsqHLTOfflineSource.todo = getFSQHI()
