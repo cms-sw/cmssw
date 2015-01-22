bc8b953f1c9a5b8e5e275d2120b1b2f15ff350ff
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-01-22 17:27:49 +0100

    test on reco::Candidate successful

--- CommonTools/Utils/test/ExprEvalStubs/CutOnCandidate.h
+++ CommonTools/Utils/test/ExprEvalStubs/CutOnCandidate.h
@@ -5,9 +5,16 @@
 
 
 #include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 namespace eetest {
   struct CutOnCandidate {
-    virtual bool eval(reco::Candidate const&)=0;
+    virtual bool eval(reco::Candidate const&) const = 0;
   };
+
+  struct ValueOnCandidate {
+    virtual double eval(reco::Candidate const&) const =0;
+  };
+
 }
--- CommonTools/Utils/test/testExpressionEvaluator.cc
+++ CommonTools/Utils/test/testExpressionEvaluator.cc
@@ -8,6 +8,8 @@
 #include "DataFormats/PatCandidates/interface/Muon.h"
 
 
+#include "CommonTools/Utils/test/ExprEvalStubs/CutOnCandidate.h"
+
 class testExpressionEvaluator : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(testExpressionEvaluator);
   CPPUNIT_TEST(checkAll);
@@ -15,16 +17,60 @@ class testExpressionEvaluator : public CppUnit::TestFixture {
 
 public:
   void setUp() {
-    system("mkdir -p $CMSSW_BASE/src/VITest/ExprEval/src; cp CommonTools/Utils/test/ExprEvalStubs/*.h $CMSSW_BASE/src/VITest/ExprEval/src/.; pushd $CMSSW_BASE; scram b -j 8; popd");
+   std::cerr << "setting up VITest/ExprEval" << std::endl;
+   system("mkdir -p $CMSSW_BASE/src/VITest/ExprEval/src; cp CommonTools/Utils/test/ExprEvalStubs/*.h $CMSSW_BASE/src/VITest/ExprEval/src/.");
+   system("cp CommonTools/Utils/test/ExprEvalStubs/BuildFile.xml $CMSSW_BASE/src/VITest/ExprEval/.; pushd $CMSSW_BASE; scram b -j 8; popd");
   }
   void tearDown() {system("rm -rf $CMSSW_BASE/src/VITest");}
   void checkAll(); 
 };
 
+CPPUNIT_TEST_SUITE_REGISTRATION( testExpressionEvaluator );
+
+#include <iostream>
+namespace {
+  void checkCandidate(reco::Candidate const & cand, const std::string & expression, double x) {
+    std::cerr << "testing " << expression << std::endl;
+    try {
+     std::string sexpr = "double eval(reco::Candidate const& cand) const override { return ";
+     sexpr += expression + ";}";
+     reco::ExpressionEvaluator parser("VITest/ExprEval","eetest::ValueOnCandidate",sexpr.c_str());
+     auto expr = parser.expr<eetest::ValueOnCandidate>();
+     CPPUNIT_ASSERT(std::abs(expr->eval(cand) - x) < 1.e-6);
+    } catch(cms::Exception const & e) {
+      std::cerr << e.what()  << std::endl;
+      CPPUNIT_ASSERT("ExpressionEvaluator threw"==0);
+    }
+  }
+}
 
 void testExpressionEvaluator::checkAll() {
 
+  reco::CompositeCandidate cand;
 
+  reco::Candidate::LorentzVector p1(1, 2, 3, 4);
+  reco::Candidate::LorentzVector p2(1.1, -2.5, 4.3, 13.7);
+  reco::LeafCandidate c1(+1, p1);
+  reco::LeafCandidate c2(-1, p2);
+  cand.addDaughter(c1);
+  cand.addDaughter(c2);
+  CPPUNIT_ASSERT(cand.numberOfDaughters()==2);
+  CPPUNIT_ASSERT(cand.daughter(0)!=0);
+  CPPUNIT_ASSERT(cand.daughter(1)!=0);
+  {
+    checkCandidate(cand,"cand.numberOfDaughters()", cand.numberOfDaughters());
+    checkCandidate(cand,"cand.daughter(0)->isStandAloneMuon()", cand.daughter(0)->isStandAloneMuon());  
+    checkCandidate(cand,"cand.daughter(1)->isStandAloneMuon()", cand.daughter(1)->isStandAloneMuon());  
+    checkCandidate(cand,"cand.daughter(0)->pt()", cand.daughter(0)->pt());
+    checkCandidate(cand,"cand.daughter(1)->pt()", cand.daughter(1)->pt());
+    checkCandidate(cand,"std::min(cand.daughter(0)->pt(), cand.daughter(1)->pt())", std::min(cand.daughter(0)->pt(), cand.daughter(1)->pt()));
+    checkCandidate(cand,"std::max(cand.daughter(0)->pt(), cand.daughter(1)->pt())", std::max(cand.daughter(0)->pt(), cand.daughter(1)->pt()));
+    checkCandidate(cand,"reco::deltaPhi(cand.daughter(0)->phi(), cand.daughter(1)->phi())", reco::deltaPhi(cand.daughter(0)->phi(), cand.daughter(1)->phi()));
+    // check also opposite order, to see that the sign is correct
+    checkCandidate(cand,"reco::deltaPhi(cand.daughter(1)->phi(), cand.daughter(0)->phi())", reco::deltaPhi(cand.daughter(1)->phi(), cand.daughter(0)->phi())); 
+    checkCandidate(cand,"reco::deltaR(*cand.daughter(0), *cand.daughter(1))", reco::deltaR(*cand.daughter(0), *cand.daughter(1)));
+
+  }
 
 
 }
