0d1281f0136a32e2f17aeb42f2635186340b265c
Sunanda <sunanda.banerjee@cern.ch>
2015-01-23 12:28:30 +0100

    Add Hcal NZS analyzers for data and for noise and MC studies

---
+++ Calibration/HcalCalibAlgos/python/recAnalyzerMinbias_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+RecAnalyzerMinbias = cms.EDAnalyzer("RecAnalyzerMinbias",
+                                    HistOutFile = cms.untracked.string('analysis_minbias.root'),
+                                    hbheInputMB = cms.InputTag("hbherecoMB"),
+                                    hfInputMB = cms.InputTag("hfrecoMB"),
+                                    Recalib = cms.bool(False)
+                                    )
