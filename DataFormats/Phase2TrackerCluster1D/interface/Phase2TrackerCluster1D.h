e27afab12721be56cf9462550116964defd3fa84
Thomas Lenzi <tlenzi@ulb.ac.be>
2015-01-25 12:20:03 +0100

    Use only edmNew from feedback from C.D. and other PR

--- DataFormats/Phase2TrackerCluster1D/interface/Phase2TrackerCluster1D.h
+++ DataFormats/Phase2TrackerCluster1D/interface/Phase2TrackerCluster1D.h
@@ -5,8 +5,6 @@
 
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/Common/interface/DetSetVectorNew.h"
-#include "DataFormats/Common/interface/Ref.h"
-#include "DataFormats/Common/interface/DetSetRefVector.h"
 
 #include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerDigi.h"
 
@@ -14,36 +12,34 @@ class Phase2TrackerCluster1D {
 
 public:
 
-    Phase2TrackerCluster1D() : size_(0) { }
-    Phase2TrackerCluster1D(unsigned int row, unsigned int col, unsigned int size) : firstDigi_(row, col), size_(size) { }
-    Phase2TrackerCluster1D(const Phase2TrackerDigi& firstDigi, unsigned int size) : firstDigi_(firstDigi), size_(size) { }
+    Phase2TrackerCluster1D() : data_(0) { }
+    Phase2TrackerCluster1D(unsigned int row, unsigned int col, unsigned int size) : firstDigi_(row, col), data_((size & 0x7fff)) { }
+    Phase2TrackerCluster1D(unsigned int row, unsigned int col, unsigned int size, unsigned int threshold) : firstDigi_(row, col), data_(((threshold & 0x1) << 15) | (size & 0x7fff)) { }
+    Phase2TrackerCluster1D(const Phase2TrackerDigi& firstDigi, unsigned int size) : firstDigi_(firstDigi), data_((size & 0x7fff)) { }
+    Phase2TrackerCluster1D(const Phase2TrackerDigi& firstDigi, unsigned int size, unsigned int threshold) : firstDigi_(firstDigi), data_(((threshold & 0x1) << 15) | (size & 0x7fff)) { }
 
     const Phase2TrackerDigi& firstDigi() const { return firstDigi_; }
     unsigned int firstStrip() const { return firstDigi_.strip(); }
     unsigned int firstRow() const { return firstDigi_.row(); }
     unsigned int edge() const { return firstDigi_.edge(); }
     unsigned int column() const { return firstDigi_.column(); }
-    uint16_t size() const { return size_; }
-    float center() const { return firstStrip() + size_ / 2.; }
+    uint16_t size() const { return (data_ & 0x7fff); }
+    uint16_t threshold() const { return ((data_ >> 15) & 0x1); }
+    float center() const { return firstStrip() + (data_ & 0x7fff) / 2.; }
     std::pair< float, float > barycenter() const { return std::make_pair(column(), center()); }
 
 private:
 
     Phase2TrackerDigi firstDigi_;
-    uint16_t size_;
+    uint16_t data_;
 
 };  
 
 inline bool operator< (const Phase2TrackerCluster1D& one, const Phase2TrackerCluster1D& other) {
-    return one.center() < other.center();
+    return one.firstStrip() < other.firstStrip();
 }
 
 typedef edm::DetSetVector< Phase2TrackerCluster1D > Phase2TrackerCluster1DCollection;
-typedef edm::Ref< Phase2TrackerCluster1DCollection, Phase2TrackerCluster1D > Phase2TrackerCluster1DRef;
-typedef edm::DetSetRefVector< Phase2TrackerCluster1D > Phase2TrackerCluster1DRefVector;
-typedef edm::RefProd< Phase2TrackerCluster1DCollection > Phase2TrackerCluster1DRefProd;
-
 typedef edmNew::DetSetVector< Phase2TrackerCluster1D > Phase2TrackerCluster1DCollectionNew;
-typedef edm::Ref< Phase2TrackerCluster1DCollectionNew, Phase2TrackerCluster1D > Phase2TrackerCluster1DRefNew;
 
 #endif 
