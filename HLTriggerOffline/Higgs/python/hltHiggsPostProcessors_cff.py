cb0a35ecb7743c8620bc3af2c8bd59d0dda03557
Silvio <silvio.donato@cern.ch>
2015-01-26 16:52:06 +0100

    ../python/hltHiggsPostProcessors_cff.py

--- HLTriggerOffline/Higgs/python/hltHiggsPostProcessors_cff.py
+++ HLTriggerOffline/Higgs/python/hltHiggsPostProcessors_cff.py
@@ -172,11 +172,34 @@ hltHiggsPostTTHbbej = hltHiggsPostProcessor.clone()
 hltHiggsPostTTHbbej.subDirs = ['HLT/Higgs/TTHbbej']
 hltHiggsPostTTHbbej.efficiencyProfile = efficiency_strings_TTHbbej
 
+#Specific plots for VBFHbb_2btag  
+#dEtaqq, mqq, dPhibb, CVS1, maxCSV_jets, maxCSV_E, MET, pt1, pt2, pt3, pt4
+NminOneCutNames = ("EffdEtaqq", "Effmqq", "EffdPhibb", "EffCSV1", "EffCSV2", "EffCSV3",  "EffmaxCSV", "", "", "TurnOn1", "TurnOn2", "TurnOn3", "TurnOn4")
+plot_types = []
+NminOneCuts = (_config.__getattribute__("VBFHbb_2btag")).__getattribute__("NminOneCuts")
+if NminOneCuts: 
+    for iCut in range(0,len(NminOneCuts)):
+        if( NminOneCuts[iCut] and NminOneCutNames[iCut] ):
+            if( NminOneCutNames[iCut] == "EffmaxCSV" ):
+                plot_types.pop()
+            plot_types.append(NminOneCutNames[iCut])
+
+efficiency_strings = []
+for type in plot_types:
+    for obj in ["Jet"]:
+        for trig in triggers:
+            efficiency_strings.append(efficiency_string(obj,type,trig))
+
+efficiency_strings = get_reco_strings(efficiency_strings)
+
+hltHiggsPostVBFHbb_2btag = hltHiggsPostProcessor.clone()
+hltHiggsPostVBFHbb_2btag.subDirs = ['HLT/Higgs/VBFHbb_2btag']
+hltHiggsPostVBFHbb_2btag.efficiencyProfile = efficiency_strings
 
 #Specific plots for VBFHbb_1btag  
 #dEtaqq, mqq, dPhibb, CVS1, maxCSV_jets, maxCSV_E, MET, pt1, pt2, pt3, pt4
 NminOneCutNames = ("EffdEtaqq", "Effmqq", "EffdPhibb", "EffCSV1", "EffCSV2", "EffCSV3",  "EffmaxCSV", "", "", "TurnOn1", "TurnOn2", "TurnOn3", "TurnOn4")
-plot_types = ["EffEta", "EffPhi"]
+plot_types = []
 NminOneCuts = (_config.__getattribute__("VBFHbb_1btag")).__getattribute__("NminOneCuts")
 if NminOneCuts: 
     for iCut in range(0,len(NminOneCuts)):
@@ -185,41 +208,23 @@ if NminOneCuts:
                 plot_types.pop()
             plot_types.append(NminOneCutNames[iCut])
     
-efficiency_strings = []
-for type in plot_types:
-    for obj in ["Jet"]:
-        for trig in triggers:
-            efficiency_strings.append(efficiency_string(obj,type,trig))
-        
-efficiency_strings = get_reco_strings(efficiency_strings)
-    
 hltHiggsPostVBFHbb_1btag = hltHiggsPostProcessor.clone()
 hltHiggsPostVBFHbb_1btag.subDirs = ['HLT/Higgs/VBFHbb_1btag']
 hltHiggsPostVBFHbb_1btag.efficiencyProfile = efficiency_strings
 
-#Specific plots for VBFHbb_2btag  
+#Specific plots for VBFHbb_0btag  
 #dEtaqq, mqq, dPhibb, CVS1, maxCSV_jets, maxCSV_E, MET, pt1, pt2, pt3, pt4
 NminOneCutNames = ("EffdEtaqq", "Effmqq", "EffdPhibb", "EffCSV1", "EffCSV2", "EffCSV3",  "EffmaxCSV", "", "", "TurnOn1", "TurnOn2", "TurnOn3", "TurnOn4")
-plot_types = ["EffEta", "EffPhi"]
-NminOneCuts = (_config.__getattribute__("VBFHbb_2btag")).__getattribute__("NminOneCuts")
+plot_types = []
+NminOneCuts = (_config.__getattribute__("VBFHbb_0btag")).__getattribute__("NminOneCuts")
 if NminOneCuts: 
     for iCut in range(0,len(NminOneCuts)):
         if( NminOneCuts[iCut] and NminOneCutNames[iCut] ):
-            if( NminOneCutNames[iCut] == "EffmaxCSV" ):
-                plot_types.pop()
             plot_types.append(NminOneCutNames[iCut])
-    
-efficiency_strings = []
-for type in plot_types:
-    for obj in ["Jet"]:
-        for trig in triggers:
-            efficiency_strings.append(efficiency_string(obj,type,trig))
-        
-efficiency_strings = get_reco_strings(efficiency_strings)
-    
-hltHiggsPostVBFHbb_2btag = hltHiggsPostProcessor.clone()
-hltHiggsPostVBFHbb_2btag.subDirs = ['HLT/Higgs/VBFHbb_2btag']
-hltHiggsPostVBFHbb_2btag.efficiencyProfile = efficiency_strings
+
+hltHiggsPostVBFHbb_0btag = hltHiggsPostProcessor.clone()
+hltHiggsPostVBFHbb_0btag.subDirs = ['HLT/Higgs/VBFHbb_0btag']
+hltHiggsPostVBFHbb_0btag.efficiencyProfile = efficiency_strings
 
 
 
@@ -285,6 +290,7 @@ hltHiggsPostProcessors = cms.Sequence(
         hltHiggsPostHtaunu+
         hltHiggsPostH2tau+
         hltHiggsPostTTHbbej+
+        hltHiggsPostVBFHbb_0btag+
         hltHiggsPostVBFHbb_1btag+
         hltHiggsPostVBFHbb_2btag+
         hltHiggsPostZnnHbb+
