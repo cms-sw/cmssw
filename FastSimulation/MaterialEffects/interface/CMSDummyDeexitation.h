0181a823ce2d0acacc6f951dd4972ba27f3004f9
Vladimir <Vladimir.Ivantchenko@cern.ch>
2015-01-27 12:09:23 +0100

    added alternative nuclear interation using G4 FTF model

---
+++ FastSimulation/MaterialEffects/interface/CMSDummyDeexitation.h
@@ -0,0 +1,31 @@
+#ifndef FastSimulation_MaterialEffects_CMSDummyDeexcitation_H
+#define FastSimulation_MaterialEffects_CMSDummyDeexcitation_H
+
+/** 
+ * This class is needed as a dummy interface to Geant4  
+ * nuclear de-excitation module; no secondary produced
+ *
+ * \author Vladimir Ivanchenko
+ * $Date: 20-Jan-2015
+ */ 
+
+#include "G4VPreCompoundModel.hh"
+#include "G4ReactionProductVector.hh"
+
+class G4Fragment;
+
+class CMSDummyDeexcitation : public G4VPreCompoundModel
+{ 
+public:
+
+  CMSDummyDeexcitation() {}; 
+
+  virtual ~CMSDummyDeexcitation() {};
+
+  //  virtual G4HadFinalState * ApplyYourself(const G4HadProjectile & thePrimary, 
+  //                                      G4Nucleus & theNucleus);
+
+  G4ReactionProductVector* DeExcite(G4Fragment&) { return new G4ReactionProductVector(); };
+
+};
+#endif
