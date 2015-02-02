a2856849f28605787b6a33bb8a8eea0d52390323
Sunanda <sunanda.banerjee@cern.ch>
2015-02-02 12:38:06 +0100

    To undo some problems

--- Calibration/HcalAlCaRecoProducers/src/SealModule.cc
+++ Calibration/HcalAlCaRecoProducers/src/SealModule.cc
@@ -3,10 +3,12 @@
 #include "Calibration/HcalAlCaRecoProducers/interface/AlCaEcalHcalReadoutsProducer.h"
 #include "Calibration/HcalAlCaRecoProducers/interface/AlCaDiJetsProducer.h"
 #include "Calibration/HcalAlCaRecoProducers/src/ProducerAnalyzer.h"
+#include "Calibration/HcalAlCaRecoProducers/interface/AlCaIsoTracksProducer.h"
 
 using cms::AlCaDiJetsProducer; 
 using cms::ProducerAnalyzer;
 
 DEFINE_FWK_MODULE(AlCaEcalHcalReadoutsProducer);
+DEFINE_FWK_MODULE(AlCaIsoTracksProducer);
 DEFINE_FWK_MODULE(AlCaDiJetsProducer);
 DEFINE_FWK_MODULE(ProducerAnalyzer);
