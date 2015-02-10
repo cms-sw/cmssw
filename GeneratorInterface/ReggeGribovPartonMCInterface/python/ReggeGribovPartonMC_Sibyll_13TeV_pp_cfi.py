b06e032e40646265d6f43f7caedcc4d1846094c8
Colin <colin.baus@kit.edu>
2015-02-10 13:44:37 +0100

    added 13 and 14 TeV configs for sibyll, epos and qgsjet

---
+++ GeneratorInterface/ReggeGribovPartonMCInterface/python/ReggeGribovPartonMC_Sibyll_13TeV_pp_cfi.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+from GeneratorInterface.ReggeGribovPartonMCInterface.ReggeGribovPartonMC_AdvancedParameters_cfi import *
+
+generator = cms.EDFilter("ReggeGribovPartonMCGeneratorFilter",
+                    ReggeGribovPartonMCAdvancedParameters,
+                    beammomentum = cms.double(6500),
+                    targetmomentum = cms.double(-6500),
+                    beamid = cms.int32(1),
+                    targetid = cms.int32(1),
+                    model = cms.int32(6)
+                    )
+
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: /local/reps/CMSSW/CMSSW/GeneratorInterface/ReggeGribovPartonMCInterface/python/ReggeGribovPartonMC_Sibyll_13TeV_pp_cfi.py,v $'),
+    annotation = cms.untracked.string('ReggeGribovMC generator')
+    )
