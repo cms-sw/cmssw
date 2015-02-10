4bb4db740af17d9f57d7c3676c41d890a98b52f4
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-02-10 10:37:12 +0100

    move CCC in pset, first step done

--- RecoLocalTracker/SiStripClusterizer/python/SiStripClusterChargeCut_cfi.py
+++ RecoLocalTracker/SiStripClusterizer/python/SiStripClusterChargeCut_cfi.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 SiStripClusterChargeCutNone = cms.PSet(
-    value     = cms.untracked.double(-1.0)
+    value     = cms.double(-1.0)
 )
   
 SiStripClusterChargeCutLoose = cms.PSet(
-    value     = cms.untracked.double(1724.0) 
+    value     = cms.double(1724.0) 
 )
 
 SiStripClusterChargeCutStrict = cms.PSet(
-    value     = cms.untracked.double(2069.0) 
+    value     = cms.double(2069.0) 
 )
 
