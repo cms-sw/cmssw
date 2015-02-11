9bbf01d21a9ca8beeadd06d92d6eba68bff1b412
Matthias <mkomm@cern.ch>
2015-02-11 16:37:43 +0100

    removed all cuts in the default seeding config

--- FastSimulation/Tracking/python/TrajectorySeedProducer_cfi.py
+++ FastSimulation/Tracking/python/TrajectorySeedProducer_cfi.py
@@ -4,26 +4,25 @@ trajectorySeedProducer = cms.EDProducer("TrajectorySeedProducer",
                                         
     simTrackSelection = cms.PSet(
          # The smallest pT (in GeV) to create a track candidate 
-         pTMin = cms.double(0.9),
+         pTMin = cms.double(-1),
          # skip SimTracks processed in previous iterations
          skipSimTrackIds = cms.VInputTag(),
-         maxZ0 = cms.double(30.0),
-         maxD0 = cms.double(1.0),
+         maxZ0 = cms.double(-1),
+         maxD0 = cms.double(-1),
     ),
     # minimum number of layer crossed (with hits on them) by the simtrack
-    minLayersCrossed = cms.uint32(5),
+    minLayersCrossed = cms.uint32(0),
     
     #if empty, BS compatibility is skipped
     beamSpot = cms.InputTag("offlineBeamSpot"),
     #if empty, PV compatibility is skipped
     primaryVertex = cms.InputTag(""),
     
-    # seed producer will use min(nSigmaZ*sigmaZ(BS or PV),originHalfLength)
-    nSigmaZ = cms.double(10000.0),
-    originHalfLength= cms.double(15.9),
+    nSigmaZ = cms.double(-1),
+    originHalfLength= cms.double(-1),
     
-    originRadius = cms.double(0.2),
-    originpTMin = cms.double(1.0),
+    originRadius = cms.double(-1),
+    originpTMin = cms.double(-1),
 
     # Inputs: tracker rechits, beam spot position.
     recHits = cms.InputTag("siTrackerGaussianSmearingRecHits","TrackerGSMatchedRecHits"),
