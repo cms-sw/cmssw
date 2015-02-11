528bd79bd367b2ae9b8d22aa6c3f76452e36afc5
Lukas <lukas.vanelderen@cern.ch>
2015-02-11 19:24:29 +0100

    FastSim: simplify setting of pufiles for gen-level pu mixing

--- FastSimulation/PileUpProducer/python/PileUpProducer_cff.py
+++ FastSimulation/PileUpProducer/python/PileUpProducer_cff.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+from FastSimulation.PileUpProducer.PileUpFiles_cff import puFileNames
+
 famosPileUp = cms.EDProducer(
     "PileUpProducer",
     # The conditions for pile-up event generation
@@ -10,17 +12,7 @@ famosPileUp = cms.EDProducer(
             inputFile = cms.untracked.string('PileUpInputFile.txt'),
             # Special files of minimum bias events (generated with 
             # cmsRun FastSimulation/PileUpProducer/test/producePileUpEvents_cfg.py)
-            fileNames = cms.untracked.vstring(
-                'MinBias14TeV_001.root', 
-                'MinBias14TeV_002.root', 
-                'MinBias14TeV_003.root', 
-                'MinBias14TeV_004.root', 
-                'MinBias14TeV_005.root', 
-                'MinBias14TeV_006.root', 
-                'MinBias14TeV_007.root', 
-                'MinBias14TeV_008.root', 
-                'MinBias14TeV_009.root', 
-                'MinBias14TeV_010.root'),
+            fileNames = puFileNames.fileNames,
             averageNumber = cms.double(0.0)
             )
         ),
@@ -38,14 +30,3 @@ famosPileUp = cms.EDProducer(
         )
     )
 
-#
-# Modify for running in Run 2
-#
-from Configuration.StandardSequences.Eras import eras
-def _modifyFamosPileUpForRun2( processObject ) :
-    from FastSimulation.PileUpProducer.PileUpFiles_cff import fileNames_13TeV
-    processObject.genMixPileUpFiles = cms.PSet(fileNames = fileNames_13TeV)
-    processObject.famosPileUp.PileUpSimulatorBlock.PileUpSimulator.fileNames = fileNames_13TeV
-
-# A unique name is required for this object, so I'll call it "modify<python filename>ForRun2_"
-modifyFastSimulationPileUpProducerPileUpProducerForRun2_ = eras.run2.makeProcessModifier( _modifyFamosPileUpForRun2 )
