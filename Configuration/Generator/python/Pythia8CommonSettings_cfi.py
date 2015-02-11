af438144bdfe92bc9a45bf27fee100ba72b931bb
cmsbuild <cmsbuild@cern.ch>
2015-02-11 13:32:42 +0100

    Merge pull request #7658 from bendavid/py8configupdates
    Disable setting of lhe scales by default and make common config fragments work with lhapdf6

--- Configuration/Generator/python/Pythia8CommonSettings_cfi.py
+++ Configuration/Generator/python/Pythia8CommonSettings_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 pythia8CommonSettingsBlock = cms.PSet(
     pythia8CommonSettings = cms.vstring(
+      'Tune:preferLHAPDF = 2',
       'Main:timesAllowErrors = 10000',
       'Check:epTolErr = 0.01',
       'Beams:setProductionScalesFromLHEF = off',
