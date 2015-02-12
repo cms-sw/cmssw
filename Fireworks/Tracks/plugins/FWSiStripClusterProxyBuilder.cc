c96871f885f8843cba30184ea393608ace5f5b5e
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2015-02-12 08:31:19 -0800

    Make getShape() public.

--- Fireworks/Tracks/plugins/FWSiStripClusterProxyBuilder.cc
+++ Fireworks/Tracks/plugins/FWSiStripClusterProxyBuilder.cc
@@ -2,7 +2,7 @@
 //
 #include <vector>
 #include "TEveGeoNode.h"
-#include "TEveStraightLineSet.h"
+#include "TEveLine.h"
 #include "TEveCompound.h"
 
 #include "Fireworks/Core/interface/fwLog.h"
@@ -17,14 +17,13 @@
 class FWSiStripClusterProxyBuilder : public FWProxyBuilderBase
 {
 public:
-   FWSiStripClusterProxyBuilder( void ) {}
-   virtual ~FWSiStripClusterProxyBuilder( void ) {}
+    FWSiStripClusterProxyBuilder( void );
+    virtual ~FWSiStripClusterProxyBuilder( void );
 
    REGISTER_PROXYBUILDER_METHODS();
 
-   virtual void clean() override;
-   virtual void cleanLocal() override;
-   
+    //    virtual void cleanLocal();
+   virtual void itemBeingDestroyed(const FWEventItem*);
 
 protected:
    using FWProxyBuilderBase::build;
@@ -35,37 +34,25 @@ private:
    FWSiStripClusterProxyBuilder( const FWSiStripClusterProxyBuilder& );
    const FWSiStripClusterProxyBuilder& operator=( const FWSiStripClusterProxyBuilder& );
 
-   std::vector<TEveGeoShape*> m_shapeMap;            
+   TEveElementList* m_shapeList;            
 };
 
-
-void
-FWSiStripClusterProxyBuilder::clean()
+FWSiStripClusterProxyBuilder::FWSiStripClusterProxyBuilder() : m_shapeList(0)
 {
-   // keep itemholders to restore configuration
-
-   for (FWProxyBuilderBase::Product_it i = m_products.begin(); i != m_products.end(); ++i)
-   {
-      if ((*i)->m_elements)
-      {
-         TEveElement* elms = (*i)->m_elements;
-         for (TEveElement::List_i it = elms->BeginChildren(); it != elms->EndChildren(); ++it)
-            (*it)->DestroyElements();
-      }
-   }
-
-   cleanLocal();
+    m_shapeList = new TEveElementList("shapePool"); 
+    m_shapeList->IncDenyDestroy();
 }
 
-
+FWSiStripClusterProxyBuilder::~FWSiStripClusterProxyBuilder()
+{
+    m_shapeList->DecDenyDestroy();
+}
 
 void
-FWSiStripClusterProxyBuilder::cleanLocal()
+FWSiStripClusterProxyBuilder::itemBeingDestroyed(const FWEventItem* iItem)
 {
-   for (TEveGeoShape* i : m_shapeMap)
-      i->Destroy();
-
-   m_shapeMap.clear();
+    m_shapeList->DestroyElements();
+    FWProxyBuilderBase::itemBeingDestroyed(iItem);
 }
 
 void
@@ -74,75 +61,77 @@ FWSiStripClusterProxyBuilder::build( const FWEventItem* iItem, TEveElementList* 
    const edmNew::DetSetVector<SiStripCluster>* clusters = 0;
    iItem->get( clusters );
    if( 0 == clusters ) return;
-   int cnt = 0;
+   int cntEl = 0;
 
+   for (TEveElement::List_i ei = product->BeginChildren(); ei != product->EndChildren(); ++ei) {
+       TEveElement* holder = *ei;
+       if (holder->HasChildren()) {
+           holder->SetRnrSelfChildren(false, false); 
+           holder->RemoveElement(holder->LastChild());
+       }
+   }
+
+   // check if need to create more shapes
+   int sdiff =  clusters->size() - m_shapeList->NumChildren();
+   for (int i = 0; i <= sdiff; ++i) 
+       m_shapeList->AddElement(new TEveGeoShape("Det"));
+   
+   TEveElement::List_i shapeIt = m_shapeList->BeginChildren();
    for( edmNew::DetSetVector<SiStripCluster>::const_iterator set = clusters->begin(), setEnd = clusters->end();
         set != setEnd; ++set) {
-      unsigned int id = set->detId();
 
-      TEveGeoShape* shape = item()->getGeom()->getEveShape( id );
-      if (shape) 
-      {
-         shape->SetMainTransparency( 75 );    
-         shape->SetElementName( "Det" );
-         m_shapeMap.push_back(shape);
-      }
-      else      
-      {
-         fwLog( fwlog::kWarning ) 
-            << "failed to get shape of SiStripCluster with detid: "
-            << id << std::endl;
-      }  
-       
-  
+      unsigned int id = set->detId();
+      const FWGeometry::GeomDetInfo& info = *iItem->getGeom()->find( id );
+
+      double array[16] = { info.matrix[0], info.matrix[3], info.matrix[6], 0.,
+			   info.matrix[1], info.matrix[4], info.matrix[7], 0.,
+			   info.matrix[2], info.matrix[5], info.matrix[8], 0.,
+			   info.translation[0], info.translation[1], info.translation[2], 1.
+      };
+
+
+      // note TEveGeoShape owns shape
+      TEveGeoShape* shape = (TEveGeoShape*)(*shapeIt);
+      shape->SetShape(iItem->getGeom()->getShape(info));
+      shape->SetTransMatrix(array);
+      shape->SetRnrSelf(true);
+      shapeIt++;
+
       for( edmNew::DetSet<SiStripCluster>::const_iterator ic = set->begin (), icEnd = set->end (); ic != icEnd; ++ic ) 
       {
-         TEveCompound* itemHolder = 0;
-         if (cnt < product->NumChildren())
-         {
-            TEveElement::List_i pit = product->BeginChildren();
-            std::advance(pit, cnt);
-            itemHolder = (TEveCompound*)*pit;
-            itemHolder->SetRnrSelfChildren(true, true);
-         }
-         else {
-            itemHolder = createCompound(); 
-            setupAddElement( itemHolder, product );
-         }
+          TEveCompound* itemHolder = 0;
+          TEveLine* line = 0;
 
-  // add common shape
-         if (shape) 
-         {
-            setupAddElement( shape, itemHolder );
-            increaseComponentTransparency( cnt, itemHolder, "Det", 60 );
-         }
+          if (cntEl < product->NumChildren())
+          {
+              TEveElement::List_i pit = product->BeginChildren();
+              std::advance(pit, cntEl);
+              itemHolder = (TEveCompound*)*pit;
+              itemHolder->SetRnrSelfChildren(true, true);
 
-         // add line        
-         if( ! item()->getGeom()->contains( id ))
-         {
-            fwLog( fwlog::kError )
-               << "failed to geometry of SiStripCluster with detid: " 
-               << id << std::endl;
-            continue;
-         }
+              line = (TEveLine*) (itemHolder->FirstChild());
+              setupAddElement( shape, itemHolder );
+          }
+          else {
+              itemHolder = createCompound(); 
+              setupAddElement( itemHolder, product );
+              line = new TEveLine("line");
+              setupAddElement( line, itemHolder );
+              setupAddElement( shape, itemHolder );
+          }
+          shape->SetMainTransparency(75);
 
-    
+          // setup line pnts
+          float localTop[3] = { 0.0, 0.0, 0.0 };
+          float localBottom[3] = { 0.0, 0.0, 0.0 };
+          fireworks::localSiStrip( (*ic).firstStrip(), localTop, localBottom, iItem->getGeom()->getParameters( id ), id );
+          float globalTop[3];
+          float globalBottom[3];
+          iItem->getGeom()->localToGlobal( id, localTop, globalTop, localBottom, globalBottom );
+          line->SetPoint(0, globalTop[0], globalTop[1], globalTop[2]);
+          line->SetPoint(1, globalBottom[0], globalBottom[1], globalBottom[2]);
 
-         TEveStraightLineSet *lineSet = new TEveStraightLineSet( "strip" );
-         setupAddElement( lineSet, itemHolder ); 
-         float localTop[3] = { 0.0, 0.0, 0.0 };
-         float localBottom[3] = { 0.0, 0.0, 0.0 };
-
-         fireworks::localSiStrip( (*ic).firstStrip(), localTop, localBottom, iItem->getGeom()->getParameters( id ), id );
-
-         float globalTop[3];
-         float globalBottom[3];
-         iItem->getGeom()->localToGlobal( id, localTop, globalTop, localBottom, globalBottom );
-  
-         lineSet->AddLine( globalTop[0], globalTop[1], globalTop[2],
-                           globalBottom[0], globalBottom[1], globalBottom[2] ); 
-
-         cnt++;
+          cntEl++;
       }
    }
 }
