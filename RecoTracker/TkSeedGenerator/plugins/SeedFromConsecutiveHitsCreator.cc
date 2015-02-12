428e67fdbd34b7c42f679d0d567d2aad90d2d671
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-02-12 11:31:05 +0100

    remove all existsAs, fix default config

--- RecoTracker/TkSeedGenerator/plugins/SeedFromConsecutiveHitsCreator.cc
+++ RecoTracker/TkSeedGenerator/plugins/SeedFromConsecutiveHitsCreator.cc
@@ -41,15 +41,9 @@ void SeedFromConsecutiveHitsCreator::init(const TrackingRegion & iregion,
   isBOFF = (0==nomField);
 
   edm::ESHandle<TransientTrackingRecHitBuilder> builderH;
-  try { // one sure we need to propagate the ocnfig to HLT
-    es.get<TransientRecHitRecord>().get(TTRHBuilder, builderH);
-    auto builder = (TkTransientTrackingRecHitBuilder const *)(builderH.product());
-    cloner = (*builder).cloner();
-  } catch(...) {
-    es.get<TransientRecHitRecord>().get("hltESPTTRHBWithTrackAngle", builderH);
-    auto builder = (TkTransientTrackingRecHitBuilder const *)(builderH.product());
-    cloner = (*builder).cloner();
- }
+  es.get<TransientRecHitRecord>().get(TTRHBuilder, builderH);
+  auto builder = (TkTransientTrackingRecHitBuilder const *)(builderH.product());
+  cloner = (*builder).cloner();
 
 }
 
