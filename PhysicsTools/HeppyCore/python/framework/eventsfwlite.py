78cb849d399c2f761a5eacda5b5bd88d7cb63337
Andrea <andrea.rizzi@cern.ch>
2015-02-12 14:08:55 +0100

    add cmsRun preprocessor

--- PhysicsTools/HeppyCore/python/framework/eventsfwlite.py
+++ PhysicsTools/HeppyCore/python/framework/eventsfwlite.py
@@ -1,8 +1,18 @@
 from DataFormats.FWLite import Events as FWLiteEvents
 
 class Events(object):
-    def __init__(self, files, tree_name):
-        self.events = FWLiteEvents(files)
+    def __init__(self, files, tree_name,  **kwargs):
+	if "options" in kwargs :
+		options=kwargs["options"]
+		if not hasattr(options,"inputFiles"):
+		 	options.inputFiles=files
+		if not hasattr(options,"maxEvents"):
+			options.maxEvents = 0	
+		if not hasattr(options,"secondaryInputFiles"):
+			options.secondaryInputFiles = []
+	        self.events = FWLiteEvents(options=options)
+	else :
+	        self.events = FWLiteEvents(files)
 
     def __len__(self):
         return self.events.size()
