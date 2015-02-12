e7299da7922f6fafb979ec6e3d5408373f5b48d1
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-02-12 17:14:09 +0100

    Document changes.

--- FWCore/Framework/interface/global/EDAnalyzer.h
+++ FWCore/Framework/interface/global/EDAnalyzer.h
@@ -35,9 +35,12 @@ namespace edm {
       
     public:
       EDAnalyzer() = default;
- #ifdef __INTEL_COMPILER
+// We do this only in the case of the intel compiler as this might
+// end up creating a lot of code bloat due to inline symbols being generated 
+// in each DSO which uses this header.
+#ifdef __INTEL_COMPILER
       virtual ~EDAnalyzer() {}
- #endif
+#endif
       // ---------- const member functions ---------------------
       
       // ---------- static member functions --------------------
