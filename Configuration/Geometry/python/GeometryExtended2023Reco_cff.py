f605a4525b7e65c875dcb5cc7cd2bf0325e1755c
Ianna Osborne <ianna.osborne@cern.ch>
2015-02-12 17:48:23 +0100

    2023 scenario

--- Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2023Reco_cff.py
@@ -6,7 +6,7 @@ from Configuration.Geometry.GeometryExtended2023_cff import *
 # Reconstruction geometry services
 #  Tracking Geometry
 #bah - well, this is not a cfi!
-from Geometry.CommonDetUnit.globalTracking2023Geometry_cfi import *
+from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
 
 #Tracker
 from RecoTracker.GeometryESProducer.TrackerRecoGeometryESProducer_cfi import *
@@ -15,13 +15,12 @@ from Geometry.TrackerNumberingBuilder.trackerTopology2023Constants_cfi import *
 #Muon
 from Geometry.MuonNumbering.muonNumberingInitialization_cfi import *
 from RecoMuon.DetLayers.muonDetLayerGeometry_cfi import *
-from Geometry.GEMGeometry.gemGeometry_cfi import *
 
 #  Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerSLHCGeometry_cff import *
 from Geometry.CSCGeometryBuilder.idealForDigiCscGeometry_cff import *
 from Geometry.DTGeometryBuilder.idealForDigiDtGeometry_cff import *
-trackerSLHCGeometry.applyAlignment = cms.bool(False)
+trackerGeometry.applyAlignment = cms.bool(False)
 
 #  Calorimeters
 from Geometry.CaloEventSetup.CaloTopology_cfi import *
