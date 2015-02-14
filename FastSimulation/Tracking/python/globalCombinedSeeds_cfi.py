ddfcc86d603bdc0cdb9477a0df9bece6a498a825
Lukas <lukas.vanelderen@cern.ch>
2015-02-14 14:33:25 +0100

    add a dummy iteration such that generaltracks cfg can be imported from full tracking

--- FastSimulation/Tracking/python/globalCombinedSeeds_cfi.py
+++ FastSimulation/Tracking/python/globalCombinedSeeds_cfi.py
@@ -3,11 +3,11 @@ import FWCore.ParameterSet.Config as cms
 newCombinedSeeds = cms.EDProducer(
     "SeedCombiner",
     seedCollections = cms.VInputTag(
-        cms.InputTag('iterativeInitialSeeds'),
-        #cms.InputTag('iterativeLowPtTripletSeeds'),
-        cms.InputTag('iterativePixelPairSeeds'),
-        cms.InputTag('iterativeMixedTripletStepSeeds'),
-        cms.InputTag('iterativePixelLessSeeds'),
+        cms.InputTag('initialStepSeeds'),
+        #cms.InputTag('lowPtTripletStepSeeds'),
+        cms.InputTag('pixelPairStepSeeds'),
+        cms.InputTag('mixedTripletStepSeeds'),
+        cms.InputTag('pixelLessStepSeeds'),
+        )
     )
-)
 
