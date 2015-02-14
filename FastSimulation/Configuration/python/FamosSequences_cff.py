91ee33caa87cab531dc000098bdbb5677ff5b425
Lukas <lukas.vanelderen@cern.ch>
2015-02-14 14:33:25 +0100

    add a dummy iteration such that generaltracks cfg can be imported from full tracking

--- FastSimulation/Configuration/python/FamosSequences_cff.py
+++ FastSimulation/Configuration/python/FamosSequences_cff.py
@@ -36,7 +36,12 @@ from FastSimulation.Tracking.GSTrackFinalFitCommon_cff import *
 # this one is added before 340pre3 to cope with adding SiPixelTemplateDBObjectESProducer and corresponding objects to the ConfDB (MC_3XY_V11, STARTUP3X_V10)
 from CalibTracker.SiPixelESProducers.SiPixelTemplateDBObjectESProducer_cfi import *
 from FastSimulation.Tracking.GlobalPixelTracking_cff import *
-from FastSimulation.Tracking.iterativeTk_cff import *
+
+from FastSimulation.Tracking.RecoTracker_cff import *
+
+from FastSimulation.Tracking.PixelTracksProducer_cff import *
+from FastSimulation.Tracking.PixelVerticesProducer_cff import *
+
 
 # Calo RecHits producer (with no HCAL miscalibration by default)
 from FastSimulation.CaloRecHitsProducer.CaloRecHits_cff import *
@@ -207,6 +212,7 @@ electronGsfTracks.TrajectoryInEvent = True
 # PF related electron sequences defined in FastSimulation.ParticleFlow.ParticleFlowFastSim_cff
 from RecoEgamma.ElectronIdentification.electronIdSequence_cff import *
 
+
 iterativeTrackingBeginning = cms.Sequence(
     initialStepSeeds+
     pixelPairStepSeeds+
@@ -274,6 +280,7 @@ simulationSequence = cms.Sequence(
     )
 trackReco = cms.Sequence(
     siTrackerGaussianSmearingRecHits+
+    MeasurementTrackerEvent+
     iterTracking
     )
 
--- FastSimulation/Tracking/python/iterativeTk_cff.py
+++ FastSimulation/Tracking/python/iterativeTk_cff.py
@@ -1,37 +1,34 @@
+##############################
+# FastSim equivalent of RecoTracker/IterativeTracking/python/iterativeTk_cff.py
+##############################
+
 import FWCore.ParameterSet.Config as cms
 
-from FastSimulation.Tracking.PixelTracksProducer_cff import *
-from FastSimulation.Tracking.PixelVerticesProducer_cff import *
-from FastSimulation.Tracking.GeneralTracks_cfi import *
-from TrackingTools.TrackFitters.TrackFitters_cff import *
-from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
+from FastSimulation.Tracking.InitialStep_cff import *
+from FastSimulation.Tracking.DetachedTripletStep_cff import *
+from FastSimulation.Tracking.LowPtTripletStep_cff import *
+from FastSimulation.Tracking.PixelPairStep_cff import *
+from FastSimulation.Tracking.MixedTripletStep_cff import *
+from FastSimulation.Tracking.PixelLessStep_cff import *
+from FastSimulation.Tracking.TobTecStep_cff import *
+# the following loads a dummy empty track collection
+# such that FastSim can import earlyGeneralTracks_cfi from full tracking
+# todo: actual implementation of JetCore iteration  
+from FastSimulation.Tracking.JetCoreRegionalStep_cff import *
 
-from FastSimulation.Tracking.IterativeInitialStep_cff import *
-from FastSimulation.Tracking.IterativeDetachedTripletStep_cff import *
-from FastSimulation.Tracking.IterativeLowPtTripletStep_cff import *
-from FastSimulation.Tracking.IterativePixelPairStep_cff import *
-from FastSimulation.Tracking.IterativeMixedTripletStep_cff import *
-from FastSimulation.Tracking.IterativePixelLessStep_cff import *
-from FastSimulation.Tracking.IterativeTobTecStep_cff import *
+import RecoTracker.FinalTrackSelectors.earlyGeneralTracks_cfi
+# todo, import MuonSeededStep_cff, preDuplicateMergingGeneralTracks_cfi, MergeTrackCollections_cff, ConversionStep_cff
 
-#trackExtrapolator.trackSrc = cms.InputTag("generalTracksBeforeMixing")
-trackExtrapolator.trackSrc = cms.InputTag("generalTracks")
-        
-import RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi
-MeasurementTrackerEvent = RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi.MeasurementTrackerEvent.clone(
-    pixelClusterProducer = '',
-    stripClusterProducer = '',
-    inactivePixelDetectorLabels = cms.VInputTag(),
-    inactiveStripDetectorLabels = cms.VInputTag(),
-    switchOffPixelsIfEmpty = False
-)
+generalTracksBeforeMixing = RecoTracker.FinalTrackSelectors.earlyGeneralTracks_cfi.earlyGeneralTracks.clone()
+
 iterTracking = cms.Sequence(
-    MeasurementTrackerEvent 
-    +InitialStep
+    InitialStep
     +DetachedTripletStep
     +LowPtTripletStep
     +PixelPairStep
     +MixedTripletStep
     +PixelLessStep
     +TobTecStep
+    +JetCoreRegionalStep
     +generalTracksBeforeMixing)
+
