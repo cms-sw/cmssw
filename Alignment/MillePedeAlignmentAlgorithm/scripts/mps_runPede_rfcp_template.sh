304de05cc488cfeb5504a0ae2ce64072c70f92c7
Frank Meier <frmeier@cern.ch>
2015-02-17 00:48:36 +0100

    Changed mps scripts to copy back the millepede.end file. This file contains the detailed exit status of pede.

--- Alignment/MillePedeAlignmentAlgorithm/scripts/mps_runPede_rfcp_template.sh
+++ Alignment/MillePedeAlignmentAlgorithm/scripts/mps_runPede_rfcp_template.sh
@@ -31,6 +31,7 @@ clean_up () {
     cp -p *.log.gz $RUNDIR
     cp -p millePedeMonitor*root $RUNDIR
     cp -p millepede.res* $RUNDIR
+    cp -p millepede.end $RUNDIR
     cp -p millepede.his* $RUNDIR
     cp -p *.db $RUNDIR
     exit
