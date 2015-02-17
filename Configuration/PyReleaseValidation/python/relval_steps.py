fac2ba787fed557fac117d01e0a752f4d8b15f2f
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2015-02-17 23:43:07 +0100

    premix: upgradePLS150ns => run2_mc_50ns; same phy processes as classi mix; include miniaod

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -842,7 +842,7 @@ premixUp2015Defaults = {
     '--magField'    : '38T_PostLS1',
     '--customise'   : 'SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1' # temporary replacement for premix; to be brought back to customisePostLS1
 }
-premixUp2015Defaults50ns = merge([{'--conditions':'auto:upgradePLS150ns'},premixUp2015Defaults])
+premixUp2015Defaults50ns = merge([{'--conditions':'auto:run2_mc_50ns'},premixUp2015Defaults])
 
 steps['PREMIXUP15_PU25']=merge([PU25,Kby(100,100),premixUp2015Defaults])
 steps['PREMIXUP15_PU50']=merge([PU50,Kby(100,100),premixUp2015Defaults50ns])
@@ -857,7 +857,7 @@ digiPremixUp2015Defaults25ns = {
     '--customise'    : 'SLHCUpgradeSimulations/Configuration/postLS1CustomsPreMixing.customisePostLS1', # temporary replacement for premix; to be brought back to customisePostLS1
     '--magField'     : '38T_PostLS1',
     }
-digiPremixUp2015Defaults50ns=merge([{'--conditions':'auto:upgradePLS150ns'},
+digiPremixUp2015Defaults50ns=merge([{'--conditions':'auto:run2_mc_50ns'},
                                     {'--pileup_input' : 'das:/RelValPREMIXUP15_PU50/%s/GEN-SIM-DIGI-RAW'%baseDataSetRelease[6]},
                                     digiPremixUp2015Defaults25ns])
 steps['DIGIPRMXUP15_PU25']=merge([digiPremixUp2015Defaults25ns])
@@ -1021,7 +1021,7 @@ recoPremixUp15prod = merge([
 steps['RECOPRMXUP15PROD_PU25']=merge([
         recoPremixUp15prod])
 steps['RECOPRMXUP15PROD_PU50']=merge([
-        {'--conditions':'auto:upgradePLS150ns'},
+        {'--conditions':'auto:run2_mc_50ns'},
         recoPremixUp15prod])
 
 
