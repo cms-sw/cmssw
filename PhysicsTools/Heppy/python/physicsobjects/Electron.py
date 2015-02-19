30d2c1c323731e95251784af751dd81f980ad758
Jan Steggemann <jan.steggemann@cern.ch>
2015-02-19 09:23:59 +0100

    Fix name of R-dependent all charged iso; redefine default all charged iso from non-existent method to raising an error since not available in default miniAOD PAT electrons

--- PhysicsTools/Heppy/python/physicsobjects/Electron.py
+++ PhysicsTools/Heppy/python/physicsobjects/Electron.py
@@ -1,5 +1,5 @@
 from PhysicsTools.Heppy.physicsobjects.Lepton import Lepton
-from PhysicsTools.Heppy.physicsutils.ElectronMVAID import ElectronMVAID_Trig, ElectronMVAID_NonTrig, ElectronMVAID_TrigNoIP
+from PhysicsTools.Heppy.physicsutils.ElectronMVAID import *
 import ROOT
 
 class Electron( Lepton ):
@@ -147,23 +147,17 @@ class Electron( Lepton ):
         elif R == 0.4: return self.physObj.photonIso()
         raise RuntimeError, "Electron photonIso missing for R=%s" % R
 
-    def chargedAllIso(self,R=0.4):
+    def chargedAllIsoR(self,R=0.4):
         if   R == 0.3: return self.physObj.pfIsolationVariables().sumChargedParticlePt 
         raise RuntimeError, "Electron chargedAllIso missing for R=%s" % R
+
+    def chargedAllIso(self):
+        raise RuntimeError, "Electron chargedAllIso missing"
 
     def puChargedHadronIsoR(self,R=0.4):
         if   R == 0.3: return self.physObj.pfIsolationVariables().sumPUPt 
         elif R == 0.4: return self.physObj.puChargedHadronIso()
         raise RuntimeError, "Electron chargedHadronIso missing for R=%s" % R
-
-
-
-
-    def chargedAllIso(self):
-        '''This function is used in the isolation, see Lepton class.
-        Here, we replace the all charged isolation by the all charged isolation with cone veto'''
-        return self.chargedAllIsoWithConeVeto()
-
 
     def dxy(self, vertex=None):
         '''Returns dxy.
