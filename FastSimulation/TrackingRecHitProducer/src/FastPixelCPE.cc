36dfead781899e5dcfca960f6e5c74f08599736a
Matthias <mkomm@cern.ch>
2015-02-20 15:16:37 +0100

    deleted pixel/strip fastclusters

--- FastSimulation/TrackingRecHitProducer/src/FastPixelCPE.cc
+++
@@ -1,9 +0,0 @@
-#include "FastPixelCPE.h"
-
-#include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetUnit.h"
-#include "Geometry/TrackerGeometryBuilder/interface/RectangularPixelTopology.h"
-
-
-// Services
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-
