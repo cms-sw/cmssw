f6593710936290e8027bd36b1aafa38f8f343834
Ianna Osborne <ianna.osborne@cern.ch>
2015-02-24 10:33:52 +0100

    Update Tracker parameters schema. Add test configuration files. Add parameter analyzer. Add plugins for run 3 tests.

---
+++ CondTools/Geometry/test/trackergeometrywriterRun2.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TrackerGeometryWriter")
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("Configuration.Geometry.GeometryExtended2015_cff")
+
+process.source = cms.Source("EmptyIOVSource",
+                            lastValue = cms.uint64(1),
+                            timetype = cms.string('runnumber'),
+                            firstValue = cms.uint64(1),
+                            interval = cms.uint64(1)
+                            )
+
+process.TrackerGeometricDetExtraESModule = cms.ESProducer( "TrackerGeometricDetExtraESModule",
+                                                           fromDDD = cms.bool( True )
+                                                           )
+
+process.TrackerGeometryWriter = cms.EDAnalyzer("PGeometricDetBuilder")
+process.TrackerGeometryExtraWriter = cms.EDAnalyzer("PGeometricDetExtraBuilder")
+process.TrackerParametersWriter = cms.EDAnalyzer("PTrackerParametersDBBuilder")
+
+process.CondDBCommon.BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService')
+process.CondDBCommon.timetype = cms.untracked.string('runnumber')
+process.CondDBCommon.connect = cms.string('sqlite_file:myfilerun2.db')
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+                                          process.CondDBCommon,
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('IdealGeometryRecord'),tag = cms.string('TKRECO_Geometry_Run2_Test02')),
+                                                            cms.PSet(record = cms.string('PGeometricDetExtraRcd'),tag = cms.string('TKExtra_Geometry_Run2_Test02')),
+                                                            cms.PSet(record = cms.string('PTrackerParametersRcd'),tag = cms.string('TK_Parameters_Run2_Test02'))
+                                                            )
+                                          )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+    )
+
+process.p1 = cms.Path(process.TrackerGeometryWriter*process.TrackerGeometryExtraWriter*process.TrackerParametersWriter)
+
