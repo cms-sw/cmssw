5102975da3ea951417967c4a4af67e3a80ea19aa
Ianna Osborne <ianna.osborne@cern.ch>
2015-02-24 10:48:02 +0100

    Apply material budget from Test2014

--- Geometry/CMSCommonData/python/cmsExtendedGeometryXML_cfi.py
+++ Geometry/CMSCommonData/python/cmsExtendedGeometryXML_cfi.py
@@ -239,7 +239,6 @@ XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
         'Geometry/ForwardCommonData/data/zdclumi.xml',
         'Geometry/ForwardCommonData/data/cmszdc.xml')+cms.vstring(
         'Geometry/ForwardCommonData/data/bhm.xml',
-        ##'Geometry/ForwardCommonData/data/bhmsens.xml', #not needed for geomtery purposes only
         'Geometry/MuonCommonData/data/muonNumbering.xml',
         'Geometry/TrackerCommonData/data/trackerStructureTopology.xml',
         'Geometry/TrackerSimData/data/trackersens.xml',
