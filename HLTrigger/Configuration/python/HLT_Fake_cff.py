41e8819dc228727e2f56355635b042d93239d4f7
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-02-24 08:59:10 +0100

    Update for Ecal/fillDescription/MultiFit

--- HLTrigger/Configuration/python/HLT_Fake_cff.py
+++ HLTrigger/Configuration/python/HLT_Fake_cff.py
@@ -1,10 +1,10 @@
-# /dev/CMSSW_7_4_0/Fake/V2 (CMSSW_7_4_0_pre7)
+# /dev/CMSSW_7_4_0/Fake/V3 (CMSSW_7_4_0_pre7_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 
 HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/Fake/V2')
+  tableName = cms.string('/dev/CMSSW_7_4_0/Fake/V3')
 )
 
 streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
