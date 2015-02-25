bce4d481c57e3cd99f5aee447188b0f3c773f523
shubhanshu chauhan <shubhanshu.chauhan.cern.ch>
2015-02-25 12:27:05 +0100

    sync seed cuts with full tracking

--- FastSimulation/Tracking/python/TobTecStep_cff.py
+++ FastSimulation/Tracking/python/TobTecStep_cff.py
@@ -1,58 +1,58 @@
 import FWCore.ParameterSet.Config as cms
 
-# trajectory seeds
+# import the full tracking equivalent of this file
+import RecoTracker.IterativeTracking.TobTecStep_cff
 
+# trajectory seeds 
 import FastSimulation.Tracking.TrajectorySeedProducer_cfi
-tobTecStepSeeds = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.clone( 
-   simTrackSelection = trajectorySeedProducer.simTrackSelection.clone(
+tobTecStepSeeds = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.clone(
+    simTrackSelection = FastSimulation.Tracking.TrajectorySeedProducer_cfi.trajectorySeedProducer.simTrackSelection.clone(
         skipSimTrackIds = [
-            cms.InputTag("initialStepSimTrackIds"), 
-            cms.InputTag("detachedTripletStepSimTrackIds"), 
-            cms.InputTag("lowPtTripletStepSimTrackIds"), 
-            cms.InputTag("pixelPairStepSimTrackIds"), 
-            cms.InputTag("mixedTripletStepSimTrackIds"), 
+            cms.InputTag("initialStepSimTrackIds"),
+            cms.InputTag("detachedTripletStepSimTrackIds"),
+            cms.InputTag("lowPtTripletStepSimTrackIds"),
+            cms.InputTag("pixelPairStepSimTrackIds"),
+            cms.InputTag("mixedTripletStepSimTrackIds"),
             cms.InputTag("pixelLessStepSimTrackIds")],
         pTMin = 0.3,
-        maxD0 = 99.,
-        maxZ0 = 99.
-        ),
-   minLayersCrossed = 4,
-   originRadius = 6.0,
-   originHalfLength = 30.0,
-   originpTMin = 0.6,
+        maxD0 = 99.0,
+        maxZ0 = 99
+    ),
+    minLayersCrossed = 4,
+    originpTMin = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepSeedsPair.RegionFactoryPSet.RegionPSet.ptMin,
+    originHalfLength = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepSeedsPair.RegionFactoryPSet.RegionPSet.originHalfLength,
+    originRadius = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepSeedsPair.RegionFactoryPSet.RegionPSet.originRadius,
+    layerList = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepSeedLayersPair.layerList.value()
 )
-from RecoTracker.IterativeTracking.TobTecStep_cff import tobTecStepSeedLayersPair as _tobTecStepSeedLayersPair
-tobTecSeeds.layerList = ['TOB1+TOB2']
-tobTecSeeds.layerList.extend(_tobTecStepSeedLayersPair.layerList)
 
-# candidate producer
-from FastSimulation.Tracking.TrackCandidateProducer_cfi import trackCandidateProducer
-tobTecStepTrackCandidates = trackCandidateProducer.clone(
+# track candidate
+import FastSimulation.Tracking.TrackCandidateProducer_cfi
+tobTecStepTrackCandidates = FastSimulation.Tracking.TrackCandidateProducer_cfi.trackCandidateProducer.clone(
     SeedProducer = cms.InputTag("tobTecStepSeeds"),
-    MinNumberOfCrossedLayers = 3)
+    MinNumberOfCrossedLayers = 3
+)
 
-# track producer
-from RecoTracker.IterativeTracking.TobTecStep_cff import tobTecStepTracks
-tobTecStepTracks = tobTecStepTracks.clone(
+# tracks 
+tobTecStepTracks = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepTracks.clone(
     TTRHBuilder = 'WithoutRefit',
     Fitter = 'KFFittingSmootherFifth',
-    Propagator = 'PropagatorWithMaterial')
+    Propagator = 'PropagatorWithMaterial'
+)
 
 # simtrack id producer
 tobTecStepSimTrackIds = cms.EDProducer("SimTrackIdProducer",
                                        trackCollection = cms.InputTag("tobTecStepTracks"),
                                        HitProducer = cms.InputTag("siTrackerGaussianSmearingRecHits","TrackerGSMatchedRecHits")
-                                       )
+)
 
-
-
-# track selection
-from RecoTracker.IterativeTracking.TobTecStep_cff import tobTecStepSelector
+# final selection
+tobTecStepSelector = RecoTracker.IterativeTracking.TobTecStep_cff.tobTecStepSelector.clone()
 tobTecStepSelector.vertices = "firstStepPrimaryVerticesBeforeMixing"
 
-# sequence
+# Final sequence 
 TobTecStep = cms.Sequence(tobTecStepSeeds
                           +tobTecStepTrackCandidates
                           +tobTecStepTracks
-                          +tobTecStepSelector
-                          +tobTecStepSimTrackIds)
+                          +tobTecStepSelector                          
+                          +tobTecStepSimTrackIds
+                      )
