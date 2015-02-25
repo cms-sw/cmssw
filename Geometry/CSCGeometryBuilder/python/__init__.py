a0f4461d792fe0c95d0081290a98290ef259efd3
David <dnash@cern.ch>
2015-02-25 18:03:57 +0100

    Added ME0s to HitPattern

---
+++ Geometry/CSCGeometryBuilder/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Geometry/CSCGeometryBuilder/',1)[0])+'/cfipython/slc6_amd64_gcc472/Geometry/CSCGeometryBuilder')
