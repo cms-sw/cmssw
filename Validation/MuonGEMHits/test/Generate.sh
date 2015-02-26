4ed25edd04cdb9b24ca5331d45eeb407e6ea1abc
Geonmo <ry840901@gmail.com>
2015-02-26 13:29:43 +0100

    Fix bug and remove legacy.

--- Validation/MuonGEMHits/test/Generate.sh
+++ Validation/MuonGEMHits/test/Generate.sh
@@ -7,6 +7,6 @@
 
 cmsDriver.py SingleMuPt40Fwd_cfi -s GEN,SIM --conditions auto:run1_mc --datatier GEN-SIM --geometry Extended2023 --evt_type Validation/MuonGEMHits/SingleMuPt40Fwd_cfi --eventcontent FEVTDEBUG -n 1000 --no_exec --fileout out_sim.root --python_filename=sim_cfg.py
 
-cmsDriver.py validation --conditions auto:run1_mc -n 1000 --eventcontent FEVTDEBUGHLT -s VALIDATION:genvalid_all --customise Validation/MuonGEMHits/gemCustum.customise2023 --datatier GEN-SIM --geometry Extended2023 --no_exec --filein file:out_sim.root --fileout file:out_valid.root --python_filename=valid_cfg.py
+cmsDriver.py validation --conditions auto:run1_mc -n 1000 --eventcontent FEVTDEBUGHLT -s VALIDATION:genvalid_all --customise Validation/MuonGEMHits/gemCustom.customise2023 --datatier GEN-SIM --geometry Extended2023 --no_exec --filein file:out_sim.root --fileout file:out_valid.root --python_filename=valid_cfg.py
 
-cmsDriver.py harvest --conditions auto:run1_mc -n 1000 --eventcontent FEVTDEBUGHLT -s HARVESTING:genHarvesting --customise Validation/MuonGEMHits/gemCustum.customise2023 --datatier GEN-SIM --geometry Extended2023 --no_exec --filein file:out_valid.root --python_filename=harvest_cfg.py
+cmsDriver.py harvest --conditions auto:run1_mc -n 1000 --eventcontent FEVTDEBUGHLT -s HARVESTING:genHarvesting --customise Validation/MuonGEMHits/gemCustom.customise2023 --datatier GEN-SIM --geometry Extended2023 --no_exec --filein file:out_valid.root --python_filename=harvest_cfg.py
