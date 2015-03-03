4e5ee01b4e96a58d1800eb646574100a8e3bc3c8
Adrien <adrien.caudron@cern.ch>
2015-03-03 18:27:28 +0100

    Switch by default to new flavour tool based on Hadrons. Update of example scripts.

--- DQMOffline/RecoB/python/bTagSequences_cff.py
+++ DQMOffline/RecoB/python/bTagSequences_cff.py
@@ -3,8 +3,6 @@ import FWCore.ParameterSet.Config as cms
 #define you jet ID
 jetID = cms.InputTag("ak4PFJetsCHS")
 
-
-
 #JTA for your jets
 from RecoJets.JetAssociationProducers.j2tParametersVX_cfi import *
 myak4JetTracksAssociatorAtVertex = cms.EDProducer("JetTracksAssociatorAtVertex",
@@ -20,11 +18,20 @@ softPFMuonsTagInfos.jets = jetID
 softPFElectronsTagInfos.jets = jetID
 
 #for MC do the matching with you jet collection
+from PhysicsTools.JetMCAlgos.HadronAndPartonSelector_cfi import selectedHadronsAndPartons
+from PhysicsTools.JetMCAlgos.AK4PFJetsMCFlavourInfos_cfi import ak4JetFlavourInfos
+ak4JetFlavourInfos.jets = jetID
+
 from PhysicsTools.JetMCAlgos.CaloJetsMCFlavour_cfi import *
 AK4byRef.jets = jetID
 
 #do the matching : only for MC
 flavourSeq = cms.Sequence(
+    selectedHadronsAndPartons *
+    ak4JetFlavourInfos
+    )
+
+oldFlavourSeq = cms.Sequence(
     myPartons *
     AK4Flavour
     )
@@ -42,17 +49,3 @@ from CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi import goodOfflineP
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 bTagHLT  = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = ["HLT_PFJet40_v*"])
 
-#for the  use of JEC, could change with time : be careful if recommandations change for the correctors
-#define you sequence like  process.JECAlgo = cms.Sequence(process.ak4PFJetsJEC * process.PFJetsFilter)
-JetCut=cms.string("neutralHadronEnergyFraction < 0.99 && neutralEmEnergyFraction < 0.99 && nConstituents > 1 && chargedHadronEnergyFraction > 0.0 && chargedMultiplicity > 0.0 && chargedEmEnergyFraction < 0.99")
-#JetCut=cms.string("chargedEmEnergyFraction < 99999")
-
-from JetMETCorrections.Configuration.DefaultJEC_cff import *
-from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
-
-
-PFJetsFilter = cms.EDFilter("PFJetSelector",
-                            src = cms.InputTag("ak4PFJetsL2L3"),
-                            cut = JetCut,
-                            filter = cms.bool(True)
-                            )
--- Validation/RecoB/test/validation_customJet_cfg.py
+++ Validation/RecoB/test/validation_customJet_cfg.py
@@ -7,7 +7,7 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 options = VarParsing.VarParsing ('analysis')
 
 options.register ('jets',
-                  "ak4PFCHS", # default value, allowed : "ak4PF", "ak4PFCHS", add "NoJEC" to run the code with no JEC applied
+                  "ak4PFJetsCHS", # default value, examples : "ak4PFJets", "ak4PFJetsCHS"
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string,  
                   "jet collection to use")
@@ -15,55 +15,41 @@ options.register ('jets',
 options.parseArguments()
 
 whichJets  = options.jets 
+applyJEC = True
+corrLabel = 'ak4PFCHSL1FastL2L3'
+tag =  'MCRUN2_74_V7::All'
 useTrigger = False
+triggerPath = "HLT_PFJet80_v*"
 runOnMC    = True
-tag =  'POSTLS172_V3::All'
+#Flavour plots for MC: "all" = plots for all jets ; "dusg" = plots for d, u, s, dus, g independently ; not mandatory and any combinations are possible                                     
+#b, c, light (dusg), non-identified (NI), PU jets plots are always produced
+flavPlots = "allbcldusg"
 
 ###prints###
 print "jet collcetion asked : ", whichJets
-print "trigger will be used ? : ", useTrigger
-print "is it MC ? : ", runOnMC
+print "JEC applied?", applyJEC, ", correction:", corrLabel 
+print "trigger will be used ? : ", useTrigger, ", Trigger paths:", triggerPath
+print "is it MC ? : ", runOnMC, ", Flavours:", flavPlots
 print "Global Tag : ", tag
 ############
 
 process = cms.Process("validation")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
-
-#keep the logging output to a nice level
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
-
-# load the full reconstraction configuration, to make sure we're getting all needed dependencies
-process.load("Configuration.StandardSequences.MagneticField_cff")
-#process.load("Configuration.StandardSequences.GeometryRecoDB_cff") #old one, to use for old releases
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-#process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')                                                                                                                                    
-#process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')                                                                                                                                    
-#process.load('Configuration.Geometry.GeometryExtended2019_cff')
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.GlobalTag.globaltag = tag
-
 process.load("DQMServices.Core.DQM_cfg")
 
 process.load("DQMOffline.RecoB.bTagSequences_cff")
-#process.bTagHLT.HLTPaths = ["HLT_PFJet80_v*"] #uncomment this line if you want to use different trigger
-
-newjetID=cms.InputTag("ak4PFJetsCHS")
+process.load("JetMETCorrections.Configuration.JetCorrectionServices_cff")
+process.load("PhysicsTools.PatAlgos.mcMatchLayer0.jetMatch_cfi")
 process.jetSequences = cms.Sequence(process.goodOfflinePrimaryVertices * process.btagSequence)
-if "NoJEC" in whichJets and not "CHS" in whichJets : newjetID=cms.InputTag("ak4PFJets")
-if not "NoJEC" in whichJets:
-    process.JECAlgo = cms.Sequence(process.ak4JetsJEC * process.PFJetsFilter)
-    process.jetSequences = cms.Sequence(process.goodOfflinePrimaryVertices * process.JECAlgo * process.btagSequence)
-    newjetID=cms.InputTag("PFJetsFilter")
-    if whichJets=="ak4PF":
-        process.ak4JetsJEC.src = 'ak4PFJets'
-        process.ak4JetsJEC.correctors = ['ak4PFL1FastL2L3']
-process.myak4JetTracksAssociatorAtVertex.jets = newjetID
-process.pfImpactParameterTagInfos.jets        = newjetID
-process.softPFMuonsTagInfos.jets              = newjetID
-process.softPFElectronsTagInfos.jets          = newjetID
-process.AK4byRef.jets                         = newjetID
+if not "ak4PFJetsCHS" in whichJets:
+    newjetID=cms.InputTag(whichJets)
+    process.myak4JetTracksAssociatorAtVertex.jets = newjetID
+    process.pfImpactParameterTagInfos.jets        = newjetID
+    process.softPFMuonsTagInfos.jets              = newjetID
+    process.softPFElectronsTagInfos.jets          = newjetID
+    process.ak4JetFlavourInfos.jets               = newjetID
+    process.AK4byRef.jets                         = newjetID
+    process.patJetGenJetMatch.src                 = newjetID
 
 ###
 print "inputTag : ", process.myak4JetTracksAssociatorAtVertex.jets
@@ -71,10 +57,17 @@ print "inputTag : ", process.myak4JetTracksAssociatorAtVertex.jets
 
 if runOnMC:
     process.load("Validation.RecoB.bTagAnalysis_cfi")
-    process.bTagValidation.jetMCSrc = 'AK4byValAlgo'
+    process.bTagValidation.jetMCSrc = 'ak4JetFlavourInfos'
+    if "Calo" in whichJets:
+        process.bTagValidation.caloJetMCSrc = 'AK4byValAlgo'
+        process.bTagValidation.useOldFlavourTool = True
+        process.flavourSeq = cms.Sequence(process.oldFlavourSeq)
     process.bTagValidation.applyPtHatWeight = False
-    process.bTagValidation.flavPlots = "allbcl" #if contains "all" plots for all jets booked, if contains "bcl" histograms for b, c and light jets booked, if contains "dusg" all histograms booked
-    process.bTagHarvestMC.flavPlots = "allbcl"
+    process.bTagValidation.doJetID = True
+    process.bTagValidation.doJEC = applyJEC
+    process.bTagValidation.JECsource = cms.string(corrLabel)
+    process.bTagValidation.flavPlots = flavPlots
+    process.bTagHarvestMC.flavPlots = flavPlots
     #process.bTagValidation.ptRecJetMin = cms.double(20.)
     process.bTagValidation.genJetsMatched = cms.InputTag("patJetGenJetMatch")
     process.bTagValidation.doPUid = cms.bool(True)
@@ -83,14 +76,13 @@ if runOnMC:
                                              cut = cms.string('pt > 8.'),
                                              filter = cms.bool(False)
                                              )
-    process.load("PhysicsTools.PatAlgos.mcMatchLayer0.jetMatch_cfi")
-    process.patJetGenJetMatch.src = newjetID
     process.patJetGenJetMatch.matched = cms.InputTag("ak4GenJetsForPUid")
     process.patJetGenJetMatch.maxDeltaR = cms.double(0.25)
     process.patJetGenJetMatch.resolveAmbiguities = cms.bool(True)
-else :
-    process.ak4JetsJEC.correctors[0] += 'Residual'
+else:
     process.load("DQMOffline.RecoB.bTagAnalysisData_cfi")
+    process.bTagAnalysis.doJEC = applyJEC
+    process.bTagAnalysis.JECsource = cms.string(corrLabel)
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -98,6 +90,8 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring()
 )
+
+if useTrigger : process.bTagHLT.HLTPaths = [triggerPath]
 
 if runOnMC:
     process.dqmSeq = cms.Sequence(process.ak4GenJetsForPUid * process.patJetGenJetMatch * process.flavourSeq * process.bTagValidation * process.bTagHarvestMC * process.dqmSaver)
@@ -117,6 +111,16 @@ process.dqmSaver.saveByRun = cms.untracked.int32(-1)
 process.dqmSaver.saveAtJobEnd =cms.untracked.bool(True) 
 process.dqmSaver.forceRunNumber = cms.untracked.int32(1)
 process.PoolSource.fileNames = [
-
 ]
+
+#keep the logging output to a nice level
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+# load the full reconstraction configuration, to make sure we're getting all needed dependencies
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.GlobalTag.globaltag = tag
 
