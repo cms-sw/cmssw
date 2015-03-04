ed17c20fef48ce1a3797f0ef8d1c82fb1d8168f6
David <dnash@cern.ch>
2015-03-04 15:08:23 +0100

    Commiting missed classes_def file in GEMDigi

--- DataFormats/GEMDigi/src/classes.h
+++ DataFormats/GEMDigi/src/classes.h
@@ -1,34 +1,42 @@
-#include "DataFormats/GEMRecHit/interface/GEMRecHit.h"
-#include "DataFormats/GEMRecHit/interface/GEMRecHitCollection.h"
-#include "DataFormats/GEMRecHit/interface/ME0RecHit.h"
-#include "DataFormats/GEMRecHit/interface/ME0RecHitCollection.h"
-#include "DataFormats/GEMRecHit/interface/ME0Segment.h"
-#include "DataFormats/GEMRecHit/interface/ME0SegmentCollection.h"
-#include "DataFormats/Common/interface/Wrapper.h"
+#include <DataFormats/GEMDigi/interface/GEMDigi.h>
+#include <DataFormats/GEMDigi/interface/GEMDigiCollection.h>
+#include <DataFormats/GEMDigi/interface/ME0DigiPreReco.h>
+#include <DataFormats/GEMDigi/interface/ME0DigiPreRecoCollection.h>
 
-namespace { 
+#include <DataFormats/GEMDigi/interface/GEMCSCPadDigi.h>
+#include <DataFormats/GEMDigi/interface/GEMCSCPadDigiCollection.h>
+#include <DataFormats/GEMDigi/interface/GEMCSCCoPadDigi.h>
+#include <DataFormats/GEMDigi/interface/GEMCSCCoPadDigiCollection.h>
+
+#include <DataFormats/Common/interface/Wrapper.h>
+#include <vector>
+
+namespace{ 
   struct dictionary {
-    std::pair<unsigned int, unsigned int> dummyrpc1;
-    std::pair<unsigned long, unsigned long> dummyrpc2;
-    std::map<GEMDetId, std::pair<unsigned int, unsigned int> > dummyrpcdetid1;
-    std::map<GEMDetId, std::pair<unsigned long, unsigned long> > dummyrpcdetid2;
-    std::map<ME0DetId, std::pair<unsigned int, unsigned int> > dummyme0detid1;
-    std::map<ME0DetId, std::pair<unsigned long, unsigned long> > dummyme0detid2;
+    
+    GEMDigi g;
+    std::vector<GEMDigi>  vg;
+    std::vector<std::vector<GEMDigi> >  vvg;
+    GEMDigiCollection gcol;
+    edm::Wrapper<GEMDigiCollection> wg;
 
-    GEMRecHit rrh;
-    std::vector<GEMRecHit> vrh;
-    GEMRecHitCollection c;
-    edm::Wrapper<GEMRecHitCollection> w;
+    GEMCSCPadDigi gc;
+    std::vector<GEMCSCPadDigi>  vgc;
+    std::vector<std::vector<GEMCSCPadDigi> >  vvgc;
+    GEMCSCPadDigiCollection gccol;
+    edm::Wrapper<GEMCSCPadDigiCollection> wgc;
 
-    ME0RecHit mrh;
-    std::vector<ME0RecHit> vmrh;
-    ME0RecHitCollection mc;
-    edm::Wrapper<ME0RecHitCollection> mw;
+    GEMCSCCoPadDigi gcp;
+    std::vector<GEMCSCCoPadDigi>  vgcp;
+    std::vector<std::vector<GEMCSCCoPadDigi> >  vvgcp;
+    GEMCSCCoPadDigiCollection gcpcol;
+    edm::Wrapper<GEMCSCCoPadDigiCollection> wgcp;
 
-    ME0Segment ms;
-    ME0SegmentCollection seg;    
-    edm::Wrapper<ME0SegmentCollection> dwc1;
-    ME0SegmentRef ref;    
+    ME0DigiPreReco m;
+    std::vector<ME0DigiPreReco>  vm;
+    std::vector<std::vector<ME0DigiPreReco> >  vvm;
+    ME0DigiPreRecoCollection mcol;
+    edm::Wrapper<ME0DigiPreRecoCollection> wm;
+
   };
 }
-
