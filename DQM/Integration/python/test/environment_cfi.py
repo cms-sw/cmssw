e18e0d75070cdbdfa67513aa5ff98438551f52f4
federico de guio <federico.de.guio@cern.ch>
2015-03-04 16:20:28 +0100

    changing the default to users gui

--- DQM/Integration/python/test/environment_cfi.py
+++ DQM/Integration/python/test/environment_cfi.py
@@ -6,8 +6,8 @@ from DQMServices.Core.DQMStore_cfi import *
 DQM = cms.Service("DQM",                                                                                                                                                                           
                   debug = cms.untracked.bool(False),
                   publishFrequency = cms.untracked.double(5.0),
-                  collectorPort = cms.untracked.int32(9090),
-                  collectorHost = cms.untracked.string('dqm-prod-local.cms'),
+                  collectorPort = cms.untracked.int32(9190),
+                  collectorHost = cms.untracked.string('dqm-c2d07-29.cms'),
                   filter = cms.untracked.string('')
                   )
 
