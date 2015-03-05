3c83e439302122138733f3ab5caac5f4702341c8
Lukas <lukas.vanelderen@cern.ch>
2015-03-05 20:39:18 +0100

    got premixing step2 to run

--- FastSimulation/Configuration/python/DataMixerPreMix_cff.py
+++ FastSimulation/Configuration/python/DataMixerPreMix_cff.py
@@ -6,3 +6,13 @@ mixData.GeneralTrackDigiCollectionDM = cms.string("generalTracks")
 mixData.GeneralTrackLabelSig = cms.InputTag("generalTracksBeforePreMixing")
 mixData.GeneralTrackPileInputTag = cms.InputTag("generalTracksBeforeMixing")
 mixData.hitProducer = cms.InputTag("famosSimHits")
+
+# get rid of sistrip and sipixel raw2digi modules run inside DataMixingModule
+for p in reversed(range(0,len(mixData.input.producers))):
+    module_type =  getattr(mixData.input.producers[p],"@module_type").value()
+    for _str in ["SiStrip","SiPixel"]:
+        if module_type.find(_str) == 0:
+            del mixData.input.producers[p]
+
+
+
