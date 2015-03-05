5e66155c874fb93fd7cc88f69d126ffff4318805
Marco Rovere <marco.rovere@cern.ch>
2015-03-05 18:40:49 +0100

    Add function to automatically compare old and new values

--- SimTracker/TrackerMaterialAnalysis/test/dumpFullXML.py
+++ SimTracker/TrackerMaterialAnalysis/test/dumpFullXML.py
@@ -13,16 +13,30 @@ def checkEnvironment():
         print 'CMSSW Environments not setup, quitting\n'
         sys.exit(CMSSW_NOT_SET)
 
-def getTrackerRecoMaterialCopy():
+def getTrackerRecoMaterialCopy(filename):
     tracker_reco_material = os.path.join(os.environ['CMSSW_RELEASE_BASE'],
                                          'src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml')
     if not os.path.exists(tracker_reco_material):
         print 'Something is wrong with the CMSSW installation. The file %s is missing. Quitting.\n' % tracker_reco_material
         sys.exit(TRACKER_MATERIAL_FILE_MISSING)
-    copy2(tracker_reco_material, './trackerRecoMaterial.xml')
+    copy2(tracker_reco_material, filename)
                                                 
-def main():
-    tracker_reco_material = './trackerRecoMaterial.xml'
+def produceXMLFromParameterFile():
+    """
+    Starting from the file parameters.xml produced by the
+    TrackingMaterialAnalyser via cmsRun, it writes out a new XML,
+    taking into account the proper names and grouping of detectors
+    together.
+
+    The skeleton of the XML is taken directly from the release the
+    user is currently using, i.e. from
+    $CMSSW_RELEASE_BASE/src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml.
+
+    A new file, named trackerRecoMaterialUpdated.xml, is saved in the
+    current directory.
+    """
+
+    tracker_reco_material = './trackerRecoMaterialFromRelease.xml'
     tracker_reco_material_updated = './parameters.xml'
     ET.register_namespace('', "http://www.cern.ch/cms/DDL")
     tree = ET.parse(tracker_reco_material)
@@ -45,7 +59,55 @@ def main():
                     print current_detector, parameter.attrib['name'], parameter.attrib['value']
     tree.write('trackerRecoMaterialUpdated.xml', encoding='UTF-8', xml_declaration=True)
 
+def compareNewXMLWithOld():
+    """
+    Comutes the difference between the old values, stored in the
+    central repository for the current release, i.e. from
+    $CMSSW_RELEASE_BASE/src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml,
+    and the new values that we assume are present in the same file
+    under the locally installed release, i.e. under
+    $CMSSW_BASE/src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml. No
+    check is performed to guarantee that the files are already there:
+    a missing file will result in an exception.
+
+    The output of this function is a formatted structured as:
+    ComponentsName KindOfParameter OldValue NewValue Difference
+    where the Difference is computed as (NewValue-OldValue)/OldValue.
+
+    Results are flushed at the terminal, nothing is saved.
+    """
+    
+    tracker_reco_material = './trackerRecoMaterialFromRelease.xml'
+    tracker_reco_material_updated = os.path.join(os.environ['CMSSW_BASE'],
+                                                 'src/Geometry/TrackerRecoData/data/trackerRecoMaterial.xml')
+    ET.register_namespace('', "http://www.cern.ch/cms/DDL")
+    tree = ET.parse(tracker_reco_material)
+    root = tree.getroot()
+    tree_updated = ET.parse(tracker_reco_material_updated)
+    root_updated = tree_updated.getroot()
+    sections = root.getchildren()
+
+    for spec_par in root.iter('%sSpecPar' % TAG_PREFIX):
+        current_detector = spec_par.attrib['name']
+        for parameter in spec_par.iter('%sParameter' % TAG_PREFIX):
+            updated_current_detector_node = root_updated.find(".//%sSpecPar[@name='%s']" % (TAG_PREFIX,current_detector))
+            if updated_current_detector_node:
+                for child in updated_current_detector_node:
+                    name = child.get('name', None)
+                    if name and name == parameter.attrib['name']:
+                        print "%s %s %s %s %f%%" % (current_detector,
+                                                    parameter.attrib['name'],
+                                                    parameter.attrib['value'],
+                                                    child.attrib['value'],
+                                                    (float(child.attrib['value'])-float(parameter.attrib['value']))
+                                                    /float(parameter.attrib['value'])*100.
+                                                    )
+            else:
+                print 'Element not found: %s' % current_detector
+    
+    
 if __name__ == '__main__':
     checkEnvironment()
-    getTrackerRecoMaterialCopy()
-    main()
+    getTrackerRecoMaterialCopy('trackerRecoMaterialFromRelease.xml')
+#    produceXMLFromParameterFile()
+    compareNewXMLWithOld()
