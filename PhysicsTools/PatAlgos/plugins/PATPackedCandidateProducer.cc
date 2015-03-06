25583c7374e0fa647b3523d140129d9d8e1442b3
Jason Lee <jason.lee@cern.ch>
2015-03-07 02:29:11 +0900

    removed bool for checking if collection exists

--- PhysicsTools/PatAlgos/plugins/PATPackedCandidateProducer.cc
+++ PhysicsTools/PatAlgos/plugins/PATPackedCandidateProducer.cc
@@ -94,9 +94,8 @@ void pat::PATPackedCandidateProducer::produce(edm::Event& iEvent, const edm::Eve
     edm::Handle<reco::PFCandidateFwdPtrVector> candsFromPVTight;
     iEvent.getByToken( CandsFromPVTight_, candsFromPVTight );
 
-    bool hasPuppiWeights = false;
     edm::Handle< edm::ValueMap<float> > puppiWeight;
-    if (iEvent.getByToken( PuppiWeight_, puppiWeight )) hasPuppiWeights = true;
+    iEvent.getByToken( PuppiWeight_, puppiWeight );
     
     std::vector<pat::PackedCandidate::PVAssoc> fromPV(cands->size(), pat::PackedCandidate::NoPV);
     for (const reco::PFCandidateFwdPtr &ptr : *candsFromPVLoose) {
@@ -235,7 +234,7 @@ void pat::PATPackedCandidateProducer::produce(edm::Event& iEvent, const edm::Eve
             outPtrP->back().setFromPV( fromPV[ic] );
         }
 	
-	if (hasPuppiWeights){
+	if (puppiWeight.isValid()){
 	  reco::PFCandidateRef pkref( cands, ic );
 	  outPtrP->back().setPuppiWeight( (*puppiWeight)[pkref]);
 	}
