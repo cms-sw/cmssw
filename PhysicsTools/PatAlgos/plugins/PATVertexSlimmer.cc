969330f6b354adea6bc01d88b3118960e1fed51d
Andrea <andrea.rizzi@cern.ch>
2015-03-10 20:57:58 +0100

    avoid conflict

--- PhysicsTools/PatAlgos/plugins/PATVertexSlimmer.cc
+++ PhysicsTools/PatAlgos/plugins/PATVertexSlimmer.cc
@@ -5,11 +5,11 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/ValueMap.h"
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/Common/interface/ValueMap.h"
 
 namespace pat {
     class PATVertexSlimmer : public edm::EDProducer {
