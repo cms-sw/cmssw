af1bde9b011bc54ce6f9369ed36f87bb0ce18b36
Lukas <lukas.vanelderen@cern.ch>
2015-03-10 21:21:12 +0100

    first iteration of corrections in reconstruction cfg

--- FastSimulation/Configuration/test/minbias_fast_cfg.py
+++ FastSimulation/Configuration/test/minbias_fast_cfg.py
@@ -19,7 +19,7 @@ process.load('Configuration.StandardSequences.Generator_cff')
 process.load('IOMC.EventVertexGenerators.VtxSmearedNominalCollision2015_cfi')
 process.load('GeneratorInterface.Core.genFilterSummary_cff')
 process.load('FastSimulation.Configuration.SimIdeal_cff')
-process.load('FastSimulation.Configuration.Reconstruction_Tk_cff')
+process.load('FastSimulation.Configuration.Reconstruction_BefMix_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
