bb2f1d68e8eafaf4292c025cd8797526f77fbc9f
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2015-03-10 15:47:36 +0100

    Correct L3 filter

--- HLTriggerOffline/SUSYBSM/python/SUSYBSM_Mu_HT_SingleLepton_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SUSYBSM_Mu_HT_SingleLepton_cff.py
@@ -12,7 +12,7 @@ SUSY_HLT_Mu_HT_SingleLepton = cms.EDAnalyzer('SUSY_HLT_SingleLepton',
                                              conversionCollection = cms.InputTag(''),
                                              beamSpot = cms.InputTag(''),
 
-                                             leptonFilter = cms.InputTag('hltL3crIsoL1sMu5L1f0L2f3QL3f15QL3crIsoRhoFiltered0p15IterTrk02','','HLT'),
+                                             leptonFilter = cms.InputTag('hltL3fL1sMu5L1f0L2f3QL3Filtered15QL3trkIsoFiltered0p09','','HLT'),
                                              hltHt = cms.InputTag('hltPFHT','','HLT'),
                                              hltMet = cms.InputTag(''),
                                              hltJets = cms.InputTag(''),
@@ -23,7 +23,7 @@ SUSY_HLT_Mu_HT_SingleLepton = cms.EDAnalyzer('SUSY_HLT_SingleLepton',
 
                                              hltProcess = cms.string('HLT'),
 
-                                             triggerPath = cms.string('HLT_Mu15_IsoVVVL_PFHT600'),
+                                             triggerPath = cms.string('HLT_Mu15_IsoVVVL_PFHT600_v'),
                                              triggerPathAuxiliary = cms.string('HLT_IsoMu24_eta2p1_v'),
                                              triggerPathLeptonAuxiliary = cms.string('HLT_PFHT350_PFMET120_NoiseCleaned_v'),
 
