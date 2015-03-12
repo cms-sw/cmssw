9d8e7a5c314f22fd1ef403abdc130f3b442dc8d5
Lukas <lukas.vanelderen@cern.ch>
2015-03-12 16:54:38 +0100

    fixes after second comparison between old and  over new sequences

--- FastSimulation/Configuration/python/Reconstruction_AftMix_cff.py
+++ FastSimulation/Configuration/python/Reconstruction_AftMix_cff.py
@@ -1,14 +1,25 @@
-import FWCore.ParameterSet.Config as cms
-from FastSimulation.Configuration.CfgUtilities import *
+#############################
+# This cfg configures the part of reconstruction 
+# in FastSim to be done after event mixing
+# FastSim mixes tracker information on reconstruction level,
+# so tracks are recontructed before mixing.
+# At present, only the generalTrack collection, produced with iterative tracking is mixed.
+#############################
 
-from Configuration.StandardSequences.Reconstruction_cff import *
+import FWCore.ParameterSet.Config as cms
 
 ###########################################
-# no castor in FastSim
+# import the standard reconstruction sequences
+###########################################
+from Configuration.StandardSequences.Reconstruction_cff import *
+
+
+###########################################
+# no castor / zdc in FastSim
 ###########################################
 localreco.remove(castorreco)
 globalreco.remove(CastorFullReco)
-
+hcalLocalRecoSequence.remove(zdcreco)
 
 ###########################################
 # FastSim does the beamspot before mixing
@@ -30,6 +41,7 @@ globalreco.remove(trackingGlobalReco)
 # list names of tracking modules to be deleted
 # weneed a full deletion to avoid interference with fast tracking
 # that uses the same names as full tracking for corresponding collections
+from FastSimulation.Configuration.CfgUtilities import getSeqEntryNames,removeSeqEntriesEveryWhere
 namesObjsToDel = set(getSeqEntryNames(trackerlocalreco,locals()))
 namesObjsToDel = namesObjsToDel.union(getSeqEntryNames(trackingGlobalReco,locals()))
 namesObjsToDel = namesObjsToDel.union(getSeqEntryNames(recopixelvertexing,locals()))
@@ -41,7 +53,7 @@ removeSeqEntriesEveryWhere(trackerlocalreco,locals())
 removeSeqEntriesEveryWhere(trackingGlobalReco,locals())
 removeSeqEntriesEveryWhere(recopixelvertexing,locals())
 
-# now do the actual deletion
+# now do the actual deletions
 for name in namesObjsToDel:
     exec("del " + name)
 
@@ -68,7 +80,7 @@ from FastSimulation.Tracking.globalCombinedSeeds_cfi import newCombinedSeeds
 # tracker driven electron seeds depend on the generalTracks trajectory collection
 # However, in FastSim jobs, trajectories are only available for signal tracks
 # Therefore we let the seeds depend on the 'before mixing' generalTracks collection
-trackerDrivenElectronSeeds.TkColList = ['generalTracksBeforeMixing']
+trackerDrivenElectronSeeds.TkColList = [cms.InputTag('generalTracksBeforeMixing')]
 # TODO: add the fixes to the TrackRefs in the tracker-driven seeds
 
 # the conversion producer depends on trajectories
@@ -91,22 +103,46 @@ electronGsfTracks.TTRHBuilder = "WithoutRefit"
 # seems not to be used and causes crashes in FastSim
 egammaHighLevelRecoPrePF.remove(uncleanedOnlyElectronSequence)
 
+# having this in FastSim requires significant work
+# all related tracking needs to be emulated
+egammareco.remove(conversionSequence)
+egammaHighLevelRecoPrePF.remove(conversionSequence)
+
+# wasn't there before and is not used (?)
+
+ecalRecHitSequence.remove(ecalTPSkim)
 
 ##########################################
 # FastSim changes to muon reconstruction
 ##########################################
-# because there is no initialStep track collection with both pu and signal tracks:
-earlyDisplacedMuons.inputCollectionLabels[0] = "generalTracks"
 
-# no veto on cosmic muons because it relies on track finding
-removeSeqEntriesEveryWhere(muoncosmichighlevelreco,locals())
+# not commisoned and not relevant in FastSim (?):
+globalreco.remove(muoncosmicreco)
+highlevelreco.remove(muoncosmichighlevelreco)
 muons.FillCosmicsIdMap = False
 
+# not commisoned and not relevant in FastSim (?):
+globalmuontracking.remove(displacedGlobalMuonTracking)
+standalonemuontracking.remove(displacedMuonSeeds)
+standalonemuontracking.remove(displacedStandAloneMuons)
+
+# not commisoned and not relevant in FastSim (?):
+muonGlobalReco.remove(muonreco_with_SET)
+
+# not commisoned and not relevant in FastSim (?):
+muonGlobalReco.remove(muonSelectionTypeSequence)
+muons.FillSelectorMaps = False
+
 # FastSim has no template fit on tracker hits
-GlobalMuonRefitter.TrackerRecHitBuilder = 'WithoutRefit'
+globalMuons.GLBTrajBuilderParameters.GlbRefitterParameters.TrackerRecHitBuilder = 'WithoutRefit'
+globalMuons.GLBTrajBuilderParameters.TrackerRecHitBuilder = 'WithoutRefit'
+globalMuons.GLBTrajBuilderParameters.TrackTransformer.TrackerRecHitBuilder = 'WithoutRefit'
 tevMuons.RefitterParameters.TrackerRecHitBuilder = 'WithoutRefit'
 
-# FastSim uses a faster propagator
+# FastSim doesn't use Runge Kute for propagation
+globalMuons.GLBTrajBuilderParameters.GlbRefitterParameters.Propagator = 'SmartPropagatorAny'
+globalMuons.GLBTrajBuilderParameters.GlobalMuonTrackMatcher.Propagator = 'SmartPropagator'
+globalMuons.GLBTrajBuilderParameters.TrackTransformer.Propagator = 'SmartPropagatorAny'
 GlbMuKFFitter.Propagator = "SmartPropagatorAny"
 GlobalMuonRefitter.Propagator = "SmartPropagatorAny"
 KFSmootherForMuonTrackLoader.Propagator = "SmartPropagatorAny"
@@ -114,18 +150,24 @@ KFSmootherForRefitInsideOut.Propagator = "SmartPropagatorAny"
 KFFitterForRefitInsideOut.Propagator = "SmartPropagatorAny"
 tevMuons.RefitterParameters.Propagator = "SmartPropagatorAny"
 
+
 ##########################################
 # FastSim changes to jet/met reconstruction
 ##########################################
-# because it crashes and is not used in FastSim samples as far as we know
+
+# not commisoned and not relevant in FastSim (?):
 jetHighLevelReco.remove(recoJetAssociationsExplicit)
 
-# depends on cosmic muons reco, which doesn't run in FastSim
+# not commisoned and not relevant in FastSim (?):
 metreco.remove(BeamHaloId)
+
+# not commisoned and not relevant in FastSim (?):
+metrecoPlusHCALNoise.remove(hcalnoise)
 
 ##########################################
 # The PF Patch
 ##########################################
+
 # throws random neutral hadrons in the detector to fix jet response
 # we should get rid of this asap, but the source of the issue is not pointed down
 particleFlowTmpTmp = particleFlowTmp
@@ -133,14 +175,38 @@ from FastSimulation.ParticleFlow.FSparticleFlow_cfi import FSparticleFlow as par
 particleFlowTmp.pfCandidates = cms.InputTag("particleFlowTmpTmp")
 particleFlowReco.insert(particleFlowReco.index(particleFlowTmpTmp)+1,particleFlowTmp)
 
+
+##########################################
+# Calo rechits
+##########################################
+
+# not commisoned and not relevant in FastSim (?):
+reducedEcalRecHitsSequence.remove(seldigis)
+ecalRecHitSequence.remove(ecalCompactTrigPrim)
+
+
 ###########################################
 # sequences that are not part of any path
-# but cause python compilation errors
+# but cause python compilation errors after the full track sequence removal
 ###########################################
+
 del ckftracks_plus_pixelless
 del ckftracks_woBH
 del reconstruction_fromRECO
 del ckftracks_wodEdX
+
+###########################################
+# deleting some services that are not used
+###########################################
+
+del BeamHaloMPropagatorAlong
+del BeamHaloMPropagatorOpposite
+del BeamHaloPropagatorAlong
+del BeamHaloPropagatorAny
+del BeamHaloPropagatorOpposite
+del BeamHaloSHPropagatorAlong
+del BeamHaloSHPropagatorAny
+del BeamHaloSHPropagatorOpposite
 
 ############################################
 # the final reconstruction sequence
--- FastSimulation/Configuration/python/Reconstruction_BefMix_cff.py
+++ FastSimulation/Configuration/python/Reconstruction_BefMix_cff.py
@@ -1,10 +1,27 @@
+#############################
+# This cfg configures the part of reconstruction 
+# in FastSim to be done before event mixing
+# FastSim mixes tracker information on reconstruction level,
+# so tracks are recontructed before mixing.
+# At present, only the generalTrack collection, produced with iterative tracking is mixed.
+#############################
+
 import FWCore.ParameterSet.Config as cms
 
-# probably this has to be moved
-# why is FastSim actually using this?
+
+# iterative tracking relies on the beamspot
+from RecoVertex.BeamSpotProducer.BeamSpot_cff import offlineBeamSpot
+
+# and of course it needs tracker hits
+from FastSimulation.TrackingRecHitProducer.SiTrackerGaussianSmearingRecHitConverter_cfi import siTrackerGaussianSmearingRecHits
+
+# FastSim stores the IdealMagneticFieldRecord and the TrackerInteractionGeometryRecord in a particular structure
+# This extra layer is probably more confusing than it is useful and we should consider to remove it
 from FastSimulation.ParticlePropagator.MagneticFieldMapESProducer_cfi import *
 
-# get the track fitters, consider moving and renaming
+# confusing name for the file that imports 
+# the fitters used by the TrackProducer
+# 
 from FastSimulation.Tracking.GSTrackFinalFitCommon_cff import *
 
 # we need this stuff to prevent MeasurementTrackerEvent to crash
@@ -15,14 +32,11 @@ from RecoTracker.MeasurementDet.MeasurementTrackerESProducer_cff import *
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import TransientTrackBuilderESProducer
 from RecoTracker.TkNavigation.NavigationSchoolESProducer_cfi import navigationSchoolESProducer
 
-from RecoVertex.BeamSpotProducer.BeamSpot_cff import offlineBeamSpot
-from FastSimulation.TrackingRecHitProducer.SiTrackerGaussianSmearingRecHitConverter_cfi import siTrackerGaussianSmearingRecHits
-
 
 from FastSimulation.Tracking.IterativeTracking_cff import *
 from TrackingTools.TrackFitters.TrackFitters_cff import *
 
-fastTkReconstruction = cms.Sequence(
+reconstruction_befmix = cms.Sequence(
     offlineBeamSpot
     * siTrackerGaussianSmearingRecHits
     * iterTracking
