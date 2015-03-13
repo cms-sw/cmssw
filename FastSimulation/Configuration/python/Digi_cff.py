312774195ae854e9c54c12312c05689f2a2a9f8a
Lukas <lukas.vanelderen@cern.ch>
2015-03-13 15:15:31 +0100

    fixes after another round of comparions between old and new cfg

--- FastSimulation/Configuration/python/Digi_cff.py
+++ FastSimulation/Configuration/python/Digi_cff.py
@@ -1,8 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
+# the actual digi sequence
 from Configuration.StandardSequences.Digi_cff import *
 
 simMuonCSCDigis.InputCollection = 'MuonSimHitsMuonCSCHits'
 simMuonDTDigis.InputCollection = 'MuonSimHitsMuonDTHits'
 simMuonRPCDigis.InputCollection = 'MuonSimHitsMuonRPCHits'
+calDigi.remove(castorDigiSequence)
 
+# extend it with some digi2raw
+from Configuration.StandardSequences.DigiToRaw_cff import ecalPacker,esDigiToRaw,hcalRawData,rawDataCollector
+for _entry in [cms.InputTag("SiStripDigiToRaw"), cms.InputTag("castorRawData"),cms.InputTag("siPixelRawData")]:
+    rawDataCollector.RawCollectionList.remove(_entry)
+
+# extend it with some raw2digi
+from Configuration.StandardSequences.RawToDigi_cff import ecalPreshowerDigis,ecalDigis,hcalDigis
+
+digiToRawToDigi = cms.Sequence(ecalPacker+esDigiToRaw+hcalRawData+rawDataCollector+ecalPreshowerDigis+ecalDigis+hcalDigis)
+pdigi.replace(calDigi,calDigi+digiToRawToDigi)
+
+# TODO: understand whether this digi2raw+raw2digi is useful
