c5441017696d77e290659caf9588996e29d244c5
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-03-16 15:55:52 +0100

    Move all InputTags to configuration in V0Validator

--- Validation/RecoVertex/python/v0validator_cfi.py
+++ Validation/RecoVertex/python/v0validator_cfi.py
@@ -4,5 +4,9 @@ v0Validator = cms.EDAnalyzer('V0Validator',
     DQMRootFileName = cms.untracked.string(''),
     kShortCollection = cms.untracked.InputTag('generalV0Candidates:Kshort'),
     lambdaCollection = cms.untracked.InputTag('generalV0Candidates:Lambda'),
+    trackAssociatorMap = cms.untracked.InputTag("trackingParticleRecoTrackAsssociation"),
+    trackingParticleCollectionEff = cms.untracked.InputTag("mix", "MergedTrackTruth"),
+    trackCollection = cms.untracked.InputTag("generalTracks"),
+    vertexCollection = cms.untracked.InputTag("offlinePrimaryVertices"),
     dirName = cms.untracked.string('Vertexing/V0V')
 )
