a93bd32d90b634c7276736ad52b48bc81f903cc2
Salavat <abdullin@mail.cern.ch>
2015-03-16 13:45:08 +0100

    adding updated HF SL for Run 1

---
+++ SimG4CMS/Calo/python/customizeHF.py
@@ -0,0 +1,11 @@
+def customise_HFSLrun1(process):
+    #enable Run 1 HF shower library
+    process.g4SimHits.HCalSD.UseShowerLibrary   = True
+    process.g4SimHits.HCalSD.UseParametrize     = False
+    process.g4SimHits.HCalSD.UsePMTHits         = False
+    process.g4SimHits.HCalSD.UseFibreBundleHits = False
+    process.g4SimHits.HFShowerLibrary.FileName  = 'SimG4CMS/Calo/data/HFShowerLibrary_oldpmt_noatt_eta4_16en.root'
+    process.g4SimHits.HFShowerLibrary.BranchPost= ''
+    process.g4SimHits.HFShowerLibrary.BranchPre = ''
+    process.g4SimHits.HFShowerLibrary.BranchEvt = ''
+    return process
