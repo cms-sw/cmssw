ee6bd7c9726d5ada1e3a7722f33d98816c0a8de2
Salavat <abdullin@mail.cern.ch>
2015-03-17 12:15:32 +0100

    changing to v3 HF SL again

--- SimG4CMS/Calo/python/customizeHF.py
+++ SimG4CMS/Calo/python/customizeHF.py
@@ -4,7 +4,7 @@ def customise_HFSLrun1(process):
     process.g4SimHits.HCalSD.UseParametrize     = False
     process.g4SimHits.HCalSD.UsePMTHits         = False
     process.g4SimHits.HCalSD.UseFibreBundleHits = False
-    process.g4SimHits.HFShowerLibrary.FileName  = 'SimG4CMS/Calo/data/HFShowerLibrary_oldpmt_noatt_eta4_16en.root'
+    process.g4SimHits.HFShowerLibrary.FileName  = 'SimG4CMS/Calo/data/HFShowerLibrary_oldpmt_noatt_eta4_16en_v3.root'  
     process.g4SimHits.HFShowerLibrary.BranchPost= ''
     process.g4SimHits.HFShowerLibrary.BranchPre = ''
     process.g4SimHits.HFShowerLibrary.BranchEvt = ''
