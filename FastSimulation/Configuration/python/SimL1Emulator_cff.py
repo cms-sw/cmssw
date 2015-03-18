934084001dba77e4f46c16d3f903d402ea253d18
Lukas <lukas.vanelderen@cern.ch>
2015-03-18 12:02:14 +0100

    integrating new cfgs in cmsDriver

--- FastSimulation/Configuration/python/SimL1Emulator_cff.py
+++ FastSimulation/Configuration/python/SimL1Emulator_cff.py
@@ -1,2 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.SimL1Emulator_cff import *
+
+for _entry in [SimL1TechnicalTriggers]:
+    SimL1Emulator.remove(_entry)
+
+del simGtDigis.TechnicalTriggersInputTags
+
+# TODO: check impact on digi cfg with respect to old FastSim cfg
