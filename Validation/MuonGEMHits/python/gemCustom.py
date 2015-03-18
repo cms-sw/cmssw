610c5a3ece716f2ea1e3c594506fcaccbc8446e9
Geonmo <ry840901@gmail.com>
2015-03-18 15:37:08 +0100

    Add digi setup to custom and Generate.sh

--- Validation/MuonGEMHits/python/gemCustom.py
+++ Validation/MuonGEMHits/python/gemCustom.py
@@ -1,9 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 def customise2023(process):
+  if hasattr(process,'digitisation_step') :
+    process=customise_digitization(process)
   if hasattr(process,'dqmHarvesting'):
     process=customise_harvesting(process)
   if hasattr(process,'validation_step'):
     process=customise_Validation(process)
+  return process
+def customise_digitization(process):
+  from SimMuon.GEMDigitizer.customizeGEMDigi import customize_digi_addGEM_muon_only
+  process = customize_digi_addGEM_muon_only(process)
+  process.simMuonGEMDigis.mixLabel = cms.string("mix")
+  #process.simMuonME0Digis.mixLabel = cms.string("mix")
+  process.digitisation_step.remove(process.simMuonRPCDigis)
   return process
 
 def customise_Validation(process):
