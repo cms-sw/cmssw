99700486dd1ec073ccb4a845c46a150757c7ddf8
matthieu <matthieu.marionneau@cern.ch>
2015-03-19 08:35:28 +0100

    fix tau charged fraction and computation of L1 corrections in the mva met code

--- RecoMET/METPUSubtraction/plugins/PFMETProducerMVA.cc
+++ RecoMET/METPUSubtraction/plugins/PFMETProducerMVA.cc
@@ -315,20 +315,20 @@ double PFMETProducerMVA::chargedEnFrac(const reco::Candidate *iCand,
   const reco::PFTau *lPFTau = 0; 
   lPFTau = dynamic_cast<const reco::PFTau*>(iCand);
   if(lPFTau != nullptr) { 
-    for (UInt_t i0 = 0; i0 < lPFTau->signalPFCands().size(); i0++) { 
-      lPtTot += (lPFTau->signalPFCands())[i0]->pt(); 
-      if((lPFTau->signalPFCands())[i0]->charge() == 0) continue;
-      lPtCharged += (lPFTau->signalPFCands())[i0]->pt(); 
+    for (UInt_t i0 = 0; i0 < lPFTau->signalCands().size(); i0++) { 
+      lPtTot += (lPFTau->signalCands())[i0]->pt(); 
+      if((lPFTau->signalCands())[i0]->charge() == 0) continue;
+      lPtCharged += (lPFTau->signalCands())[i0]->pt(); 
     }
   } 
   else { 
     const pat::Tau *lPatPFTau = nullptr; 
     lPatPFTau = dynamic_cast<const pat::Tau*>(iCand);
     if(lPatPFTau != nullptr) { 
-      for (UInt_t i0 = 0; i0 < lPatPFTau->signalPFCands().size(); i0++) { 
-	lPtTot += (lPatPFTau->signalPFCands())[i0]->pt(); 
-	if((lPatPFTau->signalPFCands())[i0]->charge() == 0) continue;
-	lPtCharged += (lPatPFTau->signalPFCands())[i0]->pt(); 
+      for (UInt_t i0 = 0; i0 < lPatPFTau->signalCands().size(); i0++) { 
+	lPtTot += (lPatPFTau->signalCands())[i0]->pt(); 
+	if((lPatPFTau->signalCands())[i0]->charge() == 0) continue;
+	lPtCharged += (lPatPFTau->signalCands())[i0]->pt(); 
       }
     }
   }
