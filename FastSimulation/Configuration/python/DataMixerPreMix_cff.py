fc69434327098e300e8429d308ad6f13f4d4614f
Lukas <lukas.vanelderen@cern.ch>
2015-03-19 15:21:33 +0100

    got trigger to run without copying track and calo rechit collections

--- FastSimulation/Configuration/python/DataMixerPreMix_cff.py
+++ FastSimulation/Configuration/python/DataMixerPreMix_cff.py
@@ -19,14 +19,42 @@ for p in reversed(range(0,len(mixData.input.producers))):
             del mixData.input.producers[p]
 
 # extend it with some digi2raw
-from Configuration.StandardSequences.DigiToRaw_cff import ecalPacker,esDigiToRaw,hcalRawData,rawDataCollector
+from Configuration.StandardSequences.DigiToRawDM_cff import ecalPacker,esDigiToRaw,hcalRawData,rawDataCollector
 for _entry in [cms.InputTag("SiStripDigiToRaw"), cms.InputTag("castorRawData"),cms.InputTag("siPixelRawData")]:
     rawDataCollector.RawCollectionList.remove(_entry)
 
 # extend it with some raw2digi
 from Configuration.StandardSequences.RawToDigi_cff import ecalPreshowerDigis,ecalDigis,hcalDigis
 
+
+# aliases for muons
+muonDTDigis = cms.EDAlias(
+    mixData = cms.VPSet(
+        cms.PSet(type = cms.string("DTLayerIdDTDigiMuonDigiCollection"))
+        )
+    )
+muonRPCDigis = cms.EDAlias(
+    mixData = cms.VPSet(
+        cms.PSet(type = cms.string("RPCDetIdRPCDigiMuonDigiCollection"))
+        )
+    )
+
+muonCSCDigis = cms.EDAlias(
+    mixData = cms.VPSet(
+        cms.PSet(
+            type = cms.string("CSCDetIdCSCWireDigiMuonDigiCollection"),
+            fromProductInstance = cms.string("MuonCSCWireDigisDM"),
+            ),
+        cms.PSet(
+            type = cms.string("CSCDetIdCSCStripDigiMuonDigiCollection"),
+            fromProductInstance = cms.string("MuonCSCStripDigisDM"),
+            )
+        )
+    )
+
+from FastSimulation.Tracking.GeneralTracksAlias_cfi import generalTracksAliasInfo
+generalTracksAliasInfo.key = "mixData"
+generalTracks = cms.EDAlias(**{generalTracksAliasInfo.key.value():generalTracksAliasInfo.value})
+
 calDigiToRawToDigi = cms.Sequence(ecalPacker+esDigiToRaw+hcalRawData+rawDataCollector+ecalPreshowerDigis+ecalDigis+hcalDigis)
-pdatamix.replace(postDMDigi,calDigi+calDigiToRawToDigi)
-
-
+pdatamix.replace(postDMDigi,postDMDigi+calDigiToRawToDigi)
