89f5f0326bf6633595abda9ebed3325fa0dbceab
Colin <colin.bernet@cern.ch>
2015-03-19 11:37:59 +0100

    bug fix: setup object passed to analyzer beginLoop

--- PhysicsTools/HeppyCore/python/framework/looper.py
+++ PhysicsTools/HeppyCore/python/framework/looper.py
@@ -219,7 +219,7 @@ class Looper(object):
         self.iEvent = iEv
         for i,analyzer in enumerate(self.analyzers):
             if not analyzer.beginLoopCalled:
-                analyzer.beginLoop()
+                analyzer.beginLoop(self.setup)
             start = timeit.default_timer()
             ret = analyzer.process( self.event )
             if self.timeReport:
