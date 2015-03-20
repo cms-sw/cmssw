1bd6aa8b5e0d566b8b526da8f63edb0cca9c77f9
Lukas <lukas.vanelderen@cern.ch>
2015-03-20 14:54:37 +0100

    removed digi packing and unpacking from DataMix cfg, added EDAliases such that digi collections get names expected by HLT and RECO

--- FastSimulation/Configuration/python/DataMixerPreMix_cff.py
+++ FastSimulation/Configuration/python/DataMixerPreMix_cff.py
@@ -9,25 +9,30 @@ import FastSimulation.Tracking.recoTrackAccumulator_cfi
 mixData.tracker = FastSimulation.Tracking.recoTrackAccumulator_cfi.recoTrackAccumulator.clone()
 mixData.tracker.pileUpTracks = cms.InputTag("mix","generalTracks")
 mixData.tracker.pileUpMVAValues = cms.InputTag("mix","generalTracksMVAVals")
-mixData.hitProducer = cms.InputTag("famosSimHits")
+mixData.hitsProducer = "famosSimHits"
 
-# get rid of sistrip and sipixel raw2digi modules run inside DataMixingModule
-for p in reversed(range(0,len(mixData.input.producers))):
-    module_type =  getattr(mixData.input.producers[p],"@module_type").value()
-    for _str in ["SiStrip","SiPixel"]:
-        if module_type.find(_str) == 0:
-            del mixData.input.producers[p]
+# give digi collections the names expected by RECO and HLT
+ecalPreshowerDigis = cms.EDAlias(
+    DMEcalPreshowerDigis = cms.VPSet(
+        cms.PSet(type = cms.string("ESDigiCollection"))
+        )
+    )
 
-# extend it with some digi2raw
-from Configuration.StandardSequences.DigiToRawDM_cff import ecalPacker,esDigiToRaw,hcalRawData,rawDataCollector
-for _entry in [cms.InputTag("SiStripDigiToRaw"), cms.InputTag("castorRawData"),cms.InputTag("siPixelRawData")]:
-    rawDataCollector.RawCollectionList.remove(_entry)
+ecalDigis = cms.EDAlias(
+    DMEcalDigis = cms.VPSet(
+        cms.PSet(type = cms.string("EBDigiCollection")),
+        cms.PSet(type = cms.string("EEDigiCollection"))
+        )
+    )
 
-# extend it with some raw2digi
-from Configuration.StandardSequences.RawToDigi_cff import ecalPreshowerDigis,ecalDigis,hcalDigis
+hcalDigis = cms.EDAlias(
+    DMHcalDigis = cms.VPSet(
+        cms.PSet(type = cms.string("HBHEDataFramesSorted")),
+        cms.PSet(type = cms.string("HFDataFramesSorted")),
+        cms.PSet(type = cms.string("HODataFramesSorted"))
+        )
+    )
 
-
-# aliases for muons
 muonDTDigis = cms.EDAlias(
     mixData = cms.VPSet(
         cms.PSet(type = cms.string("DTLayerIdDTDigiMuonDigiCollection"))
@@ -51,10 +56,6 @@ muonCSCDigis = cms.EDAlias(
             )
         )
     )
-
 from FastSimulation.Tracking.GeneralTracksAlias_cfi import generalTracksAliasInfo
 generalTracksAliasInfo.key = "mixData"
 generalTracks = cms.EDAlias(**{generalTracksAliasInfo.key.value():generalTracksAliasInfo.value})
-
-calDigiToRawToDigi = cms.Sequence(ecalPacker+esDigiToRaw+hcalRawData+rawDataCollector+ecalPreshowerDigis+ecalDigis+hcalDigis)
-pdatamix.replace(postDMDigi,postDMDigi+calDigiToRawToDigi)
