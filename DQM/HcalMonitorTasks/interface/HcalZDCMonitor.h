a85f787db0f8e441a14ffa9b83c3bd4709630322
Quan Wang <BetterWang@gmail.com>
2015-03-20 14:14:02 +0100

    initial port from 53X
    update
    
    add new histo

--- DQM/HcalMonitorTasks/interface/HcalZDCMonitor.h
+++ DQM/HcalMonitorTasks/interface/HcalZDCMonitor.h
@@ -16,54 +16,80 @@
 
 class HcalZDCMonitor: public HcalBaseMonitor
 {
- public:
-  HcalZDCMonitor();
-  ~HcalZDCMonitor();
-  void setup(const edm::ParameterSet& ps, DQMStore::IBooker& ib);
-  void processEvent(const ZDCDigiCollection& digi,
-                    const ZDCRecHitCollection& rechit);
-  void reset();
-  void endLuminosityBlock(void);
- private:
-  double getTime(const std::vector<double>& fData, unsigned int ts_min, unsigned int ts_max, double& fSum);
-  //int getTSMax(std::vector<double> fData);
-  //bool isGood(std::vector<double> fData, double fCut, double fPercentage);
+public:
+	HcalZDCMonitor();
+	~HcalZDCMonitor();
+	void setup(const edm::ParameterSet& ps, DQMStore::IBooker& ib);
+	void processEvent(const ZDCDigiCollection& digi,
+			const ZDCRecHitCollection& rechit);
+	void reset();
+	void endLuminosityBlock(void);
+private:
+	double getTime(const std::vector<double>& fData, unsigned int ts_min, unsigned int ts_max, double& fSum);
+	//int getTSMax(std::vector<double> fData);
+	//bool isGood(std::vector<double> fData, double fCut, double fPercentage);
 
-  bool checkZDC_;
-  int NumBadZDC;
-  MonitorElement* ProblemsVsLB_ZDC;
+	bool checkZDC_;
+	int NumBadZDC;
+	MonitorElement* ProblemsVsLB_ZDC;
 
-  const HcalQIEShape* shape_;
-  const HcalQIECoder* channelCoder_;
-  HcalCalibrations calibs_;
-  int ievt_;
-  //int zdc_checkNevents_;
-  MonitorElement* meEVT_;
-  MonitorElement* h_2D_saturation;
-  MonitorElement* h_2D_charge;
-  MonitorElement* h_2D_TSMean;
-  MonitorElement* h_2D_RecHitEnergy;
-  MonitorElement* h_2D_RecHitTime;
-  MonitorElement* h_ZDCP_EM_Pulse[5];
-  MonitorElement* h_ZDCM_EM_Pulse[5];
-  MonitorElement* h_ZDCP_EM_Charge[5];
-  MonitorElement* h_ZDCM_EM_Charge[5];
-  MonitorElement* h_ZDCP_EM_TSMean[5];
-  MonitorElement* h_ZDCM_EM_TSMean[5];
-  MonitorElement* h_ZDCP_HAD_Pulse[4];
-  MonitorElement* h_ZDCM_HAD_Pulse[4];
-  MonitorElement* h_ZDCP_HAD_Charge[4];
-  MonitorElement* h_ZDCM_HAD_Charge[4];
-  MonitorElement* h_ZDCP_HAD_TSMean[4];
-  MonitorElement* h_ZDCM_HAD_TSMean[4];
-  MonitorElement* h_ZDCP_EM_RecHitEnergy[5];
-  MonitorElement* h_ZDCM_EM_RecHitEnergy[5];
-  MonitorElement* h_ZDCP_EM_RecHitTiming[5];
-  MonitorElement* h_ZDCM_EM_RecHitTiming[5];
-  MonitorElement* h_ZDCP_HAD_RecHitEnergy[4];
-  MonitorElement* h_ZDCM_HAD_RecHitEnergy[4];
-  MonitorElement* h_ZDCP_HAD_RecHitTiming[4];
-  MonitorElement* h_ZDCM_HAD_RecHitTiming[4];
+	int NLumiBlocks_;
+
+	std::vector<double> ChannelWeighting_; //Quality index(QI) see description below
+
+	const HcalQIEShape* shape_;
+	const HcalQIECoder* channelCoder_;
+	HcalCalibrations calibs_;
+	int ievt_;
+	//int zdc_checkNevents_;
+	MonitorElement* meEVT_;
+	MonitorElement* h_2D_saturation;
+	MonitorElement* h_2D_charge;
+	MonitorElement* h_2D_TSMean;
+	MonitorElement* h_2D_RecHitEnergy;
+	MonitorElement* h_2D_RecHitTime;
+	MonitorElement* h_ZDCP_EM_Pulse[5];
+	MonitorElement* h_ZDCM_EM_Pulse[5];
+	MonitorElement* h_ZDCP_EM_Charge[5];
+	MonitorElement* h_ZDCM_EM_Charge[5];
+	MonitorElement* h_ZDCP_EM_TSMean[5];
+	MonitorElement* h_ZDCM_EM_TSMean[5];
+	MonitorElement* h_ZDCP_HAD_Pulse[4];
+	MonitorElement* h_ZDCM_HAD_Pulse[4];
+	MonitorElement* h_ZDCP_HAD_Charge[4];
+	MonitorElement* h_ZDCM_HAD_Charge[4];
+	MonitorElement* h_ZDCP_HAD_TSMean[4];
+	MonitorElement* h_ZDCM_HAD_TSMean[4];
+	MonitorElement* h_ZDCP_EM_RecHitEnergy[5];
+	MonitorElement* h_ZDCM_EM_RecHitEnergy[5];
+	MonitorElement* h_ZDCP_EM_RecHitTiming[5];
+	MonitorElement* h_ZDCM_EM_RecHitTiming[5];
+	MonitorElement* h_ZDCP_HAD_RecHitEnergy[4];
+	MonitorElement* h_ZDCM_HAD_RecHitEnergy[4];
+	MonitorElement* h_ZDCP_HAD_RecHitTiming[4];
+	MonitorElement* h_ZDCM_HAD_RecHitTiming[4];
+
+
+	MonitorElement* ZDC_Digi_Errors;
+	MonitorElement* ZDC_DigiErrorsVsLS;
+	MonitorElement* ZDC_DigiErrors_DVER;
+	MonitorElement* ZDC_DigiErrors_CAPID;
+
+	MonitorElement* ZDC_Hot_Channel_Errors;
+	MonitorElement* ZDC_HotChannelErrorsVsLS;
+
+	MonitorElement* ZDC_Cold_Channel_Errors;
+	MonitorElement* ZDC_ColdChannelErrorsVsLS;
+
+	MonitorElement* ZDC_Dead_Channel_Errors;
+	MonitorElement* ZDC_DeadChannelErrorsVsLS;
+
+	MonitorElement* ZDC_TotalChannelErrors;
+
+	// Quality index(QI) per LS...QI is a number between 0 and 1. Each ZDC channel is assigned a custom weighting factor, found in /python/HcalZDCMonitor_cfi.py. This weighting factor is representative of the "value" of that channel to data taking. For example, HAD1 is more valuable than HAD4, this is because HAD1 is closer to the IP and will absorb more of the energy of the neutron, so if it is broken, we care more about it being broken than if HAD4 is broken. If all channels are working properly in a given ZDC (ZDC+/-),the QI for that ZDC will be 1. If a channel is deemed malfunctional for a given LS, then the assigned weightfactor will be subtracted from 1.
+	MonitorElement* PZDC_QualityIndexVsLB_;
+	MonitorElement* NZDC_QualityIndexVsLB_;
+	MonitorElement* EventsVsLS;
 };
 
 #endif
