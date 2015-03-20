b5a6a6d142eacc774b2a55cb68446ab0e65124b7
Koushik <koushik.m@niser.ac.in>
2015-03-20 16:30:54 +0100

    HLT pathname change

--- HLTriggerOffline/SUSYBSM/python/SUSYBSM_HLT_HT_DoubleElectron_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SUSYBSM_HLT_HT_DoubleElectron_cff.py
@@ -1,16 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 SUSY_HLT_HT_DoubleEle = cms.EDAnalyzer("SUSY_HLT_DoubleEle_Hadronic",
-  trigSummary = cms.InputTag('hltTriggerSummaryAOD','','HLT'),
+  trigSummary = cms.InputTag("hltTriggerSummaryAOD",'', 'HLT'), #to use with test sample
+  #trigSummary = cms.InputTag("hltTriggerSummaryAOD"),
   ElectronCollection = cms.InputTag("gedGsfElectrons"),
   pfJetCollection = cms.InputTag("ak4PFJetsCHS"),
   caloJetCollection = cms.InputTag("ak4CaloJets"),
-  TriggerResults = cms.InputTag('TriggerResults','','HLT'),
+  TriggerResults = cms.InputTag('TriggerResults','','HLT'), #to use with test sample
+  #TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   HLTProcess = cms.string('HLT'),
-  TriggerPath = cms.string('HLT_DoubleEle8_CaloIdL_TrkIdVL_Mass8_PFHT300_v'),
+  TriggerPath = cms.string('HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v1'),
   TriggerPathAuxiliaryForElectron = cms.string('HLT_PFHT900_v'),
   TriggerPathAuxiliaryForHadronic = cms.string('HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v'),
-  TriggerFilter = cms.InputTag('hltDoubleEle8Mass8Filter','','HLT'), #the last filter in the path
+  TriggerFilter = cms.InputTag('hltDoubleEle8Mass8Filter', '', 'HLT'), #the last filter in the path
   PtThrJet = cms.untracked.double(40.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
@@ -18,23 +20,25 @@ SUSY_HLT_HT_DoubleEle = cms.EDAnalyzer("SUSY_HLT_DoubleEle_Hadronic",
 
 
 SUSY_HLT_HT_DoubleEle_FASTSIM = cms.EDAnalyzer("SUSY_HLT_DoubleEle_Hadronic",
-  trigSummary = cms.InputTag('hltTriggerSummaryAOD','','HLT'),
+  trigSummary = cms.InputTag("hltTriggerSummaryAOD",'', 'HLT'), #to use with test sample
+  #trigSummary = cms.InputTag("hltTriggerSummaryAOD"),
   ElectronCollection = cms.InputTag("gedGsfElectrons"),
   pfJetCollection = cms.InputTag("ak4PFJetsCHS"),
   caloJetCollection = cms.InputTag("ak4CaloJets"),
-  TriggerResults = cms.InputTag('TriggerResults','','HLT'),
+  TriggerResults = cms.InputTag('TriggerResults','','HLT'), #to use with test sample
+  #TriggerResults = cms.InputTag('TriggerResults','','HLT'),
   HLTProcess = cms.string('HLT'),
-  TriggerPath = cms.string('HLT_DoubleEle8_CaloIdL_TrkIdVL_Mass8_PFHT300_v'),
+  TriggerPath = cms.string('HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v1'),
   TriggerPathAuxiliaryForElectron = cms.string('HLT_PFHT900_v'),
   TriggerPathAuxiliaryForHadronic = cms.string('HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v'),
-  TriggerFilter = cms.InputTag('hltDoubleEle8Mass8Filter','','HLT'), #the last filter in the path
+  TriggerFilter = cms.InputTag('hltDoubleEle8Mass8Filter', '', 'HLT'), #the last filter in the path
   PtThrJet = cms.untracked.double(40.0),
   EtaThrJet = cms.untracked.double(3.0)
 )
 
 
 SUSY_HLT_HT_DoubleEle_POSTPROCESSING = cms.EDAnalyzer("DQMGenericClient",
-    subDirs        = cms.untracked.vstring("HLT/SUSYBSM/HLT_DoubleEle8_CaloIdL_TrkIdVL_Mass8_PFHT300"),
+    subDirs        = cms.untracked.vstring("HLT/SUSYBSM/HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHTT300"),
     verbose        = cms.untracked.uint32(2), # Set to 2 for all messages
     resolution     = cms.vstring(""),
     efficiency     = cms.vstring(
@@ -45,7 +49,7 @@ SUSY_HLT_HT_DoubleEle_POSTPROCESSING = cms.EDAnalyzer("DQMGenericClient",
 
 
 SUSY_HLT_HT_DoubleEle_FASTSIM_POSTPROCESSING = cms.EDAnalyzer("DQMGenericClient",
-    subDirs        = cms.untracked.vstring("HLT/SUSYBSM/HLT_DoubleEle8_CaloIdL_TrkIdVL_Mass8_PFHT300"),
+    subDirs        = cms.untracked.vstring("HLT/SUSYBSM/HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHTT300"),
     verbose        = cms.untracked.uint32(2), # Set to 2 for all messages
     resolution     = cms.vstring(""),
     efficiency     = cms.vstring(
@@ -53,3 +57,7 @@ SUSY_HLT_HT_DoubleEle_FASTSIM_POSTPROCESSING = cms.EDAnalyzer("DQMGenericClient"
        "EleTurnOn_eff 'Turn-on vs Mu pT; pT (GeV); #epsilon' EleTurnOn_num EleTurnOn_den",
     )
 )
+
+
+
+
