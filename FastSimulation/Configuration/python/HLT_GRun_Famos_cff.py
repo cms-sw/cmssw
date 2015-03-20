348b23ec44e703cd503657dc76917d502c5792dd
Lukas <lukas.vanelderen@cern.ch>
2015-03-20 10:16:28 +0100

    no longer removing the HLT iterative tracking sequences from tigger paths (they are empty anyway)

--- FastSimulation/Configuration/python/HLT_GRun_Famos_cff.py
+++ FastSimulation/Configuration/python/HLT_GRun_Famos_cff.py
@@ -1,40 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from HLTrigger.Configuration.HLT_GRun_Famos_cff import *
-
-# remove track producers from trigger path
-_toremove = [
-    HLTIterativeTrackingIter04,
-    HLTIterativeTrackingIter02,
-    HLTIterativeTracking,
-    HLTIterativeTrackingTau3Mu,
-    HLTIterativeTrackingReg,
-    HLTIterativeTrackingForElectronIter02,
-    HLTIterativeTrackingForPhotonsIter02,
-    HLTIterativeTrackingL3MuonIter02,
-    HLTIterativeTrackingL3MuonRegIter02,
-    HLTIterativeTrackingForBTagIter02,
-    HLTIterativeTrackingForTauIter02,
-    HLTIterativeTrackingForTauIter04,
-    HLTIterativeTrackingGlbTrkMuonIter02,
-    HLTIterativeTrackingHighPtTkMu,
-    HLTIterativeTrackingHighPtTkMuIsoIter02,
-    HLTIterativeTrackingDisplacedJpsiIter02,
-    HLTIterativeTrackingDisplacedPsiPrimeIter02,
-    HLTIterativeTrackingDisplacedNRMuMuIter02,
-    HLTIterativeTrackingForBTagIter12,
-    HLTIterativeTrackingForBTagIteration0,
-    HLTIterativeTrackingIteration4DisplacedJets
-    ]
-for _key,_value in locals().items():
-    if isinstance(_value,cms.Path):
-        for _entry in _toremove:
-            _value.remove(_entry)
-
-_toremove = [hltEcalRegionalPi0RecHit,hltHfreco,hltHoreco,hltHbhereco,hltEcalRecHit,hltEcalPreshowerRecHit]
-for _key,_value in locals().items():
-    if isinstance(_value,cms.Sequence):
-        for _entry in _toremove:
-            _value.remove(_entry)
             
 hltEcalPreshowerRecHit = cms.EDAlias(
     ecalPreshowerRecHit = cms.VPSet(
