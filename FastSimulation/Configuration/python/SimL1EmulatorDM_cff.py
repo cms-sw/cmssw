9e19ee796d27b41faee0c2103a70c0841ba90f31
Lukas <lukas.vanelderen@cern.ch>
2015-03-20 13:54:59 +0100

    moved the L1 emulation and L1 reconstruction from the FastSim HLT step to separate steps

--- FastSimulation/Configuration/python/SimL1EmulatorDM_cff.py
+++ FastSimulation/Configuration/python/SimL1EmulatorDM_cff.py
@@ -1,2 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.SimL1EmulatorDM_cff import *
+
+for _entry in [SimL1TechnicalTriggers]:
+    SimL1Emulator.remove(_entry)
+
+del simGtDigis.TechnicalTriggersInputTags
