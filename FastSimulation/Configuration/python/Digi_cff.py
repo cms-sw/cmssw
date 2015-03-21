afdbea7f5098e900c2bb80684256eeb095bdedbd
Lukas <lukas.vanelderen@cern.ch>
2015-03-21 15:36:14 +0100

    all packing and unpacking of digis removed and replaced with EDAliases

--- FastSimulation/Configuration/python/Digi_cff.py
+++ FastSimulation/Configuration/python/Digi_cff.py
@@ -8,56 +8,14 @@ simMuonDTDigis.InputCollection = 'MuonSimHitsMuonDTHits'
 simMuonRPCDigis.InputCollection = 'MuonSimHitsMuonRPCHits'
 calDigi.remove(castorDigiSequence)
 
-# extend it with some digi2raw
-#from Configuration.StandardSequences.DigiToRaw_cff import ecalPacker,esDigiToRaw,hcalRawData,rawDataCollector
-#for _entry in [cms.InputTag("SiStripDigiToRaw"), cms.InputTag("castorRawData"),cms.InputTag("siPixelRawData")]:
-#    rawDataCollector.RawCollectionList.remove(_entry)
+# give digi collections the names expected by RECO and HLT
+import FastSimulation.Configuration.DigiAndMixAliasInfo_cff as _aliasInfo
+_aliasInfo.convertAliasInfoForDataMixer()
+generalTracks = _aliasInfo.infoToAlias(_aliasInfo.generalTracksAliasInfo)
+ecalPreshowerDigis = _aliasInfo.infoToAlias(_aliasInfo.ecalPreShowerDigisAliasInfo)
+ecalDigis = _aliasInfo.infoToAlias(_aliasInfo.ecalDigisAliasInfo)
+hcalDigis = _aliasInfo.infoToAlias(_aliasInfo.hcalDigisAliasInfo)
+muonDTDigis = _aliasInfo.infoToAlias(_aliasInfo.muonDTDigisAliasInfo)
+muonCSCDigis = _aliasInfo.infoToAlias(_aliasInfo.muonCSCDigisAliasInfo)
+muonRPCDigis = _aliasInfo.infoToAlias(_aliasInfo.muonRPCDigisAliasInfo)
 
-# extend it with some raw2digi
-#from Configuration.StandardSequences.RawToDigi_cff import ecalPreshowerDigis,ecalDigis,hcalDigis
-
-#calDigiToRawToDigi = cms.Sequence(ecalPacker+esDigiToRaw+hcalRawData+rawDataCollector+ecalPreshowerDigis+ecalDigis+hcalDigis)
-#doAllDigi.replace(calDigi,calDigi+calDigiToRawToDigi)
-
-# aliases for muons, to emulate digi2raw raw2digi
-
-#ecalPreshowerDigis  = cms.EDAlias(
-#    
-#    )
-
-#hcalDigis  = cms.EDAlias(
-#
-#    )
-
-#ecalDigis = cms.EDalias(
-
-"""
-muonDTDigis = cms.EDAlias(
-    mix = cms.VPSet(
-        cms.PSet(type = cms.string("DTLayerIdDTDigiMuonDigiCollection"))
-        )
-    )
-muonRPCDigis = cms.EDAlias(
-    mix = cms.VPSet(
-        cms.PSet(type = cms.string("RPCDetIdRPCDigiMuonDigiCollection"))
-        )
-    )
-
-muonCSCDigis = cms.EDAlias(
-    mix = cms.VPSet(
-        cms.PSet(
-            type = cms.string("CSCDetIdCSCWireDigiMuonDigiCollection"),
-            fromProductInstance = cms.string("MuonCSCWireDigisDM"),
-            ),
-        cms.PSet(
-            type = cms.string("CSCDetIdCSCStripDigiMuonDigiCollection"),
-            fromProductInstance = cms.string("MuonCSCStripDigisDM"),
-            )
-        )
-    )
-"""
-
-from FastSimulation.Tracking.GeneralTracksAlias_cfi import generalTracksAliasInfo
-generalTracks = cms.EDAlias(**{generalTracksAliasInfo.key.value():generalTracksAliasInfo.value})
-
-# TODO: understand whether this digi2raw+raw2digi is useful
