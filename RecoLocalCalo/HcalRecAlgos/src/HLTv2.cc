9eb598e3a02244d293652f61a5e1f818b38e9da9
Stephanie Akemi Brandt <sabrandt@mit.edu>
2015-03-22 11:27:27 +0100

    moved some arrays

--- RecoLocalCalo/HcalRecAlgos/src/HLTv2.cc
+++ RecoLocalCalo/HcalRecAlgos/src/HLTv2.cc
@@ -149,29 +149,27 @@ void HLTv2::applyXM(const std::vector<double> & inputCharge, const std::vector<d
 
 }
 
+constexpr Float_t HLTv2::landauFrac[];
 // Landau function integrated in 1 ns intervals
 //Landau pulse shape from https://indico.cern.ch/event/345283/contribution/3/material/slides/0.pdf
 //Landau turn on by default at left edge of time slice 
 // normalized to 1 on [0,10000]
 void HLTv2::getLandauFrac(Float_t tStart, Float_t tEnd, Float_t &sum) const{
 
-   Float_t landauFrac[125] = {0, 7.6377e-05, 0.000418655, 0.00153692, 0.00436844, 0.0102076, 0.0204177, 0.0360559, 0.057596, 0.0848493, 0.117069, 0.153152, 0.191858, 0.23198, 0.272461, 0.312438, 0.351262, 0.388476, 0.423788, 0.457036, 0.488159, 0.517167, 0.54412, 0.569112, 0.592254, 0.613668, 0.633402, 0.651391, 0.667242, 0.680131, 0.688868, 0.692188, 0.689122, 0.67928, 0.662924, 0.64087, 0.614282, 0.584457, 0.552651, 0.51997, 0.487317, 0.455378, 0.424647, 0.395445, 0.367963, 0.342288, 0.318433, 0.29636, 0.275994, 0.257243, 0.24, 0.224155, 0.2096, 0.196227, 0.183937, 0.172635, 0.162232, 0.15265, 0.143813, 0.135656, 0.128117, 0.12114, 0.114677, 0.108681, 0.103113, 0.0979354, 0.0931145, 0.0886206, 0.0844264, 0.0805074, 0.0768411, 0.0734075, 0.0701881, 0.0671664, 0.0643271, 0.0616564, 0.0591418, 0.0567718, 0.054536, 0.0524247, 0.0504292, 0.0485414, 0.046754, 0.0450602, 0.0434538, 0.041929, 0.0404806, 0.0391037, 0.0377937, 0.0365465, 0.0353583, 0.0342255, 0.0331447, 0.032113, 0.0311274, 0.0301854, 0.0292843, 0.0284221, 0.0275964, 0.0268053, 0.0253052, 0.0238536, 0.0224483, 0.0210872, 0.0197684, 0.0184899, 0.01725, 0.0160471, 0.0148795, 0.0137457, 0.0126445, 0.0115743, 0.0105341, 0.00952249, 0.00853844, 0.00758086, 0.00664871,0.00574103, 0.00485689, 0.00399541, 0.00315576, 0.00233713, 0.00153878, 0.000759962, 0 };
-
   if (abs(tStart-tEnd-25)<0.1) {
     cout << abs(tStart-tEnd) << "???" << endl;
     sum=0;
     return;
   }
-  sum=landauFrac[int(ceil(tStart+25))];
+  sum= landauFrac[int(ceil(tStart+25))];
   return;
 }
 
-void HLTv2::PulseFraction(Double_t fC, Double_t *TS46) const{
+constexpr Double_t HLTv2::TS4par[];
+constexpr Double_t HLTv2::TS5par[];
+constexpr Double_t HLTv2::TS6par[];
 
-  //static Double_t TS3par[3] = {0.44, -18.6, 5.136}; //Gaussian parameters: norm, mean, sigma for the TS3 fraction                    
-  static Double_t TS4par[3] = {0.71, -5.17, 12.23}; //Gaussian parameters: norm, mean, sigma for the TS4 fraction                      
-  static Double_t TS5par[3] = {0.258, 0.0178, 4.786e-4}; // pol2 parameters for the TS5 fraction                                       
-  static Double_t TS6par[4] = {0.06391, 0.002737, 8.396e-05, 1.475e-06};// pol3 parameters for the TS6 fraction                        
+void HLTv2::PulseFraction(Double_t fC, Double_t *TS46) const{
 
   Double_t tslew = -HcalTimeSlew::delay(fC,HcalTimeSlew::MC,fTimeSlewBias);
 
--- RecoLocalCalo/HcalRecAlgos/src/PedestalSub.cc
+++ RecoLocalCalo/HcalRecAlgos/src/PedestalSub.cc
@@ -17,15 +17,17 @@ void PedestalSub::Init(Method method=AvgWithThresh, int runCond=0, float thresho
   fQuantile=quantile;
   fCondition=runCond;
   
-  if ( (fMethod==DoNothing||fMethod==AvgWithoutThresh||fMethod==Percentile)&&(fThreshold!=0.0) ) {
-    cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're passing a threshold for a fMethod that doesn't use them!" << endl;
-  }
-  else if ( (fMethod!=Percentile)&&(fQuantile!=0.0) ) {
-    cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're passing a quantile for a fMethod that doesn't use them!" << endl;
-  }
-  else if ( (fMethod==AvgWithThresh||fMethod==AvgWithThreshNoPedSub)&&(fThreshold==0) ) {
-    cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're using 0.0 as your threshold!" << endl;
-  }
+  // parameters are hardcoded for testing purposes so print statements are not necessary at the moment
+  // if ( (fMethod==DoNothing||fMethod==AvgWithoutThresh||fMethod==Percentile)&&(fThreshold!=0.0) ) {
+    // cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're passing a threshold for a fMethod that doesn't use them!" << endl;
+  // }
+  // else if ( (fMethod!=Percentile)&&(fQuantile!=0.0) ) {
+    // cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're passing a quantile for a fMethod that doesn't use them!" << endl;
+  // }
+  // else if ( (fMethod==AvgWithThresh||fMethod==AvgWithThreshNoPedSub)&&(fThreshold==0) ) {
+    // cout << "You are almost certainly doing something wrong. Check your PedestalSub::Calculate parameters, you're using 0.0 as your threshold!" << endl;
+  // }
+  
  /* else if (fMethod==Percentile) {
     //need to fix these parameters;
     if (fCondition==0) {
@@ -45,7 +47,7 @@ void PedestalSub::Init(Method method=AvgWithThresh, int runCond=0, float thresho
 void PedestalSub::Calculate(const std::vector<double> & inputCharge, const std::vector<double> & inputPedestal, std::vector<double> & corrCharge) const {
 
   double bseCorr=PedestalSub::GetCorrection(inputCharge, inputPedestal);
-  for (Int_t i=0; i<10; i++) {
+  for (auto i=0; i<10; i++) {
     if (fMethod==AvgWithThresh||fMethod==Percentile||fMethod==AvgWithoutThresh) {
       corrCharge.push_back(inputCharge[i]-inputPedestal[i]-bseCorr);
     }
@@ -63,7 +65,7 @@ double PedestalSub::GetCorrection(const std::vector<double> & inputCharge, const
     baseline=0;
   }
   else if (fMethod==AvgWithThresh) {
-    for (Int_t i=0; i<10; i++) {
+    for (auto i=0; i<10; i++) {
       if (i==4||i==5) continue;
       if ( (inputCharge[i]-inputPedestal[i])<fThreshold) {
 	baseline+=(inputCharge[i]-inputPedestal[i]);
@@ -75,14 +77,14 @@ double PedestalSub::GetCorrection(const std::vector<double> & inputCharge, const
     baseline/=8;
   }
   else if (fMethod==AvgWithoutThresh) {
-    for (Int_t i=0; i<10; i++) {
+    for (auto i=0; i<10; i++) {
       if (i==4||i==5) continue;
       baseline+=(inputCharge[i]-inputPedestal[i]);
     }
     baseline/=8;
   }
   if (fMethod==AvgWithThreshNoPedSub) {
-    for (Int_t i=0; i<10; i++) {
+    for (auto i=0; i<10; i++) {
       if (i==4||i==5) continue;
       if ( (inputCharge[i])<fThreshold) {
 	baseline+=(inputCharge[i]);
