9b6705cafd54ce78952aa26a3ddabea4d68354b2
Giovanni <gpetruc@gmail.com>
2015-03-22 14:42:39 +0100

    HeppyCore: support deltaR(obj1, obj2) --> deltaR(obj1.eta(),obj1.phi(),obj2.eta(),obj2.phi())

--- PhysicsTools/HeppyCore/python/utils/deltar.py
+++ PhysicsTools/HeppyCore/python/utils/deltar.py
@@ -4,7 +4,10 @@
 import math
 import copy
 
-def deltaR2( e1, p1, e2, p2):
+def deltaR2( e1, p1, e2=None, p2=None):
+    """Take either 4 arguments (eta,phi, eta,phi) or two objects that have 'eta', 'phi' methods)"""
+    if (e2 == None and p2 == None):
+        return deltaR2(e1.eta(),e1.phi(), p1.eta(), p1.phi())
     de = e1 - e2
     dp = deltaPhi(p1, p2)
     return de*de + dp*dp
