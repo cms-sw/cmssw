18e99f61a3b328b02b077a8726c0222ec3e2e547
Lukas <lukas.vanelderen@cern.ch>
2015-03-22 14:23:45 +0100

    put back the parameter TechnicalTriggersInputTags to simGTDigs

--- FastSimulation/Configuration/python/SimL1Emulator_cff.py
+++ FastSimulation/Configuration/python/SimL1Emulator_cff.py
@@ -4,7 +4,5 @@ from Configuration.StandardSequences.SimL1Emulator_cff import *
 for _entry in [SimL1TechnicalTriggers]:
     SimL1Emulator.remove(_entry)
 
-del simGtDigis.TechnicalTriggersInputTags
-
 import FastSimulation.Configuration.DigiAndMixAliasInfo_cff as _aliasInfo
 gtDigis = _aliasInfo.infoToAlias(_aliasInfo.gtDigisAliasInfo)
