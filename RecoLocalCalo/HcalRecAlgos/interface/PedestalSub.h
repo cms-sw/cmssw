e7bc13d04ea453d24c584a4db00259d8ef917458
Stephanie Akemi Brandt <sabrandt@mit.edu>
2015-03-22 11:27:40 +0100

    moved some arrays

--- RecoLocalCalo/HcalRecAlgos/interface/PedestalSub.h
+++ RecoLocalCalo/HcalRecAlgos/interface/PedestalSub.h
@@ -2,11 +2,7 @@
 #define PedestalSub_h 1
 
 #include <typeinfo>
-
-#include <TH1.h>
-#include "TF1.h"
-#include "TF2.h"
-#include "TMath.h"
+#include <vector>
 
 class PedestalSub
 {
@@ -17,10 +13,6 @@ class PedestalSub
   ~PedestalSub();
   
   void Init(Method method, int runCond, float threshold, float quantile);
-  
-  // This is the CMSSW Implementation of the apply function
-  //void apply(const CaloSamples & cs, const std::vector<int> & capidvec, const HcalCalibrations & calibs, std::vector<double> & correctedOutput) const;
-  // This is the edited implementation for our standalone test code
   
   void Calculate(const std::vector<double> & inputCharge, const std::vector<double> & inputPedestal, std::vector<double> & corrCharge) const;
   double GetCorrection(const std::vector<double> & inputCharge, const std::vector<double> & inputPedestal) const;
