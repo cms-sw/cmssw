1318e41aa8b53be4ba6bc3465af47d0f8f5a294e
Mia <mia.tosi@cern.ch>
2015-03-23 11:51:44 +0100

    fix propagator in SeedFromConsecutiveHitsCreator config file

--- RecoTracker/TkSeedGenerator/python/SeedFromConsecutiveHitsCreator_cfi.py
+++ RecoTracker/TkSeedGenerator/python/SeedFromConsecutiveHitsCreator_cfi.py
@@ -1,9 +1,9 @@
-import FWCore.ParameterSet.Config as cms
-
+Bimport FWCore.ParameterSet.Config as cms
+0;95;c
 SeedFromConsecutiveHitsCreator = cms.PSet(
   ComponentName = cms.string('SeedFromConsecutiveHitsCreator'),
-  propagator = cms.string('PropagatorWithMaterial'),
-#  propagator = cms.string('PropagatorWithMaterialParabolicMf'),
+#  propagator = cms.string('PropagatorWithMaterial'),
+  propagator = cms.string('PropagatorWithMaterialParabolicMf'),
   SeedMomentumForBOFF = cms.double(5.0),
   OriginTransverseErrorMultiplier = cms.double(1.0),
   MinOneOverPtError = cms.double(1.0),
