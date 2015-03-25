f109cad0c99cace6acbd56519de0668e800cef60
Piet <piet.verwilligen@cern.ch>
2015-03-25 17:00:44 +0100

    port of SLHC fix to RPCSimSetup

--- SimMuon/RPCDigitizer/src/RPCSimSetUp.cc
+++ SimMuon/RPCDigitizer/src/RPCSimSetUp.cc
@@ -99,6 +99,8 @@ void RPCSimSetUp::setRPCSetUp(const std::vector<RPCStripNoises::NoiseItem>& vnoi
   }
 }
 
+// Previous version with hardcoded limit of max 96 strips per roll
+/*
 void RPCSimSetUp::setRPCSetUp(const std::vector<RPCStripNoises::NoiseItem>& vnoise, const std::vector<RPCClusterSize::ClusterSizeItem>& vClusterSize){
 
   std::vector<RPCClusterSize::ClusterSizeItem>::const_iterator itCls;
@@ -166,6 +168,135 @@ void RPCSimSetUp::setRPCSetUp(const std::vector<RPCStripNoises::NoiseItem>& vnoi
     n++;
   }
 }
+*/
+
+// New version that can deal with rolls with an arbitrary number of strips, no hardcoded value 96 here
+void RPCSimSetUp::setRPCSetUp(const std::vector<RPCStripNoises::NoiseItem>& vnoise, const std::vector<RPCClusterSize::ClusterSizeItem>& vClusterSize){
+
+  uint32_t detId, current_detId, this_detId;
+  RPCDetId rpcId, current_rpcId, this_rpcId;
+  const RPCRoll * current_roll,* this_roll;
+  unsigned int current_nStrips;
+  // ### ClusterSizeItem #######################################################                        
+  std::vector<RPCClusterSize::ClusterSizeItem>::const_iterator itCls;
+  int clsCounter(1);
+  std::vector<double> clsVect;
+  for(itCls = vClusterSize.begin(); itCls != vClusterSize.end(); ++itCls){
+    clsVect.push_back(((double)(itCls->clusterSize)));
+    if((!(clsCounter%100)) && (clsCounter!=0)){
+      detId=itCls->dpid;
+      _mapDetClsMapLegacy[detId]=clsVect;
+      clsVect.clear();
+      clsCounter=0;
+    }
+    ++clsCounter;
+  }
+  // ###########################################################################
+
+
+  // ### NoiseItem #############################################################
+  unsigned int count_strips = 1;
+  unsigned int count_all    = 1;
+  std::vector<float> vveff, vvnoise;
+  // vveff.clear();
+  // vvnoise.clear();
+
+  // DetId to start with
+  current_detId   = vnoise.begin()->dpid;
+  current_rpcId   = RPCDetId(current_detId);
+  current_roll    = dynamic_cast<const RPCRoll* >(theGeometry->roll(current_rpcId));
+  current_nStrips = current_roll->nstrips();
+
+  LogDebug ("rpssimsetup") <<"Start Position ::            current_detId = "<<current_detId<<" aka "<<current_rpcId;
+  LogDebug ("rpssimsetup") <<" is a valid roll with pointer "<<current_roll<<" and has "<<current_roll->nstrips()<<" strips"<<std::endl;
+  LogDebug ("rpssimsetup") <<" ------------------------------------------------------------------------------------------------------------------------------------- "<<std::endl;
+  for(std::vector<RPCStripNoises::NoiseItem>::const_iterator it = vnoise.begin(); it != vnoise.end(); ++it) {
+
+    // roll associated to the conditions of this strip (iterator)
+    this_detId = it->dpid;
+    this_rpcId = RPCDetId(this_detId);
+    // Test whether this roll (picked up from the conditions) is inside the RPC Geometry
+    const RPCRoll* roll = dynamic_cast<const RPCRoll* >(theGeometry->roll(this_rpcId));
+    if(roll==0) {
+      LogDebug ("rpssimsetup") <<"Inside Loop :: ["<<std::setw(6)<<count_all<<"]["<<std::setw(3)<<count_strips<<"] :: this_detId = "<<this_detId<<" aka "<<this_rpcId<<" which is not in current Geometry --> Skip "<<std::endl;
+      continue;
+    }
+    // Case 1 :: FIRST ENTRY
+    // ---------------------
+    if(this_detId == current_detId && count_strips == 1) {
+      LogDebug ("rpssimsetup") <<"Inside Loop :: ["<<std::setw(6)<<count_all<<"]["<<std::setw(3)<<count_strips<<"] :: this_detId = "<<this_detId<<" aka "<<this_rpcId<<" Noise = "<<it->noise<<" Hz/cm2";
+      LogDebug ("rpssimsetup") <<" is a valid roll with pointer "<<roll<<" and has "<<roll->nstrips()<<" strips"<<std::endl;
+      // fill bx in map
+      _bxmap[current_detId] = it->time;
+      // clear vectors
+      vveff.clear();
+      vvnoise.clear();
+      // fill the vectors
+      vvnoise.push_back((it->noise));
+      vveff.push_back((it->eff));
+      // update counter
+      ++count_strips;
+      ++count_all;
+    }
+
+    // Case 2 :: 2ND ENTRY --> LAST-1 ENTRY
+    // ------------------------------------
+    if(this_detId == current_detId && count_strips > 1 && count_strips < current_nStrips) {
+      LogDebug ("rpssimsetup") <<"Inside Loop :: ["<<std::setw(6)<<count_all<<"]["<<std::setw(3)<<count_strips<<"] :: this_detId = "<<this_detId<<" aka "<<this_rpcId<<" Noise = "<<it->noise<<" Hz/cm2"<<std::endl;
+      // fill the vectors
+      vvnoise.push_back((it->noise));
+      vveff.push_back((it->eff));
+      // update counter
+      ++count_strips;
+      ++count_all;
+    }
+    // Case 3 :: LAST ENTRY
+    // --------------------
+    if(this_detId == current_detId && count_strips == current_nStrips) {
+      // fill last value in the vector
+      LogDebug ("rpssimsetup") <<"Last Value ::  ["<<std::setw(6)<<count_all<<"]["<<std::setw(3)<<count_strips<<"] :: this_detId = "<<this_detId<<" aka "<<this_rpcId<<" Noise = "<<it->noise<<" Hz/cm2";
+      vvnoise.push_back((it->noise));
+      vveff.push_back((it->eff));
+      // update counter
+      ++count_strips;
+      ++count_all;
+      // fill vectors into map
+      LogDebug ("rpssimsetup") <<" fill vectors into map"<<std::endl;
+      _mapDetIdNoise[current_detId]= vvnoise;
+      _mapDetIdEff[current_detId] = vveff;
+      // look for next different detId and rename it to the current_detId
+      // at this point we skip all the conditions for the strips that are not in this roll
+      // and we will go to the conditions for the first strip of the next roll
+      bool next_detId_found = 0;
+      LogDebug ("rpssimsetup") <<"look for next different detId"<<std::endl;
+      while(next_detId_found==0 && it != vnoise.end()-1) {
+        ++it;
+        this_detId = it->dpid;
+        this_rpcId = RPCDetId(this_detId);
+        this_roll = dynamic_cast<const RPCRoll* >(theGeometry->roll(this_rpcId));
+        if(!this_roll) continue;
+        LogDebug ("rpssimsetup") <<"Inside While:: ["<<std::setw(6)<<count_all<<"]["<<std::setw(3)<<count_strips<<"] :: this_detId = "<<this_detId<<" aka "<<this_rpcId<<" Noise = "<<it->noise<<" Hz/cm2"<<std::endl;
+        ++count_strips;
+        // ++count_all;                                                                      
+        if(this_detId != current_detId) {
+          LogDebug ("rpssimsetup") <<"Different detId is found ::                  "<<this_detId<<" aka "<<this_rpcId<<" Noise = "<<it->noise<<" Hz/cm2";
+          // next roll is found. update current_detId to this newly found detId
+          // and update also the number of strips
+          current_detId = this_detId;
+          current_rpcId = RPCDetId(current_detId);
+          next_detId_found = 1;
+          current_nStrips = dynamic_cast<const RPCRoll* >(theGeometry->roll(current_rpcId))->nstrips();
+          LogDebug ("rpssimsetup") <<" with "<<current_nStrips<<" strips"<<std::endl;
+          --it; // subtract one, because at the end of the loop the iterator will be increased with one
+        }
+      }
+      // reset count_strips
+      count_strips = 1;
+    }
+  }
+  // ###########################################################################
+}
+
 
 const std::vector<float>& RPCSimSetUp::getNoise(uint32_t id)
 {
@@ -184,10 +315,15 @@ const std::vector<float>& RPCSimSetUp::getEff(uint32_t id)
     throw cms::Exception("DataCorrupt") 
       << "Exception comming from RPCSimSetUp - no efficiency information for DetId\t"<<id<< std::endl;
   }
-  if((iter->second).size() != 96){
-    throw cms::Exception("DataCorrupt") 
+  RPCDetId rpcId = RPCDetId(id);
+  const RPCRoll* roll = dynamic_cast<const RPCRoll* >(theGeometry->roll(rpcId));
+  unsigned int numbStrips = roll->nstrips();
+
+  if((iter->second).size() < numbStrips){
+    throw cms::Exception("DataCorrupt")
       << "Exception comming from RPCSimSetUp - efficiency information in a wrong format for DetId\t"<<id<< std::endl;
   }
+
   return iter->second;
 }
 
@@ -215,7 +351,6 @@ const std::map< int, std::vector<double> >& RPCSimSetUp::getClsMap()
 //const std::map<int, std::vector<double> >& RPCSimSetUp::getClsMap(uint32_t id)
 const std::vector<double>& RPCSimSetUp::getCls(uint32_t id) //legacy member function
 {
-
   map<uint32_t,std::vector<double> >::iterator iter = _mapDetClsMapLegacy.find(id);
   if(iter == _mapDetClsMapLegacy.end()){
     throw cms::Exception("DataCorrupt") 
@@ -223,7 +358,7 @@ const std::vector<double>& RPCSimSetUp::getCls(uint32_t id) //legacy member func
   }
   if((iter->second).size() != 100){
     throw cms::Exception("DataCorrupt") 
-      << "Exception comming from RPCSimSetUp - cluster size information in a wrong format for DetId\t"<<id<< std::endl;
+      << "Exception comming from RPCSimSetUp - _mapDetClsMapLegacy - cluster size information in a wrong format for DetId\t"<<id<< std::endl;
   }
   return iter->second;
 }
@@ -233,11 +368,11 @@ const std::vector<double> & RPCSimSetUp::getAsymmetricClsDistribution(uint32_t i
  map<uint32_t,std::vector<double> >::const_iterator iter = _mapDetClsMap.find(id);
   if(iter == _mapDetClsMap.end()){
     throw cms::Exception("DataCorrupt")
-      << "Exception comming from RPCSimSetUp - no cluster size information for DetId\t"<<id<< std::endl;
+      << "Exception comming from RPCSimSetUp - _mapDetClsMap - no cluster size information for DetId\t"<<id<< std::endl;
   }
   if((iter->second).size() != 120){
     throw cms::Exception("DataCorrupt")
-      << "Exception comming from RPCSimSetUp - cluster size information in a wrong format for DetId\t"<<id<< std::endl;
+      << "Exception comming from RPCSimSetUp - _mapDetClsMap - cluster size information in a wrong format for DetId\t"<<id<< std::endl;
   }
 //  return iter->second;
 
@@ -297,11 +432,11 @@ const std::vector<double> & RPCSimSetUp::getAsymmetryForCls(uint32_t id, uint32_
  map<uint32_t,std::vector<double> >::const_iterator iter = _mapDetClsMap.find(id);
   if(iter == _mapDetClsMap.end()){
     throw cms::Exception("DataCorrupt")
-      << "Exception comming from RPCSimSetUp - no cluster size information for DetId\t"<<id<< std::endl;
+      << "Exception comming from RPCSimSetUp - _mapDetClsMap - no cluster size information for DetId\t"<<id<< std::endl;
   }
   if((iter->second).size() != 120){
     throw cms::Exception("DataCorrupt")
-      << "Exception comming from RPCSimSetUp - cluster size information in a wrong format for DetId\t"<<id<<'\t'<<(iter->second).size()<< std::endl;
+      << "Exception comming from RPCSimSetUp - _mapDetClsMap - cluster size information in a wrong format for DetId\t"<<id<<'\t'<<(iter->second).size()<< std::endl;
   }
 
 std::vector<double> dataForAsymmCls = iter->second;
@@ -345,8 +480,6 @@ vector<double> clsDetAsymmetryForCls;
     value = sliceVsFewStripsDistribution[slice][(cls-1)*4+i];
     clsDetAsymmetryForCls.push_back(value);
     sum +=value; 
-    //     std::cout<<"value\t"<<value<<std::endl;
-     //    std::cout<<"sum\t"<<sum<<std::endl;
   }
   
   float accum=0;
