98d6a6ea476715a81fa31951f2cea0b4863665fd
Steve Sanders <ssanders@ku.edu>
2015-03-25 16:12:10 -0500

    new EP code

--- RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatproducer_cfi.py
+++ RecoHI/HiEvtPlaneAlgos/python/hievtplaneflatproducer_cfi.py
@@ -1,7 +1,20 @@
 import FWCore.ParameterSet.Config as cms
 
 hiEvtPlaneFlat = cms.EDProducer('HiEvtPlaneFlatProducer',
-                                vtxCollection_=cms.InputTag("hiSelectedVertex"),
-                                inputPlanes_ = cms.InputTag("hiEvtPlane","recoLevel"),
-                                centrality_  = cms.InputTag("centralityBin","HFtowers")
+                                vertexTag_=cms.InputTag("hiSelectedVertex"),
+                                centralityTag_=cms.InputTag("hiCentrality"),
+                                centralityBinTag_ = cms.InputTag("centralityBin","HFtowers"),
+                                centralityVariable = cms.string("HFtowers"),
+                                nonDefaultGlauberModel = cms.string(""),
+                                inputPlanesTag_ = cms.InputTag("hiEvtPlane","recoLevel"),
+                                trackTag_=cms.InputTag("hiGeneralTracks"),                           
+                                FlatOrder_ = cms.untracked.int32(9),
+                                NumFlatBins_ = cms.untracked.int32(40),
+                                Noffmin_ = cms.untracked.int32 (-1),
+                                Noffmax_ = cms.untracked.int32 (10000),
+                                CentBinCompression_ = cms.untracked.int32(5),
+                                caloCentRef_ = cms.untracked.double(80.),
+                                caloCentRefWidth_ = cms.untracked.double(5.0),
+                                HFEtScale_ = cms.untracked.int32(3800),
+                                useOffsetPsi_ = cms.untracked.bool(True)
                                 )
