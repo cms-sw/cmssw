05bb0487407f817ad588f0299231fa298f6261c0
weihua19900704 <hua.wei@cern.ch>
2015-03-26 14:53:49 +0100

    test

--- CalibCalorimetry/HcalAlgos/test/maptester_cfg.py
+++ CalibCalorimetry/HcalAlgos/test/maptester_cfg.py
@@ -14,9 +14,14 @@ process.demo = cms.EDAnalyzer("MapTester",
 #    mapIOV = cms.uint32(2), # HO 2009 until May 6
 #    mapIOV = cms.uint32(3), # HO May 6 2009 and onwards
 #    mapIOV = cms.uint32(4), # ZDC Oct 12 2009 and onwards
-    mapIOV = cms.uint32(5), # Remapping HO ring 0 for SiPMs
-    generateTextfiles = cms.bool(True),
+#    mapIOV = cms.uint32(5), # Remapping HO ring 0 for SiPMs
+#    mapIOV = cms.uint32(7), # add QIEId into Lmap and also include OfflineDB and QIEMap
+    mapIOV = cms.uint32(7), #
+    generateHTRLmap   = cms.bool(True),
+    generateuHTRLmap  = cms.bool(True),
     generateEmap      = cms.bool(True),
+    generateOfflineDB = cms.bool(True),
+    generateQIEMap    = cms.bool(True),
 )
 
 process.p = cms.Path(process.demo)
