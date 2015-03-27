f0dbbffeb0d48e55fe1952299bcef41905cf8871
Lindsey Gray <lindsey.gray@gmail.com>
2015-03-27 09:54:49 +0100

    bug searching -- force daughter class functions to be used

--- PhysicsTools/SelectorUtils/interface/VersionedSelector.h
+++ PhysicsTools/SelectorUtils/interface/VersionedSelector.h
@@ -19,9 +19,7 @@
 #include "PhysicsTools/SelectorUtils/interface/CutApplicatorBase.h"
 #include "PhysicsTools/SelectorUtils/interface/CutApplicatorWithEventContentBase.h"
 
-//#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
 #include "FWCore/Framework/interface/ConsumesCollector.h"
-//#endif
 
 // because we need to be able to validate the ID
 #include <openssl/md5.h>
@@ -59,11 +57,7 @@ class VersionedSelector : public Selector<T> {
     this->retInternal_  = this->getBitTemplate();
   }
   
-  bool operator()( const T& ref, pat::strbitset& ret ) 
-    //#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
-    override final
-    //#endif
-  {
+  virtual bool operator()( const T& ref, pat::strbitset& ret ) override final {
     howfar_ = 0;
     bool failed = false;
     if( !initialized_ ) {
@@ -84,11 +78,14 @@ class VersionedSelector : public Selector<T> {
     return (bool)ret;
   }
   
-  bool operator()(const T& ref, edm::EventBase const& e, pat::strbitset& ret) 
-    //#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
-    override final
-    //#endif
-  {
+  virtual bool operator()(const T& ref) override final {
+    this->retInternal_.set(false);
+    this->operator()(ref, this->retInternal_);
+    this->setIgnored(this->retInternal_);
+    return (bool)this->retInternal_;
+  }
+
+  virtual bool operator()(const T& ref, edm::EventBase const& e, pat::strbitset& ret) override final {
     // setup isolation needs
     for( size_t i = 0, cutssize = cuts_.size(); i < cutssize; ++i ) {
       if( needs_event_content_[i] ) {
@@ -99,9 +96,9 @@ class VersionedSelector : public Selector<T> {
     }
     return this->operator()(ref, ret);
   }
-    
+
   using Selector<T>::operator();
-    
+
   const unsigned char* md55Raw() const { return id_md5_; } 
   bool operator==(const VersionedSelector& other) const {
     constexpr unsigned length = MD5_DIGEST_LENGTH;
@@ -117,9 +114,7 @@ class VersionedSelector : public Selector<T> {
 
   void initialize(const edm::ParameterSet&);
 
-  //#if !defined(__CINT__) && !defined(__MAKECINT__) && !defined(__REFLEX__)
   void setConsumes(edm::ConsumesCollector);
-  //#endif
 
  protected:
   bool initialized_;
