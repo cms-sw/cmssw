134a5e57b078abdc4234b5feef295155be75cc9f
Lindsey Gray <lindsey.gray@gmail.com>
2015-03-30 17:20:14 +0200

    first fully functioning prototype of pyVID

--- RecoEgamma/ElectronIdentification/src/classes.h
+++ RecoEgamma/ElectronIdentification/src/classes.h
@@ -4,10 +4,17 @@
 #include "RecoEgamma/ElectronIdentification/interface/VersionedGsfElectronSelector.h"
 #include "RecoEgamma/ElectronIdentification/interface/VersionedPatElectronSelector.h"
 
+#include "PhysicsTools/SelectorUtils/interface/MakePyVIDClassBuilder.h"
+
 namespace RecoEgamma_ElectronIdentification {
   struct dictionary {    
     //for using the selectors in python
     VersionedGsfElectronSelector vGsfElectronSelector; 
     VersionedPatElectronSelector vPatElectronSelector; 
+    MakeVersionedSelector<reco::GsfElectron> vMakeGsfElectronVersionedSelector;
+    MakeVersionedSelector<pat::Electron> vMakePatElectronVersionedSelector;
+    
   };
 }
+
+
