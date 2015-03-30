a08e2f018505ded76e0289b0ea139d2606eead68
lveldere <lveldere@users.noreply.github.com>
2015-03-30 08:35:10 +0200

    Update postLS1Customs.py

--- SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
@@ -86,10 +86,6 @@ def customisePostLS1_HI(process):
 def digiEventContent(process):
     #extend the event content
 
-    # skip this for fastsim (output module complaints about edaliases)
-    #if hasattr(process,"FastSimCoreFEVT"):
-    #    return process
-
     alist=['RAWSIM','RAWDEBUG','FEVTDEBUG','FEVTDEBUGHLT','GENRAW','RAWSIMHLT','FEVT','PREMIX','PREMIXRAW']
     for a in alist:
         b = a + 'output'
