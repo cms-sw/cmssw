6c8764eb2b9229457f2ebfd25c34e660a08ab24f
jingyu zhang <jzhan30@uic.edu>
2015-03-30 23:40:13 +0200

    Histogram for L1T Stage1

--- DQM/L1TMonitorClient/python/L1TStage1Layer2Client_cfi.py
+++ DQM/L1TMonitorClient/python/L1TStage1Layer2Client_cfi.py
@@ -1,14 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-l1tGctClient = cms.EDAnalyzer("L1TGCTClient",
+l1tStage1Layer2Client = cms.EDAnalyzer("L1TGCTClient",
     prescaleLS = cms.untracked.int32(-1),
-    monitorDir = cms.untracked.string('L1T/L1TGCT'),
+    monitorDir = cms.untracked.string('L1T/stage1layer2'),
     prescaleEvt = cms.untracked.int32(1),
     runInEventLoop=cms.untracked.bool(False),
     runInEndLumi=cms.untracked.bool(True),
     runInEndRun=cms.untracked.bool(True),
     runInEndJob=cms.untracked.bool(False),
-    stage1_layer2_=cms.untracked.bool(False)
+    stage1_layer2_=cms.untracked.bool(True)
 )
-
-
