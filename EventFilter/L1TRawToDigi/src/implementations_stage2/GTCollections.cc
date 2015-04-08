1fc21455b4d7c63fda912115508c32550821d95e
Jim <jim.brooke@cern.ch>
2015-04-08 18:57:36 +0200

    propagate L1TObjectCollections to GTCollections

--- EventFilter/L1TRawToDigi/src/implementations_stage2/GTCollections.cc
+++ EventFilter/L1TRawToDigi/src/implementations_stage2/GTCollections.cc
@@ -6,10 +6,14 @@ namespace l1t {
    namespace stage2 {
       GTCollections::~GTCollections()
       {
-	
-         event_.put(algBlk_);
-         event_.put(extBlk_); 
 
+	event_.put(egammas_, "GT");
+	event_.put(etsums_, "GT");
+	event_.put(jets_, "GT");
+	event_.put(taus_, "GT");
+	
+	event_.put(algBlk_);
+	event_.put(extBlk_); 
 
       }
    }
--- EventFilter/L1TRawToDigi/src/implementations_stage2/GTCollections.h
+++ EventFilter/L1TRawToDigi/src/implementations_stage2/GTCollections.h
@@ -9,26 +9,37 @@
 #include "DataFormats/L1TGlobal/interface/GlobalAlgBlk.h"
 #include "DataFormats/L1TGlobal/interface/GlobalExtBlk.h"
 
-#include "EventFilter/L1TRawToDigi/interface/UnpackerCollections.h"
+//#include "EventFilter/L1TRawToDigi/interface/UnpackerCollections.h"
+#include "L1TObjectCollections.h"
 
 namespace l1t {
   namespace stage2 {
-      class GTCollections : public UnpackerCollections {
+      class GTCollections : public L1TObjectCollections {
          public:
             GTCollections(edm::Event& e) :
-               UnpackerCollections(e),
+               L1TObjectCollections(e),
 		 
 		 algBlk_(new GlobalAlgBlkBxCollection()),
 		 extBlk_(new GlobalExtBlkBxCollection())  {};
 
             virtual ~GTCollections();
             
+	    inline EGammaBxCollection* getEGammas() override { return egammas_.get(); };
+            inline EtSumBxCollection* getEtSums() override { return etsums_.get(); };
+            inline JetBxCollection* getJets() override { return jets_.get(); };
+            inline TauBxCollection* getTaus() override { return taus_.get(); };
+
             inline GlobalAlgBlkBxCollection* getAlgs() { return algBlk_.get(); };
             inline GlobalExtBlkBxCollection* getExts() { return extBlk_.get(); };
 
 
          private:
 	    
+	    std::auto_ptr<EGammaBxCollection> egammas_;
+	    std::auto_ptr<EtSumBxCollection> etsums_;
+	    std::auto_ptr<JetBxCollection> jets_;
+	    std::auto_ptr<TauBxCollection> taus_;
+
             std::auto_ptr<GlobalAlgBlkBxCollection> algBlk_;
             std::auto_ptr<GlobalExtBlkBxCollection> extBlk_;
 
