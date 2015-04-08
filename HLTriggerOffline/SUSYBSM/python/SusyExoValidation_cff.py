9225c9d51090ba6ce14339e6e026f03948f379bc
amkalsi <rimmy330@gmail.com>
2015-04-08 15:05:21 -0500

    Updated HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py file

--- HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SusyExoValidation_cff.py
@@ -109,5 +109,6 @@ HLTSusyExoValSeq_FastSim = cms.Sequence(SUSY_HLT_HT_MET_FASTSIM +
                                         SUSY_HLT_HT_DoubleEle_FASTSIM +
                                         SUSY_HLT_HT_MuEle_FASTSIM +
                                         SUSY_HLT_Muon_BJet_FASTSIM +
-                                        SUSY_HLT_Electron_BJet_FASTSIM)
+                                        SUSY_HLT_Electron_BJet_FASTSIM + 
+                                        SUSY_HLT_Mu_VBF_FASTSIM_POSTPROCESSING )
 
