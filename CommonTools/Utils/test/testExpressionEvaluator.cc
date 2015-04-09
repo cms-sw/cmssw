abfedc4b7e5599b8e2516ad1326d0a794e5b080b
Lukas <lukas.vanelderen@cern.ch>
2015-04-09 22:05:18 +0200

    undo some accidental commit from ktf

--- CommonTools/Utils/test/testExpressionEvaluator.cc
+++ CommonTools/Utils/test/testExpressionEvaluator.cc
@@ -9,7 +9,6 @@
 #include "DataFormats/Candidate/interface/LeafCandidate.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
-#include "Cintex/Cintex.h"
 
 
 class testExpressionEvaluator : public CppUnit::TestFixture {
