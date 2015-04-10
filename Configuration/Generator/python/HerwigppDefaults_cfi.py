2ea97f08b39e5a9196d62b8ab158a79a5b620430
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2015-04-10 15:35:43 +0200

    HerwigPPDefault config: Added NNPDF30NLO

--- Configuration/Generator/python/HerwigppDefaults_cfi.py
+++ Configuration/Generator/python/HerwigppDefaults_cfi.py
@@ -79,6 +79,16 @@ herwigDefaultsBlock = cms.PSet(
                  'set /Herwig/Particles/pbar-:PDF cmsPDFSet',
                  'cd /',
         ),
+        
+        pdfNNPDF30NLO = cms.vstring(
+                'cd /Herwig/Partons',
+                'create ThePEG::LHAPDF cmsPDFSet ThePEGLHAPDF.so',
+                 'set /cmsPDFSet:PDFName NNPDF30_nlo_as_0118.LHgrid',
+                 'set /cmsPDFSet:RemnantHandler HadronRemnants',
+                 'set /Herwig/Particles/p+:PDF cmsPDFSet',
+                 'set /Herwig/Particles/pbar-:PDF cmsPDFSet',
+                 'cd /',
+        ),
 
 	# CME presets
 	##############################
