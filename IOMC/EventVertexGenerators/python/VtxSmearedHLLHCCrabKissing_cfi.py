d26e12e43e3b309f2733b5b2f038ab22e333f3c8
Pedro Silva <psilva@cern.ch>
2015-04-16 16:35:44 +0200

    fixing typo

--- IOMC/EventVertexGenerators/python/VtxSmearedHLLHCCrabKissing_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedHLLHCCrabKissing_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 HLLHCCrabKissingVtxSmearingParameters14TeV=HLLHCCrabKissingVtxSmearingParameters.clone(EprotoninGeV = cms.double(7000.0))
 VtxSmeared = cms.EDProducer("HLLHCEvtVtxGeneratorFix",
-                            HLLHCCrabKissingVtxSmearingParameters,
+                            HLLHCCrabKissingVtxSmearingParameters14TeV,
                             VtxSmearedCommon
                             )
 
