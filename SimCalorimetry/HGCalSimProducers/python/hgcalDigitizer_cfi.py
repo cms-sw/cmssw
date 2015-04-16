df3d80000069e899e9f3856fdef3885e24a74bda
Pedro Silva <psilva@cern.ch>
2015-04-16 14:31:11 +0200

    adding average pulse shape time in each bx bin, and removing average with respect to original toa when there is leakage/fixing bug in local variable defining TDC saturation...

--- SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py
+++ SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py
@@ -19,7 +19,8 @@ hgceeDigitizer = cms.PSet( accumulatorType   = cms.string("HGCDigiProducer"),
                                                feCfg   = cms.PSet( # 0 only ADC, 1 ADC with pulse shape, 2 ADC+TDC with pulse shape
                                                                    fwVersion         = cms.uint32(2),
                                                                    # leakage to bunches -2, -1, in-time, +1, +2, +3 (from J. Kaplon)
-                                                                   adcPulse          = cms.vdouble(0.00,0.017,0.817,0.163,0.003,0.000), 
+                                                                   adcPulse          = cms.vdouble(0.00, 0.017,   0.817,   0.163,  0.003,  0.000), 
+                                                                   pulseAvgT         = cms.vdouble(0.00, 23.42298,13.16733,6.41062,5.03946,4.5320), 
                                                                    # n bits for the ADC 
                                                                    adcNbits          = cms.uint32(10),
                                                                    # ADC saturation
@@ -62,7 +63,8 @@ hgchefrontDigitizer = cms.PSet( accumulatorType   = cms.string("HGCDigiProducer"
                                                     feCfg   = cms.PSet( # 0 only ADC, 1 ADC with pulse shape, 2 ADC+TDC with pulse shape
                                                                         fwVersion         = cms.uint32(2),
                                                                         # leakage to bunches -2, -1, in-time, +1, +2, +3 (from J. Kaplon)
-                                                                        adcPulse          = cms.vdouble(0.00,0.017,0.817,0.163,0.003,0.000), 
+                                                                        adcPulse          = cms.vdouble(0.00, 0.017,   0.817,   0.163,  0.003,  0.000), 
+                                                                        pulseAvgT         = cms.vdouble(0.00, 23.42298,13.16733,6.41062,5.03946,4.5320), 
                                                                         # n bits for the ADC 
                                                                         adcNbits          = cms.uint32(10),
                                                                         # ADC saturation
