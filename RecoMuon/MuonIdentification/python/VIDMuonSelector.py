0df3dd94f8ca21241ea421f7f17546b62bd9c8f2
Lindsey Gray <lindsey.gray@gmail.com>
2015-04-16 21:36:48 +0200

    move pyVID python classes out of __init__.py since otherwise it confuses CMSSW, add Photon pyVID

--- RecoMuon/MuonIdentification/python/VIDMuonSelector.py
+++ RecoMuon/MuonIdentification/python/VIDMuonSelector.py
@@ -1,9 +1,5 @@
-#definition of pythonized VIDElectron class
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/RecoMuon/MuonIdentification/',1)[0])+'/cfipython/slc6_amd64_gcc491/RecoMuon/MuonIdentification')
-
 import ROOT
-from PhysicsTools.SelectorUtils import VIDSelectorBase
+from PhysicsTools.SelectorUtils.VIDSelectorBase import VIDSelectorBase
 
 class VIDMuonSelector(VIDSelectorBase):
     def __init__(self,pythonpset = None):
