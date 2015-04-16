f22b5a292a5271199d14ca7422287835fe90699c
Olga <Olga.Kodolova@cern.ch>
2015-04-16 14:32:25 +0200

    Remove from EventFilter.HcalRawToDigi.HcalRawToDigi_cfi import *

--- Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalPedestal_cff.py
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalPedestal_cff.py
@@ -6,7 +6,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Calibration.HcalAlCaRecoProducers.ALCARECOHcalCalMinBias_cff import *
 hcalDigiAlCaMB.InputLabel = 'hltHcalCalibrationRaw'
-from EventFilter.HcalRawToDigi.HcalRawToDigi_cfi import *
+#from EventFilter.HcalRawToDigi.HcalRawToDigi_cfi import *
 #hcalDigis.InputLabel = 'hltHcalCalibrationRaw'
 from RecoLocalCalo.Configuration.hcalLocalRecoNZS_cff import *
 
