2c5588146866b74b8023e0efcf7642211fb9b4e8
Pedro Silva <psilva@cern.ch>
2015-04-16 13:20:21 +0200

    updating thr for PF, fixing double counting issue for SARS ADC when fwVersion=2

--- RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGCEE_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowRecHitHGCEE_cfi.py
@@ -27,7 +27,7 @@ particleFlowRecHitHGCEE = cms.EDProducer("PFRecHitProducer",
              qualityTests = cms.VPSet( 
                 cms.PSet(
                   name = cms.string("PFRecHitQTestThresholdInMIPs"),
-                  thresholdInMIPs = cms.double(0.77),
+                  thresholdInMIPs = cms.double(0.50),
                   mipValueInkeV = cms.double(55.1),
                   recHitEnergyIs_keV = cms.bool(False),
                   recHitEnergyMultiplier = cms.double(1.0)
