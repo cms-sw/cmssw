d01b97d6a5d3db4eb6fb2b8e1e80895b2b13dd88
Lindsey Gray <lindsey.gray@gmail.com>
2015-04-16 21:36:48 +0200

    move pyVID python classes out of __init__.py since otherwise it confuses CMSSW, add Photon pyVID

--- RecoEgamma/ElectronIdentification/python/VIDElectronSelector.py
+++ RecoEgamma/ElectronIdentification/python/VIDElectronSelector.py
@@ -1,9 +1,5 @@
-#definition of pythonized VIDElectron class
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/RecoEgamma/ElectronIdentification/',1)[0])+'/cfipython/slc6_amd64_gcc491/RecoEgamma/ElectronIdentification')
-
 import ROOT
-from PhysicsTools.SelectorUtils import VIDSelectorBase
+from PhysicsTools.SelectorUtils.VIDSelectorBase import VIDSelectorBase
 
 class VIDElectronSelector(VIDSelectorBase):
     def __init__(self,pythonpset = None):
