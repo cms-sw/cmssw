e90b4f9a9e3022b38c5e4c50ee904683a6a43da7
Geonmo <ry840901@gmail.com>
2015-04-16 18:35:26 +0200

    fix for LS3 configuration.

--- SLHCUpgradeSimulations/Configuration/python/gemCustom.py
+++ SLHCUpgradeSimulations/Configuration/python/gemCustom.py
@@ -2,16 +2,19 @@ import FWCore.ParameterSet.Config as cms
 def customise2023(process):
   if hasattr(process,'digitisation_step') :
     process=customise_digitization(process)
-  if hasattr(process,'dqmHarvesting'):
-    process=customise_harvesting(process)
+  if hasattr(process,'L1simulation_step'):
+    process=customise_L1Emulator2023(process)
   if hasattr(process,'validation_step'):
     process=customise_Validation(process)
+  if hasattr(process,'dqmHarvesting'):
+    process=customise_harvesting(process)
   return process
+
 def customise_digitization(process):
   from SimMuon.GEMDigitizer.customizeGEMDigi import customize_digi_addGEM_muon_only
   process = customize_digi_addGEM_muon_only(process)
   process.simMuonGEMDigis.mixLabel = cms.string("mix")
-  #process.simMuonRPCDigis.digiModel = cms.string('RPCSimParam')
+  process.simMuonRPCDigis.digiModel = cms.string('RPCSimParam')
   #process.simMuonME0Digis.mixLabel = cms.string("mix")
   process.digitisation_step.remove(process.simMuonRPCDigis)
   return process
@@ -32,3 +35,18 @@ def customise_harvesting(process):
   process.genHarvesting += process.gemPostValidation
   return process
 
+def customise_L1Emulator2023(process) :
+  from L1Trigger.CSCTriggerPrimitives.cscTriggerPrimitiveDigisPostLS3_cfi import cscTriggerPrimitiveDigisPostLS3
+  process.simCscTriggerPrimitiveDigis = cscTriggerPrimitiveDigisPostLS3
+  process.simCscTriggerPrimitiveDigis.clctSLHC.clctNplanesHitPattern = 3
+  process.simCscTriggerPrimitiveDigis.clctSLHC.clctPidThreshPretrig = 2
+  process.simCscTriggerPrimitiveDigis.clctParam07.clctPidThreshPretrig = 2
+
+  ## ME21 has its own SLHC processors
+  process.simCscTriggerPrimitiveDigis.alctSLHCME21 = process.simCscTriggerPrimitiveDigis.alctSLHC.clone()
+  process.simCscTriggerPrimitiveDigis.clctSLHCME21 = process.simCscTriggerPrimitiveDigis.clctSLHC.clone()
+  process.simCscTriggerPrimitiveDigis.alctSLHCME21.alctNplanesHitPattern = 3
+  process.simCscTriggerPrimitiveDigis.alctSLHCME21.runME21ILT = cms.bool(True)
+  process.simCscTriggerPrimitiveDigis.clctSLHCME21.clctNplanesHitPattern = 3
+  process.simCscTriggerPrimitiveDigis.clctSLHCME21.clctPidThreshPretrig = 2
+  return process
--- Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
+++ Validation/MuonGEMDigis/python/MuonGEMDigis_cfi.py
@@ -18,7 +18,7 @@ gemPadValidation = cms.EDAnalyzer('GEMPadDigiValidation',
 )
 gemCoPadValidation = cms.EDAnalyzer('GEMCoPadDigiValidation',
   outputFile = cms.string(''),
-  CopadLabel = cms.InputTag('simMuonGEMPadDigis') ,
+  CopadLabel = cms.InputTag('simCscTriggerPrimitiveDigis') ,
   simInputLabel = cms.InputTag('g4SimHits',"MuonGEMHits"),
   nBinGlobalZR = cms.untracked.vdouble(200,200,200,150,180,250), 
   RangeGlobalZR = cms.untracked.vdouble(564,572,786,794,794,802,110,260,170,350,100,350), 
@@ -34,7 +34,7 @@ gemDigiTrackValidation = cms.EDAnalyzer('GEMDigiTrackMatch',
   verboseGEMDigi = cms.untracked.int32(0),
   gemDigiInput = cms.InputTag("simMuonGEMDigis"),
   gemPadDigiInput = cms.InputTag("simMuonGEMPadDigis"),
-  gemCoPadDigiInput = cms.InputTag("simMuonGEMPadDigis", "Coincidence"),
+  gemCoPadDigiInput = cms.InputTag("simCscTriggerPrimitiveDigis"),
   minBXGEM = cms.untracked.int32(-1),
   maxBXGEM = cms.untracked.int32(1),
   matchDeltaStripGEM = cms.untracked.int32(1),
@@ -43,4 +43,4 @@ gemDigiTrackValidation = cms.EDAnalyzer('GEMDigiTrackMatch',
   gemMaxEta = cms.untracked.double(2.45),
 )
 
-gemDigiValidation = cms.Sequence( gemStripValidation+gemPadValidation+gemDigiTrackValidation)
+gemDigiValidation = cms.Sequence( gemStripValidation+gemPadValidation+gemCoPadValidation+gemDigiTrackValidation)
