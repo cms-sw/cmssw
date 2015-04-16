5b23cf858609939392a569966ef8afa57141c997
Pedro Silva <psilva@cern.ch>
2015-04-16 16:16:50 +0200

    adding crab kissing scenario

---
+++ IOMC/EventVertexGenerators/python/VtxSmearedHLLHCCrabKissing_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
+HLLHCCrabKissingVtxSmearingParameters14TeV=HLLHCCrabKissingVtxSmearingParameters.clone(EprotoninGeV = cms.double(7000.0))
+VtxSmeared = cms.EDProducer("HLLHCEvtVtxGeneratorFix",
+                            HLLHCCrabKissingVtxSmearingParameters,
+                            VtxSmearedCommon
+                            )
+
