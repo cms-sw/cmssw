ca05e41a103000f61178e2c7692dd5f85127e849
Nicola Amapane <nicola.amapane@cern.ch>
2015-04-21 22:49:24 +0200

    Add back the additional parabolic field map for the tracker

--- Configuration/StandardSequences/python/MagneticField_38T_cff.py
+++ Configuration/StandardSequences/python/MagneticField_38T_cff.py
@@ -3,3 +3,10 @@
 import FWCore.ParameterSet.Config as cms
 from MagneticField.Engine.volumeBasedMagneticFieldFromDB_cfi import *
 VolumeBasedMagneticFieldESProducer.valueOverride = 18268
+
+
+# Parabolic parametrized magnetic field used for track building.
+# NOTE that as of now  this does not scale with the current from the DB.
+from MagneticField.ParametrizedEngine.ParabolicParametrizedField_cfi import ParametrizedMagneticFieldProducer as ParabolicParametrizedMagneticFieldProducer
+ParabolicParametrizedMagneticFieldProducer.label = "ParabolicMf"
+
