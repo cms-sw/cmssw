f1565e8ac6cfaa41b5becd680183295aca89a8ec
Josh Bendavid <Josh.Bendavid@cern.ch>
2015-04-22 04:10:51 +0200

    update to simplified ecal pfcluster corrections (using sqlite file for now)

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterECAL_cfi.py
@@ -1,5 +1,265 @@
 import FWCore.ParameterSet.Config as cms
 
+#temporarily load regression weights with esprefer
+from CondCore.DBCommon.CondDBCommon_cfi import *
+
+pfcluscorsource = cms.ESSource("PoolDBESSource",
+    CondDBCommon,
+    DumpStat=cms.untracked.bool(False),
+    toGet = cms.VPSet(
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize1_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize1_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize2_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize2_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize1_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize1_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize2_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize2_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_50ns')
+      ),      
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize1_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize1_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize2_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize2_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize1_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize1_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize2_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize2_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_50ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_50ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize1_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize1_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize2_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize2_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize1_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize1_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize2_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize2_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_25ns')
+      ),      
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize1_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize1_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize2_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize2_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize1_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize1_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize2_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize2_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_25ns')
+      ),
+      cms.PSet(
+        record = cms.string('GBRDWrapperRcd'),
+        label = cms.untracked.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_25ns'),
+        tag = cms.string('ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_25ns')
+      ),      
+    )
+)
+
+#pfcluscorsource.connect = cms.string('frontier://FrontierProd/CMS_COND_PAT_000')
+pfcluscorsource.connect = cms.string("sqlite_file:/afs/cern.ch/user/b/bendavid/work/public/ecalPFClusterCorV2_22042015.db")
+
+pfclusprefer = cms.ESPrefer(
+    'PoolDBESSource',
+    'pfcluscorsource',
+    GBRDWrapperRcd = cms.vstring(
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize1_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize2_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize1_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize2_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_50ns',                                
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize1_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize2_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize1_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize2_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_50ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_50ns',      
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize1_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize2_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin1_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin2_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin3_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize1_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize2_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin1_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin2_mean_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin3_mean_25ns',                                
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize1_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize2_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin1_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin2_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EB_pfSize3_ptbin3_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize1_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize2_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin1_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin2_sigma_25ns',
+                                'GBRForestD/ecalPFClusterCorV2_EE_pfSize3_ptbin3_sigma_25ns',                                  
+                                )
+)
+
 #### PF CLUSTER ECAL ####
 
 #energy corrector for corrected cluster producer
@@ -9,7 +269,6 @@ _emEnergyCorrector = cms.PSet(
     applyMVACorrections = cms.bool(True),
     recHitsEBLabel = cms.InputTag('ecalRecHit', 'EcalRecHitsEB'),
     recHitsEELabel = cms.InputTag('ecalRecHit', 'EcalRecHitsEE'),
-    verticesLabel = cms.InputTag('offlinePrimaryVertices'),    
     autoDetectBunchSpacing = cms.bool(True),
 )
 
