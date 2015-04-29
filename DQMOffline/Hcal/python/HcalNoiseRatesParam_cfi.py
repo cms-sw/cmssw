2919c1e84ae61d32deb4e9e03491d5a611ba97f0
Ken Call <ken_call@baylor.edu>
2015-04-29 15:37:54 -0500

    This adds DQMOffline/Hcal and fixes the "severity typo"

---
+++ DQMOffline/Hcal/python/HcalNoiseRatesParam_cfi.py
@@ -0,0 +1,10 @@
+import FWCore.ParameterSet.Config as cms
+
+hcalNoiseRates = cms.EDAnalyzer('HcalNoiseRates',
+#    outputFile   = cms.untracked.string('NoiseRatesRelVal.root'),
+    outputFile   = cms.untracked.string(''),
+    rbxCollName  = cms.untracked.InputTag('hcalnoise'),
+    minRBXEnergy = cms.untracked.double(20.0),
+    minHitEnergy = cms.untracked.double(1.5),
+    useAllHistos = cms.untracked.bool(False)
+)
