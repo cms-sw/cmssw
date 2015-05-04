1d2c2c1c5bedb59143f15d46b4c05ac3d5c16a76
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-04 17:51:10 +0200

    remove METCorrectionsRecord

--- JetMETCorrections/Objects/src/METCorrectionsRecord.cc
+++
@@ -1,5 +0,0 @@
-
-#include "JetMETCorrections/Objects/interface/METCorrectionsRecord.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
-
-EVENTSETUP_RECORD_REG(METCorrectionsRecord);
