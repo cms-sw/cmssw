05ccfaffefb83c6357e5f5cad86e9f3b24482c12
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-04 17:16:14 +0200

    clean up METCorrectorDBWriter.cc

--- JetMETCorrections/Modules/plugins/METCorrectorDBWriter.cc
+++ JetMETCorrections/Modules/plugins/METCorrectorDBWriter.cc
@@ -1,3 +1,4 @@
+// -*- C++ -*-
 
 #include <memory>
 #include <string>
@@ -10,9 +11,9 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
-//#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
 #include "CondFormats/JetMETObjects/interface/METCorrectorParameters.h"
 
+//____________________________________________________________________________||
 class  METCorrectorDBWriter : public edm::EDAnalyzer
 {
  public:
@@ -29,83 +30,35 @@ class  METCorrectorDBWriter : public edm::EDAnalyzer
   std::string payloadTag;
 };
 
-// Constructor
 METCorrectorDBWriter::METCorrectorDBWriter(const edm::ParameterSet& pSet)
 {
   era    = pSet.getUntrackedParameter<std::string>("era");
   algo   = pSet.getUntrackedParameter<std::string>("algo");
-  //payloadTag = "JetCorrectorParametersCollection_"+era+"_"+algo;
   payloadTag = algo;
 }
 
-// Begin Job
 void METCorrectorDBWriter::beginJob()
 {
   std::string path("CondFormats/JetMETObjects/data/");
 
-  //JetCorrectorParametersCollection *payload = new JetCorrectorParametersCollection();
-  //METCorrectorParameters *payload = new METCorrectorParameters();
-   std::cout << "Starting to import payload " << payloadTag << " from text files." << std::endl;
+  std::cout << "Starting to import payload " << payloadTag << " from text files." << std::endl;
 
-   std::string append("_");
-   append += algo;
-   append += ".txt";
-   inputTxtFile = path+era+append;  
-   std::cout << " inputTxtFile " << inputTxtFile << std::endl;
-   std::ifstream input( ("../../../"+inputTxtFile).c_str() );
-   edm::FileInPath fip(inputTxtFile);
-   std::string mSection = "";
-   //METCorrectorParameters *payload = METCorrectorParameters(const std::string& fFile, const std::string& fSection = "");
-   METCorrectorParameters *payload = new METCorrectorParameters(fip.fullPath(),mSection);
-   payload->printScreen();
-   if ( input.good() ) {
-      edm::FileInPath fip(inputTxtFile);
-      std::cout << "Opened file " << inputTxtFile << std::endl;
-   }
-      // create the parameter object from file 
-      //std::vector<std::string> sections;
-      
-   
-      //METCorrectorParameters *payload = METCorrectorParameters(const std::string& fFile, const std::string& fSection = "");
-   /*
-   /// ------------------------------------------------------- 
-
-   for ( int i = 0; i < JetCorrectorParametersCollection::N_LEVELS; ++i ) {
-    
-    std::string append("_");
-    std::string ilev = JetCorrectorParametersCollection::findLabel( static_cast<JetCorrectorParametersCollection::Level_t>(i) );
-    append += ilev;
-    append += "_";
-    append += algo;
-    append += ".txt"; 
-    inputTxtFile = path+era+append;
-    std::ifstream input( ("../../../"+inputTxtFile).c_str() );
-    if ( input.good() ) {
-      edm::FileInPath fip(inputTxtFile);
-      std::cout << "Opened file " << inputTxtFile << std::endl;
-      // create the parameter object from file 
-      std::vector<std::string> sections;
-      JetCorrectorParametersCollection::getSections("../../../"+inputTxtFile, sections );
-      if ( sections.size() == 0 ) {
-	payload->push_back( i, JetCorrectorParameters(fip.fullPath(),"") );
-      }
-      else {
-	for ( std::vector<std::string>::const_iterator isectbegin = sections.begin(), isectend = sections.end(), isect = isectbegin;
-	      isect != isectend; ++isect ) {
-	  payload->push_back( i, JetCorrectorParameters(fip.fullPath(),*isect), ilev + "_" + *isect );	  
-	  std::cout << "Added " << ilev + "_" + *isect <<  " to record " << i << std::endl;
-	}
-      }
-      std::cout << "Added record " << i << std::endl;
-    } else {
-      std::cout << "Did not find JEC file " << inputTxtFile << std::endl;
-    }
-    
+  std::string append("_");
+  append += algo;
+  append += ".txt";
+  inputTxtFile = path + era + append;
+  std::cout << " inputTxtFile " << inputTxtFile << std::endl;
+  std::ifstream input( ("../../../"+inputTxtFile).c_str() );
+  edm::FileInPath fip(inputTxtFile);
+  std::string mSection = "";
+  METCorrectorParameters *payload = new METCorrectorParameters(fip.fullPath(),mSection);
+  payload->printScreen();
+  if ( input.good() ) {
+    edm::FileInPath fip(inputTxtFile);
+    std::cout << "Opened file " << inputTxtFile << std::endl;
   }
-  */  
   std::cout << "Opening PoolDBOutputService" << std::endl;
 
-  // now write it into the DB
   edm::Service<cond::service::PoolDBOutputService> s;
   if (s.isAvailable()) 
     {
@@ -119,6 +72,5 @@ void METCorrectorDBWriter::beginJob()
   
 }
 
-
+//____________________________________________________________________________||
 DEFINE_FWK_MODULE(METCorrectorDBWriter);
-
