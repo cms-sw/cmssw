475e2256dccd21a3ff02c8a34bdaad9e0b8e1225
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-04 17:52:11 +0200

    clean up METCorrectionDBWriter_cfg.py

--- JetMETCorrections/Modules/test/METCorrectionDBWriter_cfg.py
+++ JetMETCorrections/Modules/test/METCorrectionDBWriter_cfg.py
@@ -1,38 +1,38 @@
 
 ##____________________________________________________________________________||
-import FWCore.ParameterSet.Config as cms 
+import FWCore.ParameterSet.Config as cms
 
 ##____________________________________________________________________________||
-process = cms.Process('metdb') 
+process = cms.Process('metdb')
 
 ##____________________________________________________________________________||
-process.load('CondCore.DBCommon.CondDBCommon_cfi') 
-process.CondDBCommon.connect = 'sqlite_file:MET11_V0.db' 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1)) 
-process.source = cms.Source('EmptySource') 
+process.load('CondCore.DBCommon.CondDBCommon_cfi')
+process.CondDBCommon.connect = 'sqlite_file:MET11_V0.db'
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+process.source = cms.Source('EmptySource')
 
 ##____________________________________________________________________________||
-process.PoolDBOutputService = cms.Service('PoolDBOutputService', 
-   process.CondDBCommon, 
-   toPut = cms.VPSet( 
+process.PoolDBOutputService = cms.Service('PoolDBOutputService',
+   process.CondDBCommon,
+   toPut = cms.VPSet(
       cms.PSet(
-         record = cms.string('MetShiftXY'), 
-         #record = cms.string('METCorrectionsRecord'), 
-         tag    = cms.string('METCorrectorParametersCollection_MET15_V0'), 
-         label  = cms.string('MetShiftXY') 
+         record = cms.string('MetShiftXY'),
+         #record = cms.string('METCorrectionsRecord'),
+         tag    = cms.string('METCorrectorParametersCollection_MET15_V0'),
+         label  = cms.string('MetShiftXY')
       )
-   ) 
-) 
+   )
+)
 
 ##____________________________________________________________________________||
-process.dbWriterXYshift = cms.EDAnalyzer('METCorrectorDBWriter', 
-   era    = cms.untracked.string('MET15_V0'), 
-   algo   = cms.untracked.string('MetShiftXY') 
-) 
+process.dbWriterXYshift = cms.EDAnalyzer('METCorrectorDBWriter',
+   era    = cms.untracked.string('MET15_V0'),
+   algo   = cms.untracked.string('MetShiftXY')
+)
 
 ##____________________________________________________________________________||
-process.p = cms.Path( 
+process.p = cms.Path(
     process.dbWriterXYshift
-) 
+)
 
 ##____________________________________________________________________________||
