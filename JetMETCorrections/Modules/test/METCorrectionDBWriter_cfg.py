620e3d5418fb4089e41b74331d03f03f64a94ad4
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-04 17:07:03 +0200

    clean up METCorrectionDBWriter_cfg.py

--- JetMETCorrections/Modules/test/METCorrectionDBWriter_cfg.py
+++ JetMETCorrections/Modules/test/METCorrectionDBWriter_cfg.py
@@ -1,10 +1,17 @@
-# location /JetMETCorrections/Modules/test
+
+##____________________________________________________________________________||
 import FWCore.ParameterSet.Config as cms 
+
+##____________________________________________________________________________||
 process = cms.Process('metdb') 
+
+##____________________________________________________________________________||
 process.load('CondCore.DBCommon.CondDBCommon_cfi') 
 process.CondDBCommon.connect = 'sqlite_file:MET11_V0.db' 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1)) 
 process.source = cms.Source('EmptySource') 
+
+##____________________________________________________________________________||
 process.PoolDBOutputService = cms.Service('PoolDBOutputService', 
    process.CondDBCommon, 
    toPut = cms.VPSet( 
@@ -17,27 +24,15 @@ process.PoolDBOutputService = cms.Service('PoolDBOutputService',
    ) 
 ) 
 
+##____________________________________________________________________________||
 process.dbWriterXYshift = cms.EDAnalyzer('METCorrectorDBWriter', 
    era    = cms.untracked.string('MET15_V0'), 
-   #era    = cms.untracked.string('myEpoch'), 
    algo   = cms.untracked.string('MetShiftXY') 
 ) 
 
-
-
+##____________________________________________________________________________||
 process.p = cms.Path( 
-process.dbWriterXYshift 
-#process.dbWriterAK5PF *
-#process.dbWriterAK5PFchs *
-#process.dbWriterAK5JPT *
-#process.dbWriterAK5TRK *
-#process.dbWriterAK7Calo *
-#process.dbWriterAK7PF *
-#process.dbWriterAK7JPT *
-#process.dbWriterKT4Calo *
-#process.dbWriterKT4PF *
-#process.dbWriterKT6Calo *
-#process.dbWriterKT6PF *
-#process.dbWriterIC5Calo *
-#process.dbWriterIC5PF
+    process.dbWriterXYshift
 ) 
+
+##____________________________________________________________________________||
