bedfcd09930f3f385f8608e6da9543a068e43c1f
Eric <eric.appelt@gmail.com>
2015-05-05 19:21:16 +0200

    add to event content, remove unused parameter

--- RecoHI/HiCentralityAlgos/python/HiClusterCompatibility_cfi.py
+++ RecoHI/HiCentralityAlgos/python/HiClusterCompatibility_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 hiClusterCompatibility = cms.EDProducer("ClusterCompatibilityProducer",
    inputTag      = cms.InputTag( "siPixelRecHits" ),
-   saveTags = cms.bool( False ),
    minZ          = cms.double(-40.0),
    maxZ          = cms.double(40.05),
    zStep         = cms.double(0.2)
--- RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
+++ RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
@@ -2,12 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 RecoHiCentralityFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
 
 RecoHiCentralityRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
 
 RecoHiCentralityAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*')
+    outputCommands = cms.untracked.vstring('keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
