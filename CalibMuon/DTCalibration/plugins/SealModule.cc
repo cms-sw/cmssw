3b583221250fe4abf23573883206e3a2d901720f
Nicola Amapane <nicola.amapane@cern.ch>
2015-05-05 23:00:47 +0200

    more cleanup of unused stuff

--- CalibMuon/DTCalibration/plugins/SealModule.cc
+++ CalibMuon/DTCalibration/plugins/SealModule.cc
@@ -21,7 +21,6 @@
 #include "CalibMuon/DTCalibration/plugins/DTVDriftSegmentCalibration.h"
 #include "CalibMuon/DTCalibration/plugins/DTResidualCalibration.h"
 
-#include "CalibMuon/DTCalibration/plugins/DTFakeTTrigESProducer.h"
 #include "CalibMuon/DTCalibration/plugins/DTFakeT0ESProducer.h"
 
 #include "CalibMuon/DTCalibration/interface/DTTTrigCorrectionFactory.h"
@@ -60,7 +59,6 @@ DEFINE_FWK_MODULE(DTTTrigOffsetCalibration);
 DEFINE_FWK_MODULE(DTVDriftSegmentCalibration);
 DEFINE_FWK_MODULE(DTResidualCalibration);
 
-DEFINE_FWK_EVENTSETUP_SOURCE(DTFakeTTrigESProducer);
 DEFINE_FWK_EVENTSETUP_SOURCE(DTFakeT0ESProducer);
 
 DEFINE_EDM_PLUGIN(DTTTrigCorrectionFactory,dtCalibration::DTTTrigT0SegCorrection,"DTTTrigT0SegCorrection");
