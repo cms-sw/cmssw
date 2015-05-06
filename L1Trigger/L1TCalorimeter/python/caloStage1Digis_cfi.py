8eab9eb335ce22d12d07ad131f5de60d4c928eef
R. Alex Barbieri <luck@mit.edu>
2015-05-06 20:43:06 +0200

    remove parameter which is no handled by CaloConfigHelper

--- L1Trigger/L1TCalorimeter/python/caloStage1Digis_cfi.py
+++ L1Trigger/L1TCalorimeter/python/caloStage1Digis_cfi.py
@@ -4,10 +4,6 @@ caloStage1Digis = cms.EDProducer(
     "L1TStage1Layer2Producer",
     CaloRegions = cms.InputTag("rctUpgradeFormatDigis"),
     CaloEmCands = cms.InputTag("rctUpgradeFormatDigis"),
-    FirmwareVersion = cms.uint32(3),  ## 1=HI algo, 2= pp algo (obsolete), 3= pp algo in HW
     tauMaxJetIsolationA = cms.double(0.1), ## tau isolation cut
     conditionsLabel = cms.string("")
 )
-
-
-
