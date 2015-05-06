9edfa3a19398aa57d3b4a75d8f66781b9b85235a
R. Alex Barbieri <luck@mit.edu>
2015-05-06 21:49:55 +0200

    make sure the updated RCT configuration for HI is in the customise statement.
    Also set default background subtraction for HI to zeroWall method

--- L1Trigger/L1TCalorimeter/python/L1TCaloStage1_HIFromRaw_cff.py
+++ L1Trigger/L1TCalorimeter/python/L1TCaloStage1_HIFromRaw_cff.py
@@ -11,6 +11,7 @@ RCTConfigProducers.eMinForFGCut = cms.double(999)
 
 from L1Trigger.L1TCalorimeter.caloStage1Params_cfi import *
 caloStage1Params.jetSeedThreshold = cms.double(0.)
+caloStage1Params.regionPUSType = cms.string("zeroWall")
 
 from Configuration.StandardSequences.RawToDigi_Repacked_cff import ecalDigis, hcalDigis
 
