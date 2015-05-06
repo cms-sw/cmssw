1ee4c171b3eac9053472d4066cdbf9be35233b57
R. Alex Barbieri <luck@mit.edu>
2015-05-06 20:59:30 +0200

    remove confusing, no-longer-used, python duplicates

--- L1Trigger/L1TCalorimeter/python/caloStage1Digis_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-caloStage1Digis = cms.EDProducer(
-    "L1TStage1Layer2Producer",
-    CaloRegions = cms.InputTag("rctUpgradeFormatDigis"),
-    CaloEmCands = cms.InputTag("rctUpgradeFormatDigis"),
-    tauMaxJetIsolationA = cms.double(0.1), ## tau isolation cut
-    conditionsLabel = cms.string("")
-)
