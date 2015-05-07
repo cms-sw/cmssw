4bd8a9264af43d7fe40cb0dc5ebd753ad295c225
wmtan <wmtan@fnal.gov>
2015-05-08 00:35:38 +0200

    Better consumes interface for TrackerHitAssociator

--- Validation/RecoTrack/interface/SiStripTrackingRecHitsValid.h
+++ Validation/RecoTrack/interface/SiStripTrackingRecHitsValid.h
@@ -421,7 +421,7 @@ class SiStripTrackingRecHitsValid : public DQMEDAnalyzer
   
 
   edm::ParameterSet conf_;
-  TrackerHitAssociator trackerHitAssociator_;
+  TrackerHitAssociator::Config trackerHitAssociatorConfig_;
   unsigned long long m_cacheID_;
   edm::ParameterSet Parameters;
 
