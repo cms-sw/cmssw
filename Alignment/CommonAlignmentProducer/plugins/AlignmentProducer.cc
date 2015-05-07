d9dcd10bfe524ed9718156f606df892ab71d629b
Gianluca <gianluca.cerminara@cern.ch>
2015-05-07 13:07:27 +0200

    remove the dependency from IdealGeometry when not running the muon alignment

--- Alignment/CommonAlignmentProducer/plugins/AlignmentProducer.cc
+++ Alignment/CommonAlignmentProducer/plugins/AlignmentProducer.cc
@@ -643,8 +643,6 @@ void AlignmentProducer::simpleMisalignment_(const Alignables &alivec, const std:
 //__________________________________________________________________________________________________
 void AlignmentProducer::createGeometries_( const edm::EventSetup& iSetup )
 {
-   edm::ESTransientHandle<DDCompactView> cpv;
-   iSetup.get<IdealGeometryRecord>().get( cpv );
 
    if (doTracker_) {
      edm::ESHandle<GeometricDet> geometricDet;
@@ -656,6 +654,9 @@ void AlignmentProducer::createGeometries_( const edm::EventSetup& iSetup )
    }
 
    if (doMuon_) {
+     edm::ESTransientHandle<DDCompactView> cpv;
+     iSetup.get<IdealGeometryRecord>().get( cpv );
+
      edm::ESHandle<MuonDDDConstants> mdc;
      iSetup.get<MuonNumberingRecord>().get(mdc);
      DTGeometryBuilderFromDDD DTGeometryBuilder;
