254d11da6f49bf67a616fac4a26a70819d0edcde
Giacomo Govi <giacomo.govi@cern.ch>
2015-05-08 17:31:13 +0200

    Storing payloads with the related serialization info. Improved error handling in serialization.

--- CondCore/CondDB/src/Serialization.cc
+++ CondCore/CondDB/src/Serialization.cc
@@ -133,3 +133,14 @@ void cond::RootInputArchive::read( const std::type_info& destinationType, void* 
   m_streamer->read( destinationInstance, r_class );
 }
 
+std::string cond::StreamerInfo::jsonString(){
+  std::stringstream ss;
+  ss<<" {"<<std::endl;
+  ss<<"\""<<CMSSW_VERSION_LABEL<<"\": \""<<currentCMSSWVersion()<<"\","<<std::endl;
+  ss<<"\""<<ARCH_LABEL<<"\": \""<<currentArchitecture()<<"\","<<std::endl;
+  ss<<"\""<<TECH_LABEL<<"\": \""<<TECHNOLOGY<<"\","<<std::endl;
+  ss<<"\""<<TECH_VERSION_LABEL<<"\": \""<<VERSION<<"\""<<std::endl;
+  ss<<" }"<<std::endl;
+  return ss.str();
+}
+
