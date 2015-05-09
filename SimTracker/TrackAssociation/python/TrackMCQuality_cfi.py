d1596658e1a92ac09deb02ee628dfb3f00df78f3
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-05-09 17:01:17 +0200

    modify TrackMCQuality to produce a vector of floats, not a trackcollection

--- SimTracker/TrackAssociation/python/TrackMCQuality_cfi.py
+++ SimTracker/TrackAssociation/python/TrackMCQuality_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 TrackMCQuality = cms.EDProducer("TrackMCQuality",
                                 label_tr = cms.InputTag('generalTracks'),
                                 label_tp = cms.InputTag('mix','MergedTrackTruth'),
-                                associator = cms.string('TrackAssociatorByHits')
+                                associator = cms.string('quickTrackAssociatorByHits')
                                 )
 
