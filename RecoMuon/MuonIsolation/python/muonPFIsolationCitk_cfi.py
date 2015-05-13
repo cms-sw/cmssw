7065424231e8df51278b38509ebef06b830a90b0
Junghwan John Goh <jhgoh@cern.ch>
2015-05-13 17:31:44 +0200

    add iso at dR=0.4

--- RecoMuon/MuonIsolation/python/muonPFIsolationCitk_cfi.py
+++ RecoMuon/MuonIsolation/python/muonPFIsolationCitk_cfi.py
@@ -5,37 +5,63 @@ muonPFNoPileUpIsolation = cms.EDProducer(
     srcToIsolate = cms.InputTag("muons"),
     srcForIsolationCone = cms.InputTag('pfNoPileUpCandidates'),
     isolationConeDefinitions = cms.VPSet(
-        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'), 
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
                   coneSize = cms.double(0.3),
                   VetoThreshold = cms.double(0.0),
                   VetoConeSize = cms.double(0.0),
                   isolateAgainst = cms.string('h+'),
                   miniAODVertexCodes = cms.vuint32(2,3) ),
-        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'), 
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
                   coneSize = cms.double(0.3),
                   VetoThreshold = cms.double(0.5),
                   VetoConeSize = cms.double(0.0),
                   isolateAgainst = cms.string('h0'),
                   miniAODVertexCodes = cms.vuint32(2,3) ),
-        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'), 
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
                   coneSize = cms.double(0.3),
                   VetoThreshold = cms.double(0.5),
                   VetoConeSize = cms.double(0.0),
                   isolateAgainst = cms.string('gamma'),
-                  miniAODVertexCodes = cms.vuint32(2,3) )
-        )
-    )
+                  miniAODVertexCodes = cms.vuint32(2,3) ),
+
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
+                  coneSize = cms.double(0.4),
+                  VetoThreshold = cms.double(0.0),
+                  VetoConeSize = cms.double(0.0),
+                  isolateAgainst = cms.string('h+'),
+                  miniAODVertexCodes = cms.vuint32(2,3) ),
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
+                  coneSize = cms.double(0.4),
+                  VetoThreshold = cms.double(0.5),
+                  VetoConeSize = cms.double(0.0),
+                  isolateAgainst = cms.string('h0'),
+                  miniAODVertexCodes = cms.vuint32(2,3) ),
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
+                  coneSize = cms.double(0.4),
+                  VetoThreshold = cms.double(0.5),
+                  VetoConeSize = cms.double(0.0),
+                  isolateAgainst = cms.string('gamma'),
+                  miniAODVertexCodes = cms.vuint32(2,3) ),
+    ),
+)
 
 muonPFPileUpIsolation = cms.EDProducer(
     "CITKPFIsolationSumProducer",
     srcToIsolate = cms.InputTag("muons"),
     srcForIsolationCone = cms.InputTag('pfPileUpAllChargedParticles'),
     isolationConeDefinitions = cms.VPSet(
-        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'), 
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
                   coneSize = cms.double(0.3),
                   VetoThreshold = cms.double(0.5),
                   VetoConeSize = cms.double(0.0),
                   isolateAgainst = cms.string('h+'),
-                  miniAODVertexCodes = cms.vuint32(0,1) )
-        )
-    )
+                  miniAODVertexCodes = cms.vuint32(0,1) ),
+
+        cms.PSet( isolationAlgo = cms.string('MuonPFIsolationWithConeVeto'),
+                  coneSize = cms.double(0.4),
+                  VetoThreshold = cms.double(0.5),
+                  VetoConeSize = cms.double(0.0),
+                  isolateAgainst = cms.string('h+'),
+                  miniAODVertexCodes = cms.vuint32(0,1) ),
+    ),
+)
