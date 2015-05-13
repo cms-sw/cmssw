5df2cbec634c00916de22e19200f462ead0bd302
Junghwan John Goh <jhgoh@cern.ch>
2015-05-13 17:30:23 +0200

    cleanup headers and variables, simpler routines

--- RecoMuon/MuonIsolation/plugins/MuonPFIsolationWithConeVeto.cc
+++ RecoMuon/MuonIsolation/plugins/MuonPFIsolationWithConeVeto.cc
@@ -1,36 +1,10 @@
+#include "PhysicsTools/IsolationAlgos/interface/CITKIsolationConeDefinitionBase.h"
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-
-#include "DataFormats/Candidate/interface/Candidate.h"
-#include "DataFormats/Candidate/interface/CandidateFwd.h"
-
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-
-
 #include "FWCore/Framework/interface/ConsumesCollector.h"
-
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
-
-#include "PhysicsTools/IsolationAlgos/interface/IsoDepositVetoFactory.h"
-#include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
-
-#include "PhysicsTools/IsolationAlgos/interface/CITKIsolationConeDefinitionBase.h"
 #include "DataFormats/Math/interface/deltaR.h"
-
-#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
-
-
-#include <unordered_map>
-
-using namespace reco;
-
-namespace reco {
-  typedef edm::Ptr<reco::Muon> muonPtr;
-}
 
 namespace pat {
   typedef edm::Ptr<pat::PackedCandidate> PackedCandidatePtr;
@@ -41,33 +15,27 @@ public:
   MuonPFIsolationWithConeVeto(const edm::ParameterSet& c) :
     citk::IsolationConeDefinitionBase(c),
     _vetoThreshold(c.getParameter<double>("VetoThreshold")),
-    _vetoConeSize(std::pow(c.getParameter<double>("VetoConeSize"),2.0)),
-    _miniAODVertexCodes(c.getParameter<std::vector<unsigned> >("miniAODVertexCodes")),
-    _isolateAgainst(c.getParameter<std::string>("isolateAgainst")) {
+    _vetoConeSize2(std::pow(c.getParameter<double>("VetoConeSize"),2.0)),
+    _miniAODVertexCodes(c.getParameter<std::vector<unsigned> >("miniAODVertexCodes"))
+  {
     char buf[50];
-    sprintf(buf,"ThresholdVeto%.2f-ConeVeto%.2f", _vetoThreshold, std::sqrt(_vetoConeSize));
+    snprintf(buf,49,"ThresholdVeto%03.0f-ConeVeto%03.0f", 100*_vetoThreshold, 100*std::sqrt(_vetoConeSize2));
     _additionalCode = std::string(buf);
-    auto decimal = _additionalCode.find('.');
-    while( decimal != std::string::npos ) {
-      _additionalCode.erase(decimal,1);
-      decimal = _additionalCode.find('.');
-    }
   }
   MuonPFIsolationWithConeVeto(const MuonPFIsolationWithConeVeto&) = delete;
   MuonPFIsolationWithConeVeto& operator=(const MuonPFIsolationWithConeVeto&) =delete;
-  
+
   void setConsumes(edm::ConsumesCollector) {}
 
   bool isInIsolationCone(const reco::CandidatePtr& physob,
 			 const reco::CandidatePtr& other) const override final;
-  
+
   //! Destructor
   virtual ~MuonPFIsolationWithConeVeto(){};
-  
-private:    
-  const double _vetoThreshold, _vetoConeSize;  
+
+private:
+  const double _vetoThreshold, _vetoConeSize2;
   const std::vector<unsigned> _miniAODVertexCodes;
-  const std::string _isolateAgainst;
   edm::EDGetTokenT<reco::VertexCollection> _vtxToken;
 };
 
@@ -75,16 +43,12 @@ DEFINE_EDM_PLUGIN(CITKIsolationConeDefinitionFactory,
 		  MuonPFIsolationWithConeVeto,
 		  "MuonPFIsolationWithConeVeto");
 
-bool MuonPFIsolationWithConeVeto::
-isInIsolationCone(const reco::CandidatePtr& physob,
-		  const reco::CandidatePtr& iso_obj  ) const {
-  reco::muonPtr muonref(physob);
-  pat::PackedCandidatePtr aspacked(iso_obj);
-  reco::PFCandidatePtr aspf(iso_obj);
-  //bool isEB = ( abs(muonref->eta()) < 1.1 );
-  const double deltar2 = reco::deltaR2(*physob,*iso_obj);  
-  const double vetoConeSize2 =  _vetoConeSize;
-  const double vetoThreshold =  _vetoThreshold;
+bool MuonPFIsolationWithConeVeto::isInIsolationCone(const reco::CandidatePtr& physob,
+                                                    const reco::CandidatePtr& iso_obj  ) const {
+  const pat::PackedCandidatePtr aspacked(iso_obj);
+  const reco::PFCandidatePtr aspf(iso_obj);
+  const double deltar2 = reco::deltaR2(*physob,*iso_obj);
+
   bool result = true;
   if( aspacked.isNonnull() && aspacked.get() ) {
     if( aspacked->charge() != 0 ) {
@@ -94,12 +58,12 @@ isInIsolationCone(const reco::CandidatePtr& physob,
           is_vertex_allowed = true;
           break;
         }
-      }      
+      }
       result *= ( is_vertex_allowed );
     }
-    result *= aspacked->pt() >  vetoThreshold && deltar2 > vetoConeSize2 && deltar2 < _coneSize2 ;
-  } else if ( aspf.isNonnull() && aspf.get() ) {    
-    result *= aspf->pt() >  vetoThreshold && deltar2 > vetoConeSize2 && deltar2 < _coneSize2 ;
+    result *= aspacked->pt() > _vetoThreshold && deltar2 > _vetoConeSize2 && deltar2 < _coneSize2 ;
+  } else if ( aspf.isNonnull() && aspf.get() ) {
+    result *= aspf->pt() > _vetoThreshold && deltar2 > _vetoConeSize2 && deltar2 < _coneSize2 ;
   } else {
     throw cms::Exception("InvalidIsolationInput")
       << "The supplied candidate to be used as isolation "
