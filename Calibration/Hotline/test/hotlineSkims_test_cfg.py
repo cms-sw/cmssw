d26a767606f675ed9f12b353563b20241808fb6a
Dustin <dustin.james.anderson@cern.ch>
2015-05-13 11:10:59 +0200

    Added new input file and updated configuration

--- Calibration/Hotline/test/hotlineSkims_test_cfg.py
+++ Calibration/Hotline/test/hotlineSkims_test_cfg.py
@@ -2,11 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("skim")
 
-process.load("Configuration.Geometry.GeometryIdeal_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-from Configuration.AlCa.autoCond_condDBv2 import autoCond
-process.GlobalTag.globaltag=autoCond['run2_mc']
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag,'auto:run2_mc_GRun')
 
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
@@ -15,15 +14,13 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:step3QCD.root')
+        fileNames = cms.untracked.vstring('root://eoscms:///eos/cms/store/relval/CMSSW_7_5_0_pre1/RelValQCD_Pt_3000_3500_13/GEN-SIM-RECO/MCRUN2_74_V7-v1/00000/826FE575-5CE3-E411-BA9F-00248C0BE012.root')
 )
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
-process.load('Configuration.StandardSequences.GeometryExtended_cff')
-process.load('Configuration/StandardSequences/Reconstruction_cff')
-process.load('Configuration/EventContent/EventContent_cff')
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
 
 #hotline filters
 process.load("Calibration.Hotline.hotlineSkims_cff")
