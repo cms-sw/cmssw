5f4ed125c050db6a5fb74dd67b7416a068b05309
Chris <cfmcginn@mit.edu>
2015-05-14 20:34:55 +0200

    Removed cff file per merge request

--- RecoHI/HiJetAlgos/python/ParticleTowerProducer_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-particleTowerProducer = cms.EDProducer('ParticleTowerProducer',
-                                       src    = cms.InputTag('particleFlowTmp'),
-                                       useHF = cms.untracked.bool(True)
-                                       )
-
