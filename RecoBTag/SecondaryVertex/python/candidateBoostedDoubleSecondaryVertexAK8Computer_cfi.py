159e383bd47772adfdf744cb889228664cea6d94
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2015-05-16 16:44:27 -0500

    added the first implementation of the boosted double SV tagger

--- RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexAK8Computer_cfi.py
+++ RecoBTag/SecondaryVertex/python/candidateBoostedDoubleSecondaryVertexAK8Computer_cfi.py
@@ -1,3 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-candidateBoostedDoubleSecondaryVertexAK8Computer = cms.ESProducer("CandidateBoostedDoubleSecondaryVertexESProducer")
+candidateBoostedDoubleSecondaryVertexAK8Computer = cms.ESProducer("CandidateBoostedDoubleSecondaryVertexESProducer",
+    beta = cms.double(1.0),
+    R0 = cms.double(0.8),
+    maxSVDeltaRToJet = cms.double(0.7),
+    weightFile = cms.FileInPath('RecoBTag/SecondaryVertex/data/BoostedDoubleSV_AK08_BDT.weights.xml.gz')
+)
