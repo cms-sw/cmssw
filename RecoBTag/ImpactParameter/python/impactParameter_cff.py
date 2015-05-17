3f41625e141735d9cec2332fdc4521e019b4d9b2
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2015-05-17 09:44:00 -0500

    added boosted double SV tagger for CA15 jets

--- RecoBTag/ImpactParameter/python/impactParameter_cff.py
+++ RecoBTag/ImpactParameter/python/impactParameter_cff.py
@@ -4,6 +4,7 @@ from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from RecoBTag.ImpactParameter.impactParameterTagInfos_cfi import *
 from RecoBTag.ImpactParameter.pfImpactParameterTagInfos_cfi import *
 from RecoBTag.ImpactParameter.pfImpactParameterTagInfosAK8_cfi import *
+from RecoBTag.ImpactParameter.pfImpactParameterTagInfosCA15_cfi import *
 from RecoBTau.JetTagComputer.jetTagRecord_cfi import *
 # MVA
 from RecoBTag.ImpactParameter.impactParameterMVAComputer_cfi import *
