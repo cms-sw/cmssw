77393c776da225d31352018a4f52ef9e09158c88
Marton <marton.bartok@cern.ch>
2015-05-18 14:51:41 +0200

    adding_config_compare_to_reader

--- CondTools/SiPixel/test/SiPixelDynamicInefficiencyReader.h
+++ CondTools/SiPixel/test/SiPixelDynamicInefficiencyReader.h
@@ -29,6 +29,14 @@
 
   private:
     bool printdebug_;
+    double thePixelEfficiency[20];     // Single pixel effciency
+    double thePixelColEfficiency[20];  // Column effciency
+    double thePixelChipEfficiency[20]; // ROC efficiency
+    std::vector<double> theLadderEfficiency_BPix[20]; // Ladder efficiency
+    std::vector<double> theModuleEfficiency_BPix[20]; // Module efficiency
+    std::vector<double> thePUEfficiency[20]; // Instlumi dependent efficiency
+    double theInnerEfficiency_FPix[20]; // Fpix inner module efficiency
+    double theOuterEfficiency_FPix[20]; // Fpix outer module efficiency
   };
 
 #endif
