8b5dbcaf335aa74aee92925960dada78c8ef63d5
Jim <jim.brooke@cern.ch>
2015-05-18 11:10:55 +0200

    update GCT JEC HB,HE to 13 TeV.  HF remains legacy JEC

--- L1TriggerConfig/GctConfigProducers/python/l1GctConfig_cfi.py
+++ L1TriggerConfig/GctConfigProducers/python/l1GctConfig_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 L1GctConfigProducers = cms.ESProducer("L1GctConfigProducers",
-    JetFinderCentralJetSeed = cms.double(0.0),
-    JetFinderForwardJetSeed = cms.double(0.0),
+    JetFinderCentralJetSeed = cms.double(5.0),
+    JetFinderForwardJetSeed = cms.double(5.0),
     TauIsoEtThreshold = cms.double(2.0),
     HtJetEtThreshold = cms.double(10.0),
     MHtJetEtThreshold = cms.double(10.0),
@@ -20,24 +20,24 @@ L1GctConfigProducers = cms.ESProducer("L1GctConfigProducers",
     # "PowerSeries", "ORCAStyle" are also available, but not recommended
     CalibrationStyle = cms.string('PF'),
     PFCoefficients = cms.PSet(
-           nonTauJetCalib0 = cms.vdouble(1.114,2.297,5.959,1.181,0.7286,0.3673  ),
-           nonTauJetCalib1 = cms.vdouble(0.7842,4.331,2.672,0.5743,0.8811,0.4085),
-           nonTauJetCalib2 = cms.vdouble(0.961,2.941,2.4,1.248,0.666,0.1041     ),
-           nonTauJetCalib3 = cms.vdouble(0.6318,6.6,3.21,0.8551,0.9786,0.291    ),
-           nonTauJetCalib4 = cms.vdouble(0.3456,8.992,3.165,0.5798,2.146,0.4912 ),
-           nonTauJetCalib5 = cms.vdouble(0.8501,3.892,2.466,1.236,0.8323,0.1809 ),
-           nonTauJetCalib6 = cms.vdouble(0.9027,2.581,1.453,1.029,0.6767,-0.1476),
-           nonTauJetCalib7 = cms.vdouble(1.117,2.382,1.769,0.0,-1.306,-0.4741   ),
-           nonTauJetCalib8 = cms.vdouble(1.634,-1.01,0.7184,1.639,0.6727,-0.2129),
-           nonTauJetCalib9 = cms.vdouble(0.9862,3.138,4.672,2.362,1.55,-0.7154  ),
-           nonTauJetCalib10 = cms.vdouble(1.245,1.103,1.919,0.3054,5.745,0.8622 ),
-           tauJetCalib0 = cms.vdouble(1.114,2.297,5.959,1.181,0.7286,0.3673  ),
-           tauJetCalib1 = cms.vdouble(0.7842,4.331,2.672,0.5743,0.8811,0.4085),
-           tauJetCalib2 = cms.vdouble(0.961,2.941,2.4,1.248,0.666,0.1041     ),
-           tauJetCalib3 = cms.vdouble(0.6318,6.6,3.21,0.8551,0.9786,0.291    ),
-           tauJetCalib4 = cms.vdouble(0.3456,8.992,3.165,0.5798,2.146,0.4912 ),
-           tauJetCalib5 = cms.vdouble(0.8501,3.892,2.466,1.236,0.8323,0.1809 ),
-           tauJetCalib6 = cms.vdouble(0.9027,2.581,1.453,1.029,0.6767,-0.1476),
+        nonTauJetCalib0 = cms.vdouble(3.01791485,104.05404643,4.37671951,-511.10059192,0.00975609,-17.19462193),
+        nonTauJetCalib1 = cms.vdouble(6.51467316,55.13357477,4.42877284,-75.99041992,0.00574098,-16.07076961),
+        nonTauJetCalib2 = cms.vdouble(2.66631385,59.40320962,3.64453392,-458.28002325,0.00869394,-19.25715247),
+        nonTauJetCalib3 = cms.vdouble(0.71562578,54.71640347,2.72016310,-9009.00062999,0.01044813,-24.07475734),
+        nonTauJetCalib4 = cms.vdouble(1.39149927,31.51196317,2.49437717,-533.64858609,0.01071669,-18.94264462),
+        nonTauJetCalib5 = cms.vdouble(1.56945764,10.19680631,1.61637149,-37.03219196,0.00785145,-17.15336214),
+        nonTauJetCalib6 = cms.vdouble(1.57965654,9.64485987,1.84352989,-53.54520963,0.00818620,-18.04202617),
+        nonTauJetCalib7 = cms.vdouble(1.117,2.382,1.769,0.0,-1.306,-0.4741   ), # OLD HF CALIBS!
+        nonTauJetCalib8 = cms.vdouble(1.634,-1.01,0.7184,1.639,0.6727,-0.2129),
+        nonTauJetCalib9 = cms.vdouble(0.9862,3.138,4.672,2.362,1.55,-0.7154  ),
+        nonTauJetCalib10 = cms.vdouble(1.245,1.103,1.919,0.3054,5.745,0.8622 ),
+        tauJetCalib0 = cms.vdouble(3.01791485,104.05404643,4.37671951,-511.10059192,0.00975609,-17.19462193),
+        tauJetCalib1 = cms.vdouble(6.51467316,55.13357477,4.42877284,-75.99041992,0.00574098,-16.07076961),
+        tauJetCalib2 = cms.vdouble(2.66631385,59.40320962,3.64453392,-458.28002325,0.00869394,-19.25715247),
+        tauJetCalib3 = cms.vdouble(0.71562578,54.71640347,2.72016310,-9009.00062999,0.01044813,-24.07475734),
+        tauJetCalib4 = cms.vdouble(1.39149927,31.51196317,2.49437717,-533.64858609,0.01071669,-18.94264462),
+        tauJetCalib5 = cms.vdouble(1.56945764,10.19680631,1.61637149,-37.03219196,0.00785145,-17.15336214),
+        tauJetCalib6 = cms.vdouble(1.57965654,9.64485987,1.84352989,-53.54520963,0.00818620,-18.04202617)
     )
 
 )
