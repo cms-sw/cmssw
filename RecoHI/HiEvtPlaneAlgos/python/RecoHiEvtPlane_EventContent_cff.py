5731f4cd37ed7b6283234c6fb4285c6c9e22891d
Quan Wang <BetterWang@gmail.com>
2015-05-20 23:37:06 +0200

    [!] reset

--- RecoHI/HiEvtPlaneAlgos/python/RecoHiEvtPlane_EventContent_cff.py
+++ RecoHI/HiEvtPlaneAlgos/python/RecoHiEvtPlane_EventContent_cff.py
@@ -9,9 +9,5 @@ RecoHiEvtPlaneRECO = cms.PSet(
     )
 
 RecoHiEvtPlaneAOD = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*',
-                                           'keep ZDCRecHitsSorted_zdcreco_*_*',
-                                           'keep ZDCDataFramesSorted_hcalDigis_*_*',
-                                           'keep HFRecHitsSorted_hfreco_*_*'
-                                          )
+    outputCommands = cms.untracked.vstring('keep recoEvtPlanes_hiEvtPlane_*_*')
     )
