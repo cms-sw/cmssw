3ba4974e913667bce422a9059181674f7880fc3f
wmtan <wmtan@fnal.gov>
2015-05-20 06:18:09 +0200

    Remove deprecated unused interface

--- SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h
+++ SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h
@@ -68,10 +68,6 @@ class TrackerHitAssociator {
   // The constructor supporting the consumes interface and tokens
   TrackerHitAssociator(const edm::Event& e, const Config& config);
 
-  TrackerHitAssociator(const edm::ParameterSet& conf, edm::ConsumesCollector&& iC); // deprecated ctor supports consumes but not tokens
-  TrackerHitAssociator(const edm::Event& e); // deprecated simple ctor
-  TrackerHitAssociator(const edm::Event& e, const edm::ParameterSet& conf); // deprecated ctor with config
-
   // Destructor
   virtual ~TrackerHitAssociator(){}
   
@@ -112,8 +108,6 @@ class TrackerHitAssociator {
   typedef std::vector<std::string> vstring;
 
   void makeMaps(const edm::Event& theEvent, const Config& config);
-  void makeMaps(const edm::Event& theEvent, const vstring& trackerContainers); // deprecated
-
   edm::Handle< edm::DetSetVector<StripDigiSimLink> >  stripdigisimlink;
   edm::Handle< edm::DetSetVector<PixelDigiSimLink> >  pixeldigisimlink;
   bool doPixel_, doStrip_, doTrackAssoc_, assocHitbySimTrack_;
