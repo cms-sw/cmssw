82d63947ffbdb10b944dbe6e20ffa2db8265f384
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2015-05-21 17:29:47 +0200

    Herwigpp: Defined the often mistakenly used CTEQ6LL PDF by CTEQ6L1

--- Configuration/Generator/python/HerwigppPDF_CTEQ6_LO_cfi.py
+++ Configuration/Generator/python/HerwigppPDF_CTEQ6_LO_cfi.py
@@ -4,17 +4,6 @@ import FWCore.ParameterSet.Config as cms
 
 herwigppPDFSettingsBlock = cms.PSet(
 
-	hwpp_pdf_CTEQ6LL = cms.vstring(
-                'cd /Herwig/Partons',
-                'create ThePEG::LHAPDF cmsPDFSet ThePEGLHAPDF.so',
-                'set cmsPDFSet:PDFName cteq6ll.LHpdf',
-                'set cmsPDFSet:RemnantHandler HadronRemnants',
-                'set /Herwig/Particles/p+:PDF cmsPDFSet',
-                'set /Herwig/Particles/pbar-:PDF cmsPDFSet',
-                '+hwpp_ue_EE5C', # Tune for CTEQ6L1 from 2.7, see HerwigppUE_EE_5C
-                'cd /',
-        ),
-
         hwpp_pdf_pdfCTEQ6L1 = cms.vstring(
                 'cd /Herwig/Partons',
                 'create ThePEG::LHAPDF cmsPDFSet ThePEGLHAPDF.so',
@@ -24,6 +13,10 @@ herwigppPDFSettingsBlock = cms.PSet(
                 'set /Herwig/Particles/pbar-:PDF cmsPDFSet',
                 '+hwpp_ue_EE5C', # Tune for CTEQ6L1 from 2.7, see HerwigppUE_EE_5C                 
                 'cd /',
+        ),
+
+	hwpp_pdf_CTEQ6LL = cms.vstring(
+                '+hwpp_pdf_CTEQ6L1',
         ),
 )
 
