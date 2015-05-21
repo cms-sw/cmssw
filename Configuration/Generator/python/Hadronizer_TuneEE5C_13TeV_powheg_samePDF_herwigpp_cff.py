02baa4415e525c35e97e6048a29f42cf83cbfcda
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2015-05-21 22:22:06 +0200

    HerwigPP: Added Hadronizer to shower either a MadGraph5_aMC@NLO or a Powheg LHE file while using a PDF in the shower which must match with the hard subprocess

---
+++ Configuration/Generator/python/Hadronizer_TuneEE5C_13TeV_powheg_samePDF_herwigpp_cff.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.HerwigppDefaults_cfi import *
+from Configuration.Generator.HerwigppUE_EE_5C_cfi import *
+from Configuration.Generator.HerwigppPDF_CTEQ6_LO_cfi import *									# Import CTEQ6L PDF as shower pdf
+from Configuration.Generator.HerwigppEnergy_13TeV_cfi import *
+from Configuration.Generator.HerwigppLHEFile_cfi import *
+
+generator = cms.EDFilter("ThePEGHadronizerFilter",
+	herwigDefaultsBlock,
+	herwigppUESettingsBlock,
+	herwigppPDFSettingsBlock,
+	herwigppEnergySettingsBlock,
+	herwigppLHEFileSettingsBlock,
+
+	configFiles = cms.vstring(),
+	parameterSets = cms.vstring(
+		'hwpp_cmsDefaults',
+		'hwpp_ue_EE5C',
+		'hwpp_cm_13TeV',
+		'hwpp_pdf_CTEQ6L1',			# Shower PDF matching with the tune
+		'hwpp_LHE_Powheg',
+	),
+
+        crossSection = cms.untracked.double(-1),
+        filterEfficiency = cms.untracked.double(1.0),
+)
+ProductionFilterSequence = cms.Sequence(generator)
