b207c44d4cc588ea85d21df5ac09a7117c3f8f04
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2015-05-21 21:52:48 +0200

    HerwigPP: Example file with dummy process created

--- Configuration/Generator/python/Herwigpp_DummyProcess_cff.py
+++ Configuration/Generator/python/Herwigpp_DummyProcess_cff.py
@@ -5,8 +5,7 @@ from Configuration.Generator.HerwigppUE_EE_5C_cfi import *
 from Configuration.Generator.HerwigppPDF_CTEQ6_LO_cfi import *
 from Configuration.Generator.HerwigppEnergy_13TeV_cfi import *
 
-
-generator = cms.EDFilter("ThePEGHadronizerFilter",
+generator = cms.EDFilter("ThePEGGeneratorFilter",
 	herwigDefaultsBlock,
 	herwigppUESettingsBlock,
 	herwigppPDFSettingsBlock,
@@ -18,9 +17,15 @@ generator = cms.EDFilter("ThePEGHadronizerFilter",
 		'hwpp_ue_EE5C',
 		'hwpp_pdf_CTEQ6L1',
 		'hwpp_cm_13TeV',
+		'dummyprocess',
+
 	),
 
-        crossSection = cms.untracked.double(-1.),
+	dummyprocess = cms.vstring(
+                'insert /Herwig/MatrixElements/SimpleQCD:MatrixElements[0] /Herwig/MatrixElements/MEPP2ttbarH',
+	),
+
+        crossSection = cms.untracked.double(-1),
         filterEfficiency = cms.untracked.double(1.0),
 )
 ProductionFilterSequence = cms.Sequence(generator)
