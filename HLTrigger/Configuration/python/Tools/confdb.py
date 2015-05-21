869f55e616cd926c2edd6261e2c008b681ca53a4
Andrea <perrotta@cern.ch>
2015-05-21 14:58:50 +0200

    fix

--- HLTrigger/Configuration/python/Tools/confdb.py
+++ HLTrigger/Configuration/python/Tools/confdb.py
@@ -267,7 +267,7 @@ process = loadL1Menu(process)
         elif 'LowPU' in self.config.type :
           self.data += """
 # load 2015 Run-2 L1 Menu for HIon
-from L1Trigger.Configuration.customise_overwriteL1Menu import  L1Menu_Collisions_low_v2 as loadL1Menu
+from L1Trigger.Configuration.customise_overwriteL1Menu import  L1Menu_Collisions2015_lowPU_v2 as loadL1Menu
 process = loadL1Menu(process)
 """
         else :
