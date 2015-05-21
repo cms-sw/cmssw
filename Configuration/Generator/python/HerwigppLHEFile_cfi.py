e5f9e3781aebc19f752b2c556172fb8ac48fbb47
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2015-05-21 20:55:07 +0200

    HerwigPP: LHEFile showering Drastically improved the options to shower a LHE file

---
+++ Configuration/Generator/python/HerwigppLHEFile_cfi.py
@@ -0,0 +1,79 @@
+import FWCore.ParameterSet.Config as cms
+
+# Read in a LHE file from MadGraph5_aMC@NLO
+
+herwigppLHEFileSettingsBlock = cms.PSet(
+
+    hwpp_LHE_Common = cms.vstring(
+
+        'create ThePEG::Cuts /Herwig/Cuts/NoCuts', 									# Use no cuts in shower step, just shower
+
+        'cd /Herwig/EventHandlers', 
+        'create ThePEG::LesHouchesInterface /Herwig/EventHandlers/LHEReader', 
+        'set /Herwig/EventHandlers/LHEReader:Cuts /Herwig/Cuts/NoCuts', 						# No cuts while read in of LHE file
+        'set /Herwig/EventHandlers/LHEReader:MomentumTreatment RescaleEnergy', 	
+        'set /Herwig/EventHandlers/LHEReader:WeightWarnings 0', 							# Suppress weight warnings 
+        'set /Herwig/EventHandlers/LHEReader:InitPDFs 0',								# Explicitly set PDF of hard subprocess / Do not retrieve PDF from LHEReader
+
+        'create ThePEG::LesHouchesEventHandler /Herwig/EventHandlers/LHEHandler',
+        'insert /Herwig/EventHandlers/LHEHandler:LesHouchesReaders 0 /Herwig/EventHandlers/LHEReader', 
+        'set /Herwig/EventHandlers/LHEHandler:WeightOption VarNegWeight', 						# Allow positive and negative event weight
+        'set /Herwig/EventHandlers/LHEHandler:PartonExtractor /Herwig/Partons/QCDExtractor', 
+        'set /Herwig/EventHandlers/LHEHandler:CascadeHandler /Herwig/Shower/ShowerHandler', 
+        'set /Herwig/EventHandlers/LHEHandler:HadronizationHandler /Herwig/Hadronization/ClusterHadHandler', 		# Switch Hadronization on
+        'set /Herwig/EventHandlers/LHEHandler:DecayHandler /Herwig/Decays/DecayHandler', 				# Switch Decay on
+        'insert /Herwig/EventHandlers/LHEHandler:PreCascadeHandlers 0 /Herwig/NewPhysics/DecayHandler', 		# Needed in 2.7, must be removed in 3.0
+
+        'cd /Herwig/Generators', 
+        'set /Herwig/Generators/LHCGenerator:EventHandler /Herwig/EventHandlers/LHEHandler', 				# Activate LHEHandler
+
+        'cd /Herwig/Shower', 
+        'set /Herwig/Shower/Evolver:MaxTry 100',									# Try to shower an event maximum 100 times
+        'set /Herwig/Shower/Evolver:HardVetoScaleSource Read', 								# Read event to define hard veto scale
+        'set /Herwig/Shower/Evolver:MECorrMode No', 									# Switch off ME corrections
+
+        'cd /', 
+        'set /Herwig/Shower/KinematicsReconstructor:ReconstructionOption General', 
+        'set /Herwig/Shower/KinematicsReconstructor:InitialInitialBoostOption LongTransBoost', 
+
+        'cd /',
+    ),
+
+    # Showering MadGraph5_aMC@NLO LHE files: The same PDF for the hard subprocess and the shower must be used
+    hwpp_LHE_MadGraph = cms.vstring(
+
+    	'+hwpp_LHE_Common',      
+	'set /Herwig/EventHandlers/LHEReader:PDFA /Herwig/Partons/cmsPDFSet', 						# Shower PDF defined by HerwigppPDF_
+        'set /Herwig/EventHandlers/LHEReader:PDFB /Herwig/Partons/cmsPDFSet', 						
+    ),
+
+    # Additional common block for Powheg
+    hwpp_LHE_Powheg_Common = cms.vstring(
+
+    	'+hwpp_LHE_Common',
+	'cd /Herwig/Shower', 
+        'set /Herwig/Shower/Evolver:HardVetoMode Yes',									# Treat hardest emission differently
+        'set /Herwig/Shower/Evolver:HardVetoReadOption PrimaryCollision',			
+        'cd /', 
+    ),
+
+    # Showering Powheg LHE files with the same PDF for the hard subprocess and the shower  
+    hwpp_LHE_Powheg = cms.vstring(
+
+    	'+hwpp_LHE_Powheg_Common',       
+	'set /Herwig/EventHandlers/LHEReader:PDFA /Herwig/Partons/cmsPDFSet', 						# Shower PDF defined by HerwigppPDF_
+        'set /Herwig/EventHandlers/LHEReader:PDFB /Herwig/Partons/cmsPDFSet', 						
+    ),
+
+    # Showering Powheg LHE files with a different PDF for the hard subprocess 
+    # The shower pdf is the standard PDF which one can get including a predefined PDF using HerwigppPDF_
+    # The additional pdf of the hard subprocess is also predefined in HerwigppPDF_. However it has the additional suffix _Hard
+    # E.g. hwpp_pdf_NNPDF30NLO shower pdf, hwpp_pdf_NNPDF30NLO_Hard hard subprocess pdf
+    hwpp_LHE_Powheg_DifferentPDFs = cms.vstring(
+
+    	'+hwpp_LHE_Powheg_Common', 
+	'set /Herwig/EventHandlers/LHEReader:PDFA /Herwig/Partons/cmsHardPDFSet', 					# Hard subprocess PDF defined by HerwigppPDF_
+        'set /Herwig/EventHandlers/LHEReader:PDFB /Herwig/Partons/cmsHardPDFSet', 						
+    ),
+)
+
