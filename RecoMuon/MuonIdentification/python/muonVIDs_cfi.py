a449c465fa72cb837ffb7b74e6d28a730c45c124
Junghwan John Goh <jhgoh@cern.ch>
2015-05-22 15:12:38 +0200

    remove dupl

--- RecoMuon/MuonIdentification/python/muonVIDs_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-muonVIDs = cms.EDProducer("VersionedMuonIdProducer",
-    physicsObjectSrc = cms.InputTag('muons'),
-    physicsObjectIDs = cms.VPSet( )
-)
-
