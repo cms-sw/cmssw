ac2a81cd8ff5716ba36ccbe65b98422b64b439b3
Michael Mulhearn <mulhearn@cern.ch>
2015-05-25 23:46:28 +0200

    call common Stage1 customs when 50ns and LowPU customization versions are called

--- L1Trigger/L1TCommon/python/customsPostLS1.py
+++ L1Trigger/L1TCommon/python/customsPostLS1.py
@@ -80,11 +80,15 @@ def customiseSimL1EmulatorForStage1(process):
 from L1Trigger.Configuration.customise_overwriteL1Menu import *
 
 def customiseSimL1EmulatorForPostLS1_lowPU(process):
+    # load the Stage 1 configuration
+    process = customiseSimL1EmulatorForStage1(process)
     # move to the lowPU v2 L1 menu once the HLT has been updated accordingly
     process = L1Menu_Collisions2015_lowPU_v2(process)
     return process
 
 def customiseSimL1EmulatorForPostLS1_50ns(process):
+    # load the Stage 1 configuration
+    process = customiseSimL1EmulatorForStage1(process)
     # move to the 50ns v0 L1 menu once the HLT has been updated accordingly
     process = L1Menu_Collisions2015_50ns_v1(process)
     return process
