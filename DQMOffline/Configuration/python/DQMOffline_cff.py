47b55ae9683b53c2b149be790e54d4b9f7d775b5
Matthias <matthias.artur.weber@cern.ch>
2015-05-26 20:17:29 +0200

    latest changes in config file

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -121,7 +121,7 @@ DQMOfflineEGamma = cms.Sequence( egammaDQMOffline )
 
 DQMOfflineBTag = cms.Sequence( bTagPlotsDATA )
                                                                  
-DQMOfflineMiniAOD = cms.Sequence( miniAODDQMSequence*jetMETDQMOfflineSourceMiniAOD  )
+DQMOfflineMiniAOD = cms.Sequence( miniAODDQMSequence*jetMETDQMOfflineSourceMiniAOD )
 
 DQMOfflineNoHWW = cms.Sequence(DQMOffline)
 DQMOfflineNoHWW.remove(hwwAnalyzer)
