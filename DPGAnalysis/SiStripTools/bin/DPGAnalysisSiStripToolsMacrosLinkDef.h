2a0c46bc1171f7ca365bc2eb3f4ce37c0d0ae407
Andrea Venturi <andrea.venturi@cern.ch>
2015-05-27 16:10:20 +0200

    New macros for the OOT fraction analysis
    Conflicts:
    	DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h

--- DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
+++ DPGAnalysis/SiStripTools/bin/DPGAnalysisSiStripToolsMacrosLinkDef.h
@@ -8,6 +8,7 @@
 #include "UtilityMacros.h"
 #include "TrackPlots.h"
 #include "SeedMultiplicityPlots.h"
+#include "OOTMultiplicityPlotMacros.h"
 #ifdef __CINT__
 #pragma link off all functions;
 #pragma link C++ function PlotTrackerXsect;
@@ -43,4 +44,11 @@
 #pragma link C++ function SeedMultiplicityPlots;
 #pragma link C++ function SeedMultiplicityComparisonPlot;
 #pragma link C++ function PlotDebugFPIX_XYMap;
+#pragma link C++ function FillingScheme;
+#pragma link C++ function FillingSchemeFromProfile;
+#pragma link C++ function ComputeOOTFraction;
+#pragma link C++ function ComputeOOTFractionvsRun;
+#pragma link C++ function ComputeOOTFractionvsFill;
+#pragma link C++ class OOTResult;
+#pragma link C++ class OOTSummary;
 #endif
