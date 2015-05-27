14b3d6d2202753ec508088d8a45b0e28cfe5e912
Igor <i.volobouev@ttu.eu>
2015-05-27 06:08:22 +0200

    fixing MET filter paths

--- CommonTools/RecoAlgos/python/HBHENoiseFilter_cfi.py
+++ CommonTools/RecoAlgos/python/HBHENoiseFilter_cfi.py
@@ -1,13 +1,18 @@
 #
 # This is a replacement for the original HBHENoiseFilter configuration.
 # See https://twiki.cern.ch/twiki/bin/viewauth/CMS/HCALNoiseFilterRecipe.
-# Note that this replacement relies on the HBHENoiseFilterResultProducer
-# output.
+# Note that this replacement relies on having the HcalNoiseSummary in the
+# event record but not necessarily HBHENoiseFilterResult.
 #
 import FWCore.ParameterSet.Config as cms
 
+# Module which will remake HBHENoiseFilterResult
+from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import HBHENoiseFilterResultProducer
+MakeHBHENoiseFilterResult = HBHENoiseFilterResultProducer.clone()
+
+# Filter on the standard HCAL noise decision
 HBHENoiseFilter = cms.EDFilter(
     'BooleanFlagFilter',
-    inputLabel = cms.InputTag('HBHENoiseFilterResultProducer','HBHENoiseFilterResult'),
+    inputLabel = cms.InputTag('MakeHBHENoiseFilterResult','HBHENoiseFilterResult'),
     reverseDecision = cms.bool(False)
 )
