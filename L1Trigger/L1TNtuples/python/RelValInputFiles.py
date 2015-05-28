57aa418b46fdf7fb801a11122fa341e6c3fb0835
Jim <jim.brooke@cern.ch>
2015-05-28 18:41:32 +0200

    add example test config

---
+++ L1Trigger/L1TNtuples/python/RelValInputFiles.py
@@ -0,0 +1,7 @@
+
+def RelValInputFile_AOD():
+    return '/store/relval/CMSSW_7_5_0_pre1/RelValProdTTbar_13/AODSIM/MCRUN2_74_V7-v1/00000/48159643-5EE3-E411-818F-0025905A48F0.root'
+
+def RelValInputFile_RAW():
+    return ''
+
