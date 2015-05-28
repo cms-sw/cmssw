65fb2709436df9a0364ec62d69b51317c0e7e812
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-05-28 09:09:57 +0200

    Naming convention

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/HIon/V65 (CMSSW_7_4_3)
+# /dev/CMSSW_7_4_0/HIon/V66 (CMSSW_7_4_3)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V65')
+  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V66')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
