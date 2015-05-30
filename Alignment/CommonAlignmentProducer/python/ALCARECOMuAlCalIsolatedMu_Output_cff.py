92889fe9c16856b231429cc82cbc64cf983a43b0
Yuriy Pakhotin <pakhotin@users.noreply.github.com>
2015-05-30 13:09:50 -0500

    Update ALCARECOMuAlCalIsolatedMu_Output_cff.py

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_Output_cff.py
@@ -6,10 +6,11 @@ OutALCARECOMuAlCalIsolatedMu_noDrop = cms.PSet(
         SelectEvents = cms.vstring('pathALCARECOMuAlCalIsolatedMu')
     ),
     outputCommands = cms.untracked.vstring(
-        'keep *_ALCARECOMuAlCalIsolatedMu_*_*', 
-        'keep *_muonCSCDigis_*_*', 
-        'keep *_muonDTDigis_*_*', 
-        'keep *_muonRPCDigis_*_*', 
+        'keep *_ALCARECOMuAlCalIsolatedMu_*_*', # selected muons
+        'keep *_ALCARECOMuAlGeneralTracks_*_*', # selected general tracks
+        'keep *_muonCSCDigis_*_*',
+        'keep *_muonDTDigis_*_*',
+        'keep *_muonRPCDigis_*_*',
         'keep *_dt1DRecHits_*_*', 
         'keep *_dt2DSegments_*_*', 
         'keep *_dt4DSegments_*_*', 
@@ -19,7 +20,14 @@ OutALCARECOMuAlCalIsolatedMu_noDrop = cms.PSet(
         'keep L1AcceptBunchCrossings_*_*_*',
         'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
         'keep *_TriggerResults_*_*',
-        'keep DcsStatuss_scalersRawToDigi_*_*')
+        'keep *_offlineBeamSpot_*_*',
+        'keep *_offlinePrimaryVertices_*_*',
+        'keep DcsStatuss_scalersRawToDigi_*_*', # DCS information
+        # Generator level information
+        'keep *_genParticles_*_*',
+        'keep *_generator_*_*',
+        'drop edmHepMCProduct_generator_*_*',
+    )
 )
 
 import copy
