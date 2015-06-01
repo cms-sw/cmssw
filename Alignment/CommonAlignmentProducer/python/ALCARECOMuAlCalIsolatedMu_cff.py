02af62652211c5c67b09d04d1cff75b2f6ab07f7
Yuriy Pakhotin <pakhotin@users.noreply.github.com>
2015-06-01 08:45:25 -0500

    Update ALCARECOMuAlCalIsolatedMu_cff.py

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlCalIsolatedMu_cff.py
@@ -1,4 +1,4 @@
-# AlCaReco for muon based alignment using any individual muon tracks
+# AlCaReco selected muons for track based muon alignment
 import FWCore.ParameterSet.Config as cms
 
 import HLTrigger.HLTfilters.hltHighLevel_cfi
@@ -8,14 +8,29 @@ ALCARECOMuAlCalIsolatedMuHLT = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLeve
     throw = False # tolerate triggers not available
     )
 
+# DCS partitions
+# "EBp","EBm","EEp","EEm","HBHEa","HBHEb","HBHEc","HF","HO","RPC" 
+# "DT0","DTp","DTm","CSCp","CSCm","CASTOR","TIBTID","TOB","TECp","TECm"
+# "BPIX","FPIX","ESp","ESm"
+
+import DPGAnalysis.Skims.skim_detstatus_cfi
+ALCARECOMuAlCalIsolatedDCSFilter = DPGAnalysis.Skims.skim_detstatus_cfi.dcsstatus.clone(
+    DetectorType = cms.vstring('DT0','DTp','DTm','CSCp','CSCm'),
+    ApplyFilter  = cms.bool(True),
+    AndOr        = cms.bool(False),
+    DebugOn      = cms.untracked.bool(False)
+)
+
 import Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi
 ALCARECOMuAlCalIsolatedMu = Alignment.CommonAlignmentProducer.AlignmentMuonSelector_cfi.AlignmentMuonSelector.clone(
-    src = cms.InputTag("muons"),
-    filter = True, # not strictly necessary, but provided for symmetry with MuAlStandAloneCosmics
+    src    = cms.InputTag("muons"),
+    filter = cms.bool(True), # not strictly necessary, but provided for symmetry with MuAlStandAloneCosmics
 # DT calibration needs as many muons with DIGIs as possible, which in cosmic ray runs means standAloneMuons
 #    nHitMinGB = 1, # muon collections now merge globalMuons, standAlone, and trackerMuons: this stream has always assumed globalMuons
-    ptMin=cms.double(0.),
-    pMin=cms.double(10.)
+    ptMin  = cms.double(0.),
+    pMin   = cms.double(25.),
+    etaMin = cms.double(-2.6),
+    etaMax = cms.double(2.6),
     )
 
-seqALCARECOMuAlCalIsolatedMu = cms.Sequence(ALCARECOMuAlCalIsolatedMuHLT + ALCARECOMuAlCalIsolatedMu)
+seqALCARECOMuAlCalIsolatedMu = cms.Sequence(ALCARECOMuAlCalIsolatedMuHLT + ALCARECOMuAlCalIsolatedDCSFilter + ALCARECOMuAlCalIsolatedMu)
