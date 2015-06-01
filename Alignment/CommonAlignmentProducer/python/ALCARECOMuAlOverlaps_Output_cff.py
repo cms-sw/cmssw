58bd8a276393c603115dbb6662d10614df6b2e67
Yuriy Pakhotin <pakhotin@users.noreply.github.com>
2015-06-01 12:53:27 -0500

    Update ALCARECOMuAlOverlaps_Output_cff.py

--- Alignment/CommonAlignmentProducer/python/ALCARECOMuAlOverlaps_Output_cff.py
+++ Alignment/CommonAlignmentProducer/python/ALCARECOMuAlOverlaps_Output_cff.py
@@ -6,11 +6,23 @@ OutALCARECOMuAlOverlaps_noDrop = cms.PSet(
         SelectEvents = cms.vstring('pathALCARECOMuAlOverlaps')
     ),
     outputCommands = cms.untracked.vstring(
-        'keep *_ALCARECOMuAlOverlaps_*_*',
+        'keep *_ALCARECOMuAlOverlaps_*_*', # selected muons through CSC overlaps
+        'keep *_ALCARECOMuAlOverlapsGeneralTracks_*_*', # selected general tracks
+        'keep *_muonCSCDigis_*_*',
+        'keep *_muonDTDigis_*_*',
+        'keep *_muonRPCDigis_*_*',
+        'keep *_dt1DRecHits_*_*',
+        'keep *_dt2DSegments_*_*',
+        'keep *_dt4DSegments_*_*',
+        'keep *_csc2DRecHits_*_*',
+        'keep *_cscSegments_*_*',
+        'keep *_rpcRecHits_*_*',
         'keep L1AcceptBunchCrossings_*_*_*',
         'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
         'keep *_TriggerResults_*_*',
-        'keep DcsStatuss_scalersRawToDigi_*_*')
+        'keep *_offlineBeamSpot_*_*',
+        'keep *_offlinePrimaryVertices_*_*',
+        'keep DcsStatuss_scalersRawToDigi_*_*',
 )
 
 import copy
