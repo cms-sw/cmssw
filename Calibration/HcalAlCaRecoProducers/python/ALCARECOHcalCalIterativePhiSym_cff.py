92acdd66e5715f3d3a22b98c89bab8a334f23daf
Natalia <Natalia.Lychkovskaya@cern.ch>
2015-06-02 19:57:36 +0200

    correction

---
+++ Calibration/HcalAlCaRecoProducers/python/ALCARECOHcalCalIterativePhiSym_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+#------------------------------------------------
+#AlCaReco filtering for HCAL Iterative Phi Symmetry:
+#------------------------------------------------
+
+from Calibration.HcalAlCaRecoProducers.alcaiterphisym_cfi import *
+
+
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+hcalphisymHLT =  HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+#    HLTPaths = ['HLT_HcalPhiSym'],
+    eventSetupPathsKey='HcalCalIterativePhiSym',
+    throw = False #dont throw except on unknown path name 
+)
+
+seqALCARECOHcalCalIterativePhiSym = cms.Sequence(hcalphisymHLT*IterativePhiSymProd)
