3fc2fac9237338032cdab877fe6a7b370bf783a5
Colin <colin.bernet@cern.ch>
2015-06-03 16:49:57 +0200

    added unittest for diclist, and secured add function against integer keys

--- PhysicsTools/HeppyCore/python/utils/diclist.py
+++ PhysicsTools/HeppyCore/python/utils/diclist.py
@@ -2,49 +2,43 @@
 # https://github.com/cbernet/heppy/blob/master/LICENSE
 
 class diclist( list ):
+    '''list with an internal dictionary for indexing, 
+    allowing to keep dictionary elements ordered. 
+    keys can be everything except an integer.
+    '''
 
     def __init__(self):
         super( diclist, self).__init__()
+        # internal dictionary, will contain key -> index in list
         self.dico = {}
 
     def add( self, key, value ):
+        if isinstance(key, (int, long)):
+            raise ValueError("key cannot be an integer")
         if key in self.dico:
             raise ValueError("key '{key}' already exists".format(key=key) )
         self.dico[key] = len(self)
         self.append(value)
 
     def __getitem__(self, index):
-        '''These functions are quite risky...cannot use an integer as key...'''
+        '''index can be a dictionary key, or an integer specifying 
+        the rank of the value to be accessed
+        '''
         try:
-            # index = int( index )
+            # if index is an integer (the rank), use the list. 
             return super(diclist, self).__getitem__(index)
         except TypeError, ValueError:
+            # else it's the dictionary key.
+            # use the internal dictionary to get the index, 
+            # and return the corresponding value from the list
             return super(diclist, self).__getitem__( self.dico[index] )
             
     def __setitem__(self, index, value):
         '''These functions are quite risky...'''
         try:
-            # why did I add this cast? it's casting string to int...
-            # index = int( index )
             return super(diclist, self).__setitem__(index, value)
         except TypeError,ValueError:
             return super(diclist, self).__setitem__( self.dico[index], value )
             
-if __name__ == '__main__':
-    dl = diclist()
-    dl.add(0, 1)
-    dl.add(2, 2)
-    dl.add(1, 3)
 
-    print dl
-    print dl[0], dl[1]
-    
-    dl = diclist()
-    dl.add('0', 1)
-    dl.add('2', 2)
-    dl.add('1', 3)
-
-    print dl
-    print dl['0'], dl['1']
-    
     
