e280ae96e8da15e913e3f73971a4e90cc5c81ac6
Colin <colin.bernet@cern.ch>
2015-06-03 18:19:59 +0200

    looper main section removed.

--- PhysicsTools/HeppyCore/python/framework/looper.py
+++ PhysicsTools/HeppyCore/python/framework/looper.py
@@ -242,27 +242,28 @@ class Looper(object):
         self.setup.close() 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    import pickle
-    import sys
-    import os
-    if len(sys.argv) == 2 :
-        cfgFileName = sys.argv[1]
-        pckfile = open( cfgFileName, 'r' )
-        config = pickle.load( pckfile )
-        comp = config.components[0]
-        events_class = config.events_class
-    elif len(sys.argv) == 3 :
-        cfgFileName = sys.argv[1]
-        file = open( cfgFileName, 'r' )
-        cfg = imp.load_source( 'cfg', cfgFileName, file)
-        compFileName = sys.argv[2]
-        pckfile = open( compFileName, 'r' )
-        comp = pickle.load( pckfile )
-        cfg.config.components=[comp]
-        events_class = cfg.config.events_class
+#     import pickle
+#     import sys
+#     import os
+#     if len(sys.argv) == 2 :
+#         cfgFileName = sys.argv[1]
+#         pckfile = open( cfgFileName, 'r' )
+#         config = pickle.load( pckfile )
+#         comp = config.components[0]
+#         events_class = config.events_class
+#     elif len(sys.argv) == 3 :
+#         cfgFileName = sys.argv[1]
+#         file = open( cfgFileName, 'r' )
+#         cfg = imp.load_source( 'cfg', cfgFileName, file)
+#         compFileName = sys.argv[2]
+#         pckfile = open( compFileName, 'r' )
+#         comp = pickle.load( pckfile )
+#         cfg.config.components=[comp]
+#         events_class = cfg.config.events_class
 
-    looper = Looper( 'Loop', cfg.config,nPrint = 5)
-    looper.loop()
-    looper.write()
+#     looper = Looper( 'Loop', cfg.config,nPrint = 5)
+#     looper.loop()
+#     looper.write()
+
