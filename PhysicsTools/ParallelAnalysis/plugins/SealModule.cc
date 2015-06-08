71c4285d506c76a9a3f308e20d34c225441120cf
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-06-08 15:00:43 +0200

    To remove Capabilities Plugins in Root6: move TrackTSelectorAnalyzer EDM Plugin out of Capabilities

--- PhysicsTools/ParallelAnalysis/plugins/SealModule.cc
+++ PhysicsTools/ParallelAnalysis/plugins/SealModule.cc
@@ -1,4 +1,3 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "PhysicsTools/ParallelAnalysis/interface/TSelectorAnalyzer.h"
 #include "PhysicsTools/ParallelAnalysis/interface/TrackAnalysisAlgorithm.h"
