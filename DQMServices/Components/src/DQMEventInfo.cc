1e0ab2704b4ee1da2f63a71cd9e0dea444e9b98c
Dmitrijus Bugelskis <dmitrijus.bugelskis@cern.ch>
2015-06-08 15:53:26 +0200

    Properly zero terminate a string in DQMEventInfo.

--- DQMServices/Components/src/DQMEventInfo.cc
+++ DQMServices/Components/src/DQMEventInfo.cc
@@ -80,8 +80,9 @@ void DQMEventInfo::bookHistograms(DQMStore::IBooker & ibooker,
   processStartTimeStamp_->Fill(currentTime_);
   runStartTimeStamp_ = ibooker.bookFloat("runStartTimeStamp");
   runStartTimeStamp_->Fill(stampToReal(iRun.beginTime()));
-  char hostname[33];
-  gethostname(hostname,32);
+  char hostname[65];
+  gethostname(hostname,64);
+  hostname[64] = 0;
   hostName_= ibooker.bookString("hostName",hostname);
   processName_= ibooker.bookString("processName",subsystemname_);
   char* pwd = getcwd(NULL, 0);
