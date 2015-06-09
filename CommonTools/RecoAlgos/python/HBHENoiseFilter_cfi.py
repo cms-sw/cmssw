5a4e37101233e6df8337d83b21841becc685b298
Igor <i.volobouev@ttu.eu>
2015-06-10 00:04:04 +0200

    Forward port of changes in PR #9395

--- CommonTools/RecoAlgos/python/HBHENoiseFilter_cfi.py
+++ CommonTools/RecoAlgos/python/HBHENoiseFilter_cfi.py
@@ -1,24 +1,14 @@
 #
 # This is a replacement for the original HBHENoiseFilter configuration.
 # See https://twiki.cern.ch/twiki/bin/viewauth/CMS/HCALNoiseFilterRecipe.
-# Note that this replacement relies on having the HcalNoiseSummary in the
-# event record but not necessarily HBHENoiseFilterResult.
+# This replacement relies on having the HBHENoiseFilterResult in the
+# event record.
 #
 import FWCore.ParameterSet.Config as cms
-
-# Module which will remake HBHENoiseFilterResult
-from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import HBHENoiseFilterResultProducer
-MakeHBHENoiseFilterResult = HBHENoiseFilterResultProducer.clone()
 
 # Filter on the standard HCAL noise decision
 HBHENoiseFilter = cms.EDFilter(
     'BooleanFlagFilter',
-    inputLabel = cms.InputTag('MakeHBHENoiseFilterResult','HBHENoiseFilterResult'),
+    inputLabel = cms.InputTag('HBHENoiseFilterResultProducer','HBHENoiseFilterResult'),
     reverseDecision = cms.bool(False)
 )
-
-# Customize MakeHBHENoiseFilterResult in the same manner
-# as HBHENoiseFilterResultProducer
-from Configuration.StandardSequences.Eras import eras
-eras.run2_common.toModify(MakeHBHENoiseFilterResult, IgnoreTS4TS5ifJetInLowBVRegion=False)
-eras.run2_25ns_specific.toModify(MakeHBHENoiseFilterResult, defaultDecision="HBHENoiseFilterResultRun2Loose")
--- SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/postLS1Customs.py
@@ -138,8 +138,6 @@ def customise_DQM(process):
     # Turn off "low bias voltage" region in HCAL noise filters
     if hasattr(process,'HBHENoiseFilterResultProducer'):
         process.HBHENoiseFilterResultProducer.IgnoreTS4TS5ifJetInLowBVRegion = cms.bool(False)
-    if hasattr(process,'MakeHBHENoiseFilterResult'):
-        process.MakeHBHENoiseFilterResult.IgnoreTS4TS5ifJetInLowBVRegion = cms.bool(False)
     return process
 
 
@@ -147,8 +145,6 @@ def customise_DQM_25ns(process):
     # Switch the default decision of the HCAL noise filter
     if hasattr(process,'HBHENoiseFilterResultProducer'):
         process.HBHENoiseFilterResultProducer.defaultDecision = cms.string("HBHENoiseFilterResultRun2Loose")
-    if hasattr(process,'MakeHBHENoiseFilterResult'):
-        process.MakeHBHENoiseFilterResult.defaultDecision = cms.string("HBHENoiseFilterResultRun2Loose")
     return process
 
 
