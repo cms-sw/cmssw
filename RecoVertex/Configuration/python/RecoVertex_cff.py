e89401d21d27478f392a38f15f2ab987aafa7e6d
Andrea <andrea.rizzi@cern.ch>
2015-06-10 15:53:04 +0200

    fix bug spoling PV sorting on W' and Z'. Override default ptMax cut

--- RecoVertex/Configuration/python/RecoVertex_cff.py
+++ RecoVertex/Configuration/python/RecoVertex_cff.py
@@ -17,7 +17,9 @@ unsortedOfflinePrimaryVertices=offlinePrimaryVertices.clone()
 offlinePrimaryVertices=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices", particles="trackRefsForJetsBeforeSorting")
 offlinePrimaryVerticesWithBS=sortedPrimaryVertices.clone(vertices=cms.InputTag("unsortedOfflinePrimaryVertices","WithBS"), particles="trackRefsForJetsBeforeSorting")
 trackWithVertexRefSelectorBeforeSorting = trackWithVertexRefSelector.clone(vertexTag="unsortedOfflinePrimaryVertices")
-trackRefsForJetsBeforeSorting = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSorting")
+trackWithVertexRefSelectorBeforeSorting.ptMax=9e99
+trackWithVertexRefSelectorBeforeSorting.ptErrorCut=9e99
+rackRefsForJetsBeforeSorting = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSorting")
 
 
 vertexreco = cms.Sequence(unsortedOfflinePrimaryVertices*
