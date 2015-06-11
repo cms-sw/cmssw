82fcf603ec0a719d4741b1aeecd3da66ba18a057
Alexis Kalogeropoulos <alkaloge@cern.ch>
2015-06-11 17:20:42 +0200

    1) autoskim, ZElectron skim 2) Conflicts (resolved, only empty lines and verbose comment):	DPGAnalysis/Skims/python/Skims_DPG_cff.py	DPGAnalysis/Skims/python/ZElectronSkim_cff.py 3) exclute newly added Configuration/Skimming/autoSkim.py

---
+++ Configuration/Skimming/autoSkim.py
@@ -0,0 +1,34 @@
+autoSkim = {
+    'Commissioning':'DT+LogError',
+    'Cosmics':'CosmicSP+CosmicTP+LogError',
+    'CosmicsSP':'CosmicSP++LogError',
+    'TopMuEG':'TopMuEG+LogError',
+    'HighMET': 'HighMET+LogError',
+    'ZElectron' : 'ZElectron+LogError',
+    'ZMu' : 'ZMu+LogError',
+    'MuTau': 'MuTau+LogError',
+    
+    }
+
+
+autoSkimPDWG = {
+    
+    }
+
+autoSkimDPG = {
+
+    }
+
+def mergeMapping(map1,map2):
+    merged={}
+    for k in list(set(map1.keys()+map2.keys())):
+        items=[]
+        if k in map1: 
+            items.append(map1[k])
+        if k in map2:
+            items.append(map2[k])
+        merged[k]='+'.join(items)
+    return merged
+    
+#autoSkim = mergeMapping(autoSkimPDWG,autoSkimDPG)
+#print autoSkim
