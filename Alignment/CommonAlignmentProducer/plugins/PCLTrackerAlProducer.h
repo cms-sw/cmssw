34b5bef635530e167de9879c91c08585dd0785f5
DirtyDan88 <Max.stark88@web.de>
2015-06-12 16:46:32 +0200

    implemented new AlignmentProducer running on PCL * created in CMSSW_7_5_0_pre5 * aligns only tracker * AP is no longer an ESProducer but an EDAnalyzer * runs only with MillePede at the moment
    Conflicts:
    	Alignment/CommonAlignmentAlgorithm/interface/AlignmentAlgorithmBase.h
    	Alignment/CommonAlignmentProducer/python/TrackerAlignmentProducerForPCL_cff.py
    	Alignment/MillePedeAlignmentAlgorithm/src/MillePedeAlignmentAlgorithm.cc

---
+++ Alignment/CommonAlignmentProducer/plugins/PCLTrackerAlProducer.h
@@ -0,0 +1,193 @@
+#ifndef Alignment_CommonAlignmentAlgorithm_TrackerAlignmentProducer_h
+#define Alignment_CommonAlignmentAlgorithm_TrackerAlignmentProducer_h
+
+/// \class AlignmentProducer
+///
+/// Package     : Alignment/CommonAlignmentProducer
+/// Description : calls alignment algorithms
+///
+///  \author    : Frederic Ronga
+///  Revision   : $Revision: 1.27 $
+///  last update: $Date: 2012/06/13 16:23:30 $
+///  by         : $Author: yana $
+
+#include <vector>
+
+// Framework
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
+
+#include "DataFormats/Provenance/interface/RunID.h"
+
+// Geometry
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+
+// Alignment
+#include "Alignment/CommonAlignmentAlgorithm/interface/AlignmentAlgorithmBase.h"
+#include "Alignment/CommonAlignmentMonitor/interface/AlignmentMonitorBase.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h" 
+#include <Geometry/Records/interface/MuonGeometryRecord.h> 
+#include "Alignment/CommonAlignment/interface/AlignableExtras.h"
+#include "Alignment/TrackerAlignment/interface/AlignableTracker.h"
+#include "Alignment/MuonAlignment/interface/AlignableMuon.h"
+#include <FWCore/Framework/interface/Frameworkfwd.h>
+#include "CondCore/DBCommon/interface/Time.h"
+#include "CondFormats/Alignment/interface/Alignments.h"
+#include "CondFormats/Alignment/interface/AlignmentSurfaceDeformations.h"
+
+// for watcher
+#include "CondFormats/AlignmentRecord/interface/TrackerSurveyRcd.h"
+#include "CondFormats/AlignmentRecord/interface/TrackerSurveyErrorExtendedRcd.h"
+#include "CondFormats/AlignmentRecord/interface/DTSurveyRcd.h"
+#include "CondFormats/AlignmentRecord/interface/DTSurveyErrorExtendedRcd.h"
+#include "CondFormats/AlignmentRecord/interface/CSCSurveyRcd.h"
+#include "CondFormats/AlignmentRecord/interface/CSCSurveyErrorExtendedRcd.h"
+
+
+class Alignments;
+class IntegratedCalibrationBase;
+class SurveyErrors;
+namespace edm {
+  class Run;
+  class LuminosityBlock;
+}
+
+class PCLTrackerAlProducer : public edm::EDAnalyzer
+{
+
+ public:
+  typedef std::vector<Alignable*> Alignables;
+  typedef std::pair<const Trajectory*, const reco::Track*> ConstTrajTrackPair; 
+  typedef std::vector<ConstTrajTrackPair>  ConstTrajTrackPairCollection;
+
+  typedef AlignmentAlgorithmBase::RunNumber            RunNumber;
+  typedef AlignmentAlgorithmBase::RunRange             RunRange;
+  typedef std::vector<RunRange>                        RunRanges;
+
+  /// Constructor
+  PCLTrackerAlProducer( const edm::ParameterSet& iConfig );
+  
+  /// Destructor
+  ~PCLTrackerAlProducer();
+
+  /// Called at beginning of job
+  virtual void beginOfJob();
+
+  /// Called at end of job
+  virtual void endOfJob();
+
+  /// Called at run start and calling algorithms beginRun
+  virtual void beginRun(const edm::Run &run, const edm::EventSetup &setup);
+  /// Called at run end - currently reading TkFittedLasBeam if an InpuTag is given for that
+  virtual void endRun(const edm::Run &run, const edm::EventSetup &setup);
+
+  /// Called at lumi block start, calling algorithm's beginLuminosityBlock
+  virtual void beginLuminosityBlock(const edm::LuminosityBlock &lumiBlock,
+				    const edm::EventSetup &setup);
+  /// Called at lumi block end, calling algorithm's endLuminosityBlock
+  virtual void endLuminosityBlock(const edm::LuminosityBlock &lumiBlock,
+				  const edm::EventSetup &setup);
+  /// Called at each event 
+  virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+
+ private:
+
+  // private member functions
+  void init(const edm::EventSetup&);
+
+  /// Apply random shifts and rotations to selected alignables, according to configuration
+  void simpleMisalignment_(const Alignables &alivec, const std::string &selection,
+                          float shift, float rot, bool local);
+
+  /// Create tracker and muon geometries
+  void createGeometries_( const edm::EventSetup& );
+
+  /// Apply DB constants belonging to (Err)Rcd to geometry,
+  /// taking into account 'globalPosition' correction.
+  template<class G, class Rcd, class ErrRcd>
+    void applyDB(G *geometry, const edm::EventSetup &iSetup,
+		 const AlignTransform &globalPosition) const;
+  /// Apply DB constants for surface deformations
+  template<class G, class DeformationRcd>
+    void applyDB(G *geometry, const edm::EventSetup &iSetup) const;
+
+  // write alignments and alignment errors for all sub detectors and
+  // the given run number
+  void writeForRunRange(cond::Time_t time);
+
+  /// Write alignment and/or errors to DB for record names
+  /// (removes *globalCoordinates before writing if non-null...).
+  /// Takes over ownership of alignments and alignmentErrrors.
+  void writeDB(Alignments *alignments, const std::string &alignRcd,
+	       AlignmentErrorsExtended *alignmentErrors, const std::string &errRcd,
+	       const AlignTransform *globalCoordinates,
+	       cond::Time_t time) const;
+  /// Write surface deformations (bows & kinks) to DB for given record name
+  /// Takes over ownership of alignmentsurfaceDeformations.
+  void writeDB(AlignmentSurfaceDeformations *alignmentSurfaceDeformations,
+	       const std::string &surfaceDeformationRcd,
+	       cond::Time_t time) const;
+
+  /// Add survey info to an alignable
+  void addSurveyInfo_(Alignable*);
+	
+  /// read in survey records
+  void readInSurveyRcds( const edm::EventSetup& );
+
+  RunRanges makeNonOverlappingRunRanges(const edm::VParameterSet& RunRangeSelectionVPSet);
+
+  // private data members
+
+  unsigned int        theSurveyIndex;
+  const Alignments*   theSurveyValues;
+  const SurveyErrors* theSurveyErrors;
+
+  AlignmentAlgorithmBase* theAlignmentAlgo;
+  AlignmentParameterStore* theAlignmentParameterStore;
+  std::vector<AlignmentMonitorBase*> theMonitors;
+  std::vector<IntegratedCalibrationBase*> theCalibrations;
+
+  AlignableExtras* theAlignableExtras;
+  AlignableTracker* theAlignableTracker;
+  AlignableMuon* theAlignableMuon;
+
+  boost::shared_ptr<TrackerGeometry> theTracker;
+  boost::shared_ptr<DTGeometry> theMuonDT;
+  boost::shared_ptr<CSCGeometry> theMuonCSC;
+  /// GlobalPositions that might be read from DB, NULL otherwise
+  const Alignments *globalPositions_;
+
+  int nevent_;
+  edm::ParameterSet theParameterSet;
+
+  // steering parameters
+
+  const unsigned int theMaxLoops;     // Number of loops to loop
+
+  const int stNFixAlignables_;
+  const double stRandomShift_,stRandomRotation_;
+  const bool applyDbAlignment_,checkDbAlignmentValidity_;
+  const bool doMisalignmentScenario_;
+  const bool saveToDB_, saveApeToDB_,saveDeformationsToDB_;
+  const bool doTracker_,doMuon_,useExtras_;
+  const bool useSurvey_; // true to read survey info from DB
+
+  // event input tags
+  const edm::InputTag tjTkAssociationMapTag_; // map with tracks/trajectories
+  const edm::InputTag beamSpotTag_;           // beam spot
+  const edm::InputTag tkLasBeamTag_;          // LAS beams in edm::Run (ignore if empty)
+  const edm::InputTag clusterValueMapTag_;              // ValueMap containing associtaion cluster - flag
+
+  // ESWatcher
+  edm::ESWatcher<TrackerSurveyRcd> watchTkSurveyRcd_;
+  edm::ESWatcher<TrackerSurveyErrorExtendedRcd> watchTkSurveyErrRcd_;
+  edm::ESWatcher<DTSurveyRcd> watchDTSurveyRcd_;
+  edm::ESWatcher<DTSurveyErrorExtendedRcd> watchDTSurveyErrRcd_;
+  edm::ESWatcher<CSCSurveyRcd> watchCSCSurveyRcd_;
+  edm::ESWatcher<CSCSurveyErrorExtendedRcd> watchCSCSurveyErrRcd_;	
+
+  edm::ESWatcher<TrackerAlignmentRcd> watchTrackerAlRcd;
+};
+
+#endif
