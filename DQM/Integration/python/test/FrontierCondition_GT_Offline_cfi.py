84a8545b05d24c3fa9ddda62c593a0356792fede
Charlie Mueller <charles.mueller@cern.ch>
2015-06-18 09:53:13 +0200

    updating py files to be consistent with master cmssw 74x

--- DQM/Integration/python/test/FrontierCondition_GT_Offline_cfi.py
+++ DQM/Integration/python/test/FrontierCondition_GT_Offline_cfi.py
@@ -3,7 +3,4 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
 from Configuration.AlCa.GlobalTag import GlobalTag as gtCustomise
 
-#GlobalTag = gtCustomise(GlobalTag, 'auto:com10_GRun', '')
-#customise for 740
-GlobalTag = gtCustomise(GlobalTag, 'auto:run2_hlt_GRun', '')
-#run2_hlt_50nsGRun
+GlobalTag = gtCustomise(GlobalTag, 'auto:com10_GRun', '')
--- DQM/Integration/python/test/fileinputsource_cfi.py
+++ DQM/Integration/python/test/fileinputsource_cfi.py
@@ -2,10 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 source = cms.Source("PoolSource",
                     fileNames = cms.untracked.vstring(
-#        "file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root",
-#        "file:/afs/cern.ch/user/m/muell149/public/outputA_25nsFrozenMenuTrgRes.root"
-#        "file:/afs/cern.ch/user/m/muell149/work/CMSSW_7_4_0/src/HLTrigger/Configuration/test/outputA_extraCollections.root"
-        "file:/afs/cern.ch/user/m/muell149/public/25ns_ppMenu_xtraCollections.root"
+        "file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root",
 #        "/store/data/Commissioning2014/Cosmics/RAW//v3/000/224/380/00000/E05943D1-1227-E411-BB8E-02163E00F0C4.root"
 #    "/store/data/Commissioning2014/Cosmics/RAW/v3/000/224/380/00000/68FDADE5-1227-E411-8AA6-02163E00A10C.root"
         )
@@ -22,12 +19,6 @@ from dqmPythonTypes import *
 
 options = VarParsing.VarParsing('analysis')
 
-options.register('runtype',
-         'cosmic_run',
-        VarParsing.VarParsing.multiplicity.singleton,
-        VarParsing.VarParsing.varType.string,
-          "Type of Run in CMS")
-
 options.register ('runkey',
           'cosmic_run',
           VarParsing.VarParsing.multiplicity.singleton,
@@ -40,8 +31,8 @@ options.parseArguments()
 #if len(sys.argv) > 1:
 #  options.parseArguments()
 
-runType = RunType(['pp_run','cosmic_run','hi_run','hpu_run'])
+runType = RunType()
 if not options.runkey.strip():
-  options.runkey = 'cosmic_run'
+  options.runkey = 'pp_run'
 
 runType.setRunType(options.runkey.strip())
