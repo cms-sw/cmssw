1640752c3cf233c9bf10181578d57de6dcefaaf2
Ilya Kravchenko <ikrav@unl.edu>
2015-06-18 18:16:43 +0200

    A few more fixes, and now the templated MVA compiles

--- RecoEgamma/ElectronIdentification/plugins/ElectronMVAValueMapProducer.cc
+++ RecoEgamma/ElectronIdentification/plugins/ElectronMVAValueMapProducer.cc
@@ -1,5 +1,8 @@
 #include "RecoEgamma/EgammaTools/interface/MVAValueMapProducer.h"
 
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+
 typedef MVAValueMapProducer<reco::GsfElectron> ElectronMVAValueMapProducer;
 
 //define this as a plug-in
