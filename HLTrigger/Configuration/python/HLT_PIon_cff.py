176b7f0e79a2d2ed2c9e02bbce37c2dca45fc7ac
Andrea <perrotta@cern.ch>
2015-06-19 16:02:16 +0200

    remove from the subtables all paths not yet in DQM

--- HLTrigger/Configuration/python/HLT_PIon_cff.py
+++ HLTrigger/Configuration/python/HLT_PIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/PIon/V81 (CMSSW_7_4_3)
+# /dev/CMSSW_7_4_0/PIon/V82 (CMSSW_7_4_3)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V81')
+  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V82')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
