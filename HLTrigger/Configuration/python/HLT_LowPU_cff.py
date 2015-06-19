66cd3e3aa2a54800ca068b5ab83fc34bee78005d
Andrea <perrotta@cern.ch>
2015-06-19 11:21:07 +0200

    50ns menu updates

--- HLTrigger/Configuration/python/HLT_LowPU_cff.py
+++ HLTrigger/Configuration/python/HLT_LowPU_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/LowPU/V36 (CMSSW_7_4_3)
+# /dev/CMSSW_7_4_0/LowPU/V37 (CMSSW_7_4_3)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/LowPU/V36')
+  tableName = cms.string('/dev/CMSSW_7_4_0/LowPU/V37')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_LowPU.py
+++ HLTrigger/Configuration/test/OnLine_HLT_LowPU.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/LowPU/V36 (CMSSW_7_4_3)
+# /dev/CMSSW_7_4_0/LowPU/V37 (CMSSW_7_4_3)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTLowPU" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/LowPU/V36')
+  tableName = cms.string('/dev/CMSSW_7_4_0/LowPU/V37')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
