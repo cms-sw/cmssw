7afeeecf85bfc24c82cabccce4bbcd10a6286e4b
Andrea <perrotta@cern.ch>
2015-06-22 23:04:30 +0200

    Update also the 25ns and Fake menus

--- HLTrigger/Configuration/test/OnLine_HLT_Fake.py
+++ HLTrigger/Configuration/test/OnLine_HLT_Fake.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/Fake/V11 (CMSSW_7_4_3)
+# /dev/CMSSW_7_4_0/Fake/V12 (CMSSW_7_4_5)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTFake" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/Fake/V11')
+  tableName = cms.string('/dev/CMSSW_7_4_0/Fake/V12')
 )
 
 process.streams = cms.PSet(  A = cms.vstring( 'InitialPD' ) )
