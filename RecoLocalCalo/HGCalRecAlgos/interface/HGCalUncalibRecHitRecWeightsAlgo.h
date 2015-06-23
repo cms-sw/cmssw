18d9f3a12d870d0a3a6dd513738d231a962a7439
Lindsey Gray <lindsey.gray@gmail.com>
2015-06-23 17:35:41 +0200

    add tdc onset charge offset to uncalib rechits, produces continuous spectra

--- RecoLocalCalo/HGCalRecAlgos/interface/HGCalUncalibRecHitRecWeightsAlgo.h
+++ RecoLocalCalo/HGCalRecAlgos/interface/HGCalUncalibRecHitRecWeightsAlgo.h
@@ -12,6 +12,7 @@
 
 #include "RecoLocalCalo/HGCalRecAlgos/interface/HGCalUncalibRecHitRecAbsAlgo.h"
 #include <vector>
+#include <cmath>
 
 #include "FWCore/MessageService/interface/MessageLogger.h"
 #include "DataFormats/ForwardDetId/interface/ForwardSubdetector.h"
@@ -29,6 +30,8 @@ template<class C> class HGCalUncalibRecHitRecWeightsAlgo
 
   virtual void set_toaLSBToNS(const double lsb2ns) { toaLSBToNS_ = lsb2ns; }
 
+  virtual void set_tdcOnsetfC(const double tdcOnset) { tdcOnsetfC_ = tdcOnset; }
+
   /// Compute parameters
   virtual HGCUncalibratedRecHit makeRecHit( const C& dataFrame ) {
     double amplitude_(-1.),  pedestal_(-1.), jitter_(-1.), chi2_(-1.);
@@ -44,7 +47,11 @@ template<class C> class HGCalUncalibRecHitRecWeightsAlgo
       // mode == true means TDC readout was activated
       if( sample.mode() ) {
 	flag       = !sample.threshold();  //raise flag if busy cell
-	amplitude_ = double(sample.data()) * tdcLSB_;
+        // LG (23/06/2015): 
+        //to get a continuous energy spectrum we must add here the maximum value in fC ever
+        //reported by the ADC. Namely: floor(tdcOnset/adcLSB_) * adcLSB_
+        // need to increment by one so TDC doesn't overlap with ADC last bin
+	amplitude_ = ( std::floor(tdcOnsetfC_/adcLSB_) + 1.0 )* adcLSB_ + double(sample.data()) * tdcLSB_;
 	jitter_    = double(sample.toa()) * toaLSBToNS_;
 	LogDebug("HGCUncalibratedRecHit") << "TDC+: set the energy to: " << amplitude_ << ' ' << sample.data() 
                                           << ' ' << tdcLSB_ << std::endl
@@ -70,6 +77,6 @@ template<class C> class HGCalUncalibRecHitRecWeightsAlgo
   
  private:
    bool   isSiFESim_;
-   double adcLSB_, tdcLSB_, fCToMIP_, toaLSBToNS_;
+   double adcLSB_, tdcLSB_, fCToMIP_, toaLSBToNS_, tdcOnsetfC_;
 };
 #endif
