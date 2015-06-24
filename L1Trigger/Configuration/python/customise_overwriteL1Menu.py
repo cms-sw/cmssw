e5d1e31877822fe7670b4517024a17a12f4f2bae
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-06-24 11:48:47 +0200

    Adding v3 of 25ns L1T menu

--- L1Trigger/Configuration/python/customise_overwriteL1Menu.py
+++ L1Trigger/Configuration/python/customise_overwriteL1Menu.py
@@ -45,6 +45,17 @@ def L1Menu_Collisions2015_25ns_v2(process):
     return process
 
 
+def L1Menu_Collisions2015_25ns_v3(process):
+    process.load( 'L1TriggerConfig.L1GtConfigProducers.l1GtTriggerMenuXml_cfi' )
+    process.l1GtTriggerMenuXml.TriggerMenuLuminosity = 'startup'
+    process.l1GtTriggerMenuXml.DefXmlFile            = 'L1Menu_Collisions2015_25nsStage1_v3_L1T_Scales_20141121_Imp0_0x1031.xml'
+
+    process.load( 'L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMenuConfig_cff' )
+    process.es_prefer_l1GtParameters = cms.ESPrefer( 'L1GtTriggerMenuXmlProducer', 'l1GtTriggerMenuXml' )
+
+    return process
+
+
 def L1Menu_Collisions2015_50ns_v0(process):
     process.load( 'L1TriggerConfig.L1GtConfigProducers.l1GtTriggerMenuXml_cfi' )
     process.l1GtTriggerMenuXml.TriggerMenuLuminosity = 'startup'
