0c2b508d54fd59819df7bcdd5e035598beef6ab2
Gianluca <gianluca.cerminara@cern.ch>
2015-06-25 15:16:43 +0200

    move to the new EDAnalyzer for the alignment producer

--- Alignment/CommonAlignmentProducer/python/AlcaSiPixelAliHarvester_cff.py
+++ Alignment/CommonAlignmentProducer/python/AlcaSiPixelAliHarvester_cff.py
@@ -1,38 +1,25 @@
 import FWCore.ParameterSet.Config as cms
+import copy
 
+from Alignment.MillePedeAlignmentAlgorithm.MillePedeAlignmentAlgorithm_cfi import *
+from Alignment.CommonAlignmentProducer.TrackerAlignmentProducerForPCL_cff import AlignmentProducer
+SiPixelAliPedeAlignmentProducer = copy.deepcopy(AlignmentProducer)
 
-
-
-# Alignment producer
-from Alignment.CommonAlignmentProducer.AlignmentProducer_cff import *
-
-#process.AlignmentProducer.parameterTypes = cms.vstring('Selector,RigidBody')
-#process.AlignmentProducer.ParameterBuilder.parameterTypes = [
-#    'SelectorRigid,RigidBody',
-#    'SelectorBowed,BowedSurface'
-#    ,'Selector2Bowed,TwoBowedSurfaces'
-#    ]
-looper.ParameterBuilder.Selector = cms.PSet(
+SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector = cms.PSet(
     alignParams = cms.vstring(
         'TrackerTPBHalfBarrel,111111',
         'TrackerTPEHalfCylinder,111111',
-#        'TrackerTPBLayer,111111',
-#        'TrackerTPEHalfDisk,111111',
 
-        'TrackerTIBHalfBarrel,ffffff', # or fff fff?
-        'TrackerTOBHalfBarrel,ffffff', # dito...
+        'TrackerTIBHalfBarrel,ffffff',
+        'TrackerTOBHalfBarrel,rrrrrr',
         'TrackerTIDEndcap,ffffff',
         'TrackerTECEndcap,ffffff'
         )
     )
 
-looper.doMisalignmentScenario = False #True
+SiPixelAliPedeAlignmentProducer.doMisalignmentScenario = False #True
 
-# If the above is true, you might want to choose the scenario:
-#from Alignment.TrackerAlignment.Scenarios_cff import Tracker10pbScenario as Scenario # TrackerSurveyLASOnlyScenario
-
-#FIXME: is this needed given the above parameters?
-looper.MisalignmentScenario = cms.PSet(
+SiPixelAliPedeAlignmentProducer.MisalignmentScenario = cms.PSet(
     setRotations = cms.bool(True),
     setTranslations = cms.bool(True),
     seed = cms.int32(1234567),
@@ -45,7 +32,7 @@ looper.MisalignmentScenario = cms.PSet(
         phiXlocal = cms.double(1.e-4),
         phiYlocal = cms.double(-2.e-4),
         phiZlocal = cms.double(5.e-4),
-        
+
         ),
     TPBHalfBarrel2 = cms.PSet(
         dXlocal = cms.double(-0.0020),
@@ -92,38 +79,27 @@ looper.MisalignmentScenario = cms.PSet(
             phiZlocal = cms.double(3.e-4),
         ),
     )
+)
 
+SiPixelAliPedeAlignmentProducer.checkDbAlignmentValidity = False
+SiPixelAliPedeAlignmentProducer.applyDbAlignment = True
+SiPixelAliPedeAlignmentProducer.tjTkAssociationMapTag = 'TrackRefitter2'
 
-    )
-looper.checkDbAlignmentValidity = False
-
-looper.applyDbAlignment = True
-
-
-looper.tjTkAssociationMapTag = 'TrackFitter'
-
-# assign by reference (i.e. could change MillePedeAlignmentAlgorithm as well):
-looper.algoConfig = MillePedeAlignmentAlgorithm
-
-#from Alignment.MillePedeAlignmentAlgorithm.PresigmaScenarios_cff import *
-#looper.algoConfig.pedeSteerer.Presigmas.extend(TrackerShortTermPresigmas.Presigmas)
-looper.algoConfig.mode = 'pede' #'mille' #'full' # 'pede' # 'full' # 'pedeSteerer'
-#looper.algoConfig.mergeBinaryFiles = ['milleBinaryISN.dat']
-#looper.algoConfig.mergeTreeFiles = ['treeFileISN_reg.root']
-looper.algoConfig.binaryFile = 'milleBinaryISN.dat'
-looper.algoConfig.treeFile = 'treeFileISN.root'
-
-
-
-
-looper.algoConfig.TrajectoryFactory = BrokenLinesTrajectoryFactory
-looper.algoConfig.TrajectoryFactory.MaterialEffects = 'BrokenLinesCoarse' #Coarse' #Fine' #'BreakPoints'
-#looper.algoConfig.pedeSteerer.pedeCommand = '/afs/cern.ch/user/f/flucke/cms/pede/trunk_v69/pede_8GB'
-#FIXME: this needs to come from the release
-looper.algoConfig.pedeSteerer.pedeCommand = '/afs/cern.ch/user/c/ckleinw/bin/rev125/pede'
-#default is  sparseMINRES 6 0.8:                     <method>  n(iter)  Delta(F)
-looper.algoConfig.pedeSteerer.method = 'inversion  5  0.8'  ##DNOONAN can be set to inversion instead (faster)
-looper.algoConfig.pedeSteerer.options = cms.vstring(
+SiPixelAliPedeAlignmentProducer.algoConfig = MillePedeAlignmentAlgorithm
+SiPixelAliPedeAlignmentProducer.algoConfig.mode = 'pede'
+# FIXME: this needs to be addressed
+SiPixelAliPedeAlignmentProducer.algoConfig.mergeBinaryFiles = [
+				'milleBinary0.dat'
+				]
+SiPixelAliPedeAlignmentProducer.algoConfig.monitorFile = 'millePedeMonitor_pede.root'
+SiPixelAliPedeAlignmentProducer.algoConfig.binaryFile = ''
+SiPixelAliPedeAlignmentProducer.algoConfig.TrajectoryFactory = BrokenLinesBzeroTrajectoryFactory
+SiPixelAliPedeAlignmentProducer.algoConfig.TrajectoryFactory.MomentumEstimate = 10
+SiPixelAliPedeAlignmentProducer.algoConfig.TrajectoryFactory.MaterialEffects = 'BrokenLinesCoarse' #Coarse' #Fine' #'BreakPoints'
+SiPixelAliPedeAlignmentProducer.algoConfig.TrajectoryFactory.UseInvalidHits = True # to account for multiple scattering in these layers
+SiPixelAliPedeAlignmentProducer.algoConfig.pedeSteerer.pedeCommand = 'pede'
+SiPixelAliPedeAlignmentProducer.algoConfig.pedeSteerer.method = 'inversion  5  0.8' 
+SiPixelAliPedeAlignmentProducer.algoConfig.pedeSteerer.options = cms.vstring(
     #'regularisation 1.0 0.05', # non-stated pre-sigma 50 mrad or 500 mum
     'entries 500',
     'chisqcut  30.0  4.5', #,
@@ -131,10 +107,10 @@ looper.algoConfig.pedeSteerer.options = cms.vstring(
     #'outlierdownweighting 3','dwfractioncut 0.1'
     #'outlierdownweighting 5','dwfractioncut 0.2'
     )
+SiPixelAliPedeAlignmentProducer.algoConfig.minNumHits = 8
 
-
-looper.algoConfig.minNumHits = 8
 
 from Alignment.FileConverterPlaceHolder.fileconverterplaceholder_cfi import *
 
-ALCAHARVESTSiPixelAli = cms.Sequence(demo)
+ALCAHARVESTSiPixelAli = cms.Sequence(SiPixelAliPedeAlignmentProducer*
+                                     demo)
