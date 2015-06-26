a7d1ed84ce767c3a59924eb3836be4e1cdd6c0ed
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-06-26 14:12:07 +0200

    Generalize makeTrackValidationPlots.py to work on all subfolders of Tracking/Track in the DQM files

--- Validation/RecoTrack/scripts/makeTrackValidationPlots.py
+++ Validation/RecoTrack/scripts/makeTrackValidationPlots.py
@@ -7,20 +7,11 @@ import Validation.RecoTrack.plotting.plotting as plotting
 from Validation.RecoTrack.plotting.validation import SimpleValidation
 import Validation.RecoTrack.plotting.trackingPlots as trackingPlots
 
-Algos= ['ootb', 'initialStep', 'lowPtTripletStep', 'pixelPairStep', 'detachedTripletStep', 'mixedTripletStep', 'pixelLessStep', 'tobTecStep', 'jetCoreRegionalStep', 'muonSeededStepInOut', 'muonSeededStepOutIn']
-Qualities=['', 'highPurity']
-
 def newdirname(algo, quality):
-    ret = ""
-    if quality != "":
-        ret += "_"+quality
-    if not (algo == "ootb" and quality != ""):
-        ret += "_"+algo
+    return algo+"_"+quality
 
-    if ret != "" and ret[0] == "_":
-        ret = ret[1:]
-
-    return ret
+def subdirToAlgoQuality(subdir):
+    return subdir.split("_")
 
 def main(opts):
     files = opts.files
@@ -35,11 +26,10 @@ def main(opts):
         drawArgs["saveFormat"] = ".png"
 
     val = SimpleValidation(files, labels, opts.outputDir)
-    val.doPlots(Algos, Qualities, trackingPlots.plotter, algoDirMap=trackingPlots._tracks_map, newdirFunc=newdirname,
-                plotterDrawArgs=drawArgs)
+    val.doPlotsAuto(trackingPlots.plotter, subdirToAlgoQuality=subdirToAlgoQuality, newdirFunc=newdirname, plotterDrawArgs=drawArgs)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create standard set of tracking validation plots from one or more DQM files")
+    parser = argparse.ArgumentParser(description="Create standard set of tracking validation plots from one or more DQM files. Note that the output directory structure is not exactly the same as with test/trackingPerformanceValidation.py or test/trackingCompare.py")
     parser.add_argument("files", metavar="file", type=str, nargs="+",
                         help="DQM file to plot the validation plots from")
     parser.add_argument("-o", "--outputDir", type=str, default="plots",
