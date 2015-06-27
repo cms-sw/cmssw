11a4f5391718be70b4e651268fde250f976bad71
wmtan <wmtan@fnal.gov>
2015-06-28 00:40:16 +0200

    Account for ROOT5/ROOT6 differences

--- FWCore/FWLite/src/FWCoreFWLiteLinkDef.h
+++ FWCore/FWLite/src/FWCoreFWLiteLinkDef.h
@@ -1,6 +1,8 @@
 #include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/FWLiteEnabler.h"
 #include "FWCore/FWLite/src/branchToClass.h"
 #ifdef __CINT__
 #pragma link C++ class AutoLibraryLoader;
+#pragma link C++ class FWLiteEnabler;
 #pragma link C++ function branchToClass;
 #endif
