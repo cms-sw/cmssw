ec6f960467359896f0279e9237a1248506f4759d
Gaelle <Gaelle.Boudoul@cern.ch>
2015-06-27 05:09:52 +0200

    removing obsolete/unused files

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_Phase1_R34F16_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SLHCUpgradeSimulations.Geometry.fakeConditions_Phase1_cff import *
-siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
-        file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/R34F16/PixelSkimmedGeometry_phase1.txt')
-        )
-es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
