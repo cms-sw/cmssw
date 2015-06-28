fb9c973da773f16a6e231c59cfd2ad36d35deb97
Matthias <matthias.artur.weber@cern.ch>
2015-06-28 19:14:50 +0200

    enable MiniAOD and RECO monitoring, fix for unscheduled mode

--- DQMOffline/Configuration/python/DQMOffline_cff.py
+++ DQMOffline/Configuration/python/DQMOffline_cff.py
@@ -121,7 +121,7 @@ DQMOfflineEGamma = cms.Sequence( egammaDQMOffline )
 
 DQMOfflineBTag = cms.Sequence( bTagPlotsDATA )
                                                                  
-DQMOfflineMiniAOD = cms.Sequence( miniAODDQMSequence )
+DQMOfflineMiniAOD = cms.Sequence( miniAODDQMSequence * jetMETDQMOfflineSourceMiniAOD  )
 
 #nothing has hww for now
 #DQMOfflineNoHWW = cms.Sequence(DQMOffline)
