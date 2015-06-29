27f0537ed4af3398fd5323d660723079b97d9a72
Lindsey Gray <lindsey.gray@gmail.com>
2015-06-29 18:24:22 +0200

    enable MINIAODfromMINIAOD in cmsDriver.py

--- PhysicsTools/PatAlgos/python/slimming/modifiedMuons_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/modifiedMuons_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 slimmedMuons = cms.EDProducer(
     "ModifiedMuonProducer",
-    src = cms.InputTag("slimmedMuons"),
+    src = cms.InputTag("slimmedMuons::PAT"),
     modifierConfig = cms.PSet( modifications = cms.VPSet() )
 )
