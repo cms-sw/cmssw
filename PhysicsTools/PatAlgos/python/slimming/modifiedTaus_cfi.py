dcdc12a52e33234324afbaa9aaf4232555300e1c
Lindsey Gray <lindsey.gray@gmail.com>
2015-07-01 00:13:42 +0200

    working example of object editors for EGM

--- PhysicsTools/PatAlgos/python/slimming/modifiedTaus_cfi.py
+++ PhysicsTools/PatAlgos/python/slimming/modifiedTaus_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 slimmedTaus = cms.EDProducer(
     "ModifiedTauProducer",
-    src = cms.InputTag("slimmedTaus::PAT"),
+    src = cms.InputTag("slimmedTaus",processName=cms.InputTag.skipCurrentProcess()),
     modifierConfig = cms.PSet( modifications = cms.VPSet() )
 )
--- PhysicsTools/PatAlgos/python/slimming/modifyPrimaryPhysicsObjects_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/modifyPrimaryPhysicsObjects_cff.py
@@ -6,8 +6,13 @@ from PhysicsTools.PatAlgos.slimming.modifiedMuons_cfi import *
 from PhysicsTools.PatAlgos.slimming.modifiedTaus_cfi import *
 from PhysicsTools.PatAlgos.slimming.modifiedJets_cfi import *
 
-modifyPrimaryPhysicsObjects = cms.Sequence( slimmedElectrons *
-                                            slimmedPhotons   *
+#get any prereqs from POG areas
+from RecoEgamma.ElectronIdentification.ElectronMVAValueMapProducer_cfi import *
+from RecoEgamma.PhotonIdentification.PhotonIDValueMapProducer_cfi import *
+from RecoEgamma.PhotonIdentification.PhotonMVAValueMapProducer_cfi import *
+
+modifyPrimaryPhysicsObjects = cms.Sequence( electronMVAValueMapProducer * slimmedElectrons *
+                                            photonIDValueMapProducer * photonMVAValueMapProducer * slimmedPhotons *
                                             slimmedMuons     *
                                             slimmedTaus      *
                                             slimmedJets      *
