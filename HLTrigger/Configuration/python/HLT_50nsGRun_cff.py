4ac0ae1a5391f9930c780f189d7dfbc051be3059
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-07-03 17:18:03 +0200

    More HLT updates

--- HLTrigger/Configuration/python/HLT_50nsGRun_cff.py
+++ HLTrigger/Configuration/python/HLT_50nsGRun_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/50nsGRun/V139 (CMSSW_7_4_6_patch3)
+# /dev/CMSSW_7_4_0/50nsGRun/V141 (CMSSW_7_4_6_patch3)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/50nsGRun/V139')
+  tableName = cms.string('/dev/CMSSW_7_4_0/50nsGRun/V141')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
