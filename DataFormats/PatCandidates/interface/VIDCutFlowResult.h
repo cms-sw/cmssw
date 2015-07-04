29dae5645c7521a3541740431db97faca212ea6b
Lindsey Gray <lindsey.gray@gmail.com>
2015-07-04 13:14:24 +0200

    documentation, bugfix, and address code review for vid::CutFlowResult

--- DataFormats/PatCandidates/interface/VIDCutFlowResult.h
+++ DataFormats/PatCandidates/interface/VIDCutFlowResult.h
@@ -5,36 +5,67 @@
 #include <vector>
 #include <string>
 
+/*********
+ *
+ * Class: vid::CutFlowResult
+ * Author: L. Gray (FNAL)
+ * 
+ * A synthesis of the output of a VID selector into an easily
+ * manipulated class, such that cuts can be masked and sidebands
+ * created without major intervention on the part of the person
+ * doing analysis.
+ *
+ * The class is self-describing and the original cut-names, the
+ * values cut upon, the result of each cut used, and final cutflow 
+ * decision can be accessed with this class. Using this information
+ * the cut flow can be masked interactively by the user, allowing
+ * for a large degree of flexibility at the analysis level.
+ *
+ *********/
+
 namespace vid {
   class CutFlowResult {    
-  public:
     template<class T> friend class VersionedSelector;
+  public:    
 
     CutFlowResult() : bitmap_(0) {}
     CutFlowResult(const std::string& name,
-                  const std::map<std::string,unsigned>& n2idx,
-                  unsigned bitmap, 
-                  const std::vector<double>& values) : 
-      name_(name),
-      bitmap_(bitmap), 
-      values_(values), 
-      name_to_index_(n2idx) {}
-    
+                  const std::string& hash,
+                  const std::map<std::string,unsigned>& n2idx,                   
+                  const std::vector<double>& values,
+                  unsigned bitmap,
+                  unsigned mask = 0);
+
+    // get the original name of this cutflow
     const std::string& cutFlowName()   const { return name_; }
+    // get the md5 hash for this cutflow
+    const std::string& cutFlowHash()   const { return hash_; }
+    // did this cutflow (in its current state!) pass?
     bool               cutFlowPassed() const { 
-      const unsigned all_pass = name_to_index_.size()-1;
+      const unsigned all_pass = (1 << indices_.size()) - 1;
       return (all_pass&bitmap_) == all_pass; 
     } 
-    size_t             cutFlowSize()   const { return name_to_index_.size(); } 
-
+    // how many cuts in this cutflow?
+    size_t             cutFlowSize()   const { return indices_.size(); } 
+    
+    // get the name of a cut in the cutflow
+    // indexed by order it was executed
     const std::string& getNameAtIndex(const unsigned idx) const;
     
+    // get the individual cut result (pass/fail) either by name or by index
     bool getCutResultByIndex(const unsigned idx) const;
     bool getCutResultByName(const std::string& name) const;
 
+    // return true if the cut as index/name is masked out
+    bool isCutMasked(const unsigned idx) const;
+    bool isCutMasked(const std::string& name) const;
+
+    // get the value of variable that was cut on, either by name or by index
     double getValueCutUpon(const unsigned idx) const;
     double getValueCutUpon(const std::string& name) const;
 
+    // create a new copy of this cutflow masking out the listed cuts
+    // can be done either by name or by index
     CutFlowResult getCutFlowResultMasking(const unsigned idx) const;
     CutFlowResult getCutFlowResultMasking(const std::string& name) const;
     
@@ -42,10 +73,31 @@ namespace vid {
     CutFlowResult getCutFlowResultMasking(const std::vector<std::string>& names) const;
 
   private:
-    std::string name_;
-    unsigned bitmap_;
+    std::string name_, hash_;
+    unsigned bitmap_, mask_;
     std::vector<double> values_;
-    std::map<std::string,unsigned> name_to_index_;
+    std::vector<std::string> names_;
+    std::vector<unsigned> indices_;
+
+    CutFlowResult(const std::string& name,
+                  const std::string& hash,
+                  const std::vector<std::string>& names,
+                  const std::vector<unsigned>& indices,
+                  const std::vector<double>& values,
+                  unsigned bitmap,
+                  unsigned mask) :
+      name_(name),
+      hash_(hash),
+      bitmap_(bitmap),
+      mask_(mask),
+      values_(values),
+      names_(names),
+      indices_(indices) {}
+      
+
+    bool getMaskBit(const unsigned idx) const {
+      return (bool)(0x1&(mask_>>idx));
+    }
 
     bool getCutBit(const unsigned idx) const { 
       return (bool)(0x1&(bitmap_>>idx)); 
--- DataFormats/PatCandidates/src/VIDCutFlowResult.cc
+++ DataFormats/PatCandidates/src/VIDCutFlowResult.cc
@@ -6,9 +6,29 @@ namespace {
 }
 
 namespace vid {
+
+  CutFlowResult::CutFlowResult(const std::string& name,
+                               const std::string& hash,
+                               const std::map<std::string,unsigned>& n2idx,
+                               const std::vector<double>& values,
+                               unsigned bitmap,
+                               unsigned mask) : 
+    name_(name),
+    hash_(hash),
+    bitmap_(bitmap), 
+    mask_(mask),
+    values_(values) {
+    for( const auto& val : n2idx ) {
+      names_.push_back(val.first);
+      indices_.push_back(val.second);
+    }
+  }
+
   const std::string& CutFlowResult::getNameAtIndex(const unsigned idx) const {
-    for( const auto& value : name_to_index_ ) {
-      if( value.second == idx ) return value.first;
+    unsigned internal_idx = 0;
+    for( const auto& value : indices_ ) {
+      if( value == idx ) return names_[internal_idx];
+      ++internal_idx;
     }
     throw cms::Exception("IndexNotFound")
       << "index = " << idx << " has no corresponding cut name!";
@@ -16,25 +36,43 @@ namespace vid {
   }
 
   bool CutFlowResult::getCutResultByIndex(const unsigned idx) const {
-    if( idx >= name_to_index_.size() ) {
+    if( idx >= indices_.size() ) {
         throw cms::Exception("OutOfBounds")
           << idx << " is out of bounds for this cut flow!";
     }
-    return getCutBit(idx); 
+    return getCutBit(idx);
   }
 
   bool CutFlowResult::getCutResultByName(const std::string& name) const {
-    auto idx = name_to_index_.find(name);
-    if( idx == name_to_index_.end() ) {
+    auto found_name = std::lower_bound(names_.begin(),names_.end(),name);
+    if( found_name == names_.end() || *found_name != name ) {
       throw cms::Exception("UnknownName")
         << "Cut name: " << name 
         << " is not known for this cutflow!";
     }
-    return getCutBit(idx->second);
+    return getCutBit(indices_[std::distance(names_.begin(),found_name)]);
+  }
+
+  bool CutFlowResult::isCutMasked(const unsigned idx) const {
+    if( idx >= indices_.size() ) {
+        throw cms::Exception("OutOfBounds")
+          << idx << " is out of bounds for this cut flow!";
+    }
+    return getMaskBit(idx);
+  }
+
+  bool CutFlowResult::isCutMasked(const std::string& name) const {
+    auto found_name = std::lower_bound(names_.begin(),names_.end(),name);
+    if( found_name == names_.end() || *found_name != name ) {
+      throw cms::Exception("UnknownName")
+        << "Cut name: " << name 
+        << " is not known for this cutflow!";
+    }
+    return getMaskBit(indices_[std::distance(names_.begin(),found_name)]);
   }
 
   double CutFlowResult::getValueCutUpon(const unsigned idx) const {
-    if( idx >= name_to_index_.size() ) {
+    if( idx >= indices_.size() ) {
         throw cms::Exception("OutOfBounds")
           << idx << " is out of bounds for this cut flow!";
     }
@@ -42,64 +80,72 @@ namespace vid {
   }
   
   double CutFlowResult::getValueCutUpon(const std::string& name) const {
-    auto idx = name_to_index_.find(name);
-    if( idx == name_to_index_.end() ) {
+    auto found_name = std::lower_bound(names_.begin(),names_.end(),name);
+    if( found_name == names_.end() || *found_name != name ) {
       throw cms::Exception("UnknownName")
         << "Cut name: " << name 
         << " is not known for this cutflow!";
     }
-    return getCutValue(idx->second);
+    return getCutValue(indices_[std::distance(names_.begin(),found_name)]);
   }
 
   CutFlowResult CutFlowResult::
   getCutFlowResultMasking(const std::vector<unsigned>& idxs) const {
     unsigned bitmap = bitmap_;
+    unsigned mask = mask_;
     for( const unsigned idx : idxs ) {
-      if( idx >= name_to_index_.size() ) {
+      if( idx >= indices_.size() ) {
         throw cms::Exception("OutOfBounds")
           << idx << " is out of bounds for this cut flow!";
       }
-      bitmap = bitmap | 1 << idx;
+      mask = mask | 1 << idx;
     }
-    return CutFlowResult(name_,name_to_index_,bitmap,values_);
+    bitmap = bitmap | mask;
+    return CutFlowResult(name_,empty_str,names_,indices_,values_,bitmap,mask);
   }
 
   CutFlowResult CutFlowResult::
   getCutFlowResultMasking(const std::vector<std::string>& names) const {
     unsigned bitmap = bitmap_;
+    unsigned mask = mask_;
     for( const std::string& name : names ) {
-      auto idx = name_to_index_.find(name);
-      if( idx == name_to_index_.end() ) {
+      auto found_name = std::lower_bound(names_.begin(),names_.end(),name);
+      if( found_name == names_.end() || *found_name != name ) {
         throw cms::Exception("UnknownName")
           << "Cut name: " << name 
           << " is not known for this cutflow!";
       }
-      bitmap = bitmap | 1 << idx->second;
+      mask = mask | 1 << indices_[std::distance(names_.begin(),found_name)];
     }
-    return CutFlowResult(name_,name_to_index_,bitmap,values_);
+    bitmap = bitmap | mask;
+    return CutFlowResult(name_,empty_str,names_,indices_,values_,bitmap,mask);
   }
 
   CutFlowResult CutFlowResult::
   getCutFlowResultMasking(const unsigned idx) const {
-    unsigned bitmap = bitmap_;   
-    if( idx >= name_to_index_.size() ) {
+    unsigned bitmap = bitmap_;
+    unsigned mask = mask_;
+    if( idx >= indices_.size() ) {
       throw cms::Exception("OutOfBounds")
         << idx << " is out of bounds for this cut flow!";
     }
-    bitmap = bitmap | 1 << idx;
-    return CutFlowResult(name_,name_to_index_,bitmap,values_);
+    mask = mask | 1 << idx;
+    bitmap = bitmap | mask;
+    return CutFlowResult(name_,empty_str,names_,indices_,values_,bitmap,mask);
   }
   
   CutFlowResult CutFlowResult::
   getCutFlowResultMasking(const std::string& name) const {
     unsigned bitmap = bitmap_;
-    auto idx = name_to_index_.find(name);
-    if( idx == name_to_index_.end() ) {
+    unsigned mask = mask_;
+    auto found_name = std::lower_bound(names_.begin(),names_.end(),name);
+    if( found_name == names_.end() || *found_name != name ) {
       throw cms::Exception("UnknownName")
         << "Cut name: " << name 
         << " is not known for this cutflow!";
     }
-    bitmap = bitmap | 1 << idx->second;
-    return CutFlowResult(name_,name_to_index_,bitmap,values_);
+    mask = mask | 1 << indices_[std::distance(names_.begin(),found_name)];
+    bitmap = bitmap | mask;
+    return CutFlowResult(name_,empty_str,names_,indices_,values_,bitmap,mask);
   }
 }
