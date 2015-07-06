a3ca2436a61b4326994ef457b2702cffba5dc0c4
Broen van Besien <broen.van.besien@cern.ch>
2015-07-06 17:07:07 +0200

    Cleaning FileBlobCollection.cc

--- CondFormats/Common/src/FileBlobCollection.cc
+++ CondFormats/Common/src/FileBlobCollection.cc
@@ -5,14 +5,7 @@ void FileBlobCollection::addFileBlob(FileBlob &fileBlob) {
 }
 
 bool FileBlobCollection::mergeProduct(FileBlobCollection const &other) {
-  std::cout << "+++++ Length of fileBlobs is: " << fileBlobs.size()
-            << std::endl;
-  // fileBlobs.push_back(other.fileBlobs.front());
-  std::cout << "+++++ Using multi method!" << std::endl;
   fileBlobs.insert(fileBlobs.end(), other.fileBlobs.begin(),
                    other.fileBlobs.end());
-  std::cout << "+++++ Length of fileBlobs is: " << fileBlobs.size()
-            << std::endl;
-  // TODO: Should also work for more than one
   return true;
 }
