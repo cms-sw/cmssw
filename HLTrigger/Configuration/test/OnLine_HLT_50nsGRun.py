449e30033ae3bbdc2e136c7012ba0c7c32bc1313
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-07-06 11:03:50 +0200

    CMSHLT 464 and 465

--- HLTrigger/Configuration/test/OnLine_HLT_50nsGRun.py
+++ HLTrigger/Configuration/test/OnLine_HLT_50nsGRun.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/50nsGRun/V143 (CMSSW_7_4_6_patch3)
+# /dev/CMSSW_7_4_0/50nsGRun/V144 (CMSSW_7_4_6_patch3)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT50nsGRun" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/50nsGRun/V143')
+  tableName = cms.string('/dev/CMSSW_7_4_0/50nsGRun/V144')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
