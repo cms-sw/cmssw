184ecaf30ffae4ce61cdece1f38bf7b28012f3d8
Lukas <lukas.vanelderen@cern.ch>
2015-07-07 16:25:27 +0200

    esthetical fixes, removed obsolete if clausule

--- FastSimulation/TrackingRecHitProducer/plugins/SiTrackerGaussianSmearingRecHitConverter.cc
+++ FastSimulation/TrackingRecHitProducer/plugins/SiTrackerGaussianSmearingRecHitConverter.cc
@@ -1217,7 +1217,7 @@ SiTrackerGaussianSmearingRecHitConverter::matchHits(
 	    if(partnersFound == 1) {
 	      SiTrackerGSMatchedRecHit2D theMatchedHit = GSRecHitMatcher().match( &(*partner),  &(*rit),  gluedDet  , gluedsimtrackdir );
 	      if(partner == partnerNext)
-		theMatchedHit->setStereoLayerFirst();
+		theMatchedHit.setStereoLayerFirst();
 
 	      //	      std::cout << "Matched  hit: isMatched =\t" << theMatchedHit->isMatched() << ", "
 	      //	<<  theMatchedHit->monoHit() << ", " <<  theMatchedHit->stereoHit() << std::endl;
