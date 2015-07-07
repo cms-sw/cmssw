8fbbb22c10f004e4b463ffec659f50990b0de5a4
Sunanda <sunanda.banerjee@cern.ch>
2015-07-07 18:41:36 +0200

    Add Sim/Rec NumberingConstants and associated files

---
+++ Geometry/HcalCommonData/src/ES_HcalDDDConstants.cc
@@ -0,0 +1,6 @@
+#include "Geometry/HcalCommonData/interface/HcalDDDSimConstants.h"
+#include "Geometry/HcalCommonData/interface/HcalDDDRecConstants.h"
+#include "FWCore/Utilities/interface/typelookup.h"
+
+TYPELOOKUP_DATA_REG(HcalDDDSimConstants);
+TYPELOOKUP_DATA_REG(HcalDDDRecConstants);
