a4a7f1c59e6c5117d5d2b8cf8b89b3d94dbef6e3
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2015-07-10 15:00:43 +0200

    remove conflicts in HighMETSkim and ZMuSkim

--- DPGAnalysis/Skims/python/HighMETSkim_cff.py
+++ DPGAnalysis/Skims/python/HighMETSkim_cff.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 ## select events with at least one good PV
-pvFilter = cms.EDFilter(
+pvFilterHighMETSkim = cms.EDFilter(
     "VertexSelector",
     src = cms.InputTag("offlinePrimaryVertices"),
     cut = cms.string("!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2"),
@@ -16,86 +16,86 @@ pvFilter = cms.EDFilter(
 
 
 ## select events with high pfMET
-pfMETSelector = cms.EDFilter(
+pfMETSelectorHighMETSkim = cms.EDFilter(
     "CandViewSelector",
     src = cms.InputTag("pfMet"),
     cut = cms.string( "pt()>200" )
     )
 
-pfMETCounter = cms.EDFilter(
+pfMETCounterHighMETSkim = cms.EDFilter(
     "CandViewCountFilter",
-    src = cms.InputTag("pfMETSelector"),
+    src = cms.InputTag("pfMETSelectorHighMETSkim"),
     minNumber = cms.uint32(1),
     )
 
 pfMETSelSeq = cms.Sequence(
-			   pvFilter*
+			   pvFilterHighMETSkim*
                            ##HBHENoiseFilter*
-                           pfMETSelector*
-                           pfMETCounter
+                           pfMETSelectorHighMETSkim*
+                           pfMETCounterHighMETSkim
                            )
 
 
 
 ## select events with high caloMET
-caloMETSelector = cms.EDFilter(
+caloMETSelectorHighMETSkim = cms.EDFilter(
     "CandViewSelector",
     src = cms.InputTag("caloMetM"),
     cut = cms.string( "pt()>200" )
     )
 
-caloMETCounter = cms.EDFilter(
+caloMETCounterHighMETSkim = cms.EDFilter(
     "CandViewCountFilter",
-    src = cms.InputTag("caloMETSelector"),
+    src = cms.InputTag("caloMETSelectorHighMETSkim"),
     minNumber = cms.uint32(1),
     )
 
 caloMETSelSeq = cms.Sequence(
-			   pvFilter*
+			   pvFilterHighMETSkim*
                            ##HBHENoiseFilter*
-                           caloMETSelector*
-                           caloMETCounter
+                           caloMETSelectorHighMETSkim*
+                           caloMETCounterHighMETSkim
                            )
 
 
 ## select events with high MET dependent on PF and Calo MET Conditions
-CondMETSelector = cms.EDProducer(
+CondMETSelectorHighMETSkim = cms.EDProducer(
    "CandViewShallowCloneCombiner",
    decay = cms.string("pfMet caloMetM"),
    cut = cms.string(" (daughter(0).pt > 200) || (daughter(0).pt/daughter(1).pt > 2 && daughter(1).pt > 150 ) || (daughter(1).pt/daughter(0).pt > 2 && daughter(0).pt > 150 )  " )
    )
 
-CondMETCounter = cms.EDFilter(
+CondMETCounterHighMETSkim = cms.EDFilter(
     "CandViewCountFilter",
-    src = cms.InputTag("CondMETSelector"),
+    src = cms.InputTag("CondMETSelectorHighMETSkim"),
     minNumber = cms.uint32(1),
     )
 
 CondMETSelSeq = cms.Sequence(
-                           pvFilter*
+                           pvFilterHighMETSkim*
                            ##HBHENoiseFilter*
-                           CondMETSelector*
-                           CondMETCounter
+                           CondMETSelectorHighMETSkim*
+                           CondMETCounterHighMETSkim
                            )
 
 
 
 ## select events with PAT METs in MINIAODSIM - remember to keep the right branches in the cmsDriver
-miniMETSelector = cms.EDFilter(
+miniMETSelectorHighMETSkim = cms.EDFilter(
     "CandViewSelector",
     src = cms.InputTag("slimmedMETs"),
     cut = cms.string( "pt()>200" )
     )
 
-miniMETCounter = cms.EDFilter(
+miniMETCounterHighMETSkim = cms.EDFilter(
     "CandViewCountFilter",
-    src = cms.InputTag("miniMETSelector"),
+    src = cms.InputTag("miniMETSelectorHighMETSkim"),
     minNumber = cms.uint32(1),
     )
 
 miniMETSelSeq = cms.Sequence(
                            ##HBHENoiseFilter*
-                           miniMETSelector*
-                           miniMETCounter
+                           miniMETSelectorHighMETSkim*
+                           miniMETCounterHighMETSkim
                            )
 
