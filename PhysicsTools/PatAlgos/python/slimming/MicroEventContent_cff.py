93d87df435ab44958e65ada812878d6508c793d2
Roberto <roberto.covarelli@cern.ch>
2015-07-13 19:14:13 +0200

    added GenLumiInfoProduct for a posteriori cross-section calculation

--- PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
@@ -59,6 +59,7 @@ MicroEventContentMC.outputCommands += [
         'keep LHEEventProduct_*_*_*',
         'keep PileupSummaryInfos_*_*_*',
         'keep GenFilterInfo_*_*_*',
+        'keep GenLumiInfoProduct_*_*_*',
         'keep GenEventInfoProduct_generator_*_*',
         # RUN
         'keep LHERunInfoProduct_*_*_*',
