b5447c748d194ec2c47e002820a17f06b2b6b995
Hengne <Hengne.Li@CERN.CH>
2015-07-15 06:36:55 +0200

    update fastSim classical mixing PU input

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -343,7 +343,7 @@ baseDataSetRelease=[
     'CMSSW_7_3_0_pre1-PRE_LS172_V15_FastSim-v1',            # 4 - fast sim GEN-SIM-DIGI-RAW-HLTDEBUG for id tests
     'CMSSW_7_5_0_pre6-PU25ns_75X_mcRun2_asymptotic_v1-v1',  # 5 - fullSim PU 25ns premix
     'CMSSW_7_5_0_pre6-PU50ns_75X_mcRun2_startup_v1-v1',     # 6 - fullSim PU 50ns premix
-    'CMSSW_7_5_0_pre6-75X_mcRun2_asymptotic_v1_FastSim-v1', # 7 - fastSim premix
+    'CMSSW_7_5_0-75X_mcRun2_asymptotic_v1_FastSim-v1',      # 7 - fastSim PU for mixing
     'CMSSW_7_5_0_pre5-MCRUN2_75_V5_FastSim-v1',             # 8 - fastSim mb
     'CMSSW_7_5_0_pre4-PU25ns_MCRUN2_75_V1_FastSim-v2'       # 9 - fastSim pre-premix 
     ]
