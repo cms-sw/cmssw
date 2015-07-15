2c4ddd46fb16907fcff5c2573a24c07d4868f1c7
Lingshan <xulingshan@gmail.com>
2015-07-15 23:25:20 +0200

    bug fix eventplane DQM

--- DQM/Physics/src/CentralityDQM.cc
+++ DQM/Physics/src/CentralityDQM.cc
@@ -165,24 +165,25 @@ void CentralityDQM::analyze(const edm::Event& iEvent,
   h_vertex_y->Fill(vertex->begin()->y());
   h_vertex_z->Fill(vertex->begin()->z());
 
-  if (!ep.isValid()) return;
+  if (ep.isValid()){ 
 
   EvtPlaneCollection::const_iterator rp = ep->begin();
-  h_ep_HFm1->Fill((rp+HFm1)->angle());
-  h_ep_HFp1->Fill((rp+HFp1)->angle());
-  h_ep_trackm1->Fill((rp+trackm1)->angle());
-  h_ep_trackp1->Fill((rp+trackp1)->angle());
-  h_ep_castor1->Fill((rp+Castor1)->angle());
+  h_ep_HFm1->Fill(rp[HFm1].angle());
+  h_ep_HFp1->Fill(rp[HFp1].angle());
+  h_ep_trackm1->Fill(rp[trackm1].angle());
+  h_ep_trackp1->Fill(rp[trackp1].angle());
+  h_ep_castor1->Fill(rp[Castor1].angle());
 
-  h_ep_HFm2->Fill((rp+HFm2)->angle());
-  h_ep_HFp2->Fill((rp+HFp2)->angle());
-  h_ep_trackmid2->Fill((rp+trackmid2)->angle());
-  h_ep_trackm2->Fill((rp+trackm2)->angle());
-  h_ep_trackp2->Fill((rp+trackp2)->angle());
-  h_ep_castor2->Fill((rp+Castor2)->angle());
+  h_ep_HFm2->Fill(rp[HFm2].angle());
+  h_ep_HFp2->Fill(rp[HFp2].angle());
+  h_ep_trackmid2->Fill(rp[trackmid2].angle());
+  h_ep_trackm2->Fill(rp[trackm2].angle());
+  h_ep_trackp2->Fill(rp[trackp2].angle());
+  h_ep_castor2->Fill(rp[Castor2].angle());
 
-  h_ep_HFm3->Fill((rp+HFm3)->angle());
-  h_ep_HFp3->Fill((rp+HFp3)->angle());
-  h_ep_trackmid3->Fill((rp+trackmid3)->angle());
+  h_ep_HFm3->Fill(rp[HFm3].angle());
+  h_ep_HFp3->Fill(rp[HFp3].angle());
+  h_ep_trackmid3->Fill(rp[trackmid3].angle());
+}
 
 }
