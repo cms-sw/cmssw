da1f2810e133f74fee05a7fbd8333c98a36e40f6
Luke Molacek <molac007@umn.edu>
2015-07-15 09:03:27 -0500

    Added DualAnode Digis code

---
+++ DataFormats/HcalDigi/interface/DualAnodeDigis.h
@@ -0,0 +1,14 @@
+//#ifndef DUALANODEDIGIS_H
+//#define DUALANODEDIGIS_H
+
+//class DualAnodeDigis {
+//public:
+//	DualAnodeDigis();
+//	DualAnodeDigis(const DualAnodeDigis& orig);
+//	virtual ~DualAnodeDigis();
+//private:
+
+//};
+
+//#endif  
+
