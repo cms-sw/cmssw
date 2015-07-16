f021e7f88653a0f07fe39799276feb7987c10042
matthieu <matthieu.marionneau@cern.ch>
2015-07-16 10:18:03 +0200

    update the MET XY correction and met reprocessing example

---
+++ PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
@@ -0,0 +1,89 @@
+import FWCore.ParameterSet.Config as cms
+
+# Define the CMSSW process
+process = cms.Process("RERUN")
+
+# Load the standard set of configuration modules
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# Message Logger settings
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+# Set the process options -- Display summary at the end, enable unscheduled execution
+process.options = cms.untracked.PSet( 
+    allowUnscheduled = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False) 
+)
+
+# How many events to process
+process.maxEvents = cms.untracked.PSet( 
+   input = cms.untracked.int32(20)
+)
+
+### =====================================================================================================
+usePrivateSQlite =False
+
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag.globaltag = '75X_mcRun2_asymptotic_v1'   # for Simulation #same globalTag
+
+if usePrivateSQlite:
+    from CondCore.DBCommon.CondDBSetup_cfi import *
+    era="Summer15_V5_MC"
+    process.jec = cms.ESSource("PoolDBESSource",CondDBSetup,
+                               connect = cms.string( "sqlite_file:PhysicsTools/PatAlgos/test/"+era+".db" ),
+                               toGet =  cms.VPSet(
+            cms.PSet(
+                record = cms.string("JetCorrectionsRecord"),
+                tag = cms.string("JetCorrectorParametersCollection_"+era+"_AK4PF"),
+                label= cms.untracked.string("AK4PF")
+                ),
+            cms.PSet(
+                record = cms.string("JetCorrectionsRecord"),
+                tag = cms.string("JetCorrectorParametersCollection_"+era+"_AK4PFchs"),
+                label= cms.untracked.string("AK4PFchs")
+                ),
+            )
+                               )
+    process.es_prefer_jec = cms.ESPrefer("PoolDBESSource",'jec')
+
+### =====================================================================================================
+
+
+# Define the input source
+process.source = cms.Source("PoolSource", 
+    fileNames = cms.untracked.vstring([
+            #"file:patMiniAOD_standard.root"
+            "root://eoscms//eos/cms/store/relval/CMSSW_7_5_0/RelValTTbar_13/MINIAODSIM/PU25ns_75X_mcRun2_asymptotic_v1_FastSim-v1/00000/2C2B7040-042B-E511-9888-0025905A60D6.root",
+    ])
+)
+
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+
+#default configuration for miniAOD reprocessing
+runMetCorAndUncFromMiniAOD(process)
+
+
+process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionLevel = cms.untracked.int32(4),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = cms.untracked.vstring( "keep *_slimmedMETs_*_RERUN",
+                                        #    "keep *_*_*_PAT"
+                                            ),
+    fileName = cms.untracked.string('corMETMiniAOD.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    dropMetaData = cms.untracked.string('ALL'),
+    fastCloning = cms.untracked.bool(False),
+    overrideInputFileSplitLevels = cms.untracked.bool(True)
+)
+
+
+process.MINIAODSIMoutput_step = cms.EndPath(process.MINIAODSIMoutput)
