f46a467f78efaf701a9a3a4f45dd0eba55f25bc0
Mia <mia.tosi@cern.ch>
2015-07-16 12:12:14 +0200

    fix location

--- RecoTracker/TkTrackingRegions/src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducerFactory.h" 	 
-#include "RecoTracker/TkTrackingRegions/interface/TrackingRegionProducer.h" 	 
-#include "RecoTracker/TkTrackingRegions/src/PointSeededTrackingRegionsProducer.h"
-#include "RecoTracker/MeasurementDet/interface/MeasurementTrackerEvent.h"
-
-DEFINE_EDM_PLUGIN(TrackingRegionProducerFactory, PointSeededTrackingRegionsProducer, "PointSeededTrackingRegionsProducer");
-//
