4f455ed77724e24abae6273828cce1f1f5006a97
Quan Wang <betterwang@gmail.com>
2015-07-17 16:56:14 +0200

    Update CentralityDQM.h

--- DQM/Physics/src/CentralityDQM.h
+++ DQM/Physics/src/CentralityDQM.h
@@ -26,7 +26,7 @@ class CentralityDQM : public DQMEDAnalyzer {
   virtual ~CentralityDQM();
 
  protected:
-  virtual void analyze(edm::Event const& e, edm::EventSetup const& eSetup);
+  virtual void analyze(edm::Event const& e, edm::EventSetup const& eSetup) override;
 
  private:
   virtual void bookHistograms(DQMStore::IBooker& bei, edm::Run const&,
