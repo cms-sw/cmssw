9a127cc682139c00518b24e61cd82275170b413a
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-07-17 06:29:46 +0200

    Adjust menus used for relvals

--- Configuration/HLT/python/autoHLT.py
+++ Configuration/HLT/python/autoHLT.py
@@ -2,10 +2,10 @@
 #   cmsDiver.py hlt -s HLT:@relval
 
 autoHLT = {
-  'frozen25ns' : '25ns14e33_v1',
-  'relval25ns' : 'GRun',
-  'frozen50ns' : '50ns_5e33_v1',
-  'relval50ns' : '50nsGRun',
+  'frozen25ns' : '25ns14e33_v3',
+  'relval25ns' : '25ns14e33_v3',
+  'frozen50ns' : '50ns_5e33_v3',
+  'relval50ns' : '50ns_5e33_v3',
   'fake'       : 'Fake',
   'test'       : 'GRun',
 }
