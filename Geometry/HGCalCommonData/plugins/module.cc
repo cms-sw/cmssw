bfb4a5b02c813da6d514e4d8834a6df7d255002d
Sunanda <sunanda.banerjee@cern.ch>
2015-07-17 23:44:28 +0200

    Now the plugins

---
+++ Geometry/HGCalCommonData/plugins/module.cc
@@ -0,0 +1,6 @@
+#include "Geometry/HGCalCommonData/plugins/DDHGCalEEAlgo.h"
+#include "Geometry/HGCalCommonData/plugins/DDHGCalHEAlgo.h"
+#include "DetectorDescription/Algorithm/interface/DDAlgorithmFactory.h"
+
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalEEAlgo, "hgcal:DDHGCalEEAlgo");
+DEFINE_EDM_PLUGIN (DDAlgorithmFactory, DDHGCalHEAlgo, "hgcal:DDHGCalHEAlgo");
