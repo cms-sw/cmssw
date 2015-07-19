c9cf9c9a14e26d9602b7e0030a040a52511ca11f
Hengne <Hengne.Li@CERN.CH>
2015-07-19 11:10:01 +0200

    update fullsim premixing input pu sample

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -341,8 +341,8 @@ baseDataSetRelease=[
                                                             # THIS ABOVE IS NOT USED, AT THE MOMENT
     'CMSSW_7_5_0-75X_mcRun2_asymptotic_v1-v1',                     # 3 - 13 TeV samples with GEN-SIM from 750_p4; also GEN-SIM-DIGI-RAW-HLTDEBUG for id tests
     'CMSSW_7_3_0_pre1-PRE_LS172_V15_FastSim-v1',            # 4 - fast sim GEN-SIM-DIGI-RAW-HLTDEBUG for id tests
-    'CMSSW_7_5_0_pre6-PU25ns_75X_mcRun2_asymptotic_v1-v1',  # 5 - fullSim PU 25ns premix
-    'CMSSW_7_5_0_pre6-PU50ns_75X_mcRun2_startup_v1-v1',     # 6 - fullSim PU 50ns premix
+    'CMSSW_7_5_0-PU25ns_75X_mcRun2_asymptotic_v1-v1',       # 5 - fullSim PU 25ns premix
+    'CMSSW_7_5_0-PU50ns_75X_mcRun2_startup_v1-v1',          # 6 - fullSim PU 50ns premix
     'CMSSW_7_5_0-75X_mcRun2_asymptotic_v1_FastSim-v1',      # 7 - fastSim PU for mixing
     'CMSSW_7_5_0_pre5-MCRUN2_75_V5_FastSim-v1',             # 8 - fastSim mb
     'CMSSW_7_5_0_pre4-PU25ns_MCRUN2_75_V1_FastSim-v2'       # 9 - fastSim pre-premix 
