8b4907f146fdc25fbf4e5741fdca3b517f9208de
Matthias <matthias.artur.weber@cern.ch>
2015-07-21 16:22:34 +0200

    fix for running in unscheduled mode, rebased to 75X

--- DQMOffline/JetMET/test/run_PromptAna.py
+++ DQMOffline/JetMET/test/run_PromptAna.py
@@ -6,13 +6,13 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 from Configuration.StandardSequences.GeometryRecoDB_cff import *
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration/StandardSequences/MagneticField_cff")
-process.load("Configuration/StandardSequences/FrontierConditions_GlobalTag_cff")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
 #for data in 720pre7
-process.GlobalTag.globaltag ='MCRUN2_74_V9'
+process.GlobalTag.globaltag ='75X_dataRun1_v2'
 
 # check # of bins
 process.load("DQMServices.Components.DQMStoreStats_cfi")
@@ -21,28 +21,14 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/2AE21D5C-6FF1-E411-B3AC-02163E00E60F.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/2C0E0382-68F1-E411-A8B2-02163E00E640.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/34FEEE59-70F1-E411-9554-02163E00F420.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/403D5922-84F1-E411-8F42-02163E00AD2E.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/42658BC2-69F1-E411-9D7E-02163E00F8B3.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/4E24BEC6-8FF1-E411-92A5-02163E010FCF.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/5CB03F23-5EF1-E411-9294-02163E013B50.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/6A08C46A-6DF1-E411-93A5-02163E00F298.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/6E82B231-7BF1-E411-BBD7-02163E013D2D.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/76E26B39-61F1-E411-BB03-02163E00E814.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/84E5227D-73F1-E411-B9F9-02163E010FD9.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/8C966E42-72F1-E411-95F8-02163E0130C3.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/9A1C1859-63F2-E411-8935-02163E00E913.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/A65D6BC7-64F1-E411-B8E9-02163E00F710.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/A6FF4CF5-6CF1-E411-9580-02163E00E61F.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/A8E1054B-EDF1-E411-864F-02163E00B782.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/CECD5CE1-66F1-E411-BD44-02163E0130F9.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/D27937E4-6AF1-E411-B9E8-02163E00C323.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/E6AD30C8-63F1-E411-9828-02163E00B999.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/F0A1C285-75F1-E411-9981-02163E00E96D.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/F4B1EF05-69F1-E411-BFEF-02163E00EBA7.root',
-       '/store/relval/CMSSW_7_4_1/RelValZMM_13/GEN-SIM-RECO/MCRUN2_74_V9_extended-v2/00000/FC2233E0-72F1-E411-82EA-02163E00E5B6.root'
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/003E50FE-F71B-E511-B849-002590593902.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/0212B89F-401C-E511-90B7-0025905964A2.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/02925C10-FC1B-E511-A4B4-0025905B85A2.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/040DBAE5-FB1B-E511-9854-0025905B85D0.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/0658D146-2C1C-E511-BCA1-0025905A48F2.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/0A06BB23-401C-E511-ACE9-0025905A610C.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/0AA1F5F7-FA1B-E511-B761-0025905A48FC.root',
+       '/store/relval/CMSSW_7_5_0_pre6/JetHT/RECO/75X_dataRun1_v2_RelVal_jet2012D-v1/00000/0C963256-311C-E511-88DA-002618943842.root'
        ] );
 
 
--- Validation/RecoMET/test/sequence_validation_cfg.py
+++ Validation/RecoMET/test/sequence_validation_cfg.py
@@ -3,15 +3,15 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("METVALIDATION")
 
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration/StandardSequences/MagneticField_cff")
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
 #process.GlobalTag.globaltag = 'START42_V17::All'
 ##process.GlobalTag.globaltag = 'MC_38Y_V14::All'
 ## for 6_2_0 QCD
-process.GlobalTag.globaltag = 'MCRUN2_74_V7::All'
+process.GlobalTag.globaltag = '75X_mcRun2_asymptotic_v1'
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
@@ -29,10 +29,10 @@ secFiles = cms.untracked.vstring()
 process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
        #for RECO
-       '/store/relval/CMSSW_7_4_0_pre8/RelValTTbar_13/GEN-SIM-RECO/MCRUN2_74_V7-v1/00000/48E3FDFE-3DBD-E411-9B99-0025905A613C.root',
-       '/store/relval/CMSSW_7_4_0_pre8/RelValTTbar_13/GEN-SIM-RECO/MCRUN2_74_V7-v1/00000/706A960F-54BD-E411-8561-00261894384F.root',
-       '/store/relval/CMSSW_7_4_0_pre8/RelValTTbar_13/GEN-SIM-RECO/MCRUN2_74_V7-v1/00000/E4EF6410-54BD-E411-8838-002590593920.root',
-       '/store/relval/CMSSW_7_4_0_pre8/RelValTTbar_13/GEN-SIM-RECO/MCRUN2_74_V7-v1/00000/FA18AB00-3EBD-E411-AAE8-0025905A608A.root'
+       '/store/relval/CMSSW_7_5_0_pre6/RelValQCD_FlatPt_15_3000HS_13/GEN-SIM-RECO/75X_mcRun2_asymptotic_v1-v1/00000/06121D17-661B-E511-AE2A-0025905A60B2.root',
+       '/store/relval/CMSSW_7_5_0_pre6/RelValQCD_FlatPt_15_3000HS_13/GEN-SIM-RECO/75X_mcRun2_asymptotic_v1-v1/00000/103A92CC-681B-E511-A923-002618943935.root',
+       '/store/relval/CMSSW_7_5_0_pre6/RelValQCD_FlatPt_15_3000HS_13/GEN-SIM-RECO/75X_mcRun2_asymptotic_v1-v1/00000/16752615-911B-E511-991B-0025905A613C.root',
+       '/store/relval/CMSSW_7_5_0_pre6/RelValQCD_FlatPt_15_3000HS_13/GEN-SIM-RECO/75X_mcRun2_asymptotic_v1-v1/00000/1C073F39-671B-E511-94BE-0025905A60D0.root'
         #for MINIAODtests 
         #'/store/relval/CMSSW_7_3_0_pre1/RelValTTbar_13/MINIAODSIM/PU50ns_PRE_LS172_V16-v1/00000/9886ACB4-F45E-E411-9E5D-02163E00F01E.root' 
 ] );
