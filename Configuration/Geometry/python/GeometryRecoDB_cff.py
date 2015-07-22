8a8d399f00ba5a5bfbf3e5327ab661fcff1e551a
Sunanda <sunanda.banerjee@cern.ch>
2015-07-22 17:13:48 +0200

    Remove reference to hcalParameters

--- Configuration/Geometry/python/GeometryRecoDB_cff.py
+++ Configuration/Geometry/python/GeometryRecoDB_cff.py
@@ -16,7 +16,7 @@ from Geometry.CaloEventSetup.AlignedCaloGeometryDBReader_cfi import *
 from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
 from Geometry.EcalMapping.EcalMapping_cfi import *
 from Geometry.EcalMapping.EcalMappingRecord_cfi import *
-from Geometry.HcalCommonData.hcalDDConstants_cff import *
+from Geometry.HcalCommonData.hcalDBConstants_cff import *
 
 #  Alignment
 from Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometryDB_cff import *
