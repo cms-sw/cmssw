41024f997b18e2360ae638113eb2aca72b4a5a76
Sunanda <sunanda.banerjee@cern.ch>
2015-07-23 22:06:43 +0200

    Add reconstruction geometry and topology for HGCal

---
+++ Geometry/CaloEventSetup/python/HGCalTopology_cfi.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+#
+# This cfi should be included to build the HGCal Topologies
+#
+HGCalEETopologyBuilder = cms.ESProducer("HGCalTopologyBuilder",
+                                        Name = cms.untracked.string("HGCalEESensitive"),
+                                        Type = cms.untracked.int32(0),
+                                        HalfType = cms.untracked.bool(False)
+                                        )
+
+
+HGCalHESilTopologyBuilder = cms.ESProducer("HGCalTopologyBuilder",
+                                           Name = cms.untracked.string("HGCalHESiliconSensitive"),
+                                           Type = cms.untracked.int32(1),
+                                           HalfType = cms.untracked.bool(False)
+                                           )
+
+
+HGCalHESciTopologyBuilder = cms.ESProducer("HGCalTopologyBuilder",
+                                           Name = cms.untracked.string("HGCalHEScintillatorSensitive"),
+                                           Type = cms.untracked.int32(2),
+                                           HalfType = cms.untracked.bool(True)
+                                           )
+
