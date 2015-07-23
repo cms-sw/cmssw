5a9a9a5d5e0ebf789d7bea6d8ee732b5f02f85e5
Piet <piet.verwilligen@cern.ch>
2015-07-23 12:25:32 +0200

    ME0 Segment

---
+++ RecoLocalMuon/GEMSegment/plugins/ME0SegmentBuilderPluginFactory.cc
@@ -0,0 +1,12 @@
+/** \file ME0SegmentBuilderPluginFactory.cc
+ *
+ * $Date: 2014/02/04 13:32:56 $
+ * $Revision: 1.1 $
+ * \author Marcello Maggi
+ * 
+ */
+
+#include <RecoLocalMuon/GEMSegment/plugins/ME0SegmentBuilderPluginFactory.h>
+
+EDM_REGISTER_PLUGINFACTORY(ME0SegmentBuilderPluginFactory,"ME0SegmentBuilderPluginFactory");
+
---
+++ RecoLocalMuon/GEMSegment/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/RecoLocalMuon/GEMSegment/',1)[0])+'/cfipython/slc6_amd64_gcc491/RecoLocalMuon/GEMSegment')
