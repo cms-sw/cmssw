d629b427e02a1115a644f79e8a14c68304680770
jingyu zhang <jzhan30@uic.edu>
2015-07-24 21:17:45 +0200

    update a file

--- DQM/L1TMonitorClient/python/L1TStage1Layer2Client_cfi.py
+++ DQM/L1TMonitorClient/python/L1TStage1Layer2Client_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 l1tStage1Layer2Client = cms.EDAnalyzer("L1TGCTClient",
     prescaleLS = cms.untracked.int32(-1),
-    monitorDir = cms.untracked.string('L1T/stage1layer2'),
+    monitorDir = cms.untracked.string('L1T/L1TStage1Layer2'),
     prescaleEvt = cms.untracked.int32(1),
     runInEventLoop=cms.untracked.bool(False),
     runInEndLumi=cms.untracked.bool(True),
