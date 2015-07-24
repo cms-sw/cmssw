176a7e98de04ad385081e8b57a2aa69fa86f26a2
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-07-24 15:54:35 +0200

    Select menus

--- Configuration/HLT/python/autoHLT.py
+++ Configuration/HLT/python/autoHLT.py
@@ -3,8 +3,8 @@
 
 autoHLT = {
   'frozen25ns' : '25ns14e33_v3',
-  'relval25ns' : '25ns14e33_v2',
-  'frozen50ns' : '50ns_5e33_v3',
+  'relval25ns' : '25ns14e33_v3',
+  'frozen50ns' : '50ns_5e33_v2',
   'relval50ns' : '50ns_5e33_v2',
   'fake'       : 'Fake',
   'test'       : 'GRun',
