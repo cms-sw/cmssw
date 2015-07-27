31a9989fb36fafbb0ee08abb29a0712b15677f4a
Sachiko Toda <sachikot@phys.ksu.edu>
2015-07-27 22:47:55 +0200

    scal dqm monitoring for 76X (cherry picked from commit 87804255e3fa12a26b3aa865d395318f95ac32b8)

--- DQMServices/Components/src/DQMScalInfo.cc
+++ DQMServices/Components/src/DQMScalInfo.cc
@@ -19,7 +19,6 @@ using namespace std;
 
 DQMScalInfo::DQMScalInfo(const edm::ParameterSet& ps)
 {
-  std::cout << "Parameter setting" << std::endl;
   parameters_ = ps;
 
   scalfolder_          = parameters_.getUntrackedParameter<std::string>("dqmScalFolder", "Scal") ;
@@ -35,36 +34,22 @@ DQMScalInfo::~DQMScalInfo(){
 void DQMScalInfo::bookHistograms(DQMStore::IBooker & ibooker,
                                 edm::Run const & /* iRun */,
                                 edm::EventSetup const & /* iSetup */) {
-  std::cout << "histo booking" << std::endl;
+
   // Fetch GlobalTag information and fill the string/ME.
   ibooker.cd();
   ibooker.setCurrentFolder(scalfolder_ +"/L1TriggerScalers/");
   const int fracLS = 16;
   const int maxLS  = 250;
-  //  MySCALhisto_ = ibooker.book1D("MyScalHisto","MyScalHisto",10,0.,10.);
   hlresync_    = ibooker.book1D("lresync","Orbit of last resync",fracLS*maxLS,0,maxLS*262144);
   hlOC0_       = ibooker.book1D("lOC0","Orbit of last OC0",fracLS*maxLS,0,maxLS*262144);
   hlTE_        = ibooker.book1D("lTE","Orbit of last TestEnable",fracLS*maxLS,0,maxLS*262144);
   hlstart_     = ibooker.book1D("lstart","Orbit of last Start",fracLS*maxLS,0,maxLS*262144);
   hlEC0_       = ibooker.book1D("lEC0","Orbit of last EC0",fracLS*maxLS,0,maxLS*262144);
   hlHR_        = ibooker.book1D("lHR","Orbit of last HardReset",fracLS*maxLS,0,maxLS*262144);
-
-
 }
 
 void DQMScalInfo::analyze(const edm::Event& e, const edm::EventSetup& c){
   makeL1Scalars(e);
-  return;
-}
-
-void
-DQMScalInfo::endLuminosityBlock(const edm::LuminosityBlock& l, const edm::EventSetup& c)
-{
-  std::cout << "Ending LS blocks" << std::endl;
-
-  //Set info to be plotted vs LS
-  //  MySCALhisto_->setBinContent(1,1.);
-
   return;
 }
 
@@ -81,6 +66,5 @@ DQMScalInfo::makeL1Scalars(const edm::Event& e)
   hlEC0_->Fill((*l1ts)[0].lastEventCounter0());
   hlHR_->Fill((*l1ts)[0].lastHardReset());  
 
-  //Access whatever collections needed and fill histos...
   return ;
 }
