be2c7a5098c5b3dd2c23df3909309e1bc34bb7be
Johannes <johannes.brandstetter@cern.ch>
2015-07-27 14:50:25 +0200

    without subdirectory

--- DQMOffline/JetMET/plugins/SealModule.cc
+++
@@ -1,5 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "DQMOffline/JetMET/plugins/SusyPostProcessor.h"
-
-DEFINE_FWK_MODULE(SusyPostProcessor);
--- DQMOffline/JetMET/python/dataCertificationJetMET_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DQMOffline.JetMET.dataCertificationJetMET_cfi import *
-
-dataCertificationJetMETSequence = cms.Sequence(qTesterJet + qTesterMET + dataCertificationJetMET)
