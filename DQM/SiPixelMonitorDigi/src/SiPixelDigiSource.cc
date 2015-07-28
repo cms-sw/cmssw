0701ae38b61074387fa10761e6d37ce0fbf778c8
Francesco Fiori <francesco.fiori@cern.ch>
2015-07-28 15:36:34 +0200

    prevent empty histo in Offline DQM GUI

--- DQM/SiPixelMonitorDigi/src/SiPixelDigiSource.cc
+++ DQM/SiPixelMonitorDigi/src/SiPixelDigiSource.cc
@@ -65,6 +65,7 @@ SiPixelDigiSource::SiPixelDigiSource(const edm::ParameterSet& iConfig) :
   diskOn( conf_.getUntrackedParameter<bool>("diskOn",false) ),
   bigEventSize( conf_.getUntrackedParameter<int>("bigEventSize",1000) ), 
   isUpgrade( conf_.getUntrackedParameter<bool>("isUpgrade",false) ),
+  isOffline( conf_.getUntrackedParameter<bool>("isOffline",false) ), 
   noOfLayers(0),
   noOfDisks(0)
 {
@@ -198,7 +199,7 @@ void SiPixelDigiSource::analyze(const edm::Event& iEvent, const edm::EventSetup&
   int lumiSection = (int)iEvent.luminosityBlock();
   int nEventDigis = 0; int nActiveModules = 0;
   
-  if(modOn){
+  if(modOn && !isOffline){
     if(averageDigiOccupancy && lumiSection%8==0){
       averageDigiOccupancy->Reset();
       nBPIXDigis = 0; 
@@ -206,7 +207,7 @@ void SiPixelDigiSource::analyze(const edm::Event& iEvent, const edm::EventSetup&
       for(int i=0; i!=40; i++) nDigisPerFed[i]=0;  
     }
   }
-  if(!modOn){
+  if(!modOn && !isOffline){
     if(averageDigiOccupancy && lumiSection%1==0){
       averageDigiOccupancy->Reset();
       nBPIXDigis = 0; 
@@ -538,14 +539,11 @@ void SiPixelDigiSource::analyze(const edm::Event& iEvent, const edm::EventSetup&
   
   // Actual digi occupancy in a FED compared to average digi occupancy per FED
   if(averageDigiOccupancy){
-    int maxfed=0;
-    for(int i=0; i!=32; i++){
-      if(nDigisPerFed[i]>maxfed) maxfed=nDigisPerFed[i];
-    }
+    
     for(int i=0; i!=40; i++){
       float averageOcc = 0.;
       if(i<32){
-        float averageBPIXFed = float(nBPIXDigis-maxfed)/31.;
+        float averageBPIXFed = float(nBPIXDigis)/32.;
 	if(averageBPIXFed>0.) averageOcc = nDigisPerFed[i]/averageBPIXFed;
       }else{
         float averageFPIXFed = float(nFPIXDigis)/8.;
