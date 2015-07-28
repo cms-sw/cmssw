56d3713eabb2a1ea3fb471aca12a3044bf6e0d96
Gianluca <gianluca.cerminara@cern.ch>
2015-07-28 16:51:55 +0200

    revert all changes to the AlignmentProducer EDLooper since we now have a dedicated producer

--- Alignment/CommonAlignmentProducer/plugins/AlignmentProducer.cc
+++ Alignment/CommonAlignmentProducer/plugins/AlignmentProducer.cc
@@ -640,6 +640,8 @@ void AlignmentProducer::simpleMisalignment_(const Alignables &alivec, const std:
 //__________________________________________________________________________________________________
 void AlignmentProducer::createGeometries_( const edm::EventSetup& iSetup )
 {
+   edm::ESTransientHandle<DDCompactView> cpv;
+   iSetup.get<IdealGeometryRecord>().get( cpv );
 
    if (doTracker_) {
      edm::ESHandle<GeometricDet> geometricDet;
@@ -649,9 +651,6 @@ void AlignmentProducer::createGeometries_( const edm::EventSetup& iSetup )
    }
 
    if (doMuon_) {
-     edm::ESTransientHandle<DDCompactView> cpv;
-     iSetup.get<IdealGeometryRecord>().get( cpv );
-
      edm::ESHandle<MuonDDDConstants> mdc;
      iSetup.get<MuonNumberingRecord>().get(mdc);
      DTGeometryBuilderFromDDD DTGeometryBuilder;
