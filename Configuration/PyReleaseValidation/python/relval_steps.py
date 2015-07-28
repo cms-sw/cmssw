9be147c8d3e98b4efeded93baa2178970e26faf7
Gianluca <gianluca.cerminara@cern.ch>
2015-07-28 15:53:00 +0200

    Minor fixes to finalize the check of the new workflow in in the relval 1001

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -1197,7 +1197,7 @@ steps['ALCAHARVD4']={'-s':'ALCAHARVEST:SiPixelAli',
                     '--conditions':'auto:run1_data',
                     '--scenario':'pp',
                     '--data':'',
-                    '--filein':'file:PromptCalibProdSiStripGains.root'}
+                    '--filein':'file:PromptCalibProdSiPixelAli.root'}
 
 steps['RECOHISt4']=steps['RECOHI']
 steps['RECOHIMIX']=merge([steps['RECOHI'],{'--pileup':'HiMix','--pileup_input':'das:/RelValHydjetQ_MinBias_5020GeV/%s/GEN-SIM'%(baseDataSetRelease[8])}])
