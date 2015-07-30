1a6b6679fb25eb75508f674f0209651a918a2a71
Jay Dittmann <dittmann@fnal.gov>
2015-07-29 23:04:52 -0500

    very minor histogram binning adjustment

--- Validation/HcalDigis/src/HcalDigisValidation.cc
+++ Validation/HcalDigis/src/HcalDigisValidation.cc
@@ -79,7 +79,7 @@ void HcalDigisValidation::bookHistograms(DQMStore::IBooker &ib, edm::Run const &
     HistLim tp_hl_et(260, -10, 250);
     HistLim tp_hl_ntp(640, -20, 3180);
     HistLim tp_hl_ntp_sub(404, -20, 2000);
-    HistLim tp_hl_ieta(71, -35.5, 35.5);
+    HistLim tp_hl_ieta(85, -42.5, 42.5);
     
 
     book1D(ib,"HcalDigiTask_tp_et", tp_hl_et);
