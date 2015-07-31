103e5fcd8476cad3902aa2d60d01cc7ce797d432
Junghwan John Goh <jhgoh@cern.ch>
2015-07-31 16:24:38 +0200

    remove process name in inputtag

--- GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
+++ GeneratorInterface/RivetInterface/python/genParticles2HepMC_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 genParticles2HepMC = cms.EDProducer("GenParticles2HepMCConverter",
     genParticles = cms.InputTag("genParticles"),
-    genEventInfo = cms.InputTag("generator", "", "SIM"),
+    genEventInfo = cms.InputTag("generator"),
 )
