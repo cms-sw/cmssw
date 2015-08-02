577d765e51606bdbb8282f0112c777c449de8a97
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2015-08-02 12:11:26 -0500

    now keeping track of the order of input variables, added option to use AdaBoost with GBRForest

--- CommonTools/Utils/interface/TMVAEvaluator.h
+++ CommonTools/Utils/interface/TMVAEvaluator.h
@@ -18,20 +18,21 @@ class TMVAEvaluator {
     ~TMVAEvaluator();
 
     void initialize(const std::string & options, const std::string & method, const std::string & weightFile,
-                    const std::vector<std::string> & variables, const std::vector<std::string> & spectators, const bool useGBRForest=true);
-    float evaluate(const std::map<std::string,float> & inputs, const bool useSpectators=false);
+                    const std::vector<std::string> & variables, const std::vector<std::string> & spectators, bool useGBRForest=false, bool useAdaBoost=false);
+    float evaluate(const std::map<std::string,float> & inputs, bool useSpectators=false);
 
   private:
     bool mIsInitialized;
     bool mUsingGBRForest;
+    bool mUseAdaBoost;
 
     std::string mMethod;
     std::unique_ptr<TMVA::Reader> mReader;
     std::unique_ptr<TMVA::IMethod> mIMethod;
     std::unique_ptr<const GBRForest> mGBRForest;
 
-    std::map<std::string,float> mVariables;
-    std::map<std::string,float> mSpectators;
+    std::map<std::string,std::pair<size_t,float>> mVariables;
+    std::map<std::string,std::pair<size_t,float>> mSpectators;
 };
 
 #endif // CommonTools_Utils_TMVAEvaluator_h
