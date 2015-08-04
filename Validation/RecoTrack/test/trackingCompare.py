43ccc08e13fe253e090d90f6cceeb6fc339bb1e4
anshul <anshul.kapoor@students.iiserpune.ac.in>
2015-08-04 10:07:37 +0200

    seed state back propagated to first rechit and then instead of sim info, the propagated state is supplied as initial track state.

--- Validation/RecoTrack/test/trackingCompare.py
+++ Validation/RecoTrack/test/trackingCompare.py
@@ -11,8 +11,8 @@ import Validation.RecoTrack.plotting.trackingPlots as trackingPlots
 
 # Example of file - label pairs
 filesLabels = [
-    ("DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO_1.root", "Option 1"),
-    ("DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO_2.root", "Option 2"),
+    ("DQM_CMSSW_7_6_X_2015-07-31-2300_AfterSeedStateBackPropagation.root", "Changed_FastSim_7_6_X_AfterSeedStateBackPropagation"),
+    ("DQM_V0001_R000000001__RelValTTbar_13__CMSSW_7_5_0_pre3-MCRUN2_74_V7_FastSim-v1__DQMIO.root", "StandardFastSim_7_5_0_pre3"),
 ]
 
 outputDir = "plots"
