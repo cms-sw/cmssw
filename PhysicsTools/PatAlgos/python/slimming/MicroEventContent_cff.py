57fd6f1a4002cd81f2a1e3ffa00c390efce4d6bd
Lindsey Gray <lindsey.gray@gmail.com>
2015-08-05 19:19:53 +0200

    pileup summary info slimmer for miniaod

--- PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/MicroEventContent_cff.py
@@ -57,7 +57,7 @@ MicroEventContentMC.outputCommands += [
         'keep patPackedGenParticles_packedGenParticles_*_*',
         'keep recoGenParticles_prunedGenParticles_*_*',
         'keep LHEEventProduct_*_*_*',
-        'keep PileupSummaryInfos_*_*_*',
+        'keep PileupSummaryInfos_slimmedAddPileupInfo_*_*',
         'keep GenFilterInfo_*_*_*',
         'keep GenLumiInfoProduct_*_*_*',
         'keep GenEventInfoProduct_generator_*_*',
