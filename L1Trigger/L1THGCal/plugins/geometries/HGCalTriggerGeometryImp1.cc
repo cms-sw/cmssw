1f5d095c2c4efd6d70ae820dc2327b277805750c
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2015-08-05 11:59:18 +0200

    Add dummy modules in HGCalTriggerGeometryImp1

--- L1Trigger/L1THGCal/plugins/geometries/HGCalTriggerGeometryImp1.cc
+++ L1Trigger/L1THGCal/plugins/geometries/HGCalTriggerGeometryImp1.cc
@@ -31,22 +31,31 @@ HGCalTriggerGeometryImp1::HGCalTriggerGeometryImp1(const edm::ParameterSet& conf
 void HGCalTriggerGeometryImp1::initialize(const es_info& esInfo)
 /*****************************************************************/
 {
-    // FIXME: Only for HGCEE for the moment
+    // FIXME: !!!Only for HGCEE for the moment!!!
+    edm::LogWarning("HGCalTriggerGeometry") << "WARNING: This HGCal trigger geometry is incomplete.\n"\
+                                            << "WARNING: Only the EE part is covered.\n"\
+                                            << "WARNING: Dummy hardcoded modules are used.\n"\
+                                            << "WARNING: There is no neighbor information.\n";
+
     std::ifstream l1tMappingStream(l1tMapping_.fullPath());
     if(!l1tMappingStream.is_open()) edm::LogError("HGCalTriggerGeometry") << "Cannot open L1TMapping file\n";
-
+    //
     // read mapping file
     short layer       = 0;
     short cell        = 0;
     short triggercell = 0;
     short subsector   = 0;
+    short module      = 0;
     for(; l1tMappingStream>>layer>>cell>>triggercell>>subsector; )
     {
-        if(layer>=30 || layer<0) 
+        layer++; //FIXME: currently the first layer in the mapping file has index=0, should be changed to 1
+        if(layer>30 || layer<=0) 
         {
             edm::LogWarning("HGCalTriggerGeometry") << "Bad layer index in L1TMapping\n"; 
             continue; 
         }
+        // FIXME: dummy module ID (mapping not yet available)
+        module = triggercell/64;
         // Loop on all sectors
         // FIXME:  Number of sectors in each zside should not be hardcoded
         for(unsigned z=0; z<=1; z++)
@@ -55,17 +64,25 @@ void HGCalTriggerGeometryImp1::initialize(const es_info& esInfo)
             for(unsigned sector=1; sector<=18; sector++)
             {
                 HGCEEDetId detid(HGCEE, zside, layer, sector, subsector, cell); 
+                GlobalPoint point = esInfo.geom_ee->getPosition(detid);
+                if(point.x()==0 && point.y()==0) std::cout<<" Cell "<<detid()<<" layer="<<layer<<" sector="<<sector<<" subsec="<<subsector<<" cell="<<cell<<"\n";
                 // FIXME: Use temporarily HGCEEDetId to compute trigger cell id
                 HGCEEDetId triggerDetid(HGCEE, zside, layer, sector, 1, triggercell); // Dummy subsector
                 const auto& ret = cells_to_trigger_cells_.insert( std::make_pair(detid(), triggerDetid()) );
                 if(!ret.second) edm::LogWarning("HGCalTriggerGeometry") << "Duplicate cell in L1TMapping\n";
+                // FIXME: Use temporarily HGCEEDetId to compute module id
+                HGCEEDetId moduleDetid(HGCEE, zside, layer, sector, 1, module); // Dummy subsector
+                // Only the first occurence of the trigger cell is filled (one trigger cell is not
+                // shared between different modules)
+                trigger_cells_to_modules_.insert( std::make_pair(triggerDetid(), moduleDetid()) );
             }
         }
     }
     l1tMappingStream.close();
-
+    //
     // Build trigger cells and fill map
     typedef HGCalTriggerGeometry::TriggerCell::list_type list_cells;
+    // make list of cells in trigger cells
     std::map<unsigned, list_cells> trigger_cells_to_cells;
     for(const auto& cell_triggercell : cells_to_trigger_cells_)
     {
@@ -86,9 +103,37 @@ void HGCalTriggerGeometryImp1::initialize(const es_info& esInfo)
             triggercellVector += esInfo.geom_ee->getPosition(cellId).basicVector();
         }
         GlobalPoint triggercellPoint( triggercellVector/cellIds.size() );
-        // FIXME: dummy module ID, empty neighbours
-        std::unique_ptr<const HGCalTriggerGeometry::TriggerCell> triggercellPtr(new HGCalTriggerGeometry::TriggerCell(triggercellId, 1, triggercellPoint, list_cells(), cellIds));
+        unsigned moduleId = trigger_cells_to_modules_.at(triggercellId);
+        // FIXME: empty neighbours
+        std::unique_ptr<const HGCalTriggerGeometry::TriggerCell> triggercellPtr(new HGCalTriggerGeometry::TriggerCell(triggercellId, moduleId, triggercellPoint, list_cells(), cellIds));
         trigger_cells_.insert( std::make_pair(triggercellId, std::move(triggercellPtr)) );
+    }
+    //
+    // Build modules and fill map
+    typedef HGCalTriggerGeometry::Module::list_type list_triggercells;
+    // make list of trigger cells in modules
+    std::map<unsigned, list_triggercells> modules_to_trigger_cells;
+    for(const auto& triggercell_module : trigger_cells_to_modules_)
+    {
+        unsigned triggercell = triggercell_module.first;
+        unsigned module      = triggercell_module.second;
+        modules_to_trigger_cells.insert( std::make_pair(module, list_triggercells()) );
+        modules_to_trigger_cells.at(module).insert(triggercell);
+    }
+    for(const auto& module_triggercell : modules_to_trigger_cells)
+    {
+        unsigned moduleId = module_triggercell.first;
+        list_triggercells triggercellIds = module_triggercell.second;
+        // Position: for the moment, barycenter of the module, from trigger cell positions
+        Basic3DVector<float> moduleVector(0.,0.,0.);
+        for(const auto& triggercell : triggercellIds)
+        {
+            moduleVector += trigger_cells_.at(triggercell)->position().basicVector();
+        }
+        GlobalPoint modulePoint( moduleVector/triggercellIds.size() );
+        // FIXME: empty neighbours
+        std::unique_ptr<const HGCalTriggerGeometry::Module> modulePtr(new HGCalTriggerGeometry::Module(moduleId, modulePoint, list_triggercells(), triggercellIds));
+        modules_.insert( std::make_pair(moduleId, std::move(modulePtr)) );
     }
 }
 
