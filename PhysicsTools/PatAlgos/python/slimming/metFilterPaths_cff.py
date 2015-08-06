46cfc91bc2a076c2f7d9fedaa5b2076945b7c675
schoefbeck <robert.schoefbeck@cern.ch>
2015-08-06 12:26:22 +0200

    added boundary filter to allMetFilters

--- PhysicsTools/PatAlgos/python/slimming/metFilterPaths_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/metFilterPaths_cff.py
@@ -27,7 +27,7 @@ Flag_trkPOG_logErrorTooManyClusters = cms.Path(~logErrorTooManyClusters)
 Flag_METFilters = cms.Path(metFilters)
 
 #add your new path here!!
-allMetFilterPaths=['HBHENoiseFilter','CSCTightHaloFilter','hcalLaserEventFilter','EcalDeadCellTriggerPrimitiveFilter','goodVertices','eeBadScFilter',
+allMetFilterPaths=['HBHENoiseFilter','CSCTightHaloFilter','hcalLaserEventFilter','EcalDeadCellTriggerPrimitiveFilter','EcalDeadCellBoundaryEnergyFilter','goodVertices','eeBadScFilter',
                    'ecalLaserCorrFilter','trkPOGFilters','trkPOG_manystripclus53X','trkPOG_toomanystripclus53X','trkPOG_logErrorTooManyClusters','METFilters']
 
        
