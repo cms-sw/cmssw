d40b7e1a4d07d840c7e42728d054ba6a7ee80353
schoefbeck <robert.schoefbeck@cern.ch>
2015-08-06 12:34:36 +0200

    resolved

--- RecoMET/METFilters/python/metFilters_cff.py
+++ RecoMET/METFilters/python/metFilters_cff.py
@@ -30,6 +30,12 @@ goodVertices = cms.EDFilter(
 ## The tracking failure filter _______________________________________________||
 from RecoMET.METFilters.trackingFailureFilter_cfi import *
 
+##noscraping (outdated)_______________________________________________________||
+from RecoMET.METFilters.scrapingFilter_cfi import *
+
+## The primary vertex filter__ _______________________________________________||
+from RecoMET.METFilters.primaryVertexFilter_cfi import *
+
 ## The tracking POG filters __________________________________________________||
 from RecoMET.METFilters.trackingPOGFilters_cff import *
 ## NOTE: to make tagging mode of the tracking POG filters (three of them), please do:
