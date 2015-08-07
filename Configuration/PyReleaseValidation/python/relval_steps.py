4b862d28603800dad1ea598b2a4fafa128e320ce
Hengne <Hengne.Li@CERN.CH>
2015-08-07 12:40:22 +0200

    remove duplicated baseDataSetRelease parameter for fastSim PU mixing input

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -344,8 +344,7 @@ baseDataSetRelease=[
     'CMSSW_7_5_0-PU25ns_75X_mcRun2_asymptotic_v1-v1',       # 5 - fullSim PU 25ns premix
     'CMSSW_7_5_0-PU50ns_75X_mcRun2_startup_v1-v1',          # 6 - fullSim PU 50ns premix
     'CMSSW_7_5_0-75X_mcRun2_asymptotic_v1_FastSim-v1',      # 7 - fastSim PU for mixing
-    'CMSSW_7_5_0-75X_mcRun2_asymptotic_v1_FastSim-v1',      # 8 - fastSim mb // FIXME [Hengne]: this is actually the same as 7, to be removed
-    'CMSSW_7_5_0_pre4-PU25ns_MCRUN2_75_V1_FastSim-v2'       # 9 - fastSim pre-premix 
+    'CMSSW_7_5_0_pre4-PU25ns_MCRUN2_75_V1_FastSim-v2'       # 8 - fastSim PU for premixing 
     ]
 
 # note: INPUT commands to be added once GEN-SIM w/ 13TeV+PostLS1Geo will be available 
@@ -667,7 +666,7 @@ steps['MinBiasFS_13_ForMixing']=merge([{'cfg':'MinBias_13TeV_pythia8_TuneCUETP8M
 
 ### FastSim: template to produce signal and overlay with minbias events
 PUFS25={'--pileup':'AVE_35_BX_25ns',
-        '--pileup_input':'das:/RelValMinBiasFS_13_ForMixing/%s/GEN-SIM-RECO'%(baseDataSetRelease[8],)}
+        '--pileup_input':'das:/RelValMinBiasFS_13_ForMixing/%s/GEN-SIM-RECO'%(baseDataSetRelease[7],)}
 FS_UP15_PU25_OVERLAY = merge([PUFS25,Kby(100,500),steps['TTbarFS_13']] )
 
 ### FastSim: produce sample of premixed minbias events
@@ -686,7 +685,7 @@ steps["FS_PREMIXUP15_PU25"] = merge([
 FS_PREMIXUP15_PU25_OVERLAY = merge([
         {"-s" : "GEN,SIM,RECOBEFMIX,DIGIPREMIX_S2:pdigi_valid,DATAMIX,L1,L1Reco,RECO,HLT:@relval25ns,VALIDATION",
          "--datamix" : "PreMix",
-         "--pileup_input" : "dbs:/RelValFS_PREMIXUP15_PU25/%s/GEN-SIM-DIGI-RAW"%(baseDataSetRelease[9],),
+         "--pileup_input" : "dbs:/RelValFS_PREMIXUP15_PU25/%s/GEN-SIM-DIGI-RAW"%(baseDataSetRelease[8],),
          "--customise":"SLHCUpgradeSimulations/Configuration/postLS1CustomsPreMixing.customisePostLS1"
          },
         Kby(100,500),step1FastUpg2015Defaults])
