3cee798a549619e38207d824b046149841ba2e44
Matteo <matteosan1@gmail.com>
2015-08-08 12:16:34 +0200

    final update

--- RecoEgamma/EgammaTools/python/egammaObjectModificationsInMiniAOD_cff.py
+++ RecoEgamma/EgammaTools/python/egammaObjectModificationsInMiniAOD_cff.py
@@ -68,7 +68,7 @@ egamma_modifications.append(
     cms.PSet( modifierName    = cms.string('EGExtraInfoModifierFromDB'),  
               autoDetectBunchSpacing = cms.bool(True),
               bunchSpacingTag = cms.InputTag("addPileupInfo:bunchSpacing"),
-              #manualBunchSpacing = cms.int32(25),              
+              manualBunchSpacing = cms.int32(50),              
               rhoCollection = cms.InputTag("fixedGridRhoFastjetAll"),
               vertexCollection = cms.InputTag("offlinePrimaryVertices"),
               electron_config = cms.PSet( sigmaIetaIphi = cms.InputTag('electronRegressionValueMapProducer:eleFull5x5SigmaIEtaIPhi'),
@@ -95,15 +95,16 @@ egamma_modifications.append(
                                           iEta          = cms.InputTag("electronRegressionValueMapProducer:iEta"),
                                           cryPhi        = cms.InputTag("electronRegressionValueMapProducer:cryPhi"),
                                           cryEta        = cms.InputTag("electronRegressionValueMapProducer:cryEta"),
+                                          intValueMaps = cms.vstring("iPhi", "iEta"),                                          
                                           
                                           # EB, EE
                                           regressionKey_25ns  = cms.vstring('gedelectron_EBCorrection_25ns', 'gedelectron_EECorrection_25ns'),
                                           uncertaintyKey_25ns = cms.vstring('gedelectron_EBUncertainty_25ns', 'gedelectron_EEUncertainty_25ns'),
-                                          combinationKey_25ns   = cms.string("gedelectron_p4combination_25ns"),
+                                          combinationKey_25ns   = cms.string('gedelectron_p4combination_25ns'),
                                           
                                           regressionKey_50ns  = cms.vstring('gedelectron_EBCorrection_50ns', 'gedelectron_EECorrection_50ns'),
                                           uncertaintyKey_50ns = cms.vstring('gedelectron_EBUncertainty_50ns', 'gedelectron_EEUncertainty_50ns'),
-                                          combinationKey_50ns   = cms.string("gedelectron_p4combination_50ns"),
+                                          combinationKey_50ns   = cms.string('gedelectron_p4combination_50ns'),
                                           ),
 
               photon_config   = cms.PSet( sigmaIetaIphi = cms.InputTag('photonRegressionValueMapProducer:phoFull5x5SigmaIEtaIPhi'),
