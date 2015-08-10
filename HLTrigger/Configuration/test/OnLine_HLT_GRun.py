ce4c7715897ecd2a421f9f4fb07fffc5366db273
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-08-10 13:58:22 +0200

    AlCa ecal paths and fixes

--- HLTrigger/Configuration/test/OnLine_HLT_GRun.py
+++ HLTrigger/Configuration/test/OnLine_HLT_GRun.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/GRun/V117 (CMSSW_7_4_8_patch1)
+# /dev/CMSSW_7_4_0/GRun/V118 (CMSSW_7_4_8_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTGRun" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/GRun/V117')
+  tableName = cms.string('/dev/CMSSW_7_4_0/GRun/V118')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
@@ -489,7 +489,7 @@ process.streams = cms.PSet(
     'HcalHPDNoise',
     'HcalNZS',
     'HighMultiplicity',
-    'HighMultplicity85',
+    'HighMultiplicity85',
     'MonteCarlo',
     'NoBPTX',
     'SingleElectron',
@@ -690,7 +690,7 @@ process.datasets = cms.PSet(
     'HLT_PixelTracks_Multiplicity135ForEndOfFill_v1',
     'HLT_PixelTracks_Multiplicity160ForEndOfFill_v1',
     'HLT_PixelTracks_Multiplicity60ForEndOfFill_v1' ),
-  HighMultplicity85 = cms.vstring( 'HLT_PixelTracks_Multiplicity85ForEndOfFill_v1' ),
+  HighMultiplicity85 = cms.vstring( 'HLT_PixelTracks_Multiplicity85ForEndOfFill_v1' ),
   JetHT = cms.vstring( 'HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV0p45_v2',
     'HLT_AK8PFHT700_TrimR0p1PT0p03Mass50_v2',
     'HLT_AK8PFJet360_TrimMass30_v2',
