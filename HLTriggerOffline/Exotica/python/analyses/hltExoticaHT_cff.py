3ed11e97eb5565b2294320dd63cf31d07e91089a
bing <bingxuan.liu@cern.ch>
2015-08-11 09:55:26 -0400

    Backport from 76X 10681, added new HT parking paths and changed the binning

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaHT_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaHT_cff.py
@@ -13,6 +13,11 @@ HTPSet = cms.PSet(
         "HLT_PFHT650_v",
         "HLT_HT900_v",        # Run2
         "HLT_HT300_v",        # Run2
+        "HLT_HT450to470_v",        # HT Parking
+        "HLT_HT470to500_v",        # HT Parking
+        "HLT_HT500to550_v",        # HT Parking
+        "HLT_HT550to650_v",        # HT Parking
+        "HLT_HT650_v",        # HT Parking
         "HLT_ECALHT800_v",     # Run2 7e33
         "HLT_Photon90_CaloIdL_PFHT600_v" # 50ns backup menu
         #"HLT_HT750_v"        # Run1 (frozenHLT)
@@ -24,7 +29,7 @@ HTPSet = cms.PSet(
     MET_recCut      = cms.string("sumEt > 75"),
     minCandidates = cms.uint32(1),
     # -- Analysis specific binnings
-    parametersTurnOn = cms.vdouble(0, 50, 100, 150, 200, 250, 300, 350, 400,
-                                   500, 600, 700, 800, 900, 1000
+    parametersTurnOn = cms.vdouble(0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 470, 
+                                   500, 550, 600, 650, 700, 800, 900, 1000
                                    )
 )
