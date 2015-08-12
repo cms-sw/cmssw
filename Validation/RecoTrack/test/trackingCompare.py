c4986da8d23dc3936dcd2978935edba90af68af6
ANSH0712 <anshul.kapoor@students.iiserpune.ac.in>
2015-08-12 12:03:43 +0530

    Update trackingCompare.py

--- Validation/RecoTrack/test/trackingCompare.py
+++ Validation/RecoTrack/test/trackingCompare.py
@@ -11,8 +11,8 @@ import Validation.RecoTrack.plotting.trackingPlots as trackingPlots
 
 # Example of file - label pairs
 filesLabels = [
-    ("DQM_CMSSW_7_6_X_2015-07-31-2300_AfterSeedStateBackPropagation.root", "Changed_FastSim_7_6_X_AfterSeedStateBackPropagation"),
-    ("DQM_V0001_R000000001__RelValTTbar_13__CMSSW_7_5_0_pre3-MCRUN2_74_V7_FastSim-v1__DQMIO.root", "StandardFastSim_7_5_0_pre3"),
+    ("DQM_1.root", "DQM 1 details"),
+    ("DQM_2.root", "DQM 2 details"),
 ]
 
 outputDir = "plots"
