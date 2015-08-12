f13a7230d5402baa3bea8d7b55388b8ecf1a571a
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-08-12 17:17:56 +0200

    Another JIRA ticket

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/PIon/V122 (CMSSW_7_4_8_patch1)
+# /dev/CMSSW_7_4_0/PIon/V123 (CMSSW_7_4_8_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V122')
+  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V123')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
