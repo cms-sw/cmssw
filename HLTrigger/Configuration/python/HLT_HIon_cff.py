bd4dc24cfa7a19380e648030335ed76c39bcfead
geoff-smith <geoff127@gmail.com>
2015-08-13 17:57:38 +0200

    adding HLT_Ele23_WPLoose_Gsf_v1 to GRun

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/HIon/V124 (CMSSW_7_4_8_patch1)
+# /dev/CMSSW_7_4_0/HIon/V125 (CMSSW_7_4_8_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V124')
+  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V125')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
