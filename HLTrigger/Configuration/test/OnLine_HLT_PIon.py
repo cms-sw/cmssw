c5d4b900834f2e23d510f4a0ad0c96efc0bbf284
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-08-14 11:34:46 +0200

    Bookkeeping fixes

--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/PIon/V125 (CMSSW_7_4_8_patch1)
+# /dev/CMSSW_7_4_0/PIon/V126 (CMSSW_7_4_8_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V125')
+  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V126')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
