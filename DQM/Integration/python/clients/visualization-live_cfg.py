c1d02febb996c3fd201999aa286416df167ce540
Dmitrijus Bugelskis <dmitrijus.bugelskis@cern.ch>
2015-08-14 19:26:17 +0200

    Fix online DQM configuration for visualization.
    (cherry picked from commit 55d618132b0b6dd3ce373592c4530052d13e5a5d)

--- DQM/Integration/python/clients/visualization-live_cfg.py
+++ DQM/Integration/python/clients/visualization-live_cfg.py
@@ -5,7 +5,7 @@ from Configuration.DataProcessing.GetScenario import getScenario
 """
 Example configuration for online reconstruction meant for visualization clients.
 """
-from DQM.Integration.test.inputsource_cfi import options,runType,source
+from DQM.Integration.config.inputsource_cfi import options,runType,source
 
 # this is needed to map the names of the run-types chosen by DQM to the scenarios, ideally we could converge to the same names
 #scenarios = {'pp_run': 'ppRun2','cosmic_run':'cosmicsRun2','hi_run':'HeavyIons'}
@@ -31,7 +31,7 @@ except Exception, ex:
 
 kwds = {}
 # example of how to add a filer IN FRONT of all the paths, eg for HLT selection
-#kwds['preFilter'] = 'DQM/Integration/python/test/visualizationPreFilter.hltfilter'
+#kwds['preFilter'] = 'DQM/Integration/python/config/visualizationPreFilter.hltfilter'
 
 process = scenario.visualizationProcessing(globalTag='DUMMY', writeTiers=['FEVT'], **kwds)
 
@@ -52,7 +52,7 @@ try:
 except:
     pass
 
-process.load("DQM.Integration.test.FrontierCondition_GT_autoExpress_cfi")
+process.load("DQM.Integration.config.FrontierCondition_GT_autoExpress_cfi")
 
 process.options = cms.untracked.PSet(
         Rethrow = cms.untracked.vstring('ProductNotFound'),
