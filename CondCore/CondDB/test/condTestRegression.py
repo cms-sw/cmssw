9d092ef1047a91bad4367354a3d3971aac2eab6e
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2015-08-14 09:49:06 +0200

    first version of regression test as addOnTest

---
+++ CondCore/CondDB/test/condTestRegression.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+import os
+import time
+import subprocess
+
+
+def check_output(*popenargs, **kwargs):
+    '''Mimics subprocess.check_output() in Python 2.6
+    '''
+
+    process = subprocess.Popen(*popenargs, **kwargs)
+    stdout, stderr = process.communicate()
+    returnCode = process.returncode
+
+    if returnCode:
+        print 'ERROR from process (ret=%s): ' %(str(returnCode),)
+	print '      stderr: %s' % (str(stderr),)
+	print '      stdout: %s' % (str(stdout),)
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(returnCode, cmd)
+
+    return stdout
+
+
+# nice one from:
+# https://www.daniweb.com/software-development/python/code/216610/timing-a-function-python
+def print_timing(func):
+    def wrapper(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        print '\n%s(%s) took %0.3f ms\n' % (func.func_name, ','.join([str(x) for x in arg[1:]]), (t2-t1)*1000.0)
+        return res
+    return wrapper
+
+
+class CondRegressionTester(object):
+
+      @print_timing
+      def __init__(self):
+
+          self.topDir = '/tmp/cmsCondRegTst-2015-08-13-16-14' # +time.strftime('%Y-%m-%d-%H-%M')
+          if not os.path.exists(self.topDir): os.makedirs(self.topDir)
+          
+          self.dbDir = os.path.join( self.topDir, 'dbDir' )
+          if not os.path.exists(self.dbDir): os.makedirs(self.dbDir)
+          
+          self.logDir = os.path.join( self.topDir, 'logs' )
+          if not os.path.exists(self.logDir): os.makedirs(self.logDir)
+
+          # add the IB/release itself:
+          self.rel = os.environ['CMSSW_VERSION']
+          self.arch = os.environ['SCRAM_ARCH']
+	  self.dbName = 'self-%s-%s.db' % (self.rel, self.arch)
+
+          self.dbNameList = [ self.dbName ]
+
+	  self.status = {}
+          
+          return
+
+      def summary(self, verbose=False):
+          if verbose: 
+              import json
+              print json.dumps( self.status, sort_keys=True, indent=4 )
+
+          overall = True
+          for k,v in self.status.items():
+              if not v : overall = False
+
+          return overall
+
+
+      @print_timing
+      def setup(self, rel, arch):
+
+      	  # check if we need to do anything at all:
+      	  if os.path.exists( os.path.join( self.topDir, rel, 'test', arch, 'testReadWritePayloads' ) ): 
+             print "area for %s/%s already setup: found %s " % (rel, arch, os.path.join( self.topDir, rel, 'test', arch, 'testReadWritePayloads' ))
+          #    return
+
+          # prepare the devel area, if it does not yet exist:
+          if not os.path.exists( os.path.join( self.topDir, rel, 'src') ):
+             print "going to create devel area for %s/%s " % (rel, arch)
+             cmd = 'cd %s; export SCRAM_ARCH=%s; scram p CMSSW %s' % (self.topDir, arch, rel)
+             res = check_output(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+          
+          # check out the package and build the tests
+          print "going to build the test for %s/%s " % (rel, arch)
+          cmd = 'cd %s/%s/src; eval `scram run -sh`; ' % (self.topDir, rel, )
+          cmd += 'git cms-addpkg CondCore/CondDB 2>&1; cd CondCore/CondDB/test; '
+	  if not os.path.exists( os.path.join( self.topDir, rel, 'src', 'CondCore/CondDB/test/testReadWritePayloads.cpp') ):
+             print "copying over test source ... "
+	     cmd += 'cp /afs/cern.ch/user/a/andreasp/public/BuildFile.xml .;'
+             cmd += 'cp /afs/cern.ch/user/a/andreasp/public/testReadWritePayloads.cpp .;'
+          cmd += 'scram b -j 10 2>&1 ;'
+          res = check_output(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+          with open(os.path.join(self.logDir, rel+arch+'-build.log'), 'w') as logFile:
+             logFile.write( ''.join(res) )	
+
+      @print_timing
+      def run(self, rel, arch, readOrWrite, dbName):
+
+          cmd = 'cd %s/%s/src; eval `scram run -sh 2>/dev/null` ; ' % (self.topDir, rel, )
+          cmd += '../test/%s/testReadWritePayloads %s sqlite_file:///%s/%s ' % (arch, readOrWrite, self.dbDir, dbName)
+          
+          res = check_output(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+          with open(os.path.join(self.logDir, rel+arch+'-'+readOrWrite+'.log'), 'w') as logFile:
+             logFile.write( ''.join(res) )
+
+          if readOrWrite == 'write':
+              self.dbNameList.append( dbName )
+
+      @print_timing
+      def runSelf(self, readOrWrite, dbNameIn=None):
+
+          dbName = self.dbName # set the default
+          if dbNameIn : dbName = dbNameIn
+
+          # we run in the local environment, but need to make sure that we start "top-level" of the devel area
+          # and we assume that the test was already built 
+          cmd = 'cd %s/src; eval `scram run -sh 2>/dev/null` ; ' % (os.environ['CMSSW_BASE'], )
+          cmd += '../test/%s/testReadWritePayloads %s sqlite_file:///%s/%s ' % (self.arch, readOrWrite, self.dbDir, dbName)
+          
+          res = check_output(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+          with open(os.path.join(self.logDir, self.rel+self.arch+'-'+readOrWrite+'.log'), 'w') as logFile:
+             logFile.write( ''.join(res) )
+
+
+      @print_timing
+      def runAll(self):
+
+          map = { 'CMSSW_7_6_0_pre2' : [ 'slc6_amd64_gcc493', 'ref76pre2-s6493.db'],
+                  'CMSSW_7_5_1'      : [ 'slc6_amd64_gcc491', 'ref751-s6491.db'],
+                  'CMSSW_7_4_9'      : [ 'slc6_amd64_gcc491', 'ref749-s6491.db'],
+          }
+
+          print '='*80
+          print "going to set up areas ..."
+          for rel, info in map.items():
+             arch, dbName = info
+             self.setup(rel, arch)
+          
+          print '='*80
+          print "going to write DBs ..."
+          self.runSelf('write')
+          for rel, info in map.items():
+             arch, dbName = info
+             self.run(rel, arch, 'write', dbName)
+          
+
+          print '='*80
+          print "going to read back DBs ..."
+          for rel, info in map.items():
+             arch, dbName = info
+             for item in self.dbNameList: # for any given rel/arch we check all written DBs
+                 try:
+                    self.run(rel, arch, 'read', item)
+                    self.status['%s-%s-%s' % (rel,arch,item)] = True
+                 except:
+                    self.status['%s-%s-%s' % (rel,arch,item)] = False
+          for item in self.dbNameList: # for any given rel/arch we check all written DBs
+             try:
+                self.runSelf('read', item)
+                self.status['%s-%s-%s' % (self.rel,self.arch,item)] = True
+             except:
+                self.status['%s-%s-%s' % (self.rel,self.arch,item)] = False
+
+          print '='*80
+
+crt = CondRegressionTester()
+crt.runAll()
+print "\n==> overall status: ", crt.summary(verbose=True)
+
