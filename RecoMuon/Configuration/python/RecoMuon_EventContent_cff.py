2aaacb9608b2f75115841f7d92a95629300523ac
jeongeun <jeongeun.lee@cern.ch>
2015-08-15 20:27:02 +0900

    Update RecoMuon_EventContent_cff.py

--- RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
+++ RecoMuon/Configuration/python/RecoMuon_EventContent_cff.py
@@ -23,6 +23,7 @@ from RecoMuon.MuonIsolationProducers.muIsolation_EventContent_cff import *
 # AOD content
 RecoMuonAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep *_muons_*_*',
+                                           #'keep *_*_muons_*',
                                            'keep *_particleFlow_muons_*',
                                            'drop *_muons_muons1stStep2muonsMap_*',
                                            'drop recoIsoDepositedmValueMap_muons_*_*', #not really used
@@ -59,8 +60,10 @@ RecoMuonAOD = cms.PSet(
 )
 # RECO content
 RecoMuonRECO = cms.PSet(
-    outputCommands = cms.untracked.vstring('keep *_ancientMuonSeed_*_*',
+    outputCommands = cms.untracked.vstring('keep *_MuonSeed_*_*',
+                                           'keep *_ancientMuonSeed_*_*',
                                            'keep *_displacedMuonSeeds_*_*',
+                                           'keep *_mergedStandAloneMuonSeeds_*_*',
                                            'keep TrackingRecHitsOwned_globalMuons_*_*', 
                                            'keep TrackingRecHitsOwned_tevMuons_*_*',
                                            'keep recoCaloMuons_calomuons_*_*',
@@ -78,7 +81,8 @@ RecoMuonRECO = cms.PSet(
                                            'keep TrackingRecHitsOwned_standAloneSETMuons_*_*',
                                            'keep recoTracks_globalSETMuons_*_*',
                                            'keep recoTrackExtras_globalSETMuons_*_*',
-                                           'keep TrackingRecHitsOwned_globalSETMuons_*_*')
+                                           'keep TrackingRecHitsOwned_globalSETMuons_*_*',
+                                           'keep recoMuons_muonsWithSET_*_*')
 )
 # Full Event content 
 RecoMuonFEVT = cms.PSet(
