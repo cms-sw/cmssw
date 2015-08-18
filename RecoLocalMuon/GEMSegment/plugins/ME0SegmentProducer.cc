71a67a86ae70cb48748518256c34f0b4e821ea5a
Piet <piet.verwilligen@cern.ch>
2015-08-18 14:23:08 +0200

    migrate to consumes

--- RecoLocalMuon/GEMSegment/plugins/ME0SegmentProducer.cc
+++ RecoLocalMuon/GEMSegment/plugins/ME0SegmentProducer.cc
@@ -15,10 +15,11 @@
 #include <DataFormats/GEMRecHit/interface/ME0SegmentCollection.h>
 #include <DataFormats/GEMRecHit/interface/ME0Segment.h>
 
-ME0SegmentProducer::ME0SegmentProducer(const edm::ParameterSet& pas) : iev(0) {
+ME0SegmentProducer::ME0SegmentProducer(const edm::ParameterSet& ps) : iev(0) {
 	
-    inputObjectsTag = pas.getParameter<edm::InputTag>("me0RecHitLabel");
-    segmentBuilder_ = new ME0SegmentBuilder(pas); // pass on the Parameter Set
+    // inputObjectsTag = pas.getParameter<edm::InputTag>("me0RecHitLabel");
+    theME0RecHitToken = consumes<ME0RecHitCollection>(ps.getParameter<edm::InputTag>("me0RecHitLabel"));
+    segmentBuilder_ = std::unique_ptr<ME0SegmentBuilder>(new ME0SegmentBuilder(ps)); // pass on the Parameter Set
 
   	// register what this produces
     produces<ME0SegmentCollection>();
@@ -26,8 +27,6 @@ ME0SegmentProducer::ME0SegmentProducer(const edm::ParameterSet& pas) : iev(0) {
 
 ME0SegmentProducer::~ME0SegmentProducer() {
 
-    LogDebug("ME0SegmentProducer") << "deleting ME0SegmentBuilder after " << iev << " events w/csc data.";
-    delete segmentBuilder_;
 }
 
 void ME0SegmentProducer::produce(edm::Event& ev, const edm::EventSetup& setup) {
@@ -43,7 +42,8 @@ void ME0SegmentProducer::produce(edm::Event& ev, const edm::EventSetup& setup) {
 
     // get the collection of ME0RecHit
     edm::Handle<ME0RecHitCollection> me0RecHits;
-    ev.getByLabel(inputObjectsTag, me0RecHits);  
+    // ev.getByLabel(inputObjectsTag, me0RecHits);  
+    ev.getByToken(theME0RecHitToken,me0RecHits);
 
     // create empty collection of Segments
     std::auto_ptr<ME0SegmentCollection> oc( new ME0SegmentCollection );
