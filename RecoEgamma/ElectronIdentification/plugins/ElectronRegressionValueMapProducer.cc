dc5c85f19357744e2e411c2bef9feda2f34b605c
Lindsey Gray <lindsey.gray@gmail.com>
2015-08-19 13:46:06 +0200

    make full5x5 or fraction-ified shower shapes configurable in regressions

--- RecoEgamma/ElectronIdentification/plugins/ElectronRegressionValueMapProducer.cc
+++ RecoEgamma/ElectronIdentification/plugins/ElectronRegressionValueMapProducer.cc
@@ -22,13 +22,13 @@
 #include <vector>
 
 namespace {
-  constexpr char eleFull5x5SigmaIEtaIPhi_[] = "eleFull5x5SigmaIEtaIPhi";
-  constexpr char eleFull5x5eMax_[] = "eMax";
-  constexpr char eleFull5x5e2nd_[] = "e2nd";
-  constexpr char eleFull5x5eTop_[] = "eTop";
-  constexpr char eleFull5x5eBottom_[] = "eBottom";
-  constexpr char eleFull5x5eLeft_[] = "eLeft";
-  constexpr char eleFull5x5eRight_[] = "eRight";
+  constexpr char sigmaIEtaIPhi_[] = "sigmaIEtaIPhi";
+  constexpr char eMax_[] = "eMax";
+  constexpr char e2nd_[] = "e2nd";
+  constexpr char eTop_[] = "eTop";
+  constexpr char eBottom_[] = "eBottom";
+  constexpr char eLeft_[] = "eLeft";
+  constexpr char eRight_[] = "eRight";
   constexpr char clusterMaxDR_[] = "clusterMaxDR";
   constexpr char clusterMaxDRDPhi_[] = "clusterMaxDRDPhi";
   constexpr char clusterMaxDRDEta_[] = "clusterMaxDRDEta";
@@ -71,7 +71,7 @@ class ElectronRegressionValueMapProducer : public edm::stream::EDProducer<> {
 		     const std::vector<int> & values,
 		     const std::string    & label) const ;
 
-  noZS::EcalClusterLazyTools *lazyToolnoZS;
+  std::unique_ptr<EcalClusterLazyToolsBase> lazyTools;
 
   // for AOD case
   edm::EDGetTokenT<EcalRecHitCollection> ebReducedRecHitCollection_;
@@ -84,9 +84,12 @@ class ElectronRegressionValueMapProducer : public edm::stream::EDProducer<> {
   edm::EDGetTokenT<EcalRecHitCollection> eeReducedRecHitCollectionMiniAOD_;
   edm::EDGetTokenT<EcalRecHitCollection> esReducedRecHitCollectionMiniAOD_;
   edm::EDGetToken srcMiniAOD_;
+
+  const bool use_full5x5_;
 };
 
-ElectronRegressionValueMapProducer::ElectronRegressionValueMapProducer(const edm::ParameterSet& iConfig) {
+ElectronRegressionValueMapProducer::ElectronRegressionValueMapProducer(const edm::ParameterSet& iConfig) :
+  use_full5x5_(iConfig.getParameter<bool>("useFull5x5")) {
 
   //
   // Declare consummables, handle both AOD and miniAOD case
@@ -109,13 +112,13 @@ ElectronRegressionValueMapProducer::ElectronRegressionValueMapProducer(const edm
   src_        = mayConsume<edm::View<reco::GsfElectron> >(iConfig.getParameter<edm::InputTag>("src"));
   srcMiniAOD_ = mayConsume<edm::View<reco::GsfElectron> >(iConfig.getParameter<edm::InputTag>("srcMiniAOD"));
 
-  produces<edm::ValueMap<float> >(eleFull5x5SigmaIEtaIPhi_); 
-  produces<edm::ValueMap<float> >(eleFull5x5eMax_);
-  produces<edm::ValueMap<float> >(eleFull5x5e2nd_);
-  produces<edm::ValueMap<float> >(eleFull5x5eTop_);
-  produces<edm::ValueMap<float> >(eleFull5x5eBottom_);
-  produces<edm::ValueMap<float> >(eleFull5x5eLeft_);
-  produces<edm::ValueMap<float> >(eleFull5x5eRight_);
+  produces<edm::ValueMap<float> >(sigmaIEtaIPhi_); 
+  produces<edm::ValueMap<float> >(eMax_);
+  produces<edm::ValueMap<float> >(e2nd_);
+  produces<edm::ValueMap<float> >(eTop_);
+  produces<edm::ValueMap<float> >(eBottom_);
+  produces<edm::ValueMap<float> >(eLeft_);
+  produces<edm::ValueMap<float> >(eRight_);
   produces<edm::ValueMap<float> >(clusterMaxDR_);
   produces<edm::ValueMap<float> >(clusterMaxDRDPhi_);
   produces<edm::ValueMap<float> >(clusterMaxDRDEta_);
@@ -138,6 +141,135 @@ ElectronRegressionValueMapProducer::ElectronRegressionValueMapProducer(const edm
 ElectronRegressionValueMapProducer::~ElectronRegressionValueMapProducer() {
 }
 
+template<typename LazyTools>
+inline void calculateValues(EcalClusterLazyToolsBase* tools_tocast,
+                            const edm::Ptr<reco::GsfElectron>& iEle,
+                            const edm::EventSetup& iSetup,
+                            std::vector<float>& vsigmaIEtaIPhi,
+                            std::vector<float>& veMax,
+                            std::vector<float>& ve2nd,
+                            std::vector<float>& veTop,
+                            std::vector<float>& veBottom,
+                            std::vector<float>& veLeft,
+                            std::vector<float>& veRight,
+                            std::vector<float>& vclusterMaxDR,
+                            std::vector<float>& vclusterMaxDRDPhi,
+                            std::vector<float>& vclusterMaxDRDEta,
+                            std::vector<float>& vclusterMaxDRRawEnergy,
+                            std::vector<float>& vclusterRawEnergy0, 
+                            std::vector<float>& vclusterRawEnergy1, 
+                            std::vector<float>& vclusterRawEnergy2, 
+                            std::vector<float>& vclusterDPhiToSeed0,
+                            std::vector<float>& vclusterDPhiToSeed1,
+                            std::vector<float>& vclusterDPhiToSeed2,
+                            std::vector<float>& vclusterDEtaToSeed0,
+                            std::vector<float>& vclusterDEtaToSeed1,
+                            std::vector<float>& vclusterDEtaToSeed2,
+                            std::vector<int>& veleIPhi,
+                            std::vector<int>& veleIEta,
+                            std::vector<float>& veleCryPhi,
+                            std::vector<float>& veleCryEta) {
+  LazyTools* tools = static_cast<LazyTools*>(tools_tocast);
+  
+  const auto& the_sc  = iEle->superCluster();
+  const auto& theseed = the_sc->seed();
+  
+  std::vector<float> vCov = tools->localCovariances( *theseed );
+  
+  const float eMax = tools->eMax( *theseed );
+  const float e2nd = tools->e2nd( *theseed );
+  const float eTop = tools->eTop( *theseed );
+  const float eLeft = tools->eLeft( *theseed );
+  const float eRight = tools->eRight( *theseed );
+  const float eBottom = tools->eBottom( *theseed );
+  
+  float dummy;
+  int iPhi;
+  int iEta;
+  float cryPhi;
+  float cryEta;
+  EcalClusterLocal _ecalLocal;
+  if (iEle->isEB()) 
+    _ecalLocal.localCoordsEB(*theseed, iSetup, cryEta, cryPhi, iEta, iPhi, dummy, dummy);
+  else 
+    _ecalLocal.localCoordsEE(*theseed, iSetup, cryEta, cryPhi, iEta, iPhi, dummy, dummy);
+  
+  double see = (isnan(vCov[0]) ? 0. : sqrt(vCov[0]));
+  double spp = (isnan(vCov[2]) ? 0. : sqrt(vCov[2]));
+  double sep;    
+  if (see*spp > 0)
+    sep = vCov[1] / (see * spp);
+  else if (vCov[1] > 0)
+    sep = 1.0;
+  else
+    sep = -1.0;
+  
+  vsigmaIEtaIPhi.push_back(sep);
+  veMax.push_back(eMax);
+  ve2nd.push_back(e2nd);
+  veTop.push_back(eTop);
+  veBottom.push_back(eBottom);
+  veLeft.push_back(eLeft);
+  veRight.push_back(eRight);
+  veleIPhi.push_back(iPhi);
+  veleIEta.push_back(iEta);
+  veleCryPhi.push_back(cryPhi);
+  veleCryEta.push_back(cryEta);
+  
+    // loop over all clusters that aren't the seed
+  auto clusend = the_sc->clustersEnd();
+  int numberOfClusters =  the_sc->clusters().size();
+  
+  std::vector<float> _clusterRawEnergy;
+  _clusterRawEnergy.resize(std::max(3, numberOfClusters), 0);
+  std::vector<float> _clusterDEtaToSeed;
+  _clusterDEtaToSeed.resize(std::max(3, numberOfClusters), 0);
+  std::vector<float> _clusterDPhiToSeed;
+  _clusterDPhiToSeed.resize(std::max(3, numberOfClusters), 0);
+  float _clusterMaxDR     = 999.;
+  float _clusterMaxDRDPhi = 999.;
+  float _clusterMaxDRDEta = 999.;
+  float _clusterMaxDRRawEnergy = 0.;
+  
+  size_t iclus = 0;
+  float maxDR = 0;
+  edm::Ptr<reco::CaloCluster> pclus;
+  for( auto clus = the_sc->clustersBegin(); clus != clusend; ++clus ) {
+    pclus = *clus;
+    
+    if(theseed == pclus ) 
+      continue;
+    _clusterRawEnergy.push_back(pclus->energy());
+    _clusterDPhiToSeed.push_back(reco::deltaPhi(pclus->phi(),theseed->phi()));
+    _clusterDEtaToSeed.push_back(pclus->eta() - theseed->eta());
+    
+    // find cluster with max dR
+    if(reco::deltaR(*pclus, *theseed) > maxDR) {
+      maxDR = reco::deltaR(*pclus, *theseed);
+      _clusterMaxDR = maxDR;
+      _clusterMaxDRDPhi = _clusterDPhiToSeed[iclus];
+      _clusterMaxDRDEta = _clusterDEtaToSeed[iclus];
+      _clusterMaxDRRawEnergy = _clusterRawEnergy[iclus];
+    }
+    
+    ++iclus;
+  }
+  
+  vclusterMaxDR.push_back(_clusterMaxDR);
+  vclusterMaxDRDPhi.push_back(_clusterMaxDRDPhi);
+  vclusterMaxDRDEta.push_back(_clusterMaxDRDEta);
+  vclusterMaxDRRawEnergy.push_back(_clusterMaxDRRawEnergy);
+  vclusterRawEnergy0.push_back(_clusterRawEnergy[0]); 
+  vclusterRawEnergy1.push_back(_clusterRawEnergy[1]); 
+  vclusterRawEnergy2.push_back(_clusterRawEnergy[2]); 
+  vclusterDPhiToSeed0.push_back(_clusterDPhiToSeed[0]);
+  vclusterDPhiToSeed1.push_back(_clusterDPhiToSeed[1]);
+  vclusterDPhiToSeed2.push_back(_clusterDPhiToSeed[2]);
+  vclusterDEtaToSeed0.push_back(_clusterDEtaToSeed[0]);
+  vclusterDEtaToSeed1.push_back(_clusterDEtaToSeed[1]);
+  vclusterDEtaToSeed2.push_back(_clusterDEtaToSeed[2]);
+}
+
 void ElectronRegressionValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   using namespace edm;
@@ -150,29 +282,39 @@ void ElectronRegressionValueMapProducer::produce(edm::Event& iEvent, const edm::
   bool isAOD = true;
   iEvent.getByToken(src_, src);
 
-  if( !src.isValid() ){
+  if( !src.isValid() ) {
     isAOD = false;
     iEvent.getByToken(srcMiniAOD_,src);
   }
+  
+  // configure lazy tools
+  edm::EDGetTokenT<EcalRecHitCollection> ebrh, eerh, esrh;
 
-  if( isAOD )
-    lazyToolnoZS = new noZS::EcalClusterLazyTools(iEvent, iSetup, 
-						  ebReducedRecHitCollection_, 
-						  eeReducedRecHitCollection_,
-						  esReducedRecHitCollection_ );
-  else
-    lazyToolnoZS = new noZS::EcalClusterLazyTools(iEvent, iSetup, 
-						  ebReducedRecHitCollectionMiniAOD_, 
-						  eeReducedRecHitCollectionMiniAOD_,
-						  esReducedRecHitCollectionMiniAOD_ );
- 
-  std::vector<float> eleFull5x5SigmaIEtaIPhi;
-  std::vector<float> eleFull5x5eMax;
-  std::vector<float> eleFull5x5e2nd;
-  std::vector<float> eleFull5x5eTop;
-  std::vector<float> eleFull5x5eBottom;
-  std::vector<float> eleFull5x5eLeft;
-  std::vector<float> eleFull5x5eRight;
+  if( isAOD ) {
+    ebrh = ebReducedRecHitCollection_;
+    eerh = eeReducedRecHitCollection_;
+    esrh = esReducedRecHitCollection_;
+  } else {
+    ebrh = ebReducedRecHitCollectionMiniAOD_;
+    eerh = eeReducedRecHitCollectionMiniAOD_;
+    esrh = esReducedRecHitCollectionMiniAOD_;
+  }
+  
+  if( use_full5x5_ ) {
+    lazyTools = std::make_unique<noZS::EcalClusterLazyTools>(iEvent, iSetup, 
+                                                             ebrh, eerh, esrh );
+  } else {
+    lazyTools = std::make_unique<EcalClusterLazyTools>(iEvent, iSetup, 
+                                                       ebrh, eerh, esrh );
+  }
+
+  std::vector<float> sigmaIEtaIPhi;
+  std::vector<float> eMax;
+  std::vector<float> e2nd;
+  std::vector<float> eTop;
+  std::vector<float> eBottom;
+  std::vector<float> eLeft;
+  std::vector<float> eRight;
   std::vector<float> clusterMaxDR;
   std::vector<float> clusterMaxDRDPhi;
   std::vector<float> clusterMaxDRDEta;
@@ -194,111 +336,73 @@ void ElectronRegressionValueMapProducer::produce(edm::Event& iEvent, const edm::
   // reco::GsfElectron::superCluster() is virtual so we can exploit polymorphism
   for (size_t i = 0; i < src->size(); ++i){
     auto iEle = src->ptrAt(i);
-    const auto theseed = iEle->superCluster()->seed();
 
-    std::vector<float> vCov = lazyToolnoZS->localCovariances( *theseed );
-        
-    const float eMax = lazyToolnoZS->eMax( *theseed );
-    const float e2nd = lazyToolnoZS->e2nd( *theseed );
-    const float eTop = lazyToolnoZS->eTop( *theseed );
-    const float eLeft = lazyToolnoZS->eLeft( *theseed );
-    const float eRight = lazyToolnoZS->eRight( *theseed );
-    const float eBottom = lazyToolnoZS->eBottom( *theseed );
-    
-    float dummy;
-    int iPhi;
-    int iEta;
-    float cryPhi;
-    float cryEta;
-    EcalClusterLocal _ecalLocal;
-    if (iEle->isEB()) 
-      _ecalLocal.localCoordsEB(*theseed, iSetup, cryEta, cryPhi, iEta, iPhi, dummy, dummy);
-    else 
-      _ecalLocal.localCoordsEE(*theseed, iSetup, cryEta, cryPhi, iEta, iPhi, dummy, dummy);
-    
-    double see = (isnan(vCov[0]) ? 0. : sqrt(vCov[0]));
-    double spp = (isnan(vCov[2]) ? 0. : sqrt(vCov[2]));
-    double sep;    
-    if (see*spp > 0)
-        sep = vCov[1] / (see * spp);
-    else if (vCov[1] > 0)
-        sep = 1.0;
-    else
-        sep = -1.0;
- 
-    eleFull5x5SigmaIEtaIPhi.push_back(sep);
-    eleFull5x5eMax.push_back(eMax);
-    eleFull5x5e2nd.push_back(e2nd);
-    eleFull5x5eTop.push_back(eTop);
-    eleFull5x5eBottom.push_back(eBottom);
-    eleFull5x5eLeft.push_back(eLeft);
-    eleFull5x5eRight.push_back(eRight);
-    eleIPhi.push_back(iPhi);
-    eleIEta.push_back(iEta);
-    eleCryPhi.push_back(cryPhi);
-    eleCryEta.push_back(cryEta);
-
-    // loop over all clusters that aren't the seed
-    auto clusend = iEle->superCluster()->clustersEnd();
-    int numberOfClusters =  iEle->superCluster()->clusters().size();
-
-    std::vector<float> _clusterRawEnergy;
-    _clusterRawEnergy.resize(std::max(3, numberOfClusters), 0);
-    std::vector<float> _clusterDEtaToSeed;
-    _clusterDEtaToSeed.resize(std::max(3, numberOfClusters), 0);
-    std::vector<float> _clusterDPhiToSeed;
-    _clusterDPhiToSeed.resize(std::max(3, numberOfClusters), 0);
-    float _clusterMaxDR     = 999.;
-    float _clusterMaxDRDPhi = 999.;
-    float _clusterMaxDRDEta = 999.;
-    float _clusterMaxDRRawEnergy = 0.;
-    
-    size_t iclus = 0;
-    float maxDR = 0;
-    edm::Ptr<reco::CaloCluster> pclus;
-    for( auto clus = iEle->superCluster()->clustersBegin(); clus != clusend; ++clus ) {
-      pclus = *clus;
-            
-      if(theseed == pclus ) 
-	continue;
-      _clusterRawEnergy.push_back(pclus->energy());
-      _clusterDPhiToSeed.push_back(reco::deltaPhi(pclus->phi(),theseed->phi()));
-      _clusterDEtaToSeed.push_back(pclus->eta() - theseed->eta());
-
-      // find cluster with max dR
-      if(reco::deltaR(*pclus, *theseed) > maxDR) {
-	maxDR = reco::deltaR(*pclus, *theseed);
-	_clusterMaxDR = maxDR;
-	_clusterMaxDRDPhi = _clusterDPhiToSeed[iclus];
-	_clusterMaxDRDEta = _clusterDEtaToSeed[iclus];
-	_clusterMaxDRRawEnergy = _clusterRawEnergy[iclus];
-      }
-
-      ++iclus;
+    if( use_full5x5_ ) {
+      calculateValues<noZS::EcalClusterLazyTools>(lazyTools.get(),
+                                                  iEle,
+                                                  iSetup,
+                                                  sigmaIEtaIPhi,
+                                                  eMax,
+                                                  e2nd,
+                                                  eTop,
+                                                  eBottom,
+                                                  eLeft,
+                                                  eRight,
+                                                  clusterMaxDR,
+                                                  clusterMaxDRDPhi,
+                                                  clusterMaxDRDEta,
+                                                  clusterMaxDRRawEnergy,
+                                                  clusterRawEnergy0, 
+                                                  clusterRawEnergy1, 
+                                                  clusterRawEnergy2, 
+                                                  clusterDPhiToSeed0,
+                                                  clusterDPhiToSeed1,
+                                                  clusterDPhiToSeed2,
+                                                  clusterDEtaToSeed0,
+                                                  clusterDEtaToSeed1,
+                                                  clusterDEtaToSeed2,
+                                                  eleIPhi,
+                                                  eleIEta,
+                                                  eleCryPhi,
+                                                  eleCryEta);
+    } else {
+      calculateValues<EcalClusterLazyTools>(lazyTools.get(),
+                                            iEle,
+                                            iSetup,
+                                            sigmaIEtaIPhi,
+                                            eMax,
+                                            e2nd,
+                                            eTop,
+                                            eBottom,
+                                            eLeft,
+                                            eRight,
+                                            clusterMaxDR,
+                                            clusterMaxDRDPhi,
+                                            clusterMaxDRDEta,
+                                            clusterMaxDRRawEnergy,
+                                            clusterRawEnergy0, 
+                                            clusterRawEnergy1, 
+                                            clusterRawEnergy2, 
+                                            clusterDPhiToSeed0,
+                                            clusterDPhiToSeed1,
+                                            clusterDPhiToSeed2,
+                                            clusterDEtaToSeed0,
+                                            clusterDEtaToSeed1,
+                                            clusterDEtaToSeed2,
+                                            eleIPhi,
+                                            eleIEta,
+                                            eleCryPhi,
+                                            eleCryEta);
     }
-  
-    clusterMaxDR.push_back(_clusterMaxDR);
-    clusterMaxDRDPhi.push_back(_clusterMaxDRDPhi);
-    clusterMaxDRDEta.push_back(_clusterMaxDRDEta);
-    clusterMaxDRRawEnergy.push_back(_clusterMaxDRRawEnergy);
-    clusterRawEnergy0.push_back(_clusterRawEnergy[0]); 
-    clusterRawEnergy1.push_back(_clusterRawEnergy[1]); 
-    clusterRawEnergy2.push_back(_clusterRawEnergy[2]); 
-    clusterDPhiToSeed0.push_back(_clusterDPhiToSeed[0]);
-    clusterDPhiToSeed1.push_back(_clusterDPhiToSeed[1]);
-    clusterDPhiToSeed2.push_back(_clusterDPhiToSeed[2]);
-    clusterDEtaToSeed0.push_back(_clusterDEtaToSeed[0]);
-    clusterDEtaToSeed1.push_back(_clusterDEtaToSeed[1]);
-    clusterDEtaToSeed2.push_back(_clusterDEtaToSeed[2]);
   }
   
-  writeValueMap(iEvent, src, eleFull5x5SigmaIEtaIPhi, eleFull5x5SigmaIEtaIPhi_);  
-  writeValueMap(iEvent, src, eleFull5x5eMax      ,eleFull5x5eMax_);
-  writeValueMap(iEvent, src, eleFull5x5e2nd	 ,eleFull5x5e2nd_);
-  writeValueMap(iEvent, src, eleFull5x5eTop	 ,eleFull5x5eTop_);
-  writeValueMap(iEvent, src, eleFull5x5eBottom	 ,eleFull5x5eBottom_);
-  writeValueMap(iEvent, src, eleFull5x5eLeft     ,eleFull5x5eLeft_);
-  writeValueMap(iEvent, src, eleFull5x5eRight	 ,eleFull5x5eRight_);
+  writeValueMap(iEvent, src, sigmaIEtaIPhi, sigmaIEtaIPhi_);  
+  writeValueMap(iEvent, src, eMax      ,eMax_);
+  writeValueMap(iEvent, src, e2nd	 ,e2nd_);
+  writeValueMap(iEvent, src, eTop	 ,eTop_);
+  writeValueMap(iEvent, src, eBottom	 ,eBottom_);
+  writeValueMap(iEvent, src, eLeft     ,eLeft_);
+  writeValueMap(iEvent, src, eRight	 ,eRight_);
   writeValueMap(iEvent, src, clusterMaxDR,	   clusterMaxDR_);	  
   writeValueMap(iEvent, src, clusterMaxDRDPhi,	   clusterMaxDRDPhi_);	  
   writeValueMap(iEvent, src, clusterMaxDRDEta,	   clusterMaxDRDEta_);	  
@@ -316,7 +420,7 @@ void ElectronRegressionValueMapProducer::produce(edm::Event& iEvent, const edm::
   writeValueMap(iEvent, src, eleIEta		 ,eleIEta_);
   writeValueMap(iEvent, src, eleCryPhi		 ,eleCryPhi_);
   writeValueMap(iEvent, src, eleCryEta           ,eleCryEta_);
-  delete lazyToolnoZS;
+  lazyTools.reset();
 }
 
 void ElectronRegressionValueMapProducer::writeValueMap(edm::Event &iEvent,
