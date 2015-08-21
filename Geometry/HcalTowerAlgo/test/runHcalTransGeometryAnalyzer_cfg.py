1d279932823374f7b94815106fa338d683335132
Sunanda <sunanda.banerjee@cern.ch>
2015-08-21 21:27:34 +0200

    Sort out rebasing

--- Geometry/HcalTowerAlgo/test/runHcalTransGeometryAnalyzer_cfg.py
+++ Geometry/HcalTowerAlgo/test/runHcalTransGeometryAnalyzer_cfg.py
@@ -3,19 +3,13 @@ from Geometry.HcalEventSetup.HcalRelabel_cfi import HcalReLabel
 
 process = cms.Process("HcalGeometryTest")
 
-process.load("Configuration.Geometry.GeometryExtended_cff")
+process.load("Configuration.Geometry.GeometryExtendedPostLS2_cff")
 process.load("Geometry.HcalEventSetup.HcalTopology_cfi")
 
 process.HcalHardcodeGeometryEP = cms.ESProducer( "HcalHardcodeGeometryEP" ,
                                                  appendToDataLabel = cms.string("_master"),
                                                  HcalReLabel = HcalReLabel
                                                  )
-## Comment it out to test std Hcal geometry
-##
-import Geometry.HcalEventSetup.hcalSLHCTopologyConstants_cfi as hcalTopologyConstants_cfi
-process.hcalTopologyIdeal.hcalTopologyConstants = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
-##
-
 process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
