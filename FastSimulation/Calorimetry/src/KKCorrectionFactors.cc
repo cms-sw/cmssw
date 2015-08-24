f2dc7272765f7afe407fd7334d9f1cd54c1963a7
Knut Kiesel <kiesel@cern.ch>
2015-08-24 09:20:42 +0200

    changed variable names for filename and histogramName

--- FastSimulation/Calorimetry/src/KKCorrectionFactors.cc
+++ FastSimulation/Calorimetry/src/KKCorrectionFactors.cc
@@ -6,21 +6,21 @@ KKCorrectionFactors::KKCorrectionFactors( const edm::ParameterSet& pset )
   interpolate_ = pset.exists("interpolate") && pset.getUntrackedParameter<bool>("interpolate");
 
   // Get the filename and histogram name
-  std::string ecalScalesFileName_ = pset.getUntrackedParameter<std::string>("fileName");
-  std::string ecalScalesHistogramName_ = pset.getUntrackedParameter<std::string>("histogramName");
+  std::string fileName = pset.getUntrackedParameter<std::string>("fileName");
+  std::string histogramName = pset.getUntrackedParameter<std::string>("histogramName");
 
   // Read histo
-  edm::FileInPath myDataFile( ecalScalesFileName_ ); // TODO: check if this is necessary
+  edm::FileInPath myDataFile( fileName ); // TODO: check if this is necessary
   TFile * myFile = new TFile( myDataFile.fullPath().c_str(), "READ" );
 
   gROOT->cd(); // create histogram in memory
-  auto obj = myFile->Get( ecalScalesHistogramName_.c_str() );
+  auto obj = myFile->Get( histogramName.c_str() );
 
   // Check if histogram exists in file
   if(!obj) {
     throw cms::Exception( "FileReadError" )
-      << "Histogram \"" << ecalScalesHistogramName_
-      << "\" not found in file \"" << ecalScalesFileName_
+      << "Histogram \"" << histogramName
+      << "\" not found in file \"" << fileName
       << "\", used for correcting the response of the ECAL in FastSim.\n";
   }
   h3_ = new TH3F( *((TH3F*)obj) );
