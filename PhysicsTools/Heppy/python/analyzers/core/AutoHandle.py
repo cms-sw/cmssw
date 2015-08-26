6bdd036ecc341308ee3fd456102df120ed487f7d
Giovanni <gpetruc@gmail.com>
2015-08-26 10:24:16 +0200

    Heppy TriggerBitAnalyzer: Support for fallback process name (for RECO vs PAT process in met filters)

--- PhysicsTools/Heppy/python/analyzers/core/AutoHandle.py
+++ PhysicsTools/Heppy/python/analyzers/core/AutoHandle.py
@@ -14,8 +14,7 @@ class AutoHandle( Handle, object ):
         self.type = type
         self.mayFail = mayFail
         self.lazy = lazy
-        self.autoDisable = disableAtFirstFail;
-        self.disabled= False
+        self.isLoaded = False
         Handle.__init__(self, self.type)
     def product(self):
         if not self.isLoaded :
