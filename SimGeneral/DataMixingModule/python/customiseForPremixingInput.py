9c7f0e12f5231933f64a0a97b44fd31aea26957f
Michael <mikeh@fnal.gov>
2015-08-27 05:50:49 +0200

    next pass at global replacement of input tags

--- SimGeneral/DataMixingModule/python/customiseForPremixingInput.py
+++ SimGeneral/DataMixingModule/python/customiseForPremixingInput.py
@@ -5,9 +5,13 @@ def customiseForPremixingInput(process):
 
     # Replace TrackingParticles and TrackingVertices globally
     for s in process.paths_().keys():
-        massSearchReplaceAnyInputTag(getattr(process, s), cms.InputTag("mix", "MergedTrackTruth"), cms.InputTag("mixData", "MergedTrackTruth"), skipLabelTest=True)
+        massSearchReplaceAnyInputTag(getattr(process, s), cms.InputTag("mix", "MergedTrackTruth"), cms.InputTag("mixData", "MergedTrackTruth"), skipLabelTest=True) 
+
     for s in process.endpaths_().keys():
         massSearchReplaceAnyInputTag(getattr(process, s), cms.InputTag("mix", "MergedTrackTruth"), cms.InputTag("mixData", "MergedTrackTruth"), skipLabelTest=True)
+
+    
+
 
     # Replace Pixel/StripDigiSimLinks only for the known modules
     def replaceInputTag(tag, old, new):
@@ -23,10 +27,42 @@ def customiseForPremixingInput(process):
         if producer.type_() == "ClusterTPAssociationProducer":
             replacePixelDigiSimLink(producer.pixelSimLinkSrc)
             replaceStripDigiSimLink(producer.stripSimLinkSrc)
+        if producer.type_() == "QuickTrackAssociatorByHitsProducer":
+            replacePixelDigiSimLink(producer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(producer.stripSimLinkSrc)
+        if producer.type_() == "MuonToTrackingParticleAssociatorEDProducer":
+            replacePixelDigiSimLink(producer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(producer.stripSimLinkSrc)
+        if producer.type_() == "TrackAssociatorByHitsProducer":
+            replacePixelDigiSimLink(producer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(producer.stripSimLinkSrc)
         if producer.type_() == "MuonAssociatorEDProducer":
             producer.DTdigisimlinkTag = cms.InputTag("mixData","simMuonDTDigis")
             producer.CSClinksTag = cms.InputTag("mixData","MuonCSCStripDigiSimLinks")
             producer.CSCwireLinksTag = cms.InputTag("mixData","MuonCSCWireDigiSimLinks")
             producer.RPCdigisimlinkTag = cms.InputTag("mixData","RPCDigiSimLink")
+            replacePixelDigiSimLink(producer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(producer.stripSimLinkSrc)
+
+
+    for label, analyzer in process.analyzers_().iteritems():
+        if analyzer.type_() == "GlobalRecHitsAnalyzer":
+            replacePixelDigiSimLink(analyzer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(analyzer.stripSimLinkSrc)
+        if analyzer.type_() == "SiPixelTrackingRecHitsValid":
+            replacePixelDigiSimLink(analyzer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(analyzer.stripSimLinkSrc)
+        if analyzer.type_() == "SiStripTrackingRecHitsValid":
+            replacePixelDigiSimLink(analyzer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(analyzer.stripSimLinkSrc)
+        if analyzer.type_() == "SiPixelRecHitsValid":
+            replacePixelDigiSimLink(analyzer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(analyzer.stripSimLinkSrc)
+        if analyzer.type_() == "SiStripRecHitsValid":
+            replacePixelDigiSimLink(analyzer.pixelSimLinkSrc)
+            replaceStripDigiSimLink(analyzer.stripSimLinkSrc)
+
+
+
 
     return process
