97985d36ac71fccf5dc43385153a288fa46d61c9
Martin Grunewald <Martin.Grunewald@cern.ch>
2015-08-27 08:09:10 +0200

    Subtables of the morning

--- HLTrigger/Configuration/python/HLT_GRun_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/GRun/V133 (CMSSW_7_4_10_patch1)
+# /dev/CMSSW_7_4_0/GRun/V135 (CMSSW_7_4_10_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/GRun/V133')
+  tableName = cms.string('/dev/CMSSW_7_4_0/GRun/V135')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/PIon/V133 (CMSSW_7_4_10_patch1)
+# /dev/CMSSW_7_4_0/PIon/V135 (CMSSW_7_4_10_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V133')
+  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V135')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
