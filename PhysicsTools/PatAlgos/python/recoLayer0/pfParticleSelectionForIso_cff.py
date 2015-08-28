c84b62d69f0cfe7d23a0f4f5fe3661095efdd656
Giovanni <gpetruc@gmail.com>
2015-08-28 14:22:00 +0200

    Don't load particleFlowPtrs (PhysicsTools/PatAlgos/python/recoLayer0/pfParticleSelectionForIso_cff)

--- PhysicsTools/PatAlgos/python/recoLayer0/pfParticleSelectionForIso_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/pfParticleSelectionForIso_cff.py
@@ -1,12 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-from CommonTools.ParticleFlow.PFBRECO_cff import particleFlowPtrs
 from CommonTools.ParticleFlow.PFBRECO_cff import pfPileUpIsoPFBRECO, pfNoPileUpIsoPFBRECO, pfNoPileUpIsoPFBRECOSequence
 from CommonTools.ParticleFlow.PFBRECO_cff import pfPileUpPFBRECO, pfNoPileUpPFBRECO, pfNoPileUpPFBRECOSequence
 from CommonTools.ParticleFlow.PFBRECO_cff import pfAllNeutralHadronsPFBRECO, pfAllChargedHadronsPFBRECO, pfAllPhotonsPFBRECO, pfAllChargedParticlesPFBRECO, pfPileUpAllChargedParticlesPFBRECO, pfAllNeutralHadronsAndPhotonsPFBRECO, pfSortByTypePFBRECOSequence
 from CommonTools.ParticleFlow.PFBRECO_cff import pfParticleSelectionPFBRECOSequence
 
 pfParticleSelectionForIsoSequence = cms.Sequence(
-    particleFlowPtrs +
     pfParticleSelectionPFBRECOSequence
     )
