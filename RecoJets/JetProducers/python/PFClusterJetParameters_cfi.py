c41d70915510c3ade8f3485c52bd0e66f7897bc3
Andreas Hinzmann <hinzmann@cern.ch>
2015-09-01 11:13:51 +0200

    enable fastjet area computation by default

--- RecoJets/JetProducers/python/PFClusterJetParameters_cfi.py
+++ RecoJets/JetProducers/python/PFClusterJetParameters_cfi.py
@@ -19,7 +19,7 @@ PFClusterJetParameters = cms.PSet(
     nSigmaPU = cms.double(1.0),
     radiusPU = cms.double(0.5),  
     # fastjet-style pileup 
-    doAreaFastjet       = cms.bool( False),
+    doAreaFastjet       = cms.bool( True),
     doRhoFastjet        = cms.bool( False),
     doAreaDiskApprox    = cms.bool( False),
     Active_Area_Repeats = cms.int32(    1),
