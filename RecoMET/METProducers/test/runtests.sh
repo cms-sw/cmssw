e00f602d61796528d8a2a3b512af4cfb1e67b930
Lukas <lukas.vanelderen@cern.ch>
2015-09-01 10:47:57 +0200

    fix unit tests by producing fastsim on the fly (temporary)

--- RecoMET/METProducers/test/runtests.sh
+++ RecoMET/METProducers/test/runtests.sh
@@ -4,6 +4,10 @@
 function die { echo $1: status $2 ;  exit $2; }
 
 ##____________________________________________________________________________||
+
+# temporary: produce fastsim sample on the fly
+# can be removed as soon as relval samples are available with the new fastsim rechits
+cmsDriver.py TTbar_13TeV_TuneCUETP8M1_cfi  --conditions auto:run2_mc --fast  -n 1 --eventcontent FEVTDEBUGHLT -s GEN,SIM,RECOBEFMIX,DIGI:pdigi_valid,L1,L1Reco,RECO --beamspot NominalCollision2015 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --fileout ttbarForMetTests.root
 cmsRun ${LOCAL_TEST_DIR}/recoMET_pfMet_cfg.py || die 'Failure using recoMET_pfMet_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/recoMET_caloMet_cfg.py || die 'Failure using recoMET_caloMet_cfg.py' $?
 cmsRun ${LOCAL_TEST_DIR}/recoMET_tcMet_cfg.py || die 'Failure using recoMET_tcMet_cfg.py' $?
