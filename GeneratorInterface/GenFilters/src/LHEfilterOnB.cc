15c1e0facdbafc0bbf7ad8d705678403c0a4a115
perrozzi <perrozzi@cern.ch>
2015-09-02 16:31:50 +0200

    moved LHE filter to GeneratorInterface/GenFilters

--- GeneratorInterface/GenFilters/src/LHEfilterOnB.cc
+++ GeneratorInterface/GenFilters/src/LHEfilterOnB.cc
@@ -5,16 +5,16 @@ using namespace edm;
 using namespace std;
 
 LHEfilterOnB::LHEfilterOnB(const edm::ParameterSet & iConfig): 
-  // LHE collection
-  lhesrc_(iConfig.getParameter<InputTag>( "LHEEventProduct" ) ),
-  totalEvents_(0), passedEvents_(0)
+// LHE collection
+lhesrc_(iConfig.getParameter<InputTag>( "LHEEventProduct" ) ),
+totalEvents_(0), passedEvents_(0)
 {
 
 }
 
 LHEfilterOnB::~LHEfilterOnB()
 {
- 
+
 }
 
 bool LHEfilterOnB::filter(edm::Event & iEvent, const edm::EventSetup & iSetup) {
@@ -26,9 +26,9 @@ bool LHEfilterOnB::filter(edm::Event & iEvent, const edm::EventSetup & iSetup) {
 
   const lhef::HEPEUP hepeup = lheevt->hepeup();
   int nb_lhe = 0;
-	for(int i = 0; i < hepeup.NUP; i++) {
+  for(int i = 0; i < hepeup.NUP; i++) {
     // check outgoing b partons only
-		if (hepeup.ISTUP[i] == 1 && TMath::Abs(hepeup.IDUP[i])==5){
+    if (hepeup.ISTUP[i] == 1 && TMath::Abs(hepeup.IDUP[i])==5){
       nb_lhe++;
       // if more than one is found, reject event
       if(nb_lhe>1){
@@ -36,7 +36,7 @@ bool LHEfilterOnB::filter(edm::Event & iEvent, const edm::EventSetup & iSetup) {
       }
     }
     cout << endl;
-	}
+  }
   
   passedEvents_++;
   return true;
@@ -46,7 +46,7 @@ bool LHEfilterOnB::filter(edm::Event & iEvent, const edm::EventSetup & iSetup) {
 // ------------ method called once each job just after ending the event loop  ------------
 void LHEfilterOnB::endJob() {
   edm::LogInfo("LHEfilterOnB") << "=== Results of LHEfilterOnB: passed "
-                                        << passedEvents_ << "/" << totalEvents_ << " events" << std::endl;
+  << passedEvents_ << "/" << totalEvents_ << " events" << std::endl;
 }
 
 //define this as a plug-in
