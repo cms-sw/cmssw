a8af2baed27a1c0b525f373db343ab28aa9aaf7f
Andreas Hinzmann <hinzmann@cern.ch>
2015-09-02 11:35:52 +0200

    revert last commit

--- PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
+++ PhysicsTools/PatAlgos/python/slimming/miniAOD_tools.py
@@ -301,6 +301,7 @@ def miniAOD_customizeMC(process):
     process.photonMatch.src = cms.InputTag("reducedEgamma","reducedGedPhotons")
     process.tauMatch.matched = "prunedGenParticles"
     process.tauGenJets.GenParticles = "prunedGenParticles"
+    process.patJetPartons.particles = "prunedGenParticles"
     process.patJetPartonMatch.matched = "prunedGenParticles"
     process.patJetPartonMatch.mcStatus = [ 3, 23 ]
     process.patJetGenJetMatch.matched = "slimmedGenJets"
