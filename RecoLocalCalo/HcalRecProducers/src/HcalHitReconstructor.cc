470212e7b7dbf20fd39d5473c56ee729041e885f
Katharina <katharina.bierwagen@cern.ch>
2015-09-03 09:21:18 +0200

    Added back the fillDescriptions method with all Method3 configurations

--- RecoLocalCalo/HcalRecProducers/src/HcalHitReconstructor.cc
+++ RecoLocalCalo/HcalRecProducers/src/HcalHitReconstructor.cc
@@ -282,6 +282,17 @@ HcalHitReconstructor::HcalHitReconstructor(edm::ParameterSet const& conf):
 
 }
 
+void HcalHitReconstructor::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.setAllowAnything();
+  desc.add<int>("pedestalSubtractionType", 1); 
+  desc.add<double>("pedestalUpperLimit", 2.7); 
+  desc.add<int>("timeSlewParsType",3);
+  desc.add<std::vector<double>>("timeSlewPars", {15.5, -3.2, 32, 15.5, -3.2, 32, 15.5, -3.2, 32});
+  desc.add<double>("respCorrM3", 0.95);
+  descriptions.add("hltHbhereco",desc);
+}
+
 
 HcalHitReconstructor::~HcalHitReconstructor() {
   delete hbheFlagSetter_;
