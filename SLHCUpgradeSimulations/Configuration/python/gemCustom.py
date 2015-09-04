dd7abee28cfc27a4b6db0e3c2cf4c4323071a936
Geonmo <ry840901@gmail.com>
2015-09-04 11:16:58 +0900

    Change if statement to avoid confusing. Add EDMtoME for our module.

--- SLHCUpgradeSimulations/Configuration/python/gemCustom.py
+++ SLHCUpgradeSimulations/Configuration/python/gemCustom.py
@@ -11,14 +11,14 @@ def customise_digitization(process):
   from SimMuon.GEMDigitizer.customizeGEMDigi import customize_digi_addGEM_muon_only
   if ( not hasattr(process,"simMuonGEMDigis")) :
     process = customize_digi_addGEM_muon_only(process)
-  if ( hasattr(process,"simMuonGEMDigis")) :
-    if ( not hasattr(process,"simMuonGEMDigis.mixLabel")) :
-      process.simMuonGEMDigis.mixLabel = cms.string("mix")
+  if ( not hasattr(process,"simMuonGEMDigis.mixLabel")) :
+    process.simMuonGEMDigis.mixLabel = cms.string("mix")
   if ( hasattr(process,"simMuonME0Digis")) :
     if ( not hasattr(process,"simMuonME0Digis.mixLabel")) :
       process.simMuonME0Digis.mixLabel = cms.string("mix")
   return process
 
+### MuonGEM Validation module need dqm saver because it doesn't run all step.
 def customise_Validation(process):
   process.load('Validation.MuonGEMHits.gemSimValid_cff')
   process.load('Validation.MuonGEMDigis.MuonGEMDigis_cfi')
@@ -29,5 +29,7 @@ def customise_Validation(process):
 def customise_harvesting(process):
   process.load('Validation.MuonGEMHits.gemPostValidation_cff')
   process.genHarvesting += process.gemPostValidation
+  process.load('DQMServices.Components.EDMtoMEConverter_cff')
+  process.genHarvesting += process.EDMtoMEConverter
   return process
 
