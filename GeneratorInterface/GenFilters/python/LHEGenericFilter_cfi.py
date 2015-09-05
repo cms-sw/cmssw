71215be424d31af55947cc4739f428dac2a07589
perrozzi <perrozzi@cern.ch>
2015-09-05 03:42:44 +0200

    remove spurious LHEGenericFilter options

--- GeneratorInterface/GenFilters/python/LHEGenericFilter_cfi.py
+++ GeneratorInterface/GenFilters/python/LHEGenericFilter_cfi.py
@@ -4,5 +4,5 @@ lheGenericFilter = cms.EDFilter("LHEGenericFilter",
     src = cms.InputTag("source"),
     NumRequired = cms.int32(2),
     ParticleID = cms.vint32(5),
-    AcceptLogic = cms.string("LT") # LT meaning < NumRequired, LE <=, GT >, GE >=, EQ =, NE !=
+    AcceptLogic = cms.string("LT") # LT meaning < NumRequired, GT >, EQ =, NE !=
 )                                
