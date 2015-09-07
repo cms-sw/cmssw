5394e434409714f669821ffc2de5333ccf1e35e7
Piet <piet.verwilligen@cern.ch>
2015-09-07 11:53:54 +0200

    updated

--- RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
+++ RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
@@ -6,6 +6,9 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
 # process.load('Configuration.Geometry.GeometryExtended2023HGCalMuonReco_cff')
 
+# CSCGeometry depends on alignment ==> necessary to provide GlobalPositionRecord
+process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi") 
+process.load("Geometry.CSCGeometry.cscGeometry_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
