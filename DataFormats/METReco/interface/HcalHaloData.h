6807d8d61616e1d0d9ae00040f444b4b959731f6
Nick <namin@cern.ch>
2015-09-08 04:40:47 +0200

    initial commit...import from 76X version

--- DataFormats/METReco/interface/HcalHaloData.h
+++ DataFormats/METReco/interface/HcalHaloData.h
@@ -8,21 +8,45 @@
 */
 #include <vector>
 #include "DataFormats/METReco/interface/PhiWedge.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/CaloTowers/interface/CaloTowerDetId.h"
+
+struct HaloTowerStrip {
+    std::vector<std::pair<uint8_t, CaloTowerDetId> > cellTowerIds;
+    float hadEt;
+    HaloTowerStrip() {
+        hadEt = 0.0;
+        cellTowerIds.clear();
+    }
+    HaloTowerStrip(const HaloTowerStrip& strip) {
+        cellTowerIds = strip.cellTowerIds;
+        hadEt = strip.hadEt;
+    }
+};
 
 namespace reco {
+
   class HcalHaloData {
+
   public:
     //constructor
     HcalHaloData();
     //destructor
     ~HcalHaloData(){}
+
+
     
     // Return collection of 5-degree Phi Wedges built from Hcal RecHits
     const std::vector<PhiWedge>& GetPhiWedges() const {return PhiWedgeCollection;}
     std::vector<PhiWedge>& GetPhiWedges()  {return PhiWedgeCollection;}
-    
+
+    // Return collection of problematic strips (pairs of # of problematic HCAL cells and CaloTowerDetId)
+    const std::vector<HaloTowerStrip>& getProblematicStrips() const {return problematicStripCollection;}
+    std::vector<HaloTowerStrip>& getProblematicStrips()  {return problematicStripCollection;}
+
   private:
     std::vector<PhiWedge> PhiWedgeCollection;
+    std::vector<HaloTowerStrip> problematicStripCollection;
     
   };
 }
