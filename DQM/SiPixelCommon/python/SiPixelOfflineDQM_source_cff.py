21e0f5966253eecd9c20203f28c4f5be81b40d85
Francesco Fiori <francesco.fiori@cern.ch>
2015-09-08 17:23:47 +0200

    avoid empty FED occupancy plot in offline Pixel DQM

--- DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
+++ DQM/SiPixelCommon/python/SiPixelOfflineDQM_source_cff.py
@@ -51,7 +51,6 @@ SiPixelDigiSource.bladeOn = True
 SiPixelDigiSource.diskOn = True
 SiPixelDigiSource.ringOn = False
 SiPixelDigiSource.bigEventSize = 2600
-SiPixelDigiSource.isOffline = True
 #Cluster
 SiPixelClusterSource.modOn = False
 SiPixelClusterSource.twoDimOn = False
