73e68eb443d9eec1a4c7a1fa93491453c977d04f
jasper.lauwers <jasper.lauwers@uantwerpen.be>
2015-09-09 11:08:23 +0200

    Update WHToENuBB paths

--- HLTriggerOffline/Higgs/python/hltHiggsValidator_cfi.py
+++ HLTriggerOffline/Higgs/python/hltHiggsValidator_cfi.py
@@ -350,7 +350,7 @@ hltHiggsValidator = cms.EDAnalyzer("HLTHiggsValidator",
             "HLT_Ele27_eta2p1_WPLoose_Gsf_v",
             "HLT_Ele27_eta2p1_WPLoose_Gsf_HT200_v",
             
-            # frozen menu paths (already in dqm)
+            # frozen menu paths
             "HLT_Ele27_WP85_Gsf_v",
             "HLT_Ele27_eta2p1_WP85_Gsf_HT200_v"
             ),
@@ -382,6 +382,7 @@ hltHiggsValidator = cms.EDAnalyzer("HLTHiggsValidator",
     WHToENuBB  = cms.PSet( 
         hltPathsToCheck = cms.vstring(
             "HLT_Ele27_WPLoose_Gsf_WHbbBoost_v",
+            "HLT_Ele23_WPLoose_Gsf_WHbbBoost_v"
             ),
         recElecLabel   = cms.string("gedGsfElectrons"),
         recJetLabel  = cms.string("ak4PFJetsCHS"),
