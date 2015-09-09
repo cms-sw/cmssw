7a26930789d1cf7c683404202eb420037c8e7d3e
jasper.lauwers <jasper.lauwers@uantwerpen.be>
2015-09-09 16:52:32 +0200

    code fix for Htaunu

--- HLTriggerOffline/Higgs/src/HLTHiggsSubAnalysis.cc
+++ HLTriggerOffline/Higgs/src/HLTHiggsSubAnalysis.cc
@@ -746,12 +746,18 @@ const std::vector<unsigned int> HLTHiggsSubAnalysis::getObjectsType(const std::s
         {
             if( (objtriggernames[i] == EVTColContainer::PFJET && TString(hltPath).Contains("WHbbBoost") ) ||   // fix for HLT_Ele27_WPLoose_Gsf_WHbbBoost_v
                 (objtriggernames[i] == EVTColContainer::PFJET && TString(hltPath).Contains("CSV") ) ||   // fix for ZnnHbb PFJET            
-                (objtriggernames[i] == EVTColContainer::PFMET && TString(hltPath).Contains("MHT")) )        // fix for ZnnHbb PFMET
-                objsType.insert(objtriggernames[i]);
-            else if (objtriggernames[i] == EVTColContainer::PHOTON && TString(hltPath).Contains("Diphoton") ) objsType.insert(objtriggernames[i]);    //case of the New Diphoton paths
+                (objtriggernames[i] == EVTColContainer::PFMET && TString(hltPath).Contains("MHT")) ||        // fix for ZnnHbb PFMET
+                (objtriggernames[i] == EVTColContainer::PHOTON && TString(hltPath).Contains("Diphoton")) ) 
+            {
+                    objsType.insert(objtriggernames[i]);    //case of the New Diphoton paths
+            }
            continue;
         }
-        if( objtriggernames[i] == EVTColContainer::CALOMET && (TString(hltPath).Contains("PFMET") || TString(hltPath).Contains("MHT") ) ) continue; // fix for PFMET
+        if( ( objtriggernames[i] == EVTColContainer::CALOMET && (TString(hltPath).Contains("PFMET") || TString(hltPath).Contains("MHT") ) ) || // fix for PFMET
+        (objtriggernames[i] == EVTColContainer::PFJET && TString(hltPath).Contains("JetIdCleaned")) ) // fix for Htaunu
+        {
+            continue;
+        }
 
         objsType.insert(objtriggernames[i]);
     }
