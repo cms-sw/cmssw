88b0d547c975399b285edc0ec549ca8abe47a583
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-09-10 15:18:17 +0200

    Heppy: more flexibility for CMSSW pre-processor

--- PhysicsTools/Heppy/python/utils/cmsswPreprocessor.py
+++ PhysicsTools/Heppy/python/utils/cmsswPreprocessor.py
@@ -1,22 +1,47 @@
-import os                                                               
+import os 
+import sys                                                              
 import re 
 import imp
 from PhysicsTools.HeppyCore.framework.config import CFG
 class CmsswPreprocessor :
-	def __init__(self,configFile,command="cmsRun") :
+	def __init__(self,configFile,command="cmsRun",options={}) :
 		self.configFile=configFile
 		self.command=command
+                self.options=options
 	
 	def run(self,component,wd,firstEvent,nEvents):
 		print wd,firstEvent,nEvents
 		if nEvents is None:
 			nEvents = -1
-		cmsswConfig = imp.load_source("cmsRunProcess",self.configFile)
+
+                cmsswConfig = imp.load_source("cmsRunProcess",self.configFile)
 		inputfiles= []
 		for fn in component.files :
 			if not re.match("file:.*",fn) and not re.match("root:.*",fn) :
 				fn="file:"+fn
 			inputfiles.append(fn)
+
+                # Four cases: 
+                # - no options, cmsswConfig with initialize function
+                #     run initialize with default parameters
+                # - filled options, cmsswConfig with initialize function
+                #     pass on options to initialize
+                # - no options, classic cmsswConfig
+                #     legacy mode
+                # - filled options, classic cmsswConfig
+                #     legacy mode but warn that options are not passed on
+
+                if hasattr(cmsswConfig, "initialize"):
+                        if len(self.options) == 0:                                
+                                cmsswConfig.process = cmsswConfig.initialize()                                 
+                        else:                                
+                                cmsswConfig.process = cmsswConfig.initialize(**self.options)                                 
+                else:
+                        if len(self.options) == 0:                             
+                                pass
+                        else:
+                                print "WARNING: cmsswPreprocessor received options but can't pass on to cmsswConfig"
+                
 		cmsswConfig.process.source.fileNames = inputfiles
 		cmsswConfig.process.maxEvents.input=nEvents
 		#fixme: implement skipEvent / firstevent
