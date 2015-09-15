02807b11fef443a1fa1605122def4ad2d3667469
Mauro <mauro.dinardo@cern.ch>
2015-09-15 17:40:05 +0200

    Just for clarity...
    Change just for clarity

--- DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/beampixel_dqm_sourceclient-live_cfg.py
@@ -33,11 +33,12 @@ process.dqmSaver.tag = "BeamPixel"
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
-process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
 # Use this to run locally (for testing purposes)
 #process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 #from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 #process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_Prompt_v0', '')
+# Otherwise use this
+process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
 
 
 #----------------------------
