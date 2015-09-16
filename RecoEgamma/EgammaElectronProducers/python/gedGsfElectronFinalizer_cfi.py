adaba7937372e2641f68deca2232a1b0ddca75d1
Lindsey Gray <lindsey.gray@gmail.com>
2015-09-16 23:14:10 +0200

    update photon and electron final producers to apply new regression

--- RecoEgamma/EgammaElectronProducers/python/gedGsfElectronFinalizer_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/gedGsfElectronFinalizer_cfi.py
@@ -1,12 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-gedGsfElectrons = cms.EDProducer("GEDGsfElectronFinalizer",
-                                 previousGsfElectronsTag = cms.InputTag("gedGsfElectronsTmp"),
-                                 pfCandidatesTag = cms.InputTag("particleFlowTmp"),
-                                 pfIsolationValues = cms.PSet(
-                                       pfSumChargedHadronPt = cms.InputTag('gedElPFIsoValueCharged03'),
-                                       pfSumPhotonEt = cms.InputTag('gedElPFIsoValueGamma03'),
-                                       pfSumNeutralHadronEt= cms.InputTag('gedElPFIsoValueNeutral03'),
-                                       pfSumPUPt = cms.InputTag('gedElPFIsoValuePU03')),
-                                 outputCollectionLabel = cms.string("")
-                                 )
+from RecoEgamma.EgammaTools.regressionModifier_cfi import *
+
+gedGsfElectrons = cms.EDProducer(
+    "GEDGsfElectronFinalizer",
+    previousGsfElectronsTag = cms.InputTag("gedGsfElectronsTmp"),
+    pfCandidatesTag = cms.InputTag("particleFlowTmp"),
+    regressionConfig = regressionModifier.clone(rhoCollection=cms.InputTag("fixedGridRhoFastjetAllTmp")),
+    pfIsolationValues = cms.PSet(
+        pfSumChargedHadronPt = cms.InputTag('gedElPFIsoValueCharged03'),
+        pfSumPhotonEt = cms.InputTag('gedElPFIsoValueGamma03'),
+        pfSumNeutralHadronEt= cms.InputTag('gedElPFIsoValueNeutral03'),
+        pfSumPUPt = cms.InputTag('gedElPFIsoValuePU03')),
+    outputCollectionLabel = cms.string("")
+    )
