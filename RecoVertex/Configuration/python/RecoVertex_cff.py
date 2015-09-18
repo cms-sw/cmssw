a8a4caf75fe4a34f39da7b1f0daeaa5ce7365d65
Piet <piet.verwilligen@cern.ch>
2015-09-18 11:49:14 +0200

    commented out Run-II config and restored Run-I config

--- RecoVertex/Configuration/python/RecoVertex_cff.py
+++ RecoVertex/Configuration/python/RecoVertex_cff.py
@@ -13,19 +13,21 @@ from RecoJets.JetProducers.TracksForJets_cff import *
 from CommonTools.RecoAlgos.sortedPrimaryVertices_cfi import *
 from RecoJets.JetProducers.caloJetsForTrk_cff import *
 
-unsortedOfflinePrimaryVertices=offlinePrimaryVertices.clone()
-offlinePrimaryVertices=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices", particles="trackRefsForJetsBeforeSorting")
-offlinePrimaryVerticesWithBS=sortedPrimaryVertices.clone(vertices=cms.InputTag("unsortedOfflinePrimaryVertices","WithBS"), particles="trackRefsForJetsBeforeSorting")
-trackWithVertexRefSelectorBeforeSorting = trackWithVertexRefSelector.clone(vertexTag="unsortedOfflinePrimaryVertices")
-trackWithVertexRefSelectorBeforeSorting.ptMax=9e99
-trackWithVertexRefSelectorBeforeSorting.ptErrorCut=9e99
-trackRefsForJetsBeforeSorting = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSorting")
+# lines below are for Run-II, comment out to configure for Run-I
+# unsortedOfflinePrimaryVertices=offlinePrimaryVertices.clone()
+# offlinePrimaryVertices=sortedPrimaryVertices.clone(vertices="unsortedOfflinePrimaryVertices", particles="trackRefsForJetsBeforeSorting")
+# offlinePrimaryVerticesWithBS=sortedPrimaryVertices.clone(vertices=cms.InputTag("unsortedOfflinePrimaryVertices","WithBS"), particles="trackRefsForJetsBeforeSorting")
+# trackWithVertexRefSelectorBeforeSorting = trackWithVertexRefSelector.clone(vertexTag="unsortedOfflinePrimaryVertices")
+# trackWithVertexRefSelectorBeforeSorting.ptMax=9e99
+# trackWithVertexRefSelectorBeforeSorting.ptErrorCut=9e99
+# trackRefsForJetsBeforeSorting = trackRefsForJets.clone(src="trackWithVertexRefSelectorBeforeSorting")
 
 
-vertexreco = cms.Sequence(unsortedOfflinePrimaryVertices*
-        trackWithVertexRefSelectorBeforeSorting*
-        trackRefsForJetsBeforeSorting*
-        caloJetsForTrk * 
+vertexreco = cms.Sequence(
+#         unsortedOfflinePrimaryVertices*
+#         trackWithVertexRefSelectorBeforeSorting*
+#         trackRefsForJetsBeforeSorting*
+#         caloJetsForTrk * 
         offlinePrimaryVertices*
         offlinePrimaryVerticesWithBS*
         generalV0Candidates*
