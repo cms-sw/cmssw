bf6457ff95f6feeb90b22da9a8afeca9f0922909
Gaelle <Gaelle.Boudoul@cern.ch>
2015-09-20 19:12:22 +0200

    fix dev scenarios

--- Configuration/Geometry/python/GeometryExtended2017devReco_cff.py
+++ Configuration/Geometry/python/GeometryExtended2017devReco_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for transient ECAL alignement
-from Configuration.Geometry.GeometryExtended2017_cff import *
+from Configuration.Geometry.GeometryExtended2017dev_cff import *
 
 
 # Reconstruction geometry services
