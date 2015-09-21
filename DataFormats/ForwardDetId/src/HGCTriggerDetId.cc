ad187a15912e4c11e650b0a6f93dd3b2292d3b8f
Andrea Marini <andrea.marini@cern.ch>
2015-09-21 17:59:36 +0200

    changing trigger det id maps to include modules

--- DataFormats/ForwardDetId/src/HGCTriggerDetId.cc
+++ DataFormats/ForwardDetId/src/HGCTriggerDetId.cc
@@ -11,15 +11,15 @@ HGCTriggerDetId::HGCTriggerDetId() : DetId() {
 HGCTriggerDetId::HGCTriggerDetId(uint32_t rawid) : DetId(rawid) {
 }
 
-HGCTriggerDetId::HGCTriggerDetId(ForwardSubdetector subdet, int zp, int lay, int sec, int subsec, int cell) : DetId(Forward,subdet) {  
+HGCTriggerDetId::HGCTriggerDetId(ForwardSubdetector subdet, int zp, int lay, int sec, int mod, int cell) : DetId(Forward,subdet) {  
 
-  id_ |= (( cell & cell_mask) << cell_shift) ;
-  id_ |= (( sec & sector_mask)<< sector_shift);
-  if(subsec<0) subsec=0;
-  id_ |= (( subsec & subsector_mask) <<subsector_shift);
-  id_ |= (( lay & layer_mask) <<layer_shift);
   if( zp<0) zp = 0;
-  id_ |= (( zp & zside_mask) <<zside_shift); 
+
+  setMaskedId( cell, cell_shift  , cell_mask  );
+  setMaskedId( sec , sector_shift, sector_mask);
+  setMaskedId( mod , module_shift, module_mask);
+  setMaskedId( lay , layer_shift , layer_mask );
+  setMaskedId( zp  , zside_shift , zside_mask ) ;
 }
 
 HGCTriggerDetId::HGCTriggerDetId(const DetId& gen) {
@@ -48,7 +48,7 @@ std::ostream& operator<<(std::ostream& s,const HGCTriggerDetId& id) {
   case(HGCTrigger) : return s << "isEE=" << id.isEE() 
 			 << " zpos=" << id.zside() 
 			 << " layer=" << id.layer() 
-			 << " phi subSector=" << id.subsector()
+			 << " module=" << id.module()
 			 << " sector=" << id.sector() 
 			 << " cell=" << id.cell();
   default : return s << id.rawId();
