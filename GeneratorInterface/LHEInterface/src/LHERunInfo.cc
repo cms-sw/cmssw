86ad1743a374aca0de23d3caa311c36084f23b8a
Shin-Shan <syu@cern.ch>
2015-09-23 15:11:30 +0200

    replace cout with LogDebug

--- GeneratorInterface/LHEInterface/src/LHERunInfo.cc
+++ GeneratorInterface/LHEInterface/src/LHERunInfo.cc
@@ -296,170 +296,166 @@ LHERunInfo::XSec LHERunInfo::xsec() const
 
 void LHERunInfo::statistics() const
 {
-	// double sigSelSum = 0.0;
-	// double sigSum = 0.0;
-	// double sigBrSum = 0.0;
-	// double errSel2Sum = 0.0;
-	// double err2Sum = 0.0;
-	// double errBr2Sum = 0.0;
-	// double errMatch2Sum = 0.0;
-	// unsigned long nAccepted = 0;
-	// unsigned long nTried = 0;
-	// unsigned long nAccepted_pos = 0;
-	// unsigned long nTried_pos = 0;
-	// unsigned long nAccepted_neg = 0;
-	// unsigned long nTried_neg = 0;
-	// int idwtup = heprup.IDWTUP;
+	double sigSelSum = 0.0;
+	double sigSum = 0.0;
+	double sigBrSum = 0.0;
+	double errSel2Sum = 0.0;
+	double err2Sum = 0.0;
+	double errBr2Sum = 0.0;
+	double errMatch2Sum = 0.0;
+	unsigned long nAccepted = 0;
+	unsigned long nTried = 0;
+	unsigned long nAccepted_pos = 0;
+	unsigned long nTried_pos = 0;
+	unsigned long nAccepted_neg = 0;
+	unsigned long nTried_neg = 0;
+	int idwtup = heprup.IDWTUP;
 
-	// std::cout << std::endl;
-	// std::cout << "Process and cross-section statistics" << std::endl;
-	// std::cout << "------------------------------------" << std::endl;
-	// std::cout << "Process\t\txsec_before [pb]\t\tpassed\tnposw\tnnegw\ttried\tnposw\tnnegw \txsec_match [pb]\t\t\taccepted [%]\t event_eff [%]"
-	//           << std::endl;
+	LogDebug("LHERunInfo") << " statistics";
+	LogDebug("LHERunInfo") << "Process and cross-section statistics";
+	LogDebug("LHERunInfo") << "------------------------------------";
+	LogDebug("LHERunInfo") << "Process\t\txsec_before [pb]\t\tpassed\tnposw\tnnegw\ttried\tnposw\tnnegw \txsec_match [pb]\t\t\taccepted [%]\t event_eff [%]";
 
-	// for(std::vector<Process>::const_iterator proc = processes.begin();
-	//     proc != processes.end(); ++proc) {
-	//   unsigned int idx = proc->heprupIndex();
+	for(std::vector<Process>::const_iterator proc = processes.begin();
+	    proc != processes.end(); ++proc) {
+	  unsigned int idx = proc->heprupIndex();
 
-	// 	if (!proc->selected().n()) {
-	// 	  std::cout << proc->process() << "\t0\t0\tn/a\t\t\tn/a"
-	// 		          << std::endl;
-	// 		continue;
-	// 	}
+		if (!proc->selected().n()) {
+		  LogDebug("LHERunInfo") << proc->process() << "\t0\t0\tn/a\t\t\tn/a";
+			continue;
+		}
 
-	// 	double sigma2Sum, sigma2Err;
-	// 	sigma2Sum = heprup.XSECUP[idx]* heprup.XSECUP[idx];
-	// 	sigma2Err = heprup.XERRUP[idx]* heprup.XERRUP[idx];
+		double sigma2Sum, sigma2Err;
+		sigma2Sum = heprup.XSECUP[idx]* heprup.XSECUP[idx];
+		sigma2Err = heprup.XERRUP[idx]* heprup.XERRUP[idx];
 
-	//         double sigmaAvg = heprup.XSECUP[idx];
+	        double sigmaAvg = heprup.XSECUP[idx];
 
-	// 	double fracAcc = 0;
-	// 	double ntotal = proc->nTotalPos()-proc->nTotalNeg();
-	// 	double npass  = proc->nPassPos() -proc->nPassNeg();
-	// 	switch(idwtup){
-	// 	case 3: case -3:
-	// 	  fracAcc = ntotal > 0? npass/ntotal: -1;
-	// 	    break;
-	// 	default:
-	// 	  fracAcc = proc->selected().sum() > 0? proc->killed().sum() / proc->selected().sum():-1;
-	// 	  break;
-	// 	}
+		double fracAcc = 0;
+		double ntotal = proc->nTotalPos()-proc->nTotalNeg();
+		double npass  = proc->nPassPos() -proc->nPassNeg();
+		switch(idwtup){
+		case 3: case -3:
+		  fracAcc = ntotal > 0? npass/ntotal: -1;
+		    break;
+		default:
+		  fracAcc = proc->selected().sum() > 0? proc->killed().sum() / proc->selected().sum():-1;
+		  break;
+		}
 
 
-	// 	double fracBr = proc->accepted().sum() > 0.0 ?
-	// 	                proc->acceptedBr().sum() / proc->accepted().sum() : 1;
-	// 	double sigmaFin = fracAcc >0? sigmaAvg * fracAcc : 0;
-	// 	double sigmaFinBr = sigmaFin * fracBr;
+		double fracBr = proc->accepted().sum() > 0.0 ?
+		                proc->acceptedBr().sum() / proc->accepted().sum() : 1;
+		double sigmaFin = fracAcc >0? sigmaAvg * fracAcc : 0;
+		double sigmaFinBr = sigmaFin * fracBr;
 
-	// 	double relErr = 1.0;
-	// 	double relAccErr = 1.0;
-	// 	double efferr2=0;
+		double relErr = 1.0;
+		double relAccErr = 1.0;
+		double efferr2=0;
 
-	// 	if (proc->killed().n() > 0 && fracAcc > 0) {
-	// 		switch(idwtup) {
-	// 		case 3: case -3:
-	// 		  {
-	// 		    double ntotal_pos = proc->nTotalPos();
-	// 		    double effp  = ntotal_pos > 0?
-	// 		      (double)proc->nPassPos()/ntotal_pos:0;
-	// 		    double effp_err2 = ntotal_pos > 0?
-	// 		      (1-effp)*effp/ntotal_pos: 0;
+		if (proc->killed().n() > 0 && fracAcc > 0) {
+			switch(idwtup) {
+			case 3: case -3:
+			  {
+			    double ntotal_pos = proc->nTotalPos();
+			    double effp  = ntotal_pos > 0?
+			      (double)proc->nPassPos()/ntotal_pos:0;
+			    double effp_err2 = ntotal_pos > 0?
+			      (1-effp)*effp/ntotal_pos: 0;
 
-	// 		    double ntotal_neg = proc->nTotalNeg();
-	// 		    double effn  = ntotal_neg > 0?
-	// 		      (double)proc->nPassNeg()/ntotal_neg:0;
-	// 		    double effn_err2 = ntotal_neg > 0?
-	// 		      (1-effn)*effn/ntotal_neg: 0;
+			    double ntotal_neg = proc->nTotalNeg();
+			    double effn  = ntotal_neg > 0?
+			      (double)proc->nPassNeg()/ntotal_neg:0;
+			    double effn_err2 = ntotal_neg > 0?
+			      (1-effn)*effn/ntotal_neg: 0;
 
-	// 		    efferr2 = ntotal > 0 ? 
-	// 		      (ntotal_pos*ntotal_pos*effp_err2 +
-	// 		       ntotal_neg*ntotal_neg*effn_err2)/ntotal/ntotal:0;
-	// 		    break;
-	// 		  }
-	// 		default:
-	// 		  {
-	// 		    double denominator = pow(proc->selected().sum(),4);
-	// 		    double passw       = proc->killed().sum();
-	// 		    double passw2      = proc->killed().sum2();
-	// 		    double failw       = proc->selected().sum() - passw;
-	// 		    double failw2      = proc->selected().sum2() - passw2;
-	// 		    double numerator   = (passw2*failw*failw + failw2*passw*passw); 
+			    efferr2 = ntotal > 0 ? 
+			      (ntotal_pos*ntotal_pos*effp_err2 +
+			       ntotal_neg*ntotal_neg*effn_err2)/ntotal/ntotal:0;
+			    break;
+			  }
+			default:
+			  {
+			    double denominator = pow(proc->selected().sum(),4);
+			    double passw       = proc->killed().sum();
+			    double passw2      = proc->killed().sum2();
+			    double failw       = proc->selected().sum() - passw;
+			    double failw2      = proc->selected().sum2() - passw2;
+			    double numerator   = (passw2*failw*failw + failw2*passw*passw); 
 			    
-	// 		    efferr2 = denominator>0?
-	// 		      numerator/denominator:0;
-	// 		    break;
-	// 		  }
-	// 		}
-	// 		double delta2Veto = efferr2/fracAcc/fracAcc;
-	// 		double delta2Sum = delta2Veto
-	// 		                   + sigma2Err / sigma2Sum;
-	// 		relErr = (delta2Sum > 0.0 ?
-	// 				std::sqrt(delta2Sum) : 0.0);
-	// 		relAccErr = (delta2Veto > 0.0 ?
-	// 				std::sqrt(delta2Veto) : 0.0);
-	// 	}
-	// 	double deltaFin = sigmaFin * relErr;
-	// 	double deltaFinBr = sigmaFinBr * relErr;
+			    efferr2 = denominator>0?
+			      numerator/denominator:0;
+			    break;
+			  }
+			}
+			double delta2Veto = efferr2/fracAcc/fracAcc;
+			double delta2Sum = delta2Veto
+			                   + sigma2Err / sigma2Sum;
+			relErr = (delta2Sum > 0.0 ?
+					std::sqrt(delta2Sum) : 0.0);
+			relAccErr = (delta2Veto > 0.0 ?
+					std::sqrt(delta2Veto) : 0.0);
+		}
+		double deltaFin = sigmaFin * relErr;
+		double deltaFinBr = sigmaFinBr * relErr;
 		
-	// 	double ntotal_proc = proc->nTotalPos()+proc->nTotalNeg();
-	// 	double event_eff_proc = ntotal_proc>0? (double)(proc->nPassPos()+ proc->nPassNeg())/ntotal_proc: -1;
-	// 	double event_eff_err_proc = ntotal_proc>0? std::sqrt((1-event_eff_proc)*event_eff_proc/ntotal_proc): -1;
+		double ntotal_proc = proc->nTotalPos()+proc->nTotalNeg();
+		double event_eff_proc = ntotal_proc>0? (double)(proc->nPassPos()+ proc->nPassNeg())/ntotal_proc: -1;
+		double event_eff_err_proc = ntotal_proc>0? std::sqrt((1-event_eff_proc)*event_eff_proc/ntotal_proc): -1;
 
-	// 	std::cout << proc->process() << "\t\t"
-	// 		  << std::scientific << std::setprecision(3)
-	// 		  << heprup.XSECUP[proc->heprupIndex()] << " +/- " 
-	// 		  << heprup.XERRUP[proc->heprupIndex()] << "\t\t"
-	// 	          << proc->accepted().n() << "\t"
-	// 		  << proc->nPassPos() << "\t"
-	// 		  << proc->nPassNeg() << "\t"
-	// 	          << proc->tried().n() << "\t"
-	// 		  << proc->nTotalPos() << "\t"
-	// 		  << proc->nTotalNeg() << "\t"
-	// 	          << std::scientific << std::setprecision(3)
-	// 	          << sigmaFinBr << " +/- "
-	// 	          << deltaFinBr << "\t\t"
-	// 	          << std::fixed << std::setprecision(1)
-	// 	          << (fracAcc * 100)  << " +/- " << ( std::sqrt(efferr2) * 100) << "\t"
-	// 	          << std::fixed << std::setprecision(1)
-	// 	          << (event_eff_proc * 100) << " +/- " << ( event_eff_err_proc * 100)
-	// 		  << std::endl;
+		LogDebug("LHERunInfo") << proc->process() << "\t\t"
+			  << std::scientific << std::setprecision(3)
+			  << heprup.XSECUP[proc->heprupIndex()] << " +/- " 
+			  << heprup.XERRUP[proc->heprupIndex()] << "\t\t"
+		          << proc->accepted().n() << "\t"
+			  << proc->nPassPos() << "\t"
+			  << proc->nPassNeg() << "\t"
+		          << proc->tried().n() << "\t"
+			  << proc->nTotalPos() << "\t"
+			  << proc->nTotalNeg() << "\t"
+		          << std::scientific << std::setprecision(3)
+		          << sigmaFinBr << " +/- "
+		          << deltaFinBr << "\t\t"
+		          << std::fixed << std::setprecision(1)
+		          << (fracAcc * 100)  << " +/- " << ( std::sqrt(efferr2) * 100) << "\t"
+		          << std::fixed << std::setprecision(1)
+			  << (event_eff_proc * 100) << " +/- " << ( event_eff_err_proc * 100);
 
-	// 	nAccepted += proc->accepted().n();
-	// 	nTried += proc->tried().n();
-	// 	nAccepted_pos += proc->nPassPos();
-	// 	nTried_pos    += proc->nTotalPos();
-	// 	nAccepted_neg += proc->nPassNeg();
-	// 	nTried_neg    += proc->nTotalNeg();
-	// 	sigSelSum += sigmaAvg;
-	// 	sigSum += sigmaFin;
-	// 	sigBrSum += sigmaFinBr;
-	// 	errSel2Sum += sigma2Err;
-	// 	err2Sum += deltaFin * deltaFin;
-	// 	errBr2Sum += deltaFinBr * deltaFinBr;
-	// 	errMatch2Sum += sigmaFin*relAccErr*sigmaFin*relAccErr;
-	// }
+		nAccepted += proc->accepted().n();
+		nTried += proc->tried().n();
+		nAccepted_pos += proc->nPassPos();
+		nTried_pos    += proc->nTotalPos();
+		nAccepted_neg += proc->nPassNeg();
+		nTried_neg    += proc->nTotalNeg();
+		sigSelSum += sigmaAvg;
+		sigSum += sigmaFin;
+		sigBrSum += sigmaFinBr;
+		errSel2Sum += sigma2Err;
+		err2Sum += deltaFin * deltaFin;
+		errBr2Sum += deltaFinBr * deltaFinBr;
+		errMatch2Sum += sigmaFin*relAccErr*sigmaFin*relAccErr;
+	}
 
-	// double ntotal_all = (nTried_pos+nTried_neg);
-	// double event_eff_all = ntotal_all>0? (double)(nAccepted_pos+nAccepted_neg)/ntotal_all: -1;
-	// double event_eff_err_all = ntotal_all>0? std::sqrt((1-event_eff_all)*event_eff_all/ntotal_all): -1;
+	double ntotal_all = (nTried_pos+nTried_neg);
+	double event_eff_all = ntotal_all>0? (double)(nAccepted_pos+nAccepted_neg)/ntotal_all: -1;
+	double event_eff_err_all = ntotal_all>0? std::sqrt((1-event_eff_all)*event_eff_all/ntotal_all): -1;
 
-	// std::cout << "Total\t\t"
-	//           << std::scientific << std::setprecision(3)
-	// 	  << sigSelSum << " +/- " << std::sqrt(errSel2Sum) << "\t\t"
-	//           << nAccepted << "\t"
-	// 	  << nAccepted_pos << "\t"
-	// 	  << nAccepted_neg << "\t"
-	//           << nTried << "\t"
-	// 	  << nTried_pos << "\t"
-	// 	  << nTried_neg << "\t"
-	//           << std::scientific << std::setprecision(3)
-	//           << sigBrSum << " +/- "
-	//           << std::sqrt(errBr2Sum) << "\t\t"
-	//           << std::fixed << std::setprecision(1)
-	//           << (sigSum / sigSelSum * 100)  << " +/- " << (std::sqrt(errMatch2Sum)/sigSelSum * 100) << "\t"
-	//           << std::fixed << std::setprecision(1) 
-	//           << (event_eff_all * 100) << " +/- " << (event_eff_err_all * 100)
-	// 	  << std::endl;
+	LogDebug("LHERunInfo") << "Total\t\t"
+	          << std::scientific << std::setprecision(3)
+		  << sigSelSum << " +/- " << std::sqrt(errSel2Sum) << "\t\t"
+	          << nAccepted << "\t"
+		  << nAccepted_pos << "\t"
+		  << nAccepted_neg << "\t"
+	          << nTried << "\t"
+		  << nTried_pos << "\t"
+		  << nTried_neg << "\t"
+	          << std::scientific << std::setprecision(3)
+	          << sigBrSum << " +/- "
+	          << std::sqrt(errBr2Sum) << "\t\t"
+	          << std::fixed << std::setprecision(1)
+	          << (sigSum / sigSelSum * 100)  << " +/- " << (std::sqrt(errMatch2Sum)/sigSelSum * 100) << "\t"
+	          << std::fixed << std::setprecision(1) 
+	          << (event_eff_all * 100) << " +/- " << (event_eff_err_all * 100);
 }
 
 LHERunInfo::Header::Header() :
