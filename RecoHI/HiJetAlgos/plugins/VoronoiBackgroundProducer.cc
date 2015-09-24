f77fa0b9015138afc6ae2ae0b40090a8cb2d26cb
Yue Shi <ylai@mit.edu>
2015-09-24 14:11:33 +0200

    HF/Voronoi self subtraction plus vn controlling flags

--- RecoHI/HiJetAlgos/plugins/VoronoiBackgroundProducer.cc
+++ RecoHI/HiJetAlgos/plugins/VoronoiBackgroundProducer.cc
@@ -28,6 +28,7 @@
 #include "JetMETCorrections/Objects/interface/JetCorrectionsRecord.h"
 
 #include "VoronoiAlgorithm.h"
+#include "SameEventVoronoiAlgorithm.h"
 
 using namespace std;
 //
@@ -47,11 +48,20 @@ class VoronoiBackgroundProducer : public edm::EDProducer {
       // ----------member data ---------------------------
 
    edm::EDGetTokenT<reco::CandidateView> src_;
-   VoronoiAlgorithm* voronoi_;
+   GenericVoronoiAlgorithm* voronoi_;
    bool doEqualize_;
    double equalizeThreshold0_;
    double equalizeThreshold1_;
    double equalizeR_;
+   bool excludeV1_;
+   int maxVn_;
+   bool diagonalVn_;
+   bool sameEvent_;
+   double sameEventAntiktDistance_;
+   double sameEventExclusionPtMin_;
+   double sameEventExclusionRadius_;
+   double sameEventFakeRejectEtMax_;
+   double sameEventFakeRejectEtMaxOverMean_;
    bool useTextTable_;
    bool jetCorrectorFormat_;
    bool isCalo_;
@@ -80,6 +90,15 @@ VoronoiBackgroundProducer::VoronoiBackgroundProducer(const edm::ParameterSet& iC
    equalizeThreshold0_(iConfig.getParameter<double>("equalizeThreshold0")),
    equalizeThreshold1_(iConfig.getParameter<double>("equalizeThreshold1")),
    equalizeR_(iConfig.getParameter<double>("equalizeR")),
+   excludeV1_(iConfig.getParameter<bool>("excludeV1")),
+   maxVn_(iConfig.getParameter<int>("maxVn")),
+   diagonalVn_(iConfig.getParameter<bool>("diagonalVn")),
+   sameEvent_(iConfig.getParameter<bool>("sameEvent")),
+   sameEventAntiktDistance_(iConfig.getParameter<double>("sameEventAntiktDistance")),
+   sameEventExclusionPtMin_(iConfig.getParameter<double>("sameEventExclusionPtMin")),
+   sameEventExclusionRadius_(iConfig.getParameter<double>("sameEventExclusionRadius")),
+   sameEventFakeRejectEtMax_(iConfig.getParameter<double>("sameEventFakeRejectEtMax")),
+   sameEventFakeRejectEtMaxOverMean_(iConfig.getParameter<double>("sameEventFakeRejectEtMaxOverMean")),
    useTextTable_(iConfig.getParameter<bool>("useTextTable")),
    jetCorrectorFormat_(iConfig.getParameter<bool>("jetCorrectorFormat")),
    isCalo_(iConfig.getParameter<bool>("isCalo")),
@@ -143,7 +162,12 @@ VoronoiBackgroundProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
 	 ue = new UECalibration(ueTable->values);
 	}
 
-     voronoi_ = new VoronoiAlgorithm(ue,equalizeR_,std::pair<double, double>(equalizeThreshold0_,equalizeThreshold1_),doEqualize_);
+	if (sameEvent_) {
+		voronoi_ = new SameEventVoronoiAlgorithm(sameEventAntiktDistance_, sameEventExclusionPtMin_, sameEventExclusionRadius_, sameEventFakeRejectEtMax_, sameEventFakeRejectEtMaxOverMean_, std::pair<double, double>(equalizeThreshold0_,equalizeThreshold1_), doEqualize_);
+	}
+	else {
+	voronoi_ = new VoronoiAlgorithm(ue,equalizeR_,excludeV1_,maxVn_,diagonalVn_,std::pair<double, double>(equalizeThreshold0_,equalizeThreshold1_),doEqualize_);
+	}
    }
 
    voronoi_->clear();
