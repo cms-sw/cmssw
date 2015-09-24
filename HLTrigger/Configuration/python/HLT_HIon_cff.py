f34c3604fab3a8923df69f07431c6d4b256826da
Andrea <perrotta@cern.ch>
2015-09-24 19:00:17 +0200

    Fix a few dataset names

--- HLTrigger/Configuration/python/HLT_HIon_cff.py
+++ HLTrigger/Configuration/python/HLT_HIon_cff.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/HIon/V159 (CMSSW_7_4_10_patch1)
+# /dev/CMSSW_7_4_0/HIon/V160 (CMSSW_7_4_10_patch1)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V159')
+  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V160')
 )
 
 fragment.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
