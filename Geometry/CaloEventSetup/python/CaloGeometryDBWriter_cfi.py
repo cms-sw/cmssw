c50eece2e666c19c651c20e2adb71c0d8598ccac
Ianna Osborne <ianna.osborne@cern.ch>
2015-09-25 16:26:17 +0200

    New scripts to dump 2017 geometry payloads

--- Geometry/CaloEventSetup/python/CaloGeometryDBWriter_cfi.py
+++ Geometry/CaloEventSetup/python/CaloGeometryDBWriter_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from Geometry.EcalAlgo.EcalGeometryDBWriter_cfi import *
+from Geometry.HcalCommonData.hcalDDDRecConstants_cfi import *
 from Geometry.HcalEventSetup.HcalGeometryDBWriter_cfi import *
 from Geometry.HcalEventSetup.CaloTowerGeometryDBWriter_cfi import *
 from Geometry.HcalEventSetup.HcalTopology_cfi import *
