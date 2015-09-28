f2bd329689d845da05c20652567ea880a54ac5b6
Piet <piet.verwilligen@cern.ch>
2015-09-28 13:29:24 +0200

    work in progress on clustering

--- RecoLocalMuon/GEMSegment/plugins/GEMSegAlgoPV.cc
+++ RecoLocalMuon/GEMSegment/plugins/GEMSegAlgoPV.cc
@@ -33,6 +33,22 @@ GEMSegAlgoPV::GEMSegAlgoPV(const edm::ParameterSet& ps) : GEMSegmentAlgorithm(ps
   dPhiChainBoxMax           = ps.getParameter<double>("dPhiChainBoxMax");
   dEtaChainBoxMax           = ps.getParameter<double>("dEtaChainBoxMax");
   maxRecHitsInCluster       = ps.getParameter<int>("maxRecHitsInCluster");
+  clusterOnlySameBXRecHits  = ps.getParameter<bool>("clusterOnlySameBXRecHits");
+  // useGE21Short              = ps.getParameter<bool>("useGE21Short"); --> needed in GEMSegmentBuilder.cc
+
+  // maybe to be used in the future ???
+  // Pruning                = ps.getParameter<bool>("Pruning");
+  // BrutePruning           = ps.getParameter<bool>("BrutePruning");
+  // maxRecHitsInCluster is the maximal number of hits in a precluster that is being processed
+  // This cut is intended to remove messy events. Currently nothing is returned if there are
+  // more that maxRecHitsInCluster hits. It could be useful to return an estimate of the 
+  // cluster position, which is available.
+  // maxRecHitsInCluster    = ps.getParameter<int>("maxRecHitsInCluster");
+  // onlyBestSegment        = ps.getParameter<bool>("onlyBestSegment");
+
+  // CSC uses pruning to remove clearly bad hits, using as much info from the rechits as possible: charge, position, timing, ...
+  // In fits with bad chi^2 they look for the worst hit (hit with abnormally large residual) 
+  // if worst hit was found, refit without worst hit and select if considerably better than original fit.
 }
 
 /* Destructor
@@ -45,9 +61,9 @@ GEMSegAlgoPV::~GEMSegAlgoPV() {
 std::vector<GEMSegment> GEMSegAlgoPV::run(const GEMEnsemble& ensemble, const EnsembleHitContainer& rechits) {
 
   theEnsemble = ensemble;
-  
-  GEMDetId enId((theEnsemble.first)->id()); 
-  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::run] build segments in chamber " << enId;
+
+  GEMDetId chId((theEnsemble.first)->id());
+  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::run] build segments in chamber " << chId;
   
   // pre-cluster rechits and loop over all sub clusters separately
   std::vector<GEMSegment>          segments_temp;
@@ -59,13 +75,16 @@ std::vector<GEMSegment> GEMSegAlgoPV::run(const GEMEnsemble& ensemble, const Ens
     if(preClustering_useChaining){
       // it uses X,Y,Z information; there are no configurable parameters used;
       // the X, Y, Z "cuts" are just (much) wider than reasonable high pt segments
+      edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::run] preClustering :: use Chaining";
       rechits_clusters = this->chainHits( rechits );
     }
     else{
       // it uses X,Y information + configurable parameters
+      edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::run] Clustering";
       rechits_clusters = this->clusterHits(rechits );
     }
     // loop over the found clusters:
+      edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::run] Loop over clusters and build segments";
     for(auto sub_rechits = rechits_clusters.begin(); sub_rechits !=  rechits_clusters.end(); ++sub_rechits ) {
       // clear the buffer for the subset of segments:
       segments_temp.clear();
@@ -88,6 +107,8 @@ std::vector<GEMSegment> GEMSegAlgoPV::run(const GEMEnsemble& ensemble, const Ens
 // ********************************************************************;
 GEMSegAlgoPV::ProtoSegments 
 GEMSegAlgoPV::clusterHits(const EnsembleHitContainer& rechits) {
+
+  // think how to implement BX requirement here
 
   ProtoSegments rechits_clusters; // this is a collection of groups of rechits
 
@@ -115,15 +136,25 @@ GEMSegAlgoPV::clusterHits(const EnsembleHitContainer& rechits) {
 
     // First added hit in seed defines the mean to which the next hit is compared
     // for this seed.
+    // running_meanX.push_back( rechits[i]->localPosition().x() );
+    // running_meanY.push_back( rechits[i]->localPosition().y() );
+    GEMDetId rhID                  = rechits[i]->gemId();
+    const GEMEtaPartition * rhEP   = (theEnsemble.second.find(rhID.rawId()))->second;
+    if(!rhEP) throw cms::Exception("GEMEtaPartition not found") << "Corresponding GEMEtaPartition to GEMDetId: "<<rhID<<" not found in the GEMEnsemble";
+    const GEMSuperChamber * rhCH   = theEnsemble.first;
+    LocalPoint rhLP_inEtaPartFrame = rechits[i]->localPosition();
+    GlobalPoint rhGP_inCMSFrame    = rhEP->toGlobal(rhLP_inEtaPartFrame);
+    LocalPoint rhLP_inChamberFrame = rhCH->toLocal(rhGP_inCMSFrame);
 
-    running_meanX.push_back( rechits[i]->localPosition().x() );
-    running_meanY.push_back( rechits[i]->localPosition().y() );
+    running_meanX.push_back(rhLP_inChamberFrame.x());
+    running_meanY.push_back(rhLP_inChamberFrame.y() );
 	
     // set min/max X and Y for box containing the hits in the precluster:
-    seed_minX.push_back( rechits[i]->localPosition().x() );
-    seed_maxX.push_back( rechits[i]->localPosition().x() );
-    seed_minY.push_back( rechits[i]->localPosition().y() );
-    seed_maxY.push_back( rechits[i]->localPosition().y() );
+    seed_minX.push_back( rhLP_inChamberFrame.x() );
+    seed_maxX.push_back( rhLP_inChamberFrame.x() );
+    seed_minY.push_back( rhLP_inChamberFrame.y() );
+    seed_maxY.push_back( rhLP_inChamberFrame.y() );
+
   }
     
   // merge clusters that are too close
@@ -243,7 +274,6 @@ GEMSegAlgoPV::chainHits(const EnsembleHitContainer& rechits) {
   // hand over the final seeds to the output
   // would be more elegant if we could do the above step with
   // erasing the merged ones, rather than the
-
   for(size_t NNN = 0; NNN < seeds.size(); ++NNN) {
     if(usedCluster[NNN]) continue; //skip seeds that have been marked as used up in merging
     rechits_chains.push_back(seeds[NNN]);
@@ -258,48 +288,106 @@ bool GEMSegAlgoPV::isGoodToMerge(const EnsembleHitContainer& newChain, const Ens
 
   std::vector<float> phi_new, eta_new, phi_old, eta_old;
   std::vector<int> layer_new, layer_old;
+  // std::vector< std::vector<int> > layer_new, layer_old;
+  std::vector<int> bx_new, bx_old;
 
-  for(size_t iRH_new = 0;iRH_new<newChain.size();++iRH_new){
-    GlobalPoint pos_new = theEnsemble.first->toGlobal(newChain[iRH_new]->localPosition());
-    layer_new.push_back(newChain[iRH_new]->gemId().layer());
-    phi_new.push_back(pos_new.phi());
-    eta_new.push_back(pos_new.eta());
-  }  
-  for(size_t iRH_old = 0;iRH_old<oldChain.size();++iRH_old){
-    GlobalPoint pos_old = theEnsemble.first->toGlobal(oldChain[iRH_old]->localPosition());
-    layer_old.push_back(oldChain[iRH_old]->gemId().layer());
-    phi_old.push_back(pos_old.phi());
-    eta_old.push_back(pos_old.eta());
+
+  // Do not allow to Merge if number of hits of Merged vector > 6
+  if(newChain.size() + oldChain.size() > 6) {
+    edm::LogVerbatim("GEMSegAlgoPV") << "Merging those two vectors will lead to vector with more than 6 rechits :: n_vec1 = "<<newChain.size()<<" n_vec2 = "<<oldChain.size()<<" => Merging not allowed";
+    return false;
   }
+
+  // edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::isGoodToMerge] :: newChain ::";
+  for(size_t iRH_new = 0;iRH_new<newChain.size();++iRH_new){
+    GEMDetId rhID                  = newChain[iRH_new]->gemId();
+    const GEMEtaPartition * rhEP   = (theEnsemble.second.find(rhID.rawId()))->second;
+    if(!rhEP) throw cms::Exception("GEMEtaPartition not found") << "Corresponding GEMEtaPartition to GEMDetId: "<<rhID<<" not found in the GEMEnsemble";
+    LocalPoint rhLP_inEtaPartFrame = newChain[iRH_new]->localPosition();
+    GlobalPoint rhGP_inCMSFrame    = rhEP->toGlobal(rhLP_inEtaPartFrame);
+    layer_new.push_back((rhID.station()-1)*2+rhID.layer());
+    // std::vector<int> layer_tmp; layer_tmp.push_back((rhID.station()-1)*2+rhID.layer()); layer_new.push_back(layer_tmp);
+    phi_new.push_back(rhGP_inCMSFrame.phi());
+    eta_new.push_back(rhGP_inCMSFrame.eta());
+    bx_new.push_back(newChain[iRH_new]->BunchX());
+    // Log Message
+    #ifdef EDM_ML_DEBUG // have lines below only compiled when in debug mode
+    const GEMSuperChamber * rhCH   = theEnsemble.first;
+    LocalPoint rhLP_inChamberFrame = rhCH->toLocal(rhGP_inCMSFrame);
+    edm::LogVerbatim("GEMSegAlgoPV") << "[GoodToMerge::New Chain][RecHit :: Loc x = "<<std::showpos<<std::setw(9)<<rhLP_inChamberFrame.x()<<" Loc y = "<<std::showpos<<std::setw(9)<<rhLP_inChamberFrame.y()
+    			             << " -- Glob eta = "<<std::showpos<<std::setw(9)<<rhGP_inCMSFrame.eta()<<" Glob phi = "<<std::showpos<<std::setw(9)<<rhGP_inCMSFrame.phi()
+                                     << " -- BX = "<<newChain[iRH_new]->BunchX()
+   	                 	     << " -- "<<rhID.rawId()<<" = "<<rhID<<" ]";
+    #endif
+  }  
+
+  // edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::isGoodToMerge] :: oldChain ::";
+  for(size_t iRH_old = 0;iRH_old<oldChain.size();++iRH_old){
+    GEMDetId rhID                  = oldChain[iRH_old]->gemId();
+    const GEMEtaPartition * rhEP   = (theEnsemble.second.find(rhID.rawId()))->second;
+    if(!rhEP) throw cms::Exception("GEMEtaPartition not found") << "Corresponding GEMEtaPartition to GEMDetId: "<<rhID<<" not found in the GEMEnsemble";
+    LocalPoint rhLP_inEtaPartFrame = oldChain[iRH_old]->localPosition();
+    GlobalPoint rhGP_inCMSFrame    = rhEP->toGlobal(rhLP_inEtaPartFrame);
+    layer_old.push_back((rhID.station()-1)*2+rhID.layer());
+    // std::vector<int> layer_tmp; layer_tmp.push_back((rhID.station()-1)*2+rhID.layer()); layer_old.push_back(layer_tmp);
+    phi_old.push_back(rhGP_inCMSFrame.phi());
+    eta_old.push_back(rhGP_inCMSFrame.eta());
+    bx_old.push_back(oldChain[iRH_old]->BunchX());
+    // Log Message
+    #ifdef EDM_ML_DEBUG // have lines below only compiled when in debug mode
+    const GEMSuperChamber * rhCH   = theEnsemble.first;
+    LocalPoint rhLP_inChamberFrame = rhCH->toLocal(rhGP_inCMSFrame);
+    edm::LogVerbatim("GEMSegAlgoPV") << "[GoodToMerge::Old Chain][RecHit :: Loc x = "<<std::showpos<<std::setw(9)<<rhLP_inChamberFrame.x()<<" Loc y = "<<std::showpos<<std::setw(9)<<rhLP_inChamberFrame.y()
+                                     << " -- Glob eta = "<<std::showpos<<std::setw(9)<<rhGP_inCMSFrame.eta()<<" Glob phi = "<<std::showpos<<std::setw(9)<<rhGP_inCMSFrame.phi()
+                                     << " -- BX = "<<oldChain[iRH_old]->BunchX()
+                                     << " -- "<<rhID.rawId()<<" = "<<rhID<<" ]";
+    #endif
+  }
+
+  // Different Requirements: 
+  bool phiRequirementOK   = false; // once it is true in the loop, it is ok to merge
+  bool etaRequirementOK   = false; // once it is true in the loop, it is ok to merge
+  bool bxRequirementOK    = false; // once it is true in the loop, it is ok to merge
+  bool layerRequirementOK = true;  // once it is false in the loop, it is not ok to merge
 
   for(size_t jRH_new = 0; jRH_new<phi_new.size(); ++jRH_new){
     for(size_t jRH_old = 0; jRH_old<phi_old.size(); ++jRH_old){
 
-      // to be chained, two hits need to be in neighbouring layers...
-      // or better allow few missing layers (upto 3 to avoid inefficiencies);
-      // however we'll not make an angle correction because it
-      // worsen the situation in some of the "regular" cases 
-      // (not making the correction means that the conditions for
-      // forming a cluster are different if we have missing layers -
-      // this could affect events at the boundaries ) 
-
+      // Eta & Phi
       // to be chained, two hits need also to be "close" in phi and eta
-      bool phiRequirementOK = std::abs(reco::deltaPhi(phi_new[jRH_new],phi_old[jRH_old])) < dPhiChainBoxMax;
-      bool etaRequirementOK = fabs(eta_new[jRH_new]-eta_old[jRH_old]) < dEtaChainBoxMax;
-      // and the difference in layer index should be < (nlayers-1) // hardcoded here for GE2/1 with 2x2 layers
-      bool layerRequirementOK = abs(layer_new[jRH_new]-layer_old[jRH_old]) < 3;
+      // allow only change false --> true. once true, leave it true
+      if(phiRequirementOK==false) phiRequirementOK = std::abs(reco::deltaPhi(phi_new[jRH_new],phi_old[jRH_old])) < dPhiChainBoxMax;
+      if(etaRequirementOK==false) etaRequirementOK = fabs(eta_new[jRH_new]-eta_old[jRH_old]) < dEtaChainBoxMax;
 
-      if(layerRequirementOK && phiRequirementOK && etaRequirementOK){
-        return true;
+      // Layers
+      // hits on the same layer should not be allowed ==> if abs(layer_new - layer_old) > 0 is ok. if = 0 is false
+      // allow only change true --> false. once false, leave it false
+      if(layerRequirementOK==true) { 
+	layerRequirementOK = abs(layer_new[jRH_new]-layer_old[jRH_old]) > 0;
+	// edm::LogVerbatim("GEMSegAlgoPV") << "[GoodToMerge::Compare] :: layer_new = "<<layer_new[jRH_new]<<" layer_old = "<<layer_old[jRH_old]<<" ==> bool = "<<layerRequirementOK;
+      }
+
+      // BX
+      // and rechits should have the same BX if required so by the parameterset
+      // allow only change false --> true. once true, leave it true
+      if(bxRequirementOK==false) {
+	if(!clusterOnlySameBXRecHits) bxRequirementOK = true; // no BX requirements
+	else {
+	  if(bx_new[jRH_new]==bx_old[jRH_old]) bxRequirementOK = true; // only true if both rechits have same BX
+	}
       }
     }
+  }
+
+  // Now take decision whether to merge or not to merge
+  if(layerRequirementOK && phiRequirementOK && etaRequirementOK && bxRequirementOK){
+    edm::LogVerbatim("GEMSegAlgoPV") << "[GoodToMerge:: true]";
+    return true; // problem is that return is already given before the second element of the vector old_chain or new_chain is tested ... this logic needs to be changed
+
   } 
+  edm::LogVerbatim("GEMSegAlgoPV") << "[GoodToMerge:: false]";
   return false;
 }
-
-
-
-
 
 std::vector<GEMSegment> GEMSegAlgoPV::buildSegments(const EnsembleHitContainer& rechits) {
   std::vector<GEMSegment> gemsegs;
@@ -314,19 +402,39 @@ std::vector<GEMSegment> GEMSegAlgoPV::buildSegments(const EnsembleHitContainer& 
   }
 
   edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] will now try to fit a GEMSegment from collection of "<<rechits.size()<<" GEM RecHits";
+  #ifdef EDM_ML_DEBUG // have lines below only compiled when in debug mode 
+  for (auto rh=rechits.begin(); rh!=rechits.end(); ++rh){
+    auto gemid = (*rh)->gemId();
+    // auto rhr = gemGeom->etaPartition(gemid);
+    auto rhLP = (*rh)->localPosition();
+    // auto rhGP = rhr->toGlobal(rhLP);
+    edm::LogVerbatim("GEMSegAlgoPV") << "[RecHit :: Loc x = "<<std::showpos<<std::setw(9)<<rhLP.x()<<" Loc y = "<<std::showpos<<std::setw(9)<<rhLP.y()
+				     <<" BX = "<<std::showpos<<(*rh)->BunchX()<<" -- "<<gemid.rawId()<<" = "<<gemid<<" ]";
+  }
+  #endif
 
 
 
   // The actual fit on all hits of the vector of the selected Tracking RecHits:
   sfit_ = std::unique_ptr<GEMSegFit>(new GEMSegFit(theEnsemble.first, theEnsemble.second, rechits));
-  sfit_->fit();
-  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] GEMSegment fit done";
+  bool goodfit = sfit_->fit();
+  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] GEMSegment fit done :: fit is good = "<<goodfit;
+
+  // quit function if fit was not OK
+  if(!goodfit) return gemsegs;
 
   // obtain all information necessary to make the segment:
   LocalPoint protoIntercept      = sfit_->intercept();
   LocalVector protoDirection     = sfit_->localdir();
   AlgebraicSymMatrix protoErrors = sfit_->covarianceMatrix(); 
   double protoChi2               = sfit_->chi2();
+
+  // Calculate the bunch crossing of the GEM Segment
+  float bx = 0.0;
+  for (auto rh=rechits.begin(); rh!=rechits.end(); ++rh){
+    bx += (*rh)->BunchX();
+  }
+  if(rechits.size() != 0) bx=bx*1.0/(rechits.size());
 
   // Calculate the central value and uncertainty of the segment time
   // if we want to study impact of 2-3ns time resolution on GEM Segment 
@@ -354,10 +462,11 @@ std::vector<GEMSegment> GEMSegAlgoPV::buildSegments(const EnsembleHitContainer& 
 
   // save all information inside GEMCSCSegment
   edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] will now wrap fit info in GEMSegment dataformat";
+  // GEMSegment tmp(proto_segment, protoIntercept, protoDirection, protoErrors, protoChi2);
+  GEMSegment tmp(proto_segment, protoIntercept, protoDirection, protoErrors, protoChi2, bx);
   // GEMSegment tmp(proto_segment, protoIntercept, protoDirection, protoErrors, protoChi2, averageTime, timeUncrt);
-  GEMSegment tmp(proto_segment, protoIntercept, protoDirection, protoErrors, protoChi2);
 
-  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] GEMSegment made";
+  edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] GEMSegment made in "<<tmp.gemDetId();
   edm::LogVerbatim("GEMSegAlgoPV") << "[GEMSegAlgoPV::buildSegments] "<<tmp;
 
   gemsegs.push_back(tmp);
