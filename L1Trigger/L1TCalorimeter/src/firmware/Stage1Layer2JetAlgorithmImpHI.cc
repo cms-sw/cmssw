d2cdb14cba4e75597719ca523d823650f9a06d05
R. Alex Barbieri <luck@mit.edu>
2015-09-28 18:09:34 +0200

    change the verbosity

--- L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2JetAlgorithmImpHI.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2JetAlgorithmImpHI.cc
@@ -20,6 +20,13 @@ Stage1Layer2JetAlgorithmImpHI::Stage1Layer2JetAlgorithmImpHI(CaloParamsStage1* p
 
 Stage1Layer2JetAlgorithmImpHI::~Stage1Layer2JetAlgorithmImpHI(){};
 
+unsigned int pack16bits(int pt, int eta, int phi)
+{
+  return( ((pt & 0x3f)) + ((eta & 0xf) << 6) + ((phi & 0x1f) << 10));
+}
+
+void verboseDumpJets(const std::vector<l1t::Jet> &jets);
+
 void Stage1Layer2JetAlgorithmImpHI::processEvent(const std::vector<l1t::CaloRegion> & regions,
 						 const std::vector<l1t::CaloEmCand> & EMCands,
 						 std::vector<l1t::Jet> * jets,
@@ -31,20 +38,24 @@ void Stage1Layer2JetAlgorithmImpHI::processEvent(const std::vector<l1t::CaloRegi
   std::vector<l1t::CaloRegion> *subRegions = new std::vector<l1t::CaloRegion>();
   std::vector<l1t::Jet> *unSortedJets = new std::vector<l1t::Jet>();
   std::vector<l1t::Jet> *preGtEtaJets = new std::vector<l1t::Jet>();
+  std::vector<l1t::Jet> *preRankJets = new std::vector<l1t::Jet>();
 
   HICaloRingSubtraction(regions, subRegions, regionPUSParams, regionPUSType);
-  //TwoByTwoFinder(subRegions, unSortedJets);
-  slidingWindowJetFinder(0, subRegions, unSortedJets);
+  TwoByTwoFinder(subRegions, preRankJets);
+  //slidingWindowJetFinder(0, subRegions, unSortedJets);
+  JetToGtPtScales(params_, preRankJets, unSortedJets);
+  verboseDumpJets(*unSortedJets);
   SortJets(unSortedJets, preGtEtaJets);
   JetToGtEtaScales(params_, preGtEtaJets, preGtJets);
-  JetToGtPtScales(params_, preGtJets, jets);
+  JetToGtEtaScales(params_, preGtEtaJets, jets);
+  //JetToGtPtScales(params_, preGtJets, jets);
 
   delete subRegions;
   delete unSortedJets;
   delete preGtEtaJets;
 
-  const bool verbose = true;
-  const bool hex = true;
+  const bool verbose = false;
+  const bool hex = false;
   if(verbose)
   {
     if(!hex)
@@ -80,22 +91,78 @@ void Stage1Layer2JetAlgorithmImpHI::processEvent(const std::vector<l1t::CaloRegi
 	ajets[itJet - jets->begin()] = *itJet;
       }
       std::cout << "Jets (hex)" << std::endl;
-      std::cout << std::hex << pack15bits(ajets[0].hwPt(), ajets[0].hwEta(), ajets[0].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[0].hwPt(), ajets[0].hwEta(), ajets[0].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[1].hwPt(), ajets[1].hwEta(), ajets[1].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[1].hwPt(), ajets[1].hwEta(), ajets[1].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[4].hwPt(), ajets[4].hwEta(), ajets[4].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[4].hwPt(), ajets[4].hwEta(), ajets[4].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[5].hwPt(), ajets[5].hwEta(), ajets[5].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[5].hwPt(), ajets[5].hwEta(), ajets[5].hwPhi());
       std::cout << std::endl;
-      std::cout << std::hex << pack15bits(ajets[2].hwPt(), ajets[2].hwEta(), ajets[2].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[2].hwPt(), ajets[2].hwEta(), ajets[2].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[3].hwPt(), ajets[3].hwEta(), ajets[3].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[3].hwPt(), ajets[3].hwEta(), ajets[3].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[6].hwPt(), ajets[6].hwEta(), ajets[6].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[6].hwPt(), ajets[6].hwEta(), ajets[6].hwPhi());
       std::cout << " ";
-      std::cout << std::hex << pack15bits(ajets[7].hwPt(), ajets[7].hwEta(), ajets[7].hwPhi());
+      std::cout << std::hex << pack16bits(ajets[7].hwPt(), ajets[7].hwEta(), ajets[7].hwPhi());
       std::cout << std::endl;
     }
+  }
+}
+
+void verboseDumpJets(const std::vector<l1t::Jet> &jets)
+{
+  int fwPhi[18] = {	4,
+		   	3 ,
+		   	2  ,
+		   	1   ,
+		   	0   ,
+		   	17  ,
+		   	16  ,
+		   	15  ,
+		   	14  ,
+		   	13  ,
+		  	12  ,
+		  	11  ,
+		  	10  ,
+		  	9   ,
+		  	8   ,
+		  	7   ,
+		  	6   ,
+		  	5   };
+
+  int fwEta[22] = {0,
+		   1,
+		   2,
+		   3,
+    		   0,
+		   1,
+		   2,
+		   3,
+		   4,
+		   5,
+		   6,
+		   0,
+		   1,
+		   2,
+		   3,
+		   4,
+		   5,
+		   6,
+		   0,
+		   1,
+		   2,
+    		   3};
+
+
+  std::cout << "pt eta phi" << std::endl;
+  for(std::vector<l1t::Jet>::const_iterator itJet = jets.begin();
+	  itJet != jets.end(); ++itJet){
+
+    std::cout << itJet->hwPt() << " ";
+    std::cout << fwEta[itJet->hwEta()] << " " ;
+    std::cout << fwPhi[itJet->hwPhi()] << std::endl;
+
   }
 }
