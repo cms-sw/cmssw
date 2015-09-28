c5e777bdcb56937e0905173945d5910c82ab4350
Piet <piet.verwilligen@cern.ch>
2015-09-28 13:21:41 +0200

    up to date to test

--- RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
+++ RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
@@ -9,17 +9,30 @@ process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
 # CSCGeometry depends on alignment ==> necessary to provide GlobalPositionRecord
 process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi") 
 process.load("Geometry.CSCGeometry.cscGeometry_cfi")
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         'file:out_local_reco_gemsegment.root'
+        # 'file:out_local_reco_noise_gemsegment.root'
+        # 'file:out_local_reco_gemsegment_5000evt.root'
         # 'file:out_local_reco_gemsegment_allevts.root'
+        # 'file:out_local_reco_test_gemsegment.root'
     )
 )
 
 process.gemseg = cms.EDAnalyzer('TestGEMSegmentAnalyzer',
+                              # ----------------------------------------------------------------------
                               RootFileName = cms.untracked.string("TestGEMSegmentHistograms.root"),
+                              # RootFileName = cms.untracked.string("TestGEMSegmentHistograms_Noise.root"),
+                              # RootFileName = cms.untracked.string("TestGEMSegmentHistograms_5000evt.root"),
+                              # RootFileName = cms.untracked.string("TestGEMSegmentHistograms_Test.root"),
+                              # ----------------------------------------------------------------------
+                              printSegmntInfo = cms.untracked.bool(True),
+                              printResidlInfo = cms.untracked.bool(True),
+                              printSimHitInfo = cms.untracked.bool(False),
+                              printEventOrder = cms.untracked.bool(False),
+                              # ----------------------------------------------------------------------
 
 )
 
