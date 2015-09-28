6c9c6630a2a75ec246ba0948f582ea8985603e70
Shervin <shervin@cern.ch>
2015-09-28 12:36:52 +0200

    Updated ALCARECO event content: more esplicit list of collections, marginal change in list of collections

--- Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
+++ Calibration/EcalAlCaRecoProducers/python/ALCARECOEcalCalIsolElectron_Output_cff.py
@@ -1,20 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-# output block for alcastream Electron
-OutALCARECOEcalCalElectron_specific = cms.untracked.vstring(
-    'drop reco*Clusters_hfEMClusters_*_*',
-    'drop reco*Clusters_pfPhotonTranslator_*_*',
-    'drop *EcalRecHit*_ecalRecHit_*_*',
-    'drop *EcalrecHit*_*ecalPreshowerRecHit*_*EcalRecHitsES*_*',
-    'drop *EcalRecHit*_reducedEcalRecHitsE*_*_*',
-    'drop *_*Unclean*_*_*',
-    'drop *_*unclean*_*_*',
-    'drop *_*_*Unclean*_*',
-    'drop *_*_*unclean*_*',
-    'drop *CaloCluster*_*particleFlowEGamma*_*EBEEClusters*_*',
-    'drop *CaloCluster*_*particleFlowEGamma*_*ESClusters*_*',
-    'keep *CaloCluster*_*alCaIsolatedElectrons*_*alcaCaloCluster*_*'
-)
 
 OutALCARECOEcalCalElectron_noDrop = cms.PSet(
     # put this if you have a filter
@@ -22,47 +7,78 @@ OutALCARECOEcalCalElectron_noDrop = cms.PSet(
     SelectEvents = cms.vstring('pathALCARECOEcalCalZElectron', 'pathALCARECOEcalCalWElectron', 'pathALCARECOEcalCalZSCElectron')
     ),
     outputCommands = cms.untracked.vstring( 
-    'keep *_pfMet_*_*', # met for Wenu selection
+    'keep *_pfMet_*_*',
+    'keep recoPFMETs_pfMet_*_*', # met for Wenu selection
+
     'keep *_kt6PFJetsForRhoCorrection_rho_*', #rho for effective area subtraction
     'keep *_kt6PFJets_rho_*', #rho for effective area subtraction
-    'keep recoVertexs_offlinePrimaryVertices*_*_*',
-    'keep *BeamSpot_offlineBeamSpot_*_*',
+    'keep *_addPileupInfo_*_*',
+
+    'keep *_generator_*_*',
+    'keep *_genParticles_*_*',
+
+    'keep recoBeamSpot_offlineBeamSpot_*_*',
+    'keep recoVertexs_offlinePrimaryVerticesWithBS_*_*',
+    'keep recoVertexs_offlinePrimaryVertices_*_*',
+    'keep recoGsfTracks_electronGsfTracks_*_*',
+    'keep *GsfTrack*_*_*_*', # gsfTrackExtra
+    'keep *_alcaElectronTracksReducer_*_*',
+
     'keep *_allConversions_*_*',
     'keep *_conversions_*_*',
-    'keep *GsfTrack*_*_*_*',
-    'keep *_generator_*_*',
-    'keep *_addPileupInfo_*_*',
-    'keep *_genParticles_*_*',
-    'keep recoGsfElectron*_gsfElectron*_*_*',
-    'keep recoGsfElectron*_gedGsfElectron*_*_*',
-    'keep recoPhoton*_gedPhoton_*_*',
-    'keep recoCaloClusters_*_*_*',
-    'keep recoSuperClusters_*_*_*',
-    'keep recoPreshowerCluster*_*_*_*',
+
+    'keep recoGsfElectrons_gsfElectrons_*_*',
+    'keep recoGsfElectronCores_gsfElectronCores_*_*',
+    'keep recoGsfElectrons_gedGsfElectrons_*_*',
+    'keep recoGsfElectronCores_gedGsfElectronCores_*_*',
+
+    'keep recoPhotons_gedPhotons_*_*',
+
+    'keep recoConversions_allConversions_*_*',
+    'keep recoConversions_conversions_*_*',
+
+    'keep *EcalRecHit*_alCaIsolatedElectrons_*_*',
+    'keep *EcalRecHit*_reducedEcalRecHitsES_alCaRecHitsES_*',
+
+    'keep recoCaloClusters_alCaIsolatedElectrons_alcaCaloCluster_*', ## producer to be fixed
+    'keep recoCaloClusters_hybridSuperClusters_hybridBarrelBasicClusters_*',
+    'keep recoCaloClusters_multi5x5SuperClusters_multi5x5EndcapBasicClusters_*',
+    'keep recoCaloClusters_particleFlowSuperClusterECAL_particleFlowBasicClusterECALEndcap_*',
+    'keep recoCaloClusters_particleFlowSuperClusterECAL_particleFlowBasicClusterECALBarrel_*',
+    'keep recoCaloClusters_particleFlowSuperClusterECAL_particleFlowBasicClusterECALPreshower_*',
+    'keep recoCaloClusters_particleFlowEGamma_EBEEClusters_*',
+    'keep recoCaloClusters_particleFlowEGamma_ESClusters_*',
+
+    'keep recoSuperClusters_correctedHybridSuperClusters_*_*',
+    'keep recoSuperClusters_correctedMulti5x5SuperClustersWithPreshower_*_*',
+    'keep recoSuperClusters_particleFlowSuperClusterECAL_particleFlowSuperClusterECALBarrel_*',
+    'keep recoSuperClusters_particleFlowSuperClusterECAL_particleFlowSuperClusterECALEndcapWithPreshower_*',
+    'keep recoSuperClusters_particleFlowEGamma_*_*',
+    'keep recoSuperClusters_SCselector_*_*',
+
+    'keep recoPreshowerClusters_multi5x5SuperClustersWithPreshower_preshowerYClusters_*',
+    'keep recoPreshowerClusters_multi5x5SuperClustersWithPreshower_preshowerXClusters_*',
+    'keep recoPreshowerClusterShapes_multi5x5PreshowerClusterShape_multi5x5PreshowerYClustersShape_*',
+    'keep recoPreshowerClusterShapes_multi5x5PreshowerClusterShape_multi5x5PreshowerXClustersShape_*',
+
     'keep *_pfElectronTranslator_*_*',
-    #'keep *_*_*_HLT',
-    #'keep *_generalTracks_*_*',
-    #'keep reco*Track*Extra*_generalTracks_*_*',
-    'keep *_alcaElectronTracksReducer_*_*',
+
     # for the trigger matching
-    'keep *_l1extraParticles_*_*',
+    'keep L1GlobalTriggerObjectMaps_l1L1GtObjectMap_*_*',
     'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
+    'keep l1extraL1EmParticles_l1extraParticles_*_*',
     'keep *_l1L1GtObjectMap_*_*',
     'keep edmConditionsInEventBlock_conditionsInEdm_*_*',
     'keep edmConditionsInLumiBlock_conditionsInEdm_*_*',
     'keep edmConditionsInRunBlock_conditionsInEdm_*_*',
-    'keep *_TriggerResults_*_*',
-    'keep *_hltTriggerSummaryAOD_*_HLT',
-    # pfisolation CMSSW_5_3_X
-    'keep *EcalRecHit*_alCaIsolatedElectrons_*_*',
-    'keep *EcalRecHit*_reducedEcalRecHitsES_alCaRecHitsES_*',
+    'keep edmTriggerResults_TriggerResults_*_*',
+    'keep triggerTriggerEvent_hltTriggerSummaryAOD_*_*',
     )
 )
 
 import copy
 OutALCARECOEcalCalElectron=copy.deepcopy(OutALCARECOEcalCalElectron_noDrop)
 OutALCARECOEcalCalElectron.outputCommands.insert(0, "drop *")
-OutALCARECOEcalCalElectron.outputCommands+=OutALCARECOEcalCalElectron_specific
 
 OutALCARECOEcalCalWElectron=copy.deepcopy(OutALCARECOEcalCalElectron)
 OutALCARECOEcalCalWElectron_noDrop=copy.deepcopy(OutALCARECOEcalCalElectron_noDrop)
