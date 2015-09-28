7bbba38a197627f839f727d9fb0f13ad6bf6de99
Laurent Thomas <laurent.thomas@cern.ch>
2015-09-28 10:03:15 -0400

    small corrections

--- RecoMET/METFilters/python/metFilters_cff.py
+++ RecoMET/METFilters/python/metFilters_cff.py
@@ -1,4 +1,4 @@
-B1;3409;0cimport FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms
 
 ## The iso-based HBHE noise filter ___________________________________________||
 from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import *
@@ -12,6 +12,9 @@ from RecoMET.METFilters.CSCTightHaloTrkMuUnvetoFilter_cfi import *
 
 ## The CSC beam halo tight filter ____________________________________________||
 from RecoMET.METFilters.CSCTightHalo2015Filter_cfi import *
+
+## The hcal problematic strip halo filter ____________________________________________||
+from RecoMET.METFilters.HcalStripHaloFilter_cfi import *
 
 ## The HCAL laser filter _____________________________________________________||
 from RecoMET.METFilters.hcalLaserEventFilter_cfi import *
@@ -63,6 +66,7 @@ metFilters = cms.Sequence(
    HBHENoiseFilter *
    primaryVertexFilter*
 #   HBHENoiseIsoFilter*
+#   HcalStripHaloFilter *
    CSCTightHaloFilter *
 #   hcalLaserEventFilter *
 #  CSCTightHaloTrkMuUnvetoFilter *
