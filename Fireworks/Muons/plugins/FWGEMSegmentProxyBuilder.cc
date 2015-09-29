14361a9659a0107644ae8df31f8930507aea3ba1
Piet <piet.verwilligen@cern.ch>
2015-09-29 17:20:42 +0200

    changed from trapezoid to box in the hope to see segments

--- Fireworks/Muons/plugins/FWGEMSegmentProxyBuilder.cc
+++ Fireworks/Muons/plugins/FWGEMSegmentProxyBuilder.cc
@@ -57,8 +57,10 @@ FWGEMSegmentProxyBuilder::build( const GEMSegment& iData,
   setupAddElement( segmentSet, &oItemHolder );
 
   TEveGeoShape* shape = item()->getGeom()->getEveShape( rawid );
-  if( TGeoTrap* trap = dynamic_cast<TGeoTrap*>( shape->GetShape())) // Trapezoidal
+  // if( TGeoTrap* trap = dynamic_cast<TGeoTrap*>( shape->GetShape())) // Trapezoidal --- taken from CSC
+  if( TGeoBBox* box = dynamic_cast<TGeoBBox*>( shape->GetShape()))     // Box         --- taken from ME0
   {
+
      LocalPoint pos = iData.localPosition();
      LocalVector dir = iData.localDirection();   
      LocalVector unit = dir.unit();
@@ -67,8 +69,10 @@ FWGEMSegmentProxyBuilder::build( const GEMSegment& iData,
      Double_t localDirectionIn[3]  = {  dir.x(),  dir.y(),  dir.z() };
      Double_t localDirectionOut[3] = { -dir.x(), -dir.y(), -dir.z() };
   
-     float distIn = trap->DistFromInside( localPosition, localDirectionIn );
-     float distOut = trap->DistFromInside( localPosition, localDirectionOut );
+     // float distIn = trap->DistFromInside( localPosition, localDirectionIn );
+     // float distOut = trap->DistFromInside( localPosition, localDirectionOut );
+     float distIn = box->DistFromInside( localPosition, localDirectionIn );
+     float distOut = box->DistFromInside( localPosition, localDirectionOut );
      LocalVector vIn = unit * distIn;
      LocalVector vOut = -unit * distOut;
      float localSegmentInnerPoint[3] = { static_cast<float>(localPosition[0] + vIn.x()),
@@ -88,6 +92,10 @@ FWGEMSegmentProxyBuilder::build( const GEMSegment& iData,
 
      segmentSet->AddLine( globalSegmentInnerPoint[0], globalSegmentInnerPoint[1], globalSegmentInnerPoint[2],
 			  globalSegmentOuterPoint[0], globalSegmentOuterPoint[1], globalSegmentOuterPoint[2] );	
+
+     std::cout<<"GEMSegment in DetId = "<<iData.gemDetId()<<" = "<<rawid<<" globalSegmentInnerPoint = ["
+	      <<globalSegmentInnerPoint[0]<<","<<globalSegmentInnerPoint[1]<<","<<globalSegmentInnerPoint[2]<<"] "
+	      <<"globalSegmentOuterPoint = ["<<globalSegmentOuterPoint[0]<<","<<globalSegmentOuterPoint[1]<<","<<globalSegmentOuterPoint[2]<<"]"<<std::endl;
   }
 }
 
