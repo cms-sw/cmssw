5e226b3d09ec9352c23fae505fd5c98774d5de47
Pat <mojoe137@cern.ch>
2015-09-30 23:29:34 +0200

    add pixel lumi to text output for wbm and correct pcc cross section to Aug. 15' value.

--- DQM/PixelLumi/plugins/PixelLumiDQM.cc
+++ DQM/PixelLumi/plugins/PixelLumiDQM.cc
@@ -42,8 +42,6 @@
 #include <time.h>
 #include <vector>
 
-const unsigned int PixelLumiDQM::lastBunchCrossing;
-
 // Constructors and destructor.
 PixelLumiDQM::PixelLumiDQM(const edm::ParameterSet& iConfig):
   fPixelClusterLabel(consumes<edmNew::DetSetVector<SiPixelCluster> >(iConfig.getUntrackedParameter<edm::InputTag>("pixelClusterLabel",
@@ -668,6 +666,12 @@ PixelLumiDQM::endLuminosityBlock(edm::LuminosityBlock const& lumiBlock,
   strftime(datestring, sizeof(datestring),"%Y.%m.%d %T GMT %s",ts);
   logFile_ << "RunNumber "<< fRunNo << std::endl;
   logFile_ << "EndTimeOfFit " << datestring << std::endl;
+  logFile_ << "LumiRange "<< ls << "-" << ls << std::endl;
+  logFile_ << "Fill "<< -99 << std::endl;
+  logFile_ << "ActiveBunchCrossings "<< filledAndUnmaskedBunches << std::endl;
+  logFile_ << "PixelLumi "<< fHistTotalRecordedLumiByLS->getBinContent(ls) * 0.98 << std::endl;
+  logFile_ << "HFLumi "<< -99 << std::endl;
+  logFile_ << "Ratio " << -99 << std::endl;
   logFile_.close();
 }
 
