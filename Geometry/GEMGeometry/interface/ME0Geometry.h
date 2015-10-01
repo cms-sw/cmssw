3a97060206aa31df3ed27d0de70b0b6121610652
Piet <piet.verwilligen@cern.ch>
2015-10-01 12:29:36 +0200

    added ME0Layer and ME0Chamber to ME0Geometry

--- Geometry/GEMGeometry/interface/ME0Geometry.h
+++ Geometry/GEMGeometry/interface/ME0Geometry.h
@@ -11,6 +11,7 @@
 #include "DataFormats/DetId/interface/DetId.h"
 #include "Geometry/CommonDetUnit/interface/TrackingGeometry.h"
 #include "Geometry/GEMGeometry/interface/ME0EtaPartition.h"
+#include "Geometry/GEMGeometry/interface/ME0Chamber.h"
 #include <vector>
 #include <map>
 
@@ -52,11 +53,29 @@ class ME0Geometry : public TrackingGeometry {
   /// Return a vector of all ME0 eta partitions
   const std::vector<ME0EtaPartition const*>& etaPartitions() const;
 
+  /// Return a vector of all ME0 layers
+  const std::vector<ME0Layer const*>& layers() const;
+
+  /// Return a vector of all ME0 chambers
+  const std::vector<ME0Chamber const*>& chambers() const;
+
   /// Return a etaPartition given its id
   const ME0EtaPartition* etaPartition(ME0DetId id) const;
 
+  /// Return a layer given its id
+  const ME0Layer* layer(ME0DetId id) const;
+
+  /// Return a chamber given its id
+  const ME0Chamber* chamber(ME0DetId id) const;
+
   /// Add a ME0 etaPartition  to the Geometry
   void add(ME0EtaPartition* etaPartition);
+
+  /// Add a ME0 layer  to the Geometry
+  void add(ME0Layer* layer);
+
+  /// Add a ME0 chamber  to the Geometry
+  void add(ME0Chamber* chamber);
 
  private:
   DetUnitContainer theEtaPartitions;
@@ -68,7 +87,9 @@ class ME0Geometry : public TrackingGeometry {
   // Map for efficient lookup by DetId 
   mapIdToDet theMap;
 
-  std::vector<ME0EtaPartition const*> allEtaPartitions; // Are not owned by this class; are owned by their chamber.
+  std::vector<ME0EtaPartition const*> allEtaPartitions; // Are not owned by this class; are owned by their layer.
+  std::vector<ME0Layer const*> allLayers;               // Are not owned by this class; are owned by their chamber        
+  std::vector<ME0Chamber const*> allChambers;           // I do not know what the statements above exactly mean        
 
 };
 
