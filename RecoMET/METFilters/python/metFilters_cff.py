019f21d9859754760ae00a969589162a83140364
Laurent Thomas <laurent.thomas@cern.ch>
2015-10-01 14:40:49 -0400

    Update CSC Halo Filter

--- RecoMET/METFilters/python/metFilters_cff.py
+++ RecoMET/METFilters/python/metFilters_cff.py
@@ -7,6 +7,15 @@ from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
 ## The CSC beam halo tight filter ____________________________________________||
 from RecoMET.METFilters.CSCTightHaloFilter_cfi import *
 
+## The CSC beam halo tight filter ____________________________________________||
+from RecoMET.METFilters.CSCTightHaloTrkMuUnvetoFilter_cfi import *
+
+## The CSC beam halo tight filter ____________________________________________||
+from RecoMET.METFilters.CSCTightHalo2015Filter_cfi import *
+
+## The hcal problematic strip halo filter ____________________________________________||
+from RecoMET.METFilters.HcalStripHaloFilter_cfi import *
+
 ## The HCAL laser filter _____________________________________________________||
 from RecoMET.METFilters.hcalLaserEventFilter_cfi import *
 
@@ -57,8 +66,11 @@ metFilters = cms.Sequence(
    HBHENoiseFilter *
    primaryVertexFilter*
 #   HBHENoiseIsoFilter*
+#   HcalStripHaloFilter *
    CSCTightHaloFilter *
 #   hcalLaserEventFilter *
+#  CSCTightHaloTrkMuUnvetoFilter *
+# CSCTightHalo2015Filter *
    EcalDeadCellTriggerPrimitiveFilter* 
 #   *goodVertices * trackingFailureFilter *
    eeBadScFilter
