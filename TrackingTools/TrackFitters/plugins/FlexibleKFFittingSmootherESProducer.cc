180d611ee35c9b5ad9bfc1209ecab5a29aeaa4ed
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-10-01 19:15:03 +0200

    more private

--- TrackingTools/TrackFitters/plugins/FlexibleKFFittingSmootherESProducer.cc
+++ TrackingTools/TrackFitters/plugins/FlexibleKFFittingSmootherESProducer.cc
@@ -1,17 +1,13 @@
 
-#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/ModuleFactory.h"
 #include "FWCore/Framework/interface/ESProducer.h"
-
-#include <string>
-#include <memory>
-
-using namespace edm;
-
 
 #include "FWCore/Framework/interface/ESProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+
+
 #include "TrackingTools/TrackFitters/interface/TrajectoryFitterRecord.h"
 
 #include "TrackingTools/TrackFitters/interface/TrajectoryFitter.h"
@@ -19,87 +15,94 @@ using namespace edm;
 
 namespace {
 
-class FlexibleKFFittingSmoother GCC11_FINAL : public TrajectoryFitter {
+  class FlexibleKFFittingSmoother final : public TrajectoryFitter {
+  public:
+        ~FlexibleKFFittingSmoother(){}
 
-public:
-  /// constructor with predefined fitter and smoother and propagator
-  FlexibleKFFittingSmoother(const TrajectoryFitter& standardFitter,
-			    const TrajectoryFitter& looperFitter) :
+  private:
+    /// constructor with predefined fitter and smoother and propagator
+    FlexibleKFFittingSmoother(const TrajectoryFitter& standardFitter,
+			      const TrajectoryFitter& looperFitter) :
       theStandardFitter(standardFitter.clone()),
       theLooperFitter(looperFitter.clone()) {}
-
-  virtual ~FlexibleKFFittingSmoother(){};
-
-  Trajectory fitOne(const Trajectory& t,fitType type) const{ return fitter(type)->fitOne(t,type);}
-
-
-  Trajectory fitOne(const TrajectorySeed& aSeed,
-		    const RecHitContainer& hits,
-		    const TrajectoryStateOnSurface& firstPredTsos,
-		    fitType type) const {return fitter(type)->fitOne(aSeed,hits,firstPredTsos,type); }
-
-  Trajectory fitOne(const TrajectorySeed& aSeed,
-		    const RecHitContainer& hits,
-		    fitType type) const { return fitter(type)->fitOne(aSeed,hits,type); }
-
-  std::unique_ptr<TrajectoryFitter> clone() const override{
-    return std::unique_ptr<TrajectoryFitter>(
-        new FlexibleKFFittingSmoother(*theStandardFitter,*theLooperFitter));
-        }
-
- // FIXME a prototype:  final inplementaiton may differ
-  virtual void setHitCloner(TkCloner const * hc) {
-	theStandardFitter->setHitCloner(hc);
-        theLooperFitter->setHitCloner(hc);
-  }
-
- private:
-
-        const TrajectoryFitter* fitter(fitType type) const {
+    
+    
+    Trajectory fitOne(const Trajectory& t,fitType type) const override { return fitter(type)->fitOne(t,type);}
+    
+    
+    Trajectory fitOne(const TrajectorySeed& aSeed,
+		      const RecHitContainer& hits,
+		      const TrajectoryStateOnSurface& firstPredTsos,
+		      fitType type) const override {return fitter(type)->fitOne(aSeed,hits,firstPredTsos,type); }
+    
+    Trajectory fitOne(const TrajectorySeed& aSeed,
+		      const RecHitContainer& hits,
+		      fitType type) const override  { return fitter(type)->fitOne(aSeed,hits,type); }
+    
+    std::unique_ptr<TrajectoryFitter> clone() const override {
+      return std::unique_ptr<TrajectoryFitter>(new FlexibleKFFittingSmoother(*theStandardFitter,*theLooperFitter));
+    }
+    
+    // FIXME a prototype:  final inplementaiton may differ
+    void setHitCloner(TkCloner const * hc) override {
+      theStandardFitter->setHitCloner(hc);
+      theLooperFitter->setHitCloner(hc);
+    }
+    
+  private:
+    
+    const TrajectoryFitter* fitter(fitType type) const {
       return (type==standard) ? theStandardFitter.get() : theLooperFitter.get();
     }
-
+    
     const std::unique_ptr<TrajectoryFitter> theStandardFitter;
     const std::unique_ptr<TrajectoryFitter> theLooperFitter;
 
+
+    friend class  FlexibleKFFittingSmootherESProducer;
+    
   };
-
-
-
-class  FlexibleKFFittingSmootherESProducer: public edm::ESProducer{
- public:
-  FlexibleKFFittingSmootherESProducer(const edm::ParameterSet & p) 
-  {
-    std::string myname = p.getParameter<std::string>("ComponentName");
-    pset_ = p;
-    setWhatProduced(this,myname);
-  }
-  
-  ~FlexibleKFFittingSmootherESProducer() {}
-  
-  boost::shared_ptr<TrajectoryFitter> 
-  produce(const TrajectoryFitterRecord & iRecord){ 
-    
-    std::string standardFitterName = pset_.getParameter<std::string>("standardFitter");
-    std::string looperFitterName = pset_.getParameter<std::string>("looperFitter");
-    
-    edm::ESHandle<TrajectoryFitter> standardFitter;
-    edm::ESHandle<TrajectoryFitter> looperFitter;
-    
-    iRecord.get(standardFitterName,standardFitter);
-    iRecord.get(looperFitterName,looperFitter);
-    
-    _fitter  = boost::shared_ptr<TrajectoryFitter>(new FlexibleKFFittingSmoother(*standardFitter.product(),
-									       *looperFitter.product()   ) );
-    return _fitter;
-  }
   
   
-private:
-  boost::shared_ptr<TrajectoryFitter> _fitter;
-  edm::ParameterSet pset_;
-};
+  
+  class  FlexibleKFFittingSmootherESProducer: public edm::ESProducer{
+  public:
+    FlexibleKFFittingSmootherESProducer(const edm::ParameterSet & p) 
+    {
+      std::string myname = p.getParameter<std::string>("ComponentName");
+      pset_ = p;
+      setWhatProduced(this,myname);
+    }
+    
+    ~FlexibleKFFittingSmootherESProducer() {}
 
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+      edm::ParameterSetDescription desc;
+      desc.add<std::string>("ComponentName","FlexibleKFFittingSmoother");
+      desc.add<std::string>("standardFitter","KFFittingSmootherWithOutliersRejectionAndRK");
+      desc.add<std::string>("looperFitter","LooperFittingSmoother");
+      descriptions.add("FlexibleKFFittingSmootherESProducer", desc);
+    }
+    
+    boost::shared_ptr<TrajectoryFitter> 
+    produce(const TrajectoryFitterRecord & iRecord){ 
+      
+      edm::ESHandle<TrajectoryFitter> standardFitter;
+      edm::ESHandle<TrajectoryFitter> looperFitter;
+      
+      iRecord.get(pset_.getParameter<std::string>("standardFitter"),standardFitter);
+      iRecord.get(pset_.getParameter<std::string>("looperFitter"),looperFitter);
+      
+      return boost::shared_ptr<TrajectoryFitter>(new FlexibleKFFittingSmoother(*standardFitter.product(),
+									       *looperFitter.product())
+						 );
+    }
+    
+    
+  private:
+    edm::ParameterSet pset_;
+  };
+  
 }
 
 
