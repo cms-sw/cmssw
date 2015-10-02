f7e8613dbf12f191062f1c259628378859e258f2
Hengne <Hengne.Li@CERN.CH>
2015-10-02 10:52:32 +0200

    add finer job splitting for 2015 data workflows

--- Configuration/PyReleaseValidation/python/MatrixInjector.py
+++ Configuration/PyReleaseValidation/python/MatrixInjector.py
@@ -177,6 +177,10 @@ class MatrixInjector(object):
             wmsplit['TTbar_13_ID']=1
             wmsplit['SingleMuPt10FS_ID']=1
             wmsplit['TTbarFS_ID']=1
+            wmsplit['RECODR250nsreHLT']=1
+            wmsplit['RECODR225nsreHLT']=1
+            wmsplit['HLTDR250ns']=3
+            wmsplit['HLTDR225ns']=3
                                     
             #import pprint
             #pprint.pprint(wmsplit)            
