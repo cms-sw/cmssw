db3f79403fb67a324937951fa6d942e4c2ecbeda
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2015-10-02 12:49:33 +0200

    Included the changes of #11619(76X).

--- HLTriggerOffline/Exotica/python/analyses/hltExoticaPFHT_cff.py
+++ HLTriggerOffline/Exotica/python/analyses/hltExoticaPFHT_cff.py
@@ -5,6 +5,8 @@ PFHTPSet = cms.PSet(
         "HLT_PFHT650_WideJetMJJ900DEtaJJ1p5_v",
         "HLT_PFHT650_WideJetMJJ950DEtaJJ1p5_v",
         "HLT_PFHT750_4Jet_v",
+        "HLT_PFHT550_4JetPt50_v",
+        "HLT_PFHT650_4JetPt50_v",
         "HLT_PFHT750_4JetPt50_v",
         "HLT_PFHT650_4Jet_v", # Run2
         "HLT_PFHT550_4Jet_v", # Run2
