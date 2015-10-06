299a071a2460e9414b57353f9f4de4d4240ad490
Sunanda <sunanda.banerjee@cern.ch>
2015-10-06 11:26:39 +0200

    Validation package for HGCal

---
+++ Validation/HGCalValidation/python/rechitValidationV6_cff.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+from Validation.HGCalValidation.rechitValidation_cfi import *
+
+hgcalRecHitValidationHEF = hgcalRecHitValidationEE.clone(
+    DetectorName  = cms.string("HGCalHESiliconSensitive"),
+    RecHitSource  = cms.InputTag("HGCalRecHit", "HGCHEFRecHits"))
+
+hgcalRecHitValidationHEB = hgcalRecHitValidationEE.clone(
+    DetectorName  = cms.string("HCal"),
+    RecHitSource  = cms.InputTag("hbhereco"))
