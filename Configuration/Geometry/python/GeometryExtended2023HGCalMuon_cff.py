f32659844c6d4cb24952c7f318252b7f3332cfb1
Sunanda <sunanda.banerjee@cern.ch>
2015-10-07 21:31:50 +0200

    Add a scenario with PhaseI tracker, PhaseII muon for HGCAL

---
+++ Configuration/Geometry/python/GeometryExtended2023HGCalMuon_cff.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+from Geometry.CMSCommonData.cmsExtendedGeometry2023HGCalMuonXML_cfi import *
+from Geometry.TrackerNumberingBuilder.trackerTopology_cfi import *
+from Geometry.HcalCommonData.hcalParameters_cfi      import *
+from Geometry.HcalCommonData.hcalDDDSimConstants_cfi import *
