ff728ff4db7835182b0b0aeb38b3e9730a77edb1
Lindsey Gray <lindsey.gray@gmail.com>
2015-10-08 14:16:17 +0200

    use values from the digitizer directly

--- RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py
+++ RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # from SimCalorimetry.HGCSimProducers.hgcDigiProducers_cff import *
-#from SimGeneral.MixingModule.hgcalDigitizer_cfi import *
+from SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi import *
 # HGCAL rechit producer
 HGCalRecHit = cms.EDProducer("HGCalRecHitProducer",
                              HGCEErechitCollection = cms.string('HGCEERecHits'),
@@ -12,17 +12,17 @@ HGCalRecHit = cms.EDProducer("HGCalRecHitProducer",
                              HGCHEBuncalibRecHitCollection = cms.InputTag("HGCalUncalibRecHit","HGCHEBUncalibRecHits"),
                              # digi constants
                              
-                             HGCEEmipInKeV = cms.double(55.1),#hgceeDigitizer.digiCfg.mipInKeV,
-                             HGCEElsbInMIP = cms.double(0.25),#hgceeDigitizer.digiCfg.lsbInMIP,
-                             HGCEEmip2noise = cms.double(7.0),#cms.double(hgceeDigitizer.digiCfg.mip2noise,
+                             HGCEEmipInKeV = hgceeDigitizer.digiCfg.mipInKeV,
+                             HGCEElsbInMIP = hgceeDigitizer.digiCfg.lsbInMIP,
+                             HGCEEmip2noise = hgceeDigitizer.digiCfg.mip2noise,
 
-                             HGCHEFmipInKeV = cms.double(85.0),#hgchefrontDigitizer.digiCfg.mipInKeV,
-                             HGCHEFlsbInMIP = cms.double(0.25),#hgchefrontDigitizer.digiCfg.lsbInMIP,
-                             HGCHEFmip2noise = cms.double(7.0),#hgchefrontDigitizer.digiCfg.mip2noise,
+                             HGCHEFmipInKeV = hgchefrontDigitizer.digiCfg.mipInKeV,
+                             HGCHEFlsbInMIP = hgchefrontDigitizer.digiCfg.lsbInMIP,
+                             HGCHEFmip2noise = hgchefrontDigitizer.digiCfg.mip2noise,
 
-                             HGCHEBmipInKeV = cms.double(1498.4),#hgchebackDigitizer.digiCfg.mipInKeV,
-                             HGCHEBlsbInMIP = cms.double(0.25),#hgchebackDigitizer.digiCfg.lsbInMIP,
-                             HGCHEBmip2noise = cms.double(5.0),#hgchebackDigitizer.digiCfg.mip2noise,
+                             HGCHEBmipInKeV = hgchebackDigitizer.digiCfg.mipInKeV,
+                             HGCHEBlsbInMIP = hgchebackDigitizer.digiCfg.lsbInMIP,
+                             HGCHEBmip2noise = hgchebackDigitizer.digiCfg.mip2noise,
                              
                              # algo
                              algo = cms.string("HGCalRecHitWorkerSimple")
