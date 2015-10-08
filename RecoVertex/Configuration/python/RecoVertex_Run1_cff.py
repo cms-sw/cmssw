ac31083705f0328aa078844a2d9d9854c71a10fb
Piet <piet.verwilligen@cern.ch>
2015-10-08 18:27:14 +0200

    configuration file to run Run1 vertexing

---
+++ RecoVertex/Configuration/python/RecoVertex_Run1_cff.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+# Reco Vertex
+# initialize magnetic field #########################
+from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
+from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import *
+from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesWithBS_cfi import *
+from RecoVertex.V0Producer.generalV0Candidates_cff import *
+from RecoVertex.AdaptiveVertexFinder.inclusiveVertexing_cff import *
+
+from CommonTools.RecoAlgos.TrackWithVertexRefSelector_cfi import *
+from RecoJets.JetProducers.TracksForJets_cff import *
+from CommonTools.RecoAlgos.sortedPrimaryVertices_cfi import *
+from RecoJets.JetProducers.caloJetsForTrk_cff import *
+
+vertexreco = cms.Sequence(
+        offlinePrimaryVertices*
+        offlinePrimaryVerticesWithBS*
+        generalV0Candidates*
+        inclusiveVertexing
+)
