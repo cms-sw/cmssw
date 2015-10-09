554b625744527f4eb2c6217b2f7a4ecc593f7f89
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-10-09 15:52:38 +0200

    forgot

---
+++ RecoTracker/TrackProducer/interface/AlgoProductTraits.h
@@ -0,0 +1,21 @@
+#ifndef AlgoProductTraits_H
+#define AlgoProductTraits_H
+
+#include "DataFormats/TrajectorySeed/interface/PropagationDirection.h"
+#include <vector>
+class Trajectory;
+
+
+template <class T>
+class AlgoProductTraits {
+public:
+  using TrackCollection=std::vector<T>;
+  struct  AlgoProduct {
+    Trajectory * trajectory;
+    T * track;
+    PropagationDirection pDir;
+    int indexInput;
+  };
+  using AlgoProductCollection = std::vector< AlgoProduct >;
+};
+#endif  //AlgoProductTraits_H
