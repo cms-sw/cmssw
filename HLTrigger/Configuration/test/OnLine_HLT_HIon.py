5f794ed255d32b300b6762d35d981a973e5855d3
Andrea <perrotta@cern.ch>
2015-10-11 22:57:10 +0200

    Update HLT menu up to /dev/CMSSW_7_4_0/HLT/V535

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/HIon/V167 (CMSSW_7_4_13)
+# /dev/CMSSW_7_4_0/HIon/V168 (CMSSW_7_4_13)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V167')
+  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V168')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
--- HLTrigger/Configuration/test/OnLine_HLT_PIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_PIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/PIon/V167 (CMSSW_7_4_13)
+# /dev/CMSSW_7_4_0/PIon/V168 (CMSSW_7_4_13)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTPIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V167')
+  tableName = cms.string('/dev/CMSSW_7_4_0/PIon/V168')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
