b6ff8f490b0e5e4d2b3db42062ba3d954c1f49ed
Igor <i.volobouev@ttu.eu>
2015-10-13 17:56:41 +0200

    fixing config file to initialize Method 3

--- RecoLocalCalo/HcalRecProducers/python/HcalSimpleReconstructor_hf_cfi.py
+++ RecoLocalCalo/HcalRecProducers/python/HcalSimpleReconstructor_hf_cfi.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+import RecoLocalCalo.HcalRecProducers.HBHEMethod3Parameters_cfi as method3
 
 hfreco = cms.EDProducer("HcalSimpleReconstructor",
     correctionPhaseNS = cms.double(0.0),
@@ -9,7 +10,11 @@ hfreco = cms.EDProducer("HcalSimpleReconstructor",
     dropZSmarkedPassed = cms.bool(True),
     firstSample = cms.int32(4),
     samplesToAdd = cms.int32(2),
-    tsFromDB = cms.bool(True)
+    tsFromDB = cms.bool(True),
+    # Configuration parameters for Method 3
+    pedestalSubtractionType = cms.int32(method3.pedestalSubtractionType),
+    pedestalUpperLimit      = cms.double(method3.pedestalUpperLimit),
+    timeSlewParsType        = cms.int32(method3.timeSlewParsType),
+    timeSlewPars            = cms.vdouble(method3.timeSlewPars),
+    respCorrM3              = cms.double(method3.respCorrM3)
 )
-
-
