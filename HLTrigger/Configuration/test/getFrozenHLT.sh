fd6d8ad285e45633e561838a01783f15d202ce57
Andrea <perrotta@cern.ch>
2015-10-14 14:28:43 +0200

    Update also the dump of the frozen menu 25ns v4

--- HLTrigger/Configuration/test/getFrozenHLT.sh
+++ HLTrigger/Configuration/test/getFrozenHLT.sh
@@ -6,7 +6,7 @@ TABLES="Fake 50ns_5e33_v1 25ns14e33_v1 50ns_5e33_v3 25ns14e33_v3 25ns14e33_v4"
 # Do not update the frozen menus in 74X
 # TABLES="Fake Fake"
 HLT_Fake="/dev/CMSSW_7_4_0/Fake"
-HLT_25ns14e33_v4="/frozen/2015/25ns14e33/v4.3/HLT"
+HLT_25ns14e33_v4="/frozen/2015/25ns14e33/v4.4/HLT"
 HLT_50ns_5e33_v3="/frozen/2015/50ns_5e33/v3.0/HLT"
 HLT_25ns14e33_v3="/frozen/2015/25ns14e33/v3.3/HLT"
 HLT_50ns_5e33_v1="/frozen/2015/50ns_5e33/v1.2/HLT"
