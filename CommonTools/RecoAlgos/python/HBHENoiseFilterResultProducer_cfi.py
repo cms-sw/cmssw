c085e6645150142d59c2a178bd11299ba519f9d4
dertexaner <halilsaka@gmail.com>
2015-10-14 16:05:04 +0200

    Switch to Run2-25ns as defult, use bunchSpacingProducer

--- CommonTools/RecoAlgos/python/HBHENoiseFilterResultProducer_cfi.py
+++ CommonTools/RecoAlgos/python/HBHENoiseFilterResultProducer_cfi.py
@@ -6,13 +6,10 @@ HBHENoiseFilterResultProducer = cms.EDProducer(
     minHPDHits = cms.int32(17),
     minHPDNoOtherHits = cms.int32(10),
     minZeros = cms.int32(9999),
-    IgnoreTS4TS5ifJetInLowBVRegion = cms.bool(True),
-    defaultDecision = cms.string("HBHENoiseFilterResultRun1"),
+    IgnoreTS4TS5ifJetInLowBVRegion = cms.bool(False),
+    defaultDecision = cms.string("HBHENoiseFilterResultRun2Loose"),
     minNumIsolatedNoiseChannels = cms.int32(10),
     minIsolatedNoiseSumE = cms.double(50.0),
-    minIsolatedNoiseSumEt = cms.double(25.0)
+    minIsolatedNoiseSumEt = cms.double(25.0),
+    useBunchSpacingProducer = cms.bool(True)
 )
-
-from Configuration.StandardSequences.Eras import eras
-eras.run2_common.toModify(HBHENoiseFilterResultProducer, IgnoreTS4TS5ifJetInLowBVRegion=False)
-eras.run2_25ns_specific.toModify(HBHENoiseFilterResultProducer, defaultDecision="HBHENoiseFilterResultRun2Loose")
