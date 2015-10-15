95786e06d02ebb1a8016c2bd1dcfe55239a0c9fe
Sunanda <sunanda.banerjee@cern.ch>
2015-10-15 11:00:29 +0200

    Delete unwanted hcalTopologyConstants_cfi

--- Configuration/Geometry/python/constantsHCALPostLS2.py
+++ Configuration/Geometry/python/constantsHCALPostLS2.py
@@ -1,11 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-import Geometry.HcalEventSetup.hcalTopologyConstants_cfi
-
 def customise(process):
-
-    process.hcalTopologyIdeal.hcalTopologyConstants = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
-    process.es_hardcode.hcalTopologyConstants = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
-    process.CaloTowerHardcodeGeometryEP = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
 
     return process
--- Configuration/Geometry/python/constantsPostLS2.py
+++ Configuration/Geometry/python/constantsPostLS2.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-import Geometry.HcalEventSetup.hcalTopologyConstants_cfi
 import Geometry.TrackerGeometryBuilder.trackerSLHCGeometryConstants_cfi as trackerGeometryConstants_cfi
 
 def customise(process):
@@ -11,9 +10,5 @@ def customise(process):
     process.trackerNumberingGeometry.fromDDD = cms.bool( True )
     process.trackerNumberingGeometry.layerNumberPXB = cms.uint32(18)
     process.trackerNumberingGeometry.totalBlade = cms.uint32(56)
-
-    process.hcalTopologyIdeal.hcalTopologyConstants = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
-    process.es_hardcode.hcalTopologyConstants = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
-    process.CaloTowerHardcodeGeometryEP = cms.PSet(hcalTopologyConstants_cfi.hcalTopologyConstants)
 
     return process
