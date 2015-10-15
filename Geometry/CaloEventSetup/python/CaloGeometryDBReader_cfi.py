43a24059e6a080d7a73ec7555a07befbcd174125
Sunanda <sunanda.banerjee@cern.ch>
2015-10-15 12:40:04 +0200

    Remove all reference to HcalTopology_cfi.py

--- Geometry/CaloEventSetup/python/CaloGeometryDBReader_cfi.py
+++ Geometry/CaloEventSetup/python/CaloGeometryDBReader_cfi.py
@@ -5,6 +5,6 @@ from Geometry.CaloEventSetup.CaloGeometryBuilder_cfi import *
 from Geometry.EcalAlgo.EcalGeometryDBReader_cfi import *
 from Geometry.HcalEventSetup.HcalGeometryDBReader_cfi import *
 from Geometry.HcalEventSetup.CaloTowerGeometryDBReader_cfi import *
-from Geometry.HcalEventSetup.HcalTopology_cfi import *
+from Geometry.HcalEventSetup.hcalTopologyIdeal_cfi import *
 from Geometry.ForwardGeometry.ForwardGeometryDBReader_cfi import *
 
--- Geometry/CaloEventSetup/python/CaloGeometryDBWriter_cfi.py
+++ Geometry/CaloEventSetup/python/CaloGeometryDBWriter_cfi.py
@@ -4,6 +4,6 @@ from Geometry.EcalAlgo.EcalGeometryDBWriter_cfi import *
 from Geometry.HcalCommonData.hcalDDDRecConstants_cfi import *
 from Geometry.HcalEventSetup.HcalGeometryDBWriter_cfi import *
 from Geometry.HcalEventSetup.CaloTowerGeometryDBWriter_cfi import *
-from Geometry.HcalEventSetup.HcalTopology_cfi import *
+from Geometry.HcalEventSetup.hcalTopologyIdeal_cfi import *
 from Geometry.ForwardGeometry.ForwardGeometryDBWriter_cfi import *
 
