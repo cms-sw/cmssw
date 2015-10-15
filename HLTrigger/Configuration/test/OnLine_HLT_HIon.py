a87d0a0f57444d15e28429b37c01248ec8692c97
Andrea <perrotta@cern.ch>
2015-10-15 12:15:29 +0200

    Small fixes to the HLT menu

--- HLTrigger/Configuration/test/OnLine_HLT_HIon.py
+++ HLTrigger/Configuration/test/OnLine_HLT_HIon.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_7_4_0/HIon/V172 (CMSSW_7_4_15)
+# /dev/CMSSW_7_4_0/HIon/V173 (CMSSW_7_4_15)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLTHIon" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V172')
+  tableName = cms.string('/dev/CMSSW_7_4_0/HIon/V173')
 )
 
 process.HLTIter4PSetTrajectoryFilterIT = cms.PSet( 
