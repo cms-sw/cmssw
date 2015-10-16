9fc6eb9afb3b2fc1daabbbd4d7508d633d2938de
Piet <piet.verwilligen@cern.ch>
2015-10-16 12:53:40 +0200

    development

--- RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
+++ RecoLocalMuon/GEMSegment/test/testgemsegementanalyzer_cfg.py
@@ -9,7 +9,8 @@ process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
 # CSCGeometry depends on alignment ==> necessary to provide GlobalPositionRecord
 process.load("Alignment.CommonAlignmentProducer.FakeAlignmentSource_cfi") 
 process.load("Geometry.CSCGeometry.cscGeometry_cfi")
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+# process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -28,8 +29,8 @@ process.gemseg = cms.EDAnalyzer('TestGEMSegmentAnalyzer',
                               # RootFileName = cms.untracked.string("TestGEMSegmentHistograms_5000evt.root"),
                               # RootFileName = cms.untracked.string("TestGEMSegmentHistograms_Test.root"),
                               # ----------------------------------------------------------------------
-                              printSegmntInfo = cms.untracked.bool(True),
-                              printResidlInfo = cms.untracked.bool(True),
+                              printSegmntInfo = cms.untracked.bool(False),
+                              printResidlInfo = cms.untracked.bool(False),
                               printSimHitInfo = cms.untracked.bool(False),
                               printEventOrder = cms.untracked.bool(False),
                               # ----------------------------------------------------------------------
